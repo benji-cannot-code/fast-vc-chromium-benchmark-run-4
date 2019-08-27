af2e973941886b380d814af7fdcb3f3e55eaad4a
Vadym Doroshenko <dvadym@chromium.org>
2019-08-27 16:05:33 +0000

    Use NewPasswordFormManager in sync_credentials_filter_unittest.cc
    This CL replaces PasswordFormManager->NewPasswordFormManager. The main
    difference is that a constructor of form managers are different:
    PasswordFormManager takes PasswordForm
    NewPasswordFormManager takes FormData (which is converted to PasswordForm
    inside NewPasswordFormManager during parsing).
    
    Bug: 949519
    Change-Id: Ibd1ddda5f9a20495f00cea9cc2ceed6f7ba2a5e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772876
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690748}

--- components/password_manager/core/browser/sync_username_test_base.cc
+++ components/password_manager/core/browser/sync_username_test_base.cc
@@ -6,10 +6,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/sync_username_test_base.h"
 
 #include "base/strings/utf_string_conversions.h"
+#include "components/autofill/core/common/form_data.h"
 
+using autofill::FormData;
+using autofill::FormFieldData;
 using autofill::PasswordForm;
+using base::ASCIIToUTF16;
 
 namespace password_manager {
+
+namespace {
+
+FormData CreateSigninFormData(const GURL& url, const char* username) {
+  FormData form;
+  form.url = url;
+  FormFieldData field;
+  field.name = ASCIIToUTF16("username_element");
+  field.form_control_type = "text";
+  field.value = ASCIIToUTF16(username);
+  form.fields.push_back(field);
+
+  field.name = ASCIIToUTF16("password_element");
+  field.form_control_type = "password";
+  field.value = ASCIIToUTF16("strong_pw");
+  form.fields.push_back(field);
+  return form;
+}
+
+}  // namespace
 
 SyncUsernameTestBase::SyncUsernameTestBase() = default;
 
@@ -33,7 +57,8 @@ void SyncUsernameTestBase::FakeSigninAs(const std::string& email) {
 PasswordForm SyncUsernameTestBase::SimpleGaiaForm(const char* username) {
   PasswordForm form;
   form.signon_realm = "https://accounts.google.com";
-  form.username_value = base::ASCIIToUTF16(username);
+  form.username_value = ASCIIToUTF16(username);
+  form.form_data = CreateSigninFormData(GURL(form.signon_realm), username);
   return form;
 }
 
@@ -41,7 +66,8 @@ PasswordForm SyncUsernameTestBase::SimpleGaiaForm(const char* username) {
 PasswordForm SyncUsernameTestBase::SimpleNonGaiaForm(const char* username) {
   PasswordForm form;
   form.signon_realm = "https://site.com";
-  form.username_value = base::ASCIIToUTF16(username);
+  form.username_value = ASCIIToUTF16(username);
+  form.form_data = CreateSigninFormData(GURL(form.signon_realm), username);
   return form;
 }
 
@@ -50,8 +76,9 @@ PasswordForm SyncUsernameTestBase::SimpleNonGaiaForm(const char* username,
                                                      const char* origin) {
   PasswordForm form;
   form.signon_realm = "https://site.com";
-  form.username_value = base::ASCIIToUTF16(username);
+  form.username_value = ASCIIToUTF16(username);
   form.origin = GURL(origin);
+  form.form_data = CreateSigninFormData(GURL(form.signon_realm), username);
   return form;
 }
 
