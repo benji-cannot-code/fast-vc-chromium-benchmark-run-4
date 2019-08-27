3e3081308d0c78e3f6e6979920080cd1d1c9d803
Michael Lippautz <mlippautz@chromium.org>
2019-08-27 14:20:30 +0000

    renderer: Avoid accessing stale reference in pre-finalizer
    Pre-finalizers run after marking has finished and V8 has reset
    TracedGlobal handles. It is unsafe to access TracedGlobal and
    TraceWrapperV8Reference in pre-finalizers.
    
    Previously, such handles would have been in cleared state. With the
    recent changes they are just dangling pointers. Follow up changes
    in V8 will add proper checks to avoid such accesses.
    
    Bug: 997996, 995684
    Change-Id: Ifb0d8f47846fe9369bc15b38e37be11f4cfd5822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772880
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690712}

--- third_party/blink/renderer/core/events/promise_rejection_event.h
+++ third_party/blink/renderer/core/events/promise_rejection_event.h
@@ -19,7 +19,6 @@ namespace blink {
 
 class CORE_EXPORT PromiseRejectionEvent final : public Event {
   DEFINE_WRAPPERTYPEINFO();
-  USING_PRE_FINALIZER(PromiseRejectionEvent, Dispose);
 
  public:
   static PromiseRejectionEvent* Create(
@@ -47,7 +46,6 @@ class CORE_EXPORT PromiseRejectionEvent final : public Event {
 
  private:
   ~PromiseRejectionEvent() override;
-  void Dispose();
 
   scoped_refptr<DOMWrapperWorld> world_;
   TraceWrapperV8Reference<v8::Value> promise_;
