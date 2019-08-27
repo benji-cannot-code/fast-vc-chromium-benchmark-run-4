93f1e57bcc1bd976906c2972355be33683a64d5e
Hitoshi Yoshida <peria@chromium.org>
2019-08-27 05:22:59 +0000

    IDL Compiler: Group operations by their identifiers
    Bug: 839389
    Change-Id: Ica834921eb62d4367116eb52aa64507db7f6e343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1750540
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690635}

--- third_party/blink/renderer/bindings/scripts/web_idl/idl_compiler.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/idl_compiler.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import itertools
+
 from .callback_function import CallbackFunction
 from .callback_interface import CallbackInterface
 from .composition_parts import Identifier
@@ -14,6 +16,7 @@ from .ir_map import IRMap
 from .idl_type import IdlTypeFactory
 from .interface import Interface
 from .make_copy import make_copy
+from .operation import OperationGroup
 from .reference import RefByIdFactory
 from .typedef import Typedef
 from .union import Union
@@ -79,6 +82,9 @@ class IdlCompiler(object):
         self._merge_partial_dictionaries()
         # Merge mixins.
         self._merge_interface_mixins()
+
+        self._group_overloaded_functions()
+
         # Process inheritances.
         self._process_interface_inheritances()
 
@@ -154,6 +160,25 @@ class IdlCompiler(object):
                     make_copy(to_be_merged.constants))
                 new_interface.operations.extend(
                     make_copy(to_be_merged.operations))
+            self._ir_map.add(new_interface)
+
+    def _group_overloaded_functions(self):
+        old_interfaces = self._ir_map.find_by_kind(IRMap.IR.Kind.INTERFACE)
+
+        self._ir_map.move_to_new_phase()
+
+        for old_interface in old_interfaces.itervalues():
+            assert not old_interface.operation_groups
+            new_interface = make_copy(old_interface)
+
+            sort_key = lambda x: x.identifier
+            sorted_operations = sorted(new_interface.operations, key=sort_key)
+            new_interface.operation_groups = [
+                OperationGroup.IR(operations=list(operations))
+                for identifier, operations in itertools.groupby(
+                    sorted_operations, key=sort_key) if identifier
+            ]
+
             self._ir_map.add(new_interface)
 
     def _process_interface_inheritances(self):
