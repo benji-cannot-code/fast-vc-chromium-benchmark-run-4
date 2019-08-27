bf384ff1c2a63c50d1846f0b4f6206f528ece002
Jack Lynch <jalyn@microsoft.com>
2019-08-27 21:59:55 +0000

    DevTools: Performance Event Log duration filter label
    This CL adds an aria-label to the select box in the Performance panel
    Event Log pane that filters by duration in order to make its purpose
    clearer to screen reader users and meet WCAG 1.3.1.
    
    Screenshot of the select in question: https://gyazo.com/9ecb8d9699a05fbb83fcf6f0925f3c47
    
    Bug: 963183
    Change-Id: I887f6192b08dbff113d9ac61fa185ffa9ce195ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1723804
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Commit-Queue: Jack Lynch <jalyn@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#690895}

--- third_party/blink/renderer/devtools/front_end/timeline/EventsTimelineTreeView.js
+++ third_party/blink/renderer/devtools/front_end/timeline/EventsTimelineTreeView.js
@@ -168,6 +168,7 @@ Timeline.EventsTimelineTreeView.Filters = class extends Common.Object {
                        Common.UIString('Show all records'),
           String(durationMs)));
     }
+    UI.ARIAUtils.setAccessibleName(durationFilterUI.selectElement(), ls`Duration filter`);
     toolbar.appendToolbarItem(durationFilterUI);
 
     const categoryFiltersUI = {};
