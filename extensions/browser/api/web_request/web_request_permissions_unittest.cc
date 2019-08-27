4b96b7799f08632c0d5c964c962cd247fbe92174
Takashi Sakamoto <tasak@google.com>
2019-08-27 05:10:02 +0000

    Fix ExtensionWebRequestPermissionsTest.CanExtensionAccessURLWithWithheldPermissions crash
    TBR=haraken@chromium.org
    
    Change-Id: Ie0f24d3f71a6662250c24ebb21fb69238f1153ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771641
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690633}

--- extensions/browser/api/web_request/web_request_permissions_unittest.cc
+++ extensions/browser/api/web_request/web_request_permissions_unittest.cc
@@ -194,6 +194,7 @@ TEST_F(ExtensionWebRequestPermissionsTest, TestHideRequestForURL) {
 
 TEST_F(ExtensionWebRequestPermissionsTest,
        CanExtensionAccessURLWithWithheldPermissions) {
+  ExtensionsAPIClient api_client;
   scoped_refptr<const Extension> extension =
       ExtensionBuilder("ext").AddPermission("<all_urls>").Build();
   URLPatternSet all_urls(
