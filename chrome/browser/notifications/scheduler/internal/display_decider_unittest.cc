91f6681add3d52a949cd23ef5892243cf42de11d
Xing Liu <xingliu@chromium.org>
2019-08-27 00:37:26 +0000

    Notification scheduler: Remove distribute policy.
    This CL removes unused distribute policy.
    
    Bug: 991010
    Change-Id: I95cc48305bfb93b376382a845cba0638245bab72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769224
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690570}

--- chrome/browser/notifications/scheduler/internal/display_decider_unittest.cc
+++ chrome/browser/notifications/scheduler/internal/display_decider_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/stringprintf.h"
 #include "base/test/task_environment.h"
-#include "chrome/browser/notifications/scheduler/internal/distribution_policy.h"
 #include "chrome/browser/notifications/scheduler/internal/notification_entry.h"
 #include "chrome/browser/notifications/scheduler/internal/scheduler_config.h"
 #include "chrome/browser/notifications/scheduler/public/notification_scheduler_types.h"
