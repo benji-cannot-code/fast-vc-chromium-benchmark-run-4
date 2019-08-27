ddbc2a5f2c579b6fd55c7f3ce9b29d08a0151c80
Thomas Guilbert <tguilbert@chromium.org>
2019-08-27 19:56:31 +0000

    Add MediaMetricsProvider::AcquireLearningTaskController()
    This CL allows the Renderer process to request a LearningTaskController
    from the Browser process.
    
    The MediaMetricsProvider is used as a bridge between the Renderer and
    the Browser process. It binds the incoming mojo request to a
    MojoLearningTaskControllerService. The service in turn forwards calls
    to a LTC owned by the appropriate BrowserContext's LearningSession.
    
    Bug: 994831
    Change-Id: Ib2e3e35500da858688de0d49f3989968730b86ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764791
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690848}

--- media/mojo/services/media_metrics_provider.h
+++ media/mojo/services/media_metrics_provider.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/container_names.h"
 #include "media/base/pipeline_status.h"
 #include "media/base/timestamp_constants.h"
+#include "media/learning/common/learning_session.h"
 #include "media/learning/common/value.h"
 #include "media/mojo/mojom/media_metrics_provider.mojom.h"
 #include "media/mojo/services/media_mojo_export.h"
@@ -30,11 +31,15 @@ class MEDIA_MOJO_EXPORT MediaMetricsProvider
 
   enum class FrameStatus : bool { kTopFrame, kNotTopFrame };
 
+  using GetLearningSessionCallback =
+      base::RepeatingCallback<learning::LearningSession*()>;
+
   MediaMetricsProvider(BrowsingMode is_incognito,
                        FrameStatus is_top_frame,
                        ukm::SourceId source_id,
                        learning::FeatureValue origin,
-                       VideoDecodePerfHistory::SaveCallback save_cb);
+                       VideoDecodePerfHistory::SaveCallback save_cb,
+                       GetLearningSessionCallback learning_session_cb);
   ~MediaMetricsProvider() override;
 
   // Callback for retrieving a ukm::SourceId.
@@ -58,6 +63,7 @@ class MEDIA_MOJO_EXPORT MediaMetricsProvider
                      GetSourceIdCallback get_source_id_cb,
                      GetOriginCallback get_origin_cb,
                      VideoDecodePerfHistory::SaveCallback save_cb,
+                     GetLearningSessionCallback learning_session_cb,
                      mojom::MediaMetricsProviderRequest request);
 
  private:
@@ -100,6 +106,9 @@ class MEDIA_MOJO_EXPORT MediaMetricsProvider
       mojom::WatchTimeRecorderRequest request) override;
   void AcquireVideoDecodeStatsRecorder(
       mojom::VideoDecodeStatsRecorderRequest request) override;
+  void AcquireLearningTaskController(
+      const std::string& taskName,
+      media::learning::mojom::LearningTaskControllerRequest request) override;
   void AddBytesReceived(uint64_t bytes_received) override;
 
   void ReportPipelineUMA();
@@ -116,6 +125,7 @@ class MEDIA_MOJO_EXPORT MediaMetricsProvider
   const learning::FeatureValue origin_;
 
   const VideoDecodePerfHistory::SaveCallback save_cb_;
+  const GetLearningSessionCallback learning_session_cb_;
 
   // UMA pipeline packaged data
   PipelineInfo uma_info_;
