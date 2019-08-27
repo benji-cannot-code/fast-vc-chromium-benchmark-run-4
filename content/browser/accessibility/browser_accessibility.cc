36af5caa4bd54fb849a139f7a7ec95dc56100fe9
Daniel Clark <daniec@microsoft.com>
2019-08-27 16:15:04 +0000

    Re-land Fix for <article> ControlType and <figure> LocalizedControlType
    This is a re-land of
    https://chromium-review.googlesource.com/c/chromium/src/+/1757145,
    which was reverted due to Win7 failures of
    DumpAccessibilityTreeTest.AccessibilityArticle/uia.  The issue is that
    I had not updated BrowserAccessibility::GetLocalizedStringForRoleDescription()
    to return a result for "article", as the Win10 native UIA implementation
    was already coming up with the correct value for <article>'s
    LocalizedControlType based on its Aria Role.  However, appparently Win7's
    native UIA does not provide this value, so
    DumpAccessibilityTreeTest.AccessibilityArticle/uia -- which I had updated to
    dump the LocalizedControlType -- was failing on Win7.  In the re-land I've
    updated BrowserAccessibility::GetLocalizedStringForRoleDescription() to handle
    <article> so that we no longer depend on the platform for providing the
    correct LocalizedControlType.  Otherwise the change is the same as before.
    
    Original change description follows:
    
    Change ControlType of <article> from "Document" to "Group" per
    https://w3c.github.io/html-aam/#el-article
    
    Change LocalizedControlType of <figure> from "group" to "figure" per
    https://thepaciellogroup.github.io/AT-browser-tests/test-files/figure.html
    
    Bug: 994380
    Change-Id: I00fb18edda6c3bf86804bc26bf82201e65fab06c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769051
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#690752}

--- content/browser/accessibility/browser_accessibility.cc
+++ content/browser/accessibility/browser_accessibility.cc
@@ -1852,6 +1852,9 @@ base::string16 BrowserAccessibility::GetLocalizedStringForRoleDescription()
   const ui::AXNodeData& data = GetData();
 
   switch (data.role) {
+    case ax::mojom::Role::kArticle:
+      return content_client->GetLocalizedString(IDS_AX_ROLE_ARTICLE);
+
     case ax::mojom::Role::kAudio:
       return content_client->GetLocalizedString(IDS_AX_ROLE_AUDIO);
 
@@ -1877,6 +1880,9 @@ base::string16 BrowserAccessibility::GetLocalizedStringForRoleDescription()
 
     case ax::mojom::Role::kDetails:
       return content_client->GetLocalizedString(IDS_AX_ROLE_DETAILS);
+
+    case ax::mojom::Role::kFigure:
+      return content_client->GetLocalizedString(IDS_AX_ROLE_FIGURE);
 
     case ax::mojom::Role::kMeter:
       return content_client->GetLocalizedString(IDS_AX_ROLE_METER);
--- ui/accessibility/platform/test_ax_node_wrapper.cc
+++ ui/accessibility/platform/test_ax_node_wrapper.cc
@@ -579,6 +579,9 @@ base::string16 TestAXNodeWrapper::GetLocalizedStringForLandmarkType() const {
 base::string16 TestAXNodeWrapper::GetLocalizedStringForRoleDescription() const {
   const AXNodeData& data = GetData();
   switch (data.role) {
+    case ax::mojom::Role::kArticle:
+      return base::ASCIIToUTF16("article");
+
     case ax::mojom::Role::kAudio:
       return base::ASCIIToUTF16("audio");
 
@@ -603,6 +606,9 @@ base::string16 TestAXNodeWrapper::GetLocalizedStringForRoleDescription() const {
 
     case ax::mojom::Role::kDetails:
       return base::ASCIIToUTF16("details");
+
+    case ax::mojom::Role::kFigure:
+      return base::ASCIIToUTF16("figure");
 
     case ax::mojom::Role::kMeter:
       return base::ASCIIToUTF16("meter");
