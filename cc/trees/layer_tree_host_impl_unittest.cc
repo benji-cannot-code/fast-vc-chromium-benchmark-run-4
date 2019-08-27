a9cfc9bec95ba411b325bab412f579592f23c245
David Bokan <bokan@chromium.org>
2019-08-27 18:19:59 +0000

    Fix scroll chaining from pos:fixed with root scroller
    Normally, scrolls started from a position: fixed element will chain up
    to the "viewport", reglardless of DOM ancestry. This is so that
    scrolling over a banner/menu scrolls the page.
    
    The situation is a little different in the root scroller case. There,
    the "viewport" is composed of the visual viewport, and a non-default
    layout viewport. e.g.
    
    <body>
      <div id='myrootscroller'>...</div>
      <div id='fixed'></div>
    </body>
    
    In this case, "viewport" scrolling will scroll |myrootscroller|.
    
    In the above case, scrolling over |fixed| shouldn't cause scrolling in
    |myrootscroller| since that would change semantics on the page which
    isn't acceptable for an implicitly applied feature (e.g. this is an
    interop problem).
    
    However, in BGPT the scroll_tree_index on a fixed cc::Layer is made to
    point to the ScrollNode for the nearest ScrollTranslationTransformNode
    which will always be the visual viewport's, since the Fixed layer
    translates with the visual viewport.
    
    In the compositor, we unconditionally treat "inner/visual viewport"
    scrolls as scrolling using cc::Viewport which will have the behavior
    of scrolling both visual and layout viewports (it will scroll
    |myrootscroller| above).
    
    This CL makes two conceptual changes:
    
    -The compositor no longer unconditionally scrolls the inner ScrollNode
     using cc::Viewport. Whether or not the inner ScrollNode should scroll
     with the cc::Viewport now depends on a Blink-set bit
     |prevent_viewport_scrolling|. We scroll using cc::Viewport only when
     this bit is false. The change in LayerTreeHostImpl::TryScroll to scroll
     the outer viewport even if it has no extent is to compensate for the
     fact that we no longer unconditionally fallback to viewport scrolling.
    
    -Set this bit in Blink during pre-paint based on whether or not we
     have a non-default root scroller. That is, we only want to provide
     full viewport scrolling in this case if the root scroller hasn't been
     changed.
    
    This also required some small fixes to tests, in particular, some tests
    use an awkward layer tree where the outer viewport has no srolling and
    the visual viewport is scrollable. Inner/Outer scrolling is unusual in
    these cases because we typically want to scroll both at the same time
    using cc::Viewport. Changing to explicitly scroll inner only meant these
    tests break. I've changed them by adding a real scrolling layer as a
    child of the outer.
    
    For more details see:
    https://docs.google.com/document/d/1bp7nQq76I1HgtWW6fjfTFo__7Sy5XTUsigS-WHpC3B0/edit
    
    Note: This is still an issue in main thread scrolling (added tests fail
    there). I don't believe this is worth fixing there since it would
    require major changes to allow adding a non-DOM element to the scroll
    chain and the provided value is low (root scroller is currently only
    active on Android where main thread scrolling is rare). This should be
    fixed by scroll unification.
    
    Bug: 977954
    Change-Id: I644e0d618a21baced50b7b99d543bd517adbd91a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752866
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690805}

--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -723,7 +723,10 @@ class LayerTreeHostImplTest : public testing::Test,
   }
 
   LayerImpl* CreateLayerForSnapping() {
-    LayerImpl* scroll_layer = SetupScrollAndContentsLayers(gfx::Size(200, 200));
+    SetupScrollAndContentsLayers(gfx::Size(200, 200));
+    LayerImpl* scroll_layer =
+        host_impl_->active_tree()->OuterViewportScrollLayer();
+
     host_impl_->active_tree()->SetDeviceViewportSize(gfx::Size(100, 100));
 
     gfx::Size overflow_size(400, 400);
@@ -1755,8 +1758,13 @@ TEST_F(LayerTreeHostImplTest, GetSnapFlingInfoWhenZoomed) {
 }
 
 TEST_F(LayerTreeHostImplTest, OverscrollBehaviorPreventsPropagation) {
-  LayerImpl* scroll_layer = SetupScrollAndContentsLayers(gfx::Size(200, 200));
-  host_impl_->active_tree()->SetDeviceViewportSize(gfx::Size(100, 100));
+  const gfx::Size kViewportSize(100, 100);
+  const gfx::Size kContentSize(200, 200);
+  CreateBasicVirtualViewportLayers(kViewportSize, kContentSize);
+
+  LayerImpl* scroll_layer =
+      host_impl_->active_tree()->OuterViewportScrollLayer();
+  host_impl_->active_tree()->SetDeviceViewportSize(kViewportSize);
 
   gfx::Size overflow_size(400, 400);
   ASSERT_EQ(1u, scroll_layer->test_properties()->children.size());
@@ -1925,8 +1933,13 @@ TEST_F(LayerTreeHostImplTest, OverscrollBehaviorPreventsPropagation) {
 }
 
 TEST_F(LayerTreeHostImplTest, ScrollWithUserUnscrollableLayers) {
-  LayerImpl* scroll_layer = SetupScrollAndContentsLayers(gfx::Size(200, 200));
-  host_impl_->active_tree()->SetDeviceViewportSize(gfx::Size(100, 100));
+  const gfx::Size kViewportSize(100, 100);
+  const gfx::Size kContentSize(200, 200);
+  CreateBasicVirtualViewportLayers(kViewportSize, kContentSize);
+
+  LayerImpl* scroll_layer =
+      host_impl_->active_tree()->OuterViewportScrollLayer();
+  host_impl_->active_tree()->SetDeviceViewportSize(kViewportSize);
 
   gfx::Size overflow_size(400, 400);
   ASSERT_EQ(1u, scroll_layer->test_properties()->children.size());
--- third_party/blink/renderer/core/page/scrolling/root_scroller_controller.cc
+++ third_party/blink/renderer/core/page/scrolling/root_scroller_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/frame/browser_controls.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/core/frame/visual_viewport.h"
 #include "third_party/blink/renderer/core/fullscreen/document_fullscreen.h"
 #include "third_party/blink/renderer/core/html/html_frame_owner_element.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
@@ -211,8 +212,13 @@ void RootScrollerController::RecomputeEffectiveRootScroller() {
   ApplyRootScrollerProperties(*old_effective_root_scroller);
   ApplyRootScrollerProperties(*effective_root_scroller_);
 
-  if (Page* page = document_->GetPage())
+  if (Page* page = document_->GetPage()) {
     page->GlobalRootScrollerController().DidChangeRootScroller();
+
+    // Needed to set the |prevent_viewport_scrolling_from_inner| bit on the
+    // VisualViewportScrollNode.
+    page->GetVisualViewport().SetNeedsPaintPropertyUpdate();
+  }
 }
 
 bool RootScrollerController::IsValidRootScroller(const Element& element) const {
