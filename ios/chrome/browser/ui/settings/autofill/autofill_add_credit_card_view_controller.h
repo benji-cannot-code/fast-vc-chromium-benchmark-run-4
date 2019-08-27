6f876164de8d75984e9570d6a449d0745ba208dc
redatawfik <redatawfik@google.com>
2019-08-27 12:00:13 +0000

    Modify "AutofillAddCreditCardViewController" swipe down callback
    1- Make "AutofillAddCreditCardCoordinator" conforms to protocol
       "UIAdaptivePresentationControllerDelegate" and implement
       "presentationControllerShouldDismiss" such that it return "YES" to
       allow dismiss if Credit Card text fields are empty.
    2- set "AutofillAddCreditCardCoordinator" as a delegate for add credit card navigation controller.
    3- Create method "textFieldsEmpty" which returns "YES" if Credit Card text fields are empty.
    
    Bug: 984545
    Change-Id: I89bf9f8f5e92867e15ffb1dbaacf2c06e85b9a0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763694
    Commit-Queue: Reda Tawfik <redatawfik@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690691}

--- ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_view_controller.h
+++ ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_view_controller.h
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                (ChromeTableViewControllerStyle)appBarStyle
     NS_UNAVAILABLE;
 
+// Returns "YES" if any of tableview cells has user input.
+@property(nonatomic, getter=tableViewHasUserInput, readonly)
+    BOOL tableViewHasUserInput;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_SETTINGS_AUTOFILL_AUTOFILL_ADD_CREDIT_CARD_VIEW_CONTROLLER_H_
