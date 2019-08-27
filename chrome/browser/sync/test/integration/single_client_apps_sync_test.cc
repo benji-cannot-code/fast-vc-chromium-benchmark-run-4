4df63b3ccbea8ec22276192cb53c70edc0013bf1
Kentaro Hara <haraken@chromium.org>
2019-08-27 04:34:58 +0000

    Sheriff: Disable SingleClientAppsTest with MSan
    TBR=haraken@chromium.org
    
    Bug: 997984
    Change-Id: Icee0159dede515d7c113e1b02434044baa55fb25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768512
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690628}

--- chrome/browser/sync/test/integration/single_client_apps_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_apps_sync_test.cc
@@ -25,12 +25,29 @@ class SingleClientAppsSyncTest : public SyncTest {
   DISALLOW_COPY_AND_ASSIGN(SingleClientAppsSyncTest);
 };
 
-IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, StartWithNoApps) {
+// crbug.com/997984
+#if defined(MEMORY_SANITIZER)
+#define MAYBE_StartWithNoApps DISABLED_StartWithNoApps
+#define MAYBE_StartWithSomeLegacyApps DISABLED_StartWithSomeLegacyApps
+#define MAYBE_StartWithSomePlatformApps DISABLED_StartWithSomePlatformApps
+#define MAYBE_InstallSomeLegacyApps DISABLED_InstallSomeLegacyApps
+#define MAYBE_InstallSomePlatformApps DISABLED_InstallSomePlatformApps
+#define MAYBE_InstallSomeApps DISABLED_InstallSomeApps
+#else
+#define MAYBE_StartWithNoApps StartWithNoApps
+#define MAYBE_StartWithSomeLegacyApps StartWithSomeLegacyApps
+#define MAYBE_StartWithSomePlatformApps StartWithSomePlatformApps
+#define MAYBE_InstallSomeLegacyApps InstallSomeLegacyApps
+#define MAYBE_InstallSomePlatformApps InstallSomePlatformApps
+#define MAYBE_InstallSomeApps InstallSomeApps
+#endif
+
+IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, MAYBE_StartWithNoApps) {
   ASSERT_TRUE(SetupSync());
   ASSERT_TRUE(AllProfilesHaveSameApps());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, StartWithSomeLegacyApps) {
+IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, MAYBE_StartWithSomeLegacyApps) {
   ASSERT_TRUE(SetupClients());
 
   const int kNumApps = 5;
@@ -43,7 +60,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, StartWithSomeLegacyApps) {
   ASSERT_TRUE(AllProfilesHaveSameApps());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, StartWithSomePlatformApps) {
+IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, MAYBE_StartWithSomePlatformApps) {
   ASSERT_TRUE(SetupClients());
 
   const int kNumApps = 5;
@@ -56,7 +73,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, StartWithSomePlatformApps) {
   ASSERT_TRUE(AllProfilesHaveSameApps());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, InstallSomeLegacyApps) {
+IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, MAYBE_InstallSomeLegacyApps) {
   ASSERT_TRUE(SetupSync());
 
   const int kNumApps = 5;
@@ -69,7 +86,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, InstallSomeLegacyApps) {
   ASSERT_TRUE(AllProfilesHaveSameApps());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, InstallSomePlatformApps) {
+IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, MAYBE_InstallSomePlatformApps) {
   ASSERT_TRUE(SetupSync());
 
   const int kNumApps = 5;
@@ -82,7 +99,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, InstallSomePlatformApps) {
   ASSERT_TRUE(AllProfilesHaveSameApps());
 }
 
-IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, InstallSomeApps) {
+IN_PROC_BROWSER_TEST_F(SingleClientAppsSyncTest, MAYBE_InstallSomeApps) {
   ASSERT_TRUE(SetupSync());
 
   int i = 0;
