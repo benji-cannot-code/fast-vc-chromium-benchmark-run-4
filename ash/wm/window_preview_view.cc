b14b64c462f323a6a66ee8ded8c6170e86835da9
Eliot Courtney <edcourtney@google.com>
2019-08-27 03:05:50 +0000

    Expand PIP if we receive a window state change for it.
    This is necessary to support alt-tab triggering PIP expand. Upon
    selecting the PIP window in alt-tab, Chrome needs to be able to expand
    it via wayland protocol.
    
    Bug: b/129242864
    Test: Alt-tab correctly expands PIP
    Change-Id: If4352cbd683eaa98ac5f6fd34200f6a69fd553ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730555
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690609}

--- ash/wm/window_preview_view.cc
+++ ash/wm/window_preview_view.cc
@@ -5,7 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/window_preview_view.h"
 
+#include "ash/wm/window_mirror_view_pip.h"
+#include "ash/wm/window_state.h"
 #include "ash/wm/window_transient_descendant_iterator.h"
+#include "ash/wm/window_util.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/client/transient_window_client.h"
 #include "ui/aura/window.h"
@@ -112,8 +115,11 @@ void WindowPreviewView::AddWindow(aura::Window* window) {
   DCHECK(!mirror_views_.contains(window));
 
   window->AddObserver(this);
+
   auto* mirror_view =
-      new WindowMirrorView(window, trilinear_filtering_on_init_);
+      window_util::IsArcPipWindow(window)
+          ? new WindowMirrorViewPip(window, trilinear_filtering_on_init_)
+          : new WindowMirrorView(window, trilinear_filtering_on_init_);
   mirror_views_[window] = mirror_view;
   AddChildView(mirror_view);
 }
--- ash/wm/window_util.cc
+++ ash/wm/window_util.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_positioning_utils.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/wm_event.h"
+#include "ui/aura/client/aura_constants.h"
 #include "ui/aura/client/capture_client.h"
 #include "ui/aura/client/focus_client.h"
 #include "ui/aura/window.h"
@@ -298,6 +299,15 @@ aura::Window* GetRootWindowMatching(const gfx::Rect& rect_in_screen) {
       Shell::GetRootWindowControllerWithDisplayId(display.id());
   return root_window_controller ? root_window_controller->GetRootWindow()
                                 : nullptr;
+}
+
+bool IsArcWindow(const aura::Window* window) {
+  return window->GetProperty(aura::client::kAppType) ==
+         static_cast<int>(ash::AppType::ARC_APP);
+}
+
+bool IsArcPipWindow(const aura::Window* window) {
+  return WindowState::Get(window)->IsPip() && window_util::IsArcWindow(window);
 }
 
 }  // namespace window_util
