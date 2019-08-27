06d1e7f91c0e17de8a78d6f1fe62dbc489ead954
Renjie Tang <renjietang@chromium.org>
2019-08-27 18:56:23 +0000

    Roll src/net/third_party/quiche/src/ 3de7170b3..6c73c33b7 (7 commits)
    https://quiche.googlesource.com/quiche.git/+log/3de7170b31c5..6c73c33b7d8c
    
    $ git log 3de7170b3..6c73c33b7 --date=short --no-merges --format='%ad %ae %s'
    2019-08-26 renjietang No longer send SETTINGS in gQUIC.
    2019-08-26 fayang gfe-relnote: In TLS client handshaker, call OnHandshakeComplete when handshake is finished. Protected by existing FLAGS_quic_supports_tls_handshake.
    2019-08-26 bnc Remove VersionUsesQpack() TODO.
    2019-08-26 zhongyi Always create and destroy a single client in the creation of the QuicClientTest to ensure persistent sockets for certificate verifiers are opened before testing.
    2019-08-26 bnc gfe-relnote: Deprecate gfe2_reloadable_flag_quic_avoid_empty_frame_after_empty_headers.
    2019-08-26 fkastenholz Save all data from received IETF Connection Close frames
    2019-08-24 ianswett gfe-relnote: Do not enable version 99 unless gfe2_reloadable_flag_quic_use_parse_public_header is also enabled.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I71b64773eb6640f78dba6b43cf864070127998f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772294
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690826}

--- net/quic/quic_http_stream_test.cc
+++ net/quic/quic_http_stream_test.cc
@@ -1131,6 +1131,7 @@ TEST_P(QuicHttpStreamTest, LogGranularQuicConnectionError) {
 
   quic::QuicConnectionCloseFrame frame;
   frame.quic_error_code = quic::QUIC_PEER_GOING_AWAY;
+  frame.extracted_error_code = quic::QUIC_PEER_GOING_AWAY;
   session_->connection()->OnConnectionCloseFrame(frame);
 
   NetErrorDetails details;
@@ -1177,6 +1178,7 @@ TEST_P(QuicHttpStreamTest, LogGranularQuicErrorIfHandshakeNotConfirmed) {
 
   quic::QuicConnectionCloseFrame frame;
   frame.quic_error_code = quic::QUIC_PEER_GOING_AWAY;
+  frame.extracted_error_code = quic::QUIC_PEER_GOING_AWAY;
   session_->connection()->OnConnectionCloseFrame(frame);
 
   NetErrorDetails details;
