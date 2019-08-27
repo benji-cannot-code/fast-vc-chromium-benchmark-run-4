83ba9d67fb2519974728e923d2c7ca96f2d1d62c
Xida Chen <xidachen@chromium.org>
2019-08-27 18:19:37 +0000

    [Code health] Make FrameSequenceTrackerCollection own all trackers
    Right now all the FrameSequenceTrackers are owned by LayerTreeHostImpl,
    moreover, LayerTreeHostImpl also owns a FrameSequenceTrackerCollection.
    
    This CL makes FrameSequenceTrackerCollection owns all the trackers,
    any operation on the trackers can be done throughput
    FrameSequenceTrackerCollection.
    
    This CL should not cause any behavior change.
    
    Bug: None
    Change-Id: If8b15990ed6c1664af8eaa9c81c1a2888cf40ee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769025
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690804}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -467,15 +467,11 @@ void LayerTreeHostImpl::CommitComplete() {
   UpdateSyncTreeAfterCommitOrImplSideInvalidation();
   micro_benchmark_controller_.DidCompleteCommit();
 
-  if (mutator_host_->CurrentFrameHadRAF() &&
-      !request_animation_frame_tracker_) {
-    request_animation_frame_tracker_ =
-        frame_trackers_.CreateTracker(FrameSequenceTrackerType::kRAF);
-  }
+  if (mutator_host_->CurrentFrameHadRAF())
+    frame_trackers_.StartSequence(FrameSequenceTrackerType::kRAF);
 
-  if (mutator_host_->MainThreadAnimationsCount() > 0 &&
-      !main_thread_animation_frame_tracker_) {
-    main_thread_animation_frame_tracker_ = frame_trackers_.CreateTracker(
+  if (mutator_host_->MainThreadAnimationsCount() > 0) {
+    frame_trackers_.StartSequence(
         FrameSequenceTrackerType::kMainThreadAnimation);
   }
 }
@@ -2240,16 +2236,12 @@ bool LayerTreeHostImpl::DrawLayers(FrameData* frame) {
   frame_trackers_.NotifySubmitFrame(compositor_frame.metadata.frame_token,
                                     frame->begin_frame_ack,
                                     frame->origin_begin_main_frame_args);
-  if (request_animation_frame_tracker_ &&
-      !mutator_host_->NextFrameHasPendingRAF()) {
-    frame_trackers_.ScheduleRemoval(
-        std::move(request_animation_frame_tracker_));
-  }
+  if (!mutator_host_->NextFrameHasPendingRAF())
+    frame_trackers_.StopSequence(FrameSequenceTrackerType::kRAF);
 
-  if (main_thread_animation_frame_tracker_ &&
-      mutator_host_->MainThreadAnimationsCount() == 0) {
-    frame_trackers_.ScheduleRemoval(
-        std::move(main_thread_animation_frame_tracker_));
+  if (mutator_host_->MainThreadAnimationsCount() == 0) {
+    frame_trackers_.StopSequence(
+        FrameSequenceTrackerType::kMainThreadAnimation);
   }
 
   // Clears the list of swap promises after calling DidSwap on each of them to
@@ -3408,11 +3400,6 @@ void LayerTreeHostImpl::ReleaseLayerTreeFrameSink() {
   bool all_resources_are_lost = layer_tree_frame_sink_->context_provider();
 
   // Destroy the submit-frame trackers before destroying the frame sink.
-  pinch_frame_tracker_ = nullptr;
-  scroll_frame_tracker_ = nullptr;
-  compositor_animation_frame_tracker_ = nullptr;
-  request_animation_frame_tracker_ = nullptr;
-  main_thread_animation_frame_tracker_ = nullptr;
   frame_trackers_.ClearAll();
 
   // Detach from the old LayerTreeFrameSink and reset |layer_tree_frame_sink_|
@@ -3814,9 +3801,9 @@ InputHandler::ScrollStatus LayerTreeHostImpl::ScrollBeginImpl(
     scroll_status.bubble = true;
   }
 
-  scroll_frame_tracker_ = frame_trackers_.CreateTracker(
-      wheel_scrolling_ ? FrameSequenceTrackerType::kWheelScroll
-                       : FrameSequenceTrackerType::kTouchScroll);
+  frame_trackers_.StartSequence(wheel_scrolling_
+                                    ? FrameSequenceTrackerType::kWheelScroll
+                                    : FrameSequenceTrackerType::kTouchScroll);
   client_->RenewTreePriority();
   RecordCompositorSlowScrollMetric(type, CC_THREAD);
 
@@ -4910,7 +4897,9 @@ void LayerTreeHostImpl::ScrollEndImpl(ScrollState* scroll_state) {
   DistributeScrollDelta(scroll_state);
   browser_controls_offset_manager_->ScrollEnd();
   ClearCurrentlyScrollingNode();
-  frame_trackers_.ScheduleRemoval(std::move(scroll_frame_tracker_));
+  frame_trackers_.StopSequence(wheel_scrolling_
+                                   ? FrameSequenceTrackerType::kWheelScroll
+                                   : FrameSequenceTrackerType::kTouchScroll);
 }
 
 void LayerTreeHostImpl::ScrollEnd(ScrollState* scroll_state, bool should_snap) {
@@ -5044,8 +5033,7 @@ void LayerTreeHostImpl::PinchGestureBegin() {
                        OuterViewportScrollNode() ? false : true);
   active_tree_->SetCurrentlyScrollingNode(OuterViewportScrollNode());
   browser_controls_offset_manager_->PinchBegin();
-  pinch_frame_tracker_ =
-      frame_trackers_.CreateTracker(FrameSequenceTrackerType::kPinchZoom);
+  frame_trackers_.StartSequence(FrameSequenceTrackerType::kPinchZoom);
 }
 
 void LayerTreeHostImpl::PinchGestureUpdate(float magnify_delta,
@@ -5078,7 +5066,7 @@ void LayerTreeHostImpl::PinchGestureEnd(const gfx::Point& anchor,
   // scales that we want when we're not inside a pinch.
   active_tree_->set_needs_update_draw_properties();
   SetNeedsRedraw();
-  frame_trackers_.ScheduleRemoval(std::move(pinch_frame_tracker_));
+  frame_trackers_.StopSequence(FrameSequenceTrackerType::kPinchZoom);
 }
 
 void LayerTreeHostImpl::CollectScrollDeltas(
@@ -5259,13 +5247,11 @@ bool LayerTreeHostImpl::AnimateLayers(base::TimeTicks monotonic_time,
   // still request an extra SetNeedsAnimate here.
   if (animated) {
     SetNeedsOneBeginImplFrame();
-    if (!compositor_animation_frame_tracker_) {
-      compositor_animation_frame_tracker_ = frame_trackers_.CreateTracker(
-          FrameSequenceTrackerType::kCompositorAnimation);
-    }
+    frame_trackers_.StartSequence(
+        FrameSequenceTrackerType::kCompositorAnimation);
   } else {
-    frame_trackers_.ScheduleRemoval(
-        std::move(compositor_animation_frame_tracker_));
+    frame_trackers_.StopSequence(
+        FrameSequenceTrackerType::kCompositorAnimation);
   }
 
   // TODO(crbug.com/551138): We could return true only if the animaitons are on
