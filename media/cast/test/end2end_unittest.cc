ad82f3c09932cfd0c196a47db9a748bb47a692ad
Vasilii Sukhanov <vasilii@chromium.org>
2019-08-27 13:22:05 +0000

    Disable End2EndTest.ShoveHighFrameRateDownYerThroat
    TBR=miu@chromium.org
    
    Bug: 801794
    Change-Id: I41d1d58756721960f346ca81e04c547118b0b607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772881
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690699}

--- media/cast/test/end2end_unittest.cc
+++ media/cast/test/end2end_unittest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/simple_test_tick_clock.h"
 #include "base/test/task_environment.h"
 #include "base/time/tick_clock.h"
+#include "build/build_config.h"
 #include "media/base/audio_bus.h"
 #include "media/base/fake_single_thread_task_runner.h"
 #include "media/base/video_frame.h"
@@ -1219,7 +1220,9 @@ TEST_F(End2EndTest, MAYBE_EvilNetwork) {
 // Tests that a system configured for 30 FPS drops frames when input is provided
 // at a much higher frame rate.
 // Fails consistently on official builds: crbug.com/612496
-#ifdef OFFICIAL_BUILD
+// crbug.com/997944. Flaky on multiple platforms.
+#if defined(OFFICIAL_BUILD) || defined(OS_LINUX) || defined(OS_MACOSX) || \
+    defined(OS_WIN)
 #define MAYBE_ShoveHighFrameRateDownYerThroat \
   DISABLED_ShoveHighFrameRateDownYerThroat
 #else
