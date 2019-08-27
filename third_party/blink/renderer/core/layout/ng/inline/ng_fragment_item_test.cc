6b1bdcaf31f47032d540333961cff87e624b6ee2
Koji Ishii <kojii@chromium.org>
2019-08-27 03:35:12 +0000

    [NGFragmentItem] Fix block offset
    This patch fixes the block offset of |NGFragmentItem|.
    
    Because it was determined by |NGBlockLayoutAlgorithm|, this
    patch changes the adding process to two-pass, so that
    |NGInlineLayoutAlgorithm| creates children while allowing its
    parent algorithm to position the line box.
    
    Bug: 982194
    Change-Id: I3859f6b19ba0ec1ea83d68ab8759cd5af4e1d007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768508
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690618}

--- third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item_test.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item_test.cc
@@ -7,13 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items_builder.h"
-#include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_line_box_fragment.h"
-#include "third_party/blink/renderer/core/layout/ng/inline/ng_physical_text_fragment.h"
-#include "third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_test.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
-#include "third_party/blink/renderer/core/paint/ng/ng_paint_fragment.h"
+
+using testing::ElementsAre;
 
 namespace blink {
 
@@ -21,21 +19,59 @@ class NGFragmentItemTest : public NGLayoutTest,
                            ScopedLayoutNGFragmentItemForTest {
  public:
   NGFragmentItemTest() : ScopedLayoutNGFragmentItemForTest(true) {}
+
+  Vector<const NGFragmentItem*> ItemsForAsVector(
+      const LayoutObject& layout_object) {
+    const auto items = NGFragmentItem::ItemsFor(layout_object);
+    Vector<const NGFragmentItem*> list;
+    for (const NGFragmentItem& item : items) {
+      EXPECT_EQ(item.GetLayoutObject(), &layout_object);
+      list.push_back(&item);
+    }
+    return list;
+  }
 };
 
-TEST_F(NGFragmentItemTest, Simple) {
+TEST_F(NGFragmentItemTest, BasicText) {
+  LoadAhem();
   SetBodyInnerHTML(R"HTML(
+    <style>
+    html, body {
+      margin: 0;
+      font-family: Ahem;
+      font-size: 10px;
+      line-height: 1;
+    }
+    div {
+      width: 10ch;
+    }
+    </style>
     <div id="container">
-      text
+      1234567 98765
     </div>
   )HTML");
 
   LayoutBlockFlow* container =
       To<LayoutBlockFlow>(GetLayoutObjectByElementId("container"));
+  LayoutText* layout_text = ToLayoutText(container->FirstChild());
   const NGPhysicalBoxFragment* box = container->CurrentFragment();
   EXPECT_NE(box, nullptr);
   const NGFragmentItems* items = box->Items();
   EXPECT_NE(items, nullptr);
+  EXPECT_EQ(items->Items().size(), 4u);
+
+  const NGFragmentItem& text1 = *items->Items()[1];
+  EXPECT_EQ(text1.Type(), NGFragmentItem::kText);
+  EXPECT_EQ(text1.GetLayoutObject(), layout_text);
+  EXPECT_EQ(text1.Offset(), PhysicalOffset());
+
+  const NGFragmentItem& text2 = *items->Items()[3];
+  EXPECT_EQ(text2.Type(), NGFragmentItem::kText);
+  EXPECT_EQ(text2.GetLayoutObject(), layout_text);
+  EXPECT_EQ(text2.Offset(), PhysicalOffset(0, 10));
+
+  Vector<const NGFragmentItem*> items_for_text = ItemsForAsVector(*layout_text);
+  EXPECT_THAT(items_for_text, ElementsAre(&text1, &text2));
 }
 
 TEST_F(NGFragmentItemTest, ForLayoutObject) {
@@ -58,14 +94,8 @@ TEST_F(NGFragmentItemTest, ForLayoutObject) {
 
   const LayoutObject* span = GetLayoutObjectByElementId("span");
   ASSERT_NE(span, nullptr);
-  const auto items = NGFragmentItem::ItemsFor(*span);
-  EXPECT_FALSE(items.IsEmpty());
-  unsigned count = 0;
-  for (const NGFragmentItem& item : items) {
-    EXPECT_EQ(item.GetLayoutObject(), span);
-    ++count;
-  }
-  EXPECT_EQ(count, 2u);
+  Vector<const NGFragmentItem*> items_for_span = ItemsForAsVector(*span);
+  EXPECT_EQ(items_for_span.size(), 2u);
 }
 
 }  // namespace blink
