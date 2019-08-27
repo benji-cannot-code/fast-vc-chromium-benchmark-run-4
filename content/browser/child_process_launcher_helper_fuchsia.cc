c9400791e74d8b4aaa08a51ee196d60156dc6d59
Sergey Ulanov <sergeyu@chromium.org>
2019-08-27 17:53:18 +0000

    [Fuchsia] Set name suffix for web_engine processes.
    Previously all web_engine processes had the same name, which made it
    hard to distinguish between processes. Now each process started by
    web_engine will have the name suffixed with process type, e.g.
    web_engine_exe:renderer.
    
    Change-Id: Ic1fa0f4f7bc87f4789d0d6d85d71d836d89df99f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769996
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690787}

--- content/browser/child_process_launcher_helper_fuchsia.cc
+++ content/browser/child_process_launcher_helper_fuchsia.cc
@@ -7,13 +7,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/process/launch.h"
+#include "base/strings/stringprintf.h"
 #include "content/browser/child_process_launcher.h"
 #include "content/public/browser/child_process_launcher_utils.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
 #include "services/service_manager/embedder/result_codes.h"
+#include "services/service_manager/embedder/switches.h"
 
 namespace content {
 namespace internal {
+
+namespace {
+
+const char* ProcessNameFromSandboxType(
+    service_manager::SandboxType sandbox_type) {
+  switch (sandbox_type) {
+    case service_manager::SANDBOX_TYPE_NO_SANDBOX:
+      return nullptr;
+    case service_manager::SANDBOX_TYPE_WEB_CONTEXT:
+      return "context";
+    case service_manager::SANDBOX_TYPE_RENDERER:
+      return "renderer";
+    case service_manager::SANDBOX_TYPE_UTILITY:
+      return "utility";
+    case service_manager::SANDBOX_TYPE_GPU:
+      return "gpu";
+    case service_manager::SANDBOX_TYPE_NETWORK:
+      return "network";
+    default:
+      NOTREACHED() << sandbox_type;
+      return nullptr;
+  }
+}
+
+}  // namespace
 
 void ChildProcessLauncherHelper::SetProcessPriorityOnLauncherThread(
     base::Process process,
@@ -68,6 +95,12 @@ bool ChildProcessLauncherHelper::BeforeLaunchOnLauncherThread(
   mojo_channel_->PrepareToPassRemoteEndpoint(&options->handles_to_transfer,
                                              command_line());
   sandbox_policy_.UpdateLaunchOptionsForSandbox(options);
+
+  // Set process name suffix to make it easier to identify the process.
+  const char* process_type =
+      ProcessNameFromSandboxType(delegate_->GetSandboxType());
+  if (process_type)
+    options->process_name_suffix = base::StringPrintf(":%s", process_type);
 
   return true;
 }
