1340b2fd76593055326990163ff7db41c11f4718
Andrew Xu <andrewxu@chromium.org>
2019-08-27 23:19:08 +0000

    Refactor on ScrollArrowView
    In this CL, ScrollArrowView inherits the ShelfButton. It improves the
    code readability and also makes Shelf code coherent: in current code
    base all of buttons in Shelf are ShelfButtons.
    
    Bug: 973481
    Change-Id: I73aab558948b4ef32f79fa6e4ce243e9eba269a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772293
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690918}

--- ash/shelf/scrollable_shelf_view.h
+++ ash/shelf/scrollable_shelf_view.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_model.h"
 #include "ash/shelf/scroll_arrow_view.h"
 #include "ash/shelf/shelf.h"
+#include "ash/shelf/shelf_button_delegate.h"
 #include "ash/shelf/shelf_container_view.h"
 #include "ash/shelf/shelf_view.h"
 #include "ui/views/animation/ink_drop_host_view.h"
@@ -19,7 +20,7 @@ namespace ash {
 
 class ASH_EXPORT ScrollableShelfView : public views::View,
                                        public ShellObserver,
-                                       public views::ButtonListener {
+                                       public ShelfButtonDelegate {
  public:
   enum LayoutStrategy {
     // The arrow buttons are not shown. It means that there is enough space to
@@ -98,8 +99,12 @@ class ASH_EXPORT ScrollableShelfView : public views::View,
   void OnMouseEvent(ui::MouseEvent* event) override;
   void OnGestureEvent(ui::GestureEvent* event) override;
 
-  // views::ButtonListener:
-  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
+  // ShelfButtonDelegate:
+  void OnShelfButtonAboutToRequestFocusFromTabTraversal(ShelfButton* button,
+                                                        bool reverse) override;
+  void ButtonPressed(views::Button* sender,
+                     const ui::Event& event,
+                     views::InkDrop* ink_drop) override;
 
   // Overridden from ShellObserver:
   void OnShelfAlignmentChanged(aura::Window* root_window) override;
