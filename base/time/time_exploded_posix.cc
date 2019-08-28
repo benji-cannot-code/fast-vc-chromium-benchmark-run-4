3b979e9cc050d31f0f397f55c8f5995e1874a8d1
David Jean <djean@chromium.org>
2019-08-28 10:14:54 +0000

    [ios] Use posix TimeExploded
    With 32 bits build gone, iOS can use the posix implementation of
    TimeExploded, which is faster.
    
    Bug: 985061
    Change-Id: Ic1d42ebddfa2d0bb0d37bb7b2086683914aa87a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763679
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691094}

--- base/time/time_exploded_posix.cc
+++ base/time/time_exploded_posix.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/clamped_math.h"
 #endif
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) || defined(OS_IOS)
 static_assert(sizeof(time_t) >= 8, "Y2038 problem!");
 #endif
 
