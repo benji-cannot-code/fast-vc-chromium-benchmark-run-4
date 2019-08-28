ae0cc9b2da4f63c3ee8075c2e62a8d55fcb00bfc
Lan Wei <lanwei@chromium.org>
2019-08-28 15:33:55 +0000

    Add tick duration argument to testdriver Action
    We want to allow the users to define the duration for every tick, so we
    add an argument to Action class which will be a duration for all
    ticks. If there is a tick which need a different duration, we can call
    addTick or pause to pass a different value.
    
    Bug: 606367
    Change-Id: I7e5d88557f5f4a4c63dbf85985c30881aee9f739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1762836
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691186}

--- third_party/blink/web_tests/external/wpt/resources/testdriver-actions.js
+++ third_party/blink/web_tests/external/wpt/resources/testdriver-actions.js
@@ -4,8 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   /**
    * Builder for creating a sequence of actions
+   * The default tick duration is set to 32ms, which is 2 frame time based on
+   * 60Hz display.
    */
-  function Actions() {
+  function Actions(defaultTickDuration=32) {
     this.sourceTypes = new Map([["key", KeySource],
                                 ["pointer", PointerSource],
                                 ["none", GeneralSource]]);
@@ -20,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
     this.createSource("none");
     this.tickIdx = 0;
+    this.defaultTickDuration = defaultTickDuration;
   }
 
   Actions.prototype = {
@@ -41,7 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       let actions = [];
       for (let [sourceType, sourceName] of this.sourceOrder) {
         let source = this.sources.get(sourceType).get(sourceName);
-        let serialized = source.serialize(this.tickIdx + 1);
+        let serialized = source.serialize(this.tickIdx + 1, this.defaultTickDuration);
         if (serialized) {
           serialized.id = sourceName;
           actions.push(serialized);
@@ -279,17 +282,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   GeneralSource.prototype = {
-    serialize: function(tickCount) {
-      if (!this.actions.size) {
-        return undefined;
-      }
+    serialize: function(tickCount, defaultTickDuration) {
       let actions = [];
       let data = {"type": "none", "actions": actions};
       for (let i=0; i<tickCount; i++) {
         if (this.actions.has(i)) {
           actions.push(this.actions.get(i));
         } else {
-          actions.push({"type": "pause"});
+          actions.push({"type": "pause", duration: defaultTickDuration});
         }
       }
       return data;
