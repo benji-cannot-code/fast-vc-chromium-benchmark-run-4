461f98e5e1708036553ad939416c43fadd881d8d
Edvard Thörnros <edvardt@opera.com>
2019-08-28 14:03:31 +0000

    Reland "Started implementing the STAPIT algorithm"
    Changed SMILTimeContainer to step through all times where
    an animation stops or ends when just playing it normally. Currently
    it will only add slowdowns but also makes the animations more "correct".
    
    The "repeatn-remove-add-animation.html" test has been changed to work more
    reliably with the new stepping.
    
    This commit also fixes issue 688742
    
    Bug: 688742, 986219, 997279
    Change-Id: I7f1a68c42994eb5b080a87bd2df53cb80316ecff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768765
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Edvard Thörnros <edvardt@opera.com>
    Cr-Commit-Position: refs/heads/master@{#691141}

--- third_party/blink/web_tests/external/wpt/resources/SVGAnimationTestCase-testharness.js
+++ third_party/blink/web_tests/external/wpt/resources/SVGAnimationTestCase-testharness.js
@@ -8,13 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Helper functions
 const xlinkNS = "http://www.w3.org/1999/xlink"
 
-function expectFillColor(element, red, green, blue) {
+function expectFillColor(element, red, green, blue, message) {
     let color = window.getComputedStyle(element, null).fill;
     var re = new RegExp("rgba?\\(([^, ]*), ([^, ]*), ([^, ]*)(?:, )?([^, ]*)\\)");
     rgb = re.exec(color);
-    assert_approx_equals(Number(rgb[1]), red, 2.0);
-    assert_approx_equals(Number(rgb[2]), green, 2.0);
-    assert_approx_equals(Number(rgb[3]), blue, 2.0);
+
+    assert_approx_equals(Number(rgb[1]), red, 2.0, message);
+    assert_approx_equals(Number(rgb[2]), green, 2.0, message);
+    assert_approx_equals(Number(rgb[3]), blue, 2.0, message);
 }
 
 function expectColor(element, red, green, blue, property) {
