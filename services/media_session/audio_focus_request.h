5888036446e4ac6d4c28779d22ac5d2ee83277bc
Becca Hughes <beccahughes@chromium.org>
2019-08-28 21:28:56 +0000

    [Media Session] Remove audio focus histograms
    These histograms are expiring and can be removed.
    
    BUG=995521
    
    Change-Id: Ie96e8aa65619742d05ffcdc0ba29d9f4fb14159b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771827
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Auto-Submit: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691343}

--- services/media_session/audio_focus_request.h
+++ services/media_session/audio_focus_request.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "mojo/public/cpp/bindings/receiver.h"
-#include "services/media_session/audio_focus_manager_metrics_helper.h"
 #include "services/media_session/public/mojom/audio_focus.mojom.h"
 #include "services/media_session/public/mojom/media_controller.mojom.h"
 
@@ -91,7 +90,6 @@ class AudioFocusRequest : public mojom::AudioFocusRequestClient {
   void OnImageDownloaded(GetMediaImageBitmapCallback callback,
                          const SkBitmap& bitmap);
 
-  AudioFocusManagerMetricsHelper metrics_helper_;
   bool encountered_error_ = false;
   bool was_suspended_ = false;
 
