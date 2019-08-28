6f1d720f03dd139ca74d7c7e582c090f25ab34f0
Anand K. Mistry <amistry@chromium.org>
2019-08-28 23:35:46 +0000

    Deflake ConditionVariableImplTest.ThreadCannotWaitIfStillOwnsLock
    The test uses EXPECT_DEATH in a multi-threaded environment, which is
    known to be unsafe by default. Changing the death test style to
    "threadsafe" makes it safer.
    
    BUG=None
    
    Change-Id: I0cbd046f294a6c4237da102739ee231f28e3eebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772955
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691374}

--- chromeos/components/nearby/condition_variable_impl_unittest.cc
+++ chromeos/components/nearby/condition_variable_impl_unittest.cc
@@ -187,6 +187,7 @@ TEST_F(ConditionVariableImplTest,
 }
 
 TEST_F(ConditionVariableImplTest, ThreadCannotWaitIfStillOwnsLock) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   fake_lock()->set_is_held_by_current_thread(true);
   WaitOnConditionVariableFromParallelSequence(false /* should_succeed */);
 }
