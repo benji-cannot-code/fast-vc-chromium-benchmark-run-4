c8314d75157f9cd999a80be5b9cc972f4cd28ac8
Abhishek Bhardwaj <abhishekbh@chromium.org>
2019-08-28 00:45:22 +0000

    DeviceScheduledUpdateChecker: Move files to own folder
    This changed moves device scheduled update checker file to a sub folder
    called scheduled_update_checker.
    
    BUG=924762
    TEST=Compile.
    
    Change-Id: I7bb5be971524daf32c8e88333f9d9a9354194585
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757392
    Commit-Queue: Abhishek Bhardwaj <abhishekbh@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690958}

--- chrome/browser/chromeos/policy/scheduled_update_checker/device_scheduled_update_checker_unittest.cc
+++ chrome/browser/chromeos/policy/scheduled_update_checker/device_scheduled_update_checker_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/policy/device_scheduled_update_checker.h"
+#include "chrome/browser/chromeos/policy/scheduled_update_checker/device_scheduled_update_checker.h"
 
 #include <algorithm>
 #include <sstream>
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/clock.h"
 #include "base/time/time.h"
 #include "base/values.h"
-#include "chrome/browser/chromeos/policy/os_and_policies_update_checker.h"
-#include "chrome/browser/chromeos/policy/scoped_wake_lock.h"
+#include "chrome/browser/chromeos/policy/scheduled_update_checker/os_and_policies_update_checker.h"
+#include "chrome/browser/chromeos/policy/scheduled_update_checker/scoped_wake_lock.h"
 #include "chrome/browser/chromeos/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/chromeos/settings/stub_cros_settings_provider.h"
 #include "chrome/test/base/testing_browser_process.h"
--- chrome/browser/chromeos/policy/scheduled_update_checker/os_and_policies_update_checker.cc
+++ chrome/browser/chromeos/policy/scheduled_update_checker/os_and_policies_update_checker.cc
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/policy/os_and_policies_update_checker.h"
+#include "chrome/browser/chromeos/policy/scheduled_update_checker/os_and_policies_update_checker.h"
 
 #include <utility>
 
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chromeos/policy/task_executor_with_retries.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/update_engine_client.h"
 #include "chromeos/network/network_handler.h"
