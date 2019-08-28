a79bdbc6046b705741c5a46ada0f3e85b419e3f8
Max Moroz <mmoroz@chromium.org>
2019-08-28 16:07:52 +0000

    [chrome] Use FuzzedDataProvider included in LLVM package.
    This CL was uploaded by git cl split.
    
    TBR=thestig@chromium.org
    
    Bug: 991200
    Change-Id: I24e719990d5f24aec7b00ab5fd116f2e65f6be3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774685
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Commit-Queue: Max Moroz <mmoroz@chromium.org>
    Auto-Submit: Max Moroz <mmoroz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691196}

--- chrome/browser/chromeos/printing/zeroconf_printer_detector_fuzzer.cc
+++ chrome/browser/chromeos/printing/zeroconf_printer_detector_fuzzer.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <fuzzer/FuzzedDataProvider.h>
+
 #include <cstddef>
 #include <cstdint>
 #include <map>
@@ -14,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "chrome/browser/chromeos/printing/zeroconf_printer_detector.h"
 #include "chrome/browser/local_discovery/service_discovery_device_lister.h"
-#include "third_party/libFuzzer/src/utils/FuzzedDataProvider.h"
 
 namespace {
 
