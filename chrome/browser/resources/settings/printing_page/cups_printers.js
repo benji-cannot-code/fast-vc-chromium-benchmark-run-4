5deb997731f4eb141fb25a4ed420a5288547eaad
Jimmy Gong <jimmyxgong@google.com>
2019-08-28 00:35:01 +0000

    Update offline UI for printer settings
    - When there is no connectivity, we disable the ability to add a
      printer and display a message to users.
    
    Bug: 993834
    Test: browser tests, end to end manual
    Change-Id: I6f0eb28bcd2df604c8e27e799df239a646d0119a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1761482
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690952}

--- chrome/browser/resources/settings/printing_page/cups_printers.js
+++ chrome/browser/resources/settings/printing_page/cups_printers.js
@@ -37,8 +37,11 @@ Polymer({
       type: String,
     },
 
-    /** @private */
-    canAddPrinter_: Boolean,
+    /** This is also used as an attribute for css styling. */
+    canAddPrinter: {
+      type: Boolean,
+      reflectToAttribute: true,
+    },
 
     /** @private */
     showCupsEditPrinterDialog_: Boolean,
@@ -108,7 +111,7 @@ Polymer({
    * @private
    */
   onActiveNetworksChanged: function(networks) {
-    this.canAddPrinter_ = networks.some(function(network) {
+    this.canAddPrinter = networks.some(function(network) {
       // Note: Check for kOnline rather than using
       // OncMojo.connectionStateIsConnected() since the latter could return true
       // for networks without connectivity (e.g., captive portals).
