a509bb1fb2c5aed96330b9f141fb1b65c521a094
Scott Violet <sky@chromium.org>
2019-08-28 02:59:12 +0000

    weblayer: adds plumbing for BrowserObserver
    Here's the set of classes/changes:
    
    weblayer_private.BrowserControllerClient: this is a singleton that is supplied
    when creating BrowserControllerImpl. It will duplicate the C++ BrowserObserver
    interface along with any other server->client calls we want.
    
    BrowserController: supplies an implementation (as an inner class) of
    BrowserControllerClient to BrowserControllerImpl. BrowserController's inner
    class forwards calls to BrowserObservers.
    
    BrowserObserverProxy: there is a C++ and Java class with this name. The two
    are tightly coupled. The C++ side implements BrowserObserver and forwards to the
    java side. The java side forwards to BrowserControllerClient.
    
    BUG=none
    TEST=none
    
    Change-Id: I2817149224f4ac2045769c4ce0c3f81040dc7507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772501
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691007}

---
+++ weblayer/browser/java/org/chromium/weblayer_private/BrowserControllerClient.java
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer_private;
+
+/**
+ * Interface used by BrowserController to inform the client of changes. This largely duplicates the
+ *  BrowserObserver interface, but is a singleton to avoid unnecessary IPC.
+ */
+public interface BrowserControllerClient { public void displayURLChanged(String url); }
---
+++ weblayer/public/java/org/chromium/weblayer/BrowserObserver.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer;
+
+import android.net.Uri;
+
+/**
+ * Informed of interesting events that happen during the lifetime of a BrowserController.
+ */
+public abstract class BrowserObserver {
+    /**
+     * The Uri that should be displayed in the url-bar has updated.
+     */
+    public void displayURLChanged(Uri url) {}
+}
