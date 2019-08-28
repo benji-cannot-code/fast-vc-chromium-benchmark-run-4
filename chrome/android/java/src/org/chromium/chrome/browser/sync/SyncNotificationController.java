dbd9b4d596079f174ece78a561fd2e07aa330cea
Boris Sazonov <bsazonov@chromium.org>
2019-08-28 20:30:43 +0000

    [Android] Cleanup intent creation for PassphraseActivity
    From Intent.setComponent documentation: "If this class is defined, the
    specified class will always be used regardless of the other fields."
    Moreover, PassphraseActivity doesn't use the action from the intent, so
    it can be safely omitted, too. Thus the only relevant parameter for the
    intent is ComponentName that can be created using a two-argument ctor.
    
    Bug: None
    Change-Id: Id6875c683b23525e2581ec3bc6ce149cd7382ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774446
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691309}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncNotificationController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncNotificationController.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.sync;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -161,10 +160,7 @@ public class SyncNotificationController implements ProfileSyncService.SyncStateC
         // Make sure we don't prompt too many times.
         mProfileSyncService.setPassphrasePrompted(true);
 
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.setComponent(
-                new ComponentName(ContextUtils.getApplicationContext(), PassphraseActivity.class));
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        Intent intent = new Intent(ContextUtils.getApplicationContext(), PassphraseActivity.class);
         // This activity will become the start of a new task on this history stack.
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         // Clears the task stack above this activity if it already exists.
