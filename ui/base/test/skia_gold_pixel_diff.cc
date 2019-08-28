a3bc9c190a7b8fd811b3aab7c2383b73daf97263
Sven Zheng <svenzheng@chromium.org>
2019-08-28 22:15:49 +0000

    Switch pixeltests bucket from chrome-gpu to chrome
    In this way, we still have the same bucket with chrome gpu, but
    have different corpora with gpu tests.
    
    Bug: 996316
    Change-Id: Ia18b4662a333ca8e9af0aea282aade1bd70e1b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773837
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691352}

--- ui/base/test/skia_gold_pixel_diff.cc
+++ ui/base/test/skia_gold_pixel_diff.cc
@@ -28,8 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image.h"
 #include "ui/snapshot/snapshot.h"
 
-// This may change to a different bucket in the future.
-const char* kSkiaGoldInstance = "chrome-gpu";
+const char* kSkiaGoldInstance = "chrome";
 
 #if defined(OS_WIN)
 const wchar_t* kSkiaGoldCtl = L"tools/skia_goldctl/goldctl.exe";
@@ -202,6 +201,7 @@ bool SkiaGoldPixelDiff::UploadToSkiaGoldServer(
   base::ScopedAllowBlockingForTesting allow_blocking;
   base::CommandLine cmd(GetAbsoluteSrcRelativePath(kSkiaGoldCtl));
   cmd.AppendSwitchASCII("test-name", remote_golden_image_name);
+  cmd.AppendSwitchASCII("add-test-key", "source_type:gtest-pixeltests");
   cmd.AppendSwitchPath("png-file", local_file_path);
   cmd.AppendSwitchPath("work-dir", working_dir_);
   AppendArgsJustAfterProgram(
