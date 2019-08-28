bcc02de36063e306de7db74d1808e5a194884fbe
Darren Shen <shend@chromium.org>
2019-08-28 02:41:14 +0000

    sheriff: Fix flakiness in ExtensionInputMethodApiTest.Typing.
    TBR=shuchen@chromium.org
    
    Bug: 997888
    Change-Id: Ic7dfb8088ec76028c2c1daabdcd582c210c7560b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772931
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691004}

--- chrome/test/data/extensions/api_test/input_method/typing/background.js
+++ chrome/test/data/extensions/api_test/input_method/typing/background.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class TestEnv {
   constructor() {
     this.inputContext = null;
+    this.surroundingText = '';
+    this.compositionBounds = [];
 
     chrome.input.ime.onFocus.addListener((context) => {
       this.inputContext = context;
@@ -14,33 +16,33 @@ class TestEnv {
     chrome.input.ime.onBlur.addListener(() => {
       this.inputContext = null;
     });
+
+    chrome.input.ime.onSurroundingTextChanged.addListener(
+        (_, surroundingInfo) => {
+          this.surroundingText = surroundingInfo.text;
+        });
+
+    chrome.inputMethodPrivate.onCompositionBoundsChanged.addListener(
+        (_, boundsList) => {
+          this.compositionBounds = boundsList;
+        });
   }
 
   getContextID() {
     return this.inputContext.contextID;
   }
-
-  onSurroundingTextChanged() {
-    return new Promise((resolve) => {
-      chrome.input.ime.onSurroundingTextChanged.addListener(
-          function listener(_, surroundingInfo) {
-            chrome.input.ime.onSurroundingTextChanged.removeListener(listener);
-            resolve(surroundingInfo.text);
-          });
-    });
-  }
-
-  onCompositionBoundsChanged() {
-    return new Promise((resolve) => {
-      chrome.inputMethodPrivate.onCompositionBoundsChanged.addListener(
-          function listener(_, boundsList) {
-            chrome.inputMethodPrivate.onCompositionBoundsChanged.removeListener(
-                listener);
-            resolve(boundsList);
-          });
-    });
-  }
 };
+
+function waitUntil(predicate) {
+  return new Promise((resolve) => {
+    const timer = setInterval(() => {
+      if (predicate()) {
+        clearInterval(timer);
+        resolve();
+      }
+    }, 100);
+  });
+}
 
 const testEnv = new TestEnv();
 
@@ -86,8 +88,7 @@ chrome.test.runTests([
       text: 'hello world'
     });
 
-    chrome.test.assertEq('hello world',
-        await testEnv.onSurroundingTextChanged());
+    await waitUntil(() => testEnv.surroundingText === 'hello world');
 
     // Cursor is at the end of the string.
     await asyncInputMethodPrivate.setCompositionRange({
@@ -101,8 +102,7 @@ chrome.test.runTests([
     });
 
     // Should underline "world".
-    chrome.test.assertEq(5,
-      (await testEnv.onCompositionBoundsChanged()).length);
+    await waitUntil(() => testEnv.compositionBounds.length === 5);
 
     await asyncInputIme.setComposition({
       contextID: testEnv.getContextID(),
@@ -111,8 +111,7 @@ chrome.test.runTests([
     });
 
     // Composition should change to "foo".
-    chrome.test.assertEq(3,
-      (await testEnv.onCompositionBoundsChanged()).length);
+    await waitUntil(() => testEnv.compositionBounds.length === 3);
 
     // Should replace composition with "again".
     await asyncInputIme.commitText({
@@ -120,8 +119,7 @@ chrome.test.runTests([
       text: 'again'
     });
 
-    chrome.test.assertEq('hello again',
-        await testEnv.onSurroundingTextChanged());
+    await waitUntil(() => testEnv.surroundingText === 'hello again');
 
     // Cursor is at end of the string.
     // Call setCompositionRange with no segments.
@@ -132,8 +130,7 @@ chrome.test.runTests([
     });
 
     // Composition should be "again".
-    chrome.test.assertEq(5,
-      (await testEnv.onCompositionBoundsChanged()).length);
+    waitUntil(() => testEnv.compositionBounds.length === 5);
 
     // Should commit "again" and set composition to "in".
     await asyncInputMethodPrivate.setCompositionRange({
@@ -141,6 +138,8 @@ chrome.test.runTests([
       selectionBefore: 2,
       selectionAfter: 0
     });
+
+    await waitUntil(() => testEnv.compositionBounds.length === 2);
 
     chrome.test.succeed();
   }
