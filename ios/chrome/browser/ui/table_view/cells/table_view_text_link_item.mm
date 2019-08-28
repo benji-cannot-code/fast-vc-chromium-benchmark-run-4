1822ead0443c95e458bb558eb532ca8978bafd43
Nazerke <Nazerke@google.com>
2019-08-28 15:22:01 +0000

    [iOS][dark mode] clear browsing data page in history
    The colors are changed for the Find-out-more button and for tint color
    of this view used for checked mark sign.
    
    Bug: 995271
    Change-Id: I19eef25e353b615df72412d2b959aeab8376c337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1765692
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691180}

--- ios/chrome/browser/ui/table_view/cells/table_view_text_link_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_link_item.mm
@@ -11,17 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/util/label_link_controller.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/colors/UIColor+cr_semantic_colors.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/chrome/common/string_util.h"
 #include "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
-
-namespace {
-// The width and height of the favicon ImageView.
-const CGFloat kTextCellLinkColor = 0x1A73E8;
-}  // namespace
 
 #pragma mark - TableViewTextLinkItem
 
@@ -107,7 +103,7 @@ const CGFloat kTextCellLinkColor = 0x1A73E8;
                [[weakSelf delegate] tableViewTextLinkCell:weakSelf
                                         didRequestOpenURL:URL];
              }];
-  [self.labelLinkController setLinkColor:UIColorFromRGB(kTextCellLinkColor)];
+  [self.labelLinkController setLinkColor:[UIColor colorNamed:kBlueColor]];
 
   // Remove link delimiter from text and get ranges for links. Must be parsed
   // before being added to the controller because modifying the label text
