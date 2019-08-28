83758c8a2ff4a6a32e52a523fa04fd41e5ddc16a
Alexey Baskakov <loyso@chromium.org>
2019-08-28 03:03:12 +0000

    WebApp: Make GetAppById to return read-only const WebApp*
    We will introduce a "batched writes" API in next CLs.
    Current access to the apps that are owned by WebAppRegistrar (the LevelDb)
    should be read-only.
    
    Next CL: Make WebAppRegistrar::AllApps API read only (const).
    
    Bug: 891172
    Change-Id: Id3fb57f341683ead6ce83359d9f8d2147603b73b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772932
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691008}

--- chrome/browser/web_applications/web_app_install_task_unittest.cc
+++ chrome/browser/web_applications/web_app_install_task_unittest.cc
@@ -367,7 +367,7 @@ TEST_F(WebAppInstallTaskTest, InstallFromWebContents) {
 
   EXPECT_TRUE(callback_called);
 
-  WebApp* web_app = registrar_->GetAppById(app_id);
+  const WebApp* web_app = registrar_->GetAppById(app_id);
   EXPECT_NE(nullptr, web_app);
 
   EXPECT_EQ(app_id, web_app->app_id());
@@ -505,7 +505,7 @@ TEST_F(WebAppInstallTaskTest, InstallableCheck) {
 
   EXPECT_TRUE(callback_called);
 
-  WebApp* web_app = registrar_->GetAppById(app_id);
+  const WebApp* web_app = registrar_->GetAppById(app_id);
   EXPECT_NE(nullptr, web_app);
 
   // Manifest data overrides Renderer data, except |description|.
@@ -715,7 +715,7 @@ TEST_F(WebAppInstallTaskTest, UserInstallDeclined) {
 
   EXPECT_TRUE(callback_called);
 
-  WebApp* web_app = registrar_->GetAppById(app_id);
+  const WebApp* web_app = registrar_->GetAppById(app_id);
   EXPECT_EQ(nullptr, web_app);
 }
 
