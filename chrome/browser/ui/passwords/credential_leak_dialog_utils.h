a787104949c398aae9d080b046c44dd596dcd354
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-08-28 17:05:49 +0000

    [Passwords] Record Metrics for Leak Detection Dialog
    This change implements logic to record metrics for interactions with the
    Leak Detection Dialog on Desktop.
    
    Bug: 986317
    Change-Id: Ifb2a7693ffaafa929f718a93198369ff09220fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773144
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691214}

--- chrome/browser/ui/passwords/credential_leak_dialog_utils.h
+++ chrome/browser/ui/passwords/credential_leak_dialog_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "components/password_manager/core/browser/leak_detection_dialog_utils.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "url/gurl.h"
 
 namespace leak_dialog_utils {
@@ -32,6 +33,10 @@ bool ShouldCheckPasswords(password_manager::CredentialLeakType leak_type);
 
 // Checks whether the leak dialog should show cancel button.
 bool ShouldShowCancelButton(password_manager::CredentialLeakType leak_type);
+
+// Returns the LeakDialogType corresponding to |leak_type|.
+password_manager::metrics_util::LeakDialogType GetLeakDialogType(
+    password_manager::CredentialLeakType leak_type);
 
 // Returns the URL used to launch the password checkup.
 GURL GetPasswordCheckupURL();
