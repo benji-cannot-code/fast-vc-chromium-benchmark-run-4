5a05f62050355879c7ec31b47f46e0d3f615cd8b
Zhenyao Mo <zmo@chromium.org>
2019-08-28 20:35:23 +0000

    Add support for IPG to run on MacOSX
    BUG=867155
    TEST=bots
    R=sunnyps@chromium.org
    
    Change-Id: I07c9d86f4db8e47a5c6ab59a3ba841436904dcc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772392
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691314}

--- content/test/gpu/gpu_tests/ipg_utils.py
+++ content/test/gpu/gpu_tests/ipg_utils.py
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """This script implements a few IntelPowerGadget related helper functions.
 
-This script only works on Windows with Intel CPU. Intel Power Gadget needs to
-be installed on the machine before this script works. The software can be
+This script only works on Windows/Mac with Intel CPU. Intel Power Gadget needs
+to be installed on the machine before this script works. The software can be
 downloaded from:
-  https://software.intel.com/en-us/articles/intel-power-gadget-20
+  https://software.intel.com/en-us/articles/intel-power-gadget
 
 An easy way to use the APIs are:
 1) Launch your program.
@@ -25,15 +25,21 @@ import json
 import logging
 import os
 import subprocess
+import sys
 
 def LocateIPG():
-  ipg_dir = os.getenv('IPG_Dir')
-  if not ipg_dir:
-    raise Exception("No env IPG_Dir")
-  gadget_path = os.path.join(ipg_dir, "PowerLog3.0.exe")
-  if not os.path.isfile(gadget_path):
-    raise Exception("Can't locate Intel Power Gadget at " + gadget_path)
-  return gadget_path
+  if sys.platform == 'win32':
+    ipg_dir = os.getenv('IPG_Dir')
+    if not ipg_dir:
+      raise Exception("No env IPG_Dir")
+    gadget_path = os.path.join(ipg_dir, "PowerLog3.0.exe")
+    if not os.path.isfile(gadget_path):
+      raise Exception("Can't locate Intel Power Gadget at " + gadget_path)
+    return gadget_path
+  if sys.platform == 'darwin':
+    return '/Applications/Intel Power Gadget/PowerLog'
+  raise Exception("Only supported on Windows/Mac")
+
 
 def GenerateIPGLogFilename(log_prefix='PowerLog', log_dir=None, current_run=1,
                            total_runs=1, timestamp=False):
@@ -47,6 +53,7 @@ def GenerateIPGLogFilename(log_prefix='PowerLog', log_dir=None, current_run=1,
     log_prefix = "%s_%s" % (log_prefix, now.strftime('%Y%m%d%H%M%S'))
   return os.path.join(log_dir, log_prefix + '.csv')
 
+
 def RunIPG(duration_in_s=60, resolution_in_ms=100, logfile=None):
   intel_power_gadget_path = LocateIPG()
   command = ('"%s" -duration %d -resolution %d' %
@@ -56,9 +63,10 @@ def RunIPG(duration_in_s=60, resolution_in_ms=100, logfile=None):
     logfile = GenerateIPGLogFilename()
   command = command + (' -file %s' %logfile)
   logging.debug("Running: " + command)
-  output = subprocess.check_output(command)
+  output = subprocess.check_output(command, shell=True)
   logging.debug("Running: DONE")
   logging.debug(output)
+
 
 def AnalyzeIPGLogFile(logfile=None, skip_in_sec=0):
   if not logfile:
@@ -73,16 +81,17 @@ def AnalyzeIPGLogFile(logfile=None, skip_in_sec=0):
   sums = []
   col_time = None
   for line in open(logfile):
-    tokens = line.split(',')
+    tokens = [token.strip('" ') for token in line.split(',')]
     if first_line:
       first_line = False
       cols = len(tokens)
       for ii in range(0, cols):
-        if tokens[ii].startswith('Elapsed Time'):
+        token = tokens[ii]
+        if token.startswith('Elapsed Time'):
           col_time = ii
-        elif tokens[ii].endswith('(Watt)'):
+        elif token.endswith('(Watt)'):
           indices.append(ii)
-          labels.append(tokens[ii][:-len('(Watt)')])
+          labels.append(token[:-len('(Watt)')])
           sums.append(0.0)
       assert col_time
       assert cols > 0
@@ -101,6 +110,7 @@ def AnalyzeIPGLogFile(logfile=None, skip_in_sec=0):
     for ii in range(0, len(indices)):
       results[labels[ii]] = sums[ii] / samples
   return results
+
 
 def ProcessResultsFromMultipleIPGRuns(logfiles, skip_in_seconds=0,
                                       outliers=0, output_json=None):
--- content/test/gpu/gpu_tests/power_measurement_integration_test.py
+++ content/test/gpu/gpu_tests/power_measurement_integration_test.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """This script only works on Windows with Intel CPU. Intel Power Gadget needs
 to be installed on the machine before this script works. The software can be
 downloaded from:
-  https://software.intel.com/en-us/articles/intel-power-gadget-20
+  https://software.intel.com/en-us/articles/intel-power-gadget
 
 To run this test on a target machine without Chromium workspace checked out:
 1) inside Chromium workspace, run
