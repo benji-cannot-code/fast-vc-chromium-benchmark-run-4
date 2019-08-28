17855b7926aa8eede0cda065a5e79649a6145a46
Hans Wennborg <hans@chromium.org>
2019-08-28 17:45:00 +0000

    Build clang with gcc 5.3.0 on Linux
    This brings us closer to the gcc version used on Xenial, which we will use once
    all buildbots have upgraded to it.
    
    Bug: 994556,998569
    Change-Id: I5dded6ff0651f481ccd1c1ddd66caadb2f55df4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771908
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Reid Kleckner <rnk@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691238}

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -120,6 +120,7 @@ def CheckoutLLVM(commit, dir):
 
   # Otherwise, do a fresh clone.
   if os.path.isdir(dir):
+    os.chdir(CHROMIUM_DIR)  # Can't remove dir if we're in it.
     print('Removing %s.' % dir)
     RmTree(dir)
   if RunCommand(clone_cmd, fail_hard=False):
@@ -240,9 +241,9 @@ def MaybeDownloadHostGcc(args):
   """Download a modern GCC host compiler on Linux."""
   if not sys.platform.startswith('linux') or args.gcc_toolchain:
     return
-  gcc_dir = os.path.join(LLVM_BUILD_TOOLS_DIR, 'gcc510trusty')
+  gcc_dir = os.path.join(LLVM_BUILD_TOOLS_DIR, 'gcc530trusty')
   if not os.path.exists(gcc_dir):
-    DownloadAndUnpack(CDS_URL + '/tools/gcc510trusty.tgz', gcc_dir)
+    DownloadAndUnpack(CDS_URL + '/tools/gcc530trusty.tgz', gcc_dir)
   args.gcc_toolchain = gcc_dir
 
 
