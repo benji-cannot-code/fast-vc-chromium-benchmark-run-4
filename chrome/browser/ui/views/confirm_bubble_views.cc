1ea98006162afec9c49573b9a558178eb66e432d
Allen Bauer <kylixrd@chromium.org>
2019-08-28 17:50:40 +0000

    Make confirm bubble message text Body 1 secondary.
    While the referenced bug only specifies the Ask Google For suggestions dialog, the issue is the same for all confirmation dialogs. In fact, there is no way to only change the one dialog.
    
    Bug: 654139
    Change-Id: Ic34bf6ee98fb11ad37f31bd48c63eec7453fa6fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774470
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691243}

--- chrome/browser/ui/views/confirm_bubble_views.cc
+++ chrome/browser/ui/views/confirm_bubble_views.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/confirm_bubble.h"
 #include "chrome/browser/ui/confirm_bubble_model.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "chrome/browser/ui/views/chrome_typography.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/image_button_factory.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/grid_layout.h"
+#include "ui/views/style/typography.h"
 #include "ui/views/widget/widget.h"
 
 ConfirmBubbleViews::ConfirmBubbleViews(
@@ -40,7 +42,9 @@ ConfirmBubbleViews::ConfirmBubbleViews(
                 kMaxMessageWidth, false);
 
   // Add the message label.
-  auto label = std::make_unique<views::Label>(model_->GetMessageText());
+  auto label = std::make_unique<views::Label>(
+      model_->GetMessageText(), views::style::CONTEXT_MESSAGE_BOX_BODY_TEXT,
+      views::style::STYLE_SECONDARY);
   DCHECK(!label->GetText().empty());
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   label->SetMultiLine(true);
