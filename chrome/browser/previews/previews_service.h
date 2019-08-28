336ae682d59db60cd1e0f6ed61214a0b6895fe23
Tarun Bansal <tbansal@chromium.org>
2019-08-28 17:59:26 +0000

    Detects a client redirect loop with DeferAllScript
    Navigating to some websites with DeferAllScript enabled for that site
    could cause a client redirect loop. A previous CL
    (https://chromium-review.googlesource.com/c/chromium/src/+/1739493)
    implemented this in a different way but that CL is not
    mergeable back.
    
    This CL alternates it in a different way which also fixes the problem
    and can be merged back.
    
    All the logic is behind a finch parameter and so can be easil turned off
    using finch.
    
    Tested by commenting out the older code. This will also
    be tested by beta builders using browser tests.
    
    Bug: 987062
    Change-Id: Ia5f9c693f3d99a0829f426ddcc0507c60280aba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774214
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691246}

--- chrome/browser/previews/previews_service.h
+++ chrome/browser/previews/previews_service.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/mru_cache.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
@@ -93,6 +94,20 @@ class PreviewsService : public KeyedService {
   // Returns the enabled PreviewsTypes with their version.
   static blacklist::BlacklistData::AllowedTypesAndVersions GetAllowedPreviews();
 
+  // Called when that there is a redirect from |start_url| to |end_url|. Called
+  // only when DeferAllScript preview feature is enabled.
+  void ReportObservedRedirectWithDeferAllScriptPreview(const GURL& start_url,
+                                                       const GURL& end_url);
+
+  // Returns true if |url| is marked as eligible for defer all script preview.
+  bool IsUrlEligibleForDeferAllScriptPreview(const GURL& url) const;
+
+  // Returns true if |start_url| leads to a URL redirect cycle based on
+  // |redirect_history|.
+  static bool HasURLRedirectCycle(
+      const GURL& start_url,
+      const base::MRUCache<GURL, GURL>& redirect_history);
+
  private:
   // The top site provider for use with the Previews Optimization Guide's Hints
   // Fetcher.
@@ -113,6 +128,11 @@ class PreviewsService : public KeyedService {
   // URL Factory for the Previews Optimization Guide's Hints Fetcher.
   scoped_refptr<network::SharedURLLoaderFactory>
       optimization_guide_url_loader_factory_;
+
+  // Stores history of URL redirects. Key is the starting URL and value is the
+  // URL that the starting URL redirected to. Populated only when DeferAllScript
+  // preview feature is enabled.
+  base::MRUCache<GURL, GURL> redirect_history_;
 
   DISALLOW_COPY_AND_ASSIGN(PreviewsService);
 };
