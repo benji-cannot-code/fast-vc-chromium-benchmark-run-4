9b38ddb03201c7e9c825dd04e53a10ec3467e473
Erik Chen <erikchen@chromium.org>
2019-08-28 01:02:28 +0000

    Fix RenderWidget's WebWidget references.
    RenderWidget holds onto a WebWidget. It has two methods GetFrameWidget() and
    GetWebWidget() which should both just reference the WebWidget. Instead,
    RenderWidget will [inappropriately] call out to its delegate to get the
    FrameWebWidget, which the WebWidget should already be. This induces confusion.
    
    This CL fixes the RenderWidget's webwidget_internal_ to always be a
    WebFrameWidget when the RenderWidget is associated with a RenderView. This
    changes the semantics, as we will never pass a WebViewImpl as a
    RenderWidget's webwidget_internal_. This has one main side effect -- a remote
    main frame's RenderWidget will now have a null WebWidget.
    
    Change-Id: I930a9c91a88ac05882289df795ed1c4008e146fd
    Bug: 995981, 419087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1766889
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#690968}

--- content/renderer/render_widget_browsertest.cc
+++ content/renderer/render_widget_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/visual_properties.h"
 #include "content/public/renderer/render_frame_visitor.h"
 #include "content/public/test/render_view_test.h"
+#include "content/renderer/compositor/layer_tree_view.h"
 #include "content/renderer/render_frame_proxy.h"
 #include "content/renderer/render_thread_impl.h"
 #include "content/renderer/render_view_impl.h"
@@ -222,6 +223,25 @@ TEST_F(RenderWidgetTest, PageFocusIme) {
   CommitText(text);
   EXPECT_EQ("hello world",
             GetInputMethodController()->TextInputInfo().value.Utf8());
+}
+
+// Tests that the value of VisualProperties::is_pinch_gesture_active is
+// not propagated to the LayerTreeHost when properties are synced for main
+// frame.
+TEST_F(RenderWidgetTest, ActivePinchGestureUpdatesLayerTreeHost) {
+  auto* layer_tree_host = widget()->layer_tree_view()->layer_tree_host();
+  EXPECT_FALSE(layer_tree_host->is_external_pinch_gesture_active_for_testing());
+  content::VisualProperties visual_properties;
+
+  // Sync visual properties on a mainframe RenderWidget.
+  visual_properties.is_pinch_gesture_active = true;
+  widget()->OnSynchronizeVisualProperties(visual_properties);
+  // We do not expect the |is_pinch_gesture_active| value to propagate to the
+  // LayerTreeHost for the main-frame. Since GesturePinch events are handled
+  // directly by the layer tree for the main frame, it already knows whether or
+  // not a pinch gesture is active, and so we shouldn't propagate this
+  // information to the layer tree for a main-frame's widget.
+  EXPECT_FALSE(layer_tree_host->is_external_pinch_gesture_active_for_testing());
 }
 
 }  // namespace content
