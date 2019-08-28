b6b949f64b172cc5246d68ee5bdcbdc4e9ba8bfc
Nicolás Peña Moreno <npm@chromium.org>
2019-08-28 15:24:10 +0000

    [Longtasks] Add microtask test
    Bug: 980581
    Change-Id: I3756836d5d865e7d68530f515a310bf925229148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769296
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691181}

---
+++ third_party/blink/web_tests/external/wpt/longtask-timing/long-microtask.window.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async_test(function (t) {
+    if (typeof PerformanceLongTaskTiming === 'undefined') {
+        assert_unreached("Longtasks are not supported.");
+        t.done();
+    }
+    new PerformanceObserver(
+        t.step_func_done(entryList => {
+            const entries = entryList.getEntries();
+            assert_equals(entries.length, 1,
+                'Exactly one entry is expected.');
+            const longtask = entries[0];
+            assert_equals(longtask.entryType, 'longtask');
+            assert_equals(longtask.name, 'self');
+            assert_greater_than(longtask.duration, 50);
+            assert_greater_than_equal(longtask.startTime, 0);
+            const currentTime = performance.now();
+            assert_less_than_equal(longtask.startTime, currentTime);
+            t.done();
+        })
+    ).observe({entryTypes: ['longtask']});
+
+    window.onload = () => {
+        /* Generate a slow microtask */
+        Promise.resolve().then(() => {
+            const begin = window.performance.now();
+            while (window.performance.now() < begin + 60);
+        });
+    };
+}, 'A short task followed by a long microtask is observable.');
