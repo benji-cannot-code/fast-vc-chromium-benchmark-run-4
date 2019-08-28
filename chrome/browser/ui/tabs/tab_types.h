10c2b60c1fa4eeba02161598c251a394e7bfb801
Connie Wan <connily@chromium.org>
2019-08-28 18:08:32 +0000

    Create universal Active, Open, and Pinned enums
    These represent the possible states for each property. Most booleans related to these properties should use these enums instead.
    
    This replaces some duplicate existing enums:
    TabAnimationState: https://cs.chromium.org/chromium/src/chrome/browser/ui/views/tabs/tab_animation_state.h?l=15,17,19&rcl=858114e992439c4897df9ecc7f910a8cb1840c65
    TabState: https://cs.chromium.org/chromium/src/chrome/browser/ui/views/tabs/tab_strip_types.h?l=27&rcl=858114e992439c4897df9ecc7f910a8cb1840c65
    
    Most bool-based APIs will also need to be migrated over.
    
    See subsequent CLs for followup migrations.
    
    Bug: 966627
    Change-Id: I64d32624a1c2deb6f90264179e5be1f5334d8d5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764447
    Commit-Queue: Connie Wan <connily@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691253}

---
+++ chrome/browser/ui/tabs/tab_types.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_TABS_TAB_TYPES_H_
+#define CHROME_BROWSER_UI_TABS_TAB_TYPES_H_
+
+// Potential Active states for a tab.
+enum class TabActive {
+  kActive,
+  kInactive,
+};
+
+// Potential Open states for a tab.
+enum class TabOpen {
+  kOpen,
+  kClosed,
+};
+
+// Potential Pinned states for a tab.
+enum class TabPinned {
+  kPinned,
+  kUnpinned,
+};
+
+#endif  // CHROME_BROWSER_UI_TABS_TAB_TYPES_H_
