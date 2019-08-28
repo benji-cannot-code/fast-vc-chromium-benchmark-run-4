5a26294fbbf1e0528236bd8b8a5b9a46457a3e56
Christopher Grant <cjgrant@chromium.org>
2019-08-28 21:22:21 +0000

    VR: Do not dlclose() the UI native library on VR shutdown
    On some platforms, dlclose() on the UI lib works fine. On Android M, it
    causes a crash when stopping presentation on non-Daydream-ready devices
    (eg. a Nexus 5X).
    
    Using dlclose() on this DFM library was originally a risky proposition,
    given the warnings about potential issues. Now that we've seen such an
    issue, abandon that dream.
    
    Bug: 994029
    Change-Id: I28f3180383c583fc2e0c2ecc932e4697d18c43a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774832
    Auto-Submit: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691340}

--- chrome/browser/android/vr/ui_module_factory.h
+++ chrome/browser/android/vr/ui_module_factory.h
@@ -17,8 +17,6 @@ namespace vr {
 // construct a UI.
 class UiModuleFactory : public UiFactory {
  public:
-  ~UiModuleFactory() override;
-
   std::unique_ptr<UiInterface> Create(
       UiBrowserInterface* browser,
       PlatformInputHandler* content_input_forwarder,
@@ -26,9 +24,6 @@ class UiModuleFactory : public UiFactory {
       std::unique_ptr<TextInputDelegate> text_input_delegate,
       std::unique_ptr<AudioDelegate> audio_delegate,
       const UiInitialState& ui_initial_state) override;
-
- private:
-  void* ui_library_handle_ = nullptr;
 };
 
 }  // namespace vr
