39d82a68b37b8fa68159795540eaa33612007b76
Nazerke <Nazerke@google.com>
2019-08-28 14:46:25 +0000

    [iOS][dark mode] Time range page in history
    Tint color for checked mark and the background color are changed
    accordingly to dark mode.
    
    Bug: 996246
    Change-Id: I47f8f1d15c491ce4b7740ff8e17de99e9fa6f7f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768319
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691161}

--- ios/chrome/browser/ui/table_view/cells/table_view_detail_text_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_detail_text_item.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/colors/UIColor+cr_semantic_colors.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -59,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   } else {
     cell.detailTextLabel.textColor = UIColor.cr_secondaryLabelColor;
   }
+  cell.tintColor = [UIColor colorNamed:kBlueColor];
   cell.textLabel.textAlignment =
       self.textAlignment ? self.textAlignment : NSTextAlignmentNatural;
   cell.detailTextLabel.textAlignment =
