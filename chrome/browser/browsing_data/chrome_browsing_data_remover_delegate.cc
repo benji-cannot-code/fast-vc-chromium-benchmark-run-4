0d7a4f45f872c8640e5d1dbb0e8606399992adf0
Robert Ogden <robertogden@chromium.org>
2019-08-28 23:51:01 +0000

    Clear AvailabilityProber along with browsing history
    The cached data is an optimization and non-trivial to sort out per-
    origin, so just remove it all.
    
    Note that this is safe to do even when an active prober has a copy of
    the dictionary being cleared because the dictionary is read before
    being written to.
    
    Change-Id: Id1980ef20c3e08a1b0e556a616266e3b156080da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772240
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691378}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/feed/feed_host_service_factory.h"
 #include "chrome/browser/autofill/personal_data_manager_factory.h"
 #include "chrome/browser/autofill/strike_database_factory.h"
+#include "chrome/browser/availability/availability_prober.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browsing_data/browsing_data_helper.h"
@@ -585,6 +586,8 @@ void ChromeBrowsingDataRemoverDelegate::RemoveEmbedderData(
         OptimizationGuideKeyedServiceFactory::GetForProfile(profile_);
     if (optimization_guide_keyed_service)
       optimization_guide_keyed_service->ClearData();
+
+    AvailabilityProber::ClearData(prefs);
 
 #if defined(OS_ANDROID)
     OomInterventionDecider* oom_intervention_decider =
