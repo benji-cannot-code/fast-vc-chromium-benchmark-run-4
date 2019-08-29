ea49dec5539f31a59107df70c9757fc2292bf908
Ryan Hamilton <rch@chromium.org>
2019-08-29 19:41:53 +0000

    Always return 0 from QuicPickServerPortForTestsOrDieImpl to force the OS to pick an unused port and hence avoid flakiness in QUIC EndToEndTests.
    Bug: 997325
    Change-Id: I1c6eecac8e599863ab60077f1692d209fe055dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776603
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Auto-Submit: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691754}

--- net/quic/platform/impl/quic_port_utils_impl.cc
+++ net/quic/platform/impl/quic_port_utils_impl.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace quic {
 
 int QuicPickServerPortForTestsOrDieImpl() {
-  return 12345 + (QuicRandom::GetInstance()->RandUint64() % 20000);
+  return 0;  // Let the OS find an unused port.
 }
 
 void QuicRecyclePortImpl(int port) {}
