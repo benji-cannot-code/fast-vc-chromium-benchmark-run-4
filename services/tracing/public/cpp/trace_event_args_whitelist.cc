6dc403f3919a043906f3aabdd21464c87ee46b90
Etienne Bergeron <etienneb@chromium.org>
2019-08-29 18:40:57 +0000

    Whitelist arguments for ScopedThreadMayLoadLibraryOnBackgroundThread
    Add arguments from ScopedThreadMayLoadLibraryOnBackgroundThread to the
    tracing whitelist.
    
    ScopedThreadMayLoadLibraryOnBackgroundThread::
        ScopedThreadMayLoadLibraryOnBackgroundThread(const Location& from_here) {
    
      TRACE_EVENT_BEGIN2("base", "ScopedThreadMayLoadLibraryOnBackgroundThread",
                         "file_name", from_here.file_name(), "function_name",
                         from_here.function_name());
      [...]
    }
    
    R=oysteine@chromium.org
    
    Bug: 973868
    Change-Id: If6bfa7c224a4c69c9d004526c773ad0d69534ea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773702
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691735}

--- services/tracing/public/cpp/trace_event_args_whitelist.cc
+++ services/tracing/public/cpp/trace_event_args_whitelist.cc
@@ -51,6 +51,8 @@ const WhitelistEntry kEventArgsWhitelist[] = {
     {"__metadata", "stackFrames", nullptr},
     {"__metadata", "typeNames", nullptr},
     {"base", "ScopedBlockingCall*", kScopedBlockingCallAllowedArgs},
+    {"base", "ScopedThreadMayLoadLibraryOnBackgroundThread",
+     kScopedBlockingCallAllowedArgs},
     {"benchmark", "TestWhitelist*", nullptr},
     {"browser", "KeyedServiceFactory::GetServiceForContext", nullptr},
     {"fonts", "CachedFontLinkSettings::GetLinkedFonts", nullptr},
