ad36ac5d35ac56f8ffc9492ccb8250ffab78befb
rajendrant <rajendrant@chromium.org>
2019-08-29 23:19:19 +0000

    LazyLoad: Show translucent placeholder when deferring the full images
    When the image is fully deferred (2kb range request is not fetched) the
    image is shown as broken image with ALT text, for a brief moment until
    the image is fetched. This CL shows a 1x1 translucent placeholder in
    that case.
    
    
    Bug: 999019
    Change-Id: I523785ba7f4f4151a324895a4f8f7f612a7141cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773869
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691851}

--- third_party/blink/renderer/core/html/lazy_load_image_observer_test.cc
+++ third_party/blink/renderer/core/html/lazy_load_image_observer_test.cc
@@ -911,6 +911,7 @@ class LazyLoadAutomaticImagesTest : public SimTest {
         ScrollOffset(0, kLoadingDistanceThreshold + kViewportHeight),
         kProgrammaticScroll);
     Compositor().BeginFrame();
+    test::RunPendingTasks();
     full_resource.Complete(ReadTestImage());
     ExpectResourceIsFullImage(GetDocument().Fetcher()->CachedResource(
         KURL("https://example.com/image.png")));
@@ -1353,6 +1354,7 @@ TEST_F(LazyLoadAutomaticImagesTest, LazyLoadDisabledOnReload) {
                             LazyLoadAutomaticImagesTest::kViewportHeight),
         kProgrammaticScroll);
     Compositor().BeginFrame();
+    test::RunPendingTasks();
     auto_image.Complete(ReadTestImage());
     lazy_image.Complete(ReadTestImage());
     test::RunPendingTasks();
@@ -1380,6 +1382,7 @@ TEST_F(LazyLoadAutomaticImagesTest, LazyLoadDisabledOnReload) {
     SimSubresourceRequest lazy_image("https://example.com/image_lazy.png",
                                      "image/png");
     Compositor().BeginFrame();
+    test::RunPendingTasks();
     lazy_image.Complete(ReadTestImage());
     test::RunPendingTasks();
     histogram_tester.ExpectTotalCount(
