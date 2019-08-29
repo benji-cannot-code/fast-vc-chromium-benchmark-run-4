d977e9457c67ebb04e8ff62e5f7e0972d791c9f5
rbpotter <rbpotter@chromium.org>
2019-08-29 18:33:14 +0000

    WebUI: Migrate chrome://local-state to use JS modules.
    This is the first WebUI page leveraging native Javascript
    modules, used as a proof of concept for other WebUI pages to
    follow.
    
    Type-checking should also work
    ninja -C out/<out>/ chrome/browser/resources/local_state:closure_compile
    
    Bug: 915053
    Change-Id: I274f02c5f95e395131318ece0172750c9d815139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1414153
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691733}

--- chrome/browser/resources/local_state/local_state.js
+++ chrome/browser/resources/local_state/local_state.js
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Javascript for local_state.html, served from chrome://local-state/
  * This is used to debug the contents of the Local State file.
  */
+import {sendWithPromise} from 'chrome://resources/js/cr.m.js';
+import {$} from 'chrome://resources/js/util.m.js';
 
 // When the page loads, request the JSON local state data from C++.
 document.addEventListener('DOMContentLoaded', function() {
-  cr.sendWithPromise('requestJson').then(localState => {
+  sendWithPromise('requestJson').then(localState => {
     $('content').textContent = localState;
   });
 });
