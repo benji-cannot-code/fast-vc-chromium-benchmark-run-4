725f0906f2117eca89c27541e4ed9e06f51ecf37
Lucas Furukawa Gadani <lfg@chromium.org>
2019-08-29 22:44:34 +0000

    Fix CSP frame-ancestors wpt tests.
    These tests were broken by
    https://github.com/web-platform-tests/wpt/commit/38be30e734ea5afcdc5801613f197d17d716674d.
    
    Bug: 759184
    Change-Id: I39a9bc3da1d5a955975ce35a3201613c4efba010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776600
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691836}

--- third_party/blink/web_tests/external/wpt/content-security-policy/frame-ancestors/support/frame-ancestors-test.sub.js
+++ third_party/blink/web_tests/external/wpt/content-security-policy/frame-ancestors/support/frame-ancestors-test.sub.js
@@ -27,7 +27,7 @@ window.addEventListener("message", function (e) {
         window.parent.postMessage(e.data, "*");
     else
         if (e.data.type === 'test_result')
-            endTest(e.data.failed, "Inner IFrame msg: " + e.data.msg);
+            endTest(e.data.failed, "Inner IFrame msg: " + e.data.message);
 });
 
 function injectNestedIframe(policy, parent, child, expectation, isSandboxed) {
@@ -68,6 +68,7 @@ function iframeLoaded(expectBlock) {
         var failed = true;
         var message = "";
         try {
+            ev.target.contentWindow.location.href;
             if (expectBlock) {
                 message = "The IFrame should have been blocked (or cross-origin). It wasn't.";
                 failed = true;
