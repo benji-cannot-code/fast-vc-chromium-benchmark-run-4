8fb89b5a51ae082093845f1de2593665ca4a8516
Antonio Gomes <tonikitoo@igalia.com>
2019-08-29 11:50:15 +0000

    Replace uses of blink::WebRTCStatsMemberType
    ... by webrtc::::RTCStatsMemberInterface::Type
    
    This is a follow up of haraken@'s remark on [1].
    
    [1] https://crrev.com/c/1772550/3/third_party/blink/renderer/platform/peerconnection/rtc_stats.cc@223
    
    BUG=787254, 919392
    R=haraken@chromium.org
    
    Change-Id: I32e74b03813ee324263070603c4582e564381ffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772215
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#691587}

--- third_party/blink/renderer/platform/peerconnection/rtc_stats.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_stats.h
@@ -80,7 +80,7 @@ class PLATFORM_EXPORT RTCStatsMember : public blink::WebRTCStatsMember {
   ~RTCStatsMember() override;
 
   blink::WebString GetName() const override;
-  blink::WebRTCStatsMemberType GetType() const override;
+  webrtc::RTCStatsMemberInterface::Type GetType() const override;
   bool IsDefined() const override;
 
   bool ValueBool() const override;
