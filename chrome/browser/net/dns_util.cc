3b836318fb339cffd46b72d4252a220a2eceb5f1
dalyk <dalyk@google.com>
2019-08-29 01:06:21 +0000

    Reland "Add domain joined check to managed device detection for DoH."
    Now marks SystemNetworkContextManagerBrowsertests as non-enterprise-
    managed.
    
    > Original change's description:
    > > Add domain joined check to managed device detection for DoH.
    > >
    > > Some devices may be domain joined but not have any active policies.
    > > We should also disable DoH by default for that scenario.
    > >
    > > Bug: 955454
    > > Change-Id: I50ad015e9e5d072dddcaa72524e5d39ffb2e005c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773397
    > > Reviewed-by: Eric Orth <ericorth@chromium.org>
    > > Commit-Queue: Katharine Daly <dalyk@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#690809}
    
    Change-Id: I3840ffecaea5f7be465f6b08da4340be0e9503cc
    Bug: 955454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773750
    Commit-Queue: Katharine Daly <dalyk@google.com>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691422}

--- chrome/browser/net/dns_util.cc
+++ chrome/browser/net/dns_util.cc
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/uri_template/uri_template.h"
 #include "url/gurl.h"
 
+#if defined(OS_WIN)
+#include "base/enterprise_util.h"
+#endif
+
 namespace chrome_browser_net {
 
 bool IsValidDohTemplate(const std::string& server_template,
@@ -44,8 +48,12 @@ bool IsValidDohTemplate(const std::string& server_template,
 
 bool ShouldDisableDohForManaged() {
 #if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
-  return g_browser_process->browser_policy_connector()
-      ->HasMachineLevelPolicies();
+  if (g_browser_process->browser_policy_connector()->HasMachineLevelPolicies())
+    return true;
+#endif
+#if defined(OS_WIN)
+  if (base::IsMachineExternallyManaged())
+    return true;
 #endif
   return false;
 }
--- chrome/browser/net/system_network_context_manager_browsertest.cc
+++ chrome/browser/net/system_network_context_manager_browsertest.cc
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 
+#if defined(OS_WIN)
+#include "base/win/win_util.h"
+#endif
+
 namespace {
 
 void GetStubResolverConfig(
@@ -47,6 +51,10 @@ void GetStubResolverConfig(
 // various DoH modes and DoH template strings and makes sure the settings are
 // respected.
 void RunStubResolverConfigTests(bool async_dns_feature_enabled) {
+  // Mark as not enterprise managed.
+#if defined(OS_WIN)
+  base::win::ScopedDomainStateForTesting scoped_domain(false);
+#endif
   // Check initial state.
   bool insecure_stub_resolver_enabled = !async_dns_feature_enabled;
   net::DnsConfig::SecureDnsMode secure_dns_mode;
