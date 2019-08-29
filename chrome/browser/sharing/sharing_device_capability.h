6df146dd9674311a72c5c4336520c9881797d792
Rayan Kanso <rayankans@chromium.org>
2019-08-29 13:47:25 +0000

    [Sharing] Add a JNI method to query for device candidates.
    Bug: 997200
    Change-Id: If0de01ac867d143a02870d5856dd97b9f7e4c0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773256
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Commit-Queue: Alex Chau <alexchau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691609}

--- chrome/browser/sharing/sharing_device_capability.h
+++ chrome/browser/sharing/sharing_device_capability.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // adding a new capability, the value should be '1 << (NEXT_FREE_BIT_ID)' and
 // NEXT_FREE_BIT_ID should be incremented by one.
 // NEXT_FREE_BIT_ID: 2
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chrome.browser.sharing
 enum class SharingDeviceCapability {
   kNone = 0,
   kClickToCall = 1 << 0,
