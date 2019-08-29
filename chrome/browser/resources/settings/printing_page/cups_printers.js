e61d9bd9f475eb2cd3ec2729671df5e6b1b5a63d
Jimmy Gong <jimmyxgong@google.com>
2019-08-29 00:04:31 +0000

    Hide saved printers when empty
    - The header for saved printers will be hidden when the saved printers
      list is empty.
    - Adds a browsertest to reflect this change.
    
    Bug: 993819
    Test: browser tests, end to end manual
    Change-Id: I4bbae8f389161a7c3252df09d39380c896413fa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767488
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691391}

--- chrome/browser/resources/settings/printing_page/cups_printers.js
+++ chrome/browser/resources/settings/printing_page/cups_printers.js
@@ -15,6 +15,7 @@ Polymer({
 
   behaviors: [
       CrNetworkListenerBehavior,
+      settings.RouteObserverBehavior,
       WebUIListenerBehavior,
   ],
 
@@ -41,6 +42,15 @@ Polymer({
     canAddPrinter: {
       type: Boolean,
       reflectToAttribute: true,
+    },
+
+    /**
+     * @type {!Array<!PrinterListEntry>}
+     * @private
+     */
+    savedPrinters_: {
+      type: Array,
+      value: () => [],
     },
 
     /** @private */
@@ -94,14 +104,26 @@ Polymer({
     if (this.enableUpdatedUi_) {
       return;
     }
-
-    this.addWebUIListener(
-        'on-printers-changed', this.printersChanged_.bind(this));
   },
 
   /** @override */
   ready: function() {
     this.updateCupsPrintersList_();
+  },
+
+
+  /**
+   * settings.RouteObserverBehavior
+   * @param {!settings.Route} route
+   * @protected
+   */
+  currentRouteChanged: function(route) {
+    if (route != settings.routes.CUPS_PRINTERS) {
+      cr.removeWebUIListener('on-printers-changed');
+      return;
+    }
+    cr.addWebUIListener(
+        'on-printers-changed', this.onPrintersChanged_.bind(this));
   },
 
   /**
@@ -131,21 +153,13 @@ Polymer({
     const printerName = event.detail.printerName;
     switch (event.detail.resultCode) {
       case PrinterSetupResult.SUCCESS:
-        if (this.enableUpdatedUi_) {
-          this.$$('#savedPrinters').updateSavedPrintersList();
-        } else {
-          this.updateCupsPrintersList_();
-        }
+        this.updateCupsPrintersList_();
         this.addPrinterResultText_ =
             loadTimeData.getStringF('printerAddedSuccessfulMessage',
                                     printerName);
         break;
       case PrinterSetupResult.EDIT_SUCCESS:
-        if (this.enableUpdatedUi_) {
-          this.$$('#savedPrinters').updateSavedPrintersList();
-        } else {
-          this.updateCupsPrintersList_();
-        }
+        this.updateCupsPrintersList_();
         this.addPrinterResultText_ =
             loadTimeData.getStringF('printerEditedSuccessfulMessage',
                                     printerName);
@@ -177,15 +191,22 @@ Polymer({
   updateCupsPrintersList_: function() {
     settings.CupsPrintersBrowserProxyImpl.getInstance()
         .getCupsPrintersList()
-        .then(this.printersChanged_.bind(this));
+        .then(this.onPrintersChanged_.bind(this));
   },
 
   /**
    * @param {!CupsPrintersList} cupsPrintersList
    * @private
    */
-  printersChanged_: function(cupsPrintersList) {
-    this.printers = cupsPrintersList.printerList;
+  onPrintersChanged_: function(cupsPrintersList) {
+    if (this.enableUpdatedUi_) {
+      this.savedPrinters_ = cupsPrintersList.printerList.map(
+          printer => /** @type {!PrinterListEntry} */({
+              printerInfo: printer,
+              printerType: PrinterType.SAVED}));
+    } else {
+      this.printers = cupsPrintersList.printerList;
+    }
   },
 
   /** @private */
@@ -236,5 +257,13 @@ Polymer({
   addPrinterButtonActive_: function(
       connectedToNetwork, userNativePrintersAllowed) {
     return connectedToNetwork && userNativePrintersAllowed;
+  },
+
+  /**
+   * @return {boolean} Whether |savedPrinters_| is empty.
+   * @private
+   */
+  doesAccountHaveSavedPrinters_: function() {
+    return !!this.savedPrinters_.length;
   }
 });
