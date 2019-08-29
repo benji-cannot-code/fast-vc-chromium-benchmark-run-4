525bff2ff5ef758edf4df0c79092364c30e62692
Etienne Pierre-doray <etiennep@chromium.org>
2019-08-29 17:43:17 +0000

    Reland "Reland "[ThreadPool]: Implement Job Delegate's ShouldYield and concurrency increase.""
    This is a reland of 792309b6fabf5663fabaefdbb6920a2796f3ad5c
    
    ThreadGroupTest.JobTaskSourceConcurrencyIncrease was previously failing:
    The issue is caused by a race between EnsureEnoughWorkersLockRequired
    and DidProcessTask:
    - Worker 1 and worker 2 are running the job TaskSource.
    - Main: SetNumTasksToRun() sets |remaining_num_tasks_to_run_| to 4 and unblocks the callbacks running on worker 1 and worker 2.
    - Worker 1: The callback returns and |remaining_num_tasks_to_run_| is decremented by MockJobTask::Run(). The new value is 3.
    - Main: NotifyConcurrencyIncrease > PushTaskSourceAndWakeUpWorkers > EnsureEnoughWorkersLockRequired wakes up 1 worker instead of 2. From the point of view of EnsureEnoughWorkersLockRequired(), worker 1 is still running a task from the job TaskSource. 3 workers needed - 2 workers already running = 1 worker to wake up.
    - Worker 1: DidProcessTask() is called and tries to reenqueue the TaskSource, but it is already in the PriorityQueue (added by the main thread). It does not wake up any worker.
    - Worker 2: Finishes running the job callback. DidProcessTask() is called and tries to reenqueue the TaskSource, but it is already in the PriorityQueue (added by the main thread). It does not wake up any worker.
    - Worker 3: Wakes up, gets the job TaskSource from the PriorityQueue and runs it.
    - Worker 3: Finishes running the job callback. DidProcessTask() is called and tries to reenqueue the TaskSource, but it is already in the PriorityQueue (added by the main thread). It does not wake up any worker.
    
    No additional worker is woken up.
    On the native thread group, a worker runs only a single task so the 4th never runs and
    the test waits forever.
    
    This is fixed by calling EnsureEnoughWorkersLockRequired unconditionally
    in ThreadGroup::ReEnqueueTaskSource to avoid leaving a
    task source with no worker.
    
    Original change's description:
    > Reland "[ThreadPool]: Implement Job Delegate's ShouldYield and concurrency increase."
    >
    > This is a reland of 63fbbb9216d0e5ee288b0257596929b55dee823d
    >
    > Reason for reland:
    >  - Fixed with ScopedAllowBaseSyncPrimitivesOutsideBlockingScope.
    >
    > Original change's description:
    > > [ThreadPool]: Implement Job Delegate's ShouldYield and concurrency increase.
    > >
    > > This CL partially implement job's delegate, as well as concurrency
    > > usage assertion:
    > > - ShouldYield must be called in worker task.
    > > - max concurrency is expected to decrease unless
    > >   NotifyConcurrencyIncrease() is called.
    > >
    > > To implement ShouldYield, PooledTaskRunnerDelegate is used
    > > to communicate with ThreadPool and a racy priority is added to
    > > task source.
    > >
    > > Bug: 839091
    > > Change-Id: I3504f00ec48ab600f79b64e44151bc9dded408a8
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713146
    > > Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    > > Reviewed-by: François Doray <fdoray@chromium.org>
    > > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#690127}
    >
    > Bug: 839091
    > Change-Id: I6f36f27426351e4efbcbaf7e9356840c004e6630
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769502
    > Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#690437}
    
    Bug: 839091
    Change-Id: I043e6c129e0ebf4f80483a70045866402db776f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773323
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691703}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -280,6 +280,7 @@ class JavaHandlerThread;
 }
 
 namespace internal {
+class JobTaskSource;
 class TaskTracker;
 }
 
@@ -457,6 +458,7 @@ class BASE_EXPORT ScopedAllowBaseSyncPrimitivesOutsideBlockingScope {
   friend class audio::OutputDevice;
   friend class base::sequence_manager::internal::TaskQueueImpl;
   friend class base::FileDescriptorWatcher;
+  friend class base::internal::JobTaskSource;
   friend class base::MessageLoopImpl;
   friend class base::ScopedAllowThreadRecallForStackSamplingProfiler;
   friend class base::StackSamplingProfiler;
