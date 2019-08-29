d56b473e3eeeb6771d72408474b6fd51362b4616
Natalie Chouinard <chouinard@chromium.org>
2019-08-29 16:54:26 +0000

    Modern status bar color in Preferences
    Match Settings activity status bar to the modern status bar design used
    in the main activity.
    
    Status bar color is set programmatically for M+ devices. The default is
    set via XML only for O+ devices due to crbug.com/884144. A kill switch
    is included to disable programmatically setting the status bar color
    due to crbug.com/880694.
    
    Bug: 860848
    Change-Id: Icb966bd3655b986d82eddf19efb20e6fd89063ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1748123
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691674}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -167,6 +167,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kSearchEnginePromoNewDevice,
     &kServiceManagerForBackgroundPrefetch,
     &kServiceManagerForDownload,
+    &kSettingsModernStatusBar,
     &kShoppingAssist,
     &kSpannableInlineAutocomplete,
     &kSpecialLocaleWrapper,
@@ -522,6 +523,9 @@ const base::Feature kServiceManagerForBackgroundPrefetch{
 
 const base::Feature kServiceManagerForDownload{
     "ServiceManagerForDownload", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kSettingsModernStatusBar{"SettingsModernStatusBar",
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kShoppingAssist{"ShoppingAssist",
                                     base::FEATURE_DISABLED_BY_DEFAULT};
