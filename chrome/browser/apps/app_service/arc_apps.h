79a0814218e68fb3787f7d05ac33faf179e02c59
Nigel Tao <nigeltao@chromium.org>
2019-08-29 03:20:38 +0000

    Delete ArcApps' ARC_GET_INSTANCE_FOR_METHOD code
    The ARC_GET_INSTANCE_FOR_METHOD macro is really a private implementation
    detail of the ArcAppListPrefs class.
    
    Bug: 826982
    Change-Id: Ibc6a74a2ebb54282b36b361ad2ee561ca600dce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767468
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691465}

--- chrome/browser/apps/app_service/arc_apps.h
+++ chrome/browser/apps/app_service/arc_apps.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/icon_key_util.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/services/app_service/public/mojom/app_service.mojom.h"
-#include "components/arc/session/connection_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/interface_ptr_set.h"
@@ -34,12 +33,8 @@ class AppServiceProxy;
 // See chrome/services/app_service/README.md.
 class ArcApps : public KeyedService,
                 public apps::mojom::Publisher,
-                public arc::ConnectionObserver<arc::mojom::AppInstance>,
                 public ArcAppListPrefs::Observer {
  public:
-  using AppConnectionHolder =
-      arc::ConnectionHolder<arc::mojom::AppInstance, arc::mojom::AppHost>;
-
   static ArcApps* Get(Profile* profile);
 
   static ArcApps* CreateForTesting(Profile* profile,
@@ -72,9 +67,6 @@ class ArcApps : public KeyedService,
                      apps::mojom::PermissionPtr permission) override;
   void Uninstall(const std::string& app_id) override;
   void OpenNativeSettings(const std::string& app_id) override;
-
-  // arc::ConnectionObserver<arc::mojom::AppInstance> overrides.
-  void OnConnectionReady() override;
 
   // ArcAppListPrefs::Observer overrides.
   void OnAppRegistered(const std::string& app_id,
@@ -118,9 +110,6 @@ class ArcApps : public KeyedService,
 
   Profile* profile_;
   ArcIconOnceLoader arc_icon_once_loader_;
-
-  std::vector<base::OnceCallback<void(AppConnectionHolder*)>>
-      pending_load_icon_calls_;
 
   apps_util::IncrementingIconKeyFactory icon_key_factory_;
 
