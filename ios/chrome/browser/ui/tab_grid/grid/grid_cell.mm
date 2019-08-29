72eab23c318bdd67304e186a0742b5c068e9b299
Robbie Gibson <rkgibson@google.com>
2019-08-29 13:58:24 +0000

    [iOS][Dark Mode] Fix flickering with NTP snapshot animation
    The root cause was that the contentView of the grid cell was getting
    set to black, instead of the same as the top bar background color,
    causing a brief flicker to black as different views faded in and out.
    
    Also removes a now-unnecessary iOS 13 SDK compiler guard.
    
    Bug: 997165
    Change-Id: I56ed43b35aa3f61a22df5b0abf9c1398a8e513f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771905
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#691616}

--- ios/chrome/browser/ui/tab_grid/grid/grid_cell.mm
+++ ios/chrome/browser/ui/tab_grid/grid/grid_cell.mm
@@ -176,7 +176,6 @@ void PositionView(UIView* view, CGPoint point) {
     return;
 
   self.iconView.backgroundColor = UIColor.clearColor;
-  self.contentView.backgroundColor = UIColor.blackColor;
   switch (theme) {
     // This is necessary for iOS 13 because on iOS 13, this will return
     // the dynamic color (which will then be colored with the user
@@ -184,6 +183,7 @@ void PositionView(UIView* view, CGPoint point) {
     // On iOS 12, this will always return the dynamic color in the light
     // variant.
     case GridThemeLight:
+      self.contentView.backgroundColor = [UIColor colorNamed:kBackgroundColor];
       self.snapshotView.backgroundColor = [UIColor colorNamed:kBackgroundColor];
       self.topBar.backgroundColor = [UIColor colorNamed:kBackgroundColor];
       self.titleLabel.textColor = [UIColor colorNamed:kTextPrimaryColor];
@@ -194,6 +194,8 @@ void PositionView(UIView* view, CGPoint point) {
     // TODO (crbug.com/981889): The following lines will be removed
     // along with iOS 12
     case GridThemeDark:
+      self.contentView.backgroundColor =
+          [UIColor colorNamed:kBackgroundDarkColor];
       self.snapshotView.backgroundColor =
           [UIColor colorNamed:kBackgroundDarkColor];
       self.topBar.backgroundColor = [UIColor colorNamed:kBackgroundDarkColor];
@@ -202,7 +204,6 @@ void PositionView(UIView* view, CGPoint point) {
       break;
   }
 
-#if defined(__IPHONE_13_0) && (__IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_13_0)
   if (@available(iOS 13, *)) {
     // When iOS 12 is dropped, only the next line is needed for styling.
     // Every other check for |GridThemeDark| can be removed, as well as
@@ -211,7 +212,6 @@ void PositionView(UIView* view, CGPoint point) {
                                           ? UIUserInterfaceStyleDark
                                           : UIUserInterfaceStyleUnspecified;
   }
-#endif
 
   // When iOS 12 is dropped, only the next switch statement is needed for
   // styling.
