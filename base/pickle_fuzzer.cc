81ec6389b1d74fc81d347cf6ac7a405bfb7e6c1e
Oskar Sundbom <ossu@chromium.org>
2019-08-29 10:55:49 +0000

    Revert "[base] Use FuzzedDataProvider included in LLVM package."
    This reverts commit 20d2671d9bb37c0545a39be4364134df1c19d44a.
    
    Reason for revert: Causes build failures on iOS bots, see: https://crbug.com/999096
    
    Original change's description:
    > [base] Use FuzzedDataProvider included in LLVM package.
    > 
    > Bug: 991200
    > Change-Id: Iee88e3a81057d509e18a2875a3070c5abc17c959
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774303
    > Auto-Submit: Max Moroz <mmoroz@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#691207}
    
    TBR=thestig@chromium.org,mmoroz@chromium.org
    
    Change-Id: I044f8863f8fa7e2bb9e70b0f33e73fdd8bfa2a29
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 991200, 999096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776043
    Reviewed-by: Oskar Sundbom <ossu@chromium.org>
    Commit-Queue: Oskar Sundbom <ossu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691575}

--- base/pickle_fuzzer.cc
+++ base/pickle_fuzzer.cc
@@ -3,10 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <fuzzer/FuzzedDataProvider.h>
-
 #include "base/macros.h"
 #include "base/pickle.h"
+#include "third_party/libFuzzer/src/utils/FuzzedDataProvider.h"
 
 namespace {
 constexpr int kIterations = 16;
