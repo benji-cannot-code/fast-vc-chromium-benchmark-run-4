666f3067125f5aa850c4edf7cd8557a4dea1895f
Boris Sazonov <bsazonov@chromium.org>
2019-08-29 08:11:29 +0000

    [Android] Fix PassphraseActivity backstack check and base class
    PassphraseActivity was broken by https://crrev.com/c/1708895, as it was
    checking for backstack entry count in the framework FragmentManager,
    while fragments were added to FragmentManager from the support library.
    This is fixed by using getSupportFragmentManager.
    A second unrelated fix is a change of the base class from
    FragmentActivity to AppCompatActivity. All Chrome activities should
    inherit AppCompatActivity to make sure proper view classes from support
    library are inflated.
    
    Bug: 998412
    Change-Id: I286544233c18f4bf0289cee7e3c7aa2618a76b54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768350
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691525}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/ui/PassphraseActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/ui/PassphraseActivity.java
@@ -10,9 +10,9 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import org.chromium.base.library_loader.ProcessInitException;
@@ -27,10 +27,8 @@ import org.chromium.components.signin.ChromeSigninController;
  * This activity is used for requesting a sync passphrase from the user. Typically,
  * this will be the target of an Android notification.
  */
-public class PassphraseActivity extends FragmentActivity implements
-        PassphraseDialogFragment.Listener,
-        FragmentManager.OnBackStackChangedListener {
-
+public class PassphraseActivity extends AppCompatActivity
+        implements PassphraseDialogFragment.Listener, FragmentManager.OnBackStackChangedListener {
     public static final String FRAGMENT_PASSPHRASE = "passphrase_fragment";
     public static final String FRAGMENT_SPINNER = "spinner_fragment";
     private static final String TAG = "PassphraseActivity";
@@ -145,7 +143,7 @@ public class PassphraseActivity extends FragmentActivity implements
 
     @Override
     public void onBackStackChanged() {
-        if (getFragmentManager().getBackStackEntryCount() == 0) {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 0) {
             finish();
         }
     }
