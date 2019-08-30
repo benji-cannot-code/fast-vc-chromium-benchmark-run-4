4141c27755ffaa6751ae57ddf3625f57c899dc5c
Jun Mukai <mukai@chromium.org>
2019-08-30 23:17:38 +0000

    Add tablet-mode case of LauncherDragTest
    This CL adds the home-launcher state changes in ShellTestApi, and
    so LauncherDragTest can conduct within tablet mode.
    
    As far as I tested, the input latency for dragging in tablet mode is
    not quite bad. It is 50-60msecs in scarlet; that is comparable to
    the latency of clamshell mode in nocturne.
    
    Bug: 995991
    Test: interactive_ui_tests
    Change-Id: Ifd7d2694bdcf5a60eb295d11773df1eb73ec181b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1775598
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692271}

--- chrome/browser/ui/ash/launcher_drag_interactive_uitest.cc
+++ chrome/browser/ui/ash/launcher_drag_interactive_uitest.cc
@@ -22,16 +22,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Test launcher drag performance in clamshell mode.
 // TODO(oshima): Add test for tablet mode.
-class LauncherDragTest : public UIPerformanceTest {
+class LauncherDragTest : public UIPerformanceTest,
+                         public ::testing::WithParamInterface<bool> {
  public:
   LauncherDragTest() = default;
   ~LauncherDragTest() override = default;
 
   // UIPerformanceTest:
   void SetUpOnMainThread() override {
+    tablet_mode_ = GetParam();
     UIPerformanceTest::SetUpOnMainThread();
 
     test::PopulateDummyAppListItems(100);
+    if (tablet_mode_)
+      ash::ShellTestApi().SetTabletModeEnabledForTest(true);
     // Ash may not be ready to receive events right away.
     int warmup_seconds = base::SysInfo::IsRunningOnChromeOS() ? 5 : 1;
     base::RunLoop run_loop;
@@ -43,7 +47,8 @@ class LauncherDragTest : public UIPerformanceTest {
   // UIPerformanceTest:
   std::vector<std::string> GetUMAHistogramNames() const override {
     return {
-        "Apps.StateTransition.Drag.PresentationTime.ClamshellMode",
+        base::StringPrintf("Apps.StateTransition.Drag.PresentationTime.%s",
+                           tablet_mode_ ? "TabletMode" : "ClamshellMode"),
     };
   }
 
@@ -54,11 +59,13 @@ class LauncherDragTest : public UIPerformanceTest {
   }
 
  private:
+  bool tablet_mode_ = false;
+
   DISALLOW_COPY_AND_ASSIGN(LauncherDragTest);
 };
 
 // Drag to open the launcher from shelf.
-IN_PROC_BROWSER_TEST_F(LauncherDragTest, Open) {
+IN_PROC_BROWSER_TEST_P(LauncherDragTest, Open) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   aura::Window* browser_window = browser_view->GetWidget()->GetNativeWindow();
   ash::ShellTestApi shell_test_api;
@@ -80,7 +87,7 @@ IN_PROC_BROWSER_TEST_F(LauncherDragTest, Open) {
 }
 
 // Drag to close the launcher.
-IN_PROC_BROWSER_TEST_F(LauncherDragTest, Close) {
+IN_PROC_BROWSER_TEST_P(LauncherDragTest, Close) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   aura::Window* browser_window = browser_view->GetWidget()->GetNativeWindow();
   ash::ShellTestApi shell_test_api;
@@ -105,3 +112,7 @@ IN_PROC_BROWSER_TEST_F(LauncherDragTest, Close) {
 
   shell_test_api.WaitForLauncherAnimationState(ash::AppListViewState::kClosed);
 }
+
+INSTANTIATE_TEST_SUITE_P(,
+                         LauncherDragTest,
+                         /*tablet_mode=*/::testing::Bool());
