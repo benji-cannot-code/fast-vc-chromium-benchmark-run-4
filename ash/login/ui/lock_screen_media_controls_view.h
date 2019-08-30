4583dabb8ed4da0ba6b301a7ed52e6d34ff7c737
Becca Hughes <beccahughes@chromium.org>
2019-08-30 22:54:49 +0000

    [Lock screen media controls] Make button ink drop rounded
    As per UI review, make the action button ink
    drop round. We do this by subclassing image button
    and returning a custom ink drop.
    
    BUG=999708
    
    Change-Id: Ib7067cb4a27b9a01c01eb397c80e665a23027224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778652
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692264}

--- ash/login/ui/lock_screen_media_controls_view.h
+++ ash/login/ui/lock_screen_media_controls_view.h
@@ -23,7 +23,6 @@ class Connector;
 namespace views {
 class Label;
 class ImageView;
-class ToggleImageButton;
 }  // namespace views
 
 namespace media_message_center {
@@ -31,6 +30,10 @@ class MediaControlsProgressView;
 }
 
 namespace ash {
+
+namespace {
+class MediaActionButton;
+}  // namespace
 
 class MediaControlsHeaderView;
 class NonAccessibleView;
@@ -168,12 +171,6 @@ class ASH_EXPORT LockScreenMediaControlsView
   // Returns the rounded rectangle clip path for the current artwork.
   SkPath GetArtworkClipPath() const;
 
-  // Creates and adds a new media button to |button_row_|. This should not be
-  // used to create toggle buttons such as play/pause.
-  void CreateMediaButton(int size,
-                         media_session::mojom::MediaSessionAction action,
-                         const base::string16& accessible_name);
-
   // Updates the visibility of buttons on |button_row_| depending on what is
   // available in the current media session.
   void UpdateActionButtonsVisibility();
@@ -252,7 +249,7 @@ class ASH_EXPORT LockScreenMediaControlsView
   views::Label* title_label_ = nullptr;
   views::Label* artist_label_ = nullptr;
   NonAccessibleView* button_row_ = nullptr;
-  views::ToggleImageButton* play_pause_button_ = nullptr;
+  MediaActionButton* play_pause_button_ = nullptr;
   media_message_center::MediaControlsProgressView* progress_ = nullptr;
 
   // Callbacks.
