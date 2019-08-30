278ebd0fc2b58238fc241ff8b0ed19cce6e85e2e
Ryan Powell <ryanpow@google.com>
2019-08-30 16:33:18 +0000

    [PM] Add function CallOnGraphAndReplyWithResult to performance manager.
    Adds a function, CallOnGraphAndReplyWithResult, to the performance
    manager api. This function allows you to post a callback that will be
    run on the PM sequence, and be provided a pointer to the Graph. The
    return value of that callback is passed back as an argument to the reply
    callback.
    
    Bug: 980965
    Change-Id: Icf1abc0d738bd1edd8ff2f50d5dba824fa861853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773961
    Commit-Queue: Ryan Powell <ryanpow@google.com>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692088}

--- chrome/browser/performance_manager/performance_manager.h
+++ chrome/browser/performance_manager/performance_manager.h
@@ -12,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/callback_helpers.h"
 #include "base/location.h"
 #include "base/sequence_checker.h"
 #include "base/sequenced_task_runner.h"
+#include "base/task_runner_util.h"
 #include "chrome/browser/performance_manager/graph/graph_impl.h"
 #include "chrome/browser/performance_manager/public/graph/worker_node.h"
 #include "chrome/browser/performance_manager/public/render_process_host_proxy.h"
@@ -57,6 +59,16 @@ class PerformanceManager {
   using GraphCallback = base::OnceCallback<void(GraphImpl*)>;
   static void CallOnGraph(const base::Location& from_here,
                           GraphCallback graph_callback);
+
+  // Posts a callback that will run on the PM sequence, and be provided a
+  // pointer to the Graph. Valid to be called from the main thread only, and
+  // only if "IsAvailable" returns true. The return value is returned as an
+  // argument to the reply callback.
+  template <typename TaskReturnType>
+  void CallOnGraphAndReplyWithResult(
+      const base::Location& from_here,
+      base::OnceCallback<TaskReturnType(GraphImpl*)> task,
+      base::OnceCallback<void(TaskReturnType)> reply);
 
   // Passes a GraphOwned object into the Graph on the PM sequence. Should only
   // be called from the main thread and only if "IsAvailable" returns true.
@@ -151,6 +163,10 @@ class PerformanceManager {
   void OnStartImpl(std::unique_ptr<service_manager::Connector> connector);
   void CallOnGraphImpl(GraphCallback graph_callback);
 
+  template <typename TaskReturnType>
+  TaskReturnType CallOnGraphAndReplyWithResultImpl(
+      base::OnceCallback<TaskReturnType(GraphImpl*)> task);
+
   // The performance task runner.
   const scoped_refptr<base::SequencedTaskRunner> task_runner_;
   GraphImpl graph_;
@@ -158,7 +174,6 @@ class PerformanceManager {
   // Provided to |graph_|.
   // TODO(siggi): This no longer needs to go through mojo.
   std::unique_ptr<ukm::MojoUkmRecorder> ukm_recorder_;
-
 
   SEQUENCE_CHECKER(sequence_checker_);
 
@@ -168,6 +183,26 @@ class PerformanceManager {
 template <typename NodeType>
 void PerformanceManager::DeleteNode(std::unique_ptr<NodeType> node) {
   PostDeleteNode(std::move(node));
+}
+
+template <typename TaskReturnType>
+void PerformanceManager::CallOnGraphAndReplyWithResult(
+    const base::Location& from_here,
+    base::OnceCallback<TaskReturnType(GraphImpl*)> task,
+    base::OnceCallback<void(TaskReturnType)> reply) {
+  auto* pm = GetInstance();
+  base::PostTaskAndReplyWithResult(
+      pm->task_runner_.get(), from_here,
+      base::BindOnce(&PerformanceManager::CallOnGraphAndReplyWithResultImpl<
+                         TaskReturnType>,
+                     base::Unretained(pm), std::move(task)),
+      std::move(reply));
+}
+
+template <typename TaskReturnType>
+TaskReturnType PerformanceManager::CallOnGraphAndReplyWithResultImpl(
+    base::OnceCallback<TaskReturnType(GraphImpl*)> task) {
+  return std::move(task).Run(&graph_);
 }
 
 }  // namespace performance_manager
