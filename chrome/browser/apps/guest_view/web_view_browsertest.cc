70cb5eb06f6dd81d88522b5cc8a25a9114e30bca
Kevin McNee <mcnee@chromium.org>
2019-08-30 19:47:46 +0000

    Devtools: Don't auto attach guest views when auto attaching portals
    TargetAutoAttacher::UpdatePortals currently adds all inner WebContents
    including guest views. When devtools closes, SetAutoAttach clears
    |auto_attached_hosts_| of any portals, but leaves any guest views which
    leads to the failed DCHECK that all auto attached hosts have been
    removed.
    
    Now in UpdatePortals, we check that the inner contents is a portal
    before adding it.
    
    Bug: 961113
    Change-Id: I758b3818c3f390f47c4020d88094387ee85d07e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1777022
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692179}

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/platform_apps/app_browsertest_util.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/devtools/devtools_window_testing.h"
 #include "chrome/browser/download/download_core_service.h"
 #include "chrome/browser/download/download_core_service_factory.h"
 #include "chrome/browser/download/download_history.h"
@@ -4424,4 +4425,14 @@ IN_PROC_BROWSER_TEST_F(WebViewTest, TouchpadPinchSyntheticWheelEvents) {
                                         blink::WebGestureDevice::kTouchpad);
 
   ASSERT_TRUE(synthetic_wheel_listener.WaitUntilSatisfied());
+}
+
+// Tests that we can open and close a devtools window that inspects a contents
+// containing a guest view without crashing.
+IN_PROC_BROWSER_TEST_F(WebViewTest, OpenAndCloseDevTools) {
+  LoadAppWithGuest("web_view/simple");
+  content::WebContents* embedder = GetEmbedderWebContents();
+  DevToolsWindow* devtools = DevToolsWindowTesting::OpenDevToolsWindowSync(
+      embedder, false /* is_docked */);
+  DevToolsWindowTesting::CloseDevToolsWindowSync(devtools);
 }
