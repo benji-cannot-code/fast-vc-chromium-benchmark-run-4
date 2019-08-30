4c91107f1ed13e9baf0c3e7acbc53abe6e1a8b5a
Takashi Sakamoto <tasak@google.com>
2019-08-30 04:19:13 +0000

    Revert "[Fuchsia] Handle VULKAN feature flag in WebEngine"
    This reverts commit 74c69a759fdc688ffa8151228e2dff91bbb85c02.
    
    Reason for revert: causing first build failure on Deterministic Fuchsia (dbg).
    
    Sample build: https://ci.chromium.org/p/chromium/builders/ci/Deterministic%20Fuchsia%20%28dbg%29/561
    
    Sample log: https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8903731569632286240/+/steps/First_build/0/stdout?format=raw
    
    [47036/47333] SOLINK ./lib/libweb_engine_core.so
    ...
    ld.lld: error: undefined symbol: switches::kEnableFeatures
    >>> referenced by context_provider_impl.cc:208 (../../fuchsia/engine/context_provider_impl.cc:208)
    >>>               obj/fuchsia/engine/web_engine_core/context_provider_impl.o:(ContextProviderImpl::Create(fuchsia::web::CreateContextParams, fidl::InterfaceRequest<fuchsia::web::Context>))
    
    ld.lld: error: undefined symbol: gl::kGLImplementationStubName
    >>> referenced by context_provider_impl.cc:212 (../../fuchsia/engine/context_provider_impl.cc:212)
    >>>               obj/fuchsia/engine/web_engine_core/context_provider_impl.o:(ContextProviderImpl::Create(fuchsia::web::CreateContextParams, fidl::InterfaceRequest<fuchsia::web::Context>))
    clang: error: ld.lld command failed with exit code 1 (use -v to see invocation)
    [47037/47333] AR obj/components/password_manager/core/browser/libbrowser.a
    [47038/47333] AR obj/components/sync_sessions/libsync_sessions.a
    
    
    Original change's description:
    > [Fuchsia] Handle VULKAN feature flag in WebEngine
    > 
    > Now WebEngine will enable Vulkan only when ContextFeatureFlags::VULKAN
    > is specified for web::Context. Also Context process now explicitly
    > disables hardware-accelerated compositing when VULKAN is not enabled
    > (otherwise it restarts the GPU process several times before it falls
    > back to software compositing).
    > 
    > Bug: 962617, fuchsia:35009
    > Change-Id: I74a463a1514df99134d929981fe44151c1d9dd17
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774967
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Jonathan Backer <backer@chromium.org>
    > Reviewed-by: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#691856}
    
    TBR=backer@chromium.org,wez@chromium.org,sergeyu@chromium.org
    
    Change-Id: I71d9bb87b79928bbffa9f123408b4e4bafad31bc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 962617, fuchsia:35009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1775955
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#691941}

--- fuchsia/engine/context_provider_impl.cc
+++ fuchsia/engine/context_provider_impl.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base_paths_fuchsia.h"
-#include "base/base_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/files/scoped_file.h"
@@ -34,13 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
-#include "components/viz/common/features.h"
 #include "content/public/common/content_switches.h"
 #include "fuchsia/engine/common.h"
-#include "gpu/command_buffer/service/gpu_switches.h"
 #include "net/http/http_util.h"
 #include "services/service_manager/sandbox/fuchsia/sandbox_policy_fuchsia.h"
-#include "ui/gl/gl_switches.h"
 
 namespace {
 
@@ -197,21 +193,14 @@ void ContextProviderImpl::Create(
                                       base::JoinString(handles_ids, ","));
   }
 
-  fuchsia::web::ContextFeatureFlags features = {};
-  if (params.has_features())
-    features = params.features();
-
-  bool enable_vulkan = (features & fuchsia::web::ContextFeatureFlags::VULKAN) ==
-                       fuchsia::web::ContextFeatureFlags::VULKAN;
-
-  if (enable_vulkan) {
-    launch_command.AppendSwitch(switches::kUseVulkan);
-    launch_command.AppendSwitchASCII(switches::kEnableFeatures,
-                                     features::kUseSkiaRenderer.name);
-    launch_command.AppendSwitch(switches::kEnableOopRasterization);
-    launch_command.AppendSwitchASCII(switches::kUseGL,
-                                     gl::kGLImplementationStubName);
-  }
+#if defined(WEB_ENGINE_ENABLE_VULKAN)
+  // TODO(fbx/35009): Add a flag in CreateContextParams to enable/disable Vulkan
+  // and use it here.
+  launch_command.AppendSwitchASCII(
+      "--enable-features", "DefaultEnableOopRasterization,UseSkiaRenderer");
+  launch_command.AppendSwitch("--use-vulkan");
+  launch_command.AppendSwitchASCII("--use-gl", "stub");
+#endif  // WEB_ENGINE_ENABLE_VULKAN
 
   // Validate embedder-supplied product, and optional version, and pass it to
   // the Context to include in the UserAgent.
