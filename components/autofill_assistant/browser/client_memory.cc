01446050c3d1944116a4a9eebf7741aaed6421fe
Mathias Carlen <mcarlen@chromium.org>
2019-08-30 09:51:20 +0000

    [Autofill Assistant] Unit tests ShowDetailsAction.
    Smoke tests for the ShowDetails action. This patch only tests the
    structural integrity of setting up the ShowDetails action.
    
    The meat of unit testing will be in an upcoming patch around the content
    of details.{h,cc}.
    
    R=arbesser@google.com
    
    Bug: 806868
    Bug: b/140018932
    Change-Id: I7471fe0c54348bb22a5fa4650c1c7f83edd08507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768327
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#691987}

--- components/autofill_assistant/browser/client_memory.cc
+++ components/autofill_assistant/browser/client_memory.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill_assistant {
 
-ClientMemory::ClientMemory() = default;
-ClientMemory::~ClientMemory() = default;
+ClientMemory::ClientMemory() {}
+ClientMemory::~ClientMemory() {}
 
 const autofill::CreditCard* ClientMemory::selected_card() const {
   if (selected_card_.has_value())
