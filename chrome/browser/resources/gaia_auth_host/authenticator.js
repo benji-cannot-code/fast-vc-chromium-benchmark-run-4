43949c6a89082f148004601b33102cab2c8cd8b2
Roman Sorokin <rsorokin@chromium.org>
2019-08-30 13:27:45 +0000

    Chrome OS OOBE: Use Gaia native navigation
    Bug: 954572
    Change-Id: Ic682f73b52a24c1acb0e40df200e0724160d2e82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774447
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692033}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -597,6 +597,9 @@ cr.define('cr.login', function() {
       if (data.ignoreCrOSIdpSetting === true) {
         url = appendParam(url, 'ignoreCrOSIdpSetting', 'true');
       }
+      if (data.enableGaiaActionButtons) {
+        url = appendParam(url, 'use_native_navigation', 1);
+      }
       return url;
     }
 
