546a7e9a25d19b51e86b47c0140add48c722b5a4
John Delaney <johnidel@chromium.org>
2019-08-30 21:20:02 +0000

    Add peakwindowed usage as heavy ad criteria
    This change adds a threshold for peak windowed cpu usage to the
    heavy ad thresholds. Relevant histograms are versioned.
    
    Bug: 989141
    Change-Id: I71d1064cb809ca3adafb1dd576e7d2a6363036cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769514
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692226}

--- chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer_browsertest.cc
@@ -70,6 +70,9 @@ const char kSmallestDimensionHistogramId[] =
 const char kPeakWindowdPercentHistogramId[] =
     "PageLoad.Clients.Ads.Cpu.FullPage.PeakWindowedPercent";
 
+const char kHeavyAdInterventionTypeHistogramId[] =
+    "PageLoad.Clients.Ads.HeavyAds.InterventionType2";
+
 const char kHttpOkResponseHeader[] =
     "HTTP/1.1 200 OK\r\n"
     "Content-Type: text/html; charset=utf-8\r\n"
@@ -900,9 +903,8 @@ IN_PROC_BROWSER_TEST_F(AdsPageLoadMetricsObserverResourceBrowserTest,
       heavy_ad_thresholds::kMaxNetworkBytes);
   waiter->Wait();
 
-  histogram_tester.ExpectUniqueSample(
-      "PageLoad.Clients.Ads.HeavyAds.InterventionType",
-      FrameData::HeavyAdStatus::kNetwork, 1);
+  histogram_tester.ExpectUniqueSample(kHeavyAdInterventionTypeHistogramId,
+                                      FrameData::HeavyAdStatus::kNetwork, 1);
 
   // Wait for the intervention page navigation to finish on the frame.
   same_tab_observer.Wait();
@@ -943,8 +945,7 @@ IN_PROC_BROWSER_TEST_F(AdsPageLoadMetricsObserverResourceBrowserTest,
   // We can't check whether the navigation didn't occur because the error page
   // load is not synchronous. Instead check that we didn't log intervention UMA
   // that is always recorded when the intervention occurs.
-  histogram_tester.ExpectTotalCount(
-      "PageLoad.Clients.Ads.HeavyAds.InterventionType", 0);
+  histogram_tester.ExpectTotalCount(kHeavyAdInterventionTypeHistogramId, 0);
 }
 
 // Check that we don't activate a HeavyAdIntervention field trial if we don't
@@ -973,8 +974,7 @@ IN_PROC_BROWSER_TEST_F(AdsPageLoadMetricsObserverResourceBrowserTest,
       heavy_ad_thresholds::kMaxNetworkBytes / 2);
   waiter->Wait();
 
-  histogram_tester.ExpectTotalCount(
-      "PageLoad.Clients.Ads.HeavyAds.InterventionType", 0);
+  histogram_tester.ExpectTotalCount(kHeavyAdInterventionTypeHistogramId, 0);
 
   // Verify that the trial is not activated if no heavy ads are seen.
   EXPECT_FALSE(base::FieldTrialList::IsTrialActive(
@@ -1020,9 +1020,8 @@ IN_PROC_BROWSER_TEST_F(AdsPageLoadMetricsObserverResourceBrowserTest,
       heavy_ad_thresholds::kMaxNetworkBytes);
   waiter->Wait();
 
-  histogram_tester.ExpectUniqueSample(
-      "PageLoad.Clients.Ads.HeavyAds.InterventionType",
-      FrameData::HeavyAdStatus::kNetwork, 1);
+  histogram_tester.ExpectUniqueSample(kHeavyAdInterventionTypeHistogramId,
+                                      FrameData::HeavyAdStatus::kNetwork, 1);
 
   // Wait for the intervention page navigation to finish on the frame.
   first_frame_observer.Wait();
@@ -1044,9 +1043,8 @@ IN_PROC_BROWSER_TEST_F(AdsPageLoadMetricsObserverResourceBrowserTest,
   waiter->Wait();
 
   // Check that the intervention did not trigger on this frame.
-  histogram_tester.ExpectUniqueSample(
-      "PageLoad.Clients.Ads.HeavyAds.InterventionType",
-      FrameData::HeavyAdStatus::kNetwork, 1);
+  histogram_tester.ExpectUniqueSample(kHeavyAdInterventionTypeHistogramId,
+                                      FrameData::HeavyAdStatus::kNetwork, 1);
 }
 
 // Verify that UKM metrics are recorded correctly.
