2bacea86156019db72a10b0169fccf5ac27fb3ed
rbpotter <rbpotter@chromium.org>
2019-08-30 19:04:22 +0000

    WebUI Polymer 3: Port cr-drawer and tests
    Autogenerate Polymer 3 version of cr-drawer, as well as tests for the
    Polymer 3 version. Update the demo page to include a drawer.
    
    Bug: 965770
    Change-Id: I6df30fdcbe819a94ad1745425619e8df7a5d1b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776610
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692156}

--- chrome/browser/resources/print_preview/polymer3/demo.js
+++ chrome/browser/resources/print_preview/polymer3/demo.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
+import 'chrome://resources/cr_elements/cr_drawer/cr_drawer.m.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 import 'chrome://resources/cr_elements/cr_input/cr_input.m.js';
 import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.m.js';
@@ -60,6 +61,13 @@ class HelloPolymer3Element extends PolymerElement {
           <cr-radio-button name="buttons">buttons</cr-radio-button>
         </cr-radio-group>
       </div>
+
+      <div>
+        <cr-button on-click="showDrawer_">Show drawer</cr-button>
+        <cr-drawer heading="Drawer">
+          <div class="drawer-content">Content of drawer</div>
+        </cr-drawer>
+      </div>
     `;
   }
 
@@ -76,6 +84,11 @@ class HelloPolymer3Element extends PolymerElement {
   /** @private */
   onClick_() {
     this.shadowRoot.querySelector('cr-toast').show(2000);
+  }
+
+  /** @private */
+  showDrawer_() {
+    this.shadowRoot.querySelector('cr-drawer').openDrawer();
   }
 }  // class HelloPolymer3
 
