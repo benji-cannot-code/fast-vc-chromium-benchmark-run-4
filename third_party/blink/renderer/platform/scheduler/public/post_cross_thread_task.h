bc744c7da0f2cc191a0c7c5164f346b7cca4c638
Antonio Gomes <tonikitoo@igalia.com>
2019-08-30 20:09:01 +0000

    Add a return value to PostCrossThreadTask()
    PostCrossThreadTask() (and FWIW PostDelayedCrossThreadTask too) are
    thin wrapper to //base's equivalent in //base/task_runner.h.
    The goal is to ensure CrossThreadXXX is used when binding the functors.
    
    However, the wrappers miss the boolean return type of their //base equivalent.
    The limination makes switching some code from using base::Bind
    to use WTF::Bind if not impossible, at least not straightforward.
    
    eg.:
    
    484 bool RTCVideoDecoderAdapter::ReinitializeSync(
    485     const media::VideoDecoderConfig& config) {
    486   DCHECK_CALLED_ON_VALID_SEQUENCE(decoding_sequence_checker_);
    487
          (...)
    499   if (media_task_runner_->PostTask(
    500           FROM_HERE, base::BindOnce(&RTCVideoDecoderAdapter::FlushOnMediaThread,
    501                                     weak_this_, std::move(flush_success_cb),
    502                                     std::move(flush_fail_cb)))) {
    503     waiter.Wait();
    504   }
    
    ... in //third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc.
    
    This CL pairs up both wrapper and wrapped behavior, so it unblocks the
    migration.
    
    BUG=787254
    R=haraken@chromium.org
    
    Change-Id: I518bdd03496f58ffcd36906e19dfc5c4112a493d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778663
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692194}

--- third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h
+++ third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h
@@ -15,18 +15,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // For cross-thread posting. Can be called from any thread.
-inline void PostCrossThreadTask(base::SequencedTaskRunner& task_runner,
+inline bool PostCrossThreadTask(base::SequencedTaskRunner& task_runner,
                                 const base::Location& location,
                                 WTF::CrossThreadOnceClosure task) {
-  task_runner.PostDelayedTask(
+  return task_runner.PostDelayedTask(
       location, ConvertToBaseOnceCallback(std::move(task)), base::TimeDelta());
 }
 
-inline void PostDelayedCrossThreadTask(base::SequencedTaskRunner& task_runner,
+inline bool PostDelayedCrossThreadTask(base::SequencedTaskRunner& task_runner,
                                        const base::Location& location,
                                        WTF::CrossThreadOnceClosure task,
                                        base::TimeDelta delay) {
-  task_runner.PostDelayedTask(
+  return task_runner.PostDelayedTask(
       location, ConvertToBaseOnceCallback(std::move(task)), delay);
 }
 
