6f6136ce326cb32b65cf6c09f0f1f75bb444e7e7
Xida Chen <xidachen@chromium.org>
2019-08-30 16:53:24 +0000

    Fix a bug in FrameSequenceTrackerCollection::NotifyFramePresented
    Previously in this small refactor CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/1769025,
    we introduced a bug that StopSequence moves a tracker to the list of
    |removal_tracker_|, and that NotifyFramePresented will never apply to
    that list.
    
    This CL fixes the bug and added unit testing.
    
    Bug: None
    Change-Id: I37a39994b26c5627e5fc1add9d9688cbf69184a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774313
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692095}

--- cc/metrics/frame_sequence_tracker.h
+++ cc/metrics/frame_sequence_tracker.h
@@ -90,6 +90,8 @@ class CC_EXPORT FrameSequenceTrackerCollection {
   FrameSequenceTracker* GetTrackerForTesting(FrameSequenceTrackerType type);
 
  private:
+  friend class FrameSequenceTrackerTest;
+
   // The callsite can use the type to manipulate the tracker.
   base::flat_map<FrameSequenceTrackerType,
                  std::unique_ptr<FrameSequenceTracker>>
@@ -161,6 +163,7 @@ class CC_EXPORT FrameSequenceTracker {
 
  private:
   friend class FrameSequenceTrackerCollection;
+  friend class FrameSequenceTrackerTest;
 
   explicit FrameSequenceTracker(FrameSequenceTrackerType type);
 
