66fb6ece0aaa6637b8c08e53219b71513da9f8e3
Ilya Bizyaev <ilyabiz@google.com>
2019-08-30 14:33:24 +0000

    Ansible package management playbook generation
    Implement Ansible playbook generation for APT package management
    in the Crostini container. The code is not wired up with preferences
    or policies at this point and accepts strings of JSON input.
    
    This code will be used in a handler for an experimental enterprise
    policy on Chrome OS.
    
    Bug: 993321
    Change-Id: Ifb07f787a1daf6b2829a8cce73e30b990e25116e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1751253
    Commit-Queue: Ilya Bizyaev <ilyabiz@google.com>
    Reviewed-by: Olya Kalitova <okalitova@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/master@{#692047}

---
+++ chrome/browser/chromeos/crostini/crostini_ansible_management_util.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_CROSTINI_CROSTINI_ANSIBLE_MANAGEMENT_UTIL_H_
+#define CHROME_BROWSER_CHROMEOS_CROSTINI_CROSTINI_ANSIBLE_MANAGEMENT_UTIL_H_
+
+#include <string>
+
+#include "base/optional.h"
+
+namespace crostini {
+
+// Based on previous and current software configurations in JSON format,
+// generate a playbook that applies requested changes.
+// Convenience wrapper for AnsiblePendingChanges::ToAnsiblePlaybook().
+base::Optional<std::string> GeneratePlaybookFromConfig(
+    const std::string& new_config_json,
+    const std::string& old_config_json = "");
+
+}  // namespace crostini
+
+#endif  // CHROME_BROWSER_CHROMEOS_CROSTINI_CROSTINI_ANSIBLE_MANAGEMENT_UTIL_H_
