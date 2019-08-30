31e168634ecc9491481099190210d57923cd7274
Robert Ogden <robertogden@chromium.org>
2019-08-30 22:36:17 +0000

    Add experiment control of ignoring the Bloom Filter
    Also will end up ignoring regexps, oh well.
    
    Bug: 999702
    Change-Id: Id7e961b1ff9ac8948a2cd53f01e58164c0a79c7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1779178
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692260}

--- components/previews/content/previews_optimization_guide_decider.cc
+++ components/previews/content/previews_optimization_guide_decider.cc
@@ -110,8 +110,7 @@ bool PreviewsOptimizationGuideDecider::CanApplyPreview(
     PreviewsType type) {
   // See if we need to bypass the lite page redirect blacklist.
   if (type == PreviewsType::LITE_PAGE_REDIRECT &&
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kIgnoreLitePageRedirectOptimizationBlacklist)) {
+      params::LitePageRedirectPreviewIgnoresOptimizationGuideFilter()) {
     return true;
   }
 
