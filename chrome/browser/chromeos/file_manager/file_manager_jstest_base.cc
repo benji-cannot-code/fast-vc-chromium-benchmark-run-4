8595ea25e3861a08ad937fd52eeb5a3be4051946
Luciano Pacheco <lucmult@chromium.org>
2019-08-30 07:15:53 +0000

    Files app: Auto generate JS unittest HTML with deps using chrome://file_manager_test/
    Add js_test_gen_html build template and script that works similarly
    to js_unit_tests, which generates a HTML file with all JS dependencies
    to be able to run the JS unit tests.
    
    The new js_test_gen_html has 3 different behaviours from
    js_unit_tests:
    
    1. Point scripts to chrome:// URLs instead of using file system files
    and directories. The files are served from either regular
    chrome://resources or from the test-only chrome://file_manager_test/.
    This allows to run tests for Polymer elements that load JS/HTML from
    chrome:// URLs.
    
    2. Takes care of special cases where file paths aren't directly
    served from chrome://resources e.g.: jstemplate and some Polymer
    pahts.
    
    3. Add the "html_import" boolean config to allow to use HTML Imports
    instead of <script> for unittesting Polymer elements.
    
    Change TestFilesDataSource (which backs chrome://file_manager_test/)
    to serve:
    
    1. From //src/ and from //$OUT/gen/. From //$OUT/gen is to be able to
    serve the generated HTML file, all other files are served from
    //src/.
    
    2. Serve both roots //src/ and //$OUT/gen/ instead of narrowing down
    only to ./ui/file_manager/, this makes simpler to keep the difference
    from previous path style for all apps (Files, Gallery, etc).
    
    Convert all unittests from //ui/file_manager/file_manager/ to use
    js_test_gen_html instead of js_unit_tests.
    
    
    Test: browser_tests --gtest_filter="FileManagerJsTest.*"
    Bug: 991105
    Change-Id: I61c17edd4af10ac5ba739725585c23472b27b49e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767297
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#691960}

--- chrome/browser/chromeos/file_manager/file_manager_jstest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_jstest_base.cc
@@ -25,6 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+base::FilePath GetGenRoot() {
+  base::FilePath executable_path;
+  CHECK(base::PathService::Get(base::DIR_EXE, &executable_path));
+  return executable_path.AppendASCII("gen");
+}
+
 // URLDataSource for the test URL chrome://file_manager_test/. It reads files
 // directly from repository source.
 class TestFilesDataSource : public content::URLDataSource {
@@ -53,21 +59,30 @@ class TestFilesDataSource : public content::URLDataSource {
     if (source_root_.empty()) {
       CHECK(base::PathService::Get(base::DIR_SOURCE_ROOT, &source_root_));
     }
-    base::FilePath root =
-        source_root_.Append(FILE_PATH_LITERAL("ui/file_manager"));
+    if (gen_root_.empty()) {
+      CHECK(base::PathService::Get(base::DIR_EXE, &gen_root_));
+      gen_root_ = GetGenRoot();
+    }
+
     std::string content;
 
-    base::FilePath file_path =
-        root.Append(base::FilePath::FromUTF8Unsafe(path));
+    base::FilePath src_file_path =
+        source_root_.Append(base::FilePath::FromUTF8Unsafe(path));
+    base::FilePath gen_file_path =
+        gen_root_.Append(base::FilePath::FromUTF8Unsafe(path));
 
     // Do some basic validation of the file extension.
-    CHECK(file_path.Extension() == ".html" || file_path.Extension() == ".js" ||
-          file_path.Extension() == ".css")
+    CHECK(src_file_path.Extension() == ".html" ||
+          src_file_path.Extension() == ".js" ||
+          src_file_path.Extension() == ".css")
         << "chrome://file_manager_test/ only supports .html/.js/.css extension "
            "files";
 
-    CHECK(base::PathExists(file_path)) << file_path.value();
-    CHECK(base::ReadFileToString(file_path, &content)) << file_path.value();
+    CHECK(base::PathExists(src_file_path) || base::PathExists(gen_file_path))
+        << src_file_path << " or: " << gen_file_path << " input path: " << path;
+    CHECK(base::ReadFileToString(src_file_path, &content) ||
+          base::ReadFileToString(gen_file_path, &content))
+        << src_file_path << " or: " << gen_file_path;
 
     scoped_refptr<base::RefCountedString> response =
         base::RefCountedString::TakeString(&content);
@@ -88,8 +103,15 @@ class TestFilesDataSource : public content::URLDataSource {
     return "application/javascript";
   }
 
+  std::string GetContentSecurityPolicyScriptSrc() override {
+    // Add 'unsafe-inline' to CSP to allow the inline <script> in the generated
+    // HTML to run see js_test_gen_html.py.
+    return "script-src chrome://resources 'self'  'unsafe-inline'; ";
+  }
+
   // Root of repository source, where files are served directly from.
   base::FilePath source_root_;
+  base::FilePath gen_root_;
 
   DISALLOW_COPY_AND_ASSIGN(TestFilesDataSource);
 };
@@ -139,9 +161,7 @@ void FileManagerJsTestBase::RunTest(const base::FilePath& file) {
 }
 
 void FileManagerJsTestBase::RunGeneratedTest(const std::string& file) {
-  base::FilePath path;
-  ASSERT_TRUE(base::PathService::Get(base::DIR_EXE, &path));
-  path = path.AppendASCII("gen");
+  base::FilePath path = GetGenRoot();
 
   // Serve the generated html file from out/gen. It references files from
   // DIR_SOURCE_ROOT, so serve from there as well. An alternative would be to
@@ -155,7 +175,8 @@ void FileManagerJsTestBase::RunGeneratedTest(const std::string& file) {
 }
 
 void FileManagerJsTestBase::RunTestURL(const std::string& file) {
-  RunTestImpl(GURL("chrome://file_manager_test" + file));
+  RunTestImpl(
+      GURL("chrome://file_manager_test/" + base_path_.Append(file).value()));
 }
 
 void FileManagerJsTestBase::RunTestImpl(const GURL& url) {
