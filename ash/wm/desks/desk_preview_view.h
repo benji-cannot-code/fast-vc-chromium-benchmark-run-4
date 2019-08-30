8af723c9b24c5fb91d42c868c6bfa05ee720bb35
Sammie Quon <sammiequon@google.com>
2019-08-30 17:23:35 +0000

    desks: Add shadow to mini views.
    Test: manual
    Bug: 988521
    
    Change-Id: Iee9cc88044ecdef9b42645cadce9dd2037918dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771672
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692105}

--- ash/wm/desks/desk_preview_view.h
+++ ash/wm/desks/desk_preview_view.h
@@ -80,6 +80,8 @@ class DeskPreviewView : public views::View {
   void Layout() override;
 
  private:
+  class ShadowRenderer;
+
   DeskMiniView* const mini_view_;
 
   // A view to paint a background color behind the |wallpaper_preview_| to
@@ -107,6 +109,9 @@ class DeskPreviewView : public views::View {
   // the desk container to be visible.
   std::unique_ptr<aura::WindowOcclusionTracker::ScopedForceVisible>
       force_occlusion_tracker_visible_;
+
+  ui::Layer shadow_layer_;
+  std::unique_ptr<ShadowRenderer> shadow_delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(DeskPreviewView);
 };
