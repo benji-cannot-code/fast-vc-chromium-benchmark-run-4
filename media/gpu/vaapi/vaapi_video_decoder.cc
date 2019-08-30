149910994eef49edfef1271916409e5dd2dd505f
Vladislav Kuzkokov <vkuzkokov@chromium.org>
2019-08-30 13:29:57 +0000

    Revert "media/gpu: make VideoDecoderPipeline select VD implementation during Initialize()."
    This reverts commit cae62756bf88cfe77be0d558b2d35ac88fe5b53f.
    
    Reason for revert: Breaks build with target_os="chromeos"
    
    FAILED: libmedia_gpu.so libmedia_gpu.so.TOC
    python "../../build/toolchain/gcc_solink_wrapper.py" --readelf="readelf" --nm="nm" --sofile="./libmedia_gpu.so" --tocfile="./libmedia_gpu.so.TOC" --output="./libmedia_gpu.so" -- ../../third_party/llvm-build/Release+Asserts/bin/clang++ -shared -Wl,-soname="libmedia_gpu.so" -Wl,--fatal-warnings -fPIC -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,defs -Wl,--as-needed -fuse-ld=lld -Wl,--color-diagnostics -m64 -Werror -Wl,--gdb-index -nostdlib++ --sysroot=../../build/linux/debian_sid_amd64-sysroot -L../../build/linux/debian_sid_amd64-sysroot/usr/local/lib/x86_64-linux-gnu -L../../build/linux/debian_sid_amd64-sysroot/lib/x86_64-linux-gnu -L../../build/linux/debian_sid_amd64-sysroot/usr/lib/x86_64-linux-gnu -Wl,-rpath=\$ORIGIN -Wl,-rpath=\$ORIGIN -o "./libmedia_gpu.so" @"./libmedia_gpu.so.rsp"
    ld.lld: error: undefined symbol: media::VideoDecoderPipeline::Create(scoped_refptr<base::SequencedTaskRunner>, std::__Cr::unique_ptr<media::DmabufVideoFramePool, std::__Cr::default_delete<media::DmabufVideoFramePool> >, std::__Cr::unique_ptr<media::VideoFrameConverter, std::__Cr::default_delete<media::VideoFrameConverter> >)
    >>> referenced by chromeos_video_decoder_factory.cc:54 (../../media/gpu/chromeos/chromeos_video_decoder_factory.cc:54)
    >>>               obj/media/gpu/chromeos/chromeos/chromeos_video_decoder_factory.o:(media::ChromeosVideoDecoderFactory::Create(scoped_refptr<base::SequencedTaskRunner>, std::__Cr::unique_ptr<media::DmabufVideoFramePool, std::__Cr::default_delete<media::DmabufVideoFramePool> >, std::__Cr::unique_ptr<media::VideoFrameConverter, std::__Cr::default_delete<media::VideoFrameConverter> >))
    clang: error: linker command failed with exit code 1 (use -v to see invocation)
    
    Original change's description:
    > media/gpu: make VideoDecoderPipeline select VD implementation during Initialize().
    >
    > Some devices select the video decoder implementation by the video
    > configuration (e.g. codec). VideoDecoderPipeline needs to select a
    > proper video decoder in Initialize() when a video configuration is
    > specified.
    >
    > This CL implements the mechanism of selecting VD at Initialize()
    > instead of Create() method.
    >
    > BUG=chromium:952730
    > TEST=Run video_decode_accelerator_tests on Kevin and Eve
    >
    > Change-Id: I76077df0bc1e1eeae730e618a62c6e1049e02500
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1708693
    > Reviewed-by: David Staessens <dstaessens@chromium.org>
    > Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#691946}
    
    TBR=deanliao@chromium.org,akahuang@chromium.org,hiroh@chromium.org,acourbot@chromium.org,dstaessens@chromium.org
    
    Change-Id: I2edea554734a766fce1d61943297547159c8cbeb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:952730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778048
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Vladislav Kuzkokov <vkuzkokov@chromium.org>
    Commit-Queue: Vladislav Kuzkokov <vkuzkokov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692035}

--- media/gpu/vaapi/vaapi_video_decoder.cc
+++ media/gpu/vaapi/vaapi_video_decoder.cc
@@ -69,11 +69,9 @@ VaapiVideoDecoder::DecodeTask::DecodeTask(DecodeTask&&) = default;
 // static
 std::unique_ptr<VideoDecoder> VaapiVideoDecoder::Create(
     scoped_refptr<base::SequencedTaskRunner> client_task_runner,
-    scoped_refptr<base::SequencedTaskRunner> decoder_task_runner,
-    GetFramePoolCB get_pool_cb) {
+    std::unique_ptr<DmabufVideoFramePool> frame_pool) {
   return base::WrapUnique<VideoDecoder>(new VaapiVideoDecoder(
-      std::move(client_task_runner), std::move(decoder_task_runner),
-      std::move(get_pool_cb)));
+      std::move(client_task_runner), std::move(frame_pool)));
 }
 
 // static
@@ -84,11 +82,10 @@ SupportedVideoDecoderConfigs VaapiVideoDecoder::GetSupportedConfigs() {
 
 VaapiVideoDecoder::VaapiVideoDecoder(
     scoped_refptr<base::SequencedTaskRunner> client_task_runner,
-    scoped_refptr<base::SequencedTaskRunner> decoder_task_runner,
-    GetFramePoolCB get_pool_cb)
-    : get_pool_cb_(std::move(get_pool_cb)),
+    std::unique_ptr<DmabufVideoFramePool> frame_pool)
+    : frame_pool_(std::move(frame_pool)),
       client_task_runner_(std::move(client_task_runner)),
-      decoder_task_runner_(std::move(decoder_task_runner)),
+      decoder_thread_("VaapiDecoderThread"),
       weak_this_factory_(this) {
   DETACH_FROM_SEQUENCE(decoder_sequence_checker_);
   VLOGF(2);
@@ -153,7 +150,17 @@ void VaapiVideoDecoder::Initialize(const VideoDecoderConfig& config,
     return;
   }
 
-  decoder_task_runner_->PostTask(
+  if (!decoder_thread_.IsRunning() && !decoder_thread_.Start()) {
+    std::move(init_cb).Run(false);
+    return;
+  }
+
+  if (!decoder_thread_task_runner_) {
+    decoder_thread_task_runner_ = decoder_thread_.task_runner();
+    frame_pool_->set_parent_task_runner(decoder_thread_task_runner_);
+  }
+
+  decoder_thread_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&VaapiVideoDecoder::InitializeTask, weak_this_, config,
                      std::move(init_cb), std::move(output_cb)));
@@ -221,9 +228,6 @@ void VaapiVideoDecoder::InitializeTask(const VideoDecoderConfig& config,
   }
   needs_bitstream_conversion_ = (config.codec() == kCodecH264);
 
-  // Get and initialize the frame pool.
-  frame_pool_ = get_pool_cb_.Run();
-
   visible_rect_ = config.visible_rect();
   pixel_aspect_ratio_ = config.GetPixelAspectRatio();
   profile_ = profile;
@@ -240,8 +244,14 @@ void VaapiVideoDecoder::Destroy() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(client_sequence_checker_);
   VLOGF(2);
 
-  decoder_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&VaapiVideoDecoder::DestroyTask, weak_this_));
+  if (decoder_thread_task_runner_) {
+    decoder_thread_task_runner_->PostTask(
+        FROM_HERE, base::BindOnce(&VaapiVideoDecoder::DestroyTask, weak_this_));
+    decoder_thread_.Stop();
+  }
+
+  delete this;
+  VLOGF(2) << "Destroying VAAPI VD done";
 }
 
 void VaapiVideoDecoder::DestroyTask() {
@@ -256,17 +266,18 @@ void VaapiVideoDecoder::DestroyTask() {
     decoder_ = nullptr;
   }
 
-  weak_this_factory_.InvalidateWeakPtrs();
+  // Drop all video frame references. This will cause the frames to be
+  // destroyed once the decoder's client is done using them.
+  frame_pool_ = nullptr;
 
-  delete this;
-  VLOGF(2) << "Destroying VAAPI VD done";
+  weak_this_factory_.InvalidateWeakPtrs();
 }
 
 void VaapiVideoDecoder::Decode(scoped_refptr<DecoderBuffer> buffer,
                                DecodeCB decode_cb) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(client_sequence_checker_);
 
-  decoder_task_runner_->PostTask(
+  decoder_thread_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&VaapiVideoDecoder::QueueDecodeTask, weak_this_,
                                 std::move(buffer), std::move(decode_cb)));
 }
@@ -314,7 +325,7 @@ void VaapiVideoDecoder::ScheduleNextDecodeTask() {
                         *current_decode_task_->buffer_);
   }
 
-  decoder_task_runner_->PostTask(
+  decoder_thread_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&VaapiVideoDecoder::HandleDecodeTask, weak_this_));
 }
@@ -531,7 +542,7 @@ void VaapiVideoDecoder::ChangeFrameResolutionTask() {
   vaapi_wrapper_->CreateContext(pic_size);
 
   // Retry the current decode task.
-  decoder_task_runner_->PostTask(
+  decoder_thread_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(&VaapiVideoDecoder::HandleDecodeTask, weak_this_));
 }
@@ -560,7 +571,7 @@ void VaapiVideoDecoder::NotifyFrameAvailableTask() {
   if (state_ == State::kWaitingForOutput) {
     DCHECK(current_decode_task_);
     SetState(State::kDecoding);
-    decoder_task_runner_->PostTask(
+    decoder_thread_task_runner_->PostTask(
         FROM_HERE,
         base::BindOnce(&VaapiVideoDecoder::HandleDecodeTask, weak_this_));
   }
@@ -600,7 +611,7 @@ void VaapiVideoDecoder::Reset(base::OnceClosure reset_cb) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(client_sequence_checker_);
   DVLOGF(2);
 
-  decoder_task_runner_->PostTask(
+  decoder_thread_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&VaapiVideoDecoder::ResetTask, weak_this_,
                                 std::move(reset_cb)));
 }
@@ -622,7 +633,7 @@ void VaapiVideoDecoder::ResetTask(base::OnceClosure reset_cb) {
   SetState(State::kResetting);
 
   // Wait until any pending decode task has been aborted.
-  decoder_task_runner_->PostTask(
+  decoder_thread_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&VaapiVideoDecoder::ResetDoneTask, weak_this_,
                                 std::move(reset_cb)));
 }
