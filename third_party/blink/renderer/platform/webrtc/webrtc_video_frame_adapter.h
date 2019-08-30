829fd10d8524044e19ba63938d7c304a241d6466
Antonio Gomes <tonikitoo@igalia.com>
2019-08-30 20:02:26 +0000

    Move webrtc_video_frame_adapter.h out of the Blink exposed API
    All its clients have been Onion soup'ed.
    
    BUG=787254, 919392
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: Ibbef921caf036d9224de6195371b700057193add
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778661
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692184}

--- third_party/blink/renderer/platform/webrtc/webrtc_video_frame_adapter.h
+++ third_party/blink/renderer/platform/webrtc/webrtc_video_frame_adapter.h
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_WEBRTC_WEBRTC_VIDEO_FRAME_ADAPTER_H_
-#define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_WEBRTC_WEBRTC_VIDEO_FRAME_ADAPTER_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_WEBRTC_WEBRTC_VIDEO_FRAME_ADAPTER_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_WEBRTC_WEBRTC_VIDEO_FRAME_ADAPTER_H_
 
 #include <stdint.h>
 
 #include "media/base/video_frame.h"
-#include "third_party/blink/public/platform/web_common.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/webrtc/api/video/video_frame_buffer.h"
 
 namespace blink {
@@ -17,10 +17,7 @@ namespace blink {
 // implementation is read-only and will return null if trying to get a
 // non-const pointer to the pixel data. This object will be accessed from
 // different threads, but that's safe since it's read-only.
-//
-// TODO(crbug.com/704136): Move the class below out of the Blink exposed
-// API when all users of it have been Onion souped.
-class BLINK_PLATFORM_EXPORT WebRtcVideoFrameAdapter
+class PLATFORM_EXPORT WebRtcVideoFrameAdapter
     : public webrtc::VideoFrameBuffer {
  public:
   explicit WebRtcVideoFrameAdapter(scoped_refptr<media::VideoFrame> frame);
@@ -49,4 +46,4 @@ class BLINK_PLATFORM_EXPORT WebRtcVideoFrameAdapter
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_PUBLIC_PLATFORM_MODULES_WEBRTC_WEBRTC_VIDEO_FRAME_ADAPTER_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_WEBRTC_WEBRTC_VIDEO_FRAME_ADAPTER_H_
