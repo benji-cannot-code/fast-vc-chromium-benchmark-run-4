500282eb20ea8b31cc548ed93ca59eb0663dffe1
Sebastien Marchand <sebmarchand@chromium.org>
2019-08-30 14:38:21 +0000

    [PM] Use the SystemNode observer to notify of new process metrics data
    Bug: 998546
    Change-Id: Ibad528d47add2430955af8142aa1372fee5d0648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774792
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692048}

--- chrome/browser/performance_manager/public/graph/system_node.h
+++ chrome/browser/performance_manager/public/graph/system_node.h
@@ -42,6 +42,10 @@ class SystemNodeObserver {
   // Called before the |system_node| is removed from the graph.
   virtual void OnBeforeSystemNodeRemoved(const SystemNode* system_node) = 0;
 
+  // Called when a new set of process memory metrics is available.
+  virtual void OnProcessMemoryMetricsAvailable(
+      const SystemNode* system_node) = 0;
+
  private:
   DISALLOW_COPY_AND_ASSIGN(SystemNodeObserver);
 };
@@ -57,6 +61,8 @@ class SystemNode::ObserverDefaultImpl : public SystemNodeObserver {
   // SystemNodeObserver implementation:
   void OnSystemNodeAdded(const SystemNode* system_node) override {}
   void OnBeforeSystemNodeRemoved(const SystemNode* system_node) override {}
+  void OnProcessMemoryMetricsAvailable(const SystemNode* system_node) override {
+  }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(ObserverDefaultImpl);
