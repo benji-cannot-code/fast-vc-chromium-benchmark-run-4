daf4a5504d9d84e773a00538321ba4662875b08b
Erik Chen <erikchen@chromium.org>
2019-08-30 23:48:30 +0000

    WebViewImpl is-not-a WebWidget
    This CL makes WebViewImpl stop inheriting from WebWidget. The main functional
    change is that WebViewImpl::MainFrameWidget now returns the actual WebWidget
    [assuming one exists] or else nullptr. Previously it always returns `this`.
    
    All non-test callers of MainFrameWidget have previously been audited, and
    problematic call sites fixed.
    
    Change-Id: Ifb585add3a0e397af338022c5e30d36bdd90f83b
    Bug: 995981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771350
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692286}

--- third_party/blink/renderer/core/exported/web_frame_test.cc
+++ third_party/blink/renderer/core/exported/web_frame_test.cc
@@ -8711,7 +8711,7 @@ TEST_F(WebFrameSwapTest, ValidateSizeOnRemoteToLocalMainFrameSwap) {
   WebRemoteFrame* remote_frame = frame_test_helpers::CreateRemote();
   MainFrame()->Swap(remote_frame);
 
-  remote_frame->View()->MainFrameWidget()->Resize(size);
+  remote_frame->View()->Resize(size);
 
   WebLocalFrame* local_frame =
       frame_test_helpers::CreateProvisional(*remote_frame);
@@ -9508,7 +9508,7 @@ TEST_F(WebFrameTest, FrameWidgetTest) {
       *helper.RemoteMainFrame(), WebString(), WebFrameOwnerProperties(),
       nullptr, nullptr, &child_widget_client);
 
-  helper.GetWebView()->MainFrameWidget()->Resize(WebSize(1000, 1000));
+  helper.GetWebView()->Resize(WebSize(1000, 1000));
 
   WebGestureEvent event(WebInputEvent::kGestureTap, WebInputEvent::kNoModifiers,
                         WebInputEvent::GetStaticTimeStampForTests(),
--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -1015,7 +1015,8 @@ void WebViewImpl::EnableTapHighlightAtPoint(
 void WebViewImpl::EnableTapHighlights(
     HeapVector<Member<Node>>& highlight_nodes) {
   GetPage()->GetLinkHighlights().SetTapHighlights(highlight_nodes);
-  UpdateAllLifecyclePhases(LifecycleUpdateReason::kOther);
+  UpdateLifecycle(WebWidget::LifecycleUpdate::kAll,
+                  WebWidget::LifecycleUpdateReason::kOther);
 }
 
 void WebViewImpl::AnimateDoubleTapZoom(const gfx::Point& point_in_root_frame,
@@ -1379,7 +1380,8 @@ void WebViewImpl::ResizeViewWhileAnchored(float top_controls_height,
   // Update lifecyle phases immediately to recalculate the minimum scale limit
   // for rotation anchoring, and to make sure that no lifecycle states are
   // stale if this WebView is embedded in another one.
-  UpdateAllLifecyclePhases(LifecycleUpdateReason::kOther);
+  UpdateLifecycle(WebWidget::LifecycleUpdate::kAll,
+                  WebWidget::LifecycleUpdateReason::kOther);
 }
 
 void WebViewImpl::ResizeWithBrowserControls(
@@ -1456,6 +1458,10 @@ void WebViewImpl::DidEnterFullscreen() {
 
 void WebViewImpl::DidExitFullscreen() {
   fullscreen_controller_->DidExitFullscreen();
+}
+
+void WebViewImpl::SetWebWidget(WebWidget* widget) {
+  web_widget_ = widget;
 }
 
 void WebViewImpl::SetSuppressFrameRequestsWorkaroundFor704763Only(
@@ -1574,8 +1580,8 @@ void WebViewImpl::RecordEndOfFrameMetrics(base::TimeTicks frame_begin_time) {
       .RecordEndOfFrameMetrics(frame_begin_time, base::TimeTicks::Now());
 }
 
-void WebViewImpl::UpdateLifecycle(LifecycleUpdate requested_update,
-                                  LifecycleUpdateReason reason) {
+void WebViewImpl::UpdateLifecycle(WebWidget::LifecycleUpdate requested_update,
+                                  WebWidget::LifecycleUpdateReason reason) {
   TRACE_EVENT0("blink", "WebViewImpl::updateAllLifecyclePhases");
   if (!MainFrameImpl())
     return;
@@ -1585,7 +1591,7 @@ void WebViewImpl::UpdateLifecycle(LifecycleUpdate requested_update,
 
   PageWidgetDelegate::UpdateLifecycle(
       *AsView().page, *MainFrameImpl()->GetFrame(), requested_update, reason);
-  if (requested_update != LifecycleUpdate::kAll)
+  if (requested_update != WebWidget::LifecycleUpdate::kAll)
     return;
 
   // There is no background color for non-composited WebViews (eg printing).
@@ -3586,7 +3592,7 @@ void WebViewImpl::RestorePageFromBackForwardCache() {
 }
 
 WebWidget* WebViewImpl::MainFrameWidget() {
-  return this;
+  return web_widget_;
 }
 
 void WebViewImpl::AddAutoplayFlags(int32_t value) {
