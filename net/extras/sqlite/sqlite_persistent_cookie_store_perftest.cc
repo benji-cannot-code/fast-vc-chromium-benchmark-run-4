54fcd18336e8a607fb97d89b36b443070a32c16f
Yannic Bonenberger <contact@yannic-bonenberger.com>
2019-08-31 04:42:13 +0000

    Apply base_bind_rewriters to //net/extras/sqlite
    *** Note: There is no behavior change from this patch. ***
    
    This CL replaces calls to base::{Bind,BindRepeating} with calls to
    base::BindOnce, and removes calls to base::AdaptCallbackForRepeating
    when the returned base::RepeatingCallback is immediately converted
    to a base::OnceCallback.
    
    This CL was uploaded by git cl split.
    
    R=pwnall@chromium.org
    
    Bug: 714018
    Change-Id: I65d9dd97238e1f1db116183e6a4909a5e8d54015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778775
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Yannic Bonenberger <contact@yannic-bonenberger.com>
    Cr-Commit-Position: refs/heads/master@{#692336}

--- net/extras/sqlite/sqlite_persistent_cookie_store_perftest.cc
+++ net/extras/sqlite/sqlite_persistent_cookie_store_perftest.cc
@@ -67,8 +67,8 @@ class SQLitePersistentCookieStorePerfTest : public testing::Test {
   }
 
   void Load() {
-    store_->Load(base::Bind(&SQLitePersistentCookieStorePerfTest::OnLoaded,
-                            base::Unretained(this)),
+    store_->Load(base::BindOnce(&SQLitePersistentCookieStorePerfTest::OnLoaded,
+                                base::Unretained(this)),
                  NetLogWithSource());
     loaded_event_.Wait();
   }
@@ -163,8 +163,8 @@ TEST_F(SQLitePersistentCookieStorePerfTest, TestLoadForKeyPerformance) {
     StartPerfMeasurement();
     store_->LoadCookiesForKey(
         domain_name,
-        base::Bind(&SQLitePersistentCookieStorePerfTest::OnKeyLoaded,
-                   base::Unretained(this)));
+        base::BindOnce(&SQLitePersistentCookieStorePerfTest::OnKeyLoaded,
+                       base::Unretained(this)));
     key_loaded_event_.Wait();
     EndPerfMeasurement();
 
