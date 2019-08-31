9c11502de9b682baf34079d2910067da5a133ae7
Yannic Bonenberger <contact@yannic-bonenberger.com>
2019-08-31 20:46:19 +0000

    Apply base_bind_rewriters to //net/spdy
    *** Note: There is no behavior change from this patch. ***
    
    This CL replaces calls to base::{Bind,BindRepeating} with calls to
    base::BindOnce, and removes calls to base::AdaptCallbackForRepeating
    when the returned base::RepeatingCallback is immediately converted
    to a base::OnceCallback.
    
    This CL was uploaded by git cl split.
    
    R=rch@chromium.org
    
    Bug: 714018
    Change-Id: I49c3d769611846d8a717b44d86519368df975758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778772
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Auto-Submit: Yannic Bonenberger <contact@yannic-bonenberger.com>
    Cr-Commit-Position: refs/heads/master@{#692364}

--- net/spdy/spdy_network_transaction_unittest.cc
+++ net/spdy/spdy_network_transaction_unittest.cc
@@ -2361,8 +2361,8 @@ TEST_F(SpdyNetworkTransactionTest, StartTransactionOnReadCallback) {
   scoped_refptr<IOBuffer> buf = base::MakeRefCounted<IOBuffer>(kSize);
   rv = trans->Read(
       buf.get(), kSize,
-      base::Bind(&SpdyNetworkTransactionTest::StartTransactionCallback,
-                 helper.session(), default_url_, log_));
+      base::BindOnce(&SpdyNetworkTransactionTest::StartTransactionCallback,
+                     helper.session(), default_url_, log_));
   ASSERT_THAT(rv, IsError(ERR_IO_PENDING));
   // This forces an err_IO_pending, which sets the callback.
   data.Resume();
@@ -2409,10 +2409,9 @@ TEST_F(SpdyNetworkTransactionTest, DeleteSessionOnReadCallback) {
   const int kSize = 3000;
   scoped_refptr<IOBuffer> buf = base::MakeRefCounted<IOBuffer>(kSize);
   rv = trans->Read(
-      buf.get(),
-      kSize,
-      base::Bind(&SpdyNetworkTransactionTest::DeleteSessionCallback,
-                 base::Unretained(&helper)));
+      buf.get(), kSize,
+      base::BindOnce(&SpdyNetworkTransactionTest::DeleteSessionCallback,
+                     base::Unretained(&helper)));
   ASSERT_THAT(rv, IsError(ERR_IO_PENDING));
   data.Resume();
 
