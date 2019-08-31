23ac9fca5a4968fe5c38b8908042bb5f4fba2e51
Yannic Bonenberger <contact@yannic-bonenberger.com>
2019-08-31 00:22:05 +0000

    Apply base_bind_rewriters to //net/cert
    *** Note: There is no behavior change from this patch. ***
    
    This CL replaces calls to base::{Bind,BindRepeating} with calls to
    base::BindOnce, and removes calls to base::AdaptCallbackForRepeating
    when the returned base::RepeatingCallback is immediately converted
    to a base::OnceCallback.
    
    This CL was uploaded by git cl split.
    
    R=mattm@chromium.org
    
    Change-Id: I8b415eafa1b618785870087c39e813d615e4e7ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778550
    Auto-Submit: Yannic Bonenberger <contact@yannic-bonenberger.com>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692298}

--- net/cert/trial_comparison_cert_verifier_unittest.cc
+++ net/cert/trial_comparison_cert_verifier_unittest.cc
@@ -1207,7 +1207,7 @@ TEST_F(TrialComparisonCertVerifierTest, CancelledDuringPrimaryVerification) {
   CertVerifyResult result;
   std::unique_ptr<CertVerifier::Request> request;
   int error =
-      verifier.Verify(params, &result, base::BindRepeating(&NotCalledCallback),
+      verifier.Verify(params, &result, base::BindOnce(&NotCalledCallback),
                       &request, NetLogWithSource());
   ASSERT_THAT(error, IsError(ERR_IO_PENDING));
   EXPECT_TRUE(request);
@@ -1265,7 +1265,7 @@ TEST_F(TrialComparisonCertVerifierTest, DeletedDuringPrimaryVerification) {
   CertVerifyResult result;
   std::unique_ptr<CertVerifier::Request> request;
   int error =
-      verifier->Verify(params, &result, base::BindRepeating(&NotCalledCallback),
+      verifier->Verify(params, &result, base::BindOnce(&NotCalledCallback),
                        &request, NetLogWithSource());
   ASSERT_THAT(error, IsError(ERR_IO_PENDING));
   EXPECT_TRUE(request);
