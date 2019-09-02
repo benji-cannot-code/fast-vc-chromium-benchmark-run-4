1abc305a253ca3ad316ff509cf70101bfdeb37ed
Rune Lillesveen <futhark@chromium.org>
2019-09-02 10:04:09 +0000

    Make sure only slotable fallback nodes are assigned.
    Slot element fallback nodes were added to flat_tree_children_ without
    checking whether they were slotable. That made us try to mark e.g
    comment nodes for style recalc for FlatTreeParentChanged.
    
    This caused DCHECKs when trying to enforce not marking anything but
    elements and text nodes style dirty in a different CL.
    
    Shadow DOM V0 does not have the concept of slotable, but we can still
    skip style recalc for other nodes.
    
    Bug: 972752
    Change-Id: Iafdb56f1a2fee3dbbe293967d1386c2adeb501ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1777929
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692428}

--- third_party/blink/renderer/core/dom/node.cc
+++ third_party/blink/renderer/core/dom/node.cc
@@ -3151,6 +3151,12 @@ bool Node::HasMediaControlAncestor() const {
 }
 
 void Node::FlatTreeParentChanged() {
+  // TODO(futhark): Replace with DCHECK(IsSlotable()) when Shadow DOM V0 support
+  // is removed.
+  if (!IsElementNode() && !IsTextNode()) {
+    DCHECK(GetDocument().MayContainV0Shadow());
+    return;
+  }
   // The node changed the flat tree position by being slotted to a new slot or
   // slotted for the first time. We need to recalc style since the inheritance
   // parent may have changed.
