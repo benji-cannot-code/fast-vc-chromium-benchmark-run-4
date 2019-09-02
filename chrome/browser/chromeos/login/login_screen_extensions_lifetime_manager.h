db479704baa5ae51784422eeb4060eb3afccbf1b
Maksim Ivanov <emaxx@chromium.org>
2019-09-02 21:43:14 +0000

    Fix unwanted load of login-screen forced extension
    Close the hole that made it possible for the login-screen
    force-installed extensions/apps to be loaded back during the user
    session.
    
    This could happen before this CL, when, for example, the
    DeviceLoginScreenExtensions policy get changed during the user's
    session.
    
    This could lead to surprising or undesired effects, as this would mean
    that the lifetime management implemented in r685632 doesn't get applied
    in a few cases.
    
    The fix is to additionally observe all extensions/apps loaded in the
    sign-in profile, and stop those extensions that are loaded in undesired
    situation.
    
    Bug: 990484
    Test: log in, change the DeviceLoginScreenExtensions policy to add a new extension, check in the remote DevTools that the extension isn't loaded
    Change-Id: I9070682256667db23187b79aafba9544a3e35fd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1780835
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692519}

--- chrome/browser/chromeos/login/login_screen_extensions_lifetime_manager.h
+++ chrome/browser/chromeos/login/login_screen_extensions_lifetime_manager.h
@@ -7,16 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_LOGIN_LOGIN_SCREEN_EXTENSIONS_LIFETIME_MANAGER_H_
 
 #include "components/session_manager/core/session_manager_observer.h"
+#include "extensions/browser/extension_registry_observer.h"
 
 namespace chromeos {
 
 // Manages the lifetime of the login-screen policy-installed extensions and
 // apps, making sure that they are stopped during an active user session.
-//
-// TODO(crbug.com/990484): Handle policy changes correctly, so that extensions
-// are kept disabled even after the policy is reapplied.
 class LoginScreenExtensionsLifetimeManager final
-    : public session_manager::SessionManagerObserver {
+    : public session_manager::SessionManagerObserver,
+      public extensions::ExtensionRegistryObserver {
  public:
   LoginScreenExtensionsLifetimeManager();
   LoginScreenExtensionsLifetimeManager(
@@ -27,6 +26,10 @@ class LoginScreenExtensionsLifetimeManager final
 
   // session_manager::SessionManagerObserver:
   void OnSessionStateChanged() override;
+
+  // extensions::ExtensionRegistryObserver:
+  void OnExtensionLoaded(content::BrowserContext* browser_context,
+                         const extensions::Extension* extension) override;
 };
 
 }  // namespace chromeos
