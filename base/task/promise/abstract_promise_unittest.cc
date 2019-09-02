30730bbc18602b26aaebcc6ba7deff1163add80d
Yannic Bonenberger <contact@yannic-bonenberger.com>
2019-09-02 10:41:31 +0000

    Apply base_bind_rewriters to //base/task
    *** Note: There is no behavior change from this patch. ***
    
    This CL replaces calls to base::{Bind,BindRepeating} with calls to
    base::BindOnce, and removes calls to base::AdaptCallbackForRepeating
    when the returned base::RepeatingCallback is immediately converted
    to a base::OnceCallback.
    
    This CL was uploaded by git cl split.
    
    R=carlscab@google.com
    
    Bug: 714018
    Change-Id: Ib768ddfcfe5e3902658d35deb2b3b5aa3e10b0f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778776
    Reviewed-by: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Auto-Submit: Yannic Bonenberger <contact@yannic-bonenberger.com>
    Cr-Commit-Position: refs/heads/master@{#692437}

--- base/task/promise/abstract_promise_unittest.cc
+++ base/task/promise/abstract_promise_unittest.cc
@@ -2419,7 +2419,7 @@ TEST_F(AbstractPromiseTest, SingleRejectPrerequisitePolicyALLModified) {
             }));
 
     base::PostTask(FROM_HERE, {base::ThreadPool()},
-                   base::Bind(
+                   base::BindOnce(
                        [](scoped_refptr<AbstractPromise> p2) {
                          p2->emplace(Rejected<void>());
                        },
