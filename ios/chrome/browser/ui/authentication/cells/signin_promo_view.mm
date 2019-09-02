d157be3210b04f5224d30a0d97b2f8eb9c031089
Jérôme Lebel <jlebel@chromium.org>
2019-09-02 19:55:46 +0000

    [iOS][Accessibility] Adding a default accessibility action to the sign-in promo
    Adding a main action for the sign-in promo. So the user can double tap
    directly on the sign-in promo element. This main action opens the user consent
    in the same way than the primary button.
    
    Screenshot for the new string:
    https://drive.google.com/open?id=1sELSdGEqJBGbrWQNe6dK0MssM2-yc2iW
    
    Bug: 979471
    Change-Id: If6a348777b5b7201fdc735a4d457b525c424a017
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771914
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692510}

--- ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
+++ ios/chrome/browser/ui/authentication/cells/signin_promo_view.mm
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/colors/UIColor+cr_semantic_colors.h"
 #import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
-#include "ios/chrome/grit/ios_chromium_strings.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -68,14 +67,6 @@ NSString* const kSigninPromoCloseButtonId = @"kSigninPromoCloseButtonId";
 @implementation SigninPromoView {
   signin_metrics::AccessPoint _accessPoint;
 }
-
-@synthesize delegate = _delegate;
-@synthesize mode = _mode;
-@synthesize imageView = _imageView;
-@synthesize textLabel = _textLabel;
-@synthesize primaryButton = _primaryButton;
-@synthesize secondaryButton = _secondaryButton;
-@synthesize closeButton = _closeButton;
 
 - (instancetype)initWithFrame:(CGRect)frame {
   self = [super initWithFrame:frame];
@@ -226,9 +217,6 @@ NSString* const kSigninPromoCloseButtonId = @"kSigninPromoCloseButtonId";
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
   DCHECK(logo);
   _imageView.image = logo;
-  [_primaryButton
-      setTitle:l10n_util::GetNSString(IDS_IOS_OPTIONS_IMPORT_DATA_TITLE_SIGNIN)
-      forState:UIControlStateNormal];
   _secondaryButton.hidden = YES;
 }
 
@@ -280,17 +268,13 @@ NSString* const kSigninPromoCloseButtonId = @"kSigninPromoCloseButtonId";
 
 #pragma mark - NSObject(Accessibility)
 
+- (BOOL)accessibilityActivate {
+  [self accessibilityPrimaryAction:nil];
+  return YES;
+}
+
 - (NSArray<UIAccessibilityCustomAction*>*)accessibilityCustomActions {
   NSMutableArray* actions = [NSMutableArray array];
-
-  NSString* primaryActionName =
-      [self.primaryButton titleForState:UIControlStateNormal];
-  UIAccessibilityCustomAction* primaryCustomAction =
-      [[UIAccessibilityCustomAction alloc]
-          initWithName:primaryActionName
-                target:self
-              selector:@selector(accessibilityPrimaryAction:)];
-  [actions addObject:primaryCustomAction];
 
   if (_mode == SigninPromoViewModeWarmState) {
     NSString* secondaryActionName =
@@ -315,10 +299,6 @@ NSString* const kSigninPromoCloseButtonId = @"kSigninPromoCloseButtonId";
   }
 
   return actions;
-}
-
-- (NSString*)accessibilityLabel {
-  return self.textLabel.text;
 }
 
 @end
