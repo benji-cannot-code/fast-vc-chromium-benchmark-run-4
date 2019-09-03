f04c5ee21680790841ef8e712159863058613a9c
Matt Jones <mdjones@chromium.org>
2019-09-03 18:33:10 +0000

    Remove onTransitionPeekToHalf BottomSheetObserver method
    The onTransitionPeekToHalf method is no longer used by any features;
    this patch removes the observer method and related tests.
    
    Bug: 986310
    Change-Id: Ida0d4a55d2d08df2e9fa62224dd64095a635256c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1779421
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692747}

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/EmptyBottomSheetObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/EmptyBottomSheetObserver.java
@@ -28,9 +28,6 @@ public class EmptyBottomSheetObserver implements BottomSheetObserver {
     public void onSheetOffsetChanged(float heightFraction, float offsetPx) {}
 
     @Override
-    public void onTransitionPeekToHalf(float transitionFraction) {}
-
-    @Override
     public void onSheetStateChanged(int newState) {}
 
     @Override
