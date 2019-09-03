1f6bca30e59580d8237749bbd90e84ec4a7c5c45
Yutaka Hirano <yhirano@chromium.org>
2019-09-03 02:25:15 +0000

    Revert "[Extensions] Implement chrome.loginState API"
    This reverts commit 92956759f440c91cfe11ae447499fe279c480ab9.
    
    Reason for revert: Due to test flakiness (suspected by FindIt). See https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/14960
    
    Original change's description:
    > [Extensions] Implement chrome.loginState API
    > 
    > This CL adds a new API with 2 methods (getProfileType, getSessionState)
    > and 1 event (onSessionStateChanged).
    > 
    > Design Doc: go/chrome-login-state-api-proposal
    > 
    > Bug: 980460
    > Change-Id: I91ddccd808dfa3c3e2ca96716a856d108c909a85
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1751988
    > Commit-Queue: Jit Yao Yap <jityao@google.com>
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#692452}
    
    TBR=avi@chromium.org,rdevlin.cronin@chromium.org,hendrich@chromium.org,jityao@google.com
    
    Change-Id: I3079b35c02dbdf8c2f684a342b56737219a11a39
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 980460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1781302
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692535}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -111,7 +111,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/android_sms/android_sms_service_factory.h"
 #include "chrome/browser/chromeos/crostini/crostini_registry_service_factory.h"
-#include "chrome/browser/chromeos/extensions/login_screen/login_state/session_state_changed_event_dispatcher.h"
 #include "chrome/browser/chromeos/ownership/owner_settings_service_chromeos_factory.h"
 #include "chrome/browser/chromeos/policy/policy_cert_service_factory.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_token_forwarder_factory.h"
@@ -273,9 +272,8 @@ void ChromeBrowserMainExtraPartsProfiles::
   chromeos::SyncedPrintersManagerFactory::GetInstance();
   chromeos::smb_client::SmbServiceFactory::GetInstance();
   crostini::CrostiniRegistryServiceFactory::GetInstance();
-  extensions::SessionStateChangedEventDispatcher::GetFactoryInstance();
-  extensions::VerifyTrustAPI::GetFactoryInstance();
   TetherServiceFactory::GetInstance();
+  extensions::VerifyTrustAPI::GetFactoryInstance();
 #endif
   FaviconServiceFactory::GetInstance();
   HistoryUiFaviconRequestHandlerFactory::GetInstance();
