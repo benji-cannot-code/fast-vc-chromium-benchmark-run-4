36e79d4e9944229a093478f3e73534a0afbafd36
Kent Tamura <tkent@chromium.org>
2019-09-03 11:29:23 +0000

    Include lifecycle.mojom-blink-forward.h instead of lifecycle.mojom-blink.h in context_lifecycle_state_observer.h
    This CL reduces one of full mojom includes from document.h
    
    Bug: 242216
    Change-Id: I47ded423e137dd9bc11e34c691016fe29a6be848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1781912
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692612}

--- third_party/blink/renderer/modules/battery/battery_manager.cc
+++ third_party/blink/renderer/modules/battery/battery_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/battery/battery_manager.h"
 
+#include "third_party/blink/public/mojom/frame/lifecycle.mojom-blink.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
