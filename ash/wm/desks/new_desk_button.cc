86422f80a7ff8949f2d36d6d1ab8c336d55a92c6
Sammie Quon <sammiequon@google.com>
2019-09-03 19:52:17 +0000

    desks: Fix unnecessary highlight on disabled new desk button.
    Test: manual
    Bug: 1000191
    Change-Id: Iad3e0918ae6c82e3939e0e9413d93b0f58c2eda5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782671
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692794}

--- ash/wm/desks/new_desk_button.cc
+++ ash/wm/desks/new_desk_button.cc
@@ -169,7 +169,7 @@ void NewDeskButton::OnViewUnhighlighted() {
 }
 
 void NewDeskButton::UpdateBorderState() {
-  if (IsViewHighlighted()) {
+  if (IsViewHighlighted() && DesksController::Get()->CanCreateDesks()) {
     SetBorder(views::CreateRoundedRectBorder(
         kHighlightThicknessDp, kCornerRadius,
         GetNativeTheme()->GetSystemColor(
