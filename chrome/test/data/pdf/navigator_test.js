b34b1d24e5bd795e24c3ddcf15de383e85b31205
rbpotter <rbpotter@chromium.org>
2019-09-03 19:19:40 +0000

    PDF Viewer: Update PdfNavigator to use URL
    Change-Id: Iff82626585bfe2610c454e1b0a25e62ccc177ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1777138
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692773}

--- chrome/test/data/pdf/navigator_test.js
+++ chrome/test/data/pdf/navigator_test.js
@@ -200,11 +200,10 @@ var tests = [
         url,
         'foo.com/bar.pdf',
         'http://www.example.com/subdir/foo.com/bar.pdf');
-    // The expected result is not normalized here.
     doNavigationUrlTests(
         url,
         '../www.foo.com/bar.pdf',
-        'http://www.example.com/subdir/../www.foo.com/bar.pdf');
+        'http://www.example.com/www.foo.com/bar.pdf');
 
     // Open an absolute link.
     doNavigationUrlTests(
@@ -228,15 +227,15 @@ var tests = [
     doNavigationUrlTests(
         url,
         '..\\bar.pdf',
-        'http://www.example.com/subdir/..\\bar.pdf');
+        'http://www.example.com/bar.pdf');
     doNavigationUrlTests(
         url,
         '.\\bar.pdf',
-        'http://www.example.com/subdir/.\\bar.pdf');
+        'http://www.example.com/subdir/bar.pdf');
     doNavigationUrlTests(
         url,
         '\\bar.pdf',
-        'http://www.example.com/subdir/\\bar.pdf');
+        'http://www.example.com/subdir//bar.pdf');
 
     // Regression test for https://crbug.com/569040
     doNavigationUrlTests(
