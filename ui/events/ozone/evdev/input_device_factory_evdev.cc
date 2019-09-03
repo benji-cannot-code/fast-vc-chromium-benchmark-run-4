ea14c29d67163324d8a7aa8ed3dec4ed9ab370af
Zentaro Kavanagh <zentaro@chromium.org>
2019-09-03 16:18:23 +0000

    Add support for disabling mouse acceleration
    - Adds a toggle in settings
    - Adds plumbing to send value to gesture library
    
    BUG=chromium:589190
    TEST=Ad hoc
    
    Change-Id: Id7166dcdddfc3615d0f93d1a3f3aa20dddd62a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730964
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692671}

--- ui/events/ozone/evdev/input_device_factory_evdev.cc
+++ ui/events/ozone/evdev/input_device_factory_evdev.cc
@@ -318,6 +318,9 @@ void InputDeviceFactoryEvdev::ApplyInputDeviceSettings() {
                            input_device_settings_.touchpad_sensitivity);
   SetIntPropertyForOneType(DT_TOUCHPAD, "Scroll Sensitivity",
                            input_device_settings_.touchpad_sensitivity);
+  SetBoolPropertyForOneType(
+      DT_TOUCHPAD, "Pointer Acceleration",
+      input_device_settings_.touchpad_acceleration_enabled);
 
   SetBoolPropertyForOneType(DT_TOUCHPAD, "Tap Enable",
                             input_device_settings_.tap_to_click_enabled);
@@ -333,6 +336,8 @@ void InputDeviceFactoryEvdev::ApplyInputDeviceSettings() {
                            input_device_settings_.mouse_sensitivity);
   SetIntPropertyForOneType(DT_MOUSE, "Scroll Sensitivity",
                            input_device_settings_.mouse_sensitivity);
+  SetBoolPropertyForOneType(DT_MOUSE, "Pointer Acceleration",
+                            input_device_settings_.mouse_acceleration_enabled);
   SetBoolPropertyForOneType(
       DT_MOUSE, "Mouse Reverse Scrolling",
       input_device_settings_.mouse_reverse_scroll_enabled);
