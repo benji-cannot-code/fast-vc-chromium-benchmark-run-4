18483e93eeac742b199d238ec8b3a60453179ae9
Yoshifumi Inoue <yosin@chromium.org>
2019-09-03 06:59:26 +0000

    Make ReusingTextShaper to reuse only same text direction shape results
    This patch changes |ReusingTextShaper| to reuse only same text direciton
    shape results to create valid |ShapeResult|.
    
    Bug: 999469, 999766
    Change-Id: Ic53b0d45d4691ad7951b8d06e5ce9852ec798140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1780099
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692565}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_node.cc
@@ -155,7 +155,8 @@ class ReusingTextShaper final {
       return Reshape(start_item, start_offset, end_offset);
 
     const Vector<const ShapeResult*> reusable_shape_results =
-        CollectReusableShapeResults(start_offset, end_offset);
+        CollectReusableShapeResults(start_offset, end_offset,
+                                    start_item.Direction());
     if (reusable_shape_results.IsEmpty())
       return Reshape(start_item, start_offset, end_offset);
 
@@ -194,8 +195,10 @@ class ReusingTextShaper final {
                            target);
   }
 
-  Vector<const ShapeResult*> CollectReusableShapeResults(unsigned start_offset,
-                                                         unsigned end_offset) {
+  Vector<const ShapeResult*> CollectReusableShapeResults(
+      unsigned start_offset,
+      unsigned end_offset,
+      TextDirection direction) {
     DCHECK_LT(start_offset, end_offset);
     Vector<const ShapeResult*> shape_results;
     if (!reusable_items_)
@@ -211,7 +214,7 @@ class ReusingTextShaper final {
         break;
       if (item->EndOffset() < start_offset)
         continue;
-      if (!item->TextShapeResult())
+      if (!item->TextShapeResult() || item->Direction() != direction)
         continue;
       shape_results.push_back(item->TextShapeResult());
     }
