242e5d462fb520eedd4e7d513b670d173be51d01
Sergey Ulanov <sergeyu@chromium.org>
2019-09-03 22:40:39 +0000

    Reland "[Fuchsia] Handle VULKAN feature flag in WebEngine"
    This is a reland of 74c69a759fdc688ffa8151228e2dff91bbb85c02
    
    Original change's description:
    > [Fuchsia] Handle VULKAN feature flag in WebEngine
    >
    > Now WebEngine will enable Vulkan only when ContextFeatureFlags::VULKAN
    > is specified for web::Context. Also Context process now explicitly
    > disables hardware-accelerated compositing when VULKAN is not enabled
    > (otherwise it restarts the GPU process several times before it falls
    > back to software compositing).
    >
    > Bug: 962617, fuchsia:35009
    > Change-Id: I74a463a1514df99134d929981fe44151c1d9dd17
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774967
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Jonathan Backer <backer@chromium.org>
    > Reviewed-by: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#691856}
    
    TBR: backer@chromium.org
    Bug: 962617, fuchsia:35009
    Change-Id: Ia284d035deb396c57684ef069d54ddb9b7d5b15d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1779064
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692889}

--- fuchsia/engine/context_provider_impl.cc
+++ fuchsia/engine/context_provider_impl.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base_paths_fuchsia.h"
+#include "base/base_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/files/scoped_file.h"
@@ -33,10 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
+#include "components/viz/common/features.h"
 #include "content/public/common/content_switches.h"
 #include "fuchsia/engine/common.h"
+#include "gpu/command_buffer/service/gpu_switches.h"
 #include "net/http/http_util.h"
 #include "services/service_manager/sandbox/fuchsia/sandbox_policy_fuchsia.h"
+#include "ui/gl/gl_switches.h"
 
 namespace {
 
@@ -193,14 +197,21 @@ void ContextProviderImpl::Create(
                                       base::JoinString(handles_ids, ","));
   }
 
-#if defined(WEB_ENGINE_ENABLE_VULKAN)
-  // TODO(fbx/35009): Add a flag in CreateContextParams to enable/disable Vulkan
-  // and use it here.
-  launch_command.AppendSwitchASCII(
-      "--enable-features", "DefaultEnableOopRasterization,UseSkiaRenderer");
-  launch_command.AppendSwitch("--use-vulkan");
-  launch_command.AppendSwitchASCII("--use-gl", "stub");
-#endif  // WEB_ENGINE_ENABLE_VULKAN
+  fuchsia::web::ContextFeatureFlags features = {};
+  if (params.has_features())
+    features = params.features();
+
+  bool enable_vulkan = (features & fuchsia::web::ContextFeatureFlags::VULKAN) ==
+                       fuchsia::web::ContextFeatureFlags::VULKAN;
+
+  if (enable_vulkan) {
+    launch_command.AppendSwitch(switches::kUseVulkan);
+    launch_command.AppendSwitchASCII(switches::kEnableFeatures,
+                                     features::kUseSkiaRenderer.name);
+    launch_command.AppendSwitch(switches::kEnableOopRasterization);
+    launch_command.AppendSwitchASCII(switches::kUseGL,
+                                     gl::kGLImplementationStubName);
+  }
 
   // Validate embedder-supplied product, and optional version, and pass it to
   // the Context to include in the UserAgent.
