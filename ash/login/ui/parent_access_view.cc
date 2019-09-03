72e25bde95cfafdfa8a95b4bc29c31e1885c1819
Aga Wronska <agawronska@chromium.org>
2019-09-03 20:14:51 +0000

    Make ParentAccessWidget adapt to tablet mode
    Parent access view has different layout and size in tablet mode
    (virtual PIN keyboard is shown). The widget should resize together
    with the view.
    
    Bug: 991387
    Test: ParentAccessWidgetTest
    Change-Id: I8f0232e2618198950b4e23acf7a7ada82400e570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1779663
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692805}

--- ash/login/ui/parent_access_view.cc
+++ ash/login/ui/parent_access_view.cc
@@ -800,6 +800,8 @@ void ParentAccessView::UpdatePreferredSize() {
   pin_keyboard_to_footer_spacer_->SetPreferredSize(
       GetPinKeyboardToFooterSpacerSize());
   SetPreferredSize(CalculatePreferredSize());
+  if (GetWidget())
+    GetWidget()->CenterWindow(GetPreferredSize());
 }
 
 void ParentAccessView::FocusSubmitButton() {
