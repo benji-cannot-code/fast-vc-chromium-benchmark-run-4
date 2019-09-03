74f9e654cc633385b95b324fdee30e39c3765ee4
Antonio Gomes <tonikitoo@igalia.com>
2019-09-03 18:57:06 +0000

    Move definition of kWebRtcH264WithOpenH264FFmpeg to Blink
    This is a precursor CL to Onion soup'ing
    content::RtcVideoEncoderFactory (phase 2.3 in the design document [1]).
    
    [1] https://docs.google.com/document/d/1AJKVA5U4nDkyDB9p4ROrggWXadCxyy-grKaE9KS5vOU/
    
    Basically, it merged the definition and declaration of
    content/public/common/feature_h264_with_openh264_ffmpeg.cc|h
    into Blink's features.h. More specifically, to
    third_party/blink/public/common/features.h (declaration) and
    third_party/blink/common/features.cc (definition).
    
    This allows rtc_video_encoder_factory.cc|h to move to Blink in a
    follow up step.
    
    BUG=787254
    R=guidou@chromium.org, haraken@chromium.org, jam@chromium.org
    
    Change-Id: I4ba7c59da0f799e1d30053923fb1e692202223b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778671
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692757}

--- chrome/browser/media/webrtc/webrtc_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_browsertest.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/system_connector.h"
 #include "content/public/common/buildflags.h"
 #include "content/public/common/content_switches.h"
-#include "content/public/common/feature_h264_with_openh264_ffmpeg.h"
 #include "content/public/common/network_service_util.h"
 #include "content/public/common/service_names.mojom.h"
 #include "content/public/test/browser_test_utils.h"
@@ -30,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/mojom/network_service_test.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
+#include "third_party/blink/public/common/features.h"
 
 #if defined(OS_MACOSX)
 #include "base/mac/mac_util.h"
@@ -188,7 +188,8 @@ IN_PROC_BROWSER_TEST_F(MAYBE_WebRtcBrowserTest,
 IN_PROC_BROWSER_TEST_F(WebRtcBrowserTest,
                        RunsAudioVideoWebRTCCallInTwoTabsH264) {
   // Only run test if run-time feature corresponding to |rtc_use_h264| is on.
-  if (!base::FeatureList::IsEnabled(content::kWebRtcH264WithOpenH264FFmpeg)) {
+  if (!base::FeatureList::IsEnabled(
+          blink::features::kWebRtcH264WithOpenH264FFmpeg)) {
     LOG(WARNING) << "Run-time feature WebRTC-H264WithOpenH264FFmpeg disabled. "
         "Skipping WebRtcBrowserTest.RunsAudioVideoWebRTCCallInTwoTabsH264 "
         "(test \"OK\")";
