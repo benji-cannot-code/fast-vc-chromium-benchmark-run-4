22f784641cffe8faec3708fab725d0dcee1a8927
Mugdha Lakhani <nator@chromium.org>
2019-09-03 09:36:43 +0000

    [Background Sync] Add browsertests
    to test that periodicsync events are fired at an expected frequency,
    and that we update registrations as expected.
    
    Bug: 925297
    Change-Id: I8b26337c8ef6b8d38d3466c2fe15e3252bdd266c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1713642
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692592}

--- content/test/data/background_sync/service_worker.js
+++ content/test/data/background_sync/service_worker.js
@@ -25,6 +25,7 @@ this.onmessage = (event) => {
         return;
       }
 
+      periodicSyncEventCount++;
       resolveCallback();
       sendMessageToClients('sync', 'ok - delay completed');
       return;
