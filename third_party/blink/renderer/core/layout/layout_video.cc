90a69015d53fe47cd345aad3640dc932fdf46544
Koji Ishii <kojii@chromium.org>
2019-09-03 03:25:33 +0000

    LayoutVideo should not mark NeedsLayout in layout
    This patch fixes |LayoutVideo| not to mark |NeedsLayout|
    even when intrinsic size was changed, if it is during the
    layout. Layout clears |NeedsLayout| of its inline ancestors
    that doing this can leave them |NeedsLayout| set after the
    layout was complete.
    
    This is probably an old issue, but there were no code path
    to hit it, unitl r684192 (crrev.com/c/1703540) changed the
    intrinsic size computations.
    
    yosin@ pointed out that |intrinsic_size_| is not used at all
    when |ShouldApplySizeContainment()| since r684192, so we can
    probably skip |LayoutVideo::UpdateIntrinsicSize()|. This
    should fix the problem and also is supposed to improve the
    performance a bit. This patch takes more conservative
    approach to ensure we never |SetNeedsLayout| of inline
    ancestors during the layout.
    
    Bug: 998395
    Change-Id: I735780bbc3b65321796d217934b57c6be2d2adbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1780986
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692541}

--- third_party/blink/renderer/core/layout/layout_video.cc
+++ third_party/blink/renderer/core/layout/layout_video.cc
@@ -47,10 +47,10 @@ LayoutSize LayoutVideo::DefaultSize() {
 void LayoutVideo::IntrinsicSizeChanged() {
   if (VideoElement()->ShouldDisplayPosterImage())
     LayoutMedia::IntrinsicSizeChanged();
-  UpdateIntrinsicSize();
+  UpdateIntrinsicSize(/* is_in_layout */ false);
 }
 
-void LayoutVideo::UpdateIntrinsicSize() {
+void LayoutVideo::UpdateIntrinsicSize(bool is_in_layout) {
   LayoutSize size = CalculateIntrinsicSize();
   size.Scale(StyleRef().EffectiveZoom());
 
@@ -64,8 +64,10 @@ void LayoutVideo::UpdateIntrinsicSize() {
 
   SetIntrinsicSize(size);
   SetPreferredLogicalWidthsDirty();
-  SetNeedsLayoutAndFullPaintInvalidation(
-      layout_invalidation_reason::kSizeChanged);
+  if (!is_in_layout) {
+    SetNeedsLayoutAndFullPaintInvalidation(
+        layout_invalidation_reason::kSizeChanged);
+  }
 }
 
 LayoutSize LayoutVideo::CalculateIntrinsicSize() {
@@ -115,7 +117,7 @@ void LayoutVideo::ImageChanged(WrappedImagePtr new_image,
 
   // The intrinsic size is now that of the image, but in case we already had the
   // intrinsic size of the video we call this here to restore the video size.
-  UpdateIntrinsicSize();
+  UpdateIntrinsicSize(/* is_in_layout */ false);
 }
 
 bool LayoutVideo::ShouldDisplayVideo() const {
@@ -128,7 +130,7 @@ void LayoutVideo::PaintReplaced(const PaintInfo& paint_info,
 }
 
 void LayoutVideo::UpdateLayout() {
-  UpdatePlayer();
+  UpdatePlayer(/* is_in_layout */ true);
   LayoutMedia::UpdateLayout();
 }
 
@@ -138,14 +140,14 @@ HTMLVideoElement* LayoutVideo::VideoElement() const {
 
 void LayoutVideo::UpdateFromElement() {
   LayoutMedia::UpdateFromElement();
-  UpdatePlayer();
+  UpdatePlayer(/* is_in_layout */ false);
 
   // If the DisplayMode of the video changed, then we need to paint.
   SetShouldDoFullPaintInvalidation();
 }
 
-void LayoutVideo::UpdatePlayer() {
-  UpdateIntrinsicSize();
+void LayoutVideo::UpdatePlayer(bool is_in_layout) {
+  UpdateIntrinsicSize(is_in_layout);
 
   WebMediaPlayer* media_player = MediaElement()->GetWebMediaPlayer();
   if (!media_player)
--- third_party/blink/renderer/core/layout/layout_video.h
+++ third_party/blink/renderer/core/layout/layout_video.h
@@ -59,7 +59,7 @@ class LayoutVideo final : public LayoutMedia {
   void UpdateFromElement() override;
 
   LayoutSize CalculateIntrinsicSize();
-  void UpdateIntrinsicSize();
+  void UpdateIntrinsicSize(bool is_in_layout);
 
   void ImageChanged(WrappedImagePtr, CanDeferInvalidation) override;
 
@@ -80,7 +80,7 @@ class LayoutVideo final : public LayoutMedia {
 
   CompositingReasons AdditionalCompositingReasons() const override;
 
-  void UpdatePlayer();
+  void UpdatePlayer(bool is_in_layout);
 
   LayoutSize cached_image_size_;
 };
