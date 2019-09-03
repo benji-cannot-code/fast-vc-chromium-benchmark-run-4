c6cad81b7ba748a5f47178e2793cfd64d8b1a645
Denis Kuznetsov <antrim@google.com>
2019-09-03 15:17:59 +0000

    Correctly delegate focusing on network screen
    Bug: 999058
    Change-Id: I81f1efca3e5b1d20ddeb3b25ed29aeafea5659e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1781451
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692648}

--- chrome/browser/resources/chromeos/login/oobe_network.js
+++ chrome/browser/resources/chromeos/login/oobe_network.js
@@ -67,6 +67,10 @@ Polymer({
     this.$.networkDialog.show();
   },
 
+  focus: function() {
+    this.$.networkDialog.focus();
+  },
+
   /** Updates localized elements of the UI. */
   updateLocalizedContent: function() {
     this.$.networkSelectLogin.setCrOncStrings();
