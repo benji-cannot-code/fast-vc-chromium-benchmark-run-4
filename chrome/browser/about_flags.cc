b226d735306663fd3322d8299db8aef32eb9701a
Antonio Gomes <tonikitoo@igalia.com>
2019-09-03 21:27:17 +0000

    Reland "Move definition of kWebRtcH264WithOpenH264FFmpeg to Blink"
    This is a reland of f8ab5df96b5ddc2f17b8f7bc37337f81d80bbe9c
    
    The original (reverted CL) https://crrev.com/c/1778671 had one mistake:
    it missed to remove the includes of the autogenerated file
    
      <out>/gen/content/public/common/buildflags.h in chrome/.
    
    ... that is now gone. Given that the bots do not do a full rebuild
    (remove <out>/), CQ did not catch it.
    This reland CL should fix it.
    
    Original change's description:
    > Move definition of kWebRtcH264WithOpenH264FFmpeg to Blink
    >
    > This is a precursor CL to Onion soup'ing
    > content::RtcVideoEncoderFactory (phase 2.3 in the design document [1]).
    >
    > [1] https://docs.google.com/document/d/1AJKVA5U4nDkyDB9p4ROrggWXadCxyy-grKaE9KS5vOU/
    >
    > Basically, it merged the definition and declaration of
    > content/public/common/feature_h264_with_openh264_ffmpeg.cc|h
    > into Blink's features.h. More specifically, to
    > third_party/blink/public/common/features.h (declaration) and
    > third_party/blink/common/features.cc (definition).
    >
    > This allows rtc_video_encoder_factory.cc|h to move to Blink in a
    > follow up step.
    >
    > BUG=787254
    > R=guidou@chromium.org, haraken@chromium.org, jam@chromium.org
    >
    > Change-Id: I4ba7c59da0f799e1d30053923fb1e692202223b6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778671
    > Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#692757}
    
    TBR=guidou@chromium.org
    
    Bug: 787254
    Change-Id: I479b07d13802e3cad25b57e8c7dbeee048db0fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782514
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692861}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -116,7 +116,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/version_info/version_info.h"
 #include "components/viz/common/features.h"
 #include "components/viz/common/switches.h"
-#include "content/public/common/buildflags.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "device/base/features.h"
