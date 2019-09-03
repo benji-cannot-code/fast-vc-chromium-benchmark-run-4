47175f0d6ea826fba3342f44a34f9c0b43bf8508
Shik Chen <shik@chromium.org>
2019-09-03 17:52:26 +0000

    Configure streams according to the capture intent.
    Chrome OS VCD configures the still capture stream lazily when taking the
    first photo to optimize the performance for video conferencing scenario.
    But the underlying camera pipeline might have different tuning for
    video/photo mode based on the streams configured, so the preview in CCA
    might change significantly after taking the first photo, which confuses
    the users and image quality engineers.
    
    This CL adds SetCaptureIntent() in the CameraAppDevice interface, so CCA
    could set the capture intent to help the camera device decide optimal
    configurations, which is similar to CONTROL_CAPTURE_INTENT [1] in
    Android.
    
    [1] https://developer.android.com/reference/android/hardware/camera2/CaptureRequest#CONTROL_CAPTURE_INTENT
    
    Bug: b:139648548, b:135715807
    Test: Check the streams configured properly in CCA for each mode, and
    the color of preview does not change after the first capture.
    
    Change-Id: Ib15e608299cf4a90ff061fb69c2d95a79f0addab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1780990
    Commit-Queue: Shik Chen <shik@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692723}

--- chrome/browser/resources/chromeos/camera/src/js/views/camera/modes.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/camera/modes.js
@@ -129,6 +129,7 @@ cca.views.camera.Modes = function(
       resolutionConfig: videoPreferrer,
       v1Config: cca.views.camera.Modes.getV1Constraints.bind(this, true),
       nextMode: 'photo-mode',
+      captureIntent: cros.mojom.CaptureIntent.VIDEO_RECORD,
     },
     'photo-mode': {
       captureFactory: () => new cca.views.camera.Photo(
@@ -137,6 +138,7 @@ cca.views.camera.Modes = function(
       resolutionConfig: photoResolPreferrer,
       v1Config: cca.views.camera.Modes.getV1Constraints.bind(this, false),
       nextMode: 'square-mode',
+      captureIntent: cros.mojom.CaptureIntent.STILL_CAPTURE,
     },
     'square-mode': {
       captureFactory: () => new cca.views.camera.Square(
@@ -145,6 +147,7 @@ cca.views.camera.Modes = function(
       resolutionConfig: photoResolPreferrer,
       v1Config: cca.views.camera.Modes.getV1Constraints.bind(this, false),
       nextMode: 'portrait-mode',
+      captureIntent: cros.mojom.CaptureIntent.STILL_CAPTURE,
     },
     'portrait-mode': {
       captureFactory: () => new cca.views.camera.Portrait(
@@ -160,6 +163,7 @@ cca.views.camera.Modes = function(
       resolutionConfig: photoResolPreferrer,
       v1Config: cca.views.camera.Modes.getV1Constraints.bind(this, false),
       nextMode: 'photo-mode',
+      captureIntent: cros.mojom.CaptureIntent.STILL_CAPTURE,
     },
   };
 
@@ -289,6 +293,15 @@ cca.views.camera.Modes.prototype.getResolutionCandidatesV1 = function(
     mode, deviceId) {
   return this.allModes_[mode].v1Config(deviceId).map(
       (constraints) => [null, [constraints]]);
+};
+
+/**
+ * Gets capture intent for the given mode.
+ * @param {string} mode
+ * @return {cros.mojom.CaptureIntent} Capture intent for the given mode.
+ */
+cca.views.camera.Modes.prototype.getCaptureIntent = function(mode) {
+  return this.allModes_[mode].captureIntent;
 };
 
 /**
