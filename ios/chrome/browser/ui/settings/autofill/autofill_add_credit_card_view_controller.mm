f2bc655cce2ef5ddf66bcc5211c03f04462cf9de
redatawfik <redatawfik@google.com>
2019-09-03 15:07:16 +0000

    Disable "Add" Button if TableView text fields are empty.
    1- Listen for notifications posted when the text field
    is the first responder.
    2- Add method "textFieldDidChange" which disable "Add" button if
    TableView text fields are empty and enable it otherwise.
    
    Bug: 984545
    Change-Id: I1dd36af08634f74672445253b5bf2e8a67382f42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1777869
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Reda Tawfik <redatawfik@google.com>
    Cr-Commit-Position: refs/heads/master@{#692646}

--- ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_view_controller.mm
+++ ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_view_controller.mm
@@ -99,6 +99,8 @@ typedef NS_ENUM(NSInteger, ItemType) {
               style:UIBarButtonItemStyleDone
              target:self
              action:@selector(didTapAddButton:)];
+  self.navigationItem.rightBarButtonItem.enabled = NO;
+
   [self loadModel];
 }
 
@@ -219,6 +221,13 @@ typedef NS_ENUM(NSInteger, ItemType) {
       base::mac::ObjCCast<TableViewTextEditCell>(cell);
   editCell.textField.delegate = self;
   editCell.selectionStyle = UITableViewCellSelectionStyleNone;
+  // The cell could be reused by TableView.
+  [editCell.textField removeTarget:self
+                            action:@selector(textFieldDidChange:)
+                  forControlEvents:UIControlEventEditingChanged];
+  [editCell.textField addTarget:self
+                         action:@selector(textFieldDidChange:)
+               forControlEvents:UIControlEventEditingChanged];
 
   return cell;
 }
@@ -305,6 +314,12 @@ typedef NS_ENUM(NSInteger, ItemType) {
       [self.tableViewModel itemAtIndexPath:path]);
   item.textFieldValue = text;
   [self reconfigureCellsForItems:@[ item ]];
+}
+
+// Updates the status of the "Add" button based on the content of the
+// textfields.
+- (void)textFieldDidChange:(id)sender {
+  self.navigationItem.rightBarButtonItem.enabled = [self tableViewHasUserInput];
 }
 
 // Dimisses this view controller when Cancel button is tapped.
