65b0d24527498dfafd612433c770ce2d2e7f6e75
Miyoung Shin <myid.shin@igalia.com>
2019-09-03 13:59:59 +0000

    Convert MimeRegistry to new Mojo types
    This CL converts MimeRegistryRequest in content to the new Mojo
    types.
    
    Bug: 955171
    Change-Id: I6dc6705230205dac418d5a1259c0add99b511112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1781905
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692631}

--- content/browser/mime_registry_impl.h
+++ content/browser/mime_registry_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/sequence_checker.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "third_party/blink/public/mojom/mime/mime_registry.mojom.h"
 
 namespace content {
@@ -17,7 +18,8 @@ class MimeRegistryImpl : public blink::mojom::MimeRegistry {
   MimeRegistryImpl();
   ~MimeRegistryImpl() override;
 
-  static void Create(blink::mojom::MimeRegistryRequest request);
+  static void Create(
+      mojo::PendingReceiver<blink::mojom::MimeRegistry> receiver);
 
  private:
   void GetMimeTypeFromExtension(
