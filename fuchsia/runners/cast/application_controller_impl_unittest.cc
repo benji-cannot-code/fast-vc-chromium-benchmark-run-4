33f22fc13ee1c4ad641dfbe9a5dc57e7842ca7c0
Carlos Caballero <carlscab@google.com>
2019-09-03 18:09:57 +0000

    Remove MessageLoopForIO references
    Remove references in /fuchsia
    
    MessageLoopForIO is going away soon. Use SingleThreadTaskExecutor for
    main functions and SingleThreadTaskEnvironment for tests instead. Both
    are no op changes.
    
    This CL was uploaded by git cl split.
    
    R=sergeyu@chromium.org
    
    Bug: 891670
    Change-Id: I83f99926d31766f55ec1094394d7a26254fbfdf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1781426
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692733}

--- fuchsia/runners/cast/application_controller_impl_unittest.cc
+++ fuchsia/runners/cast/application_controller_impl_unittest.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
-#include "base/message_loop/message_loop.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/task_environment.h"
 #include "base/test/test_timeouts.h"
 #include "fuchsia/base/fit_adapter.h"
 #include "fuchsia/fidl/chromium/cast/cpp/fidl.h"
@@ -60,7 +60,8 @@ class ApplicationControllerImplTest
   }
 
   const base::RunLoop::ScopedRunTimeoutForTest run_timeout_;
-  base::MessageLoopForIO message_loop_;
+  base::test::SingleThreadTaskEnvironment task_environment_{
+      base::test::SingleThreadTaskEnvironment::MainThreadType::IO};
 
   MockFrame frame_;
   fidl::Binding<chromium::cast::ApplicationControllerReceiver>
