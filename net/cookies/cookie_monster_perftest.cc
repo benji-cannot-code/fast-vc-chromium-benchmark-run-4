1c3bab5a2407734d4fcf591ab9d3097d75752e49
Carlos Caballero <carlscab@google.com>
2019-09-03 14:48:33 +0000

    Remove MessageLoopForIO references
    Remove references in /net/cookies
    
    MessageLoopForIO is going away soon. Use SingleThreadTaskExecutor for
    main functions and SingleThreadTaskEnvironment for tests instead. Both
    are no op changes.
    
    This CL was uploaded by git cl split.
    
    R=morlovich@chromium.org
    
    Bug: 891670
    Change-Id: I4336336e3f3c5de94a40932fee4dbafa31df20d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1780739
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#692641}

--- net/cookies/cookie_monster_perftest.cc
+++ net/cookies/cookie_monster_perftest.cc
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/optional.h"
 #include "base/run_loop.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/perf_time_logger.h"
+#include "base/test/task_environment.h"
 #include "base/time/time.h"
 #include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_monster.h"
@@ -33,10 +33,11 @@ const char kGoogleURL[] = "http://www.foo.com";
 
 class CookieMonsterTest : public testing::Test {
  public:
-  CookieMonsterTest() : message_loop_(new base::MessageLoopForIO()) {}
+  CookieMonsterTest() {}
 
  private:
-  std::unique_ptr<base::MessageLoop> message_loop_;
+  base::test::SingleThreadTaskEnvironment task_environment_{
+      base::test::SingleThreadTaskEnvironment::MainThreadType::IO};
 };
 
 class CookieTestCallback {
