307abecd86fe3344a0506e6af3e37c7187d10e31
Andrey Kosyakov <caseq@chromium.org>
2019-09-03 17:07:14 +0000

    Remove ExternalBeginFrameControllerClient interface
    ... and replace it with a callback to IssueExternalBeginFrame.
    
    Also, deprecate Headless.needsBeginFrameChanged in the protocol --
    the client is supposed to always attempt to drive frames.
    
    Bug: 997946
    Change-Id: Idd0f773b80e027241960451b638427366b4b07c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768880
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Johannes Henkel <johannes@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692688}

--- headless/lib/browser/protocol/headless_handler.cc
+++ headless/lib/browser/protocol/headless_handler.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
-#include "base/lazy_instance.h"
 #include "cc/base/switches.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/switches.h"
@@ -29,8 +28,6 @@ using HeadlessExperimental::ScreenshotParams;
 
 namespace {
 
-base::LazyInstance<std::set<HeadlessHandler*>>::Leaky g_instances =
-    LAZY_INSTANCE_INITIALIZER;
 
 enum class ImageEncoding { kPng, kJpeg };
 constexpr int kDefaultScreenshotQuality = 80;
@@ -69,25 +66,12 @@ void OnBeginFrameFinished(
 
 }  // namespace
 
-// static
-void HeadlessHandler::OnNeedsBeginFrames(
-    HeadlessWebContentsImpl* headless_contents,
-    bool needs_begin_frames) {
-  if (!g_instances.IsCreated())
-    return;
-  for (const HeadlessHandler* handler : g_instances.Get()) {
-    if (handler->enabled_ && handler->frontend_)
-      handler->frontend_->NeedsBeginFramesChanged(needs_begin_frames);
-  }
-}
-
 HeadlessHandler::HeadlessHandler(base::WeakPtr<HeadlessBrowserImpl> browser,
                                  content::WebContents* web_contents)
     : DomainHandler(HeadlessExperimental::Metainfo::domainName, browser),
       web_contents_(web_contents) {}
 
 HeadlessHandler::~HeadlessHandler() {
-  DCHECK(g_instances.Get().find(this) == g_instances.Get().end());
 }
 
 void HeadlessHandler::Wire(UberDispatcher* dispatcher) {
@@ -96,18 +80,12 @@ void HeadlessHandler::Wire(UberDispatcher* dispatcher) {
 }
 
 Response HeadlessHandler::Enable() {
-  g_instances.Get().insert(this);
-  HeadlessWebContentsImpl* headless_contents =
-      HeadlessWebContentsImpl::From(browser().get(), web_contents_);
-  enabled_ = true;
-  if (headless_contents->needs_external_begin_frames() && frontend_)
+  if (frontend_)
     frontend_->NeedsBeginFramesChanged(true);
   return Response::OK();
 }
 
 Response HeadlessHandler::Disable() {
-  enabled_ = false;
-  g_instances.Get().erase(this);
   return Response::OK();
 }
 
@@ -126,9 +104,11 @@ void HeadlessHandler::BeginFrame(Maybe<double> in_frame_time_ticks,
 
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
           ::switches::kRunAllCompositorStagesBeforeDraw)) {
-    LOG(WARNING) << "BeginFrameControl commands are designed to be used with "
-                    "--run-all-compositor-stages-before-draw, see "
-                    "https://goo.gl/3zHXhB for more info.";
+    callback->sendFailure(
+        Response::Error("Command is only supported with "
+                        "--run-all-compositor-stages-before-draw, see "
+                        "https://goo.gl/3zHXhB for more info."));
+    return;
   }
 
   base::TimeTicks frame_time_ticks;
@@ -181,19 +161,15 @@ void HeadlessHandler::BeginFrame(Maybe<double> in_frame_time_ticks,
     }
   }
 
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          ::switches::kRunAllCompositorStagesBeforeDraw) &&
-      headless_contents->HasPendingFrame()) {
-    LOG(WARNING) << "A BeginFrame is already in flight. In "
-                    "--run-all-compositor-stages-before-draw mode, only a "
-                    "single BeginFrame should be active at the same time.";
+  if (!headless_contents->BeginFrame(
+          frame_time_ticks, deadline, interval, no_display_updates,
+          capture_screenshot,
+          base::BindOnce(&OnBeginFrameFinished, std::move(callback), encoding,
+                         quality))) {
+    callback->sendFailure(Response::Error(
+        "Failed to request a frame, is another frame pending?"));
+    return;
   }
-
-  headless_contents->BeginFrame(
-      frame_time_ticks, deadline, interval, no_display_updates,
-      capture_screenshot,
-      base::BindOnce(&OnBeginFrameFinished, std::move(callback), encoding,
-                     quality));
 }
 
 }  // namespace protocol
--- ui/aura/window_tree_host_platform.h
+++ ui/aura/window_tree_host_platform.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 enum class DomCode;
-class ExternalBeginFrameClient;
 class KeyboardHook;
 struct PlatformWindowInitProperties;
 }  // namespace ui
@@ -37,7 +36,7 @@ class AURA_EXPORT WindowTreeHostPlatform : public WindowTreeHost,
       ui::PlatformWindowInitProperties properties,
       std::unique_ptr<Window> = nullptr,
       const char* trace_environment_name = nullptr,
-      ui::ExternalBeginFrameClient* external_begin_frame_client = nullptr);
+      bool use_external_begin_frame_control = false);
   ~WindowTreeHostPlatform() override;
 
   // WindowTreeHost:
