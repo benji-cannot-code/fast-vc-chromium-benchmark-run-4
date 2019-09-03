ff563b80c81707b0afedded358c8705c32225be3
Matt Wolenetz <wolenetz@chromium.org>
2019-09-03 21:42:37 +0000

    MSE: Internally differentiate specific vs non-specific isTypeSupported
    This change prepares for relaxing MSE addSourceBuffer and changeType
    codec specificity per the non-normative guidance in the MSE
    codec-switching incubation spec at [1], especially sections 3.5.7 and
    10, whose text change for codec-switching is best viewed via the W3C
    diff of the MSE REC spec vs the codec-switching incubation spec [2].
    
    This preparation will allow later CLs to also enable implicit codec
    switching (e.g., without explicitly calling changeType) in Chrome MSE.
    Such implicit codec switching would still require the bytestream
    format to remain the same (explicit changeType can be used to switch
    bytestream formats).
    
    It updates StreamParserFactory to understand and report one of
    media::SupportsType values instead of boolean for support, and updates
    usage of the updated implementation such that no behavioral change
    should yet be observed with just this change. Forthcoming change(s) will
    allow addSourceBuffer and changeType to succeed for incompletely
    specified parameters (e.g. kMayBeSupported SupportsType value), while
    retaining strictness throughout of MediaSource.isTypeSupported's
    requirement for codec specificity.
    
    This change also fixes a previous code style issue where
    MIMETypeRegistry::IsSupportedMediaSourceMIMEType cast a boolean to an
    enum and then implicitly converted that result back to a boolean.
    
    BUG=535738
    
    [1] https://github.com/WICG/media-source/tree/codec-switching
    [2] https://services.w3.org/htmldiff?doc1=https%3A%2F%2Fwww.w3.org%2FTR%2Fmedia-source%2F&doc2=https%3A%2F%2Frawgit.com%2FWICG%2Fmedia-source%2Fcodec-switching%2Findex.html
    
    Change-Id: I444e0f60c04e15df7299af0df932f4fa20b2e747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776902
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692867}

--- third_party/blink/renderer/modules/media_capabilities/media_capabilities.cc
+++ third_party/blink/renderer/modules/media_capabilities/media_capabilities.cc
@@ -423,7 +423,8 @@ bool CheckMseSupport(const WebMediaConfiguration& configuration) {
     if (!audio_config.codec.Ascii().empty())
       codec_vector.push_back(audio_config.codec.Ascii());
 
-    if (!media::StreamParserFactory::IsTypeSupported(
+    if (media::IsSupported !=
+        media::StreamParserFactory::IsTypeSupported(
             audio_config.mime_type.Ascii(), codec_vector)) {
       DVLOG(2) << __func__ << " MSE does not support audio config: "
                << audio_config.mime_type.Ascii() << " "
@@ -440,7 +441,8 @@ bool CheckMseSupport(const WebMediaConfiguration& configuration) {
     if (!video_config.codec.Ascii().empty())
       codec_vector.push_back(video_config.codec.Ascii());
 
-    if (!media::StreamParserFactory::IsTypeSupported(
+    if (media::IsSupported !=
+        media::StreamParserFactory::IsTypeSupported(
             video_config.mime_type.Ascii(), codec_vector)) {
       DVLOG(2) << __func__ << " MSE does not support video config: "
                << video_config.mime_type.Ascii() << " "
