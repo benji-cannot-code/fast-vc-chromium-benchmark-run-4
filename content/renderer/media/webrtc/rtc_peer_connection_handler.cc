4ed51f96a8f49a94bfc3cb5dec4b6451af297763
Antonio Gomes <tonikitoo@igalia.com>
2019-09-03 18:09:01 +0000

    Rename rtc_event_log_output_sink_proxy_factory.h
    ... to rtc_event_log_output_sink_proxy_uilt.h, so we avoid
    future naming clashes, and to standardize the naming of these helper methods.
    
    This is a follow up of guidou's remark on [1].
    
    [1] https://crrev.com/c/1778669/1#message-7497fe86eae4a796099d9e76acfce6cc7312f69a
    
    BUG=787254
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I973b38c8c657b07c58ced3a24132d489e6862c28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782423
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#692732}

--- content/renderer/media/webrtc/rtc_peer_connection_handler.cc
+++ content/renderer/media/webrtc/rtc_peer_connection_handler.cc
@@ -38,7 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/modules/mediastream/web_platform_media_stream_track.h"
 #include "third_party/blink/public/platform/modules/mediastream/webrtc_uma_histograms.h"
 #include "third_party/blink/public/platform/modules/peerconnection/rtc_event_log_output_sink.h"
-#include "third_party/blink/public/platform/modules/peerconnection/rtc_event_log_output_sink_proxy_factory.h"
+#include "third_party/blink/public/platform/modules/peerconnection/rtc_event_log_output_sink_proxy_util.h"
 #include "third_party/blink/public/platform/web_media_constraints.h"
 #include "third_party/blink/public/platform/web_rtc_answer_options.h"
 #include "third_party/blink/public/platform/web_rtc_data_channel_init.h"
--- third_party/blink/renderer/platform/peerconnection/rtc_event_log_output_sink_proxy.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_event_log_output_sink_proxy.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "third_party/blink/public/platform/modules/peerconnection/rtc_event_log_output_sink.h"
-#include "third_party/blink/public/platform/modules/peerconnection/rtc_event_log_output_sink_proxy_factory.h"
+#include "third_party/blink/public/platform/modules/peerconnection/rtc_event_log_output_sink_proxy_util.h"
 
 namespace blink {
 
