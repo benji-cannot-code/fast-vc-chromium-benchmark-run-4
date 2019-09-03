cd6e370ead2de3e2181068f261fd33308b1e03b0
Theresa <twellington@chromium.org>
2019-09-03 22:15:31 +0000

    Revert "Reland #2: ProGuard: Move -checkdiscard for Log.* into @RemovableInRelease annotation"
    This reverts commit d713f16062fe57d58d0da052775f29f6c76fd763.
    
    Reason for revert: Appears to have broke compilation
    
    Original change's description:
    > Reland #2: ProGuard: Move -checkdiscard for Log.* into @RemovableInRelease annotation
    > 
    > This reverts commit 1595330c6bbbd233f758ea6a73dce2c042b530fe.
    > 
    > Reason for reland: Tweaked rules & ensured all targets compile locally.
    > 
    > Bug: 987226, 986693
    > Change-Id: I6987ca442089db80b7f4b8f44d51e1fda923bb3b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1726230
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Sam Maier <smaier@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#692815}
    
    TBR=agrieve@chromium.org,smaier@chromium.org
    
    Change-Id: I88c50b9e48bea3b7342fe9f402964c6c3209c25e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 987226, 986693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783558
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692881}

--- base/android/java/src/org/chromium/base/Log.java
+++ base/android/java/src/org/chromium/base/Log.java
@@ -83,8 +83,10 @@ public class Log {
     }
 
     /**
-     * In debug: Forwards to {@link android.util.Log#isLoggable(String, int)}, but alway
-     * In release: Always returns false (via @RemovableInRelease).
+     * Convenience function, forwards to {@link android.util.Log#isLoggable(String, int)}.
+     *
+     * Note: Has no effect on whether logs are sent or not. Use a method with
+     * {@link RemovableInRelease} to log something in Debug builds only.
      */
     public static boolean isLoggable(String tag, int level) {
         return android.util.Log.isLoggable(tag, level);
@@ -104,7 +106,6 @@ public class Log {
      * @param args Arguments referenced by the format specifiers in the format string. If the last
      *             one is a {@link Throwable}, its trace will be printed.
      */
-    @RemovableInRelease
     private static void verbose(String tag, String messageTemplate, Object... args) {
         String message = formatLogWithStack(messageTemplate, args);
         Throwable tr = getThrowableToLog(args);
@@ -190,7 +191,6 @@ public class Log {
      * @param args Arguments referenced by the format specifiers in the format string. If the last
      *             one is a {@link Throwable}, its trace will be printed.
      */
-    @RemovableInRelease
     private static void debug(String tag, String messageTemplate, Object... args) {
         String message = formatLogWithStack(messageTemplate, args);
         Throwable tr = getThrowableToLog(args);
@@ -363,7 +363,6 @@ public class Log {
     }
 
     /** Returns a string form of the origin of the log call, to be used as secondary tag.*/
-    @RemovableInRelease
     private static String getCallOrigin() {
         StackTraceElement[] st = Thread.currentThread().getStackTrace();
 
