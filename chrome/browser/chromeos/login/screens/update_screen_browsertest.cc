96dee654f861002e8b376db73592ad68a4d5ac43
Petr Smirnov <petrsmirnov@google.com>
2019-09-03 14:18:15 +0000

    Refactor UpdateScreen
    Divide UpdateScreen class into UpdateScreen and VersionUpdater classes. The latter is responsible for communication with UpdateEngineClient and NetworkPortalDetector, and UpdateScreen is responsible for screen's specific logic only.
    
    The purpose of it is to make possible creation of another update screen.
    
    Bug: 997841
    Change-Id: I8a77403dc8e86f304e10021a9d690c015cf8a14a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771596
    Commit-Queue: Petr Smirnov <petrsmirnov@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692636}

--- chrome/browser/chromeos/login/screens/update_screen_browsertest.cc
+++ chrome/browser/chromeos/login/screens/update_screen_browsertest.cc
@@ -16,11 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/chromeos/login/login_wizard.h"
 #include "chrome/browser/chromeos/login/screens/error_screen.h"
-#include "chrome/browser/chromeos/login/screens/update_screen.h"
 #include "chrome/browser/chromeos/login/test/js_checker.h"
 #include "chrome/browser/chromeos/login/test/network_portal_detector_mixin.h"
 #include "chrome/browser/chromeos/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host.h"
+#include "chrome/browser/chromeos/login/version_updater/version_updater.h"
 #include "chrome/browser/ui/webui/chromeos/login/error_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/network_screen_handler.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
@@ -76,7 +76,8 @@ class UpdateScreenTest : public MixinBasedInProcessBrowserTest {
         GetOobeUI()->GetView<UpdateScreenHandler>(), error_screen_,
         base::BindRepeating(&UpdateScreenTest::HandleScreenExit,
                             base::Unretained(this)));
-    update_screen_->set_tick_clock_for_testing(&tick_clock_);
+    version_updater_ = update_screen_->GetVersionUpdaterForTesting();
+    version_updater_->set_tick_clock_for_testing(&tick_clock_);
 
     MixinBasedInProcessBrowserTest::SetUpOnMainThread();
   }
@@ -104,6 +105,8 @@ class UpdateScreenTest : public MixinBasedInProcessBrowserTest {
   NetworkPortalDetectorMixin network_portal_detector_{&mixin_host_};
 
   std::unique_ptr<UpdateScreen> update_screen_;
+  // Version updater - owned by |update_screen_|.
+  VersionUpdater* version_updater_ = nullptr;
   // Error screen - owned by OobeUI.
   ErrorScreen* error_screen_ = nullptr;
 
@@ -359,8 +362,8 @@ IN_PROC_BROWSER_TEST_F(UpdateScreenTest, TestUpdateAvailable) {
   EXPECT_EQ(1, fake_update_engine_client_->reboot_after_update_call_count());
 
   // Simulate the situation where reboot does not happen in time.
-  ASSERT_TRUE(update_screen_->GetRebootTimerForTesting()->IsRunning());
-  update_screen_->GetRebootTimerForTesting()->FireNow();
+  ASSERT_TRUE(version_updater_->GetRebootTimerForTesting()->IsRunning());
+  version_updater_->GetRebootTimerForTesting()->FireNow();
 
   test::OobeJS().ExpectHiddenPath({"oobe-update-md", "updating-progress"});
   test::OobeJS().ExpectEQ("$('oobe-update-md').$$('#updating-progress').value",
@@ -390,7 +393,7 @@ IN_PROC_BROWSER_TEST_F(UpdateScreenTest, TestErrorCheckingForUpdate) {
   // GetLastStatus() will be called via ExitUpdate() called from
   // UpdateStatusChanged().
   fake_update_engine_client_->set_default_status(status);
-  update_screen_->UpdateStatusChanged(status);
+  version_updater_->UpdateStatusChangedForTesting(status);
 
   ASSERT_TRUE(last_screen_result_.has_value());
   EXPECT_EQ(UpdateScreen::Result::UPDATE_NOT_REQUIRED,
@@ -607,7 +610,7 @@ IN_PROC_BROWSER_TEST_F(UpdateScreenTest, UpdateOverCellularAccepted) {
       {"oobe-update-md", "checking-for-updates-dialog"});
 
   status.status = UpdateEngineClient::UPDATE_STATUS_UPDATED_NEED_REBOOT;
-  update_screen_->UpdateStatusChanged(status);
+  version_updater_->UpdateStatusChangedForTesting(status);
 
   // UpdateStatusChanged(status) calls RebootAfterUpdate().
   EXPECT_EQ(1, fake_update_engine_client_->reboot_after_update_call_count());
