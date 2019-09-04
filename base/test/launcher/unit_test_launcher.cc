8ee3599a43eabf4dd203f125f086ade9570510c3
Carlos Caballero <carlscab@google.com>
2019-09-04 09:05:17 +0000

    Remove MessageLoopForIO references
    Remove references in /base/test/launcher
    
    MessageLoopForIO is going away soon. Use SingleThreadTaskExecutor for
    main functions and SingleThreadTaskEnvironment for tests instead. Both
    are no op changes.
    
    This CL was uploaded by git cl split.
    
    R=erikchen@chromium.org
    
    Bug: 891670
    Change-Id: Ic8ca95716f8679b687f586893f7033802f022432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1780827
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#693068}

--- base/test/launcher/unit_test_launcher.cc
+++ base/test/launcher/unit_test_launcher.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/format_macros.h"
 #include "base/location.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_pump_type.h"
 #include "base/sequence_checker.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/system/sys_info.h"
+#include "base/task/single_thread_task_executor.h"
 #include "base/test/launcher/test_launcher.h"
 #include "base/test/test_switches.h"
 #include "base/test/test_timeouts.h"
@@ -183,9 +184,9 @@ int LaunchUnitTestsInternal(RunTestSuiteCallback run_test_suite,
           "--single-process-tests.\n");
   fflush(stdout);
 
-  MessageLoopForIO message_loop;
+  base::SingleThreadTaskExecutor executor(base::MessagePumpType::IO);
 #if defined(OS_POSIX)
-  FileDescriptorWatcher file_descriptor_watcher(message_loop.task_runner());
+  FileDescriptorWatcher file_descriptor_watcher(executor.task_runner());
 #endif
   use_job_objects =
       use_job_objects &&
