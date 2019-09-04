4aa153a275235a15815aa51be2688b095e15e821
Kent Tamura <tkent@chromium.org>
2019-09-04 06:30:20 +0000

    Reland "Move NavigationInitiator implementation from Document to a new class"
    This is a reland of d0b088a28ea6af4d19c4f4ab0e7728de2b8d2538
    
    The difference from the original commit:
      Fix a decoded_data_document_parser.cc compilation error.
    
    Original change's description:
    > Move NavigationInitiator implementation from Document to a new class
    >
    > This CL reduces the esitmated pre-processed size of document.h
    > from 4.18MB to 3.05MB.
    >
    > Change-Id: Icf6ed523ace7f7ed827861b3fdb5b81923ed16cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782210
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#693003}
    
    Bug: 242216
    Change-Id: I09c8915b9e6a08967b558751bb93d5171e00d171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782406
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693028}

--- third_party/blink/renderer/core/dom/decoded_data_document_parser.cc
+++ third_party/blink/renderer/core/dom/decoded_data_document_parser.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_encoding_data.h"
 #include "third_party/blink/renderer/core/html/parser/text_resource_decoder.h"
+#include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 
 namespace blink {
 
