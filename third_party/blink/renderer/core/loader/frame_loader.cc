8ed2986640f799d25573043aab50d29ad8f32715
Kent Tamura <tkent@chromium.org>
2019-09-04 04:19:21 +0000

    Move NavigationInitiator implementation from Document to a new class
    This CL reduces the esitamated pre-processed size of document.h
    from 4.18MB to 3.05MB.
    
    Change-Id: Icf6ed523ace7f7ed827861b3fdb5b81923ed16cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782210
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693003}

--- third_party/blink/renderer/core/loader/frame_loader.cc
+++ third_party/blink/renderer/core/loader/frame_loader.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/ignore_opens_during_unload_count_incrementer.h"
 #include "third_party/blink/renderer/core/events/page_transition_event.h"
 #include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
+#include "third_party/blink/renderer/core/frame/csp/navigation_initiator_impl.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_client.h"
@@ -679,7 +680,7 @@ void FrameLoader::StartNavigation(const FrameLoadRequest& passed_request,
                              ->ExperimentalFeaturesEnabled()) {
     initiator_csp = origin_document->GetContentSecurityPolicy()
                         ->ExposeForNavigationalChecks();
-    origin_document->BindNavigationInitiatorReceiver(
+    origin_document->NavigationInitiator().BindReceiver(
         navigation_initiator.InitWithNewPipeAndPassReceiver());
   }
 
