618bf59056592f6202bd78c0e8512169221a4c4d
redatawfik <redatawfik@google.com>
2019-09-04 15:35:52 +0000

    Fix TableViewTextCell background
    -When TableViewTextCell is tapped the background for the cell turned to
    gray while the background for the textLabel stays white.
    -Removing the code which sets TableViewTextCell textLabel background
    solves the problem.
    
    Bug: 984545
    Change-Id: I615041f21b29e4c93e7d6b98b87d40be02c636e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778043
    Commit-Queue: Reda Tawfik <redatawfik@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693185}

--- ios/chrome/browser/ui/table_view/cells/table_view_text_item.mm
+++ ios/chrome/browser/ui/table_view/cells/table_view_text_item.mm
@@ -47,13 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         self.accessibilityLabel ? self.accessibilityLabel : self.text;
   }
 
-  // Decide cell.textLabel.backgroundColor in order:
-  //   1. styler.cellBackgroundColor;
-  //   2. styler.tableViewBackgroundColor.
-  cell.textLabel.backgroundColor = styler.cellBackgroundColor
-                                       ? styler.cellBackgroundColor
-                                       : styler.tableViewBackgroundColor;
-
   // Decide cell.textLabel.textColor in order:
   //   1. this.textColor;
   //   2. styler.cellTitleColor;
