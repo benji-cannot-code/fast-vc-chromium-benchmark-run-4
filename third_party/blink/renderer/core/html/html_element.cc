cfe72176aae6424fc2a4be8358d1a9fdc937f878
Rakina Zata Amni <rakina@chromium.org>
2019-09-04 04:24:23 +0000

    DL: Adjust containment for elements with rendersubtree
    The rendersubtree attribute adds style, layout, and (if it contains
    "invisible") size containment to the element it's on, keeping
    existing containments and adding to them.
    See draft spec at https://github.com/whatwg/html/pull/4862.
    
    Bug: 991095
    Change-Id: I91666603e3fde7cdb637e0911a232daa7245edb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1780093
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693006}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -31,11 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/string_or_trusted_script.h"
 #include "third_party/blink/renderer/bindings/core/v8/string_treat_null_as_empty_string_or_trusted_script.h"
 #include "third_party/blink/renderer/core/css/css_color_value.h"
-#include "third_party/blink/renderer/core/css/css_identifier_value.h"
 #include "third_party/blink/renderer/core/css/css_markup.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
-#include "third_party/blink/renderer/core/css/css_value_list.h"
 #include "third_party/blink/renderer/core/css/style_change_reason.h"
 #include "third_party/blink/renderer/core/css_value_keywords.h"
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
@@ -247,9 +245,7 @@ bool HTMLElement::IsPresentationAttribute(const QualifiedName& name) const {
   if (name == kAlignAttr || name == kContenteditableAttr ||
       name == kHiddenAttr || name == kLangAttr ||
       name.Matches(xml_names::kLangAttr) || name == kDraggableAttr ||
-      name == kDirAttr ||
-      (RuntimeEnabledFeatures::DisplayLockingEnabled() &&
-       name == kRendersubtreeAttr))
+      name == kDirAttr)
     return true;
   return Element::IsPresentationAttribute(name);
 }
@@ -338,18 +334,6 @@ void HTMLElement::CollectStyleForPresentationAttribute(
     // xml:lang has a higher priority than lang.
     if (!FastHasAttribute(xml_names::kLangAttr))
       MapLanguageAttributeToLocale(value, style);
-  } else if (RuntimeEnabledFeatures::DisplayLockingEnabled() &&
-             name == kRendersubtreeAttr) {
-    // Add contain: style layout size.
-    CSSValueList* list = CSSValueList::CreateSpaceSeparated();
-    list->Append(*CSSIdentifierValue::Create(CSSValueID::kStyle));
-    list->Append(*CSSIdentifierValue::Create(CSSValueID::kLayout));
-    if (EqualIgnoringASCIICase(value, "invisible") ||
-        EqualIgnoringASCIICase(value, "invisible-activatable")) {
-      list->Append(*CSSIdentifierValue::Create(CSSValueID::kSize));
-    }
-    AddPropertyToPresentationAttributeStyle(style, CSSPropertyID::kContain,
-                                            *list);
   } else {
     Element::CollectStyleForPresentationAttribute(name, value, style);
   }
