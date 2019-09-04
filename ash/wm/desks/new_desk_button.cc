daaea14899cc30e319ae9c8b566b7bb5d7b41ced
Yutaka Hirano <yhirano@chromium.org>
2019-09-04 00:07:46 +0000

    Revert "desks: Fix unnecessary highlight on disabled new desk button."
    This reverts commit 619a3f3ecd8bb9f9d2a72031c1969378f69bb580.
    
    Reason for revert: suspected to break ash_unittests on MSAN: https://findit-for-me.appspot.com/waterfall/failure?url=https://build.chromium.org/p/chromium.memory/builders/Linux%20ChromiumOS%20MSan%20Tests/builds/14981
    
    Original change's description:
    > desks: Fix unnecessary highlight on disabled new desk button.
    > 
    > Test: manual
    > Bug: 1000191
    > Change-Id: Iad3e0918ae6c82e3939e0e9413d93b0f58c2eda5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782671
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#692794}
    
    TBR=afakhry@chromium.org,sammiequon@chromium.org
    
    Change-Id: I4baf70a77f62b2880aaeca4d5b3b7d4aca9aa51f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1000191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782360
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692936}

--- ash/wm/desks/new_desk_button.cc
+++ ash/wm/desks/new_desk_button.cc
@@ -169,7 +169,7 @@ void NewDeskButton::OnViewUnhighlighted() {
 }
 
 void NewDeskButton::UpdateBorderState() {
-  if (IsViewHighlighted() && DesksController::Get()->CanCreateDesks()) {
+  if (IsViewHighlighted()) {
     SetBorder(views::CreateRoundedRectBorder(
         kHighlightThicknessDp, kCornerRadius,
         GetNativeTheme()->GetSystemColor(
