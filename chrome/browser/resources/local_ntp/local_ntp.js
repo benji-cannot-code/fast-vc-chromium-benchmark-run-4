92a8d02d2b571f927d2f631c14cb0c6bbed24b25
Kristi Park <kristipark@chromium.org>
2019-09-04 18:16:31 +0000

    [NTP] Fix white flash before the page is initialized in dark mode
    The NTP body is hidden before initialization is complete, so the theme
    background color must be set on the HTML element instead. This prevents
    a white flash before the NTP fully initializes.
    
    The background color will be removed after initialization in order to
    dynamically update the color when the theme is changed.
    
    Bug: 1000186
    Change-Id: I6b8c4b2b76599af41df088214f372506eb48bc8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783407
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693255}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -759,7 +759,7 @@ function init() {
 
   utils.setPlatformClass(document.body);
   utils.disableOutlineOnMouseClick($(customize.IDS.EDIT_BG));
-  document.body.classList.add(CLASSES.INITED);
+  document.documentElement.classList.add(CLASSES.INITED);
 }
 
 /**
