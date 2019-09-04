65a7810772767c6f0a12e22d53976e7d6a766ee9
Balazs Engedy <engedy@chromium.org>
2019-09-04 19:35:51 +0000

    Experimental Mini-Infobar UI for Notifications Prompts
    This CL adds the ability to display notification permission requests as
    mini-infobars on Android by re-purposing dead code that had been earlier
    used for "grouped permissions".
    
    Java-side support is added for initially showing the infobar in the
    compact form (i.e. as a mini-infobar), with a "Details" link that allows
    toggling into a full infobar.
    
    The full infobar has a "Manage" button that opens notification settings,
    such that the infobar will still be visible after the user returns to
    the tab requesting the permission.
    
    Bug: 986737
    Change-Id: Id25bf9bbb4e1d92082523a2557c10db2927d87c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760727
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693312}

--- chrome/browser/permissions/permission_prompt_android.cc
+++ chrome/browser/permissions/permission_prompt_android.cc
@@ -27,6 +27,16 @@ PermissionPromptAndroid::PermissionPromptAndroid(
       weak_factory_(this) {
   DCHECK(web_contents);
 
+  InfoBarService* infobar_service =
+      InfoBarService::FromWebContents(web_contents_);
+  if (infobar_service &&
+      GroupedPermissionInfoBarDelegate::ShouldShowMiniInfobar(
+          GetContentSettingType(0u /* position */))) {
+    GroupedPermissionInfoBarDelegate::Create(weak_factory_.GetWeakPtr(),
+                                             infobar_service);
+    return;
+  }
+
   if (PermissionRequestNotificationAndroid::ShouldShowAsNotification(
           GetContentSettingType(0u /* position */))) {
     permission_request_notification_ =
@@ -73,7 +83,7 @@ size_t PermissionPromptAndroid::PermissionCount() const {
 ContentSettingsType PermissionPromptAndroid::GetContentSettingType(
     size_t position) const {
   const std::vector<PermissionRequest*>& requests = delegate_->Requests();
-  DCHECK_LT(position, requests.size());
+  CHECK_LT(position, requests.size());
   return requests[position]->GetContentSettingsType();
 }
 
