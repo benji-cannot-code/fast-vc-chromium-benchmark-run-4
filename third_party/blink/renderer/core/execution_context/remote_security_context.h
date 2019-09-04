4f81ef8d992fee119091c5a543c0276064086a51
Kent Tamura <tkent@chromium.org>
2019-09-04 00:25:34 +0000

    Remove feature_policy.mojom-blink.h dependency from document.h
    * Change the type of potentially_violated_features_ from std::bitset to
    Vector<bool>.
    
    * Remove feature_policy.mojom-blink.h dependency from security_context.h
     Also, apply *.mojom-blink-forward.h as much as possible.
    
    Bug: 242216
    Change-Id: If0c0d1816a5dcad726507c0a22cddbebac34dccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782209
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692944}

--- third_party/blink/renderer/core/execution_context/remote_security_context.h
+++ third_party/blink/renderer/core/execution_context/remote_security_context.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_EXECUTION_CONTEXT_REMOTE_SECURITY_CONTEXT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_EXECUTION_CONTEXT_REMOTE_SECURITY_CONTEXT_H_
 
+#include "third_party/blink/public/common/feature_policy/feature_policy.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/execution_context/security_context.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
--- third_party/blink/renderer/modules/idle/idle_detector.cc
+++ third_party/blink/renderer/modules/idle/idle_detector.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
+#include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink.h"
 #include "third_party/blink/public/mojom/idle/idle_manager.mojom-blink.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/security_context.h"
