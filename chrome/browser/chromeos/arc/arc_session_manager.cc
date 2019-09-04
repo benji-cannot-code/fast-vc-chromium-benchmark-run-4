7df5cdad6ea2a90e4519a216d84d5ca50780bc5c
Satoshi Niwa <niwa@google.com>
2019-09-04 14:30:34 +0000

    ArcSessionManager : Use TimeTicks for arc_start_time and sign_in_start_time
    Also update ArcPolicyBridge, the sole user of these time values.
    
    Context : We have been using SessionManagerImpl::GetArcStartTimeTicks()
    for perf measurements, etc, but this doesn't work for ARCVM.
    This CL makes ArcSessionManager.arc_start_time() equivalent to
    SessionManagerImpl::GetArcStartTimeTicks() by using TimeTicks.
    
    Bug: b/140361966
    Test: try
    Change-Id: Ic935ccdf8b2177abb0644551a3b422a5e5c0c25c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783687
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693157}

--- chrome/browser/chromeos/arc/arc_session_manager.cc
+++ chrome/browser/chromeos/arc/arc_session_manager.cc
@@ -337,7 +337,7 @@ void ArcSessionManager::OnProvisioningFinished(ProvisioningResult result) {
   } else if (!sign_in_start_time_.is_null()) {
     arc_sign_in_timer_.Stop();
 
-    UpdateProvisioningTiming(base::Time::Now() - sign_in_start_time_,
+    UpdateProvisioningTiming(base::TimeTicks::Now() - sign_in_start_time_,
                              provisioning_successful, profile_);
     UpdateProvisioningResultUMA(result, profile_);
     if (!provisioning_successful)
@@ -931,7 +931,7 @@ void ArcSessionManager::OnAndroidManagementChecked(
   switch (result) {
     case policy::AndroidManagementClient::Result::UNMANAGED:
       VLOG(1) << "Starting ARC for first sign in.";
-      sign_in_start_time_ = base::Time::Now();
+      sign_in_start_time_ = base::TimeTicks::Now();
       arc_sign_in_timer_.Start(
           FROM_HERE, kArcSignInTimeout,
           base::Bind(&ArcSessionManager::OnArcSignInTimeout,
@@ -1008,7 +1008,7 @@ void ArcSessionManager::StartArc() {
   for (auto& observer : observer_list_)
     observer.OnArcStarted();
 
-  arc_start_time_ = base::Time::Now();
+  arc_start_time_ = base::TimeTicks::Now();
   provisioning_reported_ = false;
 
   std::string locale;
