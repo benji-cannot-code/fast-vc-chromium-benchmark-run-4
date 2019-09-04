3437514ed844b716d0e8338bdab13113400bc7c7
Roger Tawa <rogerta@chromium.org>
2019-09-04 13:37:56 +0000

    Add DomainsToCheckForMalwareOfUploadedContent policy.
    This policy is checked if scanning for malware on uploads is enabled.  Only
    URLs whose host is in this list will be scanned.
    
    Also adds enum values to SendFilesForMalwareCheck to handle case of malware
    check during uploads.
    
    Bug: 991553
    Change-Id: I55dd5529a0438cf375272c808102c2b9a8fe4f5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782513
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693142}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1108,6 +1108,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kDomainsToCheckComplianceOfDownloadedContent,
     prefs::kDomainsToCheckComplianceOfDownloadedContent,
     base::Value::Type::LIST },
+  { key::kDomainsToCheckForMalwareOfUploadedContent,
+    prefs::kDomainsToCheckForMalwareOfUploadedContent,
+    base::Value::Type::LIST },
 #endif
 #if defined(OS_WIN)
   { key::kBrowserSwitcherUseIeSitelist,
