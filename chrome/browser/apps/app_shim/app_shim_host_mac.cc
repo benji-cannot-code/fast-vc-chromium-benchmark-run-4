9b4c0597e75dc93391c4bb2b2c9e73d644f0b28a
Christopher Cameron <ccameron@chromium.org>
2019-09-04 20:36:35 +0000

    Add feature for AppShimMultiProfile
    Also rename the RemoteCocoa flag to be kAppShimRemoteCocoa. Scope this
    flag to only control RemoteCocoa (the actual app shim is still created),
    since that has been more helpful in debugging. Also disable the app
    shim menu when RemoteCocoa is enabled.
    
    Bug: 982024
    Change-Id: I4a083d4aae892e05e942d5ca5f6252ccca44ca8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1775265
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693344}

--- chrome/browser/apps/app_shim/app_shim_host_mac.cc
+++ chrome/browser/apps/app_shim/app_shim_host_mac.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "chrome/browser/apps/app_shim/app_shim_host_bootstrap_mac.h"
 #include "chrome/browser/apps/app_shim/app_shim_host_mac.h"
+#include "chrome/common/chrome_features.h"
 #include "components/remote_cocoa/browser/application_host.h"
 #include "components/remote_cocoa/common/application.mojom.h"
 #include "content/public/browser/browser_thread.h"
@@ -32,7 +33,8 @@ AppShimHost::AppShimHost(AppShimHost::Client* client,
       launch_weak_factory_(this) {
   // Create the interfaces used to host windows, so that browser windows may be
   // created before the host process finishes launching.
-  if (uses_remote_views_) {
+  if (uses_remote_views_ &&
+      base::FeatureList::IsEnabled(features::kAppShimRemoteCocoa)) {
     // Create the interface that will be used by views::NativeWidgetMac to
     // create NSWindows hosted in the app shim process.
     remote_cocoa::mojom::ApplicationAssociatedRequest views_application_request;
--- chrome/browser/apps/app_shim/extension_app_shim_handler_mac.cc
+++ chrome/browser/apps/app_shim/extension_app_shim_handler_mac.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
 #include "chrome/browser/web_applications/components/web_app_shortcut_mac.h"
 #include "chrome/browser/web_applications/extensions/web_app_extension_shortcut.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/extension_metrics.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
@@ -60,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry.h"
 #include "extensions/common/constants.h"
 #include "ui/base/cocoa/focus_window_set.h"
-#include "ui/base/ui_base_features.h"
 
 using extensions::AppWindow;
 using extensions::AppWindowRegistry;
@@ -412,8 +410,6 @@ AppShimHost* ExtensionAppShimHandler::FindOrCreateHost(
 }
 
 AppShimHost* ExtensionAppShimHandler::GetHostForBrowser(Browser* browser) {
-  if (!features::HostWindowsInAppShimProcess())
-    return nullptr;
   Profile* profile = Profile::FromBrowserContext(browser->profile());
   const Extension* extension =
       apps::ExtensionAppShimHandler::MaybeGetAppForBrowser(browser);
