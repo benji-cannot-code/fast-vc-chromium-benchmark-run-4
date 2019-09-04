9f31988403580a30996ed1d23f577a159091515a
Xiaocheng Hu <xiaochengh@chromium.org>
2019-09-04 12:49:00 +0000

    Fix zoom handling in CSSMathFunctionValue::Create(length, zoom)
    This patch fixes a regression introduced by crrev.com/c/1744799.
    
    Before the regression, the creator divides the pixel value of the length
    by the zooming after. The above patch incorrectly changed that into
    multiplication, which is the regression.
    
    This patch changes that back into a division.
    
    Bug: 999875
    Change-Id: Ic5f07c2af52cdc94bde8b355e5c30a7f01c0a49c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783638
    Commit-Queue: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693131}

--- third_party/blink/renderer/core/css/css_primitive_value_test.cc
+++ third_party/blink/renderer/core/css/css_primitive_value_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_math_expression_node.h"
 #include "third_party/blink/renderer/core/css/css_math_function_value.h"
 #include "third_party/blink/renderer/core/css/css_numeric_literal_value.h"
+#include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
 
 namespace blink {
 namespace {
@@ -77,6 +78,29 @@ TEST(CSSPrimitiveValueTest, IsResolution) {
   EXPECT_FALSE(Create({5.0, UnitType::kDegrees})->IsResolution());
   EXPECT_TRUE(Create({5.0, UnitType::kDotsPerPixel})->IsResolution());
   EXPECT_TRUE(Create({5.0, UnitType::kDotsPerCentimeter})->IsResolution());
+}
+
+// https://crbug.com/999875
+TEST(CSSPrimitiveValueTest, Zooming) {
+  // Tests that the conversion CSSPrimitiveValue -> Length -> CSSPrimitiveValue
+  // yields the same value under zooming.
+
+  UnitValue a = {100, UnitType::kPixels};
+  UnitValue b = {10, UnitType::kPercentage};
+  CSSPrimitiveValue* original = CreateAddition(a, b);
+
+  CSSToLengthConversionData conversion_data;
+  conversion_data.SetZoom(0.5);
+
+  Length length = original->ConvertToLength(conversion_data);
+  EXPECT_TRUE(length.IsCalculated());
+  EXPECT_EQ(50.0, length.GetPixelsAndPercent().pixels);
+  EXPECT_EQ(10.0, length.GetPixelsAndPercent().percent);
+
+  CSSPrimitiveValue* converted =
+      CSSPrimitiveValue::CreateFromLength(length, conversion_data.Zoom());
+  EXPECT_TRUE(converted->IsMathFunctionValue());
+  EXPECT_EQ("calc(10% + 100px)", converted->CustomCSSText());
 }
 
 }  // namespace
