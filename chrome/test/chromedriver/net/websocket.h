6fec269f5a56b34343423e020ee2369f2e6535fe
Yutaka Hirano <yhirano@chromium.org>
2019-09-04 23:48:03 +0000

    Get rid of masking logic from WebSocketFrameParser
    We are planning to remove memcpy in
    WebSocketFrameParser::DecodeFramePayload. Unfortunately, we cannot
    keep the constness of |payload| as long as we have the unmasking logic
    there.
    
    This CL moves the logic to the caller. It is a spec violation for a
    server to set the "masked" bit in a frame, so we don't need the
    additional logic in net/websockets.
    
    Bug: 865001
    Change-Id: I73042683fba799d5b4072ec619ab52ece6723e17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1784244
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693440}

--- chrome/test/chromedriver/net/websocket.h
+++ chrome/test/chromedriver/net/websocket.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "net/base/completion_once_callback.h"
 #include "net/socket/tcp_client_socket.h"
+#include "net/websockets/websocket_frame.h"
 #include "net/websockets/websocket_frame_parser.h"
 #include "url/gurl.h"
 
@@ -79,6 +80,9 @@ class WebSocket {
 
   scoped_refptr<net::IOBufferWithSize> read_buffer_;
   net::WebSocketFrameParser parser_;
+  net::WebSocketMaskingKey current_masking_key_ = {};
+  bool is_current_frame_masked_ = false;
+  uint64_t current_frame_offset_ = 0;
   std::string next_message_;
 
   DISALLOW_COPY_AND_ASSIGN(WebSocket);
