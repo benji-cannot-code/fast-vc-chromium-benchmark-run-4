0c6f8b8351dddaf3fc3d7f77c307a431ba77030f
Michael Spang <spang@chromium.org>
2019-09-04 13:29:45 +0000

    Fix gn check error when printing is disabled
    GN doesn't understand conditional includes:
    
    ERROR at //chrome/utility/services.cc:74:11: Include not allowed.
              ^---------------------------------------------------------------------
    It is not in any dependency of
      //chrome/utility:utility
    The include file is in the target(s):
      //components/services/pdf_compositor/public/mojom:mojom
      //components/services/pdf_compositor/public/mojom:mojom__generator
    at least one of which should somehow be reachable.
    
    Bug: 1000645, b/140414631
    Change-Id: I94ab9df85f9c50f1999acec999e8c06a93eea6b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1784098
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693139}

--- chrome/utility/services.cc
+++ chrome/utility/services.cc
@@ -71,8 +71,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(ENABLE_PRINTING)
-#include "components/services/pdf_compositor/pdf_compositor_impl.h"
-#include "components/services/pdf_compositor/public/mojom/pdf_compositor.mojom.h"
+#include "components/services/pdf_compositor/pdf_compositor_impl.h"  // nogncheck
+#include "components/services/pdf_compositor/public/mojom/pdf_compositor.mojom.h"  // nogncheck
 #endif
 
 #if defined(OS_CHROMEOS)
