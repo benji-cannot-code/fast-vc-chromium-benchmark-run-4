6a2f492b6f2b4cc365cab786db2259688adaf896
Luciano Pacheco <lucmult@chromium.org>
2019-09-04 08:16:02 +0000

    Files app: Fix grid view scrolling back to top
    Scroll issue introduced CL:1636665, which replaced a call to
    mergeItems() method in a base class to super, however the immediate
    parent class |Grid| also has the mergeItems() method, which was
    interfering with the scroll. This CL reverts back to call this method
    from |List| base class.
    
    Test: manually
    Bug: 993068
    Change-Id: I19d225dcc4277a409e824c7cac580c571ba1f051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783690
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693052}

--- ui/file_manager/file_manager/foreground/js/ui/file_grid.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_grid.js
@@ -238,7 +238,7 @@ class FileGrid extends cr.ui.Grid {
    * @override
    */
   mergeItems(beginIndex, endIndex) {
-    super.mergeItems(beginIndex, endIndex);
+    cr.ui.List.prototype.mergeItems.call(this, beginIndex, endIndex);
 
     const afterFiller = this.afterFiller_;
     const columns = this.columns;
