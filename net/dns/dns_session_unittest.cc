a16ca148679e8d9d6948b9d06233cb0bc6e3184b
dalyk <dalyk@google.com>
2019-09-04 23:12:11 +0000

    Add UMA metrics for DoH experiment.
    These metrics will be used to compare the performance of DNS-over-UDP
    with DoH for each of the providers in the hardcoded mapping, to
    understand whether the probe-first approach to automatic mode
    successfully limits negative performance effects, to understand how
    the probes are behaving, and to understand the overall effect on
    start-to-finish DNS resolution times.
    
    Bug: 985589
    Change-Id: I2d35eb6a11e52220a0af540ffc978c4576c31570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771990
    Commit-Queue: Katharine Daly <dalyk@google.com>
    Reviewed-by: Nik Bhagat <nikunjb@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693423}

--- net/dns/dns_session_unittest.cc
+++ net/dns/dns_session_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/rand_util.h"
 #include "base/strings/stringprintf.h"
 #include "net/base/ip_address.h"
+#include "net/base/net_errors.h"
 #include "net/dns/dns_socket_pool.h"
 #include "net/dns/public/dns_protocol.h"
 #include "net/log/net_log_source.h"
@@ -277,9 +278,9 @@ TEST_F(DnsSessionTest, HistogramTimeoutLong) {
 TEST_F(DnsSessionTest, NegativeRtt) {
   Initialize(2 /* num_servers */, 2 /* num_doh_servers */);
   session_->RecordRTT(0, false /* is_doh_server */,
-                      base::TimeDelta::FromMilliseconds(-1));
+                      base::TimeDelta::FromMilliseconds(-1), OK /* rv */);
   session_->RecordRTT(0, true /* is_doh_server */,
-                      base::TimeDelta::FromMilliseconds(-1));
+                      base::TimeDelta::FromMilliseconds(-1), OK /* rv */);
 }
 
 TEST_F(DnsSessionTest, DohServerAvailability) {
