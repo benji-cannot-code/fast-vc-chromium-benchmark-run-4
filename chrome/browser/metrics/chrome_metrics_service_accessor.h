5b5b783bdac3c628b61232fbf7497537e674c718
Alexei Svitkine <asvitkine@chromium.org>
2019-09-04 18:16:38 +0000

    Add browser test to check first and second run trial groups.
    The test ensures that if master prefs are used to import the
    variations seed on first run, that permanent consistency trials
    choose the same group between those two runs.
    
    Also regenerates the seed data used by the test as the previous
    one had a study that was expired.
    
    Bug: 947656
    Change-Id: Ie5bb77cbcbec6cd6a94f0a2523e460405289da53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772296
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693256}

--- chrome/browser/metrics/chrome_metrics_service_accessor.h
+++ chrome/browser/metrics/chrome_metrics_service_accessor.h
@@ -41,6 +41,10 @@ class ChromeMetricsPrivateDelegate;
 class FileManagerPrivateIsUMAEnabledFunction;
 }
 
+namespace first_run {
+class FirstRunMasterPrefsVariationsSeedTest;
+}
+
 namespace metrics {
 class UkmConsentParamBrowserTest;
 }
@@ -113,6 +117,7 @@ class ChromeMetricsServiceAccessor : public metrics::MetricsServiceAccessor {
   friend class NavigationMetricsRecorder;
 
   // Testing related friends.
+  friend class first_run::FirstRunMasterPrefsVariationsSeedTest;
   friend class ForceFieldTrialsBrowserTest;
   friend class MetricsReportingStateTest;
   friend class metrics::UkmConsentParamBrowserTest;
