1d69da04e893eb98b5d4ffaf3539048a6a415763
Ricardo Quesada <ricardoq@google.com>
2019-09-04 20:37:38 +0000

    autotest_private: support PIP window state for ARC windows
    This CL adds support for PIP (Picture in Picture) window state when
    querying ARC window state via autotest_private_api.
    
    This is needed because we are replacing "dumpsys" with "dumpsys --proto"
    and "dumpsys --proto" does include the PIP state.
    And besides that, it is better to query Ash, than Android, for ARC
    states.
    
    TEST=Calling "ash.GetARCAppWindowState" from Tast returns PIP on PIP ARC
         windows.
    BUG=chromium:1000774
    
    Change-Id: I4096ab205836f3f3f8b1d4eb964321dd62df77dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1785058
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Ricardo Quesada <ricardoq@chromium.org>
    Auto-Submit: Ricardo Quesada <ricardoq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693345}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
@@ -392,6 +392,8 @@ api::autotest_private::WindowStateType ToWindowStateType(
     case ash::WindowStateType::kRightSnapped:
       return api::autotest_private::WindowStateType::
           WINDOW_STATE_TYPE_RIGHTSNAPPED;
+    case ash::WindowStateType::kPip:
+      return api::autotest_private::WindowStateType::WINDOW_STATE_TYPE_PIP;
     default:
       NOTREACHED();
       return api::autotest_private::WindowStateType::WINDOW_STATE_TYPE_NONE;
