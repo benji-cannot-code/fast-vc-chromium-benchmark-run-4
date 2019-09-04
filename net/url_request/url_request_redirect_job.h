f6b1d6e6d5669c2254388d31ec0c648073e47756
Aaron Tagliaboschi <aarontag@chromium.org>
2019-09-04 20:08:35 +0000

    Call RequestHeadersCallback on internal redirects
    On HSTS redirects, a new URLRequestRedirectJob is made in such a way
    that calls the `responseReceivedExtraInfo` DevTools protocol event,
    but not the `requestWillBeSentExtraInfo` event called from the request
    headers callback. This calls the callback with fake headers to for the
    matching fake headers sent out to the other event.
    
    Bug: 993019
    Change-Id: Ibd1645f4ba1458557095e992d910e3aedfdbbad9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758341
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693330}

--- net/url_request/url_request_redirect_job.h
+++ net/url_request/url_request_redirect_job.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "net/base/net_export.h"
+#include "net/http/http_raw_request_headers.h"
 #include "net/http/http_response_info.h"
 #include "net/url_request/url_request_job.h"
 
@@ -50,6 +51,7 @@ class NET_EXPORT URLRequestRedirectJob : public URLRequestJob {
   void Start() override;
   void Kill() override;
   bool CopyFragmentOnRedirect(const GURL& location) const override;
+  void SetRequestHeadersCallback(RequestHeadersCallback callback) override;
 
  private:
   void StartAsync();
@@ -61,6 +63,8 @@ class NET_EXPORT URLRequestRedirectJob : public URLRequestJob {
   std::string redirect_reason_;
 
   scoped_refptr<HttpResponseHeaders> fake_headers_;
+
+  RequestHeadersCallback request_headers_callback_;
 
   base::WeakPtrFactory<URLRequestRedirectJob> weak_factory_{this};
 };
