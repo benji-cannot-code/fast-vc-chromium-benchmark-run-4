23b98943f57d1ac91423aa373e1eff5cc4164211
Ivana Zuzic <izuzic@google.com>
2019-09-04 15:52:46 +0000

    [PWD Editing Android] Add the mask/unmask password logic
    When the visibility icon is clicked, the user needs to reauthenticate
    and the password unmasks. It becomes if the visibility icon is clicked
    again.
    
    Bug: 377410
    Change-Id: I7f7feb17ca5a475c3a5331a715894254c7e51d98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1781639
    Commit-Queue: Ivana Zuzic <izuzic@google.com>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693191}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordEntryViewer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordEntryViewer.java
@@ -184,7 +184,7 @@ public class PasswordEntryViewer
                     "PasswordManager.Android.PasswordExceptionEntry", PASSWORD_ENTRY_ACTION_VIEWED,
                     PASSWORD_ENTRY_ACTION_BOUNDARY);
         }
-        PasswordManagerHandlerProvider.getInstance().addObserver(this);
+
         return inflatedView;
     }
 
@@ -197,12 +197,13 @@ public class PasswordEntryViewer
 
             if (mCopyButtonPressed) copyPassword();
         }
+        PasswordManagerHandlerProvider.getInstance().addObserver(this);
     }
 
     @Override
-    public void onDestroyView() {
+    public void onPause() {
+        super.onPause();
         PasswordManagerHandlerProvider.getInstance().removeObserver(this);
-        super.onDestroyView();
     }
 
     private boolean isPasswordSyncingUser() {
