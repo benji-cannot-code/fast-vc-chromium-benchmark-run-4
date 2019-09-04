a64952560dcd66da372e7e8497d674f6e33556e1
Jérôme Lebel <jlebel@chromium.org>
2019-09-04 19:11:12 +0000

    [iOS] Adding MDM error dailog in Google services settings
    Show MDM error dialog when the user tap on the sync error cell in Google
    services settings.
    
    Bug: 1000680
    Change-Id: Ifcb1da55cbf2a9a8fb3b4f7a844a7864e72ddadf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1784612
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693300}

--- ios/chrome/browser/ui/settings/google_services/google_services_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/google_services_settings_coordinator.mm
@@ -185,6 +185,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)openReauthDialogAsSyncIsInAuthError {
+  ChromeIdentity* identity = self.authService->GetAuthenticatedIdentity();
+  if (self.authService->HasCachedMDMErrorForIdentity(identity)) {
+    self.authService->ShowMDMErrorDialogForIdentity(identity);
+    return;
+  }
   // Sync enters in a permanent auth error state when fetching an access token
   // fails with invalid credentials. This corresponds to Gaia responding with an
   // "invalid grant" error. The current implementation of the iOS SSOAuth
