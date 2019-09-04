a3e141c6ab102710b4eea73650b3d1faf03a0bcb
Dominic Mazzoni <dmazzoni@chromium.org>
2019-09-04 04:06:34 +0000

    Don't play synthesized speech from web pages when Chrome is in the background.
    Fixes two issues:
    1. If a web page starts talking and you switch out of Chrome, it will stop
       speech now.
    2. If a web page tries to start speaking after a setTimeout delay, it won't
       speak if Chrome doesn't have a visible window at the time it would speak.
    
    This doesn't necessarily address the issue of a background tab trying to speak,
    that would require plumbing through a lot of information that's not
    available now. Let's see if this is good enough, and revisit if we also
    need to keep track of tabs?
    
    Bug: 998328
    Change-Id: I5f4d2dbeb033b9868eee0faf242522f1f0796a6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1779172
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693001}

--- content/public/android/java/src/org/chromium/content/browser/TtsPlatformImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/TtsPlatformImpl.java
@@ -5,10 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser;
 
+import android.app.Activity;
 import android.os.Build;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 
+import org.chromium.base.ActivityState;
+import org.chromium.base.ApplicationStatus;
+import org.chromium.base.ApplicationStatus.ActivityStateListener;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.annotations.CalledByNative;
@@ -33,7 +37,7 @@ import java.util.Locale;
  * use PostTask.runOrPostTask(UiThreadTaskTraits.DEFAULT, ...)  when calling back to C++.
  */
 @JNINamespace("content")
-class TtsPlatformImpl {
+class TtsPlatformImpl implements ActivityStateListener {
     private static class TtsVoice {
         private TtsVoice(String name, String language) {
             mName = name;
@@ -84,6 +88,8 @@ class TtsPlatformImpl {
             }
         });
         addOnUtteranceProgressListener();
+
+        ApplicationStatus.registerStateListenerForAllActivities(this);
     }
 
     /**
@@ -107,6 +113,7 @@ class TtsPlatformImpl {
      */
     @CalledByNative
     private void destroy() {
+        ApplicationStatus.unregisterActivityStateListener(this);
         mNativeTtsPlatformImplAndroid = 0;
     }
 
@@ -162,6 +169,9 @@ class TtsPlatformImpl {
     @CalledByNative
     private boolean speak(
             int utteranceId, String text, String lang, float rate, float pitch, float volume) {
+        // Don't speak when in the background.
+        if (!ApplicationStatus.hasVisibleActivities()) return false;
+
         if (!mInitialized) {
             mPendingUtterance =
                     new PendingUtterance(this, utteranceId, text, lang, rate, pitch, volume);
@@ -319,8 +329,18 @@ class TtsPlatformImpl {
         }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
+    @Override
+    public void onActivityStateChange(Activity activity, @ActivityState int newState) {
+        // Stop speech if all browser windows are no longer visible.
+        if (!ApplicationStatus.hasVisibleActivities()) {
+            TtsPlatformImplJni.get().requestTtsStop(
+                    mNativeTtsPlatformImplAndroid, TtsPlatformImpl.this);
+        }
+    }
+
     @NativeMethods
     interface Natives {
+        void requestTtsStop(long nativeTtsPlatformImplAndroid, TtsPlatformImpl caller);
         void voicesChanged(long nativeTtsPlatformImplAndroid, TtsPlatformImpl caller);
         void onEndEvent(long nativeTtsPlatformImplAndroid, TtsPlatformImpl caller, int utteranceId);
         void onStartEvent(
