f6722f84b25a0779f3d3291d2bbce1ee034fcd5b
Miguel Casas-Sanchez <mcasas@chromium.org>
2019-09-04 21:22:36 +0000

    flags: Surface kUseFakeDeviceForMediaStream to chrome:flags
    This CL creates a chrome:flags entry for UseFakeDeviceForMediaStream,
    allowing for ChromeOS clients to replace the audio/video capture
    devices in the system with a fake capture device, a venerable PacMan
    with a timestamp.  Such fake webcam has been around forever for
    testing and verification, in the form of a command line, that is
    used in a bazillion places, so this CL basically checks the FeatureFlag
    and appends the command line if needed, in MediaStreamManager, the
    singleton hanging from BrowserMainLoop where media capture first
    starts.
    
    The purpose of this flag is to facilitate debugging of webrtc+video
    capture on ChromeOS, by allowing the developers to switch off the
    capture stack entirely, verifying the WebRTC stack in isolation.
    
    Bug: b:140250310
    Change-Id: Iac8ff27ac3cc1a178d58661f53b0ea9737f6d10f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783308
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693374}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2136,6 +2136,9 @@ extern const char kUiSlowAnimationsDescription[];
 extern const char kUsbguardName[];
 extern const char kUsbguardDescription[];
 
+extern const char kUseFakeDeviceForMediaStreamName[];
+extern const char kUseFakeDeviceForMediaStreamDescription[];
+
 extern const char kUseMonitorColorSpaceName[];
 extern const char kUseMonitorColorSpaceDescription[];
 
