9b23c47f0b65a1567153aa011cab206043cb0d26
Kenichi Ishibashi <bashi@chromium.org>
2019-09-04 03:44:23 +0000

    service worker: Merge DidInitializeWorkerContext() into WillEvaluateScript()
    This CL:
    * removes one not-well-documented method from WebServiceWorkerContextClient
    * is a preparation for a potential fix of race condition in service worker
      extension
    
    The context of the second bullet is a bit complicated. Service worker
    background extension has following ordering assumption at initialization:
    1. ExtensionMsg_Loaded is received on renderer
       extensions::Dispatcher::OnLoaded()
    2. Installing extension API bindings
       extensions::Dispatcher::DidInitializeServiceWorkerContextOnWorkerThread()
    
    However there is no such guarantees. The current implementation
    happens to be working just because creating EmbeddedWorkerInstanceClient
    is bound to RenderThreadImpl and StartWorker() mojo message is handled
    after ExtensionMsg_Loaded.
    
    To make sure the above ordering we need to wait for the loaded message
    before installing extension API bindings. DidInitializeWorkerContext()
    isn't a right place to install extension API bindings because it is called
    via blink::WorkerThread::InitializeOnWorkerThread(), which doesn't wait
    for anything after StartWorker() mojo message is received.
    
    Fortunately we have blink::WorkerGlobalScope::ReadyToRunScript(). This
    was introduced for service worker update to suspend service worker
    execution after script fetch. We could probably use this to wait for
    the extension loaded IPC message. ReadyToRunScript() calls
    WillEvaluateScript() before executing the script so this would be a
    better place to install extension API bindings.
    
    Bug: 995134
    Change-Id: I54d44391149225c5c52294bcca32a86705a9c0a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1775856
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#692997}

--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -323,7 +323,7 @@ void Dispatcher::DidCreateScriptContext(
       break;
     case Feature::BLESSED_EXTENSION_CONTEXT:
       // For service workers this is handled in
-      // DidInitializeServiceWorkerContextOnWorkerThread().
+      // WillEvaluateServiceWorkerOnWorkerThread().
       DCHECK(!context->IsForServiceWorker());
       UMA_HISTOGRAM_TIMES("Extensions.DidCreateScriptContext_Blessed", elapsed);
       break;
@@ -354,7 +354,7 @@ void Dispatcher::DidCreateScriptContext(
   VLOG(1) << "Num tracked contexts: " << script_context_set_->size();
 }
 
-void Dispatcher::DidInitializeServiceWorkerContextOnWorkerThread(
+void Dispatcher::WillEvaluateServiceWorkerOnWorkerThread(
     blink::WebServiceWorkerContextProxy* context_proxy,
     v8::Local<v8::Context> v8_context,
     int64_t service_worker_version_id,
--- extensions/renderer/resources/service_worker_bindings.js
+++ extensions/renderer/resources/service_worker_bindings.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This function is returned to DidInitializeServiceWorkerContextOnWorkerThread
+// This function is returned to WillEvaluateServiceWorkerOnWorkerThread
 // then executed, passing in dependencies as function arguments.
 //
 // |backgroundUrl| is the URL of the extension's background page.
