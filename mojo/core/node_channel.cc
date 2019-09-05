bbda25a7b8d79a80a66f51ca8d6d7a9eb7c99f38
Dave Tapuska <dtapuska@chromium.org>
2019-09-05 13:23:36 +0000

    Fix variable shadowing issues in mojo/core
    Remove a nested RequestContext that didn't do anything.
    Rename or reuse variable on stack for remaining shadowed errors.
    
    BUG=794619
    
    Change-Id: I12f61d5528d62ee64d82104d190eecb6ccd65a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1784973
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693682}

--- mojo/core/node_channel.cc
+++ mojo/core/node_channel.cc
@@ -655,7 +655,7 @@ void NodeChannel::OnChannelMessage(const void* payload,
     }
 
 #if defined(OS_WIN)
-    case MessageType::EVENT_MESSAGE_FROM_RELAY:
+    case MessageType::EVENT_MESSAGE_FROM_RELAY: {
       const EventMessageFromRelayData* data;
       if (GetMessagePayload(payload, payload_size, &data)) {
         size_t num_bytes = payload_size - sizeof(*data);
@@ -673,7 +673,7 @@ void NodeChannel::OnChannelMessage(const void* payload,
         return;
       }
       break;
-
+    }
 #endif  // defined(OS_WIN)
 
     case MessageType::ACCEPT_PEER: {
