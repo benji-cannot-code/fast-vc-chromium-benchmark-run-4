932c0321d83a32d6009fefc5e466d2acbed4d61a
Kenichi Ishibashi <bashi@chromium.org>
2019-09-05 09:32:23 +0000

    Remove WorkerReportingProxy::DidInitializeWorkerContext()
    ServiceWorkerGlobalScopeProxy was the only subclass that implemented
    the method but after crrev.com/c/1786523 it became no-op.
    
    This is a follow-up CL of crrev.com/c/1775856
    
    Bug: N/A
    Change-Id: Ic6a2780d135339432d131d73b275c1f94bb8a237
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786892
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693620}

--- third_party/blink/renderer/core/workers/worker_thread_test.cc
+++ third_party/blink/renderer/core/workers/worker_thread_test.cc
@@ -100,9 +100,6 @@ void CreateNestedWorkerThenTerminateParent(
               DidCreateWorkerGlobalScope(_))
       .Times(1);
   EXPECT_CALL(*nested_worker_helper->reporting_proxy,
-              DidInitializeWorkerContext())
-      .Times(1);
-  EXPECT_CALL(*nested_worker_helper->reporting_proxy,
               WillEvaluateClassicScriptMock(_, _))
       .Times(1);
   EXPECT_CALL(*nested_worker_helper->reporting_proxy,
@@ -192,7 +189,6 @@ class WorkerThreadTest : public testing::Test {
  protected:
   void ExpectReportingCalls() {
     EXPECT_CALL(*reporting_proxy_, DidCreateWorkerGlobalScope(_)).Times(1);
-    EXPECT_CALL(*reporting_proxy_, DidInitializeWorkerContext()).Times(1);
     EXPECT_CALL(*reporting_proxy_, WillEvaluateClassicScriptMock(_, _))
         .Times(1);
     EXPECT_CALL(*reporting_proxy_, DidEvaluateClassicScript(true)).Times(1);
@@ -202,8 +198,6 @@ class WorkerThreadTest : public testing::Test {
 
   void ExpectReportingCallsForWorkerPossiblyTerminatedBeforeInitialization() {
     EXPECT_CALL(*reporting_proxy_, DidCreateWorkerGlobalScope(_)).Times(1);
-    EXPECT_CALL(*reporting_proxy_, DidInitializeWorkerContext())
-        .Times(AtMost(1));
     EXPECT_CALL(*reporting_proxy_, WillEvaluateClassicScriptMock(_, _))
         .Times(AtMost(1));
     EXPECT_CALL(*reporting_proxy_, DidEvaluateClassicScript(_))
@@ -215,7 +209,6 @@ class WorkerThreadTest : public testing::Test {
 
   void ExpectReportingCallsForWorkerForciblyTerminated() {
     EXPECT_CALL(*reporting_proxy_, DidCreateWorkerGlobalScope(_)).Times(1);
-    EXPECT_CALL(*reporting_proxy_, DidInitializeWorkerContext()).Times(1);
     EXPECT_CALL(*reporting_proxy_, WillEvaluateClassicScriptMock(_, _))
         .Times(1);
     EXPECT_CALL(*reporting_proxy_, DidEvaluateClassicScript(false)).Times(1);
@@ -381,7 +374,6 @@ TEST_F(WorkerThreadTest, Terminate_WhileDebuggerTaskIsRunningOnInitialization) {
   SetForcibleTerminationDelay(kDelay);
 
   EXPECT_CALL(*reporting_proxy_, DidCreateWorkerGlobalScope(_)).Times(1);
-  EXPECT_CALL(*reporting_proxy_, DidInitializeWorkerContext()).Times(1);
   EXPECT_CALL(*reporting_proxy_, WillDestroyWorkerGlobalScope()).Times(1);
   EXPECT_CALL(*reporting_proxy_, DidTerminateWorkerThread()).Times(1);
 
