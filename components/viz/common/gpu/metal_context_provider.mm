3608a56f6c3cb0914c9558d35cfd8a031f370621
Christopher Cameron <ccameron@chromium.org>
2019-09-05 21:04:25 +0000

    Use WaitableEvent instead of ConditionVariable for Metal test shader
    Bug: 974219
    Change-Id: I49f3e30ccad376dff72e51ec836185e390d1dee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787559
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693931}

--- components/viz/common/gpu/metal_context_provider.mm
+++ components/viz/common/gpu/metal_context_provider.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_nsobject.h"
 #include "base/memory/ref_counted.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/synchronization/condition_variable.h"
+#include "base/synchronization/waitable_event.h"
 #include "base/time/time.h"
 #include "components/viz/common/gpu/metal_api_proxy.h"
 #include "third_party/skia/include/gpu/GrContext.h"
@@ -54,11 +54,8 @@ constexpr base::TimeDelta kTestShaderCompileTimeout =
 class API_AVAILABLE(macos(10.11)) TestShaderState
     : public base::RefCountedThreadSafe<TestShaderState> {
  public:
-  TestShaderState() : condition_variable(&lock) {}
-
-  base::Lock lock;
-  base::ConditionVariable condition_variable;
-  bool compile_has_completed = false;
+  TestShaderState() {}
+  base::WaitableEvent event;
 
  protected:
   friend class base::RefCountedThreadSafe<TestShaderState>;
@@ -77,17 +74,14 @@ bool CompileTestShader(id<MTLDevice> device) API_AVAILABLE(macos(10.11)) {
       [[MTLCompileOptions alloc] init]);
   MTLNewLibraryCompletionHandler completion_handler =
       ^(id<MTLLibrary> library, NSError* error) {
-        base::AutoLock lock(state->lock);
-        state->compile_has_completed = true;
-        state->condition_variable.Signal();
+        DCHECK(library);
+        state->event.Signal();
       };
   [device newLibraryWithSource:source
                        options:options
              completionHandler:completion_handler];
 
-  base::AutoLock lock(state->lock);
-  state->condition_variable.TimedWait(kTestShaderCompileTimeout);
-  return state->compile_has_completed;
+  return state->event.TimedWait(kTestShaderCompileTimeout);
 }
 
 struct API_AVAILABLE(macos(10.11)) MetalContextProviderImpl
