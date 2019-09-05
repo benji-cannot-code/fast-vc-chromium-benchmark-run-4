66de7dbf665612d056551eba111420d1e331c35c
Gauthier Ambard <gambard@chromium.org>
2019-09-05 08:22:35 +0000

    [iOS] Stop ReadingListCoordinator when clearing state
    When the BrowserCoordinator is clearing the presented state, the
    coordinators which might be presenting a ViewController should be
    dismissed.
    This is a first CL doing it for the ReadingListCoordinator.
    
    Bug: 988909
    Change-Id: I912db5c5e19734ea88423159de159938154c3034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768324
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693593}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -188,6 +188,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self.printController dismissAnimated:YES];
 
+  [self.readingListCoordinator stop];
+  self.readingListCoordinator = nil;
+
   [self.viewController clearPresentedStateWithCompletion:completion
                                           dismissOmnibox:dismissOmnibox];
 }
