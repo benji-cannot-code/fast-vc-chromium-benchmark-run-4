9796fdc3ebd1479bc05b7cba249639fb93611bf9
liberato@chromium.org <liberato@chromium.org>
2019-09-05 23:51:32 +0000

    Add ExperimentHelper and integrate with WMPI.
    ExperimentHelper lets us factor out some of the redundant code when
    adding an experiment.  It keeps track of in-flight observations, so
    that they can be completed or cancelled by the client code, and
    also handles the case where the experiment is turned off.
    
    This CL doesn't actually cause WMPI to run any experiments; it just
    adds calls to an ExperimentHelper at the right times.
    
    Change-Id: Ic07ac28374252df58aef95dd55c0f9875f1e8c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1775267
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694011}

--- media/learning/common/value.h
+++ media/learning/common/value.h
@@ -37,6 +37,7 @@ class COMPONENT_EXPORT(LEARNING_COMMON) Value {
 
   explicit Value(const char* x);
   explicit Value(const std::string& x);
+  explicit Value(bool x);
 
   Value(const Value& other);
   Value(Value&&) noexcept;
