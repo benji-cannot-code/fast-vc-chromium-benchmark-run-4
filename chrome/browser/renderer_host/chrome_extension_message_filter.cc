6cba15399f60d60440b3f79d1941f243cbc3c522
Sami Kyostila <skyostil@chromium.org>
2019-09-05 13:47:58 +0000

    base: Remove temporary "WithTraits" task API wrappers
    Now that everyone has been migrated to the renamed
    PostTask/CreateTaskRunner APIs, we can remove these temporary wrappers.
    
    TBR=gab@chromium.org
    (to apply side-effects of mechanical change in //base)
    
    Bug: 968047
    Change-Id: I69c53abd899f37586f23ec295ffcc9bf12042c4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1752787
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Auto-Submit: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693695}

--- chrome/browser/renderer_host/chrome_extension_message_filter.cc
+++ chrome/browser/renderer_host/chrome_extension_message_filter.cc
@@ -164,7 +164,7 @@ void ChromeExtensionMessageFilter::OnGetExtMessageBundle(
   }
 
   // This blocks tab loading. Priority is inherited from the calling context.
-  base::PostTaskWithTraits(
+  base::PostTask(
       FROM_HERE, {base::ThreadPool(), base::MayBlock()},
       base::BindOnce(&ChromeExtensionMessageFilter::OnGetExtMessageBundleAsync,
                      this, paths_to_load, extension_id, default_locale,
