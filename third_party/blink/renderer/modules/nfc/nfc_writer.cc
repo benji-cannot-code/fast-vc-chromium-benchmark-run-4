817570ecb0528a6e1cc42185001fee582327131b
Leon Han <leon.han@intel.com>
2019-09-05 10:41:48 +0000

    Reland "[webnfc] Refactor/Refine impl for "Creating Web NFC message" algorithm"
    This is a reland of 581b273e3743fcbd168775edfeebae8b092fc706
    
    Original change's description:
    > [webnfc] Refactor/Refine impl for "Creating Web NFC message" algorithm
    >
    > This CL implements strictly the "Creating Web NFC message" algorithm
    > described at http://w3c.github.io/web-nfc/#creating-web-nfc-message.
    >
    > 1.
    > Changes the Web IDL definition for NDEFRecordData from
    > "typedef (DOMString or unrestricted double or ArrayBuffer or Dictionary) NDEFRecordData;"
    > to
    > "typedef any NDEFRecordData;"
    >
    > 2.
    > Adds verification of NDEFRecordInit when creating NDEFRecord, i.e. makes
    > NDEFRecord ctor throw exceptions in case of invalid NDEFRecordInit.
    > Also, the same thing for NDEFMessage and NFCReadingEvent.
    >
    > 3.
    > Changes code flow for Step 10.9 of "9.10.1 The push() method" described
    > at https://w3c.github.io/web-nfc/#the-push-method.
    > "
    > Let output be the notation for the NDEF message to be created by UA, as
    > the result of passing message to create Web NFC message. If this throws
    > an exception, reject p with that exception and abort these steps.
    > "
    > Previously,
    >   NDEFMessageSource (NDEFMessageInit) -Validity check->
    >   -Mojo TypeConverter-> Mojom NDEFMessagePtr
    > Now,
    >   Remove "Validity check" and "Mojo TypeConverter" mentioned above,
    >   instead, we reuse the process of creating NDEFMessage from
    >   NDEFMessageSource (NDEFMessageInit), then the next step converting
    >   NDEFMessage to Mojom NDEFMessagePtr is quite an easy job.
    >
    > BUG=520391
    >
    > Change-Id: I628981e556f89ffdd0f883da0cfa99b20a6f8300
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767719
    > Commit-Queue: Leon Han <leon.han@intel.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    > Cr-Commit-Position: refs/heads/master@{#693539}
    
    Bug: 520391,923974
    TBR: dcheng@,reillyg@
    Change-Id: I608e28638c0909c9d4f675774514cd990f9878c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787557
    Commit-Queue: Leon Han <leon.han@intel.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693638}

--- third_party/blink/renderer/modules/nfc/nfc_writer.cc
+++ third_party/blink/renderer/modules/nfc/nfc_writer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/callback_helpers.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/bindings/modules/v8/string_or_array_buffer_or_ndef_message_init.h"
 #include "third_party/blink/renderer/core/dom/abort_signal.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/nfc/nfc_push_options.h"
@@ -36,7 +37,8 @@ void NFCWriter::Trace(blink::Visitor* visitor) {
 // https://w3c.github.io/web-nfc/#the-push-method
 ScriptPromise NFCWriter::push(ScriptState* script_state,
                               const NDEFMessageSource& push_message,
-                              const NFCPushOptions* options) {
+                              const NFCPushOptions* options,
+                              ExceptionState& exception_state) {
   ExecutionContext* execution_context = GetExecutionContext();
   // https://w3c.github.io/web-nfc/#security-policies
   // WebNFC API must be only accessible from top level browsing context.
@@ -55,11 +57,6 @@ ScriptPromise NFCWriter::push(ScriptState* script_state,
                           DOMExceptionCode::kAbortError, kNfcCancelled));
   }
 
-  ScriptPromise is_valid_message =
-      RejectIfInvalidNDEFMessageSource(script_state, push_message);
-  if (!is_valid_message.IsEmpty())
-    return is_valid_message;
-
   // 9. If timeout value is NaN or negative, reject promise with "TypeError"
   // and abort these steps.
   if (options->hasTimeout() &&
@@ -69,12 +66,23 @@ ScriptPromise NFCWriter::push(ScriptState* script_state,
                           script_state->GetIsolate(), kNfcInvalidPushTimeout));
   }
 
-  auto message = device::mojom::blink::NDEFMessage::From(push_message);
-  if (!message) {
-    return ScriptPromise::RejectWithDOMException(
-        script_state, MakeGarbageCollected<DOMException>(
-                          DOMExceptionCode::kSyntaxError, kNfcMsgConvertError));
+  // Step 10.8: Run "create Web NFC message", if this throws an exception,
+  // reject p with that exception and abort these steps.
+  NDEFMessage* ndef_message =
+      NDEFMessage::Create(push_message, exception_state);
+  if (exception_state.HadException()) {
+    return ScriptPromise();
   }
+
+  // If NDEFMessage.records is empty, reject promise with TypeError
+  if (ndef_message->records().size() == 0) {
+    return ScriptPromise::Reject(script_state,
+                                 V8ThrowException::CreateTypeError(
+                                     script_state->GetIsolate(), kNfcEmptyMsg));
+  }
+
+  auto message = device::mojom::blink::NDEFMessage::From(ndef_message);
+  DCHECK(message);
 
   if (!SetNDEFMessageURL(execution_context->GetSecurityOrigin()->ToString(),
                          message)) {
