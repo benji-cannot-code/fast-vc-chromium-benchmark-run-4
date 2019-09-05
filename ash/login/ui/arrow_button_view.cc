db7f4f563e1194b6862510d7bf638835d5ee9a95
Aga Wronska <agawronska@chromium.org>
2019-09-05 15:43:29 +0000

    Add disabled state to login arrow button
    Update submit button color in parent access view.
    
    Bug: 999389
    Change-Id: I0db11ec38ec29ab418e12bc126ccf7be3fa8e696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1785999
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693749}

--- ash/login/ui/arrow_button_view.cc
+++ ash/login/ui/arrow_button_view.cc
@@ -15,6 +15,8 @@ namespace {
 
 // Arrow icon size.
 constexpr int kArrowIconSizeDp = 20;
+// An alpha value for disabled button.
+constexpr SkAlpha kButtonDisabledAlpha = 0x80;
 
 }  // namespace
 
@@ -30,6 +32,10 @@ ArrowButtonView::ArrowButtonView(views::ButtonListener* listener, int size)
   SetImage(Button::STATE_NORMAL,
            gfx::CreateVectorIcon(kLockScreenArrowIcon, kArrowIconSizeDp,
                                  SK_ColorWHITE));
+  SetImage(
+      views::Button::STATE_DISABLED,
+      gfx::CreateVectorIcon(kLockScreenArrowIcon, kArrowIconSizeDp,
+                            SkColorSetA(SK_ColorWHITE, kButtonDisabledAlpha)));
 }
 
 ArrowButtonView::~ArrowButtonView() = default;
