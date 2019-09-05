7fc0100368c09ccb8655cfc40b1a0b4f2b9d29f2
tby <tby@chromium.org>
2019-09-05 02:26:02 +0000

    [Files Ranking] Set up Dolphin config deployment
    This adds the logic to decode a JSON version of a Dolphin config proto
    acquired from Finch.
    
    Bug: 959679
    Change-Id: I4b7bb185e15e0400df9c7e123f11a7d127e7bb89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1784237
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Charles . <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693500}

--- chrome/browser/ui/app_list/search/search_result_ranker/search_result_ranker_unittest.cc
+++ chrome/browser/ui/app_list/search/search_result_ranker/search_result_ranker_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/mixer.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/app_launch_data.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/ranking_item_util.h"
+#include "chrome/browser/ui/app_list/search/search_result_ranker/recurrence_predictor.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/recurrence_ranker.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/history/core/browser/history_database_params.h"
@@ -46,6 +47,7 @@ using ResultType = ash::SearchResultType;
 using base::ScopedTempDir;
 using base::test::ScopedFeatureList;
 using testing::ElementsAre;
+using testing::StrEq;
 using testing::UnorderedElementsAre;
 using testing::WhenSorted;
 
@@ -833,6 +835,31 @@ TEST_F(SearchResultRankerTest, ZeroStateLowScoringResultIgnored) {
               WhenSorted(ElementsAre(HasId("A1"), HasId("A2"), HasId("O1"),
                                      HasId("O2"), HasId("Z1"), HasId("Z2"),
                                      HasId("Z3"), HasId("D1"))));
+}
+
+TEST_F(SearchResultRankerTest, ZeroStateGroupRankerUsesFinchConfig) {
+  const std::string json = R"({
+      "min_seconds_between_saves": 250,
+      "target_limit": 100,
+      "target_decay": 0.5,
+      "condition_limit": 50,
+      "condition_decay": 0.7,
+      "predictor": {
+        "predictor_type": "fake"
+      }
+    })";
+
+  EnableOneFeature(app_list_features::kEnableZeroStateMixedTypesRanker,
+                   {{"config", json}});
+
+  auto ranker = MakeRanker();
+  ranker->InitializeRankers();
+  Wait();
+
+  // We expect a FakePredictor to have been loaded because predictor_type is set
+  // to "fake" in the json config.
+  ASSERT_THAT(ranker->zero_state_group_ranker_->GetPredictorNameForTesting(),
+              StrEq(FakePredictor::kPredictorName));
 }
 
 }  // namespace app_list
