4c6f617af30248e0fcdc18a3f391a31c349e1b8c
Lijin Shen <lazzzis@google.com>
2019-09-05 02:59:10 +0000

    Restrict tab switcher long-press menu tests on phone
    This CL makes tests of tab switcher long-press menu only executed on
    mobile phones, because tab switcher only exists on phone.
    
    Bug: 1000040
    Change-Id: I64f8e247500a9e7dd05e88c078b5f14a1b6898b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1785219
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#693509}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuTest.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -30,6 +30,7 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.ui.DummyUiActivityTestCase;
 import org.chromium.chrome.test.util.browser.Features;
+import org.chromium.ui.test.util.UiRestriction;
 
 /**
  * Instrumentation tests for tab switcher long-press menu popup
@@ -38,6 +39,7 @@ import org.chromium.chrome.test.util.browser.Features;
 // clang-format off
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @Features.EnableFeatures(ChromeFeatureList.TAB_SWITCHER_LONGPRESS_MENU)
+@Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
 public class TabSwitcherActionMenuTest extends DummyUiActivityTestCase {
     // clang-format on
     @Rule
@@ -50,7 +52,6 @@ public class TabSwitcherActionMenuTest extends DummyUiActivityTestCase {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1000040")
     public void testCloseTab() {
         int tabCount = mActivityTestRule.getActivity().getCurrentTabModel().getCount();
         onView(withId(R.id.tab_switcher_button)).perform(longClick());
@@ -64,7 +65,6 @@ public class TabSwitcherActionMenuTest extends DummyUiActivityTestCase {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1000040")
     public void testOpenNewTab() {
         int tabCount = mActivityTestRule.getActivity().getCurrentTabModel().getCount();
         onView(withId(R.id.tab_switcher_button)).perform(longClick());
@@ -78,7 +78,6 @@ public class TabSwitcherActionMenuTest extends DummyUiActivityTestCase {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "https://crbug.com/1000040")
     public void testOpenNewIncognitoTab() {
         onView(withId(R.id.tab_switcher_button)).perform(longClick());
 
