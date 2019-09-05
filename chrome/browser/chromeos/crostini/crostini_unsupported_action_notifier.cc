da9f8010a1e575d3342f207127848f97534431ce
David Munro <davidmunro@google.com>
2019-09-05 07:15:37 +0000

    Crostini: Change requested by accessibility review
    Keep the unsupported action toast on screen for longer (10 seconds)
    if a screen magnifier is enabled, 5 seconds otherwise
    
    BUG=chromium:996945
    TEST=manual
    
    Change-Id: I3f78f4110aaa6b3139cec143acfab059904b84a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786883
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/master@{#693540}

--- chrome/browser/chromeos/crostini/crostini_unsupported_action_notifier.cc
+++ chrome/browser/chromeos/crostini/crostini_unsupported_action_notifier.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/chromeos/accessibility/magnification_manager.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/exo/wm_helper.h"
 #include "ui/aura/client/aura_constants.h"
@@ -111,11 +112,12 @@ void CrostiniUnsupportedActionNotifier::
   }
   EmitMetricReasonTriggered(reason);
   if (!virtual_keyboard_unsupported_message_shown_) {
+    int duration_s = delegate_->IsMagnificationEnabled() ? 10 : 5;
     ash::ToastData data = {
         /*id=*/"VKUnsupportedInCrostini",
         /*text=*/
         l10n_util::GetStringUTF16(IDS_CROSTINI_UNSUPPORTED_VIRTUAL_KEYBOARD),
-        /*timeout_ms=*/5000,
+        /*timeout_ms=*/duration_s * 1000,
         /*dismiss_text=*/base::nullopt};
     delegate_->ShowToast(data);
     virtual_keyboard_unsupported_message_shown_ = true;
@@ -134,11 +136,12 @@ void CrostiniUnsupportedActionNotifier::
   if (!ime_unsupported_message_shown_) {
     auto ime_name =
         base::UTF8ToUTF16(delegate_->GetLocalizedDisplayName(method));
+    int duration_s = delegate_->IsMagnificationEnabled() ? 10 : 5;
     ash::ToastData data = {
         /*id=*/"IMEUnsupportedInCrostini",
         /*text=*/
         l10n_util::GetStringFUTF16(IDS_CROSTINI_UNSUPPORTED_IME, ime_name),
-        /*timeout_ms=*/5000,
+        /*timeout_ms=*/duration_s * 1000,
         /*dismiss_text=*/base::nullopt};
     delegate_->ShowToast(data);
     ime_unsupported_message_shown_ = true;
@@ -186,6 +189,12 @@ CrostiniUnsupportedActionNotifier::Delegate::GetLocalizedDisplayName(
   return chromeos::input_method::InputMethodManager::Get()
       ->GetInputMethodUtil()
       ->GetLocalizedDisplayName(descriptor);
+}
+
+bool CrostiniUnsupportedActionNotifier::Delegate::IsMagnificationEnabled() {
+  auto* manager = chromeos::MagnificationManager::Get();
+  return manager &&
+         (manager->IsMagnifierEnabled() || manager->IsDockedMagnifierEnabled());
 }
 
 void CrostiniUnsupportedActionNotifier::Delegate::AddFocusObserver(
