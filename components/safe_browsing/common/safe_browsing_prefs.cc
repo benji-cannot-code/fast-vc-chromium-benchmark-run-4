6cdcaadf93433c12b67233f908ef1f7f2cee3c11
Marc-André (MAD) Decoste <mad@chromium.org>
2019-09-05 17:28:12 +0000

    Register the DomainsToCheckComplianceOfDownloadedContent policy pref
    Change-Id: Ia7ae936644266d98abe9be88341e81fb3fc2eb96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786138
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Marc-André Decoste <mad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693825}

--- components/safe_browsing/common/safe_browsing_prefs.cc
+++ components/safe_browsing/common/safe_browsing_prefs.cc
@@ -201,6 +201,8 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
       prefs::kAllowPasswordProtectedFiles,
       AllowPasswordProtectedFilesValues::ALLOW_UPLOADS_AND_DOWNLOADS);
   registry->RegisterIntegerPref(prefs::kCheckContentCompliance, 0);
+  registry->RegisterListPref(
+      prefs::kDomainsToCheckComplianceOfDownloadedContent);
 }
 
 void SetExtendedReportingPrefAndMetric(
