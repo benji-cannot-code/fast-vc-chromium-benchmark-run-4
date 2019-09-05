8bf2adc9d709df9c358741eafced75c14095aed9
Gabriel Charette <gab@chromium.org>
2019-09-05 13:52:42 +0000

    Backport some tests in /components/policy to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /components/policy.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=pastarmovj@chromium.org
    
    Bug: 891670
    Change-Id: I47db4c4af8e3eb339e6ed231f6c666a2c79d6a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786938
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693698}

--- components/policy/core/common/cloud/user_cloud_policy_store_unittest.cc
+++ components/policy/core/common/cloud/user_cloud_policy_store_unittest.cc
@@ -56,7 +56,8 @@ bool WriteStringToFile(const base::FilePath path, const std::string& data) {
 class UserCloudPolicyStoreTest : public testing::Test {
  public:
   UserCloudPolicyStoreTest()
-      : task_environment_(base::test::TaskEnvironment::MainThreadType::UI) {}
+      : task_environment_(
+            base::test::SingleThreadTaskEnvironment::MainThreadType::UI) {}
 
   void SetUp() override {
     ASSERT_TRUE(tmp_dir_.CreateUniqueTempDir());
@@ -135,7 +136,7 @@ class UserCloudPolicyStoreTest : public testing::Test {
   std::unique_ptr<UserCloudPolicyStore> store_;
   std::unique_ptr<MockCloudExternalDataManager> external_data_manager_;
 
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 
   base::ScopedTempDir tmp_dir_;
 
