a3154a9783969080ed06dc442a3b52ea0e6da897
Gabriel Charette <gab@chromium.org>
2019-09-05 17:32:35 +0000

    Reland "Backport some tests in /remoting to SingleThreadTaskEnvironment"
    This reverts commit 85d4a0fa5ae2afcd3d6c578a1465a23d74eb334a.
    
    Reason for revert: most likely wrong culprit?
    
    Original change's description:
    > Revert "Backport some tests in /remoting to SingleThreadTaskEnvironment"
    > 
    > This reverts commit afa2cbc718a5063e2d797c30a63935b1e35bcfaf.
    > 
    > Reason for revert: non_viz_browser_tests failed because of:
    > OAuth2Test.SetInvalidTokenStatus
    > 
    > https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-google-rel/6325
    > 
    > Original change's description:
    > > Backport some tests in /remoting to SingleThreadTaskEnvironment
    > > 
    > > These tests were previously migrated from single-threaded MessageLoop to
    > > a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    > > part of crbug.com/891670.
    > > 
    > > //base OWNERS decided in retrospect that it was better to keep a
    > > single-threaded option for TaskEnvironment and introduced
    > > SingleThreadTaskEnvironment. This CL retrofits that decision for
    > > /remoting.
    > > 
    > > This CL is a no-op if it passes CQ.
    > > 
    > > This CL was uploaded by git cl split.
    > > 
    > > R=​jamiewalch@chromium.org
    > > 
    > > Bug: 891670
    > > Change-Id: Id50cc3412d6fbec3e5c71672bf74f6abf0f7332d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786679
    > > Auto-Submit: Gabriel Charette <gab@chromium.org>
    > > Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    > > Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#693548}
    > 
    > TBR=jamiewalch@chromium.org,lambroslambrou@chromium.org,gab@chromium.org
    > 
    > Change-Id: Ibe96ffa6ddf39371dc64c6ad7236ec366f9331c4
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 891670
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787279
    > Reviewed-by: Olga Sharonova <olka@chromium.org>
    > Commit-Queue: Olga Sharonova <olka@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#693613}
    
    TBR=jamiewalch@chromium.org,lambroslambrou@chromium.org,gab@chromium.org,olka@chromium.org
    
    Change-Id: Idb489a187fcfb8343079c83f66088c6b6c8b5091
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 891670
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787184
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693829}

--- remoting/client/queued_task_poster_unittest.cc
+++ remoting/client/queued_task_poster_unittest.cc
@@ -43,7 +43,7 @@ class QueuedTaskPosterTest : public testing::Test {
   void AssertSequenceNotStarted();
 
   base::Thread target_thread_;
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   bool sequence_started_ = false;
 };
 
