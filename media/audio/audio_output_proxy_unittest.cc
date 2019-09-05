351758d1e2b11d2d9bf0728fe6fdbd296b188a41
Gabriel Charette <gab@chromium.org>
2019-09-05 08:44:49 +0000

    Backport some tests in /media/audio to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /media/audio.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=tommi@chromium.org
    
    Bug: 891670
    Change-Id: I9bff22af41a71e1fd0483d76412748a38433da6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787199
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Commit-Queue: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693603}

--- media/audio/audio_output_proxy_unittest.cc
+++ media/audio/audio_output_proxy_unittest.cc
@@ -489,7 +489,7 @@ class AudioOutputProxyTest : public testing::Test {
     proxy->Close();
   }
 
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   MockAudioManager manager_;
   std::unique_ptr<AudioOutputDispatcherImpl> dispatcher_impl_;
   MockAudioSourceCallback callback_;
