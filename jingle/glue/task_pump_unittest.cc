f5b72659b6ea3415299a2c444da7ebb208887596
Gabriel Charette <gab@chromium.org>
2019-09-05 07:03:28 +0000

    Backport some tests in /jingle to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /jingle.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=sergeyu@chromium.org
    
    Bug: 891670
    Change-Id: I66d5be191e3c335b0f584b011c78956a234fc08b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786950
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693519}

--- jingle/glue/task_pump_unittest.cc
+++ jingle/glue/task_pump_unittest.cc
@@ -18,7 +18,7 @@ using ::testing::Return;
 
 class TaskPumpTest : public testing::Test {
  private:
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 };
 
 TEST_F(TaskPumpTest, Basic) {
