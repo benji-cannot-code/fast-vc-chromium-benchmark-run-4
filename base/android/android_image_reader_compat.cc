466cba534f78710bd34d5d8df2fd4e96efb62830
Reid Kleckner <rnk@google.com>
2019-09-05 02:02:45 +0000

    Revert "Work around bug in clang coverage with a helper function"
    This reverts commit 0a88a4c2b7d06882a41eb38bbd3af7421a267dd6.
    
    Reason for revert: This work around is no longer necessary since https://llvm.org/PR39942 was fixed upstream in clang r367337 from July 30. We should've picked it up in the last clang roll.
    
    Original change's description:
    > Work around bug in clang coverage with a helper function
    > 
    > Clang code coverage asserts on the following code pattern when
    > targetting Android:
    > 
    >   #define LOAD_FUNCTION(lib, func)                             \
    >     do {                                                       \
    >       func##Fn = reinterpret_cast<p##func>(dlsym(lib, #func)); \
    >       if (!func##Fn) {                                         \
    >         supported = false;                                     \
    >         LOG(ERROR) << "Unable to load function " << #func;     \
    >       }                                                        \
    >     } while (0)
    > 
    > The important thing in this snippet is the use of the base/logging.h
    > macros inside another macro. Clang code coverage expects source
    > locations to appear in ascending order, and somehow with these multiple
    > levels of macro expansion, that invariant is broken. The upstream bug is
    > https://llvm.org/pr39942. However, it's not making progress, so as a
    > workaround, wrap the logging in a plain function.
    > 
    > Test: Compile libchrome with use_clang_coverage=true for Android, build
    > succeeds.
    > 
    > R=​backer@chromium.org, liaoyuke@chromium.org, rmcilroy@chromium.org
    > 
    > Bug: 843356
    > Change-Id: I6a729adec482fd8a2903c80d919b7e0c5e6dd267
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1490740
    > Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    > Reviewed-by: Jonathan Backer <backer@chromium.org>
    > Reviewed-by: Yuke Liao <liaoyuke@chromium.org>
    > Commit-Queue: Reid Kleckner <rnk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#639124}
    
    TBR=backer@chromium.org,rmcilroy@chromium.org,rnk@chromium.org,liaoyuke@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 843356
    Change-Id: Iae3321f9d6cb9ca4724d62ba5257c0a5740daf5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786245
    Reviewed-by: Reid Kleckner <rnk@chromium.org>
    Commit-Queue: Reid Kleckner <rnk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693493}

--- base/android/android_image_reader_compat.cc
+++ base/android/android_image_reader_compat.cc
@@ -11,17 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/logging.h"
 
-// Helper function to log errors from dlsym. Calling DLOG(ERROR) inside a macro
-// crashes clang code coverage. https://crbug.com/843356
-static void LogDlsymError(const char* func) {
-  DLOG(ERROR) << "Unable to load function " << func;
-}
-
 #define LOAD_FUNCTION(lib, func)                            \
   do {                                                      \
     func##_ = reinterpret_cast<p##func>(dlsym(lib, #func)); \
     if (!func##_) {                                         \
-      LogDlsymError(#func);                                 \
+      DLOG(ERROR) << "Unable to load function " << #func;   \
       return false;                                         \
     }                                                       \
   } while (0)
