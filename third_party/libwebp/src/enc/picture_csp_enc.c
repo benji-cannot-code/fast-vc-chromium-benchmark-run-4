08b757152e916c9e419ab7efcf5350742522f3d1
Nico Weber <thakis@chromium.org>
2019-09-05 22:37:11 +0000

    Fix a Wxor-used-as-pow false positive
    Since people seem to write "2 ^ X" hoping that it means "1 << X", clang
    recently added a warning for this pattern.
    
    It incorrectly fires on this file. To suppress it, restructure the code
    to be less clever. (Alternatively we could use "xor" instead of "^" or
    write "0x2" instead of "2" but both seem worse.)
    
    No intended behavior change.
    
    Bug: chromium:995200
    Change-Id: I64744345be5f5a8cd1f4aaeaf0982da239b378a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1784158
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/master@{#693964}

--- third_party/libwebp/src/enc/picture_csp_enc.c
+++ third_party/libwebp/src/enc/picture_csp_enc.c
@@ -30,10 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define USE_INVERSE_ALPHA_TABLE
 
 #ifdef WORDS_BIGENDIAN
-#define ALPHA_OFFSET 0   // uint32_t 0xff000000 is 0xff,00,00,00 in memory
+// uint32_t 0xff000000 is 0xff,00,00,00 in memory
+#define CHANNEL_OFFSET(i) (i)
 #else
-#define ALPHA_OFFSET 3   // uint32_t 0xff000000 is 0x00,00,00,ff in memory
+// uint32_t 0xff000000 is 0x00,00,00,ff in memory
+#define CHANNEL_OFFSET(i) (3-(i))
 #endif
+
+#define ALPHA_OFFSET CHANNEL_OFFSET(0)
 
 //------------------------------------------------------------------------------
 // Detection of non-trivial transparency
@@ -998,10 +1002,10 @@ static int PictureARGBToYUVA(WebPPicture* picture, WebPEncCSP colorspace,
     return WebPEncodingSetError(picture, VP8_ENC_ERROR_INVALID_CONFIGURATION);
   } else {
     const uint8_t* const argb = (const uint8_t*)picture->argb;
-    const uint8_t* const a = argb + (0 ^ ALPHA_OFFSET);
-    const uint8_t* const r = argb + (1 ^ ALPHA_OFFSET);
-    const uint8_t* const g = argb + (2 ^ ALPHA_OFFSET);
-    const uint8_t* const b = argb + (3 ^ ALPHA_OFFSET);
+    const uint8_t* const a = argb + CHANNEL_OFFSET(0);
+    const uint8_t* const r = argb + CHANNEL_OFFSET(1);
+    const uint8_t* const g = argb + CHANNEL_OFFSET(2);
+    const uint8_t* const b = argb + CHANNEL_OFFSET(3);
 
     picture->colorspace = WEBP_YUV420;
     return ImportYUVAFromRGBA(r, g, b, a, 4, 4 * picture->argb_stride,
