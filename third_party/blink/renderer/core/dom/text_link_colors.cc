fe93d5892481d0d07253e42ba0b2174e2998d9c3
Rune Lillesveen <futhark@chromium.org>
2019-09-05 14:14:07 +0000

    Implement -internal-light-dark-color().
    To support different color values for form controls in UA stylesheets
    based on color-scheme, we need a way to provide a way to specify that.
    This CL introduces a new color function only available in UA stylesheets
    to do that.
    
    Design document:
    
    https://docs.google.com/document/d/1RRyQSdohsl8S9kQIdC6rYwwVy6cY15gjkGMgyadTORg
    
    Bug: 939811
    Change-Id: I9c68d0bf0dea1eb6c5bd6c3b07d677e57779b6ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1782158
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693710}

--- third_party/blink/renderer/core/dom/text_link_colors.cc
+++ third_party/blink/renderer/core/dom/text_link_colors.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/css/css_color_value.h"
 #include "third_party/blink/renderer/core/css/css_identifier_value.h"
+#include "third_party/blink/renderer/core/css/css_value_pair.h"
 #include "third_party/blink/renderer/core/css/style_color.h"
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -64,6 +65,12 @@ Color TextLinkColors::ColorFromCSSValue(const CSSValue& value,
                                         bool for_visited_link) const {
   if (auto* color_value = DynamicTo<CSSColorValue>(value))
     return color_value->Value();
+
+  if (auto* pair = DynamicTo<CSSValuePair>(value)) {
+    const CSSColorValue& color_value = To<CSSColorValue>(
+        WebColorScheme::kLight ? pair->First() : pair->Second());
+    return color_value.Value();
+  }
 
   CSSValueID value_id = To<CSSIdentifierValue>(value).GetValueID();
   switch (value_id) {
