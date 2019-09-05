615f2b5b17e3d7b97de885ea29cde096ae3c7d9d
Chris Lu <thegreenfrog@chromium.org>
2019-09-05 17:04:58 +0000

    [ios] Remove Infobar feature flag check for badges logic
    Now that Badges logic will be used when in Incognito, it should not be
    tied to the Infobar feature flag, which won't be rolled out to 100%
    soon.
    
    Bug: 976901
    Change-Id: I94ece69646370de01672ba10c2ef15aad60e2fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783226
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693800}

--- ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
@@ -167,10 +167,8 @@ const double kHideBadgeViewThreshold = 0.1;
 - (void)viewDidLoad {
   [super viewDidLoad];
 
-  if (IsInfobarUIRebootEnabled()) {
     DCHECK(self.badgeView) << "The badge view must be set at this point";
     self.locationBarSteadyView.badgeView = self.badgeView;
-  }
 
   [_locationBarSteadyView.locationButton
              addTarget:self
@@ -207,11 +205,9 @@ const double kHideBadgeViewThreshold = 0.1;
   CGFloat alphaValue = fmax((progress - 0.85) / 0.15, 0);
   CGFloat scaleValue = 0.79 + 0.21 * progress;
   self.locationBarSteadyView.trailingButton.alpha = alphaValue;
-  if (IsInfobarUIRebootEnabled()) {
     BOOL badgeViewShouldCollapse = progress <= kHideBadgeViewThreshold;
     [self.locationBarSteadyView
         setFullScreenCollapsedMode:badgeViewShouldCollapse];
-  }
   self.locationBarSteadyView.transform =
       CGAffineTransformMakeScale(scaleValue, scaleValue);
 }
@@ -294,15 +290,11 @@ const double kHideBadgeViewThreshold = 0.1;
 }
 
 - (void)hideSteadyViewBadgeView {
-  if (IsInfobarUIRebootEnabled()) {
     [self.locationBarSteadyView displayBadgeView:NO animated:NO];
-  }
 }
 
 - (void)showSteadyViewBadgeView {
-  if (IsInfobarUIRebootEnabled()) {
     [self.locationBarSteadyView displayBadgeView:YES animated:NO];
-  }
 }
 
 - (void)setEditViewFaded:(BOOL)hidden {
