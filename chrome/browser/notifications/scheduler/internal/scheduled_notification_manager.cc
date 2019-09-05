b09b244c5c9ad901b0ec7768cfa1b4e6f347f13f
Xing Liu <xingliu@chromium.org>
2019-09-05 17:48:46 +0000

    Reland "Notification scheduler: Add icon database metrics."
    This reverts commit e5bdbac6b6db9da1eac9c3b309fda0e8a4ba616a.
    
    Reason for revert: Wrong revert, test added here is not failing. The other CL introduced a flaky, which is reverted in:
    https://chromium-review.googlesource.com/c/chromium/src/+/1787277
    
    Original change's description:
    > Revert "Notification scheduler: Add icon database metrics."
    > 
    > This reverts commit 63c7234ee901d4ec9e9b8647c9131737bb963907.
    > 
    > Reason for revert: Unit test failure (crbug.com/1001018)
    > 
    > Original change's description:
    > > Notification scheduler: Add icon database metrics.
    > > 
    > > This CL adds icon database metrics, also fixes a typo in
    > > stats::LogImpressionEvent.
    > > 
    > > Bug: 963298
    > > Change-Id: I2fcd44d773166345760ee647c26e464c9ca0f89e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1785161
    > > Commit-Queue: Xing Liu <xingliu@chromium.org>
    > > Reviewed-by: Hesen Zhang <hesen@chromium.org>
    > > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#693510}
    > 
    > TBR=isherman@chromium.org,dtrainor@chromium.org,xingliu@chromium.org,hesen@chromium.org
    > 
    > Change-Id: I94b298d9754e4651f757de2ba82a369be6523801
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 963298
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786372
    > Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    > Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#693607}
    
    TBR=peria@chromium.org,isherman@chromium.org,dtrainor@chromium.org,xingliu@chromium.org,hesen@chromium.org
    
    Change-Id: I7db11edabd91c9a8265b7f8961054def38f804ab
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 963298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786644
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693842}

--- chrome/browser/notifications/scheduler/internal/scheduled_notification_manager.cc
+++ chrome/browser/notifications/scheduler/internal/scheduled_notification_manager.cc
@@ -200,6 +200,9 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
   }
 
   void OnIconStoreInitialized(InitCallback callback, bool success) {
+    // TODO(xingliu): Load icon store keys and count the number of records.
+    // Delete icons without notification entry associated.
+    stats::LogDbInit(stats::DatabaseType::kIconDb, success, 0 /*entry_count*/);
     if (!success) {
       std::move(callback).Run(false);
       return;
@@ -214,7 +217,8 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
       InitCallback callback,
       bool success,
       CollectionStore<NotificationEntry>::Entries entries) {
-    stats::LogNotificationDbInit(success, entries.size());
+    stats::LogDbInit(stats::DatabaseType::kNotificationDb, success,
+                     entries.size());
 
     if (!success) {
       std::move(callback).Run(false);
@@ -251,7 +255,10 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
     for (const auto& icon_id : entry.icons_uuid) {
       icons_to_delete.emplace_back(icon_id.second);
     }
-    icon_store_->DeleteIcons(std::move(icons_to_delete), base::DoNothing());
+    icon_store_->DeleteIcons(
+        std::move(icons_to_delete),
+        base::BindOnce(&ScheduledNotificationManagerImpl::OnIconDeleted,
+                       weak_ptr_factory_.GetWeakPtr()));
 
     // Deletes notification entry.
     notification_store_->Delete(
@@ -264,8 +271,9 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
                     ScheduleCallback schedule_callback,
                     IconStore::IconTypeUuidMap icons_uuid_map,
                     bool success) {
+    stats::LogDbOperation(stats::DatabaseType::kIconDb, success);
+
     if (!success) {
-      // TODO(hesen): Log icon stats.
       std::move(schedule_callback).Run(false);
       return;
     }
@@ -282,7 +290,7 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
   void OnNotificationAdded(std::unique_ptr<NotificationEntry> entry,
                            ScheduleCallback schedule_callback,
                            bool success) {
-    stats::LogNotificationDbOperation(success);
+    stats::LogDbOperation(stats::DatabaseType::kNotificationDb, success);
 
     // Delete the icons when failed to add to notification database.
     if (!success) {
@@ -290,7 +298,10 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
       for (const auto& uuid : entry->icons_uuid) {
         icons_to_delete.emplace_back(uuid.second);
       }
-      icon_store_->DeleteIcons(std::move(icons_to_delete), base::DoNothing());
+      icon_store_->DeleteIcons(
+          std::move(icons_to_delete),
+          base::BindOnce(&ScheduledNotificationManagerImpl::OnIconDeleted,
+                         weak_ptr_factory_.GetWeakPtr()));
       std::move(schedule_callback).Run(false);
       return;
     }
@@ -305,15 +316,19 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
   }
 
   void OnNotificationDeleted(bool success) {
-    stats::LogNotificationDbOperation(success);
+    stats::LogDbOperation(stats::DatabaseType::kNotificationDb, success);
   }
 
-  void OnIconDeleted(bool success) { NOTIMPLEMENTED(); }
+  void OnIconDeleted(bool success) {
+    stats::LogDbOperation(stats::DatabaseType::kIconDb, success);
+  }
 
   void OnIconsLoaded(SchedulerClientType client_type,
                      const std::string& guid,
                      bool success,
                      IconStore::LoadedIconsMap loaded_icons_map) {
+    stats::LogDbOperation(stats::DatabaseType::kIconDb, success);
+
     // TODO(hesen): delete notification entry if icons failed to load.
     if (!success)
       return;
@@ -323,7 +338,10 @@ class ScheduledNotificationManagerImpl : public ScheduledNotificationManager {
     for (const auto& loaded_icon : loaded_icons_map) {
       icons_to_delete.emplace_back(loaded_icon.first);
     }
-    icon_store_->DeleteIcons(std::move(icons_to_delete), base::DoNothing());
+    icon_store_->DeleteIcons(
+        std::move(icons_to_delete),
+        base::BindOnce(&ScheduledNotificationManagerImpl::OnIconDeleted,
+                       weak_ptr_factory_.GetWeakPtr()));
 
     // Can't find the entry.
     if (!FindNotificationEntry(client_type, guid)) {
