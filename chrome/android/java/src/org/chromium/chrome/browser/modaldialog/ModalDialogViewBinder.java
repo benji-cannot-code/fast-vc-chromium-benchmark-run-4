28e019cc65052a25e9998509c964d821b5a1d08a
Mehran Mahmoudi <mahmoudi@google.com>
2019-09-05 01:56:27 +0000

    Add content description to ModalDialogProperties
    This adds support for setting content description for positive and
    negative buttons in a modal dialogs.
    
    A use case for this change is in the AddToHomescreen component. The
    dialog in this component is implemented using AlertDialog at it sets
    content description for the positive dialog button. (Refer to
    AddToHomescreenDialog.java). As part of crbug/994759, I'm porting this
    component to use ModalDialogManager, and thus need to set content
    description for dialog buttons.
    
    Bug: 994759
    Change-Id: I43e0420bf5778eec5a52397007f467905d2dfaf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783340
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#693489}

--- chrome/android/java/src/org/chromium/chrome/browser/modaldialog/ModalDialogViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/modaldialog/ModalDialogViewBinder.java
@@ -33,6 +33,9 @@ public class ModalDialogViewBinder
             assert checkFilterTouchConsistency(model);
             view.setButtonText(ModalDialogProperties.ButtonType.POSITIVE,
                     model.get(ModalDialogProperties.POSITIVE_BUTTON_TEXT));
+        } else if (ModalDialogProperties.POSITIVE_BUTTON_CONTENT_DESCRIPTION == propertyKey) {
+            view.setButtonContentDescription(ModalDialogProperties.ButtonType.POSITIVE,
+                    model.get(ModalDialogProperties.POSITIVE_BUTTON_CONTENT_DESCRIPTION));
         } else if (ModalDialogProperties.POSITIVE_BUTTON_DISABLED == propertyKey) {
             view.setButtonEnabled(ModalDialogProperties.ButtonType.POSITIVE,
                     !model.get(ModalDialogProperties.POSITIVE_BUTTON_DISABLED));
@@ -40,6 +43,9 @@ public class ModalDialogViewBinder
             assert checkFilterTouchConsistency(model);
             view.setButtonText(ModalDialogProperties.ButtonType.NEGATIVE,
                     model.get(ModalDialogProperties.NEGATIVE_BUTTON_TEXT));
+        } else if (ModalDialogProperties.NEGATIVE_BUTTON_CONTENT_DESCRIPTION == propertyKey) {
+            view.setButtonContentDescription(ModalDialogProperties.ButtonType.NEGATIVE,
+                    model.get(ModalDialogProperties.NEGATIVE_BUTTON_CONTENT_DESCRIPTION));
         } else if (ModalDialogProperties.NEGATIVE_BUTTON_DISABLED == propertyKey) {
             view.setButtonEnabled(ModalDialogProperties.ButtonType.NEGATIVE,
                     !model.get(ModalDialogProperties.NEGATIVE_BUTTON_DISABLED));
