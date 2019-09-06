11fb32d5512282026e20e9114253ae5702853257
Ahmed Fakhry <afakhry@google.com>
2019-09-06 21:45:38 +0000

    Docked Magnifier: Fix a crash due to recursion
    Fix a crash observed when the docked magnifier switches
    displays, moving away from a display with a maximized window
    that has a focused text input field, which causes changing
    the old display's work area bound, which in turn used to
    cause a recursive caret bounds change notification into the
    docked magnifier.
    
    BUG=1000903
    TEST=Manually, added a new test.
    
    Change-Id: I8c861a4bf9a0f885ce881825231e5d4727068749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789757
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694431}

--- ash/magnifier/magnifier_test_utils.h
+++ ash/magnifier/magnifier_test_utils.h
@@ -79,6 +79,9 @@ class MagnifierTextInputTestHelper {
 
   void FocusOnTextInputView();
 
+  // Maximizes the widget of |text_input_view_|.
+  void MaximizeWidget();
+
  private:
   ui::InputMethod* GetInputMethod();
 
