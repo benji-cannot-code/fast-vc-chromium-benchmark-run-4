da5f7f518c510a1115c372f0b5b11fd9a6e30b85
Rose Garcia <rosalindag@google.com>
2019-09-06 00:07:50 +0000

    Fix bug on move down and up by line during selection.
    Fixed a bug where move down and up by line would not work properly
    when repeated while making a selection due to needing to reset the
    cursor position first.
    
    Bug: 999400
    Change-Id: I14b9f17e2bb5481d6e95494d8d17316f92287963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786869
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Rose Garcia <rosalindag@google.com>
    Cr-Commit-Position: refs/heads/master@{#694026}

--- chrome/browser/resources/chromeos/switch_access/text_navigation_manager.js
+++ chrome/browser/resources/chromeos/switch_access/text_navigation_manager.js
@@ -121,7 +121,7 @@ class TextNavigationManager {
    */
   moveUpOneLine() {
     if (this.currentlySelecting_)
-      this.setupDynamicSelection_(false /* resetCursor */);
+      this.setupDynamicSelection_(true /* resetCursor */);
     this.navigationManager_.simulateKeyPress(SAConstants.KeyCode.UP_ARROW, {});
   }
 
@@ -133,7 +133,7 @@ class TextNavigationManager {
    */
   moveDownOneLine() {
     if (this.currentlySelecting_)
-      this.setupDynamicSelection_(false /* resetCursor */);
+      this.setupDynamicSelection_(true /* resetCursor */);
     this.navigationManager_.simulateKeyPress(
         SAConstants.KeyCode.DOWN_ARROW, {});
   }
