02177e2a24c4a0c7dfb95d0b42303297729d3ea0
Rune Lillesveen <futhark@chromium.org>
2019-09-06 23:08:13 +0000

    Ensure ancestors for SVGElement::EnsureComputedStyle.
    If UseOverrideComputedStyle() is true, we would just use the the parent
    element's LayoutObject to get a parent style, which could be null. It
    was in the referenced TEST which made the ensured computed style inherit
    from initial styles.
    
    Instead EnsureComputedStyle for the flat tree parent like we do in
    Element::EnsureComputedStyle.
    
    This issue was found trying to add DCHECKs to StyleResolver for when we
    compute style which inherits initial styles.
    
    TEST=svg/animations/target-move-values-crash.html
    
    Bug: 972752
    Change-Id: Iba3def094c488e7868aafcad73a54e0cdcea1a80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789803
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694466}

--- third_party/blink/renderer/core/svg/svg_element.cc
+++ third_party/blink/renderer/core/svg/svg_element.cc
@@ -1067,10 +1067,8 @@ const ComputedStyle* SVGElement::EnsureComputedStyle(
     return Element::EnsureComputedStyle(pseudo_element_specifier);
 
   const ComputedStyle* parent_style = nullptr;
-  if (Element* parent = ParentOrShadowHostElement()) {
-    if (LayoutObject* layout_object = parent->GetLayoutObject())
-      parent_style = layout_object->Style();
-  }
+  if (ContainerNode* parent = LayoutTreeBuilderTraversal::Parent(*this))
+    parent_style = parent->EnsureComputedStyle();
 
   return SvgRareData()->OverrideComputedStyle(this, parent_style);
 }
