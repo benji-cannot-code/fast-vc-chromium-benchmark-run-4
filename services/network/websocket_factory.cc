00a89cdfabb71b3b44886cc655dc162687465f86
Yutaka Hirano <yhirano@chromium.org>
2019-09-06 05:58:03 +0000

    Fix WebSocket double deletion from WebSocketFactory::connections_
    WebSocketFactory::Remove can be called twice with the same WebSocket
    pointer because WebSocket tries to send a close frame in the destructor
    in some cases, and that can be failed. Fix that.
    
    Bug: 999235
    Change-Id: I92e3a9811c343c4c5c48c53c86dee2bae1b57aec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787763
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694146}

--- services/network/websocket_factory.cc
+++ services/network/websocket_factory.cc
@@ -19,7 +19,11 @@ namespace network {
 WebSocketFactory::WebSocketFactory(NetworkContext* context)
     : context_(context) {}
 
-WebSocketFactory::~WebSocketFactory() {}
+WebSocketFactory::~WebSocketFactory() {
+  // Subtle: This is important to avoid WebSocketFactory::Remove calls during
+  // |connections_| destruction.
+  connections_.clear();
+}
 
 void WebSocketFactory::CreateWebSocket(
     const GURL& url,
@@ -73,7 +77,11 @@ void WebSocketFactory::OnSSLCertificateError(
 
 void WebSocketFactory::Remove(WebSocket* impl) {
   auto it = connections_.find(impl);
-  DCHECK(it != connections_.end());
+  if (it == connections_.end()) {
+    // This is possible when this function is called inside the WebSocket
+    // destructor.
+    return;
+  }
   connections_.erase(it);
 }
 
