ad70dcf621676ba509c0e9e29f8f1e0cbe9c5f44
Peter Kasting <pkasting@chromium.org>
2019-09-06 00:52:16 +0000

    Restore popup windows.
    There's a long history here; see crbug.com/18862 and crosbug.com/11152 .  In
    short, we first restored popups, then we stopped doing it because sometimes
    that's not what you want, then we thought about putting it back and decided to
    do it only for CrOS, then we turned it off on CrOS because it broke app popups.
    
    Some of my own comments regarding the above:
    
    * Bug 18862 comment 4 shows there's some thought that an open popup at exit
      could be intentional; bug 41055 comment 7 gives a sample such case.  Bug 18862
      comment 9 questions whether this is the default, and it's not clear to me
      which case is more common; that said, I'm concerned about dataloss risk
      regardless.  I think if these are even sometimes what the user wants, we
      should err on the side of restoring.
    * Bug 18862 comment 9 asks about closing a tabbed browser and then a popup, and
      asks what you'd expect to happen.  With this patch, the behavior on launch is
      to reopen the popup and open a new browser with a New Tab; this seems like the
      most reasonable behavior to me (restoring the previous tab would be
      inconsistent with how we otherwise handle sequential window closer, and not
      opening a tabbed browser at all prevents the user from normal use).
    * CrOS bug 11152 notes app popups breaking.  There now seems to be code in place
      specifically to prevent this; see the is_trusted_source() checks for TYPE_APP
      in SessionService::ShouldTrackBrowser() and
      chrome::ShouldSaveWindowPlacement().  I'm assuming these checks address this
      issue, as they were added in 2014, years after this bug was fixed.
    
    Along the way, I discovered that non-tabbed glass frame windows were erroneously
    using the 5 DIP "above tabstrip" height in computing the top border thickness,
    instead of the appropriate system metric.  This led to wrongly-sized popup
    windows, which also then restored to a different size.  Fixed that.
    
    Bug: 41055
    Change-Id: I64f6781d8705e4f142c87b6f9179a8fba90f0c91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783167
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694048}

--- chrome/browser/sessions/session_service.cc
+++ chrome/browser/sessions/session_service.cc
@@ -590,7 +590,8 @@ bool SessionService::ShouldRestoreWindowOfType(
     return true;
 #endif
 
-  return window_type == sessions::SessionWindow::TYPE_NORMAL;
+  return (window_type == sessions::SessionWindow::TYPE_NORMAL) ||
+         (window_type == sessions::SessionWindow::TYPE_POPUP);
 }
 
 void SessionService::RemoveUnusedRestoreWindows(
