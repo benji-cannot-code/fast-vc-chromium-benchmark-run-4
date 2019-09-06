3f2e1418f9247e55ebae0008a407ed7e42730562
Peter Kotwicz <pkotwicz@chromium.org>
2019-09-06 03:18:32 +0000

    Move webapp files in org.chromium.chrome.test.util.browser to dedicated dir 🚚
    This CL moves webapp files in
    //chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/
    to dedicated directory
    //chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/webapps/
    
    The new directory has the same OWNERS as other webapps/ directories. This CL
    avoids having to ask global OWNERS for boiler plate changes to webapp testing code
    
    BUG=1000949
    
    Change-Id: I37662780cf3889c75be1a5a6be99b43e409c1444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788221
    Auto-Submit: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694111}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/webapps/WebappTestPage.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/webapps/WebappTestPage.java
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.test.util.browser;
+package org.chromium.chrome.test.util.browser.webapps;
 
 import android.net.Uri;
 
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.test.util.browser.TabLoadObserver;
 import org.chromium.net.test.EmbeddedTestServer;
 
 /**
