69b20d6a9948002c74cdace174811b6dcfdccb8a
Qingsi Wang <qingsi@chromium.org>
2019-09-06 17:15:28 +0000

    Disable the response to mDNS name generator queries.
    Bug: 1001371
    Change-Id: I1bbf963db8f77ba122f47821e36b491e137dfb1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788685
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Qingsi Wang <qingsi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694294}

--- services/network/mdns_responder_unittest.cc
+++ services/network/mdns_responder_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/strings/string_piece.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "mojo/public/cpp/bindings/connector.h"
 #include "net/base/ip_address.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/dns_util.h"
 #include "net/dns/mock_mdns_socket_factory.h"
 #include "net/dns/public/dns_protocol.h"
+#include "services/network/public/cpp/features.h"
 #include "services/network/public/mojom/mdns_responder.mojom.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -393,6 +395,8 @@ class MdnsResponderTest : public testing::Test {
  public:
   MdnsResponderTest()
       : failing_socket_factory_(task_environment_.GetMainThreadTaskRunner()) {
+    feature_list_.InitAndEnableFeature(
+        features::kMdnsResponderGeneratedNameListing);
     Reset();
   }
 
@@ -483,6 +487,7 @@ class MdnsResponderTest : public testing::Test {
     task_environment_.FastForwardBy(duration);
   }
 
+  base::test::ScopedFeatureList feature_list_;
   base::test::TaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   // Overrides the current thread task runner, so we can simulate the passage
