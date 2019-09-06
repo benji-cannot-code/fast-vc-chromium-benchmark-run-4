991363318cf88d323e30faea9af90ef4879f3d66
Andrew Comminos <acomminos@fb.com>
2019-09-06 21:01:40 +0000

    Add internals method to force CPU profiler sample collection
    To enable less flaky JS Self-Profiling API tests, add
    window.internals.collectSample for forcing a sample to be taken across all
    profilers in the isolate.
    
    Bug: 956688
    Change-Id: I42237e8f8e1752b8096bbb8d80f25f6053daaa3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1785458
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Luke Z <lpz@chromium.org>
    Commit-Queue: Andrew Comminos <acomminos@fb.com>
    Cr-Commit-Position: refs/heads/master@{#694409}

---
+++ third_party/blink/renderer/core/timing/internals_profiler.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_INTERNALS_PROFILER_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_INTERNALS_PROFILER_H_
+
+#include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
+
+namespace blink {
+
+class Internals;
+class ScriptState;
+
+class InternalsProfiler {
+  STATIC_ONLY(InternalsProfiler);
+
+ public:
+  static void collectSample(ScriptState*, Internals&);
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_TIMING_INTERNALS_PROFILER_H_
