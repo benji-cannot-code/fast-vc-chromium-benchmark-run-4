be7d79e025e319942ccbd96f9e31574ab86bbf17
Stepan Khapugin <stkhapugin@chromium.org>
2019-09-06 09:26:20 +0000

    [multiball] Create scene-related classes.
    Creates the controller, state, and delegate for scenes. Updates the
    plist to use the SceneDelegate. Fixes the bundling issue for the
    storyboard.
    
    Bug: none
    Change-Id: I92b80d3c95fcedb413bbeb5d5a20378a3b09afd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787779
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694182}

---
+++ ios/chrome/browser/ui/main/scene_controller.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_MAIN_SCENE_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_MAIN_SCENE_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+// The controller object for a scene. Reacts to scene state changes.
+@interface SceneController : NSObject
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_MAIN_SCENE_CONTROLLER_H_
---
+++ ios/chrome/browser/ui/main/scene_state.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_MAIN_SCENE_STATE_H_
+#define IOS_CHROME_BROWSER_UI_MAIN_SCENE_STATE_H_
+
+#import <UIKit/UIKit.h>
+
+// An object containing the state of a UIWindowScene. One state object
+// corresponds to one scene.
+@interface SceneState : NSObject
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_MAIN_SCENE_STATE_H_
