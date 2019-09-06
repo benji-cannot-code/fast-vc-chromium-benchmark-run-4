b9a624d33a6bcc55e173cfb75ddf46cba9fbfd09
yilkal <yilkal@chromium.org>
2019-09-06 23:50:03 +0000

    Adds SupervisedUserIframeFilter flag.
    This CL adds SupervisedUserIframeFilter feature flag.
    
    Bug: 850328
    Change-Id: I5e06f250d7a5024412d28b5878cb3db0397705ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771950
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694483}

--- chrome/browser/supervised_user/supervised_user_features.cc
+++ chrome/browser/supervised_user/supervised_user_features.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace supervised_users {
 
+const base::Feature kSupervisedUserIframeFilter{
+    "SupervisedUserIframeFilter", base::FEATURE_DISABLED_BY_DEFAULT};
+
 const base::Feature kSupervisedUserInitiatedExtensionInstall{
     "SupervisedUserInitiatedExtensionInstall",
     base::FEATURE_DISABLED_BY_DEFAULT};
