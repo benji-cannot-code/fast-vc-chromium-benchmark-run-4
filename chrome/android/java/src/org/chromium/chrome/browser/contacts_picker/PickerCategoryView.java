271746e7c05cc1f22851b9bc3c5023b6d766eea7
Finnur Thorarinsson <finnur@chromium.org>
2019-09-06 14:45:31 +0000

    Contacts Picker: Clamp width of dialog to a fixed max.
    Bug: 988278, 860467
    Change-Id: Ie77f86b054818f87d9d75531c5d8f160ba8e2fc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778499
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694246}

--- chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/PickerCategoryView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contacts_picker/PickerCategoryView.java
@@ -14,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.VisibleForTesting;
@@ -31,6 +30,7 @@ import org.chromium.chrome.browser.widget.selection.SelectionDelegate;
 import org.chromium.content.browser.contacts.ContactsPickerPropertiesRequested;
 import org.chromium.ui.ContactsPickerListener;
 import org.chromium.ui.UiUtils;
+import org.chromium.ui.widget.OptimizedFrameLayout;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -42,7 +42,7 @@ import java.util.Set;
  * A class for keeping track of common data associated with showing contact details in
  * the contacts picker, for example the RecyclerView.
  */
-public class PickerCategoryView extends RelativeLayout
+public class PickerCategoryView extends OptimizedFrameLayout
         implements View.OnClickListener, RecyclerView.RecyclerListener,
                    SelectionDelegate.SelectionObserver<ContactDetails>,
                    SelectableListToolbar.SearchDelegate, TopView.SelectAllToggleCallback {
@@ -122,7 +122,7 @@ public class PickerCategoryView extends RelativeLayout
     public PickerCategoryView(Context context, boolean multiSelectionAllowed,
             boolean shouldIncludeNames, boolean shouldIncludeEmails, boolean shouldIncludeTel,
             String formattedOrigin, ContactsPickerToolbar.ContactsToolbarDelegate delegate) {
-        super(context);
+        super(context, null);
 
         mActivity = (ChromeActivity) context;
         mMultiSelectionAllowed = multiSelectionAllowed;
@@ -158,6 +158,7 @@ public class PickerCategoryView extends RelativeLayout
         mToolbar.initializeSearchView(this, R.string.contacts_picker_search, 0);
         mToolbar.setDelegate(delegate);
         mToolbar.showBackArrow();
+        mSelectableListLayout.configureWideDisplayStyle();
 
         mSearchButton = (ImageView) mToolbar.findViewById(R.id.search);
         mSearchButton.setOnClickListener(this);
