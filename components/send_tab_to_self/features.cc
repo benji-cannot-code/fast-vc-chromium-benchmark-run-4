251c8d92683b4126eb70dfe29fdddf4a8b969889
Ramya Nagarajan <ramyan@chromium.org>
2019-09-06 00:28:27 +0000

    [SendTabToSelf] Enable SendTabToSelfShowSendingUI by default.
    Enable SendTabToSelfShowSendingUI by default on ToT, and remove the
    associated fieldtrial testing config.
    
    Bug: 901429
    Change-Id: I6be6a538e9ad41778e97008ac35200e95c9583b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1785083
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Ramya Nagarajan <ramyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694032}

--- components/send_tab_to_self/features.cc
+++ components/send_tab_to_self/features.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace send_tab_to_self {
 
 const base::Feature kSendTabToSelfShowSendingUI{
-    "SendTabToSelfShowSendingUI", base::FEATURE_DISABLED_BY_DEFAULT};
+    "SendTabToSelfShowSendingUI", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kSendTabToSelfBroadcast{"SendTabToSelfBroadcast",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
