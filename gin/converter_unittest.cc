a047e11f1161c3dfcdd0c7bf1cc6fdc77399e2f3
Shelley Vohr <shelley.vohr@gmail.com>
2019-09-06 00:41:39 +0000

    Adapt string16 converter to handle empty strings
    At present, if an empty string is passed to gin's string16 converter
    it will crash on a string_util DCHECK that length_with_null >= 1u; as this
    is not ideal in this case, this change updates the DCHECK in
    base::WriteIntoT to ensure no crashes occur on 0-length strings.
    
    Change-Id: I41042f77d52fef3731f664a0903b3ba37270ab8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769665
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694042}

--- gin/converter_unittest.cc
+++ gin/converter_unittest.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/stl_util.h"
+#include "base/strings/string16.h"
+#include "base/strings/utf_string_conversions.h"
 #include "gin/handle.h"
 #include "gin/public/isolate_holder.h"
 #include "gin/test/v8_test.h"
@@ -78,6 +80,31 @@ TEST_F(ConverterTest, Bool) {
                                         test_data[i].input, &result));
     EXPECT_EQ(test_data[i].expected, result);
   }
+}
+
+TEST_F(ConverterTest, String16) {
+  v8::Isolate* isolate = instance_->isolate();
+
+  HandleScope handle_scope(isolate);
+
+  EXPECT_TRUE(Converter<base::string16>::ToV8(isolate, base::ASCIIToUTF16(""))
+                  ->StrictEquals(StringToV8(isolate, "")));
+  EXPECT_TRUE(
+      Converter<base::string16>::ToV8(isolate, base::ASCIIToUTF16("hello"))
+          ->StrictEquals(StringToV8(isolate, "hello")));
+
+  base::string16 result;
+
+  ASSERT_FALSE(
+      Converter<base::string16>::FromV8(isolate, v8::False(isolate), &result));
+  ASSERT_FALSE(
+      Converter<base::string16>::FromV8(isolate, v8::True(isolate), &result));
+  ASSERT_TRUE(Converter<base::string16>::FromV8(
+      isolate, v8::String::Empty(isolate), &result));
+  EXPECT_EQ(result, base::string16());
+  ASSERT_TRUE(Converter<base::string16>::FromV8(
+      isolate, StringToV8(isolate, "hello"), &result));
+  EXPECT_EQ(result, base::ASCIIToUTF16("hello"));
 }
 
 TEST_F(ConverterTest, Int32) {
