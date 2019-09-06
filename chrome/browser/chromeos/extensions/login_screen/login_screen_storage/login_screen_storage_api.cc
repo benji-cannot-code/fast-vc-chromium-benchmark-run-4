81c2c67d2f6ad90ee44c5104f7f1fc72a9d8c853
Zakhar Voit <voit@google.com>
2019-09-06 18:51:49 +0000

    [Extensions] Make chrome.loginScreenStorageApi keep owner ID in keys
    Keys for persistent data in the login screen storage now include owner
    ID to clear data saved by each owner when its uninstalled.
    
    Bug: 980171
    Change-Id: Ie755faf6eec89a809b83f2df76b9a6d109a19f9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1753007
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694343}

--- chrome/browser/chromeos/extensions/login_screen/login_screen_storage/login_screen_storage_api.cc
+++ chrome/browser/chromeos/extensions/login_screen/login_screen_storage/login_screen_storage_api.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/extensions/login_screen/login_screen_storage/login_screen_storage_api.h"
 
+#include "base/strings/strcat.h"
 #include "base/values.h"
 #include "chrome/common/extensions/api/login_screen_storage.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
@@ -83,10 +84,11 @@ void LoginScreenStorageStorePersistentDataFunction::StoreDataForExtensions(
   if (extension_ids.empty())
     return;
 
-  std::string extension_id = extension_ids.back();
+  std::string receiver_id = extension_ids.back();
   extension_ids.pop_back();
   chromeos::SessionManagerClient::Get()->LoginScreenStorageStore(
-      kPersistentDataKeyPrefix + extension_id, metadata, data,
+      kPersistentDataKeyPrefix + extension_id() + "_" + receiver_id, metadata,
+      data,
       base::BindOnce(
           &LoginScreenStorageStorePersistentDataFunction::OnDataStored, this,
           std::move(extension_ids), metadata, data));
@@ -99,8 +101,13 @@ LoginScreenStorageRetrievePersistentDataFunction::
 
 ExtensionFunction::ResponseAction
 LoginScreenStorageRetrievePersistentDataFunction::Run() {
+  std::unique_ptr<login_screen_storage::RetrievePersistentData::Params> params =
+      login_screen_storage::RetrievePersistentData::Params::Create(*args_);
+  EXTENSION_FUNCTION_VALIDATE(params);
+
   chromeos::SessionManagerClient::Get()->LoginScreenStorageRetrieve(
-      kPersistentDataKeyPrefix + extension_id(),
+      base::StrCat(
+          {kPersistentDataKeyPrefix, params->owner_id, "_", extension_id()}),
       base::BindOnce(
           &LoginScreenStorageRetrievePersistentDataFunction::OnDataRetrieved,
           this));
