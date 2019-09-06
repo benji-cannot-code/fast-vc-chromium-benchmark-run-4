19163fd750935f155caca65cc393a44515691e40
Jimmy Gong <jimmyxgong@google.com>
2019-09-06 02:25:50 +0000

    Display a message when no printers match the search query
    - Saved printers and nearby printers will now have their own separate
      message for when the search query fails to filter their respective
      printers.
    - Update browser test to reflect this change.
    
    Bug: 997116
    Test: browser test, end to end manual
    Change-Id: Ifc6b2cdd0bc55c0cf8ec5d46b4fdbf2d0e6eba70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769238
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694099}

--- chrome/browser/resources/settings/printing_page/cups_printers_entry_list.js
+++ chrome/browser/resources/settings/printing_page/cups_printers_entry_list.js
@@ -38,7 +38,20 @@ Polymer({
      * Search term for filtering |printers|.
      * @type {string}
      */
-    searchTerm: String,
+    searchTerm: {
+      type: String,
+      value: '',
+    },
+
+    /**
+     * Whether to show the no search results message.
+     * @type {boolean}
+     * @private
+     */
+     showNoSearchResultsMessage_: {
+      type: Boolean,
+      value: false,
+    },
   },
 
   observers: [
@@ -64,6 +77,9 @@ Polymer({
 
     this.updateList('filteredPrinters_', printer => printer.printerInfo,
         updatedPrinters);
+
+    this.showNoSearchResultsMessage_ =
+        !!this.searchTerm && !this.filteredPrinters_.length;
   },
 
 
