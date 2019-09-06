57014a6ec69d31cc9f4df2d4326e605f5ad9a247
Gabriel Charette <gab@chromium.org>
2019-09-06 04:04:20 +0000

    Backport some tests in /chromeos/timezone to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /chromeos/timezone.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=alemate@chromium.org
    
    Bug: 891670
    Change-Id: Ie18745b2640744662ece01c9aaf703ded8fee79c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786844
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694122}

--- chromeos/timezone/timezone_unittest.cc
+++ chromeos/timezone/timezone_unittest.cc
@@ -160,7 +160,7 @@ class TimeZoneReceiver {
 
 class TimeZoneTest : public testing::Test {
  private:
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 };
 
 TEST_F(TimeZoneTest, ResponseOK) {
