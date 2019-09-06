be189673bbbcf3b80ec5e53272491607fffe58ef
Peter Kotwicz <pkotwicz@chromium.org>
2019-09-06 03:50:14 +0000

    Re-enable WebappSplashScreenTest
    BUG=None
    
    Change-Id: I9d0d8f6d6995084843a437590368e77d5ddee534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1775745
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694116}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappSplashScreenTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappSplashScreenTest.java
@@ -28,7 +28,6 @@ import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.ShortcutHelper;
@@ -80,7 +79,6 @@ public class WebappSplashScreenTest {
     }
 
     @Test
-    @DisabledTest
     @SmallTest
     @Feature({"Webapps"})
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
