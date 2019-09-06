f940a9dddc5a8447224410f2d86975684e4524c0
Olga Sharonova <olka@chromium.org>
2019-09-06 12:44:35 +0000

    Disabling flaky BookmarksFocusTest.All.
    TBR=aee@chromium.org
    
    Bug: 1000950
    Change-Id: Iee20cc3507c1e3e174450bb1f2dc7bf90114ca90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789286
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694213}

--- chrome/test/data/webui/bookmarks/bookmarks_focus_test.js
+++ chrome/test/data/webui/bookmarks/bookmarks_focus_test.js
@@ -28,7 +28,9 @@ BookmarksFocusTest.prototype = {
   ],
 };
 
-TEST_F('BookmarksFocusTest', 'All', function() {
+// http://crbug.com/1000950 : Flaky.
+GEN('#define MAYBE_All DISABLED_All');
+TEST_F('BookmarksFocusTest', 'MAYBE_All', function() {
   suite('<bookmarks-folder-node>', function() {
     let rootNode;
     let store;
