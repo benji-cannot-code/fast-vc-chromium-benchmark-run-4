9804762e6fe99013e3ab9c56ca64b6b336161e4a
Clark DuVall <cduvall@chromium.org>
2019-09-06 19:28:34 +0000

    [WebLayer] Refactor WebLayer native init
    This pulls out some changes from http://crrev.com/c/1783727 that are
    needed to make the WebLayer lib work when used with monochrome.
    
    Change-Id: Ia749d0b9c7efdfd5906a684346cce30ad9b2c7b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788236
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694369}

---
+++ weblayer/app/entry_point.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/jni_android.h"
+#include "base/android/library_loader/library_loader_hooks.h"
+#include "weblayer/app/jni_onload.h"
+
+namespace {
+
+bool NativeInit(base::android::LibraryProcessType) {
+  return weblayer::OnJNIOnLoadInit();
+}
+
+}  // namespace
+
+JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+  base::android::InitVM(vm);
+  base::android::SetNativeInitializationHook(&NativeInit);
+  return JNI_VERSION_1_4;
+}
