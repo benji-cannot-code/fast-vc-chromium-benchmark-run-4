63e5683f4c1631b5484b16ed12874f9d3c4fcb03
Jesse Schettler <jschettler@chromium.org>
2019-09-06 14:01:14 +0000

    arc: Delete print document on non-UI thread
    Ensure the print document is being deleted on a non-UI thread when the
    print session ends.
    
    Bug: b:139551164
    Test: Close print preview and verify print document is deleted
    Change-Id: I4e0afbaf2942b7a2fb89b319a3885855243963d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787558
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694236}

--- chrome/browser/chromeos/arc/print_spooler/arc_print_spooler_util.cc
+++ chrome/browser/chromeos/arc/print_spooler/arc_print_spooler_util.cc
@@ -16,6 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace arc {
 
+void DeletePrintDocument(const base::FilePath& file_path) {
+  if (!base::DeleteFile(file_path, false))
+    LOG(ERROR) << "Failed to delete print document.";
+}
+
 base::FilePath SavePrintDocument(mojo::ScopedHandle scoped_handle) {
   base::PlatformFile platform_file = base::kInvalidPlatformFile;
   if (mojo::UnwrapPlatformFile(std::move(scoped_handle), &platform_file) !=
