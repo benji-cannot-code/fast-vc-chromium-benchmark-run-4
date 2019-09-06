a3ee5e490cee69064ae2cace9c06d943787ba8ed
Pavel Feldman <pfeldman@chromium.org>
2019-09-06 20:16:45 +0000

    DevTools: return frameId from DOM.getNodeForLocation
    Otherwise, there is no way to resolve resulting backendNodeId the isolated
    world. Knowing the frame, one can pick execution context belonging to that
    frame and DOM.resolveNode it there.
    
    Drive-by: since I'm there, adding ignorePointerEventsNone to match behavior
    in the InspectTool.
    
    Drive-by: remove DOM.getNodeForLocation from experimental, it has been there
    forever, it is used in screencast, etc.
    
    Change-Id: Ibbcb06e9b767d44a4b2a620e2b67a10f155267ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788682
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694390}

--- third_party/blink/web_tests/inspector-protocol/dom/dom-getNodeForLocation-pointer-events.js
+++ third_party/blink/web_tests/inspector-protocol/dom/dom-getNodeForLocation-pointer-events.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var {page, session, dp} = await testRunner.startHTML(`
     <div style='position:absolute;top:0;left:0;width:100;height:100'></div>
     <div style='position:absolute;top:0;left:0;width:200;height:200;pointer-events:none'></div>
-  `, 'Tests DOM.getNodeForLocation method.');
+  `, 'Tests DOM.getNodeForLocation method with ignorePointerEventsNone: true.');
   var NodeTracker = await testRunner.loadScript('../resources/node-tracker.js');
   var nodeTracker = new NodeTracker(dp);
 
@@ -11,11 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var backendNodeId = response.result.backendNodeId;
   await dp.DOM.enable();
   await dp.DOM.getDocument();
-  testRunner.log(await nodeTracker.nodeForBackendId(backendNodeId), 'Node: ');
+  testRunner.log((await nodeTracker.nodeForBackendId(backendNodeId)).attributes, 'Node: ');
 
-  await dp.Page.enable();
-  var response2 = await dp.Page.getFrameTree({});
-  testRunner.log('FrameID matched: ' + (response2.result.frameTree.frame.id === response.result.frameId));
+  response = await dp.DOM.getNodeForLocation({x: 10, y: 10, ignorePointerEventsNone: true});
+  backendNodeId = response.result.backendNodeId;
+  await dp.DOM.enable();
+  await dp.DOM.getDocument();
+  testRunner.log((await nodeTracker.nodeForBackendId(backendNodeId)).attributes, 'Overlay node: ');
 
   testRunner.completeTest();
 })
