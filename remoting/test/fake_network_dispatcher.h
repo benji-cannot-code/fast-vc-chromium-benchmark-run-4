163214d86b228e495cd366b5356b1c12d18acba2
Patrik Höglund <phoglund@chromium.org>
2019-09-06 07:15:21 +0000

    Made downstream users of rtc::PacketSocketFactory use the new override.
    The old override that takes proxy_info etc is going away.
    
    Bug: webrtc:7477
    Change-Id: Ic1c6d79856371f63df251286032559ea306c9a01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778870
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Patrik Höglund <phoglund@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694158}

--- remoting/test/fake_network_dispatcher.h
+++ remoting/test/fake_network_dispatcher.h
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
-#include "third_party/webrtc/p2p/base/packet_socket_factory.h"
+#include "third_party/webrtc/rtc_base/ip_address.h"
+#include "third_party/webrtc/rtc_base/socket_address.h"
 
 namespace base {
 class SingleThreadTaskRunner;
