ebced46b58e1042966d8522a7bf3e48832e4c545
Owen Min <zmin@chromium.org>
2019-09-06 21:27:08 +0000

    Update DCHECK in extension/policy_handlers.cc to match the policy schema.
    ExtensionSettings policy's schema is updated in
    https://chromium-review.googlesource.com/c/chromium/src/+/1548379
    
    Update the DCHECK in policy_handler so that the input pass schema validation
    won't cause DCHECK failure.
    
    Bug: 1001166
    Change-Id: I906de4ff36ae1041abd293c91305d3720411be08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787889
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694425}

--- chrome/browser/extensions/policy_handlers.cc
+++ chrome/browser/extensions/policy_handlers.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_split.h"
 #include "build/build_config.h"
 #include "chrome/browser/extensions/extension_management_constants.h"
 #include "chrome/browser/extensions/external_policy_loader.h"
@@ -30,7 +31,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace extensions {
-
+namespace {
+// Returns true if extensions_ids contains a list of valid extension ids,
+// divided by comma.
+bool IsValidIdList(const std::string& extension_ids) {
+  std::vector<base::StringPiece> ids = base::SplitStringPiece(
+      extension_ids, ",", base::WhitespaceHandling::TRIM_WHITESPACE,
+      base::SplitResult::SPLIT_WANT_NONEMPTY);
+  if (ids.size() == 0)
+    return false;
+  for (const auto& id : ids) {
+    if (!crx_file::id_util::IdIsValid(id.as_string()))
+      return false;
+  }
+  return true;
+}
+}  // namespace
 // ExtensionListPolicyHandler implementation -----------------------------------
 
 ExtensionListPolicyHandler::ExtensionListPolicyHandler(const char* policy_name,
@@ -249,8 +265,7 @@ bool ExtensionSettingsPolicyHandler::CheckPolicySettings(
 
   for (base::DictionaryValue::Iterator it(*dict_value); !it.IsAtEnd();
        it.Advance()) {
-    DCHECK(it.key() == schema_constants::kWildcard ||
-           crx_file::id_util::IdIsValid(it.key()));
+    DCHECK(it.key() == schema_constants::kWildcard || IsValidIdList(it.key()));
     DCHECK(it.value().is_dict());
 
     // Extracts sub dictionary.
