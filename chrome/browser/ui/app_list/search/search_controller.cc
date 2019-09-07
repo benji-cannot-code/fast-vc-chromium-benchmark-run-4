f5d752d14695f0d763aabd8e168fab9a69588102
Rachel Wong <wrong@chromium.org>
2019-09-07 01:34:36 +0000

    [Files ranking] Log zero state display metrics.
    This change adds three metrics:
    1. The position index of the launched item.
    2. The number of item types included in each impression set. (Diversity)
    3. Whether or not an item was clicked. (CTR)
    
    Bug: 999912
    Change-Id: I0a725c141f353f4fb89cd04110cf9731d01f96fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1781124
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694511}

--- chrome/browser/ui/app_list/search/search_controller.cc
+++ chrome/browser/ui/app_list/search/search_controller.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/chrome_search_result.h"
 #include "chrome/browser/ui/app_list/search/search_provider.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/app_list_launch_recorder.h"
+#include "chrome/browser/ui/app_list/search/search_result_ranker/histogram_util.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/ranking_item_util.h"
 #include "chrome/browser/ui/app_list/search/search_result_ranker/search_result_ranker.h"
 #include "third_party/metrics_proto/chrome_os_app_list_launch_event.pb.h"
@@ -174,8 +175,17 @@ void SearchController::OnSearchResultsDisplayed(
     const base::string16& trimmed_query,
     const ash::SearchResultIdWithPositionIndices& results,
     int launched_index) {
-  if (trimmed_query.empty())
+  if (trimmed_query.empty()) {
     mixer_->GetNonAppSearchResultRanker()->ZeroStateResultsDisplayed(results);
+
+    // Extract result types for logging.
+    std::vector<RankingItemType> result_types;
+    for (const auto& result : results) {
+      result_types.push_back(
+          RankingItemTypeFromSearchResult(*FindSearchResult(result.id)));
+    }
+    LogZeroStateResultsListMetrics(result_types, launched_index);
+  }
 }
 
 ChromeSearchResult* SearchController::GetResultByTitleForTest(
