c08c2443df967fe972d20f278dfe9d59f79c6f41
Gabriel Charette <gab@chromium.org>
2019-09-07 14:03:40 +0000

    Backport some tests in /components/password_manager to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /components/password_manager.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    Edit: PasswordStore* tests need the full TaskEnvironment. Updated them
    as such in PS2 and used the cleaner inline member initialization
    paradigm as a bonus.
    
    R=dvadym@chromium.org
    
    Bug: 891670
    Change-Id: I94a7533fc2904a90bf7344c030c28f4a89c5f91b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787200
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694577}

--- components/password_manager/core/browser/password_manager_unittest.cc
+++ components/password_manager/core/browser/password_manager_unittest.cc
@@ -489,7 +489,7 @@ class PasswordManagerTest : public testing::Test {
   }
 
   const GURL test_url_;
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   scoped_refptr<MockPasswordStore> store_;
   testing::NiceMock<MockPasswordManagerClient> client_;
   MockPasswordManagerDriver driver_;
