46d964185341b3f815f6f23a6fa43f13348d391f
Gabriel Charette <gab@chromium.org>
2019-09-07 14:07:38 +0000

    Backport some tests in /extensions/renderer to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /extensions/renderer.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    Edit: Revert changes to gc_callback_unittest.cc which now
          needs base::ThreadPoolInstance.
    
    R=lazyboy@chromium.org
    
    Bug: 891670
    Change-Id: I864886ed84ed11d568478282227ee1555121d4d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786680
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694579}

--- extensions/renderer/script_context_set_unittest.cc
+++ extensions/renderer/script_context_set_unittest.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 TEST(ScriptContextSetTest, Lifecycle) {
-  base::test::TaskEnvironment task_environment;
+  base::test::SingleThreadTaskEnvironment task_environment;
   ScopedWebFrame web_frame;
   // Used by ScriptContextSet::Register().
   TestExtensionsRendererClient extensions_renderer_client;
