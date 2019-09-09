49b7d77161b805812ab6879f53f485ee1c3dc665
Robbie McElrath <rmcelrath@chromium.org>
2019-09-09 21:22:25 +0000

    Don't include JNI headers in chrome android when they're not used.
    This CL removes dependencies on JNI headers when they're not used and
    the Java implementation isn't included. This makes chrome_public_apk
    work with crrev.com/c/1743088.
    
    Bug: 836335
    Change-Id: Iec02f4fec9cf93183949547960e191e675e62ebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746641
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694921}

--- components/about_ui/credit_utils.cc
+++ components/about_ui/credit_utils.cc
@@ -11,11 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/grit/components_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 
-#if defined(OS_ANDROID)
-#include "base/files/file.h"
-#include "components/about_ui/android/about_ui_jni_headers/CreditUtils_jni.h"
-#endif
-
 namespace about_ui {
 
 std::string GetCredits(bool include_scripts) {
@@ -30,13 +25,5 @@ std::string GetCredits(bool include_scripts) {
   response += "</body>\n</html>";
   return response;
 }
-
-#if defined(OS_ANDROID)
-static void JNI_CreditUtils_WriteCreditsHtml(JNIEnv* env, jint fd) {
-  std::string html_content = GetCredits(false);
-  base::File out_file(fd);
-  out_file.WriteAtCurrentPos(html_content.c_str(), html_content.size());
-}
-#endif
 
 }  // namespace about_ui
