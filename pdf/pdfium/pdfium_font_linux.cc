4f7aa7195d9bde4f8cf1062cbd0a861e1f6ab5f9
Peter Kasting <pkasting@chromium.org>
2019-09-09 23:09:40 +0000

    Replace nested min/max calls with base::ClampToRange() in pdf/.
    Bug: 1000055
    Change-Id: Ic19035c7dd1bc69cccc1161e2007563051bcda36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1793442
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694973}

--- pdf/pdfium/pdfium_font_linux.cc
+++ pdf/pdfium/pdfium_font_linux.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/encoding_detection.h"
 #include "base/i18n/icu_string_conversions.h"
 #include "base/logging.h"
+#include "base/numerics/ranges.h"
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "ppapi/cpp/instance.h"
@@ -33,7 +34,7 @@ PP_BrowserFont_Trusted_Weight WeightToBrowserFontTrustedWeight(int weight) {
   constexpr int kMinimumWeight = 100;
   constexpr int kMaximumWeight = 900;
   int normalized_weight =
-      std::min(std::max(weight, kMinimumWeight), kMaximumWeight);
+      base::ClampToRange(weight, kMinimumWeight, kMaximumWeight);
   normalized_weight = (normalized_weight / 100) - 1;
   return static_cast<PP_BrowserFont_Trusted_Weight>(normalized_weight);
 }
