b981046afaba2d5ba6c11162fc5beb05f1e44a10
Gabriel Charette <gab@chromium.org>
2019-09-09 20:09:17 +0000

    Backport some tests in /components/browsing_data to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /components/browsing_data.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=dullweber@chromium.org
    
    Bug: 891670
    Change-Id: I31e0a16f1dc0e605fad78e6252ec7d7a833efb9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786839
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694883}

--- components/browsing_data/core/browsing_data_utils_unittest.cc
+++ components/browsing_data/core/browsing_data_utils_unittest.cc
@@ -51,7 +51,7 @@ class BrowsingDataUtilsTest : public testing::Test {
   PrefService* prefs() { return &prefs_; }
 
  private:
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   sync_preferences::TestingPrefServiceSyncable prefs_;
 };
 
