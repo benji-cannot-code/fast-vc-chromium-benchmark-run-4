4bc34f0b4530da5943324eb884ffdd6c3acb53f1
Richard Knoll <knollr@chromium.org>
2019-09-09 09:57:23 +0000

    Hotfix UAF when closing tab with sharing dialog.
    This fixes a UAF when closing a tab while there is a
    SharingDialog open.
    
    Bug: 1000934
    Change-Id: Ie8f6e52626ee834bc72acfe318c5a28aafe27635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789150
    Commit-Queue: Alex Chau <alexchau@chromium.org>
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694682}

--- chrome/browser/ui/views/sharing/sharing_dialog_view.cc
+++ chrome/browser/ui/views/sharing/sharing_dialog_view.cc
@@ -334,7 +334,8 @@ base::string16 SharingDialogView::GetWindowTitle() const {
 }
 
 void SharingDialogView::WindowClosing() {
-  controller_->OnDialogClosed(this);
+  if (web_contents())
+    controller_->OnDialogClosed(this);
 }
 
 // static
