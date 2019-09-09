33bb4c43a007541e0aa7c475d6cab029b9e1123b
Bo Liu <boliu@chromium.org>
2019-09-09 20:11:25 +0000

    weblayer: Make keyboard IME work
    Use the ContentView as the WebContents container view
    Also emulate focus changes like content shell.
    
    Change-Id: I6edf3e7be920bcc2a385d0698b1bf949a496c91e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790150
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694886}

--- weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java
+++ weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java
@@ -37,6 +37,7 @@ public class WebLayerShellActivity extends FragmentActivity {
     private Profile mProfile;
     private BrowserController mBrowserController;
     private EditText mUrlView;
+    private View mMainView;
 
     public static class ShellFragment extends Fragment {
         private BrowserController mBrowserController;
@@ -62,6 +63,7 @@ public class WebLayerShellActivity extends FragmentActivity {
         LinearLayout mainView = new LinearLayout(this);
         int viewId = View.generateViewId();
         mainView.setId(viewId);
+        mMainView = mainView;
         setContentView(mainView);
 
         mUrlView = new EditText(this);
@@ -112,6 +114,7 @@ public class WebLayerShellActivity extends FragmentActivity {
 
     private void loadUrl(String url) {
         mBrowserController.getNavigationController().navigate(Uri.parse(sanitizeUrl(url)));
+        mUrlView.clearFocus();
     }
 
     /**
