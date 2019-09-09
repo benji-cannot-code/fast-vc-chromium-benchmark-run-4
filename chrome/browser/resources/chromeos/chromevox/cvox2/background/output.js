0b3f39e6f823fdbd2ba050dd5f67ceb5772921a4
David Tseng <dtseng@chromium.org>
2019-09-09 20:08:10 +0000

    Do not announce selections in editables when not focused
    Issue:
    Whenever a node does not have focus, the textSelStart and textSelEnd attributes are not populated properly.
    For example,
    Node role=textField textSelStart=2 textSelEnd=3 value="asdf"
    
    regardless of where the actual selection is in the text field.
    
    Change-Id: I542e58384268b069d6d015e981a2ad3fc7c20ece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790268
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694881}

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/output.js
@@ -1221,7 +1221,8 @@ Output.prototype = {
             }
           }
           options.annotation.push(token);
-          if (selectedText && !this.formatOptions_.braille) {
+          if (selectedText && !this.formatOptions_.braille &&
+              node.state[StateType.FOCUSED]) {
             this.append_(buff, selectedText, options);
             this.append_(buff, Msgs.getMsg('selected'));
             ruleStr.writeTokenWithValue(token, selectedText);
