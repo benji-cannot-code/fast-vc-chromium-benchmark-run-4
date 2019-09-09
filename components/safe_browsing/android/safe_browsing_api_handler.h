39dde80d454bbd5bfbfae632f6dde9e3318229ed
Xinghui Lu <xinghuilu@chromium.org>
2019-09-09 20:06:02 +0000

    Synchronous implementation of CSD allowlist check on Android
    Before allowlist is available from GmsCore, we plan to have it
    hardcoded in our code. This call is synchronous for now,
    since it gets result directly instead of a IPC call.
    
    I will have a follow up CL for Async implementation.
    
    Bug: 999344
    Change-Id: Ide3b7de83f0e32c5be8ac657369ce9f95a848f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776601
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694876}

--- components/safe_browsing/android/safe_browsing_api_handler.h
+++ components/safe_browsing/android/safe_browsing_api_handler.h
@@ -37,6 +37,8 @@ class SafeBrowsingApiHandler {
                              const GURL& url,
                              const SBThreatTypeSet& threat_types) = 0;
 
+  virtual bool StartCSDAllowlistCheck(const GURL& url) = 0;
+
   virtual ~SafeBrowsingApiHandler() {}
 
  private:
