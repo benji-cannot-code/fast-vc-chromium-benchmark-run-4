dfd9fd459e319dc1d4995fffa8f6895da18fe39e
Tommy Li <tommycli@chromium.org>
2019-09-09 18:55:53 +0000

    [omnibox] Slight refactors to AutocompleteController::in_start_
    This makes some minor refactors to the usage of
    AutocompleteController::in_start_.
    
     1. It delays setting it to false until after the UpdateResult call
        within the synchronous pass. This is more accurate, and also allows
        in_start_ to be used as a synonoym of in_synchronous_pass_.
    
     2. It also moves in_start_ to be an early exit condition for
        OnProviderUpdate. It likely doesn't need to be checked there at
        all, since the comments indicate it's likely ALWAYS false when
        OnProviderUpdate is called.
    
    These refactors help pave the way for the feature to preserve the
    default match during the asynchronous pass, by clarifying in the code
    when the synchronous vs. asynchronous pass occurs.
    
    Bug: 398135
    Change-Id: I8f8c61b8233b1c049338dac448b6ef8f1b0e3b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789823
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694850}

--- components/omnibox/browser/clipboard_provider.cc
+++ components/omnibox/browser/clipboard_provider.cc
@@ -296,6 +296,10 @@ void ClipboardProvider::ConstructImageMatchCallback(
 
   match.transition = ui::PAGE_TRANSITION_GENERATED;
 
+  field_trial_triggered_ = true;
+  field_trial_triggered_in_session_ = true;
+  done_ = true;
+
   // Some users may be in a counterfactual study arm in which we perform all
   // necessary work but do not forward the autocomplete matches.
   bool in_counterfactual_group = base::GetFieldTrialParamByFeatureAsBool(
@@ -305,9 +309,6 @@ void ClipboardProvider::ConstructImageMatchCallback(
     AddCreatedMatchWithTracking(input, match, clipboard_contents_age);
     listener_->OnProviderUpdate(true);
   }
-  field_trial_triggered_ = true;
-  field_trial_triggered_in_session_ = true;
-  done_ = true;
 }
 
 void ClipboardProvider::AddProviderInfo(ProvidersInfo* provider_info) const {
