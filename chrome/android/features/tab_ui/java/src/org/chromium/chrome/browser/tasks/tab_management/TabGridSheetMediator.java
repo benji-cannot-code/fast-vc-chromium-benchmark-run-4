e79a055be89f2f550c3bee4d5314cc30b57b1d2c
Yue Zhang <yuezhanggg@chromium.org>
2019-09-09 13:03:26 +0000

    Make content in TabGridSheet visible when initialization
    Bug: 1000202
    Change-Id: I7a28746d4cb8eb785c96b4bcc51497adebe37ace
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787891
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694724}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridSheetMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridSheetMediator.java
@@ -111,6 +111,7 @@ class TabGridSheetMediator {
         mTabModelSelector.getTabModelFilterProvider().addTabModelFilterObserver(mTabModelObserver);
         mThemeColorProvider.addThemeColorObserver(mThemeColorObserver);
         mThemeColorProvider.addTintObserver(mTintObserver);
+        mModel.set(TabGridSheetProperties.IS_MAIN_CONTENT_VISIBLE, true);
 
         // setup toolbar property model
         setupToolbarClickHandlers();
