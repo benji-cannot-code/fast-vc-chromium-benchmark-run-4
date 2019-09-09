69e17b3dd982bf91deca5259d7767912170cf0be
Tarun Bansal <tbansal@chromium.org>
2019-09-09 22:37:20 +0000

    Add few more browswer tests for optimization hints fetcher
    Add browsertests that check the interaction between the site
    engagement service and top hosts provider.
    
    Change-Id: I8161c9c823ff98027ddb5674fead173606232ed3
    Bug: 999541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790289
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694958}

--- chrome/browser/previews/previews_service.h
+++ chrome/browser/previews/previews_service.h
@@ -15,9 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/single_thread_task_runner.h"
+#include "chrome/browser/data_saver/data_saver_top_host_provider.h"
 #include "chrome/browser/previews/previews_lite_page_decider.h"
 #include "components/blacklist/opt_out_blacklist/opt_out_blacklist_data.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/optimization_guide/top_host_provider.h"
 #include "third_party/re2/src/re2/re2.h"
 
 namespace base {
@@ -113,6 +115,10 @@ class PreviewsService : public KeyedService {
   // Returns true if |url| patially matches any of the regular expressions for
   // which DeferAllScript preview can't be shown.
   bool MatchesDeferAllScriptDenyListRegexp(const GURL& url) const;
+
+  optimization_guide::TopHostProvider* GetTopHostProviderForTesting() const {
+    return top_host_provider_.get();
+  }
 
  private:
   // The top site provider for use with the Previews Optimization Guide's Hints
