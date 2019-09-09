5ef9b96030fccb0ce64200e338aa2902197b7564
Miyoung Shin <myid.shin@igalia.com>
2019-09-09 11:17:57 +0000

    Convert DeviceNotifier and DeviceListener to new Mojo types
    This CL converts DeviceNotifierPtr and mojo::Binding<DeviceListener>
    in content to the new Mojo types.
    
    Bug: 955171
    Change-Id: I5636f652ea7ed3211dd725b5c3d4c8fa20b107f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786888
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#694701}

--- content/browser/renderer_host/media/media_devices_manager.cc
+++ content/browser/renderer_host/media/media_devices_manager.cc
@@ -34,7 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/audio_device_description.h"
 #include "media/audio/audio_system.h"
 #include "media/base/media_switches.h"
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "services/audio/public/mojom/constants.mojom.h"
 #include "services/audio/public/mojom/device_notifications.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
@@ -300,8 +301,7 @@ MediaDevicesManager::SubscriptionRequest::operator=(SubscriptionRequest&&) =
 class MediaDevicesManager::AudioServiceDeviceListener
     : public audio::mojom::DeviceListener {
  public:
-  explicit AudioServiceDeviceListener(service_manager::Connector* connector)
-      : binding_(this) {
+  explicit AudioServiceDeviceListener(service_manager::Connector* connector) {
     TryConnectToService(connector);
   }
   ~AudioServiceDeviceListener() override = default;
@@ -338,22 +338,21 @@ class MediaDevicesManager::AudioServiceDeviceListener
   void DoConnectToService(service_manager::Connector* connector) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     DCHECK(!mojo_audio_device_notifier_);
-    DCHECK(!binding_);
-    connector->BindInterface(audio::mojom::kServiceName,
-                             mojo::MakeRequest(&mojo_audio_device_notifier_));
-    mojo_audio_device_notifier_.set_connection_error_handler(base::BindOnce(
+    DCHECK(!receiver_.is_bound());
+    connector->Connect(
+        audio::mojom::kServiceName,
+        mojo_audio_device_notifier_.BindNewPipeAndPassReceiver());
+    mojo_audio_device_notifier_.set_disconnect_handler(base::BindOnce(
         &MediaDevicesManager::AudioServiceDeviceListener::OnConnectionError,
         weak_factory_.GetWeakPtr(), connector));
-    audio::mojom::DeviceListenerPtr audio_device_listener_ptr;
-    binding_.Bind(mojo::MakeRequest(&audio_device_listener_ptr));
     mojo_audio_device_notifier_->RegisterListener(
-        audio_device_listener_ptr.PassInterface());
+        receiver_.BindNewPipeAndPassRemote());
   }
 
   void OnConnectionError(service_manager::Connector* connector) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     mojo_audio_device_notifier_.reset();
-    binding_.Close();
+    receiver_.reset();
 
     // Resetting the error handler in a posted task since doing it synchronously
     // results in a browser crash. See https://crbug.com/845142.
@@ -363,8 +362,8 @@ class MediaDevicesManager::AudioServiceDeviceListener
                        weak_factory_.GetWeakPtr(), connector));
   }
 
-  mojo::Binding<audio::mojom::DeviceListener> binding_;
-  audio::mojom::DeviceNotifierPtr mojo_audio_device_notifier_;
+  mojo::Receiver<audio::mojom::DeviceListener> receiver_{this};
+  mojo::Remote<audio::mojom::DeviceNotifier> mojo_audio_device_notifier_;
 
   SEQUENCE_CHECKER(sequence_checker_);
 
