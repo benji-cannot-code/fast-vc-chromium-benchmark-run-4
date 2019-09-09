0ba6acd8904432b18464cad386e7ab383cecd6ae
Nicholas Hollingum <hollingum@google.com>
2019-09-09 23:47:50 +0000

    Wiring up the shell surface and crostini
    For more information on the crostini force-close UI, see
    crbug.com/964162.
    
    To provide a force-close UI to crostini windows which don't respond.
    We use a window property, set by the shell surface, and look for that
    property during crostini window registration.
    
    See crrev.com/c/1776140 for the changes that allow us to know when a
    shell surface is attempting to close.
    
    This CL shows how we will wire those up. The actual UI work will be
    done in a separate CL.
    
    Bug: 914322, 964162
    Change-Id: Ieafd9bbaf8fe49e3411f4f76dabe52b63d7e784b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1775884
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694991}

--- components/exo/shell_surface_util.cc
+++ components/exo/shell_surface_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/shell_surface_util.h"
 
 #include "base/trace_event/trace_event.h"
+#include "components/exo/shell_surface_base.h"
 #include "components/exo/surface.h"
 #include "components/exo/wm_helper.h"
 #include "ui/aura/client/capture_client.h"
@@ -62,6 +63,16 @@ void SetShellMainSurface(aura::Window* window, Surface* surface) {
 
 Surface* GetShellMainSurface(const aura::Window* window) {
   return window->GetProperty(kMainSurfaceKey);
+}
+
+ShellSurfaceBase* GetShellSurfaceBaseForWindow(aura::Window* window) {
+  // Only windows with a surface can have a shell surface.
+  if (!GetShellMainSurface(window))
+    return nullptr;
+  views::Widget* widget = views::Widget::GetWidgetForNativeWindow(window);
+  if (!widget)
+    return nullptr;
+  return static_cast<ShellSurfaceBase*>(widget->widget_delegate());
 }
 
 Surface* GetTargetSurfaceForLocatedEvent(ui::LocatedEvent* event) {
