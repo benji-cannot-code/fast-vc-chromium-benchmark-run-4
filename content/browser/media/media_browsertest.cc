63573595642268a1c018db6b0a5419e0738dc652
Dale Curtis <dalecurtis@chromium.org>
2019-09-09 23:00:32 +0000

    Revert to waiting for kHaveFutureData for "can stay suspended"
    As with 69e8f3069ac190128d51151f3effb92060ce9c03 and subsequently
    ad645f7af818c484bcb189a01d1142c184dbaf9f. It appears that allowing
    media elements to remain suspended while in the metadata state is
    not working as expected.
    
    This change reverts our play state calculations to requiring the
    element to reach the future data state to remain suspended. This is
    okay for preload=metadata suspend and lazy load suspend since we
    advance to have future data for these modes (in order to not break
    any pages).
    
    This patch also modifies our browser tests to expose the suspend
    state for the element under test using the window.internals()
    methods so we can debug this further in case this turns out not
    to be the right root cause.
    
    BUG=986021
    TEST=updated tests.
    R=sandersd
    
    Change-Id: Idae559195c238edd3ccf6b2477ae18c3dfbda7c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1793129
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694969}

--- content/browser/media/media_browsertest.cc
+++ content/browser/media/media_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
+#include "content/shell/common/shell_switches.h"
 #include "media/base/media_switches.h"
 #include "media/base/test_data_util.h"
 #include "media/media_buildflags.h"
@@ -31,6 +32,8 @@ void MediaBrowserTest::SetUpCommandLine(base::CommandLine* command_line) {
   command_line->AppendSwitchASCII(
       switches::kAutoplayPolicy,
       switches::autoplay::kNoUserGestureRequiredPolicy);
+  command_line->AppendSwitch(switches::kExposeInternalsForTesting);
+
   // Disable fallback after decode error to avoid unexpected test pass on the
   // fallback path.
   scoped_feature_list_.InitAndDisableFeature(media::kFallbackAfterDecodeError);
