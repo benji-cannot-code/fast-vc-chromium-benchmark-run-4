ea6b28044477140585aa97ef1a74921ee76b2f37
Anton Bikineev <bikineev@chromium.org>
2019-09-09 15:23:05 +0000

    Reland "heap: Avoid eager destruction of heap collections allocated on stack"
    Before this CL the rules for GarbageCollectedFinalized were diverging
    from standard C++ std::is_trivially_destructible because heap
    collections had a destructor that would be ignored because the classes
    were not required to be GarbageCollectedFinalized.
    
    After the CL, std::is_trivially_destructible can be used as replacement
    for GarbageCollectedFinalized.
    
    Benchmarks (blink_perf.dom, blink_perf.bindings, speedometer) show no
    regressions.
    
    Bug: 990913
    Change-Id: I62de9fd28238ea02ee440f3244007f1d26a97b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742147
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#685557}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789809
    Cr-Commit-Position: refs/heads/master@{#694773}

--- third_party/blink/renderer/platform/heap/heap_allocator.h
+++ third_party/blink/renderer/platform/heap/heap_allocator.h
@@ -496,6 +496,8 @@ class HeapHashMap : public HashMap<KeyArg,
   DISALLOW_NEW();
 
   static void CheckType() {
+    static_assert(std::is_trivially_destructible<HeapHashMap>::value,
+                  "HeapHashMap must be trivially destructible.");
     static_assert(
         IsAllowedInContainer<KeyArg>::value,
         "Not allowed to directly nest type. Use Member<> indirection instead.");
@@ -505,7 +507,7 @@ class HeapHashMap : public HashMap<KeyArg,
     static_assert(
         WTF::IsTraceable<KeyArg>::value || WTF::IsTraceable<MappedArg>::value,
         "For hash maps without traceable elements, use HashMap<> "
-        "instead of HeapHashMap<>");
+        "instead of HeapHashMap<>.");
   }
 
  public:
@@ -527,12 +529,14 @@ class HeapHashSet
   DISALLOW_NEW();
 
   static void CheckType() {
+    static_assert(std::is_trivially_destructible<HeapHashSet>::value,
+                  "HeapHashSet must be trivially destructible.");
     static_assert(
         IsAllowedInContainer<ValueArg>::value,
         "Not allowed to directly nest type. Use Member<> indirection instead.");
     static_assert(WTF::IsTraceable<ValueArg>::value,
                   "For hash sets without traceable elements, use HashSet<> "
-                  "instead of HeapHashSet<>");
+                  "instead of HeapHashSet<>.");
   }
 
  public:
@@ -561,7 +565,7 @@ class HeapLinkedHashSet
         "Not allowed to directly nest type. Use Member<> indirection instead.");
     static_assert(WTF::IsTraceable<ValueArg>::value,
                   "For sets without traceable elements, use LinkedHashSet<> "
-                  "instead of HeapLinkedHashSet<>");
+                  "instead of HeapLinkedHashSet<>.");
   }
 
  public:
@@ -587,12 +591,14 @@ class HeapListHashSet
   DISALLOW_NEW();
 
   static void CheckType() {
+    static_assert(std::is_trivially_destructible<HeapListHashSet>::value,
+                  "HeapListHashSet must be trivially destructible.");
     static_assert(
         IsAllowedInContainer<ValueArg>::value,
         "Not allowed to directly nest type. Use Member<> indirection instead.");
     static_assert(WTF::IsTraceable<ValueArg>::value,
                   "For sets without traceable elements, use ListHashSet<> "
-                  "instead of HeapListHashSet<>");
+                  "instead of HeapListHashSet<>.");
   }
 
  public:
@@ -613,12 +619,14 @@ class HeapHashCountedSet
   DISALLOW_NEW();
 
   static void CheckType() {
+    static_assert(std::is_trivially_destructible<HeapHashCountedSet>::value,
+                  "HeapHashCountedSet must be trivially destructible.");
     static_assert(
         IsAllowedInContainer<Value>::value,
         "Not allowed to directly nest type. Use Member<> indirection instead.");
     static_assert(WTF::IsTraceable<Value>::value,
                   "For counted sets without traceable elements, use "
-                  "HashCountedSet<> instead of HeapHashCountedSet<>");
+                  "HashCountedSet<> instead of HeapHashCountedSet<>.");
   }
 
  public:
@@ -637,11 +645,14 @@ class HeapVector : public Vector<T, inlineCapacity, HeapAllocator> {
 
   static void CheckType() {
     static_assert(
+        std::is_trivially_destructible<HeapVector>::value || inlineCapacity,
+        "HeapVector must be trivially destructible.");
+    static_assert(
         IsAllowedInContainer<T>::value,
         "Not allowed to directly nest type. Use Member<> indirection instead.");
     static_assert(WTF::IsTraceable<T>::value,
                   "For vectors without traceable elements, use Vector<> "
-                  "instead of HeapVector<>");
+                  "instead of HeapVector<>.");
   }
 
  public:
@@ -684,6 +695,9 @@ class HeapDeque : public Deque<T, inlineCapacity, HeapAllocator> {
   DISALLOW_NEW();
 
   static void CheckType() {
+    static_assert(
+        std::is_trivially_destructible<HeapDeque>::value || inlineCapacity,
+        "HeapDeque must be trivially destructible.");
     static_assert(
         IsAllowedInContainer<T>::value,
         "Not allowed to directly nest type. Use Member<> indirection instead.");
