df21d9ab28a0fc614dd1d0c5128df78cbc33b059
Esmael El-Moslimany <aee@chromium.org>
2019-09-09 19:13:23 +0000

    WebUI: cr-slider, force the label to be redrawn after the location is updated
    Bug: 980856
    Change-Id: Ia28c8b4fe90b7fdad5c43660e3a4006a8673c321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788225
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#694858}

--- ui/webui/resources/cr_elements/cr_slider/cr_slider.js
+++ ui/webui/resources/cr_elements/cr_slider/cr_slider.js
@@ -7,9 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview 'cr-slider' is a slider component used to select a number from
  * a continuous or discrete range of numbers.
  */
-
-
-
 cr.define('cr_slider', function() {
   /**
    * The |value| is the corresponding value that the current slider tick is
@@ -190,7 +187,6 @@ cr.define('cr_slider', function() {
 
     /** @private {Map<string, number>} */
     deltaKeyMap_: null,
-
 
     /** @private {EventTracker} */
     draggingEventTracker_: null,
