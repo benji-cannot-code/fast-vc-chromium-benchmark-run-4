957943dc669032f544f8ed905d11a4f188c5f636
Dave Tapuska <dtapuska@chromium.org>
2019-09-10 14:23:05 +0000

    Fix history crash with null DocumentLoader
    Its possible that the frame doesn't have a document loader anymore.
    Check document loader like other paths do in the History object.
    
    BUG=1000206
    
    Change-Id: Idc1eda15c194e26ff818855fc1ac5e595ae693a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1791421
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695151}

--- third_party/blink/renderer/core/frame/history.cc
+++ third_party/blink/renderer/core/frame/history.cc
@@ -90,7 +90,7 @@ SerializedScriptValue* History::state(ExceptionState& exception_state) {
 }
 
 SerializedScriptValue* History::StateInternal() const {
-  if (!GetFrame())
+  if (!GetFrame() || !GetFrame()->Loader().GetDocumentLoader())
     return nullptr;
 
   if (HistoryItem* history_item =
