e0183af9c9d948696503305bd9e4b7e308834152
Gabriel Charette <gab@chromium.org>
2019-09-10 14:27:55 +0000

    Backport some tests in /ui/events/gesture_detection to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /ui/events/gesture_detection.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=nzolghadr@chromium.org
    
    Bug: 891670
    Change-Id: I3f1e76a565e3cbecb3b5f34e8cb96620fa68df27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787075
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695154}

--- ui/events/gesture_detection/filtered_gesture_provider_unittest.cc
+++ ui/events/gesture_detection/filtered_gesture_provider_unittest.cc
@@ -15,7 +15,8 @@ class FilteredGestureProviderTest : public GestureProviderClient,
                                     public testing::Test {
  public:
   FilteredGestureProviderTest()
-      : task_environment_(base::test::TaskEnvironment::MainThreadType::UI) {}
+      : task_environment_(
+            base::test::SingleThreadTaskEnvironment::MainThreadType::UI) {}
   ~FilteredGestureProviderTest() override {}
 
   // GestureProviderClient implementation.
@@ -23,7 +24,7 @@ class FilteredGestureProviderTest : public GestureProviderClient,
   bool RequiresDoubleTapGestureEvents() const override { return false; }
 
  private:
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 };
 
 // Single touch drag test: After touch-start, the moved_beyond_slop_region bit
--- ui/events/gesture_detection/gesture_provider_unittest.cc
+++ ui/events/gesture_detection/gesture_provider_unittest.cc
@@ -67,7 +67,8 @@ gfx::RectF BoundsForSingleMockTouchAtLocation(float x, float y) {
 class GestureProviderTest : public testing::Test, public GestureProviderClient {
  public:
   GestureProviderTest()
-      : task_environment_(base::test::TaskEnvironment::MainThreadType::UI) {}
+      : task_environment_(
+            base::test::SingleThreadTaskEnvironment::MainThreadType::UI) {}
   ~GestureProviderTest() override {}
 
   static MockMotionEvent ObtainMotionEvent(base::TimeTicks event_time,
@@ -438,7 +439,7 @@ class GestureProviderTest : public testing::Test, public GestureProviderClient {
   std::vector<GestureEventData> gestures_;
   std::unique_ptr<GestureProvider> gesture_provider_;
   std::unique_ptr<GestureEventData> active_scroll_begin_event_;
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   bool should_process_double_tap_events_ = true;
 };
 
