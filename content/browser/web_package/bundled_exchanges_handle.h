ab37f6a5afa837cd47e1c852f7d2f98421ce7576
Tsuyoshi Horo <horo@chromium.org>
2019-09-10 04:12:45 +0000

    Cleanup PrimaryURLRedirectLoader in BundledExchangesHandle
    This CL do the following refactoring:
     - Move PrimaryURLRedirectLoader to anonymous namespace.
     - Remove unnecessary methods, arguments, casting.
    
    And this CL sets encoded_data_length = 0 for redirect response. This is
    because we have "DCHECK_GE(redirect_response.EncodedDataLength(), 0)"
    check in resource_timing_info.cc.
    
    Bug: 966753
    Change-Id: I35e877c0e928e596596d3743175d65241ca7bed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1792413
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695030}

--- content/browser/web_package/bundled_exchanges_handle.h
+++ content/browser/web_package/bundled_exchanges_handle.h
@@ -49,8 +49,6 @@ class BundledExchangesHandle final {
   bool IsReadyForLoading();
 
  private:
-  class PrimaryURLRedirectLoader;
-
   void CreateURLLoader(const network::ResourceRequest& resource_request,
                        network::mojom::URLLoaderRequest request,
                        network::mojom::URLLoaderClientPtr client);
