f606fecd8cb69d5dbe0dd89dcca611a1a8417e66
Jérôme Lebel <jlebel@chromium.org>
2019-09-10 16:27:24 +0000

    [iOS] Adding -[SigninEarlGreyUI signOutWithManagedAccount:]
    Adding -[SigninEarlGreyUI signOutWithManagedAccount:] and removing
    some calls for TapButtonWithLabelId().
    
    Bug: 684817
    Change-Id: Ie99d6c64eb5566e9f423f19e0b856b32c6d8d525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1795584
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695203}

--- ios/chrome/browser/metrics/ukm_egtest.mm
+++ ios/chrome/browser/metrics/ukm_egtest.mm
@@ -38,12 +38,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using chrome_test_util::AccountsSyncButton;
 using chrome_test_util::ButtonWithAccessibilityLabel;
 using chrome_test_util::ButtonWithAccessibilityLabelId;
-using chrome_test_util::ClearBrowsingDataView;
 using chrome_test_util::GoogleServicesSettingsButton;
 using chrome_test_util::SettingsAccountButton;
 using chrome_test_util::SettingsDoneButton;
 using chrome_test_util::SettingsMenuPrivacyButton;
-using chrome_test_util::SignOutAccountsButton;
 using chrome_test_util::SyncSwitchCell;
 using chrome_test_util::TurnSyncSwitchOn;
 
--- ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h
+++ ios/chrome/browser/ui/authentication/signin_earl_grey_ui.h
@@ -48,6 +48,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Checks that the sign-in promo view is not visible.
 + (void)checkSigninPromoNotVisible;
 
+// Signs out from the current identity. if |isManagedAccount| is true, the
+// confirmed managed dialog is confirmed while signing out.
++ (void)signOutWithManagedAccount:(BOOL)isManagedAccount;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_EARL_GREY_UI_H_
