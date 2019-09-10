c6416dea859af364bf932f306bd366bb02771b12
Robbie Gibson <rkgibson@google.com>
2019-09-10 14:53:37 +0000

    [iOS][Dark Mode] Replace remaining label colors with dark mode versions
    The changes are replacing darker, primary text with kTextPrimaryColor
    and lighter, secondary text with kTextSecondaryColor.
    
    Bug: 1002409
    Change-Id: I2a1cfa0eb5907cc1c40b722c2ddbd9285187a56a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1794865
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695164}

--- ios/chrome/browser/ui/settings/cells/search_engine_item.mm
+++ ios/chrome/browser/ui/settings/cells/search_engine_item.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 #include "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #include "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -55,7 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     cell.titleLabel.textColor = styler.cellTitleColor;
   }
 
-  cell.URLLabel.textColor = UIColorFromRGB(kSettingsCellsURLTextColor);
+  cell.URLLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
 
   [cell configureUILayout];
 }
--- ios/chrome/browser/ui/settings/language/language_details_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/language/language_details_table_view_controller.mm
@@ -117,8 +117,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   }
   if (!self.languageItem.canOfferTranslate) {
     offerTranslateItem.enabled = NO;
-    offerTranslateItem.textColor =
-        UIColorFromRGB(kSettingsCellsDetailTextColor);
+    offerTranslateItem.textColor = [UIColor colorNamed:kTextSecondaryColor];
   }
   [model addItem:offerTranslateItem
       toSectionWithIdentifier:SectionIdentifierOptions];
