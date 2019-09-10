3c9f17b9fc95ff96b16a3ad6aa18639620f756eb
James Hawkins <jhawkins@chromium.org>
2019-09-10 04:54:39 +0000

    Internet detail dialog: Focus the action button on load.
    This is modeled after the same functionality in the internet detail
    page implementation.
    
    R=stevenjb@chromium.org
    
    Bug: 999474
    Test: none
    Change-Id: If5dcac633d5881aa04878b0e57223a437c9af4bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1793808
    Commit-Queue: James Hawkins <jhawkins@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695049}

--- chrome/browser/resources/chromeos/internet_detail_dialog/internet_detail_dialog.js
+++ chrome/browser/resources/chromeos/internet_detail_dialog/internet_detail_dialog.js
@@ -25,7 +25,10 @@ Polymer({
     guid: String,
 
     /** @private {!chromeos.networkConfig.mojom.ManagedProperties|undefined} */
-    managedProperties_: Object,
+    managedProperties_: {
+      type: Object,
+      observer: 'managedPropertiesChanged_',
+    },
 
     /** @private {?OncMojo.DeviceStateProperties} */
     deviceState_: {
@@ -54,6 +57,12 @@ Polymer({
       }
     },
   },
+
+  /**
+   * Set to true once the action button has been focused.
+   * @private {boolean}
+   */
+  didSetFocus_: false,
 
   /**
    * Set to true to once the initial properties have been received. This
@@ -133,6 +142,21 @@ Polymer({
           loadTimeData.getString('networkListItemNoNetwork'),
       vpnNameTemplate: loadTimeData.getString('vpnNameTemplate'),
     };
+  },
+
+  /** @private */
+  managedPropertiesChanged_: function() {
+    assert(this.managedProperties_);
+
+    // Focus the action button once the initial state is set.
+    if (!this.didSetFocus_ &&
+        this.showConnectDisconnect_(this.managedProperties_)) {
+      const button = this.$$('#title .action-button:not([hidden])');
+      if (button) {
+        button.focus();
+        this.didSetFocus_ = true;
+      }
+    }
   },
 
   /** @private */
