55d32f9413a9b7f29f6674ad42f23017c3ccd62b
Himanshu Jaju <himanshujaju@chromium.org>
2019-09-10 16:26:20 +0000

    Zero state for shared clipboard
    Adds illustration to educate user on how to use devices
    with shared clipboard. Button to open chrome settings page
    to turn on sync would be a follow up CL.
    
    Dark mode - https://drive.google.com/open?id=1bgNYKQIAppVpqDfknpntFderdEzcLkYL
    Light mode - https://drive.google.com/open?id=106EBilI5KGzpdpPzLgrQUccmgwpFAT5s
    
    Bug: 1001102
    Change-Id: I48815dc1822657125ca7538b6ab69c3fa8b38886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790202
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695201}

--- chrome/android/java/src/org/chromium/chrome/browser/sharing/shared_clipboard/SharedClipboardShareActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sharing/shared_clipboard/SharedClipboardShareActivity.java
@@ -14,7 +14,6 @@ import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
@@ -74,13 +73,10 @@ public class SharedClipboardShareActivity
         View mask = findViewById(R.id.mask);
         mask.setOnClickListener(v -> finish());
 
-        TextView toolbarText = findViewById(R.id.device_picker_toolbar);
-        toolbarText.setText(R.string.send_tab_to_self_sheet_toolbar);
-
         mListView = findViewById(R.id.device_picker_list);
         mListView.setAdapter(mAdapter);
         mListView.setOnItemClickListener(this);
-        mListView.setEmptyView(findViewById(android.R.id.empty));
+        mListView.setEmptyView(findViewById(R.id.empty_state));
 
         View content = findViewById(R.id.device_picker_content);
         content.startAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_up));
@@ -99,6 +95,10 @@ public class SharedClipboardShareActivity
         super.finishNativeInitialization();
 
         mAdapter = new SharingAdapter(SharingDeviceCapability.SHARED_CLIPBOARD);
+        if (!mAdapter.isEmpty()) {
+            findViewById(R.id.device_picker_toolbar).setVisibility(View.VISIBLE);
+        }
+
         mListView.setAdapter(mAdapter);
     }
 
