9f32c880aa66db2a262764096fb0f8def10e1128
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-09-10 20:19:17 +0000

    Printing API: Extend CupsPrintJob with print settings
    Bug: 992889
    Change-Id: I47b47236a4baa9f508373bf74e66130a6236c2f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1774920
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695303}

---
+++ chrome/browser/chromeos/printing/history/print_job_info_conversions.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_PRINTING_HISTORY_PRINT_JOB_INFO_CONVERSIONS_H_
+#define CHROME_BROWSER_CHROMEOS_PRINTING_HISTORY_PRINT_JOB_INFO_CONVERSIONS_H_
+
+#include "chrome/browser/chromeos/printing/history/print_job_info.pb.h"
+#include "printing/print_settings.h"
+
+namespace chromeos {
+
+// This conversion is used to store only necessary part of huge PrintSettings
+// object in proto format. This proto is later used for saving print job
+// history.
+printing::proto::PrintSettings PrintSettingsToProto(
+    const ::printing::PrintSettings& settings);
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_PRINTING_HISTORY_PRINT_JOB_INFO_CONVERSIONS_H_
