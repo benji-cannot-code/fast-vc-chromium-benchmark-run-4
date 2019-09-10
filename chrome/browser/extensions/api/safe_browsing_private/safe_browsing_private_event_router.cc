9ca3d554c41d9879696b9083ff9c3a0930c76ed2
Roger Tawa <rogerta@chromium.org>
2019-09-10 22:46:24 +0000

    Enable reporting api feature by default.
    Bug: 1002517
    Change-Id: Ie68223675f5802619ae3c317e0e06fbd8bb4ac9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1796043
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695362}

--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.cc
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/policy/browser_dm_token_storage.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
+#include "chrome/browser/policy/machine_level_user_cloud_policy_controller.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
@@ -32,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 const base::Feature SafeBrowsingPrivateEventRouter::kRealtimeReportingFeature{
-    "SafeBrowsingRealtimeReporting", base::FEATURE_DISABLED_BY_DEFAULT};
+    "SafeBrowsingRealtimeReporting", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Key names used with when building the dictionary to pass to the real-time
 // reporting API.
@@ -297,6 +298,17 @@ void SafeBrowsingPrivateEventRouter::SetCloudPolicyClientForTesting(
 }
 
 void SafeBrowsingPrivateEventRouter::InitRealtimeReportingClient() {
+#if !defined(OS_CHROMEOS)
+  // This method is not compiled on chromeos because
+  // MachineLevelUserCloudPolicyController does not exist.  Also,
+  // policy::BrowserDMTokenStorage::Get()->RetrieveDMToken() does return a
+  // valid token either.  Once these are fixed the #if !define can be removed.
+
+  if (!policy::MachineLevelUserCloudPolicyController::
+          IsMachineLevelUserCloudPolicyEnabled()) {
+    return;
+  }
+
   if (!base::FeatureList::IsEnabled(kRealtimeReportingFeature))
     return;
 
@@ -350,6 +362,7 @@ void SafeBrowsingPrivateEventRouter::InitRealtimeReportingClient() {
         dm_token, client_id,
         /*user_affiliation_ids=*/std::vector<std::string>());
   }
+#endif
 }
 
 void SafeBrowsingPrivateEventRouter::ReportRealtimeEvent(const char* name,
--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_unittest.cc
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/mock_callback.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router_factory.h"
 #include "chrome/common/extensions/api/safe_browsing_private.h"
@@ -115,10 +114,6 @@ class SafeBrowsingPrivateEventRouterTest : public testing::Test {
     SafeBrowsingPrivateEventRouterFactory::GetInstance()->SetTestingFactory(
         profile_, base::BindRepeating(&BuildSafeBrowsingPrivateEventRouter));
 
-    // Make sure real-time feature is eanbled so that the tests will run.
-    scoped_feature_list_.InitAndEnableFeature(
-        SafeBrowsingPrivateEventRouter::kRealtimeReportingFeature);
-
     // Set a mock cloud policy client in the router.  The router will own the
     // client, but a pointer to the client is maintained in the test class to
     // manage expectations.
@@ -130,7 +125,6 @@ class SafeBrowsingPrivateEventRouterTest : public testing::Test {
 
  protected:
   content::BrowserTaskEnvironment task_environment_;
-  base::test::ScopedFeatureList scoped_feature_list_;
   TestingProfileManager profile_manager_;
   TestingProfile* profile_;
   extensions::TestEventRouter* event_router_ = nullptr;
