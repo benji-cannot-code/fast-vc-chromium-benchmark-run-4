b37976b356a6f71000bcb2378044355dfcbce188
rbpotter <rbpotter@chromium.org>
2019-09-11 22:06:04 +0000

    Web UI Polymer 3: Port cr-lazy-render
    - Autogenerating Polymer 3 version of cr-lazy-render and test
    - Adding to the demo page
    - Removing legacy templatizer behavior from Polymer 3 exports, since it
    should not be used now that cr-lazy-render is updated to use
    Polymer.Templatize.templatize() instead (see
    https://crrev.com/c/1786248).
    
    Bug: 965770
    Change-Id: I58b0cd8b19b6f668888aa48fc56c7ad9817efac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790641
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695810}

--- chrome/test/data/webui/cr_elements/cr_lazy_render_tests.js
+++ chrome/test/data/webui/cr_elements/cr_lazy_render_tests.js
@@ -3,13 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import 'chrome://resources/cr_elements/cr_lazy_render/cr_lazy_render.m.js';
+// #import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
+// #import {Polymer, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// clang-format on
+
 suite('cr-lazy-render', function() {
   let lazy;
   let bind;
 
   suiteSetup(function() {
-    return PolymerTest.importHtml(
-        'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.html');
+    /* #ignore */ return PolymerTest.importHtml(
+        /* #ignore */ 'chrome://resources/cr_elements/cr_checkbox/' +
+        /* #ignore */ 'cr_checkbox.html');
   });
 
   setup(function() {
@@ -56,7 +63,7 @@ suite('cr-lazy-render', function() {
     const inner = lazy.get();
     const checkbox = document.querySelector('cr-checkbox');
     assertTrue(checkbox.checked);
-    MockInteractions.tap(checkbox);
+    checkbox.click();
     assertFalse(checkbox.checked);
     assertFalse(bind.checked);
   });
--- ui/webui/resources/cr_elements/cr_lazy_render/cr_lazy_render.js
+++ ui/webui/resources/cr_elements/cr_lazy_render/cr_lazy_render.js
@@ -23,7 +23,7 @@ Polymer({
   /** @private {?Element} */
   child_: null,
 
-  /** @private {?Element} */
+  /** @private {?Element|?TemplateInstanceBase} */
   instance_: null,
 
   /**
@@ -55,7 +55,7 @@ Polymer({
     });
     const parentNode = this.parentNode;
     if (parentNode && !this.child_) {
-      this.instance_ = new TemplateClass({});
+      this.instance_ = new TemplateClass();
       this.child_ = this.instance_.root.firstElementChild;
       parentNode.insertBefore(this.instance_.root, this);
     }
