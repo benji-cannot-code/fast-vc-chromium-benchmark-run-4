fb2f76dd4cde3f52f423906ffbf9753467590604
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-09-11 07:11:42 +0000

    Use new downcast helper for blink::HTMLMetaElement
    This CL has two goals,
    1. Use To<HTMLMetaElement> as new downcast helper
    2. Use IsA<HTMLMetaElement>(element) in place of
       IsHTMLMetaElement(element)
    
    Bug: 891908
    Change-Id: I36dcb12b1e8be1bbc5221284a882a4fa415e553b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1792690
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#695507}

--- third_party/blink/renderer/core/exported/web_meta_element.cc
+++ third_party/blink/renderer/core/exported/web_meta_element.cc
@@ -19,7 +19,7 @@ WebMetaElement::WebMetaElement(HTMLMetaElement* element)
     : WebElement(element) {}
 
 DEFINE_WEB_NODE_TYPE_CASTS(WebMetaElement,
-                           IsHTMLMetaElement(ConstUnwrap<Node>()))
+                           IsA<HTMLMetaElement>(ConstUnwrap<Node>()))
 
 WebMetaElement& WebMetaElement::operator=(HTMLMetaElement* element) {
   private_ = element;
@@ -27,7 +27,7 @@ WebMetaElement& WebMetaElement::operator=(HTMLMetaElement* element) {
 }
 
 WebMetaElement::operator HTMLMetaElement*() const {
-  return ToHTMLMetaElement(private_.Get());
+  return blink::To<HTMLMetaElement>(private_.Get());
 }
 
 }  // namespace blink
