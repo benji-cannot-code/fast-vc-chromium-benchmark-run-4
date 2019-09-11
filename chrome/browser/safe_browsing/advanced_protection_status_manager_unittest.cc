3ced8e0f469ffa3b3a1344d91b809458b7d5e424
Daniel Rubery <drubery@chromium.org>
2019-09-11 16:50:39 +0000

    Track Unconsented Primary Account for Advanced Protection status
    The unconsented primary account is the best notion of profile identity
    for our purposes, since the additional download warnings do not
    depend on consenting to Sync. (They are gated behind the Safe Browsing
    enabled preference).
    
    Bug: 917190
    Change-Id: I61c1a8d5e47865f61e052ac75de03560e7b52e7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1793445
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695674}

--- chrome/browser/safe_browsing/advanced_protection_status_manager_unittest.cc
+++ chrome/browser/safe_browsing/advanced_protection_status_manager_unittest.cc
@@ -94,7 +94,7 @@ TEST_F(AdvancedProtectionStatusManagerTest, NotSignedInOnStartUp) {
   AdvancedProtectionStatusManager aps_manager(
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
-  ASSERT_TRUE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_TRUE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
 
   // If user's not signed-in. No refresh is required.
   EXPECT_FALSE(aps_manager.is_under_advanced_protection());
@@ -118,7 +118,7 @@ TEST_F(AdvancedProtectionStatusManagerTest,
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
   base::RunLoop().RunUntilIdle();
-  ASSERT_FALSE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_FALSE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
 
   // Waits for access token request and respond with an error without advanced
   // protection set.
@@ -149,7 +149,7 @@ TEST_F(AdvancedProtectionStatusManagerTest,
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
   base::RunLoop().RunUntilIdle();
-  ASSERT_FALSE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_FALSE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
 
   // Waits for access token request and respond with an error without advanced
   // protection set.
@@ -179,7 +179,7 @@ TEST_F(AdvancedProtectionStatusManagerTest, SignedInLongTimeAgoNotUnderAP) {
   AdvancedProtectionStatusManager aps_manager(
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
-  ASSERT_FALSE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_FALSE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
   base::RunLoop().RunUntilIdle();
   // Waits for access token request and respond with a token without advanced
   // protection set.
@@ -232,7 +232,7 @@ TEST_F(AdvancedProtectionStatusManagerTest, AlreadySignedInAndUnderAP) {
   AdvancedProtectionStatusManager aps_manager(
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
-  ASSERT_FALSE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_FALSE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
   ASSERT_TRUE(aps_manager.is_under_advanced_protection());
 
   // A refresh is scheduled in the future.
@@ -270,7 +270,7 @@ TEST_F(AdvancedProtectionStatusManagerTest, StayInAdvancedProtection) {
   AdvancedProtectionStatusManager aps_manager(
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
-  ASSERT_FALSE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_FALSE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
   ASSERT_TRUE(aps_manager.is_under_advanced_protection());
 
   // Simulate gets refresh token.
@@ -290,7 +290,7 @@ TEST_F(AdvancedProtectionStatusManagerTest, SignInAndSignOutEvent) {
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
   ASSERT_FALSE(aps_manager.is_under_advanced_protection());
-  ASSERT_TRUE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_TRUE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
 
   SignIn("test@test.com",
          /* is_under_advanced_protection = */ true);
@@ -311,7 +311,7 @@ TEST_F(AdvancedProtectionStatusManagerTest, AccountRemoval) {
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
   ASSERT_FALSE(aps_manager.is_under_advanced_protection());
-  ASSERT_TRUE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_TRUE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
 
   std::string account_id = SignIn("test@test.com",
                                   /* is_under_advanced_protection = */ false);
@@ -382,7 +382,7 @@ TEST_F(AdvancedProtectionStatusManagerTest,
   AdvancedProtectionStatusManager aps_manager(
       &pref_service_, identity_test_env_.identity_manager(),
       base::TimeDelta() /*no min delay*/);
-  ASSERT_FALSE(aps_manager.GetPrimaryAccountId().empty());
+  ASSERT_FALSE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
   ASSERT_TRUE(aps_manager.is_under_advanced_protection());
   EXPECT_TRUE(aps_manager.IsRefreshScheduled());
 
@@ -394,5 +394,30 @@ TEST_F(AdvancedProtectionStatusManagerTest,
 
   aps_manager.UnsubscribeFromSigninEvents();
 }
+
+// On ChromeOS, there is no unconsented primary account. We can only track the
+// primary account.
+#if !defined(OS_CHROMEOS)
+TEST_F(AdvancedProtectionStatusManagerTest, TracksUnconsentedPrimaryAccount) {
+  AdvancedProtectionStatusManager aps_manager(
+      &pref_service_, identity_test_env_.identity_manager(),
+      base::TimeDelta() /*no min delay*/);
+  ASSERT_FALSE(aps_manager.is_under_advanced_protection());
+  ASSERT_TRUE(aps_manager.GetUnconsentedPrimaryAccountId().empty());
+
+  // Sign in, but don't set this as the primary account.
+  AccountInfo account_info =
+      identity_test_env_.MakeAccountAvailable("test@test.com");
+  account_info.is_under_advanced_protection = true;
+  identity_test_env_.SetCookieAccounts(
+      {{account_info.email, account_info.gaia}});
+  identity_test_env_.UpdateAccountInfoForAccount(account_info);
+
+  EXPECT_TRUE(aps_manager.is_under_advanced_protection());
+  EXPECT_TRUE(aps_manager.IsRefreshScheduled());
+
+  aps_manager.UnsubscribeFromSigninEvents();
+}
+#endif
 
 }  // namespace safe_browsing
