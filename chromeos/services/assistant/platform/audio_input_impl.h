5ddd6fa0f89fb3c8b977f843d69f0b21ca34b9a9
Meilin Wang <meilinw@google.com>
2019-09-11 21:07:25 +0000

    assistant: Check for lid state when DSP is enabled.
    This change adds logic to check for the device lid state by observing
    the lid state switch when DSP is enabled, and stop DSP from recording
    when the lid is closed. In this way, the Assistant cannot be waked up
    by voice queries when the lid is closed.
    
    Bug: b/139313417
    Test: run unittest in this CL.
    Change-Id: I1765db4f16e43ac7a5de71acde9dff253be8f363
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1758991
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695778}

--- chromeos/services/assistant/platform/audio_input_impl.h
+++ chromeos/services/assistant/platform/audio_input_impl.h
@@ -10,11 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/component_export.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
+#include "base/scoped_observer.h"
 #include "base/sequence_checker.h"
 #include "base/synchronization/lock.h"
 #include "base/time/time.h"
+#include "chromeos/dbus/power/power_manager_client.h"
 #include "chromeos/services/assistant/public/mojom/assistant.mojom.h"
 #include "libassistant/shared/public/platform_audio_input.h"
 #include "media/base/audio_capturer_source.h"
@@ -23,10 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace assistant {
 
-class AudioInputImpl : public assistant_client::AudioInput,
-                       public media::AudioCapturerSource::CaptureCallback {
+class COMPONENT_EXPORT(ASSISTANT_SERVICE) AudioInputImpl
+    : public assistant_client::AudioInput,
+      public media::AudioCapturerSource::CaptureCallback,
+      public chromeos::PowerManagerClient::Observer {
  public:
   AudioInputImpl(mojom::Client* client,
+                 chromeos::PowerManagerClient* power_manager_client,
                  const std::string& device_id,
                  const std::string& hotword_device_id);
   ~AudioInputImpl() override;
@@ -65,6 +71,10 @@ class AudioInputImpl : public assistant_client::AudioInput,
   void RemoveObserver(
       assistant_client::AudioInput::Observer* observer) override;
 
+  // chromeos::PowerManagerClient::Observer overrides:
+  void LidEventReceived(chromeos::PowerManagerClient::LidState state,
+                        const base::TimeTicks& timestamp) override;
+
   // Called when the mic state associated with the interaction is changed.
   void SetMicState(bool mic_open);
   void OnConversationTurnStarted();
@@ -82,10 +92,17 @@ class AudioInputImpl : public assistant_client::AudioInput,
 
   bool IsHotwordAvailable();
 
+  // Returns the recording state used in unittests.
+  bool IsRecordingForTesting() const;
+
  private:
   void StartRecording();
   void StopRecording();
   void UpdateRecordingState();
+
+  // Updates lid state from received switch states.
+  void OnSwitchStatesReceived(
+      base::Optional<chromeos::PowerManagerClient::SwitchStates> switch_states);
 
   scoped_refptr<media::AudioCapturerSource> source_;
 
@@ -112,6 +129,11 @@ class AudioInputImpl : public assistant_client::AudioInput,
 
   mojom::Client* const client_;
 
+  chromeos::PowerManagerClient* power_manager_client_;
+  ScopedObserver<chromeos::PowerManagerClient,
+                 chromeos::PowerManagerClient::Observer>
+      power_manager_client_observer_;
+
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
   std::unique_ptr<HotwordStateManager> state_manager_;
@@ -120,6 +142,9 @@ class AudioInputImpl : public assistant_client::AudioInput,
   std::string device_id_;
   // Hotword input device used for hardware based hotword detection.
   std::string hotword_device_id_;
+
+  chromeos::PowerManagerClient::LidState lid_state_ =
+      chromeos::PowerManagerClient::LidState::NOT_PRESENT;
 
   base::WeakPtrFactory<AudioInputImpl> weak_factory_;
   DISALLOW_COPY_AND_ASSIGN(AudioInputImpl);
