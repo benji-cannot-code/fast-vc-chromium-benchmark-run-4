580d8670cbc0bdd2db3ec2e7b15e03fc83174452
Luciano Pacheco <lucmult@chromium.org>
2019-09-11 06:09:12 +0000

    Files app: ES6 class ui/file_manager/file_manager/foreground/js/ui/table/table_splitter.js
    Since cr.ui.table* is now Files app-only I removed the FileTableSplitter and merged it
    directly into TableSplitter and the additional "decorate()" call from FileTable.
    
    Move TableSplitter outside of "cr.ui" namespace so it doesn't use the cr.define() which
    uses a special constructor "createElementHelper" which forces a wrapping function changing
    the prototype and calling decorate(). Note that this "special constructor" is still in use
    in its base class cr.ui.Splitter, so restoring TableSplitter prototype is required.
    
    Test: manually resized columns
    Bug: 778674
    Change-Id: I9bfb6fc757c431748ff896134b662113d417b976
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1792211
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695500}

--- ui/file_manager/file_manager/foreground/js/ui/table/table_header.js
+++ ui/file_manager/file_manager/foreground/js/ui/table/table_header.js
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 cr.define('cr.ui.table', function() {
-  /** @const */ const TableSplitter = cr.ui.TableSplitter;
-
   /**
    * Creates a new table header.
    * @param {Object=} opt_propertyBag Optional properties.
@@ -209,7 +207,7 @@ cr.define('cr.ui.table', function() {
       let minDistance = TableHeader.TOUCH_DRAG_AREA_WIDTH;
       let candidate;
 
-      const splitters = /** @type {NodeList<cr.ui.TableSplitter>} */ (
+      const splitters = /** @type {NodeList<TableSplitter>} */ (
           this.querySelectorAll('.table-header-splitter'));
       for (let i = 0; i < splitters.length; i++) {
         const r = splitters[i].getBoundingClientRect();
