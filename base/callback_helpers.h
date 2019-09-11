62c7cbeb989c57b519636e3493bcbf746d2c49db
Khushal <khushalsagar@chromium.org>
2019-09-11 01:12:09 +0000

    viz: Add temp CHECKs for debugging.
    R=kylechar@chromium.org
    
    Bug: 992626
    Change-Id: If13d2f4c9660c49db82e586929b2686b54f3760a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797128
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695430}

--- base/callback_helpers.h
+++ base/callback_helpers.h
@@ -115,6 +115,8 @@ class BASE_EXPORT ScopedClosureRunner {
   // Releases the Closure without calling.
   OnceClosure Release() WARN_UNUSED_RESULT;
 
+  bool is_null() const { return closure_.is_null(); }
+
  private:
   OnceClosure closure_;
 
