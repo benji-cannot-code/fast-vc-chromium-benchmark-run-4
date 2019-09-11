bade648be9f19d7168851429dd3ffabbe38766b0
Yoav Weiss <yoavweiss@chromium.org>
2019-09-11 13:28:25 +0000

    [resource-timing] Report performance entries with failing status codes
    Currently we don't report performance entries with failing status codes.
    From the spec's perspective, reporting aborts is a MAY, but failing
    status code responses should not be considered aborts. [1]
    Chromium is the only engine which doesn't report those entries.
    This CL fixes that to report them similarly to successful status codes.
    
    Bug: 883400, 990849
    Change-Id: Ic5e99e3df77f3869aa0dd70f0141d88016fdb972
    
    [1] https://github.com/w3c/resource-timing/issues/165#issuecomment-441413636
    
    Change-Id: Ic5e99e3df77f3869aa0dd70f0141d88016fdb972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1796544
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695596}

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -1794,8 +1794,7 @@ void ResourceFetcher::HandleLoaderFinish(Resource* resource,
 
   if (scoped_refptr<ResourceTimingInfo> info =
           resource_timing_info_map_.Take(resource)) {
-    if (resource->GetResponse().IsHTTP() &&
-        resource->GetResponse().HttpStatusCode() < 400) {
+    if (resource->GetResponse().IsHTTP()) {
       info->SetInitialURL(resource->GetResourceRequest()
                                   .GetInitialUrlForResourceTiming()
                                   .IsNull()
