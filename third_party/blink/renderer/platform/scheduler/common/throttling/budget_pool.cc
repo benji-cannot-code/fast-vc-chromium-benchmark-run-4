4056365118a1369394bf22454f018f906c098509
Katie Dillon <kdillon@chromium.org>
2019-09-11 22:52:39 +0000

    [scheduler] Experiment with prioritizing compositing based on a budget.
    This cl adds an experiment to the CompositorPriorityExperiments class that will
    set the compositior to kVeryHighPriority until it exhausts a budget. Once the budget
    runs out it will set the compositor back to kNormalPriority until the budget is
    replenished.
    
    Bug: 966177
    Change-Id: I62d9b261501c99dbe62efa008cbdbc8acd902f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767956
    Commit-Queue: Katie Dillon <kdillon@chromium.org>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695821}

--- third_party/blink/renderer/platform/scheduler/common/throttling/budget_pool.cc
+++ third_party/blink/renderer/platform/scheduler/common/throttling/budget_pool.cc
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 
 #include "base/optional.h"
-#include "third_party/blink/renderer/platform/scheduler/common/throttling/task_queue_throttler.h"
+#include "third_party/blink/renderer/platform/scheduler/common/throttling/budget_pool_controller.h"
+#include "third_party/blink/renderer/platform/scheduler/common/tracing_helper.h"
 
 namespace blink {
 namespace scheduler {
