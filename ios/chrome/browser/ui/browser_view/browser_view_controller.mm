a011aa648abac0d7c19706b3bdab27e304ae795c
Mohammad Refaat <mrefaat@chromium.org>
2019-09-11 18:21:24 +0000

    Stop disabling WebUsage before clearing browsing data for iOS 13
    This should fix the problem of cookies not clearing consistently
    on iOS 13.
    More details are on the crbug comments.
    
    Also Change onViewUpdated on web_frames_manager_impl to remove all
    messages instead of looking for specific messages. This is needed
    as removing specific messages DCHECKs if the message is not registered.
    and during the Purge which happens in clear browsing data the message
    router is destroyed so there will be no messages in the registered with
    the webview.
    
    TEST= I tested manually on ipad, iphone for both iOS 12 & 13 on devices
    , a test on the simulator is meaningless as the simulator doesn't use
    the same cookie storage that the device use. There exist unit tests but
    they will not fail in the case that i'm fixing because in these tests
    we set cookies manually and this current case happen when cookies are set
    by the web view. I'll discuss further creating EG tests to test the failed
    scenario using a browsing test page that can set/get cookies.
    
    Bug: 997172, 956516
    Change-Id: I3befee1bf38ac1d3d86c33e50bbbbd4fb9e75ec9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1791629
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695709}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -1237,14 +1237,7 @@ NSString* const kBrowserViewControllerSnackbarCategory =
   // prevent interaction with the web page.
   // TODO(crbug.com/637093): This coordinator should be managed by the
   // coordinator used to present BrowserViewController, when implemented.
-  if (active) {
-    [self.activityOverlayCoordinator stop];
-    self.activityOverlayCoordinator = nil;
-  } else if (!self.activityOverlayCoordinator) {
-    self.activityOverlayCoordinator =
-        [[ActivityOverlayCoordinator alloc] initWithBaseViewController:self];
-    [self.activityOverlayCoordinator start];
-  }
+  [self showActivityOverlay:!active];
 
   if (self.browserState) {
     ActiveStateManager* active_state_manager =
@@ -4386,6 +4379,17 @@ NSString* const kBrowserViewControllerSnackbarCategory =
 - (void)sendTabToSelf:(SendTabToSelfCommand*)command {
   [self sendTabToSelfTargetDeviceID:[command targetDeviceID]
                    targetDeviceName:[command targetDeviceName]];
+}
+
+- (void)showActivityOverlay:(BOOL)shown {
+  if (!shown) {
+    [self.activityOverlayCoordinator stop];
+    self.activityOverlayCoordinator = nil;
+  } else if (!self.activityOverlayCoordinator) {
+    self.activityOverlayCoordinator =
+        [[ActivityOverlayCoordinator alloc] initWithBaseViewController:self];
+    [self.activityOverlayCoordinator start];
+  }
 }
 
 #pragma mark - FindInPageResponseDelegate
