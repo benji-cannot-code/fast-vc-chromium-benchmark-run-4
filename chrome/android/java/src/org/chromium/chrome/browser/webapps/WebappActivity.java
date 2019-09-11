f0d2ad4c2e79c13eaa357f00aadd078d9fdeafe4
Peter Kotwicz <pkotwicz@chromium.org>
2019-09-11 23:19:17 +0000

    [Android WebAPK] Add UMA for whether WebAPK navigation is in scope
    This CL adds a new UMA metric WebApk.Navigation.InScope which is
    recorded on every WebAPK navigation. It records whether WebAPK
    navigations are within the WebAPK's scope.
    
    BUG=999648
    
    Change-Id: I0d8fc678f396e8fd55c48aabd41b8878ee8fdcd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788304
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695828}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -42,6 +42,7 @@ import org.chromium.chrome.browser.compositor.layouts.LayoutManager;
 import org.chromium.chrome.browser.customtabs.CustomTabAppMenuPropertiesDelegate;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.document.DocumentUtils;
+import org.chromium.chrome.browser.metrics.WebApkUma;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabBrowserControlsState;
@@ -601,12 +602,16 @@ public class WebappActivity extends SingleTabActivity {
 
                     updateToolbarCloseButtonVisibility();
 
-                    if (!WebappScopePolicy.isUrlInScope(
-                                scopePolicy(), mWebappInfo, navigation.getUrl())) {
+                    boolean isNavigationInScope = WebappScopePolicy.isUrlInScope(
+                            scopePolicy(), mWebappInfo, navigation.getUrl());
+                    if (!isNavigationInScope) {
                         // Briefly show the toolbar for off-scope navigations.
                         getFullscreenManager()
                                 .getBrowserVisibilityDelegate()
                                 .showControlsTransient();
+                    }
+                    if (mWebappInfo.isForWebApk()) {
+                        WebApkUma.recordNavigation(isNavigationInScope);
                     }
                 }
             }
