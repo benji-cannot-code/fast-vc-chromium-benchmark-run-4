bd56318e47947f7ae00ff3590106b3e77d2f01f6
Dan Beam <dbeam@chromium.org>
2019-09-11 03:21:11 +0000

    Local NTP: recompute isDarkModeEnabled when prefer-color-scheme changes
    The browser apparently sends theme changes before the renderer-side dark
    mode detection is updated (i.e. window.matchMedia()). In addition to
    consuming theme info changes, also re-run theme change event handlers
    when matchMedia().onchange fires.
    
    Bug: 913573
    Change-Id: Ia9c4c07b0bc05c7ff29bf2aa41a1851958fc556c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797153
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695470}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -725,6 +725,8 @@ function init() {
 
   renderTheme();
 
+  window.matchMedia('(prefers-color-scheme: dark)').onchange = onThemeChange;
+
   const searchboxApiHandle = embeddedSearchApiHandle.searchBox;
 
   if (configData.isGooglePage) {
