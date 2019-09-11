574857a0d2848206a766b8924a885942457c3078
Paul Jensen <pauljensen@chromium.org>
2019-09-11 21:47:37 +0000

    Fix potential use-after-free in NetworkChangeNotifierWin shutdown
    Make NetworkChangeNotifierWin::RecomputeCurrentConnectionType() static
    to avoid referencing NetworkChangeNotifierWin vtable pointer when it
    might be dead.  This can happen when RecomputeCurrentConnectionType()
    is posted to |blocking_task_runner_| and NetworkChangeNotifierWin is
    destroyed.
    
    Bug: 995781
    Change-Id: I56805777fb77a5b37829962e41d4d0b0e09eb468
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789666
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695799}

--- net/base/network_change_notifier_win.cc
+++ net/base/network_change_notifier_win.cc
@@ -120,8 +120,9 @@ NetworkChangeNotifierWin::NetworkChangeCalculatorParamsWin() {
 // experiments I ran... However none of them correctly returned "offline" when
 // executing 'ipconfig /release'.
 //
+// static
 NetworkChangeNotifier::ConnectionType
-NetworkChangeNotifierWin::RecomputeCurrentConnectionType() const {
+NetworkChangeNotifierWin::RecomputeCurrentConnectionType() {
   EnsureWinsockInit();
 
   // The following code was adapted from:
@@ -195,8 +196,7 @@ void NetworkChangeNotifierWin::RecomputeCurrentConnectionTypeOnBlockingSequence(
   // thread is stopped in this object's destructor.
   base::PostTaskAndReplyWithResult(
       blocking_task_runner_.get(), FROM_HERE,
-      base::BindOnce(&NetworkChangeNotifierWin::RecomputeCurrentConnectionType,
-                     base::Unretained(this)),
+      base::BindOnce(&NetworkChangeNotifierWin::RecomputeCurrentConnectionType),
       std::move(reply_callback));
 }
 
