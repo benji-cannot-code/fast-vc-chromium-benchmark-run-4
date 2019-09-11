749c061efb090f28b43f3343bfdfd5e5ab216971
Gabriel Charette <gab@chromium.org>
2019-09-11 20:08:11 +0000

    Backport some tests in /components/dom_distiller to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /components/dom_distiller.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=bengr@chromium.org
    
    Bug: 891670
    Change-Id: Id769068426f8af6e23a32f62202883e3a2a8789e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787106
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Ben Greenstein <bengr@chromium.org>
    Reviewed-by: Ben Greenstein <bengr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695761}

--- components/dom_distiller/core/dom_distiller_store_unittest.cc
+++ components/dom_distiller/core/dom_distiller_store_unittest.cc
@@ -160,7 +160,7 @@ class DomDistillerStoreTest : public testing::Test {
     return data;
   }
 
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 
   EntryMap db_model_;
   EntryMap sync_model_;
