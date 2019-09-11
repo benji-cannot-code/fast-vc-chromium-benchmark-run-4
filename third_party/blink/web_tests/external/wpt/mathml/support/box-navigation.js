1734e88e85f560682e7d00817d4fd56c6d828a2f
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-09-11 21:38:39 +0000

    Import wpt@58815a8d4d42a6952bc3d975031562a3de4b2c94
    Using wpt-import in Chromium 4a9804e263e373053826475e25a4545831edd945.
    With Chromium commits locally applied on WPT:
    cf88186a26 "[LayoutNG] Fix `vertical-align: top` and `bottom`"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    kojii@chromium.org:
      external/wpt/css/CSS2/linebox
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: Ie688514c5d7d1690b80b57123dd18ef9173c628d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1799083
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#695793}

--- third_party/blink/web_tests/external/wpt/mathml/support/box-navigation.js
+++ third_party/blink/web_tests/external/wpt/mathml/support/box-navigation.js
@@ -6,6 +6,13 @@ function IsInFlow(element) {
         style.getPropertyValue("position") !== "fixed";
 }
 
+function firstInFlowChild(element) {
+    var child = element.firstElementChild;
+    if (!child || IsInFlow(child))
+        return child;
+    return nextInFlowSibling(child);
+}
+
 function nextInFlowSibling(element) {
     var child = element;
     do {
