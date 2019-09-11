2bd77e1002d57dae4544f72ac59d6e1d580f8ecb
Gabriel Charette <gab@chromium.org>
2019-09-11 19:31:27 +0000

    Backport some tests in /chrome/utility/image_writer to SingleThreadTaskEnvironment
    These tests were previously migrated from single-threaded MessageLoop to
    a multi-threaded TaskEnvironment (then named ScopedTaskEnvironment) as
    part of crbug.com/891670.
    
    //base OWNERS decided in retrospect that it was better to keep a
    single-threaded option for TaskEnvironment and introduced
    SingleThreadTaskEnvironment. This CL retrofits that decision for
    /chrome/utility/image_writer.
    
    This CL is a no-op if it passes CQ.
    
    This CL was uploaded by git cl split.
    
    R=haven@chromium.org
    
    Bug: 891670
    Change-Id: Id42c705ac4a5dba6785b770aa62c377b029e1faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786968
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695741}

--- chrome/utility/image_writer/image_writer_unittest.cc
+++ chrome/utility/image_writer/image_writer_unittest.cc
@@ -56,7 +56,7 @@ class ImageWriterUtilityTest : public testing::Test {
   base::FilePath device_path_;
 
  private:
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   base::ScopedTempDir temp_dir_;
 };
 
