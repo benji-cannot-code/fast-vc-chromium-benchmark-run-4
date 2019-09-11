c5778ba393845d451a564f2aaf72f0db3e78cd1e
Matt Menke <mmenke@chromium.org>
2019-09-11 02:14:05 +0000

    Delete ExternalFileURLRequestJob and ExternalFileProtocolHandler.
    These were used for hooking requests into Chrome's network stack, but
    with the advent of the network service, these classes are no longer
    being used.
    
    Bug: 934009
    Change-Id: I08c5ed0c999cf1472c2eeb9fc478536ee3ce2c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790490
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695450}

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -81,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/certificate_provider/certificate_provider.h"
 #include "chrome/browser/chromeos/certificate_provider/certificate_provider_service.h"
 #include "chrome/browser/chromeos/certificate_provider/certificate_provider_service_factory.h"
-#include "chrome/browser/chromeos/fileapi/external_file_protocol_handler.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/net/client_cert_filter_chromeos.h"
 #include "chrome/browser/chromeos/net/client_cert_store_chromeos.h"
