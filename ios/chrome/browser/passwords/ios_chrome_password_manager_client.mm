85abb413ceada43f6d61819780d339e989fe2e84
Mohamed Amir Yosef <mamir@chromium.org>
2019-09-11 14:06:00 +0000

    [Passwords] Introduce PasswordManagerClient.GetAccountPasswordStore()
    The new API will be used by password manager code within the effort of
    Butter4Passwords
    
    TBR=vasilii@chromium.org
    
    Change-Id: I1b0bc300fce37e3695f579aaabbbc7c5bfba4796
    Bug: 1002000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1795323
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695609}

--- ios/chrome/browser/passwords/ios_chrome_password_manager_client.mm
+++ ios/chrome/browser/passwords/ios_chrome_password_manager_client.mm
@@ -162,6 +162,11 @@ PasswordStore* IOSChromePasswordManagerClient::GetProfilePasswordStore() const {
       .get();
 }
 
+PasswordStore* IOSChromePasswordManagerClient::GetAccountPasswordStore() const {
+  // AccountPasswordStore is currenly not supported on iOS.
+  return nullptr;
+}
+
 void IOSChromePasswordManagerClient::NotifyUserAutoSignin(
     std::vector<std::unique_ptr<autofill::PasswordForm>> local_forms,
     const GURL& origin) {
