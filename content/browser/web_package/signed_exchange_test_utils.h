e1c6f658736d0e388f979046b08e8baa45685ecb
Matt Mueller <mattm@chromium.org>
2019-09-11 00:57:35 +0000

    Fix SignedExchangeRequestHandlerRealCertVerifierBrowserTest.Basic on macOS 10.15
    10.15 adds two certificate requirements that were tripped by the cert
    generated for this test:
    * validity period must be 825 days or less.
    * must specify ExtendedKeyUsage.
    
    See https://support.apple.com/en-us/HT210176
    
    Bug: 1001601
    Change-Id: Id4b77f4b0df726e704e7ddcffe0b1c9ae4cf11cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1796743
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695415}

--- content/browser/web_package/signed_exchange_test_utils.h
+++ content/browser/web_package/signed_exchange_test_utils.h
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 constexpr char kPEMECDSAP256SPKIHash[] =
-    "gyt+9XZNn/phgPx5H8+75u0Kcr1UxLnfz1+5ccndF3A=";
+    "Roij6gF5orGtG9K8u8PkTe1LiRbVklxLM+EVABTK8I0=";
 constexpr char kPEMECDSAP384SPKIHash[] =
-    "ymo7jA8HxN67FG5u4dp9kxzM6LGvqEWLv8kCkKRguA4=";
+    "wfk/4dogHdX/MbLIvpjbKQ7fhO+ovkX2wjzDPSrIsMY=";
 
 }  // namespace content
 
