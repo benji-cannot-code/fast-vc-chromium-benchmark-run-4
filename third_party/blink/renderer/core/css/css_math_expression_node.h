10301b02bd32cf2c4af6f0b97946fd67f3fd1806
Xiaocheng Hu <xiaochengh@chromium.org>
2019-09-11 06:02:52 +0000

    Revert "Switch InterpolableLength implementation from length array to expression"
    This reverts commit dd1318d7b1537af982313d51433e00fc02421225.
    
    Reason for revert: caused performance regression crbug.com/1002177
    
    Original change's description:
    > Switch InterpolableLength implementation from length array to expression
    > 
    > This patch changes the internal implementation of InterpolableLength
    > from CSSLengthArray to the following:
    > - A CSSNumericLiteralValue to keep it simple when it's a simple value
    > - A CSSMathExpressionNode to make it general when the underlying length
    >   involves calculation and math functions
    > 
    > In this way, we can interpolate when the length cannot be represented
    > as a CSSLengthArray (i.e., when comparison functions min/max are
    > involved).
    > 
    > Bug: 991672
    > Change-Id: Ib83fc0a07d0eb1d03a2465ac9df5282f963b935b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1777025
    > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > Reviewed-by: Alan Cutter <alancutter@chromium.org>
    > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#694443}
    
    TBR=alancutter@chromium.org,eae@chromium.org,xiaochengh@chromium.org,futhark@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 991672
    Change-Id: I4467c0b4c8202dbc38ac5152620666caa38648e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797822
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695498}

--- third_party/blink/renderer/core/css/css_math_expression_node.h
+++ third_party/blink/renderer/core/css/css_math_expression_node.h
@@ -115,11 +115,6 @@ class CORE_EXPORT CSSMathExpressionNode
   virtual bool IsComputationallyIndependent() const = 0;
 
   CalculationCategory Category() const { return category_; }
-  bool HasPercentage() const {
-    return category_ == kCalcPercent || category_ == kCalcPercentNumber ||
-           category_ == kCalcPercentLength ||
-           category_ == kCalcPercentLengthNumber;
-  }
 
   // Returns the unit type of the math expression *without doing any type
   // conversion* (e.g., 1px + 1em needs type conversion to resolve).
@@ -165,7 +160,6 @@ class CORE_EXPORT CSSMathExpressionNumericLiteral final
                                   bool is_integer);
 
   bool IsNumericLiteral() const final { return true; }
-  const CSSNumericLiteralValue* GetValue() const { return value_; }
 
   bool IsZero() const final;
   String CustomCSSText() const final;
