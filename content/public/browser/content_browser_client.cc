b4d91c7819dff56e73c4d01fc3d84b3a5addd469
Ian Vollick <vollick@chromium.org>
2019-09-11 02:24:29 +0000

    Move explicit navigation check to ContentBrowserClient
    This permits the embedder to ensure that debug URLs are handled
    correctly.
    
    Bug: 986346
    Change-Id: I5f9f36298d44afd7ef9f4a71566a51f1ba258e63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1795749
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695454}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -92,6 +92,10 @@ bool ContentBrowserClient::ShouldCompareEffectiveURLsForSiteInstanceSelection(
   return true;
 }
 
+bool ContentBrowserClient::IsExplicitNavigation(ui::PageTransition transition) {
+  return transition & ui::PAGE_TRANSITION_FROM_ADDRESS_BAR;
+}
+
 bool ContentBrowserClient::ShouldUseMobileFlingCurve() {
   return false;
 }
