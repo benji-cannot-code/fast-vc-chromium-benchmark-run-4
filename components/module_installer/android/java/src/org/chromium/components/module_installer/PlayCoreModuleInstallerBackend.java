aa8c0e9f3fe575390db607d690e7d94537359778
Peter Wen <wnwen@chromium.org>
2019-09-11 14:07:50 +0000

    Android: Use SystemClock#uptimeMillis for modules
    SystemClock#uptimeMillis is better for interval timing since it stops
    counting when the system enters deep sleep, which would otherwise
    greatly inflate our module installation metrics.
    
    Details:
    https://developer.android.com/reference/android/os/SystemClock
    
    Bug: None
    Change-Id: I58ce7f1b64ae1dbfbf6402f0380eafd76a161555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1776706
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695611}

--- components/module_installer/android/java/src/org/chromium/components/module_installer/PlayCoreModuleInstallerBackend.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/PlayCoreModuleInstallerBackend.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.module_installer;
 
 import android.content.SharedPreferences;
+import android.os.SystemClock;
 import android.util.SparseLongArray;
 
 import com.google.android.play.core.splitinstall.SplitInstallException;
@@ -41,7 +42,7 @@ import java.util.Set;
 
         public InstallTimes(boolean isCached) {
             mIsCached = isCached;
-            mInstallTimes.put(SplitInstallSessionStatus.UNKNOWN, System.currentTimeMillis());
+            mInstallTimes.put(SplitInstallSessionStatus.UNKNOWN, SystemClock.uptimeMillis());
         }
     }
 
@@ -191,7 +192,7 @@ import java.util.Set;
             case SplitInstallSessionStatus.INSTALLED:
                 for (String name : state.moduleNames()) {
                     mInstallTimesMap.get(name).mInstallTimes.put(
-                            state.status(), System.currentTimeMillis());
+                            state.status(), SystemClock.uptimeMillis());
                 }
                 if (state.status() == SplitInstallSessionStatus.INSTALLED) {
                     finish(true, state.moduleNames(), INSTALL_STATUS_SUCCESS);
@@ -311,7 +312,7 @@ import java.util.Set;
             return;
         }
         RecordHistogram.recordLongTimesHistogram(
-                String.format("Android.FeatureModules.%sInstallDuration%s.%s",
+                String.format("Android.FeatureModules.%sAwakeInstallDuration%s.%s",
                         installTimes.mIsCached ? "Cached" : "Uncached", histogramSubname,
                         moduleName),
                 installTimes.mInstallTimes.get(endKey) - installTimes.mInstallTimes.get(startKey));
