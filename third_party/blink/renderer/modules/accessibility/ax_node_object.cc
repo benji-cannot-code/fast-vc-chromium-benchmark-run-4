340ca2bbf8f8a9fa4cd3b0e516c58578998ca54f
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-09-11 03:50:30 +0000

    Use new downcast helper for blink::HTMLOutputElement
    This CL has two goals,
    1. Use IsA<HTMLOutputElement>(element) in place of
       IsHTMLOutputElement(element)
    
    Bug: 891908
    Change-Id: I701ed8089fb0e91b344db0465ab2f8e34f334718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1795531
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#695475}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -605,7 +605,7 @@ ax::mojom::Role AXNodeObject::NativeRoleIgnoringAria() const {
   if (IsHTMLProgressElement(*GetNode()))
     return ax::mojom::Role::kProgressIndicator;
 
-  if (IsHTMLOutputElement(*GetNode()))
+  if (IsA<HTMLOutputElement>(*GetNode()))
     return ax::mojom::Role::kStatus;
 
   if (IsHTMLParagraphElement(*GetNode()))
