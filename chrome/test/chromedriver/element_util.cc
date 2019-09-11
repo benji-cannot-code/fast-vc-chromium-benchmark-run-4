51a4138933ff94d8c3afc5170ed2e690ce908bfd
Tatiana Buldina <buldina@chromium.org>
2019-09-11 20:57:01 +0000

    [ChromeDriver] Fix ClickScrollingTests on Mac
    Update ScrollElementRegionIntoViewHelper() to look for
    kElementClickIntercepted instead of kUnknownError.
    Reason was in https://crrev.com/634697 which changed
    VerifyElementClickable() function to return
    kElementClickIntercepted instead of kUnknownError.
    
    Bug: chromedriver:2856
    Change-Id: Ic2eee972663df9163e6fa6c56adcca1512e14efc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797706
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tatiana Buldina <buldina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695775}

--- chrome/test/chromedriver/element_util.cc
+++ chrome/test/chromedriver/element_util.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/chromedriver/chrome/js.h"
 #include "chrome/test/chromedriver/chrome/status.h"
 #include "chrome/test/chromedriver/chrome/web_view.h"
+#include "chrome/test/chromedriver/net/timeout.h"
 #include "chrome/test/chromedriver/session.h"
 #include "third_party/webdriver/atoms.h"
 
@@ -158,8 +159,7 @@ Status ScrollElementRegionIntoViewHelper(
     middle.Offset(region.Width() / 2, region.Height() / 2);
     status = VerifyElementClickable(
         frame, web_view, clickable_element_id, middle);
-    if (status.code() == kUnknownError &&
-        status.message().find("is not clickable") != std::string::npos) {
+    if (status.code() == kElementClickIntercepted) {
       // Clicking at the target location isn't reaching the target element.
       // One possible cause is a scroll event handler has shifted the element.
       // Try again to get the updated location of the target element.
@@ -175,8 +175,15 @@ Status ScrollElementRegionIntoViewHelper(
       }
       middle = tmp_location;
       middle.Offset(region.Width() / 2, region.Height() / 2);
-      status =
-          VerifyElementClickable(frame, web_view, clickable_element_id, middle);
+      Timeout response_timeout(base::TimeDelta::FromSeconds(1));
+      do {
+        status =
+         VerifyElementClickable(frame, web_view, clickable_element_id, middle);
+        if (status.code() == kElementClickIntercepted)
+          base::PlatformThread::Sleep(base::TimeDelta::FromMilliseconds(50));
+        else
+          break;
+      } while (!response_timeout.IsExpired());
     }
     if (status.IsError())
       return status;
