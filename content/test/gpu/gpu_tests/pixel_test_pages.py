ae0cfacad08176235cc4d4090d1e71de2a721bd6
Zhenyao Mo <zmo@chromium.org>
2019-09-11 19:12:55 +0000

    Relax VP9 pixel test tolerance for direct composition tests.
    BUG=1002988
    TEST=bot
    R=kbr@chromium.org
    
    Change-Id: I3f940a19a067fe9a5aac9c7ca8aababb55b5c13f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1798842
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695728}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -1170,6 +1170,7 @@ class PixelTestPages(object):
       '--disable-features=D3D11VideoDecoder']
 
     tolerance_dc = 5
+    tolerance_dc_vp9 = 15
 
     return [
       PixelTestPage(
@@ -1413,7 +1414,7 @@ class PixelTestPages(object):
         test_rect=[0, 0, 240, 135],
         revision=0, # Golden image revision is not used
         browser_args=browser_args,
-        tolerance=tolerance_dc,
+        tolerance=tolerance_dc_vp9,
         expected_colors=_FOUR_COLOR_VIDEO_240x135_EXPECTED_COLORS),
 
       PixelTestPage(
@@ -1422,7 +1423,7 @@ class PixelTestPages(object):
         browser_args=browser_args_DXVA,
         test_rect=[0, 0, 240, 135],
         revision=0, # Golden image revision is not used
-        tolerance=tolerance_dc,
+        tolerance=tolerance_dc_vp9,
         expected_colors=_FOUR_COLOR_VIDEO_240x135_EXPECTED_COLORS),
 
       PixelTestPage(
@@ -1432,7 +1433,7 @@ class PixelTestPages(object):
         revision=0, # Golden image revision is not used
         browser_args=browser_args,
         other_args={'zero_copy': True},
-        tolerance=tolerance_dc,
+        tolerance=tolerance_dc_vp9,
         expected_colors=[
           {
             'comment': 'top left video, yellow',
@@ -1467,7 +1468,7 @@ class PixelTestPages(object):
         revision=0, # Golden image revision is not used
         browser_args=browser_args_YUY2,
         other_args={'expect_yuy2': True},
-        tolerance=tolerance_dc,
+        tolerance=tolerance_dc_vp9,
         expected_colors=_FOUR_COLOR_VIDEO_240x135_EXPECTED_COLORS),
 
       PixelTestPage(
