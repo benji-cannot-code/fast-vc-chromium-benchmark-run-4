9fb9bcfab222c4702e7b3b63a3c2cbb99934cf3b
Olivier Robin <olivierrobin@chromium.org>
2019-09-11 20:10:06 +0000

    Only execute shortcut handler on active application.
    The shortcut handler is already executed when application becomes
    active.
    Executing it in performActionForShortcutItem will result in two
    execution which can cause issues (e.g. if action is to trigger camera).
    
    Bug: 1000940
    Change-Id: Id89a1c1c88fc9594599772cb0170c7c6e08deded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789523
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695763}

--- ios/chrome/app/application_delegate/user_activity_handler.mm
+++ ios/chrome/app/application_delegate/user_activity_handler.mm
@@ -210,7 +210,9 @@ NSString* const kShortcutQRScanner = @"OpenQRScanner";
   BOOL handledShortcutItem =
       [UserActivityHandler handleShortcutItem:shortcutItem
                            startupInformation:startupInformation];
-  if (handledShortcutItem) {
+  BOOL isActive = [[UIApplication sharedApplication] applicationState] ==
+                  UIApplicationStateActive;
+  if (handledShortcutItem && isActive) {
     [UserActivityHandler
         handleStartupParametersWithTabOpener:tabOpener
                           startupInformation:startupInformation
