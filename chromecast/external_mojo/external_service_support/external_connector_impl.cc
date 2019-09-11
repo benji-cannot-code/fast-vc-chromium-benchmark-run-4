7db18f6a0e7d95a42f604dda3288aeaf50bf51a2
Joshua LeVasseur <jlevasseur@chromium.org>
2019-09-11 18:14:39 +0000

    Convert ExternalConnector into an abstract class.
    This supports writing unit tests.
    
    Merge-With: eureka-internal/311500
    
    Bug: None
    Test: build
    Change-Id: I61d6bd28599f260091f5f9ad43575fe4214d28b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764393
    Commit-Queue: Joshua LeVasseur <jlevasseur@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695706}

--- chromecast/external_mojo/external_service_support/external_connector_impl.cc
+++ chromecast/external_mojo/external_service_support/external_connector_impl.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chromecast/external_mojo/external_service_support/external_connector.h"
+#include "chromecast/external_mojo/external_service_support/external_connector_impl.h"
 
 #include "base/bind.h"
 #include "base/location.h"
@@ -54,30 +54,35 @@ void ExternalConnector::Connect(
   external_mojo::mojom::ExternalConnectorPtr connector(
       external_mojo::mojom::ExternalConnectorPtrInfo(std::move(pipe), 0));
   std::move(callback).Run(
-      std::make_unique<ExternalConnector>(std::move(connector)));
+      std::make_unique<ExternalConnectorImpl>(std::move(connector)));
 }
 
-ExternalConnector::ExternalConnector(
+ExternalConnectorImpl::ExternalConnectorImpl(
     external_mojo::mojom::ExternalConnectorPtr connector)
     : connector_(std::move(connector)) {
   connector_.set_connection_error_handler(base::BindOnce(
-      &ExternalConnector::OnConnectionError, base::Unretained(this)));
+      &ExternalConnectorImpl::OnConnectionError, base::Unretained(this)));
 }
 
-ExternalConnector::ExternalConnector(
+ExternalConnectorImpl::ExternalConnectorImpl(
     external_mojo::mojom::ExternalConnectorPtrInfo unbound_state)
     : unbound_state_(std::move(unbound_state)) {
   DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
-ExternalConnector::~ExternalConnector() = default;
+ExternalConnectorImpl::~ExternalConnectorImpl() = default;
 
-void ExternalConnector::RegisterService(const std::string& service_name,
-                                        ExternalService* service) {
+void ExternalConnectorImpl::SetConnectionErrorCallback(
+    base::OnceClosure callback) {
+  connection_error_callback_ = std::move(callback);
+}
+
+void ExternalConnectorImpl::RegisterService(const std::string& service_name,
+                                            ExternalService* service) {
   RegisterService(service_name, service->GetBinding());
 }
 
-void ExternalConnector::RegisterService(
+void ExternalConnectorImpl::RegisterService(
     const std::string& service_name,
     external_mojo::mojom::ExternalServicePtr service_ptr) {
   if (BindConnectorIfNecessary()) {
@@ -85,7 +90,7 @@ void ExternalConnector::RegisterService(
   }
 }
 
-void ExternalConnector::QueryServiceList(
+void ExternalConnectorImpl::QueryServiceList(
     base::OnceCallback<void(
         std::vector<chromecast::external_mojo::mojom::ExternalServiceInfoPtr>)>
         callback) {
@@ -94,7 +99,7 @@ void ExternalConnector::QueryServiceList(
   }
 }
 
-void ExternalConnector::BindInterface(
+void ExternalConnectorImpl::BindInterface(
     const std::string& service_name,
     const std::string& interface_name,
     mojo::ScopedMessagePipeHandle interface_pipe) {
@@ -104,23 +109,23 @@ void ExternalConnector::BindInterface(
   }
 }
 
-std::unique_ptr<ExternalConnector> ExternalConnector::Clone() {
+std::unique_ptr<ExternalConnector> ExternalConnectorImpl::Clone() {
   external_mojo::mojom::ExternalConnectorPtrInfo connector_info;
   auto request = mojo::MakeRequest(&connector_info);
   if (BindConnectorIfNecessary()) {
     connector_->Clone(std::move(request));
   }
-  return std::make_unique<ExternalConnector>(std::move(connector_info));
+  return std::make_unique<ExternalConnectorImpl>(std::move(connector_info));
 }
 
-void ExternalConnector::SendChromiumConnectorRequest(
+void ExternalConnectorImpl::SendChromiumConnectorRequest(
     mojo::ScopedMessagePipeHandle request) {
   if (BindConnectorIfNecessary()) {
     connector_->BindChromiumConnector(std::move(request));
   }
 }
 
-void ExternalConnector::OnConnectionError() {
+void ExternalConnectorImpl::OnConnectionError() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   connector_.reset();
   if (connection_error_callback_) {
@@ -128,7 +133,7 @@ void ExternalConnector::OnConnectionError() {
   }
 }
 
-bool ExternalConnector::BindConnectorIfNecessary() {
+bool ExternalConnectorImpl::BindConnectorIfNecessary() {
   // Bind the message pipe and SequenceChecker to the current thread the first
   // time it is used to connect.
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -143,7 +148,7 @@ bool ExternalConnector::BindConnectorIfNecessary() {
 
   connector_.Bind(std::move(unbound_state_));
   connector_.set_connection_error_handler(base::BindOnce(
-      &ExternalConnector::OnConnectionError, base::Unretained(this)));
+      &ExternalConnectorImpl::OnConnectionError, base::Unretained(this)));
 
   return true;
 }
