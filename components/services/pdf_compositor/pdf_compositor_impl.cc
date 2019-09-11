f3b4e448ad1915ce39c453a47cdc84e2bcc0151e
Lei Zhang <thestig@chromium.org>
2019-09-11 16:45:40 +0000

    Do less initialization in PdfCompositorImpl ctor.
    Currently it initializes some DirectWrite code on Windows in unit tests
    that is only intended to be called once per process, whereas
    PdfCompositorImplTest constructs multiple PdfCompositorImpl objects and
    ends up calling that repeatedly, which then fails. Avoid doing this, as
    the DirectWrite code is not needed in the test.
    
    Bug: 1002663
    Change-Id: I741690466d3111f0095a070bd64f39ebfb835804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797225
    Reviewed-by: Wei Li <weili@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695669}

--- components/services/pdf_compositor/pdf_compositor_impl.cc
+++ components/services/pdf_compositor/pdf_compositor_impl.cc
@@ -47,6 +47,9 @@ PdfCompositorImpl::PdfCompositorImpl(
   if (receiver)
     receiver_.Bind(std::move(receiver));
 
+  if (!initialize_environment)
+    return;
+
 #if defined(OS_WIN)
   // Initialize direct write font proxy so skia can use it.
   content::InitializeDWriteFontProxy();
@@ -55,9 +58,6 @@ PdfCompositorImpl::PdfCompositorImpl(
   // Hook up blink's codecs so skia can call them.
   SkGraphics::SetImageGeneratorFromEncodedDataFactory(
       blink::WebImageGenerator::CreateAsSkImageGenerator);
-
-  if (!initialize_environment)
-    return;
 
 #if defined(OS_POSIX) && !defined(OS_ANDROID)
   content::UtilityThread::Get()->EnsureBlinkInitializedWithSandboxSupport();
