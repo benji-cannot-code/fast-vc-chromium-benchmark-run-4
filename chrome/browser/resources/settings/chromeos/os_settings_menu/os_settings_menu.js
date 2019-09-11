f53fb9dd4458d991551013aec7e8b43eaba03a38
Martin Robinson <mrobinson@igalia.com>
2019-09-11 19:02:59 +0000

    Replace aria-active-attribute in WebUI
    With the switch to cr-button from paper-button, the component no longer
    has support for aria-active-attribute. Instead we simply set the
    expanded value via an element attribute binding.
    
    Bug: 967793
    Change-Id: I033c025aa25160ede9271cd5e8725e61630183f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1798342
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Martin Robinson <mrobinson@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#695723}

--- chrome/browser/resources/settings/chromeos/os_settings_menu/os_settings_menu.js
+++ chrome/browser/resources/settings/chromeos/os_settings_menu/os_settings_menu.js
@@ -109,4 +109,13 @@ Polymer({
     const submenu = /** @type {IronCollapseElement} */ (this.$.advancedSubmenu);
     return submenu.opened;
   },
+
+  /**
+   * @param {boolean} bool
+   * @return {string}
+   * @private
+   */
+  boolToString_: function(bool) {
+    return bool.toString();
+  },
 });
