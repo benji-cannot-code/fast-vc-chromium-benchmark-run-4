19c212014d2a143c50dc28502efcea91cc45fd51
Miyoung Shin <myid.shin@igalia.com>
2019-09-12 01:02:35 +0000

    Convert XmlParser to new Mojo types
    This CL converts XmlParserPtr and XmlParserRequest in services to the
    new Mojo type, and remove unused XmlParserPtr in chrome.
    
    Bug: 955171
    Change-Id: Ifde5cea32a811c597ad902500e3357df2e41255d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1792515
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695874}

--- services/data_decoder/data_decoder_service.cc
+++ services/data_decoder/data_decoder_service.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
-#include "mojo/public/cpp/bindings/strong_binding.h"
 #include "services/data_decoder/bundled_exchanges_parser_factory.h"
 #include "services/data_decoder/image_decoder_impl.h"
 #include "services/data_decoder/json_parser_impl.h"
@@ -92,9 +91,10 @@ void DataDecoderService::BindJsonParser(
       std::move(receiver));
 }
 
-void DataDecoderService::BindXmlParser(mojom::XmlParserRequest request) {
-  mojo::MakeStrongBinding(std::make_unique<XmlParser>(keepalive_.CreateRef()),
-                          std::move(request));
+void DataDecoderService::BindXmlParser(
+    mojo::PendingReceiver<mojom::XmlParser> receiver) {
+  mojo::MakeSelfOwnedReceiver(
+      std::make_unique<XmlParser>(keepalive_.CreateRef()), std::move(receiver));
 }
 
 void DataDecoderService::BindBundledExchangesParserFactory(
