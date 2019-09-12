17b4e8361441f8201a7bd076d927037ed92f04f5
Christopher Grant <cjgrant@chromium.org>
2019-09-12 14:22:55 +0000

    Modules: Add native library coverage to bundle smoke test
    - Add a new test case to exercise a native library.
    
    - Generalize the display of the result dialog for multiple test cases.
    
    - Specify a "pass" or "fail" in the results dialog text, to be parsed by
      the test, and also aid in manual testing (where the test activity may
      be spawned manually).
    
    - Component build coverage is stubbed out; it will be added once the
      component build places feature libs in DFMs.
    
    Bug: 989646
    Change-Id: I724d0725f71ad7897ceb7b81d9fe7d22183889ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1796308
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696035}

---
+++ chrome/android/features/test_dummy/public/test_dummy.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_ANDROID_FEATURES_TEST_DUMMY_PUBLIC_TEST_DUMMY_H_
+#define CHROME_ANDROID_FEATURES_TEST_DUMMY_PUBLIC_TEST_DUMMY_H_
+
+namespace test_dummy {
+
+// The test_dummy feature's token native entrypoint.
+int TestDummy();
+
+}  // namespace test_dummy
+
+#endif  // CHROME_ANDROID_FEATURES_TEST_DUMMY_PUBLIC_TEST_DUMMY_H_
