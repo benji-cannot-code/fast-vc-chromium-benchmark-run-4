b33733e45a488b824f23d59a4e23812628b58c6c
Kyle Horimoto <khorimoto@chromium.org>
2019-09-12 22:24:55 +0000

    [CrOS Network] Fix network icon for secondary users.
    The issue was that the signalStrength property was not being copied from
    NetworkStateProperties to ManagedProperties.
    
    This CL also adds default managed tether values to onc_mojo.js.
    
    Bug: 990312
    Change-Id: Ib6167cd786fcbc5f7f7103fbe13ca36621e39b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797324
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696188}

--- ui/webui/resources/js/chromeos/onc_mojo.js
+++ ui/webui/resources/js/chromeos/onc_mojo.js
@@ -662,6 +662,12 @@ class OncMojo {
         result.ethernet = {};
         break;
       case mojom.NetworkType.kTether:
+        result.tether = {
+          batteryPercentage: 0,
+          carrier: '',
+          hasConnectedToHost: false,
+          signalStrength: 0,
+        };
         break;
       case mojom.NetworkType.kVPN:
         result.vpn = {
