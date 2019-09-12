fa5313b1076a2496e56e9ef1f810e5ec717ad5b9
Khushal <khushalsagar@chromium.org>
2019-09-12 13:48:17 +0000

    canvas2d: Don't access Thread in CanvasResource dtor.
    There is no ordering guarentee in the destruction order for objects in
    thread local storage. Since Thread and SharedGpuContext are both in
    thread local storage and destruction of CanvasResource can be triggered
    by SharedGpuContext being destroyed, we shouldn't access Thread in the
    CanvasResource dtor since it may have already been destroyed.
    
    R=fserb@chromium.org
    
    Change-Id: Id7efe3eb51fa242382f7446b731f0531c6bd9816
    Bug: 1002792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1799223
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696023}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -47,11 +47,10 @@ gpu::mojom::blink::MailboxPtr SharedBitmapIdToGpuMailboxPtr(
 CanvasResource::CanvasResource(base::WeakPtr<CanvasResourceProvider> provider,
                                SkFilterQuality filter_quality,
                                const CanvasColorParams& color_params)
-    : provider_(std::move(provider)),
+    : owning_thread_id_(base::PlatformThread::CurrentId()),
+      provider_(std::move(provider)),
       filter_quality_(filter_quality),
-      color_params_(color_params) {
-  thread_of_origin_ = Thread::Current()->ThreadId();
-}
+      color_params_(color_params) {}
 
 CanvasResource::~CanvasResource() {
 #if DCHECK_IS_ON()
@@ -60,7 +59,7 @@ CanvasResource::~CanvasResource() {
 }
 
 void CanvasResource::OnDestroy() {
-  if (thread_of_origin_ != Thread::Current()->ThreadId()) {
+  if (owning_thread_id_ != base::PlatformThread::CurrentId()) {
     // Destroyed on wrong thread. This can happen when the thread of origin was
     // torn down, in which case the GPU context owning any underlying resources
     // no longer exists.
@@ -711,7 +710,6 @@ CanvasResourceSharedImage::CanvasResourceSharedImage(
                     context_provider_wrapper_->ContextProvider()
                         ->GetCapabilities())
               : GL_TEXTURE_2D),
-      owning_thread_id_(base::PlatformThread::CurrentId()),
       owning_thread_task_runner_(Thread::Current()->GetTaskRunner()) {
   if (!context_provider_wrapper_)
     return;
@@ -1302,8 +1300,7 @@ CanvasResourceSwapChain::CanvasResourceSwapChain(
     SkFilterQuality filter_quality)
     : CanvasResource(std::move(provider), filter_quality, color_params),
       context_provider_wrapper_(std::move(context_provider_wrapper)),
-      size_(size),
-      owning_thread_id_(base::PlatformThread::CurrentId()) {
+      size_(size) {
   if (!context_provider_wrapper_)
     return;
 
