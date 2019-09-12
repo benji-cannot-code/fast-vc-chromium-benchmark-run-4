613fecaf567fa03494a82628a1a0df4607a3b473
Fredrik Söderquist <fs@opera.com>
2019-09-12 22:12:25 +0000

    Add SMILTime::Earliest()
    This abstracts away the use of the -infinity value that is used in
    SVGSMILElement::ResolveInterval. Also use this for the
    kInvalidCachedTime cache sentinel.
    
    Bug: 1003338
    Change-Id: I908ef136383b935bf27abb68f8aa109ffbd1ffa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1801105
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#696183}

--- third_party/blink/renderer/core/svg/animation/svg_smil_element.cc
+++ third_party/blink/renderer/core/svg/animation/svg_smil_element.cc
@@ -50,7 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 // This is used for duration type time values that can't be negative.
-static const double kInvalidCachedTime = -1.;
+static constexpr SMILTime kInvalidCachedTime = SMILTime::Earliest();
 
 class ConditionEventListener final : public NativeEventListener {
  public:
@@ -797,9 +797,8 @@ SMILInterval SVGSMILElement::ResolveInterval(
     IntervalSelector interval_selector) const {
   bool first = interval_selector == kFirstInterval;
   // See the pseudocode in http://www.w3.org/TR/SMIL3/smil-timing.html#q90.
-  auto constexpr infinity = std::numeric_limits<double>::infinity();
-  SMILTime begin_after = first ? -infinity : interval_.end;
-  SMILTime last_interval_temp_end = infinity;
+  SMILTime begin_after = first ? SMILTime::Earliest() : interval_.end;
+  SMILTime last_interval_temp_end = SMILTime::Indefinite();
   while (true) {
     bool equals_minimum_ok = !first || interval_.end > interval_.begin;
     SMILTime temp_begin =
