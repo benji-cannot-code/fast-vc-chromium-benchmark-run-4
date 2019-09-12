a491b60016db8b171a5e2dcc5c8c480aada47904
Jeremy Roman <jbroman@chromium.org>
2019-09-12 06:08:39 +0000

    Move PortalActivateEvent to core/html/portal/.
    This code is more tightly entangled with the other portal code than the other
    event types, and should usually be reviewed by the same owners that review
    HTMLPortalElement and related changes.
    
    There are already a number of events that live outside core/events/, so this
    is not the first instance of that, either.
    
    Change-Id: I470457d065f9e669664b2b645655563f1878d059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1799399
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695939}

--- third_party/blink/renderer/core/html/portal/portal_activate_event.h
+++ third_party/blink/renderer/core/html/portal/portal_activate_event.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_EVENTS_PORTAL_ACTIVATE_EVENT_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_EVENTS_PORTAL_ACTIVATE_EVENT_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_PORTAL_PORTAL_ACTIVATE_EVENT_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_PORTAL_PORTAL_ACTIVATE_EVENT_H_
 
 #include "base/unguessable_token.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
@@ -102,4 +102,4 @@ class CORE_EXPORT PortalActivateEvent : public Event {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_EVENTS_PORTAL_ACTIVATE_EVENT_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_HTML_PORTAL_PORTAL_ACTIVATE_EVENT_H_
