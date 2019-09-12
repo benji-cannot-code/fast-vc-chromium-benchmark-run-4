9f5cf6560513a32999126236913750b05de95acd
Donn Denman <donnd@chromium.org>
2019-09-12 19:05:59 +0000

    [TTS] Allow parallel Smart Selection requests.
    Allows Android Smart Text Selection to be enabled along with full
    Contextual Search semantics.  Both systems look at page text.
    
    There used to be a bottleneck in getting surrounding text that
    only allowed a single request/response, but now that's been moved
    to Mojo, so multiple requests are allowed.  The limitation was in
    the IPC browser/renderer connection, which Mojo avoids.
    
    This CL removes the logic that limited these systems when they
    were both active.
    
    Also annotate all the SelectionEventType ints correctly.
    
    BUG=961114
    
    Change-Id: If588a532ccb3fe46a39528ec725c277ec77e7e0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1769532
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696125}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPolicy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchPolicy.java
@@ -154,8 +154,9 @@ class ContextualSearchPolicy {
         }
 
         if (isPrivacyAggressiveResolveEnabled()
-                && mSelectionController.getSelectionType() == SelectionType.RESOLVING_LONG_PRESS)
+                && mSelectionController.getSelectionType() == SelectionType.RESOLVING_LONG_PRESS) {
             return true;
+        }
 
         return (isPromoAvailable()
                        || (mContextualSearchPreferenceHelper != null
@@ -261,7 +262,7 @@ class ContextualSearchPolicy {
 
     /**
      * @return Whether a verbatim request should be made for the given base page, assuming there
-     *         is no exiting request.
+     *         is no existing request.
      */
     boolean shouldCreateVerbatimRequest() {
         if (isPrivacyAggressiveResolveEnabled()) return false;
@@ -507,8 +508,9 @@ class ContextualSearchPolicy {
      */
     String getHomeCountry(Context context) {
         if (ContextualSearchFieldTrial.getSwitch(
-                    ContextualSearchSwitch.IS_SEND_HOME_COUNTRY_DISABLED))
+                    ContextualSearchSwitch.IS_SEND_HOME_COUNTRY_DISABLED)) {
             return "";
+        }
 
         TelephonyManager telephonyManager =
                 (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
