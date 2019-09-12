df2e22be7f361c6220b2c923fb7ba31025ef6018
Toni Barzic <tbarzic@chromium.org>
2019-09-12 14:53:31 +0000

    Remove AppListController::ShowApplistAndSwitchToState
    Does not seem to be used from anywhere.
    
    Bug: None
    Change-Id: I6046a34d852c2234489d673ef842b20df21e46d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1799423
    Auto-Submit: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696039}

--- ash/app_list/app_list_controller_impl.h
+++ ash/app_list/app_list_controller_impl.h
@@ -124,7 +124,6 @@ class ASH_EXPORT AppListControllerImpl
 
   void DismissAppList() override;
   void GetAppInfoDialogBounds(GetAppInfoDialogBoundsCallback callback) override;
-  void ShowAppListAndSwitchToState(ash::AppListState state) override;
   void ShowAppList() override;
   aura::Window* GetWindow() override;
 
