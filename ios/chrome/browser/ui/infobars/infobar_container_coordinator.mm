586d72d5fb7bf15c99658cbb9515347af13f6f6b
sczs <sczs@chromium.org>
2019-09-12 23:54:57 +0000

    [ios] Uses InfobarType to display InfobarModal.
    Since now infobarType is passed to displayModalInfobar, we can use it
    to query the right InfobarCoordinator and display its modal.
    
    Bug: 961343
    Change-Id: I0a69c62e3d8e966fc415eeed66017b9478ac0d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1801751
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696226}

--- ios/chrome/browser/ui/infobars/infobar_container_coordinator.mm
+++ ios/chrome/browser/ui/infobars/infobar_container_coordinator.mm
@@ -265,8 +265,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark InfobarCommands
 
 - (void)displayModalInfobar:(InfobarType)infobarType {
-  NSArray* allCoordinators = [self.infobarCoordinators allValues];
-  InfobarCoordinator* infobarCoordinator = [allCoordinators lastObject];
+  NSNumber* infobarKey = [NSNumber numberWithInt:static_cast<int>(infobarType)];
+  InfobarCoordinator* infobarCoordinator = self.infobarCoordinators[infobarKey];
+  DCHECK(infobarCoordinator);
   [infobarCoordinator presentInfobarModal];
 }
 
