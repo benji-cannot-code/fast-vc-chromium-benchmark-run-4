ce3c65efab2b6ebd846ef7b7074c09de6f5c145f
Matt Wolenetz <wolenetz@chromium.org>
2019-09-12 19:16:56 +0000

    MSE: Experimental auto-revoke MSE object URL on attachment start
    This change:
    * Adds a base::Feature "RevokeMediaSourceObjectURLOnAttach", disabled by default.
    * Adds logic to auto-revoke the MediaSource object URL immediately on successful
      start of attachment to HTMLMediaElement using that URL. This logic is used only
      if the feature, above, is enabled.
    * Updates URLFileAPI to also have a revokeObjectURL method that takes an
      ExecutionContext*, since finding a suitable ScriptState* for the existing
      version of revokeObjectURL is fraught with complexity due to the asynchronous
      selection of, and loading of, various URLs by an HTMLMediaElement.
    * Lets HTMLMediaElement find the new base::Feature, which is in media::,
      including relaxing the associated presubmit audit similar to other relaxations.
    
    Since unrevoked MSE Object URLs retain a strong reference to the underlying
    MediaSource objects, MSE Object URL autorevocation is hoped to reduce memory
    bloat from idle, otherwise unreferenced, attached HTMLMediaElement+MediaSource
    object groups by letting them be garbage collected.
    
    With this change, short-term experimentation using Chrome variations of the
    new feature can help validate if autorevocation is helping reduce such bloat.
    
    BUG=920044
    
    Change-Id: Ibc15c59d0e8166a0b1d28fcf3e0eb275bb69244d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1708897
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696128}

--- third_party/blink/renderer/core/fileapi/url_file_api.h
+++ third_party/blink/renderer/core/fileapi/url_file_api.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ExceptionState;
+class ExecutionContext;
 class ScriptState;
 class Blob;
 
@@ -21,6 +22,7 @@ class URLFileAPI {
  public:
   static String createObjectURL(ScriptState*, Blob*, ExceptionState&);
   static void revokeObjectURL(ScriptState*, const String&);
+  static void revokeObjectURL(ExecutionContext*, const String&);
 };
 
 }  // namespace blink
