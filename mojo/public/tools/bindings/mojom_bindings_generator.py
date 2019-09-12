d9d58f44ca596f8ec9fcce286874a6f025dadaaa
John Budorick <jbudorick@chromium.org>
2019-09-12 02:04:45 +0000

    Add diagnostic logging for missing codecs.
    Bug: 1001171
    Change-Id: I19912e1cfcb3e4a97dd485f5b751484b4334f690
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786867
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695900}

--- mojo/public/tools/bindings/mojom_bindings_generator.py
+++ mojo/public/tools/bindings/mojom_bindings_generator.py
@@ -53,6 +53,10 @@ from mojom.generate.generator import AddComputedData, WriteFile
 from mojom.parse.conditional_features import RemoveDisabledDefinitions
 from mojom.parse.parser import Parse
 
+sys.path.append(
+    os.path.join(_GetDirAbove("mojo"), "tools", "diagnosis"))
+import crbug_1001171
+
 
 _BUILTIN_GENERATORS = {
   "c++": "mojom_cpp_generator",
@@ -548,4 +552,5 @@ def main():
 
 
 if __name__ == "__main__":
-  sys.exit(main())
+  with crbug_1001171.DumpStateOnLookupError():
+    sys.exit(main())
--- mojo/public/tools/bindings/pylib/mojom/generate/template_expander.py
+++ mojo/public/tools/bindings/pylib/mojom/generate/template_expander.py
@@ -5,13 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Based on third_party/WebKit/Source/build/scripts/template_expander.py.
 
-import codecs
 import os.path
 import sys
-
-# Cache the ascii codec to ensure pickle can find it at jinja2 import time.
-# See https://crbug.com/997598#c5 for context.
-codecs.lookup('ascii')
 
 _current_dir = os.path.dirname(os.path.realpath(__file__))
 # jinja2 is in chromium's third_party directory
