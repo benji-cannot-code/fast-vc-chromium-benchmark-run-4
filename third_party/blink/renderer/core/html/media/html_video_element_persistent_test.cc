a434254d8ee433e53e6b3ce27e2f78ac98403998
Rune Lillesveen <futhark@chromium.org>
2019-09-12 11:00:23 +0000

    Use :fullscreen instead of :-webkit-full-screen.
    The selectors matching for those pseudo classes are the same. No need to
    use the legacy one internally.
    
    Change-Id: I7a4f6d25e6e016a2af95e863948fda63cfa6d6fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1800182
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#695990}

--- third_party/blink/renderer/core/html/media/html_video_element_persistent_test.cc
+++ third_party/blink/renderer/core/html/media/html_video_element_persistent_test.cc
@@ -195,7 +195,7 @@ TEST_F(HTMLVideoElementPersistentTest, internalPseudoClassOnlyUAStyleSheet) {
 
   DummyExceptionStateForTesting exception_state;
 
-  EXPECT_FALSE(DivElement()->matches(":-webkit-full-screen"));
+  EXPECT_FALSE(DivElement()->matches(":fullscreen"));
   EXPECT_FALSE(DivElement()->matches(":-internal-video-persistent-ancestor",
                                      exception_state));
   EXPECT_TRUE(exception_state.HadException());
@@ -221,7 +221,7 @@ TEST_F(HTMLVideoElementPersistentTest, internalPseudoClassOnlyUAStyleSheet) {
   EXPECT_TRUE(VideoElement()->ContainsPersistentVideo());
 
   // The :internal-* rules apply only from the UA stylesheet.
-  EXPECT_TRUE(DivElement()->matches(":-webkit-full-screen"));
+  EXPECT_TRUE(DivElement()->matches(":fullscreen"));
   EXPECT_FALSE(DivElement()->matches(":-internal-video-persistent-ancestor",
                                      exception_state));
   EXPECT_TRUE(exception_state.HadException());
