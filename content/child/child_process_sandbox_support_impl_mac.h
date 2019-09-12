373498a2cced2a40baabdc6fbc5c7776390e5f1b
Julie Jeongeun Kim <jkim@igalia.com>
2019-09-12 00:17:16 +0000

    Convert SandboxSupportMac to new Mojo types
    This CL converts SandboxSupportMacPtr and
    SandboxSupportMacRequest to new Mojo types. It
    uses PendingReceiver, ReceiverSet, and Remote
    instead of SandboxSupportMacRequest, BindingSet,
    and SandboxSupportMacPtr.
    
    Bug: 955171
    Change-Id: I8d216a85939cba01c392bb821e189610d5e5b885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786708
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#695842}

--- content/child/child_process_sandbox_support_impl_mac.h
+++ content/child/child_process_sandbox_support_impl_mac.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/shared_memory_mapping.h"
 #include "content/common/sandbox_support_mac.mojom.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/platform/mac/web_sandbox_support.h"
 
 namespace service_manager {
@@ -34,7 +35,7 @@ class WebSandboxSupportMac : public blink::WebSandboxSupport {
  private:
   void OnGotSystemColors(base::ReadOnlySharedMemoryRegion region);
 
-  mojom::SandboxSupportMacPtr sandbox_support_;
+  mojo::Remote<mojom::SandboxSupportMac> sandbox_support_;
   base::ReadOnlySharedMemoryMapping color_map_;
 
   DISALLOW_COPY_AND_ASSIGN(WebSandboxSupportMac);
