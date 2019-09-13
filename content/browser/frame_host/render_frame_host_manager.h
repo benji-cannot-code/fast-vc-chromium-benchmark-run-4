23eec156046ef01de31c3f2214640663ef028447
Lowell Manners <lowell@chromium.org>
2019-09-13 11:15:57 +0000

    [bfcache] Destroy evicted documents via PostTask.
    This CL changes the behavior of
    RenderFrameHost:EvictFromBackForwardCache to mark the frame as evicted,
    and use PostTask to schedule the actual destruction of the frame.
    
    This has several benefits:
    1. Protects callers of RenderFrameHost::EvictFromBackForwardCache from
    accidentally doing a use-after-free of the evicted frame.
    
    2. Destroys NavigationRequests that reference a frame being
    restored *before* the frame is destroyed, (the code worked before but
    the new behavior is more correct).
    
    3. There is now only *one* EvictFromBackForwardCache method, (the one
    that everyone should use).
    
    4. Removes possibility of nested WebContentsObserver callbacks when
    eviction is triggered from a WebContentsObserver callback.
    
    Change-Id: I72b6823f65cf5ff65cba32c2cf9a9b0b696db64b
    Bug: 1001528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1794868
    Commit-Queue: Lowell Manners <lowell@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696343}

--- content/browser/frame_host/render_frame_host_manager.h
+++ content/browser/frame_host/render_frame_host_manager.h
@@ -318,7 +318,6 @@ class CONTENT_EXPORT RenderFrameHostManager
   // During a history navigation, unfreezes and swaps in a document from the
   // BackForwardCache, making it active.
   void RestoreFromBackForwardCache(std::unique_ptr<RenderFrameHostImpl>);
-  void EvictFromBackForwardCache(RenderFrameHostImpl*);
 
   // BackForwardCache:
   // Unfreezes the current frame host. This is called after committing a
