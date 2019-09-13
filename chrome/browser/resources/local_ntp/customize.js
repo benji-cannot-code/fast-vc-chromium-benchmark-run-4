b1abd2e337a5b8edf37bfa9adb2ac6c094a4024a
Gayane Petrosyan <gayane@chromium.org>
2019-09-13 18:25:12 +0000

    [Chrome Colors] Fix color preselect when themes are present.
    The problem occurs when preselectedOptions.colorsMenuTile is not
    initialized(e.g. when themes are present). In this case when background
    selection is canceled that triggers a onThemeChange to unpreview the
    image. At the same time if color is selected that will be set to
    preselectedOptions.colorsMenuTile during onThemeChange. This becomes
    problematic for the subsequent dialog interaction.
    
    In this CL:
     - preselectedOptions.colorsMenuTile should always have a valid value,
       even when themes is present.
     - Reset preselectedOptions.colorsMenuTile when colors menu is loaded.
    
    Bug: 1001981
    Change-Id: I5c27b1541ce0f2902763d287c9a6cfb70edbbf3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797362
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696456}

--- chrome/browser/resources/local_ntp/customize.js
+++ chrome/browser/resources/local_ntp/customize.js
@@ -1394,10 +1394,7 @@ customize.richerPicker_resetSelectedOptions = function() {
   customize.selectedOptions.background = null;
   customize.selectedOptions.backgroundData = null;
 
-  // Reset color selection.
-  customize.richerPicker_removeSelectedState(customize.selectedOptions.color);
-  customize.selectedOptions.color = null;
-  customize.preselectedOptions.colorsMenuTile = null;
+  customize.resetColorsSelectedOptions();
 
   customize.richerPicker_preselectShortcutOptions();
 };
@@ -1462,7 +1459,7 @@ customize.richerPicker_resetCustomizationMenu = function() {
   customize.richerPicker_resetSelectedOptions();
   customize.richerPicker_resetImageMenu();
   customize.richerPicker_hideOpenSubmenu();
-  customize.resetColorsMenu();
+  customize.resetColorPicker();
 };
 
 /**
@@ -2245,14 +2242,22 @@ customize.colorPickerTileInteraction = function(event) {
 };
 
 /**
- * Loads Colors menu elements.
+ * Loads Colors menu elements and initializes the selected state.
  */
 customize.loadColorsMenu = function() {
-  if (customize.colorsMenuLoaded) {
-    customize.colorsMenuPreselectTile();
-    return;
+  if (!customize.colorsMenuLoaded) {
+    customize.loadColorMenuTiles();
+    customize.colorsMenuLoaded = true;
   }
 
+  customize.resetColorsSelectedOptions();
+  customize.colorsMenuOnThemeChange();
+};
+
+/**
+ * Loads Colors menu tiles, e.g. color picker, default tile and color tiles.
+ */
+customize.loadColorMenuTiles = function() {
   const colorsColl = ntpApiHandle.getColorsInfo();
   for (let i = 0; i < colorsColl.length; ++i) {
     // After 4 color tiles create an empty tile to take the place of the color
@@ -2304,15 +2309,11 @@ customize.loadColorsMenu = function() {
     $(customize.IDS.COLOR_PICKER).onchange =
         customize.colorPickerTileInteraction;
   }
-
-  customize.colorsMenuOnThemeChange();
-
-  customize.colorsMenuLoaded = true;
 };
 
 /**
- * Update webstore theme info and preselect Colors menu tile according to the
- * theme update.
+ * Update webstore theme info and preselect Colors menu tile according to
+ * the theme update.
  */
 customize.colorsMenuOnThemeChange = function() {
   // Update webstore theme information.
@@ -2331,6 +2332,10 @@ customize.colorsMenuOnThemeChange = function() {
           customize.selectedOptions.color);
       customize.selectedOptions.color = null;
     }
+    if (!customize.preselectedOptions.colorsMenuTile) {
+      customize.preselectedOptions.colorsMenuTile =
+          $(customize.IDS.COLORS_THEME);
+    }
   } else {
     $(customize.IDS.COLORS_THEME).classList.remove(customize.CLASSES.VISIBLE);
 
@@ -2344,7 +2349,6 @@ customize.colorsMenuOnThemeChange = function() {
  */
 customize.colorsMenuPreselectTile = function() {
   const themeInfo = assert(ntpApiHandle.themeBackgroundInfo);
-
   let tile;
   if (themeInfo.usingDefaultTheme) {
     tile = $(customize.IDS.COLORS_DEFAULT_ICON);
@@ -2384,6 +2388,7 @@ customize.colorsMenuPreselectTile = function() {
     if (!customize.preselectedOptions.colorsMenuTile) {
       customize.preselectedOptions.colorsMenuTile = tile;
     }
+
     customize.updateColorsMenuTileSelection(
         /** @type HTMLElement */ (tile));
   }
@@ -2394,11 +2399,8 @@ customize.colorsMenuPreselectTile = function() {
  * menu.
  */
 customize.isColorOptionSelected = function() {
-  return (!customize.preselectedOptions.colorsMenuTile &&
-          customize.selectedOptions.color) ||
-      (customize.preselectedOptions.colorsMenuTile &&
-       customize.selectedOptions.color.id !==
-           customize.preselectedOptions.colorsMenuTile.id);
+  return customize.preselectedOptions.colorsMenuTile !==
+      customize.selectedOptions.color;
 };
 
 /**
@@ -2429,16 +2431,24 @@ customize.cancelColor = function() {
 };
 
 /**
- * Reset Colors Menu elements to the default state, specifically the color
- * picker.
+ * Reset color picker to its default state.
  */
-customize.resetColorsMenu = function() {
+customize.resetColorPicker = function() {
   customize.customColorPicked = customize.defaultCustomColor;
   $(customize.IDS.COLOR_PICKER).value = null;
   $(customize.IDS.COLORS_MENU).style.setProperty('--custom-color-border', '');
   $(customize.IDS.COLORS_MENU).style.setProperty('--custom-color-dark', '');
   $(customize.IDS.COLORS_MENU).style.setProperty('--custom-color-light', '');
   $(customize.IDS.COLOR_PICKER_ICON).classList.toggle('white', false);
+};
+
+/**
+ * Reset color selection.
+ */
+customize.resetColorsSelectedOptions = function() {
+  customize.richerPicker_removeSelectedState(customize.selectedOptions.color);
+  customize.selectedOptions.color = null;
+  customize.preselectedOptions.colorsMenuTile = null;
 };
 
 /**
