e39875bae173f161a010f2337f527a9ec45883ec
Mohamed Amir Yosef <mamir@chromium.org>
2019-09-13 16:30:44 +0000

    [Passwords] Fetching and processing credential in MultiStoreFormFethcer
    Bug: 1002000
    Change-Id: Ie54979ecdaa64da02923fd03804abb10839156c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1796433
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696411}

--- components/password_manager/core/browser/form_fetcher_impl.cc
+++ components/password_manager/core/browser/form_fetcher_impl.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using autofill::PasswordForm;
 
-// Shorten the name to spare line breaks. The code provides enough context
-// already.
 using Logger = autofill::SavePasswordProgressLogger;
 
 namespace password_manager {
--- components/password_manager/core/browser/form_fetcher_impl_unittest.cc
+++ components/password_manager/core/browser/form_fetcher_impl_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/password_manager/core/browser/mock_password_store.h"
+#include "components/password_manager/core/browser/multi_store_form_fetcher.h"
 #include "components/password_manager/core/browser/password_manager_test_utils.h"
 #include "components/password_manager/core/browser/password_store.h"
 #include "components/password_manager/core/browser/statistics_table.h"
@@ -96,6 +97,7 @@ class FakePasswordManagerClient : public StubPasswordManagerClient {
   }
 
   PasswordStore* GetProfilePasswordStore() const override { return store_; }
+  PasswordStore* GetAccountPasswordStore() const override { return nullptr; }
 
   std::unique_ptr<CredentialsFilter> filter_;
   PasswordStore* store_ = nullptr;
@@ -176,7 +178,8 @@ ACTION_P(GetAndAssignWeakPtr, ptr) {
 
 }  // namespace
 
-class FormFetcherImplTest : public testing::Test {
+class FormFetcherImplTest : public testing::Test,
+                            public testing::WithParamInterface<bool> {
  public:
   FormFetcherImplTest()
       : form_digest_(PasswordForm::Scheme::kHtml,
@@ -186,8 +189,13 @@ class FormFetcherImplTest : public testing::Test {
     mock_store_->Init(syncer::SyncableService::StartSyncFlare(), nullptr);
     client_.set_store(mock_store_.get());
 
-    form_fetcher_ = std::make_unique<FormFetcherImpl>(
-        form_digest_, &client_, false /* should_migrate_http_passwords */);
+    if (!GetParam()) {
+      form_fetcher_ = std::make_unique<FormFetcherImpl>(
+          form_digest_, &client_, false /* should_migrate_http_passwords */);
+    } else {
+      form_fetcher_ = std::make_unique<MultiStoreFormFetcher>(
+          form_digest_, &client_, false /* should_migrate_http_passwords */);
+    }
   }
 
   ~FormFetcherImplTest() override { mock_store_->ShutdownOnUIThread(); }
@@ -217,7 +225,7 @@ class FormFetcherImplTest : public testing::Test {
 };
 
 // Check that the absence of PasswordStore results is handled correctly.
-TEST_F(FormFetcherImplTest, NoStoreResults) {
+TEST_P(FormFetcherImplTest, NoStoreResults) {
   Fetch();
   EXPECT_CALL(consumer_, OnFetchCompleted).Times(0);
   form_fetcher_->AddConsumer(&consumer_);
@@ -225,7 +233,7 @@ TEST_F(FormFetcherImplTest, NoStoreResults) {
 }
 
 // Check that empty PasswordStore results are handled correctly.
-TEST_F(FormFetcherImplTest, Empty) {
+TEST_P(FormFetcherImplTest, Empty) {
   Fetch();
   form_fetcher_->AddConsumer(&consumer_);
   EXPECT_CALL(consumer_, OnFetchCompleted);
@@ -238,7 +246,7 @@ TEST_F(FormFetcherImplTest, Empty) {
 }
 
 // Check that non-federated PasswordStore results are handled correctly.
-TEST_F(FormFetcherImplTest, NonFederated) {
+TEST_P(FormFetcherImplTest, NonFederated) {
   Fetch();
   PasswordForm non_federated = CreateNonFederated();
   form_fetcher_->AddConsumer(&consumer_);
@@ -254,7 +262,7 @@ TEST_F(FormFetcherImplTest, NonFederated) {
 }
 
 // Check that federated PasswordStore results are handled correctly.
-TEST_F(FormFetcherImplTest, Federated) {
+TEST_P(FormFetcherImplTest, Federated) {
   Fetch();
   PasswordForm federated = CreateFederated();
   PasswordForm android_federated = CreateAndroidFederated();
@@ -275,7 +283,7 @@ TEST_F(FormFetcherImplTest, Federated) {
 // Check that blacklisted PasswordStore results are handled correctly.
 // Blacklisted PSL matches in the store should be ignored and not returned as a
 // blacklisted match.
-TEST_F(FormFetcherImplTest, Blacklited) {
+TEST_P(FormFetcherImplTest, Blacklited) {
   Fetch();
   PasswordForm blacklisted = CreateBlacklisted();
   PasswordForm blacklisted_psl = CreateBlacklistedPsl();
@@ -293,7 +301,7 @@ TEST_F(FormFetcherImplTest, Blacklited) {
 }
 
 // Check that mixed PasswordStore results are handled correctly.
-TEST_F(FormFetcherImplTest, Mixed) {
+TEST_P(FormFetcherImplTest, Mixed) {
   Fetch();
   PasswordForm federated1 = CreateFederated();
   federated1.username_value = ASCIIToUTF16("user");
@@ -333,7 +341,7 @@ TEST_F(FormFetcherImplTest, Mixed) {
 }
 
 // Check that PasswordStore results are filtered correctly.
-TEST_F(FormFetcherImplTest, Filtered) {
+TEST_P(FormFetcherImplTest, Filtered) {
   Fetch();
   PasswordForm federated = CreateFederated();
   federated.username_value = ASCIIToUTF16("user");
@@ -363,7 +371,7 @@ TEST_F(FormFetcherImplTest, Filtered) {
 }
 
 // Check that stats from PasswordStore are handled correctly.
-TEST_F(FormFetcherImplTest, Stats) {
+TEST_P(FormFetcherImplTest, Stats) {
   Fetch();
   form_fetcher_->AddConsumer(&consumer_);
   std::vector<InteractionsStats> stats(1);
@@ -373,7 +381,7 @@ TEST_F(FormFetcherImplTest, Stats) {
 
 // Test that multiple calls of Fetch() are handled gracefully, and that they
 // always result in passing the most up-to-date information to the consumers.
-TEST_F(FormFetcherImplTest, Update_Reentrance) {
+TEST_P(FormFetcherImplTest, Update_Reentrance) {
   Fetch();
   form_fetcher_->AddConsumer(&consumer_);
   // The fetcher is currently waiting for a store response, after it fired a
@@ -413,7 +421,7 @@ TEST_F(FormFetcherImplTest, Update_Reentrance) {
 }
 
 #if !defined(OS_IOS) && !defined(OS_ANDROID)
-TEST_F(FormFetcherImplTest, FetchStatistics) {
+TEST_P(FormFetcherImplTest, FetchStatistics) {
   InteractionsStats stats;
   stats.origin_domain = form_digest_.origin.GetOrigin();
   stats.username_value = ASCIIToUTF16("some username");
@@ -429,7 +437,7 @@ TEST_F(FormFetcherImplTest, FetchStatistics) {
               UnorderedElementsAre(stats));
 }
 #else
-TEST_F(FormFetcherImplTest, DontFetchStatistics) {
+TEST_P(FormFetcherImplTest, DontFetchStatistics) {
   EXPECT_CALL(*mock_store_, GetLogins(form_digest_, form_fetcher_.get()));
   EXPECT_CALL(*mock_store_, GetSiteStatsImpl(_)).Times(0);
   form_fetcher_->Fetch();
@@ -438,7 +446,7 @@ TEST_F(FormFetcherImplTest, DontFetchStatistics) {
 #endif
 
 // Test that ensures HTTP passwords are not migrated on HTTP sites.
-TEST_F(FormFetcherImplTest, DoNotTryToMigrateHTTPPasswordsOnHTTPSites) {
+TEST_P(FormFetcherImplTest, DoNotTryToMigrateHTTPPasswordsOnHTTPSites) {
   GURL::Replacements http_rep;
   http_rep.SetSchemeStr(url::kHttpScheme);
   const GURL http_origin = form_digest_.origin.ReplaceComponents(http_rep);
@@ -486,7 +494,7 @@ TEST_F(FormFetcherImplTest, DoNotTryToMigrateHTTPPasswordsOnHTTPSites) {
 
 // Test that ensures HTTP passwords are only migrated on HTTPS sites when no
 // HTTPS credentials are available.
-TEST_F(FormFetcherImplTest, TryToMigrateHTTPPasswordsOnHTTPSSites) {
+TEST_P(FormFetcherImplTest, TryToMigrateHTTPPasswordsOnHTTPSSites) {
   GURL::Replacements https_rep;
   https_rep.SetSchemeStr(url::kHttpsScheme);
   const GURL https_origin = form_digest_.origin.ReplaceComponents(https_rep);
@@ -562,7 +570,7 @@ TEST_F(FormFetcherImplTest, TryToMigrateHTTPPasswordsOnHTTPSSites) {
 
 // When the FormFetcher delegates to the HttpPasswordMigrator, its state should
 // be WAITING until the migrator passes the results.
-TEST_F(FormFetcherImplTest, StateIsWaitingDuringMigration) {
+TEST_P(FormFetcherImplTest, StateIsWaitingDuringMigration) {
   GURL::Replacements https_rep;
   https_rep.SetSchemeStr(url::kHttpsScheme);
   const GURL https_origin = form_digest_.origin.ReplaceComponents(https_rep);
@@ -616,7 +624,7 @@ TEST_F(FormFetcherImplTest, StateIsWaitingDuringMigration) {
 
 // Cloning a FormFetcherImpl with empty results should result in an
 // instance with empty results.
-TEST_F(FormFetcherImplTest, Clone_EmptyResults) {
+TEST_P(FormFetcherImplTest, Clone_EmptyResults) {
   Fetch();
   form_fetcher_->OnGetPasswordStoreResults(
       std::vector<std::unique_ptr<PasswordForm>>());
@@ -635,7 +643,7 @@ TEST_F(FormFetcherImplTest, Clone_EmptyResults) {
 
 // Cloning a FormFetcherImpl with non-empty results should result in an
 // instance with the same results.
-TEST_F(FormFetcherImplTest, Clone_NonEmptyResults) {
+TEST_P(FormFetcherImplTest, Clone_NonEmptyResults) {
   Fetch();
   PasswordForm non_federated = CreateNonFederated();
   PasswordForm federated = CreateFederated();
@@ -677,7 +685,7 @@ TEST_F(FormFetcherImplTest, Clone_NonEmptyResults) {
 
 // Cloning a FormFetcherImpl with some stats should result in an instance with
 // the same stats.
-TEST_F(FormFetcherImplTest, Clone_Stats) {
+TEST_P(FormFetcherImplTest, Clone_Stats) {
   Fetch();
   // Pass empty results to make the state NOT_WAITING.
   form_fetcher_->OnGetPasswordStoreResults(
@@ -690,7 +698,7 @@ TEST_F(FormFetcherImplTest, Clone_Stats) {
 }
 
 // Check that removing consumers stops them from receiving store updates.
-TEST_F(FormFetcherImplTest, RemoveConsumer) {
+TEST_P(FormFetcherImplTest, RemoveConsumer) {
   Fetch();
   form_fetcher_->AddConsumer(&consumer_);
   form_fetcher_->RemoveConsumer(&consumer_);
@@ -698,5 +706,7 @@ TEST_F(FormFetcherImplTest, RemoveConsumer) {
   form_fetcher_->OnGetPasswordStoreResults(
       std::vector<std::unique_ptr<PasswordForm>>());
 }
+
+INSTANTIATE_TEST_SUITE_P(, FormFetcherImplTest, testing::Values(false, true));
 
 }  // namespace password_manager
