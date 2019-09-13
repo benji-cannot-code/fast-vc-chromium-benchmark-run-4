fb1c1a5a8dfc4d46c7d0835afd2b66c99d650804
Scott Violet <sky@chromium.org>
2019-09-13 16:38:59 +0000

    blink: adds ComputedStyle::IsDeprecatedWebKitBox()
    This cuts down on duplicated code.
    
    BUG=993813
    TEST=none
    
    Change-Id: I3338df0b4a51fd208ccf51f74b0f6c2983703975
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1799344
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696418}

--- third_party/blink/renderer/core/layout/order_iterator.cc
+++ third_party/blink/renderer/core/layout/order_iterator.cc
@@ -74,10 +74,8 @@ void OrderIterator::Reset() {
 }
 
 int OrderIterator::ResolvedOrder(const LayoutBox& child) const {
-  if (container_box_->StyleRef().Display() == EDisplay::kWebkitBox ||
-      container_box_->StyleRef().Display() == EDisplay::kWebkitInlineBox) {
+  if (container_box_->StyleRef().IsDeprecatedWebkitBox())
     return child.StyleRef().BoxOrdinalGroup();
-  }
   return child.StyleRef().Order();
 }
 
