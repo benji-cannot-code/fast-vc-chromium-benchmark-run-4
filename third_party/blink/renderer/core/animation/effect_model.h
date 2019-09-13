6e2c4d574428bc88ef4d8d735adc5c0de2c50b53
Stephen McGruer <smcgruer@chromium.org>
2019-09-13 13:21:36 +0000

    Animations: Accept 'accumulate' as a composite operation
    This CL does the first step in adding support for the accumulate
    operation, which is to allow it in the JavaScript APIs. Because
    accumulate is often spec'd to be identical to add, this actually
    passes the tests for many properties. The properties that actually
    need real accumulation implementations (to come in future CLs)
    include transform and filter lists.
    
    As with 'add', 'accumulate' is hidden behind CSSAdditiveAnimationsEnabled.
    
    Bug: 788440
    Change-Id: I0eb3374a9b76a4e6a90ae85602fddac9ab0d3a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797652
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696368}

--- third_party/blink/renderer/core/animation/effect_model.h
+++ third_party/blink/renderer/core/animation/effect_model.h
@@ -51,6 +51,7 @@ class CORE_EXPORT EffectModel : public GarbageCollectedFinalized<EffectModel> {
   enum CompositeOperation {
     kCompositeReplace,
     kCompositeAdd,
+    kCompositeAccumulate,
   };
   static base::Optional<CompositeOperation> StringToCompositeOperation(
       const String&);
