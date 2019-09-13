4bb2d1b371b734ae9771fbc9e217783abf720923
Matt Menke <mmenke@chromium.org>
2019-09-13 21:27:00 +0000

    Remove two unused URLRequest APIs.
    StopCaching() stops writing entries to the cache. This was added to
    prevent downloads from being written to the cache, but we stopped
    using it 7 years ago, due to causing performance issues with "save-as",
    which would no longer use the cache when we made the call.
    
    This CL also removes URLRequest::[set_]is_pac_request(). The PAC code
    does actually set this bit, but nothing reads it. It was added to
    prevent extensions from interdicting PAC script requests to effectively
    monitor every request and be able to hijack any HTTP requests. With
    the advent of the network service, this was no longer necessary, as
    extensions have no way to interdict internal network service requests,
    like PAC script requests.
    
    Bug: None
    Change-Id: I837ce356c2e0994165cf35878c956a06d509bc3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804191
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696525}

--- net/url_request/url_request_job.cc
+++ net/url_request/url_request_job.cc
@@ -128,10 +128,6 @@ int URLRequestJob::Read(IOBuffer* buf, int buf_size) {
   return result;
 }
 
-void URLRequestJob::StopCaching() {
-  // Nothing to do here.
-}
-
 int64_t URLRequestJob::GetTotalReceivedBytes() const {
   return 0;
 }
