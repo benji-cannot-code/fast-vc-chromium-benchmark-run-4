e7667a27f3ef4c0d7136d756412721ee93b6a681
Pavel Shmakov <pshmakov@google.com>
2019-09-13 15:14:07 +0000

    Support finishAndRemoveTaskSdk23 api in TWAs for integration with WebApks
    WebApks use finishAndRemoveTaskSdk23 api in two cases:
    1) When user presses back during splash screen (see WebApkActivity#handleBackPressed).
    If both Chrome activity and the underlying client activity finish themselves
    sequentially, there can be a visual glitch. This happens only when the client activity
    is opaque which is why TWAs currently don't suffer from this.
    2) For dealing with interstitial pages (see WebAppActivity#createTabObserver).
    
    This CL addresses the first case. For now we don't expose finishAndRemoveTaskSdk23
    in a public API in AndroidX and use extraCommand.
    
    Bug: 989603
    Change-Id: If67be8c024e78d9f86118198f7b57b821c2c46ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787385
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Pavel Shmakov <pshmakov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696392}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/dependency_injection/CustomTabActivityComponent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/dependency_injection/CustomTabActivityComponent.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.customtabs.dependency_injection;
 
 import org.chromium.chrome.browser.browserservices.trustedwebactivityui.TrustedWebActivityCoordinator;
 import org.chromium.chrome.browser.customtabs.CustomTabActivityClientConnectionKeeper;
+import org.chromium.chrome.browser.browserservices.trustedwebactivityui.TwaFinishHandler;
 import org.chromium.chrome.browser.customtabs.CustomTabActivityLifecycleUmaTracker;
 import org.chromium.chrome.browser.customtabs.CustomTabBottomBarDelegate;
 import org.chromium.chrome.browser.customtabs.CustomTabCompositorContentInitializer;
@@ -50,6 +51,7 @@ public interface CustomTabActivityComponent extends ChromeActivityComponent {
     CustomTabCompositorContentInitializer resolveCompositorContentInitializer();
     CustomTabSessionHandler resolveSessionHandler();
     CustomTabActivityClientConnectionKeeper resolveConnectionKeeper();
+    TwaFinishHandler resolveTwaFinishHandler();
 
     CustomTabTabPersistencePolicy resolveTabPersistencePolicy(); // For testing
 }
