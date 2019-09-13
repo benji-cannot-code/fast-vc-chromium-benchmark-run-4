d1dee892459a9b3312e4497627d70224c950b827
rajendrant <rajendrant@chromium.org>
2019-09-13 23:50:44 +0000

    Refactor lazy load logic to LazyImageHelper
    Separate the lazy load deferral logic in ImageLoader out to a new
    LazyImageHelper with static functions.
    [No intended change in logic]
    
    Bug: 998691
    Change-Id: I4ea07cc18183aa96301116097241e38cb97fab2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788278
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Scott Little <sclittle@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696589}

--- third_party/blink/renderer/core/css/resolver/element_style_resources.cc
+++ third_party/blink/renderer/core/css/resolver/element_style_resources.cc
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/tree_scope.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
-#include "third_party/blink/renderer/core/html/lazy_load_image_observer.h"
+#include "third_party/blink/renderer/core/loader/lazy_image_helper.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style/content_data.h"
 #include "third_party/blink/renderer/core/style/cursor_data.h"
@@ -223,7 +223,7 @@ void ElementStyleResources::LoadPendingImages(ComputedStyle* style) {
                 LoadPendingImage(style, To<StylePendingImage>(background_image),
                                  image_request_optimization);
             if (new_image && new_image->IsLazyloadPossiblyDeferred()) {
-              LazyLoadImageObserver::StartMonitoring(
+              LazyImageHelper::StartMonitoring(
                   pseudo_element_ ? pseudo_element_ : element_.Get());
             }
             background_layer->SetImage(new_image);
