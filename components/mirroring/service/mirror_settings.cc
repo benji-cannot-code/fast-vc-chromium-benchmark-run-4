1dcb0bd1dcf1469b7fbddf641bd20e19e3c2c2c0
Takumi Fujimoto <takumif@chromium.org>
2019-09-13 06:50:12 +0000

    [Mirroring Service] Enable sender-side letterboxing
    Enable sender-side letterboxing by keeping the aspect ratio fixed. This
    fixes the issue where aspect ratios were wrong when mirroring to Android TV
    devices.
    
    Bug: 1001313
    Change-Id: I419d29967829dfad2f1de8f3ad0b7de7b2f02afa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1796622
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696304}

--- components/mirroring/service/mirror_settings.cc
+++ components/mirroring/service/mirror_settings.cc
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/audio_parameters.h"
 
-using media::cast::FrameSenderConfig;
-using media::cast::Codec;
-using media::cast::RtpPayloadType;
 using media::ResolutionChangePolicy;
+using media::cast::Codec;
+using media::cast::FrameSenderConfig;
+using media::cast::RtpPayloadType;
 
 namespace mirroring {
 
@@ -109,8 +109,7 @@ media::VideoCaptureParams MirrorSettings::GetVideoCaptureParams() {
                                 kMaxFrameRate, media::PIXEL_FORMAT_I420);
   if (max_height_ == min_height_ && max_width_ == min_width_) {
     params.resolution_change_policy = ResolutionChangePolicy::FIXED_RESOLUTION;
-  } else if ((100 * min_width_ / min_height_) ==
-             (100 * max_width_ / max_height_)) {
+  } else if (enable_sender_side_letterboxing_) {
     params.resolution_change_policy =
         ResolutionChangePolicy::FIXED_ASPECT_RATIO;
   } else {
@@ -134,7 +133,8 @@ base::Value MirrorSettings::ToDictionaryValue() {
   settings.SetKey("maxHeight", base::Value(max_height_));
   settings.SetKey("minWidth", base::Value(min_width_));
   settings.SetKey("minHeight", base::Value(min_height_));
-  settings.SetKey("senderSideLetterboxing", base::Value(true));
+  settings.SetKey("senderSideLetterboxing",
+                  base::Value(enable_sender_side_letterboxing_));
   settings.SetKey("minFrameRate", base::Value(0));
   settings.SetKey("maxFrameRate", base::Value(kMaxFrameRate));
   settings.SetKey("minVideoBitrate", base::Value(kMinVideoBitrate));
