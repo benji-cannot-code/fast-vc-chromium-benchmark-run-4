eedc6dcea72bda6cf1eed9ecb05fd8840dbca616
Avi Drissman <avi@chromium.org>
2019-09-13 19:31:51 +0000

    Disable flaky tests.
    CleanerSandboxInterface.DeleteService_Running
    ServiceUtilCleanerTest.DeleteRunningService
    CleanerSandboxInterface.DeleteService_HandleHeld
    ServiceUtilCleanerTest.StopAndDeleteRunningService
    
    BUG=956016
    TBR=thakis@chromium.org
    
    Change-Id: Ifab32b0ee3913a0a1b006e55bfae07a3bb1ff1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1803797
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696483}

--- chrome/chrome_cleaner/os/system_util_cleaner_unittest.cc
+++ chrome/chrome_cleaner/os/system_util_cleaner_unittest.cc
@@ -86,7 +86,8 @@ TEST_F(ServiceUtilCleanerTest, DeleteService) {
   EXPECT_FALSE(DoesServiceExist(service_handle.service_name()));
 }
 
-TEST_F(ServiceUtilCleanerTest, StopAndDeleteRunningService) {
+// Disabled: https://crbug.com/956016
+TEST_F(ServiceUtilCleanerTest, DISABLED_StopAndDeleteRunningService) {
   // Install and launch the service.
   TestScopedServiceHandle service_handle;
   ASSERT_TRUE(service_handle.InstallService());
@@ -109,7 +110,8 @@ TEST_F(ServiceUtilCleanerTest, StopAndDeleteRunningService) {
   EXPECT_FALSE(IsProcessRunning(kTestServiceExecutableName));
 }
 
-TEST_F(ServiceUtilCleanerTest, DeleteRunningService) {
+// Disabled: https://crbug.com/956016
+TEST_F(ServiceUtilCleanerTest, DISABLED_DeleteRunningService) {
   // Install and launch the service.
   TestScopedServiceHandle service_handle;
   ASSERT_TRUE(service_handle.InstallService());
