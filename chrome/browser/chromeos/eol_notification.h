b650812dc7966c3c55ce58f5bbb3e7e90e52acd7
Regan Hsu <hsuregan@chromium.org>
2019-09-13 00:22:15 +0000

    Revert "Add number of milestones parameter to EOL."
    This CL is reverted order to improve clarity of
    https://chromium-review.googlesource.com/c/chromium/src/+/1775160,
    and because we are no longer using number_of_milestones to
    determine when to show a warning EOL notification.
    
    This reverts commit d205c87234c07f6eb110f3a1797c0deb6300d4e0.
    
    Bug: 994999
    Change-Id: I39f032946dc52f19191f9ce04ed158c3fc52f127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1802097
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696240}

--- chrome/browser/chromeos/eol_notification.h
+++ chrome/browser/chromeos/eol_notification.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/optional.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/profiles/profile.h"
 #include "third_party/cros_system_api/dbus/update_engine/dbus-constants.h"
@@ -31,11 +30,7 @@ class EolNotification final {
 
  private:
   // Callback invoked when |GetEolStatus()| has finished.
-  // - EndOfLife status: the end of life status of the device.
-  // - Optional<int32_t> number_of_milestones: the number of milestones before
-  // end of life, if known by update engine.
-  void OnEolStatus(update_engine::EndOfLifeStatus status,
-                   base::Optional<int32_t> number_of_milestones);
+  void OnEolStatus(update_engine::EndOfLifeStatus status);
 
   // Create or updates the notfication.
   void Update();
@@ -45,9 +40,6 @@ class EolNotification final {
 
   // Device EndOfLife status.
   update_engine::EndOfLifeStatus status_;
-
-  // Number of milestones remaining until end of life.
-  base::Optional<int32_t> number_of_milestones_;
 
   // Factory of callbacks.
   base::WeakPtrFactory<EolNotification> weak_factory_{this};
--- chromeos/dbus/update_engine_client.cc
+++ chromeos/dbus/update_engine_client.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
-#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chromeos/dbus/constants/dbus_switches.h"
@@ -407,16 +406,14 @@ class UpdateEngineClientImpl : public UpdateEngineClient {
   void OnGetEolStatus(GetEolStatusCallback callback, dbus::Response* response) {
     if (!response) {
       LOG(ERROR) << "Failed to request getting eol status";
-      std::move(callback).Run(update_engine::EndOfLifeStatus::kSupported,
-                              base::nullopt /* number_of_milestones */);
+      std::move(callback).Run(update_engine::EndOfLifeStatus::kSupported);
       return;
     }
     dbus::MessageReader reader(response);
     int status;
     if (!reader.PopInt32(&status)) {
       LOG(ERROR) << "Incorrect response: " << response->ToString();
-      std::move(callback).Run(update_engine::EndOfLifeStatus::kSupported,
-                              base::nullopt /* number_of_milestones */);
+      std::move(callback).Run(update_engine::EndOfLifeStatus::kSupported);
       return;
     }
 
@@ -424,25 +421,13 @@ class UpdateEngineClientImpl : public UpdateEngineClient {
     if (status > update_engine::EndOfLifeStatus::kEol ||
         status < update_engine::EndOfLifeStatus::kSupported) {
       LOG(ERROR) << "Incorrect status value: " << status;
-      std::move(callback).Run(update_engine::EndOfLifeStatus::kSupported,
-                              base::nullopt /* number_of_milestones */);
+      std::move(callback).Run(update_engine::EndOfLifeStatus::kSupported);
       return;
     }
 
-    base::Optional<int32_t> opt_number_of_milestones;
-    int32_t number_of_milestones;
-    if (reader.PopInt32(&number_of_milestones)) {
-      opt_number_of_milestones = number_of_milestones;
-    }
-
-    std::string str_number_of_milestones =
-        opt_number_of_milestones
-            ? base::NumberToString(opt_number_of_milestones.value())
-            : "not provided";
-    VLOG(1) << "Eol status received: " << status
-            << ". Number of Milestones: " << str_number_of_milestones;
-    std::move(callback).Run(static_cast<update_engine::EndOfLifeStatus>(status),
-                            std::move(opt_number_of_milestones));
+    VLOG(1) << "Eol status received: " << status;
+    std::move(callback).Run(
+        static_cast<update_engine::EndOfLifeStatus>(status));
   }
 
   // Called when a response for SetUpdateOverCellularPermission() is received.
@@ -602,8 +587,7 @@ class UpdateEngineClientStubImpl : public UpdateEngineClient {
   }
 
   void GetEolStatus(GetEolStatusCallback callback) override {
-    std::move(callback).Run(update_engine::EndOfLifeStatus::kSupported,
-                            base::nullopt /* number_of_milestones */);
+    std::move(callback).Run(update_engine::EndOfLifeStatus::kSupported);
   }
 
   void SetUpdateOverCellularPermission(bool allowed,
