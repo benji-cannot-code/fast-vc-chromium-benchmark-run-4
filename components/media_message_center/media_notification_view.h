99ac7af5ef9be8a37ec044ec5521ccea7e623e3b
Tommy Steimel <steimel@chromium.org>
2019-09-13 17:41:56 +0000

    GMC: Force notifications to be expanded when artwork is available
    This CL adds the ability to force an expanded state on a
    MediaNotificationView. This allows GMC to force media sessions with
    artwork (or 4+ enabled actions) to be always expanded, which prevents
    contrast issues with the buttons and the artwork.
    
    Bug: 1003061
    Change-Id: If963a551c94aecb1682ef299a4b033aa1cce455f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1800915
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696445}

--- components/media_message_center/media_notification_view.h
+++ components/media_message_center/media_notification_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "services/media_session/public/mojom/media_session.mojom.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/image_button.h"
@@ -71,6 +72,12 @@ class COMPONENT_EXPORT(MEDIA_MESSAGE_CENTER) MediaNotificationView
   void SetExpanded(bool expanded);
   void UpdateCornerRadius(int top_radius, int bottom_radius);
 
+  // When |forced_expanded_state| has a value, the notification will be forced
+  // into that expanded state and the user won't be given a button to toggle the
+  // expanded state. Subsequent |SetExpanded()| calls will be ignored until
+  // |SetForcedExpandedState(nullptr)| is called.
+  void SetForcedExpandedState(bool* forced_expanded_state);
+
   // views::View:
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
 
@@ -121,6 +128,9 @@ class COMPONENT_EXPORT(MEDIA_MESSAGE_CENTER) MediaNotificationView
 
   // Whether this notification is expanded or not.
   bool expanded_ = false;
+
+  // Used to force the notification to remain in a specific expanded state.
+  base::Optional<bool> forced_expanded_state_;
 
   // Set of enabled actions.
   std::set<media_session::mojom::MediaSessionAction> enabled_actions_;
