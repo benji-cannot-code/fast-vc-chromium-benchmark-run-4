b572af55c6bd8aa1f18c912dc8e7a288c29813d7
Ryan Daum <rdaum@chromium.org>
2019-09-13 18:34:05 +0000

    [chromecast] Allow temporary override of display settings
    Adds a method to allow the temporary override of the cast's primary
    display settings. This will be used to adjust the bounds and scale
    factor primary to the construction of an application's web view.
    
    Bug: internal b/138446155
    Test: manual
    Change-Id: I2c9c027a86389863aa6d3095344f3cf30cf8f948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1802830
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696463}

--- chromecast/graphics/cast_screen.h
+++ chromecast/graphics/cast_screen.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMECAST_GRAPHICS_CAST_SCREEN_H_
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "chromecast/public/graphics_types.h"
 #include "ui/display/display.h"
 #include "ui/display/screen_base.h"
@@ -38,7 +39,21 @@ class CastScreen : public display::ScreenBase {
                         display::Display::Rotation rotation,
                         const gfx::Rect& bounds);
 
+  // Temporarily override the primary display settings with new bounds, scale
+  // factor, and rotation.
+  // The original display settings are stashed for later retrieval.
+  void OverridePrimaryDisplaySettings(const gfx::Rect& bounds,
+                                      float scale_factor,
+                                      display::Display::Rotation rotation);
+
+  // Restore stashed display settings stored by OverridePrimaryDisplaySettings.
+  // Returns true if stashed settings were applied. False if none were
+  // available.
+  bool RestorePrimaryDisplaySettings();
+
  private:
+  base::Optional<display::Display> stashed_display_settings_;
+
   DISALLOW_COPY_AND_ASSIGN(CastScreen);
 };
 
