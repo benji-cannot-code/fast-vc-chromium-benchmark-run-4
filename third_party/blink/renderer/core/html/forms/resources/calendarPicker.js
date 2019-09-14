8140dc6bb16e735ad2b97c4b1b42429112106346
Ionel Popescu <iopopesc@microsoft.com>
2019-09-14 15:31:39 +0000

    Update the popup UI for input type="month"
    The input type="month" popup UI is updated by introducing a new MonthPicker.
    MonthPicker uses 2 parts already implemented:
    	- year list view
            - today button
    
    Added tests to validate the new UI for the popup.
    
    Further CLs will be sent for updating the font, colors, keyboard navigation
    and introducing support for step attribute.
    
    Bug: 1003625
    Change-Id: Id6ef4e88877edea4cba5b963f27681ce56d79d77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1802684
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696651}

--- third_party/blink/renderer/core/html/forms/resources/calendarPicker.js
+++ third_party/blink/renderer/core/html/forms/resources/calendarPicker.js
@@ -834,6 +834,12 @@ function openSuggestionPicker() {
 
 function openCalendarPicker() {
   closePicker();
+  if (global.params.isFormControlsRefreshEnabled) {
+    if (global.params.mode == 'month') {
+      return initializeMonthPicker(global.params);
+    }
+  }
+
   global.picker = new CalendarPicker(global.params.mode, global.params);
   global.picker.attachTo($('main'));
 };
