64c9a0c4adb1b5f5cabdd60ac1bb8d380a0bf893
Kurt Horimoto <kkhorimoto@google.com>
2019-09-16 22:24:46 +0000

    [iOS] Use non-modal contained presenter for HTTP auth dialogs
    HTTP auth dialogs are now presented as child view controllers.  This
    allows for usage NonModalViewControllerPresenter, which has the
    durations provided by UX.
    
    Also, since the view controllers can be presented immediately, this
    CL also prevents the flicker from occurring when exiting the tab
    switcher by avoiding the asynchronicity of view controller
    presentation.
    
    Bug: 979030, 999960, 999962
    Change-Id: I8aec7617bd4900aa05859aea514f2429b95f7d37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788513
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696966}

--- ios/chrome/browser/ui/overlays/web_content_area/http_auth_dialogs/http_auth_dialog_overlay_coordinator.mm
+++ ios/chrome/browser/ui/overlays/web_content_area/http_auth_dialogs/http_auth_dialog_overlay_coordinator.mm
@@ -10,18 +10,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/alert_view_controller/alert_view_controller.h"
 #import "ios/chrome/browser/ui/overlays/overlay_request_coordinator_delegate.h"
 #import "ios/chrome/browser/ui/overlays/web_content_area/http_auth_dialogs/http_auth_dialog_overlay_mediator.h"
+#import "ios/chrome/browser/ui/presenters/contained_presenter_delegate.h"
+#import "ios/chrome/browser/ui/presenters/non_modal_view_controller_presenter.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 @interface HTTPAuthDialogOverlayCoordinator () <
+    ContainedPresenterDelegate,
     HTTPAuthDialogOverlayMediatorDataSource,
     HTTPAuthDialogOverlayMediatorDelegate>
 // Whether the coordinator has been started.
 @property(nonatomic, getter=isStarted) BOOL started;
 
 @property(nonatomic) AlertViewController* alertViewController;
+@property(nonatomic) NonModalViewControllerPresenter* presenter;
 @property(nonatomic) HTTPAuthDialogOverlayMediator* mediator;
 @end
 
@@ -37,6 +41,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _mediator = mediator;
   _mediator.dataSource = self;
   _mediator.delegate = self;
+}
+
+#pragma mark - ContainedPresenterDelegate
+
+- (void)containedPresenterDidPresent:(id<ContainedPresenter>)presenter {
+  self.delegate->OverlayUIDidFinishPresentation(self.request);
+}
+
+- (void)containedPresenterDidDismiss:(id<ContainedPresenter>)presenter {
+  self.alertViewController = nil;
+  self.presenter = nil;
+  self.delegate->OverlayUIDidFinishDismissal(self.request);
 }
 
 #pragma mark - HTTPAuthDialogOverlayMediatorDataSource
@@ -66,6 +82,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return !!request->GetConfig<HTTPAuthOverlayRequestConfig>();
 }
 
++ (BOOL)usesChildViewController {
+  return YES;
+}
+
 - (UIViewController*)viewController {
   return self.alertViewController;
 }
@@ -81,31 +101,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.mediator =
       [[HTTPAuthDialogOverlayMediator alloc] initWithRequest:self.request];
   self.mediator.consumer = self.alertViewController;
-  __weak __typeof__(self) weakSelf = self;
-  [self.baseViewController
-      presentViewController:self.alertViewController
-                   animated:animated
-                 completion:^{
-                   weakSelf.delegate->OverlayUIDidFinishPresentation(
-                       weakSelf.request);
-                 }];
+  self.presenter = [[NonModalViewControllerPresenter alloc] init];
+  self.presenter.delegate = self;
+  self.presenter.baseViewController = self.baseViewController;
+  self.presenter.presentedViewController = self.alertViewController;
+  [self.presenter prepareForPresentation];
+  [self.presenter presentAnimated:animated];
   self.started = YES;
 }
 
 - (void)stopAnimated:(BOOL)animated {
   if (!self.started)
     return;
-  __weak __typeof__(self) weakSelf = self;
-  [self.baseViewController
-      dismissViewControllerAnimated:animated
-                         completion:^{
-                           __typeof__(self) strongSelf = weakSelf;
-                           if (!strongSelf)
-                             return;
-                           strongSelf.alertViewController = nil;
-                           strongSelf.delegate->OverlayUIDidFinishDismissal(
-                               weakSelf.request);
-                         }];
+  [self.presenter dismissAnimated:animated];
   self.started = NO;
 }
 
