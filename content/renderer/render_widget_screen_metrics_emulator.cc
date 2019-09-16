07f77f93a252137ccc23c6b429a3efa4536cfec0
Erik Chen <erikchen@chromium.org>
2019-09-16 18:46:34 +0000

    Remove dead code in RenderWidgetScreenMetricsEmulator.
    Change-Id: I2aa9448b49b4b1a56cb9644fc35f878b0c3a6323
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804376
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696882}

--- content/renderer/render_widget_screen_metrics_emulator.cc
+++ content/renderer/render_widget_screen_metrics_emulator.cc
@@ -159,9 +159,4 @@ void RenderWidgetScreenMetricsEmulator::OnShowContextMenu(
   params->y *= scale_;
 }
 
-gfx::Rect RenderWidgetScreenMetricsEmulator::AdjustValidationMessageAnchor(
-    const gfx::Rect& anchor) {
-  return gfx::ScaleToEnclosedRect(anchor, scale_);
-}
-
 }  // namespace content
