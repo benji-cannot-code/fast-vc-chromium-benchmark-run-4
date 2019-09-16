63c82f3fed2b35a24f6d3feb3940690d89e42cc3
Xianzhu Wang <wangxianzhu@chromium.org>
2019-09-16 17:18:47 +0000

    Reland "Remove viewport features from PropertyTreeBuilder"
    This reverts commit bc63d69f47f5498eededb4f553b59d6312d2d670.
    
    Fix cc_unittest by removing unnecessary casts.
    
    Original change's description:
    > Revert "Remove viewport features from PropertyTreeBuilder"
    >
    > This reverts commit 79901dcd9293ef2e8e78c1a48a2d3c53af3b13f8.
    >
    > Reason for revert: Breaks cc_unittests
    >
    > https://findit-for-me.appspot.com/waterfall/failure?url=https://build.chromium.org/p/chromium.memory/builders/Linux%20CFI/builds/14983
    >
    > https://ci.chromium.org/p/chromium/builders/ci/Linux%20CFI/14997
    >
    > [ RUN      ] LayerTreeHostScrollTestCaseWithChild.DeviceScaleFactor15_ScrollChild
    > ../../cc/trees/layer_tree_host_unittest_scroll.cc:671:9: runtime error: control flow integrity check for type 'cc::FakePictureLayerImpl' failed during base-to-derived cast (vtable address 0x55dc2aba43f0)
    > 0x55dc2aba43f0: note: vtable is of type 'cc::LayerImpl'
    >
    > Original change's description:
    > > Remove viewport features from PropertyTreeBuilder
    > >
    > > These features are not used in ui compositor, so PropertyTreeBuilder
    > > doesn't need to support them.
    > >
    > > Bug: 994361
    > > Change-Id: I5159cf8ab36cd42e4807a1d190f987b4e07c0407
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804383
    > > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#696658}
    >
    > TBR=wangxianzhu@chromium.org,pdr@chromium.org
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: 994361
    > Change-Id: I919048adf9c88511a8c58aae6170c8944035d64b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804943
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#696781}
    
    TBR=wangxianzhu@chromium.org,pdr@chromium.org,ortuno@chromium.org,ioanap@chromium.org
    
    Change-Id: I6aeaef29a537c44478f322283f8d96761e64e2ce
    Bug: 994361
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807096
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696839}

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -201,7 +201,8 @@ LayerTreeHost::~LayerTreeHost() {
   mutator_host_->SetMutatorHostClient(nullptr);
 
   // We must clear any pointers into the layer tree prior to destroying it.
-  RegisterViewportLayers(ViewportLayers());
+  if (IsUsingLayerLists())
+    RegisterViewportLayers(ViewportLayers());
 
   if (root_layer_) {
     root_layer_->SetLayerTreeHost(nullptr);
@@ -802,13 +803,7 @@ bool LayerTreeHost::DoUpdateLayers() {
   // need to be built here.
   if (!IsUsingLayerLists()) {
     TRACE_EVENT0("cc", "LayerTreeHost::UpdateLayers::BuildPropertyTrees");
-    Layer* page_scale_layer = viewport_layers_.page_scale.get();
-    gfx::Transform identity_transform;
-    PropertyTreeBuilder::BuildPropertyTrees(
-        root_layer_.get(), page_scale_layer, inner_viewport_scroll_layer(),
-        outer_viewport_scroll_layer(), overscroll_elasticity_element_id(),
-        elastic_overscroll_, page_scale_factor_, device_scale_factor_,
-        device_viewport_rect_, identity_transform, &property_trees_);
+    PropertyTreeBuilder::BuildPropertyTrees(this);
     TRACE_EVENT_INSTANT1(TRACE_DISABLED_BY_DEFAULT("cc.debug"),
                          "LayerTreeHost::UpdateLayers_BuiltPropertyTrees",
                          TRACE_EVENT_SCOPE_THREAD, "property_trees",
@@ -1111,12 +1106,7 @@ void LayerTreeHost::RegisterViewportLayers(const ViewportLayers& layers) {
   //     overscroll elasticity (optional)
   //       page scale
   //         inner viewport scroll
-  DCHECK(IsUsingLayerLists() || !layers.page_scale ||
-         layers.inner_viewport_scroll->parent() == layers.page_scale);
-  DCHECK(IsUsingLayerLists() || !layers.page_scale ||
-         layers.page_scale->parent()->element_id() ==
-             layers.overscroll_elasticity_element_id ||
-         layers.page_scale->parent() == layers.inner_viewport_container);
+  DCHECK(IsUsingLayerLists());
   viewport_layers_.overscroll_elasticity_element_id =
       layers.overscroll_elasticity_element_id;
   viewport_layers_.page_scale = layers.page_scale;
@@ -1694,12 +1684,7 @@ void LayerTreeHost::SetElementIdsForTesting() {
 }
 
 void LayerTreeHost::BuildPropertyTreesForTesting() {
-  gfx::Transform identity_transform;
-  PropertyTreeBuilder::BuildPropertyTrees(
-      root_layer(), page_scale_layer(), inner_viewport_scroll_layer(),
-      outer_viewport_scroll_layer(), overscroll_elasticity_element_id(),
-      elastic_overscroll(), page_scale_factor(), device_scale_factor(),
-      device_viewport_rect(), identity_transform, property_trees());
+  PropertyTreeBuilder::BuildPropertyTrees(this);
 }
 
 bool LayerTreeHost::IsElementInPropertyTrees(ElementId element_id,
