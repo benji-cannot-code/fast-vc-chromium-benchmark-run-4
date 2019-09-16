17fc4ed8c6bd275554e852320059981befa7207b
John Rummell <jrummell@chromium.org>
2019-09-16 22:45:57 +0000

    Enable Widevine AV1 playback
    With the new Widevine CDM, support for AV1 has been added, so add AV1 to
    the list of codecs supported by Widevine on Linux and ChromeOS devices.
    Other OSes will get AV1 support from the manifest included with
    Widevine.
    
    Bug: 953504
    Test: enabled browser_tests pass
    Change-Id: I33764dda1239c0ac4578d7ba09604fc59f2d1e79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1802834
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696973}

--- chrome/browser/media/encrypted_media_browsertest.cc
+++ chrome/browser/media/encrypted_media_browsertest.cc
@@ -612,24 +612,10 @@ IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoOnly_MP4_VP9Profile2) {
 
 #if BUILDFLAG(ENABLE_AV1_DECODER)
 IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoOnly_WebM_AV1) {
-#if BUILDFLAG(ENABLE_WIDEVINE)
-  // TODO(crbug.com/884845): Update Widevine CDM to support AV1.
-  if (IsWidevine(CurrentKeySystem())) {
-    DVLOG(0) << "Skipping test - Widevine CDM does not support AV1";
-    return;
-  }
-#endif
   TestSimplePlayback("bear-av1-cenc.webm");
 }
 
 IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoOnly_WebM_AV1_10bit) {
-#if BUILDFLAG(ENABLE_WIDEVINE)
-  // TODO(crbug.com/884845): Update Widevine CDM to support AV1.
-  if (IsWidevine(CurrentKeySystem())) {
-    DVLOG(0) << "Skipping test - Widevine CDM does not support AV1";
-    return;
-  }
-#endif
   TestSimplePlayback("bear-av1-320x180-10bit-cenc.webm");
 }
 
@@ -639,13 +625,6 @@ IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoOnly_MP4_AV1) {
     DVLOG(0) << "Skipping test; Can only play MP4 encrypted streams by MSE.";
     return;
   }
-#if BUILDFLAG(ENABLE_WIDEVINE)
-  // TODO(crbug.com/884845): Update Widevine CDM to support AV1.
-  if (IsWidevine(CurrentKeySystem())) {
-    DVLOG(0) << "Skipping test - Widevine CDM does not support AV1";
-    return;
-  }
-#endif
   TestSimplePlayback("bear-av1-cenc.mp4");
 }
 
@@ -655,13 +634,6 @@ IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoOnly_MP4_AV1_10bit) {
     DVLOG(0) << "Skipping test; Can only play MP4 encrypted streams by MSE.";
     return;
   }
-#if BUILDFLAG(ENABLE_WIDEVINE)
-  // TODO(crbug.com/884845): Update Widevine CDM to support AV1.
-  if (IsWidevine(CurrentKeySystem())) {
-    DVLOG(0) << "Skipping test - Widevine CDM does not support AV1";
-    return;
-  }
-#endif
   TestSimplePlayback("bear-av1-320x180-10bit-cenc.mp4");
 }
 #endif  // BUILDFLAG(ENABLE_AV1_DECODER)
