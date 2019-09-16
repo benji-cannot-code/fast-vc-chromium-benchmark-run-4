14f2b7d777b875a5311e57b679ea39cd65126bd4
Sujie Zhu <sujiezhu@google.com>
2019-09-16 20:55:52 +0000

    [AF] Clean up flag autofill-import-dynamic-forms
    AutofillImportDynamicForms is launched 100% Stable. We clean up the flag together with any related usage of this flag.
    
    Bug: 947312
    Change-Id: I928794ae1d5fcc7a794d140a0370c515e9dff6db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1777073
    Commit-Queue: Sujie Zhu <sujiezhu@google.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#696932}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2609,10 +2609,6 @@ const FeatureEntry kFeatureEntries[] = {
      kOsAll,
      FEATURE_VALUE_TYPE(
          autofill::features::kAutofillUpstreamEditableExpirationDate)},
-    {"enable-autofill-import-dynamic-forms",
-     flag_descriptions::kEnableAutofillImportDynamicFormsName,
-     flag_descriptions::kEnableAutofillImportDynamicFormsDescription, kOsAll,
-     FEATURE_VALUE_TYPE(autofill::features::kAutofillImportDynamicForms)},
     {"enable-autofill-local-card-migration-uses-strike-system-v2",
      flag_descriptions::kEnableAutofillLocalCardMigrationUsesStrikeSystemV2Name,
      flag_descriptions::
