d65725294f2ecaa5f4595eb8b441d0442d0315bf
Joe DeBlasio <jdeblasio@chromium.org>
2019-09-16 23:44:01 +0000

    Safety Tip: Switch to shield on Android infobar.
    This CL swaps the lightbulb icon for the new hybrid shield-bulb and
    switches to the "big icon" infobar.  This CL also removes the lightbulb
    icon from Android, since nothing is using it anymore.
    
    Bug: 1000337
    Change-Id: Iabacf98e0a568bfaff03e8e6a0318f055cfe74a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1783279
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696995}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/SafetyTipInfoBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/SafetyTipInfoBar.java
@@ -56,6 +56,7 @@ public class SafetyTipInfoBar extends ConfirmInfoBar {
     @Override
     public void createContent(InfoBarLayout layout) {
         super.createContent(layout);
+        layout.setIsUsingBigIcon();
         layout.getMessageLayout().addDescription(mDescription);
     }
 }
--- chrome/browser/lookalikes/safety_tips/safety_tip_infobar_delegate.cc
+++ chrome/browser/lookalikes/safety_tips/safety_tip_infobar_delegate.cc
@@ -97,7 +97,7 @@ SafetyTipInfoBarDelegate::GetIdentifier() const {
 }
 
 int SafetyTipInfoBarDelegate::GetIconId() const {
-  return IDR_ANDROID_INFOBAR_TIP;
+  return IDR_ANDROID_INFOBAR_SAFETYTIP_SHIELD;
 }
 
 void SafetyTipInfoBarDelegate::InfoBarDismissed() {
