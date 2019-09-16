6f34fc85235a3e666732766fa5ec40f8ea0b7840
Jesse Doherty <jwd@chromium.org>
2019-09-16 16:01:33 +0000

    Fix some logic in the ukm metric type validation.
    Previously owners errors were ignored unless there were metric type errors. Also if any metric was missing an enum/unit it would create warnings for every event.
    
    Change-Id: Ib52034d659d553fdf369025d7399a65dfac32454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804506
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696812}

--- tools/metrics/ukm/validate_format.py
+++ tools/metrics/ukm/validate_format.py
@@ -31,7 +31,7 @@ def main():
 
     results = dict();
 
-    if not metricCheckSuccess or not metricCheckSuccess:
+    if not ownerCheckSuccess or not metricCheckSuccess:
       results['Errors'] = ownerCheckErrors + metricCheckErrors
     if metricCheckWarnings and not IGNORE_METRIC_CHECK_WARNINGS:
       results['Warnings'] = metricCheckWarnings
