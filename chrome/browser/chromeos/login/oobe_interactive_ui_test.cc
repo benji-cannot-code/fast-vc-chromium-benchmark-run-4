bb368320e50d6411d64f9d4ff96dd76205f8895c
Ioana Pandele <ioanap@chromium.org>
2019-09-16 17:33:56 +0000

    [Sheriff] Disable OobeInteractiveUITest.SimpleEndToEnd/* in Debug Also disable OobeZeroTouchInteractiveUITest.EndToEnd.*.
    They are all timing out on linux-chromeos-dbg.
    
    TBR=antrim@chromium.org
    
    Bug: 1004327
    Change-Id: Ic3bc507119d99c64b1a8b1016643f3fd8d8c2178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807115
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696844}

--- chrome/browser/chromeos/login/oobe_interactive_ui_test.cc
+++ chrome/browser/chromeos/login/oobe_interactive_ui_test.cc
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -586,7 +587,13 @@ void OobeInteractiveUITest::SimpleEndToEnd() {
   WaitForLoginDisplayHostShutdown();
 }
 
-IN_PROC_BROWSER_TEST_P(OobeInteractiveUITest, SimpleEndToEnd) {
+// Timing out on debug bots. crbug.com/1004327
+#if defined(NDEBUG)
+#define MAYBE_SimpleEndToEnd SimpleEndToEnd
+#else
+#define MAYBE_SimpleEndToEnd DISABLED_SimpleEndToEnd
+#endif
+IN_PROC_BROWSER_TEST_P(OobeInteractiveUITest, MAYBE_SimpleEndToEnd) {
   SimpleEndToEnd();
 }
 
@@ -644,8 +651,8 @@ void OobeZeroTouchInteractiveUITest::ZeroTouchEndToEnd() {
   WaitForLoginDisplayHostShutdown();
 }
 
-// crbug.com/997987
-#if defined(MEMORY_SANITIZER)
+// crbug.com/997987. Disabled in debug since they time out.crbug.com/1004327
+#if defined(MEMORY_SANITIZER) || !defined(NDEBUG)
 #define MAYBE_EndToEnd DISABLED_EndToEnd
 #else
 #define MAYBE_EndToEnd EndToEnd
