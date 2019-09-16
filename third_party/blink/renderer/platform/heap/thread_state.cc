8a0721dba7c1bda6b7ec60f455be3fa419b2e3cd
Omer Katz <omerkatz@chromium.org>
2019-09-16 16:14:40 +0000

    heap: Stop and repost concurrent marking tasks
    If concurrent marking tasks go on until they empty the worklist, it
    might result in priority inversion that will starve higher priority
    tasks.
    Concurrent tasks should have a timeout, after which tasks stop and
    repost themselves. This gives the scheduler more control over when
    concurrent marking tasks are executed.
    
    Bug: 986235
    Change-Id: I9f0747ed51b27aa9f6a85a9e8aab8564e3804ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1803446
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696819}

--- third_party/blink/renderer/platform/heap/thread_state.cc
+++ third_party/blink/renderer/platform/heap/thread_state.cc
@@ -104,6 +104,14 @@ namespace {
 constexpr base::TimeDelta kDefaultIncrementalMarkingStepDuration =
     base::TimeDelta::FromMilliseconds(2);
 
+// Concurrent marking should stop every once in a while to flush private
+// segments to v8 marking worklist. It should also stop to avoid priority
+// inversion.
+//
+// TODO(omerkatz): What is a good value to set here?
+constexpr base::TimeDelta kConcurrentMarkingStepDuration =
+    base::TimeDelta::FromMilliseconds(2);
+
 constexpr size_t kMaxTerminationGCLoops = 20;
 
 // Helper function to convert a byte count to a KB count, capping at
@@ -1648,7 +1656,10 @@ void ThreadState::PerformConcurrentMark(int concurrent_marker_id) {
           : std::make_unique<ConcurrentMarkingVisitor>(
                 this, GetMarkingMode(Heap().Compaction()->IsCompacting()),
                 task_id);
-  Heap().AdvanceConcurrentMarking(concurrent_visitor.get());
+
+  const bool finished = Heap().AdvanceConcurrentMarking(
+      concurrent_visitor.get(),
+      base::TimeTicks::Now() + kConcurrentMarkingStepDuration);
 
   concurrent_visitor->FlushWorklists();
   {
@@ -1656,8 +1667,16 @@ void ThreadState::PerformConcurrentMark(int concurrent_marker_id) {
     // When marking is done, flush visitor worklists and decrement number of
     // active markers so we know how many markers are left
     concurrently_marked_bytes_ += concurrent_visitor->marked_bytes();
-    --active_markers_;
+    if (finished) {
+      --active_markers_;
+      return;
+    }
   }
+
+  // Reschedule this marker
+  marker_scheduler_->ScheduleTask(WTF::CrossThreadBindOnce(
+      &ThreadState::PerformConcurrentMark, WTF::CrossThreadUnretained(this),
+      concurrent_marker_id));
 }
 
 }  // namespace blink
