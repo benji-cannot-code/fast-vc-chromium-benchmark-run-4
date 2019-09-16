2e0824d3e2ac59fd7db24343d2aad6b7c6531fb8
jonross <jonross@chromium.org>
2019-09-16 22:01:40 +0000

    Vulkan Pixel Tests stop Software Fallback Crash
    We were trying to have all vulkan_pixel_skia tests not fallback to software
    with the flag '--disable-software-compositing-fallback'. However several tests
    wish to explicitly run in that mode.
    
    This removes the flag from the test configuration, while looking to add it to
    tests which actually only want to test hardware compositing.
    
    TEST=vulkan_pixel_skia, pixel tests
    
    Bug: 984973
    Change-Id: I5b54bcde0c10be874ad5fe5cd00168dfd98ba08a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1795307
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696957}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -622,7 +622,8 @@ class PixelTestPages(object):
   # Pages that should be run with GPU rasterization enabled.
   @staticmethod
   def GpuRasterizationPages(base_name):
-    browser_args = ['--force-gpu-rasterization']
+    browser_args = ['--force-gpu-rasterization',
+                    '--disable-software-compositing-fallback']
     return [
       PixelTestPage(
         'pixel_background.html',
@@ -745,6 +746,7 @@ class PixelTestPages(object):
   def ExperimentalCanvasFeaturesPages(base_name):
     browser_args = [
       '--enable-experimental-web-platform-features'] # for lowLatency
+    accelerated_args = ['--disable-software-compositing-fallback']
     unaccelerated_args = [
       '--disable-accelerated-2d-canvas',
       '--disable-gpu-compositing']
@@ -834,14 +836,14 @@ class PixelTestPages(object):
         base_name + '_OffscreenCanvasAccelerated2D',
         test_rect=[0, 0, 360, 200],
         revision=12,
-        browser_args=browser_args),
+        browser_args=browser_args + accelerated_args),
 
       PixelTestPage(
         'pixel_offscreenCanvas_2d_commit_worker.html',
         base_name + '_OffscreenCanvasAccelerated2DWorker',
         test_rect=[0, 0, 360, 200],
         revision=12,
-        browser_args=browser_args),
+        browser_args=browser_args + accelerated_args),
 
       PixelTestPage(
         'pixel_offscreenCanvas_2d_commit_main.html',
@@ -890,7 +892,7 @@ class PixelTestPages(object):
         base_name + '_CanvasDisplayLinearRGBAccelerated2D',
         test_rect=[0, 0, 140, 140],
         revision=10,
-        browser_args=browser_args),
+        browser_args=browser_args + accelerated_args),
 
       PixelTestPage(
         'pixel_canvas_display_linear-rgb.html',
@@ -911,7 +913,7 @@ class PixelTestPages(object):
         base_name + '_CanvasDisplaySRGBAccelerated2D',
         test_rect=[0, 0, 140, 140],
         revision=0, # not used, unsupported
-        browser_args=browser_args),
+        browser_args=browser_args + accelerated_args),
 
       PixelTestPage(
         'pixel_canvas_display_srgb.html',
