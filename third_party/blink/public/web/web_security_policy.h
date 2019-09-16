0ba1f9aa85715f8c1db886c526f30a2e5f0a8af1
Dominic Farolino <dom@chromium.org>
2019-09-16 16:03:30 +0000

    Revert "Re-land: Fix `Referer` for descendant module scripts and worklets"
    This reverts commit c99349614d37ba823fdab9532cddd269d56d8436.
    
    Reason for revert: This CL was causing DumpWithoutCrashes that we’re
    currently working to debug. See https://crbug.com/1004083 for more
    details.
    
    Original change's description:
    > Re-land: Fix `Referer` for descendant module scripts and worklets
    >
    > This CL addresses a problem with SecurityPolicy::GenerateReferrer
    > when it comes to checking the same-origin-ness of a request. The
    > WebAppSec Referrer Policy Standard defines a same-origin request [1] as
    > one where the request's origin and current URL are same-origin with
    > each other. This comparison is done in "determine a request's referrer"
    > algorithm.
    >
    > The analogous place in our implementation is
    > SecurityPolicy::GenerateReferrer. Before this CL, GenerateReferrer would
    > determine a request's same-origin-ness by comparing the origin of the
    > request's referrer string and the origin of the request's current URL.
    > Most of the time this was sufficient, as the request's referrer string
    > is almost always same-origin with the request's origin (initiator
    > in Blink). With descendant module scripts and worklets however, the
    > origin of the request's referrer string and request's origin (initiator)
    > could be different, which breaks the correctness of our GenerateReferrer
    > method.
    >
    > This CL introduces a blink::SecurityOrigin parameter to the
    > GenerateReferrer method, so that correct same-origin comparisons can be
    > carried out. In all GenerateReferrer call-sites, an appropriate origin
    > is passed in.
    >
    > The original CL [2] was reverted because the semantics of
    > SecurityPolicy::GenerateReferrer were not kept in sync with the similar
    > logic in net::URLRequestJob::ComputeReferrerForPolicy, which caused a
    > DumpWithoutCrashing bug seen in https://crbug.com/1000614, and request
    > cancellations. This reland updates the ComputeReferrerForPolicy logic
    > to match the corresponding Blink logic, and includes documentation
    > mentioning that changes to one section should be reflected in the other.
    > This CL also includes web platform tests for the scenario in the
    > aforementioned bug, which pass with this CL, as well as net unit tests
    > for RedirectInfo and URLRequestJob.
    >
    > [1]: https://www.w3.org/TR/referrer-policy/#same-origin-request
    >
    > [2]: https://crrev.com/c/1768501
    >
    > Bug: 786862
    > Change-Id: I1deeaae8191b07856c593ddb2486297344e0b846
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786260
    > Commit-Queue: Dominic Farolino <dom@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Benoit L <lizeb@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#695523}
    
    TBR=kinuko@chromium.org,yhirano@chromium.org,caseq@chromium.org,kouhei@chromium.org,mmenke@chromium.org,nhiroki@chromium.org,tbansal@chromium.org,lizeb@chromium.org,dom@chromium.org
    
    Change-Id: I3d812f99e3a2e6cb6b8d56a78580273cf266a22f
    No-Presubmit: true
    No-Tree-Checks: true
    Bug: 786862
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1806618
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696813}

--- third_party/blink/public/web/web_security_policy.h
+++ third_party/blink/public/web/web_security_policy.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/cors_origin_pattern.mojom-shared.h"
 #include "services/network/public/mojom/referrer_policy.mojom-shared.h"
 #include "third_party/blink/public/platform/web_common.h"
-#include "third_party/blink/public/platform/web_security_origin.h"
 
 namespace blink {
 
@@ -110,7 +109,6 @@ class WebSecurityPolicy {
   // referrer header should be omitted.
   BLINK_EXPORT static WebString GenerateReferrerHeader(
       network::mojom::ReferrerPolicy,
-      const WebSecurityOrigin& origin,
       const WebURL&,
       const WebString& referrer);
 
