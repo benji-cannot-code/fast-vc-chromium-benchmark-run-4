b0d4789863c982db86e82bd1ebeec57d99fb1b06
David Black <dmblack@google.com>
2019-09-16 18:46:24 +0000

    Restrict proactive suggestions feature based on sync settings.
    The proactive suggestions feature will not make requests to the backend
    if either:
    - User history sync is disabled, or
    - User has a sync password
    
    Bug: b:141025289
    Change-Id: Iba8548e1f873a63c721a71bf8135b9a744da882d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804066
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#696881}

--- chrome/browser/ui/ash/assistant/proactive_suggestions_client_impl.cc
+++ chrome/browser/ui/ash/assistant/proactive_suggestions_client_impl.cc
@@ -7,9 +7,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/assistant/proactive_suggestions.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/ash/assistant/proactive_suggestions_loader.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "components/sync/driver/sync_service.h"
+#include "components/sync/driver/sync_user_settings.h"
+
+namespace {
+
+// Helpers ---------------------------------------------------------------------
+
+syncer::SyncService* GetSyncService(Profile* profile) {
+  return profile->IsSyncAllowed()
+             ? ProfileSyncServiceFactory::GetForProfile(profile)
+             : nullptr;
+}
+
+bool IsHistorySyncEnabledWithoutPassphrase(Profile* profile) {
+  auto* sync_service = GetSyncService(profile);
+  if (!sync_service)
+    return false;
+
+  const auto* user_settings = sync_service->GetUserSettings();
+  if (!user_settings)
+    return false;
+
+  return user_settings->GetSelectedTypes().Has(
+             syncer::UserSelectableType::kHistory) &&
+         !user_settings->IsUsingSecondaryPassphrase();
+}
+
+}  // namespace
+
+// ProactiveSuggestionsClientImpl ----------------------------------------------
 
 ProactiveSuggestionsClientImpl::ProactiveSuggestionsClientImpl(
     Profile* profile)
@@ -21,6 +52,12 @@ ProactiveSuggestionsClientImpl::ProactiveSuggestionsClientImpl(
   // We observe the singleton BrowserList to receive events pertaining to the
   // currently active browser.
   BrowserList::AddObserver(this);
+
+  // We observe the SyncService to detect changes in user sync settings which
+  // may affect whether or not we will observe the active browser.
+  auto* sync_service = GetSyncService(profile_);
+  if (sync_service)
+    sync_service->AddObserver(this);
 }
 
 ProactiveSuggestionsClientImpl::~ProactiveSuggestionsClientImpl() {
@@ -31,6 +68,10 @@ ProactiveSuggestionsClientImpl::~ProactiveSuggestionsClientImpl() {
 
   if (active_browser_)
     active_browser_->tab_strip_model()->RemoveObserver(this);
+
+  auto* sync_service = GetSyncService(profile_);
+  if (sync_service)
+    sync_service->RemoveObserver(this);
 
   BrowserList::RemoveObserver(this);
   ash::AssistantState::Get()->RemoveObserver(this);
@@ -88,6 +129,12 @@ void ProactiveSuggestionsClientImpl::OnAssistantContextEnabled(bool enabled) {
   // When Assistant screen context is enabled/disabled in settings we may need
   // to resume/pause observation of the browser. We accomplish this by updating
   // active state.
+  UpdateActiveState();
+}
+
+void ProactiveSuggestionsClientImpl::OnStateChanged(syncer::SyncService* sync) {
+  // When the state of the SyncService has changed, we may need to resume/pause
+  // observation of the browser. We accomplish this by updating active state.
   UpdateActiveState();
 }
 
@@ -162,10 +209,13 @@ void ProactiveSuggestionsClientImpl::UpdateActiveState() {
   auto* tab_strip_model = active_browser_->tab_strip_model();
 
   // We never observe browsers that are off the record and we never observe
-  // browsers when the user has disabled either Assistant or screen context.
+  // browsers when the user has disabled either Assistant or screen context. We
+  // also don't observe the browser when the user has disabled history sync or
+  // is using a sync passphrase.
   if (active_browser_->profile()->IsOffTheRecord() ||
       !ash::AssistantState::Get()->settings_enabled().value_or(false) ||
-      !ash::AssistantState::Get()->context_enabled().value_or(false)) {
+      !ash::AssistantState::Get()->context_enabled().value_or(false) ||
+      !IsHistorySyncEnabledWithoutPassphrase(profile_)) {
     tab_strip_model->RemoveObserver(this);
     SetActiveContents(nullptr);
     return;
--- chrome/browser/ui/ash/assistant/proactive_suggestions_client_impl.h
+++ chrome/browser/ui/ash/assistant/proactive_suggestions_client_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "components/sync/driver/sync_service_observer.h"
 #include "content/public/browser/web_contents_observer.h"
 
 class ProactiveSuggestionsLoader;
@@ -26,7 +27,8 @@ class ProactiveSuggestionsClientImpl : public ash::ProactiveSuggestionsClient,
                                        public BrowserListObserver,
                                        public TabStripModelObserver,
                                        public content::WebContentsObserver,
-                                       public ash::AssistantStateObserver {
+                                       public ash::AssistantStateObserver,
+                                       public syncer::SyncServiceObserver {
  public:
   explicit ProactiveSuggestionsClientImpl(Profile* profile);
   ~ProactiveSuggestionsClientImpl() override;
@@ -52,6 +54,9 @@ class ProactiveSuggestionsClientImpl : public ash::ProactiveSuggestionsClient,
   // AssistantStateObserver:
   void OnAssistantSettingsEnabled(bool enabled) override;
   void OnAssistantContextEnabled(bool enabled) override;
+
+  // syncer::SyncServiceObserver:
+  void OnStateChanged(syncer::SyncService* sync) override;
 
  private:
   void SetActiveBrowser(Browser* browser);
