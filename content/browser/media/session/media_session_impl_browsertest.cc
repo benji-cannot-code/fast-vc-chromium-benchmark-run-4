6754669b2eb1429c8479fd0be76a9e724f42e9b3
Xi Han <hanxi@google.com>
2019-09-16 20:19:06 +0000

    Disable MediaSessionImplBrowserTest.PositionStateRouteWithOnePlayer on Android.
    Test is flaky.
    
    TBR=beccahughes@chromium.org
    
    Bug: 1000400
    Change-Id: Icaefdb8956223fa8a13db56560f3d00b7d361747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807569
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#696915}

--- content/browser/media/session/media_session_impl_browsertest.cc
+++ content/browser/media/session/media_session_impl_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/simple_test_tick_clock.h"
+#include "build/build_config.h"
 #include "content/browser/media/session/audio_focus_delegate.h"
 #include "content/browser/media/session/mock_media_session_player_observer.h"
 #include "content/browser/media/session/mock_media_session_service_impl.h"
@@ -2703,7 +2704,7 @@ IN_PROC_BROWSER_TEST_F(MediaSessionImplBrowserTest,
   }
 }
 
-#if defined(OS_CHROMEOS)
+#if defined(OS_CHROMEOS) || defined(OS_ANDROID)
 // TODO(https://crbug.com/1000400): Re-enable this test.
 #define MAYBE_PositionStateRouteWithOnePlayer \
   DISABLED_PositionStateRouteWithOnePlayer
