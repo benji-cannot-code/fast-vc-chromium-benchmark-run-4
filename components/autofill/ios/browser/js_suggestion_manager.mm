12c4ace428d708829c659912a8c89b47558e897d
Mike Dougherty <michaeldo@chromium.org>
2019-09-17 16:25:49 +0000

    [iOS] Ensure classes don’t retain injection receiver.
    CRWJSInjectionReceiver shouldn’t be retained, except by CRWWebController. Update
    other references to weak.
    
    Bug: 1004321
    Change-Id: I0c936b476fb65c8d1d2e0f0be6f5179257897aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807606
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697248}

--- components/autofill/ios/browser/js_suggestion_manager.mm
+++ components/autofill/ios/browser/js_suggestion_manager.mm
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation JsSuggestionManager {
   // The injection receiver used to evaluate JavaScript.
-  CRWJSInjectionReceiver* _receiver;
+  __weak CRWJSInjectionReceiver* _receiver;
   web::WebFramesManager* _webFramesManager;
 }
 
