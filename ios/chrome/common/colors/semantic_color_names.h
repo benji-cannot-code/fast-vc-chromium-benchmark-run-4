9bcbcd156218facdbbb7a71d4117f9d2a4c0bf21
Nazerke <Nazerke@google.com>
2019-09-17 18:10:21 +0000

    [iOS] tab grid colors
    New colors, which are only used in tab grid, should be local colors
    instead of global.
    
    This CL is moving |kGridThemeSelectionTintColor| and
    |kGridThemeDarkSelectionTintColor| to
    ios/chrome/browser/ui/tab_grid/grid/resources/ from the common
    semantic colors folder.
    
    Bug: 995746
    Change-Id: Ia7afebfc21add3466e7e8ad8e94ba9c49659fa8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807193
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Cr-Commit-Position: refs/heads/master@{#697292}

--- ios/chrome/common/colors/semantic_color_names.h
+++ ios/chrome/common/colors/semantic_color_names.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern NSString* const kBackgroundColor;
 extern NSString* const kCloseButtonColor;
 extern NSString* const kDisabledTintColor;
-extern NSString* const kGridThemeSelectionTintColor;
 // Background color used in the rounded squares behind favicons.
 extern NSString* const kFaviconBackgroundColor;
 // Ink color for an MDC button.
@@ -70,7 +69,6 @@ extern NSString* const kGrey900Color;
 
 extern NSString* const kBackgroundDarkColor;
 extern NSString* const kCloseButtonDarkColor;
-extern NSString* const kGridThemeDarkSelectionTintColor;
 extern NSString* const kTableViewRowHighlightDarkColor;
 extern NSString* const kTextPrimaryDarkColor;
 extern NSString* const kTextSecondaryDarkColor;
--- ios/chrome/common/colors/semantic_color_names.mm
+++ ios/chrome/common/colors/semantic_color_names.mm
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 NSString* const kBackgroundColor = @"background_color";
 NSString* const kCloseButtonColor = @"close_button_color";
 NSString* const kDisabledTintColor = @"disabled_tint_color";
-NSString* const kGridThemeSelectionTintColor =
-    @"grid_theme_selection_tint_color";
 NSString* const kFaviconBackgroundColor = @"favicon_background_color";
 NSString* const kMDCInkColor = @"mdc_ink_color";
 NSString* const kMDCSecondaryInkColor = @"mdc_secondary_ink_color";
@@ -52,8 +50,6 @@ NSString* const kGrey900Color = @"grey_900_color";
 
 NSString* const kBackgroundDarkColor = @"background_dark_color";
 NSString* const kCloseButtonDarkColor = @"close_button_dark_color";
-NSString* const kGridThemeDarkSelectionTintColor =
-    @"grid_theme_dark_selection_tint_color";
 NSString* const kTableViewRowHighlightDarkColor =
     @"table_view_row_highlight_dark_color";
 NSString* const kTextPrimaryDarkColor = @"text_primary_dark_color";
