d61499d11121d7e847978cd1a3d60b221751372f
Yue Li <updowndota@chromium.org>
2019-09-17 00:33:01 +0000

    Update VoiceInteraction related legacy code names
    Update legacy code names and comments to use 'Assistant'.
    Remove obsolete logics.
    
    Bug: b/140823590
    Test: Run existing tests
    Change-Id: Ie5647e85b0030c2cddfa1dfeab86c752ab6148f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808091
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697020}

--- ash/system/palette/tools/metalayer_mode.h
+++ ash/system/palette/tools/metalayer_mode.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 // A palette tool that lets the user select a screen region to be passed
-// to the voice interaction framework.
+// to the Assistant framework.
 //
 // Unlike other palette tools, it can be activated not only through the stylus
 // menu, but also by the stylus button click.
