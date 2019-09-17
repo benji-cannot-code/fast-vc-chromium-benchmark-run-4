6207dbb806d6901ca99273c14865ce0d20921ef6
Gyuyoung Kim <gyuyoung@igalia.com>
2019-09-17 08:25:09 +0000

    Include *.mojom-forward.h instead of .mojom.h from logging_utils.h
    This CL replaces .mojom.h with .mojom-forward.h to reduce pre-processed
    size of logging_utils.h
    
    This CL has no behavior changes.
    
    Bug: 1001360
    Change-Id: Idd305572c67e46a5f7a01937ec1d2e31fc62d8ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808619
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#697134}

--- third_party/blink/common/logging/logging_utils.cc
+++ third_party/blink/common/logging/logging_utils.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/logging/logging_utils.h"
 
+#include "third_party/blink/public/mojom/devtools/console_message.mojom-blink.h"
+
 namespace blink {
 
 logging::LogSeverity ConsoleMessageLevelToLogSeverity(
