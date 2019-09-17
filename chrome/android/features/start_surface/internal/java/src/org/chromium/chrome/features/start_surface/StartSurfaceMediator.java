73ca766fbe14f2b2776e93615c974367a2139022
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-09-17 02:13:34 +0000

    Do not show content overlay until GTS-to-Tab animation is done
    Bug: 998949
    Change-Id: I29fbedca05f986d33f486684c1db8909bcd6b14a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1805296
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697053}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
@@ -32,15 +32,6 @@ import org.chromium.ui.modelutil.PropertyModel;
 /** The mediator implements the logic to interact with the surfaces and caller. */
 class StartSurfaceMediator
         implements StartSurface.Controller, TabSwitcher.OverviewModeObserver, View.OnClickListener {
-    /** Interface to control overlay visibility. */
-    interface OverlayVisibilityHandler {
-        /**
-         * Set the content overlay visibility.
-         * @param isVisible Whether the content overlay should be visible.
-         */
-        void setContentOverlayVisibility(boolean isVisible);
-    }
-
     /** Interface to initialize a secondary tasks surface for more tabs. */
     interface SecondaryTasksSurfaceInitializer {
         /**
@@ -53,7 +44,6 @@ class StartSurfaceMediator
 
     private final ObserverList<StartSurface.OverviewModeObserver> mObservers = new ObserverList<>();
     private final TabSwitcher.Controller mController;
-    private final OverlayVisibilityHandler mOverlayVisibilityHandler;
     @Nullable
     private final PropertyModel mPropertyModel;
     @Nullable
@@ -66,13 +56,11 @@ class StartSurfaceMediator
     private boolean mIsIncognito;
 
     StartSurfaceMediator(TabSwitcher.Controller controller, TabModelSelector tabModelSelector,
-            OverlayVisibilityHandler overlayVisibilityHandler,
             @Nullable PropertyModel propertyModel,
             @Nullable ExploreSurfaceCoordinator.FeedSurfaceCreator feedSurfaceCreator,
             @Nullable SecondaryTasksSurfaceInitializer secondaryTasksSurfaceInitializer,
             @StartSurfaceCoordinator.SurfaceMode int mode) {
         mController = controller;
-        mOverlayVisibilityHandler = overlayVisibilityHandler;
         mPropertyModel = propertyModel;
         mFeedSurfaceCreator = feedSurfaceCreator;
         mSecondaryTasksSurfaceInitializer = secondaryTasksSurfaceInitializer;
@@ -234,12 +222,10 @@ class StartSurfaceMediator
         for (StartSurface.OverviewModeObserver observer : mObservers) {
             observer.finishedShowing();
         }
-        mOverlayVisibilityHandler.setContentOverlayVisibility(false);
     }
 
     @Override
     public void startedHiding() {
-        mOverlayVisibilityHandler.setContentOverlayVisibility(true);
         if (mPropertyModel != null) {
             mPropertyModel.set(IS_SHOWING_OVERVIEW, false);
             destroyFeedSurfaceCoordinator();
