a69eee5fdc4e11844cc17a9178515a7b8a9c45cc
Miyoung Shin <myid.shin@igalia.com>
2019-09-17 01:32:55 +0000

    Use base::NullCallback in TrayBluetoothHelperExperimental
    This is a followup CL of https://crrev.com/c/1787663 and uses
    base::NullCallback instead of base::DoNothing for mojo interface
    in TrayBluetoothHelperExperimental class.
    
    Bug: 955171
    Change-Id: I66fba9b9ffb8db2163111930254a9a87eaa19667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1806157
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#697043}

--- ash/system/bluetooth/tray_bluetooth_helper_experimental.cc
+++ ash/system/bluetooth/tray_bluetooth_helper_experimental.cc
@@ -51,11 +51,11 @@ void TrayBluetoothHelperExperimental::Initialize() {
 }
 
 void TrayBluetoothHelperExperimental::StartBluetoothDiscovering() {
-  bluetooth_system_->StartScan(base::DoNothing());
+  bluetooth_system_->StartScan(base::NullCallback());
 }
 
 void TrayBluetoothHelperExperimental::StopBluetoothDiscovering() {
-  bluetooth_system_->StopScan(base::DoNothing());
+  bluetooth_system_->StopScan(base::NullCallback());
 }
 
 void TrayBluetoothHelperExperimental::ConnectToBluetoothDevice(
@@ -69,7 +69,7 @@ TrayBluetoothHelperExperimental::GetBluetoothState() {
 }
 
 void TrayBluetoothHelperExperimental::SetBluetoothEnabled(bool enabled) {
-  bluetooth_system_->SetPowered(enabled, base::DoNothing());
+  bluetooth_system_->SetPowered(enabled, base::NullCallback());
 }
 
 bool TrayBluetoothHelperExperimental::HasBluetoothDiscoverySession() {
