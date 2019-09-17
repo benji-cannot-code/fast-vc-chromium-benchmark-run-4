d8a6fc7ad4bfe1ccdb02f8863f07be424189ebd3
Mathias Bynens <mathias@chromium.org>
2019-09-17 13:32:10 +0000

    [devtools] Fix property order mismatch
    Previously, there was a property order mismatch between the DevTools
    Console Preview and the expanded view. For example:
    
           input> ({ c: 1, b: 1, a: 1 })
         preview> {c: 1, b: 1, a: 1}
        expanded> {a: 1, b: 1, c: 1, __proto__: Object}
    
    This patch removes the confusing mismatch as follows:
    
           input> ({ c: 1, b: 1, a: 1 })
         preview> {c: 1, b: 1, a: 1}
        expanded> {c: 1, b: 1, a: 1, __proto__: Object}
    
    The core change happens in ObjectPropertiesSection.js. The test
    expectations are updated accordingly (note that for each test, only
    the order in which properties/values are printed changes). The patch
    also includes a drive-by change across files unifying the way U+00A0
    is escaped in string literals.
    
    Screenshot: https://goo.gle/devtools-property-order-mismatch
    
    BUG=989514
    
    Change-Id: I102afcbd107100a52e0932401429a3fa8db99eaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1806457
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697200}

--- third_party/blink/renderer/devtools/front_end/object_ui/ObjectPropertiesSection.js
+++ third_party/blink/renderer/devtools/front_end/object_ui/ObjectPropertiesSection.js
@@ -582,7 +582,6 @@ ObjectUI.ObjectPropertyTreeElement = class extends UI.TreeElement {
       value,
       linkifier,
       emptyPlaceholder) {
-    properties.sort(ObjectUI.ObjectPropertiesSection.CompareProperties);
     internalProperties = internalProperties || [];
 
     const entriesProperty = internalProperties.find(property => property.name === '[[Entries]]');
--- third_party/blink/renderer/devtools/front_end/platform/utilities.js
+++ third_party/blink/renderer/devtools/front_end/platform/utilities.js
@@ -1022,7 +1022,7 @@ self.countRegexMatches = function(regex, content) {
  * @return {string}
  */
 self.spacesPadding = function(spacesCount) {
-  return '\u00a0'.repeat(spacesCount);
+  return '\xA0'.repeat(spacesCount);
 };
 
 /**
