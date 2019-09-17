c686f274a13a1af6b8ee08f84070b29922fd94f0
Nazerke <Nazerke@google.com>
2019-09-17 14:50:20 +0000

    [iOS][dark mode] Link color in the signin
    Link for settings in the 'turn on sync' page is wrong blue. This CL
    sets the kBlueColor dynamic color as a link color, according to the dark
    mode theme.
    
    Bug: 1004685
    Change-Id: I6be3e299055dda213a8854cf03f6a6d119d59acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809297
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Cr-Commit-Position: refs/heads/master@{#697214}

--- ios/chrome/browser/ui/authentication/unified_consent/unified_consent_view_controller.mm
+++ ios/chrome/browser/ui/authentication/unified_consent/unified_consent_view_controller.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/util/label_link_controller.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/colors/UIColor+cr_semantic_colors.h"
+#import "ios/chrome/common/colors/semantic_color_names.h"
 #include "ios/chrome/common/string_util.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
 #include "ios/chrome/grit/ios_chromium_strings.h"
@@ -393,8 +394,7 @@ const char* const kSettingsSyncURL = "internal://settings-sync";
                                             action:^(const GURL& URL) {
                                               [weakSelf openSettings];
                                             }];
-    [self.settingsLinkController
-        setLinkColor:[[MDCPalette cr_bluePalette] tint500]];
+    [self.settingsLinkController setLinkColor:[UIColor colorNamed:kBlueColor]];
     [self.settingsLinkController
         addLinkWithRange:range
                      url:URL
