e610aa4f9c52dd5a13b61c610b9ae34dc56b9929
Eric Stevenson <estevenson@chromium.org>
2019-09-17 18:07:38 +0000

    JNI refactor: @NativeMethods conversion (//chrome/android/feed).
    This CL was generated by
    //base/android/jni_generator/jni_refactorer.py.
    
    Bug: 929661
    Change-Id: I8da45e352eb54662a5debedbfb394fd7687ac1f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808552
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697290}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedLifecycleBridge.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedLifecycleBridge.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.feed;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
 import org.chromium.chrome.browser.profiles.Profile;
 
 /**
@@ -24,7 +25,7 @@ public class FeedLifecycleBridge {
      * @param profile Profile of the user whose lifecycle events we care about.
      */
     public FeedLifecycleBridge(Profile profile) {
-        mNativeBridge = nativeInit(profile);
+        mNativeBridge = FeedLifecycleBridgeJni.get().init(FeedLifecycleBridge.this, profile);
     }
 
     /*
@@ -32,7 +33,7 @@ public class FeedLifecycleBridge {
      */
     public void destroy() {
         assert mNativeBridge != 0;
-        nativeDestroy(mNativeBridge);
+        FeedLifecycleBridgeJni.get().destroy(mNativeBridge, FeedLifecycleBridge.this);
         mNativeBridge = 0;
     }
 
@@ -54,6 +55,9 @@ public class FeedLifecycleBridge {
         }
     }
 
-    private native long nativeInit(Profile profile);
-    private native void nativeDestroy(long nativeFeedLifecycleBridge);
+    @NativeMethods
+    interface Natives {
+        long init(FeedLifecycleBridge caller, Profile profile);
+        void destroy(long nativeFeedLifecycleBridge, FeedLifecycleBridge caller);
+    }
 }
