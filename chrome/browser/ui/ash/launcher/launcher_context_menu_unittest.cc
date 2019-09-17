7213db19dc4418b1775b7833536d08b829ca469e
nancy <nancylingwang@chromium.org>
2019-09-17 00:27:03 +0000

    Create AppServiceTest class to integrate AppService to UI unit tests.
    This CL is the follow up for crrev.com/c/1802776 based on review
    comments, to create AppServiceTest, and integrate AppService to
    LauncherContextMenuTest only. There will be one or a few separate
    CL(s) to integrate AppService to other UI unit tests.
    
    BUG=1002351
    
    Change-Id: I60e7fb4e7b48b2431ac0a3e3398e6ae1ffe8ace5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804095
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697016}

--- chrome/browser/ui/ash/launcher/launcher_context_menu_unittest.cc
+++ chrome/browser/ui/ash/launcher/launcher_context_menu_unittest.cc
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/app_list/app_service/app_service_test.h"
+#include "chrome/browser/ui/app_list/arc/arc_app_icon.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_test.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ui/app_list/internal_app/internal_app_metadata.h"
@@ -74,6 +76,7 @@ class LauncherContextMenuTest : public ChromeAshTestBase {
 
   void SetUp() override {
     arc_test_.SetUp(&profile_);
+    app_service_test_.SetUp(&profile_);
     session_manager_ = std::make_unique<session_manager::SessionManager>();
     ChromeAshTestBase::SetUp();
     model_ = std::make_unique<ash::ShelfModel>();
@@ -82,6 +85,7 @@ class LauncherContextMenuTest : public ChromeAshTestBase {
 
     // Disable safe icon decoding to ensure ArcAppShortcutRequests returns in
     // the test environment.
+    ArcAppIcon::DisableSafeDecodingForTesting();
     arc::IconDecodeRequest::DisableSafeDecodingForTesting();
   }
 
@@ -141,6 +145,8 @@ class LauncherContextMenuTest : public ChromeAshTestBase {
 
   ArcAppTest& arc_test() { return arc_test_; }
 
+  AppServiceTest& app_service_test() { return app_service_test_; }
+
   TestingProfile* profile() { return &profile_; }
 
   ChromeLauncherController* controller() { return launcher_controller_.get(); }
@@ -150,6 +156,7 @@ class LauncherContextMenuTest : public ChromeAshTestBase {
  private:
   TestingProfile profile_;
   ArcAppTest arc_test_;
+  AppServiceTest app_service_test_;
   std::unique_ptr<session_manager::SessionManager> session_manager_;
   std::unique_ptr<ash::ShelfModel> model_;
   std::unique_ptr<ChromeLauncherController> launcher_controller_;
