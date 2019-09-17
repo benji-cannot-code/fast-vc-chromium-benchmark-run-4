d6c01bc727b043c8590e4784825a425e5316b9b7
Sammie Quon <sammiequon@google.com>
2019-09-17 21:52:49 +0000

    overview: Improvement to screen rotating smoothness.
    1) Remove extra PositionWindows call when rotated since
    SetBoundsAndUpdatePositions calls it already.
    2) Allow SetBoundsAndUpdatePositions to not animate and use no animation
    when rotation.
    3) Pause the occlusion tracker when rotating.
    
    Ash.Rotation.Smoothness has around 70~80 before, 80~90 after.
    
    always using 8 windows and NTP page
    
    Test: interactive_ui_tests ScreenRotationTest.RotateInTabletOverview,
    Bug: 1004820
    Change-Id: Ie7bb137185d34ceb1b0c0e42f7aba021a673ecee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809502
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697376}

--- ash/wm/overview/overview_session.cc
+++ ash/wm/overview/overview_session.cc
@@ -978,7 +978,7 @@ void OverviewSession::OnSplitViewDividerPositionChanged() {
     grid->SetBoundsAndUpdatePositions(
         GetGridBoundsInScreen(const_cast<aura::Window*>(grid->root_window()),
                               /*divider_changed=*/true),
-        /*ignored_items=*/{});
+        /*ignored_items=*/{}, /*animate=*/true);
   }
   PositionWindows(/*animate=*/false);
   UpdateNoWindowsWidget();
@@ -1064,9 +1064,8 @@ void OverviewSession::OnDisplayBoundsChanged() {
     grid->SetBoundsAndUpdatePositions(
         GetGridBoundsInScreen(const_cast<aura::Window*>(grid->root_window()),
                               /*divider_changed=*/false),
-        /*ignored_items=*/{});
+        /*ignored_items=*/{}, /*animate=*/false);
   }
-  PositionWindows(/*animate=*/false);
   UpdateNoWindowsWidget();
   if (split_view_drag_indicators_)
     split_view_drag_indicators_->OnDisplayBoundsChanged();
