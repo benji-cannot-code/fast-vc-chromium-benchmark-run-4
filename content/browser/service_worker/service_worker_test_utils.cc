5cf31f885f1c7028b581f699ec0d9c3519874e21
Wei Wang <wei4.wang@intel.com>
2019-09-17 07:55:43 +0000

    Have StartServiceWorker, StopServiceWorker as helper test utils
    This patch makes StartServiceWorker and StopServiceWorker the helper test
    utils which can be used by all tests.
    
    In this patch, we have already used the StartServiceWorker and
    StopServiceWorker in ServiceWorkerVersionTest, ServiceWorkerJobTest and
    ServiceWorkerRegistrationTest. In the next step, other tests will be changed
    as well.
    
    Bug: 931095
    Change-Id: I91d70239b6d76498d01c2adba684fde3e7bff121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788251
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697127}

--- content/browser/service_worker/service_worker_test_utils.cc
+++ content/browser/service_worker/service_worker_test_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/barrier_closure.h"
 #include "base/run_loop.h"
+#include "base/test/bind_test_util.h"
 #include "base/time/time.h"
 #include "content/browser/frame_host/frame_tree_node.h"
 #include "content/browser/service_worker/embedded_worker_test_helper.h"
@@ -294,6 +295,26 @@ ReceiveServiceWorkerStatus(base::Optional<blink::ServiceWorkerStatusCode>* out,
         std::move(quit_closure).Run();
       },
       std::move(quit_closure), out);
+}
+
+blink::ServiceWorkerStatusCode StartServiceWorker(
+    ServiceWorkerVersion* version) {
+  blink::ServiceWorkerStatusCode status;
+  base::RunLoop run_loop;
+  version->StartWorker(ServiceWorkerMetrics::EventType::UNKNOWN,
+                       base::BindLambdaForTesting(
+                           [&](blink::ServiceWorkerStatusCode result_status) {
+                             status = result_status;
+                             run_loop.Quit();
+                           }));
+  run_loop.Run();
+  return status;
+}
+
+void StopServiceWorker(ServiceWorkerVersion* version) {
+  base::RunLoop run_loop;
+  version->StopWorker(run_loop.QuitClosure());
+  run_loop.Run();
 }
 
 base::WeakPtr<ServiceWorkerProviderHost>
