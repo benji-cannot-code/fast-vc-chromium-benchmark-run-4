5c24578c26c31d1dda4d39d0956010ac1a780eef
rbpotter <rbpotter@chromium.org>
2019-09-17 01:49:20 +0000

    Web UI Polymer 3: Revert anchor alignment split
    Since the tools/polymer/polymer.py script has been updated to handle
    export annotations, revert
    https://chromium-review.googlesource.com/c/chromium/src/+/1795315,
    which split of the AnchorAlignment enum into a separate file, since
    it is no longer necessary.
    
    Bug: 965770
    Change-Id: I7109485280d8d22befe4cdccca3a7e5cf54629bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804957
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697046}

--- chrome/test/data/webui/cr_elements/cr_action_menu_test.js
+++ chrome/test/data/webui/cr_elements/cr_action_menu_test.js
@@ -4,10 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // clang-format off
-// #import 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.m.js';
+// #import {AnchorAlignment} from 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.m.js';
 // #import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
 //
-// #import {AnchorAlignment} from 'chrome://resources/cr_elements/cr_action_menu/anchor_alignment.m.js';
 // #import {eventToPromise, flushTasks} from 'chrome://test/test_util.m.js';
 // #import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
 // #import {keyDownOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
