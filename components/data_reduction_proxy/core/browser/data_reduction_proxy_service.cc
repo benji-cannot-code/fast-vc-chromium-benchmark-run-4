e06adf7cdc73b9cf464933274ee518afdf682ee7
Robert Ogden <robertogden@chromium.org>
2019-09-17 18:48:56 +0000

    Remove DRP Pingbacks
    Removes all the pingback code. There's still some stale code left but
    that's all related to v1 HTTPS Server Previews which will be removed
    soon.
    
    Bug: 957753
    Change-Id: I4d5cccd5e402a786c49b8ea1c5b1d95a396f8850
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807962
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697311}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_configurator.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_mutable_config_values.h"
-#include "components/data_reduction_proxy/core/browser/data_reduction_proxy_pingback_client.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_request_options.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_util.h"
@@ -45,7 +44,6 @@ DataReductionProxyService::DataReductionProxyService(
     PrefService* prefs,
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
     std::unique_ptr<DataStore> store,
-    std::unique_ptr<DataReductionProxyPingbackClient> pingback_client,
     network::NetworkQualityTracker* network_quality_tracker,
     network::NetworkConnectionTracker* network_connection_tracker,
     data_use_measurement::DataUseMeasurement* data_use_measurement,
@@ -55,7 +53,6 @@ DataReductionProxyService::DataReductionProxyService(
     const std::string& channel,
     const std::string& user_agent)
     : url_loader_factory_(std::move(url_loader_factory)),
-      pingback_client_(std::move(pingback_client)),
       settings_(settings),
       prefs_(prefs),
       db_data_owner_(new DBDataOwner(std::move(store))),
@@ -270,12 +267,6 @@ void DataReductionProxyService::SetProxyPrefs(bool enabled, bool at_startup) {
     for (auto& client : proxy_config_clients_)
       client->ClearBadProxiesCache();
   }
-}
-
-void DataReductionProxyService::SetPingbackReportingFraction(
-    float pingback_reporting_fraction) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  pingback_client_->SetPingbackReportingFraction(pingback_reporting_fraction);
 }
 
 void DataReductionProxyService::OnCacheCleared(const base::Time start,
