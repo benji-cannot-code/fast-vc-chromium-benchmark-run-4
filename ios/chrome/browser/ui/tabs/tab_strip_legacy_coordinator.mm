64b9aee79f8addd4e825a1714f50a7c0a624384d
Mark Cogan <marq@google.com>
2019-09-17 16:07:05 +0000

    [iOS] Add (and use) an explicit -disconnect for TabStripController.
    TabStripController relies on -dalloc to stop its various WebStateList
    observations. If the WebStateList is destroyed before the dealloc runs,
    the non-empty observer DCHECKs are hit. The upcoming change to
    WebStateList ownership (see crrev.com/c/1796358) will cause this to
    happen, for example.
    
    To fix this, an explicit -disconnect method is added to
    TabStripController, and the tab strip coordinator calls it from -stop.
    
    Change-Id: I22c2fc967c0b42c544ab93dce17518e93d272f43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809298
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697241}

--- ios/chrome/browser/ui/tabs/tab_strip_legacy_coordinator.mm
+++ ios/chrome/browser/ui/tabs/tab_strip_legacy_coordinator.mm
@@ -89,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)stop {
   self.started = NO;
+  [self.tabStripController disconnect];
   self.tabStripController = nil;
   self.dispatcher = nil;
   self.tabModel = nil;
