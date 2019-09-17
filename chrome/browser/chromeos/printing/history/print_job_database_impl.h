a2949a7478ddd67209143de93f7ca43b6232c8bc
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-09-17 12:13:03 +0000

    Printing API: Add initialization status for PrintJobDatabase
    This will allow to resolve possible race conditions when the client
    tries to access database before it's fully initialized.
    
    Bug: 992889
    Change-Id: Ie890e1c83add83f7be0892ad8a4a990730a38a90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1798603
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Cr-Commit-Position: refs/heads/master@{#697178}

--- chrome/browser/chromeos/printing/history/print_job_database_impl.h
+++ chrome/browser/chromeos/printing/history/print_job_database_impl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_PRINTING_HISTORY_PRINT_JOB_DATABASE_IMPL_H_
 #define CHROME_BROWSER_CHROMEOS_PRINTING_HISTORY_PRINT_JOB_DATABASE_IMPL_H_
 
+#include <queue>
 #include <unordered_map>
 
 #include "base/macros.h"
@@ -41,6 +42,8 @@ class PrintJobDatabaseImpl : public PrintJobDatabase {
  private:
   friend class PrintJobDatabaseImplTest;
 
+  enum class InitStatus { UNINITIALIZED, PENDING, INITIALIZED, FAILED };
+
   void OnInitialized(InitializeCallback callback,
                      leveldb_proto::Enums::InitStatus status);
 
@@ -74,12 +77,14 @@ class PrintJobDatabaseImpl : public PrintJobDatabase {
   // Cached PrintJobInfo entries.
   std::unordered_map<std::string, printing::proto::PrintJobInfo> cache_;
 
-  // Whether or not the ProtoDatabase database has been initialized and entries
-  // have been loaded.
-  bool is_initialized_ = false;
+  // Indicates the status of database initialization.
+  InitStatus init_status_;
 
   // Number of initialize attempts.
   int initialize_attempts_ = 0;
+
+  // Stores callbacks for delayed execution once database is initialized.
+  std::queue<base::OnceClosure> deferred_callbacks_;
 
   base::WeakPtrFactory<PrintJobDatabaseImpl> weak_ptr_factory_{this};
 
