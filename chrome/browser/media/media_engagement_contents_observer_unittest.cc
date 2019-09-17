75fc1d10d9fddaa9efb4edacaa3ff9b2e6b37546
Fergus Dall <sidereal@google.com>
2019-09-17 02:06:48 +0000

    Sheriff: Disable MediaEngagementContentsObserverTest on Android
    TBR=tommi@chromium.org
    TBR=sergeyu@chromium.org
    TBR=sandersd@chromium.org
    TBR=xhwang@chromium.org
    TBR=liberato@chromium.org
    TBR=dalecurtis@chromium.org
    TBR=chcunningham@chromium.org
    TBR=wolenetz@chromium.org
    TBR=jrummell@chromium.org
    TBR=tguilbert@chromium.org
    
    Bug: 1004580
    Change-Id: I82e9610eda564e0197e5e4f546108f4ac7fd5f23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808292
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/master@{#697051}

--- chrome/browser/media/media_engagement_contents_observer_unittest.cc
+++ chrome/browser/media/media_engagement_contents_observer_unittest.cc
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_source.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+// TODO(crbug/1004580) All these tests crash on Android
+#if !defined(OS_ANDROID)
 class MediaEngagementContentsObserverTest
     : public ChromeRenderViewHostTestHarness {
  public:
@@ -1392,3 +1394,5 @@ TEST_F(MediaEngagementContentsObserverTest, IgnoreAudioContextIfDisabled) {
   SimulateAudioContextPlaybackTimerFired();
   EXPECT_FALSE(WasSignificantAudioContextPlaybackRecorded());
 }
+
+#endif  // !defined(OS_ANDROID)
