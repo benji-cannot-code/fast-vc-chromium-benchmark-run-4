ecb27dea848d326055e51cc5f4a0e084e2aafde3
Oleg Davydov <burunduk@chromium.org>
2019-09-17 15:21:46 +0000

    Move extensions-related stuff from PolicyTest
    Methods and initializations which make sense only for extensions tests
    moved from generic PolicyTest (int policy_test_util.h) to special
    ExtensionPolicyTest in extension_policy_browsertest.cc.
    
    Note that some tests in policy_browsertest.cc use extensions but don't
    test extension-related policies, so PolicyTest::LoadUpackedExtension
    remained in PolicyTest.
    
    Bug: 1002483
    Change-Id: Ia5a0114bb1c2b8b1240cf41ad8e2c977418efe8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807219
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697224}

--- chrome/browser/policy/policy_test_utils.h
+++ chrome/browser/policy/policy_test_utils.h
@@ -8,17 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/keyboard/keyboard_types.h"
 #include "base/files/file_path.h"
-#include "chrome/browser/extensions/updater/extension_cache_fake.h"
-#include "chrome/browser/extensions/updater/extension_updater.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/security_interstitials/core/controller_client.h"
-#include "extensions/browser/scoped_ignore_content_verifier_for_test.h"
 
 namespace extensions {
 class Extension;
-class ExtensionRegistry;
-class ExtensionService;
 }  // namespace extensions
 
 namespace policy {
@@ -46,21 +41,8 @@ class PolicyTest : public InProcessBrowserTest {
 
   void SetShouldRequireCTForTesting(bool* required);
 
-  extensions::ExtensionService* extension_service();
-
-  extensions::ExtensionRegistry* extension_registry();
-
-  const extensions::Extension* InstallExtension(
-      const base::FilePath::StringType& name);
-
-  const extensions::Extension* InstallBookmarkApp();
-
   scoped_refptr<const extensions::Extension> LoadUnpackedExtension(
       const base::FilePath::StringType& name);
-
-  void UninstallExtension(const std::string& id, bool expect_success);
-
-  void DisableExtension(const std::string& id);
 
   void UpdateProviderPolicy(const PolicyMap& policy);
 
@@ -114,10 +96,6 @@ class PolicyTest : public InProcessBrowserTest {
       security_interstitials::SecurityInterstitialCommand command);
 
   MockConfigurationPolicyProvider provider_;
-  std::unique_ptr<extensions::ExtensionCacheFake> test_extension_cache_;
-  extensions::ScopedIgnoreContentVerifierForTest ignore_content_verifier_;
-  extensions::ExtensionUpdater::ScopedSkipScheduledCheckForTest
-      skip_scheduled_extension_checks_;
 };
 
 }  // namespace policy
