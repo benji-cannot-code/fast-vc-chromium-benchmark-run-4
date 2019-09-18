f2087c837ac9d6585afacb4411f36b3584cb8896
Tarun Bansal <tbansal@chromium.org>
2019-09-18 18:33:02 +0000

    Add experiment to preconnect to predicted redirect target
    Add experiment to preconnect to predicted redirect target.
    The redirect target is returned as part of loading prediction
    by the loading predictor.
    
    Change-Id: I9e00363db609470d92dd5f2196aad71868dc68ed
    Bug: 983234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1699738
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697698}

--- chrome/browser/predictors/predictors_features.h
+++ chrome/browser/predictors/predictors_features.h
@@ -15,6 +15,8 @@ namespace features {
 
 extern const base::Feature kLoadingOnlyLearnHighPriorityResources;
 
+extern const base::Feature kLoadingPreconnectToRedirectTarget;
+
 }  // namespace features
 
 #endif  // CHROME_BROWSER_PREDICTORS_PREDICTORS_FEATURES_H_
