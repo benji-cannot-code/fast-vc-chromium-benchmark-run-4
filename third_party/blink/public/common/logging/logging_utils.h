24feaf6b6ee056ab6d2d605634d4e64fd4088134
Fergus Dall <sidereal@google.com>
2019-09-18 04:57:04 +0000

    Revert "Reland "Include *.mojom-forward.h instead of .mojom.h from logging_utils.h""
    This reverts commit 8d32b63824f7fc5c440077a55cd30eead9b00243.
    
    Reason for revert: Broke the build:
    [56563/84904] CXX obj/third_party/blink/common/common/logging_utils.obj
    FAILED: obj/third_party/blink/common/common/logging_utils.obj
    C:\b\s\w\ir\cache\goma\client\gomacc.exe ..\..\third_party\llvm-build\Release+Asserts\bin\clang-cl.e...(too long)
    ../../third_party/blink/common/logging/logging_utils.cc(7,10): fatal error: 'third_party/blink/public/mojom/devtools/console_message.mojom-blink.h' file not found
    #include "third_party/blink/public/mojom/devtools/console_message.mojom-blink.h"
    ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    1 error generated.
    
    Original change's description:
    > Reland "Include *.mojom-forward.h instead of .mojom.h from logging_utils.h"
    > 
    > This is a reland of be56a0b92d387cd8ac8476bb01f34536dcbf4000
    > 
    > Fix the build break caused by the missing dependency.
    > 
    > Original change's description:
    > > Include *.mojom-forward.h instead of .mojom.h from logging_utils.h
    > >
    > > This CL replaces .mojom.h with .mojom-forward.h to reduce pre-processed
    > > size of logging_utils.h
    > >
    > > This CL has no behavior changes.
    > >
    > > Bug: 1001360
    > > Change-Id: Idd305572c67e46a5f7a01937ec1d2e31fc62d8ea
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808619
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > > Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    > > Cr-Commit-Position: refs/heads/master@{#697134}
    > 
    > Bug: 1001360
    > Change-Id: I5a8112a21782863c6420eddd2fe717d368419b90
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808953
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#697475}
    
    TBR=haraken@chromium.org,tkent@chromium.org,tikuta@chromium.org,gyuyoung@igalia.com
    
    Change-Id: Ie875b1e2e62833b5042444e4f3a1af3400467217
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1001360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810169
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/master@{#697484}

--- third_party/blink/public/common/logging/logging_utils.h
+++ third_party/blink/public/common/logging/logging_utils.h
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_PUBLIC_COMMON_LOGGING_LOGGING_UTILS_H_
 
 #include "base/logging.h"
-#include "third_party/blink/public/common/common_export.h"
-#include "third_party/blink/public/mojom/devtools/console_message.mojom-forward.h"
+#include "third_party/blink/public/mojom/devtools/console_message.mojom.h"
 
 namespace blink {
 
