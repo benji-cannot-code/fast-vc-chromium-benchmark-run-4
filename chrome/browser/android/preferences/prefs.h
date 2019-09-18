8586898b5ca148cfc7fc874fdbcdb0b9a49d16d7
Andrei Chulkov <achulkov@google.com>
2019-09-18 10:40:23 +0000

    Add integration tests for the password manager onboarding flow
    This CL add tests to ensure that the onboarding UI is prompted when the
    user submits a password form and that the subsequent flow is functioning
    correctly.
    
    Bug: 983445
    Change-Id: Ic2489724b74bf90e0120de3fff1266e1aaa071ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809305
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Andrei Chulkov <achulkov@google.com>
    Cr-Commit-Position: refs/heads/master@{#697550}

--- chrome/browser/android/preferences/prefs.h
+++ chrome/browser/android/preferences/prefs.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(ENABLE_FEED_IN_CHROME)
 #include "components/ntp_snippets/pref_names.h"
 #include "components/offline_pages/core/prefetch/prefetch_prefs.h"
+#include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/payments/core/payment_prefs.h"
 #include "components/safe_browsing/common/safe_browsing_prefs.h"
 
@@ -39,6 +40,7 @@ enum Pref {
   USAGE_STATS_ENABLED,
   OFFLINE_PREFETCH_USER_SETTING_ENABLED,
   SAFE_BROWSING_EXTENDED_REPORTING_OPT_IN_ALLOWED,
+  PASSWORD_MANAGER_ONBOARDING_STATE,
   // PREF_NUM_PREFS must be the last entry.
   PREF_NUM_PREFS
 };
@@ -67,6 +69,7 @@ const char* const kPrefsExposedToJava[] = {
     prefs::kUsageStatsEnabled,
     offline_pages::prefetch_prefs::kUserSettingEnabled,
     prefs::kSafeBrowsingExtendedReportingOptInAllowed,
+    password_manager::prefs::kPasswordManagerOnboardingState,
 };
 
 #endif  // CHROME_BROWSER_ANDROID_PREFERENCES_PREFS_H_
