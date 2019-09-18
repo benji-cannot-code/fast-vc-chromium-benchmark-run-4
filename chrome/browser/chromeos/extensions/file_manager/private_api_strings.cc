6c2a1385c2552b1845a8d227cf48e50297ef3db8
Noel Gordon <noel@chromium.org>
2019-09-18 08:23:54 +0000

    Make file tasks C++ control FilesApp PDF/SWF viewing actions
    Viewing PDF files in a renderer (via chrome PDFium) is controlled by a
    system pref PluginsAlwaysOpenPdfExternally, which is read once-only at
    FileApp startup. Dynamic pref changes are ignored (see bug).
    
    Remove loadTimeData setters for PDF and SWF file viewing actions: file
    private_api_strings.cc ShouldBeOpenedWithPlugin calls begone, and same
    for the file_task.js loadTimeData code in FilesApp.
    
    Instead, call ShouldBeOpenedWithPlugin in file_tasks.cc. When the list
    of file tasks is requested by FilesApp, this new code enables/disables
    FilesApp PDF/SWF viewing actions ... dynamically based on prefs.
    
    Add an integration test: tests that QuickView does not show a PDF when
    the system PluginsAlwaysOpenPdfExternally pref is enabled.
    
    Test: browser_tests --gtest_filter="*QuickViewPdfPreviewsDisabled*
    Bug: 999103
    Change-Id: I455fdbd794247a0a57105a2abd6ed929c752a8bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808943
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697515}

--- chrome/browser/chromeos/extensions/file_manager/private_api_strings.cc
+++ chrome/browser/chromeos/extensions/file_manager/private_api_strings.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/browser/chromeos/file_manager/file_manager_string_util.h"
-#include "chrome/browser/chromeos/file_manager/open_with_browser.h"
 #include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -33,19 +32,10 @@ FileManagerPrivateGetStringsFunction::~FileManagerPrivateGetStringsFunction() =
 
 ExtensionFunction::ResponseAction FileManagerPrivateGetStringsFunction::Run() {
   auto dict = GetFileManagerStrings();
-  const std::string empty;
 
   dict->SetBoolean("VIDEO_PLAYER_NATIVE_CONTROLS_ENABLED",
                    base::FeatureList::IsEnabled(
                        chromeos::features::kVideoPlayerNativeControls));
-  dict->SetBoolean("PDF_VIEW_ENABLED",
-                   file_manager::util::ShouldBeOpenedWithPlugin(
-                       Profile::FromBrowserContext(browser_context()),
-                       FILE_PATH_LITERAL(".pdf"), empty));
-  dict->SetBoolean("SWF_VIEW_ENABLED",
-                   file_manager::util::ShouldBeOpenedWithPlugin(
-                       Profile::FromBrowserContext(browser_context()),
-                       FILE_PATH_LITERAL(".swf"), empty));
   // TODO(crbug.com/868747): Find a better solution for demo mode.
   dict->SetBoolean("HIDE_SPACE_INFO",
                    chromeos::DemoSession::IsDeviceInDemoMode());
@@ -68,6 +58,7 @@ ExtensionFunction::ResponseAction FileManagerPrivateGetStringsFunction::Run() {
                        Profile::FromBrowserContext(browser_context())));
   dict->SetBoolean("FILES_NG_ENABLED",
                    base::FeatureList::IsEnabled(chromeos::features::kFilesNG));
+
   dict->SetString("UI_LOCALE", extension_l10n_util::CurrentLocaleOrDefault());
 
   return RespondNow(OneArgument(std::move(dict)));
--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/api/file_manager_private.h"
+#include "chrome/common/pref_names.h"
 #include "chromeos/components/drivefs/drivefs_host.h"
 #include "chromeos/components/drivefs/fake_drivefs.h"
 #include "chromeos/constants/chromeos_features.h"
@@ -2077,6 +2078,14 @@ void FileManagerBrowserTestBase::OnCommand(const std::string& name,
     bool enabled;
     ASSERT_TRUE(value.GetBoolean("enabled", &enabled));
     profile()->GetPrefs()->SetBoolean(drive::prefs::kDisableDrive, !enabled);
+    return;
+  }
+
+  if (name == "setPdfPreviewEnabled") {
+    bool enabled;
+    ASSERT_TRUE(value.GetBoolean("enabled", &enabled));
+    profile()->GetPrefs()->SetBoolean(prefs::kPluginsAlwaysOpenPdfExternally,
+                                      !enabled);
     return;
   }
 
