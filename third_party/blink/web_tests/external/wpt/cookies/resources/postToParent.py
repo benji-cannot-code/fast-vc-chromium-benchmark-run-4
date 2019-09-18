bb6087ac52a8a7c91c488327762647f1595f78a1
Maks Orlovich <morlovich@chromium.org>
2019-09-18 09:32:23 +0000

    Fix about:blank/sandbox handling for site_for_cookies computation for navigation
    Bug: 911299
    
    Change-Id: Icb13317ebaf558753d828754148009cff9ad9ece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1784957
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697531}

--- third_party/blink/web_tests/external/wpt/cookies/resources/postToParent.py
+++ third_party/blink/web_tests/external/wpt/cookies/resources/postToParent.py
@@ -11,9 +11,14 @@ def main(request, response):
 <!DOCTYPE html>
 <script>
   var data = %s;
+  data.type = "COOKIES";
 
-  if (window.parent != window)
+  if (window.parent != window) {
     window.parent.postMessage(data, "*");
+    if (window.top != window.parent)
+      window.top.postMessage(data, "*");
+  }
+
 
   if (window.opener)
     window.opener.postMessage(data, "*");
