a0f8c2e034f7ea6e0104cfaf01b24781b8830d8b
Patrick Monette <pmonette@chromium.org>
2019-09-18 19:31:27 +0000

    Enforce same-origin policy for shared workers in the browser process
    Bug: 1004324
    Change-Id: I85a80a8d590c44f2f23659bbafb79c6c3b83e059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804513
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697723}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -327,6 +327,11 @@ bool ContentBrowserClient::AllowSharedWorker(
   return true;
 }
 
+bool ContentBrowserClient::DoesSchemeAllowCrossOriginSharedWorker(
+    const std::string& scheme) {
+  return false;
+}
+
 bool ContentBrowserClient::AllowSignedExchange(BrowserContext* context) {
   return true;
 }
