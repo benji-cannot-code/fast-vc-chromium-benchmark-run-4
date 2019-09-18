14f80e15562a21ef4eb73985f0ba4c1160ae4acf
Hirokazu Honda <hiroh@chromium.org>
2019-09-18 09:40:45 +0000

    media/gpu/V4L2VEA: Enable to encode GpuMemoryBuffer based VideoFrame
    This CL enables V4L2VideoEncodeAccelerator to encode GpuMemoryBuffer based
    VideoFrame. GpuMemoryBuffer is preferred to DMABUF on encoding because
    GpumemoryBuffer interface support Map() and multiplatform support (e.g.
    Android and Windows). V4L2VideoEncodeAccelerator still can encode DMABUF
    based VideoFrame.
    
    Bug: 1001413
    Test: video_encode_accelerator_unittest on kevin with crrev.com/c/1808939
    Change-Id: I1bc19d47bea88077cb778cb2c7c70618d22731a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808946
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697534}

--- media/gpu/v4l2/v4l2_device.h
+++ media/gpu/v4l2/v4l2_device.h
@@ -49,6 +49,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define V4L2_PIX_FMT_MM21 v4l2_fourcc('M', 'M', '2', '1')
 #endif
 
+namespace gfx {
+struct NativePixmapPlane;
+}  // namespace gfx
+
 namespace media {
 
 class V4L2Queue;
@@ -97,6 +101,15 @@ class MEDIA_GPU_EXPORT V4L2WritableBufferRef {
   // In case of error, false is returned and the buffer is returned to the free
   // list.
   bool QueueDMABuf(const std::vector<base::ScopedFD>& fds) &&;
+  // Queue a DMABUF buffer, assigning file descriptors of |planes| for planes.
+  // It is allowed the number of |planes| might be greater than the number of
+  // planes of this buffer. It happens when the v4l2 pixel format is single
+  // planar. The fd of the first plane of |planes| is only used in that case.
+  // If successful, true is returned and the reference to the buffer is dropped
+  // so this reference becomes invalid.
+  // In case of error, false is returned and the buffer is returned to the free
+  // list.
+  bool QueueDMABuf(const std::vector<gfx::NativePixmapPlane>& planes) &&;
 
   // Returns the number of planes in this buffer.
   size_t PlanesCount() const;
