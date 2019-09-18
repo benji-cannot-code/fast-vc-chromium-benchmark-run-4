5eeff05b4c3b3a4c50e0816e2c28209c436e8b98
Jesse Schettler <jschettler@chromium.org>
2019-09-18 02:50:04 +0000

    printing: Increment/decrement IPC nesting level in helper methods
    Now that legacy printing IPC messages are being converted to Mojo, the
    IPC nesting level needs to be incremented/decremented in each Mojo
    method. This change moves the incrementing/decrementing of the IPC
    nesting level to helper methods that are used by the new Mojo methods
    and OnMessageReceived().
    
    Bug: b:140576300
    Test: Print a webpage or PDF
    Change-Id: Ib71796367cd36eef9293dfad455504b3ee2b57c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809803
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697461}

--- components/printing/renderer/print_render_frame_helper.cc
+++ components/printing/renderer/print_render_frame_helper.cc
@@ -1156,14 +1156,7 @@ void PrintRenderFrameHelper::ScriptedPrint(bool user_initiated) {
 }
 
 bool PrintRenderFrameHelper::OnMessageReceived(const IPC::Message& message) {
-  // The class is not designed to handle recursive messages. This is not
-  // expected during regular flow. However, during rendering of content for
-  // printing, lower level code may run nested run loop. E.g. PDF may has
-  // script to show message box http://crbug.com/502562. In that moment browser
-  // may receive updated printer capabilities and decide to restart print
-  // preview generation. When this happened message handling function may
-  // choose to ignore message or safely crash process.
-  ++ipc_nesting_level_;
+  IPCReceived();
 
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP(PrintRenderFrameHelper, message)
@@ -1180,9 +1173,7 @@ bool PrintRenderFrameHelper::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
 
-  --ipc_nesting_level_;
-  if (ipc_nesting_level_ == 0 && render_frame_gone_)
-    base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
+  IPCProcessed();
   return handled;
 }
 
@@ -1203,6 +1194,8 @@ void PrintRenderFrameHelper::InitiatePrintPreview(
     mojom::PrintRendererAssociatedPtrInfo print_renderer,
     bool has_selection) {
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  IPCReceived();
+
   if (ipc_nesting_level_ > 1)
     return;
 
@@ -1222,6 +1215,8 @@ void PrintRenderFrameHelper::InitiatePrintPreview(
   RequestPrintPreview(has_selection
                           ? PRINT_PREVIEW_USER_INITIATED_SELECTION
                           : PRINT_PREVIEW_USER_INITIATED_ENTIRE_FRAME);
+
+  IPCProcessed();
 #endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
 }
 
@@ -1909,6 +1904,23 @@ std::vector<int> PrintRenderFrameHelper::GetPrintedPages(
     }
   }
   return printed_pages;
+}
+
+void PrintRenderFrameHelper::IPCReceived() {
+  // The class is not designed to handle recursive messages. This is not
+  // expected during regular flow. However, during rendering of content for
+  // printing, lower level code may run a nested run loop. E.g. PDF may have a
+  // script to show message box (http://crbug.com/502562). In that moment, the
+  // browser may receive updated printer capabilities and decide to restart
+  // print preview generation. When this happens, message handling functions may
+  // choose to ignore messages or safely crash the process.
+  ++ipc_nesting_level_;
+}
+
+void PrintRenderFrameHelper::IPCProcessed() {
+  --ipc_nesting_level_;
+  if (ipc_nesting_level_ == 0 && render_frame_gone_)
+    base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
 }
 
 bool PrintRenderFrameHelper::InitPrintSettings(bool fit_to_paper_size) {
