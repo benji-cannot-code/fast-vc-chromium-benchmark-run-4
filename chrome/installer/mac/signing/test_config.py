4d8836c51443443c9aa1115b73561ee7233be2e3
Robert Sesek <rsesek@chromium.org>
2019-09-18 00:57:47 +0000

    macOS Signing Scripts: Split the config class and expand the hierarchy.
    The base CodeSignConfig is now in a normal Python module and it has
    unimplemented properties. The build-system-injected variables inherit
    from that for the BuildPropsCodeSignConfig. A new
    ChromiumCodeSignConfig class is created to set the signing properties
    for Chromium, to mirror the InternalCodeSignConfig for Google Chrome
    builds. Both ChromiumConfig and InternalCodeSignConfig inherit from
    the BuildPropsCodeSignConfig.
    
    This makes it easier to use the signing module without needing to
    process a config file.
    
    Change-Id: I2041da6091f98e3b118a2ddbc9108197ee1003a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808344
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697437}

--- chrome/installer/mac/signing/test_config.py
+++ chrome/installer/mac/signing/test_config.py
@@ -3,13 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import imp
-import os
-
-THIS_DIR = os.path.abspath(os.path.dirname(__file__))
-
-config = imp.load_source('signing.config', os.path.join(THIS_DIR,
-                                                        'config.py.in'))
+from . import config
 
 
 class TestConfig(config.CodeSignConfig):
