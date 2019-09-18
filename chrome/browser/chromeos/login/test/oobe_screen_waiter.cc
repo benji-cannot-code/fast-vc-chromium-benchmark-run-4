78bfb3f79147fc5e4a63befd1679844f2f8879b8
Jit Yao Yap <jityao@google.com>
2019-09-18 09:50:09 +0000

    Chrome OS Oobe: Show Ash dialog only if required
    When starting a managed guest session (with no Terms of Service), a
    blank Ash dialog is shown for a brief moment before the session starts.
    This is caused by the Ash dialog for Oobe being shown even when all the
    Oobe screens are skipped.
    
    This CL changes the behavior of the Ash dialog to only show when
    required by using a OobeUI::Observer. The dialog is not shown by
    default, and will be shown when a Oobe screen is shown.
    
    OobeScreenWaiter now checks that the native window is visible by
    default. A small number of tests had to be changed to either show the
    native window or ignore the visibility check. The reasoning for the
    changes are in the comments for their respective tests.
    
    The WebUI behavior is unchanged.
    
    Bug: 1000164
    Change-Id: I9995f4beba85d7c5af7b13f81f1a654e2d52f372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790450
    Commit-Queue: Jit Yao Yap <jityao@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697539}

--- chrome/browser/chromeos/login/test/oobe_screen_waiter.cc
+++ chrome/browser/chromeos/login/test/oobe_screen_waiter.cc
@@ -7,7 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host.h"
+#include "chrome/browser/chromeos/login/ui/webui_login_view.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/aura/window.h"
+#include "ui/views/controls/webview/webview.h"
+#include "ui/views/view.h"
 
 namespace chromeos {
 
@@ -19,13 +23,20 @@ OobeScreenWaiter::~OobeScreenWaiter() = default;
 void OobeScreenWaiter::Wait() {
   DCHECK_EQ(State::IDLE, state_);
 
-  if (GetOobeUI()->current_screen() == target_screen_) {
+  if ((!check_native_window_visible_ || IsNativeWindowVisible()) &&
+      IsTargetScreenReached()) {
     state_ = State::DONE;
     return;
   }
   DCHECK(!run_loop_);
 
   oobe_ui_observer_.Add(GetOobeUI());
+  if (check_native_window_visible_) {
+    aura::Window* native_window =
+        LoginDisplayHost::default_host()->GetNativeWindow();
+    DCHECK(native_window);
+    native_window_observer_.Add(native_window);
+  }
 
   state_ = State::WAITING_FOR_SCREEN;
 
@@ -36,6 +47,8 @@ void OobeScreenWaiter::Wait() {
   DCHECK_EQ(State::DONE, state_);
 
   oobe_ui_observer_.RemoveAll();
+  if (check_native_window_visible_)
+    native_window_observer_.RemoveAll();
 
   if (assert_last_screen_)
     EXPECT_EQ(target_screen_, GetOobeUI()->current_screen());
@@ -61,8 +74,31 @@ void OobeScreenWaiter::OnCurrentScreenChanged(OobeScreenId current_screen,
     return;
   }
 
-  if (new_screen == target_screen_)
+  if (check_native_window_visible_ && !IsNativeWindowVisible()) {
+    return;
+  }
+
+  if (IsTargetScreenReached())
     EndWait();
+}
+
+void OobeScreenWaiter::OnWindowVisibilityChanged(aura::Window* window,
+                                                 bool visible) {
+  DCHECK_NE(state_, State::IDLE);
+  DCHECK(check_native_window_visible_);
+
+  if (IsNativeWindowVisible() && IsTargetScreenReached())
+    EndWait();
+}
+
+bool OobeScreenWaiter::IsTargetScreenReached() {
+  return GetOobeUI()->current_screen() == target_screen_;
+}
+
+bool OobeScreenWaiter::IsNativeWindowVisible() {
+  aura::Window* native_window =
+      LoginDisplayHost::default_host()->GetNativeWindow();
+  return native_window && native_window->IsVisible();
 }
 
 void OobeScreenWaiter::OnDestroyingOobeUI() {
