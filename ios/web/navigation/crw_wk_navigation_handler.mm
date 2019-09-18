856e9747eb6c5df3356798a5f5127c4594667991
Justin Cohen <justincohen@google.com>
2019-09-18 20:39:56 +0000

    [ios] Fix kRestoreNavigationTime histogram.
    kRestoreNavigationTime was a timer created by WKBasedNavigationManagerImpl::Restore
    and stopped when session restoration begins navigating to its final URL.
    
    Unfortunately, WKBasedNavigationManagerImpl::Restore is called for every restored
    WebState on startup.  Actual network access doesn't happen until a tab is selected. That
    means if you have 10 tabs, background/kill/restore, navigate a bit, and then change tabs,
    that second tab will have had a timer running since the app started.
    
    Instead, start kRestoreNavigationTime when ios/web begins loading the restore page, and
    finish the timer when ios/web begins loading the real page.  This should more correctly
    tell how much time SlimNav restoration adds.
    
    Bug: 1005044
    Change-Id: I35fee36bb50ec627d7455d6215669a92a2b4bc6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808355
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697757}

--- ios/web/navigation/crw_wk_navigation_handler.mm
+++ ios/web/navigation/crw_wk_navigation_handler.mm
@@ -569,6 +569,8 @@ void ReportOutOfSyncURLInDidStartProvisionalNavigation(
     }
 
     self.webStateImpl->OnNavigationStarted(context);
+    self.webStateImpl->GetNavigationManagerImpl().OnNavigationStarted(
+        webViewURL);
     return;
   }
 
@@ -619,8 +621,7 @@ void ReportOutOfSyncURLInDidStartProvisionalNavigation(
     return;
   }
 
-  self.webStateImpl->GetNavigationManagerImpl()
-      .OnRendererInitiatedNavigationStarted(webViewURL);
+  self.webStateImpl->GetNavigationManagerImpl().OnNavigationStarted(webViewURL);
 
   // When a client-side redirect occurs while an interstitial warning is
   // displayed, clear the warning and its navigation item, so that a new
--- ios/web/navigation/legacy_navigation_manager_impl.mm
+++ ios/web/navigation/legacy_navigation_manager_impl.mm
@@ -60,8 +60,7 @@ void LegacyNavigationManagerImpl::OnNavigationItemCommitted() {
   delegate_->OnNavigationItemCommitted(item);
 }
 
-void LegacyNavigationManagerImpl::OnRendererInitiatedNavigationStarted(
-    const GURL& url) {}
+void LegacyNavigationManagerImpl::OnNavigationStarted(const GURL& url) {}
 
 CRWSessionController* LegacyNavigationManagerImpl::GetSessionController()
     const {
