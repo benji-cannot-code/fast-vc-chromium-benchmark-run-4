2870d2b56e9963697942efa06bafade4af3820ad
Chinglin Yu <chinglinyu@chromium.org>
2019-09-18 15:51:19 +0000

    Use SEQUENCE_CHECKER in PerfOutputCall
    Switching to running the perf collector on a dedicated sequence triggers
    a crash in DCHECK_CALLED_ON_VALID_THREAD when the THREAD_CHECKER is
    checked on 2 different threads in the same thread pool at different
    times. Switch to SEQUENCE_CHECKER to fix the crash.
    
    Bug: 904785
    Test: Turn dcheck_always_on = true and revert https://crrev.com/c/1764319
          Verify on DUT that collections doesn't crash the system UI.
    
    Change-Id: I9972baa568a2f7bad373777750085a160c6deb23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810146
    Reviewed-by: Gabriel Marin <gmx@chromium.org>
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697638}

--- chrome/browser/metrics/perf/perf_output.h
+++ chrome/browser/metrics/perf/perf_output.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
-#include "base/threading/thread_checker.h"
+#include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "chromeos/dbus/dbus_method_call_status.h"
 #include "chromeos/dbus/pipe_reader.h"
@@ -99,7 +99,7 @@ class PerfOutputCall {
   bool pending_stop_;
   base::Optional<uint64_t> perf_session_id_;
 
-  THREAD_CHECKER(thread_checker_);
+  SEQUENCE_CHECKER(sequence_checker_);
 
   // To pass around the "this" pointer across threads safely.
   base::WeakPtrFactory<PerfOutputCall> weak_factory_{this};
