65453c6d8123aa6c1052d04f1b6da0db8e8a7536
Maksim Ivanov <emaxx@chromium.org>
2019-09-18 18:30:43 +0000

    Move SystemTokenCertDBInitializer into sep files
    Refactor the SystemTokenCertDBInitializer class to live in its own
    .h+.cc files, rather than being defined inside the already heavy
    chrome_browser_main_chromeos.cc.
    
    Other minor changes made there:
    1. Removed the Initialize() method, and added a ShutDown() method
       (the latter will become non-empty in a follow-up CL:1796426).
    2. Replaced base::Bind/base::Callback with their modern
       Once/Repeating analogs.
    
    Bug: 725500
    Change-Id: Ia172f3cf1e049171914f5fd251a8cc88f6643e55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1806859
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697696}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.h
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.h
@@ -61,6 +61,7 @@ class RendererFreezer;
 class SchedulerConfigurationManager;
 class SessionTerminationManager;
 class ShutdownPolicyForwarder;
+class SystemTokenCertDBInitializer;
 class WakeOnWifiManager;
 class WilcoDtcSupportdManager;
 
@@ -70,7 +71,6 @@ class ExternalLoader;
 
 namespace internal {
 class DBusServices;
-class SystemTokenCertDBInitializer;
 }  // namespace internal
 
 namespace power {
@@ -125,7 +125,7 @@ class ChromeBrowserMainPartsChromeos : public ChromeBrowserMainPartsLinux {
 
   std::unique_ptr<internal::DBusServices> dbus_services_;
 
-  std::unique_ptr<internal::SystemTokenCertDBInitializer>
+  std::unique_ptr<SystemTokenCertDBInitializer>
       system_token_certdb_initializer_;
 
   std::unique_ptr<ShutdownPolicyForwarder> shutdown_policy_forwarder_;
