e6db958db6480233587b6c38ee0f775682bfe819
Antonio Gomes <tonikitoo@igalia.com>
2019-09-18 15:26:32 +0000

    Move filtering_network_manager.h and network_manager_uma.h out of the Blink API
    This is a follow up of [1].
    
    [1] https://crrev.com/c/1805159
    
    BUG=787254, 919392
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: Ib3198c792586ebe2671b365d17d46201a07d5aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1806975
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697621}

--- third_party/blink/renderer/platform/p2p/empty_network_manager.cc
+++ third_party/blink/renderer/platform/p2p/empty_network_manager.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "third_party/blink/public/platform/modules/p2p/network_manager_uma.h"
+#include "third_party/blink/renderer/platform/p2p/network_manager_uma.h"
 
 namespace blink {
 
