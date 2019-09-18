913bb0ad5adee96a2a1bbbe3c7aee257ac51ad20
Connie Wan <connily@chromium.org>
2019-09-18 22:32:37 +0000

    Implement basic TabGroupUnderline
    The underline is composed of two parts:
    1) A straight underline, painted by TabStrip, extending from the group header to the last time (minus some inset).
    2) A stroke above the active tab, painted by TabStyleViews, which (along with the active tab background) is layered over the straight underline.
    
    The underline is still missing the following polish:
    - Half-rounded edges.
    - Potential redesign of the stroke above the active tab.
    
    Screenshots are in the attached bug.
    
    Bug: 905491
    Change-Id: I4a99a67f9637b8a879bba7589feb848edb30deee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809546
    Commit-Queue: Connie Wan <connily@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697805}

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -686,7 +686,7 @@ void Tab::SetGroup(base::Optional<TabGroupId> group) {
   if (group_ == group)
     return;
   group_ = group;
-  GroupColorChanged();
+  SchedulePaint();
 }
 
 base::Optional<SkColor> Tab::GetGroupColor() const {
@@ -694,11 +694,6 @@ base::Optional<SkColor> Tab::GetGroupColor() const {
              ? base::make_optional(
                    controller_->GetVisualDataForGroup(group_.value())->color())
              : base::nullopt;
-}
-
-void Tab::GroupColorChanged() {
-  UpdateForegroundColors();
-  SchedulePaint();
 }
 
 SkColor Tab::GetAlertIndicatorColor(TabAlertState state) const {
--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/browser/ui/views/tabs/tab_drag_controller.h"
 #include "chrome/browser/ui/views/tabs/tab_group_header.h"
+#include "chrome/browser/ui/views/tabs/tab_group_underline.h"
 #include "chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_controller.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_layout_helper.h"
@@ -1144,6 +1145,12 @@ void TabStrip::ChangeTabGroup(int model_index,
         base::BindOnce(&TabStrip::OnGroupCloseAnimationCompleted,
                        base::Unretained(this), new_group.value()));
     group_headers_[new_group.value()] = std::move(header);
+
+    auto underline =
+        std::make_unique<TabGroupUnderline>(this, new_group.value());
+    underline->set_owned_by_client();
+    AddChildView(underline.get());
+    group_underlines_[new_group.value()] = std::move(underline);
   }
   if (old_group.has_value()) {
     if (controller_->ListTabsInGroup(old_group.value()).size() == 0) {
@@ -1160,8 +1167,6 @@ void TabStrip::ChangeTabGroup(int model_index,
 
 void TabStrip::GroupVisualsChanged(TabGroupId group) {
   group_headers_[group]->VisualsChanged();
-  for (int i : controller_->ListTabsInGroup(group))
-    tabs_.view_at(i)->GroupColorChanged();
   // The group title may have changed size.
   UpdateIdealBounds();
   AnimateToIdealBounds();
@@ -1833,6 +1838,10 @@ void TabStrip::PaintChildren(const views::PaintInfo& paint_info) {
   for (const auto& header_pair : group_headers_)
     header_pair.second->Paint(paint_info);
 
+  // Paint group underlines.
+  for (const auto& underline_pair : group_underlines_)
+    underline_pair.second->Paint(paint_info);
+
   // Always paint the active tab over all the inactive tabs.
   if (active_tab && !is_dragging)
     active_tab->Paint(paint_info);
@@ -2411,6 +2420,7 @@ void TabStrip::OnTabCloseAnimationCompleted(Tab* tab) {
 
 void TabStrip::OnGroupCloseAnimationCompleted(TabGroupId group) {
   group_headers_.erase(group);
+  group_underlines_.erase(group);
   // TODO(crbug.com/905491): We might want to simulate a mouse move here, like
   // we do in OnTabCloseAnimationCompleted.
 }
