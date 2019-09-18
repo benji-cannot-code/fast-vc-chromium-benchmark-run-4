3b2b2eeea48d9f3b7aa86485b23bfe20fdcae780
Toby Huang <tobyhuang@chromium.org>
2019-09-18 04:57:19 +0000

    Always plug "chrome://add-supervision" into FindInstance()
    Currently, GetDialogId() starts out as the empty string, gets set to
    kChromeUIAddSupervisionURL="chrome://add-supervision" when the Add
    Supervision dialog is opened, and sometimes gets reset to empty string
    when the dialog is closed depending on which button is pressed. This CL
    ensures kChromeUIAddSupervisionURL is always passed to FindInstance()
    instead of the empty string which can lead to unpredictable behavior
    and can easily return something other than null.
    
    Bug: 1000978
    Change-Id: I0287aefdb3451db2e8f388dbff659623d4e7f982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787339
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697485}

--- chrome/browser/ui/webui/chromeos/add_supervision/add_supervision_ui.cc
+++ chrome/browser/ui/webui/chromeos/add_supervision/add_supervision_ui.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/no_destructor.h"
 #include "base/system/sys_info.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -37,11 +36,6 @@ namespace {
 
 constexpr int kDialogHeightPx = 608;
 constexpr int kDialogWidthPx = 768;
-// Id of System Dialog used to show the Add Supervision flow.
-std::string& GetDialogId() {
-  static base::NoDestructor<std::string> dialog_id;
-  return *dialog_id;
-}
 
 // Shows the dialog indicating that user has to sign out if supervision has been
 // enabled for their account.  Returns a boolean indicating whether the
@@ -79,8 +73,6 @@ void AddSupervisionDialog::Show(gfx::NativeView parent) {
   // SystemWebDialogDelegate::OnDialogClosed() is called.
   current_instance = new AddSupervisionDialog();
 
-  GetDialogId() = current_instance->Id();
-
   current_instance->ShowSystemDialogForBrowserContext(
       ProfileManager::GetPrimaryUserProfile(), parent);
 
@@ -90,11 +82,24 @@ void AddSupervisionDialog::Show(gfx::NativeView parent) {
 }
 
 // static
+SystemWebDialogDelegate* AddSupervisionDialog::GetInstance() {
+  return SystemWebDialogDelegate::FindInstance(
+      chrome::kChromeUIAddSupervisionURL);
+}
+
+// static
 void AddSupervisionDialog::Close() {
   SystemWebDialogDelegate* current_instance = GetInstance();
   if (current_instance) {
     current_instance->Close();
-    GetDialogId() = std::string();
+  }
+}
+
+void AddSupervisionDialog::CloseNowForTesting() {
+  SystemWebDialogDelegate* current_instance = GetInstance();
+  if (current_instance) {
+    DCHECK(dialog_window()) << "No dialog window instance currently set.";
+    views::Widget::GetWidgetForNativeWindow(dialog_window())->CloseNow();
   }
 }
 
@@ -123,11 +128,6 @@ AddSupervisionDialog::AddSupervisionDialog()
                               base::string16()) {}
 
 AddSupervisionDialog::~AddSupervisionDialog() = default;
-
-// static
-SystemWebDialogDelegate* AddSupervisionDialog::GetInstance() {
-  return SystemWebDialogDelegate::FindInstance(GetDialogId());
-}
 
 // AddSupervisionUI implementations.
 
