2eaac10337125f8f721fdd2ff11b510555de34dd
Gyuyoung Kim <gyuyoung@igalia.com>
2019-09-18 00:37:07 +0000

    Convert mojo::Binding to mojo::Receiver in XRSession
    This CL applies mojo::Receiver to |client_receiver_|
    member variable.
    
    Bug: 955171, 978694
    Change-Id: I6c473edc4cb46d2e92c6dd0e6a3b8a9343b2f978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808658
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#697425}

--- third_party/blink/renderer/modules/xr/xr_session.h
+++ third_party/blink/renderer/modules/xr/xr_session.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "device/vr/public/mojom/vr_service.mojom-blink.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
 #include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
@@ -70,7 +70,8 @@ class XRSession final
   };
 
   XRSession(XR* xr,
-            device::mojom::blink::XRSessionClientRequest client_request,
+            mojo::PendingReceiver<device::mojom::blink::XRSessionClient>
+                client_receiver,
             SessionMode mode,
             EnvironmentBlendMode environment_blend_mode,
             bool uses_input_eventing,
@@ -306,7 +307,7 @@ class XRSession final
   unsigned int stage_parameters_id_ = 0;
   device::mojom::blink::VRDisplayInfoPtr display_info_;
 
-  mojo::Binding<device::mojom::blink::XRSessionClient> client_binding_;
+  mojo::Receiver<device::mojom::blink::XRSessionClient> client_receiver_;
   mojo::AssociatedBinding<device::mojom::blink::XRInputSourceButtonListener>
       input_binding_;
 
