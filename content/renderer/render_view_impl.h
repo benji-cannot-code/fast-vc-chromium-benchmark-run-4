c965094d1de289cf7959720f17524dffafd6710b
danakj <danakj@chromium.org>
2019-09-18 21:05:20 +0000

    Revert "Move page zoom level responsibility into RenderViewImpl."
    This reverts commit 6876fb4fffcf526da756a794c91b60d58ef60c07.
    
    Reason for revert: Breaks layout tests even though they passed in the CQ???
    
    Linux:
    animations/animationworklet/worklet-animation-responsive-to-zoom.html
    svg/as-image/img-zoom-svg-stylesheet.html
    fast/media/mq-width-pagezoom.html
    
    Windows:
    animations/responsive/zoom-responsive-translate-animation.html
    fast/media/mq-resolution-pagezoom.html
    fast/backgrounds/tiny-tile-zoomed.html
    
    animations/animationworklet/worklet-animation-responsive-to-zoom.html
    svg/as-image/img-zoom-svg-stylesheet.html
    fast/media/mq-resolution-pagezoom.html
    
    
    Original change's description:
    > Move page zoom level responsibility into RenderViewImpl.
    >
    > Page zoom level is a page/view concept, but the code to manage it is
    > currently in RenderFrameImpl, though only run when it is the root frame.
    >
    > This doesn't dramatically change ownership of responsibility but moves
    > the code up into RenderViewImpl. RenderWidget still goes through
    > RenderFrameImpl (though more explicitly) to set the zoom level on the
    > RenderView. And RenderWidget takes responsibility for closing the page
    > popup instead of having multiple "set page zoom" methods on
    > RenderViewImpl.
    >
    > This uncovered some spooky action-at-a-distance and makes it more
    > explicit and documented, though no less unfortunate. A local root
    > LocalFrame does not initialize with the correct page zoom value because
    > it has no parent to read it from. We (accidentally?) worked around that
    > by setting the RenderView's page zoom to its current value when
    > navigating a (new) LocalFrame, which would call into blink's WebView
    > and walk through the entire frame tree setting the value on everything,
    > eventually also on the new LocalFrame.
    >
    > Now on navigation, for a local root, we call RenderWidget's
    > UpdateWebViewWithDeviceScaleFactor() which does the same thing by
    > re-setting the WebView's page zoom factor. This was done when making
    > a new main frame in multiple places (RenderView and RenderFrame both
    > called it in the same CreateFrame() callstack). Now it is consolidated
    > to a single place on navigation for the main frame and child frames
    > giving them more consistent behaviour, and going explicitly through
    > this method that should go away if things were well designed, rather
    > than re-setting the RenderView to its own value.
    >
    > Remove calls to UpdateWebViewWithDeviceScaleFactor() scattered
    > throughout RenderViewImpl and RenderFrameImpl. What we need is for
    > 1) The Page to be updated with PreferCompositingToLCDText when the
    > device scale factor changes.
    > 2) The WebView to be updated when the device scale factor changes.
    > 3) The LocalFrame of a local root needs its page zoom set by poking
    > the WebViewImpl to walk through all frames and set it.
    >
    > R=​erikchen@chromium.org
    >
    > Bug: 419087
    > Change-Id: I97c0e287be8f96a4ac632d63b981c190169dc1a9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1799386
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Commit-Queue: danakj <danakj@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#697687}
    
    R=avi@chromium.org, dcheng@chromium.org, erikchen@chromium.org
    TBR=danakj@chromium.org
    
    Change-Id: I3177d7f7a601b6692fd931623ca749bacce051c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 419087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1811937
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697766}

--- content/renderer/render_view_impl.h
+++ content/renderer/render_view_impl.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/containers/flat_map.h"
 #include "base/containers/id_map.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
@@ -168,26 +167,12 @@ class CONTENT_EXPORT RenderViewImpl : public blink::WebViewClient,
   void AddObserver(RenderViewObserver* observer);
   void RemoveObserver(RenderViewObserver* observer);
 
-  // Stores a map of url to zoom level, specified by the browser.
-  void SetHostZoomLevel(const GURL& url, double zoom_level);
+  // Sets the zoom level and notifies observers.
+  void SetZoomLevel(double zoom_level);
 
-  // When the main frame commits, the zoom level may need to be changed.
-  void UpdateZoomLevelForNavigationCommitOfMainFrame(const GURL& loading_url);
-
-  // Sets the zoom level and notifies observers. Returns true if the zoom level
-  // changed. A value of 0 means the default zoom level.
-  bool SetZoomLevel(double zoom_level);
-
-  // Passes along the prefer compositing preference to the WebView's settings.
-  void SetPreferCompositingToLCDTextEnabled(bool prefer);
-
-  // Passes along the device scale factor to the WebView.
-  void SetDeviceScaleFactor(bool use_zoom_for_dsf, float device_scale_factor);
-
-  // Passes along the page zoom to the WebView to set it on a newly attached
-  // LocalFrame.
-  void PropagatePageZoomToNewlyAttachedFrame(bool use_zoom_for_dsf,
-                                             float device_scale_factor);
+  double page_zoom_level() {
+    return page_zoom_level_;
+  }
 
   // Sets page-level focus in this view and notifies plugins and Blink's
   // FocusController.
@@ -318,6 +303,7 @@ class CONTENT_EXPORT RenderViewImpl : public blink::WebViewClient,
   bool renderer_wide_named_frame_lookup() {
     return renderer_wide_named_frame_lookup_;
   }
+  void UpdateZoomLevel(double zoom_level);
 
  protected:
   RenderViewImpl(CompositorDependencies* compositor_deps,
@@ -659,9 +645,6 @@ class CONTENT_EXPORT RenderViewImpl : public blink::WebViewClient,
   // Used to indicate the zoom level to be used during subframe loads, since
   // they should match page zoom level.
   double page_zoom_level_ = 0;
-
-  // A map of urls to zoom levels specified by the browser.
-  base::flat_map<GURL, double> host_zoom_levels_;
 
   // Helper objects ------------------------------------------------------------
 
