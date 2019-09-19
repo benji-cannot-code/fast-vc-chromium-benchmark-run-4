e10943a83219366874133b3a2dc65a94146ad3b0
Anastasiia Nikolaienko <anastasiian@chromium.org>
2019-09-19 16:20:07 +0000

    Add 'secondary accounts disabled' message to settings
    Add "Your administrator has disabled adding more Google accounts"
    message to Account Manager when addition of secondary accounts
    is disabled.
    
    Bug: 987222
    Change-Id: Idcf06e184be4029d75739086c884531bbf080537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1731936
    Commit-Queue: Anastasiia Nikolaienko <anastasiian@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698049}

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/settings_private/chromeos_resolve_time_zone_by_geolocation_method_short.h"
 #include "chrome/browser/extensions/api/settings_private/chromeos_resolve_time_zone_by_geolocation_on_off.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "chromeos/constants/chromeos_pref_names.h"
 #include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
 #include "chromeos/settings/cros_settings_names.h"
 #include "components/arc/arc_prefs.h"
@@ -345,6 +346,8 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetWhitelistedKeys() {
       settings_api::PrefType::PREF_TYPE_BOOLEAN;
   (*s_whitelist)[chromeos::kAccountsPrefUsers] =
       settings_api::PrefType::PREF_TYPE_LIST;
+  (*s_whitelist)[chromeos::prefs::kSecondaryGoogleAccountSigninAllowed] =
+      settings_api::PrefType::PREF_TYPE_BOOLEAN;
   // kEnableAutoScreenLock is read-only.
   (*s_whitelist)[ash::prefs::kEnableAutoScreenLock] =
       settings_api::PrefType::PREF_TYPE_BOOLEAN;
@@ -754,8 +757,13 @@ std::unique_ptr<settings_api::PrefObject> PrefsUtil::GetPref(
 #endif
 
   if (pref && pref->IsManaged()) {
-    pref_object->controlled_by =
-        settings_api::ControlledBy::CONTROLLED_BY_USER_POLICY;
+    if (profile_->IsChild()) {
+      pref_object->controlled_by =
+          settings_api::ControlledBy::CONTROLLED_BY_CHILD_RESTRICTION;
+    } else {
+      pref_object->controlled_by =
+          settings_api::ControlledBy::CONTROLLED_BY_USER_POLICY;
+    }
     pref_object->enforcement = settings_api::Enforcement::ENFORCEMENT_ENFORCED;
     return pref_object;
   }
