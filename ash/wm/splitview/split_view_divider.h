679482027fd32d815490d12b71cc809af2422e44
Avery Musbach <amusbach@chromium.org>
2019-09-19 16:55:52 +0000

    Reland "split view: Remove window activation in SplitViewController's snap logic"
    This is a reland of 588d7e153d2df1bedd449f1f3182c6b2cafe2ffc
    
    Original change's description:
    > split view: Remove window activation in SplitViewController's snap logic
    > 
    > SplitViewController::OnWindowSnapped shall no longer activate the newly
    > snapped window. In many workflows, such window activation is redundant
    > or inappropriate (see the referenced issues on Monorail). Window
    > activation shall be done separately by code for workflows where the
    > snapped window actually does need to be activated.
    > 
    > Bug: 982954, 983227, 991611, 995946
    > Change-Id: Ie111d90786764f0612aea73ba9cfa4ce640fe1f2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1738338
    > Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    > Reviewed-by: Mitsuru Oshima (slow - perfing & gardening) <oshima@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Avery Musbach <amusbach@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#696599}
    
    Bug: 982954, 983227, 991611, 995946
    Change-Id: I607c26cf4457171436a638db75d9a350a5ea405b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809452
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698075}

--- ash/wm/splitview/split_view_divider.h
+++ ash/wm/splitview/split_view_divider.h
@@ -68,6 +68,8 @@ class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
   // position.
   gfx::Rect GetDividerBoundsInScreen(bool is_dragging);
 
+  void SetAlwaysOnTop(bool on_top);
+
   void AddObservedWindow(aura::Window* window);
   void RemoveObservedWindow(aura::Window* window);
 
@@ -98,7 +100,6 @@ class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
 
  private:
   void CreateDividerWidget(aura::Window* root_window);
-  void SetAlwaysOnTop(bool on_top);
 
   SplitViewController* controller_;
 
