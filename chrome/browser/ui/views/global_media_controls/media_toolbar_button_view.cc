3b2a4af491795c2ef9e18cd841eee88a81de06d8
Tommy Steimel <steimel@chromium.org>
2019-09-19 20:02:44 +0000

    GMC: Fix inverted icon in RTL UI
    Bug: 1005722
    Change-Id: I22866ac8bca245f2072fa3b92d5787fa24877df4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814654
    Reviewed-by: Jazz Xu <jazzhsu@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698151}

--- chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.cc
+++ chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.cc
@@ -33,6 +33,7 @@ MediaToolbarButtonView::MediaToolbarButtonView(
 
   button_controller()->set_notify_action(
       views::ButtonController::NotifyAction::NOTIFY_ON_PRESS);
+  EnableCanvasFlippingForRTLUI(false);
   SetTooltipText(
       l10n_util::GetStringUTF16(IDS_GLOBAL_MEDIA_CONTROLS_ICON_TOOLTIP_TEXT));
 
