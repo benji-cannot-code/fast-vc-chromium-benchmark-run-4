994df43ff29fb049ba4e8f96e90a45896c7a01c7
Maksim Sisov <msisov@igalia.com>
2019-09-19 06:35:17 +0000

    Ease expectation of having move/resize handler.
    It is not guaranteed that move/resize handler is set by a
    PlatformWindow. Thus, remove the DCHECK and add if (handler_)
    condition as part of the existing condition to dispatch move/resize
    requests.
    
    Bug: 990756
    Change-Id: Ibfa1251f65af547382f04d44fdef28d620c0bdd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807311
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#697919}

--- ui/views/widget/desktop_aura/window_event_filter_linux.cc
+++ ui/views/widget/desktop_aura/window_event_filter_linux.cc
@@ -26,9 +26,7 @@ namespace views {
 WindowEventFilterLinux::WindowEventFilterLinux(
     DesktopWindowTreeHost* window_tree_host,
     ui::WmMoveResizeHandler* handler)
-    : window_tree_host_(window_tree_host), handler_(handler) {
-  DCHECK(handler_);
-}
+    : window_tree_host_(window_tree_host), handler_(handler) {}
 
 WindowEventFilterLinux::~WindowEventFilterLinux() = default;
 
@@ -155,7 +153,8 @@ void WindowEventFilterLinux::LowerWindow() {}
 void WindowEventFilterLinux::MaybeDispatchHostWindowDragMovement(
     int hittest,
     ui::MouseEvent* event) {
-  if (event->IsLeftMouseButton() && ui::CanPerformDragOrResize(hittest)) {
+  if (handler_ && event->IsLeftMouseButton() &&
+      ui::CanPerformDragOrResize(hittest)) {
     // Some platforms (eg X11) may require last pointer location not in the
     // local surface coordinates, but rather in the screen coordinates for
     // interactive move/resize.
