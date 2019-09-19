52781ad08aeb94945c62d0efce9c2043a9fe0def
George Zhang <zhanggeorge@google.com>
2019-09-19 02:10:05 +0000

    Generate perfetto output file separately for Android
    Bug: 1002956
    Change-Id: Ic36813f80cd7861531212bd46bd0e6d0af36a90b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804184
    Commit-Queue: ssid <ssid@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697872}

--- content/browser/tracing/perfetto_file_tracer.cc
+++ content/browser/tracing/perfetto_file_tracer.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/task/post_task.h"
 #include "base/threading/scoped_blocking_call.h"
+#include "build/build_config.h"
 #include "components/tracing/common/trace_startup_config.h"
 #include "components/tracing/common/tracing_switches.h"
 #include "content/public/browser/system_connector.h"
@@ -22,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/tracing/public/mojom/constants.mojom.h"
 #include "third_party/perfetto/include/perfetto/tracing/core/trace_config.h"
 #include "third_party/perfetto/protos/perfetto/config/trace_config.pb.h"
+
+#if defined(OS_ANDROID)
+#include "content/browser/android/tracing_controller_android.h"
+#endif
 
 namespace content {
 
@@ -36,6 +41,14 @@ class BackgroundDrainer : public mojo::DataPipeDrainer::Client {
     base::FilePath output_file =
         base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
             switches::kPerfettoOutputFile);
+
+#if defined(OS_ANDROID)
+    if (output_file.empty()) {
+      TracingControllerAndroid::GenerateTracingFilePath(&output_file);
+      VLOG(0) << "Writing to output file: " << output_file.value();
+    }
+#endif
+
     file_.Initialize(output_file,
                      base::File::FLAG_CREATE_ALWAYS | base::File::FLAG_WRITE);
     if (!file_.IsValid()) {
