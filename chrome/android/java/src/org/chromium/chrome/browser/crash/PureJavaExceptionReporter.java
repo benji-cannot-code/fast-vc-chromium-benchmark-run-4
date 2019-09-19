3649bb38a6c60a06e1e7a0a7df528e5411c0744d
Joshua Peraza <jperaza@chromium.org>
2019-09-19 17:00:47 +0000

    Use an empty string for stable channel
    Since moving the Crashpad, Android native crashes use an empty string
    for the stable channel (as is done on other platforms). This patch
    makes the PureJavaExceptionReporter also use an empty string on the
    stable channel for consistency.
    
    Bug: 941326
    Change-Id: Iecb7343e6a076a12ab21d5ecc4e18ca3d1a2b1a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1811960
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698082}

--- chrome/android/java/src/org/chromium/chrome/browser/crash/PureJavaExceptionReporter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/crash/PureJavaExceptionReporter.java
@@ -181,9 +181,7 @@ public class PureJavaExceptionReporter {
         if (ChromeVersionInfo.isBetaBuild()) {
             return "beta";
         }
-        if (ChromeVersionInfo.isStableBuild()) {
-            return "stable";
-        }
+        // An empty string indicates the stable channel.
         return "";
     }
 
