520c0a1e1008aef235a3c72d5d03b448489c700d
Gyuyoung Kim <gyuyoung@igalia.com>
2019-09-19 01:39:01 +0000

    Include *.mojom-forward.h instead of .mojom.h from fetch header files
    To reduce pre-processed size of fetch_api_request_body_mojom_traits.h
    and fetch_api_request_headers_mojom_traits.h, this CL replaces
    .mojom.h with .mojom-forward.h.
    
    This CL has no behavior changes.
    
    Bug: 1001360
    Change-Id: I1360771e268e7e079b9d9f05685c7af138c4f0e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1806533
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#697867}

--- third_party/blink/common/fetch/fetch_api_request_body_mojom_traits.cc
+++ third_party/blink/common/fetch/fetch_api_request_body_mojom_traits.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/fetch/fetch_api_request_body_mojom_traits.h"
 
 #include "services/network/public/cpp/url_request_mojom_traits.h"
+#include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom.h"
 
 namespace mojo {
 
