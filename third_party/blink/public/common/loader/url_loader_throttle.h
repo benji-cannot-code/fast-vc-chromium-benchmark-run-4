128a4d71142fa4d3093b46566a4b229946bf7895
Gyuyoung Kim <gyuyoung@igalia.com>
2019-09-19 08:33:18 +0000

    Include *.mojom-forward.h instead of .mojom.h from url_loader_throttle.h
    To reduce pre-processed size of url_loader_throttle.h, this CL replaces .mojom.h
    with .mojom-forward.h.
    
    This CL has no behavior changes.
    
    Bug: 1001360
    Change-Id: I25bba6a8dc64cfe0960103588a0b871d065a76f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1806166
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697937}

--- third_party/blink/public/common/loader/url_loader_throttle.h
+++ third_party/blink/public/common/loader/url_loader_throttle.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/string_piece.h"
 #include "net/base/request_priority.h"
-#include "services/network/public/mojom/url_loader.mojom.h"
+#include "services/network/public/mojom/url_loader.mojom-forward.h"
 #include "third_party/blink/public/common/common_export.h"
 
 class GURL;
