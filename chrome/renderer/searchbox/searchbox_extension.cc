2dd2270cfe9bc75619fcdb5b49d866a5c5c680d6
Dan Beam <dbeam@chromium.org>
2019-09-19 16:15:41 +0000

    Local NTP: simplify how RTL is communicated to the page
    Also reduces flicker in cases that JS takes a sec to start.
    
    R=kmilka@chromium.org
    BUG=none
    
    Change-Id: I1527b69d5727117d331bf575912689ef7908ec3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1812000
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698047}

--- chrome/renderer/searchbox/searchbox_extension.cc
+++ chrome/renderer/searchbox/searchbox_extension.cc
@@ -540,7 +540,6 @@ class SearchBoxBindings : public gin::Wrappable<SearchBoxBindings> {
       v8::Isolate* isolate) final;
 
   // Handlers for JS properties.
-  static bool GetRightToLeft();
   static bool IsFocused();
   static bool IsKeyCaptureEnabled();
 
@@ -563,7 +562,7 @@ SearchBoxBindings::~SearchBoxBindings() = default;
 gin::ObjectTemplateBuilder SearchBoxBindings::GetObjectTemplateBuilder(
     v8::Isolate* isolate) {
   return gin::Wrappable<SearchBoxBindings>::GetObjectTemplateBuilder(isolate)
-      .SetProperty("rtl", &SearchBoxBindings::GetRightToLeft)
+      .SetProperty("rtl", &base::i18n::IsRTL)
       .SetProperty("isFocused", &SearchBoxBindings::IsFocused)
       .SetProperty("isKeyCaptureEnabled",
                    &SearchBoxBindings::IsKeyCaptureEnabled)
@@ -574,10 +573,6 @@ gin::ObjectTemplateBuilder SearchBoxBindings::GetObjectTemplateBuilder(
                  &SearchBoxBindings::StartCapturingKeyStrokes)
       .SetMethod("stopCapturingKeyStrokes",
                  &SearchBoxBindings::StopCapturingKeyStrokes);
-}
-
-bool SearchBoxBindings::GetRightToLeft() {
-  return base::i18n::IsRTL();
 }
 
 // static
