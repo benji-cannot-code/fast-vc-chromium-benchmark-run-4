3cee628b61ae76fab64c7edc8c410eb138c51361
Gyuyoung Kim <gyuyoung@igalia.com>
2019-09-19 08:30:24 +0000

    Include *.mojom-forward.h instead of .mojom.h from indexed_db_default_mojom_traits.h
    To reduce pre-processed size of indexed_db_default_mojom_traits.h,
    this CL replaces .mojom.h with .mojom-forward.h.
    
    This CL has no behavior changes.
    
    Bug: 1001360
    Change-Id: I769dbf5bd67181276bce08369c493e570c530b58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1806613
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#697936}

--- third_party/blink/common/indexeddb/indexed_db_default_mojom_traits.cc
+++ third_party/blink/common/indexeddb/indexed_db_default_mojom_traits.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/indexeddb/indexeddb_key.h"
 #include "third_party/blink/public/common/indexeddb/indexeddb_key_range.h"
 #include "third_party/blink/public/common/indexeddb/indexeddb_metadata.h"
+#include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom.h"
 
 namespace mojo {
 
--- third_party/blink/public/common/indexeddb/indexed_db_default_mojom_traits.h
+++ third_party/blink/public/common/indexeddb/indexed_db_default_mojom_traits.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "third_party/blink/public/common/common_export.h"
 #include "third_party/blink/public/common/indexeddb/indexeddb_key.h"
-#include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom.h"
+#include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom-forward.h"
 
 namespace mojo {
 
