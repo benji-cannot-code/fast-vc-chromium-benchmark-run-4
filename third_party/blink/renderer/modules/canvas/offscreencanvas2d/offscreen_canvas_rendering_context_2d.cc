5529b9f81db7d7e9bbf17cbee23289351b2d8eb4
Aaron Krajeski <aaronhk@chromium.org>
2019-09-19 17:08:15 +0000

    Check if offscreen canvas host is valid size
    As with all canvases, zero-sized offscreen canvases should not create
    resource providers. If we do, skia fails. Simply checking on
    CanCreateCanvas2dResourceProvider solves this problem.
    
    Bug: 1005201, 1004026
    Change-Id: I07949a937d8834bd8a3c8ce7a38b2fadec4c092d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810561
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698086}

--- third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
@@ -133,6 +133,8 @@ int OffscreenCanvasRenderingContext2D::Height() const {
 
 bool OffscreenCanvasRenderingContext2D::CanCreateCanvas2dResourceProvider()
     const {
+  if (!Host() || Host()->Size().IsEmpty())
+    return false;
   return !!offscreenCanvasForBinding()->GetOrCreateResourceProvider();
 }
 
