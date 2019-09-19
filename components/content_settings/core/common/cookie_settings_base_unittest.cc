233eded9ff3444abf46c48fa1c1180c524152535
Christian Dullweber <dullweber@chromium.org>
2019-09-19 10:06:05 +0000

    Fix ClearOnExit for content settings with secondary pattern
    Currently a <URL>,* exception can control whether cookies are deleted on
    exit. Exceptions that only specify a secondary pattern are ignored.
    
    While we don't know the top-frame-origin, it would still be useful to
    keep cookies from being deleted that match the url in *,<URL> pattern.
    
    Bug: 967668
    Change-Id: I98ad6f1a2d77f53c39f7e04f160e40eec2e18b06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807246
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697952}

--- components/content_settings/core/common/cookie_settings_base_unittest.cc
+++ components/content_settings/core/common/cookie_settings_base_unittest.cc
@@ -24,6 +24,13 @@ ContentSettingPatternSource CreateSetting(ContentSetting setting) {
       false);
 }
 
+ContentSettingPatternSource CreateThirdPartySetting(ContentSetting setting) {
+  return ContentSettingPatternSource(
+      ContentSettingsPattern::Wildcard(),
+      ContentSettingsPattern::FromString(kDomain), base::Value(setting),
+      std::string(), false);
+}
+
 class CallbackCookieSettings : public CookieSettingsBase {
  public:
   explicit CallbackCookieSettings(GetSettingCallback callback)
@@ -74,6 +81,13 @@ TEST(CookieSettingsBaseTest, ShouldDeleteDomainSettingSessionOnly) {
       {CreateSetting(CONTENT_SETTING_SESSION_ONLY)}, kDomain, false));
 }
 
+TEST(CookieSettingsBaseTest, ShouldDeleteDomainThirdPartySettingSessionOnly) {
+  CallbackCookieSettings settings(
+      base::BindRepeating([](const GURL&) { return CONTENT_SETTING_BLOCK; }));
+  EXPECT_TRUE(settings.ShouldDeleteCookieOnExit(
+      {CreateThirdPartySetting(CONTENT_SETTING_SESSION_ONLY)}, kDomain, false));
+}
+
 TEST(CookieSettingsBaseTest, ShouldNotDeleteDomainSettingAllow) {
   CallbackCookieSettings settings(
       base::BindRepeating([](const GURL&) { return CONTENT_SETTING_BLOCK; }));
@@ -103,6 +117,14 @@ TEST(CookieSettingsBaseTest, ShouldNotDeleteNoDomainMatch) {
       base::BindRepeating([](const GURL&) { return CONTENT_SETTING_BLOCK; }));
   EXPECT_FALSE(settings.ShouldDeleteCookieOnExit(
       {CreateSetting(CONTENT_SETTING_SESSION_ONLY)}, "other.com", false));
+}
+
+TEST(CookieSettingsBaseTest, ShouldNotDeleteNoThirdPartyDomainMatch) {
+  CallbackCookieSettings settings(
+      base::BindRepeating([](const GURL&) { return CONTENT_SETTING_BLOCK; }));
+  EXPECT_FALSE(settings.ShouldDeleteCookieOnExit(
+      {CreateThirdPartySetting(CONTENT_SETTING_SESSION_ONLY)}, "other.com",
+      false));
 }
 
 TEST(CookieSettingsBaseTest, CookieAccessNotAllowedWithBlockedSetting) {
