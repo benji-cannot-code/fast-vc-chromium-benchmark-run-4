86ead3fafb0ab594d1d903b17a7d27a7945dc277
Tomasz Wiszkowski <ender@google.com>
2019-09-19 21:09:58 +0000

    Don't use RoundedCornerImageView for RevampedContextMenu
    RoundedCornerImageView wasn't properly rounding corners for the header
    image in the revamped context menu. New UX direction is that we do not
    need rounded corners, so removing RoundedCornerImageView in favor of a
    plain ImageView.
    
    Bug: 1001209
    
    Change-Id: I3a53d8b4a38e16eba9f7b92c77d64861626b11b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1811340
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698183}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/RevampedContextMenuHeaderViewTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/RevampedContextMenuHeaderViewTest.java
@@ -16,6 +16,7 @@ import android.support.test.annotation.UiThreadTest;
 import android.support.test.filters.SmallTest;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.junit.BeforeClass;
@@ -29,7 +30,6 @@ import org.chromium.chrome.test.ui.DummyUiActivityTestCase;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
-import org.chromium.ui.widget.RoundedCornerImageView;
 
 /**
  * Tests for RevampedContextMenuHeader view and {@link RevampedContextMenuHeaderViewBinder}
@@ -45,7 +45,7 @@ public class RevampedContextMenuHeaderViewTest extends DummyUiActivityTestCase {
     private TextView mTitle;
     private TextView mUrl;
     private View mTitleAndUrl;
-    private RoundedCornerImageView mImage;
+    private ImageView mImage;
     private View mCircleBg;
     private PropertyModel mModel;
     private PropertyModelChangeProcessor mMCP;
@@ -176,8 +176,8 @@ public class RevampedContextMenuHeaderViewTest extends DummyUiActivityTestCase {
         assertThat("Incorrect circle background visibility.", mCircleBg.getVisibility(),
                 equalTo(View.VISIBLE));
 
-        assertNull("Thumbnail drawable isn't null when it should be, initially.",
-                mImage.getDrawable());
+        assertFalse("Thumbnail drawable should use fallback color initially.",
+                mImage.getDrawable() instanceof BitmapDrawable);
         final Bitmap bitmap = Bitmap.createBitmap(4, 4, Bitmap.Config.ARGB_8888);
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> mModel.set(RevampedContextMenuHeaderProperties.IMAGE, bitmap));
