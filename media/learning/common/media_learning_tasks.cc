4bda8da2479a71252a3148227a0940e8a504b7d8
liberato@chromium.org <liberato@chromium.org>
2019-09-19 16:32:14 +0000

    Added MediaLearningTasks.
    This CL just adds a place for us to define the LearningTasks for all
    the renderer-side media experiments.
    
    Change-Id: I1253506541f1bc87a859f7a212d3f0894281de66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809112
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698059}

---
+++ media/learning/common/media_learning_tasks.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/learning/common/media_learning_tasks.h"
+
+namespace media {
+namespace learning {
+
+static const LearningTask& GetWillPlayTask() {
+  static LearningTask task_;
+  if (!task_.feature_descriptions.size()) {
+    task_.name = "MediaLearningWillPlay";
+    // TODO(liberato): fill in the rest here, once we have the features picked.
+  }
+
+  return task_;
+}
+
+// static
+const LearningTask& MediaLearningTasks::Get(Id id) {
+  switch (id) {
+    case Id::kWillPlay:
+      return GetWillPlayTask();
+  }
+}
+
+// static
+void MediaLearningTasks::Register(
+    base::RepeatingCallback<void(const LearningTask&)> cb) {
+  cb.Run(Get(Id::kWillPlay));
+}
+
+}  // namespace learning
+}  // namespace media
