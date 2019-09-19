6049d42a8674ff7fdf63677a2e5bb8ced7baca87
Eric Karl <ericrk@chromium.org>
2019-09-19 17:58:47 +0000

    CC De-Jelly Logic
    Adds the CC-side logic for de-jelly. Does not yet do anything as viz
    process bits are still needed.
    
    Adds one main class: cc::DeJellyState, which is used to track movement
    of scrolling layers frame over frame. This is used to calculate a
    skew amount.
    
    Bug: 995965
    Change-Id: I6e5da5dc857c21e55ebe89c4ba9582422d0ea64b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1811458
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698109}

--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/tiles/image_decode_cache.h"
 #include "cc/tiles/tile_manager.h"
 #include "cc/trees/animated_paint_worklet_tracker.h"
+#include "cc/trees/de_jelly_state.h"
 #include "cc/trees/layer_tree_frame_sink_client.h"
 #include "cc/trees/layer_tree_host.h"
 #include "cc/trees/layer_tree_mutator.h"
@@ -1277,6 +1278,9 @@ class CC_EXPORT LayerTreeHostImpl : public InputHandler,
   // Responsible for storing animated custom property values and for
   // invalidating PaintWorklets as the property values change.
   AnimatedPaintWorkletTracker paint_worklet_tracker_;
+
+  // Helper for de-jelly logic.
+  DeJellyState de_jelly_state_;
 
   // Must be the last member to ensure this is destroyed first in the
   // destruction order and invalidates all weak pointers.
