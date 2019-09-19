a0fbbada7f501033493730cf0dcebea4b5796f77
Oksana Zhuravlova <oksamyt@chromium.org>
2019-09-19 22:06:25 +0000

    [mojo] Add rvalue qualifiers to Pending{Receiver,Remote} convenience operators
    This change adds rvalue qualifiers to InterfaceRequest() and
    InterfacePtrInfo() operators in PendindReceiver and PendingRemote
    respectively to enforce usage with std::move.
    
    Also updates existing callsites.
    
    Bug: 1002297
    Change-Id: I578e0cea03d799cbcc04e531fdb9dea96387a6bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797132
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698217}

--- services/network/network_context_unittest.cc
+++ services/network/network_context_unittest.cc
@@ -4352,7 +4352,7 @@ class MockMojoProxyResolverFactory
     mojo::MakeSelfOwnedReceiver(
         std::make_unique<MockMojoProxyResolverFactory>(),
         remote.InitWithNewPipeAndPassReceiver());
-    return remote;
+    return std::move(remote);
   }
 
  private:
