131f8ba599d1e51a56d51fa38993bcab69abd1f7
Gyuyoung Kim <gyuyoung@igalia.com>
2019-09-19 08:03:14 +0000

    Include *.mojom-forward.h instead of .mojom.h from media_stream_mojom_traits.h
    To reduce pre-processed size of media_stream_mojom_traits.h,
    this CL replaces .mojom.h with .mojom-forward.h.
    
    This CL has no behavior changes.
    
    Bug: 1001360
    Change-Id: I0222048155c83893594d85c2edf314c802d68a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808627
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#697932}

--- third_party/blink/public/common/mediastream/media_stream_mojom_traits.h
+++ third_party/blink/public/common/mediastream/media_stream_mojom_traits.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/common_export.h"
 #include "third_party/blink/public/common/mediastream/media_stream_controls.h"
 #include "third_party/blink/public/common/mediastream/media_stream_request.h"
-#include "third_party/blink/public/mojom/mediastream/media_stream.mojom.h"
+#include "third_party/blink/public/mojom/mediastream/media_stream.mojom-forward.h"
 
 namespace mojo {
 
