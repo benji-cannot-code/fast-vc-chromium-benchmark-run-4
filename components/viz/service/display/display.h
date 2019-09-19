30a040e6d2353d65df71e2e6000e1fc7f13135ff
Nathan Zabriskie <nazabris@microsoft.com>
2019-09-19 17:42:10 +0000

    Cleanup components/viz/service/display.display.cc
    The new PresentationGroupTimings class makes some fields redundant.
    This CL simply removes them.
    
    Bug: 1003892
    Change-Id: I79efe18b905d3ee6b4fc7ae7910b8dd973145b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1812132
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Nathan Zabriskie <nazabris@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#698101}

--- components/viz/service/display/display.h
+++ components/viz/service/display/display.h
@@ -186,7 +186,7 @@ class VIZ_SERVICE_EXPORT Display : public DisplaySchedulerClient,
   // presented.
   class PresentationGroupTiming {
    public:
-    explicit PresentationGroupTiming(base::TimeTicks swap_triggered_timestamp);
+    PresentationGroupTiming();
     PresentationGroupTiming(PresentationGroupTiming&& other);
     ~PresentationGroupTiming();
 
@@ -197,14 +197,11 @@ class VIZ_SERVICE_EXPORT Display : public DisplaySchedulerClient,
     bool HasSwapped() const { return !swap_timings_.is_null(); }
     void OnPresent(const gfx::PresentationFeedback& feedback);
 
-    base::TimeTicks draw_and_swap_triggered_timestamp() const {
-      return draw_and_swap_triggered_timestamp_;
+    base::TimeTicks draw_start_timestamp() const {
+      return draw_start_timestamp_;
     }
 
    private:
-    // Not currently tracked by metrics but is used for
-    // SanitizePresentationFeedback()
-    base::TimeTicks draw_and_swap_triggered_timestamp_;
     base::TimeTicks draw_start_timestamp_;
     gfx::SwapTimings swap_timings_;
     std::vector<std::unique_ptr<Surface::PresentationHelper>>
@@ -266,10 +263,6 @@ class VIZ_SERVICE_EXPORT Display : public DisplaySchedulerClient,
 
   int64_t swapped_trace_id_ = 0;
   int64_t last_presented_trace_id_ = 0;
-
-  // TODO(nazabris, crbug.com/1003892): remove this member now that
-  // PresentationGroupTiming stores the same info.
-  base::circular_deque<base::TimeTicks> draw_start_times_pending_swap_ack_;
 
   DISALLOW_COPY_AND_ASSIGN(Display);
 };
