f5d83e9e7f47041242046d1a4868d26097310613
Wez <wez@chromium.org>
2019-09-19 21:57:56 +0000

    [base] Reduce TaskEnvironment default Run() timeout.
    Reduce the default Run() timeout set by TaskEnvironment from
    TestTimeouts::action_max_timeout() to action_timeout(). This typically
    reduces the timeout from 30s (45s in Debug) down to 10s.
    
    Sheriffs: If tests start to timeout as a result of this change, please
    file a bug and update the test(s) to use action_max_timeout() instead,
    rather than reverting, if possible.
    
    For example:
    - If an individual test starts timing-out, add a
      ScopedDefaultRunTimeout to the test (e.g. see the
      SnapshotAuraTest.FullScreenWindow change in this CL).
    - If a whole suite of tests time-out, add a ScopedDefaultRunTimeout to
      the test base-class, if there is one (e.g. see the change to
      WebRtcMediaStreamTrackAdapterMapStressTest in this CL).
    - If an entire bot is affected, and you know the build configuration,
      update the constructor in task_environment.cc to restore the longer
      action_max_timeout() for that configuration, e.g:
        #if defined(OS_LINUX) && !defined(NDEBUG)
           TestTimeouts::action_max_timeout(),
        #else
           TestTimeouts::action_timeout(),
        #endif
    - If in doubt, please file a bug and revert and we'll investigate.
    
    TBR=gab,morlovich
    
    Bug: 922098, 1002716, 1002762, 1002761
    Change-Id: Ib5b043b1893c42692380ac916ed40c7f7e956598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1797203
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698212}

--- chrome/browser/offline_pages/offline_page_utils_unittest.cc
+++ chrome/browser/offline_pages/offline_page_utils_unittest.cc
@@ -45,6 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 
 #if defined(OS_ANDROID)
+#include "base/test/bind_test_util.h"
+#include "base/test/test_timeouts.h"
 #include "chrome/browser/android/download/mock_download_controller.h"
 #include "components/gcm_driver/instance_id/instance_id_android.h"
 #include "components/gcm_driver/instance_id/scoped_use_fake_instance_id_android.h"
@@ -408,6 +410,14 @@ TEST_F(OfflinePageUtilsTest, TestGetCachedOfflinePageSizeBetween) {
 }
 
 TEST_F(OfflinePageUtilsTest, TestGetCachedOfflinePageSizeNoPageInModel) {
+#if defined(OS_ANDROID)
+  // TODO(https://crbug.com/1002762): Fix this test to run in < action_timeout()
+  // on the Android bots.
+  const base::RunLoop::ScopedRunTimeoutForTest increased_run_timeout(
+      TestTimeouts::action_max_timeout(),
+      base::MakeExpectedNotRunClosure(FROM_HERE, "RunLoop::Run() timed out."));
+#endif  // defined(OS_ANDROID)
+
   clock()->Advance(base::TimeDelta::FromHours(3));
 
   // Get the size of cached offline pages between 01:00:00 and 03:00:00.
--- ui/snapshot/snapshot_aura_unittest.cc
+++ ui/snapshot/snapshot_aura_unittest.cc
@@ -11,8 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_simple_task_runner.h"
+#include "base/test/test_timeouts.h"
 #include "base/win/windows_version.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -202,6 +204,14 @@ INSTANTIATE_TEST_SUITE_P(, SnapshotAuraTest, ::testing::Bool());
 #define MAYBE_FullScreenWindow FullScreenWindow
 #endif
 TEST_P(SnapshotAuraTest, MAYBE_FullScreenWindow) {
+#if defined(OS_LINUX)
+  // TODO(https://crbug.com/1002716): Fix this test to run in < action_timeout()
+  // on the Linux Debug & TSAN bots.
+  const base::RunLoop::ScopedRunTimeoutForTest increased_run_timeout(
+      TestTimeouts::action_max_timeout(),
+      base::MakeExpectedNotRunClosure(FROM_HERE, "RunLoop::Run() timed out."));
+#endif  // defined(OS_LINUX)
+
 #if defined(OS_WIN)
   // TODO(https://crbug.com/850556): Make work on Win10.
   base::win::Version version = base::win::GetVersion();
