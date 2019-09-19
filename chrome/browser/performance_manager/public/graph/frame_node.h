a3fb91803708e911e4361d7ba7ba846a21acd711
Patrick Monette <pmonette@chromium.org>
2019-09-19 01:17:23 +0000

    Make OnURLChanged() report the previous value
    Bug: 993029
    Change-Id: I340a39e49f1ca80f874c879f28368a1c76084b4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787888
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#697848}

--- chrome/browser/performance_manager/public/graph/frame_node.h
+++ chrome/browser/performance_manager/public/graph/frame_node.h
@@ -180,7 +180,8 @@ class FrameNodeObserver {
       const InterventionPolicy& previous_value) = 0;
 
   // Invoked when the URL property changes.
-  virtual void OnURLChanged(const FrameNode* frame_node) = 0;
+  virtual void OnURLChanged(const FrameNode* frame_node,
+                            const GURL& previous_value) = 0;
 
   // Invoked when the IsAdFrame property changes.
   virtual void OnIsAdFrameChanged(const FrameNode* frame_node) = 0;
@@ -215,7 +216,8 @@ class FrameNode::ObserverDefaultImpl : public FrameNodeObserver {
   void OnOriginTrialFreezePolicyChanged(
       const FrameNode* frame_node,
       const InterventionPolicy& previous_value) override {}
-  void OnURLChanged(const FrameNode* frame_node) override {}
+  void OnURLChanged(const FrameNode* frame_node,
+                    const GURL& previous_value) override {}
   void OnIsAdFrameChanged(const FrameNode* frame_node) override {}
   void OnNonPersistentNotificationCreated(
       const FrameNode* frame_node) override {}
