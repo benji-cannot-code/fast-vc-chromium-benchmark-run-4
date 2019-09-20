a736a0fa11ff37037301173708bd3a46059e3f3a
Alexandre Frechette <frechette@chromium.org>
2019-09-20 15:09:01 +0000

    Adding fluent language model to iOS.
    Change-Id: Ie542c2ea632f7bddf50914f7af611bb5911ca427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1798823
    Reviewed-by: Alexandre Frechette <frechette@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Alexandre Frechette <frechette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698484}

--- ios/chrome/browser/language/language_model_manager_factory.cc
+++ ios/chrome/browser/language/language_model_manager_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/language/core/browser/baseline_language_model.h"
+#include "components/language/core/browser/fluent_language_model.h"
 #include "components/language/core/browser/heuristic_language_model.h"
 #include "components/language/core/browser/language_model.h"
 #include "components/language/core/browser/language_model_manager.h"
@@ -27,6 +28,14 @@ void PrepareLanguageModels(ios::ChromeBrowserState* const chrome_state,
   // Create and set the primary Language Model to use based on the state of
   // experiments.
   switch (language::GetOverrideLanguageModel()) {
+    case language::OverrideLanguageModel::FLUENT:
+      manager->AddModel(
+          language::LanguageModelManager::ModelType::FLUENT,
+          std::make_unique<language::FluentLanguageModel>(
+              chrome_state->GetPrefs(), language::prefs::kAcceptLanguages));
+      manager->SetPrimaryModel(
+          language::LanguageModelManager::ModelType::FLUENT);
+      break;
     case language::OverrideLanguageModel::HEURISTIC:
       manager->AddModel(language::LanguageModelManager::ModelType::HEURISTIC,
                         std::make_unique<language::HeuristicLanguageModel>(
