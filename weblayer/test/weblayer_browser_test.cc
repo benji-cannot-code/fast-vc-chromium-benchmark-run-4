6ebf2d2a2881222c41ea6c0ad1feaec0957db503
John Abd-El-Malek <jam@chromium.org>
2019-09-20 00:14:01 +0000

    Run weblayer_browsertests on Mojo Linux & Mojo Windows bots.
    Change-Id: Ie4236b7e6e925cb7ef7272678c9824f7ec07eca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1811970
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698260}

--- weblayer/test/weblayer_browser_test.cc
+++ weblayer/test/weblayer_browser_test.cc
@@ -28,6 +28,8 @@ void WebLayerBrowserTest::PreRunTestOnMainThread() {
   shell_ = Shell::windows()[0];
 }
 
-void WebLayerBrowserTest::PostRunTestOnMainThread() {}
+void WebLayerBrowserTest::PostRunTestOnMainThread() {
+  Shell::CloseAllWindows();
+}
 
 }  // namespace weblayer
