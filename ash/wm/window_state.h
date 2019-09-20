325830a19c4f6f4a85c1233dd711cf51ea22f20f
Eliot Courtney <edcourtney@google.com>
2019-09-20 05:31:28 +0000

    Add metric for Android PIP use time.
      different amounts of time and verifying histogram via
      chrome://histograms
    
    Bug: b/121299293
    Test: added unit test
    Test: manual test via entering Android PIP, then dismissing after
    Change-Id: I009047ae38990dde4c7c0f966d930c80ff4880d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1800854
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Mitsuru Oshima (OOO - back on 9/24) <oshima@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698369}

--- ash/wm/window_state.h
+++ ash/wm/window_state.h
@@ -440,6 +440,9 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
   // keyboard position changes.
   void UpdatePipBounds();
 
+  // Collects PIP enter and exit metrics:
+  void CollectPipEnterExitMetrics(bool enter);
+
   // aura::WindowObserver:
   void OnWindowPropertyChanged(aura::Window* window,
                                const void* key,
@@ -492,6 +495,9 @@ class ASH_EXPORT WindowState : public aura::WindowObserver {
   // The animation type for the bounds change.
   BoundsChangeAnimationType bounds_animation_type_ =
       BoundsChangeAnimationType::DEFAULT;
+
+  // When the current (or last) PIP session started.
+  base::TimeTicks pip_start_time_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowState);
 };
