b3ce05cd5ae7715b46a8d209ae403226c9895242
Sven Zheng <svenzheng@chromium.org>
2019-09-20 23:08:06 +0000

    Extract a base class for live tests
    All new live tests should inherit from LiveTest.
    
    Bug: 992143
    Change-Id: Ic8f0d05275a2f3fd4ab8eec40d8cb21c5215f8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1767087
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698662}

---
+++ chrome/browser/signin/e2e_tests/live_test.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SIGNIN_E2E_TESTS_LIVE_TEST_H_
+#define CHROME_BROWSER_SIGNIN_E2E_TESTS_LIVE_TEST_H_
+
+#include "chrome/browser/signin/e2e_tests/test_accounts_util.h"
+#include "chrome/test/base/in_process_browser_test.h"
+
+namespace signin {
+namespace test {
+
+class LiveTest : public InProcessBrowserTest {
+ protected:
+  void SetUpInProcessBrowserTestFixture() override;
+  void SetUp() override;
+
+  const TestAccountsUtil* GetTestAccountsUtil() const {
+    return &test_accounts_;
+  }
+
+ private:
+  TestAccountsUtil test_accounts_;
+};
+
+}  // namespace test
+}  // namespace signin
+
+#endif  // CHROME_BROWSER_SIGNIN_E2E_TESTS_LIVE_TEST_H_
