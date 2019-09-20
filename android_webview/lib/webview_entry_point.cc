acd93abe1dae7bf721a8495021f9cbd932fada63
Eric Stevenson <estevenson@chromium.org>
2019-09-20 04:17:00 +0000

    Revert "[WebLayer] Load WebLayer from WebView"
    This reverts commit be5bd323d0ca687b0b685f5251a3811b4da68235.
    
    Reason for revert: crbug.com/1006100
    
    Original change's description:
    > [WebLayer] Load WebLayer from WebView
    > 
    > This adds WebLayer to WebView, allowing us to load the WebLayer
    > implementation from a monochrome APK (or other APK which includes
    > WebView).
    > 
    > Run the shell loading from MonochromePublic.apk with:
    > 
    >   $ autoninja -C out/Android run_weblayer_shell_monochrome
    >   $ out/Android/bin/run_weblayer_shell_monochrome
    > 
    > Run the shell loading from TrichromeWebView.apk with:
    > 
    >   $ autoninja -C out/Android run_weblayer_shell_trichrome
    >   $ out/Android/bin/run_weblayer_shell_trichrome
    > 
    > Change-Id: Ief31c2b79edae6b697ca2927fd6eccbf255515b3
    > Binary-Size: Increase needed for implementing WebLayer, see go/weblayer-dev-intro
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810329
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698321}
    
    TBR=dtrainor@chromium.org,torne@chromium.org,cduvall@chromium.org
    
    Change-Id: Icbd70716d1bf1808fda17b5f0af25231d5e969d6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815604
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698353}

--- android_webview/lib/webview_entry_point.cc
+++ android_webview/lib/webview_entry_point.cc
@@ -6,19 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/lib/webview_jni_onload.h"
 #include "base/android/jni_android.h"
 #include "base/android/library_loader/library_loader_hooks.h"
-#include "weblayer/app/jni_onload.h"
 
 namespace {
 
-bool NativeInit(base::android::LibraryProcessType library_process_type) {
-  switch (library_process_type) {
-    case base::android::PROCESS_WEBLAYER:
-    case base::android::PROCESS_WEBLAYER_CHILD:
-      return weblayer::OnJNIOnLoadInit("resources.pak");
-      break;
-    default:
-      return android_webview::OnJNIOnLoadInit();
-  }
+bool NativeInit(base::android::LibraryProcessType) {
+  return android_webview::OnJNIOnLoadInit();
 }
 
 }  // namespace
