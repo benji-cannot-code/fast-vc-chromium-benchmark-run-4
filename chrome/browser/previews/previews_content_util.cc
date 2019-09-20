1335152b5e93179daebe41d1743c06b3655a13a5
Robert Ogden <robertogden@chromium.org>
2019-09-20 22:41:26 +0000

    Remove PreviewsLitePageNavigationThrottleManager
    Bug: 1005295
    Change-Id: Ie301aae1507a6fa0f020fe302386458d924c4256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1811959
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698653}

--- chrome/browser/previews/previews_content_util.cc
+++ chrome/browser/previews/previews_content_util.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/data_reduction_proxy/data_reduction_proxy_chrome_settings_factory.h"
 #include "chrome/browser/previews/previews_lite_page_decider.h"
 #include "chrome/browser/previews/previews_lite_page_navigation_throttle.h"
-#include "chrome/browser/previews/previews_lite_page_navigation_throttle_manager.h"
 #include "chrome/browser/previews/previews_lite_page_url_loader_interceptor.h"
 #include "chrome/browser/previews/previews_offline_helper.h"
 #include "chrome/browser/previews/previews_service.h"
--- chrome/browser/previews/previews_lite_page_serving_url_loader.cc
+++ chrome/browser/previews/previews_lite_page_serving_url_loader.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "chrome/browser/previews/previews_lite_page_decider.h"
-#include "chrome/browser/previews/previews_lite_page_navigation_throttle_manager.h"
 #include "chrome/browser/previews/previews_lite_page_url_loader_interceptor.h"
 #include "chrome/browser/previews/previews_service.h"
 #include "chrome/browser/previews/previews_service_factory.h"
