14f09a548873020a5761a074f9432882bb410847
Charlene Yan <cyan@chromium.org>
2019-09-20 00:34:45 +0000

    Store Group metadata (title and color) for reverting a drag.
    Bug: 1004346
    Change-Id: I8f41c8ccaf93f882f54a6503f0d5e8a12d37c224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808245
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698274}

--- chrome/browser/ui/views/tabs/tab_drag_controller.h
+++ chrome/browser/ui/views/tabs/tab_drag_controller.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/ui/tabs/tab_group_visual_data.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/tabs/tab_drag_context.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_types.h"
@@ -237,8 +238,15 @@ class TabDragController : public views::WidgetObserver {
     // Is the tab pinned?
     bool pinned;
 
-    // Stores the group the tab is in, or nullopt if tab is not grouped.
-    base::Optional<TabGroupId> group_id;
+    // Contains the information for the tab's group at the start of the drag.
+    struct TabGroupData {
+      TabGroupId group_id;
+      TabGroupVisualData group_visual_data;
+    };
+
+    // Stores the information of the group the tab is in, or nullopt if tab is
+    // not grouped.
+    base::Optional<TabGroupData> tab_group_data;
 
    private:
     DISALLOW_COPY_AND_ASSIGN(TabDragData);
