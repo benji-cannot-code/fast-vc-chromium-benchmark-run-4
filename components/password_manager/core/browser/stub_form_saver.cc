2ba8a4bae384e569044b7394f8b39fd7e3566e86
Mohamed Amir Yosef <mamir@chromium.org>
2019-09-20 15:32:48 +0000

    [Passwords] Use PasswordStore.Unblacklist() inside PasswordFormManager
    This CL removes all the complexity of unblacklisting listing an origin
    and uses the recently introduce Unblacklist() method inside
    PasswordStore.
    
    Change-Id: Ia66625be452e8545db44e86d6c0380d55ed13a5a
    Bug: 1004783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810898
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698495}

--- components/password_manager/core/browser/stub_form_saver.cc
+++ components/password_manager/core/browser/stub_form_saver.cc
@@ -12,6 +12,8 @@ autofill::PasswordForm StubFormSaver::PermanentlyBlacklist(
   return autofill::PasswordForm();
 }
 
+void StubFormSaver::Unblacklist(const PasswordStore::FormDigest& digest) {}
+
 std::unique_ptr<FormSaver> StubFormSaver::Clone() {
   return nullptr;
 }
