5c25a1d1cff341706361455fa85a57553395c37e
Matt Menke <mmenke@chromium.org>
2019-09-20 17:56:32 +0000

    Remove URLRequest::identifier().
    This field isn't used outside of tests and a debug line in an
    unmaintained example QUIC proxy.
    
    Bug: 89321, 934009
    Change-Id: I579df4e9c7ec5f90d9b3878be6e0828047714819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814663
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698544}

--- net/tools/quic/quic_http_proxy_backend_stream.cc
+++ net/tools/quic/quic_http_proxy_backend_stream.cc
@@ -198,7 +198,6 @@ void QuicHttpProxyBackendStream::SendRequestOnBackendThread() {
   url_request_->Start();
   VLOG(1) << "Quic Proxy Sending Request to Backend for quic_conn_id: "
           << quic_connection_id_ << " quic_stream_id: " << quic_stream_id_
-          << " backend_req_id: " << url_request_->identifier()
           << " url: " << url_;
 }
 
@@ -211,7 +210,7 @@ void QuicHttpProxyBackendStream::OnReceivedRedirect(
   // Do not defer redirect, retry again from the proxy with the new url
   *defer_redirect = false;
   LOG(ERROR) << "Received Redirect from Backend "
-             << " BackendReqId: " << request->identifier() << " redirectUrl: "
+             << " redirectUrl: "
              << redirect_info.new_url.possibly_invalid_spec().c_str()
              << " RespCode " << request->GetResponseCode();
 }
@@ -242,7 +241,7 @@ void QuicHttpProxyBackendStream::OnResponseStarted(net::URLRequest* request,
   DCHECK_NE(net::ERR_IO_PENDING, net_error);
   if (net_error != net::OK) {
     LOG(ERROR) << "OnResponseStarted Error from Backend "
-               << url_request_->identifier() << " url: "
+               << " url: "
                << url_request_->url().possibly_invalid_spec().c_str()
                << " RespError " << net::ErrorToString(net_error);
     OnResponseCompleted();
@@ -266,9 +265,9 @@ void QuicHttpProxyBackendStream::OnReadCompleted(net::URLRequest* unused,
                                                  int bytes_read) {
   DCHECK_EQ(url_request_.get(), unused);
   LOG(INFO) << "OnReadCompleted Backend with"
-            << " ReqId: " << url_request_->identifier() << " RespCode "
-            << url_request_->GetResponseCode() << " RcvdBytesCount "
-            << bytes_read << " RcvdTotalBytes " << data_received_.size();
+            << " RespCode " << url_request_->GetResponseCode()
+            << " RcvdBytesCount " << bytes_read << " RcvdTotalBytes "
+            << data_received_.size();
 
   if (bytes_read > 0) {
     data_received_.append(buf_->data(), bytes_read);
@@ -289,7 +288,6 @@ void QuicHttpProxyBackendStream::OnResponseCompleted() {
   DCHECK(!response_completed_);
   LOG(INFO) << "Quic Proxy Received Response from Backend for quic_conn_id: "
             << quic_connection_id_ << " quic_stream_id: " << quic_stream_id_
-            << " backend_req_id: " << url_request_->identifier()
             << " url: " << url_;
 
   // ToDo Stream the response
