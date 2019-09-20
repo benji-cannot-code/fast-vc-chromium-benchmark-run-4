6d4e70a0c03b2907f00ff7cd4ce1e96b8c564831
Luum Habtemariam <luum@chromium.org>
2019-09-20 01:12:02 +0000

    Limit spoofed CUPS-Get-Printers response to 40
    PluginVm has an internal printer limit of 40, so we limit the printers
    we serve. Current strategy is to serve as many saved printers as
    possible, backfilled by enterprise printers in alphabetical order by
    display name.
    
    Bug: chromium:945409
    Test: unittests pass
    Change-Id: Ic9204584c29bc20302ead8775b6cbba60047e9f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804601
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Luum Habtemariam <luum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698289}

--- chrome/services/cups_proxy/fake_cups_proxy_service_delegate.cc
+++ chrome/services/cups_proxy/fake_cups_proxy_service_delegate.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cups_proxy {
 
-std::vector<chromeos::Printer> FakeCupsProxyServiceDelegate::GetPrinters() {
+std::vector<chromeos::Printer> FakeCupsProxyServiceDelegate::GetPrinters(
+    chromeos::PrinterClass printer_class) {
   return {};
 }
 
