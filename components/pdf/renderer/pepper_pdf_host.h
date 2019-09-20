64468073ddba09baeec7c90277c7991f2ad8666a
Julie Jeongeun Kim <jkim@igalia.com>
2019-09-20 00:29:01 +0000

    Convert pdf.mojom to new Mojo types
    This CL converts PdfListener and PdfService to new Mojo types.
    
    It uses AssociatedRemote, Remote, and Receiver instead of
    PdfServiceAssociatedPtr, PdfListenerPtr, and Binding.
    
    Bug: 955171
    Change-Id: I5d9ce284dc39248b567ed1666df8124b7c2a664d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810507
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#698268}

--- components/pdf/renderer/pepper_pdf_host.h
+++ components/pdf/renderer/pepper_pdf_host.h
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "components/pdf/common/pdf.mojom.h"
 #include "ipc/ipc_platform_file.h"
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/associated_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "ppapi/c/ppb_image_data.h"
 #include "ppapi/c/private/ppb_pdf.h"
 #include "ppapi/host/resource_host.h"
@@ -138,8 +139,8 @@ class PepperPDFHost : public ppapi::host::ResourceHost,
 
   std::unique_ptr<PdfAccessibilityTree> pdf_accessibility_tree_;
   content::RendererPpapiHost* const host_;
-  mojom::PdfServiceAssociatedPtr remote_pdf_service_;
-  mojo::Binding<mojom::PdfListener> binding_;
+  mojo::AssociatedRemote<mojom::PdfService> remote_pdf_service_;
+  mojo::Receiver<mojom::PdfListener> receiver_{this};
 
   DISALLOW_COPY_AND_ASSIGN(PepperPDFHost);
 };
