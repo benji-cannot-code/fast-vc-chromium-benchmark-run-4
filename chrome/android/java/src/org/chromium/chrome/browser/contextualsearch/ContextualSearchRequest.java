1ffd623eb340338149411edd7e17e98aee52eb2c
Eric Stevenson <estevenson@chromium.org>
2019-09-20 22:25:33 +0000

    Reland "JNI refactor: @NativeMethods conversion for more //chrome classes."
    This reverts commit 611c2959a066552b624c8d98802a30236862a0bf.
    
    Reason for revert: Relanding without breaking downstream by keeping
    nativeIsGoogleSearchUrl around until downstream use is removed.
    
    Original change's description:
    > Revert "JNI refactor: @NativeMethods conversion for more //chrome classes."
    >
    > This reverts commit 266b083ee2368895f012532eab5c907c7df2396d.
    >
    > Reason for revert: Compile break on android-builder-perf: https://ci.chromium.org/p/chrome/builders/ci/android-builder-perf/113718
    >
    > Original change's description:
    > > JNI refactor: @NativeMethods conversion for more //chrome classes.
    > >
    > > This CL was partially generated by
    > > //base/android/jni_generator/jni_refactorer.py.
    > >
    > > Two classes converted in this CL:
    > >   * UrlUtilities.java
    > >   * DevUiModuleProvider.java
    > >
    > > The tests were manually refactored to not use robolectric shadows
    > > for UrlUtilities native methods.
    > >
    > > Bug: 929661
    > > Change-Id: Id3fbe2f7e53f3c0aa454aa51e1e94a2a6f0f1cb6
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815344
    > > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    > > Auto-Submit: Eric Stevenson <estevenson@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#698556}
    >
    > TBR=agrieve@chromium.org,estevenson@chromium.org,tiborg@chromium.org
    >
    > Change-Id: I2c9251ec65af0ab7ff5012d218c3860f14214e9d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 929661
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816970
    > Reviewed-by: Nate Chapin <japhet@chromium.org>
    > Commit-Queue: Nate Chapin <japhet@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698594}
    
    TBR=japhet@chromium.org,agrieve@chromium.org,estevenson@chromium.org,tiborg@chromium.org
    
    Bug: 929661
    Change-Id: I0fe8d84b9fd1992d598fa7630b1f42042dca3279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816728
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698646}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchRequest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchRequest.java
@@ -11,7 +11,7 @@ import androidx.annotation.Nullable;
 
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.search_engines.TemplateUrlServiceFactory;
-import org.chromium.chrome.browser.util.UrlUtilities;
+import org.chromium.chrome.browser.util.UrlUtilitiesJni;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -235,7 +235,7 @@ class ContextualSearchRequest {
      */
     @VisibleForTesting
     boolean isGoogleUrl(@Nullable String someUrl) {
-        return !TextUtils.isEmpty(someUrl) && UrlUtilities.nativeIsGoogleSubDomainUrl(someUrl);
+        return !TextUtils.isEmpty(someUrl) && UrlUtilitiesJni.get().isGoogleSubDomainUrl(someUrl);
     }
 
     /**
