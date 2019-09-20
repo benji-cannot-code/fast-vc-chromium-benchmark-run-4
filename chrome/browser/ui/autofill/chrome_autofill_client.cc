6cc95d2ac9862e2a9d56362ff1c43f66be8ba5c4
Aidan Beggs <beggs@google.com>
2019-09-20 17:42:38 +0000

    Change autofill trust logic to match omnibox security level shown.
    Before this change, autofill had its own logic for determining a page
    was secure. This CL changes the behavior to match the security state
    shown in the omnibox.
    
    Bug: 701018
    Change-Id: Idd38fc77f93155412961c29a85f237c42ad21563
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808226
    Commit-Queue: Aidan Beggs <beggs@google.com>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698536}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_requirements_service.h"
 #include "components/prefs/pref_service.h"
+#include "components/security_state/core/security_state.h"
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -502,20 +503,15 @@ void ChromeAutofillClient::DidFillOrPreviewField(
 }
 
 bool ChromeAutofillClient::IsContextSecure() {
-  content::SSLStatus ssl_status;
-  content::NavigationEntry* navigation_entry =
-      web_contents()->GetController().GetLastCommittedEntry();
-  if (!navigation_entry)
-    return false;
+  // Note: Defer to SecurityStateTabHelper to determine what pages
+  // are secure so that autofill behavior matches that shown in the omnibox.
 
-  ssl_status = navigation_entry->GetSSL();
-  // Note: If changing the implementation below, also change
-  // AwAutofillClient::IsContextSecure. See crbug.com/505388
-  return navigation_entry->GetURL().SchemeIsCryptographic() &&
-         ssl_status.certificate &&
-         !net::IsCertStatusError(ssl_status.cert_status) &&
-         !(ssl_status.content_status &
-           content::SSLStatus::RAN_INSECURE_CONTENT);
+  SecurityStateTabHelper* helper =
+      SecurityStateTabHelper::FromWebContents(web_contents());
+
+  // There may be no SecurityStateTabHelper attached in some tests.
+  return helper &&
+         security_state::IsSslCertificateValid(helper->GetSecurityLevel());
 }
 
 bool ChromeAutofillClient::ShouldShowSigninPromo() {
