8af0cfb7a3dc2cacf9cdfd1fa49235f6dabefde4
Peter Boström <pbos@chromium.org>
2019-09-20 22:18:00 +0000

    Use TabStrip as Tabs' context-menu controller
    Speculative fix for UAF when Tabs are being destroyed while their
    context menu is still active. This destruction seems to happen due to
    nested event loops running during context menus where tab-removal
    animations can finish and delete a Tab while its context menu is still
    running.
    
    This ends up cleaner than prior as Tab would always delegate to
    TabStrip, now TabStrip gets called directly, removing one step in the
    call chain. It also removes a method from the TabController interface.
    
    Before this change Tab was its own ContextMenuController and
    ContextMenuController::ShowContextMenuForView() accesses |is_opening_|
    after ShowContextMenuForViewImpl returns, hence the use-after-free.
    
    After this change TabStrip is the context-menu-controller. TabStrip is
    expected to outlive any Tab context menus here, which would take care of
    this specific use-after-free.
    
    This implementation is still brittle as
    BrowserTabStripController::TabContextMenuContents holds on to a dangling
    pointer, though this seems OK for now as tabstrip_->GetModelIndexOfTab()
    is checked, so the pointer isn't dereferenced as it won't belong to any
    TabStrip. A follow-up change here might track the Tab pointer using a
    ViewTracker.
    
    Bug: chromium:976318, chromium:1003313
    Change-Id: I4c9a6b16ca12e695fdf055a995d526f6da1a0712
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816971
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698644}

--- chrome/browser/ui/views/tabs/tab.h
+++ chrome/browser/ui/views/tabs/tab.h
@@ -51,7 +51,6 @@ class View;
 ///////////////////////////////////////////////////////////////////////////////
 class Tab : public gfx::AnimationDelegate,
             public views::ButtonListener,
-            public views::ContextMenuController,
             public views::MaskedTargeterDelegate,
             public views::ViewObserver,
             public TabSlotView {
@@ -74,11 +73,6 @@ class Tab : public gfx::AnimationDelegate,
 
   // views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
-
-  // views::ContextMenuController:
-  void ShowContextMenuForViewImpl(views::View* source,
-                                  const gfx::Point& point,
-                                  ui::MenuSourceType source_type) override;
 
   // views::MaskedTargeterDelegate:
   bool GetHitTestMask(SkPath* mask) const override;
