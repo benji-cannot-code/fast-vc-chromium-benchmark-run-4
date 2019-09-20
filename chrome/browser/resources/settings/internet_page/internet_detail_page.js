e3ddb606cd947da35bf15628370f7416d054fd94
Steven Bennetts <stevenjb@chromium.org>
2019-09-20 01:44:27 +0000

    Settings: Fix VPN provider details
    This CL:
    * Displays the VPN Provider name for third party VPNs in the details
      page and fixes localization.
    * Provides VPN provider details in ManagedProperties for the details
      page.
    * Creates providers for all configured VPNs, regardless of whether
      provider details exist so that all ARCVPN networks show up.
    
    
    Bug: 995583
    Change-Id: I249eb228dc44c44451de5fbba7a7dc5563b04bf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815082
    Auto-Submit: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698299}

--- chrome/browser/resources/settings/internet_page/internet_detail_page.js
+++ chrome/browser/resources/settings/internet_page/internet_detail_page.js
@@ -1349,6 +1349,7 @@ Polymer({
           break;
         case mojom.VpnType.kArc:
           fields.push('vpn.type');
+          fields.push('vpn.providerName');
           break;
         case mojom.VpnType.kOpenVPN:
           fields.push(
