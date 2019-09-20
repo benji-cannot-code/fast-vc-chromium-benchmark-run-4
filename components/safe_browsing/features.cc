9298d00808fcba985d98e2c44cddb928ffc12d33
Bettina <bdea@chromium.org>
2019-09-20 18:29:44 +0000

    Create an experiment to send sample pings.
    Creating an experiment to allow sending sample pings of
    allowlist domains to Safe Browsing. Also updating the
    request proto to specify if it's a full or sample ping.
    
    Bug: 953106
    Change-Id: Iedf4ec390fca9697c2d57c001e7f46a5e8fa4b5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814957
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698561}

--- components/safe_browsing/features.cc
+++ components/safe_browsing/features.cc
@@ -39,6 +39,9 @@ const base::Feature kCaptureSafetyNetId{"SafeBrowsingCaptureSafetyNetId",
 const base::Feature kCommittedSBInterstitials{
     "SafeBrowsingCommittedInterstitials", base::FEATURE_DISABLED_BY_DEFAULT};
 
+const base::Feature kDeepScanningOfDownloads{
+    "SafeBrowsingDeepScanningOfDownloads", base::FEATURE_ENABLED_BY_DEFAULT};
+
 const base::Feature kForceUseAPDownloadProtection{
     "ForceUseAPDownloadProtection", base::FEATURE_DISABLED_BY_DEFAULT};
 
@@ -63,6 +66,13 @@ const base::Feature kSendOnFocusPing {
 };
 #endif
 
+const base::Feature kSendSampledPingsForAllowlistDomains{
+    "SafeBrowsingSendSampledPingsForAllowlistDomain",
+    base::FEATURE_DISABLED_BY_DEFAULT};
+
+constexpr base::FeatureParam<bool> kShouldFillOldPhishGuardProto{
+    &kPasswordProtectionForSignedInUsers, "DeprecateOldProto", false};
+
 const base::Feature kSuspiciousSiteTriggerQuotaFeature{
     "SafeBrowsingSuspiciousSiteTriggerQuota", base::FEATURE_ENABLED_BY_DEFAULT};
 
@@ -78,12 +88,6 @@ const base::Feature kUseLocalBlacklistsV2{"SafeBrowsingUseLocalBlacklistsV2",
 
 const base::Feature kUploadForMalwareCheck{"SafeBrowsingUploadForMalwareCheck",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kDeepScanningOfDownloads{
-    "SafeBrowsingDeepScanningOfDownloads", base::FEATURE_ENABLED_BY_DEFAULT};
-
-constexpr base::FeatureParam<bool> kShouldFillOldPhishGuardProto{
-    &kPasswordProtectionForSignedInUsers, "DeprecateOldProto", false};
 
 namespace {
 // List of Safe Browsing features. Boolean value for each list member should be
@@ -105,6 +109,7 @@ constexpr struct {
     {&kRealTimeUrlLookupEnabled, true},
     {&kRealTimeUrlLookupFetchAllowlist, true},
     {&kSendOnFocusPing, true},
+    {&kSendSampledPingsForAllowlistDomains, false},
     {&kSuspiciousSiteTriggerQuotaFeature, true},
     {&kThreatDomDetailsTagAndAttributeFeature, false},
     {&kTriggerThrottlerDailyQuotaFeature, false},
