3bf1765fc107683eb9d2500e8978daba3afee6f8
Andrew Moylan <amoylan@chromium.org>
2019-09-20 05:57:31 +0000

    Use 20190521 model for SmartDimModelV3
    Change the model that SmartDimModelV3 flag uses.
    This flag is not enabled via Finch for any users yet.
    
    * Add 20190521 preprocessor .pb file
    * Remove 20190221 preprocessor .pb file
    
    	flag through chrome://flags, and manually invoked Smart Dim via
    	its D-Bus interface.
    
    Bug: chromium:937063
    Change-Id: Iac3a1613c0d3ce2af830e6eedda1cfa13eec9d31
    Tested: Pushed updated Chrome to DUT, enabled the SmartDimV3 feature
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1813538
    Auto-Submit: Andrew Moylan <amoylan@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698378}

--- chrome/browser/chromeos/power/ml/smart_dim/ml_service_client.cc
+++ chrome/browser/chromeos/power/ml/smart_dim/ml_service_client.cc
@@ -134,7 +134,7 @@ void MlServiceClientImpl::InitMlServiceHandlesIfNeeded() {
     // Load the model.
     ModelSpecPtr spec =
         ModelSpec::New(base::FeatureList::IsEnabled(features::kSmartDimModelV3)
-                           ? ModelId::SMART_DIM_20190221
+                           ? ModelId::SMART_DIM_20190521
                            : ModelId::SMART_DIM_20181115);
     chromeos::machine_learning::ServiceConnection::GetInstance()->LoadModel(
         std::move(spec), mojo::MakeRequest(&model_),
