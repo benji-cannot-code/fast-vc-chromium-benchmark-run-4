9973fbf96414342c8454daf06c6bee807299f0bb
Marc Treib <treib@chromium.org>
2019-09-20 09:59:30 +0000

    Revert "Enable the flag of scrollable shelf by default"
    This reverts commit d76a27dd1331bee0691059d28e38f8183d08e22c.
    
    Reason for revert: Seems to have broken ~all the browser_tests and interactive_ui_tests on ChromeOS, see crbug.com/1006165
    
    Original change's description:
    > Enable the flag of scrollable shelf by default
    > 
    > This CL enables scrollable shelf as default. It will be landed after
    > discussion with other team members.
    > 
    > Bug: 1002576
    > Change-Id: I4dab5b81bf6418d5cc066f2763b4b5862ebff716
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1790755
    > Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    > Reviewed-by: Manu Cornet <manucornet@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698284}
    
    TBR=manucornet@chromium.org,andrewxu@chromium.org
    
    Change-Id: Ie19273c5fd4187c67dc0646278e04eb04fce3eb8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1002576, 1006165
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816540
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698425}

--- chromeos/constants/chromeos_switches.cc
+++ chromeos/constants/chromeos_switches.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/metrics/field_trial.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "third_party/icu/source/common/unicode/locid.h"
 
 namespace chromeos {
@@ -459,6 +458,9 @@ const char kShelfHotseat[] = "shelf-hotseat";
 // App window previews when hovering over the shelf.
 const char kShelfHoverPreviews[] = "shelf-hover-previews";
 
+// Scrollable list of apps on the shelf.
+const char kShelfScrollable[] = "shelf-scrollable";
+
 // If true, files in Android internal storage will be shown in Files app.
 const char kShowAndroidFilesInFilesApp[] = "show-android-files-in-files-app";
 
@@ -559,8 +561,7 @@ bool ShouldShowScrollableShelf() {
   // If we're showing the new shelf design, also enable scrollable shelf.
   if (ShouldShowShelfHotseat())
     return true;
-
-  return base::FeatureList::IsEnabled(features::kShelfScrollable);
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(kShelfScrollable);
 }
 
 bool ShouldTetherHostScansIgnoreWiredConnections() {
