f694f5f91e1c3b69b7c7eb80276008950fca9de3
Christopher Gunadi <chrisgunadi@google.com>
2019-09-20 11:37:56 +0000

    Set Crostini Ansible software dialog to observe Ansible management service
    Wire up dialog to Ansible management service. States are added to modify
    strings and buttons based on events that happened.
    
    Tests for successful and unsuccessful dialog flow.
    
    TEST: browser_tests --gtest_filter="*CrostiniAnsibleSoftware*"
    BUG: 998097
    Change-Id: I19d0693a068efeb5b9b6af7a0b04c4ee0ad38ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1789229
    Commit-Queue: Christopher Gunadi <chrisgunadi@google.com>
    Reviewed-by: Olya Kalitova <okalitova@chromium.org>
    Reviewed-by: Alex Oldemeier <aoldemeier@chromium.org>
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698444}

--- chrome/browser/chromeos/crostini/ansible/ansible_management_service.cc
+++ chrome/browser/chromeos/crostini/ansible/ansible_management_service.cc
@@ -71,6 +71,8 @@ void AnsibleManagementService::InstallAnsibleInDefaultContainer(
   DCHECK(ansible_installation_finished_callback_.is_null());
   ansible_installation_finished_callback_ = std::move(callback);
 
+  // TODO(chrisgunadi): Show Ansible software config dialog.
+
   CrostiniManager::GetForProfile(profile_)->InstallLinuxPackageFromApt(
       kCrostiniDefaultVmName, kCrostiniDefaultContainerName,
       kCrostiniDefaultAnsibleVersion,
@@ -84,6 +86,9 @@ void AnsibleManagementService::OnInstallAnsibleInDefaultContainer(
   if (result == CrostiniResult::INSTALL_LINUX_PACKAGE_FAILED) {
     LOG(ERROR) << "Ansible installation failed";
     std::move(ansible_installation_finished_callback_).Run(/*success=*/false);
+    for (auto& observer : observers_) {
+      observer.OnError();
+    }
     return;
   }
 
@@ -108,6 +113,9 @@ void AnsibleManagementService::OnInstallLinuxPackageProgress(
       return;
     case InstallLinuxPackageProgressStatus::FAILED:
       std::move(ansible_installation_finished_callback_).Run(/*success=*/false);
+      for (auto& observer : observers_) {
+        observer.OnError();
+      }
       return;
     // TODO(okalitova): Report Ansible downloading/installation progress.
     case InstallLinuxPackageProgressStatus::DOWNLOADING:
@@ -138,6 +146,9 @@ void AnsibleManagementService::ApplyAnsiblePlaybookToDefaultContainer(
     LOG(ERROR)
         << "Attempted to apply playbook when progress signal not connected.";
     std::move(callback).Run(/*success=*/false);
+    for (auto& observer : observers_) {
+      observer.OnError();
+    }
     return;
   }
 
@@ -149,6 +160,10 @@ void AnsibleManagementService::ApplyAnsiblePlaybookToDefaultContainer(
   request.set_vm_name(std::move(kCrostiniDefaultVmName));
   request.set_container_name(std::move(kCrostiniDefaultContainerName));
   request.set_playbook(std::move(playbook));
+
+  for (auto& observer : observers_) {
+    observer.OnApplicationStarted();
+  }
 
   GetCiceroneClient()->ApplyAnsiblePlaybook(
       std::move(request),
@@ -162,6 +177,9 @@ void AnsibleManagementService::OnApplyAnsiblePlaybook(
     LOG(ERROR) << "Failed to apply Ansible playbook. Empty response.";
     std::move(ansible_playbook_application_finished_callback_)
         .Run(/*success=*/false);
+    for (auto& observer : observers_) {
+      observer.OnError();
+    }
     return;
   }
 
@@ -171,6 +189,9 @@ void AnsibleManagementService::OnApplyAnsiblePlaybook(
                << response->failure_reason();
     std::move(ansible_playbook_application_finished_callback_)
         .Run(/*success=*/false);
+    for (auto& observer : observers_) {
+      observer.OnError();
+    }
     return;
   }
 
@@ -184,10 +205,16 @@ void AnsibleManagementService::OnApplyAnsiblePlaybookProgress(
     case vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal::SUCCEEDED:
       std::move(ansible_playbook_application_finished_callback_)
           .Run(/*success=*/true);
+      for (auto& observer : observers_) {
+        observer.OnApplicationFinished();
+      }
       break;
     case vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal::FAILED:
       std::move(ansible_playbook_application_finished_callback_)
           .Run(/*success=*/false);
+      for (auto& observer : observers_) {
+        observer.OnError();
+      }
       break;
     case vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal::IN_PROGRESS:
       // TODO(okalitova): Report Ansible playbook application progress.
@@ -195,6 +222,15 @@ void AnsibleManagementService::OnApplyAnsiblePlaybookProgress(
     default:
       NOTREACHED();
   }
+}
+
+void AnsibleManagementService::AddObserver(Observer* observer) {
+  DCHECK(observer);
+  observers_.AddObserver(observer);
+}
+
+void AnsibleManagementService::RemoveObserver(Observer* observer) {
+  observers_.RemoveObserver(observer);
 }
 
 }  // namespace crostini
--- chrome/browser/chromeos/crostini/ansible/ansible_management_service.h
+++ chrome/browser/chromeos/crostini/ansible/ansible_management_service.h
@@ -25,6 +25,15 @@ constexpr char kCrostiniDefaultAnsibleVersion[] =
 class AnsibleManagementService : public KeyedService,
                                  public LinuxPackageOperationProgressObserver {
  public:
+  // Observer class for Ansible Management Service related events.
+  class Observer : public base::CheckedObserver {
+   public:
+    ~Observer() override = default;
+    virtual void OnApplicationStarted() = 0;
+    virtual void OnApplicationFinished() = 0;
+    virtual void OnError() = 0;
+  };
+
   static AnsibleManagementService* GetForProfile(Profile* profile);
 
   explicit AnsibleManagementService(Profile* profile);
@@ -52,6 +61,9 @@ class AnsibleManagementService : public KeyedService,
   void OnApplyAnsiblePlaybookProgress(
       vm_tools::cicerone::ApplyAnsiblePlaybookProgressSignal::Status status);
 
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
+
  private:
   void OnInstallAnsibleInDefaultContainer(CrostiniResult result);
 
@@ -62,6 +74,7 @@ class AnsibleManagementService : public KeyedService,
           response);
 
   Profile* profile_;
+  base::ObserverList<Observer> observers_;
   base::OnceCallback<void(bool success)>
       ansible_installation_finished_callback_;
   base::OnceCallback<void(bool success)>
