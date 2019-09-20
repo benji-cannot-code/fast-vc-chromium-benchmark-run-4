2dcaa40e61f1a05b0d624cb17a96263ff484e02f
Clark DuVall <cduvall@chromium.org>
2019-09-20 06:26:11 +0000

    Reland "[WebLayer] Load WebLayer from WebView"
    This is a reland of be5bd323d0ca687b0b685f5251a3811b4da68235
    
    Original change's description:
    > [WebLayer] Load WebLayer from WebView
    >
    > This adds WebLayer to WebView, allowing us to load the WebLayer
    > implementation from a monochrome APK (or other APK which includes
    > WebView).
    >
    > Run the shell loading from MonochromePublic.apk with:
    >
    >   $ autoninja -C out/Android run_weblayer_shell_monochrome
    >   $ out/Android/bin/run_weblayer_shell_monochrome
    >
    > Run the shell loading from TrichromeWebView.apk with:
    >
    >   $ autoninja -C out/Android run_weblayer_shell_trichrome
    >   $ out/Android/bin/run_weblayer_shell_trichrome
    >
    > Change-Id: Ief31c2b79edae6b697ca2927fd6eccbf255515b3
    > Binary-Size: Increase needed for implementing WebLayer, see go/weblayer-dev-intro
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810329
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698321}
    
    TBR=dtrainor@chromium.org,torne@chromium.org
    
    Change-Id: I3e76e41a9f808f8a85010bae2953a534e917ec75
    Binary-Size: Increase needed for implementing WebLayer, see go/weblayer-dev-intro
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815605
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698383}

--- chrome/browser/android/monochrome_entry_point.cc
+++ chrome/browser/android/monochrome_entry_point.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/library_loader/library_loader_hooks.h"
 #include "base/bind.h"
 #include "chrome/app/android/chrome_jni_onload.h"
+#include "weblayer/app/jni_onload.h"
 
 namespace {
 
@@ -20,6 +21,10 @@ bool NativeInit(base::android::LibraryProcessType library_process_type) {
     case base::android::PROCESS_BROWSER:
     case base::android::PROCESS_CHILD:
       return android::OnJNIOnLoadInit();
+      break;
+    case base::android::PROCESS_WEBLAYER:
+    case base::android::PROCESS_WEBLAYER_CHILD:
+      return weblayer::OnJNIOnLoadInit("resources.pak");
       break;
     default:
       NOTREACHED();
