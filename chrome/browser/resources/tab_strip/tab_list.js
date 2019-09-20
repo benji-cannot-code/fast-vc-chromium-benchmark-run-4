acf3c7d4b04e35d2ced5d1cb757deff22d7f5531
John Lee <johntlee@chromium.org>
2019-09-20 20:47:55 +0000

    Tab Strip WebUI: Remove ghost pinned tabs
    Before: https://i.imgur.com/XfCXuIb.png
    After: https://i.imgur.com/Nlc9fqi.png
    
    Bug: 989131
    Change-Id: I2f8a69701fbf48fa5329f98c37e5a5bba08f82fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809802
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698611}

--- chrome/browser/resources/tab_strip/tab_list.js
+++ chrome/browser/resources/tab_strip/tab_list.js
@@ -10,9 +10,6 @@ import {CustomElement} from './custom_element.js';
 import {TabElement} from './tab.js';
 import {TabsApiProxy} from './tabs_api_proxy.js';
 
-/** @const {number} */
-const GHOST_PINNED_TAB_COUNT = 3;
-
 /**
  * The amount of padding to leave between the edge of the screen and the active
  * tab when auto-scrolling. This should leave some room to show the previous or
@@ -175,15 +172,6 @@ class TabListElement extends CustomElement {
   }
 
   /**
-   * @return {number}
-   * @private
-   */
-  getPinnedTabsCount_() {
-    return this.pinnedTabsContainerElement_.childElementCount -
-        GHOST_PINNED_TAB_COUNT;
-  }
-
-  /**
    * @param {!TabElement} tabElement
    * @param {number} index
    * @private
@@ -198,12 +186,11 @@ class TabListElement extends CustomElement {
     } else {
       // Pinned tabs are in their own container, so the index of non-pinned
       // tabs need to be offset by the number of pinned tabs
-      const offsetIndex = index - this.getPinnedTabsCount_();
+      const offsetIndex =
+          index - this.pinnedTabsContainerElement_.childElementCount;
       this.tabsContainerElement_.insertBefore(
           tabElement, this.tabsContainerElement_.childNodes[offsetIndex]);
     }
-
-    this.updatePinnedTabsState_();
   }
 
   /**
@@ -235,13 +222,10 @@ class TabListElement extends CustomElement {
       return;
     }
 
-    let dragOverIndex =
-        Array.from(dragOverItem.parentNode.children).indexOf(dragOverItem);
     event.dataTransfer.dropEffect = 'move';
-    if (!dragOverItem.tab.pinned) {
-      dragOverIndex += this.getPinnedTabsCount_();
-    }
 
+    const dragOverIndex =
+        Array.from(dragOverItem.parentNode.children).indexOf(dragOverItem);
     this.tabsApi_.moveTab(this.draggedItem_.tab.id, dragOverIndex);
   }
 
@@ -369,7 +353,6 @@ class TabListElement extends CustomElement {
     if (tabElement) {
       this.addAnimationPromise_(new Promise(async resolve => {
         await tabElement.slideOut();
-        this.updatePinnedTabsState_();
         resolve();
       }));
     }
@@ -436,14 +419,6 @@ class TabListElement extends CustomElement {
     if (tab) {
       tab.updateThumbnail(imgData);
     }
-  }
-
-  /** @private */
-  updatePinnedTabsState_() {
-    this.pinnedTabsContainerElement_.toggleAttribute(
-        'empty',
-        this.pinnedTabsContainerElement_.childElementCount ===
-            GHOST_PINNED_TAB_COUNT);
   }
 }
 
