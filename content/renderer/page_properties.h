e8dbf246f01c516bd37bfbd3de7c4ff684cd25f5
Erik Chen <erikchen@chromium.org>
2019-09-21 02:33:04 +0000

    Move RenderWidgetScreenMetricsEmulator to be a member of PageProperties.
    This CL is primarily a code move. It sets the stage for making screen metrics
    emulator a RenderView concept rather than a RenderWidget concept.
    
    The main behavior change is that screen metrics emulation now only applies to
    main frame render widgets. The changes will propagate to child render widgets
    via the SynchronizeVisualProperties dance. It doesn't make sense to set screen
    metrics emulation on a child render widget, since that will create racy
    conflicts with the actual page properties.
    
    Bug: 1006052
    Change-Id: I5b4a62bf6fd6cc03ace8cdc546ac90ed2d6f0e29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815771
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698720}

--- content/renderer/page_properties.h
+++ content/renderer/page_properties.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+class RenderWidgetScreenMetricsEmulator;
+
 // This interface exposes "page global" concepts to child frames. Historically
 // such state was managed by the RenderView API. However RenderView also has
 // to be have as the WebViewClient as well as the IPC anchor for content. As
@@ -35,9 +37,20 @@ class CONTENT_EXPORT PageProperties {
   // reduces boilerplate code.
   float GetDeviceScaleFactor() { return screen_info_.device_scale_factor; }
 
+  RenderWidgetScreenMetricsEmulator* ScreenMetricsEmulator() {
+    return screen_metrics_emulator_.get();
+  }
+  void SetScreenMetricsEmulator(
+      std::unique_ptr<RenderWidgetScreenMetricsEmulator> emulator);
+
  private:
   // Properties of the screen hosting the page.
   ScreenInfo screen_info_;
+
+  // TODO(https://crbug.com/1006052): Rename RenderWidgetScreenMetricsEmulator
+  // to RenderViewScreenMetricsEmulator and reroute IPCs through RenderView
+  // instead of RenderWidget.
+  std::unique_ptr<RenderWidgetScreenMetricsEmulator> screen_metrics_emulator_;
 };
 
 }  // namespace content
