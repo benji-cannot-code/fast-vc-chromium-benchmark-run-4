9ee8351f07c2f74c822c9b203e5191143843f035
Jan Krcal <jkrcal@chromium.org>
2019-09-21 00:49:16 +0000

    [Toolbar button] Animate border+text+background with the new style
    This CL shows a border around toolbar buttons that have non-empty
    labels (with a nice fade-in / fade-out while animating the label).
    
    This is only enabled when the avatar animation feature is enabled.
    
    Bug: 967317
    Change-Id: I06668d3548f83d9e646d5f52eb995fb93875a55f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1771405
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698700}

--- chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.cc
+++ chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.cc
@@ -21,10 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
 
-namespace {
-constexpr float kToolbarInkDropHighlightVisibleOpacity = 0.08f;
-}
-
 gfx::Insets GetToolbarInkDropInsets(const views::View* host_view,
                                     const gfx::Insets& margin_insets) {
   // Inset the inkdrop insets so that the end result matches the target inkdrop
