07926b957876a0b8d6b0cfd0bd1827ac5e07b3ba
Peter Kasting <pkasting@chromium.org>
2019-09-21 14:10:33 +0000

    Remove using directives ("using namespace x") from google_apis/.
    Bug: 82078
    Change-Id: I5b7a8fe9f0dd2cf999ee7a084f32c1f3c02b6cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816358
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698761}

--- google_apis/gaia/fake_gaia.cc
+++ google_apis/gaia/fake_gaia.cc
@@ -39,7 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   request_handlers_.insert(std::make_pair( \
         path, base::Bind(&FakeGaia::method, base::Unretained(this))))
 
-using namespace net::test_server;
+using net::test_server::BasicHttpResponse;
+using net::test_server::HttpRequest;
 
 namespace {
 
@@ -185,17 +186,15 @@ std::string FakeGaia::GetGaiaIdOfEmail(const std::string& email) const {
       it->second;
 }
 
-void FakeGaia::AddGoogleAccountsSigninHeader(
-    net::test_server::BasicHttpResponse* http_response,
-    const std::string& email) const {
+void FakeGaia::AddGoogleAccountsSigninHeader(BasicHttpResponse* http_response,
+                                             const std::string& email) const {
   http_response->AddCustomHeader("google-accounts-signin",
       base::StringPrintf(
           "email=\"%s\", obfuscatedid=\"%s\", sessionindex=0",
           email.c_str(), GetGaiaIdOfEmail(email).c_str()));
 }
 
-void FakeGaia::SetOAuthCodeCookie(
-    net::test_server::BasicHttpResponse* http_response) const {
+void FakeGaia::SetOAuthCodeCookie(BasicHttpResponse* http_response) const {
   http_response->AddCustomHeader(
       "Set-Cookie",
       base::StringPrintf("oauth_code=%s; Path=/; Secure; HttpOnly;",
@@ -274,20 +273,20 @@ void FakeGaia::Initialize() {
       HandleGetCheckConnectionInfo);
 }
 
-std::unique_ptr<HttpResponse> FakeGaia::HandleRequest(
+std::unique_ptr<net::test_server::HttpResponse> FakeGaia::HandleRequest(
     const HttpRequest& request) {
   // The scheme and host of the URL is actually not important but required to
   // get a valid GURL in order to parse |request.relative_url|.
   GURL request_url = GURL("http://localhost").Resolve(request.relative_url);
   std::string request_path = request_url.path();
-  std::unique_ptr<BasicHttpResponse> http_response(new BasicHttpResponse());
+  auto http_response = std::make_unique<BasicHttpResponse>();
   RequestHandlerMap::iterator iter = request_handlers_.find(request_path);
   if (iter != request_handlers_.end()) {
     LOG(WARNING) << "Serving request " << request_path;
     iter->second.Run(request, http_response.get());
   } else {
     LOG(ERROR) << "Unhandled request " << request_path;
-    return std::unique_ptr<HttpResponse>();  // Request not understood.
+    return std::unique_ptr<net::test_server::HttpResponse>();
   }
 
   return std::move(http_response);
@@ -520,8 +519,8 @@ void FakeGaia::HandleServiceLoginAuth(const HttpRequest& request,
 }
 
 void FakeGaia::HandleEmbeddedLookupAccountLookup(
-    const net::test_server::HttpRequest& request,
-    net::test_server::BasicHttpResponse* http_response) {
+    const HttpRequest& request,
+    BasicHttpResponse* http_response) {
   std::string email;
   const bool is_saml =
       GetQueryParameter(request.content, "identifier", &email) &&
@@ -725,9 +724,8 @@ void FakeGaia::HandleGetUserInfo(const HttpRequest& request,
   http_response->set_code(net::HTTP_OK);
 }
 
-void FakeGaia::HandleOAuthUserInfo(
-    const net::test_server::HttpRequest& request,
-    net::test_server::BasicHttpResponse* http_response) {
+void FakeGaia::HandleOAuthUserInfo(const HttpRequest& request,
+                                   BasicHttpResponse* http_response) {
   const AccessTokenInfo* token_info = nullptr;
   std::string access_token;
   if (GetAccessToken(request, kAuthHeaderBearer, &access_token) ||
@@ -747,9 +745,8 @@ void FakeGaia::HandleOAuthUserInfo(
   }
 }
 
-void FakeGaia::HandleSAMLRedirect(
-    const net::test_server::HttpRequest& request,
-    net::test_server::BasicHttpResponse* http_response) {
+void FakeGaia::HandleSAMLRedirect(const HttpRequest& request,
+                                  BasicHttpResponse* http_response) {
   GURL request_url = GURL("http://localhost").Resolve(request.relative_url);
   std::string domain;
   GetQueryParameter(request_url.query(), "domain", &domain);
@@ -774,9 +771,8 @@ void FakeGaia::HandleSAMLRedirect(
   http_response->AddCustomHeader("Location", redirect_url);
 }
 
-void FakeGaia::HandleGetCheckConnectionInfo(
-    const net::test_server::HttpRequest& request,
-    net::test_server::BasicHttpResponse* http_response) {
+void FakeGaia::HandleGetCheckConnectionInfo(const HttpRequest& request,
+                                            BasicHttpResponse* http_response) {
   base::ListValue connection_list;
   FormatJSONResponse(connection_list, http_response);
 }
--- google_apis/gcm/engine/mcs_client.cc
+++ google_apis/gcm/engine/mcs_client.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gcm/engine/connection_factory.h"
 #include "google_apis/gcm/monitoring/gcm_stats_recorder.h"
 
-using namespace google::protobuf::io;
-
 namespace gcm {
 
 namespace {
