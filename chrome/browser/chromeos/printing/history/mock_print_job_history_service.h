e3b5b1f61545b73b846117a9c31bfb2cf09ff620
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-09-21 10:57:09 +0000

    Printing API: Add comments for MockPrintJobHistoryService class
    Bug: 992889
    Change-Id: I4d4c2f8dcd906e532754e2d306a5be3441324e48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810997
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Cr-Commit-Position: refs/heads/master@{#698756}

--- chrome/browser/chromeos/printing/history/mock_print_job_history_service.h
+++ chrome/browser/chromeos/printing/history/mock_print_job_history_service.h
@@ -12,11 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
+// The mock implementation of PrintJobHistoryService for testing.
 class MockPrintJobHistoryService : public PrintJobHistoryService {
  public:
   MockPrintJobHistoryService();
   ~MockPrintJobHistoryService() override;
 
+  // This method doesn't save print job to the persistent storage.
+  // It should be used only for testing to notify observers.
   void SavePrintJobProto(const printing::proto::PrintJobInfo& print_job_info);
 
   MOCK_METHOD(void,
