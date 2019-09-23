daa437608839d7142ff3680843215e283848636b
Guido Urdaneta <guidou@chromium.org>
2019-09-23 18:44:20 +0000

    Update getDisplayMedia tests for macOS Catalina
    Starting in macOS 10.15, screen capture requires system permissions that are
    disabled by default.
    This CL updates getDisplayMedia() test expectations to take this into account.
    
    Bug: 1002984
    Change-Id: I26150e8add754831e40702681719a92b33b55bff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819252
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698964}

--- chrome/browser/media/webrtc/display_media_access_handler_unittest.cc
+++ chrome/browser/media/webrtc/display_media_access_handler_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/run_loop.h"
+#include "build/build_config.h"
 #include "chrome/browser/media/webrtc/fake_desktop_media_picker_factory.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "content/public/browser/browser_context.h"
@@ -22,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/mediastream/media_stream_request.h"
 #include "third_party/blink/public/mojom/mediastream/media_stream.mojom-shared.h"
+
+#if defined(OS_MACOSX)
+#include "base/mac/mac_util.h"
+#endif
 
 class DisplayMediaAccessHandlerTest : public ChromeRenderViewHostTestHarness {
  public:
@@ -97,6 +102,16 @@ TEST_F(DisplayMediaAccessHandlerTest, PermissionGiven) {
   ProcessRequest(content::DesktopMediaID(content::DesktopMediaID::TYPE_SCREEN,
                                          content::DesktopMediaID::kFakeId),
                  &result, &devices, false /* request_audio */);
+#if defined(OS_MACOSX)
+  // Starting from macOS 10.15, screen capture requires system permissions
+  // that are disabled by default.
+  if (base::mac::IsAtLeastOS10_15()) {
+    EXPECT_EQ(blink::mojom::MediaStreamRequestResult::PERMISSION_DENIED,
+              result);
+    return;
+  }
+#endif
+
   EXPECT_EQ(blink::mojom::MediaStreamRequestResult::OK, result);
   EXPECT_EQ(1u, devices.size());
   EXPECT_EQ(blink::mojom::MediaStreamType::DISPLAY_VIDEO_CAPTURE,
@@ -111,6 +126,15 @@ TEST_F(DisplayMediaAccessHandlerTest, PermissionGivenToRequestWithAudio) {
                                         content::DesktopMediaID::kFakeId,
                                         true /* audio_share */);
   ProcessRequest(fake_media_id, &result, &devices, true /* request_audio */);
+#if defined(OS_MACOSX)
+  // Starting from macOS 10.15, screen capture requires system permissions
+  // that are disabled by default.
+  if (base::mac::IsAtLeastOS10_15()) {
+    EXPECT_EQ(blink::mojom::MediaStreamRequestResult::PERMISSION_DENIED,
+              result);
+    return;
+  }
+#endif
   EXPECT_EQ(blink::mojom::MediaStreamRequestResult::OK, result);
   EXPECT_EQ(2u, devices.size());
   EXPECT_EQ(blink::mojom::MediaStreamType::DISPLAY_VIDEO_CAPTURE,
@@ -233,6 +257,16 @@ TEST_F(DisplayMediaAccessHandlerTest, MultipleRequests) {
   wait_loop[0].Run();
   EXPECT_TRUE(test_flags[0].picker_created);
   EXPECT_TRUE(test_flags[0].picker_deleted);
+#if defined(OS_MACOSX)
+  // Starting from macOS 10.15, screen capture requires system permissions
+  // that are disabled by default.
+  if (base::mac::IsAtLeastOS10_15()) {
+    EXPECT_EQ(blink::mojom::MediaStreamRequestResult::PERMISSION_DENIED,
+              result);
+    access_handler_.reset();
+    return;
+  }
+#endif
   EXPECT_EQ(blink::mojom::MediaStreamRequestResult::OK, result);
   EXPECT_EQ(1u, devices.size());
   EXPECT_EQ(blink::mojom::MediaStreamType::DISPLAY_VIDEO_CAPTURE,
--- chrome/browser/media/webrtc/webrtc_getdisplaymedia_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_getdisplaymedia_browsertest.cc
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "media/base/media_switches.h"
 
+#if defined(OS_MACOSX)
+#include "base/mac/mac_util.h"
+#endif
+
 namespace {
 
 static const char kMainHtmlPage[] = "/webrtc/webrtc_getdisplaymedia_test.html";
@@ -40,7 +44,14 @@ class WebRtcGetDisplayMediaBrowserTest : public WebRtcTestBase {
         tab->GetMainFrame(),
         base::StringPrintf("runGetDisplayMedia(%s);", constraints.c_str()),
         &result));
+#if defined(OS_MACOSX)
+    // Starting from macOS 10.15, screen capture requires system permissions
+    // that are disabled by default.
+    EXPECT_EQ(result, base::mac::IsAtMostOS10_14() ? "getdisplaymedia-success"
+                                                   : "getdisplaymedia-failure");
+#else
     EXPECT_EQ(result, "getdisplaymedia-success");
+#endif
   }
 };
 
