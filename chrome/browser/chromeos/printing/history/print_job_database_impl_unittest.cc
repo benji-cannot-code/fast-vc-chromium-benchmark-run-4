2112e918850a904c39bc1aa46875b9288c998405
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-09-23 12:05:46 +0000

    Printing API: Add UMA histograms to track database performance
    Bug: 992889
    Change-Id: I970100c5b1060ef31a3e67d0ff431851b3f8eaab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1813352
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698858}

--- chrome/browser/chromeos/printing/history/print_job_database_impl_unittest.cc
+++ chrome/browser/chromeos/printing/history/print_job_database_impl_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/run_loop.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/chromeos/printing/history/print_job_info.pb.h"
@@ -127,6 +128,7 @@ class PrintJobDatabaseImplTest : public ::testing::Test {
 
   base::test::TaskEnvironment task_environment_;
   std::unique_ptr<PrintJobDatabaseImpl> print_job_database_;
+  base::HistogramTester histogram_tester_;
 
  private:
   base::ScopedTempDir temp_dir_;
@@ -220,6 +222,24 @@ TEST_F(PrintJobDatabaseImplTest, RequestsBeforeInitialization) {
   EXPECT_EQ(1u, print_job_entries.size());
   EXPECT_EQ(kId1, print_job_entries[0].id());
   EXPECT_EQ(kTitle1, print_job_entries[0].title());
+}
+
+TEST_F(PrintJobDatabaseImplTest, Histograms) {
+  Initialize();
+  PrintJobInfo print_job_info = ConstructPrintJobInfo(kId1, kTitle1);
+  SavePrintJob(print_job_info);
+  GetPrintJobs();
+  histogram_tester_.ExpectTotalCount(
+      PrintJobDatabaseImpl::kPrintJobDatabaseEntries, 1);
+  histogram_tester_.ExpectUniqueSample(
+      PrintJobDatabaseImpl::kPrintJobDatabaseEntries, 0, 1);
+  histogram_tester_.ExpectTotalCount(
+      PrintJobDatabaseImpl::kPrintJobDatabaseEntrySize, 1);
+  histogram_tester_.ExpectUniqueSample(
+      PrintJobDatabaseImpl::kPrintJobDatabaseEntrySize,
+      print_job_info.ByteSizeLong(), 1);
+  histogram_tester_.ExpectTotalCount(
+      PrintJobDatabaseImpl::kPrintJobDatabaseLoadTime, 1);
 }
 
 }  // namespace chromeos
