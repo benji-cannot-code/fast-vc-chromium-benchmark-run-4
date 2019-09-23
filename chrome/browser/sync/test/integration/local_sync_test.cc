9feb0f839372f8ee7d4ad52c63a78d0b02bdc7d6
Mikel Astiz <mastiz@chromium.org>
2019-09-23 16:56:48 +0000

    Fix send-tab-to-self UI displayed for roaming profiles
    The sync datatype doesn't provide any useful features to enterprise
    users using local sync (aka roaming profiles), so displaying the UI is
    unnecessary and causes confusion.
    
    We avoid such UI by forcefully disabling the sync datatype for local
    sync users, which is also sufficient to hide the UI.
    
    Bug: 1004400
    Change-Id: I3c5d93a9b78de3172550d7e63915b878c2d75099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1813346
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698921}

--- chrome/browser/sync/test/integration/local_sync_test.cc
+++ chrome/browser/sync/test/integration/local_sync_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/send_tab_to_self/send_tab_to_self_util.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/sync/test/integration/single_client_status_change_checker.h"
 #include "chrome/browser/ui/browser.h"
@@ -74,6 +75,9 @@ IN_PROC_BROWSER_TEST_F(LocalSyncTest, ShouldStart) {
   ASSERT_TRUE(SyncTransportActiveChecker(service).Wait());
 
   EXPECT_TRUE(service->IsLocalSyncEnabled());
+
+  // Verify certain features are disabled.
+  EXPECT_FALSE(send_tab_to_self::IsUserSyncTypeActive(browser()->profile()));
 }
 #endif  // defined(OS_WIN)
 
