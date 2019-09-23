72fe3551264c3ebec467af4d9e130b6afedad344
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-09-23 10:49:32 +0000

    Printing API: Rename proto conversion helper
    Bug: 992889
    Change-Id: I510d33e358a9c2dbacecf4f99adf6ad68a0435ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816465
    Commit-Queue: Nikita Podguzov <nikitapodguzov@google.com>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698856}

--- chrome/browser/chromeos/printing/cups_print_job_manager_impl.cc
+++ chrome/browser/chromeos/printing/cups_print_job_manager_impl.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/printing/cups_printers_manager.h"
 #include "chrome/browser/chromeos/printing/cups_printers_manager_factory.h"
 #include "chrome/browser/chromeos/printing/history/print_job_info.pb.h"
-#include "chrome/browser/chromeos/printing/history/print_job_info_conversions.h"
+#include "chrome/browser/chromeos/printing/history/print_job_info_proto_conversions.h"
 #include "chrome/browser/printing/print_job.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/grit/generated_resources.h"
--- chrome/browser/chromeos/printing/history/print_job_history_service_impl.cc
+++ chrome/browser/chromeos/printing/history/print_job_history_service_impl.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
 #include "chrome/browser/chromeos/printing/cups_print_job.h"
-#include "chrome/browser/chromeos/printing/history/print_job_info_conversions.h"
+#include "chrome/browser/chromeos/printing/history/print_job_info_proto_conversions.h"
 
 namespace chromeos {
 
