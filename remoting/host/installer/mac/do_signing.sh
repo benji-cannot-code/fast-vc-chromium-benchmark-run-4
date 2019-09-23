ee5fa6508f744dff6d25d725ffee9609b0f5aa8f
Lambros Lambrou <lambroslambrou@chromium.org>
2019-09-23 23:29:01 +0000

    Add notarization option to do_signing.sh .
    This adds an option '-n <notarization username>' to the signing script
    for the Mac Chromoting host installer. If the option is present, the
    final disk image (DMG) will be uploaded to Apple for notarization,
    with the provided username, and the password from the environment
    variable $NOTARIZATION_PASSWORD. If successful, the DMG will be stapled
    with the result.
    
    Bug: 999406
    Change-Id: I745d15f35deb2c8360bb9e2ed236ce27a636c50b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807961
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699106}

--- remoting/host/installer/mac/do_signing.sh
+++ remoting/host/installer/mac/do_signing.sh
@@ -9,11 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # installer and then packages it into a .dmg.  It requires that Packages be
 # installed (for 'packagesbuild').
 # Packages: http://s.sudre.free.fr/Software/Packages/about.html
-#
-# usage: do_signing.sh output_dir input_dir [codesign_keychain codesign_id
-#            [productsign_id]]
-#
-# The final disk image (dmg) is placed in |output_dir|.
+# Optionally, it will submit the .dmg to Apple for notarization.
+# Run with "-h" to see usage information.
 
 set -e -u
 
@@ -205,6 +202,19 @@ build_dmg() {
   fi
 }
 
+notarize() {
+  local input_dir="${1}"
+  local dmg="${2}"
+  local user="${3}"
+
+  echo "Notarizing and stapling .dmg..."
+  "${input_dir}/notarize_thing.py" \
+      --user "${user}" \
+      --password @env:NOTARIZATION_PASSWORD \
+      --bundle-id "${HOST_BUNDLE_NAME}" \
+      "${dmg}"
+}
+
 cleanup() {
   if [[ "${#g_cleanup_dirs[@]}" > 0 ]]; then
     rm -rf "${g_cleanup_dirs[@]}"
@@ -213,7 +223,8 @@ cleanup() {
 
 usage() {
   echo "Usage: ${ME} -o output_dir -i input_dir "\
-      "[-c codesign_id] [-p productsign_id] [-k keychain]" >&2
+       "[-c codesign_id] [-p productsign_id] [-k keychain] "\
+       "[-n notarization_user]" >&2
   echo >&2
   echo "  Sign the binaries using the specified <codesign_id>, build" >&2
   echo "  the installer, and then sign the installer using the given" >&2
@@ -222,6 +233,9 @@ usage() {
   echo "  installer is built without signing any binaries." >&2
   echo "  If <keychain> is specified, it must contain all the signing ids." >&2
   echo "  If not specified, then the default keychains will be used." >&2
+  echo "  If <notarization_user> is specified, the final DMG will be" >&2
+  echo "  notarized by Apple and stapled, using the given user and the" >&2
+  echo "  password from \$NOTARIZATION_PASSWORD variable." >&2
 }
 
 main() {
@@ -231,9 +245,10 @@ main() {
   local codesign_id=""
   local productsign_id=""
   local keychain=""
+  local notarization_user=""
 
   local OPTNAME OPTIND OPTARG
-  while getopts ":o:i:c:p:k:h" OPTNAME; do
+  while getopts ":o:i:c:p:k:n:h" OPTNAME; do
     case ${OPTNAME} in
       o )
         output_dir="$(shell_safe_path "${OPTARG}")"
@@ -249,6 +264,9 @@ main() {
         ;;
       k )
         keychain="$(shell_safe_path "${OPTARG}")"
+        ;;
+      n )
+        notarization_user="${OPTARG}"
         ;;
       h )
         usage
@@ -299,7 +317,13 @@ main() {
   fi
   build_dmg "${input_dir}" "${output_dir}"
   if [[ "${do_sign_binaries}" == 1 ]]; then
-      sign "${output_dir}/${DMG_FILE_NAME}" "${keychain}" "${codesign_id}"
+    sign "${output_dir}/${DMG_FILE_NAME}" "${keychain}" "${codesign_id}"
+  fi
+
+  if [[ -n "${notarization_user}" ]]; then
+    notarize "${input_dir}" \
+             "${output_dir}/${DMG_FILE_NAME}" \
+             "${notarization_user}"
   fi
 
   cleanup
