eade403253ed02d0d0ab030b7da2ff1ec873324e
Leonard Grey <lgrey@chromium.org>
2019-09-23 18:04:55 +0000

    Revert "Remove unused headers from ios/web/public/web_state"
    This reverts commit 480313d96e893f0d34c6091faac66866835ede9e.
    
    Reason for revert: https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8901505278757132992/+/steps/compile/0/stdout
    
    Original change's description:
    > Remove unused headers from ios/web/public/web_state
    > 
    > This CL removes web_state_observer.h and web_state.h from
    > ios/web/public/web_state which are no longer used in downlstream repo
    > (downstream code was switched to use files from ios/web/public).
    > 
    > Bug: 942244
    > Change-Id: I1480b115343735a69347578cfd0628f983c069ca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816807
    > Commit-Queue: Yi Su <mrsuyi@chromium.org>
    > Auto-Submit: Eugene But <eugenebut@chromium.org>
    > Reviewed-by: Yi Su <mrsuyi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698929}
    
    TBR=eugenebut@chromium.org,mrsuyi@chromium.org
    
    Change-Id: I7f9a7453be97af8ecd702ac4392e73416ce7dc7d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 942244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815914
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698938}

---
+++ ios/web/public/web_state/web_state.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_PUBLIC_WEB_STATE_WEB_STATE_H_
+#define IOS_WEB_PUBLIC_WEB_STATE_WEB_STATE_H_
+
+// TODO(crbug.com/942244): Remove this header after downstream code
+// stopped including ios/web/public/web_state/web_state.h
+
+#import "ios/web/public/web_state.h"
+
+#endif  // IOS_WEB_PUBLIC_WEB_STATE_WEB_STATE_H_
---
+++ ios/web/public/web_state/web_state_observer.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_PUBLIC_WEB_STATE_WEB_STATE_OBSERVER_H_
+#define IOS_WEB_PUBLIC_WEB_STATE_WEB_STATE_OBSERVER_H_
+
+// TODO(crbug.com/942244): Remove this header after downstream code
+// stopped including ios/web/public/web_state/web_state.h
+
+#include "ios/web/public/web_state_observer.h"
+
+#endif  // IOS_WEB_PUBLIC_WEB_STATE_WEB_STATE_OBSERVER_H_
