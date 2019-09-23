fe1066a7fb008becba3b973d7fe00dd17078582e
Steven Bennetts <stevenjb@chromium.org>
2019-09-23 22:11:14 +0000

    Fix network_ui.js: getShillDeviceProperties() call for mojo
    This is developer debugging UI only, and a subtle use cases that I
    only noticed when investigating a bug where a Cellular device property
    is not getting set by Shill. (The information is still available in
    chrome://system and in feedback reports).
    
    Bug: none
    Change-Id: I6435435c0ccf7cfd1608635631c0f6311c30fc34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815547
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699067}

--- chrome/browser/resources/chromeos/network_ui/network_ui.js
+++ chrome/browser/resources/chromeos/network_ui/network_ui.js
@@ -389,7 +389,9 @@ const NetworkUI = (function() {
    */
   const handleDeviceDetail = function(state, selectedId, detailCell) {
     if (selectedId == 'shill') {
-      chrome.send('getShillDeviceProperties', [state.type]);
+      chrome.send(
+          'getShillDeviceProperties',
+          [OncMojo.getNetworkTypeString(state.type)]);
     } else {
       showDetail(detailCell, state);
     }
