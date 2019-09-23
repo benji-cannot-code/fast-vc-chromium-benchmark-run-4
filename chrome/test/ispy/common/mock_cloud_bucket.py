8a7b6f46297f358df90ab9a9f5dc30fb93316c87
Raul Tambre <raul@tambre.ee>
2019-09-23 16:13:25 +0000

    chrome/: Convert uses of has_key
    Conversion performed automatically using '2to3 -w -f has_key .'.
    This is backwards compatible with Python 2.
    
    Bug: 941669
    Change-Id: I566bdb8b3bc522ffba87e9159f20062a192bf492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1817602
    Auto-Submit: Raul Tambre <raul@tambre.ee>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698909}

--- chrome/test/ispy/common/mock_cloud_bucket.py
+++ chrome/test/ispy/common/mock_cloud_bucket.py
@@ -33,7 +33,7 @@ class MockCloudBucket(cloud_bucket.BaseCloudBucket):
 
   # override
   def DownloadFile(self, path):
-    if self.datastore.has_key(path):
+    if path in self.datastore:
       return self.datastore[path]
     else:
       raise cloud_bucket.FileNotFoundError
@@ -46,16 +46,16 @@ class MockCloudBucket(cloud_bucket.BaseCloudBucket):
 
   # override
   def RemoveFile(self, path):
-    if self.datastore.has_key(path):
+    if path in self.datastore:
       self.datastore.pop(path)
 
   # override
   def FileExists(self, path):
-    return self.datastore.has_key(path)
+    return path in self.datastore
 
   # override
   def GetImageURL(self, path):
-    if self.datastore.has_key(path):
+    if path in self.datastore:
       return path
     else:
       raise cloud_bucket.FileNotFoundError
