1eaa96bc9270496c1111235b265d36fc55c7dc0f
Stefan Zager <szager@chromium.org>
2019-09-23 23:09:35 +0000

    [IntersectionObserver] If necessary, schedule a frame after delay
    If the following sequence happens:
    
    - IntersectionObserver sends a notification
    - Layout is dirtied in a way that should generate a new notification
    - Lifecyce update runs
      - IntersectionObserver skips processing due to its 'delay' parameter
    
    ... then we need to guarantee that the IntersectionObserver will send
    the necessary notification after the required delay. If animation
    frames are being regularly generated, this won't be a problem, because
    the observer will get a chance to run at the end of every lifecycle
    update. But if the document goes dormant, and frames are not being
    regularly generated, then we must force a lifecycle update after enough
    time has passed, to give the observer a chance to run.
    
    Change-Id: I74b63486e5c9b05c662f9e2e6e1c0b4baf00eba5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1812798
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699093}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -3357,9 +3357,9 @@ void LocalFrameView::RemoveResizerArea(LayoutBox& resizer_box) {
     resizer_areas_->erase(it);
 }
 
-void LocalFrameView::ScheduleAnimation() {
+void LocalFrameView::ScheduleAnimation(base::TimeDelta delay) {
   if (auto* client = GetChromeClient())
-    client->ScheduleAnimation(this);
+    client->ScheduleAnimation(this, delay);
 }
 
 bool LocalFrameView::FrameIsScrollableDidChange() {
--- third_party/blink/renderer/core/frame/web_frame_widget_base.h
+++ third_party/blink/renderer/core/frame/web_frame_widget_base.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/user_gesture_indicator.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_image.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/timer.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace cc {
@@ -143,6 +144,8 @@ class CORE_EXPORT WebFrameWidgetBase
   // focused frame has a different local root.
   LocalFrame* FocusedLocalFrameInWidget() const;
 
+  void RequestAnimationAfterDelay(const base::TimeDelta&);
+
   virtual void Trace(blink::Visitor*);
 
  protected:
@@ -187,6 +190,7 @@ class CORE_EXPORT WebFrameWidgetBase
 
  private:
   void CancelDrag();
+  void RequestAnimationAfterDelayTimerFired(TimerBase*);
 
   WebWidgetClient* client_;
 
@@ -211,6 +215,9 @@ class CORE_EXPORT WebFrameWidgetBase
   // PaintWorkletPaintDispatcher on the compositor thread.
   base::WeakPtr<PaintWorkletPaintDispatcher> paint_dispatcher_;
   scoped_refptr<base::SingleThreadTaskRunner> paint_task_runner_;
+
+  std::unique_ptr<TaskRunnerTimer<WebFrameWidgetBase>>
+      request_animation_after_delay_timer_;
 
   friend class WebViewImpl;
 };
