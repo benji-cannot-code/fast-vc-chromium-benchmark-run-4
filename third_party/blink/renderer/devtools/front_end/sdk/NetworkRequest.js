87c2c923027cc1aa1870a163a55db028d1f7de3e
Brandon Goddard <brgoddar@microsoft.com>
2019-09-23 17:04:40 +0000

    Revert "[devtools] Add Url and Path Column to Network Log"
    This reverts commit 12915a6957ebe9ca20ef30c0e5141ad1dee87a7d.
    
    Reason for revert: Hiding the name column prevents the ability to 
    open the sidepane for network requests (headers, timing, etc).
    This will be confusing to users, 
    as none of the visible columns will provide the ability
    to see the view they are used to when selecting a network request.
    
    Original change's description:
    > [devtools] Add Url and Path Column to Network Log
    > 
    > This patch adds url and path as available columns to
    > the network log view and allows the name to be removed
    > because it is not needed if the user decides to show
    > the path or the whole url.
    > 
    > Bug: chromium:993366
    > Change-Id: Ib4600d1631bd436cb3c8b8e79b3709dddc38b166
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809161
    > Reviewed-by: Yang Guo <yangguo@chromium.org>
    > Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698891}
    
    TBR=yangguo@chromium.org,sigurds@chromium.org,bmeurer@chromium.org,janscheffler@chromium.org
    
    Change-Id: I5363b928cd8f21b8b6677e8b2bc38e42e7918140
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:993366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819617
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Brandon Goddard <brgoddar@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#698923}

--- third_party/blink/renderer/devtools/front_end/sdk/NetworkRequest.js
+++ third_party/blink/renderer/devtools/front_end/sdk/NetworkRequest.js
@@ -628,13 +628,6 @@ SDK.NetworkRequest = class extends Common.Object {
   }
 
   /**
-   * @return {string}
-   */
-  get pathname() {
-    return this._parsedURL.path;
-  }
-
-  /**
    * @return {!Common.ResourceType}
    */
   resourceType() {
