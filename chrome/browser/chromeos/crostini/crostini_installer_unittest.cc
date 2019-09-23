2539cef6a3be3d078ea4612cdd301415e0b78061
Jason Lin <lxj@google.com>
2019-09-23 08:04:32 +0000

    Move enums from CrostiniInstallerUIDelegate to a mojom file
    Enums |InstallationState| and |Error| in class |CrostiniInstallerUIDelegate|
    are moved to file crostini_installer_types.mojom and renamed to
    `InstallerState` and `InstallerError`. All other changes to .h and .cc files
    are just simple replacement to migrate to the new types.
    
    The purpose of this CL is to allow the js code of the new Crostini WebUI
    installer to use the same enums.
    
    Bug: 929571
    Change-Id: I185bb94b79187db98850dba4d23a7ba221e1d1e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815797
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Jason Lin <lxj@google.com>
    Cr-Commit-Position: refs/heads/master@{#698836}

--- chrome/browser/chromeos/crostini/crostini_installer_unittest.cc
+++ chrome/browser/chromeos/crostini/crostini_installer_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "chrome/browser/chromeos/crostini/crostini_installer_types.mojom.h"
 #include "chrome/browser/chromeos/crostini/crostini_installer_ui_delegate.h"
 #include "chrome/browser/chromeos/crostini/crostini_test_helper.h"
 #include "chrome/test/base/testing_profile.h"
@@ -23,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+using crostini::mojom::InstallerError;
+using crostini::mojom::InstallerState;
 using testing::_;
 using testing::AnyNumber;
 using testing::Expectation;
@@ -39,14 +42,12 @@ class CrostiniInstallerTest : public testing::Test {
  public:
   using ResultCallback = CrostiniInstallerUIDelegate::ResultCallback;
   using ProgressCallback = CrostiniInstallerUIDelegate::ProgressCallback;
-  using Error = CrostiniInstallerUIDelegate::Error;
-  using InstallationState = CrostiniInstallerUIDelegate::InstallationState;
 
   class MockCallbacks {
    public:
     MOCK_METHOD2(OnProgress,
-                 void(InstallationState state, double progress_fraction));
-    MOCK_METHOD1(OnFinished, void(Error error));
+                 void(InstallerState state, double progress_fraction));
+    MOCK_METHOD1(OnFinished, void(InstallerError error));
     MOCK_METHOD0(OnCanceled, void());
   };
 
@@ -185,7 +186,8 @@ TEST_F(CrostiniInstallerTest, InstallFlow) {
                   MountPath("sshfs://testing_profile@hostname:", _, _, _, _, _))
           .WillOnce(Invoke(&mount_path_waiter_, &MountPathWaiter::MountPath));
   // |OnProgress()| should not happens after |OnFinished()|
-  EXPECT_CALL(mock_callbacks_, OnFinished(Error::NONE)).After(expectation_set);
+  EXPECT_CALL(mock_callbacks_, OnFinished(InstallerError::kNone))
+      .After(expectation_set);
 
   Install();
   mount_path_waiter_.WaitForMountPathCalled();
@@ -269,11 +271,12 @@ TEST_F(CrostiniInstallerTest, CancelAfterStartBeforeCheckDisk) {
       << "Installer should recover to installable state";
 }
 
-TEST_F(CrostiniInstallerTest, Error) {
+TEST_F(CrostiniInstallerTest, InstallerError) {
   Expectation expect_progresses =
       EXPECT_CALL(mock_callbacks_, OnProgress(_, _)).Times(AnyNumber());
   // |OnProgress()| should not happens after |OnFinished()|
-  EXPECT_CALL(mock_callbacks_, OnFinished(Error::ERROR_STARTING_TERMINA))
+  EXPECT_CALL(mock_callbacks_,
+              OnFinished(InstallerError::kErrorStartingTermina))
       .After(expect_progresses);
 
   // Fake a failure for starting vm.
--- chrome/browser/ui/views/crostini/crostini_installer_view_browsertest.cc
+++ chrome/browser/ui/views/crostini/crostini_installer_view_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/chromeos/crostini/crostini_installer_types.mojom.h"
 #include "chrome/browser/ui/views/crostini/crostini_installer_view.h"
 
 #include "base/bind.h"
@@ -23,9 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_types.h"
 #include "ui/views/window/dialog_client_view.h"
 
-using Error = crostini::CrostiniInstallerUIDelegate::Error;
-using InstallationState =
-    crostini::CrostiniInstallerUIDelegate::InstallationState;
+using crostini::mojom::InstallerError;
+using crostini::mojom::InstallerState;
 
 class CrostiniInstallerViewBrowserTest : public CrostiniDialogBrowserTest {
  public:
@@ -83,11 +83,9 @@ IN_PROC_BROWSER_TEST_F(CrostiniInstallerViewBrowserTest, InstallFlow) {
       << "Install() should be called";
   EXPECT_TRUE(fake_delegate_.result_callback_);
 
-  fake_delegate_.progress_callback_.Run(InstallationState::CREATE_CONTAINER,
-                                        0.4);
-  fake_delegate_.progress_callback_.Run(InstallationState::MOUNT_CONTAINER,
-                                        0.8);
-  std::move(fake_delegate_.result_callback_).Run(Error::NONE);
+  fake_delegate_.progress_callback_.Run(InstallerState::kCreateContainer, 0.4);
+  fake_delegate_.progress_callback_.Run(InstallerState::kMountContainer, 0.8);
+  std::move(fake_delegate_.result_callback_).Run(InstallerError::kNone);
 
   // This allow the dialog to be destructed.
   base::RunLoop().RunUntilIdle();
@@ -105,7 +103,7 @@ IN_PROC_BROWSER_TEST_F(CrostiniInstallerViewBrowserTest, ErrorThenCancel) {
 
   ASSERT_TRUE(fake_delegate_.result_callback_);
   std::move(fake_delegate_.result_callback_)
-      .Run(Error::ERROR_CREATING_DISK_IMAGE);
+      .Run(InstallerError::kErrorCreatingDiskImage);
 
   EXPECT_FALSE(ActiveView()->GetWidget()->IsClosed());
   EXPECT_TRUE(HasEnabledAcceptButton());
@@ -130,7 +128,7 @@ IN_PROC_BROWSER_TEST_F(CrostiniInstallerViewBrowserTest, ErrorThenRetry) {
 
   ASSERT_TRUE(fake_delegate_.result_callback_);
   std::move(fake_delegate_.result_callback_)
-      .Run(Error::ERROR_CREATING_DISK_IMAGE);
+      .Run(InstallerError::kErrorCreatingDiskImage);
 
   EXPECT_FALSE(ActiveView()->GetWidget()->IsClosed());
   EXPECT_TRUE(HasEnabledAcceptButton());
@@ -141,7 +139,7 @@ IN_PROC_BROWSER_TEST_F(CrostiniInstallerViewBrowserTest, ErrorThenRetry) {
   EXPECT_TRUE(fake_delegate_.result_callback_)
       << "Install() should be called again";
 
-  std::move(fake_delegate_.result_callback_).Run(Error::NONE);
+  std::move(fake_delegate_.result_callback_).Run(InstallerError::kNone);
 
   // This allow the dialog to be destructed.
   base::RunLoop().RunUntilIdle();
