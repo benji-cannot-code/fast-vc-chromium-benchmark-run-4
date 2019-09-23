3e525669264055bc61f7cfc1d6c030184699f104
Vadym Doroshenko <dvadym@chromium.org>
2019-09-23 16:35:20 +0000

    Removing unused consts in SavePasswordProgressLogger::StringID
    Bug:875768
    
    Change-Id: I37be128e8371ccb9f08654c7c8d0d61b666e5253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819308
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698915}

--- components/password_manager/core/browser/browser_save_password_progress_logger_unittest.cc
+++ components/password_manager/core/browser/browser_save_password_progress_logger_unittest.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+using Logger = autofill::SavePasswordProgressLogger;
+
 namespace password_manager {
 
 namespace {
@@ -93,11 +95,10 @@ TEST_F(BrowserSavePasswordProgressLoggerTest, LogFormSignatures) {
   // Add a server prediction for the text field.
   form_structure.field(1)->set_server_type(autofill::EMAIL_ADDRESS);
 
-  logger.LogFormStructure(
-      autofill::SavePasswordProgressLogger::STRING_FORM_VOTES, form_structure);
+  logger.LogFormStructure(Logger::STRING_PASSWORD_FORM_VOTE, form_structure);
   SCOPED_TRACE(testing::Message() << "Log string = ["
                                   << logger.accumulated_log() << "]");
-  EXPECT_TRUE(logger.LogsContainSubstring("Form votes: {"));
+  EXPECT_TRUE(logger.LogsContainSubstring("PasswordForm vote: {"));
   EXPECT_TRUE(logger.LogsContainSubstring("Signature of form"));
   EXPECT_TRUE(logger.LogsContainSubstring("Origin: http://myform.com"));
   EXPECT_TRUE(logger.LogsContainSubstring("Form fields:"));
@@ -114,8 +115,7 @@ TEST_F(BrowserSavePasswordProgressLoggerTest, LogFormSignatures) {
 TEST_F(BrowserSavePasswordProgressLoggerTest, LogFormData) {
   MockLogManager log_manager;
   TestLogger logger(&log_manager);
-  logger.LogFormData(
-      autofill::SavePasswordProgressLogger::STRING_FORM_PARSING_INPUT, form_);
+  logger.LogFormData(Logger::STRING_FORM_PARSING_INPUT, form_);
   SCOPED_TRACE(testing::Message()
                << "Log string = [" << logger.accumulated_log() << "]");
   EXPECT_TRUE(logger.LogsContainSubstring("Origin: http://myform.com"));
