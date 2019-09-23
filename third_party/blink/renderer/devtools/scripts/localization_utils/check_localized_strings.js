74d5592035bc3d28f6cd7a4171cef2be2930dd4d
Mandy Chen <mandy.chen@microsoft.com>
2019-09-23 19:57:20 +0000

    DevTools: Clean up localization parsing scripts
    This CL:
    
    * updates parseLocalizableResourceMaps to return maps of localizable
    resources
    * removes localizable resources from exports
    * fixes non-async functions
    * fixes existing parsing error
    
    This CL is in preparation of https://crrev.com/c/1671848.
    
    Bug: 941561
    Change-Id: Id18faf3086fb8d9d3c75b110733784ba95cf56e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814539
    Commit-Queue: Mandy Chen <mandy.chen@microsoft.com>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699005}

--- third_party/blink/renderer/devtools/scripts/localization_utils/check_localized_strings.js
+++ third_party/blink/renderer/devtools/scripts/localization_utils/check_localized_strings.js
@@ -189,6 +189,12 @@ function validateGrdpFile(dir, grdpFiles, grdFileContent, shouldAutoFix, renameF
  * Parse localizable resources.
  */
 async function parseLocalizableResourceMaps() {
+  if (frontendStrings.size === 0 && IDSkeys.size === 0)
+    await parseLocalizableResourceMapsHelper();
+  return [frontendStrings, IDSkeys];
+}
+
+async function parseLocalizableResourceMapsHelper() {
   const grdpToFiles = new Map();
   const dirs = devtoolsFrontendDirs || await localizationUtils.getChildDirectoriesFromDirectory(devtoolsFrontendPath);
   const grdpToFilesPromises = dirs.map(dir => {
@@ -486,7 +492,7 @@ function addMessage(expectedIDSKey, actualIDSKey, grdpPath, line, description) {
  * with grdp <message>s and report error of resources to add,
  * remove or modify.
  */
-async function getAndReportResourcesToAdd() {
+function getAndReportResourcesToAdd() {
   const keysToAddToGRD = getMessagesToAdd();
   if (keysToAddToGRD.size === 0)
     return;
@@ -626,8 +632,6 @@ function getLongestDescription(messages) {
 }
 
 module.exports = {
-  frontendStrings,
-  IDSkeys,
   parseLocalizableResourceMaps,
   getAndReportIDSKeysToModify,
   getAndReportResourcesToAdd,
