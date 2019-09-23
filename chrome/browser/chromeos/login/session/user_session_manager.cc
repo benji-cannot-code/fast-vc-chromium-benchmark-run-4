babf95456a03f235bea419b77d5c38eb2fa1d094
Yulun Wu <yulunwu@chromium.org>
2019-09-23 17:10:18 +0000

    CrOS: Check channel name in user_session_manager before showing release notes notification.
    Bug: 998055
    Change-Id: Ic0421f238a581fd5c9e0c6944cd2fdf46261a641
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1817209
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698924}

--- chrome/browser/chromeos/login/session/user_session_manager.cc
+++ chrome/browser/chromeos/login/session/user_session_manager.cc
@@ -2465,7 +2465,8 @@ void UserSessionManager::MaybeShowReleaseNotesNotification(Profile* profile) {
   if (!release_notes_notification_) {
     release_notes_notification_ =
         std::make_unique<ReleaseNotesNotification>(profile);
-    release_notes_notification_->MaybeShowReleaseNotes();
+    if (chrome::GetChannel() == version_info::Channel::STABLE)
+      release_notes_notification_->MaybeShowReleaseNotes();
   }
 }
 
