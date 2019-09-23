697044ce96bb53cccbbb1aecf7f3ba14a08c1eaf
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-09-23 17:30:03 +0000

    [base] Remove base::KEEP_LAST_OF_DUPES from base::flat_tree
    This change removes base::KEEP_LAST_OF_DUPES and thus FlatContainerDupes
    from the flat associative containers. This change leads to making the
    interface of flat_tree cleaner, simplifying its implementation and
    aligning it closer to what is currently proposed for standardisation
    as std::flat_set [1] and std::flat_map [2].
    
    base::JSONParser, the only customer of KEEP_LAST_OF_DUPES, is modified
    to keep the same semantics.
    
    [1] https://wg21.link/p1222
    [2] https://wg21.link/p0429
    
    TBR=rockot,avi,thakis,kmackay
    
    Bug: 682254
    Change-Id: I2189331df969c42d0de7cc4924bed35abdaffc52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816408
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698928}

--- base/containers/flat_map.h
+++ base/containers/flat_map.h
@@ -61,15 +61,12 @@ struct GetKeyFromValuePairFirst {
 //
 // Constructors (inputs need not be sorted):
 //   flat_map(InputIterator first, InputIterator last,
-//            FlatContainerDupes = KEEP_FIRST_OF_DUPES,
 //            const Compare& compare = Compare());
 //   flat_map(const flat_map&);
 //   flat_map(flat_map&&);
 //   flat_map(std::vector<value_type>,
-//            FlatContainerDupes = KEEP_FIRST_OF_DUPES,
 //            const Compare& compare = Compare()); // Re-use storage.
 //   flat_map(std::initializer_list<value_type> ilist,
-//            FlatContainerDupes = KEEP_FIRST_OF_DUPES,
 //            const Compare& comp = Compare());
 //
 // Assignment functions:
@@ -109,8 +106,7 @@ struct GetKeyFromValuePairFirst {
 //   pair<iterator, bool> insert(value_type&&);
 //   iterator             insert(const_iterator hint, const value_type&);
 //   iterator             insert(const_iterator hint, value_type&&);
-//   void                 insert(InputIterator first, InputIterator last,
-//                               FlatContainerDupes = KEEP_FIRST_OF_DUPES);
+//   void                 insert(InputIterator first, InputIterator last);
 //   pair<iterator, bool> insert_or_assign(K&&, M&&);
 //   iterator             insert_or_assign(const_iterator hint, K&&, M&&);
 //   pair<iterator, bool> emplace(Args&&...);
@@ -184,18 +180,15 @@ class flat_map : public ::base::internal::flat_tree<
   template <class InputIterator>
   flat_map(InputIterator first,
            InputIterator last,
-           FlatContainerDupes dupe_handling = KEEP_FIRST_OF_DUPES,
            const Compare& comp = Compare());
 
   flat_map(const flat_map&) = default;
   flat_map(flat_map&&) noexcept = default;
 
   flat_map(std::vector<value_type> items,
-           FlatContainerDupes dupe_handling = KEEP_FIRST_OF_DUPES,
            const Compare& comp = Compare());
 
   flat_map(std::initializer_list<value_type> ilist,
-           FlatContainerDupes dupe_handling = KEEP_FIRST_OF_DUPES,
            const Compare& comp = Compare());
 
   ~flat_map() = default;
@@ -250,22 +243,19 @@ template <class Key, class Mapped, class Compare>
 template <class InputIterator>
 flat_map<Key, Mapped, Compare>::flat_map(InputIterator first,
                                          InputIterator last,
-                                         FlatContainerDupes dupe_handling,
                                          const Compare& comp)
-    : tree(first, last, dupe_handling, comp) {}
+    : tree(first, last, comp) {}
 
 template <class Key, class Mapped, class Compare>
 flat_map<Key, Mapped, Compare>::flat_map(std::vector<value_type> items,
-                                         FlatContainerDupes dupe_handling,
                                          const Compare& comp)
-    : tree(std::move(items), dupe_handling, comp) {}
+    : tree(std::move(items), comp) {}
 
 template <class Key, class Mapped, class Compare>
 flat_map<Key, Mapped, Compare>::flat_map(
     std::initializer_list<value_type> ilist,
-    FlatContainerDupes dupe_handling,
     const Compare& comp)
-    : flat_map(std::begin(ilist), std::end(ilist), dupe_handling, comp) {}
+    : flat_map(std::begin(ilist), std::end(ilist), comp) {}
 
 // ----------------------------------------------------------------------------
 // Assignments.
--- chrome/browser/metrics/process_memory_metrics_emitter_unittest.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter_unittest.cc
@@ -180,8 +180,7 @@ MetricMap GetExpectedBrowserMetrics() {
 #if defined(OS_LINUX) || defined(OS_ANDROID)
             {"PrivateSwapFootprint", 50},
 #endif
-      },
-      base::KEEP_FIRST_OF_DUPES);
+      });
 }
 
 void PopulateRendererMetrics(GlobalMemoryDumpPtr& global_dump,
@@ -376,8 +375,7 @@ MetricMap GetExpectedRendererMetrics() {
             {"NumberOfDocuments", 1}, {"NumberOfFrames", 2},
             {"NumberOfLayoutObjects", 5}, {"NumberOfNodes", 3},
             {"PartitionAlloc.Partitions.ArrayBuffer", 10},
-      },
-      base::KEEP_FIRST_OF_DUPES);
+      });
 }
 
 void AddPageMetrics(MetricMap& expected_metrics) {
@@ -432,8 +430,7 @@ MetricMap GetExpectedGpuMetrics() {
 #if defined(OS_LINUX) || defined(OS_ANDROID)
             {"PrivateSwapFootprint", 50},
 #endif
-      },
-      base::KEEP_FIRST_OF_DUPES);
+      });
 }
 
 void PopulateAudioServiceMetrics(GlobalMemoryDumpPtr& global_dump,
@@ -472,8 +469,7 @@ MetricMap GetExpectedAudioServiceMetrics() {
 #if defined(OS_LINUX) || defined(OS_ANDROID)
             {"PrivateSwapFootprint", 50},
 #endif
-      },
-      base::KEEP_FIRST_OF_DUPES);
+      });
 }
 
 void PopulateMetrics(GlobalMemoryDumpPtr& global_dump,
