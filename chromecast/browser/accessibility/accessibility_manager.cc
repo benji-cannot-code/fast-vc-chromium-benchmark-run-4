fc14925da9dca49aaf032e9ca63189b8a6fb9656
Ryan Daum <rdaum@chromium.org>
2019-09-23 19:39:00 +0000

    [chromecast] Clean up CastWindowManagerAura
    This source file was getting a bit hard to read. So some minor cleanups:
    
      * Pull the window tree host out into its own source file. Rename to
        CastWindowTreeHostAura to reflect naming scheme elsewhere in
        chromecast/graphics. (CastFocusClientAura, CastWindowManagerAura,
        etc.)
      * Move CastLayoutManager into the anonymous namespace
      * Fix order of methods in the window manager header.
    
    Bug: none
    Test: build & unit tests
    Change-Id: I1e9f6a3ac30417b9f02ddf2c9d4439eb7b3bc734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819639
    Commit-Queue: Ryan Daum <rdaum@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#698997}

--- chromecast/browser/accessibility/accessibility_manager.cc
+++ chromecast/browser/accessibility/accessibility_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/graphics/accessibility/focus_ring_controller.h"
 #include "chromecast/graphics/accessibility/fullscreen_magnification_controller.h"
 #include "chromecast/graphics/cast_window_manager_aura.h"
+#include "chromecast/graphics/cast_window_tree_host_aura.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
 #include "ui/wm/public/activation_client.h"
--- chromecast/graphics/cast_external_begin_frame_source.cc
+++ chromecast/graphics/cast_external_begin_frame_source.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "chromecast/graphics/cast_window_manager_aura.h"
+#include "chromecast/graphics/cast_window_tree_host_aura.h"
 
 namespace chromecast {
 namespace {
