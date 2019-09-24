88d68eb982054d5cc71b23d88f3db09ba0d5e5ce
Jesse McKenna <jessemckenna@google.com>
2019-09-24 22:22:07 +0000

    Reland "desktop-pwas: Set display name for Open With menu"
    This relands crrev.com/c/1810242, reverted by crrev.com/c/1815802.
    
    Unit test ShellUtilRegistryTest.AddFileAssociations was failing on
    Windows 7 bots when checking for registry key
    HKCU\Software\Classes\<progID>\Application, because this key is only
    created by ShellUtil::AddFileAssociations() on Windows 8 and later. I
    changed this CHECK() to only take place when on Windows 8 or later.
    
    Original change description:
    
    When an application is registered as a handler for a file type, the name
    that is displayed in the "Open With..." menu for that file type is
    controlled by registry value
    HKCR/<application ProgID>/Application::ApplicationName.
    
    This change extends ShellUtil::AddFileAssociations(), which will soon be
    used to register Progressive Web Apps (PWAs) as file handlers on
    Windows, to set ApplicationName. This enables PWAs registered as file
    handlers to appear in the "Open With..." menu with a readable display
    name such as "My PWA".
    
    Bug: 960245
    Change-Id: I0790faa2b3eedec82ace2c324ff582515af048aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1818429
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699518}

--- chrome/installer/util/shell_util_unittest.cc
+++ chrome/installer/util/shell_util_unittest.cc
@@ -42,6 +42,7 @@ const wchar_t kOtherIco[] = L"other.ico";
 // For registry tests.
 const wchar_t kTestProgid[] = L"TestApp";
 const wchar_t kTestOpenCommand[] = L"C:\\test.exe";
+const wchar_t kTestApplicationName[] = L"Test Application";
 const wchar_t kTestFileTypeName[] = L"Test File Type";
 const wchar_t kTestIconPath[] = L"D:\\test.ico";
 const wchar_t* kTestFileExtensions[] = {
@@ -833,11 +834,9 @@ class ShellUtilRegistryTest : public testing::Test {
 
 TEST_F(ShellUtilRegistryTest, AddFileAssociations) {
   // Create file associations.
-  EXPECT_TRUE(ShellUtil::AddFileAssociations(kTestProgid,
-                                             OpenCommand(),
-                                             kTestFileTypeName,
-                                             base::FilePath(kTestIconPath),
-                                             FileExtensions()));
+  EXPECT_TRUE(ShellUtil::AddFileAssociations(
+      kTestProgid, OpenCommand(), kTestApplicationName, kTestFileTypeName,
+      base::FilePath(kTestIconPath), FileExtensions()));
 
   // Ensure that the registry keys have been correctly set.
   base::win::RegKey key;
@@ -861,6 +860,15 @@ TEST_F(ShellUtilRegistryTest, AddFileAssociations) {
                      KEY_READ));
   EXPECT_EQ(ERROR_SUCCESS, key.ReadValue(L"", &value));
   EXPECT_EQ(L"\"C:\\test.exe\" \"%1\"", value);
+
+  // The Application subkey and values are only required by Windows 8 and later.
+  if (base::win::GetVersion() >= base::win::Version::WIN8) {
+    ASSERT_EQ(ERROR_SUCCESS,
+              key.Open(HKEY_CURRENT_USER,
+                       L"Software\\Classes\\TestApp\\Application", KEY_READ));
+    EXPECT_EQ(ERROR_SUCCESS, key.ReadValue(L"ApplicationName", &value));
+    EXPECT_EQ(L"Test Application", value);
+  }
 
   // .test1 should be default-associated with our test app.
   ASSERT_EQ(
@@ -894,11 +902,9 @@ TEST_F(ShellUtilRegistryTest, AddFileAssociations) {
 
 TEST_F(ShellUtilRegistryTest, DeleteFileAssociations) {
   // Create file associations.
-  EXPECT_TRUE(ShellUtil::AddFileAssociations(kTestProgid,
-                                             OpenCommand(),
-                                             kTestFileTypeName,
-                                             base::FilePath(kTestIconPath),
-                                             FileExtensions()));
+  EXPECT_TRUE(ShellUtil::AddFileAssociations(
+      kTestProgid, OpenCommand(), kTestApplicationName, kTestFileTypeName,
+      base::FilePath(kTestIconPath), FileExtensions()));
 
   // Delete them.
   EXPECT_TRUE(ShellUtil::DeleteFileAssociations(kTestProgid));
