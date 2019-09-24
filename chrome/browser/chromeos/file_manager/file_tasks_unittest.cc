983bab82611b54311e81abbe001778a615f74a36
Joel Hockey <joelhockey@chromium.org>
2019-09-24 20:51:47 +0000

    Ignore import-crostini-image FileHandler when disabled by policy
    Enterprise policy can disable crostini backup and restore.
    When it is disabled, we remove the 'import-crostini-image'
    FileHandler from FilesApp.
    
    Bug: 1003944
    Bug: 1006112
    Change-Id: I46a58a751002c308eeae3e07be796b956dba5818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808979
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699495}

--- chrome/browser/chromeos/file_manager/file_tasks_unittest.cc
+++ chrome/browser/chromeos/file_manager/file_tasks_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chromeos/crostini/crostini_mime_types_service.h"
 #include "chrome/browser/chromeos/crostini/crostini_mime_types_service_factory.h"
+#include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/chromeos/crostini/crostini_test_helper.h"
 #include "chrome/browser/chromeos/drive/file_system_util.h"
 #include "chrome/browser/chromeos/file_manager/app_id.h"
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/fake_concierge_client.h"
 #include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/pref_service.h"
 #include "components/prefs/testing_pref_service.h"
 #include "components/services/app_service/public/cpp/file_handler_info.h"
 #include "content/public/test/browser_task_environment.h"
@@ -303,6 +305,29 @@ TEST(FileManagerFileTasksTest, ChooseAndSetDefaultTask_FallbackOfficeEditing) {
   // file browser handler.
   ChooseAndSetDefaultTask(pref_service, entries, &tasks);
   EXPECT_TRUE(tasks[0].is_default());
+}
+
+// Test IsFileHandlerEnabled which returns whether a file handler should be
+// used.
+TEST(FileManagerFileTasksTest, IsFileHandlerEnabled) {
+  content::BrowserTaskEnvironment task_environment;
+  TestingProfile test_profile;
+  crostini::CrostiniTestHelper helper(&test_profile);
+
+  apps::FileHandlerInfo crostini_import_handler;
+  crostini_import_handler.id = "import-crostini-image";
+  apps::FileHandlerInfo test_handler;
+  test_handler.id = "test";
+
+  test_profile.GetPrefs()->SetBoolean(
+      crostini::prefs::kUserCrostiniExportImportUIAllowedByPolicy, true);
+  EXPECT_TRUE(IsFileHandlerEnabled(&test_profile, crostini_import_handler));
+  EXPECT_TRUE(IsFileHandlerEnabled(&test_profile, test_handler));
+
+  test_profile.GetPrefs()->SetBoolean(
+      crostini::prefs::kUserCrostiniExportImportUIAllowedByPolicy, false);
+  EXPECT_FALSE(IsFileHandlerEnabled(&test_profile, crostini_import_handler));
+  EXPECT_TRUE(IsFileHandlerEnabled(&test_profile, test_handler));
 }
 
 // Test IsGoodMatchFileHandler which returns whether a file handle info matches
