eb6517512e07324ee55cc2eb1994c15b39404974
John Z Wu <jzw@chromium.org>
2019-09-24 02:43:53 +0000

    Fix lldb script for base::string16
    - Make it work with python3's bytestring semantics.
    - Not sure why, but looks like libcpp is compiled as big endian now?
    
    Change-Id: Ia73fd960472606b0f72970674b80200a1ae467d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816642
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699167}

--- tools/lldb/lldb_chrome.py
+++ tools/lldb/lldb_chrome.py
@@ -12,16 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import lldb
 
+
 def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand('type summary add -F ' +
         'lldb_chrome.basestring16_SummaryProvider base::string16')
 
-# This is highly dependent on libc++ being compiled with little endian.
+
 def basestring16_SummaryProvider(valobj, internal_dict):
     s = valobj.GetValueForExpressionPath('.__r_.__value_.__s')
     l = valobj.GetValueForExpressionPath('.__r_.__value_.__l')
     size = s.GetChildMemberWithName('__size_').GetValueAsUnsigned(0)
-    is_short_string = size & 1 == 0
+    is_short_string = size & 128 == 0  # Assumes _LIBCPP_BIG_ENDIAN is defined.
     if is_short_string:
         length = size >> 1
         data = s.GetChildMemberWithName('__data_').GetPointeeData(0, length)
@@ -36,4 +37,4 @@ def basestring16_SummaryProvider(valobj, internal_dict):
     if error.fail:
         return 'Summary error: %s' % error.description
     else:
-        return '"' + byte_string.decode('utf-16').encode('utf-8') + '"'
+        return '"' + byte_string.decode('utf-16') + '"'
