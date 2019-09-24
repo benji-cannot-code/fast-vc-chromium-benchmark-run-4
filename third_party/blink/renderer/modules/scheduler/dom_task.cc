dd8668016c361bc75b29c598c2297a2366e068e1
Scott Haseley <shaseley@chromium.org>
2019-09-24 01:53:25 +0000

    [scheduling APIs] Prototype follow-up cleanup
    Cleaning a few things up in response to post-landing reviews of
    crrev.com/r/1735774 and crrev.com/r/1737169.
    
    The CL makes the following changes:
     - Split out the task.result mega web test into separate tests.
     - Add new task.result tests for error handling and cancel order.
     - Rename ResolveOrRejectPromiseIfNeeded to
       LazilyResolveOrRejectPromiseIfReady.
     - Add more comments.
     - Make TaskQueue's cached TaskRunner pointer const.
    
    Bug: 979017
    Change-Id: I9c44ed09a0c20ce59717ee9a9a1251fd8185b04c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1764753
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699154}

--- third_party/blink/renderer/modules/scheduler/dom_task.cc
+++ third_party/blink/renderer/modules/scheduler/dom_task.cc
@@ -68,7 +68,7 @@ void DOMTask::cancel(ScriptState* script_state) {
     return;
   CancelPendingTask();
   SetTaskStatus(Status::kCanceled);
-  ResolveOrRejectPromiseIfNeeded(script_state);
+  LazilyResolveOrRejectPromiseIfReady(script_state);
 }
 
 ScriptPromise DOMTask::result(ScriptState* script_state) {
@@ -76,7 +76,7 @@ ScriptPromise DOMTask::result(ScriptState* script_state) {
     result_promise_ = MakeGarbageCollected<TaskResultPromise>(
         ExecutionContext::From(script_state), this,
         TaskResultPromise::kFinished);
-    ResolveOrRejectPromiseIfNeeded(script_state);
+    LazilyResolveOrRejectPromiseIfReady(script_state);
   }
   return result_promise_->Promise(script_state->World());
 }
@@ -133,7 +133,7 @@ void DOMTask::Invoke() {
   InvokeInternal(script_state);
   SetTaskStatus(Status::kCompleted);
   task_queue_->GetScheduler()->OnTaskCompleted(task_queue_, this);
-  ResolveOrRejectPromiseIfNeeded(script_state);
+  LazilyResolveOrRejectPromiseIfReady(script_state);
   callback_.Release();
 }
 
@@ -152,9 +152,7 @@ void DOMTask::InvokeInternal(ScriptState* script_state) {
   result_value_.Set(script_state->GetIsolate(), result.V8Value());
 }
 
-void DOMTask::ResolveOrRejectPromiseIfNeeded(ScriptState* script_state) {
-  // Lazily resolove or reject the Promise - we don't do anything unless the
-  // result property has been accessed.
+void DOMTask::LazilyResolveOrRejectPromiseIfReady(ScriptState* script_state) {
   if (!result_promise_)
     return;
 
