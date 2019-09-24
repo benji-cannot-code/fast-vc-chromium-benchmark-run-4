4fc7eb3acaf00ae3b28ee1b0494bfd640eb44954
Robert Liao <robliao@chromium.org>
2019-09-24 02:00:54 +0000

    Cleanup NativeAppWindowViews
    Quick cleanup for NativeAppWindowViews in preparation for Layout
    changes in http://crbug.com/1005568.
    
    BUG=
    
    Change-Id: I7fcc633917ce81496750740501a1ca1b88edf0b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819913
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699159}

--- extensions/components/native_app_window/native_app_window_views.cc
+++ extensions/components/native_app_window/native_app_window_views.cc
@@ -20,22 +20,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #endif
 
-using extensions::AppWindow;
-
 namespace native_app_window {
 
-NativeAppWindowViews::NativeAppWindowViews()
-    : app_window_(NULL),
-      web_view_(NULL),
-      widget_(NULL),
-      frameless_(false),
-      resizable_(false) {
-}
+NativeAppWindowViews::NativeAppWindowViews() = default;
 
-void NativeAppWindowViews::Init(AppWindow* app_window,
-                                const AppWindow::CreateParams& create_params) {
+void NativeAppWindowViews::Init(
+    extensions::AppWindow* app_window,
+    const extensions::AppWindow::CreateParams& create_params) {
   app_window_ = app_window;
-  frameless_ = create_params.frame == AppWindow::FRAME_NONE;
+  frameless_ = create_params.frame == extensions::AppWindow::FRAME_NONE;
   resizable_ = create_params.resizable;
   size_constraints_.set_minimum_size(
       create_params.GetContentMinimumSize(gfx::Insets()));
@@ -51,7 +44,7 @@ void NativeAppWindowViews::Init(AppWindow* app_window,
 }
 
 NativeAppWindowViews::~NativeAppWindowViews() {
-  web_view_->SetWebContents(NULL);
+  web_view_->SetWebContents(nullptr);
 }
 
 void NativeAppWindowViews::OnCanHaveAlphaEnabledChanged() {
@@ -59,8 +52,8 @@ void NativeAppWindowViews::OnCanHaveAlphaEnabledChanged() {
 }
 
 void NativeAppWindowViews::InitializeWindow(
-    AppWindow* app_window,
-    const AppWindow::CreateParams& create_params) {
+    extensions::AppWindow* app_window,
+    const extensions::AppWindow::CreateParams& create_params) {
   // Stub implementation. See also ChromeNativeAppWindowViews.
   views::Widget::InitParams init_params(views::Widget::InitParams::TYPE_WINDOW);
   init_params.delegate = this;
@@ -180,8 +173,8 @@ gfx::NativeView NativeAppWindowViews::GetHostView() const {
 
 gfx::Point NativeAppWindowViews::GetDialogPosition(const gfx::Size& size) {
   gfx::Size app_window_size = widget_->GetWindowBoundsInScreen().size();
-  return gfx::Point(app_window_size.width() / 2 - size.width() / 2,
-                    app_window_size.height() / 2 - size.height() / 2);
+  return gfx::Point((app_window_size.width() - size.width()) / 2,
+                    (app_window_size.height() - size.height()) / 2);
 }
 
 gfx::Size NativeAppWindowViews::GetMaximumDialogSize() {
@@ -324,8 +317,8 @@ void NativeAppWindowViews::Layout() {
 void NativeAppWindowViews::ViewHierarchyChanged(
     const views::ViewHierarchyChangedDetails& details) {
   if (details.is_add && details.child == this) {
-    web_view_ = new views::WebView(NULL);
-    AddChildView(web_view_);
+    DCHECK(!web_view_);
+    web_view_ = AddChildView(std::make_unique<views::WebView>(nullptr));
     web_view_->SetWebContents(app_window_->web_contents());
   }
 }
@@ -346,7 +339,8 @@ void NativeAppWindowViews::OnFocus() {
 
 void NativeAppWindowViews::SetFullscreen(int fullscreen_types) {
   // Stub implementation. See also ChromeNativeAppWindowViews.
-  widget_->SetFullscreen(fullscreen_types != AppWindow::FULLSCREEN_TYPE_NONE);
+  widget_->SetFullscreen(fullscreen_types !=
+                         extensions::AppWindow::FULLSCREEN_TYPE_NONE);
 }
 
 bool NativeAppWindowViews::IsFullscreenOrPending() const {
@@ -368,7 +362,8 @@ void NativeAppWindowViews::UpdateDraggableRegions(
   if (!frameless_)
     return;
 
-  draggable_region_.reset(AppWindow::RawDraggableRegionsToSkRegion(regions));
+  draggable_region_.reset(
+      extensions::AppWindow::RawDraggableRegionsToSkRegion(regions));
   OnViewWasResized();
 }
 
