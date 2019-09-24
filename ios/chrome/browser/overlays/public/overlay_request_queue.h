831e2076c7283e155c80099e17ce380a69da760f
Kurt Horimoto <kkhorimoto@chromium.org>
2019-09-24 20:35:50 +0000

    [iOS] Allow custom OverlayRequest cancellation behavior.
    This CL adds OverlayRequestCancelHandler, an object supplied to the
    OverlayRequestQueue that can cancel an individual request.  A default
    option is available that cancels requests for all committed, document-
    changing navigations.  Other overlays that may want to cancel using
    different heuristics can supply a custom cancel handler subclass.
    
    Bug: 1005613
    Change-Id: I1113e26a52f9b910195be2a5a35416f3cc994dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1812362
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699487}

--- ios/chrome/browser/overlays/public/overlay_request_queue.h
+++ ios/chrome/browser/overlays/public/overlay_request_queue.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_OVERLAYS_PUBLIC_OVERLAY_REQUEST_QUEUE_H_
 
 #include "ios/chrome/browser/overlays/public/overlay_modality.h"
+#import "ios/chrome/browser/overlays/public/overlay_request_cancel_handler.h"
 
 class OverlayRequest;
 namespace web {
@@ -23,7 +24,14 @@ class OverlayRequestQueue {
                                            OverlayModality modality);
 
   // Adds |request| to be displayed alongside the content area of queue's
-  // corresponding WebState.
+  // corresponding WebState.  The request may be cancelled by
+  // |cancel_handler|.
+  virtual void AddRequest(
+      std::unique_ptr<OverlayRequest> request,
+      std::unique_ptr<OverlayRequestCancelHandler> cancel_handler) = 0;
+
+  // Adds |request| to the queue using a default cancellation handler that
+  // cancels requests for committed, document-changing navigations.
   virtual void AddRequest(std::unique_ptr<OverlayRequest> request) = 0;
 
   // Returns the front request in the queue, or nullptr if the queue is empty.
@@ -38,7 +46,11 @@ class OverlayRequestQueue {
   OverlayRequestQueue() = default;
 
  private:
+  friend class OverlayRequestCancelHandler;
   DISALLOW_COPY_AND_ASSIGN(OverlayRequestQueue);
+
+  // Called by cancellation handlers to cancel |request|.
+  virtual void CancelRequest(OverlayRequest* request) = 0;
 };
 
 #endif  // IOS_CHROME_BROWSER_OVERLAYS_PUBLIC_OVERLAY_REQUEST_QUEUE_H_
