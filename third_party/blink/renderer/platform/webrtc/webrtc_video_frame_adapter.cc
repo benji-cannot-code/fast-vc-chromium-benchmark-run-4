4a6336f47805a3615434cc65c1a144ae4632200e
Ricky Liang <jcliang@chromium.org>
2019-09-24 14:03:36 +0000

    Video capture with GpuMemoryBuffer - Video encoders
    This CL allows VideoTrackRecorder and WebRtcVideoFrameAdapter in Blink
    to consume GpuMemoryBuffer VideoFrame.
    
    Design doc: go/cros-camera:dd:zero-copy
    
    Bug: 982201
    Change-Id: Iab3b33940c01cde5ad4be67c326a20273c0bef6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1710552
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699328}

--- third_party/blink/renderer/platform/webrtc/webrtc_video_frame_adapter.cc
+++ third_party/blink/renderer/platform/webrtc/webrtc_video_frame_adapter.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/webrtc/webrtc_video_frame_adapter.h"
 
+#include "base/bind_helpers.h"
 #include "base/logging.h"
+#include "third_party/libyuv/include/libyuv.h"
 #include "third_party/webrtc/common_video/include/video_frame_buffer.h"
 #include "third_party/webrtc/rtc_base/ref_counted_object.h"
+#include "ui/gfx/gpu_memory_buffer.h"
 
 namespace {
 
@@ -79,6 +82,37 @@ void IsValidFrame(const media::VideoFrame& frame) {
   CHECK(frame.stride(media::VideoFrame::kVPlane));
 }
 
+scoped_refptr<media::VideoFrame> ConstructI420VideoFrame(
+    const media::VideoFrame& source_frame) {
+  // NV12 is the only supported format.
+  DCHECK_EQ(source_frame.format(), media::PIXEL_FORMAT_NV12);
+  gfx::GpuMemoryBuffer* gmb = source_frame.GetGpuMemoryBuffer();
+  if (!gmb || !gmb->Map()) {
+    return nullptr;
+  }
+  scoped_refptr<media::VideoFrame> i420_frame = media::VideoFrame::CreateFrame(
+      media::PIXEL_FORMAT_I420, source_frame.coded_size(),
+      source_frame.visible_rect(), source_frame.natural_size(),
+      source_frame.timestamp());
+  i420_frame->metadata()->MergeMetadataFrom(source_frame.metadata());
+  const auto& i420_planes = i420_frame->layout().planes();
+  int ret = libyuv::NV12ToI420(
+      reinterpret_cast<const uint8_t*>(gmb->memory(0)), gmb->stride(0),
+      reinterpret_cast<const uint8_t*>(gmb->memory(1)), gmb->stride(1),
+      i420_frame->data(media::VideoFrame::kYPlane),
+      i420_planes[media::VideoFrame::kYPlane].stride,
+      i420_frame->data(media::VideoFrame::kUPlane),
+      i420_planes[media::VideoFrame::kUPlane].stride,
+      i420_frame->data(media::VideoFrame::kVPlane),
+      i420_planes[media::VideoFrame::kVPlane].stride,
+      i420_frame->coded_size().width(), i420_frame->coded_size().height());
+  gmb->Unmap();
+  if (ret) {
+    return nullptr;
+  }
+  return i420_frame;
+}
+
 }  // anonymous namespace
 
 namespace blink {
@@ -103,6 +137,25 @@ int WebRtcVideoFrameAdapter::height() const {
 
 rtc::scoped_refptr<webrtc::I420BufferInterface>
 WebRtcVideoFrameAdapter::CreateFrameAdapter() const {
+  if (frame_->storage_type() ==
+      media::VideoFrame::StorageType::STORAGE_GPU_MEMORY_BUFFER) {
+    auto i420_frame = ConstructI420VideoFrame(*frame_);
+    if (!i420_frame) {
+      return new rtc::RefCountedObject<
+          FrameAdapter<webrtc::I420BufferInterface>>(
+          media::VideoFrame::CreateColorFrame(frame_->visible_rect().size(), 0u,
+                                              0x80, 0x80, frame_->timestamp()));
+    }
+
+    // Keep |frame_| alive until |i420_frame| is destroyed.
+    i420_frame->AddDestructionObserver(base::BindOnce(
+        base::DoNothing::Once<scoped_refptr<media::VideoFrame>>(), frame_));
+
+    IsValidFrame(*i420_frame);
+    return new rtc::RefCountedObject<FrameAdapter<webrtc::I420BufferInterface>>(
+        i420_frame);
+  }
+
   // We cant convert texture synchronously due to threading issues, see
   // https://crbug.com/663452. Instead, return a black frame (yuv = {0, 0x80,
   // 0x80}).
