bb97a1b22ebc30f560f8783260665d3d672cce7f
Kurt Horimoto <kkhorimoto@chromium.org>
2019-09-24 19:23:08 +0000

    [iOS] Update http auth overlays to use AlertOverlayCoordinator.
    This eliminates the boilerplate code of UIViewController setup in the
    coordinator, as well as the delegate and datasource capabilities
    provided to the mediator.
    
    Bug: 990070
    Change-Id: Id0a385a7ebc171f7ff103b9bda1928d1a813eac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809515
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699449}

--- ios/chrome/browser/ui/overlays/web_content_area/http_auth_dialogs/http_auth_dialog_overlay_mediator.mm
+++ ios/chrome/browser/ui/overlays/web_content_area/http_auth_dialogs/http_auth_dialog_overlay_mediator.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/alert_view_controller/alert_action.h"
 #import "ios/chrome/browser/ui/alert_view_controller/alert_consumer.h"
 #import "ios/chrome/browser/ui/elements/text_field_configuration.h"
+#import "ios/chrome/browser/ui/overlays/common/alerts/alert_overlay_mediator+subclassing.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -23,8 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface HTTPAuthDialogOverlayMediator ()
 @property(nonatomic, readonly) OverlayRequest* request;
-// The HTTP authentication config.
 @property(nonatomic, readonly) HTTPAuthOverlayRequestConfig* config;
+
+// Sets the OverlayResponse using the user input from the prompt UI.
+// |cancelled| indicates whether the alert's cancel button was tapped.
+- (void)updateResponseCancelled:(BOOL)cancelled;
 @end
 
 @implementation HTTPAuthDialogOverlayMediator
@@ -45,16 +49,59 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self.request->GetConfig<HTTPAuthOverlayRequestConfig>();
 }
 
-- (void)setConsumer:(id<AlertConsumer>)consumer {
-  if (self.consumer == consumer)
-    return;
-  _consumer = consumer;
-  [_consumer setTitle:l10n_util::GetNSStringWithFixup(IDS_LOGIN_DIALOG_TITLE)];
-  [self.consumer setMessage:base::SysUTF8ToNSString(self.config->message())];
+#pragma mark - Response helpers
+
+- (void)updateResponseCancelled:(BOOL)cancelled {
+  std::unique_ptr<OverlayResponse> response;
+  if (!cancelled) {
+    std::string user =
+        base::SysNSStringToUTF8([self.dataSource textFieldInputForMediator:self
+                                                            textFieldIndex:0]);
+    std::string password =
+        base::SysNSStringToUTF8([self.dataSource textFieldInputForMediator:self
+                                                            textFieldIndex:1]);
+    response = OverlayResponse::CreateWithInfo<HTTPAuthOverlayResponseInfo>(
+        user, password);
+  }
+  self.request->set_response(std::move(response));
+}
+
+@end
+
+@implementation HTTPAuthDialogOverlayMediator (Subclassing)
+
+- (NSString*)alertTitle {
+  return l10n_util::GetNSStringWithFixup(IDS_LOGIN_DIALOG_TITLE);
+}
+
+- (NSString*)alertMessage {
+  return base::SysUTF8ToNSString(self.config->message());
+}
+
+- (NSArray<TextFieldConfiguration*>*)alertTextFieldConfigurations {
+  NSString* defaultUsername =
+      base::SysUTF8ToNSString(self.config->default_username());
+  NSString* usernamePlaceholder =
+      l10n_util::GetNSString(IDS_IOS_HTTP_LOGIN_DIALOG_USERNAME_PLACEHOLDER);
+  NSString* passwordPlaceholder =
+      l10n_util::GetNSString(IDS_IOS_HTTP_LOGIN_DIALOG_PASSWORD_PLACEHOLDER);
+  return @[
+    [[TextFieldConfiguration alloc] initWithText:defaultUsername
+                                     placeholder:usernamePlaceholder
+                         accessibilityIdentifier:nil
+                                 secureTextEntry:NO],
+    [[TextFieldConfiguration alloc] initWithText:nil
+                                     placeholder:passwordPlaceholder
+                         accessibilityIdentifier:nil
+                                 secureTextEntry:YES]
+  ];
+}
+
+- (NSArray<AlertAction*>*)alertActions {
   __weak __typeof__(self) weakSelf = self;
   NSString* OKLabel =
       l10n_util::GetNSStringWithFixup(IDS_LOGIN_DIALOG_OK_BUTTON_LABEL);
-  [self.consumer setActions:@[
+  return @[
     [AlertAction actionWithTitle:OKLabel
                            style:UIAlertActionStyleDefault
                          handler:^(AlertAction* action) {
@@ -71,41 +118,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                            [strongSelf.delegate
                                stopDialogForMediator:strongSelf];
                          }],
-  ]];
-
-  NSString* defaultUsername =
-      base::SysUTF8ToNSString(self.config->default_username());
-  NSString* usernamePlaceholder =
-      l10n_util::GetNSString(IDS_IOS_HTTP_LOGIN_DIALOG_USERNAME_PLACEHOLDER);
-  NSString* passwordPlaceholder =
-      l10n_util::GetNSString(IDS_IOS_HTTP_LOGIN_DIALOG_PASSWORD_PLACEHOLDER);
-  [self.consumer setTextFieldConfigurations:@[
-    [[TextFieldConfiguration alloc] initWithText:defaultUsername
-                                     placeholder:usernamePlaceholder
-                         accessibilityIdentifier:nil
-                                 secureTextEntry:NO],
-    [[TextFieldConfiguration alloc] initWithText:nil
-                                     placeholder:passwordPlaceholder
-                         accessibilityIdentifier:nil
-                                 secureTextEntry:YES]
-  ]];
-}
-
-#pragma mark - Response helpers
-
-// Sets the OverlayResponse using the user input from the prompt UI.
-// |cancelled| indicates whether the alert's cancel button was tapped.
-- (void)updateResponseCancelled:(BOOL)cancelled {
-  std::unique_ptr<OverlayResponse> response;
-  if (!cancelled) {
-    std::string user =
-        base::SysNSStringToUTF8([self.dataSource userForMediator:self]);
-    std::string password =
-        base::SysNSStringToUTF8([self.dataSource passwordForMediator:self]);
-    response = OverlayResponse::CreateWithInfo<HTTPAuthOverlayResponseInfo>(
-        user, password);
-  }
-  self.request->set_response(std::move(response));
+  ];
 }
 
 @end
