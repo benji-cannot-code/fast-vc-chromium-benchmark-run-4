157999ea198a1f6c70c6da2ef6595d29a9b41d61
Kristi Park <kristipark@chromium.org>
2019-09-24 23:21:38 +0000

    [Self Share] Fix missing include for SendTabToSelfSubMenuModelTest
    M78 builders require include for base::ASCIIToUTF16.
    
    Bug: 1007538
    Change-Id: I2715b0a25d680e1ed17aa01c15dfa8228442a04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824181
    Auto-Submit: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Ramya Nagarajan <ramyan@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699541}

--- chrome/browser/ui/send_tab_to_self/send_tab_to_self_sub_menu_model_unittest.cc
+++ chrome/browser/ui/send_tab_to_self/send_tab_to_self_sub_menu_model_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/send_tab_to_self/send_tab_to_self_sub_menu_model.h"
 
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/sync/send_tab_to_self_sync_service_factory.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "components/send_tab_to_self/send_tab_to_self_sync_service.h"
