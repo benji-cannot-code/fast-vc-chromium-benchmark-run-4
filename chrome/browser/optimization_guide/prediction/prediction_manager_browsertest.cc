fdbb36928bd813bab3d232341f73dedba86e69fd
Michael Crouse <mcrouse@chromium.org>
2019-09-24 20:32:11 +0000

    Creates the OptimizationGuidePredictionModel interface.
    This change creates the interface for the PredictionModels to be
    used in the OptimizationGuide Prediction System. The interface
    provides a factory method for creating the ModelTypes supported
    by the current client. The interface implements functions common
    to all PredictionModels, including the version and providing the
    set of features required for the model to be executed.
    
    The Prediction API function is pure virtual so that all concrete
    implementations can implement the evaluation methods specific to
    their ModelType.
    
    The Slow Page Prediction model will be of DecisionTree ModelType.
    The next change will implement the decision tree evaluation.
    
    Once the PredictionManager and ModelFetcher is in place, a browsertest
    will be implemented to test the IsValid proto checks for ModelType
    and ClientModelFeatures which cannot be easily unittested.
    
    Bug: 1001194
    Change-Id: Ifae7af2712568405909632e1ae2e6beca2e001da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1817582
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699483}

--- chrome/browser/optimization_guide/prediction/prediction_manager_browsertest.cc
+++ chrome/browser/optimization_guide/prediction/prediction_manager_browsertest.cc
@@ -20,11 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
 
-class OptimizationGuidePredictionManagerBrowserTest
-    : public InProcessBrowserTest {
+namespace optimization_guide {
+
+class PredictionManagerBrowserTest : public InProcessBrowserTest {
  public:
-  OptimizationGuidePredictionManagerBrowserTest() = default;
-  ~OptimizationGuidePredictionManagerBrowserTest() override = default;
+  PredictionManagerBrowserTest() = default;
+  ~PredictionManagerBrowserTest() override = default;
 
   void SetUp() override {
     scoped_feature_list_.InitWithFeatures(
@@ -69,10 +70,10 @@ class OptimizationGuidePredictionManagerBrowserTest
   std::unique_ptr<net::EmbeddedTestServer> https_server_;
   base::test::ScopedFeatureList scoped_feature_list_;
 
-  DISALLOW_COPY_AND_ASSIGN(OptimizationGuidePredictionManagerBrowserTest);
+  DISALLOW_COPY_AND_ASSIGN(PredictionManagerBrowserTest);
 };
 
-IN_PROC_BROWSER_TEST_F(OptimizationGuidePredictionManagerBrowserTest,
+IN_PROC_BROWSER_TEST_F(PredictionManagerBrowserTest,
                        FCPReachedSessionStatisticsUpdated) {
   OptimizationGuideKeyedService* keyed_service =
       OptimizationGuideKeyedServiceFactory::GetForProfile(browser()->profile());
@@ -90,7 +91,7 @@ IN_PROC_BROWSER_TEST_F(OptimizationGuidePredictionManagerBrowserTest,
   EXPECT_EQ(1u, session_fcp->GetNumberOfSamples());
 }
 
-IN_PROC_BROWSER_TEST_F(OptimizationGuidePredictionManagerBrowserTest,
+IN_PROC_BROWSER_TEST_F(PredictionManagerBrowserTest,
                        NoFCPSessionStatisticsUnchanged) {
   OptimizationGuideKeyedService* keyed_service =
       OptimizationGuideKeyedServiceFactory::GetForProfile(browser()->profile());
@@ -114,3 +115,5 @@ IN_PROC_BROWSER_TEST_F(OptimizationGuidePredictionManagerBrowserTest,
   EXPECT_EQ(1u, session_fcp->GetNumberOfSamples());
   EXPECT_EQ(current_mean, session_fcp->GetMean());
 }
+
+}  // namespace optimization_guide
