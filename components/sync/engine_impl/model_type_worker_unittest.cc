4819f4b7907e01a8c6c1db6a75ce6ab0114fff88
Marc Treib <treib@chromium.org>
2019-09-24 12:08:10 +0000

    Sync protocol: Remove deprecated fields and replace by "reserved"
    Some deprecated fields were instead commented out, or prefixed with
    "deprecated_". Better to properly remove them, and make sure the tags
    aren't accidentally reused.
    
    Bug: 950904
    Change-Id: Ibe7515c1658adc2076f775281b5b928bc16687d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1820802
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699301}

--- components/sync/engine_impl/model_type_worker_unittest.cc
+++ components/sync/engine_impl/model_type_worker_unittest.cc
@@ -1306,7 +1306,6 @@ TEST_F(ModelTypeWorkerTest, CommitOnly) {
   const SyncEntity entity =
       server()->GetNthCommitMessage(0).commit().entries(0);
 
-  EXPECT_EQ(0, entity.attachment_id_size());
   EXPECT_FALSE(entity.has_ctime());
   EXPECT_FALSE(entity.has_deleted());
   EXPECT_FALSE(entity.has_folder());
