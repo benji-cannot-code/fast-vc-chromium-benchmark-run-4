1e479c277fe4d2ba1dc366011700465db624b995
Mike Jackson <mjackson@microsoft.com>
2019-09-24 02:43:03 +0000

    Reland Update axe-core to v3.3.2
    This change updates the version of axe-core to the latest released
    version. As part of picking up the latest version, the following
    additional changes were made:
    
    1) axe-core removed the 'href-no-hash' rule, so that been removed
       from the test list.
    
    2) Several 'link-in-text-block' tests started failing with
       axe-core v3.0.2 (and later). These tests do pass in v3.0.1, but
       are disabled as part of this change.
         - CrManagementA11yTest.SimpleTest_link_in_text_block
         - CrManagementA11yTestWithExtension.
                                    ExtensionSection_link_in_text_block
         - CrostiniAccessibilityTest.CROSTINI_link_in_text_block
         - CrExtensionsA11yTest.NoExtensions_link_in_text_block
         - MultideviceA11yTest.MULTIDEVICE_link_in_text_block
         - MultideviceFeaturesA11yTest.
                  MULTIDEVICE_FEATURES_ACCESSIBILITY_link_in_text_block
    
    3) One 'heading-order' test started failing in v3.0.0 (and later).
       This tests does pass in V2.4.2, but is disabled as part of this
       change.
         - CrExtensionsA11yTestWithMultipleExensions.
                                           WithExtensions_heading_order
    
    4) Several DevTools a11y tests were failing. This change either
       updates the baseline or excludes the rule depending upon the
       test/rule itself.
         - http/tests/devtools/a11y-axe-core/basic-a11y-test.js
         - http/tests/devtools/a11y-axe-core/console-a11y-test.js
         - http/tests/devtools/a11y-axe-core/memory/heap-profiler-a11y-test.js
         - http/tests/devtools/a11y-axe-core/sources/dom-breakpoints-pane-a11y-test.js
    
    I spent some time investigating the test failures, and it looks
    like axe-core is highlighting legitimate product issues.
    
    Change-Id: I0ef08cdace91250936f83705fb3cd0fbb35bed4b
    Bug: 984817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809407
    Commit-Queue: Mike Jackson <mjackson@microsoft.com>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699166}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/console-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/console-a11y-test.js
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await UI.viewManager.showView('console');
   const widget = await UI.viewManager.view('console').widget();
 
+  //TODO(crbug.com/1004940): expected.txt file has 'aria-input-field-name' exceptions
   await AxeCoreTestRunner.runValidation(widget.element);
   TestRunner.completeTest();
 })();
