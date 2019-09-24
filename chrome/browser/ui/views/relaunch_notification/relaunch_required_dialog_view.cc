c47fdcaa4b01afde12abbacc3928cdf718a206b1
Robert Liao <robliao@chromium.org>
2019-09-24 07:47:42 +0000

    Use FillLayout in RelaunchRequiredDialogView
    BUG=1005568
    
    Change-Id: I36dd422d65620ac945bd139b41afd274f2837f0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1820045
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699234}

--- chrome/browser/ui/views/relaunch_notification/relaunch_required_dialog_view.cc
+++ chrome/browser/ui/views/relaunch_notification/relaunch_required_dialog_view.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/text_constants.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/layout_provider.h"
 #include "ui/views/style/typography.h"
 #include "ui/views/view.h"
@@ -108,16 +109,6 @@ bool RelaunchRequiredDialogView::ShouldShowWindowIcon() const {
   return true;
 }
 
-int RelaunchRequiredDialogView::GetHeightForWidth(int width) const {
-  const gfx::Insets insets = GetInsets();
-  return body_label_->GetHeightForWidth(width - insets.width()) +
-         insets.height();
-}
-
-void RelaunchRequiredDialogView::Layout() {
-  body_label_->SetBoundsRect(GetContentsBounds());
-}
-
 gfx::Size RelaunchRequiredDialogView::CalculatePreferredSize() const {
   const int width = ChromeLayoutProvider::Get()->GetDistanceMetric(
                         DISTANCE_MODAL_DIALOG_PREFERRED_WIDTH) -
@@ -136,6 +127,7 @@ RelaunchRequiredDialogView::RelaunchRequiredDialogView(
           deadline,
           base::BindRepeating(&RelaunchRequiredDialogView::UpdateWindowTitle,
                               base::Unretained(this))) {
+  SetLayoutManager(std::make_unique<views::FillLayout>());
   chrome::RecordDialogCreation(chrome::DialogIdentifier::RELAUNCH_REQUIRED);
   set_margins(ChromeLayoutProvider::Get()->GetDialogInsetsForContentType(
       views::TEXT, views::TEXT));
