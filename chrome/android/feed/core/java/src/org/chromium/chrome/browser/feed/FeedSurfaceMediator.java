f356618ed2b9e60c47fa6b99a080a878209a51ca
Mia Glaese <glamia@chromium.org>
2019-09-24 11:42:23 +0000

    [Start Surface] Formatting tab carousel header and feed surface header.
    Formatted tab carousel header.
    Changed feed surface to allow for different/no feed header.
    Changed/formatted feed header for Single Surface.
    Removed feed header for Explore Pane in Two Pane Surface.
    
    Before
    Single Surface: https://drive.google.com/file/d/192XyBnXfhxGxOjVv1kW_ac1uuPxFFG6b/view?usp=sharing
    Explore Pane: https://drive.google.com/file/d/1BhIuD_HJDkpUmr-huuo_6sCNw_PU5z2C/view?usp=sharing
    
    After
    Single Surface: https://drive.google.com/file/d/1VdrxIBHJ1trovSyJesPa4qjg2pzGPa8s/view?usp=sharing
    Explore Pane: https://drive.google.com/file/d/1hbZ-0OEYCA4ycWdBYD2g_LwtOBAeNOaU/view?usp=sharing
    
    Bug: 982018
    
    Change-Id: Ica548d2007a9c9d029fb9a15369630a8b1fe0ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1792857
    Commit-Queue: Mia Glaese <glamia@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699294}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -51,6 +51,7 @@ class FeedSurfaceMediator
     private @Nullable SignInPromo mSignInPromo;
 
     private boolean mFeedEnabled;
+    private boolean mHasHeader;
     private boolean mTouchEnabled = true;
     private boolean mStreamContentChanged;
     private int mThumbnailWidth;
@@ -68,6 +69,7 @@ class FeedSurfaceMediator
         mSigninManager = IdentityServicesProvider.getSigninManager();
 
         mPrefChangeRegistrar = new PrefChangeRegistrar();
+        mHasHeader = mCoordinator.getSectionHeaderView() != null;
         mPrefChangeRegistrar.addObserver(Pref.NTP_ARTICLES_SECTION_ENABLED, this::updateContent);
         initialize();
         // Create the content.
@@ -96,19 +98,22 @@ class FeedSurfaceMediator
     private void updateContent() {
         mFeedEnabled = FeedProcessScopeFactory.isFeedProcessEnabled();
         if ((mFeedEnabled && mCoordinator.getStream() != null)
-                || (!mFeedEnabled && mCoordinator.getScrollViewForPolicy() != null))
+                || (!mFeedEnabled && mCoordinator.getScrollViewForPolicy() != null)) {
             return;
+        }
 
         if (mFeedEnabled) {
             mCoordinator.createStream();
-            if (mSnapScrollHelper != null)
+            if (mSnapScrollHelper != null) {
                 mSnapScrollHelper.setView(mCoordinator.getStream().getView());
+            }
             initializePropertiesForStream();
         } else {
             destroyPropertiesForStream();
             mCoordinator.createScrollViewForPolicy();
-            if (mSnapScrollHelper != null)
+            if (mSnapScrollHelper != null) {
                 mSnapScrollHelper.setView(mCoordinator.getScrollViewForPolicy());
+            }
             initializePropertiesForPolicy();
         }
     }
@@ -141,13 +146,19 @@ class FeedSurfaceMediator
 
         boolean suggestionsVisible =
                 PrefServiceBridge.getInstance().getBoolean(Pref.NTP_ARTICLES_LIST_VISIBLE);
-        Resources res = mCoordinator.getSectionHeaderView().getResources();
-        mSectionHeader =
-                new SectionHeader(res.getString(R.string.ntp_article_suggestions_section_header),
-                        suggestionsVisible, this::onSectionHeaderToggled);
-        mPrefChangeRegistrar.addObserver(Pref.NTP_ARTICLES_LIST_VISIBLE, this::updateSectionHeader);
-        mCoordinator.getSectionHeaderView().setHeader(mSectionHeader);
-        stream.setStreamContentVisibility(mSectionHeader.isExpanded());
+
+        if (mHasHeader) {
+            Resources res = mCoordinator.getSectionHeaderView().getResources();
+            mSectionHeader = new SectionHeader(
+                    res.getString(R.string.ntp_article_suggestions_section_header),
+                    suggestionsVisible, this::onSectionHeaderToggled);
+            mPrefChangeRegistrar.addObserver(
+                    Pref.NTP_ARTICLES_LIST_VISIBLE, this::updateSectionHeader);
+            mCoordinator.getSectionHeaderView().setHeader(mSectionHeader);
+        }
+        // Show feed if there is no header that would allow user to hide feed.
+        // This is currently only relevant for the two panes start surface.
+        stream.setStreamContentVisibility(mHasHeader ? mSectionHeader.isExpanded() : true);
 
         if (SignInPromo.shouldCreatePromo()) {
             mSignInPromo = new FeedSignInPromo(mSigninManager);
