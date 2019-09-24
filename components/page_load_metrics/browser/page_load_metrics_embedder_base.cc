da0d8a91e0576f02c3739740e41aff7a4f6544ca
Tao Bai <michaelbai@chromium.org>
2019-09-24 19:23:50 +0000

    Add PageLoadMetricsEmbedderBase
    PageLoadMetricsEmbedderBase registers common observers for the embedders,
    the embedders shall derive the implementation from
    PageLoadMetricsEmbedderBase and initialize its specific observers.
    
    Bug: 995880
    Change-Id: Ie60163f3191394a63fd144d52f3433a9ec513e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816801
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699450}

---
+++ components/page_load_metrics/browser/page_load_metrics_embedder_base.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/page_load_metrics/browser/page_load_metrics_embedder_base.h"
+
+#include "base/timer/timer.h"
+
+namespace page_load_metrics {
+
+PageLoadMetricsEmbedderBase::PageLoadMetricsEmbedderBase(
+    content::WebContents* web_contents)
+    : web_contents_(web_contents) {}
+
+PageLoadMetricsEmbedderBase::~PageLoadMetricsEmbedderBase() = default;
+
+void PageLoadMetricsEmbedderBase::RegisterObservers(PageLoadTracker* metrics) {
+  RegisterEmbedderObservers(metrics);
+}
+
+std::unique_ptr<base::OneShotTimer> PageLoadMetricsEmbedderBase::CreateTimer() {
+  return std::make_unique<base::OneShotTimer>();
+}
+
+}  // namespace page_load_metrics
