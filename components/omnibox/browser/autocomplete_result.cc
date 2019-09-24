d266c3e382df8420d03c47c96c5115f18272a24f
Tommy Li <tommycli@chromium.org>
2019-09-24 18:53:55 +0000

    [omnibox] Add Omnibox.MatchStability.AsyncMatchChange histogram UMA
    Adds a metric to track how unstable our matches are due to asynchronous
    updates. Whenever a dropdown position gets asynchronously set to a new
    match that's different from the old match, this histogram logs the
    index.
    
    Bug: 398135
    Change-Id: I9e7ba88c2ad8be616d933649805697aeb23cd938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1803830
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699432}

--- components/omnibox/browser/autocomplete_result.cc
+++ components/omnibox/browser/autocomplete_result.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -57,6 +58,10 @@ void RotateMatchToFront(ACMatches::iterator it, ACMatches* matches) {
   std::rotate(matches->begin(), it, next);
 }
 
+// This value should be comfortably larger than any max-autocomplete-matches
+// under consideration.
+constexpr size_t kMaxAutocompletePositionValue = 30;
+
 }  // namespace
 
 struct MatchGURLHash {
@@ -76,11 +81,16 @@ size_t AutocompleteResult::GetMaxMatches(bool is_zero_suggest) {
 #else
   constexpr size_t kDefaultMaxAutocompleteMatches = 6;
 #endif  // defined(OS_ANDROID)
+  static_assert(kMaxAutocompletePositionValue > kDefaultMaxAutocompleteMatches,
+                "kMaxAutocompletePositionValue must be larger than the largest "
+                "possible autocomplete result size.");
 
-  return base::GetFieldTrialParamByFeatureAsInt(
+  size_t field_trial_value = base::GetFieldTrialParamByFeatureAsInt(
       omnibox::kUIExperimentMaxAutocompleteMatches,
       OmniboxFieldTrial::kUIMaxAutocompleteMatchesParam,
       kDefaultMaxAutocompleteMatches);
+  DCHECK(kMaxAutocompletePositionValue > field_trial_value);
+  return field_trial_value;
 }
 
 AutocompleteResult::AutocompleteResult() {
@@ -713,6 +723,30 @@ size_t AutocompleteResult::EstimateMemoryUsage() const {
   res += base::trace_event::EstimateMemoryUsage(alternate_nav_url_);
 
   return res;
+}
+
+// static
+void AutocompleteResult::LogAsynchronousUpdateMetrics(
+    const AutocompleteResult& old_result,
+    const AutocompleteResult& new_result) {
+  constexpr char kAsyncMatchChangeHistogramName[] =
+      "Omnibox.MatchStability.AsyncMatchChange";
+
+  size_t min_size = std::min(old_result.size(), new_result.size());
+  for (size_t i = 0; i < min_size; ++i) {
+    if (GetMatchComparisonFields(old_result.match_at(i)) !=
+        GetMatchComparisonFields(new_result.match_at(i))) {
+      base::UmaHistogramExactLinear(kAsyncMatchChangeHistogramName, i,
+                                    kMaxAutocompletePositionValue);
+    }
+  }
+
+  // Also log a change for when the match count decreases. But don't make a log
+  // for appending new matches on the bottom, since that's less disruptive.
+  for (size_t i = new_result.size(); i < old_result.size(); ++i) {
+    base::UmaHistogramExactLinear(kAsyncMatchChangeHistogramName, i,
+                                  kMaxAutocompletePositionValue);
+  }
 }
 
 // static
