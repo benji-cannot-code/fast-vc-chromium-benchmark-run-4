2ad54237f5b65e05a344c0f3af84543c1a78acf8
Ken Rockot <rockot@chromium.org>
2019-09-24 07:41:48 +0000

    Reland "Move File Service off Service Manager"
    This is a reland of 749dcc3bc7930c2e9bb37b2f93792d125e02b6ff
    
    PS1->PS4 shows the exclusive delta from the original patch, fixing a subtle
    lifetime issue in FileService. This turns out to have always been wrong:
    LevelDBServiceImpl can be destroyed before its created
    LevelDBDatabaseImpl objects have received all messages, and the service
    impl owns the shared message pipe for the associated database interfaces; thus
    messages can be dropped and data lost. This never mattered before because
    FileService instances effectively lived forever anyway.
    
    The fix here makes LevelDBServiceImpl lifetime independent of FileService
    lifetime, tying it instead to client lifetime.
    
    Original change's description:
    > Move File Service off Service Manager
    >
    > This is a first step towards eliminating the File Service and migrating
    > DOM Storage code into the Storage Service.
    >
    > File Service and its only clients (DOM storage objects) are moved off of
    > Service Manager APIs. LocalStorageContextMojo and
    > SessionStorageContextMojo are changed to own their own FileService
    > instance, now constructed directly over a directory path.
    >
    > No functional changes intended in this CL.
    >
    > Bug: 994911,977637
    > Change-Id: I2ffd0bef670304a0dc3c103981139603521a14ac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1778801
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698487}
    
    NOPRESUBMIT=true
    
    Bug: 994911, 977637
    Change-Id: I12658247e428f2e5eee32a2e3eabe3eb35381238
    Tbr: avi@chromium.org
    Tbr: mek@chromium.org
    Tbr: rsesek@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816582
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#699231}

--- content/browser/dom_storage/session_storage_context_mojo_unittest.cc
+++ content/browser/dom_storage/session_storage_context_mojo_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/guid.h"
 #include "base/memory/ref_counted.h"
 #include "base/sequenced_task_runner.h"
@@ -24,9 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/dom_storage/session_storage_database.h"
 #include "content/browser/dom_storage/test/fake_leveldb_database_error_on_write.h"
 #include "content/browser/dom_storage/test/fake_leveldb_service.h"
-#include "content/browser/dom_storage/test/mojo_test_with_file_service.h"
 #include "content/browser/dom_storage/test/storage_area_test_util.h"
 #include "content/public/browser/session_storage_usage_info.h"
+#include "content/public/test/browser_task_environment.h"
 #include "content/public/test/test_browser_context.h"
 #include "content/public/test/test_utils.h"
 #include "mojo/core/embedder/embedder.h"
@@ -35,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/bindings/self_owned_associated_receiver.h"
-#include "services/file/public/mojom/constants.mojom.h"
+#include "services/file/file_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 
@@ -57,13 +58,19 @@ void GetStorageUsageCallback(base::OnceClosure callback,
   std::move(callback).Run();
 }
 
-class SessionStorageContextMojoTest : public test::MojoTestWithFileService {
+class SessionStorageContextMojoTest : public testing::Test {
  public:
-  SessionStorageContextMojoTest() {}
+  SessionStorageContextMojoTest() { CHECK(temp_dir_.CreateUniqueTempDir()); }
 
   ~SessionStorageContextMojoTest() override {
     if (context_)
       ShutdownContext();
+
+    // There may be pending tasks to clean up files in the temp dir. Make sure
+    // they run so temp dir deletion can succeed.
+    task_environment_.RunUntilIdle();
+
+    EXPECT_TRUE(temp_dir_.Delete());
   }
 
   void SetUp() override {
@@ -98,8 +105,8 @@ class SessionStorageContextMojoTest : public test::MojoTestWithFileService {
   SessionStorageContextMojo* context() {
     if (!context_) {
       context_ = new SessionStorageContextMojo(
-          base::SequencedTaskRunnerHandle::Get(), connector(), backing_mode_,
-          base::FilePath(), kSessionStorageDirectory);
+          temp_path(), base::SequencedTaskRunnerHandle::Get(), backing_mode_,
+          kSessionStorageDirectory);
     }
     return context_;
   }
@@ -164,9 +171,14 @@ class SessionStorageContextMojoTest : public test::MojoTestWithFileService {
   }
 
  protected:
+  const base::FilePath& temp_path() const { return temp_dir_.GetPath(); }
+  void RunUntilIdle() { task_environment_.RunUntilIdle(); }
+
   bool bad_message_called_ = false;
 
  private:
+  BrowserTaskEnvironment task_environment_;
+  base::ScopedTempDir temp_dir_;
   TestBrowserContext browser_context_;
   SessionStorageContextMojo::BackingMode backing_mode_ =
       SessionStorageContextMojo::BackingMode::kRestoreDiskState;
@@ -644,7 +656,7 @@ TEST_F(SessionStorageContextMojoTest, RecreateOnCommitFailure) {
   url::Origin origin3 = url::Origin::Create(GURL("http://example.com"));
 
   test::FakeLevelDBService fake_leveldb_service;
-  file_service()->GetBinderMapForTesting().Add(
+  context()->GetFileServiceForTesting()->OverrideLevelDBBinderForTesting(
       base::BindRepeating(&test::FakeLevelDBService::Bind,
                           base::Unretained(&fake_leveldb_service)));
 
@@ -782,7 +794,7 @@ TEST_F(SessionStorageContextMojoTest, DontRecreateOnRepeatedCommitFailure) {
   url::Origin origin1 = url::Origin::Create(GURL("http://foobar.com"));
 
   test::FakeLevelDBService fake_leveldb_service;
-  file_service()->GetBinderMapForTesting().Add(
+  context()->GetFileServiceForTesting()->OverrideLevelDBBinderForTesting(
       base::BindRepeating(&test::FakeLevelDBService::Bind,
                           base::Unretained(&fake_leveldb_service)));
 
--- content/browser/dom_storage/test/fake_leveldb_service.h
+++ content/browser/dom_storage/test/fake_leveldb_service.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "services/file/public/mojom/file_system.mojom.h"
-#include "services/service_manager/public/cpp/bind_source_info.h"
 
 namespace content {
 namespace test {
@@ -86,8 +85,7 @@ class FakeLevelDBService : public leveldb::mojom::LevelDBService {
     on_open_callback_ = std::move(on_open_callback);
   }
 
-  void Bind(const service_manager::Identity& remote_identity,
-            mojo::PendingReceiver<leveldb::mojom::LevelDBService> receiver);
+  void Bind(mojo::PendingReceiver<leveldb::mojom::LevelDBService> receiver);
 
   void FlushBindingsForTesting();
 
