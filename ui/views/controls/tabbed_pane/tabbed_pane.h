c38b2d6499c42d9901b3bd2a087a514547c20736
Peter Kasting <pkasting@chromium.org>
2019-09-24 14:26:39 +0000

    Convert TabbedPane to use LayoutManagers.
    Bug: 1005568
    Change-Id: Icae338018be268ac2ec395f5926efd4fc6efe764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819564
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699335}

--- ui/views/controls/tabbed_pane/tabbed_pane.h
+++ ui/views/controls/tabbed_pane/tabbed_pane.h
@@ -87,9 +87,6 @@ class VIEWS_EXPORT TabbedPane : public View {
   // Selects |tab| (the tabstrip view, not its content) if it is valid.
   void SelectTab(Tab* tab);
 
-  // Overridden from View:
-  gfx::Size CalculatePreferredSize() const override;
-
   // Gets the orientation of the tab alignment.
   Orientation GetOrientation() const;
 
@@ -126,7 +123,6 @@ class VIEWS_EXPORT TabbedPane : public View {
   bool MoveSelectionBy(int delta);
 
   // Overridden from View:
-  void Layout() override;
   void ViewHierarchyChanged(
       const ViewHierarchyChangedDetails& details) override;
   bool AcceleratorPressed(const ui::Accelerator& accelerator) override;
@@ -208,10 +204,7 @@ class TabStrip : public View, public gfx::AnimationDelegate {
            TabbedPane::TabStripStyle style);
   ~TabStrip() override;
 
-  // Overridden from View:
-  void OnPaintBorder(gfx::Canvas* canvas) override;
-
-  // Overridden from AnimationDelegate:
+  // AnimationDelegate:
   void AnimationProgressed(const gfx::Animation* animation) override;
   void AnimationEnded(const gfx::Animation* animation) override;
 
@@ -228,6 +221,11 @@ class TabStrip : public View, public gfx::AnimationDelegate {
   TabbedPane::Orientation GetOrientation() const;
 
   TabbedPane::TabStripStyle GetStyle() const;
+
+ protected:
+  // View:
+  gfx::Size CalculatePreferredSize() const override;
+  void OnPaintBorder(gfx::Canvas* canvas) override;
 
  private:
   // The orientation of the tab alignment.
