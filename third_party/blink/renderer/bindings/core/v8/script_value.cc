83660c0fcf4bfe197b1253ad850ed4564d7825e7
Rika Fujimaki <rikaf@google.com>
2019-09-24 11:14:30 +0000

    Replace SharedPersistent with WorldSafeV8Reference Replace ScriptState* with Isolate*
    We replace SharedPersistent with WorldSafeV8Reference to remove
    persistent, and to ensure v8::Local<v8::Value> is world safe. (ref. V8Value())
    
    We also replace ScriptState* with Isolate* because ScriptValue doesn't
    depend on context any more.
    
    Bug: 1000152, 998994
    Change-Id: If232d1da9579cf73157e400bf0c40950b2f3a095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1787672
    Commit-Queue: Rika Fujimaki <rikaf@google.com>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699288}

--- third_party/blink/renderer/bindings/core/v8/script_value.cc
+++ third_party/blink/renderer/bindings/core/v8/script_value.cc
@@ -36,43 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 
 namespace blink {
-namespace {
-
-v8::Local<v8::Value> ToWorldSafeValue(ScriptState* target_script_state,
-                                      v8::Local<v8::Value> value) {
-  if (value.IsEmpty() || !value->IsObject())
-    return value;
-
-  v8::Local<v8::Context> creation_context =
-      value.As<v8::Object>()->CreationContext();
-  v8::Isolate* isolate = target_script_state->GetIsolate();
-  if (&ScriptState::From(creation_context)->World() ==
-      &target_script_state->World()) {
-    return value;
-  }
-
-  v8::Context::Scope target_context_scope(target_script_state->GetContext());
-  scoped_refptr<SerializedScriptValue> serialized =
-      SerializedScriptValue::SerializeAndSwallowExceptions(isolate, value);
-  return serialized->Deserialize(isolate);
-}
-
-}  // namespace
 
 v8::Local<v8::Value> ScriptValue::V8Value() const {
   if (IsEmpty())
     return v8::Local<v8::Value>();
 
   DCHECK(GetIsolate()->InContext());
-
-  // This is a check to validate that you don't return a ScriptValue to a world
-  // different from the world that created the ScriptValue.
-  // Probably this could be:
-  //   if (&script_state_->world() == &DOMWrapperWorld::current(isolate()))
-  //       return v8::Local<v8::Value>();
-  // instead of triggering CHECK.
-  CHECK_EQ(&script_state_->World(), &DOMWrapperWorld::Current(GetIsolate()));
-  return value_->NewLocal(GetIsolate());
+  return value_.Get(ScriptState::From(isolate_->GetCurrentContext()));
 }
 
 v8::Local<v8::Value> ScriptValue::V8ValueFor(
@@ -80,15 +50,13 @@ v8::Local<v8::Value> ScriptValue::V8ValueFor(
   if (IsEmpty())
     return v8::Local<v8::Value>();
 
-  return ToWorldSafeValue(target_script_state,
-                          value_->NewLocal(target_script_state->GetIsolate()));
+  return value_.GetAcrossWorld(target_script_state);
 }
 
 bool ScriptValue::ToString(String& result) const {
   if (IsEmpty())
     return false;
 
-  ScriptState::Scope scope(script_state_);
   v8::Local<v8::Value> string = V8Value();
   if (string.IsEmpty() || !string->IsString())
     return false;
@@ -96,8 +64,10 @@ bool ScriptValue::ToString(String& result) const {
   return true;
 }
 
+// TODO(rikaf): Replace with CreateNull(v8::Isolate*).
 ScriptValue ScriptValue::CreateNull(ScriptState* script_state) {
-  return ScriptValue(script_state, v8::Null(script_state->GetIsolate()));
+  return ScriptValue(script_state->GetIsolate(),
+                     v8::Null(script_state->GetIsolate()));
 }
 
 }  // namespace blink
--- third_party/blink/renderer/core/script/module_script.h
+++ third_party/blink/renderer/core/script/module_script.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/bindings/core/v8/module_record.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
+#include "third_party/blink/renderer/bindings/core/v8/world_safe_v8_reference.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/script/modulator.h"
 #include "third_party/blink/renderer/core/script/script.h"
@@ -116,10 +117,10 @@ class CORE_EXPORT ModuleScript : public Script {
   //   https://github.com/whatwg/html/pull/2991. This shouldn't cause any
   //   observable functional changes, and updating the classic script handling
   //   will require moderate code changes (e.g. to move compilation timing).
-  TraceWrapperV8Reference<v8::Value> parse_error_;
+  WorldSafeV8Reference<v8::Value> parse_error_;
 
   // https://html.spec.whatwg.org/C/#concept-script-error-to-rethrow
-  TraceWrapperV8Reference<v8::Value> error_to_rethrow_;
+  WorldSafeV8Reference<v8::Value> error_to_rethrow_;
 
   mutable HashMap<String, KURL> specifier_to_url_cache_;
   KURL source_url_;
