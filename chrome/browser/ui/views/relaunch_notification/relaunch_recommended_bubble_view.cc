de84bcb3aa9198020bbafc9d6f7bfe866048fa23
Robert Liao <robliao@chromium.org>
2019-09-24 07:46:44 +0000

    Use FillLayout in RelaunchRecommendedBubbleView
    BUG=1005568
    
    Change-Id: Ibbf77ef3de28e836d85afd2f2a9a1fa87cce1b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1820000
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699233}

--- chrome/browser/ui/views/relaunch_notification/relaunch_recommended_bubble_view.cc
+++ chrome/browser/ui/views/relaunch_notification/relaunch_recommended_bubble_view.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/layout/fill_layout.h"
 #include "ui/views/style/typography.h"
 #include "ui/views/widget/widget.h"
 
@@ -108,17 +109,8 @@ bool RelaunchRecommendedBubbleView::ShouldShowWindowIcon() const {
   return true;
 }
 
-int RelaunchRecommendedBubbleView::GetHeightForWidth(int width) const {
-  const gfx::Insets insets = GetInsets();
-  return body_label_->GetHeightForWidth(width - insets.width()) +
-         insets.height();
-}
-
-void RelaunchRecommendedBubbleView::Layout() {
-  body_label_->SetBoundsRect(GetContentsBounds());
-}
-
 void RelaunchRecommendedBubbleView::Init() {
+  SetLayoutManager(std::make_unique<views::FillLayout>());
   body_label_ =
       new views::Label(l10n_util::GetStringUTF16(IDS_RELAUNCH_RECOMMENDED_BODY),
                        views::style::CONTEXT_MESSAGE_BOX_BODY_TEXT);
