b31623d55f490f7d7814c9a1ce10f5b56b74f601
Carlos Caballero <carlscab@google.com>
2019-09-25 08:18:55 +0000

    Remove MessageLoopForIO references
    Remove references in /device/bluetooth/bluez
    
    MessageLoopForIO is going away soon. Use SingleThreadTaskExecutor for
    main functions and SingleThreadTaskEnvironment for tests instead. Both
    are no op changes.
    
    This CL was uploaded by git cl split.
    
    R=mcchou@chromium.org
    
    Bug: 891670
    Change-Id: I4b67ceda2cf508e5f5d76493c5d2aa05e3619924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1780738
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699662}

--- device/bluetooth/bluez/bluetooth_advertisement_bluez_unittest.cc
+++ device/bluetooth/bluez/bluetooth_advertisement_bluez_unittest.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/task_environment.h"
 #include "device/bluetooth/bluetooth_adapter.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/bluetooth/bluetooth_advertisement.h"
@@ -167,7 +167,8 @@ class BluetoothAdvertisementBlueZTest : public testing::Test {
 
   BluetoothAdvertisement::ErrorCode last_error_code_;
 
-  base::MessageLoopForIO message_loop_;
+  base::test::SingleThreadTaskEnvironment task_environment_{
+      base::test::SingleThreadTaskEnvironment::MainThreadType::IO};
 
   std::unique_ptr<TestBluetoothAdvertisementObserver> observer_;
   scoped_refptr<BluetoothAdapter> adapter_;
