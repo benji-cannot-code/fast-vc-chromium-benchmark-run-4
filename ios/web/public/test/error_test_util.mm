14b24e400f88383f7bf110ffc5920bd9b2b0df59
Eugene But <eugenebut@google.com>
2019-09-25 08:54:54 +0000

    Reland: Remove unused headers from ios/web/public/web_state
    Relanding crrv.com/c/1816807
    
    This CL removes web_state_observer.h and web_state.h from
    ios/web/public/web_state which are no longer used in downlstream repo
    (downstream code was switched to use files from ios/web/public).
    
    Bug: 942244
    Change-Id: I9a6000fd421f9e6a5ac81e45a08d0761d0b9134f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1818222
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699670}

--- ios/web/public/test/error_test_util.mm
+++ ios/web/public/test/error_test_util.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
-#import "ios/web/public/web_state/web_state.h"
+#import "ios/web/public/web_state.h"
 #import "ios/web/web_view/error_translation_util.h"
 #include "url/gurl.h"
 
