95c014314767321e66e0f21a6890de56a16f3078
Peter K. Lee <pkl@chromium.org>
2019-09-25 23:08:39 +0000

    Global variables should not be declared in header files
    Declare them as extern in .h files and have a single implementation
    in .mm file. Nothing bad happened fortunately because this variable is
    a const, but it's wrong.
    
    Bug: None
    Change-Id: Ia9bcb4121594fae9dd6c95679cdcdf6b67f411ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825170
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Peter Lee <pkl@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699998}

--- ios/chrome/browser/ui/settings/language/language_settings_histograms.h
+++ ios/chrome/browser/ui/settings/language/language_settings_histograms.h
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_SETTINGS_LANGUAGE_LANGUAGE_SETTINGS_HISTOGRAMS_H_
 #define IOS_CHROME_BROWSER_UI_SETTINGS_LANGUAGE_LANGUAGE_SETTINGS_HISTOGRAMS_H_
 
-// UMA histogram names. These constants are repeated in LanguagesManager.java.
-const char kLanguageSettingsPageImpressionHistogram[] =
-    "LanguageSettings.PageImpression";
-const char kLanguageSettingsActionsHistogram[] = "LanguageSettings.Actions";
+// UMA histogram names.
+extern const char kLanguageSettingsPageImpressionHistogram[];
+extern const char kLanguageSettingsActionsHistogram[];
 
 // Enum for the LanguageSettings.PageImpression histogram. These constants are
 // repeated in LanguagesManager.java.
