8b6941242858d24a0cf09a1cfe37447b4fb9c016
Hiroki Nakagawa <nhiroki@chromium.org>
2019-09-25 05:09:31 +0000

    Sheriff: Disable WebEmbeddedWorkerImplTest for test flakiness
    Bug: 1007616
    Change-Id: I8f9d11ec78dd43210b3b3d5293f0b69b1a992fad
    Tbr: nhiroki@chromium.org
    Notry: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824526
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699628}

--- third_party/blink/renderer/modules/service_worker/web_embedded_worker_impl_test.cc
+++ third_party/blink/renderer/modules/service_worker/web_embedded_worker_impl_test.cc
@@ -233,7 +233,8 @@ class WebEmbeddedWorkerImplTest : public testing::Test {
 
 }  // namespace
 
-TEST_F(WebEmbeddedWorkerImplTest, TerminateSoonAfterStart) {
+// Disabled due to flakiness: https://crbug.com/1007616
+TEST_F(WebEmbeddedWorkerImplTest, DISABLED_TerminateSoonAfterStart) {
   worker_->StartWorkerContext(
       CreateStartData(),
       /*installed_scripts_manager_params=*/nullptr,
@@ -246,7 +247,8 @@ TEST_F(WebEmbeddedWorkerImplTest, TerminateSoonAfterStart) {
   worker_->WaitForShutdownForTesting();
 }
 
-TEST_F(WebEmbeddedWorkerImplTest, TerminateWhileWaitingForDebugger) {
+// Disabled due to flakiness: https://crbug.com/1007616
+TEST_F(WebEmbeddedWorkerImplTest, DISABLED_TerminateWhileWaitingForDebugger) {
   std::unique_ptr<WebEmbeddedWorkerStartData> start_data = CreateStartData();
   start_data->wait_for_debugger_mode =
       WebEmbeddedWorkerStartData::kWaitForDebugger;
@@ -261,7 +263,8 @@ TEST_F(WebEmbeddedWorkerImplTest, TerminateWhileWaitingForDebugger) {
   worker_->WaitForShutdownForTesting();
 }
 
-TEST_F(WebEmbeddedWorkerImplTest, TerminateWhileLoadingScript) {
+// Disabled due to flakiness: https://crbug.com/1007616
+TEST_F(WebEmbeddedWorkerImplTest, DISABLED_TerminateWhileLoadingScript) {
   // Load the shadow page.
   worker_->StartWorkerContext(
       CreateStartData(),
@@ -276,7 +279,8 @@ TEST_F(WebEmbeddedWorkerImplTest, TerminateWhileLoadingScript) {
   worker_->WaitForShutdownForTesting();
 }
 
-TEST_F(WebEmbeddedWorkerImplTest, ScriptNotFound) {
+// Disabled due to flakiness: https://crbug.com/1007616
+TEST_F(WebEmbeddedWorkerImplTest, DISABLED_ScriptNotFound) {
   // Load the shadow page.
   WebURL script_url = url_test_helpers::ToKURL(kNotFoundScriptURL);
   WebURLResponse response;
