b5c07aec9bf96056f57abf9eb6accf34437947d0
Shakti Sahu <shaktisahu@chromium.org>
2019-09-25 18:01:13 +0000

    Ephemeral tab : Several fixes
    This CL adds several fixes:
    1 - Adds custom height for full state
    2 - Fixes a crash when trying to obtain ChromeActivity by using
     ActivityWindowAndroid. This is required especially during text input.
    
    Bug: 998826
    Change-Id: Ib9a7bb370c7c230d2ca22064d92f23d1161ae0e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815053
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699867}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabSheetContent.java
@@ -27,7 +27,7 @@ import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet;
 import org.chromium.components.embedder_support.view.ContentView;
 import org.chromium.content_public.browser.RenderCoordinates;
 import org.chromium.content_public.browser.WebContents;
-import org.chromium.ui.base.WindowAndroid;
+import org.chromium.ui.base.ActivityWindowAndroid;
 
 /**
  * Represents ephemeral tab content and the toolbar, which can be included inside the bottom sheet.
@@ -82,7 +82,7 @@ public class EphemeralTabSheetContent implements BottomSheet.BottomSheetContent 
      * bottom sheet.
      */
     private void createThinWebView() {
-        mThinWebView = ThinWebViewFactory.create(mContext, new WindowAndroid(mContext));
+        mThinWebView = ThinWebViewFactory.create(mContext, new ActivityWindowAndroid(mContext));
 
         mSheetContentView = new FrameLayout(mContext);
         mSheetContentView.addView(mThinWebView.getView());
@@ -191,6 +191,11 @@ public class EphemeralTabSheetContent implements BottomSheet.BottomSheetContent 
     @Override
     public boolean wrapContentEnabled() {
         return true;
+    }
+
+    @Override
+    public float getCustomFullRatio() {
+        return 0.9f;
     }
 
     // TODO(shaktisahu): Provide correct strings for the following methods.
--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -1214,8 +1214,8 @@ public class BottomSheet
      * @return The height of the sheet at the provided state.
      */
     private float getSheetHeightForState(@SheetState int state) {
-        if (mSheetContent != null && mSheetContent.wrapContentEnabled()
-                && state == SheetState.FULL) {
+        if (mSheetContent != null && mSheetContent.wrapContentEnabled() && state == SheetState.FULL
+                && mSheetContent.getCustomFullRatio() == INVALID_HEIGHT_RATIO) {
             ensureContentDesiredHeightIsComputed();
             return mContentDesiredHeight + mToolbarShadowHeight;
         }
