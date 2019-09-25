e4e657b09b40df2f134f468e92ac560a6be35627
Anastasia Helfinstein <anastasi@google.com>
2019-09-25 16:24:09 +0000

    [Switch Access] Enable dictation from Switch Access menu
    The Switch Access menu currently contains a button to activate
    dictation. However, if the user has not already enabled the feature,
    the button does nothing.
    
    This change has Switch Access enable dictation when activated.
    If they used dictation previously, it enables and activates immediately.
    If they have not used dictation before, it opens a confirmation dialog.
    
    Bug: None
    Change-Id: I981ba4666a89909055d27996e256d6ca8a89aa13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824318
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699822}

--- ash/accessibility/accessibility_controller_impl.cc
+++ ash/accessibility/accessibility_controller_impl.cc
@@ -883,6 +883,7 @@ void AccessibilityControllerImpl::ToggleDictationFromSource(
   base::RecordAction(base::UserMetricsAction("Accel_Toggle_Dictation"));
   UserMetricsRecorder::RecordUserToggleDictation(source);
 
+  SetDictationEnabled(true);
   ToggleDictation();
 }
 
