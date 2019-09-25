a03554013cf5f6033b5c44e87890c24f4d08f707
Yuwei Huang <yuweih@chromium.org>
2019-09-25 01:41:21 +0000

    [remoting][mobile] Notification client
    This CL implements a notification client for mobile clients to fetch
    notification from gstatic. Currently it only has logic to match rules
    and select a notification. It isn't connected to gstatic or any UI
    component yet.
    
    Bug: 1001291
    Change-Id: I2d58b53363b1db0b9bb836dfcfb5ec2a36126d19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1812398
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699582}

---
+++ remoting/client/notification/notification_message.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/client/notification/notification_message.h"
+
+namespace remoting {
+
+NotificationMessage::NotificationMessage() = default;
+NotificationMessage::NotificationMessage(const NotificationMessage&) = default;
+NotificationMessage::NotificationMessage(NotificationMessage&&) = default;
+NotificationMessage::~NotificationMessage() = default;
+NotificationMessage& NotificationMessage::operator=(
+    const NotificationMessage&) = default;
+NotificationMessage& NotificationMessage::operator=(NotificationMessage&&) =
+    default;
+
+}  // namespace remoting
