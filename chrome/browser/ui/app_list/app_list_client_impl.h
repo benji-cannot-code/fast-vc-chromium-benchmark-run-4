159f9966ff7a1ec64749421220a14392de796d34
Thanh Nguyen <thanhdng@chromium.org>
2019-09-25 19:13:42 +0000

    [offline-model] Move SearchRankingEventLogger to search_result_ranker
    This CL doesn't change any logic. It moves SearchRankingEventLogger to
    search_result_ranker, which is a more suitable place. The proto is
    deleted because it's not currently being used.
    
    This CL will be followed by several CLs that refactors the class.
    
    Bug: 1006133
    Change-Id: I1b9180a6b9613d0627a1a00ca8cf55204c4987b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816058
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699911}

--- chrome/browser/ui/app_list/app_list_client_impl.h
+++ chrome/browser/ui/app_list/app_list_client_impl.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/ui/app_list/app_list_controller_delegate.h"
-#include "chrome/browser/ui/app_list/search/logging/search_ranking_event_logger.h"
+#include "chrome/browser/ui/app_list/search/search_result_ranker/search_ranking_event_logger.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/template_url_service_observer.h"
 #include "components/user_manager/user_manager.h"
--- chrome/browser/ui/app_list/search/search_result_ranker/search_ranking_event_logger_unittest.cc
+++ chrome/browser/ui/app_list/search/search_result_ranker/search_ranking_event_logger_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/app_list/search/logging/search_ranking_event_logger.h"
+#include "chrome/browser/ui/app_list/search/search_result_ranker/search_ranking_event_logger.h"
 
 #include <map>
 #include <memory>
