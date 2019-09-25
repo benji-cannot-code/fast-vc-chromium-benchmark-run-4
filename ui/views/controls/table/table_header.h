56cbde03e047cfdaf87e9b8ac9ca58d548be0b63
Allen Bauer <kylixrd@chromium.org>
2019-09-25 18:01:52 +0000

    Removed TableHeader::Layout(). Does header layout in TableView::Layout().
    Bug: 1005568
    Change-Id: Idd5cec9dea5e717279487a5601c16522c78278c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825458
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699868}

--- ui/views/controls/table/table_header.h
+++ ui/views/controls/table/table_header.h
@@ -35,7 +35,6 @@ class VIEWS_EXPORT TableHeader : public views::View {
                                TableView::AdvanceDirection direction);
 
   // views::View overrides.
-  void Layout() override;
   void OnPaint(gfx::Canvas* canvas) override;
   const char* GetClassName() const override;
   gfx::Size CalculatePreferredSize() const override;
