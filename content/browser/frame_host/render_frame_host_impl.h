b395f12955f0b367b946a0cfc401b8db9dbdd2b8
Oksana Zhuravlova <oksamyt@chromium.org>
2019-09-25 17:33:18 +0000

    [mojo] Convert PushMessagingClient to use BrowserInterfaceBroker
    Bug: 995422
    Change-Id: I384e85321bdc8987e0e39a633833bb242354dfc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1811990
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699853}

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -1084,6 +1084,9 @@ class CONTENT_EXPORT RenderFrameHostImpl
   void CreateWebBluetoothService(
       mojo::PendingReceiver<blink::mojom::WebBluetoothService> receiver);
 
+  void GetPushMessaging(
+      mojo::PendingReceiver<blink::mojom::PushMessaging> receiver);
+
   // https://mikewest.github.io/corpp/#initialize-embedder-policy-for-global
   network::mojom::CrossOriginEmbedderPolicy cross_origin_embedder_policy()
       const {
@@ -1598,8 +1601,6 @@ class CONTENT_EXPORT RenderFrameHostImpl
       mojo::PendingReceiver<blink::mojom::CredentialManager> receiver) override;
   void GetAuthenticator(
       mojo::PendingReceiver<blink::mojom::Authenticator> receiver) override;
-  void GetPushMessaging(
-      mojo::PendingReceiver<blink::mojom::PushMessaging> receiver) override;
   void GetVirtualAuthenticatorManager(
       mojo::PendingReceiver<blink::test::mojom::VirtualAuthenticatorManager>
           receiver) override;
--- third_party/blink/renderer/modules/push_messaging/push_messaging_client.cc
+++ third_party/blink/renderer/modules/push_messaging/push_messaging_client.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "third_party/blink/public/mojom/frame/document_interface_broker.mojom-blink.h"
+#include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/public/mojom/push_messaging/push_messaging_status.mojom-blink.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/web/web_local_frame.h"
@@ -44,7 +44,7 @@ PushMessagingClient* PushMessagingClient::From(LocalFrame* frame) {
 
 mojom::blink::PushMessaging* PushMessagingClient::GetPushMessagingRemote() {
   if (!push_messaging_manager_) {
-    GetSupplementable()->GetDocumentInterfaceBroker().GetPushMessaging(
+    GetSupplementable()->GetBrowserInterfaceBroker().GetInterface(
         push_messaging_manager_.BindNewPipeAndPassReceiver(
             GetSupplementable()->GetTaskRunner(TaskType::kMiscPlatformAPI)));
   }
