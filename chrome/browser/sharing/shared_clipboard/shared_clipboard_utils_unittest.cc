6958bccc6bd04d510b7d549580914a6836c1b76b
Yasmin <yasmo@chromium.org>
2019-09-25 16:17:57 +0000

    Create Shared Clipboard Chrome Enterprise Policy.
    Shared Clipboard feature is available in Win, Mac, Linux, Android and ChromeOS.
    The default value for this policy should be True.
    It's up to the admins to make sure this value is set consistently across all platforms.
    
    Bug: 1005288
    Change-Id: Ida556ccf633aa4cedc62d46cfe35ffa3123bc77b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819443
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Yasmin Molazadeh <yasmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699817}

--- chrome/browser/sharing/shared_clipboard/shared_clipboard_utils_unittest.cc
+++ chrome/browser/sharing/shared_clipboard/shared_clipboard_utils_unittest.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sharing/sharing_service_factory.h"
 #include "chrome/browser/sharing/sharing_sync_preference.h"
 #include "chrome/browser/sharing/vapid_key_manager.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -113,6 +115,14 @@ TEST_F(SharedClipboardUtilsTest, ClipboardProtocol_ShowMenu) {
 TEST_F(SharedClipboardUtilsTest, NoSharingService_DoNotShowMenu) {
   scoped_feature_list_.InitAndEnableFeature(kSharedClipboardUI);
   create_service_ = false;
+  EXPECT_FALSE(
+      ShouldOfferSharedClipboard(&profile_, base::ASCIIToUTF16(kText)));
+}
+
+TEST_F(SharedClipboardUtilsTest, EnterprisePolicy_Disabled) {
+  scoped_feature_list_.InitAndEnableFeature(kSharedClipboardUI);
+  // Set the enterprise policy to false:
+  profile_.GetPrefs()->SetBoolean(prefs::kSharedClipboardEnabled, false);
   EXPECT_FALSE(
       ShouldOfferSharedClipboard(&profile_, base::ASCIIToUTF16(kText)));
 }
