721a217dce904dceacc7c87622d57eeee0feddbe
John Chen <johnchen@chromium.org>
2019-09-25 18:32:29 +0000

    [ChromeDriver] Stop using --ignore-certificate-errors
    ChromeDriver was passing --ignore-certificate-errors switch to Chrome,
    regardless of whether user wants to ignore certificate errors or not.
    Fixing by stop passing this flag to Chrome, and relying on using
    Security.setIgnoreCertificateErrors command when user wants to ignore
    certificate errors.
    
    Bug: chromedriver:3148
    Change-Id: I5e8493f4cca48ebaeb211c95cd7fd645891c8659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824005
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#699890}

--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -70,10 +70,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// TODO(eseckler): Remove --ignore-certificate-errors for newer Chrome versions
-// that support the Security DevTools domain on the browser target.
 const char* const kCommonSwitches[] = {
-    "disable-popup-blocking", "enable-automation", "ignore-certificate-errors",
+    "disable-popup-blocking",
+    "enable-automation",
 };
 
 const char* const kDesktopSwitches[] = {
