584975899612b242ca4f4166a284e89155930e2c
Ben Kelly <wanderview@chromium.org>
2019-09-25 23:29:55 +0000

    CacheStorage: Disable parallel reads on android.
    The parallel read experiment shows that cache match miss rate regresses
    on android for some reason.  All other platforms show big progressions.
    Therefore we are going to keep parallel reads disabled on just android
    while enabling it by default for all other platforms.
    
    Bug: 985379
    Change-Id: I05c24f94f4ce2fe5246f2811ed605adf094412f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825398
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700008}

--- content/browser/cache_storage/cache_storage_scheduler.cc
+++ content/browser/cache_storage/cache_storage_scheduler.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/sequenced_task_runner.h"
+#include "build/build_config.h"
 #include "content/browser/cache_storage/cache_storage_histogram_utils.h"
 #include "content/browser/cache_storage/cache_storage_operation.h"
 #include "content/public/common/content_features.h"
@@ -25,8 +26,17 @@ namespace {
 // Maximum parallel shared operations.  This constant was selected via
 // experimentation.  We tried 4, 16, and 64 for the limit.  16 was clearly
 // better than 4, but 64 was did not provide significant further benefit.
+// TODO(crbug/1007994): Enable parallel shared operations on android after
+//                      performance regressions are addressed.
+#if defined(OS_ANDROID)
+constexpr int kDefaultMaxSharedOps = 1;
+#else
+constexpr int kDefaultMaxSharedOps = 16;
+#endif
+
 const base::FeatureParam<int> kCacheStorageMaxSharedOps{
-    &features::kCacheStorageParallelOps, "max_shared_ops", 16};
+    &features::kCacheStorageParallelOps, "max_shared_ops",
+    kDefaultMaxSharedOps};
 
 }  // namespace
 
