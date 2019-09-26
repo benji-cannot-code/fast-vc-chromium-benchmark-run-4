1d910db94f0e9503cc4cb8e0b55e549fea6cd975
Klaus Weidner <klausw@chromium.org>
2019-09-26 20:27:46 +0000

    Refactor WebXR reference space and pose internals
    Avoid using generic names such as "base pose" or "transform", and try to
    always specifically say which two spaces are connected by transforms.
    
    Use the B_from_A naming convention where possible, so that transforms
    can be chained naturally.
    
    Bug: 1008102
    Change-Id: I74988f89340375197017f9f518f69d4fd03e7d76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825585
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700403}

--- third_party/blink/renderer/modules/xr/xr_input_source.cc
+++ third_party/blink/renderer/modules/xr/xr_input_source.cc
@@ -82,7 +82,7 @@ XRInputSource* XRInputSource::CreateOrUpdateFrom(
     updated_source->state_.handedness = desc->handedness;
     updated_source->state_.emulated_position = desc->emulated_position;
 
-    updated_source->pointer_transform_matrix_ =
+    updated_source->input_from_pointer_ =
         TryGetTransformationMatrix(desc->pointer_offset);
 
     updated_source->state_.profiles.clear();
@@ -91,7 +91,7 @@ XRInputSource* XRInputSource::CreateOrUpdateFrom(
     }
   }
 
-  updated_source->base_pose_matrix_ = TryGetTransformationMatrix(state->grip);
+  updated_source->mojo_from_input_ = TryGetTransformationMatrix(state->grip);
 
   return updated_source;
 }
@@ -115,10 +115,10 @@ XRInputSource::XRInputSource(const XRInputSource& other)
           MakeGarbageCollected<XRTargetRaySpace>(other.session_, this)),
       grip_space_(MakeGarbageCollected<XRGripSpace>(other.session_, this)),
       gamepad_(other.gamepad_),
-      base_pose_matrix_(
-          TryGetTransformationMatrix(other.base_pose_matrix_.get())),
-      pointer_transform_matrix_(
-          TryGetTransformationMatrix(other.pointer_transform_matrix_.get())) {}
+      mojo_from_input_(
+          TryGetTransformationMatrix(other.mojo_from_input_.get())),
+      input_from_pointer_(
+          TryGetTransformationMatrix(other.input_from_pointer_.get())) {}
 
 const String XRInputSource::handedness() const {
   switch (state_.handedness) {
@@ -187,10 +187,9 @@ bool XRInputSource::InvalidatesSameObject(
   return false;
 }
 
-void XRInputSource::SetPointerTransformMatrix(
-    const TransformationMatrix* pointer_transform_matrix) {
-  pointer_transform_matrix_ =
-      TryGetTransformationMatrix(pointer_transform_matrix);
+void XRInputSource::SetInputFromPointer(
+    const TransformationMatrix* input_from_pointer) {
+  input_from_pointer_ = TryGetTransformationMatrix(input_from_pointer);
 }
 
 void XRInputSource::SetGamepadConnected(bool state) {
