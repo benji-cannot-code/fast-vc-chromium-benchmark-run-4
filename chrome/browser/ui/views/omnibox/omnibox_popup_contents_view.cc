c00980005ec5b57a67df0ab78a44b6a5afb20337
Peter Kasting <pkasting@chromium.org>
2019-09-26 22:37:39 +0000

    Convert OmniboxPopupContentsView to use a LayoutManager.
    No functional change intended, ideally just more maintainable code.
    
    Bug: 1005568
    Change-Id: I94bec353c48114e79546e38474d4f55edb04442f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1812317
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700465}

--- chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/image/image.h"
 #include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/layout/box_layout.h"
 #include "ui/views/widget/widget.h"
 
 class OmniboxPopupContentsView::AutocompletePopupWidget
@@ -149,6 +150,9 @@ OmniboxPopupContentsView::OmniboxPopupContentsView(
   // The contents is owned by the LocationBarView.
   set_owned_by_client();
 
+  SetLayoutManager(std::make_unique<views::BoxLayout>(
+      views::BoxLayout::Orientation::kVertical));
+
   for (size_t i = 0; i < AutocompleteResult::GetMaxMatches(); ++i) {
     OmniboxResultView* result_view =
         new OmniboxResultView(this, i, theme_provider);
@@ -274,7 +278,7 @@ void OmniboxPopupContentsView::UpdatePopupAppearance() {
 
   if (popup_) {
     popup_->SetTargetBounds(new_target_bounds);
-    Layout();
+    InvalidateLayout();
     return;
   }
 
@@ -308,7 +312,7 @@ void OmniboxPopupContentsView::UpdatePopupAppearance() {
     result_view_at(0)->NotifyAccessibilityEvent(ax::mojom::Event::kSelection,
                                                 true);
   }
-  Layout();
+  InvalidateLayout();
 }
 
 void OmniboxPopupContentsView::OnMatchIconUpdated(size_t match_index) {
@@ -321,15 +325,6 @@ void OmniboxPopupContentsView::OnDragCanceled() {
 
 ////////////////////////////////////////////////////////////////////////////////
 // OmniboxPopupContentsView, views::View overrides:
-
-void OmniboxPopupContentsView::Layout() {
-  // Size our children to the available content area.
-  LayoutChildren();
-
-  // We need to manually schedule a paint here since we are a layered window and
-  // won't implicitly require painting until we ask for one.
-  SchedulePaint();
-}
 
 bool OmniboxPopupContentsView::OnMouseDragged(const ui::MouseEvent& event) {
   size_t index = GetIndexForPoint(event.location());
@@ -400,18 +395,6 @@ gfx::Rect OmniboxPopupContentsView::GetTargetBounds() {
   // Finally, expand the widget to accomodate the custom-drawn shadows.
   content_rect.Inset(-RoundedOmniboxResultsFrame::GetShadowInsets());
   return content_rect;
-}
-
-void OmniboxPopupContentsView::LayoutChildren() {
-  gfx::Rect contents_rect = GetContentsBounds();
-  int top = contents_rect.y();
-  for (View* v : children()) {
-    if (v->GetVisible()) {
-      v->SetBounds(contents_rect.x(), top, contents_rect.width(),
-                   v->GetPreferredSize().height());
-      top = v->bounds().bottom();
-    }
-  }
 }
 
 bool OmniboxPopupContentsView::HasMatchAt(size_t index) const {
