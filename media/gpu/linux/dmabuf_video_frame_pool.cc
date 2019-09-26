e869ad3db9cd7b3927fc76f90d5660ebb72476c9
Chih-Yu Huang <akahuang@google.com>
2019-09-26 06:55:25 +0000

    media/gpu/linux: Move GetDmabufId() method to DmabufVideoFramePool.
    The GetDmabufId() method is getting an identifier of a Dma-buf
    VideoFrame. The method is used for all DmabufVideoFramePool
    implementations, not only PlatformVideoFramePool. This CL moves the
    method to DmabufVideoFramePool.
    
    BUG=b:136716638
    TEST=media_unittests --gtest_filter=PlatformVideoFramePoolTest.*
    
    Change-Id: I6950c1cf5a5127778a3dba2bbf2475fd4779a679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824501
    Auto-Submit: Chih-Yu Huang <akahuang@chromium.org>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700120}

--- media/gpu/linux/dmabuf_video_frame_pool.cc
+++ media/gpu/linux/dmabuf_video_frame_pool.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+// static
+DmabufVideoFramePool::DmabufId DmabufVideoFramePool::GetDmabufId(
+    const VideoFrame& frame) {
+  return &(frame.DmabufFds());
+}
+
 DmabufVideoFramePool::DmabufVideoFramePool() = default;
 
 DmabufVideoFramePool::~DmabufVideoFramePool() = default;
