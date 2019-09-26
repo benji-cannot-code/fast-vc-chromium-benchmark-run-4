1aae7b1e10c3d0ba89e543dba6c5e5b9be8c220d
Ce Chen <cch@chromium.org>
2019-09-26 13:36:34 +0000

    Register on device head suggest component updater at iOS.
    This feature is gated by flag OmniboxOnDeviceHeadProvider.
    
    Tested on iPhone X.
    
    Bug: 925072
    Change-Id: I5ba4551971922b0948ce9b5e7e626e346c59290b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814644
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Ce Chen <cch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700226}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/component_updater/component_updater_service.h"
 #include "components/component_updater/crl_set_remover.h"
+#include "components/component_updater/installer_policies/on_device_head_suggest_component_installer.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/feature_engagement/public/event_constants.h"
 #include "components/feature_engagement/public/tracker.h"
@@ -242,6 +243,9 @@ void RegisterComponentsForUpdate() {
   // Clean up any legacy CRLSet on the local disk - CRLSet used to be shipped
   // as a component on iOS but is not anymore.
   component_updater::DeleteLegacyCRLSet(path);
+
+  RegisterOnDeviceHeadSuggestComponent(
+      cus, GetApplicationContext()->GetApplicationLocale());
 }
 
 // Used to update the current BVC mode if a new tab is added while the tab
