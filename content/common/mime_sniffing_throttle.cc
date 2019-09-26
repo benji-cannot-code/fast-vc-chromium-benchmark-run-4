6f4345cefc5983e90bff1147f6bb70c1b3aa00ff
Minggang Wang <minggang.wang@intel.com>
2019-09-26 10:51:07 +0000

    Replace the old Mojo types with the new Mojo types for MimeSniffingURLLoader
    This CL converts old Mojo types used by MimeSniffingURLLoader and its unit
    test to new Mojo types.
    
    Bug: 955171
    Change-Id: I142abf816370adba3ad44fa26cbaf0e86a1e61c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1817956
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Minggang Wang <minggang.wang@intel.com>
    Cr-Commit-Position: refs/heads/master@{#700184}

--- content/common/mime_sniffing_throttle.cc
+++ content/common/mime_sniffing_throttle.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/mime_sniffing_throttle.h"
 
 #include "content/common/mime_sniffing_url_loader.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
 #include "net/base/mime_sniffer.h"
 
 namespace content {
@@ -38,19 +40,19 @@ void MimeSniffingThrottle::WillProcessResponse(
     // Pause the response until the mime type becomes ready.
     *defer = true;
 
-    network::mojom::URLLoaderPtr new_loader;
-    network::mojom::URLLoaderClientRequest new_loader_request;
+    mojo::PendingRemote<network::mojom::URLLoader> new_remote;
+    mojo::PendingReceiver<network::mojom::URLLoaderClient> new_receiver;
     network::mojom::URLLoaderPtr source_loader;
     network::mojom::URLLoaderClientRequest source_client_request;
     MimeSniffingURLLoader* mime_sniffing_loader;
-    std::tie(new_loader, new_loader_request, mime_sniffing_loader) =
+    std::tie(new_remote, new_receiver, mime_sniffing_loader) =
         MimeSniffingURLLoader::CreateLoader(weak_factory_.GetWeakPtr(),
                                             response_url, *response_head,
                                             task_runner_);
-    delegate_->InterceptResponse(std::move(new_loader),
-                                 std::move(new_loader_request), &source_loader,
-                                 &source_client_request);
-    mime_sniffing_loader->Start(std::move(source_loader),
+    delegate_->InterceptResponse(
+        network::mojom::URLLoaderPtr(std::move(new_remote)),
+        std::move(new_receiver), &source_loader, &source_client_request);
+    mime_sniffing_loader->Start(source_loader.PassInterface(),
                                 std::move(source_client_request));
   }
 }
