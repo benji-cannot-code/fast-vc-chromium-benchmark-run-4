8c3d45d2c5ff9826bbe2ba747c8aef0b4fe4fd65
Samuel Huang <huangs@chromium.org>
2019-09-26 21:00:04 +0000

    [DevUI DFM] Move chrome://usb-internals to the DFM.
    This CL also serves as a self-contained "vertical" example for how to
    move a WebUI page to the DevUI DFM.
    
    Bug: 927131
    Change-Id: I88c5f624d2094ecfd1cf1919d9cb4abe8d1daea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1823605
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700415}

--- chrome/browser/ui/webui/usb_internals/usb_internals_ui.cc
+++ chrome/browser/ui/webui/usb_internals/usb_internals_ui.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/usb_internals/usb_internals_page_handler.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
+#include "chrome/grit/usb_internals_resources.h"
 #include "content/public/browser/web_ui_data_source.h"
 
 UsbInternalsUI::UsbInternalsUI(content::WebUI* web_ui)
