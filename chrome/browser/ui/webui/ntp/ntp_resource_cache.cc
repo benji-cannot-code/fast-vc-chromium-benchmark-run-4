855386e5c7a3c2e6a292c159c2aa7ec6c274f99b
Huanzhong Huang <huanzhong@google.com>
2019-09-26 16:42:05 +0000

    Cookie controls notice on incognito ntp
    Show cookie controls notice on incognito ntp when
    the improvedCookieControls flag is enabled.
    
    screenshot: https://screenshot.googleplex.com/WTCUPwSowr9
    
    Bug: 967668
    Change-Id: I83b0d6c607ccd4a339ea66b829405224bfbc1732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814817
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Huanzhong Huang <huanzhong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700290}

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/google/core/common/google_util.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -275,6 +276,14 @@ void NTPResourceCache::CreateNewTabIncognitoHTML() {
       l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_NOT_SAVED);
   replacements["learnMoreLink"] = kLearnMoreIncognitoUrl;
   replacements["title"] = l10n_util::GetStringUTF8(IDS_NEW_TAB_TITLE);
+  replacements["hiddenIfCookieControlsDisabled"] =
+      base::FeatureList::IsEnabled(content_settings::kImprovedCookieControls)
+          ? ""
+          : "hidden";
+  replacements["cookieControlsTitle"] =
+      l10n_util::GetStringUTF8(IDS_SETTINGS_SITE_SETTINGS_THIRD_PARTY_COOKIE);
+  replacements["cookieControlsDescription"] =
+      l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_COOKIE_CONTROLS_DESCRIPTION);
 
   const ui::ThemeProvider& tp =
       ThemeService::GetThemeProviderForProfile(profile_);
