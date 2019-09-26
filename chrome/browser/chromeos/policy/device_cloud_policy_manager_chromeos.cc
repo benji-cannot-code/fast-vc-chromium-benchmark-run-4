237ac865180ec81a82e0ce451a266e88eefdccf9
Paul Moy <pmoy@chromium.org>
2019-09-26 17:46:49 +0000

    DeviceStatusCollector: start migration to cros_healthd
    Switch the collection of cached_vpd and storage information
    from runtime_probe to cros_healthd.
    
    to a sona device with some code to log what StatusUploader is
    uploading. I then checked that data was consistent with the data
    reported by the command-line invocation of cros_healthd.
    
    Bug: b:128683357
    Test: manual and unit tests. Manually, I built and deployed Chrome
    Change-Id: Id680449eed59cba7ed92bc4b4934e466d457ff79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1809612
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700324}

--- chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
@@ -434,6 +434,7 @@ void DeviceCloudPolicyManagerChromeOS::CreateStatusUploader() {
           DeviceStatusCollector::TpmStatusFetcher(),
           DeviceStatusCollector::EMMCLifetimeFetcher(),
           DeviceStatusCollector::StatefulPartitionInfoFetcher(),
+          DeviceStatusCollector::CrosHealthdDataFetcher(),
           true /* is_enterprise_device */),
       task_runner_, kDeviceStatusUploadFrequency));
 }
--- chromeos/dbus/cros_healthd/cros_healthd_client.h
+++ chromeos/dbus/cros_healthd/cros_healthd_client.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_file.h"
 #include "base/macros.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd.mojom.h"
+#include "chromeos/services/cros_healthd/public/mojom/cros_healthd_probe.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
@@ -31,10 +32,6 @@ class COMPONENT_EXPORT(CROS_HEALTHD) CrosHealthdClient {
 
   // Creates and initializes a fake global instance if not already created.
   static void InitializeFake();
-
-  static void InitializeFakeWithMockService(
-      mojo::PendingRemote<cros_healthd::mojom::CrosHealthdService>
-          mock_service);
 
   // Destroys the global instance.
   static void Shutdown();
