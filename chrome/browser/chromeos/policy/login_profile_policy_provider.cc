4574c08456ea4afa536b30e2cf63db6cabdc2c74
Amr Aboelkher <amraboelkher@google.com>
2019-09-26 19:48:45 +0000

    Add a device login policy for select to speak accessibility feature
    A device policy which controls the select to speak accessibility feature on the
    login screen.The policy can be applied as a recommended or mandatory policy.
    
    Test:
    1. Set the login_screen_select_to_speak_enabled policy to True, open System
    Tray => "Accessibility", check that "Select-to-Speak" is displayed with a green
    check in the login screen.
    2. Set the policy to False, open System Tray, check that "Select-to-Speak" is
    not displayed in the login screen.
    
    Bug: 552439
    Change-Id: I86bfea61ccf3cac74934f17181c260d5649e04c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816558
    Commit-Queue: Amr Aboelkher <amraboelkher@google.com>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700387}

--- chrome/browser/chromeos/policy/login_profile_policy_provider.cc
+++ chrome/browser/chromeos/policy/login_profile_policy_provider.cc
@@ -46,6 +46,7 @@ const DevicePolicyToUserPolicyMapEntry kDevicePoliciesWithPolicyOptionsMap[] = {
     {key::kDeviceLoginScreenVirtualKeyboardEnabled,
      key::kVirtualKeyboardEnabled},
     {key::kDeviceLoginScreenDictationEnabled, key::kDictationEnabled},
+    {key::kDeviceLoginScreenSelectToSpeakEnabled, key::kSelectToSpeakEnabled},
 };
 
 const DevicePolicyToUserPolicyMapEntry kRecommendedDevicePoliciesMap[] = {
