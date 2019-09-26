530548967f39f3d1912116509786b77c6e2fb9a1
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-09-26 09:32:55 +0000

    Printing API: Add integration tests for chrome.printingMetrics API
    Bug: 992889
    Change-Id: I5c45d9089a02844e0043e314e69cb57058b33b7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1798666
    Commit-Queue: Nikita Podguzov <nikitapodguzov@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700158}

---
+++ chrome/test/data/extensions/api_test/printing_metrics/get_print_jobs.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function testGetPrintJobs(expectedTitle) {
+  chrome.test.runTests([() => {
+    chrome.printingMetrics.getPrintJobs(printJobs => {
+      chrome.test.assertEq(1, printJobs.length);
+      chrome.test.assertEq(expectedTitle, printJobs[0].title);
+      chrome.test.succeed();
+    });
+  }]);
+}
+
+chrome.test.getConfig(config => {
+  testGetPrintJobs(config.customArg);
+});
