20b95bc8caeb084c95f736846356bef0e9ed94f4
Miyoung Shin <myid.shin@igalia.com>
2019-09-26 15:14:10 +0000

    Convert GamepadMonitor client to use BrowserInterfaceBroker
    This change converts GamepadMonitor mojom interface client in blink
    to use BrowserInterfaceBroker.
    
    Bug: 936482
    Change-Id: I34b6f39335544966a2ccd3cc5e134dd1149e3fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819083
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700254}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/service_worker_context.h"
 #include "content/public/browser/shared_worker_instance.h"
+#include "device/gamepad/gamepad_monitor.h"
+#include "device/gamepad/public/mojom/gamepad.mojom.h"
 #include "media/capture/mojom/image_capture.mojom.h"
 #include "services/device/public/mojom/constants.mojom.h"
 #include "services/device/public/mojom/sensor_provider.mojom.h"
@@ -106,6 +108,9 @@ void PopulateFrameBinders(RenderFrameHostImpl* host,
 
   map->Add<blink::mojom::FileChooser>(base::BindRepeating(
       &RenderFrameHostImpl::GetFileChooser, base::Unretained(host)));
+
+  map->Add<device::mojom::GamepadMonitor>(
+      base::BindRepeating(&device::GamepadMonitor::Create));
 
 #if defined(OS_ANDROID)
   if (base::FeatureList::IsEnabled(features::kWebNfc)) {
