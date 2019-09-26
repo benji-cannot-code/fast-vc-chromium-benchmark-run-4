57dbfc387169461bc7510bea1a21e73de92994cf
Roman Sorokin <rsorokin@chromium.org>
2019-09-26 09:04:08 +0000

    cros settings helper: Commit writes on store
    I believe it causes flakes of kiosk browsertest which use this helper
    
    Bug: 1008290
    Change-Id: I883a18445b4bbc8d4db6a2901f61c7eb5bf5f8e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1826478
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700146}

--- chrome/browser/chromeos/settings/scoped_cros_settings_test_helper.cc
+++ chrome/browser/chromeos/settings/scoped_cros_settings_test_helper.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ownership/mock_owner_key_util.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
 #include "components/policy/proto/device_management_backend.pb.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/test/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -122,6 +123,8 @@ void ScopedCrosSettingsTestHelper::StoreCachedDeviceSetting(
     OwnerSettingsServiceChromeOS::UpdateDeviceSettings(path, *value, settings);
     CHECK(settings.SerializeToString(data.mutable_policy_value()));
     CHECK(device_settings_cache::Store(data, g_browser_process->local_state()));
+    g_browser_process->local_state()->CommitPendingWrite(base::DoNothing(),
+                                                         base::DoNothing());
   }
 }
 
