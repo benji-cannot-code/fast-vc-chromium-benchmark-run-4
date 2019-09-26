906b9d3d8121a6cecb2e7a71e25fe2ec5373ca38
Henrique Nakashima <hnakashima@chromium.org>
2019-09-26 19:52:55 +0000

    Rename DocumentUtils to AndroidTaskUtils, move to chrome/browser/util
    Bug: 995916
    Change-Id: Ic874d6287e224da3277cff98457604617f5a64dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814919
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700390}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappActivity.java
@@ -41,7 +41,6 @@ import org.chromium.chrome.browser.browserservices.BrowserServicesIntentDataProv
 import org.chromium.chrome.browser.compositor.layouts.LayoutManager;
 import org.chromium.chrome.browser.customtabs.CustomTabAppMenuPropertiesDelegate;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
-import org.chromium.chrome.browser.document.DocumentUtils;
 import org.chromium.chrome.browser.metrics.WebApkUma;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
@@ -53,6 +52,7 @@ import org.chromium.chrome.browser.tab.TabState;
 import org.chromium.chrome.browser.toolbar.top.ToolbarControlContainer;
 import org.chromium.chrome.browser.ui.widget.TintedDrawable;
 import org.chromium.chrome.browser.usage_stats.UsageStatsService;
+import org.chromium.chrome.browser.util.AndroidTaskUtils;
 import org.chromium.chrome.browser.util.ColorUtils;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.NavigationController;
@@ -454,7 +454,7 @@ public class WebappActivity extends SingleTabActivity {
         if (!isFinishing()) {
             if (getIntent() != null) {
                 // Avoid situations where Android starts two Activities with the same data.
-                DocumentUtils.finishOtherTasksWithData(getIntent().getData(), getTaskId());
+                AndroidTaskUtils.finishOtherTasksWithData(getIntent().getData(), getTaskId());
             }
             updateTaskDescription();
         }
