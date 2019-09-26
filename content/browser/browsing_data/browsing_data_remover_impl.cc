9f309b6ccb15e33d690083a7557c2eadcab114c0
Rohit Agarwal <roagarwal@chromium.org>
2019-09-26 07:39:25 +0000

    Delete shader cache after writing to remove mask.
    The deletion of data from StoragePartition is  triggered before
    REMOVE_DATA_MASK_SHADER_CACHE gets written to the deletion mask which
    led to not deletion of shader cache.
    
    The fix was to write to the remove mask before deletion is called.
    
    Bug: 960783
    Change-Id: I023462d6d419f369286380751746814dbef404a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819438
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700127}

--- content/browser/browsing_data/browsing_data_remover_impl.cc
+++ content/browser/browsing_data/browsing_data_remover_impl.cc
@@ -361,7 +361,12 @@ void BrowsingDataRemoverImpl::RemoveImpl(
     storage_partition_remove_mask |=
         StoragePartition::REMOVE_DATA_MASK_BACKGROUND_FETCH;
   }
-
+  if (remove_mask & DATA_TYPE_CACHE) {
+    // Tell the shader disk cache to clear.
+    base::RecordAction(UserMetricsAction("ClearBrowsingData_ShaderCache"));
+    storage_partition_remove_mask |=
+        StoragePartition::REMOVE_DATA_MASK_SHADER_CACHE;
+  }
   // Content Decryption Modules used by Encrypted Media store licenses in a
   // private filesystem. These are different than content licenses used by
   // Flash (which are deleted father down in this method).
@@ -448,11 +453,6 @@ void BrowsingDataRemoverImpl::RemoveImpl(
 
     // Clears the PrefetchedSignedExchangeCache of all RenderFrameHostImpls.
     RenderFrameHostImpl::ClearAllPrefetchedSignedExchangeCache();
-
-    // Tell the shader disk cache to clear.
-    base::RecordAction(UserMetricsAction("ClearBrowsingData_ShaderCache"));
-    storage_partition_remove_mask |=
-        StoragePartition::REMOVE_DATA_MASK_SHADER_CACHE;
   }
 
 #if BUILDFLAG(ENABLE_REPORTING)
