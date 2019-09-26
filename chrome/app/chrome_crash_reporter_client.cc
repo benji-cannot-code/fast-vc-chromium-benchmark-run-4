496954561f28b821695e8ab29599fe63161a6358
Brian Sheedy <bsheedy@chromium.org>
2019-09-26 20:37:43 +0000

    Fix GPU stack symbolization on Android
    Adds the necessary Chromium-side changes to fix stack symbolization for
    non-browser crashes in GPU Telemetry tests on Android. Specifically:
    
    1. Makes the GPU tests use GetStackTrace() on Android instead of
       LogSymbolizedUnsymbolizedMinidumps(), as this is currently the only
       way to get stack traces through Telemetry on Android.
    2. Adds a --breakpad-dump-location argument to ChromeCrashReporterClient
       that is functionally equivalent to the existing
       BREAKPAD_DUMP_LOCATION environment variable, but necessary for
       Android since we can't set environment variables before launching
       Chrome on Android.
    3. Moves the dump location override to ChromeCrashReporterClient::Create
       instead of whe retrieving the crash dump location in order to avoid
       issues with Android calling blocking functions in a scope that does
       not allow blocking functions.
    
    Bug: 949321
    Change-Id: I9be2446a73a2359157cd94c889bf34951b78c86b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825719
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700407}

--- chrome/app/chrome_crash_reporter_client.cc
+++ chrome/app/chrome_crash_reporter_client.cc
@@ -46,6 +46,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void ChromeCrashReporterClient::Create() {
   static base::NoDestructor<ChromeCrashReporterClient> crash_client;
   crash_reporter::SetCrashReporterClient(crash_client.get());
+
+  // By setting the BREAKPAD_DUMP_LOCATION environment variable, an alternate
+  // location to write crash dumps can be set.
+  std::unique_ptr<base::Environment> env(base::Environment::Create());
+  std::string alternate_crash_dump_location;
+  base::FilePath crash_dumps_dir_path;
+  if (env->GetVar("BREAKPAD_DUMP_LOCATION", &alternate_crash_dump_location)) {
+    crash_dumps_dir_path =
+        base::FilePath::FromUTF8Unsafe(alternate_crash_dump_location);
+  } else if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+                 "breakpad-dump-location")) {
+    // This is needed for Android tests, where we want dumps to go to a location
+    // where they don't get uploaded/deleted, but we can't use environment
+    // variables.
+    crash_dumps_dir_path =
+        base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
+            "breakpad-dump-location");
+  }
+  if (!crash_dumps_dir_path.empty()) {
+    base::PathService::Override(chrome::DIR_CRASH_DUMPS, crash_dumps_dir_path);
+  }
 }
 
 #if defined(OS_CHROMEOS)
@@ -116,15 +137,6 @@ base::FilePath ChromeCrashReporterClient::GetReporterLogFilename() {
 
 bool ChromeCrashReporterClient::GetCrashDumpLocation(
     base::FilePath* crash_dir) {
-  // By setting the BREAKPAD_DUMP_LOCATION environment variable, an alternate
-  // location to write breakpad crash dumps can be set.
-  std::unique_ptr<base::Environment> env(base::Environment::Create());
-  std::string alternate_crash_dump_location;
-  if (env->GetVar("BREAKPAD_DUMP_LOCATION", &alternate_crash_dump_location)) {
-    base::FilePath crash_dumps_dir_path =
-        base::FilePath::FromUTF8Unsafe(alternate_crash_dump_location);
-    base::PathService::Override(chrome::DIR_CRASH_DUMPS, crash_dumps_dir_path);
-  }
   return base::PathService::Get(chrome::DIR_CRASH_DUMPS, crash_dir);
 }
 
