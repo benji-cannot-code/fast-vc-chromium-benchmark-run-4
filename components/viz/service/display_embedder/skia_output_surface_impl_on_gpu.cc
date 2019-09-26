bf9b2b1afbe06d5e890d0f60482240c5581f0f86
Vasiliy Telezhnikov <vasilyt@chromium.org>
2019-09-26 20:18:06 +0000

    SkiaRenderer: Add missed MakeCurrent calls/checks
    This CLs makes sure that GL Context made current inside
    SwapBuffers and SetEnableDCOverlays to prevent potential crashes
    inside DirectCompositionChildSurfaceWin.
    
    Bug: 1007919
    Change-Id: Ib28bd2a72405ea9f11fcbfecea50cecfb4d40760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827484
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700397}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -685,7 +685,7 @@ void SkiaOutputSurfaceImplOnGpu::Reshape(
   }
 }
 
-void SkiaOutputSurfaceImplOnGpu::FinishPaintCurrentFrame(
+bool SkiaOutputSurfaceImplOnGpu::FinishPaintCurrentFrame(
     std::unique_ptr<SkDeferredDisplayList> ddl,
     std::unique_ptr<SkDeferredDisplayList> overdraw_ddl,
     std::vector<ImageContextImpl*> image_contexts,
@@ -699,7 +699,7 @@ void SkiaOutputSurfaceImplOnGpu::FinishPaintCurrentFrame(
   DCHECK(!scoped_output_device_paint_);
 
   if (!MakeCurrent(true /* need_fbo0 */))
-    return;
+    return false;
 
   if (draw_rectangle)
     output_device_->SetDrawRectangle(*draw_rectangle);
@@ -772,7 +772,7 @@ void SkiaOutputSurfaceImplOnGpu::FinishPaintCurrentFrame(
           scoped_promise_image_access.end_semaphores().empty())) {
       // TODO(penghuang): handle vulkan device lost.
       DLOG(ERROR) << "output_sk_surface()->flush() failed.";
-      return;
+      return false;
     }
     if (output_device_->need_swap_semaphore()) {
       auto& semaphore = scoped_promise_image_access.end_semaphores().back();
@@ -781,6 +781,7 @@ void SkiaOutputSurfaceImplOnGpu::FinishPaintCurrentFrame(
     }
   }
   ReleaseFenceSyncAndPushTextureUpdates(sync_fence_release);
+  return true;
 }
 
 void SkiaOutputSurfaceImplOnGpu::ScheduleOutputSurfaceAsOverlay(
@@ -791,13 +792,15 @@ void SkiaOutputSurfaceImplOnGpu::ScheduleOutputSurfaceAsOverlay(
 
 void SkiaOutputSurfaceImplOnGpu::SwapBuffers(
     OutputSurfaceFrame frame,
-    base::OnceClosure deferred_framebuffer_draw_closure,
+    base::OnceCallback<bool()> deferred_framebuffer_draw_closure,
     uint64_t sync_fence_release) {
   TRACE_EVENT0("viz", "SkiaOutputSurfaceImplOnGpu::SwapBuffers");
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
   if (deferred_framebuffer_draw_closure) {
-    std::move(deferred_framebuffer_draw_closure).Run();
+    // Returns false if context not set to current, i.e lost
+    if (!std::move(deferred_framebuffer_draw_closure).Run())
+      return;
     DCHECK(context_state_->IsCurrent(nullptr /* surface */));
   } else {
     if (!MakeCurrent(!dependency_->IsOffscreen() /* need_fbo0 */))
@@ -931,7 +934,7 @@ void SkiaOutputSurfaceImplOnGpu::CopyOutput(
     const copy_output::RenderPassGeometry& geometry,
     const gfx::ColorSpace& color_space,
     std::unique_ptr<CopyOutputRequest> request,
-    base::OnceClosure deferred_framebuffer_draw_closure) {
+    base::OnceCallback<bool()> deferred_framebuffer_draw_closure) {
   TRACE_EVENT0("viz", "SkiaOutputSurfaceImplOnGpu::CopyOutput");
   // TODO(crbug.com/898595): Do this on the GPU instead of CPU with Vulkan.
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
@@ -942,7 +945,9 @@ void SkiaOutputSurfaceImplOnGpu::CopyOutput(
                      std::move(destroy_after_swap_)));
 
   if (deferred_framebuffer_draw_closure) {
-    std::move(deferred_framebuffer_draw_closure).Run();
+    // returns false if context not set to current, i.e lost
+    if (!std::move(deferred_framebuffer_draw_closure).Run())
+      return;
     DCHECK(context_state_->IsCurrent(nullptr /* surface */));
   } else {
     if (!MakeCurrent(true /* need_fbo0 */))
@@ -1198,6 +1203,8 @@ void SkiaOutputSurfaceImplOnGpu::ReleaseImageContexts(
 }
 
 void SkiaOutputSurfaceImplOnGpu::SetEnableDCLayers(bool enable) {
+  if (!MakeCurrent(false /* need_fbo0 */))
+    return;
   output_device_->SetEnableDCLayers(enable);
 }
 
