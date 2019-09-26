cafe4f3ae60bf400b709be24012a4bab4cf0ada7
Sigurdur Asgeirsson <siggi@chromium.org>
2019-09-26 21:03:23 +0000

    Mojo: Defeat tail-call optimization in MaybeDumpWithoutCrashing.
    Bug: 1003391
    Change-Id: I27a60bffbf951c92ed1bcf8ffbafd7e473335f25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827379
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700418}

--- mojo/public/cpp/bindings/lib/connector.cc
+++ mojo/public/cpp/bindings/lib/connector.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/compiler_specific.h"
+#include "base/debug/alias.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -105,6 +106,11 @@ NOINLINE void MaybeDumpWithoutCrashing(int quota_used) {
   // stack has queued up an unreasonable number of messages, namely
   // |quota_used|.
   base::debug::DumpWithoutCrashing();
+
+  // Defeat tail-call optimization and ensure these two variables are available
+  // on the stack.
+  base::debug::Alias(&crash_threshold);
+  base::debug::Alias(&quota_used);
 }
 
 }  // namespace
