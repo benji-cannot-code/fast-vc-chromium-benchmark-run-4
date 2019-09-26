c33e1be8d890a3745db9e94b42cede2cd951dc2f
Jeroen Dhollander <jeroendh@google.com>
2019-09-26 20:51:41 +0000

    Introduce AssistantServiceManager::State::STARTING state.
    Until now the |AssistantServiceManager::State| enum did not distinguish
    between the state in which libassistant was started (but not ready yet)
    and in which it is ready.
    That distinction is needed to be able to add tests using libassistant,
    as they have to wait until it is ready.
    
    So now there are 4 states:
        - STOPPED: It has not started/it has been stopped (this is the same
        as the old STOPPED state).
        - STARTING: |AssistantServiceManager::Start| has been called and the
        libassistant |AssistantManager::Start| call has not finished yet (this
        is the same as the old STARTED state).
        - STARTED: the libassistant |AssistantManager::Start| call has
        finished, but libassistant has not signalled it is ready yet (this
        is the same as the old RUNNING state).
        - RUNNING: libassistant signalled it is ready (this state is new).
    
    As this CL also introduces the first unittests for
    |AssistantServiceManagerImpl|, it also contains a bunch of fake/dummy
    classes used for the unittests.
    
    This CL increases test coverage in //chromeos/services/assistant from 28%
    to 41%.
    
    Change-Id: If38a6b70e22e6392537b6658e5f39eebf6dd9099
    BUG: b/141264108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810019
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700410}

--- chromeos/services/assistant/assistant_manager_service_impl.h
+++ chromeos/services/assistant/assistant_manager_service_impl.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/assistant/assistant_manager_service.h"
 #include "chromeos/services/assistant/assistant_settings_manager_impl.h"
 #include "chromeos/services/assistant/chromium_api_delegate.h"
-#include "chromeos/services/assistant/platform_api_impl.h"
 #include "chromeos/services/assistant/public/mojom/assistant.mojom.h"
 #include "libassistant/shared/internal_api/assistant_manager_delegate.h"
 #include "libassistant/shared/public/conversation_state_listener.h"
@@ -54,7 +53,9 @@ namespace chromeos {
 namespace assistant {
 
 class AssistantMediaSession;
+class CrosPlatformApi;
 class ServiceContext;
+class AssistantManagerServiceDelegate;
 
 // Enumeration of Assistant query response type, also recorded in histograms.
 // These values are persisted to logs. Entries should not be renumbered and
@@ -84,7 +85,7 @@ enum class AssistantQueryResponseType {
 // Since LibAssistant is a standalone library, all callbacks come from it
 // running on threads not owned by Chrome. Thus we need to post the callbacks
 // onto the main thread.
-class AssistantManagerServiceImpl
+class COMPONENT_EXPORT(ASSISTANT_SERVICE) AssistantManagerServiceImpl
     : public AssistantManagerService,
       public ::chromeos::assistant::action::AssistantActionObserver,
       public AssistantEventObserver,
@@ -98,8 +99,8 @@ class AssistantManagerServiceImpl
   // |service| owns this class and must outlive this class.
   AssistantManagerServiceImpl(
       mojom::Client* client,
-      mojo::PendingRemote<device::mojom::BatteryMonitor> battery_monitor,
       ServiceContext* context,
+      std::unique_ptr<AssistantManagerServiceDelegate> delegate,
       std::unique_ptr<network::SharedURLLoaderFactoryInfo>
           url_loader_factory_info,
       bool is_signed_out_mode);
@@ -223,6 +224,10 @@ class AssistantManagerServiceImpl
   void UpdateInternalMediaPlayerStatus(
       media_session::mojom::MediaSessionAction action);
 
+  // The start runs in the background. This will wait until the background
+  // thread is finished.
+  void WaitUntilStartIsFinishedForTesting();
+
  private:
   void StartAssistantInternal(const base::Optional<std::string>& access_token);
   void PostInitAssistant(base::OnceClosure post_init_callback);
@@ -310,10 +315,13 @@ class AssistantManagerServiceImpl
   mojom::DeviceActions* device_actions();
   scoped_refptr<base::SequencedTaskRunner> main_task_runner();
 
+  bool HasStartFinished() const;
+  void SetStartFinished();
+
   mojom::Client* const client_;
   State state_ = State::STOPPED;
   std::unique_ptr<AssistantMediaSession> media_session_;
-  std::unique_ptr<PlatformApiImpl> platform_api_;
+  std::unique_ptr<CrosPlatformApi> platform_api_;
   std::unique_ptr<action::CrosActionModule> action_module_;
   ChromiumApiDelegate chromium_api_delegate_;
   // NOTE: |display_connection_| is used by |assistant_manager_| and must be
@@ -321,11 +329,14 @@ class AssistantManagerServiceImpl
   std::unique_ptr<CrosDisplayConnection> display_connection_;
   std::unique_ptr<assistant_client::AssistantManager> assistant_manager_;
   std::unique_ptr<AssistantSettingsManagerImpl> assistant_settings_manager_;
-  // |new_asssistant_manager_| is created on |background_thread_| then posted to
+  // |new_assistant_manager_| is created on |background_thread_| then posted to
   // main thread to finish initialization then move to |assistant_manager_|.
   std::unique_ptr<assistant_client::AssistantManager> new_assistant_manager_;
+  // Same ownership as |new_assistant_manager_|.
+  assistant_client::AssistantManagerInternal* new_assistant_manager_internal_ =
+      nullptr;
   base::Lock new_assistant_manager_lock_;
-  // same ownership as assistant_manager_.
+  // same ownership as |assistant_manager_|.
   assistant_client::AssistantManagerInternal* assistant_manager_internal_ =
       nullptr;
   mojo::RemoteSet<mojom::AssistantInteractionSubscriber>
@@ -334,6 +345,8 @@ class AssistantManagerServiceImpl
 
   // Owned by the parent |Service| which will destroy |this| before |context_|.
   ServiceContext* const context_;
+
+  std::unique_ptr<AssistantManagerServiceDelegate> delegate_;
 
   bool spoken_feedback_enabled_ = false;
 
@@ -368,8 +381,6 @@ class AssistantManagerServiceImpl
 
   base::UnguessableToken media_session_audio_focus_id_ =
       base::UnguessableToken::Null();
-
-  bool start_finished_ = false;
 
   mojo::Binding<mojom::AppListEventSubscriber> app_list_subscriber_binding_;
 
--- chromeos/services/assistant/assistant_settings_manager_impl.cc
+++ chromeos/services/assistant/assistant_settings_manager_impl.cc
@@ -25,6 +25,17 @@ using SpeakerIdEnrollmentState =
 namespace chromeos {
 namespace assistant {
 
+namespace {
+
+bool HasStarted(const AssistantManagerService* assistant_manager_service) {
+  return (assistant_manager_service->GetState() ==
+              AssistantManagerService::STARTED ||
+          assistant_manager_service->GetState() ==
+              AssistantManagerService::RUNNING);
+}
+
+}  // namespace
+
 AssistantSettingsManagerImpl::AssistantSettingsManagerImpl(
     ServiceContext* context,
     AssistantManagerServiceImpl* assistant_manager_service)
@@ -41,8 +52,7 @@ void AssistantSettingsManagerImpl::BindRequest(
 
 void AssistantSettingsManagerImpl::GetSettings(const std::string& selector,
                                                GetSettingsCallback callback) {
-  DCHECK(assistant_manager_service_->GetState() ==
-         AssistantManagerService::State::RUNNING);
+  DCHECK(HasStarted(assistant_manager_service_));
   DCHECK(main_task_runner()->RunsTasksInCurrentSequence());
 
   // TODO(xiaohuic): libassistant could be restarting for various reasons. In
@@ -80,8 +90,7 @@ void AssistantSettingsManagerImpl::GetSettings(const std::string& selector,
 void AssistantSettingsManagerImpl::UpdateSettings(
     const std::string& update,
     GetSettingsCallback callback) {
-  DCHECK(assistant_manager_service_->GetState() ==
-         AssistantManagerService::State::RUNNING);
+  DCHECK(HasStarted(assistant_manager_service_));
   DCHECK(main_task_runner()->RunsTasksInCurrentSequence());
 
   if (!assistant_manager_service_->assistant_manager_internal()) {
@@ -115,8 +124,7 @@ void AssistantSettingsManagerImpl::UpdateSettings(
 void AssistantSettingsManagerImpl::StartSpeakerIdEnrollment(
     bool skip_cloud_enrollment,
     mojom::SpeakerIdEnrollmentClientPtr client) {
-  DCHECK(assistant_manager_service_->GetState() ==
-         AssistantManagerService::State::RUNNING);
+  DCHECK(HasStarted(assistant_manager_service_));
   DCHECK(main_task_runner()->RunsTasksInCurrentSequence());
 
   if (!assistant_manager_service_->assistant_manager_internal())
@@ -143,8 +151,7 @@ void AssistantSettingsManagerImpl::StartSpeakerIdEnrollment(
 
 void AssistantSettingsManagerImpl::StopSpeakerIdEnrollment(
     StopSpeakerIdEnrollmentCallback callback) {
-  DCHECK(assistant_manager_service_->GetState() ==
-         AssistantManagerService::State::RUNNING);
+  DCHECK(HasStarted(assistant_manager_service_));
   DCHECK(main_task_runner()->RunsTasksInCurrentSequence());
 
   if (!assistant_manager_service_->assistant_manager_internal()) {
