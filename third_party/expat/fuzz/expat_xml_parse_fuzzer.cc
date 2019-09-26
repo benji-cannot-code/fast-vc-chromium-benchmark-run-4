f0cc45fe28756445b2e55aee0c1e33c74634191c
Ben Wagner <bungeman@chromium.org>
2019-09-26 19:20:36 +0000

    Move expat to DEPS.
    The third_party/expat directory has had a checked in copy of bits of
    libexpat. This has make updating quite difficult and blame more or less
    impossible. Move to using expat from DEPS to make rolling easier and
    more verifiable. This change should be build only and is not expected to
    actually change the source compiled.
    
    Bug: chromium:1004341
    Change-Id: I7fd74ddb97e8f5302bd4dfe9b24a1bd20821cf55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1822703
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700374}

--- third_party/expat/fuzz/expat_xml_parse_fuzzer.cc
+++ third_party/expat/fuzz/expat_xml_parse_fuzzer.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
-#include "third_party/expat/files/lib/expat.h"
+#include "third_party/expat/src/expat/lib/expat.h"
 
 #include <vector>
 
