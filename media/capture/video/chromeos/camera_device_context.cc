a2c768cacd4c0a71240667d8f504b08946b7a7a2
Ricky Liang <jcliang@chromium.org>
2019-09-26 09:20:49 +0000

    Video capture with GpuMemoryBuffer - Support buffer rotation
    Currently throughout the Chrome and WebRTC stack we implicitly assume
    that the camera frames are pre-rotated to the display orientation.
    This CL allows the CrOS VideoCaptureDevice to pre-rotate the
    GpuMemoryBuffer-based camera frames.
    
          Rotate the DUT UI and verify that the frames are displayed /
          encoded correctly.
    
    Bug: 982201
    Test: Manually test on DUT using Chrome camera app and WebRTC apps.
    Change-Id: I7f7a6d470280200ea89f60ca3dcbe8f521756e8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824760
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700152}

--- media/capture/video/chromeos/camera_device_context.cc
+++ media/capture/video/chromeos/camera_device_context.cc
@@ -5,25 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/capture/video/chromeos/camera_device_context.h"
 
-namespace {
-media::VideoRotation CameraFrameOrientationToVideoRotation(
-    int frame_orientation) {
-  switch (frame_orientation) {
-    case 0:
-      return media::VideoRotation::VIDEO_ROTATION_0;
-    case 90:
-      return media::VideoRotation::VIDEO_ROTATION_90;
-    case 180:
-      return media::VideoRotation::VIDEO_ROTATION_180;
-    case 270:
-      return media::VideoRotation::VIDEO_ROTATION_270;
-    default:
-      NOTREACHED() << "Invalid frame orientation " << frame_orientation;
-      return media::VideoRotation::VIDEO_ROTATION_0;
-  }
-}
-}  // namespace
-
 namespace media {
 
 CameraDeviceContext::CameraDeviceContext(
@@ -69,9 +50,9 @@ void CameraDeviceContext::SubmitCapturedVideoCaptureBuffer(
     base::TimeTicks reference_time,
     base::TimeDelta timestamp) {
   VideoFrameMetadata metadata;
-  metadata.SetRotation(
-      VideoFrameMetadata::Key::ROTATION,
-      CameraFrameOrientationToVideoRotation(GetCameraFrameOrientation()));
+  // All frames are pre-rotated to the display orientation.
+  metadata.SetRotation(VideoFrameMetadata::Key::ROTATION,
+                       VideoRotation::VIDEO_ROTATION_0);
   // TODO: Figure out the right color space for the camera frame.  We may need
   // to populate the camera metadata with the color space reported by the V4L2
   // device.
