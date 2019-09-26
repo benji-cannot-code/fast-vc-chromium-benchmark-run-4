6d4eca12dcee925cae3cd6600ee1c7adee6fd6a7
Rachel Wong <wrong@chromium.org>
2019-09-26 23:52:47 +0000

    [Files ranking] Add metrics for Quick Access fetch times and file type CTR.
    The Quick Access metrics will measure:
    1. The time it takes for GetQuickAccessItems to obtain results;
    2. The time between the start of the GetQuickAccessItems fetch, and the
    user entering zero state search.
    
    These will be used to evaluate whether there is enough time for
    results to be obtained before they are required to be displayed to the
    user.
    
    The CTR metrics will measure the number of impressions for local zero state files and Drive Quick Access files. An impression is counted if it contains at least one file of the relevant type.
    
    
    
    
    Bug: 999912
    Change-Id: I94d0dccc6407a3fbc4487c5ff884d8eff89acfce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1788778
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700497}

--- ash/app_list/views/search_result_list_view_unittest.cc
+++ ash/app_list/views/search_result_list_view_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "ui/views/test/views_test_base.h"
+#include "ui/views/test/widget_test.h"
 
 namespace app_list {
 namespace test {
@@ -28,16 +29,26 @@ namespace {
 int kDefaultSearchItems = 5;
 }  // namespace
 
-class SearchResultListViewTest : public views::ViewsTestBase {
+class SearchResultListViewTest : public views::test::WidgetTest {
  public:
   SearchResultListViewTest() = default;
   ~SearchResultListViewTest() override = default;
 
   // Overridden from testing::Test:
   void SetUp() override {
-    views::ViewsTestBase::SetUp();
+    views::test::WidgetTest::SetUp();
+    widget_ = CreateTopLevelPlatformWidget();
     view_ = std::make_unique<SearchResultListView>(nullptr, &view_delegate_);
+    widget_->SetBounds(gfx::Rect(0, 0, 300, 200));
+    widget_->GetContentsView()->AddChildView(view_.get());
+    widget_->Show();
     view_->SetResults(view_delegate_.GetSearchModel()->results());
+  }
+
+  void TearDown() override {
+    view_.reset();
+    widget_->CloseNow();
+    views::test::WidgetTest::TearDown();
   }
 
  protected:
@@ -100,6 +111,7 @@ class SearchResultListViewTest : public views::ViewsTestBase {
  private:
   AppListTestViewDelegate view_delegate_;
   std::unique_ptr<SearchResultListView> view_;
+  views::Widget* widget_;
 
   DISALLOW_COPY_AND_ASSIGN(SearchResultListViewTest);
 };
