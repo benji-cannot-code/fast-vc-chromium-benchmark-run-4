ad3c1a085df56b0e6d770daf784a6148d657e98f
Xiaohan Wang <xhwang@chromium.org>
2019-09-26 01:17:38 +0000

    Fix CdmHostFilePaths for single chrome dll
    File chrome_child.dll is only available when CHROME_MULTIPLE_DLL_BROWSER
    is defined. Check CHROME_MULTIPLE_DLL_BROWSER and fix the logic in
    cdm_host_file_path.cc.
    
    Bug: 1008039
    Test: Manually build Chrome and verified the behavior.
    Change-Id: I92d23b7a449ddfdfcdadc231ab0f012b7bd8b95d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825163
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700043}

--- chrome/common/media/cdm_host_file_path.cc
+++ chrome/common/media/cdm_host_file_path.cc
@@ -12,11 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
+#include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_version.h"
 
 #if defined(OS_MACOSX)
 #include "base/mac/bundle_locations.h"
-#include "chrome/common/chrome_constants.h"
 #endif
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -45,9 +45,16 @@ void AddCdmHostFilePaths(
 #if defined(OS_WIN)
 
   static const base::FilePath::CharType* const kUnversionedFiles[] = {
-      FILE_PATH_LITERAL("chrome.exe")};
+      chrome::kBrowserProcessExecutableName};
+
   static const base::FilePath::CharType* const kVersionedFiles[] = {
-      FILE_PATH_LITERAL("chrome.dll"), FILE_PATH_LITERAL("chrome_child.dll")};
+#if defined(CHROME_MULTIPLE_DLL)
+    chrome::kBrowserResourcesDll,
+    chrome::kChildDll
+#else
+    chrome::kBrowserResourcesDll
+#endif  // defined(CHROME_MULTIPLE_DLL)
+  };
 
   // Find where chrome.exe is installed.
   base::FilePath chrome_exe_dir;
