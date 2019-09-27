b4e3507288c3cca8cc94f12926c265ac911039e7
Kuo Jen Wei <inker@google.com>
2019-09-27 08:40:10 +0000

    Extract CameraAppHelper out of cros_camera.
    Leave all camera related mojo APIs in cros_camera, other mojo APIs
    not related to camera will be extracted out from cros_camera into a
    standalone build target.
    
    Bug: 980812
    Test: Apply intent forwarding CL, test file intent manually.
    Change-Id: Id2215d75d17291d4122844cc35c7d62722d67e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1813121
    Commit-Queue: Kuo Jen Wei <inker@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700619}

--- components/chromeos_camera/camera_app_helper_impl.h
+++ components/chromeos_camera/camera_app_helper_impl.h
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "media/capture/video/chromeos/mojom/camera_app.mojom.h"
+#include "components/chromeos_camera/common/camera_app_helper.mojom.h"
 
 namespace chromeos_camera {
 
-class CameraAppHelperImpl : public cros::mojom::CameraAppHelper {
+class CameraAppHelperImpl : public chromeos_camera::mojom::CameraAppHelper {
  public:
   using CameraResultCallback =
       base::RepeatingCallback<void(uint32_t,
@@ -23,7 +23,7 @@ class CameraAppHelperImpl : public cros::mojom::CameraAppHelper {
   explicit CameraAppHelperImpl(CameraResultCallback camera_result_callback);
   ~CameraAppHelperImpl() override;
 
-  // cros::mojom::CameraAppHelper implementations.
+  // chromeos_camera::mojom::CameraAppHelper implementations.
   void HandleCameraResult(uint32_t intent_id,
                           arc::mojom::CameraIntentAction action,
                           const std::vector<uint8_t>& data,
