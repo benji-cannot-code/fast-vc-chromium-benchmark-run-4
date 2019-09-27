442fadfef5c3ff64e97e96c77844ff92998f40f4
Xiaoqian Dai <xdai@chromium.org>
2019-09-27 17:45:06 +0000

    Extract GetSnapPosition() and GetIndicatorState() into util file.
    These two functions are used in multiple places and will also be used
    in overview gesture. So extract the common logic out and put into util
    functions.
    
    Bug: 997885
    Change-Id: I140c599d96998a5e61e0a37c6a16e4b9012846d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827189
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700777}

--- ash/wm/splitview/split_view_utils.h
+++ ash/wm/splitview/split_view_utils.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/display/screen_orientation_controller.h"
 #include "ash/wm/splitview/split_view_controller.h"
+#include "ash/wm/splitview/split_view_drag_indicators.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/transform.h"
 
@@ -105,6 +106,19 @@ ASH_EXPORT bool CanSnapInSplitview(aura::Window* window);
 ASH_EXPORT void ShowAppCannotSnapToast();
 
 ASH_EXPORT bool IsPhysicalLeftOrTop(SplitViewController::SnapPosition position);
+
+// Returns the desired snap position based on |location_in_screen|. The window
+// needs to be dragged into the drag indicator area on the edge of the screen
+// to be able to get snapped.
+ASH_EXPORT SplitViewController::SnapPosition GetSnapPosition(
+    aura::Window* window,
+    const gfx::Point& location_in_screen,
+    const gfx::Rect& work_area);
+
+// Returns the desried indicator state based on the desired |snap_position|.
+ASH_EXPORT IndicatorState
+GetIndicatorState(aura::Window* window,
+                  SplitViewController::SnapPosition snap_position);
 
 }  // namespace ash
 
