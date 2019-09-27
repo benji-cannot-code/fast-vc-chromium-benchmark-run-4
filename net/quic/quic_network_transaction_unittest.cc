cbefba48cc3307af1fb7debfb9735b292e123ded
Renjie Tang <renjietang@chromium.org>
2019-09-27 21:52:28 +0000

    Clean up quic_network_transaction_unittests for v99.
    Change-Id: I03ee9470e8e5cdae4221902c2a0da1251148cf5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1828283
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700868}

--- net/quic/quic_network_transaction_unittest.cc
+++ net/quic/quic_network_transaction_unittest.cc
@@ -1284,10 +1284,6 @@ TEST_P(QuicNetworkTransactionTest, ForceQuic) {
 }
 
 TEST_P(QuicNetworkTransactionTest, LargeResponseHeaders) {
-  // TODO(rch): honor the max header list size. b/136108828
-  if (quic::VersionUsesQpack(version_.transport_version))
-    return;
-
   session_params_.quic_params.origins_to_force_quic_on.insert(
       HostPortPair::FromString("mail.example.org:443"));
 
@@ -2470,7 +2466,7 @@ TEST_P(QuicNetworkTransactionTest, UseAlternativeServiceAllSupportedVersion) {
 
 TEST_P(QuicNetworkTransactionTest, GoAwayWithConnectionMigrationOnPortsOnly) {
   if (version_.transport_version == quic::QUIC_VERSION_99) {
-    // Not available under version 99
+    // GoAway is not available under version 99
     return;
   }
   MockQuicData mock_quic_data(version_);
@@ -8887,8 +8883,7 @@ TEST_P(QuicNetworkTransactionTest, QuicServerPushUpdatesPriority) {
   }
 
   if (quic::VersionUsesQpack(version_.transport_version)) {
-    // TODO(rch): both stream_dependencies and priority frames need to be
-    // supported in IETF QUIC.
+    // HTTP/3 currently doesn't support PRIORITY.
     return;
   }
 
