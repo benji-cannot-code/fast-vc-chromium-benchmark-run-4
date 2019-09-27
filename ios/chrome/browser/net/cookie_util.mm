f379d92009d79a3c061a1dfa75bc9a17ca35bd6f
Mohammad Refaat <mrefaat@chromium.org>
2019-09-27 06:26:46 +0000

    Remove CookieStoreIOSPersistent class
    This is no longer used in iOS
    
    Bug: 989554
    Change-Id: Ic3419218953881fc58b608021637db994cd0db9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824000
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700602}

--- ios/chrome/browser/net/cookie_util.mm
+++ ios/chrome/browser/net/cookie_util.mm
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/task/post_task.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#include "ios/net/cookies/cookie_store_ios_persistent.h"
+#import "ios/net/cookies/cookie_store_ios.h"
 #import "ios/net/cookies/system_cookie_store.h"
 #include "ios/web/common/features.h"
 #include "ios/web/public/thread/web_task_traits.h"
