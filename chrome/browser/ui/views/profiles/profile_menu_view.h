08265ed6047d169c1555d9e265b15dc04ccd47cb
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-09-27 13:48:11 +0000

    Revert "[profile-menu] Implement sync states"
    This reverts commit 551bbdf8c7e25ba7a587c2ad92d2d4d52526368e.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 700645 as the
    culprit for flakes in the build cycles as shown on:
    https://analysis.chromium.org/p/chromium/flake-portal/analysis/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyQwsSDEZsYWtlQ3VscHJpdCIxY2hyb21pdW0vNTUxYmJkZjhjN2UyNWJhN2E1ODdjMmFkOTJkMmQ0ZDUyNTI2MzY4ZQw
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.linux/Linux%20Tests/82259
    
    Sample Failed Step: browser_tests
    
    Sample Flaky Test: ProfileMenuClickTest_SyncError.SetupAndRunTest/5
    
    Original change's description:
    > [profile-menu] Implement sync states
    > 
    > Sync-is-on and sync-error states are added
    > to the profile menu.
    > 
    > Bug: 995720
    > Change-Id: I8e1b385298464dc57990cb75cec397851197cb30
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827423
    > Commit-Queue: Thomas Tangl <tangltom@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#700645}
    
    
    Change-Id: I1abe2790f5bd1d2311189b98a6386eaf7ed8e081
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 995720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829025
    Cr-Commit-Position: refs/heads/master@{#700701}

--- chrome/browser/ui/views/profiles/profile_menu_view.h
+++ chrome/browser/ui/views/profiles/profile_menu_view.h
@@ -50,8 +50,7 @@ class ProfileMenuView : public ProfileMenuViewBase, public AvatarMenuObserver {
     kOtherProfileButton = 13,
     kCookiesClearedOnExitLink = 14,
     kAddNewProfileButton = 15,
-    kSyncSettingsButton = 16,
-    kMaxValue = kSyncSettingsButton,
+    kMaxValue = kAddNewProfileButton,
   };
 
   ProfileMenuView(views::Button* anchor_button,
@@ -81,7 +80,6 @@ class ProfileMenuView : public ProfileMenuViewBase, public AvatarMenuObserver {
   void OnManageProfilesButtonClicked();
   void OnLockButtonClicked();
   void OnExitProfileButtonClicked();
-  void OnSyncSettingsButtonClicked();
   void OnSyncErrorButtonClicked(sync_ui_util::AvatarSyncErrorType error);
   void OnCurrentProfileCardClicked();
   void OnSigninButtonClicked();
