58bbfb1ad754330a381bef2c11bb3b3966b58404
Joey Arhar <jarhar@chromium.org>
2019-09-27 20:23:23 +0000

    Fix duplicate head and body from insertAdjacentHTML
    This patch makes document.body.insertAdjacentHTML('afterend', '<p>')
    insert the <p> as a sibling of document.body rather than creating a
    duplicate body and putting <p> in the duplicate.
    
    This fix was made in webkit in 2016:
    https://github.com/WebKit/webkit/commit/e3ca66a98a2a5edd553c411d6249a3a6a176174a
    
    fast/dom/insertAdjacentHTML-DocumentFragment-crash no longer produces a
    console message because using insertAdjacentHTML to create a child of a
    DocumentFragment now works, just as it does in safari and firefox.
    
    Bug: 978148
    Change-Id: I22248b914753a450e54f0a72066f5b0dcf584cb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824030
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700842}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -117,6 +117,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/custom/v0_custom_element_registration_context.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_controls_collection.h"
 #include "third_party/blink/renderer/core/html/forms/html_options_collection.h"
+#include "third_party/blink/renderer/core/html/html_body_element.h"
 #include "third_party/blink/renderer/core/html/html_collection.h"
 #include "third_party/blink/renderer/core/html/html_document.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
@@ -4430,6 +4431,7 @@ void Element::setOuterHTML(const StringOrTrustedHTML& string_or_html,
   }
 }
 
+// Step 4 of http://domparsing.spec.whatwg.org/#insertadjacenthtml()
 Node* Element::InsertAdjacent(const String& where,
                               Node* new_child,
                               ExceptionState& exception_state) {
@@ -4538,13 +4540,13 @@ ScriptPromise Element::updateRendering(ScriptState* script_state) {
 }
 
 // Step 1 of http://domparsing.spec.whatwg.org/#insertadjacenthtml()
-static Element* ContextElementForInsertion(const String& where,
-                                           Element* element,
-                                           ExceptionState& exception_state) {
+static Node* ContextNodeForInsertion(const String& where,
+                                     Element* element,
+                                     ExceptionState& exception_state) {
   if (DeprecatedEqualIgnoringCase(where, "beforeBegin") ||
       DeprecatedEqualIgnoringCase(where, "afterEnd")) {
-    Element* parent = element->parentElement();
-    if (!parent) {
+    Node* parent = element->parentNode();
+    if (!parent || IsA<Document>(parent)) {
       exception_state.ThrowDOMException(
           DOMExceptionCode::kNoModificationAllowedError,
           "The element has no parent.");
@@ -4579,11 +4581,22 @@ void Element::insertAdjacentText(const String& where,
 void Element::insertAdjacentHTML(const String& where,
                                  const String& markup,
                                  ExceptionState& exception_state) {
-  Element* context_element =
-      ContextElementForInsertion(where, this, exception_state);
-  if (!context_element)
+  Node* context_node = ContextNodeForInsertion(where, this, exception_state);
+  if (!context_node)
     return;
 
+  // Step 2 of http://domparsing.spec.whatwg.org/#insertadjacenthtml()
+  Element* context_element;
+  if (!IsA<Element>(context_node) ||
+      (context_node->GetDocument().IsHTMLDocument() &&
+       IsA<HTMLHtmlElement>(context_node))) {
+    context_element =
+        MakeGarbageCollected<HTMLBodyElement>(context_node->GetDocument());
+  } else {
+    context_element = To<Element>(context_node);
+  }
+
+  // Step 3 of http://domparsing.spec.whatwg.org/#insertadjacenthtml()
   DocumentFragment* fragment = CreateFragmentForInnerOuterHTML(
       markup, context_element, kAllowScriptingContent, "insertAdjacentHTML",
       exception_state);
