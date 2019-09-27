6ce3beca8a0e3afab26d7ed5cdfc9b04c3695d33
rbpotter <rbpotter@chromium.org>
2019-09-27 19:58:09 +0000

    Print Preview: Fix buggy delimiter replacement
    (1) Regex for replacing the thousands delimiter needs to use an escape,
    because the thousands delimiter may have another meaning (e.g. in the
    case of '.').
    (2) Remove thousands delimiters before replacing the decimal delimiter
    with '.', because the thousands delimiter may be '.', in which case
    replacing decimal delimiters with this value first will lead to them
    being removed.
    
    Also adding a test to run all current textbox parsing logic test cases
    with a metric unit system and different delimiters, which would have
    caught this bug.
    
    Bug: 1005816
    Change-Id: I7bf84fe819bdfecfc344ff0e9fefd1322a5b622a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827682
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700831}

--- chrome/test/data/webui/print_preview/print_preview_ui_browsertest.js
+++ chrome/test/data/webui/print_preview/print_preview_ui_browsertest.js
@@ -894,6 +894,13 @@ TEST_F('PrintPreviewCustomMarginsTest', 'SetControlsWithTextbox', function() {
 });
 
 TEST_F(
+    'PrintPreviewCustomMarginsTest', 'SetControlsWithTextboxMetric',
+    function() {
+      this.runMochaTest(
+          custom_margins_test.TestNames.SetControlsWithTextboxMetric);
+    });
+
+TEST_F(
     'PrintPreviewCustomMarginsTest', 'RestoreStickyMarginsAfterDefault',
     function() {
       this.runMochaTest(
