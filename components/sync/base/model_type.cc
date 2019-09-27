eb73ed482f04286bc66e3f1488c098d359285c8a
Marc Treib <treib@chromium.org>
2019-09-27 11:41:30 +0000

    Fix the build: Remove include of generated file that doesn't exist anymore
    Problem was introduced in
    https://chromium-review.googlesource.com/c/chromium/src/+/1821520
    
    Change-Id: If3e21e47c2b0111bf6e3422f62a14fd45f6e4ad7
    No-Tree-Checks: true
    No-Try: true
    Bug: 965503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827376
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700663}

--- components/sync/base/model_type.cc
+++ components/sync/base/model_type.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/string_split.h"
 #include "base/values.h"
-#include "components/sync/protocol/app_notification_specifics.pb.h"
 #include "components/sync/protocol/app_setting_specifics.pb.h"
 #include "components/sync/protocol/app_specifics.pb.h"
 #include "components/sync/protocol/autofill_specifics.pb.h"
