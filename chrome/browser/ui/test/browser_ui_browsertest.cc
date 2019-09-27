77240fb345958317570b0f6002d307fe7acb6f87
Robert Liao <robliao@chromium.org>
2019-09-27 20:56:21 +0000

    Centralize --test-launcher-interactive Options To Browser Tests Main
    This ensure all invocations of --test-launcher-interactive get the
    prerequisite flags even if they do not run through
    BrowserUiTest.Invoke.
    
    BUG=687387
    
    Change-Id: Iaa72a6281c0e561f44d919c487fd8b7bcbc1689c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816646
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700853}

--- chrome/browser/ui/test/browser_ui_browsertest.cc
+++ chrome/browser/ui/test/browser_ui_browsertest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "content/public/common/content_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/compositor/compositor_switches.h"
 
 namespace {
 
@@ -69,22 +68,13 @@ TEST(BrowserUiTest, Invoke) {
 
   base::LaunchOptions options;
 
-  // Generate screen output if --test-launcher-interactive was specified.
-  if (command.HasSwitch(switches::kTestLauncherInteractive)) {
-    command.AppendSwitch(switches::kEnablePixelOutputInTests);
 #if defined(OS_WIN)
-    // Under Windows, the child process won't launch without the wait option.
-    // See http://crbug.com/688534.
-    options.wait = true;
-    // Under Windows, dialogs (but not the browser window) created in the
-    // spawned browser_test process are invisible for some unknown reason.
-    // Pass in --disable-gpu to resolve this for now. See
-    // http://crbug.com/687387.
-    command.AppendSwitch(switches::kDisableGpu);
+  // Under Windows, the child process won't launch without the wait option.
+  // See http://crbug.com/688534.
+  options.wait = true;
+#else
+  options.wait = !command.HasSwitch(switches::kTestLauncherInteractive);
 #endif
-  } else {
-    options.wait = true;
-  }
 
   base::LaunchProcess(command, options);
 }
