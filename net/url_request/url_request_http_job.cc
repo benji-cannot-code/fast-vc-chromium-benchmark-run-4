7aee3d054257c29f8342a934d23685692549ced8
Imam Mohammad Bokhary <imam.bokhary@samsung.com>
2019-09-27 16:35:19 +0000

    Cookie exclusion and flagging event be logged in NetLog
    When cookies are associated with domain but not sent
    and received but not stored, Netlog event will be logged
    in Netlog viewer
    
    Bug: 1005217
    Change-Id: Iff497b9cd72d1827a04b6d135721d8ee96e3fb02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1817862
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700743}

--- net/url_request/url_request_http_job.cc
+++ net/url_request/url_request_http_job.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_util.h"
 #include "net/log/net_log.h"
 #include "net/log/net_log_event_type.h"
+#include "net/log/net_log_values.h"
 #include "net/log/net_log_with_source.h"
 #include "net/nqe/network_quality_estimator.h"
 #include "net/proxy_resolution/proxy_info.h"
@@ -672,6 +673,13 @@ void URLRequestHttpJob::SetCookieHeaderAndStart(
       status.AddExclusionReason(
           CanonicalCookie::CookieInclusionStatus::EXCLUDE_USER_PREFERENCES);
     }
+    if (!status.IsInclude()) {
+      request_->net_log().AddEvent(
+          NetLogEventType::COOKIE_INCLUSION_STATUS, [&] {
+            return NetLogParamsWithString("exclusion_reason",
+                                          status.GetDebugString());
+          });
+    }
     maybe_sent_cookies.push_back({cookie_with_status.cookie, status});
   }
 
@@ -750,6 +758,11 @@ void URLRequestHttpJob::SaveCookiesAndNotifyHeadersComplete(int result) {
           CanonicalCookie::CookieInclusionStatus::EXCLUDE_USER_PREFERENCES);
     }
     if (!returned_status.IsInclude()) {
+      request_->net_log().AddEvent(
+          NetLogEventType::COOKIE_INCLUSION_STATUS, [&] {
+            return NetLogParamsWithString("exclusion_reason",
+                                          returned_status.GetDebugString());
+          });
       OnSetCookieResult(options, cookie_to_return, std::move(cookie_string),
                         returned_status);
       continue;
