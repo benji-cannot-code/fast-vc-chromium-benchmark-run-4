e259fc169438b6b7e9f78b1335ddd205601d8c85
John Lee <johntlee@chromium.org>
2019-09-27 03:07:36 +0000

    WebUI: Update ThemeHandler to use FireWebUIListener for theme updates
    This will allow other WebUI to use the ThemeHandler to listen for theme
    updates without having to define a `ntp.themeChanged` method.
    
    Change-Id: I0c7a952f278477a99ea60b0eb08c68af37c11adf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1828122
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700566}

--- chrome/browser/resources/ntp4/incognito_tab.js
+++ chrome/browser/resources/ntp4/incognito_tab.js
@@ -4,6 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 window.addEventListener('load', function() {
+  cr.addWebUIListener('theme-changed', themeData => {
+    document.documentElement.setAttribute(
+        'hascustombackground', themeData.hasCustomBackground);
+    $('incognitothemecss').href =
+        'chrome://theme/css/incognito_new_tab_theme.css?' + Date.now();
+  });
   chrome.send('observeThemeChanges');
 });
 
@@ -13,13 +19,5 @@ const ntp = {
   /** @param {string} attached */
   setBookmarkBarAttached: function(attached) {
     document.documentElement.setAttribute('bookmarkbarattached', attached);
-  },
-
-  /** @param {!{hasCustomBackground: boolean}} themeData */
-  themeChanged: function(themeData) {
-    document.documentElement.setAttribute(
-        'hascustombackground', themeData.hasCustomBackground);
-    $('incognitothemecss').href =
-        'chrome://theme/css/incognito_new_tab_theme.css?' + Date.now();
   },
 };
