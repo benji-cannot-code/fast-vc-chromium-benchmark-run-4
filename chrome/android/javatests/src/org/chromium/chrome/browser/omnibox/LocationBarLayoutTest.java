b10aab7ff74ed162f68684468ec19df03899f790
Brandon Wylie <wylieb@chromium.org>
2019-09-28 15:27:04 +0000

    Fix and re-enable testOmniboxSearchEngineLogo_goneWhenIncognito
    This test was enabled for phones+tablets, but it should've only been
    enabled for phones.
    
    Bug: 1008912
    Change-Id: I36a0248b9db0b600399cc3af489b86077e263622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1830141
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#700970}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
@@ -22,7 +22,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
@@ -243,7 +242,7 @@ public class LocationBarLayoutTest {
 
     @Test
     @SmallTest
-    @DisabledTest
+    @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     @EnableFeatures(ChromeFeatureList.OMNIBOX_SEARCH_ENGINE_LOGO)
     @Feature({"OmniboxSearchEngineLogo"})
     public void testOmniboxSearchEngineLogo_goneWhenIncognito() throws Exception {
