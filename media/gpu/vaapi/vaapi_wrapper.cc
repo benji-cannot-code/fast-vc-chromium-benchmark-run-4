bab162084eb8179a89586316c9fcf54f444f1a1f
Daniele Castagna <dcastagna@chromium.org>
2019-09-29 22:56:02 +0000

    vaapi: Improve VAAPI encoding tracing
    While investigating if we have any lock contention in VaapiWrapper we
    noticed in VaapiVideoEncodeAccelerator many parts of the decoding are
    not taken into account.
    This CL adds traces on parts that accounts for the total encode time
    and that were not traced already.
    
    in particular:
     - VaapiWrapper::DownloadFromVABuffer that is the time it takes to
       copy the encoded stream into shared memory.
     - VaapiWrapper::CreateVABuffer, needed to create a buffer to decode
       into, while it's fast by itself, this might be waiting on another
       encode to finish.
     - VaapiWrapper::DestroyPendingBuffers, that is called before
       dowloading the buffer and it's in the critical path.
     - VAVEA::FromExecuteToReturn, it's the time from VAAPI::Execute to when
       we call the callback to notify the encode has finished.
     - VAVEA::FromCreateEncodeJobToReturn, is the time from when we start trying
       to process the encoding to when we notify the client the encode
       is done.
    
    We additionally added a few "Locked" traces, like for example
    
    VaapiWrapper::Execute and VaapiWrapper::ExecuteLocked, with the second
    one starting tracing after the Lock is acquired, in this way it's easy
    to see where we're waiting for vaapi and where we're waiting to acquire
    a lock.
    
    https://screenshot.googleplex.com/maVPXzKd7v0 makes clear how we were not
    accounting for all the decoding. VAVEA:: Execute trackes only a tiny
    percentange of the decoding wall time and DownloadFromVABuffer might take
    significantly longer.
    
    Bug: 1005592
    Test: Trace a video a encode and look at it.
    
    Change-Id: Ic7e5d58442238241b573d05c4ad86c2443aed74d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815951
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701007}

--- media/gpu/vaapi/vaapi_wrapper.cc
+++ media/gpu/vaapi/vaapi_wrapper.cc
@@ -1591,7 +1591,9 @@ bool VaapiWrapper::SubmitBuffer(VABufferType va_buffer_type,
                                 const void* buffer) {
   DCHECK_LT(va_buffer_type, VABufferTypeMax);
   DCHECK(buffer);
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::SubmitBuffer");
   base::AutoLock auto_lock(*va_lock_);
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::SubmitBufferLocked");
 
   VABufferID buffer_id;
   VAStatus va_res = vaCreateBuffer(va_display_, va_context_id_, va_buffer_type,
@@ -1650,7 +1652,9 @@ bool VaapiWrapper::SubmitVAEncMiscParamBuffer(
 }
 
 void VaapiWrapper::DestroyPendingBuffers() {
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::DestroyPendingBuffers");
   base::AutoLock auto_lock(*va_lock_);
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::DestroyPendingBuffersLocked");
 
   for (const auto& pending_va_buf : pending_va_bufs_) {
     VAStatus va_res = vaDestroyBuffer(va_display_, pending_va_buf);
@@ -1712,7 +1716,9 @@ std::unique_ptr<ScopedVAImage> VaapiWrapper::CreateVaImage(
 
 bool VaapiWrapper::UploadVideoFrameToSurface(const VideoFrame& frame,
                                              VASurfaceID va_surface_id) {
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::UploadVideoFrameToSurface");
   base::AutoLock auto_lock(*va_lock_);
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::UploadVideoFrameToSurfaceLocked");
 
   const gfx::Size size = frame.coded_size();
   bool va_create_put_fallback = false;
@@ -1786,7 +1792,9 @@ bool VaapiWrapper::UploadVideoFrameToSurface(const VideoFrame& frame,
 }
 
 bool VaapiWrapper::CreateVABuffer(size_t size, VABufferID* buffer_id) {
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::CreateVABuffer");
   base::AutoLock auto_lock(*va_lock_);
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::CreateVABufferLocked");
   VAStatus va_res =
       vaCreateBuffer(va_display_, va_context_id_, VAEncCodedBufferType, size, 1,
                      NULL, buffer_id);
@@ -1803,7 +1811,9 @@ bool VaapiWrapper::DownloadFromVABuffer(VABufferID buffer_id,
                                         size_t target_size,
                                         size_t* coded_data_size) {
   DCHECK(target_ptr);
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::DownloadFromVABuffer");
   base::AutoLock auto_lock(*va_lock_);
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::DownloadFromVABufferLocked");
 
   VAStatus va_res = vaSyncSurface(va_display_, sync_surface_id);
   VA_SUCCESS_OR_RETURN(va_res, "Failed syncing surface", false);
@@ -2177,6 +2187,7 @@ void VaapiWrapper::DestroySurface(VASurfaceID va_surface_id) {
 bool VaapiWrapper::Execute(VASurfaceID va_surface_id) {
   TRACE_EVENT0("media,gpu", "VaapiWrapper::Execute");
   base::AutoLock auto_lock(*va_lock_);
+  TRACE_EVENT0("media,gpu", "VaapiWrapper::ExecuteLocked");
 
   DVLOG(4) << "Pending VA bufs to commit: " << pending_va_bufs_.size();
   DVLOG(4) << "Pending slice bufs to commit: " << pending_slice_bufs_.size();
