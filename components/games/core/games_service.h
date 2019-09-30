1105ad6901e1de975fa7b7417aa0eb87f5ce6562
Sebastien <seblalancette@chromium.org>
2019-09-30 19:45:29 +0000

    Creating a new Games component.
    Creating a new component to be used to support upcoming games' surfaces.
    
    Added directory structure with a basic, stubbed service.
    
    More details about this component can be found in this design
    doc (Googlers Only):
    https://docs.google.com/document/d/1T5Le4V_a0J5idNWNmcGFZ4qFf_b_uUw2Z00zor8Piuo
    
    Bug: 1006874
    Change-Id: Id5e4e9785f3d4ab2343d30c09e6dfbd86cc6d3fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819680
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701244}

---
+++ components/games/core/games_service.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_GAMES_CORE_GAMES_SERVICE_H_
+#define COMPONENTS_GAMES_CORE_GAMES_SERVICE_H_
+
+#include "components/games/core/games_types.h"
+
+namespace games {
+
+class GamesService {
+ public:
+  virtual ~GamesService() = default;
+
+  virtual void GetHighlightedGame(HighlightedGameCallback callback) = 0;
+};
+
+}  // namespace games
+
+#endif  // COMPONENTS_GAMES_CORE_GAMES_SERVICE_H_
---
+++ components/games/core/games_service_impl.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_GAMES_CORE_GAMES_SERVICE_IMPL_H_
+#define COMPONENTS_GAMES_CORE_GAMES_SERVICE_IMPL_H_
+
+#include <memory>
+
+#include "components/games/core/games_service.h"
+#include "components/games/core/games_types.h"
+
+namespace games {
+
+class GamesServiceImpl : public GamesService {
+ public:
+  GamesServiceImpl();
+  ~GamesServiceImpl() override;
+
+  void GetHighlightedGame(HighlightedGameCallback callback) override;
+};
+
+}  // namespace games
+
+#endif  // COMPONENTS_GAMES_CORE_GAMES_SERVICE_IMPL_H_
