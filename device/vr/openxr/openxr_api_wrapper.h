77740594313b34a01948a7de33fe021e6c0a22ce
Kevin Qin <zheqi@microsoft.com>
2019-09-30 23:55:39 +0000

    Support WebXR Inputs Using OpenXR
    Support webxr input using OpenXR And Enable related WebXR input tests.
    This change can be tested with this page.
    https://immersive-web.github.io/webxr-samples/controller-state.html
    
    Bug: 986394
    Change-Id: I346d7808769a2ab1979d0f1f023b9c815611f292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1808086
    Commit-Queue: Zheng Qin <zheqi@microsoft.com>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701357}

--- device/vr/openxr/openxr_api_wrapper.h
+++ device/vr/openxr/openxr_api_wrapper.h
@@ -27,7 +27,7 @@ class Transform;
 
 namespace device {
 
-class OpenXrGamepadHelper;
+class OpenXRInputHelper;
 class VRTestHook;
 class ServiceTestHook;
 
@@ -47,7 +47,7 @@ class OpenXrApiWrapper {
   bool session_ended() const { return session_ended_; }
 
   XrResult InitSession(const Microsoft::WRL::ComPtr<ID3D11Device>& d3d_device,
-                       std::unique_ptr<OpenXrGamepadHelper>* gamepad_helper);
+                       std::unique_ptr<OpenXRInputHelper>* input_helper);
 
   XrResult BeginFrame(Microsoft::WRL::ComPtr<ID3D11Texture2D>* texture);
   XrResult EndFrame();
@@ -80,7 +80,7 @@ class OpenXrApiWrapper {
   XrResult CreateSwapchain();
   XrResult CreateSpace(XrReferenceSpaceType type, XrSpace* space);
   XrResult CreateGamepadHelper(
-      std::unique_ptr<OpenXrGamepadHelper>* gamepad_helper);
+      std::unique_ptr<OpenXRInputHelper>* input_helper);
 
   XrResult BeginSession();
   XrResult UpdateProjectionLayers();
