8f387cd99a2227326ddf263e1bf5c54eeffaee28
David Van Cleve <davidvc@chromium.org>
2019-09-30 14:15:27 +0000

    Adds referrer cap on cross-origin requests.
    This change adds an experimental feature to cap referrer granularity to
    origin on cross-origin requests. This requires making sure that both
    Blink and the network stack receive the updated referrer, which is a bit
    challenging on redirects: the networking stack computes some redirect
    state quite soon after determining that a redirect is happening (in
    URLRequestJob) and, whenever a deferred redirect gets the go-ahead,
    happily resumes using that state (cf. URLRequest::Redirect).
    
    This patch contains changes to:
    - //services/network's feature list, to add the feature;
    - content::Referrer's sanitization method, to perform an additional
      check when the feature is enabled;
    - NavigationRequest, to propagate its sanitized referrers to the
      WebNavigationParams Blink will receive, so that Blink will be able
      to get the correct referrer on redirects without having to be aware
      of the flag; and
    - NetworkContextNetworkDelegate, to truncate the referrer as appropriate
      on outgoing URLRequests
    
    It adds browser tests to make sure the cap overrides more permissive
    referrer policies, but doesn't override more restrictive ones.
    
    R=mmenke
    
    r=mmenke
    cc=kaustubhag
    
    Bug: 1004486
    Change-Id: I637ffe45fa3f73d775c714e72c1e78dc3d3c5617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816643
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701102}

--- content/public/common/referrer.cc
+++ content/public/common/referrer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "net/base/features.h"
 #include "services/network/loader_util.h"
+#include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/mojom/referrer.mojom.h"
 
 namespace content {
@@ -98,9 +99,13 @@ blink::mojom::ReferrerPtr Referrer::SanitizeForRequest(
       break;
   }
 
-  if (base::FeatureList::IsEnabled(net::features::kCapRefererHeaderLength) &&
-      base::saturated_cast<int>(sanitized_referrer->url.spec().length()) >
-          net::features::kMaxRefererHeaderLength.Get()) {
+  if ((base::FeatureList::IsEnabled(net::features::kCapRefererHeaderLength) &&
+       base::saturated_cast<int>(sanitized_referrer->url.spec().length()) >
+           net::features::kMaxRefererHeaderLength.Get()) ||
+      (base::FeatureList::IsEnabled(
+           network::features::kCapReferrerToOriginOnCrossOrigin) &&
+       !url::Origin::Create(sanitized_referrer->url)
+            .IsSameOriginWith(url::Origin::Create(request)))) {
     sanitized_referrer->url = sanitized_referrer->url.GetOrigin();
   }
 
--- services/network/public/cpp/features.h
+++ services/network/public/cpp/features.h
@@ -14,6 +14,8 @@ namespace network {
 namespace features {
 
 COMPONENT_EXPORT(NETWORK_CPP)
+extern const base::Feature kCapReferrerToOriginOnCrossOrigin;
+COMPONENT_EXPORT(NETWORK_CPP)
 extern const base::Feature kExpectCTReporting;
 COMPONENT_EXPORT(NETWORK_CPP)
 extern const base::Feature kNetworkErrorLogging;
