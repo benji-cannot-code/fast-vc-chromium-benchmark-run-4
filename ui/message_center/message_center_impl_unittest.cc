b680edf023900e1e3e7a558d062d262bb19f1475
Sylvain Defresne <sdefresne@chromium.org>
2019-09-30 12:43:05 +0000

    Convert ui/message_center away from base::Bind/base::Callback
    base::Bind/base::Callback are deprecated in favor of either
    base::BindOnce/base::OnceCallback or base::BindRepeating/
    base::RepeatingCallback (depending on whether the callback
    is invoked once or multiple time).
    
    Convert all uses of base::Bind/base::Callback in ui/message_center
    to the recommended methods/types.
    
    Bug: 1007852
    Change-Id: I98daa048c8729aaa3d42b35e69d562178c04ab95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1831859
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701079}

--- ui/message_center/message_center_impl_unittest.cc
+++ ui/message_center/message_center_impl_unittest.cc
@@ -167,7 +167,7 @@ class MessageCenterImplTest : public testing::Test {
   }
 
   base::RunLoop* run_loop() const { return run_loop_.get(); }
-  base::Closure closure() const { return closure_; }
+  base::RepeatingClosure closure() const { return closure_; }
 
  protected:
   std::unique_ptr<Notification> CreateSimpleNotification(
@@ -218,7 +218,7 @@ class MessageCenterImplTest : public testing::Test {
   MessageCenter* message_center_;
   std::unique_ptr<base::MessageLoop> loop_;
   std::unique_ptr<base::RunLoop> run_loop_;
-  base::Closure closure_;
+  base::RepeatingClosure closure_;
 
   DISALLOW_COPY_AND_ASSIGN(MessageCenterImplTest);
 };
@@ -317,7 +317,7 @@ namespace internal {
 class MockPopupTimersController : public PopupTimersController {
  public:
   MockPopupTimersController(MessageCenter* message_center,
-                            base::Closure quit_closure)
+                            base::RepeatingClosure quit_closure)
       : PopupTimersController(message_center),
         timer_finished_(0),
         quit_closure_(quit_closure) {}
@@ -335,7 +335,7 @@ class MockPopupTimersController : public PopupTimersController {
  private:
   int timer_finished_;
   std::string last_id_;
-  base::Closure quit_closure_;
+  base::RepeatingClosure quit_closure_;
 };
 
 TEST_F(MessageCenterImplTest, PopupTimersEmptyController) {
