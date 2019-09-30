7429db792ed52281d232beeb2b74132f0314e172
Patrick Monette <pmonette@chromium.org>
2019-09-30 21:41:43 +0000

    Add missing NOTIFICATION_PROFILE_CREATED
    This ensures that profiles created by the
    IndependentOTRProfileManager class triggers both
    NOTIFICATION_PROFILE_CREATED and
    NOTIFICATION_PROFILE_DESTROYED.
    
    Change-Id: Ie62b994ed7a02790c30331f9fb0053d1e9f38c29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1812002
    Reviewed-by: Brandon Tolsch <btolsch@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701301}

--- chrome/browser/media/router/presentation/independent_otr_profile_manager.cc
+++ chrome/browser/media/router/presentation/independent_otr_profile_manager.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 
 using content::BrowserThread;
@@ -59,6 +60,12 @@ IndependentOTRProfileManager::CreateFromOriginalProfile(
       callbacks_map_.emplace(otr_profile, std::move(callback));
   DCHECK(entry.second);
   DCHECK(callback_entry.second);
+
+  content::NotificationService::current()->Notify(
+      chrome::NOTIFICATION_PROFILE_CREATED,
+      content::Source<Profile>(otr_profile),
+      content::NotificationService::NoDetails());
+
   return base::WrapUnique(new OTRProfileRegistration(this, otr_profile));
 }
 
--- chrome/browser/media/router/presentation/independent_otr_profile_manager_browsertest.cc
+++ chrome/browser/media/router/presentation/independent_otr_profile_manager_browsertest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/test/test_utils.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
 
@@ -378,4 +379,32 @@ IN_PROC_BROWSER_TEST_F(IndependentOTRProfileManagerTest,
   run_loop.Run();
   ASSERT_FALSE(base::Contains(*BrowserList::GetInstance(), otr_browser));
   EXPECT_TRUE(destroyed);
+}
+
+IN_PROC_BROWSER_TEST_F(IndependentOTRProfileManagerTest, Notifications) {
+  // Create the OTR profile.
+  content::WindowedNotificationObserver profile_created_observer(
+      chrome::NOTIFICATION_PROFILE_CREATED,
+      content::NotificationService::AllSources());
+
+  auto* profile = browser()->profile();
+  auto profile_registration = manager_->CreateFromOriginalProfile(
+      profile, base::BindOnce(&OriginalProfileNeverDestroyed));
+  profile_created_observer.Wait();
+
+  // Verify the received notification.
+  auto* otr_profile = profile_registration->profile();
+  EXPECT_EQ(profile_created_observer.source(),
+            content::Source<Profile>(otr_profile));
+  EXPECT_FALSE(profile->HasOffTheRecordProfile());
+  EXPECT_TRUE(otr_profile->IsOffTheRecord());
+  EXPECT_TRUE(otr_profile->IsIndependentOffTheRecordProfile());
+
+  // Destroy the OTR profile.
+  content::WindowedNotificationObserver profile_destroyed_observer(
+      chrome::NOTIFICATION_PROFILE_DESTROYED,
+      content::Source<Profile>(otr_profile));
+
+  profile_registration.reset();
+  profile_destroyed_observer.Wait();
 }
