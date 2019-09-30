22f3fea83c43fe16188373ec72bb99d1bc90c454
Julie Jeongeun Kim <jkim@igalia.com>
2019-09-30 15:37:07 +0000

    Clean up MediaParserPtr and FilePatcherPtr from comments
    This CL cleans up MediaParserPtr and FilePatcher from
    comments since these interfaces has been converted to
    new mojo types already and mojo::Remote<MediaParser>
    and mojo::Remote<FilePatcher> has been used instead
    of MediaParserPtr and FilePatcherPtr from the code.
    
    NOTRY=true
    Bug: 955171
    
    Change-Id: I09d1f15e2c27147361a44a1117f1fd77af4e548d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827108
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701129}

--- chrome/services/media_gallery_util/public/cpp/media_parser_provider.h
+++ chrome/services/media_gallery_util/public/cpp/media_parser_provider.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 
 // Base class used by SafeMediaMetadataParser and SafeAudioVideoChecker to
-// retrieve a MediaParserPtr.
+// retrieve a mojo::Remote<MediaParser>.
 class MediaParserProvider {
  public:
   MediaParserProvider();
