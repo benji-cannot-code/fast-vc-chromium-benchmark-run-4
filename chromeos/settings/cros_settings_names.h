3d9a6de5845a30a563e1a140bc825aa06f246dda
Anatoliy Potapchuk <apotapchuk@chromium.org>
2019-09-30 12:45:54 +0000

    Add new device local account type -- Web Kiosk App
    This CL updates proto for DeviceLocalAccount policy and adds a new type
    of DeviceLocalAccount itself, which holds all the data.
    
    Bug: 1006230
    Change-Id: I446594f8d28350353cdf8c7a7687a9c3bd2abdf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816508
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701080}

--- chromeos/settings/cros_settings_names.h
+++ chromeos/settings/cros_settings_names.h
@@ -39,6 +39,8 @@ extern const char kAccountsPrefDeviceLocalAccountsKeyArcKioskAction[];
 COMPONENT_EXPORT(CHROMEOS_SETTINGS)
 extern const char kAccountsPrefDeviceLocalAccountsKeyArcKioskDisplayName[];
 COMPONENT_EXPORT(CHROMEOS_SETTINGS)
+extern const char kAccountsPrefDeviceLocalAccountsKeyWebKioskUrl[];
+COMPONENT_EXPORT(CHROMEOS_SETTINGS)
 extern const char kAccountsPrefDeviceLocalAccountAutoLoginId[];
 COMPONENT_EXPORT(CHROMEOS_SETTINGS)
 extern const char kAccountsPrefDeviceLocalAccountAutoLoginDelay[];
