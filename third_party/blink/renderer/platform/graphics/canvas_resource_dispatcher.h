2ae0470c697c1a36720271a1d73c93ae684cccfd
Fernando Serboncini <fserb@chromium.org>
2019-09-30 23:55:43 +0000

    Block Worker.RAF if OffscreenCanvas has too many pending frames
    Bug: 995235
    Change-Id: I6a3f55e6b4700c8eab85c6a266e6b2263b10d989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829487
    Auto-Submit: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701358}

--- third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_dispatcher.h
@@ -64,6 +64,7 @@ class PLATFORM_EXPORT CanvasResourceDispatcher
   void ReplaceBeginFrameAck(const viz::BeginFrameArgs& args) {
     current_begin_frame_ack_ = viz::BeginFrameAck(args, true);
   }
+  bool HasTooManyPendingFrames() const;
 
   void Reshape(const IntSize&);
 
