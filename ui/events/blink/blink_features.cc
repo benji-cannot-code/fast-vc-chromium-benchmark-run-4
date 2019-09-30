69656a1cb37104aec4297e04d57047e6d9767158
Ella Ge <eirage@chromium.org>
2019-09-30 14:24:29 +0000

    Revert "Enable scroll resampling on ToT"
    This reverts commit 91c87593b9577992baad8a594707d792714ccc23.
    
    Reason for revert: Perf regression crbug/1006810
    
    Original change's description:
    > Enable scroll resampling on ToT
    > 
    > \o/
    > 
    > Doc:
    > https://docs.google.com/document/d/1KrlQ0m5Ty9RKE1IorjEXDldYFJvUV_Ww10lnjh-TRN0/
    > 
    > Bug: 1000682
    > Change-Id: Ia77af76fd530be740d7d7c64706f47a48c248387
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1786591
    > Commit-Queue: Ella Ge <eirage@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#698698}
    
    TBR=bokan@chromium.org,eirage@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1000682
    Change-Id: Id30009dd4bae3dd366460f334b9d44b3d616b609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832141
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701103}

--- ui/events/blink/blink_features.cc
+++ ui/events/blink/blink_features.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 
 const base::Feature kResamplingScrollEvents{"ResamplingScrollEvents",
-                                            base::FEATURE_ENABLED_BY_DEFAULT};
+                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kFilteringScrollPrediction{
     "FilteringScrollPrediction", base::FEATURE_DISABLED_BY_DEFAULT};
