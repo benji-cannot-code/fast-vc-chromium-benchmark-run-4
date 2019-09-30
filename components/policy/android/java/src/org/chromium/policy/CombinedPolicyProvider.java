ca033a8dfb4e2fba6acf1a159a8f4990295973bd
Eric Stevenson <estevenson@chromium.org>
2019-09-30 21:54:54 +0000

    JNI refactor: @NativeMethods conversion (//components/policy).
    This CL was partially created by
    //base/android/jni_generator/jni_refactorer.py.
    
    The conversion also required converting unit tests to use the new JNI
    mocking approach provided by JniMocker.
    
    Bug: 929661
    Change-Id: If47b5ba988f61e4b179d483fb0073e11f0d50992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1831416
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701307}

--- components/policy/android/java/src/org/chromium/policy/CombinedPolicyProvider.java
+++ components/policy/android/java/src/org/chromium/policy/CombinedPolicyProvider.java
@@ -11,6 +11,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -93,7 +94,7 @@ public class CombinedPolicyProvider {
                 mPolicyConverter.setPolicy(key, settings.get(key));
             }
         }
-        nativeFlushPolicies(mNativeCombinedPolicyProvider);
+        CombinedPolicyProviderJni.get().flushPolicies(mNativeCombinedPolicyProvider, get());
     }
 
     void terminateIncognitoSession() {
@@ -133,14 +134,12 @@ public class CombinedPolicyProvider {
         void terminateIncognitoSession();
     }
 
-    @VisibleForTesting
-    public static void set(CombinedPolicyProvider p) {
+    static void setForTesting(CombinedPolicyProvider p) {
         sInstance = p;
     }
 
-    @VisibleForTesting
-    CombinedPolicyProvider() {}
-
-    @VisibleForTesting
-    protected native void nativeFlushPolicies(long nativeAndroidCombinedPolicyProvider);
+    @NativeMethods
+    interface Natives {
+        void flushPolicies(long nativeAndroidCombinedPolicyProvider, CombinedPolicyProvider caller);
+    }
 }
