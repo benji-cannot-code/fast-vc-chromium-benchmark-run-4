fcb6780918a8acb440b46a96100799e32e4adb39
Kenichi Ishibashi <bashi@chromium.org>
2019-09-30 08:35:37 +0000

    Remove unused include headers from ServiceWorkerVersion
    Bug: N/A
    Change-Id: Ie4d5166b58097a4ab290a74dd1267b2276811682
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1831622
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701040}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -47,11 +47,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/origin_trials/trial_token_validator.h"
 #include "third_party/blink/public/common/service_worker/service_worker_type_converters.h"
 #include "third_party/blink/public/common/service_worker/service_worker_utils.h"
-#include "third_party/blink/public/mojom/service_worker/embedded_worker.mojom.h"
-#include "third_party/blink/public/mojom/service_worker/service_worker_error_type.mojom.h"
-#include "third_party/blink/public/mojom/service_worker/service_worker_installed_scripts_manager.mojom.h"
-#include "third_party/blink/public/mojom/service_worker/service_worker_object.mojom.h"
-#include "third_party/blink/public/web/web_console_message.h"
 
 namespace content {
 namespace {
