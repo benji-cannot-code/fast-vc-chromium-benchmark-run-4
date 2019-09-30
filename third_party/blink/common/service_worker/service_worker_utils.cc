25c7aa75b7aa9c97fe41c515f0946e081e4fa0dd
Kenichi Ishibashi <bashi@chromium.org>
2019-09-30 04:53:26 +0000

    service worker: Remove ServiceWorkerAggressiveCodeCache feature
    We've concluded that we won't actively pursue this feature as we
    weren't able to see performance improvements.
    
    Bug: 915081
    Change-Id: I0a6a804b4fbc613dc8b8c7abff64e4d4d21419df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1828726
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701024}

--- third_party/blink/common/service_worker/service_worker_utils.cc
+++ third_party/blink/common/service_worker/service_worker_utils.cc
@@ -6,37 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/service_worker/service_worker_utils.h"
 
 #include "base/feature_list.h"
-#include "base/metrics/field_trial_params.h"
 #include "third_party/blink/public/common/features.h"
 
 namespace blink {
 
-const char kServiceWorkerEagerCodeCacheStrategy[] = "sw_cache_strategy";
-
-namespace {
-
-constexpr base::FeatureParam<EagerCodeCacheStrategy>::Option
-    kEagerCodeCacheStrategyOptions[] = {
-        {EagerCodeCacheStrategy::kDontGenerate, "dontgenerate"},
-        {EagerCodeCacheStrategy::kDuringInstallEvent, "installevent"},
-        {EagerCodeCacheStrategy::kOnIdleTask, "idletask"},
-};
-
-constexpr base::FeatureParam<EagerCodeCacheStrategy>
-    kEagerCodeCacheStrategyParam{&features::kServiceWorkerAggressiveCodeCache,
-                                 "sw_cache_strategy",
-                                 EagerCodeCacheStrategy::kDuringInstallEvent,
-                                 &kEagerCodeCacheStrategyOptions};
-
-}  // namespace
-
 bool ServiceWorkerUtils::IsImportedScriptUpdateCheckEnabled() {
   return base::FeatureList::IsEnabled(
       blink::features::kServiceWorkerImportedScriptUpdateCheck);
-}
-
-EagerCodeCacheStrategy ServiceWorkerUtils::GetEagerCodeCacheStrategy() {
-  return kEagerCodeCacheStrategyParam.Get();
 }
 
 }  // namespace blink
