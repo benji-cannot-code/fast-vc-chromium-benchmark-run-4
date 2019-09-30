6b4575f513b86307689d00bb3442f8e4345f1fe5
Erik Hendriks <hendriks@chromium.org>
2019-09-30 22:52:24 +0000

    Add title and document content width/height to DOMSnapshot.captureSnapshot results.
    Add a test to see that misc details are showing up ok. Also, make
    stabilization of the string table in the pseudo element test more
    robust.
    
    Change-Id: I3e5500c8095f41e4dc15ce3d6181c9fa8c5d33a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827180
    Commit-Queue: Erik Hendriks <hendriks@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701328}

--- third_party/blink/renderer/core/inspector/inspector_dom_snapshot_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_dom_snapshot_agent.cc
@@ -325,6 +325,7 @@ void InspectorDOMSnapshotAgent::VisitDocument(Document* document) {
               AddString(InspectorDOMAgent::DocumentURLString(document)))
           .setBaseURL(
               AddString(InspectorDOMAgent::DocumentBaseURLString(document)))
+          .setTitle(AddString(document->title()))
           .setContentLanguage(AddString(document->ContentLanguage()))
           .setEncodingName(AddString(document->EncodingName()))
           .setFrameId(
@@ -374,6 +375,9 @@ void InspectorDOMSnapshotAgent::VisitDocument(Document* document) {
     auto offset = document->View()->LayoutViewport()->GetScrollOffset();
     document_->setScrollOffsetX(offset.Width());
     document_->setScrollOffsetY(offset.Height());
+    auto contents_size = document->View()->LayoutViewport()->ContentsSize();
+    document_->setContentWidth(contents_size.Width());
+    document_->setContentHeight(contents_size.Height());
   }
 
   if (paint_order_map_) {
