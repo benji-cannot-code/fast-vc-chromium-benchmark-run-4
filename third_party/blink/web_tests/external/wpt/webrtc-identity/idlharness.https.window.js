ab2fa9e172ebfd858e3ff66ba1d39906a4fe76d7
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-09-30 18:44:59 +0000

    Import wpt@521caf5b7fb93f198a3f4de3d81f5f023765a8c5
    Using wpt-import in Chromium 49215b9c0d04a3b5eb7aa1348957ac6144c04ccf.
    With Chromium commits locally applied on WPT:
    03ccd4e912 "html: Move autofocus tests to html/interaction/focus/the-autofocus-attribute/"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: I947e87177f8b63649d4deb435a1184ed351caee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832112
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#701202}

---
+++ third_party/blink/web_tests/external/wpt/webrtc-identity/idlharness.https.window.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['webrtc-identity'],
+  ['webrtc', 'mediacapture-streams', 'html', 'dom'],
+  async idlArray => {
+    idlArray.add_objects({
+      RTCPeerConnection: [`new RTCPeerConnection()`],
+      RTCIdentityAssertion: [`new RTCIdentityAssertion('idp', 'name')`],
+      MediaStreamTrack: ['track'],
+      // TODO: RTCIdentityProviderGlobalScope
+      // TODO: RTCIdentityProviderRegistrar
+    });
+
+    try {
+      self.track = await navigator.mediaDevices
+          .getUserMedia({audio: true})
+          .then(m => m.getTracks()[0]);
+    } catch (e) {}
+  }
+);
