40d554a7a59b017bf7378d5c79ca0b37b573beb1
Joel Hockey <joelhockey@chromium.org>
2019-09-30 22:49:58 +0000

    Add --use-system-clipboard flag for linux-chromeos
    Bug: 985187
    Change-Id: I789786546d0ca7556d4f17bc87b6c0ff5eb02f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1826754
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701327}

--- ui/base/clipboard/clipboard_aura.cc
+++ ui/base/clipboard/clipboard_aura.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard_monitor.h"
 #include "ui/base/clipboard/custom_data_helper.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/ozone/buildflags.h"
 
 namespace ui {
 
@@ -411,10 +412,14 @@ class ClipboardDataBuilder {
 
 ClipboardData* ClipboardDataBuilder::current_data_ = nullptr;
 
+// linux-chromeos uses aura clipboard by default, but supports ozone x11
+// with flag --use-system-clipbboard.
+#if !defined(OS_CHROMEOS) || !BUILDFLAG(OZONE_PLATFORM_X11)
 // Clipboard factory method.
 Clipboard* Clipboard::Create() {
   return new ClipboardAura;
 }
+#endif
 
 // ClipboardAura implementation.
 ClipboardAura::ClipboardAura()
--- ui/base/clipboard/clipboard_ozone.cc
+++ ui/base/clipboard/clipboard_ozone.cc
@@ -20,8 +20,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard_constants.h"
 #include "ui/base/clipboard/custom_data_helper.h"
 #include "ui/gfx/codec/png_codec.h"
+#include "ui/ozone/buildflags.h"
 #include "ui/ozone/public/ozone_platform.h"
 #include "ui/ozone/public/platform_clipboard.h"
+
+#if defined(OS_CHROMEOS) && BUILDFLAG(OZONE_PLATFORM_X11)
+#include "base/command_line.h"
+#include "ui/base/clipboard/clipboard_aura.h"
+#include "ui/base/ui_base_switches.h"
+#endif
 
 namespace ui {
 
@@ -286,6 +293,14 @@ class ClipboardOzone::AsyncClipboardOzone {
 
 // Clipboard factory method.
 Clipboard* Clipboard::Create() {
+// linux-chromeos uses aura clipboard by default, but supports ozone x11
+// with flag --use-system-clipbboard.
+#if defined(OS_CHROMEOS) && BUILDFLAG(OZONE_PLATFORM_X11)
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kUseSystemClipboard)) {
+    return new ClipboardAura;
+  }
+#endif
   return new ClipboardOzone;
 }
 
