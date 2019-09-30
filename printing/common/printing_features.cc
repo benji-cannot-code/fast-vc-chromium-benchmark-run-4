54e6cdfb77e3797963ecd5f731b190a757785ca4
Daniel Hosseinian <dhoss@chromium.org>
2019-09-30 21:37:10 +0000

    Enable HarfbuzzPDFSubsetter feature by default.
    This is the last step before rolling out the feature to 100% stable.
    Experiment results may be found on the issue.
    
    Bug: 931719
    Change-Id: Ibd9cbeea735066e57e61a0fa8853b8b2a8ad737e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832299
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701298}

--- printing/common/printing_features.cc
+++ printing/common/printing_features.cc
@@ -9,7 +9,7 @@ namespace printing {
 namespace features {
 
 const base::Feature kHarfBuzzPDFSubsetter{"HarfBuzzPDFSubsetter",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
+                                          base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 }  // namespace printing
