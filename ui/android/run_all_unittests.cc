619b55312811b41fbe98350e7b892239c7344694
Sylvain Defresne <sdefresne@chromium.org>
2019-10-01 14:29:55 +0000

    Convert ui/android away from base::Bind/base::Callback
    base::Bind/base::Callback are deprecated in favor of either
    base::BindOnce/base::OnceCallback or base::BindRepeating/
    base::RepeatingCallback (depending on whether the callback
    is invoked once or multiple time).
    
    Convert all uses of base::Bind/base::Callback in ui/android
    to the recommended methods/types.
    
    Bug: 1007845
    Change-Id: Ia401c734f7530732917c94f7dea6359200f34199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1833461
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701559}

--- ui/android/run_all_unittests.cc
+++ ui/android/run_all_unittests.cc
@@ -47,5 +47,5 @@ int main(int argc, char** argv) {
 
   return base::LaunchUnitTests(
       argc, argv,
-      base::Bind(&UIAndroidTestSuite::Run, base::Unretained(&test_suite)));
+      base::BindOnce(&UIAndroidTestSuite::Run, base::Unretained(&test_suite)));
 }
