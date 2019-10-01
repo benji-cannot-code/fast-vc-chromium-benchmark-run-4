5a1327e2c1e790bc01e236188208f056267c963e
Trevor Perrier <perrier@chromium.org>
2019-10-01 17:08:23 +0000

    Add "Learn more" link to Lite mode FRE
    This CL changes the Lite mode FRE to include a "Learn more" link that
    points to the Chrome Lite mode help documents. This uses p-link "lite_mode"
    
    The wording on the FRE page has also been changed to make indicate
    that Lite mode sends web traffic to Google.
    
    
    
    Bug: 997248
    Change-Id: Idb2f69ecfefe68f1a5f6d8052d393d213283e64c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1830106
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Trevor  Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701632}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/DataReductionProxyFirstRunFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/DataReductionProxyFirstRunFragment.java
@@ -5,18 +5,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.firstrun;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.SwitchCompat;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.datareduction.DataReductionPromoUtils;
 import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
+import org.chromium.ui.text.NoUnderlineClickableSpan;
+import org.chromium.ui.text.SpanApplier;
+import org.chromium.ui.text.SpanApplier.SpanInfo;
 
 /**
  * The First Run Experience fragment that allows the user to opt in to Data Saver.
@@ -40,6 +46,8 @@ public class DataReductionProxyFirstRunFragment extends Fragment implements Firs
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        final TextView promoSummaryTextView =
+                (TextView) view.findViewById(R.id.data_reduction_promo_summary_text);
         final SwitchCompat enableDataSaverSwitch = (SwitchCompat) view
                 .findViewById(R.id.enable_data_saver_switch);
         Button nextButton = (Button) view.findViewById(R.id.next_button);
@@ -57,6 +65,19 @@ public class DataReductionProxyFirstRunFragment extends Fragment implements Firs
                 }
             }
         });
+
+        // Setup Promo Text Learn More Link
+        Resources resources = getResources();
+        NoUnderlineClickableSpan clickablePromoLearnMoreSpan =
+                new NoUnderlineClickableSpan(resources, (view1) -> {
+                    if (!isAdded()) return;
+                    getPageDelegate().showInfoPage(R.string.data_reduction_promo_learn_more_url);
+                });
+
+        promoSummaryTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        promoSummaryTextView.setText(
+                SpanApplier.applySpans(getString(R.string.data_reduction_promo_summary_lite_mode),
+                        new SpanInfo("<link>", "</link>", clickablePromoLearnMoreSpan)));
 
         nextButton.setOnClickListener(new OnClickListener() {
             @Override
