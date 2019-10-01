db8437744f1f567e1d184aaf67ad0ee401e44a72
Alex Newcomer <newcomer@chromium.org>
2019-10-01 23:13:22 +0000

    Cros: Fix scrollable shelf broken with hotseat enabled
    Remove the custom event routing, this fixes the bug.
    
    Bug: 1009041
    Change-Id: I88a096a18ce70f459afcf7ecaa328c4ac2296d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832283
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Auto-Submit: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701807}

--- ash/shelf/hotseat_widget.cc
+++ ash/shelf/hotseat_widget.cc
@@ -11,10 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_model.h"
 #include "ash/public/cpp/wallpaper_controller_observer.h"
 #include "ash/shelf/scrollable_shelf_view.h"
-#include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shelf/shelf_navigation_widget.h"
 #include "ash/shelf/shelf_view.h"
-#include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/wallpaper/wallpaper_controller_impl.h"
@@ -188,11 +186,6 @@ void HotseatWidget::OnMouseEvent(ui::MouseEvent* event) {
 void HotseatWidget::OnGestureEvent(ui::GestureEvent* event) {
   if (event->type() == ui::ET_GESTURE_TAP_DOWN)
     keyboard::KeyboardUIController::Get()->HideKeyboardImplicitlyByUser();
-  GetShelfView()
-      ->shelf_widget()
-      ->shelf_layout_manager()
-      ->ProcessGestureEventFromHotseatWidget(
-          event, static_cast<aura::Window*>(event->target()));
 
   if (!event->handled())
     views::Widget::OnGestureEvent(event);
