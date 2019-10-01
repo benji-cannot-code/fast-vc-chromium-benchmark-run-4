b04a39c069208c31e95f8b12f46ee6598408edad
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-10-01 05:11:29 +0000

    [code health] Remove NOTIFICATION_BOOKMARK_CONTEXT_MENU_SHOWN
    NOTIFICATION_BOOKMARK_CONTEXT_MENU_SHOWN is used to schedule a callback
    right before the bookmark context menu is run. This is used only in
    interactive tests.  Instead of using this notification, use a more
    explicit API to register callbacks to run before running the context
    menu.
    
    BUG=268984
    
    Change-Id: I43c78118d6fd7ef53f0b7e7c70aefc85670cf78b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1831275
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701434}

--- chrome/browser/ui/views/bookmarks/bookmark_context_menu.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_context_menu.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/i18n/rtl.h"
+#include "base/lazy_instance.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -22,6 +23,9 @@ using bookmarks::BookmarkNode;
 using content::PageNavigator;
 
 namespace {
+
+base::LazyInstance<base::OnceClosure>::Leaky pre_run_callback =
+    LAZY_INSTANCE_INITIALIZER;
 
 // Returns true if |command_id| corresponds to a command that causes one or more
 // bookmarks to be removed.
@@ -70,15 +74,19 @@ BookmarkContextMenu::BookmarkContextMenu(
 BookmarkContextMenu::~BookmarkContextMenu() {
 }
 
+void BookmarkContextMenu::InstallPreRunCallback(base::OnceClosure callback) {
+  DCHECK(pre_run_callback.Get().is_null());
+  pre_run_callback.Get() = std::move(callback);
+}
+
 void BookmarkContextMenu::RunMenuAt(const gfx::Point& point,
                                     ui::MenuSourceType source_type) {
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kKioskMode))
     return;
 
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_BOOKMARK_CONTEXT_MENU_SHOWN,
-      content::Source<BookmarkContextMenu>(this),
-      content::NotificationService::NoDetails());
+  if (!pre_run_callback.Get().is_null())
+    std::move(pre_run_callback.Get()).Run();
+
   // width/height don't matter here.
   menu_runner_->RunMenuAt(parent_widget_, nullptr,
                           gfx::Rect(point.x(), point.y(), 0, 0),
