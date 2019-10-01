45452fbb5f29759f717db125f7ec9b259724a49f
Jinsuk Kim <jinsukkim@chromium.org>
2019-10-01 04:59:59 +0000

    Android: Move API Tab.getProfile handler to Profile
    Moves the internal job handling API to Profile class
    where it suits better. The API is still present; it will
    be refactored by updating the call sites to use a helper
    method in other CL.
    
    Bug: 925242, 995903
    Change-Id: I292adc2bb9d4bddaccc6857ad33d7b9444c51b53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1830255
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701433}

--- chrome/browser/profiles/profile_android.cc
+++ chrome/browser/profiles/profile_android.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_destroyer.h"
 #include "chrome/browser/profiles/profile_key_android.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "content/public/browser/web_contents.h"
 
 using base::android::AttachCurrentThread;
 using base::android::JavaParamRef;
@@ -123,6 +124,23 @@ void ProfileAndroid::Wipe(JNIEnv* env, const JavaParamRef<jobject>& obj) {
 // static
 ScopedJavaLocalRef<jobject> JNI_Profile_GetLastUsedProfile(JNIEnv* env) {
   return ProfileAndroid::GetLastUsedProfile(env);
+}
+
+// static
+ScopedJavaLocalRef<jobject> JNI_Profile_FromWebContents(
+    JNIEnv* env,
+    const JavaParamRef<jobject>& jweb_contents) {
+  auto* web_contents = content::WebContents::FromJavaWebContents(jweb_contents);
+  if (!web_contents)
+    return ScopedJavaLocalRef<jobject>();
+  Profile* profile =
+      Profile::FromBrowserContext(web_contents->GetBrowserContext());
+  if (!profile)
+    return ScopedJavaLocalRef<jobject>();
+  ProfileAndroid* profile_android = ProfileAndroid::FromProfile(profile);
+  if (!profile_android)
+    return ScopedJavaLocalRef<jobject>();
+  return profile_android->GetJavaObject();
 }
 
 ProfileAndroid::ProfileAndroid(Profile* profile)
