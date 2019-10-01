c05327244ee2fc3201fb620097b117b9e632c2f5
Matthew Mourgos <mmourgos@chromium.org>
2019-10-01 16:00:32 +0000

    cros: Bring shelf UI colors up to spec.
    This change unifies the shelf colors by moving shelf color
    calculation into ShelfConfig, to be accessed with
    ShelfConfig::Get()->GetDefaultShelfColor().
    
    The base color for the shelf is calculated using colors from
    AshColorProvider and wallpaper_controller.
    
    Bug: 1008700
    Change-Id: If25805e613d4f47ecb834ac5a2912b22688fe5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1823603
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701599}

--- ash/shelf/shelf_background_animator_unittest.cc
+++ ash/shelf/shelf_background_animator_unittest.cc
@@ -215,7 +215,7 @@ TEST_F(ShelfBackgroundAnimatorTest, DefaultBackground) {
   PaintBackground(SHELF_BACKGROUND_DEFAULT);
 
   EXPECT_EQ(SHELF_BACKGROUND_DEFAULT, animator_->target_background_type());
-  EXPECT_EQ(ShelfConfig::Get()->shelf_translucent_alpha(),
+  EXPECT_EQ((int)SkColorGetA(ShelfConfig::Get()->GetDefaultShelfColor()),
             observer_.GetBackgroundAlpha());
 }
 
@@ -224,7 +224,7 @@ TEST_F(ShelfBackgroundAnimatorTest, MaximizedBackground) {
   PaintBackground(SHELF_BACKGROUND_MAXIMIZED);
 
   EXPECT_EQ(SHELF_BACKGROUND_MAXIMIZED, animator_->target_background_type());
-  EXPECT_EQ(ShelfConfig::Get()->shelf_translucent_maximized_window(),
+  EXPECT_EQ((int)SkColorGetA(ShelfConfig::Get()->GetMaximizedShelfColor()),
             observer_.GetBackgroundAlpha());
 }
 
@@ -233,7 +233,7 @@ TEST_F(ShelfBackgroundAnimatorTest, FullscreenAppListBackground) {
   PaintBackground(SHELF_BACKGROUND_APP_LIST);
 
   EXPECT_EQ(SHELF_BACKGROUND_APP_LIST, animator_->target_background_type());
-  EXPECT_EQ(ShelfConfig::Get()->shelf_translucent_over_app_list(),
+  EXPECT_EQ((int)SkColorGetA(ShelfConfig::Get()->GetShelfWithAppListColor()),
             observer_.GetBackgroundAlpha());
 }
 
@@ -326,8 +326,7 @@ TEST_F(ShelfBackgroundTargetColorTest, ShelfBackgroundColorUpdatedFromLogin) {
 
   NotifySessionStateChanged(session_manager::SessionState::ACTIVE);
   EXPECT_EQ(test_api.shelf_background_target_color(),
-            SkColorSetA(ShelfConfig::Get()->shelf_default_base_color(),
-                        ShelfConfig::Get()->shelf_translucent_alpha()));
+            ShelfConfig::Get()->GetDefaultShelfColor());
 }
 
 // Verify the target color of the shelf background is updated based on session
@@ -349,8 +348,7 @@ TEST_F(ShelfBackgroundTargetColorTest, ShelfBackgroundColorUpdatedFromOOBE) {
 
   NotifySessionStateChanged(session_manager::SessionState::ACTIVE);
   EXPECT_EQ(test_api.shelf_background_target_color(),
-            SkColorSetA(ShelfConfig::Get()->shelf_default_base_color(),
-                        ShelfConfig::Get()->shelf_translucent_alpha()));
+            ShelfConfig::Get()->GetDefaultShelfColor());
 }
 
 }  // namespace ash
