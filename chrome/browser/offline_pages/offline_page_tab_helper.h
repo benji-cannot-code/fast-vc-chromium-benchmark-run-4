37f6b8d0856b300233d79b016f920b611ba4979d
Julie Jeongeun Kim <jkim@igalia.com>
2019-10-01 00:53:19 +0000

    Clean up CreateMhtmlPageNotifier from OfflinePageTabHelper
    This CL cleans up CreateMhtmlPageNotifier method from
    OfflinePageTabHelper.
    
    Bug: 955171
    Change-Id: Id0ab7104717bb9cf8e485fe1b23952ab9691de78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829634
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#701380}

--- chrome/browser/offline_pages/offline_page_tab_helper.h
+++ chrome/browser/offline_pages/offline_page_tab_helper.h
@@ -59,10 +59,6 @@ class OfflinePageTabHelper
  public:
   ~OfflinePageTabHelper() override;
 
-  // Creates the Mojo service that can listen to the renderer's archive events.
-  void CreateMhtmlPageNotifier(
-      offline_pages::mojom::MhtmlPageNotifierRequest request);
-
   // MhtmlPageNotifier overrides.
   void NotifyMhtmlPageLoadAttempted(blink::mojom::MHTMLLoadResult result,
                                     const GURL& main_frame_url,
