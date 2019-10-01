102a21370ffcc1b36ab6e12807d4042dfd10aa66
Andrew Xu <andrewxu@chromium.org>
2019-10-01 17:44:28 +0000

    Re-enable launcher tests
    Re-enable those tests which were disabled due to crbug.com/1008744.
    
    Bug: 1008744
    Change-Id: Id67c436f50777631b855eb5c4405473dcadf14d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832708
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701656}

--- chrome/browser/ui/ash/launcher_animations_interactive_uitest.cc
+++ chrome/browser/ui/ash/launcher_animations_interactive_uitest.cc
@@ -112,13 +112,6 @@ class LauncherAnimationsTestBase : public UIPerformanceTest,
   DISALLOW_COPY_AND_ASSIGN(LauncherAnimationsTestBase);
 };
 
-// TODO(crbug.com/1009459): many of these tests are failing on MSan builds.
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_Run DISABLED_Run
-#else
-#define MAYBE_Run Run
-#endif
-
 class LauncherAnimationsFullscreenTest : public LauncherAnimationsTestBase {
  public:
   LauncherAnimationsFullscreenTest() = default;
@@ -134,7 +127,7 @@ class LauncherAnimationsFullscreenTest : public LauncherAnimationsTestBase {
   DISALLOW_COPY_AND_ASSIGN(LauncherAnimationsFullscreenTest);
 };
 
-IN_PROC_BROWSER_TEST_P(LauncherAnimationsFullscreenTest, MAYBE_Run) {
+IN_PROC_BROWSER_TEST_P(LauncherAnimationsFullscreenTest, Run) {
   SendKeyAndWaitForState(ui::VKEY_BROWSER_SEARCH, true,
                          ash::AppListViewState::kFullscreenAllApps);
   SendKeyAndWaitForState(ui::VKEY_BROWSER_SEARCH, true,
@@ -160,7 +153,7 @@ class LauncherAnimationsExpandToFullscreenTest
   DISALLOW_COPY_AND_ASSIGN(LauncherAnimationsExpandToFullscreenTest);
 };
 
-IN_PROC_BROWSER_TEST_P(LauncherAnimationsExpandToFullscreenTest, MAYBE_Run) {
+IN_PROC_BROWSER_TEST_P(LauncherAnimationsExpandToFullscreenTest, Run) {
   SendKeyAndWaitForState(ui::VKEY_BROWSER_SEARCH, false,
                          ash::AppListViewState::kPeeking);
   SendKeyAndWaitForState(ui::VKEY_BROWSER_SEARCH, true,
@@ -188,7 +181,7 @@ class LauncherAnimationsPeekingTest : public LauncherAnimationsTestBase {
   DISALLOW_COPY_AND_ASSIGN(LauncherAnimationsPeekingTest);
 };
 
-IN_PROC_BROWSER_TEST_P(LauncherAnimationsPeekingTest, MAYBE_Run) {
+IN_PROC_BROWSER_TEST_P(LauncherAnimationsPeekingTest, Run) {
   SendKeyAndWaitForState(ui::VKEY_BROWSER_SEARCH, false,
                          ash::AppListViewState::kPeeking);
   SendKeyAndWaitForState(ui::VKEY_BROWSER_SEARCH, false,
