655819ca3b9ec322804d868f9d9622121bda429b
Long Cheng <lgcheng@google.com>
2019-10-01 19:09:53 +0000

    Uprev closure_compiler/externs/chrome_extension.js
    Run third_party/closure_compiler/roll_closure_compiler to update
    closure_compiler/externs/chrome_extension.js and revert
    closuer_compiler binary.
    
    Change-Id: Ia2bb9afeaa38623808e81515043714ff8c0e05f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814389
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Cr-Commit-Position: refs/heads/master@{#701700}

--- chrome/browser/resources/chromeos/braille_ime/braille_ime.js
+++ chrome/browser/resources/chromeos/braille_ime/braille_ime.js
@@ -203,7 +203,7 @@ BrailleIme.prototype = {
 
   /**
    * Called by the IME framework when a text field receives focus.
-   * @param {InputContext} context Input field context.
+   * @param {chrome.input.ime.InputContext} context Input field context.
    * @private
    */
   onFocus_: function(context) {
@@ -223,7 +223,7 @@ BrailleIme.prototype = {
 
   /**
    * Called by the IME framework when the current input context is updated.
-   * @param {InputContext} context Input field context.
+   * @param {chrome.input.ime.InputContext} context Input field context.
    * @private
    */
   onInputContextUpdate_: function(context) {
@@ -416,8 +416,8 @@ BrailleIme.prototype = {
 
   /**
    * Sends the given input context to ChromeVox.
-   * @param {InputContext} context Input context, or null when there's no input
-   *    context.
+   * @param {chrome.input.ime.InputContext} context Input context, or null when
+   *    there's no input context.
    * @private
    */
   sendInputContext_: function(context) {
