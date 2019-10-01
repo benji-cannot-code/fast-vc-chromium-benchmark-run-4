cbe5f6b428423487b10f131d36ed9879eb712bf7
Steven Bennetts <stevenjb@chromium.org>
2019-10-01 20:49:51 +0000

    Internet > Details: Set proxySettings correctly
    Also adds some debug logging that will help investigate this sort
    of failure.
    
    Bug: 1009988
    Change-Id: I4814a4eaf5685e6148628856494859d71b8b5744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834203
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701748}

--- chrome/browser/resources/settings/internet_page/internet_detail_page.js
+++ chrome/browser/resources/settings/internet_page/internet_detail_page.js
@@ -1210,7 +1210,7 @@ Polymer({
       return;
     }
     const config = this.getDefaultConfigProperties_();
-    config.proxySetings = event.detail;
+    config.proxySettings = event.detail;
     this.setMojoNetworkProperties_(config);
   },
 
