73fb16cd6ef011f9be81fbc68218c0d1e7ae68f5
Fredrik Söderquist <fs@opera.com>
2019-10-01 19:25:16 +0000

    Don't perform recursive interval updates on interval notifications
    When we're updating an interval and get a new interval notifications are
    sent to all dependent syncbases. These dependents may in turn deliver
    notifications back to the original timed element, which would
    recursively update its interval - potentially clobbering the current
    one. Instead of doing that, leave the notification function early if we
    detect that we're in the process of notifying dependents, and then let
    the regular interval update code pick up and process the new instance
    time(s). This means we can notify dependents in more logical ways (i.e
    directly after updating |interval_|) rather than setting
    |interval_has_changed_| and let UpdateSyncBases() do the notifications.
    UpdateSyncBases() remains for now, but will be removed in a follow-up.
    
    Bug: 998526
    Change-Id: I2ace98c3443fd95645df724e5532465f4ca307f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832807
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701707}

--- third_party/blink/renderer/core/svg/animation/svg_smil_element.h
+++ third_party/blink/renderer/core/svg/animation/svg_smil_element.h
@@ -87,10 +87,7 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
   SMILTime SimpleDuration() const;
 
   bool CurrentIntervalIsActive(SMILTime elapsed);
-  void DiscardOrRevalidateCurrentInterval(SMILTime presentation_time);
-  // Check if the current interval is still current, and if not compute the
-  // next interval.
-  void CheckAndUpdateInterval(SMILTime elapsed);
+  void UpdateInterval(SMILTime presentation_time);
   void UpdateActiveState(SMILTime elapsed);
   void UpdateSyncBases();
 
@@ -165,6 +162,10 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
 
   SMILInterval ResolveInterval(SMILTime begin_after, SMILTime end_after) const;
   bool ResolveFirstInterval();
+  // Check if the current interval is still current, and if not compute the
+  // next interval.
+  base::Optional<SMILInterval> CheckAndUpdateInterval(SMILTime elapsed);
+  void DiscardOrRevalidateCurrentInterval(SMILTime presentation_time);
   SMILTime ResolveActiveEnd(SMILTime resolved_begin,
                             SMILTime resolved_end) const;
   SMILTime RepeatingDuration() const;
@@ -282,6 +283,7 @@ class CORE_EXPORT SVGSMILElement : public SVGElement, public SVGTests {
   mutable SMILTime cached_max_;
 
   bool interval_has_changed_;
+  bool instance_lists_have_changed_;
   bool is_notifying_dependents_;
 
   friend class ConditionEventListener;
