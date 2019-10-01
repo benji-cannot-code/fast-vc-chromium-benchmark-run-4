e713553b0090e63dc4e447dc8e8ce2572538de3e
Henrik Boström <hbos@chromium.org>
2019-10-01 09:30:37 +0000

    Revert "[PM] Add SharedWorkerWatcher"
    This reverts commit 7c39080741de6a79aa8a1a0f7e8fb3eb8f95f111.
    
    Reason for revert: Speculative revert due to failures on
    ChromeWorkerBrowserTest.SharedWorkerScriptFetchWithThirdPartyBlocking
    on windows.
    First one on the Win7 (32) Tests:
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20%2832%29%20Tests/52766
    
    Original change's description:
    > [PM] Add SharedWorkerWatcher
    > 
    > This class observes running shared worker instances, keeps track of
    > the connection to their client frames and manages the lifetime of
    > worker nodes in the Performance Manager graph.
    > 
    > Bug: 993029
    > Change-Id: I2e0c7cd9f194a06405293c22fd543a14ff3c6cbc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1757283
    > Commit-Queue: Patrick Monette <pmonette@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#701445}
    
    TBR=falken@chromium.org,pmonette@chromium.org,siggi@chromium.org
    
    Change-Id: I60357d4217f658967920dc456517764b5123c77c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 993029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832266
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701495}

--- content/public/browser/shared_worker_service.h
+++ content/public/browser/shared_worker_service.h
@@ -13,13 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 
-namespace base {
-class UnguessableToken;
-}
-
 namespace url {
 class Origin;
-}
+}  // namespace url
 
 namespace content {
 
