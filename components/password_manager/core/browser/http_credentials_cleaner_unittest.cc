f3b69d13f52e3d81bd6885b5a5e34bf2daf02a6b
Xida Chen <xidachen@chromium.org>
2019-10-01 13:36:24 +0000

    Disable HttpCredentialCleanerTest.ReportHttpMigrationMetrics on Mac
    TBR=hbos@chromium.org
    NOTRY=true
    
    Bug: 1010016
    Change-Id: Ifff6105c6f46a35d8248f1c5c70237a104e8d714
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1833311
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701542}

--- components/password_manager/core/browser/http_credentials_cleaner_unittest.cc
+++ components/password_manager/core/browser/http_credentials_cleaner_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/browser/test_password_store.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
@@ -124,7 +125,12 @@ class HttpCredentialCleanerTest : public ::testing::TestWithParam<TestCase> {
   DISALLOW_COPY_AND_ASSIGN(HttpCredentialCleanerTest);
 };
 
-TEST_P(HttpCredentialCleanerTest, ReportHttpMigrationMetrics) {
+#if defined(OS_MACOSX)
+#define MAYBE_ReportHttpMigrationMetrics DISABLED_ReportHttpMigrationMetrics
+#else
+#define MAYBE_ReportHttpMigrationMetrics ReportHttpMigrationMetrics
+#endif
+TEST_P(HttpCredentialCleanerTest, MAYBE_ReportHttpMigrationMetrics) {
   struct Histogram {
     bool test_hsts_enabled;
     HttpCredentialType test_type;
