cec63512be5da750448f8d5da7610324b80e98fe
Daniel Nicoara <dnicoara@chromium.org>
2019-10-01 17:38:29 +0000

    Pass URL in page events
    Webview clients want to know the URL for the page event. Include it for
    convenience to reduce the number of IPCs.
    
    Bug: b/132811925
    Test: Compiled
    Change-Id: I4d2b4fd7ead9002270f5008cb4bbfb5be57b3904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1833117
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701650}

--- chromecast/browser/webview/webview_controller.cc
+++ chromecast/browser/webview/webview_controller.cc
@@ -336,6 +336,7 @@ void WebviewController::OnPageStateChanged(CastWebContents* cast_web_contents) {
     std::unique_ptr<webview::WebviewResponse> response =
         std::make_unique<webview::WebviewResponse>();
     auto* event = response->mutable_page_event();
+    event->set_url(contents_->GetURL().spec());
     event->set_current_page_state(current_state());
     client_->EnqueueSend(std::move(response));
   }
@@ -348,6 +349,7 @@ void WebviewController::OnPageStopped(CastWebContents* cast_web_contents,
     std::unique_ptr<webview::WebviewResponse> response =
         std::make_unique<webview::WebviewResponse>();
     auto* event = response->mutable_page_event();
+    event->set_url(contents_->GetURL().spec());
     event->set_current_page_state(current_state());
     event->set_stopped_error_code(error_code);
     client_->EnqueueSend(std::move(response));
@@ -362,6 +364,7 @@ void WebviewController::ResourceLoadFailed(CastWebContents* cast_web_contents) {
     std::unique_ptr<webview::WebviewResponse> response =
         std::make_unique<webview::WebviewResponse>();
     auto* event = response->mutable_page_event();
+    event->set_url(contents_->GetURL().spec());
     event->set_current_page_state(current_state());
     event->set_resource_load_failed(true);
     client_->EnqueueSend(std::move(response));
