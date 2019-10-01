53b4ebd859023c5e2ef6b96bcf1ebe009c3839f2
Bo Liu <boliu@chromium.org>
2019-10-01 01:15:48 +0000

    weblayer: Avoid some flicker scenarios
    Ideally, during a surface swap, the new view is inserted *below* the
    existing view in hwui. Then swap chromium compositor to the surface from
    the new view, and only detach the existing view when chromium compositor
    has swapped a frame into the surface of the new view. This should in
    theory avoid flickers of blank frames during surface swaps.
    
    This CL does various things to achieve the the above behavior:
    * Insert new view below old view. New view is inserted at position 0.
    * For SurfaceView that uses surface control, need retain the EGL surface
      to keep the surface alive.
    * Use postOnAnimation to manipulate view tree. It is unsafe to
      add/remove child views during draw.
    * Separate SurfaceData.destroy into markForDestroy and destroy.
      markForDestroy stops surface notifications. destroy will now be called
      after the first swap to remove the view from the view tree.
    * Ensure surfaceDestroyed is always called in markForDestroy.
      surfaceCreated and surfaceDestroyed is not guaranteed to be paired.
    
    And a clean up: remove all unused caller args in jni.
    
    Note there is still no guarantee there is no flicker. There will
    probably need to require more manul experimentation and manual testing.
    Potential more hacks:
    * TextureView may not have drawn the frame that chromium compositor
      produced. Can consider listening to TextureView's invalidate.
    * SurfaceView will teardown its own surface before removing the "hole"
      it draws into hwui surface, leading to a flicker. May need to either
      avoid this or cover it up before detaching it.
    
    Change-Id: I350f7da1890fdf72253759af00cb9f7f74dff0d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1830080
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701392}

--- weblayer/browser/content_view_render_view.cc
+++ weblayer/browser/content_view_render_view.cc
@@ -52,14 +52,12 @@ static jlong JNI_ContentViewRenderView_Init(
   return reinterpret_cast<intptr_t>(content_view_render_view);
 }
 
-void ContentViewRenderView::Destroy(JNIEnv* env,
-                                    const JavaParamRef<jobject>& obj) {
+void ContentViewRenderView::Destroy(JNIEnv* env) {
   delete this;
 }
 
 void ContentViewRenderView::SetCurrentWebContents(
     JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& jweb_contents) {
   InitCompositor();
   content::WebContents* web_contents =
@@ -75,7 +73,6 @@ void ContentViewRenderView::SetCurrentWebContents(
 
 void ContentViewRenderView::OnPhysicalBackingSizeChanged(
     JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
     const JavaParamRef<jobject>& jweb_contents,
     jint width,
     jint height) {
@@ -85,19 +82,20 @@ void ContentViewRenderView::OnPhysicalBackingSizeChanged(
   web_contents->GetNativeView()->OnPhysicalBackingSizeChanged(size);
 }
 
-void ContentViewRenderView::SurfaceCreated(JNIEnv* env,
-                                           const JavaParamRef<jobject>& obj) {
+void ContentViewRenderView::SurfaceCreated(JNIEnv* env) {
   InitCompositor();
 }
 
 void ContentViewRenderView::SurfaceDestroyed(JNIEnv* env,
-                                             const JavaParamRef<jobject>& obj) {
+                                             jboolean cache_back_buffer) {
+  evict_back_buffer_on_next_swap_ = cache_back_buffer;
+  if (evict_back_buffer_on_next_swap_)
+    compositor_->CacheBackBufferForCurrentSurface();
   compositor_->SetSurface(nullptr, false);
 }
 
 void ContentViewRenderView::SurfaceChanged(
     JNIEnv* env,
-    const JavaParamRef<jobject>& obj,
     jboolean can_be_used_with_surface_control,
     jint width,
     jint height,
@@ -108,9 +106,7 @@ void ContentViewRenderView::SurfaceChanged(
 }
 
 base::android::ScopedJavaLocalRef<jobject>
-ContentViewRenderView::GetResourceManager(
-    JNIEnv* env,
-    const base::android::JavaParamRef<jobject>& jobj) {
+ContentViewRenderView::GetResourceManager(JNIEnv* env) {
   return compositor_->GetResourceManager().GetJavaObject();
 }
 
@@ -122,6 +118,10 @@ void ContentViewRenderView::UpdateLayerTreeHost() {
 void ContentViewRenderView::DidSwapFrame(int pending_frames) {
   JNIEnv* env = base::android::AttachCurrentThread();
   Java_ContentViewRenderView_didSwapFrame(env, java_obj_);
+  if (evict_back_buffer_on_next_swap_) {
+    compositor_->EvictCachedBackBuffer();
+    evict_back_buffer_on_next_swap_ = false;
+  }
 }
 
 void ContentViewRenderView::InitCompositor() {
