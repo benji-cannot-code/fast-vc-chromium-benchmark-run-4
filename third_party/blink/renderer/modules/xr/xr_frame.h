f62e4943e438eadad9cbd5590680a57ba8c2d6ca
Jacob DeWitt <jacde@chromium.org>
2019-10-01 22:36:42 +0000

    Reland "Update emulated position on each XRFrame"
    CL https://chromium-review.googlesource.com/c/chromium/src/+/1808086,
    which conflicted with this one has been reverted, which means this CL
    can be relanded as-is.
    
    This is a reland of 8be89ae08126913ff4447927ac5a087b8d6babcf
    
    Original change's description:
    > Update emulated position on each XRFrame
    >
    > For both the head poses and input poses, send this value through mojo
    > from the device process to the renderer process on every frame.
    >
    > Bug: 969131
    > Change-Id: Ie895deb75bddb6dd883d09828cfcb30f335d3793
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825478
    > Auto-Submit: Jacob DeWitt <jacde@chromium.org>
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Klaus Weidner <klausw@chromium.org>
    > Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#701389}
    
    Bug: 969131
    Change-Id: Ib6ea499fbc081962e5572591258c1247d057279f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834202
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Jacob DeWitt <jacde@chromium.org>
    Commit-Queue: Jacob DeWitt <jacde@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701782}

--- third_party/blink/renderer/modules/xr/xr_frame.h
+++ third_party/blink/renderer/modules/xr/xr_frame.h
@@ -42,7 +42,7 @@ class XRFrame final : public ScriptWrappable {
   XRWorldInformation* worldInformation() const { return world_information_; }
   XRAnchorSet* trackedAnchors() const;
 
-  void SetMojoFromViewer(const TransformationMatrix&);
+  void SetMojoFromViewer(const TransformationMatrix&, bool emulated_position);
 
   void Trace(blink::Visitor*) override;
 
@@ -55,6 +55,8 @@ class XRFrame final : public ScriptWrappable {
   HeapVector<Member<XRHitTestResult>> getHitTestResults(
       XRHitTestSource* hitTestSource,
       XRSpace* relativeTo);
+
+  bool EmulatedPosition() const { return emulated_position_; }
 
  private:
   std::unique_ptr<TransformationMatrix> GetAdjustedPoseMatrix(XRSpace*) const;
@@ -77,6 +79,8 @@ class XRFrame final : public ScriptWrappable {
   // animation frames. getViewerPose should only be called from JS on active
   // animation frames.
   bool is_animation_frame_ = false;
+
+  bool emulated_position_ = false;
 };
 
 }  // namespace blink
