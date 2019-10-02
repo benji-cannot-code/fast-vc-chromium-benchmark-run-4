2fe99c403d07dffe1ce1695042e3d8ba8b91ae65
Antonio Gomes <tonikitoo@igalia.com>
2019-10-02 14:20:11 +0000

    Migrate away from using ChildThreadImpl in MediaStreamTrackMetrics
    This CL prepares MediaStreamTrackMetrics to be Onion souped.
    
    BUG=787254
    R=guidou@chromium.org
    
    Change-Id: I9c31edef1cc690afcbc08b0cde2b18faec866558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834363
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702022}

--- content/renderer/media/webrtc/media_stream_track_metrics.cc
+++ content/renderer/media/webrtc/media_stream_track_metrics.cc
@@ -9,10 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/hash/md5.h"
-#include "base/threading/thread_task_runner_handle.h"
-#include "content/child/child_thread_impl.h"
-#include "content/public/common/service_names.mojom.h"
+#include "base/strings/stringprintf.h"
+#include "base/threading/thread_checker.h"
 #include "content/renderer/render_thread_impl.h"
+#include "third_party/blink/public/common/thread_safe_browser_interface_broker_proxy.h"
+#include "third_party/blink/public/platform/platform.h"
 
 namespace content {
 
@@ -265,7 +266,7 @@ uint64_t MediaStreamTrackMetrics::MakeUniqueId(const std::string& track_id,
 mojo::Remote<blink::mojom::MediaStreamTrackMetricsHost>&
 MediaStreamTrackMetrics::GetMediaStreamTrackMetricsHost() {
   if (!track_metrics_host_) {
-    ChildThreadImpl::current()->BindHostReceiver(
+    blink::Platform::Current()->GetBrowserInterfaceBrokerProxy()->GetInterface(
         track_metrics_host_.BindNewPipeAndPassReceiver());
   }
   return track_metrics_host_;
