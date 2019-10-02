23319de695b7cef838cd058af5fbbdae13b1f03e
David Staessens <dstaessens@chromium.org>
2019-10-02 02:18:46 +0000

    media/gpu/test: Allow video frame validator to write corrupted frames to disk.
    This CL adds functionality to the new video_decode_accelerator_tests to write
    frames that fail frame validation to disk. The "--output_frames" parameter now
    takes "all" and "corrupt" as possible values. If "corrupt" is specified the
    number of frames written to disk is limited to 3 by default, to avoid generating
    to may artifacts in Tast tests.
    
    TEST=ran new VDA tests on eve
    
    BUG=962354
    
    Change-Id: Iad086c099d486e05b343d6fe6550807007ef1c92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1609025
    Commit-Queue: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701872}

--- media/gpu/test/video_frame_file_writer.h
+++ media/gpu/test/video_frame_file_writer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_GPU_TEST_VIDEO_FRAME_FILE_WRITER_H_
 #define MEDIA_GPU_TEST_VIDEO_FRAME_FILE_WRITER_H_
 
+#include <limits>
 #include <memory>
 
 #include "base/files/file_path.h"
@@ -35,9 +36,13 @@ class VideoFrameFileWriter : public VideoFrameProcessor {
   ~VideoFrameFileWriter() override;
 
   // Create an instance of the video frame file writer.
+  // |output_folder| specifies the folder video frames will be written to.
+  // |output_format| specifies the output file format.
+  // |output_limit| limits the max number of files that can be written.
   static std::unique_ptr<VideoFrameFileWriter> Create(
       const base::FilePath& output_folder,
-      OutputFormat output_format = OutputFormat::kPNG);
+      OutputFormat output_format = OutputFormat::kPNG,
+      size_t output_limit = std::numeric_limits<size_t>::max());
 
   // Interface VideoFrameProcessor
   void ProcessVideoFrame(scoped_refptr<const VideoFrame> video_frame,
@@ -47,7 +52,8 @@ class VideoFrameFileWriter : public VideoFrameProcessor {
 
  private:
   VideoFrameFileWriter(const base::FilePath& output_folder,
-                       OutputFormat output_format);
+                       OutputFormat output_format,
+                       size_t output_limit);
 
   // Initialize the video frame file writer.
   bool Initialize();
@@ -67,12 +73,16 @@ class VideoFrameFileWriter : public VideoFrameProcessor {
   const base::FilePath output_folder_;
   // Output format of the frames.
   const OutputFormat output_format_;
+  // The maximum number of frames that can be written.
+  const size_t output_limit_;
 
   // The video frame mapper used to gain access to the raw video frame memory.
   std::unique_ptr<VideoFrameMapper> video_frame_mapper_;
 
   // The number of frames currently queued for writing.
   size_t num_frames_writing_ GUARDED_BY(frame_writer_lock_);
+  // The number of frames currently written or queued to be written.
+  size_t num_frames_writes_requested_ = 0u;
 
   // Thread on which video frame writing is done.
   base::Thread frame_writer_thread_;
