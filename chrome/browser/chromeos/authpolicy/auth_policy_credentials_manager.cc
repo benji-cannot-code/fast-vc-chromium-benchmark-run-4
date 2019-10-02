0bf3cb281e3fb1784aefd013f70ed9ea36eda297
Kush Sinha <sinhak@chromium.org>
2019-10-02 16:33:29 +0000

    Account Manager flag cleanup: AD credentials manager
    Chrome OS Account Manager has been turned on by default on ToT, and has
    been launched via Finch for some time now. Clean up its flag's usages in
    Active Directory credentials manager.
    
    TBR=rsorokin@chromium.org
    
    Bug: 1009960
    Change-Id: Id81addd05162e2bd911b555ab47f24248a9312e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832212
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702061}

--- chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.cc
+++ chrome/browser/chromeos/authpolicy/auth_policy_credentials_manager.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/theme_resources.h"
 #include "chromeos/components/account_manager/account_manager.h"
 #include "chromeos/components/account_manager/account_manager_factory.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/dbus/auth_policy/auth_policy_client.h"
 #include "chromeos/network/network_handler.h"
 #include "chromeos/network/network_state.h"
@@ -51,9 +50,6 @@ constexpr char kProfileSigninNotificationId[] = "chrome://settings/signin/";
 // |profile| is a non-owning pointer to |Profile|.
 // |account_id| is the |AccountId| for the Device Account.
 void SetupAccountManager(Profile* profile, const AccountId& account_id) {
-  if (!features::IsAccountManagerEnabled())
-    return;
-
   AccountManagerFactory* factory =
       g_browser_process->platform_part()->GetAccountManagerFactory();
   DCHECK(factory);
