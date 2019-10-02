61a1f07415ae547f4a3ca1db74a1bb0c01dba160
Tim van der Lippe <tvanderlippe@chromium.org>
2019-10-02 18:15:32 +0000

    Migrate platform/ to ESM
    - Change Multimap to Platform.Multimap
    - Importscripts in workers. This means that the file does not export any
    utilities, but we will need to clean that up later.
    
    Bug: 1006759
    Change-Id: I91d45febebfc597f0d0b5c6a017c26f6b12b84b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832218
    Commit-Queue: Tim Van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702111}

--- third_party/blink/renderer/devtools/front_end/heap_snapshot_worker.js
+++ third_party/blink/renderer/devtools/front_end/heap_snapshot_worker.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 // Release build has Runtime.js bundled.
 
+self.importScripts('platform/utilities.js');
 if (!self.Root || !self.Root.Runtime) {
   self.importScripts('Runtime.js');
 }
--- third_party/blink/renderer/devtools/front_end/root.js
+++ third_party/blink/renderer/devtools/front_end/root.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './Runtime.js';
+import './platform/utilities.js';
 import './common/common.js';
 import './host/host.js';
 import './ui/ui.js';
