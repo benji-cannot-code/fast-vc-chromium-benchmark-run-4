8d9ec329bca00cd8d70260ffe75165ee62145e55
Darren Shen <shend@chromium.org>
2019-10-02 08:31:32 +0000

    ime: Refactor request ID code.
    Add a new struct representing a pending key event as we'll be adding
    new data to the key event in a follow up patch.
    
    Change-Id: I87040818e619236e2e514173ce66db64e66a1177
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816197
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701939}

--- chrome/browser/extensions/api/input_ime/input_ime_api.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api.cc
@@ -81,7 +81,7 @@ void ImeObserver::OnBlur(int context_id) {
 void ImeObserver::OnKeyEvent(
     const std::string& component_id,
     const InputMethodEngineBase::KeyboardEvent& event,
-    IMEEngineHandlerInterface::KeyEventDoneCallback key_data) {
+    IMEEngineHandlerInterface::KeyEventDoneCallback callback) {
   if (extension_id_.empty())
     return;
 
@@ -90,7 +90,7 @@ void ImeObserver::OnKeyEvent(
   if (!ShouldForwardKeyEvent()) {
     // Continue processing the key event so that the physical keyboard can
     // still work.
-    std::move(key_data).Run(false);
+    std::move(callback).Run(false);
     return;
   }
 
@@ -100,7 +100,7 @@ void ImeObserver::OnKeyEvent(
     return;
   const std::string request_id =
       event_router->GetEngineIfActive(extension_id_)
-          ->AddRequest(component_id, std::move(key_data));
+          ->AddPendingKeyEvent(component_id, std::move(callback));
 
   input_ime::KeyboardEvent key_data_value;
   key_data_value.type = input_ime::ParseKeyboardEventType(event.type);
