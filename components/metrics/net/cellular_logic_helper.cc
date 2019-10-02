0aefa348e0f776b2b2e398b9ccde0ccc9785da58
Nate Fischer <ntfschr@chromium.org>
2019-10-02 19:56:09 +0000

    UMA: include missing headers in cellular_logic_helper
    No change to logic.
    
    This adds two header files which this C++ file was depending on
    transitively (for base::TimeDelta and OS_ANDROID/OS_IOS).
    
    Bug: 1010369
    Test: N/A
    Change-Id: I16ceb2d05beaca66ea254f1e1c7ddd541b8ff05c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1835132
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702155}

--- components/metrics/net/cellular_logic_helper.cc
+++ components/metrics/net/cellular_logic_helper.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/metrics/net/cellular_logic_helper.h"
 
+#include "base/time/time.h"
+#include "build/build_config.h"
 #include "net/base/network_change_notifier.h"
 
 namespace metrics {
