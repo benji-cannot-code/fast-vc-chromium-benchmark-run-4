099faf0478fe706911ca7fd0c3aef8bc8d7a24f2
Joshua Peraza <jperaza@chromium.org>
2019-10-02 18:51:34 +0000

    linux: enable monitor self and crashpad metrics
    Also, Crashpad for Android can't yet monitor itself in the various
    configurations that Chrome packages it, so remove references to the
    flag from crashpad_android.cc to avoid accidentally turning it on.
    
    Bug: 942279
    Change-Id: I1afae5fd433f9e2b1475e8fa3e883547542eb6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834401
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702128}

--- chrome/app/chrome_crash_reporter_client_mac.mm
+++ chrome/app/chrome_crash_reporter_client_mac.mm
@@ -16,11 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/policy_constants.h"
 #include "components/version_info/version_info.h"
 
-bool ChromeCrashReporterClient::GetCrashMetricsLocation(
-    base::FilePath* metrics_dir) {
-  return base::PathService::Get(chrome::DIR_USER_DATA, metrics_dir);
-}
-
 bool ChromeCrashReporterClient::ReportingIsEnforcedByPolicy(
     bool* breakpad_enabled) {
   base::ScopedCFTypeRef<CFStringRef> key(
