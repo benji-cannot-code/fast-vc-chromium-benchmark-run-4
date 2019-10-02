afca0e695d567a81f976faebb0a15f35dd10f85c
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-10-02 21:06:57 +0000

    Remove testing binder on tear down in some Blink unit tests.
    Follow-up to commits 4ba0b61c22e ("Convert ShareService client to use
    BrowserInterfaceBroker"), 1dec3c1c2 ("Convert WakeLockService client to use
    BrowserInterfaceBroker") and e676be1fe3 ("Convert PermissionService client
    to use BrowserInterfaceBroker").
    
    Essentially, the BrowserInterfaceBroker conversion introduced flakiness to
    some unit tests, and this CL fixes the ones I have noticed. The flakiness is
    caused by the fact that we after the first test case we end up trying to
    rebind an already bound object, which often leads to DCHECKs and other
    crashes.
    
    Follow what is done in commits 567040bd ("[mojo] Convert
    CredentialManagerProxy to use BrowserInterfaceBroker") and
    b5630d76 ("Convert NFC client to use BrowserInterfaceBroker") and explicitly
    remove the test binders between test cases.
    
    Bug: 936482, 1010116
    Change-Id: If31d8f3dfb95c69651265a6e8cdcebdfb3816728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1835360
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#702183}

--- third_party/blink/renderer/modules/wake_lock/wake_lock_test_utils.h
+++ third_party/blink/renderer/modules/wake_lock/wake_lock_test_utils.h
@@ -151,6 +151,7 @@ class WakeLockTestingContext final {
 
  public:
   WakeLockTestingContext(MockWakeLockService* mock_wake_lock_service);
+  ~WakeLockTestingContext();
 
   Document* GetDocument();
   LocalFrame* Frame();
