f89012f1dfdfbc1a1b40ec30a8b5f08d9809ac0d
Gyuyoung Kim <gyuyoung@igalia.com>
2019-10-02 03:04:48 +0000

    Migrate XRRuntime::ListenToDeviceChanges to new Mojo types
    This CL applies pending_associated_remote to ListenToDeviceChanges()
    in XRRuntime interface.
    
     - Covert InterfaceAssociatedPtr to mojo::AssociatedRemote<>
     - Convert InterfaceAssociatedPtrInfo to mojo::PendingAssociatedRemote<>
     - Rename BindPtrInfo() to BindPendingRemote()
    
    Bug: 955171
    Change-Id: I63bba4db383fa9f3a46987fa04fc0afa381abf18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1831657
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#701881}

--- device/vr/vr_device_base.h
+++ device/vr/vr_device_base.h
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/public/mojom/vr_service.mojom.h"
 #include "device/vr/vr_device.h"
 #include "device/vr/vr_export.h"
+#include "mojo/public/cpp/bindings/associated_remote.h"
 #include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/pending_associated_remote.h"
 #include "ui/display/display.h"
 
 namespace device {
@@ -29,7 +31,7 @@ class DEVICE_VR_EXPORT VRDeviceBase : public mojom::XRRuntime {
 
   // VRDevice Implementation
   void ListenToDeviceChanges(
-      mojom::XRRuntimeEventListenerAssociatedPtrInfo listener,
+      mojo::PendingAssociatedRemote<mojom::XRRuntimeEventListener> listener,
       mojom::XRRuntime::ListenToDeviceChangesCallback callback) final;
   void SetListeningForActivate(bool is_listening) override;
   void EnsureInitialized(EnsureInitializedCallback callback) override;
@@ -76,7 +78,7 @@ class DEVICE_VR_EXPORT VRDeviceBase : public mojom::XRRuntime {
   // TODO(https://crbug.com/842227): Rename methods to HandleOnXXX
   virtual void OnListeningForActivate(bool listening);
 
-  mojom::XRRuntimeEventListenerAssociatedPtr listener_;
+  mojo::AssociatedRemote<mojom::XRRuntimeEventListener> listener_;
 
   bool presenting_ = false;
 
--- device/vr/vr_device_base_unittest.cc
+++ device/vr/vr_device_base_unittest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/public/mojom/vr_service.mojom.h"
 #include "device/vr/test/fake_vr_device.h"
 #include "device/vr/vr_device_base.h"
-#include "mojo/public/cpp/bindings/associated_binding.h"
+#include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -53,8 +53,8 @@ class VRDeviceBaseForTesting : public VRDeviceBase {
 
 class StubVRDeviceEventListener : public mojom::XRRuntimeEventListener {
  public:
-  StubVRDeviceEventListener() : binding_(this) {}
-  ~StubVRDeviceEventListener() override {}
+  StubVRDeviceEventListener() = default;
+  ~StubVRDeviceEventListener() override = default;
 
   MOCK_METHOD1(DoOnChanged, void(mojom::VRDisplayInfo* vr_device_info));
   void OnDisplayInfoChanged(mojom::VRDisplayInfoPtr vr_device_info) override {
@@ -76,13 +76,12 @@ class StubVRDeviceEventListener : public mojom::XRRuntimeEventListener {
   MOCK_METHOD1(OnDeviceIdle, void(mojom::VRDisplayEventReason));
   MOCK_METHOD0(OnInitialized, void());
 
-  mojom::XRRuntimeEventListenerAssociatedPtrInfo BindPtrInfo() {
-    mojom::XRRuntimeEventListenerAssociatedPtrInfo ret;
-    binding_.Bind(mojo::MakeRequest(&ret));
-    return ret;
+  mojo::PendingAssociatedRemote<mojom::XRRuntimeEventListener>
+  BindPendingRemote() {
+    return receiver_.BindNewEndpointAndPassRemote();
   }
 
-  mojo::AssociatedBinding<mojom::XRRuntimeEventListener> binding_;
+  mojo::AssociatedReceiver<mojom::XRRuntimeEventListener> receiver_{this};
 };
 
 }  // namespace
@@ -120,7 +119,7 @@ TEST_F(VRDeviceTest, DeviceChangedDispatched) {
   auto device_ptr = device->BindXRRuntimePtr();
   StubVRDeviceEventListener listener;
   device_ptr->ListenToDeviceChanges(
-      listener.BindPtrInfo(),
+      listener.BindPendingRemote(),
       base::DoNothing());  // TODO: consider getting initial info
   base::RunLoop().RunUntilIdle();
   EXPECT_CALL(listener, DoOnChanged(testing::_)).Times(1);
@@ -135,7 +134,7 @@ TEST_F(VRDeviceTest, DisplayActivateRegsitered) {
   auto device_ptr = device->BindXRRuntimePtr();
   StubVRDeviceEventListener listener;
   device_ptr->ListenToDeviceChanges(
-      listener.BindPtrInfo(),
+      listener.BindPendingRemote(),
       base::DoNothing());  // TODO: consider getting initial data
   base::RunLoop().RunUntilIdle();
 
