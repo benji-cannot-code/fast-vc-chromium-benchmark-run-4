3f7cd5e9e25bd6a47df274a4e832c27c2475035c
Dominic Battré <battre@chromium.org>
2019-10-02 07:26:29 +0000

    Revert "Disable 3 tests in encrypted_media_browsertest.cc"
    This reverts commit c53bd714d30b8bd85a973edd2849c2eef1e9723d.
    
    Reason for revert: Disabling is non-actionable, see crbug.com/1010061#c11
    
    Original change's description:
    > Disable 3 tests in encrypted_media_browsertest.cc
    > 
    > TBR=hbos@chromium.org
    > NOTRY=true
    > 
    > Bug: 1010061
    > Change-Id: Iac97955e581bfbe5091c7cab8ca453fdd63c1c7e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832693
    > Reviewed-by: Xida Chen <xidachen@chromium.org>
    > Commit-Queue: Xida Chen <xidachen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#701584}
    
    TBR=hbos@chromium.org,xidachen@chromium.org
    
    Change-Id: Ibf089b59ecebccdabe63c1169be9ca572f2f2301
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1010061
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1835353
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701917}

--- content/browser/media/encrypted_media_browsertest.cc
+++ content/browser/media/encrypted_media_browsertest.cc
@@ -207,8 +207,7 @@ IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_AudioClearVideo_WebM) {
   TestSimplePlayback("bear-320x240-av_enc-a.webm");
 }
 
-// crbug.com/1010061: disabled due to flakiness.
-IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, DISABLED_Playback_VideoAudio_WebM) {
+IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoAudio_WebM) {
   TestSimplePlayback("bear-320x240-av_enc-av.webm");
 }
 
@@ -228,9 +227,7 @@ IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoClearAudio_WebM) {
   TestSimplePlayback("bear-320x240-av_enc-v.webm");
 }
 
-// crbug.com/1010061: disabled due to flakiness.
-IN_PROC_BROWSER_TEST_P(EncryptedMediaTest,
-                       DISABLED_Playback_AudioOnly_WebM_Opus) {
+IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_AudioOnly_WebM_Opus) {
 #if defined(OS_ANDROID)
   if (!media::MediaCodecUtil::IsOpusDecoderAvailable())
     return;
@@ -366,8 +363,7 @@ IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_Encryption_CENS) {
                       media::kError);
 }
 
-// crbug.com/1010061: disabled due to flakiness.
-IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, DISABLED_Playback_Encryption_CBCS) {
+IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_Encryption_CBCS) {
   std::string expected_result =
       BUILDFLAG(ENABLE_CBCS_ENCRYPTION_SCHEME) ? media::kEnded : media::kError;
   RunMultipleFileTest("bear-640x360-v_frag-cbcs.mp4",
