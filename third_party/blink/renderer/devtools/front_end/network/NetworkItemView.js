fc9b760a60a6d976da16395f0534955782847521
Junyi Xiao <juxiao@microsoft.com>
2019-10-02 18:43:08 +0000

    Devtools: New "Initiator" tab in Network Item view
    Issue: In the network tool, there is a pop up for stack traces that appears
    when you hover over the initiator column. This info is not available
    anywhere else, and is not keyboard accessible.
    
    This changes: add a new tab to the network item view, similar to the
    Timings tab and Waterfall column (which is available when hovering over the
    waterfall, but also accessible when selecting a network request to view).
    
    Bug: 963183, 842488
    
    workflow gif: https://imgur.com/a/wmdNYXE
    
    Change-Id: Ib2f23b924a06e687acd362ccf4d6764f8a996559
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1742990
    Commit-Queue: Junyi Xiao <juxiao@microsoft.com>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702125}

--- third_party/blink/renderer/devtools/front_end/network/NetworkItemView.js
+++ third_party/blink/renderer/devtools/front_end/network/NetworkItemView.js
@@ -80,6 +80,8 @@ Network.NetworkItemView = class extends UI.TabbedPane {
 
     /** @type {?Network.RequestCookiesView} */
     this._cookiesView = null;
+    /** @type {?Network.RequestInitiatorView} */
+    this._initiatorView = null;
   }
 
   /**
@@ -92,6 +94,7 @@ Network.NetworkItemView = class extends UI.TabbedPane {
     this._request.addEventListener(
         SDK.NetworkRequest.Events.ResponseHeadersChanged, this._maybeAppendCookiesPanel, this);
     this._maybeAppendCookiesPanel();
+    this._maybeAppendInitiatorPanel();
     this._selectTab();
   }
 
@@ -113,6 +116,15 @@ Network.NetworkItemView = class extends UI.TabbedPane {
       this.appendTab(
           Network.NetworkItemView.Tabs.Cookies, Common.UIString('Cookies'), this._cookiesView,
           Common.UIString('Request and response cookies'));
+    }
+  }
+
+  _maybeAppendInitiatorPanel() {
+    const initiator = this._request.initiator();
+    if (initiator && initiator.stack && !this._initiatorView) {
+      this._initiatorView = new Network.RequestInitiatorView(this._request);
+      this.appendTab(
+          Network.NetworkItemView.Tabs.Initiator, ls`Initiator`, this._initiatorView, ls`Request initiator call stack`);
     }
   }
 
@@ -178,6 +190,7 @@ Network.NetworkItemView.Tabs = {
   Cookies: 'cookies',
   EventSource: 'eventSource',
   Headers: 'headers',
+  Initiator: 'initiator',
   Preview: 'preview',
   Response: 'response',
   Timing: 'timing',
