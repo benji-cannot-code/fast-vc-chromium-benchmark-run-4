97f15adeafa12de599f3e4a3e992980deeccaad9
Sylvain Defresne <sdefresne@chromium.org>
2019-10-02 09:15:59 +0000

    Convert components/favicon away from base::Bind/base::Callback
    base::Bind/base::Callback are deprecated in favor of either
    base::BindOnce/base::OnceCallback or base::BindRepeating/
    base::RepeatingCallback (depending on whether the callback
    is invoked once or multiple time).
    
    Convert all uses of base::Bind/base::Callback in components/favicon
    to the recommended methods/types.
    
    Bug: 1007688
    Change-Id: I2554067448ac8ac8d27776a7c0e9d1977568cd57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832809
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701950}

--- components/favicon/ios/web_favicon_driver.mm
+++ components/favicon/ios/web_favicon_driver.mm
@@ -24,14 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-// Callback for the download of favicon.
-using ImageDownloadCallback =
-    base::Callback<void(int image_id,
-                        int http_status_code,
-                        const GURL& image_url,
-                        const std::vector<SkBitmap>& bitmaps,
-                        const std::vector<gfx::Size>& sizes)>;
-
 namespace favicon {
 
 // static
