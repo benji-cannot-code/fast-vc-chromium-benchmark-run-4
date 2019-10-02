2d1c351b53e18e3034076f99b695eed4ec8265bd
Lei Zhang <thestig@chromium.org>
2019-10-02 17:52:04 +0000

    Implement base::SHA1HashSpan().
    Hopefully this can replace base::SHA1HashBytes() in the long run.
    
    Change-Id: Ie9138b569d59c8b252fa14262a56ced56f74a397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827647
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702097}

--- base/hash/sha1_boringssl.cc
+++ base/hash/sha1_boringssl.cc
@@ -13,17 +13,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-void SHA1HashBytes(const unsigned char* data, size_t len, unsigned char* hash) {
+std::array<uint8_t, kSHA1Length> SHA1HashSpan(span<const uint8_t> data) {
   CRYPTO_library_init();
-  SHA1(data, len, hash);
+  std::array<uint8_t, kSHA1Length> digest;
+  SHA1(data.data(), data.size(), digest.data());
+  return digest;
 }
 
 std::string SHA1HashString(const std::string& str) {
   CRYPTO_library_init();
   std::string digest;
   SHA1(reinterpret_cast<const uint8_t*>(str.data()), str.size(),
-       reinterpret_cast<uint8_t*>(base::WriteInto(&digest, kSHA1Length + 1)));
+       reinterpret_cast<uint8_t*>(WriteInto(&digest, kSHA1Length + 1)));
   return digest;
+}
+
+void SHA1HashBytes(const unsigned char* data, size_t len, unsigned char* hash) {
+  CRYPTO_library_init();
+  SHA1(data, len, hash);
 }
 
 }  // namespace base
