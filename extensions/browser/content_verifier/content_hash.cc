5ccf26869cb6e886b0565cd430f4b83291a60e9c
Oleg Davydov <burunduk@chromium.org>
2019-10-02 12:22:38 +0000

    Replace ContentHash::status_ with a simple flag.
    ContentHash::Status enum has three values assuming that having valid
    verified_contents.json is mandatory intermediate step on a way to get
    full ContentHash. We can simplify code a bit by removing this
    intermediate stage from enum (and replacing remaining two-values enum
    with just a bool). Also this will simply things a little for self-hosted
    extensions (which don't have verified_contents.json, but still have to
    be checked for corruptions).
    
    Bug: 796395
    Bug: 958794
    Change-Id: Iacbd7a90853b0e30d611c7f0e90eb2d53aea989d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810899
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701986}

--- extensions/browser/content_verifier/content_hash.cc
+++ extensions/browser/content_verifier/content_hash.cc
@@ -122,7 +122,7 @@ void ContentHash::ForceBuildComputedHashes(
 ContentHash::TreeHashVerificationResult ContentHash::VerifyTreeHashRoot(
     const base::FilePath& relative_path,
     const std::string* root) const {
-  DCHECK(status_ >= Status::kHasVerifiedContents && verified_contents_);
+  DCHECK(verified_contents_);
   if (!verified_contents_->HasTreeHashRoot(relative_path))
     return TreeHashVerificationResult::NO_ENTRY;
 
@@ -133,7 +133,7 @@ ContentHash::TreeHashVerificationResult ContentHash::VerifyTreeHashRoot(
 }
 
 const ComputedHashes::Reader& ContentHash::computed_hashes() const {
-  DCHECK(status_ == Status::kSucceeded && computed_hashes_);
+  DCHECK(succeeded_ && computed_hashes_);
   return *computed_hashes_;
 }
 
@@ -146,12 +146,7 @@ ContentHash::ContentHash(
       extension_root_(root),
       verified_contents_(std::move(verified_contents)),
       computed_hashes_(std::move(computed_hashes)) {
-  if (!verified_contents_)
-    status_ = Status::kInvalid;
-  else if (!computed_hashes_)
-    status_ = Status::kHasVerifiedContents;
-  else
-    status_ = Status::kSucceeded;
+  succeeded_ = verified_contents_ != nullptr && computed_hashes_ != nullptr;
 }
 
 ContentHash::~ContentHash() = default;
@@ -310,7 +305,7 @@ bool ContentHash::CreateHashes(const base::FilePath& hashes_file,
                       timer.Elapsed());
 
   if (result)
-    status_ = Status::kSucceeded;
+    succeeded_ = true;
 
   return result;
 }
@@ -345,7 +340,7 @@ void ContentHash::BuildComputedHashes(bool attempted_fetching_verified_contents,
       // will_create = true;
     } else {
       // Read successful.
-      status_ = Status::kSucceeded;
+      succeeded_ = true;
       computed_hashes_ = std::move(computed_hashes);
       return;
     }
@@ -365,7 +360,7 @@ void ContentHash::BuildComputedHashes(bool attempted_fetching_verified_contents,
     return;
 
   // Read successful.
-  status_ = Status::kSucceeded;
+  succeeded_ = true;
   computed_hashes_ = std::move(computed_hashes);
 }
 
