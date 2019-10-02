ba952b13acef333de835e20d7bfd1ccf1b7e97d9
James Cook <jamescook@chromium.org>
2019-10-02 14:54:06 +0000

    settings: Focus sync passphrase field after entering wrong passphrase
    The user is likely to try re-entering their passphrase, so ensure
    focus stays in the input field.
    
    Convert the Print Preview PIN settings element to use the same
    approach to focus, so callers aren't depending on internal
    implementation details of cr-input.
    
    Bug: 805829
    Test: See bug. sign in to account with sync passphrase, enter
      incorrect phrase, focus returns to input field. Tested with both
      Chrome OS and Linux.
    
    Change-Id: I012f5b5368654c8b0f36b66f9fd07124d394c9a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832632
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702032}

--- ui/webui/resources/cr_elements/cr_input/cr_input.js
+++ ui/webui/resources/cr_elements/cr_input/cr_input.js
@@ -226,7 +226,7 @@ Polymer({
 
   /** @private */
   onFocus_: function() {
-    if (!this.focusInput_()) {
+    if (!this.focusInput()) {
       return;
     }
     // Always select the <input> element on focus. TODO(stevenjb/scottchen):
@@ -236,10 +236,12 @@ Polymer({
   },
 
   /**
+   * Focuses the input element.
+   * TODO(crbug.com/882612): Replace this with focus() after resolving the text
+   * selection issue described in onFocus_().
    * @return {boolean} Whether the <input> element was focused.
-   * @private
    */
-  focusInput_: function() {
+  focusInput: function() {
     if (this.shadowRoot.activeElement == this.inputElement) {
       return false;
     }
@@ -347,7 +349,7 @@ Polymer({
    * @param {number=} end
    */
   select: function(start, end) {
-    this.focusInput_();
+    this.focusInput();
     if (start !== undefined && end !== undefined) {
       this.inputElement.setSelectionRange(start, end);
     } else {
