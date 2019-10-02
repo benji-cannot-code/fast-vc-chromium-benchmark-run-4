0352fbb0018b0db8634fcc9dc6dedb3739d89d9b
Eric Lawrence <ericlaw@microsoft.com>
2019-10-02 00:36:27 +0000

    Read Product Version correctly in mojo traits
    The chrome://conflicts page was not correctly showing the product
    version in the Version column. The root cause was that the accessor
    was incorrectly reading the |basename| instead of the |version|.
    
    Bug: 996766
    Change-Id: I158ca52ff201ebb0c67856ae806c9cff2712c840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832530
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#701843}

--- chrome/services/util_win/public/mojom/util_win_mojom_traits.cc
+++ chrome/services/util_win/public/mojom/util_win_mojom_traits.cc
@@ -132,7 +132,7 @@ const base::string16& StructTraits<
 const base::string16& StructTraits<
     chrome::mojom::InspectionResultDataView,
     ModuleInspectionResult>::version(const ModuleInspectionResult& input) {
-  return input.basename;
+  return input.version;
 }
 // static
 chrome::mojom::CertificateType
