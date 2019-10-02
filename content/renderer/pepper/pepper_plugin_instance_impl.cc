8a90e57fddda76fc1a4a584f2d47a15e194565bb
Tommy Li <tommycli@chromium.org>
2019-10-02 21:32:10 +0000

    [flash] Remove Pepper plugin size metric logging
    No one is monitoring these metrics.
    
    Bug: 974982
    Change-Id: I81676fd22bd354391f2acd38385152d90b4e8669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1830362
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702194}

--- content/renderer/pepper/pepper_plugin_instance_impl.cc
+++ content/renderer/pepper/pepper_plugin_instance_impl.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/pepper/pepper_file_ref_renderer_host.h"
 #include "content/renderer/pepper/pepper_graphics_2d_host.h"
 #include "content/renderer/pepper/pepper_in_process_router.h"
-#include "content/renderer/pepper/pepper_plugin_instance_metrics.h"
 #include "content/renderer/pepper/pepper_try_catch.h"
 #include "content/renderer/pepper/pepper_url_loader_host.h"
 #include "content/renderer/pepper/plugin_instance_throttler_impl.h"
@@ -1134,10 +1133,6 @@ bool PepperPluginInstanceImpl::HandleInputEvent(
       event.GetType() == blink::WebInputEvent::kMouseDown &&
       (event.GetModifiers() & blink::WebInputEvent::kLeftButtonDown)) {
     has_been_clicked_ = true;
-    blink::WebRect bounds = container()->GetElement().BoundsInViewport();
-    render_frame()->GetLocalRootRenderWidget()->ConvertViewportToWindow(
-        &bounds);
-    RecordFlashClickSizeMetric(bounds.width, bounds.height);
   }
 
   if (throttler_ && throttler_->ConsumeInputEvent(event))
@@ -1845,16 +1840,9 @@ void PepperPluginInstanceImpl::SendDidChangeView() {
       viewport_to_dip_scale_);
 
   // During the first view update, initialize the throttler.
-  if (!sent_initial_did_change_view_) {
-    if (is_flash_plugin_ && RenderThread::Get()) {
-      RecordFlashSizeMetric(unobscured_rect_.width(),
-                            unobscured_rect_.height());
-    }
-
-    if (throttler_) {
-      throttler_->Initialize(render_frame_, url::Origin::Create(plugin_url_),
-                             module()->name(), unobscured_rect_.size());
-    }
+  if (!sent_initial_did_change_view_ && throttler_) {
+    throttler_->Initialize(render_frame_, url::Origin::Create(plugin_url_),
+                           module()->name(), unobscured_rect_.size());
   }
 
   ppapi::ViewData view_data = view_data_;
