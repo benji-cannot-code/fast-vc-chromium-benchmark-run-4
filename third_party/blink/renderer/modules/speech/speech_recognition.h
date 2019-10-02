120fee6c6744c5a9d33adafd141fca056a91fe52
Dave Tapuska <dtapuska@chromium.org>
2019-10-02 09:09:56 +0000

    Change Speech Recognition to use a normal Remote instead of a RevocableBinding.
    RevocableBinding is going to go away. Change to use a direct Receiver
    interface.
    
    BUG=978694
    
    Change-Id: Ib936e500b8e203ac1cbbe739867ff671bb1f22fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834074
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Mario Sanchez Prada <mario@igalia.com>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#701946}

--- third_party/blink/renderer/modules/speech/speech_recognition.h
+++ third_party/blink/renderer/modules/speech/speech_recognition.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_SPEECH_SPEECH_RECOGNITION_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_SPEECH_SPEECH_RECOGNITION_H_
 
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/mojom/speech/speech_recognizer.mojom-blink.h"
 #include "third_party/blink/public/platform/web_private_ptr.h"
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
@@ -37,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/speech/speech_grammar_list.h"
 #include "third_party/blink/renderer/modules/speech/speech_recognition_result.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
-#include "third_party/blink/renderer/platform/mojo/revocable_binding.h"
 #include "third_party/blink/renderer/platform/mojo/revocable_interface_ptr.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -136,7 +136,7 @@ class MODULES_EXPORT SpeechRecognition final
   bool started_;
   bool stopping_;
   HeapVector<Member<SpeechRecognitionResult>> final_results_;
-  RevocableBinding<mojom::blink::SpeechRecognitionSessionClient> binding_;
+  mojo::Receiver<mojom::blink::SpeechRecognitionSessionClient> receiver_;
   RevocableInterfacePtr<mojom::blink::SpeechRecognitionSession> session_;
 };
 
