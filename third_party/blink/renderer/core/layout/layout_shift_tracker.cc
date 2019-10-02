a42b035d4277fd3b68ffceb46a866f55a10e293b
Steve Kobes <skobes@chromium.org>
2019-10-02 00:53:13 +0000

    Clean up LayoutInstabilityAPI REF.
    Bug: 581518
    Change-Id: Ibd6a02dadcc9d90d26d20fb827d2c5bdfb52b3d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1831948
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701846}

--- third_party/blink/renderer/core/layout/layout_shift_tracker.cc
+++ third_party/blink/renderer/core/layout/layout_shift_tracker.cc
@@ -304,9 +304,7 @@ void LayoutShiftTracker::ReportShift(double score_delta,
     }
   }
 
-  if (RuntimeEnabledFeatures::LayoutInstabilityAPIEnabled(
-          frame.GetDocument()) &&
-      frame.DomWindow()) {
+  if (frame.DomWindow()) {
     WindowPerformance* performance =
         DOMWindowPerformance::performance(*frame.DomWindow());
     if (performance) {
