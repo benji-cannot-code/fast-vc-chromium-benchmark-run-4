d0a9be4cc4348fab10c8be76d0af791b9a582e7e
Tommy Li <tommycli@chromium.org>
2019-10-02 21:47:13 +0000

    [omnibox] Remove context menu for suggestions in OmniboxResultView
    We're proceeding with a button-based remove interface instead of the
    context menu.
    
    We may add this back later for some other reason, but we don't need it
    currently, so this CL rips it out.
    
    Bug: 1205, 929477
    Change-Id: I87a6b9b283e6d16656fe263c5271f51192d74adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834895
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702202}

--- chrome/browser/ui/views/omnibox/remove_suggestion_bubble.h
+++ chrome/browser/ui/views/omnibox/remove_suggestion_bubble.h
@@ -23,8 +23,4 @@ void ShowRemoveSuggestion(TemplateURLService* template_url_service,
                           const AutocompleteMatch& match,
                           base::OnceClosure remove_closure);
 
-// Shows a simple "Why this suggestion" info bubble for |match|.
-void ShowWhyThisSuggestion(views::View* anchor_view,
-                           const AutocompleteMatch& match);
-
 #endif  // CHROME_BROWSER_UI_VIEWS_OMNIBOX_REMOVE_SUGGESTION_BUBBLE_H_
