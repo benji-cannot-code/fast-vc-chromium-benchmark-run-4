e406120bcc2ed6bb66ec3e346dc985c0d48786ae
Robert Ogden <robertogden@chromium.org>
2019-10-02 21:09:44 +0000

    Add count of CSS and JS resources loaded before FCP to PLM
    Adds a new PLMObserver, SubresourceLoading, to track these metrics.
    
    Bug: 995437
    Change-Id: I049d96eafd93b089b06131fb8ad35eb30d0c3a6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829881
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702187}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/security_state_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/service_worker_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/signed_exchange_page_load_metrics_observer.h"
+#include "chrome/browser/page_load_metrics/observers/subresource_loading_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/tab_restore_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/third_party_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.h"
@@ -118,6 +119,8 @@ void PageLoadMetricsEmbedder::RegisterEmbedderObservers(
             previews::OfflinePagePreviewsPageLoadMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<OptimizationGuidePageLoadMetricsObserver>());
+    tracker->AddObserver(
+        std::make_unique<SubresourceLoadingPageLoadMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<PreviewsLitePageRedirectMetricsObserver>());
     tracker->AddObserver(
