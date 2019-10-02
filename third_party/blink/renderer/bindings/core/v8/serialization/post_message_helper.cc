ecbfdbd9e5cdfc3aa4194af77cc33decbc4d34bf
Gyuyoung Kim <gyuyoung@igalia.com>
2019-10-02 07:53:56 +0000

    Include *.mojom-blink-forward.h instead of .mojom-blink.h from post_message_helper.h
    To reduce pre-processed size of post_message_helper.h, this CL replaces
    .mojom.h with .mojom-forward.h.
    
    This CL has no behavior changes.
    
    Bug: 1001360
    Change-Id: I12a338fc3d62f85e032f0ed6f845d3fa098017c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1833352
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#701928}

--- third_party/blink/renderer/bindings/core/v8/serialization/post_message_helper.cc
+++ third_party/blink/renderer/bindings/core/v8/serialization/post_message_helper.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/bindings/core/v8/serialization/post_message_helper.h"
 
+#include "third_party/blink/public/mojom/messaging/user_activation_snapshot.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/frame.h"
