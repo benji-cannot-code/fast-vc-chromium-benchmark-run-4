9cfc39cac5987ffc0b4889ee3853e9b5e9b54987
Lukasz Anforowicz <lukasza@chromium.org>
2019-10-02 20:41:13 +0000

    Remove ContentBrowserClient::ShouldAllowOpenURL.
    After recent CLs (r699929, r700322), there are no more overrides of
    ContentBrowserClient::ShouldAllowOpenURL.  This CL takes advantage of
    that, and removes the ContentBrowserClient::ShouldAllowOpenURL method
    entirely.  Removing callers of this method is straightforward - we just
    observe that the default implementation of the method always returns
    true.
    
    The CL is careful to preserve test coverage in tests that used to rely
    on ShouldAllowOpenURL overrides:
    
    1. After the CL, the TestContentBrowserClient in
       //chrome/browser/prerender/prerender_browsertest.cc still "cancels
       all prerenders on OpenURL" (doing this from an overload of
       OverrideNavigationParams instead of from an overload of
       ShouldAllowOpenURL;  OverrideNavigationParams is called from
       NavigatorImpl::RequestOpenURL exactly once during the test).
    
    2. The CL refactors and moves service worker tests as follows:
       2a. ServiceWorkerClientsApiBrowserTest.NavigateDisallowedUrl
           moves from //content to //chrome layer and becomes
           ExtensionResourceRequestPolicyTest.
           WebNavigationToNonWebAccessibleResource_ViaServiceWorkerNavigate
       2b. ServiceWorkerClientsApiBrowserTest.OpenWindowDisallowedUrl
           moves from //content to //chrome layer and becomes
           ExtensionResourceRequestPolicyTest.
           WebNavigationToNonWebAccessibleResource_ViaServiceWorkerOpenWindow
       2c. ServiceWorkerClientsApiBrowserTest.OpenWindow is refactored to
           use exiting testing helpers which are more detached from
           implementation details (using OpenURL is an example of an
           implementation detail) and more focused on end-to-end
           verification (e.g. that a new window is actually opened;  not all
           calls to OpenURL end up opening a new window).
    2a and 2b preserves the essence of the existing test coverage, since
    ShouldAllowOpenURL was mostly replaced by ExtensionNavigationThrottle -
    see r699929.
    
    To support the move in #2, the CL performs the following additional
    changes:
    
    - Moving the code to simulate dispatching of a service worker
      notification click from ServiceWorkerClientsApiBrowserTest to
      DispatchServiceWorkerNotificationClick - a standalone helper function
      in //content/public/test/service_worker_test_helpers.cc
    
    - Tweaking client_api_worker.js to support calling openWindow with
      a custom URL, provided by a test via notification body.
    
    Bug: 1003957
    Change-Id: I486d153ca03a10a30a39204cfe6dc88e726f854d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829539
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702175}

--- chrome/test/data/service_worker/client_api_worker.js
+++ chrome/test/data/service_worker/client_api_worker.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,4 +17,3 @@ self.addEventListener('message', async (event) => {
 self.addEventListener('notificationclick', event => {
   clients.openWindow(event.notification.body);
 });
-
