832db76e85a759d33fc44d15847c0da09c67c4e3
Ken Rockot <rockot@chromium.org>
2019-10-02 06:48:28 +0000

    [mojo] Fix remote process disconnection raciness
    Currently if a remote process is detected as disconnected (i.e. your
    internal Channel transport to that process signals an error), unread
    messages which have already arrived from that process may be unreadable
    from the ports that received them.
    
    Bug: 1005510
    Change-Id: Id743ce4560a0309a2058d0b825a2bb4a6065b342
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832244
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Darin Fisher <darin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#701907}

--- mojo/core/ports/node.cc
+++ mojo/core/ports/node.cc
@@ -88,6 +88,8 @@ bool CanAcceptMoreMessages(const Port* port) {
   if (port->state == Port::kClosed)
     return false;
   if (port->peer_closed || port->remove_proxy_on_last_message) {
+    if (port->peer_lost_unexpectedly)
+      return port->message_queue.HasNextMessage();
     if (port->last_sequence_num_to_receive == next_sequence_num - 1)
       return false;
   }
@@ -1539,9 +1541,7 @@ void Node::DestroyAllPortsWithPeer(const NodeName& node_name,
           // messages.
 
           port->peer_closed = true;
-          port->last_sequence_num_to_receive =
-              port->message_queue.next_sequence_num() - 1;
-
+          port->peer_lost_unexpectedly = true;
           if (port->state == Port::kReceiving)
             ports_to_notify.push_back(local_port_ref);
         }
