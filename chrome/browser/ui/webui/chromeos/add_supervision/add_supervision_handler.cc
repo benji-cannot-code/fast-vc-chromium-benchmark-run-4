a2d42331c8ea8b9941b688b4486896f64ff30c46
Danan S <danan@chromium.org>
2019-10-03 00:50:54 +0000

    Check for active ArcSessionManager in GetInstalledArcApps
    This fixes an AppService triggered ARC++ crash in M78 which appears
    to be resolved in M79, but going forward it's a guard against
    things that might cause the crash to occur again in the future.
    
    Bug: 1010600
    Change-Id: I80fa47a2686b29c073b45c269b965fe8e6d2ece4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1836606
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Dan S <danan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702263}

--- chrome/browser/ui/webui/chromeos/add_supervision/add_supervision_handler.cc
+++ chrome/browser/ui/webui/chromeos/add_supervision/add_supervision_handler.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
+#include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/supervised_user/supervised_user_service.h"
@@ -55,6 +56,18 @@ void AddSupervisionHandler::GetInstalledArcApps(
   Profile* profile = Profile::FromWebUI(web_ui_);
   apps::AppServiceProxy* proxy =
       apps::AppServiceProxyFactory::GetForProfile(profile);
+
+  if (arc::ArcSessionManager::Get() == nullptr) {
+    DLOG(WARNING) << "No ArcSessionManager available";
+    std::move(callback).Run({});
+    return;
+  }
+  if (arc::ArcSessionManager::Get()->state() !=
+      arc::ArcSessionManager::State::ACTIVE) {
+    DLOG(WARNING) << "ArcSessionManager is not active";
+    std::move(callback).Run({});
+    return;
+  }
 
   std::vector<std::string> installed_arc_apps;
 
