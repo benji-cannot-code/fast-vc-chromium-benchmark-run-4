8df46efa7e18e50618e6abcabb966d08708f23d7
Kevin Bailey <krb@chromium.org>
2019-10-03 18:12:37 +0000

    [omnibox] Don't generate document suggestions in explicit keyword mode
    When keyword mode has been explicitly entered, and experimental keyword
    mode has been enabled, we don't want document suggestions (unless its
    search provider matches the keyword provider). Without this restriction,
    these suggestions tend to show up when we're only expecting keyword
    suggestions, since they rank highly.
    
    Bug: 837395, 883901
    Change-Id: If821f0efa8e3ffb6b05555ec6455849dc1d7f059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825779
    Commit-Queue: Kevin Bailey <krb@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702485}

--- components/omnibox/browser/search_provider.cc
+++ components/omnibox/browser/search_provider.cc
@@ -1385,11 +1385,12 @@ int SearchProvider::GetVerbatimRelevance(bool* relevance_from_server) const {
 bool SearchProvider::ShouldCurbDefaultSuggestions() const {
   // Only curb if the global experimental keyword feature is enabled, we're
   // in keyword mode and we believe the user selected the mode explicitly.
-  if (providers_.has_keyword_provider())
+  if (providers_.has_keyword_provider()) {
     return InExplicitExperimentalKeywordMode(input_,
                                              providers_.keyword_provider());
-  else
+  } else {
     return false;
+  }
 }
 
 int SearchProvider::CalculateRelevanceForVerbatim() const {
