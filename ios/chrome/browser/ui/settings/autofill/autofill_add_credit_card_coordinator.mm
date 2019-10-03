e5ad096a2fd0a407277458ac06877abf42265fd6
Gauthier Ambard <gambard@chromium.org>
2019-10-03 14:46:19 +0000

    [iOS] Fix style presentation for settings
    This CL fixes the presentation style of the settings.
    The presentation style isn't taken into account if it is set after the
    delegate of the presentationController of the ViewController.
    For the SettingsNavigationController, -viewDidLoad is called before the
    -init is finished, leading the presentation style set in the init to
    be ignored.
    
    Bug: 1010903
    Change-Id: Ia09a0bb1e57ed69899fd27de6f73e4405e3fbba6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1837691
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702408}

--- ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_coordinator.mm
+++ ios/chrome/browser/ui/settings/autofill/autofill_add_credit_card_coordinator.mm
@@ -65,6 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   UINavigationController* navigationController = [[UINavigationController alloc]
       initWithRootViewController:self.addCreditCardViewController];
+  navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
   navigationController.presentationController.delegate = self;
 
   [self.baseViewController presentViewController:navigationController
