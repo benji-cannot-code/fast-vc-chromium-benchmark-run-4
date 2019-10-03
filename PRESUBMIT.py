a220d09de9378417081a2d5cafb267e163b859e6
Robert Liao <robliao@chromium.org>
2019-10-03 17:16:46 +0000

    Add Conversion Guidance to the GetAddressOf() PRESUBMIT
    BUG=914910
    
    Change-Id: I46198de26b7963d31f216a2355bba0d8a4e6999a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1837751
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702459}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1048,7 +1048,8 @@ _BANNED_CPP_FUNCTIONS = (
       'GetAddressOf',
       (
         'Improper use of Microsoft::WRL::ComPtr<T>::GetAddressOf() has been ',
-        'implicated in a few leaks. Use operator& instead.'
+        'implicated in a few leaks. Use operator& instead. See ',
+        'http://crbug.com/914910 for more conversion guidance.'
       ),
       True,
       (),
