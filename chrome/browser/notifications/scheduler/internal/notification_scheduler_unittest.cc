0c2dda165e9cc0d1d7b80b12d61395a6ac4f53e1
Xing Liu <xingliu@chromium.org>
2019-10-03 18:06:56 +0000

    Notification scheduler: Remove ImpressionHistoryTracker::Delegate.
    This CL removed the unused delegate class for ImpressionHistoryTracker.
    
    Bug: 965133
    Change-Id: I0463a99f4dc7344c66533b4881fec5fe8fa6393a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832711
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702481}

--- chrome/browser/notifications/scheduler/internal/notification_scheduler_unittest.cc
+++ chrome/browser/notifications/scheduler/internal/notification_scheduler_unittest.cc
@@ -87,9 +87,8 @@ class NotificationSchedulerTest : public testing::Test {
 
  protected:
   void Init() {
-    EXPECT_CALL(*impression_tracker(), Init(_, _))
-        .WillOnce(Invoke([&](ImpressionHistoryTracker::Delegate* delegate,
-                             ImpressionHistoryTracker::InitCallback callback) {
+    EXPECT_CALL(*impression_tracker(), Init(_))
+        .WillOnce(Invoke([&](ImpressionHistoryTracker::InitCallback callback) {
           std::move(callback).Run(true);
         }));
 
@@ -161,9 +160,8 @@ TEST_F(NotificationSchedulerTest, InitSuccess) {
 
 // Tests the case when impression tracker failed to initialize.
 TEST_F(NotificationSchedulerTest, InitImpressionTrackerFailed) {
-  EXPECT_CALL(*impression_tracker(), Init(_, _))
-      .WillOnce(Invoke([](ImpressionHistoryTracker::Delegate* delegate,
-                          ImpressionHistoryTracker::InitCallback callback) {
+  EXPECT_CALL(*impression_tracker(), Init(_))
+      .WillOnce(Invoke([](ImpressionHistoryTracker::InitCallback callback) {
         // Impression tracker failed to load.
         std::move(callback).Run(false);
       }));
@@ -182,9 +180,8 @@ TEST_F(NotificationSchedulerTest, InitImpressionTrackerFailed) {
 
 // Tests the case when scheduled notification manager failed to initialize.
 TEST_F(NotificationSchedulerTest, InitScheduledNotificationManagerFailed) {
-  EXPECT_CALL(*impression_tracker(), Init(_, _))
-      .WillOnce(Invoke([](ImpressionHistoryTracker::Delegate* delegate,
-                          ImpressionHistoryTracker::InitCallback callback) {
+  EXPECT_CALL(*impression_tracker(), Init(_))
+      .WillOnce(Invoke([](ImpressionHistoryTracker::InitCallback callback) {
         std::move(callback).Run(true);
       }));
 
