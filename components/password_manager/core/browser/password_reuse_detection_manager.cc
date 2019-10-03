7c421265055983aa38cc227c08095e7313744b8f
Varun Khaneja <vakh@chromium.org>
2019-10-03 18:34:17 +0000

    [PasswordProtection] kMaxNumberOfCharactersToStore: 30 => 45
    See discussion here: http://shortn/_slnTdcsaT9
    
    R=bdea, dvadym
    
    Bug: 1010158
    Change-Id: Ib8c881acf825078ed0471e9a838e678ecccb15c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832408
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702498}

--- components/password_manager/core/browser/password_reuse_detection_manager.cc
+++ components/password_manager/core/browser/password_reuse_detection_manager.cc
@@ -19,7 +19,7 @@ using base::TimeDelta;
 namespace password_manager {
 
 namespace {
-constexpr size_t kMaxNumberOfCharactersToStore = 30;
+constexpr size_t kMaxNumberOfCharactersToStore = 45;
 constexpr TimeDelta kMaxInactivityTime = TimeDelta::FromSeconds(10);
 }
 
