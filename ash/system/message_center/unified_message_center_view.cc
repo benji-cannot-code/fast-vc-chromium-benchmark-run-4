c6f643579cbc25c6b458fe67212f7cde70b2bfc5
Allen Bauer <kylixrd@chromium.org>
2019-10-03 17:15:34 +0000

    Clarify ScrollView scrollbar ownership.
    Reland of https://crrev.com/c/1825620
    
    Change-Id: Iae413f777b3fdb094cf85d49e6110321628125e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1836985
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702458}

--- ash/system/message_center/unified_message_center_view.cc
+++ ash/system/message_center/unified_message_center_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ash/system/unified/unified_system_tray_model.h"
 #include "ash/system/unified/unified_system_tray_view.h"
+#include "base/memory/ptr_util.h"
 #include "base/metrics/user_metrics.h"
 #include "ui/gfx/animation/linear_animation.h"
 #include "ui/message_center/message_center.h"
@@ -95,7 +96,7 @@ UnifiedMessageCenterView::UnifiedMessageCenterView(
   scroller_->SetContents(
       std::make_unique<ScrollerContentsView>(message_list_view_, this));
   scroller_->SetBackgroundColor(SK_ColorTRANSPARENT);
-  scroller_->SetVerticalScrollBar(scroll_bar_);
+  scroller_->SetVerticalScrollBar(base::WrapUnique(scroll_bar_));
   scroller_->SetDrawOverflowIndicator(false);
   AddChildView(scroller_);
 }
