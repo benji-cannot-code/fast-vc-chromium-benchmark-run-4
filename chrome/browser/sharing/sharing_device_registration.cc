69b4f22946c2f8bb7dd091c9b31299d9b2f43bc3
Yasmin <yasmo@chromium.org>
2019-10-03 12:25:35 +0000

    Check enterprise policy on the receiver side for Sharing Device Registration.
    Bug: 1005288
    Change-Id: Ieff5fe5112ae5bcc3a7fba02424b7abdecf53555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1836509
    Commit-Queue: Yasmin Molazadeh <yasmo@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702381}

--- chrome/browser/sharing/sharing_device_registration.cc
+++ chrome/browser/sharing/sharing_device_registration.cc
@@ -18,8 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sharing/sharing_device_registration_result.h"
 #include "chrome/browser/sharing/sharing_sync_preference.h"
 #include "chrome/browser/sharing/vapid_key_manager.h"
+#include "chrome/common/pref_names.h"
 #include "components/gcm_driver/crypto/p256_key_util.h"
 #include "components/gcm_driver/instance_id/instance_id_driver.h"
+#include "components/prefs/pref_service.h"
 #include "components/sync_device_info/device_info.h"
 #include "crypto/ec_private_key.h"
 
@@ -31,10 +33,12 @@ using instance_id::InstanceID;
 using sync_pb::SharingSpecificFields;
 
 SharingDeviceRegistration::SharingDeviceRegistration(
+    PrefService* pref_service,
     SharingSyncPreference* sharing_sync_preference,
     instance_id::InstanceIDDriver* instance_id_driver,
     VapidKeyManager* vapid_key_manager)
-    : sharing_sync_preference_(sharing_sync_preference),
+    : pref_service_(pref_service),
+      sharing_sync_preference_(sharing_sync_preference),
       instance_id_driver_(instance_id_driver),
       vapid_key_manager_(vapid_key_manager) {}
 
@@ -212,6 +216,11 @@ bool SharingDeviceRegistration::IsClickToCallSupported() const {
 }
 
 bool SharingDeviceRegistration::IsSharedClipboardSupported() const {
+  // Check the enterprise policy for Shared Clipboard.
+  if (pref_service_ &&
+      !pref_service_->GetBoolean(prefs::kSharedClipboardEnabled)) {
+    return false;
+  }
   return base::FeatureList::IsEnabled(kSharedClipboardReceiver);
 }
 
