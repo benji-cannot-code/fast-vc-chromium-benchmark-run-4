53ed16ca48c7308a1694614184de51651746ebe7
Ahmed Mehfooz <amehfooz@chromium.org>
2019-10-03 19:51:30 +0000

    Merge SystemTrayFeaturePodsPagination flag with MessageCenterRefactor flag
    Change-Id: I8938d8c8db0a3275f4cc272d980c2a09576eba7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1836658
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702541}

--- ash/system/unified/feature_pods_container_view.cc
+++ ash/system/unified/feature_pods_container_view.cc
@@ -75,7 +75,7 @@ int FeaturePodsContainerView::GetExpandedHeight() const {
   int number_of_lines = (visible_count + kUnifiedFeaturePodItemsInRow - 1) /
                         kUnifiedFeaturePodItemsInRow;
 
-  if (features::IsSystemTrayFeaturePodsPaginationEnabled())
+  if (features::IsUnifiedMessageCenterRefactorEnabled())
     number_of_lines = std::min(number_of_lines, feature_pod_rows_);
 
   return kUnifiedFeaturePodBottomPadding +
@@ -316,7 +316,7 @@ void FeaturePodsContainerView::CalculateIdealBoundsForFeaturePods() {
 }
 
 int FeaturePodsContainerView::GetTilesPerPage() const {
-  if (features::IsSystemTrayFeaturePodsPaginationEnabled())
+  if (features::IsUnifiedMessageCenterRefactorEnabled())
     return kUnifiedFeaturePodItemsInRow * feature_pod_rows_;
   else
     return children().size();
