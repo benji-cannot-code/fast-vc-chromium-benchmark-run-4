33059f727f1194445dc218213f1ff335fa208213
Christian Biesinger <cbiesinger@chromium.org>
2019-10-03 23:23:44 +0000

    Make pkg-config.py support Python 3
    Use decode('utf-8') to turn bytes into a string.
    This works in Python 2 and 3.
    
    Tested by running this script under Python 2 and 3.
    
    Bug: 941669
    Change-Id: I0cc4e58031f67867f56d37e47e54ef3dd7a49416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838258
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702633}

--- build/config/linux/pkg-config.py
+++ build/config/linux/pkg-config.py
@@ -80,7 +80,7 @@ def GetPkgConfigPrefixToStrip(options, args):
   # To support this correctly, it's necessary to extract the prefix to strip
   # from pkg-config's |prefix| variable.
   prefix = subprocess.check_output([options.pkg_config,
-      "--variable=prefix"] + args, env=os.environ)
+      "--variable=prefix"] + args, env=os.environ).decode('utf-8')
   if prefix[-4] == '/usr':
     return prefix[4:]
   return prefix
@@ -159,7 +159,7 @@ def main():
   if options.version_as_components:
     cmd = [options.pkg_config, "--modversion"] + args
     try:
-      version_string = subprocess.check_output(cmd)
+      version_string = subprocess.check_output(cmd).decode('utf-8')
     except:
       sys.stderr.write('Error from pkg-config.\n')
       return 1
@@ -172,7 +172,7 @@ def main():
     if options.debug:
       sys.stderr.write('Running: %s\n' % cmd)
     try:
-      libdir = subprocess.check_output(cmd)
+      libdir = subprocess.check_output(cmd).decode('utf-8')
     except:
       print("Error from pkg-config.")
       return 1
@@ -184,7 +184,7 @@ def main():
     if options.debug:
       sys.stderr.write('Running: %s\n' % cmd)
     try:
-      dridriverdir = subprocess.check_output(cmd)
+      dridriverdir = subprocess.check_output(cmd).decode('utf-8')
     except:
       print("Error from pkg-config.")
       return 1
@@ -196,7 +196,7 @@ def main():
     sys.stderr.write('Running: %s\n' % ' '.join(cmd))
 
   try:
-    flag_string = subprocess.check_output(cmd)
+    flag_string = subprocess.check_output(cmd).decode('utf-8')
   except:
     sys.stderr.write('Could not run pkg-config.\n')
     return 1
