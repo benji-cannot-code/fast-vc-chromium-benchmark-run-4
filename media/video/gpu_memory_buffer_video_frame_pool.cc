13403e25da71b6cf782f5d421e9c45c8f354911c
Sunny Sachanandani <sunnyps@chromium.org>
2019-10-03 21:31:30 +0000

    gpu: Do not promote I420A video to overlay on Windows
    GpuMemoryBufferVideoFramePool uses RGBA/BGRA as the output format for
    I420A video frames, and those are promoted to overlays on all platforms.
    This causes SwapBuffers to fail in the GPU process on Windows since the
    direct composition path can only handle YUV video.  This wasn't a
    problem until we started promoting TextureDrawQuads to overlays in
    crrev.com/c/1746796 assuming that the is_overlay_candidate flag would
    only be set for low latency canvas that uses a swap chain.
    
    A pixel test with yuva420p vp9 video is added.  The video was generated
    using the following ffmpeg command:
    
    ffmpeg -i four-colors.mp4 -filter_complex \
    "color=c=#7F7F7F7F:s=960x540:rate=12[a];[0][a]alphamerge,format=yuva420p" \
    -c:v libvpx-vp9 -auto-alt-ref 0 four-colors-vp9-i420a.webm
    
    Bug: 1005028
    Change-Id: Idc84d29c588cf3b9273fb605b2542be5441d61d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829556
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702590}

--- media/video/gpu_memory_buffer_video_frame_pool.cc
+++ media/video/gpu_memory_buffer_video_frame_pool.cc
@@ -917,6 +917,11 @@ void GpuMemoryBufferVideoFramePool::PoolImpl::
   frame->set_color_space(video_frame->ColorSpace());
 
   bool allow_overlay = false;
+#if defined(OS_WIN)
+  // Windows direct composition path only supports dual GMB NV12 video overlays.
+  allow_overlay = (output_format_ ==
+                   GpuVideoAcceleratorFactories::OutputFormat::NV12_DUAL_GMB);
+#else
   switch (output_format_) {
     case GpuVideoAcceleratorFactories::OutputFormat::I420:
       allow_overlay =
@@ -926,9 +931,7 @@ void GpuMemoryBufferVideoFramePool::PoolImpl::
       allow_overlay = true;
       break;
     case GpuVideoAcceleratorFactories::OutputFormat::NV12_DUAL_GMB:
-#if defined(OS_WIN)
-      allow_overlay = true;
-#endif
+      // Only used on Windows where we can't use single NV12 textures.
       break;
     case GpuVideoAcceleratorFactories::OutputFormat::XR30:
     case GpuVideoAcceleratorFactories::OutputFormat::XB30:
@@ -947,7 +950,7 @@ void GpuMemoryBufferVideoFramePool::PoolImpl::
     case GpuVideoAcceleratorFactories::OutputFormat::UNDEFINED:
       break;
   }
-
+#endif  // OS_WIN
   frame->metadata()->MergeMetadataFrom(video_frame->metadata());
   frame->metadata()->SetBoolean(VideoFrameMetadata::ALLOW_OVERLAY,
                                 allow_overlay);
