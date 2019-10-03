8d90407b37a458ff27378158a63b29851faa0bf4
Andrew Grieve <agrieve@google.com>
2019-10-03 15:43:59 +0000

    Android: Strip Pre-M background scheduler code for Monochrome+.
    To save on binary size (~50 methods).
    
    Bug: 921413
    Change-Id: I61ab4d0915c6ffef11721271aa12a6b277c4b254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1831984
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702425}

--- components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskSchedulerImpl.java
+++ components/background_task_scheduler/android/java/src/org/chromium/components/background_task_scheduler/BackgroundTaskSchedulerImpl.java
@@ -8,6 +8,7 @@ package org.chromium.components.background_task_scheduler;
 import android.content.Context;
 import android.os.Build;
 
+import org.chromium.base.BuildConfig;
 import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
@@ -209,7 +210,10 @@ class BackgroundTaskSchedulerImpl implements BackgroundTaskScheduler {
     }
 
     private boolean osUpgradeChangesDelegateType(int oldSdkInt, int newSdkInt) {
-        return oldSdkInt < Build.VERSION_CODES.M && newSdkInt >= Build.VERSION_CODES.M;
+        // Assuming no upgrades from L->N (without going through M) allows us to remove
+        // GCMNetworkManager codepaths for Monochrome and above.
+        return BuildConfig.MIN_SDK_VERSION < Build.VERSION_CODES.N
+                && oldSdkInt < Build.VERSION_CODES.M && newSdkInt >= Build.VERSION_CODES.M;
     }
 
     private void selectDelegateAndCancel(
