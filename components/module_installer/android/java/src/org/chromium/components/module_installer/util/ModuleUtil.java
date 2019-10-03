a1ab0f77ea92ebc18b25a8617e58f06e08437163
Fred Mello <fredmello@chromium.org>
2019-10-03 22:51:21 +0000

    Fix binary size increase for APKs
    Make sure that public methods are optimized for APKs.
    
    Related to:
    https://crrev.com/c/1813520
    
    Bug: 1010777, 1005802
    Change-Id: I62fdac9b4acd12f5e09de01dc975cbdb5f116b99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838141
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Fred Mello <fredmello@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702615}

--- components/module_installer/android/java/src/org/chromium/components/module_installer/util/ModuleUtil.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/util/ModuleUtil.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.module_installer.util;
 
 import org.chromium.base.annotations.MainDex;
+import org.chromium.components.module_installer.builder.ModuleInstallerConfig;
 import org.chromium.components.module_installer.logger.SplitAvailabilityLogger;
 
 /**
@@ -15,8 +16,13 @@ import org.chromium.components.module_installer.logger.SplitAvailabilityLogger;
 public class ModuleUtil {
     /**
      * Records the execution time (ms) taken by the module installer framework.
+     *
+     * Make sure that public methods check for bundle config so that tree shaking can remove
+     * unnecessary code (modules are not supported in APKs).
      */
     public static void recordStartupTime() {
+        if (!ModuleInstallerConfig.IS_BUNDLE) return;
+
         Timer.recordStartupTime();
     }
 
@@ -24,6 +30,8 @@ public class ModuleUtil {
      * Records the start time in order to later report the install duration via UMA.
      */
     public static void recordModuleAvailability() {
+        if (!ModuleInstallerConfig.IS_BUNDLE) return;
+
         try (Timer timer = new Timer()) {
             initApplication();
             SplitAvailabilityLogger.logModuleAvailability();
@@ -34,6 +42,8 @@ public class ModuleUtil {
      * Updates the CrashKey report containing modules currently present.
      */
     public static void updateCrashKeys() {
+        if (!ModuleInstallerConfig.IS_BUNDLE) return;
+
         try (Timer timer = new Timer()) {
             CrashKeyRecorder.updateCrashKeys();
         }
@@ -43,6 +53,8 @@ public class ModuleUtil {
      * Initializes the PlayCore SplitCompat framework.
      */
     public static void initApplication() {
+        if (!ModuleInstallerConfig.IS_BUNDLE) return;
+
         try (Timer timer = new Timer()) {
             SplitCompatInitializer.initApplication();
         }
