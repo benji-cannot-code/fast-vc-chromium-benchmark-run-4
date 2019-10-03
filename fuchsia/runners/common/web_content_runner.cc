4e3ab2bd77dde127518e8675f4535e0a39fa8289
Yoichi Osato <yoichio@chromium.org>
2019-10-03 04:54:14 +0000

    Revert "[fuchsia] Implement per-app remote debugging in CastRunner."
    This reverts commit 304c2408e9fb598f0b963327d90f74ef8d9eb1fa.
    
    Reason for revert: This causes test failure.
    Bug: 1010801
    
    Original change's description:
    > [fuchsia] Implement per-app remote debugging in CastRunner.
    > 
    > * Handle the |enable_remote_debugging| parameter in
    >   chromium.cast.ApplicationConfig to enable per-app remote debugging
    >   in CastRunner.
    > * Set the CastRunner debugging port to 9222.
    > * Remove a buildflag to enable remote debugging Context-wide.
    > * Add a test to check remote debugging is properly enabled per-App.
    > 
    > Bug: 997895
    > 
    > Change-Id: I19aced4e90790e77cc3f55b1118fbf459ae19b14
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1773707
    > Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    > Reviewed-by: Wez <wez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#702264}
    
    TBR=wez@chromium.org,fdegans@chromium.org
    
    Change-Id: I0d5499e443b3fe4f640860df9437def2d9466535
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 997895
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1837322
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702317}

--- fuchsia/runners/common/web_content_runner.cc
+++ fuchsia/runners/common/web_content_runner.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/scoped_service_binding.h"
 #include "base/fuchsia/startup_context.h"
 #include "base/logging.h"
+#include "fuchsia/runners/buildflags.h"
 #include "fuchsia/runners/common/web_component.h"
 #include "url/gurl.h"
 
@@ -72,6 +73,12 @@ fuchsia::web::CreateContextParams WebContentRunner::BuildCreateContextParams(
   if (data_directory)
     create_params.set_data_directory(std::move(data_directory));
 
+  // Set |remote_debugging_port| on the context, if set.
+  if (BUILDFLAG(ENABLE_REMOTE_DEBUGGING_ON_PORT) != 0) {
+    create_params.set_remote_debugging_port(
+        BUILDFLAG(ENABLE_REMOTE_DEBUGGING_ON_PORT));
+  }
+
   create_params.set_features(features);
 
   return create_params;
@@ -100,7 +107,6 @@ void WebContentRunner::StartComponent(
       this,
       std::make_unique<base::fuchsia::StartupContext>(std::move(startup_info)),
       std::move(controller_request));
-  component->StartComponent();
   component->LoadUrl(url, std::vector<fuchsia::net::http::Header>());
   RegisterComponent(std::move(component));
 }
