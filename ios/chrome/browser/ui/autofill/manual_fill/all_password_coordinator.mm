a093565dfef07c9b50b26f5b399c1812059a8bb3
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-10-03 08:55:46 +0000

    [iOS][MF] Fix search in all passwords
    If the password view controller was presenting something, it will stay
    around. This is fixed and a test is added.
    
    Bug: 1008421
    Change-Id: I8248cbbf28be94bc4b84c65ccda42129a192db0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1826895
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702358}

--- ios/chrome/browser/ui/autofill/manual_fill/all_password_coordinator.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/all_password_coordinator.mm
@@ -76,8 +76,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
-  [self.passwordViewController dismissViewControllerAnimated:YES
-                                                  completion:nil];
+  [self.passwordViewController.presentingViewController
+      dismissViewControllerAnimated:YES
+                         completion:nil];
   self.passwordViewController = nil;
   self.passwordMediator = nil;
   [super stop];
