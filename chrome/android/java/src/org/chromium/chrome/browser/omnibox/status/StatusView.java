1d34b2d613a1da6f21523b5fa453028a3f151b2f
Brandon Wylie <wylieb@chromium.org>
2019-10-03 17:07:42 +0000

    Remove specialty padding for incognito icon when dse icon is active
    I've simplified the padding logic for the dse icon experiment, so
    there's no need for specialty logic.
    
    Bug: 1008496
    Change-Id: Ib2a88b3b47b37eb88a59f345c8be6d56a5464b6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1828126
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Ender <ender@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702444}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/status/StatusView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/status/StatusView.java
@@ -29,6 +29,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.omnibox.SearchEngineLogoUtils;
 import org.chromium.chrome.browser.toolbar.ToolbarCommonPropertiesModel;
@@ -40,6 +41,19 @@ import org.chromium.ui.widget.Toast;
  * StatusView is a location bar's view displaying status (icons and/or text).
  */
 public class StatusView extends LinearLayout {
+    @VisibleForTesting
+    class StatusViewDelegate {
+        /** @see {@link SearchEngineLogoUtils#shouldShowSearchEngineLogo} */
+        boolean shouldShowSearchEngineLogo(boolean isIncognito) {
+            return SearchEngineLogoUtils.shouldShowSearchEngineLogo(isIncognito);
+        }
+
+        /** @see {@link SearchEngineLogoUtils#isSearchEngineLogoEnabled()} */
+        boolean isSearchEngineLogoEnabled() {
+            return SearchEngineLogoUtils.isSearchEngineLogoEnabled();
+        }
+    }
+
     private @Nullable View mIncognitoBadge;
     private int mIncognitoBadgeEndPaddingWithIcon;
     private int mIncognitoBadgeEndPaddingWithoutIcon;
@@ -63,6 +77,7 @@ public class StatusView extends LinearLayout {
 
     private TouchDelegate mTouchDelegate;
     private CompositeTouchDelegate mCompositeTouchDelegate;
+    private StatusViewDelegate mDelegate;
 
     private boolean mLastTouchDelegateRtlness;
     private Rect mLastTouchDelegateRect;
@@ -71,6 +86,7 @@ public class StatusView extends LinearLayout {
 
     public StatusView(Context context, AttributeSet attributes) {
         super(context, attributes);
+        mDelegate = new StatusViewDelegate();
     }
 
     @Override
@@ -96,7 +112,7 @@ public class StatusView extends LinearLayout {
     public void updateSearchEngineStatusIcon(boolean shouldShowSearchEngineLogo,
             boolean isSearchEngineGoogle, String searchEngineUrl) {
         if (mToolbarCommonPropertiesModel != null
-                && SearchEngineLogoUtils.shouldShowSearchEngineLogo(
+                && mDelegate.shouldShowSearchEngineLogo(
                         mToolbarCommonPropertiesModel.isIncognito())) {
             LinearLayout.LayoutParams layoutParams =
                     new LinearLayout.LayoutParams(mIconView.getLayoutParams());
@@ -388,11 +404,15 @@ public class StatusView extends LinearLayout {
     private void updateIncognitoBadgeEndPadding() {
         if (mIncognitoBadge == null) return;
 
+        int endPadding = -1;
+        if (mDelegate.isSearchEngineLogoEnabled()) {
+            endPadding = 0;
+        } else {
+            endPadding = mIconRes != 0 ? mIncognitoBadgeEndPaddingWithIcon
+                                       : mIncognitoBadgeEndPaddingWithoutIcon;
+        }
         mIncognitoBadge.setPaddingRelative(mIncognitoBadge.getPaddingStart(),
-                mIncognitoBadge.getPaddingTop(),
-                mIconRes != 0 ? mIncognitoBadgeEndPaddingWithIcon
-                              : mIncognitoBadgeEndPaddingWithoutIcon,
-                mIncognitoBadge.getPaddingBottom());
+                mIncognitoBadge.getPaddingTop(), endPadding, mIncognitoBadge.getPaddingBottom());
     }
 
     /**
@@ -486,5 +506,9 @@ public class StatusView extends LinearLayout {
 
     TouchDelegate getTouchDelegateForTesting() {
         return mTouchDelegate;
+    }
+
+    void setDelegateForTesting(StatusViewDelegate delegate) {
+        mDelegate = delegate;
     }
 }
