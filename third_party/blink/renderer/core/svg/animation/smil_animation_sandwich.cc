9aa2507394d2ec9c3ef832cbe36e752effa05791
Fredrik Söderquist <fs@opera.com>
2019-10-03 08:02:59 +0000

    Move the update of the active state for SMIL out of UpdateTiming
    This moves the call to SVGSMILElement::UpdateActiveState() from
    SMILAnimationSandwich's UpdateTiming() method to UpdateSyncBases() and
    renames the latter UpdateActiveStateAndOrder().
    The reset of the |interval_has_changed_| flag is moved to
    UpdateActiveState(), and SVGSMILElement::UpdateSyncBases() is
    decommissioned.
    This exposed that SVGSMILElement::UpdateInterval(...) could end up
    clobbering |previous_interval_| when re-resolving the current interval.
    
    Bug: 998526
    Change-Id: I0b3787f93898b062a3a1e790bf761402c0515156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834351
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702353}

--- third_party/blink/renderer/core/svg/animation/smil_animation_sandwich.cc
+++ third_party/blink/renderer/core/svg/animation/smil_animation_sandwich.cc
@@ -61,7 +61,6 @@ void SMILAnimationSandwich::UpdateTiming(SMILTime elapsed) {
     if (!animation->CurrentIntervalIsActive(elapsed))
       continue;
     animation->UpdateInterval(elapsed);
-    animation->UpdateActiveState(elapsed);
   }
 }
 
@@ -87,13 +86,15 @@ SMILTime SMILAnimationSandwich::NextProgressTime(
   return earliest_progress_time;
 }
 
-void SMILAnimationSandwich::UpdateSyncBases(SMILTime elapsed) {
+void SMILAnimationSandwich::UpdateActiveStateAndOrder(
+    SMILTime presentation_time) {
   for (auto& animation : sandwich_)
-    animation->UpdateSyncBases();
+    animation->UpdateActiveState(presentation_time);
 
   if (!std::is_sorted(sandwich_.begin(), sandwich_.end(),
-                      PriorityCompare(elapsed))) {
-    std::sort(sandwich_.begin(), sandwich_.end(), PriorityCompare(elapsed));
+                      PriorityCompare(presentation_time))) {
+    std::sort(sandwich_.begin(), sandwich_.end(),
+              PriorityCompare(presentation_time));
   }
 }
 
