f6d287477b4182e81dad90b1b7c6ddba99924a1c
Maxim Smirnov <smirnov-maxim@yandex-team.ru>
2019-10-04 17:25:56 +0000

    Remove "LoadBrokenImagesFromContextMenu" feature flag
    The feature is enabled by default for some time now, so it is suggested
    to removed it from code.
    
    R=avi@chromium.org
    
    Bug: 603
    Change-Id: I7a25ff9ae7c93a01ad536a29eafa10165749482e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1800408
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702930}

--- chrome/browser/renderer_context_menu/render_view_context_menu_browsertest.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu_browsertest.cc
@@ -1146,8 +1146,6 @@ class LoadImageBrowserTest : public InProcessBrowserTest {
   void SetupAndLoadImagePage(const std::string& page_path,
                              const std::string& image_path) {
     image_path_ = image_path;
-    scoped_feature_list_.InitAndEnableFeature(
-        features::kLoadBrokenImagesFromContextMenu);
 
     embedded_test_server()->RegisterRequestHandler(base::BindRepeating(
         &LoadImageBrowserTest::HandleRequest, base::Unretained(this)));
@@ -1199,7 +1197,6 @@ class LoadImageBrowserTest : public InProcessBrowserTest {
 
   std::string image_path_;
   size_t request_attempts_ = 0u;
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(LoadImageBrowserTest, LoadImage) {
