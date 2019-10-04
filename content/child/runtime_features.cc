8814f4435edecd4ae1f5e116d9620ff70b1b2142
Dave Tapuska <dtapuska@chromium.org>
2019-10-04 21:44:05 +0000

    Remove PauseExecutionContextOnBackgroundFreeze
    This shipped in M77 and can be now removed.
    
    BUG=957589
    
    Change-Id: I89ab170207d352377760d60f5f1414c94053c615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1807568
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703050}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -384,11 +384,6 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
                                                 true);
   }
 
-  if (!base::FeatureList::IsEnabled(
-          features::kPauseExecutionContextOnBackgroundFreeze)) {
-    WebRuntimeFeatures::EnablePauseExecutionContextOnBackgroundFreeze(false);
-  }
-
   WebRuntimeFeatures::EnableConsolidatedMovementXY(
       base::FeatureList::IsEnabled(features::kConsolidatedMovementXY));
 
