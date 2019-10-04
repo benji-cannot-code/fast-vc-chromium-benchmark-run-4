4853bee98adf59894f72eb9f306b2544951473c9
Matt Wolenetz <wolenetz@chromium.org>
2019-10-04 22:18:05 +0000

    MSE: Make SBStream::Append return void
    Long ago, this method could return false if internal buffering detected
    a problem in the buffer sequence/metadata being appended. With the
    MseBufferByPts-related cleanup, this method was made to always return
    true, with a note that it might in future return false if one current
    CHECK is changed to a DCHECK. This CL asserts that that CHECK is
    necessary to verify a key assumption hasn't regressed in release
    builds, and changes the interface to void to match current logic and
    usage.
    
    BUG=580621
    
    Change-Id: Ie81d6f1681b65ca93e6af1f21d6f2289c6c0b7da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841237
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703067}

--- media/filters/source_buffer_stream.cc
+++ media/filters/source_buffer_stream.cc
@@ -247,7 +247,7 @@ void SourceBufferStream::OnStartOfCodedFrameGroup(
   }
 }
 
-bool SourceBufferStream::Append(const BufferQueue& buffers) {
+void SourceBufferStream::Append(const BufferQueue& buffers) {
   TRACE_EVENT2("media", "SourceBufferStream::Append",
                "stream type", GetStreamTypeName(),
                "buffers to append", buffers.size());
@@ -270,9 +270,8 @@ bool SourceBufferStream::Append(const BufferQueue& buffers) {
       << buffers.front()->timestamp().InMicroseconds() << "us";
 
   // New coded frame groups emitted by the coded frame processor must begin with
-  // a keyframe. TODO(wolenetz): Change this to [DCHECK + MEDIA_LOG(ERROR...) +
-  // return false] once the CHECK has baked in a stable release. See
-  // https://crbug.com/580621.
+  // a keyframe. Avoid propagating with escalating impact if this assumption is
+  // broken.
   CHECK(!new_coded_frame_group_ || buffers.front()->is_key_frame());
 
   // Buffers within each GOP in a coded frame group must be monotonically
@@ -359,7 +358,7 @@ bool SourceBufferStream::Append(const BufferQueue& buffers) {
                     " Skipping further processing.";
         DVLOG(1) << __func__ << " " << GetStreamTypeName()
                  << ": done. ranges_=" << RangesToString(ranges_);
-        return true;
+        return;
       } else if (itr != buffers.begin()) {
         // Copy the first key frame and everything after it into
         // |trimmed_buffers|.
@@ -435,7 +434,6 @@ bool SourceBufferStream::Append(const BufferQueue& buffers) {
            << ": done. ranges_=" << RangesToString(ranges_);
   DCHECK(IsRangeListSorted(ranges_));
   DCHECK(OnlySelectedRangeIsSeeked());
-  return true;
 }
 
 void SourceBufferStream::Remove(base::TimeDelta start,
