44c281d9a641d87885d1624400415de70667892b
Ben Wagner <bungeman@chromium.org>
2019-10-04 17:13:57 +0000

    Roll src/third_party/expat/src/ 4f23e05a3..a7bc26b69 (5 commits)
    https://chromium.googlesource.com/external/github.com/libexpat/libexpat.git/+log/4f23e05a33a6..a7bc26b69768
    
    $ git log 4f23e05a3..a7bc26b69 --date=short --no-merges --format='%ad %ae %s'
    2019-09-25 sebastian Set release date for 2.2.9
    2019-09-25 sebastian Bump version info from 7:10:6 to 7:11:6
    2019-09-13 sebastian Bump version from 2.2.8 to 2.2.9
    2019-09-25 sebastian Changes: Document #347
    2019-09-21 sebastian CMake: Use libexpat*.(dll|lib) not expat*.(dll|lib) on Windows
    
    Created with:
      roll-dep src/third_party/expat/src
    R=bungeman@chromium.org,dcheng@chromium.org
    
    Change-Id: I1b04348966d0cc4872a89a45547cb16a28e11776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829678
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702917}

--- third_party/expat/include/expat_config/expat_config.h
+++ third_party/expat/include/expat_config/expat_config.h
@@ -78,7 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PACKAGE_NAME "expat"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "expat 2.2.8"
+#define PACKAGE_STRING "expat 2.2.9"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "expat"
@@ -87,13 +87,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.2.8"
+#define PACKAGE_VERSION "2.2.9"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.2.8"
+#define VERSION "2.2.9"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
