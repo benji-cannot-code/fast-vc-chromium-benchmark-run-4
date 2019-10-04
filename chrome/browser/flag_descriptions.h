4c7c0c4f8a606e59e1690bea3eb2e502e1c057ea
Thanh Nguyen <thanhdng@chromium.org>
2019-10-04 01:35:37 +0000

    [cros-fuzzy-app] Add a Chrome flag to enable Fuzzy App Search
    Bug: 990684
    Change-Id: I953b5e6140b7d22815f81abba4e9049813a9cd3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838892
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702682}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2074,6 +2074,9 @@ extern const char kFirstRunUiTransitionsDescription[];
 extern const char kFsNosymfollowName[];
 extern const char kFsNosymfollowDescription[];
 
+extern const char kFuzzyAppSearchName[];
+extern const char kFuzzyAppSearchDescription[];
+
 extern const char kGaiaActionButtonsName[];
 extern const char kGaiaActionButtonsDescription[];
 
