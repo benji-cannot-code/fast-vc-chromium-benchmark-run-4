3454a4bce040cac891ce176f068297db8a5754e6
Abigail Klein <abigailbklein@google.com>
2019-10-04 19:14:44 +0000

    [chrome:accessibility] Launch filters
    The feature flag is remaining in the code as future work on this page
    will be hidden behind the flag while in progress. Filters, however, are
    done and so can be removed from behind the flag.
    
    Bug: 785493,959368
    Change-Id: I605f6a44948648720298a1f609a55235054bf8c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829956
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702983}

--- chrome/browser/accessibility/accessibility_ui.cc
+++ chrome/browser/accessibility/accessibility_ui.cc
@@ -57,7 +57,6 @@ static const char kEnabledField[] = "enabled";
 static const char kErrorField[] = "error";
 static const char kFaviconUrlField[] = "faviconUrl";
 static const char kFlagNameField[] = "flagName";
-static const char kImprovementsEnabledField[] = "improvementsEnabled";
 static const char kModeIdField[] = "modeId";
 static const char kNameField[] = "name";
 static const char kPagesField[] = "pages";
@@ -161,7 +160,6 @@ bool ShouldHandleAccessibilityRequestCallback(const std::string& path) {
 
 void HandleAccessibilityRequestCallback(
     content::BrowserContext* current_context,
-    bool improvements_enabled,
     const std::string& path,
     const content::WebUIDataSource::GotDataCallback& callback) {
   DCHECK(ShouldHandleAccessibilityRequestCallback(path));
@@ -246,8 +244,6 @@ void HandleAccessibilityRequestCallback(
   }
 #endif  // !defined(OS_ANDROID)
   data.Set(kBrowsersField, std::move(browser_list));
-
-  data.SetBoolean(kImprovementsEnabledField, improvements_enabled);
 
   std::string json_string;
   base::JSONWriter::Write(data, &json_string);
@@ -334,9 +330,6 @@ AccessibilityUI::AccessibilityUI(content::WebUI* web_ui)
   content::WebUIDataSource* html_source =
       content::WebUIDataSource::Create(chrome::kChromeUIAccessibilityHost);
 
-  const bool improvements_enabled = base::FeatureList::IsEnabled(
-      features::kAccessibilityInternalsPageImprovements);
-
   // Add required resources.
   html_source->UseStringsJs();
   html_source->AddResourcePath("accessibility.css", IDR_ACCESSIBILITY_CSS);
@@ -345,23 +338,18 @@ AccessibilityUI::AccessibilityUI(content::WebUI* web_ui)
   html_source->SetRequestFilter(
       base::BindRepeating(&ShouldHandleAccessibilityRequestCallback),
       base::Bind(&HandleAccessibilityRequestCallback,
-                 web_ui->GetWebContents()->GetBrowserContext(),
-                 improvements_enabled));
+                 web_ui->GetWebContents()->GetBrowserContext()));
 
   content::BrowserContext* browser_context =
       web_ui->GetWebContents()->GetBrowserContext();
   content::WebUIDataSource::Add(browser_context, html_source);
 
-  web_ui->AddMessageHandler(
-      std::make_unique<AccessibilityUIMessageHandler>(improvements_enabled));
+  web_ui->AddMessageHandler(std::make_unique<AccessibilityUIMessageHandler>());
 }
 
 AccessibilityUI::~AccessibilityUI() {}
 
-AccessibilityUIMessageHandler::AccessibilityUIMessageHandler(
-    bool improvements_enabled)
-    : improvements_enabled_(base::FeatureList::IsEnabled(
-          features::kAccessibilityInternalsPageImprovements)) {}
+AccessibilityUIMessageHandler::AccessibilityUIMessageHandler() {}
 
 AccessibilityUIMessageHandler::~AccessibilityUIMessageHandler() {}
 
@@ -564,7 +552,6 @@ void AccessibilityUIMessageHandler::RequestWebContentsTree(
       web_contents->DumpAccessibilityTree(internal, property_filters);
   result->SetString(kTreeField,
                     base::UTF16ToUTF8(accessibility_contents_utf16));
-  result->SetBoolean(kImprovementsEnabledField, improvements_enabled_);
   CallJavascriptFunction(request_type, *(result.get()));
 }
 
@@ -614,7 +601,6 @@ void AccessibilityUIMessageHandler::RequestNativeUITree(
       result->SetKey(kTreeField,
                      base::Value(RecursiveDumpAXPlatformNodeAsString(
                          node, 0, property_filters)));
-      result->SetBoolean(kImprovementsEnabledField, improvements_enabled_);
       CallJavascriptFunction(request_type, *(result.get()));
       return;
     }
--- chrome/browser/accessibility/accessibility_ui.h
+++ chrome/browser/accessibility/accessibility_ui.h
@@ -27,7 +27,7 @@ class AccessibilityUI : public content::WebUIController {
 
 class AccessibilityUIMessageHandler : public content::WebUIMessageHandler {
  public:
-  explicit AccessibilityUIMessageHandler(bool improvements_enabled);
+  AccessibilityUIMessageHandler();
   ~AccessibilityUIMessageHandler() override;
 
   void RegisterMessages() override;
@@ -35,8 +35,6 @@ class AccessibilityUIMessageHandler : public content::WebUIMessageHandler {
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
  private:
-  bool improvements_enabled_;
-
   void ToggleAccessibility(const base::ListValue* args);
   void SetGlobalFlag(const base::ListValue* args);
   void RequestWebContentsTree(const base::ListValue* args);
