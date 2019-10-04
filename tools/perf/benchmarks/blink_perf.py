a30a232cebeb1eb19e7d4ce67918fbd15588cfe0
Vladimir Levin <vmpstr@chromium.org>
2019-10-04 23:13:56 +0000

    DL: Update perf_tests to use rendersubtree version of display locking.
    This patch updates the perftests to use rendersubtree attribute instead
    of the imperative displayLock. Also adds an activatable perftest.
    
    For this change to be equivalent to the previous one, the perf tests
    also need to run with CSSContentSize enabled, which this adds as well.
    
    R=chrishtr@chromium.org, crouleau@chromium.org
    
    Change-Id: I6447b467aa9cd58da9f572bca0eca7eda04f3216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841233
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703088}

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -564,4 +564,5 @@ class BlinkPerfDisplayLocking(_BlinkPerfBenchmark):
     return 'blink_perf.display_locking'
 
   def SetExtraBrowserOptions(self, options):
-    options.AppendExtraBrowserArgs(['--enable-blink-features=DisplayLocking'])
+    options.AppendExtraBrowserArgs(
+      ['--enable-blink-features=DisplayLocking,CSSContentSize'])
