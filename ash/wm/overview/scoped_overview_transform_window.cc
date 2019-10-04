ab5817925560dd7e869d8027f708822dab8e7d2d
Xiaoqian Dai <xdai@chromium.org>
2019-10-04 22:01:10 +0000

    Introduce ScopedEventTargetingBlocker class.
    It's a class to temporarily block event targeting for a window. There
    can be multiple ScopedEventTargetingBlocker placed on the same window
    and when that happens, the original event targeting policy will only be
    restored after all ScopedEventTargetingBlockers are removed from the
    window.
    
    No functional change in this CL. This CL will be useful in my another CL
    for Bug 997885.
    
    Bug: 997885
    Change-Id: I5f77eb001428be896570d5f3a5b92d98465374a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1835279
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703060}

--- ash/wm/overview/scoped_overview_transform_window.cc
+++ ash/wm/overview/scoped_overview_transform_window.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/client/transient_window_client.h"
+#include "ui/aura/scoped_window_event_targeting_blocker.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_observer.h"
@@ -112,8 +113,9 @@ ScopedOverviewTransformWindow::ScopedOverviewTransformWindow(
 
   std::vector<aura::Window*> transient_children_to_hide;
   for (auto* transient : GetTransientTreeIterator(window)) {
-    targeting_policy_map_[transient] = transient->event_targeting_policy();
-    transient->SetEventTargetingPolicy(aura::EventTargetingPolicy::kNone);
+    event_targeting_blocker_map_[transient] =
+        std::make_unique<aura::ScopedWindowEventTargetingBlocker>(transient);
+
     transient->SetProperty(kIsShowingInOverviewKey, true);
 
     // Hide transient children which have been specified to be hidden in
@@ -161,10 +163,8 @@ ScopedOverviewTransformWindow::ScopedOverviewTransformWindow(
 ScopedOverviewTransformWindow::~ScopedOverviewTransformWindow() {
   for (auto* transient : GetTransientTreeIterator(window_)) {
     transient->ClearProperty(kIsShowingInOverviewKey);
-    DCHECK(targeting_policy_map_.contains(transient));
-    auto it = targeting_policy_map_.find(transient);
-    transient->SetEventTargetingPolicy(it->second);
-    targeting_policy_map_.erase(it);
+    DCHECK(event_targeting_blocker_map_.contains(transient));
+    event_targeting_blocker_map_.erase(transient);
   }
 
   // No need to update the clip since we're about to restore it to
@@ -426,10 +426,11 @@ void ScopedOverviewTransformWindow::OnTransientChildWindowAdded(
   if (parent != window_ && !::wm::HasTransientAncestor(parent, window_))
     return;
 
-  DCHECK(!targeting_policy_map_.contains(transient_child));
-  targeting_policy_map_[transient_child] =
-      transient_child->event_targeting_policy();
-  transient_child->SetEventTargetingPolicy(aura::EventTargetingPolicy::kNone);
+  DCHECK(!event_targeting_blocker_map_.contains(transient_child));
+  event_targeting_blocker_map_[transient_child] =
+      std::make_unique<aura::ScopedWindowEventTargetingBlocker>(
+          transient_child);
+  transient_child->SetProperty(kIsShowingInOverviewKey, true);
 }
 
 void ScopedOverviewTransformWindow::OnTransientChildWindowRemoved(
@@ -438,10 +439,9 @@ void ScopedOverviewTransformWindow::OnTransientChildWindowRemoved(
   if (parent != window_ && !::wm::HasTransientAncestor(parent, window_))
     return;
 
-  DCHECK(targeting_policy_map_.contains(transient_child));
-  auto it = targeting_policy_map_.find(transient_child);
-  transient_child->SetEventTargetingPolicy(it->second);
-  targeting_policy_map_.erase(it);
+  transient_child->ClearProperty(kIsShowingInOverviewKey);
+  DCHECK(event_targeting_blocker_map_.contains(transient_child));
+  event_targeting_blocker_map_.erase(transient_child);
 }
 
 void ScopedOverviewTransformWindow::CloseWidget() {
