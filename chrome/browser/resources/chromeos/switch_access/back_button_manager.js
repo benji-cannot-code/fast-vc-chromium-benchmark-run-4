9469219f913384bbd9325542f7fac38ba10f8faa
Anastasia Helfinstein <anastasi@google.com>
2019-10-04 15:52:23 +0000

    [Switch Access] Add methods to RectHelper
    Some methods (i.e. center, unionAll) are not used in this change, but
    will be in a followup change.
    
    Bug: None
    Change-Id: I2fe31bdfa1e02d609813e352a51e8b105631660c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1830361
    Reviewed-by: Aran Gilman <gilmanmh@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#702880}

--- chrome/browser/resources/chromeos/switch_access/back_button_manager.js
+++ chrome/browser/resources/chromeos/switch_access/back_button_manager.js
@@ -49,7 +49,7 @@ class BackButtonManager {
   hide() {
     this.backButtonOpen_ = false;
     chrome.accessibilityPrivate.setSwitchAccessMenuState(
-        false, SAConstants.EMPTY_LOCATION, 0);
+        false, RectHelper.ZERO_RECT, 0);
     this.menuPanel_.setFocusRing(SAConstants.BACK_ID, false);
   }
 
