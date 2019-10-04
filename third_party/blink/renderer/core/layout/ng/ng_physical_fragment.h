71e37ea9d68dfc95aaf080115d30d827ed9acbfe
Ian Kilpatrick <ikilpatrick@chromium.org>
2019-10-04 20:31:41 +0000

    [LayoutNG] Account for relpos offset when adding layout overflow for floats.
    We missed accounting for the relpos offset when adding the layout
    overflow for floats.
    
    This resulted in a larger overflow area than intended.
    
    Bug: 1007026
    Change-Id: Ib35e0444c4a069a5399aa8967b5ebcf853677e8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1835973
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703016}

--- third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h
@@ -221,6 +221,7 @@ class CORE_EXPORT NGPhysicalFragment
   PhysicalRect ScrollableOverflow() const;
 
   // ScrollableOverflow(), with transforms applied wrt container if needed.
+  // This does not include any offsets from the parent (including relpos).
   PhysicalRect ScrollableOverflowForPropagation(
       const LayoutObject* container) const;
 
