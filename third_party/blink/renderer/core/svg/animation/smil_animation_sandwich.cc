8a98bf2bfe3a2b66b5fccd7d0a46b43df2f87a69
Fredrik Söderquist <fs@opera.com>
2019-10-04 22:40:15 +0000

    Make PriorityCompare a SMILAnimationSandwich detail
    The ordering required in SMILTimeContainer::ApplyAnimationValues isn't
    really the full-blown ordering used for sandwiches - using only the
    "document order index" should be sufficient (although arguably still not
    correct).
    
    Bug: 998526
    Change-Id: I293b9e55824c2b683b5991e7da3266958eb94e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1840991
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#703073}

--- third_party/blink/renderer/core/svg/animation/smil_animation_sandwich.cc
+++ third_party/blink/renderer/core/svg/animation/smil_animation_sandwich.cc
@@ -32,6 +32,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+struct PriorityCompare {
+  PriorityCompare(SMILTime elapsed) : elapsed_(elapsed) {}
+  bool operator()(const Member<SVGSMILElement>& a,
+                  const Member<SVGSMILElement>& b) {
+    // FIXME: This should also consider possible timing relations between the
+    // elements.
+    SMILTime a_begin = a->BeginTimeForPrioritization(elapsed_);
+    SMILTime b_begin = b->BeginTimeForPrioritization(elapsed_);
+    if (a_begin == b_begin)
+      return a->DocumentOrderIndex() < b->DocumentOrderIndex();
+    return a_begin < b_begin;
+  }
+  SMILTime elapsed_;
+};
+
+}  // namespace
+
 SMILAnimationSandwich::SMILAnimationSandwich() = default;
 
 void SMILAnimationSandwich::Schedule(SVGSMILElement* animation) {
