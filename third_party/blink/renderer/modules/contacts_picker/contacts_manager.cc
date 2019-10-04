189d91ef13749a932a7c15c641083ce5ab260dbf
Rayan Kanso <rayankans@chromium.org>
2019-10-04 11:22:47 +0000

    [Contacts] Update implementation to match spec changes.
    - Expose the `address` property and place it behind a flag.
    - Add & implement the getProperties method.
    
    Change-Id: If9bbebde6b8d076fd48b0b8a3584181e40adc1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1835621
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702810}

--- third_party/blink/renderer/modules/contacts_picker/contacts_manager.cc
+++ third_party/blink/renderer/modules/contacts_picker/contacts_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/contacts_picker/contacts_manager.h"
 
+#include "base/stl_util.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -65,8 +66,23 @@ TypeConverter<blink::ContactInfo*, blink::mojom::blink::ContactInfoPtr>::
 }  // namespace mojo
 
 namespace blink {
+namespace {
 
-ContactsManager::ContactsManager() = default;
+// ContactProperty enum strings.
+constexpr char kAddress[] = "address";
+constexpr char kEmail[] = "email";
+constexpr char kName[] = "name";
+constexpr char kTel[] = "tel";
+
+}  // namespace
+
+ContactsManager::ContactsManager() {
+  properties_ = {kEmail, kName, kTel};
+
+  if (RuntimeEnabledFeatures::ContactsManagerAddressesEnabled())
+    properties_.push_back(kAddress);
+}
+
 ContactsManager::~ContactsManager() = default;
 
 mojo::Remote<mojom::blink::ContactsManager>&
@@ -114,21 +130,30 @@ ScriptPromise ContactsManager::select(ScriptState* script_state,
                           "Contacts Picker is already in use."));
   }
 
-  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
-  ScriptPromise promise = resolver->Promise();
-
   bool include_names = false;
   bool include_emails = false;
   bool include_tel = false;
 
   for (const String& property : properties) {
-    if (property == "name")
+    if (!base::Contains(properties_, property)) {
+      return ScriptPromise::Reject(
+          script_state,
+          V8ThrowException::CreateTypeError(
+              script_state->GetIsolate(),
+              "The provided value '" + property +
+                  "' is not a valid enum value of type ContactProperty"));
+    }
+
+    if (property == kName)
       include_names = true;
-    else if (property == "email")
+    else if (property == kEmail)
       include_emails = true;
-    else if (property == "tel")
+    else if (property == kTel)
       include_tel = true;
   }
+
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
+  ScriptPromise promise = resolver->Promise();
 
   contact_picker_in_use_ = true;
   GetContactsManager(script_state)
@@ -165,6 +190,10 @@ void ContactsManager::OnContactsSelected(
     contacts_list.push_back(contact.To<blink::ContactInfo*>());
 
   resolver->Resolve(contacts_list);
+}
+
+ScriptPromise ContactsManager::getProperties(ScriptState* script_state) {
+  return ScriptPromise::Cast(script_state, ToV8(properties_, script_state));
 }
 
 }  // namespace blink
--- third_party/blink/renderer/modules/contacts_picker/contacts_manager.h
+++ third_party/blink/renderer/modules/contacts_picker/contacts_manager.h
@@ -31,6 +31,7 @@ class ContactsManager final : public ScriptWrappable {
   ScriptPromise select(ScriptState* script_state,
                        const Vector<String>& properties,
                        ContactsSelectOptions* options);
+  ScriptPromise getProperties(ScriptState* script_state);
 
  private:
   mojo::Remote<mojom::blink::ContactsManager>& GetContactsManager(
@@ -43,6 +44,7 @@ class ContactsManager final : public ScriptWrappable {
   // Created lazily.
   mojo::Remote<mojom::blink::ContactsManager> contacts_manager_;
   bool contact_picker_in_use_ = false;
+  Vector<String> properties_;
 };
 
 }  // namespace blink
