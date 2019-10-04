0ecda8ac5667ebbbad4a40ef38e7d4820b249796
Lei Zhang <thestig@chromium.org>
2019-10-04 21:18:19 +0000

    Make FileVersionInfoTest.NoVersionInfo 64-bit only.
    https://crrev.com/702886 checked in a 64-bit DLL for testing, which does
    not work on 32-bit bots. Temporarily mark the test 64-bit only, so the
    32-bit Windows bots go green.
    
    TBR=grt@chromium.org
    
    Bug: 1011439
    Change-Id: I0a64a10bfa785c4033bb2a626941d7ea42bac526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841657
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703039}

--- base/file_version_info_win_unittest.cc
+++ base/file_version_info_win_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/scoped_native_library.h"
 #include "base/strings/string_util.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::FilePath;
@@ -158,6 +159,9 @@ TYPED_TEST(FileVersionInfoTest, CustomProperties) {
             version_info_win->GetFileVersion());
 }
 
+#if defined(ARCH_CPU_64_BITS)
+// TODO(bug_1011439): Change no_version_info.dll to 32 bit, so this test will
+// work for 32-bit Windows as well.
 TYPED_TEST(FileVersionInfoTest, NoVersionInfo) {
   FilePath dll_path = GetTestDataPath();
   dll_path = dll_path.AppendASCII("no_version_info.dll");
@@ -165,3 +169,4 @@ TYPED_TEST(FileVersionInfoTest, NoVersionInfo) {
   TypeParam factory(dll_path);
   ASSERT_FALSE(factory.Create());
 }
+#endif
