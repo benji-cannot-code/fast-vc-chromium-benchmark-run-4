d766aeda074b39af548235039d5b0176a6b042e9
Fred Mello <fredmello@chromium.org>
2019-10-04 22:00:05 +0000

    Remove the need for srcjar_deps in module_installer
    Moved logic from ModuleInstallerConfig to BuildConfig (template).
    
    TBR=fredmello  #trivial refactor
    
    Bug: 1005802
    Change-Id: Iee0a409105c42bebac88d6d92ef227391697ff49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841694
    Commit-Queue: Fred Mello <fredmello@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703058}

--- components/module_installer/android/java/src/org/chromium/components/module_installer/engine/EngineFactory.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/engine/EngineFactory.java
@@ -5,15 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.module_installer.engine;
 
+import org.chromium.base.BuildConfig;
 import org.chromium.base.CommandLine;
-import org.chromium.components.module_installer.builder.ModuleInstallerConfig;
 
 /**
  * Factory used to build concrete engines.
  */
 public class EngineFactory {
     public InstallEngine getEngine() {
-        if (!ModuleInstallerConfig.IS_BUNDLE) {
+        if (!BuildConfig.IS_BUNDLE) {
             return new ApkEngine();
         }
         if (CommandLine.getInstance().hasSwitch("fake-feature-module-install")) {
