a5472a381dee4c07527d7fe06c5afcad54f61035
François Beaufort <beaufort.francois@gmail.com>
2019-10-04 13:46:36 +0000

    Roll src/third_party/dawn/ 3fd022ef6..21dfc9195 (2 commits)
    https://dawn.googlesource.com/dawn.git/+log/3fd022ef6041..21dfc919547f
    
    $ git log 3fd022ef6..21dfc9195 --date=short --no-merges --format='%ad %ae %s'
    2019-10-03 bryan.bernhart Resource Management 7: Device memory sub-allocation using buddy allocator.
    2019-10-03 beaufort.francois Add missing optional label member to descriptors
    
    Created with:
      roll-dep src/third_party/dawn
    
    Change-Id: I1011570dcc920cd005ad5bba2a5022f63b595fb5
    Bug: 877147
    Bug: dawn:22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1839691
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Auto-Submit: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#702849}

--- gpu/command_buffer/tests/webgpu_fence_unittest.cc
+++ gpu/command_buffer/tests/webgpu_fence_unittest.cc
@@ -57,12 +57,12 @@ TEST_F(WebGPUFenceTest, InitialValue) {
   dawn::Device device = dawn::Device::Acquire(webgpu()->GetDefaultDevice());
   dawn::Queue queue = device.CreateQueue();
   {
-    dawn::FenceDescriptor fence_desc{nullptr, 0};
+    dawn::FenceDescriptor fence_desc{nullptr, nullptr, 0};
     dawn::Fence fence = queue.CreateFence(&fence_desc);
     EXPECT_EQ(fence.GetCompletedValue(), 0u);
   }
   {
-    dawn::FenceDescriptor fence_desc{nullptr, 2};
+    dawn::FenceDescriptor fence_desc{nullptr, nullptr, 2};
     dawn::Fence fence = queue.CreateFence(&fence_desc);
     EXPECT_EQ(fence.GetCompletedValue(), 2u);
   }
@@ -76,7 +76,7 @@ TEST_F(WebGPUFenceTest, GetCompletedValue) {
   }
   dawn::Device device = dawn::Device::Acquire(webgpu()->GetDefaultDevice());
   dawn::Queue queue = device.CreateQueue();
-  dawn::FenceDescriptor fence_desc{nullptr, 0};
+  dawn::FenceDescriptor fence_desc{nullptr, nullptr, 0};
   dawn::Fence fence = queue.CreateFence(&fence_desc);
   queue.Signal(fence, 2u);
   WaitForFence(device, fence, 2u);
@@ -92,7 +92,7 @@ TEST_F(WebGPUFenceTest, OnCompletion) {
   }
   dawn::Device device = dawn::Device::Acquire(webgpu()->GetDefaultDevice());
   dawn::Queue queue = device.CreateQueue();
-  dawn::FenceDescriptor fence_desc{nullptr, 0};
+  dawn::FenceDescriptor fence_desc{nullptr, nullptr, 0};
   dawn::Fence fence = queue.CreateFence(&fence_desc);
   queue.Signal(fence, 2u);
 
@@ -111,7 +111,7 @@ TEST_F(WebGPUFenceTest, SignalManyTimes) {
   }
   dawn::Device device = dawn::Device::Acquire(webgpu()->GetDefaultDevice());
   dawn::Queue queue = device.CreateQueue();
-  dawn::FenceDescriptor fence_desc{nullptr, 0};
+  dawn::FenceDescriptor fence_desc{nullptr, nullptr, 0};
   dawn::Fence fence = queue.CreateFence(&fence_desc);
 
   uint64_t max_value = 1000000u;
