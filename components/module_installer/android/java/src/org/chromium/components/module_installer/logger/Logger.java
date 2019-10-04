64edfa0b497b74eef174e8f8912c3573cd191b8a
Fred Mello <fredmello@chromium.org>
2019-10-04 02:22:35 +0000

    Fix interface declaration for logging status
    Also renamed param from status to errorCode.
    
    Bug: 1005802
    Change-Id: Iee6991724fb10b9e30ca7919ae21e5a4829e3324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838415
    Commit-Queue: Fred Mello <fredmello@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702702}

--- components/module_installer/android/java/src/org/chromium/components/module_installer/logger/Logger.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/logger/Logger.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.components.module_installer.logger;
 
 import com.google.android.play.core.splitinstall.model.SplitInstallErrorCode;
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus;
 
 /**
  * Logger for SplitCompat Engine.
@@ -15,17 +16,17 @@ public interface Logger {
      * Logs exceptions that happen during module request.
      *
      * @param moduleName The module name.
-     * @param status The error code.
+     * @param errorCode The error code.
      */
-    void logRequestFailure(String moduleName, @SplitInstallErrorCode int status);
+    void logRequestFailure(String moduleName, @SplitInstallErrorCode int errorCode);
 
     /**
      * Logs exceptions that happen during the installation process.
      *
      * @param moduleName The module name.
-     * @param status The error code.
+     * @param errorCode The error code.
      */
-    void logStatusFailure(String moduleName, @SplitInstallErrorCode int status);
+    void logStatusFailure(String moduleName, @SplitInstallErrorCode int errorCode);
 
     /**
      * Logs the status count and duration during a module installation process.
@@ -33,7 +34,7 @@ public interface Logger {
      * @param moduleName The module name
      * @param status The status code
      */
-    void logStatus(String moduleName, @SplitInstallErrorCode int status);
+    void logStatus(String moduleName, @SplitInstallSessionStatus int status);
 
     /**
      * Logs the request start time.
