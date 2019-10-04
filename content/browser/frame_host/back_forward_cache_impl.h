6e37e639e765501a70903e76486577575b2a1a7e
sreejakshetty@chromium.org <sreejakshetty@google.com>
2019-10-04 12:23:29 +0000

    [bfcache] Domain specific controls for BackForwardCache
    We want to test storing only few domains in BackForwardCache.
    We should be able to configure the list via an experiment.
    
    Design Document:
    https://docs.google.com/document/d/16_ykRK9YHSo3DJqb48hpO67lpeYoG65E8g7TYFVv9Hs/edit?usp=sharing
    
    Change-Id: Icfdd2501cb94f405770e1e0a803c2310b931b396
    Bug: 1005735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1811120
    Reviewed-by: Lowell Manners <lowell@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702826}

--- content/browser/frame_host/back_forward_cache_impl.h
+++ content/browser/frame_host/back_forward_cache_impl.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/back_forward_cache.h"
 #include "content/public/browser/global_routing_id.h"
+#include "url/gurl.h"
 
 namespace content {
 
@@ -140,7 +141,6 @@ class CONTENT_EXPORT BackForwardCacheImpl : public BackForwardCache {
     task_runner_for_testing_ = task_runner;
   }
 
-
   // Sets the number of documents that can be stored in the cache. This is meant
   // for use from within tests only.
   // If |cache_size_limit_for_testing| is 0 (the default), the normal cache
@@ -163,6 +163,11 @@ class CONTENT_EXPORT BackForwardCacheImpl : public BackForwardCache {
       RenderFrameHostImpl* render_frame_host,
       uint64_t disallowed_features);
 
+  // Checks if the url's host and path matches with the |allowed_urls_| host and
+  // path. This is controlled by "allowed_websites" param on BackForwardCache
+  // feature and if the param is not set, it will allow all websites by default.
+  bool IsAllowed(const GURL& current_url);
+
   // Contains the set of stored Entries.
   // Invariant:
   // - Ordered from the most recently used to the last recently used.
@@ -180,6 +185,14 @@ class CONTENT_EXPORT BackForwardCacheImpl : public BackForwardCache {
   // Only used for tests. This task runner is used for precise injection in
   // browser tests and for timing control.
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_for_testing_;
+
+  // To enter the back-forward cache, the main document URL's must match one of
+  // the field trial parameter "allowed_websites". This is represented here by a
+  // set of host and path prefix.
+  std::map<std::string,              // URL's host,
+           std::vector<std::string>  // URL's path prefix
+           >
+      allowed_urls_;
 
   base::WeakPtrFactory<BackForwardCacheImpl> weak_factory_;
 
--- content/browser/frame_host/back_forward_cache_metrics.h
+++ content/browser/frame_host/back_forward_cache_metrics.h
@@ -41,7 +41,8 @@ class BackForwardCacheMetrics
     kLoading,
     kWasGrantedMediaAccess,
     kBlocklistedFeatures,
-    kDisableForRenderFrameHostCalled
+    kDisableForRenderFrameHostCalled,
+    kDomainNotAllowed
   };
 
   // Please keep in sync with BackForwardCacheHistoryNavigationOutcome in
