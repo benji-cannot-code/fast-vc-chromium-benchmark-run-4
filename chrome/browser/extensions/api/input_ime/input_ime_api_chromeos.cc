690bd1f81e80d00b38cf863e47a5e8c4a9e57fc0
Darren Shen <shend@chromium.org>
2019-10-04 06:26:20 +0000

    imf: Use unique_ptr for managing engine lifetimes.
    We currently manually manage the lifetime of the engines in engine map.
    But we can use a unique_ptr instead. It automatically gets released
    when we remove an engine from the engine map.
    
    Bug: 1009903
    Change-Id: I93874b0154b949ddceef29695fc000cacfef73cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1837443
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#702745}

--- chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.cc
@@ -445,12 +445,13 @@ bool InputImeEventRouter::RegisterImeExtension(
 
   std::unique_ptr<InputMethodEngineBase::Observer> observer(
       new ImeObserverChromeOS(extension_id, profile));
-  auto* engine = new chromeos::InputMethodEngine();
+  auto engine = std::make_unique<chromeos::InputMethodEngine>();
   engine->Initialize(std::move(observer), extension_id.c_str(), profile);
-  engine_map_[extension_id] = engine;
+  engine_map_[extension_id] = std::move(engine);
   chromeos::UserSessionManager::GetInstance()
       ->GetDefaultIMEState(profile)
-      ->AddInputMethodExtension(extension_id, descriptors, engine);
+      ->AddInputMethodExtension(extension_id, descriptors,
+                                engine_map_[extension_id].get());
 
   return true;
 }
@@ -461,7 +462,6 @@ void InputImeEventRouter::UnregisterAllImes(const std::string& extension_id) {
     chromeos::input_method::InputMethodManager::Get()
         ->GetActiveIMEState()
         ->RemoveInputMethodExtension(extension_id);
-    delete it->second;
     engine_map_.erase(it);
   }
 }
@@ -469,13 +469,13 @@ void InputImeEventRouter::UnregisterAllImes(const std::string& extension_id) {
 InputMethodEngine* InputImeEventRouter::GetEngine(
     const std::string& extension_id) {
   auto it = engine_map_.find(extension_id);
-  return (it != engine_map_.end()) ? it->second : nullptr;
+  return (it != engine_map_.end()) ? it->second.get() : nullptr;
 }
 
 InputMethodEngineBase* InputImeEventRouter::GetEngineIfActive(
     const std::string& extension_id) {
   auto it = engine_map_.find(extension_id);
-  return (it != engine_map_.end() && it->second->IsActive()) ? it->second
+  return (it != engine_map_.end() && it->second->IsActive()) ? it->second.get()
                                                              : nullptr;
 }
 
