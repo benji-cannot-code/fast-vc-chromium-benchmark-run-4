7209e81cc2edc286053b05ff4256da72bc58f99a
David Tseng <dtseng@chromium.org>
2019-10-05 01:08:28 +0000

    Add proper bindings to Aria row and column index based on AXTableInfo
    Change-Id: I9bfbf38f64f0a498e71c5cf38ec5908f2b3de03c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827469
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703117}

--- extensions/renderer/resources/automation/automation_node.js
+++ extensions/renderer/resources/automation/automation_node.js
@@ -451,6 +451,20 @@ var GetTableCellColumnIndex = natives.GetTableCellColumnIndex;
 var GetTableCellRowIndex = natives.GetTableCellRowIndex;
 
 /**
+ * @param {string} axTreeID The id of the accessibility tree.
+ * @param {number} nodeID The id of a node.
+ * @return {number} Column index for this cell.
+ */
+var GetTableCellAriaColumnIndex = natives.GetTableCellAriaColumnIndex;
+
+/**
+ * @param {string} axTreeID The id of the accessibility tree.
+ * @param {number} nodeID The id of a node.
+ * @return {number} Row index for this cell.
+ */
+var GetTableCellAriaRowIndex = natives.GetTableCellAriaRowIndex;
+
+/**
  * @param {string} axTreeId The id of the accessibility tree.
  * @param {number} nodeID The id of a node.
  * @return {string} Detected language for this node.
@@ -720,6 +734,15 @@ AutomationNodeImpl.prototype = {
 
   get tableCellRowIndex() {
     return GetTableCellRowIndex(this.treeID, this.id);
+  },
+
+
+  get tableCellAriaColumnIndex() {
+    return GetTableCellAriaColumnIndex(this.treeID, this.id);
+  },
+
+  get tableCellAriaRowIndex() {
+    return GetTableCellAriaRowIndex(this.treeID, this.id);
   },
 
   get nonInlineTextWordStarts() {
@@ -1163,9 +1186,7 @@ var intAttributes = [
     'scrollYMax',
     'scrollYMin',
     'setSize',
-    'ariaCellColumnIndex',
     'tableCellColumnSpan',
-    'ariaCellRowIndex',
     'tableCellRowSpan',
     'tableColumnCount',
     'ariaColumnCount',
@@ -1740,6 +1761,8 @@ utils.expose(AutomationNode, AutomationNodeImpl, {
         'tableCellRowHeaders',
         'tableCellColumnIndex',
         'tableCellRowIndex',
+        'tableCellAriaRowIndex',
+        'tableCellAriaColumnIndex',
         'nonInlineTextWordStarts',
         'nonInlineTextWordEnds',
       ]),
