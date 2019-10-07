da1870ad5f316507126ea41814a554258881bc23
Julian Watson <juwa@google.com>
2019-10-07 23:54:09 +0000

    crostini: use ContainerId instead of pair/tuple
    Change-Id: I7be1eae82702d0576e6bcd701c985be97724c7dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1839279
    Commit-Queue: Julian Watson <juwa@google.com>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Auto-Submit: Julian Watson <juwa@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703537}

--- chrome/browser/chromeos/crostini/crostini_package_notification_unittest.cc
+++ chrome/browser/chromeos/crostini/crostini_package_notification_unittest.cc
@@ -67,8 +67,7 @@ TEST_F(CrostiniPackageNotificationTest, InstallWithNoIcons) {
       profile_.get(),
       CrostiniPackageNotification::NotificationType::PACKAGE_INSTALL,
       PackageOperationStatus::RUNNING,
-      std::pair<std::string, std::string>(kCrostiniDefaultVmName,
-                                          kCrostiniDefaultContainerName),
+      ContainerId(kCrostiniDefaultVmName, kCrostiniDefaultContainerName),
       base::string16(), kNotificationId, service_.get());
 
   notification.UpdateProgress(PackageOperationStatus::SUCCEEDED, 100);
@@ -80,8 +79,7 @@ TEST_F(CrostiniPackageNotificationTest, InstallWithOneIcon) {
       profile_.get(),
       CrostiniPackageNotification::NotificationType::PACKAGE_INSTALL,
       PackageOperationStatus::RUNNING,
-      std::pair<std::string, std::string>(kCrostiniDefaultVmName,
-                                          kCrostiniDefaultContainerName),
+      ContainerId(kCrostiniDefaultVmName, kCrostiniDefaultContainerName),
       base::string16(), kNotificationId, service_.get());
 
   auto app = CrostiniTestHelper::BasicApp(kDefaultAppFileId);
@@ -96,8 +94,7 @@ TEST_F(CrostiniPackageNotificationTest, InstallWithTwoIcons) {
       profile_.get(),
       CrostiniPackageNotification::NotificationType::PACKAGE_INSTALL,
       PackageOperationStatus::RUNNING,
-      std::pair<std::string, std::string>(kCrostiniDefaultVmName,
-                                          kCrostiniDefaultContainerName),
+      ContainerId(kCrostiniDefaultVmName, kCrostiniDefaultContainerName),
       base::string16(), kNotificationId, service_.get());
 
   auto app = CrostiniTestHelper::BasicApp(kDefaultAppFileId);
@@ -118,8 +115,7 @@ TEST_F(CrostiniPackageNotificationTest, InstallIgnorePreviousIcons) {
       profile_.get(),
       CrostiniPackageNotification::NotificationType::PACKAGE_INSTALL,
       PackageOperationStatus::RUNNING,
-      std::pair<std::string, std::string>(kCrostiniDefaultVmName,
-                                          kCrostiniDefaultContainerName),
+      ContainerId(kCrostiniDefaultVmName, kCrostiniDefaultContainerName),
       base::string16(), kNotificationId, service_.get());
 
   app = CrostiniTestHelper::BasicApp(kSecondAppFileId);
