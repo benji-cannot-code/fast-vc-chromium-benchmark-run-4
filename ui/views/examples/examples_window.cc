c2b30a7c90cf598e93cc73297619833ba9224a03
Elly Fong-Jones <ellyjones@chromium.org>
2019-10-07 19:48:26 +0000

    views: remove AnimatedImageView
    As far as I can tell this class has never had a user in chromium.
    
    This change also removes the ImageViewBase abstraction layer, which
    was added to support AnimatedImageView.
    
    Bug: None
    Change-Id: I9af55bfb0ef1b7a07a2d5a1db234b5e998a66c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1842037
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703416}

--- ui/views/examples/examples_window.cc
+++ ui/views/examples/examples_window.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/background.h"
 #include "ui/views/controls/combobox/combobox.h"
 #include "ui/views/controls/label.h"
-#include "ui/views/examples/animated_image_view_example.h"
 #include "ui/views/examples/box_layout_example.h"
 #include "ui/views/examples/bubble_example.h"
 #include "ui/views/examples/button_example.h"
@@ -63,7 +62,6 @@ namespace {
 // Creates the default set of examples.
 ExampleVector CreateExamples() {
   ExampleVector examples;
-  examples.push_back(std::make_unique<AnimatedImageViewExample>());
   examples.push_back(std::make_unique<BoxLayoutExample>());
   examples.push_back(std::make_unique<BubbleExample>());
   examples.push_back(std::make_unique<ButtonExample>());
