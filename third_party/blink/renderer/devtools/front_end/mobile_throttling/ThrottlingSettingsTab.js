aabe6a6f61478b2636d5256748060a23d2385e6f
Chandani Shrestha <chshrest@microsoft.com>
2019-10-07 21:20:34 +0000

    DevTools: Remove extra tab in Settings Throttling Pane
    Without this change when user navigates back by pressing SHIFT TAB from inputs in throttling settings pane.
    Tab stops at whole throttling settings pane. Again, pressing SHIFT TAB it takes to 'Throttling' in Settings then, pressing TAB it lands in Throttling Pane content section instead of landing on first control 'Add custom profile button'.
    This change removes that extra TAB so that, when user presses TAB it lands only in interactive controls and satisfies below given rule:
    - Don't allow users to tab to text elements (such as labels) or other static content.
    More info: https://accessibilityinsights.io/docs/en/windows/reference/tabstops
    
    Bug: 963183
    Change-Id: I100ccc0b541f2eb8a077a7fdb63c6f594dbc6c4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1817412
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: John Emau <John.Emau@microsoft.com>
    Commit-Queue: Chandani Shrestha <chshrest@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#703472}

--- third_party/blink/renderer/devtools/front_end/mobile_throttling/ThrottlingSettingsTab.js
+++ third_party/blink/renderer/devtools/front_end/mobile_throttling/ThrottlingSettingsTab.js
@@ -29,7 +29,6 @@ MobileThrottling.ThrottlingSettingsTab = class extends UI.VBox {
     this._customSetting.addChangeListener(this._conditionsUpdated, this);
 
     this.setDefaultFocusedElement(addButton);
-    this.contentElement.tabIndex = 0;
   }
 
   /**
