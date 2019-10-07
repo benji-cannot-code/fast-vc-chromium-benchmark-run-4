eb250e8dfe8b9afb928912a8170be2ce189af6b7
Jinho Bang <jinho.bang@samsung.com>
2019-10-07 18:10:59 +0000

    WebGPU: Rename PipelineStageDescriptor to ProgrammableStageDescriptor
    This is to match the WebGPU's WebIDL[1].
    
    [1] https://github.com/gpuweb/gpuweb/pull/359
    
    Bug: 877147
    Change-Id: Ia5fedae01348411f5e1c59faebe5c13a3dd85f91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1843791
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#703362}

--- third_party/blink/renderer/modules/webgpu/dawn_conversions.h
+++ third_party/blink/renderer/modules/webgpu/dawn_conversions.h
@@ -22,7 +22,7 @@ namespace blink {
 
 class DoubleSequenceOrGPUColorDict;
 class GPUColorDict;
-class GPUPipelineStageDescriptor;
+class GPUProgrammableStageDescriptor;
 class UnsignedLongSequenceOrGPUExtent3DDict;
 class UnsignedLongSequenceOrGPUOrigin3DDict;
 
@@ -45,9 +45,10 @@ DawnColor AsDawnType(const DoubleSequenceOrGPUColorDict*);
 DawnExtent3D AsDawnType(const UnsignedLongSequenceOrGPUExtent3DDict*);
 DawnOrigin3D AsDawnType(const UnsignedLongSequenceOrGPUOrigin3DDict*);
 
-using OwnedPipelineStageDescriptor =
+using OwnedProgrammableStageDescriptor =
     std::tuple<DawnPipelineStageDescriptor, std::unique_ptr<char[]>>;
-OwnedPipelineStageDescriptor AsDawnType(const GPUPipelineStageDescriptor*);
+OwnedProgrammableStageDescriptor AsDawnType(
+    const GPUProgrammableStageDescriptor*);
 
 // WebGPU objects are converted to Dawn objects by getting the opaque handle
 // which can be passed to Dawn.
