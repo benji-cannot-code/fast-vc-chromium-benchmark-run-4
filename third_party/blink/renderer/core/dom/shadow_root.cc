c03f11c3f6568fb1b6334843c5625b63f4c140d2
Rune Lillesveen <futhark@chromium.org>
2019-10-07 14:05:28 +0000

    Skip ShadowRoot::RecalcStyle for FlatTreeStyleRecalc.
    The ShadowRoot does not take part in dirty marking with the
    FlatTreeStyleRecalc runtime flag enabled. We can just skip directly to
    RecalcDescendants(). Since ShadowRoot was not marked with
    ChildNeedsStyleRecalc we would otherwise not traverse into the shadow
    root children via ShadowRoot::RecalcStyle().
    
    Bug: 972752
    Change-Id: I5a3debfa25d1d431e5dab43729c843592d3c6a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841339
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703288}

--- third_party/blink/renderer/core/dom/shadow_root.cc
+++ third_party/blink/renderer/core/dom/shadow_root.cc
@@ -142,8 +142,7 @@ void ShadowRoot::setInnerHTML(const StringOrTrustedHTML& stringOrHtml,
 void ShadowRoot::RecalcStyle(const StyleRecalcChange change) {
   // ShadowRoot doesn't support custom callbacks.
   DCHECK(!HasCustomStyleCallbacks());
-  DCHECK(!NeedsStyleRecalc() ||
-         !RuntimeEnabledFeatures::FlatTreeStyleRecalcEnabled());
+  DCHECK(!RuntimeEnabledFeatures::FlatTreeStyleRecalcEnabled());
 
   StyleRecalcChange child_change = change;
   if (GetStyleChangeType() == kSubtreeStyleChange)
