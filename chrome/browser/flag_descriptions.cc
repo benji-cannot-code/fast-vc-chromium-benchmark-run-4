f7ea678c236c9dbd11a1f83a565fb357ca92ccfb
Dan Harrington <harringtond@chromium.org>
2019-10-07 20:41:50 +0000

    Hard-code feature NewNetErrorPageUI to enabled.
    Bug: 932256
    
    Change-Id: I0621e289ba149b619f3c97905d7abec371e6821d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1667548
    Reviewed-by: Jian Li <jianli@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703445}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -2551,10 +2551,6 @@ const char kManualPasswordGenerationAndroidDescription[] =
     "Whether Chrome should offer users the option to manually request to "
     "generate passwords on Android.";
 
-const char kNewNetErrorPageUIName[] = "Enable new UI for net-error page";
-const char kNewNetErrorPageUIDescription[] =
-    "Enables showing available offline content on the net-error (Dino) page.";
-
 const char kNewPhotoPickerName[] = "Enable new Photopicker";
 const char kNewPhotoPickerDescription[] =
     "Activates the new picker for selecting photos.";
