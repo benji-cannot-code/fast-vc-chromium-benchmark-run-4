a981d457854b5ec0923bf6d5cc7401513676ed1d
Tibor Goldschwendt <tiborg@chromium.org>
2019-10-07 21:12:59 +0000

    [android] Load native resources in //component/module_installer
    The native resources are loaded centrally when calling Module.getImpl()
    so that all modules can reuse the same infrastructure. Currently, the
    modules with native resources are whitelisted until we have a solution
    that automatically determines whether a module has native resources.
    
    + Make test dummy use the new infrastructure.
    
    Bug: 1011943
    Change-Id: Ia47de6eafdae1700e948208427d3c68e3086308a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838915
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703468}

--- chrome/android/modules/test_dummy/provider/java/src/org/chromium/chrome/modules/test_dummy/TestDummyModuleProvider.java
+++ chrome/android/modules/test_dummy/provider/java/src/org/chromium/chrome/modules/test_dummy/TestDummyModuleProvider.java
@@ -5,19 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.modules.test_dummy;
 
-import org.chromium.base.ThreadUtils;
-import org.chromium.base.annotations.JNINamespace;
-import org.chromium.base.annotations.NativeMethods;
 import org.chromium.components.module_installer.engine.InstallListener;
 
 /** Installs and loads the test dummy module. */
-@JNINamespace("test_dummy")
 public class TestDummyModuleProvider {
-    private static boolean sIsModuleLoaded;
-
     /** Returns true if the module is installed. */
     public static boolean isModuleInstalled() {
-        ThreadUtils.assertOnUiThread();
         return TestDummyModule.isInstalled();
     }
 
@@ -26,11 +19,10 @@ public class TestDummyModuleProvider {
      *
      * Can only be called if the module is not installed.
      *
-     * @param onFinished Called when the install has finished.
+     * @param listener Called when the install has finished.
      */
-    public static void installModule(InstallListener onFinished) {
-        ThreadUtils.assertOnUiThread();
-        TestDummyModule.install(onFinished);
+    public static void installModule(InstallListener listener) {
+        TestDummyModule.install(listener);
     }
 
     /**
@@ -40,17 +32,6 @@ public class TestDummyModuleProvider {
      * call.
      */
     public static TestDummyProvider getTestDummyProvider() {
-        ThreadUtils.assertOnUiThread();
-        assert isModuleInstalled();
-        if (!sIsModuleLoaded) {
-            TestDummyModuleProviderJni.get().loadNative();
-            sIsModuleLoaded = true;
-        }
         return TestDummyModule.getImpl();
-    }
-
-    @NativeMethods
-    interface Natives {
-        void loadNative();
     }
 }
