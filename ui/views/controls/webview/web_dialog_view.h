e4cf2dc5868fe73a0f945f49a183cb00e678ee9e
Wei Li <weili@chromium.org>
2019-10-07 19:34:51 +0000

    [HaTS] Don't show survey when resource fails to load
    There are cases that HaTS javascript library may not be loaded
    successfully, for example, when the browser is offline. In this
    case, we monitor the resource load complete call to make sure HaTS
    library is loaded. Otherwise, we don't proceed with showing the
    survey.
    
    BUG=1006674
    
    Change-Id: Ibcd725728978ce96fbbe263fb0c5d5c894d1ced9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1826291
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703407}

--- ui/views/controls/webview/web_dialog_view.h
+++ ui/views/controls/webview/web_dialog_view.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 class BrowserContext;
 class RenderFrameHost;
+struct GlobalRequestID;
 }
 
 namespace views {
@@ -40,6 +41,10 @@ class ObservableWebView : public WebView {
   // content::WebContentsObserver
   void DidFinishLoad(content::RenderFrameHost* render_frame_host,
                      const GURL& validated_url) override;
+  void ResourceLoadComplete(
+      content::RenderFrameHost* render_frame_host,
+      const content::GlobalRequestID& request_id,
+      const content::mojom::ResourceLoadInfo& resource_load_info) override;
 
  private:
   ui::WebDialogDelegate* delegate_;
