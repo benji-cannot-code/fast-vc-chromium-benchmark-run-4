c88861dc6bfb7eb3ac226d3a3667531fb1a6950d
Peter Kasting <pkasting@chromium.org>
2019-10-07 19:02:33 +0000

    Remove using directives ("using namespace x") from chrome/browser/extensions/.
    Bug: 82078
    Change-Id: Ic2152a5bc8464df9354dcd158a72d6f5168d6030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844338
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703387}

--- chrome/browser/extensions/external_provider_impl_unittest.cc
+++ chrome/browser/extensions/external_provider_impl_unittest.cc
@@ -45,15 +45,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/scoped_user_manager.h"
 #endif
 
-using ::testing::NotNull;
-using ::testing::Return;
-using ::testing::_;
-
 namespace extensions {
 
 namespace {
-
-using namespace net::test_server;
 
 const char kManifestPath[] = "/update_manifest";
 const char kAppPath[] = "/app.crx";
@@ -100,7 +94,7 @@ class ExternalProviderImplTest : public ExtensionServiceTestBase {
   // ExtensionServiceTestBase overrides:
   void SetUp() override {
     ExtensionServiceTestBase::SetUp();
-    test_server_.reset(new EmbeddedTestServer());
+    test_server_ = std::make_unique<net::test_server::EmbeddedTestServer>();
 
     test_server_->RegisterRequestHandler(
         base::Bind(&ExternalProviderImplTest::HandleRequest,
@@ -114,10 +108,11 @@ class ExternalProviderImplTest : public ExtensionServiceTestBase {
   }
 
  private:
-  std::unique_ptr<HttpResponse> HandleRequest(const HttpRequest& request) {
+  std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
+      const net::test_server::HttpRequest& request) {
     GURL url = test_server_->GetURL(request.relative_url);
     if (url.path() == kManifestPath) {
-      std::unique_ptr<BasicHttpResponse> response(new BasicHttpResponse);
+      auto response = std::make_unique<net::test_server::BasicHttpResponse>();
       response->set_code(net::HTTP_OK);
       response->set_content(base::StringPrintf(
           "<?xml version='1.0' encoding='UTF-8'?>\n"
@@ -139,7 +134,7 @@ class ExternalProviderImplTest : public ExtensionServiceTestBase {
       base::ReadFileToString(
           test_data_dir.AppendASCII("extensions/dummyiap.crx"),
           &contents);
-      std::unique_ptr<BasicHttpResponse> response(new BasicHttpResponse);
+      auto response = std::make_unique<net::test_server::BasicHttpResponse>();
       response->set_code(net::HTTP_OK);
       response->set_content(contents);
       return std::move(response);
@@ -148,7 +143,7 @@ class ExternalProviderImplTest : public ExtensionServiceTestBase {
     return nullptr;
   }
 
-  std::unique_ptr<EmbeddedTestServer> test_server_;
+  std::unique_ptr<net::test_server::EmbeddedTestServer> test_server_;
   std::unique_ptr<ExtensionCacheFake> test_extension_cache_;
 
 #if defined(OS_CHROMEOS)
