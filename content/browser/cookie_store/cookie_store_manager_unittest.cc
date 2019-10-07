3d03a31d8c3914fb10c3827168d55ec8c549d926
Victor Costan <pwnall@chromium.org>
2019-10-07 21:18:28 +0000

    Cookie Store: Remove instance name from unittests.
    This CL removes a redundant "CookieStoreManagerTest/" prefix from all
    unit test names. For example,
    CookieStoreManagerTest/CookieStoreManagerTest.OneCookieChange/0 becomes
    CookieStoreManagerTest.OneCookieChange/0.
    
    Change-Id: I9f07ad0c322c55a7d26e5d02a16fb52aa1350844
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844152
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Staphany Park <staphany@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703470}

--- content/browser/cookie_store/cookie_store_manager_unittest.cc
+++ content/browser/cookie_store/cookie_store_manager_unittest.cc
@@ -969,7 +969,7 @@ TEST_P(CookieStoreManagerTest, GetSubscriptionsFromWrongOrigin) {
   EXPECT_FALSE(wrong_subscriptions_opt.has_value());
 }
 
-INSTANTIATE_TEST_SUITE_P(CookieStoreManagerTest,
+INSTANTIATE_TEST_SUITE_P(,
                          CookieStoreManagerTest,
                          testing::Bool() /* reset_storage_during_test */);
 
