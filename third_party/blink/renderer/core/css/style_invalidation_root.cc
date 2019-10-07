5005fb8eb4be0fd8604e1e113aad977b499f0fd1
Rune Lillesveen <futhark@chromium.org>
2019-10-07 15:13:28 +0000

    Clear removed style traversal root ancestors correctly.
    Since Document and ShadowRoot do not have dirty-bits set for layout tree
    rebuilding, and for style recalc with FlatTreeStyleRecalc enabled, the
    parent node in StyleTraversalRoot::Update might not be IsChildDirty().
    
    Instead, move the DCHECKs to ClearChildDirtyForAncestors in the
    respective implementations.
    
    There was no known issue for this for LayoutTreeRebuild root, most
    likely because the tree does not change between Recalc, which sets the
    layout tree rebuild root, and RebuildLayoutTree. But, for
    StyleTraversalRoot there were a bunch of web_tests triggering DCHECKs
    when FlatTreeStyleRecalc is enabled.
    
    Bug: 972752
    Change-Id: I258564f7eee6e155b8b4bbffaa286222cd98b6a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841453
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703305}

--- third_party/blink/renderer/core/css/style_invalidation_root.cc
+++ third_party/blink/renderer/core/css/style_invalidation_root.cc
@@ -38,8 +38,9 @@ void StyleInvalidationRoot::ClearChildDirtyForAncestors(
     ContainerNode& parent) const {
   for (Node* ancestor = &parent; ancestor;
        ancestor = ancestor->ParentOrShadowHostNode()) {
-    ancestor->ClearChildNeedsStyleInvalidation();
+    DCHECK(ancestor->ChildNeedsStyleInvalidation());
     DCHECK(!ancestor->NeedsStyleInvalidation());
+    ancestor->ClearChildNeedsStyleInvalidation();
   }
 }
 
