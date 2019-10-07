d25d53c3a87f71888c406262a5fbd10ce80f686f
Kazuki Takise <takise@google.com>
2019-10-07 02:03:03 +0000

    Add visible property to GetArcAppWindowInfo
    Bug: b/142095503
    Bug: b/141899229
    Test: tast -verbose run my_crbook arc.PIP
    Change-Id: I259dfc8f8b1c1ffaaa24ff2fd274dba65a9784c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838840
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703196}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
@@ -2481,8 +2481,9 @@ AutotestPrivateGetArcAppWindowInfoFunction::Run() {
 
   auto result = std::make_unique<base::DictionaryValue>();
   result->SetDictionary("bounds", std::move(bounds_dict));
-  result->SetBoolean("is_animating", is_animating);
   result->SetString("display_id", base::NumberToString(display_id));
+  result->SetBoolean("is_animating", is_animating);
+  result->SetBoolean("is_visible", arc_window->IsVisible());
 
   return RespondNow(OneArgument(std::move(result)));
 }
