bcd172b23188037711c9b50be14ac8b6064e4b7c
Peter Kasting <pkasting@chromium.org>
2019-10-07 22:15:46 +0000

    Allow device chooser table to handle clicks.
    Originally, when the table was visible, the device chooser had three children: a
    scroll view containiner the table, and two invisible centered labels.  In
    https://chromium-review.googlesource.com/c/chromium/src/+/1837079 I reworked
    this to center the labels using anonymous views with layout managers.  This
    meant that the device chooser now contained three visible full-size children
    (which, in two cases, just contained an invisible centered child).  As a result
    the last child (which was highest in Z order) became the event handler at all
    times, and while it would bubble events to its parent, there was no way for them
    to get over to the sibling scroll view.
    
    The solution is to toggle the visibility of these centering views, rather than
    toggling the visibility of their contained labels.  This causes event targeting
    to select the scroll view again.
    
    Also does various additional cleanup I noticed while working on this.
    
    Bug: 1011536
    Change-Id: I27c496ebf4899b94bd442a1eaeb5e6ddbca387a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1843294
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703497}

--- chrome/browser/ui/views/device_chooser_content_view_unittest.cc
+++ chrome/browser/ui/views/device_chooser_content_view_unittest.cc
@@ -49,7 +49,8 @@ class DeviceChooserContentViewTest : public ChromeViewsTestBase {
     extra_views_container_ = content_view().CreateExtraView();
 
     ASSERT_NE(nullptr, table_view());
-    ASSERT_NE(nullptr, adapter_off_help_link());
+    ASSERT_NE(nullptr, no_options_view());
+    ASSERT_NE(nullptr, adapter_off_view());
     ASSERT_NE(nullptr, re_scan_button());
     ASSERT_NE(nullptr, throbber());
     ASSERT_NE(nullptr, scanning_label());
@@ -62,21 +63,19 @@ class DeviceChooserContentViewTest : public ChromeViewsTestBase {
   MockTableViewObserver& table_observer() { return *table_observer_; }
   DeviceChooserContentView& content_view() { return *content_view_; }
 
-  views::TableView* table_view() { return content_view().table_view_; }
+  views::TableView* table_view() {
+    return content_view().table_view_for_testing();
+  }
   views::View* table_parent() { return content_view().table_parent_; }
   ui::TableModel* table_model() { return table_view()->model(); }
-  views::Label* no_options_label() { return content_view().no_options_help_; }
-  views::StyledLabel* adapter_off_help_link() {
-    return content_view().adapter_off_help_;
-  }
+  views::View* no_options_view() { return content_view().no_options_view_; }
+  views::View* adapter_off_view() { return content_view().adapter_off_view_; }
   views::LabelButton* re_scan_button() {
-    return content_view().bluetooth_status_container_->re_scan_button();
+    return content_view().ReScanButtonForTesting();
   }
-  views::Throbber* throbber() {
-    return content_view().bluetooth_status_container_->throbber();
-  }
+  views::Throbber* throbber() { return content_view().ThrobberForTesting(); }
   views::Label* scanning_label() {
-    return content_view().bluetooth_status_container_->scanning_label();
+    return content_view().ScanningLabelForTesting();
   }
 
   void AddUnpairedDevice() {
@@ -106,7 +105,7 @@ class DeviceChooserContentViewTest : public ChromeViewsTestBase {
   bool IsDeviceSelected() { return !table_view()->selection_model().empty(); }
 
   void ExpectNoDevices() {
-    EXPECT_TRUE(no_options_label()->GetVisible());
+    EXPECT_TRUE(no_options_view()->GetVisible());
     EXPECT_EQ(0, table_view()->GetRowCount());
     // The table should be disabled since there are no (real) options.
     EXPECT_FALSE(table_parent()->GetVisible());
@@ -129,7 +128,7 @@ TEST_F(DeviceChooserContentViewTest, InitialState) {
   EXPECT_CALL(table_observer(), OnSelectionChanged()).Times(0);
 
   ExpectNoDevices();
-  EXPECT_FALSE(adapter_off_help_link()->GetVisible());
+  EXPECT_FALSE(adapter_off_view()->GetVisible());
   EXPECT_FALSE(throbber()->GetVisible());
   EXPECT_FALSE(scanning_label()->GetVisible());
   EXPECT_TRUE(re_scan_button()->GetVisible());
@@ -211,8 +210,8 @@ TEST_F(DeviceChooserContentViewTest, TurnBluetoothOffAndOn) {
       FakeBluetoothChooserController::BluetoothStatus::UNAVAILABLE);
 
   EXPECT_FALSE(table_parent()->GetVisible());
-  EXPECT_FALSE(no_options_label()->GetVisible());
-  EXPECT_TRUE(adapter_off_help_link()->GetVisible());
+  EXPECT_FALSE(no_options_view()->GetVisible());
+  EXPECT_TRUE(adapter_off_view()->GetVisible());
   EXPECT_FALSE(throbber()->GetVisible());
   EXPECT_FALSE(scanning_label()->GetVisible());
   EXPECT_TRUE(re_scan_button()->GetVisible());
@@ -222,7 +221,7 @@ TEST_F(DeviceChooserContentViewTest, TurnBluetoothOffAndOn) {
   controller()->SetBluetoothStatus(
       FakeBluetoothChooserController::BluetoothStatus::IDLE);
   ExpectNoDevices();
-  EXPECT_FALSE(adapter_off_help_link()->GetVisible());
+  EXPECT_FALSE(adapter_off_view()->GetVisible());
   EXPECT_FALSE(throbber()->GetVisible());
   EXPECT_FALSE(scanning_label()->GetVisible());
   EXPECT_TRUE(re_scan_button()->GetVisible());
@@ -234,7 +233,7 @@ TEST_F(DeviceChooserContentViewTest, ScanForDevices) {
       FakeBluetoothChooserController::BluetoothStatus::SCANNING);
   EXPECT_EQ(0, table_view()->GetRowCount());
   EXPECT_FALSE(table_view()->GetEnabled());
-  EXPECT_FALSE(adapter_off_help_link()->GetVisible());
+  EXPECT_FALSE(adapter_off_view()->GetVisible());
   EXPECT_TRUE(throbber()->GetVisible());
   EXPECT_TRUE(scanning_label()->GetVisible());
   EXPECT_FALSE(re_scan_button()->GetVisible());
@@ -242,7 +241,7 @@ TEST_F(DeviceChooserContentViewTest, ScanForDevices) {
   AddUnpairedDevice();
   EXPECT_EQ(1, table_view()->GetRowCount());
   EXPECT_TRUE(table_view()->GetEnabled());
-  EXPECT_FALSE(adapter_off_help_link()->GetVisible());
+  EXPECT_FALSE(adapter_off_view()->GetVisible());
   EXPECT_TRUE(throbber()->GetVisible());
   EXPECT_TRUE(scanning_label()->GetVisible());
   EXPECT_FALSE(IsDeviceSelected());
@@ -251,7 +250,8 @@ TEST_F(DeviceChooserContentViewTest, ScanForDevices) {
 
 TEST_F(DeviceChooserContentViewTest, ClickAdapterOffHelpLink) {
   EXPECT_CALL(*controller(), OpenAdapterOffHelpUrl()).Times(1);
-  adapter_off_help_link()->LinkClicked(nullptr, 0);
+  static_cast<views::StyledLabel*>(adapter_off_view()->children().front())
+      ->LinkClicked(nullptr, 0);
 }
 
 TEST_F(DeviceChooserContentViewTest, ClickRescanButton) {
