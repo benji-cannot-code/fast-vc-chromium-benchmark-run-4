af2115b6a0d2518f3048226fcbc35300197acedf
Carlos IL <carlosil@chromium.org>
2019-10-07 23:10:36 +0000

    Fix back navigation and reloads after a post commit error page navigation.
    When navigating to an error page with the LoadPostCommitError() API,
    we want to treat reload and back as they would work on a regular error
    page. That is, they should reload the site displayed in the omnibox, and go
    back from that navigation. Currently, they instead reload the error
    page itself, and go back from there. This change corrects that behavior by
    replacing the active entry with the post commit entry when showing an error
    and replacing it back with the original entry on any navigation away, or
    before a reload.
    
    This CL does not address reload in post commit error pages if there is
    no entry to replace the entry with.
    
    Bug: 996942
    
    Change-Id: Ic6a473d321f2e40c850c679e4e2a5cd37ebfdde9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1804575
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703519}

--- chrome/browser/heavy_ad_intervention/heavy_ad_helper_browsertest.cc
+++ chrome/browser/heavy_ad_intervention/heavy_ad_helper_browsertest.cc
@@ -60,8 +60,8 @@ IN_PROC_BROWSER_TEST_F(HeavyAdHelperBrowserTest,
   web_contents->SetDelegate(&console_delegate);
 
   content::TestNavigationObserver error_observer(web_contents);
-  controller.LoadErrorPage(child, url, heavy_ads::PrepareHeavyAdPage(),
-                           net::ERR_BLOCKED_BY_CLIENT);
+  controller.LoadPostCommitErrorPage(
+      child, url, heavy_ads::PrepareHeavyAdPage(), net::ERR_BLOCKED_BY_CLIENT);
   error_observer.Wait();
 
   EXPECT_TRUE(console_delegate.messages().empty());
@@ -82,8 +82,8 @@ IN_PROC_BROWSER_TEST_F(HeavyAdHelperBrowserTest,
       ChildFrameAt(web_contents->GetMainFrame(), 0);
 
   content::TestNavigationObserver error_observer(web_contents);
-  controller.LoadErrorPage(child, url, heavy_ads::PrepareHeavyAdPage(),
-                           net::ERR_BLOCKED_BY_CLIENT);
+  controller.LoadPostCommitErrorPage(
+      child, url, heavy_ads::PrepareHeavyAdPage(), net::ERR_BLOCKED_BY_CLIENT);
   error_observer.Wait();
 
   EXPECT_TRUE(IsContentInDocument(
