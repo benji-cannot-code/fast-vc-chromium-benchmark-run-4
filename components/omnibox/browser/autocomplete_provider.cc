4b6b22ebf14d281b89d8a3c193aa9c42f6ceeebf
Ce Chen <cch@chromium.org>
2019-10-07 19:52:58 +0000

    [omnibox] Add missing provider type ON_DEVICE_HEAD for AsOmniboxEventProviderType.
    This is to fix crash in http://crbug/1011730.
    
    Bug: 1011730
    Bug: 925072
    Change-Id: I1cd11156505ce501fac45527b5c59af74e8b96cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844056
    Commit-Queue: Ce Chen <cch@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703419}

--- components/omnibox/browser/autocomplete_provider.cc
+++ components/omnibox/browser/autocomplete_provider.cc
@@ -41,6 +41,8 @@ const char* AutocompleteProvider::TypeToString(Type type) {
       return "Bookmark";
     case TYPE_BUILTIN:
       return "Builtin";
+    case TYPE_CLIPBOARD:
+      return "Clipboard";
     case TYPE_DOCUMENT:
       return "Document";
     case TYPE_HISTORY_QUICK:
@@ -49,16 +51,14 @@ const char* AutocompleteProvider::TypeToString(Type type) {
       return "HistoryURL";
     case TYPE_KEYWORD:
       return "Keyword";
+    case TYPE_ON_DEVICE_HEAD:
+      return "OnDeviceHead";
     case TYPE_SEARCH:
       return "Search";
     case TYPE_SHORTCUTS:
       return "Shortcuts";
     case TYPE_ZERO_SUGGEST:
       return "ZeroSuggest";
-    case TYPE_CLIPBOARD:
-      return "Clipboard";
-    case TYPE_ON_DEVICE_HEAD:
-      return "OnDeviceHead";
     case TYPE_ZERO_SUGGEST_LOCAL_HISTORY:
       return "LocalHistoryZeroSuggest";
     default:
@@ -111,6 +111,8 @@ metrics::OmniboxEventProto_ProviderType AutocompleteProvider::
       return metrics::OmniboxEventProto::BOOKMARK;
     case TYPE_BUILTIN:
       return metrics::OmniboxEventProto::BUILTIN;
+    case TYPE_CLIPBOARD:
+      return metrics::OmniboxEventProto::CLIPBOARD;
     case TYPE_DOCUMENT:
       return metrics::OmniboxEventProto::DOCUMENT;
     case TYPE_HISTORY_QUICK:
@@ -119,14 +121,14 @@ metrics::OmniboxEventProto_ProviderType AutocompleteProvider::
       return metrics::OmniboxEventProto::HISTORY_URL;
     case TYPE_KEYWORD:
       return metrics::OmniboxEventProto::KEYWORD;
+    case TYPE_ON_DEVICE_HEAD:
+      return metrics::OmniboxEventProto::ON_DEVICE_HEAD;
     case TYPE_SEARCH:
       return metrics::OmniboxEventProto::SEARCH;
     case TYPE_SHORTCUTS:
       return metrics::OmniboxEventProto::SHORTCUTS;
     case TYPE_ZERO_SUGGEST:
       return metrics::OmniboxEventProto::ZERO_SUGGEST;
-    case TYPE_CLIPBOARD:
-      return metrics::OmniboxEventProto::CLIPBOARD;
     case TYPE_ZERO_SUGGEST_LOCAL_HISTORY:
       return metrics::OmniboxEventProto::ZERO_SUGGEST_LOCAL_HISTORY;
     default:
