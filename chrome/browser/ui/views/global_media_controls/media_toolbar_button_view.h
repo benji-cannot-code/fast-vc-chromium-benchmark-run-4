1eb04cccc7fb91a42277d8b9dbd55b6445ffe376
Tommy Steimel <steimel@chromium.org>
2019-10-08 16:48:45 +0000

    GMC: Fix crash when MediaToolbarButtonView is deleted
    This CL changes MediaToolbarButtonView to delete its
    MediaToolbarButtonController in its destructor so that it's destroyed
    before any of its other members. This prevents a use-after-free that
    can happen because the MediaToolbarButtonController will sometimes
    call methods on the MediaToolbarButtonView when it is deleted.
    
    Bug: 1011677
    Change-Id: I07b13d8ac9ea40d78a2ed42c1cce7226b568f188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1845794
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703774}

--- chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.h
+++ chrome/browser/ui/views/global_media_controls/media_toolbar_button_view.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_GLOBAL_MEDIA_CONTROLS_MEDIA_TOOLBAR_BUTTON_VIEW_H_
 
 #include "base/macros.h"
-#include "chrome/browser/ui/global_media_controls/media_toolbar_button_controller.h"
 #include "chrome/browser/ui/global_media_controls/media_toolbar_button_controller_delegate.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 
@@ -21,6 +20,7 @@ class Connector;
 
 class Browser;
 class GlobalMediaControlsPromoController;
+class MediaToolbarButtonController;
 class MediaToolbarButtonObserver;
 
 // Media icon shown in the trusted area of toolbar. Its lifetime is tied to that
@@ -83,7 +83,7 @@ class MediaToolbarButtonView : public ToolbarButton,
   bool is_promo_showing_ = false;
 
   service_manager::Connector* const connector_;
-  MediaToolbarButtonController controller_;
+  std::unique_ptr<MediaToolbarButtonController> controller_;
   const Browser* const browser_;
 
   base::ObserverList<MediaToolbarButtonObserver> observers_;
