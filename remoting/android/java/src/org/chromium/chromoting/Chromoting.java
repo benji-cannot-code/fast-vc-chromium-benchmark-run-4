74ae99678634b0d80c738eb2775b6e62fb147238
Yuwei Huang <yuweih@chromium.org>
2019-10-08 21:46:29 +0000

    [remoting][Android] Basic notification UI implement for Android
    This CL implements a basic dialog-based notification UI for Android
    client.
    
    Bug: 1001291
    Change-Id: If7df03179ad7a143a6c805c66595dbf7815eb074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846048
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703926}

--- remoting/android/java/src/org/chromium/chromoting/Chromoting.java
+++ remoting/android/java/src/org/chromium/chromoting/Chromoting.java
@@ -44,6 +44,7 @@ import org.chromium.chromoting.jni.ConnectionListener;
 import org.chromium.chromoting.jni.DirectoryService;
 import org.chromium.chromoting.jni.DirectoryServiceRequestError;
 import org.chromium.chromoting.jni.JniOAuthTokenGetter;
+import org.chromium.chromoting.jni.NotificationPresenter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -97,6 +98,9 @@ public class Chromoting extends AppCompatActivity
 
     /** Dialog for reporting connection progress. */
     private ProgressDialog mProgressIndicator;
+
+    /** Helper for fetching notification and presenting it. */
+    private NotificationPresenter mNotificationPresenter;
 
     /**
      * Helper used by SessionConnection for session authentication. Receives onNewIntent()
@@ -237,6 +241,7 @@ public class Chromoting extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         mDirectoryService = new DirectoryService();
+        mNotificationPresenter = new NotificationPresenter(this);
 
         // Get ahold of our view widgets.
         mHostListView = (ListView) findViewById(R.id.hostList_chooser);
@@ -627,6 +632,7 @@ public class Chromoting extends AppCompatActivity
         }
         mAccount = accountName;
         JniOAuthTokenGetter.setAccount(accountName);
+        mNotificationPresenter.presentIfNecessary(accountName);
 
         // The current host list is no longer valid for the new account, so clear the list.
         mHosts = new HostInfo[0];
--- remoting/client/notification/notification_message.h
+++ remoting/client/notification/notification_message.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 
 struct NotificationMessage final {
+  // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chromoting
+  // GENERATED_JAVA_CLASS_NAME_OVERRIDE: NotificationAppearance
   enum class Appearance {
     TOAST,
     DIALOG,
