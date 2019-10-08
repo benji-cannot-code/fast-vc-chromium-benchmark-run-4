9fbf735c591fd79f8a81df946c7b6b66deaa8dca
Tibor Goldschwendt <tiborg@chromium.org>
2019-10-08 15:22:49 +0000

    [DevUI DFM] Move chrome://omnibox resources to the DFM
    Moves resources used in chrome://omnibox out of browser_resources.grd
    into its own GRD file. Moves those resources into the Dev UI DFM to
    save binary size on Chrome Android.
    
    Bug: 927131
    Change-Id: Iabef47e999a5504969595b1d403123157fc338fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1845823
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703721}

--- chrome/browser/ui/webui/omnibox/omnibox_ui.cc
+++ chrome/browser/ui/webui/omnibox/omnibox_ui.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/version_handler.h"
 #include "chrome/browser/ui/webui/version_ui.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/grit/browser_resources.h"
+#include "chrome/grit/omnibox_resources.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/web_ui_data_source.h"
