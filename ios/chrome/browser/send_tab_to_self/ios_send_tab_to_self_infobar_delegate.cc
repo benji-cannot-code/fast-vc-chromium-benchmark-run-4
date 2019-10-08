94424d66418cdd924d8a17aeb3d1ade7f3f2f3f7
Lei Zhang <thestig@chromium.org>
2019-10-08 17:46:10 +0000

    Clean up some mobile STTS string resources.
    Move iOS strings from ui/strings/ui_strings.grd to
    ios/chrome/app/strings/ios_strings.grd.
    
    Change-Id: I1e8c443bd3dc3631e77d06cbc4cfc9eb286aeb7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1839373
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703812}

--- ios/chrome/browser/send_tab_to_self/ios_send_tab_to_self_infobar_delegate.cc
+++ ios/chrome/browser/send_tab_to_self/ios_send_tab_to_self_infobar_delegate.cc
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/send_tab_to_self/send_tab_to_self_entry.h"
 #include "components/send_tab_to_self/send_tab_to_self_metrics.h"
 #include "components/send_tab_to_self/send_tab_to_self_model.h"
+#include "ios/chrome/grit/ios_strings.h"
 #include "ios/chrome/grit/ios_theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/window_open_disposition.h"
-#include "ui/strings/grit/ui_strings.h"
 
 namespace send_tab_to_self {
 
