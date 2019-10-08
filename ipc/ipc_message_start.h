fbd3f82d33f3f4292f8fe038375f4334b3658475
Colin Blundell <blundell@chromium.org>
2019-10-08 11:17:05 +0000

    [IPC] Remove unused Chromecast *MsgStart messages
    Part of the effort to eliminate the legacy IPC system.
    
    Bug: 1007300
    Change-Id: Idbe41f4be45af0be18eea4fd485bcf313dca480c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1836494
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Martin Barbella <mbarbella@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703659}

--- ipc/ipc_message_start.h
+++ ipc/ipc_message_start.h
@@ -45,13 +45,8 @@ enum IPCMessageStart {
   OzoneGpuMsgStart,
   WebTestMsgStart,
   NetworkHintsMsgStart,
-  CastMediaMsgStart,
   ExtensionsGuestViewMsgStart,
   GuestViewMsgStart,
-  // Note: CastCryptoMsgStart and CastChannelMsgStart reserved for Chromecast
-  // internal code. Contact gunsch@ before changing/removing.
-  CastCryptoMsgStart,
-  CastChannelMsgStart,
   IPCTestMsgStart,
   MediaPlayerDelegateMsgStart,
   SurfaceViewManagerMsgStart,
