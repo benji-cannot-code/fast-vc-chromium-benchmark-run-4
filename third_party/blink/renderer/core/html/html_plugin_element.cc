3d0caf7022eceb3dfdee2f3c35c96672d010157c
Mike West <mkwst@chromium.org>
2019-10-08 15:23:50 +0000

    Measure usage of `<embed>` and `<object>` for non-plugin content.
    The conversation in https://github.com/whatwg/fetch/pull/948 makes me
    curious about how locked-in we are to the existing behavior around both
    `<embed>` and `<object>` when used for non-plugin content. They're
    basically strange `<iframe>`s, and it might be reasonable to push them
    closer towards _being_ `<iframe>` elements, or to push them further
    away, depending on how usage numbers look.
    
    Change-Id: I843ae37ae74033fdb3ea9b920ce3932004a067b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1847291
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703722}

--- third_party/blink/renderer/core/html/html_plugin_element.cc
+++ third_party/blink/renderer/core/html/html_plugin_element.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/scrolling/scrolling_coordinator.h"
 #include "third_party/blink/renderer/platform/bindings/v8_per_isolate_data.h"
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
 #include "third_party/blink/renderer/platform/network/mime/mime_type_from_url.h"
 #include "third_party/blink/renderer/platform/network/mime/mime_type_registry.h"
@@ -582,6 +583,16 @@ bool HTMLPlugInElement::RequestObject(const PluginParameters& plugin_params) {
     ResetInstance();
   if (object_type == ObjectContentType::kFrame ||
       object_type == ObjectContentType::kImage || handled_externally) {
+    if (object_type == ObjectContentType::kFrame) {
+      UseCounter::Count(GetDocument(),
+                        WebFeature::kPluginElementLoadedDocument);
+    } else if (object_type == ObjectContentType::kImage) {
+      UseCounter::Count(GetDocument(), WebFeature::kPluginElementLoadedImage);
+    } else {
+      UseCounter::Count(GetDocument(),
+                        WebFeature::kPluginElementLoadedExternal);
+    }
+
     if (ContentFrame() && ContentFrame()->IsRemoteFrame()) {
       // During lazy reattaching, the plugin element loses EmbeddedContentView.
       // Since the ContentFrame() is not torn down the options here are to
