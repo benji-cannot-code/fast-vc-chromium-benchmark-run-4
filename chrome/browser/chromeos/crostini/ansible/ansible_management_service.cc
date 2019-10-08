93d77193d64c25433fa0b01e77be7198c8b014b3
Julian Watson <juwa@google.com>
2019-10-08 10:40:20 +0000

    crostini: Use ContainerId in LinuxPackageOperationProgressObserver interface
    Change-Id: I81cd0f7ac56d381662a9c644bcbe9699f54b9e4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1840271
    Auto-Submit: Julian Watson <juwa@google.com>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703650}

--- chrome/browser/chromeos/crostini/ansible/ansible_management_service.cc
+++ chrome/browser/chromeos/crostini/ansible/ansible_management_service.cc
@@ -100,12 +100,11 @@ void AnsibleManagementService::OnInstallAnsibleInDefaultContainer(
 }
 
 void AnsibleManagementService::OnInstallLinuxPackageProgress(
-    const std::string& vm_name,
-    const std::string& container_name,
+    const ContainerId& container_id,
     InstallLinuxPackageProgressStatus status,
     int progress_percent) {
-  DCHECK_EQ(vm_name, kCrostiniDefaultVmName);
-  DCHECK_EQ(container_name, kCrostiniDefaultContainerName);
+  DCHECK_EQ(container_id.first, kCrostiniDefaultVmName);
+  DCHECK_EQ(container_id.second, kCrostiniDefaultContainerName);
 
   switch (status) {
     case InstallLinuxPackageProgressStatus::SUCCEEDED:
@@ -130,8 +129,7 @@ void AnsibleManagementService::OnInstallLinuxPackageProgress(
 }
 
 void AnsibleManagementService::OnUninstallPackageProgress(
-    const std::string& vm_name,
-    const std::string& container_name,
+    const ContainerId& container_id,
     UninstallPackageProgressStatus status,
     int progress_percent) {
   NOTIMPLEMENTED();
