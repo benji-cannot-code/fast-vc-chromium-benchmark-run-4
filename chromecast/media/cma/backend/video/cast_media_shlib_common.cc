74d3e41ffdf72d20e81021ee5d3ce8ab3007317a
Ken MacKay <kmackay@chromium.org>
2019-10-08 19:02:11 +0000

    Reland "[Chromecast] Statically link CMA backend when possible"
    This reverts commit 51439d2623f8f6e2264f06441da1979b6a368b29.
    
    Merge-With: eureka-internal/321778
    
    Bug: internal b/127963522
    Bug: internal b/141138924
    Change-Id: I192bd87c0e766aef66ed6b8c7004c026cde0fbc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846314
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703850}

--- chromecast/media/cma/backend/video/cast_media_shlib_common.cc
+++ chromecast/media/cma/backend/video/cast_media_shlib_common.cc
@@ -3,15 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/at_exit.h"
 #include "chromecast/public/media/media_capabilities_shlib.h"
 
 namespace chromecast {
 namespace media {
-
-namespace {
-base::AtExitManager g_at_exit_manager;
-}  // namespace
 
 bool MediaCapabilitiesShlib::IsSupportedAudioConfig(const AudioConfig& config) {
   switch (config.codec) {
