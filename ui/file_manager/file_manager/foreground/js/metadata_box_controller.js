f6cb7824fecf582b86fbdd5a90fabe034dc0dc8b
Jeremie Boulic <jboulic@chromium.org>
2019-10-08 02:34:28 +0000

    Update QuickView file size with extra precision digit
    Add optional argument to the QuickView file_metadata_formatter.js to
    add extra precision to the file size shown in the metadata box.
    
    Change the underlying bytesToString() formatting function to add the
    desired number of precision digits to the file size text if needed.
    
    Test: browser_tests --gtest_filter="*FileManagerJsTest.UtilTest"
    Bug: 977068
    Change-Id: Iaa6a08fbcbd7663ce2ec8d9347fc659894343926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1760840
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703562}

--- ui/file_manager/file_manager/foreground/js/metadata_box_controller.js
+++ ui/file_manager/file_manager/foreground/js/metadata_box_controller.js
@@ -142,7 +142,7 @@ class MetadataBoxController {
       this.setDirectorySize_(directory, isSameEntry);
     } else if (item.size) {
       this.metadataBox_.size =
-          this.fileMetadataFormatter_.formatSize(item.size, item.hosted);
+          this.fileMetadataFormatter_.formatSize(item.size, item.hosted, true);
       this.metadataBox_.metadataRendered('size');
     }
 
@@ -275,7 +275,7 @@ class MetadataBoxController {
       }
 
       this.metadataBox_.size =
-          this.fileMetadataFormatter_.formatSize(size, true);
+          this.fileMetadataFormatter_.formatSize(size, true, true);
       this.metadataBox_.isSizeLoading = false;
       this.metadataBox_.metadataRendered('size');
     });
