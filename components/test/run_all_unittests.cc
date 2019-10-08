d02f6502832036d34832db292771c283e92cc47d
John Budorick <jbudorick@chromium.org>
2019-10-08 17:33:19 +0000

    Revert "Change default retries for components unittests to 0"
    This reverts commit ed8151f9f9c500a7b7d32867a8735bf61d9585bb.
    
    Reason for revert: suspected cause of increased CQ false rejection rate, https://bugs.chromium.org/p/chromium/issues/detail?id=1011915
    
    Original change's description:
    > Change default retries for components unittests to 0
    > 
    > Bug: 993428
    > Change-Id: Id21490633dfb7ae92f73b0bdc9b2f81191f00ccb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1801021
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    > Cr-Commit-Position: refs/heads/master@{#700857}
    
    TBR=thakis@chromium.org,erikchen@chromium.org,isamsonov@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 993428
    Change-Id: Ie9c97d3e250eb5789d7095dd145b66afddf4363d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846805
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703806}

--- components/test/run_all_unittests.cc
+++ components/test/run_all_unittests.cc
@@ -6,5 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/test/components_test_suite.h"
 
 int main(int argc, char** argv) {
-  return base::LaunchUnitTests(argc, argv, GetLaunchCallback(argc, argv), 0U);
+  return base::LaunchUnitTests(argc, argv, GetLaunchCallback(argc, argv));
 }
