e65f1fc147497ec71975e2fe9d990c36cf43190b
Mostyn Bramley-Moore <mostynb@vewd.com>
2019-10-08 09:09:23 +0000

    IWYU: #include <memory> for std::unique_ptr
    Change-Id: I1777730958e0e36fb65450abe2178669a6e29996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1845216
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#703635}

--- storage/browser/test/mock_blob_util.h
+++ storage/browser/test/mock_blob_util.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef STORAGE_BROWSER_TEST_MOCK_BLOB_UTIL_H_
 #define STORAGE_BROWSER_TEST_MOCK_BLOB_UTIL_H_
 
+#include <memory>
 #include <string>
 
 #include "base/macros.h"
