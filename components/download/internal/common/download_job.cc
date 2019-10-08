d60b8c38ea0cb7900f488cc154adba8be59486cd
Min Qin <qinmin@chromium.org>
2019-10-08 00:05:14 +0000

    Remove length from DownloadSaveInfo
    All the parallel requests are now half open, no need for this field.
    And it is very strange to pass a request's length to download file, as
    the download file should know the whole file length, rather than
    an individual stream's length.
    
    BUG=1011427
    
    Change-Id: Id29b5c2859441e7bfffee165bc760c183b5a6882
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841183
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703539}

--- components/download/internal/common/download_job.cc
+++ components/download/internal/common/download_job.cc
@@ -77,8 +77,7 @@ void DownloadJob::OnDownloadFileInitialized(
 }
 
 bool DownloadJob::AddInputStream(std::unique_ptr<InputStream> stream,
-                                 int64_t offset,
-                                 int64_t length) {
+                                 int64_t offset) {
   DownloadFile* download_file = download_item_->GetDownloadFile();
   if (!download_file) {
     CancelRequestWithOffset(offset);
@@ -91,7 +90,7 @@ bool DownloadJob::AddInputStream(std::unique_ptr<InputStream> stream,
   GetDownloadTaskRunner()->PostTask(
       FROM_HERE, base::BindOnce(&DownloadFile::AddInputStream,
                                 base::Unretained(download_file),
-                                std::move(stream), offset, length));
+                                std::move(stream), offset));
   return true;
 }
 
