a9ae40d5fedbdbd527a25cd1a3f4c8b9be710602
Gyuyoung Kim <gyuyoung@igalia.com>
2019-10-08 00:33:07 +0000

    Replace .mojom-blink.h with .mojom-blink-forward.h in blink - 5
    To reduce pre-processed size of header files have been including
    foo.mojom-blink.h, this CL replaces .mojom-blink.h with
    .mojom-blink-forward.h in blink header files.
    
    This CL has no behavior changes and following CLs will
    continue replacing them more.
    
    Bug: 1001360
    Change-Id: I49cfab31ec57b183570cf4551e4cd1acd20a1a75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844752
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#703549}

--- third_party/blink/renderer/core/loader/prefetched_signed_exchange_manager.cc
+++ third_party/blink/renderer/core/loader/prefetched_signed_exchange_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
+#include "services/network/public/mojom/url_loader_factory.mojom-blink.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom-blink.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/task_type.h"
--- third_party/blink/renderer/core/testing/document_interface_broker_test_helpers.h
+++ third_party/blink/renderer/core/testing/document_interface_broker_test_helpers.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
-#include "third_party/blink/public/mojom/frame/document_interface_broker.mojom-blink.h"
+#include "third_party/blink/public/mojom/frame/document_interface_broker.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/frame/frame_host_test_interface.mojom-blink.h"
 #include "third_party/blink/renderer/core/testing/test_document_interface_broker.h"
 
