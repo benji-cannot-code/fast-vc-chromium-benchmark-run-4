378df788f817e5f9a215b62bf638356cc3703b13
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-10-08 19:41:53 +0000

    Revert "[Paint Preview] Inject PaintPreviewTracker"
    This reverts commit 2c483e9385818283717b451f1027f9c571ac5c46.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 703720 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzJjNDgzZTkzODU4MTgyODM3MTdiNDUxZjEwMjdmOWM1NzFhYzVjNDYM
    
    Sample Failed Build: https://ci.chromium.org/buildbot/chromium.memory/Linux%20MSan%20Tests/19798
    
    Sample Failed Step: browser_tests
    
    Original change's description:
    > [Paint Preview] Inject PaintPreviewTracker
    > 
    > Adds the ability to inject paint_preview::PaintPreviewTracker into
    > blink::GraphicsContext to act similarly to printing::MetafileSkia.
    > This class will track various metadata about the Paint Preview
    > capture process.
    > 
    > Design Doc: go/fdt-design (internal)
    > Part of Landing: crrev/1786583
    > 
    > Bug: 1008885
    > Change-Id: Iba2819491bee7b284de62a1bf888aa9486c788cf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829277
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#703720}
    
    
    Change-Id: Id0364a41756df743ed5af5aa64e991da34ba2e1b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1008885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846727
    Cr-Commit-Position: refs/heads/master@{#703874}

--- third_party/blink/renderer/platform/graphics/paint/paint_record_builder.cc
+++ third_party/blink/renderer/platform/graphics/paint/paint_record_builder.cc
@@ -11,11 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-PaintRecordBuilder::PaintRecordBuilder(
-    printing::MetafileSkia* metafile,
-    GraphicsContext* containing_context,
-    PaintController* paint_controller,
-    paint_preview::PaintPreviewTracker* tracker)
+PaintRecordBuilder::PaintRecordBuilder(printing::MetafileSkia* metafile,
+                                       GraphicsContext* containing_context,
+                                       PaintController* paint_controller)
     : paint_controller_(nullptr) {
   GraphicsContext::DisabledMode disabled_mode =
       GraphicsContext::kNothingDisabled;
@@ -33,13 +31,12 @@ PaintRecordBuilder::PaintRecordBuilder(
   paint_controller_->UpdateCurrentPaintChunkProperties(
       base::nullopt, PropertyTreeState::Root());
 
-  context_ = std::make_unique<GraphicsContext>(
-      *paint_controller_, disabled_mode, metafile, tracker);
+  context_ = std::make_unique<GraphicsContext>(*paint_controller_,
+                                               disabled_mode, metafile);
   if (containing_context) {
     context_->SetDarkMode(containing_context->dark_mode_settings());
     context_->SetDeviceScaleFactor(containing_context->DeviceScaleFactor());
     context_->SetPrinting(containing_context->Printing());
-    context_->SetIsPaintingPreview(containing_context->IsPaintingPreview());
   }
 }
 
