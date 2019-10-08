ac0d7b8d483cb3240876e8ffcc2224c908d6e409
Kyle Horimoto <khorimoto@chromium.org>
2019-10-08 18:07:33 +0000

    [CrOS Settings] Revert "About Chrome" redirect.
    This CL reverts
    https://chromium-review.googlesource.com/c/chromium/src/+/1784011.
    This UI will be implemented in a different way; see discussion on
    https://crbug.com/1010996.
    
    TBR: stevenjb@chromium.org
    Bug: 1010996
    Change-Id: Id97868df660ec86e61d50aa100399c3b96801715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846814
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703820}

--- chrome/browser/resources/settings/about_page/about_page.js
+++ chrome/browser/resources/settings/about_page/about_page.js
@@ -16,7 +16,6 @@ Polymer({
     settings.MainPageBehavior,
     settings.RouteObserverBehavior,
     I18nBehavior,
-    PrefsBehavior,
   ],
 
   properties: {
@@ -44,13 +43,6 @@ Polymer({
     },
 
     // <if expr="chromeos">
-    /** @private */
-    showAboutOSBanner_: {
-      type: Boolean,
-      computed: 'computeShowAboutOSBanner_(' +
-          'prefs.settings.cros.show_about_os_banner.value)',
-    },
-
     /** @private */
     hasCheckedForUpdates_: {
       type: Boolean,
@@ -566,21 +558,6 @@ Polymer({
   },
 
   // <if expr="chromeos">
-  /**
-   * @return {boolean}
-   * @private
-   */
-  computeShowAboutOSBanner_: function() {
-    // Show when SplitSettings is off and the user hasn't closed it.
-    return !this.showOsSettings_ && /** @type {boolean} */
-        (this.getPref('settings.cros.show_about_os_banner').value);
-  },
-
-  /** @private */
-  onAboutOSBannerClosed_: function() {
-    this.setPrefValue('settings.cros.show_about_os_banner', false);
-  },
-
   /**
    * @return {boolean}
    * @private
