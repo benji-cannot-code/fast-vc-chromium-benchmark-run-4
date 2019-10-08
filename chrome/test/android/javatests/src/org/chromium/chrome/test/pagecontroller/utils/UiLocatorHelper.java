fa0650bfe2edc883885ca758388b5e91394c2fff
John Budorick <jbudorick@chromium.org>
2019-10-08 23:32:20 +0000

    Revert "Add withTextString, withDescString locators, and some refactorings."
    This reverts commit 850e8d2b7484c56420ea0db6e180de31ea519729.
    
    Reason for revert: https://ci.chromium.org/p/chromium/builders/ci/android-archive-rel/5470
    
    Original change's description:
    > Add withTextString, withDescString locators, and some refactorings.
    > 
    > Refactorings include replacing *ByIndex with withIndex wrapper, renamed
    > locators that take variable number of resource ids to have "Any" in the name, removed
    > unnecessary params in the Page Controller tests build target.
    > 
    > Bug: 924194
    > Change-Id: I0a2b01c4d1d7ca52243988c8f028553135843441
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846480
    > Commit-Queue: Andrew Luo <aluo@chromium.org>
    > Reviewed-by: John Budorick <jbudorick@chromium.org>
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#703953}
    
    TBR=skym@chromium.org,jbudorick@chromium.org,aluo@chromium.org,wuwang@google.com
    
    Change-Id: Ib1a7b7ec8bb2ecce0e857a900218054266c41336
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 924194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1848399
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703970}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/pagecontroller/utils/UiLocatorHelper.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/pagecontroller/utils/UiLocatorHelper.java
@@ -326,7 +326,7 @@ public class UiLocatorHelper {
      * Delegate to be used with getCustomElements.
      * @param <T> The type of the element.
      */
-    public static interface CustomElementMaker<T> {
+    static public interface CustomElementMaker<T> {
         /**
          * Should construct an element given a node.
          * @param root          The input node.
@@ -404,11 +404,10 @@ public class UiLocatorHelper {
 
     private <T> T getOneElement(IUi2Locator locator, ElementConverter<T> converter) {
         List<T> all = getAllElements(locator, converter);
-        if (all.size() > 0) {
+        if (all.size() > 0)
             return all.get(0);
-        } else {
+        else
             return null;
-        }
     }
 
     private <T> T getOneElementImmediate(
