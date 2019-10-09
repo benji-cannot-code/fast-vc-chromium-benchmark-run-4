9a7a4318606024977a50078f796c2691f4113f13
Samuel Huang <huangs@chromium.org>
2019-10-09 00:37:50 +0000

    [GRIT] pak_util.py {extract, list-id, print}: Add support for textual-ID usage.
    This CL adds options to pak_util.py {extract, list-id, print} so that
    the .pak.info for the given .pak is used to translate ID to textual ID,
    which is then used. Details:
    * data_pack.py: Add GrdInfoItem type and ReadGrdInfo().
    * extract command: Add --textual-id/-t to use textual IDs as filenames
      for extracted resources.
    * list-id command: Add --textual-id/-t to print textual IDs, and
      --path/-p to print resource paths, formatted as:
      ID[ = textual-ID][ @ path]
    * print command: Add --textual-id/-t to print textual IDs instead of
      IDs.
    
    Change-Id: I2086efc69867e56c8312aaec9545113e9a869266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846728
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#703991}

--- tools/grit/grit/format/data_pack.py
+++ tools/grit/grit/format/data_pack.py
@@ -10,6 +10,7 @@ files.
 
 from __future__ import print_function
 
+import collections
 import os
 import struct
 import sys
@@ -24,6 +25,10 @@ from grit.node import structure
 
 PACK_FILE_VERSION = 5
 BINARY, UTF8, UTF16 = range(3)
+
+
+GrdInfoItem = collections.namedtuple('GrdInfoItem',
+                                     ['textual_id', 'id', 'path'])
 
 
 class WrongFileVersion(Exception):
@@ -199,6 +204,15 @@ def WriteDataPack(resources, output_file, encoding):
   content = WriteDataPackToString(resources, encoding)
   with open(output_file, 'wb') as file:
     file.write(content)
+
+
+def ReadGrdInfo(grd_file):
+  info_dict = {}
+  with open(grd_file + '.info', 'rt') as f:
+    for line in f:
+      item = GrdInfoItem._make(line.strip().split(','))
+      info_dict[int(item.id)] = item
+  return info_dict
 
 
 def RePack(output_file, input_files, whitelist_file=None,
