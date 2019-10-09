9a003063b69402be0a102f5792ae7e815fc5e8bd
Mounir Lamouri <mlamouri@chromium.org>
2019-10-09 01:31:26 +0000

    Media Notification: do not show the scheme and trivial subdomains on Android.
    In practice, it means:
     - https://www.example.com => example.com
     - http://www.example.com => example.com
     - https://m.example.com => m.example.com
    
    Bug: 1011900
    Change-Id: I9713dd22693e35c67ecdebdca67f4642442d2c3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846472
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704025}

--- chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/ui/MediaSessionTabHelper.java
@@ -15,7 +15,6 @@ import android.text.TextUtils;
 
 import androidx.annotation.Nullable;
 
-import org.chromium.base.Log;
 import org.chromium.base.SysUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
@@ -33,13 +32,12 @@ import org.chromium.content_public.browser.MediaSessionObserver;
 import org.chromium.content_public.browser.NavigationHandle;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.media_session.mojom.MediaSessionAction;
+import org.chromium.net.GURLUtils;
 import org.chromium.services.media_session.MediaImage;
 import org.chromium.services.media_session.MediaMetadata;
 import org.chromium.services.media_session.MediaPosition;
 import org.chromium.ui.base.WindowAndroid;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Set;
 
@@ -324,16 +322,8 @@ public class MediaSessionTabHelper implements MediaImageCallback {
                 return;
             }
 
-            String origin = mTab.getUrl();
-            try {
-                URI uri = new URI(origin);
-                origin = UrlFormatter.formatUrlForSecurityDisplay(origin);
-            } catch (URISyntaxException e) {
-                Log.e(TAG, "Unable to parse the origin from the URL. "
-                                + "Using the full URL instead.");
-            }
-
-            mOrigin = origin;
+            mOrigin = UrlFormatter.formatUrlForDisplayOmitSchemeOmitTrivialSubdomains(
+                    GURLUtils.getOrigin(mTab.getUrl()));
             mFavicon = null;
             mPageMediaImage = null;
             mPageMetadata = null;
--- net/android/java/src/org/chromium/net/GURLUtils.java
+++ net/android/java/src/org/chromium/net/GURLUtils.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net;
 
+import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
 
@@ -35,8 +36,9 @@ public final class GURLUtils {
         return GURLUtilsJni.get().getScheme(url);
     }
 
+    @VisibleForTesting
     @NativeMethods
-    interface Natives {
+    public interface Natives {
         String getOrigin(String url);
         String getScheme(String url);
     }
