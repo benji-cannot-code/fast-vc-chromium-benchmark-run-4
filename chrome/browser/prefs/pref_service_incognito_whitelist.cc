6e82fe09815a3eb7f60e1c3a12fb068ca2ea53d6
Huanzhong Huang <huanzhong@google.com>
2019-10-09 08:18:12 +0000

    Whitelist cookie controls so changes are persisted
    At this point cookie controls setting is exposed only in incognito mode.
    This cl makes changes to the setting being persisted across incognito
    sessions.
    
    Bug: 967668
    Change-Id: Icc42a7365186af5ccc74777236574ee9813ae3a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844819
    Commit-Queue: Huanzhong Huang <huanzhong@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704103}

--- chrome/browser/prefs/pref_service_incognito_whitelist.cc
+++ chrome/browser/prefs/pref_service_incognito_whitelist.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/common/pref_names.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
+#include "components/content_settings/core/common/pref_names.h"
 #include "components/metrics/metrics_pref_names.h"
 #include "components/rappor/rappor_pref_names.h"
 #include "components/reading_list/core/reading_list_pref_names.h"
@@ -208,6 +209,11 @@ const char* const kPersistentPrefNames[] = {
     ukm::prefs::kUkmClientId,
     ukm::prefs::kUkmUnsentLogStore,
     ukm::prefs::kUkmSessionId,
+
+    // Cookie controls preference is, as in an initial release, surfaced only in
+    // the incognito mode and therefore should be persisted between incognito
+    // sessions.
+    prefs::kCookieControlsMode,
 };
 
 }  // namespace
