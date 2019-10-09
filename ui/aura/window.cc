5a56fccf407412d4af62b6d4e8739eafb7e637e8
Peter Kasting <pkasting@chromium.org>
2019-10-09 18:35:05 +0000

    Remove layer painting suppression when closing a window.
    This is hopefully unnecessary now, and causes problems for views that paint to a
    layer and outlive their windows.
    
    Bug: none
    Change-Id: I211e30550af9923bca821e3d7a65b8e112211185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1848911
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704266}

--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -123,7 +123,6 @@ Window::~Window() {
 
   if (layer()->owner() == this)
     layer()->CompleteAllAnimations();
-  layer()->SuppressPaint();
 
   // Let the delegate know we're in the processing of destroying.
   if (delegate_)
@@ -744,10 +743,6 @@ std::unique_ptr<ScopedKeyboardHook> Window::CaptureSystemKeyEvents(
     return nullptr;
 
   return host->CaptureSystemKeyEvents(std::move(dom_codes));
-}
-
-void Window::SuppressPaint() {
-  layer()->SuppressPaint();
 }
 
 // {Set,Get,Clear}Property are implemented in class_property.h.
