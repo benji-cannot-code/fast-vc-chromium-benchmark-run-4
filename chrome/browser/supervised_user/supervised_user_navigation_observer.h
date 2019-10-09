82abdf7ef13843ec3db5ec63c0f25977e9ebce76
yilkal <yilkal@chromium.org>
2019-10-09 23:51:04 +0000

    Prepare for iframe filtering in  supervised_user/*
    This CL updates SupervisedUserNavigationObserver and
    SupervisedUserInterstitial to be able to handle multiple interstitial
    error pages inside a particular web contents. This is required because
    we are enabling iframe filtering for SupervisedUserNavigationThrottle.
    
    The CL also adds WebContents::ReloadFrame(RenderFrameHost* rfh,
    bool bypass_cache) interface and implementation.
    
    The CL also adds test in SupervisedUserNavigationThrottle for iframe
    filters.
    
    Bug: 850328
    Change-Id: I6feea8f39a947011d91d1abfb27621a54281353c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816580
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704413}

--- chrome/browser/supervised_user/supervised_user_navigation_observer.h
+++ chrome/browser/supervised_user/supervised_user_navigation_observer.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SUPERVISED_USER_SUPERVISED_USER_NAVIGATION_OBSERVER_H_
 #define CHROME_BROWSER_SUPERVISED_USER_SUPERVISED_USER_NAVIGATION_OBSERVER_H_
 
+#include <memory>
 #include <vector>
 
 #include "base/macros.h"
@@ -25,8 +26,9 @@ class SupervisedUserInterstitial;
 
 namespace content {
 class NavigationHandle;
+class RenderFrameHost;
 class WebContents;
-}
+}  // namespace content
 
 class SupervisedUserNavigationObserver
     : public content::WebContentsUserData<SupervisedUserNavigationObserver>,
@@ -47,6 +49,7 @@ class SupervisedUserNavigationObserver
       const GURL& url,
       supervised_user_error_page::FilteringBehaviorReason reason,
       int64_t navigation_id,
+      int frame_id,
       const base::Callback<
           void(SupervisedUserNavigationThrottle::CallbackActions)>& callback);
 
@@ -64,16 +67,22 @@ class SupervisedUserNavigationObserver
     return main_frame_filtering_behavior_reason_;
   }
 
-  // WebContentsObserver implementation.
+  // WebContentsObserver:
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
+  void FrameDeleted(content::RenderFrameHost* render_frame_host) override;
 
-  // SupervisedUserServiceObserver implementation.
+  // SupervisedUserServiceObserver:
   void OnURLFilterChanged() override;
 
   // Called when interstitial error page is no longer being shown in the main
   // frame.
-  void OnInterstitialDone();
+  void OnInterstitialDone(int frame_id);
+
+  const std::map<int, std::unique_ptr<SupervisedUserInterstitial>>&
+  interstitials_for_test() {
+    return supervised_user_interstitials_;
+  }
 
  private:
   friend class content::WebContentsUserData<SupervisedUserNavigationObserver>;
@@ -84,11 +93,14 @@ class SupervisedUserNavigationObserver
       const GURL& url,
       supervised_user_error_page::FilteringBehaviorReason reason,
       int64_t navigation_id,
+      int frame_id,
       const base::Callback<
           void(SupervisedUserNavigationThrottle::CallbackActions)>& callback);
 
   void URLFilterCheckCallback(
       const GURL& url,
+      int render_frame_process_id,
+      int render_frame_routing_id,
       SupervisedUserURLFilter::FilteringBehavior behavior,
       supervised_user_error_page::FilteringBehaviorReason reason,
       bool uncertain);
@@ -98,8 +110,12 @@ class SupervisedUserNavigationObserver
       supervised_user_error_page::FilteringBehaviorReason reason,
       bool initial_page_load,
       int64_t navigation_id,
+      int frame_id,
       const base::Callback<
           void(SupervisedUserNavigationThrottle::CallbackActions)>& callback);
+
+  // Filters the render frame host if render frame is live.
+  void FilterRenderFrame(content::RenderFrameHost* render_frame_host);
 
   // supervised_user::mojom::SupervisedUserCommands implementation. Should not
   // be called when an interstitial is no longer showing. This should be
@@ -114,8 +130,10 @@ class SupervisedUserNavigationObserver
   // Owned by SupervisedUserServiceFactory (lifetime of Profile).
   SupervisedUserService* supervised_user_service_;
 
-  // Navigation ID of the navigation that triggered the last interstitial.
-  int64_t interstitial_navigation_id_ = -1;
+  // Keeps track of the blocked frames. It maps the frame's globally unique
+  // id to its corresponding |SupervisedUserInterstitial| instance.
+  std::map<int, std::unique_ptr<SupervisedUserInterstitial>>
+      supervised_user_interstitials_;
 
   SupervisedUserURLFilter::FilteringBehavior main_frame_filtering_behavior_ =
       SupervisedUserURLFilter::FilteringBehavior::ALLOW;
@@ -125,8 +143,6 @@ class SupervisedUserNavigationObserver
 
   std::vector<std::unique_ptr<const sessions::SerializedNavigationEntry>>
       blocked_navigations_;
-
-  std::unique_ptr<SupervisedUserInterstitial> interstitial_;
 
   content::WebContentsFrameBindingSet<
       supervised_user::mojom::SupervisedUserCommands>
--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -314,6 +314,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   void UpdateBrowserControlsState(BrowserControlsState constraints,
                                   BrowserControlsState current,
                                   bool animate) override;
+  void Reload() override;
 
   void SendAccessibilityEventsToManager(
       const AXEventNotificationDetails& details);
