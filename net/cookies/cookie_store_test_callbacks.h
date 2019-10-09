040cd7dcf0c73666352c2c37d89cc2d5ef0beebf
Justin Cohen <justincohen@chromium.org>
2019-10-09 03:30:25 +0000

    Revert "Add CookieStore method to fetch all cookies with their access semantics"
    This reverts commit 8d1685b909c7c3d3bc38fc098815aee5dd5c3047.
    
    Reason for revert: Consistently failing on SlimNav-off bot.
    
    https://ci.chromium.org/p/chromium/builders/ci/ios-slimnav/8980
    
    Original change's description:
    > Add CookieStore method to fetch all cookies with their access semantics
    > 
    > This change adds a CookieStore method,
    > GetAllCookiesWithAccessSemanticsAsync, which returns a list of all
    > cookies in the CookieStore along with a list of the
    > CookieAccessSemantics for each returned cookie. These lists will be in
    > the same order, so that each cookie in the returned list of cookies has
    > the access semantics at the same index in the other list.
    > 
    > This is implemented for CookieMonster only, and other CookieStore
    > implementations will use the default impl which returns UNKNOWN for
    > all the cookies. A later CL will additionally expose this method
    > via the CookieManager interface.
    > 
    > Bug: 978172
    > Change-Id: I8463806f5cc6ce6c8d200bec57bbecba5fe3aec6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846071
    > Reviewed-by: Mohammad Refaat <mrefaat@chromium.org>
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Commit-Queue: Lily Chen <chlily@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#703824}
    
    TBR=mrefaat@chromium.org,morlovich@chromium.org,chlily@chromium.org
    
    Change-Id: I2a185de1291efb615aae82b8664116701549055d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 978172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849135
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704064}

--- net/cookies/cookie_store_test_callbacks.h
+++ net/cookies/cookie_store_test_callbacks.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "net/cookies/canonical_cookie.h"
-#include "net/cookies/cookie_constants.h"
 #include "net/cookies/cookie_store.h"
 
 namespace base {
@@ -151,36 +150,6 @@ class GetAllCookiesCallback : public CookieCallback {
 
  private:
   CookieList cookies_;
-};
-
-class GetAllCookiesWithAccessSemanticsCallback : public CookieCallback {
- public:
-  GetAllCookiesWithAccessSemanticsCallback();
-  explicit GetAllCookiesWithAccessSemanticsCallback(
-      base::Thread* run_in_thread);
-
-  ~GetAllCookiesWithAccessSemanticsCallback();
-
-  void Run(const CookieList& cookies,
-           const std::vector<CookieAccessSemantics>& access_semantics_list);
-
-  // Makes a callback that will invoke Run. Assumes that |this| will be kept
-  // alive till the time the callback is used.
-  base::OnceCallback<void(const CookieList&,
-                          const std::vector<CookieAccessSemantics>&)>
-  MakeCallback() {
-    return base::BindOnce(&GetAllCookiesWithAccessSemanticsCallback::Run,
-                          base::Unretained(this));
-  }
-
-  const CookieList& cookies() { return cookies_; }
-  const std::vector<CookieAccessSemantics>& access_semantics_list() {
-    return access_semantics_list_;
-  }
-
- private:
-  CookieList cookies_;
-  std::vector<CookieAccessSemantics> access_semantics_list_;
 };
 
 }  // namespace net
