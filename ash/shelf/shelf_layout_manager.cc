e3e6a0e9465f2e907633b3089f60d8b40d57e207
Alex Newcomer <newcomer@chromium.org>
2019-10-09 21:43:59 +0000

    cros: Hide the hotseat when apps are launched in the hotseat
    Send the ShelfAction to Shelf -> ShelfLayoutManager. Then hide the
    hotseat if an app is launched or swapped to.
    
    Bug: 1011901
    Change-Id: I32dd58045f75d7cf915ee4523acd21b58a370077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1847946
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704352}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -705,6 +705,21 @@ void ShelfLayoutManager::ResumeVisiblityUpdate() {
   MaybeUpdateShelfBackground(AnimationChangeType::ANIMATE);
 }
 
+void ShelfLayoutManager::OnShelfItemSelected(ShelfAction action) {
+  switch (action) {
+    case SHELF_ACTION_NONE:
+    case SHELF_ACTION_APP_LIST_SHOWN:
+    case SHELF_ACTION_APP_LIST_DISMISSED:
+    case SHELF_ACTION_WINDOW_MINIMIZED:
+      break;
+    case SHELF_ACTION_NEW_WINDOW_CREATED:
+    case SHELF_ACTION_WINDOW_ACTIVATED: {
+      base::AutoReset<bool> reset(&should_hide_hotseat_, true);
+      UpdateVisibilityState();
+    } break;
+  }
+}
+
 void ShelfLayoutManager::OnWindowResized() {
   LayoutShelf();
 }
@@ -1043,7 +1058,8 @@ HotseatState ShelfLayoutManager::CalculateHotseatState(
             return HotseatState::kShown;
           }
           if (visibility_state == SHELF_AUTO_HIDE) {
-            if (auto_hide_state == SHELF_AUTO_HIDE_HIDDEN)
+            if (auto_hide_state == SHELF_AUTO_HIDE_HIDDEN ||
+                should_hide_hotseat_)
               return HotseatState::kHidden;
             return HotseatState::kExtended;
           }
--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_context_menu_model.h"
 #include "ash/shelf/shelf_controller.h"
 #include "ash/shelf/shelf_focus_cycler.h"
+#include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shelf/shelf_menu_model_adapter.h"
 #include "ash/shelf/shelf_tooltip_manager.h"
 #include "ash/shelf/shelf_widget.h"
@@ -397,10 +398,7 @@ gfx::Rect ShelfView::GetIdealBoundsOfItemIcon(const ShelfID& id) {
     return GetMirroredRect(overflow_button_->bounds());
 
   const gfx::Rect& ideal_bounds(view_model_->ideal_bounds(index));
-  views::View* view = view_model_->view_at(index);
-
-  CHECK_EQ(ShelfAppButton::kViewClassName, view->GetClassName());
-  ShelfAppButton* button = static_cast<ShelfAppButton*>(view);
+  ShelfAppButton* button = GetShelfAppButton(id);
   gfx::Rect icon_bounds = button->GetIconBounds();
   return gfx::Rect(GetMirroredXWithWidthInView(
                        ideal_bounds.x() + icon_bounds.x(), icon_bounds.width()),
@@ -448,6 +446,16 @@ bool ShelfView::ShouldShowTooltipForView(const views::View* view) const {
 
   return overflow_shelf() &&
          overflow_shelf()->ShouldShowTooltipForChildView(view);
+}
+
+ShelfAppButton* ShelfView::GetShelfAppButton(const ShelfID& id) {
+  const int index = model_->ItemIndexByID(id);
+  if (index < 0)
+    return nullptr;
+
+  views::View* const view = view_model_->view_at(index);
+  DCHECK_EQ(ShelfAppButton::kViewClassName, view->GetClassName());
+  return static_cast<ShelfAppButton*>(view);
 }
 
 bool ShelfView::ShouldHideTooltip(const gfx::Point& cursor_location) const {
@@ -2285,10 +2293,7 @@ void ShelfView::ShelfItemStatusChanged(const ShelfID& id) {
     return;
 
   const ShelfItem item = model_->items()[index];
-  views::View* view = view_model_->view_at(index);
-  CHECK_EQ(ShelfAppButton::kViewClassName, view->GetClassName());
-  // TODO(manucornet): Add a helper to get the button.
-  ShelfAppButton* button = static_cast<ShelfAppButton*>(view);
+  ShelfAppButton* button = GetShelfAppButton(id);
   button->ReflectItemStatus(item);
   button->SchedulePaint();
 }
@@ -2344,6 +2349,7 @@ void ShelfView::AfterItemSelected(const ShelfItem& item,
       ink_drop->AnimateToState(views::InkDropState::ACTION_TRIGGERED);
     }
   }
+  shelf_->shelf_layout_manager()->OnShelfItemSelected(action);
   scoped_root_window_for_new_windows_.reset();
 }
 
