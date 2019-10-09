c5e9d6ca69b0e171ca2a12d5764f4036ef8f5286
Matt Menke <mmenke@chromium.org>
2019-10-09 22:34:24 +0000

    Remove net::LOAD_MAYBE_USER_GESTURE.
    It was only being set by the AccountAvatarFetcher and the
    AssistantImageDownloader, and only read by the ExtensionThrottleEntry
    (which may well not even be on the path for the two classes that set
    the flag).
    
    As such, it's not serving any significant purpose, so can now be
    removed.
    
    Bug: 516495
    Change-Id: Ib1b52ab6100a5810836a413e4b05f7a3d06ae7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1827489
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704375}

--- extensions/renderer/extension_throttle_simulation_unittest.cc
+++ extensions/renderer/extension_throttle_simulation_unittest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "extensions/renderer/extension_throttle_entry.h"
 #include "extensions/renderer/extension_throttle_test_support.h"
-#include "net/base/load_flags.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::TimeDelta;
@@ -406,7 +405,7 @@ class Requester : public DiscreteTimeSimulation::Actor {
 
     if (throttler_entry_->ImplGetTimeNow() - time_of_last_attempt_ >
         effective_delay) {
-      if (!throttler_entry_->ShouldRejectRequest(net::LOAD_NORMAL)) {
+      if (!throttler_entry_->ShouldRejectRequest()) {
         int status_code = server_->HandleRequest();
         throttler_entry_->UpdateWithResponse(status_code);
 
