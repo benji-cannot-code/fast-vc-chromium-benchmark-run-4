b06f32c140e277ec605d8867a888baf2f2416fc7
Nicholas Hollingum <hollingum@google.com>
2019-10-09 02:56:26 +0000

    Fixed/modified "uninstall" for crostini shelf items
    Two main changes
     - Removed "uninstall" from the terminal's menu, which previously would
       uninstall crostini. This must now be done via settings.
     - Fixed "uninstall" for shelf apps, which previously did not work.
    
    Bug: 1009925
    Change-Id: Icba200b4ed054a09a8fe1c5d777362cf353438d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846631
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704053}

--- chrome/browser/ui/ash/launcher/crostini_shelf_context_menu.cc
+++ chrome/browser/ui/ash/launcher/crostini_shelf_context_menu.cc
@@ -68,10 +68,12 @@ bool CrostiniShelfContextMenu::IsCommandIdEnabled(int command_id) const {
 }
 
 void CrostiniShelfContextMenu::ExecuteCommand(int command_id, int event_flags) {
-  if (ExecuteCommonCommand(command_id, event_flags))
-    return;
-
   switch (command_id) {
+    case ash::UNINSTALL:
+      DCHECK_NE(item().id.app_id, crostini::kCrostiniTerminalId);
+      crostini::ShowCrostiniAppUninstallerView(controller()->profile(),
+                                               item().id.app_id);
+      return;
     case ash::STOP_APP:
       if (item().id.app_id == crostini::kCrostiniTerminalId) {
         crostini::CrostiniManager::GetForProfile(controller()->profile())
@@ -94,7 +96,7 @@ void CrostiniShelfContextMenu::ExecuteCommand(int command_id, int event_flags) {
       return;
     }
     default:
-      NOTREACHED();
+      ExecuteCommonCommand(command_id, event_flags);
   }
 }
 
