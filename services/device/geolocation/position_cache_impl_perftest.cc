b68095bac01679a6d716ceb872f8f4f3bcf87b27
Brian Sheedy <bsheedy@chromium.org>
2019-10-09 19:01:19 +0000

    Switch services_perftests to histograms
    Switches use of PrintResult in services_perftests to PerfResultReporter
    and whitelists the test suite for conversion to histograms before
    uploading to the perf dashboard.
    
    Bug: 923564
    Change-Id: Ia6640f2ba9c16d28fec876446fc19a0d206d2cdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1848480
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704277}

--- services/device/geolocation/position_cache_impl_perftest.cc
+++ services/device/geolocation/position_cache_impl_perftest.cc
@@ -14,7 +14,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "services/device/geolocation/position_cache_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "testing/perf/perf_test.h"
+#include "testing/perf/perf_result_reporter.h"
+
+namespace {
+
+constexpr char kMetricPrefixPositionCacheImpl[] = "PositionCacheImpl.";
+constexpr char kMetricAddTimeMs[] = "add_time";
+constexpr char kMetricFindTimeMs[] = "find_time";
+constexpr char kStoryBaseline[] = "baseline_story";
+
+perf_test::PerfResultReporter SetUpReporter(const std::string& story) {
+  perf_test::PerfResultReporter reporter(kMetricPrefixPositionCacheImpl, story);
+  reporter.RegisterImportantMetric(kMetricAddTimeMs, "ms");
+  reporter.RegisterImportantMetric(kMetricFindTimeMs, "ms");
+  return reporter;
+}
+
+}  // namespace
 
 namespace device {
 
@@ -43,9 +59,8 @@ TEST_F(PositionCacheImplPerfTest, Adding) {
   for (const auto& pair : data_)
     cache_.CachePosition(pair.first, pair.second);
   base::Time end = base::Time::Now();
-  perf_test::PrintResult("adding_to_cache", "", "",
-                         base::TimeDelta(end - start).InMillisecondsF(),
-                         "ms per batch", true);
+  auto reporter = SetUpReporter(kStoryBaseline);
+  reporter.AddResult(kMetricAddTimeMs, base::TimeDelta(end - start));
 }
 
 TEST_F(PositionCacheImplPerfTest, Finding) {
@@ -55,8 +70,7 @@ TEST_F(PositionCacheImplPerfTest, Finding) {
   for (const auto& pair : data_)
     cache_.FindPosition(pair.first);
   base::Time end = base::Time::Now();
-  perf_test::PrintResult("finding_in_cache", "", "",
-                         base::TimeDelta(end - start).InMillisecondsF(),
-                         "ms per batch", true);
+  auto reporter = SetUpReporter(kStoryBaseline);
+  reporter.AddResult(kMetricFindTimeMs, base::TimeDelta(end - start));
 }
 }  // namespace device
