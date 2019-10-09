15499a18372ef7a7feecfde82443bb5cc8046522
Andrew Grieve <agrieve@google.com>
2019-10-09 14:11:24 +0000

    Compress IDR_TRANSLATE_JS
    Bug: 982762
    Change-Id: I320edd26015e34708e582b5062c8bdb7d34b1aae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1837202
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Rachel Blum <groby@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Rachel Blum <groby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704178}

--- chrome/renderer/translate/translate_script_browsertest.cc
+++ chrome/renderer/translate/translate_script_browsertest.cc
@@ -80,11 +80,9 @@ class TranslateScriptBrowserTest : public ChromeRenderViewTest {
 
  protected:
   void InjectElementLibrary() {
-    std::string script;
-    base::StringPiece translate_js =
-        ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
+    std::string script =
+        ui::ResourceBundle::GetSharedInstance().DecompressDataResource(
             IDR_TRANSLATE_JS);
-    translate_js.CopyToString(&script);
     script += kElementJs;
     ExecuteScript(script);
   }
