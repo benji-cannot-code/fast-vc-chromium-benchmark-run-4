f5e5f8836bd9e5a6638ace5e10bfafeb268fde1e
Makoto Shimazu <shimazu@chromium.org>
2019-10-09 21:14:50 +0000

    Convert base::Bind to base::Bind{Once,Repeating} in content/browser
    This CL converts base::Bind to BindOnce or BindRepeating, and also changes
    relevant base::Callbacks or base::Closures to appropriate types.
    
    This CL is split from https://crrev.com/1831621.
    
    This CL was uploaded by git cl split.
    
    R=csharrison@chromium.org
    
    Bug: 1007760
    Change-Id: I7a5689ed0f5fe3d14063312230f774a61e1b0111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849477
    Auto-Submit: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704341}

--- content/browser/loader/loader_browsertest.cc
+++ content/browser/loader/loader_browsertest.cc
@@ -296,7 +296,7 @@ IN_PROC_BROWSER_TEST_F(LoaderBrowserTest, SyncXMLHttpRequest_Cancelled) {
   if (IsInProcessNetworkService())
     return;
 
-  embedded_test_server()->RegisterRequestHandler(base::Bind(
+  embedded_test_server()->RegisterRequestHandler(base::BindRepeating(
       &CancelOnRequest, "/hung",
       shell()->web_contents()->GetMainFrame()->GetProcess()->GetID(),
       base::BindRepeating(&BrowserTestBase::SimulateNetworkServiceCrash,
@@ -377,7 +377,7 @@ std::unique_ptr<net::test_server::HttpResponse> NoContentResponseHandler(
 IN_PROC_BROWSER_TEST_F(LoaderBrowserTest, CrossSiteNoUnloadOn204) {
   const char kNoContentPath[] = "/nocontent";
   embedded_test_server()->RegisterRequestHandler(
-      base::Bind(&NoContentResponseHandler, kNoContentPath));
+      base::BindRepeating(&NoContentResponseHandler, kNoContentPath));
 
   ASSERT_TRUE(embedded_test_server()->Start());
 
@@ -567,7 +567,7 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRedirectRequest(
 // navigations.
 IN_PROC_BROWSER_TEST_F(LoaderBrowserTest, CookiePolicy) {
   embedded_test_server()->RegisterRequestHandler(
-      base::Bind(&HandleRedirectRequest, "/redirect?"));
+      base::BindRepeating(&HandleRedirectRequest, "/redirect?"));
   ASSERT_TRUE(embedded_test_server()->Start());
 
   std::string set_cookie_url(base::StringPrintf(
