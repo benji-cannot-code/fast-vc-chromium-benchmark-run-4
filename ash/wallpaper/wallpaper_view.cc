204a88b2f8918294944d2731b251208798925ce0
Sammie Quon <sammiequon@google.com>
2019-10-09 16:38:10 +0000

    wallpaper: Do not dim in overview if in tablet mode.
    Dimming is already applied in OnPaint so the filter is not needed,
    requested by UX.
    
    Test: manual
    Bug: 1012042
    Change-Id: I2467e4a927c6786f1289e7417bea8bb05716b14f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846473
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704218}

--- ash/wallpaper/wallpaper_view.cc
+++ ash/wallpaper/wallpaper_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wallpaper/wallpaper_controller_impl.h"
 #include "ash/wallpaper/wallpaper_widget_controller.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "cc/paint/render_surface_filters.h"
 #include "ui/aura/window.h"
 #include "ui/display/display.h"
@@ -134,8 +135,14 @@ void WallpaperView::DrawWallpaper(const gfx::ImageSkia& wallpaper,
   // Create the blur and brightness filter to apply to the downsampled image.
   cc::PaintFlags filter_flags;
   cc::FilterOperations operations;
-  operations.Append(
-      cc::FilterOperation::CreateBrightnessFilter(repaint_opacity_));
+  // In tablet mode, the wallpaper already has a color filter applied in
+  // |OnPaint| so we don't need to darken here.
+  // TODO(crbug.com/944152): Merge this with the color filter in
+  // WallpaperBaseView.
+  if (!Shell::Get()->tablet_mode_controller()->InTabletMode()) {
+    operations.Append(
+        cc::FilterOperation::CreateBrightnessFilter(repaint_opacity_));
+  }
   operations.Append(cc::FilterOperation::CreateBlurFilter(
       blur, SkBlurImageFilter::kClamp_TileMode));
   sk_sp<cc::PaintFilter> filter = cc::RenderSurfaceFilters::BuildImageFilter(
