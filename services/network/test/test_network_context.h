336a8011e8ea3c80d97bc578098045c8adef950e
Julie Jeongeun Kim <jkim@igalia.com>
2019-10-10 23:54:04 +0000

    Convert OriginPolicyManager in origin_policy_manager.mojom to new Mojo types
    This CL converts OriginPolicyManagerRequest and OriginPolicyManagerPtr
    to new Mojo types using PendingRemote, Remote, PendingReceiver, and
    ReceiverSet.
    
    Bug: 955171
    Change-Id: I370ef0d56a0fee3d358233f49d85f31e7233e0df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846630
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#704893}

--- services/network/test/test_network_context.h
+++ services/network/test/test_network_context.h
@@ -237,7 +237,7 @@ class TestNetworkContext : public mojom::NetworkContext {
       const GURL& url,
       LookupBasicAuthCredentialsCallback callback) override {}
   void GetOriginPolicyManager(
-      mojom::OriginPolicyManagerRequest request) override {}
+      mojo::PendingReceiver<mojom::OriginPolicyManager> receiver) override {}
 };
 
 }  // namespace network
