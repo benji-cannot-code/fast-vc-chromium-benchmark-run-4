4e9477e8c507b63fc5d4367b1ec589663cd74ba9
David Roger <droger@chromium.org>
2019-10-10 14:16:03 +0000

    Disable flaky touchToFillIntegrationTest#testClickingSuggestionsTriggersCallback
    TBR=fhorschig
    
    Bug: 1012221
    Change-Id: I3b3f8f7fd9eb4c4a30db55410b9cda4a932c4590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1851710
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704618}

--- chrome/browser/touch_to_fill/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillIntegrationTest.java
+++ chrome/browser/touch_to_fill/android/javatests/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillIntegrationTest.java
@@ -25,6 +25,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -74,6 +75,7 @@ public class TouchToFillIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1012221")
     public void testClickingSuggestionsTriggersCallback() {
         runOnUiThreadBlocking(
                 () -> { mTouchToFill.showCredentials(EXAMPLE_URL, Arrays.asList(ANA, BOB)); });
