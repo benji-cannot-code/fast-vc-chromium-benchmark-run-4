6b61f489fbdf06e245e09095d2d2f5ba7cbcf8ab
David Roger <droger@chromium.org>
2019-10-10 14:21:42 +0000

    Disable flaky PerformanceManagerTabHelperTest.PageIsAudible
    TBR=fdoray
    
    Bug: 1012601
    Change-Id: I9540e12715b4b431af0b886c280f609b51851b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1852429
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704622}

--- components/performance_manager/performance_manager_tab_helper_unittest.cc
+++ components/performance_manager/performance_manager_tab_helper_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/stl_util.h"
 #include "base/test/bind_test_util.h"
+#include "build/build_config.h"
 #include "components/performance_manager/graph/frame_node_impl.h"
 #include "components/performance_manager/graph/graph_impl_operations.h"
 #include "components/performance_manager/graph/page_node_impl.h"
@@ -219,7 +220,13 @@ void ExpectPageIsAudible(bool is_audible) {
 
 }  // namespace
 
-TEST_F(PerformanceManagerTabHelperTest, PageIsAudible) {
+// This test is flaky on windows. See https://1012601
+#if defined(OS_WIN)
+#define MAYBE_PageIsAudible DISABLED_PageIsAudible
+#else
+#define MAYBE_PageIsAudible PageIsAudible
+#endif
+TEST_F(PerformanceManagerTabHelperTest, MAYBE_PageIsAudible) {
   SetContents(CreateTestWebContents());
 
   ExpectPageIsAudible(false);
