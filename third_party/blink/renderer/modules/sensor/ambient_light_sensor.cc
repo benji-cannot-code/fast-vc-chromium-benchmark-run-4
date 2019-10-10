af777feb8afb3ad42876530a1a956e078a311104
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-10-10 17:18:20 +0000

    AmbientLightSensor: Remove unnecessary Trace() override.
    This method is just calling its parent's implementation, so there is no need
    for it.
    
    Change-Id: I32bcab5f4f9af63947b3fff87fb276a321845ef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1851708
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704680}

--- third_party/blink/renderer/modules/sensor/ambient_light_sensor.cc
+++ third_party/blink/renderer/modules/sensor/ambient_light_sensor.cc
@@ -43,8 +43,4 @@ double AmbientLightSensor::illuminance(bool& is_null) const {
   return GetReading().als.value;
 }
 
-void AmbientLightSensor::Trace(blink::Visitor* visitor) {
-  Sensor::Trace(visitor);
-}
-
 }  // namespace blink
--- third_party/blink/renderer/modules/sensor/ambient_light_sensor.h
+++ third_party/blink/renderer/modules/sensor/ambient_light_sensor.h
@@ -22,8 +22,6 @@ class AmbientLightSensor final : public Sensor {
   AmbientLightSensor(ExecutionContext*, const SensorOptions*, ExceptionState&);
 
   double illuminance(bool& is_null) const;
-
-  void Trace(blink::Visitor*) override;
 };
 
 }  // namespace blink
