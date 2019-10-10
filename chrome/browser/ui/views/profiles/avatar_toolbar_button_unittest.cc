9d23e7e7cf0c8a770d5d141d12ad64b8e5e057e5
Jan Krcal <jkrcal@chromium.org>
2019-10-10 13:27:45 +0000

    [Avatar button] Keep the email shown while parent is highlighted
    This CL keeps the user email shown also if parent container is focused
    or hovered. It introduces a notification from the container towards the
    avatar button when the highlight state changes.
    
    In a follow-up CL the same notification will also be used to hide the
    border of the button.
    
    Bug: 967317
    Change-Id: I80866957d7098c675ad20d7fd03d1f45ad5aeec9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1826678
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704595}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button_unittest.cc
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/test_with_browser_view.h"
+#include "chrome/browser/ui/views/toolbar/toolbar_account_icon_container_view.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/views/widget/widget.h"
@@ -19,7 +20,8 @@ class AvatarToolbarButtonTest : public TestWithBrowserView {};
 #if !defined(OS_CHROMEOS)
 
 TEST_F(AvatarToolbarButtonTest, HighlightMeetsMinimumContrast) {
-  auto button = std::make_unique<AvatarToolbarButton>(browser());
+  auto parent = std::make_unique<ToolbarIconContainerView>(browser());
+  auto button = std::make_unique<AvatarToolbarButton>(browser(), parent.get());
   button->set_owned_by_client();
 
   browser_view()->GetWidget()->GetContentsView()->AddChildView(button.get());
--- chrome/browser/ui/views/toolbar/toolbar_icon_container_view.h
+++ chrome/browser/ui/views/toolbar/toolbar_icon_container_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TOOLBAR_TOOLBAR_ICON_CONTAINER_VIEW_H_
 
 #include "base/macros.h"
+#include "base/observer_list.h"
 #include "ui/gfx/animation/animation_delegate.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/button_observer.h"
@@ -18,6 +19,11 @@ class ToolbarIconContainerView : public views::View,
                                  public views::ButtonObserver,
                                  public views::ViewObserver {
  public:
+  class Observer : public base::CheckedObserver {
+   public:
+    virtual void OnHighlightChanged() = 0;
+  };
+
   explicit ToolbarIconContainerView(bool uses_highlight);
   ~ToolbarIconContainerView() override;
 
@@ -26,6 +32,11 @@ class ToolbarIconContainerView : public views::View,
 
   // Adds the RHS child as well as setting its margins.
   void AddMainButton(views::Button* main_button);
+
+  void AddObserver(Observer* obs);
+  void RemoveObserver(const Observer* obs);
+
+  bool IsHighlighted();
 
   // views::ButtonObserver:
   void OnHighlightChanged(views::Button* observed_button,
@@ -68,6 +79,8 @@ class ToolbarIconContainerView : public views::View,
 
   // Fade-in/out animation for the highlight border.
   gfx::SlideAnimation highlight_animation_{this};
+
+  base::ObserverList<Observer> observers_;
 
   DISALLOW_COPY_AND_ASSIGN(ToolbarIconContainerView);
 };
