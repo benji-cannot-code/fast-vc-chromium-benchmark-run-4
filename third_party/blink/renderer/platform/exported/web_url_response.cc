c17d70e7cb29687a1f67220c4576aeeec31a61e0
Dmitry Gozman <dgozman@chromium.org>
2019-10-10 14:39:09 +0000

    Remove main resource response data from DocumentState
    Load times extension bindings are using some rare response data like
    "was alpn negotiated". Currently, these fields are stored in DocumentState,
    but they should be available on WebURLResponse instead.
    
    Moving these fields to WebURLResponse almost gets rid of DocumentState,
    which opens up more opportunities for cleanup around navigation flow.
    
    This corresponds to the step 15.e from the doc linked to the bug.
    
    TBR=csharrison@chromium.org
    
    Bug: 855189
    Change-Id: I9c83b88d5d941798be4a851166b8e9894cc82974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844052
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704625}

--- third_party/blink/renderer/platform/exported/web_url_response.cc
+++ third_party/blink/renderer/platform/exported/web_url_response.cc
@@ -304,6 +304,10 @@ void WebURLResponse::SetWasCached(bool value) {
   resource_response_->SetWasCached(value);
 }
 
+bool WebURLResponse::WasFetchedViaSPDY() const {
+  return resource_response_->WasFetchedViaSPDY();
+}
+
 void WebURLResponse::SetWasFetchedViaSPDY(bool value) {
   resource_response_->SetWasFetchedViaSPDY(value);
 }
@@ -406,6 +410,14 @@ void WebURLResponse::SetRecursivePrefetchToken(
   resource_response_->SetRecursivePrefetchToken(token);
 }
 
+bool WebURLResponse::WasAlpnNegotiated() const {
+  return resource_response_->WasAlpnNegotiated();
+}
+
+void WebURLResponse::SetWasAlpnNegotiated(bool was_alpn_negotiated) {
+  resource_response_->SetWasAlpnNegotiated(was_alpn_negotiated);
+}
+
 WebString WebURLResponse::AlpnNegotiatedProtocol() const {
   return resource_response_->AlpnNegotiatedProtocol();
 }
@@ -413,6 +425,16 @@ WebString WebURLResponse::AlpnNegotiatedProtocol() const {
 void WebURLResponse::SetAlpnNegotiatedProtocol(
     const WebString& alpn_negotiated_protocol) {
   resource_response_->SetAlpnNegotiatedProtocol(alpn_negotiated_protocol);
+}
+
+bool WebURLResponse::WasAlternateProtocolAvailable() const {
+  return resource_response_->WasAlternateProtocolAvailable();
+}
+
+void WebURLResponse::SetWasAlternateProtocolAvailable(
+    bool was_alternate_protocol_available) {
+  resource_response_->SetWasAlternateProtocolAvailable(
+      was_alternate_protocol_available);
 }
 
 net::HttpResponseInfo::ConnectionInfo WebURLResponse::ConnectionInfo() const {
