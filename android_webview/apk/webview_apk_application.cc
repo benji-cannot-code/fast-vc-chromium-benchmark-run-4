b4b5c562a40f6eab420027722436b8c343a8fba6
Robbie McElrath <rmcelrath@chromium.org>
2019-10-10 17:41:16 +0000

    [AW] Move build rules for //aw/apk/ source to //aw/apk/BUILD.gn
    Test: ninja -C out/aw system_webview_apk
    Bug: 934152
    Change-Id: I54583c5c318e974b6eba1bca17e320f1f4bbf291
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825580
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704693}

--- android_webview/apk/webview_apk_application.cc
+++ android_webview/apk/webview_apk_application.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "android_webview/browser_jni_headers/WebViewApkApplication_jni.h"
+#include "android_webview/apk/apk_jni_headers/WebViewApkApplication_jni.h"
 
 #include "android_webview/common/aw_resource_bundle.h"
 #include "base/android/base_jni_onload.h"
