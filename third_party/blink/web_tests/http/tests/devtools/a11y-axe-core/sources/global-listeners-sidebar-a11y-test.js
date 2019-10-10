f92f9b7389076dd9193d04ab9d2e7d50742061f4
Jack Lynch <jalyn@microsoft.com>
2019-10-10 18:38:15 +0000

    DevTools: Add axe test for the Global listeners pane
    This adds an axe tests for the Sources panel global listeners pane.
    
    
    Bug: 963183
    Change-Id: Iba5ef2bdc927439604f01d91c1e8b9966c43fdf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1666329
    Commit-Queue: Jack Lynch <jalyn@microsoft.com>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704732}

---
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/global-listeners-sidebar-a11y-test.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  await TestRunner.loadModule('axe_core_test_runner');
+  await TestRunner.loadModule('elements_test_runner');
+  await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.showPanel('sources');
+  await TestRunner.navigatePromise('../sources/debugger-breakpoints/resources/dom-breakpoints.html');
+
+  TestRunner.addResult('Adding global listener.');
+  await TestRunner.evaluateInPagePromise(`window.addEventListener('touchstart', () => console.log);`);
+  await UI.viewManager.showView('sources.globalListeners');
+  const globalListenersPane = self.runtime.sharedInstance(BrowserDebugger.ObjectEventListenersSidebarPane);
+  const eventListenersView = globalListenersPane._eventListenersView;
+
+  TestRunner.addResult('Dumping event listeners view:');
+  await ElementsTestRunner.expandAndDumpEventListenersPromise(eventListenersView);
+  TestRunner.addResult('Running the axe-core linter on the global listeners pane.');
+  await AxeCoreTestRunner.runValidation(globalListenersPane.contentElement);
+  TestRunner.completeTest();
+})();
