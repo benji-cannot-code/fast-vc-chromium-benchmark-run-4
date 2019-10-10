a85fd95bbb16cd0f1b911f46f5043cd445de27fc
Makoto Shimazu <shimazu@chromium.org>
2019-10-10 02:40:12 +0000

    Convert base::Bind to base::Bind{Once,Repeating} in content/browser
    This CL converts base::Bind to BindOnce or BindRepeating, and also changes
    relevant base::Callbacks or base::Closures to appropriate types.
    
    This CL is split from https://crrev.com/1831621.
    
    This CL was uploaded by git cl split.
    
    R=reillyg@chromium.org
    
    Bug: 1007760
    Change-Id: I915acf6516bd67d6c244dfb2fd6688df7717ace6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849534
    Auto-Submit: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704441}

--- content/browser/bluetooth/bluetooth_device_chooser_controller.cc
+++ content/browser/bluetooth/bluetooth_device_chooser_controller.cc
@@ -236,10 +236,11 @@ BluetoothDeviceChooserController::BluetoothDeviceChooserController(
       discovery_session_timer_(
           FROM_HERE,
           base::TimeDelta::FromSeconds(scan_duration_),
-          base::Bind(&BluetoothDeviceChooserController::StopDeviceDiscovery,
-                     // base::Timer guarantees it won't call back after its
-                     // destructor starts.
-                     base::Unretained(this))) {
+          base::BindRepeating(
+              &BluetoothDeviceChooserController::StopDeviceDiscovery,
+              // base::Timer guarantees it won't call back after its
+              // destructor starts.
+              base::Unretained(this))) {
   CHECK(adapter_);
 }
 
@@ -314,9 +315,9 @@ void BluetoothDeviceChooserController::GetDevice(
     return;
   }
 
-  BluetoothChooser::EventHandler chooser_event_handler =
-      base::Bind(&BluetoothDeviceChooserController::OnBluetoothChooserEvent,
-                 base::Unretained(this));
+  BluetoothChooser::EventHandler chooser_event_handler = base::BindRepeating(
+      &BluetoothDeviceChooserController::OnBluetoothChooserEvent,
+      base::Unretained(this));
 
   if (WebContentsDelegate* delegate = web_contents_->GetDelegate()) {
     chooser_ = delegate->RunBluetoothChooser(render_frame_host_,
--- content/browser/bluetooth/bluetooth_device_scanning_prompt_controller.cc
+++ content/browser/bluetooth/bluetooth_device_scanning_prompt_controller.cc
@@ -29,9 +29,10 @@ BluetoothDeviceScanningPromptController::
 }
 
 void BluetoothDeviceScanningPromptController::ShowPermissionPrompt() {
-  BluetoothScanningPrompt::EventHandler prompt_event_handler = base::Bind(
-      &BluetoothDeviceScanningPromptController::OnBluetoothScanningPromptEvent,
-      base::Unretained(this));
+  BluetoothScanningPrompt::EventHandler prompt_event_handler =
+      base::BindRepeating(&BluetoothDeviceScanningPromptController::
+                              OnBluetoothScanningPromptEvent,
+                          base::Unretained(this));
   WebContentsDelegate* delegate =
       WebContents::FromRenderFrameHost(render_frame_host_)->GetDelegate();
   if (delegate) {
