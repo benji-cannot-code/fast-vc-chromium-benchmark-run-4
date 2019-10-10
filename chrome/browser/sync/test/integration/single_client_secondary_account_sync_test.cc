421b66196d68cb498cc690239c643d4eed76976b
Mikel Astiz <mastiz@chromium.org>
2019-10-10 17:12:52 +0000

    Fix flakiness in ReusesSameCacheGuid
    Blocking calls to base::ReadFileToString() must be preceded by
    base::ScopedAllowBlockingForTesting.
    
    Bug: 1004312
    Change-Id: I33b421436b7c8483ff8fa646fb15caa2f7f240a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1853264
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704676}

--- chrome/browser/sync/test/integration/single_client_secondary_account_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_secondary_account_sync_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/path_service.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/threading/thread_restrictions.h"
 #include "build/build_config.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/profiles/profile.h"
@@ -194,14 +195,8 @@ IN_PROC_BROWSER_TEST_F(SingleClientSecondaryAccountSyncTest,
 // The unconsented primary account (aka secondary account) isn't supported on
 // ChromeOS, see IdentityManager::ComputeUnconsentedPrimaryAccountInfo().
 #if !defined(OS_CHROMEOS)
-// Flaky on ASan/TSan, crbug.com/1004312.
-#if defined(ADDRESS_SANITIZER) || defined(THREAD_SANITIZER)
-#define MAYBE_ReusesSameCacheGuid DISABLED_ReusesSameCacheGuid
-#else
-#define MAYBE_ReusesSameCacheGuid ReusesSameCacheGuid
-#endif
 IN_PROC_BROWSER_TEST_F(SingleClientSecondaryAccountSyncTest,
-                       MAYBE_ReusesSameCacheGuid) {
+                       ReusesSameCacheGuid) {
   ASSERT_TRUE(SetupClients()) << "SetupClients() failed.";
   ASSERT_TRUE(GetClient(0)->AwaitSyncTransportActive());
 
@@ -215,6 +210,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientSecondaryAccountSyncTest,
   ASSERT_FALSE(prefs.GetCacheGuid().empty());
 
   std::string old_cache_guid;
+  base::ScopedAllowBlockingForTesting allow_blocking;
   ASSERT_TRUE(
       base::ReadFileToString(GetTestFilePathForCacheGuid(), &old_cache_guid));
   ASSERT_FALSE(old_cache_guid.empty());
