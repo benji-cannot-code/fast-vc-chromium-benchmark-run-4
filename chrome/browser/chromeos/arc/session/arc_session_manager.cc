5605e25a8e52867c82d2475356f50494cc16e479
Dominik Röttsches <drott@chromium.org>
2019-10-10 14:06:15 +0000

    Revert "arc: Provide replacement for deprecated PlayStoreShown UMA"
    This reverts commit dcdf39248f4b5199b56a110d783c13a06fd6993f.
    
    Reason for revert: Flaky on linux-chromeos-rel, see https://crbug.com/1013148
    
    Original change's description:
    > arc: Provide replacement for deprecated PlayStoreShown UMA
    > 
    > This rolls new set of UMA that replaces derpecated PlayStoreShown UMA
    > DD:
    > https://docs.google.com/document/d/1vfJMzoYsXtH-HWYydbb5VJvpT6WeJrfvaO81RHI5v2M/edit
    > 
    > BUG=b:142251935
    > TEST=Locally + unit tests.
    > 
    > Change-Id: Ie2ec139b41e702dda604d92324b426782ed494ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1845935
    > Commit-Queue: Yury Khmel <khmel@chromium.org>
    > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#704384}
    
    TBR=xiyuan@chromium.org,yusukes@chromium.org,isherman@chromium.org,khmel@chromium.org
    
    Change-Id: Ie870309e4d2b879679e79d3b468736e51bab096f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:142251935, 1013148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1852785
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704615}

--- chrome/browser/chromeos/arc/session/arc_session_manager.cc
+++ chrome/browser/chromeos/arc/session/arc_session_manager.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/arc_migration_guide_notification.h"
 #include "chrome/browser/chromeos/arc/arc_optin_uma.h"
 #include "chrome/browser/chromeos/arc/arc_support_host.h"
-#include "chrome/browser/chromeos/arc/arc_ui_availability_reporter.h"
 #include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/arc/auth/arc_auth_service.h"
 #include "chrome/browser/chromeos/arc/optin/arc_terms_of_service_default_negotiator.h"
@@ -522,7 +521,6 @@ void ArcSessionManager::Shutdown() {
   }
   pai_starter_.reset();
   fast_app_reinstall_starter_.reset();
-  arc_ui_availability_reporter_.reset();
   profile_ = nullptr;
   state_ = State::NOT_INITIALIZED;
   if (scoped_opt_in_tracker_) {
@@ -682,9 +680,7 @@ bool ArcSessionManager::RequestEnableImpl() {
   // the initial request.
   // |prefs::kArcProvisioningInitiatedFromOobe| is reset when provisioning is
   // done or ARC is opted out.
-  const bool opt_in_start = IsArcOobeOptInActive();
-  const bool signed_in = prefs->GetBoolean(prefs::kArcSignedIn);
-  if (opt_in_start)
+  if (IsArcOobeOptInActive())
     prefs->SetBoolean(prefs::kArcProvisioningInitiatedFromOobe, true);
 
   // If it is marked that sign in has been successfully done or if Play Store is
@@ -694,9 +690,9 @@ bool ArcSessionManager::RequestEnableImpl() {
   // In Public Session mode ARC should be started silently without user
   // interaction. If opt-in verification is disabled, skip negotiation, too.
   // This is for testing purpose.
-  const bool start_arc_directly = signed_in || ShouldArcAlwaysStart() ||
-                                  IsRobotOrOfflineDemoAccountMode() ||
-                                  IsArcOptInVerificationDisabled();
+  const bool start_arc_directly =
+      prefs->GetBoolean(prefs::kArcSignedIn) || ShouldArcAlwaysStart() ||
+      IsRobotOrOfflineDemoAccountMode() || IsArcOptInVerificationDisabled();
 
   // When ARC is blocked because of filesystem compatibility, do not proceed
   // to starting ARC nor follow further state transitions.
@@ -706,18 +702,6 @@ bool ArcSessionManager::RequestEnableImpl() {
     if (!start_arc_directly && g_ui_enabled)
       arc::ShowArcMigrationGuideNotification(profile_);
     return false;
-  }
-
-  // ARC might be re-enabled and in this case |arc_ui_availability_reporter_| is
-  // already set.
-  if (!arc_ui_availability_reporter_) {
-    arc_ui_availability_reporter_ = std::make_unique<ArcUiAvailabilityReporter>(
-        profile_,
-        opt_in_start
-            ? ArcUiAvailabilityReporter::Mode::kOobeProvisioning
-            : signed_in
-                  ? ArcUiAvailabilityReporter::Mode::kAlreadyProvisioned
-                  : ArcUiAvailabilityReporter::Mode::kInSessionProvisioning);
   }
 
   if (!pai_starter_ && IsPlayStoreAvailable())
@@ -769,7 +753,6 @@ void ArcSessionManager::RequestDisable() {
   scoped_opt_in_tracker_.reset();
   pai_starter_.reset();
   fast_app_reinstall_starter_.reset();
-  arc_ui_availability_reporter_.reset();
 
   // Reset any pending request to re-enable ARC.
   reenable_arc_ = false;
