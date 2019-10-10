bc31efa988fa0d1723795ba17e08827fe417cc0a
Bettina <bdea@chromium.org>
2019-10-10 00:13:29 +0000

    Remove change_password_handler code.
    The change password card in the settings is not triggered
    from password protection so it should be removed completely.
    This is part 2 of the removal of the change password handler.
    First part was crrev.com/c/1834950.
    
    Bug: 991764
    Change-Id: I2d877366f67bb7e30763b232211df62458554084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834906
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704421}

--- chrome/browser/resources/settings/basic_page/basic_page.js
+++ chrome/browser/resources/settings/basic_page/basic_page.js
@@ -57,11 +57,6 @@ Polymer({
     /** @type {!AndroidAppsInfo|undefined} */
     androidAppsInfo: Object,
 
-    showChangePassword: {
-      type: Boolean,
-      value: false,
-    },
-
     /**
      * Dictionary defining page visibility.
      * @type {!PageVisibility}
@@ -149,10 +144,6 @@ Polymer({
 
     this.allowCrostini_ = loadTimeData.valueExists('allowCrostini') &&
         loadTimeData.getBoolean('allowCrostini');
-
-    this.addWebUIListener('change-password-visibility', visibility => {
-      this.showChangePassword = visibility;
-    });
 
     if (settings.AndroidAppsBrowserProxyImpl) {
       this.addWebUIListener(
