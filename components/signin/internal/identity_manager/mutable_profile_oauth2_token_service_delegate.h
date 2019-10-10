66e23cb0859e1db064d6ff8a67748811d7bdda78
Kush Sinha <sinhak@chromium.org>
2019-10-10 14:20:00 +0000

    Cleanup conditional token revocation in MPO2TSDelegate
    MutableProfileOAuth2TokenServiceDelegate takes a boolean constructor
    parameter to assess whether token revocation is supported. This was
    required only on Chrome OS, for supervised users.
    
    With the introduction of Chrome OS Account Manager, this code path is no
    longer used. i.e. revoking all tokens from "Chrome" is no longer
    supported. This was partially cleaned up in https://crrev.com/c/1833609,
    which set the aforementioned parameter to |true| by default.
    
    Clean it up completely by removing the parameter altogether since it is
    always set to |true| anyways.
    
    Bug: 1010532
    Change-Id: Idf585b5e6c2096febce83e641443fce394968319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1852427
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704620}

--- components/signin/internal/identity_manager/mutable_profile_oauth2_token_service_delegate.h
+++ components/signin/internal/identity_manager/mutable_profile_oauth2_token_service_delegate.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SIGNIN_INTERNAL_IDENTITY_MANAGER_MUTABLE_PROFILE_OAUTH2_TOKEN_SERVICE_DELEGATE_H_
 #define COMPONENTS_SIGNIN_INTERNAL_IDENTITY_MANAGER_MUTABLE_PROFILE_OAUTH2_TOKEN_SERVICE_DELEGATE_H_
 
+#include <map>
 #include <memory>
+#include <string>
 #include <vector>
 
 #include "base/callback.h"
@@ -40,7 +42,6 @@ class MutableProfileOAuth2TokenServiceDelegate
       scoped_refptr<TokenWebData> token_web_data,
       signin::AccountConsistencyMethod account_consistency,
       bool revoke_all_tokens_on_load,
-      bool can_revoke_credentials,
       FixRequestErrorCallback fix_request_error_callback);
   ~MutableProfileOAuth2TokenServiceDelegate() override;
 
@@ -229,11 +230,6 @@ class MutableProfileOAuth2TokenServiceDelegate
   // completely removed, and the primary account will be kept in authentication
   // error state.
   const bool revoke_all_tokens_on_load_;
-
-  // Supervised users cannot revoke credentials.
-  // TODO(droger): remove this when supervised users are no longer supported on
-  // any platform.
-  const bool can_revoke_credentials_;
 
   // Callback function that attempts to correct request errors.  Best effort
   // only.  Returns true if the error was fixed and retry should be reattempted.
