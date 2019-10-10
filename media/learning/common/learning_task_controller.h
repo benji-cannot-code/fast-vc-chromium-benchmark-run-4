9563d048b068898f552d46b5b0db57bee69a75ce
liberato@chromium.org <liberato@chromium.org>
2019-10-10 19:33:11 +0000

    Add default target value to LearningTaskController::BeginObservation
    Previously, destroying a LearningTaskController would cancel any in-
    flight observations.  However, it's convenient to sometimes specify
    a default value to complete them with if, for example, a browser tab
    is closed while an observation is pending.
    
    This CL adds an optional TargetValue to BeginObservation that will
    be used to complete the observation if the controller is destroyed.
    While it might make more sense to include it only on the mojo
    client, instead of the base interface, it turns out to be easier
    just to put it there.
    
    Change-Id: I9ed7ebf089be123a905265a780e427d0b11dfa54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841248
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704765}

--- media/learning/common/learning_task_controller.h
+++ media/learning/common/learning_task_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/component_export.h"
 #include "base/macros.h"
+#include "base/optional.h"
 #include "base/unguessable_token.h"
 #include "media/learning/common/labelled_example.h"
 #include "media/learning/common/learning_task.h"
@@ -58,14 +59,19 @@ class COMPONENT_EXPORT(LEARNING_COMMON) LearningTaskController {
   // features into an example for training a model, then call
   // CompleteObservation with the same id and an ObservationCompletion.
   // Otherwise, call CancelObservation with |id|.  It's also okay to destroy the
-  // controller with outstanding observations; these will be cancelled.
+  // controller with outstanding observations; these will be cancelled if no
+  // |default_target| was specified, or completed with |default_target|.
+  //
   // TODO(liberato): This should optionally take a callback to receive a
   // prediction for the FeatureVector.
   // TODO(liberato): See if this ends up generating smaller code with pass-by-
   // value or with |FeatureVector&&|, once we have callers that can actually
   // benefit from it.
-  virtual void BeginObservation(base::UnguessableToken id,
-                                const FeatureVector& features) = 0;
+  virtual void BeginObservation(
+      base::UnguessableToken id,
+      const FeatureVector& features,
+      const base::Optional<TargetValue>& default_target =
+          base::Optional<TargetValue>()) = 0;
 
   // Complete an observation by sending a completion.
   virtual void CompleteObservation(base::UnguessableToken id,
