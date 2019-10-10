88525fec0463980d91d61786c19f9335baf81de1
Julie Jeongeun Kim <jkim@igalia.com>
2019-10-10 02:58:41 +0000

    Convert MdnsResponder in mdns_responder.mojom to new Mojo types
    This CL converts MdnsResponder in mdns_responder.mojom using
    SharedRemote and PendingRemote.
    
    Bug: 955171,978694
    Change-Id: Idd336a690d55e65817ec2a42bd173dc2b3dd59fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846217
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#704475}

--- third_party/blink/renderer/platform/p2p/mdns_responder_adapter.h
+++ third_party/blink/renderer/platform/p2p/mdns_responder_adapter.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_P2P_MDNS_RESPONDER_ADAPTER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_P2P_MDNS_RESPONDER_ADAPTER_H_
 
+#include "mojo/public/cpp/bindings/shared_remote.h"
 #include "services/network/public/mojom/mdns_responder.mojom-blink.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/webrtc/rtc_base/mdns_responder_interface.h"
@@ -35,8 +36,8 @@ class PLATFORM_EXPORT MdnsResponderAdapter
                             NameRemovedCallback callback) override;
 
  private:
-  scoped_refptr<network::mojom::blink::ThreadSafeMdnsResponderPtr>
-      thread_safe_client_;
+  mojo::SharedRemote<network::mojom::blink::MdnsResponder>
+      shared_remote_client_;
 
   DISALLOW_COPY_AND_ASSIGN(MdnsResponderAdapter);
 };
