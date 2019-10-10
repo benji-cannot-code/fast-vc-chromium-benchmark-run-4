b021adffff99408ad284369b0e6da1e2a3aec725
yilkal <yilkal@chromium.org>
2019-10-10 02:39:44 +0000

    Record the number of blocked iframes per unblocked main frame load.
    Bug: 850328
    Change-Id: Iae5af42094a9ac0943b7a50f270abaae11b23f23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838521
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704440}

--- chrome/browser/supervised_user/supervised_user_navigation_observer.cc
+++ chrome/browser/supervised_user/supervised_user_navigation_observer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/metrics/histogram_macros.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/supervised_user/supervised_user_interstitial.h"
@@ -117,6 +118,21 @@ void SupervisedUserNavigationObserver::FrameDeleted(
     content::RenderFrameHost* render_frame_host) {
   int frame_id = render_frame_host->GetFrameTreeNodeId();
   supervised_user_interstitials_.erase(frame_id);
+}
+
+void SupervisedUserNavigationObserver::DidFinishLoad(
+    content::RenderFrameHost* render_frame_host,
+    const GURL& validated_url) {
+  if (IsMainFrame(render_frame_host)) {
+    bool main_frame_blocked =
+        base::Contains(supervised_user_interstitials_,
+                       render_frame_host->GetFrameTreeNodeId());
+    int count = supervised_user_interstitials_.size();
+    if (main_frame_blocked)
+      count = 0;
+
+    UMA_HISTOGRAM_COUNTS_1000("ManagedUsers.BlockedIframeCount", count);
+  }
 }
 
 void SupervisedUserNavigationObserver::OnURLFilterChanged() {
