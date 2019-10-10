b0cb398b71b048f8abc738e87852b773a9f4cb2e
Clemens Arbesser <arbesser@google.com>
2019-10-10 12:51:12 +0000

    [Autofill Assistant] Clear field inputs immediately after use.
    See bug description for more info.
    
    Bug: b/142454202
    Change-Id: Iadebe24cc579502f32db8503e35e262651464ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1852087
    Reviewed-by: Jordan Demeulenaere <jdemeulenaere@chromium.org>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#704583}

--- components/autofill_assistant/browser/actions/set_form_field_value_action.h
+++ components/autofill_assistant/browser/actions/set_form_field_value_action.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "components/autofill_assistant/browser/actions/action.h"
@@ -25,6 +26,9 @@ class SetFormFieldValueAction : public Action {
   ~SetFormFieldValueAction() override;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(SetFormFieldValueActionTest,
+                           PasswordIsClearedFromMemory);
+
   // A field input as extracted from the proto, but already checked for
   // validity.
   struct FieldInput {
