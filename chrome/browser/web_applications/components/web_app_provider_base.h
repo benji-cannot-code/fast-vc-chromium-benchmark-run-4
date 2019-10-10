f49a576b10da6b241b3b4afc35dc2d92fe26d59c
Jay Harris <harrisjay@chromium.org>
2019-10-10 04:56:59 +0000

    WebApps: Creates an AppShortcutManager and specific Bookmark and Web subclasses.
    This CL is just the boilerplate of creating new classes and wiring them
    up to WebAppProvider.
    
    Eventually, the functionality from web_app_extension_shortcut should be
    migrated into this class. A WIP CL for beginning this is available here:
    https://crrev.com/c/1846422.
    
    The motivation for beginning this now is file handling on Linux, as
    registering and unregistering file handlers in the OS is tightly
    coupled to shortcut management.
    
    Bug: 860581
    Change-Id: I3e28018d6fd6656fccd3c22ed62b9576db43f51c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1847708
    Commit-Queue: Jay Harris <harrisjay@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704484}

--- chrome/browser/web_applications/components/web_app_provider_base.h
+++ chrome/browser/web_applications/components/web_app_provider_base.h
@@ -21,6 +21,7 @@ class AppRegistrar;
 class AppRegistryController;
 class FileHandlerManager;
 class AppIconManager;
+class AppShortcutManager;
 class WebAppPolicyManager;
 class ManifestUpdateManager;
 class WebAppAudioFocusIdMap;
@@ -58,6 +59,8 @@ class WebAppProviderBase : public KeyedService {
 
   // Implements fetching of app icons.
   virtual AppIconManager& icon_manager() = 0;
+
+  virtual AppShortcutManager& shortcut_manager() = 0;
 
   DISALLOW_COPY_AND_ASSIGN(WebAppProviderBase);
 };
--- chrome/browser/web_applications/web_app_provider.h
+++ chrome/browser/web_applications/web_app_provider.h
@@ -32,6 +32,7 @@ namespace web_app {
 // Forward declarations of generalized interfaces.
 class AppRegistryController;
 class AppIconManager;
+class AppShortcutManager;
 class ExternalWebAppManager;
 class FileHandlerManager;
 class InstallFinalizer;
@@ -78,6 +79,7 @@ class WebAppProvider : public WebAppProviderBase {
   WebAppAudioFocusIdMap& audio_focus_id_map() override;
   FileHandlerManager& file_handler_manager() override;
   AppIconManager& icon_manager() override;
+  AppShortcutManager& shortcut_manager() override;
 
   SystemWebAppManager& system_web_app_manager();
 
@@ -122,6 +124,7 @@ class WebAppProvider : public WebAppProviderBase {
   std::unique_ptr<InstallFinalizer> install_finalizer_;
   std::unique_ptr<ManifestUpdateManager> manifest_update_manager_;
   std::unique_ptr<PendingAppManager> pending_app_manager_;
+  std::unique_ptr<AppShortcutManager> shortcut_manager_;
   std::unique_ptr<SystemWebAppManager> system_web_app_manager_;
   std::unique_ptr<WebAppAudioFocusIdMap> audio_focus_id_map_;
   std::unique_ptr<WebAppInstallManager> install_manager_;
