2efaccfb0d7d1962028dc4c4d161a0ac4088efab
Christopher Cameron <ccameron@chromium.org>
2019-10-10 19:26:54 +0000

    Add about:flags entry for Metal
    Bug: 952063, 988964
    Change-Id: Ie78e8c7b264dd5f713949cc4acbc6ad5df657f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849982
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704759}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3121,6 +3121,11 @@ const char kMacSystemMediaPermissionsInfoUiDescription[] =
     "the Omnibox, which, when clicked, displays a bubble with information on "
     "how to toggle Chrome's system-level media permissions.";
 
+const char kMetalName[] = "Metal";
+const char kMetalDescription[] =
+    "Use Metal instead of OpenGL for rasterization (if out-of-process "
+    "rasterization is enabled) and display (if the Skia renderer is enabled)";
+
 #endif
 
 // Chrome OS -------------------------------------------------------------------
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1833,6 +1833,9 @@ extern const char kMacViewsTaskManagerDescription[];
 extern const char kMacSystemMediaPermissionsInfoUiName[];
 extern const char kMacSystemMediaPermissionsInfoUiDescription[];
 
+extern const char kMetalName[];
+extern const char kMetalDescription[];
+
 // Non-Mac --------------------------------------------------------------------
 
 #else  // !defined(OS_MACOSX)
