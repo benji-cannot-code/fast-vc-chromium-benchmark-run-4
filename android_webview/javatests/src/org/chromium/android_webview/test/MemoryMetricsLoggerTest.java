e6f79507c66177e0f97274c229969f0d7c331898
Tao Bai <michaelbai@chromium.org>
2019-10-10 21:09:58 +0000

    AW: Add Memory.Total.PrivateMemoryFootprint
    Bug: 1012823
    Change-Id: Ibe238ddd409a00ae256d15e705d2c95dfad67f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849633
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704830}

--- android_webview/javatests/src/org/chromium/android_webview/test/MemoryMetricsLoggerTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/MemoryMetricsLoggerTest.java
@@ -55,6 +55,9 @@ public class MemoryMetricsLoggerTest {
         Assert.assertNotEquals(0,
                 RecordHistogram.getHistogramTotalCountForTesting(
                         "Memory.Renderer.PrivateMemoryFootprint"));
+        Assert.assertNotEquals(0,
+                RecordHistogram.getHistogramTotalCountForTesting(
+                        "Memory.Total.PrivateMemoryFootprint"));
     }
 
     @Test
@@ -65,9 +68,13 @@ public class MemoryMetricsLoggerTest {
         Assert.assertNotEquals(0,
                 RecordHistogram.getHistogramTotalCountForTesting(
                         "Memory.Browser.PrivateMemoryFootprint"));
+        // Verify no renderer record in single process mode.
         Assert.assertEquals(0,
                 RecordHistogram.getHistogramTotalCountForTesting(
                         "Memory.Renderer.PrivateMemoryFootprint"));
+        Assert.assertNotEquals(0,
+                RecordHistogram.getHistogramTotalCountForTesting(
+                        "Memory.Total.PrivateMemoryFootprint"));
     }
 
     /**
