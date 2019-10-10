c731b9853f6fda4a4ad22064c8cfe63547485d56
Mohsen Izadi <mohsen@chromium.org>
2019-10-10 21:28:04 +0000

    Remove OzonePlatform::EnsureInstance()
    Everywhere we access OzonePlatform, it is already initialized (and
    should be in future). There is no need for EnsureInstance() anymore.
    
    BUG=958387
    
    Change-Id: I1adf0f65ce8a446275417856caeb56c2310bea2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1763633
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704838}

--- ui/ozone/public/ozone_platform.cc
+++ ui/ozone/public/ozone_platform.cc
@@ -19,7 +19,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 namespace {
+
 OzonePlatform* g_instance = nullptr;
+
+void EnsureInstance() {
+  if (g_instance)
+    return;
+
+  TRACE_EVENT1("ozone", "OzonePlatform::Initialize", "platform",
+               GetOzonePlatformName());
+  std::unique_ptr<OzonePlatform> platform =
+      PlatformObject<OzonePlatform>::Create();
+
+  // TODO(spang): Currently need to leak this object.
+  OzonePlatform* pl = platform.release();
+  DCHECK_EQ(g_instance, pl);
+}
+
 }  // namespace
 
 OzonePlatform::OzonePlatform() {
@@ -61,23 +77,6 @@ OzonePlatform* OzonePlatform::GetInstance() {
 // static
 const char* OzonePlatform::GetPlatformName() {
   return GetOzonePlatformName();
-}
-
-// static
-OzonePlatform* OzonePlatform::EnsureInstance() {
-  if (!g_instance) {
-    TRACE_EVENT1("ozone",
-                 "OzonePlatform::Initialize",
-                 "platform",
-                 GetOzonePlatformName());
-    std::unique_ptr<OzonePlatform> platform =
-        PlatformObject<OzonePlatform>::Create();
-
-    // TODO(spang): Currently need to leak this object.
-    OzonePlatform* pl = platform.release();
-    DCHECK_EQ(g_instance, pl);
-  }
-  return g_instance;
 }
 
 IPC::MessageFilter* OzonePlatform::GetGpuMessageFilter() {
