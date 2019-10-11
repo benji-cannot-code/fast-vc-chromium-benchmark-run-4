c2a9bdb95c67d9c4ee280df7cbc37f0f68457193
Brandon Goddard <brgoddar@microsoft.com>
2019-10-11 17:38:45 +0000

    Devtools: Screen Reader fix for Profiler (Memory and CPU) Datagrid
    This change contains a small fix for the Profiler Datagrid, which is
    used in the Top down and bottom up views for Memory Panel Allocation
    samples and the Javascript Profiler tool.
    
    It adds an aria label to the percentage columns to separate the raw
    number from the percentage of the Heap/CPU
    (uses a comma in the aria label,
    which causes screen readers to separate the 2 numbers when reading)
    
    Before:
    Screen Reader: https://imgur.com/OA0scyB
    
    After:
    Screen Reader: https://imgur.com/a/Ke7m76C
    
    Bug: 963183
    Change-Id: I6e8f9fe59cbe17b90d581a52fc08d0024fe09107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1730595
    Commit-Queue: Brandon Goddard <brgoddar@microsoft.com>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Jeff Fisher <jeffish@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#705173}

--- third_party/blink/renderer/devtools/front_end/profiler/CPUProfileView.js
+++ third_party/blink/renderer/devtools/front_end/profiler/CPUProfileView.js
@@ -283,6 +283,15 @@ Profiler.CPUProfileView.NodeFormatter = class {
   /**
    * @override
    * @param {number} value
+   * @return {string}
+   */
+  formatValueAccessibleText(value) {
+    return this.formatValue(value);
+  }
+
+  /**
+   * @override
+   * @param {number} value
    * @param {!Profiler.ProfileDataGridNode} node
    * @return {string}
    */
