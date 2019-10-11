6fcfe0c736f43a1d4006063a6af9453479dcfda7
Klaus Weidner <klausw@chromium.org>
2019-10-11 22:37:07 +0000

    Refactor AR frame transport and MailboxToSurfaceBridge use
    Instead of partially initializing this in arcore_device, just pass the initial
    object and let the GL thread take care of setting it up.
    
    This also makes it possible to simplify mailbox_to_surface_bridge, it can now
    assume that all post-creation methods are used on a single GL thread.
    
    Add logic to enforce that at most one frame is allowed to be in "processing"
    state, deferring the next frame in case of attempted overlap. This matches
    the current logic used in GVR's immersive-vr mode.
    
    Also update some stale comments and make simplifications that are now possible
    due to the new lifecycle.
    
    Bug: 1012972, 836524
    Change-Id: I9b85bb6dd261c0c2fa41f443222e34b76a64c5c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1854506
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705317}

--- chrome/browser/android/vr/arcore_device/ar_image_transport.h
+++ chrome/browser/android/vr/arcore_device/ar_image_transport.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
 #include "chrome/browser/android/vr/arcore_device/ar_renderer.h"
 #include "device/vr/public/mojom/vr_service.mojom.h"
@@ -30,30 +31,40 @@ struct WebXrSharedBuffer;
 
 namespace device {
 
-// This class copies the camera texture to a shared image and returns a mailbox
-// holder which is suitable for mojo transport to the Renderer.
+// This class handles transporting WebGL rendered output from the GPU process's
+// command buffer GL context to the local GL context, and compositing WebGL
+// output onto the camera image using the local GL context.
 class ArImageTransport {
  public:
   explicit ArImageTransport(
       std::unique_ptr<vr::MailboxToSurfaceBridge> mailbox_bridge);
   virtual ~ArImageTransport();
 
-  // Initialize() must be called on a valid GL thread.
-  virtual bool Initialize(vr::WebXrPresentationState* webxr);
+  virtual void DestroySharedBuffers(vr::WebXrPresentationState* webxr);
+
+  // All methods must be called on a valid GL thread. Initialization
+  // must happen after the local GL context is ready for use. That
+  // starts the asynchronous setup for the GPU process command buffer
+  // GL context via MailboxToSurfaceBridge, and the callback is called
+  // once that's complete.
+  virtual void Initialize(vr::WebXrPresentationState* webxr,
+                          base::OnceClosure callback);
 
   virtual GLuint GetCameraTextureId();
 
   // This transfers whatever the contents of the texture specified
   // by GetCameraTextureId() is at the time it is called and returns
   // a gpu::MailboxHolder with that texture copied to a shared buffer.
-  virtual gpu::MailboxHolder TransferFrame(const gfx::Size& frame_size,
+  virtual gpu::MailboxHolder TransferFrame(vr::WebXrPresentationState* webxr,
+                                           const gfx::Size& frame_size,
                                            const gfx::Transform& uv_transform);
   virtual void CreateGpuFenceForSyncToken(
       const gpu::SyncToken& sync_token,
       base::OnceCallback<void(std::unique_ptr<gfx::GpuFence>)>);
   virtual void CopyCameraImageToFramebuffer(const gfx::Size& frame_size,
                                             const gfx::Transform& uv_transform);
-  virtual void CopyDrawnImageToFramebuffer(const gfx::Size& frame_size,
+  virtual void CopyDrawnImageToFramebuffer(vr::WebXrPresentationState* webxr,
+                                           const gfx::Size& frame_size,
                                            const gfx::Transform& uv_transform);
   virtual void CopyTextureToFramebuffer(GLuint texture,
                                         const gfx::Size& frame_size,
@@ -62,10 +73,13 @@ class ArImageTransport {
 
  private:
   std::unique_ptr<vr::WebXrSharedBuffer> CreateBuffer();
-  void ResizeSharedBuffer(const gfx::Size& size, vr::WebXrSharedBuffer* buffer);
+  void ResizeSharedBuffer(vr::WebXrPresentationState* webxr,
+                          const gfx::Size& size,
+                          vr::WebXrSharedBuffer* buffer);
   bool IsOnGlThread() const;
+  void OnMailboxBridgeReady(base::OnceClosure callback);
   std::unique_ptr<ArRenderer> ar_renderer_;
-  // samplerExternalOES texture data for WebXR content image.
+  // samplerExternalOES texture for the camera image.
   GLuint camera_texture_id_arcore_ = 0;
   GLuint camera_fbo_ = 0;
 
@@ -73,8 +87,8 @@ class ArImageTransport {
 
   std::unique_ptr<vr::MailboxToSurfaceBridge> mailbox_bridge_;
 
-  vr::WebXrPresentationState* webxr_ = nullptr;
-
+  // Must be last.
+  base::WeakPtrFactory<ArImageTransport> weak_ptr_factory_{this};
   DISALLOW_COPY_AND_ASSIGN(ArImageTransport);
 };
 
