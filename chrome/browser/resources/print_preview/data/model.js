eecf2a6971060acb5c0561a0d82fe6e403080d19
rbpotter <rbpotter@chromium.org>
2019-10-11 16:49:48 +0000

    Print Preview: Fix flash of more settings expansion
    Initialize everything to available, so that we don't briefly expand more
    settings due to having <= 6 available settings between setting
    availability from document settings and setting availability from
    destination capabilities for PDFs.
    
    Bug: 1013045
    Change-Id: Idbcaa78d433f314415425e6ebaf699122c6d7803
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1853824
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705147}

--- chrome/browser/resources/print_preview/data/model.js
+++ chrome/browser/resources/print_preview/data/model.js
@@ -190,9 +190,9 @@ Polymer({
     /**
      * Object containing current settings of Print Preview, for use by Polymer
      * controls.
-     * Initialize settings that are only available on some printers to
-     * unavailable, and settings that are provided by PDF generation to
-     * available.
+     * Initialize all settings to available so that more settings always stays
+     * in a collapsed state during startup, when document information and
+     * printer capabilities may arrive at slightly different times.
      * @type {!print_preview.Settings}
      */
     settings: {
@@ -244,7 +244,7 @@ Polymer({
             value: true, /* color */
             unavailableValue: false,
             valid: true,
-            available: false,
+            available: true,
             setByPolicy: false,
             setFromUi: false,
             key: 'isColorEnabled',
@@ -257,7 +257,7 @@ Polymer({
               height_microns: 279400,
             },
             valid: true,
-            available: false,
+            available: true,
             setByPolicy: false,
             setFromUi: false,
             key: 'mediaSize',
@@ -287,7 +287,7 @@ Polymer({
             value: {},
             unavailableValue: {},
             valid: true,
-            available: false,
+            available: true,
             setByPolicy: false,
             setFromUi: false,
             key: 'dpi',
@@ -327,7 +327,7 @@ Polymer({
             value: true,
             unavailableValue: false,
             valid: true,
-            available: false,
+            available: true,
             setByPolicy: false,
             setFromUi: false,
             key: 'isDuplexEnabled',
@@ -387,7 +387,7 @@ Polymer({
             value: {},
             unavailableValue: {},
             valid: true,
-            available: false,
+            available: true,
             setByPolicy: false,
             setFromUi: false,
             key: 'vendorOptions',
@@ -442,7 +442,7 @@ Polymer({
             value: false,
             unavailableValue: false,
             valid: true,
-            available: false,
+            available: true,
             setByPolicy: false,
             setFromUi: false,
             key: 'isPinEnabled',
@@ -452,7 +452,7 @@ Polymer({
             value: '',
             unavailableValue: '',
             valid: true,
-            available: false,
+            available: true,
             setByPolicy: false,
             setFromUi: false,
             key: 'pinValue',
