d243d58d05b2604036ad3fe33b5de06141b78f66
Tim Song <tengs@chromium.org>
2019-10-11 00:55:11 +0000

    Ash Tray: Do not announce certain buttons as toggle buttons for ChromeVox.
    Certain feature pod buttons (e.g. accesibility) are not togglable, but are
    currently announced as such for ChromeVox spoken feedback.
    
    BUG=1009727
    
    Change-Id: I2db6c3a53bb04ba677457ff5590574d2c384b503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1848454
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704913}

--- ash/system/unified/feature_pod_button.cc
+++ ash/system/unified/feature_pod_button.cc
@@ -43,8 +43,9 @@ void ConfigureFeaturePodLabel(views::Label* label) {
 
 }  // namespace
 
-FeaturePodIconButton::FeaturePodIconButton(views::ButtonListener* listener)
-    : views::ImageButton(listener) {
+FeaturePodIconButton::FeaturePodIconButton(views::ButtonListener* listener,
+                                           bool is_togglable)
+    : views::ImageButton(listener), is_togglable_(is_togglable) {
   SetPreferredSize(kUnifiedFeaturePodIconSize);
   SetBorder(views::CreateEmptyBorder(kUnifiedFeaturePodIconPadding));
   SetImageHorizontalAlignment(ALIGN_CENTER);
@@ -56,6 +57,9 @@ FeaturePodIconButton::FeaturePodIconButton(views::ButtonListener* listener)
 FeaturePodIconButton::~FeaturePodIconButton() = default;
 
 void FeaturePodIconButton::SetToggled(bool toggled) {
+  if (!is_togglable_)
+    return;
+
   toggled_ = toggled;
   SchedulePaint();
 }
@@ -115,9 +119,13 @@ std::unique_ptr<views::InkDropMask> FeaturePodIconButton::CreateInkDropMask()
 void FeaturePodIconButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   ImageButton::GetAccessibleNodeData(node_data);
   node_data->SetName(GetTooltipText(gfx::Point()));
-  node_data->role = ax::mojom::Role::kToggleButton;
-  node_data->SetCheckedState(toggled_ ? ax::mojom::CheckedState::kTrue
-                                      : ax::mojom::CheckedState::kFalse);
+  if (is_togglable_) {
+    node_data->role = ax::mojom::Role::kToggleButton;
+    node_data->SetCheckedState(toggled_ ? ax::mojom::CheckedState::kTrue
+                                        : ax::mojom::CheckedState::kFalse);
+  } else {
+    node_data->role = ax::mojom::Role::kButton;
+  }
 }
 
 const char* FeaturePodIconButton::GetClassName() const {
@@ -271,9 +279,10 @@ void FeaturePodLabelButton::LayoutInCenter(views::View* child, int y) {
       child_width, preferred_size.height());
 }
 
-FeaturePodButton::FeaturePodButton(FeaturePodControllerBase* controller)
+FeaturePodButton::FeaturePodButton(FeaturePodControllerBase* controller,
+                                   bool is_togglable)
     : controller_(controller),
-      icon_button_(new FeaturePodIconButton(this)),
+      icon_button_(new FeaturePodIconButton(this, is_togglable)),
       label_button_(new FeaturePodLabelButton(this)) {
   auto* layout = SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical, gfx::Insets(),
