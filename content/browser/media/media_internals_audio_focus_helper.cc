b866ba60c54f80934a62c68c9820974e7caf2084
Miyoung Shin <myid.shin@igalia.com>
2019-10-11 05:18:29 +0000

    Convert AudioFocusManagerDebug to new Mojo types
    This CL converts AudioFocusManagerDebug{Ptr, Request} in content
    and services to the new Mojo type.
    
    Bug: 955171
    Change-Id: I9b219438be413d57b0c97e635d39b48d8a2352b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849734
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#704995}

--- content/browser/media/media_internals_audio_focus_helper.cc
+++ content/browser/media/media_internals_audio_focus_helper.cc
@@ -95,7 +95,7 @@ void MediaInternalsAudioFocusHelper::SetEnabled(bool enabled) {
 
   if (!enabled) {
     audio_focus_.reset();
-    audio_focus_debug_ptr_.reset();
+    audio_focus_debug_.reset();
     receiver_.reset();
   }
 }
@@ -120,10 +120,10 @@ bool MediaInternalsAudioFocusHelper::EnsureServiceConnection() {
   }
 
   // Connect to the media session service debug interface.
-  if (!audio_focus_debug_ptr_.is_bound()) {
-    connector->BindInterface(media_session::mojom::kServiceName,
-                             mojo::MakeRequest(&audio_focus_debug_ptr_));
-    audio_focus_debug_ptr_.set_connection_error_handler(
+  if (!audio_focus_debug_.is_bound()) {
+    connector->Connect(media_session::mojom::kServiceName,
+                       audio_focus_debug_.BindNewPipeAndPassReceiver());
+    audio_focus_debug_.set_disconnect_handler(
         base::BindRepeating(&MediaInternalsAudioFocusHelper::OnDebugMojoError,
                             base::Unretained(this)));
   }
@@ -145,7 +145,7 @@ void MediaInternalsAudioFocusHelper::OnMojoError() {
 }
 
 void MediaInternalsAudioFocusHelper::OnDebugMojoError() {
-  audio_focus_debug_ptr_.reset();
+  audio_focus_debug_.reset();
 }
 
 void MediaInternalsAudioFocusHelper::DidGetAudioFocusRequestList(
@@ -172,7 +172,7 @@ void MediaInternalsAudioFocusHelper::DidGetAudioFocusRequestList(
 
     request_state_.emplace(id_string, session.Clone());
 
-    audio_focus_debug_ptr_->GetDebugInfoForRequest(
+    audio_focus_debug_->GetDebugInfoForRequest(
         session->request_id.value(),
         base::BindOnce(
             &MediaInternalsAudioFocusHelper::DidGetAudioFocusDebugInfo,
