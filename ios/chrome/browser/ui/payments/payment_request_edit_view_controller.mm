cdd3ceeaa651d6dd2aedb486bffa4f840a5fad60
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-10-11 10:27:13 +0000

    [iOS][Payment-Request] Separator color
    Updates the current separator color to a dynamic one.
    
    Bug: 1008832
    Change-Id: I1b408fb1b60a6f516fd2bfab430e18f79fbb58dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1855741
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705037}

--- ios/chrome/browser/ui/payments/payment_request_edit_view_controller.mm
+++ ios/chrome/browser/ui/payments/payment_request_edit_view_controller.mm
@@ -251,6 +251,7 @@ PaymentsTextItem* ErrorMessageItemForError(NSString* errorMessage) {
   self.styler.cellStyle = MDCCollectionViewCellStyleCard;
   self.styler.separatorInset =
       UIEdgeInsetsMake(0, kSeparatorEdgeInset, 0, kSeparatorEdgeInset);
+  self.styler.separatorColor = [UIColor colorNamed:kSeparatorColor];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
