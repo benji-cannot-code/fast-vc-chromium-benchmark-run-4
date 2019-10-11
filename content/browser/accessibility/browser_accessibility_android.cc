6022d113f3fc72e65f449ff70e06fff5476bfd8f
Dominic Mazzoni <dmazzoni@chromium.org>
2019-10-11 18:43:23 +0000

    Fix accessibility issues with sliders on Android
    First, re-lands the fix to bug 836554, which ensures TalkBack
    announces when a slider changes. This time, adds a check to only fire
    the TYPE_VIEW_SELECTED event if the slider has accessibility focus,
    hoping that will address the problems with fullscreen video by suppressing
    announcements when an unfocused slider changes.
    
    Also, doesn't expose the "can scroll forwards / backwards" actions on a slider
    that isn't an HTML INPUT control, because a pure ARIA slider won't respond
    to those. That addresses bug 917845.
    
    Finally, added code to ensure that a slider updates the accessibility tree
    when its value changes - I found this was necessary in order to get
    announcements after changing a slider with the volume keys.
    
    Bug: 836554, 917845, 953652
    Change-Id: I3f1f88321a4644bc5729c16c14e0bd9037a641ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1853857
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705199}

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -1165,6 +1165,13 @@ int BrowserAccessibilityAndroid::GetItemCount() const {
 
 bool BrowserAccessibilityAndroid::CanScrollForward() const {
   if (IsSlider()) {
+    // If it's not a native INPUT element, then increment and decrement
+    // won't work.
+    std::string html_tag =
+        GetStringAttribute(ax::mojom::StringAttribute::kHtmlTag);
+    if (html_tag != "input")
+      return false;
+
     float value = GetFloatAttribute(ax::mojom::FloatAttribute::kValueForRange);
     float max = GetFloatAttribute(ax::mojom::FloatAttribute::kMaxValueForRange);
     return value < max;
@@ -1175,6 +1182,13 @@ bool BrowserAccessibilityAndroid::CanScrollForward() const {
 
 bool BrowserAccessibilityAndroid::CanScrollBackward() const {
   if (IsSlider()) {
+    // If it's not a native INPUT element, then increment and decrement
+    // won't work.
+    std::string html_tag =
+        GetStringAttribute(ax::mojom::StringAttribute::kHtmlTag);
+    if (html_tag != "input")
+      return false;
+
     float value = GetFloatAttribute(ax::mojom::FloatAttribute::kValueForRange);
     float min = GetFloatAttribute(ax::mojom::FloatAttribute::kMinValueForRange);
     return value > min;
