299e96d46aa8f6536af118fb15df8e9c067d319d
Pavel Shmakov <pshmakov@google.com>
2019-10-11 12:29:47 +0000

    Add canGoBack, canGoForward APIs
    Change-Id: I74d0822b697f55a9d1e53a106a60d784c352a9f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1852214
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Pavel Shmakov <pshmakov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705071}

--- weblayer/browser/navigation_controller_impl.h
+++ weblayer/browser/navigation_controller_impl.h
@@ -41,6 +41,12 @@ class NavigationControllerImpl : public NavigationController,
   void GoForward(JNIEnv* env, const base::android::JavaParamRef<jobject>& obj) {
     GoForward();
   }
+  bool CanGoBack(JNIEnv* env, const base::android::JavaParamRef<jobject>& obj) {
+    return CanGoBack();
+  }
+  bool CanGoForward(JNIEnv* env, const base::android::JavaParamRef<jobject>& obj) {
+    return CanGoForward();
+  }
   void Reload(JNIEnv* env, const base::android::JavaParamRef<jobject>& obj) {
     Reload();
   }
@@ -69,6 +75,8 @@ class NavigationControllerImpl : public NavigationController,
   void Navigate(const GURL& url) override;
   void GoBack() override;
   void GoForward() override;
+  bool CanGoBack() override;
+  bool CanGoForward() override;
   void Reload() override;
   void Stop() override;
   int GetNavigationListSize() override;
