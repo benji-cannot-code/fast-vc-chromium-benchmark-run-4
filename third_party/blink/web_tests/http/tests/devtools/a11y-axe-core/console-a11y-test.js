0ff4c555caaa30024dd6108b9a9683b401d92d19
Mike Jackson <mjackson@microsoft.com>
2019-10-11 21:54:37 +0000

    DevTools: Update axe-core rules
    This change ensures that we only run best-practice
    and wcag2a rules, and disables low value or performance
    impacting rules.
    
    These are the net new rules enabled:
    - duplicate-id-active
    - duplicate-id-aria
    - input-button-name
    - role-img-alt
    
    This change does not disable any rules that were
    previously running prior to the axe-core upgrade.
    
    Change-Id: I3b1942e09b29aed6e1055e5159d2d52eb2aa260d
    Bug: 1013391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1854561
    Commit-Queue: Mike Jackson <mjackson@microsoft.com>
    Reviewed-by: Lorne Mitchell <lomitch@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#705295}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/console-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/console-a11y-test.js
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await UI.viewManager.showView('console');
   const widget = await UI.viewManager.view('console').widget();
 
-  //TODO(crbug.com/1004940): expected.txt file has 'aria-input-field-name' exceptions
   await AxeCoreTestRunner.runValidation(widget.element);
   TestRunner.completeTest();
 })();
