9c24157627729155623c465ce37239d10d532c1a
Colin Blundell <blundell@chromium.org>
2019-10-11 07:10:56 +0000

    Convert //components/web_resource away from base::Bind()
    As part of the effort to replace base::Bind by
    base::Bind{Once, Repeating}, this CL converts the one usage of
    base::Bind() in //components/web_resource to instead be a usage of
    base::BindRepeating(). The rational for using base::BindRepeating() is
    that the callback is being passed in to a PrefChangeRegistrar, which
    can invoke it multiple times in response to pref changes.
    
    This CL also takes the opportunity to update the PrefChangeRegistrar
    API in question to take in a base::RepeatingClosure rather than a
    base::Closure.
    
    TBR=jochen@chromium.org
    
    Bug: 1007753
    Change-Id: I7550f64eedb9d0da719f9ff8617e2c4d9d415eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1847216
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705009}

--- components/prefs/pref_change_registrar.h
+++ components/prefs/pref_change_registrar.h
@@ -41,7 +41,7 @@ class COMPONENTS_PREFS_EXPORT PrefChangeRegistrar final : public PrefObserver {
   // the preference that is changing as its parameter.
   //
   // Only one observer may be registered per path.
-  void Add(const std::string& path, const base::Closure& obs);
+  void Add(const std::string& path, const base::RepeatingClosure& obs);
   void Add(const std::string& path, const NamedChangeCallback& obs);
 
   // Removes the pref observer registered for |path|.
@@ -68,7 +68,7 @@ class COMPONENTS_PREFS_EXPORT PrefChangeRegistrar final : public PrefObserver {
   void OnPreferenceChanged(PrefService* service,
                            const std::string& pref_name) override;
 
-  static void InvokeUnnamedCallback(const base::Closure& callback,
+  static void InvokeUnnamedCallback(base::OnceClosure callback,
                                     const std::string& pref_name);
 
   using ObserverMap = std::map<std::string, NamedChangeCallback>;
--- components/web_resource/eula_accepted_notifier.cc
+++ components/web_resource/eula_accepted_notifier.cc
@@ -34,8 +34,8 @@ bool EulaAcceptedNotifier::IsEulaAccepted() {
   if (registrar_.IsEmpty()) {
     registrar_.Init(local_state_);
     registrar_.Add(prefs::kEulaAccepted,
-                   base::Bind(&EulaAcceptedNotifier::OnPrefChanged,
-                              base::Unretained(this)));
+                   base::BindRepeating(&EulaAcceptedNotifier::OnPrefChanged,
+                                       base::Unretained(this)));
   }
   return false;
 }
