30b929f97aaf5c8607583d811d590a57766fbe31
Wenbin Zhang <wenbinzhang@google.com>
2019-10-11 04:45:54 +0000

    [Benchmarking] Presubmit checks story versions for smoke tests
    In order to save CQ capacity, system_health_smoke_tests.py checks the
    story versions to make sure that we only run the latest version. The
    version check happens when loadng all the stories. When it fails, the
    try job shows all shards as failed, instead of reporting the correct
    info. Hers's an example from the bug:
    https://ci.chromium.org/p/chromium/builders/try/android-marshmallow-arm64-rel/369559
    
    Since this is more a configuration issue, we now change it to be reported
    at an earlier stage, i.e., in PRESUBMIT.
    
    Bug: 1006771
    Change-Id: I53882078a4784b460b2add78725ea18e8bfffddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1850353
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#704989}

---
+++ tools/perf/benchmarks/system_health_load_tests_smoke_test.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env vpython
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Script to check story version in system_health_smoke_tests."""
+
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+from core import path_util
+path_util.AddTelemetryToPath()
+
+from benchmarks import system_health_smoke_test
+
+def main():
+  system_health_smoke_test.load_tests(None, None, None)
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main())
