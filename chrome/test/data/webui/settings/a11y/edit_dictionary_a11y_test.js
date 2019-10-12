2cb6d2fd731bd9b89ad0e96d04499915b40a22ac
Hector Carmona <hcarmona@chromium.org>
2019-10-12 01:09:00 +0000

    Disable EditDictionaryA11yTest on linux because of flaky runs.
    Bug: 1012370
    Change-Id: Ibd7304cc2a9c8fe81b60e26da5bbffeaa0eee324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857541
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705354}

--- chrome/test/data/webui/settings/a11y/edit_dictionary_a11y_test.js
+++ chrome/test/data/webui/settings/a11y/edit_dictionary_a11y_test.js
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @fileoverview Define accessibility tests for the EDIT_DICTIONARY route.  */
 
 // Disable since the EDIT_DICTIONARY route does not exist on Mac.
-GEN('#if !defined(OS_MACOSX)');
+// TODO(crbug.com/1012370) flaky on Linux b/c assertTrue(!!languagesPage);
+GEN('#if !defined(OS_MACOSX) && !defined(OS_LINUX)');
 
 // SettingsAccessibilityTest fixture.
 GEN_INCLUDE([
@@ -104,4 +105,4 @@ AccessibilityTest.define('EditDictionaryA11yTest', {
   },
 });
 
-GEN('#endif  // !defined(OS_MACOSX)');
+GEN('#endif  // !defined(OS_MACOSX) && !defined(OS_LINUX)');
