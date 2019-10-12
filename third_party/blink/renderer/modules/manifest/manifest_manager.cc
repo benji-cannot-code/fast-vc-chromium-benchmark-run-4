e0290e577ce5c0f429f1b77beec6f524a0493118
Miyoung Shin <myid.shin@igalia.com>
2019-10-12 02:12:49 +0000

    Clear ReceiverSet<ManifestManager> in Prefinalize.
    ManifestManager could be unreachable before ContextDestroyed()
    gets called and destructed before the receiver set is cleared.
    Then, it could lead to UAP bug, so this CL clears ReceiverSet<T> in
    Prefinalize instead of ContextDestroyed.
    
    Bug: 1012055
    Change-Id: I3b1a569c8b4e26fde0c8ae3ac3cab27f5efc7c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1855323
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#705368}

--- third_party/blink/renderer/modules/manifest/manifest_manager.cc
+++ third_party/blink/renderer/modules/manifest/manifest_manager.cc
@@ -256,10 +256,6 @@ void ManifestManager::BindReceiver(
 }
 
 void ManifestManager::ContextDestroyed(ExecutionContext*) {
-  Dispose();
-}
-
-void ManifestManager::Dispose() {
   if (fetcher_)
     fetcher_->Cancel();
 
@@ -268,6 +264,10 @@ void ManifestManager::Dispose() {
   // in the renderer process should be correctly notified.
   ResolveCallbacks(ResolveStateFailure);
 
+  receivers_.Clear();
+}
+
+void ManifestManager::Prefinalize() {
   receivers_.Clear();
 }
 
