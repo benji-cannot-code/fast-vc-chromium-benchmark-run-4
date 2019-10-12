5502d8fc05f3ae022b9a462b808c499227dd9245
Mario Sanchez Prada <mario@igalia.com>
2019-10-12 01:02:04 +0000

    Add missing JS generation support for PendingAssociated{Remote,Receiver}
    This CL adds the remaining bits needed to add support for the new mojo types
    pending_associated_remote & pending_associated_receiver to the JavaScript
    generators. Similarly to what was done for CL1507289, these types translate
    for now to the existing endpoint types in those language, to allow mojom
    conversions without having to adapt those bindings first.
    
    This will unblock cleaning up Blink from the old mojo types, as this is at
    least required to migrate references to the XREnvironmentIntegrationProvider
    and XRInputSourceButtonListener mojo interfaces (see CLs 1836416 & 1844995).
    
    Bug: 875030, 955171, 978694
    Change-Id: I9ccf1ef2dabcab1ef49b35cae244924bfb44534b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1847331
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#705343}

--- mojo/public/tools/bindings/generators/mojom_js_generator.py
+++ mojo/public/tools/bindings/generators/mojom_js_generator.py
@@ -287,8 +287,11 @@ class Generator(generator.Generator):
       "is_any_handle_or_interface_kind": mojom.IsAnyHandleOrInterfaceKind,
       "is_array_kind": mojom.IsArrayKind,
       "is_associated_interface_kind": mojom.IsAssociatedInterfaceKind,
+      "is_pending_associated_remote_kind": mojom.IsPendingAssociatedRemoteKind,
       "is_associated_interface_request_kind":
           mojom.IsAssociatedInterfaceRequestKind,
+      "is_pending_associated_receiver_kind":
+          mojom.IsPendingAssociatedReceiverKind,
       "is_bool_kind": mojom.IsBoolKind,
       "is_enum_kind": mojom.IsEnumKind,
       "is_any_handle_kind": mojom.IsAnyHandleKind,
@@ -448,7 +451,9 @@ class Generator(generator.Generator):
           self._LiteClosureTypeWithNullability(kind.value_kind))
 
     if (mojom.IsAssociatedKind(kind) or mojom.IsInterfaceRequestKind(kind) or
-        mojom.IsPendingRemoteKind(kind) or mojom.IsPendingReceiverKind(kind)):
+        mojom.IsPendingRemoteKind(kind) or mojom.IsPendingReceiverKind(kind) or
+        mojom.IsPendingAssociatedRemoteKind(kind) or
+        mojom.IsPendingAssociatedReceiverKind(kind)):
       named_kind = kind.kind
     else:
       named_kind = kind
@@ -547,7 +552,9 @@ class Generator(generator.Generator):
           "true" if mojom.IsNullableKind(kind.value_kind) else "false")
 
     if (mojom.IsAssociatedKind(kind) or mojom.IsInterfaceRequestKind(kind) or
-        mojom.IsPendingRemoteKind(kind) or mojom.IsPendingReceiverKind(kind)):
+        mojom.IsPendingRemoteKind(kind) or mojom.IsPendingReceiverKind(kind) or
+        mojom.IsPendingAssociatedRemoteKind(kind) or
+        mojom.IsPendingAssociatedReceiverKind(kind)):
       named_kind = kind.kind
     else:
       named_kind = kind
