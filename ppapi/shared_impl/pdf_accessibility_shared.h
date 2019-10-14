9f259870b08c3fb68693724e6d325e07e5f5f64d
Kalpak Tapas <katapas@microsoft.com>
2019-10-14 08:19:50 +0000

    Group PDF page accessibility objects into new struct
    This is a preparatory change for adding accessibility support for PDF
    annotations. Currently, text, links and images within a PDF page are
    supported in an accessibility context. These are passed as separate
    parameters in Set/GetAccessibilityPageInfo methods exposed through the
    Pepper API. When annotation support comes in, the number of parameters
    would increase to 6 making those function calls unwieldy and requiring
    additional extensions to the templated DispatchResourceCall function.
    This change groups links and images into a new *PageObjects struct which
    will also be the home for the upcoming representation of PDF
    annotations.
    
    Bug: 1008775
    Change-Id: Icee1b9b17290614265be8c65c5b594d3a99fe767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1831957
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Ian Prest <iapres@microsoft.com>
    Commit-Queue: Kalpak Tapas <katapas@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#705481}

--- ppapi/shared_impl/pdf_accessibility_shared.h
+++ ppapi/shared_impl/pdf_accessibility_shared.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PPAPI_SHARED_IMPL_PDF_ACCESSIBILITY_SHARED_H_
 
 #include <string>
+#include <vector>
 
 #include "ppapi/c/pp_rect.h"
 #include "ppapi/c/private/ppb_pdf.h"
@@ -19,7 +20,6 @@ struct PPAPI_SHARED_EXPORT PdfAccessibilityLinkInfo {
   PdfAccessibilityLinkInfo();
   explicit PdfAccessibilityLinkInfo(
       const PP_PrivateAccessibilityLinkInfo& link);
-  PdfAccessibilityLinkInfo(PdfAccessibilityLinkInfo&& other);
   ~PdfAccessibilityLinkInfo();
 
   std::string url;
@@ -34,12 +34,22 @@ struct PPAPI_SHARED_EXPORT PdfAccessibilityImageInfo {
   PdfAccessibilityImageInfo();
   explicit PdfAccessibilityImageInfo(
       const PP_PrivateAccessibilityImageInfo& image);
-  PdfAccessibilityImageInfo(PdfAccessibilityImageInfo&& other);
   ~PdfAccessibilityImageInfo();
 
   std::string alt_text;
   uint32_t text_run_index;
   PP_FloatRect bounds;
+};
+
+// Needs to stay in sync with PP_PrivateAccessibilityPageObjects.
+struct PPAPI_SHARED_EXPORT PdfAccessibilityPageObjects {
+  PdfAccessibilityPageObjects();
+  explicit PdfAccessibilityPageObjects(
+      const PP_PrivateAccessibilityPageObjects& page_objects);
+  ~PdfAccessibilityPageObjects();
+
+  std::vector<PdfAccessibilityLinkInfo> links;
+  std::vector<PdfAccessibilityImageInfo> images;
 };
 
 }  // namespace ppapi
