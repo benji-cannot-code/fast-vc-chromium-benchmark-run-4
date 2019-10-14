918b41e07b63eff017e1bedb2ebdea86bc88662c
Francois Doray <fdoray@chromium.org>
2019-10-14 15:22:32 +0000

    [PM] Notify content::LockObserver of active IndexedDB connections (reland).
    This was reverted because of navigation failure on Android Kit Kat in
    IndexedDBExecutionContextConnectionTrackerBrowserTest*
    (https://crbug.com/1014015). Since the purpose of these tests is to
    test usage of IndexedDB connections, not navigation, this reland
    disables them on Android Kit Kat until we find a way to make
    navigation work.
    
    Original CL:
    https://crrev.com/c/chromium/src/+/1848794
    
    Diff from original CL:
    https://crrev.com/c/chromium/src/+/1860094/1..4
    
    With this CL, content::LockObserver is notified when the number of
    IndexedDB connections (active or pending) in a frame switches between
    zero and non-zero. The information is plumbed to the Performance Manager
    which uses it to avoid freezing pages with IndexedDB connections. This
    is because freezing a page with active IndexedDB connections can
    prevent other pages from performing IndexedDB operations. In the
    future, we would like to get more granular information about when it is
    really problematic to freeze a frame because of its current usage of
    IndexedDB, but this is a good first step to allow shipping page
    freezing without breaking usage of IndexedDB.
    
    More details at
    https://docs.google.com/document/d/1CUE1pfS5pGhJt6X0EKwv2sQYP5PcdzyGZKKtdBYzEOw/edit?usp=sharing
    
    TBR=kinuko@chromium.org,dmurph@chromium.org,siggi@chromium.org
    
    Bug: 980533, 1014015
    Change-Id: I0a63e86af88a2e5ee306c33b8df00e7f7817d814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860094
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705551}

--- content/browser/indexed_db/indexed_db_database.h
+++ content/browser/indexed_db/indexed_db_database.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/indexed_db/indexed_db_backing_store.h"
 #include "content/browser/indexed_db/indexed_db_callbacks.h"
 #include "content/browser/indexed_db/indexed_db_connection_coordinator.h"
+#include "content/browser/indexed_db/indexed_db_execution_context_connection_tracker.h"
 #include "content/browser/indexed_db/indexed_db_observer.h"
 #include "content/browser/indexed_db/indexed_db_origin_state_handle.h"
 #include "content/browser/indexed_db/indexed_db_pending_connection.h"
@@ -56,7 +57,6 @@ namespace content {
 class IndexedDBClassFactory;
 class IndexedDBConnection;
 class IndexedDBDatabaseCallbacks;
-struct IndexedDBExecutionContext;
 class IndexedDBFactory;
 class IndexedDBMetadataCoding;
 class IndexedDBOriginStateHandle;
@@ -360,7 +360,8 @@ class CONTENT_EXPORT IndexedDBDatabase {
   std::unique_ptr<IndexedDBConnection> CreateConnection(
       IndexedDBOriginStateHandle origin_state_handle,
       scoped_refptr<IndexedDBDatabaseCallbacks> database_callbacks,
-      const IndexedDBExecutionContext& execution_context);
+      IndexedDBExecutionContextConnectionTracker::Handle
+          execution_context_connection_handle);
 
   // Ack that one of the connections notified with a "versionchange" event did
   // not promptly close. Therefore a "blocked" event should be fired at the
