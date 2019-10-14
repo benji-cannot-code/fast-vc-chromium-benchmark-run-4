3ef47532890c23037f9ae526a1e20588f0c292f5
Steven Bennetts <stevenjb@chromium.org>
2019-10-14 22:33:02 +0000

    Internet Details Dialog: Set proxySettings correctly
    Same fix as:
    https://chromium-review.googlesource.com/c/chromium/src/+/1834203
    except for the details dialog.
    
    TBR=khorimoto@chromium.org
    
    Bug: 1014200
    Change-Id: Idc6a7c63ec72995d78dec38497904290197a22f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860874
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705688}

--- chrome/browser/resources/chromeos/internet_detail_dialog/internet_detail_dialog.js
+++ chrome/browser/resources/chromeos/internet_detail_dialog/internet_detail_dialog.js
@@ -524,7 +524,7 @@ Polymer({
       return;
     }
     const config = this.getDefaultConfigProperties_();
-    config.proxySetings = event.detail;
+    config.proxySettings = event.detail;
     this.setMojoNetworkProperties_(config);
   },
 
