e6d29d5b2f73dd99672a8f71879c54299e6119cd
Alex Chau <alexchau@chromium.org>
2019-10-14 20:14:05 +0000

    Create ECPrivateKey from secret
    - Derive ECPrivateKey from secret deterministically using
      EC_KEY_derive_from_secret
    - Will be used for VapidKeyManager to generate a ECPrivateKey from
      256-bit secret from Chrome Sync, as illustrated in
      https://crrev.com/c/1848171
    
    Bug: 1010968
    Change-Id: Id2445ff802e9598ad3b454dc42972a6812713885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1844961
    Commit-Queue: Alex Chau <alexchau@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705661}

--- crypto/ec_private_key.h
+++ crypto/ec_private_key.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "crypto/crypto_export.h"
@@ -38,7 +39,7 @@ class CRYPTO_EXPORT ECPrivateKey {
   // an ASN.1-encoded PrivateKeyInfo block from PKCS #8. This can return
   // nullptr if initialization fails.
   static std::unique_ptr<ECPrivateKey> CreateFromPrivateKeyInfo(
-      const std::vector<uint8_t>& input);
+      base::span<const uint8_t> input);
 
   // Creates a new instance by importing an existing key pair.
   // The key pair is given as an ASN.1-encoded PKCS #8 EncryptedPrivateKeyInfo
@@ -48,7 +49,13 @@ class CRYPTO_EXPORT ECPrivateKey {
   // This function is deprecated. Use CreateFromPrivateKeyInfo for new code.
   // See https://crbug.com/603319.
   static std::unique_ptr<ECPrivateKey> CreateFromEncryptedPrivateKeyInfo(
-      const std::vector<uint8_t>& encrypted_private_key_info);
+      base::span<const uint8_t> encrypted_private_key_info);
+
+  // Creates a new instance by deriving private key from |secret|, and generates
+  // public key points accordingly. The created key will use the NIST P-256
+  // curve. This can return nullptr if initialization fails.
+  static std::unique_ptr<ECPrivateKey> DeriveFromSecret(
+      base::span<const uint8_t> secret);
 
   // Returns a copy of the object.
   std::unique_ptr<ECPrivateKey> Copy() const;
@@ -80,7 +87,6 @@ class CRYPTO_EXPORT ECPrivateKey {
 
   DISALLOW_COPY_AND_ASSIGN(ECPrivateKey);
 };
-
 
 }  // namespace crypto
 
