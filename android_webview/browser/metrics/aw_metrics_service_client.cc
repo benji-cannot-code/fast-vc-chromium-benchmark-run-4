3c169c95e5f3181097af63189a85fbdc03973f4b
Nate Fischer <ntfschr@chromium.org>
2019-10-14 20:09:51 +0000

    AW UMA: persist install date
    This persists the install date pref, so this will now log the install
    date of the WebView-embedding application. For existing clients, this
    will log the first startup in a milestone of WebView which persists this
    pref.
    
    This also includes logic to look up the install date if it's not already
    logged, otherwise the install_date field would just log the date users
    updated to a version of WebView which supports this field. This logic is
    meant to be temporary.
    
    I've added a histogram to track when we can safely remove this
    workaround, and added a tests to verify the logic and that the histogram
    is logged.
    
    Bug: 1012025
    Test: startup webview shell multiple times, log install_date inside
    Test: AwMetricsLogUploader::UploadLog, verify it stays consistent
    Test: run_android_webview_unittests --gtest_filter=AwMetricsServiceClientTest.*
    Change-Id: I2d9238120b6bab2e08c116bf8f57e541b905534d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846812
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705657}

--- android_webview/browser/metrics/aw_metrics_service_client.cc
+++ android_webview/browser/metrics/aw_metrics_service_client.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/hash.h"
 #include "base/i18n/rtl.h"
 #include "base/lazy_instance.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string16.h"
 #include "components/metrics/call_stack_profile_metrics_provider.h"
 #include "components/metrics/cpu_metrics_provider.h"
@@ -122,6 +123,39 @@ std::unique_ptr<metrics::MetricsService> CreateMetricsService(
   return service;
 }
 
+// Queries the system for the app's first install time and uses this in the
+// kInstallDate pref. Must be called before created a MetricsStateManager.
+// TODO(https://crbug.com/1012025): remove this when the kInstallDate pref has
+// been persisted for one or two milestones.
+void PopulateSystemInstallDateIfNecessary(PrefService* prefs) {
+  int64_t install_date = prefs->GetInt64(metrics::prefs::kInstallDate);
+  if (install_date > 0) {
+    // kInstallDate appears to be valid (common case). Finish early as an
+    // optimization to avoid a JNI call below.
+    base::UmaHistogramEnumeration("Android.WebView.Metrics.BackfillInstallDate",
+                                  BackfillInstallDate::kValidInstallDatePref);
+    return;
+  }
+
+  JNIEnv* env = base::android::AttachCurrentThread();
+  int64_t system_install_date =
+      Java_AwMetricsServiceClient_getAppInstallTime(env);
+  if (system_install_date < 0) {
+    // Could not figure out install date from the system. Let the
+    // MetricsStateManager set this pref to its best guess for a reasonable
+    // time.
+    base::UmaHistogramEnumeration(
+        "Android.WebView.Metrics.BackfillInstallDate",
+        BackfillInstallDate::kCouldNotGetPackageManagerInstallDate);
+    return;
+  }
+
+  base::UmaHistogramEnumeration(
+      "Android.WebView.Metrics.BackfillInstallDate",
+      BackfillInstallDate::kPersistedPackageManagerInstallDate);
+  prefs->SetInt64(metrics::prefs::kInstallDate, system_install_date);
+}
+
 }  // namespace
 
 // static
@@ -140,6 +174,7 @@ void AwMetricsServiceClient::Initialize(PrefService* pref_service) {
 
   pref_service_ = pref_service;
 
+  PopulateSystemInstallDateIfNecessary(pref_service_);
   metrics_state_manager_ = metrics::MetricsStateManager::Create(
       pref_service_, this, base::string16(),
       base::BindRepeating(&StoreClientInfo),
