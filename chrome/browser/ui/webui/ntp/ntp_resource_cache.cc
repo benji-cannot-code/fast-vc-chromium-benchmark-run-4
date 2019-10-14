79e5897b1845eca3d030debeafbd683fb88c7abc
Huanzhong Huang <huanzhong@google.com>
2019-10-14 10:22:19 +0000

    Hide cookie controls when third-party cookie blocking managed or enabled
    Cookie controls UI should be hidden when (the original) third-party cookie
    blocking is policy-managed or turned on.
    
    Bug: 1012684, 967668
    Change-Id: I2657454ed4c71f221708247e197c0de57b3db3fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849681
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Huanzhong Huang <huanzhong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705497}

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/layout_constants.h"
 #include "chrome/browser/ui/webui/app_launcher_login_handler.h"
 #include "chrome/browser/ui/webui/ntp/app_launcher_handler.h"
+#include "chrome/browser/ui/webui/ntp/cookie_controls_handler.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
@@ -36,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/theme_resources.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
 #include "components/content_settings/core/common/features.h"
+#include "components/content_settings/core/common/pref_names.h"
 #include "components/google/core/common/google_util.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -276,10 +278,9 @@ void NTPResourceCache::CreateNewTabIncognitoHTML() {
       l10n_util::GetStringUTF8(IDS_NEW_TAB_OTR_NOT_SAVED);
   replacements["learnMoreLink"] = kLearnMoreIncognitoUrl;
   replacements["title"] = l10n_util::GetStringUTF8(IDS_NEW_TAB_TITLE);
-  replacements["hiddenIfCookieControlsDisabled"] =
-      base::FeatureList::IsEnabled(content_settings::kImprovedCookieControls)
-          ? ""
-          : "hidden";
+  replacements["hideCookieControls"] =
+      CookieControlsHandler::ShouldHideCookieControlsUI(profile_) ? "hidden"
+                                                                  : "";
   replacements["cookieControlsTitle"] =
       l10n_util::GetStringUTF8(IDS_SETTINGS_SITE_SETTINGS_THIRD_PARTY_COOKIE);
   replacements["cookieControlsDescription"] =
