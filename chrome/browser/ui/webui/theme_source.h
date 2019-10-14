e083b4653115f429eef3bcce0aa983dbcc0ac19e
John Lee <johntlee@chromium.org>
2019-10-14 18:09:43 +0000

    Allow Chrome pages to use chrome://theme resources as mask images
    As of CL 1838592, -webkit-mask-image now uses CORS-enabled loading,
    blocking chrome:// pages from using chrome://theme resources as mask
    images. This only applies to OOBE and the WebUI tab strip currently.
    
    Change-Id: I10f8a0e68ff2de4603a7bbb816c1f1c4a42b76e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1855182
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705596}

--- chrome/browser/ui/webui/theme_source.h
+++ chrome/browser/ui/webui/theme_source.h
@@ -34,6 +34,8 @@ class ThemeSource : public content::URLDataSource {
   bool ShouldServiceRequest(const GURL& url,
                             content::ResourceContext* resource_context,
                             int render_process_id) override;
+  std::string GetAccessControlAllowOriginForOrigin(
+      const std::string& origin) override;
 
  private:
   // Fetches and sends the theme bitmap.
