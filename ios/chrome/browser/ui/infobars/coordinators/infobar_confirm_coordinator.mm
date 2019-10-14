60d76346d149b1749d068a442143fff4a22f3c35
sczs <sczs@chromium.org>
2019-10-14 16:51:57 +0000

    [ios] Uses ConfirmInfobarDelegate icon on Banner.
    Uses the InfobarDelegate Icon to populate the Banner's icon.
    
    Screenshot:
    https://drive.google.com/open?id=1ig7tT72B6mFsqHtrAmDO5ANww6HKEGpj
    
    Bug: 961343
    Change-Id: If9b8cddfd41b3cef7bc45e7165234f543b3a681d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857048
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705573}

--- ios/chrome/browser/ui/infobars/coordinators/infobar_confirm_coordinator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_confirm_coordinator.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/infobars/coordinators/infobar_coordinator_implementation.h"
 #import "ios/chrome/browser/ui/infobars/infobar_container.h"
 #import "ios/chrome/browser/ui/infobars/modals/infobar_modal_view_controller.h"
+#include "ui/gfx/image/image.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -61,6 +62,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.bannerViewController.buttonText =
         base::SysUTF16ToNSString(self.confirmInfobarDelegate->GetButtonLabel(
             ConfirmInfoBarDelegate::BUTTON_OK));
+    gfx::Image modelIcon = self.confirmInfobarDelegate->GetIcon();
+    if (!modelIcon.IsEmpty())
+      self.bannerViewController.iconImage = modelIcon.ToUIImage();
   }
 }
 
