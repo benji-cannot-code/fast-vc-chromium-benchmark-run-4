e1740c49f8680d231e08a8d32c26fcd6e1bbf1a2
David Grogan <dgrogan@chromium.org>
2019-10-14 02:08:55 +0000

    [LayoutNG] Use legacy borders when painting table cell fragments
    NG fragments don't support collapsed borders yet. This manifested with
    background-clip. When the position/size of the clipped background was
    determined, the cells' full borders were used, not half of the winning
    border as is correct in collapsed mode.
    
    Bug: 1008400
    Change-Id: I53a80c88d854136e8d3f540fe7b8c969152aad13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1858519
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705454}

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.cc
@@ -1237,6 +1237,8 @@ PhysicalRect NGBoxFragmentPainter::AdjustRectForScrolledContent(
 }
 
 LayoutRectOutsets NGBoxFragmentPainter::ComputeBorders() const {
+  if (box_fragment_.GetLayoutObject()->IsTableCell())
+    return ToLayoutBox(box_fragment_.GetLayoutObject())->BorderBoxOutsets();
   return BoxStrutToLayoutRectOutsets(PhysicalFragment().BorderWidths());
 }
 
