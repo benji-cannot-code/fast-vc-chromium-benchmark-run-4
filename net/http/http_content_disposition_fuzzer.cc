3a7c4026d98bcad5e6d526f6c30c197a01ea697a
Asanka Herath <asanka@chromium.org>
2019-10-14 19:27:39 +0000

    [net/http] Add fuzzer for net::HttpContentDisposition.
    Bug: None
    Change-Id: I37729941b4e3ca3837d259c6064d236635dd8b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857399
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Auto-Submit: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705638}

---
+++ net/http/http_content_disposition_fuzzer.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstddef>
+#include <cstdint>
+
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "net/http/http_content_disposition.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  FuzzedDataProvider input{data, size};
+  auto charset = input.ConsumeRandomLengthString(100u);
+  auto header = input.ConsumeRemainingBytesAsString();
+  net::HttpContentDisposition content_disposition{header, charset};
+  return 0;
+}
