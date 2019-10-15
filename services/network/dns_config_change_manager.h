8bec0a9a07411282de2904c7a058d605330e6351
Miyoung Shin <myid.shin@igalia.com>
2019-10-15 08:23:40 +0000

    Convert DnsConfigChangeManagerClient to new Mojo types
    This CL converts DnsConfigChangeManagerClient{Ptr, Request} in
    chrome and services to the new Mojo type, and uses
    pending_remote<DnsConfigChangeManagerClient> in host_resolver.mojom.
    
    Bug: 955171
    Change-Id: If8038bf601d99eb11c6f62f250872919fa6338f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1859655
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705871}

--- services/network/dns_config_change_manager.h
+++ services/network/dns_config_change_manager.h
@@ -11,8 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/macros.h"
-#include "mojo/public/cpp/bindings/interface_ptr_set.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
+#include "mojo/public/cpp/bindings/remote_set.h"
 #include "net/base/network_change_notifier.h"
 #include "services/network/public/mojom/host_resolver.mojom.h"
 
@@ -30,7 +31,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) DnsConfigChangeManager
 
   // mojom::DnsConfigChangeManager implementation:
   void RequestNotifications(
-      mojom::DnsConfigChangeManagerClientPtr client) override;
+      mojo::PendingRemote<mojom::DnsConfigChangeManagerClient> client) override;
 
  private:
   // net::NetworkChangeNotifier::DNSObserver implementation:
@@ -38,7 +39,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) DnsConfigChangeManager
   void OnInitialDNSConfigRead() override;
 
   mojo::ReceiverSet<mojom::DnsConfigChangeManager> receivers_;
-  mojo::InterfacePtrSet<mojom::DnsConfigChangeManagerClient> clients_;
+  mojo::RemoteSet<mojom::DnsConfigChangeManagerClient> clients_;
 
   DISALLOW_COPY_AND_ASSIGN(DnsConfigChangeManager);
 };
--- services/network/dns_config_change_manager_unittest.cc
+++ services/network/dns_config_change_manager_unittest.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace network {
@@ -22,13 +22,7 @@ class TestDnsConfigChangeManagerClient
   explicit TestDnsConfigChangeManagerClient(DnsConfigChangeManager* manager) {
     mojo::Remote<mojom::DnsConfigChangeManager> manager_remote;
     manager->AddReceiver(manager_remote.BindNewPipeAndPassReceiver());
-
-    mojom::DnsConfigChangeManagerClientPtr client_ptr;
-    mojom::DnsConfigChangeManagerClientRequest client_request(
-        mojo::MakeRequest(&client_ptr));
-    binding_.Bind(std::move(client_request));
-
-    manager_remote->RequestNotifications(std::move(client_ptr));
+    manager_remote->RequestNotifications(receiver_.BindNewPipeAndPassRemote());
   }
 
   void OnSystemDnsConfigChanged() override {
@@ -49,7 +43,7 @@ class TestDnsConfigChangeManagerClient
   int num_notifications_ = 0;
   int num_notifications_expected_ = INT_MAX;
   base::RunLoop run_loop_;
-  mojo::Binding<mojom::DnsConfigChangeManagerClient> binding_{this};
+  mojo::Receiver<mojom::DnsConfigChangeManagerClient> receiver_{this};
 
   DISALLOW_COPY_AND_ASSIGN(TestDnsConfigChangeManagerClient);
 };
