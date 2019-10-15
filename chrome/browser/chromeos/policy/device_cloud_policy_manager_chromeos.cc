9d4d14a8caac589ef4f3ca92b09348c6219cb791
Sergey Poromov <poromov@chromium.org>
2019-10-15 15:44:10 +0000

    Remove |is_enterprise_reporting_| from DeviceStatusCollector.
    |is_enterprise_reporting_| is always set to 'true' and is redundant.
    DeviceStatusCollector is used only for enterprise reporting,
    consumer (child) reporting is extracted to ChildStatusCollector,
    so it's safe to remove this flag for simplicity and ensure that
    DeviceStatusCollector is initialized only for enterprise reporting.
    
    Test: Run trybots.
    Change-Id: I87509316e5d3c46fa186b8d322b69e3dadba7a2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1862679
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705978}

--- chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
@@ -434,8 +434,7 @@ void DeviceCloudPolicyManagerChromeOS::CreateStatusUploader() {
           DeviceStatusCollector::TpmStatusFetcher(),
           DeviceStatusCollector::EMMCLifetimeFetcher(),
           DeviceStatusCollector::StatefulPartitionInfoFetcher(),
-          DeviceStatusCollector::CrosHealthdDataFetcher(),
-          true /* is_enterprise_device */),
+          DeviceStatusCollector::CrosHealthdDataFetcher()),
       task_runner_, kDeviceStatusUploadFrequency));
 }
 
