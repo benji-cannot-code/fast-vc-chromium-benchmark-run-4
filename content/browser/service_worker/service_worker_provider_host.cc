6bddb1dfb9c9ea503920d7e1c82d11b5d145cdca
Yannic Bonenberger <yannic.bonenberger@gmail.com>
2019-10-15 02:13:27 +0000

    Revert "[ServiceWorker] Stop resurrecting uninstalled workers"
    This reverts commit 6216be03fea4d16279b92b1afe650f49a92482cb.
    
    Reason for revert: Causes crashes in Canary: https://crbug.com/1014075
    
    Original change's description:
    > [ServiceWorker] Stop resurrecting uninstalled workers
    > 
    > Previously, service workers were resurrected when they were updated
    > or when the same script was registered while the service worker was
    > still alive. Now, registering a service worker will always create a
    > new instance, regardless of whether there already is a running
    > instance or not.
    > 
    > Chrome status:
    > https://chromestatus.com/feature/5646687634718720
    > 
    > Intent to Implement and Ship:
    > https://groups.google.com/a/chromium.org/d/msg/blink-dev/r176Lvgxfys/4W4IGM4uAgAJ
    > 
    > Bug: 971571,988582
    > Change-Id: Id7748377af107099d7c10fb2e455f30f7423e5f8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1768759
    > Commit-Queue: Yannic Bonenberger <yannic.bonenberger@gmail.com>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#705159}
    
    TBR=falken@chromium.org,kinuko@chromium.org,yannic.bonenberger@gmail.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 971571, 988582, 1014075
    Change-Id: I070b328a33db63ac3dba128d43d026c86aec1498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860029
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705800}

--- content/browser/service_worker/service_worker_provider_host.cc
+++ content/browser/service_worker/service_worker_provider_host.cc
@@ -598,15 +598,8 @@ void ServiceWorkerProviderHost::AddMatchingRegistration(
   if (!IsContextSecureForServiceWorker())
     return;
   size_t key = registration->scope().spec().size();
-  if (base::Contains(matching_registrations_, key)) {
-    if (registration == matching_registrations_[key]) {
-      // TODO(https://crbug.com/971571): Verify this can never happen and
-      // replace with a DCHECK.
-      return;
-    }
-    uninstalled_matching_registrations_[key].emplace(
-        std::move(matching_registrations_[key]));
-  }
+  if (base::Contains(matching_registrations_, key))
+    return;
   registration->AddListener(this);
   matching_registrations_[key] = registration;
   ReturnRegistrationForReadyIfNeeded();
@@ -616,18 +609,12 @@ void ServiceWorkerProviderHost::RemoveMatchingRegistration(
     ServiceWorkerRegistration* registration) {
   DCHECK_NE(controller_registration_, registration);
 #if DCHECK_IS_ON()
-  DCHECK(IsMatchingRegistration(registration) ||
-         IsMatchingUninstalledRegistration(registration));
+  DCHECK(IsMatchingRegistration(registration));
 #endif  // DCHECK_IS_ON()
 
   registration->RemoveListener(this);
   size_t key = registration->scope().spec().size();
-  if (registration == matching_registrations_[key]) {
-    matching_registrations_.erase(key);
-    return;
-  }
-  DCHECK(base::Contains(uninstalled_matching_registrations_, key));
-  uninstalled_matching_registrations_.erase(key);
+  matching_registrations_.erase(key);
 }
 
 ServiceWorkerRegistration* ServiceWorkerProviderHost::MatchRegistration()
@@ -817,15 +804,6 @@ bool ServiceWorkerProviderHost::IsMatchingRegistration(
     return false;
   return true;
 }
-
-bool ServiceWorkerProviderHost::IsMatchingUninstalledRegistration(
-    ServiceWorkerRegistration* registration) {
-  size_t key = registration->scope().spec().size();
-  if (!base::Contains(uninstalled_matching_registrations_, key)) {
-    return false;
-  }
-  return base::Contains(uninstalled_matching_registrations_[key], registration);
-}
 #endif  // DCHECK_IS_ON()
 
 void ServiceWorkerProviderHost::RemoveAllMatchingRegistrations() {
@@ -835,13 +813,6 @@ void ServiceWorkerProviderHost::RemoveAllMatchingRegistrations() {
     registration->RemoveListener(this);
   }
   matching_registrations_.clear();
-
-  for (const auto& it : uninstalled_matching_registrations_) {
-    for (const auto& registration : it.second) {
-      registration->RemoveListener(this);
-    }
-  }
-  uninstalled_matching_registrations_.clear();
 }
 
 void ServiceWorkerProviderHost::ReturnRegistrationForReadyIfNeeded() {
