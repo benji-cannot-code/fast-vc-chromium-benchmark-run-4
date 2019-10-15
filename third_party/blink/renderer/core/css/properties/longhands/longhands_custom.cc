56b365b8ca502c4e595aa019f2a0975a1e4f41aa
Meredith Lane <meredithl@chromium.org>
2019-10-15 02:57:33 +0000

    Remove flag for CSS Alt Text
    Enabled by default in stable for awhile now, so safe to remove.
    
    BUG=860016
    
    Change-Id: I2deea943849ce11a3db2a98e85df6c6aee5d38cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1850689
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705811}

--- third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
+++ third_party/blink/renderer/core/css/properties/longhands/longhands_custom.cc
@@ -1900,8 +1900,7 @@ const CSSValue* Content::ParseSingleValue(CSSParserTokenRange& range,
       }
     }
     if (!parsed_value) {
-      if (RuntimeEnabledFeatures::CSSAltTextEnabled() &&
-          css_property_parser_helpers::ConsumeSlashIncludingWhitespace(range)) {
+      if (css_property_parser_helpers::ConsumeSlashIncludingWhitespace(range)) {
         // No values were parsed before the slash, so nothing to apply the
         // alternative text to.
         if (!values->length())
