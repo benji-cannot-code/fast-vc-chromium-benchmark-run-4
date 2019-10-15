dc16e3a7a015ba83f1cc411057a62d6b39162a98
Torne (Richard Coles) <torne@google.com>
2019-10-15 23:03:55 +0000

    webview: Remove AwContents.activityFromContext.
    This method (now) just wraps ContextUtils.activityFromContext and adds a
    trace, but it's not used consistently. The trace was added to diagnose
    performance issues caused by class verification failures, but we should
    have this under control now.
    
    Change-Id: I2415cd99f0e9f337956888918388d36aaf6d05af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1862729
    Commit-Queue: Richard Coles <torne@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Richard Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706209}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -7,7 +7,6 @@ package org.chromium.android_webview;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.ComponentCallbacks2;
 import android.content.Context;
 import android.content.Intent;
@@ -1172,7 +1171,7 @@ public class AwContents implements SmartClipProvider {
         if (wrapper != null) return wrapper;
 
         try (ScopedSysTraceEvent e = ScopedSysTraceEvent.scoped("AwContents.getWindowAndroid")) {
-            boolean contextWrapsActivity = activityFromContext(context) != null;
+            boolean contextWrapsActivity = ContextUtils.activityFromContext(context) != null;
             if (contextWrapsActivity) {
                 ActivityWindowAndroid activityWindow;
                 try (ScopedSysTraceEvent e2 =
@@ -1530,11 +1529,6 @@ public class AwContents implements SmartClipProvider {
         AwContentsJni.get().setShouldDownloadFavicons();
     }
 
-    public static Activity activityFromContext(Context context) {
-        try (ScopedSysTraceEvent e = ScopedSysTraceEvent.scoped("AwContents.activityFromContext")) {
-            return ContextUtils.activityFromContext(context);
-        }
-    }
     /**
      * Disables contents of JS-to-Java bridge objects to be inspectable using
      * Object.keys() method and "for .. in" loops. This is intended for applications
