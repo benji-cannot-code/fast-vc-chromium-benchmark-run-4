3e485cc174cebcb75a6cd71ecc23db3ec25cd703
Eric Seckler <eseckler@chromium.org>
2019-10-15 09:20:58 +0000

    perfetto: Also write timestamp_clock_id for metadata packets
    We should also set timestamp_clock_id when we set a TracePacket's
    timestamp (since chrome isn't usually using perfetto's default
    CLOCK_BOOTTIME).
    
    Change-Id: Idb7970b0567e1466a741180b4e15d96ffbc8f7d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1859791
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705876}

--- services/tracing/public/cpp/perfetto/trace_event_data_source.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/perfetto/include/perfetto/ext/tracing/core/trace_writer.h"
 #include "third_party/perfetto/protos/perfetto/trace/chrome/chrome_metadata.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/chrome/chrome_trace_event.pbzero.h"
+#include "third_party/perfetto/protos/perfetto/trace/clock_snapshot.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/trace_packet.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/track_event/process_descriptor.pbzero.h"
 
@@ -101,6 +102,19 @@ void WriteMetadataProto(ChromeMetadataPacket* metadata_proto,
   }
 #endif  // defined(OS_ANDROID) && defined(OFFICIAL_BUILD)
 }
+
+#if defined(OS_LINUX) || defined(OS_ANDROID) || defined(OS_FUCHSIA)
+// Linux, Android, and Fuchsia all use CLOCK_MONOTONIC. See crbug.com/166153
+// about efforts to unify base::TimeTicks across all platforms.
+constexpr perfetto::protos::pbzero::ClockSnapshot::Clock::BuiltinClocks
+    kTraceClockId = perfetto::protos::pbzero::ClockSnapshot::Clock::MONOTONIC;
+#else
+// Mac and Windows TimeTicks advance when sleeping, so are closest to BOOTTIME
+// in behavior.
+// TODO(eseckler): Support specifying Mac/Win platform clocks in BuiltinClocks.
+constexpr perfetto::protos::pbzero::ClockSnapshot::Clock::BuiltinClocks
+    kTraceClockId = perfetto::protos::pbzero::ClockSnapshot::Clock::BOOTTIME;
+#endif
 
 }  // namespace
 
@@ -176,6 +190,7 @@ void TraceEventMetadataSource::GenerateMetadataFromGenerator(
   }
   trace_packet->set_timestamp(
       TRACE_TIME_TICKS_NOW().since_origin().InNanoseconds());
+  trace_packet->set_timestamp_clock_id(kTraceClockId);
   auto* chrome_metadata = trace_packet->set_chrome_metadata();
   generator.Run(chrome_metadata, privacy_filtering_enabled_);
 }
@@ -195,6 +210,7 @@ void TraceEventMetadataSource::GenerateJsonMetadataFromGenerator(
     }
     trace_packet->set_timestamp(
         TRACE_TIME_TICKS_NOW().since_origin().InNanoseconds());
+    trace_packet->set_timestamp_clock_id(kTraceClockId);
     event_bundle = trace_packet->set_chrome_events();
   }
 
@@ -268,6 +284,7 @@ void TraceEventMetadataSource::GenerateMetadata(
 
   trace_packet->set_timestamp(
       TRACE_TIME_TICKS_NOW().since_origin().InNanoseconds());
+  trace_packet->set_timestamp_clock_id(kTraceClockId);
   auto* chrome_metadata = trace_packet->set_chrome_metadata();
   for (auto& generator : *proto_generators) {
     generator.Run(chrome_metadata, privacy_filtering_enabled_);
@@ -277,8 +294,6 @@ void TraceEventMetadataSource::GenerateMetadata(
     return;
   }
 
-  trace_packet->set_timestamp(
-      TRACE_TIME_TICKS_NOW().since_origin().InNanoseconds());
   ChromeEventBundle* event_bundle = trace_packet->set_chrome_events();
 
   for (auto& generator : *json_generators) {
@@ -956,6 +971,7 @@ void TraceEventDataSource::EmitProcessDescriptor() {
   trace_packet->set_incremental_state_cleared(true);
   trace_packet->set_timestamp(
       TRACE_TIME_TICKS_NOW().since_origin().InNanoseconds());
+  trace_packet->set_timestamp_clock_id(kTraceClockId);
   auto process_type = GetProcessType(TraceLog::GetInstance()->process_name());
   ProcessDescriptor* process_desc = trace_packet->set_process_descriptor();
   process_desc->set_pid(process_id_);
