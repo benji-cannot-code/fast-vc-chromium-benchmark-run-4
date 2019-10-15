a825c5f583141e533b9b652d79dc0d35f69d4891
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-10-15 05:40:39 +0000

    Import wpt@eb22c08dda6eacf63bdefa52774c93f14bca2d5e
    Using wpt-import in Chromium dd101fc36345f162c620d33c237f76d28fda4a33.
    With Chromium commits locally applied on WPT:
    2f7c5562b5 "Fetch Metadata: Split `sec-fetch-dest` out from other headers' tests."
    d52a9d0798 "Port IndexedDB Blob Web Tests to web-platform-tests."
    6386d9d857 "Update service_worker_unregister_and_register()"
    f399b336b4 "Fetch Metadata: Add a test for `<embed>` navigated after loading."
    0583f458d7 "Fetch Metadata: split up `document` destination."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: I3283ed9a9593c61d397ec5bdbc12fffea7a556cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860782
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#705834}

--- third_party/blink/web_tests/external/wpt/workers/modules/resources/dynamic-import-remote-origin-referrer-checker-worker.sub.js
+++ third_party/blink/web_tests/external/wpt/workers/modules/resources/dynamic-import-remote-origin-referrer-checker-worker.sub.js
@@ -1,3 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Import a remote origin script.
-import('https://{{domains[www1]}}:{{ports[https][0]}}/workers/modules/resources/referrer-checker.py');
+import('https://{{domains[www1]}}:{{ports[https][0]}}/workers/modules/resources/referrer-checker.py')
+    .catch(error => postMessage(`Import failed: ${error}`));
