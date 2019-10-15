ce63b06a3ee98ce5ddc821a0084593631a34f164
Chris Harrelson <chrishtr@chromium.org>
2019-10-15 15:46:29 +0000

    Don't pixel-snap LayoutView::DocumentRect when not needed
    Bug: 650768
    
    Change-Id: I729e848121638b23f4d13971f48a5962827f7abf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1861035
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705981}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -2690,7 +2690,7 @@ IntSize WebViewImpl::ContentsSize() const {
       GetPage()->DeprecatedLocalMainFrame()->ContentLayoutObject();
   if (!layout_view)
     return IntSize();
-  return layout_view->DocumentRect().Size();
+  return PixelSnappedIntRect(layout_view->DocumentRect()).Size();
 }
 
 WebSize WebViewImpl::ContentsPreferredMinimumSize() {
