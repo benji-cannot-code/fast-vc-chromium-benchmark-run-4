15c05d6f6e3d18db6415042254ec97ba61249b27
Yoshifumi Inoue <yosin@chromium.org>
2019-10-15 07:04:28 +0000

    Make NGInlineCursor to support culled inline
    This patch makes |NGInlineCursor| to support culled inline box to repalce
    |NGInlineFragmentTraversal::SelfFragmentsOf()| with |NGInlineCursor| for
    preparation of migrating |NGFragmentItem|.
    
    Bug: 982194
    Change-Id: I86d90dac19780af31defddebc09393a5ae836a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1855618
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705852}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ComputedStyle;
-class LayoutObject;
 class LayoutBlockFlow;
+class LayoutInline;
+class LayoutObject;
 class NGFragmentItem;
 class NGFragmentItems;
 class NGPaintFragment;
@@ -176,6 +177,10 @@ class CORE_EXPORT NGInlineCursor {
   // we'll call it "void cursor" instead of "null cursor".
   NGInlineCursor() = delete;
 
+  // True if current position is descendant or self of |layout_object|.
+  // Note: This function is used for moving cursor in culled inline boxes.
+  bool IsInclusiveDescendantOf(const LayoutObject& layout_object) const;
+
   // True if the current position is a last line in inline block. It is error
   // to call at end or the current position is not line.
   bool IsLastLineInInlineBlock() const;
@@ -183,6 +188,12 @@ class CORE_EXPORT NGInlineCursor {
   // Make the current position points nothing, e.g. cursor moves over start/end
   // fragment, cursor moves to first/last child to parent has no children.
   void MakeNull();
+
+  // Move the cursor position to the first fragment in tree.
+  void MoveToFirst();
+
+  // Same as |MoveTo()| but not support culled inline.
+  void InternalMoveTo(const LayoutObject& layout_object);
 
   void SetRoot(const NGFragmentItems& items);
   void SetRoot(ItemsSpan items);
@@ -206,6 +217,9 @@ class CORE_EXPORT NGInlineCursor {
 
   const NGPaintFragment* root_paint_fragment_ = nullptr;
   const NGPaintFragment* current_paint_fragment_ = nullptr;
+
+  // Used in |MoveToNextForSameLayoutObject()| to support culled inline.
+  const LayoutInline* layout_inline_ = nullptr;
 };
 
 CORE_EXPORT std::ostream& operator<<(std::ostream&, const NGInlineCursor&);
