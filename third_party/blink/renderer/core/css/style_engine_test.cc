f2a001d6b939ddb383ce211f2ecbb5b381343953
Lei Zhang <thestig@chromium.org>
2019-10-15 05:22:29 +0000

    Disable flaky StyleEngineTest tests.
    MediaQueriesReducedMotionOverride, MediaQueriesColorSchemeOverride, and
    MediaQueriesChangeNavigationControls sometimes fail.
    
    TBR=futhark@chromium.org
    
    Bug: 1014380
    Change-Id: Iba85874c2f149c3c3d277f41f394eef7be00b8ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1861515
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705832}

--- third_party/blink/renderer/core/css/style_engine_test.cc
+++ third_party/blink/renderer/core/css/style_engine_test.cc
@@ -1665,7 +1665,8 @@ TEST_F(StyleEngineTest, MediaQueriesChangeForcedColorsAndPreferredColorScheme) {
                 GetCSSPropertyColor()));
 }
 
-TEST_F(StyleEngineTest, MediaQueriesColorSchemeOverride) {
+// Flaky. https://crbug.com/1014380
+TEST_F(StyleEngineTest, DISABLED_MediaQueriesColorSchemeOverride) {
   ScopedMediaQueryPrefersColorSchemeForTest feature_scope(true);
 
   EXPECT_EQ(PreferredColorScheme::kNoPreference,
@@ -1700,7 +1701,8 @@ TEST_F(StyleEngineTest, MediaQueriesColorSchemeOverride) {
                 GetCSSPropertyColor()));
 }
 
-TEST_F(StyleEngineTest, MediaQueriesReducedMotionOverride) {
+// Flaky. https://crbug.com/1014380
+TEST_F(StyleEngineTest, DISABLED_MediaQueriesReducedMotionOverride) {
   EXPECT_FALSE(GetDocument().GetSettings()->GetPrefersReducedMotion());
 
   GetDocument().body()->SetInnerHTMLFromString(R"HTML(
@@ -1732,7 +1734,8 @@ TEST_F(StyleEngineTest, MediaQueriesReducedMotionOverride) {
                 GetCSSPropertyColor()));
 }
 
-TEST_F(StyleEngineTest, MediaQueriesChangeNavigationControls) {
+// Flaky. https://crbug.com/1014380
+TEST_F(StyleEngineTest, DISABLED_MediaQueriesChangeNavigationControls) {
   ScopedMediaQueryNavigationControlsForTest scoped_feature(true);
   GetDocument().body()->SetInnerHTMLFromString(R"HTML(
     <style>
