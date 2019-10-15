9cabed2365121c05aaa562141398c58d2cbab53b
Eric Karl <ericrk@chromium.org>
2019-10-15 00:16:55 +0000

    Add DeJellyUtils.java
    Adds Android-specific customization of de-jelly effect based on display
    properties.
    
    Bug: 995965
    Change-Id: Ie48ddf1e3f61d601e05522cf1f7b99a3dc25f012
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1837115
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705760}

--- components/viz/common/display/de_jelly.cc
+++ components/viz/common/display/de_jelly.cc
@@ -6,7 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/display/de_jelly.h"
 
 #include "base/command_line.h"
+#include "build/build_config.h"
 #include "components/viz/common/switches.h"
+
+#if defined(OS_ANDROID)
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
+#include "base/time/time.h"
+#include "components/viz/common/common_jni_headers/DeJellyUtils_jni.h"
+#endif
 
 namespace viz {
 
@@ -16,8 +24,14 @@ bool DeJellyEnabled() {
 }
 
 bool DeJellyActive() {
-  // TODO(ericrk): Android specific bits to be added in a follow-up CL.
-  return DeJellyEnabled();
+  if (!DeJellyEnabled())
+    return false;
+
+#if defined(OS_ANDROID)
+  return Java_DeJellyUtils_useDeJelly(base::android::AttachCurrentThread());
+#endif
+
+  return true;
 }
 
 float DeJellyScreenWidth() {
@@ -27,8 +41,10 @@ float DeJellyScreenWidth() {
   if (!value.empty())
     return std::atoi(value.c_str());
 
-  // TODO(ericrk): We can automatically handle this on Android. For now return
-  // a reasonable default.
+#if defined(OS_ANDROID)
+  return Java_DeJellyUtils_screenWidth(base::android::AttachCurrentThread());
+#endif
+
   return 1440.0f;
 }
 
