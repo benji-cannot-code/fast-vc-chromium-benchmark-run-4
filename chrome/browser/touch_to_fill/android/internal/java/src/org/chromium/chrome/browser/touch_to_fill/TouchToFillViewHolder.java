80f1f151ae260f83859250a59ccefdf0cc6db283
Friedrich Horschig <fhorschig@chromium.org>
2019-10-15 15:48:18 +0000

    [TouchToFill][Android] Use RecyclerView for credential items
    This CL introduces a RecyclerView and prepares the list of credentials
    to display more generic items (like the upcoming header element).
    
    Bug: 1012219
    Change-Id: I20b8361b2a139e89a493c5292f86b7bcece1c920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857127
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705982}

---
+++ chrome/browser/touch_to_fill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillViewHolder.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.touch_to_fill;
+
+import android.support.annotation.LayoutRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
+import org.chromium.ui.modelutil.PropertyModelChangeProcessor.ViewBinder;
+
+class TouchToFillViewHolder extends RecyclerView.ViewHolder {
+    final ViewBinder<PropertyModel, ViewGroup, PropertyKey> mViewBinder;
+
+    TouchToFillViewHolder(ViewGroup parent, @LayoutRes int layout,
+            ViewBinder<PropertyModel, ViewGroup, PropertyKey> viewBinder) {
+        super(LayoutInflater.from(parent.getContext()).inflate(layout, parent, false));
+        mViewBinder = viewBinder;
+    }
+
+    /**
+     * Called whenever an item is bound to this view holder. Please note that this method
+     * might be called on the same list entry repeatedly, so make sure to always set a default
+     * for unused fields.
+     * @param model The {@link PropertyModel} whose data needs to be displayed.
+     */
+    void setupModelChangeProcessor(PropertyModel model) {
+        PropertyModelChangeProcessor.create(model, (ViewGroup) itemView, mViewBinder, true);
+    }
+}
