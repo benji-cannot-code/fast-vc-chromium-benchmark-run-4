d151c0dcbb6e904dc46069e2df49aedd571375af
John Emau <John.Emau@microsoft.com>
2019-10-15 23:53:41 +0000

    DevTools: Put audio context name in drop down label
    Previously the accessible name for the audio context selector was 'BaseAudioContext'.
    
    With this change the accessible name becomes `Audio context: ${content title}`
    and if no context is selected: `Audio context: (no recording)`.
    
    This is a much better experience for screen reader users and meets success
    criteria for WCAG 2.5.3 Label in Name https://www.w3.org/WAI/WCAG21/quickref/#label-in-name
    
    Test page: https://mdn.github.io/webaudio-examples/audiocontext-states/
    
    Screenshot (NVDA Speech Viewer):  https://i.imgur.com/m5pKYnx.png (NOTE: This screenshot was taken with https://crrev.com/c/1626643 applied to better screen reader support)
    Screenshot (Accessibility Tree): https://i.imgur.com/e04n8m9.png
    
    Bug: 963183
    Change-Id: I89e14599313766e7bd2be8a6a953df28c648dbe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1711008
    Reviewed-by: Lorne Mitchell <lomitch@microsoft.com>
    Commit-Queue: John Emau <John.Emau@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#706226}

--- third_party/blink/web_tests/http/tests/devtools/webaudio/audio-context-selector-test.js
+++ third_party/blink/web_tests/http/tests/devtools/webaudio/audio-context-selector-test.js
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       /** @type {!WebAudio.AudioContextSelector} */ selector) {
     TestRunner.addResult(`
 Number of contexts (items): ${selector._items.length}
+Title: ${selector.toolbarItem()._title}}
 Selected Context: ${JSON.stringify(selector.selectedContext(), null, 3)}
 `);
   }
