648553b4c2a19affe9e05a5a273dd74c534a8e5a
Ahmed Mehfooz <amehfooz@chromium.org>
2019-10-15 23:52:58 +0000

    Fix stacked notification icon size and padding
    Change-Id: Id28394f3d11e190c58b05e37dbdfbef34d95a7d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1862857
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706225}

--- ash/system/message_center/stacked_notification_bar.cc
+++ ash/system/message_center/stacked_notification_bar.cc
@@ -129,9 +129,7 @@ StackedNotificationBar::StackedNotificationBar(
   SetVisible(false);
 
   auto* layout = SetLayoutManager(std::make_unique<views::BoxLayout>(
-      views::BoxLayout::Orientation::kHorizontal,
-      gfx::Insets(0, kStackingNotificationClearAllButtonPadding.left(), 0, 0),
-      0));
+      views::BoxLayout::Orientation::kHorizontal));
   layout->set_cross_axis_alignment(
       views::BoxLayout::CrossAxisAlignment::kStretch);
 
@@ -140,7 +138,8 @@ StackedNotificationBar::StackedNotificationBar(
     notification_icons_container_->SetLayoutManager(
         std::make_unique<views::BoxLayout>(
             views::BoxLayout::Orientation::kHorizontal,
-            kStackedNotificationIconsContainerPadding));
+            kStackedNotificationIconsContainerPadding,
+            kStackedNotificationBarIconSpacing));
     AddChildView(notification_icons_container_);
   }
 
