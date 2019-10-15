444d5620dba4442aa1c6d34e2689930d4b7d549d
Jüri Valdmann <juri.valdmann@qt.io>
2019-10-15 12:10:55 +0000

    GCC: Fix zlib symbol visibility macro
    GCC parses the function __attribute__ syntax a bit differently from Clang,
    associating the attribute with the return type instead of the function if the
    return type is a pointer. This leads to certain zlib symbols, such as
    Cr_z_zError, to fail to be properly exported from the shared library. Fix by
    using ZEXTERN instead of ZEXPORT for the attribute which works the same for both
    GCC and Clang.
    
    Bug: 819294
    Change-Id: I5707d1b0627a503df08df9ac0bb65fda69453989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1859788
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705917}

--- third_party/zlib/chromeconf.h
+++ third_party/zlib/chromeconf.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ZEXTERN __declspec(dllimport)
 #endif
 #elif defined(ZLIB_IMPLEMENTATION)
-#define ZEXPORT __attribute__((visibility("default")))
+#define ZEXTERN __attribute__((visibility("default")))
 #endif
 #endif
 
