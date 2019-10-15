7fd22a5fbf00e44f1d8af415598f00c6dcc5dcbc
Shawn Gallea <sagallea@google.com>
2019-10-15 19:17:22 +0000

    Allow 4hz framerate on headless devices
    Allows 4hz framerate on headless devices with kReduceHeadlessFrameRate
    feature
    
    Bug: b/139299756
    Test: Compile cast_shell for headless device
    Change-Id: I13a36cbc2225c20367188375518b34baf585f2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1815049
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706035}

--- chromecast/graphics/cast_window_manager_aura.cc
+++ chromecast/graphics/cast_window_manager_aura.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/graphics/cast_window_manager_aura.h"
 
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "build/build_config.h"
 #include "chromecast/base/cast_features.h"
+#include "chromecast/chromecast_buildflags.h"
 #include "chromecast/graphics/cast_focus_client_aura.h"
 #include "chromecast/graphics/cast_touch_activity_observer.h"
 #include "chromecast/graphics/cast_touch_event_gate.h"
@@ -255,6 +257,14 @@ void CastWindowManagerAura::Setup() {
     side_swipe_detector_ = std::make_unique<SideSwipeDetector>(
         system_gesture_dispatcher_.get(), root_window);
   }
+
+#if BUILDFLAG(IS_CAST_AUDIO_ONLY)
+  if (base::FeatureList::IsEnabled(kReduceHeadlessFrameRate)) {
+    ui::Compositor* compositor = window_tree_host_->compositor();
+    compositor->SetDisplayVSyncParameters(
+        base::TimeTicks(), base::TimeDelta::FromMilliseconds(250));
+  }
+#endif
 }
 
 void CastWindowManagerAura::OnWindowOrderChanged(
