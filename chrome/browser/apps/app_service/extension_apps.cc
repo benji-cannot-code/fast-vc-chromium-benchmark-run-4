605722b8be2a9bb1208553a3bafe7f3f849d0a64
Maggie Cai <mxcai@chromium.org>
2019-10-15 02:42:21 +0000

    [IntentHandling] Launch PWA with intent in the App Service.
    This CL adds PWA implementation for |LaunchAppWithIntent|.
    
    BUG=853604
    
    Change-Id: I6257f8d473d1368f0e769b1cca332cc72808bf23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1851628
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705806}

--- chrome/browser/apps/app_service/extension_apps.cc
+++ chrome/browser/apps/app_service/extension_apps.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_system.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/switches.h"
+#include "url/url_constants.h"
 
 // TODO(crbug.com/826982): life cycle events. Extensions can be installed and
 // uninstalled. ExtensionApps should implement extensions::InstallObserver and
@@ -101,6 +102,26 @@ ash::ShelfLaunchSource ConvertLaunchSource(
     case apps::mojom::LaunchSource::kFromFileManager:
       return ash::LAUNCH_FROM_UNKNOWN;
   }
+}
+
+apps::AppLaunchParams CreateAppLaunchParamsForIntent(
+    const std::string& app_id,
+    const apps::mojom::IntentPtr& intent) {
+  apps::AppLaunchParams params(
+      app_id, apps::mojom::LaunchContainer::kLaunchContainerWindow,
+      WindowOpenDisposition::NEW_FOREGROUND_TAB,
+      apps::mojom::AppLaunchSource::kSourceNone);
+
+  if (intent->scheme.has_value() && intent->host.has_value() &&
+      intent->path.has_value()) {
+    params.source = apps::mojom::AppLaunchSource::kSourceIntentUrl;
+    params.override_url =
+        GURL(intent->scheme.value() + url::kStandardSchemeSeparator +
+             intent->host.value() + intent->path.value());
+    DCHECK(params.override_url.is_valid());
+  }
+
+  return params;
 }
 }  // namespace
 
@@ -326,7 +347,13 @@ void ExtensionApps::LaunchAppWithIntent(const std::string& app_id,
                                         apps::mojom::IntentPtr intent,
                                         apps::mojom::LaunchSource launch_source,
                                         int64_t display_id) {
-  NOTIMPLEMENTED();
+  if (!profile_) {
+    return;
+  }
+
+  AppLaunchParams params = CreateAppLaunchParamsForIntent(app_id, intent);
+
+  apps::LaunchService::Get(profile_)->OpenApplication(params);
 }
 
 void ExtensionApps::SetPermission(const std::string& app_id,
