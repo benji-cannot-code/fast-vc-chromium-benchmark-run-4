99830cf9072db3aa0326958625f2d7ff64866e14
Austin Tankiang <austinct@chromium.org>
2019-10-15 01:33:26 +0000

    Remove file manager format dialog flag
    The new feature is working fine and enabled by default, so the flag is
    unneeded now.
    
    Bug: 1013999
    Change-Id: If7a82d3f01b386ce8cd0a5c2d56271e1aa704bec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1858023
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#705789}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -54,11 +54,6 @@ struct TestCase {
     return *this;
   }
 
-  TestCase& EnableFormatDialog() {
-    enable_format_dialog.emplace(true);
-    return *this;
-  }
-
   TestCase& EnableArc() {
     enable_arc = true;
     return *this;
@@ -125,7 +120,6 @@ struct TestCase {
   bool tablet_mode = false;
   base::Optional<bool> enable_myfiles_volume;
   base::Optional<bool> enable_documents_provider;
-  base::Optional<bool> enable_format_dialog;
   bool enable_arc = false;
   bool with_browser = false;
   bool needs_zip = false;
@@ -176,11 +170,6 @@ class FilesAppBrowserTest : public FileManagerBrowserTestBase,
   bool GetEnableDocumentsProvider() const override {
     return GetParam().enable_documents_provider.value_or(
         FileManagerBrowserTestBase::GetEnableDocumentsProvider());
-  }
-
-  bool GetEnableFormatDialog() const override {
-    return GetParam().enable_format_dialog.value_or(
-        FileManagerBrowserTestBase::GetEnableFormatDialog());
   }
 
   bool GetEnableArc() const override { return GetParam().enable_arc; }
@@ -826,11 +815,11 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
     FormatDialog, /* format_dialog.js */
     FilesAppBrowserTest,
-    ::testing::Values(TestCase("formatDialog").EnableFormatDialog(),
-                      TestCase("formatDialogEmpty").EnableFormatDialog(),
-                      TestCase("formatDialogCancel").EnableFormatDialog(),
-                      TestCase("formatDialogNameLength").EnableFormatDialog(),
-                      TestCase("formatDialogNameInvalid").EnableFormatDialog(),
-                      TestCase("formatDialogGearMenu").EnableFormatDialog()));
+    ::testing::Values(TestCase("formatDialog"),
+                      TestCase("formatDialogEmpty"),
+                      TestCase("formatDialogCancel"),
+                      TestCase("formatDialogNameLength"),
+                      TestCase("formatDialogNameInvalid"),
+                      TestCase("formatDialogGearMenu")));
 
 }  // namespace file_manager
