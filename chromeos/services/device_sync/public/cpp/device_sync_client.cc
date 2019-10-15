55e3aa2f3f1c0f90b7010d8810dcb53f86a955bb
James Cook <jamescook@chromium.org>
2019-10-15 22:12:15 +0000

    Migrate the Chrome OS device_sync service off of the mojo pref service
    DeviceSyncService runs in the browser process on the UI thread. It can
    directly use the Profile's PrefService*. This simplifies the code, and
    will reduce the amount of work rockot@ has to do for Service Manager
    cleanup.
    
    Delete the DeviceSyncService mojo interface and implementation object.
    They primarily existed to support wiring up the mojo pref service.
    Instead, directly connect the DeviceSyncClientImpl to the backing
    DeviceSyncImpl.
    
    Explicitly initialize the DeviceSyncImpl when the user's profile is
    ready. The old code had startup timing dependencies that assumed that
    DeviceSyncImpl was created after IdentityManager had information about
    the primary profile. The new code instantiates the DeviceSyncImpl
    object earlier, and IdentityManager may not be ready yet.
    
    Bug: 977637, 1012941
    Test: bots
    Change-Id: Ia32b687404e5b68980cb3aecd3003e6f7e48ffcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1849217
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#706181}

--- chromeos/services/device_sync/public/cpp/device_sync_client.cc
+++ chromeos/services/device_sync/public/cpp/device_sync_client.cc
@@ -13,6 +13,10 @@ DeviceSyncClient::DeviceSyncClient() = default;
 
 DeviceSyncClient::~DeviceSyncClient() = default;
 
+mojom::DeviceSyncPtr* DeviceSyncClient::GetDeviceSyncPtr() {
+  return nullptr;
+}
+
 void DeviceSyncClient::AddObserver(Observer* observer) {
   observer_list_.AddObserver(observer);
 }
