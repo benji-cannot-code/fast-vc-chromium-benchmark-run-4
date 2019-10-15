ec1c7d441d56d565e720eceeafe1681ac0676d87
Livvie Lin <livvielin@chromium.org>
2019-10-15 19:40:17 +0000

    Handle Safety Tip learn more link in page info
    Bug: 1007497
    Change-Id: I4498d39cabf4736a697294ad027082a53894a623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857166
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706076}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_base.h
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_base.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_PAGE_INFO_PAGE_INFO_BUBBLE_VIEW_BASE_H_
 #define CHROME_BROWSER_UI_VIEWS_PAGE_INFO_PAGE_INFO_BUBBLE_VIEW_BASE_H_
 
+#include "chrome/browser/ui/page_info/page_info_ui.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
@@ -57,7 +58,12 @@ class PageInfoBubbleViewBase : public views::BubbleDialogDelegateView,
   bool ShouldShowCloseButton() const override;
   void OnWidgetDestroying(views::Widget* widget) override;
 
+  PageInfoUI::SecurityDescription GetSecurityDetails() const;
   void set_window_title(const base::string16& title) { window_title_ = title; }
+  void set_security_description(
+      const PageInfoUI::SecurityDescription& description) {
+    security_description_ = description;
+  }
 
  private:
   // WebContentsObserver:
@@ -67,6 +73,7 @@ class PageInfoBubbleViewBase : public views::BubbleDialogDelegateView,
   void DidChangeVisibleSecurityState() override;
 
   base::string16 window_title_;
+  PageInfoUI::SecurityDescription security_description_;
 
   DISALLOW_COPY_AND_ASSIGN(PageInfoBubbleViewBase);
 };
