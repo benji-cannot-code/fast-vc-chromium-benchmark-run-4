f31e70088de0ce27f9ba1b0d6967f3d05122a51a
Lukasz Anforowicz <lukasza@chromium.org>
2019-10-15 20:40:53 +0000

    Fix how CORB async_test(s) work.
    Some CORB tests run in parallel, because they use `async_test`.  See
    https://web-platform-tests.org/writing-tests/testharness-api.html#asynchronous-tests:
    "Keep in mind that other tests could start executing before an
    Asynchronous Test is finished."
    
    This CL fixes such tests, to make sure that they either migrate to
    `promise_test` [1] or avoid using shared/global state.
    
    [1] https://web-platform-tests.org/writing-tests/testharness-api.html#promise-tests
    says "Unlike Asynchronous Tests, Promise Tests don’t start running until
    after the previous Promise Test finishes. Under rare circumstances, the
    next test may begin to execute before the returned promise has settled.
    Use add_cleanup to register any necessary cleanup actions such as
    resetting global state that need to happen consistently before the next
    test starts."
    
    Bug: 1011410
    Change-Id: Ib369e6cd07aed2daaf6347ae5d6f777a2c0835db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841898
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706131}

--- third_party/blink/web_tests/external/wpt/fetch/corb/resources/html-js-polyglot2.js
+++ third_party/blink/web_tests/external/wpt/fetch/corb/resources/html-js-polyglot2.js
@@ -6,6 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // which found out that some script resources are served
 // with text/html content-type and with a body that is
 // both a valid html and a valid javascript.
-window.polyglot = "html-js-polyglot2.js";
+window['html-js-polyglot2.js'] = true;
 
 //]]>--></script>
