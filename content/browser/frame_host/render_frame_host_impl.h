5dc8e3eb0c3a03f9799830a966340e6758345f60
Maks Orlovich <morlovich@chromium.org>
2019-10-15 20:02:30 +0000

    Adjust tests for upcoming cross-site cookie requirements
    They will need to be marked explicitly and secure, so adjust some tests to
    be over SSL and mark those cookies appropriately
    
    Bug: 1006816
    Change-Id: I6da53ea64a7c03530335913fadb9099992e8ca33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863389
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706108}

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -1327,9 +1327,9 @@ class CONTENT_EXPORT RenderFrameHostImpl
   FRIEND_TEST_ALL_PREFIXES(
       SitePerProcessBrowserTest,
       IsDetachedSubframeObservableDuringUnloadHandlerCrossProcess);
-  FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
+  FRIEND_TEST_ALL_PREFIXES(SitePerProcessSSLBrowserTest,
                            UnloadHandlersArePowerful);
-  FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
+  FRIEND_TEST_ALL_PREFIXES(SitePerProcessSSLBrowserTest,
                            UnloadHandlersArePowerfulGrandChild);
 
   class DroppedInterfaceRequestLogger;
