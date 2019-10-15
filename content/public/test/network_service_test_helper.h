ade4ae0d3b49cbc28395c90b716805ef50f21b88
Julie Jeongeun Kim <jkim@igalia.com>
2019-10-15 05:48:05 +0000

    Convert network_service_test.mojom to new Mojo types
    This CL converts NetworkServiceTest from
    network_service_test.mojom to new Mojo types using Remote.
    It updates NetworkServiceTestHelper with new Mojo types.
    
    Bug: 955171
    Change-Id: Ifa4b40a69740ab90aef7a2abe03bafd72e605c15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1847691
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#705837}

--- content/public/test/network_service_test_helper.h
+++ content/public/test/network_service_test_helper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "services/network/public/mojom/network_service_test.mojom.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 
@@ -30,8 +31,8 @@ class NetworkServiceTestHelper {
  private:
   class NetworkServiceTestImpl;
 
-  void BindNetworkServiceTestRequest(
-      network::mojom::NetworkServiceTestRequest request);
+  void BindNetworkServiceTestReceiver(
+      mojo::PendingReceiver<network::mojom::NetworkServiceTest> receiver);
 
   std::unique_ptr<NetworkServiceTestImpl> network_service_test_impl_;
 
