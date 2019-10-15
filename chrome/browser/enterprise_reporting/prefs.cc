ffaa34d9e9b64309e19fa1e5058ff9415dcfbe22
Owen Min <zmin@chromium.org>
2019-10-15 22:57:32 +0000

    Register pref for extension request ids
    Once user requests an extension, the extension id will be stored in the
    Profile's pref until:
    1) User cancels the request.
    2) Admin approves or deny the request.
    
    
    Bug: 1006899
    Change-Id: Id003bfceb9d67dfe685ee75926f792f2444469da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1856800
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706205}

--- chrome/browser/enterprise_reporting/prefs.cc
+++ chrome/browser/enterprise_reporting/prefs.cc
@@ -24,6 +24,7 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(prefs::kCloudExtensionRequestEnabled, false);
+  registry->RegisterListPref(prefs::kCloudExtensionRequestIds);
 }
 
 }  // namespace enterprise_reporting
--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -846,6 +846,7 @@ extern const char kRegisteredSupervisedUserWhitelists[];
 #if !defined(OS_ANDROID)
 extern const char kCloudReportingEnabled[];
 extern const char kCloudExtensionRequestEnabled[];
+extern const char kCloudExtensionRequestIds[];
 #endif
 
 #if BUILDFLAG(ENABLE_BACKGROUND_MODE)
