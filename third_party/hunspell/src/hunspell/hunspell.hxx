18e730d25efa48d5a7847821bc8511df809446a7
Alexander Rezepkin <etu@vivaldi.net>
2019-10-16 16:21:35 +0000

    [Hunspell] Reduce maximum suggestions from 15 to 5.
    Before this patch, the maximum number of suggestions within Hunspell
    was 15, although Chrome uses only 5 suggestions.
    See: https://cs.chromium.org/chromium/src/components/spellcheck/common/spellcheck_common.h?l=22&rcl=a88c5fe6e641bdc9cb84b64ddb90ead67e0d0bcc
    
    So when a misspelled word has more than 5 suggestions, Hunspell does
    a useless job and searches for suggestions that will never be used.
    
    This patch reduces from 15 to 5 the maximum number of searched
    suggestions within Hunspell.
    
    Bug: NONE
    Change-Id: I68054cfc161ec509c99a620de2b0e5252576853c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863298
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706478}

--- third_party/hunspell/src/hunspell/hunspell.hxx
+++ third_party/hunspell/src/hunspell/hunspell.hxx
@@ -86,7 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define SPELL_XML "<?xml?>"
 
-#define MAXSUGGESTION 15
+#define MAXSUGGESTION 5
 #define MAXSHARPS 5
 
 #ifndef MAXWORDLEN
