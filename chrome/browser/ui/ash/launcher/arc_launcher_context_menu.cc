feedc44af6a74fa57bc8881ed8edcd6ec8489c51
nancy <nancylingwang@chromium.org>
2019-10-16 21:15:34 +0000

    Use AppService Uninstall to uninstall ARC apps.
    BUG=1009248
    
    Change-Id: I0aca18b0a9cf6fc3dc72b4ec0cf1f33451279acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863062
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706623}

--- chrome/browser/ui/ash/launcher/arc_launcher_context_menu.cc
+++ chrome/browser/ui/ash/launcher/arc_launcher_context_menu.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/app_menu_constants.h"
 #include "ash/public/cpp/shelf_item.h"
+#include "chrome/browser/apps/app_service/app_service_proxy.h"
+#include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/chromeos/arc/app_shortcuts/arc_app_shortcuts_menu_builder.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/app_list/app_list_controller_delegate.h"
@@ -66,6 +68,14 @@ void ArcLauncherContextMenu::ExecuteCommand(int command_id, int event_flags) {
     return;
   }
   if (command_id == ash::UNINSTALL) {
+    if (base::FeatureList::IsEnabled(features::kAppServiceShelf)) {
+      apps::AppServiceProxy* proxy =
+          apps::AppServiceProxyFactory::GetForProfile(controller()->profile());
+      DCHECK(proxy);
+      proxy->Uninstall(item().id.app_id);
+      return;
+    }
+
     arc::ShowArcAppUninstallDialog(controller()->profile(),
                                    nullptr /* controller */, item().id.app_id);
     return;
