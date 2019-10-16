ba6bf9524aa7f01bbe075c2984037aa8a8662411
Brandon Wylie <wylieb@chromium.org>
2019-10-16 00:55:48 +0000

    [SEC] Fix metrics naming and reporting issues
    Bug: 1001839
    Change-Id: Id27380f95b5c06f938826d4ec9a4725577034718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1861041
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706252}

--- chrome/android/junit/src/org/chromium/chrome/browser/search_engines/SearchEngineChoiceMetricsTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/search_engines/SearchEngineChoiceMetricsTest.java
@@ -120,6 +120,10 @@ public final class SearchEngineChoiceMetricsTest {
         assertEquals(0,
                 ShadowRecordHistogram.getHistogramValueCountForTesting(
                         HISTOGRAM_AFTER_CHOICE, SearchEngineType.SEARCH_ENGINE_GOOGLE));
+        assertEquals(0,
+                ShadowRecordHistogram.getHistogramValueCountForTesting(
+                        "Android.SearchEngineChoice.EventsV2",
+                        SearchEngineChoiceMetrics.Events.SEARCH_ENGINE_CHANGED));
     }
 
     @Test
