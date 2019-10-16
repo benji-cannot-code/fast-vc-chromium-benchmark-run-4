d3f5011c4582651d81e93019aa4651bd70459da9
Tetsui Ohkubo <tetsui@chromium.org>
2019-10-16 00:24:02 +0000

    arc: notification: Forward window activation.
    In ARC, activation of a window is mapped to activity stacking.
    Notifications, which are not activities, are always placed at the top
    of windows.
    
    When an inline notification and an activity with an input field are
    shown at the same time, it causes an issue because regardless of the
    activity having focus, the notification always receives keyboard input.
    
    In order to fix that, we have to forward ash side activation through
    mojo and reflect it to the NOT_FOCUSABLE flag of notifications.
    
    A Tast test for this will be added.
    
    Android side CL: http://ag/9537575
    
    TEST=manual (Follow the repro steps in the bug.)
    BUG=b:142103828
    
    Change-Id: Ifda5532b498ef9ea01ee70836813ae700a6ec58f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1855545
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706235}

--- ash/system/message_center/arc/arc_notification_content_view.h
+++ ash/system/message_center/arc/arc_notification_content_view.h
@@ -117,6 +117,7 @@ class ArcNotificationContentView
 
   // views::WidgetObserver:
   void OnWidgetClosing(views::Widget* widget) override;
+  void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
 
   // ArcNotificationItem::Observer
   void OnItemDestroying() override;
