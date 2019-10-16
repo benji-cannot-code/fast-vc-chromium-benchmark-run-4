fad87ca1d485b8a8709193656290e95001f7d521
Joe DeBlasio <jdeblasio@chromium.org>
2019-10-16 22:17:23 +0000

    Add banner to safety tips.
    This CL adds a banner to the Safety Tip bubble on desktop platforms. It
    introduces two UIs: one for URL lookalikes, and one for suspicious
    sites.
    
    It now looks like this:
    https://drive.google.com/file/d/1RppvDBXThzzbu5CknJ_-nYP6iKkXH8l6/view
    
    Bug: 1013401
    Change-Id: Id2eef386ddeedc89535f8e8097a8348bf2572558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865539
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706656}

--- chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view.cc
+++ chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view.cc
@@ -33,6 +33,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using security_state::SafetyTipStatus;
 
+namespace {
+
+int GetSafetyTipBannerId(security_state::SafetyTipStatus safety_tip_status) {
+  switch (safety_tip_status) {
+    case security_state::SafetyTipStatus::kBadReputation:
+      return IDR_SAFETY_TIP_SUSPICIOUS_ILLUSTRATION;
+    case security_state::SafetyTipStatus::kLookalike:
+      return IDR_SAFETY_TIP_LOOKALIKE_ILLUSTRATION;
+    case security_state::SafetyTipStatus::kBadKeyword:
+    case security_state::SafetyTipStatus::kUnknown:
+    case security_state::SafetyTipStatus::kNone:
+      NOTREACHED();
+  }
+  NOTREACHED();
+  return IDR_SAFETY_TIP_SUSPICIOUS_ILLUSTRATION;
+}
+
+}  // namespace
+
 SafetyTipPageInfoBubbleView::SafetyTipPageInfoBubbleView(
     views::View* anchor_view,
     const gfx::Rect& anchor_rect,
@@ -92,7 +111,15 @@ SafetyTipPageInfoBubbleView::SafetyTipPageInfoBubbleView(
   bubble_col_set->AddColumn(views::GridLayout::LEADING, views::GridLayout::FILL,
                             1.0, views::GridLayout::USE_PREF, 0, 0);
 
-  // TODO(crbug/996731): Add banner once available. See crrev/c/1816805/7.
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
+  const gfx::ImageSkia* image =
+      rb.GetNativeImageNamed(GetSafetyTipBannerId(safety_tip_status))
+          .ToImageSkia();
+  auto image_view = std::make_unique<NonAccessibleImageView>();
+  image_view->SetImage(*image);
+  views::BubbleFrameView* frame_view = GetBubbleFrameView();
+  CHECK(frame_view);
+  frame_view->SetHeaderView(std::move(image_view));
 
   auto bottom_view = std::make_unique<views::View>();
   views::GridLayout* bottom_layout =
