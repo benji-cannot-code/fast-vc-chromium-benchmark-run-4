dd2382420a89d8fd34bc17c148cc921d1c435cdd
Mei Liang <meiliang@chromium.org>
2019-10-16 23:51:23 +0000

    [GTS] Tab-to-Grid animation background
    This CL sets the Tab-to-Grid animation background color to be the same
    as the tab switcher background color.
    
    Change-Id: I7dc736dfa6dcdeda5a0a6f9dc4f49917b2cb9188
    Bug: 1013001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1854167
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706705}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceLayout.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceLayout.java
@@ -35,6 +35,7 @@ import org.chromium.chrome.browser.compositor.scene_layer.SceneLayer;
 import org.chromium.chrome.browser.compositor.scene_layer.TabListSceneLayer;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tasks.tab_management.TabSwitcher;
 import org.chromium.chrome.browser.ui.widget.animation.Interpolators;
 import org.chromium.chrome.browser.util.FeatureUtilities;
@@ -137,6 +138,12 @@ public class StartSurfaceLayout extends Layout implements StartSurface.OverviewM
     }
 
     // Layout implementation.
+    @Override
+    public void setTabModelSelector(TabModelSelector modelSelector, TabContentManager manager) {
+        super.setTabModelSelector(modelSelector, manager);
+        mSceneLayer.setTabModelSelector(modelSelector);
+    }
+
     @Override
     public LayoutTab getLayoutTab(int id) {
         return mDummyLayoutTab;
