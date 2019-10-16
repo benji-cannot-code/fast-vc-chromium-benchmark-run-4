1dcbe313f46393e6b098ca4dd945996dd92b4232
Vladimir Levin <vmpstr@chromium.org>
2019-10-16 22:22:57 +0000

    DL: Classify tab focus/selection as viewport activations.
    This patch updates the activation reasons to classify tab focus and
    selection as being viewport activations.
    
    This implements the resolution from:
    https://github.com/WICG/display-locking/issues/87
    
    R=chrishtr@chromium.org, rakina@chromium.org
    
    Change-Id: I9202713a5b375775a7ac5fd3348d949d8f4a3cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864821
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Auto-Submit: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706657}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4042,7 +4042,12 @@ void Element::focus(const FocusParams& params) {
       return;
     }
   }
-  ActivateDisplayLockIfNeeded(DisplayLockActivationReason::kUser);
+  // If script called focus(), then the type would be none. This means we are
+  // activating because of a script action (kUser). Otherwise, this is a
+  // viewport activation (kViewport).
+  ActivateDisplayLockIfNeeded(params.type == kWebFocusTypeNone
+                                  ? DisplayLockActivationReason::kUser
+                                  : DisplayLockActivationReason::kViewport);
   DispatchActivateInvisibleEventIfNeeded();
   if (IsInsideInvisibleSubtree()) {
     // The element stays invisible because the default event action is
@@ -4196,7 +4201,7 @@ bool Element::IsKeyboardFocusable() const {
          ((SupportsFocus() && tabIndex() >= 0) ||
           (RuntimeEnabledFeatures::KeyboardFocusableScrollersEnabled() &&
            IsScrollableNode(this))) &&
-         !DisplayLockPreventsActivation(DisplayLockActivationReason::kUser);
+         !DisplayLockPreventsActivation(DisplayLockActivationReason::kViewport);
 }
 
 bool Element::IsMouseFocusable() const {
@@ -4205,7 +4210,7 @@ bool Element::IsMouseFocusable() const {
   DCHECK(!GetDocument().IsActive() ||
          !GetDocument().NeedsLayoutTreeUpdateForNode(*this));
   return isConnected() && !IsInert() && IsFocusableStyle() && SupportsFocus() &&
-         !DisplayLockPreventsActivation(DisplayLockActivationReason::kUser);
+         !DisplayLockPreventsActivation(DisplayLockActivationReason::kViewport);
 }
 
 bool Element::IsAutofocusable() const {
