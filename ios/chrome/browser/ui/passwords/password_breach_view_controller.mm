a312d1f328cb1f246a80fa325c4833c4b21cf052
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-10-16 14:48:02 +0000

    [iOS][Password-Breach] Cappitalize title and primary action
    Change-Id: Ifc0899f198c9dad30c55e17efa6cf24bb2444b9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864680
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706446}

--- ios/chrome/browser/ui/passwords/password_breach_view_controller.mm
+++ ios/chrome/browser/ui/passwords/password_breach_view_controller.mm
@@ -292,7 +292,7 @@ constexpr CGFloat kSafeAreaMultiplier = 0.8;
       [UIFontMetrics metricsForTextStyle:UIFontTextStyleTitle1];
   title.font = [fontMetrics scaledFontForFont:font];
   title.textColor = [UIColor colorNamed:kTextPrimaryColor];
-  title.text = self.titleString;
+  title.text = self.titleString.capitalizedString;
   title.textAlignment = NSTextAlignmentCenter;
   title.translatesAutoresizingMaskIntoConstraints = NO;
   title.adjustsFontForContentSizeCategory = YES;
@@ -341,7 +341,7 @@ constexpr CGFloat kSafeAreaMultiplier = 0.8;
   [primaryActionButton addTarget:self
                           action:@selector(didTapPrimaryActionButton)
                 forControlEvents:UIControlEventTouchUpInside];
-  [primaryActionButton setTitle:self.primaryActionString
+  [primaryActionButton setTitle:self.primaryActionString.capitalizedString
                        forState:UIControlStateNormal];
   primaryActionButton.contentEdgeInsets =
       UIEdgeInsetsMake(kButtonVerticalInsets, 0, kButtonVerticalInsets, 0);
