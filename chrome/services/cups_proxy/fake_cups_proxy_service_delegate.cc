fc66361cd95f6d3cbf56bf2aac61a7ad65a1666c
Luum Habtemariam <luum@chromium.org>
2019-10-16 00:14:18 +0000

    Update CupsPrintersManager with CupsProxy printer installs
    Now CupsProxyService correctly updates CupsPrintersManager when it
    successfully installs a printer into CUPS. Since any print job involves
    many IPP requests, each of which could trigger a printer install, this
    caching debug daemon calls.
    
    Bug: chromium:945409
    Test: updated unit_tests pass
    Change-Id: I4d0cba3e5cbe35c5acd2a78fc5a94ff73da5cf1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863729
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Luum Habtemariam <luum@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706231}

--- chrome/services/cups_proxy/fake_cups_proxy_service_delegate.cc
+++ chrome/services/cups_proxy/fake_cups_proxy_service_delegate.cc
@@ -22,6 +22,9 @@ bool FakeCupsProxyServiceDelegate::IsPrinterInstalled(
   return false;
 }
 
+void FakeCupsProxyServiceDelegate::PrinterInstalled(
+    const chromeos::Printer& printer) {}
+
 scoped_refptr<base::SingleThreadTaskRunner>
 FakeCupsProxyServiceDelegate::GetIOTaskRunner() {
   return nullptr;
