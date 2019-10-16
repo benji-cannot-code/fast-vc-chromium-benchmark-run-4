712bbba7e53d40d16ea03afb14680c32b55fd1c2
Francois Doray <fdoray@chromium.org>
2019-10-16 20:59:46 +0000

    Clarify and enable the "tab freezing" enterprise policy.
    The policy was previously named TabLifecyclesEnabled and its
    name/description said that it controlled throttling, freezing and
    proactive discarding. It actually never controlled throttling and we
    decided to abandon the proactive discarding feature.
    
    Therefore, the name and description are changed to indicate that it
    controls tab freezing only. Also, the "future" tag is removed since
    we plan to ship tab freezing in M79.
    
    Bug: 844426
    Change-Id: I6fdab954aa9d1c9fb5a63217ccda7b869726fc93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863382
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706609}

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -964,7 +964,7 @@ extern const char kBlockAutoplayEnabled[];
 extern const char kNotificationNextPersistentId[];
 extern const char kNotificationNextTriggerTime[];
 
-extern const char kTabLifecyclesEnabled[];
+extern const char kTabFreezingEnabled[];
 
 extern const char kEnterpriseHardwarePlatformAPIEnabled[];
 
