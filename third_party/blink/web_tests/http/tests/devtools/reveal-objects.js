4aaa9920bb37c226791579ab909d6eeb8cda20ef
Paul Lewis <aerotwist@chromium.org>
2019-10-16 16:14:18 +0000

    DevTools: Warn when revealed node is detached from the document.
    If a user right clicks on an element in the console and chooses to reveal it in
    the elements panel, and the element is not in the document, the operation fails
    but it is not communicated to the user. This patch adds a warning if the node
    appears to be detached from the document; it is done through checking the
    parentNode property, which is the most suitable means in lieu of having the
    isConnected property available in the Node object.
    
    Bug: 967156
    Change-Id: I58228facb54e9afc87215d68176f473121121db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1681891
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706474}

--- third_party/blink/web_tests/http/tests/devtools/reveal-objects.js
+++ third_party/blink/web_tests/http/tests/devtools/reveal-objects.js
@@ -12,8 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await TestRunner.showPanel('resources');
   await TestRunner.showPanel('network');
   await TestRunner.loadHTML(`
-      <div id="div">
+      <div id="targetnode">
       </div>
+      <span id="toremove"></span>
+      <span id="containertoremove">
+        <span id="child"></span>
+      </span>
     `);
   await TestRunner.evaluateInPagePromise(`
       function loadResource(url)
@@ -25,14 +29,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   `);
   await TestRunner.addScriptTag('resources/bar.js');
 
-  var node;
+  var divNode;
+  var spanNode;
+  var childNode;
+  var container;
   var resource;
   var uiLocation;
   var requestWithResource;
   var requestWithoutResource;
 
   TestRunner.runTestSuite([
-    function init(next) {
+    async function init(next) {
       installHooks();
 
       TestRunner.resourceTreeModel.forAllResources(function(r) {
@@ -43,14 +50,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       });
       uiLocation = Workspace.workspace.uiSourceCodeForURL(resource.url).uiLocation(2, 1);
 
-      ElementsTestRunner.nodeWithId('div', nodeCallback);
+      divNode = await ElementsTestRunner.nodeWithIdPromise('targetnode');
+      spanNode = await ElementsTestRunner.nodeWithIdPromise('toremove');
+      childNode = await ElementsTestRunner.nodeWithIdPromise('child');
+      container = await ElementsTestRunner.nodeWithIdPromise('containertoremove');
 
-      function nodeCallback(foundNode) {
-        node = foundNode;
-        NetworkTestRunner.recordNetwork();
-        var url = TestRunner.url('bar.js');
-        TestRunner.evaluateInPage(`loadResource('${url}')`, firstXhrCallback);
-      }
+      NetworkTestRunner.recordNetwork();
+      var url = TestRunner.url('bar.js');
+      TestRunner.evaluateInPage(`loadResource('${url}')`, firstXhrCallback);
 
       function firstXhrCallback() {
         requestWithResource = SDK.networkLog.requestForURL(resource.url);
@@ -70,7 +77,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     function revealNode(next) {
-      Common.Revealer.reveal(node).then(next);
+      Common.Revealer.reveal(divNode).then(next);
+    },
+
+    function revealRemovedNode(next) {
+      spanNode.removeNode(function() {
+        Common.Revealer.reveal(spanNode).then(() => {
+          TestRunner.addResult('Removed node revealed');
+        }, () => {
+          TestRunner.addResult('Removed node not revealed');
+        }).finally(next);
+      });
+    },
+
+    function revealNodeWithRemovedParent(next) {
+      // Note: we remove the container, and then check that the child also can
+      // not be revealed since it is also detached.
+      container.parentNode.removeNode(function() {
+        Common.Revealer.reveal(childNode).then(() => {
+          TestRunner.addResult('Node with removed parent revealed');
+        }, () => {
+          TestRunner.addResult('Node with removed parent not revealed');
+        }).finally(next);
+      });
     },
 
     function revealUILocation(next) {
