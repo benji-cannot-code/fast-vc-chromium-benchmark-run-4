839c89deb9cdd00760dee61fe27aede8591fe555
Peter Wen <wnwen@chromium.org>
2019-10-16 13:53:43 +0000

    Android: Ban all usages of System#exit
    Apart from the build/tooling/test/third-party directories listed in
    suppressions.xml, every usage of System#exit must be explicit and have
    the @SuppressWarnings annotation, preferably with a short explanation of
    why it is necessary.
    
    Added @SuppressWarnings for all existing usages.
    
    Removed the System#exit call in ChromeApplication since we have no
    visibility into how often the application replacing bug is hit. Throwing
    an exception with a custom message will help us see its current impact.
    
    Bug: 1000651
    Change-Id: Ia48598b32e339efccb0ca1b55ef03068da505410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1851770
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706426}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -20,7 +20,6 @@ import org.chromium.base.BuildInfo;
 import org.chromium.base.CommandLineInitUtil;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.JNIUtils;
-import org.chromium.base.Log;
 import org.chromium.base.PathUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.annotations.MainDex;
@@ -55,7 +54,6 @@ import org.chromium.ui.base.ResourceBundle;
  */
 public class ChromeApplication extends Application {
     private static final String COMMAND_LINE_FILE = "chrome-command-line";
-    private static final String TAG = "ChromiumApplication";
     // Public to allow use in ChromeBackupAgent
     public static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "chrome";
 
@@ -171,8 +169,7 @@ public class ChromeApplication extends Application {
         // out-of-date application. Kill old applications in this bad state. See
         // http://crbug.com/658130 for more context and http://b.android.com/56296 for the bug.
         if (ContextUtils.getApplicationAssets() == null) {
-            Log.e(TAG, "getResources() null, closing app.");
-            System.exit(0);
+            throw new RuntimeException("App out of date, getResources() null, closing app.");
         }
     }
 
