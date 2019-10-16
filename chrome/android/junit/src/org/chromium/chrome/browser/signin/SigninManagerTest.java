7cacbdfd7c7af27c62968cb2f6e161d59c604741
Alice Wang <aliceywang@chromium.org>
2019-10-16 10:01:08 +0000

    [Signin][Android] Move DisableNativeTestRule
    Move DisableNativeTestRule from the package org.chromium.chrome.test.ui
    to org.chromium.chrome.test and simplify the SigninManagerTest.
    
    Bug: 1007903
    Change-Id: I0829f0d7e11fd32b36d40e439ca9dd1a235e7676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857326
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706366}

--- chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerTest.java
@@ -23,7 +23,6 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 import android.accounts.Account;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -31,10 +30,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.robolectric.annotation.Config;
 
-import org.chromium.base.metrics.RecordHistogram;
-import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.chrome.test.DisableNativeTestRule;
 import org.chromium.components.signin.AccountManagerFacade;
 import org.chromium.components.signin.AccountTrackerService;
 import org.chromium.components.signin.identitymanager.CoreAccountId;
@@ -51,7 +49,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Config(manifest = Config.NONE)
 public class SigninManagerTest {
     @Rule
-    public JniMocker mocker = new JniMocker();
+    public final JniMocker mocker = new JniMocker();
+
+    @Rule
+    public final DisableNativeTestRule mDisableNative = new DisableNativeTestRule();
 
     @Mock
     SigninManager.Natives mNativeMock;
@@ -64,10 +65,6 @@ public class SigninManagerTest {
 
     @Before
     public void setUp() {
-        // TODO(https://crbug.com/1007903): Use DisableNativeTestRule instead.
-        RecordHistogram.setDisabledForTests(true);
-        RecordUserAction.setDisabledForTests(true);
-
         initMocks(this);
 
         mocker.mock(SigninManagerJni.TEST_HOOKS, mNativeMock);
@@ -87,13 +84,6 @@ public class SigninManagerTest {
 
         mAccount = new CoreAccountInfo(new CoreAccountId("gaia-id-user"),
                 AccountManagerFacade.createAccountFromName("user@domain.com"), "gaia-id-user");
-    }
-
-    @After
-    public void tearDown() {
-        // TODO(https://crbug.com/1007903): Use DisableNativeTestRule instead.
-        RecordHistogram.setDisabledForTests(false);
-        RecordUserAction.setDisabledForTests(false);
     }
 
     @Test
