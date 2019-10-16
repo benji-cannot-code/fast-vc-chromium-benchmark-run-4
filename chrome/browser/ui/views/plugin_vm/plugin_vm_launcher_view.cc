a269de7dfb678a227318ddfc0e8dc746c6f54374
Timothy Loh <timloh@chromium.org>
2019-10-16 06:55:27 +0000

    Tidy PluginVmLauncherViewBrowserTest
    This CL tidies up PluginVmLauncherViewBrowserTest, removing the use of
    inheritance to observe state changes and instead allowing the class
    under test to have a callback which it calls upon completion. The newly
    added functions will also be used for an upcoming autotest private API,
    allowing the installer to be run from tast.
    
    Bug: 1006664
    Change-Id: I3abb2d0fd08d0988c140dbb564c70a02929d1ad8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863063
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706337}

--- chrome/browser/ui/views/plugin_vm/plugin_vm_launcher_view.cc
+++ chrome/browser/ui/views/plugin_vm/plugin_vm_launcher_view.cc
@@ -171,6 +171,11 @@ PluginVmLauncherView::PluginVmLauncherView(Profile* profile)
   layout->SetFlexForView(lower_container_view, 1, true);
 }
 
+// static
+PluginVmLauncherView* PluginVmLauncherView::GetActiveViewForTesting() {
+  return g_plugin_vm_launcher_view;
+}
+
 int PluginVmLauncherView::GetDialogButtons() const {
   switch (state_) {
     case State::START_DOWNLOADING:
@@ -365,6 +370,11 @@ base::string16 PluginVmLauncherView::GetMessage() const {
   }
 }
 
+void PluginVmLauncherView::SetFinishedCallbackForTesting(
+    base::OnceCallback<void(bool success)> callback) {
+  finished_callback_for_testing_ = std::move(callback);
+}
+
 PluginVmLauncherView::~PluginVmLauncherView() {
   plugin_vm_image_manager_->RemoveObserver();
   g_plugin_vm_launcher_view = nullptr;
@@ -408,6 +418,12 @@ void PluginVmLauncherView::OnStateUpdated() {
 
   DialogModelChanged();
   GetWidget()->GetRootView()->Layout();
+
+  if (state_ == State::FINISHED || state_ == State::ERROR ||
+      state_ == State::NOT_ALLOWED) {
+    if (finished_callback_for_testing_)
+      std::move(finished_callback_for_testing_).Run(state_ == State::FINISHED);
+  }
 }
 
 base::string16 PluginVmLauncherView::GetDownloadProgressMessage(
