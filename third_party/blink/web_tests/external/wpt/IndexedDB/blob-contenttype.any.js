6fabd56f31ffbe586ae8feabd16d08f86d65549f
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-10-16 00:18:02 +0000

    Import wpt@50fa6ade9bda8c138ee37878c8442f33af09cb7d
    Using wpt-import in Chromium a37797f5c44633873ab7b1b7356a68555530c3e5.
    With Chromium commits locally applied on WPT:
    6386d9d857 "Update service_worker_unregister_and_register()"
    f399b336b4 "Fetch Metadata: Add a test for `<embed>` navigated after loading."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jsbell@chromium.org:
      external/wpt/IndexedDB
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: I5a8f1e303f2276b66f008f1cf7151c6102e9b919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863436
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#706232}

--- third_party/blink/web_tests/external/wpt/IndexedDB/blob-contenttype.any.js
+++ third_party/blink/web_tests/external/wpt/IndexedDB/blob-contenttype.any.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: title=Blob Content Type
 // META: script=support.js
+// META: timeout=long
 
 indexeddb_test(
     function upgrade(t, db) {
