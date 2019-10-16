51adff37a78d8a72350229fca3a4eb26f745a85a
Parastoo Geranmayeh <parastoog@google.com>
2019-10-16 15:12:36 +0000

    Revert "Adjust tests for upcoming cross-site cookie requirements"
    This reverts commit 83b71438dce16647b8d1e5bb381c2f3ec5e854a5.
    
    Reason for revert: SitePerProcessSSLBrowserTest.UnloadHandlersArePowerful*
    failing on Android. Please see bug.
    
    Bug: 1014956
    Original change's description:
    > Adjust tests for upcoming cross-site cookie requirements
    > 
    > They will need to be marked explicitly and secure, so adjust some tests to
    > be over SSL and mark those cookies appropriately
    > 
    > Bug: 1006816
    > Change-Id: I6da53ea64a7c03530335913fadb9099992e8ca33
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863389
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#706108}
    
    TBR=avi@chromium.org,morlovich@chromium.org
    
    Change-Id: I982bc1590171f92883000c8315d3be77f7b35b9f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1006816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865249
    Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#706453}

--- content/browser/site_per_process_unload_browsertest.cc
+++ content/browser/site_per_process_unload_browsertest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "components/network_session_configurator/common/network_switches.h"
 #include "content/browser/frame_host/cross_process_frame_connector.h"
 #include "content/browser/frame_host/frame_tree.h"
 #include "content/browser/frame_host/navigation_controller_impl.h"
@@ -1294,32 +1293,6 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest,
   delete_B1.WaitUntilDeleted();
 }
 
-// Some tests need an https server because third-party cookies are used, and
-// SameSite=None cookies must be Secure. This is a separate fixture due to
-// kIgnoreCertificateErrors flag.
-class SitePerProcessSSLBrowserTest : public SitePerProcessBrowserTest {
- protected:
-  SitePerProcessSSLBrowserTest()
-      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
-
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    SitePerProcessBrowserTest::SetUpCommandLine(command_line);
-    // This is necessary to use https with arbitrary hostnames.
-    command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
-  }
-
-  void SetUpOnMainThread() override {
-    https_server()->AddDefaultHandlers(GetTestDataFilePath());
-    ASSERT_TRUE(https_server()->Start());
-    SitePerProcessBrowserTest::SetUpOnMainThread();
-  }
-
-  net::EmbeddedTestServer* https_server() { return &https_server_; }
-
- private:
-  net::EmbeddedTestServer https_server_;
-};
-
 // Unload handlers should be able to do things that might require for instance
 // the RenderFrameHostImpl to stay alive.
 // - use console.log (handled via RFHI::DidAddMessageToConsole).
@@ -1336,11 +1309,10 @@ class SitePerProcessSSLBrowserTest : public SitePerProcessBrowserTest {
 //
 // This test is similar to UnloadHandlersArePowerfulGrandChild, but with a
 // different frame hierarchy.
-IN_PROC_BROWSER_TEST_F(SitePerProcessSSLBrowserTest,
-                       UnloadHandlersArePowerful) {
+IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest, UnloadHandlersArePowerful) {
   // Navigate to a page hosting a cross-origin frame.
-  GURL url =
-      https_server()->GetURL("a.com", "/cross_site_iframe_factory.html?a(b)");
+  GURL url = embedded_test_server()->GetURL(
+      "a.com", "/cross_site_iframe_factory.html?a(b)");
   EXPECT_TRUE(NavigateToURL(shell(), url));
 
   RenderFrameHostImpl* A1 = web_contents()->GetMainFrame();
@@ -1369,8 +1341,7 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessSSLBrowserTest,
 
       // As a sanity check, test that RFHI-independent things also work fine.
       localStorage.localstorage_test_key = 'localstorage_test_value';
-      document.cookie = 'cookie_test_key=' +
-                        'cookie_test_value; SameSite=none; Secure';
+      document.cookie = 'cookie_test_key=' + 'cookie_test_value';
     });
   )"));
 
@@ -1382,7 +1353,7 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessSSLBrowserTest,
     RenderFrameDeletedObserver B2_deleted(B2);
 
     // Navigate
-    GURL away_url(https_server()->GetURL("a.com", "/title1.html"));
+    GURL away_url(embedded_test_server()->GetURL("a.com", "/title1.html"));
     ASSERT_TRUE(ExecJs(A1, JsReplace("location = $1", away_url)));
 
     // Observers must be reached.
@@ -1427,11 +1398,11 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessSSLBrowserTest,
 //
 // This test is similar to UnloadHandlersArePowerful, but with a different frame
 // hierarchy.
-IN_PROC_BROWSER_TEST_F(SitePerProcessSSLBrowserTest,
+IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest,
                        UnloadHandlersArePowerfulGrandChild) {
   // Navigate to a page hosting a cross-origin frame.
-  GURL url = https_server()->GetURL("a.com",
-                                    "/cross_site_iframe_factory.html?a(b(c))");
+  GURL url = embedded_test_server()->GetURL(
+      "a.com", "/cross_site_iframe_factory.html?a(b(c))");
   EXPECT_TRUE(NavigateToURL(shell(), url));
 
   RenderFrameHostImpl* A1 = web_contents()->GetMainFrame();
@@ -1462,8 +1433,7 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessSSLBrowserTest,
 
       // As a sanity check, test that RFHI-independent things also work fine.
       localStorage.localstorage_test_key = 'localstorage_test_value';
-      document.cookie = 'cookie_test_key=' +
-                        'cookie_test_value; SameSite=none; Secure';
+      document.cookie = 'cookie_test_key=' + 'cookie_test_value';
     });
   )"));
 
@@ -1476,7 +1446,7 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessSSLBrowserTest,
     RenderFrameDeletedObserver C3_deleted(C3);
 
     // Navigate
-    GURL away_url(https_server()->GetURL("a.com", "/title1.html"));
+    GURL away_url(embedded_test_server()->GetURL("a.com", "/title1.html"));
     ASSERT_TRUE(ExecJs(A1, JsReplace("location = $1", away_url)));
 
     // Observers must be reached.
