1002735edf09ca4415fef47a83a770ca83ca9a39
Eric Karl <ericrk@chromium.org>
2019-10-16 17:08:06 +0000

    Null DisplaySchedulerClient before teardown
    Currently, when Display is destroyed, it tears down DisplayScheduler
    midway through destruction. This is problematic as DisplayScheduler
    can call back into Display in a partially destroyed state.
    
    We could force DisplayScheduler to be destroyed earlier, but this
    is still somewhat brittle. Instead we allow DisplayScheduler to have
    a null DisplaySchedulerClient and null this before destroying
    Display.
    
    Bug: 1012943
    Change-Id: I5892ed2746ba29ef5c3c027e9564e52ee6c659ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863674
    Auto-Submit: Eric Karl <ericrk@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Eric Karl <ericrk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706492}

--- components/viz/service/display/display.cc
+++ components/viz/service/display/display.cc
@@ -219,6 +219,11 @@ Display::~Display() {
       surface_manager_->RemoveObserver(scheduler_.get());
   }
 
+  // Un-register as DisplaySchedulerClient to prevent us from being called in a
+  // partially destructed state.
+  if (scheduler_)
+    scheduler_->SetClient(nullptr);
+
   RunDrawCallbacks();
 }
 
