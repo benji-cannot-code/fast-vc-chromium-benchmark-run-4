653a4ba44eec00ceb4bcf37a5585a5b5b339e4e3
Andy Paicu <andypaicu@chromium.org>
2019-10-16 10:12:40 +0000

    Treat heads-up notification requests as ignored on tab switching
    Bug: 1014085
    Change-Id: Ibc831829014e235b964c62417d9b7c8d8467b7fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860030
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706376}

--- chrome/browser/ui/permission_bubble/mock_permission_prompt.h
+++ chrome/browser/ui/permission_bubble/mock_permission_prompt.h
@@ -21,7 +21,7 @@ class MockPermissionPrompt : public PermissionPrompt {
   // PermissionPrompt:
   void UpdateAnchorPosition() override;
   gfx::NativeWindow GetNativeWindow() override;
-  bool ShouldDestroyOnTabSwitching() override;
+  TabSwitchingBehavior GetTabSwitchingBehavior() override;
 
   bool IsVisible();
 
--- chrome/browser/ui/views/permission_bubble/permission_prompt_impl.cc
+++ chrome/browser/ui/views/permission_bubble/permission_prompt_impl.cc
@@ -261,8 +261,10 @@ gfx::NativeWindow PermissionPromptImpl::GetNativeWindow() {
   return nullptr;
 }
 
-bool PermissionPromptImpl::ShouldDestroyOnTabSwitching() {
-  return true;
+PermissionPrompt::TabSwitchingBehavior
+PermissionPromptImpl::GetTabSwitchingBehavior() {
+  return PermissionPrompt::TabSwitchingBehavior::
+      kDestroyPromptButKeepRequestPending;
 }
 
 void PermissionPromptImpl::Closing() {
