22c6799a51543daccd10e866cef1e580aac75c5b
Istiaque Ahmed <lazyboy@chromium.org>
2019-10-16 17:09:10 +0000

    Content Verification: Make computed hashes aware of dot/space suffix treatment.
    On windows, filename with (.| )+ suffix is ignored, i.e. "foo.html."
    and "foo.html" are treated the same.
    
    VerifiedContents is already aware of this and it stores a
    canonicalized version of filename for filenames containing (.| )+
    suffix.
    This CL makes ComputedHashes aware of the change too, so that searching
    for hashes will consider canonicalized version of the filename as
    candidate. This makes ComputedHashes::Reader and VerifiedContents treat
    this suffix behavior consistently.
    
    This CL also adds unittest and browsertest for the fix.
    
    Bug: 696208
    Test: See bug for test repro.
    Change-Id: I7c59add5c035815673259ef1f995c7b9ed01a75b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1772643
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706493}

---
+++ extensions/browser/content_verifier/content_verifier_utils.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef EXTENSIONS_BROWSER_CONTENT_VERIFIER_CONTENT_VERIFIER_UTILS_H_
+#define EXTENSIONS_BROWSER_CONTENT_VERIFIER_CONTENT_VERIFIER_UTILS_H_
+
+#include "base/files/file_path.h"
+#include "build/build_config.h"
+
+namespace extensions {
+namespace content_verifier_utils {
+
+#if defined(OS_WIN)
+// Returns true if |path| ends with (.| )+.
+// |out_path| will contain "." and/or " " suffix removed from |path|.
+bool TrimDotSpaceSuffix(const base::FilePath::StringType& path,
+                        base::FilePath::StringType* out_path);
+#endif  // defined(OS_WIN)
+
+}  // namespace content_verifier_utils
+}  // namespace extensions
+
+#endif  // EXTENSIONS_BROWSER_CONTENT_VERIFIER_CONTENT_VERIFIER_UTILS_H_
