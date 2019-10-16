aaae0f1f8b8e0c1cc2eaa0a2901fcd7e61eeead1
Takashi Toyoshima <toyoshim@chromium.org>
2019-10-16 04:59:06 +0000

    OOR-CORS: Add cors_fuzzer tests
    services/network/public/cpp/cors/cors.cc contains several test
    parsing code. These all are in an inner anonymous namespace, but
    can be accessible via IsCorsSafelistedHeader interface with some
    specific arguments.
    
    This patch adds fuzzing tests for the interface and call it
    with several header names so that it covers all parsing functions.
    
    Bug: 1007883
    Change-Id: I9440a34d57aedb265a4f09a3445d9edc55270d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1855627
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706309}

---
+++ services/network/public/cpp/cors/cors_fuzzer.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/network/public/cpp/cors/cors.h"
+
+#include <string>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  const std::string test_data(reinterpret_cast<const char*>(data), size);
+  network::cors::IsCorsSafelistedHeader("device-memory", test_data);
+  network::cors::IsCorsSafelistedHeader("width", test_data);
+  network::cors::IsCorsSafelistedHeader("content-type", test_data);
+  network::cors::IsCorsSafelistedHeader(test_data, test_data);
+  return 0;
+}
