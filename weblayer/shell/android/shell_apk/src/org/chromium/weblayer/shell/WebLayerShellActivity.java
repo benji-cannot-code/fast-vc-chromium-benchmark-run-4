4e3b7bcca9a97785a6690e8884a7ef050d2923c0
Pavel Shmakov <pshmakov@google.com>
2019-10-16 11:26:05 +0000

    Persist cookies for profiles with non-null path
    Currently the cookies are not persisted because the default
    implementation of ContentBrowserClient::CreateNetworkContext doesn't
    specify cookie_path parameter. In this CL CreateNetworkContext is
    overridden in WebLayer implementation.
    
    Bug: 1014048
    Change-Id: I1b7a307a64cd836eb5c4a45817412b997b89f1f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1862677
    Commit-Queue: Pavel Shmakov <pshmakov@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706390}

--- weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java
+++ weblayer/shell/android/shell_apk/src/org/chromium/weblayer/shell/WebLayerShellActivity.java
@@ -35,6 +35,7 @@ import org.chromium.weblayer.Profile;
 import org.chromium.weblayer.UnsupportedVersionException;
 import org.chromium.weblayer.WebLayer;
 
+import java.io.File;
 import java.util.List;
 
 /**
@@ -172,7 +173,8 @@ public class WebLayerShellActivity extends FragmentActivity {
             }
         }
 
-        BrowserFragment fragment = WebLayer.createBrowserFragment(null);
+        File profile = new File(getDataDir(), "defaultProfile");
+        BrowserFragment fragment = WebLayer.createBrowserFragment(profile.getPath());
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(mMainViewId, fragment);
 
