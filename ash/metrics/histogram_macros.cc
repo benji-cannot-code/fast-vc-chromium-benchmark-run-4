f7ddfeafdda2aec9e3ff80d062bbee5fb6dfd75a
Avery Musbach <amusbach@chromium.org>
2019-10-16 18:22:32 +0000

    metrics: Fix how histogram macros check for split view mode
    Bug: 970013
    Change-Id: Iebbbab09b4e1542136b79b730a53424c13ef8d05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864015
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706540}

--- ash/metrics/histogram_macros.cc
+++ ash/metrics/histogram_macros.cc
@@ -4,10 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "ash/metrics/histogram_macros.h"
+
 #include "ash/shell.h"
-#include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
-#include "ui/aura/window.h"
 
 namespace ash {
 
@@ -15,15 +14,6 @@ bool InTabletMode() {
   TabletModeController* tablet_mode_controller =
       Shell::Get()->tablet_mode_controller();
   return tablet_mode_controller && tablet_mode_controller->InTabletMode();
-}
-
-bool IsInSplitView() {
-  const aura::Window::Windows root_windows = Shell::GetAllRootWindows();
-  return std::any_of(
-      root_windows.cbegin(), root_windows.cend(),
-      [](const aura::Window* root_window) {
-        return SplitViewController::Get(root_window)->InSplitViewMode();
-      });
 }
 
 }  // namespace ash
--- ash/metrics/histogram_macros.h
+++ ash/metrics/histogram_macros.h
@@ -20,16 +20,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       UMA_HISTOGRAM_PERCENTAGE(name, __VA_ARGS__);    \
   } while (0)
 
-#define UMA_HISTOGRAM_PERCENTAGE_IN_SPLITVIEW(name, ...) \
-  do {                                                   \
-    if (ash::IsInSplitView())                            \
-      UMA_HISTOGRAM_PERCENTAGE(name, __VA_ARGS__);       \
+#define UMA_HISTOGRAM_PERCENTAGE_IN_SPLITVIEW(in_split_view, name, ...) \
+  do {                                                                  \
+    if (in_split_view)                                                  \
+      UMA_HISTOGRAM_PERCENTAGE(name, __VA_ARGS__);                      \
   } while (0)
 
-#define UMA_HISTOGRAM_PERCENTAGE_IN_TABLET_NON_SPLITVIEW(name, ...) \
-  do {                                                              \
-    if (ash::InTabletMode() && !ash::IsInSplitView())               \
-      UMA_HISTOGRAM_PERCENTAGE(name, __VA_ARGS__);                  \
+#define UMA_HISTOGRAM_PERCENTAGE_IN_TABLET_NON_SPLITVIEW(in_split_view, name, \
+                                                         ...)                 \
+  do {                                                                        \
+    if (ash::InTabletMode() && !in_split_view)                                \
+      UMA_HISTOGRAM_PERCENTAGE(name, __VA_ARGS__);                            \
   } while (0)
 
 #define UMA_HISTOGRAM_PERCENTAGE_IN_CLAMSHELL(name, ...) \
@@ -41,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 ASH_EXPORT bool InTabletMode();
-ASH_EXPORT bool IsInSplitView();
 
 }  // namespace ash
 
