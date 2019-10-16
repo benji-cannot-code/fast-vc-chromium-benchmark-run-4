aefc28d612d62cde5c92331a75968d6c6f7a74a2
Robert Liao <robliao@chromium.org>
2019-10-16 02:03:05 +0000

    Remove wrl/client.h include from views.h
    This isn't needed as of http://crrev.com/186382
    
    BUG=
    TBR=sky@chromium.org
    
    Change-Id: Ib68322494d89754f47e0bfa2e504827804d9586c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864091
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706275}

--- ui/views/view.h
+++ ui/views/view.h
@@ -50,10 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view_targeter.h"
 #include "ui/views/views_export.h"
 
-#if defined(OS_WIN)
-#include <wrl/client.h>
-#endif
-
 using ui::OSExchangeData;
 
 namespace gfx {
