0b99ad2bd4dd1d3b2ca3e8eedc18a3ce9f658a76
James Darpinian <jdarpinian@chromium.org>
2019-10-16 01:21:15 +0000

    Extend refresh rate fix to ANGLE also
    https://crrev.com/c/1812241 reads the monitor refresh rate with xrandr
    when we're using desktop GL on Linux Nvidia. This applies the same fix
    when we're using ANGLE (--use-cmd-decoder=passthrough).
    
    Also fixes X11 errors when the primary display is disconnected.
    
    Bug: 1012262, 535392
    Change-Id: I0c54371496aa141643c5f6a2950935e22ba0f534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857091
    Commit-Queue: James Darpinian <jdarpinian@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706259}

--- ui/base/x/x11_display_util.h
+++ ui/base/x/x11_display_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_BASE_X_X11_DISPLAY_UTIL_H_
 
 #include "base/component_export.h"
+#include "base/time/time.h"
 #include "ui/display/display.h"
 #include "ui/gfx/x/x11.h"
 #include "ui/gfx/x/x11_types.h"
@@ -30,11 +31,10 @@ std::vector<display::Display> BuildDisplaysFromXRandRInfo(
     float scale,
     int64_t* primary_display_index_out);
 
-// Calculates the refresh rate of an xrandr mode.
+// Returns the refresh interval of the primary display. If there is no connected
+// primary display, returns the refresh interval of the first connected display.
 COMPONENT_EXPORT(UI_BASE_X)
-float GetRefreshRateFromXRRModeInfo(XRRModeInfo* modes,
-                                    int num_of_mode,
-                                    RRMode current_mode_id);
+base::TimeDelta GetPrimaryDisplayRefreshIntervalFromXrandr(Display* display);
 
 }  // namespace ui
 
--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/system/sys_info.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "ui/events/platform/platform_event_source.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/sync_control_vsync_provider.h"
 
 #if defined(USE_X11)
+#include "ui/base/x/x11_display_util.h"
 #include "ui/base/x/x11_util_internal.h"  // nogncheck
 #include "ui/gfx/x/x11.h"
 #endif
@@ -199,6 +201,37 @@ struct TraceSwapEventsInitializer {
 
 static base::LazyInstance<TraceSwapEventsInitializer>::Leaky
     g_trace_swap_enabled = LAZY_INSTANCE_INITIALIZER;
+
+#if defined(USE_X11)
+class XrandrIntervalOnlyVSyncProvider : public gfx::VSyncProvider {
+ public:
+  XrandrIntervalOnlyVSyncProvider(Display* display)
+      : display_(display), interval_(base::TimeDelta::FromSeconds(1 / 60.)) {}
+
+  void GetVSyncParameters(UpdateVSyncCallback callback) override {
+    if (++calls_since_last_update_ >= kCallsBetweenUpdates) {
+      calls_since_last_update_ = 0;
+      interval_ = ui::GetPrimaryDisplayRefreshIntervalFromXrandr(display_);
+    }
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE,
+        base::BindOnce(std::move(callback), base::TimeTicks(), interval_));
+  }
+
+  bool GetVSyncParametersIfAvailable(base::TimeTicks* timebase,
+                                     base::TimeDelta* interval) override {
+    return false;
+  }
+  bool SupportGetVSyncParametersIfAvailable() const override { return false; }
+  bool IsHWClock() const override { return false; }
+
+ private:
+  Display* const display_ = nullptr;
+  base::TimeDelta interval_;
+  static const int kCallsBetweenUpdates = 100;
+  int calls_since_last_update_ = kCallsBetweenUpdates;
+};
+#endif
 
 class EGLSyncControlVSyncProvider : public SyncControlVSyncProvider {
  public:
@@ -1205,6 +1238,21 @@ bool NativeViewGLSurfaceEGL::Initialize(GLSurfaceFormat format) {
     return false;
   }
 
+  if (g_driver_egl.ext.b_EGL_NV_post_sub_buffer) {
+    EGLint surfaceVal;
+    EGLBoolean retVal = eglQuerySurface(
+        GetDisplay(), surface_, EGL_POST_SUB_BUFFER_SUPPORTED_NV, &surfaceVal);
+    supports_post_sub_buffer_ = (surfaceVal && retVal) == EGL_TRUE;
+  }
+
+  supports_swap_buffer_with_damage_ =
+      g_driver_egl.ext.b_EGL_KHR_swap_buffers_with_damage;
+
+  if (!vsync_provider_external_ && EGLSyncControlVSyncProvider::IsSupported()) {
+    vsync_provider_internal_ =
+        std::make_unique<EGLSyncControlVSyncProvider>(surface_);
+  }
+
 #if defined(USE_X11)
   // Query all child windows and store them. ANGLE creates a child window when
   // eglCreateWidnowSurface is called on X11 and expose events from this window
@@ -1227,22 +1275,13 @@ bool NativeViewGLSurfaceEGL::Initialize(GLSurfaceFormat format) {
   if (PlatformEventSource* source = PlatformEventSource::GetInstance()) {
     source->AddPlatformEventDispatcher(this);
   }
+
+  if (!vsync_provider_external_ && !vsync_provider_internal_) {
+    vsync_provider_internal_ =
+        std::make_unique<XrandrIntervalOnlyVSyncProvider>(x11_display);
+  }
 #endif
 
-  if (g_driver_egl.ext.b_EGL_NV_post_sub_buffer) {
-    EGLint surfaceVal;
-    EGLBoolean retVal = eglQuerySurface(
-        GetDisplay(), surface_, EGL_POST_SUB_BUFFER_SUPPORTED_NV, &surfaceVal);
-    supports_post_sub_buffer_ = (surfaceVal && retVal) == EGL_TRUE;
-  }
-
-  supports_swap_buffer_with_damage_ =
-      g_driver_egl.ext.b_EGL_KHR_swap_buffers_with_damage;
-
-  if (!vsync_provider_external_ && EGLSyncControlVSyncProvider::IsSupported()) {
-    vsync_provider_internal_ =
-        std::make_unique<EGLSyncControlVSyncProvider>(surface_);
-  }
   presentation_helper_ =
       std::make_unique<GLSurfacePresentationHelper>(GetVSyncProvider());
   return true;
