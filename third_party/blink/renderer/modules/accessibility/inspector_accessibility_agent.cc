8549c87f0d74ae6ce44150ef0f0fec55e8595e13
Lan Wei <lanwei@chromium.org>
2019-10-17 17:12:43 +0000

    Revert "DL: Fix flaky getFullAXTree & wrongly updated accessibility tests"
    This reverts commit 2000b5b18d3bc0b332feaf9f34d25b033bf3df90.
    
    Reason for revert: <INSERT REASONING HERE>
    inspector-protocol/accessibility/accessibility-getFullAXTree-display-locked.js is keeping failing on WebKit Linux MSAN.
    
    Original change's description:
    > DL: Fix flaky getFullAXTree & wrongly updated accessibility tests
    > 
    > A devtoools display locking a11y test was marked as failing in MSAN bots
    > and is also flaky when tested locally, this CL updates that test and
    > fixes the cause of the failure (getFullAXTree might be called when
    > style/layout is dirty, which caused failing DCHECKs).
    > 
    > Some other display locking a11y tests were not updated correctly, and is
    > updated in this CL as well.
    > 
    > Bug: 996625
    > Change-Id: I32eac1385c54eb07b1e6dab800620a2485158437
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846618
    > Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: vmpstr <vmpstr@chromium.org>
    > Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#706834}
    
    TBR=vmpstr@chromium.org,aboxhall@chromium.org,rakina@chromium.org
    
    Change-Id: I11a6bbc935ec0b88d9f72af37cc729cc28257766
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 996625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866620
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706969}

--- third_party/blink/renderer/modules/accessibility/inspector_accessibility_agent.cc
+++ third_party/blink/renderer/modules/accessibility/inspector_accessibility_agent.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
 #include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/dom/node_list.h"
-#include "third_party/blink/renderer/core/dom/text.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/inspector/identifiers_factory.h"
@@ -726,8 +725,6 @@ Response InspectorAccessibilityAgent::getFullAXTree(
   Document* document = inspected_frames_->Root()->GetDocument();
   if (!document)
     return Response::Error("No document.");
-  if (document->View()->NeedsLayout() || document->NeedsLayoutTreeUpdate())
-    document->UpdateStyleAndLayout();
   *nodes = std::make_unique<protocol::Array<protocol::Accessibility::AXNode>>();
   AXContext ax_context(*document);
   AXObjectCacheImpl& cache = ToAXObjectCacheImpl(ax_context.GetAXObjectCache());
--- third_party/blink/web_tests/inspector-protocol/accessibility/accessibility-getFullAXTree-display-locked.js
+++ third_party/blink/web_tests/inspector-protocol/accessibility/accessibility-getFullAXTree-display-locked.js
@@ -1,18 +1,21 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
   var {page, session, dp} = await testRunner.startHTML(`
-    <div id='activatable' rendersubtree='invisible skip-viewport-activation'>
+    <div id='activatable' rendersubtree='invisible-activatable'>
       locked
       <div id='child'>
         child
         <div id='grandChild'>grandChild</div>
       </div>
       <div id='invisible' style='display:none'>invisible</div>
-      <div id='nested' rendersubtree='invisible skip-viewport-activation'>nested</div>
+      <div id='nested' rendersubtree='invisible-activatable'>nested</div>
       text
     </div>
-    <div id='nonActivatable' rendersubtree='invisible skip-activation'>nonActivatable text</div>
+    <div id='nonActivatable' rendersubtree='invisible'>nonActivatable text</div>
     <div id='normal'>normal text</div>
+    <script>
+      activatable.getBoundingClientRect(); // Force layout
+    </script>
   `, 'Tests accessibility values of display locked nodes');
   const dumpAccessibilityNodesFromList =
       (await testRunner.loadScript('../resources/accessibility-dumpAccessibilityNodesFromList.js'))(testRunner, session);
