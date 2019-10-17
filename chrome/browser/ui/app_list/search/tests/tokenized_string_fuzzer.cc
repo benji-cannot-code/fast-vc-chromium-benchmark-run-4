ef547ba16a45a8353deb1986ac3838df91962261
Thanh Nguyen <thanhdng@chromium.org>
2019-10-17 00:26:03 +0000

    Add fuzzer for TokenizedString
    This CL adds a fuzzer for TokenizedString.
    
    Bug: 1014837
    Change-Id: Ia36339e59adc386800b19dd3d2245dff9ce629f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864500
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Abhishek Arya <inferno@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706717}

---
+++ chrome/browser/ui/app_list/search/tests/tokenized_string_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/app_list/tokenized_string.h"
+#include "base/strings/string16.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  if (size < 1 || size % 2 != 0)
+    return 0;
+
+  // Test for base::string16 if size is even.
+  base::string16 string_input16(reinterpret_cast<const base::char16*>(data),
+                                size / 2);
+  ash::TokenizedString tokenized_string_from_string16(string_input16);
+  return 0;
+}
