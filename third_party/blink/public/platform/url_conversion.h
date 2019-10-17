3e3153b904c9292ba2175f341cd568bcee269892
Min Qin <qinmin@chromium.org>
2019-10-17 23:12:51 +0000

    Make data URL to be downloaded via blob
    In Chrome, URL size is limited to 2MB when serializing the IPC. As a
    result, downloading a large data URL will fail.
    This CL fixes the issue by creating a blob to hold the data and pass
    it across process boundaries.
    It also changes the implementation of RenderFrameImpl::SaveImageFromDataURL()
    to also use a blob instead of passing a large std::string in IPC.
    
    BUG=947395
    
    Change-Id: I21779af402bb8cf4fbfa7cf73acd8cac594d0c2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1832723
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707151}

--- third_party/blink/public/platform/url_conversion.h
+++ third_party/blink/public/platform/url_conversion.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_PLATFORM_URL_CONVERSION_H_
 #define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_URL_CONVERSION_H_
 
+#include "mojo/public/cpp/system/message_pipe.h"
 #include "third_party/blink/public/platform/web_common.h"
 
 class GURL;
@@ -15,6 +16,11 @@ namespace blink {
 class WebString;
 
 BLINK_PLATFORM_EXPORT GURL WebStringToGURL(const WebString&);
+
+// Convert a data url to to message pipe handle so that it can be passed across
+// processes.
+BLINK_PLATFORM_EXPORT mojo::ScopedMessagePipeHandle DataURLToMessagePipeHandle(
+    const WebString&);
 
 }  // namespace blink
 
