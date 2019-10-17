0070e6ca35b336cb67c34df07850af855c209a26
Christopher Cameron <ccameron@chromium.org>
2019-10-17 21:05:06 +0000

    Add null check in -[NativeWidgetMacNSWindow saveRestorableState]
    TBR=sdy
    
    Bug: 1013250
    Change-Id: I9090602c6af0d01b971b6a9764e21d9c1e50987f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864319
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707091}

--- components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
+++ components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
@@ -320,6 +320,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)saveRestorableState {
+  if (!bridge_)
+    return;
   if (![self _isConsideredOpenForPersistentState])
     return;
   base::scoped_nsobject<NSMutableData> restorableStateData(
