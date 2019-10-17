4b0730da6dcf6aed295633e85aa272ba52b875ff
Maksim Moskvitin <mmoskvitin@google.com>
2019-10-17 20:19:46 +0000

    Support keystore key rotation
    Keystore key rotation is triggered by Sync server. In this case client
    receives new keystore keys together with old NigoriSpecifics.
    
    After keystore key rotation NigoriSpecifics should contain last
    keystore key and its keybag should be encrypted with this key. Because
    syncing with rotated keystore NigoriSpecifics is already supported,
    the only thing we need to do is to implement NigoriState transition:
    last keystore key should be added into |cryptographer| and used as
    the default encryption key.
    
    Additionally, we trigger keystore key rotation inside
    NigoriSyncBridgeImpl ctor, because it might be not completed in between
    of SetKeystoreKeys() call and browser restart.
    
    Bug: 922900
    Change-Id: Ie67a922edf3f573fb6c7c768df789138804d5012
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863936
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#707044}

--- components/sync/nigori/pending_local_nigori_commit.h
+++ components/sync/nigori/pending_local_nigori_commit.h
@@ -27,6 +27,8 @@ class PendingLocalNigoriCommit {
 
   static std::unique_ptr<PendingLocalNigoriCommit> ForKeystoreInitialization();
 
+  static std::unique_ptr<PendingLocalNigoriCommit> ForKeystoreKeyRotation();
+
   PendingLocalNigoriCommit() = default;
   virtual ~PendingLocalNigoriCommit() = default;
 
