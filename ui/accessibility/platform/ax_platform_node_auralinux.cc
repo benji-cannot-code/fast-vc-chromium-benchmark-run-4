cc5d4132ce9c99460d4343d7880533e7277dafb9
Joanmarie Diggs <jdiggs@igalia.com>
2019-10-17 07:43:23 +0000

    Update ATK mappings for ins and del elements
    Make the following mapping changes:
    * ins should be ATK_ROLE_CONTENT_INSERTION (if ATK version supports it)
    * del should be ATK_ROLE_CONTENT_DELETION (if ATK version supports it)
    
    Bug: 984948
    Change-Id: I62513c0a9b3af486acc54d642e40834582b1acb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1706581
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Martin Robinson <mrobinson@igalia.com>
    Commit-Queue: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#706818}

--- ui/accessibility/platform/ax_platform_node_auralinux.cc
+++ ui/accessibility/platform/ax_platform_node_auralinux.cc
@@ -62,6 +62,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ATK_230
 #endif
 
+#if defined(ATK_CHECK_VERSION) && ATK_CHECK_VERSION(2, 34, 0)
+#define ATK_234
+#endif
+
 namespace ui {
 
 namespace {
@@ -144,6 +148,14 @@ constexpr AtkRole kSuperscriptRole = ATK_ROLE_TEXT;
 constexpr AtkRole kAtkFootnoteRole = ATK_ROLE_FOOTNOTE;
 #else
 constexpr AtkRole kAtkFootnoteRole = ATK_ROLE_LIST_ITEM;
+#endif
+
+#if defined(ATK_234)
+constexpr AtkRole kAtkRoleContentDeletion = ATK_ROLE_CONTENT_DELETION;
+constexpr AtkRole kAtkRoleContentInsertion = ATK_ROLE_CONTENT_INSERTION;
+#else
+constexpr AtkRole kAtkRoleContentDeletion = ATK_ROLE_SECTION;
+constexpr AtkRole kAtkRoleContentInsertion = ATK_ROLE_SECTION;
 #endif
 
 AXPlatformNodeAuraLinux* AtkObjectToAXPlatformNodeAuraLinux(
@@ -2342,9 +2354,9 @@ AtkRole AXPlatformNodeAuraLinux::GetAtkRole() const {
     case ax::mojom::Role::kComplementary:
       return ATK_ROLE_LANDMARK;
     case ax::mojom::Role::kContentDeletion:
+      return kAtkRoleContentDeletion;
     case ax::mojom::Role::kContentInsertion:
-      // TODO(accessibility) https://github.com/w3c/html-aam/issues/141
-      return ATK_ROLE_SECTION;
+      return kAtkRoleContentInsertion;
     case ax::mojom::Role::kContentInfo:
     case ax::mojom::Role::kFooter:
       return ATK_ROLE_LANDMARK;
