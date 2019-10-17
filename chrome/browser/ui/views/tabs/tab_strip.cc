e37dff1512a0f29f105a10fb1ba7db1ff7908539
Joel Hockey <joelhockey@chromium.org>
2019-10-17 02:21:56 +0000

    Disallow tabs moving between browsers of different types
    Currently we disallow dropping tabs onto a browser
    that does not support FEATURE_TABSTRIP.  This adds
    an extra check to also disallow if the source
    and target browser are not the same type
    (APP vs NORMAL).
    
    This CL includes some refactoring of the logic to check
    whether a tab can attach to a new target.
    
    * New function CanAttachTo replaces GetContextForWindow
      and ShouldDisallowDrag.
    
    * Removed TabDragContext.IsCompatibleWith and inlined the
      profile checking into CanAttachTo.
    
    * OnWidgetBoundsChanged no longer calls GetContextForWindow
      which was previously doing a number of pointless checks
      to see if the current attached context could drag to
      itself.  Rather this codesite uses attached_context_ directly.
      From what I can tell, the widget change observer happens
      on move_loop_widget_ which is attached_context_.
    
    * GetTargetTabStripForPoint was previously doubling up on the
      checks in GetContextForWindow as this function was being
      called by ShouldDisallowDrag, then again immediately after
      to get destination_tab_strip_.  Now these checks happen only
      once in CanAttachTo.
    
    * BringWindowUnderPointToFront now calls CanAttachTo which
      has the additional modal dialog check from ShouldDisallowDrag.
    
    Bug: 1012169
    Change-Id: Iab0a09a1b37024b8611798822c94edd9c6820daf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1862884
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706756}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -445,12 +445,6 @@ class TabStrip::TabDragContextImpl : public TabDragContext {
     return drag_controller_.release();
   }
 
-  bool IsCompatibleWith(TabDragContext* other) const override {
-    return static_cast<TabDragContextImpl*>(other)
-               ->tab_strip_->controller()
-               ->GetProfile() == tab_strip_->controller()->GetProfile();
-  }
-
   bool IsDragSessionActive() const override {
     return drag_controller_ != nullptr;
   }
