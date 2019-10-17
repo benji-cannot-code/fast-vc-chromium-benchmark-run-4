7da7e138ffddb487ef055792769d0c7777555f2f
Yulun Wu <yulunwu@chromium.org>
2019-10-17 23:06:45 +0000

    Menu Model: fix bug where menu item title was greyed out because it is disabled.
    The old approach of adding title as a disabled item meant that this CL:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1788198
    
    causes the menu title to be displayed in grey text instead of solid black text. This CL introduces a new menu type that, while still disabled and non-reactive, be displayed in solid black text.
    
    Bug: 1006203
    Change-Id: I9fa38132160228ebde5bd43d8db9c2f60d5f001c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1841583
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707147}

--- ui/views/controls/menu/menu_model_adapter_unittest.cc
+++ ui/views/controls/menu/menu_model_adapter_unittest.cc
@@ -214,6 +214,9 @@ void CheckSubmenu(const RootModel& model,
 
     // Check type.
     switch (model_item.type) {
+      case ui::MenuModel::TYPE_TITLE:
+        EXPECT_EQ(views::MenuItemView::TITLE, item->GetType());
+        break;
       case ui::MenuModel::TYPE_COMMAND:
         EXPECT_EQ(views::MenuItemView::NORMAL, item->GetType());
         break;
@@ -288,6 +291,9 @@ TEST_F(MenuModelAdapterTest, BasicTest) {
 
     // Check type.
     switch (model_item.type) {
+      case ui::MenuModel::TYPE_TITLE:
+        EXPECT_EQ(views::MenuItemView::TITLE, item->GetType());
+        break;
       case ui::MenuModel::TYPE_COMMAND:
         EXPECT_EQ(views::MenuItemView::NORMAL, item->GetType());
         break;
