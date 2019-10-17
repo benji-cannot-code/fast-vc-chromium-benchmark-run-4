5019b51ee759e401e5d46f1c4eb56d0cacbf4233
Joe DeBlasio <jdeblasio@chromium.org>
2019-10-17 15:30:51 +0000

    Allow disabling of topsite lookalikes in Safety Tips.
    This CL allows us to disable topsites-based lookalike matching in Safety
    Tips via a "disable_topsites" feature parameter.  This feature param is
    inverted from the other feature params (it disables instead of enables)
    to ensure that the default no-param state is consistent before and after
    this change.
    
    Bug: 1015003
    Change-Id: I2e7e94622dd20b8879e8157d588d39cf9383e790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865415
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706927}

--- chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view_browsertest.cc
@@ -56,7 +56,7 @@ namespace {
 enum class UIStatus {
   kDisabled,
   kEnabled,
-  kEnabledWithEditDistance,
+  kEnabledWithAllFeatures,
 };
 
 // An engagement score above MEDIUM.
@@ -171,10 +171,11 @@ class SafetyTipPageInfoBubbleViewBrowserTest
               {{"topsites", "true"}}}},
             {});
         break;
-      case UIStatus::kEnabledWithEditDistance:
+      case UIStatus::kEnabledWithAllFeatures:
         feature_list_.InitWithFeaturesAndParameters(
             {{security_state::features::kSafetyTipUI,
-              {{"editdistance", "true"},
+              {{"topsites", "true"},
+               {"editdistance", "true"},
                {"editdistance_siteengagement", "true"}}},
              {features::kLookalikeUrlNavigationSuggestionsUI,
               {{"topsites", "true"}}}},
@@ -227,6 +228,11 @@ class SafetyTipPageInfoBubbleViewBrowserTest
     return ui_status() == UIStatus::kDisabled ? true : IsUIShowing();
   }
 
+  bool IsUIShowingOnlyIfFeaturesEnabled() {
+    return ui_status() == UIStatus::kEnabledWithAllFeatures ? IsUIShowing()
+                                                            : !IsUIShowing();
+  }
+
   void CheckPageInfoShowsSafetyTipInfo(Browser* browser) {
     if (ui_status() == UIStatus::kDisabled) {
       return;
@@ -259,7 +265,7 @@ INSTANTIATE_TEST_SUITE_P(,
                          SafetyTipPageInfoBubbleViewBrowserTest,
                          ::testing::Values(UIStatus::kDisabled,
                                            UIStatus::kEnabled,
-                                           UIStatus::kEnabledWithEditDistance));
+                                           UIStatus::kEnabledWithAllFeatures));
 
 // Ensure normal sites with low engagement are not blocked.
 IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
@@ -454,7 +460,7 @@ IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
   const GURL kNavigatedUrl = GetURL("googlé.sk");
   SetEngagementScore(browser(), kNavigatedUrl, kLowEngagement);
   NavigateToURL(browser(), kNavigatedUrl, WindowOpenDisposition::CURRENT_TAB);
-  EXPECT_TRUE(IsUIShowingIfEnabled());
+  EXPECT_TRUE(IsUIShowingOnlyIfFeaturesEnabled());
   ASSERT_NO_FATAL_FAILURE(CheckPageInfoDoesNotShowSafetyTipInfo(browser()));
 }
 
@@ -468,7 +474,7 @@ IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
   // Ensure a Safety Tip is triggered initially...
   SetEngagementScore(browser(), kNavigatedUrl, kLowEngagement);
   NavigateToURL(browser(), kNavigatedUrl, WindowOpenDisposition::CURRENT_TAB);
-  EXPECT_TRUE(IsUIShowingIfEnabled());
+  EXPECT_TRUE(IsUIShowingOnlyIfFeaturesEnabled());
 
   // ...but suppressed by the allowlist.
   SetSafetyTipAllowlistPatterns({"xn--googl-fsa.sk/"});
@@ -485,7 +491,7 @@ IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
   const GURL kNavigatedUrl = GetURL("goooglé.com");
   SetEngagementScore(browser(), kNavigatedUrl, kLowEngagement);
   NavigateToURL(browser(), kNavigatedUrl, WindowOpenDisposition::CURRENT_TAB);
-  EXPECT_EQ(IsUIShowing(), ui_status() == UIStatus::kEnabledWithEditDistance);
+  EXPECT_EQ(IsUIShowing(), ui_status() == UIStatus::kEnabledWithAllFeatures);
 }
 
 // Tests that the SafetyTipShown histogram triggers correctly.
@@ -509,8 +515,9 @@ IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
   const GURL kLookalikeUrl = GetURL("googlé.sk");
   SetEngagementScore(browser(), kLookalikeUrl, kLowEngagement);
   NavigateToURL(browser(), kLookalikeUrl, WindowOpenDisposition::CURRENT_TAB);
-  histograms.ExpectBucketCount(kHistogramName,
-                               security_state::SafetyTipStatus::kLookalike, 1);
+  histograms.ExpectBucketCount(
+      kHistogramName, security_state::SafetyTipStatus::kLookalike,
+      ui_status() == UIStatus::kEnabledWithAllFeatures ? 1 : 0);
   histograms.ExpectTotalCount(kHistogramName, 3);
 }
 
@@ -537,7 +544,7 @@ IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
 
   // These histograms are only recorded when the UI feature is enabled, so bail
   // out when disabled.
-  if (ui_status() != UIStatus::kEnabledWithEditDistance) {
+  if (ui_status() != UIStatus::kEnabledWithAllFeatures) {
     return;
   }
 
