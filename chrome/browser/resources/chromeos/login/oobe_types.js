5459f8729cd37303fdc89fc6bc247adc9199f7b6
Denis Kuznetsov <antrim@google.com>
2019-10-17 20:25:25 +0000

    Fix calculation of inner padding of OOBE dialog depending on outer margins
    Bug: 1007294
    Change-Id: I99b7b3cc46e12c0fb060f76e086f5f31bd4cb812
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865216
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707055}

--- chrome/browser/resources/chromeos/login/oobe_types.js
+++ chrome/browser/resources/chromeos/login/oobe_types.js
@@ -135,3 +135,13 @@ OobeTypes.SecurityTokenPinDialogErrorType = {
  * }}
  */
 OobeTypes.SecurityTokenPinDialogParameters;
+
+/**
+ * Specifies the mechanism for calculating oobe-dialog inner padding.
+ * @enum {string}
+ */
+OobeTypes.DialogPaddingMode = {
+  AUTO: 'auto',
+  NARROW: 'narrow',
+  WIDE: 'wide',
+};
