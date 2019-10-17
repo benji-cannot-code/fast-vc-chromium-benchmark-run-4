e6567e88554ac74d34dc2a82236632d3310d8335
Kehuang Li <kehuangli@chromium.org>
2019-10-17 21:26:40 +0000

    [Chromecast] Wait for MixerServiceWrapper::Stop
    According to audio_io interface, AudioOutputStream::Stop should be
    implemented in a synchronized way that no more access to source callback
    will happen after Stop. However, MixerServiceWrapper::Stop is posted to
    another thread, and complete asynchronizely, which potentially causes
    crash, as upstream may destroyed source callback before
    MixerServiceWrapper actually stops. HoweverAdd a waitable event for
    MixerServiceWrapper as we did for CmaWrapper.
    
    Bug: internal: 142355421
    Test: On device and unittests.
    Change-Id: Ia545596fbb80528d4f92debe3229b64fdea22667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866294
    Commit-Queue: Kehuang Li <kehuangli@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707104}

--- chromecast/media/audio/cast_audio_output_stream.cc
+++ chromecast/media/audio/cast_audio_output_stream.cc
@@ -102,7 +102,7 @@ class CastAudioOutputStream::MixerServiceWrapper
 
   void SetRunning(bool running);
   void Start(AudioSourceCallback* source_callback);
-  void Stop();
+  void Stop(base::WaitableEvent* finished);
   void Close(base::OnceClosure closure);
   void SetVolume(double volume);
   void Flush();
@@ -189,11 +189,14 @@ void CastAudioOutputStream::MixerServiceWrapper::Start(
   mixer_connection_->SetVolumeMultiplier(volume_);
 }
 
-void CastAudioOutputStream::MixerServiceWrapper::Stop() {
+void CastAudioOutputStream::MixerServiceWrapper::Stop(
+    base::WaitableEvent* finished) {
   DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
   mixer_connection_.reset();
-
   source_callback_ = nullptr;
+  if (finished) {
+    finished->Signal();
+  }
 }
 
 void CastAudioOutputStream::MixerServiceWrapper::Flush() {
@@ -205,7 +208,7 @@ void CastAudioOutputStream::MixerServiceWrapper::Flush() {
 void CastAudioOutputStream::MixerServiceWrapper::Close(
     base::OnceClosure closure) {
   DCHECK_CALLED_ON_VALID_THREAD(io_thread_checker_);
-  Stop();
+  Stop(nullptr);
   std::move(closure).Run();
 }
 
@@ -410,13 +413,15 @@ void CastAudioOutputStream::Stop() {
   // |cma_wrapper_| and |mixer_service_wrapper_| cannot be both active.
   DCHECK(!(cma_wrapper_ && mixer_service_wrapper_));
 
+  base::WaitableEvent finished;
   if (cma_wrapper_) {
-    base::WaitableEvent stopFinished;
-    POST_TO_CMA_WRAPPER(Stop, base::Unretained(&stopFinished));
-    stopFinished.Wait();
+    POST_TO_CMA_WRAPPER(Stop, &finished);
   } else if (mixer_service_wrapper_) {
-    POST_TO_MIXER_SERVICE_WRAPPER(Stop);
+    POST_TO_MIXER_SERVICE_WRAPPER(Stop, &finished);
+  } else {
+    finished.Signal();
   }
+  finished.Wait();
 }
 
 void CastAudioOutputStream::Flush() {
