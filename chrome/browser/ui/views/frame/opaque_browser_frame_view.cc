9683b074d173a3649129250a3f40096870551b7b
Dana Fried <dfried@chromium.org>
2019-10-17 18:35:41 +0000

    Remove superfluous hairline.
    This eliminates a hairline that had been drawn in some PWA windows but
    which had - for reasons that are unclear - not actually been rendered in
    most cases. With the new 1dip rendering code, the line WAS rendered in
    some cases, which caused a visual regression.
    
    Bug: 1012979
    Change-Id: I3cebc47cfde1b2e1205de8632fd92a8e43181a29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866277
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Reviewed-by: Charlene Yan <cyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707006}

--- chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
+++ chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
@@ -445,7 +445,7 @@ bool OpaqueBrowserFrameView::IsTabStripVisible() const {
 
 bool OpaqueBrowserFrameView::IsToolbarVisible() const {
   return browser_view()->IsToolbarVisible() &&
-      !browser_view()->toolbar()->GetPreferredSize().IsEmpty();
+         !browser_view()->toolbar()->GetPreferredSize().IsEmpty();
 }
 
 int OpaqueBrowserFrameView::GetTabStripHeight() const {
@@ -705,8 +705,8 @@ void OpaqueBrowserFrameView::PaintRestoredFrameBorder(
 
 void OpaqueBrowserFrameView::PaintMaximizedFrameBorder(
     gfx::Canvas* canvas) const {
-  frame_background_->set_maximized_top_inset(
-      GetTopInset(true) - GetTopInset(false));
+  frame_background_->set_maximized_top_inset(GetTopInset(true) -
+                                             GetTopInset(false));
   frame_background_->PaintMaximized(canvas, this);
 }
 
@@ -714,18 +714,12 @@ void OpaqueBrowserFrameView::PaintClientEdge(gfx::Canvas* canvas) const {
   const bool tabstrip_visible = browser_view()->IsTabStripVisible();
   const gfx::Rect client_bounds =
       layout_->CalculateClientAreaBounds(width(), height());
-  int y = client_bounds.y();
-  // If the toolbar isn't going to draw a top edge for us, draw one ourselves.
-  if (!tabstrip_visible) {
-    canvas->DrawLine(gfx::Point(client_bounds.x(), client_bounds.y() - 1),
-                     gfx::Point(client_bounds.right(), client_bounds.y() - 1),
-                     GetToolbarTopSeparatorColor());
-  }
 
   // In maximized mode, the only edge to draw is the top one, so we're done.
   if (IsFrameCondensed())
     return;
 
+  int y = client_bounds.y();
   const gfx::Rect toolbar_bounds = browser_view()->toolbar()->bounds();
   if (tabstrip_visible) {
     // The client edges start at the top of the toolbar.
