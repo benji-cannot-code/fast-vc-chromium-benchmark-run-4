7f6012272610d639b2041c648f87c2af5d148d2d
Rob Schonberger <robsc@chromium.org>
2019-10-17 01:41:13 +0000

    Add 1 util method, test it. Add a Model abstract class with inline docs.
    This CL could be split in two.
    
    First, adds a method to Stroke for the biggest touch-major of that
    stroke, and the corresponding unit test.
    
    More importantly: Adds an abstract class for what a checked in model
    looks like. All the configuration is accessible from the model when
    instantiated, and no implementation is added. All the fields in
    configuration are documented, and an explicit constructor for the
    configuration is added since this counts as a complex struct.
    
    Bug: 1009290
    Change-Id: I7eeca918540e80a34133230c6d0a67c6b58c2af5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864490
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706743}

---
+++ ui/events/ozone/evdev/touch_filter/neural_stylus_palm_detection_filter_model.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/events/ozone/evdev/touch_filter/neural_stylus_palm_detection_filter_model.h"
+
+namespace ui {
+
+NeuralStylusPalmDetectionFilterModelConfig::
+    NeuralStylusPalmDetectionFilterModelConfig() = default;
+
+NeuralStylusPalmDetectionFilterModelConfig::
+    NeuralStylusPalmDetectionFilterModelConfig(
+        const NeuralStylusPalmDetectionFilterModelConfig& other) = default;
+
+}  // namespace ui
