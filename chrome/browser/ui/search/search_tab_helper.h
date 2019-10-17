582ffe84cd53d345fea6782b13e74fe1af5cd7aa
Dan Beam <dbeam@chromium.org>
2019-10-17 21:06:12 +0000

    Local NTP, Realbox: show confirmation dialog to remove suggestions
    Fixed: 1013837
    Change-Id: I468696fd398714693609fe0aacb45425fc49361d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857606
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707093}

--- chrome/browser/ui/search/search_tab_helper.h
+++ chrome/browser/ui/search/search_tab_helper.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -172,6 +173,14 @@ class SearchTabHelper : public content::WebContentsObserver,
   // active tab is in mode SEARCH_SUGGESTIONS.
   bool IsInputInProgress() const;
 
+  // Called when a user confirms deleting an autocomplete match. Note: might be
+  // called synchronously with accepted = true if this feature is disabled
+  // (which defaults the behavior to silent deletions).
+  void OnDeleteAutocompleteMatchConfirm(
+      uint8_t line,
+      chrome::mojom::EmbeddedSearch::DeleteAutocompleteMatchCallback callback,
+      bool accepted);
+
   content::WebContents* web_contents_;
 
   SearchIPCRouter ipc_router_;
@@ -191,6 +200,8 @@ class SearchTabHelper : public content::WebContentsObserver,
       query_autocomplete_callback_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
+
+  base::WeakPtrFactory<SearchTabHelper> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(SearchTabHelper);
 };
