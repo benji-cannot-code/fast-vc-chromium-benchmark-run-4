85472e580f6d76bb0ed0b0e0e1576703878cc28a
Daniel Libby <dlibby@microsoft.com>
2019-10-17 17:18:54 +0000

    Remove no-op paint layer flag and compositing state query
    First in a series of changes to get rid of the
    DisableCompositingQueryAsserts in PaintLayerPainter::Paint.
    
    kPaintLayerUncachedClipRects is only used to determine whether a paint
    layer should create a subsequence (for potential benefits of caching/
    reusing the subsequence). However, it is currently only set if
    kGlobalPaintFlattenCompositingLayers is set (either manually in
    data_transfer.cc or via the code in PaintLayerPainter::Paint).
    
    If that global flag is set ShouldCreateSubsequence always returns false,
    so the kPaintLayerUncachedClipRects flag is unnecessary. Removing this
    code gets rid of 1 out of 3 compositing queries in this codepath.
    
    Bug: 1007989
    Change-Id: I59cddc51a65c9acd8e134845d7c9c4b6320fc4d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864414
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#706973}

--- third_party/blink/renderer/core/paint/paint_layer_painting_info.h
+++ third_party/blink/renderer/core/paint/paint_layer_painting_info.h
@@ -64,7 +64,6 @@ class PaintLayer;
 enum PaintLayerFlag {
   kPaintLayerNoFlag = 0,
   kPaintLayerHaveTransparency = 1,
-  kPaintLayerUncachedClipRects = 1 << 2,
   kPaintLayerPaintingOverlayOverflowControls = 1 << 3,
   kPaintLayerPaintingCompositingBackgroundPhase = 1 << 4,
   kPaintLayerPaintingCompositingForegroundPhase = 1 << 5,
@@ -138,8 +137,6 @@ inline String PaintLayerFlagsToDebugString(PaintLayerFlags flags) {
 
   if (flags & kPaintLayerHaveTransparency)
     append("kPaintLayerHaveTransparency");
-  if (flags & kPaintLayerUncachedClipRects)
-    append("kPaintLayerUncachedClipRects");
   if (flags & kPaintLayerPaintingOverlayOverflowControls)
     append("kPaintLayerPaintingOverlayOverflowControls");
   if (flags & kPaintLayerPaintingCompositingScrollingPhase)
