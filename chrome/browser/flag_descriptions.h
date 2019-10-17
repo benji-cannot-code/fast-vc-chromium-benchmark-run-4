51ef531415971832a75ad49bee1e55470498e08d
Carlos IL <carlosil@chromium.org>
2019-10-17 23:14:11 +0000

    Add MC shield removal flag to chrome://flags
    Bug: 987294
    Change-Id: I7ada477ea649652bca83eb3db7a9100662548cf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867298
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707153}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -771,6 +771,9 @@ extern const char kMediaRouterCastAllowAllIPsDescription[];
 extern const char kMimeHandlerViewInCrossProcessFrameName[];
 extern const char kMimeHandlerViewInCrossProcessFrameDescription[];
 
+extern const char kMixedContentSiteSettingName[];
+extern const char kMixedContentSiteSettingDescription[];
+
 extern const char kMobileIdentityConsistencyName[];
 extern const char kMobileIdentityConsistencyDescription[];
 
