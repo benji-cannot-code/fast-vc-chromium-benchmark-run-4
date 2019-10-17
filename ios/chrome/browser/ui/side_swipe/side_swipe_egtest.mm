f1d5bba4bca16ae3c75ab97be6828528c88120c3
Justin Cohen <justincohen@google.com>
2019-10-17 20:17:35 +0000

    [ios] Add support for side_swipe in EG2.
    Also adds helpers for isCompactHeight and isSplitToolbarMode in
    ChromeEarlGreyImpl.
    
    Bug: 987646
    Change-Id: Id8f48a0e126faab215ffb6ed8d2e306b13f33bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864355
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707040}

--- ios/chrome/browser/ui/side_swipe/side_swipe_egtest.mm
+++ ios/chrome/browser/ui/side_swipe/side_swipe_egtest.mm
@@ -3,17 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
-#import <XCTest/XCTest.h>
-
-#import "ios/chrome/browser/ui/toolbar/primary_toolbar_view.h"
-#import "ios/chrome/browser/ui/toolbar/secondary_toolbar_view.h"
-#import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #include "ios/testing/earl_grey/disabled_test_macros.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #include "net/test/embedded_test_server/default_handlers.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -30,24 +25,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Tests that swiping horizontally on the bottom toolbar is changing tab.
 - (void)testSideSwipeBottomToolbar {
-  if (!IsSplitToolbarMode()) {
+  if (![ChromeEarlGrey isSplitToolbarMode]) {
     EARL_GREY_TEST_SKIPPED(
         @"This tests should only be tested if the secondary toolbar is "
         @"present");
   }
-  [self checkSideSwipeOnToolbarClass:[SecondaryToolbarView class]];
+  [self checkSideSwipeOnToolbarClassName:@"SecondaryToolbarView"];
 }
 
 // Tests that swiping horizontally on the top toolbar is changing tab.
 - (void)testSideSwipeTopToolbar {
-  [self checkSideSwipeOnToolbarClass:[PrimaryToolbarView class]];
+  [self checkSideSwipeOnToolbarClassName:@"PrimaryToolbarView"];
 }
 
 #pragma mark - Helpers
 
 // Checks that side swipe on an element of class |klass| is working to change
 // tab.
-- (void)checkSideSwipeOnToolbarClass:(Class)klass {
+- (void)checkSideSwipeOnToolbarClassName:(NSString*)className {
   // Setup the server.
   net::test_server::RegisterDefaultHandlers(self.testServer);
   GREYAssertTrue(self.testServer->Start(), @"Test server failed to start.");
@@ -64,7 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [ChromeEarlGrey waitForWebStateContainingText:"Default response"];
 
   // Side swipe on the toolbar.
-  [[EarlGrey selectElementWithMatcher:grey_kindOfClass(klass)]
+  [[EarlGrey selectElementWithMatcher:grey_kindOfClassName(className)]
       performAction:grey_swipeSlowInDirection(kGREYDirectionRight)];
 
   // Check that we swiped back to our web page.
