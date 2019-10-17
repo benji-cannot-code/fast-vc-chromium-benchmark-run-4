c7296e6aa1c0a13fee150e02b7ba4fa75254a68a
Mitsuru Oshima <oshima@chromium.org>
2019-10-17 17:04:34 +0000

    autotest private API for application windows
    1) getAppWindowInfo to retrieve the list of application windows
       with ids.
    2) setAppWindowState to set the window state using the id.
    3) closeAppWindow to close the window.
    
    Bug: 1010671
    Test: Covered by unit test except for close. It'll be covered in next CL.
    Change-Id: Iaa71764a58663713ac5d745b550bfa7b46bb383b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857603
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706965}

---
+++ ash/public/cpp/autotest_private_api_utils.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PUBLIC_CPP_AUTOTEST_PRIVATE_API_UTILS_H_
+#define ASH_PUBLIC_CPP_AUTOTEST_PRIVATE_API_UTILS_H_
+
+#include <vector>
+
+#include "ash/ash_export.h"
+
+namespace aura {
+class Window;
+}
+
+// Utility functions for autotest private APIs and ShellTestAPI.
+namespace ash {
+
+// Get application windows, windows that are shown in overview grid.
+ASH_EXPORT std::vector<aura::Window*> GetAppWindowList();
+
+}  // namespace ash
+
+#endif  // ASH_PUBLIC_CPP_AUTOTEST_PRIVATE_API_UTILS_H_
