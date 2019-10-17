f1952e9526262e254117b42b3dd73bbf924c7672
Noel Gordon <noel@chromium.org>
2019-10-17 05:51:35 +0000

    [directory-tree] Add custom cr.ui.Tree/TreeItem row style support
    cr.ui.Tree assumes the rowElement of the TreeItem is one to indent via
    style (padding-inline-start) to give the appearance of a tree.
    
    The inner structure of TreeItem is known to the using app and it might
    be that a child element the rowElement should be indented, and not the
    rowElement itself, to give the appearance of a tree.
    
    Allow cr.ui.Tree/TreeItem using apps to customise TreeItem's row style
    using an optional rowElementDepthStyleHandler.
    
    No change in behavior, no new tests.
    
    Bug: 992819
    Change-Id: Ifb5762ed97b265602df2e50e482045fd5d3f3151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1859796
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#706796}

--- ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
+++ ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const directorytree = {};
 
 ////////////////////////////////////////////////////////////////////////////////
-// DirectoryTreeBase
+// DirectoryTreeBase methods
 
 /**
  * Implementation of methods for DirectoryTree and DirectoryItem. These classes
- * inherits cr.ui.Tree/TreeItem so we can't make them inherit this class.
+ * inherits cr.ui.Tree/cr.ui.TreeItem so we can't make them inherit this class.
  * Instead, we separate their implementations to this separate object and call
  * it with setting 'this' from DirectoryTree/Item.
  */
@@ -137,6 +137,17 @@ const TREE_ITEM_INNER_HTML = '<div class="tree-row">' +
 
 ////////////////////////////////////////////////////////////////////////////////
 // DirectoryItem
+
+/**
+ * An optional rowElement depth (indent) style handler where undefined uses the
+ * default cr.ui.Tree/cr.ui.TreeItem indent styling.
+ *
+ * TODO(crbug.com/992819): add an implementation for the FILES_NG_ENABLED case,
+ * where a rowElement child needs the indent, not the rowElement itself.
+ *
+ * @type {function(!cr.ui.TreeItem,number)|undefined}
+ */
+directorytree.styleRowElementDepth = undefined;
 
 /**
  * An expandable directory in the tree. Each element represents one folder (sub
@@ -2242,6 +2253,10 @@ DirectoryTree.decorate =
     (el, directoryModel, volumeManager, metadataModel, fileOperationManager,
      fakeEntriesVisible) => {
       el.__proto__ = DirectoryTree.prototype;
+
+      // TODO(crbug.com/992819): add overrides for the FILES_NG_ENABLED case.
+      Object.freeze(directorytree);
+
       /** @type {DirectoryTree} */ (el).decorateDirectoryTree(
           directoryModel, volumeManager, metadataModel, fileOperationManager,
           fakeEntriesVisible);
