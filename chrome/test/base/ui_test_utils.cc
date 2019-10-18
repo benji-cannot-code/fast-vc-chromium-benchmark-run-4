c33f342d78540f4a48bc53c95c4eb2b6b6b4bf5b
Tommy Li <tommycli@chromium.org>
2019-10-18 18:28:24 +0000

    [omnibox] Prevent on-focus suggestions from popping up on startup
    This CL is a followup to:
    https://chromium-review.googlesource.com/c/chromium/src/+/1865616
    
    It should prevent renderer-initiated or browser startup focus events
    from triggering on-focus suggestions in the omnibox.
    
    Bug: 1013287, 996516
    Change-Id: I5bb000a6ce37a2adbd9dec9b1ef2f3a78da1ac42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867307
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707443}

--- chrome/test/base/ui_test_utils.cc
+++ chrome/test/base/ui_test_utils.cc
@@ -482,7 +482,8 @@ void SendToOmniboxAndSubmit(Browser* browser,
                             base::TimeTicks match_selection_timestamp) {
   LocationBar* location_bar = browser->window()->GetLocationBar();
   OmniboxView* omnibox = location_bar->GetOmniboxView();
-  omnibox->model()->OnSetFocus(false);
+  omnibox->model()->OnSetFocus(/*control_down=*/false,
+                               /*suppress_on_focus_suggestions=*/false);
   omnibox->SetUserText(base::ASCIIToUTF16(input));
   location_bar->AcceptInput(match_selection_timestamp);
 
