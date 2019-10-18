3095d6e6556aa54df923fde40f79848a1806aa0f
Julie Jeongeun Kim <jkim@igalia.com>
2019-10-18 05:26:55 +0000

    Convert media_perception_service.mojom to new Mojo types
    This CL converts MediaPerceptionService, MediaPerceptionController,
    and MediaPerceptionControllerClient from media_perception_service.mojom
    to new Mojo types using Remote, PendingReceiver, and Receiver.
    
    Bug: 955171
    Change-Id: Ia6806762929076808599c90a71a743c010f3bd34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1851784
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Luke Sorenson <lasoren@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707255}

--- extensions/browser/api/media_perception_private/media_perception_api_manager.h
+++ extensions/browser/api/media_perception_private/media_perception_api_manager.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "extensions/common/api/media_perception_private.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
 
 namespace extensions {
 
@@ -153,10 +154,10 @@ class MediaPerceptionAPIManager
 
   // Pointer to the MediaPerceptionService interface for communicating with the
   // service over Mojo.
-  chromeos::media_perception::mojom::MediaPerceptionServicePtr
+  mojo::Remote<chromeos::media_perception::mojom::MediaPerceptionService>
       media_perception_service_;
 
-  chromeos::media_perception::mojom::MediaPerceptionControllerPtr
+  mojo::Remote<chromeos::media_perception::mojom::MediaPerceptionController>
       media_perception_controller_;
 
   std::unique_ptr<MediaPerceptionControllerClient>
