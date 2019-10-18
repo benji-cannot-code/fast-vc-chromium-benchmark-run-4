64a79fe327fd2c1d6711a0d644695804e3005125
Daniel Nicoara <dnicoara@chromium.org>
2019-10-18 18:56:02 +0000

    Check status on initialize & shutdown the CQ
    * If gRPC initialize failed we don't have a connection to begin with so
    can't call Read().
    * Shutdown the CompletionQueue otherwise the thread won't exit.
    
    Bug: b/142400443
    Test: internal unittests
    Change-Id: Ifc2a13e36bbdc640908ca1069afed855fc8cd790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867313
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Auto-Submit: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707463}

--- chromecast/browser/webview/webview_grpc_service.cc
+++ chromecast/browser/webview/webview_grpc_service.cc
@@ -139,6 +139,12 @@ void WebviewRpcInstance::ProcessRequestOnWebviewThread(
 }
 
 void WebviewRpcInstance::InitComplete(bool ok) {
+  if (!ok) {
+    destroying_ = true;
+    delete this;
+    return;
+  }
+
   request_ = std::make_unique<webview::WebviewRequest>();
   io_.Read(request_.get(), &read_callback_);
 
@@ -264,6 +270,7 @@ WebviewAsyncService::WebviewAsyncService(
 }
 
 WebviewAsyncService::~WebviewAsyncService() {
+  cq_->Shutdown();
   base::PlatformThread::Join(rpc_thread_);
 }
 
