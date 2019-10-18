f154ae07fb2460c0fada61312d7ad360f638d256
Yun Liu <yliuyliu@google.com>
2019-10-18 23:22:27 +0000

    [code coverage] Explicitly write profile file for android apk-based gtests
    Write profile data after Android apk-based gtests finished.
    
    Bug: 985574
    Change-Id: If22313a6eca142226a3fa095a0f40b8d44c00e85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867190
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Yun Liu <yliuyliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#707585}

--- testing/android/native_test/native_test_launcher.cc
+++ testing/android/native_test/native_test_launcher.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/at_exit.h"
 #include "base/base_switches.h"
+#include "base/clang_coverage_buildflags.h"
 #include "base/command_line.h"
 #include "base/debug/debugger.h"
 #include "base/files/file_path.h"
@@ -32,6 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/android/native_test/main_runner.h"
 #include "testing/android/native_test/native_test_jni_headers/NativeTest_jni.h"
 #include "testing/android/native_test/native_test_util.h"
+
+#if BUILDFLAG(CLANG_COVERAGE)
+#include "base/test/clang_coverage.h"
+#endif
 
 using base::android::JavaParamRef;
 
@@ -135,6 +140,11 @@ static void JNI_NativeTest_RunTests(
 
   ScopedMainEntryLogger scoped_main_entry_logger;
   main(argc, &argv[0]);
+
+// Explicitly write coverage data to LLVM profile file.
+#if BUILDFLAG(CLANG_COVERAGE)
+  base::WriteClangCoverageProfile();
+#endif
 }
 
 // TODO(nileshagrawal): now that we're using FIFO, test scripts can detect EOF.
