f516266bebe2e3e42612b574ad7338076880f4ef
Alexander Timin <altimin@chromium.org>
2019-10-18 17:09:24 +0000

    [bfcache] Add separate feature params for geolocation and SW
    R=skyostil@chromium.org,ahemery@chromium.org
    
    Change-Id: I9b4b874f8de4800dbf5d1e9b50c6c94e86be44e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868959
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707411}

--- content/browser/back_forward_cache_browsertest.cc
+++ content/browser/back_forward_cache_browsertest.cc
@@ -2359,7 +2359,7 @@ class BackForwardCacheBrowserTestWithServiceWorkerEnabled
 
  protected:
   base::FieldTrialParams GetFeatureParams() override {
-    return {{"experimental extended supported feature set", "true"}};
+    return {{"service_worker_supported", "true"}};
   }
 };
 
@@ -2620,7 +2620,7 @@ class GeolocationBackForwardCacheBrowserTest
   GeolocationBackForwardCacheBrowserTest() : geo_override_(0.0, 0.0) {}
 
   base::FieldTrialParams GetFeatureParams() override {
-    return {{"experimental extended supported feature set", "true"}};
+    return {{"geolocation_supported", "true"}};
   }
 
   device::ScopedGeolocationOverrider geo_override_;
