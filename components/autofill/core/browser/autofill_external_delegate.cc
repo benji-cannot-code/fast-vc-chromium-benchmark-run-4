5a92908c678f88dbb4ef8bd946f9bae968d451d9
Akihiro Ota <akihiroota@chromium.org>
2019-10-18 21:00:05 +0000

    Accessibility: Test autocomplete and autofill state plumbing
    This change adds tests to ensure autofill and autocomplete state
    are plumbed correctly to the accessibility tree.
    
    This change also consolidates when and where we fire these
    notifications.
    
    Change-Id: I1888b71445cf4c5db356dae090dc69f58362f077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1845916
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707521}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -133,6 +133,7 @@ void AutofillExternalDelegate::OnSuggestionsReturned(
   InsertDataListValues(&suggestions);
 
   if (suggestions.empty()) {
+    OnAutofillAvailabilityEvent(mojom::AutofillState::kNoSuggestions);
     // No suggestions, any popup currently showing is obsolete.
     manager_->client()->HideAutofillPopup();
     return;
@@ -171,6 +172,10 @@ void AutofillExternalDelegate::SetCurrentDataListValues(
 }
 
 void AutofillExternalDelegate::OnPopupShown() {
+  // If a popup was shown, then we showed either autofill or autocomplete.
+  OnAutofillAvailabilityEvent(
+      has_autofill_suggestions_ ? mojom::AutofillState::kAutofillAvailable
+                                : mojom::AutofillState::kAutocompleteAvailable);
   manager_->DidShowSuggestions(has_autofill_suggestions_, query_form_,
                                query_field_);
 
--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -1376,9 +1376,6 @@ void AutofillManager::OnSuggestionsReturned(
     int query_id,
     bool autoselect_first_suggestion,
     const std::vector<Suggestion>& suggestions) {
-  external_delegate_->OnAutofillAvailabilityEvent(
-      !suggestions.empty() ? mojom::AutofillState::kAutocompleteAvailable
-                           : mojom::AutofillState::kNoSuggestions);
   external_delegate_->OnSuggestionsReturned(query_id, suggestions,
                                             autoselect_first_suggestion);
 }
