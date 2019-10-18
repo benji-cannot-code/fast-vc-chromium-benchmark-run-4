cad1b367defd304f1bd1653fb3994e8a70e0f3c3
Jazz Xu <jazzhsu@chromium.org>
2019-10-18 16:55:34 +0000

    [Lockscreen Media Controls] Media controls layout modification.
    Update lock screen media controls layout and icons.
    
    Bug: 1015133
    Change-Id: I288f4826e490c18d40217ddbfd4305b25fd8c514
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865717
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Jazz Xu <jazzhsu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707402}

--- components/media_message_center/media_controls_progress_view.cc
+++ components/media_message_center/media_controls_progress_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/i18n/time_formatting.h"
 #include "services/media_session/public/mojom/media_session.mojom.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/font_list.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/label.h"
@@ -20,7 +21,8 @@ namespace media_message_center {
 
 namespace {
 
-constexpr int kProgressBarAndTimeSpacing = 3;
+constexpr SkColor kTimeColor = gfx::kGoogleGrey200;
+constexpr int kProgressBarAndTimeSpacing = 8;
 constexpr int kProgressTimeFontSize = 11;
 constexpr int kProgressBarHeight = 4;
 constexpr int kMinClickHeight = 14;
@@ -57,7 +59,7 @@ MediaControlsProgressView::MediaControlsProgressView(
 
   auto progress_time = std::make_unique<views::Label>();
   progress_time->SetFontList(font_list);
-  progress_time->SetEnabledColor(SK_ColorWHITE);
+  progress_time->SetEnabledColor(kTimeColor);
   progress_time->SetAutoColorReadabilityEnabled(false);
   progress_time_ = time_view->AddChildView(std::move(progress_time));
 
