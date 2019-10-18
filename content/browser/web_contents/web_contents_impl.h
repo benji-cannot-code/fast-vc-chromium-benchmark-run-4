ae22d219d90469749610c50b0c5f63d8864146a4
Nasko Oskov <nasko@chromium.org>
2019-10-18 23:33:38 +0000

    Remove GetLastCommittedNavigationEntryForRenderManager.
    This method on RenderFrameHostManager::Delegate is not really needed.
    It is a single line wrapper around NavigationController method and the
    delegate interface already has a method to get the NavigationController.
    As such, I'm removing it and replacing it with a direct call on the
    NavigationContoller.
    
    Bug: 1015882
    Change-Id: Iffc68d2e3e42e102d115337c271a820a2b368707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869714
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707589}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -862,7 +862,6 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
       RenderFrameHost* old_host,
       RenderFrameHost* new_host) override;
   NavigationControllerImpl& GetControllerForRenderManager() override;
-  NavigationEntry* GetLastCommittedNavigationEntryForRenderManager() override;
   InterstitialPageImpl* GetInterstitialForRenderManager() override;
   bool FocusLocationBarByDefault() override;
   void SetFocusToLocationBar() override;
