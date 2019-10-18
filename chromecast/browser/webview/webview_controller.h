21de50c1926438dbe32925ab64313b0900179dd6
Shawn Gallea <sagallea@google.com>
2019-10-18 19:48:43 +0000

    Add WebContentController
    Split WebviewController into a generic base class that only
    deals with mutating the WebContents. The split allows implementing
    a Webview and CastApp controller differently while retaining common
    functionality in the base class.
    
    
    Bug: b/136460199
    Test: Compile
    Change-Id: I4267e3c6ac4e3904160e7ee34e35d2b220777cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867189
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Cr-Commit-Position: refs/heads/master@{#707487}

--- chromecast/browser/webview/webview_controller.h
+++ chromecast/browser/webview/webview_controller.h
@@ -12,14 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/supports_user_data.h"
 #include "chromecast/browser/cast_web_contents.h"
 #include "chromecast/browser/webview/proto/webview.pb.h"
-#include "components/exo/surface.h"
-#include "components/exo/surface_observer.h"
-#include "ui/events/gestures/gesture_recognizer_impl.h"
+#include "chromecast/browser/webview/web_content_controller.h"
 #include "url/gurl.h"
-
-namespace aura {
-class Window;
-}  // namespace aura
 
 namespace chromecast {
 class CastWebContents;
@@ -40,15 +34,8 @@ class WebviewNavigationThrottle;
 // to allow the web contents to be controlled and embedded.
 class WebviewController : public CastWebContents::Delegate,
                           public CastWebContents::Observer,
-                          public exo::SurfaceObserver {
+                          public WebContentController {
  public:
-  class Client {
-   public:
-    virtual ~Client() {}
-    virtual void EnqueueSend(
-        std::unique_ptr<webview::WebviewResponse> response) = 0;
-    virtual void OnError(const std::string& error_message) = 0;
-  };
   WebviewController(content::BrowserContext* browser_context, Client* client);
   ~WebviewController() override;
 
@@ -60,43 +47,22 @@ class WebviewController : public CastWebContents::Delegate,
   // Cause the controller to be destroyed after giving the webpage a chance to
   // run unload events. This unsets the client so no more messages will be
   // sent.
-  void Destroy();
+  void Destroy() override;
+
+  void ProcessRequest(const webview::WebviewRequest& request) override;
 
   // Close the page. This will cause a stopped response to eventually be sent.
   void ClosePage();
-
-  void ProcessRequest(const webview::WebviewRequest& request);
-
-  // Attach this web contents to an aura window as a child.
-  void AttachTo(aura::Window* window, int window_id);
 
   void SendNavigationEvent(WebviewNavigationThrottle* throttle,
                            const GURL& url,
                            bool is_in_main_frame);
 
+ protected:
+  content::WebContents* GetWebContents() override;
+
  private:
   webview::AsyncPageEvent_State current_state();
-
-  void ProcessInputEvent(const webview::InputEvent& ev);
-  void JavascriptCallback(int64_t id, base::Value result);
-  void HandleEvaluateJavascript(
-      int64_t id,
-      const webview::EvaluateJavascriptRequest& request);
-  void HandleAddJavascriptChannels(
-      const webview::AddJavascriptChannelsRequest& request);
-  void HandleRemoveJavascriptChannels(
-      const webview::RemoveJavascriptChannelsRequest& request);
-  void HandleGetCurrentUrl(int64_t id);
-  void HandleCanGoBack(int64_t id);
-  void HandleCanGoForward(int64_t id);
-  void HandleClearCache();
-  void HandleGetTitle(int64_t id);
-  void HandleUpdateSettings(const webview::UpdateSettingsRequest& request);
-  void HandleSetAutoMediaPlaybackPolicy(
-      const webview::SetAutoMediaPlaybackPolicyRequest& request);
-  viz::SurfaceId GetSurfaceId();
-
-  bool Check(bool condition, const char* error);
 
   // CastWebContents::Delegate
   void OnPageStateChanged(CastWebContents* cast_web_contents) override;
@@ -106,19 +72,9 @@ class WebviewController : public CastWebContents::Delegate,
   // CastWebContents::Observer
   void ResourceLoadFailed(CastWebContents* cast_web_contents) override;
 
-  // exo::SurfaceObserver
-  void OnSurfaceDestroying(exo::Surface* surface) override;
-
-  Client* client_;  // Not owned.
   std::unique_ptr<content::WebContents> contents_;
   std::unique_ptr<CastWebContents> cast_web_contents_;
   bool stopped_ = false;
-
-  bool has_navigation_delegate_ = false;
-
-  ui::GestureRecognizerImpl gesture_recognizer_;
-
-  exo::Surface* surface_ = nullptr;
 
   // The navigation throttle for the current navigation event, if any.
   // Is set only:
