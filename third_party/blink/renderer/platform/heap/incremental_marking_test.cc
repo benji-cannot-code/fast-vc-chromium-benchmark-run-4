d3280a125ad392f8a0a3aa62322f2d4293b2a29b
Michael Lippautz <mlippautz@chromium.org>
2019-10-18 19:04:11 +0000

    heap: Further optimize write barrier path during GC
    - Avoid looking up GCInfo and one word in memory when pushing an object
      that was discovered through the write barrier.
    - Use already existing branch for regular and large objects to compute
      size, assuming that the object will be marked.
    
    Output on Z840 (down from ~17x):
    [ RUN      ] WriteBarrierPerfTest.MemberWritePerformance
    *RESULT WriteBarrierPerfTest writes during GC: = 36403.349108117945 writes/ms
    *RESULT WriteBarrierPerfTest writes outside GC: = 476190.4761904762 writes/ms
    *RESULT WriteBarrierPerfTest relative speed difference: = 13.08095238095238 times
    [       OK ] WriteBarrierPerfTest.MemberWritePerformance (29 ms)
    
    Bug: 1014414
    Change-Id: I2c915edd1607bc839ea0478891971d23496b2678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865344
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707469}

--- third_party/blink/renderer/platform/heap/incremental_marking_test.cc
+++ third_party/blink/renderer/platform/heap/incremental_marking_test.cc
@@ -112,12 +112,14 @@ class IncrementalMarkingScope : public IncrementalMarkingScopeBase {
       : IncrementalMarkingScopeBase(thread_state),
         gc_forbidden_scope_(thread_state),
         marking_worklist_(heap_.GetMarkingWorklist()),
+        write_barrier_worklist_(heap_.GetWriteBarrierWorklist()),
         not_fully_constructed_worklist_(
             heap_.GetNotFullyConstructedWorklist()) {
     thread_state_->SetGCPhase(ThreadState::GCPhase::kMarking);
     ThreadState::AtomicPauseScope atomic_pause_scope_(thread_state_);
     ScriptForbiddenScope script_forbidden_scope;
     EXPECT_TRUE(marking_worklist_->IsGlobalEmpty());
+    EXPECT_TRUE(write_barrier_worklist_->IsGlobalEmpty());
     EXPECT_TRUE(not_fully_constructed_worklist_->IsGlobalEmpty());
     thread_state->EnableIncrementalMarkingBarrier();
     thread_state->current_gc_data_.visitor = std::make_unique<MarkingVisitor>(
@@ -126,6 +128,7 @@ class IncrementalMarkingScope : public IncrementalMarkingScopeBase {
 
   ~IncrementalMarkingScope() {
     EXPECT_TRUE(marking_worklist_->IsGlobalEmpty());
+    EXPECT_TRUE(write_barrier_worklist_->IsGlobalEmpty());
     EXPECT_TRUE(not_fully_constructed_worklist_->IsGlobalEmpty());
     thread_state_->DisableIncrementalMarkingBarrier();
     // Need to clear out unused worklists that might have been polluted during
@@ -136,6 +139,9 @@ class IncrementalMarkingScope : public IncrementalMarkingScopeBase {
   }
 
   MarkingWorklist* marking_worklist() const { return marking_worklist_; }
+  WriteBarrierWorklist* write_barrier_worklist() const {
+    return write_barrier_worklist_;
+  }
   NotFullyConstructedWorklist* not_fully_constructed_worklist() const {
     return not_fully_constructed_worklist_;
   }
@@ -143,6 +149,7 @@ class IncrementalMarkingScope : public IncrementalMarkingScopeBase {
  protected:
   ThreadState::GCForbiddenScope gc_forbidden_scope_;
   MarkingWorklist* const marking_worklist_;
+  WriteBarrierWorklist* const write_barrier_worklist_;
   NotFullyConstructedWorklist* const not_fully_constructed_worklist_;
 };
 
@@ -157,6 +164,7 @@ class ExpectWriteBarrierFires : public IncrementalMarkingScope {
         objects_(objects),
         backing_visitor_(thread_state_, &objects_) {
     EXPECT_TRUE(marking_worklist_->IsGlobalEmpty());
+    EXPECT_TRUE(write_barrier_worklist_->IsGlobalEmpty());
     for (void* object : objects_) {
       // Ensure that the object is in the normal arena so we can ignore backing
       // objects on the marking stack.
@@ -169,9 +177,8 @@ class ExpectWriteBarrierFires : public IncrementalMarkingScope {
   }
 
   ~ExpectWriteBarrierFires() {
-    EXPECT_FALSE(marking_worklist_->IsGlobalEmpty());
+    // All objects watched should be on the marking or write barrier worklist.
     MarkingItem item;
-    // All objects watched should be on the marking stack.
     while (marking_worklist_->Pop(WorklistTaskId::MutatorThread, &item)) {
       // Inspect backing stores to allow specifying objects that are only
       // reachable through a backing store.
@@ -185,6 +192,21 @@ class ExpectWriteBarrierFires : public IncrementalMarkingScope {
       if (objects_.end() != pos)
         objects_.erase(pos);
     }
+    HeapObjectHeader* header;
+    while (
+        write_barrier_worklist_->Pop(WorklistTaskId::MutatorThread, &header)) {
+      // Inspect backing stores to allow specifying objects that are only
+      // reachable through a backing store.
+      if (!ThreadHeap::IsNormalArenaIndex(
+              PageFromObject(header->Payload())->Arena()->ArenaIndex())) {
+        backing_visitor_.ProcessBackingStore(header);
+        continue;
+      }
+      auto** pos =
+          std::find(objects_.begin(), objects_.end(), header->Payload());
+      if (objects_.end() != pos)
+        objects_.erase(pos);
+    }
     EXPECT_TRUE(objects_.IsEmpty());
     // All headers of objects watched should be marked at this point.
     for (HeapObjectHeader* header : headers_) {
@@ -192,6 +214,7 @@ class ExpectWriteBarrierFires : public IncrementalMarkingScope {
       header->Unmark();
     }
     EXPECT_TRUE(marking_worklist_->IsGlobalEmpty());
+    EXPECT_TRUE(write_barrier_worklist_->IsGlobalEmpty());
   }
 
  private:
@@ -209,6 +232,7 @@ class ExpectNoWriteBarrierFires : public IncrementalMarkingScope {
                             std::initializer_list<void*> objects)
       : IncrementalMarkingScope(thread_state) {
     EXPECT_TRUE(marking_worklist_->IsGlobalEmpty());
+    EXPECT_TRUE(write_barrier_worklist_->IsGlobalEmpty());
     for (void* object : objects_) {
       HeapObjectHeader* header = HeapObjectHeader::FromPayload(object);
       headers_.push_back(std::make_pair(header, header->IsMarked()));
@@ -217,6 +241,7 @@ class ExpectNoWriteBarrierFires : public IncrementalMarkingScope {
 
   ~ExpectNoWriteBarrierFires() {
     EXPECT_TRUE(marking_worklist_->IsGlobalEmpty());
+    EXPECT_TRUE(write_barrier_worklist_->IsGlobalEmpty());
     for (const auto& pair : headers_) {
       EXPECT_EQ(pair.second, pair.first->IsMarked());
       pair.first->Unmark();
@@ -1443,7 +1468,6 @@ TEST_F(IncrementalMarkingTest, WriteBarrierDuringMixinConstruction) {
 
   // Clear any objects that have been added to the regular marking worklist in
   // the process of calling the constructor.
-  EXPECT_FALSE(scope.marking_worklist()->IsGlobalEmpty());
   MarkingItem marking_item;
   while (scope.marking_worklist()->Pop(WorklistTaskId::MutatorThread,
                                        &marking_item)) {
@@ -1453,6 +1477,14 @@ TEST_F(IncrementalMarkingTest, WriteBarrierDuringMixinConstruction) {
       header->Unmark();
   }
   EXPECT_TRUE(scope.marking_worklist()->IsGlobalEmpty());
+  // Clear any write barriers so far.
+  HeapObjectHeader* header;
+  while (scope.write_barrier_worklist()->Pop(WorklistTaskId::MutatorThread,
+                                             &header)) {
+    if (header->IsMarked())
+      header->Unmark();
+  }
+  EXPECT_TRUE(scope.write_barrier_worklist()->IsGlobalEmpty());
 
   EXPECT_FALSE(scope.not_fully_constructed_worklist()->IsGlobalEmpty());
   NotFullyConstructedItem partial_item;
--- third_party/blink/renderer/platform/heap/marking_visitor.h
+++ third_party/blink/renderer/platform/heap/marking_visitor.h
@@ -126,9 +126,9 @@ class PLATFORM_EXPORT MarkingVisitorBase : public Visitor {
   // marked upon calling.
   inline bool MarkHeaderNoTracing(HeapObjectHeader*);
 
-  // Account for an object's live bytes. Should only be adjusted when
+  // Account for |size| live bytes. Should only be adjusted when
   // transitioning an object from unmarked to marked state.
-  ALWAYS_INLINE void AccountMarkedBytes(HeapObjectHeader*);
+  ALWAYS_INLINE void AccountMarkedBytes(size_t size);
 
   void RegisterBackingStoreReference(void** slot);
 
@@ -143,12 +143,8 @@ class PLATFORM_EXPORT MarkingVisitorBase : public Visitor {
   int task_id_;
 };
 
-ALWAYS_INLINE void MarkingVisitorBase::AccountMarkedBytes(
-    HeapObjectHeader* header) {
-  marked_bytes_ +=
-      header->IsLargeObject()
-          ? reinterpret_cast<LargeObjectPage*>(PageFromObject(header))->size()
-          : header->size();
+ALWAYS_INLINE void MarkingVisitorBase::AccountMarkedBytes(size_t size) {
+  marked_bytes_ += size;
 }
 
 inline bool MarkingVisitorBase::MarkHeaderNoTracing(HeapObjectHeader* header) {
@@ -162,7 +158,11 @@ inline bool MarkingVisitorBase::MarkHeaderNoTracing(HeapObjectHeader* header) {
   DCHECK(!header->IsFree());
 
   if (header->TryMark<HeapObjectHeader::AccessMode::kAtomic>()) {
-    AccountMarkedBytes(header);
+    const size_t size =
+        header->IsLargeObject()
+            ? reinterpret_cast<LargeObjectPage*>(PageFromObject(header))->size()
+            : header->size();
+    AccountMarkedBytes(size);
     return true;
   }
   return false;
@@ -217,6 +217,8 @@ class PLATFORM_EXPORT MarkingVisitor : public MarkingVisitorBase {
   // Exact version of the marking write barriers.
   static bool WriteBarrierSlow(void*);
   static void TraceMarkedBackingStoreSlow(void*);
+
+  WriteBarrierWorklist::View write_barrier_worklist_;
 };
 
 ALWAYS_INLINE bool MarkingVisitor::WriteBarrier(void* value) {
