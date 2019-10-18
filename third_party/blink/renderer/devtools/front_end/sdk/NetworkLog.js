6835add21788b3892131a43d942ae28c4cec010b
Junyi Xiao <juxiao@microsoft.com>
2019-10-18 20:18:06 +0000

    Devtools: fix js exception in SDK.NetworkLog._initiatorChain
    Issue: In SDK.NetworkLog._initiatorChain, we potential run into the case where
    [initiatorDataSymbol] is not defined yet when we recursively walk up the
    initiator chain for the request.
    
    This changes adds initializeInitiatorSymbolIfNeeded in the walking process
    and also fix a "typo" in initiatorGraphForRequest networkManager checking.
    
    Change-Id: Ibaa1deec6c2548e149554aa526abf27a61711621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867412
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Lorne Mitchell <lomitch@microsoft.com>
    Commit-Queue: Junyi Xiao <juxiao@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#707502}

--- third_party/blink/renderer/devtools/front_end/sdk/NetworkLog.js
+++ third_party/blink/renderer/devtools/front_end/sdk/NetworkLog.js
@@ -242,7 +242,7 @@ export default class NetworkLog extends Common.Object {
     const initiated = new Set();
     const networkManager = SDK.NetworkManager.forRequest(request);
     for (const otherRequest of this._requests) {
-      const otherRequestManager = SDK.NetworkManager.forRequest(request);
+      const otherRequestManager = SDK.NetworkManager.forRequest(otherRequest);
       if (networkManager === otherRequestManager && this._initiatorChain(otherRequest).has(request)) {
         initiated.add(otherRequest);
       }
@@ -266,6 +266,7 @@ export default class NetworkLog extends Common.Object {
 
     let checkRequest = request;
     do {
+      this._initializeInitiatorSymbolIfNeeded(checkRequest);
       if (checkRequest[_initiatorDataSymbol].chain) {
         initiatorChainCache.addAll(checkRequest[_initiatorDataSymbol].chain);
         break;
