50fbc3af9c10e7b53392ca532353a266a7963b7e
Chris Lu <thegreenfrog@chromium.org>
2019-10-18 20:12:54 +0000

    [ios] Use string IDs in PopupMenuItem
    This will ensure the title string will be translated.
    
    Bug: 1014726
    Change-Id: Ia173d5f2f46576d1db44ddb52e34ae2a9fb4be46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865625
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707499}

--- ios/chrome/browser/ui/badges/badge_popup_menu_item.mm
+++ ios/chrome/browser/ui/badges/badge_popup_menu_item.mm
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #import "ios/chrome/common/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui_util/constraints_ui_util.h"
+#include "ios/chrome/grit/ios_strings.h"
+#include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -50,11 +52,13 @@ const CGFloat kBadgeCornerRadius = 5.0;
     switch (badgeType) {
       case BadgeType::kBadgeTypePasswordSave:
         _actionIdentifier = PopupMenuActionShowSavePasswordOptions;
-        _title = @"Save Password";
+        _title = l10n_util::GetNSString(
+            IDS_IOS_PASSWORD_MANAGER_SAVE_PASSWORD_TITLE);
         break;
       case BadgeType::kBadgeTypePasswordUpdate:
         _actionIdentifier = PopupMenuActionShowUpdatePasswordOptions;
-        _title = @"Update Password";
+        _title = l10n_util::GetNSString(
+            IDS_IOS_PASSWORD_MANAGER_UPDATE_PASSWORD_TITLE);
         break;
       case BadgeType::kBadgeTypeSaveCard:
         _actionIdentifier = PopupMenuActionShowSaveCardOptions;
