c50c7830daef9884a9313ab29f90003e500f3894
Yi Gu <yigu@chromium.org>
2019-10-18 14:55:00 +0000

    Revert "[Sheriff] Disable flaky test NestedSurfaceHitTestTest"
    This reverts commit f69623b272654a86b565155f927f999004dd8ffa.
    
    Reason for revert: Unnecessary tests were disabled on unnecessary platforms. Given that the tests are critical to hit testing. Revert this patch for now and disable the one that may actually flake later.
    
    Original change's description:
    > [Sheriff] Disable flaky test NestedSurfaceHitTestTest
    > 
    > SitePerProcessNonIntegerScaleFactorHitTestBrowserTest
    > ::NestedSurfaceHitTestTest is flaky.
    > 
    > TBR=kenrb@chromium.org
    > 
    > Bug: 1014602
    > Change-Id: If7c69bcf4a50c8e7b2646ec8826ea0115018cca6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1862663
    > Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    > Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    > Cr-Commit-Position: refs/heads/master@{#706161}
    
    TBR=parastoog@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1014602
    Change-Id: Ife838c1e96c501d91559051fdcf7cf499bb9090e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869390
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707370}

--- content/browser/site_per_process_hit_test_browsertest.cc
+++ content/browser/site_per_process_hit_test_browsertest.cc
@@ -2556,17 +2556,15 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessHighDPIHitTestBrowserTest,
   SurfaceHitTestTestHelper(shell(), embedded_test_server());
 }
 
-// TODO(crbug/1014602): NestedSurfaceHitTestTest is flaky.
-
 // Test that mouse events are being routed to the correct RenderWidgetHostView
 // when there are nested out-of-process iframes.
 IN_PROC_BROWSER_TEST_P(SitePerProcessHitTestBrowserTest,
-                       DISABLED_NestedSurfaceHitTestTest) {
+                       NestedSurfaceHitTestTest) {
   NestedSurfaceHitTestTestHelper(shell(), embedded_test_server());
 }
 
 IN_PROC_BROWSER_TEST_P(SitePerProcessHighDPIHitTestBrowserTest,
-                       DISABLED_NestedSurfaceHitTestTest) {
+                       NestedSurfaceHitTestTest) {
   NestedSurfaceHitTestTestHelper(shell(), embedded_test_server());
 }
 
@@ -6283,7 +6281,7 @@ IN_PROC_BROWSER_TEST_P(SitePerProcessNonIntegerScaleFactorHitTestBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_P(SitePerProcessNonIntegerScaleFactorHitTestBrowserTest,
-                       DISABLED_NestedSurfaceHitTestTest) {
+                       NestedSurfaceHitTestTest) {
   NestedSurfaceHitTestTestHelper(shell(), embedded_test_server());
 }
 
