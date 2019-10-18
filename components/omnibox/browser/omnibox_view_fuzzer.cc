4b4ca5122c17be5aadffd6e44124ba123e354fcc
Tommy Li <tommycli@chromium.org>
2019-10-18 21:41:12 +0000

    [omnibox] Add fuzzer for OmniboxView::SanitizeTextForPaste
    I made a fuzzer for this because this code is kind of complex.
    
    Also, the user's clipboard can contain pretty arbitrary data, so we
    want to validate we can handle all inputs with no problems.
    
    Change-Id: Ifaa25d47d5cdc247bf7f73425011bab8deadd135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869075
    Reviewed-by: Kevin Bailey <krb@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707540}

---
+++ components/omnibox/browser/omnibox_view_fuzzer.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/omnibox/browser/omnibox_view.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/strings/string16.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // This fuzzer creates a random UTF16 string to represent clipboard contents.
+  base::string16 s(reinterpret_cast<const base::string16::value_type*>(data),
+                   size / sizeof(base::string16::value_type));
+  OmniboxView::SanitizeTextForPaste(s);
+  return 0;
+}
