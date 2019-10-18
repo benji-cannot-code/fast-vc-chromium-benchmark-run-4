1892c2921075c09843de1d3732f6e7141523e482
Ted Meyer <tmathmeyer@chromium.org>
2019-10-18 21:31:31 +0000

    Fix processor formatting checks for DXVA
    Note: this will also be used by D3D11VideoDecoder in the future.
    
    The old way of just checking D3D11Device->CheckFormatSupport lies about
    whether the processor / enumerator really can support those profiles.
    This cl makes a new checker which can create an enumerator and then
    check the formats that way.
    
    Change-Id: I9f25e633c161095410909afa3ba88ca4f2e09000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864349
    Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707537}

--- media/gpu/windows/supported_profile_helpers.h
+++ media/gpu/windows/supported_profile_helpers.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/windows/d3d11_com_defs.h"
 #include "ui/gfx/geometry/rect.h"
 
-#include "ui/gfx/geometry/rect.h"
 
 namespace media {
 
