188ae332cc3357ef00bc3fcc26ef29d255102456
Maks Orlovich <morlovich@chromium.org>
2019-10-18 21:00:41 +0000

    CrOS cookie using things: act as first party for cookie ops.
    ... So that cookies accessed don't have to be marked as being OK for 3p
    access once we require explicit marking for such (SameSite=none).
    
    (The Android change also looks at HttpOnly cookies, which it didn't before,
     but that's probably OK since if you can set an HttpOnly cookie you can set
     a non-HttpOnly one).
    
    Bug: 1015171
    
    Change-Id: I2fcace302c272b0f08f09214ebfe72b9aee14203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1853109
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707522}

--- chrome/browser/chromeos/android_sms/android_sms_pairing_state_tracker_impl.cc
+++ chrome/browser/chromeos/android_sms/android_sms_pairing_state_tracker_impl.cc
@@ -50,7 +50,7 @@ bool AndroidSmsPairingStateTrackerImpl::IsAndroidSmsPairingComplete() {
 
 void AndroidSmsPairingStateTrackerImpl::AttemptFetchMessagesPairingState() {
   GetCookieManager()->GetCookieList(
-      GetPairingUrl(), net::CookieOptions(),
+      GetPairingUrl(), net::CookieOptions::MakeAllInclusive(),
       base::BindOnce(&AndroidSmsPairingStateTrackerImpl::OnCookiesRetrieved,
                      base::Unretained(this)));
 }
