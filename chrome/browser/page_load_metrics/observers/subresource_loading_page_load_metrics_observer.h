6e23cd662bf369f19fd32492173da682c029187a
Robert Ogden <robertogden@chromium.org>
2019-10-18 01:59:24 +0000

    Add days since last visit to origin in Subresource PLM Observer
    Records the number of days since any origin in the redirect chain was
    last visited according to the HistoryService.
    
    Bug: 995437
    Change-Id: Id044f4845e353187c01eb5464173836296d5eff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1850419
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707213}

--- chrome/browser/page_load_metrics/observers/subresource_loading_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/subresource_loading_page_load_metrics_observer.h
@@ -9,10 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/sequence_checker.h"
+#include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
+#include "components/history/core/browser/history_types.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
+#include "url/gurl.h"
 
 // Records metrics related to loading of subresources on a page.
 class SubresourceLoadingPageLoadMetricsObserver
@@ -21,10 +25,18 @@ class SubresourceLoadingPageLoadMetricsObserver
   SubresourceLoadingPageLoadMetricsObserver();
   ~SubresourceLoadingPageLoadMetricsObserver() override;
 
+ protected:
+  // Used as a callback for history service query results. Protected for
+  // testing.
+  void OnOriginLastVisitResult(history::HistoryLastVisitToHostResult result);
+
  private:
   void RecordMetrics();
 
   // page_load_metrics::PageLoadMetricsObserver:
+  ObservePolicy OnStart(content::NavigationHandle* navigation_handle,
+                        const GURL& currently_committed_url,
+                        bool started_in_foreground) override;
   ObservePolicy OnCommit(content::NavigationHandle* navigation_handle,
                          ukm::SourceId source_id) override;
   ObservePolicy FlushMetricsOnAppEnterBackground(
@@ -38,10 +50,25 @@ class SubresourceLoadingPageLoadMetricsObserver
       const std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr>&
           resources) override;
 
+  // The time that the navigation started. Used to timebox the history service
+  // query on commit.
+  base::Time navigation_start_;
+
   size_t loaded_css_js_from_cache_before_fcp_ = 0;
   size_t loaded_css_js_from_network_before_fcp_ = 0;
 
+  // The minimum number of days since the last visit, as reported by
+  // HistoryService, to any origin in the redirect chain. Set to -1 if there is
+  // a response from the history service but was no previous visit.
+  base::Optional<int> min_days_since_last_visit_to_origin_;
+
+  // Task tracker for calls for the history service.
+  base::CancelableTaskTracker task_tracker_;
+
   SEQUENCE_CHECKER(sequence_checker_);
+
+  base::WeakPtrFactory<SubresourceLoadingPageLoadMetricsObserver> weak_factory_{
+      this};
 
   DISALLOW_COPY_AND_ASSIGN(SubresourceLoadingPageLoadMetricsObserver);
 };
