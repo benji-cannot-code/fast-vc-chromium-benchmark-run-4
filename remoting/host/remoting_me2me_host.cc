2777ac4778f075e34d9623c4644470e3c6555497
Gary Kacmarcik <garykac@chromium.org>
2019-10-18 18:35:56 +0000

    [Chromoting] Add checks for Screen Capturer
    This adds a check to determine if the Screen Capture permission has
    already been granted and also triggers a 'fake' Screen Capture so
    that the binary will be added to the list of apps that the user can
    grant Screen Recording permission to.
    
    Bug: 1012545
    Change-Id: Ia3ad4709f0e594fd07660f62a683e6725f762805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867447
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Gary Kacmarcik <garykac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707448}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -118,6 +118,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MACOSX)
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
+#include "remoting/host/desktop_capturer_checker.h"
 #include "remoting/host/mac/permission_utils.h"
 #endif  // defined(OS_MACOSX)
 
@@ -454,6 +455,10 @@ class HostProcess : public ConfigWatcher::Delegate,
   scoped_refptr<PairingRegistry> pairing_registry_;
 
   ShutdownWatchdog* shutdown_watchdog_;
+
+#if defined(OS_MACOSX)
+  std::unique_ptr<DesktopCapturerChecker> capture_checker_;
+#endif  // defined(OS_MACOSX)
 
   DISALLOW_COPY_AND_ASSIGN(HostProcess);
 };
@@ -1557,8 +1562,16 @@ void HostProcess::StartHost() {
   // Ensure we are not running as root (i.e. at the login screen).
   DCHECK_NE(getuid(), 0U);
 
+  // Capture a single screen image to trigger OS permission checks which will
+  // add our binary to the list of apps that can be granted permission. This
+  // is only needed for OS X 10.15 and later.
+  if (!base::mac::IsAtLeastOS10_15()) {
+    capture_checker_.reset(new DesktopCapturerChecker());
+    capture_checker_->TriggerSingleCapture();
+  }
+
   mac::PromptUserToChangeTrustStateIfNeeded(context_->ui_task_runner());
-#endif
+#endif  // defined(OS_MACOSX)
 
   host_->Start(host_owner_);
 
