b2db7499afe94eb68319e9789a83e60f991dbc9f
Yuki Shiino <yukishiino@chromium.org>
2019-10-18 15:16:05 +0000

    bindings: Fix breakage caused by removal of TrustedURL
    I guess that this breakage was caused by
    https://crrev.com/c/1834142 or related patches.
    
    Change-Id: I1b93bd71743474432de38e86e6384f4b76b575eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869230
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707374}

--- third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_document.h
+++ third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_document.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/generated_code_helper.h"
 #include "third_party/blink/renderer/bindings/core/v8/native_value_traits.h"
 #include "third_party/blink/renderer/bindings/core/v8/to_v8_for_core.h"
-#include "third_party/blink/renderer/bindings/core/v8/usv_string_or_trusted_url.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_document.h"
 #include "third_party/blink/renderer/bindings/tests/idls/core/test_interface_document.h"
