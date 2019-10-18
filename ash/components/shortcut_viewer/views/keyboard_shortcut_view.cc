b989826432b9617a8e6c21ad36e9f8fbd88ac221
Peter Kasting <pkasting@chromium.org>
2019-10-18 05:07:30 +0000

    Use InvalidateLayout() more correctly for the KeyboardShortcutView/TabbedPane.
    Hopefully prevents cached stale layouts.
    
    Bug: 1012324
    Change-Id: I9d7dc99890a78d6179a5347dd29ec8c9eba44953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867480
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707252}

--- ash/components/shortcut_viewer/views/keyboard_shortcut_view.cc
+++ ash/components/shortcut_viewer/views/keyboard_shortcut_view.cc
@@ -470,13 +470,12 @@ void KeyboardShortcutView::InitCategoriesTabbedPane(
     item_list_view->AddChildView(item_view.get());
     shortcut_items.emplace_back(item_view.get());
     // Remove the search query highlight.
-    description_label_view->Layout();
+    description_label_view->InvalidateLayout();
   }
   // Update node data for the last category.
   UpdateAXNodeDataPosition(shortcut_items);
 
-  tab_contents->Layout();
-  Layout();
+  tab_contents->InvalidateLayout();
 }
 
 void KeyboardShortcutView::UpdateViewsLayout(bool is_search_box_active) {
@@ -502,8 +501,7 @@ void KeyboardShortcutView::UpdateViewsLayout(bool is_search_box_active) {
   }
   categories_tabbed_pane_->SetVisible(!should_show_search_results);
   search_results_container_->SetVisible(should_show_search_results);
-  Layout();
-  SchedulePaint();
+  InvalidateLayout();
 }
 
 void KeyboardShortcutView::ShowSearchResults(
@@ -551,7 +549,7 @@ void KeyboardShortcutView::ShowSearchResults(
         description_label_view->AddStyleRange(
             gfx::Range(match_index, match_index + match_length), style);
         // Apply new styles to highlight matched search query.
-        description_label_view->Layout();
+        description_label_view->InvalidateLayout();
       }
 
       found_items_list_view->AddChildView(item_view.get());
@@ -582,8 +580,7 @@ void KeyboardShortcutView::ShowSearchResults(
           : IDS_KSV_SEARCH_BOX_ACCESSIBILITY_VALUE_WITH_RESULTS,
       replacement_strings, nullptr));
   search_results_container_->AddChildView(search_container_content_view);
-  Layout();
-  SchedulePaint();
+  InvalidateLayout();
 }
 
 bool KeyboardShortcutView::CanMaximize() const {
