92f282dd4e4345b996742f165e126cdb301e2f60
Yutaka Hirano <yhirano@chromium.org>
2019-10-19 07:44:28 +0000

    [Extension] Allow "extraHeaders" for onBeforeRequest and onErrorOccured
    We dispatch CORS preflight events only when there is an event listener
    with "extraHeaders" specified. Hence some extension developers may
    want to specify that option for onBeforeReuqest and onErrorOcurred.
    
    Bug: 1002884
    Change-Id: I7006f9b30c94858427b66aa2fd93d82e4f8f4c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866374
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707642}

--- chrome/test/data/extensions/api_test/webrequest/test_cors.js
+++ chrome/test/data/extensions/api_test/webrequest/test_cors.js
@@ -518,11 +518,6 @@ function registerPreflightBlockingListener() {
 function registerPreflightRedirectingListener() {
   const url = getServerURL(BASE + 'accept', 'cors.example.com');
 
-  // This is needed to observe preflight requests when OOR-CORS is enabled.
-  chrome.webRequest.onHeadersReceived.addListener(
-      () => {},
-      {urls: [url]}, ['extraHeaders']);
-
   const onBeforeRequestCalledForPreflight = callbackPass(() => {});
   chrome.webRequest.onBeforeRequest.addListener(
       function onBeforeRequest(details) {
@@ -534,7 +529,7 @@ function registerPreflightRedirectingListener() {
           }, 0);
           return {redirectUrl: url + '?redirected'};
         }
-      }, {urls: [url]}, ['blocking']);
+      }, {urls: [url]}, ['blocking', 'extraHeaders']);
 
   if (getCorsMode() == 'network_service') {
     // When CORS is implemented in the network service, we see failures on both
@@ -560,6 +555,23 @@ function registerPreflightRedirectingListener() {
     // See https://crbug.com/1014816.
   }
 }
+
+function registerOnBeforeRequestAndOnErrorOcurredListeners() {
+  const url = getServerURL(BASE + 'accept', 'cors.example.com');
+
+  const onBeforeRequestCalledForPreflight = callbackPass(() => {});
+  // onBeforeRequest doesn't have "extraHeaders", but it sees a preflight
+  // even when OOR-CORS is enabled, because onErrorOccurred has "extraHeaders".
+  chrome.webRequest.onBeforeRequest.addListener((details) => {
+    if (details.method === 'OPTIONS') {
+      onBeforeRequestCalledForPreflight();
+    }
+  }, {urls: [url]});
+
+  chrome.webRequest.onErrorOccurred.addListener(() => {
+  }, {urls: [url]}, ['extraHeaders']);
+}
+
 
 runTests([
   function testOriginHeader() {
@@ -637,4 +649,9 @@ runTests([
     navigateAndWait(getServerURL(
         BASE + 'fetch.html?path=accept&with-preflight'));
   },
+  function testCorsPreflightIsObservableWhenAnyListenerHasExtraHeaders() {
+    registerOnBeforeRequestAndOnErrorOcurredListeners();
+    navigateAndWait(getServerURL(
+        BASE + 'fetch.html?path=accept&with-preflight'));
+  }
 ]);
