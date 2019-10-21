fcfec8bcdaeac5101abe82cd397f2dbf9395792f
Jeroen Dhollander <jeroendh@google.com>
2019-10-21 21:52:22 +0000

    Fix DCHECK when closing Embedded Assistant UI
    This DCHECK was caused because the Embedded Assistant UI tried to grab
    the focus while it was being closed.
    
    This |RequestFocus| call was added explicitly to fix a bug where the
    input text field in the Assistant UI would lose focus when submitting
    a query while in tablet mode.
    
    And this was happening because we tried to dismiss the virtual on-screen
    keyboard.
    
    I added unittests to test for these 3 scenarios, and I fixed it by dismissing
    the virtual keyboard using a |HideKeyboard| method instead of dismissing it
    by dropping the focus of the text field.
    
    Bug: b/141945964
    Change-Id: I3aca9e08ab0e2b7419cfe53c1f4dc831130cd0df
    Tests: Added unittests and verified manually
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1848796
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707983}

--- ash/app_list/views/assistant/assistant_page_view.cc
+++ ash/app_list/views/assistant/assistant_page_view.cc
@@ -155,10 +155,6 @@ void AssistantPageView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
 void AssistantPageView::ChildPreferredSizeChanged(views::View* child) {
   MaybeUpdateAppListState(child->GetHeightForWidth(width()));
   PreferredSizeChanged();
-
-  // After layout events, focus can be lost so we need to explicitly request
-  // on behalf of the child views.
-  RequestFocus();
 }
 
 void AssistantPageView::ChildVisibilityChanged(views::View* child) {
@@ -295,7 +291,7 @@ void AssistantPageView::OnUiVisibilityChanged(
   }
 }
 
-const AssistantMainView* AssistantPageView::GetMainViewForTest() const {
+AssistantMainView* AssistantPageView::GetMainViewForTest() {
   return assistant_main_view_;
 }
 
--- ash/assistant/ui/main_stage/assistant_progress_indicator.cc
+++ ash/assistant/ui/main_stage/assistant_progress_indicator.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "ui/compositor/layer_animation_element.h"
 #include "ui/compositor/layer_animator.h"
+#include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
 #include "ui/views/background.h"
@@ -48,6 +49,11 @@ int GetDotSpacingDip() {
   return app_list_features::IsEmbeddedAssistantUIEnabled()
              ? kEmbeddedUiSpacingDip
              : kSpacingDip;
+}
+
+bool AreAnimationsEnabled() {
+  return ui::ScopedAnimationDurationScaleMode::duration_scale_mode() !=
+         ui::ScopedAnimationDurationScaleMode::ZERO_DURATION;
 }
 
 // DotBackground ---------------------------------------------------------------
@@ -142,6 +148,12 @@ void AssistantProgressIndicator::VisibilityChanged(views::View* starting_from,
   const float scale_factor = GetDotLargeSizeDip() / GetDotSmallSizeDip();
   transform.Translate(translation_dip, translation_dip);
   transform.Scale(scale_factor, scale_factor);
+
+  // Don't animate if animations are disabled (during unittests).
+  // Otherwise we get in an infinite loop due to the cyclic animation used here
+  // repeating over and over without pause.
+  if (!AreAnimationsEnabled())
+    return;
 
   base::TimeDelta start_offset;
   for (auto* child : children()) {
