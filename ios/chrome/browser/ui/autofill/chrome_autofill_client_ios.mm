c455a574338ce3e8968a6cc7efdaef7509a1f459
sczs <sczs@chromium.org>
2019-10-21 17:31:08 +0000

    [ios] Uses SaveCard Messages UI if flag is enabled.
    If IsSaveCardInfobarMessagesUIEnabled() is enabled then a
    InfobarSaveCardCoordinator will be used to create the SaveCard InfoBarIOS
    object. This will cause SaveCard to be presented using the new Messages UI.
    
    Bug: 1014652
    Change-Id: Ib3aae8939bf2a8720938dee4484adfbd8cf3c5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869712
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707866}

--- ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
+++ ios/chrome/browser/ui/autofill/chrome_autofill_client_ios.mm
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/autofill/card_name_fix_flow_view_bridge.h"
 #include "ios/chrome/browser/ui/autofill/card_unmask_prompt_view_bridge.h"
 #include "ios/chrome/browser/ui/autofill/save_card_infobar_controller.h"
+#import "ios/chrome/browser/ui/infobars/coordinators/infobar_save_card_coordinator.h"
+#import "ios/chrome/browser/ui/infobars/infobar_feature.h"
 #include "ios/chrome/browser/webdata_services/web_data_service_factory.h"
 #include "ios/chrome/common/channel_info.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
@@ -57,9 +59,16 @@ namespace {
 // Creates and returns an infobar for saving credit cards.
 std::unique_ptr<infobars::InfoBar> CreateSaveCardInfoBarMobile(
     std::unique_ptr<autofill::AutofillSaveCardInfoBarDelegateMobile> delegate) {
-  SaveCardInfoBarController* controller = [[SaveCardInfoBarController alloc]
-      initWithInfoBarDelegate:delegate.get()];
-  return std::make_unique<InfoBarIOS>(controller, std::move(delegate));
+  if (IsSaveCardInfobarMessagesUIEnabled()) {
+    InfobarSaveCardCoordinator* coordinator =
+        [[InfobarSaveCardCoordinator alloc]
+            initWithInfoBarDelegate:delegate.get()];
+    return std::make_unique<InfoBarIOS>(coordinator, std::move(delegate));
+  } else {
+    SaveCardInfoBarController* controller = [[SaveCardInfoBarController alloc]
+        initWithInfoBarDelegate:delegate.get()];
+    return std::make_unique<InfoBarIOS>(controller, std::move(delegate));
+  }
 }
 
 }  // namespace
