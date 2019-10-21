57a72a9bc23dcad2ca9b372a041d8c841ecdfe9e
Elly Fong-Jones <ellyjones@chromium.org>
2019-10-21 19:54:45 +0000

    cbui: fold ExternalProtocol delegates together
    This layer of delegation originally existed to allow for both Cocoa and Views
    versions of this dialog to share logic. Since the Cocoa versions are now gone,
    the remaining logic can be folded into the Views versions.
    
    This change:
    1) Adds ExternalProtocolHandler::Delegate::OnSetBlockState, to allow tests to
       listen directly for block state changes;
    2) Folds the body of ExternalProtocolDialogDelegate into ExternalProtocolDialog;
    3) Removes ProtocolDialog (interface only) and ExternalProtocolDialog (interface
       & implementation)
    4) Reworks the ExternalProtocolDialogDelegate browser tests to test "one layer
       up" - instead of using a test delegate for the dialog, they use the normal
       production behavior of the dialog, but hook into the ExternalProtocolHandler
       system at the ExternalProtocolHandler::Delegate layer. This provides better
       fidelity to the production behavior in the test and simplifies the tests
       considerably.
    
    Next steps here:
    As the ongoing DialogDelegate refactor proceeds (see https://crbug.com/1011446),
    ExternalProtocolDialog will shrink further until it contains only Accept and
    Cancel.
    
    Bug: None
    Change-Id: I90518f8086db4127d41776ad086210c6db9d7462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872411
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707922}

--- chrome/browser/ui/content_settings/content_setting_bubble_model.cc
+++ chrome/browser/ui/content_settings/content_setting_bubble_model.cc
@@ -41,7 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/blocked_content/popup_blocker_tab_helper.h"
 #include "chrome/browser/ui/collected_cookies_infobar_delegate.h"
 #include "chrome/browser/ui/content_settings/content_setting_bubble_model_delegate.h"
-#include "chrome/browser/ui/external_protocol_dialog_delegate.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/content_settings_renderer.mojom.h"
