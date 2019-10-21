d314c6153ff18d110fe075eaa4f2ddf138f7b59f
Anastasia Helfinstein <anastasi@google.com>
2019-10-21 20:53:59 +0000

    Update expiring accessibility flags
    There are 7 flags owned by the Accessibility team that expired before
    M79. Of these, two are unused and being removed; three are still under
    active development and are being extended substantially; and two still
    contain code that needs to be launched and/or cleaned up, and are being
    extended through M79 to allow a few weeks to wrap up the current state.
    
    Bug: 1016510
    Change-Id: If6160da6897dff5f1af282f2c92e4cafc60aa40a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863470
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#707951}

--- ui/accessibility/accessibility_switches.cc
+++ ui/accessibility/accessibility_switches.cc
@@ -47,11 +47,6 @@ const char kEnableExperimentalAccessibilityChromeVoxSubNodeLanguageSwitching[] =
     "enable-experimental-accessibility-chromevox-sub-node-language-"
     "switching";
 
-bool AreExperimentalAccessibilityFeaturesEnabled() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      ::switches::kEnableExperimentalAccessibilityFeatures);
-}
-
 bool IsExperimentalAccessibilityLanguageDetectionEnabled() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       ::switches::kEnableExperimentalAccessibilityLanguageDetection);
