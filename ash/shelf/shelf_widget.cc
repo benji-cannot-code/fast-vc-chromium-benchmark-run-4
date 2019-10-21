ac49872f8d548fdebfcbbcd7529ab7ec49644b0a
Sammie Quon <sammiequon@google.com>
2019-10-21 23:30:54 +0000

    shelf: Remove uneeded create render cache.
    It seems the opaque background has no children anymore, and repaints
    afterwards, so I don't think a render cache is needed.
    
    Test: none
    Bug: 1015884
    Change-Id: I5f99340f177d8a655f231d3565c972fee337f062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868916
    Reviewed-by: Mitsuru Oshima (slow) <oshima@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708020}

--- ash/shelf/shelf_widget.cc
+++ ash/shelf/shelf_widget.cc
@@ -258,7 +258,6 @@ void ShelfWidget::DelegateView::UpdateOpaqueBackground() {
         shelf->SelectValueForShelfAlignment(0, radius, 0),
         shelf->SelectValueForShelfAlignment(0, 0, radius),
     });
-    opaque_background_.AddCacheRenderSurfaceRequest();
   }
   opaque_background_.SetBounds(opaque_background_bounds);
   UpdateDragHandle();
