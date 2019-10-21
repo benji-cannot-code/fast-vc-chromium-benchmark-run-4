015fd9592af139e98841669581b4bbfb32a7b6e2
Olga Gerchikov <gerchiko@microsoft.com>
2019-10-21 19:25:01 +0000

    [Reland] Composited Worklet Animations: Fixed updating local time on main thread.
    The original CL caused flakiness of /virtual/threaded/animations/animationworklet* tests
    (crbug.com/1014966). The reason for the flakiness is change in timing of main thread effect local
    time update.
    
    Reverted CL: https://chromium-review.googlesource.com/c/chromium/src/+/1826872
    The reverted CL is in Patchset 1.
    Patchset 2 fixes the tests by explicitly waiting for the effects to change.
    
    This CL changes how local time of composited worklet animations is updated on main thread:
    instead of polling local time on main thread, the local time is posted via animation events
    by the composited thread to the main thread.
    
    Design document: https://docs.google.com/document/d/1vUxaKBLDHQv9ACJUQJH4RXjuQTlWIsSiAFYkMrjccyc/edit?usp=sharing
    
    Changes made:
    - Expanded AnimationEvent to support worklet animation time update event type.
    - Reused existing mechanism of posting animation events to the main thread to support new
      event type.
    - Removed "peek" logic.
    - Enabled tests that previously failed.
    
    Bug: 943787
    Change-Id: I9a2965f204cad15b405be82305c38788c0eafd62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864518
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Olga Gerchikov <gerchiko@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#707904}

--- cc/test/animation_timelines_test_common.cc
+++ cc/test/animation_timelines_test_common.cc
@@ -406,6 +406,9 @@ void TestAnimationDelegate::NotifyAnimationTakeover(
   takeover_ = true;
 }
 
+void TestAnimationDelegate::NotifyLocalTimeUpdated(
+    base::Optional<base::TimeDelta> local_time) {}
+
 AnimationTimelinesTest::AnimationTimelinesTest()
     : client_(ThreadInstance::MAIN),
       client_impl_(ThreadInstance::IMPL),
