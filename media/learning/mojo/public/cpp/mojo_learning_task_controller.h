e480b47ebf07552100ca195659c234484c27eaed
liberato@chromium.org <liberato@chromium.org>
2019-10-21 21:39:41 +0000

    Add LearningTaskController::UpdateDefaultTarget
    UpdateDefaultTarget can be used to update the default target value
    that will be used to complete the observation if the controller
    is destroyed before CompleteObservation is called.  A default value
    may be added, changed, or removed.
    
    Change-Id: I700ad31cb56e60a15e31c43367377336f4c1ad74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863593
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707972}

--- media/learning/mojo/public/cpp/mojo_learning_task_controller.h
+++ media/learning/mojo/public/cpp/mojo_learning_task_controller.h
@@ -37,6 +37,9 @@ class COMPONENT_EXPORT(MEDIA_LEARNING_MOJO) MojoLearningTaskController
   void CompleteObservation(base::UnguessableToken id,
                            const ObservationCompletion& completion) override;
   void CancelObservation(base::UnguessableToken id) override;
+  void UpdateDefaultTarget(
+      base::UnguessableToken id,
+      const base::Optional<TargetValue>& default_target) override;
   const LearningTask& GetLearningTask() override;
 
  private:
