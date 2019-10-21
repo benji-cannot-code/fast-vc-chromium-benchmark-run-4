752e295effdda395b2a60dd5b853e8aca02f6feb
Maksim Moskvitin <mmoskvitin@google.com>
2019-10-21 14:37:10 +0000

    Record SCRYPT-related histograms for USS Nigori
    This CL adds recording for some missed histograms:
     CustomPassphraseKeyDerivationMethodStateOnStartup
     CustomPassphraseKeyDerivationMethodOnNewPassphrase
     CustomPassphraseKeyDerivationMethodOnSuccessfulDecryption
    
    Bug: 922900
    Change-Id: I9e05b771a22c3480413d32788a2a59bd320bfa1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871549
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707803}

--- components/sync/nigori/pending_local_nigori_commit.h
+++ components/sync/nigori/pending_local_nigori_commit.h
@@ -15,7 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace syncer {
 
+class KeyDerivationParams;
 struct NigoriState;
+
+KeyDerivationParams CreateKeyDerivationParamsForCustomPassphrase(
+    const base::RepeatingCallback<std::string()>& random_salt_generator);
 
 // Interface representing an intended local change to the Nigori state that
 // is pending a commit to the sync server.
