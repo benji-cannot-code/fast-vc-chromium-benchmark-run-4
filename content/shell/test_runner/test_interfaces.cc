b08886093a68e25957ba1fd867b9d81021e2d1f0
Kent Tamura <tkent@chromium.org>
2019-10-21 04:58:40 +0000

    Fix use-after-free of TestRunner::main_view_
    test_runner::TestRunner::WorkQueue::ProcessWork() can run after the
    main WebView destruction, and we have to clear main_view_ when the
    WebView is destructed.
    
    The code assumed TestInterfaces::WindowClosed() cleared main_view_
    if the closing WebView is the main WebView. However,
    |proxy->webview()| is already null here because WebView is destructed
    before calling WindowClosed().
    
    This CL adds SetMainView(nullptr) to ~WebViewTestProxy(). It is called
    if the global delegate is same as the delegate owned by the
    WebViewTestProxy. This condition is equivalent to 'this
    WebViewTestProxy is the owner of the main WebView.'
    
    Bug: 1010581
    Change-Id: I868ce9223d253dbf02620fdebff8ada4a51c60d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865734
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707717}

--- content/shell/test_runner/test_interfaces.cc
+++ content/shell/test_runner/test_interfaces.cc
@@ -138,9 +138,6 @@ void TestInterfaces::WindowClosed(WebViewTestProxy* proxy) {
     return;
   }
   window_list_.erase(pos);
-
-  if (proxy->webview() == main_view_)
-    SetMainView(nullptr);
 }
 
 TestRunner* TestInterfaces::GetTestRunner() {
