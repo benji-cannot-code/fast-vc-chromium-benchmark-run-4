461008a4b973bf743d34459b896a25b20c7769d0
Benjamin Beaudry <bebeaudr@microsoft.com>
2019-10-21 15:30:12 +0000

    Fix wrong position created by AXPosition::ToPositionWithAffinity()
    When calling AXPosition::ToPositionWithAffinity() on a position with a
    |container_object_| of type AXInlineTextBox, the position returned is
    always in the parent node, the StaticText object. Think of it as
    a modification from a relative position to an absolute position, based
    in the StaticText object.
    
    However, when an InlineTextBox object has siblings (i.e. the parent
    node has multiple InlineTextBox children), the position returned is
    only valid for the first non-empty child. This bug was due to the fact
    that we wrongfully assumed that the text offset in the child object
    would be the same as the text offset in the parent position. This is
    only true for the first non-empty child.
    
    This CL introduces a fix for this bug. Instead of creating a position
    in the parent node at the exact same offset, we add to it the sum of
    the previous siblings MaxTextOffset().
    It also adds a unit test for this case.
    
    Bug: 928948
    Change-Id: I1b52c7506e726956bca8b2d41d5a33bee3a84597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1846563
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#707817}

--- third_party/blink/renderer/core/layout/line/abstract_inline_text_box.h
+++ third_party/blink/renderer/core/layout/line/abstract_inline_text_box.h
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class InlineTextBox;
+class NGOffsetMapping;
 
 // High-level abstraction of InlineTextBox to allow the accessibility module to
 // get information about InlineTextBoxes without tight coupling.
@@ -67,6 +68,7 @@ class CORE_EXPORT AbstractInlineTextBox
   virtual scoped_refptr<AbstractInlineTextBox> NextInlineTextBox() const = 0;
   virtual LayoutRect LocalBounds() const = 0;
   virtual unsigned Len() const = 0;
+  virtual unsigned TextOffsetInContainer(unsigned) const = 0;
   virtual Direction GetDirection() const = 0;
   Node* GetNode() const;
   virtual void CharacterWidths(Vector<float>&) const = 0;
@@ -113,6 +115,7 @@ class CORE_EXPORT LegacyAbstractInlineTextBox final
   scoped_refptr<AbstractInlineTextBox> NextInlineTextBox() const final;
   LayoutRect LocalBounds() const final;
   unsigned Len() const final;
+  unsigned TextOffsetInContainer(unsigned offset) const final;
   Direction GetDirection() const final;
   void CharacterWidths(Vector<float>&) const final;
   String GetText() const final;
@@ -121,6 +124,7 @@ class CORE_EXPORT LegacyAbstractInlineTextBox final
   scoped_refptr<AbstractInlineTextBox> NextOnLine() const final;
   scoped_refptr<AbstractInlineTextBox> PreviousOnLine() const final;
   bool IsLineBreak() const final;
+  const NGOffsetMapping* GetOffsetMapping() const;
 
   InlineTextBox* inline_text_box_;
 
