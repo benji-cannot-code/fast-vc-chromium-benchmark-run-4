01910f98d6a7489c59ef4adccd0e66ce4d850996
Manu Cornet <manucornet@chromium.org>
2019-10-21 21:42:44 +0000

    CrOS Shelf: Hotseat positioning in overview
    Extend the hotseat in overview mode, and hide it in split view mode.
    
    Bug: 1009550, 1009716
    Change-Id: I2494c07a2d9bf486ffd4be5a0da4779c855cd59e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872189
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707976}

--- ash/shelf/shelf_layout_manager_unittest.cc
+++ ash/shelf/shelf_layout_manager_unittest.cc
@@ -3488,7 +3488,7 @@ TEST_P(HotseatShelfLayoutManagerTest, ReleasingSlowDragAboveThreshold) {
 }
 
 // Tests that showing overview after showing the hotseat results in only one
-// animation, to |kShown|.
+// animation, to |kExtended|.
 TEST_P(HotseatShelfLayoutManagerTest, ShowingOverviewFromShownAnimatesOnce) {
   GetPrimaryShelf()->SetAutoHideBehavior(GetParam());
   TabletModeControllerTestApi().EnterTabletMode();
@@ -3509,7 +3509,7 @@ TEST_P(HotseatShelfLayoutManagerTest, ShowingOverviewFromShownAnimatesOnce) {
                                                 .CenterPoint();
   GetEventGenerator()->GestureTapAt(overview_button_center);
 
-  state_watcher_->CheckEqual({HotseatState::kExtended, HotseatState::kShown});
+  state_watcher_->CheckEqual({HotseatState::kExtended});
 }
 
 // Tests that the hotseat is not flush with the bottom of the screen when home
