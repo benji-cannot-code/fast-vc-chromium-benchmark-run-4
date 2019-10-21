ff088603bc0d8c72d2bbb83683c1329c48b1682a
Philip Rogers <pdr@chromium.org>
2019-10-21 17:48:57 +0000

    Exclude the heads up display (hud) layer from inspector 3d layers view
    The heads up display (hud) layer is used to show layer overlay
    information such as the fps meter and repaint rects. This layer should
    be excluded from the 3d layers view. This code has not been implemented
    behind IsInspectorLayer because the hud layer is used for more than just
    the inspector, such as when --show-paint-rects is used.
    
    This has been tested manually by ensuring no extra layer is present.
    
    Bug: 1015918
    Change-Id: I044ad1c7dc61385bdeb0fae916c7515dbd8aee4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869820
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707875}

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -1520,6 +1520,10 @@ void LayerTreeHost::UpdateHudLayer(bool show_hud_info) {
   }
 }
 
+bool LayerTreeHost::is_hud_layer(const Layer* layer) const {
+  return hud_layer() == layer;
+}
+
 void LayerTreeHost::SetNeedsFullTreeSync() {
   needs_full_tree_sync_ = true;
   property_trees_.needs_rebuild = true;
