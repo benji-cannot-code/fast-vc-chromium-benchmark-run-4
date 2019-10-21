43661cd7c7462a470492c327bd6c3015fdefbd46
Ken Rockot <rockot@google.com>
2019-10-21 23:36:07 +0000

    [mojo] Better sync behavior for SharedRemote
    This CL changes SharedRemote so that its sync calls now block only the
    calling sequence rather than additionally blocking the underlying
    Remote's bound sequence.
    
    Fixed: 1016022
    Change-Id: I2483c130d882de24686c0bb1396a894171a733fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871249
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Darin Fisher <darin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708028}

--- mojo/public/cpp/bindings/tests/bindings_test_base.h
+++ mojo/public/cpp/bindings/tests/bindings_test_base.h
@@ -35,6 +35,9 @@ class BindingsTestBase
   // Helper which other test fixtures can use.
   static void SetupSerializationBehavior(BindingsTestSerializationMode mode);
 
+ protected:
+  base::test::TaskEnvironment* task_environment() { return &task_environment_; }
+
  private:
   base::test::TaskEnvironment task_environment_;
 };
