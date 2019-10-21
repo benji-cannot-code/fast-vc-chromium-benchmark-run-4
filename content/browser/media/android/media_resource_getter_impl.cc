1b0bbdbd1dba5dfbee56ea1c59c14c2cae14b697
Matt Menke <mmenke@chromium.org>
2019-10-21 17:41:40 +0000

    Add NetworkIsolationKey to NetworkContext::LookupBasicAuthCredentials
    HTTP auth credentials will need to be separated by NetworkIsolationKey,
    as otherwise they can be used much like a cross-site cookie.
    
    Also rename the method to LookupServerBasicAuthCredentials, as it no
    longer returns auth credentials for proxies (That change was in an
    earlier CL).
    
    The one consumer of this API (MediaResourceGetterImpl) still needs to
    be updated to get the right NetworkIsolationKey. Figuring out how to
    do this may take some work, so this is not done in this CL, though it
    will need to be resolved before we can enable separating the auth cache
    by NetworkIsolationKey, at least on Android.
    
    Bug: 993806
    Change-Id: Ifc3eec8a4cdd2f38112116c0dc9d7e7c5c296566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865406
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707873}

--- content/browser/media/android/media_resource_getter_impl.cc
+++ content/browser/media/android/media_resource_getter_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/fileapi/browser_file_system_helper.h"
+#include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/android/media_url_interceptor.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "net/base/auth.h"
+#include "net/base/network_isolation_key.h"
 #include "net/http/http_auth.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/restricted_cookie_manager.mojom.h"
@@ -124,10 +126,19 @@ void MediaResourceGetterImpl::GetAuthCredentials(
     return;
   }
 
+  RenderFrameHostImpl* render_frame_host =
+      RenderFrameHostImpl::FromID(render_process_id_, render_frame_id_);
+  // Can't get a NetworkIsolationKey to get credentials if the RenderFrameHost
+  // has already been destroyed.
+  if (!render_frame_host) {
+    GetAuthCredentialsCallback(std::move(callback), base::nullopt);
+    return;
+  }
+
   BrowserContext::GetDefaultStoragePartition(browser_context_)
       ->GetNetworkContext()
-      ->LookupBasicAuthCredentials(
-          url,
+      ->LookupServerBasicAuthCredentials(
+          url, render_frame_host->network_isolation_key(),
           base::BindOnce(&MediaResourceGetterImpl::GetAuthCredentialsCallback,
                          weak_factory_.GetWeakPtr(), std::move(callback)));
 }
