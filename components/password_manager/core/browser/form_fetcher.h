31ba1ac5be0557f654f891a9aa52276501f4cee0
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-10-21 13:08:25 +0000

    [Passwords] Use base::ObserverList in FormFetcher
    In order to be more robust against changes to the observer list, this
    change modifies FormFetcher to use a base::ObserverList instead of a
    simple set and makes its consumers inherit from base::CheckedObserver.
    
    This allows consumers to delete the form fetcher while being notified,
    which previously would cause a use-after-free. A corresponding test is
    added.
    
    Bug: 1007556
    Change-Id: I645526027e35be222479009199af6545de6a11f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1855985
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#707783}

--- components/password_manager/core/browser/form_fetcher.h
+++ components/password_manager/core/browser/form_fetcher.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/observer_list_types.h"
 #include "base/strings/string16.h"
 
 namespace autofill {
@@ -34,10 +35,8 @@ class FormFetcher {
   enum class State { WAITING, NOT_WAITING };
 
   // API to be implemented by classes which want the results from FormFetcher.
-  class Consumer {
+  class Consumer : public base::CheckedObserver {
    public:
-    virtual ~Consumer() = default;
-
     // FormFetcher calls this method every time the state changes from WAITING
     // to NOT_WAITING. It is now safe for consumers to call the accessor
     // functions for matches.
--- components/password_manager/core/browser/form_fetcher_impl.h
+++ components/password_manager/core/browser/form_fetcher_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/observer_list.h"
 #include "components/password_manager/core/browser/form_fetcher.h"
 #include "components/password_manager/core/browser/http_password_store_migrator.h"
 #include "components/password_manager/core/browser/password_store.h"
@@ -129,8 +130,9 @@ class FormFetcherImpl : public FormFetcher,
   // Statistics for the current domain.
   std::vector<InteractionsStats> interactions_stats_;
 
-  // Consumers of the fetcher, all are assumed to outlive |this|.
-  std::set<FormFetcher::Consumer*> consumers_;
+  // Consumers of the fetcher, all are assumed to either outlive |this| or
+  // remove themselves from the list during their destruction.
+  base::ObserverList<FormFetcher::Consumer> consumers_;
 
   // Indicates whether HTTP passwords should be migrated to HTTPS.
   const bool should_migrate_http_passwords_;
