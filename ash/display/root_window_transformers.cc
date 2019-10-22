c442d8772c19d5d9f478618d7912f6a06b2a7f6b
Xiyuan Xia <xiyuan@chromium.org>
2019-10-22 17:32:49 +0000

    ash: Fix unified desktop regression
    Regressed in https://crrev.com/c/1825892 that uses a wrong transform
    in PartialBoundsRootWindowTransformer.
    
    Bug: 1016565
    Change-Id: I56febc2515f2abe26853db6292064de27d3a4e81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1873909
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708239}

--- ash/display/root_window_transformers.cc
+++ ash/display/root_window_transformers.cc
@@ -311,7 +311,7 @@ class PartialBoundsRootWindowTransformer : public RootWindowTransformer {
   }
   gfx::Insets GetHostInsets() const override { return gfx::Insets(); }
   gfx::Transform GetInsetsAndScaleTransform() const override {
-    return gfx::Transform();
+    return transform_;
   }
 
  private:
