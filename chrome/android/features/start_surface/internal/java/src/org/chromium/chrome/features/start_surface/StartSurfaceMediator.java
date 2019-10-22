a26d7d1ca4909ae18f16abcc1d2872707974daf7
gogerald <gogerald@google.com>
2019-10-22 21:22:56 +0000

    [StartSurface] Add StartSurface.TasksOnly user action
    Bug: 1016341
    Change-Id: I22bd90d258eacf5398e87c16fb450e84986c08f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872315
    Auto-Submit: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708360}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
@@ -250,6 +250,8 @@ class StartSurfaceMediator
                         : "HomeSurface";
                 RecordUserAction.record(
                         "StartSurface.TwoPanes.DefaultOn" + defaultOnUserActionString);
+            } else if (mSurfaceMode == SurfaceMode.TASKS_ONLY) {
+                RecordUserAction.record("StartSurface.TasksOnly");
             }
 
             // Make sure FeedSurfaceCoordinator is built before the explore surface is showing by
