99594b243a0d5cd8c232c1851e3f7e6ffe15e7ad
siyua <siyua@chromium.org>
2019-10-22 19:45:58 +0000

    [Autofill Auth UI] Add WebAuthn verify pending dialog
    Logic for the dialog:
    1. Show dialog when user has opted in, and after card verification
    starts.
    2. If card is not eligible for fido, fall back to CVC, close the dialog.
    Card unmask prompt will be shown next.
    3. If card is eligible for fido, close the dialog after card information
    is filled to the form.
    
    Uploaded screenshot in bug comment 21
    
    Bug: 991037
    Change-Id: Ibb32a03c491039664b0875ea40cc750f304e21b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834822
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Manas Verma <manasverma@google.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708307}

---
+++ chrome/browser/ui/autofill/payments/verify_pending_dialog_view.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_VERIFY_PENDING_DIALOG_VIEW_H_
+#define CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_VERIFY_PENDING_DIALOG_VIEW_H_
+
+namespace content {
+class WebContents;
+}
+
+namespace autofill {
+
+class VerifyPendingDialogController;
+
+// The dialog to show card verification is in progress.
+class VerifyPendingDialogView {
+ public:
+  // Factory function implemented by dialog's view implementation.
+  static VerifyPendingDialogView* CreateDialogAndShow(
+      VerifyPendingDialogController* controller,
+      content::WebContents* web_contents);
+
+  // Close the dialog and prevent callbacks being invoked.
+  virtual void Hide() = 0;
+};
+
+}  // namespace autofill
+
+#endif  // CHROME_BROWSER_UI_AUTOFILL_PAYMENTS_VERIFY_PENDING_DIALOG_VIEW_H_
--- ui/views/window/dialog_delegate.h
+++ ui/views/window/dialog_delegate.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+class BubbleFrameView;
 class DialogClientView;
 class DialogObserver;
 class LabelButton;
@@ -155,6 +156,10 @@ class VIEWS_EXPORT DialogDelegate : public WidgetDelegate {
   // delegate's Window.
   const DialogClientView* GetDialogClientView() const;
   DialogClientView* GetDialogClientView();
+
+  // Returns the BubbleFrameView of this dialog delegate. A bubble frame view
+  // will only be created when use_custom_frame() is true.
+  BubbleFrameView* GetBubbleFrameView() const;
 
   // Helpers for accessing parts of the DialogClientView without needing to know
   // about DialogClientView. Do not call these before OnDialogInitialized.
