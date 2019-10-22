8e8df7ee06fc46a976ce9fadc5197915d382f318
Tommy Li <tommycli@chromium.org>
2019-10-22 19:34:23 +0000

    [omnibox] Remove Confirmation Dialog for Remove Suggestion
    No one likes the current confirmation dialog. Begin experimentation
    without one.
    
    This CL puts in a new flag that can swap on and off the confirm dialog.
    The flag is set up so that there is NO confirm dialog by default.
    This is the same as how the NTP version: ConfirmNtpSuggestionRemovals.
    
    Bug: 1016514, 929477, 1205
    Change-Id: If158e10da3fbdccc847aafe5221fad57c5c3639b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872182
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708304}

--- components/omnibox/common/omnibox_features.h
+++ components/omnibox/common/omnibox_features.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace omnibox {
 
-// TODO(dbeam): why is this list not sorted alphabetically?
+// Please do not add more features to this "big blob" list.
+// Instead, use the categorized and alphabetized lists below this "big blob".
+// You can create a new category if none of the existing ones fit.
 extern const base::Feature kHideFileUrlScheme;
 extern const base::Feature kHideSteadyStateUrlScheme;
 extern const base::Feature kHideSteadyStateUrlTrivialSubdomains;
@@ -58,6 +60,9 @@ extern const base::Feature kOnFocusSuggestions;
 extern const base::Feature kZeroSuggestionsOnNTP;
 extern const base::Feature kZeroSuggestionsOnNTPRealbox;
 extern const base::Feature kZeroSuggestionsOnSERP;
+
+// Suggestions UI - these affect the UI or function of the suggestions popup.
+extern const base::Feature kConfirmOmniboxSuggestionRemovals;
 
 }  // namespace omnibox
 
