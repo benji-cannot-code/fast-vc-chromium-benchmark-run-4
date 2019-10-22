fa598f477d4296a7db3ba78f3d3c544dbcb1ea12
Tom Anderson <thomasanderson@chromium.org>
2019-10-22 23:04:57 +0000

    Fix missing parens in ConvertVideoFrameToRGBPixelsTask
    BUG=1016035
    R=dalecurtis
    
    Change-Id: I3c12fe8c339ab326d910699739e289b5e9aaa343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872724
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708399}

--- media/renderers/paint_canvas_video_renderer.cc
+++ media/renderers/paint_canvas_video_renderer.cc
@@ -434,7 +434,7 @@ void ConvertVideoFrameToRGBPixelsTask(const VideoFrame* video_frame,
       const uint8_t* data = video_frame->visible_data(plane);
       int rows = video_frame->rows(plane);
       meta.data =
-          data + meta.stride * chunk_start * rows_per_chunk * rows / height;
+          data + meta.stride * (chunk_start * rows_per_chunk * rows / height);
     }
   }
 
