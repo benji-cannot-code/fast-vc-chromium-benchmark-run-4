382abcefd555e2796aee821223a1a3bce6513798
Alex Newcomer <newcomer@chromium.org>
2019-10-22 21:12:00 +0000

    Enabling hotseat by default.
     - Enable the flag
     - Fix hard coded tests where it's not easy to check the feature
       status.
     - Fix a failure that has cropped up since the last round of fixes.
    
    Bug: 1009645
    Change-Id: Ie0c67f2861f5af839f8add3f4b6726ea6e4b3fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860878
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Mitsuru Oshima (slow) <oshima@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708356}

--- ash/app_list/app_list_controller_impl_unittest.cc
+++ ash/app_list/app_list_controller_impl_unittest.cc
@@ -36,9 +36,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
+#include "base/macros.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
+#include "chromeos/constants/chromeos_switches.h"
 #include "ui/compositor/scoped_animation_duration_scale_mode.h"
 #include "ui/events/test/event_generator.h"
 #include "ui/message_center/message_center.h"
@@ -435,6 +439,31 @@ TEST_F(AppListControllerImplTest,
   ASSERT_EQ(ash::AppListViewState::kFullscreenAllApps,
             GetAppListView()->app_list_state());
 }
+
+class HotseatAppListControllerImplTest
+    : public AppListControllerImplTest,
+      public testing::WithParamInterface<bool> {
+ public:
+  HotseatAppListControllerImplTest() = default;
+  ~HotseatAppListControllerImplTest() override = default;
+
+  // AshTestBase:
+  void SetUp() override {
+    if (GetParam()) {
+      feature_list_.InitAndEnableFeature(chromeos::features::kShelfHotseat);
+    } else {
+      feature_list_.InitAndDisableFeature(chromeos::features::kShelfHotseat);
+    }
+    AppListControllerImplTest::SetUp();
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+  DISALLOW_COPY_AND_ASSIGN(HotseatAppListControllerImplTest);
+};
+
+// Tests with both hotseat disabled and enabled.
+INSTANTIATE_TEST_SUITE_P(, HotseatAppListControllerImplTest, testing::Bool());
 
 // Tests for HomeScreenDelegate::GetInitialAppListItemScreenBoundsForWindow
 // implemtenation.
--- components/exo/wayland/zcr_remote_shell_unittest.cc
+++ components/exo/wayland/zcr_remote_shell_unittest.cc
@@ -24,7 +24,7 @@ TEST_F(ZcrRemoteShellTest, GetWorkAreaInsetsInClientPixel) {
   gfx::Insets insets = wayland::GetWorkAreaInsetsInClientPixel(
       display, device_scale_factor, display.GetSizeInPixel(),
       display.work_area());
-  EXPECT_EQ(gfx::Insets(0, 0, 128, 0).ToString(), insets.ToString());
+  EXPECT_EQ(gfx::Insets(0, 0, 110, 0).ToString(), insets.ToString());
 
   auto secondary_display = GetSecondaryDisplay();
   gfx::Size secondary_size(secondary_display.size());
@@ -33,7 +33,7 @@ TEST_F(ZcrRemoteShellTest, GetWorkAreaInsetsInClientPixel) {
   gfx::Insets secondary_insets = wayland::GetWorkAreaInsetsInClientPixel(
       secondary_display, device_scale_factor, secondary_size_in_client_pixel,
       secondary_display.work_area());
-  EXPECT_EQ(gfx::Insets(0, 0, 126, 0).ToString(), secondary_insets.ToString());
+  EXPECT_EQ(gfx::Insets(0, 0, 108, 0).ToString(), secondary_insets.ToString());
 
   // Stable Insets
   auto widget = CreateTestWidget();
@@ -43,11 +43,11 @@ TEST_F(ZcrRemoteShellTest, GetWorkAreaInsetsInClientPixel) {
   gfx::Insets stable_insets = wayland::GetWorkAreaInsetsInClientPixel(
       display, device_scale_factor, display.GetSizeInPixel(),
       wayland::GetStableWorkArea(display));
-  EXPECT_EQ(gfx::Insets(0, 0, 128, 0).ToString(), stable_insets.ToString());
+  EXPECT_EQ(gfx::Insets(0, 0, 110, 0).ToString(), stable_insets.ToString());
   gfx::Insets secondary_stable_insets = wayland::GetWorkAreaInsetsInClientPixel(
       secondary_display, device_scale_factor, secondary_size_in_client_pixel,
       wayland::GetStableWorkArea(secondary_display));
-  EXPECT_EQ(gfx::Insets(0, 0, 126, 0).ToString(),
+  EXPECT_EQ(gfx::Insets(0, 0, 108, 0).ToString(),
             secondary_stable_insets.ToString());
 }
 
