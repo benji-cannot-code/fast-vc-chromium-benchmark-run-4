e50fc0bbd36e9a932d08b33c844d8ef3a7dba6a5
Joel Hockey <joelhockey@chromium.org>
2019-10-22 08:12:09 +0000

    Install app dialog should not show install button until ready
    Bug: 1016202
    Change-Id: I820a076ed53960fcb4cc74a5ff1909ccf354453a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1870153
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708126}

--- ui/file_manager/file_manager/foreground/js/ui/install_linux_package_dialog.js
+++ ui/file_manager/file_manager/foreground/js/ui/install_linux_package_dialog.js
@@ -51,6 +51,7 @@ cr.define('cr.filebrowser', () => {
       // We re-use the same object, so reset any visual state that may be
       // changed.
       this.installButton_.hidden = false;
+      this.installButton_.disabled = true;
       this.okButton.hidden = true;
       this.cancelButton.hidden = false;
 
@@ -141,6 +142,9 @@ cr.define('cr.filebrowser', () => {
         this.details_frame_.appendChild(text);
         this.details_frame_.appendChild(this.document_.createElement('br'));
       }
+
+      // Allow install now.
+      this.installButton_.disabled = false;
     }
 
     /**
