aac505a3efbb7c1f5b5132383157648a54622641
Haoming Chen <hmchen@chromium.org>
2019-10-22 05:12:27 +0000

    media: Enable encrypted VP9 profile 2
    With the new Widevine CDM, support for encrypted VP9 profile 2 has
    been added. So change the supports_vp9_profile2 capability to true
    for ChromeOS. Other OSes will get the VP9 profile 2 support from
    the Widevine CDM manifest.
    
    This changes also updates the browser tests expected result to SUCCESS
    for VP9 profile 2.
    
    Bug: 707128
    Change-Id: Idfedd94c726afa22b538870eb103b1fdb6d51209
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866011
    Commit-Queue: Haoming Chen <hmchen@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708098}

--- chrome/browser/media/encrypted_media_supported_types_browsertest.cc
+++ chrome/browser/media/encrypted_media_supported_types_browsertest.cc
@@ -1008,16 +1008,14 @@ IN_PROC_BROWSER_TEST_F(EncryptedMediaSupportedTypesWidevineTest, Video_WebM) {
                                            video_webm_codecs()));
   EXPECT_WV_SUCCESS(IsSupportedByKeySystem(kWidevine, kVideoWebMMimeType,
                                            vp9_profile0_codecs()));
+  EXPECT_WV_SUCCESS(IsSupportedByKeySystem(kWidevine, kVideoWebMMimeType,
+                                           vp9_profile2_codecs()));
   EXPECT_WV_AV1(
       IsSupportedByKeySystem(kWidevine, kVideoWebMMimeType, av1_codecs()));
 
   // Non-video WebM codecs.
   EXPECT_UNSUPPORTED(IsSupportedByKeySystem(kWidevine, kVideoWebMMimeType,
                                             audio_webm_codecs()));
-
-  // TODO(crbug.com/996372): Change to EXPECT_WV_SUCCESS after new CDM is in.
-  EXPECT_ANY(IsSupportedByKeySystem(kWidevine, kVideoWebMMimeType,
-                                    vp9_profile2_codecs()));
 
   // Invalid or non-Webm codecs.
   EXPECT_UNSUPPORTED(
@@ -1056,6 +1054,8 @@ IN_PROC_BROWSER_TEST_F(EncryptedMediaSupportedTypesWidevineTest, Video_MP4) {
       IsSupportedByKeySystem(kWidevine, kVideoMP4MimeType, video_mp4_codecs()));
   EXPECT_WV_SUCCESS(IsSupportedByKeySystem(kWidevine, kVideoMP4MimeType,
                                            vp9_profile0_codecs()));
+  EXPECT_WV_SUCCESS(IsSupportedByKeySystem(kWidevine, kVideoMP4MimeType,
+                                           vp9_profile2_codecs()));
   EXPECT_WV_AV1(
       IsSupportedByKeySystem(kWidevine, kVideoMP4MimeType, av1_codecs()));
 
@@ -1068,10 +1068,6 @@ IN_PROC_BROWSER_TEST_F(EncryptedMediaSupportedTypesWidevineTest, Video_MP4) {
       IsSupportedByKeySystem(kWidevine, kVideoMP4MimeType, audio_mp4_codecs()));
   EXPECT_UNSUPPORTED(IsSupportedByKeySystem(kWidevine, kVideoMP4MimeType,
                                             audio_mp4_flac_codecs()));
-
-  // TODO(crbug.com/996372): Change to EXPECT_WV_SUCCESS after new CDM is in.
-  EXPECT_ANY(IsSupportedByKeySystem(kWidevine, kVideoMP4MimeType,
-                                    vp9_profile2_codecs()));
 
   // Invalid or non-MP4 codecs.
   EXPECT_UNSUPPORTED(
