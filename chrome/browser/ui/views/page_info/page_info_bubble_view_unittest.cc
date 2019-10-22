35f9dc114bb28b0e0feb7fada5e24762e5725633
Livvie Lin <livvielin@chromium.org>
2019-10-22 00:04:27 +0000

    Reland "Handle Safety Tip learn more link in page info"
    This is a reland of a9df5b29fa239b7497ec2d551e5cf4470330db45
    
    Differences from original cl:
    - PageInfoUI::SecurityDescription GetSecurityDetails() changed to
    PageInfoUI::SecurityDescriptionType GetSecurityDescriptionType()
    - Initialize security_description_type_ in the header file
    - In safety_tip_page_info_bubble_view_browsertest, static cast to a
    regular page info bubble instead of safety tip bubble for
    CheckPageInfoDoesNotShowSafetyTipInfo
    
    Original change's description:
    > Handle Safety Tip learn more link in page info
    >
    > Bug: 1007497
    > Change-Id: I4498d39cabf4736a697294ad027082a53894a623
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857166
    > Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    > Reviewed-by: Emily Stark <estark@chromium.org>
    > Commit-Queue: Livvie Lin <livvielin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#706076}
    
    Bug: 1007497
    Change-Id: I8dcebe8a7ab974e72eea24280578f8f6830031b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1867478
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708046}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_unittest.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_unittest.cc
@@ -92,6 +92,9 @@ class PageInfoBubbleViewTestApi {
   views::Widget::ClosedReason closed_reason() const { return *closed_reason_; }
 
   base::string16 GetWindowTitle() { return view_->GetWindowTitle(); }
+  PageInfoUI::SecurityDescriptionType GetSecurityDescriptionType() {
+    return view_->GetSecurityDescriptionType();
+  }
 
   PermissionSelectorRow* GetPermissionSelectorAt(int index) {
     return view_->selector_rows_[index].get();
@@ -692,6 +695,8 @@ TEST_F(PageInfoBubbleViewTest, OpenPageInfoBubbleAfterNavigationStart) {
   api_->CreateView();
   EXPECT_EQ(l10n_util::GetStringUTF16(IDS_PAGE_INFO_NOT_SECURE_SUMMARY),
             api_->GetWindowTitle());
+  EXPECT_EQ(PageInfoUI::SecurityDescriptionType::CONNECTION,
+            api_->GetSecurityDescriptionType());
 
   // Set up a test SSLInfo so that Page Info sees the connection as secure.
   uint16_t cipher_suite = 0xc02f;  // TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
@@ -710,6 +715,8 @@ TEST_F(PageInfoBubbleViewTest, OpenPageInfoBubbleAfterNavigationStart) {
   navigation->Commit();
   EXPECT_EQ(l10n_util::GetStringUTF16(IDS_PAGE_INFO_SECURE_SUMMARY),
             api_->GetWindowTitle());
+  EXPECT_EQ(PageInfoUI::SecurityDescriptionType::CONNECTION,
+            api_->GetSecurityDescriptionType());
 }
 
 TEST_F(PageInfoBubbleViewTest, EnsureCloseCallback) {
