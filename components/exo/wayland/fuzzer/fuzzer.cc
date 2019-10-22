43bd02fe2a0329f30b9c8929db79f77c288b9c8e
kylechar <kylechar@chromium.org>
2019-10-22 20:06:36 +0000

    Fix wayland_fuzzer crash on exit.
    base::AtExitManager is getting destroyed after the last fuzzer test case
    runs due to the static initialization. Running exit tasks after some
    arbitrary fuzzer test case isn't useful and destroying the
    TestGpuThreadService causes a crash in eglDestroySurface().
    
    Bug: 1009165
    Change-Id: Ifde4517b574aaa367a94dcbcc502bb2ad2027813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872156
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708318}

--- components/exo/wayland/fuzzer/fuzzer.cc
+++ components/exo/wayland/fuzzer/fuzzer.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/at_exit.h"
+#include "base/no_destructor.h"
 #include "components/exo/wayland/fuzzer/actions.pb.h"
 #include "components/exo/wayland/fuzzer/harness.h"
 #include "components/exo/wayland/fuzzer/server_environment.h"
@@ -20,7 +21,7 @@ class FuzzerEnvironment {
 };
 
 DEFINE_TEXT_PROTO_FUZZER(const exo::wayland_fuzzer::actions::actions& acts) {
-  static base::AtExitManager exit_manager;
+  static base::NoDestructor<base::AtExitManager> exit_manager;
   static FuzzerEnvironment environment;
 
   exo::wayland_fuzzer::Harness().Run(acts);
