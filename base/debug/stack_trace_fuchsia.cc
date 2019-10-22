d555d8e87c7d26ba3c3f3a8c76bc51c3a47146d0
Kevin Marshall <kmarshall@chromium.org>
2019-10-22 19:58:27 +0000

    [Fuchsia] Improvements for symbolization of Fuchsia binaries.
    Enhances the install script to place symbols from local builds into
    a Fuchsia build output directory.
    
    Also fixes an issue with process names being used in the module listing
    that was preventing user-mode stack traces from browser child processes
    from being symbolized properly. Process names are suffixed with a
    process type string, using a colon as a delimiter. Colons are used as
    field delimiters by the symbolizing stream parser, and so inclusion
    of the colon yielded unparseable entries in the module listing.
    
    
    Bug: 1014601
    Change-Id: If4affeca56a0c69e965ee588ddc3f861726796c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1863570
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Auto-Submit: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708313}

--- base/debug/stack_trace_fuchsia.cc
+++ base/debug/stack_trace_fuchsia.cc
@@ -30,11 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 namespace debug {
-
 namespace {
-
-const char kProcessNamePrefix[] = "app:";
-const size_t kProcessNamePrefixLength = base::size(kProcessNamePrefix) - 1;
 
 struct BacktraceData {
   void** trace_array;
@@ -91,7 +87,7 @@ class SymbolMap {
     const void* addr = nullptr;
     std::array<Segment, kMaxSegmentCount> segments;
     size_t segment_count = 0;
-    char name[ZX_MAX_NAME_LEN + kProcessNamePrefixLength + 1] = {0};
+    char name[ZX_MAX_NAME_LEN + 1] = {0};
     char build_id[kMaxBuildIdStringLength + 1] = {0};
   };
 
@@ -130,11 +126,19 @@ void SymbolMap::Populate() {
   // if we keep hitting problems with truncation, find a way to plumb argv[0]
   // through to here instead, e.g. using CommandLine::GetProgramName().
   char app_name[std::extent<decltype(SymbolMap::Module::name)>()];
-  strncpy(app_name, kProcessNamePrefix, sizeof(kProcessNamePrefix));
-  zx_status_t status = zx_object_get_property(
-      process, ZX_PROP_NAME, app_name + kProcessNamePrefixLength,
-      sizeof(app_name) - kProcessNamePrefixLength);
-  if (status != ZX_OK) {
+  zx_status_t status =
+      zx_object_get_property(process, ZX_PROP_NAME, app_name, sizeof(app_name));
+  if (status == ZX_OK) {
+    // The process name may have a process type suffix at the end (e.g.
+    // "context", "renderer", gpu"), which doesn't belong in the module list.
+    // Trim the suffix from the name.
+    for (size_t i = 0; i < base::size(app_name) && app_name[i] != '\0'; ++i) {
+      if (app_name[i] == ':') {
+        app_name[i] = 0;
+        break;
+      }
+    }
+  } else {
     DPLOG(WARNING)
         << "Couldn't get name, falling back to 'app' for program name: "
         << status;
