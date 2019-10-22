564286fc78ea0da3be9dd5fc21ee9f751e99129c
Jay Harris <harrisjay@chromium.org>
2019-10-22 03:16:53 +0000

    WebApps: Adds a AppShortcutObserver, for observing shortcut creation.
    Some actions (such as file handler registration on Linux) can only be
    run after shortcuts have been created. This CL provides a way for those
    actions to be run at the appropriate time.
    
    Bug: 860581
    Change-Id: Iea6a14c15c15c4ab39e8b0bbb71ac996dd9c0e81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868566
    Commit-Queue: Jay Harris <harrisjay@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708084}

--- chrome/browser/web_applications/test/test_app_shortcut_manager.h
+++ chrome/browser/web_applications/test/test_app_shortcut_manager.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "chrome/browser/web_applications/components/app_shortcut_manager.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
@@ -49,6 +50,8 @@ class TestAppShortcutManager : public AppShortcutManager {
 
   bool can_create_shortcuts_ = true;
   std::map<AppId, bool> next_create_shortcut_results_;
+
+  base::WeakPtrFactory<TestAppShortcutManager> weak_ptr_factory_{this};
 };
 
 }  // namespace web_app
