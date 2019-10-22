f2556296246350e8c8f71e62bdc653cabdb538f4
Synthia <synthia.is@samsung.com>
2019-10-22 18:32:12 +0000

    [PaymentRequest]Add PaymentRequestPaymentApp test case for user cancel through payment sheet
    If user click on Cancel button in dialog,then Dialog should be disapper
    and display a error message
    
    Bug: 1006820
    Change-Id: I5a75da76c4b5403a824f49855f9f939de74bd984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860734
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708271}

--- components/test/data/payments/bobpay_and_basic_card_with_modifiers.js
+++ components/test/data/payments/bobpay_and_basic_card_with_modifiers.js
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Launches the PaymentRequest UI with Bob Pay and basic-card as payment
  * methods and a modifier for basic-card.
  */
-function buy() {  // eslint-disable-line no-unused-vars
+function buy() { // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest(
         [
@@ -55,7 +55,7 @@ function buy() {  // eslint-disable-line no-unused-vars
  * Launches the PaymentRequest UI with Bob Pay and basic-card as payment
  * methods and a modifier for Bob Pay.
  */
-function buyWithBobPayDiscount() {  // eslint-disable-line no-unused-vars
+function buyWithBobPayDiscount() { // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest(
         [
@@ -99,7 +99,7 @@ function buyWithBobPayDiscount() {  // eslint-disable-line no-unused-vars
  * Launches the PaymentRequest UI with Bob Pay and basic-card as payment
  * methods and a modifier for basic-card with "credit" type
  */
-function creditSupportedType() {  // eslint-disable-line no-unused-vars
+function creditSupportedType() { // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest(
         [
@@ -146,7 +146,7 @@ function creditSupportedType() {  // eslint-disable-line no-unused-vars
  * Launches the PaymentRequest UI with Bob Pay and basic-card as payment
  * methods and a modifier for basic-card with "debit" type
  */
-function debitSupportedType() {  // eslint-disable-line no-unused-vars
+function debitSupportedType() { // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest(
         [
@@ -193,7 +193,7 @@ function debitSupportedType() {  // eslint-disable-line no-unused-vars
  * Launches the PaymentRequest UI with Bob Pay and basic-card as payment
  * methods and a modifier for basic-card with "credit" type and "visa" network
  */
-function visaSupportedNetwork() {  // eslint-disable-line no-unused-vars
+function visaSupportedNetwork() { // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest(
         [
@@ -242,7 +242,7 @@ function visaSupportedNetwork() {  // eslint-disable-line no-unused-vars
  * methods and a modifier for basic-card with "credit" type and " mastercard"
  * network
  */
-function mastercardSupportedNetwork() {  // eslint-disable-line no-unused-vars
+function mastercardSupportedNetwork() { // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest(
         [
@@ -290,7 +290,7 @@ function mastercardSupportedNetwork() {  // eslint-disable-line no-unused-vars
  * Launches the PaymentRequest UI with Bob Pay and basic-card as payment
  * methods and a modifier for basic-card with "mastercard" network.
  */
-function mastercardAnySupportedType() {  // eslint-disable-line no-unused-vars
+function mastercardAnySupportedType() { // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest(
         [
@@ -338,7 +338,7 @@ function mastercardAnySupportedType() {  // eslint-disable-line no-unused-vars
  * modifier for basic-card with "mastercard" network, but the modifier does not
  * have a total specified.
  */
-function noTotal() {  // eslint-disable-line no-unused-vars
+function noTotal() { // eslint-disable-line no-unused-vars
   try {
     new PaymentRequest([{supportedMethods: 'basic-card'}], {
       total: {label: 'Total', amount: {currency: 'USD', value: '5.00'}},
