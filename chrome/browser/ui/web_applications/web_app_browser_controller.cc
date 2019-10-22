200156c5ae71f7fcb3f1bb6d6ce1a20408ff803d
Eric Willigers <ericwilligers@chromium.org>
2019-10-22 05:09:28 +0000

    desktop-pwas: Support custom tab bar for BMO
    The implementation of ShouldShowCustomTabBar is moved up
    from HostedAppBrowserController to AppBrowserController, with
    dependencies on extensions removed.
    
    The CustomTabBarViewBrowserTest tests now pass with BMO.
    
    TBR=bsep@chromium.org
    
    Bug: 966290
    Change-Id: I541964d2deb5d30e231bdd79d199c14b26321040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871154
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708096}

--- chrome/browser/ui/web_applications/web_app_browser_controller.cc
+++ chrome/browser/ui/web_applications/web_app_browser_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/app_icon_manager.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
+#include "content/public/browser/web_contents.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/image/image.h"
 #include "url/gurl.h"
@@ -40,11 +41,6 @@ bool WebAppBrowserController::IsHostedApp() const {
 void WebAppBrowserController::SetReadIconCallbackForTesting(
     base::OnceClosure callback) {
   callback_for_testing_ = std::move(callback);
-}
-
-bool WebAppBrowserController::ShouldShowCustomTabBar() const {
-  // TODO(https://crbug.com/966290): Complete implementation.
-  return false;
 }
 
 gfx::ImageSkia WebAppBrowserController::GetWindowAppIcon() const {
