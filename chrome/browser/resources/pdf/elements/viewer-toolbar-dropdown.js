f71d5dadc8ae2ca146c045c1c732178894fce9d2
dpapad <dpapad@chromium.org>
2019-10-22 20:32:02 +0000

    PDF Viewer: Restore viewer-toolbar-dropdown box shadow.
    It was caused by a missing dependency.
    
    Fixed: 1016699
    Change-Id: I866a033782a53b2c6c1bb04479a058335cafe646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874189
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708327}

--- chrome/browser/resources/pdf/elements/viewer-toolbar-dropdown.js
+++ chrome/browser/resources/pdf/elements/viewer-toolbar-dropdown.js
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import 'chrome://resources/cr_elements/shared_vars_css.m.js';
+import 'chrome://resources/polymer/v3_0/paper-styles/shadow.js';
 
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
