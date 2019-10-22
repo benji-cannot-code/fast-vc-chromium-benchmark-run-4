0027d17b04df25d03c07431475347f1b146099d3
Andreas Haas <ahaas@chromium.org>
2019-10-22 09:02:38 +0000

    [arraybuffer] Don't adjust V8 externally allocated memory for ArrayBuffers
    With recent changes in V8 it is not necessary anymore to call
    {AdjustAmountOfExternalAllocatedMemory} for memory of ArrayBuffers. V8
    get this information now from ArrayBuffer objects on the heap.
    
    R=ulan@chromium.org, haraken@chromium.org
    
    Bug: chromium:1008840
    Change-Id: I886b391e1655835ade39dc43027a392490d30cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871876
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708130}

--- third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h
+++ third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h
@@ -334,7 +334,6 @@ class CORE_EXPORT SerializedScriptValue
   SharedArrayBufferContentsArray shared_array_buffers_contents_;
 
   bool has_registered_external_allocation_;
-  bool transferables_need_external_allocation_registration_;
 #if DCHECK_IS_ON()
   bool was_unpacked_ = false;
 #endif
