c36680604169ad7981d9305e5852944c5a42dd27
Peter Kotwicz <pkotwicz@chromium.org>
2019-10-22 18:54:58 +0000

    Disable failing TrustedWebActivityTests
    This CL disables
    TrustedWebActivityTests#testStatusBarColorCertificateError()
    TrustedWebActivityTests#testToolbarVisibleCertificateError()
    TBR=pkotwicz,alexilin
    
    BUG=1016743,1016746
    
    Change-Id: I74ac6eb22c98d561d2ab03589eea3b7b51176786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874190
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708278}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browserservices/TrustedWebActivityTest.java
@@ -33,6 +33,7 @@ import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -180,7 +181,8 @@ public class TrustedWebActivityTest {
      * in the intent).
      */
     @Test
-    @MediumTest
+    @DisabledTest(message = "crbug.com/1016743")
+    //@MediumTest
     @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP_MR1)
     @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
     public void testStatusBarColorCertificateError() throws ExecutionException, TimeoutException {
@@ -257,7 +259,8 @@ public class TrustedWebActivityTest {
      * (and origin verification succeeds).
      */
     @Test
-    @MediumTest
+    @DisabledTest(message = "crbug.com/1016743")
+    //@MediumTest
     public void testToolbarVisibleCertificateError() throws ExecutionException, TimeoutException {
         final String pageWithoutCertError =
                 mEmbeddedTestServerRule.getServer().getURL("/chrome/test/data/android/about.html");
