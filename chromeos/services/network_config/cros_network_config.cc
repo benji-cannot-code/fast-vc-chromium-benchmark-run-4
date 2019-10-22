6d5695e2220cce4f084082f181d9279fc41243a0
Steven Bennetts <stevenjb@chromium.org>
2019-10-22 20:33:46 +0000

    cros_network-config.mojo: Provide (fake) password properties to UI
    This CL provides all password fields supported in the UI in mojo.
    All such fields are already set to a fake value deep in the model
    so that plaintext values will not be accidentally leaked, and the
    UI already correctly identifies such values when provided.
    
    Bug: 1014779
    Change-Id: I9ba46c905722481b8ce4bc35bbd2534eb2ca2505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865812
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708329}

--- chromeos/services/network_config/cros_network_config.cc
+++ chromeos/services/network_config/cros_network_config.cc
@@ -993,6 +993,7 @@ mojom::ManagedEAPPropertiesPtr GetManagedEAPProperties(const base::Value* dict,
   eap->identity = GetManagedString(eap_dict, ::onc::eap::kIdentity);
   eap->inner = GetManagedString(eap_dict, ::onc::eap::kInner);
   eap->outer = GetManagedString(eap_dict, ::onc::eap::kOuter);
+  eap->password = GetManagedString(eap_dict, ::onc::eap::kPassword);
   eap->save_credentials =
       GetManagedBoolean(eap_dict, ::onc::eap::kSaveCredentials);
   eap->server_ca_pems =
@@ -1097,6 +1098,7 @@ mojom::ManagedOpenVPNPropertiesPtr GetManagedOpenVPNProperties(
       GetManagedString(openvpn_dict, ::onc::openvpn::kKeyDirection);
   openvpn->ns_cert_type =
       GetManagedString(openvpn_dict, ::onc::openvpn::kNsCertType);
+  openvpn->password = GetManagedString(openvpn_dict, ::onc::openvpn::kPassword);
   openvpn->port = GetManagedInt32(openvpn_dict, ::onc::openvpn::kPort);
   openvpn->proto = GetManagedString(openvpn_dict, ::onc::openvpn::kProto);
   openvpn->push_peer_info =
@@ -1398,6 +1400,7 @@ mojom::ManagedPropertiesPtr ManagedPropertiesToMojo(
       wifi->hex_ssid = GetManagedString(wifi_dict, ::onc::wifi::kHexSSID);
       wifi->hidden_ssid =
           GetManagedBoolean(wifi_dict, ::onc::wifi::kHiddenSSID);
+      wifi->passphrase = GetManagedString(wifi_dict, ::onc::wifi::kPassphrase);
       wifi->roam_threshold =
           GetManagedInt32(wifi_dict, ::onc::wifi::kRoamThreshold);
       wifi->ssid = GetRequiredManagedString(wifi_dict, ::onc::wifi::kSSID);
