1d3d532ed185d0637c9977c584851ba731845e1d
Dale Curtis <dalecurtis@chromium.org>
2019-10-23 23:35:55 +0000

    Enable verbose logging for a few flaky media tests on macOS.
    We're not having any luck debugging these failures locally, so enable
    VLOG(1) and DVLOG(1) for a few tests to help debug on macOS.
    
    BUG=1003761,1014646
    R=tmathmeyer
    
    Change-Id: Ie14b4c5be287eabb526a91fa6acfa8dcacc97940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876315
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Ted Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708833}

--- media/test/pipeline_integration_test.cc
+++ media/test/pipeline_integration_test.cc
@@ -192,6 +192,20 @@ static base::Time kLiveTimelineOffset() {
   return timeline_offset;
 }
 
+#if defined(OS_MACOSX)
+class ScopedVerboseLogEnabler {
+ public:
+  ScopedVerboseLogEnabler() : old_level_(logging::GetMinLogLevel()) {
+    logging::SetMinLogLevel(-1);
+  }
+  ~ScopedVerboseLogEnabler() { logging::SetMinLogLevel(old_level_); }
+
+ private:
+  const int old_level_;
+  DISALLOW_COPY_AND_ASSIGN(ScopedVerboseLogEnabler);
+};
+#endif
+
 enum PromiseResult { RESOLVED, REJECTED };
 
 // Provides the test key in response to the encrypted event.
@@ -814,6 +828,11 @@ TEST_F(PipelineIntegrationTest, PlaybackTooManyChannels) {
 }
 
 TEST_F(PipelineIntegrationTest, PlaybackWithAudioTrackDisabledThenEnabled) {
+#if defined(OS_MACOSX)
+  // Enable scoped logs to help track down hangs.  http://crbug.com/1014646
+  ScopedVerboseLogEnabler scoped_log_enabler;
+#endif
+
   ASSERT_EQ(PIPELINE_OK, Start("bear-320x240.webm", kHashed | kNoClockless));
 
   // Disable audio.
@@ -846,6 +865,11 @@ TEST_F(PipelineIntegrationTest, PlaybackWithAudioTrackDisabledThenEnabled) {
 }
 
 TEST_F(PipelineIntegrationTest, PlaybackWithVideoTrackDisabledThenEnabled) {
+#if defined(OS_MACOSX)
+  // Enable scoped logs to help track down hangs.  http://crbug.com/1014646
+  ScopedVerboseLogEnabler scoped_log_enabler;
+#endif
+
   ASSERT_EQ(PIPELINE_OK, Start("bear-320x240.webm", kHashed | kNoClockless));
 
   // Disable video.
@@ -2633,6 +2657,11 @@ TEST_F(PipelineIntegrationTest, MSE_BasicPlayback_VideoOnly_MP4_HEV1) {
 #endif  // BUILDFLAG(USE_PROPRIETARY_CODECS)
 
 TEST_F(PipelineIntegrationTest, SeekWhilePaused) {
+#if defined(OS_MACOSX)
+  // Enable scoped logs to help track down hangs.  http://crbug.com/1014646
+  ScopedVerboseLogEnabler scoped_log_enabler;
+#endif
+
   // This test is flaky without kNoClockless, see crbug.com/796250.
   ASSERT_EQ(PIPELINE_OK, Start("bear-320x240.webm", kNoClockless));
 
@@ -2657,6 +2686,11 @@ TEST_F(PipelineIntegrationTest, SeekWhilePaused) {
 }
 
 TEST_F(PipelineIntegrationTest, SeekWhilePlaying) {
+#if defined(OS_MACOSX)
+  // Enable scoped logs to help track down hangs.  http://crbug.com/1014646
+  ScopedVerboseLogEnabler scoped_log_enabler;
+#endif
+
   // This test is flaky without kNoClockless, see crbug.com/796250.
   ASSERT_EQ(PIPELINE_OK, Start("bear-320x240.webm", kNoClockless));
 
