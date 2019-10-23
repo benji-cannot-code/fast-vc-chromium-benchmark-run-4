2411af6446632187a61211d5197d8f16e4466968
Vadym Doroshenko <dvadym@chromium.org>
2019-10-23 13:00:02 +0000

    Fix manual saving on accounts.google.com
    This is a regression after the old parser removal.
    PasswordManager::ShowManualFallbackForSaving uses the result of the old
    parser. This CL makes using the result of the new parser.
    
    Bug: 1011732
    Change-Id: I0537b472984e029aa2b002ee4d79717ac3f86765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875102
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708588}

--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -410,8 +410,7 @@ void PasswordManager::ShowManualFallbackForSaving(
   if (!client_->GetProfilePasswordStore()->IsAbleToSavePasswords() ||
       !client_->IsSavingAndFillingEnabled(password_form.origin) ||
       ShouldBlockPasswordForSameOriginButDifferentScheme(
-          password_form.origin) ||
-      !client_->GetStoreResultFilter()->ShouldSave(password_form)) {
+          password_form.origin)) {
     return;
   }
 
@@ -423,6 +422,11 @@ void PasswordManager::ShowManualFallbackForSaving(
     client_->GetMetricsRecorder()->RecordFormManagerAvailable(availability);
   if (!manager)
     return;
+
+  if (!client_->GetStoreResultFilter()->ShouldSave(
+          *manager->GetSubmittedForm())) {
+    return;
+  }
 
   // Show the fallback if a prompt or a confirmation bubble should be available.
   bool has_generated_password = manager->HasGeneratedPassword();
