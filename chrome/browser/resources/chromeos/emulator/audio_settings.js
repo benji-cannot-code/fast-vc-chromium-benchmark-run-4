de7471a7233def6acfa461cb56542bde96b9060e
dpapad <dpapad@chromium.org>
2019-10-23 18:15:37 +0000

    Port chrome://device-emulator to Polymer3.
    Fixed: 1015241
    Test: Page still loads, no more HTML Imports warnings in DevTools.
    Change-Id: Id253682734ef7d456b5dd244657011f5de03d73d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874814
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708691}

--- chrome/browser/resources/chromeos/emulator/audio_settings.js
+++ chrome/browser/resources/chromeos/emulator/audio_settings.js
@@ -3,6 +3,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
+import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
+import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+import 'chrome://resources/cr_elements/cr_input/cr_input.m.js';
+import 'chrome://resources/cr_elements/cr_radio_button/cr_radio_button.m.js';
+import 'chrome://resources/cr_elements/cr_radio_group/cr_radio_group.m.js';
+import 'chrome://resources/cr_elements/shared_style_css.m.js';
+import 'chrome://resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
+import './icons.js';
+import './shared_styles.js';
+
+import {sendWithPromise} from 'chrome://resources/js/cr.m.js';
+import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
 /** @enum {string} */ var AudioNodeType = {
   HEADPHONE: 'HEADPHONE',
   MIC: 'MIC',
@@ -50,6 +65,8 @@ var AudioNode = function() {
 
 Polymer({
   is: 'audio-settings',
+
+  _template: html`{__html_template__}`,
 
   properties: {
     /**
@@ -123,7 +140,7 @@ Polymer({
   },
 
   ready: function() {
-    cr.sendWithPromise('requestAudioNodes').then(
+    sendWithPromise('requestAudioNodes').then(
         this.updateAudioNodes_.bind(this));
   },
 
---
+++ chrome/browser/resources/chromeos/emulator/shared_styles.js
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+const styleElement = document.createElement('dom-module');
+styleElement.setAttribute('assetpath', 'chrome://resources/');
+styleElement.innerHTML = `{__html_template__}`;
+styleElement.register('device-emulator-shared-styles');
