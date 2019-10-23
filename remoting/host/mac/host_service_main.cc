97abcdde98578b10b21e0970b8c6f81587e1643a
Peter Kasting <pkasting@chromium.org>
2019-10-23 19:24:14 +0000

    Remove using directives ("using namespace x") from remoting/.
    Bug: 82078
    Change-Id: I5bec95398951e1e1e453b7ab3114e1fcff106198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875269
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708722}

--- remoting/host/mac/host_service_main.cc
+++ remoting/host/mac/host_service_main.cc
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/mac/constants_mac.h"
 #include "remoting/host/username.h"
 
-using namespace remoting;
-
 namespace remoting {
 namespace {
 
@@ -326,34 +324,34 @@ int HostService::RunHostFromOldScript() {
 int main(int argc, char const* argv[]) {
   base::AtExitManager exitManager;
   base::CommandLine::Init(argc, argv);
-  InitHostLogging();
+  remoting::InitHostLogging();
 
-  HostService service;
+  remoting::HostService service;
   auto* current_cmdline = base::CommandLine::ForCurrentProcess();
   std::string pid = base::NumberToString(base::Process::Current().Pid());
-  if (current_cmdline->HasSwitch(kSwitchDisable)) {
+  if (current_cmdline->HasSwitch(remoting::kSwitchDisable)) {
     service.PrintPid();
     if (!service.Disable()) {
       LOG(ERROR) << "Failed to disable";
       return 1;
     }
-  } else if (current_cmdline->HasSwitch(kSwitchEnable)) {
+  } else if (current_cmdline->HasSwitch(remoting::kSwitchEnable)) {
     service.PrintPid();
     if (!service.Enable()) {
       LOG(ERROR) << "Failed to enable";
       return 1;
     }
-  } else if (current_cmdline->HasSwitch(kSwitchSaveConfig)) {
+  } else if (current_cmdline->HasSwitch(remoting::kSwitchSaveConfig)) {
     service.PrintPid();
     if (!service.WriteStdinToConfig()) {
       LOG(ERROR) << "Failed to save config";
       return 1;
     }
-  } else if (current_cmdline->HasSwitch(kSwitchHostVersion)) {
+  } else if (current_cmdline->HasSwitch(remoting::kSwitchHostVersion)) {
     service.PrintHostVersion();
-  } else if (current_cmdline->HasSwitch(kSwitchHostRunFromLaunchd)) {
-    RegisterSignalHandler();
-    HOST_LOG << "Host started for user " << GetUsername() << " at "
+  } else if (current_cmdline->HasSwitch(remoting::kSwitchHostRunFromLaunchd)) {
+    remoting::RegisterSignalHandler();
+    HOST_LOG << "Host started for user " << remoting::GetUsername() << " at "
              << base::Time::Now();
     return service.RunHost();
   } else {
