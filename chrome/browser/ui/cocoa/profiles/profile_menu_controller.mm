9f2189fe4955a1c68d4c836d643ac4006de14e7b
Christopher Cameron <ccameron@chromium.org>
2019-10-23 21:42:45 +0000

    Only show profiles that have app installed in PWA profile menu
    In order to do this, we will need to know whether or not a PWA is
    installed for a given profile, without actually loading that profile.
    The suggested mechanism for this is to check the for the presence of
    a Extensions/app_id subdirectory of the profile path.
    
    Add a profile_menu_items_ member to ExtensionAppShimHandler that
    lists all profiles in the AvatarMenu.
    
    Add a installed_profiles member to ExtensionAppShimHandler::AppState
    that lists all profiles for which a given app is installed (this is
    a subset of the profiles in ExtensionAppShimHandler::
    profile_menu_items_).
    
    Add a ExtensionAppShimHandler::Delegate::GetProfilesForAppAsync
    method to do the aforementioned path check.
    
    Update ExtensionAppShimHandler::UpdateAppProfileMenu to sort menu
    items by |menu_index| and to send an empty list if just one profile
    would have appeared in the menu.
    
    Add tests. Restructure tests such that ExtensionAppShimHandler
    takes its delegate as a constructor argument (instead of having it
    be set-able).
    
    R=dominickn
    TBR=skuhne
    
    Bug: 1001215
    Change-Id: I10bb7128c6f775759f1d1f381000eb7b33960eb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864098
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708792}

--- chrome/browser/ui/cocoa/profiles/profile_menu_controller.mm
+++ chrome/browser/ui/cocoa/profiles/profile_menu_controller.mm
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr int kMenuAvatarIconSize = 38;
-
 // Used in UMA histogram macros, shouldn't be reordered or renumbered
 enum ValidateMenuItemSelector {
   UNKNOWN_SELECTOR = 0,
@@ -160,16 +158,8 @@ class Observer : public BrowserListObserver, public AvatarMenuObserver {
     if (dock) {
       [item setIndentationLevel:1];
     } else {
-      gfx::Image itemIcon;
-      // Always use the low-res, small default avatars in the menu.
-      AvatarMenu::GetImageForMenuButton(itemData.profile_path, &itemIcon);
-
-      // Shapes the avatar icon into a circle for consistency with other avatar
-      // UI elements.
-      itemIcon = profiles::GetSizedAvatarIcon(
-          itemIcon, /*is_rectangle=*/true, kMenuAvatarIconSize,
-          kMenuAvatarIconSize, profiles::SHAPE_CIRCLE);
-
+      gfx::Image itemIcon =
+          profiles::GetAvatarIconForNSMenu(itemData.profile_path);
       [item setImage:itemIcon.ToNSImage()];
       [item setState:itemData.active ? NSOnState : NSOffState];
     }
