7ad5353595dae546d33992568b1e0e85184bcb30
Elly Fong-Jones <ellyjones@chromium.org>
2019-10-23 18:35:19 +0000

    cbui: remove VerifyPendingDialog GetDialogButtonLabel
    Migrate to the new DialogDelegate::set_button_label() pattern. Also, since the
    string is static, use it directly from the view.
    
    Bug: 1011446
    Change-Id: I067d07b75d7d02c628f009328e36afdfd1d1f7ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876467
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708699}

--- chrome/browser/ui/views/autofill/payments/verify_pending_dialog_view_impl.cc
+++ chrome/browser/ui/views/autofill/payments/verify_pending_dialog_view_impl.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/payments/verify_pending_dialog_controller.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "components/constrained_window/constrained_window_views.h"
+#include "components/strings/grit/components_strings.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/window/dialog_client_view.h"
 
@@ -16,6 +18,10 @@ namespace autofill {
 VerifyPendingDialogViewImpl::VerifyPendingDialogViewImpl(
     VerifyPendingDialogController* controller)
     : controller_(controller) {
+  DialogDelegate::set_button_label(
+      ui::DIALOG_BUTTON_CANCEL,
+      l10n_util::GetStringUTF16(
+          IDS_AUTOFILL_VERIFY_PENDING_DIALOG_CANCEL_BUTTON_LABEL));
   // TODO(crbug.com/1014278): Should get correct width automatically when
   // snapping.
   const int width = ChromeLayoutProvider::Get()->GetDistanceMetric(
@@ -60,12 +66,6 @@ bool VerifyPendingDialogViewImpl::Cancel() {
 
 int VerifyPendingDialogViewImpl::GetDialogButtons() const {
   return ui::DIALOG_BUTTON_CANCEL;
-}
-
-base::string16 VerifyPendingDialogViewImpl::GetDialogButtonLabel(
-    ui::DialogButton button) const {
-  DCHECK_EQ(button, ui::DIALOG_BUTTON_CANCEL);
-  return controller_->GetCancelButtonLabel();
 }
 
 ui::ModalType VerifyPendingDialogViewImpl::GetModalType() const {
