9b9f9e5fad61ad78654d9346a824e2a9ad31f798
Kent Tamura <tkent@chromium.org>
2019-10-23 02:27:51 +0000

    Graduate runtime flags for Form Participation APIs
    This CL removes the following runtime flags:
     - ElementInternals
     - FormAssociatedCustomElements
     - FormDataEvent
    
    They have been enabled since M77, and M77 stable had no critical issues
    on these features.
    
    Change-Id: I13ae205072c745bc7c6e334161fa55537fff25e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874987
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708465}

--- third_party/blink/renderer/bindings/core/v8/script_custom_element_definition_builder.cc
+++ third_party/blink/renderer/bindings/core/v8/script_custom_element_definition_builder.cc
@@ -131,7 +131,7 @@ bool ScriptCustomElementDefinitionBuilder::RememberOriginalProperties() {
     }
   }
 
-  if (RuntimeEnabledFeatures::ElementInternalsEnabled()) {
+  {
     auto* isolate = Isolate();
     v8::Local<v8::Context> current_context = isolate->GetCurrentContext();
     v8::TryCatch try_catch(isolate);
@@ -154,7 +154,7 @@ bool ScriptCustomElementDefinitionBuilder::RememberOriginalProperties() {
     }
   }
 
-  if (RuntimeEnabledFeatures::FormAssociatedCustomElementsEnabled()) {
+  {
     auto* isolate = Isolate();
     v8::Local<v8::Context> current_context = isolate->GetCurrentContext();
     v8::TryCatch try_catch(isolate);
--- third_party/blink/renderer/core/html/forms/html_form_element.cc
+++ third_party/blink/renderer/core/html/forms/html_form_element.cc
@@ -415,7 +415,6 @@ void HTMLFormElement::Submit(Event* event,
   }
 
   if (is_constructing_entry_list_) {
-    DCHECK(RuntimeEnabledFeatures::FormDataEventEnabled());
     GetDocument().AddConsoleMessage(
         ConsoleMessage::Create(mojom::ConsoleMessageSource::kJavaScript,
                                mojom::ConsoleMessageLevel::kWarning,
@@ -451,7 +450,7 @@ void HTMLFormElement::Submit(Event* event,
   FormSubmission* form_submission =
       FormSubmission::Create(this, attributes_, event, submit_button);
   // 'formdata' event handlers might disconnect the form.
-  if (RuntimeEnabledFeatures::FormDataEventEnabled() && !isConnected()) {
+  if (!isConnected()) {
     GetDocument().AddConsoleMessage(ConsoleMessage::Create(
         mojom::ConsoleMessageSource::kJavaScript,
         mojom::ConsoleMessageLevel::kWarning,
@@ -492,8 +491,7 @@ FormData* HTMLFormElement::ConstructEntryList(
         form_data.SetContainsPasswordData(true);
     }
   }
-  if (RuntimeEnabledFeatures::FormDataEventEnabled())
-    DispatchEvent(*MakeGarbageCollected<FormDataEvent>(form_data));
+  DispatchEvent(*MakeGarbageCollected<FormDataEvent>(form_data));
 
   if (submit_button)
     submit_button->SetActivatedSubmit(false);
