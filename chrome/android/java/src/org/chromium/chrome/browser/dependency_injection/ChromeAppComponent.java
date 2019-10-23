9e8973796f582b1b35e0ded1e91ee8eb7993cea0
Peter Kotwicz <pkotwicz@chromium.org>
2019-10-23 21:47:16 +0000

    Daggerize WebappActivity
    This CL daggerizes WebappActivity.
    
    BUG=1016650
    
    Change-Id: I06f75fdce90985bfb6a59efca0ce268032821375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1873276
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708801}

--- chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeAppComponent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeAppComponent.java
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.dependency_injection;
 
 import org.chromium.chrome.browser.AppHooksModule;
-import org.chromium.chrome.browser.browserservices.SessionDataHolder;
 import org.chromium.chrome.browser.browserservices.ClearDataDialogResultRecorder;
+import org.chromium.chrome.browser.browserservices.SessionDataHolder;
 import org.chromium.chrome.browser.browserservices.TrustedWebActivityClient;
 import org.chromium.chrome.browser.browserservices.permissiondelegation.NotificationPermissionUpdater;
 import org.chromium.chrome.browser.browserservices.permissiondelegation.TrustedWebActivityPermissionManager;
@@ -17,6 +17,7 @@ import org.chromium.chrome.browser.customtabs.dependency_injection.CustomTabActi
 import org.chromium.chrome.browser.customtabs.dependency_injection.CustomTabActivityModule;
 import org.chromium.chrome.browser.externalauth.ExternalAuthUtils;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
+import org.chromium.chrome.browser.webapps.dependency_injection.WebappActivityComponent;
 
 import javax.inject.Singleton;
 
@@ -32,6 +33,7 @@ public interface ChromeAppComponent {
 
     CustomTabActivityComponent createCustomTabActivityComponent(ChromeActivityCommonsModule module,
             CustomTabActivityModule customTabActivityModule);
+    WebappActivityComponent createWebappActivityComponent(ChromeActivityCommonsModule module);
 
     CustomTabsConnection resolveCustomTabsConnection();
     ChromePreferenceManager resolvePreferenceManager();
