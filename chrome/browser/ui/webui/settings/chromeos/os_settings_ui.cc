6e5cdfccf7c04923f01035b78b24e6b49e01a2d4
Dan Beam <dbeam@chromium.org>
2019-10-23 04:25:34 +0000

    [System Web Apps] Allow settings and OS settings app title to be l10n'd
    Bug: 1010815
    Change-Id: I6554af5a5b6449d4c3eb298f42ce63d7f9ae127c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868653
    Commit-Queue: calamity <calamity@chromium.org>
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708502}

--- chrome/browser/ui/webui/settings/chromeos/os_settings_ui.cc
+++ chrome/browser/ui/webui/settings/chromeos/os_settings_ui.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "build/build_config.h"
+#include "chrome/browser/ui/web_applications/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/webui/app_management/app_management.mojom.h"
 #include "chrome/browser/ui/webui/app_management/app_management_page_handler.h"
 #include "chrome/browser/ui/webui/managed_ui_handler.h"
@@ -128,7 +129,8 @@ OSSettingsUI::OSSettingsUI(content::WebUI* web_ui)
   if (web_app::SystemWebAppManager::IsEnabled()) {
     html_source->AddResourcePath("icon-192.png", IDR_SETTINGS_LOGO_192);
     html_source->AddResourcePath("pwa.html", IDR_PWA_HTML);
-    html_source->AddResourcePath("manifest.json", IDR_OS_SETTINGS_MANIFEST);
+    web_app::SetManifestRequestFilter(html_source, IDR_OS_SETTINGS_MANIFEST,
+                                      IDS_SETTINGS_SETTINGS);
   }
 
 #if BUILDFLAG(OPTIMIZE_WEBUI)
