4afa3f781d23353aac736ea914af91e091ef986c
Miguel Casas <mcasas@chromium.org>
2019-10-23 12:52:32 +0000

    CrOS: add flag to enable using HDR transfer functions when present
    This CL adds a flag to explicitly enable using the HDR monitor(s)
    EDID-provided transfer function, if present at all. By default
    this flag is disabled because blindly using an HDR transfer makes
    HDR content look great and SDR content look faded (naive SDR-HDR
    mixing). The idea is to enable the flag for development.
    
    Bug: 1012846
    Change-Id: I4d21aafa0a2b6114f68dc6a3615f72674dc0979c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872794
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mitsuru Oshima (slow) <oshima@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708587}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1972,6 +1972,9 @@ extern const char kDisableCancelAllTouchesDescription[];
 extern const char kDisableExplicitDmaFencesName[];
 extern const char kDisableExplicitDmaFencesDescription[];
 
+extern const char kEnableUseHDRTransferFunctionName[];
+extern const char kEnableUseHDRTransferFunctionDescription[];
+
 extern const char kDisableOfficeEditingComponentAppName[];
 extern const char kDisableOfficeEditingComponentAppDescription[];
 
