78e966175d5cdbd97a85dd9b674876489a1c18c6
Tommy Steimel <steimel@chromium.org>
2019-10-23 20:28:40 +0000

    GMC: Hide Chromium icons shown in notification header
    This CL adds the ability to hide the app icon view in Message Center's
    NotificationHeaderView. This allows the Global Media Controls dialog to
    hide the unnecessary Chromium icon that appears before the URL.
    
    Bug: 1011047
    Change-Id: Ic3e5a8e57ecfc14698697370e4fc0d5840c28ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875276
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708760}

--- chrome/browser/ui/views/global_media_controls/media_notification_container_impl_view.cc
+++ chrome/browser/ui/views/global_media_controls/media_notification_container_impl_view.cc
@@ -94,7 +94,7 @@ MediaNotificationContainerImplView::MediaNotificationContainerImplView(
 
   view_ = std::make_unique<media_message_center::MediaNotificationView>(
       this, std::move(item), dismiss_button_container_.get(), base::string16(),
-      kWidth);
+      kWidth, /*should_show_icon=*/false);
   view_->set_owned_by_client();
   ForceExpandedState();
 
