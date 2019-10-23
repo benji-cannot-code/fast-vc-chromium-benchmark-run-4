e46508731c5e6f6672fad74609dcf848192e6380
Mohammad Refaat <mrefaat@chromium.org>
2019-10-23 20:03:23 +0000

    Add ClearWKBrowsingData method to browsing_data_test_util.h/m
    This method clears WK  WebSiteDataStore default store data including
    wk forward/back cache
    
    
    Bug: 987646
    Change-Id: I64bbb8015cc12a623c8a2096981d16198adc5a6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876732
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708743}

--- ios/chrome/test/app/browsing_data_test_util.mm
+++ ios/chrome/test/app/browsing_data_test_util.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/test/app/browsing_data_test_util.h"
 
+#import <WebKit/WebKit.h>
+
 #include "base/task/post_task.h"
 #import "base/test/ios/wait_util.h"
 #include "components/browsing_data/core/browsing_data_utils.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/security/certificate_policy_cache.h"
 #include "ios/web/public/thread/web_task_traits.h"
 #include "ios/web/public/thread/web_thread.h"
+#import "ios/web/web_state/ui/wk_web_view_configuration_provider.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -53,6 +56,23 @@ bool ClearBrowsingHistory() {
 
 bool ClearAllBrowsingData(bool off_the_record) {
   return ClearBrowsingData(off_the_record, BrowsingDataRemoveMask::REMOVE_ALL);
+}
+
+bool ClearAllWebStateBrowsingData() {
+  __block bool callback_finished = false;
+  [[WKWebsiteDataStore defaultDataStore]
+      removeDataOfTypes:[WKWebsiteDataStore allWebsiteDataTypes]
+          modifiedSince:[NSDate distantPast]
+      completionHandler:^{
+        web::BrowserState* browser_state =
+            chrome_test_util::GetOriginalBrowserState();
+        web::WKWebViewConfigurationProvider::FromBrowserState(browser_state)
+            .Purge();
+        callback_finished = true;
+      }];
+  return WaitUntilConditionOrTimeout(20, ^{
+    return callback_finished;
+  });
 }
 
 bool ClearCertificatePolicyCache(bool off_the_record) {
