295c90c13d120a85d02afbb297034cc56ef6a584
Ken MacKay <kmackay@chromium.org>
2019-10-23 16:56:50 +0000

    Fix link error in component builds
    When building Chromecast unit tests on a component build, I see link
    errors like:
    
    ld.lld: error: undefined symbol: mojo::StructTraits<bluetooth::mojom::UUIDDataView, WTF::String>::Read(bluetooth::mojom::UUIDDataView, WTF::String*)
    >>> referenced by uuid.mojom-shared.h:129 (gen/device/bluetooth/public/mojom/uuid.mojom-shared.h:129)
    >>>               obj/third_party/blink/public/mojom/web_bluetooth_mojo_bindings_blink/web_bluetooth.mojom-blink.o:(mojo::internal::Serializer<bluetooth::mojom::UUIDDataView, WTF::String>::Deserialize(bluetooth::mojom::internal::UUID_Data*, WTF::String*, mojo::internal::SerializationContext*))
    
    This CL fixes those errors.
    
    Change-Id: I2f9fabd66013c33461c76d5e6306c600569da9c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866626
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708658}

--- third_party/blink/renderer/platform/mojo/bluetooth_mojom_traits.h
+++ third_party/blink/renderer/platform/mojo/bluetooth_mojom_traits.h
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/bluetooth/public/mojom/uuid.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/bluetooth/web_bluetooth.mojom-blink.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace mojo {
 
 template <>
-struct StructTraits<::blink::mojom::WebBluetoothDeviceIdDataView, WTF::String> {
+struct PLATFORM_EXPORT
+    StructTraits<::blink::mojom::WebBluetoothDeviceIdDataView, WTF::String> {
   static const WTF::String& device_id(const WTF::String& input) {
     return input;
   }
@@ -23,7 +25,8 @@ struct StructTraits<::blink::mojom::WebBluetoothDeviceIdDataView, WTF::String> {
 };
 
 template <>
-struct StructTraits<bluetooth::mojom::UUIDDataView, WTF::String> {
+struct PLATFORM_EXPORT
+    StructTraits<bluetooth::mojom::UUIDDataView, WTF::String> {
   static const WTF::String& uuid(const WTF::String& input) { return input; }
 
   static bool Read(bluetooth::mojom::UUIDDataView, WTF::String* output);
