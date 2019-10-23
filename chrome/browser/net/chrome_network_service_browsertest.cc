0c531edcf71c4ad02056214a315cb3d42da77d4f
Will Harris <wfh@chromium.org>
2019-10-23 00:09:30 +0000

    Ensure cookie encryption works both in and out of process.
    OSCrypt is a singleton that's process-bound, so verify that
    it behaves for network service running both in and out
    of process.
    
    BUG=1016384
    
    Change-Id: Icdcb97b976a9d282f7593d0f9eb0b152991843a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872457
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708429}

--- chrome/browser/net/chrome_network_service_browsertest.cc
+++ chrome/browser/net/chrome_network_service_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/cookie_config/cookie_store_util.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/network_service_instance.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -66,9 +67,17 @@ void FlushCookies(
 }
 
 // See |NetworkServiceBrowserTest| for content's version of tests.
-class ChromeNetworkServiceBrowserTest : public InProcessBrowserTest {
+class ChromeNetworkServiceBrowserTest
+    : public InProcessBrowserTest,
+      public ::testing::WithParamInterface</*kNetworkServiceInProcess*/ bool> {
  public:
-  ChromeNetworkServiceBrowserTest() {}
+  ChromeNetworkServiceBrowserTest() {
+    bool in_process = GetParam();
+    // Verify that cookie encryption works both in-process and out of process.
+    if (in_process)
+      scoped_feature_list_.InitAndEnableFeature(
+          features::kNetworkServiceInProcess);
+  }
 
  protected:
   mojo::PendingRemote<network::mojom::NetworkContext> CreateNetworkContext(
@@ -86,10 +95,12 @@ class ChromeNetworkServiceBrowserTest : public InProcessBrowserTest {
   }
 
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+
   DISALLOW_COPY_AND_ASSIGN(ChromeNetworkServiceBrowserTest);
 };
 
-IN_PROC_BROWSER_TEST_F(ChromeNetworkServiceBrowserTest, PRE_EncryptedCookies) {
+IN_PROC_BROWSER_TEST_P(ChromeNetworkServiceBrowserTest, PRE_EncryptedCookies) {
   // First set a cookie with cookie encryption enabled.
   mojo::Remote<network::mojom::NetworkContext> context(
       CreateNetworkContext(/*enable_encrypted_cookies=*/true));
@@ -106,7 +117,7 @@ IN_PROC_BROWSER_TEST_F(ChromeNetworkServiceBrowserTest, PRE_EncryptedCookies) {
   FlushCookies(cookie_manager);
 }
 
-IN_PROC_BROWSER_TEST_F(ChromeNetworkServiceBrowserTest, EncryptedCookies) {
+IN_PROC_BROWSER_TEST_P(ChromeNetworkServiceBrowserTest, EncryptedCookies) {
   net::CookieCryptoDelegate* crypto_delegate =
       cookie_config::GetCookieCryptoDelegate();
   std::string ciphertext;
@@ -126,6 +137,14 @@ IN_PROC_BROWSER_TEST_F(ChromeNetworkServiceBrowserTest, EncryptedCookies) {
   EXPECT_EQ(kCookieName, cookies[0].Name());
   EXPECT_EQ("", cookies[0].Value());
 }
+
+INSTANTIATE_TEST_SUITE_P(InProcess,
+                         ChromeNetworkServiceBrowserTest,
+                         ::testing::Values(true));
+
+INSTANTIATE_TEST_SUITE_P(OutOfProcess,
+                         ChromeNetworkServiceBrowserTest,
+                         ::testing::Values(false));
 
 }  // namespace
 }  // namespace content
