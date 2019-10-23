49521c4c44ff9d971d7abdd986d006bdcd0ca273
Peter Kasting <pkasting@chromium.org>
2019-10-23 22:32:45 +0000

    Clean up/simplify StyledLabel, part 1.
    * Makes CreateLabelRange() a member function (and renames it CreateLabel()) to
      allow it to use various members that are currently passed as arguments the
      same way at both callsites.  This makes the code simpler and more
      future-proof.  It also allows moving the color-related functionality in from
      the caller.
    * To allow CreateLabel() to be const, moves the set_listener() call to the
      caller side.  Only one caller needs this anyway.
    * Stop calling AdvanceOneLine() in the "very first line was empty" case, where
      it will have no effect, since |new_line| is false and there are no views to
      adjust.
    * The final post-loop call of AdvanceOneLine() doesn't actually care about
      whether |new_line| is set to false (since the adjusted values won't be read
      afterwards).  Combined with the change above, this means the |new_line| arg
      can be eliminated entirely.
    * Change |max_line_height| (which sounds like the max height of all lines) to
      the less-ambiguous (and shorter) |line_height|, since it's the height of the
      current line.
    * At the end of the loop, adjust |remaining_string| before calling
      AdvanceOneLine() instead of after.  These are independent, so this has no
      effect, but it will allow for a control flow change in a subsequent patch.
    
    Bug: 1015717
    Change-Id: Ie1858c29b70f5c8ff1e9ba005a7fddb5e446e133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875028
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708814}

--- ui/views/controls/styled_label.h
+++ ui/views/controls/styled_label.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+class Label;
 class Link;
 class StyledLabelListener;
 
@@ -183,14 +184,17 @@ class VIEWS_EXPORT StyledLabel : public View, public LinkListener {
   // |width_at_last_size_calculation_|. Returns the needed size.
   gfx::Size CalculateAndDoLayout(int width, bool dry_run);
 
+  // Creates a Label for a given |text| and |style_info|.
+  std::unique_ptr<Label> CreateLabel(const base::string16& text,
+                                     const RangeStyleInfo& style_info) const;
+
   // Adjusts the offsets of the views in a line for alignment and other line
   // parameters.
   void AdvanceOneLine(int* line_number,
                       gfx::Point* offset,
                       int* max_line_height,
                       int width,
-                      std::vector<View*>* views_in_a_line,
-                      bool new_line = true);
+                      std::vector<View*>* views_in_a_line);
 
   // The text to display.
   base::string16 text_;
