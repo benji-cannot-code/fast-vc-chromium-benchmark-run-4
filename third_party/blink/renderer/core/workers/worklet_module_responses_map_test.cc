aeb75ce3320cb5d300df12358566d8dcfe2c215a
Hiroki Nakagawa <nhiroki@chromium.org>
2019-10-23 06:10:03 +0000

    Worker: Move ClientImpl to worklet_module_responses_map_test.cc for cleanup
    ClientImpl is only used from WorkletModuleResponsesMapTest. Originally
    this was in the anonymous namespace in worklet_module_responses_map_test.cc, but
    it was moved out because of Jumbo build breakage (see
    https://crrev.com/c/968363/).
    
    Bug: n/a
    Change-Id: If39397b7320af6228591f61dfad1acb511faf961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874991
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708517}

--- third_party/blink/renderer/core/workers/worklet_module_responses_map_test.cc
+++ third_party/blink/renderer/core/workers/worklet_module_responses_map_test.cc
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/workers/worklet_module_responses_map.h"
+
 #include "base/optional.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/platform/web_url_loader_mock_factory.h"
 #include "third_party/blink/renderer/core/loader/modulescript/module_script_creation_params.h"
 #include "third_party/blink/renderer/core/loader/modulescript/worklet_module_script_fetcher.h"
 #include "third_party/blink/renderer/core/script/modulator.h"
-#include "third_party/blink/renderer/core/workers/worker_fetch_test_helper.h"
 #include "third_party/blink/renderer/platform/loader/testing/fetch_testing_platform_support.h"
 #include "third_party/blink/renderer/platform/loader/testing/mock_fetch_context.h"
 #include "third_party/blink/renderer/platform/loader/testing/test_loader_factory.h"
@@ -35,6 +35,35 @@ class WorkletModuleResponsesMapTest : public testing::Test {
                             MakeGarbageCollected<TestLoaderFactory>()));
     map_ = MakeGarbageCollected<WorkletModuleResponsesMap>();
   }
+
+  class ClientImpl final : public GarbageCollected<ClientImpl>,
+                           public ModuleScriptFetcher::Client {
+    USING_GARBAGE_COLLECTED_MIXIN(ClientImpl);
+
+   public:
+    enum class Result { kInitial, kOK, kFailed };
+
+    void NotifyFetchFinished(
+        const base::Optional<ModuleScriptCreationParams>& params,
+        const HeapVector<Member<ConsoleMessage>>&) override {
+      ASSERT_EQ(Result::kInitial, result_);
+      if (params) {
+        result_ = Result::kOK;
+        params_.emplace(*params);
+      } else {
+        result_ = Result::kFailed;
+      }
+    }
+
+    Result GetResult() const { return result_; }
+    base::Optional<ModuleScriptCreationParams> GetParams() const {
+      return params_;
+    }
+
+   private:
+    Result result_ = Result::kInitial;
+    base::Optional<ModuleScriptCreationParams> params_;
+  };
 
   void Fetch(const KURL& url, ClientImpl* client) {
     ResourceRequest resource_request(url);
