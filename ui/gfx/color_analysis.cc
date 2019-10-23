026699e650a65bfba206ec67cde8f0a711a20f77
Martin Barbella <mbarbella@chromium.org>
2019-10-23 01:00:57 +0000

    Add a fuzzer for color_utils::CalculateKMeanColorOfBitmap.
    This was inspired by crbug.com/970343, and the fuzzer catches that issue
    if its fix is reverted.
    
    Change-Id: I2a4037900e5c21c00b8625bdf0de4e0d349c6104
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874687
    Commit-Queue: Martin Barbella <mbarbella@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708444}

--- ui/gfx/color_analysis.cc
+++ ui/gfx/color_analysis.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/logging.h"
+#include "base/numerics/ranges.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkUnPreMultiply.h"
 #include "ui/gfx/codec/png_codec.h"
@@ -638,7 +639,7 @@ SkColor CalculateKMeanColorOfBitmap(const SkBitmap& bitmap,
   // we can end up creating a larger buffer than we have data for, and the end
   // of the buffer will remain uninitialized after we copy/UnPreMultiply the
   // image data into it).
-  height = std::min(height, bitmap.height());
+  height = base::ClampToRange(height, 0, bitmap.height());
 
   // SkBitmap uses pre-multiplied alpha but the KMean clustering function
   // above uses non-pre-multiplied alpha. Transform the bitmap before we
--- ui/gfx/color_analysis_unittest.cc
+++ ui/gfx/color_analysis_unittest.cc
@@ -324,7 +324,7 @@ TEST_F(ColorAnalysisTest, CalculateKMeanColorOfBitmap) {
   EXPECT_TRUE(ChannelApproximatelyEqual(200, SkColorGetB(color)));
 }
 
-// Regression test for heap-buffer-underlow. https://crbug.com/970343
+// Regression test for heap-buffer-underflow. https://crbug.com/970343
 TEST_F(ColorAnalysisTest, CalculateKMeanColorOfSmallImage) {
   SkBitmap bitmap;
 
