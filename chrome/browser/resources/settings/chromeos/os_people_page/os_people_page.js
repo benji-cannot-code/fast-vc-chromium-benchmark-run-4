d29c6333e1edbffb5a078912610f8433b0fc4090
Anastasiia Nikolaienko <anastasiian@chromium.org>
2019-10-23 18:56:31 +0000

    Fix focus configuration for Sync subpage
    Change route for Sync subpage in focus configuration map for people
    page. onIronSelect_ in settings_animated_pages.js throws an assertion
    error for '#sync-status .subpage-arrow'. It looks like .subpage-arrow
    element was removed in https://crrev.com/c/1742350.
    
    Bug: 1014317
    Change-Id: I7ca645cfae432305616fcf7e2415cebddd4c103b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875101
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Anastasiia Nikolaienko <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708711}

--- chrome/browser/resources/settings/chromeos/os_people_page/os_people_page.js
+++ chrome/browser/resources/settings/chromeos/os_people_page/os_people_page.js
@@ -123,7 +123,7 @@ Polymer({
               settings.routes.SYNC.path,
               loadTimeData.getBoolean('unifiedConsentEnabled') ?
                   '#sync-setup' :
-                  '#sync-status .subpage-arrow');
+                  '#sync-status');
         }
         if (settings.routes.LOCK_SCREEN) {
           map.set(
