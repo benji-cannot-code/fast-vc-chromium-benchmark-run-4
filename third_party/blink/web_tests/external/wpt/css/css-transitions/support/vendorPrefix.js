765b0d5a7fe49d2d69ab299151c4c866e876fc1b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-10-23 15:07:26 +0000

    Import wpt@f87822f99f72472616b0b02cde46e76b47d7ce65
    Using wpt-import in Chromium a72e422cedda055d954098ca765669f6fbac72e5.
    With Chromium commits locally applied on WPT:
    1d5b803fd3 "Add WebVTT support for inline styling - Web Platform Tests"
    707870e688 "CSS: WPT tests property values are supported."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    bjonesbe@adobe.com:
      external/wpt/css/css-shapes
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    hongchan@chromium.org, rtoy@chromium.org:
      external/wpt/webaudio
    jfernandez@igalia.com:
      external/wpt/css/css-align
    kojii@chromium.org:
      external/wpt/css/css-scoping
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    rego@igalia.com:
      external/wpt/css/css-grid
    
    NOAUTOREVERT=true
    TBR=smcgruer
    
    No-Export: true
    Change-Id: I22511fbb2084293f2d65bc6550470813294493ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876368
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#708621}

--- third_party/blink/web_tests/external/wpt/css/css-transitions/support/vendorPrefix.js
+++ third_party/blink/web_tests/external/wpt/css/css-transitions/support/vendorPrefix.js
@@ -10,9 +10,9 @@ var prefixCache = {};
 
 // convert "foo-bar" to "fooBar"
 function camelCase(str) {
-	return str.replace(/\-(\w)/g, function(match, letter){
-		return letter.toUpperCase();
-	});
+    return str.replace(/\-(\w)/g, function(match, letter){
+        return letter.toUpperCase();
+    });
 }
 
 // vendor-prefix a css property
