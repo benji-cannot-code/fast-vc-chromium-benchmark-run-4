ccbfcb8765a2c90bcd47a295ebcdb2ecf5ff0a09
Elly Fong-Jones <ellyjones@chromium.org>
2019-10-23 20:17:06 +0000

    views: remove ContentSettingBubbleContents CreateExtraView
    This change moves ContentSettingBubbleContents from overriding
    CreateExtraView to using SetExtraView. Since the code for creating the
    extra view needs to store quite a few internal parts of the created
    view, it is left as a method on ContentSettingBubbleContents.
    
    Bug: 1011446
    Change-Id: I8c14fffdfe742e3223dbb9431c6ef27761623419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876747
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708755}

--- chrome/browser/ui/views/content_setting_bubble_contents.h
+++ chrome/browser/ui/views/content_setting_bubble_contents.h
@@ -68,7 +68,6 @@ class ContentSettingBubbleContents : public content::WebContentsObserver,
 
   // views::BubbleDialogDelegateView:
   void Init() override;
-  std::unique_ptr<View> CreateExtraView() override;
   bool Accept() override;
   bool Close() override;
   int GetDialogButtons() const override;
@@ -79,6 +78,10 @@ class ContentSettingBubbleContents : public content::WebContentsObserver,
 
   // Applies coloring to the learn more button.
   void StyleLearnMoreButton();
+
+  // Create the extra view for this dialog, which contains any subset of: a
+  // "learn more" button and a "manage" button.
+  std::unique_ptr<View> CreateHelpAndManageView();
 
   // content::WebContentsObserver:
   void DidFinishNavigation(
