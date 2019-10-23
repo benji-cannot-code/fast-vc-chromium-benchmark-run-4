94fa60bca2542b4c282ba24c399a2ac32fe5e429
Christian Dullweber <dullweber@chromium.org>
2019-10-23 14:18:25 +0000

    Disable flaky ServiceProcessControlBrowserTest.LaunchAndReconnect
    The test is failing on linux and windows. ChromeOS seems fine.
    
    Tbr: thestig@chromium.org
    Bug: 978948
    Change-Id: Ia1d3f6077f3da1e6ba8ec15c111e8b6019c0e56f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876411
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708606}

--- chrome/browser/service_process/service_process_control_browsertest.cc
+++ chrome/browser/service_process/service_process_control_browsertest.cc
@@ -208,8 +208,8 @@ IN_PROC_BROWSER_TEST_F(ServiceProcessControlBrowserTest, LaunchAndIPC) {
   EXPECT_TRUE(ServiceProcessControl::GetInstance()->Shutdown());
 }
 
-// Flaky on macOS: https://crbug.com/978948
-#if defined(OS_MACOSX)
+// Flaky on macOS, linux and windows: https://crbug.com/978948
+#if defined(OS_MACOSX) || defined(OS_WIN) || defined(OS_LINUX)
 #define MAYBE_LaunchAndReconnect DISABLED_LaunchAndReconnect
 #else
 #define MAYBE_LaunchAndReconnect LaunchAndReconnect
