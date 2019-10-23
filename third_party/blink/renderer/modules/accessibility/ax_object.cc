97f0e2ceb183279f8bda3956767c765498697667
Aaron Leventhal <aleventhal@chromium.org>
2019-10-23 13:18:53 +0000

    Add origin trial feature name for ARIA annotations
    Link to I2E:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/hxV0yjyS8_4
    
    Bug: 1006767
    Change-Id: I26e453bcc5eb21e38388a4f152e85134a22d1531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1873886
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708595}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -2079,8 +2079,8 @@ ax::mojom::Role AXObject::DetermineAriaRoleAttribute() const {
     case ax::mojom::Role::kAnnotationRevision:
     case ax::mojom::Role::kAnnotationSuggestion:
       UseCounter::Count(GetDocument(), WebFeature::kARIAAnnotationRoles);
-      if (!RuntimeEnabledFeatures::
-              AccessibilityExposeARIAAnnotationsEnabled()) {
+      if (!RuntimeEnabledFeatures::AccessibilityExposeARIAAnnotationsEnabled(
+              GetDocument())) {
         role = ax::mojom::Role::kGenericContainer;
       }
       break;
