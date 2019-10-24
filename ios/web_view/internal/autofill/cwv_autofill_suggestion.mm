1792174ed4073db2b44abf1443c2c6691dd75b94
John Z Wu <jzw@chromium.org>
2019-10-24 06:58:27 +0000

    Return •••••••• for password autofill suggestions
    Change-Id: I1224ab1cb4c399f5e9cb2001524ef53cca6d83bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875277
    Auto-Submit: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708926}

--- ios/web_view/internal/autofill/cwv_autofill_suggestion.mm
+++ ios/web_view/internal/autofill/cwv_autofill_suggestion.mm
@@ -47,7 +47,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (NSString*)displayDescription {
-  return [_formSuggestion.displayDescription copy];
+  if ([self isPasswordSuggestion]) {
+    // An opaque password string used to hide the true length of the password.
+    return @"••••••••";
+  } else {
+    return [_formSuggestion.displayDescription copy];
+  }
 }
 
 - (UIImage* __nullable)icon {
