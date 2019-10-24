a2bf6c4507af37f456273b62aa4583019d4064d6
Suzy Li <suzyli@google.com>
2019-10-24 23:00:39 +0000

    Revert "Remove unused callback and handler for isUVPAA"
    This reverts commit 05e16b5279cffa6bf47fdcafd981b995cbc1bfad.
    
    Reason for revert: Clank should use the GmsCore implementation of isUVPAA, see crbug.com/1017587
    
    Original change's description:
    > Remove unused callback and handler for isUVPAA
    >
    > This CL aims at removing rest of the code to use gmscore isUVPAA.
    >
    > This CL is the third of 3 CLs to add isUVPAA implemenration on clank
    > and remove the flow to call isUVPAA that gmscore supports.
    > First CL:
    > https://chromium-review.googlesource.com/c/chromium/src/+/1739973
    > Second CL:
    > https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/1587528
    >
    > Bug: n/a
    > Change-Id: I8f045738e3b3213184e7109bb42f1924ad8c55b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1750406
    > Commit-Queue: Suzy Li <suzyli@google.com>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#686483}
    
    TBR=kenrb@chromium.org,suzyli@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1017587
    Change-Id: Ieb9e7b9c18a9f36775fdf4b579637a34e55a4959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880127
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Suzy Li <suzyli@google.com>
    Cr-Commit-Position: refs/heads/master@{#709258}

--- chrome/android/java/src/org/chromium/chrome/browser/webauth/Fido2ApiHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webauth/Fido2ApiHandler.java
@@ -51,4 +51,7 @@ public class Fido2ApiHandler {
 
     protected void getAssertion(PublicKeyCredentialRequestOptions options,
             RenderFrameHost frameHost, HandlerResponseCallback callback) {}
+
+    protected void isUserVerifyingPlatformAuthenticatorAvailable(
+            RenderFrameHost frameHost, HandlerResponseCallback callback) {}
 }
