294a71d5d1f5c31e751ec8a8290d3effa771d96f
Christopher Grant <cjgrant@chromium.org>
2019-10-24 17:03:06 +0000

    [DevUI DFM] Move chrome://webrtc-internals resources to the DFM.
    Follow the lead of other DevUI pages, and move WebRTC internals
    resources (those used in chrome://webrtc-internals) into the DFM. This
    removes ~25 KB from the main .pak file on Android.
    
    Bug: 927131
    Change-Id: Ia243ec46e728a711cc7c3bb8df10cb661300d1a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869014
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709092}

--- content/browser/webrtc/webrtc_internals_ui.cc
+++ content/browser/webrtc/webrtc_internals_ui.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/webrtc/webrtc_internals_ui.h"
 
+#include "content/browser/webrtc/resources/grit/webrtc_internals_resources.h"
 #include "content/browser/webrtc/webrtc_internals_message_handler.h"
-#include "content/grit/content_resources.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
