bb28e6242ff289b99b572dbabcd673a84f7a0dec
Eric Willigers <ericwilligers@chromium.org>
2019-10-24 07:11:53 +0000

    desktop-pwas: Run bookmark tests with BMO and extensions
    The AppController methods CreatedForInstalledPwa() and
    GetThemeColor() are now tested with BMO web apps, as well
    as with the extensions implementation of bookmark apps.
    
    Bug: 876576
    Change-Id: I6e99a0a94855aa9f077d6055155220254ec5bff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874072
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708928}

--- chrome/browser/web_applications/web_app_install_task_unittest.cc
+++ chrome/browser/web_applications/web_app_install_task_unittest.cc
@@ -370,6 +370,7 @@ TEST_F(WebAppInstallTaskTest, InstallFromWebContents) {
   const std::string description = "Description";
   const GURL scope = GURL("https://example.com/scope");
   const base::Optional<SkColor> theme_color = 0xAABBCCDD;
+  const base::Optional<SkColor> expected_theme_color = 0xFFBBCCDD;  // Opaque.
 
   const AppId app_id = GenerateAppIdFromURL(url);
 
@@ -403,7 +404,7 @@ TEST_F(WebAppInstallTaskTest, InstallFromWebContents) {
   EXPECT_EQ(description, web_app->description());
   EXPECT_EQ(url, web_app->launch_url());
   EXPECT_EQ(scope, web_app->scope());
-  EXPECT_EQ(theme_color, web_app->theme_color());
+  EXPECT_EQ(expected_theme_color, web_app->theme_color());
 }
 
 TEST_F(WebAppInstallTaskTest, AlreadyInstalled) {
@@ -503,6 +504,7 @@ TEST_F(WebAppInstallTaskTest, InstallableCheck) {
   const std::string manifest_name = "Name from Manifest";
   const GURL manifest_scope = GURL("https://example.com/scope");
   const base::Optional<SkColor> manifest_theme_color = 0xAABBCCDD;
+  const base::Optional<SkColor> expected_theme_color = 0xFFBBCCDD;  // Opaque.
 
   {
     auto manifest = std::make_unique<blink::Manifest>();
@@ -542,7 +544,7 @@ TEST_F(WebAppInstallTaskTest, InstallableCheck) {
   EXPECT_EQ(manifest_start_url, web_app->launch_url());
   EXPECT_EQ(renderer_description, web_app->description());
   EXPECT_EQ(manifest_scope, web_app->scope());
-  EXPECT_EQ(manifest_theme_color, web_app->theme_color());
+  EXPECT_EQ(expected_theme_color, web_app->theme_color());
 }
 
 TEST_F(WebAppInstallTaskTest, GetIcons) {
