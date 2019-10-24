6aeef851b1d3c08d89f7d0b3f4b4c2ad4f138463
Bo Liu <boliu@chromium.org>
2019-10-24 18:54:39 +0000

    weblayer: Use static inner class where appropriate
    Change-Id: I95df3ea154bb0a73b8842bd485129c4269d1eff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879707
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709139}

--- weblayer/public/java/org/chromium/weblayer/BrowserController.java
+++ weblayer/public/java/org/chromium/weblayer/BrowserController.java
@@ -153,7 +153,7 @@ public final class BrowserController {
         }
     }
 
-    private final class DownloadDelegateClientImpl extends IDownloadDelegateClient.Stub {
+    private static final class DownloadDelegateClientImpl extends IDownloadDelegateClient.Stub {
         private final DownloadDelegate mDelegate;
 
         DownloadDelegateClientImpl(DownloadDelegate delegate) {
@@ -172,7 +172,7 @@ public final class BrowserController {
         }
     }
 
-    private final class FullscreenDelegateClientImpl extends IFullscreenDelegateClient.Stub {
+    private static final class FullscreenDelegateClientImpl extends IFullscreenDelegateClient.Stub {
         private FullscreenDelegate mDelegate;
 
         /* package */ FullscreenDelegateClientImpl(FullscreenDelegate delegate) {
