827b4b7ef88e903ac51d39332274346e4eb013c4
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-10-24 18:54:18 +0000

    Import wpt@511fea02e78967d7f7fd99c1a884b4c06468dd3e
    Using wpt-import in Chromium 2c37032bffb7cbfaecc0514782f99d77b1a72583.
    With Chromium commits locally applied on WPT:
    1d5b803fd3 "Add WebVTT support for inline styling - Web Platform Tests"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    guidou@chromium.org, armax@chromium.org:
      external/wpt/mediacapture-streams
    rego@igalia.com:
      external/wpt/css/css-grid
    
    NOAUTOREVERT=true
    TBR=smcgruer
    
    No-Export: true
    Change-Id: Iaaad527fbb79df9ea9d0f299baab29ce297dd418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1878948
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#709138}

--- third_party/blink/web_tests/external/wpt/compression/decompression-uint8array-output.tentative.any.js
+++ third_party/blink/web_tests/external/wpt/compression/decompression-uint8array-output.tentative.any.js
@@ -1,5 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=worker
+// META: timeout=long
+//
+// This test isn't actually slow usually, but sometimes it takes >10 seconds on
+// Firefox with service worker for no obvious reason.
 
 'use strict';
 
---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/fetch-error-worker.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts("/resources/testharness.js");
+
+function doTest(event)
+{
+    if (!event.request.url.includes("fetch-error-test"))
+        return;
+
+    let counter = 0;
+    const stream = new ReadableStream({ pull: controller => {
+        switch (++counter) {
+        case 1:
+            controller.enqueue(new Uint8Array([1]));
+            return;
+        default:
+            // We asynchronously error the stream so that there is ample time to resolve the fetch promise and call text() on the response.
+            step_timeout(() => controller.error("Sorry"), 50);
+        }
+    }});
+    event.respondWith(new Response(stream));
+}
+
+self.addEventListener("fetch", doTest);
