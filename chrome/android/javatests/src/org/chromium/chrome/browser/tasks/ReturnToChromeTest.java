e62d416ef8662256da13d7a2bfdb7f82ee9254f0
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-10-24 07:40:45 +0000

    [Instant Start] Ignore background time in TabSwitcherOnReturn=Immediate
    When TabSwitcherOnReturn is set to "Immediate", the tab switcher should
    show up immediately regardless of whether the last background timestamp
    is available or not.
    
    "Immediate" is a setting for dev testing only.
    
    Bug: 1016952
    Change-Id: I3ac40d6695089ea9b14e3841b8834ae3a6b44b7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1877812
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#708934}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tasks/ReturnToChromeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tasks/ReturnToChromeTest.java
@@ -56,7 +56,7 @@ public class ReturnToChromeTest {
     public void setUp() {
         FeatureUtilities.setGridTabSwitcherEnabledForTesting(true);
 
-        mActivityTestRule.startMainActivityFromLauncher();
+        mActivityTestRule.startMainActivityOnBlankPage();
         mActivity = mActivityTestRule.getActivity();
 
         setupTabs();
