36bdd582fd0b07cd51338aec26e190b72487a72d
Nazerke <nazerke@google.com>
2019-10-24 09:04:49 +0000

    [iOS][eg2] Convert handoff manager EG tests to EG2
    This CL adds all handoff manager tests to EG2 target
    and updates headers and adds eg2_test source_set in
    build file. Moreover, new app interface for handoff
    tests is being introduced.
    
    Bug: 987646
    Change-Id: I3410f778e9f9daece1767a4f0a73c168f6e9aef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872209
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Cr-Commit-Position: refs/heads/master@{#708953}

---
+++ ios/chrome/browser/device_sharing/handoff_manager_app_interface.mm
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/device_sharing/handoff_manager_app_interface.h"
+
+#import "components/handoff/handoff_manager.h"
+#import "ios/chrome/browser/device_sharing/device_sharing_manager.h"
+#import "ios/chrome/test/app/chrome_test_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation HandoffManagerAppInterface
+
++ (NSURL*)currentUserActivityWebPageURL {
+  HandoffManager* manager =
+      [chrome_test_util::GetDeviceSharingManager() handoffManager];
+  return manager.userActivityWebpageURL;
+}
+
+@end
