2fea817cdf1307e17bcc813633a39e99a1e7421a
Antonio Gomes <tonikitoo@igalia.com>
2019-10-24 16:31:26 +0000

    Factor AudioCapturerSourceTestingPlatformSupport out of processed_local_audio_source_test.cc
    ... so that it can be used on more testing files.
    
    Note that, given that one of the methods reimplemented need access
    blink::WebLocalFrame, the newly added files can not reside in
    renderer/platform/testing, where other similar files are.
    
    BUG=787254
    R=guidou@chromium.org
    
    Change-Id: I1dd6154407d744daab6cdbad0003bf2ddafe7485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1873986
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#709074}

---
+++ third_party/blink/renderer/modules/mediastream/testing_platform_support_with_mock_audio_capture_source.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/mediastream/testing_platform_support_with_mock_audio_capture_source.h"
+
+namespace blink {
+
+scoped_refptr<media::AudioCapturerSource>
+AudioCapturerSourceTestingPlatformSupport::NewAudioCapturerSource(
+    WebLocalFrame* web_frame,
+    const media::AudioSourceParameters& params) {
+  EXPECT_EQ(nullptr, web_frame);
+  return mock_audio_capturer_source_;
+}
+
+}  // namespace blink
