62f5e79b32db8fd44e5c1c62930e6a0f6fbb1ba8
Steven Bennetts <stevenjb@chromium.org>
2019-10-24 15:38:37 +0000

    WebUI: compress about_os_credits
    Bug: 965089
    Change-Id: I1a29243c5dd4ab9a446baa7c4ddebb3408e7f5dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875485
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709046}

--- chrome/browser/ui/webui/about_ui.cc
+++ chrome/browser/ui/webui/about_ui.cc
@@ -412,9 +412,9 @@ class ChromeOSCreditsHandler
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
     // If we fail to load Chrome OS credits from disk, load it from resources.
     if (contents_.empty() && path_ != kKeyboardUtilsPath) {
-      contents_ = ui::ResourceBundle::GetSharedInstance()
-                      .GetRawDataResource(IDR_OS_CREDITS_HTML)
-                      .as_string();
+      contents_ =
+          ui::ResourceBundle::GetSharedInstance().DecompressDataResource(
+              IDR_OS_CREDITS_HTML);
     }
     callback_.Run(base::RefCountedString::TakeString(&contents_));
   }
@@ -482,9 +482,9 @@ class LinuxCreditsHandler
     // resources.
     // TODO(rjwright): Add a linux-specific placeholder in resources.
     if (contents_.empty() && path_ != kKeyboardUtilsPath) {
-      contents_ = ui::ResourceBundle::GetSharedInstance()
-                      .GetRawDataResource(IDR_OS_CREDITS_HTML)
-                      .as_string();
+      contents_ =
+          ui::ResourceBundle::GetSharedInstance().DecompressDataResource(
+              IDR_OS_CREDITS_HTML);
     }
     callback_.Run(base::RefCountedString::TakeString(&contents_));
   }
