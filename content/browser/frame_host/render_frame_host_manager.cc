3ab664689b1bc011b6d43f242914b4b3f514f3d9
Dave Tapuska <dtapuska@chromium.org>
2019-10-24 16:27:10 +0000

    Move FrameMsg_ResetContentSecurityPolicy to mojo
    Move FrameMsg_ResetContentSecurityPolicy to a RemoteFrame mojo
    interface.
    
    BUG=1008432
    
    Change-Id: I8b96015cefc57e297594ac6d2ebc4024ffd698e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1873400
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709069}

--- content/browser/frame_host/render_frame_host_manager.cc
+++ content/browser/frame_host/render_frame_host_manager.cc
@@ -931,8 +931,8 @@ void RenderFrameHostManager::OnDidAddContentSecurityPolicies(
 
 void RenderFrameHostManager::OnDidResetContentSecurityPolicy() {
   for (const auto& pair : proxy_hosts_) {
-    pair.second->Send(
-        new FrameMsg_ResetContentSecurityPolicy(pair.second->GetRoutingID()));
+    pair.second->GetAssociatedRemoteFrame()
+        ->ResetReplicatedContentSecurityPolicy();
   }
 }
 
--- third_party/blink/renderer/core/frame/remote_frame.cc
+++ third_party/blink/renderer/core/frame/remote_frame.cc
@@ -253,6 +253,10 @@ void RemoteFrame::WillEnterFullscreen() {
       Fullscreen::RequestType::kPrefixedForCrossProcessDescendant);
 }
 
+void RemoteFrame::ResetReplicatedContentSecurityPolicy() {
+  GetSecurityContext()->ResetReplicatedContentSecurityPolicy();
+}
+
 bool RemoteFrame::IsIgnoredForHitTest() const {
   HTMLFrameOwnerElement* owner = DeprecatedLocalOwner();
   if (!owner || !owner->GetLayoutObject())
