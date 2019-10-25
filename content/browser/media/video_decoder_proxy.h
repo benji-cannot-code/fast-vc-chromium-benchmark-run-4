d7d53edfbf6f6ee49011a077af988a67146e0b52
Ken MacKay <kmackay@chromium.org>
2019-10-25 22:39:41 +0000

    Export VideoDecoderProxy to avoid component build link errors
    I am seeing component build failures on Chromecast unit tests; this CL fixes them.
    
    Change-Id: Ia2e1cc9d17fa9ef754a74483720cf82fd5d4140e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879318
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709673}

--- content/browser/media/video_decoder_proxy.h
+++ content/browser/media/video_decoder_proxy.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_checker.h"
 #include "base/unguessable_token.h"
 #include "build/build_config.h"
+#include "content/common/content_export.h"
 #include "media/mojo/buildflags.h"
 #include "media/mojo/mojom/interface_factory.mojom.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
@@ -23,7 +24,7 @@ namespace content {
 // RenderProcessHostImpl. Unlike MediaInterfaceProxy, only
 // CreateVideoDecoder() is implemented. This allows WebRTC to create
 // MojoVideoDecoder instances without a RenderFrame.
-class VideoDecoderProxy : public media::mojom::InterfaceFactory {
+class CONTENT_EXPORT VideoDecoderProxy : public media::mojom::InterfaceFactory {
  public:
   VideoDecoderProxy();
   ~VideoDecoderProxy() final;
