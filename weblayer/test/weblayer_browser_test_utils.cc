67d4a2a2f847e7bd534cb995d0c8468684e3a213
Clark DuVall <cduvall@chromium.org>
2019-10-25 18:14:50 +0000

    [WebLayer] Add tests for chrome://weblayer page
    This page recently broke (fix in http://crrev.com/c/1880171), so tests
    seem like a good idea.
    
    This also adds a test helper for executing javascript, and makes the
    NavigateAndWaitForCompletion function work more similarly to the one in
    the instrumentation tests by waiting until the page is finished loading.
    
    Bug: 1017924
    Change-Id: I8ca048f9a9a92808abf6cfa5d0e091550cc3d9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880173
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709552}

--- weblayer/test/weblayer_browser_test_utils.cc
+++ weblayer/test/weblayer_browser_test_utils.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "weblayer/test/weblayer_browser_test_utils.h"
 
 #include "base/run_loop.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/test/bind_test_util.h"
 #include "url/gurl.h"
 #include "weblayer/public/browser_controller.h"
 #include "weblayer/public/navigation.h"
@@ -41,8 +43,10 @@ class TestNavigationObserver : public NavigationObserver {
   // NavigationObserver implementation:
   void NavigationCompleted(Navigation* navigation) override {
     if (navigation->GetURL() == url_ &&
-        event_ == NavigationEventToObserve::Completion)
-      std::move(closure_).Run();
+        event_ == NavigationEventToObserve::Completion) {
+      navigation_complete_ = true;
+      CheckComplete();
+    }
   }
 
   void NavigationFailed(Navigation* navigation) override {
@@ -52,10 +56,22 @@ class TestNavigationObserver : public NavigationObserver {
     }
   }
 
+  void LoadStateChanged(bool is_loading, bool to_different_document) override {
+    done_loading_ = !is_loading;
+    CheckComplete();
+  }
+
+  void CheckComplete() {
+    if (done_loading_ && navigation_complete_)
+      std::move(closure_).Run();
+  }
+
   base::OnceClosure closure_;
   const GURL url_;
   NavigationEventToObserve event_;
   BrowserController* browser_;
+  bool done_loading_ = false;
+  bool navigation_complete_ = false;
 };
 
 // Navigates to |url| in |shell| and waits for |event| to occur.
@@ -81,6 +97,20 @@ void NavigateAndWaitForCompletion(const GURL& url, Shell* shell) {
 void NavigateAndWaitForFailure(const GURL& url, Shell* shell) {
   NavigateAndWaitForEvent(
       url, shell, TestNavigationObserver::NavigationEventToObserve::Failure);
+}
+
+base::Value ExecuteScript(Shell* shell, const std::string& script) {
+  base::Value final_result;
+  base::RunLoop run_loop;
+  shell->browser_controller()->ExecuteScript(
+      base::ASCIIToUTF16(script),
+      base::BindLambdaForTesting(
+          [&run_loop, &final_result](base::Value result) {
+            final_result = std::move(result);
+            run_loop.Quit();
+          }));
+  run_loop.Run();
+  return final_result;
 }
 
 }  // namespace weblayer
