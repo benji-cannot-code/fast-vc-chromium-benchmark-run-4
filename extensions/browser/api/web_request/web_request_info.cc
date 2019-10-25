f86190e7f033d6d8f3938a777714aeca58694c5b
Lucas Furukawa Gadani <lfg@chromium.org>
2019-10-25 20:04:18 +0000

    Convert extensions to use the URLResponseHead mojom types.
    Bug: 984550
    Change-Id: I885fc575b49c4049b83b17cc4c2248daf2525b4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876974
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709596}

--- extensions/browser/api/web_request/web_request_info.cc
+++ extensions/browser/api/web_request/web_request_info.cc
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/upload_bytes_element_reader.h"
 #include "net/base/upload_data_stream.h"
 #include "net/base/upload_file_element_reader.h"
-#include "services/network/public/cpp/resource_response.h"
 #include "services/network/public/mojom/network_context.mojom.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 #include "services/network/url_loader.h"
 
 namespace keys = extension_web_request_api_constants;
@@ -245,7 +245,7 @@ WebRequestInfo::WebRequestInfo(WebRequestInfoInitParams params)
 WebRequestInfo::~WebRequestInfo() = default;
 
 void WebRequestInfo::AddResponseInfoFromResourceResponse(
-    const network::ResourceResponseHead& response) {
+    const network::mojom::URLResponseHead& response) {
   response_headers = response.headers;
   if (response_headers)
     response_code = response_headers->response_code();
--- extensions/browser/api/web_request/web_request_proxying_url_loader_factory.h
+++ extensions/browser/api/web_request/web_request_proxying_url_loader_factory.h
@@ -28,10 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/completion_once_callback.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/resource_request.h"
-#include "services/network/public/cpp/resource_response.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 #include "url/gurl.h"
 
 namespace extensions {
@@ -154,7 +154,7 @@ class WebRequestProxyingURLLoaderFactory
     // |OnHeadersReceived()| and request completion or restart. Pointers to
     // these fields are stored in a |BlockedRequest| (created and owned by
     // ExtensionWebRequestEventRouter) through much of the request's lifetime.
-    network::ResourceResponseHead current_response_;
+    network::mojom::URLResponseHeadPtr current_response_;
     scoped_refptr<net::HttpResponseHeaders> override_headers_;
     GURL redirect_url_;
 
