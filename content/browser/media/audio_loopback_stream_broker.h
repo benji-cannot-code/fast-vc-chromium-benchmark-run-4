f1722bbb10c8fb939e2e7d4ffbd056e3fa2ad8ad
Miyoung Shin <myid.shin@igalia.com>
2019-10-25 16:38:07 +0000

    Convert AudioInputStreamObserver to new Mojo types
    This CL converts AudioInputStreamObserverPtr and Binding<T> in
    content to the new Mojo type.
    
    Bug: 955171
    Change-Id: I8dae882be3afbe37ed2a4ff25e6c876b42ac2b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866749
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709504}

--- content/browser/media/audio_loopback_stream_broker.h
+++ content/browser/media/audio_loopback_stream_broker.h
@@ -18,9 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_parameters.h"
 #include "media/mojo/mojom/audio_data_pipe.mojom.h"
 #include "media/mojo/mojom/audio_input_stream.mojom.h"
-#include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace audio {
@@ -79,7 +79,7 @@ class CONTENT_EXPORT AudioLoopbackStreamBroker final
 
   mojo::Remote<mojom::RendererAudioInputStreamFactoryClient>
       renderer_factory_client_;
-  mojo::Binding<AudioInputStreamObserver> observer_binding_;
+  mojo::Receiver<AudioInputStreamObserver> observer_receiver_{this};
   mojo::PendingReceiver<media::mojom::AudioInputStreamClient> client_receiver_;
 
   base::WeakPtrFactory<AudioLoopbackStreamBroker> weak_ptr_factory_{this};
