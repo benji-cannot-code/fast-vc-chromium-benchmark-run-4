3aeebf2624f6e72a26004eb9ea2e02826aac17e4
Xing Liu <xingliu@chromium.org>
2019-10-25 20:18:46 +0000

    Chime: Add chrome layer code to register to Chime SDK.
    This CL adds chrome layer code to register to Chime. The actual
    implementation is in an Android DFM in downstream.
    
    The code is set up according to new modulization effort, that
    chrome/browser/notifications/chime/android is self contained.
    
    Bug: 1013386
    Change-Id: Iafc4269e1597af30af624e9772c6a38edcb4e5a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1874576
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709605}

---
+++ chrome/browser/notifications/chime/android/features.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/notifications/chime/android/features.h"
+#include "chrome/browser/notifications/chime/android/jni_headers/ChimeSession_jni.h"
+
+jboolean JNI_ChimeSession_IsEnabled(JNIEnv* env) {
+  return base::FeatureList::IsEnabled(
+      notifications::features::kChimeNotificationPlatform);
+}
+
+namespace notifications {
+namespace features {
+
+const base::Feature kChimeNotificationPlatform{
+    "ChimeNotificationPlatform", base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace features
+}  // namespace notifications
