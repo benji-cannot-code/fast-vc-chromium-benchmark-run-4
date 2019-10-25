6250807e78ad40851b34c71cf1a740c08e41b196
Yutaka Hirano <yhirano@chromium.org>
2019-10-25 17:19:15 +0000

    Set kLow rather than kVeryLow priority for beacon and friends
    Current the following requests have ResourceLoadPriority::kVeryLow.
    
     - beacon
     - a ping
     - csp report
    
    This CL introduces a flag. When it's enabled those requests will have
    ResourceLoadPriority::kLow.
    
    Bug: 1018081
    Change-Id: I68d5df324739999aaebfee6a1d7fa9c065c93013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880897
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709527}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -133,6 +133,8 @@ BLINK_COMMON_EXPORT extern const base::Feature kCompositeCrossOriginIframes;
 
 BLINK_COMMON_EXPORT extern const base::Feature kSubresourceRedirect;
 
+BLINK_COMMON_EXPORT extern const base::Feature kSetLowPriorityForBeacon;
+
 }  // namespace features
 }  // namespace blink
 
