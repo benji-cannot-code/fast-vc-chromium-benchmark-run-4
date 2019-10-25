41ee83c6f3971164a94ebe7a381aa095c27c886f
Tim Volodine <timvolodine@chromium.org>
2019-10-25 13:14:04 +0000

    [WebLayer] Add GmsBridge upstream classes and the relevant build configs
    In this patch:
    
    - add the abstract GmsBridge class and its corresponsing upstream
      implementation (GmsBridgeImpl.java). GmsBridge class manages
      functionality related to Google Mobile Services (GMS).
    
    - add the initialization of safebrowsing handler on WebLayer startup.
    
    - add .gn build deps and targets needed for the relevant upstream
      targets. In the future a different GmsBridgeImpl class will be
      compiled-in depending on whether an internal clank/ checkout
      is present.
    
    BUG=1015418
    
    Change-Id: I5a45023a0ab6f31ce6db1daa7f62bf7c2579a84b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876032
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709447}

---
+++ weblayer/browser/java/org/chromium/weblayer_private/GmsBridgeImpl.java
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer_private;
+
+/**
+ * Instantiable version of {@link GmsBridge}, don't add anything to this class!
+ * Downstream targets may provide a different implementation.
+ */
+public class GmsBridgeImpl extends GmsBridge {}
