2d1eea372641c5a01d94ae722352f9ddc552d19e
Xiaohui Chen <xiaohuic@google.com>
2019-10-25 17:58:34 +0000

    assistant: add query source warmer_welcome
    Bug: None
    Test: locally build and see histogram recorded
    Change-Id: I09096840496378551ad84301972d46c2f77d060d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879654
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709549}

--- chromeos/services/assistant/assistant_manager_service_impl.h
+++ chromeos/services/assistant/assistant_manager_service_impl.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/assistant/assistant_manager_service.h"
 #include "chromeos/services/assistant/assistant_settings_manager_impl.h"
 #include "chromeos/services/assistant/chromium_api_delegate.h"
+#include "chromeos/services/assistant/public/mojom/assistant.mojom-shared.h"
 #include "chromeos/services/assistant/public/mojom/assistant.mojom.h"
 #include "libassistant/shared/internal_api/assistant_manager_delegate.h"
 #include "libassistant/shared/public/conversation_state_listener.h"
@@ -290,6 +291,11 @@ class COMPONENT_EXPORT(ASSISTANT_SERVICE) AssistantManagerServiceImpl
   void RecordQueryResponseTypeUMA();
 
   void UpdateMediaState();
+
+  std::string NewPendingInteraction(
+      mojom::AssistantInteractionType interaction_type,
+      mojom::AssistantQuerySource source,
+      const std::string& query);
 
   ash::mojom::AssistantAlarmTimerController* assistant_alarm_timer_controller();
   ash::mojom::AssistantNotificationController*
