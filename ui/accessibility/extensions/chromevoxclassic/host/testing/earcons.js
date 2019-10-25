4753e71b914d0353eb275530a34e0f22640926b4
David Tseng <dtseng@chromium.org>
2019-10-25 03:44:47 +0000

    Migrate chromevox classic tests
    Change-Id: I8d8b30665ac9627c010002561e710449311a952b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879333
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709354}

---
+++ ui/accessibility/extensions/chromevoxclassic/host/testing/earcons.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Dummy earcons implementation for testing.
+ *
+ */
+
+goog.provide('cvox.TestEarcons');
+
+goog.require('cvox.AbstractEarcons');
+goog.require('cvox.HostFactory');
+
+/**
+ * @constructor
+ * @extends {cvox.AbstractEarcons}
+ */
+cvox.TestEarcons = function() {
+  cvox.Earcon.call(this);
+};
+goog.inherits(cvox.TestEarcons, cvox.AbstractEarcons);
+
+/** @override */
+cvox.HostFactory.earconsConstructor = cvox.TestEarcons;
