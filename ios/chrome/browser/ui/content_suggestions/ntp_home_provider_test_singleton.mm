34bc60267eb02266bf0aca0eabdaa898e320962e
Gauthier Ambard <gambard@chromium.org>
2019-10-25 17:12:59 +0000

    [iOS] Convert NTPHome test to EG2
    OmniboxWidth and OmniboxWidthBetween are moved to the egtest.mm file
    directly to avoid having a more complicated interface.
    Suggestions() uses are replaced by -addNumberOfSuggestions:
    additionalSuggestionsURL:.
    
    Bug: 987646
    Change-Id: I58535b52919fbd1d907c8a72c611e2fbb7f63f67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871888
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709521}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_provider_test_singleton.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_provider_test_singleton.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/ntp_snippets/content_suggestion.h"
 #import "ios/chrome/browser/ui/content_suggestions/ntp_home_test_utils.h"
+#include "ios/chrome/test/base/scoped_block_swizzler.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -19,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ntp_snippets::MockContentSuggestionsProvider* _provider;
   std::unique_ptr<ntp_snippets::AdditionalSuggestionsHelper>
       _additionalSuggestionsHelper;
+  std::unique_ptr<ScopedBlockSwizzler> _swizzler;
+  __block BOOL _tapped;
 }
 
 + (instancetype)sharedInstance {
@@ -52,6 +55,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                            ntp_snippets::KnownCategories::ARTICLES)});
   _provider = provider.get();
   service->RegisterProvider(std::move(provider));
+}
+
+- (void)swizzleLocationBarCoordinatorSearchButton {
+  _tapped = NO;
+  _swizzler = std::make_unique<ScopedBlockSwizzler>(
+      NSClassFromString(@"LocationBarCoordinator"),
+      NSSelectorFromString(@"focusOmniboxFromSearchButton"), ^{
+        _tapped = YES;
+      });
+}
+- (void)resetSwizzle {
+  _swizzler.reset();
+}
+
+- (BOOL)locationBarCoordinatorSearchButtonMethodCalled {
+  return _tapped;
 }
 
 @end
