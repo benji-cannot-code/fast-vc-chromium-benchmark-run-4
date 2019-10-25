c46a79b16cd4939b42797dd1c95cc808f35817e3
Francois Doray <fdoray@chromium.org>
2019-10-25 21:04:57 +0000

    [threadpool] Remove _MayBlock suffixes (reland).
    This is a reland of https://crrev.com/c/chromium/src/+/1813780,
    without switching to STATIC_HISTOGRAM_POINTER_GROUP, in order to
    assess whether that caused the regressions in https://crbug.com/1006940
    Diff: https://crrev.com/c/chromium/src/+/1872425/1..3
    
    Since the blocking and non-blocking have been merged, there is no
    need to report data for blocking tasks in separate histograms.
    
    This CL also makes passing an |histogram_label| to the constructor
    of ThreadPoolImpl optional. If no |histogram_label| is provided, no
    histograms are recorded. This CL takes advantage of that to not record
    histograms from TaskEnvironment.
    
    TBR=altimin@chromium.org
    
    Bug: 1006940, 1002318, 995496, 982145, 980409, 972890, 972888, 972883
    Change-Id: I0ac49273e2a25b217c1c93216737ae985f6d053d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872425
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709635}

--- base/task/thread_pool/task_tracker.h
+++ base/task/thread_pool/task_tracker.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 class ConditionVariable;
-class HistogramBase;
 
 namespace internal {
 
@@ -55,7 +54,8 @@ enum class CanRunPolicy {
 // and records metrics and trace events. This class is thread-safe.
 class BASE_EXPORT TaskTracker {
  public:
-  // |histogram_label| is used as a suffix for histograms, it must not be empty.
+  // |histogram_label| is used to label histograms. No histograms are recorded
+  // if it is empty.
   TaskTracker(StringPiece histogram_label);
 
   virtual ~TaskTracker();
@@ -132,26 +132,15 @@ class BASE_EXPORT TaskTracker {
   // no tasks are blocking shutdown).
   bool IsShutdownComplete() const;
 
-  enum class LatencyHistogramType {
-    // Records the latency of each individual task posted through TaskTracker.
-    TASK_LATENCY,
-    // Records the latency of heartbeat tasks which are independent of current
-    // workload. These avoid a bias towards TASK_LATENCY reporting that high-
-    // priority tasks are "slower" than regular tasks because high-priority
-    // tasks tend to be correlated with heavy workloads.
-    HEARTBEAT_LATENCY,
-  };
-
   // Records two histograms
   // 1. ThreadPool.[label].HeartbeatLatencyMicroseconds.[suffix]:
   //    Now() - posted_time
   // 2. ThreadPool.[label].NumTasksRunWhileQueuing.[suffix]:
   //    GetNumTasksRun() - num_tasks_run_when_posted.
   // [label] is the histogram label provided to the constructor.
-  // [suffix] is derived from |task_priority| and |may_block|.
+  // [suffix] is derived from |task_priority|.
   void RecordHeartbeatLatencyAndTasksRunWhileQueuingHistograms(
       TaskPriority task_priority,
-      bool may_block,
       TimeTicks posted_time,
       int num_tasks_run_when_posted) const;
 
@@ -214,10 +203,9 @@ class BASE_EXPORT TaskTracker {
   // manner.
   void CallFlushCallbackForTesting();
 
-  // Records |Now() - posted_time| to the appropriate |latency_histogram_type|
-  // based on |task_traits|.
-  void RecordLatencyHistogram(LatencyHistogramType latency_histogram_type,
-                              TaskTraits task_traits,
+  // Records |Now() - posted_time| to the
+  // ThreadPool.TaskLatencyMicroseconds.[label].[priority] histogram.
+  void RecordLatencyHistogram(TaskPriority priority,
                               TimeTicks posted_time) const;
 
   void IncrementNumTasksRun();
@@ -230,6 +218,9 @@ class BASE_EXPORT TaskTracker {
                                    Task* task);
 
   TaskAnnotator task_annotator_;
+
+  // Suffix for histograms recorded by this TaskTracker.
+  const std::string histogram_label_;
 
   // Indicates whether logging information about TaskPriority::BEST_EFFORT tasks
   // was enabled with a command line switch.
@@ -280,22 +271,17 @@ class BASE_EXPORT TaskTracker {
 
   // ThreadPool.TaskLatencyMicroseconds.*,
   // ThreadPool.HeartbeatLatencyMicroseconds.*, and
-  // ThreadPool.NumTasksRunWhileQueuing.* histograms. The first index is
-  // a TaskPriority. The second index is 0 for non-blocking tasks, 1 for
-  // blocking tasks. Intentionally leaked.
+  // ThreadPool.NumTasksRunWhileQueuing.* histograms. The index is a
+  // TaskPriority. Intentionally leaked.
   // TODO(scheduler-dev): Consider using STATIC_HISTOGRAM_POINTER_GROUP for
   // these.
   using TaskPriorityType = std::underlying_type<TaskPriority>::type;
   static constexpr TaskPriorityType kNumTaskPriorities =
       static_cast<TaskPriorityType>(TaskPriority::HIGHEST) + 1;
-  static constexpr uint8_t kNumBlockingModes = 2;
-  HistogramBase* const task_latency_histograms_[kNumTaskPriorities]
-                                               [kNumBlockingModes];
-  HistogramBase* const heartbeat_latency_histograms_[kNumTaskPriorities]
-                                                    [kNumBlockingModes];
+  HistogramBase* const task_latency_histograms_[kNumTaskPriorities];
+  HistogramBase* const heartbeat_latency_histograms_[kNumTaskPriorities];
   HistogramBase* const
-      num_tasks_run_while_queuing_histograms_[kNumTaskPriorities]
-                                             [kNumBlockingModes];
+      num_tasks_run_while_queuing_histograms_[kNumTaskPriorities];
 
   // Ensures all state (e.g. dangling cleaned up workers) is coalesced before
   // destroying the TaskTracker (e.g. in test environments).
