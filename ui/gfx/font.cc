fb27f597d00c86a029f31b5c41888148d4714467
Jose Dapena Paz <jose.dapena@lge.com>
2019-10-25 15:07:09 +0000

    IWYU: include algorithm to use std::lower_bound in ui/gfx/font.cc
    Fix GCC build because of missing include:
    ../../ui/gfx/font.cc: In function ‘gfx::Font::Weight gfx::FontWeightFromInt(int)’:
    ../../ui/gfx/font.cc:114:8: error: no matching function for call to ‘lower_bound(const gfx::Font::Weight*, const gfx::Font::Weight*, int&, gfx::FontWeightFromInt(int)::<lambda(const gfx::Font::Weight&, const int&)>)’
           });
            ^
    In file included from /usr/include/c++/8/bits/char_traits.h:39,
                     from /usr/include/c++/8/string:40,
                     from ../../ui/gfx/font.h:8,
                     from ../../ui/gfx/font.cc:5:
    /usr/include/c++/8/bits/stl_algobase.h:984:5: note: candidate: ‘template<class _ForwardIterator, class _Tp> _ForwardIterator std::lower_bound(_ForwardIterator, _ForwardIterator, const _Tp&)’
         lower_bound(_ForwardIterator __first, _ForwardIterator __last,
         ^~~~~~~~~~~
    /usr/include/c++/8/bits/stl_algobase.h:984:5: note:   template argument deduction/substitution failed:
    ../../ui/gfx/font.cc:114:8: note:   candidate expects 3 arguments, 4 provided
           });
            ^
    
    Bug: 819294
    Change-Id: Ic59dcf3a06bdd54d1d426c08a61624873a0ff30c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879909
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709472}

--- ui/gfx/font.cc
+++ ui/gfx/font.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/font.h"
 
+#include <algorithm>
+
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "ui/gfx/platform_font.h"
