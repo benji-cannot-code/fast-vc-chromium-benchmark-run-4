9bde75e01a0afa8f0f8dcd4b0db0851fb909912e
Roman Aleksandrov <raleksandrov@google.com>
2019-10-25 10:06:49 +0000

    Numeric keyboard for password: Add device policy
    Add ChromeOS device DeviceShowNumericKeyboardForPassword policy. It
    will be used to show numeric keyboard on the login screen to enter
    password.
    
    Bug: 1015743
    Change-Id: I22b9400be32195fd5ac136a3d1ea0434b6e904eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872225
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#709420}

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -82,6 +82,7 @@ const char* const kKnownSettings[] = {
     kDeviceLoginScreenInputMethods,
     kDeviceLoginScreenLocales,
     kDeviceLoginScreenSystemInfoEnforced,
+    kDeviceShowNumericKeyboardForPassword,
     kDeviceOffHours,
     kDeviceOwner,
     kDeviceNativePrintersAccessMode,
@@ -403,6 +404,13 @@ void DecodeLoginPolicies(const em::ChromeDeviceSettingsProto& policy,
     new_values_cache->SetBoolean(
         kDeviceLoginScreenSystemInfoEnforced,
         policy.device_login_screen_system_info_enforced().value());
+  }
+
+  if (policy.has_device_show_numeric_keyboard_for_password() &&
+      policy.device_show_numeric_keyboard_for_password().has_value()) {
+    new_values_cache->SetBoolean(
+        kDeviceShowNumericKeyboardForPassword,
+        policy.device_show_numeric_keyboard_for_password().value());
   }
 
   if (policy.has_saml_login_authentication_type() &&
