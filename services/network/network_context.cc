62c399573d9ac905ec92b99f83da9eb70b8e5b24
Matt Menke <mmenke@chromium.org>
2019-10-25 14:45:36 +0000

    Make only proxy auth cache entries copiable between caches.
    We want to make server auth cache respect the NetworkIsolationKey, but
    need that capability to be able to be toggled during runtime (and
    possibly per profile) so enterprises can disable it. Loading enterprise
    configurations while in the middle of copying server auth credentials
    gives us non-deterministic behavior configs when this setting is
    toggled, and copying it between two profiles with different settings is
    also pretty weird.
    
    The simplest solution is just to only copy proxy credentials, which are
    what the copying logic is only really intended for, anyways.
    
    Bug: 993806
    Change-Id: I26d4f0f92e695e844ff72ec5df83eeaefd9f1b5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875271
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709466}

--- services/network/network_context.cc
+++ services/network/network_context.cc
@@ -1461,7 +1461,8 @@ void NetworkContext::ForceDomainReliabilityUploadsForTesting(
   std::move(callback).Run();
 }
 
-void NetworkContext::SaveHttpAuthCache(SaveHttpAuthCacheCallback callback) {
+void NetworkContext::SaveHttpAuthCacheProxyEntries(
+    SaveHttpAuthCacheProxyEntriesCallback callback) {
   net::HttpAuthCache* http_auth_cache =
       url_request_context_->http_transaction_factory()
           ->GetSession()
@@ -1472,8 +1473,9 @@ void NetworkContext::SaveHttpAuthCache(SaveHttpAuthCacheCallback callback) {
   std::move(callback).Run(cache_key);
 }
 
-void NetworkContext::LoadHttpAuthCache(const base::UnguessableToken& cache_key,
-                                       LoadHttpAuthCacheCallback callback) {
+void NetworkContext::LoadHttpAuthCacheProxyEntries(
+    const base::UnguessableToken& cache_key,
+    LoadHttpAuthCacheProxyEntriesCallback callback) {
   net::HttpAuthCache* http_auth_cache =
       url_request_context_->http_transaction_factory()
           ->GetSession()
