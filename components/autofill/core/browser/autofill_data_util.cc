19569335242574e245cff763c2ffe0ff92aa6865
Clemens Arbesser <arbesser@google.com>
2019-10-25 11:24:33 +0000

    [Autofill Assistant] Fail UserFormAction for invalid basic card networks.
    Currently, autofill assistant does not validate the set of basic card networks sent by the autofill assistant backend. In the UI, this can lead to crash due to an assert. This CL adds the missing validation and terminates the action with INVALID_ACTION for unsupported cards.
    
    Bug: b/143136284
    Change-Id: If248fa0edd671078ef6459de34228e392f542800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871700
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709431}

--- components/autofill/core/browser/autofill_data_util.cc
+++ components/autofill/core/browser/autofill_data_util.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_data_util.h"
 
 #include <algorithm>
+#include <iterator>
 #include <vector>
 
 #include "base/i18n/char_iterator.h"
@@ -476,6 +477,16 @@ const char* GetIssuerNetworkForBasicCardIssuerNetwork(
     }
   }
   return kGenericPaymentRequestData.issuer_network;
+}
+
+bool IsValidBasicCardIssuerNetwork(
+    const std::string& basic_card_issuer_network) {
+  auto* it = std::find_if(
+      std::begin(kPaymentRequestData), std::end(kPaymentRequestData),
+      [basic_card_issuer_network](const auto& data) {
+        return data.basic_card_issuer_network == basic_card_issuer_network;
+      });
+  return it != std::end(kPaymentRequestData);
 }
 
 bool IsValidCountryCode(const std::string& country_code) {
