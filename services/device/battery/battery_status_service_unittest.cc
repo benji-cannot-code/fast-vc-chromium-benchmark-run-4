550307631ab41abdc4d1d78b30b3c0982959ef97
Carlos Caballero <carlscab@google.com>
2019-10-25 16:08:40 +0000

    Remove MessageLoop references in services/device/battery
    MessageLoop is going away soon.
    
    This CL was uploaded by git cl split.
    
    R=timvolodine@chromium.org
    
    Bug: 891670
    Change-Id: I3fb93bf9474d1d021bb8d023166d0301429d6240
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1878073
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#709489}

--- services/device/battery/battery_status_service_unittest.cc
+++ services/device/battery/battery_status_service_unittest.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
+#include "base/test/task_environment.h"
 #include "services/device/battery/battery_status_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -111,7 +111,7 @@ class BatteryStatusServiceTest : public testing::Test {
     battery_status_ = status;
   }
 
-  base::MessageLoop message_loop_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   BatteryStatusService battery_service_;
   FakeBatteryManager* battery_manager_;
   BatteryStatusService::BatteryUpdateCallback callback1_;
