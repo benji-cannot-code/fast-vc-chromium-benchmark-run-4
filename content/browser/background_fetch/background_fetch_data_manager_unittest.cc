f726b95264481b400ae0dc5e1f703516572f559b
Wei Wang <wei4.wang@intel.com>
2019-10-25 01:52:47 +0000

    Make BlobToString() a helper function in blob_test_utils.h.
    This patch makes BlobToString()/CopyBody a helper function in
    storage/browser/test/blob_test_utils.h which can be used by
    service_worker_browsertest, cache_storage_cache_unittest and
    background_fetch_data_manager_unittest.
    
    Bug: 931094
    Change-Id: I672e0896b9c817d6eeb7e61c1c02736aa62fad12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865708
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Wei4 Wang <wei4.wang@intel.com>
    Cr-Commit-Position: refs/heads/master@{#709323}

--- content/browser/background_fetch/background_fetch_data_manager_unittest.cc
+++ content/browser/background_fetch/background_fetch_data_manager_unittest.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/blob/blob_data_handle.h"
 #include "storage/browser/blob/blob_impl.h"
 #include "storage/browser/blob/blob_storage_context.h"
+#include "storage/browser/test/blob_test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "third_party/blink/public/mojom/background_fetch/background_fetch.mojom.h"
 #include "third_party/blink/public/mojom/cache_storage/cache_storage.mojom.h"
@@ -340,7 +341,7 @@ class BackgroundFetchDataManagerTest
 
     if (blob && blob->blob) {
       mojo::Remote<blink::mojom::Blob> blob_remote(std::move(blob->blob));
-      return CopyBody(blob_remote.get());
+      return storage::BlobToString(blob_remote.get());
     }
 
     return std::string();
@@ -833,32 +834,6 @@ class BackgroundFetchDataManagerTest
                           blink::mojom::CacheStorageVerboseErrorPtr error) {
     DCHECK_EQ(error->value, blink::mojom::CacheStorageError::kSuccess);
     std::move(quit_closure).Run();
-  }
-
-  class DataPipeDrainerClient : public mojo::DataPipeDrainer::Client {
-   public:
-    explicit DataPipeDrainerClient(std::string* output) : output_(output) {}
-    void Run() { run_loop_.Run(); }
-
-    void OnDataAvailable(const void* data, size_t num_bytes) override {
-      output_->append(reinterpret_cast<const char*>(data), num_bytes);
-    }
-    void OnDataComplete() override { run_loop_.Quit(); }
-
-   private:
-    base::RunLoop run_loop_;
-    std::string* output_;
-  };
-
-  std::string CopyBody(blink::mojom::Blob* blob) {
-    mojo::DataPipe pipe;
-    blob->ReadAll(std::move(pipe.producer_handle), mojo::NullRemote());
-
-    std::string output;
-    DataPipeDrainerClient client(&output);
-    mojo::DataPipeDrainer drainer(&client, std::move(pipe.consumer_handle));
-    client.Run();
-    return output;
   }
 
   blink::mojom::SerializedBlobPtr BuildBlob(const std::string data) {
@@ -1883,7 +1858,7 @@ TEST_F(BackgroundFetchDataManagerTest, MatchRequestsWithBody) {
 
   ASSERT_TRUE(request->blob->blob);
   mojo::Remote<blink::mojom::Blob> blob(std::move(request->blob->blob));
-  EXPECT_EQ(CopyBody(blob.get()), upload_data);
+  EXPECT_EQ(storage::BlobToString(blob.get()), upload_data);
 }
 
 TEST_F(BackgroundFetchDataManagerTest, MatchRequestsFromCache) {
