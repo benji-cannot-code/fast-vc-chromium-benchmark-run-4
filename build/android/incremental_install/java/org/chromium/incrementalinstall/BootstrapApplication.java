b50b8ba5543c0758ac97de571782ebcc196bcb5b
Andrew Grieve <agrieve@google.com>
2019-10-25 19:37:12 +0000

    Android: Fix running tests with incremental_install=true
    The recent refactoring (linked bug) broke them in two ways:
    * Tests were being filtered based on having "test" in the path, but the
      names now are all just "shard".
    * When an apk_under_test exists, dex files from both apks were being
      copied to the same directory, so "shard1.dex" was being clobbered.
    
    This also refactors ClassLoaderPatcher a bit to make it more clear what
    logic happens on O+ vs pre-O.
    
    Bug: 1013688
    Change-Id: I4c5e0e5a84bc9875c2262b0108d506797e430e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1881310
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709585}

--- build/android/incremental_install/java/org/chromium/incrementalinstall/BootstrapApplication.java
+++ build/android/incremental_install/java/org/chromium/incrementalinstall/BootstrapApplication.java
@@ -102,10 +102,10 @@ public final class BootstrapApplication extends Application {
             }
 
             mClassLoaderPatcher.importNativeLibs(instLibDir);
-            sIncrementalDexFiles = mClassLoaderPatcher.loadDexFiles(instDexDir);
+            sIncrementalDexFiles = mClassLoaderPatcher.loadDexFiles(instDexDir, instPackageName);
             if (instPackageNameDiffers) {
                 mClassLoaderPatcher.importNativeLibs(appLibDir);
-                mClassLoaderPatcher.loadDexFiles(appDexDir);
+                mClassLoaderPatcher.loadDexFiles(appDexDir, appPackageName);
             }
 
             if (isFirstRun && mClassLoaderPatcher.mIsPrimaryProcess) {
