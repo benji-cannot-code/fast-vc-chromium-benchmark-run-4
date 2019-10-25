10ca0a43589caf9aaad9d8cc5c3dab85427038fe
Tricia Crichton <triciac@chromium.org>
2019-10-25 19:46:38 +0000

    [ChromeDriver] Improve Error Message for CRX2
    Add more information for users when CRX extension fails to parse and we
    suspect it needs to be updated to the new CRX3 format.
    
    Fixed: chromedriver:3188
    Change-Id: I77eb8eb7231de57519e972467312ee9975d44243
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879461
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tricia Crichton <triciac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709589}

--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -843,7 +843,12 @@ Status ProcessExtension(const std::string& extension,
         crx_file::Verify(extension_crx, crx_file::VerifierFormat::CRX3,
                          {} /** required_key_hashes */,
                          {} /** required_file_hash */, &public_key_base64, &id);
-    if (result != crx_file::VerifierResult::OK_FULL) {
+    if (result == crx_file::VerifierResult::ERROR_HEADER_INVALID) {
+      return Status(kUnknownError,
+                    "CRX verification failed to parse extension header. Chrome "
+                    "supports only CRX3 format. Does the extension need to be "
+                    "updated?");
+    } else if (result != crx_file::VerifierResult::OK_FULL) {
       return Status(kUnknownError,
                     base::StringPrintf("CRX verification failed: %d", result));
     }
@@ -945,7 +950,7 @@ Status ProcessExtensions(const std::vector<std::string>& extensions,
     Status status = ProcessExtension(extensions[i], temp_dir, &path, &bg_page);
     if (status.IsError()) {
       return Status(
-          kUnknownError,
+          kSessionNotCreated,
           base::StringPrintf("cannot process extension #%" PRIuS, i + 1),
           status);
     }
