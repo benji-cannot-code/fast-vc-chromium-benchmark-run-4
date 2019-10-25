898ae3f860d6daf12ab94cd46863e7ed931ec689
Tobias Sargeant <tobiasjs@google.com>
2019-10-25 00:11:53 +0000

    [weblayer] Pass PackageInfo over aidl to WeblayerImpl
    This enables us to set the browser package info during intialization
    without having to reflect to look up the object again.
    
    Change-Id: I2a66494dde72a1d6c64140e9ac68b310f970be50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879887
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Tobias Sargeant <tobiasjs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709289}

--- weblayer/browser/java/org/chromium/weblayer_private/aidl/WebLayerVersion.java
+++ weblayer/browser/java/org/chromium/weblayer_private/aidl/WebLayerVersion.java
@@ -6,4 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.weblayer_private.aidl;
 
 /** Holds the current version number of WebLayer. */
-public final class WebLayerVersion { public static int sVersionNumber = 1; }
+public final class WebLayerVersion { public static int sVersionNumber = 2; }
