48f994e8a869f6bae565cc3eff751b1eae939812
Philip Rogers <pdr@chromium.org>
2019-10-25 16:43:15 +0000

    Make pdr and wangxianzhu owners of blink paint benchmarks
    This patch makes pdr and wangxianzhu owners of the blink paint tests.
    
    Benchmark owner list: go/chrome-benchmarks
    How to update benchmark owners: https://chromium.googlesource.com/chromium/src/+/master/docs/speed/benchmark/benchmark_ownership.md
    
    Change-Id: I38a588ade93d2a3181691df920d7ba168a218717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879149
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709508}

--- tools/perf/benchmarks/rasterize_and_record_micro.py
+++ tools/perf/benchmarks/rasterize_and_record_micro.py
@@ -45,7 +45,9 @@ class _RasterizeAndRecordMicro(perf_benchmark.PerfBenchmark):
 
 
 @benchmark.Info(
-    emails=['vmpstr@chromium.org'],
+    emails=['pdr@chromium.org',
+             'wangxianzhu@chromium.org',
+             'vmpstr@chromium.org'],
     component='Internals>Compositing>Rasterization',
     documentation_url='https://bit.ly/rasterize-and-record-benchmark')
 class RasterizeAndRecordMicroTop25(_RasterizeAndRecordMicro):
@@ -60,7 +62,9 @@ class RasterizeAndRecordMicroTop25(_RasterizeAndRecordMicro):
 
 
 @benchmark.Info(
-    emails=['vmpstr@chromium.org'],
+    emails=['pdr@chromium.org',
+             'wangxianzhu@chromium.org',
+             'vmpstr@chromium.org'],
     component='Internals>Compositing>Rasterization',
     documentation_url='https://bit.ly/rasterize-and-record-benchmark')
 class RasterizeAndRecordMicroPartialInvalidation(_RasterizeAndRecordMicro):
