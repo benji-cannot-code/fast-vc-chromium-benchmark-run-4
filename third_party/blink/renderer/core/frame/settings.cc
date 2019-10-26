4b9c97337a2539a54f32d97d920cc224ab9d3181
Xianzhu Wang <wangxianzhu@chromium.org>
2019-10-26 00:07:07 +0000

    Remove chrome mock scrollbar settings
    We are using mock overlay scrollbars in blink web tests and
    unit tests in particular cases. Mock scrollbar will not be a
    standalone setting in blink, and doesn't need to expose to chrome.
    
    Change-Id: I339398322e473d09364069129b3c716aaddd0d96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879706
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709707}

--- third_party/blink/renderer/core/frame/settings.cc
+++ third_party/blink/renderer/core/frame/settings.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "build/build_config.h"
-#include "third_party/blink/renderer/core/scroll/scrollbar_theme.h"
 #include "third_party/blink/renderer/platform/graphics/dark_mode_settings.h"
 
 namespace blink {
@@ -94,14 +93,6 @@ void Settings::SetTextAutosizingWindowSizeOverride(
 
   text_autosizing_window_size_override_ = text_autosizing_window_size_override;
   Invalidate(SettingsDelegate::kTextAutosizingChange);
-}
-
-void Settings::SetMockScrollbarsEnabled(bool flag) {
-  ScrollbarTheme::SetMockScrollbarsEnabled(flag);
-}
-
-bool Settings::MockScrollbarsEnabled() {
-  return ScrollbarTheme::MockScrollbarsEnabled();
 }
 
 void Settings::SetForceDarkModeEnabled(bool enabled) {
--- third_party/blink/renderer/core/frame/settings.h
+++ third_party/blink/renderer/core/frame/settings.h
@@ -87,10 +87,6 @@ class CORE_EXPORT Settings {
 
   SETTINGS_GETTERS_AND_SETTERS
 
-  // FIXME: This does not belong here.
-  static void SetMockScrollbarsEnabled(bool flag);
-  static bool MockScrollbarsEnabled();
-
   void SetDelegate(SettingsDelegate*);
 
  private:
