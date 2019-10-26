66173d79dc140d58ba8a3e5d30b6fb343778fbd0
Chris Mumford <cmumford@chromium.org>
2019-10-26 01:52:05 +0000

    media: Switch error callbacks to base::OnceClosure.
    Switching several error callback implementations to use
    base::OnceClosure explicitly. All of these previously used
    base::Closure (implicitly repeating), but all of them would
    only ever run the error callback the first time an error
    orrurred. The affected classes are:
    
    1. DemuxerStreamAdapter
    2. StreamProvider
    3. MediaStream
    
    Bug: 1007810
    Change-Id: I872ea38eb27ec00c99137435c0408a93f292f9f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879949
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/master@{#709733}

--- media/remoting/demuxer_stream_adapter.h
+++ media/remoting/demuxer_stream_adapter.h
@@ -41,7 +41,7 @@ namespace remoting {
 // while RPC message should be sent on main thread using |main_task_runner|.
 class DemuxerStreamAdapter {
  public:
-  using ErrorCallback = base::Callback<void(StopTrigger)>;
+  using ErrorCallback = base::OnceCallback<void(StopTrigger)>;
 
   // |main_task_runner|: Task runner to post RPC message on main thread
   // |media_task_runner|: Task runner to run whole class on media thread.
@@ -64,7 +64,7 @@ class DemuxerStreamAdapter {
       int rpc_handle,
       mojom::RemotingDataStreamSenderPtrInfo stream_sender_info,
       mojo::ScopedDataPipeProducerHandle producer_handle,
-      const ErrorCallback& error_callback);
+      ErrorCallback error_callback);
   ~DemuxerStreamAdapter();
 
   // Rpc handle for this class. This is used for sending/receiving RPC message
--- media/remoting/stream_provider.cc
+++ media/remoting/stream_provider.cc
@@ -31,7 +31,7 @@ class MediaStream final : public DemuxerStream {
   MediaStream(RpcBroker* rpc_broker,
               Type type,
               int remote_handle,
-              const base::Closure& error_callback);
+              base::OnceClosure error_callback);
   ~MediaStream() override;
 
   // DemuxerStream implementation.
@@ -93,7 +93,7 @@ class MediaStream final : public DemuxerStream {
   // Set when Read() is called. Run only once when read completes.
   ReadCB read_complete_callback_;
 
-  base::Closure error_callback_;  // Called only once when first error occurs.
+  base::OnceClosure error_callback_;  // Called when first error occurs.
 
   base::circular_deque<scoped_refptr<DecoderBuffer>> buffers_;
 
@@ -113,12 +113,12 @@ class MediaStream final : public DemuxerStream {
 MediaStream::MediaStream(RpcBroker* rpc_broker,
                          Type type,
                          int remote_handle,
-                         const base::Closure& error_callback)
+                         base::OnceClosure error_callback)
     : rpc_broker_(rpc_broker),
       type_(type),
       remote_handle_(remote_handle),
       rpc_handle_(rpc_broker_->GetUniqueHandle()),
-      error_callback_(error_callback) {
+      error_callback_(std::move(error_callback)) {
   DCHECK(remote_handle_ != RpcBroker::kInvalidHandle);
   rpc_broker_->RegisterMessageReceiverCallback(
       rpc_handle_,
@@ -392,8 +392,8 @@ void MediaStream::OnError(const std::string& error) {
 }
 
 StreamProvider::StreamProvider(RpcBroker* rpc_broker,
-                               const base::Closure& error_callback)
-    : rpc_broker_(rpc_broker), error_callback_(error_callback) {}
+                               base::OnceClosure error_callback)
+    : rpc_broker_(rpc_broker), error_callback_(std::move(error_callback)) {}
 
 StreamProvider::~StreamProvider() = default;
 
@@ -416,16 +416,16 @@ void StreamProvider::Initialize(int remote_audio_handle,
   if (remote_audio_handle != RpcBroker::kInvalidHandle) {
     audio_stream_.reset(new MediaStream(
         rpc_broker_, DemuxerStream::AUDIO, remote_audio_handle,
-        base::Bind(&StreamProvider::OnError, weak_factory_.GetWeakPtr(),
-                   "Media stream error")));
+        base::BindOnce(&StreamProvider::OnError, weak_factory_.GetWeakPtr(),
+                       "Media stream error")));
     audio_stream_->Initialize(base::Bind(
         &StreamProvider::AudioStreamInitialized, weak_factory_.GetWeakPtr()));
   }
   if (remote_video_handle != RpcBroker::kInvalidHandle) {
     video_stream_.reset(new MediaStream(
         rpc_broker_, DemuxerStream::VIDEO, remote_video_handle,
-        base::Bind(&StreamProvider::OnError, weak_factory_.GetWeakPtr(),
-                   "Media stream error")));
+        base::BindOnce(&StreamProvider::OnError, weak_factory_.GetWeakPtr(),
+                       "Media stream error")));
     video_stream_->Initialize(base::Bind(
         &StreamProvider::VideoStreamInitialized, weak_factory_.GetWeakPtr()));
   }
