fba80658ac7cd4419b6ce82c30cc98ab6d810c98
Gyuyoung Kim <gyuyoung@igalia.com>
2019-10-26 00:54:14 +0000

    Migrate CdmService::CreateCdmFactory to new Mojo type more
    This CL applies pending_receiver to the first parameter of
    CreateCdmFactory function in CdmService interface.
    
    Bug: 955171
    Change-Id: I3c262ce12c9a9a0cc095ff71a6d9901a73d2c320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880412
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#709718}

--- content/browser/media/media_interface_proxy.h
+++ content/browser/media/media_interface_proxy.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/binding.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "services/service_manager/public/mojom/interface_provider.mojom.h"
 
 namespace media {
@@ -142,9 +143,10 @@ class MediaInterfaceProxy : public media::mojom::InterfaceFactory {
 #endif  // BUILDFLAG(ENABLE_CAST_RENDERER)
 
 #if BUILDFLAG(ENABLE_LIBRARY_CDMS)
-  // CDM GUID to CDM InterfaceFactoryPtr mapping, where the InterfaceFactory
+  // CDM GUID to CDM InterfaceFactoryRemote mapping, where the InterfaceFactory
   // instances live in the standalone kCdmServiceName service instances.
-  std::map<base::Token, media::mojom::CdmFactoryPtr> cdm_factory_map_;
+  std::map<base::Token, mojo::Remote<media::mojom::CdmFactory>>
+      cdm_factory_map_;
 #endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
 
   base::ThreadChecker thread_checker_;
