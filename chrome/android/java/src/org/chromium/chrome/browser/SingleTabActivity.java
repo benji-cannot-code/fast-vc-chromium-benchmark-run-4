937aa152f65211b9fdb2b8b429f497bc79c74121
Peter Kotwicz <pkotwicz@chromium.org>
2019-10-28 23:11:53 +0000

    [Android] Remove always-true SingleTabActivity#supportsAppSwitcher()
    BUG=None
    
    Change-Id: I3acd459cddc8bfdeae25b0f6fa9c99e431fa7f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1884516
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710082}

--- chrome/android/java/src/org/chromium/chrome/browser/SingleTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/SingleTabActivity.java
@@ -5,11 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser;
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Pair;
-import android.view.KeyCharacterMap;
-import android.view.KeyEvent;
 
 import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
@@ -123,11 +120,6 @@ public abstract class SingleTabActivity<C extends ChromeActivityComponent>
      */
     protected abstract TabState restoreTabState(Bundle savedInstanceState, int tabId);
 
-    private boolean supportsAppSwitcher() {
-        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_TOUCHSCREEN)
-                || KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_APP_SWITCH);
-    }
-
     @Override
     protected boolean handleBackPressed() {
         Tab tab = getActivityTab();
@@ -140,13 +132,6 @@ public abstract class SingleTabActivity<C extends ChromeActivityComponent>
             return true;
         }
 
-        if (!supportsAppSwitcher()) {
-            // If the device has no way to get to the task switcher, we don't want the default back
-            // button behavior of finishing the Activity. If the device is low on memory LMK will
-            // kill us, and if not, we'll start up faster when returned to.
-            moveTaskToBack(true);
-            return true;
-        }
         return false;
     }
 
