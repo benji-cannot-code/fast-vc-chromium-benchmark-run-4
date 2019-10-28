1a3f415bd1a1672428333eff4370405b561eaad7
Adrienne Walker <enne@chromium.org>
2019-10-28 20:59:33 +0000

    storage: pass around blob bytes as uint8_t
    This is a followup to this patch:
    https://chromium-review.googlesource.com/c/chromium/src/+/1830360
    
    There's sort of a mismatch between whether code uses uint8_t or
    char for bytes.  I think uint8_t is a little bit better of a
    representation and matches what BigBuffer does.  This cleans up
    a bit of storage code and apis to use that in more places.
    
    Bug: 1012869
    
    Change-Id: I7b1e8eb7d9b967f451b19f9c1c6d86c0d89d3ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857648
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710035}

--- storage/browser/blob/blob_memory_controller_unittest.cc
+++ storage/browser/blob/blob_memory_controller_unittest.cc
@@ -322,8 +322,8 @@ TEST_F(BlobMemoryControllerTest, PageToDisk) {
   EXPECT_FALSE(file_runner_->HasPendingTask());
 
   // Add our original item as populated so it's paged to disk.
-  future_data.Populate(
-      base::make_span(kData, kTestBlobStorageMaxBlobMemorySize));
+  future_data.Populate(base::as_bytes(
+      base::make_span(kData, kTestBlobStorageMaxBlobMemorySize)));
   items[0]->set_state(ItemState::POPULATED_WITH_QUOTA);
   controller.NotifyMemoryItemsUsed(items);
 
@@ -398,8 +398,8 @@ TEST_F(BlobMemoryControllerTest, CancelMemoryRequest) {
   EXPECT_EQ(0u, controller.disk_usage());
 
   // Add our original item as populated so we start paging to disk.
-  future_data.Populate(
-      base::make_span(kData, kTestBlobStorageMaxBlobMemorySize));
+  future_data.Populate(base::as_bytes(
+      base::make_span(kData, kTestBlobStorageMaxBlobMemorySize)));
   items[0]->set_state(ItemState::POPULATED_WITH_QUOTA);
   controller.NotifyMemoryItemsUsed(items);
 
@@ -573,9 +573,9 @@ TEST_F(BlobMemoryControllerTest, MultipleFilesPaged) {
   EXPECT_FALSE(file_runner_->HasPendingTask());
 
   // Add our original item as populated so it's paged to disk.
-  future_data1.Populate(base::make_span(kData1, kSize1));
+  future_data1.Populate(base::as_bytes(base::make_span(kData1, kSize1)));
   items1[0]->set_state(ItemState::POPULATED_WITH_QUOTA);
-  future_data2.Populate(base::make_span(kData2, kSize2));
+  future_data2.Populate(base::as_bytes(base::make_span(kData2, kSize2)));
   items2[0]->set_state(ItemState::POPULATED_WITH_QUOTA);
 
   std::vector<scoped_refptr<ShareableBlobDataItem>> both_items = {items1[0],
@@ -620,7 +620,7 @@ TEST_F(BlobMemoryControllerTest, FullEviction) {
        i < kTestBlobStorageMaxBlobMemorySize - kTestBlobStorageMinFileSizeBytes;
        i++) {
     BlobDataBuilder builder("fake");
-    builder.AppendData(kData, 1);
+    builder.AppendData(std::string(kData, 1));
     std::vector<scoped_refptr<ShareableBlobDataItem>> items =
         CreateSharedDataItems(builder);
     base::WeakPtr<QuotaAllocationTask> memory_task =
@@ -680,7 +680,7 @@ TEST_F(BlobMemoryControllerTest, PagingStopsWhenFull) {
   bool memory_requested[kBlobsThatCanFit] = {};
   for (size_t i = 0; i < kBlobsThatCanFit; i++) {
     BlobDataBuilder builder("fake");
-    builder.AppendData(kData, kDataSize);
+    builder.AppendData(std::string(kData, kDataSize));
     std::vector<scoped_refptr<ShareableBlobDataItem>> items =
         CreateSharedDataItems(builder);
     EXPECT_TRUE(controller.CanReserveQuota(kDataSize));
@@ -825,7 +825,8 @@ TEST_F(BlobMemoryControllerTest, DisableDiskWithFileAndMemoryPending) {
   EXPECT_EQ(0u, controller.disk_usage());
 
   // Add our original item as populated so we start paging it to disk.
-  future_data.Populate(base::make_span(kDataMemoryData, kFirstMemorySize));
+  future_data.Populate(
+      base::as_bytes(base::make_span(kDataMemoryData, kFirstMemorySize)));
   items[0]->set_state(ItemState::POPULATED_WITH_QUOTA);
   controller.NotifyMemoryItemsUsed(items);
 
@@ -1141,7 +1142,7 @@ TEST_F(BlobMemoryControllerTest, OnMemoryPressure) {
                         1;
   for (size_t i = 0; i < size_to_load; i++) {
     BlobDataBuilder builder("fake");
-    builder.AppendData(kData, 1);
+    builder.AppendData(std::string(kData, 1));
     std::vector<scoped_refptr<ShareableBlobDataItem>> items =
         CreateSharedDataItems(builder);
     base::WeakPtr<QuotaAllocationTask> memory_task =
