47d372ea02aab505549f6c31d4e81c307d796c1e
Arthur Hemery <ahemery@chromium.org>
2019-10-28 13:10:44 +0000

    [navigation] Removing IsPerNavigationMojoInterface checks in NavigationRequest.
    NavigationClient is on by default, removing unnecessary branching.
    
    Bug: 784904
    Change-Id: I9024044ec53edd8776dda2427e59b754cd0f91ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1821179
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709873}

--- content/browser/frame_host/navigation_request.cc
+++ content/browser/frame_host/navigation_request.cc
@@ -906,11 +906,9 @@ NavigationRequest::NavigationRequest(
     source_site_instance_ =
         frame_tree_node->current_frame_host()->GetSiteInstance();
 
-    if (IsPerNavigationMojoInterfaceEnabled()) {
-      DCHECK(navigation_client.is_valid());
-      SetNavigationClient(std::move(navigation_client),
-                          source_site_instance_->GetId());
-    }
+    DCHECK(navigation_client.is_valid());
+    SetNavigationClient(std::move(navigation_client),
+                        source_site_instance_->GetId());
   } else if (entry) {
     DCHECK(!navigation_client.is_valid());
     FrameNavigationEntry* frame_navigation_entry =
@@ -2347,8 +2345,7 @@ void NavigationRequest::CommitErrorPage(
   // consistent with the URL being requested.
   commit_params_->origin_to_commit =
       url::Origin::Create(common_params_->url).DeriveNewOpaqueOrigin();
-  if (IsPerNavigationMojoInterfaceEnabled() &&
-      request_navigation_client_.is_bound()) {
+  if (request_navigation_client_.is_bound()) {
     if (associated_site_instance_id_ ==
         render_frame_host_->GetSiteInstance()->GetId()) {
       // Reuse the request NavigationClient for commit.
@@ -2416,8 +2413,7 @@ void NavigationRequest::CommitNavigation() {
 
   frame_tree_node_->TransferNavigationRequestOwnership(render_frame_host_);
 
-  if (IsPerNavigationMojoInterfaceEnabled() &&
-      request_navigation_client_.is_bound()) {
+  if (request_navigation_client_.is_bound()) {
     if (associated_site_instance_id_ ==
         render_frame_host_->GetSiteInstance()->GetId()) {
       // Reuse the request NavigationClient for commit.
