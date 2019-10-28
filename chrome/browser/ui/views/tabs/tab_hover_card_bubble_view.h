8bfbf55ce3cb8d37fc14c48c57cd374373d897d7
Caroline Rising <corising@chromium.org>
2019-10-28 17:47:03 +0000

    Only reset tab hover card mouse exit timestamp if the hover card is showing when exiting a tab.
    Bug: 1013958
    Change-Id: Ic653e7febf1161b23ed1ccc0e2f2b4fecc1f7817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880584
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709960}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.h
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.h
@@ -41,6 +41,8 @@ class TabHoverCardBubbleView : public views::BubbleDialogDelegateView,
 
   void UpdateAnchorBounds(gfx::Rect anchor_bounds);
 
+  bool IsVisible();
+
   void FadeOutToHide();
 
   bool IsFadingOut() const;
