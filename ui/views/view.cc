923d92b1c5c2f29efc0f2092c5b2832aae634381
Robert Liao <robliao@chromium.org>
2019-10-28 19:49:48 +0000

    Remove View::GetPageScrollIncrement and View::GetLineScrollIncrement
    Both of these methods always returned 0 and are not overridable.
    
    BUG=
    
    Change-Id: I014897e1533442232e6dbfd973186e094077cf87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879721
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710010}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -1573,16 +1573,6 @@ void View::ScrollViewToVisible() {
   ScrollRectToVisible(GetLocalBounds());
 }
 
-int View::GetPageScrollIncrement(ScrollView* scroll_view,
-                                 bool is_horizontal, bool is_positive) {
-  return 0;
-}
-
-int View::GetLineScrollIncrement(ScrollView* scroll_view,
-                                 bool is_horizontal, bool is_positive) {
-  return 0;
-}
-
 void View::AddObserver(ViewObserver* observer) {
   CHECK(observer);
   observers_.AddObserver(observer);
