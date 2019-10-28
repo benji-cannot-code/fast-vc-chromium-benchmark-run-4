6c0e4615423e6e1c114ceda205ef0d0eb84720fd
Yuchen Liu <yucliu@chromium.org>
2019-10-28 20:15:44 +0000

    Replace EncryptionScheme with EncryptionMode for mp4
    Bug: 825041
    Test: CQ, build
    Change-Id: Ieeda5d967ed6ba77334b432810f88a6e01a492bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1881688
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Auto-Submit: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710019}

--- media/formats/mp4/track_run_iterator.cc
+++ media/formats/mp4/track_run_iterator.cc
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/checked_math.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/stl_util.h"
+#include "media/base/decrypt_config.h"
 #include "media/base/demuxer_memory_limit.h"
-#include "media/base/encryption_scheme.h"
+#include "media/base/encryption_pattern.h"
 #include "media/base/media_util.h"
 #include "media/base/timestamp_constants.h"
 #include "media/formats/mp4/rcheck.h"
@@ -56,7 +57,10 @@ struct TrackRunInfo {
   std::vector<uint8_t> aux_info_sizes;  // Populated if default_size == 0.
   int aux_info_total_size;
 
-  EncryptionScheme encryption_scheme;
+  EncryptionMode encryption_mode;
+#if BUILDFLAG(ENABLE_CBCS_ENCRYPTION_SCHEME)
+  EncryptionPattern encryption_pattern;
+#endif  // BUILDFLAG(ENABLE_CBCS_ENCRYPTION_SCHEME)
 
   std::vector<CencSampleEncryptionInfoEntry> fragment_sample_encryption_info;
 
@@ -371,18 +375,18 @@ bool TrackRunIterator::Init(const MovieFragment& moof) {
       }
 
       if (!sinf->HasSupportedScheme()) {
-        tri.encryption_scheme = Unencrypted();
+        tri.encryption_mode = EncryptionMode::kUnencrypted;
       } else {
 #if BUILDFLAG(ENABLE_CBCS_ENCRYPTION_SCHEME)
-        tri.encryption_scheme = EncryptionScheme(
-            sinf->IsCbcsEncryptionScheme()
-                ? EncryptionScheme::CIPHER_MODE_AES_CBC
-                : EncryptionScheme::CIPHER_MODE_AES_CTR,
+        tri.encryption_mode = sinf->IsCbcsEncryptionScheme()
+                                  ? EncryptionMode::kCbcs
+                                  : EncryptionMode::kCenc;
+        tri.encryption_pattern =
             EncryptionPattern(track_encryption->default_crypt_byte_block,
-                              track_encryption->default_skip_byte_block));
+                              track_encryption->default_skip_byte_block);
 #else
         DCHECK(!sinf->IsCbcsEncryptionScheme());
-        tri.encryption_scheme = AesCtrEncryptionScheme();
+        tri.encryption_mode = EncryptionMode::kCenc;
 #endif
       }
 
@@ -756,16 +760,16 @@ std::unique_ptr<DecryptConfig> TrackRunIterator::GetDecryptConfig() {
       std::string iv(reinterpret_cast<const char*>(
                          sample_encryption_entry.initialization_vector),
                      base::size(sample_encryption_entry.initialization_vector));
-      switch (run_itr_->encryption_scheme.mode()) {
-        case EncryptionScheme::CIPHER_MODE_UNENCRYPTED:
+      switch (run_itr_->encryption_mode) {
+        case EncryptionMode::kUnencrypted:
           return nullptr;
-        case EncryptionScheme::CIPHER_MODE_AES_CTR:
+        case EncryptionMode::kCenc:
           return DecryptConfig::CreateCencConfig(
               key_id, iv, sample_encryption_entry.subsamples);
-        case EncryptionScheme::CIPHER_MODE_AES_CBC:
+        case EncryptionMode::kCbcs:
           return DecryptConfig::CreateCbcsConfig(
               key_id, iv, sample_encryption_entry.subsamples,
-              run_itr_->encryption_scheme.pattern());
+              run_itr_->encryption_pattern);
       }
     }
 #endif
