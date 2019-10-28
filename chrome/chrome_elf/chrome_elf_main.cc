cf334b3510ee07f084a56264a636172407cb506e
Anupam Snigdha <snianu@microsoft.com>
2019-10-28 18:50:15 +0000

    Revert "Disable legacy hooking in the browser process"
    This reverts commit f87359ae7be2906bf1b361ba4bdbd9308f7fcd9f.
    
    Reason for revert: https://crbug.com/1017694
    
    Some legacy IMEs (ex: Yahoo Chinese IME) are broken with this change.
    
    Bug: 1017694
    Change-Id: I0dceabbd5ee712c74aab113e912a3e629f79785a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880597
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709983}

--- chrome/chrome_elf/chrome_elf_main.cc
+++ chrome/chrome_elf/chrome_elf_main.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <assert.h>
 #include <windows.h>
 
-#include "chrome/chrome_elf/chrome_elf_security.h"
 #include "chrome/chrome_elf/crash/crash_helper.h"
 #include "chrome/chrome_elf/third_party_dlls/beacon.h"
 #include "chrome/chrome_elf/third_party_dlls/main.h"
@@ -71,9 +70,6 @@ BOOL APIENTRY DllMain(HMODULE module, DWORD reason, LPVOID reserved) {
     // If this is not the browser process, all done.
     if (install_static::IsNonBrowserProcess())
       return TRUE;
-
-    // Disable legacy hooking.
-    elf_security::EarlyBrowserSecurity();
 
     __try {
       // Initialize the blocking of third-party DLLs if the initialization of
