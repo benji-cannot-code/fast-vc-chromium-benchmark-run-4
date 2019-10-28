7dbe0772f7eda8be42ed74d9290eb642ec961e5e
dpapad <dpapad@chromium.org>
2019-10-28 22:33:35 +0000

    Prevent zoom preferences from affecting chrome://tab-strip.
    Fixed: 1015632
    Change-Id: I657fb15a63132ce53e2f64e7e4a5b2d1c1302074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1881808
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710062}

--- chrome/browser/ui/webui/tab_strip/tab_strip_ui.cc
+++ chrome/browser/ui/webui/tab_strip/tab_strip_ui.cc
@@ -40,10 +40,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/favicon_base/favicon_url_parser.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/favicon_status.h"
+#include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/browser/web_ui_message_handler.h"
+#include "content/public/common/url_constants.h"
 #include "third_party/skia/include/core/SkImageEncoder.h"
 #include "third_party/skia/include/core/SkStream.h"
 #include "ui/base/models/simple_menu_model.h"
@@ -425,6 +427,10 @@ class TabStripUIHandler : public content::WebUIMessageHandler,
 
 TabStripUI::TabStripUI(content::WebUI* web_ui)
     : content::WebUIController(web_ui) {
+  content::HostZoomMap::Get(web_ui->GetWebContents()->GetSiteInstance())
+      ->SetZoomLevelForHostAndScheme(content::kChromeUIScheme,
+                                     chrome::kChromeUITabStripHost, 0);
+
   Profile* profile = Profile::FromWebUI(web_ui);
   content::WebUIDataSource* html_source =
       content::WebUIDataSource::Create(chrome::kChromeUITabStripHost);
