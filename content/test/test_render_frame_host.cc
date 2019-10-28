c2fc2eeefb7714cd9b9d36045c8e292392db759a
Arthur Hemery <ahemery@chromium.org>
2019-10-28 12:48:18 +0000

    [navigation] Enforce having a NavigationClient at commit time.
    We are now always using the NavigationClient to commit a successful
    navigation or an error page. We update all the "commit calling" stack
    to reflect that.
    
    One important exception to note is committed interstitials, that commit
    without a NavigationRequest. This prevent us from fully removing the old
    FrameNavigationControl::CommitNavigation path.
    
    Bug: 784904
    Change-Id: Ife819ed0d2274630939323ad21863c354229a3f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1819242
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709866}

--- content/test/test_render_frame_host.cc
+++ content/test/test_render_frame_host.cc
@@ -442,7 +442,6 @@ void TestRenderFrameHost::SimulateCommitProcessed(
         browser_interface_broker_receiver,
     bool same_document) {
   CHECK(params);
-  blink::mojom::CommitResult result = blink::mojom::CommitResult::Ok;
 
   bool did_commit = false;
   if (!same_document) {
@@ -451,13 +450,7 @@ void TestRenderFrameHost::SimulateCommitProcessed(
     // is unique across all callback storages.
     {
       auto callback_it = commit_callback_.find(navigation_request);
-      if (callback_it != commit_callback_.end())
-        std::move(callback_it->second).Run(result);
-    }
-    {
-      auto callback_it =
-          navigation_client_commit_callback_.find(navigation_request);
-      if (callback_it != navigation_client_commit_callback_.end()) {
+      if (callback_it != commit_callback_.end()) {
         std::move(callback_it->second)
             .Run(std::move(params),
                  mojom::DidCommitProvisionalLoadInterfaceParams::New(
@@ -470,13 +463,7 @@ void TestRenderFrameHost::SimulateCommitProcessed(
     }
     {
       auto callback_it = commit_failed_callback_.find(navigation_request);
-      if (callback_it != commit_failed_callback_.end())
-        std::move(callback_it->second).Run(result);
-    }
-    {
-      auto callback_it =
-          navigation_client_commit_failed_callback_.find(navigation_request);
-      if (callback_it != navigation_client_commit_failed_callback_.end()) {
+      if (callback_it != commit_failed_callback_.end()) {
         std::move(callback_it->second)
             .Run(std::move(params),
                  mojom::DidCommitProvisionalLoadInterfaceParams::New(
@@ -533,13 +520,9 @@ void TestRenderFrameHost::SendCommitNavigation(
     const base::UnguessableToken& devtools_navigation_token) {
   if (!navigation_request)
     return;
-  if (navigation_client) {
-    navigation_client_commit_callback_[navigation_request] =
-        BuildNavigationClientCommitNavigationCallback(navigation_request);
-  } else {
-    commit_callback_[navigation_request] =
-        BuildCommitNavigationCallback(navigation_request);
-  }
+  CHECK(navigation_client);
+  commit_callback_[navigation_request] =
+      BuildCommitNavigationCallback(navigation_request);
 }
 
 void TestRenderFrameHost::SendCommitFailedNavigation(
@@ -552,13 +535,9 @@ void TestRenderFrameHost::SendCommitFailedNavigation(
     const base::Optional<std::string>& error_page_content,
     std::unique_ptr<blink::URLLoaderFactoryBundleInfo>
         subresource_loader_factories) {
-  if (navigation_client) {
-    navigation_client_commit_failed_callback_[navigation_request] =
-        BuildNavigationClientCommitFailedNavigationCallback(navigation_request);
-  } else {
-    commit_failed_callback_[navigation_request] =
-        BuildCommitFailedNavigationCallback(navigation_request);
-  }
+  CHECK(navigation_client);
+  commit_failed_callback_[navigation_request] =
+      BuildCommitFailedNavigationCallback(navigation_request);
 }
 
 std::unique_ptr<FrameHostMsg_DidCommitProvisionalLoad_Params>
