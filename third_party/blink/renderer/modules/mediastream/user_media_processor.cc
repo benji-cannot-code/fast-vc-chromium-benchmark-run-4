623f03496f209f520deb35d676a694776b4cdfb9
henrika <henrika@chromium.org>
2019-10-28 14:37:07 +0000

    Minor modifications of WebRTC logs in UserMediaProcessor.
    Trivial change which only cleans up WebRTC logs containing the string
    "request_id=n". We used to mix "request_id=n" and "request_id = n" but
    now all white-spaces are removed to make the logs more consistent and
    easier to parse.
    
    Bug: 1017219
    Change-Id: I0aed5cccb7c77d14a63b8a972ae92ce748ba88cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1878368
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709891}

--- third_party/blink/renderer/modules/mediastream/user_media_processor.cc
+++ third_party/blink/renderer/modules/mediastream/user_media_processor.cc
@@ -518,7 +518,7 @@ void UserMediaProcessor::ProcessRequest(
   request_completed_cb_ = std::move(callback);
   current_request_info_ = MakeGarbageCollected<RequestInfo>(std::move(request));
   blink::WebRtcLogMessage(base::StringPrintf(
-      "UMP::ProcessRequest. request_id = %d. Has audio = %d. Has video = %d.",
+      "UMP::ProcessRequest. request_id=%d. Has audio=%d. Has video=%d.",
       current_request_info_->request_id(),
       current_request_info_->request()->web_request.Audio(),
       current_request_info_->request()->web_request.Video()));
@@ -535,7 +535,7 @@ void UserMediaProcessor::SetupAudioInput() {
   DCHECK(current_request_info_);
   DCHECK(current_request_info_->web_request().Audio());
   blink::WebRtcLogMessage(base::StringPrintf(
-      "UMP::SetupAudioInput. request_id = %d, audio constraints = %s",
+      "UMP::SetupAudioInput. request_id=%d, audio constraints=%s",
       current_request_info_->request_id(),
       current_request_info_->request()
           ->web_request.AudioConstraints()
@@ -555,7 +555,7 @@ void UserMediaProcessor::SetupAudioInput() {
 
   if (blink::IsDeviceMediaType(audio_controls.stream_type)) {
     blink::WebRtcLogMessage(
-        base::StringPrintf("UMP::SetupAudioInput. request_id = %d, "
+        base::StringPrintf("UMP::SetupAudioInput. request_id=%d, "
                            "Requesting device capabilities",
                            current_request_info_->request_id()));
     GetMediaDevicesDispatcher()->GetAudioInputCapabilities(WTF::Bind(
@@ -624,7 +624,7 @@ void UserMediaProcessor::SelectAudioSettings(
 
   DCHECK(current_request_info_->stream_controls()->audio.requested);
   blink::WebRtcLogMessage(
-      base::StringPrintf("UMP::SelectAudioSettings. request_id = %d.",
+      base::StringPrintf("UMP::SelectAudioSettings. request_id=%d.",
                          current_request_info_->request_id()));
   auto settings = SelectSettingsAudioCapture(
       capabilities, web_request.AudioConstraints(),
@@ -703,7 +703,7 @@ void UserMediaProcessor::SetupVideoInput() {
     return;
   }
   blink::WebRtcLogMessage(base::StringPrintf(
-      "UMP::SetupVideoInput. request_id = %d, video constraints = %s",
+      "UMP::SetupVideoInput. request_id=%d, video constraints=%s",
       current_request_info_->request_id(),
       current_request_info_->request()
           ->web_request.VideoConstraints()
@@ -749,7 +749,7 @@ void UserMediaProcessor::SelectVideoDeviceSettings(
   DCHECK(blink::IsDeviceMediaType(
       current_request_info_->stream_controls()->video.stream_type));
   blink::WebRtcLogMessage(
-      base::StringPrintf("UMP::SelectVideoDeviceSettings. request_id = %d.",
+      base::StringPrintf("UMP::SelectVideoDeviceSettings. request_id=%d.",
                          current_request_info_->request_id()));
 
   blink::VideoDeviceCaptureCapabilities capabilities;
@@ -793,7 +793,7 @@ void UserMediaProcessor::SelectVideoContentSettings() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK(current_request_info_);
   blink::WebRtcLogMessage(
-      base::StringPrintf("UMP::SelectVideoContentSettings. request_id = %d.",
+      base::StringPrintf("UMP::SelectVideoContentSettings. request_id=%d.",
                          current_request_info_->request_id()));
   gfx::Size screen_size = GetScreenSize();
   blink::VideoCaptureSettings settings =
@@ -883,7 +883,7 @@ void UserMediaProcessor::OnStreamGenerated(
     return;
   }
   blink::WebRtcLogMessage(
-      base::StringPrintf("UMP::OnStreamGenerated. request_id = %d.",
+      base::StringPrintf("UMP::OnStreamGenerated. request_id=%d.",
                          current_request_info_->request_id()));
 
   current_request_info_->set_state(RequestInfo::State::GENERATED);
@@ -1034,7 +1034,7 @@ void UserMediaProcessor::OnStreamGenerationFailed(
     return;
   }
   blink::WebRtcLogMessage(
-      base::StringPrintf("UMP::OnStreamGenerationFailed. request_id = %d.",
+      base::StringPrintf("UMP::OnStreamGenerationFailed. request_id=%d.",
                          current_request_info_->request_id()));
 
   GetUserMediaRequestFailed(result);
