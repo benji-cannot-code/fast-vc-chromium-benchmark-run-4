ddb7bcb55e5d15ca77cc872e2b0e7fae6c428785
Archie Pusaka <apusaka@chromium.org>
2019-10-28 05:45:25 +0000

    [CrOS Bluetooth] Persistent Bluetooth Verbose Log
    Makes the verbose bluetooth option persist across device reboot by
    adding a new preference flag.
    
    Bug: 1004572
    Change-Id: Ibdb278c9b4a326368a591463223e0d1241f2b115
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810451
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Archie Pusaka <apusaka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709806}

--- chrome/browser/chromeos/bluetooth/debug_logs_manager.h
+++ chrome/browser/chromeos/bluetooth/debug_logs_manager.h
@@ -10,9 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals.mojom.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 
-namespace user_manager {
-class User;
-}  // namespace user_manager
+class PrefService;
+class PrefRegistrySimple;
 
 namespace chromeos {
 
@@ -25,7 +24,8 @@ namespace bluetooth {
 // state of debug logs and handles the user enabling/disabling them.
 class DebugLogsManager : public mojom::DebugLogsChangeHandler {
  public:
-  explicit DebugLogsManager(const user_manager::User* primary_user);
+  DebugLogsManager(const std::string& primary_user_email,
+                   PrefService* pref_service);
   ~DebugLogsManager() override;
 
   // State for capturing debug Bluetooth logs; logs are only captured when
@@ -38,19 +38,21 @@ class DebugLogsManager : public mojom::DebugLogsChangeHandler {
     kSupportedAndEnabled
   };
 
+  static void RegisterPrefs(PrefRegistrySimple* registry);
+
   DebugLogsState GetDebugLogsState() const;
+
+  // mojom::DebugLogsManager:
+  void ChangeDebugLogsState(bool should_debug_logs_be_enabled) override;
 
   // Generates an InterfacePtr bound to this object.
   mojom::DebugLogsChangeHandlerPtr GenerateInterfacePtr();
 
  private:
-  // mojom::DebugLogsManager:
-  void ChangeDebugLogsState(bool should_debug_logs_be_enabled) override;
-
   bool AreDebugLogsSupported() const;
 
-  const user_manager::User* primary_user_ = nullptr;
-  bool are_debug_logs_enabled_ = false;
+  const std::string primary_user_email_;
+  PrefService* pref_service_ = nullptr;
   mojo::BindingSet<mojom::DebugLogsChangeHandler> bindings_;
 
   DISALLOW_COPY_AND_ASSIGN(DebugLogsManager);
--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -244,6 +244,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/app_mode/web_app/web_kiosk_app_manager.h"
 #include "chrome/browser/chromeos/arc/policy/arc_policy_bridge.h"
 #include "chrome/browser/chromeos/arc/session/arc_session_manager.h"
+#include "chrome/browser/chromeos/bluetooth/debug_logs_manager.h"
 #include "chrome/browser/chromeos/child_accounts/parent_access_code/parent_access_service.h"
 #include "chrome/browser/chromeos/child_accounts/screen_time_controller.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
@@ -936,6 +937,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   certificate_manager::CertificatesHandler::RegisterProfilePrefs(registry);
   chromeos::AccountManager::RegisterPrefs(registry);
   chromeos::assistant::prefs::RegisterProfilePrefsForBrowser(registry);
+  chromeos::bluetooth::DebugLogsManager::RegisterPrefs(registry);
   chromeos::CupsPrintersManager::RegisterProfilePrefs(registry);
   chromeos::device_sync::DeviceSyncImpl::RegisterProfilePrefs(registry);
   chromeos::first_run::RegisterProfilePrefs(registry);
