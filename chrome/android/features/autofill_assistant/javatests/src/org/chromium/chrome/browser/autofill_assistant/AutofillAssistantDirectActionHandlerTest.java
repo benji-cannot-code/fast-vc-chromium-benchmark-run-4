3a455636bfc9961e00488d966562191ae80ea546
Mathias Carlen <mcarlen@chromium.org>
2019-10-28 11:26:02 +0000

    [Autofill Assistant] Filter fetch_website_actions.
    Before this patch, all potential direct actions were reported
    unconditionally. The fetch_website_actions direct action is only
    required to bring up the AA stack and fetch/check for scripts once.
    This patch ties this filtering to the controller being up and ready and
    the first round of scripts has been checked for matching preconditions.
    
    This will give external callers a better signal as to when AA is up to
    date and when it needs to be setup first before being able to execute
    actions.
    
    Bug: b/138833619
    Change-Id: I8acb0c7ea7937a0cbafe1462e92251c29153b818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879911
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Stephane Zermatten <szermatt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#709851}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantDirectActionHandlerTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantDirectActionHandlerTest.java
@@ -86,7 +86,7 @@ public class AutofillAssistantDirectActionHandlerTest {
         mModuleEntryProvider.setInstalled();
 
         FakeDirectActionReporter reporter = new FakeDirectActionReporter();
-        mHandler.reportAvailableDirectActions(reporter);
+        reportAvailableDirectActions(mHandler, reporter);
 
         assertEquals(1, reporter.mActions.size());
 
@@ -111,7 +111,7 @@ public class AutofillAssistantDirectActionHandlerTest {
         // Start the autofill assistant stack.
 
         FakeDirectActionReporter reporter = new FakeDirectActionReporter();
-        mHandler.reportAvailableDirectActions(reporter);
+        reportAvailableDirectActions(mHandler, reporter);
 
         assertEquals(2, reporter.mActions.size());
 
@@ -143,16 +143,15 @@ public class AutofillAssistantDirectActionHandlerTest {
         fetchWebsiteActions();
         // Reset the reported actions.
         reporter = new FakeDirectActionReporter();
-        mHandler.reportAvailableDirectActions(reporter);
+        reportAvailableDirectActions(mHandler, reporter);
 
-        assertEquals(4, reporter.mActions.size());
+        // Now that the AA stack is up, the fetdch_website_actions should no longer show up.
+        assertEquals(3, reporter.mActions.size());
 
-        // The previous two actions are still reported for now.
         assertEquals("perform_assistant_action", reporter.mActions.get(0).mId);
-        assertEquals("fetch_website_actions", reporter.mActions.get(1).mId);
 
         // Now we expect 2 dyamic actions "search" and "action2".
-        FakeDirectActionDefinition search = reporter.mActions.get(2);
+        FakeDirectActionDefinition search = reporter.mActions.get(1);
         assertEquals("search", search.mId);
         assertEquals(1, search.mParameters.size());
         assertEquals("experiment_ids", search.mParameters.get(0).mName);
@@ -161,7 +160,7 @@ public class AutofillAssistantDirectActionHandlerTest {
         assertEquals("success", search.mResults.get(0).mName);
         assertEquals(Type.BOOLEAN, search.mResults.get(0).mType);
 
-        FakeDirectActionDefinition action2 = reporter.mActions.get(3);
+        FakeDirectActionDefinition action2 = reporter.mActions.get(2);
         assertEquals("action2", action2.mId);
         assertEquals(1, action2.mParameters.size());
         assertEquals("experiment_ids", action2.mParameters.get(0).mName);
@@ -178,7 +177,7 @@ public class AutofillAssistantDirectActionHandlerTest {
         AutofillAssistantPreferencesUtil.setInitialPreferences(true);
 
         FakeDirectActionReporter reporter = new FakeDirectActionReporter();
-        mHandler.reportAvailableDirectActions(reporter);
+        reportAvailableDirectActions(mHandler, reporter);
 
         assertEquals(2, reporter.mActions.size());
 
@@ -259,9 +258,9 @@ public class AutofillAssistantDirectActionHandlerTest {
         assertEquals(Boolean.TRUE, onboardingCallback.waitForResult("accept onboarding"));
     }
 
-    private boolean isOnboardingReported() {
+    private boolean isOnboardingReported() throws Exception {
         FakeDirectActionReporter reporter = new FakeDirectActionReporter();
-        mHandler.reportAvailableDirectActions(reporter);
+        reportAvailableDirectActions(mHandler, reporter);
 
         for (FakeDirectActionDefinition definition : reporter.mActions) {
             if (definition.mId.equals("onboarding")) {
@@ -282,6 +281,18 @@ public class AutofillAssistantDirectActionHandlerTest {
                         -> mHandler.performDirectAction(
                                 "fetch_website_actions", Bundle.EMPTY, callback)));
         return callback.waitForResult("fetch_website_actions").getBoolean("success", false);
+    }
+
+    /**
+     * When reporting direct actions involves web_contents in the controller, it needs to run on the
+     * UI thread.
+     */
+    private void reportAvailableDirectActions(
+            DirectActionHandler handler, DirectActionReporter reporter) throws Exception {
+        assertTrue(TestThreadUtils.runOnUiThreadBlocking(() -> {
+            handler.reportAvailableDirectActions(reporter);
+            return true;
+        }));
     }
 
     /** Calls perform_assistant_action and returns the result. */
