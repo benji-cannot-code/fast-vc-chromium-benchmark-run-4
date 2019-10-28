b29b7e16ef3b7bc1611800da608e4d9157975d31
Carlos Caballero <carlscab@google.com>
2019-10-28 13:10:49 +0000

    Remove MessageLoop references in services/service_manager/tests/connect
    MessageLoop is going away soon.
    
    This CL was uploaded by git cl split.
    
    R=jam@chromium.org
    
    Bug: 891670
    Change-Id: I224d419e31a6fb41156fed62bf856674fc69e6e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1878372
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#709874}

--- services/service_manager/tests/connect/connect_test_exe.cc
+++ services/service_manager/tests/connect/connect_test_exe.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
+#include "base/task/single_thread_task_executor.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/service.h"
@@ -63,6 +63,6 @@ class Target : public service_manager::Service,
 }  // namespace
 
 void ServiceMain(service_manager::mojom::ServiceRequest request) {
-  base::MessageLoop message_loop;
+  base::SingleThreadTaskExecutor executor;
   Target(std::move(request)).RunUntilTermination();
 }
