362c3ec8109063822b69e2196af59a09c272af6d
kylechar <kylechar@chromium.org>
2019-10-29 20:08:10 +0000

    Fix scoped_refptr construction from NULL
    This is a precursor to adding a new scoped_refptr(std::nullptr_t)
    constructor. The implicit conversion from NULL to scoped_refptr<T>
    causes a compilation error with the new constructor. Replace NULL with
    nullptr in any files where this is a problem.
    
    This CL was uploaded by git cl split.
    
    R=raymes@chromium.org
    
    Bug: 1018887
    Change-Id: I2b2693e82a1aaf15022a680dda56122b7b09a6ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1885013
    Auto-Submit: kylechar <kylechar@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710423}

--- ppapi/host/resource_message_filter.cc
+++ ppapi/host/resource_message_filter.cc
@@ -37,15 +37,13 @@ void ResourceMessageFilterDeleteTraits::Destruct(
 ResourceMessageFilter::ResourceMessageFilter()
     : deletion_task_runner_(base::ThreadTaskRunnerHandle::Get()),
       reply_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()),
-      resource_host_(NULL) {
-}
+      resource_host_(nullptr) {}
 
 ResourceMessageFilter::ResourceMessageFilter(
     scoped_refptr<base::SingleThreadTaskRunner> reply_thread_task_runner)
     : deletion_task_runner_(base::ThreadTaskRunnerHandle::Get()),
       reply_thread_task_runner_(reply_thread_task_runner),
-      resource_host_(NULL) {
-}
+      resource_host_(nullptr) {}
 
 ResourceMessageFilter::~ResourceMessageFilter() {
 }
@@ -93,7 +91,7 @@ void ResourceMessageFilter::SendReply(const ReplyMessageContext& context,
 
 scoped_refptr<base::TaskRunner>
 ResourceMessageFilter::OverrideTaskRunnerForMessage(const IPC::Message& msg) {
-  return NULL;
+  return nullptr;
 }
 
 void ResourceMessageFilter::DispatchMessage(const IPC::Message& msg,
--- ppapi/proxy/websocket_resource.cc
+++ ppapi/proxy/websocket_resource.cc
@@ -60,17 +60,16 @@ WebSocketResource::WebSocketResource(Connection connection,
     : PluginResource(connection, instance),
       state_(PP_WEBSOCKETREADYSTATE_INVALID),
       error_was_received_(false),
-      receive_callback_var_(NULL),
+      receive_callback_var_(nullptr),
       empty_string_(new StringVar(std::string())),
       close_code_(0),
-      close_reason_(NULL),
+      close_reason_(nullptr),
       close_was_clean_(PP_FALSE),
-      extensions_(NULL),
-      protocol_(NULL),
-      url_(NULL),
+      extensions_(nullptr),
+      protocol_(nullptr),
+      url_(nullptr),
       buffered_amount_(0),
-      buffered_amount_after_close_(0) {
-}
+      buffered_amount_after_close_(0) {}
 
 WebSocketResource::~WebSocketResource() {
 }
@@ -176,7 +175,7 @@ int32_t WebSocketResource::Close(uint16_t code,
     state_ = PP_WEBSOCKETREADYSTATE_CLOSING;
     // Need to do a "Post" to avoid reentering the plugin.
     connect_callback_->PostAbort();
-    connect_callback_ = NULL;
+    connect_callback_ = nullptr;
     Post(RENDERER, PpapiHostMsg_WebSocket_Fail(
         "WebSocket was closed before the connection was established."));
     return PP_OK_COMPLETIONPENDING;
@@ -184,10 +183,10 @@ int32_t WebSocketResource::Close(uint16_t code,
 
   // Abort ongoing receive.
   if (TrackedCallback::IsPending(receive_callback_)) {
-    receive_callback_var_ = NULL;
+    receive_callback_var_ = nullptr;
     // Need to do a "Post" to avoid reentering the plugin.
     receive_callback_->PostAbort();
-    receive_callback_ = NULL;
+    receive_callback_ = nullptr;
   }
 
   // Close connection.
@@ -388,16 +387,16 @@ void WebSocketResource::OnPluginMsgCloseReply(
   close_reason_ = new StringVar(reason);
 
   if (TrackedCallback::IsPending(receive_callback_)) {
-    receive_callback_var_ = NULL;
+    receive_callback_var_ = nullptr;
     if (!TrackedCallback::IsScheduledToRun(receive_callback_))
       receive_callback_->PostRun(PP_ERROR_FAILED);
-    receive_callback_ = NULL;
+    receive_callback_ = nullptr;
   }
 
   if (TrackedCallback::IsPending(close_callback_)) {
     if (!TrackedCallback::IsScheduledToRun(close_callback_))
       close_callback_->PostRun(params.result());
-    close_callback_ = NULL;
+    close_callback_ = nullptr;
   }
 }
 
@@ -452,7 +451,7 @@ void WebSocketResource::OnPluginMsgErrorReply(
 
   // No more text or binary messages will be received. If there is ongoing
   // ReceiveMessage(), we must invoke the callback with error code here.
-  receive_callback_var_ = NULL;
+  receive_callback_var_ = nullptr;
   receive_callback_->Run(PP_ERROR_FAILED);
 }
 
@@ -483,7 +482,7 @@ int32_t WebSocketResource::DoReceive() {
 
   *receive_callback_var_ = received_messages_.front()->GetPPVar();
   received_messages_.pop();
-  receive_callback_var_ = NULL;
+  receive_callback_var_ = nullptr;
   return PP_OK;
 }
 
