7d67d01fc0956bf6705f5891141b3dc77f5ab375
Brian Geffon <bgeffon@chromium.org>
2019-10-29 23:49:08 +0000

    chromeos: Add SwapSetParamter dbus message and Feature.
    Add a SwapSetParameter dbus message and then add a feature
    with the corresponding parameter for changing min_filelist.
    
    BUG=chromium:1014871
    TESTED=On a betty VM CrOS image.
    
    Change-Id: I2c875d0d2963fdbc253ae1c4419e6ad6794e5de0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880966
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710510}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -115,6 +115,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/constants/chromeos_switches.h"
 #include "chromeos/hugepage_text/hugepage_text.h"
 #include "chromeos/memory/kstaled.h"
+#include "chromeos/memory/swap_configuration.h"
 #endif
 
 #if defined(OS_ANDROID)
@@ -602,6 +603,7 @@ void ChromeMainDelegate::PostFieldTrialInitialization() {
     heap_profiler_controller_->Start();
 
 #if defined(OS_CHROMEOS)
+    chromeos::ConfigureSwap();
     chromeos::InitializeKstaled();
 #endif
   }
