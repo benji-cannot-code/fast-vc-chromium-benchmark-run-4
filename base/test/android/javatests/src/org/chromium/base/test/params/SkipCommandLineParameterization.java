9d52bee4e9d147e6a411b7183f0fa7907629faca
Andrew Luo <aluo@chromium.org>
2019-10-29 04:25:50 +0000

    Allow modification of multiple cmdline params with ParameterizedCommandLine.
    This is needed for Page Controller or other UI-Automator based tests that can't
    use the CommandLineFlags mechanism.
    
    Bug: 1009529
    Change-Id: If0364f37c2fe3f49e2111c2ed505fd4ed9392e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860144
    Commit-Queue: Andrew Luo <aluo@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710197}

--- base/test/android/javatests/src/org/chromium/base/test/params/SkipCommandLineParameterization.java
+++ base/test/android/javatests/src/org/chromium/base/test/params/SkipCommandLineParameterization.java
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-package org.chromium.base.test.util.parameter;
+package org.chromium.base.test.params;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
