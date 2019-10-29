b6fdca075792152e0de20726c6be6aa18fc45513
Moe Ahmadi <mahmadi@chromium.org>
2019-10-29 23:04:38 +0000

    Local NTP, Realbox: Adds support for having no initially selected match
    - Makes changes to support having no selected matches. For instance,
      'Enter' should do nothing when there are no selected matches.
    - Selects the first match after populating the matches only if it can be
      the default match. For NTP zero-prefix suggestions, no match is allowed
      to be the default match. Therefore there will be no default selection.
    - Adds a new test and makes changes to the existing ones.
    
    Bug: 243926,1016845
    Change-Id: Iba56b0aef467c917d96cd01966f15d18258264df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1884266
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710487}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -1078,7 +1078,8 @@ function onDeleteAutocompleteMatch(result) {
     return matchEl.classList.contains(CLASSES.SELECTED);
   });
 
-  const wasFocused = matchEls[selected].contains(document.activeElement);
+  const wasFocused =
+      matchEls[selected] && matchEls[selected].contains(document.activeElement);
 
   populateAutocompleteMatches(result.matches);
 
@@ -1090,7 +1091,7 @@ function onDeleteAutocompleteMatch(result) {
   }
 
   const newMatchEls = Array.from($(IDS.REALBOX_MATCHES).children);
-  const newSelected = Math.min(newMatchEls.length - 1, selected);
+  const newSelected = Math.max(Math.min(newMatchEls.length - 1, selected), 0);
   const newSelectedEl = newMatchEls[newSelected];
 
   selectMatchEl(newSelectedEl);
@@ -1163,7 +1164,9 @@ function onQueryAutocompleteDone(result) {
     return;
   }
 
-  selectMatchEl(assert($(IDS.REALBOX_MATCHES).firstElementChild));
+  if (result.matches[0].allowedToBeDefaultMatch) {
+    selectMatchEl(assert($(IDS.REALBOX_MATCHES).firstElementChild));
+  }
 
   // If the user is deleting content, don't quickly re-suggest the same
   // output.
@@ -1191,7 +1194,7 @@ function onRealboxCutCopy(e) {
   });
 
   const selectedMatch = autocompleteMatches[selected];
-  if (!selectedMatch.isSearchType) {
+  if (selectedMatch && !selectedMatch.isSearchType) {
     e.clipboardData.setData('text/plain', selectedMatch.destinationUrl);
     e.preventDefault();
     if (e.type === 'cut') {
@@ -1287,13 +1290,15 @@ function onRealboxWrapperKeydown(e) {
   const selected = matchEls.findIndex(matchEl => {
     return matchEl.classList.contains(CLASSES.SELECTED);
   });
-  assert(autocompleteMatches[selected]);
 
   if (key === 'Delete') {
-    if (e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey &&
-        autocompleteMatches[selected].supportsDeletion) {
-      window.chrome.embeddedSearch.searchBox.deleteAutocompleteMatch(selected);
-      e.preventDefault();
+    if (e.shiftKey && !e.altKey && !e.ctrlKey && !e.metaKey) {
+      const selectedMatch = autocompleteMatches[selected];
+      if (selectedMatch && selectedMatch.supportsDeletion) {
+        window.chrome.embeddedSearch.searchBox.deleteAutocompleteMatch(
+            selected);
+        e.preventDefault();
+      }
     }
     return;
   }
