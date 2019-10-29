295a18855bc81d5d753af20fae0f63aa4cbbda1b
Aaron Krajeski <aaronhk@google.com>
2019-10-29 11:51:32 +0000

    Dont RestoreCanvasMatrixClipStack for deferred canvases
    When we create a resource provider we replay the old transform if
    needed. For deferred canvases, this is handled when flushing the
    recording, so with the old logic we could do it twice.
    
    Bug: 1014537
    Change-Id: I76d628df621bca5a15f2a0ce51d1a6e663593d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869513
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710299}

--- third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
@@ -271,6 +271,7 @@ ImageBitmap* OffscreenCanvasRenderingContext2D::TransferToImageBitmap(
   if (is_deferral_enabled_) {
     recorder_->getRecordingCanvas()->restore();
     recorder_->getRecordingCanvas()->save();
+    Host()->SetNeedsMatrixClipRestore();
   }
 
   return ImageBitmap::Create(std::move(image));
