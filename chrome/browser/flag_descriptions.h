cb9819eb4c3204df0799a4c3accff3c959fccfe1
My Nguyen <myy@chromium.org>
2019-10-29 03:52:32 +0000

    Add Virtual Keyboard bordered key experiment to chrome flag
    BUG: b/143232594
    
    Change-Id: I76b160ad565649db32a21b984fbed1594b0ae6ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1875347
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: My Nguyen <myy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710190}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2244,6 +2244,9 @@ extern const char kVaapiJpegImageDecodeAccelerationDescription[];
 extern const char kVaapiWebPImageDecodeAccelerationName[];
 extern const char kVaapiWebPImageDecodeAccelerationDescription[];
 
+extern const char kVirtualKeyboardBorderedKeyName[];
+extern const char kVirtualKeyboardBorderedKeyDescription[];
+
 extern const char kVirtualKeyboardName[];
 extern const char kVirtualKeyboardDescription[];
 
