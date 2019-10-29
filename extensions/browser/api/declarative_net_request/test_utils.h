11f619eb1632adc82ba15aae32f5cc80aca389e5
Karan Bhatia <karandeepb@chromium.org>
2019-10-29 23:53:18 +0000

    DNR: Add support to print RequestAction in tests.
    Add the << operator for RequestAction so that gtest can print a RequestAction
    object whenever an assertion/expectation fails. Also, improve how the equals
    operator for RequestAction is implemented.
    
    BUG=696822
    
    Change-Id: Ic93382d0459274548f850b4279f1db9944ef4864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1888068
    Auto-Submit: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710512}

--- extensions/browser/api/declarative_net_request/test_utils.h
+++ extensions/browser/api/declarative_net_request/test_utils.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_BROWSER_API_DECLARATIVE_NET_REQUEST_TEST_UTILS_H_
 
 #include <memory>
+#include <ostream>
 #include <vector>
 
 #include "extensions/browser/api/declarative_net_request/request_action.h"
@@ -43,7 +44,10 @@ RequestAction CreateRequestActionForTesting(
         api::declarative_net_request::SOURCE_TYPE_MANIFEST,
     const ExtensionId& extension_id = "extensionid");
 
+// Test helpers for help with gtest expectations and assertions.
 bool operator==(const RequestAction& lhs, const RequestAction& rhs);
+std::ostream& operator<<(std::ostream& output, RequestAction::Type type);
+std::ostream& operator<<(std::ostream& output, const RequestAction& action);
 
 // Returns true if the given extension has a valid indexed ruleset. Should be
 // called on a sequence where file IO is allowed.
