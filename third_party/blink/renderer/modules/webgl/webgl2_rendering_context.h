520c9517b255e5f20c39555a569237505a3c807c
Austin Eng <enga@chromium.org>
2019-10-29 12:55:29 +0000

    Merge WEBGL_multi_draw_instanced into WEBGL_multi_draw
    The WebGL working group decided there is enough WebGL 1 support for
    instancing that it would be simpler to only expose the instanced
    version of the extension.
    
    This also rolls WebGL 2701c13..69e333c which includes the spec and
    conformance test changes.
    
    Bug: chromium:890539
    Change-Id: Ib4dccf56f488cedf8161268ac3467d27c9b371c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1881712
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710310}

--- third_party/blink/renderer/modules/webgl/webgl2_rendering_context.h
+++ third_party/blink/renderer/modules/webgl/webgl2_rendering_context.h
@@ -23,7 +23,6 @@ class OVRMultiview2;
 class WebGLDebugRendererInfo;
 class WebGLLoseContext;
 class WebGLMultiDraw;
-class WebGLMultiDrawInstanced;
 class KHRParallelShaderCompile;
 class WebGLVideoTexture;
 
@@ -83,7 +82,6 @@ class WebGL2RenderingContext : public WebGL2RenderingContextBase {
   Member<WebGLDebugShaders> webgl_debug_shaders_;
   Member<WebGLLoseContext> webgl_lose_context_;
   Member<WebGLMultiDraw> webgl_multi_draw_;
-  Member<WebGLMultiDrawInstanced> webgl_multi_draw_instanced_;
   Member<WebGLVideoTexture> webgl_video_texture_;
 };
 
--- third_party/blink/renderer/modules/webgl/webgl_rendering_context.h
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context.h
@@ -56,7 +56,6 @@ class WebGLDebugRendererInfo;
 class WebGLDepthTexture;
 class WebGLLoseContext;
 class WebGLMultiDraw;
-class WebGLMultiDrawInstanced;
 class WebGLVideoTexture;
 
 class WebGLRenderingContext final : public WebGLRenderingContextBase {
@@ -129,7 +128,6 @@ class WebGLRenderingContext final : public WebGLRenderingContextBase {
   Member<WebGLDrawBuffers> webgl_draw_buffers_;
   Member<WebGLLoseContext> webgl_lose_context_;
   Member<WebGLMultiDraw> webgl_multi_draw_;
-  Member<WebGLMultiDrawInstanced> webgl_multi_draw_instanced_;
   Member<WebGLVideoTexture> webgl_video_texture_;
 };
 
