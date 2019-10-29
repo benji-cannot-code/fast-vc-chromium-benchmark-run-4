706a520d6c1eea399a33285da3964fce5b0badb6
Piotr Pawliczek <pawliczek@chromium.org>
2019-10-29 18:10:52 +0000

    External Print Servers: UMA metric with number of servers
    This patch adds UMA metric with a final number of print servers to
    query.
    
    BUG=chromium:1018591
    TEST=none
    
    Change-Id: I19f1608b9b60e869c461c1c8a1ffde59c9446ee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1882811
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Luum Habtemariam <luum@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Auto-Submit: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710396}

--- chrome/browser/chromeos/printing/server_printers_provider.cc
+++ chrome/browser/chromeos/printing/server_printers_provider.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/sequenced_task_runner.h"
 #include "base/stl_util.h"
 #include "base/task/post_task.h"
@@ -75,6 +76,10 @@ class ServerPrintersProviderImpl
     if (servers_are_complete) {
       PRINTER_LOG(EVENT) << "The list of print servers has been completed. "
                          << "Number of print servers: " << servers.size();
+      if (!servers.empty()) {
+        base::UmaHistogramCounts1000("Printing.PrintServers.ServersToQuery",
+                                     servers.size());
+      }
     }
     // Save previous state.
     const bool previous_complete = IsComplete();
