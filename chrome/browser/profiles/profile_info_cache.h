3e40c9f306663833d9123fa3500c3034c9638e05
Monica Basta <msalama@chromium.org>
2019-10-29 10:38:39 +0000

    [Signin]: Migrate Legacy default profile names to 'Person %n' on Desktop.
    This CL does the following:
    (1) If the user has not customized their local profile name |IsUsingDefaultName()
    == true| and their local profile name is not 'Person %n', we change the
    local profile name to 'Person %n'.
    (2) For newly created profiles, we set the local profile name to 'Person %n'
    unless the user specifies something else.
    (3) Fix some corner cases for the display profile name.
    
    Bug: 1018719
    Change-Id: Ic92bde725d5e33105e251214f01b646a2ff63088
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1878377
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710277}

--- chrome/browser/profiles/profile_info_cache.h
+++ chrome/browser/profiles/profile_info_cache.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_info_cache_observer.h"
@@ -169,6 +170,8 @@ class ProfileInfoCache : public ProfileInfoInterface,
  private:
   FRIEND_TEST_ALL_PREFIXES(ProfileAttributesStorageTest,
                            DownloadHighResAvatarTest);
+  FRIEND_TEST_ALL_PREFIXES(ProfileInfoCacheTest,
+                           MigrateLegacyProfileNamesAndRecomputeIfNeeded);
 
   const base::DictionaryValue* GetInfoForProfileAtIndex(size_t index) const;
   // Saves the profile info to a cache.
@@ -188,17 +191,18 @@ class ProfileInfoCache : public ProfileInfoInterface,
   // generic profile avatar.
   const gfx::Image* GetHighResAvatarOfProfileAtIndex(size_t index) const;
 
-  // Migrate any legacy profile names ("First user", "Default Profile") to
-  // new style default names ("Person 1"), and download and high-res avatars
-  // used by the profiles.
-  void MigrateLegacyProfileNamesAndDownloadAvatars();
-
-  // Recompute profile names to guarantee there are no duplicates of "Person n"
-  // exist, i.e. Two or more profiles with the profile name "Person 1" would be
-  // recomputed to "Person 1" and "Person 2".
-  void RecomputeProfileNamesIfNeeded();
-
+  // Download and high-res avatars used by the profiles.
+  void DownloadAvatars();
   void NotifyIfProfileNamesHaveChanged();
+
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+  // Migrate any legacy profile names ("First user", "Default Profile") to
+  // new style default names ("Person 1"). Rename any duplicates of "Person n"
+  // i.e. Two or more profiles with the profile name "Person 1" would be
+  // recomputed to "Person 1" and "Person 2".
+  void MigrateLegacyProfileNamesAndRecomputeIfNeeded();
+  static void EnableLegacyProfileMigrationForTesting();
+#endif  // !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
 
   std::vector<std::string> keys_;
   const base::FilePath user_data_dir_;
--- chrome/browser/profiles/profile_manager_unittest.cc
+++ chrome/browser/profiles/profile_manager_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/values.h"
 #include "build/build_config.h"
@@ -32,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_info_cache.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profiles_state.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
@@ -1524,6 +1526,14 @@ TEST_F(ProfileManagerTest, ProfileDisplayNamePreservesSignedInName) {
 TEST_F(ProfileManagerTest, ProfileDisplayNameIsEmailIfDefaultName) {
   if (!profiles::IsMultipleProfilesEnabled())
     return;
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+  // This test is relevant on desktop if |kProfileMenuRevamp| is disabled. If
+  // it is enabled for pre-existing directory with legacy profile name, they
+  // will be migrated to new default profile name |Person %n|. For newly created
+  // profiles, only |Person %n| is considered as a default profile name.
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(features::kProfileMenuRevamp);
+#endif  // !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
 
   ProfileManager* profile_manager = g_browser_process->profile_manager();
   ProfileAttributesStorage& storage =
@@ -1554,15 +1564,23 @@ TEST_F(ProfileManagerTest, ProfileDisplayNameIsEmailIfDefaultName) {
   entry->SetGAIAGivenName(base::string16());
   entry->SetGAIAName(base::string16());
 
-  // This may resort the cache, so be extra cautious to use the right profile.
   ASSERT_TRUE(storage.GetProfileAttributesWithPath(profile2->GetPath(),
                                                    &entry));
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+  // (Default profile, Batman,..) are legacy profile names on Desktop and are
+  // not considered default profile names for newly created profiles.
+  // We use "Person %n" as the default profile name. Set |SetIsUsingDefaultName|
+  // manually to mimick pre-existing profiles.
+  entry->SetIsUsingDefaultName(true);
+#endif  // !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+
   entry->SetAuthInfo("23456", email2, true);
   entry->SetGAIAGivenName(base::string16());
   entry->SetGAIAName(base::string16());
 
   ASSERT_TRUE(storage.GetProfileAttributesWithPath(profile3->GetPath(),
                                                    &entry));
+
   entry->SetAuthInfo("34567", email3, true);
   entry->SetGAIAGivenName(base::string16());
   entry->SetGAIAName(base::string16());
@@ -1577,7 +1595,7 @@ TEST_F(ProfileManagerTest, ProfileDisplayNameIsEmailIfDefaultName) {
 
   // Adding a Gaia name to a profile that previously had a default name should
   // start displaying it.
-  const base::string16 gaia_given_name(ASCIIToUTF16("Robin (Person 1)"));
+  const base::string16 gaia_given_name(ASCIIToUTF16("Robin"));
   ASSERT_TRUE(storage.GetProfileAttributesWithPath(profile1->GetPath(),
                                                    &entry));
   entry->SetGAIAGivenName(gaia_given_name);
