14b954eed86b97cc25bd8870f1d148ada926414a
Sajjad Mirza <sajjadm@google.com>
2019-10-29 22:36:34 +0000

    [code coverage] Add mock implementation of SetCoverageFile.
    Without this implementation the class is accidentally abstract and
    cannot be instantiated.
    
    Bug: 1018949
    Change-Id: I6bbe1bacd430182876c6d3765626d57f9a2f4880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1885494
    Commit-Queue: Roberto Carrillo <robertocn@chromium.org>
    Reviewed-by: Roberto Carrillo <robertocn@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710478}

--- content/browser/child_process_task_port_provider_mac_unittest.cc
+++ content/browser/child_process_task_port_provider_mac_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/clang_coverage_buildflags.h"
 #include "base/mac/scoped_mach_port.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/task/post_task.h"
@@ -33,6 +34,9 @@ class MockChildProcess : public mojom::ChildProcess {
   MOCK_METHOD1(GetTaskPort, void(GetTaskPortCallback));
 #if BUILDFLAG(IPC_MESSAGE_LOG_ENABLED)
   MOCK_METHOD1(SetIPCLoggingEnabled, void(bool));
+#endif
+#if BUILDFLAG(CLANG_COVERAGE)
+  MOCK_METHOD1(SetCoverageFile, void(base::File));
 #endif
   MOCK_METHOD1(GetBackgroundTracingAgentProvider,
                void(mojo::PendingReceiver<
