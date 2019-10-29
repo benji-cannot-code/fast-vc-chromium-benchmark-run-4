b0914ae8854c61b95de4b50bcdeb28ecba295fe7
Arthur Hemery <ahemery@chromium.org>
2019-10-29 09:55:11 +0000

    [navigation] Remove the PerNavigationMojoInterface feature.
    Removing the unusued feature.
    
    Bug: 784904
    Change-Id: I9d5bed00c0c95b634ce2be7cc1d251be691b2417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1883074
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710268}

--- content/public/common/navigation_policy.h
+++ content/public/common/navigation_policy.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-CONTENT_EXPORT bool IsPerNavigationMojoInterfaceEnabled();
 CONTENT_EXPORT bool IsBackForwardCacheEnabled();
 CONTENT_EXPORT bool IsProactivelySwapBrowsingInstanceEnabled();
 CONTENT_EXPORT bool IsNavigationImmediateResponseBodyEnabled();
