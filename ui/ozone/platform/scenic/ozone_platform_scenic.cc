9e7e984a59b9ca6e0dd05ddbefbd06b098ca7510
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2019-10-29 13:04:49 +0000

    Migrate scenic_gpu_*.mojom to new Mojo types
    Highlights:
    * There are some other changes to the code logic, derived from the
    usage of PendingRemote in function arguments instead of InterfacePtr.
    * Shorthands provided by the new Mojo types allowed to simplify some
    implementations.
    * Moved initialization of affected members to their declarations,
    where possible, to comply with coding guidelines.
    
    Bug: 955171
    Change-Id: Iebdc315dfcfebc671e0c91c9cf4f9fdd9fdf7b24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1847309
    Reviewed-by: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Jacobo Aragunde Pérez <jaragunde@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#710313}

--- ui/ozone/platform/scenic/ozone_platform_scenic.cc
+++ ui/ozone/platform/scenic/ozone_platform_scenic.cc
@@ -136,10 +136,11 @@ class OzonePlatformScenic
     base::MessageLoopCurrent::Get()->AddDestructionObserver(this);
 
     scenic_gpu_host_ = std::make_unique<ScenicGpuHost>(window_manager_.get());
-    scenic_gpu_host_ptr_ = scenic_gpu_host_->CreateHostProcessSelfBinding();
+    scenic_gpu_host_remote_.Bind(
+        scenic_gpu_host_->CreateHostProcessSelfRemote());
 
     surface_factory_ =
-        std::make_unique<ScenicSurfaceFactory>(scenic_gpu_host_ptr_.get());
+        std::make_unique<ScenicSurfaceFactory>(scenic_gpu_host_remote_.get());
   }
 
   void InitializeGPU(const InitParams& params) override {
@@ -154,9 +155,9 @@ class OzonePlatformScenic
     } else {
       DCHECK(!surface_factory_);
       scenic_gpu_service_ = std::make_unique<ScenicGpuService>(
-          mojo::MakeRequest(&scenic_gpu_host_ptr_));
+          scenic_gpu_host_remote_.BindNewPipeAndPassReceiver());
       surface_factory_ =
-          std::make_unique<ScenicSurfaceFactory>(scenic_gpu_host_ptr_.get());
+          std::make_unique<ScenicSurfaceFactory>(scenic_gpu_host_remote_.get());
     }
   }
 
@@ -176,7 +177,7 @@ class OzonePlatformScenic
   void WillDestroyCurrentMessageLoop() override {
     // We must ensure to destroy any resources which rely on the MessageLoop's
     // async_dispatcher.
-    scenic_gpu_host_ptr_ = nullptr;
+    scenic_gpu_host_remote_.reset();
     surface_factory_ = nullptr;
     scenic_gpu_host_ = nullptr;
     overlay_manager_ = nullptr;
@@ -196,7 +197,7 @@ class OzonePlatformScenic
   std::unique_ptr<ScenicGpuService> scenic_gpu_service_;
   std::unique_ptr<ScenicSurfaceFactory> surface_factory_;
 
-  mojom::ScenicGpuHostPtr scenic_gpu_host_ptr_;
+  mojo::Remote<mojom::ScenicGpuHost> scenic_gpu_host_remote_;
 
   DISALLOW_COPY_AND_ASSIGN(OzonePlatformScenic);
 };
