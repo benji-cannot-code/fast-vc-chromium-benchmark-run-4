5a7de8eaf6334644b37dbb2341baa76dc3b06d5c
Alexander Timin <altimin@chromium.org>
2019-10-29 13:43:00 +0000

    [bfcache] Disable bfcache for Web Authenticator API
    Disable bfcache for web authenticator, since on DidFinishNavigation it would
    cleanup all the authentication data.
    
    This would be a problem if we restore the document from back-forward cache, so
    disabling it.
    
    Bug: 1001087
    Change-Id: I928a89568fc804c6953099560b0d0cc2916d7ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1825220
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710317}

--- content/browser/webauth/webauth_browsertest.cc
+++ content/browser/webauth/webauth_browsertest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/service_manager_connection.h"
+#include "content/public/test/back_forward_cache_util.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
@@ -1453,6 +1454,21 @@ IN_PROC_BROWSER_TEST_F(WebAuthBrowserBleDisabledTest, CheckBleDisabled) {
   fake_hid_discovery->WaitForCallToStart();
   EXPECT_TRUE(fake_hid_discovery->is_start_requested());
   EXPECT_FALSE(fake_ble_discovery->is_start_requested());
+}
+
+class WebAuthLocalClientBackForwardCacheBrowserTest
+    : public WebAuthLocalClientBrowserTest {
+ protected:
+  BackForwardCacheDisabledTester tester_;
+};
+
+IN_PROC_BROWSER_TEST_F(WebAuthLocalClientBackForwardCacheBrowserTest,
+                       WebAuthDisablesBackForwardCache) {
+  // Initialisation of the test should disable bfcache.
+  EXPECT_TRUE(tester_.IsDisabledForFrameWithReason(
+      shell()->web_contents()->GetMainFrame()->GetProcess()->GetID(),
+      shell()->web_contents()->GetMainFrame()->GetRoutingID(),
+      "WebAuthenticationAPI"));
 }
 
 // WebAuthBrowserCtapTest ----------------------------------------------
