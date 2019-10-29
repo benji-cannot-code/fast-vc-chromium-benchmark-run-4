09548ea1f9f46b6aaeb5da0fcc85b8c2ae1bf617
Chris Lu <thegreenfrog@chromium.org>
2019-10-29 05:26:44 +0000

    Revert "[iOS] Convert NTPHome test to EG2"
    This reverts commit 5cd504f9e27eff88a414723068d08d475dfb5aff.
    
    Reason for revert: Failed NTPHomeTestCase tests. crbug.com/1019070
    
    Bug: 1019070
    Original change's description:
    > [iOS] Convert NTPHome test to EG2
    >
    > OmniboxWidth and OmniboxWidthBetween are moved to the egtest.mm file
    > directly to avoid having a more complicated interface.
    > Suggestions() uses are replaced by -addNumberOfSuggestions:
    > additionalSuggestionsURL:.
    >
    > Bug: 987646
    > Change-Id: I58535b52919fbd1d907c8a72c611e2fbb7f63f67
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871888
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#709521}
    
    TBR=eugenebut@chromium.org,stkhapugin@chromium.org,gambard@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 987646
    Change-Id: If3418f1cc097867fa1ef011d5036f171c3cc3fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1885971
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710215}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_app_interface.h
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_app_interface.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_CONTENT_SUGGESTIONS_APP_INTERFACE_H_
 #define IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_CONTENT_SUGGESTIONS_APP_INTERFACE_H_
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 // App interface for the Content Suggestions.
 @interface ContentSuggestionsAppInterface : NSObject
@@ -32,36 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Add one particular suggestion, following the convention explained above, with
 // |suggestionNumber|.
 + (void)addSuggestionNumber:(NSInteger)suggestionNumber;
-
-// Returns the short name of the default search engine.
-+ (NSString*)defaultSearchEngine;
-
-// Resets the default search engine to |defaultSearchEngine|.
-// |defaultSearchEngine| should be its short name.
-+ (void)resetSearchEngineTo:(NSString*)defaultSearchEngine;
-
-// Sets the what's new promo to "Move to Dock".
-+ (void)setWhatsNewPromoToMoveToDock;
-
-// Resets the what's new promo.
-+ (void)resetWhatsNewPromo;
-
-// Returns the width the search field is supposed to have when the collection
-// has |collectionWidth|.
-+ (CGFloat)searchFieldWidthForCollectionWidth:(CGFloat)collectionWidth;
-
-// Returns the collection view.
-+ (UICollectionView*)collectionView;
-
-// Returns the fake omnibox.
-+ (UIView*)fakeOmnibox;
-
-// Swizzles the callback for the search button tap action so it is recorded.
-+ (void)swizzleSearchButtonLogging;
-
-// Resets the swizzle for the search button tap, and return the whether the
-// swizzled method was called.
-+ (BOOL)resetSearchButtonLogging;
 
 @end
 
