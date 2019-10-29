20d9b027a4021f5419bbcb5c49099f448fb83eb7
dpapad <dpapad@chromium.org>
2019-10-29 18:58:09 +0000

    Port chrome://discards to Polymer3.
     - Renamed graph_tab.js to graph_tab_template.js, otherwise GN
       complains that two different rules (polymer_modulizer, generate_graph_tab)
       declare the same output file.
     - generate_graph_tab now depends on polymer_modulizer, and processes
       the JS file that holds the HTML contents (in Polymer3 all HTML is hosted in JS
       files)
     - Changed substitute() to safe_substitute() in generate_graph_tab, otherwise Python
       trips over occurrences of "$" in the same file which don't correspond to
       template strings.
     - Extracted tests to a separate file so that they can be loaded in a JS module
       context.
     - Added missing js_library() dependency for ui/webui/resources/cr:icon.m, to fix a
       type checking failure.
    
    Bug: 1017874
    Test: Page still works, no more HTML imports warnings in DevTools
    Change-Id: I77b9d7e8cb75cf8f9edc0ff91ad9eeb31118e704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879782
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710406}

--- chrome/browser/resources/discards/sorted_table_behavior.js
+++ chrome/browser/resources/discards/sorted_table_behavior.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  * @polymerBehavior
  */
-const SortedTableBehavior = {
+export const SortedTableBehavior = {
   properties: {
     /**
      * The current sort key, used for computing the appropriate sort function.
