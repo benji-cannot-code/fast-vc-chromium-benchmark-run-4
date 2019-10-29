4b9e4dc1b9fb162d7c098b387de1f1ffb30be292
Gang Wu <gangwu@chromium.org>
2019-10-29 17:49:44 +0000

    [duet] Create a new swipe handler for bottom toolbar
    Bug: 914501
    Change-Id: Iab146b0f40918ca7f91c9927c9922695050c53f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1859678
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710392}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
@@ -83,7 +83,7 @@ public class LayoutManagerChrome extends LayoutManager implements OverviewModeCo
         mOverviewModeObservers = new ObserverList<OverviewModeObserver>();
 
         // Build Event Filter Handlers
-        mToolbarSwipeHandler = new ToolbarSwipeHandler();
+        mToolbarSwipeHandler = createToolbarSwipeHandler(/* supportSwipeDown = */ true);
 
         // Build Layouts
         mOverviewListLayout = new OverviewListLayout(context, this, renderHost);
@@ -126,6 +126,11 @@ public class LayoutManagerChrome extends LayoutManager implements OverviewModeCo
     @Override
     public EdgeSwipeHandler getToolbarSwipeHandler() {
         return mToolbarSwipeHandler;
+    }
+
+    @Override
+    public EdgeSwipeHandler createToolbarSwipeHandler(boolean supportSwipeDown) {
+        return new ToolbarSwipeHandler(supportSwipeDown);
     }
 
     @Override
@@ -425,6 +430,12 @@ public class LayoutManagerChrome extends LayoutManager implements OverviewModeCo
          */
         private static final float SWIPE_RANGE_DEG = 25;
 
+        private final boolean mSupportSwipeDown;
+
+        public ToolbarSwipeHandler(boolean supportSwipeDown) {
+            mSupportSwipeDown = supportSwipeDown;
+        }
+
         @Override
         public void swipeStarted(@ScrollDirection int direction, float x, float y) {
             mScrollDirection = ScrollDirection.UNKNOWN;
@@ -443,12 +454,12 @@ public class LayoutManagerChrome extends LayoutManager implements OverviewModeCo
             mScrollDirection = computeScrollDirection(dx, dy);
             if (mScrollDirection == ScrollDirection.UNKNOWN) return;
 
-            if (mOverviewLayout != null && mScrollDirection == ScrollDirection.DOWN) {
+            if (mSupportSwipeDown && mOverviewLayout != null
+                    && mScrollDirection == ScrollDirection.DOWN) {
                 RecordUserAction.record("MobileToolbarSwipeOpenStackView");
                 showOverview(true);
-            } else if (mToolbarSwipeLayout != null
-                    && (mScrollDirection == ScrollDirection.LEFT
-                            || mScrollDirection == ScrollDirection.RIGHT)) {
+            } else if (mScrollDirection == ScrollDirection.LEFT
+                    || mScrollDirection == ScrollDirection.RIGHT) {
                 startShowing(mToolbarSwipeLayout, true);
             }
 
