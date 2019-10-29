338d72ac2441c05fbfefb4ca74e0063aee24d40c
Victor Fei <vicfei@microsoft.com>
2019-10-29 22:11:20 +0000

    Reland "Exposing button's children internally and to AT"
    This is a reland of 81487c55951e3dc058ec602acd151cf6425d9dae
    
    The original change was reverted because it failed win7 bots for the
    following tests:
    DumpAccessibilityTreeTest.AccessibilityButtonNameCalc/uia
    DumpAccessibilityTreeTest.AccessibilityInputDateWithPopupOpen/blink
    DumpAccessibilityTreeTest.AccessibilityInputDateWithPopupOpen/win
    
    This reland fixes these test failures on win7.
    
    Original change's description:
    > Exposing button's children internally and to AT
    >
    > The motivation behind this change is that we want text pattern APIs to
    > access the text content of both native buttons and ARIA role=button.
    > In order to achieve the above, we need to be able to serialize button
    > element's children to AXTree. This change enables all children of button
    > to be exposed in the AXTree.
    >
    > Additionally, if the button's children are text only and also exposed
    > in the Browser Accessibility tree, screen readers may double speak the
    > button's name. To prevent this, if button's only child is a static text
    > we mark the button Browser A11y node as leaf.
    >
    > On Android, we treat all its Browser A11y button nodes as leaves and
    > expose no child.
    >
    > Summary:
    > --------------------------------------------------------------------
    > BEFORE: Exposed in AXTree & Browser A11y Tree (BATree):
    >
    >                        native button    |   role=button
    > single text child: AXTree[N],BATree[N]  | AXTree[N],BATree[N]
    >                       BATreeAndroid[N]  |    BATreeAndroid[N]
    >
    > complex children:  AXTree[N],BATree[N]  | AXTree[Y],BATree[Y]
    >                       BATreeAndroid[N]  |    BATreeAndroid[Y]
    > --------------------------------------------------------------------
    > AFTER: Exposed in AXTree & Browser A11y Tree (BATree):
    >
    >                        native button    |   role=button
    > single text child: AXTree[Y],BATree[N], | AXTree[Y],BATree[N],
    >                       BATreeAndroid[N]  |    BATreeAndroid[N]
    >
    > multiple children: AXTree[Y],BATree[Y]  | AXTree[Y],BATree[Y]
    >                       BATreeAndroid[N]  |    BATreeAndroid[N]
    > --------------------------------------------------------------------
    >
    > Note:
    > According to the ARIA spec (below), the children of role=button are
    > presentational, which means they should not be exposed to ATs
    > (a.k.a. should all be treated as leaf nodes).
    > However, we need the button contents to be exposed sometimes because
    > they may contain many rich, interactive descendants such as buttons
    > inside a calendar popup and screen readers need to interact with these
    > contents (See https://crbug.com/689204).
    >
    > Additionally, Firefox also expose the children of both native button and
    > role=button.
    > Due to the above, we decided to departure from the ARIA spec and not
    > enforce the leafiness of buttons.
    >
    > ARIA && core-aam specs:
    > https://w3c.github.io/aria/#button
    > https://www.w3.org/TR/core-aam-1.1/#exclude_elements
    >
    > ~~~~~~
    > Note:
    > The actual functional changes are in the two files below, the rest are
    > tests:
    > content/browser/accessibility/browser_accessibility.cc
    > content/browser/accessibility/browser_accessibility_android.cc
    > third_party/blink/renderer/modules/accessibility/ax_node_object.cc
    > ~~~~~~
    >
    > Bug:1013609
    >
    > Change-Id: Ia7c4bac9940b34dd3a3122938057cb48e847b76a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1845810
    > Commit-Queue: Victor Fei <vicfei@microsoft.com>
    > Reviewed-by: Bo <boliu@chromium.org>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#709728}
    
    Bug: 1013609
    Change-Id: I088fac4c42a9c95ddee16e40f28f0154da1a9f14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1883619
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Victor Fei <vicfei@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#710463}

--- third_party/blink/web_tests/inspector-protocol/accessibility/accessibility-ignoredNodes.js
+++ third_party/blink/web_tests/inspector-protocol/accessibility/accessibility-ignoredNodes.js
@@ -9,9 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             <circle xmlns:svg='http://www.w3.org/2000/svg' cx='150px' cy='100px' r='50px' fill='#ff0000' stroke='#000000' stroke-width='5px'/>
         </svg>
     </div>
-    <button>
-      <span data-dump>Buttons are leaf nodes</span>
-    </button>
     <div data-dump aria-hidden='true'>
         <div data-dump>Descendant of aria-hidden node</div>
     </div>
