705e7deb82e1be31827982aa6d6bfd0ce0522a79
Jiawei Shao <jiawei.shao@intel.com>
2019-10-30 04:56:13 +0000

    WebGPU: Add DawnReturnDataType before the return data of WebGPU decoder
    This patch adds an enum DawnReturnDataType before the return data from
    the server side of WebGPU command buffer to the client side. This enum
    will be used as a flag to specify different type of return data received
    in the client side of WebGPU command buffer. Currently we only support
    kDawnCommands, and we will support more kinds of the return values in
    the following patches.
    
    BUG=chromium:996713
    
    Change-Id: I99f65a0b91808cea52728ea10b408c4ee9649d85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1877907
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710662}

--- gpu/command_buffer/common/webgpu_cmd_format.h
+++ gpu/command_buffer/common/webgpu_cmd_format.h
@@ -15,12 +15,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "gpu/command_buffer/common/common_cmd_format.h"
 #include "gpu/command_buffer/common/gl2_types.h"
+#include "gpu/command_buffer/common/webgpu_cmd_enums.h"
 #include "gpu/command_buffer/common/webgpu_cmd_ids.h"
 #include "ui/gfx/buffer_types.h"
 
 namespace gpu {
 namespace webgpu {
 namespace cmds {
+
+#define GPU_DAWN_RETURN_DATA_ALIGNMENT (8)
+struct alignas(GPU_DAWN_RETURN_DATA_ALIGNMENT) DawnReturnDataHeader {
+  DawnReturnDataType return_data_type;
+};
+
+static_assert(
+    sizeof(DawnReturnDataHeader) % GPU_DAWN_RETURN_DATA_ALIGNMENT == 0,
+    "DawnReturnDataHeader must align to GPU_DAWN_RETURN_DATA_ALIGNMENT");
 
 // Command buffer is GPU_COMMAND_BUFFER_ENTRY_ALIGNMENT byte aligned.
 #pragma pack(push, 4)
