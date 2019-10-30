9d32b28581dafea853581d5cb51a1123c3cdf075
Hiroki Sato <hirokisato@google.com>
2019-10-30 03:49:35 +0000

    Add buttons to set all event stream logging on/off in ChromeVox Developer Option
    This CL adds buttons to set all event stream logging to on or off in
    ChromeVox Developer option.
    
    Bug: None
    Test: manual
    Change-Id: If28b6c03427bf2c9a1109346bc79aa601eb2ad0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880212
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710649}

--- chrome/browser/resources/chromeos/chromevox/chromevox/background/options.js
+++ chrome/browser/resources/chromeos/chromevox/chromevox/background/options.js
@@ -12,7 +12,6 @@ goog.provide('cvox.OptionsPage');
 
 goog.require('BluetoothBrailleDisplayUI');
 goog.require('ConsoleTts');
-goog.require('EventStreamLogger');
 goog.require('Msgs');
 goog.require('PanelCommand');
 goog.require('cvox.BrailleTable');
@@ -20,7 +19,6 @@ goog.require('cvox.BrailleTranslatorManager');
 goog.require('cvox.ChromeVox');
 goog.require('cvox.ChromeVoxKbHandler');
 goog.require('cvox.ChromeVoxPrefs');
-goog.require('cvox.CommandStore');
 goog.require('cvox.ExtensionBridge');
 
 /**
@@ -106,17 +104,22 @@ cvox.OptionsPage.init = function() {
         }
       });
 
-  var registerEventStreamFiltersListener = function() {
-    $('toggleEventStreamFilters').addEventListener('click', function(evt) {
-      if ($('eventStreamFilters').hidden) {
-        $('eventStreamFilters').hidden = false;
-        $('toggleEventStreamFilters').textContent = hideEventStreamFilters;
-      } else {
-        $('eventStreamFilters').hidden = true;
-        $('toggleEventStreamFilters').textContent = showEventStreamFilters;
-      }
-    });
-  };
+  $('toggleEventStreamFilters').addEventListener('click', function(evt) {
+    if ($('eventStreamFilters').hidden) {
+      $('eventStreamFilters').hidden = false;
+      $('toggleEventStreamFilters').textContent = hideEventStreamFilters;
+    } else {
+      $('eventStreamFilters').hidden = true;
+      $('toggleEventStreamFilters').textContent = showEventStreamFilters;
+    }
+  });
+
+  $('enableAllEventStreamFilters').addEventListener('click', () => {
+    cvox.OptionsPage.setAllEventStreamLoggingFilters(true);
+  });
+  $('disableAllEventStreamFilters').addEventListener('click', () => {
+    cvox.OptionsPage.setAllEventStreamLoggingFilters(false);
+  });
 
   $('chromeVoxDeveloperOptions').addEventListener('expanded-changed', () => {
     const hidden = !$('chromeVoxDeveloperOptions').expanded;
@@ -132,14 +135,6 @@ cvox.OptionsPage.init = function() {
     chrome.tabs.create(logPage);
   });
 
-  // Hide developer options by default.
-  $('developerSpeechLogging').hidden = true;
-  $('developerEarconLogging').hidden = true;
-  $('developerBrailleLogging').hidden = true;
-  $('developerEventStream').hidden = true;
-  $('showDeveloperLog').hidden = true;
-
-  registerEventStreamFiltersListener();
   Msgs.addTranslatedMessagesToDom(document);
   cvox.OptionsPage.hidePlatformSpecifics();
 
@@ -405,6 +400,30 @@ cvox.OptionsPage.disableEventStreamFilterCheckBoxes = function(disable) {
 };
 
 /**
+ * Set all event stream logging filter to on or off.
+ * @param {boolean} enabled
+ */
+cvox.OptionsPage.setAllEventStreamLoggingFilters = function(enabled) {
+  for (let checkbox of document.querySelectorAll(
+           '.option-eventstream > input')) {
+    if (checkbox.checked != enabled) {
+      cvox.OptionsPage.setEventStreamFilter(checkbox.name, enabled);
+    }
+  }
+};
+
+/**
+ * Set the specified event logging filter to on or off.
+ * @param {name} string
+ * @param {enabled} boolean
+ */
+cvox.OptionsPage.setEventStreamFilter = function(name, enabled) {
+  cvox.OptionsPage.prefs.setPref(name, enabled);
+  chrome.extension.getBackgroundPage()
+      .EventStreamLogger.instance.notifyEventStreamFilterChanged(name, enabled);
+};
+
+/**
  * Event listener, called when an event occurs in the page that might
  * affect one of the preference controls.
  * @param {Event} event The event.
@@ -420,10 +439,7 @@ cvox.OptionsPage.eventListener = function(event) {
       if (target.name == 'enableEventStreamLogging')
         cvox.OptionsPage.disableEventStreamFilterCheckBoxes(!target.checked);
     } else if (target.className.indexOf('eventstream') != -1) {
-      cvox.OptionsPage.prefs.setPref(target.name, target.checked);
-      chrome.extension.getBackgroundPage()
-          .EventStreamLogger.instance.notifyEventStreamFilterChanged(
-              target.name, target.checked);
+      cvox.OptionsPage.setEventStreamFilter(target.name, target.checked);
     } else if (target.classList.contains('pref')) {
       if (target.tagName == 'INPUT' && target.type == 'checkbox') {
         cvox.OptionsPage.prefs.setPref(target.name, target.checked);
