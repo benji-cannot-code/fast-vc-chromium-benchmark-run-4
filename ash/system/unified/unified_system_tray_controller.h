3c7be22a2f9dff8817d88124a005502a0e2e9d0b
Mitsuru Oshima <oshima@chromium.org>
2019-10-30 01:23:49 +0000

    Use AnimationDelegateViews for UnifiedSystemTray animation
    Bug: 1019485
    Change-Id: I016c2529ad046180be57e8904a16fff9ff0b6c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1887874
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710581}

--- ash/system/unified/unified_system_tray_controller.h
+++ ash/system/unified/unified_system_tray_controller.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/audio/unified_volume_slider_controller.h"
 #include "ash/system/unified/unified_system_tray_model.h"
 #include "base/macros.h"
-#include "ui/gfx/animation/animation_delegate.h"
 #include "ui/gfx/geometry/point.h"
+#include "ui/views/animation/animation_delegate_views.h"
 
 namespace gfx {
 class SlideAnimation;
@@ -33,11 +33,12 @@ class UnifiedSystemTrayView;
 
 // Controller class of UnifiedSystemTrayView. Handles events of the view.
 class ASH_EXPORT UnifiedSystemTrayController
-    : public gfx::AnimationDelegate,
+    : public views::AnimationDelegateViews,
       public UnifiedVolumeSliderController::Delegate {
  public:
   UnifiedSystemTrayController(UnifiedSystemTrayModel* model,
-                              UnifiedSystemTrayBubble* bubble = nullptr);
+                              UnifiedSystemTrayBubble* bubble = nullptr,
+                              views::View* owner_view = nullptr);
   ~UnifiedSystemTrayController() override;
 
   // Create the view. The created view is unowned.
@@ -117,7 +118,7 @@ class ASH_EXPORT UnifiedSystemTrayController
   // notifications area.
   void ResetToCollapsedIfRequired();
 
-  // gfx::AnimationDelegate:
+  // views::AnimationDelegateViews:
   void AnimationEnded(const gfx::Animation* animation) override;
   void AnimationProgressed(const gfx::Animation* animation) override;
   void AnimationCanceled(const gfx::Animation* animation) override;
