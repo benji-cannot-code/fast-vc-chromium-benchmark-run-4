eb7e3d44d670ebf73645281c470ae056070eccfd
Jasper Chapman-Black <jaspercb@chromium.org>
2019-10-30 20:05:58 +0000

    SuperSize: Caspian: Add name filtering
    This mirrors the JS SuperSize implementation in supporting two regex
    parameters, for whitelisting and blacklisting.
    
    std::regex is two orders of magnitude too slow, so I've brought in RE2
    from /third_party/.
    
    The front-end has code to validate input regexes and display a user-facing
    error in case of invalid input - this justified squashing any regex-parsing
    errors encountered on the WebAssembly side.
    
    Bug: 1011921
    Change-Id: I36b73815d4fd72142580b78cf5ff08fc595990bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1891113
    Commit-Queue: Jasper Chapman-Black <jaspercb@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710930}

--- tools/binary_size/libsupersize/caspian/tree_builder.h
+++ tools/binary_size/libsupersize/caspian/tree_builder.h
@@ -18,14 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace caspian {
 class TreeBuilder {
  public:
-  TreeBuilder(SizeInfo* size_info, bool group_by_component);
+  TreeBuilder(SizeInfo* size_info,
+              bool group_by_component,
+              std::vector<std::function<bool(const Symbol&)>> filters);
   ~TreeBuilder();
   void Build();
   Json::Value Open(const char* path);
 
  private:
-  std::string_view GetPath(const Symbol* symbol);
-
   void AddFileEntry(const std::string_view source_path,
                     const std::vector<const Symbol*>& symbols);
 
@@ -34,6 +34,8 @@ class TreeBuilder {
   void AttachToParent(TreeNode* child, TreeNode* parent);
 
   ContainerType ContainerTypeFromChild(std::string_view child_id_path) const;
+
+  bool ShouldIncludeSymbol(const Symbol& symbol) const;
 
   // Merges dex method symbols into containers based on the class of the dex
   // method.
@@ -56,6 +58,7 @@ class TreeBuilder {
   // Note that we split paths on '/' no matter the value of separator, since
   // when grouping by component, paths look like Component>path/to/file.
   char sep_;
+  std::vector<std::function<bool(const Symbol&)>> filters_;
 };  // TreeBuilder
 }  // namespace caspian
 #endif  // TOOLS_BINARY_SIZE_LIBSUPERSIZE_CASPIAN_TREE_BUILDER_H_
