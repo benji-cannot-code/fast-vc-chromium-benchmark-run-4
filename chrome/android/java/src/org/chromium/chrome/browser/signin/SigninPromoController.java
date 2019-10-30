dba507e55fc4d1f0f886feed30e04b8095ab9959
Alice Wang <aliceywang@chromium.org>
2019-10-30 14:53:24 +0000

    [Android][Signin] Fix bookmark signin promo dismiss tests
    This CL fixes the bookmark signin promo tests when the promo is
    dismissed in different scenarios.
    
    Bug: 789531
    Change-Id: I852e60cb3e56f9db737cb0e03073d36373908f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1883628
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Auto-Submit: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710784}

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoController.java
@@ -47,7 +47,8 @@ public class SigninPromoController {
     private static final String SIGNIN_PROMO_IMPRESSIONS_COUNT_SETTINGS =
             "signin_promo_impressions_count_settings";
 
-    private static final int MAX_IMPRESSIONS_BOOKMARKS = 20;
+    @VisibleForTesting
+    public static final int MAX_IMPRESSIONS_BOOKMARKS = 20;
     private static final int MAX_IMPRESSIONS_SETTINGS = 20;
 
     private @Nullable DisplayableProfileData mProfileData;
@@ -353,7 +354,14 @@ public class SigninPromoController {
     }
 
     @VisibleForTesting
-    public static int getMaxImpressionsBookmarksForTests() {
-        return MAX_IMPRESSIONS_BOOKMARKS;
+    public static void setSigninPromoImpressionsCountBookmarksForTest(int count) {
+        SharedPreferences preferences = ContextUtils.getAppSharedPreferences();
+        preferences.edit().putInt(SIGNIN_PROMO_IMPRESSIONS_COUNT_BOOKMARKS, count).apply();
+    }
+
+    @VisibleForTesting
+    public static int getSigninPromoImpressionsCountBookmarksForTest() {
+        return ContextUtils.getAppSharedPreferences().getInt(
+                SIGNIN_PROMO_IMPRESSIONS_COUNT_BOOKMARKS, 0);
     }
 }
