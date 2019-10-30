afe05cce42999e3617fbe303c89f3a1efea9f6c6
Michael Spang <spang@chromium.org>
2019-10-30 20:35:30 +0000

    X11Window: Avoid size hack for tests
    The default configuration of TestScreen is to create one display and one
    WindowTreeHost with the same size.
    
    X11 is now incompatible with this requirement as of b14e13fd9b17 ("Reland
    "X11 and Ozone: DWTHPlatform handles Show/Hide/Max/Restore/Full"").
    
    Several aura tests are only passing currently due to another bug in
    DisplayList::UpdateDisplay, which prevents X11Window from
    seeing the current display size and enforcing the constraint.
    
    Other tests only work because TestScreen resizes the display to match
    the WindowTreeHost /after/ X11Window deletes the last row and column.
    i.e., X11Window tries to adjust the size to not match the display,
    and then TestScreen re-adjusts the display to match the window. This
    behavior is not convergent and causes small changes in initialization
    order to affect the dimensions of test windows.
    
    This hack should likely be removed altogether and the bug it's addressing
    fixed another way, but for now disable it in the test harness because it
    is blocking other changes such as a fix for the UpdateDisplay bug noted
    above.
    
    Bug: 1019015
    
    Change-Id: I79e830b9c35df3df29dc8af50ac424076682d180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1889294
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Michael Spang <spang@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710955}

--- ui/aura/window_tree_host_unittest.cc
+++ ui/aura/window_tree_host_unittest.cc
@@ -21,50 +21,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aura {
 
-namespace {
-
-gfx::Rect ScaleRect(const gfx::Rect& rect_in_pixels, float scale) {
-  gfx::RectF rect_in_dip(rect_in_pixels);
-  gfx::Transform transform;
-  transform.Scale(scale, scale);
-  transform.TransformRectReverse(&rect_in_dip);
-  return gfx::ToEnclosingRect(rect_in_dip);
-}
-
-gfx::Rect TransformRect(const gfx::Rect& rect_in_pixels,
-                        const gfx::Transform& transform,
-                        float device_scale_factor) {
-  gfx::RectF new_bounds =
-      gfx::ScaleRect(gfx::RectF(rect_in_pixels), 1.0f / device_scale_factor);
-  transform.TransformRect(&new_bounds);
-  return gfx::ToEnclosingRect(new_bounds);
-}
-
-}  // namespace
-
 using WindowTreeHostTest = test::AuraTestBase;
 
 TEST_F(WindowTreeHostTest, DPIWindowSize) {
-  gfx::Rect starting_bounds = host()->GetBoundsInPixels();
+  gfx::Rect starting_bounds(0, 0, 800, 600);
   EXPECT_EQ(starting_bounds.size(), host()->compositor()->size());
+  EXPECT_EQ(starting_bounds, host()->GetBoundsInPixels());
   EXPECT_EQ(starting_bounds, root_window()->bounds());
 
-  float device_scale_factor = 1.5;
-  test_screen()->SetDeviceScaleFactor(device_scale_factor);
+  test_screen()->SetDeviceScaleFactor(1.5f);
   EXPECT_EQ(starting_bounds, host()->GetBoundsInPixels());
   // Size should be rounded up after scaling.
-  gfx::Rect rect_in_dip = ScaleRect(starting_bounds, device_scale_factor);
-  EXPECT_EQ(rect_in_dip, root_window()->bounds());
+  EXPECT_EQ(gfx::Rect(0, 0, 534, 400), root_window()->bounds());
 
   gfx::Transform transform;
   transform.Translate(0, 1.1f);
   host()->SetRootTransform(transform);
-  gfx::Rect transformed_rect =
-      TransformRect(starting_bounds, transform, device_scale_factor);
-  EXPECT_EQ(transformed_rect, root_window()->bounds());
+  EXPECT_EQ(gfx::Rect(0, 1, 534, 401), root_window()->bounds());
 
   EXPECT_EQ(starting_bounds, host()->GetBoundsInPixels());
-  EXPECT_EQ(transformed_rect, root_window()->bounds());
+  EXPECT_EQ(gfx::Rect(0, 1, 534, 401), root_window()->bounds());
 }
 
 #if defined(OS_CHROMEOS)
