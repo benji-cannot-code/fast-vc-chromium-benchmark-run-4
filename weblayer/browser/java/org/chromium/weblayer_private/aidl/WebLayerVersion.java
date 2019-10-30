154abc5d65d83734b3ff8b584547307f93c68f97
Tobias Sargeant <tobiasjs@google.com>
2019-10-30 20:58:55 +0000

    [weblayer] Add presubmit for AIDL changes
    If an AIDL file changes (comment and whitespace changes are ignored) then
    assert that the API version number is incremented by 1.
    
    Change-Id: Id7f3a17f9ab2244946d0cf12d7f0af48187172ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879462
    Commit-Queue: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710964}

--- weblayer/browser/java/org/chromium/weblayer_private/aidl/WebLayerVersion.java
+++ weblayer/browser/java/org/chromium/weblayer_private/aidl/WebLayerVersion.java
@@ -5,5 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.weblayer_private.aidl;
 
-/** Holds the current version number of WebLayer. */
+/**
+ * Holds the current version number of WebLayer.
+ *
+ * Whenever any AIDL file is changed, sVersionNumber must be incremented.
+ * */
 public final class WebLayerVersion { public static int sVersionNumber = 3; }
