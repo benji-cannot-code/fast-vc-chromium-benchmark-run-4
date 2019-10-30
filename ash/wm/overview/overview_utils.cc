4461125c058f18e799b3f8109d5e0ee5ec9b5da8
Toni Barzic <tbarzic@chromium.org>
2019-10-30 20:21:34 +0000

    Implement transitions to home for homerview feature
    Updates how device transitions to home when home button is tapped and
    when user clicks in empty space in overview grid when kHomerviewGesture
    feature is enabled.
    
    When overview is active, it should use kFadeOutExit transition (instead
    of slide out) - this also fixes a bug where transform was not cleared on
    item windows when bounds were updated without animations (which was
    causing item window misplacement when the grid was updated while
    minimizing active window before exiting overview).
    FadeOutWindowsAndMaybeSlideOnExit is updated to take "slide" as an
    argument instead of deducing it depending on the animation type, as
    OVERVIEW_ANIMATION_EXIT_TO_HOME_LAUNCHER should slide if and only if
    using slide out overview exit transition (the animation can also be used
    by fade out exit).
    
    When clicking home with a window active, the top window is minimized
    using transform to home screen (the same used during drag from shelf),
    while the rest of the windows are minimized with no animations. To make
    this easier, WindowTransformToHomeScreenAnimation is moved to a separate
    file, and is updated to better handle windows whose origin is not in the
    screen origin (which is useful for transforming top windows in split
    view).
    
    Is split view, the top windows are transformed to home screen as the
    split view ends. If overview session is active together with split view,
    the overview session is ended immediately to avoid showing overview just
    after ending split view (during duration of the exit transition).
    
    BUG=1005366
    
    Change-Id: Ic23893866700f5e5580f4445915d815f188e8c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1885972
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710953}

--- ash/wm/overview/overview_utils.cc
+++ ash/wm/overview/overview_utils.cc
@@ -90,7 +90,8 @@ void FadeInWidgetAndMaybeSlideOnEnter(views::Widget* widget,
 }
 
 void FadeOutWidgetAndMaybeSlideOnExit(std::unique_ptr<views::Widget> widget,
-                                      OverviewAnimationType animation_type) {
+                                      OverviewAnimationType animation_type,
+                                      bool slide) {
   // The overview controller may be nullptr on shutdown.
   OverviewController* controller = Shell::Get()->overview_controller();
   if (!controller) {
@@ -113,8 +114,7 @@ void FadeOutWidgetAndMaybeSlideOnExit(std::unique_ptr<views::Widget> widget,
   controller->AddExitAnimationObserver(std::move(observer));
   widget_ptr->SetOpacity(0.f);
 
-  // Slide |widget| towards to top of screen if exit overview to home launcher.
-  if (animation_type == OVERVIEW_ANIMATION_EXIT_TO_HOME_LAUNCHER) {
+  if (slide) {
     gfx::Transform new_transform = widget_ptr->GetNativeWindow()->transform();
     new_transform.ConcatTransform(GetShiftTransform());
     widget_ptr->GetNativeWindow()->SetTransform(new_transform);
