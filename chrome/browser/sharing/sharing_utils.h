17c88ad4a61d1dd29ef1b1a0f5d5bf2594186d1b
Himanshu Jaju <himanshujaju@chromium.org>
2019-10-30 11:59:39 +0000

    Extract SharingUtils from SharingService with device renaming functions
    To move the callback logic to SharingFCMSender, we would also need to
    move the device naming part to SharingUtils since we use GetDeviceName
    while sending the message using FCM.
    
    Bug: 1018274
    Change-Id: I5f67d439a22015105b6202e9bba9f204034a492d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1887616
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710744}

---
+++ chrome/browser/sharing/sharing_utils.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SHARING_SHARING_UTILS_H_
+#define CHROME_BROWSER_SHARING_SHARING_UTILS_H_
+
+#include <string>
+
+namespace syncer {
+class DeviceInfo;
+}  // namespace syncer
+
+struct SharingDeviceNames {
+  std::string full_name;
+  std::string short_name;
+};
+
+// Returns full and short names for |device|.
+SharingDeviceNames GetSharingDeviceNames(const syncer::DeviceInfo* device);
+
+#endif  // CHROME_BROWSER_SHARING_SHARING_UTILS_H_
