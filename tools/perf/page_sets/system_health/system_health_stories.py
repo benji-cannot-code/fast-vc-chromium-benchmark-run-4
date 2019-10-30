77f76bd7eaf33af41585cea543ccd7370b18d1b2
Caleb Rouleau <crouleau@chromium.org>
2019-10-30 22:19:36 +0000

    [Telemetry] Provide abridged story sets for rendering and system health.
    Note that this will have no impact on what is currently running
    on the waterfall because all benchmarks are labeled as
    abridged: False right now.
    
    This change is now possible because:
    
    1. Pinpoint was taught to use the --run-full-story-set flag by
    https://chromium-review.googlesource.com/c/catapult/+/1874451
    
    2. benchmark_smoke_unittest.py was fixed:
    https://chromium-review.googlesource.com/c/chromium/src/+/1874821
    
    Note that system_health_smoke_test.py is fixed as part of this
    change.
    I tested this fix by running
    /run_tests benchmarks.system_health_smoke_test.SystemHealthBenchmarkSmokeTest.system_health.memory_desktop/load:game
    and seeing that the stories, a mix of health_check stories
    and stories without the health_check tag were all run.
    
    Bug: 965158
    Change-Id: I42ca28543155536da62d9b02d9bf31d8eb311ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1687783
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Juan Antonio Navarro Pérez <perezju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711017}

--- tools/perf/page_sets/system_health/system_health_stories.py
+++ tools/perf/page_sets/system_health/system_health_stories.py
@@ -7,6 +7,7 @@ import os
 
 from page_sets.system_health import chrome_stories
 from page_sets.system_health import platforms
+from page_sets.system_health import story_tags
 from page_sets.system_health import system_health_story
 
 from telemetry import story
@@ -32,6 +33,9 @@ class SystemHealthStorySet(story.StorySet):
           case and not story_class.NAME.startswith(case + ':')):
         continue
       self.AddStory(story_class(self, take_memory_measurement))
+
+  def GetAbridgedStorySetTagFilter(self):
+    return story_tags.HEALTH_CHECK.name
 
 
 class SystemHealthBlankStorySet(story.StorySet):
