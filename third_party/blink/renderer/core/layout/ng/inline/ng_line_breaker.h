083d3aaed51e9e82736a24e3b6b05b1662511a55
Koji Ishii <kojii@chromium.org>
2019-10-30 00:24:55 +0000

    [LayoutNG] Fix not to break after leading out-of-flow objects
    r694888 (crrev.com/c/1792467) gave a break opportunity after
    OOF objects.
    
    For leading OOF objects, there should not be a break
    opportunity, but the patch gave to them too. This patch fixes
    not to add a break opportunity if there were no preceding
    in-flow objects.
    
    Bug: 1018748
    Change-Id: I89a8614cdfd85bed1bb8c2f3b6db892697f7add7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1886130
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710527}

--- third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_line_breaker.h
@@ -172,6 +172,7 @@ class CORE_EXPORT NGLineBreaker {
       const NGInlineItemResult& item_result) const;
   void HandleFloat(const NGInlineItem&,
                    NGLineInfo*);
+  void HandleOutOfFlowPositioned(const NGInlineItem&, NGLineInfo*);
 
   void HandleOpenTag(const NGInlineItem&, NGLineInfo*);
   void HandleCloseTag(const NGInlineItem&, NGLineInfo*);
