bc0a58b5345ffe0c77efdfb4ed3f3b8502d3a2f9
Michael Lippautz <mlippautz@chromium.org>
2019-10-30 07:40:21 +0000

    heap: Move virtual methods to cc file
    - Virtual Visit methods are always called through base class Visitor
      using Trace methods. There's no need to keep them in the header file.
    - Drive-by: Move inlined definitions out-of-line to allow skimming the
      class header more easily.
    - Only mark definition as inline as this is an implementation detail.
    
    Change-Id: I44993d515eb18498105b0e592e282fcd536c82ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1884731
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#710688}

--- third_party/blink/renderer/platform/heap/unified_heap_marking_visitor.cc
+++ third_party/blink/renderer/platform/heap/unified_heap_marking_visitor.cc
@@ -53,6 +53,7 @@ UnifiedHeapMarkingVisitor::UnifiedHeapMarkingVisitor(ThreadState* thread_state,
                                     isolate,
                                     WorklistTaskId::MutatorThread) {}
 
+// static
 void UnifiedHeapMarkingVisitor::WriteBarrier(
     const TraceWrapperV8Reference<v8::Value>& object) {
   if (object.IsEmpty() || !ThreadState::IsAnyIncrementalMarking())
@@ -65,6 +66,7 @@ void UnifiedHeapMarkingVisitor::WriteBarrier(
   thread_state->CurrentVisitor()->Trace(object);
 }
 
+// static
 void UnifiedHeapMarkingVisitor::WriteBarrier(
     v8::Isolate* isolate,
     const WrapperTypeInfo* wrapper_type_info,
@@ -81,6 +83,11 @@ void UnifiedHeapMarkingVisitor::WriteBarrier(
   wrapper_type_info->Trace(thread_state->CurrentVisitor(), object);
 }
 
+void UnifiedHeapMarkingVisitor::Visit(
+    const TraceWrapperV8Reference<v8::Value>& v) {
+  VisitImpl(v);
+}
+
 ConcurrentUnifiedHeapMarkingVisitor::ConcurrentUnifiedHeapMarkingVisitor(
     ThreadState* thread_state,
     MarkingMode mode,
@@ -92,6 +99,11 @@ ConcurrentUnifiedHeapMarkingVisitor::ConcurrentUnifiedHeapMarkingVisitor(
 void ConcurrentUnifiedHeapMarkingVisitor::FlushWorklists() {
   ConcurrentMarkingVisitor::FlushWorklists();
   v8_references_worklist_.FlushToGlobal();
+}
+
+void ConcurrentUnifiedHeapMarkingVisitor::Visit(
+    const TraceWrapperV8Reference<v8::Value>& v) {
+  VisitImpl(v);
 }
 
 }  // namespace blink
