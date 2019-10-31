74f0468e2df69cbf2941461f28c25b555b1cb81b
Sebastien Marchand <sebmarchand@chromium.org>
2019-10-31 21:40:34 +0000

    Set the lower limit of the Enterprise Memory Limit evaluator to 1 GB
    On Stable 1 GB corresponds to the 60th percentile for total resident
    set, setting the limit to 1GB will let Chrome work fine with 3-4 tabs.
    
    Bug: 980965
    Change-Id: If4f0b6171eb159c3781290b8060617d57d399327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893580
    Auto-Submit: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711472}

--- chrome/browser/memory/enterprise_memory_limit_pref_observer.cc
+++ chrome/browser/memory/enterprise_memory_limit_pref_observer.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace memory {
 
 namespace {
-const int kMinimalResidentSetLimitMb = 2048;
+const int kMinimalResidentSetLimitMb = 1024;
 }  // namespace
 
 EnterpriseMemoryLimitPrefObserver::EnterpriseMemoryLimitPrefObserver(
