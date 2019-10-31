bccd2eb4aea7ef3b62579d952060ad960ec4b643
David Benjamin <davidben@chromium.org>
2019-10-31 16:34:27 +0000

    Remove ClientCertStoreChromeOS::CertFilter indirection
    Historically, ClientCertStoreChromeOS lived in //net and depended on the
    CrOS NSS profile logic in //crypto. To reduce the dependency on that
    mess, https://codereview.chromium.org/663583006 extracted the filtering
    logic into a separate ClientCertFilterChromeOS in //chrome, indirected
    via a CertFilter interface.
    
    This was done in preparation for
    https://codereview.chromium.org/419013003/, which was since abandoned
    (although the initialization still needs cleanup, per
    https://crbug.com/1018972). It also left the ClientCertFilterChromeOS
    logic entirely untested.
    
    Since then, ClientCertStoreChromeOS has moved into //chrome, so we no
    longer need the indirection. Fold it back in and test the user profile
    logic directly to restore ClientCertFilterChromeOS's test coverage. This
    is done preparation for work on https://crbug.com/1011576, which will
    affect this code somewhat.
    
    This moves the pre-NSS-3.30 workaround for importing ECDSA keys from
    ssl_platform_key_nss_unittest.cc to cert_test_util_nss.cc so it works
    for all callers of ImportSensitiveKeyFromFile.
    
    Bug: 1011576
    Change-Id: I26ae5d610b8b7e443c18ca1314d16129a8f52350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1888312
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711301}

--- chrome/browser/chromeos/platform_keys/platform_keys_nss.cc
+++ chrome/browser/chromeos/platform_keys/platform_keys_nss.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part_chromeos.h"
 #include "chrome/browser/chromeos/certificate_provider/certificate_provider.h"
-#include "chrome/browser/chromeos/net/client_cert_filter_chromeos.h"
 #include "chrome/browser/chromeos/net/client_cert_store_chromeos.h"
 #include "chrome/browser/chromeos/platform_keys/platform_keys.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
@@ -598,8 +597,7 @@ void SelectCertificatesOnIOThread(
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   state->cert_store_.reset(new ClientCertStoreChromeOS(
       nullptr,  // no additional provider
-      std::make_unique<ClientCertFilterChromeOS>(state->use_system_key_slot_,
-                                                 state->username_hash_),
+      state->use_system_key_slot_, state->username_hash_,
       ClientCertStoreChromeOS::PasswordDelegateFactory()));
 
   SelectCertificatesState* state_ptr = state.get();
--- net/test/cert_test_util.h
+++ net/test/cert_test_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_TEST_CERT_TEST_UTIL_H_
 
 #include <string>
+#include <vector>
 
 #include "base/memory/ref_counted.h"
 #include "net/cert/x509_cert_types.h"
