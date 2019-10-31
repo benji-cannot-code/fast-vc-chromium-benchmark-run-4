1fdceacc1e34019e2ede482ea0d3e0f8da57b198
Monica Basta <msalama@chromium.org>
2019-10-31 11:24:12 +0000

    [ProfileInfoCache]: Remove SetAuthInfoOfProfileAtIndex & GetUserNameOfProfileAtIndex.
    As part of the ProfileInfoInterface deprecation, we remove
    |SetAuthInfoOfProfileAtIndex| and |GetUserNameOfProfileAtIndex|.
    Afterwards, setting the auth info and getting the username should be
    done through |ProfileAttributesEntry::SetAuthInfo| and
    |ProfileAttributesEntry::GetUserName|.
    
    Change-Id: I2ccfc2260127546984f7ed4aa058b5f0cd28e9fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1892773
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711204}

--- chrome/browser/profiles/profile_attributes_entry.h
+++ chrome/browser/profiles/profile_attributes_entry.h
@@ -156,6 +156,7 @@ class ProfileAttributesEntry {
   static const char kBackgroundAppsKey[];
   static const char kProfileIsEphemeral[];
   static const char kUserNameKey[];
+  static const char kGAIAIdKey[];
   static const char kIsConsentedPrimaryAccountKey[];
 
  private:
