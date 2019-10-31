69c0eb44a0fc021e65c3db9a9f703b062b765215
Min Qin <qinmin@chromium.org>
2019-10-31 20:57:56 +0000

    Fix a crash when checking for existing download path
    DownloadPathReseavationTracker::CheckDownloadPathForExistingDownload()
    is called on UI thread. However, the g_reservation_map should only be
    used DPRT's own task runner. As a result, there are race conditions
    that cause the method to hit a null pointer.
    
    This CL fixes the issue by posting a task on to DPRT's own task
    runner. And also remove the feature flag that is already enabled.
    
    BUG=1019627
    
    Change-Id: Ibff4468f5a1fbaea4479481a115c835220089653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1891734
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711443}

--- components/download/internal/common/download_path_reservation_tracker_unittest.cc
+++ components/download/internal/common/download_path_reservation_tracker_unittest.cc
@@ -16,12 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_file_util.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
-#include "components/download/public/common/download_features.h"
 #include "components/download/public/common/download_path_reservation_tracker.h"
 #include "components/download/public/common/mock_download_item.h"
 #include "net/base/filename_util.h"
@@ -355,12 +353,6 @@ TEST_F(DownloadPathReservationTrackerTest, ConflictWithSource) {
 
 // Multiple reservations for the same path should uniquify around each other.
 TEST_F(DownloadPathReservationTrackerTest, ConflictingReservations) {
-  // When kPreventDownloadsWithSamePath is disabled,
-  // an OVERWRITE reservation with a conflicting path should return success
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndDisableFeature(
-      features::kPreventDownloadsWithSamePath);
-
   std::unique_ptr<MockDownloadItem> item1 = CreateDownloadItem(1);
   base::FilePath path(
       GetPathInDownloadsDirectory(FILE_PATH_LITERAL("foo.txt")));
@@ -410,8 +402,8 @@ TEST_F(DownloadPathReservationTrackerTest, ConflictingReservations) {
   }
   RunUntilIdle();
 
-  // Now acquire an overwriting reservation. We should end up with the same
-  // non-uniquified path for both reservations.
+  // Now acquire an overwriting reservation. It should end up with a CONFLICT
+  // result.
   std::unique_ptr<MockDownloadItem> item3 = CreateDownloadItem(2);
   base::FilePath reserved_path3;
   conflict_action = DownloadPathReservationTracker::OVERWRITE;
@@ -420,7 +412,7 @@ TEST_F(DownloadPathReservationTrackerTest, ConflictingReservations) {
   EXPECT_TRUE(IsPathInUse(path));
   EXPECT_FALSE(IsPathInUse(uniquified_path));
 
-  EXPECT_EQ(PathValidationResult::SUCCESS, result);
+  EXPECT_EQ(PathValidationResult::CONFLICT, result);
 
   EXPECT_EQ(path.value(), reserved_path1.value());
   EXPECT_EQ(path.value(), reserved_path3.value());
@@ -429,13 +421,9 @@ TEST_F(DownloadPathReservationTrackerTest, ConflictingReservations) {
   SetDownloadItemState(item3.get(), DownloadItem::COMPLETE);
 }
 
-// When kPreventDownloadsWithSamePath flag is enabled, an OVERWRITE reservation
-// with the same path as an active reservation should return a CONFLICT result.
+// An OVERWRITE reservation with the same path as an active reservation should
+// return a CONFLICT result.
 TEST_F(DownloadPathReservationTrackerTest, ConflictingReservation_Prevented) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(
-      features::kPreventDownloadsWithSamePath);
-
   std::unique_ptr<MockDownloadItem> item1 = CreateDownloadItem(1);
   base::FilePath path(
       GetPathInDownloadsDirectory(FILE_PATH_LITERAL("foo.txt")));
