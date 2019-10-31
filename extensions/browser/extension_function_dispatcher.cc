dddee7e06eabb41b3b117dabc8d0efd0991006f2
Mikhail Atuchin <atuchin@yandex-team.ru>
2019-10-31 06:26:55 +0000

    [extensions] Fix using the wrong thread id after SW restarting
    If SW was restarted and the same process could be used.
    Update UIThreadWorkerResponseCallbackWrapper to not store worker_thread_id in it
    and instead use the correct worker_thread_id per callback returned via CreateCallback.
    
    Change-Id: I5caead02f2d45fbac967b9e5d42e0e1ef5d970fc
    Bug: 1019677
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879258
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Auto-Submit: Mikhail Atuchin <atuchin@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#711155}

--- extensions/browser/extension_function_dispatcher.cc
+++ extensions/browser/extension_function_dispatcher.cc
@@ -158,8 +158,7 @@ class ExtensionFunctionDispatcher::UIThreadWorkerResponseCallbackWrapper
       int worker_thread_id)
       : dispatcher_(dispatcher),
         observer_(this),
-        render_process_host_(render_process_host),
-        worker_thread_id_(worker_thread_id) {
+        render_process_host_(render_process_host) {
     observer_.Add(render_process_host_);
 
     DCHECK(ExtensionsClient::Get()
@@ -180,10 +179,11 @@ class ExtensionFunctionDispatcher::UIThreadWorkerResponseCallbackWrapper
     CleanUp();
   }
 
-  ExtensionFunction::ResponseCallback CreateCallback(int request_id) {
+  ExtensionFunction::ResponseCallback CreateCallback(int request_id,
+                                                     int worker_thread_id) {
     return base::Bind(
         &UIThreadWorkerResponseCallbackWrapper::OnExtensionFunctionCompleted,
-        weak_ptr_factory_.GetWeakPtr(), request_id);
+        weak_ptr_factory_.GetWeakPtr(), request_id, worker_thread_id);
   }
 
  private:
@@ -197,6 +197,7 @@ class ExtensionFunctionDispatcher::UIThreadWorkerResponseCallbackWrapper
   }
 
   void OnExtensionFunctionCompleted(int request_id,
+                                    int worker_thread_id,
                                     ExtensionFunction::ResponseType type,
                                     const base::ListValue& results,
                                     const std::string& error) {
@@ -205,7 +206,7 @@ class ExtensionFunctionDispatcher::UIThreadWorkerResponseCallbackWrapper
       return;
     }
     render_process_host_->Send(new ExtensionMsg_ResponseWorker(
-        worker_thread_id_, request_id, type == ExtensionFunction::SUCCEEDED,
+        worker_thread_id, request_id, type == ExtensionFunction::SUCCEEDED,
         results, error));
   }
 
@@ -213,7 +214,6 @@ class ExtensionFunctionDispatcher::UIThreadWorkerResponseCallbackWrapper
   ScopedObserver<content::RenderProcessHost, content::RenderProcessHostObserver>
       observer_{this};
   content::RenderProcessHost* const render_process_host_;
-  const int worker_thread_id_;
   base::WeakPtrFactory<UIThreadWorkerResponseCallbackWrapper> weak_ptr_factory_{
       this};
 
@@ -318,7 +318,8 @@ void ExtensionFunctionDispatcher::Dispatch(
     }
     DispatchWithCallbackInternal(
         params, nullptr, render_process_id,
-        callback_wrapper->CreateCallback(params.request_id));
+        callback_wrapper->CreateCallback(params.request_id,
+                                         params.worker_thread_id));
   }
 }
 
