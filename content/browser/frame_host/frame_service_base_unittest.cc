caae679894b51b007d583d7edd9864558a03152d
Fergal Daly <fergal@chromium.org>
2019-10-31 23:43:42 +0000

    Disable bfcache for FrameServiceBaseTest.DidFinishNavigation.
    Change-Id: I06d29a268b5c1b0526dc04c2980b116a987d54c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1890403
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711527}

--- content/browser/frame_host/frame_service_base_unittest.cc
+++ content/browser/frame_host/frame_service_base_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/run_loop.h"
+#include "content/public/browser/back_forward_cache.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/navigation_simulator.h"
@@ -119,6 +120,10 @@ TEST_F(FrameServiceBaseTest, RenderFrameDeleted) {
 }
 
 TEST_F(FrameServiceBaseTest, DidFinishNavigation) {
+  // When a page enters the BackForwardCache, the RenderFrameHost is not
+  // deleted.
+  web_contents()->GetController().GetBackForwardCache().DisableForTesting(
+      BackForwardCache::TEST_ASSUMES_NO_CACHING);
   CreateEchoImpl(main_rfh_);
   SimulateNavigation(main_rfh_, GURL(kBarOrigin));
   EXPECT_FALSE(is_echo_impl_alive_);
