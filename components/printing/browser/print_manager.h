656c9b2d8f7a4ffe7be232b237dfc0e1244b2829
Jesse Schettler <jschettler@chromium.org>
2019-10-31 20:39:57 +0000

    printing: Store PrintRenderFrame with its RenderFrameHost
    Add a std::map to store a PrintRenderFrame with the RenderFrameHost used
    to bind it. When calling GetPrintRenderFrame(), use the provided
    RenderFrameHost to fetch the correct PrintRenderFrame associated remote
    or create a new one.
    
    Bug: 1019847, 1019960, 1020108
    Test: https://clusterfuzz.com/testcase-detail/4896664890245120
    Change-Id: I5ebf26b1dcdcd045bac1d1c5b204c8517fdb5ea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1892055
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Auto-Submit: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711434}

--- components/printing/browser/print_manager.h
+++ components/printing/browser/print_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PRINTING_BROWSER_PRINT_MANAGER_H_
 #define COMPONENTS_PRINTING_BROWSER_PRINT_MANAGER_H_
 
+#include <map>
 #include <memory>
 
 #include "base/macros.h"
@@ -53,6 +54,7 @@ class PrintManager : public content::WebContentsObserver {
   // content::WebContentsObserver
   bool OnMessageReceived(const IPC::Message& message,
                          content::RenderFrameHost* render_frame_host) override;
+  void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) override;
 
   // IPC handling support
   struct FrameDispatchHelper;
@@ -104,9 +106,12 @@ class PrintManager : public content::WebContentsObserver {
  private:
   void OnDidGetDocumentCookie(int cookie);
 
-  // Used to transmit mojom interface method calls to the PrintRenderFrame
-  // associated remote.
-  mojo::AssociatedRemote<printing::mojom::PrintRenderFrame> print_render_frame_;
+  // Stores a PrintRenderFrame associated remote with the RenderFrameHost used
+  // to bind it. The PrintRenderFrame is used to transmit mojo interface method
+  // calls to the associated receiver.
+  std::map<content::RenderFrameHost*,
+           mojo::AssociatedRemote<printing::mojom::PrintRenderFrame>>
+      print_render_frames_;
 
   DISALLOW_COPY_AND_ASSIGN(PrintManager);
 };
