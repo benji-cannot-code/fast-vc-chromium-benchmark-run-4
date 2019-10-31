34da987d3808d8317d556ff15594290b66402b22
Elly Fong-Jones <ellyjones@chromium.org>
2019-10-31 20:05:02 +0000

    flags: expire M78 & earlier flags
    This change will be merged to the M79 branch in keeping with the flag
    deprecation rollout strategy:
    
    * M78 expires flags set for 76 & earlier
    * M79 expires flags set for 78 & earlier
    * M80 expires flags set for 80 & earlier
    * MXX expires flags set for XX & earlier onward
    
    Bug: 1019714
    Change-Id: Ic789610160f1de11715ed41d6ff153a71487a08d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1890814
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711417}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1271,6 +1271,8 @@ extern const char kTurnOffStreamingMediaCachingDescription[];
 
 extern const char kUnexpireFlagsM76Name[];
 extern const char kUnexpireFlagsM76Description[];
+extern const char kUnexpireFlagsM78Name[];
+extern const char kUnexpireFlagsM78Description[];
 
 extern const char kUnsafeWebGPUName[];
 extern const char kUnsafeWebGPUDescription[];
--- chrome/browser/unexpire_flags.h
+++ chrome/browser/unexpire_flags.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace flags {
 
 extern const base::Feature kUnexpireFlagsM76;
+extern const base::Feature kUnexpireFlagsM78;
 
 bool IsFlagExpired(const char* internal_name);
 
