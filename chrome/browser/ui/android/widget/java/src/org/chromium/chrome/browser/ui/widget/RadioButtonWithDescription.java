e157e3adf2b32c7b87be2aa2881f752518bc0366
Brandon Wylie <wylieb@chromium.org>
2019-10-31 21:59:34 +0000

    Add layout for RadioButtonWithDescription to allow programmatic uses
    RadioButtonWithDescription & friends don't support adding via code.
    This change wraps up the view properly to allow for inflation and adds
    a layout to host the buttons and proxy checked events between the
    custom code and the standard callbacks.
    
    Bug: 1015218
    Change-Id: Ic4bc5e1d01ee982a4513f9c9b202c40646b8739f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1869024
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Ender <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711479}

--- chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/RadioButtonWithDescription.java
+++ chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/RadioButtonWithDescription.java
@@ -28,12 +28,18 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
     /**
      * Interface to listen to radio button changes.
      */
-    public interface OnCheckedChangeListener { abstract void onCheckedChanged(); }
+    interface ButtonCheckedStateChangedListener {
+        /**
+         * Invoked when a {@link RadioButtonWithDescription} is selected.
+         * @param checkedRadioButton The radio button that was selected.
+         */
+        void onButtonCheckedStateChanged(RadioButtonWithDescription checkedRadioButton);
+    }
 
     private RadioButton mRadioButton;
     private TextView mTitle;
     private TextView mDescription;
-    private OnCheckedChangeListener mOnCheckedChangeListener;
+    private ButtonCheckedStateChangedListener mButtonCheckedStateChangedListener;
 
     private List<RadioButtonWithDescription> mGroup;
 
@@ -101,8 +107,8 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
 
         setChecked(true);
 
-        if (mOnCheckedChangeListener != null) {
-            mOnCheckedChangeListener.onCheckedChanged();
+        if (mButtonCheckedStateChangedListener != null) {
+            mButtonCheckedStateChangedListener.onButtonCheckedStateChanged(this);
         }
     }
 
@@ -111,6 +117,13 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
      */
     public void setTitleText(CharSequence text) {
         mTitle.setText(text);
+    }
+
+    /**
+     * @return The text shown in the title section.
+     */
+    public CharSequence getTitleText() {
+        return mTitle.getText();
     }
 
     /**
@@ -126,6 +139,13 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
             ((LayoutParams) mTitle.getLayoutParams()).removeRule(RelativeLayout.CENTER_VERTICAL);
             mDescription.setVisibility(View.VISIBLE);
         }
+    }
+
+    /**
+     * @return The text shown in the description section.
+     */
+    public CharSequence getDescriptionText() {
+        return mDescription.getText();
     }
 
     /**
@@ -146,8 +166,8 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
         if (checked) requestFocus();
     }
 
-    public void setOnCheckedChangeListener(OnCheckedChangeListener listener) {
-        mOnCheckedChangeListener = listener;
+    public void setOnCheckedChangeListener(ButtonCheckedStateChangedListener listener) {
+        mButtonCheckedStateChangedListener = listener;
     }
 
     /**
