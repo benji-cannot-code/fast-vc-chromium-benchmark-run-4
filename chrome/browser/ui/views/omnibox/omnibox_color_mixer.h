008b282c5d5125e8c68c5bb70a7de1901e6f93cf
Peter Kasting <pkasting@chromium.org>
2019-10-31 03:11:16 +0000

    Initial version of an omnibox color mixer.  Not yet instantiated.
    Bug: 1003612
    Change-Id: I1ad31927926264069d8196de2c87ea23364da952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1887666
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711116}

---
+++ chrome/browser/ui/views/omnibox/omnibox_color_mixer.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_COLOR_MIXER_H_
+#define CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_COLOR_MIXER_H_
+
+namespace ui {
+class ColorProvider;
+}
+
+// Adds a color mixer to |provider| that contains recipes for omnibox colors,
+// given whether they should be |high_contrast|.
+// TODO(pkasting): Perhaps |high_contrast| should be a bit on the ColorProvider.
+void AddOmniboxColorMixer(ui::ColorProvider* provider, bool high_contrast);
+
+#endif  // CHROME_BROWSER_UI_VIEWS_OMNIBOX_OMNIBOX_COLOR_MIXER_H_
