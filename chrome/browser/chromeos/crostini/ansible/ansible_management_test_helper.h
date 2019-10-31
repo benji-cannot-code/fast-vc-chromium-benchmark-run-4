ba2c120842569154c7d952df3cb3ffa29effea43
Olya Kalitova <okalitova@chromium.org>
2019-10-31 12:13:04 +0000

    Move default Crostini configuration steps from CrostiniManager
    Moves default Crostini configuration steps from CrostiniManager to
    AnsibleManagementService. Introduces test helper class so that
    crostini_manager_unittest.cc and ansible_management_service_unittest
    don't duplicate code.
    
    --gtest_filter="Ansible*" && manually via deploying Chrome to DUT
    
    Test: unit_tests --gtest_filter="Crostini*" and unit_tests
    Bug: 998124
    Change-Id: Ie7b1d185fe556a416abde5cb0e3979d0b4adc010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1884851
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Alex Oldemeier <aoldemeier@chromium.org>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711218}

---
+++ chrome/browser/chromeos/crostini/ansible/ansible_management_test_helper.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_CROSTINI_ANSIBLE_ANSIBLE_MANAGEMENT_TEST_HELPER_H_
+#define CHROME_BROWSER_CHROMEOS_CROSTINI_ANSIBLE_ANSIBLE_MANAGEMENT_TEST_HELPER_H_
+
+#include "base/test/scoped_feature_list.h"
+#include "chromeos/dbus/cicerone/cicerone_service.pb.h"
+#include "chromeos/dbus/fake_cicerone_client.h"
+
+class TestingProfile;
+
+namespace crostini {
+
+// Helper class that provides common Ansible setup functionality for Crostini
+// tests that involve Ansible management. Test helper assumes that
+// DBusThreadManager is already initialized and Crostini is enabled.
+class AnsibleManagementTestHelper {
+ public:
+  explicit AnsibleManagementTestHelper(TestingProfile* profile);
+
+  void SetUpAnsibleInfra();
+
+  void SetUpAnsibleInstallation(
+      vm_tools::cicerone::InstallLinuxPackageResponse::Status status);
+  void SetUpPlaybookApplication(
+      vm_tools::cicerone::ApplyAnsiblePlaybookResponse::Status status);
+  void SendSucceededInstallSignal();
+  void SendSucceededApplySignal();
+
+ private:
+  void SetUpAnsiblePlaybookPreference();
+
+  TestingProfile* profile_;
+  base::test::ScopedFeatureList scoped_feature_list_;
+
+  // Owned by chromeos::DBusThreadManager
+  chromeos::FakeCiceroneClient* fake_cicerone_client_;
+};
+
+}  // namespace crostini
+
+#endif  // CHROME_BROWSER_CHROMEOS_CROSTINI_ANSIBLE_ANSIBLE_MANAGEMENT_TEST_HELPER_H_
