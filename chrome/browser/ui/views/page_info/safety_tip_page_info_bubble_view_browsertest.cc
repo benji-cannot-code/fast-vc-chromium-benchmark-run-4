b2e2ce65613ee2688bd4a39e7833e7deaf92d87d
Armando Miraglia <armax@chromium.org>
2019-10-31 11:58:32 +0000

    [Sheriff] Disabling consistently failing SafetyTipPageInfoBubbleViewBrowserTest.* tests.
    During sheriffing I have attempted to determine the reason for failing for two
    tests in SafetyTipPageInfoBubbleViewBrowserTest but did not manage. I am
    hence disabling for later further investigation.
    
    TBR=estark@chromium.org
    
    Bug: 1020109
    Change-Id: I78419a0101cfbcad04f9b4c09553de6ea442c18c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1891175
    Reviewed-by: Armando Miraglia <armax@chromium.org>
    Commit-Queue: Armando Miraglia <armax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711216}

--- chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/page_info/safety_tip_page_info_bubble_view_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/engagement/site_engagement_score.h"
 #include "chrome/browser/engagement/site_engagement_service.h"
 #include "chrome/browser/history/history_service_factory.h"
@@ -572,6 +573,12 @@ IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
   EXPECT_FALSE(IsUIShowing());
 }
 
+// Disabled due to consistent failure: http://crbug.com/1020109
+#if defined(OS_LINUX)
+#define MAYBE_TriggersOnEditDistance DISABLED_TriggersOnEditDistance
+#else
+#define MAYBE_TriggersOnEditDistance TriggersOnEditDistance
+#endif
 // Tests that Safety Tips trigger (or not) on lookalike domains with edit
 // distance when enabled, and not otherwise.
 IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
@@ -626,9 +633,15 @@ IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
                                1);
 }
 
+// Disabled due to consistent failure: http://crbug.com/1020109
+#if defined(OS_LINUX)
+#define MAYBE_InteractionsHistogram DISABLED_InteractionsHistogram
+#else
+#define MAYBE_InteractionsHistogram InteractionsHistogram
+#endif
 // Tests that Safety Tip interactions are recorded in a histogram.
 IN_PROC_BROWSER_TEST_P(SafetyTipPageInfoBubbleViewBrowserTest,
-                       InteractionsHistogram) {
+                       MAYBE_InteractionsHistogram) {
   const std::string kHistogramPrefix = "Security.SafetyTips.Interaction.";
 
   // These histograms are only recorded when the UI feature is enabled, so bail
