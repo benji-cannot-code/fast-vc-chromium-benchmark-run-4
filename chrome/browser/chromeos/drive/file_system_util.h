a82845deb0fdd79ef1da88ef869eee2dd665c0b8
Austin Tankiang <austinct@chromium.org>
2019-10-31 04:30:22 +0000

    Remove legacy drive client util functions
    Also remove WriteOnCacheFile as it is no longer being used.
    
    Bug: 1003238
    Change-Id: I8cf85264b08b7ed969c2bf88f40b36702810404c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1890396
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711140}

--- chrome/browser/chromeos/drive/file_system_util.h
+++ chrome/browser/chromeos/drive/file_system_util.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 
-namespace storage {
-class FileSystemURL;
-}
-
 namespace drive {
 
 class DriveIntegrationService;
@@ -29,14 +25,6 @@ namespace util {
 // Otherwise, nullptr.
 DriveIntegrationService* GetIntegrationServiceByProfile(Profile*);
 
-// Returns the Drive mount point path, which looks like "/special/drive-<hash>".
-base::FilePath GetDriveMountPointPath(Profile* profile);
-
-// Returns the Drive mount point path, which looks like
-// "/special/drive-<username_hash>", when provided with the |user_id_hash|.
-base::FilePath GetDriveMountPointPathForUserIdHash(
-    const std::string& user_id_hash);
-
 // Returns true if the given path is under the Drive mount point.
 bool IsUnderDriveMountPoint(const base::FilePath& path);
 
@@ -44,16 +32,6 @@ bool IsUnderDriveMountPoint(const base::FilePath& path);
 // point. Returns an empty path if |path| is not under the Drive mount point.
 // Examples: ExtractDrivePath("/special/drive-xxx/foo.txt") => "drive/foo.txt"
 base::FilePath ExtractDrivePath(const base::FilePath& path);
-
-// Extracts |profile| from the given paths located under
-// GetDriveMountPointPath(profile). Returns NULL if it does not correspond to
-// a valid mount point path. Must be called from UI thread.
-Profile* ExtractProfileFromPath(const base::FilePath& path);
-
-// Extracts the Drive path (e.g., "drive/foo.txt") from the filesystem URL.
-// Returns an empty path if |url| does not point under Drive mount point.
-base::FilePath ExtractDrivePathFromFileSystemUrl(
-    const storage::FileSystemURL& url);
 
 // Gets the cache root path (i.e. <user_profile_dir>/GCache/v1) from the
 // profile.
