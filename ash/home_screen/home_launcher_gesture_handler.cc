50102d45952dd1e5fa6dfab68df25f3908a3fd56
Toni Barzic <tbarzic@chromium.org>
2019-11-01 01:30:54 +0000

    Improve home to overview gesture detection
    Updates swipe_home_to_overview_controller to start overview session if:
    *   user drags the pointer far enough above the shelf
    *   holds for 350 ms
    
    As the user is dragging up, the home view is scaled down, with minimum
    scale being 0.92 (the home will have the target scale at a poing
    little above the transition threshold line - if the user keeps going up,
    the home scale will remain at 92).
    
    If the gesture ends before the the hold time-out has exipred, or below
    the minimal vertical distance from the shelf, the gesture is canceled,
    and the home screen.
    
    Note that the transition might trigger before the drag finger is lifted
    (in that case subsequent drag events will be ignored).
    
    BUG=1005366
    
    Change-Id: I89be582f91322595031d699dc0d335d9c97b1ebe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894316
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711580}

--- ash/home_screen/home_launcher_gesture_handler.cc
+++ ash/home_screen/home_launcher_gesture_handler.cc
@@ -327,7 +327,7 @@ bool HomeLauncherGestureHandler::OnScrollEvent(const gfx::Point& location,
 bool HomeLauncherGestureHandler::OnReleaseEvent(
     const gfx::Point& location,
     base::Optional<float> velocity_y) {
-  if (IsAnimating())
+  if (mode_ != Mode::kSwipeHomeToOverview && IsAnimating())
     return false;
 
   return OnDragEnded(location, velocity_y);
@@ -917,7 +917,7 @@ bool HomeLauncherGestureHandler::SetUpWindows(
 void HomeLauncherGestureHandler::OnDragStarted(const gfx::Point& location) {
   if (mode_ == Mode::kSwipeHomeToOverview) {
     swipe_home_to_overview_controller_ =
-        std::make_unique<SwipeHomeToOverviewController>();
+        std::make_unique<SwipeHomeToOverviewController>(display_.id());
   } else {
     NotifyHomeLauncherTargetPositionChanged(
         mode_ == Mode::kSlideUpToShow /*showing*/, display_.id());
