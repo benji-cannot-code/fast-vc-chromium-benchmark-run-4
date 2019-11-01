4a785b0ad1ad0e9d2e89cf4011c97d031a89ecf1
Jeremy Apthorp <jeremya@chromium.org>
2019-11-01 20:47:33 +0000

    include <algorithm> for std::min in MSSTL
    fixes this build error in MSSTL:
    
    ../../ui/events/blink/prediction/linear_resampling.cc(82,12): error: no member named 'min' in namespace 'std'
    
          std::min(kResampleMaxPrediction, events_dt_ / 2.0);
          ~~~~~^
    ../../ui/events/blink/prediction/linear_resampling.cc(85,12): error: no member named 'min' in namespace 'std'
          std::min(sample_time, events_queue_[0].time_stamp + max_prediction);
          ~~~~~^
    2 errors generated.
    
    Change-Id: I412007ebe847afaa667bb606c01e2d5453123089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893754
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Auto-Submit: Jeremy Apthorp <jeremya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711851}

--- ui/events/blink/prediction/least_squares_predictor.cc
+++ ui/events/blink/prediction/least_squares_predictor.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/blink/prediction/least_squares_predictor.h"
 #include "ui/events/blink/prediction/predictor_factory.h"
 
+#include <algorithm>
+
 namespace ui {
 
 namespace {
--- ui/events/blink/prediction/linear_resampling.cc
+++ ui/events/blink/prediction/linear_resampling.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/events/blink/prediction/linear_resampling.h"
 
+#include <algorithm>
+
 #include "ui/events/blink/prediction/predictor_factory.h"
 
 namespace ui {
