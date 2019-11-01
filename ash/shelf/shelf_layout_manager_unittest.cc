cd404079f691a3da3b65485733310868e8d6fd1b
Manu Cornet <manucornet@chromium.org>
2019-11-01 18:39:39 +0000

    CrOS Shelf: Start moving some hotseat logic into the hotseat widget
    The general idea here is to encapsulate more of the hotseat's behavior
    into its own class rather than have it all over the shelf layout
    manager. Eventually, I hope to get rid of the huge
    "CalculateHotseatState" method by setting the hotseat state to the
    appropriate values from the relevant places.
    
    Change-Id: I03bf50bc9f6fb68f7f22f1b6ec966f08f48220da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893997
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711796}

--- ash/shelf/shelf_layout_manager_unittest.cc
+++ ash/shelf/shelf_layout_manager_unittest.cc
@@ -3220,19 +3220,16 @@ class HotseatShelfLayoutManagerTest
 };
 
 // Records HotseatState transitions.
-class HotseatStateWatcher : public ShelfLayoutManagerObserver {
+class HotseatStateWatcher : public HotseatWidget::Observer {
  public:
-  HotseatStateWatcher(ShelfLayoutManager* shelf_layout_manager)
-      : shelf_layout_manager_(shelf_layout_manager) {
-    shelf_layout_manager_->AddObserver(this);
+  explicit HotseatStateWatcher(HotseatWidget* widget) : widget_(widget) {
+    widget_->AddObserver(this);
   }
-  ~HotseatStateWatcher() override {
-    shelf_layout_manager_->RemoveObserver(this);
-  }
+  ~HotseatStateWatcher() override { widget_->RemoveObserver(this); }
 
-  void OnHotseatStateChanged(HotseatState state) override {
+  void OnHotseatStateChanged() override {
     run_loop_.QuitWhenIdle();
-    state_changes_.push_back(state);
+    state_changes_.push_back(widget_->state());
   }
 
   void CheckEqual(std::vector<HotseatState> state_changes) {
@@ -3242,7 +3239,7 @@ class HotseatStateWatcher : public ShelfLayoutManagerObserver {
   void WaitUntilStateChanged() { run_loop_.Run(); }
 
  private:
-  ShelfLayoutManager* shelf_layout_manager_;
+  HotseatWidget* const widget_;
   std::vector<HotseatState> state_changes_;
   base::RunLoop run_loop_;
   DISALLOW_COPY_AND_ASSIGN(HotseatStateWatcher);
@@ -3540,8 +3537,8 @@ TEST_F(HotseatShelfLayoutManagerTest, ReleasingSlowDragBelowThreshold) {
   EXPECT_EQ(HotseatState::kHidden, GetShelfLayoutManager()->hotseat_state());
 }
 
-// Tests that releasing the hotseat gesture above the threshold results in a
-// kExtended hotseat.
+// Tests that releasing the hotseat gesture above the threshold results in an
+// extended hotseat.
 TEST_P(HotseatShelfLayoutManagerTest, ReleasingSlowDragAboveThreshold) {
   GetPrimaryShelf()->SetAutoHideBehavior(GetParam());
   TabletModeControllerTestApi().EnterTabletMode();
@@ -3578,7 +3575,8 @@ TEST_P(HotseatShelfLayoutManagerTest, ShowingOverviewFromShownAnimatesOnce) {
   wm::ActivateWindow(window.get());
 
   std::unique_ptr<HotseatStateWatcher> state_watcher_ =
-      std::make_unique<HotseatStateWatcher>(GetShelfLayoutManager());
+      std::make_unique<HotseatStateWatcher>(
+          GetPrimaryShelf()->shelf_widget()->hotseat_widget());
   SwipeUpOnShelf();
   ASSERT_EQ(HotseatState::kExtended, GetShelfLayoutManager()->hotseat_state());
 
@@ -3782,7 +3780,8 @@ TEST_P(HotseatShelfLayoutManagerTest, HomeToOverviewChangesStateOnce) {
                                                 .CenterPoint();
 
   {
-    HotseatStateWatcher watcher(GetShelfLayoutManager());
+    HotseatStateWatcher watcher(
+        GetPrimaryShelf()->shelf_widget()->hotseat_widget());
     OverviewAnimationWaiter waiter;
     GetEventGenerator()->GestureTapAt(overview_button_center);
     waiter.Wait();
@@ -3801,7 +3800,8 @@ TEST_P(HotseatShelfLayoutManagerTest, HomeToOverviewChangesStateOnce) {
   GetAppListTestHelper()->CheckVisibility(true);
   // Activate overview and expect the hotseat only changes state to extended.
   {
-    HotseatStateWatcher watcher(GetShelfLayoutManager());
+    HotseatStateWatcher watcher(
+        GetPrimaryShelf()->shelf_widget()->hotseat_widget());
     OverviewAnimationWaiter waiter;
     GetEventGenerator()->GestureTapAt(overview_button_center);
     waiter.Wait();
@@ -3816,7 +3816,8 @@ TEST_P(HotseatShelfLayoutManagerTest, HomeToInAppChangesStateOnce) {
   TabletModeControllerTestApi().EnterTabletMode();
 
   // Go to in-app, the hotseat should hide.
-  HotseatStateWatcher watcher(GetShelfLayoutManager());
+  HotseatStateWatcher watcher(
+      GetPrimaryShelf()->shelf_widget()->hotseat_widget());
   std::unique_ptr<aura::Window> window =
       AshTestBase::CreateTestWindow(gfx::Rect(0, 0, 400, 400));
   wm::ActivateWindow(window.get());
@@ -3840,7 +3841,8 @@ TEST_P(HotseatShelfLayoutManagerTest, InAppToHomeChangesStateOnce) {
   // Press the HomeLauncher button, the hotseat should transition directly to
   // kShown.
   {
-    HotseatStateWatcher watcher(GetShelfLayoutManager());
+    HotseatStateWatcher watcher(
+        GetPrimaryShelf()->shelf_widget()->hotseat_widget());
     views::View* home_button =
         GetPrimaryShelf()->shelf_widget()->GetHomeButton();
     GetEventGenerator()->GestureTapAt(
@@ -3858,7 +3860,8 @@ TEST_P(HotseatShelfLayoutManagerTest, InAppToHomeChangesStateOnce) {
   {
     ui::ScopedAnimationDurationScaleMode regular_animations(
         ui::ScopedAnimationDurationScaleMode::NON_ZERO_DURATION);
-    HotseatStateWatcher watcher(GetShelfLayoutManager());
+    HotseatStateWatcher watcher(
+        GetPrimaryShelf()->shelf_widget()->hotseat_widget());
     FlingUpOnShelf();
     watcher.WaitUntilStateChanged();
     watcher.CheckEqual({HotseatState::kShown});
@@ -3875,7 +3878,8 @@ TEST_P(HotseatShelfLayoutManagerTest, InAppToHomeChangesStateOnce) {
   // Press the HomeLauncher button, the hotseat should transition directly to
   // kShown.
   {
-    HotseatStateWatcher watcher(GetShelfLayoutManager());
+    HotseatStateWatcher watcher(
+        GetPrimaryShelf()->shelf_widget()->hotseat_widget());
     views::View* home_button =
         GetPrimaryShelf()->shelf_widget()->GetHomeButton();
     GetEventGenerator()->GestureTapAt(
@@ -3898,7 +3902,8 @@ TEST_F(HotseatShelfLayoutManagerTest,
       AshTestBase::CreateTestWindow(gfx::Rect(0, 0, 400, 400));
   wm::ActivateWindow(window.get());
   {
-    HotseatStateWatcher watcher(GetShelfLayoutManager());
+    HotseatStateWatcher watcher(
+        GetPrimaryShelf()->shelf_widget()->hotseat_widget());
     // Enter overview by using the controller.
     OverviewAnimationWaiter waiter;
     Shell::Get()->overview_controller()->StartOverview();
@@ -3918,7 +3923,8 @@ TEST_F(HotseatShelfLayoutManagerTest,
             GetShelfLayoutManager()->auto_hide_state());
   SwipeUpOnShelf();
   {
-    HotseatStateWatcher watcher(GetShelfLayoutManager());
+    HotseatStateWatcher watcher(
+        GetPrimaryShelf()->shelf_widget()->hotseat_widget());
     // Enter overview by using the controller.
     OverviewAnimationWaiter waiter;
     Shell::Get()->overview_controller()->StartOverview();
