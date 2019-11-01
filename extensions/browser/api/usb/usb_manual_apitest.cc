5270a74cd7094eebdc7dc7b04a84bd3e9087a369
Kevin McNee <mcnee@chromium.org>
2019-11-01 19:51:21 +0000

    Disable UsbManualApiTest.MANUAL_ListInterfaces on windows
    The win7 bots do not seem to recognize the MANUAL_ prefix, so we
    explicitly disable this test as it is being run and is flaking.
    
    Tbr: reillyg@chromium.org
    Bug: 1020591
    Change-Id: I8dea9bc8f0e3ef23bb733c9912e0011c5ac4253e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895798
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711823}

--- extensions/browser/api/usb/usb_manual_apitest.cc
+++ extensions/browser/api/usb/usb_manual_apitest.cc
@@ -3,12 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "chrome/browser/extensions/api/permissions/permissions_api.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 
 using UsbManualApiTest = extensions::ExtensionApiTest;
 
-IN_PROC_BROWSER_TEST_F(UsbManualApiTest, MANUAL_ListInterfaces) {
+// TODO(1020591): The win7 bots do not seem to recognize the MANUAL_ prefix,
+// so we explicitly disable this test.
+#if defined(OS_WIN)
+#define MAYBE_MANUAL_ListInterfaces DISABLED_MANUAL_ListInterfaces
+#else
+#define MAYBE_MANUAL_ListInterfaces MANUAL_ListInterfaces
+#endif
+IN_PROC_BROWSER_TEST_F(UsbManualApiTest, MAYBE_MANUAL_ListInterfaces) {
   extensions::PermissionsRequestFunction::SetIgnoreUserGestureForTests(true);
   extensions::PermissionsRequestFunction::SetAutoConfirmForTests(true);
   ASSERT_TRUE(RunExtensionTest("usb_manual/list_interfaces"));
