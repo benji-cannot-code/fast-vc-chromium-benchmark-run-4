f9c5614d3be8ff19c841dbaadf11af2a67c32e0b
Pavel Yatsuk <pavely@google.com>
2019-11-01 01:04:55 +0000

    [GID] Adjust IdentityDisc size depending on orientation
    The size of Identity Disc was decided once based on visibility of bottom
    toolbar. Toolbar can be shown/hidden based on experiment combination
    (enabled duet, disabled tab grid layout) and device orientation.
    
    This CL adds support for adjusting IDentityDisc size. Images are
    retrieved on demand. Images for both sizes are cached since the user is
    likely to rotate the device multiple times.
    
    R=twellington@chromium.org
    
    Bug: 971711
    Change-Id: I456168b7e1fe9956691a344e162dc9c4455e521a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1877189
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711562}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -1272,6 +1272,7 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
             if (mAppMenuButtonHelper != null) {
                 mAppMenuButtonHelper.setMenuShowsFromBottom(mIsBottomToolbarVisible);
             }
+            mIdentityDiscController.updateButtonState();
         }
     }
 
