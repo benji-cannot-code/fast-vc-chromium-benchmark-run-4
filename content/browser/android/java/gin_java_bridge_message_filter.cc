a56d0ce6d57124366622425b7888a7f9dbca3e10
Gabriel Charette <gab@chromium.org>
2019-11-01 19:42:12 +0000

    Migrate off BrowserThread::DeleteSoon in /content/browser/android/java
    Instead use base::DeleteSoon which enforces usage of TaskTraits.
    
    This CL is a no-op as-is. It was recently discovered however that some
    callers did BrowserThread::DeleteSoon() with pending tasks running on
    different task queues (different traits  -- e.g.
    
    TaskTraits to make this more obvious. Please review whether calls in
    this CL can be migrated as-is or need additional traits to match
    potentially pending tasks.
    
    Split from https://chromium-review.googlesource.com/c/chromium/src/+/1894109
    for cursory review.
    
    This CL was uploaded by git cl split.
    
    R=torne@chromium.org
    
    
    TaskPriority: :BEST_EFFORT) which can result in out-of-order deletion...
    BrowserThread: :DeleteSoon() is being migrated to base::DeleteSoon() w/
    Bug: 1019767
    Change-Id: I39d0a74b1bec2148a9dec777ace504bb0a1de4d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895642
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711819}

--- content/browser/android/java/gin_java_bridge_message_filter.cc
+++ content/browser/android/java/gin_java_bridge_message_filter.cc
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/android/java/gin_java_bridge_message_filter.h"
 
 #include "base/auto_reset.h"
+#include "base/task/post_task.h"
 #include "build/build_config.h"
 #include "content/browser/android/java/gin_java_bridge_dispatcher_host.h"
 #include "content/browser/android/java/java_bridge_thread.h"
 #include "content/common/gin_java_bridge_messages.h"
+#include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
@@ -39,7 +41,7 @@ void GinJavaBridgeMessageFilter::OnDestruct() const {
   if (BrowserThread::CurrentlyOn(BrowserThread::UI)) {
     delete this;
   } else {
-    BrowserThread::DeleteSoon(BrowserThread::UI, FROM_HERE, this);
+    base::DeleteSoon(FROM_HERE, {BrowserThread::UI}, this);
   }
 }
 
