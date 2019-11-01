098bc0111a3b1b908db082919d06e8aa9fd75028
Yue Zhang <yuezhanggg@chromium.org>
2019-11-01 17:37:09 +0000

    Enable drag-and-drop for StartSurface variations without MRU ordering
    Bug: 1005931
    Change-Id: Ie56ba62a695b9895fdd5b67ef9547aa8c59bc1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893507
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711758}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
@@ -134,7 +134,7 @@ public class TabSwitcherCoordinator implements Destroyable, TabSwitcher,
 
         if (FeatureUtilities.isTabGroupsAndroidUiImprovementsEnabled()
                 && mode == TabListCoordinator.TabListMode.GRID
-                && !FeatureUtilities.isStartSurfaceEnabled()) {
+                && !TabSwitcherMediator.isShowingTabsInMRUOrder()) {
             mTabGridIphItemCoordinator = new TabGridIphItemCoordinator(
                     context, mTabListCoordinator.getContainerView(), container);
             mMediator.setIphProvider(mTabGridIphItemCoordinator.getIphProvider());
