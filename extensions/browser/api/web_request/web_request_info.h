378dc283b2cef078b49b8a6e099b5d338d2abe5f
Lucas Furukawa Gadani <lfg@chromium.org>
2019-11-01 21:25:47 +0000

    Reland "Convert extensions to use the URLResponseHead mojom types."
    This is a reland of 38f657d5aed609ef4f834941fc47932dd110c009
    
    Original change's description:
    > Convert extensions to use the URLResponseHead mojom types.
    > 
    > Bug: 984550
    > Change-Id: I885fc575b49c4049b83b17cc4c2248daf2525b4e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1876974
    > Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    > Commit-Queue: Lucas Gadani <lfg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#709596}
    
    Bug: 984550
    Change-Id: I7876e758de65f6a5e0ff2af5954c738fa951343f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1891149
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711877}

--- extensions/browser/api/web_request/web_request_info.h
+++ extensions/browser/api/web_request/web_request_info.h
@@ -23,12 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_message.h"
 #include "net/http/http_request_headers.h"
 #include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/mojom/url_response_head.mojom-forward.h"
 #include "url/gurl.h"
 #include "url/origin.h"
-
-namespace network {
-struct ResourceResponseHead;
-}
 
 namespace extensions {
 
@@ -92,7 +89,7 @@ struct WebRequestInfo {
 
   // Fill in response data for this request.
   void AddResponseInfoFromResourceResponse(
-      const network::ResourceResponseHead& response);
+      const network::mojom::URLResponseHead& response);
 
   // A unique identifier for this request.
   const uint64_t id;
