b53eb7d586a44c592a9187f320c42b6a01f48d39
Greg Thompson <grt@chromium.org>
2019-11-01 20:45:25 +0000

    Remove dead code from TestBlinkWebUnitTestSupport.
    R=sky@chromium.org
    
    
    Bug: 546640
    Change-Id: I597edb708f20b74b54769dee7eaf813606154e17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895656
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711848}

--- content/test/test_blink_web_unit_test_support.h
+++ content/test/test_blink_web_unit_test_support.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/compiler_specific.h"
-#include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
@@ -74,7 +73,6 @@ class TestBlinkWebUnitTestSupport : public BlinkPlatformImpl {
   void BindClipboardHost(mojo::ScopedMessagePipeHandle handle);
 
   std::unique_ptr<MockClipboardHost> mock_clipboard_host_;
-  base::ScopedTempDir file_system_root_;
   std::unique_ptr<blink::WebURLLoaderMockFactory> url_loader_factory_;
   std::unique_ptr<blink::scheduler::WebThreadScheduler> main_thread_scheduler_;
   bool threaded_animation_ = true;
