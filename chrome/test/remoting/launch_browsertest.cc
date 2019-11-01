492c2cdc874b4b21f3e6f39f699e8c9a6ac42724
Kevin McNee <mcnee@chromium.org>
2019-11-01 18:55:55 +0000

    Disable MANUAL RemoteDesktopBrowserTests on windows
    The win7 bots do not seem to recognize the MANUAL_ prefix, so we
    explicitly disable these tests as they are being run and are flaking.
    
    Tbr: jamiewalch@chromium.org
    Bug: 1020591
    Change-Id: I245e2d8c4fcf59f35698561385cc8cc8a453b807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895780
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711801}

--- chrome/test/remoting/launch_browsertest.cc
+++ chrome/test/remoting/launch_browsertest.cc
@@ -3,11 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
 #include "chrome/test/remoting/remote_desktop_browsertest.h"
 
 namespace remoting {
 
-IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MANUAL_Launch) {
+// TODO(1020591): The win7 bots do not seem to recognize the MANUAL_ prefix,
+// so we explicitly disable this test.
+#if defined(OS_WIN)
+#define MAYBE_MANUAL_Launch DISABLED_MANUAL_Launch
+#else
+#define MAYBE_MANUAL_Launch MANUAL_Launch
+#endif
+IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MAYBE_MANUAL_Launch) {
   VerifyInternetAccess();
 
   Install();
@@ -17,7 +25,15 @@ IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MANUAL_Launch) {
   Cleanup();
 }
 
-IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MANUAL_LaunchDeferredStart) {
+// TODO(1020591): The win7 bots do not seem to recognize the MANUAL_ prefix,
+// so we explicitly disable this test.
+#if defined(OS_WIN)
+#define MAYBE_MANUAL_LaunchDeferredStart DISABLED_MANUAL_LaunchDeferredStart
+#else
+#define MAYBE_MANUAL_LaunchDeferredStart MANUAL_LaunchDeferredStart
+#endif
+IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest,
+                       MAYBE_MANUAL_LaunchDeferredStart) {
   VerifyInternetAccess();
 
   Install();
--- chrome/test/remoting/pin_browsertest.cc
+++ chrome/test/remoting/pin_browsertest.cc
@@ -5,12 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "build/build_config.h"
 #include "chrome/test/remoting/remote_desktop_browsertest.h"
 #include "chrome/test/remoting/waiter.h"
 
 namespace remoting {
 
-IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MANUAL_Cancel_PIN) {
+// TODO(1020591): The win7 bots do not seem to recognize the MANUAL_ prefix,
+// so we explicitly disable this test.
+#if defined(OS_WIN)
+#define MAYBE_MANUAL_Cancel_PIN DISABLED_MANUAL_Cancel_PIN
+#else
+#define MAYBE_MANUAL_Cancel_PIN MANUAL_Cancel_PIN
+#endif
+IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MAYBE_MANUAL_Cancel_PIN) {
   content::WebContents* content = SetUpTest();
   LoadScript(content, FILE_PATH_LITERAL("cancel_pin_browser_test.js"));
 
@@ -21,7 +29,14 @@ IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MANUAL_Cancel_PIN) {
   Cleanup();
 }
 
-IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MANUAL_Invalid_PIN) {
+// TODO(1020591): The win7 bots do not seem to recognize the MANUAL_ prefix,
+// so we explicitly disable this test.
+#if defined(OS_WIN)
+#define MAYBE_MANUAL_Invalid_PIN DISABLED_MANUAL_Invalid_PIN
+#else
+#define MAYBE_MANUAL_Invalid_PIN MANUAL_Invalid_PIN
+#endif
+IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MAYBE_MANUAL_Invalid_PIN) {
   content::WebContents* content = SetUpTest();
   LoadScript(content, FILE_PATH_LITERAL("invalid_pin_browser_test.js"));
 
@@ -33,7 +48,14 @@ IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MANUAL_Invalid_PIN) {
   Cleanup();
 }
 
-IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MANUAL_Update_PIN) {
+// TODO(1020591): The win7 bots do not seem to recognize the MANUAL_ prefix,
+// so we explicitly disable this test.
+#if defined(OS_WIN)
+#define MAYBE_MANUAL_Update_PIN DISABLED_MANUAL_Update_PIN
+#else
+#define MAYBE_MANUAL_Update_PIN MANUAL_Update_PIN
+#endif
+IN_PROC_BROWSER_TEST_F(RemoteDesktopBrowserTest, MAYBE_MANUAL_Update_PIN) {
   content::WebContents* content = SetUpTest();
   LoadScript(content, FILE_PATH_LITERAL("update_pin_browser_test.js"));
 
