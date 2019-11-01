f83c8d074d8d66c531bf15ad48351d1bebc738ce
Henrique Nakashima <hnakashima@chromium.org>
2019-11-01 17:16:55 +0000

    Provide SharedPreferencesManager through dagger.
    This is needed for it to replace ChromePreferenceManager.
    
    Bug: 1017800
    Change-Id: I45154b8bdfdfc0cb4816739270a42c303016d5b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894184
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711749}

--- chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeAppModule.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeAppModule.java
@@ -10,6 +10,8 @@ import static org.chromium.chrome.browser.dependency_injection.ChromeCommonQuali
 
 import android.content.Context;
 
+import androidx.browser.trusted.TrustedWebActivityServiceConnectionManager;
+
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.WarmupManager;
 import org.chromium.chrome.browser.browserservices.permissiondelegation.TrustedWebActivityPermissionStore;
@@ -17,13 +19,13 @@ import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
 import org.chromium.chrome.browser.night_mode.SystemNightModeMonitor;
 import org.chromium.chrome.browser.notifications.channels.SiteChannelsManager;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
+import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.webapps.WebappRegistry;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import androidx.browser.trusted.TrustedWebActivityServiceConnectionManager;
 import dagger.Module;
 import dagger.Provides;
 
@@ -43,6 +45,11 @@ public class ChromeAppModule {
     @Provides
     public ChromePreferenceManager providesChromePreferenceManager() {
         return ChromePreferenceManager.getInstance();
+    }
+
+    @Provides
+    public SharedPreferencesManager providesSharedPreferencesManager() {
+        return SharedPreferencesManager.getInstance();
     }
 
     @Provides
