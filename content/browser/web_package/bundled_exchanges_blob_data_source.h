a026dbc984dac0b2b8b44507b30aff9bf1b689f5
Tsuyoshi Horo <horo@chromium.org>
2019-11-01 08:17:24 +0000

    Use DeleteSoon to delete BundledExchangesBlobDataSource's Core
    Bug: 1018640
    Change-Id: Ifeed5db772abface2d6ed9a6b12c3f6e0a8481e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893127
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711653}

--- content/browser/web_package/bundled_exchanges_blob_data_source.h
+++ content/browser/web_package/bundled_exchanges_blob_data_source.h
@@ -117,8 +117,6 @@ class CONTENT_EXPORT BundledExchangesBlobDataSource {
       base::WeakPtr<BlobDataSourceCore> weak_core,
       std::unique_ptr<BlobDataSourceCore> core);
 
-  static void DeleteCore(std::unique_ptr<BlobDataSourceCore> core);
-
   void WaitForCore(base::OnceClosure callback);
 
   void ReadToDataPipeImpl(uint64_t offset,
