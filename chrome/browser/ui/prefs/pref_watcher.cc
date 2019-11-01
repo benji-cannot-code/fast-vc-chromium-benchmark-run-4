da38bc04f4330b51b2faac9cd05fa5760c565725
Alice Boxhall <aboxhall@chromium.org>
2019-11-01 01:55:41 +0000

    Always match :focus-visible on focused element according to user preference.
    The :focus-visible pseudo-selector emulates the existing behaviour of native
    elements like buttons by only showing a focus ring when focus is moved using
    the keyboard.
    
    This change modifies that behaviour to always match :focus-visible on the
    focused element, regardless of how focus was moved (effectively making
    :focus-visible equivalent to :focus) if the user has enabled the "Highlight the
    object with keyboard focus when it changes" setting on Chrome OS.
    
    This plumbs through the ash::prefs::kAccessibilityFocusHighlightEnabled
    preference on Chrome OS to a new web setting on Blink, and checks that setting
    when computing whether :focus-visible matches on the focused element.
    
    Bug: 817199
    Change-Id: I072e24eba525e5f62507fb5ab7a369d618067c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1886075
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711597}

--- chrome/browser/ui/prefs/pref_watcher.cc
+++ chrome/browser/ui/prefs/pref_watcher.cc
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/language/core/browser/pref_names.h"
 #include "third_party/blink/public/mojom/renderer_preferences.mojom.h"
 
+#if defined(OS_CHROMEOS)
+#include "ash/public/cpp/ash_pref_names.h"
+#endif
+
 namespace {
 
 // The list of prefs we want to observe.
@@ -52,6 +56,9 @@ const char* const kWebPrefsToObserve[] = {
     prefs::kWebkitTabsToLinks,
     prefs::kWebKitTextAreasAreResizable,
     prefs::kWebKitWebSecurityEnabled,
+#if defined(OS_CHROMEOS)
+    ash::prefs::kAccessibilityFocusHighlightEnabled,
+#endif
 };
 
 const int kWebPrefsToObserveLength = base::size(kWebPrefsToObserve);
