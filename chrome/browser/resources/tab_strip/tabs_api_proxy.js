2df67afefad3fe52761000b606f20364291c2397
John Lee <johntlee@chromium.org>
2019-11-01 02:34:15 +0000

    [Reland] WebUI Tab Strip: Only track thumbnails for visible tabs
    - Track thumbnails only if they are currently in view or are
    just a standard finger swipe away.
    - Do not track thumbnails for pinned tabs.
    - Do not track thumbnails when the tab strip is closed.
    
    Note that this CL does not implement actually untracking thumbnails.
    It merely calls the method in the C++ side that will eventually do it.
    
    Bug: 1015132
    Change-Id: I16cce4a71ad3e6e887555be27e1525cdef9beb33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894245
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711609}

--- chrome/browser/resources/tab_strip/tabs_api_proxy.js
+++ chrome/browser/resources/tab_strip/tabs_api_proxy.js
@@ -101,9 +101,10 @@ export class TabsApiProxy {
 
   /**
    * @param {number} tabId
+   * @param {boolean} thumbnailTracked
    */
-  trackThumbnailForTab(tabId) {
-    chrome.send('addTrackedTab', [tabId]);
+  setThumbnailTracked(tabId, thumbnailTracked) {
+    chrome.send('setThumbnailTracked', [tabId, thumbnailTracked]);
   }
 }
 
--- chrome/test/data/webui/tab_strip/test_tabs_api_proxy.js
+++ chrome/test/data/webui/tab_strip/test_tabs_api_proxy.js
@@ -12,7 +12,7 @@ export class TestTabsApiProxy extends TestBrowserProxy {
       'closeTab',
       'getTabs',
       'moveTab',
-      'trackThumbnailForTab',
+      'setThumbnailTracked',
     ]);
 
     this.tabs_;
@@ -42,7 +42,7 @@ export class TestTabsApiProxy extends TestBrowserProxy {
     this.tabs_ = tabs;
   }
 
-  trackThumbnailForTab(tabId) {
-    this.methodCalled('trackThumbnailForTab', tabId);
+  setThumbnailTracked(tabId, thumbnailTracked) {
+    this.methodCalled('setThumbnailTracked', [tabId, thumbnailTracked]);
   }
 }
