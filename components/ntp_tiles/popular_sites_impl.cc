7ac88497dc1916e60143a7bc8ac8868eeca17824
Lei Zhang <thestig@chromium.org>
2019-11-01 20:49:42 +0000

    Fix nits in components/ntp_tiles/popular_sites_impl.cc.
    - Don't use ntp_tiles:: inside namespace ntp_tiles.
    - Consistently drop curly braces for 1 line if-statements.
    
    Change-Id: I67706595e1ccd13c0a059e6297c854a838d12f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895790
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711855}

--- components/ntp_tiles/popular_sites_impl.cc
+++ components/ntp_tiles/popular_sites_impl.cc
@@ -78,8 +78,7 @@ std::string GetDefaultSearchEngineCountryCode(
   DCHECK(template_url_service);
 
   base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
-  if (!cmd_line->HasSwitch(
-          ntp_tiles::switches::kEnableNTPSearchEngineCountryDetection))
+  if (!cmd_line->HasSwitch(switches::kEnableNTPSearchEngineCountryDetection))
     return std::string();
 
   const TemplateURL* default_provider =
@@ -178,13 +177,11 @@ std::map<SectionType, PopularSites::SitesVector> ParseVersion6OrAbove(
     // Non-personalized site exploration tiles are no longer supported, so
     // ignore all other section types.
     SectionType section_type = static_cast<SectionType>(section);
-    if (section_type != SectionType::PERSONALIZED) {
+    if (section_type != SectionType::PERSONALIZED)
       continue;
-    }
     const base::ListValue* sites_list;
-    if (!item->GetList("sites", &sites_list)) {
+    if (!item->GetList("sites", &sites_list))
       continue;
-    }
     sections[section_type] = ParseSiteList(*sites_list);
   }
   return sections;
@@ -193,9 +190,8 @@ std::map<SectionType, PopularSites::SitesVector> ParseVersion6OrAbove(
 std::map<SectionType, PopularSites::SitesVector> ParseSites(
     const base::ListValue& list,
     int version) {
-  if (version >= kSitesExplorationStartVersion) {
+  if (version >= kSitesExplorationStartVersion)
     return ParseVersion6OrAbove(list);
-  }
   return ParseVersion5(list);
 }
 
@@ -205,9 +201,9 @@ void SetDefaultResourceForSite(int index,
                                int resource_id,
                                base::ListValue* sites) {
   base::DictionaryValue* site;
-  if (!sites->GetDictionary(index, &site)) {
+  if (!sites->GetDictionary(index, &site))
     return;
-  }
+
   site->SetInteger("default_icon_resource", resource_id);
 }
 #endif
@@ -217,9 +213,9 @@ base::Value DefaultPopularSites() {
 #if !defined(OS_ANDROID) && !defined(OS_IOS)
   return base::Value(base::Value::Type::LIST);
 #else
-  if (!base::FeatureList::IsEnabled(kPopularSitesBakedInContentFeature)) {
+  if (!base::FeatureList::IsEnabled(kPopularSitesBakedInContentFeature))
     return base::Value(base::Value::Type::LIST);
-  }
+
   std::unique_ptr<base::ListValue> sites =
       base::ListValue::From(base::JSONReader::ReadDeprecated(
           ui::ResourceBundle::GetSharedInstance().LoadDataResourceString(
@@ -331,7 +327,7 @@ GURL PopularSitesImpl::GetURLToFetch() {
   }
 
   const GURL override_url =
-      GURL(prefs_->GetString(ntp_tiles::prefs::kPopularSitesOverrideURL));
+      GURL(prefs_->GetString(prefs::kPopularSitesOverrideURL));
   return override_url.is_valid()
              ? override_url
              : GetPopularSitesURL(directory, country, version);
@@ -339,7 +335,7 @@ GURL PopularSitesImpl::GetURLToFetch() {
 
 std::string PopularSitesImpl::GetDirectoryToFetch() {
   std::string directory =
-      prefs_->GetString(ntp_tiles::prefs::kPopularSitesOverrideDirectory);
+      prefs_->GetString(prefs::kPopularSitesOverrideDirectory);
 
   if (directory.empty())
     directory = GetVariationDirectory();
@@ -359,7 +355,7 @@ std::string PopularSitesImpl::GetDirectoryToFetch() {
 // - A default fallback.
 std::string PopularSitesImpl::GetCountryToFetch() {
   std::string country_code =
-      prefs_->GetString(ntp_tiles::prefs::kPopularSitesOverrideCountry);
+      prefs_->GetString(prefs::kPopularSitesOverrideCountry);
 
   if (country_code.empty())
     country_code = GetVariationCountry();
@@ -386,8 +382,7 @@ std::string PopularSitesImpl::GetCountryToFetch() {
 // - The version from the field trial config (variation parameter).
 // - A default fallback.
 std::string PopularSitesImpl::GetVersionToFetch() {
-  std::string version =
-      prefs_->GetString(ntp_tiles::prefs::kPopularSitesOverrideVersion);
+  std::string version = prefs_->GetString(prefs::kPopularSitesOverrideVersion);
 
   if (version.empty())
     version = GetVariationVersion();
@@ -405,13 +400,13 @@ const base::ListValue* PopularSitesImpl::GetCachedJson() {
 // static
 void PopularSitesImpl::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* user_prefs) {
-  user_prefs->RegisterStringPref(ntp_tiles::prefs::kPopularSitesOverrideURL,
+  user_prefs->RegisterStringPref(prefs::kPopularSitesOverrideURL,
                                  std::string());
-  user_prefs->RegisterStringPref(
-      ntp_tiles::prefs::kPopularSitesOverrideDirectory, std::string());
-  user_prefs->RegisterStringPref(ntp_tiles::prefs::kPopularSitesOverrideCountry,
+  user_prefs->RegisterStringPref(prefs::kPopularSitesOverrideDirectory,
                                  std::string());
-  user_prefs->RegisterStringPref(ntp_tiles::prefs::kPopularSitesOverrideVersion,
+  user_prefs->RegisterStringPref(prefs::kPopularSitesOverrideCountry,
+                                 std::string());
+  user_prefs->RegisterStringPref(prefs::kPopularSitesOverrideVersion,
                                  std::string());
 
   user_prefs->RegisterInt64Pref(prefs::kPopularSitesLastDownloadPref, 0);
