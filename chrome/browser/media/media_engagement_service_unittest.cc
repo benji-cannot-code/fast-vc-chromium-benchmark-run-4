8151404d6b36d9ce701553607ce1191726efaf74
Greg Thompson <grt@chromium.org>
2019-11-01 20:15:42 +0000

    Fix file leaks in MediaEngagementServiceTest.CleanUpDatabaseWhenHistoryIsExpired.
    During service teardown, tasks are posted to the TestMockTimeTaskRunner
    to close open files. These must be run during TearDown so that files can
    be closed, thereby allowing the ScopedTempDir to clean itself.
    
    Bug: 546640
    Change-Id: Ibdfa0cc63f79f27237040765777fed344f7ad7b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895337
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711836}

--- chrome/browser/media/media_engagement_service_unittest.cc
+++ chrome/browser/media/media_engagement_service_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
@@ -177,6 +178,11 @@ class MediaEngagementServiceTest : public ChromeRenderViewHostTestHarness,
   void TearDown() override {
     service_->Shutdown();
     ChromeRenderViewHostTestHarness::TearDown();
+
+    // Tests that run a history service that uses the mock task runner for
+    // backend processing will post tasks there during TearDown. Run them now to
+    // avoid leaks.
+    mock_time_task_runner_->RunUntilIdle();
     service_.reset();
   }
 
