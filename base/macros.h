3eccb7363d90c07d1ace4c28106bfe5275fe5c8e
Peter Kasting <pkasting@chromium.org>
2019-11-01 22:26:01 +0000

    Deprecate DISALLOW_xxx and add Dos-And-Don'ts guidance around it.
    Bug: 1010217
    Change-Id: Iaa1d73bfec3522db08d04bedeb59603e16c8ffe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895857
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711900}

--- base/macros.h
+++ base/macros.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_MACROS_H_
 #define BASE_MACROS_H_
 
+// ALL DISALLOW_xxx MACROS ARE DEPRECATED; DO NOT USE IN NEW CODE.
+// Use explicit deletions instead.  See the section on copyability/movability in
+// //styleguide/c++/c++-dos-and-donts.md for more information.
+
 // Put this in the declarations for a class to be uncopyable.
 #define DISALLOW_COPY(TypeName) \
   TypeName(const TypeName&) = delete
