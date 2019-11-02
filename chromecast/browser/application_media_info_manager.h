5b1629b5f5c0e5fd3378ee605f0ff6c3048e1301
Miyoung Shin <myid.shin@igalia.com>
2019-11-02 01:08:40 +0000

    Convert CastApplicationMediaInfoManager to new Mojo types
    This CL converts CastApplicationMediaInfoManager{Ptr, Request} in
    chromecast to the new Mojo type
    
    Bug: 955171
    Change-Id: I3b60c6e76e166ede239df86359959a4523f60d75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1871879
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711961}

--- chromecast/browser/application_media_info_manager.h
+++ chromecast/browser/application_media_info_manager.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "content/public/browser/frame_service_base.h"
 #include "media/mojo/mojom/cast_application_media_info_manager.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 
 namespace content {
 class RenderFrameHost;
@@ -30,7 +31,8 @@ class ApplicationMediaInfoManager
  public:
   ApplicationMediaInfoManager(
       content::RenderFrameHost* render_frame_host,
-      ::media::mojom::CastApplicationMediaInfoManagerRequest request,
+      mojo::PendingReceiver<::media::mojom::CastApplicationMediaInfoManager>
+          receiver,
       std::string application_session_id,
       bool mixer_audio_enabled);
   ~ApplicationMediaInfoManager() override;
@@ -56,7 +58,8 @@ void CreateApplicationMediaInfoManager(
     content::RenderFrameHost* render_frame_host,
     std::string application_session_id,
     bool mixer_audio_enabled,
-    ::media::mojom::CastApplicationMediaInfoManagerRequest request);
+    mojo::PendingReceiver<::media::mojom::CastApplicationMediaInfoManager>
+        receiver);
 
 }  // namespace media
 }  // namespace chromecast
