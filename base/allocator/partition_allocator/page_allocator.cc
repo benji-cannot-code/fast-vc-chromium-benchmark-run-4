1c2e23d6a783b435e26c24ecd8457c0a53491b43
Wez <wez@chromium.org>
2019-11-02 01:07:31 +0000

    [Fuchsia] Implement PageAllocator APIs using VMOs instead of mmap().
    Memory is implicitly allocated/deallocated in response to mmap() and
    munmap() calls when working with MAP_ANONYMOUS mmaps.
    
    Fuchsia's Zircon kernel separates reservation of address space through
    Virtual Memory Address Ranges (VMARs) from allocation of pages through
    Virtual Memory Objects (VMOs).
    
    This implementation creates and maps a new VMO for each call to
    SystemAllocPagesInternal(). Each of these VMOs has is named based on
    the PageTag of the subsystem (e.g. V8, Blink) making the allocation,
    making it easier to identify which subsystems are consuming memory.
    
    DecommitSystemPagesInternal() will be implemented once Fuchsia provides
    the vmar_op_range(DECOMMIT) API.
    
    Bug: 927411
    Change-Id: Ie7966f9dcf487e308326e940ca3941c0f87487f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1859677
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#711960}

--- base/allocator/partition_allocator/page_allocator.cc
+++ base/allocator/partition_allocator/page_allocator.cc
@@ -24,8 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "base/allocator/partition_allocator/page_allocator_internals_win.h"
-#elif defined(OS_POSIX) || defined(OS_FUCHSIA)
+#elif defined(OS_POSIX)
 #include "base/allocator/partition_allocator/page_allocator_internals_posix.h"
+#elif defined(OS_FUCHSIA)
+#include "base/allocator/partition_allocator/page_allocator_internals_fuchsia.h"
 #else
 #error Platform not supported.
 #endif
