074126bdce73707c00712e31b88015fd2bf4b863
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-11-02 10:26:26 +0000

    Use new mojo types for service_manager.mojom
    This CL converts ServiceManagerPtr and port depending files to adapt to
    new mojo types.
    
    Bug: 955171, 978694
    Change-Id: I8391cdf9995d2395776e115baeaa2d6315fc1183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893592
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#711995}

--- services/service_manager/tests/lifecycle/lifecycle_unittest.cc
+++ services/service_manager/tests/lifecycle/lifecycle_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
 #include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "services/service_manager/public/cpp/constants.h"
 #include "services/service_manager/public/cpp/identity.h"
 #include "services/service_manager/public/cpp/manifest.h"
@@ -244,9 +245,9 @@ class LifecycleTest : public testing::Test {
 
  private:
   std::unique_ptr<InstanceState> TrackInstances() {
-    mojom::ServiceManagerPtr service_manager;
-    connector()->BindInterface(service_manager::mojom::kServiceName,
-                               &service_manager);
+    mojo::Remote<mojom::ServiceManager> service_manager;
+    connector()->Connect(service_manager::mojom::kServiceName,
+                         service_manager.BindNewPipeAndPassReceiver());
     mojo::PendingRemote<mojom::ServiceManagerListener> listener;
     base::RunLoop loop;
     InstanceState* state = new InstanceState(
