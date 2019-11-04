65bc6b42cd36561e60cb3ae34a9dae9b3a29b819
Gabriel Charette <gab@chromium.org>
2019-11-04 11:33:29 +0000

    Migrate off BrowserThread::DeleteSoon in /chrome/browser/renderer_host/chrome_extension_message_filter.cc
    Instead use base::DeleteSoon which enforces usage of TaskTraits.
    
    This CL is a no-op as-is. It was recently discovered however that some
    callers did BrowserThread::DeleteSoon() with pending tasks running on
    different task queues (different traits  -- e.g.
    
    TaskTraits to make this more obvious. Please review whether calls in
    this CL can be migrated as-is or need additional traits to match
    potentially pending tasks.
    
    Split from https://chromium-review.googlesource.com/c/chromium/src/+/1894109
    for cursory review.
    
    This CL was uploaded by git cl split.
    
    R=finnur@chromium.org
    
    
    TaskPriority: :BEST_EFFORT) which can result in out-of-order deletion...
    BrowserThread: :DeleteSoon() is being migrated to base::DeleteSoon() w/
    Bug: 1019767
    Change-Id: I826eae60256fc7c8ac5152afc9bfac6a963c7974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894888
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712073}

--- chrome/browser/renderer_host/chrome_extension_message_filter.cc
+++ chrome/browser/renderer_host/chrome_extension_message_filter.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/activity_log_private/activity_log_private_api.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/render_process_host.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
@@ -109,7 +110,7 @@ void ChromeExtensionMessageFilter::OnDestruct() const {
   if (BrowserThread::CurrentlyOn(BrowserThread::UI)) {
     delete this;
   } else {
-    BrowserThread::DeleteSoon(BrowserThread::UI, FROM_HERE, this);
+    base::DeleteSoon(FROM_HERE, {BrowserThread::UI}, this);
   }
 }
 
