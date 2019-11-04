795324c0adc6819ffaf05e8a9899ac9c17ae8338
Nate Fischer <ntfschr@chromium.org>
2019-11-04 22:05:18 +0000

    AW: rename file-scheme-cookie methods
    No change to logic. This renames android_webview::CookieManager methods
    for consistency:
    
    * Use "allow" instead of "accept," for consistency with the
      network::CookieManager
    * Use "get" and "set" prefixes for clarity
    
    This lets the Java layer be an adapter between the frameworks API method
    names and the C++ method names.
    
    This also adds documentation for SetAllowFileSchemeCookies, explaining
    why it must be synchronous and what exactly the method guarantees.
    
    Fixed: 1020604
    Fixed: 1020607
    Test: autoninja -C out/Default system_webview_apk (it compiles)
    Change-Id: If67f226ad771f6fff298b370d4da47770410efa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1898388
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712285}

--- android_webview/java/src/org/chromium/android_webview/AwCookieManager.java
+++ android_webview/java/src/org/chromium/android_webview/AwCookieManager.java
@@ -160,7 +160,7 @@ public final class AwCookieManager {
      * Whether cookies are accepted for file scheme URLs.
      */
     public boolean allowFileSchemeCookies() {
-        return AwCookieManagerJni.get().allowFileSchemeCookies(
+        return AwCookieManagerJni.get().getAllowFileSchemeCookies(
                 mNativeCookieManager, AwCookieManager.this);
     }
 
@@ -174,7 +174,7 @@ public final class AwCookieManager {
      * instance has been created.
      */
     public void setAcceptFileSchemeCookies(boolean accept) {
-        AwCookieManagerJni.get().setAcceptFileSchemeCookies(
+        AwCookieManagerJni.get().setAllowFileSchemeCookies(
                 mNativeCookieManager, AwCookieManager.this, accept);
     }
 
@@ -268,8 +268,8 @@ public final class AwCookieManager {
         void removeExpiredCookies(long nativeCookieManager, AwCookieManager caller);
         void flushCookieStore(long nativeCookieManager, AwCookieManager caller);
         boolean hasCookies(long nativeCookieManager, AwCookieManager caller);
-        boolean allowFileSchemeCookies(long nativeCookieManager, AwCookieManager caller);
-        void setAcceptFileSchemeCookies(
-                long nativeCookieManager, AwCookieManager caller, boolean accept);
+        boolean getAllowFileSchemeCookies(long nativeCookieManager, AwCookieManager caller);
+        void setAllowFileSchemeCookies(
+                long nativeCookieManager, AwCookieManager caller, boolean allow);
     }
 }
