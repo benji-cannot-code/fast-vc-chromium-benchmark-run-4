6194f696fca40f95237deebb51792858c632dc09
Gabriel Charette <gab@chromium.org>
2019-11-04 14:41:51 +0000

    Migrate off BrowserThread::DeleteSoon in /chrome/browser/browsing_data
    Instead use base::DeleteSoon which enforces usage of TaskTraits.
    
    This CL is a no-op as-is. It was recently discovered however that some
    callers did BrowserThread::DeleteSoon() with pending tasks running on
    different task queues (different traits  -- e.g.
    
    TaskTraits to make this more obvious. Please review whether calls in
    this CL can be migrated as-is or need additional traits to match
    potentially pending tasks.
    
    Split from https://chromium-review.googlesource.com/c/chromium/src/+/1894109
    for cursory review.
    
    This CL was uploaded by git cl split.
    
    R=dullweber@chromium.org
    
    
    TaskPriority: :BEST_EFFORT) which can result in out-of-order deletion...
    BrowserThread: :DeleteSoon() is being migrated to base::DeleteSoon() w/
    Bug: 1019767
    Change-Id: Ida079509147e988cddc134174315d8d24000a73f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895635
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712101}

--- chrome/browser/browsing_data/browsing_data_quota_helper.cc
+++ chrome/browser/browsing_data/browsing_data_quota_helper.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browsing_data/browsing_data_quota_helper.h"
 
 #include "base/location.h"
+#include "base/task/post_task.h"
+#include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 
 using content::BrowserThread;
@@ -29,7 +31,7 @@ BrowsingDataQuotaHelper::QuotaInfo::~QuotaInfo() {}
 // static
 void BrowsingDataQuotaHelperDeleter::Destruct(
     const BrowsingDataQuotaHelper* helper) {
-  BrowserThread::DeleteSoon(BrowserThread::IO, FROM_HERE, helper);
+  base::DeleteSoon(FROM_HERE, {BrowserThread::IO}, helper);
 }
 
 BrowsingDataQuotaHelper::BrowsingDataQuotaHelper() {}
