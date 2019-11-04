5ae91ab9ff74b918f922159b4ab604dd9ffa78b0
Antonio Gomes <tonikitoo@igalia.com>
2019-11-04 06:04:12 +0000

    Switch RtcDataChannel away from WebString
    ... and use WTF::String instead.
    
    BUG=78725
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I24b5c1c5cee12c82f9ab5f57caccaed502f1336e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1882131
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712050}

--- third_party/blink/renderer/modules/peerconnection/rtc_data_channel.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_data_channel.cc
@@ -250,7 +250,7 @@ RTCDataChannel::~RTCDataChannel() = default;
 
 String RTCDataChannel::label() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  return WebString::FromUTF8(channel()->label());
+  return String::FromUTF8(channel()->label());
 }
 
 bool RTCDataChannel::reliable() const {
@@ -285,7 +285,7 @@ uint16_t RTCDataChannel::maxRetransmits(bool& is_null) const {
 
 String RTCDataChannel::protocol() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  return WebString::FromUTF8(channel()->protocol());
+  return String::FromUTF8(channel()->protocol());
 }
 
 bool RTCDataChannel::negotiated() const {
@@ -360,8 +360,7 @@ void RTCDataChannel::send(const String& data, ExceptionState& exception_state) {
     return;
   }
 
-  std::string utf8_buffer = static_cast<WebString>(data).Utf8();
-  webrtc::DataBuffer data_buffer(utf8_buffer);
+  webrtc::DataBuffer data_buffer(data.Utf8());
   buffered_amount_ += data_buffer.size();
   RecordMessageSent(*channel().get(), data_buffer.size());
   if (!channel()->Send(data_buffer)) {
