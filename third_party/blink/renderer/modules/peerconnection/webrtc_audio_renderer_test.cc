3b344fab3bd8001eb21cf21f35338c1b2b6b79cd
Antonio Gomes <tonikitoo@igalia.com>
2019-11-04 22:35:31 +0000

    Move webrtc_audio_renderer_unittest.cc to Blink
    This is the last step to move content/renderer/media/webrtc to
    Blink.
    The remaining work happens torwards blink-fy the code now.
    
    BUG=787254
    R=guidou@chromium.org
    
    Change-Id: I6529925d68e5bf0c5177fd64ef8591255e0ffca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1890790
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712300}

--- third_party/blink/renderer/modules/peerconnection/webrtc_audio_renderer_test.cc
+++ third_party/blink/renderer/modules/peerconnection/webrtc_audio_renderer_test.cc
@@ -11,10 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/run_loop.h"
-#include "base/test/task_environment.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
-#include "content/renderer/media/audio/audio_device_factory.h"
+#include "media/audio/audio_sink_parameters.h"
+#include "media/audio/audio_source_parameters.h"
 #include "media/base/audio_capturer_source.h"
 #include "media/base/mock_audio_renderer_sink.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -37,7 +36,7 @@ using testing::InvokeWithoutArgs;
 using testing::Return;
 using testing::SaveArg;
 
-namespace content {
+namespace blink {
 
 namespace {
 
@@ -51,10 +50,11 @@ class MockAudioRendererSource : public blink::WebRtcAudioRendererSource {
  public:
   MockAudioRendererSource() {}
   ~MockAudioRendererSource() override {}
-  MOCK_METHOD4(RenderData, void(media::AudioBus* audio_bus,
-                                int sample_rate,
-                                int audio_delay_milliseconds,
-                                base::TimeDelta* current_time));
+  MOCK_METHOD4(RenderData,
+               void(media::AudioBus* audio_bus,
+                    int sample_rate,
+                    int audio_delay_milliseconds,
+                    base::TimeDelta* current_time));
   MOCK_METHOD1(RemoveAudioRenderer, void(blink::WebRtcAudioRenderer* renderer));
   MOCK_METHOD0(AudioRendererThreadStopped, void());
   MOCK_METHOD1(SetOutputDeviceForAec, void(const std::string&));
@@ -186,8 +186,6 @@ class WebRtcAudioRendererTest : public testing::Test {
       audio_device_factory_platform_;
   const base::Optional<base::UnguessableToken> kAudioProcessingId =
       base::UnguessableToken::Create();
-  base::test::SingleThreadTaskEnvironment task_environment_{
-      base::test::SingleThreadTaskEnvironment::MainThreadType::IO};
   std::unique_ptr<MockAudioRendererSource> source_;
   blink::WebMediaStream stream_;
   scoped_refptr<blink::WebRtcAudioRenderer> renderer_;
@@ -229,7 +227,7 @@ TEST_F(WebRtcAudioRendererTest, MultipleRenderers) {
   renderer_proxy_->Stop();
 
   for (int i = 0; i < kNumberOfRendererProxy; ++i) {
-    if (i != kNumberOfRendererProxy -1) {
+    if (i != kNumberOfRendererProxy - 1) {
       EXPECT_CALL(*mock_sink(), Stop()).Times(0);
     } else {
       // When the last proxy is stopped, the sink will stop.
@@ -391,4 +389,4 @@ TEST_F(WebRtcAudioRendererTest, SwitchOutputDeviceStoppedSource) {
   loop.Run();
 }
 
-}  // namespace content
+}  // namespace blink
