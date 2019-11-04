c04ba66e8f9d58992474d6480163ce6666c1e821
Ahmed Fakhry <afakhry@google.com>
2019-11-04 18:00:13 +0000

    Desks: Fix centered-layout wallpapers in the desk mini view.
    The wallpaper paint code of the centered layout used to just
    draw the image with its original size centered in the display
    area. DeskMiniView which uses WallpaperBaseView to draw the
    wallpaper in its small bounds needs to apply a scale to that
    original size of the wallpaper image, to scale it down by the
    same factor it scales down the desk.
    
    Demo: https://bugs.chromium.org/p/chromium/issues/detail?id=992339#c6
    
    BUG=992339
    TEST=Manually, Pick a wallpaper image from the local file system,
         and make its layout centered.
    
    Change-Id: I28e253c8ba48b12a25c83576e41037d1ad24783f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1896245
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712162}

--- ash/wallpaper/wallpaper_base_view.h
+++ ash/wallpaper/wallpaper_base_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WALLPAPER_WALLPAPER_BASE_VIEW_H_
 
 #include "base/macros.h"
+#include "ui/gfx/geometry/vector2d_f.h"
 #include "ui/views/view.h"
 
 namespace ash {
@@ -21,6 +22,10 @@ class WallpaperBaseView : public views::View {
   WallpaperBaseView() = default;
   ~WallpaperBaseView() override = default;
 
+  void set_centered_layout_image_scale(const gfx::Vector2dF& value) {
+    centered_layout_image_scale_ = value;
+  }
+
   // views::View:
   const char* GetClassName() const override;
   void OnPaint(gfx::Canvas* canvas) override;
@@ -33,6 +38,14 @@ class WallpaperBaseView : public views::View {
                              gfx::Canvas* canvas);
 
  private:
+  // Factor by which we scale the size of the wallpaper image when the wallpaper
+  // layout is WALLPAPER_LAYOUT_CENTER. This is used by DeskPreviewView, which
+  // acts as a minified view of the desk, in which case a centered image in the
+  // big bounds of the display, will not look the same way if we don't scale it
+  // down by the same factor by which we scale down the desk in the its
+  // mini_view.
+  gfx::Vector2dF centered_layout_image_scale_ = gfx::Vector2dF(1.0f, 1.0f);
+
   DISALLOW_COPY_AND_ASSIGN(WallpaperBaseView);
 };
 
