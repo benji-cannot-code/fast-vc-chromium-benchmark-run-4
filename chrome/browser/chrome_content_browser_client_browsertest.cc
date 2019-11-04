8fcef60abffce8b3bb3697996f30925254fa2527
Elly Fong-Jones <ellyjones@chromium.org>
2019-11-04 18:53:12 +0000

    mac: register with LaunchServices for protocol handler test
    As of 10.15, an app must be registered with LaunchServices before
    the corresponding bundle ID can become a protocol scheme handler. This
    happens as part of app startup normally, but in tests where there is
    no app, it needs to be done manually. Unfortunately these tests do not
    have a bundle to register, so this change has them try to use the main
    browser bundle when registering.
    
    This is a less evil version of
    https://chromium-review.googlesource.com/c/chromium/src/+/1890960
    
    Bug: 998740
    Change-Id: I96b2d21b1966c304dba3d61e4539308d96221d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893582
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712191}

--- chrome/browser/chrome_content_browser_client_browsertest.cc
+++ chrome/browser/chrome_content_browser_client_browsertest.cc
@@ -71,6 +71,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_constants.h"
 #endif
 
+#if defined(OS_MACOSX)
+#include "chrome/test/base/launchservices_utils_mac.h"
+#endif
+
 namespace content {
 
 namespace {
@@ -910,6 +914,9 @@ class ProtocolHandlerTest : public InProcessBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(ProtocolHandlerTest, CustomHandler) {
+#if defined(OS_MACOSX)
+  ASSERT_TRUE(test::RegisterAppWithLaunchServices());
+#endif
   AddProtocolHandler("news", "https://abc.xyz/?url=%s");
 
   ui_test_utils::NavigateToURL(browser(), GURL("news:something"));
