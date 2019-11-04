a982dbc78f3fd2821f846a3745a714968f348274
David Benjamin <davidben@chromium.org>
2019-11-04 19:28:33 +0000

    Bound the input to gzip_source_stream_fuzzer
    If the input is divided into 1-byte chunks, our existing limits don't
    directly bound things and the PostTask machinery is slow enough with
    sanitizers on that doing it 500,000 times in sequence tends to blow
    through the newly-tightened limit in
    https://chromium-review.googlesource.com/c/chromium/src/+/1797203.
    
    Bug: 1014767
    Change-Id: Iaa047613c1ace18c4639e442d2224c0abf16c741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1898166
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712206}

--- net/filter/gzip_source_stream_fuzzer.cc
+++ net/filter/gzip_source_stream_fuzzer.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fuzzer/FuzzedDataProvider.h>
 
 #include <algorithm>
+#include <memory>
 
-#include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "net/base/io_buffer.h"
 #include "net/base/test_completion_callback.h"
@@ -19,16 +19,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // |data| is used to create a FuzzedSourceStream.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // Bound the input to 256 KiB. If the input is divided into one-byte chunks,
+  // the fuzzer may time out. See https://crbug.com/1014767.
+  size = std::min(size, size_t(256 * 1024));
+
   net::TestCompletionCallback callback;
   FuzzedDataProvider data_provider(data, size);
-  std::unique_ptr<net::FuzzedSourceStream> fuzzed_source_stream(
-      new net::FuzzedSourceStream(&data_provider));
+  auto fuzzed_source_stream =
+      std::make_unique<net::FuzzedSourceStream>(&data_provider);
 
   // Gzip has a maximum compression ratio of 1032x. While, strictly speaking,
-  // linear, this means the fuzzer will often get stuck. Stop reading at a more
-  // modest compression ratio of 2x, or 512 KiB, whichever is larger. See
+  // linear, this means the fuzzer will often get stuck. Bound the output. See
   // https://crbug.com/921075.
-  size_t max_output = std::max(2u * size, static_cast<size_t>(512 * 1024));
+  size_t max_output = 512 * 1024;
 
   const net::SourceStream::SourceType kGzipTypes[] = {
       net::SourceStream::TYPE_GZIP, net::SourceStream::TYPE_DEFLATE};
