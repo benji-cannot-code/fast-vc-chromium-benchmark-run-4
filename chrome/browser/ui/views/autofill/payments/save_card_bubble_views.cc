a8eef17dfa7bf8feab928133e787165121aea7fd
Elly Fong-Jones <ellyjones@chromium.org>
2019-11-04 16:50:54 +0000

    views: add DialogDelegate SetFootnoteView
    This change follows the pattern of the CL adding SetExtraView. The few easy
    overrides are removed in this CL as well, but followup work will be needed to
    tackle the remaining ones individually and eventually get rid of
    CreateFootnoteView.
    
    Bug: 1011446
    Change-Id: I1cb7e2f7bc6ffb1ace067d205475e0f8dee86b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894033
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712135}

--- chrome/browser/ui/views/autofill/payments/save_card_bubble_views.cc
+++ chrome/browser/ui/views/autofill/payments/save_card_bubble_views.cc
@@ -80,10 +80,6 @@ void SaveCardBubbleViews::Hide() {
   CloseBubble();
 }
 
-std::unique_ptr<views::View> SaveCardBubbleViews::CreateFootnoteView() {
-  return nullptr;
-}
-
 bool SaveCardBubbleViews::Accept() {
   if (controller_)
     controller_->OnSaveButton({});
--- chrome/browser/ui/views/autofill/payments/save_card_bubble_views.h
+++ chrome/browser/ui/views/autofill/payments/save_card_bubble_views.h
@@ -40,7 +40,6 @@ class SaveCardBubbleViews : public SaveCardBubbleView,
   void Hide() override;
 
   // views::BubbleDialogDelegateView:
-  std::unique_ptr<views::View> CreateFootnoteView() override;
   bool Accept() override;
   bool Cancel() override;
   bool Close() override;
