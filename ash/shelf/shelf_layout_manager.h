14ebfba4347c59b76cda2dd47a93eb6fc6ca94ce
Andy Paicu <andypaicu@chromium.org>
2019-11-04 15:54:02 +0000

    Revert "CrOS Shelf: Start moving some hotseat logic into the hotseat widget"
    This reverts commit d1531b9ac5a49d19f949e9d8f42daf3470d3132a.
    
    Reason for revert:
    Findit (https://goo.gl/kROfz5) identified CL at revision 711796 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2QxNTMxYjlhYzVhNDlkMTlmOTQ5ZTlkOGY0MmRhZjM0NzBkMzEzMmEM
    
    Sample Failed Build: https://ci.chromium.org/b/8897957936228871696
    
    Sample Failed Step: browser_tests
    
    Original change's description:
    > CrOS Shelf: Start moving some hotseat logic into the hotseat widget
    >
    > The general idea here is to encapsulate more of the hotseat's behavior
    > into its own class rather than have it all over the shelf layout
    > manager. Eventually, I hope to get rid of the huge
    > "CalculateHotseatState" method by setting the hotseat state to the
    > appropriate values from the relevant places.
    >
    > Change-Id: I03bf50bc9f6fb68f7f22f1b6ec966f08f48220da
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893997
    > Commit-Queue: Manu Cornet <manucornet@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#711796}
    
    TBR=xiyuan@chromium.org,manucornet@chromium.org
    
    Change-Id: I7dd2357dd075bee067c3bad1aa58db788ba1039e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893885
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712119}

--- ash/shelf/shelf_layout_manager.h
+++ ash/shelf/shelf_layout_manager.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/wallpaper_controller_observer.h"
 #include "ash/session/session_observer.h"
 #include "ash/shelf/shelf.h"
-#include "ash/shelf/shelf_widget.h"
 #include "ash/shell_observer.h"
 #include "ash/system/locale/locale_update_controller_impl.h"
 #include "ash/wm/desks/desks_controller.h"
@@ -66,7 +65,6 @@ class ASH_EXPORT ShelfLayoutManager : public AppListControllerObserver,
                                       public ShellObserver,
                                       public SplitViewObserver,
                                       public OverviewObserver,
-                                      public HotseatWidget::Observer,
                                       public ::wm::ActivationChangeObserver,
                                       public LockStateObserver,
                                       public WmDefaultLayoutManager,
@@ -122,7 +120,7 @@ class ASH_EXPORT ShelfLayoutManager : public AppListControllerObserver,
   void ProcessGestureEventOfAutoHideShelf(ui::GestureEvent* event,
                                           aura::Window* target);
 
-  // Handles events that are detected while the hotseat is extended in in-app
+  // Handles events that are detected while the hotseat is kExtended in in-app
   // shelf.
   void ProcessGestureEventOfInAppHotseat(ui::GestureEvent* event,
                                          aura::Window* target);
@@ -190,9 +188,6 @@ class ASH_EXPORT ShelfLayoutManager : public AppListControllerObserver,
   void OnOverviewModeEndingAnimationComplete(bool canceled) override;
   void OnOverviewModeEnded() override;
 
-  // HotseatWidget::Observer:
-  void OnHotseatStateChanged() override;
-
   // AppListControllerObserver:
   void OnAppListVisibilityWillChange(bool shown, int64_t display_id) override;
   void OnAppListVisibilityChanged(bool shown, int64_t display_id) override;
@@ -238,9 +233,7 @@ class ASH_EXPORT ShelfLayoutManager : public AppListControllerObserver,
 
   bool updating_bounds() const { return updating_bounds_; }
   ShelfAutoHideState auto_hide_state() const { return state_.auto_hide_state; }
-  HotseatState hotseat_state() const {
-    return shelf_widget_->hotseat_widget()->state();
-  }
+  HotseatState hotseat_state() const { return state_.hotseat_state; }
 
   DragWindowFromShelfController* window_drag_controller_for_testing() {
     return window_drag_controller_.get();
