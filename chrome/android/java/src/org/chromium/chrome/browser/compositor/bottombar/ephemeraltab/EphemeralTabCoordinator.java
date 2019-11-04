ce0e579d9122eebe1d80235c3b9fd693f9a4bb6b
Jinsuk Kim <jinsukkim@chromium.org>
2019-11-04 23:25:13 +0000

    Android: Fix preview tab header click event
    This CL collapses the preview tab, upon a click event on its header,
    down to peek state.
    
    Bug: 1019294
    Change-Id: I6a9c7198b19efef61b311f6c3ba0bb4cbc7a356d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1888854
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712327}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
@@ -27,6 +27,7 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabLaunchType;
 import org.chromium.chrome.browser.ui.widget.RoundedIconGenerator;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetController;
+import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetController.SheetState;
 import org.chromium.chrome.browser.widget.bottomsheet.EmptyBottomSheetObserver;
 import org.chromium.components.embedder_support.view.ContentView;
 import org.chromium.components.security_state.ConnectionSecurityLevel;
@@ -133,8 +134,11 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
     }
 
     private void onToolbarClick() {
-        if (mBottomSheetController.getSheetState() == BottomSheetController.SheetState.PEEK) {
+        int state = mBottomSheetController.getSheetState();
+        if (state == SheetState.PEEK) {
             mBottomSheetController.expandSheet();
+        } else if (state == SheetState.FULL) {
+            mBottomSheetController.peekSheet(true);
         }
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetController.java
@@ -456,7 +456,7 @@ public class BottomSheetController implements Destroyable {
      * Expand the {@link BottomSheet}. If there is no content in the sheet, this is a noop.
      */
     public void expandSheet() {
-        if (mBottomSheet == null) return;
+        if (mBottomSheet == null || mIsSuppressed) return;
 
         if (mBottomSheet.getCurrentSheetContent() == null) return;
         mBottomSheet.setSheetState(SheetState.HALF, true);
@@ -466,6 +466,21 @@ public class BottomSheetController implements Destroyable {
             mOverlayPanelManager.get().getActivePanel().closePanel(
                     OverlayPanel.StateChangeReason.UNKNOWN, true);
         }
+    }
+
+    /**
+     * Collapse the current sheet to peek state. Sheet may not change the state if the state
+     * is not allowed.
+     * @param animate {@code true} for animation effect.
+     * @return {@code true} if the sheet could go to the peek state.
+     */
+    public boolean peekSheet(boolean animate) {
+        if (mBottomSheet == null || mIsSuppressed) return false;
+        if (mBottomSheet.isSheetOpen() && mBottomSheet.isPeekStateEnabled()) {
+            mBottomSheet.setSheetState(SheetState.PEEK, animate);
+            return true;
+        }
+        return false;
     }
 
     /**
