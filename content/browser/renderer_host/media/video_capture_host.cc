a564f6a052e16b079cfb317ac5ba53556b4b27cb
Gabriel Charette <gab@chromium.org>
2019-11-04 20:55:38 +0000

    Migrate off BrowserThread::DeleteSoon in /content/browser/renderer_host/media/video_capture_host.cc
    Instead use base::DeleteSoon which enforces usage of TaskTraits.
    
    This CL is a no-op as-is. It was recently discovered however that some
    callers did BrowserThread::DeleteSoon() with pending tasks running on
    different task queues (different traits  -- e.g.
    
    TaskTraits to make this more obvious. Please review whether calls in
    this CL can be migrated as-is or need additional traits to match
    potentially pending tasks.
    
    Split from https://chromium-review.googlesource.com/c/chromium/src/+/1894109
    for cursory review.
    
    This CL was uploaded by git cl split.
    
    R=mcasas@chromium.org
    
    
    TaskPriority: :BEST_EFFORT) which can result in out-of-order deletion...
    BrowserThread: :DeleteSoon() is being migrated to base::DeleteSoon() w/
    Bug: 1019767
    Change-Id: If0528b001a87cb4f3f43b3b5dfe6bff987eab67a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893538
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712252}

--- content/browser/renderer_host/media/video_capture_host.cc
+++ content/browser/renderer_host/media/video_capture_host.cc
@@ -104,8 +104,8 @@ VideoCaptureHost::~VideoCaptureHost() {
   }
 
   NotifyAllStreamsRemoved();
-  BrowserThread::DeleteSoon(BrowserThread::UI, FROM_HERE,
-                            render_process_host_delegate_.release());
+  base::DeleteSoon(FROM_HERE, {BrowserThread::UI},
+                   render_process_host_delegate_.release());
 }
 
 void VideoCaptureHost::OnError(const VideoCaptureControllerID& controller_id,
