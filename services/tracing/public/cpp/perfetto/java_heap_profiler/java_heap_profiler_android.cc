2caffec112af483cb0ecbdb17f32c7bc3805ff74
George Zhang <zhanggeorge@google.com>
2019-11-05 20:04:47 +0000

    Create empty parser for hprof files to be called by the java heap profiler
    The main purpose of the java heap profiler is to obtain a heap dump in
    the form of an hprof file and then parse the hprof file.
    
    Currently,there exists a way to obtain the heap dump but there is no
    way to parse the hprof file. This CL includes a barebones parser for
    these hprof files.
    
    Bug: 1012072
    Change-Id: I7a5d19ba8a4c0284fcae41d7057ad44a0b0acdab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1861103
    Commit-Queue: George Zhang <zhanggeorge@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712717}

--- services/tracing/public/cpp/perfetto/java_heap_profiler/java_heap_profiler_android.cc
+++ services/tracing/public/cpp/perfetto/java_heap_profiler/java_heap_profiler_android.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/java_heap_dump_generator.h"
 #include "base/files/scoped_temp_dir.h"
+#include "services/tracing/public/cpp/perfetto/java_heap_profiler/hprof_parser_android.h"
 #include "services/tracing/public/cpp/perfetto/perfetto_traced_process.h"
 
 namespace tracing {
@@ -33,6 +34,10 @@ void JavaHeapProfiler::StartTracing(
   base::android::WriteJavaHeapDumpToPath(file_path);
 
   // TODO(zhanggeorge): Convert heap dump and write to trace.
+  HprofParser parser(file_path);
+  parser.Parse();
+  DCHECK_EQ(parser.parse_stats().result,
+            HprofParser::ParseResult::PARSE_SUCCESS);
 }
 
 void JavaHeapProfiler::StopTracing(base::OnceClosure stop_complete_callback) {
