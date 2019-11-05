1ecabfe8f72e29cef048afa7ffcc2a59a694800d
Nazerke <nazerke@google.com>
2019-11-05 12:38:04 +0000

    [iOS][eg2] Convert showcase egtest to EG2.
    This CL
     - adds the source file to sources list,
     - makes test source file be backward compatible with EG1,
     - adds required gn dependencies,
     - links to both eg1 and eg2 frameworks.
    
    Bug: 866147
    Change-Id: I5c6dc64c783db7a8863c53e69174257e35b719bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1897830
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Auto-Submit: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712538}

--- ios/showcase/core/showcase_egtest.mm
+++ ios/showcase/core/showcase_egtest.mm
@@ -3,9 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
-
 #import "ios/showcase/test/showcase_test_case.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
