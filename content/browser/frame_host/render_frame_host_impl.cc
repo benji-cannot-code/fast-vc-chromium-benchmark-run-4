ac6a22e04428647bb7e83bd53ed837b552c85a39
arthursonzogni <arthursonzogni@chromium.org>
2019-11-05 23:01:17 +0000

    RenderFrameHostImpl::delegate_ is not null. Stop checking it.
    RenderFrameHost::delegate_ is never null.
    
    New CLs are adding more and more null check for this, just to be sure,
    because there are locations where this is checked.
    
    This patch remove all the location where is checked to avoid this to
    spread further.
    
    Bug: None.
    Change-Id: I5ba7903a199fdb3117df6adf514e187b0a36ea0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899501
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712799}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -929,6 +929,8 @@ RenderFrameHostImpl::RenderFrameHostImpl(
       commit_callback_interceptor_(nullptr),
       media_device_id_salt_base_(
           BrowserContext::CreateRandomMediaDeviceIDSalt()) {
+  DCHECK(delegate_);
+
   GetProcess()->AddRoute(routing_id_, this);
   g_routing_id_frame_map.Get().emplace(
       GlobalFrameRoutingId(GetProcess()->GetID(), routing_id_), this);
@@ -1047,7 +1049,7 @@ RenderFrameHostImpl::~RenderFrameHostImpl() {
 
   const bool was_created = render_frame_created_;
   render_frame_created_ = false;
-  if (delegate_ && was_created)
+  if (was_created)
     delegate_->RenderFrameDeleted(this);
 
   // Ensure that the render process host has been notified that all audio
@@ -2110,7 +2112,7 @@ void RenderFrameHostImpl::SetRenderFrameCreated(bool created) {
   // We should not create new RenderFrames while our delegate is being destroyed
   // (e.g., via a WebContentsObserver during WebContents shutdown).  This seems
   // to have caused crashes in https://crbug.com/717650.
-  if (created && delegate_)
+  if (created)
     CHECK(!delegate_->IsBeingDestroyed());
 
   bool was_created = render_frame_created_;
@@ -2118,7 +2120,7 @@ void RenderFrameHostImpl::SetRenderFrameCreated(bool created) {
 
   // If the current status is different than the new status, the delegate
   // needs to be notified.
-  if (delegate_ && (created != was_created)) {
+  if (created != was_created) {
     if (created) {
       SetUpMojoIfNeeded();
       delegate_->RenderFrameCreated(this);
@@ -2462,7 +2464,7 @@ void RenderFrameHostImpl::SetLastCommittedUrl(const GURL& url) {
 
 void RenderFrameHostImpl::UpdateRenderProcessHostFramePriorities() {
   const auto new_committed_document_priority =
-      (delegate_ && delegate_->IsFrameLowPriority(this))
+      delegate_->IsFrameLowPriority(this)
           ? RenderProcessHostImpl::FramePriority::kLow
           : RenderProcessHostImpl::FramePriority::kNormal;
   if (last_committed_document_priority_ != new_committed_document_priority) {
@@ -4614,19 +4616,17 @@ void RenderFrameHostImpl::RegisterMojoInterfaces() {
   PermissionControllerImpl* permission_controller =
       PermissionControllerImpl::FromBrowserContext(
           GetProcess()->GetBrowserContext());
-  if (delegate_) {
-    auto* geolocation_context = delegate_->GetGeolocationContext();
-    if (geolocation_context) {
-      geolocation_service_.reset(new GeolocationServiceImpl(
-          geolocation_context, permission_controller, this));
-      // NOTE: Both the |interface_registry_| and |geolocation_service_| are
-      // owned by |this|, so their destruction will be triggered together.
-      // |interface_registry_| is declared after |geolocation_service_|, so it
-      // will be destroyed prior to |geolocation_service_|.
-      registry_->AddInterface(
-          base::BindRepeating(&GeolocationServiceImpl::Bind,
-                              base::Unretained(geolocation_service_.get())));
-    }
+  auto* geolocation_context = delegate_->GetGeolocationContext();
+  if (geolocation_context) {
+    geolocation_service_.reset(new GeolocationServiceImpl(
+        geolocation_context, permission_controller, this));
+    // NOTE: Both the |interface_registry_| and |geolocation_service_| are
+    // owned by |this|, so their destruction will be triggered together.
+    // |interface_registry_| is declared after |geolocation_service_|, so it
+    // will be destroyed prior to |geolocation_service_|.
+    registry_->AddInterface(
+        base::BindRepeating(&GeolocationServiceImpl::Bind,
+                            base::Unretained(geolocation_service_.get())));
   }
 
   registry_->AddInterface<media::mojom::InterfaceFactory>(base::BindRepeating(
@@ -6525,8 +6525,7 @@ void RenderFrameHostImpl::BindNFCReceiver(
         "WebNFC is only allowed in a top-level browsing context.");
     return;
   }
-  if (delegate_)
-    delegate_->GetNFC(std::move(receiver));
+  delegate_->GetNFC(std::move(receiver));
 }
 #endif
 
