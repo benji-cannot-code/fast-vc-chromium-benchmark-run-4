47c2eb235b9074db51ed0c444c24e9e595fd873c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-11-05 00:32:14 +0000

    Import wpt@d9b281edc4ca4669bcae42700fe48b343c354590
    Using wpt-import in Chromium 72de1a08d63da4d61c400a5ab852475687e874e1.
    With Chromium commits locally applied on WPT:
    1d5b803fd3 "Add WebVTT support for inline styling - Web Platform Tests"
    9be931cbef "[WPT/common/security-features] Refactor navigable tests"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Ie98cb58d377b7ffc2b78c08b3182e12c19f00414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1898174
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#712359}

---
+++ third_party/blink/web_tests/external/wpt/import-maps/core/static-import.py
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    return (
+        (('Content-Type', 'text/javascript'),),
+        'import "{}";\n'.format(request.GET.first('url'))
+    )
