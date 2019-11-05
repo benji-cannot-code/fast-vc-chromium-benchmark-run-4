6b4a410df8b6bd3c5d1ace5681fa6dcfba0af5e7
Joshua Pawlicki <waffles@chromium.org>
2019-11-05 22:34:51 +0000

    Disable flaky minidump tests.
    TBR=bsheedy@chromium.org
    
    Bug: 1014754
    Change-Id: I4692c3e3a306b0576ac91291624b72be141fff68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900193
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712787}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -21,7 +21,8 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   # Android is currently hard coded to return None for minidump paths.
   # Minidump symbolization doesn't work in ChromeOS local mode if the rootfs is
   # still read-only, so skip the test in that case.
-  @decorators.Disabled('android', 'chromeos-local')
+  # Flakes on chromeos: crbug.com/1014754
+  @decorators.Disabled('android', 'chromeos', 'chromeos-local')
   def testSymbolizeMinidump(self):
     # Wait for the browser to restart fully before crashing
     self._LoadPageThenWait('var sam = "car";', 'sam')
@@ -102,7 +103,8 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   @decorators.Isolated
   # Minidump symbolization doesn't work in ChromeOS local mode if the rootfs is
   # still read-only, so skip the test in that case.
-  @decorators.Disabled('android', 'chromeos-local')
+  # Flakes on chromeos: crbug.com/1014754
+  @decorators.Disabled('android', 'chromeos', 'chromeos-local')
   def testMultipleCrashMinidumps(self):
     # Wait for the browser to restart fully before crashing
     self._LoadPageThenWait('var cat = "dog";', 'cat')
