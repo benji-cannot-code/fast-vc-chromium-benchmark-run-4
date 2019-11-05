017afab1e8e5db1e32154010795d3b80f02b450c
Julie Jeongeun Kim <jkim@igalia.com>
2019-11-05 03:19:29 +0000

    Clean up old Mojo type for mojom::SubresourceFilterAgent
    This CL cleans up old mojo types for
    mojom::SubresourceFilterAgent using AssociatedReceiver and
    PendingAssociatedReceiver.
    
    Bug: 955171
    Change-Id: Iedd96df27c643c1bd307980d77c35231650631c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1897396
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#712433}

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager_unittest.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager_unittest.cc
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_renderer_host.h"
 #include "content/public/test/test_utils.h"
+#include "mojo/public/cpp/bindings/associated_receiver.h"
+#include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "net/base/net_errors.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
@@ -65,13 +67,13 @@ enum PageActivationNotificationTiming {
 
 class FakeSubresourceFilterAgent : public mojom::SubresourceFilterAgent {
  public:
-  FakeSubresourceFilterAgent() : binding_(this) {}
+  FakeSubresourceFilterAgent() = default;
   ~FakeSubresourceFilterAgent() override = default;
 
-  void OnSubresourceFilterAgentRequest(
+  void OnSubresourceFilterAgentReceiver(
       mojo::ScopedInterfaceEndpointHandle handle) {
-    binding_.Bind(
-        mojo::AssociatedInterfaceRequest<mojom::SubresourceFilterAgent>(
+    receiver_.Bind(
+        mojo::PendingAssociatedReceiver<mojom::SubresourceFilterAgent>(
             std::move(handle)));
   }
 
@@ -100,7 +102,7 @@ class FakeSubresourceFilterAgent : public mojom::SubresourceFilterAgent {
  private:
   mojom::ActivationStatePtr last_activation_;
   bool is_ad_subframe_ = false;
-  mojo::AssociatedBinding<mojom::SubresourceFilterAgent> binding_;
+  mojo::AssociatedReceiver<mojom::SubresourceFilterAgent> receiver_{this};
 };
 
 // Simple throttle that sends page-level activation to the manager for a
@@ -295,7 +297,7 @@ class ContentSubresourceFilterThrottleManagerTest
     host->GetRemoteAssociatedInterfaces()->OverrideBinderForTesting(
         mojom::SubresourceFilterAgent::Name_,
         base::BindRepeating(
-            &FakeSubresourceFilterAgent::OnSubresourceFilterAgentRequest,
+            &FakeSubresourceFilterAgent::OnSubresourceFilterAgentReceiver,
             base::Unretained(new_agent.get())));
     agent_map_[host] = std::move(new_agent);
   }
