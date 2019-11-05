08c432c352f6b6da79baf0389d2ce72cc9939907
Esmael El-Moslimany <aee@chromium.org>
2019-11-05 19:14:06 +0000

    WebUI: relanding cr-icon-button opaque ripple when WebUIA11yEnhancements flag enabled
    The enhancements flag can be enabled by searching for webui-a11y-enhancements on
    chrome://flags or by adding the command-line switch --enable-features=WebUIA11yEnhancements.
    
    Bug: 1001742
    Change-Id: I1d2b4162a8a5954172062d64aab871f4ca5b72ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1898479
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712690}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3014,7 +3014,7 @@ const char kTabFreezeDescription[] =
     "minutes.";
 
 const char kWebUIA11yEnhancementsName[] =
-    "Enable accessibility enahncements in WebUI";
+    "Enable accessibility enhancements in WebUI";
 const char kWebUIA11yEnhancementsDescription[] =
     "This flag covers a wide variety of accessibility enhancements in WebUI "
     "and is used to demonstrate the enhancements for review and testing. When "
