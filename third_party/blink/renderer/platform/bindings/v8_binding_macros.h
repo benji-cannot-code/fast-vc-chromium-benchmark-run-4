8a65723ef02d6ca00a360053641082fd0fdbe265
Richard Townsend <Richard.Townsend@arm.com>
2019-11-05 10:12:31 +0000

    fixup alloca => _alloca for Windows on Arm
    Windows inconsistently #define's _alloca as alloca in malloc.h, the
    conditions required are not present on Windows on Arm. To fix, we check
    and (possibly redefine) alloca on all Windows targets.
    
    Bug: 1020541, 893460
    Change-Id: Ife80ce3e7478d39d36e67686d70a74ff2319c714
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1892976
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Cr-Commit-Position: refs/heads/master@{#712506}

--- third_party/blink/renderer/platform/bindings/v8_binding_macros.h
+++ third_party/blink/renderer/platform/bindings/v8_binding_macros.h
@@ -35,6 +35,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
 #include "v8/include/v8.h"
 
+#if defined(OS_WIN)
+#if !defined(alloca)
+// Windows uses _alloca instead of alloca, but only #define's alloca to _alloca
+// in malloc.h if _CRT_INTERNAL_NONSTDC_NAMES is defined. If that doesn't happen
+// (e.g. on Windows on Arm), fix up the definition.
+#include <malloc.h>
+#define alloca _alloca
+#endif
+#endif
+
 namespace blink {
 
 // type is an instance of class template V8StringResource<>,
