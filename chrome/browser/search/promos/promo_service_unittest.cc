43d1aa8e8ae648073de65214c8efc34796a9e6ad
Dan Beam <dbeam@chromium.org>
2019-11-05 19:07:18 +0000

    Local NTP: move from generic features:: namespace to ntp_features::
    Bug: None
    Change-Id: I8b7f574b49b92a6221462fd1b1139d4ab168731b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899007
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712686}

--- chrome/browser/search/promos/promo_service_unittest.cc
+++ chrome/browser/search/promos/promo_service_unittest.cc
@@ -158,7 +158,7 @@ TEST_F(PromoServiceTest, GoodPromoResponse) {
 
 TEST_F(PromoServiceTest, GoodPromoResponseCanDismiss) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDismissNtpPromos);
+  feature_list.InitAndEnableFeature(ntp_features::kDismissPromos);
 
   std::string response_string =
       "{\"update\":{\"promos\":{\"middle\":\"<style></style><div><script></"
@@ -181,7 +181,7 @@ TEST_F(PromoServiceTest, GoodPromoResponseCanDismiss) {
 
 TEST_F(PromoServiceTest, GoodPromoResponseNoIdField) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDismissNtpPromos);
+  feature_list.InitAndEnableFeature(ntp_features::kDismissPromos);
 
   std::string response_string =
       "{\"update\":{\"promos\":{\"middle\":\"<style></style><div><script></"
@@ -204,7 +204,7 @@ TEST_F(PromoServiceTest, GoodPromoResponseNoIdField) {
 
 TEST_F(PromoServiceTest, GoodPromoResponseNoIdFieldNorLogUrl) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDismissNtpPromos);
+  feature_list.InitAndEnableFeature(ntp_features::kDismissPromos);
 
   std::string response_string =
       "{\"update\":{\"promos\":{\"middle\":\"<style></style><div><script></"
@@ -225,7 +225,7 @@ TEST_F(PromoServiceTest, GoodPromoResponseNoIdFieldNorLogUrl) {
 
 TEST_F(PromoServiceTest, GoodPromoWithBlockedID) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDismissNtpPromos);
+  feature_list.InitAndEnableFeature(ntp_features::kDismissPromos);
 
   {
     DictionaryPrefUpdate update(prefs(), prefs::kNtpPromoBlocklist);
@@ -249,7 +249,7 @@ TEST_F(PromoServiceTest, GoodPromoWithBlockedID) {
 
 TEST_F(PromoServiceTest, BlocklistPromo) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDismissNtpPromos);
+  feature_list.InitAndEnableFeature(ntp_features::kDismissPromos);
 
   std::string response_string =
       "{\"update\":{\"promos\":{\"middle\":\"<style></style><div><script></"
@@ -283,7 +283,7 @@ TEST_F(PromoServiceTest, BlocklistPromo) {
 
 TEST_F(PromoServiceTest, BlocklistExpiration) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDismissNtpPromos);
+  feature_list.InitAndEnableFeature(ntp_features::kDismissPromos);
 
   {
     DictionaryPrefUpdate update(prefs(), prefs::kNtpPromoBlocklist);
@@ -317,7 +317,7 @@ TEST_F(PromoServiceTest, BlocklistExpiration) {
 
 TEST_F(PromoServiceTest, BlocklistWrongExpiryType) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kDismissNtpPromos);
+  feature_list.InitAndEnableFeature(ntp_features::kDismissPromos);
 
   {
     DictionaryPrefUpdate update(prefs(), prefs::kNtpPromoBlocklist);
