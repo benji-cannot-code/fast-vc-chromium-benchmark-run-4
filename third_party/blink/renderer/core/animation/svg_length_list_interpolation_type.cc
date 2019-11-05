cfac1399b5c7b97a1dd5e45afac0347cfd687d9b
Fredrik Söderquist <fs@opera.com>
2019-11-05 13:53:04 +0000

    Eliminate SVGAnimationElement dependency from SVGListPropertyHelper
    SVGListPropertyHelper::AdjustFromToListValues only need to know if the
    current animation mode is 'to' or not. Pass a bool with that information
    instead and avoid pulling in svg_animation_element.h.
    
    Bug: 1017723
    Change-Id: Id9cfacd44b22eaf928d27ac8f1732dd9e7c43507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893205
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#712556}

--- third_party/blink/renderer/core/animation/svg_length_list_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/svg_length_list_interpolation_type.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/svg_interpolation_environment.h"
 #include "third_party/blink/renderer/core/animation/svg_length_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/underlying_length_checker.h"
+#include "third_party/blink/renderer/core/svg/svg_element.h"
 #include "third_party/blink/renderer/core/svg/svg_length_list.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 
