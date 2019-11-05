d6c994b1fb63fb64607b1e1441e7bdbb2c9dbfc1
Fredrik Söderquist <fs@opera.com>
2019-11-05 12:17:19 +0000

    Simplify SMIL animated value calculation and application
    The ResetAnimatedType, ClearAnimatedType, ApplyResultsToTarget and
    CalculateAnimatedValue methods that handles calculation and application
    of animation values are only called on elements that has made it into a
    sandwich - this means that they have passed the "has valid target"
    check. Simplify the code accordingly by assuming there is a target and
    that other checks that HasValidTarget() make holds. The latter for
    example means dropping the null-check for the animateMotion transform
    transform.
    Also remove SVGAnimateElement::ShouldApplyAnimation() and always remove
    the CSS property in SVGAnimateElement::ClearAnimatedType() when
    animating CSS.
    
    Bug: 1017723
    Change-Id: I9d184325671fae820c5718d3145228b7bd8d284e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895336
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#712531}

--- third_party/blink/renderer/core/svg/svg_animate_element.h
+++ third_party/blink/renderer/core/svg/svg_animate_element.h
@@ -91,8 +91,6 @@ class CORE_EXPORT SVGAnimateElement : public SVGAnimationElement {
                            stringsShouldNotSupportAddition);
 
  private:
-  bool ShouldApplyAnimation(const SVGElement& target_element) const;
-
   void SetAttributeType(const AtomicString&);
 
   InsertionNotificationRequest InsertedInto(ContainerNode&) final;
