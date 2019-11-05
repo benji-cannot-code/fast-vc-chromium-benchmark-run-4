d6f4ca05fa716f49fee1e45528b7554d548c9b7c
Carlos IL <carlosil@chromium.org>
2019-11-05 22:49:17 +0000

    Fix SB main frame/subresource interstitial interaction bug
    This fixes a bug that caused commands to be non-functional on a
    subresource triggered SB committed interstitial if a different type of
    interstitial had been shown on the same webcontents previously. The bug
    was triggered depending on the order different WebContentsObservers
    were installed (which determines the order DidFinishNavigation is
    called in them). This CL changes the observer that needs to fire first
    so it uses ReadyToCommitNavigationInstead.
    
    Bug: 1021334
    Change-Id: I3e6683279523dc6b74a2266fdd2bf05e0ab4d132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1896346
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712796}

--- chrome/browser/safe_browsing/safe_browsing_subresource_tab_helper.h
+++ chrome/browser/safe_browsing/safe_browsing_subresource_tab_helper.h
@@ -23,7 +23,7 @@ class SafeBrowsingSubresourceTabHelper
   ~SafeBrowsingSubresourceTabHelper() override;
 
   // WebContentsObserver::
-  void DidFinishNavigation(
+  void ReadyToCommitNavigation(
       content::NavigationHandle* navigation_handle) override;
 
  private:
