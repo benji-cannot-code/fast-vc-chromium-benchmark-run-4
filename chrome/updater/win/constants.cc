e91a8711cdc3797ef0060bf682255de92431a1cb
Findit <findit-for-me@appspot.gserviceaccount.com>
2019-11-05 00:58:54 +0000

    Revert "Register the updater app id in registry (Windows)."
    This reverts commit 08aba3152bc297e1e300da29fdaa63cc96710307.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 712315 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzA4YWJhMzE1MmJjMjk3ZTFlMzAwZGEyOWZkYWE2M2NjOTY3MTAzMDcM
    
    Sample Failed Build: https://ci.chromium.org/b/8897675907369267936
    
    Sample Failed Step: compile
    
    Original change's description:
    > Register the updater app id in registry (Windows).
    > 
    > Send both the updater and Chrome app ids in the "update apps" use case.
    > With this change, --install and --ua commands work as expected, meaning
    > the app ids and the versions are sent during update checks.
    > 
    > There is a edge case, not handled currently, where the app id for
    > the updater is not available until the first update check has happened.
    > This is going to be resolved in a future CL which lands as part of
    > fixing this CR bug.
    > 
    > R=waffles
    > 
    > Bug: 1020285
    > Change-Id: I2856a795ed61f590f85d792e4d5e1493bd23b84f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894833
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Commit-Queue: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#712315}
    
    
    Change-Id: I82dcc710be51b507c48a66de5dcb3b9d48a50c44
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1020285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1898537
    Cr-Commit-Position: refs/heads/master@{#712380}

--- chrome/updater/win/constants.cc
+++ chrome/updater/win/constants.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace updater {
 
+// The prefix to use for global names in WIN32 API's.
 const base::char16 kGlobalPrefix[] = L"Global\\G";
-
-extern const base::char16 kRegistryValuePV[] = L"pv";
-extern const base::char16 kRegistryValueName[] = L"name";
 
 }  // namespace updater
