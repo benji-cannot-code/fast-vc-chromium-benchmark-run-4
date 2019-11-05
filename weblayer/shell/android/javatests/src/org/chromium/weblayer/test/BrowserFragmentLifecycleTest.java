0cd046ce0d875000c6c14eee1e7e8a86ef960f06
Pavel Shmakov <pshmakov@google.com>
2019-11-05 17:46:28 +0000

    Fix crash when activity is destroyed while loading a page
    BrowserControllerImpl::DidFinishNavigation can be called while
    destroying WebContents:
    
    https://cs.chromium.org/chromium/src/content/browser/web_contents/web_contents_impl.cc?rcl=81163fe37074bf1ea2bb5a76fe88b2f49e86f23a&l=694
    https://cs.chromium.org/chromium/src/content/browser/frame_host/navigation_request.cc?rcl=81163fe37074bf1ea2bb5a76fe88b2f49e86f23a&l=1062
    
    This led to a nullptr dereference.
    
    Bug: 1021041
    Change-Id: I1894912ed77e377dd0e1ffa2b746921f13f392b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1897833
    Auto-Submit: Pavel Shmakov <pshmakov@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712649}

--- weblayer/shell/android/javatests/src/org/chromium/weblayer/test/BrowserFragmentLifecycleTest.java
+++ weblayer/shell/android/javatests/src/org/chromium/weblayer/test/BrowserFragmentLifecycleTest.java
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.weblayer.test;
 
+import android.net.Uri;
 import android.support.test.filters.SmallTest;
+import android.support.v4.app.FragmentManager;
+
+import androidx.annotation.NonNull;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,17 +18,22 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.weblayer.BrowserController;
+import org.chromium.weblayer.Navigation;
+import org.chromium.weblayer.NavigationCallback;
+import org.chromium.weblayer.NavigationController;
 import org.chromium.weblayer.shell.InstrumentationActivity;
 
+import java.util.concurrent.CountDownLatch;
+
 @RunWith(BaseJUnit4ClassRunner.class)
-public class FragmentRestoreTest {
+public class BrowserFragmentLifecycleTest {
     @Rule
     public InstrumentationActivityTestRule mActivityTestRule =
             new InstrumentationActivityTestRule();
 
     @Test
     @SmallTest
-    public void successfullyLoadsUrlAfterRotation() {
+    public void successfullyLoadsUrlAfterRecreation() {
         InstrumentationActivity activity = mActivityTestRule.launchShellWithUrl("about:blank");
         BrowserController controller = TestThreadUtils.runOnUiThreadBlockingNoException(
                 () -> activity.getBrowserController());
@@ -39,5 +48,29 @@ public class FragmentRestoreTest {
                 () -> newActivity.getBrowserController());
         url = "data:text,bar";
         mActivityTestRule.navigateAndWait(controller, url, false);
+    }
+
+    // https://crbug.com/1021041
+    @Test
+    @SmallTest
+    public void handlesFragmentDestroyWhileNavigating() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        InstrumentationActivity activity = mActivityTestRule.launchShellWithUrl("about:blank");
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            NavigationController navigationController =
+                    activity.getBrowserController().getNavigationController();
+            navigationController.registerNavigationCallback(new NavigationCallback() {
+                @Override
+                public void readyToCommitNavigation(@NonNull Navigation navigation) {
+                    FragmentManager fm = activity.getSupportFragmentManager();
+                    fm.beginTransaction()
+                            .remove(fm.getFragments().get(0))
+                            .runOnCommit(latch::countDown)
+                            .commit();
+                }
+            });
+            navigationController.navigate(Uri.parse("data:text,foo"));
+        });
+        latch.await();
     }
 }
