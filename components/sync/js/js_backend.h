16af84eccfa8dffc324481a0d0568d2bb49bd14a
dpapad <dpapad@chromium.org>
2019-11-05 09:03:33 +0000

    Rename components/sync/js/README.js to README.
    README.js is clearly not holding JS contents, and is causing ESLint
    errors.
    
    This is in preparation of turning on ESLint PRESUBMIT checks for
    src/components.
    
    Bug: 1019312
    Change-Id: Id32353b0f1e4bcd1cc62bd3983739c16fe0d24bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1898868
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712489}

--- components/sync/js/js_backend.h
+++ components/sync/js/js_backend.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SYNC_JS_JS_BACKEND_H_
 #define COMPONENTS_SYNC_JS_JS_BACKEND_H_
 
-// See README.js for design comments.
+// See README for design comments.
 
 #include <string>
 
