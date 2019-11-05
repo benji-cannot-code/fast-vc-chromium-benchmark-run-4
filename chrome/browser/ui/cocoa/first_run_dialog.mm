625e1966a03a0ad7934cd6bf1358d52b91e29282
Robert Sesek <rsesek@chromium.org>
2019-11-05 21:16:40 +0000

    Remove some unused third_party/google_toolbox_for_mac DEPS rules.
    Tbr: jennyz@chromium.org
    Bug: 903800
    Change-Id: Ib5ce7df0e1173bb8010d39168255f18a07e8a8a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895911
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712752}

--- chrome/browser/ui/cocoa/first_run_dialog.mm
+++ chrome/browser/ui/cocoa/first_run_dialog.mm
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/common/content_switches.h"
-#import "third_party/google_toolbox_for_mac/src/AppKit/GTMUILocalizerAndLayoutTweaker.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 #include "url/gurl.h"
 
