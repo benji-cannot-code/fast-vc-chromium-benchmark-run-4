0972061845cb5ad9efd7a4034a1419a4c1d09f42
Leon Han <leon.han@intel.com>
2019-11-05 04:52:31 +0000

    [webnfc] toRecords() implementation
    This CL adds support for reading sub records of a NDEFRecord,
    i.e. NDEFRecord#toRecords().
    
    Note: No support for writing yet.
    
    The spec changes:
    https://github.com/w3c/web-nfc/pull/333
    https://github.com/w3c/web-nfc/pull/334
    https://github.com/w3c/web-nfc/pull/359
    
    BUG=520391
    
    Change-Id: Ic7ac74416afa8e43f5293e4ca85e538fc700f3c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1824395
    Commit-Queue: Leon Han <leon.han@intel.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712452}

--- third_party/blink/renderer/modules/nfc/ndef_record.h
+++ third_party/blink/renderer/modules/nfc/ndef_record.h
@@ -19,6 +19,7 @@ namespace blink {
 class DOMDataView;
 class ExceptionState;
 class ExecutionContext;
+class NDEFMessage;
 class NDEFRecordInit;
 
 class MODULES_EXPORT NDEFRecord final : public ScriptWrappable {
@@ -49,8 +50,11 @@ class MODULES_EXPORT NDEFRecord final : public ScriptWrappable {
   const String& encoding() const;
   const String& lang() const;
   DOMDataView* data() const;
+  base::Optional<HeapVector<Member<NDEFRecord>>> toRecords(
+      ExceptionState& exception_state) const;
 
   const WTF::Vector<uint8_t>& payloadData() const;
+  const NDEFMessage* payload_message() const;
 
   void Trace(blink::Visitor*) override;
 
@@ -63,6 +67,9 @@ class MODULES_EXPORT NDEFRecord final : public ScriptWrappable {
   // Holds the NDEFRecord.[[PayloadData]] bytes defined at
   // https://w3c.github.io/web-nfc/#the-ndefrecord-interface.
   WTF::Vector<uint8_t> payload_data_;
+  // |payload_data_| parsed as an NDEFMessage. This field will be set for some
+  // "smart-poster" and external type records.
+  Member<NDEFMessage> payload_message_;
 };
 
 }  // namespace blink
