29f19aab448ada65bef5802aeec24e21bd991b39
Wez <wez@chromium.org>
2019-11-05 00:37:00 +0000

    [fuchsia] Print the WebEngine version when starting.
    Bug: 1020694
    Change-Id: I5f0e03328646456dbc1289a259d99b7bd9d8e79b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893545
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712366}

--- fuchsia/engine/context_provider_main.cc
+++ fuchsia/engine/context_provider_main.cc
@@ -15,9 +15,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_pump_type.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_executor.h"
+#include "components/version_info/version_info.h"
 #include "fuchsia/base/init_logging.h"
 #include "fuchsia/base/lifecycle_impl.h"
 #include "fuchsia/engine/context_provider_impl.h"
+
+namespace {
+
+std::string GetVersionString() {
+  std::string version_string = version_info::GetVersionNumber();
+#if !defined(OFFICIAL_BUILD)
+  version_string += " (built at " + version_info::GetLastChange() + ")";
+#endif  // !defined(OFFICIAL_BUILD)
+  return version_string;
+}
+
+}  // namespace
 
 int ContextProviderMain() {
   base::SingleThreadTaskExecutor main_task_executor(base::MessagePumpType::UI);
@@ -28,6 +41,8 @@ int ContextProviderMain() {
           *base::CommandLine::ForCurrentProcess())) {
     return 1;
   }
+
+  LOG(INFO) << "Starting WebEngine " << GetVersionString();
 
   ContextProviderImpl context_provider;
   base::fuchsia::ScopedServiceBinding<fuchsia::web::ContextProvider> binding(
