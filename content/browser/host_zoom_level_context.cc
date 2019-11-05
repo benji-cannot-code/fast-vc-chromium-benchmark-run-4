c6e6df3f761ead0e97bd2b268781d52ad4d0c602
Gabriel Charette <gab@chromium.org>
2019-11-05 03:12:34 +0000

    Migrate off BrowserThread::DeleteSoon in /content/browser/host_zoom_level_context.cc
    Instead use base::DeleteSoon which enforces usage of TaskTraits.
    
    This CL is a no-op as-is. It was recently discovered however that some
    callers did BrowserThread::DeleteSoon() with pending tasks running on
    different task queues (different traits  -- e.g.
    
    TaskTraits to make this more obvious. Please review whether calls in
    this CL can be migrated as-is or need additional traits to match
    potentially pending tasks.
    
    Split from https://chromium-review.googlesource.com/c/chromium/src/+/1894109
    for cursory review.
    
    This CL was uploaded by git cl split.
    
    R=wjmaclean@chromium.org
    
    
    TaskPriority: :BEST_EFFORT) which can result in out-of-order deletion...
    BrowserThread: :DeleteSoon() is being migrated to base::DeleteSoon() w/
    Bug: 1019767
    Change-Id: I08632c1a3a1b69fec27fa626b6d35b61da6bfe2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895641
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712431}

--- content/browser/host_zoom_level_context.cc
+++ content/browser/host_zoom_level_context.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/files/file_path.h"
+#include "base/task/post_task.h"
 #include "content/browser/host_zoom_map_impl.h"
+#include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 
 namespace content {
@@ -26,7 +28,7 @@ HostZoomLevelContext::~HostZoomLevelContext() {}
 void HostZoomLevelContext::DeleteOnCorrectThread() const {
   if (BrowserThread::IsThreadInitialized(BrowserThread::UI) &&
       !BrowserThread::CurrentlyOn(BrowserThread::UI)) {
-    BrowserThread::DeleteSoon(BrowserThread::UI, FROM_HERE, this);
+    base::DeleteSoon(FROM_HERE, {BrowserThread::UI}, this);
     return;
   }
   delete this;
