005eb8463b2aa8b6c085407a168b043aefb71b57
Henrique Nakashima <hnakashima@chromium.org>
2019-11-05 17:31:20 +0000

    Remove Int methods and removeKey() from ChromePreferenceManager.
    Use SharedPreferencesManager instead.
    
    Bug: 1017800
    Change-Id: I3f0dafadf32f618f7baa05e5fb6f1114473658e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894399
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712643}

--- chrome/android/javatests/src/org/chromium/chrome/browser/preferences/themes/ThemePreferencesTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/themes/ThemePreferencesTest.java
@@ -23,9 +23,9 @@ import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.FeatureUtilities;
 import org.chromium.chrome.browser.night_mode.NightModeUtils;
-import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.Preferences;
 import org.chromium.chrome.browser.preferences.PreferencesTest;
+import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.preferences.themes.ThemePreferences.ThemeSetting;
 import org.chromium.chrome.browser.ui.widget.RadioButtonWithDescription;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
@@ -62,7 +62,7 @@ public class ThemePreferencesTest extends DummyUiActivityTestCase {
     @Override
     public void setUpTest() throws Exception {
         super.setUpTest();
-        ChromePreferenceManager.getInstance().removeKey(UI_THEME_SETTING_KEY);
+        SharedPreferencesManager.getInstance().removeKey(UI_THEME_SETTING_KEY);
         Preferences preferences = PreferencesTest.startPreferences(
                 InstrumentationRegistry.getInstrumentation(), ThemePreferences.class.getName());
         mFragment = (ThemePreferences) preferences.getMainFragment();
@@ -72,7 +72,7 @@ public class ThemePreferencesTest extends DummyUiActivityTestCase {
 
     @Override
     public void tearDownTest() throws Exception {
-        ChromePreferenceManager.getInstance().removeKey(UI_THEME_SETTING_KEY);
+        SharedPreferencesManager.getInstance().removeKey(UI_THEME_SETTING_KEY);
         FeatureUtilities.setNightModeDefaultToLightForTesting(null);
         super.tearDownTest();
     }
@@ -102,7 +102,7 @@ public class ThemePreferencesTest extends DummyUiActivityTestCase {
             assertButtonCheckedCorrectly("System default", 0);
             Assert.assertEquals(ThemeSetting.SYSTEM_DEFAULT, mPreference.getSetting());
             Assert.assertEquals(mPreference.getSetting(),
-                    ChromePreferenceManager.getInstance().readInt(UI_THEME_SETTING_KEY));
+                    SharedPreferencesManager.getInstance().readInt(UI_THEME_SETTING_KEY));
 
             // Select Light
             Assert.assertEquals(R.id.light, getButton(1).getId());
@@ -110,7 +110,7 @@ public class ThemePreferencesTest extends DummyUiActivityTestCase {
             assertButtonCheckedCorrectly("Light", 1);
             Assert.assertEquals(ThemeSetting.LIGHT, mPreference.getSetting());
             Assert.assertEquals(mPreference.getSetting(),
-                    ChromePreferenceManager.getInstance().readInt(UI_THEME_SETTING_KEY));
+                    SharedPreferencesManager.getInstance().readInt(UI_THEME_SETTING_KEY));
 
             // Select Dark
             Assert.assertEquals(R.id.dark, getButton(2).getId());
@@ -118,7 +118,7 @@ public class ThemePreferencesTest extends DummyUiActivityTestCase {
             assertButtonCheckedCorrectly("Dark", 2);
             Assert.assertEquals(ThemeSetting.DARK, mPreference.getSetting());
             Assert.assertEquals(mPreference.getSetting(),
-                    ChromePreferenceManager.getInstance().readInt(UI_THEME_SETTING_KEY));
+                    SharedPreferencesManager.getInstance().readInt(UI_THEME_SETTING_KEY));
         });
     }
 
--- chrome/android/junit/src/org/chromium/chrome/browser/night_mode/GlobalNightModeStateControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/night_mode/GlobalNightModeStateControllerTest.java
@@ -32,7 +32,6 @@ import org.robolectric.annotation.Config;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.browser.flags.FeatureUtilities;
-import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.preferences.themes.ThemePreferences;
 
@@ -92,7 +91,7 @@ public class GlobalNightModeStateControllerTest {
 
     @After
     public void tearDown() {
-        ChromePreferenceManager.getInstance().removeKey(UI_THEME_SETTING_KEY);
+        SharedPreferencesManager.getInstance().removeKey(UI_THEME_SETTING_KEY);
     }
 
     @Test
@@ -144,12 +143,12 @@ public class GlobalNightModeStateControllerTest {
     @Test
     public void testUpdateNightMode_Preference() {
         // Set preference to dark theme and verify night mode is enabled.
-        ChromePreferenceManager.getInstance().writeInt(
+        SharedPreferencesManager.getInstance().writeInt(
                 UI_THEME_SETTING_KEY, ThemePreferences.ThemeSetting.DARK);
         assertTrue(mGlobalNightModeStateController.isInNightMode());
 
         // Set preference to light theme and verify night mode is disabled.
-        ChromePreferenceManager.getInstance().writeInt(
+        SharedPreferencesManager.getInstance().writeInt(
                 UI_THEME_SETTING_KEY, ThemePreferences.ThemeSetting.LIGHT);
         assertFalse(mGlobalNightModeStateController.isInNightMode());
 
@@ -173,7 +172,7 @@ public class GlobalNightModeStateControllerTest {
         setSystemNightMode(true);
         assertFalse(mGlobalNightModeStateController.isInNightMode());
 
-        ChromePreferenceManager.getInstance().writeInt(
+        SharedPreferencesManager.getInstance().writeInt(
                 UI_THEME_SETTING_KEY, ThemePreferences.ThemeSetting.DARK);
         assertFalse(mGlobalNightModeStateController.isInNightMode());
 
@@ -184,7 +183,7 @@ public class GlobalNightModeStateControllerTest {
         mGlobalNightModeStateController.onApplicationStateChange(HAS_RUNNING_ACTIVITIES);
         assertTrue(mGlobalNightModeStateController.isInNightMode());
 
-        ChromePreferenceManager.getInstance().writeInt(
+        SharedPreferencesManager.getInstance().writeInt(
                 UI_THEME_SETTING_KEY, ThemePreferences.ThemeSetting.SYSTEM_DEFAULT);
         assertTrue(mGlobalNightModeStateController.isInNightMode());
 
@@ -200,7 +199,7 @@ public class GlobalNightModeStateControllerTest {
         mGlobalNightModeStateController.addObserver(mObserver);
 
         // Verify that observer is called on night mode state changed from false to true.
-        ChromePreferenceManager.getInstance().writeInt(
+        SharedPreferencesManager.getInstance().writeInt(
                 UI_THEME_SETTING_KEY, ThemePreferences.ThemeSetting.DARK);
         assertTrue(mGlobalNightModeStateController.isInNightMode());
         verify(mObserver, times(1)).onNightModeStateChanged();
@@ -216,14 +215,14 @@ public class GlobalNightModeStateControllerTest {
         verify(mObserver, times(1)).onNightModeStateChanged();
 
         // Verify that observer is called when set to light theme.
-        ChromePreferenceManager.getInstance().writeInt(
+        SharedPreferencesManager.getInstance().writeInt(
                 UI_THEME_SETTING_KEY, ThemePreferences.ThemeSetting.LIGHT);
         assertFalse(mGlobalNightModeStateController.isInNightMode());
         verify(mObserver, times(2)).onNightModeStateChanged();
 
         // Verify that observer is not called after it is removed.
         mGlobalNightModeStateController.removeObserver(mObserver);
-        ChromePreferenceManager.getInstance().writeInt(
+        SharedPreferencesManager.getInstance().writeInt(
                 UI_THEME_SETTING_KEY, ThemePreferences.ThemeSetting.DARK);
         assertTrue(mGlobalNightModeStateController.isInNightMode());
         verify(mObserver, times(2)).onNightModeStateChanged();
