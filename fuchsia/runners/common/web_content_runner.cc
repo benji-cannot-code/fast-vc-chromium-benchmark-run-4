d9f7197bd07771257027804df1f728db65efa843
Fabrice de Gans-Riberi <fdegans@chromium.org>
2019-11-05 01:41:06 +0000

    [fuchsia] Add build flag to enable remote debugging in WebRunner.
    Bug: 1016036
    Change-Id: I9c2125d7b2fe11c08f7fa7024155ae25f4a0360e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1898713
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712396}

--- fuchsia/runners/common/web_content_runner.cc
+++ fuchsia/runners/common/web_content_runner.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/scoped_service_binding.h"
 #include "base/fuchsia/startup_context.h"
 #include "base/logging.h"
+#include "fuchsia/runners/buildflags.h"
 #include "fuchsia/runners/common/web_component.h"
 #include "url/gurl.h"
 
@@ -55,10 +56,17 @@ fuchsia::web::ContextPtr WebContentRunner::CreateWebContext(
 // static
 fuchsia::web::ContextPtr WebContentRunner::CreateDefaultWebContext(
     fuchsia::web::ContextFeatureFlags features) {
-  return CreateWebContext(BuildCreateContextParams(
-      OpenDirectoryOrFail(
-          base::FilePath(base::fuchsia::kPersistedDataDirectoryPath)),
-      features));
+  fuchsia::web::CreateContextParams create_context_params =
+      BuildCreateContextParams(OpenDirectoryOrFail(base::FilePath(
+                                   base::fuchsia::kPersistedDataDirectoryPath)),
+                               features);
+
+  if (BUILDFLAG(WEB_RUNNER_REMOTE_DEBUGGING_PORT) != 0) {
+    create_context_params.set_remote_debugging_port(
+        BUILDFLAG(WEB_RUNNER_REMOTE_DEBUGGING_PORT));
+  }
+
+  return CreateWebContext(std::move(create_context_params));
 }
 
 // static
@@ -115,6 +123,8 @@ void WebContentRunner::StartComponent(
       this,
       std::make_unique<base::fuchsia::StartupContext>(std::move(startup_info)),
       std::move(controller_request));
+  if (BUILDFLAG(WEB_RUNNER_REMOTE_DEBUGGING_PORT) != 0)
+    component->EnableRemoteDebugging();
   component->StartComponent();
   component->LoadUrl(url, std::vector<fuchsia::net::http::Header>());
   RegisterComponent(std::move(component));
