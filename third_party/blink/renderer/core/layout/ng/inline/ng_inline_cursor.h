d8b083fa22bee8cd45c2eca37355cc961e747e32
Yoshifumi Inoue <yosin@chromium.org>
2019-11-06 05:57:48 +0000

    Introduce NGInlineCursor::CurrentBidiLevel()
    This patch introduces |NGInlineCursor::CurrentBidiLevel()| with helper functions
    for prepration of utilizing |NGInlineCursor| to |AbstractInlineTextBox|[1].
    
    [1] http://crrev.com/c/1888242 Utilize NGInlineCursor for inline box traversal
    
    Bug: 982194
    Change-Id: I92f5aa918f27274cfd97dd2726bf95086d0b9ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899240
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712909}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_INLINE_NG_INLINE_CURSOR_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_INLINE_NG_INLINE_CURSOR_H_
 
+#include <unicode/ubidi.h>
+
 #include "base/containers/span.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
@@ -24,6 +26,7 @@ class NGFragmentItems;
 class NGInlineBreakToken;
 class NGPaintFragment;
 class NGPhysicalBoxFragment;
+enum class NGStyleVariant;
 class Node;
 struct PhysicalOffset;
 struct PhysicalRect;
@@ -157,6 +160,11 @@ class CORE_EXPORT NGInlineCursor {
   const NGPhysicalBoxFragment* CurrentBoxFragment() const;
   const LayoutObject* CurrentLayoutObject() const;
   Node* CurrentNode() const;
+
+  // Returns bidi level of current position. It is error to call other than
+  // text and atomic inline.
+  UBiDiLevel CurrentBidiLevel() const;
+
   // Returns text direction of current text or atomic inline. It is error to
   // call at other than text or atomic inline. Note: <span> doesn't have
   // reserved direction.
@@ -275,6 +283,10 @@ class CORE_EXPORT NGInlineCursor {
   // Returns break token for line box. It is error to call other than line box.
   const NGInlineBreakToken& CurrentInlineBreakToken() const;
 
+  // Returns style variant of the current position.
+  NGStyleVariant CurrentStyleVariant() const;
+  bool UsesFirstLineStyle() const;
+
   // True if current position is descendant or self of |layout_object|.
   // Note: This function is used for moving cursor in culled inline boxes.
   bool IsInclusiveDescendantOf(const LayoutObject& layout_object) const;
@@ -308,9 +320,9 @@ class CORE_EXPORT NGInlineCursor {
   void MoveToNextPaintFragment();
   void MoveToNextSiblingPaintFragment();
   void MoveToNextPaintFragmentSkippingChildren();
-
   void MoveToPreviousPaintFragment();
   void MoveToPreviousSiblingPaintFragment();
+
   ItemsSpan items_;
   ItemsSpan::iterator item_iter_;
   const NGFragmentItem* current_item_ = nullptr;
