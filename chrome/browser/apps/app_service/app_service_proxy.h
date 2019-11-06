12f2359993db8f51e35d9f1d4d2f59673d095be1
nancy <nancylingwang@chromium.org>
2019-11-06 06:27:28 +0000

    Add App Service Instance related classes.
    Design doc:
    go/app-service-instance-registry-design-doc
    go/appservice-for-per-app-time-limit-design-doc
    
    This CL adds the App Service Instance classes:
    Instance
    InstanceUpdate
    InstanceRegistry
    
    InstanceRegistry depends on Instance and InstanceUpdate to calculate
    the instance updates to notify the client.
    
    BUG=1011235
    
    Change-Id: Ife2a25c1ae24b3a5c8e1ef67540ba49939126174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1896484
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712915}

--- chrome/browser/apps/app_service/app_service_proxy.h
+++ chrome/browser/apps/app_service/app_service_proxy.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/services/app_service/public/cpp/app_registry_cache.h"
 #include "chrome/services/app_service/public/cpp/icon_cache.h"
 #include "chrome/services/app_service/public/cpp/icon_coalescer.h"
+#include "chrome/services/app_service/public/cpp/instance_registry.h"
 #include "chrome/services/app_service/public/cpp/preferred_apps.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -53,6 +54,11 @@ class AppServiceProxy : public KeyedService,
 
   mojo::Remote<apps::mojom::AppService>& AppService();
   apps::AppRegistryCache& AppRegistryCache();
+
+#if defined(OS_CHROMEOS)
+  apps::InstanceRegistry& InstanceRegistry();
+#endif
+
   apps::PreferredApps& PreferredApps();
 
   // apps::IconLoader overrides.
@@ -205,7 +211,10 @@ class AppServiceProxy : public KeyedService,
   std::unique_ptr<CrostiniApps> crostini_apps_;
   std::unique_ptr<ExtensionApps> extension_apps_;
   std::unique_ptr<ExtensionApps> extension_web_apps_;
+
   bool arc_is_registered_ = false;
+
+  apps::InstanceRegistry instance_registry_;
 #endif  // OS_CHROMEOS
 
   Profile* profile_;
