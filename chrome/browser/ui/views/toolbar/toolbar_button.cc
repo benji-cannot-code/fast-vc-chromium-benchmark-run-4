e98367a7139d9a1cc89e882a9bdf9d3bc37d8e55
David Roger <droger@chromium.org>
2019-11-06 17:48:27 +0000

    [IdentityPill] Fix insets in RTL
    The old code was assuming that ALIGN_LEFT means that the image is on the left,
    and ALIGN_RIGHT means it is on the right.
    However, based on the comment in LabelButton::SetHorizontalAlignment(),
    this is not true in RTL:
    
    //The optional image will lead the text, unless the button is right-aligned.
    
    Here "lead the text" depends on the language direction, and text direction
    should be taken into account when computing the spacing.
    
    Screenshot with the fix in RTL:
    https://drive.google.com/open?id=1Q1rZ7yml8_QjyzRxyoqC4GdcoyQ7Pfhm
    
    LTR screenshot with the fix for reference and checking that it did not regress:
    https://drive.google.com/open?id=1kodo_HO5s58fiEtC85AvVDDI9PoboQc4
    
    Fixed: 1021510
    Change-Id: I5ba910f1e3e532764e37070cbf815fdb340eeae9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899854
    Auto-Submit: David Roger <droger@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713070}

--- chrome/browser/ui/views/toolbar/toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/toolbar_button.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
 #include "ui/gfx/color_utils.h"
+#include "ui/gfx/text_utils.h"
 #include "ui/views/animation/ink_drop.h"
 #include "ui/views/animation/ink_drop_highlight.h"
 #include "ui/views/animation/installable_ink_drop.h"
@@ -186,12 +187,10 @@ void ToolbarButton::SetLabelSideSpacing(int spacing) {
   // Add the spacing only if text is non-empty.
   if (!GetText().empty()) {
     // Add spacing to the opposing side.
-    if (GetHorizontalAlignment() == gfx::ALIGN_RIGHT) {
-      label_insets = gfx::Insets(0, spacing, 0, 0);
-    } else {
-      DCHECK_EQ(GetHorizontalAlignment(), gfx::ALIGN_LEFT);
-      label_insets = gfx::Insets(0, 0, 0, spacing);
-    }
+    label_insets =
+        gfx::MaybeFlipForRTL(GetHorizontalAlignment()) == gfx::ALIGN_RIGHT
+            ? gfx::Insets(0, spacing, 0, 0)
+            : gfx::Insets(0, 0, 0, spacing);
   }
   if (!label()->border() || label_insets != label()->border()->GetInsets()) {
     label()->SetBorder(views::CreateEmptyBorder(label_insets));
