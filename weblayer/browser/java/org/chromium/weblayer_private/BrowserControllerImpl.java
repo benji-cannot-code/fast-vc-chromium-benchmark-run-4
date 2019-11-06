60e1d37d5c955e0d96e5d67b3af3b09a8e694f4b
Clark DuVall <cduvall@chromium.org>
2019-11-06 01:46:19 +0000

    [WebLayer] Fix action mode popup when selecting text
    This enables the action mode bar, which has the "Copy", "Select", "Share
    all" and "Web search" options when text is selected.
    
    Change-Id: Id91ce30e7f389d9105626f1f463fa687751dfa65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900570
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712858}

--- weblayer/browser/java/org/chromium/weblayer_private/BrowserControllerImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/BrowserControllerImpl.java
@@ -13,6 +13,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
+import org.chromium.content_public.browser.SelectionPopupController;
 import org.chromium.content_public.browser.ViewEventSink;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.ViewAndroidDelegate;
@@ -121,6 +122,8 @@ public final class BrowserControllerImpl extends IBrowserController.Stub {
         mFragment = fragment;
         mWebContents.setTopLevelNativeWindow(fragment.getWindowAndroid());
         mViewAndroidDelegate.setContainerView(fragment.getViewAndroidDelegateContainerView());
+        SelectionPopupController.fromWebContents(mWebContents)
+                .setActionModeCallback(new ActionModeCallback(mWebContents));
     }
 
     public BrowserFragmentControllerImpl getFragment() {
