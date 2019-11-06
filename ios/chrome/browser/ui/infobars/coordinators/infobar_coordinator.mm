2204be53059ebb046eb87ab252c4b422bc156e51
Chris Lu <thegreenfrog@chromium.org>
2019-11-06 04:25:26 +0000

    [ios] Move dismissInfobarBannerAfterInteraction delay call into InfobarCoordinator
    This change move the 7 second delay dismiss call into presentInfobarBannerAnimated:.
    This will be helpful now that Translate will be presenting the banner multiple times,
    allowing the 7 second delay to reset every time the banner is displayed.
    
    Bug: 1014959
    Change-Id: I0c11f0221bfc08be9e6bb4b119bdad09b67fcfe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900294
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712901}

--- ios/chrome/browser/ui/infobars/coordinators/infobar_coordinator.mm
+++ ios/chrome/browser/ui/infobars/coordinators/infobar_coordinator.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/infobars/banners/infobar_banner_presentation_state.h"
 #import "ios/chrome/browser/ui/infobars/coordinators/infobar_coordinator_implementation.h"
 #import "ios/chrome/browser/ui/infobars/infobar_badge_ui_delegate.h"
+#import "ios/chrome/browser/ui/infobars/infobar_constants.h"
 #import "ios/chrome/browser/ui/infobars/infobar_container.h"
 #import "ios/chrome/browser/ui/infobars/presentation/infobar_banner_positioner.h"
 #import "ios/chrome/browser/ui/infobars/presentation/infobar_banner_transition_driver.h"
@@ -51,6 +52,8 @@ const CGFloat kiPadBannerOverlapWithOmnibox = 10.0;
     InfobarModalTransitionDriver* modalTransitionDriver;
 // Readwrite redefinition.
 @property(nonatomic, assign, readwrite) BOOL bannerWasPresented;
+// Completion block used to dismiss the banner after a set period of time.
+@property(nonatomic, copy) dispatch_block_t dismissBannerBlock;
 
 @end
 
@@ -142,6 +145,26 @@ const CGFloat kiPadBannerOverlapWithOmnibox = 10.0;
                    if (completion)
                      completion();
                  }];
+
+  // Dismisses the presented banner after a certain number of seconds.
+  if (!UIAccessibilityIsVoiceOverRunning()) {
+    NSTimeInterval timeInterval =
+        self.highPriorityPresentation
+            ? kInfobarBannerLongPresentationDurationInSeconds
+            : kInfobarBannerDefaultPresentationDurationInSeconds;
+    dispatch_time_t popTime =
+        dispatch_time(DISPATCH_TIME_NOW, timeInterval * NSEC_PER_SEC);
+    if (self.dismissBannerBlock) {
+      // TODO:(crbug.com/1021805): Write unittest to cover this situation.
+      dispatch_block_cancel(self.dismissBannerBlock);
+    }
+    __weak InfobarCoordinator* weakSelf = self;
+    self.dismissBannerBlock = ^(void) {
+      [weakSelf dismissInfobarBannerAfterInteraction];
+      weakSelf.dismissBannerBlock = nil;
+    };
+    dispatch_after(popTime, dispatch_get_main_queue(), self.dismissBannerBlock);
+  }
 }
 
 - (void)presentInfobarModal {
