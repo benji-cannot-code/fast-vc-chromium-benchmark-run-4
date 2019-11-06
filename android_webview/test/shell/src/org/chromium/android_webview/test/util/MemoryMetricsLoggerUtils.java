6e460aede841ec239dc08c2180a9dd5e4e204417
Eric Stevenson <estevenson@chromium.org>
2019-11-06 18:37:40 +0000

    android_webview: Refactor MemoryMetricsLoggerTest.
    Right now the Java side lives in webview_instrumentation_test_apk
    while the C++ side lives in webview_instrumentation_apk. This CL moves
    the native method to a new Java class that lives in
    webview_instrumentation_apk so that both the Java and C++ sides live
    in the same APK. This is necessary for the proxy natives conversion.
    
    Bug: 997199
    Change-Id: Ia039cfcde5d18de80479c30086f67eedb531aca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899196
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713090}

---
+++ android_webview/test/shell/src/org/chromium/android_webview/test/util/MemoryMetricsLoggerUtils.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.test.util;
+
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
+
+/**
+ * Utilities for force recording memory metrics.
+ */
+@JNINamespace("android_webview")
+public class MemoryMetricsLoggerUtils {
+    @NativeMethods
+    public interface Natives {
+        /**
+         * Calls to MemoryMetricsLogger to force recording histograms, returning true on success.
+         * A value of false means recording failed (most likely because process metrics not
+         * available.
+         */
+        boolean forceRecordHistograms();
+    }
+}
