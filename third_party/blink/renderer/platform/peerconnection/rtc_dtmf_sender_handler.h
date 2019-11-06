3e6c4d0c9ef0503f2c4a051b26945009ad237528
Antonio Gomes <tonikitoo@igalia.com>
2019-11-06 17:17:02 +0000

    Switch RtcDtmfSenderHandler away from WebString
    BUG=787254
    R=haraken@chromium.org
    
    Change-Id: Id8f711e95601f9b7b2b87794622dfa9c976fddcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899794
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713051}

--- third_party/blink/renderer/platform/peerconnection/rtc_dtmf_sender_handler.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_dtmf_sender_handler.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "third_party/blink/public/platform/web_rtc_dtmf_sender_handler_client.h"
-#include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/webrtc/api/dtmf_sender_interface.h"
@@ -40,11 +39,9 @@ class PLATFORM_EXPORT RtcDtmfSenderHandler final {
   ~RtcDtmfSenderHandler();
 
   void SetClient(blink::WebRTCDTMFSenderHandlerClient* client);
-  blink::WebString CurrentToneBuffer();
+  String CurrentToneBuffer();
   bool CanInsertDTMF();
-  bool InsertDTMF(const blink::WebString& tones,
-                  int duration,
-                  int interToneGap);
+  bool InsertDTMF(const String& tones, int duration, int inter_tone_gap);
 
   void OnToneChange(const String& tone);
 
