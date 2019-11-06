580765b78bc4ed8e3094352cfd179037d40006bd
Naoki Fukino <fukino@chromium.org>
2019-11-06 02:51:55 +0000

    Show "Available devices" for non-empty device list.
    The device list header should be "Available devices" instead of
    "Available devices will appear here." when there already is one or more
    devices in the external storage list.
    
    Bug: 1007202
    Test: Manual test
    Change-Id: Ib08103236d32ada77b8328f29eacb1ed0a6a7759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895252
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Commit-Queue: Naoki Fukino <fukino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712877}

--- chrome/browser/resources/settings/device_page/storage_external.js
+++ chrome/browser/resources/settings/device_page/storage_external.js
@@ -12,7 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'settings-storage-external',
 
-  behaviors: [WebUIListenerBehavior],
+  behaviors: [
+    I18nBehavior,
+    WebUIListenerBehavior,
+  ],
 
   properties: {
     /**
@@ -56,5 +59,17 @@ Polymer({
    */
   handleExternalStoragesUpdated_: function(storages) {
     this.externalStorages_ = storages;
+  },
+
+  /**
+   * @param {Arrray<!settings.ExternalStorage>} externalStorages
+   * @return {string}
+   * @private
+   */
+  computeStorageListHeader_: function(externalStorages) {
+    return this.i18n(
+        !externalStorages || externalStorages.length == 0 ?
+            'storageExternalStorageEmptyListHeader' :
+            'storageExternalStorageListHeader');
   },
 });
