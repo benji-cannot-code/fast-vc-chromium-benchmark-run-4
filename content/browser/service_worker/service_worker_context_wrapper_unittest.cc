7c9a357e8973154ffcc82456bdd556e1d2763271
Greg Thompson <grt@chromium.org>
2019-11-06 12:42:47 +0000

    IWYU fix in service_worker_context_wrapper_unittest.cc.
    BUG=546640
    TBR=bashi@chromium.org
    
    Change-Id: I3cf922b974fe5259962f50e2e5431900d2e3b9d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900989
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712978}

--- content/browser/service_worker/service_worker_context_wrapper_unittest.cc
+++ content/browser/service_worker/service_worker_context_wrapper_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/files/scoped_temp_dir.h"
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
