c48be5d7ed42f2a63aa8a44ba0eff7f7f2ee299d
Sahel Sharify <sahel@chromium.org>
2019-11-06 21:36:10 +0000

    [payments] remove dumpWithoutCrashing
    This cl removes dump without crashing in case of multiple attempts to
    record journey logger bit vector. Dump without crashing is not needed
    anymore since enough stacktraces and events_ are gathered from stable
    channel.
    
    Bug: 994675
    Change-Id: I150704344deff3ec8dcf5def01386a8104dc7264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900252
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713171}

--- components/payments/core/journey_logger.cc
+++ components/payments/core/journey_logger.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <vector>
 
-#include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_number_conversions.h"
@@ -269,12 +267,6 @@ void JourneyLogger::RecordJourneyStatsHistograms(
   if (has_recorded_) {
     UMA_HISTOGRAM_BOOLEAN(
         "PaymentRequest.JourneyLoggerHasRecordedMultipleTimes", true);
-    static base::debug::CrashKeyString* journey_logger_multiple_record =
-        base::debug::AllocateCrashKeyString("journey_logger_multiple_record",
-                                            base::debug::CrashKeySize::Size32);
-    base::debug::SetCrashKeyString(journey_logger_multiple_record,
-                                   base::StringPrintf("%d", events_));
-    base::debug::DumpWithoutCrashing();
   }
   has_recorded_ = true;
 
