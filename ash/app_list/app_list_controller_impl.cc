1e052560a2db41bde1c5d8a3d265af279d0a3a27
Toni Barzic <tbarzic@chromium.org>
2019-11-06 22:03:57 +0000

    Use hotseat size instead of shelf size when laying out app list
    AppListView uses AppListViewDelegate::GetShelfSize() to get the shelf
    size for calculating:
    *   background shield rounded corners.
    *   the app list layout.
    
    For both purposes, app list can assume that the shelf height equals
    non-in-app height (i.e. assume that in-app shelf is not used).
    The former is only used in clamshell mode. For the later, app list will
    not be shown with the in-app shelf (at least in static state).
    
    Though, while transitioning from app window to app list (e.g. while
    dragging the app window), app list view will be laid out while in-app
    shelf shown - if the actual current shelf height is used to calculate
    the layout, the app list grid items might jump up once the transition
    completes (as the shelf height changes and the app list gets laid out
    again with less vertical space available).
    
    This can be avoided if app list view always assumed that the shelf size
    equals the non-in-app size, as done in this cl.
    
    BUG=1013706
    
    Change-Id: I5175781baefe1e92cf8abcd92185b1b3fdf8dc19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1857200
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713184}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -1305,7 +1305,7 @@ gfx::Rect AppListControllerImpl::SnapBoundsToDisplayEdge(
 }
 
 int AppListControllerImpl::GetShelfHeight() {
-  return ShelfConfig::Get()->shelf_size();
+  return ShelfConfig::Get()->system_shelf_size();
 }
 
 void AppListControllerImpl::RecordAppLaunched(
