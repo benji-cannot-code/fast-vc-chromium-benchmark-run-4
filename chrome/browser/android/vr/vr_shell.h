f557a33a8cc560cdae62fb0fbae47b37cbd5c467
Jacob DeWitt <jacde@chromium.org>
2019-11-06 17:00:00 +0000

    Remove CardboardGamepadDataFetcher
    Now that WebVR is gone, the gamepad data fetchers and providres are
    dead code. WebXR has a different way of exposing gamepad data.
    
    Bug: 1017848
    Change-Id: Ic784b9ea5db3ef8f0399b0fa800b2d1a467e63cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1893737
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Jacob DeWitt <jacde@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713040}

--- chrome/browser/android/vr/vr_shell.h
+++ chrome/browser/android/vr/vr_shell.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/vr/ui_initial_state.h"
 #include "chrome/browser/vr/ui_unsupported_mode.h"
 #include "content/public/browser/web_contents_observer.h"
-#include "device/vr/android/gvr/cardboard_gamepad_data_provider.h"
 #include "device/vr/android/gvr/gvr_gamepad_data_provider.h"
 #include "device/vr/public/cpp/session_mode.h"
 #include "device/vr/public/mojom/vr_service.mojom.h"
@@ -68,7 +67,6 @@ struct AutocompleteRequest;
 // The native instance of the Java VrShell. This class is not threadsafe and
 // must only be used on the UI thread.
 class VrShell : device::GvrGamepadDataProvider,
-                device::CardboardGamepadDataProvider,
                 VoiceResultDelegate,
                 public ChromeLocationBarModelDelegate,
                 public PageInfoUI {
@@ -261,10 +259,6 @@ class VrShell : device::GvrGamepadDataProvider,
   // device::GvrGamepadDataProvider implementation.
   void UpdateGamepadData(device::GvrGamepadData) override;
 
-  // device::CardboardGamepadDataProvider implementation.
-  void RegisterCardboardGamepadDataFetcher(
-      device::CardboardGamepadDataFetcher*) override;
-
   // ChromeLocationBarModelDelegate implementation.
   content::WebContents* GetActiveWebContents() const override;
   bool ShouldDisplayURL() const override;
@@ -381,10 +375,6 @@ class VrShell : device::GvrGamepadDataProvider,
   bool cardboard_gamepad_source_active_ = false;
   bool pending_cardboard_trigger_ = false;
 
-  // Registered fetchers, must remain alive for UpdateGamepadData calls.
-  // That's ok since the fetcher is only destroyed from VrShell's destructor.
-  device::CardboardGamepadDataFetcher* cardboard_gamepad_data_fetcher_ =
-      nullptr;
   int64_t cardboard_gamepad_timer_ = 0;
 
   // Content id
