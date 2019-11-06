0531d064daab6805ea1b11a3b2c32a937004c0bd
Chih-Yu Huang <akahuang@google.com>
2019-11-06 07:50:51 +0000

    media/gpu/chromeos: Remove USE_CHROMEOS_MEDIA_ACCELERATION in code.
    in BUILD.gn, we already restrict all code in media/gpu/chromeos are
    only compiled when USE_V4L2_CODEC or USE_VAAPI is true. We don't need
    this buildflag in the source code.
    
    Bug: none
    Test: Run video_decode_accelerator_tests on Kevin and Eve
    Change-Id: I5449101ca5ae6702d10f4b3bfc233f27d2767aa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900786
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712920}

--- media/gpu/chromeos/chromeos_video_decoder_factory.cc
+++ media/gpu/chromeos/chromeos_video_decoder_factory.cc
@@ -10,12 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "media/base/video_decoder.h"
 #include "media/gpu/buildflags.h"
-
-#if BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
 #include "media/gpu/chromeos/mailbox_video_frame_converter.h"
 #include "media/gpu/chromeos/platform_video_frame_pool.h"
 #include "media/gpu/chromeos/video_decoder_pipeline.h"
-#endif  // BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
 
 #if BUILDFLAG(USE_VAAPI)
 #include "media/gpu/vaapi/vaapi_video_decoder.h"
@@ -29,7 +26,6 @@ namespace media {
 
 namespace {
 
-#if BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
 // Get a list of the available functions for creating VideoDeocoder.
 base::queue<VideoDecoderPipeline::CreateVDFunc> GetCreateVDFunctions(
     VideoDecoderPipeline::CreateVDFunc cur_create_vd_func) {
@@ -50,7 +46,6 @@ base::queue<VideoDecoderPipeline::CreateVDFunc> GetCreateVDFunctions(
   }
   return ret;
 }
-#endif  // BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
 
 }  // namespace
 
@@ -81,14 +76,10 @@ std::unique_ptr<VideoDecoder> ChromeosVideoDecoderFactory::Create(
     std::unique_ptr<DmabufVideoFramePool> frame_pool,
     std::unique_ptr<VideoFrameConverter> frame_converter,
     gpu::GpuMemoryBufferFactory* const gpu_memory_buffer_factory) {
-#if BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
   return VideoDecoderPipeline::Create(
       std::move(client_task_runner), std::move(frame_pool),
       std::move(frame_converter), gpu_memory_buffer_factory,
       base::BindRepeating(&GetCreateVDFunctions));
-#endif  // BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
-
-  return nullptr;
 }
 
 }  // namespace media
