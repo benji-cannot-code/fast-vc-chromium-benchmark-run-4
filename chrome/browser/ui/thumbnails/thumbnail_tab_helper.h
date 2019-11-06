56b1248c477d56d97599740f8c0fde08eebd7590
Dana Fried <dfried@chromium.org>
2019-11-06 22:13:00 +0000

    Decouple reserving a web contents for capture and actually capturing.
    This CL separates a request for capture from actual video capture
    (since there may be a delay), and captures thumbnails from pages
    opportunistically as they are loading.
    
    This does not solve the attached bug but will allow us to do the
    following in subsequent CLs:
     - isolate the issue of capture not triggering render view creation
     - rate-limit actual video capture across tabs if necessary, by putting
       candidates for capture in a queue the prioritizes (but is not
       exclusive to) those we are currently requesting
    
    Bug: 1020782
    Change-Id: Ib00747d5c839cd8fe8ca9564ee84dfa6c19262d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1897485
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713189}

--- chrome/browser/ui/thumbnails/thumbnail_tab_helper.h
+++ chrome/browser/ui/thumbnails/thumbnail_tab_helper.h
@@ -33,6 +33,7 @@ class ThumbnailTabHelper
 
  private:
   class ThumanailImageImpl;
+  class ScopedCapture;
   friend class content::WebContentsUserData<ThumbnailTabHelper>;
   friend class ThumanailImageImpl;
 
@@ -71,6 +72,8 @@ class ThumbnailTabHelper
   void OnVisibilityChanged(content::Visibility visibility) override;
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
+  void RenderViewReady() override;
+  void RenderViewDeleted(content::RenderViewHost* render_view_host) override;
 
   // viz::mojom::FrameSinkVideoConsumer:
   void OnFrameCaptured(
@@ -122,6 +125,10 @@ class ThumbnailTabHelper
   // Captures frames from the WebContents while it's hidden. The capturer count
   // of the WebContents is incremented/decremented when a capturer is set/unset.
   std::unique_ptr<viz::ClientFrameSinkVideoCapturer> video_capturer_;
+
+  // Scoped request for video capture. Ensures we always decrement the counter
+  // once per increment.
+  std::unique_ptr<ScopedCapture> scoped_capture_;
 
   // The thumbnail maintained by this instance.
   scoped_refptr<ThumbnailImage> thumbnail_ =
