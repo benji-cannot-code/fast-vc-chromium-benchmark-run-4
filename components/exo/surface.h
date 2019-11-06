8b30a76d38e161470a01cad38e2f6a1a4b32c957
Albert Chaulk <achaulk@chromium.org>
2019-11-06 20:06:27 +0000

    Change how exo handles draw quads
    Occasionally the renderer will provide an invalid surface ID, so in
    that case we use the previous valid ID. This also checks that the
    embed token is the same between both versions and updates it if
    necessary
    
    This can happen when a webcontents loads a new page, the old surface
    is deprecated but the new one isn't ready yet
    
    Bug: b/143651420
    Test: on device
    Change-Id: I943d242f3acba3e2daf3fe69b5e3d469087df781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900094
    Commit-Queue: Albert Chaulk <achaulk@chromium.org>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713128}

--- components/exo/surface.h
+++ components/exo/surface.h
@@ -448,6 +448,7 @@ class Surface final : public ui::PropertyHandler {
 #endif  // defined(OS_CHROMEOS)
 
   viz::SurfaceId first_embedded_surface_id_;
+  viz::SurfaceId latest_embedded_surface_id_;
   base::RepeatingCallback<viz::SurfaceId()> get_current_surface_id_;
 
   DISALLOW_COPY_AND_ASSIGN(Surface);
