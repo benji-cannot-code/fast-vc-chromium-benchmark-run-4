2b821759eea921687fcd72704d79879904f5f9f3
Yue Ru Sun <yrsun@chromium.org>
2019-11-06 16:11:04 +0000

    Use last committed navigation UKM source id for media-related events
    regardless whether it is from a cross-document or same-document navigation.
    
    Bug: b/112853960
    Change-Id: Ib098ba7435b3e850b59ca42b4a122fbd2b39a5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895756
    Commit-Queue: Yue Ru Sun <yrsun@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713025}

--- content/browser/frame_host/render_frame_host_delegate.cc
+++ content/browser/frame_host/render_frame_host_delegate.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include <stddef.h>
+#include <memory>
+#include <utility>
 
 #include "base/callback.h"
 #include "base/strings/string16.h"
@@ -156,6 +158,11 @@ Visibility RenderFrameHostDelegate::GetVisibility() {
 
 ukm::SourceId RenderFrameHostDelegate::GetUkmSourceIdForLastCommittedSource()
     const {
+  return ukm::kInvalidSourceId;
+}
+
+ukm::SourceId RenderFrameHostDelegate::
+    GetUkmSourceIdForLastCommittedSourceIncludingSameDocument() const {
   return ukm::kInvalidSourceId;
 }
 
