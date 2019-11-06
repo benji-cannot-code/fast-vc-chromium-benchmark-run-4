44fb8b5e19c988bcad80106dc8ef645797e30e78
Michael Spang <spang@chromium.org>
2019-11-06 23:34:36 +0000

    aura: Clean up transformation confusion for root window bounds
    Implementations of GetTransformedRootWindowBoundsInPixels() are
    currently applying the inverse of the desired transformation: the root
    window transform converts root window pixel coordinates to host pixel
    coordinates. This function needs to do the reverse: it takes host pixel
    coordinates, and returns root window pixel coordinates.
    
    The reason this was not causing bugs is that in production Chrome OS &
    Cast both override this function to throw out the origin of the returned
    rect. Setting the origin to zero after transforming corrects for applying
    the wrong transformation: the rest of the transformation is a 0, 90, 180,
    or 270 degree rotation. Since 0 and 180 degree rotations are
    self-inverses, and a 90 degree and a 270 degree rotation have identical
    effect on sizes, keeping only the size ensured correct results.
    
    Clean up this confusion by inverting the transformation applied by
    GetTransformedRootWindowBoundsInPixels(). This means that the origin of
    the transformed rect will automatically be correct (i.e., it will be
    zero), except during configuration (because bounds & transform aren't
    updated atomically).
    
    We still force the origin to zero to avoid any behavioral change from
    roundoff. There are some minor discrepancies related to that:
    
     - Chrome OS puts the window at origin, and rounds the size down
     - Cast puts the window at the origin, and rounds the size up
     - The default implementation allows arbitrary positioning, and
       takes an enclosing rect
    
    These discrepancies are unchanged in this patch.
    
    Bug: 1019015
    
    Change-Id: I9bb40872be2fc76f04e63350a67965d44b71cc6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1885633
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713223}

--- chromecast/graphics/cast_window_tree_host_aura.cc
+++ chromecast/graphics/cast_window_tree_host_aura.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/graphics/cast_window_tree_host_aura.h"
 
 #include "ui/aura/null_window_targeter.h"
+#include "ui/gfx/geometry/size_conversions.h"
 #include "ui/platform_window/platform_window_init_properties.h"
 
 namespace chromecast {
@@ -34,11 +35,13 @@ void CastWindowTreeHostAura::DispatchEvent(ui::Event* event) {
 }
 
 gfx::Rect CastWindowTreeHostAura::GetTransformedRootWindowBoundsInPixels(
-    const gfx::Size& host_size_in_pixels) const {
-  gfx::RectF new_bounds(WindowTreeHost::GetTransformedRootWindowBoundsInPixels(
-      host_size_in_pixels));
-  new_bounds.set_origin(gfx::PointF());
-  return gfx::ToEnclosingRect(new_bounds);
+    const gfx::Size& size_in_pixels) const {
+  gfx::RectF new_bounds = gfx::RectF(gfx::Rect(size_in_pixels));
+  GetInverseRootTransform().TransformRect(&new_bounds);
+
+  // Root window origin will be (0,0) except during bounds changes.
+  // Set to exactly zero to avoid rounding issues.
+  return gfx::Rect(gfx::ToCeiledSize(new_bounds.size()));
 }
 
 }  // namespace chromecast
--- ui/aura/window_tree_host_unittest.cc
+++ ui/aura/window_tree_host_unittest.cc
@@ -35,7 +35,7 @@ TEST_F(WindowTreeHostTest, DPIWindowSize) {
   EXPECT_EQ(gfx::Rect(0, 0, 534, 400), root_window()->bounds());
 
   gfx::Transform transform;
-  transform.Translate(0, 1.1f);
+  transform.Translate(0, -1.1f);
   host()->SetRootTransform(transform);
   EXPECT_EQ(gfx::Rect(0, 1, 534, 401), root_window()->bounds());
 
@@ -56,7 +56,7 @@ TEST_F(WindowTreeHostTest,
             gfx::Size(400, 300));
   EXPECT_EQ(display::Screen::GetScreen()->GetPrimaryDisplay().bounds(),
             gfx::Rect(0, 0, 400, 300));
-  EXPECT_EQ(gfx::Size(400, 300), host()->window()->bounds().size());
+  EXPECT_EQ(gfx::Rect(400, 300), host()->window()->bounds());
 
   host()->SetBoundsInPixels(gfx::Rect(0, 0, 400, 300));
   test_screen()->SetDisplayRotation(display::Display::ROTATE_90);
@@ -67,7 +67,7 @@ TEST_F(WindowTreeHostTest,
             gfx::Size(300, 400));
   EXPECT_EQ(display::Screen::GetScreen()->GetPrimaryDisplay().bounds(),
             gfx::Rect(0, 0, 300, 400));
-  EXPECT_EQ(gfx::Size(300, 400), host()->window()->bounds().size());
+  EXPECT_EQ(gfx::Rect(300, 400), host()->window()->bounds());
 
   host()->SetBoundsInPixels(gfx::Rect(0, 0, 400, 300));
   test_screen()->SetDisplayRotation(display::Display::ROTATE_180);
@@ -78,7 +78,7 @@ TEST_F(WindowTreeHostTest,
             gfx::Size(400, 300));
   EXPECT_EQ(display::Screen::GetScreen()->GetPrimaryDisplay().bounds(),
             gfx::Rect(0, 0, 400, 300));
-  EXPECT_EQ(gfx::Size(400, 300), host()->window()->bounds().size());
+  EXPECT_EQ(gfx::Rect(400, 300), host()->window()->bounds());
 
   host()->SetBoundsInPixels(gfx::Rect(0, 0, 400, 300));
   test_screen()->SetDisplayRotation(display::Display::ROTATE_270);
@@ -89,7 +89,7 @@ TEST_F(WindowTreeHostTest,
             gfx::Size(300, 400));
   EXPECT_EQ(display::Screen::GetScreen()->GetPrimaryDisplay().bounds(),
             gfx::Rect(0, 0, 300, 400));
-  EXPECT_EQ(gfx::Size(300, 400), host()->window()->bounds().size());
+  EXPECT_EQ(gfx::Rect(300, 400), host()->window()->bounds());
 }
 
 #if defined(OS_CHROMEOS)
