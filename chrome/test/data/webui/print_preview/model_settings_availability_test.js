b276a071b5bd1a3d2eb82547eda48ad549b2bac6
Jesse Schettler <jschettler@chromium.org>
2019-11-06 17:29:46 +0000

    Print Preview: Change available settings for ARC
    When printing for ARC, make the settings that ARC can handle available
    unless the selected destination does not support them.
    
    Available when supported: collate, color, copies, duplex,
    duplexShortEdge, layout, mediaSize, pin, pinValue, and vendorItems.
    
    Unavailable: cssBackground, customMargins, dpi, headerFooter, margins,
    pagesPerSheet, scaling, scalingType, scalingTypePdf, and selectionOnly.
    
    Bug: b:140576300
    Test: Print from ARC and verify the correct settings are available
    Change-Id: I4bb5d53c8c64acf9bf189476d54d25ff5162f75b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1865476
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713060}

--- chrome/test/data/webui/print_preview/model_settings_availability_test.js
+++ chrome/test/data/webui/print_preview/model_settings_availability_test.js
@@ -16,6 +16,7 @@ cr.define('model_settings_availability_test', function() {
       model.documentSettings = {
         hasCssMediaStyles: false,
         hasSelection: false,
+        isFromArc: false,
         isModifiable: true,
         isPdf: false,
         isScalingDisabled: false,
@@ -115,7 +116,12 @@ cr.define('model_settings_availability_test', function() {
       model.set('documentSettings.isModifiable', false);
       assertFalse(model.settings.layout.available);
 
+      // Test with ARC - should be available.
+      model.set('documentSettings.isFromArc', true);
+      assertTrue(model.settings.layout.available);
+
       model.set('documentSettings.isModifiable', true);
+      model.set('documentSettings.isFromArc', false);
       assertTrue(model.settings.layout.available);
 
       // Unavailable if document has CSS media styles.
@@ -277,7 +283,15 @@ cr.define('model_settings_availability_test', function() {
       assertTrue(model.settings.margins.available);
       assertTrue(model.settings.customMargins.available);
 
+      // No margins settings for ARC.
+      model.set('documentSettings.isFromArc', true);
+      assertFalse(model.settings.margins.available);
+      assertFalse(model.settings.customMargins.available);
+      assertFalse(model.settings.margins.setFromUi);
+      assertFalse(model.settings.customMargins.setFromUi);
+
       // No margins settings for PDFs.
+      model.set('documentSettings.isFromArc', false);
       model.set('documentSettings.isModifiable', false);
       assertFalse(model.settings.margins.available);
       assertFalse(model.settings.customMargins.available);
@@ -287,6 +301,13 @@ cr.define('model_settings_availability_test', function() {
 
     test('dpi', function() {
       // The settings are available since the printer has multiple DPI options.
+      assertTrue(model.settings.dpi.available);
+
+      // No resolution settings for ARC.
+      model.set('documentSettings.isFromArc', true);
+      assertFalse(model.settings.dpi.available);
+
+      model.set('documentSettings.isFromArc', false);
       assertTrue(model.settings.dpi.available);
 
       // Remove capability.
@@ -340,6 +361,14 @@ cr.define('model_settings_availability_test', function() {
       model.set('destination', defaultDestination);
       assertFalse(model.settings.scaling.available);
 
+      // ARC -> printer
+      model.set('documentSettings.isFromArc', true);
+      model.set('documentSettings.isPdf', true);
+      assertFalse(model.settings.scaling.available);
+
+      // ARC -> Save as PDF
+      setSaveAsPdfDestination();
+      assertFalse(model.settings.scaling.available);
     });
 
     test('scalingType', function() {
@@ -368,6 +397,15 @@ cr.define('model_settings_availability_test', function() {
       // Non-PDF Plugin -> printer
       model.set('destination', defaultDestination);
       assertFalse(model.settings.scalingType.available);
+
+      // ARC -> printer
+      model.set('documentSettings.isFromArc', true);
+      model.set('documentSettings.isPdf', true);
+      assertFalse(model.settings.scalingType.available);
+
+      // ARC -> Save as PDF
+      setSaveAsPdfDestination();
+      assertFalse(model.settings.scalingType.available);
     });
 
     test('scalingTypePdf', function() {
@@ -395,6 +433,15 @@ cr.define('model_settings_availability_test', function() {
 
       // Non-PDF Plugin -> printer
       model.set('destination', defaultDestination);
+      assertFalse(model.settings.scalingTypePdf.available);
+
+      // ARC -> printer
+      model.set('documentSettings.isFromArc', true);
+      model.set('documentSettings.isPdf', true);
+      assertFalse(model.settings.scalingTypePdf.available);
+
+      // ARC -> Save as PDF
+      setSaveAsPdfDestination();
       assertFalse(model.settings.scalingTypePdf.available);
     });
 
@@ -475,6 +522,11 @@ cr.define('model_settings_availability_test', function() {
       model.set('documentSettings.isModifiable', false);
       assertFalse(model.settings.headerFooter.available);
       assertFalse(model.settings.headerFooter.setFromUi);
+
+      // Header/footer is never available for ARC.
+      model.set('documentSettings.isFromArc', true);
+      assertFalse(model.settings.headerFooter.available);
+      assertFalse(model.settings.headerFooter.setFromUi);
     });
 
     test('css background', function() {
@@ -483,6 +535,11 @@ cr.define('model_settings_availability_test', function() {
 
       // No CSS background setting for PDFs.
       model.set('documentSettings.isModifiable', false);
+      assertFalse(model.settings.cssBackground.available);
+      assertFalse(model.settings.cssBackground.setFromUi);
+
+      // No CSS background setting for ARC.
+      model.set('documentSettings.isFromArc', true);
       assertFalse(model.settings.cssBackground.available);
       assertFalse(model.settings.cssBackground.setFromUi);
     });
@@ -538,6 +595,10 @@ cr.define('model_settings_availability_test', function() {
       assertEquals(
           !cr.isWindows && !cr.isMac, model.settings.rasterize.available);
       assertFalse(model.settings.rasterize.setFromUi);
+
+      // Unavailable for ARC.
+      model.set('documentSettings.isFromArc', true);
+      assertFalse(model.settings.rasterize.available);
     });
 
     test('selection only', function() {
@@ -549,6 +610,11 @@ cr.define('model_settings_availability_test', function() {
 
       // Not available for PDFs.
       model.set('documentSettings.isModifiable', false);
+      assertFalse(model.settings.selectionOnly.available);
+      assertFalse(model.settings.selectionOnly.setFromUi);
+
+      // Not available for ARC.
+      model.set('documentSettings.isFromArc', true);
       assertFalse(model.settings.selectionOnly.available);
       assertFalse(model.settings.selectionOnly.setFromUi);
     });
@@ -571,6 +637,11 @@ cr.define('model_settings_availability_test', function() {
 
       // Not available for Flash content.
       model.set('documentSettings.isPdf', false);
+      assertFalse(model.settings.pagesPerSheet.available);
+
+      // Not available for ARC.
+      model.set('documentSettings.isFromArc', true);
+      model.set('documentSettings.isPdf', true);
       assertFalse(model.settings.pagesPerSheet.available);
     });
 
