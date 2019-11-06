31e840bc47d19953223f93df3f29b1b4daef2211
Takashi Sakamoto <tasak@google.com>
2019-11-06 10:11:03 +0000

    Fix memory leak of KeepAliveOperationTest.NotifiesObserversOnResponse
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/33985
    deterministic failures
    KeepAliveOperationTest.NotifiesObserversOnResponse
    KeepAliveOperationTest.RecordsResponseDuration
    
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8910702062804733408/+/steps/chromeos_components_unittests/0/logs/Deterministic_failure:_KeepAliveOperationTest.NotifiesObserversOnResponse__status_FAILURE_ON_EXIT_/0
    
    Change-Id: I034570a4376e8d39545c31dca035ff4f61c84510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1660381
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#712953}

--- chromeos/components/tether/keep_alive_operation_unittest.cc
+++ chromeos/components/tether/keep_alive_operation_unittest.cc
@@ -44,7 +44,7 @@ class MockOperationObserver : public KeepAliveOperation::Observer {
 
   void OnOperationFinished(multidevice::RemoteDeviceRef remote_device,
                            std::unique_ptr<DeviceStatus> device_status) {
-    OnOperationFinishedRaw(remote_device, device_status.release());
+    OnOperationFinishedRaw(remote_device, device_status.get());
   }
 
  private:
