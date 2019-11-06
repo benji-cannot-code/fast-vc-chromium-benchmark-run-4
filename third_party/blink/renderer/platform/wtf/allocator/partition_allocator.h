228cecd6cb7a55aa14600468fa09a5e2b676ed74
Michael Lippautz <mlippautz@chromium.org>
2019-11-06 13:49:00 +0000

    wtf: Remove Visitor type from PartitionAllocator
    Change-Id: I6b9cbd66d7015cdf239db8c6906ee18595c2b4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1901072
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712986}

--- third_party/blink/renderer/platform/wtf/allocator/partition_allocator.h
+++ third_party/blink/renderer/platform/wtf/allocator/partition_allocator.h
@@ -19,13 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-class PartitionAllocatorDummyVisitor {
-  DISALLOW_NEW();
-};
-
 class WTF_EXPORT PartitionAllocator {
  public:
-  typedef PartitionAllocatorDummyVisitor Visitor;
   static constexpr bool kIsGarbageCollected = false;
 
   template <typename T>
