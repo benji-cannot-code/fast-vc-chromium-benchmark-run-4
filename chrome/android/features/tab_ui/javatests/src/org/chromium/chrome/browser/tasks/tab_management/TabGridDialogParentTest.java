993b2808758096aa21addfa8301b1f8f839aa58c
Yue Zhang <yuezhanggg@chromium.org>
2019-11-06 17:13:08 +0000

    Fix testSetIphDialogVisibility on O devices
    Bug: 983170
    Change-Id: I830632b436bfd4f9b159b3a183f524e4d432ad79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900362
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713046}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogParentTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogParentTest.java
@@ -5,10 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
+import static org.chromium.chrome.browser.tasks.tab_management.TabUiTestHelper.areAnimatorsEnabled;
+
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.graphics.Rect;
-import android.provider.Settings;
 import android.support.test.annotation.UiThreadTest;
 import android.support.test.filters.MediumTest;
 import android.support.test.filters.SmallTest;
@@ -24,7 +25,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.tab.TabFeatureUtilities;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -421,19 +421,5 @@ public class TabGridDialogParentTest extends DummyUiActivityTestCase {
     public void tearDownTest() throws Exception {
         mTabGridDialogParent.destroy();
         super.tearDownTest();
-    }
-
-    /**
-     * TODO(crbug.com/982018): put this interface in a place to share with
-     * TabListContainerViewBinderTest.areAnimatorsEnabled.
-     */
-    private static boolean areAnimatorsEnabled() {
-        // We default to assuming that animations are enabled in case ANIMATOR_DURATION_SCALE is not
-        // defined.
-        final float defaultScale = 1f;
-        float durationScale =
-                Settings.Global.getFloat(ContextUtils.getApplicationContext().getContentResolver(),
-                        Settings.Global.ANIMATOR_DURATION_SCALE, defaultScale);
-        return !(durationScale == 0.0);
     }
 }
