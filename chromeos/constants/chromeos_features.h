7abcd46bb2c063a80de936f919f73d73f7223af5
Curtis McMullan <curtismcmullan@chromium.org>
2019-11-06 06:35:03 +0000

    Add resizable floating keyboard flag
    We want to create a prototype of a resizable floating virtual keyboard. This flag will be used to enable / disable the prototype.
    
    Bug: 1019990
    Change-Id: I6194b024842ee177bca2180c74e69836d26897bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1882256
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#712916}

--- chromeos/constants/chromeos_features.h
+++ chromeos/constants/chromeos_features.h
@@ -126,6 +126,8 @@ COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kVideoPlayerNativeControls;
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const base::Feature kVirtualKeyboardBorderedKey;
+COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+extern const base::Feature kVirtualKeyboardFloatingResizable;
 
 // Keep alphabetized.
 
