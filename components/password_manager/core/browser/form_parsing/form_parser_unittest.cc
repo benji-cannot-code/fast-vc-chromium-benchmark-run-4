c51c100e6609c6db44c21bcda650f05935af44e5
Matthias Körber <koerber@google.com>
2019-11-06 14:47:51 +0000

    [PasswordManager] Put clear-text field password generation behind Finch flag
    To allow for a partial roll-out and better metrics, the feature to offer password generation for clear-text fields is put behind a Finch flag.
    
    Bug: 1021897
    Change-Id: I4e799b4db354d1285063bb25bd0dc902bfe35b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899380
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713003}

--- components/password_manager/core/browser/form_parsing/form_parser_unittest.cc
+++ components/password_manager/core/browser/form_parsing/form_parser_unittest.cc
@@ -16,11 +16,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "components/autofill/core/common/password_form.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
@@ -1115,6 +1117,9 @@ TEST(FormParserTest, ReadonlyFields) {
 }
 
 TEST(FormParserTest, ServerPredictionsForClearTextPasswordFields) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(
+      password_manager::features::KEnablePasswordGenerationForClearTextFields);
   CheckTestData({
       {
           .description_for_logging = "Server prediction for account change "
