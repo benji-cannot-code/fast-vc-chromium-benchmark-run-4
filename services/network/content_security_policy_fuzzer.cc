6cdf1ec59a7ca2919fd21abe4311a8e58e1ad4b8
Lucas Furukawa Gadani <lfg@chromium.org>
2019-11-07 15:48:35 +0000

    CSP fuzzer: Initialize ICU environment when fuzzing.
    Bug: 1020830
    Change-Id: Icd73d4390504a5820c94ed0cbcedb915cba5384b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899310
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713442}

--- services/network/content_security_policy_fuzzer.cc
+++ services/network/content_security_policy_fuzzer.cc
@@ -7,12 +7,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/at_exit.h"
+#include "base/i18n/icu_util.h"
 #include "net/http/http_response_headers.h"
 #include "testing/libfuzzer/libfuzzer_exports.h"
+
+namespace {
+
+// This is a workaround for https://crbug.com/778929.
+struct IcuEnvironment {
+  IcuEnvironment() { CHECK(base::i18n::InitializeICU()); }
+  // used by ICU integration.
+  base::AtExitManager at_exit_manager;
+};
+
+}  // namespace
 
 namespace network {
 
 int LLVMFuzzerInitialize(int* argc, char*** argv) {
+  static IcuEnvironment env;
+
   return 0;
 }
 
