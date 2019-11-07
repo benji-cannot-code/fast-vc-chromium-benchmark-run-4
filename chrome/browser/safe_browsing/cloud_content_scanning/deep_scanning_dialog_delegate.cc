6e42f09673c99941aff762b01444014169358333
Roger Tawa <rogerta@chromium.org>
2019-11-07 18:26:35 +0000

    Update feature flag names for deep scan of uploads.
    Bug: 1021949
    Change-Id: I0b3ae7dcb2c53902f010820f839f8a85414f888d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903950
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713489}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.cc
@@ -37,12 +37,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace safe_browsing {
 
-const base::Feature kDeepScanningOfUploads{"DeepScanningOfUploads",
+const base::Feature kDeepScanningOfUploads{"SafeBrowsingDeepScanningOfUploads",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
 
 // TODO(rogerta): keeping this disabled by default until UX is finalized.
-const base::Feature kDeepScanningOfUploadsUI{"DeepScanningOfUploadsUI",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kDeepScanningOfUploadsUI{
+    "SafeBrowsingDeepScanningOfUploadsUI", base::FEATURE_DISABLED_BY_DEFAULT};
 
 namespace {
 
