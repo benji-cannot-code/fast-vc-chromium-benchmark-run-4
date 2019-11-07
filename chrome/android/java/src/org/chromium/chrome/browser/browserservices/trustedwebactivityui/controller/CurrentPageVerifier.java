049b8856c07612e674eb5823552e37660b67a3af
Peter Kotwicz <pkotwicz@chromium.org>
2019-11-07 00:36:34 +0000

    Move TabObserverRegistar to org.chromium.chrome.browser.customtabs.content
    This CL moves TabObserverRegistrar to
    org.chromium.chrome.browser.customtabs.content in preparation for adding
    CustomTabActivity-specific logic
    
    BUG=1020911
    R=peconn
    TBR=yfriedman
        (for moving TabObserverRegistrar to customtabs/content)
    
    Change-Id: I41b3a525590939104f5557a390ee5c862ac7bc03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1901833
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713242}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/controller/CurrentPageVerifier.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/controller/CurrentPageVerifier.java
@@ -5,29 +5,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.browserservices.trustedwebactivityui.controller;
 
+import androidx.annotation.IntDef;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import org.chromium.base.ObserverList;
 import org.chromium.base.Promise;
 import org.chromium.chrome.browser.browserservices.BrowserServicesIntentDataProvider;
 import org.chromium.chrome.browser.browserservices.trustedwebactivityui.TrustedWebActivityModel;
 import org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider;
 import org.chromium.chrome.browser.customtabs.content.CustomTabActivityTabProvider;
+import org.chromium.chrome.browser.customtabs.content.TabObserverRegistrar;
 import org.chromium.chrome.browser.dependency_injection.ActivityScope;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.lifecycle.NativeInitObserver;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabObserver;
-import org.chromium.chrome.browser.tab.TabObserverRegistrar;
 import org.chromium.content_public.browser.NavigationHandle;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 import javax.inject.Inject;
-
-import androidx.annotation.IntDef;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 /**
  * Checks whether the currently seen web page belongs to a verified origin and updates the
