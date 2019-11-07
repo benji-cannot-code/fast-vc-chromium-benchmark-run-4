1089f29d266adf6fdfe0f058541f29735a285a3c
Aleks Totic <atotic@chromium.org>
2019-11-07 21:10:18 +0000

    Fix DCHECK crasher
    Change-Id: Ie6e3a29b278ee49e49dbfab0f6cfee0d1b77d6f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903539
    Commit-Queue: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Aleks Totic <atotic@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713594}

--- third_party/blink/renderer/core/layout/ng/table/interface_casting.h
+++ third_party/blink/renderer/core/layout/ng/table/interface_casting.h
@@ -70,7 +70,8 @@ const Derived& ToInterface(const Base& from) {
 
 template <typename Derived, typename Base>
 const Derived* ToInterface(const Base* from) {
-  SECURITY_DCHECK(InterfaceDowncastTraits<Derived>::AllowFrom(*from));
+  if (from)
+    SECURITY_DCHECK(InterfaceDowncastTraits<Derived>::AllowFrom(*from));
   return from ? &InterfaceDowncastTraits<Derived>::ConvertFrom(*from) : nullptr;
 }
 
@@ -84,7 +85,8 @@ Derived& ToInterface(Base& from) {
 
 template <typename Derived, typename Base>
 Derived* ToInterface(Base* from) {
-  SECURITY_DCHECK(InterfaceDowncastTraits<Derived>::AllowFrom(*from));
+  if (from)
+    SECURITY_DCHECK(InterfaceDowncastTraits<Derived>::AllowFrom(*from));
   // const_cast is safe because from is not const.
   return from ? const_cast<Derived*>(
                     &InterfaceDowncastTraits<Derived>::ConvertFrom(*from))
