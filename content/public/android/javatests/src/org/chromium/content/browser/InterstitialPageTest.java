9dd412a292dab4c7fe6b9ceb45bae42234aa3960
Hongchan Choi <hongchan@chromium.org>
2019-11-07 22:13:16 +0000

    Disable org.chromium.content.browser.InterstitialPageTest#testCloseInterstitial
    See details in:
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVycgsSBUZsYWtlImdjaHJvbWl1bUBjb250ZW50X3NoZWxsX3Rlc3RfYXBrQG9yZy5jaHJvbWl1bS5jb250ZW50LmJyb3dzZXIuSW50ZXJzdGl0aWFsUGFnZVRlc3QjdGVzdENsb3NlSW50ZXJzdGl0aWFsDA
    
    
    TBR=jinsukkim@chromium.org
    
    Bug: 1022324
    Change-Id: I36eb261d9f8c0f33884b2c40ee5d6f92aaeae494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903541
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Auto-Submit: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713628}

--- content/public/android/javatests/src/org/chromium/content/browser/InterstitialPageTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/InterstitialPageTest.java
@@ -14,8 +14,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
@@ -93,7 +93,7 @@ public class InterstitialPageTest {
     @Test
     @LargeTest
     @Feature({"Navigation"})
-    @RetryOnFailure
+    @DisabledTest(message = "crbug.com/1022324")
     public void testCloseInterstitial() throws ExecutionException {
         final String proceedCommand = "PROCEED";
         final String htmlContent = "<html>"
