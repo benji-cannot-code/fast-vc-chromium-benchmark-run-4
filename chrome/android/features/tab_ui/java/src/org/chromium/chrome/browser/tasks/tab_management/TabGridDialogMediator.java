226475592fd092704b8ecaf668a23659a744b335
Yue Zhang <yuezhanggg@chromium.org>
2019-11-07 18:44:17 +0000

    Exit tab group title editing when entering TabGridDialog selection mode
    Bug: 1019101
    Change-Id: Ie59028544a1953282a683f8740efc5fcc41e6d84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895161
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713499}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
@@ -103,6 +103,7 @@ public class TabGridDialogMediator {
     private int mCurrentTabId = Tab.INVALID_TAB_ID;
     private boolean mIsUpdatingTitle;
     private String mCurrentGroupModifiedTitle;
+    private Callback<Integer> mToolbarMenuCallback;
 
     TabGridDialogMediator(Context context, DialogController dialogController, PropertyModel model,
             TabModelSelector tabModelSelector, TabCreatorManager tabCreatorManager,
@@ -198,6 +199,14 @@ public class TabGridDialogMediator {
         mTabModelSelector.addObserver(mTabModelSelectorObserver);
         assert mTabModelSelector.getTabModelFilterProvider().getCurrentTabModelFilter()
                         instanceof TabGroupModelFilter;
+
+        mToolbarMenuCallback = result -> {
+            if (result == R.id.ungroup_tab) {
+                mModel.set(TabGridPanelProperties.IS_POPUP_WINDOW_FOCUSABLE, false);
+                List<Tab> tabs = getRelatedTabs(mCurrentTabId);
+                mTabSelectionEditorController.show(tabs);
+            }
+        };
 
         // Setup toolbar button click listeners.
         setupToolbarClickHandlers();
@@ -412,13 +421,8 @@ public class TabGridDialogMediator {
 
     private View.OnClickListener getMenuButtonClickListener() {
         assert mTabSelectionEditorController != null;
-        Callback<Integer> callback = result -> {
-            if (result == R.id.ungroup_tab) {
-                List<Tab> tabs = getRelatedTabs(mCurrentTabId);
-                mTabSelectionEditorController.show(tabs);
-            }
-        };
-        return TabGridDialogMenuCoordinator.getTabGridDialogMenuOnClickListener(callback);
+        return TabGridDialogMenuCoordinator.getTabGridDialogMenuOnClickListener(
+                mToolbarMenuCallback);
     }
 
     private List<Tab> getRelatedTabs(int tabId) {
@@ -502,5 +506,10 @@ public class TabGridDialogMediator {
     @VisibleForTesting
     String getCurrentGroupModifiedTitleForTesting() {
         return mCurrentGroupModifiedTitle;
+    }
+
+    @VisibleForTesting
+    Callback<Integer> getToolbarMenuCallbackForTesting() {
+        return mToolbarMenuCallback;
     }
 }
