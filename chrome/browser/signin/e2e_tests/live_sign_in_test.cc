20b13e0eed48e3dd826eeb4fb69c28bcaee035ad
Alex Ilin <alexilin@chromium.org>
2019-11-07 19:07:38 +0000

    Rename DemoSignInTest to LiveSignInTest
    Since the test file now contains real tests, it shouldn't be called demo.
    
    Change-Id: I46d4cc153543451ddc66265c334e91c100d778a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899505
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713516}

--- chrome/browser/signin/e2e_tests/live_sign_in_test.cc
+++ chrome/browser/signin/e2e_tests/live_sign_in_test.cc
@@ -75,10 +75,10 @@ class SignOutTestObserver : public IdentityManager::Observer {
 };
 
 // Live tests for SignIn.
-class DemoSignInTest : public signin::test::LiveTest {
+class LiveSignInTest : public signin::test::LiveTest {
  public:
-  DemoSignInTest() = default;
-  ~DemoSignInTest() override = default;
+  LiveSignInTest() = default;
+  ~LiveSignInTest() override = default;
 
   void SetUp() override {
     LiveTest::SetUp();
@@ -153,7 +153,7 @@ class DemoSignInTest : public signin::test::LiveTest {
 // Sings in an account through the settings page and checks that the account is
 // added to Chrome. Sync should be disabled because the test doesn't pass
 // through the Sync confirmation dialog.
-IN_PROC_BROWSER_TEST_F(DemoSignInTest, SimpleSignInFlow) {
+IN_PROC_BROWSER_TEST_F(LiveSignInTest, SimpleSignInFlow) {
   TestAccount ta;
   CHECK(GetTestAccountsUtil()->GetAccount("TEST_ACCOUNT_1", ta));
   SignInFromSettings(ta);
@@ -174,7 +174,7 @@ IN_PROC_BROWSER_TEST_F(DemoSignInTest, SimpleSignInFlow) {
 // Sync is enabled.
 // Then, signs out on the web and checks that the account is removed from
 // cookies and Sync paused error is displayed.
-IN_PROC_BROWSER_TEST_F(DemoSignInTest, WebSignOut) {
+IN_PROC_BROWSER_TEST_F(LiveSignInTest, WebSignOut) {
   TestAccount test_account;
   CHECK(GetTestAccountsUtil()->GetAccount("TEST_ACCOUNT_1", test_account));
   TurnOnSync(test_account);
@@ -208,7 +208,7 @@ IN_PROC_BROWSER_TEST_F(DemoSignInTest, WebSignOut) {
 // Sings in two accounts on the web and checks that cookies and refresh tokens
 // are added to Chrome. Sync should be disabled.
 // Then, signs out on the web and checks that accounts are removed from Chrome.
-IN_PROC_BROWSER_TEST_F(DemoSignInTest, WebSignInAndSignOut) {
+IN_PROC_BROWSER_TEST_F(LiveSignInTest, WebSignInAndSignOut) {
   TestAccount test_account_1;
   CHECK(GetTestAccountsUtil()->GetAccount("TEST_ACCOUNT_1", test_account_1));
   SignInFromWeb(test_account_1);
@@ -254,7 +254,7 @@ IN_PROC_BROWSER_TEST_F(DemoSignInTest, WebSignInAndSignOut) {
 // Sync is enabled. Signs in a second account on the web.
 // Then, turns Sync off from the settings page and checks that both accounts are
 // removed from Chrome and from cookies.
-IN_PROC_BROWSER_TEST_F(DemoSignInTest, TurnOffSync) {
+IN_PROC_BROWSER_TEST_F(LiveSignInTest, TurnOffSync) {
   TestAccount test_account_1;
   CHECK(GetTestAccountsUtil()->GetAccount("TEST_ACCOUNT_1", test_account_1));
   TurnOnSync(test_account_1);
