c207c9e37b9088b51dbbe15139d00a5af05c64ba
Eugene But <eugenebut@chromium.org>
2019-11-07 00:23:03 +0000

    Disabled testMostVisitedNewTab and testMostVisitedNewIncognitoTab on EG2 bot.
    Bug: 1022152
    TBR: gambard@chromium.org
    Change-Id: I51871d233e7ae1bce32d63525b551ac1a0d4faeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902519
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713239}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
@@ -277,6 +277,11 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 
 // Tests the "Open in New Tab" action of the Most Visited context menu.
 - (void)testMostVisitedNewTab {
+// TODO(crbug.com/1022152): Enable this test.
+#if defined(CHROME_EARL_GREY_2)
+  EARL_GREY_TEST_DISABLED(@"Fails with EG2");
+#endif
+
   [self setupMostVisitedTileLongPress];
   const GURL pageURL = self.testServer->GetURL(kPageURL);
 
@@ -314,6 +319,11 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // Tests the "Open in New Incognito Tab" action of the Most Visited context
 // menu.
 - (void)testMostVisitedNewIncognitoTab {
+// TODO(crbug.com/1022152): Enable this test.
+#if defined(CHROME_EARL_GREY_2)
+  EARL_GREY_TEST_DISABLED(@"Fails with EG2");
+#endif
+
   [self setupMostVisitedTileLongPress];
   const GURL pageURL = self.testServer->GetURL(kPageURL);
 
