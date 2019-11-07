f4922a367d6c3d05bbcc7fbe2b880c547f44e6f5
Tim Volodine <timvolodine@chromium.org>
2019-11-07 13:44:11 +0000

    [WebLayer] Add missing initialization bits for safebrowsing.
    In this patch:
    - Add safebrowsing threat types to watch (currently malware,
      phishing, unwanted and billing). This is needed by the
      RemoteSafeBrowsingDatabaseManager.
    - Create and set native SafeBrowsingApiHandlerBridge during
      initialization. This is needed for native <-> java
      interactions.
    
    BUG=1015418
    
    Change-Id: I2ea97435a6e3885382ca6cad163337acd17dafa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1901045
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713419}

--- weblayer/browser/safe_browsing/safe_browsing_service.cc
+++ weblayer/browser/safe_browsing/safe_browsing_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/task/post_task.h"
 #include "components/safe_browsing/android/remote_database_manager.h"
+#include "components/safe_browsing/android/safe_browsing_api_handler_bridge.h"
 #include "components/safe_browsing/browser/browser_url_loader_throttle.h"
 #include "components/safe_browsing/browser/mojo_safe_browsing_impl.h"
 #include "components/safe_browsing/browser/safe_browsing_network_context.h"
@@ -45,6 +46,11 @@ void SafeBrowsingService::Initialize() {
     // already initialized
     return;
   }
+
+  safe_browsing_api_handler_.reset(
+      new safe_browsing::SafeBrowsingApiHandlerBridge());
+  safe_browsing::SafeBrowsingApiHandler::SetInstance(
+      safe_browsing_api_handler_.get());
 
   base::FilePath user_data_dir;
   bool result =
--- weblayer/browser/safe_browsing/safe_browsing_service.h
+++ weblayer/browser/safe_browsing/safe_browsing_service.h
@@ -27,6 +27,7 @@ class SharedURLLoaderFactory;
 namespace safe_browsing {
 class UrlCheckerDelegate;
 class RemoteSafeBrowsingDatabaseManager;
+class SafeBrowsingApiHandler;
 class SafeBrowsingNetworkContext;
 }  // namespace safe_browsing
 
@@ -81,6 +82,9 @@ class SafeBrowsingService {
 
   scoped_refptr<safe_browsing::UrlCheckerDelegate>
       safe_browsing_url_checker_delegate_;
+
+  std::unique_ptr<safe_browsing::SafeBrowsingApiHandler>
+      safe_browsing_api_handler_;
 
   std::string user_agent_;
 
