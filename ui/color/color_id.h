376ab4e0c045b6dfd8322097402dd548cd59ec29
Peter Kasting <pkasting@chromium.org>
2019-11-07 03:34:30 +0000

    Reland: Windows native color mixer.  Not yet instantiated.
    Bug: 1003612
    Change-Id: I6102b7923150414c339745b3831dc607b5c145ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902719
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713310}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_COLOR_COLOR_ID_H_
 #define UI_COLOR_COLOR_ID_H_
 
+#include "build/build_config.h"
+
 namespace ui {
 
 // ColorId contains identifiers for all input, intermediary, and output colors
@@ -19,6 +21,40 @@ enum ColorIds : ColorId {
 
   // TODO(pkasting): Define this list.
   kColorX = kUiColorsStart,
+
+#if defined(OS_WIN)
+  // Windows native colors
+  kColorNative3dDkShadow,
+  kColorNative3dLight,
+  kColorNativeActiveBorder,
+  kColorNativeActiveCaption,
+  kColorNativeAppWorkspace,
+  kColorNativeBackground,
+  kColorNativeBtnFace,
+  kColorNativeBtnHighlight,
+  kColorNativeBtnShadow,
+  kColorNativeBtnText,
+  kColorNativeCaptionText,
+  kColorNativeGradientActiveCaption,
+  kColorNativeGradientInactiveCaption,
+  kColorNativeGrayText,
+  kColorNativeHighlight,
+  kColorNativeHighlightText,
+  kColorNativeHotlight,
+  kColorNativeInactiveBorder,
+  kColorNativeInactiveCaption,
+  kColorNativeInactiveCaptionText,
+  kColorNativeInfoBk,
+  kColorNativeInfoText,
+  kColorNativeMenu,
+  kColorNativeMenuBar,
+  kColorNativeMenuHilight,
+  kColorNativeMenuText,
+  kColorNativeScrollbar,
+  kColorNativeWindow,
+  kColorNativeWindowFrame,
+  kColorNativeWindowText,
+#endif  // defined(OS_WIN)
 
   // Embedders must start color IDs from this value.
   kUiColorsEnd,
@@ -38,7 +74,10 @@ enum ColorSetIds : ColorSetId {
   kUiColorSetsStart = kUiColorsLast + 1,
 
   // TODO(pkasting): Define this list.
-  kColorSetX = kUiColorSetsStart,
+
+  // A set of color IDs whose values match the native platform as closely as
+  // possible.
+  kColorSetNative = kUiColorSetsStart,
 
   // Embedders must start color set IDs from this value.
   kUiColorSetsEnd,
