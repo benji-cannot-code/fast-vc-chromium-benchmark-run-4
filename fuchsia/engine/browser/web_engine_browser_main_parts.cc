5f635f431d6334fe106273a79b26bfb9ecb66911
Wez <wez@chromium.org>
2019-11-07 00:28:32 +0000

    [fuchsia] Clean up unused WebEngineScreen an EXPORT macro.
    - Remove the unused WebEngineScreen stub.
    - Remove an errant BASE_EXPORT macro on cr_fuchsia::AgentManager.
    
    Change-Id: I5546c78b9428b56a707fa1b059fb60e9875cd337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1896393
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713240}

--- fuchsia/engine/browser/web_engine_browser_main_parts.cc
+++ fuchsia/engine/browser/web_engine_browser_main_parts.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia/engine/browser/context_impl.h"
 #include "fuchsia/engine/browser/web_engine_browser_context.h"
 #include "fuchsia/engine/browser/web_engine_devtools_controller.h"
-#include "fuchsia/engine/browser/web_engine_screen.h"
 #include "fuchsia/engine/switches.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
 #include "ui/aura/screen_ozone.h"
