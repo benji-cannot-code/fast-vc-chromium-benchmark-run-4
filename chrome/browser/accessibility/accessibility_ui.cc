32124f4b7e71fe893ce00d22bd77b47e57185bbe
Abigail Klein <abigailbklein@google.com>
2019-11-07 19:06:55 +0000

    [chrome:accessibility] Observe AXEvents
    Observe AXEvents received by the RenderFrameHost. Log them in the
    the accessibility events recorder, which records events from the
    OS.
    
    chrome: accessibility page, intermixed with the events recorded by
    Bug: 785493
    Change-Id: I7a72f786567c1f661de9f0945c01dc4d46789e72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900209
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713515}

--- chrome/browser/accessibility/accessibility_ui.cc
+++ chrome/browser/accessibility/accessibility_ui.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/accessibility_tree_formatter.h"
+#include "content/public/browser/ax_event_notification_details.h"
 #include "content/public/browser/browser_accessibility_state.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/favicon_status.h"
@@ -349,11 +350,25 @@ AccessibilityUI::AccessibilityUI(content::WebUI* web_ui)
   web_ui->AddMessageHandler(std::make_unique<AccessibilityUIMessageHandler>());
 }
 
-AccessibilityUI::~AccessibilityUI() {}
+AccessibilityUI::~AccessibilityUI() = default;
 
-AccessibilityUIMessageHandler::AccessibilityUIMessageHandler() {}
+AccessibilityUIObserver::AccessibilityUIObserver(
+    content::WebContents* web_contents,
+    std::vector<std::string>* event_logs)
+    : content::WebContentsObserver(web_contents), event_logs_(event_logs) {}
 
-AccessibilityUIMessageHandler::~AccessibilityUIMessageHandler() {}
+AccessibilityUIObserver::~AccessibilityUIObserver() = default;
+
+void AccessibilityUIObserver::AccessibilityEventReceived(
+    const content::AXEventNotificationDetails& details) {
+  for (const ui::AXEvent event : details.events) {
+    event_logs_->push_back(event.ToString());
+  }
+}
+
+AccessibilityUIMessageHandler::AccessibilityUIMessageHandler() = default;
+
+AccessibilityUIMessageHandler::~AccessibilityUIMessageHandler() = default;
 
 void AccessibilityUIMessageHandler::RegisterMessages() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
@@ -650,11 +665,15 @@ void AccessibilityUIMessageHandler::RequestAccessibilityEvents(
         base::BindRepeating(&AccessibilityUIMessageHandler::Callback,
                             base::Unretained(this)),
         true);
+    observer_ =
+        std::make_unique<AccessibilityUIObserver>(web_contents, &event_logs_);
   } else {
     web_contents->RecordAccessibilityEvents(
         base::BindRepeating(&AccessibilityUIMessageHandler::Callback,
                             base::Unretained(this)),
         false);
+    observer_.release();
+
     std::string event_logs_str;
     for (std::string log : event_logs_) {
       event_logs_str += log;
