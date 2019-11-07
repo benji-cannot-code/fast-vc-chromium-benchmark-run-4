a15253a80dc33812f426c62b5e0808f1d2ce2f94
Ken Rockot <rockot@google.com>
2019-11-07 20:36:40 +0000

    Allow multiple DataDecoderService receivers
    DataDecoderService is currently set up to allow only a single client
    pipe. This is reasonable for the out-of-process case in most production
    environments. For the in-process case (in tests and on iOS) however, it
    is possible and reasonable for a single instance to have multiple
    DataDecoderService pipes connected to it.
    
    This upgrades the lone Receiver to a ReceiverSet, allowing for multiple
    clients.
    
    Fixed: 1022313
    Change-Id: Iaeea6e1b1316351da0b6f57ae4a432eed62b5575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903748
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#713566}

--- services/data_decoder/data_decoder_service.h
+++ services/data_decoder/data_decoder_service.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
-#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
 #include "services/data_decoder/public/mojom/bundled_exchanges_parser.mojom.h"
 #include "services/data_decoder/public/mojom/data_decoder_service.mojom.h"
 #include "services/data_decoder/public/mojom/image_decoder.mojom.h"
@@ -62,7 +62,9 @@ class DataDecoderService : public mojom::DataDecoderService {
       mojo::PendingReceiver<mojom::BleScanParser> receiver) override;
 #endif  // OS_CHROMEOS
 
-  mojo::Receiver<mojom::DataDecoderService> receiver_{this};
+  // In-process instances (e.g. on iOS or in tests) may have multiple concurrent
+  // remote DataDecoderService clients.
+  mojo::ReceiverSet<mojom::DataDecoderService> receivers_;
 
   bool drop_image_decoders_ = false;
   bool drop_json_parsers_ = false;
