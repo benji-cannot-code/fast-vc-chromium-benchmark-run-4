8decb2e8a49ddac73a89c0db65379d28f4338340
Zhongyi Shi <zhongyi@chromium.org>
2019-11-07 02:06:28 +0000

    Fix a socket NULL-dereference in ProcessReadResult.
    The QuicChromiumPacketReader upon finishing the socket read, will dereference
    the socket to get local and peer addresses and report packet back to the
    visitor unconditionally even if the connection is closed. The patch landed in
    crrev.com/c/1900569 nulls out the socket upon connection close, leading to null
    dereference when fetching local and peer addresses.
    
    Bug: 1014092, 1021938
    Change-Id: I9af79f03b3e2ffbe1ed036af8980efa6b6d1478c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1901946
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Auto-Submit: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713287}

--- net/quic/quic_chromium_packet_reader.h
+++ net/quic/quic_chromium_packet_reader.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "net/base/io_buffer.h"
+#include "net/base/ip_endpoint.h"
 #include "net/base/net_export.h"
 #include "net/log/net_log_with_source.h"
 #include "net/socket/datagram_client_socket.h"
@@ -66,6 +67,9 @@ class NET_EXPORT_PRIVATE QuicChromiumPacketReader {
   void SetShouldStopReading() {
     DCHECK(!should_stop_reading_);
     should_stop_reading_ = true;
+    // Cache local and peer addresses before null out the socket.
+    socket_->GetLocalAddress(&local_address_);
+    socket_->GetPeerAddress(&peer_address_);
     socket_ = nullptr;
   }
 
@@ -86,6 +90,8 @@ class NET_EXPORT_PRIVATE QuicChromiumPacketReader {
   bool ProcessReadResult(int result);
 
   DatagramClientSocket* socket_;
+  IPEndPoint local_address_;
+  IPEndPoint peer_address_;
   // Set to true if |this| should no longer attempt read.
   bool should_stop_reading_;
 
