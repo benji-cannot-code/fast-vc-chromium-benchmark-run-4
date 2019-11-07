f0b7666015107cf208f135d74c204429b15eee33
Andrew Grieve <agrieve@google.com>
2019-11-07 20:15:10 +0000

    Fix a few instances of missing "const".
    Found via SuperSize query:
      size_info.symbols.WhereFullNameMatches(r'\bk[A-Z]').WhereInSection('d')
    
    TBR=agrieve  # Trivial refactor
    
    Bug: 747064
    Change-Id: Ib9615718e0f6cacb081f9ea2f5295348d32cdcb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1901641
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713557}

--- content/browser/devtools/protocol/tethering_handler.cc
+++ content/browser/devtools/protocol/tethering_handler.cc
@@ -34,7 +34,7 @@ const int kSocketPumpBufferSize = 16 * 1024;
 const int kMinTetheringPort = 1024;
 const int kMaxTetheringPort = 65535;
 
-net::NetworkTrafficAnnotationTag kTrafficAnnotation =
+const net::NetworkTrafficAnnotationTag kTrafficAnnotation =
     net::DefineNetworkTrafficAnnotation("tethering_handler_socket", R"(
         semantics {
           sender: "Tethering Handler"
--- net/socket/socket_bio_adapter.cc
+++ net/socket/socket_bio_adapter.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-net::NetworkTrafficAnnotationTag kTrafficAnnotation =
+const net::NetworkTrafficAnnotationTag kTrafficAnnotation =
     net::DefineNetworkTrafficAnnotation("socket_bio_adapter", R"(
       semantics {
         sender: "Socket BIO Adapter"
