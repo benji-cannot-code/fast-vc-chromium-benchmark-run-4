f64441991a814db5b069eefb07870a74f3574312
Rob Schonberger <robsc@chromium.org>
2019-11-07 23:44:33 +0000

    Add FeatureParam for Radius Polynomial, unittest thoroughly.
    This CL adds a featureparam for experimental updates of radius
    polynomial - and adds a parser for the string value.
    
    The parser is tested both via the factory and as a standalone - which
    is why it's placed in a separated namespace rather than the natural
    anonymous namespace.
    
    Bug: 1009290
    Change-Id: I8d9edcd9c8267f6d85026f79bd30dc22cacefbc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903108
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713657}

--- ui/events/ozone/evdev/touch_filter/palm_detection_filter_factory.cc
+++ ui/events/ozone/evdev/touch_filter/palm_detection_filter_factory.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/feature_list.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_split.h"
 #include "base/time/time.h"
 #include "ui/events/ozone/evdev/event_device_info.h"
 #include "ui/events/ozone/evdev/touch_filter/heuristic_stylus_palm_detection_filter.h"
@@ -26,6 +28,10 @@ const base::Feature kEnableHeuristicPalmDetectionFilter{
 const base::Feature kEnableNeuralPalmDetectionFilter{
     "EnableNeuralPalmDetectionFilter", base::FEATURE_DISABLED_BY_DEFAULT};
 
+EVENTS_OZONE_EVDEV_EXPORT
+extern const base::FeatureParam<std::string> kNeuralPalmRadiusPolynomial{
+    &kEnableNeuralPalmDetectionFilter, "neural_palm_radius_polynomial", ""};
+
 const base::FeatureParam<double> kHeuristicCancelThresholdSeconds{
     &kEnableHeuristicPalmDetectionFilter,
     "heuristic_palm_cancel_threshold_seconds", 0.4};
@@ -37,15 +43,38 @@ const base::FeatureParam<double> kHeuristicHoldThresholdSeconds{
 const base::FeatureParam<int> kHeuristicStrokeCount{
     &kEnableHeuristicPalmDetectionFilter, "heuristic_palm_stroke_count", 0};
 
+namespace internal {
+
+std::vector<float> ParseRadiusPolynomial(const std::string& radius_string) {
+  std::vector<std::string> split_radii = base::SplitString(
+      radius_string, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+  std::vector<float> return_value;
+  for (const std::string& unparsed : split_radii) {
+    double item;
+    if (!base::StringToDouble(unparsed, &item) && !std::isnan(item)) {
+      LOG(DFATAL) << "Unable to parse " << unparsed
+                  << " to a floating point; Returning empty.";
+      return {};
+    }
+    return_value.push_back(item);
+  }
+  return return_value;
+}
+
+}  // namespace internal
+
 std::unique_ptr<PalmDetectionFilter> CreatePalmDetectionFilter(
     const EventDeviceInfo& devinfo,
     SharedPalmDetectionFilterState* shared_palm_state) {
   if (base::FeatureList::IsEnabled(kEnableNeuralPalmDetectionFilter) &&
       NeuralStylusPalmDetectionFilter::
           CompatibleWithNeuralStylusPalmDetectionFilter(devinfo)) {
+    std::vector<float> radius_polynomial =
+        internal::ParseRadiusPolynomial(kNeuralPalmRadiusPolynomial.Get());
     // Theres only one model right now.
     std::unique_ptr<NeuralStylusPalmDetectionFilterModel> model =
-        std::make_unique<OneDeviceTrainNeuralStylusPalmDetectionFilterModel>();
+        std::make_unique<OneDeviceTrainNeuralStylusPalmDetectionFilterModel>(
+            radius_polynomial);
     return std::make_unique<NeuralStylusPalmDetectionFilter>(
         devinfo, std::move(model), shared_palm_state);
   }
--- ui/events/ozone/evdev/touch_filter/palm_detection_filter_factory.h
+++ ui/events/ozone/evdev/touch_filter/palm_detection_filter_factory.h
@@ -26,6 +26,9 @@ EVENTS_OZONE_EVDEV_EXPORT
 extern const base::Feature kEnableNeuralPalmDetectionFilter;
 
 EVENTS_OZONE_EVDEV_EXPORT
+extern const base::FeatureParam<std::string> kNeuralPalmRadiusPolynomial;
+
+EVENTS_OZONE_EVDEV_EXPORT
 extern const base::FeatureParam<double> kHeuristicCancelThresholdSeconds;
 
 EVENTS_OZONE_EVDEV_EXPORT
@@ -37,6 +40,13 @@ extern const base::FeatureParam<int> kHeuristicStrokeCount;
 EVENTS_OZONE_EVDEV_EXPORT std::unique_ptr<PalmDetectionFilter>
 CreatePalmDetectionFilter(const EventDeviceInfo& devinfo,
                           SharedPalmDetectionFilterState* shared_palm_state);
+
+namespace internal {
+// In a named namespace for testing.
+
+EVENTS_OZONE_EVDEV_EXPORT std::vector<float> ParseRadiusPolynomial(
+    const std::string& radius_string);
+}  // namespace internal
 
 }  // namespace ui
 
