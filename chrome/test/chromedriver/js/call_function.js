39a8584d5be063d9012434a1ab264125a31098aa
Mike Pennisi <mike@mikepennisi.com>
2019-11-07 18:42:09 +0000

    [ChromeDriver] Avoid cache interference
    Use an object with a null prototype for the internal mapping of names to
    cached values. This reduces the likelihood of unintended interaction
    with the environment (e.g. when new enumerable prototypes are defined on
    the Object prototype).
    
    Update the testing code so that this change is verifiable.
    
    Bug: 1014780
    Change-Id: Ifbdc8d09c1049d0ec07f7a0099d774579423cdb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894707
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713496}

--- chrome/test/chromedriver/js/call_function.js
+++ chrome/test/chromedriver/js/call_function.js
@@ -83,7 +83,7 @@ function newError(message, code) {
  * @constructor
  */
 function CacheWithUUID() {
-  this.cache_ = {};
+  this.cache_ = Object.create(null);
 }
 
 CacheWithUUID.prototype = {
@@ -147,7 +147,7 @@ CacheWithUUID.prototype = {
  * @constructor
  */
 function Cache() {
-  this.cache_ = {};
+  this.cache_ = Object.create(null);
   this.nextId_ = 1;
   this.idPrefix_ = Math.random().toString();
 }
