6b786c2f96f45be2f148cd2d6592fce491b8f254
Dmitry Torokhov <dtor@chromium.org>
2019-11-07 18:09:11 +0000

    Pass owners's cryptohome ID when starting Plugin VM dispatcher
    The dispatcher state will reside in daemon store/cryptohome instead of
    global stateful to avoid cross-user information disclosure, so it needs
    to know cryptohome ID of the session owner in order to bind-mount
    the right state directory.
    
    Bug: b:143443446
    Change-Id: Id29b10f2981fe9e919fe41d3e0f4c3832bde1654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899838
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Dmitry Torokhov <dtor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713488}

--- chrome/browser/chromeos/plugin_vm/plugin_vm_image_manager.cc
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_image_manager.cc
@@ -150,6 +150,7 @@ void PluginVmImageManager::StartImport() {
   chromeos::DBusThreadManager::Get()
       ->GetDebugDaemonClient()
       ->StartPluginVmDispatcher(
+          chromeos::ProfileHelper::GetUserIdHashFromProfile(profile_),
           base::BindOnce(&PluginVmImageManager::OnPluginVmDispatcherStarted,
                          weak_ptr_factory_.GetWeakPtr()));
 }
