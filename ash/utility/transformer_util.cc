c9d47b479cefef51345b8e97f765576d7dadd886
Michael Spang <spang@chromium.org>
2019-11-07 01:55:36 +0000

    ash: Remove RoundNearZero
    This shouldn't be necessary any longer; it was fixing small errors
    introduces by computing rotations using trig functions.
    
    Bug: 1019015
    Change-Id: Ia6c273454f96c556f34d0adca391eb0e121a98b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1885635
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713280}

--- ash/utility/transformer_util.cc
+++ ash/utility/transformer_util.cc
@@ -14,18 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
-// Round near zero value to zero.
-void RoundNearZero(gfx::Transform* transform) {
-  const float kEpsilon = 0.001f;
-  SkMatrix44& matrix = transform->matrix();
-  for (int x = 0; x < 4; ++x) {
-    for (int y = 0; y < 4; ++y) {
-      if (std::abs(SkMScalarToFloat(matrix.get(x, y))) < kEpsilon)
-        matrix.set(x, y, SkFloatToMScalar(0.0f));
-    }
-  }
-}
-
 display::Display::Rotation RotationBetween(
     display::Display::Rotation old_rotation,
     display::Display::Rotation new_rotation) {
@@ -41,10 +29,6 @@ gfx::Transform CreateRotationTransform(display::Display::Rotation old_rotation,
                                        const gfx::SizeF& size_to_rotate) {
   gfx::Transform transform = display::Display::GetRotationTransform(
       RotationBetween(old_rotation, new_rotation), size_to_rotate);
-
-  // TODO(spang): Remove this; it shouldn't be necessary now that the rotation
-  // matrix is computed exactly.
-  RoundNearZero(&transform);
 
   return transform;
 }
