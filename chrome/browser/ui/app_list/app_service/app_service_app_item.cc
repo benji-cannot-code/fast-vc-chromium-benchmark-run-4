b5a44ef9d351b8c119aebcc2949db41f45719104
nancy <nancylingwang@chromium.org>
2019-11-08 21:17:43 +0000

    Calls ChromeLauncherController to active app for Chrome.
    Chrome is a special extenision app, which doesn't launch a new tab,
    but reuses the current item in shelf, when click the icon in
    app_list. So work around the issue for Chrome. When AppService
    Instance feature is ready, AppService should check the shelf id or
    window for Chrome, and not re-launch a new tab.
    
    BUG=1022541
    
    Change-Id: Iddac3f0749f8bad4fc5396f1152a73cef5c2ad54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1905303
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713959}

--- chrome/browser/ui/app_list/app_service/app_service_app_item.cc
+++ chrome/browser/ui/app_list/app_service/app_service_app_item.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/arc/arc_app_context_menu.h"
 #include "chrome/browser/ui/app_list/crostini/crostini_app_context_menu.h"
 #include "chrome/browser/ui/app_list/extension_app_context_menu.h"
+#include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 
 // static
 const char AppServiceAppItem::kItemType[] = "AppServiceAppItem";
@@ -104,6 +105,14 @@ void AppServiceAppItem::OnAppUpdate(const apps::AppUpdate& app_update,
 }
 
 void AppServiceAppItem::Activate(int event_flags) {
+  // TODO(crbug.com/1022541): Move the Chrome special case to ExtensionApps,
+  // when AppService Instance feature is done.
+  if (id() == extension_misc::kChromeAppId) {
+    ChromeLauncherController::instance()->ActivateApp(
+        id(), ash::LAUNCH_FROM_APP_LIST, event_flags,
+        GetController()->GetAppListDisplayId());
+    return;
+  }
   Launch(event_flags, apps::mojom::LaunchSource::kFromAppListGrid);
 }
 
