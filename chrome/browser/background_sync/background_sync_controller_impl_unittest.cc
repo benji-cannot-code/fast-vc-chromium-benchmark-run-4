6e4a162d4597ceedf671ac9fdfeb25d33273d2a2
Olivier Li <olivierli@google.com>
2019-11-08 13:24:32 +0000

    Fix temp_dir behavior in BackgroundSyncControllerImplTest.
    The current behavior of the test contains two problems:
    
    1) It uses a  directory path that is never created to to hold the HistoryBackend
    database. This leads to a consistent sql::INIT_FAILURE status that spams the logs
    and is ignored.
    
    2) |temp_dir_| that is used by |profile_| is destroyed first which leads to more errors.
    
    Problems in this test were found while trying to create some warnings for misuse
    of HistoryBackend in tests here: https://crrev.com/c/1898109.
    
    Bug: 661143
    Change-Id: I0181a4d8c50e846469dbd435520086e3b7e35442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900208
    Auto-Submit: Oliver Li <olivierli@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713807}

--- chrome/browser/background_sync/background_sync_controller_impl_unittest.cc
+++ chrome/browser/background_sync/background_sync_controller_impl_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "chrome/browser/engagement/site_engagement_score.h"
@@ -64,10 +65,12 @@ class BackgroundSyncControllerImplTest : public testing::Test {
   void SetUp() override {
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
 
+    base::FilePath sub_dir =
+        temp_dir_.GetPath().AppendASCII("BackgroundSyncTest");
+    ASSERT_TRUE(base::CreateDirectory(sub_dir));
+
     HistoryServiceFactory::GetInstance()->SetTestingFactory(
-        &profile_, base::BindRepeating(
-                       &BuildTestHistoryService,
-                       temp_dir_.GetPath().AppendASCII("BackgroundSyncTest")));
+        &profile_, base::BindRepeating(&BuildTestHistoryService, sub_dir));
     controller_ = std::make_unique<BackgroundSyncControllerImpl>(&profile_);
   }
 
@@ -95,10 +98,10 @@ class BackgroundSyncControllerImplTest : public testing::Test {
   }
 
   content::BrowserTaskEnvironment task_environment_;
+  base::ScopedTempDir temp_dir_;
   TestingProfile profile_;
   std::unique_ptr<BackgroundSyncControllerImpl> controller_;
   std::unique_ptr<base::FieldTrialList> field_trial_list_;
-  base::ScopedTempDir temp_dir_;
 
   DISALLOW_COPY_AND_ASSIGN(BackgroundSyncControllerImplTest);
 };
