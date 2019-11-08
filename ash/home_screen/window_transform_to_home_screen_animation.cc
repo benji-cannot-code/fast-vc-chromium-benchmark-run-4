71b2fe2f03f318f1e75acb4dd859d940a3d5e523
Toni Barzic <tbarzic@chromium.org>
2019-11-08 18:42:34 +0000

    Use window util to minimize window for window to home animation
    Updates WindowTransformToHomeScreenAnimation to use
    
    window_util::HideAndMaybeMinimizeWithoutAnimation().
    
    Change-Id: I3cb191d72a351bf6dd700857b85d2b0775aa4bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1905268
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713892}

--- ash/home_screen/window_transform_to_home_screen_animation.cc
+++ ash/home_screen/window_transform_to_home_screen_animation.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/screen_util.h"
 #include "ash/shell.h"
 #include "ash/wm/window_state.h"
+#include "ash/wm/window_util.h"
 #include "base/time/time.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
@@ -66,9 +67,8 @@ WindowTransformToHomeScreenAnimation::~WindowTransformToHomeScreenAnimation() {
 
 void WindowTransformToHomeScreenAnimation::OnImplicitAnimationsCompleted() {
   // Minimize the dragged window after transform animation is completed.
-  ScopedAnimationDisabler disable(window_);
-  window_->Hide();
-  WindowState::Get(window_)->Minimize();
+  window_util::HideAndMaybeMinimizeWithoutAnimation({window_},
+                                                    /*minimize=*/true);
 
   // Reset its transform to identity transform and its original backdrop mode.
   window_->layer()->SetTransform(gfx::Transform());
