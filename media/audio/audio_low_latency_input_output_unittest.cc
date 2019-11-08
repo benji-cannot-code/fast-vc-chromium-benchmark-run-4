a76e88dd617bd52e1b8ce2183820c38dbfef09ca
Raymond Toy <rtoy@chromium.org>
2019-11-08 21:14:00 +0000

    [Code Health] Convert Bind to BindOnce/Repeating
    Change base::Bind to base::BindOnce or base::BindRepeating as
    appropriate..
    
    Bug: 1007798
    Change-Id: I0bc1f2049624a4c4a1423139fd061ae84d6aa0ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902269
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713957}

--- media/audio/audio_low_latency_input_output_unittest.cc
+++ media/audio/audio_low_latency_input_output_unittest.cc
@@ -254,7 +254,7 @@ class AudioInputStreamTraits {
       const AudioParameters& params) {
     return audio_manager->MakeAudioInputStream(
         params, AudioDeviceDescription::kDefaultDeviceId,
-        base::Bind(&OnLogMessage));
+        base::BindRepeating(&OnLogMessage));
   }
 };
 
@@ -270,8 +270,8 @@ class AudioOutputStreamTraits {
 
   static StreamType* CreateStream(AudioManager* audio_manager,
       const AudioParameters& params) {
-    return audio_manager->MakeAudioOutputStream(params, std::string(),
-                                                base::Bind(&OnLogMessage));
+    return audio_manager->MakeAudioOutputStream(
+        params, std::string(), base::BindRepeating(&OnLogMessage));
   }
 };
 
