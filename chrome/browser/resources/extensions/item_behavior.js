753d0423c9965eea47051e1abbe68247fbf13b97
rbpotter <rbpotter@chromium.org>
2019-11-08 02:18:50 +0000

    Extensions Web UI: Run clang-format
    Run clang-format on chrome/browser/resources/extensions and
    chrome/test/data/webui/extensions
    
    These directories were left unformatted when converting to Polymer 3 in
    order to reduce the diff created when migrating and make it easier to
    see the changes required for migration.
    
    Bug: 1004967
    Change-Id: Ic1eb0ee43eccd56c340d0cf3956e29136b96d10e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1904488
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713716}

--- chrome/browser/resources/extensions/item_behavior.js
+++ chrome/browser/resources/extensions/item_behavior.js
@@ -5,25 +5,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assertNotReached} from 'chrome://resources/js/assert.m.js';
 
-  /** @polymerBehavior */
-  export const ItemBehavior = {
-    /**
-     * @param {chrome.developerPrivate.ExtensionType} type
-     * @param {string} appLabel
-     * @param {string} extensionLabel
-     * @return {string} The app or extension label depending on |type|.
-     */
-    appOrExtension: function(type, appLabel, extensionLabel) {
-      const ExtensionType = chrome.developerPrivate.ExtensionType;
-      switch (type) {
-        case ExtensionType.HOSTED_APP:
-        case ExtensionType.LEGACY_PACKAGED_APP:
-        case ExtensionType.PLATFORM_APP:
-          return appLabel;
-        case ExtensionType.EXTENSION:
-        case ExtensionType.SHARED_MODULE:
-          return extensionLabel;
-      }
-      assertNotReached('Item type is not App or Extension.');
-    },
-  };
+/** @polymerBehavior */
+export const ItemBehavior = {
+  /**
+   * @param {chrome.developerPrivate.ExtensionType} type
+   * @param {string} appLabel
+   * @param {string} extensionLabel
+   * @return {string} The app or extension label depending on |type|.
+   */
+  appOrExtension: function(type, appLabel, extensionLabel) {
+    const ExtensionType = chrome.developerPrivate.ExtensionType;
+    switch (type) {
+      case ExtensionType.HOSTED_APP:
+      case ExtensionType.LEGACY_PACKAGED_APP:
+      case ExtensionType.PLATFORM_APP:
+        return appLabel;
+      case ExtensionType.EXTENSION:
+      case ExtensionType.SHARED_MODULE:
+        return extensionLabel;
+    }
+    assertNotReached('Item type is not App or Extension.');
+  },
+};
