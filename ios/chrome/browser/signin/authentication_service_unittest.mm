ddb76af128364a83bca704642e3b12e6ee6d966c
Mihai Sardarescu <msarda@chromium.org>
2019-11-08 16:51:28 +0000

    Convert to use CoreAccountId on iOS.
    This CL converts all ChromeOS code to use CoreAccountId instead of
    std::string for the account id.
    
    Bug: 959157
    Change-Id: Ia27b9e566100ecd7c1ead3a5ca8e46da39e569ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906149
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713856}

--- ios/chrome/browser/signin/authentication_service_unittest.mm
+++ ios/chrome/browser/signin/authentication_service_unittest.mm
@@ -66,6 +66,10 @@ std::unique_ptr<KeyedService> BuildMockSyncSetupService(
       ProfileSyncServiceFactory::GetForBrowserState(browser_state));
 }
 
+CoreAccountId GetAccountId(ChromeIdentity* identity) {
+  return CoreAccountId(base::SysNSStringToUTF8([identity gaiaID]));
+}
+
 }  // namespace
 
 class AuthenticationServiceTest : public PlatformTest {
@@ -113,7 +117,7 @@ class AuthenticationServiceTest : public PlatformTest {
     authentication_service()->StoreKnownAccountsWhileInForeground();
   }
 
-  std::vector<std::string> GetLastKnownAccountsFromForeground() {
+  std::vector<CoreAccountId> GetLastKnownAccountsFromForeground() {
     return authentication_service()->GetLastKnownAccountsFromForeground();
   }
 
@@ -135,14 +139,13 @@ class AuthenticationServiceTest : public PlatformTest {
   }
 
   void SetCachedMDMInfo(ChromeIdentity* identity, NSDictionary* user_info) {
-    authentication_service()
-        ->cached_mdm_infos_[base::SysNSStringToUTF8([identity gaiaID])] =
+    authentication_service()->cached_mdm_infos_[GetAccountId(identity)] =
         user_info;
   }
 
   bool HasCachedMDMInfo(ChromeIdentity* identity) {
     return authentication_service()->cached_mdm_infos_.count(
-               base::SysNSStringToUTF8([identity gaiaID])) > 0;
+               GetAccountId(identity)) > 0;
   }
 
   AuthenticationService* authentication_service() {
@@ -250,7 +253,7 @@ TEST_F(AuthenticationServiceTest, TestHandleForgottenIdentityPromptSignIn) {
 
 TEST_F(AuthenticationServiceTest, StoreAndGetAccountsInPrefs) {
   // Profile starts empty.
-  std::vector<std::string> accounts = GetLastKnownAccountsFromForeground();
+  std::vector<CoreAccountId> accounts = GetLastKnownAccountsFromForeground();
   EXPECT_TRUE(accounts.empty());
 
   // Sign in.
@@ -262,8 +265,8 @@ TEST_F(AuthenticationServiceTest, StoreAndGetAccountsInPrefs) {
   StoreKnownAccountsWhileInForeground();
   accounts = GetLastKnownAccountsFromForeground();
   ASSERT_EQ(2u, accounts.size());
-  EXPECT_EQ("foo2ID", accounts[0]);
-  EXPECT_EQ("fooID", accounts[1]);
+  EXPECT_EQ(CoreAccountId("foo2ID"), accounts[0]);
+  EXPECT_EQ(CoreAccountId("fooID"), accounts[1]);
 }
 
 TEST_F(AuthenticationServiceTest,
@@ -282,8 +285,8 @@ TEST_F(AuthenticationServiceTest,
       identity_manager()->GetAccountsWithRefreshTokens();
   std::sort(accounts.begin(), accounts.end(), account_compare_func);
   ASSERT_EQ(2u, accounts.size());
-  EXPECT_EQ("foo2ID", accounts[0].account_id);
-  EXPECT_EQ("fooID", accounts[1].account_id);
+  EXPECT_EQ(CoreAccountId("foo2ID"), accounts[0].account_id);
+  EXPECT_EQ(CoreAccountId("fooID"), accounts[1].account_id);
 
   // Simulate a switching to background and back to foreground, triggering a
   // credentials reload.
@@ -295,9 +298,9 @@ TEST_F(AuthenticationServiceTest,
   accounts = identity_manager()->GetAccountsWithRefreshTokens();
   std::sort(accounts.begin(), accounts.end(), account_compare_func);
   ASSERT_EQ(3u, accounts.size());
-  EXPECT_EQ("foo2ID", accounts[0].account_id);
-  EXPECT_EQ("foo3ID", accounts[1].account_id);
-  EXPECT_EQ("fooID", accounts[2].account_id);
+  EXPECT_EQ(CoreAccountId("foo2ID"), accounts[0].account_id);
+  EXPECT_EQ(CoreAccountId("foo3ID"), accounts[1].account_id);
+  EXPECT_EQ(CoreAccountId("fooID"), accounts[2].account_id);
 }
 
 TEST_F(AuthenticationServiceTest, HaveAccountsChanged_Default) {
@@ -430,7 +433,7 @@ TEST_F(AuthenticationServiceTest, MDMErrorsClearedOnForeground) {
   GoogleServiceAuthError error(
       GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS);
   signin::UpdatePersistentErrorOfRefreshTokenForAccount(
-      identity_manager(), base::SysNSStringToUTF8([identity(0) gaiaID]), error);
+      identity_manager(), GetAccountId(identity(0)), error);
 
   // MDM error for |identity_| is being cleared and the error state of refresh
   // token will be updated.
@@ -483,7 +486,7 @@ TEST_F(AuthenticationServiceTest, HandleMDMNotification) {
   GoogleServiceAuthError error(
       GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS);
   signin::UpdatePersistentErrorOfRefreshTokenForAccount(
-      identity_manager(), base::SysNSStringToUTF8([identity(0) gaiaID]), error);
+      identity_manager(), GetAccountId(identity(0)), error);
 
   NSDictionary* user_info1 = @{ @"foo" : @1 };
   ON_CALL(*identity_service(), GetMDMDeviceStatus(user_info1))
@@ -519,7 +522,7 @@ TEST_F(AuthenticationServiceTest, HandleMDMBlockedNotification) {
   GoogleServiceAuthError error(
       GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS);
   signin::UpdatePersistentErrorOfRefreshTokenForAccount(
-      identity_manager(), base::SysNSStringToUTF8([identity(0) gaiaID]), error);
+      identity_manager(), GetAccountId(identity(0)), error);
 
   NSDictionary* user_info1 = @{ @"foo" : @1 };
   ON_CALL(*identity_service(), GetMDMDeviceStatus(user_info1))
@@ -577,7 +580,7 @@ TEST_F(AuthenticationServiceTest, ShowMDMErrorDialog) {
   GoogleServiceAuthError error(
       GoogleServiceAuthError::INVALID_GAIA_CREDENTIALS);
   signin::UpdatePersistentErrorOfRefreshTokenForAccount(
-      identity_manager(), base::SysNSStringToUTF8([identity(0) gaiaID]), error);
+      identity_manager(), GetAccountId(identity(0)), error);
 
   NSDictionary* user_info = [NSDictionary dictionary];
   SetCachedMDMInfo(identity(0), user_info);
