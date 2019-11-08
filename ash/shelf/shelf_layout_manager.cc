20eb70dfa751b3e444e45d9dad839428a4216046
Xiaoqian Dai <xdai@chromium.org>
2019-11-08 00:22:23 +0000

    overview gesture: adjust the fling event.
    For a fling event, if the hotseat was hidden when the drag started,
    only if the fling event position exceeds the restore-to-maximized
    threshold, the window will be taken back to home screen.
    
    Also, remove the logic to decide if drag can be allowed from
    DragWindowFromShelfController as ShelfLayoutManager already handled it.
    
    Bug: 997885
    Change-Id: I35b9c35df1477b92b5c01db49c9302b6f3bace60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902176
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713677}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -2457,8 +2457,8 @@ bool ShelfLayoutManager::MaybeStartDragWindowFromShelf(
   if (!window)
     return false;
 
-  window_drag_controller_ =
-      std::make_unique<DragWindowFromShelfController>(window);
+  window_drag_controller_ = std::make_unique<DragWindowFromShelfController>(
+      window, event_in_screen.location(), hotseat_state());
   return true;
 }
 
