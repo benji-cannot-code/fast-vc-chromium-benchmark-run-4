b9407ef157c116711f9e295c3fa3c39365a6eb92
Marc Treib <treib@chromium.org>
2019-11-08 09:25:20 +0000

    Bring back required parts of ChromeBrowserSyncAdapter + Service
    ChromeBrowserSyncAdapter was an old implementation of Sync
    invalidations which is not needed anymore, and hence was removed.
    However, it *also* caused Chrome to show up as a "syncable" app in
    Android settings.
    
    This CL brings back the required parts of ChromeBrowserSyncAdapter (and
    the corresponding ..Service and manifest entry) so that Chrome shows up
    in (and honors) Android's sync settings again.
    
    This is a partial revert of crrev.com/c/1821784 and crrev.com/c/1829201.
    
    Bug: 1021873
    Change-Id: I13cb78fa2391f289763629620c69cf3a6c19e494
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903198
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713769}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/invalidation/ChromeBrowserSyncAdapterService.java
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.invalidation;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+import org.chromium.base.ContextUtils;
+import org.chromium.chrome.browser.init.ProcessInitializationHandler;
+
+/**
+ * A Service that provides access to {@link ChromeBrowserSyncAdapter}.
+ */
+public class ChromeBrowserSyncAdapterService extends Service {
+    @SuppressLint("StaticFieldLeak")
+    private static ChromeBrowserSyncAdapter sSyncAdapter;
+    private static final Object LOCK = new Object();
+
+    /**
+     * Get the sync adapter reference, creating an instance if necessary.
+     */
+    private ChromeBrowserSyncAdapter getOrCreateSyncAdapter(Context applicationContext) {
+        synchronized (LOCK) {
+            if (sSyncAdapter == null) {
+                ProcessInitializationHandler.getInstance().initializePreNative();
+                sSyncAdapter = new ChromeBrowserSyncAdapter(applicationContext);
+            }
+        }
+        return sSyncAdapter;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return getOrCreateSyncAdapter(ContextUtils.getApplicationContext()).getSyncAdapterBinder();
+    }
+}
