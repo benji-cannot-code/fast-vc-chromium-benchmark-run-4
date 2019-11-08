62886895c58fa9eebd86a576d20981857a224eb5
Antonio Gomes <tonikitoo@igalia.com>
2019-11-08 16:03:29 +0000

    Replace uses of WebRTCStats by RTCStats
    The replacement is now possible that all users are in Blink.
    Essentially, the former is a pure interface for the later.
    
    As a follow up step the use of WebString will be replace by
    WTF::String.
    
    BUG=787254, 919392
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I921d2351533f54f00c3dd1b19d20a1b6f7a2a3f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902445
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713841}

--- third_party/blink/renderer/platform/exported/web_rtc_stats.cc
+++ third_party/blink/renderer/platform/exported/web_rtc_stats.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-WebRTCStats::~WebRTCStats() = default;
-
 WebRTCStatsMember::~WebRTCStatsMember() = default;
 
 }  // namespace blink
