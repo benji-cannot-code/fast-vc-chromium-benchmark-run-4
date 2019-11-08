fa243c6b23ede8a20d8791e5271c16471da014dc
Robert Liao <robliao@chromium.org>
2019-11-08 00:29:27 +0000

    Convert Content CComPtr Uses to ComPtr
    BUG=5027
    
    Change-Id: If972ad1381437b116f3279a3a51480dd2098d564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1904495
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713679}

--- content/public/test/browser_test_utils.cc
+++ content/public/test/browser_test_utils.cc
@@ -136,7 +136,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include <uiautomation.h>
 #include <wrl/client.h>
-#include "base/win/atl.h"
 #include "base/win/scoped_safearray.h"
 #include "base/win/scoped_variant.h"
 #endif
@@ -2107,14 +2106,15 @@ void UiaGetPropertyValueVtArrayVtUnknownValidate(
 
   std::vector<std::string> names;
   for (LONG i = 0; i < size; ++i) {
-    CComPtr<IUnknown> unknown_element = nullptr;
-    ASSERT_HRESULT_SUCCEEDED(SafeArrayGetElement(V_ARRAY(result_variant.ptr()),
-                                                 &i, &unknown_element));
+    Microsoft::WRL::ComPtr<IUnknown> unknown_element;
+    ASSERT_HRESULT_SUCCEEDED(
+        SafeArrayGetElement(V_ARRAY(result_variant.ptr()), &i,
+                            static_cast<void**>(&unknown_element)));
     ASSERT_NE(nullptr, unknown_element);
 
-    CComPtr<IRawElementProviderSimple> raw_element_provider_simple = nullptr;
-    ASSERT_HRESULT_SUCCEEDED(
-        unknown_element->QueryInterface(&raw_element_provider_simple));
+    Microsoft::WRL::ComPtr<IRawElementProviderSimple>
+        raw_element_provider_simple;
+    ASSERT_HRESULT_SUCCEEDED(unknown_element.As(&raw_element_provider_simple));
     ASSERT_NE(nullptr, raw_element_provider_simple);
 
     base::win::ScopedVariant name;
