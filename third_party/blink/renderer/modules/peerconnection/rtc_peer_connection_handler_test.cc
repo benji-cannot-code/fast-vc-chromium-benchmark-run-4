0a57988fe910121a23999d7ac9435ee844fd4696
Antonio Gomes <tonikitoo@igalia.com>
2019-11-08 20:55:11 +0000

    Switch renderer/platform/peerconnection/rtc_stats* away from WebString
    Use WTF::String instead, now that the classes are solely used
    within Blink.
    
    BUG=787254
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I0b43eefea87bf48367903a935ee06bb22f245113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906507
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713944}

--- third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler_test.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler_test.cc
@@ -972,7 +972,7 @@ TEST_F(RTCPeerConnectionHandlerTest, GetRTCStats) {
             EXPECT_EQ(member->ValueDouble(), 42.0);
             break;
           case webrtc::RTCStatsMemberInterface::kString:
-            EXPECT_EQ(member->ValueString(), blink::WebString::FromUTF8("42"));
+            EXPECT_EQ(member->ValueString(), "42");
             break;
           case webrtc::RTCStatsMemberInterface::kSequenceBool:
             ExpectSequenceEquals(member->ValueSequenceBool(), 1);
@@ -998,7 +998,7 @@ TEST_F(RTCPeerConnectionHandlerTest, GetRTCStats) {
             break;
           case webrtc::RTCStatsMemberInterface::kSequenceString:
             ExpectSequenceEquals(member->ValueSequenceString(),
-                                 blink::WebString::FromUTF8("42"));
+                                 String::FromUTF8("42"));
             break;
           default:
             NOTREACHED();
