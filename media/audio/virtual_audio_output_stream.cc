504810be67dde34904c5543a1f352718a821112e
Raymond Toy <rtoy@chromium.org>
2019-11-08 17:28:18 +0000

    [Code Health] Convert AfterCloseCallback to base::OnceCallback
    This also allows us to replace base::Bind with base::BindOnce.
    
    Bug: 1007798
    Change-Id: Iab43f746e3c4507d9b9b7b82e6f58cedcd08faa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1904847
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713863}

--- media/audio/virtual_audio_output_stream.cc
+++ media/audio/virtual_audio_output_stream.cc
@@ -15,10 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 VirtualAudioOutputStream::VirtualAudioOutputStream(
-    const AudioParameters& params, VirtualAudioInputStream* target,
-    const AfterCloseCallback& after_close_cb)
-    : params_(params), target_input_stream_(target),
-      after_close_cb_(after_close_cb), callback_(NULL), volume_(1.0f) {
+    const AudioParameters& params,
+    VirtualAudioInputStream* target,
+    AfterCloseCallback after_close_cb)
+    : params_(params),
+      target_input_stream_(target),
+      after_close_cb_(std::move(after_close_cb)),
+      callback_(nullptr),
+      volume_(1.0f) {
   DCHECK(params_.IsValid());
   DCHECK(target);
 
@@ -60,9 +64,7 @@ void VirtualAudioOutputStream::Close() {
   // here.  The callback is moved to a stack-local first since |this| could be
   // destroyed during Run().
   if (after_close_cb_) {
-    const AfterCloseCallback cb = after_close_cb_;
-    after_close_cb_.Reset();
-    cb.Run(this);
+    std::move(after_close_cb_).Run(this);
   }
 }
 
