f10df74b6dfb941202bb6ac94593d66ca686b99e
Olivier Robin <olivierrobin@chromium.org>
2019-11-08 15:22:35 +0000

    Add not restored tabs to recently closed.
    With slim navigation, the tab cannot be restored anymore.
    Directly add the tab to recently closed without creating a WebState (as
    the WebState is not used anyway).
    
    Bug: 1022298
    Change-Id: I4979564e938fd49fb8b622c38561675ccf27a8c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903354
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713829}

--- components/sessions/ios/ios_serialized_navigation_builder.h
+++ components/sessions/ios/ios_serialized_navigation_builder.h
@@ -13,6 +13,8 @@ namespace web {
 class NavigationItem;
 }
 
+@class CRWNavigationItemStorage;
+
 namespace sessions {
 class SerializedNavigationEntry;
 
@@ -24,6 +26,12 @@ class IOSSerializedNavigationBuilder {
   // NavigationItem.
   static SerializedNavigationEntry FromNavigationItem(
       int index, const web::NavigationItem& item);
+
+  // Construct a SerializedNavigationEntry for a particular index from the given
+  // CRWNavigationItemStorage.
+  static SerializedNavigationEntry FromNavigationStorageItem(
+      int index,
+      CRWNavigationItemStorage* item);
 
   // Convert the given SerializedNavigationEntry into a NavigationItem. The
   // NavigationItem will have a transition type of
