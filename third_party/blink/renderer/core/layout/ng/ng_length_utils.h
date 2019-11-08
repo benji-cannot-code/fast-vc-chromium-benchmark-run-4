6c3a23532296599e4d57722c085e1c9f26703f0e
Morten Stenshorne <mstensho@chromium.org>
2019-11-08 11:41:20 +0000

    [LayoutNG] Consumed border+padding shouldn't affect column length.
    Previously consumed block-size stored in a break token is with respect
    to the border box of the node, so this will naturally also include
    block-start border and padding.
    
    When this happens to a multicol container with non-auto block-size in a
    nested fragmentation context, we need to make sure that border and
    padding in a previous fragment doesn't affect the block-size of the
    columns.
    
    Added a unit test, and fixed another one, which turned out to have
    incorrect expectations.
    
    Bug: 829028
    Change-Id: Ifad2993984f82ac2fe1b9f73e4a8599b72f327ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903193
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713790}

--- third_party/blink/renderer/core/layout/ng/ng_length_utils.h
+++ third_party/blink/renderer/core/layout/ng/ng_length_utils.h
@@ -25,6 +25,7 @@ class ComputedStyle;
 class Length;
 struct MinMaxSizeInput;
 class NGConstraintSpace;
+class NGBlockBreakToken;
 class NGBlockNode;
 class NGLayoutInputNode;
 
@@ -482,6 +483,12 @@ base::Optional<MinMaxSize> CalculateMinMaxSizesIgnoringChildren(
     const NGBlockNode&,
     const NGBoxStrut& border_scrollbar_padding,
     NGMinMaxSizeType);
+
+// Return the sum of the block-size of the content-boxes in all preceding
+// fragments.
+LayoutUnit ConsumedBlockSizeInContentBox(
+    LayoutUnit border_scrollbar_padding_block_start,
+    const NGBlockBreakToken*);
 
 }  // namespace blink
 
