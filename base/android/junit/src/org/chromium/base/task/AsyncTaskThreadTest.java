523bac7cb2a389da1ef8a9fd4a559ba0c664eed8
Shimi Zhang <ctzsm@chromium.org>
2019-11-08 20:58:20 +0000

    [Android] Disable a flaky test testCancel_MayInterrupt_TaskIsInterrupted
    TBR: changwan@chromium.org
    Bug: 1022954
    Change-Id: I3d5d259e3b045181f3c57ff70dcd3fcb4b1a279a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1907191
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713946}

--- base/android/junit/src/org/chromium/base/task/AsyncTaskThreadTest.java
+++ base/android/junit/src/org/chromium/base/task/AsyncTaskThreadTest.java
@@ -15,6 +15,7 @@ import android.support.test.filters.SmallTest;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -172,6 +173,7 @@ public class AsyncTaskThreadTest {
 
     @Test
     @SmallTest
+    @Ignore("crbug.com/1022954")
     public void testCancel_MayInterrupt_TaskIsInterrupted() throws Exception {
         mTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
