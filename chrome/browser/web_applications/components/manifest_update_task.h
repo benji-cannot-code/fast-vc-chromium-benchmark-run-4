ab2b4a56cd6ed25d52b1838e083b7fca6efcbd3c
Alan Cutter <alancutter@chromium.org>
2019-11-08 06:44:53 +0000

    WebApps: Check manifest updates on WebApplicationInfo instead of blink::Manifest
    This CL updates the PWA manifest update check to compare with a
    WebApplicationInfo generated from the page's blink::Manifest instead of the
    manifest directly.
    PWA installation uses the blink::Manifest -> WebApplicationInfo conversion
    so we must also use the same when comparing for differences.
    
    At the moment we only check theme_color which doesn't undergo any transformations
    at install time so it's fine. This is in preparation for comparing on other
    fields e.g. icon URLs which may go through sanitisation.
    
    This CL is a refactor with no changes in behaviour.
    
    Bug: 926083
    Change-Id: I477f5e4ff936f4d4fee078be0e830b39730bf06a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1905041
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#713753}

--- chrome/browser/web_applications/components/manifest_update_task.h
+++ chrome/browser/web_applications/components/manifest_update_task.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/manifest/manifest.h"
 
 struct InstallableData;
+struct WebApplicationInfo;
 
 namespace web_app {
 
@@ -72,11 +73,13 @@ class ManifestUpdateTask final
   };
 
   void OnDidGetInstallableData(const InstallableData& data);
-  bool IsUpdateNeededForManifest(const blink::Manifest& manifest) const;
-  void OnAllAppWindowsClosed(blink::Manifest manifest);
-  void OnInstallationComplete(blink::Manifest manifest,
-                              const AppId& app_id,
-                              InstallResultCode code);
+  bool IsUpdateNeeded(const WebApplicationInfo& web_application_info) const;
+  void OnAllAppWindowsClosed(
+      std::unique_ptr<WebApplicationInfo> web_application_info);
+  void OnInstallationComplete(
+      std::unique_ptr<WebApplicationInfo> opt_web_application_info,
+      const AppId& app_id,
+      InstallResultCode code);
   void DestroySelf(ManifestUpdateResult result);
 
   const AppRegistrar& registrar_;
