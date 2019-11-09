0c58ad9006e46d7005607e4091386cb6fcac8d0a
John Lee <johntlee@chromium.org>
2019-11-09 02:30:46 +0000

    WebUI Tab Strip: Add event for when a tab is replaced
    Bug: 989131
    Change-Id: I08b4da7bf37dcd0353f3553844ef7c9e8bf1cf14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900659
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714072}

--- chrome/browser/resources/tab_strip/tab_list.js
+++ chrome/browser/resources/tab_strip/tab_list.js
@@ -171,6 +171,8 @@ class TabListElement extends CustomElement {
       addWebUIListener(
           'tab-moved', (tabId, newIndex) => this.onTabMoved_(tabId, newIndex));
       addWebUIListener('tab-removed', tabId => this.onTabRemoved_(tabId));
+      addWebUIListener(
+          'tab-replaced', (oldId, newId) => this.onTabReplaced_(oldId, newId));
       addWebUIListener('tab-updated', tab => this.onTabUpdated_(tab));
       addWebUIListener(
           'tab-active-changed', tabId => this.onTabActivated_(tabId));
@@ -412,6 +414,21 @@ class TabListElement extends CustomElement {
     if (tabElement) {
       this.addAnimationPromise_(tabElement.slideOut());
     }
+  }
+
+  /**
+   * @param {number} oldId
+   * @param {number} newId
+   * @private
+   */
+  onTabReplaced_(oldId, newId) {
+    const tabElement = this.findTabElement_(oldId);
+    if (!tabElement) {
+      return;
+    }
+
+    tabElement.tab = /** @type {!TabData} */ (
+        Object.assign({}, tabElement.tab, {id: newId}));
   }
 
   /**
