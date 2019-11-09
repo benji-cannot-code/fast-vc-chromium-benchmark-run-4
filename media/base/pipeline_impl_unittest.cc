311484c86790cd70573dc47c48db56d5438b26fd
Chris Mumford <cmumford@chromium.org>
2019-11-09 00:20:58 +0000

    Switch pipeline/launcher tests to use C++ lambda.
    This change switches from using testing::CreateFunctor
    when calling testing::Invoke() to a C++ lambda. This
    improves test readibility and allows for gmock_mutant.h
    to be removed in an upcoming CL.
    
    TBR=pinkerton@chromium.org
    
    Bug: 1007833, 806952
    Change-Id: Id60006d1fb841ca89f47c20170ee2df4c16e8c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1887842
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/master@{#714030}

--- media/base/pipeline_impl_unittest.cc
+++ media/base/pipeline_impl_unittest.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/text_renderer.h"
 #include "media/base/text_track_config.h"
 #include "media/base/time_delta_interpolator.h"
-#include "testing/gmock_mutant.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/geometry/size.h"
 
@@ -38,7 +37,6 @@ using ::base::test::RunOnceCallback;
 using ::base::test::RunOnceClosure;
 using ::testing::_;
 using ::testing::AnyNumber;
-using ::testing::CreateFunctor;
 using ::testing::DeleteArg;
 using ::testing::DoAll;
 using ::testing::Invoke;
@@ -730,9 +728,10 @@ TEST_F(PipelineImplTest, NoMessageDuringTearDownFromError) {
   StartPipelineAndExpect(PIPELINE_OK);
 
   // Trigger additional requests on the pipeline during tear down from error.
-  base::Callback<void(PipelineStatus)> cb =
-      base::Bind(&TestNoCallsAfterError, pipeline_.get(), &task_environment_);
-  ON_CALL(callbacks_, OnError(_)).WillByDefault(Invoke(CreateFunctor(cb)));
+  ON_CALL(callbacks_, OnError(_))
+      .WillByDefault(Invoke([=](PipelineStatus status) {
+        TestNoCallsAfterError(pipeline_.get(), &task_environment_, status);
+      }));
 
   base::TimeDelta seek_time = base::TimeDelta::FromSeconds(5);
 
--- remoting/host/win/worker_process_launcher_unittest.cc
+++ remoting/host/win/worker_process_launcher_unittest.cc
@@ -28,18 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/win/launch_process_with_token.h"
 #include "remoting/host/worker_process_ipc_delegate.h"
 #include "testing/gmock/include/gmock/gmock.h"
-#include "testing/gmock_mutant.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::win::ScopedHandle;
 using testing::_;
 using testing::AnyNumber;
-using testing::CreateFunctor;
-using testing::DoAll;
 using testing::Expectation;
 using testing::Invoke;
 using testing::InvokeWithoutArgs;
-using testing::Return;
 
 namespace remoting {
 
@@ -435,12 +431,10 @@ TEST_F(WorkerProcessLauncherTest, Restart) {
   Expectation first_connect =
       EXPECT_CALL(server_listener_, OnChannelConnected(_))
           .Times(2)
-          .WillOnce(InvokeWithoutArgs(CreateFunctor(
-              &WorkerProcessLauncherTest::TerminateWorker,
-              base::Unretained(this),
-              CONTROL_C_EXIT)))
-          .WillOnce(InvokeWithoutArgs(this,
-                                      &WorkerProcessLauncherTest::StopWorker));
+          .WillOnce(
+              InvokeWithoutArgs([=]() { TerminateWorker(CONTROL_C_EXIT); }))
+          .WillOnce(
+              InvokeWithoutArgs(this, &WorkerProcessLauncherTest::StopWorker));
 
   EXPECT_CALL(server_listener_, OnPermanentError(_))
       .Times(0);
@@ -486,10 +480,8 @@ TEST_F(WorkerProcessLauncherTest, PermanentError) {
 
   EXPECT_CALL(server_listener_, OnChannelConnected(_))
       .Times(1)
-      .WillOnce(InvokeWithoutArgs(CreateFunctor(
-          &WorkerProcessLauncherTest::TerminateWorker,
-          base::Unretained(this),
-          kMinPermanentErrorExitCode)));
+      .WillOnce(InvokeWithoutArgs(
+          [=] { TerminateWorker(kMinPermanentErrorExitCode); }));
   EXPECT_CALL(server_listener_, OnPermanentError(_))
       .Times(1)
       .WillOnce(InvokeWithoutArgs(this,
@@ -517,10 +509,8 @@ TEST_F(WorkerProcessLauncherTest, Crash) {
 
   EXPECT_CALL(client_listener_, OnCrash(_, _, _))
       .Times(1)
-      .WillOnce(InvokeWithoutArgs(CreateFunctor(
-          &WorkerProcessLauncherTest::TerminateWorker,
-          base::Unretained(this),
-          EXCEPTION_BREAKPOINT)));
+      .WillOnce(
+          InvokeWithoutArgs([=]() { TerminateWorker(EXCEPTION_BREAKPOINT); }));
   EXPECT_CALL(server_listener_, OnWorkerProcessStopped())
       .Times(1);
 
