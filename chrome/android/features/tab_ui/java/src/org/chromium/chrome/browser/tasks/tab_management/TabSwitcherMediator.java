4d5ec4f07de68197c24dd5f3013a9d2d9996ba33
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2019-11-09 00:00:54 +0000

    Fix initial scroll for TabSwitcherOnReturn
    INITIAL_SCROLL_INDEX_OFFSET was not properly set if tab restoration
    finishes after tab switcher is shown.
    
    Bug: 1022289
    Change-Id: I63d18d0fae7b6e6d40865667f7bd0fecdd684f4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903526
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714023}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMediator.java
@@ -235,6 +235,7 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
                 mResetHandler.resetWithTabList(
                         mTabModelSelector.getTabModelFilterProvider().getCurrentTabModelFilter(),
                         false, mShowTabsInMruOrder);
+                setInitialScrollIndexOffset();
             }
         };
 
@@ -421,15 +422,20 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
         mHandler.removeCallbacks(mSoftClearTabListRunnable);
         mHandler.removeCallbacks(mClearTabListRunnable);
         boolean quick = false;
-        if (TabFeatureUtilities.isTabToGtsAnimationEnabled()
-                && mTabModelSelector.getTabModelFilterProvider()
-                           .getCurrentTabModelFilter()
-                           .isTabModelRestored()) {
-            quick = mResetHandler.resetWithTabList(
-                    mTabModelSelector.getTabModelFilterProvider().getCurrentTabModelFilter(), false,
-                    mShowTabsInMruOrder);
+        if (mTabModelSelector.getTabModelFilterProvider()
+                        .getCurrentTabModelFilter()
+                        .isTabModelRestored()) {
+            if (TabFeatureUtilities.isTabToGtsAnimationEnabled()) {
+                quick = mResetHandler.resetWithTabList(
+                        mTabModelSelector.getTabModelFilterProvider().getCurrentTabModelFilter(),
+                        false, mShowTabsInMruOrder);
+            }
+            setInitialScrollIndexOffset();
         }
+        return quick;
+    }
 
+    private void setInitialScrollIndexOffset() {
         int initialPosition = Math.max(
                 mTabModelSelector.getTabModelFilterProvider().getCurrentTabModelFilter().index()
                         - INITIAL_SCROLL_INDEX_OFFSET,
@@ -437,7 +443,6 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
         // In MRU order, selected Tab is always at the first position.
         if (mShowTabsInMruOrder) initialPosition = 0;
         mContainerViewModel.set(INITIAL_SCROLL_INDEX, initialPosition);
-        return quick;
     }
 
     @Override
