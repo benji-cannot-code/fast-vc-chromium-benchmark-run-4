9aeb7a188c7865790dcb8000c434d115e7c0c5b3
Scott Violet <sky@chromium.org>
2019-11-09 00:23:35 +0000

    weblayer: converts NavigationState to proper enum style
    BUG=none
    TEST=none
    
    Change-Id: I0bb59ce7360419c14a67074fb5a0d432aac6634d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906308
    Auto-Submit: Scott Violet <sky@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714032}

---
+++ weblayer/browser/java/org/chromium/weblayer_private/interfaces/NavigationState.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer_private.interfaces;
+
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@IntDef({NavigationState.WAITING_RESPONSE, NavigationState.RECEIVING_BYTES,
+        NavigationState.COMPLETE, NavigationState.FAILED})
+@Retention(RetentionPolicy.SOURCE)
+public @interface NavigationState {
+    int WAITING_RESPONSE = 0;
+    int RECEIVING_BYTES = 1;
+    int COMPLETE = 2;
+    int FAILED = 3;
+}
