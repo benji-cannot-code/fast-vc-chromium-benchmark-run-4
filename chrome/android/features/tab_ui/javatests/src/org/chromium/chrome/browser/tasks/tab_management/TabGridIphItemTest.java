53d719b33323317489edf6999416a55ece67e8eb
Theresa Wellington <twellington@chromium.org>
2019-11-11 21:26:41 +0000

    Disable TabGridIphItemTest#testIphItemScreenRotation on O+
    BUG=1023429
    TBR=yuezhanggg@chromium.org
    
    Change-Id: I3b7834debf66efd20034ae53699e26e34509d97f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910419
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714289}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphItemTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphItemTest.java
@@ -23,6 +23,7 @@ import static org.chromium.chrome.browser.tasks.tab_management.TabUiTestHelper.e
 import static org.chromium.chrome.browser.tasks.tab_management.TabUiTestHelper.rotateDeviceToOrientation;
 
 import android.content.res.Configuration;
+import android.os.Build;
 import android.support.test.espresso.NoMatchingRootException;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
@@ -41,6 +42,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -115,6 +117,7 @@ public class TabGridIphItemTest {
 
     @Test
     @MediumTest
+    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.N_MR1, message = "https://crbug.com/1023430")
     public void testIphItemScreenRotation() throws InterruptedException {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
