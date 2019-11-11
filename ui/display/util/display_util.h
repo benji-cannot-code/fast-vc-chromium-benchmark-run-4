d8b8b1c114e4ba1d4e1a54569c3bfb578d2b9015
Tom Anderson <thomasanderson@chromium.org>
2019-11-11 23:35:54 +0000

    [X11] Obtain colorspace info from EDID when no ICC profile is set up
    This fixes an issue where display::Display::color_space()::IsValid()
    would always return false when no ICC profile was set up.  This was
    causing HDR video playback to fallback to SDR even on HDR/WCG
    monitors.  The solution is to obtain the colorspace information from
    the monitor's EDID.
    
    BUG=1021659
    R=sky
    
    Change-Id: Iea05bc42f05112d6809887abf2b725807632e820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1907427
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714306}

--- ui/display/util/display_util.h
+++ ui/display/util/display_util.h
@@ -9,12 +9,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "ui/display/util/display_util_export.h"
+#include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace display {
 
+class EdidParser;
+
 // 1 inch in mm.
 constexpr float kInchInMm = 25.4f;
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class EdidColorSpaceChecksOutcome {
+  kSuccess = 0,
+  kErrorBadCoordinates = 1,
+  kErrorPrimariesAreaTooSmall = 2,
+  kErrorBluePrimaryIsBroken = 3,
+  kErrorCannotExtractToXYZD50 = 4,
+  kErrorBadGamma = 5,
+  kMaxValue = kErrorBadGamma
+};
 
 // Returns true if a given size is in the list of bogus sizes in mm that should
 // be ignored.
@@ -31,6 +46,11 @@ DISPLAY_UTIL_EXPORT bool IsDisplaySizeBlackListed(
 DISPLAY_UTIL_EXPORT int64_t GenerateDisplayID(uint16_t manufacturer_id,
                                               uint32_t product_code_hash,
                                               uint8_t output_index);
+
+// Uses |edid_parser| to extract a gfx::ColorSpace which will be IsValid() if
+// both gamma and the color primaries were correctly found.
+DISPLAY_UTIL_EXPORT gfx::ColorSpace GetColorSpaceFromEdid(
+    const display::EdidParser& edid_parser);
 
 }  // namespace display
 
--- ui/ozone/platform/drm/common/drm_util.h
+++ ui/ozone/platform/drm/common/drm_util.h
@@ -23,7 +23,6 @@ typedef struct _drmModeModeInfo drmModeModeInfo;
 
 namespace display {
 class DisplayMode;
-class EdidParser;
 }  // namespace display
 
 namespace gfx {
@@ -31,18 +30,6 @@ class Point;
 }
 
 namespace ui {
-
-// These values are persisted to logs. Entries should not be renumbered and
-// numeric values should never be reused.
-enum class EdidColorSpaceChecksOutcome {
-  kSuccess = 0,
-  kErrorBadCoordinates = 1,
-  kErrorPrimariesAreaTooSmall = 2,
-  kErrorBluePrimaryIsBroken = 3,
-  kErrorCannotExtractToXYZD50 = 4,
-  kErrorBadGamma = 5,
-  kMaxValue = kErrorBadGamma
-};
 
 // Representation of the information required to initialize and configure a
 // native display. |index| is the position of the connection and will be
@@ -136,10 +123,6 @@ OverlayStatusList CreateOverlayStatusListFrom(
 
 std::vector<OverlayCheckReturn_Params> CreateParamsFromOverlayStatusList(
     const OverlayStatusList& returns);
-
-// Uses |edid_parser| to extract a gfx::ColorSpace which will be IsValid() if
-// both gamma and the color primaries were correctly found.
-gfx::ColorSpace GetColorSpaceFromEdid(const display::EdidParser& edid_parser);
 
 }  // namespace ui
 
