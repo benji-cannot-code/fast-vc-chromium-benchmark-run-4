986a42768f3fa5a40a2b53e361b9f5a6da9326ff
Keishi Hattori <keishi@chromium.org>
2019-11-11 04:42:28 +0000

    Revert "Measure FCP of third-party iframes"
    This reverts commit 74a8f73ba4831303d6dfe7f1badb7698b4e8e5a6.
    
    Reason for revert: PageLoadMetricsBrowserTest.PaintInMultipleChildFrames 
     failing on many bots crbug.com/1023235
    
    Original change's description:
    > Measure FCP of third-party iframes
    > 
    > What: Measures FCP of third-party iframes on pages. Note that this
    > will be a bit wonky at times, as lazy-loaded frames will have a very
    > long FCP.
    > 
    > Why: So that we can measure the impact of triple-keying the cache on
    > loading subframes.
    > 
    > How: Added a third_party_metrics_observer metric. The class records
    > third-party origin iframes FCP and remembers that the RenderFrameHost
    > has been recorded so that it's not recorded again.
    > 
    > Change-Id: Ib4b053eee81210a5ea0a2fec167d4f5995e29967
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1890996
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: John Delaney <johnidel@chromium.org>
    > Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#714024}
    
    TBR=jkarlin@chromium.org,csharrison@chromium.org,johnidel@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: Iad78d3fd005a14486a9e98bfff8935cf599bf0d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906860
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714145}

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -480,8 +480,6 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest, PaintInChildFrame) {
   auto waiter = CreatePageLoadMetricsTestWaiter();
   waiter->AddPageExpectation(TimingField::kFirstLayout);
   waiter->AddPageExpectation(TimingField::kLoadEvent);
-  waiter->AddPageExpectation(TimingField::kFirstPaint);
-  waiter->AddPageExpectation(TimingField::kFirstContentfulPaint);
   waiter->AddSubFrameExpectation(TimingField::kFirstPaint);
   waiter->AddSubFrameExpectation(TimingField::kFirstContentfulPaint);
   ui_test_utils::NavigateToURL(browser(), a_url);
@@ -498,8 +496,6 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest, PaintInDynamicChildFrame) {
   auto waiter = CreatePageLoadMetricsTestWaiter();
   waiter->AddPageExpectation(TimingField::kFirstLayout);
   waiter->AddPageExpectation(TimingField::kLoadEvent);
-  waiter->AddPageExpectation(TimingField::kFirstPaint);
-  waiter->AddPageExpectation(TimingField::kFirstContentfulPaint);
   waiter->AddSubFrameExpectation(TimingField::kFirstPaint);
   waiter->AddSubFrameExpectation(TimingField::kFirstContentfulPaint);
   ui_test_utils::NavigateToURL(
--- components/page_load_metrics/browser/page_load_metrics_test_waiter.cc
+++ components/page_load_metrics/browser/page_load_metrics_test_waiter.cc
@@ -40,6 +40,10 @@ void PageLoadMetricsTestWaiter::AddSubFrameExpectation(TimingField field) {
   CHECK_NE(field, TimingField::kLoadTimingInfo)
       << "LOAD_TIMING_INFO should only be used as a page-level expectation";
   subframe_expected_fields_.Set(field);
+  // If the given field is also a page-level field, then add a page-level
+  // expectation as well
+  if (IsPageLevelField(field))
+    page_expected_fields_.Set(field);
 }
 
 void PageLoadMetricsTestWaiter::AddWebFeatureExpectation(
@@ -50,8 +54,9 @@ void PageLoadMetricsTestWaiter::AddWebFeatureExpectation(
   }
 }
 
-void PageLoadMetricsTestWaiter::AddSubframeNavigationExpectation() {
-  expected_subframe_navigation_ = true;
+void PageLoadMetricsTestWaiter::AddSubframeNavigationExpectation(
+    size_t expected_subframe_navigations) {
+  expected_subframe_navigations_ = expected_subframe_navigations;
 }
 
 void PageLoadMetricsTestWaiter::AddMinimumCompleteResourcesExpectation(
@@ -180,7 +185,7 @@ void PageLoadMetricsTestWaiter::OnDidFinishSubFrameNavigation(
   if (SubframeNavigationExpectationsSatisfied())
     return;
 
-  expected_subframe_navigation_ = false;
+  ++observed_subframe_navigations_;
 
   if (ExpectationsSatisfied() && run_loop_)
     run_loop_->Quit();
@@ -193,6 +198,17 @@ void PageLoadMetricsTestWaiter::FrameSizeChanged(
   expected_frame_sizes_.erase(frame_size);
   if (ExpectationsSatisfied() && run_loop_)
     run_loop_->Quit();
+}
+
+bool PageLoadMetricsTestWaiter::IsPageLevelField(TimingField field) {
+  switch (field) {
+    case TimingField::kFirstPaint:
+    case TimingField::kFirstContentfulPaint:
+    case TimingField::kFirstMeaningfulPaint:
+      return true;
+    default:
+      return false;
+  }
 }
 
 PageLoadMetricsTestWaiter::TimingFieldBitSet
@@ -264,7 +280,7 @@ bool PageLoadMetricsTestWaiter::WebFeaturesExpectationsSatisfied() const {
 
 bool PageLoadMetricsTestWaiter::SubframeNavigationExpectationsSatisfied()
     const {
-  return !expected_subframe_navigation_;
+  return observed_subframe_navigations_ >= expected_subframe_navigations_;
 }
 
 bool PageLoadMetricsTestWaiter::ExpectationsSatisfied() const {
