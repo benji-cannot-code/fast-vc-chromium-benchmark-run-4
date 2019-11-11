e0f639cfbc14af73dec8f87433956f8962657c5a
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2019-11-11 22:51:52 +0000

    bluetooth: Include <vector> for std::vector
    IWYU. This fixes the libstdc++ build after commit df930962a2 ("De-duplicate
    multiple implementations of bluetooth address and UUID parsing"):
    
    ../../device/bluetooth/public/cpp/bluetooth_uuid.h:87:8: error: no template named 'vector' in namespace 'std'
      std::vector<uint8_t> GetBytes() const;
      ~~~~~^
    
    Bug: 957519
    Change-Id: I023a57e87cfac39cac2d1db0079458a562391424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910082
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714297}

--- device/bluetooth/public/cpp/bluetooth_uuid.h
+++ device/bluetooth/public/cpp/bluetooth_uuid.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DEVICE_BLUETOOTH_PUBLIC_CPP_BLUETOOTH_UUID_H_
 
 #include <string>
+#include <vector>
 
 #include "build/build_config.h"
 
