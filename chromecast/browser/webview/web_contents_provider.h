b8d2d6d5c3026b7e04f3046554bcb6e01a616904
Shawn Gallea <sagallea@google.com>
2019-11-11 17:00:32 +0000

    Add PlatformViewsService
    This service allows control of webviews and cast apps via gRPC.
    Webview functionality will be added in a future change.
    
    Merge-With: eureka-internal/333992
    
    Bug: Internal b/143945774
    Bug: Internal b/143307560
    Test: Build cast_shell
    Change-Id: I05d36a2bd8900afeee3a175c97d773428d482311
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1907108
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714229}

---
+++ chromecast/browser/webview/web_contents_provider.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_BROWSER_WEBVIEW_WEB_CONTENTS_PROVIDER_H_
+#define CHROMECAST_BROWSER_WEBVIEW_WEB_CONTENTS_PROVIDER_H_
+
+#include "content/public/browser/web_contents.h"
+
+namespace chromecast {
+
+// Class that provides WebContents when given a window ID.
+class WebContentsProvider {
+ public:
+  virtual content::WebContents* GetWebContents(int window_id) = 0;
+};
+
+}  // namespace chromecast
+
+#endif  // CHROMECAST_BROWSER_WEBVIEW_WEB_CONTENTS_PROVIDER_H_
