38e6f99df9561324897a2f11c826e1561a7293f6
James Cook <jamescook@chromium.org>
2019-11-11 23:38:41 +0000

    Add sync integration tests for enabling Chrome OS sync feature
    Follow-up to code review comments in:
    https://chromium-review.googlesource.com/c/chromium/src/+/1904342
    
    Test that disabling the OS sync feature actually disables the sync
    system and disables the model types.
    
    Introduce shared OsSyncTest class because multiple tests will need
    to enable both the SplitSettingsSync flag and the OS sync feature.
    
    Bug: 1013466
    Test: sync_integration_tests
    Change-Id: I503548e11f6118128178d294b4fb60041670ba7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906871
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714308}

--- chrome/browser/sync/test/integration/single_client_printers_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_printers_sync_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/macros.h"
+#include "chrome/browser/sync/test/integration/os_sync_test.h"
 #include "chrome/browser/sync/test/integration/printers_helper.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
 #include "chrome/browser/sync/test/integration/updated_progress_marker_checker.h"
@@ -20,6 +21,8 @@ using printers_helper::GetPrinterCount;
 using printers_helper::GetPrinterStore;
 using printers_helper::ProfileContainsSamePrintersAsVerifier;
 using printers_helper::RemovePrinter;
+
+namespace {
 
 class SingleClientPrintersSyncTest : public SyncTest {
  public:
@@ -88,3 +91,26 @@ IN_PROC_BROWSER_TEST_F(SingleClientPrintersSyncTest, AddBeforeSetup) {
 
   EXPECT_TRUE(SetupSync()) << "SetupSync() failed.";
 }
+
+// Tests for SplitSettingsSync.
+class SingleClientPrintersOsSyncTest : public OsSyncTest {
+ public:
+  SingleClientPrintersOsSyncTest() : OsSyncTest(SINGLE_CLIENT) {}
+  ~SingleClientPrintersOsSyncTest() override = default;
+};
+
+IN_PROC_BROWSER_TEST_F(SingleClientPrintersOsSyncTest,
+                       DisablingOsSyncFeatureDisablesDataType) {
+  ASSERT_TRUE(SetupSync());
+  syncer::SyncService* service = GetSyncService(0);
+  syncer::SyncUserSettings* settings = service->GetUserSettings();
+
+  EXPECT_TRUE(settings->GetOsSyncFeatureEnabled());
+  EXPECT_TRUE(service->GetActiveDataTypes().Has(syncer::PRINTERS));
+
+  settings->SetOsSyncFeatureEnabled(false);
+  EXPECT_FALSE(settings->GetOsSyncFeatureEnabled());
+  EXPECT_FALSE(service->GetActiveDataTypes().Has(syncer::PRINTERS));
+}
+
+}  // namespace
