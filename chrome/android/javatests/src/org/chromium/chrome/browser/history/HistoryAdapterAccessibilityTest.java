e82742e27c812648819aaca48115ccc11de1ae9d
Theresa Wellington <twellington@chromium.org>
2019-11-11 22:53:45 +0000

    Disable HistoryAdapter*Test on Marshmallow 64-bit testers
    These tests are intermittently timing out on the 64-bit tester, but
    appear to be fine everywhere else.
    
    BUG=1023426
    TBR=wenyufu@google.com
    
    Change-Id: Ibfc1f882d6e124b464e28d0f94b05f0248da6b22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910302
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714299}

--- chrome/android/javatests/src/org/chromium/chrome/browser/history/HistoryAdapterAccessibilityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/history/HistoryAdapterAccessibilityTest.java
@@ -17,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.browser.ui.widget.MoreProgressButton;
 import org.chromium.chrome.browser.ui.widget.MoreProgressButton.State;
 import org.chromium.chrome.browser.widget.selection.SelectionDelegate;
@@ -31,6 +32,7 @@ import java.util.concurrent.TimeUnit;
  * turned on (HistoryManager::isScrollToLoadDisabled() == true).
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
+@DisableIf.Build(supported_abis_includes = "arm64-v8a", message = "crbug.com/1023426")
 public class HistoryAdapterAccessibilityTest {
     public static final int PAGING = 2;
 
