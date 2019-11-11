77026f9a454f67f620464f29aeb3073af53ff4f9
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-11-11 07:22:12 +0000

    Remove DEFINE_TYPE_CASTS from blink::EmptyNodeList
    This CL removes old downcast helpers generated by macro
    DEFINE_TYPE_CASTS.The primary goal of this CL is to use new downcast
    helpers implemented using C++11 type traits.
    
    Bug: 891908
    Change-Id: Ib00118d7419f371124317376076772ef81d41580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906613
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714158}

--- third_party/blink/renderer/core/dom/empty_node_list.h
+++ third_party/blink/renderer/core/dom/empty_node_list.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_EMPTY_NODE_LIST_H_
 
 #include "third_party/blink/renderer/core/dom/node_list.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 
 namespace blink {
 
@@ -56,11 +57,15 @@ class EmptyNodeList final : public NodeList {
   Member<Node> owner_;
 };
 
-DEFINE_TYPE_CASTS(EmptyNodeList,
-                  NodeList,
-                  nodeList,
-                  nodeList->IsEmptyNodeList(),
-                  nodeList.IsEmptyNodeList());
+template <>
+struct DowncastTraits<EmptyNodeList> {
+  static bool AllowFrom(const NodeList& nodeList) {
+    return nodeList.IsEmptyNodeList();
+  }
+  static bool AllowFrom(const NodeList* nodeList) {
+    return nodeList->IsEmptyNodeList();
+  }
+};
 
 }  // namespace blink
 
