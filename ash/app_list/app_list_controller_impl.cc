908c5c22fec830a50fd3814f4be4eb38ff890cb5
Manu Cornet <manucornet@chromium.org>
2019-11-11 21:28:07 +0000

    CrOS Shelf: Relax constraints on what can happen during overview anim
    Separate suspension of shelf visibility updates and work area
    updates (the former is needed for desk transitions, the latter is
    needed for overview transitions).
    
    Bug: 1021691, 1017435, 1019531
    Change-Id: Ie0bb64d931a06b2157a58afc2b1c8604ccf2d71c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1898718
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714290}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -614,12 +614,8 @@ void AppListControllerImpl::OnShellDestroying() {
   Shutdown();
 }
 
-void AppListControllerImpl::OnOverviewModeWillStart() {
+void AppListControllerImpl::OnOverviewModeStarting() {
   if (IsTabletMode()) {
-    scoped_suspend_visibility_update_ =
-        std::make_unique<ShelfLayoutManager::ScopedSuspendVisibilityUpdate>(
-            RootWindowController::ForWindow(presenter_.GetWindow())
-                ->GetShelfLayoutManager());
     const int64_t display_id = last_visible_display_id_;
     OnHomeLauncherTargetPositionChanged(false /* showing */, display_id);
     OnVisibilityWillChange(false /* visible */, display_id);
@@ -632,8 +628,6 @@ void AppListControllerImpl::OnOverviewModeStartingAnimationComplete(
     bool canceled) {
   if (!IsTabletMode())
     return;
-  if (scoped_suspend_visibility_update_)
-    scoped_suspend_visibility_update_.reset();
   OnHomeLauncherAnimationComplete(false /* shown */, last_visible_display_id_);
 }
 
