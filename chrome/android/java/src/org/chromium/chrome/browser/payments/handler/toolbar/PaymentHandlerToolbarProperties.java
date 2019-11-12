37a2abfdcaa69b22e0a78aa400949ed74644abe2
Liquan (Max) Gu <maxlg@chromium.org>
2019-11-12 15:38:36 +0000

    [PaymentHandler] Add toolbar
    This patch adds a toolbar to the Bottom-sheet based Payment handler.
    The features of the toolbar includes:
    * origin: shows the origin of the payment app url (with the scheme)
    * title: shows the payment app title
    * progress bar: shows the load progress of the payment app
    * security icon: show 3 different icons based on the security level.
    
    Because of its resemblance with the ephemeral tab toolbar, we reuse
    the ephemeral tab toolbar's layout but implement it with different
    controlling logic.
    
    Note that the above features support redirect. Whenever the payment
    app page redirect, these features would get updated to match the
    current page.
    
    The progress bar updates the load progress whenever receiving
    web-contents' progress-updating callback. It get hidden once the
    progress reaches 100%; it shows up again once the progress turns below
    100%.
    
    Bug: 999196
    
    Change-Id: Ifaf9e971256ff82888176623b3a4b51749246a73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1901639
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714484}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/handler/toolbar/PaymentHandlerToolbarProperties.java
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.payments.handler.toolbar;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableBooleanPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableFloatPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableIntPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
+
+import java.net.URI;
+
+/** PaymentHandlerToolbar UI properties, which fully describe the state of the UI. */
+/* package */ class PaymentHandlerToolbarProperties {
+    /* package */ static final WritableObjectPropertyKey<URI> ORIGIN =
+            new WritableObjectPropertyKey<>();
+
+    /* package */ static final WritableObjectPropertyKey<String> TITLE =
+            new WritableObjectPropertyKey<>();
+
+    /* package */ static final WritableFloatPropertyKey LOAD_PROGRESS =
+            new WritableFloatPropertyKey();
+
+    /* package */ static final WritableBooleanPropertyKey PROGRESS_VISIBLE =
+            new WritableBooleanPropertyKey();
+
+    /* package */ static final WritableIntPropertyKey SECURITY_ICON = new WritableIntPropertyKey();
+
+    /* package */ static final PropertyKey[] ALL_KEYS =
+            new PropertyKey[] {ORIGIN, TITLE, LOAD_PROGRESS, PROGRESS_VISIBLE, SECURITY_ICON};
+
+    // Prevent instantiation.
+    private PaymentHandlerToolbarProperties() {}
+}
