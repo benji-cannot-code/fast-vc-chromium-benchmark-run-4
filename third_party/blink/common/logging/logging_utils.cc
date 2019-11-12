d9522025326677f034b7a45e9d57e9a0fcb7efee
Evan Stade <estade@chromium.org>
2019-11-12 01:08:07 +0000

    Fix mojom include in logging_utils.cc
    logging_utils.h includes console_message.mojom-forward.h, so the
    cc should include console_message.mojom.h
    
    This prevents errors like https://pastebin.com/raw/JguWQAEZ
    
    Bug: 1001360
    Change-Id: Iee37dc1c8957fdb289c79023c266c4f3f0d9f8fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1909643
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714351}

--- third_party/blink/common/logging/logging_utils.cc
+++ third_party/blink/common/logging/logging_utils.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/logging/logging_utils.h"
 
-#include "third_party/blink/public/mojom/devtools/console_message.mojom-blink.h"
+#include "third_party/blink/public/mojom/devtools/console_message.mojom.h"
 
 namespace blink {
 
