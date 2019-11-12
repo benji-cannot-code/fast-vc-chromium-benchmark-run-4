9da7e7d91643deb2e90297fb4601bade95e6f98f
Kevin Marshall <kmarshall@chromium.org>
2019-11-12 20:39:43 +0000

    [fuchsia] Add support for running in Amber repos in fuchsia-out-dir.
    If an executable is run with "-d" and either the flag --fuchsia-out-dir
    or the GN arg default_fuchsia_build_dir_for_installation are set,
    the runner script will publish to the Fuchsia output directory's
    Amber repository and rely on an existing instance of "fx serve" for
    delivering the package to the target.
    
    Encapsulates Amber repo interactions into a new abstract class, AmberRepo.
    Adds two concrete classes, one for ephemeral repos (used by emulator runs),
    and one for external repos (used for publishing to devices or persistent
    Fuchsia AEMU instances.)
    
    Bug: 1022002
    Change-Id: Ieb330dd8cd92239c1c5341741ef559483af3285b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1907488
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714636}

--- build/fuchsia/deploy_to_amber_repo.py
+++ build/fuchsia/deploy_to_amber_repo.py
@@ -8,11 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Deploys Fuchsia packages to an Amber repository in a Fuchsia
 build output directory."""
 
+import amber_repo
 import argparse
 import os
 import sys
-
-from common import PublishPackage
 
 
 # Populates the GDB-standard symbol directory structure |build_ids_path| with
@@ -55,10 +54,12 @@ def main():
     return 1
 
   fuchsia_out_dir = os.path.expanduser(args.fuchsia_out_dir.pop())
-  tuf_root = os.path.join(fuchsia_out_dir, 'amber-files')
-  print('Installing packages and symbols in Amber repo %s...' % tuf_root)
+  repo = amber_repo.ExternalAmberRepo(
+      os.path.join(fuchsia_out_dir, 'amber-files'))
+  print('Installing packages and symbols in Amber repo %s...' % repo.GetPath())
+
   for package in args.package:
-    PublishPackage(package, tuf_root)
+    repo.PublishPackage(package)
     InstallSymbols(os.path.join(os.path.dirname(package), 'ids.txt'),
                    os.path.join(fuchsia_out_dir, '.build-id'))
 
