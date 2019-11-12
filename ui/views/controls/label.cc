7db7005b0911d3259fb3511fd141fb8db952e64f
Dana Fried <dfried@chromium.org>
2019-11-12 18:10:01 +0000

    Multiline label minimum size should respect min line height.
    Currently, a multiline label will in many cases report a minimum size
    that can be smaller than the appropriate line-height for the text style,
    causing vertical layouts to short the size of the text by a few pixels.
    
    This results in, for example, a visual up-and-down wobble in the
    domain label in a hover card as it animates between tabs, due to the
    changing hover card size. This CL fixes this issue.
    
    Note that except in vertical flex (and possibly box) layouts minimum
    size tends to not be factored in, so this should only affect a handlful
    of situations (and likely produce a better visual result).
    
    Change-Id: Icdc6006b8f51588009b0709773f1854ecf5a6a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910807
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714548}

--- ui/views/controls/label.cc
+++ ui/views/controls/label.cc
@@ -461,7 +461,8 @@ gfx::Size Label::GetMinimumSize() const {
   if (!GetVisible() && collapse_when_hidden_)
     return gfx::Size();
 
-  gfx::Size size(0, font_list().GetHeight());
+  // Always reserve vertical space for at least one line.
+  gfx::Size size(0, std::max(font_list().GetHeight(), GetLineHeight()));
   if (elide_behavior_ == gfx::ELIDE_HEAD ||
       elide_behavior_ == gfx::ELIDE_MIDDLE ||
       elide_behavior_ == gfx::ELIDE_TAIL ||
@@ -480,6 +481,7 @@ gfx::Size Label::GetMinimumSize() const {
       size.SetToMin(GetTextSize());
     }
   }
+
   size.Enlarge(GetInsets().width(), GetInsets().height());
   return size;
 }
