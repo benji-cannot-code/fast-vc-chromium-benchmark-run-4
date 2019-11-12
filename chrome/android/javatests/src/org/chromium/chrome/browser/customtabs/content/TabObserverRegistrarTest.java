3c75650d7258aa38b662df5f96af038c07eb6676
W. James MacLean <wjmaclean@chromium.org>
2019-11-12 23:23:21 +0000

    Disable testObserveActiveTab.
    TBR=pkotwicz@chromium.org
    
    Bug: 1023437
    Change-Id: I79f0974609260af143d1956ab4ecc0ba437807c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913464
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714725}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/content/TabObserverRegistrarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/content/TabObserverRegistrarTest.java
@@ -41,7 +41,6 @@ import java.util.List;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
-@DisabledTest(message = "https://crbug.com/1023437")
 public class TabObserverRegistrarTest {
     private static class LoadUrlTabObserver extends EmptyTabObserver {
         private List<String> mUrlLoadRequests = new ArrayList<>();
@@ -65,6 +64,7 @@ public class TabObserverRegistrarTest {
      * switched.
      */
     @Test
+    @DisabledTest(message = "https://crbug.com/1023437")
     @MediumTest
     public void testObserveActiveTab() throws Throwable {
         EmbeddedTestServer testServer = mCustomTabActivityTestRule.getTestServer();
