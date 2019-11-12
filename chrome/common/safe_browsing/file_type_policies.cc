9f8765f38131c25b192077ccbe1969db96016038
Daniel Rubery <drubery@chromium.org>
2019-11-12 20:14:49 +0000

    Obsolete or extend some SafeBrowsing.FileTypeUpdate metrics.
    This CL extends the following metrics:
      SafeBrowsing.FileTypeUpdate.ResourceBundleResult
      SafeBrowsing.FileTypeUpdate.DynamicUpdateResult
    These have been added to Chirp alerts, since they indicate problems with
    the FileTypePolicies rollout. In order to debug these alerts, the following
    metrics have also been extended:
      SafeBrowsing.FileTypeUpdate.ResourceBundleVersion
      SafeBrowsing.FileTypeUpdate.DynamicUpdateVersion
    
    The following metrics were obsoleted, since they are not being used, and
    are not useful in case of an alert:
      SafeBrowsing.FileTypeUpdate.ResourceBundleTypeCount
      SafeBrowsing.FileTypeUpdate.DynamicUpdateTypeCount
    
    Fixed: 988663
    Change-Id: I0ff456bf2fbbbe018781c39824fa3aa59b711252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895387
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714617}

--- chrome/common/safe_browsing/file_type_policies.cc
+++ chrome/common/safe_browsing/file_type_policies.cc
@@ -65,9 +65,6 @@ void FileTypePolicies::RecordUpdateMetrics(UpdateResult result,
     base::UmaHistogramSparse(
         "SafeBrowsing.FileTypeUpdate." + src_name + "Version",
         config_->version_id());
-    base::UmaHistogramSparse(
-        "SafeBrowsing.FileTypeUpdate." + src_name + "TypeCount",
-        config_->file_types().size());
   }
 }
 
