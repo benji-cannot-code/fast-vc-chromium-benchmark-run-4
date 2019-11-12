7190d01b4dae4789eb4157f6f4806d5dcaeabdf1
Vaclav Brozek <vabr@chromium.org>
2019-11-12 17:20:13 +0000

    Make CSVPassword::Parse differentiate failures
    Parsing a single CSV row inside CSVPassword::Parse can fail for two
    reasons: bad syntax, or incorrect semantics. So far, both cases have
    been reported simply as an error. Soon, the distinction will be needed
    to correctly report from CSVPasswordSequence.
    
    Therefore this CL changes the return type of CSVPassword::Parse from
    Boolean (whether parsing failed) to a new status enum (how it
    failed): OK, Syntax error or Semantic error. This enum describes
    a subset of ParserState::Result, but ParserState::Result was not
    directly used here, because the CSV logic should not know about
    its own client code.
    
    Bug: 934326
    Change-Id: I621e2faa38241541d684357b2f54d9f5fb8ed923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1908528
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714521}

--- components/password_manager/core/browser/import/csv_password_iterator_unittest.cc
+++ components/password_manager/core/browser/import/csv_password_iterator_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "components/password_manager/core/browser/import/csv_password.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace password_manager {
@@ -76,9 +77,10 @@ TEST(CSVPasswordIteratorTest, Success) {
 
   CSVPasswordIterator check = iter;
   for (size_t i = 0; i < base::size(kExpectedPasswords); ++i) {
-    EXPECT_TRUE((check++)->Parse(nullptr)) << "on line " << i;
+    EXPECT_EQ(CSVPassword::Status::kOK, (check++)->Parse(nullptr))
+        << "on line " << i;
   }
-  EXPECT_FALSE(check->Parse(nullptr));
+  EXPECT_NE(CSVPassword::Status::kOK, check->Parse(nullptr));
 
   for (const base::StringPiece& expected_password : kExpectedPasswords) {
     PasswordForm result = (iter++)->ParseValid();
@@ -108,12 +110,13 @@ TEST(CSVPasswordIteratorTest, Failure) {
 
   CSVPasswordIterator check = iter;
   for (size_t i = 0; i + 1 < kLinesInBlob; ++i) {
-    EXPECT_FALSE((check++)->Parse(nullptr)) << "on line " << i;
+    EXPECT_NE(CSVPassword::Status::kOK, (check++)->Parse(nullptr))
+        << "on line " << i;
   }
   // Last line was not a failure.
-  EXPECT_TRUE((check++)->Parse(nullptr));
+  EXPECT_EQ(CSVPassword::Status::kOK, (check++)->Parse(nullptr));
   // After iterating over all lines, there is no more data to parse.
-  EXPECT_FALSE(check->Parse(nullptr));
+  EXPECT_NE(CSVPassword::Status::kOK, check->Parse(nullptr));
 }
 
 }  // namespace password_manager
