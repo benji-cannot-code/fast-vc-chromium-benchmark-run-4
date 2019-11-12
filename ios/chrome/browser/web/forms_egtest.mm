3b7968e9500bfdb71c40d22cbc791bbdecf410ae
Mike Dougherty <michaeldo@chromium.org>
2019-11-12 20:26:53 +0000

    Convert FormsTestCase to EarlGrey 2
    Additionally add a boolean parameter to [ChromeEarlGrey reload] to
    make the wait after the reload optional based on the parameter value.
    
    Bug: 987646
    Change-Id: I2d6aecf5ed34274b256c66a0b9504cd9a2799992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1909710
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714623}

--- ios/chrome/browser/web/forms_egtest.mm
+++ ios/chrome/browser/web/forms_egtest.mm
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <EarlGrey/EarlGrey.h>
 #import <XCTest/XCTest.h>
 
 #include <memory>
@@ -12,23 +11,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/url_formatter/url_formatter.h"
 #import "ios/chrome/browser/ui/popup_menu/popup_menu_constants.h"
-#import "ios/chrome/test/app/chrome_test_util.h"
-#import "ios/chrome/test/app/tab_test_util.h"
+#import "ios/chrome/test/earl_grey/chrome_actions.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/chrome/test/scoped_eg_synchronization_disabler.h"
+#import "ios/testing/earl_grey/earl_grey_test.h"
 #import "ios/testing/earl_grey/matchers.h"
-#import "ios/web/public/test/earl_grey/web_view_actions.h"
-#import "ios/web/public/test/earl_grey/web_view_matchers.h"
 #include "ios/web/public/test/element_selector.h"
 #include "ios/web/public/test/http_server/data_response_provider.h"
 #import "ios/web/public/test/http_server/http_server.h"
 #include "ios/web/public/test/http_server/http_server_util.h"
-#include "ios/web/public/test/url_test_util.h"
-#import "ios/web/public/web_client.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -36,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using chrome_test_util::ButtonWithAccessibilityLabelId;
 using chrome_test_util::OmniboxText;
+using chrome_test_util::TapWebElement;
 using chrome_test_util::WebViewMatcher;
 
 using testing::ElementToDismissAlert;
@@ -219,14 +216,11 @@ id<GREYMatcher> ResendPostButtonMatcher() {
       assertWithMatcher:grey_notNil()];
 
   // WKBasedNavigationManager presents repost confirmation dialog before loading
-  // stops.
-  if ([ChromeEarlGrey isSlimNavigationManagerEnabled]) {
-    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
-  } else {
-    // Legacy navigation manager presents repost confirmation dialog after
-    // loading stops.
-    [ChromeEarlGrey reload];
-  }
+  // stops so do not wait for load to complete because it never will. Legacy
+  // navigation manager presents repost confirmation dialog after loading stops,
+  // so wait for load to finish.
+  BOOL wait = ![ChromeEarlGrey isSlimNavigationManagerEnabled];
+  [ChromeEarlGrey reloadAndWaitForCompletion:wait];
 
   {
     // When slim navigation manager is enabled, synchronization must be disabled
@@ -272,12 +266,12 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   [ChromeEarlGrey loadURL:GetGenericUrl()];
   [ChromeEarlGrey goBack];
 
-  // WKBasedNavigationManager doesn't triggere repost on |goForward| due to
-  // WKWebView's back-forward cache. Force reload to trigger repost. Not using
-  // [ChromeEarlGrey reload] because WKBasedNavigationManager presents repost
-  // confirmation dialog before loading stops.
+  // WKBasedNavigationManager doesn't trigger repost on |goForward| due to
+  // WKWebView's back-forward cache. Force reload to trigger repost. Not waiting
+  // because WKBasedNavigationManager presents repost confirmation dialog before
+  // loading stops.
   if ([ChromeEarlGrey isSlimNavigationManagerEnabled]) {
-    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
+    [ChromeEarlGrey reloadAndWaitForCompletion:NO];
   }
 
   {
@@ -328,7 +322,7 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   // [ChromeEarlGrey reload] because WKBasedNavigationManager presents repost
   // confirmation dialog before loading stops.
   if ([ChromeEarlGrey isSlimNavigationManagerEnabled]) {
-    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
+    [ChromeEarlGrey reloadAndWaitForCompletion:NO];
   }
 
   {
@@ -375,8 +369,11 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   [ChromeEarlGrey loadURL:GetGenericUrl()];
   [self openBackHistory];
   [self waitForTabHistoryView];
-  id<GREYMatcher> historyItem = grey_text(
-      base::SysUTF16ToNSString(web::GetDisplayTitleForUrl(destinationURL)));
+
+  // Mimic |web::GetDisplayTitleForUrl| behavior which uses FormatUrl
+  // internally. It can't be called directly from the EarlGrey 2 test process.
+  base::string16 title = url_formatter::FormatUrl(destinationURL);
+  id<GREYMatcher> historyItem = grey_text(base::SysUTF16ToNSString(title));
   [[EarlGrey selectElementWithMatcher:historyItem] performAction:grey_tap()];
   [ChromeEarlGrey waitForPageToFinishLoading];
 
@@ -412,7 +409,7 @@ id<GREYMatcher> ResendPostButtonMatcher() {
   // [ChromeEarlGrey reload] because WKBasedNavigationManager presents repost
   // confirmation dialog before loading stops.
   if ([ChromeEarlGrey isSlimNavigationManagerEnabled]) {
-    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
+    [ChromeEarlGrey reloadAndWaitForCompletion:NO];
   }
 
   {
@@ -471,14 +468,11 @@ id<GREYMatcher> ResendPostButtonMatcher() {
       assertWithMatcher:grey_notNil()];
 
   // WKBasedNavigationManager presents repost confirmation dialog before loading
-  // stops.
-  if ([ChromeEarlGrey isSlimNavigationManagerEnabled]) {
-    [chrome_test_util::BrowserCommandDispatcherForMainBVC() reload];
-  } else {
-    // Legacy navigation manager presents repost confirmation dialog after
-    // loading stops.
-    [ChromeEarlGrey reload];
-  }
+  // stops so do not wait for load to complete because it never will. Legacy
+  // navigation manager presents repost confirmation dialog after loading stops,
+  // so wait for load to finish.
+  BOOL wait = ![ChromeEarlGrey isSlimNavigationManagerEnabled];
+  [ChromeEarlGrey reloadAndWaitForCompletion:wait];
 
   {
     // When slim navigation manager is enabled, synchronization must be disabled
@@ -623,9 +617,7 @@ id<GREYMatcher> ResendPostButtonMatcher() {
         conditionWithName:@"Wait for web view to be interactable."
                     block:^BOOL {
                       NSError* error = nil;
-                      id<GREYMatcher> webViewMatcher = WebViewInWebState(
-                          chrome_test_util::GetCurrentWebState());
-                      [[EarlGrey selectElementWithMatcher:webViewMatcher]
+                      [[EarlGrey selectElementWithMatcher:WebViewMatcher()]
                           assertWithMatcher:grey_interactable()
                                       error:&error];
                       return !error;
@@ -634,10 +626,8 @@ id<GREYMatcher> ResendPostButtonMatcher() {
                    waitWithTimeout:base::test::ios::kWaitForUIElementTimeout],
                @"Web view did not become interactable.");
 
-    web::WebState* currentWebState = chrome_test_util::GetCurrentWebState();
     [[EarlGrey selectElementWithMatcher:WebViewMatcher()]
-        performAction:web::WebViewTapElement(
-                          currentWebState,
+        performAction:TapWebElement(
                           [ElementSelector selectorWithElementID:ID])];
 
     // Wait until the keyboard shows up before tapping.
