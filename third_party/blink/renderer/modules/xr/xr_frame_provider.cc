b504fe3e87eb5950ec6f20b52c095f219c322192
Klaus Weidner <klausw@chromium.org>
2019-11-12 01:16:35 +0000

    WebXR: move XR input processing to xr_session
    xr_frame_provider had some almost-but-not-quite-identical logic
    for XR input processing that was copied for immersive and inline
    sessions. Move this logic to xr_session to reduce duplication.
    
    Change-Id: I04955a4c579e096476b3c898f5b0069495afb9e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1907416
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714354}

--- third_party/blink/renderer/modules/xr/xr_frame_provider.cc
+++ third_party/blink/renderer/modules/xr/xr_frame_provider.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/imagebitmap/image_bitmap.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
-#include "third_party/blink/renderer/modules/xr/type_converters.h"
 #include "third_party/blink/renderer/modules/xr/xr.h"
 #include "third_party/blink/renderer/modules/xr/xr_plane_detection_state.h"
 #include "third_party/blink/renderer/modules/xr/xr_session.h"
@@ -49,16 +48,6 @@ class XRFrameProviderRequestCallback
 
   Member<XRFrameProvider> frame_provider_;
 };
-
-std::unique_ptr<TransformationMatrix> getPoseMatrix(
-    const device::mojom::blink::VRPosePtr& pose) {
-  if (!pose)
-    return nullptr;
-
-  return std::make_unique<TransformationMatrix>(
-      mojo::TypeConverter<TransformationMatrix,
-                          device::mojom::blink::VRPosePtr>::Convert(pose));
-}
 
 }  // namespace
 
@@ -401,17 +390,8 @@ void XRFrameProvider::ProcessScheduledFrame(
     // presentation frame as newly created presentation frame will get passed to
     // the input source select[/start/end] events.
     immersive_session_->UpdatePresentationFrameState(
-        high_res_now_ms, getPoseMatrix(frame_pose), frame_data,
+        high_res_now_ms, frame_pose, frame_data, frame_id_,
         is_immersive_frame_position_emulated_);
-
-    if (frame_pose) {
-      base::span<const device::mojom::blink::XRInputSourceStatePtr>
-          input_states;
-      if (frame_pose->input_state.has_value())
-        input_states = frame_pose->input_state.value();
-
-      immersive_session_->OnInputStateChange(frame_id_, input_states);
-    }
 
     // Check if immersive session is still set as OnInputStateChange may have
     // allowed a ForceEndSession to be triggered.
@@ -470,17 +450,8 @@ void XRFrameProvider::ProcessScheduledFrame(
       // presentation frame as newly created presentation frame will get passed
       // to the input source select[/start/end] events.
       session->UpdatePresentationFrameState(
-          high_res_now_ms, getPoseMatrix(frame_pose), frame_data,
+          high_res_now_ms, frame_pose, frame_data, frame_id_,
           true /* Non-immersive positions are always emulated */);
-
-      if (frame_pose) {
-        base::span<const device::mojom::blink::XRInputSourceStatePtr>
-            input_states;
-        if (frame_pose->input_state.has_value())
-          input_states = frame_pose->input_state.value();
-
-        session->OnInputStateChange(frame_id_, input_states);
-      }
 
       // If the input state change caused this session to end, we should stop
       // processing.
--- third_party/blink/renderer/modules/xr/xr_session.h
+++ third_party/blink/renderer/modules/xr/xr_session.h
@@ -190,11 +190,6 @@ class XRSession final
   void OnFrame(double timestamp,
                const base::Optional<gpu::MailboxHolder>& output_mailbox_holder);
 
-  void OnInputStateChange(
-      int16_t frame_id,
-      base::span<const device::mojom::blink::XRInputSourceStatePtr>
-          input_states);
-
   // XRInputSourceButtonListener
   void OnButtonEvent(
       device::mojom::blink::XRInputSourceStatePtr input_source) override;
@@ -264,8 +259,9 @@ class XRSession final
   // presentation frames.
   void UpdatePresentationFrameState(
       double timestamp,
-      std::unique_ptr<TransformationMatrix> mojo_from_viewer,
+      const device::mojom::blink::VRPosePtr& frame_pose,
       const device::mojom::blink::XRFrameDataPtr& frame_data,
+      int16_t frame_id,
       bool emulated_position);
 
   // Notifies immersive session that the environment integration provider has
@@ -293,8 +289,13 @@ class XRSession final
   void OnInputStateChangeInternal(
       int16_t frame_id,
       base::span<const device::mojom::blink::XRInputSourceStatePtr>
-          input_states,
-      bool from_eventing);
+          input_states);
+  void ProcessInputSourceEvents(
+      base::span<const device::mojom::blink::XRInputSourceStatePtr>
+          input_states);
+  void UpdateWorldUnderstandingStateForFrame(
+      double timestamp,
+      const device::mojom::blink::XRFrameDataPtr& frame_data);
 
   // XRSessionClient
   void OnChanged(device::mojom::blink::VRDisplayInfoPtr display_info) override;
