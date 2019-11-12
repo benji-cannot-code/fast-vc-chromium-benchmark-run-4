191c445396d454af141a986f2dddda969e8d7df8
Demetrios Papadopoulos <dpapad@chromium.org>
2019-11-12 20:46:30 +0000

    Downloads WebUI: Replace cr.ui.Command usage with a keydown handler.
    This is in preparation of migrating to Polymer3.
    
    Bug: 1022215
    Change-Id: Iee652ed3477175346f99c7a490d1637e754644ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1907413
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714640}

--- chrome/browser/resources/downloads/manager.js
+++ chrome/browser/resources/downloads/manager.js
@@ -76,6 +76,9 @@ cr.define('downloads', function() {
     /** @private {Array<number>} */
     listenerIds_: null,
 
+    /** @private {?Function} */
+    boundOnKeyDown_: null,
+
     /** @override */
     created: function() {
       const browserProxy = downloads.BrowserProxy.getInstance();
@@ -104,12 +107,18 @@ cr.define('downloads', function() {
         this.mojoEventTarget_.updateItem.addListener(
             this.updateItem_.bind(this)),
       ];
+
+      this.boundOnKeyDown_ = e => this.onKeyDown_(e);
+      document.addEventListener('keydown', this.boundOnKeyDown_);
     },
 
     /** @override */
     detached: function() {
       this.listenerIds_.forEach(
           id => assert(this.mojoEventTarget_.removeListener(id)));
+
+      document.removeEventListener('keydown', this.boundOnKeyDown_);
+      this.boundOnKeyDown_ = null;
     },
 
     /** @private */
@@ -187,34 +196,53 @@ cr.define('downloads', function() {
     },
 
     /**
-     * @param {Event} e
+     * @param {!KeyboardEvent} e
      * @private
      */
-    onCanExecute_: function(e) {
-      e = /** @type {cr.ui.CanExecuteEvent} */ (e);
-      switch (e.command.id) {
-        case 'undo-command':
-          e.canExecute = this.$.toolbar.canUndo();
-          break;
-        case 'clear-all-command':
-          e.canExecute = this.$.toolbar.canClearAll();
-          break;
+    onKeyDown_: function(e) {
+      let clearAllKey = 'c';
+      // <if expr="is_macosx">
+      // On Mac, pressing alt+c produces 'ç' as |event.key|.
+      clearAllKey = 'ç';
+      // </if>
+      if (e.key === clearAllKey && e.altKey && !e.ctrlKey && !e.shiftKey &&
+          !e.metaKey) {
+        this.onClearAllCommand_();
+        e.preventDefault();
+        return;
+      }
+
+      if (e.key === 'z' && !e.altKey && !e.shiftKey) {
+        let hasTriggerModifier = e.ctrlKey && !e.metaKey;
+        // <if expr="is_macosx">
+        hasTriggerModifier = !e.ctrlKey && e.metaKey;
+        // </if>
+        if (hasTriggerModifier) {
+          this.onUndoCommand_();
+          e.preventDefault();
+        }
       }
     },
 
-    /**
-     * @param {Event} e
-     * @private
-     */
-    onCommand_: function(e) {
-      if (e.command.id == 'clear-all-command') {
-        cr.toastManager.getInstance().show(
-            loadTimeData.getString('toastClearedAll'), true);
-        this.mojoHandler_.clearAll();
-      } else if (e.command.id == 'undo-command') {
-        cr.toastManager.getInstance().hide();
-        this.mojoHandler_.undo();
+    /** @private */
+    onClearAllCommand_() {
+      if (!this.$.toolbar.canClearAll()) {
+        return;
       }
+
+      this.mojoHandler_.clearAll();
+      cr.toastManager.getInstance().show(
+          loadTimeData.getString('toastClearedAll'), true);
+    },
+
+    /** @private */
+    onUndoCommand_() {
+      if (!this.$.toolbar.canUndo()) {
+        return;
+      }
+
+      cr.toastManager.getInstance().hide();
+      this.mojoHandler_.undo();
     },
 
     /** @private */
@@ -234,10 +262,6 @@ cr.define('downloads', function() {
      * @private
      */
     onLoad_: function() {
-      cr.ui.decorate('command', cr.ui.Command);
-      document.addEventListener('canExecute', this.onCanExecute_.bind(this));
-      document.addEventListener('command', this.onCommand_.bind(this));
-
       this.searchService_.loadMore();
       return this.loaded_.promise;
     },
