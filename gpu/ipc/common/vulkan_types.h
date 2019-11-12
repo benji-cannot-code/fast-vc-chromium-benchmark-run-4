a6030585349fd8465eb6374807270928ab657d41
Peng Huang <penghuang@chromium.org>
2019-11-12 10:19:22 +0000

    Add vulkan information in about::gpu page
    This CL adds VulkanInfo in GPUInfo, and then send it to browser
    process with GPUInfo. And it will be serialized to binary data and pass
    to about:gpu page for displaying.
    
    This CL adds a vulkan_types.mojom generated from vulkan header file.
    The structs in that files have many fields. It increases binary size.
    The increasing is expected.
    
    Bug: 887018
    Change-Id: I2725326091ef47c3034939b3970d0e5bbaf6dc3f
    Binary-Size: Size increase is unavoidable (see above)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868832
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714424}

---
+++ gpu/ipc/common/vulkan_types.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GPU_IPC_COMMON_VULKAN_TYPES_H_
+#define GPU_IPC_COMMON_VULKAN_TYPES_H_
+
+#include <vulkan/vulkan.h>
+
+#endif  // GPU_IPC_COMMON_VULKAN_TYPES_H_
--- gpu/vulkan/vulkan_instance.h
+++ gpu/vulkan/vulkan_instance.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/macros.h"
+#include "gpu/config/vulkan_info.h"
 #include "gpu/vulkan/vulkan_export.h"
-#include "gpu/vulkan/vulkan_info.h"
 #include "ui/gfx/extension_set.h"
 
 namespace gpu {
