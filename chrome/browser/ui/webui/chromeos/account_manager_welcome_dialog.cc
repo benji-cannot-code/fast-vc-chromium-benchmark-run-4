02605fb612d99732f277c0d5786cad0c52668ee0
Anastasiia Nikolaienko <anastasiian@chromium.org>
2019-11-12 10:26:22 +0000

    Don't open Settings during shutdown
    OnDialogClosed is also called during shutdown. Opening Settings at this
    point leads to a crash. Check that Chrome is not attempting shutdown
    before opening the window.
    
    Bug: 1018658
    Change-Id: I99997e8d6cc7e83d3f8f1ad3be3b6c72b3c3c8c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903365
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Anastasiia Nikolaienko <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714428}

--- chrome/browser/ui/webui/chromeos/account_manager_welcome_dialog.cc
+++ chrome/browser/ui/webui/chromeos/account_manager_welcome_dialog.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/logging.h"
+#include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
@@ -74,8 +75,11 @@ void AccountManagerWelcomeDialog::AdjustWidgetInitParams(
 
 void AccountManagerWelcomeDialog::OnDialogClosed(
     const std::string& json_retval) {
-  chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
-      ProfileManager::GetActiveUserProfile(), chrome::kAccountManagerSubPage);
+  // Opening Settings during shutdown leads to a crash.
+  if (!chrome::IsAttemptingShutdown()) {
+    chrome::SettingsWindowManager::GetInstance()->ShowOSSettings(
+        ProfileManager::GetActiveUserProfile(), chrome::kAccountManagerSubPage);
+  }
 
   SystemWebDialogDelegate::OnDialogClosed(json_retval);
 }
