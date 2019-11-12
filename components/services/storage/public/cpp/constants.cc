d33eeca3fac5ba775ff6dd01df466c29e9b14969
Ken Rockot <rockot@google.com>
2019-11-12 05:52:07 +0000

    Simplify Local Storage implementation and tests
    This eliminates redundancy in LocalStorageContextMojo ands hides some
    implementation details that were being unnecessarily exposed to
    e.g. DOMStorageContextWrapper. This is part of the preparation to move
    the implementation into the Storage Service.
    
    Bug: 1000959
    Change-Id: Ie94ff8a3948a02561aac67dcbea024d70c4a434e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900918
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714397}

---
+++ components/services/storage/public/cpp/constants.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/services/storage/public/cpp/constants.h"
+
+namespace storage {
+
+// The path where Local Storage data is persisted on disk, relative to a storage
+// partition's root directory.
+const base::FilePath::CharType kLocalStoragePath[] =
+    FILE_PATH_LITERAL("Local Storage");
+
+// The name of the Leveldb database to use for databases persisted on disk.
+const char kLocalStorageLeveldbName[] = "leveldb";
+
+}  // namespace storage
---
+++ components/services/storage/public/cpp/constants.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SERVICES_STORAGE_PUBLIC_CPP_CONSTANTS_H_
+#define COMPONENTS_SERVICES_STORAGE_PUBLIC_CPP_CONSTANTS_H_
+
+#include "base/component_export.h"
+#include "base/files/file_path.h"
+
+namespace storage {
+
+COMPONENT_EXPORT(STORAGE_SERVICE_PUBLIC)
+extern const base::FilePath::CharType kLocalStoragePath[];
+
+COMPONENT_EXPORT(STORAGE_SERVICE_PUBLIC)
+extern const char kLocalStorageLeveldbName[];
+
+}  // namespace storage
+
+#endif  // COMPONENTS_SERVICES_STORAGE_PUBLIC_CPP_CONSTANTS_H_
