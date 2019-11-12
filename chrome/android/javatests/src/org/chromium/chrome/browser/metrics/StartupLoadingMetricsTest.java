4b1be3aefbb3ad94808c0fa268392ce38e12df00
Theresa Wellington <twellington@chromium.org>
2019-11-12 16:59:18 +0000

    Disable two StartupLoadingMetricsTest tests on O+
    Disable flaky #testStartWithUrlRecorded and #testFromExternalAppRecorded
    on O+.
    
    BUG=1023433
    TBR=alexilin@chromium.org
    
    Change-Id: Ic3111d8cdd1af44003b3ffc6e211d330583b222f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911702
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714510}

--- chrome/android/javatests/src/org/chromium/chrome/browser/metrics/StartupLoadingMetricsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/metrics/StartupLoadingMetricsTest.java
@@ -24,6 +24,7 @@ import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LoadStatusRecorder.LoadLibraryStatus;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -151,6 +152,8 @@ public class StartupLoadingMetricsTest {
     @Test
     @LargeTest
     @RetryOnFailure
+    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.N_MR1,
+            message = "https://crbug.com/1023433")
     public void testStartWithURLRecorded() throws Exception {
         runAndWaitForPageLoadMetricsRecorded(
                 () -> mTabbedActivityTestRule.startMainActivityWithURL(mTestPage));
@@ -219,6 +222,8 @@ public class StartupLoadingMetricsTest {
     @Test
     @LargeTest
     @RetryOnFailure
+    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.N_MR1,
+            message = "https://crbug.com/1023433")
     public void testFromExternalAppRecorded() throws Exception {
         runAndWaitForPageLoadMetricsRecorded(
                 () -> mTabbedActivityTestRule.startMainActivityFromExternalApp(mTestPage, null));
