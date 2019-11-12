6b70993ab4d9988bc730d52066dd74f95728ad8e
Keishi Hattori <keishi@chromium.org>
2019-11-12 03:50:03 +0000

    Revert "Add image support to Remote Copy."
    This reverts commit 031922a62dfedd06a9f410d05c193ccbe7d60277.
    
    Reason for revert: RemoteCopyBrowserTest.ImageUrl is failing crbug.com/1023709
    
    Original change's description:
    > Add image support to Remote Copy.
    > 
    > Bug: 1018138, 1018132, 1020582
    > Change-Id: I50d131ec34ccee3bf5b76167178eac4728b51bea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903203
    > Reviewed-by: Richard Knoll <knollr@chromium.org>
    > Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#714193}
    
    TBR=mvanouwerkerk@chromium.org,mastiz@chromium.org,mkwst@chromium.org,huangdarwin@chromium.org,knollr@chromium.org
    
    Change-Id: Ia5f3ffae72f8812b7caf0c678bc17a65e92c89a9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1018138, 1018132, 1020582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911063
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714387}

--- ui/base/clipboard/test/test_clipboard.cc
+++ ui/base/clipboard/test/test_clipboard.cc
@@ -176,7 +176,7 @@ void TestClipboard::WriteText(const char* text_data, size_t text_len) {
   if (IsSupportedClipboardBuffer(ClipboardBuffer::kSelection))
     GetStore(ClipboardBuffer::kSelection)
         .data[ClipboardFormatType::GetPlainTextType()] = text;
-  ClipboardMonitor::GetInstance()->NotifyClipboardDataChanged();
+  ui::ClipboardMonitor::GetInstance()->NotifyClipboardDataChanged();
 }
 
 void TestClipboard::WriteHTML(const char* markup_data,
@@ -218,7 +218,6 @@ void TestClipboard::WriteBitmap(const SkBitmap& bitmap) {
     NOTREACHED() << "Unable to convert bitmap for clipboard";
     return;
   }
-  ClipboardMonitor::GetInstance()->NotifyClipboardDataChanged();
 }
 
 void TestClipboard::WriteData(const ClipboardFormatType& format,
