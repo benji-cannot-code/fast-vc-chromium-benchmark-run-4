fd4939f13faf86a31ae0b269c15cf95c5a6261fe
Willie Koomson <wvk@google.com>
2019-11-12 22:11:46 +0000

    Add throttle observer for ARC PIP
    This change adds a throttle observer that activates when
    an ARC window is added to the PipContainer, and deactivates when
    an ARC window is removed. This has the effect of unthrottling
    ARC when an ARC PIP window is visible.
    
    unthrottled when a YouTube app PIP window is visible.
    
    Bug: b:142365466
    Test: Unit tests; build/flash/run and check that container is
    Change-Id: I496e5a6ba67e433891e98601cec50f7079addd13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902523
    Commit-Queue: Willie Koomson <wvk@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714658}

--- chrome/browser/chromeos/arc/instance_throttle/arc_instance_throttle.cc
+++ chrome/browser/chromeos/arc/instance_throttle/arc_instance_throttle.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/instance_throttle/arc_active_window_throttle_observer.h"
 #include "chrome/browser/chromeos/arc/instance_throttle/arc_app_launch_throttle_observer.h"
 #include "chrome/browser/chromeos/arc/instance_throttle/arc_boot_phase_throttle_observer.h"
+#include "chrome/browser/chromeos/arc/instance_throttle/arc_pip_window_throttle_observer.h"
 #include "components/arc/arc_browser_context_keyed_service_factory_base.h"
 #include "components/arc/arc_util.h"
 
@@ -79,8 +80,9 @@ ArcInstanceThrottle::ArcInstanceThrottle(content::BrowserContext* context,
     : ThrottleService(context),
       delegate_(std::make_unique<DefaultDelegateImpl>()) {
   AddObserver(std::make_unique<ArcActiveWindowThrottleObserver>());
-  AddObserver(std::make_unique<ArcBootPhaseThrottleObserver>());
   AddObserver(std::make_unique<ArcAppLaunchThrottleObserver>());
+  AddObserver(std::make_unique<ArcBootPhaseThrottleObserver>());
+  AddObserver(std::make_unique<ArcPipWindowThrottleObserver>());
   StartObservers();
 }
 
