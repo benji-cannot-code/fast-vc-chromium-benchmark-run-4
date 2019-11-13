bac58f010942b2ee226d4ad838aed81e2e559a30
Ramin Halavati <rhalavati@chromium.org>
2019-11-13 06:15:57 +0000

    Enable serving URL from persistent filesystem in incognito.
    Serving URL Request from persistent filesystems in incognito mode is
    enabled.
    
    Bug: 990592
    Change-Id: Ieaaf59e60cc9d8de8ecbb3395ee209b0854555b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911202
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714848}

--- storage/browser/file_system/file_system_context.cc
+++ storage/browser/file_system/file_system_context.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/file_system/file_permission_policy.h"
 #include "storage/browser/file_system/file_stream_reader.h"
 #include "storage/browser/file_system/file_stream_writer.h"
+#include "storage/browser/file_system/file_system_features.h"
 #include "storage/browser/file_system/file_system_file_util.h"
 #include "storage/browser/file_system/file_system_operation.h"
 #include "storage/browser/file_system/file_system_operation_runner.h"
@@ -490,6 +491,11 @@ bool FileSystemContext::CanServeURLRequest(const FileSystemURL& url) const {
     return false;
   if (url.type() == kFileSystemTypeTemporary)
     return true;
+  if (url.type() == kFileSystemTypePersistent &&
+      base::FeatureList::IsEnabled(
+          features::kEnablePersistentFilesystemInIncognito)) {
+    return true;
+  }
   return !is_incognito_ || !FileSystemContext::IsSandboxFileSystem(url.type());
 }
 
