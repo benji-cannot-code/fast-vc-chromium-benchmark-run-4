9377fd19913707b12b1990686030d54048469f36
Sammie Quon <sammiequon@google.com>
2019-11-13 23:44:57 +0000

    overview: Close button is not layouted properly.
    Gave the parent a rect that has x-bounds, but still using the parent's
    parent width, resulting in a overflow.
    
    Test: manual
    Bug: 1020539
    Change-Id: I983cf0813c6b99a74960761c8c8fd241b2995e0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1914942
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715105}

--- ash/wm/overview/overview_item_view.cc
+++ ash/wm/overview/overview_item_view.cc
@@ -270,7 +270,8 @@ gfx::Rect OverviewItemView::GetHeaderBounds() const {
   // ---------------------------+---------+
   //                                   c
   //                                   |
-  return gfx::Rect(margin, margin, GetLocalBounds().width() - right_padding,
+  return gfx::Rect(margin, margin,
+                   GetLocalBounds().width() - margin - right_padding,
                    kHeaderHeightDp);
 }
 
