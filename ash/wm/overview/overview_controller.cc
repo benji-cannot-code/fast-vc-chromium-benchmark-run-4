852df2cefd13cd1bf5d7eec9ffb9c0643611c676
Toni Barzic <tbarzic@chromium.org>
2019-11-13 21:09:25 +0000

    Mark overview session as shutting down sooner
    Updates EndOverview logic to mark session as shutting down sooner -
    before any windows are minimized, or observers are notified the session
    start was canceled, to prevent reentry in EndOverview() - in particular
    to prevent reentry due to active window change while minimizing windows
    while exiting to home screen (fade out, slide out exit types).
    
    This also makes OverviewItem::SetBounds() triggered by minimization
    exit early (instead of updating grid item widget bounds just before
    the overview session exits) - the cl updates
    OverviewSessionWithDragFromShelfFeatureTest.FadeOutExit expectations
    to address this change in behavior. The test now tests that grid
    item widget bounds remain the same during exit directly (instead of
    checking for identity transform).
    
    BUG=1024107
    
    Change-Id: If0f301bccc41de7f33b30a6017e508c97767ced1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913508
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715046}

--- ash/wm/overview/overview_controller.cc
+++ ash/wm/overview/overview_controller.cc
@@ -415,6 +415,9 @@ void OverviewController::ToggleOverview(
     OverviewSession::EnterExitOverviewType new_type =
         MaybeOverrideEnterExitTypeForHomeScreen(type, /*enter=*/false, windows);
     overview_session_->set_enter_exit_overview_type(new_type);
+
+    overview_session_->set_is_shutting_down(true);
+
     if (type == OverviewSession::EnterExitOverviewType::kSlideOutExit ||
         type == OverviewSession::EnterExitOverviewType::kFadeOutExit ||
         type == OverviewSession::EnterExitOverviewType::kSwipeFromShelf) {
@@ -439,7 +442,6 @@ void OverviewController::ToggleOverview(
       OnStartingAnimationComplete(/*canceled=*/true);
     start_animations_.clear();
 
-    overview_session_->set_is_shutting_down(true);
     // Do not show mask and show during overview shutdown.
     overview_session_->UpdateRoundedCornersAndShadow();
 
