49b57c6853d1fea3a92976f8e0813a272cae55ff
arthursonzogni <arthursonzogni@chromium.org>
2019-11-13 01:01:40 +0000

    Unify CSP: mojoify DidAddContentSecurityPolicy.
    Replace:
      FrameHostMsg_DidAddContentSecurityPolicies (IPC)
    By:
      FrameHost.DidAddContentSecurityPolicies (mojo)
    
    This corresponds to step 1.e from the "Unify CSP" document:
    https://docs.google.com/document/d/1v5mJnXJ5dSVXE_rgvJnNM9bzH0ni0YzdhPQ7GLqyhao
    
    The native struct are going to be converted in a series of follow-up.
    
    Bug: 1021462
    Change-Id: Ie77c21206c5a40535383c5c318458545930d3465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1905689
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714758}

--- content/test/test_render_frame.cc
+++ content/test/test_render_frame.cc
@@ -102,6 +102,8 @@ class MockFrameHost : public mojom::FrameHost {
                          bool user_gesture) override {}
 
   void FrameFocused() override {}
+  void DidAddContentSecurityPolicies(
+      const std::vector<ContentSecurityPolicy>&) override {}
 
  protected:
   // mojom::FrameHost:
