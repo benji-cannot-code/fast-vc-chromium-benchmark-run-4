e6ffeace23b7ff46c118cdf2b7825b8f1bfc42e7
Kent Tamura <tkent@chromium.org>
2019-11-13 08:38:52 +0000

    autofocus: Support delegatesFocus correctly
    * Rename FindActualFocusTarget() to GetFocusableArea() to match to
      the specification.
    * FlushAutofocusCandidates() applies GetFocusableArea() instead of
      focusing delegatesFocus hosts unconditionally.
    
    Bug: 1024056
    Change-Id: I954e306d9fec20bbe26b46060aefa0d9925ad14b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913222
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714861}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4029,9 +4029,9 @@ bool Element::DelegatesFocus() const {
   return AuthorShadowRoot() && AuthorShadowRoot()->delegatesFocus();
 }
 
-// Step 1 of https://html.spec.whatwg.org/C/#focusing-steps in a case
-// where |new focus target| is an element.
-Element* Element::FindActualFocusTarget() const {
+// https://html.spec.whatwg.org/C/#get-the-focusable-area
+Element* Element::GetFocusableArea() const {
+  DCHECK(!IsFocusable());
   // TODO(crbug.com/1018619): Support AREA -> IMG delegation.
   if (!DelegatesFocus())
     return nullptr;
@@ -4085,7 +4085,7 @@ void Element::focus(const FocusParams& params) {
   //
   // 1. If new focus target is not a focusable area, ...
   if (!IsFocusable()) {
-    if (Element* new_focus_target = FindActualFocusTarget()) {
+    if (Element* new_focus_target = GetFocusableArea()) {
       // Unlike the specification, we re-run focus() for new_focus_target
       // because we can't change |this| in a member function.
       new_focus_target->focus(FocusParams(SelectionBehaviorOnFocus::kReset,
