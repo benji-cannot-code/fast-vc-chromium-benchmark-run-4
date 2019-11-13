aa5aad94a40cd6599eb96b0098e94eaefe6c58fd
Peng Huang <penghuang@chromium.org>
2019-11-13 20:38:23 +0000

    Reland "Add vulkan information in about::gpu page"
    This reverts commit fdda085df1b0f137a9839484c186976829383188.
    
    Reason for revert: Reland with fixing.
    
    Original change's description:
    > Revert "Add vulkan information in about::gpu page"
    > 
    > This reverts commit 2ef6fce852fadeb2f5b1717df90e9359937600a0.
    > 
    > Reason for revert: This CL broke GPU FYI Win x64 DX12 Vulkan Builder (dbg)
    > 
    > https://ci.chromium.org/p/chromium/builders/ci/GPU%20FYI%20Win%20x64%20DX12%20Vulkan%20Builder%20%28dbg%29
    > 
    > lld-link: error: obj/gpu/vulkan/vulkan/vulkan_instance.obj: locally defined symbol imported: public: __cdecl gpu::VulkanInfo::VulkanInfo(void) (defined in obj/gpu/config/vulkan_info/vulkan_info.obj) [LNK4217]
    > 
    > 
    > Original change's description:
    > > Add vulkan information in about::gpu page
    > > 
    > > This CL adds VulkanInfo in GPUInfo, and then send it to browser
    > > process with GPUInfo. And it will be serialized to binary data and pass
    > > to about:gpu page for displaying.
    > > 
    > > This CL adds a vulkan_types.mojom generated from vulkan header file.
    > > The structs in that files have many fields. It increases binary size.
    > > The increasing is expected.
    > > 
    > > Bug: 887018
    > > Change-Id: I2725326091ef47c3034939b3970d0e5bbaf6dc3f
    > > Binary-Size: Size increase is unavoidable (see above)
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868832
    > > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > > Reviewed-by: Jonathan Backer <backer@chromium.org>
    > > Commit-Queue: Peng Huang <penghuang@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#714424}
    > 
    > TBR=penghuang@chromium.org,backer@chromium.org,zmo@chromium.org,kbr@chromium.org,tsepez@chromium.org,alexmos@chromium.org
    > 
    > Change-Id: I3b0aff2cfcfa8b65df2ca9109325451567cf8039
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 887018
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913565
    > Reviewed-by: Chris Blume <cblume@chromium.org>
    > Commit-Queue: Chris Blume <cblume@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#714714}
    
    TBR=penghuang@chromium.org,backer@chromium.org,zmo@chromium.org,kbr@chromium.org,tsepez@chromium.org,alexmos@chromium.org,cblume@chromium.org
    
    Change-Id: I2061c0a8508be602777467b7ae16cb51827bf5f8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 887018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1914294
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715034}

--- gpu/vulkan/vulkan_device_queue.cc
+++ gpu/vulkan/vulkan_device_queue.cc
@@ -9,10 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "gpu/config/vulkan_info.h"
 #include "gpu/vulkan/vulkan_command_pool.h"
 #include "gpu/vulkan/vulkan_fence_helper.h"
 #include "gpu/vulkan/vulkan_function_pointers.h"
-#include "gpu/vulkan/vulkan_info.h"
 
 namespace gpu {
 
