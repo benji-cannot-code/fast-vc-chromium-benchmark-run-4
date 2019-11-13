5e8964ca19cb19070aa488198e5cead780816726
Koji Ishii <kojii@chromium.org>
2019-11-13 19:24:08 +0000

    Add NGTextOffset data structure
    This patch adds |NGTextOffset| to represent a range of a text
    for miscellaneous text-related classes in LayoutNG.
    
    Start with |NGInlineCurosr| to avoid multiple calls to get
    |start| and |end|, each of them to compute the current
    object, but we can apply this to more classes in following
    patches.
    
    Bug: 982194
    Change-Id: I79cd67bf6e9737f5af7e4d928212f6f01326f50d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911862
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715013}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/editing/forward.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_text_offset.h"
 #include "third_party/blink/renderer/platform/text/text_direction.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_view.h"
@@ -27,12 +28,12 @@ class NGFragmentItems;
 class NGInlineBreakToken;
 class NGPaintFragment;
 class NGPhysicalBoxFragment;
-enum class NGStyleVariant;
 class Node;
+class ShapeResultView;
+enum class NGStyleVariant;
 struct PhysicalOffset;
 struct PhysicalRect;
 struct PhysicalSize;
-class ShapeResultView;
 
 // This class traverses fragments in an inline formatting context.
 //
@@ -185,8 +186,9 @@ class CORE_EXPORT NGInlineCursor {
 
   // Returns start/end of offset in text content of current text fragment.
   // It is error when this cursor doesn't point to text fragment.
-  unsigned CurrentTextStartOffset() const;
-  unsigned CurrentTextEndOffset() const;
+  NGTextOffset CurrentTextOffset() const;
+  unsigned CurrentTextStartOffset() const { return CurrentTextOffset().start; }
+  unsigned CurrentTextEndOffset() const { return CurrentTextOffset().end; }
 
   // Returns text of the current position. It is error to call other than
   // text.
