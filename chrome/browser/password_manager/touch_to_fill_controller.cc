54ff21a23b42184e98eed7b90b237f527f663b6f
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-11-13 06:06:03 +0000

    [Passwords] Add UKMs for Touch To Fill
    This change adds UKMs for Touch To Fill User Actions.
    
    Launch Bug: https://crbug.com/1008937
    Collection Review: go/touch-to-fill-ukm-collection-review
    
    Bug: 1020996
    Change-Id: Ie2c4c731e2f53d879b9d5df73ab4e4465b21b228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1897932
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714844}

--- chrome/browser/password_manager/touch_to_fill_controller.cc
+++ chrome/browser/password_manager/touch_to_fill_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
+#include "base/util/type_safety/pass_key.h"
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
 #include "chrome/browser/touch_to_fill/touch_to_fill_view.h"
 #include "components/favicon/core/favicon_service.h"
@@ -15,7 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/origin_credential_store.h"
 #include "components/password_manager/core/browser/password_manager_driver.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
+#include "components/ukm/content/source_url_recorder.h"
 #include "components/url_formatter/elide_url.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "services/network/public/cpp/is_potentially_trustworthy.h"
 
 using ShowVirtualKeyboard =
@@ -36,9 +41,15 @@ void OnImageFetched(base::OnceCallback<void(const gfx::Image&)> callback,
 }  // namespace
 
 TouchToFillController::TouchToFillController(
+    util::PassKey<TouchToFillControllerTest>) {}
+
+TouchToFillController::TouchToFillController(
     ChromePasswordManagerClient* password_client,
     favicon::FaviconService* favicon_service)
-    : password_client_(password_client), favicon_service_(favicon_service) {}
+    : password_client_(password_client),
+      favicon_service_(favicon_service),
+      source_id_(ukm::GetSourceIdForWebContentsDocument(
+          password_client_->web_contents())) {}
 
 TouchToFillController::~TouchToFillController() = default;
 
@@ -67,6 +78,10 @@ void TouchToFillController::OnCredentialSelected(
   driver_->TouchToFillClosed(ShowVirtualKeyboard(false));
   std::exchange(driver_, nullptr)
       ->FillSuggestion(credential.username, credential.password);
+
+  ukm::builders::TouchToFill_Shown(source_id_)
+      .SetUserAction(static_cast<int64_t>(UserAction::kSelectedCredential))
+      .Record(ukm::UkmRecorder::Get());
 }
 
 void TouchToFillController::OnManagePasswordsSelected() {
@@ -77,6 +92,10 @@ void TouchToFillController::OnManagePasswordsSelected() {
       ->TouchToFillClosed(ShowVirtualKeyboard(false));
   password_client_->NavigateToManagePasswordsPage(
       password_manager::ManagePasswordsReferrer::kTouchToFill);
+
+  ukm::builders::TouchToFill_Shown(source_id_)
+      .SetUserAction(static_cast<int64_t>(UserAction::kSelectedManagePasswords))
+      .Record(ukm::UkmRecorder::Get());
 }
 
 void TouchToFillController::OnDismiss() {
@@ -84,6 +103,10 @@ void TouchToFillController::OnDismiss() {
     return;
 
   std::exchange(driver_, nullptr)->TouchToFillClosed(ShowVirtualKeyboard(true));
+
+  ukm::builders::TouchToFill_Shown(source_id_)
+      .SetUserAction(static_cast<int64_t>(UserAction::kDismissed))
+      .Record(ukm::UkmRecorder::Get());
 }
 
 gfx::NativeView TouchToFillController::GetNativeView() {
