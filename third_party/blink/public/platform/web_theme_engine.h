3bdd33d64793096466297804345f0540d683aef5
Alex Keng <shihken@microsoft.com>
2019-11-13 09:05:02 +0000

    Make the border widths consistent (1px) for range/meter/progress/radio/checkbox
    This change updates the rendering of range/meter/checkbox so their border
    widths are 1px regardless of the zoom level (same behavior progress
    and radio currently have)
    
    To prevent the borders from growing on zoom, the ThemePainterDefault are updated
    for checkbox and range so the painting context is not modified when zoom level is not 1.
    
    The zoom info are then embedded in ButtonExtraParams and SliderExtraParams
    and passed to NativeThemeAura so PaintCheckbox can render border-radius correctly
    and PaintSliderTrack can render track height correctly.
    
    For meter the border-width is changed from 1px on thin.
    
    Tests:
    meter: meter-extreme-values.html
    range: range-appearance-basic.html
    checkbox: checkbox-appearance-basic.html
    
    Bug: 1021319
    Change-Id: Icee5d83a859556909bdf930c314d70ddd17d7784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910864
    Commit-Queue: Alex Keng <shihken@microsoft.com>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714868}

--- third_party/blink/public/platform/web_theme_engine.h
+++ third_party/blink/public/platform/web_theme_engine.h
@@ -118,6 +118,7 @@ class WebThemeEngine {
     bool indeterminate;  // Whether the button state is indeterminate.
     bool has_border;
     SkColor background_color;
+    float zoom;
   };
 
   // Extra parameters for PartTextField
@@ -145,6 +146,7 @@ class WebThemeEngine {
     bool in_drag;
     int thumb_x;
     int thumb_y;
+    float zoom;
   };
 
   // Extra parameters for PartInnerSpinButton
--- ui/native_theme/native_theme_aura.cc
+++ ui/native_theme/native_theme_aura.cc
@@ -502,9 +502,10 @@ void NativeThemeAura::PaintCheckbox(cc::PaintCanvas* canvas,
                                           color_scheme);
   }
 
+  const float border_radius =
+      SkIntToScalar(kCheckboxBorderRadius) * button.zoom;
   SkRect skrect = PaintCheckboxRadioCommon(canvas, state, rect, button, true,
-                                           SkIntToScalar(kCheckboxBorderRadius),
-                                           color_scheme);
+                                           border_radius, color_scheme);
 
   if (!skrect.isEmpty()) {
     cc::PaintFlags flags;
@@ -517,14 +518,12 @@ void NativeThemeAura::PaintCheckbox(cc::PaintCanvas* canvas,
           skrect.makeInset(skrect.width() * kIndeterminateInsetWidthRatio,
                            skrect.height() * kIndeterminateInsetHeightRatio);
       flags.setStyle(cc::PaintFlags::kFill_Style);
-      canvas->drawRoundRect(indeterminate, SkIntToScalar(kCheckboxBorderRadius),
-                            SkIntToScalar(kCheckboxBorderRadius), flags);
+      canvas->drawRoundRect(indeterminate, border_radius, border_radius, flags);
     } else if (button.checked) {
       // Draw the accent background.
       flags.setStyle(cc::PaintFlags::kFill_Style);
       flags.setColor(ControlsAccentColorForState(state, color_scheme));
-      canvas->drawRoundRect(skrect, kCheckboxBorderRadius,
-                            kCheckboxBorderRadius, flags);
+      canvas->drawRoundRect(skrect, border_radius, border_radius, flags);
 
       // Draw the checkmark.
       SkPath check;
@@ -766,13 +765,14 @@ void NativeThemeAura::PaintSliderTrack(cc::PaintCanvas* canvas,
   cc::PaintFlags flags;
   flags.setAntiAlias(true);
   flags.setColor(ControlsFillColorForState(state, color_scheme));
-  SkRect track_rect = AlignSliderTrack(rect, slider, false, kSliderTrackHeight);
+  const float track_height = kSliderTrackHeight * slider.zoom;
+  SkRect track_rect = AlignSliderTrack(rect, slider, false, track_height);
   canvas->drawRoundRect(track_rect, kSliderTrackBorderRadius,
                         kSliderTrackBorderRadius, flags);
 
   // Paint the value slider track.
   flags.setColor(ControlsAccentColorForState(state, color_scheme));
-  SkRect value_rect = AlignSliderTrack(rect, slider, true, kSliderTrackHeight);
+  SkRect value_rect = AlignSliderTrack(rect, slider, true, track_height);
   canvas->drawRoundRect(value_rect, kSliderTrackBorderRadius,
                         kSliderTrackBorderRadius, flags);
 
