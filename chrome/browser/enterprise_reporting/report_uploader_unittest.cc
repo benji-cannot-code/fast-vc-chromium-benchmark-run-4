1bd6f553074eb17e26855830c4c4e9d732f5d9c1
Anqing Zhao <anqing@google.com>
2019-11-13 20:57:26 +0000

    Support ChromeOsUserReport in ReportUploader for Chrome OS
    Previously, ReportUploader is designed for reporting
    ChromeDesktopReportRequest on Windows, Mac and Linux. To supporting
    Chrome OS, now we change this class a little bit to reuse its logic.
    
    Bug: 1010213
    Change-Id: I724d203534ec1a831700842181aec4f1121b6d13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1908542
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@google.com>
    Cr-Commit-Position: refs/heads/master@{#715039}

--- chrome/browser/enterprise_reporting/report_uploader_unittest.cc
+++ chrome/browser/enterprise_reporting/report_uploader_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
+#include "build/build_config.h"
 #include "components/policy/core/common/cloud/mock_cloud_policy_client.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -21,13 +22,22 @@ using ::testing::WithArgs;
 
 namespace enterprise_reporting {
 namespace {
-constexpr const char* kOsUserNames[] = {"name1", "name2"};
+constexpr const char* kBrowserVersionNames[] = {"name1", "name2"};
 constexpr char kResponseMetricsName[] = "Enterprise.CloudReportingResponse";
 
 }  // namespace
 
 class ReportUploaderTest : public ::testing::Test {
  public:
+  // Different CloudPolicyClient proxy function will be used in test cases based
+  // on the current operation system. They share same retry and error handling
+  // behaviors provided by ReportUploader.
+#if defined(OS_CHROMEOS)
+#define UploadReportProxy UploadChromeOsUserReportProxy
+#else
+#define UploadReportProxy UploadChromeDesktopReportProxy
+#endif
+
   ReportUploaderTest()
       : task_environment_(base::test::TaskEnvironment::TimeSource::MOCK_TIME) {
     CreateUploader(0);
@@ -37,11 +47,13 @@ class ReportUploaderTest : public ::testing::Test {
   void UploadReportAndSetExpectation(
       int number_of_request,
       ReportUploader::ReportStatus expected_status) {
-    DCHECK_LE(number_of_request, 2) << "Please update kOsUserNames above.";
+    DCHECK_LE(number_of_request, 2)
+        << "Please update kBrowserVersionNames above.";
     ReportUploader::Requests requests;
     for (int i = 0; i < number_of_request; i++) {
-      auto request = std::make_unique<em::ChromeDesktopReportRequest>();
-      request->set_os_user_name(kOsUserNames[i]);
+      auto request = std::make_unique<ReportUploader::Request>();
+      request->mutable_browser_report()->set_browser_version(
+          kBrowserVersionNames[i]);
       requests.push(std::move(request));
     }
     has_responded_ = false;
@@ -97,7 +109,7 @@ class ReportUploaderTestWithTransientError
       public ::testing::WithParamInterface<policy::DeviceManagementStatus> {};
 
 TEST_F(ReportUploaderTest, Success) {
-  EXPECT_CALL(client_, UploadChromeDesktopReportProxy(_, _))
+  EXPECT_CALL(client_, UploadReportProxy(_, _))
       .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(true)));
   UploadReportAndSetExpectation(/*number_of_request=*/1,
                                 ReportUploader::kSuccess);
@@ -110,7 +122,7 @@ TEST_F(ReportUploaderTest, Success) {
 
 TEST_F(ReportUploaderTest, PersistentError) {
   CreateUploader(/* retry_count = */ 1);
-  EXPECT_CALL(client_, UploadChromeDesktopReportProxy(_, _))
+  EXPECT_CALL(client_, UploadReportProxy(_, _))
       .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)));
   client_.SetStatus(policy::DM_STATUS_SERVICE_DEVICE_NOT_FOUND);
   UploadReportAndSetExpectation(/*number_of_request=*/2,
@@ -124,7 +136,7 @@ TEST_F(ReportUploaderTest, PersistentError) {
 
 TEST_F(ReportUploaderTest, RequestTooBigError) {
   CreateUploader(/* *retyr_count = */ 2);
-  EXPECT_CALL(client_, UploadChromeDesktopReportProxy(_, _))
+  EXPECT_CALL(client_, UploadReportProxy(_, _))
       .Times(2)
       .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)))
       .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)));
@@ -140,7 +152,7 @@ TEST_F(ReportUploaderTest, RequestTooBigError) {
 }
 
 TEST_F(ReportUploaderTest, RetryAndSuccess) {
-  EXPECT_CALL(client_, UploadChromeDesktopReportProxy(_, _))
+  EXPECT_CALL(client_, UploadReportProxy(_, _))
       .Times(2)
       .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)))
       .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(true)));
@@ -164,7 +176,7 @@ TEST_F(ReportUploaderTest, RetryAndSuccess) {
 }
 
 TEST_F(ReportUploaderTest, RetryAndFailedWithPersistentError) {
-  EXPECT_CALL(client_, UploadChromeDesktopReportProxy(_, _))
+  EXPECT_CALL(client_, UploadReportProxy(_, _))
       .Times(2)
       .WillRepeatedly(WithArgs<1>(policy::ScheduleStatusCallback(false)));
   CreateUploader(/* retry_count = */ 1);
@@ -190,7 +202,7 @@ TEST_F(ReportUploaderTest, RetryAndFailedWithPersistentError) {
 }
 
 TEST_F(ReportUploaderTest, RetryAndFailedWithTransientError) {
-  EXPECT_CALL(client_, UploadChromeDesktopReportProxy(_, _))
+  EXPECT_CALL(client_, UploadReportProxy(_, _))
       .Times(2)
       .WillRepeatedly(WithArgs<1>(policy::ScheduleStatusCallback(false)));
   CreateUploader(/* retry_count = */ 1);
@@ -217,21 +229,23 @@ TEST_F(ReportUploaderTest, MultipleReports) {
   {
     InSequence s;
     // First report
-    EXPECT_CALL(client_,
-                UploadChromeDesktopReportProxy(
-                    Property(&em::ChromeDesktopReportRequest::os_user_name,
-                             Eq(kOsUserNames[0])),
-                    _))
+    EXPECT_CALL(
+        client_,
+        UploadReportProxy(Property(&ReportUploader::Request::browser_report,
+                                   Property(&em::BrowserReport::browser_version,
+                                            Eq(kBrowserVersionNames[0]))),
+                          _))
         .Times(3)
         .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)))
         .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)))
         .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(true)));
     // Second report
-    EXPECT_CALL(client_,
-                UploadChromeDesktopReportProxy(
-                    Property(&em::ChromeDesktopReportRequest::os_user_name,
-                             Eq(kOsUserNames[1])),
-                    _))
+    EXPECT_CALL(
+        client_,
+        UploadReportProxy(Property(&ReportUploader::Request::browser_report,
+                                   Property(&em::BrowserReport::browser_version,
+                                            Eq(kBrowserVersionNames[1]))),
+                          _))
         .Times(2)
         .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)))
         .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)));
@@ -266,7 +280,7 @@ TEST_F(ReportUploaderTest, MultipleReports) {
 
 // Verified three DM server error that is transient.
 TEST_P(ReportUploaderTestWithTransientError, WithoutRetry) {
-  EXPECT_CALL(client_, UploadChromeDesktopReportProxy(_, _))
+  EXPECT_CALL(client_, UploadReportProxy(_, _))
       .WillOnce(WithArgs<1>(policy::ScheduleStatusCallback(false)));
   client_.SetStatus(GetParam());
   UploadReportAndSetExpectation(/*number_of_request=*/2,
