a55312b4fb46708ac24628f5d9417c66e70f19c1
Gauthier Ambard <gambard@chromium.org>
2019-11-13 14:50:35 +0000

    [iOS] Swizzled method has to exist in ScopedMethodSwizzler
    This CL adds a DCHECK to make sure that the method to be swizzled in
    ScopedMethodSwizzler exists.
    Previously the method was added.
    
    Bug: none
    Change-Id: I9d6a9d935f7cf15cb644df1e70c44f8188f3bdf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913252
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714917}

--- ios/testing/scoped_method_swizzler.mm
+++ ios/testing/scoped_method_swizzler.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/testing/scoped_method_swizzler.h"
 
+#include "base/logging.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -13,17 +15,9 @@ ScopedMethodSwizzler::ScopedMethodSwizzler(Class klass,
                                            SEL selector_to_replace,
                                            SEL replacing_selector) {
   original_method_ = class_getInstanceMethod(klass, selector_to_replace);
+  DCHECK(original_method_);
   replacing_method_ = class_getInstanceMethod(klass, replacing_selector);
-  BOOL method_added = class_addMethod(
-      klass, selector_to_replace, method_getImplementation(replacing_method_),
-      method_getTypeEncoding(replacing_method_));
-  if (method_added) {
-    class_replaceMethod(klass, replacing_selector,
-                        method_getImplementation(original_method_),
-                        method_getTypeEncoding(original_method_));
-  } else {
-    method_exchangeImplementations(original_method_, replacing_method_);
-  }
+  method_exchangeImplementations(original_method_, replacing_method_);
 }
 
 ScopedMethodSwizzler::~ScopedMethodSwizzler() {
