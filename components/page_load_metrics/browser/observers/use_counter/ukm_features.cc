e6f3e1fac735dba2bae1b9445690eaaca9b1e88e
Reilly Grant <reillyg@chromium.org>
2019-11-13 23:13:00 +0000

    [bluetooth] Switch from RAPPOR to UKM for Web Bluetooth metrics
    This collects effectively the same data but uses the more modern
    infrastructure.
    
    Bug: 1017844
    Change-Id: Ibafe3991d5129faf0481bef7acb8e7cd157cae12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1895946
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715094}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -145,6 +145,8 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kMovedOrResizedPopup2sAfterCreation,
           WebFeature::kDOMWindowOpenPositioningFeatures,
           WebFeature::kCSSSelectorInternalMediaControlsOverlayCastButton,
+          WebFeature::kWebBluetoothRequestDevice,
+          WebFeature::kWebBluetoothRequestScan,
       }));
   return *opt_in_features;
 }
--- third_party/blink/renderer/modules/bluetooth/bluetooth.cc
+++ third_party/blink/renderer/modules/bluetooth/bluetooth.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 #include "third_party/blink/public/mojom/bluetooth/web_bluetooth.mojom-blink.h"
-#include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -237,9 +236,6 @@ ScriptPromise Bluetooth::requestDevice(ScriptState* script_state,
   if (exception_state.HadException())
     return ScriptPromise();
 
-  // Record the eTLD+1 of the frame using the API.
-  Platform::Current()->RecordRapporURL("Bluetooth.APIUsage.Origin", doc.Url());
-
   // Subsequent steps are handled in the browser process.
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
   ScriptPromise promise = resolver->Promise();
@@ -354,9 +350,6 @@ ScriptPromise Bluetooth::requestLEScan(ScriptState* script_state,
 
   if (exception_state.HadException())
     return ScriptPromise();
-
-  // Record the eTLD+1 of the frame using the API.
-  Platform::Current()->RecordRapporURL("Bluetooth.APIUsage.Origin", doc.Url());
 
   // Subsequent steps are handled in the browser process.
   auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state);
