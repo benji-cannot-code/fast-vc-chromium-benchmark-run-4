6e27639e4ec4f63686fd1322a9a4030ab5b5227d
Rune Lillesveen <futhark@chromium.org>
2019-11-13 07:08:38 +0000

    Don't ClearDistribution() until we update it.
    With FlatTreeStyleRecalc we expect to traverse up the current flat tree
    ancestor chain even when the distribution is marked dirty. Otherwise,
    we will not be able to reach the style dirty nodes marked when we have
    a dirty distribution.
    
    Bug: 1023178
    Change-Id: I2b80ad252bc17bb223a3caa9336526c9e2fb6387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911208
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714855}

--- third_party/blink/renderer/core/dom/shadow_root_v0.cc
+++ third_party/blink/renderer/core/dom/shadow_root_v0.cc
@@ -169,6 +169,8 @@ const DestinationInsertionPoints* ShadowRootV0::DestinationInsertionPointsFor(
 }
 
 void ShadowRootV0::Distribute() {
+  ClearDistribution();
+
   DistributionPool pool(GetShadowRoot().host());
   HTMLShadowElement* shadow_insertion_point = nullptr;
 
