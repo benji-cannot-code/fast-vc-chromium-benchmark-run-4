a74da0f0a5b35b1cec72b8f4afc596f2cf44f2ef
Tim Song <tengs@chromium.org>
2019-11-13 03:26:34 +0000

    Ash Tray: Update status area collapse state when state changes.
    The collapse state of the status area should be updated when the shelf state or
    bounds change or when a tray item is shown or hidden.
    
    Note that the actual calculation for which trays should be hidden or visible is
    unimplemented and will be finished in a future CL.
    
    TEST=unit test
    BUG=982511
    
    Change-Id: Ie4a46be7d03435bf8927062af0c7726f5cc090a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1904165
    Commit-Queue: Tim Song <tengs@chromium.org>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714798}

--- ash/system/status_area_widget_unittest.cc
+++ ash/system/status_area_widget_unittest.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/session/logout_button_tray.h"
 #include "ash/system/status_area_widget_test_api.h"
 #include "ash/system/status_area_widget_test_helper.h"
+#include "ash/system/tray/status_area_overflow_button_tray.h"
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ash/system/unified/unified_system_tray.h"
 #include "ash/system/virtual_keyboard/virtual_keyboard_tray.h"
@@ -366,13 +367,13 @@ class StatusAreaWidgetCollapseStateTest : public AshTestBase {
   void SetUp() override {
     AshTestBase::SetUp();
 
-    StatusAreaWidget* status_area =
-        StatusAreaWidgetTestHelper::GetStatusAreaWidget();
-    virtual_keyboard_ = status_area->virtual_keyboard_tray_for_testing();
-    ime_menu_ = status_area->ime_menu_tray();
-    palette_ = status_area->palette_tray();
-    dictation_button_ = status_area->dictation_button_tray();
-    select_to_speak_ = status_area->select_to_speak_tray();
+    status_area_ = StatusAreaWidgetTestHelper::GetStatusAreaWidget();
+    overflow_button_ = status_area_->overflow_button_tray();
+    virtual_keyboard_ = status_area_->virtual_keyboard_tray_for_testing();
+    ime_menu_ = status_area_->ime_menu_tray();
+    palette_ = status_area_->palette_tray();
+    dictation_button_ = status_area_->dictation_button_tray();
+    select_to_speak_ = status_area_->select_to_speak_tray();
 
     virtual_keyboard_->SetVisiblePreferred(true);
     ime_menu_->SetVisiblePreferred(true);
@@ -382,8 +383,7 @@ class StatusAreaWidgetCollapseStateTest : public AshTestBase {
   }
 
   void SetCollapseState(StatusAreaWidget::CollapseState collapse_state) {
-    StatusAreaWidgetTestApi test_api(
-        StatusAreaWidgetTestHelper::GetStatusAreaWidget());
+    StatusAreaWidgetTestApi test_api(status_area_);
     test_api.SetCollapseState(collapse_state);
 
     virtual_keyboard_->UpdateAfterStatusAreaCollapseChange();
@@ -393,6 +393,8 @@ class StatusAreaWidgetCollapseStateTest : public AshTestBase {
     select_to_speak_->UpdateAfterStatusAreaCollapseChange();
   }
 
+  StatusAreaWidget* status_area_;
+  TrayBackgroundView* overflow_button_;
   TrayBackgroundView* virtual_keyboard_;
   TrayBackgroundView* ime_menu_;
   TrayBackgroundView* palette_;
@@ -443,6 +445,14 @@ TEST_F(StatusAreaWidgetCollapseStateTest, ImeMenuShownWithVirtualKeyboard) {
   EXPECT_FALSE(virtual_keyboard_->GetVisible());
   EXPECT_FALSE(dictation_button_->GetVisible());
   EXPECT_FALSE(select_to_speak_->GetVisible());
+}
+
+TEST_F(StatusAreaWidgetCollapseStateTest, OverflowButtonShownWhenCollapsible) {
+  EXPECT_FALSE(overflow_button_->GetVisible());
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kAshForceStatusAreaCollapsible);
+  status_area_->UpdateCollapseState();
+  EXPECT_TRUE(overflow_button_->GetVisible());
 }
 
 }  // namespace ash
