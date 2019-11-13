4ed0225ee77e693a19e7c362efec91ddeebc6546
Eric Seckler <eseckler@chromium.org>
2019-11-13 12:14:36 +0000

    Reland "perfetto: Use TraceProcessor for proto2json conversion"
    This is a reland of 0d9298617f10dac1ff7b87e7d4e3491b629fc3af.
    
    Reland after fixes for crbug.com/1022770, crbug.com/1022386, and
    crbug.com/1022818 landed.
    
    Original change's description:
    > perfetto: Use TraceProcessor for proto2json conversion
    >
    > TraceProcessor replaces the existing (unmaintainable) proto2json
    > converter and adds features like combining of BEGIN/END events
    > and support for heap profiles.
    >
    > Also updates a devtools test that was expecting separate begin/end
    > events where TraceProcessor now combines them into a complete event.
    >
    > We have reduced the trace procesor's binary size overhead
    > by selectively excluding features Chrome doesn't need from >400kB to
    > ~50kB. We could probably save a couple more kB if we invest even more
    > eng time and accept the maintenance burden for this in trace processor,
    > however, we feel that we've reached an acceptable state that balances
    > binary size & code health.
    >
    > Binary-Size: see paragraph above.
    > Bug: 1006770
    > Change-Id: Ia7f6b3a784c1a9c40574e1dcd80eca1ca2dfb03f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1746258
    > Commit-Queue: Eric Seckler <eseckler@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: oysteine <oysteine@chromium.org>
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#713593}
    
    Binary-Size: see paragraph above.
    Bug: 1006770, 1022770, 1022386, 1022818
    Change-Id: I7297721eb67834905262081282936013b34a0184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913602
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714887}

--- services/tracing/perfetto/consumer_host.h
+++ services/tracing/perfetto/consumer_host.h
@@ -24,17 +24,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/perfetto/include/perfetto/ext/tracing/core/consumer.h"
 #include "third_party/perfetto/include/perfetto/ext/tracing/core/tracing_service.h"
 
-namespace base {
-class DictionaryValue;
-}
-
 namespace service_manager {
 struct BindSourceInfo;
 }  // namespace service_manager
 
+namespace perfetto {
+namespace trace_processor {
+class TraceProcessorStorage;
+}  // namespace trace_processor
+}  // namespace perfetto
+
 namespace tracing {
 
-class JSONTraceExporter;
 class PerfettoService;
 
 // This is a Mojo interface which enables any client
@@ -90,9 +91,8 @@ class ConsumerHost : public perfetto::Consumer, public mojom::ConsumerHost {
         DisableTracingAndEmitJsonCallback callback) override;
 
    private:
-    void OnJSONTraceData(std::string* json,
-                         base::DictionaryValue* metadata,
-                         bool has_more);
+    void ExportJson();
+    void OnJSONTraceData(std::string json, bool has_more);
     void OnEnableTracingTimeout();
     void MaybeSendEnableTracingAck();
     bool IsExpectedPid(base::ProcessId pid) const;
@@ -103,7 +103,9 @@ class ConsumerHost : public perfetto::Consumer, public mojom::ConsumerHost {
     bool privacy_filtering_enabled_ = false;
     base::SequenceBound<StreamWriter> read_buffers_stream_writer_;
     RequestBufferUsageCallback request_buffer_usage_callback_;
-    std::unique_ptr<JSONTraceExporter> json_trace_exporter_;
+    std::unique_ptr<perfetto::trace_processor::TraceProcessorStorage>
+        trace_processor_;
+    std::string json_agent_label_filter_;
     base::OnceCallback<void(bool)> flush_callback_;
     const mojom::TracingClientPriority tracing_priority_;
     base::OnceClosure on_disabled_callback_;
