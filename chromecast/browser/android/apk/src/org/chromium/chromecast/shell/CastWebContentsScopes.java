1148d0a6251e6d16cff5401e49e71574cbf0d452
Zhiheng Li <vincentli@google.com>
2019-11-13 05:31:23 +0000

    Add method in CastWebContentsView to return ContentView and ContentViewRendererView.
    Bug: internal b/143071435
    Test: tested on AndroidThings device
    Change-Id: I4b93f9ed7c39dbfa1e669c285515234f2acc241d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902075
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Zhiheng(Vincent) Li <vincentli@google.com>
    Auto-Submit: Zhiheng(Vincent) Li <vincentli@google.com>
    Cr-Commit-Position: refs/heads/master@{#714835}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsScopes.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastWebContentsScopes.java
@@ -27,6 +27,8 @@ class CastWebContentsScopes {
         IBinder provideWindowToken();
     }
 
+    static final String VIEW_TAG_CONTENT_VIEW = "ContentView";
+
     public static Observer<WebContents> onLayoutActivity(
             Activity activity, FrameLayout layout, @ColorInt int backgroundColor) {
         layout.setBackgroundColor(backgroundColor);
@@ -78,6 +80,7 @@ class CastWebContentsScopes {
             layout.addView(contentView, matchParent);
             contentView.setFocusable(true);
             contentView.requestFocus();
+            contentView.setTag(VIEW_TAG_CONTENT_VIEW);
             contentViewRenderView.setCurrentWebContents(webContents);
             return () -> {
                 layout.setForeground(new ColorDrawable(backgroundColor));
