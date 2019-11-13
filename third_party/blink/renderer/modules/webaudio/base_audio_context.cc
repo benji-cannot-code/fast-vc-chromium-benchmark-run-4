b3d7b62e7be2e0547d5018ea4b40a15d48aac0d2
Hongchan Choi <hongchan@chromium.org>
2019-11-13 19:44:32 +0000

    Check if ExecutionContext is valid in BaseAudioContext::PerformCleanupOnMainThread
    When the tear-down process of BaseAudioContext occurs rapidly along with
    the destruction of an associated ExecutionContext, a scheduled task from
    the audio rendering thread can attempt to access `this` pointer of
    BaseAudioContext.
    
    This CL add a check to verify the validity of an ExecutionContext before
    the function accesses the `this` pointer for the audio graph lock.
    
    Test: Ran the repro case locally with this patch for 20 minutes and no UAP occurred.
    Bug: 1023810
    Change-Id: I8e5b960b50aca5b0dcee52b5886804af2515806e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913872
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715021}

--- third_party/blink/renderer/modules/webaudio/base_audio_context.cc
+++ third_party/blink/renderer/modules/webaudio/base_audio_context.cc
@@ -717,6 +717,11 @@ void BaseAudioContext::HandleStoppableSourceNodes() {
 
 void BaseAudioContext::PerformCleanupOnMainThread() {
   DCHECK(IsMainThread());
+
+  // When a posted task is performed, the execution context might be gone.
+  if (!GetExecutionContext())
+    return;
+
   GraphAutoLocker locker(this);
 
   if (is_resolving_resume_promises_) {
@@ -738,6 +743,8 @@ void BaseAudioContext::PerformCleanupOnMainThread() {
 }
 
 void BaseAudioContext::ScheduleMainThreadCleanup() {
+  DCHECK(IsAudioThread());
+
   if (has_posted_cleanup_task_)
     return;
   PostCrossThreadTask(
