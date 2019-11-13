752d071bf6ea1acbf7a1dd4b05ecd92171d33be4
Morten Stenshorne <mstensho@chromium.org>
2019-11-13 19:54:32 +0000

    [LayoutNG] Fix and re-enable 3 float fragmentation unit tests.
    Float fragmentation currently doesn't work if the float is "inside" a
    line. But three existing (but disabled) tests actually had no lines, so
    they already behaved correctly. The expectation, however, was that we
    created a dummy line beside the floats (which we did, back in the days),
    but this is no longer the case, so I just removed that.
    
    Bug: 915929
    Change-Id: I6b5d04c044860d1225b155fc9c7e70512602b398
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913254
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715023}

--- third_party/blink/renderer/core/layout/ng/ng_column_layout_algorithm_test.cc
+++ third_party/blink/renderer/core/layout/ng/ng_column_layout_algorithm_test.cc
@@ -563,8 +563,7 @@ TEST_F(NGColumnLayoutAlgorithmTest, UnusedSpaceInBlock) {
   EXPECT_EQ(expectation, dump);
 }
 
-// TODO(crbug.com/915929): Fix inline-level float fragmentation.
-TEST_F(NGColumnLayoutAlgorithmTest, DISABLED_FloatInOneColumn) {
+TEST_F(NGColumnLayoutAlgorithmTest, FloatInOneColumn) {
   SetBodyInnerHTML(R"HTML(
     <style>
       #parent {
@@ -588,13 +587,11 @@ TEST_F(NGColumnLayoutAlgorithmTest, DISABLED_FloatInOneColumn) {
     offset:0,0 size:320x100
       offset:0,0 size:100x100
         offset:0,0 size:75x100
-        offset:75,0 size:0x0
 )DUMP";
   EXPECT_EQ(expectation, dump);
 }
 
-// TODO(crbug.com/915929): Fix inline-level float fragmentation.
-TEST_F(NGColumnLayoutAlgorithmTest, DISABLED_TwoFloatsInOneColumn) {
+TEST_F(NGColumnLayoutAlgorithmTest, TwoFloatsInOneColumn) {
   SetBodyInnerHTML(R"HTML(
     <style>
       #parent {
@@ -620,13 +617,11 @@ TEST_F(NGColumnLayoutAlgorithmTest, DISABLED_TwoFloatsInOneColumn) {
       offset:0,0 size:100x100
         offset:0,0 size:15x100
         offset:84,0 size:16x100
-        offset:15,0 size:0x0
 )DUMP";
   EXPECT_EQ(expectation, dump);
 }
 
-// TODO(crbug.com/915929): Fix inline-level float fragmentation.
-TEST_F(NGColumnLayoutAlgorithmTest, DISABLED_TwoFloatsInTwoColumns) {
+TEST_F(NGColumnLayoutAlgorithmTest, TwoFloatsInTwoColumns) {
   SetBodyInnerHTML(R"HTML(
     <style>
       #parent {
@@ -652,7 +647,6 @@ TEST_F(NGColumnLayoutAlgorithmTest, DISABLED_TwoFloatsInTwoColumns) {
       offset:0,0 size:100x100
         offset:0,0 size:15x100
         offset:84,0 size:16x100
-        offset:15,0 size:0x0
       offset:110,0 size:100x50
         offset:0,0 size:15x50
         offset:84,0 size:16x50
