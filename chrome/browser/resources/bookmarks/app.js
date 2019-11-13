3f36cd46e119e8e4580bc3c44e82bc4d651b2cc0
rbpotter <rbpotter@chromium.org>
2019-11-13 03:56:43 +0000

    Web UI: Add cr-splitter and use to replace cr.ui.splitter in Bookmarks
    Add a new cr-splitter Polymer element and use this to replace
    cr.ui.Splitter in the bookmarks page. cr.ui.splitter depends on
    cr.ui.define(), which will not be migrated to Polymer 3.
    
    Also generate Polymer 3 versions of the new cr-splitter and its tests.
    
    Fixed: 1023002
    Change-Id: Ie8360353c1e67fa5fc8d2fcf3d09002621d71ba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910701
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714811}

--- chrome/browser/resources/bookmarks/app.js
+++ chrome/browser/resources/bookmarks/app.js
@@ -91,7 +91,6 @@ Polymer({
    */
   initializeSplitter_: function() {
     const splitter = this.$.splitter;
-    cr.ui.Splitter.decorate(splitter);
     const splitterTarget = this.$.sidebar;
 
     // The splitter persists the size of the left component in the local store.
