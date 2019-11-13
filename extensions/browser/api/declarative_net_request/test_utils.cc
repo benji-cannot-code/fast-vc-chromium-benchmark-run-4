786cbb07a3effa1cd8a663815fe4e81b583c2171
Karan Bhatia <karandeepb@chromium.org>
2019-11-13 23:56:03 +0000

    DNR tests: Return false in CreateVerifiedMatcher on seeing install warnings.
    This helps debugging in tests when some rules fail to index correctly.
    
    BUG=696822
    
    Change-Id: I38be0f707e7de7b5a4be49aacb037b7d9bd42fbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911279
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715110}

--- extensions/browser/api/declarative_net_request/test_utils.cc
+++ extensions/browser/api/declarative_net_request/test_utils.cc
@@ -130,6 +130,9 @@ bool CreateVerifiedMatcher(const std::vector<TestRule>& rules,
     return false;
   }
 
+  if (!result.warnings.empty())
+    return false;
+
   if (expected_checksum)
     *expected_checksum = result.ruleset_checksum;
 
