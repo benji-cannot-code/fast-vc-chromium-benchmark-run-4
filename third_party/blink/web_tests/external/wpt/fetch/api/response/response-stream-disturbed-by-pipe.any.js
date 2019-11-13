3bffa2e07f0930058fd799d44fd6d820262544aa
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-11-13 06:29:08 +0000

    Import wpt@760485a4f652813d1f195ee28042c391cf2fdc02
    Using wpt-import in Chromium 79aabb068de6c71de0e88f9415d62a7b536f1fba.
    With Chromium commits locally applied on WPT:
    1d5b803fd3 "Add WebVTT support for inline styling - Web Platform Tests"
    88d98fda9d "Add more scroll to text WPTs"
    faa1edfe15 "update WPT tests to eliminate CSSPseudoElement from animations"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: Ic6db50c83c546242273ed94f8198066f6ebb6304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913112
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#714851}

---
+++ third_party/blink/web_tests/external/wpt/fetch/api/response/response-stream-disturbed-by-pipe.any.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+test(() => {
+  const r = new Response(new ReadableStream());
+  // highWaterMark: 0 means that nothing will actually be read from the body.
+  r.body.pipeTo(new WritableStream({}, {highWaterMark: 0}));
+  assert_true(r.bodyUsed, 'bodyUsed should be true');
+}, 'using pipeTo on Response body should disturb it synchronously');
+
+test(() => {
+  const r = new Response(new ReadableStream());
+  r.body.pipeThrough({
+    writable: new WritableStream({}, {highWaterMark: 0}),
+    readable: new ReadableStream()
+  });
+  assert_true(r.bodyUsed, 'bodyUsed should be true');
+}, 'using pipeThrough on Response body should disturb it synchronously');
