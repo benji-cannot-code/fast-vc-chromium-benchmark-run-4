40c97c1b94f94d8168b42b69501b21db2521f956
Joel Hockey <joelhockey@chromium.org>
2019-11-13 03:26:31 +0000

    Use dynamic CroshExtensionURL for crostini terminal
    Currently we hardcode crostini terminal to use the builtin crosh
    location, whereas crosh can dynamically allow nassh and nassh-dev
    apps to be used instead.
    
    With some expected changes coming in crrev.com/c/1905039 to
    have crosh use chrome://terminal, and other changes to gzip
    content, it is useful to have terminal use the same URL that
    is used for crosh.
    
    Bug: 1023866
    Change-Id: I7784b42171c73592acdd99d20ade6367c2afa3de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913283
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#714797}

--- chrome/browser/chromeos/crostini/crostini_terminal.cc
+++ chrome/browser/chromeos/crostini/crostini_terminal.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/crostini/crostini_terminal.h"
 
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
+#include "chrome/browser/extensions/api/terminal/terminal_extension_helper.h"
 #include "chrome/browser/ui/ash/window_properties.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -19,9 +20,10 @@ GURL GenerateVshInCroshUrl(Profile* profile,
                            const std::string& vm_name,
                            const std::string& container_name,
                            const std::vector<std::string>& terminal_args) {
-  std::string vsh_crosh = base::StringPrintf(
-      "chrome-extension://%s/html/crosh.html?command=vmshell",
-      kCrostiniCroshBuiltinAppId);
+  std::string vsh_crosh =
+      extensions::TerminalExtensionHelper::GetCroshExtensionURL(profile)
+          .spec() +
+      "?command=vmshell";
   std::string vm_name_param = net::EscapeQueryParamValue(
       base::StringPrintf("--vm_name=%s", vm_name.c_str()), false);
   std::string container_name_param = net::EscapeQueryParamValue(
