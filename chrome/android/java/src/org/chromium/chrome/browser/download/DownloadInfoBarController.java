2ea7c6a8c9402a7871b594de11ad594200ca56b9
Min Qin <qinmin@chromium.org>
2019-11-13 21:53:08 +0000

    Move DownloadManagerBridge to chrome/browser/download
    Part of the modulization effort.
    
    Bug: 1013759
    Change-Id: I3a250fb293925ebe3c98fc068003b32568112ff9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1902072
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715066}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadInfoBarController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadInfoBarController.java
@@ -392,7 +392,7 @@ public class DownloadInfoBarController implements OfflineContentProvider.Observe
             return false;
         }
 
-        if (DownloadUtils.shouldAutoOpenDownload(offlineItem.mimeType, true)) {
+        if (MimeUtils.canAutoOpenMimeType(offlineItem.mimeType)) {
             return false;
         }
 
