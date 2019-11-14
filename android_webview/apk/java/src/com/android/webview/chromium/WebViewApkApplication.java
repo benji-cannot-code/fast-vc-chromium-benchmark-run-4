2e3c1bd5696ddef875f744c59ae6fba3cdf209a2
Robbie McElrath <rmcelrath@chromium.org>
2019-11-14 01:12:13 +0000

    [AW] Rename //aw/apk to //aw/nonembedded
    WebView's apk subfolder is used to store all code that runs under
    WebView's UID rather than as part of a library loaded by other apps.
    'nonembedded' more clearly and accurately reflects this than 'apk'.
    
    In order not break clank, this CL temporarily creates a copy of
    AndroidManifest.xml that will be deleted as soon as downstream is
    updated to use the new path.
    
    TBR=fgorski@chromium.org
    
    Test: ninja -C out/aw
    Bug: 934152
    Change-Id: Icdbe95634701e7e6f31a3e3865ca80d9cbad47c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900130
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715141}

--- android_webview/apk/java/src/com/android/webview/chromium/WebViewApkApplication.java
+++ android_webview/apk/java/src/com/android/webview/chromium/WebViewApkApplication.java
@@ -6,4 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package com.android.webview.chromium;
 
 // TODO(crbug.com/934152): Delete this once Clank points to the new WebViewApkApplication package.
-public class WebViewApkApplication extends org.chromium.android_webview.app.WebViewApkApplication {}
+public class WebViewApkApplication
+        extends org.chromium.android_webview.nonembedded.WebViewApkApplication {}
