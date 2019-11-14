c34aa7703018533ef7d234cd1a211b24d375fa9e
Trent Apted <tapted@chromium.org>
2019-11-14 11:32:29 +0000

    Decouple media_app_resources grd from chromeos_resources.
    This is a better fit for layering/encapsulation/ownership. The
    tradeoff is some "slippage" in resource_ids.
    
    Bug: 996088
    Change-Id: I6b5f03460c01e979da018dec757f88452a42adbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913284
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715232}

--- chromeos/components/media_app_ui/media_app_guest_ui.cc
+++ chromeos/components/media_app_ui/media_app_guest_ui.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/media_app_ui/url_constants.h"
 #include "chromeos/grit/chromeos_media_app_bundle_resources.h"
 #include "chromeos/grit/chromeos_media_app_bundle_resources_map.h"
-#include "chromeos/grit/chromeos_resources.h"
+#include "chromeos/grit/chromeos_media_app_resources.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
