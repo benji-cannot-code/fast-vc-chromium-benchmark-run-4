d5f86ddb9970ae75951e5a0e77209a55022f4b96
Ilia Samsonov <isamsonov@google.com>
2019-11-14 21:46:52 +0000

    Move manual test filter back to launcher.
    Skipping manual test in test harness causing tests
    to fail due to timeout in some windows builders.
    
    Win7 Tests (dbg)(1) constantly fails on MANUAL_ tests:
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%28dbg%29%281%29
    
    other builders fail or flake less frequently:
    
    Win10 Tests x64
    Win7 (32) Tests
    win10_chromium_x64_rel_ng
    
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyfQsSBUZsYWtlInJjaHJvbWl1bUBicm93c2VyX3Rlc3RzQFdlYlJ0Y0ludGVybmFsc1BlcmZCcm93c2VyVGVzdC5NQU5VQUxfUnVuc09uZVdheUNhbGw2MFNlY3NBbmRMb2dzSW50ZXJuYWxNZXRyaWNzV2l0aE9wdXNEdHgM
    
    Skipping these tests in launcher will resolve flakiness on these builder.
    
    Bug: 1020591
    Change-Id: Ie2ea8510ab31a3c56b783f0e8b9bded98c21d9bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1909958
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715434}

--- content/public/test/test_launcher.cc
+++ content/public/test/test_launcher.cc
@@ -71,6 +71,8 @@ namespace {
 // that span browser restarts.
 const char kPreTestPrefix[] = "PRE_";
 
+const char kManualTestPrefix[] = "MANUAL_";
+
 TestLauncherDelegate* g_launcher_delegate = nullptr;
 #if !defined(OS_ANDROID)
 // ContentMain is not run on Android in the test process, and is run via
@@ -124,7 +126,10 @@ class WrapperTestLauncherDelegate : public base::TestLauncherDelegate {
  public:
   explicit WrapperTestLauncherDelegate(
       content::TestLauncherDelegate* launcher_delegate)
-      : launcher_delegate_(launcher_delegate) {}
+      : launcher_delegate_(launcher_delegate) {
+    run_manual_tests_ = base::CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kRunManualTestsFlag);
+  }
 
   // base::TestLauncherDelegate:
   bool GetTests(std::vector<base::TestIdentifier>* output) override;
@@ -141,6 +146,8 @@ class WrapperTestLauncherDelegate : public base::TestLauncherDelegate {
 
   base::TimeDelta GetTimeout() override;
 
+  bool ShouldRunTest(const base::TestIdentifier& test) override;
+
  private:
   // Relays timeout notification from the TestLauncher (by way of a
   // ProcessLifetimeObserver) to the caller's content::TestLauncherDelegate.
@@ -152,7 +159,7 @@ class WrapperTestLauncherDelegate : public base::TestLauncherDelegate {
 
   content::TestLauncherDelegate* launcher_delegate_;
 
-
+  bool run_manual_tests_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(WrapperTestLauncherDelegate);
 };
@@ -240,6 +247,14 @@ void WrapperTestLauncherDelegate::ProcessTestResults(
   test_results.front().elapsed_time = elapsed_time;
 
   launcher_delegate_->PostRunTest(&test_results.front());
+}
+// TODO(isamsonov): crbug.com/1004417 remove when windows builders
+// stop flaking on MANAUAL_ tests.
+bool WrapperTestLauncherDelegate::ShouldRunTest(
+    const base::TestIdentifier& test) {
+  return run_manual_tests_ ||
+         !base::StartsWith(test.test_name, kManualTestPrefix,
+                           base::CompareCase::SENSITIVE);
 }
 
 }  // namespace
