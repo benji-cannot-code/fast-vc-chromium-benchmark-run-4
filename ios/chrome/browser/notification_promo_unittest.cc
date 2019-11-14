285cdcbfaf8e229a1adee5ffb8e383552fa80f6a
Mikel Astiz <mastiz@chromium.org>
2019-11-14 18:38:35 +0000

    Do not instantiate base::FieldTrialList explicitly in tests
    The test suite already instantiates a FieldTrialList as per
    https://chromium-review.googlesource.com/c/chromium/src/+/1883567 so
    it's no longer necessary to do so explicitly in tests.
    
    This patch addresses unit tests under:
    /ios/chrome
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 1018667
    Change-Id: I3ce32f0feaa2d652f5f4b8c68ae93e337e5e16be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1916824
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715343}

--- ios/chrome/browser/notification_promo_unittest.cc
+++ ios/chrome/browser/notification_promo_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/mock_entropy_provider.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "components/prefs/testing_pref_service.h"
@@ -52,8 +51,6 @@ class NotificationPromoTest : public PlatformTest {
  public:
   NotificationPromoTest()
       : notification_promo_(&local_state_),
-        field_trial_list_(std::make_unique<base::FieldTrialList>(
-            std::make_unique<base::MockEntropyProvider>())),
         received_notification_(false),
         start_(0.0),
         end_(0.0),
@@ -294,7 +291,6 @@ class NotificationPromoTest : public PlatformTest {
 
  private:
   NotificationPromo notification_promo_;
-  std::unique_ptr<base::FieldTrialList> field_trial_list_;
   bool received_notification_;
   base::Value test_json_;
 
