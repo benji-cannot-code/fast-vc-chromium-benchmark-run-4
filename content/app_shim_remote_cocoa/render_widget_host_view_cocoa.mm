de13cc7803c834c32467092554c15b040cde4eff
Sidney San Martín <sdy@chromium.org>
2019-11-14 20:56:51 +0000

    Make autocorrect=off and spellcheck=false disable Touch Bar typing suggestions.
    Bug: 1024914
    Change-Id: I3ff68438b028fe38ad7270d7f90772b595f4f46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1917603
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Sidney San Martín <sdy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715401}

--- content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
+++ content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
@@ -2204,7 +2204,9 @@ extern NSString* NSTextInputReplacementRangeAttributeName;
 }
 
 - (NSTouchBar*)makeTouchBar {
-  if (textInputType_ != ui::TEXT_INPUT_TYPE_NONE) {
+  if (textInputType_ != ui::TEXT_INPUT_TYPE_NONE &&
+      !(textInputFlags_ & blink::kWebTextInputFlagAutocorrectOff) &&
+      !(textInputFlags_ & blink::kWebTextInputFlagSpellcheckOff)) {
     candidateListTouchBarItem_.reset([[NSCandidateListTouchBarItem alloc]
         initWithIdentifier:NSTouchBarItemIdentifierCandidateList]);
     auto* candidateListItem = candidateListTouchBarItem_.get();
