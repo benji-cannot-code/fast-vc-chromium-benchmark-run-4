b11f6f5e847ffedfa3c6b73bc2b6f1b168759b38
Ken Rockot <rockot@google.com>
2019-11-14 01:29:23 +0000

    Record service interface names in crash key
    Services ported to the newer service model have not been logging their
    service name in the "service-name" crash key used by other services.
    That can make utility process crashes difficult to pinpoint on a
    specific service.
    
    This ensures that all services stash their name in the "service-name"
    crash key.
    
    Note that unlike in the old model, there is no concept of "service name"
    beyond the name of a service's primordial Mojo interface; so that's
    what's logged in these cases.
    
    Bug: 1024560
    Fixed: 1024601
    Change-Id: I98f8887925e2e0012b6f281db5df31ccd97cb40b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1912838
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#715147}

--- content/utility/utility_thread_impl.cc
+++ content/utility/utility_thread_impl.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
 #include "base/containers/unique_ptr_adapters.h"
+#include "base/debug/crash_logging.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/optional.h"
 #include "base/sequenced_task_runner.h"
@@ -47,6 +48,13 @@ class ServiceBinderImpl {
   ~ServiceBinderImpl() = default;
 
   void BindServiceInterface(mojo::GenericPendingReceiver* receiver) {
+    // Set a crash key so utility process crash reports indicate which service
+    // was running in the process.
+    static auto* service_name_crash_key = base::debug::AllocateCrashKeyString(
+        "service-name", base::debug::CrashKeySize::Size32);
+    base::debug::SetCrashKeyString(service_name_crash_key,
+                                   receiver->interface_name().value());
+
     // We watch for and terminate on PEER_CLOSED, but we also terminate if the
     // watcher is cancelled (meaning the local endpoint was closed rather than
     // the peer). Hence any breakage of the service pipe leads to termination.
