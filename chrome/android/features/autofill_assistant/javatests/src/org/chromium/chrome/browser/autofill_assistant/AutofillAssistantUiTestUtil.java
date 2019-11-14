fa1e303d4859667dd977168db8de88293fdf49b6
Matt Jones <mdjones@chromium.org>
2019-11-14 22:35:45 +0000

    Move FullscreenManager interactions to glue code for BottomSheet
    This patch also removes some obsolete logic from the days of Chrome
    Home like whether the android version of the toolbar is showing.
    
    Bug: 986310, 1002277
    Change-Id: Ib939d02cdf783cc0fde30dbf7089171ae52c92a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1909930
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715457}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiTestUtil.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantUiTestUtil.java
@@ -216,8 +216,7 @@ class AutofillAssistantUiTestUtil {
             ViewGroup coordinator = activity.findViewById(R.id.coordinator);
             LayoutInflater.from(activity).inflate(R.layout.bottom_sheet, coordinator);
             BottomSheet bottomSheet = coordinator.findViewById(R.id.bottom_sheet);
-            bottomSheet.init(coordinator, activity.getActivityTabProvider(),
-                    activity.getFullscreenManager(), activity.getWindow(),
+            bottomSheet.init(coordinator, activity.getActivityTabProvider(), activity.getWindow(),
                     activity.getWindowAndroid().getKeyboardDelegate());
 
             return bottomSheet;
@@ -228,7 +227,8 @@ class AutofillAssistantUiTestUtil {
                 ()
                         -> activity.getCompositorViewHolder()
                                    .getLayoutManager()
-                                   .getOverlayPanelManager());
+                                   .getOverlayPanelManager(),
+                activity.getFullscreenManager());
     }
 
     /**
