a1ddef518bb5320cd8a91e9f4d3850e497a9aa18
Bailey Berro <baileyberro@chromium.org>
2019-11-14 21:01:48 +0000

    Remove unnecessary stop on Settings networking subpages
    All networking sub-pages have an unnecessary navigation stop on
    the enabled-state of the feature. This is particularly bad on the wi-fi
    sub page because the enabled-state text and the toggle are separated by
    the add network button.
    
    Fixed: 1024597, 1007058
    Change-Id: I12d6233090dc3c5ff1b37526be9bf67211f78886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1914827
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715404}

--- chrome/browser/resources/settings/bluetooth_page/bluetooth_subpage.js
+++ chrome/browser/resources/settings/bluetooth_page/bluetooth_subpage.js
@@ -356,6 +356,9 @@ Polymer({
    * @private
    */
   getOnOffString_: function(enabled, onstr, offstr) {
+    // If these strings are changed to convey more information other than "On"
+    // and "Off" in the future, revisit the a11y implementation to ensure no
+    // meaningful information is skipped.
     return enabled ? onstr : offstr;
   },
 
