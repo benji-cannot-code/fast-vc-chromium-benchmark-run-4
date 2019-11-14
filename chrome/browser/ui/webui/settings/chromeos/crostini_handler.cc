d2694648e320e5a0eb4cb538787cc53bf389350f
Victor Hsieh <victorhsieh@chromium.org>
2019-11-14 19:46:14 +0000

    Forbid a managed, primary user to enable adb sideloading
    Until the device and user policies are clearly defined, force to disable
    a managed user to enable adb sideloading, even if on an unmanaged device
    (and as the primary user).
    
    TEST=powerwash and initialize with a managed account, then try to enable
         adb sideloading
         => see policy indicator with disabled toggle
    TEST=bypass the UI to force enable
         => fail to enable
    BUG=chromium:893332
    
    Change-Id: I26b16046ef9210b878ab234b122b962bf090cf1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913071
    Commit-Queue: Victor Hsieh <victorhsieh@chromium.org>
    Auto-Submit: Victor Hsieh <victorhsieh@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715359}

--- chrome/browser/ui/webui/settings/chromeos/crostini_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/crostini_handler.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
+#include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
@@ -348,7 +349,12 @@ bool CrostiniHandler::CheckEligibilityToChangeArcAdbSideloading() const {
   }
 
   if (user_manager::UserManager::Get()->IsLoggedInAsChildUser()) {
-    DVLOG(1) << "Child account is currently unsupported";
+    DVLOG(1) << "adb sideloading is currently unsupported for child account";
+    return false;
+  }
+
+  if (profile_->GetProfilePolicyConnector()->IsManaged()) {
+    DVLOG(1) << "adb sideloading is currently unsupported for managed user";
     return false;
   }
 
