345647c932f3b3a8b447c46b359d6a7c63d3b0d2
Elly Fong-Jones <ellyjones@chromium.org>
2019-11-14 17:37:06 +0000

    flags: run expiration process for M80
    This change:
    1) Marks flags that expired in M80 or earlier as expired, as we are now
       in M80;
    2) Marks flags that expired in M76 or earlier as hard-expired by
       removing the temporary-unexpire-flags-m76 flag and associated
       feature.
    
    TPMs will begin filing cleanup bugs for the remaining M76 and earlier
    flags at this point.
    
    Bug: 953690
    Change-Id: Ibd6922fe1ffc5dbb090e91cdd558a4e60260e345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913866
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715307}

--- chrome/browser/unexpire_flags.h
+++ chrome/browser/unexpire_flags.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace flags {
 
-extern const base::Feature kUnexpireFlagsM76;
 extern const base::Feature kUnexpireFlagsM78;
+extern const base::Feature kUnexpireFlagsM80;
 
 bool IsFlagExpired(const char* internal_name);
 
