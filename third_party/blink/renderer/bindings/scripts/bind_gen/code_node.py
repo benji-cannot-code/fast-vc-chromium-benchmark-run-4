a2d7017288b459586612c2048256697010088698
Hitoshi Yoshida <peria@chromium.org>
2019-11-14 19:56:14 +0000

    CodeNode: Support an initializer list in FunctionDefinitionNode
    Makes FunctionDefinitionNode render an initializer list.
    In case of rendering constructors, we may need an ininitializer list.
    This CL supports it.
    
    
    Bug: 839389
    Change-Id: I65d5d47ac98803ce911d4dedc21d3d1207a40bca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911883
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715366}

--- third_party/blink/renderer/bindings/scripts/bind_gen/code_node.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/code_node.py
@@ -803,6 +803,7 @@ class FunctionDefinitionNode(CodeNode):
                  name,
                  arg_decls,
                  return_type,
+                 member_initializer_list=None,
                  local_vars=None,
                  body=None,
                  comment=None):
@@ -812,6 +813,7 @@ class FunctionDefinitionNode(CodeNode):
                 (i.e. 'namespace_name::' and/or 'type_name::').
             arg_decls: List of argument declaration nodes.
             return_type: Return type node.
+            member_initializer_list: List of initializer nodes.
             local_vars: List of SymbolNodes that can be used in the function
                 body.
             body: Function body node (of type SymbolScopeNode).
@@ -821,6 +823,12 @@ class FunctionDefinitionNode(CodeNode):
         assert isinstance(arg_decls, (list, tuple))
         assert all(isinstance(arg_decl, CodeNode) for arg_decl in arg_decls)
         assert isinstance(return_type, CodeNode)
+        if member_initializer_list is None:
+            member_initializer_list = []
+        assert isinstance(member_initializer_list, (list, tuple))
+        assert all(
+            isinstance(initializer, CodeNode)
+            for initializer in member_initializer_list)
         if local_vars is None:
             local_vars = []
         assert isinstance(local_vars, (list, tuple))
@@ -837,23 +845,36 @@ class FunctionDefinitionNode(CodeNode):
             "name": CodeNode.gensym(),
             "arg_decls": CodeNode.gensym(),
             "return_type": CodeNode.gensym(),
+            "member_initializer_list": CodeNode.gensym(),
             "body": CodeNode.gensym(),
             "comment": CodeNode.gensym(),
         }
 
+        maybe_colon = " : " if member_initializer_list else ""
+
         template_text = CodeNode.format_template(
             """\
 ${{{comment}}}
-${{{return_type}}} ${{{name}}}(${{{arg_decls}}}) {{
+${{{return_type}}} ${{{name}}}(${{{arg_decls}}})\
+{maybe_colon}${{{member_initializer_list}}} {{
   ${{{body}}}
 }}\\
-""", **gensyms)
+""",
+            maybe_colon=maybe_colon,
+            **gensyms)
         template_vars = {
-            gensyms["name"]: name,
-            gensyms["arg_decls"]: SequenceNode(arg_decls, separator=", "),
-            gensyms["return_type"]: return_type,
-            gensyms["body"]: body,
-            gensyms["comment"]: comment,
+            gensyms["name"]:
+            name,
+            gensyms["arg_decls"]:
+            SequenceNode(arg_decls, separator=", "),
+            gensyms["return_type"]:
+            return_type,
+            gensyms["member_initializer_list"]:
+            SequenceNode(member_initializer_list, separator=", "),
+            gensyms["body"]:
+            body,
+            gensyms["comment"]:
+            comment,
         }
 
         CodeNode.__init__(
