2985fa96aaee2603757e7591ae19ed61e8cbdf62
Anastasia Helfinstein <anastasi@google.com>
2019-11-15 20:13:44 +0000

    [Switch Access] Record metrics for menu actions
    Bug: 1018327
    Change-Id: I46aeb21d935b6533207c53743051c54b4d21e968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910877
    Reviewed-by: Aran Gilman <gilmanmh@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#715824}

---
+++ chrome/browser/resources/chromeos/switch_access/metrics.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Class to record metrics for Switch Access.
+ */
+const SwitchAccessMetrics = {
+  /**
+   * @param {string} action
+   */
+  recordMenuAction: (action) => {
+    let metricName = 'Accessibility.CrosSwitchAccess.MenuAction.' +
+        SwitchAccessMetrics.toUpperCamelCase(action);
+    chrome.metricsPrivate.recordUserAction(metricName);
+  },
+
+  /**
+   * @param {string} str
+   * @return {string}
+   */
+  toUpperCamelCase: (str) => {
+    const wordRegex = /(?:^\w|[A-Z]|(?:\b|_)\w)/g;
+    const underscoreAndWhitespaceRegex = /(\s|_)+/g;
+    return str.replace(wordRegex, (word) => word.toUpperCase())
+        .replace(underscoreAndWhitespaceRegex, '');
+  }
+};
