76d4a5843c5b066b9e31076733bb5643241ccedd
Mikel Astiz <mastiz@chromium.org>
2019-11-15 18:03:23 +0000

    Do not instantiate base::FieldTrialList explicitly in tests
    The test suite already instantiates a FieldTrialList as per
    https://chromium-review.googlesource.com/c/chromium/src/+/1883567 so
    it's no longer necessary to do so explicitly in tests.
    
    This patch addresses unit tests under:
    /chrome/browser/permissions
    
    This CL was uploaded by git cl split.
    
    R=raymes@chromium.org
    
    Bug: 1018667
    Change-Id: If47bf45568a43449af37946d1362aad9189c6803
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1917525
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715758}

--- chrome/browser/permissions/permission_manager_unittest.cc
+++ chrome/browser/permissions/permission_manager_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
-#include "base/test/mock_entropy_provider.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
@@ -650,8 +649,6 @@ TEST_F(PermissionManagerTest, KillSwitchOnIsNotOverridable) {
           PermissionType::GEOLOCATION, kLocalHost));
 
   // Turn on kill switch for GEOLOCATION.
-  base::FieldTrialList field_trial_list(
-      std::make_unique<base::MockEntropyProvider>());
   variations::testing::ClearAllVariationParams();
   std::map<std::string, std::string> params;
   params[PermissionUtil::GetPermissionString(
