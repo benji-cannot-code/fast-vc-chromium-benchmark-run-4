05546b24e9e964d0ab7a0e5aad32a9093ff7a9e5
Greg Thompson <grt@chromium.org>
2019-11-15 23:04:16 +0000

    Suppress beforeunload and in-progress download prompts for forced restarts.
    This change introduces RelaunchIgnoreUnloadHandlers and uses it in
    RelaunchNotificationController when the deadline for a forced relaunch
    is reached. This new function is to AttemptRelaunch as
    ExitIgnoreUnloadHandlers is to AttemptExit.
    
    This also introduces browser_shutdown::ShouldIgnoreUnloadHandlers(),
    which is a convenience for code that needs to know whether or not an
    in-progress shutdown should ignore unload handlers.
    
    BUG=958865
    
    Change-Id: I91a9832d745e95b7436b176d78f5e39ed14addd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1914380
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715901}

--- chrome/browser/ui/views/relaunch_notification/relaunch_notification_controller.cc
+++ chrome/browser/ui/views/relaunch_notification/relaunch_notification_controller.cc
@@ -306,5 +306,5 @@ void RelaunchNotificationController::Close() {
 }
 
 void RelaunchNotificationController::OnRelaunchDeadlineExpired() {
-  chrome::AttemptRelaunch();
+  chrome::RelaunchIgnoreUnloadHandlers();
 }
