6d6eab61289ba05991293bab4703b363dcff9a36
Luciano Pacheco <lucmult@chromium.org>
2019-11-15 07:16:33 +0000

    Files app: Add scan-completed attribute to mark directory loaded
    Add a robust way for tests to wait for a directory finish loading
    by adding a 'scan-completed' attribute to list container.
    
    Change the navigateWithDirectoryTree() integration test helper to
    use of this attribute.
    
    Bug: 992821
    Change-Id: If03ade0de3921b93a652c438f20f17ae95be7991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918795
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715618}

--- ui/file_manager/file_manager/foreground/js/scan_controller.js
+++ ui/file_manager/file_manager/foreground/js/scan_controller.js
@@ -71,6 +71,12 @@ class ScanController {
       this.listContainer_.endBatchUpdates();
     }
 
+    if (window.IN_TEST) {
+      this.listContainer_.element.removeAttribute('scan-completed');
+      this.listContainer_.element.setAttribute(
+          'scan-started', this.directoryModel_.getCurrentDirName());
+    }
+
     this.listContainer_.startBatchUpdates();
     this.scanInProgress_ = true;
 
@@ -91,6 +97,12 @@ class ScanController {
     if (!this.scanInProgress_) {
       console.error('Scan-completed event received. But scan is not started.');
       return;
+    }
+
+    if (window.IN_TEST) {
+      this.listContainer_.element.removeAttribute('scan-started');
+      this.listContainer_.element.setAttribute(
+          'scan-completed', this.directoryModel_.getCurrentDirName());
     }
 
     this.hideSpinner_();
