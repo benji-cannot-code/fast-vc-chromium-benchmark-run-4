9b5a6e0807713ab362a9f3f4f33b552c80477337
Oriol Brufau <obrufau@igalia.com>
2019-11-15 12:55:03 +0000

    [css-grid] Return cached track sizes by reference
    Currently GridTrackSizingAlgorithmStrategy::GetCachedGridTrackSize and
    GridTrack::CachedTrackSize don't return the GridTrackSize by reference.
    
    This means that some code like
    
      const GridLength& length = track.CachedTrackSize().MaxTrackBreadth();
      DCHECK(length.IsFlex());
    
    has a stack-use-after-scope issue, since GridTrackSize::MaxTrackBreadth
    returns a GridLength by reference, which will point to the temporary
    object returned by CachedTrackSize.
    
    To avoid this problem, this patch makes GetCachedGridTrackSize and
    CachedTrackSize return a const reference instead of a temporary copy.
    
    Change-Id: I3e43bf88a475b30747a101195705a22d93cc52a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919140
    Reviewed-by: Sergio Villar <svillar@igalia.com>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#715665}

--- third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.h
+++ third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.h
@@ -67,7 +67,7 @@ class GridTrack {
   }
   void SetGrowthLimitCap(base::Optional<LayoutUnit>);
 
-  const GridTrackSize CachedTrackSize() const {
+  const GridTrackSize& CachedTrackSize() const {
     DCHECK(cached_track_size_.has_value());
     return cached_track_size_.value();
   }
@@ -326,8 +326,9 @@ class GridTrackSizingAlgorithmStrategy {
     return algorithm_.AvailableSpace();
   }
 
-  GridTrackSize GetCachedGridTrackSize(GridTrackSizingDirection direction,
-                                       size_t translated_index) const {
+  const GridTrackSize& GetCachedGridTrackSize(
+      GridTrackSizingDirection direction,
+      size_t translated_index) const {
     return algorithm_.Tracks(direction)[translated_index].CachedTrackSize();
   }
 
