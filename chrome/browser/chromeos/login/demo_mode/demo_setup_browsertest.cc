2aba776359f6123d61f24305dda62c9175c67fae
Tina Wang <tinazwang@chromium.org>
2019-11-15 19:51:06 +0000

    Disabled DemoSetupTest.OnlineSetupFlowErrorPowerwashRequired
    Disabled DemoSetupTest.OnlineSetupFlowErrorPowerwashRequired on linux
    because it times out consistently and fails the linux-chromeos-dbg.
    
    Bug: 1025213
    Tbr: agawronska@chromium.org
    Change-Id: Ib1458c8d2e3018c67f406b8bae2f729a7d3c76b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918313
    Reviewed-by: Tina Wang <tinazwang@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715808}

--- chrome/browser/chromeos/login/demo_mode/demo_setup_browsertest.cc
+++ chrome/browser/chromeos/login/demo_mode/demo_setup_browsertest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time_to_iso8601.h"
 #include "base/timer/timer.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
 #include "chrome/browser/chromeos/login/demo_mode/demo_setup_test_utils.h"
 #include "chrome/browser/chromeos/login/login_manager_test.h"
@@ -729,7 +730,16 @@ IN_PROC_BROWSER_TEST_F(DemoSetupTest, OnlineSetupFlowErrorDefault) {
   EXPECT_FALSE(StartupUtils::IsDeviceRegistered());
 }
 
-IN_PROC_BROWSER_TEST_F(DemoSetupTest, OnlineSetupFlowErrorPowerwashRequired) {
+// Consistently timing out on xxx. http://crbug/com/1025213
+#if defined(OS_LINUX)
+#define MAYBE_OnlineSetupFlowErrorPowerwashRequired \
+  DISABLED_OnlineSetupFlowErrorPowerwashRequired
+#else
+#define MAYBE_OnlineSetupFlowErrorPowerwashRequired \
+  OnlineSetupFlowErrorPowerwashRequired
+#endif
+IN_PROC_BROWSER_TEST_F(DemoSetupTest,
+                       MAYBE_OnlineSetupFlowErrorPowerwashRequired) {
   // Simulate online setup failure that requires powerwash.
   enrollment_helper_.ExpectEnrollmentMode(
       policy::EnrollmentConfig::MODE_ATTESTATION);
