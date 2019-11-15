0dc145cccdc83a9e1704f42cdce97343ffc1a164
Henrique Nakashima <hnakashima@chromium.org>
2019-11-15 21:16:56 +0000

    Check the format of new ChromePreferenceKeys constants.
    A new test verified all keys conform to "Chrome.[Feature].[Key]":
    ChromePreferenceKeysTest.testDeclaredConstantsConformToFormat()
    
    Grandfather in the constants currently used, since none of them
    conforms to the format.
    
    Bug: 1023839
    Change-Id: I7175594d9027d0160d5f102175ee4df3ff27a61a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1912646
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715856}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeysTest.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeysTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.preferences;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import android.support.test.filters.SmallTest;
@@ -20,11 +21,12 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  * Test class that verifies that {@link ChromePreferenceKeys} conforms to its constraints:
  * - No keys are both in [keys in use] and in [deprecated keys].
- * TODO(crbug.com/1023839): Make sure new keys conform to format "Chrome.[Feature].[Key]"
+ * - All keys follow the format "Chrome.[Feature].[Key]"
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 public class ChromePreferenceKeysTest {
@@ -95,5 +97,92 @@ public class ChromePreferenceKeysTest {
     public void testReuseCheck_intersection() {
         doTestKeysAreNotReused(Arrays.asList("UsedKey1", "ReusedKey"),
                 Arrays.asList("ReusedKey", "DeprecatedKey1"));
+    }
+
+    /**
+     * Test that the keys in use conform to the format:
+     * "Chrome.[Feature].[Key]"
+     *
+     * Old constants are grandfathered in.
+     */
+    @Test
+    @SmallTest
+    public void testKeysConformToFormat() {
+        doTestKeysConformToFormat(ChromePreferenceKeys.createUsedKeys(),
+                ChromePreferenceKeys.createGrandfatheredFormatKeysForTesting());
+    }
+
+    private void doTestKeysConformToFormat(List<String> usedKeysList, List<String> grandfathered) {
+        Set<String> grandfatheredSet = new HashSet<>(grandfathered);
+        Pattern regex = Pattern.compile("Chrome.([A-Z][a-z0-9]+)+.([A-Z][a-z0-9]+)+");
+
+        for (String usedKey : usedKeysList) {
+            if (grandfatheredSet.contains(usedKey)) {
+                continue;
+            }
+
+            assertTrue("\"" + usedKey + "\" does not conform to format \"Chrome.[Feature].[Key]\"",
+                    regex.matcher(usedKey).matches());
+        }
+    }
+
+    // Below are tests to ensure that doTestKeysConformToFormat() works.
+
+    private static class TestFormatConstantsClass {
+        public static final String GRANDFATHERED_IN = "grandfathered_in";
+        public static final String NEW1 = "Chrome.FeatureOne.Key1";
+        public static final String NEW2 = "Chrome.Foo.Key";
+        public static final String BROKEN_PREFIX = "Chrom.Foo.Key";
+        public static final String MISSING_FEATURE = "Chrome..Key";
+        public static final String LOWERCASE_KEY = "Chrome.Foo.key";
+    }
+
+    private static final List<String> NON_FORMAT_CONFORMING_CONSTANTS = Arrays.asList(
+            TestFormatConstantsClass.GRANDFATHERED_IN, TestFormatConstantsClass.BROKEN_PREFIX,
+            TestFormatConstantsClass.MISSING_FEATURE, TestFormatConstantsClass.LOWERCASE_KEY);
+
+    @Test
+    @SmallTest
+    public void testFormatCheck_correct() {
+        doTestKeysConformToFormat(
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN,
+                        TestFormatConstantsClass.NEW1, TestFormatConstantsClass.NEW2),
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN));
+    }
+
+    @Test(expected = AssertionError.class)
+    @SmallTest
+    public void testFormatCheck_invalidFormat() {
+        doTestKeysConformToFormat(
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN,
+                        TestFormatConstantsClass.NEW1, TestFormatConstantsClass.NEW2),
+                Collections.emptyList());
+    }
+
+    @Test(expected = AssertionError.class)
+    @SmallTest
+    public void testFormatCheck_brokenPrefix() {
+        doTestKeysConformToFormat(
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN,
+                        TestFormatConstantsClass.NEW1, TestFormatConstantsClass.BROKEN_PREFIX),
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN));
+    }
+
+    @Test(expected = AssertionError.class)
+    @SmallTest
+    public void testFormatCheck_MissingFeature() {
+        doTestKeysConformToFormat(
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN,
+                        TestFormatConstantsClass.NEW1, TestFormatConstantsClass.MISSING_FEATURE),
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN));
+    }
+
+    @Test(expected = AssertionError.class)
+    @SmallTest
+    public void testFormatCheck_LowercaseKey() {
+        doTestKeysConformToFormat(
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN,
+                        TestFormatConstantsClass.NEW1, TestFormatConstantsClass.LOWERCASE_KEY),
+                Arrays.asList(TestFormatConstantsClass.GRANDFATHERED_IN));
     }
 }
