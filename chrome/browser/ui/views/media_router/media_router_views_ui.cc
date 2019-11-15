c3f3de5595e1a848965f6bb78c2ff12c32d59e47
Takumi Fujimoto <takumif@chromium.org>
2019-11-15 18:55:08 +0000

    Record casting source metrics even if MediaRouterViewsUI is destroyed
    When the Cast dialog is closed, MediaRouterViewsUI gets destroyed. If
    that happened before casting succeeded, we weren't recording the cast
    source. This CL fixes that.
    
    Change-Id: I58fb7d9db16867bce1728788b8c21694b33d3dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1904821
    Reviewed-by: Brandon Tolsch <btolsch@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715779}

--- chrome/browser/ui/views/media_router/media_router_views_ui.cc
+++ chrome/browser/ui/views/media_router/media_router_views_ui.cc
@@ -86,6 +86,12 @@ MediaSource GetSourceForRouteObserver(const std::vector<MediaSource>& sources) {
   return source_it != sources.end() ? *source_it : MediaSource(std::string());
 }
 
+void MaybeReportCastingSource(MediaCastMode cast_mode,
+                              const RouteRequestResult& result) {
+  if (result.result_code() == RouteRequestResult::OK)
+    MediaRouterMetrics::RecordMediaRouterCastingSource(cast_mode);
+}
+
 }  // namespace
 
 // Observes a WebContents and requests fullscreening of its first
@@ -341,13 +347,6 @@ bool MediaRouterViewsUI::CreateRoute(const MediaSink::Id& sink_id,
 
 void MediaRouterViewsUI::TerminateRoute(const MediaRoute::Id& route_id) {
   GetMediaRouter()->TerminateRoute(route_id);
-}
-
-void MediaRouterViewsUI::MaybeReportCastingSource(
-    MediaCastMode cast_mode,
-    const RouteRequestResult& result) {
-  if (result.result_code() == RouteRequestResult::OK)
-    MediaRouterMetrics::RecordMediaRouterCastingSource(cast_mode);
 }
 
 std::vector<MediaSinkWithCastModes> MediaRouterViewsUI::GetEnabledSinks()
@@ -627,8 +626,7 @@ base::Optional<RouteParameters> MediaRouterViewsUI::GetRouteParameters(
   // HandleCreateSessionRequestRouteResponse(), which closes the dialog and
   // destroys |this|.
   params.route_result_callbacks.push_back(
-      base::BindOnce(&MediaRouterViewsUI::MaybeReportCastingSource,
-                     weak_factory_.GetWeakPtr(), cast_mode));
+      base::BindOnce(&MaybeReportCastingSource, cast_mode));
 
   // There are 3 cases. In cases (1) and (3) the MediaRouterViewsUI will need to
   // be notified via OnRouteResponseReceived(). In case (2) the dialog will be
@@ -902,8 +900,7 @@ base::Optional<RouteParameters> MediaRouterViewsUI::GetLocalFileRouteParameters(
       GetPresentationRequestSourceName()));
 
   params.route_result_callbacks.push_back(
-      base::BindOnce(&MediaRouterViewsUI::MaybeReportCastingSource,
-                     weak_factory_.GetWeakPtr(), MediaCastMode::LOCAL_FILE));
+      base::BindOnce(&MaybeReportCastingSource, MediaCastMode::LOCAL_FILE));
 
   params.route_result_callbacks.push_back(
       base::BindOnce(&MediaRouterViewsUI::MaybeReportFileInformation,
