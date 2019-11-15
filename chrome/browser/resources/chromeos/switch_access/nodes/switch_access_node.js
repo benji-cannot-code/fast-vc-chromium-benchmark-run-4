5078aba752f736ee7352a05287a30c6e8814648d
Anastasia Helfinstein <anastasi@google.com>
2019-11-15 22:53:21 +0000

    [Switch Access] Respond to changes in the accessibility tree
    When the accessibility tree changes, events are fired on the
    automation API. This change updates the Switch Access tree when those
    events are received.
    
    Bug: 1023035
    Change-Id: I88e208b8ed9d10653db2395dd4424194c3ef3248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1907550
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715896}

--- chrome/browser/resources/chromeos/switch_access/nodes/switch_access_node.js
+++ chrome/browser/resources/chromeos/switch_access/nodes/switch_access_node.js
@@ -144,6 +144,9 @@ class SAChildNode {
    */
   performAction(action) {}
 
+  /** Called when this node may have changed. */
+  refresh() {}
+
   // ================= Debug methods =================
 
   /**
@@ -274,6 +277,13 @@ class SARootNode {
 
   /** Called when a group is exiting. */
   onExit() {}
+
+  /** Called when this node or its children may have changed. */
+  refresh() {
+    for (const child of this.children_) {
+      child.refresh();
+    }
+  }
 
   // ================= Debug methods =================
 
