6ec59d7026602fcdb2eede42df6adf70b5e7525b
Rohit Rao <rohitrao@chromium.org>
2019-11-15 01:54:51 +0000

    Revert "[ios] Move password settings tests to EG2"
    This reverts commit c273c91d38d4565ddf670f967714e7265df58396.
    
    Reason for revert: Tests fail due to multiple elements matching in SettingsMenuBackButton().
    
    Original change's description:
    > [ios] Move password settings tests to EG2
    >
    > Bug: 987646
    > Change-Id: I9e21ec812293136797b80af8c4b189331941a111
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903363
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Commit-Queue: David Jean <djean@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#715258}
    
    TBR=eugenebut@chromium.org,gambard@chromium.org,djean@chromium.org
    
    Change-Id: I6cf4a218cfc12f341591ee4a1ddaaf79f8e8d307
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 987646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918183
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715554}

--- ios/chrome/test/earl_grey/chrome_matchers_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_matchers_app_interface.mm
@@ -45,9 +45,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/google_services/advanced_signin_settings_coordinator.h"
 #import "ios/chrome/browser/ui/settings/import_data_table_view_controller.h"
-#import "ios/chrome/browser/ui/settings/password/passwords_table_view_constants.h"
+#import "ios/chrome/browser/ui/settings/password/passwords_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/privacy_table_view_controller.h"
-#import "ios/chrome/browser/ui/settings/settings_root_table_constants.h"
+#import "ios/chrome/browser/ui/settings/settings_root_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/settings_table_view_controller.h"
 #import "ios/chrome/browser/ui/static_content/static_html_view_controller.h"
 #import "ios/chrome/browser/ui/tab_grid/grid/grid_constants.h"
@@ -545,16 +545,9 @@ UIView* SubviewWithAccessibilityIdentifier(NSString* accessibility_id,
       SubviewWithAccessibilityIdentifier(
           @"SettingNavigationBar",
           [[UIApplication sharedApplication] keyWindow]));
-#if defined(CHROME_EARL_GREY_2)
-  id<GREYMatcher> buttonLabelClassMatcher = grey_anyOf(
-      grey_kindOfClassName(@"UIButton"),
-      grey_kindOfClassName(@"UIAccessibilityBackButtonElement"), nil);
-#else
-  id<GREYMatcher> buttonLabelClassMatcher = grey_kindOfClass([UIButton class]);
-#endif
   return grey_allOf(grey_anyOf(grey_accessibilityLabel(navBar.backItem.title),
                                grey_accessibilityLabel(@"Back"), nil),
-                    buttonLabelClassMatcher,
+                    grey_kindOfClass([UIButton class]),
                     grey_ancestor(grey_kindOfClass([UINavigationBar class])),
                     nil);
 }
