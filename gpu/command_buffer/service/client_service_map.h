378048417096acf5066008ab91ac67b2e522f807
kylechar <kylechar@chromium.org>
2019-11-15 14:34:46 +0000

    Fix assigning NULL to scoped_refptr
    Update code where NULL or 0 is assigned to a scoped_refptr to reset it. Just
    call reset() instead. If operator=(std::nullptr_t) is added these assignments
    become ambiguous.
    
    This CL was uploaded by git cl split.
    
    R=zmo@chromium.org
    
    Bug: 1024981
    Change-Id: Ia08fdf19c190da94edeaaaaa2d8811f91fa1b58b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918152
    Auto-Submit: kylechar <kylechar@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715678}

--- gpu/command_buffer/service/client_service_map.h
+++ gpu/command_buffer/service/client_service_map.h
@@ -77,7 +77,7 @@ class ClientServiceMap {
       return true;
     }
     if (client_id == 0) {
-      *service_id = 0;
+      *service_id = ServiceType{};
       return true;
     }
     return false;
--- gpu/command_buffer/service/gpu_tracer.cc
+++ gpu/command_buffer/service/gpu_tracer.cc
@@ -231,7 +231,7 @@ bool GPUTracer::EndDecoding() {
             marker.trace_->End();
 
             finished_traces_.push_back(marker.trace_);
-            marker.trace_ = 0;
+            marker.trace_.reset();
           }
         }
       }
@@ -383,7 +383,7 @@ void GPUTracer::ClearOngoingTraces(bool have_context) {
       TraceMarker& marker = markers_[n][i];
       if (marker.trace_.get()) {
         marker.trace_->Destroy(have_context);
-        marker.trace_ = 0;
+        marker.trace_.reset();
       }
     }
   }
