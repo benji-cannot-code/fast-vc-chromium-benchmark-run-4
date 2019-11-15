d7ea8406ce829cd36db71660a187884456adfe7c
Ken MacKay <kmackay@chromium.org>
2019-11-15 21:05:05 +0000

    [Chromecast] Make sure audio buffers are not overwritten while in use
    When the mixer is in the same process, audio buffers are sent to the
    mixer service by just posting a reference. The mixer service holds the
    reference until the audio is written out; the audio buffer must not be
    overwritten until this occurs. Therefore, use an IOBufferPool to manage
    the buffer lifetime.
    
    Bug: internal b/143628666
    Change-Id: I4bd247c806c0ec5d909ffa048dd0f8d30aeabed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1917958
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715847}

--- chromecast/media/audio/mixer_service/output_stream_connection.h
+++ chromecast/media/audio/mixer_service/output_stream_connection.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/io_buffer.h"
 
 namespace chromecast {
+class IOBufferPool;
+
 namespace media {
 namespace mixer_service {
 class Generic;
@@ -108,7 +110,9 @@ class OutputStreamConnection : public MixerConnection,
 
   const int frame_size_;
   const int fill_size_frames_;
-  const scoped_refptr<net::IOBuffer> audio_buffer_;
+
+  const scoped_refptr<IOBufferPool> buffer_pool_;
+  scoped_refptr<net::IOBuffer> audio_buffer_;
 
   std::unique_ptr<MixerSocket> socket_;
   float volume_multiplier_ = 1.0f;
