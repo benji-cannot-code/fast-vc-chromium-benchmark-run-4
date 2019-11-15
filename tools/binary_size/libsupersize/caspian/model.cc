ccff93fb9fb526deebae351c274f32b0b71b41b7
Jasper Chapman-Black <jaspercb@chromium.org>
2019-11-15 20:50:48 +0000

    SuperSize: Caspian: Aggregate padding by section
    Also adds diff_test.cc.
    
    Bug: 1011921
    Change-Id: I3d678738719531d1c74e620f4fc3605cab08b325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918027
    Commit-Queue: Jasper Chapman-Black <jaspercb@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715841}

--- tools/binary_size/libsupersize/caspian/model.cc
+++ tools/binary_size/libsupersize/caspian/model.cc
@@ -40,7 +40,7 @@ void Symbol::DeriveNames() const {
     name_ = full_name_;
   } else if (IsDex()) {
     std::tuple<std::string_view, std::string_view, std::string_view>
-        parsed_names = ParseJava(full_name_, &size_info->owned_strings);
+        parsed_names = ParseJava(full_name_, &size_info_->owned_strings);
     template_name_ = std::get<1>(parsed_names);
     name_ = std::get<2>(parsed_names);
   } else if (IsStringLiteral()) {
@@ -48,7 +48,7 @@ void Symbol::DeriveNames() const {
     name_ = full_name_;
   } else if (IsNative()) {
     std::tuple<std::string_view, std::string_view, std::string_view>
-        parsed_names = ParseCpp(full_name_, &size_info->owned_strings);
+        parsed_names = ParseCpp(full_name_, &size_info_->owned_strings);
     template_name_ = std::get<1>(parsed_names);
     name_ = std::get<2>(parsed_names);
   } else {
