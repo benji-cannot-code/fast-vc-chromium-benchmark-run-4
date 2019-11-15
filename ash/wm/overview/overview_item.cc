6515f69b8762ce23796baaedf652ca50876fd5ae
Xiaoqian Dai <xdai@chromium.org>
2019-11-15 22:19:05 +0000

    overview gesture: fix the crash when drag a window to fling.
    When dragging a window quickly up into the drag-to-snap split area, the
    overview windows are getting relayouted and while the animations are
    still going on, fling the window to homescreen. So
    OverviewItem::OnItemBoundsAnimationEnded() can be called when overview
    is in shutting down process, which should not happen.
    
    Bug: 1025267, 997885
    Change-Id: Ia40f3fce589554672f2134f78ac4ddd8147d0910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918834
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715883}

--- ash/wm/overview/overview_item.cc
+++ ash/wm/overview/overview_item.cc
@@ -1084,6 +1084,11 @@ void OverviewItem::OnItemBoundsAnimationStarted() {
 }
 
 void OverviewItem::OnItemBoundsAnimationEnded() {
+  // Do nothing if overview is shutting down. See crbug.com/1025267 for when it
+  // might happen.
+  if (!Shell::Get()->overview_controller()->InOverviewSession())
+    return;
+
   UpdateRoundedCornersAndShadow();
   OnDragAnimationCompleted();
 }
