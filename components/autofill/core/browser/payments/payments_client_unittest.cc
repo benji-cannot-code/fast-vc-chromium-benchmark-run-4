8aa0cf73149999b5e335a4ebe6b7bc9161fb4854
Mikel Astiz <mastiz@chromium.org>
2019-11-15 19:21:02 +0000

    Do not instantiate base::FieldTrialList explicitly in tests
    The test suite already instantiates a FieldTrialList as per
    https://chromium-review.googlesource.com/c/chromium/src/+/1883567 so
    it's no longer necessary to do so explicitly in tests.
    
    This patch addresses unit tests under:
    /components/autofill/core/browser/payments
    
    This CL was uploaded by git cl split.
    
    R=jsaul@google.com
    
    Bug: 1018667
    Change-Id: I2b8c3aa9abf613235936510d6678f28bf7bc858e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1917511
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715793}

--- components/autofill/core/browser/payments/payments_client_unittest.cc
+++ components/autofill/core/browser/payments/payments_client_unittest.cc
@@ -712,9 +712,6 @@ TEST_F(PaymentsClientTest, GetUploadDetailsVariationsTest) {
   // headers. Also, the variations header provider may have been registered to
   // observe some other field trial list, so reset it.
   variations::VariationsHttpHeaderProvider::GetInstance()->ResetForTesting();
-  // Note: This needs a base::FieldTrialList instance because it does not use
-  // ScopedFeatureList, which provides its own, unlike other tests that do.
-  base::FieldTrialList field_trial_list_(nullptr);
   CreateFieldTrialWithId("AutofillTest", "Group", 369);
   StartGettingUploadDetails();
 
@@ -826,9 +823,6 @@ TEST_F(PaymentsClientTest, UploadCardVariationsTest) {
   // headers. Also, the variations header provider may have been registered to
   // observe some other field trial list, so reset it.
   variations::VariationsHttpHeaderProvider::GetInstance()->ResetForTesting();
-  // Note: This needs a base::FieldTrialList instance because it does not use
-  // ScopedFeatureList, which provides its own, unlike other tests that do.
-  base::FieldTrialList field_trial_list_(nullptr);
   CreateFieldTrialWithId("AutofillTest", "Group", 369);
   StartUploading(/*include_cvc=*/true);
   IssueOAuthToken();
@@ -844,9 +838,6 @@ TEST_F(PaymentsClientTest, UnmaskCardVariationsTest) {
   // headers. Also, the variations header provider may have been registered to
   // observe some other field trial list, so reset it.
   variations::VariationsHttpHeaderProvider::GetInstance()->ResetForTesting();
-  // Note: This needs a base::FieldTrialList instance because it does not use
-  // ScopedFeatureList, which provides its own, unlike other tests that do.
-  base::FieldTrialList field_trial_list_(nullptr);
   CreateFieldTrialWithId("AutofillTest", "Group", 369);
   StartUnmasking(CardUnmaskOptions());
   IssueOAuthToken();
@@ -862,9 +853,6 @@ TEST_F(PaymentsClientTest, MigrateCardsVariationsTest) {
   // headers. Also, the variations header provider may have been registered to
   // observe some other field trial list, so reset it.
   variations::VariationsHttpHeaderProvider::GetInstance()->ResetForTesting();
-  // Note: This needs a base::FieldTrialList instance because it does not use
-  // ScopedFeatureList, which provides its own, unlike other tests that do.
-  base::FieldTrialList field_trial_list_(nullptr);
   CreateFieldTrialWithId("AutofillTest", "Group", 369);
   StartMigrating(/*has_cardholder_name=*/true);
   IssueOAuthToken();
