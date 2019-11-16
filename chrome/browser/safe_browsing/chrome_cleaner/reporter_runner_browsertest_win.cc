2ea68e206b8d93209c7ef0d5f5bf45b1320d393d
Joe Mason <joenotcharles@google.com>
2019-11-16 00:33:02 +0000

    Refactor ReporterRunner test delegate
    This lets tests mock just the reporter launch step without overriding the reset of the logic in
    LaunchAndWaitForExit. This will be used in an upcoming unit test to test reporter process
    termination.
    
    Also cleans up some code in the reporter_runner_win.cc file:
    
    * Renames LaunchAndWaitForExitOnBackgroundThread to LaunchAndWaitForExit since
      OnBackgroundThread is confusing, and exposes it for testing.
    * Renames SwReporterTestingDelegate::LaunchReporter to LaunchReporterProcess and only doubles
      the launch step.
    * Adds SwReporterTestingDelegate::WaitForReporterExit that doubles the wait step and returns
      the process exit code.
    * Moves test support classes and functions into the "internal" namespace.
    * Moves ReporterRunner, which is internal to the .cc file, to the "internal" namespace so it
      doesn't pollute the safe_browsing namespace.
    
    R=proberge
    
    Bug: 1016939
    Change-Id: I4f090f15cc40821b3faa83d820e6703f6de41fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1912804
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715947}

--- chrome/browser/safe_browsing/chrome_cleaner/reporter_runner_browsertest_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/reporter_runner_browsertest_win.cc
@@ -16,7 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/macros.h"
+#include "base/process/launch.h"
+#include "base/process/process.h"
 #include "base/run_loop.h"
 #include "base/stl_util.h"
 #include "base/synchronization/lock.h"
@@ -185,7 +188,7 @@ using ReporterRunnerTestParams =
 
 class ReporterRunnerTest
     : public InProcessBrowserTest,
-      public SwReporterTestingDelegate,
+      public internal::SwReporterTestingDelegate,
       public ::testing::WithParamInterface<ReporterRunnerTestParams> {
  public:
   ReporterRunnerTest() {
@@ -194,7 +197,7 @@ class ReporterRunnerTest
   }
 
   void SetUpInProcessBrowserTestFixture() override {
-    SetSwReporterTestingDelegate(this);
+    internal::SetSwReporterTestingDelegate(this);
     EXPECT_CALL(policy_provider_, IsInitializationComplete(_))
         .WillRepeatedly(Return(true));
     policy::BrowserPolicyConnector::SetPolicyProviderForTesting(
@@ -237,16 +240,25 @@ class ReporterRunnerTest
   }
 
   void TearDownInProcessBrowserTestFixture() override {
-    SetSwReporterTestingDelegate(nullptr);
+    internal::SetSwReporterTestingDelegate(nullptr);
   }
 
   // Records that the reporter was launched with the parameters given in
   // |invocation|.
-  int LaunchReporter(const SwReporterInvocation& invocation) override {
+  base::Process LaunchReporterProcess(
+      const SwReporterInvocation& invocation,
+      const base::LaunchOptions& options) override {
+    ANALYZER_ALLOW_UNUSED(options);
     ++reporter_launch_count_;
     reporter_launch_parameters_.push_back(invocation);
     if (first_launch_callback_)
       std::move(first_launch_callback_).Run();
+    // Need to return a valid process so the launch continues.
+    return base::Process::Current();
+  }
+
+  int WaitForReporterExit(const base::Process& reporter_process) const {
+    ANALYZER_ALLOW_UNUSED(reporter_process);
     return exit_code_to_report_;
   }
 
