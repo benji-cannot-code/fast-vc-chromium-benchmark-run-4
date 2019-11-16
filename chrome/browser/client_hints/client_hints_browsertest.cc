c8d7e321971d9c407df70a5430b09866231626f2
Mikel Astiz <mastiz@chromium.org>
2019-11-16 10:39:36 +0000

    Do not instantiate base::FieldTrialList explicitly in tests
    The test suite already instantiates a FieldTrialList as per
    https://chromium-review.googlesource.com/c/chromium/src/+/1883567 so
    it's no longer necessary to do so explicitly in tests.
    
    This patch addresses unit tests under:
    /chrome/browser/client_hints
    
    This CL was uploaded by git cl split.
    
    R=ryansturm@chromium.org
    
    Bug: 1018667
    Change-Id: I4d6ccb972fdafd58f33d11cf090fc61560bdb50a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1916825
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716021}

--- chrome/browser/client_hints/client_hints_browsertest.cc
+++ chrome/browser/client_hints/client_hints_browsertest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/synchronization/lock.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "base/test/mock_entropy_provider.h"
 #include "build/build_config.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
@@ -141,8 +140,7 @@ class ClientHintsBrowserTest : public InProcessBrowserTest,
                                public testing::WithParamInterface<bool> {
  public:
   ClientHintsBrowserTest()
-      : field_trial_list_(std::make_unique<base::MockEntropyProvider>()),
-        http_server_(net::EmbeddedTestServer::TYPE_HTTP),
+      : http_server_(net::EmbeddedTestServer::TYPE_HTTP),
         https_server_(net::EmbeddedTestServer::TYPE_HTTPS),
         https_cross_origin_server_(net::EmbeddedTestServer::TYPE_HTTPS),
         expect_client_hints_on_main_frame_(false),
@@ -428,7 +426,6 @@ class ClientHintsBrowserTest : public InProcessBrowserTest,
     return main_frame_ua_observed_;
   }
 
-  base::FieldTrialList field_trial_list_;
   base::test::ScopedFeatureList scoped_feature_list_;
 
   std::string intercept_iframe_resource_;
