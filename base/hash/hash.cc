fd2b91620d2dc1506e424c6d4c57141cde99a9db
Daniel Cheng <dcheng@chromium.org>
2019-11-16 00:21:04 +0000

    Make base::FastHash non-deterministic in DCHECK_IS_ON() builds.
    This is a prerequisite for migrating more things over to base::FastHash,
    to ensure they don't inadverdently have a dependency on a stable mapping
    of inputs to hash values.
    
    Also change the FastHash(const std::string&) overload to
    FastHash(StringPiece) to avoid potentially constructing unnecessary
    temporaries.
    
    Bug: 1025358
    Change-Id: I2e3d67ab74f869437ae2bdf12c9560f1be19688d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919804
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#715940}

--- base/hash/hash.cc
+++ base/hash/hash.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/hash/hash.h"
 
+#include "base/rand_util.h"
 #include "base/third_party/cityhash/city.h"
 #include "build/build_config.h"
 
@@ -18,20 +19,16 @@ namespace base {
 size_t FastHash(base::span<const uint8_t> data) {
   // We use the updated CityHash within our namespace (not the deprecated
   // version from third_party/smhasher).
-#if defined(ARCH_CPU_64_BITS)
+#if DCHECK_IS_ON()
+  static const uint64_t seed = base::RandUint64();
+  return base::internal::cityhash_v111::CityHash64WithSeed(
+      reinterpret_cast<const char*>(data.data()), data.size(), seed);
+#elif defined(ARCH_CPU_64_BITS)
   return base::internal::cityhash_v111::CityHash64(
       reinterpret_cast<const char*>(data.data()), data.size());
 #else
   return base::internal::cityhash_v111::CityHash32(
       reinterpret_cast<const char*>(data.data()), data.size());
-#endif
-}
-
-size_t FastHash(const std::string& str) {
-#if defined(ARCH_CPU_64_BITS)
-  return base::internal::cityhash_v111::CityHash64(str.data(), str.size());
-#else
-  return base::internal::cityhash_v111::CityHash32(str.data(), str.size());
 #endif
 }
 
