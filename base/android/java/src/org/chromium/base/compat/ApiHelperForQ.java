ee7c8e42ec26303bf3465110f71fb58ae1e9b4d6
Andrew Grieve <agrieve@google.com>
2019-11-18 22:17:48 +0000

    Android: Upstream code for variable service in BindService.java
    Bug: 1003532
    Change-Id: I8b3ec212067c0bfd145258e645c42db7ae140790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919734
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716341}

--- base/android/java/src/org/chromium/base/compat/ApiHelperForQ.java
+++ base/android/java/src/org/chromium/base/compat/ApiHelperForQ.java
@@ -7,6 +7,9 @@ package org.chromium.base.compat;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Build;
 import android.telephony.CellInfo;
 import android.telephony.TelephonyManager;
@@ -16,6 +19,7 @@ import org.chromium.base.annotations.VerifiesOnQ;
 import org.chromium.base.task.AsyncTask;
 
 import java.util.List;
+import java.util.concurrent.Executor;
 
 /**
  * Utility class to use new APIs that were added in Q (API level 29). These need to exist in a
@@ -38,5 +42,15 @@ public final class ApiHelperForQ {
                         callback.onResult(cellInfos);
                     }
                 });
+    }
+
+    public static boolean bindIsolatedService(Context context, Intent intent, int flags,
+            String instanceName, Executor executor, ServiceConnection connection) {
+        return context.bindIsolatedService(intent, flags, instanceName, executor, connection);
+    }
+
+    public static void updateServiceGroup(
+            Context context, ServiceConnection connection, int group, int importance) {
+        context.updateServiceGroup(connection, group, importance);
     }
 }
