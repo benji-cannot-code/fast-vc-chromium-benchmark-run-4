2173ff4dc8e9e43273296326bfcf463e4bfad7df
Ken Rockot <rockot@google.com>
2019-11-18 20:22:25 +0000

    Remove Connector.FilterInterfaces
    The only remaining use of this API is for filtering interfaces exposed
    by the browser to renderers. This replaces the Service Manager
    implementation and Connector API with a one-off filtering implementation
    inside content/browser, thus allowing other Service Manager dependencies
    to be stripped away from Content without compromising the security
    review process.
    
    This filtering will eventually be removed once all renderer-exposed
    interfaces have been migrated to BrowserInterfaceBroker.
    
    Bug: 977637
    Change-Id: I0d9636b974c7d51199b16e4586a09022a4ced4b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1910847
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#716270}

--- services/service_manager/public/cpp/standalone_connector_impl.cc
+++ services/service_manager/public/cpp/standalone_connector_impl.cc
@@ -64,13 +64,4 @@ void StandaloneConnectorImpl::Clone(
   receivers_.Add(this, std::move(receiver));
 }
 
-void StandaloneConnectorImpl::FilterInterfaces(
-    const std::string& spec,
-    const Identity& source,
-    mojo::PendingReceiver<mojom::InterfaceProvider> source_receiver,
-    mojo::PendingRemote<mojom::InterfaceProvider> target) {
-  NOTIMPLEMENTED()
-      << "FilterInterfaces is not supported by StandaloneConnectorImpl.";
-}
-
 }  // namespace service_manager
