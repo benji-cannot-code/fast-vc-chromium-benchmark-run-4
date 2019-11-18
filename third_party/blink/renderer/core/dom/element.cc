648b2dd921858f306fb7769bf782e47c89417819
Daniel Vogelheim <vogelheim@chromium.org>
2019-11-18 17:18:32 +0000

    [Trusted Types] Cover attribute node manipulation with Trusted Types checks.
    Element::setAttribute will perform trusted types checks, which (currently)
    can be circumvented by obtaining the DOM's attribute node and setting the
    value directly. This fixes this bypass, by performing identical checks when
    the attribute node values are set, and/or the attribute node is attached to
    an element.
    
    Bug: 1008012
    Bug: https://github.com/w3c/webappsec-trusted-types/issues/47
    Change-Id: I1d8ead85b3fa11821c329e1f4af60c1e85ea8298
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911215
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716193}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2173,7 +2173,14 @@ void Element::setAttribute(const AtomicString& local_name,
   wtf_size_t index;
   QualifiedName q_name = QualifiedName::Null();
   std::tie(index, q_name) = LookupAttributeQNameInternal(local_name);
-  SetAttributeInternal(index, q_name, value,
+
+  String trusted_value = GetStringFromSpecificTrustedType(
+      value, ExpectedTrustedTypeForAttribute(q_name), &GetDocument(),
+      exception_state);
+  if (exception_state.HadException())
+    return;
+
+  SetAttributeInternal(index, q_name, AtomicString(trusted_value),
                        kNotInSynchronizationOfLazyAttribute);
 }
 
@@ -2189,6 +2196,24 @@ void Element::setAttribute(const QualifiedName& name,
                          ? GetElementData()->Attributes().FindIndex(name)
                          : kNotFound;
   SetAttributeInternal(index, name, value,
+                       kNotInSynchronizationOfLazyAttribute);
+}
+
+void Element::setAttribute(const QualifiedName& name,
+                           const AtomicString& value,
+                           ExceptionState& exception_state) {
+  SynchronizeAttribute(name);
+  wtf_size_t index = GetElementData()
+                         ? GetElementData()->Attributes().FindIndex(name)
+                         : kNotFound;
+
+  String trusted_value = GetStringFromSpecificTrustedType(
+      value, ExpectedTrustedTypeForAttribute(name), &GetDocument(),
+      exception_state);
+  if (exception_state.HadException())
+    return;
+
+  SetAttributeInternal(index, name, AtomicString(trusted_value),
                        kNotInSynchronizationOfLazyAttribute);
 }
 
@@ -3802,6 +3827,13 @@ Attr* Element::setAttributeNode(Attr* attr_node,
   SynchronizeAllAttributes();
   const UniqueElementData& element_data = EnsureUniqueElementData();
 
+  String value = GetStringFromSpecificTrustedType(
+      attr_node->value(),
+      ExpectedTrustedTypeForAttribute(attr_node->GetQualifiedName()),
+      &GetDocument(), exception_state);
+  if (exception_state.HadException())
+    return nullptr;
+
   AttributeCollection attributes = element_data.Attributes();
   wtf_size_t index = attributes.FindIndex(attr_node->GetQualifiedName());
   AtomicString local_name;
@@ -3827,7 +3859,8 @@ Attr* Element::setAttributeNode(Attr* attr_node,
     }
   }
 
-  SetAttributeInternal(index, attr_node->GetQualifiedName(), attr_node->value(),
+  SetAttributeInternal(index, attr_node->GetQualifiedName(),
+                       AtomicString(value),
                        kNotInSynchronizationOfLazyAttribute);
 
   attr_node->AttachToElement(this, local_name);
--- third_party/blink/renderer/core/trustedtypes/trusted_types_util.h
+++ third_party/blink/renderer/core/trustedtypes/trusted_types_util.h
@@ -41,6 +41,11 @@ String CORE_EXPORT GetStringFromSpecificTrustedType(
     const ExecutionContext*,
     ExceptionState&);
 
+String CORE_EXPORT GetStringFromSpecificTrustedType(const String&,
+                                                    SpecificTrustedType,
+                                                    const ExecutionContext*,
+                                                    ExceptionState&);
+
 String CORE_EXPORT GetStringFromTrustedHTML(StringOrTrustedHTML,
                                             const ExecutionContext*,
                                             ExceptionState&);
