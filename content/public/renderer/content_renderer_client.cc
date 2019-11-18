a72fc1de64e1bf848c4b60cec2a00137b7a6774f
Mason Freed <masonfreed@chromium.org>
2019-11-18 20:58:29 +0000

    Re-enable Web Components v0 for file:// URLs
    Because the origin trial system cannot (yet) support file:// URLS for
    re-enabling deprecated features, we will temporarily re-enable WCv0
    for all file:// URLs.
    
    Additionally, this CL renames the function RequiresHtmlImports() to
    RequiresWebComponentsV0(), as that is more descriptive of how it is
    used.
    
    Bug: 1021137, 1025782, 937746
    Change-Id: I54a4794334edeaa25b3e4c2102be60f118f2372b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1920482
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716297}

--- content/public/renderer/content_renderer_client.cc
+++ content/public/renderer/content_renderer_client.cc
@@ -238,7 +238,7 @@ bool ContentRendererClient::IsSafeRedirectTarget(const GURL& url) {
 
 void ContentRendererClient::DidSetUserAgent(const std::string& user_agent) {}
 
-bool ContentRendererClient::RequiresHtmlImports(const GURL& url) {
+bool ContentRendererClient::RequiresWebComponentsV0(const GURL& url) {
   return false;
 }
 
