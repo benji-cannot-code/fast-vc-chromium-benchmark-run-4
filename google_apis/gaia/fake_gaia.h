dec5417a19d1c6f57e4fe16e8cf43cfa2990d0ac
Danan S <danan@chromium.org>
2019-11-18 17:03:10 +0000

    ReAuth API client for Chrome/ChromeOS
    Added to GaiaAuthFetcher/GaiaAuthConsumer.
    
    This implementation only includes support for the parent/child
    ReAuth delegation use case at this time, to support the ability
    for parents to give permission for requests by child accounts
    within the same family.
    
    Bug: 957832
    Change-Id: I69165e5c9300a30b32516ec76cbe728365b3063b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872768
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Auto-Submit: Dan S <danan@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716185}

--- google_apis/gaia/fake_gaia.h
+++ google_apis/gaia/fake_gaia.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "google_apis/gaia/gaia_auth_consumer.h"
+#include "net/http/http_status_code.h"
 #include "url/gurl.h"
 
 namespace base {
@@ -155,6 +157,11 @@ class FakeGaia {
   // Returns an email that is filled into the the Email field (if any).
   const std::string& prefilled_email() { return prefilled_email_; }
 
+  void SetNextReAuthStatus(
+      GaiaAuthConsumer::ReAuthProofTokenStatus next_status) {
+    next_reauth_status_ = next_status;
+  }
+
  protected:
   // HTTP handler for /MergeSession.
   virtual void HandleMergeSession(
@@ -176,15 +183,28 @@ class FakeGaia {
   void SetOAuthCodeCookie(
       net::test_server::BasicHttpResponse* http_response) const;
 
-  // Formats a JSON response with the data in |value|.
+  // Formats a JSON response with the data in |value|, setting the http status
+  // to |status|.
   void FormatJSONResponse(const base::Value& value,
+                          net::HttpStatusCode status,
                           net::test_server::BasicHttpResponse* http_response);
+
+  // Formats a JSON response with the data in |value|, setting the http status
+  // to net::HTTP_OK.
+  void FormatOkJSONResponse(const base::Value& value,
+                            net::test_server::BasicHttpResponse* http_response);
 
   typedef base::Callback<void(
       const net::test_server::HttpRequest& request,
       net::test_server::BasicHttpResponse* http_response)>
           HttpRequestHandlerCallback;
   typedef std::map<std::string, HttpRequestHandlerCallback> RequestHandlerMap;
+
+  // Finds the handler for the specified |request_path| by prefix.
+  // Used as a backup for situations where an exact match doesn't
+  // find a match.
+  RequestHandlerMap::iterator FindHandlerByPathPrefix(
+      const std::string& request_path);
 
   // HTTP request handlers.
   void HandleProgramaticAuth(
@@ -230,6 +250,9 @@ class FakeGaia {
   void HandleGetCheckConnectionInfo(
       const net::test_server::HttpRequest& request,
       net::test_server::BasicHttpResponse* http_response);
+  void HandleGetReAuthProofToken(
+      const net::test_server::HttpRequest& request,
+      net::test_server::BasicHttpResponse* http_response);
 
   // Returns the access token associated with |auth_token| that matches the
   // given |client_id| and |scope_string|. If |scope_string| is empty, the first
@@ -255,7 +278,8 @@ class FakeGaia {
   bool issue_oauth_code_cookie_;
   RefreshTokenToDeviceIdMap refresh_token_to_device_id_map_;
   std::string prefilled_email_;
-
+  GaiaAuthConsumer::ReAuthProofTokenStatus next_reauth_status_ =
+      GaiaAuthConsumer::ReAuthProofTokenStatus::kSuccess;
   DISALLOW_COPY_AND_ASSIGN(FakeGaia);
 };
 
