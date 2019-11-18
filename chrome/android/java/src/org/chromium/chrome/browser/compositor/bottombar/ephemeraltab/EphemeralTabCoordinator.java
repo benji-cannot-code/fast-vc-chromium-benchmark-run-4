41f9d19d032e27a6ee997b3b3890ae49b959d1b5
Shakti Sahu <shaktisahu@chromium.org>
2019-11-18 23:19:19 +0000

    Ephemeral tab : Some UI polish
    This CL adds
    1 - Fixes default favicon to show the globe icon
    2 - Fixes vertical alignment of favicon
    3 - Adds the close button back
    4 - Makes open_in_new_tab button visible only when in full state
    
    Bug: 1025085, 1025084, 1025088, 1023659
    Change-Id: I2dff5b9949fb33dba4c0b6f009ca5ee92503d8d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918646
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716364}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
@@ -34,6 +34,7 @@ import org.chromium.components.security_state.ConnectionSecurityLevel;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.NavigationHandle;
 import org.chromium.content_public.browser.WebContentsObserver;
+import org.chromium.ui.UiUtils;
 import org.chromium.ui.base.PageTransition;
 
 /**
@@ -67,7 +68,13 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
         mBottomSheetController.addObserver(new EmptyBottomSheetObserver() {
             @Override
             public void onSheetStateChanged(int newState) {
-                if (newState == BottomSheetController.SheetState.HIDDEN) destroyContent();
+                if (newState == SheetState.HIDDEN) {
+                    destroyContent();
+                    return;
+                }
+
+                if (mSheetContent == null) return;
+                mSheetContent.showOpenInNewTabButton(newState == SheetState.FULL);
             }
         });
     }
@@ -142,6 +149,10 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
         }
     }
 
+    private void onCloseButtonClick() {
+        mBottomSheetController.hideContent(mSheetContent, /* animate= */ true);
+    }
+
     private boolean canPromoteToNewTab() {
         return !mActivity.isCustomTab();
     }
@@ -152,8 +163,8 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
     }
 
     private EphemeralTabSheetContent createSheetContent() {
-        mSheetContent = new EphemeralTabSheetContent(
-                mActivity, () -> openInNewTab(), () -> onToolbarClick(), getMaxSheetHeight());
+        mSheetContent = new EphemeralTabSheetContent(mActivity, this::openInNewTab,
+                this::onToolbarClick, this::onCloseButtonClick, getMaxSheetHeight());
 
         mActivity.getWindow().getDecorView().addOnLayoutChangeListener(this);
         return mSheetContent;
@@ -285,7 +296,6 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
     private static class FaviconLoader {
         private final Context mContext;
         private final FaviconHelper mFaviconHelper;
-        private final FaviconHelper.DefaultFaviconHelper mDefaultFaviconHelper;
         private final RoundedIconGenerator mIconGenerator;
         private final int mFaviconSize;
 
@@ -293,7 +303,6 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
         public FaviconLoader(Context context) {
             mContext = context;
             mFaviconHelper = new FaviconHelper();
-            mDefaultFaviconHelper = new FaviconHelper.DefaultFaviconHelper();
             mIconGenerator = FaviconUtils.createCircularIconGenerator(mContext.getResources());
             mFaviconSize =
                     mContext.getResources().getDimensionPixelSize(R.dimen.preview_tab_favicon_size);
@@ -312,8 +321,8 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
                     drawable = FaviconUtils.createRoundedBitmapDrawable(
                             mContext.getResources(), bitmap);
                 } else {
-                    drawable = mDefaultFaviconHelper.getDefaultFaviconDrawable(
-                            mContext.getResources(), iconUrl, true);
+                    drawable = UiUtils.getTintedDrawable(
+                            mContext, R.drawable.ic_globe_24dp, R.color.standard_mode_tint);
                 }
 
                 callback.onResult(drawable);
