d131022bf5c2393ff88ab0eccf9ab5edc7277a9d
Martin Kreichgauer <martinkr@google.com>
2019-11-18 18:18:33 +0000

    fido: remove WebAuthenticationCredentialManagement feature flag
    This feature launched default-enabled in M78.
    
    Fixed: 982516,955859
    Change-Id: If5f459eeebd5bee44572abc6f245143e818bf0a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919723
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#716214}

--- chrome/browser/resources/settings/privacy_page/security_keys_subpage.js
+++ chrome/browser/resources/settings/privacy_page/security_keys_subpage.js
@@ -12,16 +12,6 @@ Polymer({
 
   properties: {
     /** @private */
-    enableCredentialManagement_: {
-      type: Boolean,
-      readOnly: true,
-      value: function() {
-        return loadTimeData.getBoolean(
-            'enableSecurityKeysCredentialManagement');
-      }
-    },
-
-    /** @private */
     enableBioEnrollment_: {
       type: Boolean,
       readOnly: true,
