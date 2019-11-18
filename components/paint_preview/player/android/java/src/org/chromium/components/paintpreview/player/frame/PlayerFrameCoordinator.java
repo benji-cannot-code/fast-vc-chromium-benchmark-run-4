5b22b141a5a96bf619a6ec7fa846b16c1f339351
Mehran Mahmoudi <mahmoudi@chromium.org>
2019-11-18 20:05:08 +0000

    [Paint Preview] Player infrastructure
    This implements the infrastructure of the player component for Paint
    Preview.
    - PlayerManager is the only visible class to the outer
      world. Users can employ this class for creating a new instance of
      the Paint Preview player.
    - PlayerCompositorDelegateImpl/player_compositor_delegate_android.cc
      is responsible for talking to the rest of the Paint Preview
      component.
    - PaintPreviewFrame: This class represents a Paint Preview frame,
      including all of its sub-frames.
    - PlayerFrameCoordinator: Sets up an MVC sub-component for each frame
      in the paint preview. The reset of the classes for the sub-component
      will be added in a subsequent CL.
    
    Internal design docs:
      - High-level, explaining why we chose the multi-view hierarchy
      approach (http://go/fdt-player)
      - More details about how we're planning to implement the
      multi-view hierarchy approach: (http://go/fdt-player-2)
    
    Bug: 1017927
    Change-Id: I69b85d8418a64adda56964ed86c30d8edbf302a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1887236
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716262}

---
+++ components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/frame/PlayerFrameCoordinator.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.paintpreview.player.frame;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.view.View;
+
+import org.chromium.components.paintpreview.player.PlayerCompositorDelegate;
+
+/**
+ * Sets up the view and the logic behind it for a Paint Preview frame.
+ */
+public class PlayerFrameCoordinator {
+    /**
+     * Creates a {@link PlayerFrameMediator} and {@link PlayerFrameView} for this component and
+     * binds them together.
+     */
+    public PlayerFrameCoordinator(Context context, PlayerCompositorDelegate compositorDelegate,
+            long frameGuid, int contentWidth, int contentHeight, boolean canDetectZoom) {}
+
+    /**
+     * Adds a child {@link PlayerFrameCoordinator} to this class.
+     * @param subFrame The sub-frame's {@link PlayerFrameCoordinator}.
+     * @param clipRect The {@link Rect} in which this sub-frame should be shown in.
+     */
+    public void addSubFrame(PlayerFrameCoordinator subFrame, Rect clipRect) {}
+
+    public View getView() {
+        return null;
+    }
+}
