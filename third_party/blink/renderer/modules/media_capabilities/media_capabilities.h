098ba9b2303df537faa1e41d24a15bedf685b74d
Chris Cunningham <chcunningham@chromium.org>
2019-11-18 21:54:23 +0000

    MediaCapabilities: Remove decodingInfo() Web* types
    These are a relic of the blink/media divide that was recently flattened.
    Now we can just use the blink types.
    
    Removing the same for encodingInfo() will be done in a follow up CL.
    
    Change-Id: I7e53ce07322e3d97d4aa5ee0e679ed1c27ab67a9
    
    TBR: rtoy@chromium.org
    Change-Id: I7e53ce07322e3d97d4aa5ee0e679ed1c27ab67a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1914974
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Auto-Submit: Chrome Cunningham <chcunningham@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716325}

--- third_party/blink/renderer/modules/media_capabilities/media_capabilities.h
+++ third_party/blink/renderer/modules/media_capabilities/media_capabilities.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_codecs.h"  // for media::VideoCodecProfile
 #include "media/mojo/mojom/video_decode_perf_history.mojom-blink.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "third_party/blink/renderer/modules/media_capabilities/video_configuration.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
@@ -22,8 +23,6 @@ class MediaKeySystemAccess;
 class ScriptPromise;
 class ScriptPromiseResolver;
 class ScriptState;
-struct WebMediaDecodingConfiguration;
-struct WebVideoConfiguration;
 
 class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
@@ -41,11 +40,9 @@ class MODULES_EXPORT MediaCapabilities final : public ScriptWrappable {
 
   ScriptPromise GetEmeSupport(ScriptState*,
                               media::VideoCodecProfile,
-                              const MediaDecodingConfiguration*,
-                              const WebMediaDecodingConfiguration&);
-
+                              const MediaDecodingConfiguration*);
   void GetPerfInfo(media::VideoCodecProfile,
-                   base::Optional<WebVideoConfiguration>,
+                   const VideoConfiguration*,
                    ScriptPromiseResolver*,
                    MediaKeySystemAccess*);
 
