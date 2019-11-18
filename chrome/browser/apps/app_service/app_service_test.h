652ed625101a96d221907db1d1fd835c2538fed8
nancy <nancylingwang@chromium.org>
2019-11-18 23:41:20 +0000

    Remove InternalAppModelBuilder.
    AppServiceAppModelBuilder is used to replace InternalAppModelBuilder.
    AppService integration to ui/app_list has been enabled for a couple
    of months, so it should be safe to remove InternalAppModelBuilder.
    
    Unit tests are updated to use AppServiceAppModelBuilder. I will use a
    separate CL to move the internal_app_model_builder_unittest.cc to
    src/chrome/browser/ui/app_list/app_service/, and convert it to
    AppService unit tests. This CL still uses
    internal_app_model_builder_unittest.cc, as it is more clear to show
    the change.
    
    BUG=1016159
    
    Change-Id: I2d537aa940eae29201dd64bb7b345d3ae820d777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919534
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716371}

--- chrome/browser/apps/app_service/app_service_test.h
+++ chrome/browser/apps/app_service/app_service_test.h
@@ -26,6 +26,8 @@ class AppServiceTest {
 
   void SetUp(Profile* profile);
 
+  void UninstallAllApps(Profile* profile);
+
   std::string GetAppName(const std::string& app_id) const;
 
   // Allow AppService async callbacks to run.
--- chrome/browser/ui/app_list/app_list_syncable_service.cc
+++ chrome/browser/ui/app_list/app_list_syncable_service.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/crostini/crostini_app_model_builder.h"
 #include "chrome/browser/ui/app_list/extension_app_item.h"
 #include "chrome/browser/ui/app_list/extension_app_model_builder.h"
-#include "chrome/browser/ui/app_list/internal_app/internal_app_model_builder.h"
 #include "chrome/browser/ui/app_list/page_break_app_item.h"
 #include "chrome/browser/ui/app_list/page_break_constants.h"
 #include "chrome/common/chrome_features.h"
@@ -439,8 +438,6 @@ void AppListSyncableService::BuildModel() {
       crostini_apps_builder_ =
           std::make_unique<CrostiniAppModelBuilder>(controller);
     }
-    internal_apps_builder_ =
-        std::make_unique<InternalAppModelBuilder>(controller);
   }
 
   DCHECK(profile_);
@@ -454,7 +451,6 @@ void AppListSyncableService::BuildModel() {
       arc_apps_builder_->Initialize(this, profile_, model_updater_.get());
     if (crostini_apps_builder_.get())
       crostini_apps_builder_->Initialize(this, profile_, model_updater_.get());
-    internal_apps_builder_->Initialize(this, profile_, model_updater_.get());
   }
 
   HandleUpdateFinished();
@@ -1042,7 +1038,6 @@ void AppListSyncableService::Shutdown() {
     app_service_apps_builder_.reset();
     return;
   }
-  internal_apps_builder_.reset();
   crostini_apps_builder_.reset();
   arc_apps_builder_.reset();
   ext_apps_builder_.reset();
