7beb67455f321f0b472ac90dd0bd59d4e12ba09f
Nicholas Verne <nverne@chromium.org>
2019-11-18 05:29:12 +0000

    Rename CrostiniUpgradeView to CrostiniUpdateComponentView.
    This distinguishes it from the new WebUI-based upgrade dialog that upgrades
    a container's Linux distribution.
    
    Bug: 1025629
    Change-Id: Ic84f566f7ebe482da40df227222d42d4a1de7c9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1921024
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716072}

--- chrome/browser/ui/views/crostini/crostini_update_component_view.cc
+++ chrome/browser/ui/views/crostini/crostini_update_component_view.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/views/crostini/crostini_upgrade_view.h"
+#include "chrome/browser/ui/views/crostini/crostini_update_component_view.h"
 
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -24,41 +24,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-CrostiniUpgradeView* g_crostini_upgrade_view = nullptr;
+CrostiniUpdateComponentView* g_crostini_upgrade_view = nullptr;
 
 constexpr char kCrostiniUpgradeSourceHistogram[] = "Crostini.UpgradeSource";
 
 }  // namespace
 
-void crostini::ShowCrostiniUpgradeView(Profile* profile,
-                                       crostini::CrostiniUISurface ui_surface) {
+void crostini::ShowCrostiniUpdateComponentView(
+    Profile* profile,
+    crostini::CrostiniUISurface ui_surface) {
   base::UmaHistogramEnumeration(kCrostiniUpgradeSourceHistogram, ui_surface,
                                 crostini::CrostiniUISurface::kCount);
-  return CrostiniUpgradeView::Show(profile);
+  return CrostiniUpdateComponentView::Show(profile);
 }
 
-void CrostiniUpgradeView::Show(Profile* profile) {
+void CrostiniUpdateComponentView::Show(Profile* profile) {
   DCHECK(crostini::CrostiniFeatures::Get()->IsUIAllowed(profile));
   if (!g_crostini_upgrade_view) {
-    g_crostini_upgrade_view = new CrostiniUpgradeView;
+    g_crostini_upgrade_view = new CrostiniUpdateComponentView;
     CreateDialogWidget(g_crostini_upgrade_view, nullptr, nullptr);
   }
   g_crostini_upgrade_view->GetWidget()->Show();
 }
 
-int CrostiniUpgradeView::GetDialogButtons() const {
+int CrostiniUpdateComponentView::GetDialogButtons() const {
   return ui::DIALOG_BUTTON_OK;
 }
 
-base::string16 CrostiniUpgradeView::GetWindowTitle() const {
+base::string16 CrostiniUpdateComponentView::GetWindowTitle() const {
   return l10n_util::GetStringUTF16(IDS_CROSTINI_TERMINA_UPDATE_REQUIRED);
 }
 
-bool CrostiniUpgradeView::ShouldShowCloseButton() const {
+bool CrostiniUpdateComponentView::ShouldShowCloseButton() const {
   return false;
 }
 
-gfx::Size CrostiniUpgradeView::CalculatePreferredSize() const {
+gfx::Size CrostiniUpdateComponentView::CalculatePreferredSize() const {
   const int dialog_width = ChromeLayoutProvider::Get()->GetDistanceMetric(
                                DISTANCE_STANDALONE_BUBBLE_PREFERRED_WIDTH) -
                            margins().width();
@@ -66,11 +67,12 @@ gfx::Size CrostiniUpgradeView::CalculatePreferredSize() const {
 }
 
 // static
-CrostiniUpgradeView* CrostiniUpgradeView::GetActiveViewForTesting() {
+CrostiniUpdateComponentView*
+CrostiniUpdateComponentView::GetActiveViewForTesting() {
   return g_crostini_upgrade_view;
 }
 
-CrostiniUpgradeView::CrostiniUpgradeView() {
+CrostiniUpdateComponentView::CrostiniUpdateComponentView() {
   views::LayoutProvider* provider = views::LayoutProvider::Get();
   SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical,
@@ -87,6 +89,6 @@ CrostiniUpgradeView::CrostiniUpgradeView() {
   chrome::RecordDialogCreation(chrome::DialogIdentifier::CROSTINI_UPGRADE);
 }
 
-CrostiniUpgradeView::~CrostiniUpgradeView() {
+CrostiniUpdateComponentView::~CrostiniUpdateComponentView() {
   g_crostini_upgrade_view = nullptr;
 }
