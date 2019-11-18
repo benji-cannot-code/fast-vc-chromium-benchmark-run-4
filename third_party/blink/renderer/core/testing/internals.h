89604a024307be138ed09d2d532213b3fe680799
Xianzhu Wang <wangxianzhu@chromium.org>
2019-11-18 21:06:25 +0000

    [CompositeAfterPaint] Remove internals.elementLayerTreeAsText()
    The layer tree mode is not supported in CompositedAfterPaint.
    Remove it and rewrite the tests so that the tests will pass in
    CompositeAfterPaint.
    
    Change-Id: Ided24dc50b6804397777fec8fbce45d8d6cb3174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918295
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716300}

--- third_party/blink/renderer/core/testing/internals.h
+++ third_party/blink/renderer/core/testing/internals.h
@@ -345,10 +345,6 @@ class Internals final : public ScriptWrappable {
 
   String layerTreeAsText(Document*, unsigned flags, ExceptionState&) const;
   String layerTreeAsText(Document*, ExceptionState&) const;
-  String elementLayerTreeAsText(Element*,
-                                unsigned flags,
-                                ExceptionState&) const;
-  String elementLayerTreeAsText(Element*, ExceptionState&) const;
 
   bool scrollsWithRespectTo(Element*, Element*, ExceptionState&);
 
