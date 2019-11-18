f07fe2204b77e7312a14c3a75eeb846bebd3e184
Robbie McElrath <rmcelrath@chromium.org>
2019-11-18 21:24:04 +0000

    [AW] Always schedule variations seed download jobs when a seed is requested
    We don't currently schedule a seed download in WebView if the previous
    download occurred less than 24 hours ago, which can add an additional 24
    hours to the time between seed downloads. This CL reimplements the 24
    hour throttle to be a delay in the scheduled job, meaning a job will
    always be scheduled when a new seed is requested, but Android will only
    run the job once 24 hours have past since the last download.
    
    This CL also adds a missing hamcrest dependency to
    //aw/test:webview_instrumentation_test_apk
    
    Test: ninja -C out/aw webview_instrumentation_test_apk
    Test: out/aw/bin/run_webview_instrumentation_test_apk -f 'AwVariationsSeedFetcherTest#*'
    Bug: 979075
    Change-Id: I0ea1cc8197f66b77634e8de2b145fe4228c74067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906121
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716305}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedHolder.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedHolder.java
@@ -18,6 +18,7 @@ import org.chromium.components.variations.firstrun.VariationsSeedFetcher.SeedInf
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Date;
 
 /**
  * VariationsSeedHolder is a singleton which manages the local copy of the variations seed - both
@@ -148,7 +149,7 @@ public class VariationsSeedHolder {
 
     @VisibleForTesting
     public void scheduleFetchIfNeeded() {
-        AwVariationsSeedFetcher.scheduleIfNeeded();
+        AwVariationsSeedFetcher.scheduleIfNeeded(new Date().getTime());
     }
 
     // overridden by tests
