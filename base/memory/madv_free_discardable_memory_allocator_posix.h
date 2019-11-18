8566ee2316b9cdbaafc0822e958a0440029966c4
Gordon Guan <gordonguan@google.com>
2019-11-18 20:23:31 +0000

    Add background memory tracing for MADV_FREE DiscardableMemory
    At the moment, MADV_FREE DiscardableMemory does not report
    discardable memory usage for a background dump. Register MADV_FREE
    discardable memory allocators as memory dump providers in order to
    provide a total for background dumps.
    
    Bug: 1014513
    Change-Id: I8b22ca480ff3d0ef52e5b26358975283612e38f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1907266
    Commit-Queue: Gordon Guan <gordonguan@google.com>
    Reviewed-by: ssid <ssid@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716271}

--- base/memory/madv_free_discardable_memory_allocator_posix.h
+++ base/memory/madv_free_discardable_memory_allocator_posix.h
@@ -19,11 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/discardable_memory.h"
 #include "base/memory/discardable_memory_allocator.h"
 #include "base/memory/madv_free_discardable_memory_posix.h"
+#include "base/trace_event/memory_dump_provider.h"
 #include "build/build_config.h"
 
 namespace base {
 class BASE_EXPORT MadvFreeDiscardableMemoryAllocatorPosix
-    : public DiscardableMemoryAllocator {
+    : public DiscardableMemoryAllocator,
+      public base::trace_event::MemoryDumpProvider {
  public:
   MadvFreeDiscardableMemoryAllocatorPosix();
   ~MadvFreeDiscardableMemoryAllocatorPosix() override;
@@ -37,6 +39,9 @@ class BASE_EXPORT MadvFreeDiscardableMemoryAllocatorPosix
     // Do nothing, since MADV_FREE discardable memory does not keep any memory
     // overhead that can be released.
   }
+
+  bool OnMemoryDump(const trace_event::MemoryDumpArgs& args,
+                    trace_event::ProcessMemoryDump* pmd) override;
 
  private:
   std::atomic<size_t> bytes_allocated_{0};
