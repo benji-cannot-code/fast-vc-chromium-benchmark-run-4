fecac7539a0e9f4d312a9b79f0eee974b527f03e
nancy <nancylingwang@chromium.org>
2019-11-18 04:20:02 +0000

    Remove the const for Window method in InstanceUpdate.
    InstanceUpdate's Window method is used to get the pointer of window
    in OnInstanceUpdate callback. If const, it prevents the operation on
    the window, e.g window->SetProperty in ui, so remove the const.
    
    BUG=1011235
    
    Change-Id: I14deed57307a6dd465f0c0f3cae30c86cce6279e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1921008
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716067}

--- chrome/services/app_service/public/cpp/instance_update.cc
+++ chrome/services/app_service/public/cpp/instance_update.cc
@@ -87,7 +87,7 @@ const std::string& InstanceUpdate::AppId() const {
   return delta_ ? delta_->AppId() : state_->AppId();
 }
 
-const aura::Window* InstanceUpdate::Window() const {
+aura::Window* InstanceUpdate::Window() const {
   return delta_ ? delta_->Window() : state_->Window();
 }
 
