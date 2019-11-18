f4fa7fbd41d1f22b1b2f564faf4f91a3345b752e
Sigurdur Asgeirsson <siggi@chromium.org>
2019-11-18 20:18:34 +0000

    Capture the state of local and message queueuing for reporting.
    This allows reporting the state of the message pipe and local queue
    as the crash condition tripped, rather than the state of the world
    at the time when the crash report finally captures it.
    
    Bug: 1017827
    Change-Id: I40cba719cf0541cc10f0da4362a8708f167676ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918496
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#716269}

--- mojo/public/cpp/bindings/lib/message_quota_checker.h
+++ mojo/public/cpp/bindings/lib/message_quota_checker.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/memory/ref_counted.h"
+#include "base/optional.h"
 #include "base/synchronization/lock.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 
@@ -72,7 +73,9 @@ class COMPONENT_EXPORT(MOJO_CPP_BINDINGS) MessageQuotaChecker
   static scoped_refptr<MessageQuotaChecker> MaybeCreateImpl(
       const Configuration& config);
 
-  size_t GetCurrentQuotaStatus();
+  // Returns the amount of unread message quota currently used if there is
+  // an associated message pipe.
+  base::Optional<size_t> GetCurrentMessagePipeQuota();
   void QuotaCheckImpl(size_t num_enqueued);
 
   const Configuration* config_;
@@ -95,7 +98,8 @@ struct MessageQuotaChecker::Configuration {
   size_t sample_rate = 0u;
   size_t unread_message_count_quota = 0u;
   size_t crash_threshold = 0u;
-  void (*maybe_crash_function)(size_t quota_used);
+  void (*maybe_crash_function)(size_t quota_used,
+                               base::Optional<size_t> message_pipe_quota_used);
 };
 
 }  // namespace internal
