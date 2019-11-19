cce02d7e95df234068e0b4d682d0ccfe1aa58bce
Chris Mumford <cmumford@chromium.org>
2019-11-19 18:50:04 +0000

    Using base::OnceClosure for base::PendingTask.
    base::PendingTask() accepts a base::OnceClosure so switching from
    base::Closure() to base::OnceClosure() when creating a PendingTask.
    
    Bug: 1007835
    Change-Id: Ib3c354fb93c6df7a8bb29aa73b430e033da0a016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918208
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/master@{#716721}

--- third_party/blink/renderer/platform/scheduler/main_thread/idle_time_estimator_unittest.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/idle_time_estimator_unittest.cc
@@ -59,7 +59,7 @@ class IdleTimeEstimatorTest : public testing::Test {
   void SimulateFrameWithOneCompositorTask(int compositor_time) {
     base::TimeDelta non_idle_time =
         base::TimeDelta::FromMilliseconds(compositor_time);
-    base::PendingTask task(FROM_HERE, base::Closure());
+    base::PendingTask task(FROM_HERE, base::OnceClosure());
     estimator_->WillProcessTask(task);
     task_environment_.FastForwardBy(non_idle_time);
     estimator_->DidCommitFrameToCompositor();
@@ -74,7 +74,7 @@ class IdleTimeEstimatorTest : public testing::Test {
         base::TimeDelta::FromMilliseconds(compositor_time1);
     base::TimeDelta non_idle_time2 =
         base::TimeDelta::FromMilliseconds(compositor_time2);
-    base::PendingTask task(FROM_HERE, base::Closure());
+    base::PendingTask task(FROM_HERE, base::OnceClosure());
     estimator_->WillProcessTask(task);
     task_environment_.FastForwardBy(non_idle_time1);
     estimator_->DidProcessTask(task);
@@ -152,7 +152,7 @@ TEST_F(IdleTimeEstimatorTest, IgnoresNestedTasks) {
   SimulateFrameWithOneCompositorTask(5);
   SimulateFrameWithOneCompositorTask(5);
 
-  base::PendingTask task(FROM_HERE, base::Closure());
+  base::PendingTask task(FROM_HERE, base::OnceClosure());
   estimator_->WillProcessTask(task);
   SimulateFrameWithTwoCompositorTasks(4, 4);
   SimulateFrameWithTwoCompositorTasks(4, 4);
