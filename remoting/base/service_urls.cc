370a1475c0ea23e9ab695cf623b0b6d9c22c5d9e
Joe Downing <joedow@google.com>
2019-11-19 22:15:15 +0000

    Use autopush services for Debug hosts
    This CL updates host to use Autopush service URLs for the Chromoting
    and FTL backends for local development.  Hosts heartbeating through
    autopush will still be available through the prod website however
    having the host heartbeat and communicate through autopush means
    we can apply environment-specific flags and / or test out new
    codepaths before we enable them in prod.
    
    Change-Id: I01bbd8e19f054610f5835aa67595f1e7c6b04c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1700617
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716799}

--- remoting/base/service_urls.cc
+++ remoting/base/service_urls.cc
@@ -12,8 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Configurable service data.
 constexpr char kGcdBaseUrl[] = "https://www.googleapis.com/clouddevices/v1";
 constexpr char kGcdJid[] = "clouddevices.gserviceaccount.com";
+
+// Debug builds should default to the autopush environment (can be configured
+// via cmd line switch).  Release builds will point to the prod environment.
+#if defined(NDEBUG)
 constexpr char kFtlServerEndpoint[] = "instantmessaging-pa.googleapis.com";
 constexpr char kRemotingServerEndpoint[] = "remotedesktop-pa.googleapis.com";
+#else
+constexpr char kFtlServerEndpoint[] =
+    "tachyon-playground-autopush-grpc.sandbox.googleapis.com";
+constexpr char kRemotingServerEndpoint[] =
+    "autopush-remotedesktop-pa.sandbox.googleapis.com";
+#endif
 
 // Command line switches.
 #if !defined(NDEBUG)
