69e7ea8b538c3d01b88bffc95ad9a1cdc6bed5f7
zhaoyangli <zhaoyangli@chromium.org>
2019-11-19 02:45:09 +0000

    Change FakeChromeIdentity to EDO pass-by-value class
    This change will make EG2 tests work when FakeChromeIdentity is
    passed between test and app processes.
    - Overrode (BOOL)edo_isEDOValueType to let EDO framework pass it by
    value.
    - Made the class confirm to NSSecureCoding protocol as its required
    by EDO pass-by-value.
    - Overrode -isEqual and -hash methods to compare by values so that
    comparing between copied objects will work.
    
    Bug: 1015907, 922813
    
    Change-Id: Iefadf89b401dabcee53e44ff1c6ce903cc979de4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1922336
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716467}

--- ios/public/provider/chrome/browser/signin/fake_chrome_identity.h
+++ ios/public/provider/chrome/browser/signin/fake_chrome_identity.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
 
 // A fake ChromeIdentity used for testing.
-@interface FakeChromeIdentity : ChromeIdentity
+@interface FakeChromeIdentity : ChromeIdentity <NSSecureCoding>
 
 // Returns a ChromeIdentity based on |email|, |gaiaID| and |name|.
 // The |hashedGaiaID| property will be derived from |name|.
