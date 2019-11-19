567c80c7cf2945871cc8b7c44951683e2634722a
Matt Falkenhagen <falken@chromium.org>
2019-11-19 22:52:36 +0000

    Convert ipc to Once/Repeating variants of Bind/Callback.
    Bug: 1007797
    Change-Id: I20d470fa136ee6c7dc997648725440a2fc89c65b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1923968
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716806}

--- ipc/ipc_channel_nacl.cc
+++ ipc/ipc_channel_nacl.cc
@@ -84,8 +84,9 @@ class ChannelNacl::ReaderThreadRunner
   //                      above callbacks.
   ReaderThreadRunner(
       int pipe,
-      base::Callback<void(std::unique_ptr<MessageContents>)> data_read_callback,
-      base::Callback<void()> failure_callback,
+      base::RepeatingCallback<void(std::unique_ptr<MessageContents>)>
+          data_read_callback,
+      base::RepeatingCallback<void()> failure_callback,
       scoped_refptr<base::SingleThreadTaskRunner> main_task_runner);
 
   // DelegateSimpleThread implementation. Reads data from the pipe in a loop
@@ -94,8 +95,9 @@ class ChannelNacl::ReaderThreadRunner
 
  private:
   int pipe_;
-  base::Callback<void(std::unique_ptr<MessageContents>)> data_read_callback_;
-  base::Callback<void ()> failure_callback_;
+  base::RepeatingCallback<void(std::unique_ptr<MessageContents>)>
+      data_read_callback_;
+  base::RepeatingCallback<void()> failure_callback_;
   scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
 
   DISALLOW_COPY_AND_ASSIGN(ReaderThreadRunner);
@@ -103,8 +105,9 @@ class ChannelNacl::ReaderThreadRunner
 
 ChannelNacl::ReaderThreadRunner::ReaderThreadRunner(
     int pipe,
-    base::Callback<void(std::unique_ptr<MessageContents>)> data_read_callback,
-    base::Callback<void()> failure_callback,
+    base::RepeatingCallback<void(std::unique_ptr<MessageContents>)>
+        data_read_callback,
+    base::RepeatingCallback<void()> failure_callback,
     scoped_refptr<base::SingleThreadTaskRunner> main_task_runner)
     : pipe_(pipe),
       data_read_callback_(data_read_callback),
@@ -164,8 +167,10 @@ bool ChannelNacl::Connect() {
   // ReaderThreadRunner.
   reader_thread_runner_.reset(new ReaderThreadRunner(
       pipe_,
-      base::Bind(&ChannelNacl::DidRecvMsg, weak_ptr_factory_.GetWeakPtr()),
-      base::Bind(&ChannelNacl::ReadDidFail, weak_ptr_factory_.GetWeakPtr()),
+      base::BindRepeating(&ChannelNacl::DidRecvMsg,
+                          weak_ptr_factory_.GetWeakPtr()),
+      base::BindRepeating(&ChannelNacl::ReadDidFail,
+                          weak_ptr_factory_.GetWeakPtr()),
       base::ThreadTaskRunnerHandle::Get()));
   reader_thread_.reset(
       new base::DelegateSimpleThread(reader_thread_runner_.get(),
