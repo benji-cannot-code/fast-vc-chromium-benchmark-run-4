6c9ae6b46df60039870a10f3176c6b5824a8557e
Manish Goregaokar <manish@mozilla.com>
2019-11-19 22:49:15 +0000

    Expose immersive-ar enum value in all cases
    Bug: 1025992
    Change-Id: If19cde37384656652a3dcbbf5331c13c794241f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924886
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716805}

--- third_party/blink/renderer/modules/xr/xr.cc
+++ third_party/blink/renderer/modules/xr/xr.cc
@@ -656,9 +656,7 @@ ScriptPromise XR::InternalIsSessionSupported(ScriptState* script_state,
 
   if (session_mode == XRSession::kModeImmersiveAR &&
       !RuntimeEnabledFeatures::WebXRARModuleEnabled(doc)) {
-    query->RejectWithTypeError(
-        String::Format(kImmersiveArModeNotValid, "supportsSession"),
-        &exception_state);
+    query->Resolve(false);
     return promise;
   }
 
