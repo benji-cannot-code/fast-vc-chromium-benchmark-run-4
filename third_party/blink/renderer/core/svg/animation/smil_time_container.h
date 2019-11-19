d33d218526e1657182c326689d4df87077261614
Fredrik Söderquist <fs@opera.com>
2019-11-19 16:00:48 +0000

    Externalize the SMIL priority queue priority from SVGSMILElement
    Store the priority in the PriorityQueue instead by using a std::pair
    with a priority type and the element type. This means we no longer have
    to worry about changing the priority from under the queue's feet.
    
    Bug: 998526
    Change-Id: I81238812e8cd8378bb1cd72e5a72050271d94e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1890084
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716638}

--- third_party/blink/renderer/core/svg/animation/smil_time_container.h
+++ third_party/blink/renderer/core/svg/animation/smil_time_container.h
@@ -56,7 +56,7 @@ class SMILTimeContainer final : public GarbageCollected<SMILTimeContainer> {
   ~SMILTimeContainer();
 
   void Schedule(SVGSMILElement*, SVGElement*, const QualifiedName&);
-  void Reschedule(SVGSMILElement*);
+  void Reschedule(SVGSMILElement*, SMILTime interval_time);
   void Unschedule(SVGSMILElement*, SVGElement*, const QualifiedName&);
 
   // Returns the time we are currently updating.
@@ -118,7 +118,6 @@ class SMILTimeContainer final : public GarbageCollected<SMILTimeContainer> {
   void UpdateAnimationsAndScheduleFrameIfNeeded(SMILTime elapsed);
   void RemoveUnusedKeys();
   void ResetIntervals();
-  SVGSMILElement* GetNextReady(SMILTime presentation_time) const;
   void UpdateIntervals(SMILTime presentation_time);
   void UpdateAnimationTimings(SMILTime elapsed);
   void ApplyAnimationValues(SMILTime elapsed);
@@ -151,9 +150,7 @@ class SMILTimeContainer final : public GarbageCollected<SMILTimeContainer> {
   TaskRunnerTimer<SMILTimeContainer> animation_policy_once_timer_;
 
   AnimationsMap scheduled_animations_;
-
-  struct NextIntervalTimeLess;
-  PriorityQueue<SVGSMILElement, NextIntervalTimeLess> priority_queue_;
+  PriorityQueue<SMILTime, SVGSMILElement> priority_queue_;
 
   Member<SVGSVGElement> owner_svg_element_;
 
--- third_party/blink/renderer/core/svg/animation/svg_smil_element.cc
+++ third_party/blink/renderer/core/svg/animation/svg_smil_element.cc
@@ -188,7 +188,6 @@ SVGSMILElement::SVGSMILElement(const QualifiedName& tag_name, Document& doc)
       is_scheduled_(false),
       interval_(SMILInterval::Unresolved()),
       previous_interval_(SMILInterval::Unresolved()),
-      next_interval_time_(SMILTime::Unresolved()),
       active_state_(kInactive),
       restart_(kRestartAlways),
       fill_(kFillRemove),
@@ -258,7 +257,6 @@ void SVGSMILElement::Reset() {
   is_waiting_for_first_interval_ = true;
   interval_ = SMILInterval::Unresolved();
   previous_interval_ = SMILInterval::Unresolved();
-  next_interval_time_ = SMILTime::Earliest();
   last_progress_ = {0.0f, 0};
 }
 
@@ -836,14 +834,15 @@ void SVGSMILElement::InstanceListChanged() {
   DCHECK(!previous_presentation_time.IsUnresolved());
   const bool was_active = GetActiveState() == kActive;
   UpdateInterval(previous_presentation_time);
-  UpdateNextIntervalTime(previous_presentation_time);
   if (was_active && interval_.BeginsAfter(previous_presentation_time)) {
     active_state_ = DetermineActiveState(previous_presentation_time);
     if (GetActiveState() != kActive)
       EndedActiveInterval();
   }
-  if (time_container_)
-    time_container_->Reschedule(this);
+  if (time_container_) {
+    time_container_->Reschedule(
+        this, ComputeNextIntervalTime(previous_presentation_time));
+  }
 }
 
 void SVGSMILElement::DiscardOrRevalidateCurrentInterval(
@@ -917,10 +916,6 @@ void SVGSMILElement::UpdateInterval(SMILTime presentation_time) {
   SetNewInterval(next_interval);
 }
 
-void SVGSMILElement::UpdateNextIntervalTime(SMILTime presentation_time) {
-  next_interval_time_ = ComputeNextIntervalTime(presentation_time);
-}
-
 void SVGSMILElement::AddedToTimeContainer() {
   DCHECK(time_container_);
   // Update the interval to the time just before the current presentation
@@ -929,9 +924,9 @@ void SVGSMILElement::AddedToTimeContainer() {
   SMILTime previous_presentation_time =
       time_container_->CurrentDocumentTime() - SMILTime::Epsilon();
   UpdateInterval(previous_presentation_time);
-  UpdateNextIntervalTime(previous_presentation_time);
   active_state_ = DetermineActiveState(previous_presentation_time);
-  time_container_->Reschedule(this);
+  time_container_->Reschedule(
+      this, ComputeNextIntervalTime(previous_presentation_time));
 
   // If there's an active interval, then revalidate the animation value.
   if (GetActiveState() != kInactive)
