bf2c574a8fd33d646a949486b6962f29e4d432e4
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-11-19 12:28:03 +0000

    Revert "[Android][TouchToFill] Prevent double-dismissing the bottom sheet"
    This reverts commit 56d45f06b41ea68f253a69967b34749bf7e48dcb.
    
    Reason for revert: CL apparently causes the keyboard to not open as
    expected (see https://crbug.com/1025731). Reverting it shouldn't
    reintroduce https://crbug.com/1021550 which was addressed by a fix
    to the underlying component (see https://crbug.com/1021885).
    
    Original change's description:
    > [Android][TouchToFill] Prevent double-dismissing the bottom sheet
    > 
    > If the touch to fill sheet is dismissed, it triggers a second call to
    > BottomSheetController#hideContent.
    > 
    > That call triggers an observer in the BottomContainer again which then
    > moves itself out of the visible view port.
    > (The latter can only happen since a division by zero bypasses some guard
    > conditions. It might make sense to abort attempts at repeatedly
    > dismissing the bottomsheet but this exceeds the scope of this CL.)
    > 
    > Bug: 1021550
    > Change-Id: Ida2b6bc9ffd18513419bed40b29ca84528310907
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899882
    > Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    > Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#712703}
    
    TBR=jdoerrie@chromium.org,fhorschig@chromium.org
    
    Bug: 1021550,1025731
    Change-Id: I9eee8d4e38483d1f03d6c38af7dd7628fc6ddbe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919927
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716589}

--- chrome/browser/touch_to_fill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillView.java
+++ chrome/browser/touch_to_fill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/TouchToFillView.java
@@ -86,7 +86,7 @@ class TouchToFillView implements BottomSheetContent {
         if (isVisible) {
             mBottomSheetController.addObserver(mBottomSheetObserver);
             mBottomSheetController.requestShowContent(this, true);
-        } else if (mBottomSheetController.isSheetOpen()) {
+        } else {
             mBottomSheetController.hideContent(this, true);
         }
     }
