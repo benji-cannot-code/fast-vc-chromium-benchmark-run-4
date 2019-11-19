9118ada5e697ff2984cc23e7be5996e889ac3db3
Sammie Quon <sammiequon@google.com>
2019-11-19 20:08:32 +0000

    overview: Fix minimized windows being squashed after snap.
    Bug was introduced by fixed aspect ratio work. This CL handles when
    minimized windows need to be clipped.
    
    Undos the squashed windows by transforming them to the unclipped bounds,
    then clips them.
    
    Test: added test
    Change-Id: I1e5a8fa5f624d85f8cf161d96915a8bb19cb1672
    Fixed: 1024077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1922190
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716754}

--- ash/wm/overview/overview_item.h
+++ ash/wm/overview/overview_item.h
@@ -275,6 +275,7 @@ class ASH_EXPORT OverviewItem : public views::ButtonListener,
  private:
   friend class OverviewSessionRoundedCornerTest;
   friend class OverviewSessionTest;
+  FRIEND_TEST_ALL_PREFIXES(SplitViewOverviewSessionTest, Clipping);
   FRIEND_TEST_ALL_PREFIXES(SplitViewOverviewSessionTest,
                            OverviewUnsnappableIndicatorVisibility);
 
