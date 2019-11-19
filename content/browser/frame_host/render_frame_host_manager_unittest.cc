3a7a32a9f4838dda23284bc9106e85b38c4c4793
Daniel Cheng <dcheng@chromium.org>
2019-11-19 18:43:24 +0000

    Use base::FastHash() in //content
    base::Hash() is deprecated and is being replaced by base::FastHash().
    The output of base::FastHash() is only fixed within the lifetime of a
    single process, so if the code depends on the mapping of inputs to hash
    outputs not changing over time, base::PersistentHash() must be used
    instead.
    
    Bug: 1025358
    Change-Id: I9b1c488a0246b55e9f3b32a827ad2845533dd692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1922731
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716716}

--- content/browser/frame_host/render_frame_host_manager_unittest.cc
+++ content/browser/frame_host/render_frame_host_manager_unittest.cc
@@ -149,7 +149,7 @@ class RenderFrameHostManagerTestWebUIControllerFactory
     // If WebUI creation is enabled for the test and this is a WebUI URL,
     // returns a mock WebUI type.
     if (HasWebUIScheme(url)) {
-      return reinterpret_cast<WebUI::TypeID>(base::Hash(url.host()));
+      return reinterpret_cast<WebUI::TypeID>(base::FastHash(url.host()));
     }
     return WebUI::kNoWebUI;
   }
