470b6fec81476787342d2de9661fd82d900ae6d8
kylechar <kylechar@chromium.org>
2019-11-19 03:10:52 +0000

    Fix assigning NULL to scoped_refptr
    Update code where NULL or 0 is assigned to a scoped_refptr to reset it. Just
    call reset() instead. If operator=(std::nullptr_t) is added these assignments
    become ambiguous.
    
    This CL was uploaded by git cl split.
    
    R=sadrul@chromium.org
    
    Bug: 1024981
    Change-Id: Ia7d9cd0d76794292be90704b14682ba656c45996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918148
    Auto-Submit: kylechar <kylechar@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716476}

--- ui/compositor/layer_animator_unittest.cc
+++ ui/compositor/layer_animator_unittest.cc
@@ -1707,7 +1707,7 @@ TEST(LayerAnimatorTest, AddObserverExplicit) {
 
   animator->StartAnimation(sequence);
 
-  animator = NULL;
+  animator.reset();
 
   EXPECT_EQ(observer.last_aborted_sequence(), sequence);
 }
@@ -2367,7 +2367,7 @@ TEST(LayerAnimatorTest, ImplicitObserversAtAnimatorDestruction) {
 
   EXPECT_FALSE(observer_notify.animations_completed());
   EXPECT_FALSE(observer_do_not_notify.animations_completed());
-  animator = NULL;
+  animator.reset();
   EXPECT_TRUE(observer_notify.animations_completed());
   EXPECT_TRUE(observer_notify.WasAnimationAbortedForProperty(
       LayerAnimationElement::BRIGHTNESS));
