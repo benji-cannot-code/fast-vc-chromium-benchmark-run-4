140c8d356f95e71b6b0a8ff7256ded6c4ad7f1d9
Kristi Park <kristipark@chromium.org>
2019-11-19 19:45:12 +0000

    Set up boilerplate for screenshot Image Editor DFM
    The screenshot image editor will utilize the Ink image editor
    library, which is pulled downstream in Clank and needs to be shipped
    in a DFM.
    
    This is the boilerplate for the upstream implementation of the DFM.
    
    See go/chrome-screenshotz-dd
    
    Bug: 1024586
    Change-Id: I9588dabfc0cacd86dfd4ceaf6ef78a71c45ce2c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918219
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716747}

---
+++ chrome/android/modules/image_editor/internal/java/src/org/chromium/chrome/modules/image_editor/ImageEditorCoordinatorImpl.java
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.modules.image_editor;
+
+import android.app.Activity;
+
+/**
+ * Upstream implementation for ImageEditorCoordinator. Does nothing. Actual implementation
+ * lives downstream.
+ */
+public class ImageEditorCoordinatorImpl implements ImageEditorCoordinator {
+    public ImageEditorCoordinatorImpl(Activity activity) {}
+}
