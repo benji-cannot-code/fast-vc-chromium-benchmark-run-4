46632a0fe4fb5ec69d9789fae7a6ed94ada6d47f
Tarun Bansal <tbansal@chromium.org>
2019-11-19 18:09:34 +0000

    Refactor test code for defer and resource blocking
    Also, add a test for the case where the hints server does
    not provide any subresource patterns to block for resource
    loading hints.
    
    Change-Id: Ib625fc6024e4e4cf617449389057d588f56a4c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1922599
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716701}

--- chrome/browser/previews/defer_all_script_priority_browsertest.cc
+++ chrome/browser/previews/defer_all_script_priority_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_impl.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/metrics/subprocess_metrics_provider.h"
+#include "chrome/browser/previews/previews_test_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -38,33 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
 #include "third_party/blink/public/common/features.h"
-
-namespace {
-
-// Retries fetching |histogram_name| until it contains at least |count| samples.
-void RetryForHistogramUntilCountReached(base::HistogramTester* histogram_tester,
-                                        const std::string& histogram_name,
-                                        size_t count) {
-  while (true) {
-    base::ThreadPoolInstance::Get()->FlushForTesting();
-    base::RunLoop().RunUntilIdle();
-
-    content::FetchHistogramsFromChildProcesses();
-    SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
-
-    const std::vector<base::Bucket> buckets =
-        histogram_tester->GetAllSamples(histogram_name);
-    size_t total_count = 0;
-    for (const auto& bucket : buckets) {
-      total_count += bucket.count;
-    }
-    if (total_count >= count) {
-      break;
-    }
-  }
-}
-
-}  // namespace
 
 // The first parameter selects whether the DeferAllScript optimization type is
 // enabled, and the second parameter selects whether
@@ -263,13 +237,6 @@ class DeferAllScriptPriorityBrowserTest
 INSTANTIATE_TEST_SUITE_P(,
                          DeferAllScriptPriorityBrowserTest,
                          ::testing::Bool());
-
-// Avoid flakes and issues on non-applicable platforms.
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_CHROMEOS)
-#define DISABLE_ON_WIN_MAC_CHROMEOS(x) DISABLED_##x
-#else
-#define DISABLE_ON_WIN_MAC_CHROMEOS(x) x
-#endif
 
 // Fetches an HTML weboage that fetches CSS files followed by an external
 // JavaScript file. Verifies that the fetching of the JavaScript files is
