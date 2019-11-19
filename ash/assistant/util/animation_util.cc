ee6d49c53149f31fbad5092288af2afb4cf9dcf3
Jeroen Dhollander <jeroendh@google.com>
2019-11-19 01:05:20 +0000

    Ensure Assistant greeting label is hidden when opacity is 0.
    Previously when the Assistant greeting label was not needed it remained
    visible but with an opacity of 0. This causes it to still be part of the
    accessibility tree, and the user could still tab over it when enabling
    ChromeVox.
    
    To solve this a new |FadeOutAndHide| method was introduced that will
    fade out a view and set its visibility to |false| when the fade out is
    completed.
    
    This method is now used both in the embedded and standalone Assistant
    UI.
    
    Tests: Manually tested and added tests to |ash_unittest|.
    Bug: b/142672872
    Change-Id: I2e734ef13b90f0e43dfda52d12c59b7deff5633f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913615
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716413}

--- ash/assistant/util/animation_util.cc
+++ ash/assistant/util/animation_util.cc
@@ -6,14 +6,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/util/animation_util.h"
 
 #include "base/time/time.h"
+#include "ui/compositor/callback_layer_animation_observer.h"
 #include "ui/compositor/layer_animation_element.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/compositor/layer_animation_sequence.h"
 #include "ui/compositor/layer_animator.h"
+#include "ui/views/view.h"
 
 namespace ash {
 namespace assistant {
 namespace util {
+
+namespace {
+
+// Returns an observer that will hide |view| when it fires.
+// The observer will delete itself after firing (by returning true).
+ui::CallbackLayerAnimationObserver* BuildObserverToHideView(views::View* view) {
+  return new ui::CallbackLayerAnimationObserver(base::Bind(
+      /*animation_ended_callback=*/
+      [](views::View* view,
+         const ui::CallbackLayerAnimationObserver& observer) {
+        // If the animation was aborted, we just return true to delete our
+        // observer. No further action is needed, as |view| might no longer
+        // be valid.
+        if (observer.aborted_count())
+          return true;
+
+        view->SetVisible(false);
+        // We return true to delete our observer.
+        return true;
+      },
+      view));
+}
+
+}  // namespace
 
 ::ui::LayerAnimationSequence* CreateLayerAnimationSequence(
     std::unique_ptr<::ui::LayerAnimationElement> a,
@@ -93,6 +119,15 @@ void StartLayerAnimationSequence(
   layer_animator->StartAnimation(layer_animation_sequence);
 }
 
+void StartLayerAnimationSequence(
+    views::View* view,
+    ::ui::LayerAnimationSequence* layer_animation_sequence,
+    ::ui::LayerAnimationObserver* observer) {
+  DCHECK(view->layer());
+  StartLayerAnimationSequence(view->layer()->GetAnimator(),
+                              layer_animation_sequence, observer);
+}
+
 void StartLayerAnimationSequencesTogether(
     ::ui::LayerAnimator* layer_animator,
     const std::vector<ui::LayerAnimationSequence*>& layer_animation_sequences,
@@ -104,6 +139,22 @@ void StartLayerAnimationSequencesTogether(
     }
   }
   layer_animator->StartTogether(layer_animation_sequences);
+}
+
+void FadeOutAndHide(views::View* view, base::TimeDelta fade_out_duration) {
+  // Note: We are deliberately not simply setting the layer's visibility to
+  // false by ending the animation with a |CreateVisibilityElement|.
+  // The reason is that this would hide the layer but not the view, leaving the
+  // view in the accessibility tree, causing issues like b/142672872.
+
+  auto* animation_observer = BuildObserverToHideView(view);
+
+  StartLayerAnimationSequence(view,
+                              CreateLayerAnimationSequence(
+                                  CreateOpacityElement(0.f, fade_out_duration)),
+                              animation_observer);
+
+  animation_observer->SetActive();
 }
 
 }  // namespace util
