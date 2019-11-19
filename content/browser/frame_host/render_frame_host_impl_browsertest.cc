5f76b4fa1346076ad2ae2848d2a75dc1c759fb49
Miyoung Shin <myid.shin@igalia.com>
2019-11-19 03:51:26 +0000

    Convert BrowserTarget client to use BrowserInterfaceBroker
    This CL converts BrowserTarget mojom interface client in content to use
    BrowserInterfaceBroker. PingTestWebUIController gotten from WebUI of
    WebContents is used to register its binder.
    
    Bug: 1006361, 1002647
    Change-Id: I9bbc562a0bf16a6ebf546197a6f5d3ba4eef7ca5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1921593
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#716486}

--- content/browser/frame_host/render_frame_host_impl_browsertest.cc
+++ content/browser/frame_host/render_frame_host_impl_browsertest.cc
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/url_loader_interceptor.h"
 #include "content/shell/browser/shell.h"
 #include "content/test/content_browser_test_utils_internal.h"
-#include "content/test/data/web_ui_test_mojo_bindings.mojom.h"
+#include "content/test/data/mojo_web_test_helper_test.mojom.h"
 #include "content/test/did_commit_navigation_interceptor.h"
 #include "content/test/frame_host_test_interface.mojom.h"
 #include "content/test/test_content_browser_client.h"
@@ -2028,8 +2028,8 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostImplBrowserTest,
   const GURL kUrl3(embedded_test_server()->GetURL("/title3.html"));
   const GURL kUrl4(embedded_test_server()->GetURL("/empty.html"));
 
-  // The 31-bit hash of the string "content.mojom:BrowserTarget".
-  const int32_t kHashOfContentMojomBrowserTarget = 0x1730feb8;
+  // The 31-bit hash of the string "content.mojom.MojoWebTestHelper".
+  const int32_t kHashOfContentMojomMojoWebTestHelper = 0x77b7b3d6;
 
   // Client ends of the fake interface provider requests injected for the first
   // and second navigations.
@@ -2061,9 +2061,9 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostImplBrowserTest,
 
   // Simulate two interface requests corresponding to the first navigation
   // arrived after the second navigation was committed, hence were dropped.
-  interface_provider_1->GetInterface(mojom::BrowserTarget::Name_,
+  interface_provider_1->GetInterface(mojom::MojoWebTestHelper::Name_,
                                      CreateDisconnectedMessagePipeHandle());
-  interface_provider_1->GetInterface(mojom::BrowserTarget::Name_,
+  interface_provider_1->GetInterface(mojom::MojoWebTestHelper::Name_,
                                      CreateDisconnectedMessagePipeHandle());
 
   // RFHI destroys the DroppedInterfaceRequestLogger from navigation `n` on
@@ -2077,11 +2077,11 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostImplBrowserTest,
         "RenderFrameHostImpl.DroppedInterfaceRequests", 2, 1);
     histogram_tester.ExpectUniqueSample(
         "RenderFrameHostImpl.DroppedInterfaceRequestName",
-        kHashOfContentMojomBrowserTarget, 2);
+        kHashOfContentMojomMojoWebTestHelper, 2);
   }
 
   // Simulate one interface request dropped for the second URL.
-  interface_provider_2->GetInterface(mojom::BrowserTarget::Name_,
+  interface_provider_2->GetInterface(mojom::MojoWebTestHelper::Name_,
                                      CreateDisconnectedMessagePipeHandle());
 
   // A final navigation should record the sample from the second URL.
@@ -2092,14 +2092,14 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostImplBrowserTest,
         "RenderFrameHostImpl.DroppedInterfaceRequests", 1, 1);
     histogram_tester.ExpectUniqueSample(
         "RenderFrameHostImpl.DroppedInterfaceRequestName",
-        kHashOfContentMojomBrowserTarget, 1);
+        kHashOfContentMojomMojoWebTestHelper, 1);
   }
 
   // Both the DroppedInterfaceRequestLogger for the first and second URLs are
   // destroyed -- even more interfacerequests should not cause any crashes.
-  interface_provider_1->GetInterface(mojom::BrowserTarget::Name_,
+  interface_provider_1->GetInterface(mojom::MojoWebTestHelper::Name_,
                                      CreateDisconnectedMessagePipeHandle());
-  interface_provider_2->GetInterface(mojom::BrowserTarget::Name_,
+  interface_provider_2->GetInterface(mojom::MojoWebTestHelper::Name_,
                                      CreateDisconnectedMessagePipeHandle());
 
   // The interface connections should be broken.
--- content/test/data/web_ui_mojo.js
+++ content/test/data/web_ui_mojo.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 (async () => {
-  const browserTarget = content.mojom.BrowserTarget.getRemote();
+  const browserTarget = content.mojom.BrowserTarget.getRemote(true);
 
   await browserTarget.start();
   browserTarget.stop();
