d771d92608de8027b9e7dfc1b26f931eb5240cd2
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2019-11-20 16:45:29 +0000

    Update remote copy notification according to latest specs.
    Bug: 1018137
    Change-Id: Ib553f4bd877267f5232893b7496e0347b1b0c8cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926311
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717039}

--- chrome/browser/sharing/shared_clipboard/remote_copy_message_handler.h
+++ chrome/browser/sharing/shared_clipboard/remote_copy_message_handler.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "base/strings/string16.h"
 #include "chrome/browser/image_decoder.h"
 #include "chrome/browser/sharing/shared_clipboard/remote_copy_handle_message_result.h"
 #include "chrome/browser/sharing/sharing_message_handler.h"
@@ -42,7 +43,7 @@ class RemoteCopyMessageHandler : public SharingMessageHandler,
   void HandleText(const std::string& text);
   void HandleImage(const std::string& image_url);
   void OnURLLoadComplete(std::unique_ptr<std::string> content);
-  void ShowNotification();
+  void ShowNotification(const base::string16& title);
   void Finish(RemoteCopyHandleMessageResult result);
 
   Profile* profile_ = nullptr;
