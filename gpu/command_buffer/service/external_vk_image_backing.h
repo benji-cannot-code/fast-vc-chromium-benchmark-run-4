1b0ca03314ff9c8225d540bd26482856f805e140
Kramer Ge <fangzhoug@chromium.org>
2019-11-20 19:58:43 +0000

    Implement GL passthrough interop for ExternalVkImageBacking
    Implement ExternalVkImageBacking::ProduceGLTexturePassthrough for
    passthrough decoder. Wraps VkImage to gles2::TexturePassthrough.
    
    interop.
    
    gpu: :ExternalVkImageGlRepresentationPassthrough implements
    gpu: :SharedImageRepresentationGLTexturePassthrough to support Vulkan/GL
    Bug: 1020626
    Change-Id: I87583b5b47cc1d348448ada823a2c7262bd41dd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1917323
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717144}

--- gpu/command_buffer/service/external_vk_image_backing.h
+++ gpu/command_buffer/service/external_vk_image_backing.h
@@ -52,6 +52,10 @@ class ExternalVkImageBacking final : public SharedImageBacking {
 
   SharedContextState* context_state() const { return context_state_; }
   const GrBackendTexture& backend_texture() const { return backend_texture_; }
+  const scoped_refptr<gles2::TexturePassthrough>& GetTexturePassthrough()
+      const {
+    return texture_passthrough_;
+  }
   VulkanImplementation* vulkan_implementation() const {
     return context_state()->vk_context_provider()->GetVulkanImplementation();
   }
@@ -142,6 +146,8 @@ class ExternalVkImageBacking final : public SharedImageBacking {
       base::WritableSharedMemoryMapping shared_memory_mapping,
       size_t stride,
       size_t memory_offset);
+  // Returns texture_service_id for ProduceGLTexture and GLTexturePassthrough.
+  GLuint ProduceGLTextureInternal();
 
   using FillBufferCallback = base::OnceCallback<void(void* buffer)>;
   bool WritePixels(size_t data_size,
@@ -161,6 +167,7 @@ class ExternalVkImageBacking final : public SharedImageBacking {
   bool is_write_in_progress_ = false;
   uint32_t reads_in_progress_ = 0;
   gles2::Texture* texture_ = nullptr;
+  scoped_refptr<gles2::TexturePassthrough> texture_passthrough_;
 
   // GMB related stuff.
   base::WritableSharedMemoryMapping shared_memory_mapping_;
