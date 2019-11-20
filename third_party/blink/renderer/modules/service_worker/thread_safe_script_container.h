3cc3fe0bcc9dd669e327a823a00b1b0676b8bae4
Antonio Gomes <tonikitoo@igalia.com>
2019-11-20 00:52:27 +0000

    Remove trailing call to REQUIRE_ADOPTION_FOR_REFCOUNTED_TYPE()
    ... from blink/renderer/modules/service_worker/thread_safe_script_container.h.
    
    The class ThreadSafeScriptContainer inherits from
    WTF::ThreadSafeRefCounted, and hence, has the declaration inherited
    from its parent class.
    
    BUG=None
    R=haraken@chromium.org
    
    Change-Id: Ia45df905d5e301ee477b490fdf9b5b6ca56e3dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924808
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716834}

--- third_party/blink/renderer/modules/service_worker/thread_safe_script_container.h
+++ third_party/blink/renderer/modules/service_worker/thread_safe_script_container.h
@@ -76,7 +76,6 @@ class MODULES_EXPORT ThreadSafeScriptContainer
     std::unique_ptr<CrossThreadHTTPHeaderMapData> headers_;
   };
 
-  REQUIRE_ADOPTION_FOR_REFCOUNTED_TYPE();
   ThreadSafeScriptContainer();
 
   enum class ScriptStatus {
