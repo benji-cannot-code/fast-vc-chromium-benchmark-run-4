a9cea6c02ddc3b48bae161b226e1089423e6c822
Dale Curtis <dalecurtis@chromium.org>
2019-11-20 11:15:23 +0000

    Always create base::SystemMonitor for the audio service.
    It's a tiny class which does nothing if no listeners are attached and
    having it for Windows fake audio device changes will be helpful.
    
    BUG=1020006
    R=guidou
    
    Change-Id: I690c05edb55fcbdcef72ede68e759686243586e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925291
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716951}

--- services/audio/service.cc
+++ services/audio/service.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/audio/service.h"
 
+#include <memory>
 #include <utility>
 
 #include "base/bind.h"
@@ -51,7 +52,10 @@ Service::Service(
   magic_bytes_ = 0x600DC0DEu;
   g_service_state_for_crashing.Set("constructing");
   DCHECK(audio_manager_accessor_);
+
   if (enable_remote_client_support_) {
+    CHECK(!base::SystemMonitor::Get());
+    system_monitor_ = std::make_unique<base::SystemMonitor>();
     log_factory_manager_ = std::make_unique<LogFactoryManager>();
     audio_manager_accessor_->SetAudioLogFactory(
         log_factory_manager_->GetLogFactory());
@@ -193,10 +197,6 @@ void Service::BindDeviceNotifierReceiver(
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK(enable_remote_client_support_);
 
-  if (!system_monitor_) {
-    CHECK(!base::SystemMonitor::Get());
-    system_monitor_ = std::make_unique<base::SystemMonitor>();
-  }
   InitializeDeviceMonitor();
   if (!device_notifier_)
     device_notifier_ = std::make_unique<DeviceNotifier>();
@@ -228,10 +228,8 @@ void Service::InitializeDeviceMonitor() {
 
   audio_device_listener_mac_ = std::make_unique<media::AudioDeviceListenerMac>(
       media::BindToCurrentLoop(base::BindRepeating([] {
-        if (base::SystemMonitor::Get()) {
-          base::SystemMonitor::Get()->ProcessDevicesChanged(
-              base::SystemMonitor::DEVTYPE_AUDIO);
-        }
+        if (auto* monitor = base::SystemMonitor::Get())
+          monitor->ProcessDevicesChanged(base::SystemMonitor::DEVTYPE_AUDIO);
       })),
       true /* monitor_default_input */, true /* monitor_addition_removal */,
       true /* monitor_sources */);
