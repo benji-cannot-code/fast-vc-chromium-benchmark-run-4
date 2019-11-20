b229934c5dcf2fc7f8e172f6bcdca545b8de1d1b
Sebastien <seblalancette@chromium.org>
2019-11-20 22:33:49 +0000

    Adding new Games component installer.
    This installer will only register for Chrome branded Clank builds
    that have a specific feature flag enabled. Then, upon successful
    install (where the catalog file has been downloaded), the installer
    will update the install directory pref for the Games keyed service
    to consume.
    
    Bug: 1018201
    Change-Id: I65820a14f32613800774d193787f3712271db420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1883872
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717284}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "chrome/browser/component_updater/crl_set_component_installer.h"
 #include "chrome/browser/component_updater/file_type_policies_component_installer.h"
+#include "chrome/browser/component_updater/games_component_installer.h"
 #include "chrome/browser/component_updater/mei_preload_component_installer.h"
 #include "chrome/browser/component_updater/optimization_hints_component_installer.h"
 #include "chrome/browser/component_updater/origin_trials_component_installer.h"
@@ -553,6 +554,10 @@ void RegisterComponentsForUpdate(bool is_off_the_record_profile,
 #endif
 
   RegisterSafetyTipsComponent(cus, path);
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && defined(OS_ANDROID)
+  component_updater::RegisterGamesComponent(cus, profile_prefs);
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING) && defined(OS_ANDROID)
 }
 
 #if !defined(OS_ANDROID)
---
+++ components/games/core/games_utils.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/games/core/games_utils.h"
+
+namespace games {
+
+const base::FilePath GetGamesCatalogPath(const base::FilePath& dir) {
+  return dir.Append(kGamesCatalogFileName);
+}
+
+}  // namespace games
