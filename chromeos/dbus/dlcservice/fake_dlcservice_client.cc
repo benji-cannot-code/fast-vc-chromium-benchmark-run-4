6668e7efa3f769df7961745d1fec88ce6fbc0d75
Jae Hoon Kim <kimjae@chromium.org>
2019-11-20 18:36:37 +0000

    Progress update with RepeatingCallback instead of Observer
    Remove the use of Observer design and implement with RepeatingCallback
    for reporting the download/install status of DLC(s).
    
    This reduces the complexity for users of DlcserviceClient.
    
    Bug: b:143098235
    Test: autoninja -C out/Default chromeos_unittests && ./out/Default/chromeos_unittests
    Change-Id: I3b9bd9fdbf2d31f34af5c79ad839d256308a9f3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919743
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717091}

--- chromeos/dbus/dlcservice/fake_dlcservice_client.cc
+++ chromeos/dbus/dlcservice/fake_dlcservice_client.cc
@@ -14,21 +14,10 @@ FakeDlcserviceClient::FakeDlcserviceClient() = default;
 
 FakeDlcserviceClient::~FakeDlcserviceClient() = default;
 
-void FakeDlcserviceClient::AddObserver(Observer* obs) {
-  VLOG(1) << "Requesting to add observer.";
-}
-
-void FakeDlcserviceClient::RemoveObserver(Observer* obs) {
-  VLOG(1) << "Requesting to remove observer.";
-}
-
-void FakeDlcserviceClient::NotifyProgressUpdateForTest(double progress) {
-  NOTREACHED();
-}
-
 void FakeDlcserviceClient::Install(
     const dlcservice::DlcModuleList& dlc_module_list,
-    InstallCallback callback) {
+    InstallCallback callback,
+    ProgressCallback progress_callback) {
   VLOG(1) << "Requesting to install DLC(s).";
   std::move(callback).Run(dlcservice::kErrorNone, dlcservice::DlcModuleList());
 }
@@ -42,6 +31,10 @@ void FakeDlcserviceClient::Uninstall(const std::string& dlc_id,
 void FakeDlcserviceClient::GetInstalled(GetInstalledCallback callback) {
   VLOG(1) << "Requesting to get installed DLC(s).";
   std::move(callback).Run(dlcservice::kErrorNone, dlcservice::DlcModuleList());
+}
+
+void FakeDlcserviceClient::OnInstallStatusForTest(dbus::Signal* signal) {
+  NOTREACHED();
 }
 
 }  // namespace chromeos
