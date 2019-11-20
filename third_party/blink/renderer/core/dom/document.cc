206f355408397b0c7c534424d412348d7b520d90
Vladimir Levin <vmpstr@chromium.org>
2019-11-20 20:15:09 +0000

    RenderSubtree: Schedule an event as animation frame task
    This patch schedules the beforeactivate signal to run at the animation
    frame timing to avoid re-entrancy issues.
    
    See https://github.com/WICG/display-locking/issues/98 for
    a more detailed description of the timing
    
    R=chrishtr@chromium.org, rakina@chromium.org
    
    Change-Id: I362427cc8e99c2945f99b27bf7c610f8e466822c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924835
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717162}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -5636,6 +5636,10 @@ EventListener* Document::GetWindowAttributeEventListener(
   return dom_window->GetAttributeEventListener(event_type);
 }
 
+void Document::EnqueueDisplayLockActivationTask(base::OnceClosure task) {
+  EnsureScriptedAnimationController().EnqueueTask(std::move(task));
+}
+
 void Document::EnqueueAnimationFrameTask(base::OnceClosure task) {
   EnsureScriptedAnimationController().EnqueueTask(std::move(task));
 }
