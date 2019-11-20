ff0963301b3253f3be3ab2e22f5b7b1897e88a50
Xiaoqian Dai <xdai@chromium.org>
2019-11-20 20:17:47 +0000

    Remove drag-to-snap indicators when dragging a window from top.
    UX request. So that the behavior can be consistent with
    dragging-from-shelf case.
    
    Bug: 1026433, 997885
    Change-Id: I044526a10ec08abad65cc9294c17f08da6723da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925691
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717163}

--- ash/wm/splitview/split_view_drag_indicators.cc
+++ ash/wm/splitview/split_view_drag_indicators.cc
@@ -274,6 +274,7 @@ class SplitViewDragIndicators::SplitViewDragIndicatorsView
 
     const bool previews_only =
         window_dragging_state == WindowDraggingState::kFromShelf ||
+        window_dragging_state == WindowDraggingState::kFromTop ||
         SplitViewController::Get(GetWidget()->GetNativeWindow())
             ->InSplitViewMode();
     const bool can_dragged_window_be_snapped =
