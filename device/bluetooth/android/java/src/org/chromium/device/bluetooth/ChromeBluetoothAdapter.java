37289001a47e52296b31788747fbc6e942c11908
James Hollyer <jameshollyer@chromium.org>
2019-11-20 02:11:21 +0000

    [bluetooth] Use filters when scanning for devices on Android
    Previously we were asking for all Bluetooth devices and then doing the
    filtering after we received the devices.  This caused UI jank when in
    an area with many Bluetooth devices.  This change passes as much
    filtering as we can to the OS level so that Android can handle the
    filtering and there is less of a burden on the browser.
    
    Bug: 490401
    Change-Id: I641cc8139a633bfdd56d6d082a4cd4056bd4e515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894620
    Commit-Queue: James Hollyer <jameshollyer@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716846}

--- device/bluetooth/android/java/src/org/chromium/device/bluetooth/ChromeBluetoothAdapter.java
+++ device/bluetooth/android/java/src/org/chromium/device/bluetooth/ChromeBluetoothAdapter.java
@@ -7,6 +7,7 @@ package org.chromium.device.bluetooth;
 
 import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanSettings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -144,10 +145,11 @@ final class ChromeBluetoothAdapter extends BroadcastReceiver {
 
     /**
      * Starts a Low Energy scan.
+     * @param filters List of filters used to minimize number of devices returned
      * @return True on success.
      */
     @CalledByNative
-    private boolean startScan() {
+    private boolean startScan(List<ScanFilter> filters) {
         Wrappers.BluetoothLeScannerWrapper scanner = mAdapter.getBluetoothLeScanner();
 
         if (scanner == null) {
@@ -166,7 +168,7 @@ final class ChromeBluetoothAdapter extends BroadcastReceiver {
         mScanCallback = new ScanCallback();
 
         try {
-            scanner.startScan(null /* filters */, scanMode, mScanCallback);
+            scanner.startScan(filters, scanMode, mScanCallback);
         } catch (IllegalArgumentException e) {
             Log.e(TAG, "Cannot start scan: " + e);
             mScanCallback = null;
