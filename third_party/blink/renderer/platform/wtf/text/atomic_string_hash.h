415ec760191c618c8925335fc2e74d2a5f7b366b
Caleb Raitto <caraitto@chromium.org>
2019-11-20 11:10:58 +0000

    Fix include what you use issue in atomic_string_hash.h.
    I encountered trybot failures in https://crrev.com/c/1907147 -- I was able to
    fix them by including both atomic_string_hash.h and string_hash.h, but
    atomic_string_hash.h should IWYU.
    
    Change-Id: I2501300bdb85bd765b8af6e9605dea460c26adc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924981
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716950}

--- third_party/blink/renderer/platform/wtf/text/atomic_string_hash.h
+++ third_party/blink/renderer/platform/wtf/text/atomic_string_hash.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/wtf/hash_traits.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
+#include "third_party/blink/renderer/platform/wtf/text/string_hash.h"
 
 namespace WTF {
 
