e367dd95e4d411321d5889d97ec65687975740d7
David Grogan <dgrogan@chromium.org>
2019-11-20 08:09:16 +0000

    [LayoutNG] Set ShrinkToFit when determining MinMaxSize via layout
    Bug: 635619
    Change-Id: I6f7b1e16098a773aab2ac395b3b23e8b2af2b99e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1920200
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716907}

--- third_party/blink/renderer/core/layout/ng/ng_space_utils.cc
+++ third_party/blink/renderer/core/layout/ng/ng_space_utils.cc
@@ -39,6 +39,7 @@ NGConstraintSpace CreateIndefiniteConstraintSpaceForChild(
   builder.SetAvailableSize(indefinite_size);
   builder.SetPercentageResolutionSize(indefinite_size);
   builder.SetReplacedPercentageResolutionSize(indefinite_size);
+  builder.SetIsShrinkToFit(child.Style().LogicalWidth().IsAuto());
   builder.SetIsIntermediateLayout(true);
   return builder.ToConstraintSpace();
 }
