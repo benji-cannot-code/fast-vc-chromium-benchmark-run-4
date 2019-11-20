afb05cf09d46172449ac0dd287ee375a239791ac
Alex Turner <alexmt@chromium.org>
2019-11-20 20:28:56 +0000

    Add histograms tracking the total CPU usage of a page
    While there are histograms in AdsPageLoadMetricsObserver tracking
    full-page CPU usage, these are not recorded when no ad frame is
    detected. Further, those histograms only measure CPU usage while the
    page is in the foreground.
    
    We add two new histograms that track both the total and foreground CPU
    usage attributable to the page, respectively. These are recorded for
    all page loads and will be helpful to contextualize the ads CPU metrics.
    
    Bug: 1024277
    Change-Id: I4cba216573000434291f307fdea9219974d44691
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1918102
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717173}

--- components/page_load_metrics/browser/observers/core_page_load_metrics_observer.h
+++ components/page_load_metrics/browser/observers/core_page_load_metrics_observer.h
@@ -79,6 +79,9 @@ extern const char kHistogramPageLoadCacheBytes[];
 extern const char kHistogramPageLoadNetworkBytesIncludingHeaders[];
 extern const char kHistogramPageLoadUnfinishedBytes[];
 
+extern const char kHistogramPageLoadCpuTotalUsage[];
+extern const char kHistogramPageLoadCpuTotalUsageForegrounded[];
+
 extern const char kHistogramLoadTypeTotalBytesForwardBack[];
 extern const char kHistogramLoadTypeNetworkBytesForwardBack[];
 extern const char kHistogramLoadTypeCacheBytesForwardBack[];
@@ -214,11 +217,12 @@ class CorePageLoadMetricsObserver
       content::RenderFrameHost* rfh,
       const std::vector<page_load_metrics::mojom::ResourceDataUpdatePtr>&
           resources) override;
-
   void OnTimingUpdate(
       content::RenderFrameHost* subframe_rfh,
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
-
+  void OnCpuTimingUpdate(
+      content::RenderFrameHost* subframe_rfh,
+      const page_load_metrics::mojom::CpuTiming& timing) override;
   void OnDidFinishSubFrameNavigation(
       content::NavigationHandle* navigation_handle) override;
 
@@ -228,6 +232,7 @@ class CorePageLoadMetricsObserver
       const page_load_metrics::mojom::PageLoadTiming& main_frame_timing);
   void RecordByteAndResourceHistograms(
       const page_load_metrics::mojom::PageLoadTiming& timing);
+  void RecordCpuUsageHistograms();
   void RecordForegroundDurationHistograms(
       const page_load_metrics::mojom::PageLoadTiming& timing,
       base::TimeTicks app_background_time);
@@ -247,6 +252,10 @@ class CorePageLoadMetricsObserver
   // The number of prefilter bytes consumed by completed and partial network
   // requests for the page.
   int64_t network_bytes_including_headers_;
+
+  // The CPU usage attributed to this page.
+  base::TimeDelta total_cpu_usage_;
+  base::TimeDelta foreground_cpu_usage_;
 
   // Size of the redirect chain, which excludes the first URL.
   int redirect_chain_size_;
