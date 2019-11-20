66395c43dd50a1c07766a0d12dc60a763b4fdf65
Antonio Gomes <tonikitoo@igalia.com>
2019-11-20 18:30:25 +0000

    Switch RtcIceCandidatePlatform to use WTF::ThreadSafeRefCounted
    ... instead of the base counterpart, base::RefCountedThreadSafe.
    
    BUG=787254
    R=guidou@chromium.org
    
    Change-Id: Ie9f8c7aa1512c3c4e75fd2a5723d3ab25cb88a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924807
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717090}

--- third_party/blink/renderer/platform/peerconnection/rtc_ice_candidate_platform.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_ice_candidate_platform.h
@@ -32,18 +32,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_RTC_ICE_CANDIDATE_PLATFORM_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_RTC_ICE_CANDIDATE_PLATFORM_H_
 
-#include "base/memory/ref_counted.h"
 #include "base/optional.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+#include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
 
 namespace blink {
 
 class PLATFORM_EXPORT RTCIceCandidatePlatform final
-    : public base::RefCountedThreadSafe<RTCIceCandidatePlatform> {
+    : public WTF::ThreadSafeRefCounted<RTCIceCandidatePlatform> {
  public:
-  REQUIRE_ADOPTION_FOR_REFCOUNTED_TYPE();
-
   // Creates a new RTCIceCandidatePlatform using |candidate|, |sdp_mid| and
   // |sdp_m_line_index|. If |sdp_m_line_index| is negative, it is
   // considered as having no value.
@@ -77,7 +75,7 @@ class PLATFORM_EXPORT RTCIceCandidatePlatform final
   const String& UsernameFragment() const { return username_fragment_; }
 
  private:
-  friend class base::RefCountedThreadSafe<RTCIceCandidatePlatform>;
+  friend class WTF::ThreadSafeRefCounted<RTCIceCandidatePlatform>;
 
   RTCIceCandidatePlatform(String candidate,
                           String sdp_mid,
