85d2c020b8bb19178253594ed7462ff0aff86f45
Joao da Silva <joaodasilva@chromium.org>
2019-11-20 09:44:47 +0000

    Removed the SyncClipboardService.
    This was originally introduced in
    https://chromium-review.googlesource.com/c/chromium/src/+/1781442.
    
    It's being removed now as the implementation of this service will rely
    on a different transport.
    
    Change-Id: Idc493fd59c8ef44d7867cc45c91af2ad9b9217a0
    Bug: 1020579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924191
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Joao da Silva <joaodasilva@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Joao da Silva <joaodasilva@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716924}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -165,10 +165,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/advanced_protection_status_manager_factory.h"
 #endif
 
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
-#include "chrome/browser/sync/sync_clipboard_service_factory.h"
-#endif
-
 namespace chrome {
 
 void AddProfilesExtraParts(ChromeBrowserMainParts* main_parts) {
@@ -352,9 +348,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   StorageNotificationServiceFactory::GetInstance();
 #endif
   suggestions::SuggestionsServiceFactory::GetInstance();
-#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
-  SyncClipboardServiceFactory::GetInstance();
-#endif
   TabRestoreServiceFactory::GetInstance();
   TemplateURLFetcherFactory::GetInstance();
   TemplateURLServiceFactory::GetInstance();
