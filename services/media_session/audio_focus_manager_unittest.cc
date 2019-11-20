1791a92dabd9a3532813c337ae60e85483cedda2
Ilia Samsonov <isamsonov@google.com>
2019-11-20 22:48:09 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=beccahughes@chromium.org
    
    Bug: 1023677
    Change-Id: I8baf93ab77f17565670687108e10756dae4bc448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925745
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#717297}

--- services/media_session/audio_focus_manager_unittest.cc
+++ services/media_session/audio_focus_manager_unittest.cc
@@ -298,7 +298,7 @@ class AudioFocusManagerTest
 };
 
 INSTANTIATE_TEST_SUITE_P(
-    ,
+    All,
     AudioFocusManagerTest,
     testing::Values(mojom::EnforcementMode::kDefault,
                     mojom::EnforcementMode::kNone,
