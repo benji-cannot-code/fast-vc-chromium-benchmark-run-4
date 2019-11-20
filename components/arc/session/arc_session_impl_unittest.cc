e4afa4ad6118070a632bd5e4e396c489506ce882
Ilia Samsonov <isamsonov@google.com>
2019-11-20 23:08:33 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=yusukes@chromium.org
    
    Bug: 1023677
    Change-Id: I53d26899fbfd6ebb086022bfd042418cd5861eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926915
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#717312}

--- components/arc/session/arc_session_impl_unittest.cc
+++ components/arc/session/arc_session_impl_unittest.cc
@@ -633,7 +633,7 @@ TEST_P(ArcSessionImplPackagesCacheModeTest, PackagesCacheModes) {
                 .packages_cache_mode());
 }
 
-INSTANTIATE_TEST_SUITE_P(,
+INSTANTIATE_TEST_SUITE_P(All,
                          ArcSessionImplPackagesCacheModeTest,
                          ::testing::ValuesIn(kPackagesCacheModeStates));
 
@@ -657,7 +657,9 @@ TEST_P(ArcSessionImplGmsCoreCacheTest, GmsCoreCaches) {
                             .skip_gms_core_cache());
 }
 
-INSTANTIATE_TEST_SUITE_P(, ArcSessionImplGmsCoreCacheTest, ::testing::Bool());
+INSTANTIATE_TEST_SUITE_P(All,
+                         ArcSessionImplGmsCoreCacheTest,
+                         ::testing::Bool());
 
 TEST_F(ArcSessionImplTest, DemoSession) {
   auto arc_session = CreateArcSession();
