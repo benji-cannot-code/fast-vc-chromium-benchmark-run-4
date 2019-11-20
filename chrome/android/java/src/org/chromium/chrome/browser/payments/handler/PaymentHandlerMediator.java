2b34f0209ac2d3b250e2eeb95f5a48bc1e0242fc
Matt Jones <mdjones@chromium.org>
2019-11-20 23:28:21 +0000

    BottomSheet is no longer a NativePageHost
    This patch removes the now unused NativePageHost interface from
    BottomSheet. Along with it, loadUrl and the related observer method
    have also been removed.
    
    Bug: 986310
    Change-Id: Icbde5f64354e4fdfb4f715f63d90f7ea5a83f22e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927213
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717326}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/handler/PaymentHandlerMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/handler/PaymentHandlerMediator.java
@@ -73,9 +73,6 @@ import org.chromium.ui.modelutil.PropertyModel;
     }
 
     @Override
-    public void onLoadUrl(String url) {}
-
-    @Override
     public void onSheetFullyPeeked() {}
 
     @Override
