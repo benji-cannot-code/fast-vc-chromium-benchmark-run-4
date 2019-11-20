4c1e1544e048cdb0675dcedfbd01ba8f44fbaf2c
Ilia Samsonov <isamsonov@google.com>
2019-11-20 21:51:22 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=tsepez@chromium.org
    
    Bug: 1023677
    Change-Id: Ia6d484489d2c2e0c28f46e2127cd42a5e9cde8d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927012
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717244}

--- third_party/blink/renderer/platform/weborigin/kurl_test.cc
+++ third_party/blink/renderer/platform/weborigin/kurl_test.cc
@@ -988,6 +988,8 @@ TEST_P(KURLPortTest, SetHostAndPort) {
   }
 }
 
-INSTANTIATE_TEST_SUITE_P(, KURLPortTest, ::testing::ValuesIn(port_test_cases));
+INSTANTIATE_TEST_SUITE_P(All,
+                         KURLPortTest,
+                         ::testing::ValuesIn(port_test_cases));
 
 }  // namespace blink
