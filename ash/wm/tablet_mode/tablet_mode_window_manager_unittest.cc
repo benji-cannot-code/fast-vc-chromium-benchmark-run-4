fb068b9dc9215778880811b3f563dad95050d044
Ilia Samsonov <isamsonov@google.com>
2019-11-20 23:44:35 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=skuhne@chromium.org
    
    Bug: 1023677
    Change-Id: I7334abca4c0f73ff002f1e993e40ade00f1e3419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926847
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717337}

--- ash/wm/tablet_mode/tablet_mode_window_manager_unittest.cc
+++ ash/wm/tablet_mode/tablet_mode_window_manager_unittest.cc
@@ -2106,8 +2106,8 @@ TEST_P(TabletModeWindowManagerWithClamshellSplitViewTest,
   EXPECT_TRUE(home_screen_window->TargetVisibility());
 }
 
-INSTANTIATE_TEST_SUITE_P(, TabletModeWindowManagerTest, testing::Bool());
-INSTANTIATE_TEST_SUITE_P(,
+INSTANTIATE_TEST_SUITE_P(All, TabletModeWindowManagerTest, testing::Bool());
+INSTANTIATE_TEST_SUITE_P(All,
                          TabletModeWindowManagerWithClamshellSplitViewTest,
                          testing::Bool());
 
