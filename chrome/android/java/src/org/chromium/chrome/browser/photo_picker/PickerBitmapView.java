8cb120d5346a2a67a3dd924c1574a59745704941
Finnur Thorarinsson <finnur@chromium.org>
2019-11-20 02:05:42 +0000

    Photo Picker: Fix selection animation regression.
    Bug: 1026071, 656015
    Change-Id: I387628c6309323e3a182c450987c872970387a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1922974
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716845}

--- chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerBitmapView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/photo_picker/PickerBitmapView.java
@@ -22,11 +22,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
@@ -47,6 +49,9 @@ public class PickerBitmapView extends SelectableItemView<PickerBitmap> {
 
     // The length of the image frame display (in ms).
     private static final int IMAGE_FRAME_DISPLAY = 250;
+
+    // An animation listener to verify correct selection behavior with tests.
+    private static AnimationListener sAnimationListenerForTest;
 
     // Our context.
     private Context mContext;
@@ -183,14 +188,14 @@ public class PickerBitmapView extends SelectableItemView<PickerBitmap> {
         // being initialized.
         if (mBitmapDetails == null) return;
 
+        boolean selected = selectedItems.contains(mBitmapDetails);
+        boolean checked = super.isChecked();
+
         super.onSelectionStateChange(selectedItems);
 
         updateSelectionState();
 
         if (!isPictureTile()) return;
-
-        boolean selected = selectedItems.contains(mBitmapDetails);
-        boolean checked = super.isChecked();
 
         boolean needsResize = selected != checked;
         int size = selected && !checked ? mCategoryView.getImageSize() - 2 * mBorder
@@ -223,6 +228,9 @@ public class PickerBitmapView extends SelectableItemView<PickerBitmap> {
                     Animation.RELATIVE_TO_SELF, 0.5f); // Pivot Y-axis type and value.
             animation.setDuration(ANIMATION_DURATION);
             animation.setFillAfter(true); // Keep the results of the animation.
+            if (sAnimationListenerForTest != null) {
+                animation.setAnimationListener(sAnimationListenerForTest);
+            }
             mIconView.startAnimation(animation);
 
             ObjectAnimator videoDurationX = ObjectAnimator.ofFloat(
@@ -446,5 +454,10 @@ public class PickerBitmapView extends SelectableItemView<PickerBitmap> {
     private boolean isPictureTile() {
         return mBitmapDetails.type() == PickerBitmap.TileTypes.PICTURE
                 || mBitmapDetails.type() == PickerBitmap.TileTypes.VIDEO;
+    }
+
+    @VisibleForTesting
+    public static void setAnimationListenerForTest(AnimationListener listener) {
+        sAnimationListenerForTest = listener;
     }
 }
