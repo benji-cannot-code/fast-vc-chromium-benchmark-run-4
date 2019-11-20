95acab308ad85cf4b321b6310b4ef672c56df5f5
Joe Downing <joedow@google.com>
2019-11-20 20:22:48 +0000

    Removing Chromoting Chrome app browsertests
    The Chromoting Chrome app has been deprecated and replaced by our new
    website so we no longer need these tests.  I am removing references to
    the webapp throughout Chromium with the eventual goal of deleting the
    webapp code.
    
    Change-Id: I8dcf3321fae7713b406167a82b74b428a1d79d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926537
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717166}

--- chrome/browser/payments/service_worker_payment_app_factory_browsertest.cc
+++ chrome/browser/payments/service_worker_payment_app_factory_browsertest.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_data_service_factory.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
-#include "chrome/test/remoting/remote_test_helper.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/payments/content/payment_manifest_web_data_service.h"
 #include "components/payments/core/features.h"
