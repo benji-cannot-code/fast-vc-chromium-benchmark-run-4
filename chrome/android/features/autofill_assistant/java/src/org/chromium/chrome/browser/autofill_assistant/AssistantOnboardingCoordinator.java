e945b81b1b9bf88753e4e6fafcd6eaa2f7c56505
Marian Fechete <marianfe@google.com>
2019-11-20 16:28:49 +0000

    [Autofill Assistant] Log dropout reason when user declines the onboarding.
    This CL fixes the UMA dropout logging for the case where users decline
    the onboarding. After this change, when a user declines the onboarding,
    we log that as a dropout reason in UMA.
    
    Bug: b/144834270
    Change-Id: Ic1360650777b85c355f00312a044385cec289ac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926474
    Commit-Queue: Marian Fechete <marianfe@google.com>
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Auto-Submit: Marian Fechete <marianfe@google.com>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#717035}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantOnboardingCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantOnboardingCoordinator.java
@@ -18,6 +18,7 @@ import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
 import org.chromium.chrome.autofill_assistant.R;
+import org.chromium.chrome.browser.autofill_assistant.metrics.DropOutReason;
 import org.chromium.chrome.browser.autofill_assistant.metrics.OnBoarding;
 import org.chromium.chrome.browser.autofill_assistant.overlay.AssistantOverlayCoordinator;
 import org.chromium.chrome.browser.autofill_assistant.overlay.AssistantOverlayModel;
@@ -186,6 +187,10 @@ class AssistantOnboardingCoordinator {
         AutofillAssistantPreferencesUtil.setInitialPreferences(accept);
         AutofillAssistantMetrics.recordOnBoarding(
                 accept ? OnBoarding.OB_ACCEPTED : OnBoarding.OB_CANCELLED);
+        if (!accept) {
+            AutofillAssistantMetrics.recordDropOut(DropOutReason.DECLINED);
+        }
+
         callback.onResult(accept);
         hide();
     }
