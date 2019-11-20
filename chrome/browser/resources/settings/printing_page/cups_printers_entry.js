19a38917d8fa325b142d7d53efb00374a738f4a3
Jimmy Gong <jimmyxgong@google.com>
2019-11-20 20:48:16 +0000

    Query discovered printers for autoconf
    - This fixes a regression in which we did not check if a discovered
      printer can actually be autoconfigured, which resulted in users not
      being able to add certain printers
    - This regression caused a metric to never be counted:
      Printing.CUPS.AddressResolutionResult
    - The fix is to always go call on AddDiscoveredPrinter when adding
      a printer from the nearby printers list.
    
    Bug: 1026057
    Test: browser test + end to end manual
    Change-Id: I8b611eb682ef7e04dba452ea2fa232598c95d193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1922968
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717190}

--- chrome/browser/resources/settings/printing_page/cups_printers_entry.js
+++ chrome/browser/resources/settings/printing_page/cups_printers_entry.js
@@ -34,9 +34,8 @@ Polymer({
     });
   },
 
-  onOpenManufacturerModelDialogTap_: function(e) {
-    this.fire('open-manufacturer-model-dialog-for-specified-printer',
-        {item: this.printerEntry.printerInfo});
+  onAddDiscoveredPrinterTap_: function(e) {
+    this.fire('query-discovered-printer', {item: this.printerEntry});
   },
 
   onAddAutomaticPrinterTap_: function() {
