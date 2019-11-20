e064db13e4c6144c8acbbce92f0fbd00e1895baa
Andreas Haas <ahaas@chromium.org>
2019-11-20 07:51:23 +0000

    [arraybuffer] Remove Float[32|64]Array and Big[Ui|I]nt64Array
    They are handled the same as the integral type ones.
    
    R=haraken@chromium.org
    
    Bug: chromium:1008840
    Change-Id: I7d6ee4bd3939e8458f7a10b6697f1204c911345a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924515
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#716902}

--- third_party/blink/renderer/modules/webaudio/audio_buffer.cc
+++ third_party/blink/renderer/modules/webaudio/audio_buffer.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webaudio/audio_buffer.h"
 
 #include <memory>
-#include "third_party/blink/renderer/core/typed_arrays/array_buffer/float32_array.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_buffer_options.h"
 #include "third_party/blink/renderer/modules/webaudio/base_audio_context.h"
 #include "third_party/blink/renderer/platform/audio/audio_bus.h"
