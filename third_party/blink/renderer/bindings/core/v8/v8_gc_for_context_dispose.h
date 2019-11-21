1dadba94d58515b1e7d98e463c0d1b900778b44c
Kent Tamura <tkent@chromium.org>
2019-11-21 13:05:46 +0000

    bindings: Convert some time-in-double instances to base::Time
    This CL has no behavior changes.
    
    Change-Id: If2317def8d94c339c3a3bb8d8c6518347a718da8
    Bug: 988343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928817
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717610}

--- third_party/blink/renderer/bindings/core/v8/v8_gc_for_context_dispose.h
+++ third_party/blink/renderer/bindings/core/v8/v8_gc_for_context_dispose.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_BINDINGS_CORE_V8_V8_GC_FOR_CONTEXT_DISPOSE_H_
 
 #include "base/macros.h"
+#include "base/time/time.h"
 #include "third_party/blink/renderer/bindings/core/v8/window_proxy.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/timer.h"
@@ -60,7 +61,7 @@ class V8GCForContextDispose {
 
   TaskRunnerTimer<V8GCForContextDispose> pseudo_idle_timer_;
   bool did_dispose_context_for_main_frame_;
-  double last_context_disposal_time_;
+  base::Time last_context_disposal_time_;
   bool force_page_navigation_gc_;
 
   DISALLOW_COPY_AND_ASSIGN(V8GCForContextDispose);
