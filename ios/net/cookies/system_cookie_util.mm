a80d287d28ee5517fbd79454e6b56c14f6868664
Ryan Hamilton <rch@chromium.org>
2019-11-21 23:47:52 +0000

    Do not build iOS 13 specific code for cronet in system_cookie_util.mm
    Bug: 1027274
    Change-Id: I3e5600d61adf8cb37154d996dcf85070b5caf24b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929833
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717906}

--- ios/net/cookies/system_cookie_util.mm
+++ ios/net/cookies/system_cookie_util.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
+#include "ios/net/ios_net_buildflags.h"
 #include "net/cookies/cookie_constants.h"
 #include "url/gurl.h"
 
@@ -77,6 +78,9 @@ net::CanonicalCookie CanonicalCookieFromSystemCookie(
     NSHTTPCookie* cookie,
     const base::Time& ceation_time) {
   net::CookieSameSite same_site = net::CookieSameSite::NO_RESTRICTION;
+// TODO(crbug.com/1027279): Remove this once Cronet can have
+// iOS 13 symbols.
+#if !BUILDFLAG(CRONET_BUILD)
   if (@available(iOS 13, *)) {
     same_site = net::CookieSameSite::UNSPECIFIED;
     if ([cookie.sameSitePolicy isEqual:NSHTTPCookieSameSiteLax])
@@ -89,6 +93,7 @@ net::CanonicalCookie CanonicalCookieFromSystemCookie(
             isEqual:kNSHTTPCookieSameSiteNone])
       same_site = net::CookieSameSite::NO_RESTRICTION;
   }
+#endif
 
   return net::CanonicalCookie(
       base::SysNSStringToUTF8([cookie name]),
