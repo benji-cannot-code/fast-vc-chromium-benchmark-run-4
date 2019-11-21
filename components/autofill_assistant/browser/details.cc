4fd779d39c3c6f2b55f4b70f5063a3bbb25f19f9
Mathias Carlen <mcarlen@chromium.org>
2019-11-21 13:18:01 +0000

    [Autofill Assistant] Locale cleanup.
    Before this patch we used the device/platform locale to set/get things
    in autofill profiles. That locale is dropped in the autofill code and
    not used at all. This patch removes the locale logic from two actions.
    
    The consequence now is that the mock in tests does not set the full name
    and that is used in the completeness comparison. The fix used here is to
    always construct the FullName from the first,middle,last triplet when
    needed.
    
    R=arbesser@google.com, marianfe@google.com
    
    Bug: 806868
    Change-Id: I0835da87a07c409af8832cd052f6654372b8dcf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926487
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#717615}

--- components/autofill_assistant/browser/details.cc
+++ components/autofill_assistant/browser/details.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "components/autofill/core/browser/autofill_data_util.h"
 #include "components/autofill/core/browser/geo/country_names.h"
 #include "components/autofill_assistant/browser/trigger_context.h"
 #include "components/strings/grit/components_strings.h"
@@ -77,6 +78,14 @@ std::string FormatDateTimeProto(const DateTimeProto& date_time) {
   return std::string();
 }
 
+// This logic is from NameInfo::FullName.
+base::string16 FullName(const autofill::AutofillProfile& profile) {
+  return autofill::data_util::JoinNameParts(
+      profile.GetRawInfo(autofill::NAME_FIRST),
+      profile.GetRawInfo(autofill::NAME_MIDDLE),
+      profile.GetRawInfo(autofill::NAME_LAST));
+}
+
 }  // namespace
 
 Details::Details() = default;
@@ -114,14 +123,11 @@ bool Details::UpdateFromContactDetails(const ShowDetailsProto& proto,
   ShowDetailsProto updated_proto = proto;
   auto* profile = client_memory->selected_address(contact_details);
   auto* details_proto = updated_proto.mutable_details();
-  // TODO(crbug.com/806868): Get the actual script locale.
-  std::string app_locale = "en-US";
   details_proto->set_title(
       l10n_util::GetStringUTF8(IDS_PAYMENTS_CONTACT_DETAILS_LABEL));
-  details_proto->set_description_line_1(
-      base::UTF16ToUTF8(profile->GetInfo(autofill::NAME_FULL, app_locale)));
+  details_proto->set_description_line_1(base::UTF16ToUTF8(FullName(*profile)));
   details_proto->set_description_line_2(
-      base::UTF16ToUTF8(profile->GetInfo(autofill::EMAIL_ADDRESS, app_locale)));
+      base::UTF16ToUTF8(profile->GetRawInfo(autofill::EMAIL_ADDRESS)));
   details->SetDetailsProto(updated_proto.details());
   details->SetDetailsChangesProto(updated_proto.change_flags());
   return true;
@@ -139,25 +145,20 @@ bool Details::UpdateFromShippingAddress(const ShowDetailsProto& proto,
   ShowDetailsProto updated_proto = proto;
   auto* profile = client_memory->selected_address(shipping_address);
   auto* details_proto = updated_proto.mutable_details();
-  // TODO(crbug.com/806868): Get the actual script locale.
-  std::string app_locale = "en-US";
   autofill::CountryNames* country_names = autofill::CountryNames::GetInstance();
   details_proto->set_title(
       l10n_util::GetStringUTF8(IDS_PAYMENTS_SHIPPING_ADDRESS_LABEL));
-  details_proto->set_description_line_1(
-      base::UTF16ToUTF8(profile->GetInfo(autofill::NAME_FULL, app_locale)));
+  details_proto->set_description_line_1(base::UTF16ToUTF8(FullName(*profile)));
   details_proto->set_description_line_2(base::StrCat({
       base::UTF16ToUTF8(
-          profile->GetInfo(autofill::ADDRESS_HOME_STREET_ADDRESS, app_locale)),
+          profile->GetRawInfo(autofill::ADDRESS_HOME_STREET_ADDRESS)),
       " ",
-      base::UTF16ToUTF8(
-          profile->GetInfo(autofill::ADDRESS_HOME_ZIP, app_locale)),
+      base::UTF16ToUTF8(profile->GetRawInfo(autofill::ADDRESS_HOME_ZIP)),
       " ",
-      base::UTF16ToUTF8(
-          profile->GetInfo(autofill::ADDRESS_HOME_CITY, app_locale)),
+      base::UTF16ToUTF8(profile->GetRawInfo(autofill::ADDRESS_HOME_CITY)),
       " ",
       country_names->GetCountryCode(
-          profile->GetInfo(autofill::ADDRESS_HOME_COUNTRY, app_locale)),
+          profile->GetRawInfo(autofill::ADDRESS_HOME_COUNTRY)),
   }));
   details->SetDetailsProto(updated_proto.details());
   details->SetDetailsChangesProto(updated_proto.change_flags());
