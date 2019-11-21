2ddd8e98a2c942614690a390b525d4aecf095df4
Himanshu Jaju <himanshujaju@chromium.org>
2019-11-21 15:28:25 +0000

    Move browsertests to ui/ for Sharing
    Moves all browsertests to ui/views/sharing to add UI checks
    in browsertests.
    
    Bug: 1015850
    Change-Id: I64062a8e1d89898fce7e8a3902d1eb1ea4c48d45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928707
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717647}

--- chrome/browser/ui/views/sharing/sharing_browsertest.cc
+++ chrome/browser/ui/views/sharing/sharing_browsertest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/sharing/sharing_browsertest.h"
+#include "chrome/browser/ui/views/sharing/sharing_browsertest.h"
 
 #include "base/bind.h"
 #include "base/run_loop.h"
--- chrome/browser/ui/views/sharing/sharing_browsertest.h
+++ chrome/browser/ui/views/sharing/sharing_browsertest.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_SHARING_SHARING_BROWSERTEST_H_
-#define CHROME_BROWSER_SHARING_SHARING_BROWSERTEST_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_SHARING_SHARING_BROWSERTEST_H_
+#define CHROME_BROWSER_UI_VIEWS_SHARING_SHARING_BROWSERTEST_H_
 
 #include <memory>
 #include <string>
@@ -69,4 +69,4 @@ class SharingBrowserTest : public SyncTest {
   DISALLOW_COPY_AND_ASSIGN(SharingBrowserTest);
 };
 
-#endif  // CHROME_BROWSER_SHARING_SHARING_BROWSERTEST_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_SHARING_SHARING_BROWSERTEST_H_
