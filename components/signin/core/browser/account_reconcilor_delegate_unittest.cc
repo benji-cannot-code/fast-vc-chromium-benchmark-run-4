3f3431ac942decba6344e61c6e947b0d12374a54
Ilia Samsonov <isamsonov@google.com>
2019-11-21 17:41:10 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=msarda@chromium.org
    
    Bug: 1023677
    Change-Id: Id20e94399700bb14d990b625f7703636035f2a26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925460
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#717696}

--- components/signin/core/browser/account_reconcilor_delegate_unittest.cc
+++ components/signin/core/browser/account_reconcilor_delegate_unittest.cc
@@ -147,7 +147,7 @@ TEST_P(AccountReconcilorDelegateTest, ReorderChromeAccountsForReconcile) {
                        GaiaAccountsFromString(order_as_string)));
 }
 
-INSTANTIATE_TEST_SUITE_P(,
+INSTANTIATE_TEST_SUITE_P(All,
                          AccountReconcilorDelegateTest,
                          ::testing::ValuesIn(kReorderParams));
 
