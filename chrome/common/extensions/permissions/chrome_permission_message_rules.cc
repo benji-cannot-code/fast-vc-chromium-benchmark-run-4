8290b7351cddb59413cb602287d06bd5f41de040
Nikita Podguzov <nikitapodguzov@chromium.org>
2019-11-21 21:24:25 +0000

    Printing API: Add chrome.printing permission warning
    Bug: 996785
    Change-Id: I6aac4570e1332635a3e3aadfab47bff51dd50b91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928710
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nikita Podguzov <nikitapodguzov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717794}

--- chrome/common/extensions/permissions/chrome_permission_message_rules.cc
+++ chrome/common/extensions/permissions/chrome_permission_message_rules.cc
@@ -434,6 +434,7 @@ ChromePermissionMessageRule::GetAllRules() {
        {APIPermission::kFavicon, APIPermission::kTopSites}},
       {IDS_EXTENSION_PROMPT_WARNING_FAVICON, {APIPermission::kFavicon}, {}},
       {IDS_EXTENSION_PROMPT_WARNING_TOPSITES, {APIPermission::kTopSites}, {}},
+      {IDS_EXTENSION_PROMPT_WARNING_PRINTING, {APIPermission::kPrinting}, {}},
       {IDS_EXTENSION_PROMPT_WARNING_PRINTING_METRICS,
        {APIPermission::kPrintingMetrics},
        {}},
