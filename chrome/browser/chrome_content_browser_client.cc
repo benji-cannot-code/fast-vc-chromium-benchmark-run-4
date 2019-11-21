b747176035f6703edab057ee42914b640cbab648
Greg Thompson <grt@chromium.org>
2019-11-21 12:35:36 +0000

    Reland: Browser shutdown cleanups (no functional changes).
    - Make ShutdownType an enum class.
    - Introduce browser_shutdown::HasShutdownStarted as a convenience.
    - Switch from UMA macros to functions for one-shot metrics.
    
    (cherry picked from commit 11936b318f36c4624c9bcba47f61f65cf457dc20)
    
    BUG=958865
    
    Change-Id: I2c6c018f1b2b298f19cdde819477ab084f2d1c1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924192
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717604}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1263,7 +1263,7 @@ void ChromeContentBrowserClient::SetBrowserStartupIsCompleteForTesting() {
 }
 
 bool ChromeContentBrowserClient::IsShuttingDown() {
-  return browser_shutdown::GetShutdownType() != browser_shutdown::NOT_VALID;
+  return browser_shutdown::HasShutdownStarted();
 }
 
 std::string ChromeContentBrowserClient::GetStoragePartitionIdForSite(
