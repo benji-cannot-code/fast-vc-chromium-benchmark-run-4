19694ab02ab5bb093642ace05270d866edbbbac3
Ilia Samsonov <isamsonov@google.com>
2019-11-21 22:07:15 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=miu@chromium.org
    
    Bug: 1023677
    Change-Id: Iea45769fbdef3c3a562e959b815428ad916b6103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926997
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717826}

--- content/browser/media/capture/aura_window_video_capture_device_browsertest.cc
+++ content/browser/media/capture/aura_window_video_capture_device_browsertest.cc
@@ -339,7 +339,7 @@ class AuraWindowVideoCaptureDeviceBrowserTestP
 
 #if defined(OS_CHROMEOS)
 INSTANTIATE_TEST_SUITE_P(
-    ,
+    All,
     AuraWindowVideoCaptureDeviceBrowserTestP,
     testing::Combine(
         // Note: On ChromeOS, software compositing is not an option.
@@ -348,7 +348,7 @@ INSTANTIATE_TEST_SUITE_P(
                         true /* fixed aspect ratio */)));
 #else
 INSTANTIATE_TEST_SUITE_P(
-    ,
+    All,
     AuraWindowVideoCaptureDeviceBrowserTestP,
     testing::Combine(testing::Values(false /* GPU-accelerated compositing */,
                                      true /* software compositing */),
--- content/browser/media/capture/web_contents_video_capture_device_browsertest.cc
+++ content/browser/media/capture/web_contents_video_capture_device_browsertest.cc
@@ -375,7 +375,7 @@ class WebContentsVideoCaptureDeviceBrowserTestP
 
 #if defined(OS_CHROMEOS)
 INSTANTIATE_TEST_SUITE_P(
-    ,
+    All,
     WebContentsVideoCaptureDeviceBrowserTestP,
     testing::Combine(
         // Note: On ChromeOS, software compositing is not an option.
@@ -386,7 +386,7 @@ INSTANTIATE_TEST_SUITE_P(
                         true /* page contains a cross-site iframe */)));
 #else
 INSTANTIATE_TEST_SUITE_P(
-    ,
+    All,
     WebContentsVideoCaptureDeviceBrowserTestP,
     testing::Combine(
         testing::Values(false /* GPU-accelerated compositing */,
