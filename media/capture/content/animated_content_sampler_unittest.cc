2b8a0ae6858cd769e6a03ca4105e7eb3bb1f415b
Ilia Samsonov <isamsonov@google.com>
2019-11-21 22:16:58 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=miu@chromium.org
    
    Bug: 1023677
    Change-Id: Ib799c521680e7b873c55a5ded9a700b82f1f60bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925740
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Yuri Wiitala <miu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717838}

--- media/capture/content/animated_content_sampler_unittest.cc
+++ media/capture/content/animated_content_sampler_unittest.cc
@@ -704,7 +704,7 @@ TEST_P(AnimatedContentSamplerParameterizedTest,
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    ,
+    All,
     AnimatedContentSamplerParameterizedTest,
     ::testing::Values(
         // Typical frame rate content: Compositor runs at 60 Hz, capture at 30
