d18f2ccdf8c507a7adf23eb9831f6f5101fb415c
Tim Volodine <timvolodine@chromium.org>
2019-11-21 20:10:33 +0000

    [WebLayer] Create and init pref service for safebrowsing to work.
    Currently safebrowsing is crashing due to the lack
    of pref service in weblayer (see crbug.com/1025257).
    This patch creates and initializes the pref service to
    avoid this crash.
    
    BUG=1015418,1025257
    TBR=gab@chromium.org
    
    Change-Id: I11e2ab3bc6cebbd973ad816152dfc27a090c5124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926321
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717770}

--- weblayer/browser/safe_browsing/safe_browsing_service.h
+++ weblayer/browser/safe_browsing/safe_browsing_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "weblayer/browser/safe_browsing/safe_browsing_ui_manager.h"
 
 namespace content {
+class BrowserContext;
 class ResourceContext;
 }
 
@@ -42,7 +43,7 @@ class SafeBrowsingService {
   ~SafeBrowsingService();
 
   // Executed on UI thread
-  void Initialize();
+  void Initialize(content::BrowserContext* browser_context);
   std::unique_ptr<blink::URLLoaderThrottle> CreateURLLoaderThrottle(
       content::ResourceContext* resource_context,
       const base::RepeatingCallback<content::WebContents*()>& wc_getter,
@@ -85,6 +86,8 @@ class SafeBrowsingService {
 
   std::unique_ptr<safe_browsing::SafeBrowsingApiHandler>
       safe_browsing_api_handler_;
+
+  std::unique_ptr<PrefService> user_pref_service_;
 
   std::string user_agent_;
 
