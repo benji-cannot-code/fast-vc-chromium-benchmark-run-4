5749db0d452b3a98b1322e00bfebdce91d8cda13
Michael Spang <spang@chromium.org>
2019-11-21 18:49:24 +0000

    events: ozone: Fix XkbLayoutEngineVkTest
    This test fails after 0ff1d4acbad0 ("ozone: xkbcommon: Pre-compute masks
    when setting keymap") but was not running on any bots. Fix it by
    initializing the modifier masks in the test (thanks kpschoedel@
    for debugging and finding this fix).
    
    Bug: 1026406
    Change-Id: I0639e8537d48a7cfb846dea76284dea8f4ff7eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928249
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717732}

--- ui/events/ozone/layout/xkb/xkb_keyboard_layout_engine_unittest.cc
+++ ui/events/ozone/layout/xkb/xkb_keyboard_layout_engine_unittest.cc
@@ -71,6 +71,9 @@ class VkTestXkbKeyboardLayoutEngine : public XkbKeyboardLayoutEngine {
       XkbFlagMapEntry e = {kTestFlags[i], kTestFlags[i]};
       xkb_flag_map_.push_back(e);
     }
+
+    shift_mod_mask_ = EF_SHIFT_DOWN;
+    altgr_mod_mask_ = EF_ALTGR_DOWN;
   }
   ~VkTestXkbKeyboardLayoutEngine() override {}
 
