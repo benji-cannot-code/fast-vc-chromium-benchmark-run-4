7d4ef0d17020383b9a1e0367b74aceb26eb0ecaf
Joel Hockey <joelhockey@chromium.org>
2019-11-21 07:09:43 +0000

    Use GetTerminalId within crostini while switching to Terminal System App
    GetTerminalId will return the old terminal ID, or the new
    Terminal System App ID if the feature is on.
    
    Bug: 1019021
    Change-Id: Iee4f3853c746bbe79ad32a05870712418e4c400d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924177
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717529}

--- chrome/browser/chromeos/crostini/crostini_package_notification.cc
+++ chrome/browser/chromeos/crostini/crostini_package_notification.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/chromeos/crostini/crostini_package_service.h"
 #include "chrome/browser/chromeos/crostini/crostini_registry_service_factory.h"
+#include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/browser/notifications/notification_display_service.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/grit/generated_resources.h"
@@ -273,7 +274,7 @@ void CrostiniPackageNotification::Click(
     return;
 
   if (app_count_ == 0) {
-    LaunchCrostiniApp(profile_, kCrostiniTerminalId,
+    LaunchCrostiniApp(profile_, GetTerminalId(),
                       display::Screen::GetScreen()->GetPrimaryDisplay().id());
   } else if (app_count_ == 1) {
     DCHECK(!app_id_.empty());
