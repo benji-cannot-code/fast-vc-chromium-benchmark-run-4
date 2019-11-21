029bdb34275975f88cf378d6e434727230494b6a
Xiaoqian Dai <xdai@chromium.org>
2019-11-21 01:08:49 +0000

    overview gesture: remove scale-down-to-home animation.
    It's replaced by scale-down-to-shelf-item animation.
    
    Bug: 1026408
    Change-Id: Ie5ba4987f18ca5b853ecd953db98efe962048746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926866
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717397}

--- ash/home_screen/drag_window_from_shelf_controller.cc
+++ ash/home_screen/drag_window_from_shelf_controller.cc
@@ -556,7 +556,7 @@ void DragWindowFromShelfController::ScaleDownWindowAfterDrag() {
   for (auto* window : GetTransientTreeIterator(window_)) {
     // self-destructed when window transform animation is done.
     new WindowScaleAnimation(
-        window, WindowScaleAnimation::WindowScaleType::kScaleDownToHomeScreen,
+        window, WindowScaleAnimation::WindowScaleType::kScaleDownToShelf,
         window == window_ ? base::make_optional(original_backdrop_mode_)
                           : base::nullopt,
         base::NullCallback());
