c7a4a7d84564027c84279120a371651005248a48
Kenichi Ishibashi <bashi@chromium.org>
2019-11-21 04:42:35 +0000

    Remove a blink dependency from ServiceWorkerDatabase
    Before this CL ServiceWorkerDatabase included trial_token_validator.h
    just for a type alias. Since ServiceWorkerDatabase will be moved
    into the storage service and we generally don't want to have a
    dependency from the storage service to blink, remove the include.
    
    The downside of this change is that we now need to have a dup of
    FeatureToTokensMap, but we don't have to use the same data structure
    actually. It's kind an internal representation in ServiceWorkerDatabase
    and we can use arbitrary data structure for that. I think it would make
    sense to have a separate definition.
    
    Bug: 1016064
    Change-Id: Ie9bb7ba329e3de2dd1c207b1b05e75a909eae06d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928472
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717496}

--- content/browser/service_worker/service_worker_database.h
+++ content/browser/service_worker/service_worker_database.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "content/common/content_export.h"
-#include "third_party/blink/public/common/origin_trials/trial_token_validator.h"
 #include "third_party/blink/public/mojom/service_worker/service_worker_registration.mojom.h"
 #include "third_party/blink/public/mojom/web_feature/web_feature.mojom.h"
 #include "url/gurl.h"
@@ -66,6 +65,9 @@ class CONTENT_EXPORT ServiceWorkerDatabase {
   };
   static const char* StatusToString(Status status);
 
+  using FeatureToTokensMap = std::map<std::string /* feature_name */,
+                                      std::vector<std::string /* token */>>;
+
   struct CONTENT_EXPORT RegistrationData {
     // These values are immutable for the life of a registration.
     int64_t registration_id;
@@ -82,8 +84,7 @@ class CONTENT_EXPORT ServiceWorkerDatabase {
     bool has_fetch_handler;
     base::Time last_update_check;
     base::Time script_response_time;
-    base::Optional<blink::TrialTokenValidator::FeatureToTokensMap>
-        origin_trial_tokens;
+    base::Optional<FeatureToTokensMap> origin_trial_tokens;
     blink::mojom::NavigationPreloadState navigation_preload_state;
     std::set<blink::mojom::WebFeature> used_features;
 
