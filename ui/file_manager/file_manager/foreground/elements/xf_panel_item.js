f19d4a7f055c378830853943506a877944e84ce4
Alex Danilo <adanilo@chromium.org>
2019-11-21 05:49:22 +0000

    Stop repeated screen reader summary updates
    Adding role='alert' on the summary panel item type has the side
    effect of the screen reader repeatedly reading the panel message
    for each progress update which is excessive.
    
    Removes the role='alert' for summary panels only and moves the
    role='alert' setting to the text on other panel types so that
    the screen reader reads just the text on the panel for an alert.
    
    Bug: 1026501
    Tests: browser_test --gtest_filter="*FilesDisplayPanel"
    Change-Id: Idd7e50690a7e8f36236933f5314d73e016190a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928456
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717503}

--- ui/file_manager/file_manager/foreground/elements/xf_panel_item.js
+++ ui/file_manager/file_manager/foreground/elements/xf_panel_item.js
@@ -147,10 +147,10 @@ class PanelItem extends HTMLElement {
                 height: 68px;
               }
             </style>
-            <div class='xf-panel-item' role='alert'>
+            <div class='xf-panel-item'>
                 <xf-circular-progress id='indicator'>
                 </xf-circular-progress>
-                <div class='xf-panel-text'>
+                <div class='xf-panel-text' role='alert'>
                     <span class='xf-panel-label-text' tabindex='0'>
                     </span>
                     <br class='xf-linebreaker'/>
@@ -205,6 +205,10 @@ class PanelItem extends HTMLElement {
 
     const buttonSpacer = this.shadowRoot.querySelector('#button-gap');
 
+    // Default the text host to use an alert role.
+    const textHost = assert(this.shadowRoot.querySelector('.xf-panel-text'));
+    textHost.setAttribute('role', 'alert');
+
     // Setup the panel configuration for the panel type.
     // TOOD(crbug.com/947388) Simplify this switch breaking out common cases.
     /** @type {?Element} */
@@ -228,6 +232,9 @@ class PanelItem extends HTMLElement {
         primaryButton.dataset.category = 'expand';
         primaryButton.setAttribute(
             'aria-label', '$i18n{FEEDBACK_EXPAND_LABEL}');
+        // Remove the 'alert' role to stop screen readers repeatedly
+        // reading each progress update.
+        textHost.setAttribute('role', '');
         buttonSpacer.insertAdjacentElement('afterend', primaryButton);
         break;
       case this.panelTypeDone:
