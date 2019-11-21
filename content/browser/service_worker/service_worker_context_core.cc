bc64e011c943a4597ef6c58a2d383c7e4745d248
Yuzu Saijo <yuzus@chromium.org>
2019-11-21 02:00:00 +0000

    [bfcache] Skip ServiceWorkerProviderHost in bfcache in iteration
    This CL makes the ServiceWorkerProviderHost iteration skip the ones in
    BackForwardCache. The reason we should skip them is (1) bfcached client
    should not exist theoretically and (2) it would be dangerous if
    bfcached client is accessed after eviction but before deletion (unknown
    state).
    The design doc below explains (2) in detail.
    
    Design doc: https://docs.google.com/document/d/1_AdqUvCAZffWbYlZjmfHF-EogtFjl6jfqNmSCCHNptc/edit#heading=h.nfsu54jhglw
    
    Change-Id: I2c64689ae26067b1188cdfd4ca512676eeebeb55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1880993
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717426}

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/console_message.h"
 #include "content/public/common/child_process_host.h"
+#include "content/public/common/navigation_policy.h"
 #include "content/public/common/url_utils.h"
 #include "ipc/ipc_message.h"
 #include "net/http/http_response_headers.h"
@@ -125,12 +126,30 @@ void SuccessReportingCallback(
 bool IsSameOriginClientProviderHost(const GURL& origin,
                                     bool allow_reserved_client,
                                     ServiceWorkerProviderHost* host) {
+  // If |host| is in BackForwardCache, it should be skipped in iteration,
+  // because (1) hosts in BackForwardCache should never be exposed to web as
+  // clients and (2) hosts could be in an unknown state after eviction and
+  // before deletion.
+  if (IsBackForwardCacheEnabled() &&
+      ServiceWorkerContext::IsServiceWorkerOnUIEnabled()) {
+    if (host->IsInBackForwardCache())
+      return false;
+  }
   return host->IsProviderForClient() && host->url().GetOrigin() == origin &&
          (allow_reserved_client || host->is_execution_ready());
 }
 
 bool IsSameOriginWindowProviderHost(const GURL& origin,
                                     ServiceWorkerProviderHost* host) {
+  // If |host| is in BackForwardCache, it should be skipped in iteration,
+  // because (1) hosts in BackForwardCache should never be exposed to web as
+  // clients and (2) hosts could be in an unknown state after eviction and
+  // before deletion.
+  if (IsBackForwardCacheEnabled() &&
+      ServiceWorkerContext::IsServiceWorkerOnUIEnabled()) {
+    if (host->IsInBackForwardCache())
+      return false;
+  }
   return host->provider_type() ==
              blink::mojom::ServiceWorkerProviderType::kForWindow &&
          host->url().GetOrigin() == origin && host->is_execution_ready();
