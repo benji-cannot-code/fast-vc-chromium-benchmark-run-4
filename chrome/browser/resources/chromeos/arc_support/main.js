23f83c31de90dd070e075c969b1bc1cbf8612140
Long Cheng <lgcheng@google.com>
2019-11-21 21:25:35 +0000

    Fix ARC optin extension
    html import is disabled by
    https://chromium-review.googlesource.com/c/chromium/src/+/1869562
    
    Deprecate html import in ARC optin extension.
    
    Bug: 1026272, b/144407443
    Test: manual
    Change-Id: I5d17b78c9fece5f90b20bb570102cf5e77b9cb32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924599
    Commit-Queue: Long Cheng <lgcheng@google.com>
    Auto-Submit: Long Cheng <lgcheng@google.com>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717796}

---
+++ chrome/browser/resources/chromeos/arc_support/main.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
+import 'chrome://resources/cr_elements/hidden_style_css.m.js';
+
+import 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
