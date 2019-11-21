226b28225914cf75b2078dac94dd42a2fe7a80a4
Tommy Steimel <steimel@chromium.org>
2019-11-21 20:27:04 +0000

    GMC: Record UMA for the time since last interaction
    This CL adds a new Media.GlobalMediaControls.TimeSinceLastInteraction
    metric that records the number of milliseconds since last interaction
    when a user interacts with paused media.
    
    Bug: 1026079
    Change-Id: I72d322d66b369e679c9f123d76c0bb4d5a5c9092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1922976
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717776}

--- chrome/browser/ui/global_media_controls/media_notification_service.cc
+++ chrome/browser/ui/global_media_controls/media_notification_service.cc
@@ -95,7 +95,10 @@ void MediaNotificationService::Session::MediaSessionInfoChanged(
 
   // If we've started playing, we don't want the inactive timer to be running.
   if (playing) {
-    inactive_timer_.Stop();
+    if (inactive_timer_.IsRunning()) {
+      RecordInteractionDelayAfterPause();
+      inactive_timer_.Stop();
+    }
     return;
   }
 
@@ -103,6 +106,7 @@ void MediaNotificationService::Session::MediaSessionInfoChanged(
   if (inactive_timer_.IsRunning())
     return;
 
+  last_interaction_time_ = base::TimeTicks::Now();
   StartInactiveTimer();
 }
 
@@ -130,6 +134,9 @@ void MediaNotificationService::Session::OnSessionInteractedWith() {
   if (!inactive_timer_.IsRunning())
     return;
 
+  RecordInteractionDelayAfterPause();
+  last_interaction_time_ = base::TimeTicks::Now();
+
   // Otherwise, reset the timer.
   inactive_timer_.Stop();
   StartInactiveTimer();
@@ -152,6 +159,15 @@ void MediaNotificationService::Session::OnInactiveTimerFired() {
   // If the session has been paused and inactive for long enough, then
   // dismiss it.
   item_->Dismiss();
+}
+
+void MediaNotificationService::Session::RecordInteractionDelayAfterPause() {
+  base::TimeDelta time_since_last_interaction =
+      base::TimeTicks::Now() - last_interaction_time_;
+  base::UmaHistogramCustomTimes(
+      "Media.GlobalMediaControls.InteractionDelayAfterPause",
+      time_since_last_interaction, base::TimeDelta::FromMinutes(1),
+      base::TimeDelta::FromDays(1), 100);
 }
 
 MediaNotificationService::MediaNotificationService(
