74ee574d12c94a7a461244154904cb08231bf1f8
Kevin Ellis <kevers@chromium.org>
2019-11-22 00:06:17 +0000

    Align Animation::setStartTime with web-animation spec
    BUG: 960944
    Change-Id: I587922a712387250e6e20271f79a9bd0496291dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925337
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717915}

--- third_party/blink/renderer/core/animation/animation.h
+++ third_party/blink/renderer/core/animation/animation.h
@@ -290,7 +290,6 @@ class CORE_EXPORT Animation : public EventTargetWithInlineData,
   base::Optional<double> CalculateStartTime(double current_time) const;
   double CalculateCurrentTime() const;
 
-  void SetStartTimeInternal(base::Optional<double>);
   void UpdateCurrentTimingState(TimingUpdateReason);
 
   void BeginUpdatingState();
