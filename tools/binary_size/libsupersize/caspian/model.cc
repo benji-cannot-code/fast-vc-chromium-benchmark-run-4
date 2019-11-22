381c1ba4f3c9908a1fc4e6be75f87defe04309a0
Jasper Chapman-Black <jaspercb@chromium.org>
2019-11-22 19:52:58 +0000

    SuperSize: Caspian: Fill srcPath and component fields in JS output
    Change-Id: I7b26a634d40becde771a86cd9c1e87484e521cad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931154
    Auto-Submit: Jasper Chapman-Black <jaspercb@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718246}

--- tools/binary_size/libsupersize/caspian/model.cc
+++ tools/binary_size/libsupersize/caspian/model.cc
@@ -329,6 +329,12 @@ void TreeNode::WriteIntoJson(
       (*out)["idPath"] = std::string(symbol->TemplateName());
       (*out)["fullName"] = std::string(symbol->FullName());
     }
+    if (symbol->SourcePath()) {
+      (*out)["srcPath"] = symbol->SourcePath();
+    }
+    if (symbol->Component()) {
+      (*out)["component"] = symbol->Component();
+    }
   } else {
     (*out)["idPath"] = std::string(this->id_path);
   }
