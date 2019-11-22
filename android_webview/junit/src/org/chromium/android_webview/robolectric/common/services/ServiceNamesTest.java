ae40f22b97ceecbdbdcb995836047c7c9b2fd05d
Nate Fischer <ntfschr@chromium.org>
2019-11-22 22:58:45 +0000

    AW: implement developer UI service
    No change to production behavior.
    
    This extends https://crrev.com/c/1910811 by sending the toggled flag
    state to a central service (which will, in a later CL, distribute this
    information to the WebView implementation running within apps).
    
    For simplicity, both the UI and the service maintain copies of the full
    flag override state (rather than the UI sending only updates).
    
    Bug: 981143
    Test: run_android_webview_junit_tests -f *ServiceNamesTest.*
    Change-Id: I8644c730a9641fe44d6177bca8019dd8060ca743
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1923500
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718340}

--- android_webview/junit/src/org/chromium/android_webview/robolectric/common/services/ServiceNamesTest.java
+++ android_webview/junit/src/org/chromium/android_webview/robolectric/common/services/ServiceNamesTest.java
@@ -14,6 +14,7 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.android_webview.common.services.ServiceNames;
 import org.chromium.android_webview.services.CrashReceiverService;
+import org.chromium.android_webview.services.DeveloperUiService;
 import org.chromium.android_webview.services.VariationsSeedServer;
 import org.chromium.testing.local.LocalRobolectricTestRunner;
 
@@ -26,6 +27,8 @@ public class ServiceNamesTest {
     public void testServiceNamesValid() {
         Assert.assertEquals("Incorrect class name constant", CrashReceiverService.class.getName(),
                 ServiceNames.CRASH_RECEIVER_SERVICE);
+        Assert.assertEquals("Incorrect class name constant", DeveloperUiService.class.getName(),
+                ServiceNames.DEVELOPER_UI_SERVICE);
         Assert.assertEquals("Incorrect class name constant", VariationsSeedServer.class.getName(),
                 ServiceNames.VARIATIONS_SEED_SERVER);
     }
