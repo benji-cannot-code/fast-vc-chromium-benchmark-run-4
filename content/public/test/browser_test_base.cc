98eb1019b033cc78b99efdb55bac652a60b5363d
Matt Falkenhagen <falken@chromium.org>
2019-11-22 07:29:37 +0000

    Convert content/public/test to Once/Repeating callbacks (pt 1)
    Bug: 1007763
    Change-Id: I2d0a7c9f1fea7b4357c2f961290b40bcac29af8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930281
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718053}

--- content/public/test/browser_test_base.cc
+++ content/public/test/browser_test_base.cc
@@ -378,7 +378,7 @@ void BrowserTestBase::SetUp() {
   base::FeatureList::ClearInstanceForTesting();
 
   auto created_main_parts_closure =
-      std::make_unique<CreatedMainPartsClosure>(base::Bind(
+      std::make_unique<CreatedMainPartsClosure>(base::BindOnce(
           &BrowserTestBase::CreatedBrowserMainParts, base::Unretained(this)));
 
 #if defined(OS_ANDROID)
@@ -457,8 +457,8 @@ void BrowserTestBase::SetUp() {
     base::RunLoop loop{base::RunLoop::Type::kNestableTasksAllowed};
 
     auto ui_task = std::make_unique<base::OnceClosure>(
-        base::Bind(&BrowserTestBase::WaitUntilJavaIsReady,
-                   base::Unretained(this), loop.QuitClosure()));
+        base::BindOnce(&BrowserTestBase::WaitUntilJavaIsReady,
+                       base::Unretained(this), loop.QuitClosure()));
 
     // The MainFunctionParams must out-live all the startup tasks running.
     MainFunctionParams params(*command_line);
@@ -682,8 +682,8 @@ void BrowserTestBase::ProxyRunTestOnMainThreadLoop() {
     base::RunLoop run_loop;
     TracingController::GetInstance()->StopTracing(
         TracingControllerImpl::CreateFileEndpoint(
-            trace_file, base::Bind(&TraceStopTracingComplete,
-                                   run_loop.QuitClosure(), trace_file)));
+            trace_file, base::BindOnce(&TraceStopTracingComplete,
+                                       run_loop.QuitClosure(), trace_file)));
     run_loop.Run();
   }
 
