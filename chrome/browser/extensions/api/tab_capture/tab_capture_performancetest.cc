dd33034722ce0ddc69abf8ea96590c105f6682e9
Ilia Samsonov <isamsonov@google.com>
2019-11-22 19:48:30 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=miu@chromium.org
    
    Bug: 1023677
    Change-Id: I7768ea1e922ef354176a396e34928e57780ad68c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927004
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#718238}

--- chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_performancetest.cc
@@ -334,7 +334,7 @@ IN_PROC_BROWSER_TEST_P(TabCapturePerformanceTest, Performance) {
 // On ChromeOS, software compositing is not an option, and using MSAN on
 // ChromeOS causes problems due to its hardware OpenGL library.
 #if !defined(MEMORY_SANITIZER)
-INSTANTIATE_TEST_SUITE_P(,
+INSTANTIATE_TEST_SUITE_P(All,
                          TabCapturePerformanceTest,
                          testing::Values(kUseGpu,
                                          kTestThroughWebRTC | kUseGpu));
@@ -343,7 +343,7 @@ INSTANTIATE_TEST_SUITE_P(,
 #else
 
 // Run everything on non-ChromeOS platforms.
-INSTANTIATE_TEST_SUITE_P(,
+INSTANTIATE_TEST_SUITE_P(All,
                          TabCapturePerformanceTest,
                          testing::Values(0,
                                          kUseGpu,
