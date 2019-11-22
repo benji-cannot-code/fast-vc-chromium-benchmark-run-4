7d5719f422adfb67af41f8bd51e46e5bcb658585
Elly Fong-Jones <ellyjones@chromium.org>
2019-11-22 00:36:36 +0000

    app_modal: remove DialogClientView dependency
    TBR=avi@chromium.org
    
    Bug: 1011446
    Change-Id: Id7d024a9e5c5971f874c187f07fa2b80863c9dda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929973
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717929}

--- components/app_modal/views/javascript_app_modal_dialog_views.cc
+++ components/app_modal/views/javascript_app_modal_dialog_views.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/message_box_view.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/widget/widget.h"
-#include "ui/views/window/dialog_client_view.h"
 
 namespace app_modal {
 
@@ -66,11 +65,11 @@ void JavaScriptAppModalDialogViews::CloseAppModalDialog() {
 }
 
 void JavaScriptAppModalDialogViews::AcceptAppModalDialog() {
-  GetDialogClientView()->AcceptWindow();
+  AcceptDialog();
 }
 
 void JavaScriptAppModalDialogViews::CancelAppModalDialog() {
-  GetDialogClientView()->CancelWindow();
+  CancelDialog();
 }
 
 bool JavaScriptAppModalDialogViews::IsShowing() const {
