5a531b9b241bd6eb50174264b71077a5e9182b00
Pavel Feldman <pfeldman@chromium.org>
2019-11-22 21:18:23 +0000

    DevTools: follow up to Storage.setCookies, fix browser endpoint.
    Change-Id: I18672227a10a1988ce252a8fbf528684c317714d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931267
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718280}

--- third_party/blink/web_tests/http/tests/inspector-protocol/storage/cookies.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/storage/cookies.js
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
-  const {dp} = await testRunner.startBlank(
+  const {dp, page} = await testRunner.startBlank(
       `Tests that cookies are read and written.`);
 
-  async function logCookies(title) {
-    var data = (await dp.Storage.getCookies()).result;
+  async function logCookies(title, browserContextId) {
+    var data = (await dp.Storage.getCookies({ browserContextId })).result;
     testRunner.log(`\n${title}: ${data.cookies.length} cookies`);
     data.cookies.sort((a, b) => a.name.localeCompare(b.name));
     for (var cookie of data.cookies) {
@@ -41,21 +41,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
 
-  await logCookies('Initial');
+  testRunner.runTestSuite([
+    async function testPageContext() {
+      await logCookies('Initial');
 
-  await setCookieViaFetch();
-  await logCookies('Post-fetch');
+      await setCookieViaFetch();
+      await logCookies('Post-fetch');
 
-  const cookies = [
-    {url: 'http://127.0.0.1', name: 'foo', value: 'bar1'},
-    {url: 'http://127.0.0.1', name: 'foo', value: 'second bar2'},
-    {url: 'http://127.0.0.1', name: 'foo2', value: 'bar1'}
-  ];
-  await dp.Storage.setCookies({cookies});
-  await logCookies('Post-set');
+      const cookies = [
+        {url: 'http://127.0.0.1', name: 'foo', value: 'bar1'},
+        {url: 'http://127.0.0.1', name: 'foo', value: 'second bar2'},
+        {url: 'http://127.0.0.1', name: 'foo2', value: 'bar1'}
+      ];
+      await dp.Storage.setCookies({cookies});
+      await logCookies('Post-set');
 
-  await dp.Storage.clearCookies();
-  await logCookies('Post-clear');
+      await dp.Storage.clearCookies();
+      await logCookies('Post-clear');
+    },
 
-  testRunner.completeTest();
+    async function testInvalidContext() {
+      var data = (await dp.Storage.getCookies({ browserContextId: 'invalid' }));
+      testRunner.log(data);
+    }
+  ]);
+
 })
