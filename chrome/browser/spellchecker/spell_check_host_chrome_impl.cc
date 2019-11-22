d505648ef6167dc204b2920e60ea5bcaa8b1b232
Evan Stade <estade@chromium.org>
2019-11-22 22:57:38 +0000

    Spellcheck cleanups
    Introduce a build flag for the remote spelling service, and stop
    compiling/instantiating SpellingServiceClient on platforms that
    don't use it (Android). Also try to reduce the number of platform
    ifdefs in spellcheck code, in favor of buildflag usage. Reduce
    the amount of code that's compiled but never used by removing
    several SpellCheckHostImpl methods on mac/win.
    
    Also remove a couple of obsolete includes.
    
    Change-Id: I5697a5d3f891f69159d5bba855ee72e11a3ca8be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927412
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718338}

--- chrome/browser/spellchecker/spell_check_host_chrome_impl.cc
+++ chrome/browser/spellchecker/spell_check_host_chrome_impl.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 
-#if BUILDFLAG(USE_BROWSER_SPELLCHECKER)
+#if BUILDFLAG(USE_BROWSER_SPELLCHECKER) && BUILDFLAG(ENABLE_SPELLING_SERVICE)
 #include "chrome/browser/spellchecker/spelling_request.h"
 #endif
 
@@ -151,8 +151,7 @@ std::vector<SpellCheckResult> SpellCheckHostChromeImpl::FilterCustomWordResults(
 }
 #endif  // BUILDFLAG(USE_RENDERER_SPELLCHECKER)
 
-#if defined(OS_MACOSX) || defined(OS_WIN)
-
+#if BUILDFLAG(USE_BROWSER_SPELLCHECKER) && BUILDFLAG(ENABLE_SPELLING_SERVICE)
 void SpellCheckHostChromeImpl::CheckSpelling(const base::string16& word,
                                              int route_id,
                                              CheckSpellingCallback callback) {
@@ -200,7 +199,8 @@ void SpellCheckHostChromeImpl::CombineResultsForTesting(
     const std::vector<SpellCheckResult>& local_results) {
   SpellingRequest::CombineResults(remote_results, local_results);
 }
-#endif  // defined(OS_MACOSX) || defined(OS_WIN)
+#endif  //  BUILDFLAG(USE_BROWSER_SPELLCHECKER) &&
+        //  BUILDFLAG(ENABLE_SPELLING_SERVICE)
 
 #if defined(OS_MACOSX)
 int SpellCheckHostChromeImpl::ToDocumentTag(int route_id) {
--- chrome/browser/spellchecker/spellcheck_service.cc
+++ chrome/browser/spellchecker/spellcheck_service.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/spellchecker/spellcheck_factory.h"
 #include "chrome/browser/spellchecker/spellcheck_hunspell_dictionary.h"
-#include "chrome/common/pref_names.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/prefs/pref_member.h"
 #include "components/prefs/pref_service.h"
