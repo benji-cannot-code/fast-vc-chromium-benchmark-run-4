c0d421eccea8221045f4a513eef6c72f6d5eea70
David Maunder <davidjm@chromium.org>
2019-11-22 19:22:46 +0000

    Log metrics about user feedback from Tab Suggestions
    Bug: 1026068
    Change-Id: Iddb82387326f988f5530eb08380d37dfb3f6d716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927378
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718230}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/suggestions/TabSuggestionsOrchestrator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/suggestions/TabSuggestionsOrchestrator.java
@@ -5,14 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management.suggestions;
 
+import org.chromium.base.Log;
 import org.chromium.base.ObserverList;
+import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.lifecycle.Destroyable;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Represents the entry point for the TabSuggestions component. Responsible for
@@ -20,7 +25,7 @@ import java.util.List;
  */
 public class TabSuggestionsOrchestrator implements TabSuggestions, Destroyable {
     public static final String TAB_SUGGESTIONS_UMA_PREFIX = "TabSuggestionsOrchestrator";
-    private static final String TAG = "TabSuggestionsDetailed";
+    private static final String TAG = "TabSuggestDetailed";
     private static final int MIN_CLOSE_SUGGESTIONS_THRESHOLD = 3;
 
     protected TabContextObserver mTabContextObserver;
@@ -134,8 +139,43 @@ public class TabSuggestionsOrchestrator implements TabSuggestions, Destroyable {
     }
 
     public void onTabSuggestionFeedback(TabSuggestionFeedback tabSuggestionFeedback) {
+        if (tabSuggestionFeedback == null) {
+            Log.e(TAG, "TabSuggestionFeedback is null");
+            return;
+        }
         // Record TabSuggestionFeedback for testing purposes
         mTabSuggestionFeedback = tabSuggestionFeedback;
-        // TODO(crbug.com/1026068) log tab suggestion feedback
+
+        if (tabSuggestionFeedback.tabSuggestionResponse
+                == TabSuggestionFeedback.TabSuggestionResponse.NOT_CONSIDERED) {
+            RecordUserAction.record("TabsSuggestions.Close.SuggestionsReview.Dismissed");
+        } else {
+            RecordUserAction.record("TabsSuggestions.Close.SuggestionsReview.Accepted");
+            if (tabSuggestionFeedback.tabSuggestionResponse
+                    == TabSuggestionFeedback.TabSuggestionResponse.ACCEPTED) {
+                RecordUserAction.record("TabsSuggestions.Close.Accepted");
+            } else {
+                RecordUserAction.record("TabsSuggestions.Close.Dismissed");
+            }
+        }
+
+        Set<Integer> suggestedTabIds = new HashSet<>();
+        for (TabContext.TabInfo tabInfo : tabSuggestionFeedback.tabSuggestion.getTabsInfo()) {
+            suggestedTabIds.add(tabInfo.id);
+        }
+
+        int numSelectFromSuggestion = 0;
+        int numSelectOutsideSuggestion = 0;
+        for (int selectedTabId : tabSuggestionFeedback.selectedTabIds) {
+            if (suggestedTabIds.contains(selectedTabId)) {
+                numSelectFromSuggestion++;
+            } else {
+                numSelectOutsideSuggestion++;
+            }
+        }
+        int numChanged = tabSuggestionFeedback.tabSuggestion.getTabsInfo().size()
+                - numSelectFromSuggestion + numSelectOutsideSuggestion;
+        RecordHistogram.recordCount100Histogram(
+                "TabsSuggestions.Close.NumSuggestionsChanged", numChanged);
     }
 }
--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/suggestions/TabSuggestionsOrchestratorTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/suggestions/TabSuggestionsOrchestratorTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -25,6 +26,8 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
+import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModelFilter;
@@ -81,6 +84,14 @@ public class TabSuggestionsOrchestratorTest {
                 .addTabModelFilterObserver(any(TabModelObserver.class));
         doReturn(mTabModelFilter).when(mTabModelFilterProvider).getCurrentTabModelFilter();
         doReturn(null).when(mTabModelFilter).getRelatedTabList(anyInt());
+        RecordUserAction.setDisabledForTests(true);
+        RecordHistogram.setDisabledForTests(true);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        RecordUserAction.setDisabledForTests(false);
+        RecordHistogram.setDisabledForTests(false);
     }
 
     @Test
@@ -151,7 +162,9 @@ public class TabSuggestionsOrchestratorTest {
             @Override
             public void onNewSuggestion(List<TabSuggestion> tabSuggestions,
                     Callback<TabSuggestionFeedback> tabSuggestionFeedback) {
-                tabSuggestionFeedback.onResult(new TabSuggestionFeedback(null,
+                TabSuggestion tabSuggestion = new TabSuggestion(
+                        Arrays.asList(TabContext.TabInfo.createFromTab(sTabs[0])), 0, "");
+                tabSuggestionFeedback.onResult(new TabSuggestionFeedback(tabSuggestion,
                         TabSuggestionFeedback.TabSuggestionResponse.ACCEPTED, Arrays.asList(1), 1));
             }
 
