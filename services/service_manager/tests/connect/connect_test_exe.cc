25aa7bfd04c55adfc693e631f01ba5d31f1d0e24
Julie Jeongeun Kim <jkim@igalia.com>
2019-11-22 23:00:01 +0000

    Convert Mojo interfaces from connect.test-mojom to new Mojo types
    This CL converts ClassInterface, StandaloneApp, IdentityTest,
    ExposedInterface, BlockedInterface, AlwaysAllowedInterface from
    connect.test-mojom to new Mojo types with using ReceiverSet,
    PendingReceiver, and Receiver instead of BindingSet,
    InterfaceRequest, and binding.
    
    It also introduces a new variant of Connect() which takes callback
    to Connector.
    
    Bug: 955171
    Change-Id: Ia7a1d5a48607018419b8b4d36887fee65f2f237c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930364
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#718341}

--- services/service_manager/tests/connect/connect_test_exe.cc
+++ services/service_manager/tests/connect/connect_test_exe.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/macros.h"
 #include "base/task/single_thread_task_executor.h"
-#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/service.h"
 #include "services/service_manager/public/cpp/service_binding.h"
@@ -17,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/tests/connect/connect.test-mojom.h"
 
 using service_manager::test::mojom::ConnectTestService;
-using service_manager::test::mojom::ConnectTestServiceRequest;
 
 namespace {
 
@@ -40,8 +41,8 @@ class Target : public service_manager::Service,
     registry_.BindInterface(interface_name, std::move(interface_pipe));
   }
 
-  void Create(ConnectTestServiceRequest request) {
-    bindings_.AddBinding(this, std::move(request));
+  void Create(mojo::PendingReceiver<ConnectTestService> receiver) {
+    receivers_.Add(this, std::move(receiver));
   }
 
   // ConnectTestService:
@@ -55,7 +56,7 @@ class Target : public service_manager::Service,
 
   service_manager::ServiceBinding service_binding_;
   service_manager::BinderRegistry registry_;
-  mojo::BindingSet<ConnectTestService> bindings_;
+  mojo::ReceiverSet<ConnectTestService> receivers_;
 
   DISALLOW_COPY_AND_ASSIGN(Target);
 };
