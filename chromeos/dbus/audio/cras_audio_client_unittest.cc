52e29256ae257386343d7237962c9f86a27f23ca
Reilly Grant <reillyg@chromium.org>
2019-11-22 00:02:21 +0000

    Convert chromeos/dbus/audio to base::Bind and base::Callback to Once/Repeating
    Bug: 1007656
    Change-Id: I55a712986341ea9f2f4aa162d16e4eb7ccb3c1dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925192
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#717911}

--- chromeos/dbus/audio/cras_audio_client_unittest.cc
+++ chromeos/dbus/audio/cras_audio_client_unittest.cc
@@ -373,8 +373,8 @@ class CrasAudioClientTest : public testing::Test {
 
  protected:
   // A callback to intercept and check the method call arguments.
-  typedef base::Callback<void(dbus::MessageReader* reader)>
-      ArgumentCheckCallback;
+  using ArgumentCheckCallback =
+      base::RepeatingCallback<void(dbus::MessageReader* reader)>;
 
   // Sets expectations for called method name and arguments, and sets response.
   void PrepareForMethodCall(const std::string& method_name,
@@ -830,7 +830,7 @@ TEST_F(CrasAudioClientTest, GetNodes) {
   WriteNodesToResponse(expected_node_list, &writer);
 
   // Set expectations.
-  PrepareForMethodCall(cras::kGetNodes, base::Bind(&ExpectNoArgument),
+  PrepareForMethodCall(cras::kGetNodes, base::BindRepeating(&ExpectNoArgument),
                        response.get());
   // Call method.
   bool called = false;
@@ -851,7 +851,7 @@ TEST_F(CrasAudioClientTest, GetNodesV2) {
   WriteNodesToResponse(expected_node_list, &writer);
 
   // Set expectations.
-  PrepareForMethodCall(cras::kGetNodes, base::Bind(&ExpectNoArgument),
+  PrepareForMethodCall(cras::kGetNodes, base::BindRepeating(&ExpectNoArgument),
                        response.get());
 
   // Call method.
@@ -872,7 +872,7 @@ TEST_F(CrasAudioClientTest, SetOutputNodeVolume) {
   // Set expectations.
   PrepareForMethodCall(
       cras::kSetOutputNodeVolume,
-      base::Bind(&ExpectUint64AndInt32Arguments, kNodeId, kVolume),
+      base::BindRepeating(&ExpectUint64AndInt32Arguments, kNodeId, kVolume),
       response.get());
   // Call method.
   client()->SetOutputNodeVolume(kNodeId, kVolume);
@@ -887,7 +887,7 @@ TEST_F(CrasAudioClientTest, SetOutputUserMute) {
 
   // Set expectations.
   PrepareForMethodCall(cras::kSetOutputUserMute,
-                       base::Bind(&ExpectBoolArgument, kUserMuteOn),
+                       base::BindRepeating(&ExpectBoolArgument, kUserMuteOn),
                        response.get());
   // Call method.
   client()->SetOutputUserMute(kUserMuteOn);
@@ -904,7 +904,7 @@ TEST_F(CrasAudioClientTest, SetInputNodeGain) {
   // Set expectations.
   PrepareForMethodCall(
       cras::kSetInputNodeGain,
-      base::Bind(&ExpectUint64AndInt32Arguments, kNodeId, kInputGain),
+      base::BindRepeating(&ExpectUint64AndInt32Arguments, kNodeId, kInputGain),
       response.get());
   // Call method.
   client()->SetInputNodeGain(kNodeId, kInputGain);
@@ -919,7 +919,7 @@ TEST_F(CrasAudioClientTest, SetInputMute) {
 
   // Set expectations.
   PrepareForMethodCall(cras::kSetInputMute,
-                       base::Bind(&ExpectBoolArgument, kInputMuteOn),
+                       base::BindRepeating(&ExpectBoolArgument, kInputMuteOn),
                        response.get());
   // Call method.
   client()->SetInputMute(kInputMuteOn);
@@ -934,7 +934,7 @@ TEST_F(CrasAudioClientTest, SetActiveOutputNode) {
 
   // Set expectations.
   PrepareForMethodCall(cras::kSetActiveOutputNode,
-                       base::Bind(&ExpectUint64Argument, kNodeId),
+                       base::BindRepeating(&ExpectUint64Argument, kNodeId),
                        response.get());
   // Call method.
   client()->SetActiveOutputNode(kNodeId);
@@ -949,7 +949,7 @@ TEST_F(CrasAudioClientTest, SetActiveInputNode) {
 
   // Set expectations.
   PrepareForMethodCall(cras::kSetActiveInputNode,
-                       base::Bind(&ExpectUint64Argument, kNodeId),
+                       base::BindRepeating(&ExpectUint64Argument, kNodeId),
                        response.get());
   // Call method.
   client()->SetActiveInputNode(kNodeId);
@@ -964,7 +964,7 @@ TEST_F(CrasAudioClientTest, AddActiveInputNode) {
 
   // Set expectations.
   PrepareForMethodCall(cras::kAddActiveInputNode,
-                       base::Bind(&ExpectUint64Argument, kNodeId),
+                       base::BindRepeating(&ExpectUint64Argument, kNodeId),
                        response.get());
   // Call method.
   client()->AddActiveInputNode(kNodeId);
@@ -979,7 +979,7 @@ TEST_F(CrasAudioClientTest, RemoveActiveInputNode) {
 
   // Set expectations.
   PrepareForMethodCall(cras::kRemoveActiveInputNode,
-                       base::Bind(&ExpectUint64Argument, kNodeId),
+                       base::BindRepeating(&ExpectUint64Argument, kNodeId),
                        response.get());
   // Call method.
   client()->RemoveActiveInputNode(kNodeId);
@@ -994,7 +994,7 @@ TEST_F(CrasAudioClientTest, AddActiveOutputNode) {
 
   // Set expectations.
   PrepareForMethodCall(cras::kAddActiveOutputNode,
-                       base::Bind(&ExpectUint64Argument, kNodeId),
+                       base::BindRepeating(&ExpectUint64Argument, kNodeId),
                        response.get());
   // Call method.
   client()->AddActiveOutputNode(kNodeId);
@@ -1009,7 +1009,7 @@ TEST_F(CrasAudioClientTest, RemoveActiveOutputNode) {
 
   // Set expectations.
   PrepareForMethodCall(cras::kRemoveActiveOutputNode,
-                       base::Bind(&ExpectUint64Argument, kNodeId),
+                       base::BindRepeating(&ExpectUint64Argument, kNodeId),
                        response.get());
   // Call method.
   client()->RemoveActiveOutputNode(kNodeId);
@@ -1026,7 +1026,7 @@ TEST_F(CrasAudioClientTest, SwapLeftRight) {
   // Set expectations.
   PrepareForMethodCall(
       cras::kSwapLeftRight,
-      base::Bind(&ExpectUint64AndBoolArguments, kNodeId, kSwap),
+      base::BindRepeating(&ExpectUint64AndBoolArguments, kNodeId, kSwap),
       response.get());
   // Call method.
   client()->SwapLeftRight(kNodeId, kSwap);
@@ -1043,7 +1043,8 @@ TEST_F(CrasAudioClientTest, SetGlobalOutputChannelRemix) {
   // Set expectations.
   PrepareForMethodCall(
       cras::kSetGlobalOutputChannelRemix,
-      base::Bind(&ExpectInt32AndArrayOfDoublesArguments, kChannels, kMixer),
+      base::BindRepeating(&ExpectInt32AndArrayOfDoublesArguments, kChannels,
+                          kMixer),
       response.get());
 
   // Call method.
