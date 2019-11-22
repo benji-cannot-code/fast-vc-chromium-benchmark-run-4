e3d5bbfdcc2af0e2621953f62516b8602c42abd6
Sangwoo Ko <sangwoo.ko@navercorp.com>
2019-11-22 15:44:11 +0000

    Align FullscreenPopupControl animation to the compositor.
    Adapt AnimationDelegateViews to FullscreenPopupControl, which aligns
    animations to compositor's frame tick.
    
    There’s no intentional behavior change.
    
    Bug: 824026
    Change-Id: I0db486527f8a659ab31654cee9a1870ee29a403b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930273
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718144}

--- chrome/browser/ui/views/fullscreen_control/fullscreen_control_popup.h
+++ chrome/browser/ui/views/fullscreen_control/fullscreen_control_popup.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/native_widget_types.h"
+#include "ui/views/animation/animation_delegate_views.h"
 
 namespace views {
 class Widget;
@@ -23,7 +24,7 @@ class FullscreenControlView;
 
 // FullscreenControlPopup is a helper class that holds a FullscreenControlView
 // and allows showing and hiding the view with a drop down animation.
-class FullscreenControlPopup : public gfx::AnimationDelegate {
+class FullscreenControlPopup : public views::AnimationDelegateViews {
  public:
   FullscreenControlPopup(gfx::NativeView parent_view,
                          const base::RepeatingClosure& on_button_pressed,
@@ -56,7 +57,10 @@ class FullscreenControlPopup : public gfx::AnimationDelegate {
   FullscreenControlView* control_view() { return control_view_; }
 
  private:
-  // gfx::AnimationDelegate:
+  FullscreenControlPopup(std::unique_ptr<views::Widget> popup,
+                         const base::RepeatingClosure& on_visibility_changed);
+
+  // views::AnimationDelegateViews:
   void AnimationProgressed(const gfx::Animation* animation) override;
   void AnimationEnded(const gfx::Animation* animation) override;
 
