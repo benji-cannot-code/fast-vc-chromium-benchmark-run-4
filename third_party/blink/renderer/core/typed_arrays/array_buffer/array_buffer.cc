8437a1f1eb7f1a72a32523ea85327ba3486f8649
Andreas Haas <ahaas@chromium.org>
2019-11-22 13:00:17 +0000

    Revert "[arraybuffer] Organize views in a hash_set"
    This reverts commit f366e7ddfd489752b713fd6bd3d78e59d9b63429.
    
    Reason for revert: Performance regression, see https://crbug.com/1026763.
    The original implementation optimized better for the case where there exists
    exactly one view.
    
    Original change's description:
    > [arraybuffer] Organize views in a hash_set
    >
    > This is a first step of getting rid of blink::ArrayBuffer.
    >
    > blink::ArrayBuffer serves two purposes by now:
    > 1) Managing a list of ArrayBufferViews;
    > 2) Managing the detached flag;
    >
    > With this CL I encapsulate the list of ArrayBufferViews. In follow-up
    > CLs I then want to pass the list and an ArrayBufferContents object
    > to the ArrayBufferView instead of the ArrayBuffer object. This will
    > bring us one step closer to removing blink::ArrayBuffer.
    >
    > Update: ArrayBuffer seems to be very involved. It is the connecting
    > data structure between DOMArrayBuffer, ArrayBufferContents, and
    > ArrayBufferView. Even though it is not used anymore outside these
    > classes, it is not obvious if we can replace it within these classes.
    > I still think that refactoring the organization of views here is
    > good.
    >
    > R=​haraken@chromium.org
    >
    > Bug: chromium:1008840
    > Change-Id: I533f5496b7c51312e77a0138ac8249f9f0e55ffd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911207
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Andreas Haas <ahaas@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#714880}
    
    TBR=haraken@chromium.org,ahaas@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:1008840, chromium:1026763
    Change-Id: Ifad3b3b8cf46290b9bf0f2d4ef958fdd7c414ed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930817
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718117}

--- third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer.cc
+++ third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer.cc
@@ -31,13 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-ArrayBuffer::ArrayBuffer(ArrayBufferContents& contents) : is_detached_(false) {
-  if (contents.IsShared())
-    contents.ShareWith(contents_);
-  else
-    contents.Transfer(contents_);
-}
-
 bool ArrayBuffer::Transfer(ArrayBufferContents& result) {
   DCHECK(!IsShared());
   scoped_refptr<ArrayBuffer> keep_alive(this);
@@ -48,19 +41,19 @@ bool ArrayBuffer::Transfer(ArrayBufferContents& result) {
   }
 
   bool all_views_are_detachable = true;
-  for (auto* view : views_) {
-    if (!view->IsDetachable()) {
+  for (ArrayBufferView* i = first_view_; i; i = i->next_view_) {
+    if (!i->IsDetachable())
       all_views_are_detachable = false;
-    }
   }
 
   if (all_views_are_detachable) {
     contents_.Transfer(result);
 
-    for (auto* view : views_) {
-      view->Detach();
+    while (first_view_) {
+      ArrayBufferView* current = first_view_;
+      RemoveView(current);
+      current->Detach();
     }
-    views_.clear();
 
     is_detached_ = true;
   } else {
@@ -104,12 +97,23 @@ bool ArrayBuffer::ShareNonSharedForInternalUse(ArrayBufferContents& result) {
 }
 
 void ArrayBuffer::AddView(ArrayBufferView* view) {
-  views_.insert(view);
+  view->buffer_ = this;
+  view->prev_view_ = nullptr;
+  view->next_view_ = first_view_;
+  if (first_view_)
+    first_view_->prev_view_ = view;
+  first_view_ = view;
 }
 
 void ArrayBuffer::RemoveView(ArrayBufferView* view) {
-  DCHECK(views_.Contains(view));
-  views_.erase(view);
+  DCHECK_EQ(this, view->buffer_.get());
+  if (view->next_view_)
+    view->next_view_->prev_view_ = view->prev_view_;
+  if (view->prev_view_)
+    view->prev_view_->next_view_ = view->next_view_;
+  if (first_view_ == view)
+    first_view_ = view->next_view_;
+  view->prev_view_ = view->next_view_ = nullptr;
 }
 
 }  // namespace blink
