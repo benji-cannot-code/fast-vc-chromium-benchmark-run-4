43aa038f9784475c676fd4cb40d60cddb66d3259
shrekshao <shrekshao@google.com>
2019-11-22 01:25:09 +0000

    Optimize MaxVertexAccessed for unused path
    https://pinpoint-dot-chromeperf.appspot.com/job/17af1011220000
    Reported by pinpoint: thread_total_rendering_cpu_time_per_frame: 27.56 → 28.79 (+1.228) (+4.454%)
    
    https://pinpoint-dot-chromeperf.appspot.com/job/142f245a220000
    Optimize attempt for this performance regression shows a -3.4% improvement for thread_renderer_main_cpu_time_per_frame
    
    Bug: 1026774
    Change-Id: I35cc020411ebe033a9b77a80be49b7ecac8d96b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929756
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Shrek Shao <shrekshao@google.com>
    Cr-Commit-Position: refs/heads/master@{#717955}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -2195,9 +2195,7 @@ class GLES2DecoderImpl : public GLES2Decoder,
   bool SimulateFixedAttribs(const char* function_name,
                             GLuint max_vertex_accessed,
                             bool* simulated,
-                            GLsizei primcount,
-                            GLint basevertex,
-                            GLuint baseinstance);
+                            GLsizei primcount);
   void RestoreStateForSimulatedFixedAttribs();
 
   bool CheckMultiDrawArraysVertices(const char* function_name,
@@ -11061,9 +11059,7 @@ void GLES2DecoderImpl::RestoreStateForAttrib(
 bool GLES2DecoderImpl::SimulateFixedAttribs(const char* function_name,
                                             GLuint max_vertex_accessed,
                                             bool* simulated,
-                                            GLsizei primcount,
-                                            GLint basevertex,
-                                            GLuint baseinstance) {
+                                            GLsizei primcount) {
   DCHECK(simulated);
   *simulated = false;
   if (gl_version_info().SupportsFixedType())
@@ -11090,8 +11086,8 @@ bool GLES2DecoderImpl::SimulateFixedAttribs(const char* function_name,
     const VertexAttrib* attrib = *it;
     const Program::VertexAttrib* attrib_info =
         state_.current_program->GetAttribInfoByLocation(attrib->index());
-    GLuint max_accessed = attrib->MaxVertexAccessed(
-        primcount, max_vertex_accessed, basevertex, baseinstance);
+    GLuint max_accessed =
+        attrib->MaxVertexAccessed(primcount, max_vertex_accessed);
     GLuint num_vertices = max_accessed + 1;
     if (num_vertices == 0) {
       LOCAL_SET_GL_ERROR(
@@ -11136,8 +11132,8 @@ bool GLES2DecoderImpl::SimulateFixedAttribs(const char* function_name,
     const VertexAttrib* attrib = *it;
     const Program::VertexAttrib* attrib_info =
         state_.current_program->GetAttribInfoByLocation(attrib->index());
-    GLuint max_accessed = attrib->MaxVertexAccessed(
-        primcount, max_vertex_accessed, basevertex, baseinstance);
+    GLuint max_accessed =
+        attrib->MaxVertexAccessed(primcount, max_vertex_accessed);
     GLuint num_vertices = max_accessed + 1;
     if (num_vertices == 0) {
       LOCAL_SET_GL_ERROR(
@@ -11385,11 +11381,9 @@ ALWAYS_INLINE error::Error GLES2DecoderImpl::DoMultiDrawArrays(
   bool simulated_fixed_attribs = false;
   // The branch with fixed attrib is not meant to be used
   // normally but just to pass OpenGL ES 2 conformance where there's no
-  // basevertex and baseinstance support. So just pass in 0 for base vertex and
-  // base instance.
+  // basevertex and baseinstance support.
   if (SimulateFixedAttribs(function_name, total_max_vertex_accessed,
-                           &simulated_fixed_attribs, total_max_primcount, 0,
-                           0)) {
+                           &simulated_fixed_attribs, total_max_primcount)) {
     bool textures_set;
     if (!PrepareTexturesForRender(&textures_set, function_name)) {
       return error::kNoError;
@@ -11646,13 +11640,11 @@ ALWAYS_INLINE error::Error GLES2DecoderImpl::DoMultiDrawElements(
     return error::kNoError;
   }
   bool simulated_fixed_attribs = false;
-  // TODO(shrekshao) The branch with fixed attrib is not meant to be used
+  // The branch with fixed attrib is not meant to be used
   // normally But just to pass OpenGL ES 2 conformance where there's no
-  // basevertex and baseinstance support So just pass in 0 for base vertex and
-  // base instance
+  // basevertex and baseinstance support.
   if (SimulateFixedAttribs(function_name, total_max_vertex_accessed,
-                           &simulated_fixed_attribs, total_max_primcount, 0,
-                           0)) {
+                           &simulated_fixed_attribs, total_max_primcount)) {
     bool textures_set;
     if (!PrepareTexturesForRender(&textures_set, function_name)) {
       return error::kNoError;
