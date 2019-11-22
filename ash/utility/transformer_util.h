e132b9734b62bfafc6fefb4a05486324c70147f3
Michael Spang <spang@chromium.org>
2019-11-22 18:43:46 +0000

    Reland "display: Deduplicate display transform code with overlay transform code"
    This reverts commit cf6bd85074022f0813b9823872c407b65d5f9f6d.
    
    Reason for revert: added missing #include
    
    Original change's description:
    > Revert "display: Deduplicate display transform code with overlay transform code"
    > 
    > This reverts commit 4430b4c3a8139905bc2fb6e9612511a8bbfc67bd.
    > 
    > Reason for revert: Possible cause of this failure: https://ci.chromium.org/p/chromium/builders/ci/GPU%20Mac%20Builder/173997
    > 
    > Original change's description:
    > > display: Deduplicate display transform code with overlay transform code
    > > 
    > > Display rotation transforms are a subset of overlay transforms, so we can
    > > reuse OverlayTransformToTransform to compute display rotation matrices.
    > > 
    > > Bug: 1019015
    > > 
    > > Change-Id: I95d099323cb866ccc4b9cea404e1e6f8f2dfff65
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913604
    > > Commit-Queue: Michael Spang <spang@chromium.org>
    > > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > > Reviewed-by: Mitsuru Oshima (OOO - back on Dec 2nd) <oshima@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#717832}
    > 
    > TBR=oshima@chromium.org,kenrb@chromium.org,spang@chromium.org,khushalsagar@chromium.org
    > 
    > Change-Id: I2757890a784f97adf30776c2b418fba2574f1759
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1019015
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928380
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Darren Shen <shend@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#717846}
    
    TBR=oshima@chromium.org,kenrb@chromium.org,spang@chromium.org,khushalsagar@chromium.org,shend@chromium.org
    
    Change-Id: I8988120b1dd114fdc0f5e0d515b9fb90ffe3cbb5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1019015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931159
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718220}

--- ash/utility/transformer_util.h
+++ ash/utility/transformer_util.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ui/display/display.h"
-#include "ui/gfx/overlay_transform.h"
 
 namespace gfx {
 class SizeF;
@@ -23,10 +22,6 @@ ASH_EXPORT gfx::Transform CreateRotationTransform(
     display::Display::Rotation old_rotation,
     display::Display::Rotation new_rotation,
     const gfx::SizeF& size_to_rotate);
-
-// Maps display::Display::Rotation to gfx::OverlayTransform.
-ASH_EXPORT gfx::OverlayTransform DisplayRotationToOverlayTransform(
-    display::Display::Rotation rotation);
 
 }  // namespace ash
 
