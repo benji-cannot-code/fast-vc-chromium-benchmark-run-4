bfa972a78c8d2c7eee53fa9a1e51fe1beafc4093
Ana Salazar <anasalazar@chromium.org>
2019-11-22 20:43:38 +0000

    Cros: Add metrics for transitions in the hotseat
    Report animation smoothness for the transition into the hotseat in the
    HomeLauncher.
    
    Bug: 1022178, 1022177
    Change-Id: I5dc5e76688aa5a88bcabb5d688b419d819932867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1917941
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Nik Bhagat <nikunjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718266}

--- ash/shelf/hotseat_transition_animator.cc
+++ ash/shelf/hotseat_transition_animator.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
+#include "base/metrics/histogram_macros.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animator.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
@@ -20,8 +21,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class HotseatTransitionAnimator::TransitionAnimationMetricsReporter
+    : public ui::AnimationMetricsReporter {
+ public:
+  TransitionAnimationMetricsReporter() {}
+
+  ~TransitionAnimationMetricsReporter() override = default;
+
+  void animating_to_shown_hotseat(bool animating_to_shown_hotseat) {
+    animating_to_shown_hotseat_ = animating_to_shown_hotseat;
+  }
+
+  // ui::AnimationMetricsReporter:
+  void Report(int value) override {
+    if (animating_to_shown_hotseat_) {
+      UMA_HISTOGRAM_PERCENTAGE(
+          "Ash.HotseatTransition.AnimationSmoothness.TransitionToShownHotseat",
+          value);
+    } else {
+      UMA_HISTOGRAM_PERCENTAGE(
+          "Ash.HotseatTransition.AnimationSmoothness."
+          "TransitionFromShownHotseat",
+          value);
+    }
+  }
+
+ private:
+  // Whether the animation reported is transitioning state into a shown hotseat.
+  bool animating_to_shown_hotseat_ = false;
+};
+
 HotseatTransitionAnimator::HotseatTransitionAnimator(ShelfWidget* shelf_widget)
-    : shelf_widget_(shelf_widget) {
+    : shelf_widget_(shelf_widget),
+      animation_metrics_reporter_(
+          std::make_unique<TransitionAnimationMetricsReporter>()) {
   Shell::Get()->tablet_mode_controller()->AddObserver(this);
 }
 
@@ -121,6 +154,8 @@ void HotseatTransitionAnimator::SetAnimationStartProperties(
                                : shelf_widget_->hotseat_widget()
                                      ->GetOpaqueBackground()
                                      ->rounded_corner_radii());
+  animation_metrics_reporter_->animating_to_shown_hotseat(new_state ==
+                                                          HotseatState::kShown);
 }
 
 void HotseatTransitionAnimator::StartAnimation(HotseatState old_state,
@@ -133,6 +168,8 @@ void HotseatTransitionAnimator::StartAnimation(HotseatState old_state,
   shelf_bg_animation_setter.SetTweenType(gfx::Tween::EASE_OUT);
   shelf_bg_animation_setter.SetPreemptionStrategy(
       ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
+  shelf_bg_animation_setter.SetAnimationMetricsReporter(
+      animation_metrics_reporter_.get());
   animation_complete_callback_ = base::BindOnce(
       &HotseatTransitionAnimator::NotifyHotseatTransitionAnimationEnded,
       weak_ptr_factory_.GetWeakPtr(), old_state, new_state);
