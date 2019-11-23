2d53b2f26e24aec60f42118349cd5654e90059b5
Avery Musbach <amusbach@chromium.org>
2019-11-23 02:46:33 +0000

    split view: Fix how the divider is repositioned for orientation changes
    The present CL fixes how the divider position in clamshell split view is
    adjusted when the display orientation changes.
    BEFORE: It is updated in SplitViewController::OnDisplayMetricsChanged,
    with logic meant for tablet mode, such as subtracting half the thickness
    of the divider.
    AFTER: It is updated based on the snapped window bounds after those
    bounds are updated by good old WindowState::AdjustSnappedBounds.
    
    Fixed: 1005501
    Change-Id: I9471cf54a5cc0da3e73090716493d5fea34da9d6
    Bug: 1005501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925237
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718397}

--- ash/wm/splitview/split_view_controller.cc
+++ ash/wm/splitview/split_view_controller.cc
@@ -807,10 +807,8 @@ void SplitViewController::OnWindowBoundsChanged(
     ui::PropertyChangeReason reason) {
   DCHECK_EQ(root_window_, window->GetRootWindow());
 
-  if (!InClamshellSplitViewMode() ||
-      reason == ui::PropertyChangeReason::FROM_ANIMATION) {
+  if (!InClamshellSplitViewMode())
     return;
-  }
 
   WindowState* window_state = WindowState::Get(window);
   const bool is_window_moved = window_state->is_dragged() &&
@@ -1089,6 +1087,11 @@ void SplitViewController::OnDisplayMetricsChanged(
       EndSplitView();
     return;
   }
+
+  // In clamshell split view mode, the divider position will be adjusted in
+  // |OnWindowBoundsChanged|.
+  if (split_view_type_ == SplitViewType::kClamshellType)
+    return;
 
   // Before adjusting the divider position for the new display metrics, if the
   // divider is animating to a snap position, then stop it and shove it there.
@@ -1412,14 +1415,14 @@ void SplitViewController::StopAndShoveAnimatedDivider() {
   UpdateSnappedWindowsAndDividerBounds();
 }
 
-bool SplitViewController::ShouldEndSplitViewAfterResizing() {
-  DCHECK(InSplitViewMode());
+bool SplitViewController::ShouldEndTabletSplitViewAfterResizing() {
+  DCHECK(InTabletSplitViewMode());
 
   return divider_position_ == 0 || divider_position_ == GetDividerEndPosition();
 }
 
 void SplitViewController::EndSplitViewAfterResizingIfAppropriate() {
-  if (!ShouldEndSplitViewAfterResizing())
+  if (!ShouldEndTabletSplitViewAfterResizing())
     return;
 
   aura::Window* active_window = GetActiveWindowAfterResizingUponExit();
@@ -1447,7 +1450,7 @@ void SplitViewController::EndSplitViewAfterResizingIfAppropriate() {
 aura::Window* SplitViewController::GetActiveWindowAfterResizingUponExit() {
   DCHECK(InSplitViewMode());
 
-  if (!ShouldEndSplitViewAfterResizing())
+  if (!ShouldEndTabletSplitViewAfterResizing())
     return nullptr;
 
   return divider_position_ == 0 ? GetPhysicalRightOrBottomWindow()
