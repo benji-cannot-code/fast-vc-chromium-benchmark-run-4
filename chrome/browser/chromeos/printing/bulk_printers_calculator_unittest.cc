d821466441be8db4ab0eb51f95b9cc26930d7990
Piotr Pawliczek <pawliczek@chromium.org>
2019-11-23 02:37:58 +0000

    BulkPrintersCalculator: Entry with an incorrect uri in the unittest
    This patch extends the unittest by adding an entry with incorrect uri.
    I was not able to reproduce the bug described in the issue.
    
    BUG=chromium:1015916
    TEST=on my workstation by running unit_tests
    
    Change-Id: I989fc9d82956f16b277648ec942de454763fadd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1912695
    Auto-Submit: Piotr Pawliczek <pawliczek@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Piotr Pawliczek <pawliczek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718382}

--- chrome/browser/chromeos/printing/bulk_printers_calculator_unittest.cc
+++ chrome/browser/chromeos/printing/bulk_printers_calculator_unittest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace {
 
-// The number of printers in BulkPolicyContentsJson.
+// The number of correct printers in BulkPolicyContentsJson.
 constexpr size_t kNumPrinters = 3;
 
 // An example bulk printer configuration file.
@@ -32,6 +32,18 @@ constexpr char kBulkPolicyContentsJson[] = R"json(
     "uri": "ipp://192.168.1.5",
     "ppd_resource": {
       "effective_model": "MS610de"
+    }
+  }, {
+    "id": "Incorrect uri",
+    "display_name": "aaa",
+    "description": "bbbb",
+    "manufacturer": "cccc",
+    "model":"dddd",
+    "uri":"ipp://:",
+    "uuid":"1c555fdb-1193-2204-3346-44c046e79d12",
+    "ppd_resource":{
+      "effective_manufacturer": "eee",
+      "effective_model": "fff"
     }
   }, {
     "id": "Second",
