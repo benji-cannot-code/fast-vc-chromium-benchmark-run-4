cd317e16440889f7deb211bfa7f9b71cb2e89b01
Pavel Feldman <pfeldman@chromium.org>
2019-11-23 05:00:34 +0000

    DevTools: fix crash upon emulation disable while multiclient
    Change-Id: I1fe3dfdf032c5780eb7a14f27816f235b4e27c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931345
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Auto-Submit: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718418}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1015,9 +1015,8 @@ void RenderWidget::OnDisableDeviceEmulation() {
   // TODO(https://crbug.com/1006052): We should move emulation into the browser
   // and send consistent ScreenInfo and ScreenRects to all RenderWidgets based
   // on emulation.
-  if (!delegate_)
+  if (!delegate_ || !device_emulator_)
     return;
-  DCHECK(device_emulator_);
   device_emulator_->DisableAndApply();
   device_emulator_.reset();
 }
