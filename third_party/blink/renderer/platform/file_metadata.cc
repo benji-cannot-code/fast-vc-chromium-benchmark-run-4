582182d189b583d54d676e71cf82b7cc239e7156
Kent Tamura <tkent@chromium.org>
2019-11-24 22:41:32 +0000

    Change the type of blink::FileMetadata::modification_time
    from |double| to |base::Optional<base::Time>|.
    
    This CL is the first step to stop using |double| for file timestamps in Blink.
    
    Though base::Time can be an is_null() value, we apply
    base::Optional<base::Time> instead of just base::Time because:
    
     - In JavaScript, the null base::Time, which represents
       1601-01-01 00:00 UTC, is a valid time. We have to use
       base::Optional<base::Time> in some parts of Blink.
    
     - data_element.mojom applies |mojo_base.mojom.Time?| for file timestamps,
      and it is mapped to |base::Optional<base::Time>| in C++.
    
    This CL should have no user-visible behavior changes.
    
    Bug: 988343
    Change-Id: I91f592b31cf8e61053969530fafc689eea8ff538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930756
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718489}

--- third_party/blink/renderer/platform/file_metadata.cc
+++ third_party/blink/renderer/platform/file_metadata.cc
@@ -50,10 +50,8 @@ namespace blink {
 // static
 FileMetadata FileMetadata::From(const base::File::Info& file_info) {
   FileMetadata file_metadata;
-  if (file_info.last_modified.is_null())
-    file_metadata.modification_time = std::numeric_limits<double>::quiet_NaN();
-  else
-    file_metadata.modification_time = file_info.last_modified.ToJsTime();
+  file_metadata.modification_time =
+      NullableTimeToOptionalTime(file_info.last_modified);
   file_metadata.length = file_info.size;
   if (file_info.is_directory)
     file_metadata.type = FileMetadata::kTypeDirectory;
@@ -70,7 +68,8 @@ bool GetFileSize(const String& path, int64_t& result) {
   return true;
 }
 
-bool GetFileModificationTime(const String& path, double& result) {
+bool GetFileModificationTime(const String& path,
+                             base::Optional<base::Time>& result) {
   FileMetadata metadata;
   if (!GetFileMetadata(path, metadata))
     return false;
@@ -92,10 +91,8 @@ bool GetFileMetadata(const String& path, FileMetadata& metadata) {
   if (!host->GetFileInfo(WebStringToFilePath(path), &file_info) || !file_info)
     return false;
 
-  // Blink now expects NaN as uninitialized/null Date.
-  metadata.modification_time = file_info->last_modified.is_null()
-                                   ? std::numeric_limits<double>::quiet_NaN()
-                                   : file_info->last_modified.ToJsTime();
+  metadata.modification_time =
+      NullableTimeToOptionalTime(file_info->last_modified);
   metadata.length = file_info->size;
   metadata.type = file_info->is_directory ? FileMetadata::kTypeDirectory
                                           : FileMetadata::kTypeFile;
