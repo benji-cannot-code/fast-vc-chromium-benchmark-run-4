c4f587c698cfa33edfa7ec316a199009ab7bf1a1
Aran Gilman <gilmanmh@google.com>
2019-11-24 01:13:32 +0000

    Remove unused #closeReaderView from distilled page.
    Bug: 1027612
    Change-Id: Ic4f4e1e43eb59a9aa24595a15e35d9003c718733
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929860
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Aran Gilman <gilmanmh@google.com>
    Cr-Commit-Position: refs/heads/master@{#718461}

--- components/dom_distiller/core/javascript/dom_distiller_viewer.js
+++ components/dom_distiller/core/javascript/dom_distiller_viewer.js
@@ -165,14 +165,6 @@ function updateSliderFromElement(element) {
   }
 }
 
-// Add a listener to the "View Original" link to report opt-outs.
-document.getElementById('closeReaderView')
-    .addEventListener('click', function(e) {
-      if (distiller) {
-        distiller.closePanel(true);
-      }
-    }, true);
-
 updateToolbarColor();
 maybeSetWebFont();
 updateSliderFromElement(document.querySelector('#fontSizeSelection'));
