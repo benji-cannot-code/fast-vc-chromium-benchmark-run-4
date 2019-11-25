b8691dc5bb09468dfba6d57001b590ff3287abf8
Ilia Samsonov <isamsonov@google.com>
2019-11-25 18:08:24 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=khmel@chromium.org
    
    Bug: 1023677
    Change-Id: I9fe9aae2a5a929b9631e8642e1d9cb75678b8e7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926817
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#718701}

--- ash/app_list/views/apps_grid_view_unittest.cc
+++ ash/app_list/views/apps_grid_view_unittest.cc
@@ -368,7 +368,7 @@ class AppsGridViewTest : public views::ViewsTestBase,
   DISALLOW_COPY_AND_ASSIGN(AppsGridViewTest);
 };
 
-INSTANTIATE_TEST_SUITE_P(, AppsGridViewTest, testing::Bool());
+INSTANTIATE_TEST_SUITE_P(All, AppsGridViewTest, testing::Bool());
 
 class TestAppsGridViewFolderDelegate : public AppsGridViewFolderDelegate {
  public:
@@ -1859,7 +1859,7 @@ TEST_P(AppsGridViewTabletTest, Basic) {
       1);
 }
 
-INSTANTIATE_TEST_SUITE_P(, AppsGridViewTabletTest, testing::Bool());
+INSTANTIATE_TEST_SUITE_P(All, AppsGridViewTabletTest, testing::Bool());
 
 // Test various dragging behaviors only allowed when apps grid gap (part of
 // home launcher feature) is enabled.
