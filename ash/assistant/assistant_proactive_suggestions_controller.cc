e045618c201c5f32f8aba47814a26a4087d5d77b
David Black <dmblack@google.com>
2019-11-25 22:35:05 +0000

    Split ProactiveSuggestionsSimpleView from ProactiveSuggestionsView.
    ProactiveSuggestionsView will serve as the base class for both the new
    ProactiveSuggestionsSimpleView and a new ProactiveSuggestionsRichView
    (which will be added in a follow up CL), the latter of which will
    present a rich, content-forward entry point affordance (gated behind a
    feature param).
    
    The ProactiveSuggestionsSimpleView is essentially the same, simple
    entry point affordance that is currently in the code base with shared
    logic left in the base class.
    
    Bug: b:145056104
    Change-Id: I0618c6cfa23d50ae9063117477e2879794545bd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1932879
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#718859}

--- ash/assistant/assistant_proactive_suggestions_controller.cc
+++ ash/assistant/assistant_proactive_suggestions_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/assistant_controller.h"
 #include "ash/assistant/assistant_suggestions_controller.h"
 #include "ash/assistant/assistant_ui_controller.h"
+#include "ash/assistant/ui/proactive_suggestions_simple_view.h"
 #include "ash/assistant/ui/proactive_suggestions_view.h"
 #include "ash/public/cpp/assistant/proactive_suggestions.h"
 #include "ash/public/cpp/assistant/util/histogram_util.h"
@@ -209,7 +210,12 @@ void AssistantProactiveSuggestionsController::MaybeShowUi() {
     return;
   }
 
-  view_ = new ProactiveSuggestionsView(assistant_controller_->view_delegate());
+  // TODO(dmblack): Use a different ProactiveSuggestionsView implementation to
+  // instead show a richer affordance (if enabled via feature param).
+  view_ = new ProactiveSuggestionsSimpleView(
+      assistant_controller_->view_delegate());
+
+  view_->Init();
   view_->GetWidget()->ShowInactive();
 
   RecordProactiveSuggestionsShowAttempt(
