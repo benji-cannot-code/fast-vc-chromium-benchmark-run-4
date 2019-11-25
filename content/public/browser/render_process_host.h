0200f76da86af55ff0e2839b2874bf93ac7766cb
Arthur Hemery <ahemery@chromium.org>
2019-11-25 15:48:39 +0000

    [Security] Move COEP definition into its own mojo file.
    In order to reuse COEP in other objects, we move its definition into
    an easy to include single struct mojo file. It replaces the massive
    network_context.mojom include.
    
    Change-Id: I245d68d3fe1880b369c3c65801c502a1aee8b027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930738
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718655}

--- content/public/browser/render_process_host.h
+++ content/public/browser/render_process_host.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "net/base/network_isolation_key.h"
+#include "services/network/public/mojom/cross_origin_embedder_policy.mojom-forward.h"
 #include "services/network/public/mojom/network_context.mojom-forward.h"
 #include "services/network/public/mojom/url_loader_factory.mojom-forward.h"
 #include "third_party/blink/public/mojom/appcache/appcache.mojom.h"
