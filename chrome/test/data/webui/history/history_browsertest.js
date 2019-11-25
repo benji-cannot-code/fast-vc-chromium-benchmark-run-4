b864421456131d2d72b9a06b33a4a653b44a1547
Owen Min <zmin@chromium.org>
2019-11-25 20:00:03 +0000

    Re-disable HistorySupervisedUserTest on Mac
    The test was mistakely disabled on all platforms but Mac. However, it
    should be disabled in the exactly opposite way.
    
    Tbr: dpapad@chromium.org
    Bug: 1028129
    Change-Id: Ie54a98fe84db3be5a8989bd9cf1553591c69f067
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1933597
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718770}

--- chrome/test/data/webui/history/history_browsertest.js
+++ chrome/test/data/webui/history/history_browsertest.js
@@ -211,7 +211,7 @@ HistorySupervisedUserTest.prototype = {
   ]),
 };
 
-GEN('#if !defined(OS_MACOSX)');
+GEN('#if defined(OS_MACOSX)');
 GEN('#define MAYBE_AllTest DISABLED_AllTest');
 GEN('#else');
 GEN('#define MAYBE_AllTest AllTest');
