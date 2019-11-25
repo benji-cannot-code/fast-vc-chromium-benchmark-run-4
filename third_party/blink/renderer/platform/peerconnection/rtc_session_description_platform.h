7bad5f89b770ce101da0b3546c21fa40ecee7769
Antonio Gomes <tonikitoo@igalia.com>
2019-11-25 03:33:52 +0000

    Switch RTCSessionDescriptionPlatform away from WebString
    ... and use WTF::String instead.
    
    BUG=787254
    R=guidou@chromium.org
    
    Change-Id: I90c3275bf9aa00a6e62a28369b5fab0c3f2c6b26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930854
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718517}

--- third_party/blink/renderer/platform/peerconnection/rtc_session_description_platform.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_session_description_platform.h
@@ -6,30 +6,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_RTC_SESSION_DESCRIPTION_PLATFORM_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_RTC_SESSION_DESCRIPTION_PLATFORM_H_
 
-#include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
-#include "third_party/blink/renderer/platform/wtf/ref_counted.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
 class PLATFORM_EXPORT RTCSessionDescriptionPlatform final
     : public GarbageCollected<RTCSessionDescriptionPlatform> {
  public:
-  RTCSessionDescriptionPlatform(const WebString& type, const WebString& sdp);
+  RTCSessionDescriptionPlatform(const String& type, const String& sdp);
 
-  WebString GetType() { return type_; }
-  void SetType(const WebString& type) { type_ = type; }
+  String GetType() { return type_; }
+  void SetType(const String& type) { type_ = type; }
 
-  WebString Sdp() { return sdp_; }
-  void SetSdp(const WebString& sdp) { sdp_ = sdp; }
+  String Sdp() { return sdp_; }
+  void SetSdp(const String& sdp) { sdp_ = sdp; }
 
   void Trace(blink::Visitor* visitor) {}
 
  private:
-  WebString type_;
-  WebString sdp_;
+  String type_;
+  String sdp_;
 };
 
 }  // namespace blink
