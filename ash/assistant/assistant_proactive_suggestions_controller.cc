18df60d38ae9a22f5beae59bfcf65f7fcd798c4f
David Black <dmblack@google.com>
2019-11-25 23:03:32 +0000

    Adds ProactiveSuggestionsRichView.
    Extends ProactiveSuggestionsView as the implementation of the entry
    point that is shown when the new "show-rich-entry-point" feature param
    is enabled.
    
    Note that this view hosts embedded web content and is currently
    stubbed. Follow up CLs will:
    
    - Replace hard-coded HTML w/ server provided HTML.
    - Parameterize corner radius.
    - Handle navigation attempts.
    
    See bug for screenshots.
    
    Bug: b:145056104
    Change-Id: I17be0951e707317b4903b185043c41f17deebe5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1932463
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718878}

--- ash/assistant/assistant_proactive_suggestions_controller.cc
+++ ash/assistant/assistant_proactive_suggestions_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/assistant_controller.h"
 #include "ash/assistant/assistant_suggestions_controller.h"
 #include "ash/assistant/assistant_ui_controller.h"
+#include "ash/assistant/ui/proactive_suggestions_rich_view.h"
 #include "ash/assistant/ui/proactive_suggestions_simple_view.h"
 #include "ash/assistant/ui/proactive_suggestions_view.h"
 #include "ash/public/cpp/assistant/proactive_suggestions.h"
@@ -210,10 +211,16 @@ void AssistantProactiveSuggestionsController::MaybeShowUi() {
     return;
   }
 
-  // TODO(dmblack): Use a different ProactiveSuggestionsView implementation to
-  // instead show a richer affordance (if enabled via feature param).
-  view_ = new ProactiveSuggestionsSimpleView(
-      assistant_controller_->view_delegate());
+  // Depending on which is enabled, we'll either use a rich, content-forward UI
+  // affordance or a simple UI affordance as the feature entry point.
+  if (chromeos::assistant::features::
+          IsProactiveSuggestionsShowRichEntryPointEnabled()) {
+    view_ = new ProactiveSuggestionsRichView(
+        assistant_controller_->view_delegate());
+  } else {
+    view_ = new ProactiveSuggestionsSimpleView(
+        assistant_controller_->view_delegate());
+  }
 
   view_->Init();
   view_->GetWidget()->ShowInactive();
