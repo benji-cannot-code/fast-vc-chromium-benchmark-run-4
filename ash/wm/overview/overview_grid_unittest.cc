71f21d6a08e04f53895794efa9d253a7942268b2
Alex Newcomer <newcomer@chromium.org>
2019-11-25 23:16:12 +0000

    cros: disable hotseat by default
    This CL disables a flag which controls a few features:
     - The shelf hotseat
       - Modular UI which shows in tablet mode, shows pinned and active
         apps.
     - In-app shelf
       - Smaller shelf in tablet mode.
     - Dense shelf in clamshell mode
       - Smaller shelf in clamshell mode for certain displays.
    
    Punting all of these feature to M-81!
    
    Bug: 1027343
    Change-Id: I302523d7d1938c9166a0e61c07792d8d9c6a8439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929101
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718880}

--- ash/wm/overview/overview_grid_unittest.cc
+++ ash/wm/overview/overview_grid_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/workspace/workspace_layout_manager.h"
 #include "ash/wm/workspace_controller.h"
 #include "base/strings/string_number_conversions.h"
+#include "chromeos/constants/chromeos_switches.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/display/display.h"
@@ -229,7 +230,7 @@ TEST_F(OverviewGridTest, PartiallyOffscreenWindow) {
   // Position |window2|'s destination to be partially offscreen. Tests that it
   // still animates because the onscreen portion is not occluded by |window1|.
   std::vector<gfx::RectF> target_bounds = {
-      gfx::RectF(100.f, 100.f), gfx::RectF(350.f, 350.f, 100.f, 100.f)};
+      gfx::RectF(100.f, 100.f), gfx::RectF(350.f, 100.f, 100.f, 100.f)};
   CheckAnimationStates({window1.get(), window2.get()}, target_bounds,
                        {true, true}, {true, true});
 
