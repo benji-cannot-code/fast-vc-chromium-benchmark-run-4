a20804dc4326fefa35b22e44c52ac5493083af8e
Ted Meyer <tmathmeyer@chromium.org>
2019-11-25 22:45:58 +0000

    Stringify values before displaying them
    Stop displaying [Object object] in the log now that we're sending
    objects instead of strings.
    
    Bug: 794255
    Change-Id: I4d64fb6e717db1660fac87afc343a1097b080768
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1934781
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718866}

--- content/browser/resources/media/client_renderer.js
+++ content/browser/resources/media/client_renderer.js
@@ -485,7 +485,7 @@ var ClientRenderer = (function() {
         var valueCell = row.insertCell(-1);
 
         keyCell.appendChild(document.createTextNode(key));
-        valueCell.appendChild(document.createTextNode(value));
+        valueCell.appendChild(document.createTextNode(JSON.stringify(value)));
       }
     },
 
@@ -498,7 +498,8 @@ var ClientRenderer = (function() {
         timestampCell.appendChild(
             document.createTextNode(util.millisecondsToString(event.time)));
         row.insertCell(-1).appendChild(document.createTextNode(event.key));
-        row.insertCell(-1).appendChild(document.createTextNode(event.value));
+        row.insertCell(-1).appendChild(
+            document.createTextNode(JSON.stringify(event.value)));
       }
     },
 
