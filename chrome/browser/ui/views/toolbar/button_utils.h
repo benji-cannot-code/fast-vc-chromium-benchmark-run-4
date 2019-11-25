c4d5ae0b0d83424b1c21a7f313d48a624e159bb0
Eric Willigers <ericwilligers@chromium.org>
2019-11-25 08:48:14 +0000

    desktop-pwas: Windows 10 minimal-ui icons
    When using minimal-ui on Windows 10, the icons are now
    consistent with the native Windows 10 Back and Reload icon style.
    
    The main browser toolbar is unchanged.
    
    TBR=antrim@chromium.org
    
    Bug: 1026069
    Change-Id: Ic243e22f08f4ce7467db1058e2fff948a0be854e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924180
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718559}

--- chrome/browser/ui/views/toolbar/button_utils.h
+++ chrome/browser/ui/views/toolbar/button_utils.h
@@ -8,13 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "build/build_config.h"
+#include "chrome/browser/ui/views/toolbar/reload_button.h"
+
 namespace views {
 class ButtonListener;
 }  // namespace views
 
 class Browser;
 class HomeButton;
-class ReloadButton;
 class ToolbarButton;
 
 std::unique_ptr<ToolbarButton> CreateBackButton(views::ButtonListener* listener,
@@ -22,8 +24,17 @@ std::unique_ptr<ToolbarButton> CreateBackButton(views::ButtonListener* listener,
 std::unique_ptr<ToolbarButton> CreateForwardButton(
     views::ButtonListener* listener,
     Browser* browser);
-std::unique_ptr<ReloadButton> CreateReloadButton(Browser* browser);
+std::unique_ptr<ReloadButton> CreateReloadButton(
+    Browser* browser,
+    ReloadButton::IconStyle icon_style);
 std::unique_ptr<HomeButton> CreateHomeButton(views::ButtonListener* listener,
                                              Browser* browser);
+
+#if defined(OS_WIN)
+// For Windows 10 and later, we use custom icons for minimal-ui web app
+// Back and Reload buttons, to conform to the native OS' appearance.
+// https://w3c.github.io/manifest/#dom-displaymodetype-minimal-ui
+bool UseWindowsIconsForMinimalUI();
+#endif
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TOOLBAR_BUTTON_UTILS_H_
--- chrome/browser/ui/views/web_apps/web_app_frame_toolbar_view.cc
+++ chrome/browser/ui/views/web_apps/web_app_frame_toolbar_view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/ranges.h"
 #include "base/task_runner.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/browser_command_controller.h"
@@ -112,6 +113,15 @@ void SetInsetsForWebAppToolbarButton(ToolbarButton* toolbar_button,
     constexpr gfx::Insets kInkDropInsets(2);
     toolbar_button->SetProperty(views::kInternalPaddingKey, kInkDropInsets);
   }
+}
+
+const gfx::VectorIcon& GetBackImage(bool touch_ui) {
+#if defined(OS_WIN)
+  if (UseWindowsIconsForMinimalUI())
+    return touch_ui ? kBackArrowWindowsTouchIcon : kBackArrowWindowsIcon;
+#endif
+
+  return touch_ui ? kBackArrowTouchIcon : vector_icons::kBackArrowIcon;
 }
 
 }  // namespace
@@ -297,8 +307,8 @@ WebAppFrameToolbarView::WebAppFrameToolbarView(views::Widget* widget,
 
     back_ = left_container_->AddChildView(
         CreateBackButton(this, browser_view_->browser()));
-    reload_ = left_container_->AddChildView(
-        CreateReloadButton(browser_view_->browser()));
+    reload_ = left_container_->AddChildView(CreateReloadButton(
+        browser_view_->browser(), ReloadButton::IconStyle::kMinimalUi));
 
     SetInsetsForWebAppToolbarButton(back_, is_browser_focus_mode);
     SetInsetsForWebAppToolbarButton(reload_, is_browser_focus_mode);
@@ -713,8 +723,7 @@ void WebAppFrameToolbarView::GenerateMinimalUIButtonImages() {
 
   if (back_) {
     const bool touch_ui = ui::MaterialDesignController::touch_ui();
-    const gfx::VectorIcon& back_image =
-        touch_ui ? kBackArrowTouchIcon : vector_icons::kBackArrowIcon;
+    const gfx::VectorIcon& back_image = GetBackImage(touch_ui);
     back_->SetImage(views::Button::STATE_NORMAL,
                     gfx::CreateVectorIcon(back_image, normal_color));
     back_->SetImage(views::Button::STATE_DISABLED,
