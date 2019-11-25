faca9175d975d80f4f6dc1d5405e50cddffd3e8a
Antonio Gomes <tonikitoo@igalia.com>
2019-11-25 12:48:09 +0000

    Replace uses of WebRTCStatsResponse by RTCStatsResponse
    ... and RTCStatsResponseBase.
    
    It is also possible to merge RTCStatsResponse and
    RTCStatsResponseBase in a future step, once they
    are both solely used in renderer/modules/peerconnection.
    Today, there is still some code in renderer/platform using
    RTCStatsResponseBase.
    
    BUG=787254, 919392
    R=guidou@chormium.org, haraken@chromium.org
    
    Change-Id: I788a81507447d990ca7d047d4a370f3bf8ec73d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930941
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718607}

--- third_party/blink/renderer/platform/exported/web_rtc_stats_request.cc
+++ third_party/blink/renderer/platform/exported/web_rtc_stats_request.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/platform/web_media_stream.h"
 #include "third_party/blink/public/platform/web_media_stream_track.h"
-#include "third_party/blink/public/platform/web_rtc_stats_response.h"
 #include "third_party/blink/renderer/platform/peerconnection/rtc_stats_request.h"
 #include "third_party/blink/renderer/platform/peerconnection/rtc_stats_response_base.h"
 
@@ -50,8 +49,8 @@ void WebRTCStatsRequest::Reset() {
   private_.Reset();
 }
 
-WebRTCStatsResponse WebRTCStatsRequest::CreateResponse() const {
-  return WebRTCStatsResponse(private_->CreateResponse());
+RTCStatsResponseBase* WebRTCStatsRequest::CreateResponse() const {
+  return private_->CreateResponse();
 }
 
 bool WebRTCStatsRequest::HasSelector() const {
@@ -63,7 +62,7 @@ const WebMediaStreamTrack WebRTCStatsRequest::Component() const {
 }
 
 void WebRTCStatsRequest::RequestSucceeded(
-    const WebRTCStatsResponse& response) const {
+    RTCStatsResponseBase* response) const {
   private_->RequestSucceeded(response);
 }
 
