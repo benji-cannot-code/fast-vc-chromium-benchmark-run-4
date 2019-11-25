a0a0aed5835de7b7c34262557a65452ad6b9826b
Yusuke Sato <yusukes@chromium.org>
2019-11-25 20:59:39 +0000

    arcvm: Don't set "rw" guest kernel param when the image is not ext4
    The param is just ignored when the image is squashfs, but setting
    "rw" for the read-only fs is still confusing.
    
    This also adds a test that covers the case where no serial number
    is given.
    
    BUG=None
    TEST=try
    
    Change-Id: Id153e51f4d44432728e7735620853833f784a82a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931919
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718808}

--- components/arc/session/arc_vm_client_adapter.cc
+++ components/arc/session/arc_vm_client_adapter.cc
@@ -173,8 +173,10 @@ vm_tools::concierge::StartArcVmRequest CreateStartArcVmRequest(
   request.set_owner_id(user_id_hash);
 
   request.add_params("root=/dev/vda");
-  if (file_system_status.is_host_rootfs_writable())
+  if (file_system_status.is_host_rootfs_writable() &&
+      file_system_status.is_system_image_ext_format()) {
     request.add_params("rw");
+  }
   request.add_params("init=/init");
   for (auto& entry : kernel_cmdline)
     request.add_params(std::move(entry));
@@ -323,10 +325,12 @@ class ArcVmClientAdapter : public ArcClientAdapter,
 
   void SetUserInfo(const std::string& hash,
                    const std::string& serial_number) override {
-    DCHECK(!hash.empty());
     DCHECK(user_id_hash_.empty());
-    DCHECK(!serial_number.empty());
     DCHECK(serial_number_.empty());
+    if (hash.empty())
+      LOG(WARNING) << "hash is empty";
+    if (serial_number.empty())
+      LOG(WARNING) << "serial_number is empty";
     user_id_hash_ = hash;
     serial_number_ = serial_number;
   }
