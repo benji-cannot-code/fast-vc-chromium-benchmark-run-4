e6fab1d6eae52b33d59b7d2277730e773900bd43
Ilia Samsonov <isamsonov@google.com>
2019-11-25 18:20:36 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=khmel@chromium.org
    
    Bug: 1023677
    Change-Id: Ieabfcdac4097b9cf9839dc3e05bc950b328b989a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926670
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#718712}

--- chrome/browser/ui/app_list/search/arc/arc_app_shortcuts_search_provider_unittest.cc
+++ chrome/browser/ui/app_list/search/arc/arc_app_shortcuts_search_provider_unittest.cc
@@ -113,6 +113,8 @@ TEST_P(ArcAppShortcutsSearchProviderTest, Basic) {
   }
 }
 
-INSTANTIATE_TEST_SUITE_P(, ArcAppShortcutsSearchProviderTest, testing::Bool());
+INSTANTIATE_TEST_SUITE_P(All,
+                         ArcAppShortcutsSearchProviderTest,
+                         testing::Bool());
 
 }  // namespace app_list
