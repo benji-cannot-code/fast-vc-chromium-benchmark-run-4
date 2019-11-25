fd601c09854a435793f85bd02a292370d64ba040
Yusuke Sato <yusukes@chromium.org>
2019-11-25 20:01:27 +0000

    arcvm: Make ArcVmClientAdapter more testable
    * Add setters for testing to arc::FileSystemStatus
    * Add CreateArcVmClientAdapterForTesting() that allows the unit tests
      to rewrite the FileSystemStatus object
    
    BUG=b:123309049
    TEST=try, git cl lint
    
    Change-Id: Id1054af7343c02d0f345b24ebd9546faeb98d774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931541
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718773}

--- components/arc/session/arc_vm_client_adapter.h
+++ components/arc/session/arc_vm_client_adapter.h
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/callback.h"
 #include "components/arc/session/arc_client_adapter.h"
+#include "components/arc/session/file_system_status.h"
 #include "components/version_info/channel.h"
 
 namespace arc {
@@ -16,6 +18,12 @@ namespace arc {
 // Returns an adapter for arcvm.
 std::unique_ptr<ArcClientAdapter> CreateArcVmClientAdapter(
     version_info::Channel channel);
+
+using FileSystemStatusRewriter =
+    base::RepeatingCallback<void(FileSystemStatus*)>;
+std::unique_ptr<ArcClientAdapter> CreateArcVmClientAdapterForTesting(
+    version_info::Channel channel,
+    const FileSystemStatusRewriter& rewriter);
 
 }  // namespace arc
 
