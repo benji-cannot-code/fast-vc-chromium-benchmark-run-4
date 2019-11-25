7c95168cb247157688d3c9151a0536016a356c4e
Ilia Samsonov <isamsonov@google.com>
2019-11-25 14:41:20 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=mcasas@chromium.org
    
    Bug: 1023677
    Change-Id: Iaa163d01189aba67fadcf2b14bfcadd622283228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927048
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718633}

--- third_party/blink/renderer/modules/mediacapturefromelement/canvas_capture_handler_unittest.cc
+++ third_party/blink/renderer/modules/mediacapturefromelement/canvas_capture_handler_unittest.cc
@@ -234,7 +234,7 @@ TEST_F(CanvasCaptureHandlerTest, CheckNeedsNewFrame) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    ,
+    All,
     CanvasCaptureHandlerTest,
     ::testing::Combine(::testing::Bool(),
                        ::testing::Values(kTestCanvasCaptureFrameEvenSize,
