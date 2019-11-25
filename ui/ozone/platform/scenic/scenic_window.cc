a43fa7f8c58890007f464620e68ba39c90d149f2
Felipe Archondo <felipearchondo@google.com>
2019-11-25 19:18:41 +0000

    [fuchsia] Change all Scenic Presents to Present2s
    Bug: fuchsia:41855
    Change-Id: I3d8798424fe92ef298d7a2e4a7c17ddb10398aab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927043
    Commit-Queue: Felipe Archondo <felipearchondo@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718755}

--- ui/ozone/platform/scenic/scenic_window.cc
+++ ui/ozone/platform/scenic/scenic_window.cc
@@ -79,8 +79,10 @@ void ScenicWindow::AttachSurfaceView(
   render_node_.DetachChildren();
   render_node_.AddChild(*surface_view_holder_);
 
-  scenic_session_.Present(
-      /*presentation_time=*/0, [](fuchsia::images::PresentationInfo info) {});
+  scenic_session_.Present2(
+      /*requested_presentation_time=*/0,
+      /*requested_prediction_span=*/0,
+      [](fuchsia::scenic::scheduling::FuturePresentationTimes info) {});
 }
 
 gfx::Rect ScenicWindow::GetBounds() {
@@ -99,10 +101,12 @@ void ScenicWindow::SetTitle(const base::string16& title) {
 void ScenicWindow::Show(bool inactive) {
   view_.AddChild(node_);
 
-  // Call Present() to ensure that the scenic session commands are processed,
+  // Call Present2() to ensure that the scenic session commands are processed,
   // which is necessary to receive metrics event from Scenic.
-  scenic_session_.Present(
-      /*presentation_time=*/0, [](fuchsia::images::PresentationInfo info) {});
+  scenic_session_.Present2(
+      /*requested_presentation_time=*/0,
+      /*requested_prediction_span=*/0,
+      [](fuchsia::scenic::scheduling::FuturePresentationTimes info) {});
 }
 
 void ScenicWindow::Hide() {
@@ -225,8 +229,10 @@ void ScenicWindow::UpdateSize() {
 
   // This is necessary when using vulkan because ImagePipes are presented
   // separately and we need to make sure our sizes change is committed.
-  scenic_session_.Present(
-      /*presentation_time=*/0, [](fuchsia::images::PresentationInfo info) {});
+  scenic_session_.Present2(
+      /*requested_presentation_time=*/0,
+      /*requested_prediction_span=*/0,
+      [](fuchsia::scenic::scheduling::FuturePresentationTimes info) {});
 
   delegate_->OnBoundsChanged(size_rect);
 }
