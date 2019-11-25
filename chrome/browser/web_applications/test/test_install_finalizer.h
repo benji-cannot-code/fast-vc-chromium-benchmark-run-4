5a11f6c32da76526ca9125041d0e4904476bbf8d
Alexey Baskakov <loyso@chromium.org>
2019-11-25 02:42:36 +0000

    WebApp: Move WasExternalAppUninstalledByUser from Registrar to Finalizer.
    This is a preparation CL to implement WasExternalAppUninstalledByUser in
    WebAppInstallFinalizer.
    
    Bug: 891172
    Change-Id: I3cfaf1e96c72cd1970e8cd49d2d53f0a3c268401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930283
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718512}

--- chrome/browser/web_applications/test/test_install_finalizer.h
+++ chrome/browser/web_applications/test/test_install_finalizer.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <memory>
+#include <set>
 
 #include "base/macros.h"
 #include "base/optional.h"
@@ -42,6 +43,7 @@ class TestInstallFinalizer final : public InstallFinalizer {
   bool CanUserUninstallFromSync(const AppId& app_id) const override;
   void UninstallWebAppFromSyncByUser(const AppId& app_id,
                                      UninstallWebAppCallback callback) override;
+  bool WasExternalAppUninstalledByUser(const AppId& app_id) const override;
   bool CanAddAppToQuickLaunchBar() const override;
   void AddAppToQuickLaunchBar(const AppId& app_id) override;
   bool CanReparentTab(const AppId& app_id,
@@ -56,6 +58,11 @@ class TestInstallFinalizer final : public InstallFinalizer {
                                     InstallResultCode code);
   void SetNextUninstallExternalWebAppResult(const GURL& app_url,
                                             bool uninstalled);
+
+  // Uninstall the app and add |app_id| to the map of external extensions
+  // uninstalled by the user. May be called on an app that isn't installed to
+  // simulate that the app was uninstalled previously.
+  void SimulateExternalAppUninstalledByUser(const AppId& app_id);
 
   std::unique_ptr<WebApplicationInfo> web_app_info() {
     return std::move(web_app_info_copy_);
@@ -87,6 +94,7 @@ class TestInstallFinalizer final : public InstallFinalizer {
   base::Optional<AppId> next_app_id_;
   base::Optional<InstallResultCode> next_result_code_;
   std::map<GURL, bool> next_uninstall_external_web_app_results_;
+  std::set<AppId> user_uninstalled_external_apps_;
 
   int num_reparent_tab_calls_ = 0;
   int num_reveal_appshim_calls_ = 0;
