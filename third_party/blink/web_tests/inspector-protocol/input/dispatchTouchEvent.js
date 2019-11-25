80a8bd4bc1739fdd686511d18b12abf7f856f296
Lan Wei <lanwei@chromium.org>
2019-11-25 22:05:23 +0000

    Allow another touch start happen when there are active touch points
    Now when there are active touch points and we dispatch a touch start, an
    error "Must have no prior active touch points to start a new touch." will
    throw out. We should allow this case happens:
    Input.dispatchEvent({
        type: 'touchStart',
        touchPoints: [{
          x: 100,
          y: 100,
          id: 1
        }]
      });
    Input.dispatchEvent({
        type: 'touchStart',
        touchPoints: [{
          x: 100,
          y: 100,
          id: 1
        }, {
          x: 150,
          y: 100,
          id: 2
        }]
      });
    
    Bug: 1020674
    Change-Id: Ib5cae40864312977f03b2c9671edd7c46637be7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927951
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718846}

--- third_party/blink/web_tests/inspector-protocol/input/dispatchTouchEvent.js
+++ third_party/blink/web_tests/inspector-protocol/input/dispatchTouchEvent.js
@@ -225,5 +225,39 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     touchPoints: []
   });
 
+  testRunner.log('\n------- Sequence ------');
+  await dispatchEvent({
+    type: 'touchStart',
+    touchPoints: [{
+      x: 100,
+      y: 100,
+      id: 1
+    }]
+  });
+  await dispatchEvent({
+    type: 'touchStart',
+    touchPoints: [{
+      x: 100,
+      y: 100,
+      id: 1
+    }, {
+      x: 150,
+      y: 100,
+      id: 2
+    }]
+  });
+  await dispatchEvent({
+    type: 'touchMove',
+    touchPoints: [{
+      x: 100,
+      y: 150,
+      id: 1
+    }, {
+      x: 150,
+      y: 150,
+      id: 2
+    }]
+  });
+
   testRunner.completeTest();
 })
