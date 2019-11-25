007c508be201cbe4cdf4301e3cd7d78117aaa09e
Luciano Pacheco <lucmult@chromium.org>
2019-11-25 01:37:18 +0000

    Files app: Add test for quickview video stop playing when closed
    Add test that checks that "src" attribute is cleared when closing the
    quick view, if the "src" isn't cleared the video continues to play,
    while it's hidden, but the audio is still audible.
    
    Bug: 970192, 1005556
    Change-Id: I73d628d7d424b7f795622108eb7a1f0c7b7bcc4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1933671
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718506}

--- ui/file_manager/integration_tests/file_manager/quick_view.js
+++ ui/file_manager/integration_tests/file_manager/quick_view.js
@@ -952,6 +952,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     // Check: the <webview> body backgroundColor should be transparent black.
     chrome.test.assertEq('rgba(0, 0, 0, 0)', backgroundColor[0]);
+
+    // Close Quick View.
+    await closeQuickView(appId);
+
+    // Check quickview video <files-safe-media> has no "src", so it stops
+    // playing the video. crbug.com/970192
+    const noSrcFilesSafeMedia = ['#quick-view', '#videoSafeMedia[src=""]'];
+    await remoteCall.waitForElement(appId, noSrcFilesSafeMedia);
   };
 
   /**
@@ -1097,7 +1105,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     const infoShown = ['#quick-view', '#contentPanel[metadata-box-active]'];
     const infoHidden =
         ['#quick-view', '#contentPanel:not([metadata-box-active])'];
-
 
     // Open Files app on Downloads containing ENTRIES.hello.
     const appId =
