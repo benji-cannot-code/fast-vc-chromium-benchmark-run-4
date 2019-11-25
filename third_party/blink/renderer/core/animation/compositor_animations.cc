86df5b1de2c023b08806502731ccf59e3d380c3a
Kevin Ellis <kevers@chromium.org>
2019-11-25 21:50:47 +0000

    Remove Animation::NeedsCompositorTimeSync
    This small patch is part of a cleanup effort to remove remaining
    references to the internal_play_state_ data member in animation.cc.
    Now that the timing of the pending flag resets has been fixed, the
    use of old AnimationPlayState::kPending is no longer required.
    
    Bug: 960944
    Change-Id: I2e86184c10dcacc8bf2f62a3a7410948c16b5bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1933748
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718839}

--- third_party/blink/renderer/core/animation/compositor_animations.cc
+++ third_party/blink/renderer/core/animation/compositor_animations.cc
@@ -75,7 +75,7 @@ bool ConsiderAnimationAsIncompatible(const Animation& animation,
   if (&animation == &animation_to_add)
     return false;
 
-  if (animation.NeedsCompositorTimeSync())
+  if (animation.pending())
     return true;
 
   switch (animation.CalculateAnimationPlayState()) {
