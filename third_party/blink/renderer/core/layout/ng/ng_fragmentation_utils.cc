0c1228a13ed9dc4abb6871caf27b70248209af6b
Morten Stenshorne <mstensho@chromium.org>
2019-11-25 11:38:37 +0000

    [LayoutNG] Break before a float if breaking inside isn't ideal.
    We currently don't let a break before a float affect the appeal of
    breaking inside their container. This is possible and fairly straight-forward
    to implement, at least for floats that don't participate in an inline
    formatting context.
    
    Also, since we're not hooking the floats up to the break appeal system
    at all yet, we won't see class A breakpoints between floats, or between
    a float and an in-flow block, which the spec actually requires.
    
    Will get back to this when inline formatting context float fragmentation is
    supported. Being as consistent as possible regardless of a float belonging to
    an inline formatting context or not seems like a good idea. Let's figure out
    what's feasible for inline formatting context floats first.
    
    Bug: 829028
    Change-Id: Iccb22d91865ca6aad464c2ee12462843281643a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929825
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718590}

--- third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.cc
+++ third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.cc
@@ -268,7 +268,7 @@ void BreakBeforeChild(const NGConstraintSpace& space,
                       NGLayoutInputNode child,
                       const NGLayoutResult& layout_result,
                       LayoutUnit fragmentainer_block_offset,
-                      NGBreakAppeal appeal,
+                      base::Optional<NGBreakAppeal> appeal,
                       bool is_forced_break,
                       NGBoxFragmentBuilder* builder) {
 #if DCHECK_IS_ON()
