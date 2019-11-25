c026b6934a0e091db622c8016490bb84aa544447
Kurt Horimoto <kkhorimoto@google.com>
2019-11-25 19:31:28 +0000

    [iOS] Enable non-modal dialogs by default.
    TBR=eugenebut@chromium.org
    
    Bug: none
    Change-Id: Iac187c621250cbcacf46ea7f90e8b8bfe30b5621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924964
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718760}

--- ios/chrome/browser/ui/app_launcher/app_launcher_coordinator_unittest.mm
+++ ios/chrome/browser/ui/app_launcher/app_launcher_coordinator_unittest.mm
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/foundation_util.h"
 #import "ios/chrome/browser/app_launcher/app_launcher_tab_helper.h"
+#include "ios/chrome/browser/overlays/public/overlay_request.h"
+#import "ios/chrome/browser/overlays/public/overlay_request_queue.h"
+#import "ios/chrome/browser/overlays/public/web_content_area/app_launcher_alert_overlay.h"
+#import "ios/chrome/browser/ui/dialogs/dialog_features.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/scoped_key_window.h"
 #import "ios/web/public/test/fakes/test_web_state.h"
@@ -35,10 +39,38 @@ class AppLauncherCoordinatorTest : public PlatformTest {
     OCMStub([application_ sharedApplication]).andReturn(application_);
     AppLauncherTabHelper::CreateForWebState(&web_state_, nil, nil);
   }
-  ~AppLauncherCoordinatorTest() override { [application_ stopMocking]; }
+  ~AppLauncherCoordinatorTest() override {
+    [application_ stopMocking];
+    OverlayRequestQueue::FromWebState(&web_state_,
+                                      OverlayModality::kWebContentArea)
+        ->CancelAllRequests();
+  }
 
   AppLauncherTabHelper* tab_helper() {
     return AppLauncherTabHelper::FromWebState(&web_state_);
+  }
+
+  bool IsShowingDialog(bool is_repeated_request) {
+    if (base::FeatureList::IsEnabled(dialogs::kNonModalDialogs)) {
+      OverlayRequest* request =
+          OverlayRequestQueue::FromWebState(&web_state_,
+                                            OverlayModality::kWebContentArea)
+              ->front_request();
+      if (!request)
+        return false;
+      AppLauncherAlertOverlayRequestConfig* config =
+          request->GetConfig<AppLauncherAlertOverlayRequestConfig>();
+      return config && config->is_repeated_request() == is_repeated_request;
+    } else {
+      UIAlertController* alert_controller =
+          base::mac::ObjCCastStrict<UIAlertController>(
+              base_view_controller_.presentedViewController);
+      NSString* message =
+          is_repeated_request
+              ? l10n_util::GetNSString(IDS_IOS_OPEN_REPEATEDLY_ANOTHER_APP)
+              : l10n_util::GetNSString(IDS_IOS_OPEN_IN_ANOTHER_APP);
+      return alert_controller.message == message;
+    }
   }
 
   web::TestWebState web_state_;
@@ -55,13 +87,7 @@ TEST_F(AppLauncherCoordinatorTest, ItmsUrlShowsAlert) {
                                       launchAppWithURL:GURL("itms://1234")
                                         linkTransition:NO];
   EXPECT_TRUE(app_exists);
-  ASSERT_TRUE([base_view_controller_.presentedViewController
-      isKindOfClass:[UIAlertController class]]);
-  UIAlertController* alert_controller =
-      base::mac::ObjCCastStrict<UIAlertController>(
-          base_view_controller_.presentedViewController);
-  EXPECT_NSEQ(l10n_util::GetNSString(IDS_IOS_OPEN_IN_ANOTHER_APP),
-              alert_controller.message);
+  EXPECT_TRUE(IsShowingDialog(/*is_repeated_request=*/false));
 }
 
 // Tests that in the new AppLauncher, an app URL attempts to launch the
@@ -82,11 +108,5 @@ TEST_F(AppLauncherCoordinatorTest, AppUrlShowsPrompt) {
   [coordinator_ appLauncherTabHelper:tab_helper()
                     launchAppWithURL:GURL("some-app://1234")
                       linkTransition:NO];
-  ASSERT_TRUE([base_view_controller_.presentedViewController
-      isKindOfClass:[UIAlertController class]]);
-  UIAlertController* alert_controller =
-      base::mac::ObjCCastStrict<UIAlertController>(
-          base_view_controller_.presentedViewController);
-  EXPECT_NSEQ(l10n_util::GetNSString(IDS_IOS_OPEN_IN_ANOTHER_APP),
-              alert_controller.message);
+  EXPECT_TRUE(IsShowingDialog(/*is_repeated_request=*/false));
 }
--- ios/chrome/browser/ui/dialogs/dialog_features.mm
+++ ios/chrome/browser/ui/dialogs/dialog_features.mm
@@ -12,6 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace dialogs {
 
 const base::Feature kNonModalDialogs{"kNonModalDialogs",
-                                     base::FEATURE_DISABLED_BY_DEFAULT};
+                                     base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace dialogs
