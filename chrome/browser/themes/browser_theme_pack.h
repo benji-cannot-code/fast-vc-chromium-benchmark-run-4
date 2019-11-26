c41a2aa09976f8220a79f43be4382be6ea6423f9
Wei Li <weili@chromium.org>
2019-11-26 00:05:43 +0000

    Add a build flag for using color pipeline
    Add 'use_color_pipeline' build flag to enable building with color
    pipeline feature. Excluding color_unittests so it can continue running
    on buildbots without config changes.
    
    BUG=1003612
    
    Change-Id: Ie90fe5da6671a0f0d3d0f93bdbb5dc1df8497ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928308
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718901}

--- chrome/browser/themes/browser_theme_pack.h
+++ chrome/browser/themes/browser_theme_pack.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/layout.h"
+#include "ui/color/color_buildflags.h"
 #include "ui/gfx/color_utils.h"
 
 namespace base {
@@ -32,8 +33,11 @@ class Image;
 }
 
 namespace ui {
-class ColorProvider;
 class DataPack;
+
+#if BUILDFLAG(USE_COLOR_PIPELINE)
+class ColorProvider;
+#endif
 }
 
 // An optimized representation of a theme, backed by a mmapped DataPack.
@@ -96,9 +100,11 @@ class BrowserThemePack : public CustomThemeSupplier {
       const override;
   bool HasCustomImage(int id) const override;
 
+#if BUILDFLAG(USE_COLOR_PIPELINE)
   // Builds the color mixers that represent the state of the current browser
   // theme instance.
   void AddCustomThemeColorMixers(ui::ColorProvider* provider) const;
+#endif
 
  private:
   friend class BrowserThemePackTest;
