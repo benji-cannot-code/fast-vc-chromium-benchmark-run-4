e30ef8844de01d245b626cfbc96d06504dd156b0
Julie Jeongeun Kim <jkim@igalia.com>
2019-11-26 23:01:13 +0000

    Clean up old Mojo types for interfaces from compositor_frame_sink.mojom
    This CL cleans up old Mojo types for CompositorFrameSinkClient and
    CompositorFrameSink from SynchronousLayerTreeFrameSink and uses
    PendingReceiver, Receiver, and PendingRemote instead of them.
    
    Bug: 955171
    Change-Id: Id0aa5d5dc2409a1de878764514b599a12dcce46b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935973
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#719382}

--- content/renderer/android/synchronous_layer_tree_frame_sink.h
+++ content/renderer/android/synchronous_layer_tree_frame_sink.h
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/display_client.h"
 #include "components/viz/service/display_embedder/server_shared_bitmap_manager.h"
 #include "ipc/ipc_message.h"
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "services/viz/public/mojom/compositing/compositor_frame_sink.mojom.h"
 #include "ui/gfx/transform.h"
 
@@ -91,8 +93,10 @@ class SynchronousLayerTreeFrameSink
       std::unique_ptr<viz::BeginFrameSource> begin_frame_source,
       SynchronousCompositorRegistry* registry,
       scoped_refptr<FrameSwapMessageQueue> frame_swap_message_queue,
-      viz::mojom::CompositorFrameSinkPtrInfo compositor_frame_sink_info,
-      viz::mojom::CompositorFrameSinkClientRequest client_request);
+      mojo::PendingRemote<viz::mojom::CompositorFrameSink>
+          compositor_frame_sink_remote,
+      mojo::PendingReceiver<viz::mojom::CompositorFrameSinkClient>
+          client_receiver);
   ~SynchronousLayerTreeFrameSink() override;
 
   void SetSyncClient(SynchronousLayerTreeFrameSinkClient* compositor);
@@ -175,10 +179,11 @@ class SynchronousLayerTreeFrameSink
   bool fallback_tick_pending_ = false;
   bool fallback_tick_running_ = false;
 
-  viz::mojom::CompositorFrameSinkPtrInfo unbound_compositor_frame_sink_;
-  viz::mojom::CompositorFrameSinkClientRequest unbound_client_;
+  mojo::PendingRemote<viz::mojom::CompositorFrameSink>
+      unbound_compositor_frame_sink_;
+  mojo::PendingReceiver<viz::mojom::CompositorFrameSinkClient> unbound_client_;
   viz::mojom::CompositorFrameSinkPtr compositor_frame_sink_;
-  mojo::Binding<viz::mojom::CompositorFrameSinkClient> client_binding_;
+  mojo::Receiver<viz::mojom::CompositorFrameSinkClient> client_receiver_{this};
   viz::LocalSurfaceId local_surface_id_;
 
   class StubDisplayClient : public viz::DisplayClient {
