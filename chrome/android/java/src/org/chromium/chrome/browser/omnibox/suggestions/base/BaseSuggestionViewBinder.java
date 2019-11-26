0d4ad2129226162694b954a89505bc6dfa14ef3f
Tomasz Wiszkowski <ender@google.com>
2019-11-26 00:27:10 +0000

    Update Suggestion Fallback icon color for BaseSuggestionView.
    This change ensures suggestions using BaseSuggestionView are using the
    light gray color as a tint color for suggestion fallback icons.
    
    Bug: 1027694
    Change-Id: I073c019a57180843ce703abff16828ce9db74680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935029
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#718913}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewBinder.java
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox.suggestions.base;
 
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.support.annotation.CallSuper;
+import android.support.annotation.ColorRes;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.content.res.AppCompatResources;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -77,19 +80,23 @@ public class BaseSuggestionViewBinder
             rciv.setRoundedCorners(radius, radius, radius, radius);
         }
 
-        updateIcon(view, sds, isDarkMode(model));
+        @ColorRes
+        int tint = isDarkMode(model) ? R.color.default_icon_color_secondary_list
+                                     : R.color.white_mode_tint;
+
+        updateIcon(view, sds, tint);
     }
 
     /** Update attributes of decorated suggestion icon. */
     private static void updateActionIcon(PropertyModel model, BaseSuggestionView baseView) {
         final ImageView view = baseView.getActionImageView();
         final SuggestionDrawableState sds = model.get(BaseSuggestionViewProperties.ACTION_ICON);
-        updateIcon(view, sds, isDarkMode(model));
+        updateIcon(view, sds, ChromeColors.getIconTintRes(!isDarkMode(model)));
     }
 
     /** Update image view using supplied drawable state object */
     private static void updateIcon(
-            ImageView view, SuggestionDrawableState sds, boolean useDarkColors) {
+            ImageView view, SuggestionDrawableState sds, @ColorRes int tintRes) {
         final Resources res = view.getContext().getResources();
 
         view.setVisibility(sds == null ? View.GONE : View.VISIBLE);
@@ -99,10 +106,12 @@ public class BaseSuggestionViewBinder
             return;
         }
 
-        view.setImageDrawable(sds.drawable);
+        ColorStateList tint = null;
         if (sds.allowTint) {
-            ApiCompatibilityUtils.setImageTintList(
-                    view, ChromeColors.getIconTint(view.getContext(), !useDarkColors));
+            tint = AppCompatResources.getColorStateList(view.getContext(), tintRes);
         }
+
+        view.setImageDrawable(sds.drawable);
+        ApiCompatibilityUtils.setImageTintList(view, tint);
     }
 }
