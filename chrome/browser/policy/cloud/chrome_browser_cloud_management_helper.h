30cf60fd470704304dd2c520f43b3913bda87328
Dominique Fauteux-Chapleau <domfc@chromium.org>
2019-11-26 20:22:53 +0000

    Invalidate DM token on DM_STATUS_SERVICE_DEVICE_NOT_FOUND status
    Invalidates the DM token in storage when CloudPolicyClient returns a
    410 error code.
    
    Bug: 1020289
    Change-Id: I4e6de76e3d04de6c2ffc70def84f59b94b403766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1934589
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Tien Mai <tienmai@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719303}

--- chrome/browser/policy/cloud/chrome_browser_cloud_management_helper.h
+++ chrome/browser/policy/cloud/chrome_browser_cloud_management_helper.h
@@ -78,6 +78,11 @@ class MachineLevelUserCloudPolicyFetcher : public CloudPolicyService::Observer {
   void SetupRegistrationAndFetchPolicy(const DMToken& dm_token,
                                        const std::string& client_id);
 
+  // Add or remove |observer| to/from the CloudPolicyClient embedded in
+  // |policy_manager_|.
+  void AddClientObserver(CloudPolicyClient::Observer* observer);
+  void RemoveClientObserver(CloudPolicyClient::Observer* observer);
+
   // CloudPolicyService::Observer:
   void OnCloudPolicyServiceInitializationCompleted() override;
 
