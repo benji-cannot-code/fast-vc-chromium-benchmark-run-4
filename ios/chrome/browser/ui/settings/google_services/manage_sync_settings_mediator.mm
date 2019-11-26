43584610d8ad48905a47904842c54c23bcb37964
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2019-11-26 21:10:32 +0000

    [iOS][Password-Breach] Move toggle to google services
    This changes might over update the switch and therefore the on/off
    animation is being killed. To be fixed in a follow up for a later
    release.
    
    Bug: 1028095
    Change-Id: Id9e28eeecde05f230c34d1b4f00fdb3df650f73b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937172
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719329}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_mediator.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/logging.h"
+#include "base/mac/foundation_util.h"
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/sync/driver/sync_service.h"
@@ -399,14 +400,15 @@ NSString* kGoogleServicesSyncErrorImage = @"google_services_sync_error";
 
 #pragma mark - ManageSyncSettingsServiceDelegate
 
-- (void)toggleSwitchItem:(SyncSwitchItem*)switchItem withValue:(BOOL)value {
+- (void)toggleSwitchItem:(TableViewItem*)item withValue:(BOOL)value {
   {
     // The notifications should be ignored to get smooth switch animations.
     // Notifications are sent by SyncObserverModelBridge while changing
     // settings.
     base::AutoReset<BOOL> autoReset(&_ignoreSyncStateChanges, YES);
-    switchItem.on = value;
-    ItemType itemType = static_cast<ItemType>(switchItem.type);
+    SyncSwitchItem* syncSwitchItem = base::mac::ObjCCast<SyncSwitchItem>(item);
+    syncSwitchItem.on = value;
+    ItemType itemType = static_cast<ItemType>(item.type);
     switch (itemType) {
       case SyncEverythingItemType:
         self.syncSetupService->SetSyncingAllDataTypes(value);
@@ -424,9 +426,10 @@ NSString* kGoogleServicesSyncErrorImage = @"google_services_sync_error";
       case PasswordsDataTypeItemType:
       case ReadingListDataTypeItemType:
       case SettingsDataTypeItemType: {
+        DCHECK(syncSwitchItem);
         SyncSetupService::SyncableDatatype dataType =
             static_cast<SyncSetupService::SyncableDatatype>(
-                switchItem.dataType);
+                syncSwitchItem.dataType);
         syncer::ModelType modelType =
             self.syncSetupService->GetModelType(dataType);
         self.syncSetupService->SetDataTypeEnabled(modelType, value);
