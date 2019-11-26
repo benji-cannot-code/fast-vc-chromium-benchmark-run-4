4840de26a1bdf6d2a079e244c4939647ac47b0df
Primiano Tucci <primiano@chromium.org>
2019-11-26 18:28:06 +0000

    Roll src/third_party/perfetto b62621c1dd8e..1b1f18689958 (6 commits)
    https://android.googlesource.com/platform/external/perfetto.git/+log/b62621c1dd8e..1b1f18689958
    
    git log b62621c1dd8e..1b1f18689958 --date=short --first-parent --format='%ad %ae %s'
    2019-11-26 primiano@google.com Fix chromium build, remove some assert_no_deps
    2019-11-26 treehugger-gerrit@google.com Merge "Refactor: define oom_score_adj span once"
    2019-11-26 fmayer@google.com Merge "Handle DeobfuscationMapping."
    2019-11-26 treehugger-gerrit@google.com Merge "perfetto-ui: Changing picture in the record page for heap profiling"
    2019-11-26 primiano@google.com Merge "Remove libprotobuf from IPC layer"
    2019-11-26 primiano@google.com Merge "tools(tmux): correctly find ninja"
    
    Change-Id: I3d771fa153e89ac0b8edf1a7c40b27f3d151bb89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937169
    Commit-Queue: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Primiano Tucci <primiano@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719234}

--- services/tracing/perfetto/producer_host.h
+++ services/tracing/perfetto/producer_host.h
@@ -17,10 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/tracing/public/mojom/perfetto_service.mojom.h"
 #include "third_party/perfetto/include/perfetto/ext/tracing/core/producer.h"
 #include "third_party/perfetto/include/perfetto/ext/tracing/core/tracing_service.h"
-
-namespace perfetto {
-class CommitDataRequest;
-}  // namespace perfetto
+#include "third_party/perfetto/include/perfetto/tracing/core/forward_decls.h"
 
 namespace tracing {
 
