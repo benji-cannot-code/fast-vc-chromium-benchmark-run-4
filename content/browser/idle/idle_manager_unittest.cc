cb70e0aca7bfdbb309cd8c5dfb57ff9074753f46
Gyuyoung Kim <gyuyoung@igalia.com>
2019-11-26 22:14:02 +0000

    Clean up all old uses of IdleManager interface
    This CL converts all uses of IdleManager interface
    as below,
    
      - Convert BindingSet to ReceiverSet
      - Convert IdleManagerRequest to
        PendingReceiver<IdleManager>
    
    Bug: 955171
    Change-Id: I89f1a325b0e3290c1fdb36d5bc92389c8716e546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1932139
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719360}

--- content/browser/idle/idle_manager_unittest.cc
+++ content/browser/idle/idle_manager_unittest.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/mojom/idle/idle_manager.mojom.h"
 
-using blink::mojom::IdleManagerPtr;
 using blink::mojom::IdleMonitorPtr;
 using ::testing::_;
 using ::testing::InSequence;
