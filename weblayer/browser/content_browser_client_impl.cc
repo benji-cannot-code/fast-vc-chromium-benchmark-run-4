2482eb9f36b86e5f7a6add1720f9f01560e6efb3
Bo Liu <boliu@chromium.org>
2019-11-26 19:26:03 +0000

    weblayer: Use cache dir
    Looking at chrome and android webview, looks like cache dir is used for
    http cache and code cache. Use these in weblayer as well. Note before
    these, neither disk cache are enabled, so there is no need to consider
    any migration issue.
    
    Bug: 1027627
    Change-Id: Ib6e615e975a9cd8f7e456c6f5af1c0fac4bceea7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937087
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719271}

--- weblayer/browser/content_browser_client_impl.cc
+++ weblayer/browser/content_browser_client_impl.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/devtools_manager_delegate.h"
+#include "content/public/browser/generated_code_cache_settings.h"
 #include "content/public/browser/navigation_throttle.h"
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/common/content_switches.h"
@@ -36,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 #include "weblayer/browser/browser_main_parts_impl.h"
 #include "weblayer/browser/i18n_util.h"
+#include "weblayer/browser/profile_impl.h"
 #include "weblayer/browser/ssl_error_handler.h"
 #include "weblayer/browser/tab_impl.h"
 #include "weblayer/browser/weblayer_content_browser_overlay_manifest.h"
@@ -195,6 +197,8 @@ ContentBrowserClientImpl::CreateNetworkContext(
     base::FilePath cookie_path = context->GetPath();
     cookie_path = cookie_path.Append(FILE_PATH_LITERAL("Cookies"));
     context_params->cookie_path = cookie_path;
+    context_params->http_cache_path =
+        ProfileImpl::GetCachePath(context).Append(FILE_PATH_LITERAL("Cache"));
   }
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   if (command_line->HasSwitch(switches::kProxyServer)) {
@@ -310,6 +314,17 @@ ContentBrowserClientImpl::CreateThrottlesForNavigation(
       handle, std::make_unique<SSLCertReporterImpl>(),
       base::Bind(&HandleSSLError), base::Bind(&IsInHostedApp)));
   return throttles;
+}
+
+content::GeneratedCodeCacheSettings
+ContentBrowserClientImpl::GetGeneratedCodeCacheSettings(
+    content::BrowserContext* context) {
+  DCHECK(context);
+  // If we pass 0 for size, disk_cache will pick a default size using the
+  // heuristics based on available disk size. These are implemented in
+  // disk_cache::PreferredCacheSize in net/disk_cache/cache_util.cc.
+  return content::GeneratedCodeCacheSettings(
+      true, 0, ProfileImpl::GetCachePath(context));
 }
 
 #if defined(OS_LINUX) || defined(OS_ANDROID)
--- weblayer/browser/content_browser_client_impl.h
+++ weblayer/browser/content_browser_client_impl.h
@@ -69,6 +69,8 @@ class ContentBrowserClientImpl : public content::ContentBrowserClient {
                        bool* no_javascript_access) override;
   std::vector<std::unique_ptr<content::NavigationThrottle>>
   CreateThrottlesForNavigation(content::NavigationHandle* handle) override;
+  content::GeneratedCodeCacheSettings GetGeneratedCodeCacheSettings(
+      content::BrowserContext* context) override;
 
 #if defined(OS_LINUX) || defined(OS_ANDROID)
   void GetAdditionalMappedFilesForChildProcess(
