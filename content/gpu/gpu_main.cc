25e959ddeebf09bd1b74a71d0ec8b802823820fc
Robert Sesek <rsesek@chromium.org>
2019-11-26 16:02:27 +0000

    mac: Force InitializeVideoToolbox() in the GPU's PreSandboxStartup().
    Under the V2 sandbox architecture, there is technically nothing that
    runs "pre-sandbox startup". But we hypothesize that deferring the
    initialization of VideoToolbox to the first use is leading to hangs,
    which the watchdog turns into crashes. By forcing initialization at this
    call site, the GPU watchdog thread is not running, which is what happens
    under the V1 sandbox architecture.
    
    Bug: 871280
    Change-Id: I7c1173ecf7752307c19ac15f81e9b1c699d4523b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1932166
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719164}

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -93,6 +93,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_MACOSX)
 #include "base/message_loop/message_pump_mac.h"
 #include "components/metal_util/test_shader.h"
+#include "content/public/common/content_features.h"
+#include "media/gpu/mac/vt_video_decode_accelerator_mac.h"
 #include "sandbox/mac/seatbelt.h"
 #include "services/service_manager/sandbox/mac/sandbox_mac.h"
 #endif
@@ -156,6 +158,13 @@ class ContentSandboxHelper : public gpu::GpuSandboxHelper {
 #if defined(OS_WIN)
     media::DXVAVideoDecodeAccelerator::PreSandboxInitialization();
     media::MediaFoundationVideoEncodeAccelerator::PreSandboxInitialization();
+#endif
+
+#if defined(OS_MACOSX)
+    if (base::FeatureList::IsEnabled(features::kMacV2GPUSandbox)) {
+      TRACE_EVENT0("gpu", "Initialize VideoToolbox");
+      media::InitializeVideoToolbox();
+    }
 #endif
 
     // On Linux, reading system memory doesn't work through the GPU sandbox.
