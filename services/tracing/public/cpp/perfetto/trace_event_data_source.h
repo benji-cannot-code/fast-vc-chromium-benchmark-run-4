ccebac66116097501a36a8ca60a8788e5945316e
Stephen Nusko <nuskos@chromium.org>
2019-11-26 00:27:44 +0000

    Add TRACE_EVENT_BEGIN/TRACE_EVENT_END functions that allow typed arguments.
    These macros allow users to write small lambda functions that get direct access
    to the protozero TrackEvent. They can then access and fill in the values as
    needed.
    
    This seems to have no noticeable performance impact:
    https://docs.google.com/spreadsheets/d/1YBB7V2hinQ-lcf9WBFrdMzxakosJHou0B3U10Gzpme0/edit?usp=sharing
    
    And binary size is slightly negative (probably more zero like).
    
    Change-Id: I2e9266daad902ac64bdaa1a1f1bf5678dc48ac9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1921984
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718914}

--- services/tracing/public/cpp/perfetto/trace_event_data_source.h
+++ services/tracing/public/cpp/perfetto/trace_event_data_source.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "base/trace_event/trace_config.h"
 #include "services/tracing/public/cpp/perfetto/perfetto_traced_process.h"
+#include "services/tracing/public/cpp/perfetto/track_event_thread_local_event_sink.h"
 #include "third_party/perfetto/protos/perfetto/trace/chrome/chrome_metadata.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/chrome/chrome_trace_event.pbzero.h"
 
@@ -36,6 +37,7 @@ namespace perfetto {
 class StartupTraceWriter;
 class StartupTraceWriterRegistry;
 class TraceWriter;
+class EventContext;
 }
 
 namespace tracing {
@@ -186,6 +188,23 @@ class COMPONENT_EXPORT(TRACING_CPP) TraceEventDataSource
 
   bool IsEnabled();
 
+  static TrackEventThreadLocalEventSink* GetOrPrepareEventSink(
+      bool thread_will_flush);
+
+  template <
+      typename TrackEventArgumentFunction = void (*)(perfetto::EventContext)>
+  static void OnAddTraceEvent(base::trace_event::TraceEvent* trace_event,
+                              bool thread_will_flush,
+                              base::trace_event::TraceEventHandle* handle,
+                              TrackEventArgumentFunction func) {
+    auto* thread_local_event_sink = GetOrPrepareEventSink(thread_will_flush);
+    if (thread_local_event_sink) {
+      AutoThreadLocalBoolean thread_is_in_trace_event(
+          GetThreadIsInTraceEventTLS());
+      thread_local_event_sink->AddTraceEvent(trace_event, handle, func);
+    }
+  }
+
  private:
   friend class base::NoDestructor<TraceEventDataSource>;
 
@@ -204,7 +223,8 @@ class COMPONENT_EXPORT(TRACING_CPP) TraceEventDataSource
   void UnregisterFromTraceLog();
 
   std::unique_ptr<perfetto::StartupTraceWriter> CreateTraceWriterLocked();
-  ThreadLocalEventSink* CreateThreadLocalEventSink(bool thread_will_flush);
+  TrackEventThreadLocalEventSink* CreateThreadLocalEventSink(
+      bool thread_will_flush);
 
   // Callback from TraceLog, can be called from any thread.
   static void OnAddTraceEvent(base::trace_event::TraceEvent* trace_event,
