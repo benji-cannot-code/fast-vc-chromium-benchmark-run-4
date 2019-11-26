a8f0d1d1a754439f343f61cb19f6849779e9b721
rbpotter <rbpotter@chromium.org>
2019-11-26 19:12:28 +0000

    Web UI: Move listenForPrivilegedLinkClicks and handler to history
    This utility function and corresponding handler are only used by the
    history UI, so move them to history specific files/folders.
    
    Bug: 1022212
    Change-Id: I12c1cd4758980ca36888922f289674f1b79637ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928160
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Dan Beam <dbeam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719257}

--- chrome/browser/ui/webui/history/navigation_handler.h
+++ chrome/browser/ui/webui/history/navigation_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_NAVIGATION_HANDLER_H_
-#define CHROME_BROWSER_UI_WEBUI_NAVIGATION_HANDLER_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_HISTORY_NAVIGATION_HANDLER_H_
+#define CHROME_BROWSER_UI_WEBUI_HISTORY_NAVIGATION_HANDLER_H_
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
@@ -33,4 +33,4 @@ class NavigationHandler : public content::WebUIMessageHandler {
 
 }  // namespace webui
 
-#endif  // CHROME_BROWSER_UI_WEBUI_NAVIGATION_HANDLER_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_HISTORY_NAVIGATION_HANDLER_H_
