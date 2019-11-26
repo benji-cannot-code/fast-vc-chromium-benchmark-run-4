b4a49f0769dc0b491537c933d9f4acd45863da47
Natalie Chouinard <chouinard@chromium.org>
2019-11-26 23:11:47 +0000

    Resolve settings presubmit errors
    Fix some of the more trivial presubmit warnings and  errors in the
    preferences package. This will reduce the number of failures we'll need
    to bypass in the upcoming package renaming.
    
    Bug: 1027271
    Change-Id: I5bda8192c91158d281f214a4d91e907f058a24d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935716
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719390}

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/ClearBrowsingDataFetcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/ClearBrowsingDataFetcher.java
@@ -82,8 +82,9 @@ public class ClearBrowsingDataFetcher
      * shown yet.
      */
     public void requestInfoAboutOtherFormsOfBrowsingHistory() {
-        if (!OtherFormsOfHistoryDialogFragment.wasDialogShown())
+        if (!OtherFormsOfHistoryDialogFragment.wasDialogShown()) {
             BrowsingDataBridge.getInstance().requestInfoAboutOtherFormsOfBrowsingHistory(this);
+        }
     }
 
     /**
