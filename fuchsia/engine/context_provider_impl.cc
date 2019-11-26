4d105dcedf7b0db6bca7dbd481c1945b222b89c4
Emircan Uysaler <emircan@chromium.org>
2019-11-26 03:27:08 +0000

    Revert "[Fuchsia] Allow enabling blink features"
    This reverts commit c76ce4abfb673ee8118663b566efd7d510c49484.
    
    Reason for revert: Look for an alternate way for blink features.
    
    Original change's description:
    > [Fuchsia] Allow enabling blink features
    > 
    > This CL would allow us to enable work that are behind feature flags.
    > 
    > Bug: 1026011
    > Change-Id: Ifaa69e184811ffe0062cc10389b31e54fc8a76db
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931657
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#718541}
    
    TBR=sergeyu@chromium.org,emircan@chromium.org
    
    Change-Id: I90da52000325b1045836222e59b26c0c2d6e42b9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1026011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935415
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Emircan Uysaler <emircan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718978}

--- fuchsia/engine/context_provider_impl.cc
+++ fuchsia/engine/context_provider_impl.cc
@@ -148,7 +148,6 @@ bool MaybeAddCommandLineArgsFromConfig(const base::Value& config,
     return true;
 
   static const base::StringPiece kAllowedArgs[] = {
-      switches::kEnableBlinkFeatures,
       switches::kRendererProcessLimit,
       switches::kMinHeightForGpuRasterTile,
   };
