8a40cdcaa5eae1a3f49412e92cd268dc66934a35
Yifan Luo <lyf@google.com>
2019-11-26 16:47:06 +0000

    Change sec-fetch-site to none for extension background requests
    Bug: 995475
    Change-Id: I2eba159d9a462bf29e3f3c944a57b70d19914040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1903362
    Commit-Queue: Yifan Luo <lyf@google.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719183}

--- extensions/browser/url_loader_factory_manager.cc
+++ extensions/browser/url_loader_factory_manager.cc
@@ -310,6 +310,7 @@ bool IsSpecialURLLoaderFactoryRequired(const Extension& extension,
 }
 
 void OverrideFactoryParams(const Extension& extension,
+                           FactoryUser factory_user,
                            network::mojom::URLLoaderFactoryParams* params) {
   // Setup factory bound allow list that overwrites per-profile common list
   // to allow tab specific permissions only for this newly created factory.
@@ -325,6 +326,9 @@ void OverrideFactoryParams(const Extension& extension,
   // TODO(lukasza): https://crbug.com/1016904: Use more granular CORB
   // enforcement based on the specific |extension|'s permissions.
   params->is_corb_enabled = false;
+
+  if (factory_user == FactoryUser::kExtensionProcess)
+    params->unsafe_non_webby_initiator = true;
 }
 
 void MarkIsolatedWorldsAsRequiringSeparateURLLoaderFactory(
@@ -542,7 +546,7 @@ void URLLoaderFactoryManager::OverrideURLLoaderFactoryParams(
   if (!IsSpecialURLLoaderFactoryRequired(*extension, factory_user))
     return;
 
-  OverrideFactoryParams(*extension, factory_params);
+  OverrideFactoryParams(*extension, factory_user, factory_params);
 }
 
 // static
