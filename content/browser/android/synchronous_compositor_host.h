ec60d903f0f7e64ed8e11aa62e707096f4fd86f9
Bo Liu <boliu@chromium.org>
2019-11-26 01:54:57 +0000

    Remove renderer side root fling hooks
    Fling has moved to browser side. Remove webview's hooks into root layer
    fling on the renderer side since they are obsolete.
    
    Change-Id: I81489a2458b6808ee516a4f76ca79df58e44f38e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1933448
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#718956}

--- content/browser/android/synchronous_compositor_host.h
+++ content/browser/android/synchronous_compositor_host.h
@@ -135,9 +135,6 @@ class SynchronousCompositorHost : public SynchronousCompositor,
   // isn't required.
   bool allow_async_draw_ = false;
 
-  // Indicates the next draw needs to be synchronous
-  bool compute_scroll_needs_synchronous_draw_ = false;
-
   // Indicates begin frames are paused from the browser.
   bool begin_frame_paused_ = false;
 
@@ -152,7 +149,6 @@ class SynchronousCompositorHost : public SynchronousCompositor,
 
   // From renderer.
   uint32_t renderer_param_version_;
-  bool need_animate_scroll_;
   uint32_t need_invalidate_count_;
   bool invalidate_needs_draw_;
   uint32_t did_activate_pending_tree_count_;
