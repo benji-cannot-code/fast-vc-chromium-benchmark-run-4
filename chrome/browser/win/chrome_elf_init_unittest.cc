1849746ed9c88894c9f97e55ac75a76d027072b8
Mikel Astiz <mastiz@chromium.org>
2019-11-26 17:08:36 +0000

    Do not instantiate base::FieldTrialList explicitly in tests
    The test suite already instantiates a FieldTrialList as per
    https://chromium-review.googlesource.com/c/chromium/src/+/1883567 so
    it's no longer necessary to do so explicitly in tests.
    
    This patch addresses unit tests under:
    /chrome/browser/win/chrome_elf_init_unittest.cc
    
    This CL was uploaded by git cl split.
    
    Bug: 1018667
    Change-Id: I4743155712ef4dc72ae9bde108f182d47eb5b60e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1917512
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719195}

--- chrome/browser/win/chrome_elf_init_unittest.cc
+++ chrome/browser/win/chrome_elf_init_unittest.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/chrome_elf/chrome_elf_constants.h"
 #include "chrome/common/chrome_version.h"
 #include "chrome/install_static/install_util.h"
-#include "components/variations/entropy_provider.h"
 #include "components/variations/variations_associated_data.h"
 #include "components/version_info/version_info.h"
 #include "content/public/test/browser_task_environment.h"
@@ -92,10 +91,6 @@ TEST_F(ChromeBlacklistTrialTest, BlacklistDisabledRun) {
   blacklist_registry_key_->WriteValue(blacklist::kBeaconState,
                                       blacklist::BLACKLIST_ENABLED);
   blacklist_registry_key_->WriteValue(blacklist::kBeaconVersion, L"Data");
-
-  // Create the field trial with the blacklist disabled group.
-  base::FieldTrialList field_trial_list(
-      std::make_unique<variations::SHA1EntropyProvider>("test"));
 
   scoped_refptr<base::FieldTrial> trial(
     base::FieldTrialList::CreateFieldTrial(
