9e9be1b94dd8ed76ef874fd3985a8a81e295810b
danakj <danakj@chromium.org>
2019-11-26 16:53:50 +0000

    Convert Callbacks to OnceCallbacks for //content/app/
    Use OnceCallback where possible, and BindRepeating where it is
    meant to be called more than once.
    
    The Closure here was explicitly a RepeatingClosure in one place,
    but it really should be a OnceClosure. The closure is used to
    exit the RunLoop, which can't occur more than once. It is
    currently not used at all.
    
    R=jam@chromium.org
    
    Bug: 953861
    Change-Id: I8a6b2a36b10c8fe523d163cf26fccaa8b155a319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1934785
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719186}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -390,7 +390,6 @@ _NOT_CONVERTED_TO_MODERN_BIND_AND_CALLBACK = '|'.join((
   '^components/webdata_services/',
   '^components/wifi/',
   '^components/zoom/',
-  '^content/app/',
   '^content/browser/',
   '^content/child/',
   '^content/public/',
