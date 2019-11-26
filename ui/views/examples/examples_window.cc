3bec3e091ac81ccf3d72757377b137dd2389afc7
Sergey Talantov <sergey.talantov@gmail.com>
2019-11-26 08:23:35 +0000

    Call CreateExampleView also for extra examples
    Since ContainerView was eliminated here:
    https://chromium-review.googlesource.com/c/chromium/src/+/1644758,
    manual call of CreateExampleView is required now.
    But for extra examples it is not called.
    
    BUG=1026180
    
    Change-Id: I891c6099ff742110b62d870236acfcc25039579a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926131
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719057}

--- ui/views/examples/examples_window.cc
+++ ui/views/examples/examples_window.cc
@@ -91,9 +91,6 @@ ExampleVector CreateExamples() {
   examples.push_back(std::make_unique<TreeViewExample>());
   examples.push_back(std::make_unique<VectorExample>());
   examples.push_back(std::make_unique<WidgetExample>());
-
-  for (auto& example : examples)
-    example->CreateExampleView(example->example_view());
   return examples;
 }
 
@@ -108,6 +105,9 @@ ExampleVector GetExamplesToShow(ExampleVector extra) {
   ExampleVector examples = CreateExamples();
   std::move(extra.begin(), extra.end(), std::back_inserter(examples));
   std::sort(examples.begin(), examples.end(), ExampleTitleCompare());
+
+  for (auto& example : examples)
+    example->CreateExampleView(example->example_view());
   return examples;
 }
 
