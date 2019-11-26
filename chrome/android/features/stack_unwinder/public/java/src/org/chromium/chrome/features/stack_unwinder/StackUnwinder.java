880acad4c60b40085cf628b511d6d3ac934377fb
Mike Wittman <wittman@chromium.org>
2019-11-26 22:46:45 +0000

    [Sampling profiler] Add Android unwinder DFM infrastructure
    Adds the infrastructure to support a dynamic feature module for Android
    stack unwinding. This module will provide the native code implementation
    of a stack unwinder supporting unwinding of stack frames corresponding
    to native system libraries and Java code.
    
    This CL contains just the infrastructure to set up the DFM, which
    is derived from the existing 'test dummy' DFM. The stack unwinder
    implementation and the use of the module from Chrome code are deferred
    to later CLs.
    
    Bug: 1027654
    Change-Id: I4b5dca1294fa1aa8197eab4544a03dd7d5bc48a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1934774
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719373}

---
+++ chrome/android/features/stack_unwinder/public/java/src/org/chromium/chrome/features/stack_unwinder/StackUnwinder.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.features.stack_unwinder;
+
+/** Provides a mechanism to register the native unwinder. */
+public interface StackUnwinder {
+    /**
+     * Registers the native unwinder with the StackSamplingProfiler.
+     */
+    void registerUnwinder();
+}
---
+++ chrome/android/modules/stack_unwinder/internal/entrypoints.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/jni_utils.h"
+#include "chrome/android/modules/stack_unwinder/internal/jni_registration.h"
+
+extern "C" {
+// This JNI registration method is found and called by module framework code.
+__attribute__((visibility("default"))) bool JNI_OnLoad_stack_unwinder(
+    JNIEnv* env) {
+  if (!base::android::IsSelectiveJniRegistrationEnabled(env) &&
+      !stack_unwinder::RegisterNonMainDexNatives(env)) {
+    return false;
+  }
+  if (!stack_unwinder::RegisterMainDexNatives(env)) {
+    return false;
+  }
+  return true;
+}
+
+}  // extern "C"
