6d0de3700cc2fb360bb33adbc0cff4dc0855570c
Daniel Rubery <drubery@chromium.org>
2019-11-27 01:24:52 +0000

    Extend the largeUnscannedFileEvent to a more generic unscannedFileEvent
    This CL changes the structure of the reporting event for large unscanned
    files. Future CLs will report this event with additional reasons.
    
    Bug: 1014949
    Change-Id: I8e75fd9c6bb55b09fd3b72553a636991d37d4b39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935488
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719452}

--- chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
+++ chrome/browser/safe_browsing/download_protection/check_client_download_request.cc
@@ -137,8 +137,14 @@ void MaybeReportDeepScanningVerdict(Profile* profile,
                                     DeepScanningClientResponse response) {
   if (result == BinaryUploadService::Result::FILE_TOO_LARGE) {
     extensions::SafeBrowsingPrivateEventRouterFactory::GetForProfile(profile)
-        ->OnLargeUnscannedFileEvent(url, file_name, download_digest_sha256,
-                                    mime_type, trigger, content_size);
+        ->OnUnscannedFileEvent(url, file_name, download_digest_sha256,
+                               mime_type, trigger, "fileTooLarge",
+                               content_size);
+  } else if (result == BinaryUploadService::Result::TIMEOUT) {
+    extensions::SafeBrowsingPrivateEventRouterFactory::GetForProfile(profile)
+        ->OnUnscannedFileEvent(url, file_name, download_digest_sha256,
+                               mime_type, trigger, "scanTimedOut",
+                               content_size);
   }
 
   if (result != BinaryUploadService::Result::SUCCESS)
@@ -268,6 +274,8 @@ CheckClientDownloadRequest::CheckClientDownloadRequest(
           item->GetFullPath(),
           {item->GetTabUrl(), item->GetTabReferrerUrl()},
           item->GetReceivedBytes(),
+          item->GetMimeType(),
+          item->GetHash(),
           content::DownloadItemUtils::GetBrowserContext(item),
           callback,
           service,
