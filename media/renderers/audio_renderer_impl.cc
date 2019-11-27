6323817210d07f1db1a92df03975a434d198fdce
Sergey Ulanov <sergeyu@chromium.org>
2019-11-27 21:46:35 +0000

    Update AudioRenderer::Flush() to use OnceClosure
    Flush() was taking base::Closure, which is deprecated. Replaced with
    OnceClosure.
    
    Bug: 751838
    Change-Id: I87825cfc58b92e32d6a7e3fff1cf062597fac396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940878
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719764}

--- media/renderers/audio_renderer_impl.cc
+++ media/renderers/audio_renderer_impl.cc
@@ -260,7 +260,7 @@ TimeSource* AudioRendererImpl::GetTimeSource() {
   return this;
 }
 
-void AudioRendererImpl::Flush(const base::Closure& callback) {
+void AudioRendererImpl::Flush(base::OnceClosure callback) {
   DVLOG(1) << __func__;
   DCHECK(task_runner_->BelongsToCurrentThread());
   TRACE_EVENT_ASYNC_BEGIN0("media", "AudioRendererImpl::Flush", this);
@@ -274,7 +274,7 @@ void AudioRendererImpl::Flush(const base::Closure& callback) {
   DCHECK_EQ(state_, kPlaying);
   DCHECK(!flush_cb_);
 
-  flush_cb_ = callback;
+  flush_cb_ = std::move(callback);
   ChangeState_Locked(kFlushing);
 
   if (pending_read_)
@@ -318,7 +318,7 @@ void AudioRendererImpl::ResetDecoderDone() {
 
   // Changes in buffering state are always posted. Flush callback must only be
   // run after buffering state has been set back to nothing.
-  flush_cb_ = BindToCurrentLoop(flush_cb_);
+  flush_cb_ = BindToCurrentLoop(std::move(flush_cb_));
   FinishFlush();
 }
 
