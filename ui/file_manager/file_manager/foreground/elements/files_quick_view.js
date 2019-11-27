920a054a816f8ee50fb339fdcec86b703f08cc23
Luciano Pacheco <lucmult@chromium.org>
2019-11-27 09:01:08 +0000

    Files app: Disable QuickView OPEN button when in a modal dialog
    Change the way to hide Open button to use isModal state, instead of
    changing the semantics of the property |hasTask|, so keep |hasTask|
    meaning that the file has tasks.
    
    Add |isModal| property to <files-quick-view> to be able to hide the
    OPEN button when in a modal dialog.
    
    Change the OPEN button |hidden| condition to check for isModal
    attribute.
    
    Convert some variable definitions to const as forced by presubmit.
    
    Test: browser_tests --gtest_filter="*openQuickViewInFileDialog*"
    Bug: 948605
    Fixed: 948605
    Change-Id: I1c0c5f6c0178d258ec5d1a2f72b076cded0f8adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935276
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719529}

--- ui/file_manager/file_manager/foreground/elements/files_quick_view.js
+++ ui/file_manager/file_manager/foreground/elements/files_quick_view.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var FilesQuickView = Polymer({
+const FilesQuickView = Polymer({
   is: 'files-quick-view',
 
   properties: {
@@ -39,6 +39,12 @@ var FilesQuickView = Polymer({
     // Text shown when there is no playback/preview available.
     noPlaybackText: String,
     noPreviewText: String,
+
+    /**
+     * True if the Files app window is a dialog, e.g. save-as or open-with.
+     * @type {boolean}
+     */
+    isModal: Boolean,
   },
 
   listeners: {
@@ -119,6 +125,17 @@ var FilesQuickView = Polymer({
    * @param {!Event} event
    */
   onOpenInNewButtonTap: function(event) {},
+
+  /**
+   * @param {boolean} hasTask
+   * @param {boolean} isModal
+   * @return {boolean}
+   *
+   * @private
+   */
+  shouldShowOpenButton_: function(hasTask, isModal) {
+    return hasTask && !isModal;
+  },
 
   /**
    * @param {!Event} event tap event.
