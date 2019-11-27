38d6dfe11c0ddfcc3541df2a1c19fe7b0c5b15d2
Carlos IL <carlosil@chromium.org>
2019-11-27 19:40:31 +0000

    Fix SBER reporting for subresource interstitials
    This CL prevents an extra report from being created when showing an
    interstitial for a bad subresource, which caused reporting to break
    for subresource triggered committed interstitials. This also re-enables
    the test for this behavior.
    
    Bug: 1024015
    Change-Id: I0bd601a2adfc70dc46ca34d5de0518f26dce4932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913709
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719701}

--- chrome/browser/safe_browsing/safe_browsing_blocking_page_unittest.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page_unittest.cc
@@ -130,6 +130,7 @@ class TestSafeBrowsingBlockingPage : public SafeBrowsingBlockingPage {
                                  main_frame_url,
                                  unsafe_resources,
                                  display_options,
+                                 true,
                                  url_loader_for_testing) {
     // Don't delay details at all for the unittest.
     SetThreatDetailsProceedDelayForTesting(0);
@@ -156,8 +157,8 @@ class TestSafeBrowsingBlockingPageFactory
       BaseUIManager* manager,
       WebContents* web_contents,
       const GURL& main_frame_url,
-      const SafeBrowsingBlockingPage::UnsafeResourceList& unsafe_resources)
-      override {
+      const SafeBrowsingBlockingPage::UnsafeResourceList& unsafe_resources,
+      bool should_trigger_reporting) override {
     PrefService* prefs =
         Profile::FromBrowserContext(web_contents->GetBrowserContext())
             ->GetPrefs();
@@ -197,7 +198,8 @@ class TestSafeBrowsingBlockingPageQuiet : public SafeBrowsingBlockingPage {
                                  web_contents,
                                  main_frame_url,
                                  unsafe_resources,
-                                 display_options),
+                                 display_options,
+                                 true),
         sb_error_ui_(unsafe_resources[0].url,
                      main_frame_url,
                      GetInterstitialReason(unsafe_resources),
@@ -236,8 +238,8 @@ class TestSafeBrowsingBlockingQuietPageFactory
       BaseUIManager* manager,
       WebContents* web_contents,
       const GURL& main_frame_url,
-      const SafeBrowsingBlockingPage::UnsafeResourceList& unsafe_resources)
-      override {
+      const SafeBrowsingBlockingPage::UnsafeResourceList& unsafe_resources,
+      bool should_trigger_reporting) override {
     PrefService* prefs =
         Profile::FromBrowserContext(web_contents->GetBrowserContext())
             ->GetPrefs();
