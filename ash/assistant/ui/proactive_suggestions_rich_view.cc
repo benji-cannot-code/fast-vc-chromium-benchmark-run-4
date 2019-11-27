621ed41e49e8c6e9d416dc865d6a8ab9c1bd8db2
David Black <dmblack@google.com>
2019-11-27 18:58:10 +0000

    Don't show ProactiveSuggestionsRichView until loading stops.
    ProactiveSuggestionsRichView's size (and therefore bounds) are
    determined by the web contents that it hosts. But the preferred size of
    the web contents is not known until it has stopped loading.
    
    This can cause UI jank if the widget is shown before loading has
    stopped, as our view is resized and rebounded.
    
    To address this, ProactiveSuggestionsView now exposes its own
    ShowWhenReady/Hide/Close APIs that should be used by outside classes
    instead of calling APIs on the owning widget directly.
    
    In the case of ProactiveSuggestionsRichView, calling ShowWhenReady will
    cause an async show if the widget is not yet ready for showing.
    
    This CL also addresses a TODO to parameterize corner radius for the rich
    entry point.
    
    Bug: b:145056104
    Change-Id: Ib0dbc503ec31422098a8e39a0903183a67614106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935498
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#719684}

--- ash/assistant/ui/proactive_suggestions_rich_view.cc
+++ ash/assistant/ui/proactive_suggestions_rich_view.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/assistant/ui/assistant_view_delegate.h"
 #include "base/base64.h"
+#include "chromeos/services/assistant/public/features.h"
 #include "ui/aura/window.h"
 #include "ui/views/layout/fill_layout.h"
+#include "ui/views/widget/widget.h"
 
 namespace ash {
 
@@ -125,6 +127,29 @@ void ProactiveSuggestionsRichView::InitLayout() {
   contents_->Navigate(GURL(kDataUriPrefix + encoded_html));
 }
 
+void ProactiveSuggestionsRichView::ShowWhenReady() {
+  // If no children have yet been added to the layout, the embedded web contents
+  // has not yet finished loading. In this case, we'll set a flag and delay
+  // showing until loading stops to avoid UI jank.
+  if (children().empty()) {
+    show_when_ready_ = true;
+    return;
+  }
+  // Otherwise it's fine to go ahead w/ showing the widget immediately.
+  GetWidget()->ShowInactive();
+  show_when_ready_ = false;
+}
+
+void ProactiveSuggestionsRichView::Hide() {
+  show_when_ready_ = false;
+  ProactiveSuggestionsView::Hide();
+}
+
+void ProactiveSuggestionsRichView::Close() {
+  show_when_ready_ = false;
+  ProactiveSuggestionsView::Close();
+}
+
 void ProactiveSuggestionsRichView::DidAutoResizeView(
     const gfx::Size& new_size) {
   contents_->GetView()->view()->SetPreferredSize(new_size);
@@ -135,10 +160,17 @@ void ProactiveSuggestionsRichView::DidStopLoading() {
   AddChildView(contents_->GetView()->view());
   PreferredSizeChanged();
 
-  // TODO(dmblack): Parameterize corner radius.
-  constexpr int kCornerRadiusDip = 16;
+  // Once the view for the embedded web contents has been fully initialized,
+  // it's safe to set our desired corner radius.
   contents_->GetView()->native_view()->layer()->SetRoundedCornerRadius(
-      gfx::RoundedCornersF(kCornerRadiusDip));
+      gfx::RoundedCornersF(
+          chromeos::assistant::features::
+              GetProactiveSuggestionsRichEntryPointCornerRadius()));
+
+  // This view should now be fully initialized, so it's safe to show without
+  // risk of introducing UI jank if we so desire.
+  if (show_when_ready_)
+    ShowWhenReady();
 }
 
 void ProactiveSuggestionsRichView::DidSuppressNavigation(
--- chromeos/services/assistant/public/features.h
+++ chromeos/services/assistant/public/features.h
@@ -90,6 +90,9 @@ COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
 int GetProactiveSuggestionsMaxWidth();
 
 COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
+int GetProactiveSuggestionsRichEntryPointCornerRadius();
+
+COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
 std::string GetProactiveSuggestionsServerExperimentIds();
 
 COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
