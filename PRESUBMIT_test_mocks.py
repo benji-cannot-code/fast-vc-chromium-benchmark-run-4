6ddcbdec1b0006f253e95857cc4692ffa6fdca65
Dan Beam <dbeam@chromium.org>
2019-11-27 01:17:34 +0000

    Remove --color from ESLint output when running in presubmit
    Fixed: 862792
    Change-Id: I250e91a1db5f8df71fff33c17c7a04a9fd6c8739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937584
    Commit-Queue: Dan Beam <dbeam@chromium.org>
    Auto-Submit: Dan Beam <dbeam@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719450}

--- PRESUBMIT_test_mocks.py
+++ PRESUBMIT_test_mocks.py
@@ -72,6 +72,7 @@ class MockInputApi(object):
     self.python_executable = sys.executable
     self.platform = sys.platform
     self.subprocess = subprocess
+    self.sys = sys
     self.files = []
     self.is_committing = False
     self.change = MockChange([])
