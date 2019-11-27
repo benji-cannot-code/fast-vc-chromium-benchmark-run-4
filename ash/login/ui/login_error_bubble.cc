f028a179b425fbe83e5aa2d844b6c86310fdbc4a
Thomas Tellier <tellier@google.com>
2019-11-27 18:29:12 +0000

    Access to password error text on lock screen with ChromeVox
    Bug: 884764
    Change-Id: Icb86058dfd67b8ae5e16c97835e89bca903e1430
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926771
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#719668}

--- ash/login/ui/login_error_bubble.cc
+++ ash/login/ui/login_error_bubble.cc
@@ -59,6 +59,10 @@ void LoginErrorBubble::SetContent(views::View* content) {
   AddChildView(content_);
 }
 
+void LoginErrorBubble::SetAccessibleName(const base::string16& name) {
+  accessible_name_ = name;
+}
+
 bool LoginErrorBubble::IsPersistent() const {
   return is_persistent_;
 }
@@ -82,7 +86,8 @@ const char* LoginErrorBubble::GetClassName() const {
 }
 
 void LoginErrorBubble::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  node_data->role = ax::mojom::Role::kTooltip;
+  node_data->role = ax::mojom::Role::kAlertDialog;
+  node_data->SetName(accessible_name_);
 }
 
 }  // namespace ash
--- ash/login/ui/login_error_bubble.h
+++ ash/login/ui/login_error_bubble.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/login/ui/login_base_bubble_view.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/views/view.h"
 
 namespace ash {
@@ -21,6 +22,11 @@ class ASH_EXPORT LoginErrorBubble : public LoginBaseBubbleView {
   ~LoginErrorBubble() override;
 
   void SetContent(views::View* content);
+  // We set an accessible name when content is not accessible. This happens if
+  // content is a container (e.g. a text and a "learn more" button). In such a
+  // case, it will have multiple subviews but only one which needs to be read
+  // on bubble show – when the alert event occurs.
+  void SetAccessibleName(const base::string16& name);
 
   // LoginBaseBubbleView:
   bool IsPersistent() const override;
@@ -34,6 +40,9 @@ class ASH_EXPORT LoginErrorBubble : public LoginBaseBubbleView {
  private:
   views::View* content_ = nullptr;
   bool is_persistent_;
+
+  // Accessibility data.
+  base::string16 accessible_name_;
 
   DISALLOW_COPY_AND_ASSIGN(LoginErrorBubble);
 };
