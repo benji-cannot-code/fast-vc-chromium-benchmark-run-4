9fb8a823218e9bc76de9866ee8c23000cceee988
Michael Spang <spang@chromium.org>
2019-11-27 18:44:08 +0000

    Add xkbcommon to instrumented_libraries
    This adds a new debian build method, since xkbcommon has switched to
    meson. This method has several advantages: it is agnostic to underlying
    build system, doesn't require determining which configure options to set,
    and automatically detects missing build dependencies.
    
    Bug: 1027853
    
    Change-Id: I4faaaa91be8ed032b2cf15d29a6bbca008c9db36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1938837
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719677}

---
+++ third_party/instrumented_libraries/scripts/pre-build/xkbcommon.sh
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script does some preparations before build of instrumented xkbcommon.
+
+# Do not warn about undefined sanitizer symbols in object files.
+sed -i "s/\(-Wl,--no-undefined\|-Wl,-z,defs\)//g" ./Makefile.am
+
+# Do not warn about uninstalled documentation.
+sed -i "s/--fail-missing//g" ./debian/rules
+
+# Do not warn about extra msan symbols.
+sed -i "s/dh_makeshlibs -- -c4/dh_makeshlibs/g" ./debian/rules
