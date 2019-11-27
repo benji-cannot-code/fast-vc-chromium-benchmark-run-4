eaba7caf2a8dfb8ec114e9e113a1b06b45a45d2b
Kelvin Jiang <kelvinjiang@chromium.org>
2019-11-27 00:53:01 +0000

    [DNR] Refactor ActionTracker to use a struct as a key
    Bug: 983761
    Change-Id: Ic3dd22708d8d20ce2a1cb9a1228835a81681f2ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935718
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719442}

--- extensions/browser/api/declarative_net_request/action_tracker.cc
+++ extensions/browser/api/declarative_net_request/action_tracker.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/api/declarative_net_request/action_tracker.h"
 
+#include <tuple>
+#include <utility>
+
 #include "base/stl_util.h"
 #include "base/values.h"
 #include "extensions/browser/api/declarative_net_request/request_action.h"
@@ -48,9 +51,9 @@ void ActionTracker::OnRuleMatched(const RequestAction& request_action,
     return;
 
   const ExtensionId& extension_id = request_action.extension_id;
-  auto key = std::make_pair(extension_id, tab_id);
-  int action_count = ++actions_matched_[key];
+  ExtensionTabIdKey key(extension_id, tab_id);
 
+  size_t action_count = ++actions_matched_[std::move(key)].action_count;
   if (extension_prefs_->GetDNRUseActionCountAsBadgeText(extension_id)) {
     DCHECK(ExtensionsAPIClient::Get());
     ExtensionsAPIClient::Get()->UpdateActionCount(
@@ -63,21 +66,23 @@ void ActionTracker::OnPreferenceEnabled(const ExtensionId& extension_id) const {
   DCHECK(extension_prefs_->GetDNRUseActionCountAsBadgeText(extension_id));
 
   for (auto it = actions_matched_.begin(); it != actions_matched_.end(); ++it) {
-    if (it->first.first != extension_id)
+    const ExtensionTabIdKey& key = it->first;
+    const TrackedInfo& value = it->second;
+
+    if (key.extension_id != extension_id)
       continue;
 
-    int tab_id = it->first.second;
-    int action_count = it->second;
     ExtensionsAPIClient::Get()->UpdateActionCount(
-        browser_context_, extension_id, tab_id, action_count,
+        browser_context_, extension_id, key.tab_id, value.action_count,
         true /* clear_badge_text */);
   }
 }
 
 void ActionTracker::ClearExtensionData(const ExtensionId& extension_id) {
   auto compare_by_extension_id =
-      [&extension_id](const std::pair<ExtensionTabKey, size_t>& it) {
-        return it.first.first == extension_id;
+      [&extension_id](
+          const std::pair<const ExtensionTabIdKey, TrackedInfo>& it) {
+        return it.first.extension_id == extension_id;
       };
 
   base::EraseIf(actions_matched_, compare_by_extension_id);
@@ -85,22 +90,24 @@ void ActionTracker::ClearExtensionData(const ExtensionId& extension_id) {
 
 void ActionTracker::ClearTabData(int tab_id) {
   auto compare_by_tab_id =
-      [&tab_id](const std::pair<ExtensionTabKey, size_t>& it) {
-        return it.first.second == tab_id;
+      [&tab_id](const std::pair<const ExtensionTabIdKey, TrackedInfo>& it) {
+        return it.first.tab_id == tab_id;
       };
 
   base::EraseIf(actions_matched_, compare_by_tab_id);
 }
 
 void ActionTracker::ResetActionCountForTab(int tab_id) {
+  DCHECK_NE(tab_id, extension_misc::kUnknownTabId);
+
   RulesMonitorService* rules_monitor_service =
       RulesMonitorService::Get(browser_context_);
 
   DCHECK(rules_monitor_service);
   for (const auto& extension_id :
        rules_monitor_service->extensions_with_rulesets()) {
-    auto key = std::make_pair(extension_id, tab_id);
-    actions_matched_[key] = 0;
+    ExtensionTabIdKey key(extension_id, tab_id);
+    actions_matched_[std::move(key)].action_count = 0;
 
     if (extension_prefs_->GetDNRUseActionCountAsBadgeText(extension_id)) {
       DCHECK(ExtensionsAPIClient::Get());
@@ -109,6 +116,22 @@ void ActionTracker::ResetActionCountForTab(int tab_id) {
           false /* clear_badge_text */);
     }
   }
+}
+
+ActionTracker::ExtensionTabIdKey::ExtensionTabIdKey(ExtensionId extension_id,
+                                                    int tab_id)
+    : extension_id(std::move(extension_id)), tab_id(tab_id) {}
+
+ActionTracker::ExtensionTabIdKey::ExtensionTabIdKey(
+    ActionTracker::ExtensionTabIdKey&&) = default;
+
+ActionTracker::ExtensionTabIdKey& ActionTracker::ExtensionTabIdKey::operator=(
+    ActionTracker::ExtensionTabIdKey&&) = default;
+
+bool ActionTracker::ExtensionTabIdKey::operator<(
+    const ExtensionTabIdKey& other) const {
+  return std::tie(tab_id, extension_id) <
+         std::tie(other.tab_id, other.extension_id);
 }
 
 void ActionTracker::DispatchOnRuleMatchedDebugIfNeeded(
