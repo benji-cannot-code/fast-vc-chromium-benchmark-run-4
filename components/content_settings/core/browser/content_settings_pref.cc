c3316b10bd017e91df8a0b4cec80f90465deb50f
Ayu Ishii <ayui@chromium.org>
2019-11-27 21:53:59 +0000

    Convert base::Bind and base::Callback in components/content_settings to Once/Repeating
    Bug: 1007678
    Change-Id: I3d0d9155f3a11f73fea58f711889898a77427d4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940879
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719768}

--- components/content_settings/core/browser/content_settings_pref.cc
+++ components/content_settings/core/browser/content_settings_pref.cc
@@ -93,9 +93,9 @@ ContentSettingsPref::ContentSettingsPref(
 
   ReadContentSettingsFromPref();
 
-  registrar_->Add(
-      pref_name_,
-      base::Bind(&ContentSettingsPref::OnPrefChanged, base::Unretained(this)));
+  registrar_->Add(pref_name_,
+                  base::BindRepeating(&ContentSettingsPref::OnPrefChanged,
+                                      base::Unretained(this)));
 }
 
 ContentSettingsPref::~ContentSettingsPref() {
