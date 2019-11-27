970af4e3a0d170cbd9bc26fbb93db6214948d25a
David Van Cleve <davidvc@chromium.org>
2019-11-27 18:50:53 +0000

    Enterprise policy for adapting to referrer change
    We're flipping a flag (ReducedReferrerGranularity) to change the default
    referrer policy to strict-origin-when-cross-origin. This CL adds a
    short-term enterprise policy to revert the change in case some
    administrators need some time to adjust to the change.
    
    The enterprise policy works by registering a handler that toggles a
    global in //content. (See dependent CL crrev.com/c/1904231.)
    
    It would have been possible with some indirection to add a pref that
    is in turn checked to update this value, but the pref would not have
    been used for any other purpose and, consequently, directly toggling
    the global requires less new code.
    
    In either case, toggling a runtime-modifiable global makes the policy
    dynamically updatable, but not per-profile.
    
    Bug: 1016541
    Change-Id: I0ee47a6d5a16d195dc39735516b1db300c9ca705
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872808
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719680}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "chrome/browser/net/disk_cache_dir_policy_handler.h"
+#include "chrome/browser/net/referrer_policy_policy_handler.h"
 #include "chrome/browser/net/secure_dns_policy_handler.h"
 #include "chrome/browser/policy/browsing_history_policy_handler.h"
 #include "chrome/browser/policy/developer_tools_policy_handler.h"
@@ -1313,6 +1314,7 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
   handlers->AddHandler(std::make_unique<HomepageLocationPolicyHandler>());
   handlers->AddHandler(std::make_unique<ProxyPolicyHandler>());
   handlers->AddHandler(std::make_unique<SecureDnsPolicyHandler>());
+  handlers->AddHandler(std::make_unique<ReferrerPolicyPolicyHandler>());
   handlers->AddHandler(std::make_unique<URLBlacklistPolicyHandler>());
 
   handlers->AddHandler(std::make_unique<SimpleSchemaValidatingPolicyHandler>(
