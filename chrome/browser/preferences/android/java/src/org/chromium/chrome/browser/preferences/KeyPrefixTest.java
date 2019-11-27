a3f47b2a51794c9901dd41eccfa6876a6507e22b
Tomasz Wiszkowski <ender@google.com>
2019-11-27 01:45:20 +0000

    Disable failing KeyPrefixTest cases.
    TBR=hnakashima@chromium.org
    
    Bug: 1028821
    Change-Id: I8d5b9d9a92f9ae4bd5d587fa2b4bcb4ad3ddc960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1938044
    Reviewed-by: Ender <ender@google.com>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#719464}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/KeyPrefixTest.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/KeyPrefixTest.java
@@ -12,6 +12,7 @@ import android.support.test.filters.SmallTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 
 /**
@@ -30,18 +31,21 @@ public class KeyPrefixTest {
     }
 
     @Test(expected = AssertionError.class)
+    @DisabledTest(message = "https://crbug.com/1028821")
     @SmallTest
     public void testError_missingPeriod() {
         new KeyPrefix("Chrome.Feature.KP");
     }
 
     @Test(expected = AssertionError.class)
+    @DisabledTest(message = "https://crbug.com/1028821")
     @SmallTest
     public void testError_missingStar() {
         new KeyPrefix("Chrome.Feature.KP.");
     }
 
     @Test(expected = AssertionError.class)
+    @DisabledTest(message = "https://crbug.com/1028821")
     @SmallTest
     public void testError_extraStar() {
         new KeyPrefix("Chrome.Feature.KP.**");
