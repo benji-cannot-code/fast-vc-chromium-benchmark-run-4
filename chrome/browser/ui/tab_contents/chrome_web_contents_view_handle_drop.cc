d491357aa3871c8adab58df1635876dc75139df7
Dominique Fauteux-Chapleau <domfc@chromium.org>
2019-11-27 18:10:12 +0000

    Add UMA metrics for Deep Scanning of DragAndDrop uploads.
    Bug: 1020267
    Change-Id: Id53a6fa1f371a5488cf15a2224fb370c338a60b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1934727
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719661}

--- chrome/browser/ui/tab_contents/chrome_web_contents_view_handle_drop.cc
+++ chrome/browser/ui/tab_contents/chrome_web_contents_view_handle_drop.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.h"
+#include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view_delegate.h"
 #include "content/public/common/drop_data.h"
@@ -66,5 +67,6 @@ void HandleOnPerformDrop(
 
   safe_browsing::DeepScanningDialogDelegate::ShowForWebContents(
       web_contents, std::move(data),
-      base::BindOnce(&DeepScanCompletionCallback, std::move(callback)));
+      base::BindOnce(&DeepScanCompletionCallback, std::move(callback)),
+      safe_browsing::DeepScanAccessPoint::DRAG_AND_DROP);
 }
