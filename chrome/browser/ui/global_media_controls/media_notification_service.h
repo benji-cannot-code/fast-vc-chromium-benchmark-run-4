3802713a918a2b434aaa5ec3f66250ac610f1890
Tommy Steimel <steimel@chromium.org>
2019-11-27 14:43:21 +0000

    GMC: Only count click to go back to tabs as interactions
    This CL updates the auto-dismiss logic to only count click-to-tabs as
    interactions instead of counting when the tab is focused by any means.
    The only reason we had it that way was to accommodate the
    "pre-refactor" world where it wasn't feasible to always detect the
    difference.
    
    Bug: 1026079
    Change-Id: I8806f64ade7019716413ef54c07693574f192dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937141
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719586}

--- chrome/browser/ui/global_media_controls/media_notification_service.h
+++ chrome/browser/ui/global_media_controls/media_notification_service.h
@@ -135,7 +135,6 @@ class MediaNotificationService
 
     // content::WebContentsObserver implementation.
     void WebContentsDestroyed() override;
-    void OnWebContentsFocused(content::RenderWidgetHost*) override;
 
     // media_session::mojom::MediaControllerObserver:
     void MediaSessionInfoChanged(
@@ -164,10 +163,10 @@ class MediaNotificationService
     // called if the value has not already been set.
     void set_dismiss_reason(GlobalMediaControlsDismissReason reason);
 
-   private:
     // Called when a session is interacted with (to reset |inactive_timer_|).
     void OnSessionInteractedWith();
 
+   private:
     void StartInactiveTimer();
 
     void OnInactiveTimerFired();
