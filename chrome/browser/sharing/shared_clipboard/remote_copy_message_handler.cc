b598f38c2d0aaa9b7aa43f5d98c69bef3488b439
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2019-11-27 13:48:07 +0000

    Clean up TODO for larger image limits.
    Also, use our own constant so that the limit does not get raised again
    without us knowing in the future.
    
    The in-memory limit was recently raised to 5MB:
    https://chromium-review.googlesource.com/c/chromium/src/+/1866194
    
    Bug: 1018138
    Change-Id: I2ce729203fe418d6eca8a0145d2df36be6c4bc92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1939707
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719569}

--- chrome/browser/sharing/shared_clipboard/remote_copy_message_handler.cc
+++ chrome/browser/sharing/shared_clipboard/remote_copy_message_handler.cc
@@ -41,6 +41,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 
 namespace {
+constexpr size_t kMaxImageDownloadSize = 5 * 1024 * 1024;
+
 // These values are the 2x of the preferred width and height defined in
 // message_center_constants.h, which are in dip.
 constexpr int kNotificationImageMaxWidthPx = 720;
@@ -166,13 +168,12 @@ void RemoteCopyMessageHandler::HandleImage(const std::string& image_url) {
   url_loader_ =
       network::SimpleURLLoader::Create(std::move(request), kTrafficAnnotation);
   timer_ = base::ElapsedTimer();
-  // TODO(mvanouwerkerk): Downloads > 1MB (kMaxBoundedStringDownloadSize).
   // Unretained(this) is safe here because |this| owns |url_loader_|.
   url_loader_->DownloadToString(
       profile_->GetURLLoaderFactory().get(),
       base::BindOnce(&RemoteCopyMessageHandler::OnURLLoadComplete,
                      base::Unretained(this)),
-      network::SimpleURLLoader::kMaxBoundedStringDownloadSize);
+      kMaxImageDownloadSize);
 }
 
 bool RemoteCopyMessageHandler::IsOriginAllowed(const GURL& image_url) {
