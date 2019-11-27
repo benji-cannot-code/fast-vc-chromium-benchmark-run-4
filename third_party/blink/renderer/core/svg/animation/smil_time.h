06894f04cdb02a488c5aa7ea8645281eff4a9ad3
Jeremy Apthorp <jeremya@chromium.org>
2019-11-27 14:13:26 +0000

    include <algorithm> for std::min in smil_time.h
    This caused build failures for Electron, which builds with MSSTL
    
    Change-Id: I5b21d27eb02523e975957befd50bf958a6fa6a89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1938028
    Auto-Submit: Jeremy Apthorp <jeremya@chromium.org>
    Commit-Queue: dsinclair <dsinclair@chromium.org>
    Reviewed-by: dsinclair <dsinclair@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719575}

--- third_party/blink/renderer/core/svg/animation/smil_time.h
+++ third_party/blink/renderer/core/svg/animation/smil_time.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_SVG_ANIMATION_SMIL_TIME_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_SVG_ANIMATION_SMIL_TIME_H_
 
+#include <algorithm>
 #include <ostream>
 
 #include "base/time/time.h"
