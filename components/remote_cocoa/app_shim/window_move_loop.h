e10e9a25da28938f347465bf12e10e154712b1c1
Ayu Ishii <ayui@chromium.org>
2019-11-27 00:52:58 +0000

    Convert base::Bind and base::Callback in components/remote_cocoa to Once/Repeating
    Bug: 1007723
    Change-Id: I833820b7f1175799464efb63684d680c7825ff95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937573
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719441}

--- components/remote_cocoa/app_shim/window_move_loop.h
+++ components/remote_cocoa/app_shim/window_move_loop.h
@@ -41,7 +41,7 @@ class CocoaWindowMoveLoop {
 
   // Pointer to a stack variable holding the exit reason.
   LoopExitReason* exit_reason_ref_ = nullptr;
-  base::Closure quit_closure_;
+  base::OnceClosure quit_closure_;
 
   // WeakPtrFactory for event monitor safety.
   base::WeakPtrFactory<CocoaWindowMoveLoop> weak_factory_;
