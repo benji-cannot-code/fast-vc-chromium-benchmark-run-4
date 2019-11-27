c6a44358376cc464c4102f450223e88ff619b943
Yutaka Hirano <yhirano@chromium.org>
2019-11-27 09:45:25 +0000

    Disable FramebustBlockBrowserTest.ManageButtonClicked due to flakiness
    Bug: 1028936
    Change-Id: Ia607a4e4a46cbfa2f1b7f3003a70677d779115a5
    Tbr: calamity@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1939084
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719537}

--- chrome/browser/ui/content_settings/framebust_block_browsertest.cc
+++ chrome/browser/ui/content_settings/framebust_block_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/ui/blocked_content/framebust_block_tab_helper.h"
 #include "chrome/browser/ui/blocked_content/url_list_manager.h"
@@ -211,7 +212,12 @@ IN_PROC_BROWSER_TEST_F(FramebustBlockBrowserTest, DisallowRadioButtonSelected) {
                 url, GURL(), ContentSettingsType::POPUPS, std::string()));
 }
 
-IN_PROC_BROWSER_TEST_F(FramebustBlockBrowserTest, ManageButtonClicked) {
+#if defined(OS_CHROMEOS) || defined(OS_LINUX)
+#define MAYBE_ManageButtonClicked DISABLED_ManageButtonClicked
+#else
+#define MAYBE_ManageButtonClicked ManageButtonClicked
+#endif
+IN_PROC_BROWSER_TEST_F(FramebustBlockBrowserTest, MAYBE_ManageButtonClicked) {
 #if defined(OS_CHROMEOS)
   web_app::WebAppProvider::Get(browser()->profile())
       ->system_web_app_manager()
