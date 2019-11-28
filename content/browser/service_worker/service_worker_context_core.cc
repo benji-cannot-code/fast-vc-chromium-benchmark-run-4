8b3e16fb97ff79a7506fbe115034570bcb9a2885
Hiroki Nakagawa <nhiroki@chromium.org>
2019-11-28 15:39:15 +0000

    ServiceWorker: Register only ServiceWorkerContainerHost for clients with ServiceWorkerContextCore
    This is a follow-up CL for the previous review comment:
    https://chromium-review.googlesource.com/c/chromium/src/+/1939157/5/content/browser/service_worker/service_worker_container_host.cc#70
    
    ServiceWorkerContextCore::container_host_by_uuid_ contains the pairs of service
    worker client UUID and a rawptr to ServiceWorkerContainerHost. This map is used
    only for service worker client APIs, so we don't have to put
    ServiceWorkerContainerHost for service worker execution contexts into the map.
    This CL removes the case.
    
    Also, the client UUID is specific to service worker clients, not for service
    worker execution contexts. This CL stops assigning the client UUID to
    ServiceWorkerContainerHost for service worker execution contexts.
    
    Bug: 931087
    Change-Id: Ia4cba861add33083c977845d60ec41c51db685e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1941532
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719988}

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -396,6 +396,7 @@ void ServiceWorkerContextCore::HasMainFrameProviderHost(
 void ServiceWorkerContextCore::RegisterContainerHostByClientID(
     const std::string& client_uuid,
     ServiceWorkerContainerHost* container_host) {
+  DCHECK(container_host->IsContainerForClient());
   DCHECK(!base::Contains(*container_host_by_uuid_, client_uuid));
   (*container_host_by_uuid_)[client_uuid] = container_host;
 }
@@ -412,6 +413,7 @@ ServiceWorkerContextCore::GetContainerHostByClientID(
   auto found = container_host_by_uuid_->find(client_uuid);
   if (found == container_host_by_uuid_->end())
     return nullptr;
+  DCHECK(found->second->IsContainerForClient());
   return found->second;
 }
 
