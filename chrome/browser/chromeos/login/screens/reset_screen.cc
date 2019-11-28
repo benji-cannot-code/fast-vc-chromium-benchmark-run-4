ca1a59245ed22710a29e8d24b1367e095918abf1
Igor <igorcov@chromium.org>
2019-11-28 14:10:12 +0000

    Allow powerwash by default, but forbid revert for enrolled devices
    The powerwash was forbidden for enrolled devices initially to fix an
    enrollment escape bug where users could revert to older Chrome OS
    version and apply the weaknesses from the old version.
    
    After this change lands, powerwash will be allowed for enrolled devices
    in the following cases:
    1 - Admin didn't set any value for the policy powerwash_allowed
    2 - Admin set the powerwash allowed policy value to true
    3 - Admin set the powerwash allowed policy value to false and the value
    of policy allow_tpm_firmware_update to true and the device needs to update
    the tpm firmware.
    
    In case of point 3, the powerwash is allowed only to explicitly update the
    tpm firmware.
    
    At the same time, the option to powerwash and revert remains disabled
    for enrolled devices.
    
    The CL from session manager to allow this on Chrome OS side:
    https://chromium-review.googlesource.com/c/chromiumos/platform2/+/1924409
    
    BUG=chromium:923296
    TEST=Check that ctrl+shift+R brings powerwash screen, and second press of
    the keys doesn't bring the "powerwash and revert" screen.
    
    Change-Id: I870a2c59c6989d041ad58dddf2c8ae40f20d94d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924193
    Reviewed-by: Igor <igorcov@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719966}

--- chrome/browser/chromeos/login/screens/reset_screen.cc
+++ chrome/browser/chromeos/login/screens/reset_screen.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
+#include "chrome/browser/browser_process_platform_part_chromeos.h"
 #include "chrome/browser/chromeos/login/enrollment/auto_enrollment_controller.h"
 #include "chrome/browser/chromeos/login/screens/error_screen.h"
 #include "chrome/browser/chromeos/login/screens/network_error.h"
@@ -118,8 +119,9 @@ void ResetScreen::CheckIfPowerwashAllowed(
   if (g_browser_process->platform_part()
           ->browser_policy_connector_chromeos()
           ->IsEnterpriseManaged()) {
-    // Admin can explicitly allow to powerwash. If the policy is not loaded yet,
-    // we consider by default that the device is not allowed to powerwash.
+    // Powerwash is allowed by default, if the policy is loaded. Admin can
+    // explicitly forbid powerwash. If the policy is not loaded yet, we
+    // consider by default that the device is not allowed to powerwash.
     bool is_powerwash_allowed = false;
     CrosSettings::Get()->GetBoolean(kDevicePowerwashAllowed,
                                     &is_powerwash_allowed);
@@ -433,13 +435,18 @@ void ResetScreen::UpdateStatusChanged(
 // Invoked from call to CanRollbackCheck upon completion of the DBus call.
 void ResetScreen::OnRollbackCheck(bool can_rollback) {
   VLOG(1) << "Callback from CanRollbackCheck, result " << can_rollback;
+  policy::BrowserPolicyConnectorChromeOS* connector =
+      g_browser_process->platform_part()->browser_policy_connector_chromeos();
+
+  const bool rollback_available =
+      !connector->IsEnterpriseManaged() && can_rollback;
   reset::DialogViewType dialog_type =
-      can_rollback ? reset::DIALOG_SHORTCUT_OFFERING_ROLLBACK_AVAILABLE
-                   : reset::DIALOG_SHORTCUT_OFFERING_ROLLBACK_UNAVAILABLE;
+      rollback_available ? reset::DIALOG_SHORTCUT_OFFERING_ROLLBACK_AVAILABLE
+                         : reset::DIALOG_SHORTCUT_OFFERING_ROLLBACK_UNAVAILABLE;
   UMA_HISTOGRAM_ENUMERATION("Reset.ChromeOS.PowerwashDialogShown", dialog_type,
                             reset::DIALOG_VIEW_TYPE_SIZE);
 
-  view_->SetIsRollbackAvailable(can_rollback);
+  view_->SetIsRollbackAvailable(rollback_available);
 }
 
 void ResetScreen::OnTPMFirmwareUpdateAvailableCheck(
