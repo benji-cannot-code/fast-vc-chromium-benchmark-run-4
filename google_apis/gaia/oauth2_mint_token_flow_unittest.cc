5ec191b370b31dddb8ff27b92319f5778e69be51
Alex Ilin <alexilin@chromium.org>
2019-11-28 09:18:03 +0000

    [gaia] Refactor base::Value usage in OAuth2MintTokenFlow
    This CL updates all deprecated methods of base::Value and
    base::JSONReader in OAuth2MintTokenFlow to use recommended APIs.
    
    Bug: 1026237
    Change-Id: I4e15970bc6215b74c9d4cea2d96f0923e1c7b878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940177
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719910}

--- google_apis/gaia/oauth2_mint_token_flow_unittest.cc
+++ google_apis/gaia/oauth2_mint_token_flow_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/json/json_reader.h"
+#include "base/optional.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "google_apis/gaia/google_service_auth_error.h"
@@ -175,12 +176,12 @@ class OAuth2MintTokenFlowTest : public testing::Test {
                                                   device_id, mode));
   }
 
-  // Helper to parse the given string to DictionaryValue.
-  static base::DictionaryValue* ParseJson(const std::string& str) {
-    std::unique_ptr<base::Value> value = base::JSONReader::ReadDeprecated(str);
-    EXPECT_TRUE(value.get());
-    EXPECT_EQ(base::Value::Type::DICTIONARY, value->type());
-    return static_cast<base::DictionaryValue*>(value.release());
+  // Helper to parse the given string to base::Value.
+  static std::unique_ptr<base::Value> ParseJson(const std::string& str) {
+    base::Optional<base::Value> value = base::JSONReader::Read(str);
+    EXPECT_TRUE(value.has_value());
+    EXPECT_TRUE(value->is_dict());
+    return std::make_unique<base::Value>(std::move(*value));
   }
 
   std::unique_ptr<MockMintTokenFlow> flow_;
@@ -250,8 +251,7 @@ TEST_F(OAuth2MintTokenFlowTest, CreateApiCallBody) {
 
 TEST_F(OAuth2MintTokenFlowTest, ParseMintTokenResponse) {
   {  // Access token missing.
-    std::unique_ptr<base::DictionaryValue> json(
-        ParseJson(kTokenResponseNoAccessToken));
+    std::unique_ptr<base::Value> json = ParseJson(kTokenResponseNoAccessToken);
     std::string at;
     int ttl;
     EXPECT_FALSE(OAuth2MintTokenFlow::ParseMintTokenResponse(json.get(), &at,
@@ -259,7 +259,7 @@ TEST_F(OAuth2MintTokenFlowTest, ParseMintTokenResponse) {
     EXPECT_TRUE(at.empty());
   }
   {  // All good.
-    std::unique_ptr<base::DictionaryValue> json(ParseJson(kValidTokenResponse));
+    std::unique_ptr<base::Value> json = ParseJson(kValidTokenResponse);
     std::string at;
     int ttl;
     EXPECT_TRUE(OAuth2MintTokenFlow::ParseMintTokenResponse(json.get(), &at,
@@ -271,24 +271,22 @@ TEST_F(OAuth2MintTokenFlowTest, ParseMintTokenResponse) {
 
 TEST_F(OAuth2MintTokenFlowTest, ParseIssueAdviceResponse) {
   {  // Description missing.
-    std::unique_ptr<base::DictionaryValue> json(
-        ParseJson(kIssueAdviceResponseNoDescription));
+    std::unique_ptr<base::Value> json =
+        ParseJson(kIssueAdviceResponseNoDescription);
     IssueAdviceInfo ia;
     EXPECT_FALSE(OAuth2MintTokenFlow::ParseIssueAdviceResponse(
         json.get(), &ia));
     EXPECT_TRUE(ia.empty());
   }
   {  // Detail missing.
-    std::unique_ptr<base::DictionaryValue> json(
-        ParseJson(kIssueAdviceResponseNoDetail));
+    std::unique_ptr<base::Value> json = ParseJson(kIssueAdviceResponseNoDetail);
     IssueAdviceInfo ia;
     EXPECT_FALSE(OAuth2MintTokenFlow::ParseIssueAdviceResponse(
         json.get(), &ia));
     EXPECT_TRUE(ia.empty());
   }
   {  // All good.
-    std::unique_ptr<base::DictionaryValue> json(
-        ParseJson(kValidIssueAdviceResponse));
+    std::unique_ptr<base::Value> json = ParseJson(kValidIssueAdviceResponse);
     IssueAdviceInfo ia;
     EXPECT_TRUE(OAuth2MintTokenFlow::ParseIssueAdviceResponse(
         json.get(), &ia));
