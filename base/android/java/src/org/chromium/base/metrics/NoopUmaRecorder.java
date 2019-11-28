846ba22ed52946d2be4b2cc9a5db749112266486
bttk <bttk@chromium.org>
2019-11-28 00:02:54 +0000

    Extract native bridge out of java UMA API
    A defined interface, and a hidden implementation in advance of changes
    of the behavior of metrics in Java.
    
    Bug: crbug.com/658300
    Change-Id: Iaa5a8e137526d7118315af6554e0e96d482972f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1915193
    Commit-Queue: bttk <bttk@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719815}

---
+++ base/android/java/src/org/chromium/base/metrics/NoopUmaRecorder.java
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.base.metrics;
+
+/**
+ * An empty implementation of {@link UmaRecorder} used by {@link
+ * RecordHistogram#setDisabledForTests(boolean)}.
+ */
+/* package */ final class NoopUmaRecorder implements UmaRecorder {
+    /* package */ NoopUmaRecorder() {}
+
+    @Override
+    public void recordBooleanHistogram(String name, boolean sample) {}
+
+    @Override
+    public void recordExponentialHistogram(
+            String name, int sample, int min, int max, int numBuckets) {}
+
+    @Override
+    public void recordLinearHistogram(String name, int sample, int min, int max, int numBuckets) {}
+
+    @Override
+    public void recordSparseHistogram(String name, int sample) {}
+}
