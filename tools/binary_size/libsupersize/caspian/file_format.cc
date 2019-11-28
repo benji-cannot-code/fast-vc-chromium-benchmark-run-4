10a30fb5c3023749bd48506fa3a6903ec35a786e
Jasper Chapman-Black <jaspercb@chromium.org>
2019-11-28 20:43:44 +0000

    SuperSize: Add save_diff command, .sizediff file format
    Change-Id: I5eb7006446ee403a9efe60227dba43399a135300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1941187
    Commit-Queue: Jasper Chapman-Black <jaspercb@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720059}

--- tools/binary_size/libsupersize/caspian/file_format.cc
+++ tools/binary_size/libsupersize/caspian/file_format.cc
@@ -26,8 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/binary_size/libsupersize/caspian/model.h"
 
 namespace {
-
-const char SERIALIZATION_VERSION[] = "Size File Format v1";
+const char kDiffHeader[] = "# Created by //tools/binary_size\nDIFF\n";
+const char kSerializationVersion[] = "Size File Format v1";
 
 int ReadLoneInt(char** rest) {
   char* token = strsep(rest, "\n");
@@ -207,9 +207,7 @@ void CalculatePadding(std::vector<Symbol>* raw_symbols) {
   }
 }
 
-void ParseSizeInfo(const char* gzipped,
-                   unsigned long len,
-                   ::caspian::SizeInfo* info) {
+void ParseSizeInfo(const char* gzipped, unsigned long len, SizeInfo* info) {
   // To avoid memory allocations, all the char* in our final Symbol set will
   // be pointers into the region originally pointed to by |decompressed_start|.
   // Calls to strsep() replace delimiter characters with null terminators.
@@ -221,7 +219,7 @@ void ParseSizeInfo(const char* gzipped,
 
   // Serialization version
   line = strsep(&rest, "\n");
-  if (std::strcmp(line, SERIALIZATION_VERSION)) {
+  if (std::strcmp(line, kSerializationVersion)) {
     std::cerr << "Serialization version: '" << line << "' not recognized."
               << std::endl;
     exit(1);
@@ -398,6 +396,34 @@ void ParseSizeInfo(const char* gzipped,
   CheckNoNonEmptyLinesRemain(rest);
 
   std::cout << "Parsed " << info->raw_symbols.size() << " symbols" << std::endl;
+}
+
+bool IsDiffSizeInfo(const char* file, unsigned long len) {
+  return !strncmp(file, kDiffHeader, 4);
+}
+
+void ParseDiffSizeInfo(char* file,
+                       unsigned long len,
+                       SizeInfo* before,
+                       SizeInfo* after) {
+  // Skip "DIFF" header.
+  char* rest = file;
+  rest += strlen(kDiffHeader);
+  Json::Value metadata;
+  ReadJsonBlob(&rest, &metadata);
+
+  if (metadata["version"].asInt() != 1) {
+    std::cerr << ".sizediff version mismatch, write some upgrade code. version="
+              << metadata["version"] << std::endl;
+    exit(1);
+  }
+
+  unsigned long header_len = rest - file;
+  unsigned long before_len = metadata["before_length"].asUInt();
+  unsigned long after_len = len - header_len - before_len;
+
+  ParseSizeInfo(rest, before_len, before);
+  ParseSizeInfo(rest + before_len, after_len, after);
 }
 
 }  // namespace caspian
