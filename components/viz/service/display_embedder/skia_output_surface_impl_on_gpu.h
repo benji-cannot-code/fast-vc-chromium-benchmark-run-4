37b92773df890850994930d13303efd710c2892b
Corentin Wallez <cwallez@chromium.org>
2019-11-28 15:18:31 +0000

    Revert "SkiaRenderer: Fix subtle use after free with WeakPtr"
    This reverts commit 2d5b633ac44119b42e6e936dfd7abf8ac64647c7.
    
    Reason for revert: Causes this to fire: [1028:3416:1127/151100.479:FATAL:skia_output_surface_impl.cc(770)] Check failed: (thread_checker_).CalledOnValidThread(). 
    
    Original change's description:
    > SkiaRenderer: Fix subtle use after free with WeakPtr
    > 
    > SkiaOutputSurfaceImpl owns the SkiaOutputSurfaceDependency. After Impl
    > goes out of scope, there is no more Dependency. We reference the
    > Dependency when we call CreateSafe{Once,Repeating}Callback for things
    > like presentation feedback callbacks. It is possible for presentation
    > feedback to occur after Impl is gone, in which case we can no longer use
    > Dependency.
    > 
    > Bug: 1020699
    > Change-Id: Iedb37a444358e4c543afbb8d8da5907d6b290b7b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1936889
    > Commit-Queue: Jonathan Backer <backer@chromium.org>
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#719744}
    
    TBR=backer@chromium.org,kylechar@chromium.org
    
    Change-Id: Ia092cdbd191648549381843080a01e53f53f1c31
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1020699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942342
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719980}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.h
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/output_surface_frame.h"
 #include "components/viz/service/display/overlay_processor.h"
 #include "components/viz/service/display_embedder/skia_output_device.h"
-#include "components/viz/service/display_embedder/skia_output_surface_dependency.h"
 #include "gpu/command_buffer/common/mailbox.h"
 #include "gpu/command_buffer/common/sync_token.h"
 #include "gpu/command_buffer/service/shared_context_state.h"
@@ -66,6 +65,7 @@ class DawnContextProvider;
 class DirectContextProvider;
 class GLRendererCopier;
 class ImageContextImpl;
+class SkiaOutputSurfaceDependency;
 class TextureDeleter;
 class VulkanContextProvider;
 
@@ -85,8 +85,6 @@ class SkiaOutputSurfaceImplOnGpu : public gpu::ImageTransportSurfaceDelegate,
       base::RepeatingCallback<void(const gfx::PresentationFeedback& feedback)>;
   using ContextLostCallback = base::OnceClosure;
 
-  // |gpu_vsync_callback| must be safe to call on any thread. The other
-  // callbacks will only be called via |deps->PostTaskToClientThread|.
   static std::unique_ptr<SkiaOutputSurfaceImplOnGpu> Create(
       SkiaOutputSurfaceDependency* deps,
       const RendererSettings& renderer_settings,
@@ -204,10 +202,6 @@ class SkiaOutputSurfaceImplOnGpu : public gpu::ImageTransportSurfaceDelegate,
   // gpu::DisplayContext implementation:
   void MarkContextLost() override;
 
-  void PostTaskToClientThread(base::OnceClosure closure) {
-    dependency_->PostTaskToClientThread(std::move(closure));
-  }
-
  private:
   class ScopedPromiseImageAccess;
 
@@ -255,10 +249,10 @@ class SkiaOutputSurfaceImplOnGpu : public gpu::ImageTransportSurfaceDelegate,
   // readback using GLRendererCopier.
   // TODO(samans): Remove |sequence_id| once readback always uses Skia.
   const gpu::SequenceId sequence_id_;
-  DidSwapBufferCompleteCallback did_swap_buffer_complete_callback_;
-  BufferPresentedCallback buffer_presented_callback_;
+  const DidSwapBufferCompleteCallback did_swap_buffer_complete_callback_;
+  const BufferPresentedCallback buffer_presented_callback_;
   ContextLostCallback context_lost_callback_;
-  GpuVSyncCallback gpu_vsync_callback_;
+  const GpuVSyncCallback gpu_vsync_callback_;
 
 #if defined(USE_OZONE)
   // This should outlive gl_surface_ and vulkan_surface_.
