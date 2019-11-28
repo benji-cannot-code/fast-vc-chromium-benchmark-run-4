b3f0d5c69da728387346a0c98815c64b1fa5db5a
Peter Marshall <petermarshall@chromium.org>
2019-11-28 09:51:11 +0000

    DevTools: Fix a potentially flaky test
    Awaiting onceAttachedToTarget() directly after the evaluate() call
    which creates the new worker is not safe. The event could be fired
    after the `await evaluate()` is finished but before the
    AttachedToTarget handler is registered.
    
    Listening for the attached event before triggering the creation
    of the worker is the only safe way to do this.
    
    Change-Id: I2e9374d42da5a98b18f3c643b9259818620bb2b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940245
    Auto-Submit: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719920}

--- third_party/blink/web_tests/http/tests/inspector-protocol/emulation/emulation-user-agent-override.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/emulation/emulation-user-agent-override.js
@@ -13,14 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Worker Accept Language
   await dp.Target.setAutoAttach({autoAttach: true, waitForDebuggerOnStart: false, flatten: true});
+  const attachedPromise = dp.Target.onceAttachedToTarget();
   await session.evaluate(`var w = new Worker('${testRunner.url('resources/worker.js')}')`);
-  let event = await dp.Target.onceAttachedToTarget();
+  const event = await attachedPromise;
   const workerSession = session.createChild(event.params.sessionId);
   await workerSession.protocol.Emulation.setUserAgentOverride({userAgent: '', acceptLanguage: 'ko, en, zh-CN, zh-HK, en-US, en-GB'});
+
+  const detachedPromise = dp.Target.onceDetachedFromTarget()
   testRunner.log('workerNavigator.language == ' + await session.evaluateAsync(`
       w.postMessage('ping!');
       new Promise(resolve => w.onmessage = e => resolve(e.data.language))`));
-  await dp.Target.onceDetachedFromTarget();
+  await detachedPromise;
 
   await printHeader('Accept-Language');
 
