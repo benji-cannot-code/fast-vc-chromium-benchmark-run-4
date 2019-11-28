d5e82b5f7bac6b01009e9222fc8d9194ec1982d6
kylechar <kylechar@chromium.org>
2019-11-28 19:38:58 +0000

    Cleanup for SkiaOutputSurface and related code
    1. Fix class header ordering violations.
    2. Use empty callbacks instead of optional callbacks.
    3. Pass callbacks by value when transferring ownership.
    4. Fix NOTIMPLEMENTED() for SkiaOutputSurfaceImplOngpu swap buffers and
       presentation feedback. This is handled elsewhere already.
    
    Bug: none
    Change-Id: Iedd18969fb674d48713ef406029e942b7afd947c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942969
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720046}

--- components/viz/service/display_embedder/skia_output_surface_impl.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl.cc
@@ -163,11 +163,11 @@ SkiaOutputSurfaceImpl::~SkiaOutputSurfaceImpl() {
         event->Signal();
       },
       std::move(render_pass_image_contexts), std::move(impl_on_gpu_), &event);
-  ScheduleGpuTask(std::move(callback), std::vector<gpu::SyncToken>());
+  ScheduleGpuTask(std::move(callback), {});
   event.Wait();
 
   // Delete task sequence.
-  task_sequence_ = nullptr;
+  task_sequence_.reset();
 }
 
 gpu::SurfaceHandle SkiaOutputSurfaceImpl::GetSurfaceHandle() const {
@@ -198,8 +198,7 @@ void SkiaOutputSurfaceImpl::EnsureBackbuffer() {
   // SkiaOutputSurfaceImpl::dtor. So it is safe to use base::Unretained.
   auto callback = base::BindOnce(&SkiaOutputSurfaceImplOnGpu::EnsureBackbuffer,
                                  base::Unretained(impl_on_gpu_.get()));
-  task_sequence_->ScheduleOrRetainTask(std::move(callback),
-                                       std::vector<gpu::SyncToken>());
+  task_sequence_->ScheduleOrRetainTask(std::move(callback), {});
 }
 
 void SkiaOutputSurfaceImpl::DiscardBackbuffer() {
@@ -208,8 +207,7 @@ void SkiaOutputSurfaceImpl::DiscardBackbuffer() {
   // SkiaOutputSurfaceImpl::dtor. So it is safe to use base::Unretained.
   auto callback = base::BindOnce(&SkiaOutputSurfaceImplOnGpu::DiscardBackbuffer,
                                  base::Unretained(impl_on_gpu_.get()));
-  task_sequence_->ScheduleOrRetainTask(std::move(callback),
-                                       std::vector<gpu::SyncToken>());
+  task_sequence_->ScheduleOrRetainTask(std::move(callback), {});
 }
 
 void SkiaOutputSurfaceImpl::RecreateRootRecorder() {
@@ -409,8 +407,7 @@ void SkiaOutputSurfaceImpl::ReleaseImageContexts(
   auto callback = base::BindOnce(
       &SkiaOutputSurfaceImplOnGpu::ReleaseImageContexts,
       base::Unretained(impl_on_gpu_.get()), std::move(image_contexts));
-  task_sequence_->ScheduleOrRetainTask(std::move(callback),
-                                       std::vector<gpu::SyncToken>());
+  task_sequence_->ScheduleOrRetainTask(std::move(callback), {});
 }
 
 std::unique_ptr<ExternalUseClient::ImageContext>
@@ -462,7 +459,7 @@ void SkiaOutputSurfaceImpl::ScheduleOutputSurfaceAsOverlay(
   auto callback = base::BindOnce(
       &SkiaOutputSurfaceImplOnGpu::ScheduleOutputSurfaceAsOverlay,
       base::Unretained(impl_on_gpu_.get()), std::move(output_surface_plane));
-  ScheduleGpuTask(std::move(callback), std::vector<gpu::SyncToken>());
+  ScheduleGpuTask(std::move(callback), {});
 }
 
 SkCanvas* SkiaOutputSurfaceImpl::BeginPaintRenderPass(
@@ -596,7 +593,7 @@ void SkiaOutputSurfaceImpl::RemoveRenderPassResource(
     auto callback = base::BindOnce(
         &SkiaOutputSurfaceImplOnGpu::RemoveRenderPassResource,
         base::Unretained(impl_on_gpu_.get()), std::move(image_contexts));
-    ScheduleGpuTask(std::move(callback), std::vector<gpu::SyncToken>());
+    ScheduleGpuTask(std::move(callback), {});
   }
 }
 
@@ -641,7 +638,7 @@ void SkiaOutputSurfaceImpl::SetCapabilitiesForTesting(
   auto callback =
       base::BindOnce(&SkiaOutputSurfaceImplOnGpu::SetCapabilitiesForTesting,
                      base::Unretained(impl_on_gpu_.get()), capabilities_);
-  ScheduleGpuTask(std::move(callback), std::vector<gpu::SyncToken>());
+  ScheduleGpuTask(std::move(callback), {});
 }
 
 void SkiaOutputSurfaceImpl::SendOverlayPromotionNotification(
@@ -681,7 +678,7 @@ bool SkiaOutputSurfaceImpl::Initialize() {
   bool result = false;
   auto callback = base::BindOnce(&SkiaOutputSurfaceImpl::InitializeOnGpuThread,
                                  base::Unretained(this), &event, &result);
-  ScheduleGpuTask(std::move(callback), std::vector<gpu::SyncToken>());
+  ScheduleGpuTask(std::move(callback), {});
   event.Wait();
   return result;
 }
