c13a43ea1f8de12cd0045d3959b679a4f2fead9d
Eric Willigers <ericwilligers@chromium.org>
2019-11-28 03:14:07 +0000

    Desktop PWAs: Record web app manifest display load when launching
    When a desktop PWA is launched in a window, we now record the
    display mode that was requested in the manifest.
    
    
    
    
    Bug: 1939127
    Change-Id: I98b77e5eebd355add3cb38a97924fbcd457b99fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1939082
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#719853}

--- chrome/browser/apps/launch_service/extension_app_launch_manager.cc
+++ chrome/browser/apps/launch_service/extension_app_launch_manager.cc
@@ -7,10 +7,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "chrome/browser/apps/platform_apps/platform_app_launch.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
+#include "chrome/browser/ui/web_applications/web_app_launch_manager.h"
 #include "chrome/common/chrome_features.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/common/extension.h"
 
 namespace apps {
+
+namespace {
+
+void RecordBookmarkLaunch(Profile* profile, const std::string& app_id) {
+  const extensions::Extension* extension =
+      extensions::ExtensionRegistry::Get(profile)->GetInstalledExtension(
+          app_id);
+  if (extension && extension->from_bookmark())
+    web_app::RecordAppWindowLaunch(profile, app_id);
+}
+
+}  // namespace
 
 ExtensionAppLaunchManager::ExtensionAppLaunchManager(Profile* profile)
     : LaunchManager(profile) {}
@@ -19,6 +35,8 @@ ExtensionAppLaunchManager::~ExtensionAppLaunchManager() = default;
 
 content::WebContents* ExtensionAppLaunchManager::OpenApplication(
     const AppLaunchParams& params) {
+  if (params.container == apps::mojom::LaunchContainer::kLaunchContainerWindow)
+    RecordBookmarkLaunch(profile(), params.app_id);
   return ::OpenApplication(profile(), params);
 }
 
@@ -26,6 +44,8 @@ bool ExtensionAppLaunchManager::OpenApplicationWindow(
     const std::string& app_id,
     const base::CommandLine& command_line,
     const base::FilePath& current_directory) {
+  RecordBookmarkLaunch(profile(), app_id);
+
   return OpenExtensionApplicationWindow(profile(), app_id, command_line,
                                         current_directory);
 }
