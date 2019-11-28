35794528cdd56462d7c70f41bb9db93eafabe123
Noel Gordon <noel@chromium.org>
2019-11-28 22:29:37 +0000

    [directorytree] Expand nested folders and test horizontal scroll
    CL:1924341 limited the horizontal scroll of the directorytree to 0, to
    resolve issue 1025581, and needs a test of this behavior.
    
    Create a nested tree of test folders. Expand the directory tree to the
    last folder, verify the navigation, and that the directory tree is not
    horizontally scrolled (directorytree.scrollLeft should be 0). The test
    passes with CL:1924341 and fails before that CL (good).
    
    Tbr: anyone
    Test: browser_tests --gtest_filter="*TreeExpandHorizontal*"
    Bug: 1029019
    Change-Id: I609b06ddfc3d18cf24cf5b5c3e4e118c06989c35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942667
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720078}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -473,6 +473,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
     DirectoryTree, /* directory_tree.js */
     FilesAppBrowserTest,
     ::testing::Values(TestCase("directoryTreeHorizontalScroll"),
+                      TestCase("directoryTreeExpandHorizontalScroll"),
                       TestCase("directoryTreeVerticalScroll")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
