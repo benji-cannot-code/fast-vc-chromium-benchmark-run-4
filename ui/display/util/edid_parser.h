86030c5ea73a77f18c55f73d6179f5b4bbd5d3c1
Miguel Casas <mcasas@chromium.org>
2019-11-28 04:04:27 +0000

    ui/display: teach EdidParser about max, max_avg and min luminance
    This CL teaches EdidParser about the max, max_avg and min luminance
    values present in some EDID blobs, and extends the unittests
    accordingly.
    
    Bug: 1026688
    Change-Id: Ia7051ba97852b1aca16b08dbfe82bd8f43e8b14d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937660
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Cr-Commit-Position: refs/heads/master@{#719861}

--- ui/display/util/edid_parser.h
+++ ui/display/util/edid_parser.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/macros.h"
 #include "base/optional.h"
+#include "base/stl_util.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "ui/display/util/display_util_export.h"
 #include "ui/gfx/color_space.h"
@@ -27,6 +28,22 @@ namespace display {
 // a few utility postprocessings.
 class DISPLAY_UTIL_EXPORT EdidParser {
  public:
+  // Reflects CEA 861.G-2018, Sec.7.5.13, "HDR Static Metadata Data Block"
+  // A value of 0.0 in any of this fields means that it's not indicated.
+  struct Luminance {
+    // "Desired Content Max Luminance Data. This is the content’s absolute peak
+    // luminance (in cd/m2) (likely only in a small area of the screen) that the
+    // display prefers for optimal content rendering."
+    double max;
+    // "Desired Content Max Frame-average Luminance. This is the content’s max
+    // frame-average luminance (in cd/m2) that the display prefers for optimal
+    // content rendering."
+    double max_avg;
+    // "Desired Content Min Luminance. This is the minimum value of the content
+    // (in cd/m2) that the display prefers for optimal content rendering."
+    double min;
+  };
+
   explicit EdidParser(const std::vector<uint8_t>& edid_blob);
   ~EdidParser();
 
@@ -47,6 +64,9 @@ class DISPLAY_UTIL_EXPORT EdidParser {
   const base::flat_set<gfx::ColorSpace::TransferID>&
   supported_color_transfer_ids() const {
     return supported_color_transfer_ids_;
+  }
+  const Luminance* luminance() const {
+    return base::OptionalOrNullptr(luminance_);
   }
   // Returns a 32-bit identifier for this display |manufacturer_id_| and
   // |product_id_|.
@@ -84,6 +104,7 @@ class DISPLAY_UTIL_EXPORT EdidParser {
 
   base::flat_set<gfx::ColorSpace::PrimaryID> supported_color_primary_ids_;
   base::flat_set<gfx::ColorSpace::TransferID> supported_color_transfer_ids_;
+  base::Optional<Luminance> luminance_;
 
   DISALLOW_COPY_AND_ASSIGN(EdidParser);
 };
