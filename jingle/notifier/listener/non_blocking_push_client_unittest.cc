bf07431f1f5312f5d1f59c25bec03593103f9216
danakj <danakj@chromium.org>
2019-11-28 18:17:17 +0000

    Convert Callback to {Once,Repeating}Callback in //jingle.
    Use OnceCallback where possible, and use BindRepeating() where it is
    meant to be a RepeatingCallback.
    
    TBR=sergeyu@chromium.org
    
    Bug: 1007709
    Change-Id: I4408f77d876e5b72017892ebc2195dafabadfccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1943048
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720028}

--- jingle/notifier/listener/non_blocking_push_client_unittest.cc
+++ jingle/notifier/listener/non_blocking_push_client_unittest.cc
@@ -32,11 +32,10 @@ class NonBlockingPushClientTest : public testing::Test {
   ~NonBlockingPushClientTest() override {}
 
   void SetUp() override {
-    push_client_.reset(
-        new NonBlockingPushClient(
-            base::ThreadTaskRunnerHandle::Get(),
-            base::Bind(&NonBlockingPushClientTest::CreateFakePushClient,
-                       base::Unretained(this))));
+    push_client_ = std::make_unique<NonBlockingPushClient>(
+        base::ThreadTaskRunnerHandle::Get(),
+        base::BindOnce(&NonBlockingPushClientTest::CreateFakePushClient,
+                       base::Unretained(this)));
     push_client_->AddObserver(&fake_observer_);
     // Pump message loop to run CreateFakePushClient.
     base::RunLoop().RunUntilIdle();
@@ -55,10 +54,11 @@ class NonBlockingPushClientTest : public testing::Test {
   std::unique_ptr<PushClient> CreateFakePushClient() {
     if (fake_push_client_) {
       ADD_FAILURE();
-      return std::unique_ptr<PushClient>();
+      return nullptr;
     }
-    fake_push_client_ = new FakePushClient();
-    return std::unique_ptr<PushClient>(fake_push_client_);
+    auto client = std::make_unique<FakePushClient>();
+    fake_push_client_ = client.get();
+    return client;
   }
 
   base::test::SingleThreadTaskEnvironment task_environment_;
