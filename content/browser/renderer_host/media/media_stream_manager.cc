ea7734c08a7ef0e07e8c919a4a562bfb3ca4b4b6
henrika <henrika@chromium.org>
2019-11-29 16:11:12 +0000

    Extends native logs with details about audio devices
    Creates log sequences where we can track that audio input devices
    are opened and closed correctly and what parameters are used.
    
    Example (grep for AIDM):
    
    AIDM::Open({session_id=45CE96DFC9576CDABE2524A89B6E5ADE}, {device=[type: DEVICE_AUDIO_CAPTURE, id: default, group_id: input, name: Default, parameters: [format: 4, channel_layout: 3, channels: 2, sample_rate: 44100, frames_per_buffer: 441, effects: 0, mic_positions: ]]})
    AIDM::Opened({session_id=45CE96DFC9576CDABE2524A89B6E5ADE})
    AIDM::Close({session_id=45CE96DFC9576CDABE2524A89B6E5ADE})
    AIDM::Closed({session_id=45CE96DFC9576CDABE2524A89B6E5ADE})
    
    No-Try: True
    Bug: 1017219
    Change-Id: Iad328af521f2d95d1a0d1893c23d1bc79928b22e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1899872
    Auto-Submit: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720216}

--- content/browser/renderer_host/media/media_stream_manager.cc
+++ content/browser/renderer_host/media/media_stream_manager.cc
@@ -487,6 +487,7 @@ class MediaStreamManager::DeviceRequest {
 
 // static
 void MediaStreamManager::SendMessageToNativeLog(const std::string& message) {
+  DVLOG(1) << message;
   if (!BrowserThread::CurrentlyOn(BrowserThread::IO)) {
     base::PostTask(
         FROM_HERE, {BrowserThread::IO},
