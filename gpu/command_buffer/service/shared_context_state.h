6c79d461057745d764c235b3267127f9e0ab76c2
Kramer Ge <fangzhoug@chromium.org>
2019-11-29 18:19:01 +0000

    Add MemoryTracker in SkiaOutputSurfaceImplOnGpu
    Initialize SharedImageRepresentationFactory with non-null MemoryTracker from
    SkiaOutputSurfaceImplOnGpu. Said tracker is a sub tracker of
    SharedContextState::SharedImageMemoryTracker, which contributes to task manager
    GPU Memory column and histograms Memory.GPU.PeakMemoryUsage.
    
    TODO::The new memory tracker should also track SkiaRenderer GPU memory
    allocations that are not shared image creations.
    
    Bug: 899905
    Change-Id: Idaddbddeba17361e0f4ad258d5bd4ad4b4c1509f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940559
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720257}

--- gpu/command_buffer/service/shared_context_state.h
+++ gpu/command_buffer/service/shared_context_state.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/skia_utils.h"
 #include "gpu/command_buffer/service/gl_context_virtual_delegate.h"
+#include "gpu/command_buffer/service/memory_tracking.h"
 #include "gpu/config/gpu_preferences.h"
 #include "gpu/gpu_gles2_export.h"
 #include "third_party/skia/include/gpu/GrContext.h"
@@ -61,7 +62,8 @@ class GPU_GLES2_EXPORT SharedContextState
       GrContextType gr_context_type = GrContextType::kGL,
       viz::VulkanContextProvider* vulkan_context_provider = nullptr,
       viz::MetalContextProvider* metal_context_provider = nullptr,
-      viz::DawnContextProvider* dawn_context_provider = nullptr);
+      viz::DawnContextProvider* dawn_context_provider = nullptr,
+      gpu::MemoryTracker::Observer* peak_memory_monitor = nullptr);
 
   void InitializeGrContext(const GpuDriverBugWorkarounds& workarounds,
                            GrContextOptions::PersistentCache* cache,
@@ -90,6 +92,8 @@ class GPU_GLES2_EXPORT SharedContextState
 
   void PurgeMemory(
       base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level);
+
+  uint64_t GetMemoryUsage() const { return memory_tracker_.GetMemoryUsage(); }
 
   void PessimisticallyResetGrContext() const;
 
@@ -131,6 +135,7 @@ class GPU_GLES2_EXPORT SharedContextState
   bool support_vulkan_external_object() const {
     return support_vulkan_external_object_;
   }
+  gpu::MemoryTracker::Observer* memory_tracker() { return &memory_tracker_; }
 
   // base::trace_event::MemoryDumpProvider implementation.
   bool OnMemoryDump(const base::trace_event::MemoryDumpArgs& args,
@@ -149,6 +154,28 @@ class GPU_GLES2_EXPORT SharedContextState
 
  private:
   friend class base::RefCounted<SharedContextState>;
+
+  // Observer which is notified when SkiaOutputSurfaceImpl takes ownership of a
+  // shared image, and forward information to both histograms and task manager.
+  class GPU_GLES2_EXPORT MemoryTracker : public gpu::MemoryTracker::Observer {
+   public:
+    MemoryTracker(gpu::MemoryTracker::Observer* peak_memory_monitor);
+    MemoryTracker(MemoryTracker&) = delete;
+    MemoryTracker& operator=(MemoryTracker&) = delete;
+    ~MemoryTracker() override;
+
+    // gpu::MemoryTracker::Observer implementation:
+    void OnMemoryAllocatedChange(CommandBufferId id,
+                                 uint64_t old_size,
+                                 uint64_t new_size) override;
+
+    // Reports to GpuServiceImpl::GetVideoMemoryUsageStats()
+    uint64_t GetMemoryUsage() const;
+
+   private:
+    uint64_t size_ = 0;
+    gpu::MemoryTracker::Observer* const peak_memory_monitor_;
+  };
 
   ~SharedContextState() override;
 
@@ -176,6 +203,7 @@ class GPU_GLES2_EXPORT SharedContextState
   bool support_vulkan_external_object_ = false;
   base::OnceClosure context_lost_callback_;
   GrContextType gr_context_type_ = GrContextType::kGL;
+  MemoryTracker memory_tracker_;
   viz::VulkanContextProvider* const vk_context_provider_;
   viz::MetalContextProvider* const metal_context_provider_;
   viz::DawnContextProvider* const dawn_context_provider_;
