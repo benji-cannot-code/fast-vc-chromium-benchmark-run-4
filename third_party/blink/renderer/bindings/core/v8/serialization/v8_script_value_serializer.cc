d680999a41f1aee57b9da922c9b111e575138b63
Yutaka Hirano <yhirano@chromium.org>
2019-11-29 06:32:32 +0000

    Disallow non-origin-clean ImageBitmap serialization/transferring
    Intent-to-Deprecate-and-Remove:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/Z1XdYf6SjDU
    
    Bug: 1013087
    Change-Id: I34be2e59d6fa44834333e368dbd06a5e2cf1b876
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942187
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720130}

--- third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_serializer.cc
+++ third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_serializer.cc
@@ -272,8 +272,10 @@ bool V8ScriptValueSerializer::WriteDOMObject(ScriptWrappable* wrappable,
         execution_context->CountUse(
             mojom::WebFeature::kOriginCleanImageBitmapTransfer);
       } else {
-        execution_context->CountUse(
-            mojom::WebFeature::kNonOriginCleanImageBitmapTransfer);
+        exception_state.ThrowDOMException(
+            DOMExceptionCode::kDataCloneError,
+            "Non-origin-clean ImageBitmap cannot be transferred.");
+        return false;
       }
 
       DCHECK_LE(index, std::numeric_limits<uint32_t>::max());
@@ -287,8 +289,10 @@ bool V8ScriptValueSerializer::WriteDOMObject(ScriptWrappable* wrappable,
       execution_context->CountUse(
           mojom::WebFeature::kOriginCleanImageBitmapSerialization);
     } else {
-      execution_context->CountUse(
-          mojom::WebFeature::kNonOriginCleanImageBitmapSerialization);
+      exception_state.ThrowDOMException(
+          DOMExceptionCode::kDataCloneError,
+          "Non-origin-clean ImageBitmap cannot be cloned.");
+      return false;
     }
     WriteTag(kImageBitmapTag);
     SerializedColorParams color_params(image_bitmap->GetCanvasColorParams());
