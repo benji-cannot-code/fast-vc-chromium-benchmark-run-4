38cb85be4999eb12b82264437a224f7bf55969bd
Wanming Lin <wanming.lin@intel.com>
2019-11-29 04:01:46 +0000

    [webnfc] Add test for multi scan from the same NDEFReader object
    This CL adds a test for testing multiple scan() from the same
    NDEFReader object with new options should replace existing filters.
    
    Test fails at present as which has not been implemented yet.
    
    This CL also rename NDEFReader_options.https.html as
    NDEFReader_scan_filter.https.html because NDEFReader has no
    construct options any more, and this test file is mainly for
    scan filter testing.
    
    BUG=520391
    
    Change-Id: I4d19659c990c0ddc8dc19a5984c55be1ece48e9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1939327
    Commit-Queue: Wanming Lin <wanming.lin@intel.com>
    Reviewed-by: Leon Han <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#720112}

--- third_party/blink/web_tests/external/wpt/web-nfc/resources/nfc-helpers.js
+++ third_party/blink/web_tests/external/wpt/web-nfc/resources/nfc-helpers.js
@@ -169,7 +169,7 @@ function assertWebNDEFMessagesEqual(message, expectedMessage) {
   }
 }
 
-function testNDEFScanOptions(message, scanOptions, unmatchedScanOptions, desc) {
+function testMultiScanOptions(message, scanOptions, unmatchedScanOptions, desc) {
   nfc_test(async (t, mockNFC) => {
     const reader1 = new NDEFReader();
     const reader2 = new NDEFReader();
@@ -193,8 +193,7 @@ function testNDEFScanOptions(message, scanOptions, unmatchedScanOptions, desc) {
   }, desc);
 }
 
-function testReadingMultiMessages(
-    message, scanOptions, unmatchedMessage, desc) {
+function testMultiMessages(message, scanOptions, unmatchedMessage, desc) {
   nfc_test(async (t, mockNFC) => {
     const reader = new NDEFReader();
     const controller = new AbortController();
