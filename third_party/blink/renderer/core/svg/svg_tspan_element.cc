fb4eb2dad12f3e46aa87bac0523d6bb9991d3d86
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-11-29 19:15:33 +0000

    Use new downcast helper for blink::SVGTextPathElement
    This CL has two goals,
      1. Use To<SVGTextPathElement> as new downcast helper
      2. Use IsA<SVGTextPathElement>(element) in place of
         IsSVGTextPathElement(element)
    
    Bug: 891908
    Change-Id: Ib6f170541589d0be1f9cf5bcc4583cf8427d9ed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1936626
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Abhijeet | Igalia <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#720266}

--- third_party/blink/renderer/core/svg/svg_tspan_element.cc
+++ third_party/blink/renderer/core/svg/svg_tspan_element.cc
@@ -37,7 +37,8 @@ LayoutObject* SVGTSpanElement::CreateLayoutObject(const ComputedStyle&,
 bool SVGTSpanElement::LayoutObjectIsNeeded(const ComputedStyle& style) const {
   if (parentNode() &&
       (IsSVGAElement(*parentNode()) || IsSVGTextElement(*parentNode()) ||
-       IsSVGTextPathElement(*parentNode()) || IsSVGTSpanElement(*parentNode())))
+       IsA<SVGTextPathElement>(*parentNode()) ||
+       IsSVGTSpanElement(*parentNode())))
     return SVGElement::LayoutObjectIsNeeded(style);
 
   return false;
