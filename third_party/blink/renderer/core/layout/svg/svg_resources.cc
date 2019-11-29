7bd024542e645196b0d5274791f8a02fea15bd65
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-11-29 04:53:08 +0000

    Use new downcast helper for blink::SVGPatternElement
    This CL has goal to use To<SVGPatternElement> and
    DynamicTo<SVGPatternElement> as new downcast helper
    
    Bug: 891908
    Change-Id: Ibcc937ce3e849c5132d7e412246eb32268e16f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1936614
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet | Igalia <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#720118}

--- third_party/blink/renderer/core/layout/svg/svg_resources.cc
+++ third_party/blink/renderer/core/layout/svg/svg_resources.cc
@@ -233,7 +233,7 @@ std::unique_ptr<SVGResources> SVGResources::BuildResources(
     }
   }
 
-  if (auto* pattern = ToSVGPatternElementOrNull(element)) {
+  if (auto* pattern = DynamicTo<SVGPatternElement>(element)) {
     const SVGPatternElement* directly_referenced_pattern =
         pattern->ReferencedElement();
     if (directly_referenced_pattern) {
