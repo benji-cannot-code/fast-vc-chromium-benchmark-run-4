6bdf50c2567edb7d013fefb43783201231553fb9
Koji Ishii <kojii@chromium.org>
2019-11-29 09:30:36 +0000

    [FragmentItem] Move more of |NodeForHitTest| to |LayoutObject|
    Following r719562 and r719898 to remove the duplicated logic
    for |NodeForHitTest| in |NGPaintFragment|, this patch moves
    the rest of this logic in |NGPhysicalFragment| to
    |LayoutObject| so that it can be shared with |NGFragmentItem|.
    
    There should be no behavior changes.
    
    Bug: 982194
    Change-Id: Iae467845281a9d4fcf479b9b85bb2770132f6f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942591
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720148}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/ng/custom/layout_ng_custom.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_block_flow.h"
+#include "third_party/blink/renderer/core/layout/ng/list/layout_ng_list_item.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_node.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_unpositioned_float.h"
@@ -3343,6 +3344,11 @@ Node* LayoutObject::NodeForHitTest() const {
         if (Node* node = parent->GetNode())
           return node;
       }
+    } else if (const LayoutNGListItem* list_item =
+                   LayoutNGListItem::FromMarkerOrMarkerContent(*this)) {
+      // If this is a list marker, or is inside of a list marker, return the
+      // list item.
+      return list_item->GetNode();
     }
   }
 
