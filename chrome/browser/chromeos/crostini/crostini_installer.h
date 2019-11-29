6726cecadc07b0a5b82df43333717283566fae2f
Olya Kalitova <okalitova@chromium.org>
2019-11-29 15:23:32 +0000

    Remove Crostini after failed configuration during installation
    If during Crostini installation default Crostini container is failed to be
    configured, removes Crostini at all so that "Retry" button installs
    Crostini from the very beginning and Terminal app icon is not available
    in a Chrome OS launcher.
    
    Test: unit_tests --gtest_filter="Crostini*"
    Bug: 1022784
    Change-Id: I8d8c45cf6c39593657963786c117d384a6cecf9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1917202
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720199}

--- chrome/browser/chromeos/crostini/crostini_installer.h
+++ chrome/browser/chromeos/crostini/crostini_installer.h
@@ -126,6 +126,9 @@ class CrostiniInstaller : public KeyedService,
   void OnCrostiniRestartFinished(crostini::CrostiniResult result);
   void OnAvailableDiskSpace(int64_t bytes);
 
+  void OnCrostiniRemovedAfterConfigurationFailed(
+      crostini::CrostiniResult result);
+
   Profile* profile_;
 
   State state_ = State::IDLE;
