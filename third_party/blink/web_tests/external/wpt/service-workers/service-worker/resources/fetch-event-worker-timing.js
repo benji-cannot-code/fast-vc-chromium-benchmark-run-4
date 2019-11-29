8ad122cb82c35601bbc7b8b4ddfba8d60dec64f5
Yuta Kasai <yutakasai@google.com>
2019-11-29 07:46:15 +0000

    Service Worker: Add tentative wpt test for FetchEvent WorkerTiming in frame
    This patch adds tentative wpt test. Currently FetchEvent WorkerTiming is a
    tentative feature. In chromium, this feature hasn't implemented completely yet,
    so some tests will be failed.
    
    
    Explainer : https://github.com/wanderview/fetchevent-worker-timing/blob/master/explainer.md
    
    Bug: 900700
    Change-Id: I7a43989d8833ddd65c93106de67c4ebe6bd9ed52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1915644
    Commit-Queue: Yuta Kasai <yutakasai@google.com>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720134}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/fetch-event-worker-timing.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts("/resources/testharness.js");
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.indexOf('fallback') >= 0) {
+    event.addPerformanceEntry(
+      performance.mark("network-fallback mark 1",
+        { detail: { foo: 'foo' } }));
+    event.addPerformanceEntry(
+      performance.mark("network-fallback mark 2",
+        { detail: { bar: 'bar' } }));
+    event.addPerformanceEntry(performance.measure("network-fallback measure",
+      {
+        start: "network-fallback mark 1", end: "network-fallback mark 2",
+        detail: { baz: 'baz' }
+      }));
+    return;
+  } else if (event.request.url.indexOf('fetch-event') >= 0) {
+    event.respondWith((async () => {
+      event.addPerformanceEntry(performance.mark("fetch-event mark 1",
+        { detail: { foo: 'foo' } }));
+      const response = await fetch(event.request);
+
+      event.waitUntil(new Promise((resolve) => {
+        // Add performance entries after settling a promise for respondWith().
+        step_timeout(() => {
+          event.addPerformanceEntry(performance.mark("fetch-event mark 2",
+            { detail: { bar: 'bar' } }));
+          event.addPerformanceEntry(performance.measure("fetch-event measure",
+            {
+              start: "fetch-event mark 1", end: "fetch-event mark 2",
+              detail: { baz: 'baz' }
+            }));
+          resolve();
+        }, 100);
+      }));
+      return response;
+    })());
+  }
+});
