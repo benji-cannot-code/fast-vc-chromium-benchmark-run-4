d60b5c0e0bdb0cbb403c3da5a42c9d867cfc6a3c
Mikel Astiz <mastiz@chromium.org>
2019-11-29 09:09:26 +0000

    Remove old sync datatype histograms
    This affects:
    Sync.*AssociationTime
    Sync.*ConfigureFailure
    
    Both groups are rarely consumed these days and have competing metrics
    that are more interesting (although not identical).
    
    Bug: 995514
    Change-Id: I44f7b1e18db9eebe1d8172a2b2f2ce2fa75e3ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937370
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720146}

--- components/sync/driver/shared_change_processor.cc
+++ components/sync/driver/shared_change_processor.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/threading/sequenced_task_runner_handle.h"
-#include "components/sync/base/data_type_histogram.h"
 #include "components/sync/driver/generic_change_processor.h"
 #include "components/sync/driver/generic_change_processor_factory.h"
 #include "components/sync/driver/shared_change_processor_ref.h"
@@ -99,7 +98,6 @@ void SharedChangeProcessor::StartAssociation(
   {
     SyncDataList initial_sync_data;
 
-    base::TimeTicks start_time = base::TimeTicks::Now();
     SyncError error = GetAllSyncDataReturnError(type_, &initial_sync_data);
     if (error.IsSet()) {
       local_merge_result.set_error(error);
@@ -115,7 +113,6 @@ void SharedChangeProcessor::StartAssociation(
         type_, initial_sync_data, std::unique_ptr<SyncChangeProcessor>(
                                       new SharedChangeProcessorRef(this)),
         std::unique_ptr<SyncErrorFactory>(new SharedChangeProcessorRef(this)));
-    RecordAssociationTime(base::TimeTicks::Now() - start_time);
     if (local_merge_result.error().IsSet()) {
       start_done.Run(DataTypeController::ASSOCIATION_FAILED, local_merge_result,
                      syncer_merge_result);
@@ -296,13 +293,6 @@ SyncError SharedChangeProcessor::CreateAndUploadError(
   } else {
     return SyncError(location, SyncError::DATATYPE_ERROR, message, type_);
   }
-}
-
-void SharedChangeProcessor::RecordAssociationTime(base::TimeDelta time) {
-#define PER_DATA_TYPE_MACRO(type_str) \
-  UMA_HISTOGRAM_TIMES("Sync." type_str "AssociationTime", time);
-  SYNC_DATA_TYPE_HISTOGRAM(type_);
-#undef PER_DATA_TYPE_MACRO
 }
 
 void SharedChangeProcessor::StopLocalService() {
--- components/sync/model_impl/syncable_service_based_bridge.cc
+++ components/sync/model_impl/syncable_service_based_bridge.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/trace_event/memory_usage_estimator.h"
 #include "components/sync/base/client_tag_hash.h"
-#include "components/sync/base/data_type_histogram.h"
 #include "components/sync/model/mutable_data_batch.h"
 #include "components/sync/model/sync_change.h"
 #include "components/sync/model/sync_error_factory.h"
@@ -534,8 +533,6 @@ base::Optional<ModelError> SyncableServiceBasedBridge::StartSyncableService() {
   DCHECK(!syncable_service_started_);
   DCHECK(change_processor()->IsTrackingMetadata());
 
-  const base::TimeTicks start_time = base::TimeTicks::Now();
-
   // Sync enabled, so exercise MergeDataAndStartSyncing() immediately, since
   // this function is reached only if sync is starting already.
   SyncDataList initial_sync_data;
@@ -560,8 +557,6 @@ base::Optional<ModelError> SyncableServiceBasedBridge::StartSyncableService() {
               type_, initial_sync_data, std::move(local_change_processor),
               std::make_unique<SyncErrorFactoryImpl>(type_))
           .error());
-
-  RecordAssociationTime(base::TimeTicks::Now() - start_time);
 
   if (!merge_error) {
     syncable_service_started_ = true;
@@ -679,15 +674,6 @@ void SyncableServiceBasedBridge::ReportErrorIfSet(
   if (error) {
     change_processor()->ReportError(*error);
   }
-}
-
-void SyncableServiceBasedBridge::RecordAssociationTime(
-    base::TimeDelta time) const {
-// This mimics the implementation in SharedChangeProcessor.
-#define PER_DATA_TYPE_MACRO(type_str) \
-  UMA_HISTOGRAM_TIMES("Sync." type_str "AssociationTime", time);
-  SYNC_DATA_TYPE_HISTOGRAM(type_);
-#undef PER_DATA_TYPE_MACRO
 }
 
 }  // namespace syncer
