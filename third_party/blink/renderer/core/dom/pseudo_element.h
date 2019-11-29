3d20eda0dc0c6fe489a791fcd6fba5133fd0a16e
George Steel <gtsteel@google.com>
2019-11-29 20:10:06 +0000

    Update pseudo-element tag names to follow standard (::before etc).
    Currently this only effects devTools logs.
    This change is intended to be used by an upcoming patch to the animations
    code, where KeyframeEffect will use PseudoElementTagName.
    
    Bug: 981894
    Change-Id: Iddde38c575a1769bf00f53c82a056bf1a029c6d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928307
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: George Steel <gtsteel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720274}

--- third_party/blink/renderer/core/dom/pseudo_element.h
+++ third_party/blink/renderer/core/dom/pseudo_element.h
@@ -52,7 +52,7 @@ class CORE_EXPORT PseudoElement : public Element {
   scoped_refptr<ComputedStyle> LayoutStyleForDisplayContents(
       const ComputedStyle&);
 
-  static String PseudoElementNameForEvents(PseudoId);
+  static const AtomicString& PseudoElementNameForEvents(PseudoId);
 
   // Pseudo element are not allowed to be the inner node for hit testing. Find
   // the closest ancestor which is a real dom node.
@@ -76,7 +76,7 @@ class CORE_EXPORT PseudoElement : public Element {
   PseudoId pseudo_id_;
 };
 
-const QualifiedName& PseudoElementTagName();
+const QualifiedName& PseudoElementTagName(PseudoId);
 
 bool PseudoElementLayoutObjectIsNeeded(const ComputedStyle*);
 
