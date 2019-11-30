9563c641acac5372cbd431369ff2cf4df04ee065
Bettina <bdea@chromium.org>
2019-11-30 20:51:16 +0000

    Add experiment to creation of compromised credentials table
    Depend on an saved password with domain experiment flag
    for initializing the compromised credentials table.
    
    Bug: 1026006
    Change-Id: I56b45d911a21ef675a9af1b0bc618123f86919ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1943231
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720329}

--- components/password_manager/core/browser/login_database.cc
+++ components/password_manager/core/browser/login_database.cc
@@ -51,6 +51,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 #include "url/url_constants.h"
 
+#if !defined(OS_IOS)
+#include "components/safe_browsing/features.h"
+#endif
+
 using autofill::PasswordForm;
 
 namespace password_manager {
@@ -826,9 +830,14 @@ bool LoginDatabase::Init() {
     }
   }
 
-  // TODO(bdea): Create a more generic experiment for creation of compromise
-  // credentials because the current experiment is leak detection specific.
-  if (base::FeatureList::IsEnabled(password_manager::features::kLeakHistory)) {
+  bool password_protection_show_domains_for_saved_password_is_on = true;
+#if !defined(OS_IOS)
+  password_protection_show_domains_for_saved_password_is_on =
+      base::FeatureList::IsEnabled(
+          safe_browsing::kPasswordProtectionShowDomainsForSavedPasswords);
+#endif
+  if (base::FeatureList::IsEnabled(password_manager::features::kLeakHistory) ||
+      password_protection_show_domains_for_saved_password_is_on) {
     if (!compromised_credentials_table_.CreateTableIfNecessary()) {
       LogDatabaseInitError(INIT_COMPROMISED_CREDENTIALS_ERROR);
       LOG(ERROR) << "Unable to create the compromised credentials table.";
