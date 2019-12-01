9bac0a24368798a187423d31780533e42359deba
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-12-01 05:49:09 +0000

    Use new downcast helper for blink::SVGSymbolElement
    This CL has goal to use IsA<SVGSymbolElement>(element) in place of
    IsSVGSymbolElement(element)
    
    Bug: 891908
    Change-Id: Ib5a0ea84c79388daca85cd8af541ba42522f92cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1936623
    Commit-Queue: Abhijeet | Igalia <abhijeet@igalia.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720342}

--- third_party/blink/renderer/core/svg/svg_element.cc
+++ third_party/blink/renderer/core/svg/svg_element.cc
@@ -537,7 +537,7 @@ SVGElement* SVGElement::viewportElement() const {
   ContainerNode* n = ParentOrShadowHostNode();
   while (n) {
     if (IsA<SVGSVGElement>(*n) || IsSVGImageElement(*n) ||
-        IsSVGSymbolElement(*n))
+        IsA<SVGSymbolElement>(*n))
       return To<SVGElement>(n);
 
     n = n->ParentOrShadowHostNode();
