221769d2322639bfdc301a3412ea02226057d884
Jun Mukai <mukai@chromium.org>
2019-12-02 23:20:00 +0000

    Add 'sum' field to Histogram in autotest API
    Bug: 1029152
    Test: with Tast
    Change-Id: I935c6b4feaa6e80e5c738a838c1329c4deb59036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940309
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720706}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
@@ -1314,6 +1314,7 @@ AutotestPrivateGetHistogramFunction::GetHistogram(const std::string& name) {
   std::unique_ptr<base::HistogramSamples> samples =
       histogram->SnapshotSamples();
   api::autotest_private::Histogram result;
+  result.sum = samples->sum();
 
   for (std::unique_ptr<base::SampleCountIterator> it = samples->Iterator();
        !it->Done(); it->Next()) {
