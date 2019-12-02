98458029f6ab70e856478db5e4b34d38b42eda39
Mostyn Bramley-Moore <mostynb@vewd.com>
2019-12-02 19:59:39 +0000

    remove unused net/if.h include from network_interfaces_posix.cc
    net/if.h can conflict in confusing ways with linux/if.h (discovered in
    the process of some out-of-tree jumbo experiments), but thankfully we
    don't need the net/if.h include here.
    
    Change-Id: Ic9784c5d56cf896a56b8c1b989888607d09a3289
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1944847
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Mostyn Bramley-Moore <mostynb@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#720620}

--- net/base/network_interfaces_posix.cc
+++ net/base/network_interfaces_posix.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/network_interfaces_posix.h"
 
-#include <net/if.h>
 #include <netinet/in.h>
 #include <sys/types.h>
 
