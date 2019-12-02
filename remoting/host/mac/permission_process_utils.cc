6e328ae09b6b12f9e2f0d0f051372c5710698856
Lambros Lambrou <lambroslambrou@chromium.org>
2019-12-02 18:59:45 +0000

    [remoting] Redirect stdout to dev/null when doing permission checks.
    The native-messaging host does a permission-check by launching a child
    process. This CL ensures that the child is launched with stdin/stdout
    redirected to dev/null, since they are used by the native-messaging
    host to communicate with the website.
    
    The native-messaging host actually duplicates the file-descriptors for
    stdin/stdout, and uses these FDs instead of the original. It also
    unfortunately closes the original FDs. Whenever stdout is closed,
    LaunchProcess cannot launch any child process. This CL addresses this
    problem by manually redirecting stdout.
    
    A future CL will fix the native-messaging host so that it does not
    close FDs 0 and 1, which could potentially lead to unexpected
    behavior.
    
    Bug: 1029858
    Change-Id: I7d626e9e4817159ec955b66d2338c07a73c9549a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946827
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720584}

--- remoting/host/mac/permission_process_utils.cc
+++ remoting/host/mac/permission_process_utils.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/mac/permission_process_utils.h"
 
+#include <fcntl.h>
+
 #include "base/command_line.h"
+#include "base/files/scoped_file.h"
 #include "base/logging.h"
+#include "base/posix/eintr_wrapper.h"
 #include "base/process/launch.h"
 #include "base/process/process.h"
 #include "remoting/host/mac/constants_mac.h"
@@ -33,11 +37,21 @@ bool CheckHostPermission(base::FilePath exe_path, std::string command_switch) {
   base::CommandLine cmdLine(exe_path);
   cmdLine.AppendSwitch(command_switch);
 
+  // Redirect stdout to /dev/null, otherwise the child will inherit stdout from
+  // the parent, possibly corrupting the native-messaging channels.
+  // LaunchProcess() already redirects stdin to /dev/null, so no need to do it
+  // here.
+  base::ScopedFD null_fd(HANDLE_EINTR(open("/dev/null", O_WRONLY)));
+  if (!null_fd.is_valid()) {
+    PLOG(ERROR) << "Failed to open /dev/null";
+    return false;
+  }
   base::LaunchOptions options;
   options.disclaim_responsibility = true;
+  options.fds_to_remap.push_back(std::make_pair(null_fd.get(), STDOUT_FILENO));
   base::Process process = base::LaunchProcess(cmdLine, options);
   if (!process.IsValid()) {
-    LOG(ERROR) << "Unable to launch host process";
+    PLOG(ERROR) << "Unable to launch host process";
     return false;
   }
   int exit_code;
