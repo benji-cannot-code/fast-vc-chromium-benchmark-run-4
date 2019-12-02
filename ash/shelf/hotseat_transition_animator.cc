0d0a2e59279b5b0713c965c5e1c4314a7ecd9d12
Ana Salazar <anasalazar@chromium.org>
2019-12-02 19:58:58 +0000

    Cros: Reland "Add metrics for transitions in the hotseat"
    Report animation smoothness for the transition into the hotseat in the
    HomeLauncher.
    
    The original change caused use-of-uninitialized-value flaky tests. This
    is a possible fix for the issue by explicitly resetting the
    |animation_metrics_reporter_|
    
    Bug: 1022178, 1022177, 1028255
    Change-Id: I9abe9532e4f8b8cbc8c3700b21db48144f76f767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940562
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720619}

--- ash/shelf/hotseat_transition_animator.cc
+++ ash/shelf/hotseat_transition_animator.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
+#include "base/metrics/histogram_macros.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animator.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
@@ -17,8 +18,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class HotseatTransitionAnimator::TransitionAnimationMetricsReporter
+    : public ui::AnimationMetricsReporter {
+ public:
+  TransitionAnimationMetricsReporter() {}
+  ~TransitionAnimationMetricsReporter() override = default;
+  void animating_to_shown_hotseat(bool animating_to_shown_hotseat) {
+    animating_to_shown_hotseat_ = animating_to_shown_hotseat;
+  }
+  // ui::AnimationMetricsReporter:
+  void Report(int value) override {
+    if (animating_to_shown_hotseat_) {
+      UMA_HISTOGRAM_PERCENTAGE(
+          "Ash.HotseatTransition.AnimationSmoothness.TransitionToShownHotseat",
+          value);
+    } else {
+      UMA_HISTOGRAM_PERCENTAGE(
+          "Ash.HotseatTransition.AnimationSmoothness."
+          "TransitionFromShownHotseat",
+          value);
+    }
+  }
+
+ private:
+  // Whether the animation reported is transitioning state into a shown hotseat.
+  bool animating_to_shown_hotseat_ = false;
+};
+
 HotseatTransitionAnimator::HotseatTransitionAnimator(ShelfWidget* shelf_widget)
-    : shelf_widget_(shelf_widget) {
+    : shelf_widget_(shelf_widget),
+      animation_metrics_reporter_(
+          std::make_unique<TransitionAnimationMetricsReporter>()) {
   Shell::Get()->tablet_mode_controller()->AddObserver(this);
 }
 
@@ -26,6 +56,10 @@ HotseatTransitionAnimator::~HotseatTransitionAnimator() {
   StopObservingImplicitAnimations();
   if (Shell::Get()->tablet_mode_controller())
     Shell::Get()->tablet_mode_controller()->RemoveObserver(this);
+  // Reset the pointer for |animation_metrics_reporter| to delete the reference
+  // to the TransitionAnimationMetricsReporter object that might cause a
+  // use-of-uninitialized-value. (See crbug.com/1028255)
+  animation_metrics_reporter_.reset();
 }
 
 void HotseatTransitionAnimator::OnHotseatStateChanged(HotseatState old_state,
@@ -91,6 +125,8 @@ void HotseatTransitionAnimator::DoAnimation(HotseatState old_state,
   const int y_offset = starting_y - target_bounds.y();
   transform.Translate(0, y_offset);
   shelf_widget_->GetAnimatingBackground()->SetTransform(transform);
+  animation_metrics_reporter_->animating_to_shown_hotseat(
+      animating_to_shown_hotseat);
 
   {
     ui::ScopedLayerAnimationSettings shelf_bg_animation_setter(
@@ -100,6 +136,8 @@ void HotseatTransitionAnimator::DoAnimation(HotseatState old_state,
     shelf_bg_animation_setter.SetTweenType(gfx::Tween::EASE_OUT);
     shelf_bg_animation_setter.SetPreemptionStrategy(
         ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
+    shelf_bg_animation_setter.SetAnimationMetricsReporter(
+        animation_metrics_reporter_.get());
     animation_complete_callback_ = base::BindOnce(
         &HotseatTransitionAnimator::NotifyHotseatTransitionAnimationEnded,
         weak_ptr_factory_.GetWeakPtr(), old_state, new_state);
