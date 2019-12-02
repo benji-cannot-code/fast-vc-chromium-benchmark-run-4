66187ce7afa4df61cac573503cd9978f457836a1
Matt Falkenhagen <falken@chromium.org>
2019-12-02 05:25:58 +0000

    Convert to OnceCallback in content/public/browser_context.h
    Bug: 1007763
    Change-Id: I4d8ff83c57851b4dc5d470b046ef76510eb36253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942900
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720391}

--- content/browser/devtools/protocol/service_worker_handler.cc
+++ content/browser/devtools/protocol/service_worker_handler.cc
@@ -350,7 +350,7 @@ Response ServiceWorkerHandler::DeliverPushMessage(
   BrowserContext::DeliverPushMessage(
       browser_context_, GURL(origin), id, /* push_message_id= */ std::string(),
       std::move(payload),
-      base::BindRepeating([](blink::mojom::PushDeliveryStatus status) {}));
+      base::BindOnce([](blink::mojom::PushDeliveryStatus status) {}));
 
   return Response::OK();
 }
