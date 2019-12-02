4bdab095342de34bb262f0b75afcfee646028cd1
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-12-02 05:57:15 +0000

    Use new downcast helper for blink::SVGFilterElement
    This CL has two goals,
      1. Use To<SVGFilterElement> and DynamicTo<SVGFilterElement> as new
       downcast helper
      2. Use IsA<SVGFilterElement>(element) in place of
       IsSVGFilterElement(element)
    
    Bug: 891908
    Change-Id: I3504c8bfd82dc6938ac53f8bd6169fa0be338592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1936584
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet | Igalia <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#720396}

--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_filter.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_filter.cc
@@ -88,20 +88,20 @@ bool LayoutSVGResourceFilter::RemoveClientFromCache(SVGResourceClient& client) {
 
 FloatRect LayoutSVGResourceFilter::ResourceBoundingBox(
     const FloatRect& reference_box) const {
-  const auto* filter_element = ToSVGFilterElement(GetElement());
+  const auto* filter_element = To<SVGFilterElement>(GetElement());
   return SVGLengthContext::ResolveRectangle(filter_element, FilterUnits(),
                                             reference_box);
 }
 
 SVGUnitTypes::SVGUnitType LayoutSVGResourceFilter::FilterUnits() const {
-  return ToSVGFilterElement(GetElement())
+  return To<SVGFilterElement>(GetElement())
       ->filterUnits()
       ->CurrentValue()
       ->EnumValue();
 }
 
 SVGUnitTypes::SVGUnitType LayoutSVGResourceFilter::PrimitiveUnits() const {
-  return ToSVGFilterElement(GetElement())
+  return To<SVGFilterElement>(GetElement())
       ->primitiveUnits()
       ->CurrentValue()
       ->EnumValue();
@@ -127,8 +127,8 @@ void LayoutSVGResourceFilter::PrimitiveAttributeChanged(
       return;
     node_map->InvalidateDependentEffects(effect);
   }
-  if (LocalSVGResource* resource =
-          ToSVGFilterElement(GetElement())->AssociatedResource()) {
+  if (auto* resource =
+          To<SVGFilterElement>(GetElement())->AssociatedResource()) {
     resource->NotifyContentChanged(
         SVGResourceClient::kPaintInvalidation |
         SVGResourceClient::kSkipAncestorInvalidation);
