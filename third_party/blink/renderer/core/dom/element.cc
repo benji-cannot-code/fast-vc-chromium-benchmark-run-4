c0fe269f3a7b6e31982a5b6138e050b557050803
Abhijeet Kandalkar <abhijeet@igalia.com>
2019-12-02 17:34:06 +0000

    Use new downcast helper for blink::SVGStopElement
    This CL has goal to use IsA<SVGStopElement>(element) in place of
    IsSVGStopElement(element)
    
    Bug: 891908
    Change-Id: Iba08d8b45ee7440957528beccccbea6ad3d4378f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1936621
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Abhijeet | Igalia <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#720538}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -5102,7 +5102,7 @@ bool Element::ShouldStoreComputedStyle(const ComputedStyle& style) const {
   if (auto* svg_element = DynamicTo<SVGElement>(this)) {
     if (!svg_element->HasSVGParent())
       return false;
-    if (IsSVGStopElement(*this))
+    if (IsA<SVGStopElement>(*this))
       return true;
   }
   return style.Display() == EDisplay::kContents;
