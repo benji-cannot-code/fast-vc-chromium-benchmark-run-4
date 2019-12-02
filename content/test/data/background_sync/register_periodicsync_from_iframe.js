4140d8c15fce26e15c2218bc79cbb4941841ce10
Mugdha Lakhani <nator@chromium.org>
2019-12-02 15:06:16 +0000

    [Periodic Background Sync] Require main frame host for registration.
    Only allow Periodic Background Sync registration if the requesting
    origin has a main frame host present.
    
    Add browsertests to verify this behavior.
    
    Corresponding PR against the spec: https://github.com/WICG/BackgroundSync/pull/160
    
    Bug: 1029712
    Change-Id: I1142578edbde62f3f27ae95b9ca602855df3543b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942433
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720483}

--- content/test/data/background_sync/register_periodicsync_from_iframe.js
+++ content/test/data/background_sync/register_periodicsync_from_iframe.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2016 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 navigator.serviceWorker.register('empty_service_worker.js')
   .then(() => navigator.serviceWorker.ready)
-  .then(registration => registration.sync.register('foo'))
+  .then(registration => registration.periodicSync.register('foo', {}))
   .then(() => parent.postMessage('registration succeeded', '*'),
       () => parent.postMessage('registration failed', '*'));
