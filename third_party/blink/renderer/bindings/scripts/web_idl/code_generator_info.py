244391644808a6a96e0977dcfe5aa3763c5a3d5e
Yuki Shiino <yukishiino@chromium.org>
2019-12-02 09:51:22 +0000

    IDL compiler: Record is_partial/is_mixin in CodeGeneratorInfo
    The bindings generator needs to know whether a member is defined
    in a partial and/or mixin IDL fragment in order to make a Blink
    API call.  Thus, records such information in CodeGeneratorInfo.
    
    Bug: 839389
    Change-Id: I997e518c842e93d392846bf63480a9b8e0f7e1ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942894
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720422}

--- third_party/blink/renderer/bindings/scripts/web_idl/code_generator_info.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/code_generator_info.py
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # The list of attributes that CodeGeneratorInfo supports.  CodeGeneratorInfo's
 # attributes are auto-generated from this list because they're boilerplated.
 _CODE_GENERATOR_INFO_ATTRIBUTES = (
+    'defined_in_mixin',
+    'defined_in_partial',
     'property_implemented_as',
     'receiver_implemented_as',
 )
