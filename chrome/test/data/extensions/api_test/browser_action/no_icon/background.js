a145ca1e99962a88be06c5d0fd0799779249d61c
David Bertoni <dbertoni@chromium.org>
2019-12-02 22:34:08 +0000

    [Extensions] Refactor some browserAction tests for Service Workers.
    There are some unit tests that, with a little refactoring, will work
    for both legacy background page-based extensions and service worker-
    based ones. This CL is a start at refactoring and rewriting any tests
    that are appropriate for both.
    
    Bug: 1015136
    Change-Id: I65baa3f8fa23ad1b25c067da216224b46925ea35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1922923
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720693}

--- chrome/test/data/extensions/api_test/browser_action/no_icon/background.js
+++ chrome/test/data/extensions/api_test/browser_action/no_icon/background.js
@@ -3,24 +3,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Fill the "canvas" element with some color.
-var ctx = document.getElementById("canvas").getContext('2d');
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 42, 42);
+var canvas = new OffscreenCanvas(21, 21);
+var canvasHD = new OffscreenCanvas(42, 42);
 
-var canvas = document.getElementById("canvas").getContext('2d').getImageData(
-    0, 0, 21, 21);
-var canvasHD = document.getElementById("canvas").getContext('2d').getImageData(
-    0, 0, 42, 42);
+// Fill the canvases element with some color.
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = '#FF0000';
+ctx.fillRect(0, 0, 21, 21);
+
+var ctxHD = canvasHD.getContext('2d');
+ctxHD.fillStyle = '#FF0000';
+ctxHD.fillRect(0, 0, 42, 42);
+
+var image = ctx.getImageData(0, 0, 21, 21);
+var imageHD = ctxHD.getImageData(0, 0, 42, 42);
 
 var setIconParamQueue = [
-  {imageData: canvas},
+  {imageData: image},
   {path: 'icon.png'},
-  {imageData: {'21': canvas, '42': canvasHD}},
+  {imageData: {'21': image, '42': imageHD}},
   {path: {'21': 'icon.png', '42': 'icon2.png'}},
-  {imageData: {'21': canvas}},
+  {imageData: {'21': image}},
   {path: {'21': 'icon.png'}},
-  {imageData: {'42': canvasHD}},
+  {imageData: {'42': imageHD}},
   {imageData: {}},
   {path: {}},
 ];
@@ -28,7 +33,7 @@ var setIconParamQueue = [
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(windowId) {
   if (setIconParamQueue.length == 0) {
-    chrome.test.notifyFail("Queue of params for test cases unexpectedly empty");
+    chrome.test.notifyFail('Queue of params for test cases unexpectedly empty');
     return;
   }
 
---
+++ chrome/test/data/extensions/api_test/browser_action/update/background.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function updateBrowserAction() {
+  chrome.browserAction.setTitle({title: 'Modified'});
+  chrome.browserAction.setIcon({path: 'icon2.png'});
+  chrome.browserAction.setBadgeText({text: 'badge'});
+  chrome.browserAction.setBadgeBackgroundColor({color: [255,255,255,255]});
+}
+
+chrome.extension.isAllowedIncognitoAccess(function(isAllowedAccess) {
+  if (isAllowedAccess == true) {
+    chrome.test.sendMessage('incognito ready', function(message) {
+      if (message == 'incognito update') {
+        updateBrowserAction();
+        chrome.test.notifyPass();
+      }
+    });
+  }
+});
+
+chrome.test.sendMessage('ready', function(message) {
+  if (message == 'update') {
+    updateBrowserAction();
+    chrome.test.notifyPass();
+  }
+});
