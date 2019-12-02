6f9180c71e2853aa88e50eedc8b0160086918aa1
Jeremy Apthorp <jeremya@chromium.org>
2019-12-02 20:27:39 +0000

    [win] msstl compatibility in //pdf
    Change-Id: I09d017033b1a6a93dcc4dd1f22de7a304ef5bb74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937696
    Auto-Submit: Jeremy Apthorp <jeremya@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720630}

--- pdf/document_layout.cc
+++ pdf/document_layout.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "pdf/document_layout.h"
 
+#include <algorithm>
+
 #include "base/logging.h"
 #include "ppapi/cpp/rect.h"
 #include "ppapi/cpp/size.h"
--- pdf/draw_utils/coordinates.cc
+++ pdf/draw_utils/coordinates.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/draw_utils/coordinates.h"
 
 #include <math.h>
+#include <algorithm>
 
 #include "base/logging.h"
 #include "ppapi/cpp/point.h"
