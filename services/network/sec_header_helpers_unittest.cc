48eb4545263a206c73cbad867da1761f703d1594
Yifan Luo <lyf@google.com>
2019-12-02 14:24:16 +0000

    Move `Sec-Fetch-User` to SetSecFetchMetadataHeaders.
    Rather than setting `Sec-Fetch-User` in
    NavigationRequest::AddAdditionalRequestHeaders, it should be set in
    SetFetchMetadataHeaders along with the others.
    
    Bug: 1027528
    Change-Id: I42c16dadceb7b4e69165bab67734a09747eb35a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930742
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Yifan Luo <lyf@google.com>
    Cr-Commit-Position: refs/heads/master@{#720477}

--- services/network/sec_header_helpers_unittest.cc
+++ services/network/sec_header_helpers_unittest.cc
@@ -25,6 +25,7 @@ constexpr char kInsecureSite[] = "http://othersite.tld";
 constexpr char kKnownSecChHeader[] = "Sec-CH-UA";
 constexpr char kKnownSecFetchSiteHeader[] = "Sec-Fetch-Site";
 constexpr char kKnownSecFetchModeHeader[] = "Sec-Fetch-Mode";
+constexpr char kKnownSecFetchUserHeader[] = "Sec-Fetch-User";
 constexpr char kOtherSecHeader[] = "sec-other-info-header";
 constexpr char kOtherHeader[] = "Other-Header";
 
@@ -169,7 +170,8 @@ TEST_F(SecHeaderHelpersTest, UnprivilegedRequestOnExtension) {
       url::Origin::Create(url);
 
   SetFetchMetadataHeaders(current_url_request,
-                          network::mojom::RequestMode::kCors, &url, params);
+                          network::mojom::RequestMode::kCors, false, &url,
+                          params);
   ASSERT_EQ(2, static_cast<int>(current_url_request->extra_request_headers()
                                     .GetHeaderVector()
                                     .size()));
@@ -204,8 +206,9 @@ TEST_F(SecHeaderHelpersTest, PrivilegedRequestOnExtension) {
           mojom::CorsOriginAccessMatchPriority::kDefaultPriority));
 
   SetFetchMetadataHeaders(current_url_request,
-                          network::mojom::RequestMode::kCors, &url, params);
-  ASSERT_EQ(2, static_cast<int>(current_url_request->extra_request_headers()
+                          network::mojom::RequestMode::kCors, true, &url,
+                          params);
+  ASSERT_EQ(3, static_cast<int>(current_url_request->extra_request_headers()
                                     .GetHeaderVector()
                                     .size()));
 
@@ -217,6 +220,10 @@ TEST_F(SecHeaderHelpersTest, PrivilegedRequestOnExtension) {
   ASSERT_TRUE(current_url_request->extra_request_headers().GetHeader(
       kKnownSecFetchModeHeader, &header_value));
   ASSERT_EQ(header_value, "cors");
+
+  ASSERT_TRUE(current_url_request->extra_request_headers().GetHeader(
+      kKnownSecFetchUserHeader, &header_value));
+  ASSERT_EQ(header_value, "?1");
 }
 
 }  // namespace network
