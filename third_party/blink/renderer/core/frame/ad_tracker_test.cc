e25f154d985ca59bae3ff3c4765035ff1bec59f9
Carlos Caballero <carlscab@google.com>
2019-12-03 01:14:52 +0000

    Remove MessageLoop references in /third_party/blink/renderer/core/frame
    MessageLoop is going away soon.
    
    This CL was uploaded by git cl split.
    
    R=japhet@chromium.org
    
    Bug: 891670
    Change-Id: I20f6d248ef6e288252dad8462e0712a0b4974bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1945811
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720714}

--- third_party/blink/renderer/core/frame/ad_tracker_test.cc
+++ third_party/blink/renderer/core/frame/ad_tracker_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/run_loop.h"
 #include "base/test/scoped_feature_list.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
