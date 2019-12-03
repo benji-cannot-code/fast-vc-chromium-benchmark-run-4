879afe53dd20d9def05edf7d506642e7b2d3e8fc
Takumi Fujimoto <takumif@chromium.org>
2019-12-03 23:45:13 +0000

    Disable ChromeFindRequestManagerTest.FindInPDF on win-asan
    Disabling for flakiness.
    
    TBR=thestig
    
    Bug: 1030368
    Change-Id: I0dc5f22acf376b23e54988ec01ea5e6527da5a1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949032
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721234}

--- chrome/browser/chrome_find_request_manager_browsertest.cc
+++ chrome/browser/chrome_find_request_manager_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/pdf/pdf_extension_test_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -81,9 +82,14 @@ class ChromeFindRequestManagerTest : public InProcessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(ChromeFindRequestManagerTest);
 };
 
-
 // Tests searching in a full-page PDF.
-IN_PROC_BROWSER_TEST_F(ChromeFindRequestManagerTest, FindInPDF) {
+// Flaky on Windows ASAN: crbug.com/1030368.
+#if defined(OS_WIN) && defined(ADDRESS_SANITIZER)
+#define MAYBE_FindInPDF DISABLED_FindInPDF
+#else
+#define MAYBE_FindInPDF FindInPDF
+#endif
+IN_PROC_BROWSER_TEST_F(ChromeFindRequestManagerTest, MAYBE_FindInPDF) {
   LoadAndWait("/find_in_pdf_page.pdf");
   ASSERT_TRUE(pdf_extension_test_util::EnsurePDFHasLoaded(contents()));
 
