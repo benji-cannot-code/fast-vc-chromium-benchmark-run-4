396cb274c443efbd9fe527f2740d94712afe41e4
Collin Baker <collinbaker@chromium.org>
2019-12-03 22:43:39 +0000

    Don't create WebUI tab strip for DevTools windows
    Fixed: 1010247
    Change-Id: Ic2efa71b563a339e5c9f23a76bbfbb8be5c9762a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948092
    Auto-Submit: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721207}

--- chrome/browser/ui/views/frame/webui_tab_strip_container_view_unittest.cc
+++ chrome/browser/ui/views/frame/webui_tab_strip_container_view_unittest.cc
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class WebUITabStripContainerViewTest : public TestWithBrowserView {
  public:
-  WebUITabStripContainerViewTest() : touch_mode_(true) {
+  template <typename... Args>
+  explicit WebUITabStripContainerViewTest(Args... args)
+      : TestWithBrowserView(args...), touch_mode_(true) {
     // Both the switch and |touch_mode_| are necessary since
     // MaterialDesignController::Initialize() gets called at different
     // times on different platforms.
@@ -54,4 +56,20 @@ TEST_F(WebUITabStripContainerViewTest, TouchModeTransition) {
       browser_view()->webui_tab_strip();
   ASSERT_NE(nullptr, container);
   EXPECT_TRUE(container->GetVisible());
+}
+
+class WebUITabStripDevToolsTest : public WebUITabStripContainerViewTest {
+ public:
+  WebUITabStripDevToolsTest()
+      : WebUITabStripContainerViewTest(Browser::TYPE_DEVTOOLS) {}
+  ~WebUITabStripDevToolsTest() override = default;
+};
+
+// Regression test for crbug.com/1010247.
+TEST_F(WebUITabStripDevToolsTest, DevToolsWindowHasNoTabStrip) {
+  EXPECT_EQ(nullptr, browser_view()->webui_tab_strip());
+
+  ui::test::MaterialDesignControllerTestAPI disable_touch_mode(false);
+  ui::test::MaterialDesignControllerTestAPI reenable_touch_mode(true);
+  EXPECT_EQ(nullptr, browser_view()->webui_tab_strip());
 }
