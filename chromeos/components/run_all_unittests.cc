e118872d1707e1d0d581bb36dbab6abc4929322a
Ben Pastene <bpastene@chromium.org>
2019-12-03 01:23:20 +0000

    Rename the is_cros_chrome_sdk global GN variable to is_chromeos_device.
    This makes the usage a little more clear, especially in a future world where
    (hopefully) no one is running the `cros chrome-sdk` scripts directly, subsequently
    making the phrase "cros_chrome_sdk" meaningless.
    
    As we replace/deprecate Simple Chrome, this arg will start showing up in many
    build files, so it's important that its purpose is clear in its name.
    eg: http://crrev.com/c/1922977/9/ui/events/ozone/BUILD.gn
    
    Bug: 937821
    Change-Id: I967aa9498ed04e351b9423bc75939632ad01fd7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925330
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720727}

--- chromeos/components/run_all_unittests.cc
+++ chromeos/components/run_all_unittests.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/chromeos_buildflags.h"
 #include "mojo/core/embedder/embedder.h"
 
-#if BUILDFLAG(IS_CROS_CHROME_SDK)
-#error This test target only builds with linux-chromeos, not cros chrome-sdk.\
-  See comment in build/config/chromeos/rules.gni.
+#if BUILDFLAG(IS_CHROMEOS_DEVICE)
+#error This test target only builds with linux-chromeos, not for real ChromeOS\
+ devices. See comment in build/config/chromeos/args.gni.
 #endif
 
 int main(int argc, char** argv) {
--- ui/chromeos/run_all_unittests.cc
+++ ui/chromeos/run_all_unittests.cc
@@ -19,9 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_paths.h"
 #include "ui/gl/test/gl_surface_test_support.h"
 
-#if BUILDFLAG(IS_CROS_CHROME_SDK)
-#error This test target only builds with linux-chromeos, not cros chrome-sdk.\
-  See comment in build/config/chromeos/rules.gni.
+#if BUILDFLAG(IS_CHROMEOS_DEVICE)
+#error This test target only builds with linux-chromeos, not for real ChromeOS\
+ devices. See comment in build/config/chromeos/args.gni.
 #endif
 
 namespace {
