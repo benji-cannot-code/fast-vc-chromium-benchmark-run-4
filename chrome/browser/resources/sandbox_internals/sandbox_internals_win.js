1fdca3e00b33254d9d7f9e1959d51e21ba72c9a2
Adrian Taylor <adetaylor@chromium.org>
2019-12-03 01:34:45 +0000

    Integrity typo.
    Change-Id: I661e2983db1324459fc6a112be00c908f6ba85e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1938034
    Auto-Submit: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720747}

--- chrome/browser/resources/sandbox_internals/sandbox_internals_win.js
+++ chrome/browser/resources/sandbox_internals/sandbox_internals_win.js
@@ -84,7 +84,7 @@ function onGetSandboxDiagnostics(results) {
   }
 
   // Titles.
-  addRow(['Process', 'Type', 'Name', 'Sandbox', 'Intregity', 'Mitigations']);
+  addRow(['Process', 'Type', 'Name', 'Sandbox', 'Integrity', 'Mitigations']);
 
   // Browser Processes.
   for (const process of results.browser) {
