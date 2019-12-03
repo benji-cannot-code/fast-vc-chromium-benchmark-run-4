9ee6f8a0fdbbbd76fccf34bf9743dd082a59e075
Natalie Chouinard <chouinard@chromium.org>
2019-12-03 20:51:54 +0000

    Extract site settings code from PreferencesLauncher
    Remove feature-specific method from PreferencesLauncher to the class
    where it's used.
    
    Bug: 1029194
    Change-Id: I845474b13e67df8d562a661fa916e47ac05e9405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1941307
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Pavel Shmakov <pshmakov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721140}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/TrustedWebActivitySettingsLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/TrustedWebActivitySettingsLauncher.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.browserservices;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
@@ -14,6 +15,7 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.settings.PreferencesLauncher;
 import org.chromium.chrome.browser.settings.website.SettingsNavigationSource;
 import org.chromium.chrome.browser.settings.website.SingleCategoryPreferences;
+import org.chromium.chrome.browser.settings.website.SingleWebsitePreferences;
 import org.chromium.chrome.browser.settings.website.SiteSettingsCategory;
 
 import java.util.ArrayList;
@@ -68,7 +70,7 @@ public class TrustedWebActivitySettingsLauncher {
     }
 
     private static void openSingleWebsitePrefs(Context context, String origin) {
-        context.startActivity(PreferencesLauncher.createIntentForSingleWebsitePreferences(
+        context.startActivity(createIntentForSingleWebsitePreferences(
                 context, origin, SettingsNavigationSource.TWA_CLEAR_DATA_DIALOG));
     }
 
@@ -84,5 +86,16 @@ public class TrustedWebActivitySettingsLauncher {
                 SettingsNavigationSource.TWA_CLEAR_DATA_DIALOG);
 
         PreferencesLauncher.launchSettingsPage(context, SingleCategoryPreferences.class, extras);
+    }
+
+    /**
+     * Creates an intent to launch single website preferences for the specified {@param url}.
+     */
+    private static Intent createIntentForSingleWebsitePreferences(
+            Context context, String url, @SettingsNavigationSource int navigationSource) {
+        Bundle args = SingleWebsitePreferences.createFragmentArgsForSite(url);
+        args.putInt(SettingsNavigationSource.EXTRA_KEY, navigationSource);
+        return PreferencesLauncher.createIntentForSettingsPage(
+                context, SingleWebsitePreferences.class.getName(), args);
     }
 }
