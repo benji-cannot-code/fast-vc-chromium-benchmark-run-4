0f8242d204aff752bde993ede2d91a3c3ec835fc
Oleg Davydov <burunduk@chromium.org>
2019-12-03 17:37:01 +0000

    [CV] Move hashes computing to ComputedHashes
    Historically we've computed extension resources' hashes in
    ContentHash::CreateHashes, but logically it's a job for ComputedHashes
    class, so now we do it in ComputedHashes::Compute method.
    
    In the future this will allow us to reuse ComputedHashes::Create for
    other purposes (eg. testing or computing hashes upon installation for
    self-hosted extensions).
    
    Bug: 796395, 958794
    Change-Id: Ie16c53ee682a4b8066dd2e929e13cbbc5be2d6ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913415
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Nikita Podguzov <nikitapodguzov@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721051}

--- extensions/browser/content_verify_job_unittest.cc
+++ extensions/browser/content_verify_job_unittest.cc
@@ -77,7 +77,8 @@ void WriteComputedHashes(
   for (const auto& resource : contents) {
     std::vector<std::string> hashes =
         ComputedHashes::GetHashesForContent(resource.second, block_size);
-    computed_hashes_data.AddHashes(resource.first, block_size, hashes);
+    computed_hashes_data[resource.first] =
+        ComputedHashes::HashInfo(block_size, hashes);
   }
 
   base::CreateDirectory(extension_root.Append(kMetadataFolder));
@@ -365,7 +366,8 @@ void WriteIncorrectComputedHashes(const base::FilePath& extension_path,
   const std::string kFakeContents = "fake contents";
   std::vector<std::string> hashes =
       ComputedHashes::GetHashesForContent(kFakeContents, block_size);
-  incorrect_computed_hashes_data.AddHashes(resource_path, block_size, hashes);
+  incorrect_computed_hashes_data[resource_path] =
+      ComputedHashes::HashInfo(block_size, hashes);
 
   ASSERT_TRUE(
       ComputedHashes(std::move(incorrect_computed_hashes_data))
