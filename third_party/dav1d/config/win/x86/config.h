fb41bca0c7d3208cbd62b2f29f9dca0b8851197f
Dale Curtis <dalecurtis@chromium.org>
2019-12-03 01:40:11 +0000

    Roll src/third_party/dav1d/libdav1d/ 564d3d911..39667c751 (21 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/564d3d9119da..39667c751d42
    
    $ git log 564d3d911..39667c751 --date=short --no-merges --format='%ad %ae %s'
    2019-12-02 jb Update NEWS
    2019-12-01 martin arm: 32: ipred: Fix assembling with older binutils
    2019-11-22 gramner Avoid excessive L2 collisions with certain frame widths
    2019-11-25 janne-vlc build: do not error out if clock_gettime is not found on darwin
    2019-11-22 gramner Specify inlining decisions for common ipred_z C functions
    2019-11-26 me cli: Return only EXIT_SUCCESS or EXIT_FAILURE from main
    2019-11-23 gramner tools: simplify get_seed on macOS
    2019-07-29 jbeich checkasm: replace gettimeofday with clock_gettime
    2019-11-22 jbeich tools: prefer mach_absolute_time on macOS for forward compatibility
    2019-11-22 jbeich tools: mach_absolute_time is not portable, so stop checking at configure
    2019-11-21 ltrudeau Avoid adding offsets to NULL pointers
    2019-11-22 thresh snap: bump grade to stable
    2019-11-23 gramner input: Use EIO instead of ENODATA
    2019-11-22 martin annexb: Cast size_t to int before adding to an int
    2019-11-17 rsbultje Fix accidental capital letter in variable name
    2019-11-21 rsbultje Fix stride type
    2019-11-21 martin section5: use signed type for seeking backwards
    2019-11-16 rsbultje Add demuxer probing
    2019-11-15 rsbultje Add section-5 raw OBU demuxer
    2019-11-16 rsbultje Make OBU_* types public
    2019-11-19 gramner Increase DAV1D_PICTURE_ALIGNMENT to 64
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    
    TBR=tguilbert
    
    Fixes: 1027029
    Change-Id: If00ab333797cb321a9323a3d1af8ae94d2e07fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946877
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720757}

--- third_party/dav1d/config/win/x86/config.h
+++ third_party/dav1d/config/win/x86/config.h
@@ -38,6 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define UNICODE 1
 
+#define _CRT_DECLARE_NONSTDC_NAMES 1
+
 #define _UNICODE 1
 
 // #define _WIN32_WINNT 0x0601 -- Windows version is controlled by Chromium
