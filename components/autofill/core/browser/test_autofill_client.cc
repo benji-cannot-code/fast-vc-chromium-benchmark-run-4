038096863ff4162739ddf7be28d13e71f613afc4
siyua <siyua@chromium.org>
2019-12-03 05:34:36 +0000

    [Autofill Auth UI] Disable the cancel button when OS dialog is visible.
    Disable the cancel button in the Webauthn verify pending dialog when
    the system OS authentication dialog is shown. At this moment, if user
    cancel the OS dialog, the verify pending dialog will also go away.
    
    After user interacts with the system OS dialog, the OS dialog will
    go away and the cancel button will be enabled again for users to
    cancel the flow if they want.
    
    Bug: 991037
    Change-Id: I8760690e377e3d294832dc5278d177eb3017dc65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935493
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Manas Verma <manasverma@google.com>
    Cr-Commit-Position: refs/heads/master@{#720832}

--- components/autofill/core/browser/test_autofill_client.cc
+++ components/autofill/core/browser/test_autofill_client.cc
@@ -117,6 +117,9 @@ void TestAutofillClient::ShowWebauthnVerifyPendingDialog(
 
 void TestAutofillClient::UpdateWebauthnOfferDialogWithError() {}
 
+void TestAutofillClient::UpdateWebauthnVerifyPendingCancelButton(
+    bool should_be_enabled) {}
+
 bool TestAutofillClient::CloseWebauthnDialog() {
   return true;
 }
