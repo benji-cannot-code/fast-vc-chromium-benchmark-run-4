e6944e068164c7d07654035b130d65a7fb4e17a2
Takumi Fujimoto <takumif@chromium.org>
2019-12-03 18:51:51 +0000

    Revert "Entering Picture-In-Picture re-enables disabled video track if needed."
    This reverts commit 094d246a3795336a126d9382a1e598b7e1bf335d.
    
    Reason for revert: I believe this is causing flaky failures in BackgroundBehaviorTestInstances/WebMediaPlayerImplBackgroundBehaviorTest.AudioVideo:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests/84561
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/31980
    
    Original change's description:
    > Entering Picture-In-Picture re-enables disabled video track if needed.
    > 
    > This CL makes sure video track disabled by video background track
    > optimization is re-enabled when entering Picture-in-Picture.
    > 
    > Bug: 949265
    > Change-Id: Id0cca141f262537672f49f8cdc8bc6db5c10e3dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1556837
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#721018}
    
    TBR=beaufort.francois@gmail.com,mlamouri@chromium.org,sandersd@chromium.org
    
    Change-Id: I3aea0364af64ffa231dfafde3b519bdb65c4dd33
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 949265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949144
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721096}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -3352,7 +3352,7 @@ bool WebMediaPlayerImpl::IsBackgroundOptimizationCandidate() const {
 }
 
 void WebMediaPlayerImpl::UpdateBackgroundVideoOptimizationState() {
-  if (IsHidden() && !IsInPictureInPicture()) {
+  if (IsHidden()) {
     if (ShouldPausePlaybackWhenHidden()) {
       PauseVideoIfNeeded();
     } else if (update_background_status_cb_.IsCancelled()) {
