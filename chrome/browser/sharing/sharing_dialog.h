48943a2621cd5296657d432b2ead8e5892f1c92c
Himanshu Jaju <himanshujaju@chromium.org>
2019-12-03 11:10:07 +0000

    Add virtual destructor in SharingDialog
    To avoid potential memory leak in the future, we make the destructor
    overridable.
    
    Bug: 1029505
    Change-Id: I75ac34ceface5a09404bb720528faab5e63b0f14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1945819
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720911}

--- chrome/browser/sharing/sharing_dialog.h
+++ chrome/browser/sharing/sharing_dialog.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This object is responsible for its own lifetime.
 class SharingDialog {
  public:
+  virtual ~SharingDialog() = default;
+
   // Called to close the dialog and prevent future callbacks into the
   // controller.
   virtual void Hide() = 0;
