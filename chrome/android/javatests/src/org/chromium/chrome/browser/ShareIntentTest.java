03222731542a13eec45c30cf6bada62538c4344b
Kyle Milka <kmilka@chromium.org>
2019-12-03 01:30:24 +0000

    [SharingHub] Wire up QrCode to the sharing hub
    Wire up QRCode sharing feature to the sharing hub.
    
    Bug: 1009124
    Change-Id: Ie3aa5723f90ec7ea54989e7ebe85dff1fc3dd42e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1888736
    Commit-Queue: Kyle Milka <kmilka@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720739}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ShareIntentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ShareIntentTest.java
@@ -26,8 +26,8 @@ import org.chromium.base.task.TaskTraits;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.share.ShareDelegate;
+import org.chromium.chrome.browser.share.ShareDelegateImpl;
 import org.chromium.chrome.browser.share.ShareHelper;
-import org.chromium.chrome.browser.share.ShareSheetCoordinator;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.ui.RootUiCoordinator;
 import org.chromium.chrome.browser.util.ChromeFileProvider;
@@ -170,7 +170,7 @@ public class ShareIntentTest {
         ShareHelper.setLastShareComponentName(
                 new ComponentName("test.package", "test.activity"), null);
         // Skips the capture of screenshot and notifies with an empty file.
-        ShareSheetCoordinator.setScreenshotCaptureSkippedForTesting(true);
+        ShareDelegateImpl.setScreenshotCaptureSkippedForTesting(true);
 
         WindowAndroid window = TestThreadUtils.runOnUiThreadBlocking(() -> {
             return new WindowAndroid(mActivityTestRule.getActivity()) {
@@ -200,6 +200,6 @@ public class ShareIntentTest {
 
     @After
     public void tearDown() {
-        ShareSheetCoordinator.setScreenshotCaptureSkippedForTesting(false);
+        ShareDelegateImpl.setScreenshotCaptureSkippedForTesting(false);
     }
 }
