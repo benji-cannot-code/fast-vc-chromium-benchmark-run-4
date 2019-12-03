6da920a7856de18747d44c5e180fa8d68b017dc0
Andy Paicu <andypaicu@chromium.org>
2019-12-03 12:18:34 +0000

    Add a minimum display period before the flash banner expires on navigation
    When using pinned tabs, the banner will immediately disappear because
    of the navigation to the pinned tab's address. This ensures that there
    is a minimum display period before the banner expires on navigations.
    
    Bug: 1001333
    Change-Id: Idabc5a972696068f3ce7d5a9f70ee0ca95d4c0f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925063
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720922}

--- chrome/browser/plugins/flash_deprecation_infobar_delegate.h
+++ chrome/browser/plugins/flash_deprecation_infobar_delegate.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Profile;
 class InfoBarService;
 
+namespace base {
+class Time;
+}
+
 class FlashDeprecationInfoBarDelegate : public ConfirmInfoBarDelegate {
  public:
   static void Create(InfoBarService* infobar_service, Profile* profile);
@@ -20,7 +24,6 @@ class FlashDeprecationInfoBarDelegate : public ConfirmInfoBarDelegate {
   static bool ShouldDisplayFlashDeprecation(Profile* profile);
 
   explicit FlashDeprecationInfoBarDelegate(Profile* profile);
-  ~FlashDeprecationInfoBarDelegate() override = default;
 
   // ConfirmInfobarDelegate:
   infobars::InfoBarDelegate::InfoBarIdentifier GetIdentifier() const override;
@@ -32,9 +35,16 @@ class FlashDeprecationInfoBarDelegate : public ConfirmInfoBarDelegate {
   base::string16 GetLinkText() const override;
   GURL GetLinkURL() const override;
   void InfoBarDismissed() override;
+  bool ShouldExpire(const NavigationDetails& details) const override;
 
  private:
+  // The profile associated with this infobar.
   Profile* const profile_;
+
+  // The time at which the banner has started to be displayed. Used to determine
+  // if the banner should expire on navigation, based on how long it has been
+  // visible.
+  base::Time display_start_;
 };
 
 #endif  // CHROME_BROWSER_PLUGINS_FLASH_DEPRECATION_INFOBAR_DELEGATE_H_
