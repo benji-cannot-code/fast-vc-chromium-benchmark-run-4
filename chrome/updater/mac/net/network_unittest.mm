7f9595c01359d45e2a87a949dbaa6d5b126d12a0
Carlos Caballero <carlscab@google.com>
2019-12-03 07:17:07 +0000

    Remove MessageLoop references in /chrome/updater
    MessageLoop is going away soon.
    
    This CL was uploaded by git cl split.
    
    R=sorin@chromium.org
    
    Bug: 891670
    Change-Id: Ifb3bb37fe4bfa36d11422dba619c7ade2f84d151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1944668
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#720863}

--- chrome/updater/mac/net/network_unittest.mm
+++ chrome/updater/mac/net/network_unittest.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
