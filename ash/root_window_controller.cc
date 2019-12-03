78f5808db019de6e26704b99d18018dd8057e31c
Avery Musbach <amusbach@chromium.org>
2019-12-03 17:59:02 +0000

    overview: Fix DCHECK failure and crash, in clamshell/tablet transition
    The present CL fixes what happens if you switch to tablet mode when the
    following criteria are met:
    1. There are at least two displays.
    2. There is only one window.
    3. The window is active.
    4. The window is snapped on the primary display.
    
    Without the present CL, NOTREACHED is hit (Issue 1024309), and there is
    also a crash if DCHECKs are disabled (Issue 1024325).
    
    Fixed: 1024309, 1024325
    Change-Id: Ie1a6e378067076c7dbd8ec3ed388e70cd9382611
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1938463
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721061}

--- ash/root_window_controller.cc
+++ ash/root_window_controller.cc
@@ -57,6 +57,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/lock_action_handler_layout_manager.h"
 #include "ash/wm/lock_layout_manager.h"
 #include "ash/wm/overlay_layout_manager.h"
+#include "ash/wm/overview/overview_controller.h"
+#include "ash/wm/overview/overview_session.h"
 #include "ash/wm/root_window_layout_manager.h"
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_utils.h"
@@ -618,6 +620,10 @@ void RootWindowController::CloseChildWindows() {
   // Notify the keyboard controller before closing child windows and shutting
   // down associated layout managers.
   Shell::Get()->keyboard_controller()->OnRootWindowClosing(root);
+
+  OverviewController* overview_controller = Shell::Get()->overview_controller();
+  if (overview_controller && overview_controller->InOverviewSession())
+    overview_controller->overview_session()->OnRootWindowClosing(root);
 
   shelf_->ShutdownShelfWidget();
 
