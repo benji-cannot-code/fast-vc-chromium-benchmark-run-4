b2e459e2f0cef3ec574b9db5935c530e597acb5d
Antonio Gomes <tonikitoo@igalia.com>
2019-12-03 16:59:50 +0000

    Get rid of CreateRtcEventLogOutputSinkProxy
    ... in favor calling the ctor directly.
    
    BUG=787254, 939691
    R=guidou@chromium.org
    
    Change-Id: I523d0b23d94152ce815e5ac8ab1099da307b0728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1944368
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#721022}

--- third_party/blink/renderer/platform/peerconnection/rtc_event_log_output_sink_proxy.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_event_log_output_sink_proxy.cc
@@ -10,11 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-std::unique_ptr<webrtc::RtcEventLogOutput> CreateRtcEventLogOutputSinkProxy(
-    RtcEventLogOutputSink* sink) {
-  return std::make_unique<RtcEventLogOutputSinkProxy>(sink);
-}
-
 RtcEventLogOutputSinkProxy::RtcEventLogOutputSinkProxy(
     RtcEventLogOutputSink* sink)
     : sink_(sink) {
