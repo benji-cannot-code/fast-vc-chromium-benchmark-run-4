86fa2e11b4c99e71ba65350b914113d5f37ca6ca
David Black <dmblack@google.com>
2019-12-03 18:30:50 +0000

    Fix hover state when using ProactiveSuggestionsRichView.
    The proactive suggestions entry point times out after a fixed interval.
    When the entry point is hovered over, timeout is paused.
    
    ProactiveSuggestionsRichView is not currently pausing timeout on hover
    because the embedded web contents consume the events causing mouse
    enter/exit to not be received by the embedding view.
    
    To fix this, we'll use an EventMonitor on our views native window to
    make sure we get the events we need to detect hover state.
    
    Bug: b:145056104
    Change-Id: I2588fe119addfe9577963d4c29baf566e9168336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1938448
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#721083}

--- ash/assistant/ui/proactive_suggestions_rich_view.cc
+++ ash/assistant/ui/proactive_suggestions_rich_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "chromeos/services/assistant/public/features.h"
 #include "ui/aura/window.h"
+#include "ui/views/event_monitor.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/widget/widget.h"
 
@@ -125,6 +126,50 @@ void ProactiveSuggestionsRichView::InitLayout() {
   // Navigate to the data URL representing our encoded HTML.
   constexpr char kDataUriPrefix[] = "data:text/html;base64,";
   contents_->Navigate(GURL(kDataUriPrefix + encoded_html));
+}
+
+void ProactiveSuggestionsRichView::AddedToWidget() {
+  // Our embedded web contents will consume events that would otherwise reach
+  // our view so we need to use an EventMonitor to still receive them.
+  event_monitor_ = views::EventMonitor::CreateWindowMonitor(
+      this, GetWidget()->GetNativeWindow(),
+      {ui::ET_GESTURE_TAP, ui::ET_GESTURE_TAP_CANCEL, ui::ET_GESTURE_TAP_DOWN,
+       ui::ET_MOUSE_ENTERED, ui::ET_MOUSE_MOVED, ui::ET_MOUSE_EXITED});
+}
+
+void ProactiveSuggestionsRichView::OnMouseEntered(const ui::MouseEvent& event) {
+  // Our embedded web contents is expected to consume events that would
+  // otherwise reach our view. We instead handle these events in OnEvent().
+  NOTREACHED();
+}
+
+void ProactiveSuggestionsRichView::OnMouseExited(const ui::MouseEvent& event) {
+  // Our embedded web contents is expected to consume events that would
+  // otherwise reach our view. We instead handle these events in OnEvent().
+  NOTREACHED();
+}
+
+void ProactiveSuggestionsRichView::OnGestureEvent(ui::GestureEvent* event) {
+  // Our embedded web contents is expected to consume events that would
+  // otherwise reach our view. We instead handle these events in OnEvent().
+  NOTREACHED();
+}
+
+void ProactiveSuggestionsRichView::OnEvent(const ui::Event& event) {
+  switch (event.type()) {
+    case ui::ET_GESTURE_TAP:
+    case ui::ET_GESTURE_TAP_CANCEL:
+    case ui::ET_MOUSE_EXITED:
+      delegate()->OnProactiveSuggestionsViewHoverChanged(/*is_hovering=*/false);
+      break;
+    case ui::ET_GESTURE_TAP_DOWN:
+    case ui::ET_MOUSE_ENTERED:
+      delegate()->OnProactiveSuggestionsViewHoverChanged(/*is_hovering=*/true);
+      break;
+    default:
+      NOTREACHED();
+      break;
+  }
 }
 
 void ProactiveSuggestionsRichView::ShowWhenReady() {
--- ash/assistant/ui/proactive_suggestions_rich_view.h
+++ ash/assistant/ui/proactive_suggestions_rich_view.h
@@ -12,6 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/content/public/cpp/navigable_contents.h"
 #include "services/content/public/cpp/navigable_contents_view.h"
+#include "ui/events/event_observer.h"
+
+namespace views {
+class EventMonitor;
+}  // namespace views
 
 namespace ash {
 
@@ -20,6 +25,7 @@ class AssistantViewDelegate;
 // Rich entry point for the proactive suggestions feature.
 class COMPONENT_EXPORT(ASSISTANT_UI) ProactiveSuggestionsRichView
     : public ProactiveSuggestionsView,
+      public ui::EventObserver,
       public content::NavigableContentsObserver {
  public:
   explicit ProactiveSuggestionsRichView(AssistantViewDelegate* delegate);
@@ -31,9 +37,17 @@ class COMPONENT_EXPORT(ASSISTANT_UI) ProactiveSuggestionsRichView
   // ProactiveSuggestionsView:
   const char* GetClassName() const override;
   void InitLayout() override;
+  void AddedToWidget() override;
   void ShowWhenReady() override;
   void Hide() override;
   void Close() override;
+  void OnMouseEntered(const ui::MouseEvent& event) override;
+  void OnMouseExited(const ui::MouseEvent& event) override;
+  void OnGestureEvent(ui::GestureEvent* event) override;
+
+  // ui::EventObserver:
+  using views::View::OnEvent;  // Suppress clang warning.
+  void OnEvent(const ui::Event& event) override;
 
   // content::NavigableContentsObserver:
   void DidAutoResizeView(const gfx::Size& new_size) override;
@@ -45,6 +59,7 @@ class COMPONENT_EXPORT(ASSISTANT_UI) ProactiveSuggestionsRichView
  private:
   mojo::Remote<content::mojom::NavigableContentsFactory> contents_factory_;
   std::unique_ptr<content::NavigableContents> contents_;
+  std::unique_ptr<views::EventMonitor> event_monitor_;
 
   // Because the web contents that this view embeds loads asynchronously, it
   // may cause UI jank if we show our widget before loading has stopped. To
