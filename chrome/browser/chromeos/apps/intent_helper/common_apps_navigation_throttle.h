48e38a675417f8f9e139ddf55211119df81e7eee
Maggie Cai <mxcai@chromium.org>
2019-12-03 05:55:07 +0000

    [IntentHandling] Automatically launch preferred app.
    This CL adds the logic to automatically launch the preferred app in the
    new intent picker implementation.
    
    BUG=853604
    
    Change-Id: Ie7a698a9cca259b5469535d6add0f6d1e99f7493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947863
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720840}

--- chrome/browser/chromeos/apps/intent_helper/common_apps_navigation_throttle.h
+++ chrome/browser/chromeos/apps/intent_helper/common_apps_navigation_throttle.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/apps/intent_helper/apps_navigation_throttle.h"
 #include "chrome/services/app_service/public/mojom/types.mojom.h"
 #include "content/public/browser/navigation_throttle.h"
@@ -89,6 +90,13 @@ class CommonAppsNavigationThrottle : public apps::AppsNavigationThrottle {
       content::WebContents* web_contents,
       IntentPickerAutoDisplayService* ui_auto_display_service,
       const GURL& url) override;
+
+  bool ShouldDeferNavigation(content::NavigationHandle* handle) override;
+
+  void OnDeferredNavigationProcessed(
+      std::vector<apps::IntentPickerAppInfo> apps);
+
+  base::WeakPtrFactory<CommonAppsNavigationThrottle> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(CommonAppsNavigationThrottle);
 };
