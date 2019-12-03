c9958d7d0b751c7aa6003ebaf3b57f28c5c305e3
Shuhei Takahashi <nya@chromium.org>
2019-12-03 13:33:33 +0000

    Revert "Move Utility processes off Service Manager"
    This reverts commit 0a311ff4afcdb6819e481ce5bbcb1baa617d4189.
    
    Reason for revert: breaks graphics on some Chrome OS devices.
    
    Original change's description:
    > Move Utility processes off Service Manager
    >
    > This migrates Utility processes to direct ChildProcess API usage instead
    > of bootstrapping IPC through the Service Manager.
    >
    > As this is the last remaining use of Service Manager IPC to bootstrap
    > Content child processes, this also rips out a bunch of infrastructure
    > that existed only to support that.
    >
    > Bug: 977637
    > Change-Id: I08e542f1d9f294bc1c387ea5845e8ba0d5a7d2b8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919719
    > Commit-Queue: Ken Rockot <rockot@google.com>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#718352}
    
    NOPRESUBMIT=true
    
    Bug: 1028852
    Bug: 977637
    Tbr: rockot@google.com
    Tbr: avi@chromium.org
    Tbr: rsesek@chromium.org
    Change-Id: I9b2e8ca44f5b6accc2d3718cab3cb547d979c230
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948402
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720938}

--- content/common/in_process_child_thread_params.h
+++ content/common/in_process_child_thread_params.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_IN_PROCESS_CHILD_THREAD_PARAMS_H_
 #define CONTENT_COMMON_IN_PROCESS_CHILD_THREAD_PARAMS_H_
 
+#include <string>
+
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
 #include "content/common/content_export.h"
@@ -20,7 +22,8 @@ class CONTENT_EXPORT InProcessChildThreadParams {
  public:
   InProcessChildThreadParams(
       scoped_refptr<base::SingleThreadTaskRunner> io_runner,
-      mojo::OutgoingInvitation* mojo_invitation);
+      mojo::OutgoingInvitation* mojo_invitation,
+      const std::string& service_request_token = {});
   InProcessChildThreadParams(const InProcessChildThreadParams& other);
   ~InProcessChildThreadParams();
 
@@ -30,9 +33,14 @@ class CONTENT_EXPORT InProcessChildThreadParams {
 
   mojo::OutgoingInvitation* mojo_invitation() const { return mojo_invitation_; }
 
+  const std::string& service_request_token() const {
+    return service_request_token_;
+  }
+
  private:
   scoped_refptr<base::SingleThreadTaskRunner> io_runner_;
   mojo::OutgoingInvitation* const mojo_invitation_;
+  std::string service_request_token_;
 };
 
 }  // namespace content
