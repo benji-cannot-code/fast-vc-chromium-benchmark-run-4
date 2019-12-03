f04e4a8fd7ef0dea8fd0f1902441fcfd69fa08db
Alice Wang <aliceywang@chromium.org>
2019-12-03 10:40:03 +0000

    [Android][Signin] Enable disabled signin helper test
    This CL enables the disabled signin helper test.
    
    Bug: 577880
    Change-Id: I14d5f818a1b091b6392dca8083c217e179993239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942089
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720903}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninHelperTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninHelperTest.java
@@ -15,7 +15,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.browser.signin.MockChangeEventChecker;
@@ -85,13 +84,12 @@ public class SigninHelperTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/568623")
     @SmallTest
     public void testNotSignedInAccountRename() {
         setSignedInAccountName("A");
         mEventChecker.insertRenameEvent("B", "C");
         SigninHelper.updateAccountRenameData(mEventChecker);
-        Assert.assertEquals(null, getNewSignedInAccountName());
+        Assert.assertNull(getNewSignedInAccountName());
     }
 
     @Test
@@ -114,7 +112,7 @@ public class SigninHelperTest {
         mEventChecker.insertRenameEvent("B", "C");
         mEventChecker.insertRenameEvent("C", "D");
         SigninHelper.updateAccountRenameData(mEventChecker);
-        Assert.assertEquals(null, getNewSignedInAccountName());
+        Assert.assertNull(getNewSignedInAccountName());
     }
 
     @Test
@@ -151,10 +149,6 @@ public class SigninHelperTest {
 
     private void setSignedInAccountName(String account) {
         ChromeSigninController.get().setSignedInAccountName(account);
-    }
-
-    private String getSignedInAccountName() {
-        return ChromeSigninController.get().getSignedInAccountName();
     }
 
     private String getNewSignedInAccountName() {
--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/signin/MockChangeEventChecker.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/signin/MockChangeEventChecker.java
@@ -19,8 +19,7 @@ import java.util.Map;
  */
 public final class MockChangeEventChecker
         implements SigninHelper.AccountChangeEventChecker {
-    private Map<String, List<String>> mEvents =
-            new HashMap<String, List<String>>();
+    private Map<String, List<String>> mEvents = new HashMap<>();
 
     @Override
     public List<String> getAccountChangeEvents(
@@ -37,7 +36,7 @@ public final class MockChangeEventChecker
     private List<String> getEventList(String account) {
         List<String> eventsList = mEvents.get(account);
         if (eventsList == null) {
-            eventsList = new ArrayList<String>();
+            eventsList = new ArrayList<>();
             mEvents.put(account, eventsList);
         }
         return eventsList;
