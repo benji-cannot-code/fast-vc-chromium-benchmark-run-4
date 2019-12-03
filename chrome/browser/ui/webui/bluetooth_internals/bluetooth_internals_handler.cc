dac6360a70e5d15c974a40b61d0cf86508362f94
Julie Jeongeun Kim <jkim@igalia.com>
2019-12-03 04:38:43 +0000

    Clean up old Mojo types for mojom::DebugLogsChangeHandler
    This CL cleans up old Mojo types for mojom::DebugLogsChangeHandler
    using PendingRemote and ReceiverSet instead of InterfacePtr
    and BindingSet.
    
    Bug: 955171
    Change-Id: Iffb8cb730857f8b5ea417300f0e8b9235c88ed97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1939070
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#720821}

--- chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_handler.cc
+++ chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_handler.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/bluetooth/debug_logs_manager.h"
 #include "device/bluetooth/adapter.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
-#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "url/gurl.h"
@@ -40,7 +39,7 @@ void BluetoothInternalsHandler::GetAdapter(GetAdapterCallback callback) {
 
 void BluetoothInternalsHandler::GetDebugLogsChangeHandler(
     GetDebugLogsChangeHandlerCallback callback) {
-  mojom::DebugLogsChangeHandlerPtr handler_ptr;
+  mojo::PendingRemote<mojom::DebugLogsChangeHandler> handler_remote;
   bool initial_toggle_value = false;
 
 #if defined(OS_CHROMEOS)
@@ -53,18 +52,18 @@ void BluetoothInternalsHandler::GetDebugLogsChangeHandler(
 
   switch (state) {
     case DebugLogsManager::DebugLogsState::kNotSupported:
-      // Leave |handler_ptr| null and |initial_toggle_value| false.
+      // Leave |handler_remote| NullRemote and |initial_toggle_value| false.
       break;
     case DebugLogsManager::DebugLogsState::kSupportedAndEnabled:
       initial_toggle_value = true;
       FALLTHROUGH;
     case DebugLogsManager::DebugLogsState::kSupportedButDisabled:
-      handler_ptr = debug_logs_manager_->GenerateInterfacePtr();
+      handler_remote = debug_logs_manager_->GenerateRemote();
       break;
   }
 #endif
 
-  std::move(callback).Run(std::move(handler_ptr), initial_toggle_value);
+  std::move(callback).Run(std::move(handler_remote), initial_toggle_value);
 }
 
 void BluetoothInternalsHandler::OnGetAdapter(
--- chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_handler.h
+++ chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals_handler.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "chrome/browser/ui/webui/bluetooth_internals/bluetooth_internals.mojom.h"
 #include "device/bluetooth/bluetooth_adapter.h"
-#include "mojo/public/cpp/bindings/binding_set.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 
