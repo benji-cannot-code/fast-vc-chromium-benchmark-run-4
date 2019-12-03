44a650e64d35f02b267e25ac59d86d696dab5aec
Alan Cutter <alancutter@chromium.org>
2019-12-03 06:55:36 +0000

    WebApps: Support PWA manifest icon URL updating
    This CL enables PWA manifest updating to detect when sites update their
    "icons" field (but not when the icon contents themselves change).
    
    Screencast: https://bugs.chromium.org/p/chromium/issues/attachment?aid=423127&signed_aid=pWxFmMl1EjQvs_LiPg2zog==&inline=1
    
    Bug: 926083
    Change-Id: I6ff14e7b813c9526f8e423b4d3de50c7f5349abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1935297
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720855}

--- chrome/browser/web_applications/components/app_registrar.h
+++ chrome/browser/web_applications/components/app_registrar.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "chrome/browser/web_applications/components/web_app_constants.h"
 #include "chrome/browser/web_applications/components/web_app_helpers.h"
+#include "chrome/common/web_application_info.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 class GURL;
@@ -70,6 +71,11 @@ class AppRegistrar {
   virtual base::Optional<GURL> GetAppScope(const AppId& app_id) const = 0;
   virtual DisplayMode GetAppDisplayMode(const AppId& app_id) const = 0;
   virtual DisplayMode GetAppUserDisplayMode(const AppId& app_id) const = 0;
+
+  // Returns the "icons" field from the app manifest, use |AppIconManager| to
+  // load icon bitmap data.
+  virtual std::vector<WebApplicationIconInfo> GetAppIconInfos(
+      const AppId& app_id) const = 0;
 
   virtual std::vector<AppId> GetAppIds() const = 0;
 
