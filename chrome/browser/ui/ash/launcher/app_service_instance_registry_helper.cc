a96821e10e70ef5d354b39faf55fc4c554e6efd6
nancy <nancylingwang@chromium.org>
2019-12-03 03:03:28 +0000

    Add active user changed handling for App Service app window.
    Update AppServiceProxy when the user is switched because
    AppServiceProxy is based on profile.
    
    Add MultiProfileBrowserStatusMonitor to ChromeLauncherController when
    kAppServiceInstanceRegistry flag is enabled.
    
    BUG=1011235
    
    Change-Id: Id46f72d2334b94f601275560088112180c5bfc87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940314
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720789}

--- chrome/browser/ui/ash/launcher/app_service_instance_registry_helper.cc
+++ chrome/browser/ui/ash/launcher/app_service_instance_registry_helper.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/services/app_service/public/cpp/instance_update.h"
 #include "chrome/services/app_service/public/mojom/types.mojom.h"
@@ -24,6 +25,14 @@ AppServiceInstanceRegistryHelper::AppServiceInstanceRegistryHelper(
           std::make_unique<LauncherControllerHelper>(profile)) {}
 
 AppServiceInstanceRegistryHelper::~AppServiceInstanceRegistryHelper() = default;
+
+void AppServiceInstanceRegistryHelper::ActiveUserChanged() {
+  if (!base::FeatureList::IsEnabled(features::kAppServiceInstanceRegistry))
+    return;
+
+  proxy_ = apps::AppServiceProxyFactory::GetForProfile(
+      ProfileManager::GetActiveUserProfile());
+}
 
 void AppServiceInstanceRegistryHelper::OnActiveTabChanged(
     content::WebContents* old_contents,
