2f1a00f3b66ff8de61cfcdbdc84305d97b74b7d0
John Chen <johnchen@chromium.org>
2019-12-03 16:58:16 +0000

    [ChromeDriver] Workaround DevTools fullscreen issue
    Add some workaround code while setting browser window to fullscreen
    mode, to work around an issue in DevTools fullscreen command. In
    particular, call document.documentElement.requestFullscreen() to ensure
    that document.webkitIsFullScreen returns the correct values. All WPT
    tests related to fullscreen now pass.
    
    Also made an adjustment to WPT set_window_rect/set.py
    test_fully_exit_fullscreen, which was using a width below the minimum
    supported by Chrome.
    
    Bug: 982071
    Change-Id: I0e50883c3f67a566df1bc4ffd331aeac0b0ef1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947816
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721019}

--- chrome/test/chromedriver/chrome/chrome_impl.cc
+++ chrome/test/chromedriver/chrome/chrome_impl.cc
@@ -187,7 +187,7 @@ Status ChromeImpl::SetWindowPosition(const std::string& target_id,
   auto bounds = std::make_unique<base::DictionaryValue>();
   bounds->SetInteger("left", x);
   bounds->SetInteger("top", y);
-  return SetWindowBounds(&window, std::move(bounds));
+  return SetWindowBounds(&window, target_id, std::move(bounds));
 }
 
 Status ChromeImpl::MaximizeWindow(const std::string& target_id) {
@@ -201,7 +201,7 @@ Status ChromeImpl::MaximizeWindow(const std::string& target_id) {
 
   auto bounds = std::make_unique<base::DictionaryValue>();
   bounds->SetString("windowState", "maximized");
-  return SetWindowBounds(&window, std::move(bounds));
+  return SetWindowBounds(&window, target_id, std::move(bounds));
 }
 
 Status ChromeImpl::MinimizeWindow(const std::string& target_id) {
@@ -215,7 +215,7 @@ Status ChromeImpl::MinimizeWindow(const std::string& target_id) {
 
   auto bounds = std::make_unique<base::DictionaryValue>();
   bounds->SetString("windowState", "minimized");
-  return SetWindowBounds(&window, std::move(bounds));
+  return SetWindowBounds(&window, target_id, std::move(bounds));
 }
 
 Status ChromeImpl::FullScreenWindow(const std::string& target_id) {
@@ -229,7 +229,7 @@ Status ChromeImpl::FullScreenWindow(const std::string& target_id) {
 
   auto bounds = std::make_unique<base::DictionaryValue>();
   bounds->SetString("windowState", "fullscreen");
-  return SetWindowBounds(&window, std::move(bounds));
+  return SetWindowBounds(&window, target_id, std::move(bounds));
 }
 
 Status ChromeImpl::SetWindowRect(const std::string& target_id,
@@ -257,7 +257,7 @@ Status ChromeImpl::SetWindowRect(const std::string& target_id,
     bounds->SetInteger("height", height);
   }
 
-  return SetWindowBounds(&window, std::move(bounds));
+  return SetWindowBounds(&window, target_id, std::move(bounds));
 }
 
 Status ChromeImpl::GetWindowSize(const std::string& target_id,
@@ -291,6 +291,7 @@ Status ChromeImpl::GetWindowBounds(int window_id, Window* window) {
 
 Status ChromeImpl::SetWindowBounds(
     Window* window,
+    const std::string& target_id,
     std::unique_ptr<base::DictionaryValue> bounds) {
   Status status = devtools_websocket_client_->ConnectIfNecessary();
   if (status.IsError())
@@ -318,6 +319,25 @@ Status ChromeImpl::SetWindowBounds(
   if (!bounds->GetString("windowState", &state))
     return Status(kOk);
 
+  if (state == "fullscreen") {
+    // Work around crbug.com/982071. This block of code is necessary to ensure
+    // that document.webkitIsFullScreen and document.fullscreenElement return
+    // the correct values.
+    WebView* web_view;
+    status = GetWebViewById(target_id, &web_view);
+    if (status.IsError())
+      return status;
+
+    base::DictionaryValue params;
+    params.SetString("expression",
+                     "document.documentElement.requestFullscreen()");
+    params.SetBoolean("userGesture", true);
+    params.SetBoolean("awaitPromise", true);
+    status = web_view->SendCommand("Runtime.evaluate", params);
+    if (status.IsError())
+      return status;
+  }
+
   status = GetWindowBounds(window->id, window);
   if (status.IsError())
     return status;
@@ -331,9 +351,10 @@ Status ChromeImpl::SetWindowBounds(
     // to equal to screen size. This is accordance with the W3C spec at
     // https://www.w3.org/TR/webdriver1/#dfn-maximize-the-window.
     // Get a WebView, then use it to send JavaScript to query screen size.
-    if (web_views_.size() == 0)
-      return Status(kUnknownError, "no WebView");
-    WebView* web_view = web_views_.begin()->get();
+    WebView* web_view;
+    status = GetWebViewById(target_id, &web_view);
+    if (status.IsError())
+      return status;
     std::unique_ptr<base::Value> result;
     status = web_view->EvaluateScript(
         std::string(),
@@ -372,7 +393,7 @@ Status ChromeImpl::SetWindowSize(const std::string& target_id,
   auto bounds = std::make_unique<base::DictionaryValue>();
   bounds->SetInteger("width", width);
   bounds->SetInteger("height", height);
-  return SetWindowBounds(&window, std::move(bounds));
+  return SetWindowBounds(&window, target_id, std::move(bounds));
 }
 
 Status ChromeImpl::ParseWindow(std::unique_ptr<base::DictionaryValue> params,
