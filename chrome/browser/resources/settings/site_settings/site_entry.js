d558456979f154dc2a30279d8abc70def80c1eff
Jarryd <jarrydg@chromium.org>
2019-12-03 01:33:26 +0000

    AllSites: Show overflow menu for all site entries.
    The current All Sites page forces users to go into site
    details to clear data for any site with only 1 origin
    listed in the siteGroup. This change saves the user from
    having to do so, and lets them clear data and reset
    permissions directly from the all sites page.
    
    Bug: 997258
    Change-Id: I186439d73330f8489149f849e68dfc98db2b26c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1884579
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#720744}

--- chrome/browser/resources/settings/site_settings/site_entry.js
+++ chrome/browser/resources/settings/site_settings/site_entry.js
@@ -79,7 +79,7 @@ Polymer({
      * The selected sort method.
      * @type {!settings.SortMethod|undefined}
      */
-    sortMethod: {type: String, observer: 'updateOrigins_'}
+    sortMethod: {type: String, observer: 'updateOrigins_'},
   },
 
   /** @private {?settings.LocalDataBrowserProxy} */
@@ -124,6 +124,17 @@ Polymer({
       return true;
     }
     return false;
+  },
+
+  /**
+   * Whether or not to display the overflow menu for a site group.
+   * @param {SiteGroup} siteGroup The eTLD+1 group of origins.
+   * @return {boolean}
+   * @private
+   */
+  shouldHideOverflow_: function(siteGroup) {
+    return !this.grouped_(siteGroup) &&
+        !loadTimeData.getBoolean('enableStoragePressureUI');
   },
 
   /**
