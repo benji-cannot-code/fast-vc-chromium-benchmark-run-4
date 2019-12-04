feb53526ca01a30548e6292b0f42f972574dd820
Keishi Hattori <keishi@chromium.org>
2019-12-04 06:38:09 +0000

    Change ScriptValue back to use SharedPersistent
    ScriptValue was modified to use WorldSafeV8Reference through r701030,
    r698399, r694681 but we suspect it may have caused a memory leak.
    This change ScriptValue back to use SharedPersistent.
    To maintain the same level of security, the world check from
    WorldSafeV8Reference has been transplanted into SharedPersistent.
    
    Bug: 1029738
    Change-Id: I21ed2929c3e90530a958be0503f153646f85ec7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948671
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721385}

--- third_party/blink/renderer/bindings/core/v8/script_controller.h
+++ third_party/blink/renderer/bindings/core/v8/script_controller.h
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/window_proxy_manager.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/platform/bindings/shared_persistent.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_loader_options.h"
 #include "third_party/blink/renderer/platform/loader/fetch/script_fetch_options.h"
