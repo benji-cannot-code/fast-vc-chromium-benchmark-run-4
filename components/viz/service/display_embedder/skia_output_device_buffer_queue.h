eb5e2a8f1956ac757f7919f5184b665f0bfc0cd3
Peng Huang <penghuang@chromium.org>
2019-12-04 15:43:21 +0000

    Support overlays with SkiaOutputDeviceBufferQueue
    This CL supports overlays with SkiaOutputDeviceBufferQueue via
    GLSurface. However this CL doesn't change the code in
    features::IsUsingSkiaRenderer() at [1], so SkiaRenderer is not used with
    Android surface control.
    
    [1] https://cs.chromium.org/chromium/src/components/viz/common/features.cc?type=cs&sq=package:chromium&g=0&l=76
    
    TODO:
     * Refactor overlay related code out of GLSurface to a new class. those
       code is not GL related.
     * Use the new class to replace gl_surface_ in SkiaOutputDeviceBufferQueue.
     * Turn on SkiaRenderer with Android surface control.
     * Long term, we plan to use viz::BufferQueue for overlays. We need refactor
       overlay related code from SkiaOutputDevice* to a service side
       BufferQueue on GPU thread.
    
    Bug: 1012401
    Change-Id: Ic66072cad8df72a1e4375a895ac4af5fde42a5f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949270
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721491}

--- components/viz/service/display_embedder/skia_output_device_buffer_queue.h
+++ components/viz/service/display_embedder/skia_output_device_buffer_queue.h
@@ -55,12 +55,14 @@ class VIZ_SERVICE_EXPORT SkiaOutputDeviceBufferQueue final
   gl::GLImage* GetOverlayImage() override;
   // Creates and submits gpu fence
   std::unique_ptr<gfx::GpuFence> SubmitOverlayGpuFence() override;
+  void ScheduleOverlays(SkiaOutputSurface::OverlayList overlays) override;
 
   gl::GLSurface* gl_surface() { return gl_surface_.get(); }
 
  private:
   friend class SkiaOutputDeviceBufferQueueTest;
   class Image;
+  class OverlayData;
 
   Image* GetCurrentImage();
   std::unique_ptr<Image> GetNextImage();
@@ -70,8 +72,9 @@ class VIZ_SERVICE_EXPORT SkiaOutputDeviceBufferQueue final
   // operation
   void DoFinishSwapBuffers(const gfx::Size& size,
                            std::vector<ui::LatencyInfo> latency_info,
+                           std::vector<OverlayData> overlays,
                            gfx::SwapResult result,
-                           std::unique_ptr<gfx::GpuFence>);
+                           std::unique_ptr<gfx::GpuFence> gpu_fence);
 
   SkiaOutputSurfaceDependency* const dependency_;
   scoped_refptr<gl::GLSurface> gl_surface_;
@@ -91,6 +94,10 @@ class VIZ_SERVICE_EXPORT SkiaOutputDeviceBufferQueue final
   // Entries of this deque may be nullptr, if they represent frames that have
   // been destroyed.
   base::circular_deque<std::unique_ptr<Image>> in_flight_images_;
+  // Scheduled overlays for the next SwapBuffers call.
+  std::vector<OverlayData> pending_overlays_;
+  // Committed overlays for the last SwapBuffers call.
+  std::vector<OverlayData> committed_overlays_;
 
   // Shared Image factories
   gpu::SharedImageFactory shared_image_factory_;
