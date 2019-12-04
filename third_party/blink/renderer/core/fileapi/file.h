6fcfe164751101ead3b7e43c6a78859b5cce5b9f
Kent Tamura <tkent@chromium.org>
2019-12-04 06:57:28 +0000

    File: Add more timestamp tests
    Add timestamp tests for blob-backing files and filesystem files.
    This CL makes File::LastModifiedTime public because many tests access
    it.
    
    This CL has no behavior changes.
    
    Bug: 1027959
    Change-Id: I86ff3788f44edf830b1372bc3b17c3a951a77803
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948666
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721388}

--- third_party/blink/renderer/core/fileapi/file.h
+++ third_party/blink/renderer/core/fileapi/file.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FILEAPI_FILE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FILEAPI_FILE_H_
 
-#include "base/gtest_prod_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/optional.h"
 #include "base/time/time.h"
@@ -203,6 +202,10 @@ class CORE_EXPORT File final : public Blob {
   // http://www.w3.org/TR/FileAPI/#dfn-lastModifiedDate
   ScriptValue lastModifiedDate(ScriptState* script_state) const;
 
+  // Returns File's last modified time.
+  // If the modification time isn't known, the current time is returned.
+  base::Time LastModifiedTime() const;
+
   UserVisibility GetUserVisibility() const { return user_visibility_; }
 
   // Returns the relative path of this file in the context of a directory
@@ -229,10 +232,6 @@ class CORE_EXPORT File final : public Blob {
  private:
   void InvalidateSnapshotMetadata() { snapshot_size_.reset(); }
 
-  // Returns File's last modified time.
-  // If the modification time isn't known, the current time is returned.
-  base::Time LastModifiedTime() const;
-
 #if DCHECK_IS_ON()
   // Instances backed by a file must have an empty file system URL.
   bool HasValidFileSystemURL() const {
@@ -257,14 +256,6 @@ class CORE_EXPORT File final : public Blob {
   const base::Optional<base::Time> snapshot_modification_time_;
 
   String relative_path_;
-
-  FRIEND_TEST_ALL_PREFIXES(FileTest, NativeFileWithoutTimestamp);
-  FRIEND_TEST_ALL_PREFIXES(FileTest, NativeFileWithUnixEpochTimestamp);
-  FRIEND_TEST_ALL_PREFIXES(FileTest, NativeFileWithApocalypseTimestamp);
-  FRIEND_TEST_ALL_PREFIXES(V8ScriptValueSerializerTest,
-                           DecodeFileV4WithSnapshot);
-  FRIEND_TEST_ALL_PREFIXES(V8ScriptValueSerializerTest,
-                           DecodeFileV8WithSnapshot);
 };
 
 template <>
