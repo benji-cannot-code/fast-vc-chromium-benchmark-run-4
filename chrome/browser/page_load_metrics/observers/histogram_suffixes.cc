0543fb38373019026f31f9dfe2e40bf01e5f1f3c
Francois Doray <fdoray@chromium.org>
2019-12-04 13:10:38 +0000

    [metrics] Record PageLoad.InteractiveTiming.FirstInputDelay4.ByLiveTabCount.*
    With the launch of the SkipTouchEventFilter feature,
    PageLoad.InteractiveTiming.FirstInputDelay3 is no longer recorded and
    was marked as obsolete.
    
    This CL updates the code that used to record the
    PageLoad.InteractiveTiming.FirstInputDelay3.ByLiveTabCount.* histograms
    to instead record
    PageLoad.InteractiveTiming.FirstInputDelay4.ByLiveTabCount.*.
    The same data source is used to record non-suffixed and suffixed versions
    of the PageLoad.InteractiveTiming.FirstInputDelay4 histogram.
    
    Change-Id: I74a3439d03e4cfa748711e337af680230baa05e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1932092
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721450}

--- chrome/browser/page_load_metrics/observers/histogram_suffixes.cc
+++ chrome/browser/page_load_metrics/observers/histogram_suffixes.cc
@@ -16,7 +16,7 @@ const char kHistogramFirstContentfulPaintSuffix[] =
 const char kHistogramFirstImagePaintSuffix[] =
     "PaintTiming.NavigationToFirstImagePaint";
 const char kHistogramFirstInputDelaySuffix[] =
-    "InteractiveTiming.FirstInputDelay3";
+    "InteractiveTiming.FirstInputDelay4";
 const char kHistogramFirstLayoutSuffix[] =
     "DocumentTiming.NavigationToFirstLayout";
 const char kHistogramFirstMeaningfulPaintSuffix[] =
