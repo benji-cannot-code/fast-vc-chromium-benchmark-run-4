6ced384576b97343d51755401b15b7d9e41d8178
Mike Dougherty <michaeldo@chromium.org>
2019-12-04 17:07:41 +0000

    Disable FindInPage tests for EG2 when run on iOS 12
    TBR=justincohen@chromium.org
    
    Bug: 1030701
    Change-Id: I9f7ffcccf5484514d22121defe458ada25758d29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950667
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721516}

--- ios/chrome/browser/ui/find_bar/find_in_page_egtest.mm
+++ ios/chrome/browser/ui/find_bar/find_in_page_egtest.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <XCTest/XCTest.h>
 
+#include "base/ios/ios_util.h"
 #include "base/strings/string_number_conversions.h"
 #import "base/test/ios/wait_util.h"
 #include "components/strings/grit/components_strings.h"
@@ -103,6 +104,13 @@ const std::string kFindInPageResponse = "Find in page. Find in page.";
 // Tests that find in page allows iteration between search results and displays
 // correct number of results.
 - (void)testFindInPage {
+// TODO(crbug.com/1030701): Test fails on iOS 12 when run as EG2 test.
+#if defined(CHROME_EARL_GREY_2)
+  if (!base::ios::IsRunningOnOrLater(13, 0, 0)) {
+    EARL_GREY_TEST_DISABLED(@"Fails on iOS 12.0.");
+  }
+#endif
+
   // Type "find".
   [self typeFindInPageText:@"find"];
   // Should be highlighting result 1 of 2.
@@ -161,6 +169,13 @@ const std::string kFindInPageResponse = "Find in page. Find in page.";
 
 // Tests accessibility of the Find in Page screen.
 - (void)testAccessibilityOnFindInPage {
+// TODO(crbug.com/1030701): Test fails on iOS 12 when run as EG2 test.
+#if defined(CHROME_EARL_GREY_2)
+  if (!base::ios::IsRunningOnOrLater(13, 0, 0)) {
+    EARL_GREY_TEST_DISABLED(@"Fails on iOS 12.0.");
+  }
+#endif
+
   [self typeFindInPageText:@"find"];
 
   // Wait for UI to finish loading screen, before programatically verifying
