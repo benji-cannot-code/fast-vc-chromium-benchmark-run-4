c78374b3f6d3a0d34d3114a3700de5bbb8d75c75
Zhenyao Mo <zmo@chromium.org>
2019-12-04 02:20:24 +0000

    When shutting down GPU process due to context lost, do it immediately.
    Do it in an atomic way rather than cleanly releasing resources, etc.
    
    The latter has been the source of many hangs and crashes. It's not
    worth the efforts.
    
    When browser process exits, I believe we already shuts down GPU
    process atomically.
    
    After this change, the only clean exit for GPU process is the
    unsandboxed GPU process that is to collect extra GPU information.
    It needs to shut down cleanly to make sure the info collected
    has been received by browser process.
    
    BUG=896565,1018233,1027137
    TEST=manual
    R=kbr@chromium.org,sunnyps@chromium.org
    
    Change-Id: I082a06945d261db0fb9a4549859adf1e057212ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949671
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721319}

--- components/viz/service/gl/gpu_service_impl.h
+++ components/viz/service/gl/gpu_service_impl.h
@@ -89,7 +89,7 @@ class VIZ_SERVICE_EXPORT GpuServiceImpl : public gpu::GpuChannelManagerDelegate,
                      gpu_feature_info_for_hardware_gpu,
                  const gpu::GpuExtraInfo& gpu_extra_info,
                  gpu::VulkanImplementation* vulkan_implementation,
-                 base::OnceClosure exit_callback);
+                 base::OnceCallback<void(bool /*immediately*/)> exit_callback);
 
   ~GpuServiceImpl() override;
 
@@ -369,7 +369,7 @@ class VIZ_SERVICE_EXPORT GpuServiceImpl : public gpu::GpuChannelManagerDelegate,
   base::WaitableEvent* shutdown_event_ = nullptr;
 
   // Callback that safely exits GPU process.
-  base::OnceClosure exit_callback_;
+  base::OnceCallback<void(bool)> exit_callback_;
   base::AtomicFlag is_exiting_;
 
   // Used for performing hardware decode acceleration of images. This is shared
--- components/viz/service/main/viz_main_impl.h
+++ components/viz/service/main/viz_main_impl.h
@@ -110,7 +110,7 @@ class VizMainImpl : public mojom::VizMain {
   }
 
   // Cleanly exits the process.
-  void ExitProcess();
+  void ExitProcess(bool immediately);
 
  private:
   void CreateFrameSinkManagerInternal(mojom::FrameSinkManagerParamsPtr params);
