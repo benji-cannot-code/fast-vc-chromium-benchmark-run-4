cbba2a1f5058985afcb575dc56c6add7ee2c0588
Sergio Villar Senin <svillar@igalia.com>
2019-12-04 13:36:47 +0000

    Remove WebEditingCommandType from Blink's API surface
    It's only used inside Blink so it does not make sense to expose it. As it is no
    longer part of the public API, we're renaming WebEditingCommandType to
    EditingCommandType.
    
    Bug: 919392
    Change-Id: I22e9ffa7ac9717d57cfc006ee7061ea8eb1ab0d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949793
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#721457}

--- third_party/blink/renderer/core/events/input_event.cc
+++ third_party/blink/renderer/core/events/input_event.cc
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/events/input_event.h"
 
 #include "base/stl_util.h"
-#include "third_party/blink/public/platform/web_editing_command_type.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatcher.h"
 #include "third_party/blink/renderer/core/dom/range.h"
+#include "third_party/blink/renderer/core/editing/commands/editing_command_type.h"
 
 namespace blink {
 
