e4ab48a4d73d918fee1ef4bb1041e1f3038a0c1e
Sean Harrison <harrisonsean@chromium.org>
2019-12-04 18:57:43 +0000

    Friendly Privacy Setting Initial Redesign: Reland
    Changes between original submission and reland:
    
    1.) Privacy-page subpages are now in lazy_load.html
    2.) Fixed bug that erroneously showed "privacy" in the settings menu of "Guest" mode settings.
    3.) Fixed bug where going to chrome://settings/clearBrowsingData didn't work unless the advanced section was expanded when the flag for this change was disabled.
    4.) Reordered a few settings rows to match the most current mocks.
    
    > Revert "Friendly Privacy Setting Initial Redesign"
    
    > This reverts commit 544412a4702f0ce478a409f7eea5c814c5a26a3a.
    
    > Reason for revert: Performance and functional regressions
    > https://bugs.chromium.org/p/chromium/issues/detail?id=1024034#c5
    
    > Original change's description:
    >> Friendly Privacy Setting Initial Redesign
    >>
    >> This CL does 3 things:
    >> 1) The order of rows in the privacy page is affected, regardless of the value of the "privacySettingsRedesignEnabled" flag.
    >> 2) The privacy-page is moved to the "basic", only when the "privacySettingsRedesignEnabled" flag is on.
    >> 3) A "more" expansion button is introduced in the privacy-page only when the "privacySettingsRedesignEnabled" flag.
    >>
    >> Bug: 1014143
    >> Change-Id: I678e81aa3b67acd2ae41eeda355cd3b32a5b4088
    >> Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1872094
    >> Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    >> Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    >> Cr-Commit-Position: refs/heads/master@{#714287}
    
    Change-Id: If3fa33801604491b8648672ff53d37a818f2cd08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929443
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721579}

--- chrome/browser/resources/settings/privacy_page/privacy_page.js
+++ chrome/browser/resources/settings/privacy_page/privacy_page.js
@@ -101,14 +101,6 @@ Polymer({
       value: false,
     },
 
-    /** @private */
-    privacySettingsRedesignEnabled_: {
-      type: Boolean,
-      value: function() {
-        return loadTimeData.getBoolean('privacySettingsRedesignEnabled');
-      },
-    },
-
     /**
      * Used for HTML bindings. This is defined as a property rather than within
      * the ready callback, because the value needs to be available before
@@ -126,6 +118,25 @@ Polymer({
       value: function() {
         return loadTimeData.getBoolean('enableSafeBrowsingSubresourceFilter');
       }
+    },
+
+    /** @private */
+    privacySettingsRedesignEnabled_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('privacySettingsRedesignEnabled');
+      },
+    },
+
+    /**
+     * Whether the more settings list is opened.
+     * @private
+     */
+    moreOpened_: {
+      type: Boolean,
+      value: function() {
+        return !loadTimeData.getBoolean('privacySettingsRedesignEnabled');
+      },
     },
 
     /** @private */
@@ -489,6 +500,22 @@ Polymer({
   onRestartTap_: function(e) {
     e.stopPropagation();
     settings.LifetimeBrowserProxyImpl.getInstance().restart();
+  },
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getIronCollapseCssClass_: function() {
+    return this.privacySettingsRedesignEnabled_ ? 'iron-collapse-indented' : '';
+  },
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getTopBarCssClass_: function() {
+    return this.privacySettingsRedesignEnabled_ ? 'settings-box first' : '';
   },
 });
 })();
