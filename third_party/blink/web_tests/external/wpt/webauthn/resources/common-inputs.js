e71ada38d60c8852c9ed278374c91c776081aefd
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-12-04 23:39:48 +0000

    Import wpt@eca8c168b2cb04f3f1bf223ca653640df7933c58
    Using wpt-import in Chromium 457ad5ab9aa0a2a79d5c80055f8d906f9b5741e5.
    With Chromium commits locally applied on WPT:
    500a74f5cc "Add a runtime flag to enforce strict MIME type checks for workers."
    a2a91a9912 "Allow another touch start happen when there are active touch points"
    85b61e6b69 "[resource-timing] Ensure workerStart is protected by TAO"
    a75430aaa2 "Fix appcache tests."
    a50e7e609f "Move `Sec-Fetch-User` to SetSecFetchMetadataHeaders."
    83e4a43b7f "Refactor VRPose to fit better with WebXr concepts"
    a93be69b51 "Update reversing-an-animation test"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: I78951794b58a7cb400743e107a6f78b08a828a9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950889
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#721734}

---
+++ third_party/blink/web_tests/external/wpt/webauthn/resources/common-inputs.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const ES256_ID = -7;
+const CHALLENGE = "climb the mountain";
+
+const PUBLIC_KEY_RP = {
+    id: window.location.hostname,
+    name: "Example RP",
+};
+
+const PUBLIC_KEY_USER = {
+    id: new TextEncoder().encode("123456789"),
+    name: "madeline@example.com",
+    displayName: "Madeline",
+    icon: "https://example.com/celeste.png"
+};
+
+// ES256.
+const PUBLIC_KEY_PARAMETERS =  [{
+    type: "public-key",
+    alg: ES256_ID,
+}];
+
+const AUTHENTICATOR_SELECTION_CRITERIA = {
+    requireResidentKey: false,
+    userVerification: "discouraged",
+};
+
+const MAKE_CREDENTIAL_OPTIONS = {
+    challenge: new TextEncoder("utf-8").encode(CHALLENGE),
+    rp: PUBLIC_KEY_RP,
+    user: PUBLIC_KEY_USER,
+    pubKeyCredParams: PUBLIC_KEY_PARAMETERS,
+    authenticatorSelection: AUTHENTICATOR_SELECTION_CRITERIA,
+    excludeCredentials: [],
+};
