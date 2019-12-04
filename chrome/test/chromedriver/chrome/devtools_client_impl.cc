f520e9a4133dc25ed8e7e41a0d53208c74f387f3
Tricia Crichton <triciac@chromium.org>
2019-12-04 20:29:54 +0000

    [ChromeDriver] Add alert text to UnexpectedAlertOpen
    Several places where UnexpectedAlertOpen status is returned do not
    include the alert text in the status object. This cl adds the text where
    missing.
    
    Bug: chromedriver:3184
    Change-Id: I3087ed77d73c326ca93c77667c2a9eaa7c460134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950862
    Commit-Queue: Tricia Crichton <triciac@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721641}

--- chrome/test/chromedriver/chrome/devtools_client_impl.cc
+++ chrome/test/chromedriver/chrome/devtools_client_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
 #include "chrome/test/chromedriver/chrome/devtools_event_listener.h"
+#include "chrome/test/chromedriver/chrome/javascript_dialog_manager.h"
 #include "chrome/test/chromedriver/chrome/log.h"
 #include "chrome/test/chromedriver/chrome/status.h"
 #include "chrome/test/chromedriver/chrome/util.h"
@@ -362,6 +363,15 @@ Status DevToolsClientImpl::SendCommandInternal(
       }
       if (response_info->state == kBlocked) {
         response_info->state = kIgnored;
+        if (owner_) {
+          std::string alert_text;
+          Status status =
+              owner_->GetJavaScriptDialogManager()->GetDialogMessage(
+                  &alert_text);
+          if (status.IsOk())
+            return Status(kUnexpectedAlertOpen,
+                          "{Alert text : " + alert_text + "}");
+        }
         return Status(kUnexpectedAlertOpen);
       }
       CHECK_EQ(response_info->state, kReceived);
