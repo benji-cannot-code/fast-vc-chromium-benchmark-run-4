8d14e102fcf9d54a0152259ac5ae4665804521e6
Sorin Jianu <sorin@chromium.org>
2019-12-04 18:43:51 +0000

    Remove recovery component header from chrome_component_updater_configurator.cc
    Bug: 1027610
    Change-Id: I6e2a7914a6f8694185f33e49d59a06cff21ef598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947557
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721566}

--- chrome/browser/component_updater/chrome_component_updater_configurator.cc
+++ chrome/browser/component_updater/chrome_component_updater_configurator.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/component_updater/component_updater_utils.h"
-#include "chrome/browser/component_updater/recovery_improved_component_installer.h"
 #include "chrome/browser/google/google_brand.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/update_client/chrome_update_query_params_delegate.h"
