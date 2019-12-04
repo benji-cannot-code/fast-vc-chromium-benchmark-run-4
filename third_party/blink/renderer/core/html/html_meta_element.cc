e8bc87a0b45b0a3d4a24e5d3b3735c722ad94801
David Van Cleve <davidvc@chromium.org>
2019-12-04 13:36:50 +0000

    Add a UseCounter for <meta name="referrer">.
    We currently count page-level referrer policies set by the
    Referrer-Policy header, but there's no corresponding counter for
    page-scoped policies set by the <meta> tag.
    
    Change-Id: I6f614fcc28dc58be07a029d06363325841912af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948491
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721458}

--- third_party/blink/renderer/core/html/html_meta_element.cc
+++ third_party/blink/renderer/core/html/html_meta_element.cc
@@ -575,6 +575,8 @@ void HTMLMetaElement::ProcessContent() {
     ProcessViewportContentAttribute(content_value,
                                     ViewportDescription::kViewportMeta);
   } else if (EqualIgnoringASCIICase(name_value, "referrer")) {
+    UseCounter::Count(&GetDocument(),
+                      WebFeature::kHTMLMetaElementReferrerPolicy);
     GetDocument().ParseAndSetReferrerPolicy(content_value,
                                             true /* support legacy keywords */);
   } else if (EqualIgnoringASCIICase(name_value, "handheldfriendly") &&
