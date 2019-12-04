cc91f27389ba662e6ad8753ef03ca5ace5f19400
Mohamed Amir Yosef <mamir@chromium.org>
2019-12-04 14:04:12 +0000

    Revert "Install PluginVM DLC"
    This reverts commit 6c72bce20f38c7da25fb7becfdf8a1da190efe7f.
    
    Reason for revert: Caused test failures on linux-chromeos-dbg 
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg/15583
    
    Original change's description:
    > Install PluginVM DLC
    > 
    > PluginVM DLC should be installed when the PluginVM tools are not rootfs
    > present and skip/handle the install when rootfs resident.
    > 
    > The call to |PathExists()| is not used as it is a blocking call on the
    > UI thread, instead the dlcservice error indicating an invalid DLC is
    > used to determine whether PluginVM has been enabled as a DLC (rootfs
    > resident or not).
    > 
    > Bug: b:143104338
    > Test: Manual startup of PluginVM
    > Change-Id: Id028711c98cc4cc91ef1e0b234cbf48084cc3eb3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924723
    > Commit-Queue: Timothy Loh <timloh@chromium.org>
    > Auto-Submit: Jae Hoon Kim <kimjae@chromium.org>
    > Reviewed-by: Timothy Loh <timloh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#721333}
    
    TBR=timloh@chromium.org,kimjae@chromium.org
    
    Change-Id: I4d21b8264bbde9151511a77788c692298d5d7f15
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:143104338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950507
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721461}

--- chrome/browser/ui/views/plugin_vm/plugin_vm_launcher_view.cc
+++ chrome/browser/ui/views/plugin_vm/plugin_vm_launcher_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/chromeos/plugin_vm/plugin_vm_image_manager.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_image_manager_factory.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_manager.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_metrics_util.h"
@@ -176,8 +177,6 @@ PluginVmLauncherView* PluginVmLauncherView::GetActiveViewForTesting() {
 
 int PluginVmLauncherView::GetDialogButtons() const {
   switch (state_) {
-    case State::START_DLC_DOWNLOADING:
-    case State::DOWNLOADING_DLC:
     case State::START_DOWNLOADING:
     case State::DOWNLOADING:
     case State::IMPORTING:
@@ -198,8 +197,6 @@ int PluginVmLauncherView::GetDialogButtons() const {
 base::string16 PluginVmLauncherView::GetDialogButtonLabel(
     ui::DialogButton button) const {
   switch (state_) {
-    case State::START_DLC_DOWNLOADING:
-    case State::DOWNLOADING_DLC:
     case State::START_DOWNLOADING:
     case State::DOWNLOADING:
     case State::IMPORTING: {
@@ -244,13 +241,6 @@ bool PluginVmLauncherView::Accept() {
 }
 
 bool PluginVmLauncherView::Cancel() {
-  if (state_ == State::DOWNLOADING_DLC ||
-      state_ == State::START_DLC_DOWNLOADING) {
-    plugin_vm_image_manager_->CancelDlcDownload();
-
-    plugin_vm::RecordPluginVmSetupResultHistogram(
-        plugin_vm::PluginVmSetupResult::kUserCancelledDownloadingPluginVmDlc);
-  }
   if (state_ == State::DOWNLOADING || state_ == State::START_DOWNLOADING) {
     plugin_vm_image_manager_->CancelDownload();
 
@@ -271,39 +261,8 @@ gfx::Size PluginVmLauncherView::CalculatePreferredSize() const {
   return gfx::Size(kWindowWidth, kWindowHeight);
 }
 
-void PluginVmLauncherView::OnDlcDownloadStarted() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-
-  state_ = State::DOWNLOADING_DLC;
-  OnStateUpdated();
-}
-
-void PluginVmLauncherView::OnDlcDownloadProgressUpdated(
-    double progress,
-    base::TimeDelta elapsed_time) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  DCHECK_EQ(state_, State::DOWNLOADING_DLC);
-
-  UpdateOperationProgress(progress * 100, 100.0, elapsed_time);
-}
-
-void PluginVmLauncherView::OnDlcDownloadCompleted() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  DCHECK_EQ(state_, State::DOWNLOADING_DLC);
-
-  state_ = State::START_DOWNLOADING;
-  OnStateUpdated();
-
-  plugin_vm_image_manager_->StartDownload();
-}
-
-void PluginVmLauncherView::OnDlcDownloadCancelled() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-}
-
 void PluginVmLauncherView::OnDownloadStarted() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  DCHECK_EQ(state_, State::START_DOWNLOADING);
 
   state_ = State::DOWNLOADING;
   OnStateUpdated();
@@ -388,8 +347,6 @@ void PluginVmLauncherView::OnImported() {
 
 base::string16 PluginVmLauncherView::GetBigMessage() const {
   switch (state_) {
-    case State::START_DLC_DOWNLOADING:
-    case State::DOWNLOADING_DLC:
     case State::START_DOWNLOADING:
     case State::DOWNLOADING:
     case State::IMPORTING:
@@ -411,8 +368,6 @@ base::string16 PluginVmLauncherView::GetBigMessage() const {
 
 base::string16 PluginVmLauncherView::GetMessage() const {
   switch (state_) {
-    case State::START_DLC_DOWNLOADING:
-    case State::DOWNLOADING_DLC:
     case State::START_DOWNLOADING:
       return l10n_util::GetStringUTF16(
           IDS_PLUGIN_VM_LAUNCHER_START_DOWNLOADING_MESSAGE);
@@ -470,10 +425,9 @@ void PluginVmLauncherView::OnStateUpdated() {
   SetMessageLabel();
   SetBigImage();
 
-  const bool progress_bar_visible =
-      state_ == State::START_DLC_DOWNLOADING ||
-      state_ == State::DOWNLOADING_DLC || state_ == State::START_DOWNLOADING ||
-      state_ == State::DOWNLOADING || state_ == State::IMPORTING;
+  const bool progress_bar_visible = state_ == State::START_DOWNLOADING ||
+                                    state_ == State::DOWNLOADING ||
+                                    state_ == State::IMPORTING;
   progress_bar_->SetVisible(progress_bar_visible);
   // Values outside the range [0,1] display an infinite loading animation.
   progress_bar_->SetValue(-1);
@@ -523,8 +477,7 @@ void PluginVmLauncherView::UpdateOperationProgress(
     double units_processed,
     double total_units,
     base::TimeDelta elapsed_time) const {
-  DCHECK(state_ == State::DOWNLOADING_DLC || state_ == State::DOWNLOADING ||
-         state_ == State::IMPORTING);
+  DCHECK(state_ == State::DOWNLOADING || state_ == State::IMPORTING);
 
   base::Optional<double> maybe_fraction_complete =
       GetFractionComplete(units_processed, total_units);
@@ -581,9 +534,9 @@ void PluginVmLauncherView::StartPluginVmImageDownload() {
   // retry button is clicked).
   setup_start_tick_ = base::TimeTicks::Now();
 
-  state_ = State::START_DLC_DOWNLOADING;
+  state_ = State::START_DOWNLOADING;
   OnStateUpdated();
 
   plugin_vm_image_manager_->SetObserver(this);
-  plugin_vm_image_manager_->StartDlcDownload();
+  plugin_vm_image_manager_->StartDownload();
 }
