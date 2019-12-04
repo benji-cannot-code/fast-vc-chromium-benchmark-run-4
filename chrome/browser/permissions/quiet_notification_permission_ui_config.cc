fc631f5440a1599be36bb6c912d398993b1e3d7a
Balazs Engedy <engedy@chromium.org>
2019-12-04 15:11:45 +0000

    Reland "Socialize QuietNotificationPrompt prefs."
    This is a reland of c8b3dc82c01b009efffc329a3ede74e0fef22479.
    
    The previous attempt to land led to a compile error, because it clashed with a
    concurrently landed CL (crrev.com/c/1944173), which added an #include to
    `adaptive_notification_permission_ui_selector.h`, which is renamed by this CL.
    
    Relanding this verbatim, the other CL will be rebased on top of this one.
    
    Original change's description:
    > Socialize QuietNotificationPrompt prefs.
    >
    > Decouple call sites that wish to check the quiet UI state (or the promo
    > state) from AdaptiveNotificationPermissionUiSelector, and instead make
    > this call sites consult QuietNotificationPermissionUIState, which acts
    > a thin layer to read/write preferences.
    >
    > Rename AdaptiveNotificationPermissionUiSelector to
    > AdaptiveQuietNotificationPermissionUiEnabler to better reflect the new
    > responsibility of this class is to merely toggle a setting, the actual
    > UI will be selected by the PermissionRequestManager.
    >
    > This CL also renames `permission_features.h`, for consistency, to
    > `quiet_notification_permission_ui_config.h`.
    >
    > Bug: 1028642, 1030216
    > Change-Id: If52dfab3532a473399091bf4291e863cf70142f0
    > TBR: battre@chromium.org
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948892
    > Commit-Queue: Balazs Engedy <engedy@chromium.org>
    > Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    > Reviewed-by: Martin Šrámek <msramek@chromium.org>
    > Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#721449}
    
    Bug: 1028642, 1030216
    Change-Id: I6bf5a7fa51da10fd8b7a8cdd7eecca236fbefb1d
    TBR: msramek@chromium.org, chouinard@chromium.org, battre@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950526
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721479}

--- chrome/browser/permissions/quiet_notification_permission_ui_config.cc
+++ chrome/browser/permissions/quiet_notification_permission_ui_config.cc
@@ -3,30 +3,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/permissions/permission_features.h"
-
-#include <string>
+#include "chrome/browser/permissions/quiet_notification_permission_ui_config.h"
 
 #include "base/metrics/field_trial_params.h"
 #include "chrome/common/chrome_features.h"
 
-const char QuietNotificationsPromptConfig::kEnableAdaptiveActivation[] =
+// static
+const char QuietNotificationPermissionUiConfig::kEnableAdaptiveActivation[] =
     "enable_adaptive_activation";
 
-QuietNotificationsPromptConfig::UIFlavor
-QuietNotificationsPromptConfig::UIFlavorToUse() {
+QuietNotificationPermissionUiConfig::UiFlavor
+QuietNotificationPermissionUiConfig::UiFlavorToUse() {
   if (!base::FeatureList::IsEnabled(features::kQuietNotificationPrompts))
-    return UIFlavor::NONE;
+    return UiFlavor::NONE;
 
 #if defined(OS_ANDROID)
-  return UIFlavor::MINI_INFOBAR;
+  return UiFlavor::MINI_INFOBAR;
 #else   // OS_ANDROID
-  return UIFlavor::ANIMATED_ICON;
+  return UiFlavor::ANIMATED_ICON;
 #endif  // !OS_ANDROID
 }
 
 // static
-bool QuietNotificationsPromptConfig::IsAdaptiveActivationEnabled() {
+bool QuietNotificationPermissionUiConfig::IsAdaptiveActivationEnabled() {
   if (!base::FeatureList::IsEnabled(features::kQuietNotificationPrompts))
     return false;
 
--- chrome/browser/permissions/quiet_notification_permission_ui_config.h
+++ chrome/browser/permissions/quiet_notification_permission_ui_config.h
@@ -3,14 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PERMISSIONS_PERMISSION_FEATURES_H_
-#define CHROME_BROWSER_PERMISSIONS_PERMISSION_FEATURES_H_
+#ifndef CHROME_BROWSER_PERMISSIONS_QUIET_NOTIFICATION_PERMISSION_UI_CONFIG_H_
+#define CHROME_BROWSER_PERMISSIONS_QUIET_NOTIFICATION_PERMISSION_UI_CONFIG_H_
 
 #include "build/build_config.h"
 
-class QuietNotificationsPromptConfig {
+// Field trial configuration for the quiet notificaiton permission request UI.
+class QuietNotificationPermissionUiConfig {
  public:
-  enum UIFlavor {
+  enum UiFlavor {
     NONE,
 #if defined(OS_ANDROID)
     QUIET_NOTIFICATION,
@@ -27,7 +28,11 @@ class QuietNotificationsPromptConfig {
   // consecutive prompt denies.
   static const char kEnableAdaptiveActivation[];
 
-  static UIFlavor UIFlavorToUse();
+  // Which flavor of the quiet UI to use.
+  //
+  // Deprecated: The UI flavor is now hardcoded on each platform and no longer
+  // controlled by field trials.
+  static UiFlavor UiFlavorToUse();
 
   // Whether or not adaptive activation is enabled. Adaptive activation means
   // that quiet notifications permission prompts will be turned on after three
@@ -35,4 +40,4 @@ class QuietNotificationsPromptConfig {
   static bool IsAdaptiveActivationEnabled();
 };
 
-#endif  // CHROME_BROWSER_PERMISSIONS_PERMISSION_FEATURES_H_
+#endif  // CHROME_BROWSER_PERMISSIONS_QUIET_NOTIFICATION_PERMISSION_UI_CONFIG_H_
