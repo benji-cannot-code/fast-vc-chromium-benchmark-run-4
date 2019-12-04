fc1c2da6c1b44ff0a1ba85ccffb1ae00ed615eb2
Peter Kotwicz <pkotwicz@chromium.org>
2019-12-04 19:55:36 +0000

    Remove unneeded FullscreenOptions implementation of Parcelable
    This is unused as of
    https://chromium-review.googlesource.com/c/chromium/src/+/1382499
    
    BUG=None
    
    Change-Id: I0ed23f05fe2630cdaa2ab2cf7c904f3b0cb53ce6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949859
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721623}

--- chrome/android/java/src/org/chromium/chrome/browser/fullscreen/FullscreenOptions.java
+++ chrome/android/java/src/org/chromium/chrome/browser/fullscreen/FullscreenOptions.java
@@ -5,13 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.fullscreen;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Options to control a fullscreen request.
  */
-public class FullscreenOptions implements Parcelable {
+public class FullscreenOptions {
     private boolean mShowNavigationBar;
 
     /**
@@ -37,42 +34,5 @@ public class FullscreenOptions implements Parcelable {
         }
         FullscreenOptions options = (FullscreenOptions) obj;
         return mShowNavigationBar == options.mShowNavigationBar;
-    }
-
-    /**
-     * The Parcelable interface.
-     * */
-
-    public static final Parcelable.Creator<FullscreenOptions> CREATOR =
-            new Parcelable.Creator<FullscreenOptions>() {
-                @Override
-                public FullscreenOptions createFromParcel(Parcel in) {
-                    return new FullscreenOptions(in);
-                }
-
-                @Override
-                public FullscreenOptions[] newArray(int size) {
-                    return new FullscreenOptions[size];
-                }
-            };
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void writeToParcel(Parcel out, int flags) {
-        out.writeByte(mShowNavigationBar ? (byte) 1 : (byte) 0);
-    }
-
-    private FullscreenOptions(Parcel in) {
-        mShowNavigationBar = in.readByte() != 0;
     }
 }
