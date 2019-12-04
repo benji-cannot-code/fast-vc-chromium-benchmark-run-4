f890bf2b9b4107e142358d4ba15d56feb1015acd
Nate Chapin <japhet@chromium.org>
2019-12-04 19:26:56 +0000

    Make ScriptedAnimationController a ContextLifecycleStateObserver
    This removes the need for special-case pausing/unpausing in Document, as well
    as custom Document::Shutdown() logic.
    
    Bug: 1029822
    Change-Id: I2c9cda5bb3720626cdc1f3fcadb7876422ca375e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949275
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721599}

--- third_party/blink/renderer/core/dom/scripted_animation_controller.h
+++ third_party/blink/renderer/core/dom/scripted_animation_controller.h
@@ -29,8 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/frame_request_callback_collection.h"
+#include "third_party/blink/renderer/core/execution_context/context_lifecycle_state_observer.h"
 #include "third_party/blink/renderer/platform/bindings/name_client.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/wtf/casting.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_impl.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -44,16 +46,18 @@ class MediaQueryListListener;
 
 class CORE_EXPORT ScriptedAnimationController
     : public GarbageCollected<ScriptedAnimationController>,
+      public ContextLifecycleStateObserver,
       public NameClient {
+  USING_GARBAGE_COLLECTED_MIXIN(ScriptedAnimationController);
+
  public:
   explicit ScriptedAnimationController(Document*);
-  virtual ~ScriptedAnimationController() = default;
+  ~ScriptedAnimationController() override = default;
 
-  void Trace(Visitor*);
+  void Trace(Visitor*) override;
   const char* NameInHeapSnapshot() const override {
     return "ScriptedAnimationController";
   }
-  void ClearDocumentPointer() { document_ = nullptr; }
 
   // Animation frame callbacks are used for requestAnimationFrame().
   typedef int CallbackId;
@@ -83,8 +87,8 @@ class CORE_EXPORT ScriptedAnimationController
   void ServiceScriptedAnimations(base::TimeTicks monotonic_time_now);
   void RunPostFrameCallbacks();
 
-  void Pause();
-  void Unpause();
+  void ContextLifecycleStateChanged(mojom::FrameLifecycleState) final;
+  void Disable() { disabled_ = true; }
 
   void DispatchEventsAndCallbacksForPrinting();
 
@@ -102,9 +106,10 @@ class CORE_EXPORT ScriptedAnimationController
 
   bool HasScheduledFrameTasks() const;
 
-  Member<Document> document_;
+  Document* GetDocument() const { return To<Document>(GetExecutionContext()); }
+
   FrameRequestCallbackCollection callback_collection_;
-  int suspend_count_;
+  bool disabled_ = false;
   Vector<base::OnceClosure> task_queue_;
   HeapVector<Member<Event>> event_queue_;
   HeapListHashSet<std::pair<Member<const EventTarget>, const StringImpl*>>
