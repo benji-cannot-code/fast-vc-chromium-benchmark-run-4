579ca2581a06172460275cb92673427b4c89e90d
Joe DeBlasio <jdeblasio@chromium.org>
2019-12-04 22:49:58 +0000

    [Safety Tips] Streamline safety tip UX.
    This CL removes the 'ignore' button on Safety Tips. UX folks felt that
    this would be clearer and more consistent with existing patterns. Both
    the infobar (mobile) and bubble (desktop) variants have dedicated close
    (X) buttons that do the same thing.
    
    This leaves them looking like this:
    Desktop: https://drive.google.com/open?id=1e3UlzRa0g4kzr5p1-8dCO7Hfec3rWho7
    Android: https://drive.google.com/open?id=19OGdgjZsQ4khNErmG_W160vlhQrvDiz6
    
    Bug: 1030171
    Change-Id: I906751b7ceb7b5023ae9ebaf5eea957ce220f51a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948248
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Auto-Submit: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721712}

--- chrome/browser/reputation/safety_tip_infobar_delegate.h
+++ chrome/browser/reputation/safety_tip_infobar_delegate.h
@@ -27,7 +27,6 @@ class SafetyTipInfoBarDelegate : public ConfirmInfoBarDelegate {
   int GetButtons() const override;
   base::string16 GetButtonLabel(InfoBarButton button) const override;
   bool Accept() override;
-  bool Cancel() override;
 
   // infobars::InfoBarDelegate
   infobars::InfoBarDelegate::InfoBarIdentifier GetIdentifier() const override;
