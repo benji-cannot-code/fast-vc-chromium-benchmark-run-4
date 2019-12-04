7cbaa4950e951e5308bc7033e5daeee496b27162
Emily Stark <estark@google.com>
2019-12-04 20:10:42 +0000

    Don't treat external protocols as mixed content
    This CL loosens mixed content restrictions to omit mixed content
    warnings for external protocol URLs (a.k.a. non-webby URLs). Why? Native
    applications cannot return data to the browser via an external
    protocol URL, so a page that loads a frame or a subresource over an
    external protocol is not actually displaying or running mixed
    content. One might reason that external protocols should be treated as
    mixed content because sending data to a native application can be
    dangerous (because e.g. the website could send a malicious payload to
    a vulnerable application), but the website can already do so via a
    top-level navigation, and the external protocol dialog is supposed to
    ensure that users only accept these resource loads from websites that
    they trust. (The dialog is currently not very good and needs to be
    revised to communicate more of a warning message, but that's a
    separate issue orthogonal from mixed content warnings.)
    
    This change simplifies an M80 change to give passive mixed content
    (images, particularly) a more aggressive UI treatment in advance of
    autoupgrading+blocking them in M81
    (https://security.googleblog.com/2019/10/no-more-mixed-messages-about-https_3.html).
    
    Forms that target external protocols are still treated as passive
    mixed content (this is the same as forms that target http://
    URLs). This could probably be argued either way, but the reasoning is
    that we should downgrade the UI when we know that user data is
    explicitly being sent to a potentially insecure endpoint.
    
    Bug: 621131
    Change-Id: Ie945f516db087a9a98cca294d6959d70bbf0dc71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946934
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#721630}

--- content/browser/frame_host/mixed_content_navigation_throttle.cc
+++ content/browser/frame_host/mixed_content_navigation_throttle.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/mixed_content_navigation_throttle.h"
 
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "content/browser/frame_host/frame_tree.h"
 #include "content/browser/frame_host/frame_tree_node.h"
@@ -169,9 +170,25 @@ bool MixedContentNavigationThrottle::ShouldBlockNavigation(bool for_redirect) {
   blink::WebMixedContentContextType mixed_context_type =
       request->mixed_content_context_type();
 
+  // Do not treat non-webby schemes as mixed content when loaded in subframes.
+  // Navigations to non-webby schemes cannot return data to the browser, so
+  // insecure content will not be run or displayed to the user as a result of
+  // loading a non-webby scheme. It is potentially dangerous to navigate to a
+  // non-webby scheme (e.g., the page could deliver a malicious payload to a
+  // vulnerable native application), but loading a non-webby scheme is no more
+  // dangerous in this respect than navigating the main frame to the non-webby
+  // scheme directly. See https://crbug.com/621131.
+  //
+  // TODO(https://crbug.com/1030307): decide whether CORS-enabled is really the
+  // right way to draw this distinction.
   if (!ShouldTreatURLSchemeAsCorsEnabled(request->GetURL())) {
-    mixed_context_type =
-        blink::WebMixedContentContextType::kOptionallyBlockable;
+    // Record non-webby mixed content to see if it is rare enough that it can be
+    // gated behind an enterprise policy. This excludes URLs that are considered
+    // potentially-secure such as blob: and filesystem:, which are special-cased
+    // in IsUrlPotentiallySecure() and cause an early-return because of the
+    // InWhichFrameIsContentMixed() check above.
+    UMA_HISTOGRAM_BOOLEAN("SSL.NonWebbyMixedContentLoaded", true);
+    return false;
   }
 
   bool allowed = false;
