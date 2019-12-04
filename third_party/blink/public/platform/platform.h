e38094de3a89dff113fc981bb9bd2696bf707a08
Sergio Villar Senin <svillar@igalia.com>
2019-12-04 11:16:15 +0000

    Remove web_rtc_api_name.h from Blink API surface
    It isn't used outside Blink due to the Onion Soupping of WebRTC. It was moved to
    renderer/platform/mediastream and renamed so that "web" is not prefixed to the
    file name and the types defined inside.
    
    Bug: 919392
    Change-Id: I8ccf33bdd75f0b04ffd6bc515be1b737a3ea3945
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1944864
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Sergio Villar <svillar@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#721424}

--- third_party/blink/public/platform/platform.h
+++ third_party/blink/public/platform/platform.h
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_data.h"
 #include "third_party/blink/public/platform/web_dedicated_worker_host_factory_client.h"
 #include "third_party/blink/public/platform/web_gesture_device.h"
-#include "third_party/blink/public/platform/web_rtc_api_name.h"
 #include "third_party/blink/public/platform/web_size.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/platform/web_url_error.h"
