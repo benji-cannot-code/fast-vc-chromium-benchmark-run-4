a9fffd9fead10eabcdfb3eda34f3085d3e6aec30
Brandon Wylie <wylieb@chromium.org>
2019-12-04 21:02:19 +0000

    Catch SecurityException thrown by PackageManager#hasSystemFeature
    Bug: 1027709
    Change-Id: I886efef0539a57ce4f38064d550a389340da3df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949731
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721655}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/SearchEngineLogoUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/SearchEngineLogoUtils.java
@@ -16,6 +16,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
+import org.chromium.base.Log;
 import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
@@ -39,6 +40,8 @@ import java.util.Map;
  * Collection of shared code for displaying search engine logos.
  */
 public class SearchEngineLogoUtils {
+    // Note: shortened to account for the 20 character limit.
+    private static final String TAG = "SearchLogoUtils";
     private static final String ROUNDED_EDGES_VARIANT = "rounded_edges";
     private static final String LOUPE_EVERYWHERE_VARIANT = "loupe_everywhere";
     private static final String DUMMY_URL_QUERY = "replace_me";
@@ -57,9 +60,18 @@ public class SearchEngineLogoUtils {
     static class Delegate {
         /** @see SearchEngineLogoUtils#isSearchEngineLogoEnabled */
         public boolean isSearchEngineLogoEnabled() {
-            return !LocaleManager.getInstance().needToCheckForSearchEnginePromo()
-                    && ChromeFeatureList.isInitialized()
-                    && ChromeFeatureList.isEnabled(ChromeFeatureList.OMNIBOX_SEARCH_ENGINE_LOGO);
+            // Note: LocaleManager#needToCheckForSearchEnginePromo() checks several system features
+            // which risk throwing a security exception. Catching that here to prevent it from
+            // crashing the app.
+            try {
+                return !LocaleManager.getInstance().needToCheckForSearchEnginePromo()
+                        && ChromeFeatureList.isInitialized()
+                        && ChromeFeatureList.isEnabled(
+                                ChromeFeatureList.OMNIBOX_SEARCH_ENGINE_LOGO);
+            } catch (SecurityException e) {
+                Log.e(TAG, "Security exception thrown by failed IPC, see crbug.com/1027709");
+                return false;
+            }
         }
 
         /** @see SearchEngineLogoUtils#shouldShowSearchEngineLogo */
