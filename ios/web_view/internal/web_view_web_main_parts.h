ca377863918cde70340b963fae51aa945adb13e5
Mikel Astiz <mastiz@chromium.org>
2019-12-04 18:38:28 +0000

    Instantiate FieldTrialList for ios WebView
    ios WebView does not integrate with variations, but the actual
    production code WebView relies on may assume that there an instance,
    leading to DCHECK failures.
    
    This was surfaced by integration tests where, despite base::TestSuite's
    attempts to instantiate FieldTrialList via FeatureListScopedToEachTest,
    and due to shared libraries leading to ODR violations affecting
    FieldTrialList::global_, DCHECKs could fire (mostly prevented landing
    additional DCHECKs).
    
    Bug: 1011088,1018667
    
    Change-Id: I7ff3b356bd246c5f0a9b8776ae6d372bfd4a9df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950518
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721563}

--- ios/web_view/internal/web_view_web_main_parts.h
+++ ios/web_view/internal/web_view_web_main_parts.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "base/metrics/field_trial.h"
 #include "ios/web/public/init/web_main_parts.h"
 
 namespace ios_web_view {
@@ -31,6 +32,10 @@ class WebViewWebMainParts : public web::WebMainParts {
   void LoadNonScalableResources();
   // Loads resources that can be scaled. f.e. png images for @1x, @2x, and @3x.
   void LoadScalableResources();
+
+  // Dummy FieldTrialList instance for code that consumes variations data,
+  // although ios WebView does not support variations.
+  base::FieldTrialList field_trial_list_;
 
   DISALLOW_COPY_AND_ASSIGN(WebViewWebMainParts);
 };
