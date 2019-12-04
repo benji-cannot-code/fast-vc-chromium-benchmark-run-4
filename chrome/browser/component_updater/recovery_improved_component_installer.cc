bde3291d1817b22242300a7e357aaba440bb5c34
Sorin Jianu <sorin@chromium.org>
2019-12-04 19:28:04 +0000

    Fix Android build break when is_chrome_branded = true.
    Some constexpr static class members still require definitions in C++14 but
    it is not clear why this build break occurred on Android only.
    
    Change-Id: I3e0d04d765f02ce37db2f7b4df90bb4d85fcab6d
    
    BUG: 1030716
    Change-Id: I3e0d04d765f02ce37db2f7b4df90bb4d85fcab6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950749
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721601}

--- chrome/browser/component_updater/recovery_improved_component_installer.cc
+++ chrome/browser/component_updater/recovery_improved_component_installer.cc
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace component_updater {
 
+constexpr base::TaskTraits RecoveryComponentActionHandler::kTaskTraits;
+constexpr base::TaskTraits
+    RecoveryComponentActionHandler::kTaskTraitsRunCommand;
+
 RecoveryComponentActionHandler::RecoveryComponentActionHandler(
     const std::vector<uint8_t>& key_hash,
     crx_file::VerifierFormat verifier_format)
