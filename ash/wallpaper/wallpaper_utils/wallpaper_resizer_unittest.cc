25aa9cf7f914bc5b68d8952aaf4e294cad3b68d2
Carlos Caballero <carlscab@google.com>
2019-12-04 23:06:59 +0000

    Remove MessageLoop references in /ash/wallpaper
    MessageLoop is going away soon.
    
    This CL was uploaded by git cl split.
    
    R=maybelle@chromium.org
    
    Bug: 891670
    Change-Id: I53fe47639f2f146806d52791fc7d34b8fd5d8221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1944670
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721718}

--- ash/wallpaper/wallpaper_utils/wallpaper_resizer_unittest.cc
+++ ash/wallpaper/wallpaper_utils/wallpaper_resizer_unittest.cc
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/wallpaper_types.h"
 #include "ash/wallpaper/wallpaper_utils/wallpaper_resizer_observer.h"
-#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/stl_util.h"
+#include "base/test/task_environment.h"
 #include "base/threading/thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/image/image_skia_rep.h"
@@ -95,7 +95,7 @@ class WallpaperResizerTest : public testing::Test,
   void OnWallpaperResized() override { active_runloop_->Quit(); }
 
  private:
-  base::MessageLoop message_loop_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
   std::unique_ptr<base::RunLoop> active_runloop_;
   base::Thread worker_thread_;
 
