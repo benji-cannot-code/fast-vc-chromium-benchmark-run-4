fe38991bb10ab1615c1c59f15d2d6fd665d37e9a
David Tseng <dtseng@chromium.org>
2019-12-04 05:12:38 +0000

    Adds friendlier api for markerTypes in chrome.automation
    Also, adds additional PRESUBMIT checks to ensure automation keeps in sync with
    ax_enums for MarkerType.
    
    Test: existing browser_tests --gtest_filter=ChromeVoxEditing*.*
    Change-Id: I58556cc8317cbca62f00aed8d9f3afbcf7feeee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940868
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721368}

--- extensions/renderer/resources/automation/automation_node.js
+++ extensions/renderer/resources/automation/automation_node.js
@@ -516,6 +516,13 @@ var EventListenerAdded = natives.EventListenerAdded;
  */
 var EventListenerRemoved = natives.EventListenerRemoved;
 
+/**
+ * @param {string} axTreeID The id of the accessibility tree.
+ * @param {number} nodeID The id of a node.
+ * @return {Array}
+ */
+var GetMarkers = natives.GetMarkers;
+
 var logging = requireNative('logging');
 var utils = require('utils');
 
@@ -777,6 +784,10 @@ AutomationNodeImpl.prototype = {
 
   get nonInlineTextWordEnds() {
     return GetWordEndOffsets(this.treeID, this.id);
+  },
+
+  get markers() {
+    return GetMarkers(this.treeID, this.id);
   },
 
   doDefault: function() {
@@ -1245,9 +1256,6 @@ var nodeRefAttributes = [
 
 var intListAttributes = [
     'lineBreaks',
-    'markerEnds',
-    'markerStarts',
-    'markerTypes',
     'wordEnds',
     'wordStarts'];
 
@@ -1761,43 +1769,44 @@ utils.expose(AutomationNode, AutomationNodeImpl, {
   readonly: $Array.concat(
       publicAttributes,
       [
-        'parent',
-        'firstChild',
-        'lastChild',
-        'children',
-        'previousSibling',
-        'nextSibling',
-        'isRootNode',
-        'role',
+        'bold',
         'checked',
+        'children',
+        'customActions',
         'defaultActionVerb',
+        'descriptionFrom',
+        'detectedLanguage',
+        'firstChild',
         'hasPopup',
-        'restriction',
-        'state',
-        'location',
+        'htmlAttributes',
         'imageAnnotation',
         'indexInParent',
-        'lineStartOffsets',
-        'root',
-        'htmlAttributes',
-        'nameFrom',
-        'descriptionFrom',
-        'bold',
+        'isRootNode',
         'italic',
-        'underline',
+        'lastChild',
+        'lineStartOffsets',
         'lineThrough',
-        'detectedLanguage',
-        'customActions',
-        'standardActions',
-        'unclippedLocation',
-        'tableCellColumnHeaders',
-        'tableCellRowHeaders',
-        'tableCellColumnIndex',
-        'tableCellRowIndex',
-        'tableCellAriaRowIndex',
-        'tableCellAriaColumnIndex',
-        'nonInlineTextWordStarts',
+        'location',
+        'markers',
+        'nameFrom',
+        'nextSibling',
         'nonInlineTextWordEnds',
+        'nonInlineTextWordStarts',
+        'parent',
+        'previousSibling',
+        'restriction',
+        'role',
+        'root',
+        'standardActions',
+        'state',
+        'tableCellAriaColumnIndex',
+        'tableCellAriaRowIndex',
+        'tableCellColumnHeaders',
+        'tableCellColumnIndex',
+        'tableCellRowHeaders',
+        'tableCellRowIndex',
+        'unclippedLocation',
+        'underline',
       ]),
 });
 
