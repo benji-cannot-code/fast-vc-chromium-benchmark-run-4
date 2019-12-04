291e54cf537b7d0736c6d7b8c5e48cb7df834837
Lei Zhang <thestig@chromium.org>
2019-12-04 17:07:03 +0000

    Initialize more members in the header in base/files/file.h.
    Change-Id: I37ddc01a0427105b679d6f73708e734311f5502e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947965
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721515}

--- base/files/file.cc
+++ base/files/file.cc
@@ -17,23 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-File::Info::Info()
-    : size(0),
-      is_directory(false),
-      is_symbolic_link(false) {
-}
+File::Info::Info() = default;
 
 File::Info::~Info() = default;
 
-File::File()
-    : error_details_(FILE_ERROR_FAILED),
-      created_(false),
-      async_(false) {
-}
+File::File() = default;
 
 #if !defined(OS_NACL)
-File::File(const FilePath& path, uint32_t flags)
-    : error_details_(FILE_OK), created_(false), async_(false) {
+File::File(const FilePath& path, uint32_t flags) : error_details_(FILE_OK) {
   Initialize(path, flags);
 }
 #endif
@@ -43,18 +34,13 @@ File::File(PlatformFile platform_file) : File(platform_file, false) {}
 File::File(PlatformFile platform_file, bool async)
     : file_(platform_file),
       error_details_(FILE_OK),
-      created_(false),
       async_(async) {
 #if defined(OS_POSIX) || defined(OS_FUCHSIA)
   DCHECK_GE(platform_file, -1);
 #endif
 }
 
-File::File(Error error_details)
-    : error_details_(error_details),
-      created_(false),
-      async_(false) {
-}
+File::File(Error error_details) : error_details_(error_details) {}
 
 File::File(File&& other)
     : file_(other.TakePlatformFile()),
