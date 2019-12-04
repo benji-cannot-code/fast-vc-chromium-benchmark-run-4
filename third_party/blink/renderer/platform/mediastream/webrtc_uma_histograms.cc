2e4f10d8147e4095ab1210734fe50d9bf3ba5df9
Antonio Gomes <tonikitoo@igalia.com>
2019-12-04 16:38:53 +0000

    Switch platform/mediastream/ away from SEQUENCE_CHECKER
    ... to use THREAD_CHECKER instead.
    
    Blink does not currently make use of sequences.
    
    BUG=787254
    R=guidou@chromium.org
    
    Change-Id: Idda5453badb39a2c52ea8c5d8282a338d9cb5d3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946623
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721505}

--- third_party/blink/renderer/platform/mediastream/webrtc_uma_histograms.cc
+++ third_party/blink/renderer/platform/mediastream/webrtc_uma_histograms.cc
@@ -30,7 +30,7 @@ void UpdateWebRTCMethodCount(WebRTCAPIName api_name) {
 }
 
 PerSessionWebRTCAPIMetrics::~PerSessionWebRTCAPIMetrics() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 }
 
 // static
@@ -39,12 +39,12 @@ PerSessionWebRTCAPIMetrics* PerSessionWebRTCAPIMetrics::GetInstance() {
 }
 
 void PerSessionWebRTCAPIMetrics::IncrementStreamCounter() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   ++num_streams_;
 }
 
 void PerSessionWebRTCAPIMetrics::DecrementStreamCounter() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   if (--num_streams_ == 0) {
     ResetUsage();
   }
@@ -62,7 +62,7 @@ void PerSessionWebRTCAPIMetrics::LogUsage(WebRTCAPIName api_name) {
 }
 
 void PerSessionWebRTCAPIMetrics::LogUsageOnlyOnce(WebRTCAPIName api_name) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   if (!has_used_api_[static_cast<int>(api_name)]) {
     has_used_api_[static_cast<int>(api_name)] = true;
     LogUsage(api_name);
