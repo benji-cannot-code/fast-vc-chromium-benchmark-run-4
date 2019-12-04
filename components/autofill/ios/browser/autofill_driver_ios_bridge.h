2dd11c7105259a0884f73b7ad6ec276cf6e5730a
John Z Wu <jzw@chromium.org>
2019-12-04 23:19:51 +0000

    Add method for handling parsed forms in AutofillDriverIOSBridge
    This will be used in CWVAutofillController to handle forms found in
    the page.
    
    Bug: 1030451
    Change-Id: I2224e8ed65074bf0c4658db5a3e5016c3d13d36a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949299
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721725}

--- components/autofill/ios/browser/autofill_driver_ios_bridge.h
+++ components/autofill/ios/browser/autofill_driver_ios_bridge.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 struct FormData;
+class FormStructure;
 }
 
 namespace web {
@@ -25,6 +26,9 @@ class WebFrame;
 
 - (void)fillFormData:(const autofill::FormData&)form
              inFrame:(web::WebFrame*)frame;
+
+- (void)handleParsedForms:(const std::vector<autofill::FormStructure*>&)forms
+                  inFrame:(web::WebFrame*)frame;
 
 - (void)fillFormDataPredictions:
             (const std::vector<autofill::FormDataPredictions>&)forms
