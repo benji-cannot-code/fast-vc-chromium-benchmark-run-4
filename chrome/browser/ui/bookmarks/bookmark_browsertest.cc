1e79e34124439451d5120ec6bcd38650f02fcc93
Friedrich Horschig <fhorschig@chromium.org>
2019-12-04 15:11:02 +0000

    [Flaky] Use RunLoop to fix repeated moving
    Most likely, `std::move(*quit_task).Run();` could be called twice for
    the same |quit_task| at times. By introducing a RunLoop, it doesn't
    matter (anymore) how often the loop is quit.
    
    This should fix the flaky crashes of this test as described in the
    linked bug.
    
    Bug: 1030275
    Change-Id: I9509d474cc950362996f31a879034fb01ca5303c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948895
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721478}

--- chrome/browser/ui/bookmarks/bookmark_browsertest.cc
+++ chrome/browser/ui/bookmarks/bookmark_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/macros.h"
+#include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -81,21 +82,20 @@ class BookmarkBrowsertest : public InProcessBrowserTest {
     return browser()->bookmark_bar_state() == BookmarkBar::SHOW;
   }
 
-  static void CheckAnimation(Browser* browser, base::OnceClosure* quit_task) {
+  static void CheckAnimation(Browser* browser, base::RepeatingClosure quit) {
     if (!browser->window()->IsBookmarkBarAnimating())
-      std::move(*quit_task).Run();
+      quit.Run();
   }
 
   base::TimeDelta WaitForBookmarkBarAnimationToFinish() {
     base::Time start(base::Time::Now());
-    scoped_refptr<content::MessageLoopRunner> runner =
-        new content::MessageLoopRunner;
-    base::OnceClosure quit_closure = runner->QuitClosure();
+    base::RunLoop loop;
 
     base::RepeatingTimer timer;
-    timer.Start(FROM_HERE, base::TimeDelta::FromMilliseconds(15),
-                base::BindRepeating(&CheckAnimation, browser(), &quit_closure));
-    runner->Run();
+    timer.Start(
+        FROM_HERE, base::TimeDelta::FromMilliseconds(15),
+        base::BindRepeating(&CheckAnimation, browser(), loop.QuitClosure()));
+    loop.Run();
     return base::Time::Now() - start;
   }
 
