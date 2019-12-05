7e5e6c37c98dfa817b3f952ada9dd652cf80a4d3
Sadrul Habib Chowdhury <sadrul@chromium.org>
2019-12-05 19:52:28 +0000

    [throughput metrics] Refactor metrics code into separate class.
    Refactor the code that is responsible for reporting metrics out of
    FrameSequenceTracker into a new class: FrameSequenceMetrics. This object
    only carries the information needed to report the metrics, while the
    tracker maintains the states necessary to update/generate these metrics.
    
    BUG=1031251
    
    Change-Id: Idc94078191f5274b5f11217f52e458bff3d31ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1951564
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722160}

--- cc/metrics/frame_sequence_tracker_unittest.cc
+++ cc/metrics/frame_sequence_tracker_unittest.cc
@@ -118,8 +118,8 @@ class FrameSequenceTrackerTest : public testing::Test {
     base::HistogramTester histogram_tester;
 
     // Test that there is no main thread frames expected.
-    tracker_->impl_throughput_.frames_expected = 100u;
-    tracker_->impl_throughput_.frames_produced = 85u;
+    tracker_->impl_throughput().frames_expected = 100u;
+    tracker_->impl_throughput().frames_produced = 85u;
     tracker_->ReportMetrics();
     histogram_tester.ExpectTotalCount(
         "Graphics.Smoothness.Throughput.CompositorThread.TouchScroll", 1u);
@@ -129,8 +129,8 @@ class FrameSequenceTrackerTest : public testing::Test {
         "Graphics.Smoothness.Throughput.SlowerThread.TouchScroll", 1u);
 
     // Test that both are reported.
-    tracker_->main_throughput_.frames_expected = 50u;
-    tracker_->main_throughput_.frames_produced = 25u;
+    tracker_->main_throughput().frames_expected = 50u;
+    tracker_->main_throughput().frames_produced = 25u;
     tracker_->ReportMetrics();
     histogram_tester.ExpectTotalCount(
         "Graphics.Smoothness.Throughput.CompositorThread.TouchScroll", 2u);
@@ -140,10 +140,10 @@ class FrameSequenceTrackerTest : public testing::Test {
         "Graphics.Smoothness.Throughput.SlowerThread.TouchScroll", 2u);
 
     // Test that none is reported.
-    tracker_->main_throughput_.frames_expected = 2u;
-    tracker_->main_throughput_.frames_produced = 1u;
-    tracker_->impl_throughput_.frames_expected = 2u;
-    tracker_->impl_throughput_.frames_produced = 1u;
+    tracker_->main_throughput().frames_expected = 2u;
+    tracker_->main_throughput().frames_produced = 1u;
+    tracker_->impl_throughput().frames_expected = 2u;
+    tracker_->impl_throughput().frames_produced = 1u;
     tracker_->ReportMetrics();
     histogram_tester.ExpectTotalCount(
         "Graphics.Smoothness.Throughput.CompositorThread.TouchScroll", 2u);
@@ -153,10 +153,10 @@ class FrameSequenceTrackerTest : public testing::Test {
         "Graphics.Smoothness.Throughput.SlowerThread.TouchScroll", 2u);
 
     // Test the case where compositor and main thread have the same throughput.
-    tracker_->impl_throughput_.frames_expected = 20u;
-    tracker_->impl_throughput_.frames_produced = 18u;
-    tracker_->main_throughput_.frames_expected = 20u;
-    tracker_->main_throughput_.frames_produced = 18u;
+    tracker_->impl_throughput().frames_expected = 20u;
+    tracker_->impl_throughput().frames_produced = 18u;
+    tracker_->main_throughput().frames_expected = 20u;
+    tracker_->main_throughput().frames_produced = 18u;
     tracker_->ReportMetrics();
     histogram_tester.ExpectTotalCount(
         "Graphics.Smoothness.Throughput.CompositorThread.TouchScroll", 3u);
@@ -188,16 +188,16 @@ class FrameSequenceTrackerTest : public testing::Test {
     return tracker_->ignored_frame_tokens_;
   }
 
-  FrameSequenceTracker::ThroughputData ImplThroughput() const {
-    return tracker_->impl_throughput_;
+  FrameSequenceMetrics::ThroughputData ImplThroughput() const {
+    return tracker_->impl_throughput();
   }
-  FrameSequenceTracker::ThroughputData MainThroughput() const {
-    return tracker_->main_throughput_;
+  FrameSequenceMetrics::ThroughputData MainThroughput() const {
+    return tracker_->main_throughput();
   }
 
  protected:
   uint32_t number_of_frames_checkerboarded() const {
-    return tracker_->checkerboarding_.frames_checkerboarded;
+    return tracker_->metrics_.frames_checkerboarded();
   }
 
   std::unique_ptr<CompositorFrameReportingController>
