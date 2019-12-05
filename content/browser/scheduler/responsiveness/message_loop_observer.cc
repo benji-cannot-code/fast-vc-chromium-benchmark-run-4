f135bb9cd22b13575502e377ee2f757c41511fb8
Francois Doray <fdoray@chromium.org>
2019-12-05 18:19:24 +0000

    [metrics] Record jank metric that includes queuing time.
    This CL adds the Browser.Responsiveness.JankyIntervalsPerThirtySeconds2
    histogram. This histogram is similar to
    Browser.Responsiveness.JankyIntervalsPerThirtySeconds, except that an
    interval during which an "important" task that was queued more than
    100ms ago is executing can also be marked as janky. Originally, only
    intervals during which a task or event that started executing more
    100 ms ago  is executing were marked as janky (queuing time was not
    taken into account).
    
    See discussion
    https://groups.google.com/a/google.com/forum/#!topic/chrome-scheduler/X32gKl6fW2A
    
    Bug: 1029137
    Change-Id: Ib4ad64ce750adc1bb018f4eab0e427b8320c5b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947547
    Auto-Submit: François Doray <fdoray@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722117}

--- content/browser/scheduler/responsiveness/message_loop_observer.cc
+++ content/browser/scheduler/responsiveness/message_loop_observer.cc
@@ -10,10 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace responsiveness {
 
-MessageLoopObserver::MessageLoopObserver(TaskCallback will_run_task_callback,
-                                         TaskCallback did_run_task_callback)
-    : will_run_task_callback_(will_run_task_callback),
-      did_run_task_callback_(did_run_task_callback) {
+MessageLoopObserver::MessageLoopObserver(
+    WillProcessTaskCallback will_process_task_callback,
+    DidProcessTaskCallback did_process_task_callback)
+    : will_process_task_callback_(std::move(will_process_task_callback)),
+      did_process_task_callback_(std::move(did_process_task_callback)) {
   base::MessageLoopCurrent::Get()->SetAddQueueTimeToTasks(true);
   base::MessageLoopCurrent::Get()->AddTaskObserver(this);
 }
@@ -23,15 +24,14 @@ MessageLoopObserver::~MessageLoopObserver() {
   base::MessageLoopCurrent::Get()->SetAddQueueTimeToTasks(false);
 }
 
-void MessageLoopObserver::WillProcessTask(
-    const base::PendingTask& pending_task,
-    bool /* was_blocked_or_low_priority */) {
-  will_run_task_callback_.Run(&pending_task);
+void MessageLoopObserver::WillProcessTask(const base::PendingTask& pending_task,
+                                          bool was_blocked_or_low_priority) {
+  will_process_task_callback_.Run(&pending_task, was_blocked_or_low_priority);
 }
 
 void MessageLoopObserver::DidProcessTask(
     const base::PendingTask& pending_task) {
-  did_run_task_callback_.Run(&pending_task);
+  did_process_task_callback_.Run(&pending_task);
 }
 
 }  // namespace responsiveness
