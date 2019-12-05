25125570fba4368976e2fa68de956fc65bbf7869
Alexey Baskakov <loyso@chromium.org>
2019-12-05 02:21:59 +0000

    WebApp: Implement the new Web Apps Publisher for AppService.
    The code in web_apps.cc|h is inspired by the ExtensionApps class
    implementation.
    
    WebApps publisher doesn't support AppInstanceRegistry for now.
    
    This code is disabled by default behind kDesktopPWAsWithoutExtensions
    base feature.
    
    Next thing missing: Implement WebAppContextMenu for AppServiceAppItem.
    
    Bug: 1029221
    Change-Id: I5bf2523d62b8ad990b8f7ca9017a1347903abd3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1941534
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721814}

--- chrome/browser/apps/app_service/app_service_proxy_factory.cc
+++ chrome/browser/apps/app_service/app_service_proxy_factory.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/crostini/crostini_registry_service_factory.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chrome/browser/web_applications/web_app_provider_factory.h"
 #include "extensions/browser/app_window/app_window_registry.h"
 #include "extensions/browser/extension_prefs_factory.h"
 #include "extensions/browser/extension_registry_factory.h"
@@ -50,6 +51,7 @@ AppServiceProxyFactory::AppServiceProxyFactory()
   DependsOn(extensions::AppWindowRegistry::Factory::GetInstance());
   DependsOn(extensions::ExtensionPrefsFactory::GetInstance());
   DependsOn(extensions::ExtensionRegistryFactory::GetInstance());
+  DependsOn(web_app::WebAppProviderFactory::GetInstance());
 #endif  // OS_CHROMEOS
 }
 
