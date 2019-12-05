4e8aaa247831ef61144e71b560dfc0be6371c773
Gyuyoung Kim <gyuyoung@igalia.com>
2019-12-05 02:18:23 +0000

    Inline static Create() factory functions from renderer/modules/bluetooth
    This CL removes Create() factory functions in
    //third_partyb/blink/renderer/modules/bluetooth as much as possible,
    and makes all callers use MakeGarbageCollected<Foo> instead.
    
    This CL removes below factory functions,
      - BluetoothRemoteGATTDescriptor::Create
    
    Bug: 939691
    Change-Id: I439ce17703a6a0a37ac1d690dd26c14f1cf467ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950044
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#721813}

--- third_party/blink/renderer/modules/bluetooth/bluetooth_remote_gatt_descriptor.h
+++ third_party/blink/renderer/modules/bluetooth/bluetooth_remote_gatt_descriptor.h
@@ -34,10 +34,6 @@ class BluetoothRemoteGATTDescriptor final : public ScriptWrappable {
       mojom::blink::WebBluetoothRemoteGATTDescriptorPtr,
       BluetoothRemoteGATTCharacteristic*);
 
-  static BluetoothRemoteGATTDescriptor* Create(
-      mojom::blink::WebBluetoothRemoteGATTDescriptorPtr,
-      BluetoothRemoteGATTCharacteristic*);
-
   // IDL exposed interface:
   BluetoothRemoteGATTCharacteristic* characteristic() {
     return characteristic_;
