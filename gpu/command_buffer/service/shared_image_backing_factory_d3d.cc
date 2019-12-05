c6bf71842b19027f407a6f3e600539345aa7dbe2
Rafael Cintron <rafael.cintron@microsoft.com>
2019-12-05 04:39:49 +0000

    Reland "Enable WebGPU on Skia Renderer""
    This relands 5c675fd95d32f345a68da9212c1c5692e500fd0d
    
    CreateSharedImage is not supported in Windows Server 2008 so we need to adjust the
    test skipping logic.
    
    > Enable WebGPU on Skia Renderer
    >
    > Implement SharedImageBackingD3D::ProduceSkia and supporting methods.
    >
    > Bug: 1021118,1002316
    > Change-Id: Ia699bddfa08469b9e151018eca6c5206620cb731
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1938707
    > Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#720722}
    
    Bug: 1021118,1002316
    Change-Id: I09f6a9f5a89f23dd7a0f7e07b94f5cedbae4f613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947637
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#721856}

--- gpu/command_buffer/service/shared_image_backing_factory_d3d.cc
+++ gpu/command_buffer/service/shared_image_backing_factory_d3d.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/resources/resource_format_utils.h"
 #include "gpu/command_buffer/common/shared_image_trace_utils.h"
 #include "gpu/command_buffer/service/mailbox_manager.h"
+#include "gpu/command_buffer/service/shared_context_state.h"
 #include "gpu/command_buffer/service/shared_image_backing.h"
 #include "gpu/command_buffer/service/shared_image_manager.h"
 #include "gpu/command_buffer/service/shared_image_representation.h"
+#include "gpu/command_buffer/service/shared_image_representation_skia_gl.h"
 #include "gpu/command_buffer/service/texture_manager.h"
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gl/buildflags.h"
@@ -404,6 +406,15 @@ class SharedImageBackingD3D : public SharedImageBacking {
     TRACE_EVENT0("gpu", "SharedImageBackingD3D::ProduceGLTexturePassthrough");
     return std::make_unique<SharedImageRepresentationGLTexturePassthroughD3D>(
         manager, this, tracker, texture_passthrough_);
+  }
+
+  std::unique_ptr<SharedImageRepresentationSkia> ProduceSkia(
+      SharedImageManager* manager,
+      MemoryTypeTracker* tracker,
+      scoped_refptr<SharedContextState> context_state) override {
+    return SharedImageRepresentationSkiaGL::CreateForPassthrough(
+        ProduceGLTexturePassthrough(manager, tracker), std::move(context_state),
+        manager, this, tracker);
   }
 
  private:
