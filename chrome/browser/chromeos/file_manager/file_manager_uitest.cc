e0be66b01bfd61de0961ec09e33074f1c289eccd
Alex Danilo <adanilo@chromium.org>
2019-12-05 07:35:45 +0000

    Make the navigation tree footer hidden
    First step in removal of the deprecated progress center.
    
    Bug: 1027012
    Change-Id: I34ea74d0f56b8a6327dbdf2f6b37c6d5786ac084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1952060
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721909}

--- chrome/browser/chromeos/file_manager/file_manager_uitest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_uitest.cc
@@ -94,8 +94,4 @@ IN_PROC_BROWSER_TEST_F(FileManagerUITest, PluginVmShare) {
   RunTest("pluginVmShare");
 }
 
-IN_PROC_BROWSER_TEST_F(FileManagerUITest, ProgressCenter) {
-  RunTest("progressCenter");
-}
-
 }  // namespace file_manager
