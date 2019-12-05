978eda0bcba197764735e3c3ac5839e75bceefba
Aga Wronska <agawronska@chromium.org>
2019-12-05 21:39:04 +0000

    Update auth disabled view background color
    This view is only displayed for child user and its background color
    should match ParentAccessView background color.
    
    Bug: 999390
    Change-Id: I4c38e2268a6f5f7805cb1af4eb78ad948f145e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1952086
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722210}

--- ash/login/ui/login_auth_user_view.cc
+++ ash/login/ui/login_auth_user_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/login_pin_view.h"
 #include "ash/login/ui/login_user_view.h"
 #include "ash/login/ui/non_accessible_view.h"
+#include "ash/login/ui/parent_access_view.h"
 #include "ash/login/ui/pin_keyboard_animation.h"
 #include "ash/login/ui/views_utils.h"
 #include "ash/public/cpp/login_constants.h"
@@ -81,12 +82,6 @@ constexpr SkColor kChallengeResponseArrowBackgroundColor =
     SkColorSetARGB(0x2B, 0xFF, 0xFF, 0xFF);
 constexpr SkColor kChallengeResponseErrorColor =
     SkColorSetRGB(0xEE, 0x67, 0x5C);
-
-// The color of the disabled auth message bubble when the color extracted from
-// wallpaper is transparent or invalid (i.e. color calculation fails or is
-// disabled).
-constexpr SkColor kDisabledAuthMessageBubbleColor =
-    SkColorSetRGB(0x20, 0x21, 0x24);
 
 // Date time format containing only the day of the week, for example: "Tuesday".
 constexpr char kDayOfWeekOnlyTimeFormat[] = "EEEE";
@@ -712,12 +707,8 @@ class LoginAuthUserView::DisabledAuthMessageView : public views::View {
 
     cc::PaintFlags flags;
     flags.setStyle(cc::PaintFlags::kFill_Style);
-    SkColor color = Shell::Get()->wallpaper_controller()->GetProminentColor(
-        color_utils::ColorProfile(color_utils::LumaRange::NORMAL,
-                                  color_utils::SaturationRange::MUTED));
-    if (color == kInvalidWallpaperColor || color == SK_ColorTRANSPARENT)
-      color = kDisabledAuthMessageBubbleColor;
-    flags.setColor(color);
+    flags.setColor(
+        ParentAccessView::GetChildUserDialogColor(false /*using blur*/));
     canvas->DrawRoundRect(GetContentsBounds(),
                           kDisabledAuthMessageRoundedCornerRadiusDp, flags);
   }
--- ash/login/ui/parent_access_view.h
+++ ash/login/ui/parent_access_view.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observer.h"
 #include "base/time/time.h"
 #include "components/account_id/account_id.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/window/dialog_delegate.h"
 
@@ -114,6 +115,11 @@ class ASH_EXPORT ParentAccessView : public views::DialogDelegateView,
   static constexpr char kUMAParentAccessCodeUsage[] =
       "Supervision.ParentAccessCode.Usage";
 
+  // Returns color used for dialog and UI elements specific for child user.
+  // |using_blur| should be true if the UI element is using background blur
+  // (color transparency depends on it).
+  static SkColor GetChildUserDialogColor(bool using_blur);
+
   // Creates parent access view that will validate the parent access code for a
   // specific child, when |account_id| is set, or to any child signed in the
   // device, when it is empty. |callbacks| will be called when user performs
@@ -187,9 +193,6 @@ class ASH_EXPORT ParentAccessView : public views::DialogDelegateView,
 
   // Auto submit code when the last input has been inserted.
   bool auto_submit_enabled_ = true;
-
-  // Whether view background should be blurred.
-  bool blur_background_ = false;
 
   views::Label* title_label_ = nullptr;
   views::Label* description_label_ = nullptr;
