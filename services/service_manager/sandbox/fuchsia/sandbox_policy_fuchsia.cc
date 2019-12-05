50130f320fe8ef420db919adb9aa9297a99e57ca
David Dorwin <ddorwin@chromium.org>
2019-12-05 06:53:19 +0000

    [fuchsia] Use ICU for Exploded Time
    Add an ICU-based implementation of time_exploded, removing the use of
    the POSIX implementation and thus the use of fuchsia.deprecatedtimezone.
    
    Bug: 985946
    Test: base/time/ unit tests
    Change-Id: I91440ecae48684717b6fdcfe5f7ef66028b079c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949776
    Commit-Queue: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721897}

--- services/service_manager/sandbox/fuchsia/sandbox_policy_fuchsia.cc
+++ services/service_manager/sandbox/fuchsia/sandbox_policy_fuchsia.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdio.h>
 #include <zircon/processargs.h>
 
-#include <fuchsia/deprecatedtimezone/cpp/fidl.h>
 #include <fuchsia/fonts/cpp/fidl.h>
 #include <fuchsia/intl/cpp/fidl.h>
 #include <fuchsia/logger/cpp/fidl.h>
@@ -120,8 +119,7 @@ const SandboxConfig& GetConfigForSandboxType(SandboxType type) {
 
 // Services that are passed to all processes.
 constexpr base::span<const char* const> kDefaultServices = base::make_span(
-    (const char* const[]){fuchsia::deprecatedtimezone::Timezone::Name_,
-                          fuchsia::intl::PropertyProvider::Name_,
+    (const char* const[]){fuchsia::intl::PropertyProvider::Name_,
                           fuchsia::logger::LogSink::Name_});
 
 }  // namespace
