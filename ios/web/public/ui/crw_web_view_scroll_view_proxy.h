5fd6f81c841b61371368b319ed33ae39d8d0799e
Hiroshi Ichikawa <ichikawa@chromium.org>
2019-12-05 02:44:02 +0000

    Allow CRWWebViewScrollViewProxy to be safely cast to UIScrollView.
    There are still some missing parts, which I left as TODO.
    
    Change-Id: I5fbe6e6c4e84bde922fb70ee1c8099d446025b6c
    Bug: 1023250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906863
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Auto-Submit: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721821}

--- ios/web/public/ui/crw_web_view_scroll_view_proxy.h
+++ ios/web/public/ui/crw_web_view_scroll_view_proxy.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The class forwards some of the methods onto the UIScrollView. For more
 // information look at the UIScrollView documentation.
 // TODO(crbug.com/546152): rename class to CRWContentViewScrollViewProxy.
-@interface CRWWebViewScrollViewProxy : NSObject <UIScrollViewDelegate>
+@interface CRWWebViewScrollViewProxy : NSObject
 @property(nonatomic, assign) CGPoint contentOffset;
 @property(nonatomic, assign) UIEdgeInsets contentInset;
 @property(nonatomic, readonly, getter=isDecelerating) BOOL decelerating;
@@ -60,6 +60,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Removes |observer| as a subscriber for change notifications.
 - (void)removeObserver:(id<CRWWebViewScrollViewProxyObserver>)observer;
+
+// Returns a scroll view proxy which can be accessed as UIScrollView.
+//
+// Note: This method is introduced because CRWWebViewScrollViewProxy cannot
+// simply be a subclass of UIScrollView. Its implementation relies on
+// -forwardInvocation: which only works for methods not implemented in the class
+// (including those implemented in its superclass). So -forwardInvocation:
+// cannot forward methods in UIScrollView if CRWWebViewScrollViewProxy is a
+// subclass of UIScrollView.
+//
+// TODO(crbug.com/1023250): Support KVO and delegate of this scroll view.
+// TODO(crbug.com/1023250): Restore properties of the scroll view when the
+// scroll view is reset.
+- (UIScrollView*)asUIScrollView;
 
 @end
 
