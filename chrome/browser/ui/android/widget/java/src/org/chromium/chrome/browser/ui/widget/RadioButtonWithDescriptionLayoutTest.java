daf20ebb846989bef439a6b3c478a54f23c4b0bb
Evan Stade <estade@chromium.org>
2019-12-05 19:14:57 +0000

    Change chrome.browser.ui.widget test class runner
    Use BaseJUnit4ClassRunner instead of ChromeJUnit4ClassRunner for tests
    in org.chromium.chrome.browser.ui.widget. This will allow the tests to
    move to //components/ with the rest of the package.
    
    None of these tests use any of
    - @Restriction
    - @Enable/DisableFeatures
    - @Policies
    
    So ChromeJUnit4ClassRunner shouldn't be necessary.
    
    Bug: 1030785
    Change-Id: I7390b171330ea767b6a8f5fffe49db93d163f3a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1951327
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722136}

--- chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/RadioButtonWithDescriptionLayoutTest.java
+++ chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/RadioButtonWithDescriptionLayoutTest.java
@@ -20,7 +20,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +28,7 @@ import java.util.List;
 /**
  * Tests for {@link RadioButtonLayout}.
  */
-@RunWith(ChromeJUnit4ClassRunner.class)
+@RunWith(BaseJUnit4ClassRunner.class)
 public class RadioButtonWithDescriptionLayoutTest {
     private static final String NON_ZERO_MARGIN_ASSERT_MESSAGE =
             "First N-1 items should have a non-zero margin";
