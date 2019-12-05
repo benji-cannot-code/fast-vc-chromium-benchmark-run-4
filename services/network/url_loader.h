b9729493a8d4d9efb717deb04f129635cfd3b0e9
Yutaka Hirano <yhirano@chromium.org>
2019-12-05 06:24:00 +0000

    Add UMA histogram for the end state of keepalive requests
    For better understanding of request failures
    
    Bug: 1018050, 1009889
    Change-Id: I1581672378bff9668cf4066cbad872e16acdab9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950265
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721891}

--- services/network/url_loader.h
+++ services/network/url_loader.h
@@ -196,6 +196,17 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) URLLoader
   class FileOpenerForUpload;
   friend class FileOpenerForUpload;
 
+  // An enum class representing the result of keepalive requests. This is used
+  // for UMA so do NOT re-assign values.
+  enum class KeepaliveRequestResult {
+    kOk = 0,
+    kMojoConnectionErrorBeforeResponseArrival = 1,
+    kMojoConnectionErrorAfterResponseArrival = 2,
+    kErrorBeforeResponseArrival = 3,
+    kErrorAfterResponseArrival = 4,
+    kMaxValue = kErrorAfterResponseArrival,
+  };
+
   void OpenFilesForUpload(const ResourceRequest& request);
   void SetUpUpload(const ResourceRequest& request,
                    int error_code,
@@ -204,6 +215,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) URLLoader
   void ReadMore();
   void DidRead(int num_bytes, bool completed_synchronously);
   void NotifyCompleted(int error_code);
+  void RecordKeepaliveResult(KeepaliveRequestResult result);
   void OnMojoDisconnect();
   void OnResponseBodyStreamConsumerClosed(MojoResult result);
   void OnResponseBodyStreamReady(MojoResult result);
@@ -259,6 +271,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) URLLoader
   bool corb_detachable_;
   int resource_type_;
   bool is_load_timing_enabled_;
+  bool has_received_response_ = false;
+  bool has_recorded_keepalive_result_ = false;
 
   // URLLoaderFactory is guaranteed to outlive URLLoader, so it is safe to
   // store a raw pointer to mojom::URLLoaderFactoryParams.
