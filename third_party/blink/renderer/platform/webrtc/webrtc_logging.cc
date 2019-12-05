3b1b773a0cee2109d374a6cdad86b6563d3e2f74
henrika <henrika@chromium.org>
2019-12-05 14:19:17 +0000

    Fine tunes native logging for input audio streams
    Adds some more details to the new logging structure for audio input streams.
    
    - Keeps track of number of active input streams (can be used when looking for conflicts)
    - Adds a log for "alive" at first audio callback (first power log is done after 15 seconds)
    - Adds log for "power_measurement_is_enabled" to verify that the stream is a valid "WebRTC stream"
    where an analog AGC should work and the microphone level can be changed
    
    Example:
    
    AIC::DoCreate({device_id=default}) [id=1CB1F3D6D8BFC8C3C8B37C95FDEF9409]
    AMB::MakeAudioInputStream => (number of streams=1) [id=1CB1F3D6D8BFC8C3C8B37C95FDEF9409]
    AIC::DoCreate => (power_measurement_is_enabled=1) [id=1CB1F3D6D8BFC8C3C8B37C95FDEF9409]
    audio::IS::OnCreated({id=1CB1F3D6D8BFC8C3C8B37C95FDEF9409}, {muted=0})
    audio::IS::Record({id=1CB1F3D6D8BFC8C3C8B37C95FDEF9409})
    AIC::Record() [id=1CB1F3D6D8BFC8C3C8B37C95FDEF9409]
    audio::IS::SetOutputDeviceForAec({id=1CB1F3D6D8BFC8C3C8B37C95FDEF9409}, {output_device_id=})
    AIC::OnData => (stream is alive) [id=1CB1F3D6D8BFC8C3C8B37C95FDEF9409]
    
    Bug: 1017219
    Change-Id: I0ca63abc256b39eade787438d59e1ed232ba7c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950541
    Commit-Queue: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722013}

--- third_party/blink/renderer/platform/webrtc/webrtc_logging.cc
+++ third_party/blink/renderer/platform/webrtc/webrtc_logging.cc
@@ -26,6 +26,7 @@ void InitWebRtcLogging() {
 }
 
 void WebRtcLogMessage(const std::string& message) {
+  DVLOG(1) << message;
   if (g_webrtc_logging_delegate)
     g_webrtc_logging_delegate->LogMessage(message);
 }
