851c5c64b9cf8aa0fdccd0d642c5492dad18bb53
Philip Jägenstedt <foolip@chromium.org>
2019-12-05 10:45:06 +0000

    Measure sync XHR blocking time in main thread and workers
    This also removed the very similar but expired histogram
    WebURLLoader.SyncResourceRequestDuration.
    
    Bug: 586859
    Change-Id: I4993f164d7d60ada52b644528dc69a79ce5e36bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1944480
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721960}

--- third_party/blink/renderer/core/xmlhttprequest/xml_http_request.cc
+++ third_party/blink/renderer/core/xmlhttprequest/xml_http_request.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/feature_list.h"
+#include "base/metrics/histogram_macros.h"
+#include "base/time/time.h"
 #include "third_party/blink/public/common/blob/blob_utils.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink.h"
@@ -1177,10 +1179,21 @@ void XMLHttpRequest::CreateRequest(scoped_refptr<EncodedFormData> http_body,
   loader_ = MakeGarbageCollected<ThreadableLoader>(execution_context, this,
                                                    resource_loader_options);
   loader_->SetTimeout(timeout_);
+  base::TimeTicks start_time = base::TimeTicks::Now();
   loader_->Start(request);
 
-  if (!async_)
+  if (!async_) {
+    base::TimeDelta blocking_time = base::TimeTicks::Now() - start_time;
+    if (execution_context.IsDocument()) {
+      UMA_HISTOGRAM_MEDIUM_TIMES("XHR.Sync.BlockingTime.MainThread",
+                                 blocking_time);
+    } else {
+      UMA_HISTOGRAM_MEDIUM_TIMES("XHR.Sync.BlockingTime.WorkerThread",
+                                 blocking_time);
+    }
+
     ThrowForLoadFailureIfNeeded(exception_state, String());
+  }
 }
 
 void XMLHttpRequest::abort() {
