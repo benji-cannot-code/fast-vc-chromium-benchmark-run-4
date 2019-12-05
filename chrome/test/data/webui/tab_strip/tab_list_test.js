dc5db35b29956a7007cf152ceef8c228143f4620
John Lee <johntlee@chromium.org>
2019-12-05 05:18:32 +0000

    WebUI Tab Strip: Animate scroll position
    Since there is no native way to animate scroll with custom easing,
    speeds, and offsets, a manual animation is needed to animate the scroll
    position.
    
    This CL uses requestAnimationFrame to recursively update the scrollLeft
    property of the scrolling container. Depending on the refresh rate, the
    TabList will continuously update its own scroll position based on the
    time elapsed until the animation time is over. To customize easing, a
    deceleration function is used to generally mimic slowing of the
    animation towards the end.
    
    Bug: 1023492
    Change-Id: Ib89e01c8f9e1fccd3d62b43fb5e7841a1cc311a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1951544
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721867}

--- chrome/test/data/webui/tab_strip/tab_list_test.js
+++ chrome/test/data/webui/tab_strip/tab_list_test.js
@@ -3,10 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://tab-strip/tab_list.js';
-
 import {webUIListenerCallback} from 'chrome://resources/js/cr.m.js';
 import {FocusOutlineManager} from 'chrome://resources/js/cr/ui/focus_outline_manager.m.js';
+import {setScrollAnimationEnabledForTesting} from 'chrome://tab-strip/tab_list.js';
 import {TabStripEmbedderProxy} from 'chrome://tab-strip/tab_strip_embedder_proxy.js';
 import {TabsApiProxy} from 'chrome://tab-strip/tabs_api_proxy.js';
 
@@ -125,6 +124,8 @@ suite('TabList', () => {
     });
     testTabStripEmbedderProxy.setVisible(true);
     TabStripEmbedderProxy.instance_ = testTabStripEmbedderProxy;
+
+    setScrollAnimationEnabledForTesting(false);
 
     tabList = document.createElement('tabstrip-tab-list');
     document.body.appendChild(tabList);
