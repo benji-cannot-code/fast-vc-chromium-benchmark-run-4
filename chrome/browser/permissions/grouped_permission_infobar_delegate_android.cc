a055bb724cf3bc6af3a2798a179397db7e1faba0
Andy Paicu <andypaicu@chromium.org>
2019-12-05 13:05:54 +0000

    Using crowd deny string for mini-infobar when the reason for the...
    ...quiet ui is kTriggeredByCrowdDeny
    
    Bug: 1030633
    Change-Id: Ib548aea95a949b061654fe374581a5c6c910360f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1951007
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#721994}

--- chrome/browser/permissions/grouped_permission_infobar_delegate_android.cc
+++ chrome/browser/permissions/grouped_permission_infobar_delegate_android.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/android/android_theme_resources.h"
 #include "chrome/browser/infobars/infobar_service.h"
+#include "chrome/browser/permissions/notification_permission_ui_selector.h"
 #include "chrome/browser/permissions/permission_prompt_android.h"
 #include "chrome/browser/permissions/permission_request.h"
 #include "chrome/browser/permissions/permission_request_manager.h"
@@ -57,8 +58,20 @@ base::string16 GroupedPermissionInfoBarDelegate::GetCompactLinkText() const {
 }
 
 base::string16 GroupedPermissionInfoBarDelegate::GetDescriptionText() const {
-  return l10n_util::GetStringUTF16(
-      IDS_NOTIFICATION_QUIET_PERMISSION_PROMPT_MESSAGE);
+  auto* manager = PermissionRequestManager::FromWebContents(
+      permission_prompt_->web_contents());
+
+  switch (manager->ReasonForUsingQuietUi()) {
+    case NotificationPermissionUiSelector::QuietUiReason::kEnabledInPrefs:
+      return l10n_util::GetStringUTF16(
+          IDS_NOTIFICATION_QUIET_PERMISSION_PROMPT_MESSAGE);
+    case NotificationPermissionUiSelector::QuietUiReason::kTriggeredByCrowdDeny:
+      return l10n_util::GetStringUTF16(
+          IDS_NOTIFICATIONS_QUIET_PERMISSION_BUBBLE_CROWD_DENY_DESCRIPTION);
+  }
+
+  NOTREACHED();
+  return base::string16();
 }
 
 int GroupedPermissionInfoBarDelegate::GetIconId() const {
