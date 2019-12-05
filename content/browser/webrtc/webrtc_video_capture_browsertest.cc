07b482192baffe42f38e53680b867cfb84e8b173
Julie Jeongeun Kim <jkim@igalia.com>
2019-12-05 19:51:08 +0000

    Clean up old Mojo types for video_capture::mojom::TestingControls
    This CL cleans up old Mojo types for
    video_capture::mojom::TestingControls from
    WebRtcVideoCaptureBrowserTest using Remote.
    
    Bug: 955171
    Change-Id: I4e1dda9c63c3e71b229343829c5d4dc5c4b5fb22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1939141
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722158}

--- content/browser/webrtc/webrtc_video_capture_browsertest.cc
+++ content/browser/webrtc/webrtc_video_capture_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
 #include "media/base/media_switches.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "services/video_capture/public/mojom/testing_controls.mojom.h"
 
@@ -79,9 +80,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcVideoCaptureBrowserTest,
   ASSERT_EQ("OK", result);
 
   // Simulate crash in video capture process
-  video_capture::mojom::TestingControlsPtr service_controls;
+  mojo::Remote<video_capture::mojom::TestingControls> service_controls;
   GetVideoCaptureService().BindControlsForTesting(
-      mojo::MakeRequest(&service_controls));
+      service_controls.BindNewPipeAndPassReceiver());
   service_controls->Crash();
 
   // Wait for video element to turn black
