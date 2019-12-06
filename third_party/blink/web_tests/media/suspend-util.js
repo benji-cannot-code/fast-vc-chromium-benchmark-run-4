3974a0447321e6a907e58e684f0fb007600786c8
Dale Curtis <dalecurtis@chromium.org>
2019-12-06 00:31:07 +0000

    Enable web tests for media preload=metadata suspend and lazy loading.
    These were accidentally left disabled. Fix some flakiness and use the
    new video.rAF API to ensure they are testing the right thing.
    
    R=tguilbert
    
    Fixed: 694855
    Change-Id: I1f6b8211209e405e0d81bb6368494031a89453b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954316
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722310}

--- third_party/blink/web_tests/media/suspend-util.js
+++ third_party/blink/web_tests/media/suspend-util.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+let UPON_VISIBILITY = 'uponVisibility';
+
 // Requests that |video| suspends upon reaching or exceeding |expectedState|;
 // |callback| will be called once the suspend is detected.
 function suspendMediaElement(video, expectedState, callback) {
@@ -19,7 +21,8 @@ function suspendMediaElement(video, expectedState, callback) {
 function completeTestUponPlayback(t, video) {
   var timeWatcher = t.step_func(function() {
     if (video.currentTime > 0) {
-      assert_false(internals.isMediaElementSuspended(video));
+      assert_false(internals.isMediaElementSuspended(video),
+                   'Element should not be suspended.');
       t.done();
     } else {
       window.requestAnimationFrame(timeWatcher);
@@ -34,16 +37,40 @@ function preloadMetadataSuspendTest(t, video, src, expectSuspend) {
   assert_true(!!window.internals, 'This test requires windows.internals.');
   video.onerror = t.unreached_func();
 
+  var retried = false;
   var eventListener = t.step_func(function() {
-    assert_equals(expectSuspend,
-                  internals.isMediaElementSuspended(video));
+    var hasExpectedSuspendState =
+        !!expectSuspend == internals.isMediaElementSuspended(video);
+
+    // The delivery of 'loadedmetadata' may occur immediately before the element
+    // reaches the suspended state; so allow one retry in that case.
+    if (!hasExpectedSuspendState && !retried) {
+      retried = true;
+      setTimeout(eventListener, 0);
+      return;
+    }
+
+    assert_true(hasExpectedSuspendState,
+                'Element has incorrect suspend state.');
     if (!expectSuspend) {
       t.done();
       return;
     }
 
+    if (expectSuspend == UPON_VISIBILITY)
+      return;
+
     completeTestUponPlayback(t, video);
   });
+
+  if (expectSuspend == UPON_VISIBILITY) {
+    video.requestAnimationFrame(t.step_func(function() {
+      assert_false(
+          internals.isMediaElementSuspended(video),
+          'Element should not have been suspended by the first frame.');
+      t.done();
+    }));
+  }
 
   video.addEventListener('loadedmetadata', eventListener, false);
   video.src = src;
@@ -56,7 +83,8 @@ function suspendTest(t, video, src, expectedState) {
   // We can't force a suspend state until loading has started.
   video.addEventListener('loadstart', t.step_func(function() {
     suspendMediaElement(video, expectedState, t.step_func(function() {
-      assert_true(internals.isMediaElementSuspended(video));
+      assert_true(internals.isMediaElementSuspended(video),
+                  'Element did not suspend as expected.');
       completeTestUponPlayback(t, video);
     }));
   }), false);
