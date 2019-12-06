f99947434cce513ef1da092561c2092ce6dfd3c5
Antonio Gomes <tonikitoo@igalia.com>
2019-12-06 13:22:59 +0000

    Switch RTCRtpSenderPlatform away from WebString
    Note that the use of WebVector deliverately kept as part
    of this clean up, since doing it now would create a way bigger
    CL.
    
    BUG=787254
    R=guidou@chromium.org
    
    Change-Id: Ie85cf379ae0b73902dfc30de3e4725c07e0cd290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950688
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722455}

--- third_party/blink/renderer/platform/peerconnection/rtc_rtp_sender_platform.h
+++ third_party/blink/renderer/platform/peerconnection/rtc_rtp_sender_platform.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "third_party/blink/public/platform/web_rtc_stats.h"
-#include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/webrtc/api/dtls_transport_interface.h"
 #include "third_party/webrtc/api/rtp_parameters.h"
 #include "third_party/webrtc/api/stats/rtc_stats.h"
@@ -40,7 +40,7 @@ class PLATFORM_EXPORT RTCRtpSenderPlatform {
   // The information is only interesting if DtlsTransport() is non-null.
   virtual webrtc::DtlsTransportInformation DtlsTransportInformation() = 0;
   virtual WebMediaStreamTrack Track() const = 0;
-  virtual WebVector<WebString> StreamIds() const = 0;
+  virtual WebVector<String> StreamIds() const = 0;
   // TODO(hbos): Replace RTCVoidRequest by something resolving promises based
   // on RTCError, as to surface both exception type and error message.
   // https://crbug.com/790007
@@ -52,7 +52,7 @@ class PLATFORM_EXPORT RTCRtpSenderPlatform {
                              RTCVoidRequest*) = 0;
   virtual void GetStats(blink::WebRTCStatsReportCallback,
                         const WebVector<webrtc::NonStandardGroupId>&) = 0;
-  virtual void SetStreams(const WebVector<WebString>& stream_ids) = 0;
+  virtual void SetStreams(const WebVector<String>& stream_ids) = 0;
 };
 
 }  // namespace blink
