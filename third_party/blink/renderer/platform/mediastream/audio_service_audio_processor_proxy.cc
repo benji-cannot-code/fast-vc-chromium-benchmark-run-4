2bf44d23c93733e141bb640b711a9cd8646e9163
Antonio Gomes <tonikitoo@igalia.com>
2019-12-06 12:38:10 +0000

    Switch audio_service_audio_processor_proxy.cc away from base::Bind
    BUG=704136
    R=guidou@chromium.org
    
    Change-Id: Ic0acd94b796f18d40cc806ada0f33813d56d4331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949343
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#722448}

--- third_party/blink/renderer/platform/mediastream/audio_service_audio_processor_proxy.cc
+++ third_party/blink/renderer/platform/mediastream/audio_service_audio_processor_proxy.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/bind.h"
 #include "base/single_thread_task_runner.h"
 #include "base/task/post_task.h"
 #include "base/timer/timer.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/mediastream/aec_dump_agent_impl.h"
 #include "third_party/blink/renderer/platform/scheduler/public/worker_pool.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
 
@@ -120,16 +120,16 @@ void AudioServiceAudioProcessorProxy::RescheduleStatsUpdateTimer(
   // Unretained is safe since |this| owns |stats_update_timer_|.
   stats_update_timer_.Start(
       FROM_HERE, new_interval,
-      base::BindRepeating(&AudioServiceAudioProcessorProxy::RequestStats,
-                          base::Unretained(this)));
+      WTF::BindRepeating(&AudioServiceAudioProcessorProxy::RequestStats,
+                         WTF::Unretained(this)));
 }
 
 void AudioServiceAudioProcessorProxy::RequestStats() {
   DCHECK(main_thread_runner_->BelongsToCurrentThread());
   if (processor_controls_) {
     processor_controls_->GetStats(
-        base::BindOnce(&AudioServiceAudioProcessorProxy::UpdateStats,
-                       weak_ptr_factory_.GetWeakPtr()));
+        WTF::Bind(&AudioServiceAudioProcessorProxy::UpdateStats,
+                  weak_ptr_factory_.GetWeakPtr()));
   }
 }
 
