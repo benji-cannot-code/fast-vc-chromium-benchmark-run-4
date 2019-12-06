82f95bbda4072560a3d84dd01e6c7f9374abea30
Yao Xiao <yaoxia@chromium.org>
2019-12-06 19:17:17 +0000

    Change third-party UseCounters from origin to etld+1
    What: UseCounters kThirdPartyServiceWorker/kThirdPartySharedWorker/
    kThirdPartyBroadcastChannel considers a frame to be third-party if its
    origin differs from the top frame. Change it to be based on eTLD+1
    comparison instead.
    
    Why: eTLD+1 is the more natural delimitation between same-site and
    not, as it's what is used by cookies and site isolation.
    
    Bug: 976866
    Change-Id: Ib95881f3aad95af183540869f92ce6f739b4de45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1951562
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722556}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/mojom/coordination_unit.mojom-blink.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "services/metrics/public/cpp/mojo_ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -8663,6 +8664,18 @@ bool Document::InForcedColorsMode() const {
          Platform::Current()->ThemeEngine() &&
          Platform::Current()->ThemeEngine()->GetForcedColors() !=
              ForcedColors::kNone;
+}
+
+bool Document::IsCrossSiteSubframe() const {
+  // It'd be nice to avoid the url::Origin temporaries, but that would require
+  // exposing the net internal helper.
+  // TODO: If the helper gets exposed, we could do this without any new
+  // allocations using StringUTF8Adaptor.
+  return TopFrameOrigin() &&
+         !net::registry_controlled_domains::SameDomainOrHost(
+             TopFrameOrigin()->ToUrlOrigin(),
+             GetSecurityOrigin()->ToUrlOrigin(),
+             net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
 }
 
 void Document::CountUse(mojom::WebFeature feature) const {
