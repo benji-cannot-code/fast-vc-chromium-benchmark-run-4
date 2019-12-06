9e134deab51f1511d1dadf6bc0b835d97072b4be
Ken MacKay <kmackay@chromium.org>
2019-12-06 20:58:18 +0000

    [Chromecast] Use IO task runner for multizone_backend_unittests
    Bug: internal b/145718333
    Change-Id: Ie315ccc944536f8df034e0dd30ab39b599cca855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1955027
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722606}

--- chromecast/media/cma/backend/multizone_backend_unittest.cc
+++ chromecast/media/cma/backend/multizone_backend_unittest.cc
@@ -311,7 +311,8 @@ void BufferFeeder::OnPushBufferComplete(BufferStatus status) {
 
 }  // namespace
 
-MultizoneBackendTest::MultizoneBackendTest() {}
+MultizoneBackendTest::MultizoneBackendTest()
+    : task_environment_(base::test::TaskEnvironment::MainThreadType::IO) {}
 
 MultizoneBackendTest::~MultizoneBackendTest() {}
 
