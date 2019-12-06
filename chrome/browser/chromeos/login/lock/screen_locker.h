56617e7a23cffb433f9dc8fbeba1189cc46f0da9
Roman Sorokin <rsorokin@chromium.org>
2019-12-06 08:12:44 +0000

    cros: Ensure the active user can not be changed in the locked state
    Added CHECK in the ScreenLocker
    
    Bug: 1024555
    Change-Id: I4cf76bff08b6911a914be20619767f5c4ad1a72a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1939709
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722403}

--- chrome/browser/chromeos/login/lock/screen_locker.h
+++ chrome/browser/chromeos/login/lock/screen_locker.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/login/auth/challenge_response_key.h"
 #include "chromeos/login/auth/user_context.h"
 #include "components/user_manager/user.h"
+#include "components/user_manager/user_manager.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/device/public/mojom/fingerprint.mojom.h"
@@ -44,8 +45,10 @@ class ViewsScreenLocker;
 // ScreenLocker displays the lock UI and takes care of authenticating the user
 // and managing a global instance of itself which will be deleted when the
 // system is unlocked.
-class ScreenLocker : public AuthStatusConsumer,
-                     public device::mojom::FingerprintObserver {
+class ScreenLocker
+    : public AuthStatusConsumer,
+      public device::mojom::FingerprintObserver,
+      public user_manager::UserManager::UserSessionStateObserver {
  public:
   // Delegate used to send internal state changes back to the UI.
   class Delegate {
@@ -173,6 +176,9 @@ class ScreenLocker : public AuthStatusConsumer,
       override;
   void OnSessionFailed() override;
 
+  // user_manager::UserManager::UserSessionStateObserver:
+  void ActiveUserChanged(user_manager::User* active_user) override;
+
  private:
   friend class base::DeleteHelper<ScreenLocker>;
   friend class ViewsScreenLocker;
@@ -262,6 +268,11 @@ class ScreenLocker : public AuthStatusConsumer,
   // from false to true, but will never change from true to
   // false. Instead, ScreenLocker object gets deleted when unlocked.
   bool locked_ = false;
+
+  // True if the unlock process has started, or false otherwise.  This changes
+  // from false to true, but will never change from true to false. Instead,
+  // ScreenLocker object gets deleted when unlocked.
+  bool unlock_started_ = false;
 
   // Reference to the single instance of the screen locker object.
   // This is used to make sure there is only one screen locker instance.
