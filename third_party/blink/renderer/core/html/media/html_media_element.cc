ba7b10b9fd43fe50ceec3526b0f48a3957cb7f88
Dale Curtis <dalecurtis@chromium.org>
2019-12-06 18:53:21 +0000

    Prevent leaking of pre-metadata HTMLMediaElements.
    The element's networkState is used to determine if an element can be
    GC'd, but it is not reliable prior to readyState kHaveMetadata due to
    possible side channel attacks for non-media resources.
    
    There are three fixes in this patch:
    1. Checking src= attribute was unnecessary. Should just trust the
    response from IsMediaDataCorsSameOrigin() which will either have a
    WebMediaPlayer and thus a src or will return false.
    2. Once the element reaches the kHaveMetadata state it must ensure
    its networkState matches the WebMediaPlayer's.
    3. Don't use |network_state_| but instead the WebMediaPlayer's when
    checking HasPendingActivity().
    
    The first resolves incorrect state considerations when <source> is used
    instead of src=. The second ensures the element's network state is
    properly reflected once metadata is reached. The third ensures that
    even non-media elements will be GC'd properly.
    
    BUG=1025012
    TEST=new unittest
    
    Change-Id: Idf62093d3ea4833311e6aaefe041e2f073fb8622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931377
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722548}

--- third_party/blink/renderer/core/html/media/html_media_element.cc
+++ third_party/blink/renderer/core/html/media/html_media_element.cc
@@ -1758,6 +1758,10 @@ void HTMLMediaElement::ChangeNetworkStateFromLoadingToIdle() {
       ScheduleEvent(event_type_names::kProgress);
     ScheduleEvent(event_type_names::kSuspend);
     SetNetworkState(kNetworkIdle);
+  } else {
+    // TODO(dalecurtis): Replace c-style casts in follow up patch.
+    DVLOG(1) << __func__ << "(" << (void*)this
+             << ") - Deferred network state change to idle for opaque media";
   }
 }
 
@@ -1800,6 +1804,10 @@ void HTMLMediaElement::SetReadyState(ReadyState state) {
       web_media_player_) {
     current_src_after_redirects_ =
         KURL(web_media_player_->GetSrcAfterRedirects());
+
+    // Prior to kHaveMetadata |network_state_| may be inaccurate to avoid side
+    // channel leaks. This be a no-op if nothing has changed.
+    NetworkStateChanged();
   }
 
   if (new_state > ready_state_maximum_)
@@ -3607,6 +3615,13 @@ void HTMLMediaElement::ContextDestroyed(ExecutionContext*) {
 }
 
 bool HTMLMediaElement::HasPendingActivity() const {
+  const auto result = HasPendingActivityInternal();
+  // TODO(dalecurtis): Replace c-style casts in followup patch.
+  DVLOG(3) << "HasPendingActivity(" << (void*)this << ") = " << result;
+  return result;
+}
+
+bool HTMLMediaElement::HasPendingActivityInternal() const {
   // The delaying-the-load-event flag is set by resource selection algorithm
   // when looking for a resource to load, before networkState has reached to
   // kNetworkLoading.
@@ -3623,8 +3638,18 @@ bool HTMLMediaElement::HasPendingActivity() const {
   // MediaSource API objects. This lets the group of objects be garbage
   // collected if there is no pending activity nor reachability from a GC root,
   // even while in kNetworkLoading.
-  if (!media_source_ && network_state_ == kNetworkLoading)
-    return true;
+  //
+  // We use the WebMediaPlayer's network state instead of |network_state_| since
+  // it's value is unreliable prior to ready state kHaveMetadata.
+  if (!media_source_) {
+    const auto* wmp = GetWebMediaPlayer();
+    if (!wmp) {
+      if (network_state_ == kNetworkLoading)
+        return true;
+    } else if (wmp->GetNetworkState() == WebMediaPlayer::kNetworkStateLoading) {
+      return true;
+    }
+  }
 
   {
     // Disable potential updating of playback position, as that will
@@ -4244,7 +4269,6 @@ void HTMLMediaElement::RequestMuted(bool muted) {
 
 bool HTMLMediaElement::MediaShouldBeOpaque() const {
   return !IsMediaDataCorsSameOrigin() && ready_state_ < kHaveMetadata &&
-         !FastGetAttribute(html_names::kSrcAttr).IsEmpty() &&
          EffectivePreloadType() != WebMediaPlayer::kPreloadNone;
 }
 
