b619270dfd19ae88c8455c9f95eb6793f53fa092
James Cook <jamescook@chromium.org>
2019-12-06 19:10:33 +0000

    sync: Use transport mode for ARC_PACKAGE with SplitSettingsSync
    Chrome OS SplitSettingsSync has a separate control for syncing OS data
    types that isn't controlled by the browser's sync-the-feature. Change
    ModelType::ARC_PACKAGE to run when the transport layer is enabled.
    Add a check for pref kOsSyncFeatureEnabled so the type is controlled
    by the OS sync consent.
    
    Bug: 1013466
    Test: updated sync_integration_tests
    Change-Id: Ia3b05dde436412c74fe05d8ad40c5053755acdcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954725
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722554}

--- chrome/browser/sync/test/integration/single_client_standalone_transport_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_standalone_transport_sync_test.cc
@@ -326,6 +326,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientStandaloneTransportOsSyncTest,
 
   // OS data types synced by the transport layer are active.
   syncer::ModelTypeSet active_types = service->GetActiveDataTypes();
+  EXPECT_TRUE(active_types.Has(syncer::ARC_PACKAGE));
   EXPECT_TRUE(active_types.Has(syncer::OS_PREFERENCES));
   EXPECT_TRUE(active_types.Has(syncer::OS_PRIORITY_PREFERENCES));
 
@@ -355,6 +356,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientStandaloneTransportOsSyncTest,
 
   // OS data types synced by the transport layer are not active.
   syncer::ModelTypeSet active_types = service->GetActiveDataTypes();
+  EXPECT_FALSE(active_types.Has(syncer::ARC_PACKAGE));
   EXPECT_FALSE(active_types.Has(syncer::OS_PREFERENCES));
   EXPECT_FALSE(active_types.Has(syncer::OS_PRIORITY_PREFERENCES));
 
