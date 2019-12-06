44d94b1344a0506eaba6eb301db9c26a1980b370
Mohamed Amir Yosef <mamir@chromium.org>
2019-12-06 11:02:13 +0000

    [Passwords] Implement MultiStorePasswordSaveManager::Clone()
    Bug: 1012203
    Change-Id: I91eb4f454cc899e444fda0f3983341c239ad6867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1943233
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722433}

--- components/password_manager/core/browser/multi_store_password_save_manager.h
+++ components/password_manager/core/browser/multi_store_password_save_manager.h
@@ -42,6 +42,8 @@ class MultiStorePasswordSaveManager : public PasswordSaveManagerImpl {
       const PasswordStore::FormDigest& form_digest) override;
   void Unblacklist(const PasswordStore::FormDigest& form_digest) override;
 
+  std::unique_ptr<PasswordSaveManager> Clone() override;
+
  protected:
   FormSaver* GetFormSaverForGeneration() override;
 
