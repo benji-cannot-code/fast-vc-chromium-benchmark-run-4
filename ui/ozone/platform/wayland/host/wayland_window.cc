4eea7406f044b1e60200b22652b8d08d0e643b0a
Maksim Sisov <msisov@igalia.com>
2019-12-06 19:19:30 +0000

    ozone/wayland: Fix regression with hiding nested windows.
    It must be non-tooltip that closes children windows.
    
    Bug: 1028046
    Change-Id: I9b5212873cb2c1a4179a28e915edd5fe84b29c5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954483
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#722560}

--- ui/ozone/platform/wayland/host/wayland_window.cc
+++ ui/ozone/platform/wayland/host/wayland_window.cc
@@ -389,7 +389,7 @@ void WaylandWindow::Show(bool inactive) {
 }
 
 void WaylandWindow::Hide() {
-  if (!is_tooltip_) {
+  if (is_tooltip_) {
     tooltip_subsurface_.reset();
   } else {
     if (child_window_)
