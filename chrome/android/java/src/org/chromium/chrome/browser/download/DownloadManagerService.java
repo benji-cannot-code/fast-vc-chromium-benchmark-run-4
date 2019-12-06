3b85c3be8260c1e33fd71cf141b05587f20abaf8
Ted Choc <tedchoc@chromium.org>
2019-12-06 18:52:15 +0000

    Update Java ProfileManager onProfileAdded to be consistent with native.
    BUG=
    
    Change-Id: I6ef4f5f9652027844c57e76535577b16cd7b9bbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1955174
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722547}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadManagerService.java
@@ -1206,9 +1206,9 @@ public class DownloadManagerService implements DownloadController.DownloadNotifi
     }
 
     @Override
-    public void onProfileCreated(Profile profile) {
+    public void onProfileAdded(Profile profile) {
         ProfileManager.removeObserver(this);
-        DownloadManagerServiceJni.get().onProfileCreated(
+        DownloadManagerServiceJni.get().onProfileAdded(
                 mNativeDownloadManagerService, DownloadManagerService.this);
     }
 
@@ -2055,7 +2055,7 @@ public class DownloadManagerService implements DownloadController.DownloadNotifi
     interface Natives {
         boolean isSupportedMimeType(String mimeType);
         int getAutoResumptionLimit();
-        long init(DownloadManagerService caller, boolean isProfileCreated);
+        long init(DownloadManagerService caller, boolean isProfileAdded);
         void openDownload(long nativeDownloadManagerService, DownloadManagerService caller,
                 String downloadGuid, boolean isOffTheRecord, int source);
         void resumeDownload(long nativeDownloadManagerService, DownloadManagerService caller,
@@ -2077,7 +2077,7 @@ public class DownloadManagerService implements DownloadController.DownloadNotifi
                 DownloadManagerService caller, boolean isOffTheRecord);
         void updateLastAccessTime(long nativeDownloadManagerService, DownloadManagerService caller,
                 String downloadGuid, boolean isOffTheRecord);
-        void onProfileCreated(long nativeDownloadManagerService, DownloadManagerService caller);
+        void onProfileAdded(long nativeDownloadManagerService, DownloadManagerService caller);
         void createInterruptedDownloadForTest(long nativeDownloadManagerService,
                 DownloadManagerService caller, String url, String guid, String targetPath);
         void recordFirstBackgroundInterruptReason(long nativeDownloadManagerService,
--- chrome/android/public/profiles/java/src/org/chromium/chrome/browser/profiles/ProfileManager.java
+++ chrome/android/public/profiles/java/src/org/chromium/chrome/browser/profiles/ProfileManager.java
@@ -21,7 +21,7 @@ public class ProfileManager {
          * Called whenever a profile is created.
          * @param profile The profile that has just been created.
          */
-        public void onProfileCreated(Profile profile);
+        public void onProfileAdded(Profile profile);
     }
 
     /**
@@ -50,7 +50,7 @@ public class ProfileManager {
         // If a profile has been added, we know the ProfileManager has been initialized.
         sInitialized = true;
         for (Observer observer : sObservers) {
-            observer.onProfileCreated(profile);
+            observer.onProfileAdded(profile);
         }
     }
 }
