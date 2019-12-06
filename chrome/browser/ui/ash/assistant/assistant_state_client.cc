64af55c71175d78c13fbea79fd7bed2d48479b95
Yue Li <updowndota@chromium.org>
2019-12-06 00:13:23 +0000

    Fix timing issue for SAA bit
    Add handlings for the AssistantDisabledByPolicy prefs to fix the timing
    issues for the first run.
    
    Bug: b/143101359
    Change-Id: Ic7f726c2414b99d65d16604f3634d0f05142bf19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1952283
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722304}

--- chrome/browser/ui/ash/assistant/assistant_state_client.cc
+++ chrome/browser/ui/ash/assistant/assistant_state_client.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/assistant/assistant_util.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
@@ -77,6 +78,11 @@ void AssistantStateClient::SetProfile(Profile* profile) {
   pref_change_registrar_->Add(
       language::prefs::kApplicationLocale,
       base::BindRepeating(&AssistantStateClient::NotifyLocaleChanged,
+                          base::Unretained(this)));
+
+  pref_change_registrar_->Add(
+      chromeos::assistant::prefs::kAssistantDisabledByPolicy,
+      base::BindRepeating(&AssistantStateClient::NotifyFeatureAllowed,
                           base::Unretained(this)));
 
   NotifyLocaleChanged();
