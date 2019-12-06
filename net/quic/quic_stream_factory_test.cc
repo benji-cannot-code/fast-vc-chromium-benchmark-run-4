9c454f1fd5c534c940be78367affe6842d1ba7f8
Renjie Tang <renjietang@chromium.org>
2019-12-06 22:05:07 +0000

    Clean up TestPacketMaker methods related to multiple data frames. Data frames will now be coalesced into one.
    Change-Id: Ibd65d7098cbb81d96d1325333867892cd3ce2c4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1956008
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722636}

--- net/quic/quic_stream_factory_test.cc
+++ net/quic/quic_stream_factory_test.cc
@@ -490,7 +490,6 @@ class QuicStreamFactoryTestBase : public WithTaskEnvironment {
     socket_data1.AddRead(SYNCHRONOUS, ERR_IO_PENDING);
     socket_data1.AddSocketDataToFactory(socket_factory_.get());
 
-    client_maker_.set_coalesce_http_frames(true);
     // Set up second socket data provider that is used after
     // migration.
     MockQuicData socket_data2(version_);
@@ -1616,7 +1615,6 @@ TEST_P(QuicStreamFactoryTest, PoolingWithServerMigration) {
                                quic::ConnectionCloseBehavior::SILENT_CLOSE);
 
   client_maker_.Reset();
-  client_maker_.set_coalesce_http_frames(false);
   // Set up server IP, socket, proof, and config for new session.
   HostPortPair server2(kServer2HostName, kDefaultServerPort);
   host_resolver_->rules()->AddIPLiteralRule(server2.host(), "192.168.0.1", "");
@@ -5568,7 +5566,6 @@ TEST_P(QuicStreamFactoryTest, MigrateSessionOnAysncWriteError) {
       ConstructGetRequestPacket(packet_num++,
                                 GetNthClientInitiatedBidirectionalStreamId(0),
                                 true, true));
-  client_maker_.set_coalesce_http_frames(true);
   socket_data1.AddWrite(
       SYNCHRONOUS,
       ConstructGetRequestPacket(
@@ -9039,7 +9036,6 @@ TEST_P(QuicStreamFactoryTest,
       ConstructGetRequestPacket(packet_num++,
                                 GetNthClientInitiatedBidirectionalStreamId(0),
                                 true, true));
-  client_maker_.set_coalesce_http_frames(true);
   socket_data1.AddRead(
       ASYNC,
       ConstructOkResponsePacket(
@@ -9206,7 +9202,6 @@ void QuicStreamFactoryTestBase::
       ConstructGetRequestPacket(packet_num++,
                                 GetNthClientInitiatedBidirectionalStreamId(0),
                                 true, true));
-  client_maker_.set_coalesce_http_frames(true);
   socket_data1.AddRead(
       ASYNC,
       ConstructOkResponsePacket(
