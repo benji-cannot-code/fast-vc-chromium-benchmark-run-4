7ff2393b405b0318d28d96b681b343a735f200c2
Karolina Soltys <ksolt@chromium.org>
2019-12-06 02:22:13 +0000

    [bfcache] Hide PasswordGenerationPopup on navigating away.
    BUG=1001087
    
    Change-Id: I5ae2a96ce978864e857c03ccc306019276fdebf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1840653
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722357}

--- chrome/browser/ui/passwords/password_generation_popup_controller_impl.cc
+++ chrome/browser/ui/passwords/password_generation_popup_controller_impl.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_driver.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/native_web_keyboard_event.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -258,6 +259,14 @@ void PasswordGenerationPopupControllerImpl::DidAttachInterstitialPage() {
 
 void PasswordGenerationPopupControllerImpl::WebContentsDestroyed() {
   Hide();
+}
+
+void PasswordGenerationPopupControllerImpl::DidFinishNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (navigation_handle->HasCommitted() && navigation_handle->IsInMainFrame() &&
+      !navigation_handle->IsSameDocument()) {
+    Hide();
+  }
 }
 
 #if !defined(OS_ANDROID)
