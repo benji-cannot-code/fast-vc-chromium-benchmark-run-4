db734fe120db3f0b6f5c1d175bfb507cfb9c4bf6
Mark Mentovai <mark@chromium.org>
2019-12-06 18:21:46 +0000

    mac: When interfacing with Keystone, try ksadmin at its new path first
    ksadmin moved from MacOS to Helpers in Keystone 1.2.13.112, 2019-11-12.
    A symbolic link from the old location was left in place, but may not
    remain indefinitely. Try the new location first, falling back to the old
    if needed.
    
    Change-Id: I278746a44ab35795a2294d812fd26b676b96d7c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950747
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722533}

--- remoting/host/installer/mac/uninstaller/remoting_uninstaller.mm
+++ remoting/host/installer/mac/uninstaller/remoting_uninstaller.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/installer/mac/uninstaller/remoting_uninstaller.h"
 
 #import <Cocoa/Cocoa.h>
+#include <sys/stat.h>
 
 #include "base/mac/authorization_util.h"
 #include "base/mac/scoped_authorizationref.h"
@@ -34,12 +35,6 @@ NSArray* convertToNSArray(const char** array) {
 }
 
 @implementation RemotingUninstaller
-
-// Keystone
-const char kKeystoneAdmin[] = "/Library/Google/GoogleSoftwareUpdate/"
-                              "GoogleSoftwareUpdate.bundle/Contents/MacOS/"
-                              "ksadmin";
-const char kKeystonePID[] = "com.google.chrome_remote_desktop";
 
 - (void)runCommand:(const char*)cmd
      withArguments:(const char**)args {
@@ -123,8 +118,29 @@ const char kKeystonePID[] = "com.google.chrome_remote_desktop";
 }
 
 - (void)keystoneUnregisterUsingAuth:(AuthorizationRef)authRef {
-  const char* args[] = {"--delete", "--productid", kKeystonePID, "-S", nullptr};
-  [self sudoCommand:kKeystoneAdmin withArguments:args usingAuth:authRef];
+  // ksadmin moved from MacOS to Helpers in Keystone 1.2.13.112, 2019-11-12. A
+  // symbolic link from the old location was left in place, but may not remain
+  // indefinitely. Try the new location first, falling back to the old if
+  // needed.
+  static const char kKSAdminPath[] =
+      "/Library/Google/GoogleSoftwareUpdate/"
+      "GoogleSoftwareUpdate.bundle/Contents/Helpers/"
+      "ksadmin";
+  static const char kKSAdminOldPath[] =
+      "/Library/Google/GoogleSoftwareUpdate/"
+      "GoogleSoftwareUpdate.bundle/Contents/MacOS/"
+      "ksadmin";
+
+  struct stat statbuf;
+  const char* ksadminPath =
+      (stat(kKSAdminPath, &statbuf) == 0 && (statbuf.st_mode & S_IXUSR))
+          ? kKSAdminPath
+          : kKSAdminOldPath;
+
+  static const char kKSProductID[] = "com.google.chrome_remote_desktop";
+
+  const char* args[] = {"--delete", "--productid", kKSProductID, "-S", nullptr};
+  [self sudoCommand:ksadminPath withArguments:args usingAuth:authRef];
 }
 
 - (void)remotingUninstallUsingAuth:(AuthorizationRef)authRef {
