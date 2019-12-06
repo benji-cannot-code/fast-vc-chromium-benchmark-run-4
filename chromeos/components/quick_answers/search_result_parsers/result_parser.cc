a0793333896ec4618b8c67a7c79be28f1c74eebb
llin@chromium.org <llin@chromium.org>
2019-12-06 22:12:10 +0000

    Add definition result parser to result parser factory.
    Bug: b/144316700
    Test: manual test
    Change-Id: I5b0abdfe3358a2416a213bf0661cac560869b312
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1955803
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722638}

--- chromeos/components/quick_answers/search_result_parsers/result_parser.cc
+++ chromeos/components/quick_answers/search_result_parsers/result_parser.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/components/quick_answers/search_result_parsers/result_parser.h"
 
 #include "base/values.h"
+#include "chromeos/components/quick_answers/search_result_parsers/definition_result_parser.h"
 #include "chromeos/components/quick_answers/search_result_parsers/translation_result_parser.h"
 #include "chromeos/components/quick_answers/search_result_parsers/unit_conversion_result_parser.h"
 
@@ -38,9 +39,11 @@ const Value* ResultParser::GetFirstListElement(const Value& value,
 std::unique_ptr<ResultParser> ResultParserFactory::Create(
     int one_namespace_type) {
   switch (static_cast<ResultType>(one_namespace_type)) {
+    case ResultType::kDefinitionResult:
+      return std::make_unique<DefinitionResultParser>();
     case ResultType::kTranslationResult:
       return std::make_unique<TranslationResultParser>();
-    case ResultType::kUnitCconverterResult:
+    case ResultType::kUnitConvertionResult:
       return std::make_unique<UnitConversionResultParser>();
       // TODO(llin): Add other result parsers.
   }
