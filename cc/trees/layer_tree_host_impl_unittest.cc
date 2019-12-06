eff825ed9eb2fe6db26b01aece17dd3515abad5e
David Bokan <bokan@chromium.org>
2019-12-06 18:47:37 +0000

    [Refactor] Remove current_native_scrolling_node
    This is redundant and confusing with current_native_scrolling_element.
    This CL replaces uses of current_native_scrolling_node by using
    current_native_scrolling_element. However, I believe the functionality
    isn't necessary at all. A future CL will refactor the code to not rely
    on setting this value at all.
    
    Bug: 1016229
    Change-Id: I92e915093907b7fe48fbed34c8fa8c961f733441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1952829
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722544}

--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -13108,8 +13108,6 @@ TEST_F(LayerTreeHostImplTest, ScrollByScrollingNode) {
   // still the same. |scroll_state|'s scrolling node should match
   // it.
   EXPECT_EQ(scroll_node, scroll_tree.CurrentlyScrollingNode());
-  EXPECT_EQ(scroll_state->data()->current_native_scrolling_node(),
-            scroll_tree.CurrentlyScrollingNode());
   EXPECT_EQ(scroll_state->data()->current_native_scrolling_element(),
             scroll_tree.CurrentlyScrollingNode()->element_id);
 
@@ -13121,8 +13119,6 @@ TEST_F(LayerTreeHostImplTest, ScrollByScrollingNode) {
   host_impl_->ScrollBy(scroll_state.get());
 
   EXPECT_EQ(scroll_node, scroll_tree.CurrentlyScrollingNode());
-  EXPECT_EQ(scroll_state->data()->current_native_scrolling_node(),
-            scroll_tree.CurrentlyScrollingNode());
   EXPECT_EQ(scroll_state->data()->current_native_scrolling_element(),
             scroll_tree.CurrentlyScrollingNode()->element_id);
 }
