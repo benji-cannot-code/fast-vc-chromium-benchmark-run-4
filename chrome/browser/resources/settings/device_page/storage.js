3906cc6bdb6b6663cbe0d2796d271f45b27c7939
Jeremie Boulic <jboulic@chromium.org>
2019-12-07 03:30:23 +0000

    Display My files size instead of Downloads
    On the storage management settings page, replace the label "Downloads"
    with "My files".
    
    Instead of displaying the size of the Downloads directory:
    - Compute the size of the user's personal files.
    - Add the size of Play files (Music, Pictures etc... excluding apps and
    system files).
    
    Test: Manually tested. Automated tests to be implemented in follow-up CL.
    Bug: 1013459
    Change-Id: I5ef7e5aacd1162dd219b6a093e79971f39fa65cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1866120
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722737}

--- chrome/browser/resources/settings/device_page/storage.js
+++ chrome/browser/resources/settings/device_page/storage.js
@@ -87,8 +87,8 @@ Polymer({
     this.addWebUIListener(
         'storage-size-stat-changed', this.handleSizeStatChanged_.bind(this));
     this.addWebUIListener(
-        'storage-downloads-size-changed',
-        this.handleDownloadsSizeChanged_.bind(this));
+        'storage-my-files-size-changed',
+        this.handleMyFilesSizeChanged_.bind(this));
     this.addWebUIListener(
         'storage-browsing-data-size-changed',
         this.handleBrowsingDataSizeChanged_.bind(this));
@@ -142,11 +142,11 @@ Polymer({
   },
 
   /**
-   * Handler for tapping the "Downloads" item.
+   * Handler for tapping the "My files" item.
    * @private
    */
-  onDownloadsTap_: function() {
-    chrome.send('openDownloads');
+  onMyFilesTap_: function() {
+    chrome.send('openMyFiles');
   },
 
   /**
@@ -205,11 +205,11 @@ Polymer({
   },
 
   /**
-   * @param {string} size Formatted string representing the size of Downloads.
+   * @param {string} size Formatted string representing the size of My files.
    * @private
    */
-  handleDownloadsSizeChanged_: function(size) {
-    this.$.downloadsSize.subLabel = size;
+  handleMyFilesSizeChanged_: function(size) {
+    this.$.myFilesSize.subLabel = size;
   },
 
   /**
