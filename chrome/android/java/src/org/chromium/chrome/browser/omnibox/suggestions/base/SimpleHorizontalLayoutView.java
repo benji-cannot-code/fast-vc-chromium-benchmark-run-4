2f76ee42e1dc19d95bd8b0cce7658d35e2713eca
Tomasz Wiszkowski <ender@google.com>
2019-12-07 01:24:11 +0000

    SimpleHorizontalLayout: add support for horizontal padding.
    This change is needed to properly manage suggestions with no
    decoration icons. These suggestions will receive additional padding
    at the front of the view.
    
    Bug: 1021263
    Change-Id: I78c3d8ef6ad95882a812d9a076c36fe9dd3d29e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1955088
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#722719}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/SimpleHorizontalLayoutView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/SimpleHorizontalLayoutView.java
@@ -59,7 +59,7 @@ class SimpleHorizontalLayoutView extends ViewGroup {
         final int height = getMeasuredHeight();
         int index = isRtl ? getChildCount() - 1 : 0;
 
-        left = 0;
+        left = getPaddingLeft();
 
         for (; index >= 0 && index < getChildCount(); index += increment) {
             View v = getChildAt(index);
@@ -76,7 +76,7 @@ class SimpleHorizontalLayoutView extends ViewGroup {
     @Override
     protected void onMeasure(int widthSpec, int heightSpec) {
         final int widthPx = MeasureSpec.getSize(widthSpec);
-        int contentViewWidth = widthPx;
+        int contentViewWidth = widthPx - getPaddingLeft() - getPaddingRight();
         View dynamicView = null;
 
         // Compute and apply space we can offer to content view.
