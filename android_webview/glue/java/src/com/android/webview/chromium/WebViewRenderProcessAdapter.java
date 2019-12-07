e5c4bb30e2ca94585ff026642dccf0a4e649d86a
Changwan Ryu <changwan@google.com>
2019-12-07 00:13:15 +0000

    Reduce ART verification failure check time at WebView creation
    For pre-Q devices, Q-specific references causes ART verification
    failures, and this slows down the creation time.
    
    Move Q-specific references into a smaller class to minimize the time
    taken for ART to re-verify verification failures at the run time.
    
    Expected to reduce creation time by 7-10%.
    
          WebViewChromium
    
    Test: list_class_verification_failures.py no longer shows
    Bug: 1031362
    Change-Id: I2a27079f22ef3f51b2a192182fba218af1ecf1eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954711
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722700}

---
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewRenderProcessAdapter.java
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.android.webview.chromium;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.webkit.WebViewRenderProcess;
+
+import org.chromium.android_webview.AwRenderProcess;
+import org.chromium.base.annotations.VerifiesOnQ;
+
+import java.lang.ref.WeakReference;
+import java.util.WeakHashMap;
+
+@VerifiesOnQ
+@TargetApi(Build.VERSION_CODES.Q)
+class WebViewRenderProcessAdapter extends WebViewRenderProcess {
+    private static WeakHashMap<AwRenderProcess, WebViewRenderProcessAdapter> sInstances =
+            new WeakHashMap<>();
+
+    private WeakReference<AwRenderProcess> mAwRenderProcessWeakRef;
+
+    public static WebViewRenderProcessAdapter getInstanceFor(AwRenderProcess awRenderProcess) {
+        if (awRenderProcess == null) {
+            return null;
+        }
+        WebViewRenderProcessAdapter instance = sInstances.get(awRenderProcess);
+        if (instance == null) {
+            sInstances.put(
+                    awRenderProcess, instance = new WebViewRenderProcessAdapter(awRenderProcess));
+        }
+        return instance;
+    }
+
+    private WebViewRenderProcessAdapter(AwRenderProcess awRenderProcess) {
+        mAwRenderProcessWeakRef = new WeakReference<>(awRenderProcess);
+    }
+
+    @Override
+    @SuppressLint("Override")
+    public boolean terminate() {
+        AwRenderProcess renderer = mAwRenderProcessWeakRef.get();
+        if (renderer == null) {
+            return false;
+        }
+        return renderer.terminate();
+    }
+}
