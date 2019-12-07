c04b9db19d7cc961c1c298c033026beed6a4da88
Jiawei Li <lijiawei@chromium.org>
2019-12-07 01:26:01 +0000

    [chromecast] Host QueryableDataHost in CastWebContents
    CastWebContents now provides a getter to access QueryableDataHost. If
    InitParams::provide_queryable_data_host is set false, the getter will
    return a nullptr (for non Cast application page).
    
    Merge-With: eureka-internal/342948
    
    Bug: internal b/144316072
    Test: CQ
    Change-Id: I44a2aefbed9fcdcf5481423d073ffc42a8ee358d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1956252
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Jiawei Li <lijiawei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722720}

--- chromecast/browser/cast_web_contents_impl.cc
+++ chromecast/browser/cast_web_contents_impl.cc
@@ -14,10 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/values.h"
+#include "chromecast/activity/queryable_data_host.h"
+#include "chromecast/base/cast_features.h"
 #include "chromecast/base/chromecast_switches.h"
 #include "chromecast/base/metrics/cast_metrics_helper.h"
 #include "chromecast/browser/cast_browser_process.h"
 #include "chromecast/browser/devtools/remote_debugging_server.h"
+#include "chromecast/browser/queryable_data_host_cast.h"
 #include "chromecast/common/mojom/media_playback_options.mojom.h"
 #include "chromecast/common/mojom/on_load_script_injector.mojom.h"
 #include "chromecast/common/mojom/queryable_data_store.mojom.h"
@@ -140,6 +143,13 @@ CastWebContentsImpl::CastWebContentsImpl(content::WebContents* web_contents,
   if (GetSwitchValueBoolean(switches::kDisableMojoRenderer, false)) {
     use_cma_renderer_ = false;
   }
+
+  // Provides QueryableDataHostCast if the new QueryableData bindings is not
+  // enabled.
+  if (init_params.enable_queryable_data_host) {
+    queryable_data_host_ =
+        std::make_unique<QueryableDataHostCast>(web_contents_);
+  }
 }
 
 CastWebContentsImpl::~CastWebContentsImpl() {
@@ -166,6 +176,10 @@ content::WebContents* CastWebContentsImpl::web_contents() const {
 CastWebContents::PageState CastWebContentsImpl::page_state() const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return page_state_;
+}
+
+QueryableDataHost* CastWebContentsImpl::queryable_data_host() const {
+  return queryable_data_host_.get();
 }
 
 void CastWebContentsImpl::AddRendererFeatures(
--- chromecast/browser/cast_web_contents_impl.h
+++ chromecast/browser/cast_web_contents_impl.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromecast {
 
+class QueryableDataHost;
+
 namespace shell {
 class RemoteDebuggingServer;
 }  // namespace shell
@@ -43,6 +45,8 @@ class CastWebContentsImpl : public CastWebContents,
 
   content::WebContents* web_contents() const override;
   PageState page_state() const override;
+
+  QueryableDataHost* queryable_data_host() const override;
 
   // CastWebContents implementation:
   int tab_id() const override;
@@ -177,6 +181,8 @@ class CastWebContentsImpl : public CastWebContents,
   // Map of InterfaceSet -> InterfaceProvider pointer.
   base::flat_map<InterfaceSet, service_manager::InterfaceProvider*>
       interface_providers_map_;
+
+  std::unique_ptr<QueryableDataHost> queryable_data_host_;
 
   SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<CastWebContentsImpl> weak_factory_;
