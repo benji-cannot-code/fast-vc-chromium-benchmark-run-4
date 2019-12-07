f8adc4fd0f4e3da40dedc41901cdd605e5ccb6f5
Michael Spang <spang@chromium.org>
2019-12-07 00:07:42 +0000

    chromecast: Migrate CHROMECAST_BUILD to BUILDFLAG(IS_CHROMECAST)
    This #define is only present in some downstream builds and should
    probably not be used upstream. Use the upstream BUIDLFLAG(IS_CHROMECAST)
    flag instead, so that upstream tests running with is_chromecast=true
    take the right code path.
    
    Bug: 1030969
    
    Change-Id: Iffb1b41e616904450f4946e23899613b811fd2e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1951982
    Reviewed-by: Luke Halliwell (slow) <halliwell@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722697}

--- ui/gfx/linux/client_native_pixmap_dmabuf.cc
+++ ui/gfx/linux/client_native_pixmap_dmabuf.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
+#include "build/chromecast_buildflags.h"
 #include "ui/gfx/buffer_format_util.h"
 #include "ui/gfx/switches.h"
 
@@ -92,7 +93,7 @@ ClientNativePixmapDmaBuf::PlaneInfo::~PlaneInfo() {
 bool ClientNativePixmapDmaBuf::IsConfigurationSupported(
     gfx::BufferFormat format,
     gfx::BufferUsage usage) {
-#if defined(CHROMECAST_BUILD)
+#if BUILDFLAG(IS_CHROMECAST)
   switch (usage) {
     case gfx::BufferUsage::GPU_READ_CPU_READ_WRITE:
       // TODO(spang): Fix b/121148905 and turn these back on.
