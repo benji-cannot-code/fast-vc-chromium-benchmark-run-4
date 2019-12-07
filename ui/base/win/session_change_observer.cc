461f3ab1dfa349aae4fd72b0b46946a71ab87246
David Bienvenu <davidbienvenu@chromium.org>
2019-12-07 19:20:54 +0000

    Ignore (un)lock events for sessions that aren't the current session.
    Citrix and MS Remote Desktop send session changed notifications to all
    users on a machine, apparently, even though we only register for the
    session associated with the Chrome singleton hwnd via NOTIFY_FOR_THIS_SESSION.
    
    https://docs.microsoft.com/en-us/windows/win32/api/wtsapi32/nf-wtsapi32-wtsregistersessionnotificationex
    
    So, ignore these notifications for sessions that aren't the current session.
    
    Bug: 813093
    Change-Id: I807b4c147b4212b145985bc81ea4d2aa32471231
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1929489
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722770}

--- ui/base/win/session_change_observer.cc
+++ ui/base/win/session_change_observer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtsapi32.h>
 
 #include <memory>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
@@ -60,8 +61,21 @@ class SessionChangeObserver::WtsRegistrationNotificationManager {
   void OnWndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
     switch (message) {
       case WM_WTSSESSION_CHANGE:
-        for (SessionChangeObserver& observer : observer_list_)
-          observer.OnSessionChange(wparam);
+        if (wparam == WTS_SESSION_LOCK || wparam == WTS_SESSION_UNLOCK) {
+          bool is_current_session;
+          const bool* is_current_session_ptr = &is_current_session;
+          DWORD current_session_id = 0;
+          if (!::ProcessIdToSessionId(::GetCurrentProcessId(),
+                                      &current_session_id)) {
+            PLOG(ERROR) << "ProcessIdToSessionId failed";
+            is_current_session_ptr = nullptr;
+          } else {
+            is_current_session =
+                (static_cast<DWORD>(lparam) == current_session_id);
+          }
+          for (SessionChangeObserver& observer : observer_list_)
+            observer.OnSessionChange(wparam, is_current_session_ptr);
+        }
         break;
       case WM_DESTROY:
         RemoveSingletonHwndObserver();
@@ -101,8 +115,9 @@ SessionChangeObserver::~SessionChangeObserver() {
   ClearCallback();
 }
 
-void SessionChangeObserver::OnSessionChange(WPARAM wparam) {
-  callback_.Run(wparam);
+void SessionChangeObserver::OnSessionChange(WPARAM wparam,
+                                            const bool* is_current_session) {
+  callback_.Run(wparam, is_current_session);
 }
 
 void SessionChangeObserver::ClearCallback() {
