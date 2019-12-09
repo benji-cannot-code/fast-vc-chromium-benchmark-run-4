74d8288ab988164e252059d197ccba4b19ba0d7e
Aaron Tagliaboschi <aarontag@chromium.org>
2019-12-09 16:56:45 +0000

    Revert "[client-hints] Populate 'sec-ch-ua-model' and 'sec-ch-ua-arch'"
    This reverts commit b7fd7c5a67732cda3588342fc86d9c2756d2d228.
    
    Reason for revert: Mac user agent strings were not properly accounted for
    
    Original change's description:
    > [client-hints] Populate 'sec-ch-ua-model' and 'sec-ch-ua-arch'
    > 
    > A smidge of refactoring for the user agent string code as well.
    > 
    > Bug: 1001125
    > Change-Id: I2ae97922d546b64a93bf55ef81ec40e30c765032
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1951446
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#722970}
    
    TBR=avi@chromium.org,torne@chromium.org,yoavweiss@chromium.org,aarontag@chromium.org
    
    Change-Id: Iad1db0a43a7edebbc558edf31c401998bf4c8b73
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1001125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1955037
    Reviewed-by: Aaron Tagliaboschi <aarontag@chromium.org>
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722971}

--- weblayer/browser/content_browser_client_impl.cc
+++ weblayer/browser/content_browser_client_impl.cc
@@ -174,8 +174,9 @@ blink::UserAgentMetadata ContentBrowserClientImpl::GetUserAgentMetadata() {
   metadata.full_version = version_info::GetVersionNumber();
   metadata.major_version = version_info::GetMajorVersionNumber();
   metadata.platform = version_info::GetOSType();
-  metadata.architecture = content::BuildCpuInfo();
-  metadata.model = content::BuildModelInfo();
+
+  metadata.architecture = "";
+  metadata.model = "";
 
   return metadata;
 }
