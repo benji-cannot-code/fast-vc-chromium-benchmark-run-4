e869e9940c3c46f10a6768fb74233faa03d8e437
Jason Lin <lxj@google.com>
2019-12-09 14:03:04 +0000

    Whitelist chrome UI scheme for CLIPBOARD_READ
    This will allow the terminal SWA (and other chrome:// pages) to programmatically
    read clipboard in the JS code.
    
    Bug: 1019034
    Change-Id: I0d838c67593c8499af9fc69c8052b7594bc464fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954991
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722922}

--- components/content_settings/core/browser/content_settings_registry.cc
+++ components/content_settings/core/browser/content_settings_registry.cc
@@ -35,6 +35,11 @@ std::vector<std::string> WhitelistedSchemes() {
   return std::vector<std::string>();
 }
 
+std::vector<std::string> WhitelistedSchemes(const char* scheme1) {
+  const char* schemes[] = {scheme1};
+  return std::vector<std::string>(schemes, schemes + base::size(schemes));
+}
+
 std::vector<std::string> WhitelistedSchemes(const char* scheme1,
                                             const char* scheme2) {
   const char* schemes[] = {scheme1, scheme2};
@@ -425,7 +430,7 @@ void ContentSettingsRegistry::Init() {
 
   Register(ContentSettingsType::CLIPBOARD_READ_WRITE, "clipboard",
            CONTENT_SETTING_ASK, WebsiteSettingsInfo::UNSYNCABLE,
-           WhitelistedSchemes(),
+           WhitelistedSchemes(kChromeUIScheme),
            ValidSettings(CONTENT_SETTING_ALLOW, CONTENT_SETTING_BLOCK,
                          CONTENT_SETTING_ASK),
            WebsiteSettingsInfo::SINGLE_ORIGIN_ONLY_SCOPE,
