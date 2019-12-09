dfa6d02a492e78b2b62b72fdb46a7e8b4820ee55
Gauthier Ambard <gambard@chromium.org>
2019-12-09 13:24:32 +0000

    [iOS] Have custom user agent for WebUI pages
    When loading WebUI pages, the UserAgent type returned is NONE. Because
    of that, the custom user agent isn't changed, using the last user agent
    used.
    If the WebUI page is the first page being loaded, this is the default
    WebKit user agent. Otherwise it is the user agent of the previous page
    (which can be desktop).
    
    This CL fixes it by forcing the user agent to be the Mobile one on
    those pages.
    
    Bug: 998999, 1027097
    Change-Id: I97af2863cfa2f87bb51ded4674063db6261380a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950503
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722912}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -313,6 +313,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // otherwise returns object representing execution result.
 + (id)executeJavaScript:(NSString*)javaScript error:(NSError**)error;
 
+// Returns the user agent that should be used for the mobile version.
++ (NSString*)mobileUserAgentString;
+
 #pragma mark - Accessibility Utilities (EG2)
 
 // Verifies that all interactive elements on screen (or at least one of their
