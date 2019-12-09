dd449b40a9a9d502c4177214bd3d6a3a294ad20d
Sammie Quon <sammiequon@google.com>
2019-12-09 21:50:19 +0000

    overview: Fix bounds for wide/tall windows in overview.
    Ran into this issue when trying to increase the margin for the below
    bug and OverviewSessionTest.ShadowBounds failed. The computations were
    off by a bit but happened to worked with the given margin. So, if we
    increase kWindowMargin [1] to 8, tests would fail, but that shouldn't be
    the case, as the algorithm should be able to handle that.
    
    Had to tweak the tests so that they dont run into the case the wide item's
    preferred height is taller than the area given by the computations, and
    then clamped. It's a rare case, and will just be squashed a bit, but
    messes up the test a bit that tests the regular cases.
    
    Also removed some unused codes.
    
    [1] https://cs.chromium.org/chromium/src/ash/wm/overview/overview_constants.h?rcl=42a7d2fa9327a1af93d97b309f4f0b1e53c11856&l=20
    
    Test: manual, existing tests
    Bug: 989794
    Change-Id: I0d56b03d9dac2a8ec1732bb8479ab90a1d5974f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949031
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723102}

--- ash/wm/overview/overview_item.cc
+++ ash/wm/overview/overview_item.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/compositor_extra/shadow.h"
 #include "ui/gfx/geometry/safe_integer_conversions.h"
+#include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/transform_util.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/layout/layout_provider.h"
@@ -728,7 +729,8 @@ void OverviewItem::DestroyPhantomsForDragging() {
   phantoms_for_dragging_.reset();
 }
 
-void OverviewItem::SetShadowBounds(base::Optional<gfx::Rect> bounds_in_screen) {
+void OverviewItem::SetShadowBounds(
+    base::Optional<gfx::RectF> bounds_in_screen) {
   // Shadow is normally turned off during animations and reapplied when they
   // are finished. On destruction, |shadow_| is cleaned up before
   // |transform_window_|, which may call this function, so early exit if
@@ -746,7 +748,8 @@ void OverviewItem::SetShadowBounds(base::Optional<gfx::Rect> bounds_in_screen) {
       gfx::Rect(item_widget_->GetNativeWindow()->GetTargetBounds().size());
   bounds_in_item.Inset(kOverviewMargin, kOverviewMargin);
   bounds_in_item.Inset(0, kHeaderHeightDp, 0, 0);
-  bounds_in_item.ClampToCenteredSize(bounds_in_screen.value().size());
+  bounds_in_item.ClampToCenteredSize(
+      gfx::ToRoundedSize(bounds_in_screen->size()));
 
   shadow_->SetContentBounds(bounds_in_item);
 }
@@ -777,10 +780,10 @@ void OverviewItem::UpdateRoundedCornersAndShadow() {
            ->GetAnimator()
            ->is_animating();
 
-  SetShadowBounds(should_show_shadow
-                      ? base::make_optional(gfx::ToEnclosedRect(
-                            transform_window_.GetTransformedBounds()))
-                      : base::nullopt);
+  SetShadowBounds(
+      should_show_shadow
+          ? base::make_optional(transform_window_.GetTransformedBounds())
+          : base::nullopt);
   if (transform_window_.IsMinimized()) {
     overview_item_view_->UpdatePreviewRoundedCorners(
         should_show_rounded_corners,
