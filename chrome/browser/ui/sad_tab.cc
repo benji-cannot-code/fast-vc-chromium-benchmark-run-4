e6743fc4fded4425289aa21fca1a8c8da652cfb7
Eric Lawrence <ericlaw@microsoft.com>
2019-12-09 23:11:37 +0000

    Show crash exit code on sad tab page
    To aid in troubleshooting of crashed tabs, display the renderer process'
    exit code on the sad tab page.
    
    Bug: 1020652
    Change-Id: I9850a6f186a0b4b02dc285c5b9c66b54c8246416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947360
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Auto-Submit: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Commit-Queue: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#723148}

--- chrome/browser/ui/sad_tab.cc
+++ chrome/browser/ui/sad_tab.cc
@@ -133,6 +133,10 @@ int SadTab::GetTitle() {
   return 0;
 }
 
+int SadTab::GetErrorCodeFormatString() {
+  return IDS_SAD_TAB_ERROR_CODE;
+}
+
 int SadTab::GetInfoMessage() {
   switch (kind_) {
 #if defined(OS_CHROMEOS)
@@ -197,6 +201,10 @@ std::vector<int> SadTab::GetSubMessages() {
   }
   NOTREACHED();
   return std::vector<int>();
+}
+
+int SadTab::GetCrashedErrorCode() {
+  return web_contents_->GetCrashedErrorCode();
 }
 
 void SadTab::RecordFirstPaint() {
--- chrome/browser/ui/sad_tab.h
+++ chrome/browser/ui/sad_tab.h
@@ -42,6 +42,7 @@ class SadTab {
   // suggests reloading and subsequent ones suggest sending feedback.)
   int GetTitle();
   int GetInfoMessage();
+  int GetErrorCodeFormatString();
   int GetButtonTitle();
   int GetHelpLinkTitle();
 
@@ -52,6 +53,9 @@ class SadTab {
   // Returns the target of the "Learn more" link. Use it for the context menu
   // and to show the URL on hover, but call PerformAction() for regular clicks.
   const char* GetHelpLinkURL();
+
+  // Returns the error code describing the reason for the crash.
+  int GetCrashedErrorCode();
 
   // Virtual for testing.
   virtual void RecordFirstPaint();
