13633eb19365c327ebb953b1416de47a3090ed39
Leon Han <leon.han@intel.com>
2019-12-09 14:24:36 +0000

    [webnfc] Remove NDEFErrorEvent
    It's recommended to use the generic ErrorEvent rather than creating
    unnecessary custom error event types.
    
    The spec change:
    https://github.com/w3c/web-nfc/pull/398
    
    BUG=520391
    
    Change-Id: Id8e02ed16d87d67fe36b6df4340a930893ca3b20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940814
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Cr-Commit-Position: refs/heads/master@{#722930}

--- third_party/blink/renderer/core/events/error_event.h
+++ third_party/blink/renderer/core/events/error_event.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/bindings/core/v8/source_location.h"
 #include "third_party/blink/renderer/bindings/core/v8/world_safe_v8_reference.h"
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/events/error_event_init.h"
 #include "third_party/blink/renderer/platform/bindings/dom_wrapper_world.h"
@@ -44,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ErrorEvent final : public Event {
+class CORE_EXPORT ErrorEvent final : public Event {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
