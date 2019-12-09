5ae3c2ae78274a61cea399f3463121b10736c8e4
Becca Hughes <beccahughes@chromium.org>
2019-12-09 20:37:06 +0000

    Revert "Use proper localization mechanism for throbber-notice element."
    This reverts commit c50865f6a55b864b7190c8956160b4264141f5b0.
    
    Reason for revert: Suspected failure for Chrome OS browser_tests:
    
    BUG=1029957
    
    Original change's description:
    > Use proper localization mechanism for throbber-notice element.
    > 
    > Bug: 955194
    > Change-Id: Ib6ac3fcbe446c047bd846a0c775210fda7763b9d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953626
    > Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    > Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#723023}
    
    TBR=antrim@chromium.org,rsorokin@chromium.org,raleksandrov@google.com
    
    Change-Id: I65c1717fe30d0aee33801d31971ee516099f7de5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 955194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1958709
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723063}

--- chrome/browser/resources/chromeos/login/throbber_notice.js
+++ chrome/browser/resources/chromeos/login/throbber_notice.js
@@ -6,7 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'throbber-notice',
 
-  behaviors: [OobeI18nBehavior],
-
-  properties: {textKey: String}
+  properties: {text: String}
 });
