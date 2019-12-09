e99069c6a9c7224a83bab7634efed1e2c035e3a9
Dan Harrington <harringtond@chromium.org>
2019-12-09 19:44:38 +0000

    Move feed tests to feed directory
    This required adding some deps that were in chrome/android/java
    
    Change-Id: I12f33d177fc2af89373052e5529e665f47f82c17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953755
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723046}

--- chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/FeedJournalStorageConformanceTest.java
+++ chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/FeedJournalStorageConformanceTest.java
@@ -49,7 +49,7 @@ public final class FeedJournalStorageConformanceTest extends JournalStorageConfo
         }
 
         @Override
-        public void read(String journalName, Consumer < Result < List<byte[]>>> consumer) {
+        public void read(String journalName, Consumer<Result<List<byte[]>>> consumer) {
             ConsumerSyncWrapper.waitForConsumer(
                     consumer, (wrapper) -> { super.read(journalName, wrapper); }, TIMEOUT);
         }
@@ -67,7 +67,7 @@ public final class FeedJournalStorageConformanceTest extends JournalStorageConfo
         }
 
         @Override
-        public void getAllJournals(Consumer < Result < List<String>>> consumer) {
+        public void getAllJournals(Consumer<Result<List<String>>> consumer) {
             ConsumerSyncWrapper.waitForConsumer(
                     consumer, (wrapper) -> { super.getAllJournals(wrapper); }, TIMEOUT);
         }
--- chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/FeedNewTabPageTest.java
+++ chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/FeedNewTabPageTest.java
@@ -73,9 +73,10 @@ import java.util.List;
  * {@link org.chromium.chrome.browser.ntp.NewTabPageTest}.
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
-@CommandLineFlags.Add(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
-@Features.EnableFeatures(ChromeFeatureList.INTEREST_FEED_CONTENT_SUGGESTIONS)
-public class FeedNewTabPageTest {
+@CommandLineFlags
+        .Add(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
+        @Features.EnableFeatures(ChromeFeatureList.INTEREST_FEED_CONTENT_SUGGESTIONS)
+        public class FeedNewTabPageTest {
     private static final int ARTICLE_SECTION_HEADER_POSITION = 1;
     private static final int SIGNIN_PROMO_POSITION = 2;
 
@@ -293,8 +294,9 @@ public class FeedNewTabPageTest {
     @Feature({"FeedNewTabPage"})
     public void testFeedDisabledByPolicy() throws Exception {
         final boolean pref = TestThreadUtils.runOnUiThreadBlocking(
-                () -> PrefServiceBridge.getInstance().getBoolean(
-                        Pref.NTP_ARTICLES_SECTION_ENABLED));
+                ()
+                        -> PrefServiceBridge.getInstance().getBoolean(
+                                Pref.NTP_ARTICLES_SECTION_ENABLED));
 
         // Policy is disabled. Verify the NTP root view contains only the Stream view as child.
         ViewGroup rootView = (ViewGroup) mNtp.getView();
@@ -307,8 +309,10 @@ public class FeedNewTabPageTest {
 
         // Simulate that policy is enabled. Verify the NTP root view contains only the view for
         // policy as child.
-        TestThreadUtils.runOnUiThreadBlocking(() -> PrefServiceBridge.getInstance().setBoolean(
-                Pref.NTP_ARTICLES_SECTION_ENABLED, false));
+        TestThreadUtils.runOnUiThreadBlocking(
+                ()
+                        -> PrefServiceBridge.getInstance().setBoolean(
+                                Pref.NTP_ARTICLES_SECTION_ENABLED, false));
         Assert.assertNotNull(mNtp.getCoordinatorForTesting().getScrollViewForPolicy());
         Assert.assertNull(mNtp.getCoordinatorForTesting().getStream());
         Assert.assertEquals(1, rootView.getChildCount());
@@ -329,8 +333,10 @@ public class FeedNewTabPageTest {
 
         // Simulate that policy is disabled. Verify the NTP root view is the view for policy. We
         // don't re-enable the Feed until the next restart.
-        TestThreadUtils.runOnUiThreadBlocking(() -> PrefServiceBridge.getInstance().setBoolean(
-                Pref.NTP_ARTICLES_SECTION_ENABLED, true));
+        TestThreadUtils.runOnUiThreadBlocking(
+                ()
+                        -> PrefServiceBridge.getInstance().setBoolean(
+                                Pref.NTP_ARTICLES_SECTION_ENABLED, true));
         Assert.assertNotNull(ntp2.getCoordinatorForTesting().getScrollViewForPolicy());
         Assert.assertNull(ntp2.getCoordinatorForTesting().getStream());
         Assert.assertEquals(1, rootView2.getChildCount());
@@ -346,8 +352,10 @@ public class FeedNewTabPageTest {
                 mNtp.getCoordinatorForTesting().getScrollViewForPolicy(), rootView.getChildAt(0));
 
         // Reset state.
-        TestThreadUtils.runOnUiThreadBlocking(() -> PrefServiceBridge.getInstance().setBoolean(
-                Pref.NTP_ARTICLES_SECTION_ENABLED, pref));
+        TestThreadUtils.runOnUiThreadBlocking(
+                ()
+                        -> PrefServiceBridge.getInstance().setBoolean(
+                                Pref.NTP_ARTICLES_SECTION_ENABLED, pref));
     }
 
     /**
