3fd9735a53b63ea34e7a42c8f49480c5ef80408d
Henrique Nakashima <hnakashima@chromium.org>
2019-12-09 20:48:31 +0000

    Create CrashUploadCountStore and move crash-specific logic from prefs.
    This is a step to move feature-specific logic out of
    ChromePreferencesManager and delete it.
    
    Bug: 1022102
    Change-Id: I8b0215e7cd15d5da6e4a13654bff3259159d11f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953928
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723069}

--- chrome/android/java/src/org/chromium/chrome/browser/crash/MinidumpUploadService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/crash/MinidumpUploadService.java
@@ -21,7 +21,6 @@ import org.chromium.base.Log;
 import org.chromium.base.StreamUtil;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.metrics.RecordHistogram;
-import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.settings.privacy.PrivacyPreferencesManager;
 import org.chromium.components.background_task_scheduler.TaskIds;
 import org.chromium.components.minidump_uploader.CrashFileManager;
@@ -121,7 +120,7 @@ public class MinidumpUploadService extends IntentService {
     /**
      * Stores the successes and failures from uploading crash to UMA,
      */
-    public static void storeBreakpadUploadStatsInUma(ChromePreferenceManager pref) {
+    public static void storeBreakpadUploadStatsInUma(CrashUploadCountStore pref) {
         sBrowserCrashMetricsInitialized.set(true);
         for (String type : TYPES) {
             for (int success = pref.getCrashSuccessUploadCount(type); success > 0; success--) {
@@ -135,8 +134,7 @@ public class MinidumpUploadService extends IntentService {
                 if (ProcessType.BROWSER.equals(type)) sDidBrowserCrashRecently.set(true);
             }
 
-            pref.setCrashSuccessUploadCount(type, 0);
-            pref.setCrashFailureUploadCount(type, 0);
+            pref.resetCrashUploadCounts(type);
         }
     }
 
@@ -287,7 +285,7 @@ public class MinidumpUploadService extends IntentService {
         if (ProcessType.BROWSER.equals(process_type)) {
             sDidBrowserCrashRecently.set(true);
         }
-        ChromePreferenceManager.getInstance().incrementCrashSuccessUploadCount(process_type);
+        CrashUploadCountStore.getInstance().incrementCrashSuccessUploadCount(process_type);
     }
 
     /**
@@ -303,7 +301,7 @@ public class MinidumpUploadService extends IntentService {
         if (ProcessType.BROWSER.equals(process_type)) {
             sDidBrowserCrashRecently.set(true);
         }
-        ChromePreferenceManager.getInstance().incrementCrashFailureUploadCount(process_type);
+        CrashUploadCountStore.getInstance().incrementCrashFailureUploadCount(process_type);
     }
 
     /**
