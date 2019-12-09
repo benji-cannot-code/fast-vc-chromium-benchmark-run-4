05ad6e16de01d6181c4008a97d1d86471f65ec46
Ilia Samsonov <isamsonov@google.com>
2019-12-09 09:39:27 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=kolos@chromium.org
    
    Bug: 1023677
    Change-Id: I06fa6ab71cbf2fb1cf5360512fecc84d05508c97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957531
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722862}

--- components/password_manager/core/browser/password_form_manager_unittest.cc
+++ components/password_manager/core/browser/password_form_manager_unittest.cc
@@ -2281,7 +2281,7 @@ TEST_P(PasswordFormManagerTest, PossibleUsernameFieldManager) {
   }
 }
 
-INSTANTIATE_TEST_SUITE_P(,
+INSTANTIATE_TEST_SUITE_P(All,
                          PasswordFormManagerTest,
                          testing::Values(false, true));
 
