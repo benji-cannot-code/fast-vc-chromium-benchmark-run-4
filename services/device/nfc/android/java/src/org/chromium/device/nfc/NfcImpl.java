741a6ef9d00d7f4517d3c36e3ff3c68aee92785b
François Beaufort <beaufort.francois@gmail.com>
2019-12-09 10:17:47 +0000

    [WebNFC] Jelly Bean cleanup in NfcImpl
    This CL removes some unnecessary checks now that Chrome no longer
    supports Jelly Bean.
    
    Change-Id: Ie814db4b17ab9ef9a980dceb9211872d33a9605c
    Bug: 520391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954506
    Reviewed-by: Leon Han <leon.han@intel.com>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#722869}

--- services/device/nfc/android/java/src/org/chromium/device/nfc/NfcImpl.java
+++ services/device/nfc/android/java/src/org/chromium/device/nfc/NfcImpl.java
@@ -131,7 +131,7 @@ public class NfcImpl implements Nfc {
 
         mDelegate.trackActivityForHost(mHostId, onActivityUpdatedCallback);
 
-        if (!mHasPermission || Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+        if (!mHasPermission) {
             Log.w(TAG, "NFC operations are not permitted.");
             mNfcAdapter = null;
             mNfcManager = null;
@@ -423,8 +423,6 @@ public class NfcImpl implements Nfc {
      * @see android.nfc.NfcAdapter#enableReaderMode
      */
     private void enableReaderModeIfNeeded() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return;
-
         if (mReaderCallbackHandler != null || mActivity == null || mNfcAdapter == null) return;
 
         // Do not enable reader mode, if there are no active push / watch operations.
@@ -444,8 +442,6 @@ public class NfcImpl implements Nfc {
      */
     @TargetApi(Build.VERSION_CODES.KITKAT)
     private void disableReaderMode() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) return;
-
         // There is no API that could query whether reader mode is enabled for adapter.
         // If mReaderCallbackHandler is null, reader mode is not enabled.
         if (mReaderCallbackHandler == null) return;
