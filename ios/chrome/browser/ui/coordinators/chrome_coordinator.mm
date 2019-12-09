a43d7a9447a0c066347979b35fe4f5c8270bf9ab
Mark Cogan <marq@google.com>
2019-12-09 12:10:59 +0000

    [iOS] remove coordinator stop-on-dealloc behavior.
    This CL removes the default behavior for ChromeCoordinators to stop
    when they dealloc; given that object lifetimes are opaque, this causes
    more pain than it prevents.
    
    In cases where unit/eg tests find code that depended on this general
    behavior, it's been added to those specific classes.
    
    Change-Id: I0696da343c0a5352d5de3303d0f0e160813a8eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953764
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722891}

--- ios/chrome/browser/ui/coordinators/chrome_coordinator.mm
+++ ios/chrome/browser/ui/coordinators/chrome_coordinator.mm
@@ -42,10 +42,6 @@ initWithBaseViewController:(UIViewController*)viewController
   return self;
 }
 
-- (void)dealloc {
-  [self stop];
-}
-
 #pragma mark - Accessors
 
 - (ChromeCoordinator*)activeChildCoordinator {
