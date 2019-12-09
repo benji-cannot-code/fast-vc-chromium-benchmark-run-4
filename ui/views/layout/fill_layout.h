31d251176dafbf704c5f643f2f998944b3fa2d5a
Dana Fried <dfried@chromium.org>
2019-12-09 21:33:55 +0000

    Derive FillLayout from LayoutManagerBase.
    This allows us to, among other things, allow FillLayout to ignore child
    views via SetChildViewIgnoredByLayout(), which would in turn allow us
    to, for example, include a hidden or floating view in a FillLayout.
    
    It is also a step towards unifying LayoutManager and LayoutManagerBase.
    
    Bug: 1026461
    Change-Id: I692304ebbb6b33e6a076e95dfcb5a134987640a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1927976
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723095}

--- ui/views/layout/fill_layout.h
+++ ui/views/layout/fill_layout.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "ui/views/layout/layout_manager.h"
+#include "ui/views/layout/layout_manager_base.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -21,17 +21,41 @@ namespace views {
 //  is calculated as the maximum values across all child views of the host.
 //
 ///////////////////////////////////////////////////////////////////////////////
-class VIEWS_EXPORT FillLayout : public LayoutManager {
+class VIEWS_EXPORT FillLayout : public LayoutManagerBase {
  public:
   FillLayout();
   ~FillLayout() override;
 
-  // Overridden from LayoutManager:
-  void Layout(View* host) override;
+  bool include_hidden_views() const { return include_hidden_views_; }
+  FillLayout& SetIncludeHiddenViews(bool include_hidden_views);
+
+  bool minimum_size_enabled() const { return minimum_size_enabled_; }
+  FillLayout& SetMinimumSizeEnabled(bool minimum_size_enabled);
+
+  // LayoutManagerBase:
+  ProposedLayout CalculateProposedLayout(
+      const SizeBounds& size_bounds) const override;
   gfx::Size GetPreferredSize(const View* host) const override;
+  gfx::Size GetMinimumSize(const View* host) const override;
   int GetPreferredHeightForWidth(const View* host, int width) const override;
 
  private:
+  // Uses |include_hidden_views| to determine whether a child should be
+  // included; calls either IsChildIncludedInLayout() or
+  // IsChildViewIgnoredByLayout() as appropriate.
+  bool ShouldIncludeChild(const View* view) const;
+
+  // Whether to include hidden views in size calculations. Default is to
+  // include for backwards compatibility.
+  bool include_hidden_views_ = true;
+
+  // Whether to compute minimum size separately, as the maximum of all of the
+  // included child views' minimum size (true), or to simply return the
+  // preferred size (false).
+  //
+  // Off by default for backwards-compatibility with legacy uses of FillLayout.
+  bool minimum_size_enabled_ = false;
+
   DISALLOW_COPY_AND_ASSIGN(FillLayout);
 };
 
