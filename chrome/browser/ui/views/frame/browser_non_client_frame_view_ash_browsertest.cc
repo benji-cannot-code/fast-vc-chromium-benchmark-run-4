3a767a63fbc4f9c7509ee3b839410fecf03d7b9e
David Roger <droger@chromium.org>
2019-12-09 10:39:43 +0000

    Split business logic out of avatar_toolbar_button
    Bug: 1012704
    Change-Id: Ideedb2bae2ff7216478dee97c75da9c501d14727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1868879
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722875}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_command_controller.h"
 #include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller_test.h"
