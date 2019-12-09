ac7affabaaa8203070254bf6c824be96c5c48482
Becca Hughes <beccahughes@chromium.org>
2019-12-09 21:47:30 +0000

    Revert "WebUI NTP: most-visited/shortcuts, initial support"
    This reverts commit dace0deac4eae4ca7a9e3ad10140bce401855c95.
    
    Reason for revert: Suspected for breaking browser_tests
    
    BUG=1032245
    
    Original change's description:
    > WebUI NTP: most-visited/shortcuts, initial support
    > 
    > This implementation of most-visited is functionally the same of the
    > most-visited on the current NTP. The differences in UI are going to be
    > discussed with @bklmn after this CL lands.
    > 
    >  - Show up to 10 most-visited tiles.
    >  - Toast is shown with undo and restore to defaults buttons when
    >    certain tile actions are performed.
    >  - Action menu displays options to edit and remove tile.
    >  - Show add shortcut tile if space allows.
    >  - Supports light and dark mode.
    > 
    > Bug: 997229
    > Change-Id: Ie22220e2f866ba1ba485daf504265e9d868f9050
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924702
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Dan Beam <dbeam@chromium.org>
    > Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#723050}
    
    TBR=dbeam@chromium.org,aee@chromium.org,ajgo@chromium.org
    
    Change-Id: Icd425143dc76c667c372a9d723e7fe34223cde2e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 997229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1958615
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723101}

--- chrome/browser/resources/new_tab_page/browser_proxy.js
+++ chrome/browser/resources/new_tab_page/browser_proxy.js
@@ -4,10 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
-import 'chrome://resources/mojo/url/mojom/url.mojom-lite.js';
-
 import './new_tab_page.mojom-lite.js';
-
 import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
 
 export class BrowserProxy {
--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h
@@ -18,7 +18,6 @@ class WebUI;
 }
 class GURL;
 class NewTabPageHandler;
-class Profile;
 
 class NewTabPageUI : public ui::MojoWebUIController,
                      public new_tab_page::mojom::PageHandlerFactory {
@@ -43,8 +42,6 @@ class NewTabPageUI : public ui::MojoWebUIController,
 
   mojo::Receiver<new_tab_page::mojom::PageHandlerFactory>
       page_factory_receiver_;
-
-  Profile* profile_;
 
   DISALLOW_COPY_AND_ASSIGN(NewTabPageUI);
 };
