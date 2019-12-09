3e5540dc1854f0bf77f1c8309d47e71563c28893
Tim van der Lippe <tvanderlippe@chromium.org>
2019-12-09 19:22:47 +0000

    Add missing const for strict mode
    Necessary for https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1953583
    
    Follow-up on https://chromium-review.googlesource.com/c/chromium/src/+/1958116
    
    TBR=mathias@chromium.org
    
    Bug: 1006759
    Change-Id: Id6ae5e47b836443ccb7bdf83bc789cca786484ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953742
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723032}

--- third_party/blink/web_tests/http/tests/devtools/elements/edit/edit-after-model-changed.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/edit/edit-after-model-changed.js
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await ElementsTestRunner.selectNodeAndWaitForStylesPromise('inspected');
 
   var section = ElementsTestRunner.inlineStyleSection();
-  treeElement = section.addNewBlankProperty(0);
+  const treeElement = section.addNewBlankProperty(0);
 
   // Flush the pane's throttler and then stall it.
   const originalDoUpdate = () => treeElement._parentPane.doUpdate();
