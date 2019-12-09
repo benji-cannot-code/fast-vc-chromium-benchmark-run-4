d33e4106cfb908d2af9ccbcaad0ebb4442d11e0f
Samuel Huang <huangs@chromium.org>
2019-12-09 16:42:27 +0000

    Reland "[DevUI DFM] Move 17 chrome:// pages to DFM, via new dev_ui_browser_resources.pak."
    This reverts commit c61c6764617a3862a0fc70cfd084e088df232463.
    
    Reason for reland: Fixed bug: Some deps were not moved from old
    target "resources" to new target "dev_ui_browser_resources".
    
    Original change's description:
    > Revert "[DevUI DFM] Move 17 chrome:// pages to DFM, via new dev_ui_browser_resources.pak."
    > 
    > This reverts commit 852ad1d06b4a8f6289749382d3543faf7d60b309.
    > 
    > Reason for revert: Suspected to break compile on mac-archive-rel
    > 
    > TBR=thakis@chromium.org,huangs@chromium.org,agrieve@chromium.org
    > 
    > Change-Id: I93a24570c0be77d5fc1dad2342e83ea5d9bead25
    > Bug: 927131
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1956013
    > Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    > Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#722591}
    > 
    > Original change's description:
    > > [DevUI DFM] Move 17 chrome:// pages to DFM, via new dev_ui_browser_resources.pak.
    > > 
    > > Bug: 927131
    > > Change-Id: Ideaf57e2997b4621d7ffdfea8c126bf78688c322
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954458
    > > Reviewed-by: Nico Weber <thakis@chromium.org>
    > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > Commit-Queue: Samuel Huang <huangs@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#722576}
    
    TBR=thakis@chromium.org,huangs@chromium.org,agrieve@chromium.org,karandeepb@chromium.org
    
    Bug: 927131
    Change-Id: Ia314430669020262960f5f401991cd7f5e9328c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957162
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#722963}

--- chrome/browser/ui/webui/notifications_internals/notifications_internals_ui.cc
+++ chrome/browser/ui/webui/notifications_internals/notifications_internals_ui.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/notifications_internals/notifications_internals_ui_message_handler.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/grit/browser_resources.h"
+#include "chrome/grit/dev_ui_browser_resources.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_controller.h"
 #include "content/public/browser/web_ui_data_source.h"
