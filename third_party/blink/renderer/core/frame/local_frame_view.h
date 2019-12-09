f2cf7a804dc776e10ac29434bea04f48954c3e23
Philip Rogers <pdr@chromium.org>
2019-12-09 21:43:51 +0000

    Add a histogram for the number of memory purge signals per local root
    UMA counts for the number of memory pressure signals per page load are
    high (~4%) and it is not clear if this means multiple signals are
    triggering for a low number pages, or if many pages are running under
    memory pressure. This patch adds an UMA metric for whether the memory
    pressure signals (purge == critical) given to each local frame root is
    the first.
    
    Context: https://docs.google.com/document/d/1oOebUZfMbW1lxgPifANu8BfPDN9BzRxXBRbvxZxnK8Q/view
    
    Bug: 1031634
    Change-Id: Ie63fec260611e042d17da14d200c8a458b4d63ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1956011
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Eric Karl <ericrk@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723098}

--- third_party/blink/renderer/core/frame/local_frame_view.h
+++ third_party/blink/renderer/core/frame/local_frame_view.h
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/paint_record.h"
 #include "third_party/blink/renderer/platform/graphics/paint_invalidation_reason.h"
 #include "third_party/blink/renderer/platform/graphics/subtree_paint_property_update_reason.h"
+#include "third_party/blink/renderer/platform/instrumentation/memory_pressure_listener.h"
 #include "third_party/blink/renderer/platform/timer.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
@@ -115,7 +116,8 @@ struct LifecycleData {
 
 class CORE_EXPORT LocalFrameView final
     : public GarbageCollected<LocalFrameView>,
-      public FrameView {
+      public FrameView,
+      public MemoryPressureListener {
   USING_GARBAGE_COLLECTED_MIXIN(LocalFrameView);
 
   friend class PaintControllerPaintTestBase;
@@ -824,6 +826,9 @@ class CORE_EXPORT LocalFrameView final
 
   void UpdateLayerDebugInfoEnabled();
 
+  // MemoryPressureListener
+  void OnPurgeMemory() override;
+
   LayoutSize size_;
 
   typedef HashSet<scoped_refptr<LayoutEmbeddedObject>> EmbeddedObjectSet;
@@ -943,6 +948,8 @@ class CORE_EXPORT LocalFrameView final
 
   // For testing.
   bool is_tracking_raster_invalidations_ = false;
+
+  bool received_memory_pressure_purge_signal_ = false;
 
   // Currently used in PushPaintArtifactToCompositor() to collect composited
   // layers as foreign layers. It's transient, but may live across frame updates
