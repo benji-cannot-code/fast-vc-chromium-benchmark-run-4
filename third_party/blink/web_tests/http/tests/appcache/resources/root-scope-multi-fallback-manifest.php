2125bceceb30733ed66027ee444b313185715ce3
Chase Phillips <cmp@chromium.org>
2019-12-09 19:18:34 +0000

    AppCache: Change default scope and provide additional tests
    This CL adds scope manifest override tests.  It also extends upgrade
    test cases to cover manifest data and scope data changes, which
    allows us to verify that scope changes result in differing cached
    manifests.
    
    While here, this changes override handling so relative paths are
    allowed.
    
    Bug: 999001
    Change-Id: Ia3efef020432708c7546df88bcd12b41f9d0ccf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957385
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723026}

---
+++ third_party/blink/web_tests/http/tests/appcache/resources/root-scope-multi-fallback-manifest.php
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+require_once '../../resources/portabilityLayer.php';
+
+header("Expires: Thu, 01 Dec 2003 16:00:00 GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Content-Type: text/cache-manifest");
+header("X-AppCache-Allowed: /");
+
+print("CACHE MANIFEST\n");
+print("FALLBACK:\n");
+print("/resources/network-simulator.php? simple.txt\n");
+print("does-not empty.txt\n");
+print("does-not-exist simple.txt\n");
+?>
