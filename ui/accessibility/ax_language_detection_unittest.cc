594371f0a469cccbd048974ab39774a4a78696fe
Chris Hall <chrishall@chromium.org>
2019-12-10 06:01:51 +0000

    Language detection: Simplifying method naming.
    FooForSubtree -> Foo
    FooForSubtreeInternal -> FooForSubtree
    
    R=dmazzoni,akihiroota
    
    Change-Id: Id518f3bf1c3f9a06f78be76b95068d29d0032630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949813
    Commit-Queue: Chris Hall <chrishall@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723256}

--- ui/accessibility/ax_language_detection_unittest.cc
+++ ui/accessibility/ax_language_detection_unittest.cc
@@ -73,8 +73,8 @@ TEST(AXLanguageDetectionTest, BoringTree) {
 
   AXTree tree(initial_state);
   ASSERT_NE(tree.language_detection_manager, nullptr);
-  tree.language_detection_manager->DetectLanguageForSubtree(tree.root());
-  tree.language_detection_manager->LabelLanguageForSubtree(tree.root());
+  tree.language_detection_manager->DetectLanguage(tree.root());
+  tree.language_detection_manager->LabelLanguage(tree.root());
 
   // Check that tree parenting conforms to expected shape.
   AXNode* node1 = tree.GetFromId(1);
@@ -162,8 +162,8 @@ TEST(AXLanguageDetectionTest, LangAttrInheritanceFeatureFlagOff) {
 
   AXTree tree(initial_state);
   ASSERT_NE(tree.language_detection_manager, nullptr);
-  tree.language_detection_manager->DetectLanguageForSubtree(tree.root());
-  tree.language_detection_manager->LabelLanguageForSubtree(tree.root());
+  tree.language_detection_manager->DetectLanguage(tree.root());
+  tree.language_detection_manager->LabelLanguage(tree.root());
 
   {
     AXNode* node1 = tree.GetFromId(1);
@@ -263,8 +263,8 @@ TEST(AXLanguageDetectionTest, LangAttrInheritanceFeatureFlagOn) {
 
   AXTree tree(initial_state);
   ASSERT_NE(tree.language_detection_manager, nullptr);
-  tree.language_detection_manager->DetectLanguageForSubtree(tree.root());
-  tree.language_detection_manager->LabelLanguageForSubtree(tree.root());
+  tree.language_detection_manager->DetectLanguage(tree.root());
+  tree.language_detection_manager->LabelLanguage(tree.root());
 
   {
     AXNode* node1 = tree.GetFromId(1);
@@ -376,8 +376,8 @@ TEST(AXLanguageDetectionTest, LanguageDetectionBasic) {
 
   AXTree tree(initial_state);
   ASSERT_NE(tree.language_detection_manager, nullptr);
-  tree.language_detection_manager->DetectLanguageForSubtree(tree.root());
-  tree.language_detection_manager->LabelLanguageForSubtree(tree.root());
+  tree.language_detection_manager->DetectLanguage(tree.root());
+  tree.language_detection_manager->LabelLanguage(tree.root());
 
   {
     AXNode* node1 = tree.GetFromId(1);
@@ -493,7 +493,7 @@ TEST(AXLanguageDetectionTest, LanguageDetectionDetectOnly) {
 
   AXTree tree(initial_state);
   ASSERT_NE(tree.language_detection_manager, nullptr);
-  tree.language_detection_manager->DetectLanguageForSubtree(tree.root());
+  tree.language_detection_manager->DetectLanguage(tree.root());
   // Purposefully not calling Label so we can test Detect in isolation.
 
   {
@@ -594,8 +594,8 @@ TEST(AXLanguageDetectionTest, kLanguageUntouched) {
 
   AXTree tree(initial_state);
   ASSERT_NE(tree.language_detection_manager, nullptr);
-  tree.language_detection_manager->DetectLanguageForSubtree(tree.root());
-  tree.language_detection_manager->LabelLanguageForSubtree(tree.root());
+  tree.language_detection_manager->DetectLanguage(tree.root());
+  tree.language_detection_manager->LabelLanguage(tree.root());
 
   {
     AXNode* node1 = tree.GetFromId(1);
