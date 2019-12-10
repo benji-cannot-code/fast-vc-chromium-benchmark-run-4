0f26962f4ed4ee7560e707034bb79334bb78cd87
Glen Robertson <glenrob@chromium.org>
2019-12-10 04:27:47 +0000

    desktop-pwas: Use scope from manifest even for create-shortcut apps.
    When "Create Shortcut" is selected on a page that has a manifest, we
    currently use all manifest values but keep the scope empty to
    ensure the shortcut app is treated differently to a PWA. This CL
    removes the special casing and different treatment.
    
    Also moves the default displayMode setting to web_application_info
    instead of setting it in UpdateWebAppInfoFromManifest even when it
    was not set in the manifest.
    
    Bug: 910016
    Change-Id: I61c29793d4dfec2db8342eb4e568fb422e531aaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950367
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723233}

--- chrome/browser/web_applications/extensions/install_manager_bookmark_app_unittest.cc
+++ chrome/browser/web_applications/extensions/install_manager_bookmark_app_unittest.cc
@@ -397,12 +397,7 @@ TEST_P(InstallManagerBookmarkAppInstallableSiteTest,
           web_contents(), kAppUrl, kAppUrl.spec(),
           AppBannerSettingsHelper::APP_BANNER_EVENT_DID_ADD_TO_HOMESCREEN)
           .is_null());
-
-  if (GetParam() == web_app::ForInstallableSite::kYes) {
-    EXPECT_EQ(GURL(kAppScope), GetScopeURLFromBookmarkApp(extension));
-  } else {
-    EXPECT_EQ(GURL(), GetScopeURLFromBookmarkApp(extension));
-  }
+  EXPECT_EQ(GURL(kAppScope), GetScopeURLFromBookmarkApp(extension));
 }
 
 TEST_P(InstallManagerBookmarkAppInstallableSiteTest,
@@ -452,12 +447,7 @@ TEST_P(InstallManagerBookmarkAppInstallableSiteTest,
   EXPECT_TRUE(extension->from_bookmark());
   EXPECT_EQ(kAppTitle, extension->name());
   EXPECT_EQ(kAppUrl, AppLaunchInfo::GetLaunchWebURL(extension));
-
-  if (GetParam() == web_app::ForInstallableSite::kYes) {
-    EXPECT_EQ(GURL(kAppScope), GetScopeURLFromBookmarkApp(extension));
-  } else {
-    EXPECT_EQ(GURL(), GetScopeURLFromBookmarkApp(extension));
-  }
+  EXPECT_EQ(GURL(kAppScope), GetScopeURLFromBookmarkApp(extension));
 }
 
 TEST_P(InstallManagerBookmarkAppInstallableSiteTest,
@@ -472,11 +462,7 @@ TEST_P(InstallManagerBookmarkAppInstallableSiteTest,
 
   const Extension* extension = InstallWebAppFromManifestWithFallback();
 
-  if (GetParam() == web_app::ForInstallableSite::kYes) {
-    EXPECT_EQ(GURL(kAppDefaultScope), GetScopeURLFromBookmarkApp(extension));
-  } else {
-    EXPECT_EQ(GURL(), GetScopeURLFromBookmarkApp(extension));
-  }
+  EXPECT_EQ(GURL(kAppDefaultScope), GetScopeURLFromBookmarkApp(extension));
 }
 
 INSTANTIATE_TEST_SUITE_P(All,
