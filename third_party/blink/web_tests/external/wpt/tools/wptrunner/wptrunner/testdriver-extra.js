a7f1333953ab0fae51367dc89e559739d3704819
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-12-10 23:04:45 +0000

    Import wpt@622d7254cb5a3bed5b621bda39882c3338aaf35f
    Using wpt-import in Chromium dd8930f8cc295be0fadf6581d73f6ca2c25efb80.
    With Chromium commits locally applied on WPT:
    a50e7e609f "Move `Sec-Fetch-User` to SetSecFetchMetadataHeaders."
    eb00b4d646 "LazyLoad: move invisible-image.html test to WPT"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    
    NOAUTOREVERT=true
    TBR=smcgruer
    
    No-Export: true
    Change-Id: I763d992341b8a844ba57637a196d7c1e71ab65f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960794
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#723562}

--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/testdriver-extra.js
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/testdriver-extra.js
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             result = JSON.parse(data.message).result
             pending_resolve(result);
         } else {
-            pending_reject();
+            pending_reject(`${data.status}: ${data.message}`);
         }
     });
 
