d5ffcd3a2d05e8b41c3e4935cb59472e83924863
Dave Tapuska <dtapuska@chromium.org>
2019-12-10 19:31:54 +0000

    Move service_worker_[start|ready]_time into load_timing_info.h
    If we move these values into the mojo definition then we can
    use it directly in blink and get rid of additional intermediate clases.
    ie. WebLoadTimingInfo
    
    Change-Id: I40f369597c4f305979767fb0a0d197282996a9d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954298
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723459}

--- content/renderer/service_worker/service_worker_subresource_loader.cc
+++ content/renderer/service_worker/service_worker_subresource_loader.cc
@@ -50,8 +50,10 @@ network::mojom::URLResponseHeadPtr RewriteServiceWorkerTime(
     base::TimeTicks service_worker_start_time,
     base::TimeTicks service_worker_ready_time,
     network::mojom::URLResponseHeadPtr response_head) {
-  response_head->service_worker_start_time = service_worker_start_time;
-  response_head->service_worker_ready_time = service_worker_ready_time;
+  response_head->load_timing.service_worker_start_time =
+      service_worker_start_time;
+  response_head->load_timing.service_worker_ready_time =
+      service_worker_ready_time;
   return response_head;
 }
 
@@ -219,7 +221,8 @@ void ServiceWorkerSubresourceLoader::StartRequest(
   // time to set workerStart, since it will either started soon or the fetch
   // event will be dispatched soon.
   // https://w3c.github.io/resource-timing/#dom-performanceresourcetiming-workerstart
-  response_head_->service_worker_start_time = base::TimeTicks::Now();
+  response_head_->load_timing.service_worker_start_time =
+      base::TimeTicks::Now();
   DispatchFetchEvent();
 }
 
@@ -422,9 +425,10 @@ void ServiceWorkerSubresourceLoader::OnFallback(
   mojo::PendingRemote<network::mojom::URLLoaderClient> client;
   auto client_impl = std::make_unique<HeaderRewritingURLLoaderClient>(
       std::move(url_loader_client_),
-      base::BindRepeating(&RewriteServiceWorkerTime,
-                          response_head_->service_worker_start_time,
-                          response_head_->service_worker_ready_time));
+      base::BindRepeating(
+          &RewriteServiceWorkerTime,
+          response_head_->load_timing.service_worker_start_time,
+          response_head_->load_timing.service_worker_ready_time));
   mojo::MakeSelfOwnedReceiver(std::move(client_impl),
                               client.InitWithNewPipeAndPassReceiver());
 
@@ -447,7 +451,8 @@ void ServiceWorkerSubresourceLoader::UpdateResponseTiming(
   // |service_worker_ready_time| becomes web-exposed
   // PerformanceResourceTiming#fetchStart, which is the time just before
   // dispatching the fetch event, so set it to |dispatch_event_time|.
-  response_head_->service_worker_ready_time = timing->dispatch_event_time;
+  response_head_->load_timing.service_worker_ready_time =
+      timing->dispatch_event_time;
   fetch_event_timing_ = std::move(timing);
 }
 
@@ -626,15 +631,15 @@ void ServiceWorkerSubresourceLoader::RecordTimingMetrics(bool handled) {
   UMA_HISTOGRAM_TIMES(
       "ServiceWorker.LoadTiming.Subresource."
       "ForwardServiceWorkerToWorkerReady",
-      response_head_->service_worker_ready_time -
-          response_head_->service_worker_start_time);
+      response_head_->load_timing.service_worker_ready_time -
+          response_head_->load_timing.service_worker_start_time);
 
   // Time spent by fetch handlers.
   UMA_HISTOGRAM_TIMES(
       "ServiceWorker.LoadTiming.Subresource."
       "WorkerReadyToFetchHandlerEnd",
       fetch_event_timing_->respond_with_settled_time -
-          response_head_->service_worker_ready_time);
+          response_head_->load_timing.service_worker_ready_time);
 
   if (handled) {
     // Mojo message delay. If the controller service worker lives in the same
