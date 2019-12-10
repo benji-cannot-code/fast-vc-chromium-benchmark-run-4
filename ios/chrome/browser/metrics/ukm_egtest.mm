c21ca88cb050bb6b359c3f9d136cb2c62ff21df0
Jérôme Lebel <jlebel@chromium.org>
2019-12-10 13:49:02 +0000

    [iOS][EG2] Target clean-up for ios/public/provider/chrome/browser/signin
    Fully splitting ":test_support" and ":fake_chrome_identity".
    ":test_support" should never be included in eg_test_support+eg2 targets.
    
    Updating all targets using "browser/signin" to choose either
    ":test_support", ":fake_chrome_identity", or both if needed.
    
    Bug: 987646
    Change-Id: Ie90f4a73283fc1e2fe9b231a3f6de9a2e15e52c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954482
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723340}

--- ios/chrome/browser/metrics/ukm_egtest.mm
+++ ios/chrome/browser/metrics/ukm_egtest.mm
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/public/provider/chrome/browser/signin/fake_chrome_identity.h"
-#import "ios/public/provider/chrome/browser/signin/fake_chrome_identity_service.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #include "ui/base/l10n/l10n_util.h"
 
