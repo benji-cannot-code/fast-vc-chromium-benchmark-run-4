7e8e6f9a36ff1515e9d4565b068fa2499786f618
Dana Fried <dfried@chromium.org>
2019-12-10 03:15:16 +0000

    Remove custom layout from RootView.
    FillLayout deriving from LayoutManagerBase means that we don't need a
    custom layout method to keep the announce view hidden - we can just mark
    it as ignored by the layout.
    
    Bug: 1024026
    Change-Id: I669a46e684b99a01596ab9be4d0e2899c460fd38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928319
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723220}

--- ui/views/widget/root_view.h
+++ ui/views/widget/root_view.h
@@ -124,7 +124,6 @@ class VIEWS_EXPORT RootView : public View,
   void SetMouseHandler(View* new_mouse_handler) override;
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
   void UpdateParentLayer() override;
-  void Layout() override;
 
  protected:
   // Overridden from View:
