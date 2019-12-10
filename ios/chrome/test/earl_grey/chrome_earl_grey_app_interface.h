d1357761e83dadf73a09fe6fe51488aa05495439
Kurt Horimoto <kkhorimoto@chromium.org>
2019-12-10 20:44:05 +0000

    [iOS][EG2] Convert BVC EGTests to EG2.
    In addition to adding an application-level open URL utility, this CL
    also converts the WebState visibility check to a unittest.
    
    Bug: 987646
    Change-Id: Ia2c99573dcabf64db0b06c094c95d5aa6d44c3df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1900690
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723491}

--- ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
+++ ios/chrome/test/earl_grey/chrome_earl_grey_app_interface.h
@@ -29,6 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // the operation failed.
 + (NSError*)removeBrowsingCache;
 
+// Opens |URL| using the application delegate.
++ (void)applicationOpenURL:(NSString*)spec;
+
 // Loads the URL |spec| in the current WebState with transition type
 // ui::PAGE_TRANSITION_TYPED and returns without waiting for the page to load.
 + (void)startLoadingURL:(NSString*)spec;
