298c9ce6a007fea1256be0535c4fc7a516323672
Sean Gilhuly <sgilhuly@chromium.org>
2019-12-10 20:54:57 +0000

    Use a stack for GPU fallback behaviour
    Refactor GpuDataManagerImplPrivate to decide upfront the strategy for
    GPU process fallback by pushing valid states onto a stack. Then, during
    fallback, pop the stack to get the new state, or crash if none are left.
    
    Add some unit tests to verify that fallback states occur in the right
    order.
    
    This is meant to make fallback behaviour more straightforward to help
    set up for fallback from Vulkan to GL, with no functional changes.
    
    Bug: 1005383
    Change-Id: I9644bceaabf834a98dc2a712445045f18010a8e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949007
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Sean Gilhuly <sgilhuly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723496}

--- content/browser/gpu/gpu_data_manager_impl_private_unittest.cc
+++ content/browser/gpu/gpu_data_manager_impl_private_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/gpu/gpu_data_manager_testing_autogen.h"
 #include "content/browser/gpu/gpu_data_manager_testing_entry_enums_autogen.h"
 #include "content/public/browser/gpu_data_manager_observer.h"
+#include "content/public/common/content_switches.h"
 #include "gpu/config/gpu_feature_type.h"
 #include "gpu/config/gpu_info.h"
 #include "gpu/config/gpu_switches.h"
@@ -249,5 +250,40 @@ TEST_F(GpuDataManagerImplPrivateTest, UnblockThisDomainFrom3DAPIs) {
             manager->Are3DAPIsBlockedAtTime(
                 GetDomain2ForTesting(), JustBeforeExpiration(manager.get())));
 }
+
+// Android and Chrome OS do not support software compositing, while Fuchsia does
+// not support falling back to software from Vulkan.
+#if !defined(OS_ANDROID) && !defined(OS_CHROMEOS)
+#if !defined(OS_FUCHSIA)
+TEST_F(GpuDataManagerImplPrivateTest, FallbackToSwiftShader) {
+  ScopedGpuDataManagerImplPrivate manager;
+  EXPECT_EQ(gpu::GpuMode::HARDWARE_ACCELERATED, manager->GetGpuMode());
+
+  manager->FallBackToNextGpuMode();
+  EXPECT_EQ(gpu::GpuMode::SWIFTSHADER, manager->GetGpuMode());
+}
+
+TEST_F(GpuDataManagerImplPrivateTest, FallbackWithSwiftShaderDisabled) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kDisableSoftwareRasterizer);
+  ScopedGpuDataManagerImplPrivate manager;
+  EXPECT_EQ(gpu::GpuMode::HARDWARE_ACCELERATED, manager->GetGpuMode());
+
+  manager->FallBackToNextGpuMode();
+#if defined(OS_WIN)
+  gpu::GpuMode expected_mode = gpu::GpuMode::DISABLED;
+#else
+  gpu::GpuMode expected_mode = gpu::GpuMode::DISPLAY_COMPOSITOR;
+#endif  // !OS_WIN
+  EXPECT_EQ(expected_mode, manager->GetGpuMode());
+}
+#endif  // !OS_FUCHSIA
+
+TEST_F(GpuDataManagerImplPrivateTest, GpuStartsWithGpuDisabled) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(switches::kDisableGpu);
+  ScopedGpuDataManagerImplPrivate manager;
+  EXPECT_EQ(gpu::GpuMode::SWIFTSHADER, manager->GetGpuMode());
+}
+#endif  // !OS_ANDROID && !OS_CHROMEOS
 
 }  // namespace content
