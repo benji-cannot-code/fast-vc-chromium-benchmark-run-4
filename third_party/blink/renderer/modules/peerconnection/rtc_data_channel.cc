ec9ad1bcd08e979c43b2ff47b512d82cf6119ac5
Andreas Haas <ahaas@chromium.org>
2019-12-10 19:11:27 +0000

    [peerconnection] Add overflow check to bufferedAmount in RTCDataChannel
    This CL adds an overflow check to {buffered_amount_} in RTCDataChannel.
    
    Here is some background about why I created this CL:
    We (V8 team) prepare ArrayBuffers to be bigger than 4GB. Therefore we
    changed the size field to size_t. Now we are changing all uses of
    ByteLength to be able to deal with size_t, either by accepting a size_t,
    or by throwing an exception if the size is too big.
    
    The size of an ArrayBuffer is added to {buffered_amount_}, which is of
    type {unsigned}. Therefore with a big enough ArrayBuffer an overflow
    could potentially happen. I thought that if I already add an overflow
    check for potentially too big ArrayBuffers, I could add the overflow
    check everywhere.
    
    R=guidou@chromium.org
    
    Bug: chromium:1008840
    Change-Id: I96385a52a357df1999c951c00307aea751e7a1fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953727
    Reviewed-by: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723427}

--- third_party/blink/renderer/modules/peerconnection/rtc_data_channel.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_data_channel.cc
@@ -127,6 +127,10 @@ static void ThrowNoBlobSupportException(ExceptionState* exception_state) {
                                      "Blob support not implemented yet");
 }
 
+static void ThrowBufferOverflowException(ExceptionState* exception_state) {
+  exception_state->ThrowRangeError("RTCDataChannel buffer overflow");
+}
+
 RTCDataChannel::Observer::Observer(
     scoped_refptr<base::SingleThreadTaskRunner> main_thread,
     RTCDataChannel* blink_channel,
@@ -361,6 +365,11 @@ void RTCDataChannel::send(const String& data, ExceptionState& exception_state) {
   }
 
   webrtc::DataBuffer data_buffer(data.Utf8());
+  if (!(base::CheckedNumeric<unsigned>(buffered_amount_) + data_buffer.size())
+           .IsValid()) {
+    ThrowBufferOverflowException(&exception_state);
+    return;
+  }
   buffered_amount_ += data_buffer.size();
   RecordMessageSent(*channel().get(), data_buffer.size());
   if (!channel()->Send(data_buffer)) {
@@ -381,6 +390,11 @@ void RTCDataChannel::send(DOMArrayBuffer* data,
   if (!data_length)
     return;
 
+  if (!(base::CheckedNumeric<unsigned>(buffered_amount_) + data_length)
+           .IsValid()) {
+    ThrowBufferOverflowException(&exception_state);
+    return;
+  }
   buffered_amount_ += data_length;
   if (!SendRawData(static_cast<const char*>((data->Data())), data_length)) {
     // TODO(https://crbug.com/937848): Don't throw an exception if data is
@@ -391,7 +405,13 @@ void RTCDataChannel::send(DOMArrayBuffer* data,
 
 void RTCDataChannel::send(NotShared<DOMArrayBufferView> data,
                           ExceptionState& exception_state) {
-  buffered_amount_ += data.View()->deprecatedByteLengthAsUnsigned();
+  if (!(base::CheckedNumeric<unsigned>(buffered_amount_) +
+        data.View()->byteLengthAsSizeT())
+           .IsValid()) {
+    ThrowBufferOverflowException(&exception_state);
+    return;
+  }
+  buffered_amount_ += data.View()->byteLengthAsSizeT();
   if (!SendRawData(static_cast<const char*>(data.View()->BaseAddress()),
                    data.View()->deprecatedByteLengthAsUnsigned())) {
     // TODO(https://crbug.com/937848): Don't throw an exception if data is
