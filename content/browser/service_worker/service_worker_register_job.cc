e65811331bd6ed256af706b5a00ef82074c4f758
Istiaque Ahmed <lazyboy@chromium.org>
2019-12-10 21:01:15 +0000

    SW: Ignore setting phase when a register job is already COMPLETE.
    Calling StopAllServiceWorkersForOrigin and DeleteForOrigin on a SW
    context can result in ServiceWorkerRegisterJob::OnStartWorkerFinished
    for a job that is already in COMPLETE phase.
    
    This CL bails out setting phase in this case.
    
    Bug: 1031764
    Change-Id: I863320d4dbf8606c52d8e55af017dae05d011d4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959525
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723501}

--- content/browser/service_worker/service_worker_register_job.cc
+++ content/browser/service_worker/service_worker_register_job.cc
@@ -551,6 +551,11 @@ void ServiceWorkerRegisterJob::UpdateAndContinue() {
 
 void ServiceWorkerRegisterJob::OnStartWorkerFinished(
     blink::ServiceWorkerStatusCode status) {
+  // Bail out early if the job has already completed.
+  // See https://crbug.com/1031764 for details.
+  if (phase_ == COMPLETE)
+    return;
+
   BumpLastUpdateCheckTimeIfNeeded();
 
   if (status == blink::ServiceWorkerStatusCode::kOk) {
