1da076b67d754564d5187310bcca577ae1aa7d6d
Gyuyoung Kim <gyuyoung@igalia.com>
2019-12-10 08:07:37 +0000

    Revert "Inline static Create() factory functions renderer/core/input"
    This reverts commit 8a6f6297d4f1ec404cf805e430e44e537dd1c477.
    
    Reason for revert: This CL violated the new Create() method removal
    policy as well as is suspected as a culprit of the performance
    degradation.
    
    Original change's description:
    > Inline static Create() factory functions renderer/core/input
    >
    > This CL removes Create() factory functions in
    > //third_partyb/blink/renderer/core/input as much as possible, and
    > makes all callers use MakeGarbageCollected<Foo> instead.
    >
    > Bug: 939691
    > Change-Id: I8d392effb5089f4a7025e38c5c83061652d2a9fe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1942890
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#720298}
    
    TBR=haraken@chromium.org,gyuyoung@igalia.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 939691, 1030860
    Change-Id: I9a77c756132927ae5ecf589699f6be23d0b9e13e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959987
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Gyuyoung Kim <gyuyoung@igalia.com>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#723283}

--- third_party/blink/renderer/core/input/touch_event_manager.cc
+++ third_party/blink/renderer/core/input/touch_event_manager.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/hit_test_canvas_result.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/page.h"
-#include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
@@ -396,7 +395,7 @@ TouchEventManager::DispatchTouchEventFromAccumulatdTouchPoints() {
   }
 
   // Holds the complete set of touches on the screen.
-  auto* touches = MakeGarbageCollected<TouchList>();
+  TouchList* touches = TouchList::Create();
 
   // A different view on the 'touches' list above, filtered and grouped by
   // event target. Used for the |targetTouches| list in the JS event.
@@ -424,7 +423,7 @@ TouchEventManager::DispatchTouchEventFromAccumulatdTouchPoints() {
     TargetTouchesHeapMap::iterator target_touches_iterator =
         touches_by_target.find(touch_target);
     if (target_touches_iterator == touches_by_target.end()) {
-      touches_by_target.Set(touch_target, MakeGarbageCollected<TouchList>());
+      touches_by_target.Set(touch_target, TouchList::Create());
       target_touches_iterator = touches_by_target.find(touch_target);
     }
 
@@ -445,10 +444,8 @@ TouchEventManager::DispatchTouchEventFromAccumulatdTouchPoints() {
     // for further discussion about the TouchStationary state.
     if (!touch_point_attribute->stale_ && known_target) {
       size_t event_type_idx = event_type - WebInputEvent::kPointerTypeFirst;
-      if (!changed_touches[event_type_idx].touches_) {
-        changed_touches[event_type_idx].touches_ =
-            MakeGarbageCollected<TouchList>();
-      }
+      if (!changed_touches[event_type_idx].touches_)
+        changed_touches[event_type_idx].touches_ = TouchList::Create();
       changed_touches[event_type_idx].touches_->Append(touch);
       changed_touches[event_type_idx].targets_.insert(touch_target);
     }
