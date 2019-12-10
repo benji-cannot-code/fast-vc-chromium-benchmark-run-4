67110467cc3717c58963e7344ecadafd0106d8a7
Jan Wilken Dörrie <jdoerrie@chromium.org>
2019-12-10 14:38:44 +0000

    [Touch To Fill] Move TTF Controller into TTF Component
    r696337 created a //chrome/browser/touch_to_fill component containing
    most of the classes related to the Touch To Fill feature. So far a
    notably hold-out is the TouchToFillController, which currently still
    lives in //chrome/browser/password_manager. This change fixes this by
    moving the controller to the touch_to_fill component as well.
    
    TBR=dvadym
    
    Bug: 957532
    Change-Id: I949627e2154af4499c28ef4ec18d8e932434807b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960150
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723346}

--- chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
+++ chrome/browser/password_manager/chrome_password_manager_client_unittest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/mock_password_accessory_controller.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "chrome/browser/password_manager/password_store_factory.h"
-#include "chrome/browser/password_manager/touch_to_fill_controller.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
--- chrome/browser/touch_to_fill/touch_to_fill_controller.h
+++ chrome/browser/touch_to_fill/touch_to_fill_controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PASSWORD_MANAGER_TOUCH_TO_FILL_CONTROLLER_H_
-#define CHROME_BROWSER_PASSWORD_MANAGER_TOUCH_TO_FILL_CONTROLLER_H_
+#ifndef CHROME_BROWSER_TOUCH_TO_FILL_TOUCH_TO_FILL_CONTROLLER_H_
+#define CHROME_BROWSER_TOUCH_TO_FILL_TOUCH_TO_FILL_CONTROLLER_H_
 
 #include <memory>
 #include <utility>
@@ -90,4 +90,4 @@ class TouchToFillController {
   std::unique_ptr<TouchToFillView> view_;
 };
 
-#endif  // CHROME_BROWSER_PASSWORD_MANAGER_TOUCH_TO_FILL_CONTROLLER_H_
+#endif  // CHROME_BROWSER_TOUCH_TO_FILL_TOUCH_TO_FILL_CONTROLLER_H_
