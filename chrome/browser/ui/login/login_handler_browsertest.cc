38b642dd74f463d66a82e97f2a86f0fc114d4bfd
Ilia Samsonov <isamsonov@google.com>
2019-12-10 18:59:13 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=estark@chromium.org
    
    Bug: 1023677
    Change-Id: I727b8a7caa8316441c9f496699db2312b8e988fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957551
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723405}

--- chrome/browser/ui/login/login_handler_browsertest.cc
+++ chrome/browser/ui/login/login_handler_browsertest.cc
@@ -241,7 +241,7 @@ class LoginPromptBrowserTest
 };
 
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     LoginPromptBrowserTest,
     ::testing::Values(SplitAuthCacheByNetworkIsolationKey::kFalse,
                       SplitAuthCacheByNetworkIsolationKey::kTrue));
@@ -645,7 +645,7 @@ class MultiRealmLoginPromptBrowserTest : public LoginPromptBrowserTest {
 };
 
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     MultiRealmLoginPromptBrowserTest,
     ::testing::Values(SplitAuthCacheByNetworkIsolationKey::kFalse,
                       SplitAuthCacheByNetworkIsolationKey::kTrue));
