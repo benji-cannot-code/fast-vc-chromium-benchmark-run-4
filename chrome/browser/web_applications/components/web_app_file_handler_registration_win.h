e0996a8b9b506e161f4fba06fc4d0d9415b6239f
David Bienvenu <davidbienvenu@chromium.org>
2019-12-10 21:55:16 +0000

    desktop-pwas: Get pwa registration working with windows file extensions.
    Bug: 960245
    Change-Id: Ied12e6e129388e4c7a342a8ea84f163970ef8741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1838212
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723524}

--- chrome/browser/web_applications/components/web_app_file_handler_registration_win.h
+++ chrome/browser/web_applications/components/web_app_file_handler_registration_win.h
@@ -9,8 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_file_handler_registration.h"
 
 #include "base/files/file_path.h"
+#include "base/strings/string16.h"
+#include "chrome/browser/web_applications/components/web_app_helpers.h"
 
 namespace web_app {
+
+// Returns the Windows ProgId for the web app with the passed |app_id| in
+// |profile|.
+base::string16 GetProgIdForApp(Profile* profile, const AppId& app_id);
 
 // The name of "Last Browser" file, where UpdateChromeExePath() stores the path
 // of the last Chrome executable to use the containing user-data directory.
