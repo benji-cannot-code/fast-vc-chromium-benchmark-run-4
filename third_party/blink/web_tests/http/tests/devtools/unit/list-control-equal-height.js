6c0c72a64d590bd788fad2f8757586ebc32a6577
Jack Lynch <jalyn@microsoft.com>
2019-12-11 00:43:48 +0000

    DevTools: Fix and re-enable ListControl test
    The changes to UI.ListControl in [1] break a web test, so this CL exists
    to fix and re-enable it once the product changes merge.
    
    [1] https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1880309
    
    Bug: 963183
    Change-Id: Ifd52271fdd1d43f45e4635ba3ac88a3cdb80fe07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1690089
    Reviewed-by: Lorne Mitchell <lomitch@microsoft.com>
    Commit-Queue: Jack Lynch <jalyn@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#723611}

--- third_party/blink/web_tests/http/tests/devtools/unit/list-control-equal-height.js
+++ third_party/blink/web_tests/http/tests/devtools/unit/list-control-equal-height.js
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if (toElement)
         toElement.classList.add('selected');
     }
+
+    updateSelectedItemARIA(fromElement, toElement) {
+      return false;
+    }
   }
 
   var delegate = new Delegate();
