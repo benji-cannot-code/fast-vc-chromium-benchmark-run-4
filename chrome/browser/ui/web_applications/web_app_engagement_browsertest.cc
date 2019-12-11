05f81de8d217cb70cd5f1a8e03227a9f7267fa4d
Ilia Samsonov <isamsonov@google.com>
2019-12-11 02:44:04 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=alancutter@chromium.org
    
    Bug: 1023677
    Change-Id: I3cdd20ce9042c2b5412dee8b921146f1e45dbf1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957539
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723667}

--- chrome/browser/ui/web_applications/web_app_engagement_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_engagement_browsertest.cc
@@ -485,7 +485,7 @@ IN_PROC_BROWSER_TEST_P(WebAppEngagementBrowserTest, RecordedForNonApps) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     WebAppEngagementBrowserTest,
     ::testing::Values(ControllerType::kHostedAppController,
                       ControllerType::kUnifiedControllerWithBookmarkApp,
@@ -493,7 +493,7 @@ INSTANTIATE_TEST_SUITE_P(
     ControllerTypeParamToString);
 
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     HostedAppEngagementBrowserTest,
     ::testing::Values(ControllerType::kHostedAppController,
                       ControllerType::kUnifiedControllerWithBookmarkApp),
