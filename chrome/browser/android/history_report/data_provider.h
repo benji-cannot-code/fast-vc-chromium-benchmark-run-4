66c5daff8987c0120ad24c3d16590482c96839a0
Scott Violet <sky@chromium.org>
2019-12-11 02:31:25 +0000

    android: fixs access to BookmarkModel from DataProvider
    DataProvider::Query() is called from a background thread. BookmarkModel
    isn't thread safe, and shouldn't be used from a background thread. This
    code should go through bookmarks::ModelLoader and
    bookmarks::HistoryBookmarkModel, which are thread safe.
    
    BUG=1032172
    TEST=none
    
    Change-Id: I97dd97daed79d64e7ad28b3aa37bfadc0b7d9899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961216
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723661}

--- chrome/browser/android/history_report/data_provider.h
+++ chrome/browser/android/history_report/data_provider.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/task/cancelable_task_tracker.h"
 
 class Profile;
@@ -22,6 +23,7 @@ class HistoryService;
 
 namespace bookmarks {
 class BookmarkModel;
+class ModelLoader;
 }
 
 namespace history_report {
@@ -49,7 +51,7 @@ class DataProvider {
   void RecreateLog();
 
   history::HistoryService* history_service_;
-  bookmarks::BookmarkModel* bookmark_model_;
+  scoped_refptr<bookmarks::ModelLoader> bookmark_model_loader_;
   DeltaFileService* delta_file_service_;
   base::CancelableTaskTracker history_task_tracker_;
 
