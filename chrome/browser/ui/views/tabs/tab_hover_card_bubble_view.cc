8094b0a60bda820f6e09133c1e50414fa90d91ae
Caroline Rising <corising@chromium.org>
2019-12-11 01:04:48 +0000

    Update tab hover card default flag setting to Enabled B.
    This change increases the delay before a hover card will appear.
    
    Bug: 910739
    Change-Id: I1bfc24b6ab56316fd3d05147e1c46fd6add5169d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961078
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723626}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
@@ -63,7 +63,7 @@ bool AreHoverCardImagesEnabled() {
 base::TimeDelta GetMinimumTriggerDelay() {
   int delay_group = base::GetFieldTrialParamByFeatureAsInt(
       features::kTabHoverCards, features::kTabHoverCardsFeatureParameterName,
-      2);
+      1);
   switch (delay_group) {
     case 2:
       return base::TimeDelta::FromMilliseconds(150);
@@ -78,7 +78,7 @@ base::TimeDelta GetMinimumTriggerDelay() {
 base::TimeDelta GetMaximumTriggerDelay() {
   int delay_group = base::GetFieldTrialParamByFeatureAsInt(
       features::kTabHoverCards, features::kTabHoverCardsFeatureParameterName,
-      2);
+      1);
   switch (delay_group) {
     case 2:
       return base::TimeDelta::FromMilliseconds(500);
