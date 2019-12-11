e7279db9bbe70cb80553c74815f04436ef750eaa
Rohit Agarwal <roagarwal@chromium.org>
2019-12-11 15:49:50 +0000

    Removes the TODO to extend ambient auth tests for guest sessions in ChromeOS.
    Ambient authentication is when the browser delegates the authentication
    to the OS and the OS uses the default credentials to authenticate.
    The guest sessions in ChromeOS by default were designed not to have
    any state sharing with existing profiles and any default credentials.
    
    Therefore, the TODO is removed and not valid.
    
    Bug: 1030624
    Change-Id: Ifbdde6954bd0f9bb4294cb241dae3995011ef6d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962188
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723829}

--- chrome/browser/net/profile_network_context_service_browsertest.cc
+++ chrome/browser/net/profile_network_context_service_browsertest.cc
@@ -195,8 +195,8 @@ class AmbientAuthenticationTestWithPolicy
     EXPECT_EQ(IsAmbientAuthAllowedForProfile(GetIncognitoProfile()),
               IsIncognitoAllowedInFeature() ||
                   IsIncognitoAllowedInPolicy(policy_value));
-// TODO(crbug.com/1030624): Adapt this test for chromeos guest sessions too once
-// we have device level policy for guests
+// ChromeOS guest sessions don't have the capability to
+// do ambient authentications.
 #if !defined(OS_CHROMEOS)
     EXPECT_EQ(
         IsAmbientAuthAllowedForProfile(GetGuestProfile()),
