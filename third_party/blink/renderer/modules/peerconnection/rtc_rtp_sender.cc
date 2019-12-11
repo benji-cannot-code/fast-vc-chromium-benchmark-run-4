ec51f01162dc17e83b05546721b1ac47ddf0a14c
Antonio Gomes <tonikitoo@igalia.com>
2019-12-11 11:51:31 +0000

    Switch RTCRtpSenderPlatform away from WebVector
    BUG=787254
    R=guidou@chromium.org
    
    Change-Id: Ia30e8ee0a8dacd36028cf8f181cd413674d1037b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1958444
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#723785}

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_sender.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_sender.cc
@@ -489,7 +489,7 @@ ScriptPromise RTCRtpSender::getStats(ScriptState* script_state) {
   ScriptPromise promise = resolver->Promise();
   sender_->GetStats(
       WTF::Bind(WebRTCStatsReportCallbackResolver, WrapPersistent(resolver)),
-      GetExposedGroupIdsDeprecated(script_state));
+      GetExposedGroupIds(script_state));
   return promise;
 }
 
@@ -553,7 +553,7 @@ void RTCRtpSender::setStreams(HeapVector<Member<MediaStream>> streams,
                                       kOnlySupportedInUnifiedPlanMessage);
     return;
   }
-  WebVector<WebString> stream_ids;
+  Vector<String> stream_ids;
   for (auto stream : streams)
     stream_ids.emplace_back(stream->id());
   sender_->SetStreams(stream_ids);
