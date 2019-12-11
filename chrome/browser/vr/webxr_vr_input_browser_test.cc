6a2574659704d072cdbc7e3020d5f2d64568b007
Robert Kroeger <rjkroege@chromium.org>
2019-12-11 19:12:06 +0000

    Disable TestControllerInputRegistered
    WebXrVrOpenVrBrowserTest.TestControllerInputRegistered is flaky on
    Windows GPU bots. Disabling to avoid hiding other failures.
    
    TBR=kbr@chromium.org
    
    Bug: http://crbug.com/1033087
    Change-Id: I5ef8ddf53b8856ab79b9ff2592b9f560d7aba5e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962892
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723915}

--- chrome/browser/vr/webxr_vr_input_browser_test.cc
+++ chrome/browser/vr/webxr_vr_input_browser_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/vr/test/mock_xr_device_hook_base.h"
 #include "chrome/browser/vr/test/multi_class_browser_test.h"
 #include "chrome/browser/vr/test/webxr_vr_browser_test.h"
@@ -960,7 +961,14 @@ IN_PROC_BROWSER_TEST_F(WebXrVrWmrBrowserTest, TestVoiceSelectRegistered) {
 // Test that controller input is registered via WebXR's input method.
 // Equivalent to
 // WebXrVrInputTest#testControllerClicksRegisteredOnDaydream_WebXr.
-WEBXR_VR_ALL_RUNTIMES_BROWSER_TEST_F(TestControllerInputRegistered) {
+// Fails on Win only.  http://crbug.com/1033087
+#if defined(OS_WIN)
+#define MAYBE_TestControllerInputRegistered \
+  DISABLED_TestControllerInputRegistered
+#else
+#define MAYBE_TestControllerInputRegistered TestControllerInputRegistered
+#endif
+WEBXR_VR_ALL_RUNTIMES_BROWSER_TEST_F(MAYBE_TestControllerInputRegistered) {
   WebXrControllerInputMock my_mock;
 
   unsigned int controller_index = my_mock.CreateAndConnectMinimalGamepad();
