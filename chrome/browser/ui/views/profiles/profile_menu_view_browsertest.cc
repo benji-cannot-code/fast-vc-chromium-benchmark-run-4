15403dcd14544c7e4b759892e55ff1481c13cc23
David Roger <droger@chromium.org>
2019-12-11 00:30:21 +0000

    Fieldtrial testing config for MyChromeEverywhere
    Change-Id: Iaa59fca40749663e5edce9c522dbb26c0b43cba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1955558
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Auto-Submit: David Roger <droger@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723606}

--- chrome/browser/ui/views/profiles/profile_menu_view_browsertest.cc
+++ chrome/browser/ui/views/profiles/profile_menu_view_browsertest.cc
@@ -160,7 +160,9 @@ Profile* SetupProfilesForLock(Profile* signed_in) {
 class ProfileMenuViewExtensionsTest
     : public SupportsTestDialog<extensions::ExtensionBrowserTest> {
  public:
-  ProfileMenuViewExtensionsTest() {}
+  ProfileMenuViewExtensionsTest() {
+    scoped_feature_list_.InitAndDisableFeature(features::kProfileMenuRevamp);
+  }
   ~ProfileMenuViewExtensionsTest() override {}
 
   // SupportsTestUi:
@@ -265,6 +267,8 @@ class ProfileMenuViewExtensionsTest
   }
 
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+
   DISALLOW_COPY_AND_ASSIGN(ProfileMenuViewExtensionsTest);
 };
 
