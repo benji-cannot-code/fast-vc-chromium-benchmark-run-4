d8de2030b07114e1c135f0035226319ce7b18af1
Tomasz Wiszkowski <ender@google.com>
2019-12-11 18:42:04 +0000

    Convert basic suggestion view to BaseSuggestions.
    This change updates catch-all suggestion view, enabling it to take
    advantage of the new BaseSuggestion component.
    
    Doc: http://doc/1aL_UcW1gdeSLqNzuJZlQPBqMG0XAwVaoa05UTyVulp8
    Discuss: http://doc/13OQajMRPzXP5r9Gz1KMq_G0tLLtJBAGMYbDLev487GQ
    
    Bug: 982818,1021263
    
    Change-Id: I0e5d58a92d818175a397ee9d18089e12d3d0c888
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1888953
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#723901}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/DecoratedSuggestionView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/DecoratedSuggestionView.java
@@ -52,7 +52,12 @@ class DecoratedSuggestionView extends SimpleHorizontalLayoutView {
         addView(mContentView);
     }
 
-    /** Returns widget holding suggestion decoration icon.  */
+    /** @return Embedded suggestion content view.  */
+    View getContentView() {
+        return mContentView;
+    }
+
+    /** @return Widget holding suggestion decoration icon.  */
     RoundedCornerImageView getImageView() {
         return mSuggestionIcon;
     }
