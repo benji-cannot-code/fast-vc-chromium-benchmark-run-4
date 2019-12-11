c56fe485d800b2d1c5d8be7988f12ef0e3871f56
Robert Liao <robliao@chromium.org>
2019-12-11 17:56:32 +0000

    Rename CreateFrontElidingTitleLabel to CreateTitleOriginLabel
    This removes the policy (front eliding) from the name and changes it to
    the intended use of the label.
    
    The front eliding policy is likely to change in the future, so adding
    it to the name creates unnecessary churn.
    
    BUG=987715
    
    Change-Id: I0ce4190f6de38cfc218984adb0204fcf4602e949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960969
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723873}

--- chrome/browser/ui/views/title_origin_label.h
+++ chrome/browser/ui/views/title_origin_label.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_FRONT_ELIDING_TITLE_LABEL_H_
-#define CHROME_BROWSER_UI_VIEWS_FRONT_ELIDING_TITLE_LABEL_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_TITLE_ORIGIN_LABEL_H_
+#define CHROME_BROWSER_UI_VIEWS_TITLE_ORIGIN_LABEL_H_
 
 #include <memory>
 
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(crbug.com/987715): For now this is a simplistic implementation that
 // elides the entire string from the front, which works well for English strings
 // that start with the origin, but not so well for other languages.
-std::unique_ptr<views::Label> CreateFrontElidingTitleLabel(
+std::unique_ptr<views::Label> CreateTitleOriginLabel(
     const base::string16& text);
 
-#endif  // CHROME_BROWSER_UI_VIEWS_FRONT_ELIDING_TITLE_LABEL_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_TITLE_ORIGIN_LABEL_H_
