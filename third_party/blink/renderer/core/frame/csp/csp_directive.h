8d83c62cd39fe80046761bbf7c5b34af8b75642c
Daniel Vogelheim <vogelheim@chromium.org>
2019-12-11 11:28:11 +0000

    [Trusted Types] Support 'allow-duplicates' in trusted-types directive.
    Implements 'allow-duplicates' according to
    https://github.com/w3c/webappsec-trusted-types/pull/245
    
    Also updates policy name checking to follow the spec.
    
    Bug: 1030259
    Change-Id: I74a1a12d5e36b642cfbcf2517a6ee8f0d7f000e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1950529
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723780}

--- third_party/blink/renderer/core/frame/csp/csp_directive.h
+++ third_party/blink/renderer/core/frame/csp/csp_directive.h
@@ -8,12 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
-
-class ContentSecurityPolicy;
 
 class CORE_EXPORT CSPDirective : public GarbageCollected<CSPDirective> {
  public:
