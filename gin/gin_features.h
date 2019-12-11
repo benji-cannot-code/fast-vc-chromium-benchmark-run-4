ed8cf2ca662b4672d96823c21a9f777d5af6dba2
Maya Lekova <mslekova@chromium.org>
2019-12-11 14:32:33 +0000

    Add feature config for finching concurrent inlining
    Bug: chromium:1031988
    Change-Id: I6cba869a4365b24fff4b44d6add03608ce004e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954515
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: Maya Lekova <mslekova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723811}

--- gin/gin_features.h
+++ gin/gin_features.h
@@ -19,6 +19,7 @@ GIN_EXPORT extern const base::Feature kV8HugeMaxOldGenerationSize;
 GIN_EXPORT extern const base::Feature kV8GCBackgroundSchedule;
 GIN_EXPORT extern const base::Feature kV8GCLessCompaction;
 GIN_EXPORT extern const base::Feature kV8GCAlwaysPromoteYoungMC;
+GIN_EXPORT extern const base::Feature kV8ConcurrentInlining;
 
 }  // namespace features
 
