ef30d928438c15003920e9d9ec7f5fea0131acaa
Hesen Zhang <hesen@google.com>
2019-12-11 20:59:30 +0000

    [Update notificaiton service]: Glue Android side to backend.
    - Rename the glue code to UpdateNotificationServiceBridge.
    - Connecting Android side to update notification service.
    
    Bug: 1013685
    Change-Id: I8825d1522defcceb3c28ad4df13ffe3593ce842d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949445
    Auto-Submit: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723964}

---
+++ chrome/browser/updates/update_notification_service_bridge.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UPDATES_UPDATE_NOTIFICATION_SERVICE_BRIDGE_H_
+#define CHROME_BROWSER_UPDATES_UPDATE_NOTIFICATION_SERVICE_BRIDGE_H_
+
+#include "base/optional.h"
+#include "base/time/time.h"
+
+namespace updates {
+
+// Functions for calling into UpdateNotifiactionServiceBridge.java.
+
+// Updates and persists |timestamp| in Android shared preference.
+void UpdateLastShownTimeStamp(base::Time timestamp);
+
+// Return persisted timestamp of last shown notification from Android shared
+// preference. Return nullopt if there is no data.
+base::Optional<base::Time> GetLastShownTimestamp();
+
+}  // namespace updates
+
+#endif  // CHROME_BROWSER_UPDATES_UPDATE_NOTIFICATION_SERVICE_BRIDGE_H_
