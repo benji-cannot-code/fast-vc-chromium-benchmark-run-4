c4daa18aa99e72bd5f00693f0f113266c43b11a6
Evan Stade <estade@chromium.org>
2019-12-11 23:33:44 +0000

    Move DualControlLayout to components
    Moved from chrome.browser.ui.widget to components.browser_ui.widget
    
    This establishes the testing target for //components/browser_ui/widget
    
    Bug: 1030785
    Change-Id: I866d3b18ea90cee59564cb51ec024d2f3c491a51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959931
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724033}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/DualControlLayoutTest.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/DualControlLayoutTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.ui.widget;
+package org.chromium.components.browser_ui.widget;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -25,8 +25,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.chrome.browser.ui.widget.DualControlLayout.DualControlLayoutAlignment;
-import org.chromium.chrome.browser.ui.widget.test.R;
+import org.chromium.components.browser_ui.widget.DualControlLayout.DualControlLayoutAlignment;
+import org.chromium.components.browser_ui.widget.test.R;
 
 /**
  * Tests for DualControlLayout.
