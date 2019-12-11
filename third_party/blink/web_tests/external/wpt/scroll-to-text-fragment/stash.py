dda386d09612bd49156ea9a166e6f709c944a487
Nick Burris <nburris@chromium.org>
2019-12-11 18:45:59 +0000

    [scroll-to-text-fragment] Replace BroadcastChannel with WPT Stash
    BroadcastChannel was not supported on all platforms, and also caused
    some flakiness. This patch replaces BroadcastChannel usage with WPT
    Stash.
    
    Also adds a META.yml.
    
    Bug: https://github.com/web-platform-tests/wpt/issues/20269
    Change-Id: I25f7fa30da59c156de88ee5f7259152369493629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1925428
    Commit-Queue: Nick Burris <nburris@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723903}

---
+++ third_party/blink/web_tests/external/wpt/scroll-to-text-fragment/stash.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import time
+
+def main(request, response):
+    key = request.GET.first("key")
+
+    if request.method == "POST":
+        # Received result data from target page
+        request.server.stash.put(key, request.body, '/scroll-to-text-fragment/')
+        return "ok"
+    else:
+        # Request for result data from test page
+        value = request.server.stash.take(key, '/scroll-to-text-fragment/')
+        # Poll until data is stashed
+        while value is None:
+          time.sleep(.1)
+          value = request.server.stash.take(key, '/scroll-to-text-fragment/')
+        return value
