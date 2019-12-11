3f9ebc2e9479d0a4fad8941ccd899324beff2942
Lucas Furukawa Gadani <lfg@chromium.org>
2019-12-11 21:44:25 +0000

    Portals: Send FrameSinkId with portal adoption.
    This fixes a race where the FrameSinkId might arrive after the embedder
    generated a frame, causing a flash during portal adoption.
    
    Bug: 966119
    Change-Id: Iad8948f1f0c596817fbf468a54bbd5ccf432cef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960825
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723994}

--- content/test/portal/portal_created_observer.cc
+++ content/test/portal/portal_created_observer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/frame_host/render_frame_proxy_host.h"
 #include "content/browser/portal/portal.h"
+#include "content/browser/renderer_host/render_widget_host_view_base.h"
 #include "content/test/portal/portal_interceptor_for_testing.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 
@@ -57,6 +58,10 @@ void PortalCreatedObserver::AdoptPortal(
   RenderFrameProxyHost* proxy_host = portal_->CreateProxyAndAttachPortal();
   std::move(callback).Run(
       proxy_host->GetRoutingID(),
+      static_cast<RenderWidgetHostViewBase*>(proxy_host->frame_tree_node()
+                                                 ->render_manager()
+                                                 ->GetRenderWidgetHostView())
+          ->GetFrameSinkId(),
       proxy_host->frame_tree_node()->current_replication_state(),
       portal->GetDevToolsFrameToken());
 
