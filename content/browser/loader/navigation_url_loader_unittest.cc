c09a0d57dc333cd03fa7369dd6217aae8c1e0e48
Sigurd Schneider <sigurds@chromium.org>
2019-12-11 09:05:17 +0000

    [ssl-manager] Report net error on certificate error
    This CL passes the net error instead of a generic net::ERR_ABORTED
    to clients if a certificate error occurred. This improves error
    reporting downstream (i.e. in DevTools). A detailed explanation is
    in the bug description chromium:1032104.
    
    Bug: chromium:1030746, chromium:1032104
    Change-Id: I1bef6b7afcf35bc1d972cc3290f480b316675fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1958990
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723744}

--- content/browser/loader/navigation_url_loader_unittest.cc
+++ content/browser/loader/navigation_url_loader_unittest.cc
@@ -125,7 +125,7 @@ TEST_F(NavigationURLLoaderTest, RequestFailedCertError) {
 
   // Wait for the request to fail as expected.
   delegate.WaitForRequestFailed();
-  ASSERT_EQ(net::ERR_ABORTED, delegate.net_error());
+  ASSERT_EQ(net::ERR_CERT_COMMON_NAME_INVALID, delegate.net_error());
   net::SSLInfo ssl_info = delegate.ssl_info();
   EXPECT_TRUE(ssl_info.is_valid());
   EXPECT_TRUE(
@@ -159,7 +159,7 @@ TEST_F(NavigationURLLoaderTest, RequestFailedCertErrorFatal) {
 
   // Wait for the request to fail as expected.
   delegate.WaitForRequestFailed();
-  ASSERT_EQ(net::ERR_ABORTED, delegate.net_error());
+  ASSERT_EQ(net::ERR_CERT_COMMON_NAME_INVALID, delegate.net_error());
   net::SSLInfo ssl_info = delegate.ssl_info();
   EXPECT_TRUE(ssl_info.is_valid());
   EXPECT_TRUE(
