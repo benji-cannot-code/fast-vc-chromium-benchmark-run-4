c1fd6489c5db3ba63033acb152c0cfd0c4522487
David Valleau <valleau@chromium.org>
2019-12-11 22:30:57 +0000

    Introducing PPD License lookup to PpdProvider
    This change adds the ResolvePpdLicense() method to PpdProvider which
    can be used to determine whether a given effective make and model name
    of a printer is associated with any proprietary PPD license.
    
    Change-Id: I3bdfa2aca501dbb6a9a756c86eaf7eaffb78301c
    Bug: 1020586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953694
    Commit-Queue: David Valleau <valleau@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724012}

--- chromeos/printing/ppd_provider.h
+++ chromeos/printing/ppd_provider.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/files/file_path.h"
+#include "base/strings/string_piece.h"
 #include "base/version.h"
 #include "chromeos/chromeos_export.h"
 #include "chromeos/printing/printer_configuration.h"
@@ -162,6 +163,14 @@ class CHROMEOS_EXPORT PpdProvider : public base::RefCounted<PpdProvider> {
                               const Printer::PpdReference& ref,
                               const std::string& manufacturer)>;
 
+  // Result of a ResolvePpdLicense call. If |result| is SUCCESS, then
+  // |license_name| will be used to indicate the license associated with the
+  // requested PPD. If |license_name| is empty, then the requested PPD does not
+  // require a license.
+  using ResolvePpdLicenseCallback =
+      base::OnceCallback<void(CallbackResultCode result,
+                              const std::string& license_name)>;
+
   // Result of a ReverseLookup call.  If the result code is SUCCESS, then
   // |manufactuer| and |model| contain the strings that could have generated
   // the reference being looked up.
@@ -205,6 +214,16 @@ class CHROMEOS_EXPORT PpdProvider : public base::RefCounted<PpdProvider> {
   // |cb| will be called on the invoking thread, and will be sequenced.
   virtual void ResolvePpd(const Printer::PpdReference& reference,
                           ResolvePpdCallback cb) = 0;
+
+  // Retrieves the name of the PPD license associated with the given printer
+  // |effective_make_and_model|. If the name of the retrieved license is empty,
+  // then the PPD does not require a license. If |effective_make_and_model| is
+  // already present in the cache, then |cb| will fire immediately. Otherwise,
+  // the PpdIndex will be fetched in order to retrieve the associated license.
+  //
+  // |cb| will be called on the invoking thread, and will be sequenced.
+  virtual void ResolvePpdLicense(base::StringPiece effective_make_and_model,
+                                 ResolvePpdLicenseCallback cb) = 0;
 
   // For a given PpdReference, retrieve the make and model strings used to
   // construct that reference.
