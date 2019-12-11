7faa0ad410e1f87c725c022686754b2907c32911
David Tseng <dtseng@chromium.org>
2019-12-11 19:34:37 +0000

    Revert:     Compile liblouis v3.11.0 to wasm and update
    This reverts https://chromium-review.googlesource.com/c/chromium/src/+/1785998
    
    Reason for revert:
    Causes wasm errors/crashes when typing with a physical braille device e.g.
    "Error in liblouis Uncaught RuntimeError: memory access out of bounds"
    
    TBR=dmazzoni@chromium.org
        Bug: 985499,1033082
    
    Change-Id: I3e5aebf6e64f7b16bd84d7924ba1e8a88a04fcca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962730
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723924}

--- chrome/browser/resources/chromeos/accessibility/chromevox/braille/braille_translator_manager_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/braille/braille_translator_manager_test.js
@@ -112,7 +112,7 @@ TEST_F(
           assertEquals(
               'en-UEB-g2', this.manager.getDefaultTranslator().table.id);
           assertEquals(
-              'en-US-comp8', this.manager.getUncontractedTranslator().table.id);
+              'en-UEB-g1', this.manager.getUncontractedTranslator().table.id);
         });
         this.manager.refresh('en-UEB-g2');
       });
