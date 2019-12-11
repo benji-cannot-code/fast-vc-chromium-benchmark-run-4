4c2e1063b6f47055f43b2609bdc1ad23503f5c75
Jinsuk Kim <jinsukkim@chromium.org>
2019-12-11 21:37:24 +0000

    Android: Fix find-in-page regression
    https://crrev.com/c/1898156 landed while Tab interface CL
    https://crrev.com/c/1883270 was in review. It was not picked up when
    rebasing and landing the CL. This caused the find result toolbar to
    disappear, because |hideNativePage| was called even when native
    page is not showing. Hiding native page triggered Tab.onContentChanged
    to which FindToolBar responded by dismissing the result bar.
    
    Bug: 1032489
    Change-Id: I808779fe7a8c43e284542a90ec18175d4c3a1e1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959923
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723986}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
@@ -965,7 +965,7 @@ public class TabImpl implements Tab {
      */
     void showRenderedPage() {
         updateTitle();
-        hideNativePage(true, null);
+        if (mNativePage != null) hideNativePage(true, null);
     }
 
     void updateWindowAndroid(WindowAndroid windowAndroid) {
