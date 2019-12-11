a7e7446a8b5e0c6023e0362e47f1625324064d3d
Wei Li <weili@chromium.org>
2019-12-11 21:11:08 +0000

    [HaTS] Don't show HaTS when incognito mode disabled
    When incognito mode is disabled, creating an off-the-record profile will
    run into a check to make sure incognito mode is enabled. If not, it
    leads to immediate crash. Off-the-record profile is needed for showing
    the HaTS web dialog. So, this CL adds a check to not show HaTS bubble
    when incognito mode is disabled.
    
    BUG=1032459
    
    Change-Id: I834f776c1239c8f163d6e7607767ba59f5a1d1c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961212
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#723972}

--- chrome/browser/ui/hats/hats_service_browsertest.cc
+++ chrome/browser/ui/hats/hats_service_browsertest.cc
@@ -11,12 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
+#include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/browser/profiles/profile_impl.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/hats/hats_service.h"
 #include "chrome/browser/ui/hats/hats_service_factory.h"
 #include "chrome/browser/ui/views/hats/hats_bubble_view.h"
 #include "chrome/common/chrome_features.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/metrics_services_manager/metrics_services_manager.h"
 #include "components/version_info/version_info.h"
@@ -217,4 +219,17 @@ IN_PROC_BROWSER_TEST_F(HatsServiceProbabilityOne, ProfileOldEnoughToShow) {
                                   base::TimeDelta::FromDays(31));
   GetHatsService()->LaunchSatisfactionSurvey();
   EXPECT_TRUE(HatsBubbleShown());
+}
+
+IN_PROC_BROWSER_TEST_F(HatsServiceProbabilityOne, IncognitoModeDisabledNoShow) {
+  SetMetricsConsent(true);
+  // Disable incognito mode for this profile.
+  PrefService* pref_service = browser()->profile()->GetPrefs();
+  pref_service->SetInteger(prefs::kIncognitoModeAvailability,
+                           IncognitoModePrefs::DISABLED);
+  EXPECT_EQ(IncognitoModePrefs::DISABLED,
+            IncognitoModePrefs::GetAvailability(pref_service));
+
+  GetHatsService()->LaunchSatisfactionSurvey();
+  EXPECT_FALSE(HatsBubbleShown());
 }
