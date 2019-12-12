7ce6edab32909638c1475456c84a8c7640ab330a
Matthew Mourgos <mmourgos@chromium.org>
2019-12-12 01:39:56 +0000

    CrOS Shelf: Lower duration of the back button opacity animation
    Bug: 1026919
    Change-Id: I82369fadc2275323f750c163b739840a1c567b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960838
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724099}

--- ash/shelf/shelf_navigation_widget.cc
+++ ash/shelf/shelf_navigation_widget.cc
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
+// The duration of the back button opacity animation.
+constexpr base::TimeDelta kBackButtonOpacityAnimationDuration =
+    base::TimeDelta::FromMilliseconds(50);
+
 bool IsTabletMode() {
   return Shell::Get()->tablet_mode_controller() &&
          Shell::Get()->tablet_mode_controller()->InTabletMode();
@@ -336,7 +340,7 @@ void ShelfNavigationWidget::UpdateLayout() {
                                         : views::View::FocusBehavior::NEVER);
   ui::ScopedLayerAnimationSettings settings(
       GetBackButton()->layer()->GetAnimator());
-  settings.SetTransitionDuration(animation_duration);
+  settings.SetTransitionDuration(kBackButtonOpacityAnimationDuration);
   settings.SetPreemptionStrategy(
       ui::LayerAnimator::IMMEDIATELY_ANIMATE_TO_NEW_TARGET);
   settings.AddObserver(this);
