96ccb6a26e379aa41f78c73f4b4252716759601d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-12-12 16:20:54 +0000

    Import wpt@1db21a0158e71f849e151c4111a804ed15df855e
    Using wpt-import in Chromium dc5b525a06572d383086a041498e45f0bb92ffd7.
    With Chromium commits locally applied on WPT:
    a50e7e609f "Move `Sec-Fetch-User` to SetSecFetchMetadataHeaders."
    2bc8da8c39 "LazyLoad: move invisible-image.html test to WPT"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=smcgruer
    
    No-Export: true
    Change-Id: Iefb5de8e9e95c712beb09c075286b85cac003996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1964051
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#724200}

---
+++ third_party/blink/web_tests/external/wpt/fetch/api/basic/historical.any.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+test(() => {
+  assert_false("getAll" in new Headers());
+  assert_false("getAll" in Headers.prototype);
+}, "Headers object no longer has a getAll() method");
+
+test(() => {
+  assert_false("type" in new Request("about:blank"));
+  assert_false("type" in Request.prototype);
+}, "'type' getter should not exist on Request objects");
+
+// See https://github.com/whatwg/fetch/pull/979 for the removal
+test(() => {
+  assert_false("trailer" in new Response());
+  assert_false("trailer" in Response.prototype);
+}, "Response object no longer has a trailer getter");
