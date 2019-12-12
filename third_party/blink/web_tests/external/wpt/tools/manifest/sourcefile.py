44e310ff8403b380031f39dab746bfc8ff8e8ee6
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-12-12 19:17:36 +0000

    Import wpt@5b46ea7c4fe67113a69306afbf9c95b6ba041dbf
    Using wpt-import in Chromium 32e8a88366321562f622dacbd8ec22faed82bba7.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    bjonesbe@adobe.com:
      external/wpt/css/css-shapes
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    rego@igalia.com:
      external/wpt/css/css-contain
    rouslan@chromium.org, mathp@chromium.org:
      external/wpt/payment-request
    
    NOAUTOREVERT=true
    TBR=smcgruer
    
    No-Export: true
    Change-Id: I76031d101882506edc7ca5beeefff3c583651493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1964932
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#724292}

--- third_party/blink/web_tests/external/wpt/tools/manifest/sourcefile.py
+++ third_party/blink/web_tests/external/wpt/tools/manifest/sourcefile.py
@@ -35,7 +35,7 @@ import html5lib
 
 from . import XMLParser
 from .item import (ManifestItem, ManualTest, WebDriverSpecTest, RefTestNode, TestharnessTest,
-                   SupportFile, ConformanceCheckerTest, VisualTest)
+                   SupportFile, CrashTest, ConformanceCheckerTest, VisualTest)
 from .utils import ContextManagerBytesIO, cached_property
 
 wd_pattern = "*.py"
@@ -408,6 +408,11 @@ class SourceFile(object):
         """Check if the file name matches the conditions for the file to
         be a reference file (not a reftest)"""
         return "/reference/" in self.url or bool(reference_file_re.search(self.name))
+
+    @property
+    def name_is_crashtest(self):
+        # type: () -> bool
+        return self.type_flag == "crash" or "crashtests" in self.dir_path.split(os.path.sep)
 
     @property
     def markup_type(self):
@@ -815,6 +820,15 @@ class SourceFile(object):
         elif self.name_is_visual:
             rv = VisualTest.item_type, [
                 VisualTest(
+                    self.tests_root,
+                    self.rel_path,
+                    self.url_base,
+                    self.rel_url
+                )]
+
+        elif self.name_is_crashtest:
+            rv = CrashTest.item_type, [
+                CrashTest(
                     self.tests_root,
                     self.rel_path,
                     self.url_base,
--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/executors/base.py
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/executors/base.py
@@ -122,6 +122,9 @@ def pytest_result_converter(self, test, data):
     return (harness_result, subtest_results)
 
 
+def crashtest_result_converter(self, test, result):
+    return test.result_cls(**result), []
+
 class ExecutorException(Exception):
     def __init__(self, status, message):
         self.status = status
@@ -315,6 +318,10 @@ class RefTestExecutor(TestExecutor):
                               debug_info=debug_info)
 
         self.screenshot_cache = screenshot_cache
+
+
+class CrashtestExecutor(TestExecutor):
+    convert_result = crashtest_result_converter
 
 
 class RefTestImplementation(object):
@@ -574,6 +581,9 @@ class WdspecRun(object):
 
 
 class ConnectionlessBaseProtocolPart(BaseProtocolPart):
+    def load(self, url):
+        pass
+
     def execute_script(self, script, asynchronous=False):
         pass
 
