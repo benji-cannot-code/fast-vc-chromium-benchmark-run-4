4aab17181cf2aed711443a69626b36cdd50d5871
Julie Jeongeun Kim <jkim@igalia.com>
2019-12-12 01:30:14 +0000

    Use ExceptionState for immediate promise rejections in push_messaging
    This is a part of effort for using [RaisesException] when
    synchronously rejecting a promise.
    
    It uses ExceptionState instead of using
    ScriptPromise::RejectWithDOMException from
    //third_party/blink/renderer/modules/push_messaging.
    
    Bug: 1001114
    Change-Id: I869b7d465b9f0c2db70dc8a02171a04d8ec7950c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960086
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#724092}

--- third_party/blink/renderer/modules/push_messaging/push_manager.cc
+++ third_party/blink/renderer/modules/push_messaging/push_manager.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/push_messaging/push_subscription_options.h"
 #include "third_party/blink/renderer/modules/push_messaging/push_subscription_options_init.h"
 #include "third_party/blink/renderer/modules/service_worker/service_worker_registration.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
@@ -57,10 +58,10 @@ ScriptPromise PushManager::subscribe(
     const PushSubscriptionOptionsInit* options_init,
     ExceptionState& exception_state) {
   if (!registration_->active()) {
-    return ScriptPromise::RejectWithDOMException(
-        script_state, MakeGarbageCollected<DOMException>(
-                          DOMExceptionCode::kAbortError,
-                          "Subscription failed - no active Service Worker"));
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kAbortError,
+        "Subscription failed - no active Service Worker");
+    return ScriptPromise();
   }
 
   PushSubscriptionOptions* options =
@@ -88,10 +89,9 @@ ScriptPromise PushManager::subscribe(
           DynamicTo<Document>(ExecutionContext::From(script_state))) {
     LocalFrame* frame = document->GetFrame();
     if (!document->domWindow() || !frame) {
-      return ScriptPromise::RejectWithDOMException(
-          script_state, MakeGarbageCollected<DOMException>(
-                            DOMExceptionCode::kInvalidStateError,
-                            "Document is detached from window."));
+      exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
+                                        "Document is detached from window.");
+      return ScriptPromise();
     }
 
     PushMessagingClient* messaging_client = PushMessagingClient::From(frame);
@@ -127,10 +127,9 @@ ScriptPromise PushManager::permissionState(
   if (auto* document =
           DynamicTo<Document>(ExecutionContext::From(script_state))) {
     if (!document->domWindow() || !document->GetFrame()) {
-      return ScriptPromise::RejectWithDOMException(
-          script_state, MakeGarbageCollected<DOMException>(
-                            DOMExceptionCode::kInvalidStateError,
-                            "Document is detached from window."));
+      exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
+                                        "Document is detached from window.");
+      return ScriptPromise();
     }
   }
 
