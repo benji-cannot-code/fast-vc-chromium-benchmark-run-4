d745ee0b8f50871abd9281582927b19ae378df4b
Carlos Caballero <carlscab@google.com>
2019-12-12 19:18:18 +0000

    Remove MessageLoop references in /third_party/blink/public/platform/scheduler
    MessageLoop is going away soon.
    
    This CL was uploaded by git cl split.
    
    R=rmcilroy@chromium.org
    
    Bug: 891670
    Change-Id: Iac7c0f04449c36d45eb416bd532af22cae432584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946232
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#724293}

--- third_party/blink/public/platform/scheduler/test/web_fake_thread_scheduler.h
+++ third_party/blink/public/platform/scheduler/test/web_fake_thread_scheduler.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_SCHEDULER_TEST_WEB_FAKE_THREAD_SCHEDULER_H_
 
 #include "base/macros.h"
-#include "base/message_loop/message_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "build/build_config.h"
 #include "third_party/blink/public/platform/scheduler/web_thread_scheduler.h"
