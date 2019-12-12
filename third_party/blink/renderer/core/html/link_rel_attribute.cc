623007926e7e86b75a5bf24526092d3239bf0919
Frédéric Wang <fwang@igalia.com>
2019-12-12 22:34:01 +0000

    Add counters for Web Monetization
    This patch adds two counters for two tags currently investigated by the
    Web Monetization people [1] in order to help them analyzing current
    usage on web sites:
    - Use of <meta name="monetization"> [2]
    - Use of <link rel="monetization"> [3]
    
    [1] https://github.com/interledger/webmonetization.org/#webmonetizationorg
    [2] https://webmonetization.org/specification.html#meta-tags-set
    [3] https://github.com/interledger/webmonetization.org/issues/19
    
    Bug: 1031476
    Change-Id: I0568bd55ea051a432a97277a3ba5a5b84e951984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954468
    Commit-Queue: Frédéric Wang <fwang@igalia.com>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724368}

--- third_party/blink/renderer/core/html/link_rel_attribute.cc
+++ third_party/blink/renderer/core/html/link_rel_attribute.cc
@@ -50,7 +50,8 @@ LinkRelAttribute::LinkRelAttribute()
       is_manifest_(false),
       is_module_preload_(false),
       is_service_worker_(false),
-      is_canonical_(false) {}
+      is_canonical_(false),
+      is_monetization_(false) {}
 
 LinkRelAttribute::LinkRelAttribute(const String& rel) : LinkRelAttribute() {
   if (rel.IsEmpty())
@@ -100,6 +101,8 @@ LinkRelAttribute::LinkRelAttribute(const String& rel) : LinkRelAttribute() {
       is_service_worker_ = true;
     } else if (DeprecatedEqualIgnoringCase(link_type, "canonical")) {
       is_canonical_ = true;
+    } else if (DeprecatedEqualIgnoringCase(link_type, "monetization")) {
+      is_monetization_ = true;
     }
     // Adding or removing a value here requires you to update
     // RelList::supportedTokens()
