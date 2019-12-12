6712d074a3dce51358f6072eda86f0dd4cbb454d
Jiawei Shao <jiawei.shao@intel.com>
2019-12-12 00:18:29 +0000

    Reland "WebGPU: Create GPUDevice in the callback of RequestDeviceAsync()"
    This reverts commit 7dd5390925a522b58d40d157e04e79541534babd.
    
    Reason for revert: this CL is not the root cause of the try bot failures.
    
    Original change's description:
    > Revert "WebGPU: Create GPUDevice in the callback of RequestDeviceAsync()"
    > 
    > This reverts commit 2625327a72b4e1c04538912e43ac51033ac35569.
    > 
    > Reason for revert: This is likely breaking https://ci.chromium.org/p/chromium/builders/ci/Mac%20Retina%20Debug%20%28AMD%29
    > 
    > Original change's description:
    > > WebGPU: Create GPUDevice in the callback of RequestDeviceAsync()
    > > 
    > > This patch updates RequestDevice() into an asynchronous function
    > > RequestDeviceAsync() and moves the creation of GPUDevice into the
    > > callback function passed as a parameter of RequestDeviceAsync().
    > > 
    > > BUG=chromium:996713
    > > 
    > > Change-Id: I7208a107670ca6e79ad3a4a3107f35ae26330edf
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1955171
    > > Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    > > Reviewed-by: Austin Eng <enga@chromium.org>
    > > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#723595}
    > 
    > TBR=zmo@chromium.org,kbr@chromium.org,cwallez@chromium.org,shaobo.yan@intel.com,yunchao.he@intel.com,jiawei.shao@intel.com,yang.gu@intel.com,kainino@chromium.org,jiajie.hu@intel.com,enga@chromium.org,hao.x.li@intel.com
    > 
    > Change-Id: I7f78422e0c841a17692a7030690fe911202aebeb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: chromium:996713
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961353
    > Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    > Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#723712}
    
    TBR=zmo@chromium.org,kbr@chromium.org,cwallez@chromium.org,shaobo.yan@intel.com,yunchao.he@intel.com,jiawei.shao@intel.com,yang.gu@intel.com,kainino@chromium.org,jiajie.hu@intel.com,enga@chromium.org,hao.x.li@intel.com,melzhang@chromium.org
    
    Change-Id: I5fb3d180e89a88803e93668b57dc5e5b3ea5f481
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:996713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1963373
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Cr-Commit-Position: refs/heads/master@{#724049}

--- gpu/command_buffer/tests/webgpu_mailbox_unittest.cc
+++ gpu/command_buffer/tests/webgpu_mailbox_unittest.cc
@@ -92,7 +92,8 @@ TEST_F(WebGPUMailboxTest, WriteToMailboxThenReadFromIt) {
   webgpu()->WaitSyncTokenCHROMIUM(mailbox_produced_token.GetConstData());
 
   constexpr uint32_t kAdapterID = 0;
-  webgpu()->RequestDevice(kAdapterID, nullptr);
+  webgpu()->RequestDeviceAsync(kAdapterID, nullptr,
+                               base::BindOnce(&OnRequestDeviceCallback));
   wgpu::Device device = wgpu::Device::Acquire(webgpu()->GetDefaultDevice());
 
   // Part 1: Write to the texture using Dawn
@@ -212,7 +213,8 @@ TEST_F(WebGPUMailboxTest, ErrorWhenUsingTextureAfterDissociate) {
 
   // Create the device, and expect a validation error.
   constexpr uint32_t kAdapterID = 0;
-  webgpu()->RequestDevice(kAdapterID, nullptr);
+  webgpu()->RequestDeviceAsync(kAdapterID, nullptr,
+                               base::BindOnce(&OnRequestDeviceCallback));
   wgpu::Device device = wgpu::Device::Acquire(webgpu()->GetDefaultDevice());
   device.SetUncapturedErrorCallback(ToMockUncapturedErrorCallback, 0);
 
