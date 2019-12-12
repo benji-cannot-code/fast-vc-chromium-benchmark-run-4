eefde05093ca431af7b3e48e0f5a7ce8f13b9622
Yi Su <mrsuyi@chromium.org>
2019-12-12 23:06:23 +0000

    Remove unused property of CRWJSNavigationHandler
    CRWJSNavigationHandler.dispatchingSameDocumentHashChangeEvent can be
    removed since it's never set to YES.
    
    Bug: 1029306
    Change-Id: Ifc4762262390fc17e2fff5a298e9cd43ed347c62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1963859
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Yi Su <mrsuyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724385}

--- ios/web/navigation/crw_js_navigation_handler.h
+++ ios/web/navigation/crw_js_navigation_handler.h
@@ -34,10 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithDelegate:(id<CRWJSNavigationHandlerDelegate>)delegate
     NS_DESIGNATED_INITIALIZER;
 
-// Set to YES when a hashchange event is manually dispatched for same-document
-// history navigations.
-@property(nonatomic, assign) BOOL dispatchingSameDocumentHashChangeEvent;
-
 // Whether the web page is currently performing window.history.pushState or
 // window.history.replaceState.
 @property(nonatomic, assign) BOOL changingHistoryState;
