8d5af4685e0f3244196e168b7e61adea6896fe85
Melissa Zhang <melzhang@chromium.org>
2019-12-12 03:48:40 +0000

    Revert "overview gesture: Always restore window backdrop mode after drag ends."
    This reverts commit 5548de3559b1f5212843df960a6c61849d1bfcf5.
    
    Reason for revert: potentially causing https://ci.chromium.org/p/chromium/builders/ci/Mac%20ASan%2064%20Tests%20%281%29/59219 and https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/16748
    
    Original change's description:
    > overview gesture: Always restore window backdrop mode after drag ends.
    > 
    > Previously the window's original backdrop mode is not reset back to its
    > original backdrop mode if the window ends in overview or splitview. This
    > CL fixes that.
    > 
    > Bug: 1030857, 997885
    > Change-Id: I7f0bbc24ca8257ed1bfeb8cb41517268b6f2c2c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1963109
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#724047}
    
    TBR=xiyuan@chromium.org,xdai@chromium.org
    
    Change-Id: I739a01f5ef7fd00f3be890e66cbe6857c7c683bc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1030857, 997885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961877
    Reviewed-by: Melissa Zhang <melzhang@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724136}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -215,8 +215,7 @@ base::Optional<InAppShelfGestures> CalculateHotseatGestureToRecord(
     HotseatState old_state,
     HotseatState current_state) {
   if (window_drag_result.has_value() &&
-      (window_drag_result == ShelfWindowDragResult::kGoToOverviewMode ||
-       window_drag_result == ShelfWindowDragResult::kGoToSplitviewMode) &&
+      window_drag_result == ShelfWindowDragResult::kGoToOverviewMode &&
       old_state == HotseatState::kHidden) {
     return InAppShelfGestures::kSwipeUpToShow;
   }
