7dafb9448f9d273b1055b2dfdd4f2b78c9885dae
Jack Lynch <jalyn@microsoft.com>
2019-12-12 23:48:16 +0000

    DevTools: Add axe accessibility test for event listener breakpoints pane
    This adds an axe tests for the event listener breakpoints pane in the
    Sources panel sidebar.
    
    Bug: 963183
    Change-Id: Ife83c6f707d84c243671cdcd32d04679944d5047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1656209
    Commit-Queue: Jack Lynch <jalyn@microsoft.com>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724407}

---
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/sources/event-listener-breakpoints-a11y-test.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function () {
+  await TestRunner.loadModule('axe_core_test_runner');
+  await TestRunner.showPanel('sources');
+
+  await UI.viewManager.showView('sources.eventListenerBreakpoints');
+  const eventListenerWidget = self.runtime.sharedInstance(
+      BrowserDebugger.EventListenerBreakpointsSidebarPane);
+  TestRunner.addResult('Setting event listener breakpoints.');
+  const {checkbox, element} = eventListenerWidget._categories.get('Animation');
+  element.revealAndSelect();
+  checkbox.click();
+
+  TestRunner.addResult('Dumping Animation category.');
+  TestRunner.addResult(element.listItemElement.deepTextContent());
+  TestRunner.addResult('Running the axe-core linter on the Animation category of the event listener breakpoints pane.');
+  await AxeCoreTestRunner.runValidation(element.listItemElement);
+
+  TestRunner.completeTest();
+})();
