62ad9ca77fc5f5f0667b15b9c04396b0fef04a2a
Jing Wang <jiwan@chromium.org>
2019-12-12 23:30:21 +0000

    Use DCHECK instead of CHECK in input ime APIs
    R=shend
    
    Bug: 1031350
    Change-Id: I49bd8fa745821e4a009ab2523f8bc4c9629978a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953235
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724399}

--- chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.cc
@@ -388,7 +388,7 @@ InputMethodEngine* GetEngineIfActive(Profile* profile,
                                      const std::string& extension_id,
                                      std::string* error) {
   InputImeEventRouter* event_router = GetInputImeEventRouter(profile);
-  CHECK(event_router) << kErrorRouterNotAvailable;
+  DCHECK(event_router) << kErrorRouterNotAvailable;
   InputMethodEngine* engine = static_cast<InputMethodEngine*>(
       event_router->GetEngineIfActive(extension_id, error));
   return engine;
@@ -399,7 +399,7 @@ InputMethodEngine* GetEngine(content::BrowserContext* browser_context,
                              std::string* error) {
   Profile* profile = Profile::FromBrowserContext(browser_context);
   InputImeEventRouter* event_router = GetInputImeEventRouter(profile);
-  CHECK(event_router) << kErrorRouterNotAvailable;
+  DCHECK(event_router) << kErrorRouterNotAvailable;
   InputMethodEngine* engine =
       static_cast<InputMethodEngine*>(event_router->GetEngine(extension_id));
   DCHECK(engine) << kErrorEngineNotAvailable;
