393ed4cab50499ca93e32094b97cba4aec548527
Andrii Sagaidak <ansagaid@microsoft.com>
2019-12-12 03:43:39 +0000

    Adding support for multiple public keys in Origin Trials framework
    Adding support for Origin Trial tokens to be validated by multiple public keys.
    The command line switch "origin-trial-public-key" which currently takes one argument
    and overrides the default public key, will now take a list of public keys instead. This
    will allow adding a test key along with the default public key.
    
    *Testing*: Unit Tested, Manually verified.
    
    Bug: 584737
    Change-Id: I53960543b138794f661aa6fbae61f5777e81ca49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947046
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Andrii Sagaidak <ansagaid@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#724134}

--- content/shell/common/shell_origin_trial_policy.cc
+++ content/shell/common/shell_origin_trial_policy.cc
@@ -26,10 +26,11 @@ static const uint8_t kOriginTrialPublicKey[] = {
 
 }  // namespace
 
-ShellOriginTrialPolicy::ShellOriginTrialPolicy()
-    : public_key_(base::StringPiece(
-          reinterpret_cast<const char*>(kOriginTrialPublicKey),
-          base::size(kOriginTrialPublicKey))) {}
+ShellOriginTrialPolicy::ShellOriginTrialPolicy() {
+  public_keys_.push_back(base::StringPiece(
+      reinterpret_cast<const char*>(kOriginTrialPublicKey),
+      base::size(kOriginTrialPublicKey)));
+}
 
 ShellOriginTrialPolicy::~ShellOriginTrialPolicy() {}
 
@@ -37,8 +38,8 @@ bool ShellOriginTrialPolicy::IsOriginTrialsSupported() const {
   return true;
 }
 
-base::StringPiece ShellOriginTrialPolicy::GetPublicKey() const {
-  return public_key_;
+std::vector<base::StringPiece> ShellOriginTrialPolicy::GetPublicKeys() const {
+  return public_keys_;
 }
 
 bool ShellOriginTrialPolicy::IsOriginSecure(const GURL& url) const {
