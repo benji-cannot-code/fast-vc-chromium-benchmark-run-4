042c6adeea5815f97d3e51aff704df8caf9ff885
Rahul Arakeri <arakeri@microsoft.com>
2019-12-12 20:32:52 +0000

    Fix for scroll_latency_browsertest
    Native scrollbars on Mac are overlay by default. This is a  problem to
    the tests that rely on scrollbar manipulation . This CL fixes the issue
    with scroll_latency_browsertest by ensuring that the scrollbars on Mac
    remain solid for the duration of the test.
    
    Bug: 1026720
    Change-Id: I01c986e735cdffe64bc749a9689876a43a85c74a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962771
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724315}

--- content/browser/renderer_host/input/scroll_latency_browsertest.cc
+++ content/browser/renderer_host/input/scroll_latency_browsertest.cc
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_features.h"
 #include "ui/native_theme/native_theme_features.h"
 
+#if defined(OS_MACOSX)
+#include "ui/base/test/scoped_preferred_scroller_style_mac.h"
+#endif
+
 namespace {
 
 const char kDataURL[] =
@@ -276,6 +280,13 @@ class ScrollLatencyScrollbarBrowserTest : public ScrollLatencyBrowserTest {
 
   ~ScrollLatencyScrollbarBrowserTest() override {}
 
+ private:
+#if defined(OS_MACOSX)
+  // Native scrollbars on Mac are overlay scrollbars. Hence they need to be
+  // disabled.
+  ui::test::ScopedPreferredScrollerStyle scroller_style_override{false};
+#endif
+
  protected:
   void RunScrollbarButtonLatencyTest() {
     // We don't run tests that click the scrollbar on Android for a few reasons:
@@ -289,7 +300,6 @@ class ScrollLatencyScrollbarBrowserTest : public ScrollLatencyBrowserTest {
     // WebPreferences) but at that point, we're not really testing a shipping
     // configuration.
 #if !defined(OS_ANDROID)
-
     // Click on the forward scrollbar button to induce a compositor thread
     // scrollbar scroll.
     blink::WebFloatPoint scrollbar_forward_button(795, 595);
@@ -433,9 +443,8 @@ IN_PROC_BROWSER_TEST_F(ScrollLatencyScrollbarBrowserTest,
   RunScrollbarButtonLatencyTest();
 }
 
-// Disabled due to test failures (crbug.com/1026720).
 IN_PROC_BROWSER_TEST_F(ScrollLatencyScrollbarBrowserTest,
-                       DISABLED_ScrollbarThumbDragLatency) {
+                       ScrollbarThumbDragLatency) {
   LoadURL();
 
   RunScrollbarThumbDragLatencyTest();
@@ -461,7 +470,7 @@ class ScrollLatencyCompositedScrollbarBrowserTest
 };
 
 IN_PROC_BROWSER_TEST_F(ScrollLatencyCompositedScrollbarBrowserTest,
-                       DISABLED_ScrollbarButtonLatency) {
+                       ScrollbarButtonLatency) {
   LoadURL();
 
   RunScrollbarButtonLatencyTest();
