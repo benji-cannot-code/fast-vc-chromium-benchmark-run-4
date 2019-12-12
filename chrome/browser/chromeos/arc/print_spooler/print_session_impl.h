be2f1fee95cf04ec93e1424fd23b69bc0f8847d8
Julie Jeongeun Kim <jkim@igalia.com>
2019-12-12 06:10:34 +0000

    Convert arc::mojom::PrintSessionHost to new Mojo types
    This CL partially converts arc::mojom::PrintSessionHost to new Mojo
    types using PendingRemote, PendingReceiver and Receiver. It still keeps
    StartPrintInCustomTab callback from
    //components/arc/mojom/print_spooler.mojom with InterfacePtr since arc
    mojom files are shared with other repo and it doesn't have new Mojo
    types yet.
    
    Bug: 955171
    Change-Id: I77f7a0296294fc5ab96f770f6b2c419e33108e21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959985
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#724148}

--- chrome/browser/chromeos/arc/print_spooler/print_session_impl.h
+++ chrome/browser/chromeos/arc/print_spooler/print_session_impl.h
@@ -18,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/printing/common/print.mojom.h"
 #include "content/public/browser/web_contents_user_data.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 
 namespace ash {
@@ -38,7 +40,7 @@ class PrintSessionImpl : public mojom::PrintSessionHost,
                          public content::WebContentsUserData<PrintSessionImpl>,
                          public printing::mojom::PrintRenderer {
  public:
-  static mojom::PrintSessionHostPtr Create(
+  static mojo::PendingRemote<mojom::PrintSessionHost> Create(
       std::unique_ptr<content::WebContents> web_contents,
       std::unique_ptr<ash::ArcCustomTab> custom_tab,
       mojom::PrintSessionInstancePtr instance);
@@ -52,7 +54,7 @@ class PrintSessionImpl : public mojom::PrintSessionHost,
   PrintSessionImpl(std::unique_ptr<content::WebContents> web_contents,
                    std::unique_ptr<ash::ArcCustomTab> custom_tab,
                    mojom::PrintSessionInstancePtr instance,
-                   mojom::PrintSessionHostRequest request);
+                   mojo::PendingReceiver<mojom::PrintSessionHost> receiver);
   friend class content::WebContentsUserData<PrintSessionImpl>;
 
   // printing::mojom::PrintRenderer:
@@ -89,7 +91,7 @@ class PrintSessionImpl : public mojom::PrintSessionHost,
 
   // Used to bind the PrintSessionHost interface implementation to a message
   // pipe.
-  mojo::Binding<mojom::PrintSessionHost> session_binding_;
+  mojo::Receiver<mojom::PrintSessionHost> session_receiver_;
 
   // Remote interface used to flatten a PDF (preview document).
   mojo::Remote<printing::mojom::PdfFlattener> pdf_flattener_;
