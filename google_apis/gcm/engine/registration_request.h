9d6cadde04af23dab6f3e1840b46369e13df0402
Josh Nohle <nohle@google.com>
2019-12-12 20:35:40 +0000

    Increase log severity levels for GCM registration failures
    Because some GCM registration failures are quietly retried instead of
    invoking the registration callback, the app requesting registration has
    no way of knowing whether a failure occurred. By increasing the log
    severity levels, we can at least identify GCM registration failures and
    the reason for callback delays in logs.
    
    Bug: 1032362
    Change-Id: Idc9de4174b030e9c970dee45ee8a17cc0a732f37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962979
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724318}

--- google_apis/gcm/engine/registration_request.h
+++ google_apis/gcm/engine/registration_request.h
@@ -23,12 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 class HttpRequestHeaders;
-}
+}  // namespace net
 
 namespace network {
 class SharedURLLoaderFactory;
 class SimpleURLLoader;
-}
+}  // namespace network
 
 namespace gcm {
 
@@ -37,7 +37,7 @@ class GCMStatsRecorder;
 // Registration request is used to obtain registration IDs for applications that
 // want to use GCM. It requires a set of parameters to be specified to identify
 // the Chrome instance, the user, the application and a set of senders that will
-// be authorized to address the application using it's assigned registration ID.
+// be authorized to address the application using its assigned registration ID.
 class GCM_EXPORT RegistrationRequest {
  public:
   // This enum is also used in an UMA histogram (GCMRegistrationRequestStatus
