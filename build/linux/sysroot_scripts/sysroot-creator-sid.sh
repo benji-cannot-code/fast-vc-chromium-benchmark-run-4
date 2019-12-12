68d2a3f6c33b8e3774d8c909f3adb0ab4dc55b18
Tom Anderson <thomasanderson@chromium.org>
2019-12-12 20:01:41 +0000

    Add libinput-dev and libevdev-dev to sysroots
    This is needed for building weston so we can run tests with it.
    
    R=msisov
    
    Bug: 1033602
    Change-Id: I018bbcc1dccb1adc241d45f07cf62a5fde7b65e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965973
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724305}

--- build/linux/sysroot_scripts/sysroot-creator-sid.sh
+++ build/linux/sysroot_scripts/sysroot-creator-sid.sh
@@ -20,7 +20,7 @@ DIST=sid
 # from sid.  It will be needed until gnome keyring is removed
 # (http://crbug.com/466975 and http://crbug.com/355223).
 ARCHIVE_URL="https://snapshot.debian.org/archive/debian"
-ARCHIVE_TIMESTAMP=20191205T152154Z
+ARCHIVE_TIMESTAMP=20191212T145612Z
 APT_SOURCES_LIST="${ARCHIVE_URL}/${ARCHIVE_TIMESTAMP}/ stretch main
 ${ARCHIVE_URL}/${ARCHIVE_TIMESTAMP}/ experimental main
 ${ARCHIVE_URL}/${ARCHIVE_TIMESTAMP}/ sid main"
@@ -104,6 +104,8 @@ DEBIAN_PACKAGES="\
   libelf1
   libepoxy-dev
   libepoxy0
+  libevdev-dev
+  libevdev2
   libevent-2.1-7
   libexpat1
   libexpat1-dev
@@ -159,6 +161,7 @@ DEBIAN_PACKAGES="\
   libgtk-4-0
   libgtk-4-dev
   libgtk2.0-0
+  libgudev-1.0-0
   libharfbuzz-dev
   libharfbuzz-gobject0
   libharfbuzz-icu0
@@ -172,6 +175,8 @@ DEBIAN_PACKAGES="\
   libidn2-0
   libindicator3-7
   libindicator7
+  libinput10
+  libinput-dev
   libjbig0
   libjpeg62-turbo
   libjpeg62-turbo-dev
@@ -195,6 +200,7 @@ DEBIAN_PACKAGES="\
   libminizip1
   libmount-dev
   libmount1
+  libmtdev1
   libnettle7
   libnspr4
   libnspr4-dev
@@ -224,6 +230,7 @@ DEBIAN_PACKAGES="\
   libpcre2-8-0
   libpcre2-dev
   libpcre2-posix0
+  libpcre2-posix2
   libpcre3
   libpcre3-dev
   libpcre32-3
@@ -280,6 +287,7 @@ DEBIAN_PACKAGES="\
   libvpx-dev
   libvpx6
   libvulkan1
+  libwacom2
   libwayland-client0
   libwayland-cursor0
   libwayland-dev
