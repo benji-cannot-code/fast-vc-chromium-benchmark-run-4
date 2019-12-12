6531b197f2c8ea2dd1973c65af64e67bd1f96c79
gogerald <gogerald@google.com>
2019-12-12 20:14:04 +0000

    [StartSurface] Display incognito description and learn more option
    Screenshot:
    https://drive.google.com/file/d/1lsuhY1tP7URrx_4ZXMaJrmuFPKajss3S/view?usp=sharing
    
    Bug: 1025296
    Change-Id: I93540227eab5aa690eee9fd4c270982569916b76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924692
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Auto-Submit: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724309}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/TasksSurfaceMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/TasksSurfaceMediator.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.tasks;
 
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.FAKE_SEARCH_BOX_CLICK_LISTENER;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.FAKE_SEARCH_BOX_TEXT_WATCHER;
+import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.INCOGNITO_LEARN_MORE_CLICK_LISTENER;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_FAKE_SEARCH_BOX_VISIBLE;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_TAB_CAROUSEL_VISIBLE;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_VOICE_RECOGNITION_BUTTON_VISIBLE;
@@ -31,8 +32,8 @@ class TasksSurfaceMediator {
     private final FakeboxDelegate mFakeboxDelegate;
     private final PropertyModel mModel;
 
-    TasksSurfaceMediator(
-            PropertyModel model, FakeboxDelegate fakeboxDelegate, boolean isTabCarousel) {
+    TasksSurfaceMediator(PropertyModel model, FakeboxDelegate fakeboxDelegate,
+            View.OnClickListener incognitoLearnMoreClickListener, boolean isTabCarousel) {
         mFakeboxDelegate = fakeboxDelegate;
         assert mFakeboxDelegate != null;
 
@@ -72,6 +73,7 @@ class TasksSurfaceMediator {
                 RecordUserAction.record("TasksSurface.FakeBox.VoiceSearch");
             }
         });
+        model.set(INCOGNITO_LEARN_MORE_CLICK_LISTENER, incognitoLearnMoreClickListener);
 
         // Set the initial state.
         mModel.set(IS_FAKE_SEARCH_BOX_VISIBLE, true);
