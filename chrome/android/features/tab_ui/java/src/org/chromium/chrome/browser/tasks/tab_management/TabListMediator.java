f2f1710bf5fff5fa7f343cfe5c59e3aa85e5e381
Mei Liang <meiliang@chromium.org>
2019-12-12 22:58:06 +0000

    Remove TabObserver after tab is closed
    The crash happens when an undo-able closed tab updates. In other words, tab
    updates before the tab closure is committed, but the TabListMediator and
    TabGroupModelFilter have already updated their internal models to reflect the
    temporary closure. They are operating on a tab that implicitly exists in
    TabModel, but no longer exists in their internal models. The simple fix is to
    remove the TabObserver in TabListMediator when the tab starts closing.
    
    Change-Id: I2b20227609c6f0f3230d186dc3b6d4a321863baf
    Bug: 1027419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1937139
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724381}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -522,6 +522,7 @@ class TabListMediator {
             @Override
             public void willCloseTab(Tab tab, boolean animate) {
                 if (mModel.indexFromId(tab.getId()) == TabModel.INVALID_TAB_INDEX) return;
+                tab.removeObserver(mTabObserver);
                 mModel.removeAt(mModel.indexFromId(tab.getId()));
             }
 
@@ -785,6 +786,7 @@ class TabListMediator {
         sTabClosedFromMapTabClosedFromMap.put(tabId, TabClosedFrom.GRID_TAB_SWITCHER_GROUP);
     }
 
+    @VisibleForTesting
     void setActionOnAllRelatedTabsForTesting(boolean actionOnAllRelatedTabs) {
         mActionsOnAllRelatedTabs = actionOnAllRelatedTabs;
     }
