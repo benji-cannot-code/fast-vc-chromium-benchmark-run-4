7c51ca04975a9a8a4e13cab2ffa1e7efa4073dda
David Black <dmblack@google.com>
2019-12-12 02:10:06 +0000

    Enable all Italian locales.
    Bug: b:144802324
    Change-Id: I527e0da6a5cd1e890ec27cdfd9376e1b2037d038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1963431
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#724109}

--- chrome/browser/chromeos/assistant/assistant_util.cc
+++ chrome/browser/chromeos/assistant/assistant_util.cc
@@ -68,6 +68,7 @@ ash::mojom::AssistantAllowedState IsAssistantAllowedForProfile(
                                          "es_ES",
                                          "es_MX",
                                          "fr_BE",
+                                         "it",
                                          "nb",
                                          "nl",
                                          "nn",
