629b56b6f687dce8e2bdf20542b51e4891efc9cc
Nate Fischer <ntfschr@chromium.org>
2019-12-12 04:34:09 +0000

    AW UMA: add DriveMetricsProvider
    This adds DriveMetricsProvider to WebView's list of MetricsProviders. At
    the moment, this is hardcoded to log `has_seek_penalty == false`, but
    this registers the provider anyway for the sake of consistency with
    Chrome, and in case the implementation changes in the future to log
    other values.
    
    Bug: 1008534
    Test: run_webview_instrumentation_test_apk -f AwMetricsIntegrationTest#*
    Change-Id: I8e9b0c91bab1dffa5b6c13ccc323ae7bfe46eef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960476
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724142}

--- android_webview/browser/metrics/aw_metrics_service_client.cc
+++ android_webview/browser/metrics/aw_metrics_service_client.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
+#include "base/base_paths_android.h"
 #include "base/feature_list.h"
 #include "base/hash/hash.h"
 #include "base/i18n/rtl.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/android_metrics_provider.h"
 #include "components/metrics/call_stack_profile_metrics_provider.h"
 #include "components/metrics/cpu_metrics_provider.h"
+#include "components/metrics/drive_metrics_provider.h"
 #include "components/metrics/enabled_state_provider.h"
 #include "components/metrics/gpu/gpu_metrics_provider.h"
 #include "components/metrics/metrics_log_uploader.h"
@@ -127,6 +129,9 @@ std::unique_ptr<metrics::MetricsService> CreateMetricsService(
       std::make_unique<metrics::CPUMetricsProvider>());
   service->RegisterMetricsProvider(
       std::make_unique<metrics::GPUMetricsProvider>());
+  service->RegisterMetricsProvider(
+      std::make_unique<metrics::DriveMetricsProvider>(
+          base::DIR_ANDROID_APP_DATA));
   service->RegisterMetricsProvider(
       std::make_unique<metrics::ScreenInfoMetricsProvider>());
   service->RegisterMetricsProvider(
