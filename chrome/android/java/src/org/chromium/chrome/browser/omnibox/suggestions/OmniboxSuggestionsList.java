1f81c5342b228fd20514e671eb3da4c6afc100b0
Evan Stade <estade@chromium.org>
2019-12-12 01:57:14 +0000

    Move chrome.browser.ui.styles to components.browser_ui.styles
    These common style values are needed by various bits of Android UI,
    including chrome.browser.ui.widget and chrome.browser.modaldialogs.
    
    Bug: 1030785
    Change-Id: If02230ea364a8f7a84f49ac6abdd778368d38e9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960809
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724105}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxSuggestionsList.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxSuggestionsList.java
@@ -21,9 +21,9 @@ import android.widget.ListView;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.ui.styles.ChromeColors;
 import org.chromium.chrome.browser.util.KeyNavigationUtil;
 import org.chromium.chrome.browser.util.ViewUtils;
+import org.chromium.components.browser_ui.styles.ChromeColors;
 
 import java.util.ArrayList;
 
