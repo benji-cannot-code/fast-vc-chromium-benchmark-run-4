48a364275e3a96a91bd3b6d5c0b2c0b38b718f7b
Alexandre Courbot <acourbot@chromium.org>
2019-12-12 11:11:15 +0000

    media/gpu/v4l2: fix path of libv4l2 for 64-bit ARM
    On 64-bit systems, the V4L2 libraries are in /usr/lib64, not /usr/lib.
    Turns out we don't even need to specify the full path of the libraries
    as dlopen() will lookup the right path for us.
    
    Since the path is only used in PostSandboxInitialization(), also move it
    there.
    
    BUG=1029396
    TEST=VDAtest passes on kevin64.
    
    Change-Id: I37215e31d6cbc15d46aeb4d62069bbc9f6028a51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961873
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724175}

--- media/gpu/v4l2/generic_v4l2_device.cc
+++ media/gpu/v4l2/generic_v4l2_device.cc
@@ -45,9 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using media_gpu_v4l2::kModuleV4l2;
 using media_gpu_v4l2::InitializeStubs;
 using media_gpu_v4l2::StubPathMap;
-
-static const base::FilePath::CharType kV4l2Lib[] =
-    FILE_PATH_LITERAL("/usr/lib/libv4l2.so");
 #endif
 
 namespace media {
@@ -511,6 +508,9 @@ void GenericV4L2Device::CloseDevice() {
 // static
 bool GenericV4L2Device::PostSandboxInitialization() {
 #if BUILDFLAG(USE_LIBV4L2)
+  static const base::FilePath::CharType kV4l2Lib[] =
+    FILE_PATH_LITERAL("libv4l2.so");
+
   StubPathMap paths;
   paths[kModuleV4l2].push_back(kV4l2Lib);
 
