821a23fb27ef63a99626bd30fc3872a8e95993bb
Aaron Colwell <acolwell@chromium.org>
2019-12-12 17:12:12 +0000

    Reduce site URL usage in extension related code.
    Removing references to site URLs in extension related code that doesn't
    really need to know about this implementation detail. No behavior has
    changed.
    
    - Replaced many (GetSiteForExtensionId(), GetStoragePartitionForSite()) combos
      with a single GetStoragePartitionForExtensionId() call.
    - Added extensions::util::HasIsolatedStorage() to hide site URL details
      from PolicyCertService.
    
    Bug: none
    Change-Id: I67e155f6546b97c1d18c1aa8f5f4ee08beecedb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1947384
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Aaron Colwell <acolwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724228}

--- chrome/browser/extensions/api/file_system/chrome_file_system_delegate.cc
+++ chrome/browser/extensions/api/file_system/chrome_file_system_delegate.cc
@@ -141,9 +141,8 @@ void OnConsentReceived(
     return;
   }
 
-  const GURL site = util::GetSiteForExtensionId(extension_id, browser_context);
   scoped_refptr<storage::FileSystemContext> file_system_context =
-      content::BrowserContext::GetStoragePartitionForSite(browser_context, site)
+      util::GetStoragePartitionForExtensionId(extension_id, browser_context)
           ->GetFileSystemContext();
   storage::ExternalFileSystemBackend* const backend =
       file_system_context->external_backend();
@@ -366,10 +365,8 @@ void ChromeFileSystemDelegate::RequestFileSystem(
     return;
   }
 
-  const GURL site =
-      util::GetSiteForExtensionId(extension.id(), browser_context);
   scoped_refptr<storage::FileSystemContext> file_system_context =
-      content::BrowserContext::GetStoragePartitionForSite(browser_context, site)
+      util::GetStoragePartitionForExtensionId(extension.id(), browser_context)
           ->GetFileSystemContext();
   storage::ExternalFileSystemBackend* const backend =
       file_system_context->external_backend();
