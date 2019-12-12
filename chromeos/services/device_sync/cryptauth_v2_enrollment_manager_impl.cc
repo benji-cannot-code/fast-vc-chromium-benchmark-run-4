e81b2d99fd741717ba3b87b46bc8265316d669f8
Josh Nohle <nohle@google.com>
2019-12-12 22:21:30 +0000

    [Enrollment v2] Remove GCM registration timeout
    Remove the CryptAuth v2 Enrollment GCM registration timeout and allow
    the GCM driver code to handle retries and timeouts silently. This
    mirrors v1 Enrollment which does not separately enforce a timeout.
    
    The GCM driver code ignores all registration requests until the current
    request, along with its possible failure retries, finishes. So, retries
    from the v2 Enrollment flow are not only pointless but also introduce
    longer retry delays. This CL aims to restore parity between v1 and v2
    Enrollment GCM registrations.
    
    Bug: 1032362
    Fixed: 1033675
    Change-Id: Ibf498108183f9c76944a07faa33b710cffb3f297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962568
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724357}

--- chromeos/services/device_sync/cryptauth_v2_enrollment_manager_impl.cc
+++ chromeos/services/device_sync/cryptauth_v2_enrollment_manager_impl.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "base/values.h"
 #include "chromeos/components/multidevice/logging/logging.h"
-#include "chromeos/services/device_sync/async_execution_time_metrics_logger.h"
 #include "chromeos/services/device_sync/cryptauth_enrollment_constants.h"
 #include "chromeos/services/device_sync/cryptauth_key_registry.h"
 #include "chromeos/services/device_sync/cryptauth_task_metrics_logger.h"
@@ -32,15 +31,9 @@ namespace device_sync {
 
 namespace {
 
-// Timeout values for asynchronous operations.
+// Timeout value for asynchronous operation.
 // TODO(https://crbug.com/933656): Use async execution time metrics to tune
-// these timeout values. For now, set these timeouts to the max execution time
-// recorded by the metrics.
-constexpr base::TimeDelta kWaitingForGcmRegistrationTimeout =
-    kMaxAsyncExecutionTime;
-
-// Note: Based on initial findings, about 6% of ClientAppMetadata fetches were
-// timing out after 30s.
+// this timeout value.
 constexpr base::TimeDelta kWaitingForClientAppMetadataTimeout =
     base::TimeDelta::FromSeconds(60);
 
@@ -128,11 +121,14 @@ void RecordEnrollmentResult(const CryptAuthEnrollmentResult& result) {
 }
 
 void RecordGcmRegistrationMetrics(const base::TimeDelta& execution_time,
-                                  CryptAuthAsyncTaskResult result) {
-  LogAsyncExecutionTimeMetric(
-      "CryptAuth.EnrollmentV2.ExecutionTime.GcmRegistration", execution_time);
-  LogCryptAuthAsyncTaskSuccessMetric(
-      "CryptAuth.EnrollmentV2.AsyncTaskResult.GcmRegistration", result);
+                                  bool success) {
+  base::UmaHistogramCustomTimes(
+      "CryptAuth.EnrollmentV2.GcmRegistration.AttemptTime", execution_time,
+      base::TimeDelta::FromSeconds(1) /* min */,
+      base::TimeDelta::FromMinutes(10) /* max */, 100 /* buckets */);
+
+  base::UmaHistogramBoolean("CryptAuth.EnrollmentV2.GcmRegistration.Success",
+                            success);
 }
 
 void RecordClientAppMetadataFetchMetrics(const base::TimeDelta& execution_time,
@@ -176,37 +172,6 @@ void CryptAuthV2EnrollmentManagerImpl::RegisterPrefs(
                                std::string());
   registry->RegisterStringPref(prefs::kCryptAuthEnrollmentUserPrivateKey,
                                std::string());
-}
-
-// static
-// Note: The enroller handles timeouts internally.
-base::Optional<base::TimeDelta>
-CryptAuthV2EnrollmentManagerImpl::GetTimeoutForState(State state) {
-  switch (state) {
-    case State::kWaitingForGcmRegistration:
-      return kWaitingForGcmRegistrationTimeout;
-    case State::kWaitingForClientAppMetadata:
-      return kWaitingForClientAppMetadataTimeout;
-    default:
-      // Signifies that there should not be a timeout.
-      return base::nullopt;
-  }
-}
-
-// static
-base::Optional<CryptAuthEnrollmentResult::ResultCode>
-CryptAuthV2EnrollmentManagerImpl::ResultCodeErrorFromTimeoutDuringState(
-    State state) {
-  switch (state) {
-    case State::kWaitingForGcmRegistration:
-      return CryptAuthEnrollmentResult::ResultCode::
-          kErrorTimeoutWaitingForGcmRegistration;
-    case State::kWaitingForClientAppMetadata:
-      return CryptAuthEnrollmentResult::ResultCode::
-          kErrorTimeoutWaitingForClientAppMetadata;
-    default:
-      return base::nullopt;
-  }
 }
 
 CryptAuthV2EnrollmentManagerImpl::Factory::~Factory() = default;
@@ -380,11 +345,8 @@ void CryptAuthV2EnrollmentManagerImpl::OnGCMRegistrationResult(bool success) {
 
   bool was_successful = success && !gcm_manager_->GetRegistrationId().empty();
 
-  CryptAuthAsyncTaskResult result = was_successful
-                                        ? CryptAuthAsyncTaskResult::kSuccess
-                                        : CryptAuthAsyncTaskResult::kError;
   RecordGcmRegistrationMetrics(
-      base::TimeTicks::Now() - last_state_change_timestamp_, result);
+      base::TimeTicks::Now() - last_state_change_timestamp_, was_successful);
 
   if (!was_successful) {
     OnEnrollmentFinished(CryptAuthEnrollmentResult(
@@ -511,40 +473,27 @@ void CryptAuthV2EnrollmentManagerImpl::SetState(State state) {
   state_ = state;
   last_state_change_timestamp_ = base::TimeTicks::Now();
 
-  base::Optional<base::TimeDelta> timeout_for_state = GetTimeoutForState(state);
-  if (!timeout_for_state)
+  if (state_ != State::kWaitingForClientAppMetadata)
     return;
 
   // TODO(https://crbug.com/936273): Add metrics to track failure rates due to
   // async timeouts.
-  timer_->Start(FROM_HERE, *timeout_for_state,
+  timer_->Start(FROM_HERE, kWaitingForClientAppMetadataTimeout,
                 base::BindOnce(&CryptAuthV2EnrollmentManagerImpl::OnTimeout,
                                callback_weak_ptr_factory_.GetWeakPtr()));
 }
 
 void CryptAuthV2EnrollmentManagerImpl::OnTimeout() {
-  // If there's a timeout specified, there should be a corresponding error code.
-  base::Optional<CryptAuthEnrollmentResult::ResultCode> error_code =
-      ResultCodeErrorFromTimeoutDuringState(state_);
-  DCHECK(error_code);
+  DCHECK(state_ == State::kWaitingForClientAppMetadata);
+  RecordClientAppMetadataFetchMetrics(
+      base::TimeTicks::Now() - last_state_change_timestamp_
+      /* execution_time */,
+      CryptAuthAsyncTaskResult::kTimeout);
 
-  base::TimeDelta execution_time =
-      base::TimeTicks::Now() - last_state_change_timestamp_;
-  switch (state_) {
-    case State::kWaitingForGcmRegistration:
-      RecordGcmRegistrationMetrics(execution_time,
-                                   CryptAuthAsyncTaskResult::kTimeout);
-      break;
-    case State::kWaitingForClientAppMetadata:
-      RecordClientAppMetadataFetchMetrics(execution_time,
-                                          CryptAuthAsyncTaskResult::kTimeout);
-      break;
-    default:
-      NOTREACHED();
-  }
-
-  OnEnrollmentFinished(CryptAuthEnrollmentResult(
-      *error_code, base::nullopt /*client_directive */));
+  OnEnrollmentFinished(
+      CryptAuthEnrollmentResult(CryptAuthEnrollmentResult::ResultCode::
+                                    kErrorTimeoutWaitingForClientAppMetadata,
+                                base::nullopt /*client_directive */));
 }
 
 std::string CryptAuthV2EnrollmentManagerImpl::GetV1UserPublicKey() const {
--- chromeos/services/device_sync/cryptauth_v2_enrollment_manager_impl.h
+++ chromeos/services/device_sync/cryptauth_v2_enrollment_manager_impl.h
@@ -110,10 +110,6 @@ class CryptAuthV2EnrollmentManagerImpl
 
   friend std::ostream& operator<<(std::ostream& stream, const State& state);
 
-  static base::Optional<base::TimeDelta> GetTimeoutForState(State state);
-  static base::Optional<CryptAuthEnrollmentResult::ResultCode>
-  ResultCodeErrorFromTimeoutDuringState(State state);
-
   // CryptAuthEnrollmentManager:
   void Start() override;
   void ForceEnrollmentNow(
