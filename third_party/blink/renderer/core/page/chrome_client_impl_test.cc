df32b532625d43aa6c4a4d480918319c70885dfb
Carlos Caballero <carlscab@google.com>
2019-12-12 17:35:27 +0000

    Fix includes in chrome_client_impl_test.cc
    This file uses RunLoop but does not include it. It relies on any of the
    other includes including it. That is generally bad and in particular
    breaks https://crrev.com/c/1946232
    
    Bug: 891670
    Change-Id: Iac646ac7fd1f230776c516391d1124285f866341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946231
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#724242}

--- third_party/blink/renderer/core/page/chrome_client_impl_test.cc
+++ third_party/blink/renderer/core/page/chrome_client_impl_test.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "third_party/blink/renderer/core/page/chrome_client_impl.h"
+#include "base/run_loop.h"
 #include "cc/trees/layer_tree_host.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/feature_policy/feature_policy.h"
