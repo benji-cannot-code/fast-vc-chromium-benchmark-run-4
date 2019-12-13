22194026129ec8bb384a05b054e4e6b211a87d6b
Eric Stevenson <estevenson@chromium.org>
2019-12-13 17:03:36 +0000

    Android: Support using multiple targets for JNI sources.
    Will be used by Trichrome so that the library APK can generate JNI for
    both Chrome and Webview.
    
    Tbr: pauljensen@chromium.org  # Mechanical build change.
    Bug: 901465
    Change-Id: Ibed4e508f2d01951919a36105cf8e7cf9373e7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962531
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Auto-Submit: Eric Stevenson <estevenson@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724672}

--- base/android/jni_generator/jni_registration_generator.py
+++ base/android/jni_generator/jni_registration_generator.py
@@ -540,6 +540,7 @@ def main(argv):
   arg_parser.add_argument(
       '--sources-files',
       required=True,
+      action='append',
       help='A list of .sources files which contain Java '
       'file paths.')
   arg_parser.add_argument(
@@ -584,15 +585,14 @@ def main(argv):
         'Invalid arguments: --require_mocks without --enable_proxy_mocks. '
         'Cannot require mocks if they are not enabled.')
 
-  args.sources_files = build_utils.ParseGnList(args.sources_files)
-
+  sources_files = sorted(set(build_utils.ParseGnList(args.sources_files)))
   proxy_opts = ProxyOptions(
       use_hash=args.use_proxy_hash,
       require_mocks=args.require_mocks,
       enable_mocks=args.enable_proxy_mocks)
 
   java_file_paths = []
-  for f in args.sources_files:
+  for f in sources_files:
     # Skip generated files, since the GN targets do not declare any deps.
     java_file_paths.extend(
         p for p in build_utils.ReadSourcesList(f)
@@ -608,7 +608,7 @@ def main(argv):
     build_utils.WriteDepfile(
         args.depfile,
         args.srcjar_path,
-        args.sources_files + java_file_paths,
+        sources_files + java_file_paths,
         add_pydeps=False)
 
 
