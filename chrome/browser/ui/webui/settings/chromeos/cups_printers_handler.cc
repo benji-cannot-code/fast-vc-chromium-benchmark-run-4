cbd2dfbc12d6bf25a4a6bfc70f468cd22678258c
Kalvin Lee <kdlee@chromium.org>
2019-12-13 23:51:07 +0000

    ppd_provider: shape gn scaffolding
    This change primes the implementation PpdProvider class for developing
    its successor. Note that the successor does not yet exist.
    
    Bug: 1025995
    Change-Id: I938d10d11501fe946c1a12e754110561621ecf75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946937
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724837}

--- chrome/browser/ui/webui/settings/chromeos/cups_printers_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/cups_printers_handler.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/debug_daemon/debug_daemon_client.h"
-#include "chromeos/printing/ppd_cache.h"
 #include "chromeos/printing/ppd_line_reader.h"
 #include "chromeos/printing/ppd_provider.h"
 #include "chromeos/printing/printer_configuration.h"
