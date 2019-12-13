a579b9a1c0529a0c8bd960e4d9d78349f50e5118
nancy <nancylingwang@chromium.org>
2019-12-13 01:03:56 +0000

    Move PluginVm to Chrome OS.
    BUG=1005640
    
    Change-Id: Ib4cd83b6602f931ac8526ff0aac34c661f4091d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1963911
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724449}

--- chrome/browser/apps/app_service/app_service_metrics.cc
+++ chrome/browser/apps/app_service/app_service_metrics.cc
@@ -9,10 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "chrome/browser/chromeos/extensions/default_web_app_ids.h"
 #include "chrome/browser/chromeos/file_manager/app_id.h"
-#include "chrome/browser/chromeos/plugin_vm/plugin_vm_util.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "chrome/services/app_service/public/mojom/app_service.mojom.h"
 #include "extensions/common/constants.h"
+
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/plugin_vm/plugin_vm_util.h"
+#endif  // OS_CHROMEOS
 
 namespace {
 
@@ -136,8 +139,10 @@ void RecordAppLaunch(const std::string& app_id,
     RecordBuiltInAppLaunch(BuiltInAppName::kDiscover, launch_source);
   else if (app_id == ash::kInternalAppIdSettings)
     RecordBuiltInAppLaunch(BuiltInAppName::kSettings, launch_source);
+#if defined(OS_CHROMEOS)
   else if (app_id == plugin_vm::kPluginVmAppId)
     RecordBuiltInAppLaunch(BuiltInAppName::kPluginVm, launch_source);
+#endif  // OS_CHROMEOS
 }
 
 void RecordBuiltInAppSearchResult(const std::string& app_id) {
@@ -156,9 +161,11 @@ void RecordBuiltInAppSearchResult(const std::string& app_id) {
   } else if (app_id == ash::kInternalAppIdSettings) {
     UMA_HISTOGRAM_ENUMERATION("Apps.AppListSearchResultInternalApp.Show",
                               BuiltInAppName::kSettings);
+#if defined(OS_CHROMEOS)
   } else if (app_id == plugin_vm::kPluginVmAppId) {
     UMA_HISTOGRAM_ENUMERATION("Apps.AppListSearchResultInternalApp.Show",
                               BuiltInAppName::kPluginVm);
+#endif  // OS_CHROMEOS
   }
 }
 
