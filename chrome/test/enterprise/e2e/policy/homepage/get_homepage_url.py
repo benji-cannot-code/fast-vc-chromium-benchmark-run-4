4222219a882f80e2b989396848380ddab3909251
Xiang Ji <jxiang@google.com>
2019-12-13 22:57:17 +0000

    Copy policy e2e tests to chrome/test/enterprise
    The first CL for tests folder move.
    
    1. Copy the policy tests into chrome/test/enterprise with new folder layout
    2. Add a readme.md file as place holder for the net folder
    3. Add jxiang as the OWNER of these policy tests
    
    Context: https://docs.google.com/document/d/1cHopqvLxnfhrQDPmrPEbalK8WAyQJyK91lrLqjFk1Os/edit?ts=5deec8b8#
    Bug: 35021
    Change-Id: I178042b76262bec762ad63013e4f11592c96dd6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1963198
    Auto-Submit: Xiang Ji <jxiang@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/master@{#724815}

---
+++ chrome/test/enterprise/e2e/policy/homepage/get_homepage_url.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from pywinauto.application import Application
+
+import test_util
+
+driver = test_util.create_chrome_webdriver()
+
+try:
+  app = Application(backend="uia")
+  app.connect(title_re='.*Chrome|.*Chromium')
+
+  # Use shortcut Alt+HOME to go to the home page
+  app.top_window().type_keys("%{HOME}")
+
+  print 'homepage:%s' % driver.current_url
+finally:
+  driver.quit()
