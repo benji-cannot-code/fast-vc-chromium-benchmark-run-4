a1f19371bc36c7b59ebe03f69c8d998bd4de62a9
Tom Anderson <thomasanderson@chromium.org>
2019-12-13 02:17:18 +0000

    Whitelist dependencies on lib{drm,gbm,xcb-dri3}
    All of these are needed to support GpuMemoryBuffers on Linux.
    
    BUG=1031269
    R=thestig
    
    Change-Id: If3ff3421a7c411eb2dc1759d11e258208df50482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966391
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724463}

--- chrome/installer/linux/rpm/update_package_provides.py
+++ chrome/installer/linux/rpm/update_package_provides.py
@@ -36,8 +36,10 @@ PACKAGE_FILTER = [
     "libcairo.so",
     "libcups.so",
     "libdbus-1.so",
+    "libdrm.so.2",
     "libdl.so",
     "libexpat.so",
+    "libgbm.so.1",
     "libgcc_s.so",
     "libgdk-3.so",
     "libgdk_pixbuf-2.0.so",
@@ -58,6 +60,7 @@ PACKAGE_FILTER = [
     "libstdc++.so",
     "libuuid.so",
     "libxcb.so",
+    "libxcb-dri3.so.0",
     "rtld(GNU_HASH)",
 ]
 
