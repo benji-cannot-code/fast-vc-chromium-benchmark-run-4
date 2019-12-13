eeaa5b06f96558a8941a6f66237209ed3d5c3713
Julie Jeongeun Kim <jkim@igalia.com>
2019-12-13 02:50:46 +0000

    Use [RaisesException] for immediate promise rejections in webusb
    This is a part of effort for using [RaisesException] when synchronously
    rejecting a promise.
    
    It uses [RaisesException] for
    //third_party/blink/renderer/modules/webusb.
    
    Bug: 1001114
    Change-Id: I1c8d992d2f55b4e111486f67fa6cc255dee73268
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961832
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#724474}

--- third_party/blink/renderer/modules/webusb/usb.h
+++ third_party/blink/renderer/modules/webusb/usb.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class ExceptionState;
 class ScriptPromiseResolver;
 class ScriptState;
 class USBDevice;
@@ -44,8 +45,10 @@ class USB final : public EventTargetWithInlineData,
   void Dispose();
 
   // USB.idl
-  ScriptPromise getDevices(ScriptState*);
-  ScriptPromise requestDevice(ScriptState*, const USBDeviceRequestOptions*);
+  ScriptPromise getDevices(ScriptState*, ExceptionState&);
+  ScriptPromise requestDevice(ScriptState*,
+                              const USBDeviceRequestOptions*,
+                              ExceptionState&);
   DEFINE_ATTRIBUTE_EVENT_LISTENER(connect, kConnect)
   DEFINE_ATTRIBUTE_EVENT_LISTENER(disconnect, kDisconnect)
 
