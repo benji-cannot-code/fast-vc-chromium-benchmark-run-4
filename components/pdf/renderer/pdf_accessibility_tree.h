4306f25e0ef5e96f211ef425bb3f684aaea3b5f1
Ankit Kumar 🌪️ <ankk@microsoft.com>
2019-12-13 02:38:22 +0000

    Simplify PdfAccessibilityTree::GetPdfAnnotationInfoFromAXNode()
    Return base::Optional<PdfAccessibilityTree::AnnotationInfo> instead of
    returning the individual elements of AnnotationInfo as out parameters.
    
    Bug: 1008775
    Change-Id: I8490b735c48d8e5e70eb27c7895001ad98c1dccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1958496
    Commit-Queue: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724472}

--- components/pdf/renderer/pdf_accessibility_tree.h
+++ components/pdf/renderer/pdf_accessibility_tree.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/optional.h"
 #include "content/public/renderer/plugin_ax_tree_source.h"
 #include "ppapi/c/pp_instance.h"
 #include "ppapi/c/private/ppb_pdf.h"
@@ -42,6 +43,16 @@ class PdfAccessibilityTree : public content::PluginAXTreeSource {
       const std::vector<PP_PrivateAccessibilityCharInfo>& chars,
       const ppapi::PdfAccessibilityPageObjects& page_objects);
 
+  // Stores the page index and annotation index in the page.
+  struct AnnotationInfo {
+    AnnotationInfo(uint32_t page_index, uint32_t annotation_index);
+    AnnotationInfo(const AnnotationInfo& other);
+    ~AnnotationInfo();
+
+    uint32_t page_index;
+    uint32_t annotation_index;
+  };
+
   void SetAccessibilityViewportInfo(
       const PP_PrivateAccessibilityViewportInfo& viewport_info);
   void SetAccessibilityDocInfo(
@@ -52,9 +63,8 @@ class PdfAccessibilityTree : public content::PluginAXTreeSource {
       const std::vector<PP_PrivateAccessibilityCharInfo>& chars,
       const ppapi::PdfAccessibilityPageObjects& page_objects);
   void HandleAction(const PP_PdfAccessibilityActionData& action_data);
-  bool GetPdfAnnotationInfoFromAXNode(int32_t ax_node_id,
-                                      uint32_t* page_index,
-                                      uint32_t* annotation_index_in_page) const;
+  base::Optional<AnnotationInfo> GetPdfAnnotationInfoFromAXNode(
+      int32_t ax_node_id) const;
 
   // PluginAXTreeSource implementation.
   bool GetTreeData(ui::AXTreeData* tree_data) const override;
@@ -148,16 +158,6 @@ class PdfAccessibilityTree : public content::PluginAXTreeSource {
   content::RenderAccessibility* GetRenderAccessibility();
   gfx::Transform* MakeTransformFromViewInfo();
   void AddWordStartsAndEnds(ui::AXNodeData* inline_text_box);
-
-  // Stores the page index and annotation index in the page.
-  struct AnnotationInfo {
-    AnnotationInfo(uint32_t page_index, uint32_t annotation_index);
-    AnnotationInfo(const AnnotationInfo& other);
-    ~AnnotationInfo();
-
-    uint32_t page_index;
-    uint32_t annotation_index;
-  };
 
   ui::AXTreeData tree_data_;
   ui::AXTree tree_;
