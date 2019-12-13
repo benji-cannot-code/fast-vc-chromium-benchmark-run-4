5505839b1d1c10681d1cc469bdffce9cd2af76c1
Mike Dougherty <michaeldo@chromium.org>
2019-12-13 07:28:04 +0000

    Attach BreadcrumbStorage to BrowserState as a KeyedService
    BreadcrumbPersistentStorage stores breadcrumbs to disk and returns the
    breadcrumbs from the persisted file. This will allow breadcrumbs to be
    saved during an application run and retrieved after a crash on the next
    launch.
    
    Bug: 1003922
    Change-Id: Iffa9dc2797b66eda0793c9eafef6bddc8c73200d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959777
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724543}

--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_observer_unittest.mm
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_observer_unittest.mm
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_observer.h"
+#include "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_observer.h"
 
 #import "base/macros.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state_manager.h"
-#import "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_keyed_service.h"
+#include "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_keyed_service.h"
 #include "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_keyed_service_factory.h"
 #include "ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.h"
 #include "ios/web/public/test/web_task_environment.h"
---
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_persistent_storage_util.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_persistent_storage_util.h"
+
+#include "base/path_service.h"
+#include "ios/web/public/browser_state.h"
+
+namespace breadcrumb_persistent_storage_util {
+
+const base::FilePath::CharType kBreadcrumbsFile[] =
+    FILE_PATH_LITERAL("iOS Breadcrumbs");
+
+base::FilePath GetBreadcrumbPersistentStorageFilePath(
+    web::BrowserState* browser_state) {
+  return browser_state->GetStatePath().Append(kBreadcrumbsFile);
+}
+
+}  // namespace breadcrumb_persistent_storage_util
