934610ab75ba6817685a78d2c596d5b80fee91b6
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-12-13 18:54:07 +0000

    Import wpt@22511e549cdfbfef3cef7c888b1799bad755e07c
    Using wpt-import in Chromium 6db8a4844467f14b5bc95913409cfe900d67766b.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    chcunningham@chromium.org, wolenetz@chromium.org:
      external/wpt/media-source
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    hongchan@chromium.org, rtoy@chromium.org:
      external/wpt/webaudio
    
    NOAUTOREVERT=true
    TBR=smcgruer
    
    No-Export: true
    Change-Id: I7e60a3921710c29e100f8c5f401b35a694062ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967631
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#724710}

--- third_party/blink/web_tests/external/wpt/webaudio/js/worklet-recorder.js
+++ third_party/blink/web_tests/external/wpt/webaudio/js/worklet-recorder.js
@@ -31,6 +31,7 @@ class RecorderProcessor extends AudioWorkletProcessor {
         type: 'recordfinished',
         recordBuffer: this._recordBuffer
       });
+      this.port.close();
       return false;
     }
 
