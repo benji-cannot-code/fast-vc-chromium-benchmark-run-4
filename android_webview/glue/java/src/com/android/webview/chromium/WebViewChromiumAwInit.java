f3faa9a3bb7922b1e1ed65472f64ab79868e5e68
Oksana Zhuravlova <oksamyt@chromium.org>
2019-12-13 21:53:11 +0000

    [perf] Enable atrace earlier and turn off chrome tracing
    This change adds a call to TraceEvent.setATraceEnabled at a point before
    AwBrowserProcess.start() is called so that native startup chromium
    traces are captured by atrace. This allows us to turn off chrome tracing
    in the system_health.webview_startup benchmark since having both atrace
    and chrome tracing on causes incorrect event nesting (see
    https://crbug.com/1027261 for more context).
    
    Bug: 1027261
    Change-Id: I3435bc2f6a8e4cd340d3d40d70709ecc1ada5c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962632
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724793}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
@@ -172,6 +172,8 @@ public class WebViewChromiumAwInit {
             // available when AwFeatureListCreator::SetUpFieldTrials() runs.
             finishVariationsInitLocked();
 
+            TraceEvent.setATraceEnabled(mFactory.getWebViewDelegate().isTraceTagEnabled());
+
             AwBrowserProcess.start();
             AwBrowserProcess.handleMinidumpsAndSetMetricsConsent(true /* updateMetricsConsent */);
 
@@ -180,7 +182,6 @@ public class WebViewChromiumAwInit {
                 mSharedStatics.setWebContentsDebuggingEnabledUnconditionally(true);
             }
 
-            TraceEvent.setATraceEnabled(mFactory.getWebViewDelegate().isTraceTagEnabled());
             mFactory.getWebViewDelegate().setOnTraceEnabledChangeListener(
                     new WebViewDelegate.OnTraceEnabledChangeListener() {
                         @Override
