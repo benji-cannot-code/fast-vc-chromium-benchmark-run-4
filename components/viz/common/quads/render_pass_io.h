6fe6102ae1fcaa749690a7367090f0f671365485
Zhenyao Mo <zmo@chromium.org>
2019-12-13 20:43:16 +0000

    Optimize RenderPassIO for empty RRectF
    We don't need to dump all the fields if type is kEmpty.
    
    BUG=1026447
    TEST=viz_unittests
    R=sunnyps@chromium.org
    
    Change-Id: I14b306e4db88272e67102485514ba61031168750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966396
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724756}

--- components/viz/common/quads/render_pass_io.h
+++ components/viz/common/quads/render_pass_io.h
@@ -13,14 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/viz_common_export.h"
 
 namespace viz {
-namespace internal {
-// These functions are declared publicly so they could have friend access to
-// private fields of gfx::ColorSpace for the purpose of serialization and
-// deserialization. They should not be used outside render_pass_io.cc.
-base::Value ColorSpaceToDict(const gfx::ColorSpace& color_space);
-bool ColorSpaceFromDict(const base::Value& dict, gfx::ColorSpace* color_space);
-}  // namespace internal
-
 VIZ_COMMON_EXPORT base::Value RenderPassToDict(const RenderPass& render_pass);
 VIZ_COMMON_EXPORT std::unique_ptr<RenderPass> RenderPassFromDict(
     const base::Value& dict);
@@ -29,7 +21,6 @@ VIZ_COMMON_EXPORT base::Value RenderPassListToDict(
     const RenderPassList& render_pass_list);
 VIZ_COMMON_EXPORT bool RenderPassListFromDict(const base::Value& dict,
                                               RenderPassList* render_pass_list);
-
 }  // namespace viz
 
 #endif  // COMPONENTS_VIZ_COMMON_QUADS_RENDER_PASS_IO_H_
