be455247505d0edaeca72c8859349b3aab9326bd
pwarren <pwarren@google.com>
2019-12-13 19:18:16 +0000

    flag: restore show-overdraw-feedback flag
    This CL reverts the removal of the show-overdraw-feedback
    flag that was removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/1529513
    
    Bug: 1025119
    Test: Open chrome://flags and verify the flag is there.
    Change-Id: I3410097067e2f607bcf68bc8a0534e45c6facf10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967194
    Commit-Queue: Paulo Warren <pwarren@google.com>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724722}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1139,6 +1139,9 @@ extern const char kShowAutofillSignaturesDescription[];
 extern const char kShowAutofillTypePredictionsName[];
 extern const char kShowAutofillTypePredictionsDescription[];
 
+extern const char kShowOverdrawFeedbackName[];
+extern const char kShowOverdrawFeedbackDescription[];
+
 extern const char kSkiaRendererName[];
 extern const char kSkiaRendererDescription[];
 
