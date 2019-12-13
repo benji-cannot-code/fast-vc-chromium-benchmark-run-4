eda29d60bff52f3f70055b34eb0fd83b76e0c1aa
Josh Nohle <nohle@google.com>
2019-12-13 20:28:01 +0000

    [DeviceSync v2] Use SoftwareFeatureManager to call SetFeatureStatus
    When v1 and v2 DeviceSync are running in parallel, before v1 DeviceSync
    is deprecated, use the SoftwareFeatureManager to indirectly call
    CryptAuthFeatureStatusSetter::SetFeatureStatus(). This is to ensure an
    ordering of SetSoftwareFeatureState() and SetFeatureStatus() calls.
    These functions mutate the same feature states on the CryptAuth backend
    and the order of the calls matters; we need to avoid race conditions.
    
    When v1 DeviceSync is deprecated, the CryptAuthFeatureStatusSetter is
    used directly.
    
    Bug: 951969, 1019206
    Change-Id: I0711b011dc91a0218503a01f3131d4062f51bc36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1944888
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724747}

--- chromeos/services/device_sync/fake_software_feature_manager.h
+++ chromeos/services/device_sync/fake_software_feature_manager.h
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_SERVICES_DEVICE_SYNC_FAKE_SOFTWARE_FEATURE_MANAGER_H_
 #define CHROMEOS_SERVICES_DEVICE_SYNC_FAKE_SOFTWARE_FEATURE_MANAGER_H_
 
+#include <memory>
 #include <string>
 #include <vector>
 
 #include "base/callback.h"
 #include "chromeos/components/multidevice/software_feature.h"
+#include "chromeos/services/device_sync/feature_status_change.h"
 #include "chromeos/services/device_sync/network_request_error.h"
 #include "chromeos/services/device_sync/software_feature_manager.h"
 
@@ -25,6 +27,7 @@ class FakeSoftwareFeatureManager : public SoftwareFeatureManager {
    public:
     virtual ~Delegate() = default;
     virtual void OnSetSoftwareFeatureStateCalled() {}
+    virtual void OnSetFeatureStatusCalled() {}
     virtual void OnFindEligibleDevicesCalled() {}
   };
 
@@ -47,6 +50,25 @@ class FakeSoftwareFeatureManager : public SoftwareFeatureManager {
 
    private:
     DISALLOW_COPY_AND_ASSIGN(SetSoftwareFeatureStateArgs);
+  };
+
+  struct SetFeatureStatusArgs {
+    SetFeatureStatusArgs(
+        const std::string& device_id,
+        multidevice::SoftwareFeature feature,
+        FeatureStatusChange status_change,
+        base::OnceClosure success_callback,
+        base::OnceCallback<void(NetworkRequestError)> error_callback);
+    ~SetFeatureStatusArgs();
+
+    std::string device_id;
+    multidevice::SoftwareFeature feature;
+    FeatureStatusChange status_change;
+    base::OnceClosure success_callback;
+    base::OnceCallback<void(NetworkRequestError)> error_callback;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(SetFeatureStatusArgs);
   };
 
   struct FindEligibleDevicesArgs {
@@ -78,6 +100,11 @@ class FakeSoftwareFeatureManager : public SoftwareFeatureManager {
     return set_software_feature_state_calls_;
   }
 
+  const std::vector<std::unique_ptr<SetFeatureStatusArgs>>&
+  set_feature_status_calls() {
+    return set_feature_status_calls_;
+  }
+
   const std::vector<std::unique_ptr<FindEligibleDevicesArgs>>&
   find_eligible_multidevice_host_calls() {
     return find_eligible_multidevice_host_calls_;
@@ -91,6 +118,12 @@ class FakeSoftwareFeatureManager : public SoftwareFeatureManager {
       const base::Closure& success_callback,
       const base::Callback<void(NetworkRequestError)>& error_callback,
       bool is_exclusive = false) override;
+  void SetFeatureStatus(
+      const std::string& device_id,
+      multidevice::SoftwareFeature feature,
+      FeatureStatusChange status_change,
+      base::OnceClosure success_callback,
+      base::OnceCallback<void(NetworkRequestError)> error_callback) override;
   void FindEligibleDevices(
       multidevice::SoftwareFeature software_feature,
       const base::Callback<void(
@@ -103,6 +136,7 @@ class FakeSoftwareFeatureManager : public SoftwareFeatureManager {
 
   std::vector<std::unique_ptr<SetSoftwareFeatureStateArgs>>
       set_software_feature_state_calls_;
+  std::vector<std::unique_ptr<SetFeatureStatusArgs>> set_feature_status_calls_;
   std::vector<std::unique_ptr<FindEligibleDevicesArgs>>
       find_eligible_multidevice_host_calls_;
 
