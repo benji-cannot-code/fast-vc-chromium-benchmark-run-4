6455f6cc6a3b761cb751d95226fb401a84563922
Meredith Lane <meredithl@chromium.org>
2019-12-13 03:04:19 +0000

    [AOM] Integrate element reflection with the Accessibility Tree
    This is a reland, the original review can be seen at:
    https://crrev.com/c/1826397
    
    This change now migrates the existing accessibility code to rely on the
    attr-associated-elements set via the content attribute or IDL attributes.
    
    Followup work to: https://crrev.com/c/1687013, https://crrev.com/c/1781917
    
    Bug: 981423
    Change-Id: Ie5cbf514a74e68dfee3074aa9b473dfafd3a81e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1911347
    Commit-Queue: Meredith Lane <meredithl@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724479}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -414,6 +414,8 @@ bool IsElementReflectionAttribute(const QualifiedName& name) {
     return true;
   if (name == html_names::kAriaFlowtoAttr)
     return true;
+  if (name == html_names::kAriaLabeledbyAttr)
+    return true;
   if (name == html_names::kAriaLabelledbyAttr)
     return true;
   if (name == html_names::kAriaOwnsAttr)
@@ -423,7 +425,7 @@ bool IsElementReflectionAttribute(const QualifiedName& name) {
 
 HeapVector<Member<Element>>* GetExplicitlySetElementsForAttr(
     Element* element,
-    QualifiedName name) {
+    const QualifiedName& name) {
   ExplicitlySetAttrElementsMap* element_attribute_map =
       element->GetDocument().GetExplicitlySetAttrElementsMap(element);
   return element_attribute_map->at(name);
@@ -672,6 +674,11 @@ void Element::SetBooleanAttribute(const QualifiedName& name, bool value) {
     removeAttribute(name);
 }
 
+bool Element::HasExplicitlySetAttrAssociatedElements(
+    const QualifiedName& name) {
+  return GetExplicitlySetElementsForAttr(this, name);
+}
+
 void Element::SynchronizeContentAttributeAndElementReference(
     const QualifiedName& name) {
   ExplicitlySetAttrElementsMap* element_attribute_map =
@@ -719,6 +726,11 @@ void Element::SetElementAttribute(const QualifiedName& name, Element* element) {
     result.stored_value->value->clear();
   }
   result.stored_value->value->push_back(element);
+
+  if (isConnected()) {
+    if (AXObjectCache* cache = GetDocument().ExistingAXObjectCache())
+      cache->HandleAttributeChanged(name, this);
+  }
 }
 
 Element* Element::GetElementAttribute(const QualifiedName& name) {
@@ -799,6 +811,10 @@ void Element::SetElementArrayAttribute(
   }
 
   setAttribute(name, value.SerializeToString());
+  if (isConnected()) {
+    if (AXObjectCache* cache = GetDocument().ExistingAXObjectCache())
+      cache->HandleAttributeChanged(name, this);
+  }
   element_attribute_map->Set(name, elements);
 }
 
@@ -807,12 +823,23 @@ HeapVector<Member<Element>> Element::GetElementArrayAttribute(
     bool& is_null) {
   HeapVector<Member<Element>>* explicitly_set_elements =
       GetExplicitlySetElementsForAttr(this, name);
+
   is_null = false;
   if (explicitly_set_elements) {
     return *explicitly_set_elements;
   }
 
-  String attribute_value = getAttribute(name).GetString();
+  QualifiedName attr = name;
+
+  // Account for labelled vs labeled spelling
+  if (attr == html_names::kAriaLabelledbyAttr) {
+    attr = hasAttribute(html_names::kAriaLabeledbyAttr) &&
+                   !hasAttribute(html_names::kAriaLabelledbyAttr)
+               ? html_names::kAriaLabeledbyAttr
+               : html_names::kAriaLabelledbyAttr;
+  }
+
+  String attribute_value = getAttribute(attr).GetString();
   HeapVector<Member<Element>> content_elements;
 
   Vector<String> tokens;
