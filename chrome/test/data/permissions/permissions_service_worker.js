7cd875769982f1c29befa930b6cf9428bf7322b4
Rohit Agarwal <roagarwal@chromium.org>
2019-12-13 15:38:17 +0000

    Delete PermissionServiceContext instance when shutting down RPHI.
    The PermissionServiceContext holds PermissionSubscriptions originating
    from service workers. These subscriptions observe the
    PermissionControllerImpl that is owned by the Profile. When we delete
    the profile, we terminate the RenderProcessHosts associated with
    the profile first but however, in the OTR case, the RenderProcessHost
    corresponding to PermissionControllerImpl is not deleted before
    Profile.
    
    The fix was to explicitly delete the PermissionServiceContext instance
    when we call the RenderProcessHost::CleanUp method.
    
    This CL also adds the regression test.
    
    Bug: 889276
    Change-Id: I226647172b6f9cc36efbe2cddedaa921dbde4afe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960458
    Commit-Queue: Rohit Agarwal <roagarwal@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724643}

---
+++ chrome/test/data/permissions/permissions_service_worker.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+self.addEventListener('install', async function(event) {
+    let permissionState = await navigator.permissions.query({name:'geolocation'})
+    permissionState.onchange = function() {}
+});
