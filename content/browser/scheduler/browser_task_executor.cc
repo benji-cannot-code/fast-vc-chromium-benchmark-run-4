37a0fff07f6576d19f44443fe247c1f0cd4f888b
Carlos Caballero <carlscab@google.com>
2019-12-13 13:22:36 +0000

    Add missing include in browser_task_executor.cc
    Compile fails in https://crrev.com/c/1964490
    
    Bug: 891670
    Change-Id: Ie2ef8b24812546b7ba55d79e298f9fb123a7bb11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965589
    Auto-Submit: Carlos Caballero <carlscab@google.com>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724606}

--- content/browser/scheduler/browser_task_executor.cc
+++ content/browser/scheduler/browser_task_executor.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/deferred_sequenced_task_runner.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/no_destructor.h"
+#include "base/run_loop.h"
 #include "base/task/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/trace_event.h"
