2bcb3880ab82129195ce489ff3866cd9c90605b0
Maks Orlovich <morlovich@chromium.org>
2019-12-13 01:28:23 +0000

    Remove apparently dead URLRequestContext::CopyFrom
    Bug: 934009, 146593
    Change-Id: Iff9eedc8e0fec5b4abdc0673ba9104f100728169
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965854
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724454}

--- net/url_request/url_request_context.h
+++ net/url_request/url_request_context.h
@@ -28,18 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_request.h"
 
-class ChromeBrowserStateImplIOData;
-class ProfileImplIOData;
-
 namespace base {
 namespace trace_event {
 class ProcessMemoryDump;
 }
 }
-
-namespace safe_browsing {
-class SafeBrowsingURLRequestContextGetter;
-}  // namespace safe_browsing
 
 namespace net {
 class CertVerifier;
@@ -79,9 +72,7 @@ class ReportingService;
 class NET_EXPORT URLRequestContext
     : public base::trace_event::MemoryDumpProvider {
  public:
-  // Contexts that are known to not currently be copied should set |allow_copy|
-  // to false to prevent added copying.
-  explicit URLRequestContext(bool allow_copy = true);
+  URLRequestContext();
   ~URLRequestContext() override;
 
   // May return nullptr if this context doesn't have an associated network
@@ -318,25 +309,6 @@ class NET_EXPORT URLRequestContext
   }
 
  private:
-  // Allowed legacy usage of now-deprecated CopyFrom().
-  friend class ::ChromeBrowserStateImplIOData;
-  friend class ::ProfileImplIOData;
-  friend class safe_browsing::SafeBrowsingURLRequestContextGetter;
-
-  // Copies the state from |other| into this context.
-  //
-  // Due to complex interdependencies between various fields as well as fields
-  // that should be unique to each context, copy is fundamentally broken, and
-  // should not be done. If a modified context is needed (and that is not
-  // typical), a new context should always be fully created (via
-  // URLRequestContextBuilder) rather than copying from a previous one.
-  void CopyFrom(const URLRequestContext* other);
-
-  // ---------------------------------------------------------------------------
-  // Important: When adding any new members below, consider whether they need to
-  // be added to CopyFrom.
-  // ---------------------------------------------------------------------------
-
   // Ownership for these members are not defined here. Clients should either
   // provide storage elsewhere or have a subclass take ownership.
   NetLog* net_log_;
@@ -366,11 +338,6 @@ class NET_EXPORT URLRequestContext
   FtpAuthCache* ftp_auth_cache_;
 #endif  // !BUILDFLAG(DISABLE_FTP_SUPPORT)
 
-  // ---------------------------------------------------------------------------
-  // Important: When adding any new members below, consider whether they need to
-  // be added to CopyFrom.
-  // ---------------------------------------------------------------------------
-
   std::unique_ptr<std::set<const URLRequest*>> url_requests_;
 
   // Enables Brotli Content-Encoding support.
@@ -383,8 +350,6 @@ class NET_EXPORT URLRequestContext
   // Used in MemoryDumpProvier to annotate memory usage. The name does not need
   // to be unique.
   std::string name_;
-
-  const bool allow_copy_;
 
   THREAD_CHECKER(thread_checker_);
 
