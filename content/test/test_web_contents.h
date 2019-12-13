c1b02ddf0c166b45a4e908ab7ff9045b31569bc2
Tomasz Moniuszko <tmoniuszko@opera.com>
2019-12-13 08:55:36 +0000

    Fix Referrer forward declaration mismatch
    Bug: 1033438
    Change-Id: I0e892a2c8ea7b15e9f2c13f86162b011a3e75100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1963852
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Tomasz Moniuszko <tmoniuszko@opera.com>
    Cr-Commit-Position: refs/heads/master@{#724557}

--- content/test/test_web_contents.h
+++ content/test/test_web_contents.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/page_transition_types.h"
 
 class GURL;
-class Referrer;
 class SkBitmap;
 
 namespace gfx {
@@ -40,6 +39,7 @@ class HttpResponseHeaders;
 namespace content {
 
 class NavigationHandle;
+struct Referrer;
 class RenderViewHost;
 class TestRenderViewHost;
 class WebContentsTester;
