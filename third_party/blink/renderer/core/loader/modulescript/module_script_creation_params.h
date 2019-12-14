774b31b8f776065056f2adf67d7250de1197be84
Sam Sebree <sasebree@microsoft.com>
2019-12-14 03:19:41 +0000

    [SyntheticModules] Implements CSS Modules
    This is the final change required for CSS Modules to be utilized by developers.
    Following the acceptance of this change, if you run chromium with the CSSModules runtime flag, the following is now valid syntax:
    
    <script type="module">
        import sheet from "./example.css";
    </script>
    
    CSS Modules Explainer:
    https://github.com/w3c/webcomponents/blob/gh-pages/proposals/css-modules-v1-explainer.md
    
    CSS Modules Spec PR:
    https://github.com/whatwg/html/pull/4898
    
    Bug: 967018
    Change-Id: Ifdee5b92259fb7e4e9c8f9aa88e69a98eb55c551
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1799923
    Commit-Queue: Sam Sebree <sasebree@microsoft.com>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724896}

--- third_party/blink/renderer/core/loader/modulescript/module_script_creation_params.h
+++ third_party/blink/renderer/core/loader/modulescript/module_script_creation_params.h
@@ -21,7 +21,7 @@ namespace blink {
 class ModuleScriptCreationParams {
   DISALLOW_NEW();
 
-  enum class ModuleType { kJavaScriptModule, kJSONModule };
+  enum class ModuleType { kJavaScriptModule, kJSONModule, kCSSModule };
 
  public:
   ModuleScriptCreationParams(
