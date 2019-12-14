0c11f885a0db8613ba542093205a93af6a78652f
Virender Singh <virens@microsoft.com>
2019-12-14 04:18:41 +0000

    Implement scroll to global point action in PDFiumEngine
    This change adds the following
    - Adds name PP_PDF_SCROLL_TO_GLOBAL_POINT in PP_PdfAccessibilityAction
      action enum.
    - Handle PP_PDF_SCROLL_TO_GLOBAL_POINT action in PDFiumEngine.
      The action is used by certain screen readers to
      scroll content in viewport during continuous read operation.
    
    Bug: 811233
    Change-Id: I725a5295f42049af139856ce9815799cf67c0088
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954800
    Commit-Queue: Virender Singh <virens@microsoft.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#724911}

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -2025,6 +2025,10 @@ void PDFiumEngine::HandleAccessibilityAction(
       }
       break;
     }
+    case PP_PdfAccessibilityAction::PP_PDF_SCROLL_TO_GLOBAL_POINT: {
+      ScrollToGlobalPoint(action_data.target_rect, action_data.target_point);
+      break;
+    }
     default:
       NOTREACHED();
       break;
@@ -2196,6 +2200,12 @@ void PDFiumEngine::ScrollBasedOnScrollAlignment(
   }
 
   client_->ScrollBy(scroll_offset);
+}
+
+void PDFiumEngine::ScrollToGlobalPoint(const pp::Rect& target_rect,
+                                       const pp::Point& global_point) {
+  pp::Point scroll_offset = GetScreenRect(target_rect).point();
+  client_->ScrollBy(scroll_offset - global_point);
 }
 
 base::Optional<PDFEngine::NamedDestination> PDFiumEngine::GetNamedDestination(
