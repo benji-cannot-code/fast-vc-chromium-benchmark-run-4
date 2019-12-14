bfdce43d87fb50265b7a731b67cde6d340b6b9df
Ce Chen <cch@chromium.org>
2019-12-14 02:38:45 +0000

    [omnibox] Make on device head model class static, to remove |model_| and get rid of |this| from task posting in on device head provider.
    This should fix all issues introduced in https://crrev.com/c/1927471,
    since no |this| is needed when posting task to worker thread and
    only weak ptr is used after this change.
    
    Note this change just simply changes all related functions into static
    and wraps all member variables into an OnDeviceModelParams object and
    pass it around all functions need them in class OnDeviceHeadModel.
    Not much optimizations are made here in order to keep this CL small and
    straightforward to review (though it's still large; we can continue to
    optimize the code in future CLs).
    
    Tested in Linux.
    
    Bug: 925072
    Bug: 1032705
    Bug: 1029568
    Change-Id: I43d5aad52985eac4a2315606a6e03d41ca151173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966553
    Commit-Queue: Ce Chen <cch@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724886}

--- components/omnibox/browser/on_device_head_provider_unittest.cc
+++ components/omnibox/browser/on_device_head_provider_unittest.cc
@@ -59,7 +59,7 @@ class OnDeviceHeadProviderTest : public testing::Test,
 
   void ResetModelInstance() {
     if (provider_) {
-      provider_->model_.reset();
+      provider_->model_filename_.clear();
     }
   }
 
