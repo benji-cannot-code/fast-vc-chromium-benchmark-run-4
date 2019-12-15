4db449bcc05fcbc4859f0a9ee945fc568ae06809
Christopher Lam <calamity@chromium.org>
2019-12-15 08:23:34 +0000

    [WebUIController] Add safe downcasts.
    In order to support BrowserInterfaceBroker migrations for WebUI, we need
    a safe way to downcast (see https://crrev.com/c/1947656).
    
    This CL adds a static variable like WebContentsUserData which is used as
    a way to reliably distinguish between subclasses, providing a safe cast
    mechanism, which is used in https://crrev.com/c/1959228.
    
    Bug: 936482
    Change-Id: Iefd1be8b6037d7e6b5cb2dfbbd53e987339ddf40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959199
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Mario Sanchez Prada <mario@igalia.com>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724965}

--- content/public/browser/web_ui_controller.cc
+++ content/public/browser/web_ui_controller.cc
@@ -13,4 +13,8 @@ bool WebUIController::OverrideHandleWebUIMessage(const GURL& source_url,
   return false;
 }
 
+WebUIController::Type WebUIController::GetType() {
+  return nullptr;
+}
+
 }  // namespace content
