85a21df258f686421074f491277a10895c9d5668
Rune Lillesveen <futhark@chromium.org>
2019-12-16 13:34:28 +0000

    Better test descriptions for meta color-scheme tests.
    Bug: 925935
    Change-Id: I5e9d30743a8d4ef7dd257d297ad7a4039407110a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1968990
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725090}

--- third_party/blink/web_tests/external/wpt/css/css-color-adjust/meta/support/compute-root-color-scheme.js
+++ third_party/blink/web_tests/external/wpt/css/css-color-adjust/meta/support/compute-root-color-scheme.js
@@ -1,8 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
-function assert_root_color_scheme(expected) {
+function assert_root_color_scheme(expected, description) {
   test(() => {
-    assert_equals(getComputedStyle(document.documentElement).colorScheme, expected);
-  }, "Computed root color-scheme should be " + expected);
+    assert_equals(getComputedStyle(document.documentElement).colorScheme, expected), "Check root element color scheme";
+  }, description);
 }
