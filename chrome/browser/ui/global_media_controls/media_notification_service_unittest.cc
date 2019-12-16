7b4b1c5e6569ca21d617972d4571ab77058b8b8f
Alexander Hendrich <hendrich@chromium.org>
2019-12-16 09:19:53 +0000

    disable flaky MediaNotificationServiceCastTest.CountCastSessionsAsActive
    Test is flaky on Mac10.12
    
    TBR=takumif@chromium.org,steimel@chromium.org
    
    Bug: 1034406
    Change-Id: I0eeba82837727249fa472952a4b3a8b72f936179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967460
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725045}

--- chrome/browser/ui/global_media_controls/media_notification_service_unittest.cc
+++ chrome/browser/ui/global_media_controls/media_notification_service_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/unguessable_token.h"
+#include "build/build_config.h"
 #include "chrome/browser/media/router/media_router_factory.h"
 #include "chrome/browser/media/router/test/mock_media_router.h"
 #include "chrome/browser/ui/global_media_controls/cast_media_notification_provider.h"
@@ -603,7 +604,13 @@ TEST_F(MediaNotificationServiceTest, DismissesMediaSession) {
       1);
 }
 
-TEST_F(MediaNotificationServiceCastTest, CountCastSessionsAsActive) {
+// TODO(https://crbug.com/1034406) Flaky on Mac10.12
+#if defined(OS_MACOSX)
+#define MAYBE_CountCastSessionsAsActive DISABLED_CountCastSessionsAsActive
+#else
+#define MAYBE_CountCastSessionsAsActive CountCastSessionsAsActive
+#endif
+TEST_F(MediaNotificationServiceCastTest, MAYBE_CountCastSessionsAsActive) {
   media_router::MediaRoute media_route("id",
                                        media_router::MediaSource("source_id"),
                                        "sink_id", "description", true, true);
