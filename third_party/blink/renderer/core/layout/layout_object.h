da61d7c7b67d235847b19352bf4694d8d5440843
Nancy Wang <nancylingwang@chromium.org>
2019-12-16 12:56:06 +0000

    Reland "[mathml] Implement MathML containers (<math>, <mrow>, etc)"
    This reverts commit 317241081a95448447f6632dbca7824b8d140bfe.
    
    Reason for revert: <INSERT REASONING HERE>
    Wrong revert.
    
    Original change's description:
    > Revert "[mathml] Implement MathML containers (<math>, <mrow>, etc)"
    > 
    > This reverts commit f815237956a0e1fbd6d956690dfd79f74736842f.
    > 
    > Reason for revert: <INSERT REASONING HERE>
    > Break build:
    > https://ci.chromium.org/p/chromium/builders/ci/chromeos-kevin-rel/20182
    > 
    > Original change's description:
    > > [mathml] Implement MathML containers (<math>, <mrow>, etc)
    > > 
    > > This adds basic support for containers which typically layout their
    > > children in a row and control stretching of their vertical stretchy
    > > operator children. Handling of out-of-flow elements is not supported yet.
    > > 
    > > The following elements are implemented:
    > > https://mathml-refresh.github.io/mathml-core/#the-top-level-math-element
    > > https://mathml-refresh.github.io/mathml-core/#horizontally-group-sub-expressions-mrow
    > > https://mathml-refresh.github.io/mathml-core/#style-change-mstyle
    > > https://mathml-refresh.github.io/mathml-core/#error-message-merror
    > > https://mathml-refresh.github.io/mathml-core/#making-sub-expressions-invisible-mphantom
    > > 
    > > These legacy MathML3 elements are just implemented as basic <mrow>:
    > > https://mathml-refresh.github.io/mathml-core/#dfn-maction (as in Core)
    > > https://mathml-refresh.github.io/mathml-core/#dfn-semantics (as in Core)
    > > 
    > > Bug: 6606
    > > Change-Id: I29fa3883a6f8df7bd93c891da7c1ddea072af169
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1913250
    > > Commit-Queue: Rob Buis <rbuis@igalia.com>
    > > Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    > > Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    > > Reviewed-by: Mason Freed <masonfreed@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#724981}
    > 
    > TBR=ikilpatrick@chromium.org,mstensho@chromium.org,rbuis@igalia.com,masonfreed@chromium.org
    > 
    > Change-Id: I243034ff65fc2d9a71efa221681009e5776ba3dd
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 6606
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1968771
    > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#724985}
    
    TBR=ikilpatrick@chromium.org,mstensho@chromium.org,rbuis@igalia.com,masonfreed@chromium.org,nancylingwang@chromium.org
    
    Change-Id: Iecf13770dd3e9d39587469fd9e7761409fc22fe3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 6606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969415
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725087}

--- third_party/blink/renderer/core/layout/layout_object.h
+++ third_party/blink/renderer/core/layout/layout_object.h
@@ -670,6 +670,8 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
   bool IsListBox() const { return IsOfType(kLayoutObjectListBox); }
   bool IsListItem() const { return IsOfType(kLayoutObjectListItem); }
   bool IsListMarker() const { return IsOfType(kLayoutObjectListMarker); }
+  bool IsMathML() const { return IsOfType(kLayoutObjectMathML); }
+  bool IsMathMLRoot() const { return IsOfType(kLayoutObjectMathMLRoot); }
   bool IsMedia() const { return IsOfType(kLayoutObjectMedia); }
   bool IsMenuList() const { return IsOfType(kLayoutObjectMenuList); }
   bool IsProgress() const { return IsOfType(kLayoutObjectProgress); }
@@ -2492,6 +2494,8 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
     kLayoutObjectListBox,
     kLayoutObjectListItem,
     kLayoutObjectListMarker,
+    kLayoutObjectMathML,
+    kLayoutObjectMathMLRoot,
     kLayoutObjectMedia,
     kLayoutObjectMenuList,
     kLayoutObjectNGBlockFlow,
---
+++ third_party/blink/renderer/core/layout/ng/mathml/ng_math_layout_utils.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_MATHML_NG_MATH_LAYOUT_UTILS_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_MATHML_NG_MATH_LAYOUT_UTILS_H_
+
+namespace blink {
+
+struct LogicalSize;
+class NGBlockNode;
+class NGConstraintSpace;
+class NGLayoutInputNode;
+
+// Creates a new constraint space for the current child.
+NGConstraintSpace CreateConstraintSpaceForMathChild(
+    const NGBlockNode& parent_node,
+    const LogicalSize& child_available_size,
+    const NGConstraintSpace& parent_constraint_space,
+    const NGLayoutInputNode&);
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_MATHML_NG_MATH_LAYOUT_UTILS_H_
