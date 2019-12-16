e92529d9e5f364496539a47705c90d6be863fc21
Eugene But <eugenebut@google.com>
2019-12-16 13:24:12 +0000

    Avoid NSURL -> GURL conversion in currentURLWithTrustLevel:
    Add IsWKInternalUrl(NSURL*) method, which allows to avoid potentially
    expensive NSURL -> GURL conversion where it's not necessary.
    
    This change does not fully fix crbug.com/1032037, but still improve the
    performance.
    
    Bug: 1032037
    Change-Id: I501a532d0c24f1fe645c34979f8b5613239e8cf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967927
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725089}

--- ios/web/navigation/wk_navigation_util.h
+++ ios/web/navigation/wk_navigation_util.h
@@ -46,6 +46,7 @@ extern NSString* const kReferrerHeaderName;
 
 // Returns true if |url| is a placeholder URL or restore_session.html URL.
 bool IsWKInternalUrl(const GURL& url);
+bool IsWKInternalUrl(NSURL* url);
 
 // Returns true if |url| is an app specific url or an about:// scheme
 // non-placeholder url.
@@ -68,6 +69,7 @@ void CreateRestoreSessionUrl(
 
 // Returns true if the base URL of |url| is restore_session.html.
 bool IsRestoreSessionUrl(const GURL& url);
+bool IsRestoreSessionUrl(NSURL* url);
 
 // Creates a restore_session.html URL that encodes the specified |target_url| in
 // the URL fragment with a "targetUrl=" prefix. When this URL is loaded in the
@@ -85,6 +87,7 @@ bool ExtractTargetURL(const GURL& restore_session_url, GURL* target_url);
 
 // Returns true if |URL| is a placeholder navigation URL.
 bool IsPlaceholderUrl(const GURL& url);
+bool IsPlaceholderUrl(NSURL* url);
 
 // Creates the URL for the placeholder navigation required for Native View and
 // WebUI URLs.
