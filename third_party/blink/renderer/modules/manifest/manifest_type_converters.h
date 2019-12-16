2c13e87137255b2e3021be6753d63dd694796e86
Rahul Singh <rahsin@microsoft.com>
2019-12-16 21:17:20 +0000

    Add support for parsing shortcuts in Web App Manifest
    This change is part 1 of 2 for adding App shortcut menu for PWAs.
    
    As part of this change, when a shortcuts section is included in the Web
    App Manifest, it gets parsed by the manifest parser.
    
    It is then appropriately formatted to be accessed from the Browser
    process using Mojo.
    
    The shortcuts will be used during the PWA installation flow to add in
    app shortcuts.( This will be a follow up CL)
    
    Bug: 955497
    Change-Id: I913141a0308a279271f862baae20132cae22aa68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1941051
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725263}

--- third_party/blink/renderer/modules/manifest/manifest_type_converters.h
+++ third_party/blink/renderer/modules/manifest/manifest_type_converters.h
@@ -33,6 +33,13 @@ struct TypeConverter<blink::Manifest::ImageResource,
 };
 
 template <>
+struct TypeConverter<blink::Manifest::ShortcutItem,
+                     blink::mojom::blink::ManifestShortcutItemPtr> {
+  static blink::Manifest::ShortcutItem Convert(
+      const blink::mojom::blink::ManifestShortcutItemPtr& input);
+};
+
+template <>
 struct TypeConverter<blink::Manifest::ShareTarget,
                      blink::mojom::blink::ManifestShareTargetPtr> {
   static blink::Manifest::ShareTarget Convert(
