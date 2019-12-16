2a3ead59c4279fd6be6f409640ffc89287fffd58
Alexander Hendrich <hendrich@chromium.org>
2019-12-16 11:29:25 +0000

    Revert "system-web-apps: add support to pass launch_files to running Apps"
    This reverts commit 42bdc71e796176e2cb2db5b38f90338d19070722.
    
    Reason for revert: https://crbug.com/1034456 (All/SharedPWATest.MixedContentInPWA/1 failing on linux-chromeos-chrome bot)
    
    Original change's description:
    > system-web-apps: add support to pass launch_files to running Apps
    > 
    > - Clean up ShowApplicationWindow logic, split it into sub functions.
    > - Remove SetInitialFocus, which is no longer necessary.
    > - Remove extra HostedAppBrowserController::SetAppPrefsForWebContents
    >   during app launch. HostedAppBrowserController already does this.
    > - Set launch files before showing the window.
    > 
    > Fixed: 1023742
    > Change-Id: I074a917bb0ed557d1a967a04cb3042c9ee29113b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1955247
    > Reviewed-by: Michael Wasserman <msw@chromium.org>
    > Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    > Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    > Commit-Queue: Jiewei Qian  <qjw@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#725054}
    
    TBR=msw@chromium.org,ericwilligers@chromium.org,ortuno@chromium.org,joelhockey@chromium.org,qjw@chromium.org
    
    Change-Id: Ic5e4c4afec085827e7ac879bd1f67f84e100be4f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969341
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725069}

--- chrome/browser/ui/extensions/hosted_app_browser_controller.cc
+++ chrome/browser/ui/extensions/hosted_app_browser_controller.cc
@@ -55,9 +55,12 @@ bool IsSameHostAndPort(const GURL& app_url, const GURL& page_url) {
          app_url.port() == page_url.port();
 }
 
-// Set preferences that are unique to app windows.
-void SetAppPrefsForWebContents(web_app::AppBrowserController* controller,
-                               content::WebContents* web_contents) {
+}  // namespace
+
+// static
+void HostedAppBrowserController::SetAppPrefsForWebContents(
+    web_app::AppBrowserController* controller,
+    content::WebContents* web_contents) {
   web_contents->GetMutableRendererPrefs()->can_accept_load_drops = false;
   web_contents->SyncRendererPrefs();
 
@@ -74,8 +77,9 @@ void SetAppPrefsForWebContents(web_app::AppBrowserController* controller,
   web_contents->NotifyPreferencesChanged();
 }
 
-// Clear preferences that are unique to app windows.
-void ClearAppPrefsForWebContents(content::WebContents* web_contents) {
+// static
+void HostedAppBrowserController::ClearAppPrefsForWebContents(
+    content::WebContents* web_contents) {
   web_contents->GetMutableRendererPrefs()->can_accept_load_drops = true;
   web_contents->SyncRendererPrefs();
 
@@ -84,8 +88,6 @@ void ClearAppPrefsForWebContents(content::WebContents* web_contents) {
 
   web_contents->NotifyPreferencesChanged();
 }
-
-}  // namespace
 
 HostedAppBrowserController::HostedAppBrowserController(Browser* browser)
     : AppBrowserController(
@@ -272,12 +274,13 @@ void HostedAppBrowserController::OnReceivedInitialURL() {
 
 void HostedAppBrowserController::OnTabInserted(content::WebContents* contents) {
   AppBrowserController::OnTabInserted(contents);
-  SetAppPrefsForWebContents(this, contents);
+  extensions::HostedAppBrowserController::SetAppPrefsForWebContents(this,
+                                                                    contents);
 }
 
 void HostedAppBrowserController::OnTabRemoved(content::WebContents* contents) {
   AppBrowserController::OnTabRemoved(contents);
-  ClearAppPrefsForWebContents(contents);
+  extensions::HostedAppBrowserController::ClearAppPrefsForWebContents(contents);
 }
 
 }  // namespace extensions
