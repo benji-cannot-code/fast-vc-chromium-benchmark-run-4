058e847c8a4ad02de5864e3b23b80bfec9eeb560
Nate Chapin <japhet@chromium.org>
2019-12-16 02:13:50 +0000

    Remove ExecutionContext task pausing callbacks
    These callbacks used accomplish something similar to
    ContextLifecycleStateObserver, but all uses have been converted.
    
    Bug: 1029822
    Change-Id: I220efe216dfd6a6f73c1a96140d9f467001891d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1938188
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724999}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1125,10 +1125,6 @@ class CORE_EXPORT Document : public ContainerNode,
   bool IsDNSPrefetchEnabled() const { return is_dns_prefetch_enabled_; }
   void ParseDNSPrefetchControlHeader(const String&);
 
-  void TasksWerePaused() final;
-  void TasksWereUnpaused() final;
-  bool TasksNeedPause() final;
-
   void FinishedParsing();
 
   void SetEncodingData(const DocumentEncodingData& new_data);
