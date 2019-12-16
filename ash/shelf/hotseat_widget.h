92ca16259185d04394a9870a4e17ab60720605eb
Sammie Quon <sammiequon@google.com>
2019-12-16 19:36:43 +0000

    shelf: Do not process hotseat events on non visible part.
    And not process while hotseat is animating.
    
    Test: manual, added test
    Bug: 1032134
    
    Change-Id: I60d6ab9f4a05497180a9eb15363855866b57b5b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966428
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725219}

--- ash/shelf/hotseat_widget.h
+++ ash/shelf/hotseat_widget.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_types.h"
 #include "ui/views/widget/widget.h"
 
+namespace aura {
+class ScopedWindowTargeter;
+}
+
 namespace ash {
 class FocusCycler;
 class ScrollableShelfView;
@@ -53,6 +57,8 @@ class ASH_EXPORT HotseatWidget : public views::Widget,
 
   // Updates the opaque background which functions as the hotseat background.
   void UpdateOpaqueBackground();
+
+  gfx::Size GetOpaqueBackgroundSize() const;
 
   // Sets the focus cycler and adds the hotseat to the cycle.
   void SetFocusCycler(FocusCycler* focus_cycler);
@@ -98,6 +104,11 @@ class ASH_EXPORT HotseatWidget : public views::Widget,
   // Whether the widget is currently extended because the user has manually
   // dragged it. This will be reset with any visible shelf configuration change.
   bool is_manually_extended_ = false;
+
+  // The window targeter installed on the hotseat. Filters out events which land
+  // on the non visible portion of the hotseat, or events that reach the hotseat
+  // during an animation.
+  std::unique_ptr<aura::ScopedWindowTargeter> hotseat_window_targeter_;
 
   DISALLOW_COPY_AND_ASSIGN(HotseatWidget);
 };
