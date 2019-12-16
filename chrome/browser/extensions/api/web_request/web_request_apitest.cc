b72c177a0676599b940c5cafc9b3a8fe97d9ae1e
Ilia Samsonov <isamsonov@google.com>
2019-12-16 18:12:51 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=karandeepb@chromium.org
    
    Bug: 1023677
    Change-Id: I028773e89d29f625ca3e7660d81aaaffee9893fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957489
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#725176}

--- chrome/browser/extensions/api/web_request/web_request_apitest.cc
+++ chrome/browser/extensions/api/web_request/web_request_apitest.cc
@@ -2719,7 +2719,7 @@ class ServiceWorkerWebRequestApiTest : public testing::WithParamInterface<bool>,
   }
 };
 
-INSTANTIATE_TEST_SUITE_P(/* No prefix */,
+INSTANTIATE_TEST_SUITE_P(All,
                          ServiceWorkerWebRequestApiTest,
                          ::testing::Bool());
 
