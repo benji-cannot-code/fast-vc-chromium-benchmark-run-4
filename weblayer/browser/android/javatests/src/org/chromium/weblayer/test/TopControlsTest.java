a4fb7ba684cf0e1ea0c7935d96b27e1376a36ae6
Clark DuVall <cduvall@chromium.org>
2019-12-16 18:15:47 +0000

    [WebLayer] Fix top controls reappearing on scroll
    On pages that had a lot of same document navigations, the top controls
    would keep reappearing.
    
    Before: https://screenshot.googleplex.com/oTUd4Omgga4
    After: https://screenshot.googleplex.com/70eygfXeVvR
    
    Bug: 1033733
    Change-Id: I4021c256c524efda93c189c35d04620a5df97b8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966356
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725177}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/TopControlsTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/TopControlsTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.weblayer.test;
 
+import android.os.SystemClock;
 import android.support.test.filters.SmallTest;
 import android.support.v4.app.Fragment;
 import android.view.View;
@@ -88,6 +89,10 @@ public class TopControlsTest {
         // Get the size of the page.
         mInitialVisiblePageHeight = getVisiblePageHeight();
         Assert.assertTrue(mInitialVisiblePageHeight > 0);
+
+        // TODO(http://crbug.com/1034147): Remove this and find a way to pass command line arguments
+        // to the implementation.
+        SystemClock.sleep(3100);
 
         // Move by the size of the top-controls.
         EventUtils.simulateDragFromCenterOfView(
