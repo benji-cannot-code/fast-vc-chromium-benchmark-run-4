ae99126d0b39179540af73e4bf83ee4c5380d1d3
Tibor Goldschwendt <tiborg@chromium.org>
2019-12-16 20:03:46 +0000

    Deprecate several Chrome theme metrics as they are no longer needed
    Specifically, this CL deprecates:
    * the histograms:
      * AutogeneratedTheme.ColorGenerationTime
      * ChromeColors.AppliedColor
      * ChromeColors.ChangesConfirmed
      * ChromeColors.RevertReason
    * And the user actions:
      * ChromeColors_ColorApplied
      * ChromeColors_DefaultApplied
      ChromeColors_ThemeUninstalled
    
    Bug: 1032327
    Change-Id: Idef876f3e224fbe189190a25902e0c777ae11a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966595
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725230}

--- chrome/browser/search/chrome_colors/chrome_colors_service.cc
+++ chrome/browser/search/chrome_colors/chrome_colors_service.cc
@@ -11,15 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/search/generated_colors_info.h"
 #include "chrome/common/search/selected_colors_info.h"
 
-namespace {
-
-// Records whether current theme changes are confirmed or reverted.
-void RecordChangesConfirmedHistogram(bool confirmed) {
-  UMA_HISTOGRAM_BOOLEAN("ChromeColors.ChangesConfirmed", confirmed);
-}
-
-}  // namespace
-
 namespace chrome_colors {
 
 ChromeColorsService::ChromeColorsService(Profile* profile)
@@ -89,7 +80,6 @@ void ChromeColorsService::ConfirmThemeChanges() {
     return;
   prev_theme_reinstaller_ = nullptr;
   dialog_tab_ = nullptr;
-  RecordChangesConfirmedHistogram(true);
 }
 
 void ChromeColorsService::RevertThemeChangesWithReason(RevertReason reason) {
@@ -97,8 +87,6 @@ void ChromeColorsService::RevertThemeChangesWithReason(RevertReason reason) {
     prev_theme_reinstaller_->Reinstall();
     prev_theme_reinstaller_ = nullptr;
     dialog_tab_ = nullptr;
-    UMA_HISTOGRAM_ENUMERATION("ChromeColors.RevertReason", reason);
-    RecordChangesConfirmedHistogram(false);
   }
 }
 
