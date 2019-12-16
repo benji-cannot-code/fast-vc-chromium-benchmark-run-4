8e8246e7d50587612736689dc9614161302b94ac
Hirokazu Honda <hiroh@chromium.org>
2019-12-16 00:44:46 +0000

    media/gpu/vaapi: Rename AcceleratedVideoDecoder's Accelerator for Vaapi
    The names of AcceleratedVideoDecder's Accelerators for Vaapi doesn't
    describe what they do well, e.g., VaapiH264Accelerator. This renames
    the classes, e.g., H264VaapiVideoDecoderDelegate.
    Besides, this add a class of their common parts,
    VaapiVideoDecoderDelegate, which has SurfaceHandler and VaapiWrapper.
    
    Bug: 1022246
    Test: build chromium
    Change-Id: I90b6b760844f249f61d260e50d226f9c6ab1b9ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961842
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#724988}

--- media/gpu/vaapi/vp8_vaapi_video_decoder_delegate.h
+++ media/gpu/vaapi/vp8_vaapi_video_decoder_delegate.h
@@ -3,25 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_GPU_VAAPI_VAAPI_VP8_ACCELERATOR_H_
-#define MEDIA_GPU_VAAPI_VAAPI_VP8_ACCELERATOR_H_
+#ifndef MEDIA_GPU_VAAPI_VP8_VAAPI_VIDEO_DECODER_DELEGATE_H_
+#define MEDIA_GPU_VAAPI_VP8_VAAPI_VIDEO_DECODER_DELEGATE_H_
 
+#include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
+#include "media/gpu/vaapi/vaapi_video_decoder_delegate.h"
 #include "media/gpu/vp8_decoder.h"
 #include "media/parsers/vp8_parser.h"
 
 namespace media {
 
-template <class T> class DecodeSurfaceHandler;
-class VASurface;
 class VP8Picture;
-class VaapiWrapper;
 
-class VaapiVP8Accelerator : public VP8Decoder::VP8Accelerator {
+class VP8VaapiVideoDecoderDelegate : public VP8Decoder::VP8Accelerator,
+                                     public VaapiVideoDecoderDelegate {
  public:
-  VaapiVP8Accelerator(DecodeSurfaceHandler<VASurface>* vaapi_dec,
-                      scoped_refptr<VaapiWrapper> vaapi_wrapper);
-  ~VaapiVP8Accelerator() override;
+  VP8VaapiVideoDecoderDelegate(DecodeSurfaceHandler<VASurface>* const vaapi_dec,
+                               scoped_refptr<VaapiWrapper> vaapi_wrapper);
+
+  ~VP8VaapiVideoDecoderDelegate() override;
 
   // VP8Decoder::VP8Accelerator implementation.
   scoped_refptr<VP8Picture> CreateVP8Picture() override;
@@ -29,15 +30,9 @@ class VaapiVP8Accelerator : public VP8Decoder::VP8Accelerator {
                     const Vp8ReferenceFrameVector& reference_frames) override;
   bool OutputPicture(const scoped_refptr<VP8Picture>& pic) override;
 
- private:
-  const scoped_refptr<VaapiWrapper> vaapi_wrapper_;
-  DecodeSurfaceHandler<VASurface>* vaapi_dec_;
-
-  SEQUENCE_CHECKER(sequence_checker_);
-
-  DISALLOW_COPY_AND_ASSIGN(VaapiVP8Accelerator);
+  DISALLOW_COPY_AND_ASSIGN(VP8VaapiVideoDecoderDelegate);
 };
 
 }  // namespace media
 
-#endif  // MEDIA_GPU_VAAPI_VAAPI_VP8_ACCELERATOR_H_
+#endif  // MEDIA_GPU_VAAPI_VP8_VAAPI_VIDEO_DECODER_DELEGATE_H_
