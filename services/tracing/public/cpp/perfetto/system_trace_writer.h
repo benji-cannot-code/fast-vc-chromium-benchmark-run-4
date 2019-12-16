af852f27a6f9e8d15c5eeea581847057f3b18b99
Eric Seckler <eseckler@chromium.org>
2019-12-16 11:06:53 +0000

    perfetto: Set sequence flags & timestamps on all packets
    Ensure that Chrome sets SEQ_NEEDS_INCREMENTAL state on packets that
    require incremental state. Also use the sequence flags to indicate
    incremental state reset, and fix missing timestamps on system trace
    event packets.
    
    Bug: 928738
    Change-Id: I0017480017691ca113616d0fb6e66af5220e10ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1964272
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725064}

--- services/tracing/public/cpp/perfetto/system_trace_writer.h
+++ services/tracing/public/cpp/perfetto/system_trace_writer.h
@@ -15,7 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "base/trace_event/trace_event.h"
 #include "services/tracing/public/cpp/perfetto/perfetto_producer.h"
+#include "services/tracing/public/cpp/perfetto/trace_time.h"
 #include "third_party/perfetto/include/perfetto/ext/tracing/core/trace_writer.h"
 #include "third_party/perfetto/protos/perfetto/trace/chrome/chrome_trace_event.pbzero.h"
 #include "third_party/perfetto/protos/perfetto/trace/trace_packet.pbzero.h"
@@ -90,6 +92,9 @@ class COMPONENT_EXPORT(TRACING_CPP) SystemTraceWriter {
       {
         perfetto::TraceWriter::TracePacketHandle trace_packet_handle =
             trace_writer_->NewTracePacket();
+        trace_packet_handle->set_timestamp(
+            TRACE_TIME_TICKS_NOW().since_origin().InNanoseconds());
+        trace_packet_handle->set_timestamp_clock_id(kTraceClockId);
         ChromeEventBundleHandle event_bundle =
             ChromeEventBundleHandle(trace_packet_handle->set_chrome_events());
 
