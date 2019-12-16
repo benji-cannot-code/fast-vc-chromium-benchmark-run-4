fe50f56c4711e5992013ce14b433bf294fc1d025
Kenichi Ishibashi <bashi@chromium.org>
2019-12-16 12:45:00 +0000

    bindings: Deprecate [Constructor] extended attribute
    The code generator will raise an exception when [Constructor] is
    used.
    
    Convert test IDL files to use constructor operations. Also remove
    [NamedConstructor] from TestInterfaceConstructor because we don't
    want to mix constructor operations and [NamedConstructor]. We have
    test cases for [NamedConstructor] in other places like
    TestInterfaceNamedConstructor.
    
    Bug: 999961
    Change-Id: I162bce6b5de32a4f5ca0d2c2863d8e2944ee4ec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969191
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725084}

--- third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_constructor.h
+++ third_party/blink/renderer/bindings/tests/results/core/v8_test_interface_constructor.h
@@ -26,18 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-extern const WrapperTypeInfo v8_test_interface_constructor_constructor_wrapper_type_info;
-
-class V8TestInterfaceConstructorConstructor {
-  STATIC_ONLY(V8TestInterfaceConstructorConstructor);
- public:
-  static v8::Local<v8::FunctionTemplate> DomTemplate(v8::Isolate*, const DOMWrapperWorld&);
-  static void NamedConstructorAttributeGetter(v8::Local<v8::Name> property_name, const v8::PropertyCallbackInfo<v8::Value>& info);
-  static constexpr const WrapperTypeInfo* GetWrapperTypeInfo() {
-    return &v8_test_interface_constructor_constructor_wrapper_type_info;
-  }
-};
-
 CORE_EXPORT extern const WrapperTypeInfo v8_test_interface_constructor_wrapper_type_info;
 
 class V8TestInterfaceConstructor {
