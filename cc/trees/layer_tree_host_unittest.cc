b7beb82e513ef138cbb4bfb2d03f428d5f521f9b
Xianzhu Wang <wangxianzhu@chromium.org>
2019-12-17 00:27:20 +0000

    Revert "Reland "Move fields in cc::Layer::Inputs for layer tree mode only into LayerTreeInputs""
    This reverts commit ba176792ce89970e83b1767fb6a68cb099f0cd1e.
    
    Reason for revert: b/146358579
    
    Original change's description:
    > Reland "Move fields in cc::Layer::Inputs for layer tree mode only into LayerTreeInputs"
    > 
    > This reverts commit 074fbb7f672b607245fed8f3ea416787e9024e97.
    > 
    > Reason for revert: Fixed break by not printing position in
    > cc::Layer::ToString().
    > 
    > Original change's description:
    > > Revert "Move fields in cc::Layer::Inputs for layer tree mode only into LayerTreeInputs"
    > > 
    > > This reverts commit d46e17a7bd41536263e7385045f210c14d9b3ff4.
    > > 
    > > Reason for revert: breaks cc::Layer::ToString()
    > > 
    > > Original change's description:
    > > > Move fields in cc::Layer::Inputs for layer tree mode only into LayerTreeInputs
    > > > 
    > > > This makes clear what fields are for layer tree mode only, and avoid
    > > > unnecessary memory usage of the unused fields in layer tree mode.
    > > > 
    > > > (TBR for passive one line change in ui/compositor/layer.h)
    > > > TBR=flackr@chromium.org
    > > > 
    > > > Change-Id: I711db13c08885038728af71776c02e5d40ab5d86
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965743
    > > > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#725002}
    > > 
    > > TBR=flackr@chromium.org,wangxianzhu@chromium.org,pdr@chromium.org
    > > 
    > > Change-Id: Ie742d5a9baac487564635f5ce214f878c37f104f
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969330
    > > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#725058}
    > 
    > TBR=flackr@chromium.org,wangxianzhu@chromium.org,pdr@chromium.org
    > 
    > Change-Id: Ibe5bc5f35ec7d8efb492cd0fe028c016fcebfb9f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969331
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#725068}
    
    TBR=flackr@chromium.org,wangxianzhu@chromium.org,pdr@chromium.org
    
    Change-Id: I2c80f9ee78ddaee2625b59f691356c52fa4720a6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970304
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725336}

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -1754,6 +1754,7 @@ class LayerTreeHostTestAnimationOpacityMutatedUsingLayerLists
  protected:
   void BeginTest() override {
     Layer* root = layer_tree_host()->root_layer();
+    EXPECT_EQ(1.0f, root->opacity());
     EXPECT_EQ(1.0f, layer_tree_host()
                         ->property_trees()
                         ->effect_tree.FindNodeFromElementId(root->element_id())
@@ -1762,6 +1763,8 @@ class LayerTreeHostTestAnimationOpacityMutatedUsingLayerLists
     layer_tree_host()->SetElementOpacityMutated(root->element_id(),
                                                 ElementListType::ACTIVE, 0.3f);
 
+    // When using layer lists, we don't have to store the opacity on the layer.
+    EXPECT_EQ(1.0f, root->opacity());
     // The opacity should have been set directly on the effect node instead.
     EXPECT_EQ(0.3f, layer_tree_host()
                         ->property_trees()
@@ -1813,6 +1816,7 @@ class LayerTreeHostTestAnimationTransformMutatedUsingLayerLists
  protected:
   void BeginTest() override {
     Layer* root = layer_tree_host()->root_layer();
+    EXPECT_EQ(gfx::Transform(), root->transform());
     EXPECT_EQ(gfx::Transform(),
               layer_tree_host()
                   ->property_trees()
@@ -1824,6 +1828,9 @@ class LayerTreeHostTestAnimationTransformMutatedUsingLayerLists
     layer_tree_host()->SetElementTransformMutated(
         root->element_id(), ElementListType::ACTIVE, expected_transform);
 
+    // When using layer lists, we don't have to store the transform on the
+    // layer.
+    EXPECT_EQ(gfx::Transform(), root->transform());
     // The transform should have been set directly on the transform node
     // instead.
     EXPECT_EQ(expected_transform,
@@ -1866,6 +1873,7 @@ class LayerTreeHostTestAnimationFilterMutatedUsingLayerLists
  protected:
   void BeginTest() override {
     Layer* root = layer_tree_host()->root_layer();
+    EXPECT_EQ(FilterOperations(), root->filters());
     EXPECT_EQ(FilterOperations(),
               layer_tree_host()
                   ->property_trees()
@@ -1877,6 +1885,8 @@ class LayerTreeHostTestAnimationFilterMutatedUsingLayerLists
     layer_tree_host()->SetElementFilterMutated(
         root->element_id(), ElementListType::ACTIVE, filters);
 
+    // When using layer lists, we don't have to store the filters on the layer.
+    EXPECT_EQ(FilterOperations(), root->filters());
     // The filter should have been set directly on the effect node instead.
     EXPECT_EQ(filters,
               layer_tree_host()
