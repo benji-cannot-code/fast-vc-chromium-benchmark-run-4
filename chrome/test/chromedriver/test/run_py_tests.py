3903d2600d79726a856b1f6c5224e5c0cbf7e89e
Lan Wei <lanwei@chromium.org>
2019-12-17 19:28:40 +0000

    Disable ChromeDriverTest.testActionsMultiTouchPoint on Mac
    ChromeDriverTest.testActionsMultiTouchPoint is only flaky on Mac now,
    because Mac does not support touch events, so it is safe to disable
    this touch test on Mac.
    
    Add time delay on testActionsMouseDrag test to avoid flakiness when
    performing actions.
    
    Bug: 1011225, 1033382
    Change-Id: I891516824391c467707453a213a00eb48f43101a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970748
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725634}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -98,6 +98,8 @@ _OS_SPECIFIC_FILTER['mac'] = [
     'MobileEmulationCapabilityTest.testTapElement',
     # https://bugs.chromium.org/p/chromium/issues/detail?id=946023
     'ChromeDriverTest.testWindowFullScreen',
+    # https://bugs.chromium.org/p/chromium/issues/detail?id=1011225
+    'ChromeDriverTest.testActionsMultiTouchPoint',
 ]
 
 _DESKTOP_NEGATIVE_FILTER = [
@@ -881,6 +883,7 @@ class ChromeDriverTest(ChromeDriverBaseTestWithWebServer):
       ],
       'parameters': {'pointerType': 'mouse'},
       'id': 'pointer1'}]})
+    time.sleep(1)
     self._driver.PerformActions(actions)
     rect = target.GetRect()
     self.assertEquals(150, rect['x'])
@@ -898,6 +901,7 @@ class ChromeDriverTest(ChromeDriverBaseTestWithWebServer):
       ],
       'parameters': {'pointerType': 'mouse'},
       'id': 'pointer1'}]})
+    time.sleep(1)
     self._driver.PerformActions(actions)
     rect = target.GetRect()
     self.assertEquals(180, rect['x'])
@@ -917,6 +921,7 @@ class ChromeDriverTest(ChromeDriverBaseTestWithWebServer):
       ],
       'parameters': {'pointerType': 'mouse'},
       'id': 'pointer1'}]})
+    time.sleep(1)
     self._driver.PerformActions(actions)
     rect = target.GetRect()
     self.assertEquals(180, rect['x'])
