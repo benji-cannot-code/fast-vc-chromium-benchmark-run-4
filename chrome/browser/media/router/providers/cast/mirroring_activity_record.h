3cd2acd48f97ee1cc5274dc1a0be35821b6522f6
John Williams <jrw@google.com>
2019-12-17 00:40:18 +0000

    [Native Cast MRP] Fixed media remoting.
    Bug: 972823
    Change-Id: I076daaa06d730987912513c4ce3ab8a35af0a710
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960723
    Commit-Queue: John Williams <jrw@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725337}

--- chrome/browser/media/router/providers/cast/mirroring_activity_record.h
+++ chrome/browser/media/router/providers/cast/mirroring_activity_record.h
@@ -96,6 +96,9 @@ class MirroringActivityRecord : public ActivityRecord,
     kMaxValue = kOffscreenTab,
   };
 
+  void HandleParseJsonResult(const std::string& route_id,
+                             data_decoder::DataDecoder::ValueOrError result);
+
   void StopMirroring();
 
   mojo::Remote<mirroring::mojom::MirroringServiceHost> host_;
