133bca8f69e2228c0fc2a4de554b2f0910b7b18a
Elly Fong-Jones <ellyjones@chromium.org>
2019-12-17 16:58:45 +0000

    sheriff: disable EditDictionaryA11yTest.* on Windows
    These tests are flaky and seemingly always have been.
    
    Bug: 1012370
    Change-Id: Ia69f6f801cef40a1f5bad6e625e031abd9a09fdc
    Tbr: hcarmona@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970267
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725567}

--- chrome/test/data/webui/settings/a11y/edit_dictionary_a11y_test.js
+++ chrome/test/data/webui/settings/a11y/edit_dictionary_a11y_test.js
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Disable since the EDIT_DICTIONARY route does not exist on Mac.
 // TODO(crbug.com/1012370) flaky on Linux b/c assertTrue(!!languagesPage);
-GEN('#if !defined(OS_MACOSX) && !defined(OS_LINUX)');
+// TODO(crbug.com/1012370) flaky on Win the same way
+GEN('#if !defined(OS_MACOSX) && !defined(OS_LINUX) && !defined(OS_WIN)');
 
 // SettingsAccessibilityTest fixture.
 GEN_INCLUDE([
