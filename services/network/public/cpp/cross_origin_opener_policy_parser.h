093aa64095f417bc7aac12b5f8854cdded349266
Arthur Hemery <ahemery@chromium.org>
2019-12-17 14:47:40 +0000

    [security] Move COEP parsing to the network service.
    We added COOP parsing and it makes sense to have COEP parsed in the
    network service as well, for security and consistency reasons.
    
    Change-Id: I5cef31934572dd1fab7774488a29085a20fe3e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1926504
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Arthur Hemery <ahemery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725516}

--- services/network/public/cpp/cross_origin_opener_policy_parser.h
+++ services/network/public/cpp/cross_origin_opener_policy_parser.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace network {
 
-const char kCrossOriginOpenerPolicyHeader[] = "cross-origin-opener-policy";
-
 // Parsing is done following the COOP spec draft:
 // https://gist.github.com/annevk/6f2dd8c79c77123f39797f6bdac43f3e
 // TODO(ahemery): add a fuzzer for the parser, see
