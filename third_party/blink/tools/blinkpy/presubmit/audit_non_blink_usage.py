95c486b39e0ac41bfc7d1e853372d339f81fbaa1
Fergus Dall <sidereal@google.com>
2019-12-17 02:50:27 +0000

    Revert "xpath_grammar.y: Switch to C++ parser"
    This reverts commit 982f6f23057bd2becad4cb219fd655a4dd73d532.
    
    Reason for revert: Appears to have broken linux-gcc-rel. See https://ci.chromium.org/p/chromium/builders/ci/linux-gcc-rel/125378 for first build failure.
    
    Original change's description:
    > xpath_grammar.y: Switch to C++ parser
    > 
    > We used non-standard alloca() in Bison-generated C parser because the
    > semantic union object contains raw pointers of Oilpan objects and they
    > need to be on stack.
    > 
    > We'd like to avoid non-standard alloca(), and would like to manage
    > lifetime of union member objects correctly. So, we switch to
    > Bison-generated C++ parser with 'variant' semantic object. It allocates
    > the stack of semantic objects in the normal heap, and correctly
    > construct and destruct each member objects.  We can use WTF::String and
    > blink::Persistent as members without any hacks.
    > 
    > * rule_bison.py
    >  Replace xpath_grammar_generated.hh with xpath_grammar_generated.h on
    >  preprocessor directives such as #include and #line.
    > 
    > * xml/BUILD.gn
    >  Move xpath_grammar_generated.* and xpath_parser.cc to a separated
    >  target, and it disables chromium-style clang plugin.
    >  chromium-style plugin produces errors on xpath_grammar_generated.h.
    > 
    > * xml/xpath_grammar.y
    >   Switch to C++ parser.
    >   Switch to 'variant' from 'union'.
    >   Add missing |$$ = $1|.
    >   Remove unnecessary DeleteString() calls.
    > 
    > * xml/xpath_parser.cc
    >   Adopt to the Bison-generated C++ parser.
    >   Remove unused |strings_| code.
    > 
    > * audit_non_blink_usage.py
    >   Allow xpathyy:: namespace in core/xml/.
    > 
    > 
    > Change-Id: I1b4f3a224dca058bee83518573d1482259f221bf
    > Bug: 1032320
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961464
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#725348}
    
    TBR=dcheng@chromium.org,tkent@chromium.org
    
    Change-Id: I51e0fe04989c9882ab12ff77be5e13d06017ded6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1032320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1971037
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Commit-Queue: Fergus Dall <sidereal@google.com>
    Cr-Commit-Position: refs/heads/master@{#725388}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -598,12 +598,6 @@ _CONFIG = [
         ],
     },
     {
-        'paths': ['third_party/blink/renderer/core/xml'],
-        'allowed': [
-            'xpathyy::.+',
-        ],
-    },
-    {
         'paths': [
             'third_party/blink/renderer/modules/device_orientation/',
             'third_party/blink/renderer/modules/gamepad/',
