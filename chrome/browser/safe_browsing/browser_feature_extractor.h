b9496750f3f1b0a0bfcdf122ad4a921d19738127
Daniel Rubery <drubery@chromium.org>
2019-12-17 17:14:20 +0000

    Initial pass at removing ClientMalwareRequest
    This CL removes the ClientMalwareRequest proto and the code that
    supported sending it.
    
    Bug: 1029982
    Change-Id: I01d561ae05b6c7865f7a1075b076f9d3e4fb38eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954535
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725572}

--- chrome/browser/safe_browsing/browser_feature_extractor.h
+++ chrome/browser/safe_browsing/browser_feature_extractor.h
@@ -22,12 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
-#include "chrome/browser/safe_browsing/safe_browsing_service.h"
-#include "chrome/browser/safe_browsing/ui_manager.h"
 #include "components/history/core/browser/history_types.h"
-#include "content/public/common/resource_type.h"
+#include "components/security_interstitials/content/unsafe_resource.h"
 #include "url/gurl.h"
-
 
 namespace content {
 class WebContents;
@@ -40,39 +37,11 @@ struct QueryURLResult;
 }
 
 namespace safe_browsing {
-class ClientMalwareRequest;
 class ClientPhishingRequest;
-class ClientSideDetectionHost;
-
-struct IPUrlInfo {
-  // The origin of the final URL for the request to the bad IP address (final =
-  // after redirects).
-  //
-  // The full URL is not available, because in some cases the path
-  // and query may be sanitized away - see https://crbug.com/973885.
-  std::string origin_of_final_url;
-
-  std::string method;
-  std::string referrer;
-  content::ResourceType resource_type;
-
-  IPUrlInfo(const std::string& origin_of_final_url,
-            const std::string& method,
-            const std::string& referrer,
-            const content::ResourceType& resource_type);
-  IPUrlInfo(const IPUrlInfo& other);
-  ~IPUrlInfo();
-};
-
-typedef std::map<std::string, std::vector<IPUrlInfo> > IPUrlMap;
 
 struct BrowseInfo {
   // The URL we're currently browsing.
   GURL url;
-
-  // List of IPv4 and IPv6 addresses from which content was requested
-  // together with the hosts on it, while browsing to the |url|.
-  IPUrlMap ips;
 
   // If a SafeBrowsing interstitial was shown for the current URL
   // this will contain the UnsafeResource struct for that URL.
@@ -104,15 +73,11 @@ class BrowserFeatureExtractor {
   using DoneCallback =
       base::OnceCallback<void(bool feature_extraction_succeeded,
                               std::unique_ptr<ClientPhishingRequest> request)>;
-  using MalwareDoneCallback =
-      base::OnceCallback<void(bool feature_extraction_succeeded,
-                              std::unique_ptr<ClientMalwareRequest> request)>;
 
   // The caller keeps ownership of the tab and host objects and is
   // responsible for ensuring that they stay valid for the entire
   // lifetime of this object.
-  BrowserFeatureExtractor(content::WebContents* tab,
-                          ClientSideDetectionHost* host);
+  explicit BrowserFeatureExtractor(content::WebContents* tab);
 
   // The destructor will cancel any pending requests.
   virtual ~BrowserFeatureExtractor();
@@ -126,16 +91,6 @@ class BrowserFeatureExtractor {
   virtual void ExtractFeatures(const BrowseInfo* info,
                                std::unique_ptr<ClientPhishingRequest> request,
                                DoneCallback callback);
-
-  // Begins extraction of the malware related features.  We take ownership
-  // of the request object until |callback| is called.  Once feature extraction
-  // is complete, |callback| will run on the UI thread.  |info| is not expected
-  // to stay valid after ExtractMalwareFeatures returns.  All IPs stored in
-  // |info| will be cleared by calling this function.
-  virtual void ExtractMalwareFeatures(
-      BrowseInfo* info,
-      std::unique_ptr<ClientMalwareRequest> request,
-      MalwareDoneCallback callback);
 
  private:
   // Synchronous browser feature extraction.
@@ -177,15 +132,7 @@ class BrowserFeatureExtractor {
   // is set it will return true and false otherwise.
   bool GetHistoryService(history::HistoryService** history);
 
-  // Helper function which is called when we're done filtering out benign IPs
-  // on the IO thread.  This function is called on the UI thread.
-  void FinishExtractMalwareFeatures(
-      std::unique_ptr<IPUrlMap> bad_ips,
-      MalwareDoneCallback callback,
-      std::unique_ptr<ClientMalwareRequest> request);
-
   content::WebContents* tab_;
-  ClientSideDetectionHost* host_;
   base::CancelableTaskTracker cancelable_task_tracker_;
   base::WeakPtrFactory<BrowserFeatureExtractor> weak_factory_{this};
 
