e8581333634c3a7acbe80f8ac8fd4cf600688381
Rakib Hasan <rmhasan@google.com>
2019-12-17 03:09:13 +0000

    Revert "media/mojo: Enable to pass VideoEncoderInfo via mojo"
    This reverts commit 7dc7ddb0e41cb9e0ca37a1af46dbc61760022c3a.
    
    Reason for revert:
    webrtc.pause_play_peerconnections and webrtc.anvas_capture_peer_connection benchmarks are failing
    on almost all Android Perf Builders.
    
    https://ci.chromium.org/p/chrome/builders/ci/Android%20Nexus5X%20WebView%20Perf/8596
    
    Original change's description:
    > media/mojo: Enable to pass VideoEncoderInfo via mojo
    >
    > crrev.com/c/1949841 introduces VideoEncoderInfo. The info may be
    > useful for libwebrtc. The goal is to acquire VideoEncoderInfo of
    > hw encoders running in GPU process.
    > This CL is the second step, which enables to pass VideoEncoderInfo
    > via mojo.
    >
    > Bug: 1031965
    > Test: VaapiVideoEncoder is shown in chrome://webrtc-internals
    > Change-Id: I2fe4e83888d06c2f49200ddcf4930884e4e499b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959231
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#724936}
    
    TBR=sandersd@chromium.org,sprang@chromium.org,dominickn@chromium.org,hiroh@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1031965
    Bug: 1034158
    Bug: 1034771
    Change-Id: I1a57ecba9101812a204aa48115804f7381884555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970915
    Commit-Queue: Rakib Hasan <rmhasan@google.com>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/master@{#725397}

--- media/mojo/clients/mojo_video_encode_accelerator_unittest.cc
+++ media/mojo/clients/mojo_video_encode_accelerator_unittest.cc
@@ -36,9 +36,6 @@ class MockMojoVideoEncodeAccelerator : public mojom::VideoEncodeAccelerator {
   MockMojoVideoEncodeAccelerator() = default;
 
   // mojom::VideoEncodeAccelerator impl.
-  void GetEncoderInfo(GetEncoderInfoCallback callback) override {
-    return std::move(callback).Run(media::VideoEncoderInfo());
-  }
   void Initialize(
       const media::VideoEncodeAccelerator::Config& config,
       mojo::PendingRemote<mojom::VideoEncodeAcceleratorClient> client,
