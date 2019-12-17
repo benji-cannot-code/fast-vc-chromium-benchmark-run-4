fe618bfc4b3ea89161bfeeb160e576a68dd2ed47
Peter Marshall <petermarshall@chromium.org>
2019-12-17 09:46:43 +0000

    DevTools: Go back to using both Session and IOSession for messages
    There are potential ordering problems with sending all messages over
    the IO session, as it is not associated with the DevToolsAgent mojo
    interface, like the normal session is.
    
    We don't actually need this part to fix the bug we're trying to fix
    so this CL reverts that part of the change.
    
    Bug: 1010534
    Change-Id: I8509a283dc454e53275e83f60f3f3c1c0c1df1ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967184
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725464}

--- content/browser/devtools/devtools_session.cc
+++ content/browser/devtools/devtools_session.cc
@@ -22,6 +22,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 namespace {
 
+bool ShouldSendOnIO(const std::string& method) {
+  // Keep in sync with WebDevToolsAgent::ShouldInterruptForMethod.
+  // TODO(petermarshall): find a way to share this.
+  return method == "Debugger.pause" || method == "Debugger.setBreakpoint" ||
+         method == "Debugger.setBreakpointByUrl" ||
+         method == "Debugger.removeBreakpoint" ||
+         method == "Debugger.setBreakpointsActive" ||
+         method == "Debugger.getStackTrace" ||
+         method == "Performance.getMetrics" || method == "Page.crash" ||
+         method == "Runtime.terminateExecution" ||
+         method == "Emulation.setScriptExecutionDisabled";
+}
+
 // Async control commands (such as CSS.enable) are idempotant and can
 // be safely replayed in the new render frame host. We will always forward
 // them to the new renderer on cross process navigation. Main rationale for
@@ -279,13 +292,25 @@ void DevToolsSession::DispatchProtocolMessageToAgent(
   auto message_ptr = blink::mojom::DevToolsMessage::New();
   message_ptr->data =
       mojo_base::BigBuffer(base::make_span(message.data(), message.size()));
-  if (io_session_) {
-    TRACE_EVENT_WITH_FLOW2("devtools",
-                           "DevToolsSession::DispatchProtocolMessageToAgent",
-                           call_id, TRACE_EVENT_FLAG_FLOW_OUT, "method",
-                           method.c_str(), "call_id", call_id);
-    io_session_->DispatchProtocolCommand(call_id, method,
-                                         std::move(message_ptr));
+
+  if (ShouldSendOnIO(method)) {
+    if (io_session_) {
+      TRACE_EVENT_WITH_FLOW2(
+          "devtools", "DevToolsSession::DispatchProtocolMessageToAgent on IO",
+          call_id, TRACE_EVENT_FLAG_FLOW_OUT, "method", method.c_str(),
+          "call_id", call_id);
+      io_session_->DispatchProtocolCommand(call_id, method,
+                                           std::move(message_ptr));
+    }
+  } else {
+    if (session_) {
+      TRACE_EVENT_WITH_FLOW2("devtools",
+                             "DevToolsSession::DispatchProtocolMessageToAgent",
+                             call_id, TRACE_EVENT_FLAG_FLOW_OUT, "method",
+                             method.c_str(), "call_id", call_id);
+      session_->DispatchProtocolCommand(call_id, method,
+                                        std::move(message_ptr));
+    }
   }
 }
 
