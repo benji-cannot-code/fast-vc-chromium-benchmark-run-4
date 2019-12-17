e27cd4a7b288346563057e612963bc4d2b14bc8b
Koji Ishii <kojii@chromium.org>
2019-12-17 02:42:07 +0000

    [FragmentItem] Support |SelfInkOverflow|
    This patch supports |SelfInkOverflow| for
    |NGBoxFragmentPainter| when |NGPaintFragment| is not
    available.
    
    Fixes crashes in external/wpt/intersection-observer.
    
    Bug: 982194
    Change-Id: I0831b4eb26a1734f9dc118d49f9241b202c0637f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969179
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725385}

--- third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.h
+++ third_party/blink/renderer/core/paint/ng/ng_box_fragment_painter.h
@@ -241,6 +241,7 @@ class NGBoxFragmentPainter : public BoxPainterBase {
     return display_item_client_;
   }
   const NGBorderEdges& BorderEdges() const;
+  PhysicalRect SelfInkOverflow() const;
 
   const NGPhysicalBoxFragment& box_fragment_;
   const DisplayItemClient& display_item_client_;
