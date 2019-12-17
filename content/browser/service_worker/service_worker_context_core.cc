a6b09a09d2fa61421e925fec8c59659aca53deaf
Yuzu Saijo <yuzus@chromium.org>
2019-12-17 04:13:35 +0000

    [bfcache]Remove ServiceWorkerOnUI dependency
    This CL removes the ServiceWorkerOnUI dependency from BackForward Cache
    feature.
    
    Bug:1034831
    
    Change-Id: Ic39b1dd317c86b06bfed0ee663b4c76cf9b1bfef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969195
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725409}

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -132,8 +132,7 @@ bool IsSameOriginClientContainerHost(
   // iteration, because (1) hosts in BackForwardCache should never be exposed to
   // web as clients and (2) hosts could be in an unknown state after eviction
   // and before deletion.
-  if (IsBackForwardCacheEnabled() &&
-      ServiceWorkerContext::IsServiceWorkerOnUIEnabled()) {
+  if (IsBackForwardCacheEnabled()) {
     if (container_host->IsInBackForwardCache())
       return false;
   }
@@ -150,8 +149,7 @@ bool IsSameOriginWindowClientContainerHost(
   // iteration, because (1) hosts in BackForwardCache should never be exposed to
   // web as clients and (2) hosts could be in an unknown state after eviction
   // and before deletion.
-  if (IsBackForwardCacheEnabled() &&
-      ServiceWorkerContext::IsServiceWorkerOnUIEnabled()) {
+  if (IsBackForwardCacheEnabled()) {
     if (container_host->IsInBackForwardCache())
       return false;
   }
