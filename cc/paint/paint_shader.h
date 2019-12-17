38cb53c55d0e4bbbe3c32e5f9df06c0528a7fe57
Xida Chen <xidachen@chromium.org>
2019-12-17 16:41:22 +0000

    [OT-PW] Make it work with OOP-R
    This CL makes off-thread paint worklet work with OOP-R. OOP-R serialize
    an PaintOp and stores the serialized buffer in a shared memory. GPU
    then reads the memory and de-serialize it.
    
    For off-thread paint worklet, we are producing a DrawImageRectOp. Note
    that this DrawImageRectOp is different than a normal one. It is a
    DrawImageRectOp that contains a DrawRecord that contains multiple
    PaintOps in it. So this CL adds a special code path to serialize it,
    for paint worklet DrawImageRectOp only. The way to serialize it is to
    iterate over all the PaintOps inside the DrawRecord. In this way, we
    do not need to change anything on the GPU de-serialize code.
    
    We also changed the layout tests. We changed some tests, now using
    fillRect instead of strokeRect.
    The problem with strokeRect is that using GPU rasterization could
    easily result in subpixel difference on different platforms. Using
    fillRect will avoid that.
    
    Bug: 1020238
    Change-Id: Ie0aadfe217e462547cc16a273d09ec8a64f055a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957012
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725554}

--- cc/paint/paint_shader.h
+++ cc/paint/paint_shader.h
@@ -206,6 +206,10 @@ class CC_PAINT_EXPORT PaintShader : public SkRefCnt {
                                         SkFilterQuality* raster_quality,
                                         bool* needs_mips) const;
 
+  // Creates a paint record shader for worklet-backed images.
+  sk_sp<PaintShader> CreatePaintWorkletRecord(
+      ImageProvider* image_provider) const;
+
   void SetColorsAndPositions(const SkColor* colors,
                              const SkScalar* positions,
                              int count);
