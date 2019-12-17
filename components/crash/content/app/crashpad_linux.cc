b8715c57f0808ad1e1ff894c5a8f96a2490ddaf5
Joshua Peraza <jperaza@chromium.org>
2019-12-17 01:13:27 +0000

    chromeos: set dump dir for tests
    By default, dumps on chromeos are written to a cryptohome where they
    are inconvenient to access in tests. This flag directs the dumps to
    a different directory, in a way compatible with legacy tests.
    
    Bug: b/145241430
    Change-Id: I45b656456e7d6c62e0482a77c3ec50516064e59c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1946826
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725350}

--- components/crash/content/app/crashpad_linux.cc
+++ components/crash/content/app/crashpad_linux.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/posix/global_descriptors.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "build/branding_buildflags.h"
 #include "components/crash/content/app/crash_reporter_client.h"
 #include "components/crash/content/app/crash_switches.h"
@@ -159,6 +160,11 @@ base::FilePath PlatformCrashpadInitialization(
 
 #if defined(OS_CHROMEOS)
     arguments.push_back("--use-cros-crash-reporter");
+
+    if (crash_reporter_client->IsRunningUnattended()) {
+      arguments.push_back(base::StringPrintf("--minidump-dir-for-tests=%s",
+                                             database_path.value().c_str()));
+    }
 #endif
 
     bool result =
