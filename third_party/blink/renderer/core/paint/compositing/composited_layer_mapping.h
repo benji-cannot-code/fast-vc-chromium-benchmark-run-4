b211321465f84a241791268da26ea0ae1aaca0e0
Philip Rogers <pdr@chromium.org>
2019-12-18 02:28:13 +0000

    Remove CompositedLayerMapping::DestroyGraphicsLayers
    CompositedLayerMapping::DestroyGraphicsLayers destructed some, but not
    all of the GraphicsLayers owned by CompositedLayerMapping. This made it
    difficult to determine if there was a dangling reference to a CLM from
    a GraphicsLayer. This patch removes DestroyGraphicsLayers as all
    GraphicsLayers will be destroyed as part of the CLM destructor.
    
    Bug: 1033240
    Change-Id: I57d69e1b95249864683227c1dec270879ef46004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970422
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725790}

--- third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h
+++ third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h
@@ -324,7 +324,6 @@ class CORE_EXPORT CompositedLayerMapping final : public GraphicsLayerClient {
   void UpdateScrollingLayerGeometry();
 
   void CreatePrimaryGraphicsLayer();
-  void DestroyGraphicsLayers();
 
   std::unique_ptr<GraphicsLayer> CreateGraphicsLayer(
       CompositingReasons,
