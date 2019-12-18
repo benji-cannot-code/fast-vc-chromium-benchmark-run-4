a2408add980209f362a086a719f809b510e03467
Christos Froussios <cfroussios@chromium.org>
2019-12-18 13:53:03 +0000

    [OSCrypt] Add logging of when an encryption key has been overwritten
    We compare a key's creation and last-modified time. A key is intended
    to be permanent and the two timestamps should always be the same.
    
    Bug: 1031797
    Change-Id: Icf40993af528f70bea59d168087e67867a08c8bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970315
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725939}

--- components/os_crypt/key_storage_libsecret.cc
+++ components/os_crypt/key_storage_libsecret.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/time/time.h"
 #include "build/branding_buildflags.h"
 #include "components/os_crypt/libsecret_util_linux.h"
 
@@ -48,6 +49,25 @@ SecretValue* ToSingleSecret(GList* secret_items) {
   SecretValue* secret_value =
       LibsecretLoader::secret_item_get_secret(secret_item);
   return secret_value;
+}
+
+// Checks the timestamps of the secret item and prints findings to logs. We
+// presume that at most one secret item can be present.
+void AnalyseKeyHistory(GList* secret_items) {
+  GList* first = g_list_first(secret_items);
+  if (first == nullptr)
+    return;
+
+  SecretItem* secret_item = static_cast<SecretItem*>(first->data);
+  auto created = base::Time::FromTimeT(
+      LibsecretLoader::secret_item_get_created(secret_item));
+  auto last_modified = base::Time::FromTimeT(
+      LibsecretLoader::secret_item_get_modified(secret_item));
+
+  VLOG(1) << "Libsecret key created: " << created;
+  VLOG(1) << "Libsecret key last modified: " << last_modified;
+  LOG_IF(WARNING, created != last_modified)
+      << "the encryption key has been modified since it was created.";
 }
 
 }  // namespace
@@ -92,6 +112,7 @@ std::string KeyStorageLibsecret::GetKeyImpl() {
       return password;
     return AddRandomPasswordInLibsecret();
   }
+  AnalyseKeyHistory(helper.results());
   std::string password(
       LibsecretLoader::secret_value_get_text(password_libsecret));
   LibsecretLoader::secret_value_unref(password_libsecret);
