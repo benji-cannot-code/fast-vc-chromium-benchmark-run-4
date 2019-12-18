46a2740274e8ba8d86083af9719f855ba13d0d03
Etienne Bergeron <etienneb@chromium.org>
2019-12-18 02:08:32 +0000

    Add graphemes API to RenderText API fuzzer
    The graphemes API has been refactored recently. This CL is adding
    the calls to these API in the fuzzer to increase the coverage of tests.
    
    Bug: 1019913
    Change-Id: Iafb5c73a8700e5ac1b0b2096ccbcdcacd67dea9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970134
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725781}

--- ui/gfx/render_text_api_fuzzer.cc
+++ ui/gfx/render_text_api_fuzzer.cc
@@ -70,7 +70,9 @@ enum class RenderTextAPI {
   kApplyWeight,
   kSetDirectionalityMode,
   kSetElideBehavior,
-  kMaxValue = kSetElideBehavior
+  kIsGraphemeBoundary,
+  kIndexOfAdjacentGrapheme,
+  kMaxValue = kIndexOfAdjacentGrapheme
 };
 
 gfx::DirectionalityMode ConsumeDirectionalityMode(FuzzedDataProvider* fdp) {
@@ -155,6 +157,16 @@ gfx::ElideBehavior ConsumeElideBehavior(FuzzedDataProvider* fdp) {
       return gfx::FADE_TAIL;
     default:
       return gfx::NO_ELIDE;
+  }
+}
+
+gfx::LogicalCursorDirection ConsumeLogicalCursorDirection(
+    FuzzedDataProvider* fdp) {
+  switch (fdp->ConsumeIntegralInRange(0, 1)) {
+    case 0:
+      return gfx::CURSOR_BACKWARD;
+    default:
+      return gfx::CURSOR_FORWARD;
   }
 }
 
@@ -302,6 +314,19 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       case RenderTextAPI::kSetElideBehavior:
         render_text->SetElideBehavior(ConsumeElideBehavior(&fdp));
         break;
+
+      case RenderTextAPI::kIsGraphemeBoundary:
+        render_text->IsGraphemeBoundary(fdp.ConsumeIntegralInRange<size_t>(
+            0, render_text->text().length()));
+        break;
+
+      case RenderTextAPI::kIndexOfAdjacentGrapheme: {
+        size_t index = render_text->IndexOfAdjacentGrapheme(
+            fdp.ConsumeIntegralInRange<size_t>(0, render_text->text().length()),
+            ConsumeLogicalCursorDirection(&fdp));
+        bool is_grapheme = render_text->IsGraphemeBoundary(index);
+        DCHECK(is_grapheme);
+      } break;
     }
   }
 
