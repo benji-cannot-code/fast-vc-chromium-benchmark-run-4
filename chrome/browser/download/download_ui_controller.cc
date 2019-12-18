93497df644e636ffeb3e2da141a5d5230301af74
Min Qin <qinmin@chromium.org>
2019-12-18 03:20:50 +0000

    Canceling a download should close blank tabs triggered by it
    This CL fixes an issue that cancelling a download goesn't cause
    a blank tab to be closed. On desktop, this is done in
    DownloadUIController. This CL makes Android to go through the
    same code logic.
    
    BUG=1034608
    
    Change-Id: I08b7723368086c33e5f337e8a9915c4670b385dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972241
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725810}

--- chrome/browser/download/download_ui_controller.cc
+++ chrome/browser/download/download_ui_controller.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_delegate.h"
 
 #if defined(OS_ANDROID)
+#include "chrome/browser/download/android/download_controller.h"
 #include "chrome/browser/download/android/download_controller_base.h"
 #else
 #include "chrome/browser/profiles/profile.h"
@@ -183,10 +184,12 @@ void DownloadUIController::OnDownloadUpdated(content::DownloadManager* manager,
       item->GetState() != download::DownloadItem::CANCELLED)
     return;
 
-#if !defined(OS_ANDROID)
   content::WebContents* web_contents =
       content::DownloadItemUtils::GetWebContents(item);
   if (web_contents) {
+#if defined(OS_ANDROID)
+    DownloadController::CloseTabIfEmpty(web_contents);
+#else
     Browser* browser = chrome::FindBrowserWithWebContents(web_contents);
     // If the download occurs in a new tab, and it's not a save page
     // download (started before initial navigation completed) close it.
@@ -200,8 +203,8 @@ void DownloadUIController::OnDownloadUpdated(content::DownloadManager* manager,
         !item->IsSavePackageDownload()) {
       web_contents->Close();
     }
+#endif  // defined(OS_ANDROID)
   }
-#endif
 
   if (item->GetState() == download::DownloadItem::CANCELLED)
     return;
