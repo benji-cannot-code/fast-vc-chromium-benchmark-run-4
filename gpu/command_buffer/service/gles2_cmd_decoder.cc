67458634ca006177b3349b8c0438fca04cdf5a55
Kramer Ge <fangzhoug@chromium.org>
2019-12-18 04:09:51 +0000

    gpu::gles2::BackRenderbuffer::Invalidate() calls TrackMemFree
    Resources related to a lost GL context will be freed and handled by the
    driver. We should not be stuck with allocated memory. The memory tracker
    should reflect it.
    
    Bug: 1035034
    Change-Id: If7c510d8f962e7372a864b7ea6be84f51a62e893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972247
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725822}

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -3363,6 +3363,8 @@ void BackRenderbuffer::Destroy() {
 
 void BackRenderbuffer::Invalidate() {
   id_ = 0;
+  memory_tracker_.TrackMemFree(bytes_allocated_);
+  bytes_allocated_ = 0;
 }
 
 BackFramebuffer::BackFramebuffer(GLES2DecoderImpl* decoder)
