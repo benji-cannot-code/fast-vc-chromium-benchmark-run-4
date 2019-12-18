0893d8e162bc9bbcba7c5e5dfc7a6d8f1d6a911b
sczs <sczs@chromium.org>
2019-12-18 18:50:44 +0000

    [ios] Improves Banner Layout for Larger Fonts.
    -Increases the Compresion rate for the Title and Modal Buttons so they're
    not compressed.
    -Increases the max banner height by 30 pts.
    
    Bug: 1032995,1032621
    Change-Id: I87233e835f18f83d4dbdda8349af2bd322118947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969521
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726035}

--- ios/chrome/browser/ui/infobars/presentation/infobar_banner_presentation_controller.mm
+++ ios/chrome/browser/ui/infobars/presentation/infobar_banner_presentation_controller.mm
@@ -18,7 +18,7 @@ const CGFloat kContainerHorizontalPadding = 8;
 // The presented view maximum width.
 const CGFloat kContainerMaxWidth = 398;
 // The presented view maximum height.
-const CGFloat kContainerMaxHeight = 200;
+const CGFloat kContainerMaxHeight = 230;
 }
 
 @implementation InfobarBannerPresentationController
