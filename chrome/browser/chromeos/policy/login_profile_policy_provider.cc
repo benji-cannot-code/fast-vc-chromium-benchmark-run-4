5bcd2f6456a253bf0e62ea8eeb29a3d40c88c3a0
Amr Aboelkher <amraboelkher@google.com>
2019-12-18 12:49:48 +0000

    Device login policy for controlling the mouse primary button
    A device policy which controls the mouse primary button,
    whether its the right or the left button on the login screen.
    
    Test:
    1. Set the login_screen_primary_mouse_button_switch policy
    to True, check that right click is the primary button.
    2. Set the policy to False, check that left button is the
    primary key.
    
    Bug: 956373, 552439
    Change-Id: Ia8b949745f06e4c7016c403df44ea06364e36db3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1930803
    Commit-Queue: Amr Aboelkher <amraboelkher@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725929}

--- chrome/browser/chromeos/policy/login_profile_policy_provider.cc
+++ chrome/browser/chromeos/policy/login_profile_policy_provider.cc
@@ -58,6 +58,8 @@ const DevicePolicyToUserPolicyMapEntry kDevicePoliciesWithPolicyOptionsMap[] = {
     {key::kDeviceLoginScreenScreenMagnifierType, key::kScreenMagnifierType},
     {key::kDeviceLoginScreenShowOptionsInSystemTrayMenu,
      key::kShowAccessibilityOptionsInSystemTrayMenu},
+    {key::kDeviceLoginScreenPrimaryMouseButtonSwitch,
+     key::kPrimaryMouseButtonSwitch},
 };
 
 const DevicePolicyToUserPolicyMapEntry kRecommendedDevicePoliciesMap[] = {
