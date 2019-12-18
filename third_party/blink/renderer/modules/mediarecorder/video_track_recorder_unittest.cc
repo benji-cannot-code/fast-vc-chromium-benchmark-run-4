1bc30f5b0f5322dae50c912bc8d8b8e5dfc253dd
Ilia Samsonov <isamsonov@google.com>
2019-12-18 22:32:03 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=guidou@chromium.org
    
    Bug: 1023677
    Change-Id: I31c5dfab9cfb46fa70639b5a1f1f5a04a1bce2ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957503
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726133}

--- third_party/blink/renderer/modules/mediarecorder/video_track_recorder_unittest.cc
+++ third_party/blink/renderer/modules/mediarecorder/video_track_recorder_unittest.cc
@@ -605,7 +605,7 @@ TEST_F(VideoTrackRecorderPassthroughTest, PausesAndResumes) {
                                                        base::TimeTicks::Now());
 }
 
-INSTANTIATE_TEST_SUITE_P(,
+INSTANTIATE_TEST_SUITE_P(All,
                          VideoTrackRecorderPassthroughTest,
                          ValuesIn(kTrackRecorderTestCodec));
 
