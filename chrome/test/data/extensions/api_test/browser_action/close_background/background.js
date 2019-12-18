6844c955cca74f0fa1136e15d7cd9bb5c9540259
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-12-18 18:17:25 +0000

    [Extensions] Possibly de-flake BrowserActionApiTest.CloseBackgroundPage
    Improve and possibly de-flake the browser test
    BrowserActionApiTest, CloseBackgroundPage. This includes:
    - Moving away from the content::NotificationService system to an
      ExtensionHostObserver; this ensures we receive events for the right
      ExtensionHost.
    - Waiting for the setBadgeText() call to finish before closing the
      background page.
    
    Neither of these is guaranteed to fix the problem (which I was unable
    to reproduce locally), but they're still desirable, so may as well give
    it a shot.
    
    Bug: 1035075
    Change-Id: I6b24d64d7ddd95b46bb26aae411e8a7ce269e3d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972292
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726020}

--- chrome/test/data/extensions/api_test/browser_action/close_background/background.js
+++ chrome/test/data/extensions/api_test/browser_action/close_background/background.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.browserAction.setBadgeText({text:"X"});
+  chrome.browserAction.setBadgeText({text: 'X'}, () => {
     window.close();
+  });
 });
