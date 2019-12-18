9d617432fb3b50c2c73a1f1083d06e388291e32d
Zentaro Kavanagh <zentaro@chromium.org>
2019-12-18 03:00:02 +0000

    Revert "Ambient EQ: Make it enabled by default"
    This reverts commit f9d31381b2cce03e5444d2e4040ca21c121d441c.
    
    Reason for revert: Needs additional guard on other devices
    
    Original change's description:
    > Ambient EQ: Make it enabled by default
    > 
    > - On devices that support AEQ enable it by default.
    > - Turn off AEQ by default in tests because most nightlight tests
    >   need it off to verify the night light colors.
    > 
    > Bug: 1034068
    > Test: manual
    > Change-Id: I24a87cd9c73c42474086b3955952f7e8670e7f93
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966530
    > Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#725752}
    
    TBR=jamescook@chromium.org,dcastagna@chromium.org,zentaro@chromium.org
    
    Change-Id: I393fd71797e13a4aeb9f1314331ea6e3459b69bf
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1034068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972415
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725801}

--- ash/system/night_light/night_light_controller_impl.cc
+++ ash/system/night_light/night_light_controller_impl.cc
@@ -421,7 +421,7 @@ void NightLightControllerImpl::RegisterProfilePrefs(
                                 kDefaultStartTimeOffsetMinutes);
   registry->RegisterIntegerPref(prefs::kNightLightCustomEndTime,
                                 kDefaultEndTimeOffsetMinutes);
-  registry->RegisterBooleanPref(prefs::kAmbientColorEnabled, true);
+  registry->RegisterBooleanPref(prefs::kAmbientColorEnabled, false);
   registry->RegisterBooleanPref(prefs::kAutoNightLightNotificationDismissed,
                                 false);
 
--- ash/system/night_light/night_light_controller_unittest.cc
+++ ash/system/night_light/night_light_controller_unittest.cc
@@ -193,9 +193,6 @@ class NightLightTest : public NoSessionAshTestBase {
 
     // Simulate user 1 login.
     SwitchActiveUser(kUser1Email);
-
-    // Start with ambient color disabled.
-    SetAmbientColorEnabled(false);
   }
 
   void CreateTestUserSessions() {
@@ -212,10 +209,6 @@ class NightLightTest : public NoSessionAshTestBase {
   void SetNightLightEnabled(bool enabled) {
     GetController()->SetEnabled(
         enabled, NightLightControllerImpl::AnimationDuration::kShort);
-  }
-
-  void SetAmbientColorEnabled(bool enabled) {
-    GetController()->SetAmbientColorEnabled(enabled);
   }
 
   // Simulate powerd sending multiple times an ambient temperature of
@@ -365,8 +358,6 @@ TEST_F(NightLightTest, TestNightLightWithDisplayConfigurationChanges) {
 TEST_F(NightLightTest, TestUserSwitchAndSettingsPersistence) {
   // Test start with user1 logged in.
   NightLightControllerImpl* controller = GetController();
-  controller->SetAmbientColorEnabled(false);
-  EXPECT_FALSE(controller->GetAmbientColorEnabled());
   SetNightLightEnabled(true);
   EXPECT_TRUE(controller->GetEnabled());
   const float user1_temperature = 0.8f;
@@ -376,7 +367,6 @@ TEST_F(NightLightTest, TestUserSwitchAndSettingsPersistence) {
 
   // Switch to user 2, and expect NightLight to be disabled.
   SwitchActiveUser(kUser2Email);
-  SetAmbientColorEnabled(false);
   EXPECT_FALSE(controller->GetEnabled());
   // Changing user_2's color temperature shouldn't affect user_1's settings.
   const float user2_temperature = 0.2f;
