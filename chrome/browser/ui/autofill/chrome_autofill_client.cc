0a364924181b0fafd518388d7797de7611a51148
siyua <siyua@chromium.org>
2019-12-18 01:58:50 +0000

    [CloudToken] Add card selection dialog
    Mock: https://docs.google.com/presentation/d/1gKUmVZP3SmAOzMdVJtG7LOi114xXJ2BiAwPKni0r7UM/edit#slide=id.g63170f4a54_0_0
    
    Content Matrix: go/token-cm
    
    This dialog will be shown when user accept the autofill popup
    "Use a virtual card number..." suggestion. This dialog will list all
    available credit cards.
    
    Bug: 1020740
    Change-Id: Iaa0ca9fd7c490e123e9a29ef47ed0e661e2e46ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961220
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/master@{#725777}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else  // !OS_ANDROID
 #include "chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/save_upi_bubble_controller_impl.h"
+#include "chrome/browser/ui/autofill/payments/virtual_card_selection_dialog_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/webauthn_dialog_controller_impl.h"
 #include "chrome/browser/ui/autofill/payments/webauthn_dialog_state.h"
 #include "chrome/browser/ui/autofill/payments/webauthn_dialog_view.h"
@@ -328,6 +329,15 @@ void ChromeAutofillClient::ConfirmSaveUpiIdLocally(
   autofill::SaveUPIBubbleControllerImpl* controller =
       autofill::SaveUPIBubbleControllerImpl::FromWebContents(web_contents());
   controller->OfferUpiIdLocalSave(upi_id, std::move(callback));
+}
+
+void ChromeAutofillClient::OfferVirtualCardOptions(
+    const std::vector<CreditCard*>& candidates,
+    base::OnceCallback<void(const std::string&)> callback) {
+  VirtualCardSelectionDialogControllerImpl::CreateForWebContents(
+      web_contents());
+  VirtualCardSelectionDialogControllerImpl::FromWebContents(web_contents())
+      ->ShowDialog(candidates, std::move(callback));
 }
 #endif
 
