3de996a34737eea53cdcf7766eb94f83338e08a7
Matt Menke <mmenke@chromium.org>
2019-12-18 13:36:10 +0000

    Remove use of URLRequestStatus in http_bridge.
    It wasn't being used meaningfully, and we want to remove the class.
    
    Bug: 651119
    Change-Id: Id4bbde9e44ce2b59f3d1b2bd0936b4938d73c0f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972691
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725937}

--- components/sync/engine/net/http_bridge.cc
+++ components/sync/engine/net/http_bridge.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_headers.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/static_http_user_agent_settings.h"
-#include "net/url_request/url_request_status.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "third_party/zlib/google/compression_utils.h"
 
@@ -384,12 +383,10 @@ void HttpBridge::OnURLLoadCompleteInternal(
 
   if (fetch_state_.request_succeeded)
     LogTimeout(false);
-  base::UmaHistogramSparse(
-      "Sync.URLFetchResponse",
-      fetch_state_.request_succeeded
-          ? fetch_state_.http_status_code
-          : net::URLRequestStatus::FromError(fetch_state_.net_error_code)
-                .ToNetError());
+  base::UmaHistogramSparse("Sync.URLFetchResponse",
+                           fetch_state_.request_succeeded
+                               ? fetch_state_.http_status_code
+                               : fetch_state_.net_error_code);
   UMA_HISTOGRAM_LONG_TIMES("Sync.URLFetchTime",
                            fetch_state_.end_time - fetch_state_.start_time);
 
