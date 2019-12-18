791e4acb284972285d7c3ec3e050c78b82c1acdb
Brandon Wylie <wylieb@chromium.org>
2019-12-18 19:56:06 +0000

    Account for the padding that's built into the Google G SVG
    UX provided me an asset that makes the specialty drawing logic
    obsolete. Replacing that drawing logic with the xml drawable.
    
    Bug: 1020774
    Change-Id: I3683c79a61c67764996da94da0b70701b405beee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954933
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ender <ender@google.com>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726061}

--- chrome/android/junit/src/org/chromium/chrome/browser/omnibox/status/StatusMediatorUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omnibox/status/StatusMediatorUnitTest.java
@@ -95,8 +95,8 @@ public final class StatusMediatorUnitTest {
         mMediator.setUrlHasFocus(true);
         mMediator.setShowIconsWhenUrlFocused(true);
         mMediator.updateSearchEngineStatusIcon(true, true, TEST_SEARCH_URL);
-        Assert.assertEquals(TEST_SEARCH_URL,
-                mModel.get(StatusProperties.STATUS_ICON_RESOURCE).getIconIdentifierForTesting());
+        Assert.assertEquals(R.drawable.ic_logo_googleg_20dp,
+                mModel.get(StatusProperties.STATUS_ICON_RESOURCE).getIconResForTesting());
     }
 
     @Test
@@ -147,8 +147,8 @@ public final class StatusMediatorUnitTest {
         mMediator.setShowIconsWhenUrlFocused(true);
         mMediator.setUrlFocusChangePercent(1f);
         mMediator.updateSearchEngineStatusIcon(true, true, TEST_SEARCH_URL);
-        Assert.assertEquals(TEST_SEARCH_URL,
-                mModel.get(StatusProperties.STATUS_ICON_RESOURCE).getIconIdentifierForTesting());
+        Assert.assertEquals(R.drawable.ic_logo_googleg_20dp,
+                mModel.get(StatusProperties.STATUS_ICON_RESOURCE).getIconResForTesting());
     }
 
     @Test
@@ -312,8 +312,8 @@ public final class StatusMediatorUnitTest {
         mMediator.updateSearchEngineStatusIcon(true, true, TEST_SEARCH_URL);
 
         Assert.assertTrue(mMediator.maybeUpdateStatusIconForSearchEngineIcon());
-        Assert.assertEquals(TEST_SEARCH_URL,
-                mModel.get(StatusProperties.STATUS_ICON_RESOURCE).getIconIdentifierForTesting());
+        Assert.assertEquals(R.drawable.ic_logo_googleg_20dp,
+                mModel.get(StatusProperties.STATUS_ICON_RESOURCE).getIconResForTesting());
     }
 
     @Test
