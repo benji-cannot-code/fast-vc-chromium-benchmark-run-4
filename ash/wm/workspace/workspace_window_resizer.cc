12e413767d8128e8c265a8620c629bd386b3cebd
Avery Musbach <amusbach@chromium.org>
2019-12-18 00:20:35 +0000

    workspace: Remove TwoStepEdgeCycler
    TwoStepEdgeCycler is currently not responsible for any functionality
    whatsoever. For its original purpose, see:
    https://codereview.chromium.org/1127133003
    
    Bug: None
    Change-Id: I78eca6fe7c48920738f41072266de581800bcaba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972574
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725744}

--- ash/wm/workspace/workspace_window_resizer.cc
+++ ash/wm/workspace/workspace_window_resizer.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_util.h"
 #include "ash/wm/wm_event.h"
 #include "ash/wm/workspace/phantom_window_controller.h"
-#include "ash/wm/workspace/two_step_edge_cycler.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/user_metrics.h"
 #include "ui/aura/client/aura_constants.h"
@@ -472,7 +471,6 @@ void WorkspaceWindowResizer::Drag(const gfx::Point& location_in_parent,
   } else {
     snap_type_ = SNAP_NONE;
     snap_phantom_window_controller_.reset();
-    edge_cycler_.reset();
   }
 }
 
@@ -1047,7 +1045,6 @@ void WorkspaceWindowResizer::UpdateSnapPhantomWindow(const gfx::Point& location,
   snap_type_ = GetSnapType(location);
   if (snap_type_ == SNAP_NONE || snap_type_ != last_type) {
     snap_phantom_window_controller_.reset();
-    edge_cycler_.reset();
     if (snap_type_ == SNAP_NONE)
       return;
   }
@@ -1057,16 +1054,7 @@ void WorkspaceWindowResizer::UpdateSnapPhantomWindow(const gfx::Point& location,
   if (!can_snap) {
     snap_type_ = SNAP_NONE;
     snap_phantom_window_controller_.reset();
-    edge_cycler_.reset();
     return;
-  }
-  if (!edge_cycler_) {
-    edge_cycler_.reset(new TwoStepEdgeCycler(
-        location, snap_type_ == SNAP_LEFT
-                      ? TwoStepEdgeCycler::DIRECTION_LEFT
-                      : TwoStepEdgeCycler::DIRECTION_RIGHT));
-  } else {
-    edge_cycler_->OnMove(location);
   }
 
   // Update phantom window with snapped guide bounds.
--- ash/wm/workspace/workspace_window_resizer.h
+++ ash/wm/workspace/workspace_window_resizer.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 class PhantomWindowController;
-class TwoStepEdgeCycler;
 class WindowSize;
 class WindowState;
 
@@ -196,10 +195,6 @@ class ASH_EXPORT WorkspaceWindowResizer : public WindowResizer {
   // Gives a previews of where the the window will end up. Only used if there
   // is a grid and the caption is being dragged.
   std::unique_ptr<PhantomWindowController> snap_phantom_window_controller_;
-
-  // Used to determine whether the window should be snapped when the user drags
-  // a window to the edge of the screen.
-  std::unique_ptr<TwoStepEdgeCycler> edge_cycler_;
 
   // The edge to which the window should be snapped to at the end of the drag.
   SnapType snap_type_;
