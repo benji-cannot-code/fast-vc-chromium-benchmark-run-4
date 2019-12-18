b9ac261b9294b608b69cada8427a8f5521cda0ba
Avery Musbach <amusbach@chromium.org>
2019-12-18 22:00:45 +0000

    wm: Make it clear that certain functions are only for clamshell mode
    Bug: None
    Change-Id: I7a48a3a9664d8ce5220284c77d9f0535248f26db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974892
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726122}

--- ash/wm/window_positioning_utils.cc
+++ ash/wm/window_positioning_utils.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/shell_state.h"
 #include "ash/wm/system_modal_container_layout_manager.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_properties.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/window_util.h"
@@ -87,6 +88,7 @@ void AdjustBoundsToEnsureMinimumWindowVisibility(const gfx::Rect& visible_area,
 }
 
 gfx::Rect GetDefaultLeftSnappedWindowBoundsInParent(aura::Window* window) {
+  DCHECK(!Shell::Get()->tablet_mode_controller()->InTabletMode());
   gfx::Rect work_area_in_parent(
       screen_util::GetDisplayWorkAreaBoundsInParent(window));
   const int middle = work_area_in_parent.CenterPoint().x();
@@ -97,6 +99,7 @@ gfx::Rect GetDefaultLeftSnappedWindowBoundsInParent(aura::Window* window) {
 }
 
 gfx::Rect GetDefaultRightSnappedWindowBoundsInParent(aura::Window* window) {
+  DCHECK(!Shell::Get()->tablet_mode_controller()->InTabletMode());
   gfx::Rect work_area_in_parent(
       screen_util::GetDisplayWorkAreaBoundsInParent(window));
   const int middle = work_area_in_parent.CenterPoint().x();
