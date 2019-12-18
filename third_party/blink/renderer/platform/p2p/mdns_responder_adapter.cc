03e57c6db9707a080e43c1466ae4cb7f5502bbb4
Antonio Gomes <tonikitoo@igalia.com>
2019-12-18 23:26:30 +0000

    Migrate MdnsResponderAdapter and SocketDispatcher away from base::Bind
    ... to use the corresponding WTF constructions.
    
    BUG=787254
    R=haraken@chromium.org
    
    Change-Id: I964c991d53e1b28e93bce5f5fc9c26a0f7d29afc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1973993
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726148}

--- third_party/blink/renderer/platform/p2p/mdns_responder_adapter.cc
+++ third_party/blink/renderer/platform/p2p/mdns_responder_adapter.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/bind.h"
 #include "jingle/glue/utils.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "net/base/ip_address.h"
@@ -15,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/mdns_responder.mojom-blink.h"
 #include "third_party/blink/public/common/thread_safe_browser_interface_broker_proxy.h"
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/webrtc/rtc_base/ip_address.h"
 
@@ -57,14 +57,14 @@ void MdnsResponderAdapter::CreateNameForAddress(const rtc::IPAddress& addr,
                                                 NameCreatedCallback callback) {
   shared_remote_client_->CreateNameForAddress(
       jingle_glue::RtcIPAddressToNetIPAddress(addr),
-      base::BindOnce(&OnNameCreatedForAddress, callback, addr));
+      WTF::Bind(&OnNameCreatedForAddress, callback, addr));
 }
 
 void MdnsResponderAdapter::RemoveNameForAddress(const rtc::IPAddress& addr,
                                                 NameRemovedCallback callback) {
   shared_remote_client_->RemoveNameForAddress(
       jingle_glue::RtcIPAddressToNetIPAddress(addr),
-      base::BindOnce(&OnNameRemovedForAddress, callback));
+      WTF::Bind(&OnNameRemovedForAddress, callback));
 }
 
 }  // namespace blink
