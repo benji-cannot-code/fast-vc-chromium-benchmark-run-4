c3f502d884dbbd58b7b0b63daf42a6be5eb46bf9
Aaron Krajeski <aaronhk@chromium.org>
2019-12-18 20:57:51 +0000

    Create MemoryManagedPaintCanvas class
    With deferral enabled a canvas rendering context can create an
    unlimited amount of GPU resources between frames. This class allows us
    to track how much memory is being pinned and to flush the recording
    after we pass a certain threshold.
    
    Bug: 1016727, 1015729
    Change-Id: I3a64dc07cc0239447aa721ea2a7cc808541a0a36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1960469
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726085}

--- third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/fonts/text_run_paint_info.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_types.h"
+#include "third_party/blink/renderer/platform/graphics/memory_managed_paint_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_canvas.h"
 #include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
@@ -86,6 +87,11 @@ OffscreenCanvasRenderingContext2D::OffscreenCanvasRenderingContext2D(
       bernoulli_distribution_(kUMASampleProbability) {
   is_valid_size_ = IsValidImageSize(Host()->Size());
 
+  // A raw pointer is safe here because the callback is only used by the
+  // recorder_
+  finalize_frame_callback_ =
+      WTF::BindRepeating(&OffscreenCanvasRenderingContext2D::FinalizeFrame,
+                         WrapWeakPersistent(this));
   StartRecording();
 
   // Clear the background transparent or opaque. Similar code at
@@ -127,13 +133,12 @@ void OffscreenCanvasRenderingContext2D::commit() {
 }
 
 void OffscreenCanvasRenderingContext2D::StartRecording() {
-  recorder_ = std::make_unique<PaintRecorder>();
-
+  recorder_ =
+      std::make_unique<MemoryManagedPaintRecorder>(finalize_frame_callback_);
   cc::PaintCanvas* canvas = recorder_->beginRecording(Width(), Height());
   // Always save an initial frame, to support resetting the top level matrix
   // and clip.
   canvas->save();
-
   RestoreMatrixClipStack(canvas);
 }
 
@@ -294,17 +299,10 @@ cc::PaintCanvas* OffscreenCanvasRenderingContext2D::DrawingCanvas() const {
   return recorder_->getRecordingCanvas();
 }
 
-cc::PaintCanvas* OffscreenCanvasRenderingContext2D::ExistingDrawingCanvas()
-    const {
-  if (!is_valid_size_)
-    return nullptr;
-  return recorder_->getRecordingCanvas();
-}
-
 void OffscreenCanvasRenderingContext2D::DidDraw() {
   have_recorded_draw_commands_ = true;
-  Host()->DidDraw();
   dirty_rect_for_commit_.setWH(Width(), Height());
+  Host()->DidDraw();
 }
 
 void OffscreenCanvasRenderingContext2D::DidDraw(const SkIRect& dirty_rect) {
