cc2183f9c976006065eeecedc5d01a66afd85482
Mila Green <milagreen@chromium.org>
2019-12-18 16:34:15 +0000

    GoogleUpdate setup project for Mac.
    Add a new executable to the mac updater project. It copies the bundle
    to the appropriate ~/Library location and creates a launchd item to
    run "GoogleUpdate --ua" every 5 hours.
    
    Bug: 1031293
    Change-Id: I02026497d31c5ca8739ffede4d86252e6ab55492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1954549
    Commit-Queue: Mila Green <milagreen@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725976}

---
+++ chrome/updater/mac/updater_setup/main.cc
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/updater/mac/updater_setup/updater_setup.h"
+
+int main(int argc, const char* argv[]) {
+  return updater_setup::UpdaterSetupMain(argc, argv);
+}
