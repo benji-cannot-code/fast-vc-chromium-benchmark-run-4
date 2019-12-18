237210c2acd802fdc5375bec04fea94e71355bcd
James Cook <jamescook@chromium.org>
2019-12-18 16:32:12 +0000

    Check for Chrome OS sync feature when showing passphrase notification
    Chrome OS sync and browser sync can both be encrypted with a shared
    passphrase. However, a user can choose to leave OS sync enabled and
    browser sync disabled, or vice versa. Show the sync passphrase error
    notification when either feature is enabled.
    
    Bug: 1033222, 1013466
    Test: updated unit_tests
    Change-Id: If4b8eddb76f7c9be1de3a7f094c692b2d03b7c6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972899
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725974}

--- chrome/browser/sync/sync_ui_util.cc
+++ chrome/browser/sync/sync_ui_util.cc
@@ -22,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_urls.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 
+#if defined(OS_CHROMEOS)
+#include "chromeos/constants/chromeos_features.h"
+#endif
+
 namespace sync_ui_util {
 
 namespace {
@@ -178,6 +182,20 @@ void OpenTabForSyncKeyRetrievalWithURL(Browser* browser, const GURL& url) {
   ShowSingletonTabOverwritingNTP(browser, std::move(params));
 }
 
+// Returns true if the user has consented to browser sync-the-feature or
+// Chrome OS sync.
+bool HasUserOptedInToSync(const syncer::SyncUserSettings* settings) {
+  if (settings->IsFirstSetupComplete())
+    return true;
+#if defined(OS_CHROMEOS)
+  if (chromeos::features::IsSplitSettingsSyncEnabled() &&
+      settings->GetOsSyncFeatureEnabled()) {
+    return true;
+  }
+#endif  // defined(OS_CHROMEOS)
+  return false;
+}
+
 }  // namespace
 
 StatusLabels GetStatusLabels(syncer::SyncService* sync_service,
@@ -307,15 +325,15 @@ bool ShouldRequestSyncConfirmation(const syncer::SyncService* service) {
 }
 
 bool ShouldShowPassphraseError(const syncer::SyncService* service) {
-  return service->GetUserSettings()->IsFirstSetupComplete() &&
-         service->GetUserSettings()
-             ->IsPassphraseRequiredForPreferredDataTypes();
+  const syncer::SyncUserSettings* settings = service->GetUserSettings();
+  return HasUserOptedInToSync(settings) &&
+         settings->IsPassphraseRequiredForPreferredDataTypes();
 }
 
 bool ShouldShowSyncKeysMissingError(const syncer::SyncService* service) {
-  return service->GetUserSettings()->IsFirstSetupComplete() &&
-         service->GetUserSettings()
-             ->IsTrustedVaultKeyRequiredForPreferredDataTypes();
+  const syncer::SyncUserSettings* settings = service->GetUserSettings();
+  return HasUserOptedInToSync(settings) &&
+         settings->IsTrustedVaultKeyRequiredForPreferredDataTypes();
 }
 
 void OpenTabForSyncKeyRetrieval(Browser* browser) {
