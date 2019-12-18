8053d535bf7182a4147f47cd91419b01204ce7b6
Maggie Chen <magchen@chromium.org>
2019-12-18 05:26:12 +0000

    Use the same set of V2 watchdog timeouts for V1
    The watchdog timeouts in V2 have recently changed from 15 seconds to 17
    seconds for Mac and from 10 seconds to 15 seconds for Android. This CL
    uses the same numbers for V1 so we can compare the results between V1
    and V2 later.
    
    Bug: 949839
    Change-Id: I4d00c8e91e5dcdae0417c21777bbb305f5d48339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1973230
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#725842}

--- gpu/config/gpu_finch_features.h
+++ gpu/config/gpu_finch_features.h
@@ -39,6 +39,8 @@ GPU_EXPORT extern const base::Feature
 
 GPU_EXPORT extern const base::Feature kGpuWatchdogV2;
 
+GPU_EXPORT extern const base::Feature kGpuWatchdogV1NewTimeout;
+
 #if defined(OS_MACOSX)
 GPU_EXPORT extern const base::Feature kMetal;
 #endif
