82c5732cc857068c02d496b5c9adf4e1550c7b93
Clemens Arbesser <arbesser@google.com>
2019-12-18 14:57:24 +0000

    [Autofill Assistant] Fix problems with using vector drawables.
    This CL also changes potentially problematic vector graphics in a test to standard
    raster graphics.
    
    Bug: 1033877
    Change-Id: I46ac4434563693fe7fa6633135efeed44c9085f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972143
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#725950}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/generic_ui/AssistantDrawable.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/generic_ui/AssistantDrawable.java
@@ -6,15 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill_assistant.generic_ui;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
+import android.support.v7.content.res.AppCompatResources;
 
-import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Callback;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -50,12 +49,7 @@ public abstract class AssistantDrawable {
         if (drawableId == 0) {
             return false;
         }
-        try {
-            ApiCompatibilityUtils.getDrawable(context.getResources(), drawableId);
-            return true;
-        } catch (Resources.NotFoundException e) {
-            return false;
-        }
+        return AppCompatResources.getDrawable(context, drawableId) != null;
     }
 
     @CalledByNative
@@ -136,12 +130,7 @@ public abstract class AssistantDrawable {
             if (drawableId == 0) {
                 callback.onResult(null);
             }
-            try {
-                callback.onResult(
-                        ApiCompatibilityUtils.getDrawable(context.getResources(), drawableId));
-            } catch (Resources.NotFoundException e) {
-                callback.onResult(null);
-            }
+            callback.onResult(AppCompatResources.getDrawable(context, drawableId));
         }
     }
 }
