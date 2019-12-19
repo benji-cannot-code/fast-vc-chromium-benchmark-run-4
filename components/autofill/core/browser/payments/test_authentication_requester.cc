c3723e7681ef7f98c26946c0624f6cc916c0db60
Manas Verma <manasverma@google.com>
2019-12-19 23:49:04 +0000

    [Autofill Auth] Autofill FIDO Auth should retrieve dcvv for cloud tokens.
    Bug: 949269
    Change-Id: I914b20bc5532a4df59120f6954235b1a5daa9013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977185
    Reviewed-by: Jared Saul <jsaul@google.com>
    Commit-Queue: Manas Verma <manasverma@google.com>
    Cr-Commit-Position: refs/heads/master@{#726601}

--- components/autofill/core/browser/payments/test_authentication_requester.cc
+++ components/autofill/core/browser/payments/test_authentication_requester.cc
@@ -31,7 +31,8 @@ void TestAuthenticationRequester::OnCVCAuthenticationComplete(
 #if !defined(OS_IOS)
 void TestAuthenticationRequester::OnFIDOAuthenticationComplete(
     bool did_succeed,
-    const CreditCard* card) {
+    const CreditCard* card,
+    const base::string16& cvc) {
   did_succeed_ = did_succeed;
   if (did_succeed_) {
     DCHECK(card);
