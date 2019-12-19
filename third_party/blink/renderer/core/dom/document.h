107738ebcca84e17626ad1e5342ebe7ed179fedf
Nate Chapin <japhet@chromium.org>
2019-12-19 01:53:51 +0000

    Reland "Make ScriptedAnimationController a ContextLifecycleStateObserver"
    This removes the need for custom pause/unpause logic for
    ScriptedAnimationController in Document.
    
    Also, construct ScriptedAnimationController in the Document
    constructor, rather than lazily. It will get initialized on the
    first frame, so we're not saving much by deferring its initialization.
    This simplifies a bunch of logic around its handling in Document, and
    guarantees that its lifetime will be predictable for leak detection.
    
    TBR=creis@chromium.org
    
    Bug: 1029822
    Change-Id: Ia30db6b7dac25c834152ae546364d77ff089fca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970259
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726206}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1126,10 +1126,6 @@ class CORE_EXPORT Document : public ContainerNode,
   bool IsDNSPrefetchEnabled() const { return is_dns_prefetch_enabled_; }
   void ParseDNSPrefetchControlHeader(const String&);
 
-  void TasksWerePaused() final;
-  void TasksWereUnpaused() final;
-  bool TasksNeedPause() final;
-
   void FinishedParsing();
 
   void SetEncodingData(const DocumentEncodingData& new_data);
@@ -1680,7 +1676,6 @@ class CORE_EXPORT Document : public ContainerNode,
   bool IsElementNode() const =
       delete;  // This will catch anyone doing an unnecessary check.
 
-  ScriptedAnimationController& EnsureScriptedAnimationController();
   ScriptedIdleTaskController& EnsureScriptedIdleTaskController();
   void InitSecurityContext(const DocumentInit&,
                            const SecurityContextInit& security_initializer);
--- third_party/blink/renderer/core/testing/null_execution_context.h
+++ third_party/blink/renderer/core/testing/null_execution_context.h
@@ -40,9 +40,6 @@ class NullExecutionContext : public GarbageCollected<NullExecutionContext>,
 
   EventTarget* ErrorEventTarget() override { return nullptr; }
 
-  bool TasksNeedPause() override { return tasks_need_pause_; }
-  void SetTasksNeedPause(bool flag) { tasks_need_pause_ = flag; }
-
   void AddConsoleMessageImpl(ConsoleMessage*,
                              bool discard_duplicates) override {}
   void ExceptionThrown(ErrorEvent*) override {}
@@ -63,7 +60,6 @@ class NullExecutionContext : public GarbageCollected<NullExecutionContext>,
   BrowserInterfaceBrokerProxy& GetBrowserInterfaceBroker() override;
 
  private:
-  bool tasks_need_pause_;
   bool is_secure_context_;
 
   KURL url_;
