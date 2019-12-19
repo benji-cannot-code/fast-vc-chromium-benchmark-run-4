f8f616a67621b9e8bc01f816414e3eba637a7923
Aran Gilman <gilmanmh@google.com>
2019-12-19 23:09:16 +0000

    Factor out common code in distiller page browser test.
    ExecuteJsScript() is used only once in this CL but will be used in
    new tests added by https://crrev.com/c/1922788.
    
    This CL also runs clang-format on pinch_tester.js and modifies it to
    return a bool instead of a dict. The latter simplifies verification
    of script execution in the C++ portion of the test.
    
    Bug: 1016615
    Change-Id: I2c847eb53556a30e991d988fb676c6a3fa5aee8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1971702
    Commit-Queue: Aran Gilman <gilmanmh@google.com>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726585}

--- components/test/data/dom_distiller/pinch_tester.js
+++ components/test/data/dom_distiller/pinch_tester.js
@@ -8,15 +8,15 @@ const pinchtest = (function() {
 
   function assertTrue(condition, message) {
     if (!condition) {
-      message = message || "Assertion failed";
+      message = message || 'Assertion failed';
       console.trace();
       throw new Error(message);
     }
   }
 
   function assertClose(a, b, message) {
-    if (Math.abs(a-b) > 1e-5) {
-      message = message || "Assertion failed";
+    if (Math.abs(a - b) > 1e-5) {
+      message = message || 'Assertion failed';
       console.log('"', a, '" and "', b, '" are not close.');
       console.trace();
       throw new Error(message);
@@ -51,7 +51,7 @@ const pinchtest = (function() {
 
   function assertEqual(a, b, message) {
     if (!isEquivalent(a, b)) {
-      message = message || "Assertion failed";
+      message = message || 'Assertion failed';
       console.log('"', a, '" and "', b, '" are not equal');
       console.trace();
       throw new Error(message);
@@ -63,22 +63,17 @@ const pinchtest = (function() {
     const points = {};
     function lowestID() {
       let ans = -1;
-      for(const key in points) {
+      for (const key in points) {
         ans = Math.max(ans, key);
       }
       return ans + 1;
     }
-    function changeTouchPoint (key, x, y, offsetX, offsetY) {
-      const e = {
-        clientX: x,
-        clientY: y,
-        pageX: x,
-        pageY: y
-      };
-      if (typeof(offsetX) === 'number') {
+    function changeTouchPoint(key, x, y, offsetX, offsetY) {
+      const e = {clientX: x, clientY: y, pageX: x, pageY: y};
+      if (typeof (offsetX) === 'number') {
         e.clientX += offsetX;
       }
-      if (typeof(offsetY) === 'number') {
+      if (typeof (offsetY) === 'number') {
         e.clientY += offsetY;
       }
       points[key] = e;
@@ -93,13 +88,10 @@ const pinchtest = (function() {
       },
       events: function() {
         const arr = [];
-        for(const key in points) {
+        for (const key in points) {
           arr.push(points[key]);
         }
-        return {
-          touches: arr,
-          preventDefault: function(){}
-        };
+        return {touches: arr, preventDefault: function() {}};
       }
     };
   });
@@ -415,7 +407,7 @@ const pinchtest = (function() {
   }
 
   return {
-    run: function(){
+    run: function() {
       testZoomOut();
       testZoomIn();
       testZoomOutAndPan();
@@ -428,7 +420,7 @@ const pinchtest = (function() {
       testFontScaling();
       pincher.reset();
 
-      return {success: true};
+      return true;
     }
   };
 }());
