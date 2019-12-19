1e5337c849971eabd59c16ef4e901f834298f73d
Yue Li <updowndota@chromium.org>
2019-12-19 02:42:21 +0000

    Check ShouldArcAlwaysStart flag for play store state
    For testing purposes, check the ShouldArcAlwaysStart flag for the play
    store state. The flag will only turned on by a command line flag for
    testing purposes.
    
    Background: https://chrome-internal-review.googlesource.com/c/chromeos/platform/tast-tests-private/+/1948653
    
    Bug: 982538
    Test: Run existing tests
    Change-Id: I7c8c3c97df5b696b9fcc467e7abc7e980305f05a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962598
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726228}

--- chrome/browser/ui/ash/assistant/assistant_state_client.cc
+++ chrome/browser/ui/ash/assistant/assistant_state_client.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
+#include "components/arc/arc_util.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
@@ -55,7 +56,10 @@ void AssistantStateClient::ActiveUserChanged(user_manager::User* active_user) {
 }
 
 void AssistantStateClient::OnArcPlayStoreEnabledChanged(bool enabled) {
-  ash::AssistantState::Get()->NotifyArcPlayStoreEnabledChanged(enabled);
+  // ARC Android instance will be enabled if user opt-in play store, or the ARC
+  // always start flag is set.
+  ash::AssistantState::Get()->NotifyArcPlayStoreEnabledChanged(
+      arc::ShouldArcAlwaysStart() || enabled);
 }
 
 void AssistantStateClient::SetProfileByUser(const user_manager::User* user) {
