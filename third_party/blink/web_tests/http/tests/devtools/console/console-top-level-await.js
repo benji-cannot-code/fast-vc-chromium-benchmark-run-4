1b2aed3350542c6e3e33582a2234e6c9daea2606
Simon Zünd <szuend@chromium.org>
2019-12-19 15:59:00 +0000

    Re-enable web tests affected by implementing REPL top-level await in V8
    This CL re-enables tests that are affected by V8's top-level
    await implementation.
    
    Manual wrapping of code in an async arrow function was
    removed from the frontend, so the 'unit' test for that wrapping
    code is also removed as part of this CL.
    
    Since we also correctly handle the completion value of async scripts,
    the top-level-await web test was rebaselined. All other tests
    can simply be re-enabled.
    
    Bug: chromium:1021921
    Change-Id: I902f36fa5b662f7df549dc233eee94212aaeadb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962259
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726387}

--- third_party/blink/web_tests/http/tests/devtools/console/console-top-level-await.js
+++ third_party/blink/web_tests/http/tests/devtools/console/console-top-level-await.js
@@ -50,8 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await test('class Foo{}; await 1;');
   await test('Foo');
   await test('await 0;function* gen(){}');
-  await test('for (var i = 0; i < 10; ++i) { await i; }')
-  await test('i');
   await test('for (let j = 0; j < 5; ++j) { await j; }')
   await test('j');
   await test('gen');
