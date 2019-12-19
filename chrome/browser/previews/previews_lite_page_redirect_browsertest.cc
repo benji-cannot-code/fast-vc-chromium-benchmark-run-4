e328941f2d1908ad7ce09df7ed524a30455c3f47
Ilia Samsonov <isamsonov@google.com>
2019-12-19 22:25:20 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=dougarnett@chromium.org
    
    Bug: 1023677
    Change-Id: I947641610097a19e4c4f1db6c2eca32045869f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957492
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Ilia Samsonov <isamsonov@google.com>
    Cr-Commit-Position: refs/heads/master@{#726556}

--- chrome/browser/previews/previews_lite_page_redirect_browsertest.cc
+++ chrome/browser/previews/previews_lite_page_redirect_browsertest.cc
@@ -922,7 +922,7 @@ class PreviewsLitePageRedirectServerBrowserTest
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectServerBrowserTest,
     ::testing::Bool());
 
@@ -1638,7 +1638,7 @@ class PreviewsLitePageRedirectServerTimeoutBrowserTest
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectServerTimeoutBrowserTest,
     ::testing::Bool());
 
@@ -1706,7 +1706,7 @@ class PreviewsLitePageRedirectServerBadServerBrowserTest
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectServerBadServerBrowserTest,
     ::testing::Bool());
 
@@ -1752,7 +1752,7 @@ class PreviewsLitePageRedirectServerDataSaverBrowserTest
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectServerDataSaverBrowserTest,
     ::testing::Bool());
 
@@ -1789,7 +1789,7 @@ class PreviewsLitePageRedirectServerNoDataSaverHeaderBrowserTest
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectServerNoDataSaverHeaderBrowserTest,
     ::testing::Bool());
 
@@ -1830,7 +1830,7 @@ class PreviewsLitePageRedirectNotificationDSEnabledBrowserTest
 
 // Pecond param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectNotificationDSEnabledBrowserTest,
     ::testing::Bool());
 
@@ -1890,7 +1890,7 @@ class PreviewsLitePageRedirectDSDisabledBrowserTest
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectDSDisabledBrowserTest,
     ::testing::Bool());
 
@@ -1922,7 +1922,7 @@ class PreviewsLitePageRedirectControlBrowserTest
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectControlBrowserTest,
     ::testing::Bool());
 
@@ -2011,7 +2011,7 @@ IN_PROC_BROWSER_TEST_P(
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectServerNetworkIsolationBrowserTest,
     ::testing::Values(NetworkIsolationKeyMode::kNone,
                       NetworkIsolationKeyMode::kTopFrameOrigin,
@@ -2054,7 +2054,7 @@ class PreviewsLitePageRedirectAndPageHintsBrowserTest
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     PreviewsLitePageRedirectAndPageHintsBrowserTest,
     ::testing::Bool());
 
@@ -2296,22 +2296,22 @@ class CoinFlipHoldbackExperimentBrowserTestWithCoinFlipHoldbackDisabled
 
 // Param is true if DRP holdback should be enabled.
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     CoinFlipHoldbackExperimentBrowserTest,
     ::testing::Bool());
 
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     CoinFlipHoldbackExperimentBrowserTestWithRandomNavigationCoinFlip,
     ::testing::Bool());
 
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     CoinFlipHoldbackExperimentBrowserTestWithoutRandomNavigationCoinFlip,
     ::testing::Bool());
 
 INSTANTIATE_TEST_SUITE_P(
-    /* no prefix */,
+    All,
     CoinFlipHoldbackExperimentBrowserTestWithCoinFlipHoldbackDisabled,
     ::testing::Bool());
 
