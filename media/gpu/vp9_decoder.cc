2c2fa336b573a6f441f7898bce95659dd69e0ce2
Chih-Yu Huang <akahuang@google.com>
2019-12-19 14:36:11 +0000

    media/gpu/VP9Decoder: use std::move() to reduce scoped_refptr duplication.
    Bug: 1020668
    Test: run vda_tests on kevin
    
    Change-Id: I5679d5ff65159f4f3bcb7dd62f5a3e1e09964739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974726
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726369}

--- media/gpu/vp9_decoder.cc
+++ media/gpu/vp9_decoder.cc
@@ -254,7 +254,7 @@ VP9Decoder::DecodeResult VP9Decoder::Decode() {
     }
     pic->frame_hdr = std::move(curr_frame_hdr_);
 
-    if (!DecodeAndOutputPicture(pic)) {
+    if (!DecodeAndOutputPicture(std::move(pic))) {
       SetError();
       return kDecodeError;
     }
