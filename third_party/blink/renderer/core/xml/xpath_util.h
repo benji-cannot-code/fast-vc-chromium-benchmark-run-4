3995479827092cf9415ecc07d4355731fc656ec0
Kent Tamura <tkent@chromium.org>
2019-12-19 22:26:25 +0000

    XPath: Fix normalize-space()
    It should handle only XML white spaces; #x20, #x9, #xD, and #xA.
    We incorrectly handled other white spaces such as U+3000.
    
    Bug: 893929
    Change-Id: I59708f978ba1c23141f748de7a8f4c58d58c7acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974713
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726557}

--- third_party/blink/renderer/core/xml/xpath_util.h
+++ third_party/blink/renderer/core/xml/xpath_util.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_XML_XPATH_UTIL_H_
 
 #include "third_party/blink/renderer/platform/wtf/forward.h"
+#include "third_party/blink/renderer/platform/wtf/text/unicode.h"
 
 namespace blink {
 
@@ -45,6 +46,9 @@ String StringValue(Node*);
 
 // @return whether the given node is a valid context node
 bool IsValidContextNode(Node*);
+
+// https://www.w3.org/TR/REC-xml/#NT-S
+bool IsXMLSpace(UChar ch);
 
 }  // namespace xpath
 
