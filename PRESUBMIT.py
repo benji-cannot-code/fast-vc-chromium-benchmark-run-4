fdbad25d223803c6f3e6d01c4117d57896e4f1fa
Oksana Zhuravlova <oksamyt@chromium.org>
2019-12-19 19:21:16 +0000

    [mojo] Add presubmit warning for renderer-side GetInterfaceProvider
    This change adds a warning for new uses of GetInterfaceProvider() and
    suggests using GetBrowserInterfaceBroker() instead.
    
    Bug: 718652
    Change-Id: I2e99ba36a8cdb198200967d27a64931a32adcdc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967759
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726462}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1082,6 +1082,16 @@ _BANNED_CPP_FUNCTIONS = (
       ),
     ),
     (
+      'GetInterfaceProvider',
+      (
+        'InterfaceProvider is deprecated.',
+        'Please use ExecutionContext::GetBrowserInterfaceBroker and overrides',
+        'or Platform::GetBrowserInterfaceBroker.'
+      ),
+      False,
+      (),
+    ),
+    (
       'CComPtr',
       (
         'New code should use Microsoft::WRL::ComPtr from wrl/client.h as a ',
