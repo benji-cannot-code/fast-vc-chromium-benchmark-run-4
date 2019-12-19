50bec3c93a91d7d47c79d702246d8a94e77caf7e
Demetrios Papadopoulos <dpapad@chromium.org>
2019-12-19 15:58:05 +0000

    SplitSettings: Remove wallpaper UI from browser settings.
    Also moving wallpaper_browser_proxy.html/js to c/b/r/chromeos/
    
    Bug: 1026455
    Change-Id: I89e8fbcfff964fede0361e65346acce63ea8f1e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1975291
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726386}

--- chrome/browser/resources/settings/appearance_page/appearance_page.js
+++ chrome/browser/resources/settings/appearance_page/appearance_page.js
@@ -101,11 +101,6 @@ Polymer({
   /** @private {?settings.AppearanceBrowserProxy} */
   appearanceBrowserProxy_: null,
 
-  // <if expr="chromeos">
-  /** @private {?settings.WallpaperBrowserProxy} */
-  wallpaperBrowserProxy_: null,
-  // </if>
-
   observers: [
     'defaultFontSizeChanged_(prefs.webkit.webprefs.default_font_size.value)',
     'themeChanged_(prefs.extensions.theme.id.value, useSystemTheme_)',
@@ -120,10 +115,6 @@ Polymer({
   created: function() {
     this.appearanceBrowserProxy_ =
         settings.AppearanceBrowserProxyImpl.getInstance();
-    // <if expr="chromeos">
-    this.wallpaperBrowserProxy_ =
-        settings.WallpaperBrowserProxyImpl.getInstance();
-    // </if>
   },
 
   /** @override */
@@ -137,18 +128,6 @@ Polymer({
 
     this.pageZoomLevels_ = /** @type {!Array<number>} */ (
         JSON.parse(loadTimeData.getString('presetZoomFactors')));
-
-    // <if expr="chromeos">
-    this.wallpaperBrowserProxy_.isWallpaperSettingVisible().then(
-        isWallpaperSettingVisible => {
-          assert(this.pageVisibility);
-          this.pageVisibility.setWallpaper = isWallpaperSettingVisible;
-        });
-    this.wallpaperBrowserProxy_.isWallpaperPolicyControlled().then(
-        isPolicyControlled => {
-          this.isWallpaperPolicyControlled_ = isPolicyControlled;
-        });
-    // </if>
   },
 
   /**
@@ -206,16 +185,6 @@ Polymer({
   openThemeUrl_: function() {
     window.open(this.themeUrl_ || loadTimeData.getString('themesGalleryUrl'));
   },
-
-  // <if expr="chromeos">
-  /**
-   * ChromeOS only.
-   * @private
-   */
-  openWallpaperManager_: function() {
-    this.wallpaperBrowserProxy_.openWallpaperManager();
-  },
-  // </if>
 
   /** @private */
   onUseDefaultTap_: function() {
