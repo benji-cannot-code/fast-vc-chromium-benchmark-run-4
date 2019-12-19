cec487cb99f80dacfd50279818e04a5de1ea3e5d
Jaeyong Bae <jdragon.bae@gmail.com>
2019-12-19 03:54:14 +0000

    [Passwords] Add PasswordManager.AccessPasswordInSettings
    This adds histograms to count how passwords are shown in settings.
    Having PasswordManager.AccessPasswordInSettings on Android
    in addition to the current histograms is beneficial.
    
    Bug: 885245
    Change-Id: Ia231316ac476135c6eb4dcecc3f070f7a0e585c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966961
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Jaeyong Bae <jdragon.bae@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#726251}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/password/PasswordEntryViewer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/password/PasswordEntryViewer.java
@@ -80,6 +80,11 @@ public class PasswordEntryViewer
     private static final int PASSWORD_ACTION_HIDDEN = 2;
     private static final int PASSWORD_ACTION_BOUNDARY = 3;
 
+    // Metrics: "PasswordManager.AccessPasswordInSettings"
+    private static final int ACCESS_PASSWORD_VIEWED = 0;
+    private static final int ACCESS_PASSWORD_COPIED = 1;
+    private static final int ACCESS_PASSWORD_COUNT = 2;
+
     // ID of this name/password or exception.
     private int mID;
 
@@ -356,6 +361,9 @@ public class PasswordEntryViewer
         RecordHistogram.recordEnumeratedHistogram(
                 "PasswordManager.Android.PasswordCredentialEntry.Password",
                 PASSWORD_ACTION_DISPLAYED, PASSWORD_ACTION_BOUNDARY);
+
+        RecordHistogram.recordEnumeratedHistogram("PasswordManager.AccessPasswordInSettings",
+                ACCESS_PASSWORD_VIEWED, ACCESS_PASSWORD_COUNT);
     }
 
     private void hidePassword() {
@@ -381,6 +389,9 @@ public class PasswordEntryViewer
         RecordHistogram.recordEnumeratedHistogram(
                 "PasswordManager.Android.PasswordCredentialEntry.Password", PASSWORD_ACTION_COPIED,
                 PASSWORD_ACTION_BOUNDARY);
+
+        RecordHistogram.recordEnumeratedHistogram("PasswordManager.AccessPasswordInSettings",
+                ACCESS_PASSWORD_COPIED, ACCESS_PASSWORD_COUNT);
     }
 
     private void hookupPasswordButtons() {
