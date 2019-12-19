1878e79b8919c880c69eedf30d38146f0c485a80
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-12-19 13:32:54 +0000

    Import wpt@0ae74ec621f758cfe426ab7cb71b5dd6be0a241c
    Using wpt-import in Chromium ed56d990775d7705e280296a6e438c2956a5a503.
    With Chromium commits locally applied on WPT:
    c15c108392 "Add test that datachannels close when connection does."
    fadf602792 "[css-pseudo] Recalculate viewport units in ::marker"
    044ad8ad18 "[OT-PW] Make it work with OOP-R"
    9899425da3 "[css-layout-api] IntrinsicSizes Implementation"
    7d838af77c "[scroll-to-text-fragment] Apply :target CSS pseudo-class to match."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Ic4871dcc160a3fc64e4c616e90c1268fb04fe5fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1975831
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#726350}

---
+++ third_party/blink/web_tests/external/wpt/wasm/webapi/historical.any.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+
+promise_test(async t => {
+  const db_name = "WebAssembly";
+  const obj_store = "store";
+  const module_key = "module";
+
+  await new Promise((resolve, reject) => {
+    const delete_request = indexedDB.deleteDatabase(db_name);
+    delete_request.onsuccess = resolve;
+    delete_request.onerror = reject;
+  });
+
+  const db = await new Promise((resolve, reject) => {
+    const open_request = indexedDB.open(db_name);
+    open_request.onupgradeneeded = function() {
+      open_request.result.createObjectStore(obj_store);
+    };
+    open_request.onsuccess = function() {
+      resolve(open_request.result);
+    };
+    open_request.onerror = reject;
+  });
+
+  const mod = await WebAssembly.compileStreaming(fetch('../incrementer.wasm'));
+  const tx = db.transaction(obj_store, 'readwrite');
+  const store = tx.objectStore(obj_store);
+  assert_throws("DataCloneError", () => store.put(mod, module_key));
+});
