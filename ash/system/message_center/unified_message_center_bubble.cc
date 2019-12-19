7d108437db71489b827aebf740a6baf11e38adf5
Ahmed Mehfooz <amehfooz@chromium.org>
2019-12-19 23:08:10 +0000

    Add verbalization for message center widget
    Bug: 1021133
    Change-Id: Ic7cbf25c094006efd970c75dff2b733b31620bc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974599
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726583}

--- ash/system/message_center/unified_message_center_bubble.cc
+++ ash/system/message_center/unified_message_center_bubble.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/accessibility/accessibility_controller_impl.h"
 #include "ash/shelf/shelf.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/shell.h"
 #include "ash/style/ash_color_provider.h"
 #include "ash/style/default_color_constants.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/unified/unified_system_tray.h"
 #include "ash/system/unified/unified_system_tray_bubble.h"
 #include "ash/system/unified/unified_system_tray_view.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/compositor/paint_recorder.h"
 #include "ui/views/focus/focus_search.h"
 #include "ui/views/widget/widget.h"
@@ -199,6 +201,10 @@ TrayBubbleView* UnifiedMessageCenterBubble::GetBubbleView() const {
 
 views::Widget* UnifiedMessageCenterBubble::GetBubbleWidget() const {
   return bubble_widget_;
+}
+
+base::string16 UnifiedMessageCenterBubble::GetAccessibleNameForBubble() {
+  return l10n_util::GetStringUTF16(IDS_ASH_MESSAGE_CENTER_ACCESSIBLE_NAME);
 }
 
 bool UnifiedMessageCenterBubble::ShouldEnableExtraKeyboardAccessibility() {
--- ash/system/message_center/unified_message_center_bubble.h
+++ ash/system/message_center/unified_message_center_bubble.h
@@ -67,6 +67,7 @@ class ASH_EXPORT UnifiedMessageCenterBubble : public TrayBubbleBase,
   views::Widget* GetBubbleWidget() const override;
 
   // TrayBubbleView::Delegate:
+  base::string16 GetAccessibleNameForBubble() override;
   bool ShouldEnableExtraKeyboardAccessibility() override;
 
   // views::ViewObserver:
