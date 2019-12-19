ab0ed83dee58c7a290f44e7df1a0a6d5b5469964
Mike Wittman <wittman@chromium.org>
2019-12-19 21:49:56 +0000

    [Sampling profiler] Provide timestamp with sample
    Records timestamps along with samples and plumbs them through to
    the ProfileBuilder interface. The CallStackProfileBuilder subclass
    will be updated in a following CL to record the timestamps, to
    support retrospective tagging of metadata on the samples.
    
    Bug: 1034758
    Change-Id: Ic631d47773db5ce8ed5117c08aba5d5931cb7884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972931
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726526}

--- services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.cc
+++ services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.cc
@@ -211,12 +211,13 @@ TracingSamplerProfiler::TracingProfileBuilder::GetModuleCache() {
 }
 
 void TracingSamplerProfiler::TracingProfileBuilder::OnSampleCompleted(
-    std::vector<base::Frame> frames) {
+    std::vector<base::Frame> frames,
+    base::TimeTicks sample_timestamp) {
   base::AutoLock l(trace_writer_lock_);
   if (!trace_writer_) {
     if (buffered_samples_.size() < kMaxBufferedSamples) {
       buffered_samples_.emplace_back(
-          BufferedSample(TRACE_TIME_TICKS_NOW(), std::move(frames)));
+          BufferedSample(sample_timestamp, std::move(frames)));
     }
     return;
   }
@@ -226,7 +227,7 @@ void TracingSamplerProfiler::TracingProfileBuilder::OnSampleCompleted(
     }
     buffered_samples_.clear();
   }
-  WriteSampleToTrace(BufferedSample(TRACE_TIME_TICKS_NOW(), std::move(frames)));
+  WriteSampleToTrace(BufferedSample(sample_timestamp, std::move(frames)));
 }
 
 void TracingSamplerProfiler::TracingProfileBuilder::WriteSampleToTrace(
