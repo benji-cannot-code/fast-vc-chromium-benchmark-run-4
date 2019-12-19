d88a64240e8f0e9ff1e5794c48c3840e50b38f23
Darren Shen <shend@chromium.org>
2019-12-19 00:36:54 +0000

    rule-based: Use native code for activating/deactivating engine.
    <This change is hidden behind a flag>
    
    As part of migrating PK rule-based IMEs to native code, this CL migrates
    activate/deactivate to native code path.
    
    We port RuleBasedController#onActivate/onDeactivate in
    chos/pk/rulebasedcontroller.js.
    
    Note that we need these to call through to the extension, as it has
    side effects that may affect the extension when it is running (e.g.
    when VK is up).
    
    We also added a bunch of metrics collection that was in the extension
    code.
    
    Note that the JS code has already been guarded by the runtime flag.
    
    Bug: 1009903
    Change-Id: Ib72ea3916d6f66c17c6553a7f137e1eb1647e181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970294
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Shu Chen <shuchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726174}

--- chrome/browser/chromeos/input_method/native_input_method_engine_browsertest.cc
+++ chrome/browser/chromeos/input_method/native_input_method_engine_browsertest.cc
@@ -39,7 +39,9 @@ class TestObserver : public InputMethodEngineBase::Observer {
   void OnKeyEvent(
       const std::string& engine_id,
       const InputMethodEngineBase::KeyboardEvent& event,
-      ui::IMEEngineHandlerInterface::KeyEventDoneCallback key_data) override {}
+      ui::IMEEngineHandlerInterface::KeyEventDoneCallback callback) override {
+    std::move(callback).Run(/*handled=*/false);
+  }
   void OnInputContextUpdate(
       const ui::IMEEngineHandlerInterface::InputContext& context) override {}
   void OnCandidateClicked(
@@ -131,6 +133,7 @@ class NativeInputMethodEngineTest : public InProcessBrowserTest,
 
 // ID is specified in google_xkb_manifest.json.
 constexpr char kEngineIdVietnameseTelex[] = "vkd_vi_telex";
+constexpr char kEngineIdArabic[] = "vkd_ar";
 
 }  // namespace
 
@@ -181,6 +184,48 @@ IN_PROC_BROWSER_TEST_F(NativeInputMethodEngineTest, VietnameseTelex_Reset) {
   ASSERT_EQ(text_input_client.insert_text_history().size(), 1U);
   EXPECT_EQ(text_input_client.insert_text_history()[0],
             base::ASCIIToUTF16("s"));
+
+  SetFocus(nullptr);
+}
+
+IN_PROC_BROWSER_TEST_F(NativeInputMethodEngineTest, SwitchActiveController) {
+  // Swap between two controllers.
+  engine_.Enable(kEngineIdVietnameseTelex);
+  engine_.FlushForTesting();
+  engine_.Disable();
+  engine_.Enable(kEngineIdArabic);
+  engine_.FlushForTesting();
+
+  // Create a fake text field.
+  ui::DummyTextInputClient text_input_client(ui::TEXT_INPUT_TYPE_TEXT);
+  SetFocus(&text_input_client);
+
+  DispatchKeyPress(ui::VKEY_A);
+
+  // Expect to commit 'ش'.
+  ASSERT_EQ(text_input_client.composition_history().size(), 0U);
+  ASSERT_EQ(text_input_client.insert_text_history().size(), 1U);
+  EXPECT_EQ(text_input_client.insert_text_history()[0],
+            base::UTF8ToUTF16(u8"ش"));
+
+  SetFocus(nullptr);
+}
+
+IN_PROC_BROWSER_TEST_F(NativeInputMethodEngineTest, NoActiveController) {
+  engine_.Enable(kEngineIdVietnameseTelex);
+  engine_.FlushForTesting();
+  engine_.Disable();
+
+  // Create a fake text field.
+  ui::DummyTextInputClient text_input_client(ui::TEXT_INPUT_TYPE_TEXT);
+  SetFocus(&text_input_client);
+
+  DispatchKeyPress(ui::VKEY_A);
+  engine_.Reset();
+
+  // Expect no changes.
+  ASSERT_EQ(text_input_client.composition_history().size(), 0U);
+  ASSERT_EQ(text_input_client.insert_text_history().size(), 0U);
 
   SetFocus(nullptr);
 }
