33232e2ba16bf4f9a86ea76f5b18a0f52d349b42
Ilia Samsonov <isamsonov@google.com>
2019-12-19 23:10:38 +0000

    Add prefix to empty param tests.
    googletest will no longer support empty prefix
    for parameterized INSTANTIATE_TEST_SUITE_P.
    Adding "All" prefix to existing empty prefix test suites.
    
    This CL was uploaded by git cl split.
    
    R=ericorth@chromium.org
    
    Bug: 1023677
    Change-Id: Ic527ff1a7cb4654e17df53ba36a3da314187de17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1957495
    Auto-Submit: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726587}

--- services/network/public/cpp/simple_url_loader_unittest.cc
+++ services/network/public/cpp/simple_url_loader_unittest.cc
@@ -2813,7 +2813,7 @@ TEST_P(SimpleURLLoaderTest, GetFinalURLAfterRedirect) {
 }
 
 INSTANTIATE_TEST_SUITE_P(
-    /* No prefix */,
+    All,
     SimpleURLLoaderTest,
     testing::Values(SimpleLoaderTestHelper::DownloadType::TO_STRING,
                     SimpleLoaderTestHelper::DownloadType::TO_FILE,
--- services/network/tcp_socket_unittest.cc
+++ services/network/tcp_socket_unittest.cc
@@ -693,7 +693,7 @@ class TCPSocketWithMockSocketTest
   net::MockClientSocketFactory mock_client_socket_factory_;
 };
 
-INSTANTIATE_TEST_SUITE_P(/* no prefix */,
+INSTANTIATE_TEST_SUITE_P(All,
                          TCPSocketWithMockSocketTest,
                          testing::Values(net::SYNCHRONOUS, net::ASYNC));
 
