638afda7d5ac8a91dd1ae069de5da540a9fa6a59
Marijn Kruisselbrink <mek@chromium.org>
2019-12-19 18:46:03 +0000

    [FileSystem] Get rid of no longer needed snapshot blob.
    Since all this runs on the same thread these days, there is no longer any
    reason for FileSystemDispatcher::DidCreateSnapshotFile to create a
    BlobDataHandle. The callbacks called by it already do the same
    synchronously anyway.
    
    Bug: none
    Change-Id: I86cbc0f0c4feeb20b6495f6741493d9a11ec98b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1975088
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726446}

--- third_party/blink/renderer/modules/filesystem/file_system_callbacks.h
+++ third_party/blink/renderer/modules/filesystem/file_system_callbacks.h
@@ -81,9 +81,7 @@ class SnapshotFileCallbackBase {
   virtual ~SnapshotFileCallbackBase() = default;
 
   // Called when a snapshot file is created successfully.
-  virtual void DidCreateSnapshotFile(
-      const FileMetadata&,
-      scoped_refptr<BlobDataHandle> snapshot) = 0;
+  virtual void DidCreateSnapshotFile(const FileMetadata&) = 0;
 
   virtual void DidFail(base::File::Error error) = 0;
 };
@@ -246,8 +244,7 @@ class SnapshotFileCallback final : public SnapshotFileCallbackBase,
                        ExecutionContext*);
 
   // Called when a snapshot file is created successfully.
-  void DidCreateSnapshotFile(const FileMetadata&,
-                             scoped_refptr<BlobDataHandle> snapshot) override;
+  void DidCreateSnapshotFile(const FileMetadata&) override;
 
   // Called when a request operation has failed.
   void DidFail(base::File::Error error) override;
