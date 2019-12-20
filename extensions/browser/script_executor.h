6557077d6726ac822bb2737fc615bb9d3f4f0256
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-12-20 17:59:02 +0000

    [Extensions] Set tabs.executeScript() URLs to chrome-extension: scheme
    Set the script URL for scripts executed via chrome.tabs.executeScript()
    to use the chrome-extension: scheme, e.g.
    chrome-extension://<id>/<path-to-script>, rather than the file URL.
    This prevents referencing the filesystem in the URL, and is consistent
    with content scripts that are statically specified in the manifest.
    
    Add a regression test (that also tests the statically-defined content
    script behavior). This entailed adding a new test utility,
    WebContentsConsoleObserver, to track the messages sent to the console
    for a given WebContents. This can replace ConsoleObserverDelegate in
    the future.
    
    Bug: 714617
    Change-Id: I3de400e6dccf9f9a662824b4810bd52245cd4d62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962676
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726842}

--- extensions/browser/script_executor.h
+++ extensions/browser/script_executor.h
@@ -103,7 +103,7 @@ class ScriptExecutor {
                      UserScript::RunLocation run_at,
                      ProcessType process_type,
                      const GURL& webview_src,
-                     const GURL& file_url,
+                     const GURL& script_url,
                      bool user_gesture,
                      base::Optional<CSSOrigin> css_origin,
                      ResultType result_type,
