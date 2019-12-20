15a2356a690c8a04713eb236f0fbba0e22d12c4d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2019-12-20 01:00:00 +0000

    Import wpt@30896bb9378b84ffd5583a1088d4e2862f19dc66
    Using wpt-import in Chromium a436532f5842d7a6be09802dc95cd77a42cb2b0b.
    With Chromium commits locally applied on WPT:
    c15c108392 "Add test that datachannels close when connection does."
    9899425da3 "[css-layout-api] IntrinsicSizes Implementation"
    d75fede577 "[EventTiming] Fix crossiframe test"
    93b036391f "Roll WebGPU CTS"
    01fd8522ea "XPath: Fix normalize-space()"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Ibedf0d608dbd96588e655809068e60c32d6a2458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1976852
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#726626}

---
+++ third_party/blink/web_tests/external/wpt/html/infrastructure/safe-passing-of-structured-data/structured_clone_blob_array.window.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async_test(function(t) {
+    var blob = new Blob(['<a id="a"><b id="b">hey!</b></a>'], {type:"text/plain"});
+    var another_blob = new Blob(['<a id="a"><b id="b">hey!</b></a>'], {type:"text/plain"});
+    onmessage = t.step_func(function(msg) {
+        assert_true(msg.data instanceof Array);
+        assert_equals(msg.data.length, 2);
+        msg.data.forEach((function(blob, index) {
+          assert_true(blob instanceof Blob);
+          var cloned_content, original_content;
+          var reader = new FileReader();
+          reader.addEventListener("loadend", t.step_func(function() {
+              original_content = reader.result;
+              var reader2 = new FileReader();
+              reader2.addEventListener("loadend", t.step_func_done(function() {
+                  cloned_content = reader2.result;
+                  assert_equals(typeof cloned_content, typeof original_content);
+                  assert_equals(cloned_content, original_content);
+              }));
+              reader2.readAsText(msg.data[index]);
+          }));
+          reader.readAsText(blob);
+        }));
+    });
+    postMessage([blob, another_blob], '*');
+}, "Cloning an array of blobs into the same realm");
