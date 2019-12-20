48c10653ec04cc9b1ddd2a10906c1d1a14dea4f6
jessing <jessing@google.com>
2019-12-20 04:57:53 +0000

    system-web-apps: register sample system web app as a system app.
    This is an example of how to register a system web app as a system app.
    
    Bug: 1032810
    Change-Id: If1e6c67305a7baeef8467eab6580e86fdf75db8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1971039
    Commit-Queue: Jessica Huang <jessing@google.com>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726697}

--- chrome/browser/web_applications/system_web_app_manager.h
+++ chrome/browser/web_applications/system_web_app_manager.h
@@ -44,6 +44,9 @@ enum class SystemAppType {
   TERMINAL,
   MEDIA,
   HELP,
+#if !defined(OFFICIAL_BUILD)
+  SAMPLE,
+#endif  // !defined(OFFICIAL_BUILD)
 };
 
 // The configuration options for a System App.
--- chromeos/components/sample_system_web_app_ui/url_constants.cc
+++ chromeos/components/sample_system_web_app_ui/url_constants.cc
@@ -8,5 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 const char kChromeUISampleSystemWebAppHost[] = "sample-system-web-app";
+const char kChromeUISampleSystemWebAppURL[] = "chrome://sample-system-web-app";
 
 }  // namespace chromeos
