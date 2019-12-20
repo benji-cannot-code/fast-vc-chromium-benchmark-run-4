76dcd395f7cb0886f941770f3d432148172fc0bf
Peter Kotwicz <pkotwicz@chromium.org>
2019-12-20 21:19:24 +0000

    Decrease WebappActionsNotificationTest flakiness
    This CL:
    - Moves logic to launch WebappActivity to test rule so that it does not
    get run on KitKat (both of the WebappActionsNotificationTest tests are
    M+)
    - Increases the timeout for
    WebappActivityTestRule#waitUntilSplashHides(). Hopefully this will
    reduce test failures due to
    WebappActivityTestRule#waitUntilSplashHides() timing out
    
    BUG=1031094
    
    Change-Id: I8cc1cec080460f74b0282fb472e471f11872df6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977188
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726936}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappActionsNotificationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappActionsNotificationTest.java
@@ -42,6 +42,7 @@ import org.chromium.content_public.browser.test.util.TestThreadUtils;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@MinAndroidSdkLevel(Build.VERSION_CODES.M) // NotificationManager.getActiveNotifications
 @TargetApi(Build.VERSION_CODES.M)
 public class WebappActionsNotificationTest {
     private static final String WEB_APP_PATH = "/chrome/test/data/banners/manifest_test_page.html";
@@ -60,7 +61,6 @@ public class WebappActionsNotificationTest {
     @SmallTest
     @Feature({"Webapps"})
     @RetryOnFailure
-    @MinAndroidSdkLevel(Build.VERSION_CODES.M) // NotificationManager.getActiveNotifications
     public void testNotification_openInChrome() throws Exception {
         Notification notification = getWebappNotification();
 
@@ -90,7 +90,6 @@ public class WebappActionsNotificationTest {
       @SmallTest
       @Feature({"Webapps"})
       @RetryOnFailure
-      @MinAndroidSdkLevel(Build.VERSION_CODES.M) // NotificationManager.getActiveNotifications
     */
     @DisabledTest(message = "crbug.com/774491")
     public void testNotification_copyUrl() throws Exception {
