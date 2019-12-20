b058c138fe3d2c0e8289890185efc1e703b3a7b4
Mia Glaese <glamia@chromium.org>
2019-12-20 21:27:18 +0000

    [Start Surface] Fix bug that voice rec. button and omnibox disappear.
    Follow up to: https://chromium-review.googlesource.com/c/chromium/src/+/1960080
    
    Before:
    Tabswitcher toolbar is not hidden when focusing omnibox in incognito.
    Voice recognition button sometimes disappeared in normal mode when closing all
    Incognito tabs from notifications.
    
    After:
    Tabswitcher toolbar hides when focusing omnibox in incognito.
    Voice recognition button visibility is updated.
    
    Bug: 1031973, 1034867
    Change-Id: Id5f9c694501b7b52f7710ad97491d82fe1b3acdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978876
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Mia Glaese <glamia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726939}

--- chrome/android/features/start_surface/internal/junit/src/org/chromium/chrome/features/start_surface/StartSurfaceMediatorUnitTest.java
+++ chrome/android/features/start_surface/internal/junit/src/org/chromium/chrome/features/start_surface/StartSurfaceMediatorUnitTest.java
@@ -184,6 +184,7 @@ public class StartSurfaceMediatorUnitTest {
         assertThat(mPropertyModel.get(IS_FAKE_SEARCH_BOX_VISIBLE), equalTo(false));
         mUrlFocusChangeListenerCaptor.getValue().onUrlFocusChange(false);
         assertThat(mPropertyModel.get(IS_FAKE_SEARCH_BOX_VISIBLE), equalTo(true));
+        assertThat(mPropertyModel.get(IS_VOICE_RECOGNITION_BUTTON_VISIBLE), equalTo(true));
 
         mediator.hideOverview(true);
         verify(mMainTabGridController).hideOverview(eq(true));
@@ -231,6 +232,7 @@ public class StartSurfaceMediatorUnitTest {
         assertThat(mPropertyModel.get(IS_FAKE_SEARCH_BOX_VISIBLE), equalTo(false));
         mUrlFocusChangeListenerCaptor.getValue().onUrlFocusChange(false);
         assertThat(mPropertyModel.get(IS_FAKE_SEARCH_BOX_VISIBLE), equalTo(true));
+        assertThat(mPropertyModel.get(IS_VOICE_RECOGNITION_BUTTON_VISIBLE), equalTo(true));
 
         mediator.hideOverview(true);
         verify(mMainTabGridController).hideOverview(eq(true));
@@ -517,6 +519,8 @@ public class StartSurfaceMediatorUnitTest {
         assertThat(mPropertyModel.get(IS_SECONDARY_SURFACE_VISIBLE), equalTo(true));
         assertThat(
                 mSecondaryTasksSurfacePropertyModel.get(IS_FAKE_SEARCH_BOX_VISIBLE), equalTo(true));
+        assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_VOICE_RECOGNITION_BUTTON_VISIBLE),
+                equalTo(false));
         assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_INCOGNITO), equalTo(true));
 
         mediator.setOverviewState(OverviewModeState.SHOWN_TABSWITCHER);
@@ -541,6 +545,8 @@ public class StartSurfaceMediatorUnitTest {
         assertThat(mPropertyModel.get(IS_SECONDARY_SURFACE_VISIBLE), equalTo(true));
         assertThat(
                 mSecondaryTasksSurfacePropertyModel.get(IS_FAKE_SEARCH_BOX_VISIBLE), equalTo(true));
+        assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_VOICE_RECOGNITION_BUTTON_VISIBLE),
+                equalTo(false));
         assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_INCOGNITO), equalTo(true));
 
         mediator.hideOverview(false);
@@ -607,6 +613,8 @@ public class StartSurfaceMediatorUnitTest {
         assertThat(mPropertyModel.get(IS_SECONDARY_SURFACE_VISIBLE), equalTo(true));
         assertThat(
                 mSecondaryTasksSurfacePropertyModel.get(IS_FAKE_SEARCH_BOX_VISIBLE), equalTo(true));
+        assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_VOICE_RECOGNITION_BUTTON_VISIBLE),
+                equalTo(false));
         assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_INCOGNITO), equalTo(true));
 
         doReturn(false).when(mTabModelSelector).isIncognitoSelected();
@@ -705,6 +713,8 @@ public class StartSurfaceMediatorUnitTest {
         assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_INCOGNITO), equalTo(true));
         assertThat(
                 mSecondaryTasksSurfacePropertyModel.get(IS_FAKE_SEARCH_BOX_VISIBLE), equalTo(true));
+        assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_VOICE_RECOGNITION_BUTTON_VISIBLE),
+                equalTo(false));
         assertThat(mSecondaryTasksSurfacePropertyModel.get(IS_INCOGNITO), equalTo(true));
     }
 
