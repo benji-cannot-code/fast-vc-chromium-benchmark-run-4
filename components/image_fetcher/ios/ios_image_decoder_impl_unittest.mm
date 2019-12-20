65f3b2073b47122d23dd739fd13de54ba800733b
Ken Rockot <rockot@google.com>
2019-12-20 18:37:16 +0000

    🚫Bind: Miscellanous components/ subdirs
    Migrates some directories off of deprecated base::Bind, base::Callback,
    etc, and onto the newer APIs.
    
    Specifically this covers:
    
    - components/guest_view/
    - components/heap_profiling/
    - components/image_fetcher/
    - components/login/
    
    All fairly trivial.
    
    Fixed: 1007700, 1007697, 1007695, 1007694
    Change-Id: I666bd5a19221f275f1ec57ccf710a849d4d88cc2
    Tbr: hanxi@chromium.org
    Tbr: wylieb@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978237
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#726862}

--- components/image_fetcher/ios/ios_image_decoder_impl_unittest.mm
+++ components/image_fetcher/ios/ios_image_decoder_impl_unittest.mm
@@ -79,8 +79,8 @@ TEST_F(IOSImageDecoderImplTest, JPGImage) {
       std::string(reinterpret_cast<char*>(kJPGImage), sizeof(kJPGImage));
   ios_image_decoder_impl_->DecodeImage(
       image_data, gfx::Size(),
-      base::Bind(&IOSImageDecoderImplTest::OnImageDecoded,
-                 base::Unretained(this)));
+      base::BindOnce(&IOSImageDecoderImplTest::OnImageDecoded,
+                     base::Unretained(this)));
 
   scoped_task_evironment_.RunUntilIdle();
 
@@ -94,8 +94,8 @@ TEST_F(IOSImageDecoderImplTest, WebpImage) {
       std::string(reinterpret_cast<char*>(kWEBPImage), sizeof(kWEBPImage));
   ios_image_decoder_impl_->DecodeImage(
       image_data, gfx::Size(),
-      base::Bind(&IOSImageDecoderImplTest::OnImageDecoded,
-                 base::Unretained(this)));
+      base::BindOnce(&IOSImageDecoderImplTest::OnImageDecoded,
+                     base::Unretained(this)));
 
   scoped_task_evironment_.RunUntilIdle();
 
