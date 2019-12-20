ba8ce8da5c5ce6593a08840250cd14808383e9d6
Mounir Lamouri <mlamouri@chromium.org>
2019-12-20 08:25:48 +0000

    XR: remove check for insecure context from the browser process.
    These checks are not done in the browser process. The API is already
    restricted to secure origins.
    
    Bug: None
    Change-Id: Iedada4f84171b2af0e6427da3845a71fee4fd201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1975727
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726726}

--- chrome/browser/vr/service/vr_service_impl.cc
+++ chrome/browser/vr/service/vr_service_impl.cc
@@ -40,16 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-bool IsSecureContext(content::RenderFrameHost* host) {
-  if (!host)
-    return false;
-  while (host != nullptr) {
-    if (!content::IsOriginSecure(host->GetLastCommittedURL()))
-      return false;
-    host = host->GetParent();
-  }
-  return true;
-}
 
 device::mojom::XRRuntimeSessionOptionsPtr GetRuntimeOptions(
     device::mojom::XRSessionOptions* options) {
@@ -369,14 +359,6 @@ void VRServiceImpl::RequestSession(
     pending_requests_.push_back(
         base::BindOnce(&VRServiceImpl::RequestSession, base::Unretained(this),
                        std::move(options), std::move(callback)));
-    return;
-  }
-
-  // Check that the request satisfies secure context requirements.
-  if (!IsSecureContext(render_frame_host_)) {
-    std::move(callback).Run(
-        device::mojom::RequestSessionResult::NewFailureReason(
-            device::mojom::RequestSessionError::ORIGIN_NOT_SECURE));
     return;
   }
 
