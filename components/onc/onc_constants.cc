1752603252e8c361e52887892b422e995b8f4e19
Brian Norris <briannorris@chromium.org>
2019-12-20 18:56:46 +0000

    Drop RoamThreshold configurability
    This feature
    (a) currently doesn't do much on most boards (we've introduced different
        roaming heuristics that override this one)
    (b) was never really proven to be useful, even to the Feature Requester
        (see ${BUG})
    (c) has no real testing
    (d) is not actually exposed to Enterprise management -- so it only could
        possibly be used in the obscure manual ONC configuration settings.
    
    Drop it.
    
    Bug: 1035191
    Change-Id: I9e54a104f1e1a082782e99f5e48f4ad00f607c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974691
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Brian Norris <briannorris@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726874}

--- components/onc/onc_constants.cc
+++ components/onc/onc_constants.cc
@@ -227,7 +227,6 @@ const char kFTEnabled[] = "FTEnabled";
 const char kHexSSID[] = "HexSSID";
 const char kHiddenSSID[] = "HiddenSSID";
 const char kPassphrase[] = "Passphrase";
-const char kRoamThreshold[] = "RoamThreshold";
 const char kSSID[] = "SSID";
 const char kSecurity[] = "Security";
 const char kSecurityNone[] = "None";
