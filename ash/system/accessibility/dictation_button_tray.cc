dcb3675a1f4fc29052cc8449557fe825e96beae3
Tim Song <tengs@chromium.org>
2019-12-20 22:56:40 +0000

    Ash Tray: Fix crash when toggling mirror mode display.
    The DictationButtonTray is updating its visibility in the constructor, which
    causes a crash now that we have more complicated visibility logic. Instead move
    the visibility update to the Initialize() function.
    
    TEST=manually verified on device
    BUG=1036052
    
    Change-Id: I887a47ac2a655994b99d438df1b644d8fe75fd98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1976857
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726967}

--- ash/system/accessibility/dictation_button_tray.cc
+++ ash/system/accessibility/dictation_button_tray.cc
@@ -24,8 +24,6 @@ namespace ash {
 
 DictationButtonTray::DictationButtonTray(Shelf* shelf)
     : TrayBackgroundView(shelf), icon_(new views::ImageView()) {
-  UpdateVisibility();
-
   off_image_ = gfx::CreateVectorIcon(kDictationOffNewuiIcon,
                                      ShelfConfig::Get()->shelf_icon_color());
   on_image_ = gfx::CreateVectorIcon(kDictationOnNewuiIcon,
@@ -53,6 +51,11 @@ bool DictationButtonTray::PerformAction(const ui::Event& event) {
 
   CheckDictationStatusAndUpdateIcon();
   return true;
+}
+
+void DictationButtonTray::Initialize() {
+  TrayBackgroundView::Initialize();
+  UpdateVisibility();
 }
 
 void DictationButtonTray::ClickedOutsideBubble() {}
