4b972a59ea7dc6d952df302d365e3966cad089d9
Brian Sheedy <bsheedy@chromium.org>
2019-12-20 18:12:11 +0000

    Add ArCore playback functionality
    Adds the ability for AR instrumentation tests to play back recorded
    AR datasets, allowing ArCore to have consistent camera and sensor data.
    This allows us to add tests for parts of the API that require actual
    data such as hit testing.
    
    This is achieved by adding a manifest entry telling ArCore where to
    look for the dataset, then copying the desired dataset to that location
    before running the test.
    
    Bug: 1035690
    Change-Id: I37f01a410f126476d2c9f64063a9f59e5feade4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974535
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726848}

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrArSessionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr/WebXrArSessionTest.java
@@ -25,8 +25,8 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.vr.rules.XrActivityRestriction;
+import org.chromium.chrome.browser.vr.util.ArTestRuleUtils;
 import org.chromium.chrome.browser.vr.util.PermissionUtils;
-import org.chromium.chrome.browser.vr.util.XrTestRuleUtils;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.content_public.browser.WebContents;
@@ -45,7 +45,7 @@ import java.util.concurrent.Callable;
 public class WebXrArSessionTest {
     @ClassParameter
     private static List<ParameterSet> sClassParams =
-            XrTestRuleUtils.generateDefaultTestRuleParameters();
+            ArTestRuleUtils.generateDefaultTestRuleParameters();
     @Rule
     public RuleChain mRuleChain;
 
@@ -54,7 +54,7 @@ public class WebXrArSessionTest {
 
     public WebXrArSessionTest(Callable<ChromeActivityTestRule> callable) throws Exception {
         mTestRule = callable.call();
-        mRuleChain = XrTestRuleUtils.wrapRuleInActivityRestrictionRule(mTestRule);
+        mRuleChain = ArTestRuleUtils.wrapRuleInActivityRestrictionRule(mTestRule);
     }
 
     @Before
