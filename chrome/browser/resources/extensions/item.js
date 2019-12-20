6be82bc6ef5d985aa892d1f8deb0fb33e2497349
Archana Simha <archanasimha@chromium.org>
2019-12-20 21:48:36 +0000

    [Extensions Metrics] Add enable, disable, and removal user actions.
    Design Doc: http://go/extensionscheckupmetrics
    
    Bug: 1034541
    Change-Id: Ie29b858206e2570c13a4442ed53db0c7ec18cc06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965996
    Commit-Queue: Archana Simha <archanasimha@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726944}

--- chrome/browser/resources/extensions/item.js
+++ chrome/browser/resources/extensions/item.js
@@ -207,7 +207,7 @@ Polymer({
 
   /** @private */
   onEnableChange_: function() {
-    this.delegate.setItemEnabled(this.data.id, this.$['enable-toggle'].checked);
+    this.delegate.setItemEnabled(this.data.id, this.$.enableToggle.checked);
   },
 
   /** @private */
