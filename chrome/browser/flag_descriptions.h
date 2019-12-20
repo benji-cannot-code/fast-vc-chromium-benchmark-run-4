3d9b3173d8d0fd072e37bb178ba1af7e8404e505
Jason Lin <lxj@google.com>
2019-12-20 02:11:52 +0000

    Remove flag crostini-webui-installer
    We by default use the webui installer. In addition, the deprecated
    CrostiniInstallerView has been removed. So, this flag is useless now.
    
    
    Bug: 1026199
    Change-Id: I99d45670f39bc032cc017bfe03a4117b294040b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1963595
    Commit-Queue: Jason Lin <lxj@google.com>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726646}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1986,9 +1986,6 @@ extern const char kCrostiniGpuSupportDescription[];
 extern const char kCrostiniUsbAllowUnsupportedName[];
 extern const char kCrostiniUsbAllowUnsupportedDescription[];
 
-extern const char kCrostiniWebUIInstallerName[];
-extern const char kCrostiniWebUIInstallerDescription[];
-
 extern const char kCrostiniWebUIUpgraderName[];
 extern const char kCrostiniWebUIUpgraderDescription[];
 
--- chrome/test/data/webui/chromeos/crostini_installer_browsertest.js
+++ chrome/test/data/webui/chromeos/crostini_installer_browsertest.js
@@ -25,7 +25,6 @@ CrostiniInstallerBrowserTest.prototype = {
 
   featureList: {
     enabled: [
-      'chromeos::features::kCrostiniWebUIInstaller',
       'network::features::kOutOfBlinkCors'
     ]
   },
