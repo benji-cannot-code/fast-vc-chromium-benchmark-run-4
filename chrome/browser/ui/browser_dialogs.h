16bdea3ac8ccbea0ea3daa5a39c65fa53c0fceda
Alan Cutter <alancutter@chromium.org>
2019-12-20 00:41:34 +0000

    Remove unused ShowPWAInstallDialog()
    Change-Id: Ibf17313912ad4ce5f0b41cb1b9032dc70ec16690
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1971291
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726612}

--- chrome/browser/ui/browser_dialogs.h
+++ chrome/browser/ui/browser_dialogs.h
@@ -112,13 +112,6 @@ void ShowBookmarkAppDialog(content::WebContents* web_contents,
 void SetAutoAcceptBookmarkAppDialogForTesting(bool auto_accept,
                                               bool auto_open_in_window);
 
-// Shows the PWA installation confirmation modal dialog.
-//
-// |web_app_info| is the WebApplicationInfo to be installed.
-void ShowPWAInstallDialog(content::WebContents* web_contents,
-                          std::unique_ptr<WebApplicationInfo> web_app_info,
-                          AppInstallationAcceptanceCallback callback);
-
 // Shows the PWA installation confirmation bubble anchored off the PWA install
 // icon in the omnibox.
 //
@@ -127,8 +120,8 @@ void ShowPWAInstallBubble(content::WebContents* web_contents,
                           std::unique_ptr<WebApplicationInfo> web_app_info,
                           AppInstallationAcceptanceCallback callback);
 
-// Sets whether |ShowPWAInstallDialog| and |ShowPWAInstallBubble| should accept
-// immediately without any user interaction.
+// Sets whether |ShowPWAInstallBubble| should accept immediately without any
+// user interaction.
 void SetAutoAcceptPWAInstallConfirmationForTesting(bool auto_accept);
 
 #if defined(OS_MACOSX)
