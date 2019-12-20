519fa3217e628d01f7b48abaadd3cbffbda20848
Archana Simha <archanasimha@chromium.org>
2019-12-20 00:43:53 +0000

    Update button reloads local extensions on chrome://extensions page.
    Bug: 986368
    Change-Id: I2fe3a93d911bfac94f04ba62863bf80fad830514
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1924724
    Commit-Queue: Archana Simha <archanasimha@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726615}

--- chrome/browser/resources/extensions/toolbar.js
+++ chrome/browser/resources/extensions/toolbar.js
@@ -34,9 +34,10 @@ export class ToolbarDelegate {
 
   /**
    * Updates all extensions.
+   * @param {!Array<!chrome.developerPrivate.ExtensionInfo>} extensions
    * @return {!Promise}
    */
-  updateAllExtensions() {}
+  updateAllExtensions(extensions) {}
 }
 
 Polymer({
@@ -45,6 +46,9 @@ Polymer({
   _template: html`{__html_template__}`,
 
   properties: {
+    /** @type {!Array<!chrome.developerPrivate.ExtensionInfo>} */
+    extensions: Array,
+
     /** @type {ToolbarDelegate} */
     delegate: Object,
 
@@ -189,16 +193,18 @@ Polymer({
     // Keep the toast open indefinitely.
     toastManager.duration = 0;
     toastManager.show(this.i18n('toolbarUpdatingToast'));
-    this.delegate.updateAllExtensions().then(
-        () => {
-          toastManager.hide();
-          toastManager.duration = 3000;
-          toastManager.show(this.i18n('toolbarUpdateDone'));
-          this.isUpdating_ = false;
-        },
-        () => {
-          toastManager.hide();
-          this.isUpdating_ = false;
-        });
+    this.delegate.updateAllExtensions(this.extensions)
+        .then(
+            () => {
+              toastManager.hide();
+              toastManager.duration = 3000;
+              toastManager.show(this.i18n('toolbarUpdateDone'));
+              this.isUpdating_ = false;
+            },
+            loadError => {
+              this.fire('load-error', loadError);
+              toastManager.hide();
+              this.isUpdating_ = false;
+            });
   },
 });
