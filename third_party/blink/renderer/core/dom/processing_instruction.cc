6e59e2333169ea893623c177fff23edfa5198f2d
Takashi Sakamoto <tasak@google.com>
2019-12-20 08:24:40 +0000

    ProcessingInstruction should not remove pending sheets when is_xsl_.
    Because ProcessingInstruction::Process only invokes AddPendingSheet when !is_xsl_. If removing pending sheets, StyleEngine will not find whether all pending parser blocking stylesheets are loaded or not correctly.
    
    Bug: 905827, 1035293
    Change-Id: Id617c2001601bef6e4e867ea1a193d5b4e8a51fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978540
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#726724}

--- third_party/blink/renderer/core/dom/processing_instruction.cc
+++ third_party/blink/renderer/core/dom/processing_instruction.cc
@@ -284,6 +284,8 @@ void ProcessingInstruction::ClearSheet() {
 }
 
 void ProcessingInstruction::RemovePendingSheet() {
+  if (is_xsl_)
+    return;
   GetDocument().GetStyleEngine().RemovePendingSheet(*this,
                                                     style_engine_context_);
 }
