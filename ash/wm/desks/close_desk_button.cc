0c2c1aa0ec2d8e1b6e4bc166e9405859ece1d4a3
Ahmed Fakhry <afakhry@google.com>
2019-12-20 22:28:22 +0000

    Desks: Add tooltip for the desk close button
    BUG=1006204
    
    Change-Id: Ibf37042ed2e5f57671bcba707d83645d4713669e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1979255
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726955}

--- ash/wm/desks/close_desk_button.cc
+++ ash/wm/desks/close_desk_button.cc
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/style/ash_color_provider.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/paint_vector_icon.h"
+#include "ui/strings/grit/ui_strings.h"
 #include "ui/views/animation/flood_fill_ink_drop_ripple.h"
 #include "ui/views/animation/ink_drop_impl.h"
 #include "ui/views/animation/ink_drop_mask.h"
@@ -45,6 +47,7 @@ CloseDeskButton::CloseDeskButton(views::ButtonListener* listener)
   SetBackground(
       CreateBackgroundFromPainter(views::Painter::CreateSolidRoundRectPainter(
           icon_background_color, kCornerRadius)));
+  SetTooltipText(l10n_util::GetStringUTF16(IDS_APP_ACCNAME_CLOSE));
 
   AshColorProvider::RippleAttributes ripple_attributes =
       AshColorProvider::Get()->GetRippleAttributes(icon_background_color);
