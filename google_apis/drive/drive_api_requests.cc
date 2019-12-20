28c79df0c492cc5566288028bf97d40108f3bb61
Anand K. Mistry <amistry@chromium.org>
2019-12-20 00:55:47 +0000

    Convert google_apis::PrepareCallback to OnceCallback
    Bug: 1007788
    Change-Id: Ic449af55f727461f70e7d130f0d489f86a731572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974297
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Auto-Submit: Anand Mistry <amistry@chromium.org>
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726625}

--- google_apis/drive/drive_api_requests.cc
+++ google_apis/drive/drive_api_requests.cc
@@ -1129,8 +1129,8 @@ SingleBatchableDelegateRequest::GetExtraRequestHeaders() const {
   return delegate_->GetExtraRequestHeaders();
 }
 
-void SingleBatchableDelegateRequest::Prepare(const PrepareCallback& callback) {
-  delegate_->Prepare(callback);
+void SingleBatchableDelegateRequest::Prepare(PrepareCallback callback) {
+  delegate_->Prepare(std::move(callback));
 }
 
 bool SingleBatchableDelegateRequest::GetContentData(
@@ -1226,10 +1226,10 @@ void BatchUploadRequest::Commit() {
   }
 }
 
-void BatchUploadRequest::Prepare(const PrepareCallback& callback) {
+void BatchUploadRequest::Prepare(PrepareCallback callback) {
   DCHECK(CalledOnValidThread());
-  DCHECK(!callback.is_null());
-  prepare_callback_ = callback;
+  DCHECK(callback);
+  prepare_callback_ = std::move(callback);
   MayCompletePrepare();
 }
 
@@ -1291,7 +1291,7 @@ void BatchUploadRequest::MayCompletePrepare() {
   for (size_t i = 0; i < child_requests_.size(); ++i) {
     child_requests_[i]->data_offset += part_data_offset[i];
   }
-  prepare_callback_.Run(HTTP_SUCCESS);
+  std::move(prepare_callback_).Run(HTTP_SUCCESS);
 }
 
 bool BatchUploadRequest::GetContentData(std::string* upload_content_type,
