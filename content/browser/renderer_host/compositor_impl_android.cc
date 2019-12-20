f0682bcc32677600e55122ab92d4b51cf533a67f
Saman Sami <samans@chromium.org>
2019-12-20 20:18:08 +0000

    Fix crash in CompositorImpl when adding/removing a child FrameSink
    TearDownDisplayAndUnregisterRootFrameSink() performs a sync call to the
    GPU process and in the meantime a sync call arrives from the renderer
    that attempts to modify the FrameSink hierarchy and we get in a bad
    state. Always check that the root FrameSinkId is registered before
    attempting to add a child.
    
    Bug: 1030618
    Change-Id: Ibb58dc65ef71ce9c4af063a0d255729d6a277124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1976558
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Auto-Submit: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726908}

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -440,7 +440,6 @@ void CompositorImpl::SetVisible(bool visible) {
     // Hide the LayerTreeHost and release its frame sink.
     host_->SetVisible(false);
     host_->ReleaseLayerTreeFrameSink();
-    has_layer_tree_frame_sink_ = false;
     pending_frames_ = 0;
 
     // Notify CompositorDependenciesAndroid of visibility changes last, to
@@ -485,6 +484,9 @@ void CompositorImpl::RegisterRootFrameSink() {
       frame_sink_id_, this, viz::ReportFirstSurfaceActivation::kNo);
   GetHostFrameSinkManager()->SetFrameSinkDebugLabel(frame_sink_id_,
                                                     "CompositorImpl");
+  for (auto& frame_sink_id : pending_child_frame_sink_ids_)
+    AddChildFrameSink(frame_sink_id);
+  pending_child_frame_sink_ids_.clear();
 }
 
 void CompositorImpl::SetWindowBounds(const gfx::Size& size) {
@@ -548,11 +550,6 @@ void CompositorImpl::RequestNewLayerTreeFrameSink() {
 
 void CompositorImpl::DidInitializeLayerTreeFrameSink() {
   layer_tree_frame_sink_request_pending_ = false;
-  has_layer_tree_frame_sink_ = true;
-  for (auto& frame_sink_id : pending_child_frame_sink_ids_)
-    AddChildFrameSink(frame_sink_id);
-
-  pending_child_frame_sink_ids_.clear();
 }
 
 void CompositorImpl::DidFailToInitializeLayerTreeFrameSink() {
@@ -676,7 +673,6 @@ void CompositorImpl::DidReceiveCompositorFrameAck() {
 
 void CompositorImpl::DidLoseLayerTreeFrameSink() {
   TRACE_EVENT0("compositor", "CompositorImpl::DidLoseLayerTreeFrameSink");
-  has_layer_tree_frame_sink_ = false;
   client_->DidSwapFrame(0);
 }
 
@@ -712,11 +708,10 @@ viz::FrameSinkId CompositorImpl::GetFrameSinkId() {
 }
 
 void CompositorImpl::AddChildFrameSink(const viz::FrameSinkId& frame_sink_id) {
-  if (has_layer_tree_frame_sink_) {
+  if (GetHostFrameSinkManager()->IsFrameSinkIdRegistered(frame_sink_id_)) {
     bool result = GetHostFrameSinkManager()->RegisterFrameSinkHierarchy(
         frame_sink_id_, frame_sink_id);
-    // TODO(samans): Delete this once https://crbug.com/1030618 is resolved.
-    CHECK(result);
+    DCHECK(result);
   } else {
     pending_child_frame_sink_ids_.insert(frame_sink_id);
   }
