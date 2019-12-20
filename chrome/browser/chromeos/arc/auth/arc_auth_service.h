a67f1a134135746aba752a492e1285581941f33d
Anastasiia Nikolaienko <anastasiian@chromium.org>
2019-12-20 09:32:34 +0000

    ARC: Add a mojo method for getting the primary account
    ARC needs to know the identity of the primary account, but it cannot
    retrieve that from Android account manager.
    
    Add a mojo method in ArcAuthService for returning the primary account
    information from Chrome OS.
    
    Bug: b/141108534
    Test: browser_tests --gtest_filter="*Arc*AuthService*Test*"
    Change-Id: I01ec976a330d52421ab2ddfe5155c712c81967af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1810359
    Commit-Queue: Anastasiia Nikolaienko <anastasiian@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726738}

--- chrome/browser/chromeos/arc/auth/arc_auth_service.h
+++ chrome/browser/chromeos/arc/auth/arc_auth_service.h
@@ -66,6 +66,8 @@ class ArcAuthService : public KeyedService,
   // OS Account Manager.
   void GetGoogleAccountsInArc(GetGoogleAccountsInArcCallback callback);
 
+  void RequestPrimaryAccount(RequestPrimaryAccountCallback callback) override;
+
   // For supporting ArcServiceManager::GetService<T>().
   static const char kArcServiceName[];
 
