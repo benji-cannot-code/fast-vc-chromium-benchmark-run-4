bff232623243d3e6043305be884c9e080bda3ba8
Ahmed Fakhry <afakhry@google.com>
2019-12-20 01:42:16 +0000

    TopControlsSlide: Allow only a single BrowserView::Layout() once sliding begins
    If BrowserView::Layout() gets called multiple times while sliding
    the top controls is in progress, it could break sliding altogether,
    or at least will make its initiation janky.
    
    This CL allows only a single layout to go through once sliding begins.
    
    BUG=1034047, 950981
    TEST=Manually, and expanded an existing test.
    
    Change-Id: I7b906fc2956aa455b573083d7cdaa40812141bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1968320
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726637}

--- chrome/browser/ui/views/frame/top_controls_slide_controller_chromeos.h
+++ chrome/browser/ui/views/frame/top_controls_slide_controller_chromeos.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/top_controls_slide_controller.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "ui/display/display_observer.h"
 
 class BrowserView;
 class TopControlsSlideTabObserver;
@@ -37,11 +38,11 @@ class TopControlsSlideTabObserver;
 // - Exiting tablet mode.
 // - Entering immersive fullscreen mode.
 // - Page security level changes.
-class TopControlsSlideControllerChromeOS
-    : public TopControlsSlideController,
-      public ash::TabletModeObserver,
-      public TabStripModelObserver,
-      public content::NotificationObserver {
+class TopControlsSlideControllerChromeOS : public TopControlsSlideController,
+                                           public ash::TabletModeObserver,
+                                           public TabStripModelObserver,
+                                           public content::NotificationObserver,
+                                           public display::DisplayObserver {
  public:
   explicit TopControlsSlideControllerChromeOS(BrowserView* browser_view);
   ~TopControlsSlideControllerChromeOS() override;
@@ -55,6 +56,7 @@ class TopControlsSlideControllerChromeOS
       const content::WebContents* contents) const override;
   void SetTopControlsGestureScrollInProgress(bool in_progress) override;
   bool IsTopControlsGestureScrollInProgress() const override;
+  bool IsTopControlsSlidingInProgress() const override;
 
   // ash::TabletModeObserver:
   void OnTabletModeStarted() override;
@@ -71,6 +73,10 @@ class TopControlsSlideControllerChromeOS
   void Observe(int type,
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
+
+  // display::DisplayObserver:
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t changed_metrics) override;
 
  private:
   // Returns true if this feature can be turned on. If |fullscreen_state| is
@@ -145,6 +151,18 @@ class TopControlsSlideControllerChromeOS
   // false, the layer transforms are reset to identity and the browser view is
   // re-laid out.
   bool is_sliding_in_progress_ = false;
+
+  // Set to true when the state of this feature is temporarily disabled when
+  // some display metrics change while sliding and scrolling are both in
+  // progress, so that once the user lifts their finger to end the gesture
+  // scrolling, the enabled state is updated to the correct value.
+  bool update_state_after_gesture_scrolling_ends_ = false;
+
+  // True when it's desired to ignore shown ratios sent by the renderer for this
+  // tab. This happens when a display property changes while sliding and gesture
+  // scrolling are in progress, in which case we force-set the top controls to
+  // fully shown, and ignore any renderer-sent values.
+  bool pause_updates_ = false;
 
   // We need to observe the tab's web contents to listen to events that affect
   // the browser top controls shown state for each tab.
