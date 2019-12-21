fbabf6e6af815747be7f8cc843f969ce32f28f19
rbpotter <rbpotter@chromium.org>
2019-12-21 02:38:34 +0000

    SplitSettings: More cleanup of about page
    - Delete powerwash dialog from browser settings
    - Remove settings animated pages, main page behavior, and route
      observer behavior from browser about page
    
    Bug: 990170
    Change-Id: I1f2f0a1eb454d55c8f84b7374c7be4e66b238ea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1979082
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727019}

--- chrome/browser/resources/settings/about_page/about_page.js
+++ chrome/browser/resources/settings/about_page/about_page.js
@@ -13,8 +13,6 @@ Polymer({
 
   behaviors: [
     WebUIListenerBehavior,
-    settings.MainPageBehavior,
-    settings.RouteObserverBehavior,
     I18nBehavior,
   ],
 
@@ -106,20 +104,6 @@ Polymer({
       this.aboutBrowserProxy_.requestUpdate();
     }
     // </if>
-  },
-
-  /**
-   * @param {!settings.Route} newRoute
-   * @param {settings.Route} oldRoute
-   */
-  currentRouteChanged: function(newRoute, oldRoute) {
-    settings.MainPageBehavior.currentRouteChanged.call(
-        this, newRoute, oldRoute);
-  },
-
-  // Override settings.MainPageBehavior method.
-  containsRoute: function(route) {
-    return !route || settings.routes.ABOUT.contains(route);
   },
 
   // <if expr="not chromeos">
