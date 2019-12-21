9e3ceaacdd91cbaf222f870ce02f4e90f8950562
Kenneth Russell <kbr@chromium.org>
2019-12-21 00:25:54 +0000

    Address comments from zmo on an earlier CL.
    Address zmo's comments on the earlier change:
    https://chromium-review.googlesource.com/1885636
    which were published after the CL landed.
    
    Bug: 681341
    Change-Id: Ib89967cc4f6b7f11371e7694689445d8afc34883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977739
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Auto-Submit: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726997}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.cc
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrContext.h"
 #include "third_party/skia/include/gpu/gl/GrGLTypes.h"
-#include "ui/gl/gpu_preference.h"
 #include "v8/include/v8.h"
 
 namespace blink {
