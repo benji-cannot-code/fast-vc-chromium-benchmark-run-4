db8df3aadee3659d78a1e83c4ce5dc8c20ebc7e6
Brandon Wylie <wylieb@chromium.org>
2019-12-21 00:25:43 +0000

    Revert "Reparent the current Tab when the theme is changed"
    This reverts commit 24eca4b91d4023762573f0712a7fe67525c59d7d.
    
    Reason for revert: Crashing Tot
    
    Original change's description:
    > Reparent the current Tab when the theme is changed
    >
    > * Creating a class which handles reparenting the current tab when the
    >   theme is swapped.
    > * Hooking that class into ChromeActivity.
    > * Adding plumbing to a few ReparentingTask/TabReparentingParams to
    >   facilitate the process.
    >
    > Bug: 944302
    > Change-Id: I2801eba979b3acafa02856a9053bc474bf2f581c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1931763
    > Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    > Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#726136}
    
    TBR=fgorski@chromium.org,yusufo@chromium.org,wylieb@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 944302
    Change-Id: If032ff5e4cf61f4047e4cf146c8ae6a03a78aa77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1979398
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726996}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/ChromeTabCreator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/ChromeTabCreator.java
@@ -26,7 +26,6 @@ import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tab.TabParentIntent;
 import org.chromium.chrome.browser.tab.TabRedirectHandler;
 import org.chromium.chrome.browser.tab.TabState;
-import org.chromium.chrome.browser.tab_activity_glue.ReparentingDelegateFactory;
 import org.chromium.chrome.browser.tab_activity_glue.ReparentingTask;
 import org.chromium.chrome.browser.util.IntentUtils;
 import org.chromium.components.url_formatter.UrlFormatter;
@@ -130,8 +129,7 @@ public class ChromeTabCreator extends TabCreatorManager.TabCreator {
                 TabReparentingParams params = (TabReparentingParams) asyncParams;
                 tab = params.getTabToReparent();
                 ReparentingTask.from(tab).finish(
-                        ReparentingDelegateFactory.createReparentingTaskDelegate(mActivity),
-                        params.getFinalizeCallback());
+                        mActivity, createDefaultTabDelegateFactory(), params.getFinalizeCallback());
             } else if (asyncParams != null && asyncParams.getWebContents() != null) {
                 openInForeground = true;
                 WebContents webContents = asyncParams.getWebContents();
