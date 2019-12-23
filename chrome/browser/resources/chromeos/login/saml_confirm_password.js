ab9fe113c7761a29a1b7645bf8c37c0b36f86b68
Denis Kuznetsov <antrim@google.com>
2019-12-23 18:16:57 +0000

    Reland: Replace i18n-content with i18nDynamic in ARC TOS and SAML password screens
    Initially reviewed on: https://chromium-review.googlesource.com/c/chromium/src/+/1958136
    
    Bug: 955194
    Change-Id: I061c4d0fe4c54c9c1bfb58ca741ed1cce5bc475c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980177
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727185}

--- chrome/browser/resources/chromeos/login/saml_confirm_password.js
+++ chrome/browser/resources/chromeos/login/saml_confirm_password.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'saml-confirm-password',
 
+  behaviors: [OobeI18nBehavior],
+
   properties: {
     email: String,
 
