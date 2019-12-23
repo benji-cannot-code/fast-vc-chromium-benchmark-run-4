8e0b5046f95dc3215b882677c82ab9a8cc1187d5
Denis Kuznetsov <antrim@google.com>
2019-12-23 17:28:28 +0000

    Reland of: Use proper localization mechanism for throbber-notice element.
    Bug: 955194
    
    Previously reviewed on: https://chromium-review.googlesource.com/c/chromium/src/+/1953626
    
    Change-Id: I4408d126089be1c32710d9f4e564a84ef2f3ec7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980176
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727174}

--- chrome/browser/resources/chromeos/login/throbber_notice.js
+++ chrome/browser/resources/chromeos/login/throbber_notice.js
@@ -6,5 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'throbber-notice',
 
-  properties: {text: String}
+  behaviors: [OobeI18nBehavior],
+
+  properties: {textKey: String}
 });
