f6f41a50cb7aa5ec167e8f705fa7dd8831241152
Sammie Quon <sammiequon@chromium.org>
2019-12-23 16:33:08 +0000

    overview: Fix using arrow keys in tablet mode.
    Arrow keys cause a double selection movement in tablet mode. This was a
    regression caused by introducing ctrl + arrow key to move the scrollable
    grid. This patch fixes that by handling on key presses. Also stops scrolling
    if ctrl is lifted before arrow key.
    
    Test: added regression test
    Change-Id: I89595a37880e85cbb903b0039785b20e8f7d0f71
    Fixed: 1036140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977843
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727167}

--- ash/wm/overview/overview_highlight_controller_unittest.cc
+++ ash/wm/overview/overview_highlight_controller_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_item_view.h"
 #include "ash/wm/overview/overview_test_util.h"
 #include "ash/wm/overview/scoped_overview_transform_window.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller_test_api.h"
 #include "ash/wm/window_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "ui/aura/window.h"
@@ -72,6 +73,30 @@ TEST_F(OverviewHighlightControllerTest, BasicTabKeyNavigation) {
   EXPECT_EQ(overview_windows[1]->GetWindow(), GetOverviewHighlightedWindow());
   SendKeyUntilOverviewItemIsHighlighted(ui::VKEY_TAB);
   EXPECT_EQ(overview_windows[0]->GetWindow(), GetOverviewHighlightedWindow());
+  SendKeyUntilOverviewItemIsHighlighted(ui::VKEY_RIGHT);
+  EXPECT_EQ(overview_windows[1]->GetWindow(), GetOverviewHighlightedWindow());
+  SendKeyUntilOverviewItemIsHighlighted(ui::VKEY_LEFT);
+  EXPECT_EQ(overview_windows[0]->GetWindow(), GetOverviewHighlightedWindow());
+}
+
+// Same as above but for tablet mode. Regression test for crbug.com/1036140.
+TEST_F(OverviewHighlightControllerTest, BasicTabKeyNavigationTablet) {
+  std::unique_ptr<aura::Window> window1(CreateTestWindow());
+  std::unique_ptr<aura::Window> window2(CreateTestWindow());
+  std::unique_ptr<aura::Window> window3(CreateTestWindow());
+
+  TabletModeControllerTestApi().EnterTabletMode();
+  ToggleOverview();
+  const std::vector<std::unique_ptr<OverviewItem>>& overview_windows =
+      GetOverviewItemsForRoot(0);
+  SendKeyUntilOverviewItemIsHighlighted(ui::VKEY_TAB);
+  EXPECT_EQ(overview_windows[0]->GetWindow(), GetOverviewHighlightedWindow());
+  SendKeyUntilOverviewItemIsHighlighted(ui::VKEY_TAB);
+  EXPECT_EQ(overview_windows[1]->GetWindow(), GetOverviewHighlightedWindow());
+  SendKeyUntilOverviewItemIsHighlighted(ui::VKEY_RIGHT);
+  EXPECT_EQ(overview_windows[2]->GetWindow(), GetOverviewHighlightedWindow());
+  SendKeyUntilOverviewItemIsHighlighted(ui::VKEY_LEFT);
+  EXPECT_EQ(overview_windows[1]->GetWindow(), GetOverviewHighlightedWindow());
 }
 
 // Tests that pressing Ctrl+W while a window is selected in overview closes it.
