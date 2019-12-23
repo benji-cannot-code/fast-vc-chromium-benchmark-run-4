e856483e15303e75e0b12f71f0dd98953dc52642
Robert Liao <robliao@chromium.org>
2019-12-23 05:28:20 +0000

    Run clang-tidy modernize-use-override on //base/win
    BUG=1034666
    TBR=wfh@chromium.org
    clang-tidy change.
    
    Change-Id: I6729a235c96ab9e508234cc20a87f849586614aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980315
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727120}

--- base/win/map_unittest.cc
+++ base/win/map_unittest.cc
@@ -215,7 +215,7 @@ class FakeMapChangedEventHandler
     EXPECT_HRESULT_SUCCEEDED(map_->add_MapChanged(this, &token_));
   }
 
-  ~FakeMapChangedEventHandler() {
+  ~FakeMapChangedEventHandler() override {
     EXPECT_HRESULT_SUCCEEDED(map_->remove_MapChanged(token_));
   }
 
--- base/win/reference.h
+++ base/win/reference.h
@@ -38,7 +38,7 @@ class Reference
   }
 
  private:
-  ~Reference() = default;
+  ~Reference() override = default;
   AbiT value_;
 
   DISALLOW_COPY_AND_ASSIGN(Reference);
