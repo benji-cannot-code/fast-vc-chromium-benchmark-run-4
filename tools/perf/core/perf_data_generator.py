31df44a04e6edc60747bd47d29e0b269776c0f2e
Haiyang Pan <hypan@google.com>
2019-12-23 19:32:12 +0000

    perf: use platform android-chrome-bundle for android-pixel2-perf-aab-fyi
    Requires crrev.com/c/1979399 to land first
    
    Bug: 986990
    Change-Id: I2e83a442b85a9c9ca494804d074b9cc51f01d669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1979590
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#727209}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -133,8 +133,7 @@ FYI_BUILDERS = {
         'isolate': 'performance_test_suite',
       }
     ],
-    'platform': 'android-chrome',
-    'browser': 'bin/monochrome_bundle',
+    'platform': 'android-chrome-bundle',
     'dimension': {
       'pool': 'chrome.tests.perf-fyi',
       'os': 'Android',
