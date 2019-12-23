f63e0edfebbe9b4cbf7f0ab7ff38bd79d2534959
Robert Woods <robertwoods@google.com>
2019-12-23 23:46:14 +0000

    WebApps: Add file handling support to BMO on CrOS.
    In this CL:
    - Save FileHandlers to WebAppDatabase
    - Implement GetAllFileHandlers in WebAppFileHandlerManager
    
    Bug: 938103
    Change-Id: I64a2115dc800b8216f7899d9f83ae19586b45db7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1958297
    Commit-Queue: Robert Woods <robertwoods@google.com>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727279}

--- chrome/browser/web_applications/web_app_file_handler_manager.h
+++ chrome/browser/web_applications/web_app_file_handler_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_FILE_HANDLER_MANAGER_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_FILE_HANDLER_MANAGER_H_
 
+#include <map>
 #include <vector>
 
 #include "chrome/browser/web_applications/components/file_handler_manager.h"
@@ -24,6 +25,15 @@ class WebAppFileHandlerManager : public FileHandlerManager {
  protected:
   const std::vector<apps::FileHandlerInfo>* GetAllFileHandlers(
       const AppId& app_id) override;
+
+ private:
+  using FileHandlerInfos = std::map<AppId, std::vector<apps::FileHandlerInfo>>;
+
+  // TODO(crbug.com/938103): At the moment, we have two equivalent
+  // representations of these data held in memory: here, and in WebApp. If
+  // GetAllFileHandlers can be modified to return a copy rather than a
+  // reference, there would be no need to cache here.
+  FileHandlerInfos file_handler_infos_;
 };
 
 }  // namespace web_app
