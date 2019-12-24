9bfd82f96813e80a55537cae54a291d10790c6c8
Gyuyoung Kim <gyuyoung@igalia.com>
2019-12-24 01:47:03 +0000

    Remove unused mojo binding.h includes in //media
    This CL cleans up unnecessary mojo/public/cpp/bindings/binding.h
    includes in //media.
    
    Bug: 955171
    Change-Id: I79b71fcf3453e49ef0d4909c7a71d3521f33ae62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977894
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#727316}

--- media/capture/video/mock_device_factory.h
+++ media/capture/video/mock_device_factory.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "media/capture/video/video_capture_device_factory.h"
-#include "mojo/public/cpp/bindings/binding.h"
 
 namespace media {
 
