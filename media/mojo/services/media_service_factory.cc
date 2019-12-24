6bfb9853054eb8ff3966b6e50a1bbe27177c6553
Dongheun Kang <dongheun.kang@lge.com>
2019-12-24 01:39:01 +0000

    media : Convert missing mojo media defines to buildflags
    Bug: none
    Change-Id: I117fcb0d9936ea73ad92fb9d5e9d4f180b42817e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974714
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727314}

--- media/mojo/services/media_service_factory.cc
+++ media/mojo/services/media_service_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/logging.h"
+#include "media/mojo/buildflags.h"
 #include "media/mojo/services/gpu_mojo_media_client.h"
 #include "media/mojo/services/media_service.h"
 #include "media/mojo/services/test_mojo_media_client.h"
@@ -20,7 +21,7 @@ namespace media {
 
 std::unique_ptr<MediaService> CreateMediaService(
     mojo::PendingReceiver<mojom::MediaService> receiver) {
-#if defined(ENABLE_TEST_MOJO_MEDIA_CLIENT)
+#if BUILDFLAG(ENABLE_TEST_MOJO_MEDIA_CLIENT)
   return CreateMediaServiceForTesting(std::move(receiver));
 #elif defined(OS_ANDROID)
   return std::make_unique<MediaService>(
