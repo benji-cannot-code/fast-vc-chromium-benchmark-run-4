ede20b530d11c8dd277e7b2d7cc7c72a31f47b24
Chih-Yu Huang <akahuang@chromium.org>
2019-12-26 02:36:42 +0000

    Revert "media/gpu/vaapi: Fill min and max qp in VAEncMiscParameterRateControl"
    This reverts commit f20690e8cc16b38649f4a97514a2536d230c369c.
    
    Reason for revert: This CL broke some CTS. b/146838257
    
    Original change's description:
    > media/gpu/vaapi: Fill min and max qp in VAEncMiscParameterRateControl
    > 
    > VAEncMiscParameterRateControl has parameters about minimum and
    > maximum of quantization values. These values should be filled.
    > 
    > Bug: 1034382
    > Test: VEA test on atlas
    > Change-Id: I185b3e710c05095313aa71bc9fe90b23c8885345
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969196
    > Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-by: Miguel Casas <mcasas@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#726700}
    
    TBR=mcasas@chromium.org,sprang@chromium.org,hiroh@chromium.org,sreerenj.balachandran@intel.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1034382
    Change-Id: I3bd3007985adfc72f0601b8995c2bf4fd91c47f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981961
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727435}

--- media/gpu/vaapi/vaapi_video_encode_accelerator.cc
+++ media/gpu/vaapi/vaapi_video_encode_accelerator.cc
@@ -1076,8 +1076,6 @@ bool VaapiVideoEncodeAccelerator::H264Accelerator::SubmitFrameParameters(
   rate_control_param.target_percentage = kTargetBitratePercentage;
   rate_control_param.window_size = encode_params.cpb_window_size_ms;
   rate_control_param.initial_qp = pic_param.pic_init_qp;
-  rate_control_param.min_qp = encode_params.min_qp;
-  rate_control_param.max_qp = encode_params.max_qp;
   rate_control_param.rc_flags.bits.disable_frame_skip = true;
 
   VAEncMiscParameterFrameRate framerate_param = {};
@@ -1288,8 +1286,6 @@ bool VaapiVideoEncodeAccelerator::VP8Accelerator::SubmitFrameParameters(
   rate_control_param.target_percentage = kTargetBitratePercentage;
   rate_control_param.window_size = encode_params.cpb_window_size_ms;
   rate_control_param.initial_qp = encode_params.initial_qp;
-  rate_control_param.min_qp = encode_params.min_qp;
-  rate_control_param.max_qp = encode_params.max_qp;
   rate_control_param.rc_flags.bits.disable_frame_skip = true;
 
   VAEncMiscParameterFrameRate framerate_param = {};
@@ -1426,8 +1422,6 @@ bool VaapiVideoEncodeAccelerator::VP9Accelerator::SubmitFrameParameters(
   rate_control_param.target_percentage = kTargetBitratePercentage;
   rate_control_param.window_size = encode_params.cpb_window_size_ms;
   rate_control_param.initial_qp = encode_params.initial_qp;
-  rate_control_param.min_qp = encode_params.min_qp;
-  rate_control_param.max_qp = encode_params.max_qp;
   rate_control_param.rc_flags.bits.disable_frame_skip = true;
 
   VAEncMiscParameterFrameRate framerate_param = {};
