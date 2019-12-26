20f1debfa083ea2deeca1376253c8199b15e0357
Andrey Kosyakov <caseq@chromium.org>
2019-12-26 22:16:28 +0000

    Revert "Headless: fix a crash in ~HeadlessWebContentsImpl"
    This reverts commit 20cefba5a5f59d91f9d3ba5639c08140271fe398.
    
    Reason for revert: suspected cause of crbug.com/1037652
    
    BUG: 1037652, 1034954
    
    Original change's description:
    > Headless: fix a crash in ~HeadlessWebContentsImpl
    > 
    > ... due to reentrancy while waiting for a [sync] call to
    > FrameSink::DestroyCompositorFrameSink() from Compositor destructor.
    > 
    > Bug: 1034954
    > Change-Id: I8b47560d18bdab38e40e67e6506d30a2043f1474
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977741
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#726858}
    
    TBR=caseq@chromium.org,kylechar@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1034954
    Change-Id: I244a6d9a9e36b8edb1297e9a3aa523b133f764e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982223
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727504}

--- headless/lib/browser/headless_web_contents_impl.cc
+++ headless/lib/browser/headless_web_contents_impl.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/stl_util.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/task/post_task.h"
 #include "base/trace_event/trace_event.h"
 #include "base/values.h"
 #include "components/security_state/content/content_utils.h"
@@ -306,10 +305,6 @@ HeadlessWebContentsImpl::~HeadlessWebContentsImpl() {
   agent_host_->RemoveObserver(this);
   if (render_process_host_)
     render_process_host_->RemoveObserver(this);
-  // Defer destruction of WindowTreeHost, as it does sync mojo calls
-  // in the destructor of ui::Compositor.
-  base::DeleteSoon(FROM_HERE, {base::CurrentThread()},
-                   std::move(window_tree_host_));
 }
 
 void HeadlessWebContentsImpl::RenderFrameCreated(
