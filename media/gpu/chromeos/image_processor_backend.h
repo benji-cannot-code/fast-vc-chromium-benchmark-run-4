3cffbda1c1a2d1a06fe365a9845ab89477bff3de
Hirokazu Honda <hiroh@chromium.org>
2019-12-26 07:03:36 +0000

    media/gpu: Change visible size to visible rect in ImageProcessor config
    It is necessary to configure an image processor with a visible
    rectangle if the image processor will crop a given frame.
    
    Bug: 1034415
    Test: vda_tests w/wo --use_vd and IP test
    Change-Id: I701cc4cf1c31bc69c59efc856f9ded6dc60248d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980519
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727445}

--- media/gpu/chromeos/image_processor_backend.h
+++ media/gpu/chromeos/image_processor_backend.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_frame.h"
 #include "media/gpu/chromeos/fourcc.h"
 #include "media/gpu/media_gpu_export.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 
 namespace media {
@@ -50,7 +51,7 @@ class MEDIA_GPU_EXPORT ImageProcessorBackend {
         Fourcc fourcc,
         const gfx::Size& size,
         const std::vector<ColorPlaneLayout>& planes,
-        const gfx::Size& visible_size,
+        const gfx::Rect& visible_rect,
         const std::vector<VideoFrame::StorageType>& preferred_storage_types);
     ~PortConfig();
 
@@ -64,7 +65,7 @@ class MEDIA_GPU_EXPORT ImageProcessorBackend {
     // Output human readable string of PortConfig.
     // Example:
     // PortConfig(format::NV12, size:640x480, planes:[(640, 0, 307200),
-    // (640,0,153600)], visible_size:640x480, storage_types:[DMABUFS])
+    // (640,0,153600)], visible_rect:0, 0, 640x480, storage_types:[DMABUFS])
     std::string ToString() const;
 
     // Video frame format represented as fourcc type.
@@ -78,7 +79,7 @@ class MEDIA_GPU_EXPORT ImageProcessorBackend {
 
     // Layout property (stride, offset, size of bytes) for each color plane.
     const std::vector<ColorPlaneLayout> planes;
-    const gfx::Size visible_size;
+    const gfx::Rect visible_rect;
     // List of preferred storage types.
     const std::vector<VideoFrame::StorageType> preferred_storage_types;
   };
