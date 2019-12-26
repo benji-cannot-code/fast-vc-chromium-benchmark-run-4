685cea13dc70990105b5f67e7cddc83d96596378
Bartek Nowierski <bartekn@chromium.org>
2019-12-26 09:17:59 +0000

    Revert "Emit "function calls in detached window" counters via DetachedWindows UKM event"
    This reverts commit 73a295cac297dd646cacb560a2a52e56b956b7b4.
    
    Reason for revert:
    Instrumentation no longer needed. It was meant only for 1 milestone.
    
    Original change's description:
    > Emit "function calls in detached window" counters via DetachedWindows UKM event
    > 
    > Privacy review:
    > https://docs.google.com/document/d/1cgwwc8jY7A3MLCHAh7yvONTAWTUTlsjnBsHp2oIuMv0/edit?usp=sharing
    > 
    > Change-Id: Ic7ee5a3664d592230e1e120391724dd4e7a0c5dd
    > Bug: chromium:1018156
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965747
    > Reviewed-by: Brian White <bcwhite@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    > Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#727045}
    
    TBR=dcheng@chromium.org,haraken@chromium.org,keishi@chromium.org,bcwhite@chromium.org,bmcquade@chromium.org,altimin@chromium.org,bartekn@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:1018156
    Change-Id: Ifad3f6c0164c82da2b1d2bf6ef0c3b0a1c1151fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982338
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727449}

--- components/ukm/content/source_url_recorder.cc
+++ components/ukm/content/source_url_recorder.cc
@@ -81,7 +81,6 @@ class SourceUrlRecorderWebContentsObserver
 
   // blink::mojom::UkmSourceIdFrameHost
   void SetDocumentSourceId(int64_t source_id) override;
-  void GetNavigationSourceId(GetNavigationSourceIdCallback callback) override;
 
  private:
   explicit SourceUrlRecorderWebContentsObserver(
@@ -317,11 +316,6 @@ ukm::SourceId SourceUrlRecorderWebContentsObserver::GetLastCommittedSourceId()
 ukm::SourceId SourceUrlRecorderWebContentsObserver::
     GetLastCommittedFullNavigationOrSameDocumentSourceId() const {
   return last_committed_full_navigation_or_same_document_source_id_;
-}
-
-void SourceUrlRecorderWebContentsObserver::GetNavigationSourceId(
-    GetNavigationSourceIdCallback callback) {
-  std::move(callback).Run(last_committed_full_navigation_source_id_);
 }
 
 void SourceUrlRecorderWebContentsObserver::SetDocumentSourceId(
