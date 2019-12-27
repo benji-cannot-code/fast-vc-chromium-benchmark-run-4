eff14377ff57b61e640a73bd7c51a5187cb8672f
Brandon Wylie <wylieb@chromium.org>
2019-12-27 22:21:25 +0000

    Disable flaky test TopControlsTest.testBasic
    TBR=cduvall@chromium.org
    
    Bug: 1035894
    Change-Id: Ie212a40b4aa33390734e3871e68ab03d84cd1db3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1983347
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727636}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/TopControlsTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/TopControlsTest.java
@@ -17,6 +17,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content_public.browser.test.util.Criteria;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
@@ -76,6 +77,7 @@ public class TopControlsTest {
 
     @Test
     @SmallTest
+    @FlakyTest(message = "see crbug.com/1035894")
     public void testBasic() throws Exception {
         final String url = UrlUtils.encodeHtmlDataUri("<body><p style='height:5000px'>");
         InstrumentationActivity activity = mActivityTestRule.launchShellWithUrl(url);
