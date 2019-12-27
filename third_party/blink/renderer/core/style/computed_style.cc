813b5b3df9b141d7cf4ede01135e97b145a18fd9
Chris Harrelson <chrishtr@chromium.org>
2019-12-27 18:53:24 +0000

    Invalidate visual overflow only on visual overflow changes to style.
    This is now possible due to past work to separate layout and visual
    overflow recalc.
    
    Change-Id: Icc1a9236390005949048d817f7660c26dfc91dda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1904871
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727590}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -904,8 +904,9 @@ void ComputedStyle::UpdatePropertySpecificDifferences(
     diff.SetFilterChanged();
 
   if (ComputedStyleBase::
-          UpdatePropertySpecificDifferencesNeedsRecomputeOverflow(*this, other))
-    diff.SetNeedsRecomputeOverflow();
+          UpdatePropertySpecificDifferencesNeedsRecomputeVisualOverflow(*this,
+                                                                        other))
+    diff.SetNeedsRecomputeVisualOverflow();
 
   if (ComputedStyleBase::UpdatePropertySpecificDifferencesBackdropFilter(*this,
                                                                          other))
