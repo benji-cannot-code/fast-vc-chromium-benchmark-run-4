24edcc8caadc6328fb7a03e9049e55eb0d4f5f77
Adithya Srinivasan <adithyas@chromium.org>
2019-12-27 16:43:52 +0000

    Sheriff: Disable BrowserMinidumpTest tests on ChromeOS
    Tests are currently failing on chromeos-betty-chrome. I'm not sure how to
    just disable the tests on the bot, so I'm disabling generally on chromeos.
    
    Bug: 1038043
    Change-Id: I2ae30a98157164889fb6acb305409091028bdf3b
    Tbr: achuith@chromium.org
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981261
    Reviewed-by: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727569}

--- tools/perf/core/minidump_unittest.py
+++ tools/perf/core/minidump_unittest.py
@@ -19,7 +19,8 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   @decorators.Isolated
   # Minidump symbolization doesn't work in ChromeOS local mode if the rootfs is
   # still read-only, so skip the test in that case.
-  @decorators.Disabled('chromeos-local')
+  # TODO(crbug.com/1038043): Test is failing on chromeos-betty-chrome.
+  @decorators.Disabled('chromeos-local', 'chromeos')
   def testSymbolizeMinidump(self):
     # Wait for the browser to restart fully before crashing
     self._LoadPageThenWait('var sam = "car";', 'sam')
@@ -59,7 +60,8 @@ class BrowserMinidumpTest(tab_test_case.TabTestCase):
   @decorators.Isolated
   # Minidump symbolization doesn't work in ChromeOS local mode if the rootfs is
   # still read-only, so skip the test in that case.
-  @decorators.Disabled('chromeos-local')
+  # TODO(crbug.com/1038043): Test is failing on chromeos-betty-chrome.
+  @decorators.Disabled('chromeos-local', 'chromeos')
   def testMultipleCrashMinidumps(self):
     # Wait for the browser to restart fully before crashing
     self._LoadPageThenWait('var cat = "dog";', 'cat')
