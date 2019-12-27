7b4afbd30ea87b8718be09808d32c718debc3e3b
Avery Musbach <amusbach@chromium.org>
2019-12-27 23:43:47 +0000

    overview: Clean up OverviewWindowDragController::DragResult
    OverviewWindowDragController::DragResult values are only ever checked
    against kSuccessfulDragToSnap. Distinctions among the other values are
    for readability only. See the code review comment at [1]. So the present
    CL cleans it up with semantic meaning in mind, and not much concern
    about functionality. OverviewWindowDragController::CompleteNormalDrag is
    significantly simplified near the end.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1570218/3/ash/wm/overview/overview_window_drag_controller.h
    
    Bug: None
    Change-Id: I0ae77c4b7d8d4dfa5f01dfbcbd6990c8bd27a60f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1983349
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727651}

--- ash/wm/overview/overview_session.cc
+++ ash/wm/overview/overview_session.cc
@@ -482,9 +482,8 @@ void OverviewSession::CompleteDrag(OverviewItem* item,
                                    const gfx::PointF& location_in_screen) {
   DCHECK(window_drag_controller_);
   DCHECK_EQ(item, window_drag_controller_->item());
-  const bool snap =
-      window_drag_controller_->CompleteDrag(location_in_screen) ==
-      OverviewWindowDragController::DragResult::kSuccessfulDragToSnap;
+  const bool snap = window_drag_controller_->CompleteDrag(location_in_screen) ==
+                    OverviewWindowDragController::DragResult::kSnap;
   for (std::unique_ptr<OverviewGrid>& grid : grid_list_)
     grid->OnSelectorItemDragEnded(snap);
 
@@ -505,10 +504,9 @@ void OverviewSession::Fling(OverviewItem* item,
   if (!window_drag_controller_ || item != window_drag_controller_->item())
     return;
 
-  const bool snap =
-      window_drag_controller_->Fling(location_in_screen, velocity_x,
-                                     velocity_y) ==
-      OverviewWindowDragController::DragResult::kSuccessfulDragToSnap;
+  const bool snap = window_drag_controller_->Fling(location_in_screen,
+                                                   velocity_x, velocity_y) ==
+                    OverviewWindowDragController::DragResult::kSnap;
   for (std::unique_ptr<OverviewGrid>& grid : grid_list_)
     grid->OnSelectorItemDragEnded(snap);
 }
