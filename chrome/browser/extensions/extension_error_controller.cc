071da14a4d95451f9e32de07cfb14d679d1d3373
Devlin Cronin <rdevlin.cronin@chromium.org>
2019-12-27 20:02:39 +0000

    [Extensions] Remove external extensions from extension error ui
    At one point in the past, the ExtensionErrorUi was responsible for
    alerting the user about sideloaded extensions (extensions that were
    added by another binary via registry or preferences modification).
    This changed in https://chromiumcodereview.appspot.com/11150002/, but
    some of the old UI was never cleaned up.
    
    Remove it, the unnecessary strings, and clean up the related methods.
    
    Bug: None
    Change-Id: Iaea002e4123e64697144dce507d9ecf0cd2ccaff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980167
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727610}

--- chrome/browser/extensions/extension_error_controller.cc
+++ chrome/browser/extensions/extension_error_controller.cc
@@ -63,10 +63,6 @@ content::BrowserContext* ExtensionErrorController::GetContext() {
   return browser_context_;
 }
 
-const ExtensionSet& ExtensionErrorController::GetExternalExtensions() {
-  return external_extensions_;
-}
-
 const ExtensionSet& ExtensionErrorController::GetBlacklistedExtensions() {
   return blacklisted_extensions_;
 }
