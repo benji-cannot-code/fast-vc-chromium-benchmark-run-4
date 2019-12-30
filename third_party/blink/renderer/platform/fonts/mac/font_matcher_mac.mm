e2d511b9ef4627950ece30f932ad3aca015aa1ee
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2019-12-30 08:28:42 +0000

    Rename toYosemiteFontWeight to toFontWeight.
    Bug: 834860
    Change-Id: I292c4c7fde0c17c0ba59aa468f78e56f7fe98e13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982511
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727733}

--- third_party/blink/renderer/platform/fonts/mac/font_matcher_mac.mm
+++ third_party/blink/renderer/platform/fonts/mac/font_matcher_mac.mm
@@ -47,7 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-static CGFloat toYosemiteFontWeight(blink::FontSelectionValue font_weight) {
+static CGFloat toFontWeight(blink::FontSelectionValue font_weight) {
   static uint64_t ns_font_weights[] = {
       0xbfe99999a0000000,  // NSFontWeightUltraLight
       0xbfe3333340000000,  // NSFontWeightThin
@@ -182,8 +182,7 @@ NSFont* MatchNSFontFamily(const AtomicString& desired_family_string,
 // On OSX 10.10+, the default system font has more weights.
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunguarded-availability"
-    font = [NSFont systemFontOfSize:size
-                             weight:toYosemiteFontWeight(desired_weight)];
+    font = [NSFont systemFontOfSize:size weight:toFontWeight(desired_weight)];
 #pragma clang diagnostic pop
 
     if (desired_traits & IMPORTANT_FONT_TRAITS)
