f07ba03bcd5e12355c86469c2a7c1d1df97b7433
Haiyang Pan <hypan@google.com>
2019-12-30 22:45:21 +0000

    perf: support browser type that starts with 'reference'
    And test if it works fine on the builder android-pixel2-perf-aab-fyi
    Needs crrev.com/c/1975037 to land first
    
    Bug: 986990
    Change-Id: I26e492803eceb8bebc56e1e1b778cd6c4ebe04a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982234
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#727820}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -132,6 +132,9 @@ FYI_BUILDERS = {
     'tests': [
       {
         'isolate': 'performance_test_suite',
+        'extra_args': [
+           '--run-ref-build',
+        ],
       }
     ],
     'platform': 'android-chrome-bundle',
