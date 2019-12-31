c8d4832b0e7e942af5397ad1d1de186e8ecbb25e
Philip Rogers <pdr@chromium.org>
2019-12-31 14:52:17 +0000

    Improve comments around IsLowEndDevice
    base::SysInfo::IsLowEndDevice has some nuance that can be clarified with
    better documentation. In particular:
    1) It can be used for more extreme performance tradeoffs.
    2) It is not the same as "low-memory" and will be true on many <=1GB
    pre-O Android devices. SysInfo::AmountOfPhysicalMemoryMB is an option
    for explicit memory queries.
    3) It can return true on <=512MB desktop devices.
    
    In addition, the comments above blink::Platform::IsLowEndDevice and
    blink::MemoryPressureListener::IsLowEndDevice have been updated to
    provide a little more information and a reference to
    base::SysInfo::isLowEndDevice.
    
    For more information, see this document:
    https://docs.google.com/document/d/1npdd1gz2oU_ij_Gsp5QcvMltdwz8Zl38M2VpRi_bKJk
    
    Change-Id: I3fb13c39ba70d4ceff5a0051fc01c96d87fba4a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974615
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727882}

--- base/android/java/src/org/chromium/base/SysUtils.java
+++ base/android/java/src/org/chromium/base/SysUtils.java
@@ -178,6 +178,7 @@ public class SysUtils {
             return false;
         }
 
+        // If this logic changes, update the comments above base::SysUtils::IsLowEndDevice.
         sAmountOfPhysicalMemoryKB = detectAmountOfPhysicalMemoryKB();
         boolean isLowEnd = true;
         if (sAmountOfPhysicalMemoryKB <= 0) {
