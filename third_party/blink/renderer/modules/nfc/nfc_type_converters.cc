de70c4b81c55a7e5c5950bcd5899195e327f6cf0
Leon Han <leon.han@intel.com>
2019-12-31 06:46:25 +0000

    [webnfc] Implement the algo for validating external types
    The spec change:
    https://github.com/w3c/web-nfc/pull/474/
    
    BUG=520391
    
    Change-Id: If1e4a6513410f3705bec4da7b2cc514c209a3cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978622
    Commit-Queue: Leon Han <leon.han@intel.com>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#727853}

--- third_party/blink/renderer/modules/nfc/nfc_type_converters.cc
+++ third_party/blink/renderer/modules/nfc/nfc_type_converters.cc
@@ -31,8 +31,8 @@ namespace mojo {
 NDEFRecordPtr TypeConverter<NDEFRecordPtr, blink::NDEFRecord*>::Convert(
     const blink::NDEFRecord* record) {
   return NDEFRecord::New(
-      record->recordType(), record->mediaType(), record->id(),
-      record->encoding(), record->lang(), record->payloadData(),
+      record->category(), record->recordType(), record->mediaType(),
+      record->id(), record->encoding(), record->lang(), record->payloadData(),
       TypeConverter<NDEFMessagePtr, blink::NDEFMessage*>::Convert(
           record->payload_message()));
 }
