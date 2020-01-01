91e9c4d11fe1b24e1a1bcbe492ca51f18eca919d
Dana Fried <dfried@chromium.org>
2020-01-01 06:48:34 +0000

    Remove account control's icon container view.
    ToolbarAccountIconContainerView now directly implements
    PageActionIconContainer, simplifying the view hierarchy, and ensuring
    that the container's AnimatingLayoutManager has direct control over the
    individual page actions.
    
    (We do not yet support an animating layout owning descendent views
    other than children.)
    
    Bug: 1023528
    Change-Id: I1bf5d351951e3b6b0ade415e39158096fd298b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1979949
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727913}

--- chrome/browser/ui/views/toolbar/toolbar_account_icon_container_view.h
+++ chrome/browser/ui/views/toolbar/toolbar_account_icon_container_view.h
@@ -7,18 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_TOOLBAR_TOOLBAR_ACCOUNT_ICON_CONTAINER_VIEW_H_
 
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
+#include "chrome/browser/ui/views/page_action/page_action_icon_container.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_icon_container_view.h"
 
 class AvatarToolbarButton;
 class Browser;
-class PageActionIconContainerView;
 class PageActionIconController;
 
 // A container view for user-account-related PageActionIconViews and the profile
 // avatar icon.
 class ToolbarAccountIconContainerView : public ToolbarIconContainerView,
                                         public IconLabelBubbleView::Delegate,
+                                        public PageActionIconContainer,
                                         public PageActionIconView::Delegate {
  public:
   explicit ToolbarAccountIconContainerView(Browser* browser);
@@ -45,18 +46,17 @@ class ToolbarAccountIconContainerView : public ToolbarIconContainerView,
   const char* GetClassName() const override;
 
   PageActionIconController* page_action_icon_controller() {
-    return page_action_icon_controller_;
+    return page_action_icon_controller_.get();
   }
   AvatarToolbarButton* avatar_button() { return avatar_; }
 
   static const char kToolbarAccountIconContainerViewClassName[];
 
  private:
-  // ToolbarIconContainerView:
-  const views::View::Views& GetChildren() const override;
+  // PageActionIconContainer:
+  void AddPageActionIcon(views::View* icon) override;
 
-  PageActionIconContainerView* page_action_icon_container_view_ = nullptr;
-  PageActionIconController* page_action_icon_controller_ = nullptr;
+  std::unique_ptr<PageActionIconController> page_action_icon_controller_;
 
   AvatarToolbarButton* const avatar_ = nullptr;
 
