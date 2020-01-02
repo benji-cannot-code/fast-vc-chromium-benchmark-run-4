a65b179a2d66ea9987d67df8e11cdbf88171f0fc
Alex Cooper <alcooper@chromium.org>
2020-01-02 22:46:10 +0000

    Fix screen taps for Sensorless Inline XRSessions
    Currently, getPose for a screen tap on a sensorless inline session fails
    because there is no "MojoFromViewer" pose available.  In a sensorless
    session "Mojo" Space and "Viewer" space are the same, so this should
    return identity.
    
    Fixed: 1031596
    Change-Id: Ic09590be6bbeff84cfaad894a2c1702ed7d3f896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978901
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728051}

--- third_party/blink/renderer/modules/xr/xr_session.cc
+++ third_party/blink/renderer/modules/xr/xr_session.cc
@@ -1529,8 +1529,12 @@ base::Optional<TransformationMatrix> XRSession::MojoFromViewer() const {
   if (!CanReportPoses())
     return base::nullopt;
 
-  if (!mojo_from_viewer_)
+  if (!mojo_from_viewer_) {
+    if (sensorless_session_)
+      return TransformationMatrix();
+
     return base::nullopt;
+  }
 
   return *mojo_from_viewer_.get();
 }
