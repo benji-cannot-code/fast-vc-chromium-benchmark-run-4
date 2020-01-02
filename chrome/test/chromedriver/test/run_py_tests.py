02a82ca8298a157fbf6872998a7f150458b00507
Tricia Crichton <triciac@chromium.org>
2020-01-02 21:59:10 +0000

    [ChromeDriver] Change window and element size in tests
    Alter the element and window size to reduce flakiness of tests caused by
    scrolling.
    
    Bug: 1038366
    Change-Id: I448ffd291a55d596f2a832021645061fde168931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1984533
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728030}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -81,8 +81,6 @@ _NEGATIVE_FILTER = [
     'ChromeDriverPageLoadTimeoutTest.testRefreshWithPageLoadTimeout',
     # testFocus is failing
     'JavaScriptTests.testFocus',
-    # https://bugs.chromium.org/p/chromium/issues/detail?id=1038366
-    'JavaScriptTests.testAllJS',
 ]
 
 
@@ -238,6 +236,8 @@ _ANDROID_NEGATIVE_FILTER['chrome'] = (
         'ChromeDriverTest.testPushAndNotificationsPermissions',
         'ChromeDriverTest.testSensorPermissions',
         'ChromeDriverTest.testSettingPermissionDoesNotAffectOthers',
+        # Android does not allow changing window size
+        'JavaScriptTests.*',
     ]
 )
 _ANDROID_NEGATIVE_FILTER['chrome_stable'] = (
@@ -4213,6 +4213,7 @@ class JavaScriptTests(ChromeDriverBaseTestWithWebServer):
   def setUp(self):
     self._driver = self.CreateDriver()
     self.js_root = os.path.dirname(os.path.realpath(__file__)) + '/../js/'
+    self._driver.SetWindowRect(1000, 1000, 0, 0)
 
   def checkTestResult(self):
     def getStatus():
