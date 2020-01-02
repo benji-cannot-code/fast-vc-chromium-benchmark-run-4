2382dc9a20d552d6a0f22b8a5f0fced4d23aaf33
Kehuang Li <kehuangli@chromium.org>
2020-01-02 21:46:48 +0000

    [Chromecast] Add header for int64 in capture_service constants.h
    A defined struct uses int64_t but didn't include cstdint, which causes
    issue if some class doens't have cstdint included but have constants.h
    included.
    
    Bug: NONE.
    Test: NONE.
    Change-Id: I0a1700fac5c4a91735f889514d95c9a03b59edda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986013
    Commit-Queue: Kehuang Li <kehuangli@chromium.org>
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Auto-Submit: Kehuang Li <kehuangli@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728026}

--- chromecast/media/audio/capture_service/constants.h
+++ chromecast/media/audio/capture_service/constants.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMECAST_MEDIA_AUDIO_CAPTURE_SERVICE_CONSTANTS_H_
 #define CHROMECAST_MEDIA_AUDIO_CAPTURE_SERVICE_CONSTANTS_H_
 
+#include <cstdint>
+
 namespace chromecast {
 namespace media {
 namespace capture_service {
