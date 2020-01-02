076bd6d327e78929d424a07b9415577556aca17c
Philip Rogers <pdr@chromium.org>
2020-01-02 14:37:32 +0000

    Remove the DoNotCompositeTrivial3D experiment
    After collecting data on Android canary/dev, we are going to fold this
    project into "reducing compositing under memory pressure" so we only
    stop compositing trivial 3D transforms when under memory pressure. This
    achieves the majority of the OOM benefits with minimal user impact. This
    experiment resulted in interesting data about the effects of memory
    savings on OOM crash rates. On canary/dev Android, the experiment showed
    a decrease in GPU tile memory (-2.4% @ 50th, -4.4% @ 99th) resulted in a
    decrease in peak GPU memory usage (-1.95% during scroll), a decrease in
    memory pressure signals (-6% critical), and a decrease in crashes
    (-8.25% visible renderer foreground OOM). These benefits came at a cost
    of additional raster which could be seen in
    Scheduling.Renderer.DrawIntervalWithMainThreadAnimations2 which
    increased 4.66% at the 99th. More information can be found in:
    https://docs.google.com/document/d/1yFSWUhHjneEnjx_vf9SBpYAQOoce8JMbtTvAiHhGD-o
    
    Bug: 1012775
    Change-Id: I9ce1c4f47860d9ab0dbd7cbae68662cf4d657885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981804
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#727972}

--- third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.cc
+++ third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/paint/compositing/compositing_reason_finder.h"
 
-#include "base/feature_list.h"
-#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/node.h"
@@ -166,10 +164,8 @@ bool CompositingReasonFinder::RequiresCompositingFor3DTransform(
     return false;
 
   // Don't composite "trivial" 3D transforms such as translateZ(0).
-  if (Platform::Current()->IsLowEndDevice() ||
-      base::FeatureList::IsEnabled(blink::features::kDoNotCompositeTrivial3D)) {
+  if (Platform::Current()->IsLowEndDevice())
     return layout_object.StyleRef().HasNonTrivial3DTransformOperation();
-  }
 
   return layout_object.StyleRef().Has3DTransformOperation();
 }
