dc0057ac6cb719edb143b88cbb35bec788bbce64
Lukasz Anforowicz <lukasza@chromium.org>
2020-01-03 22:46:32 +0000

    Add DwoC for DCHECK(was_notify_will_be_destroyed_called_).
    This CL adds a DumpWithoutCrashing call when a destructor of
    content::BrowserContext gets called without an earlier call
    to BrowserContext::NotifyWillBeDestroyed.
    
    This CL will hopefully help
    1) verify if there are scenarios in the wild, where a
       BrowserContext/Profile gets destroyed without calling
       BrowserContext::NotifyWillBeDestroyed first
    2) find root cause of these scenarios
    
    Bug: 1038844
    Change-Id: Ifb40daa27b7b4a4fc301df2ea6f09ffa499fe7d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986262
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728321}

--- content/browser/browser_context.cc
+++ content/browser/browser_context.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/guid.h"
@@ -612,7 +613,10 @@ BrowserContext::~BrowserContext() {
   DCHECK(!GetUserData(kStoragePartitionMapKeyName))
       << "StoragePartitionMap is not shut down properly";
 
-  DCHECK(was_notify_will_be_destroyed_called_);
+  if (!was_notify_will_be_destroyed_called_) {
+    NOTREACHED();
+    base::debug::DumpWithoutCrashing();
+  }
 
   RemoveBrowserContextFromInstanceGroupMap(this);
 
