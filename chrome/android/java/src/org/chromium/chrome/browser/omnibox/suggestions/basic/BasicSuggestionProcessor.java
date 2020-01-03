efba872d7ce42d74e857dfc2d0e42e7bceab6120
Tomasz Wiszkowski <ender@google.com>
2020-01-03 17:39:11 +0000

    Move Spannable container to common class.
    This change allows all suggestions to benefit from Spannable
    container that introduces .equals for Spannables.
    
    Bug: 982818
    Change-Id: Ia31d7baf2fb4a97556090867b1f29e9f8fb1547b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1968115
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#728228}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/BasicSuggestionProcessor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/BasicSuggestionProcessor.java
@@ -8,8 +8,6 @@ package org.chromium.chrome.browser.omnibox.suggestions.basic;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.annotation.DrawableRes;
-import android.text.Spannable;
-import android.text.SpannableString;
 import android.text.TextUtils;
 
 import org.chromium.base.Supplier;
@@ -24,8 +22,8 @@ import org.chromium.chrome.browser.omnibox.suggestions.OmniboxSuggestion;
 import org.chromium.chrome.browser.omnibox.suggestions.OmniboxSuggestionUiType;
 import org.chromium.chrome.browser.omnibox.suggestions.base.BaseSuggestionViewProcessor;
 import org.chromium.chrome.browser.omnibox.suggestions.base.SuggestionDrawableState;
+import org.chromium.chrome.browser.omnibox.suggestions.base.SuggestionSpannable;
 import org.chromium.chrome.browser.omnibox.suggestions.basic.SuggestionViewProperties.SuggestionIcon;
-import org.chromium.chrome.browser.omnibox.suggestions.basic.SuggestionViewProperties.SuggestionTextContainer;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -194,21 +192,21 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
     public void populateModel(OmniboxSuggestion suggestion, PropertyModel model, int position) {
         super.populateModel(suggestion, model, position);
         final @OmniboxSuggestionType int suggestionType = suggestion.getType();
-        Spannable textLine2 = null;
+        SuggestionSpannable textLine2 = null;
         boolean urlHighlighted = false;
 
         if (suggestion.isUrlSuggestion()) {
             if (!TextUtils.isEmpty(suggestion.getUrl())) {
-                Spannable str = SpannableString.valueOf(suggestion.getDisplayText());
+                SuggestionSpannable str = new SuggestionSpannable(suggestion.getDisplayText());
                 urlHighlighted = applyHighlightToMatchRegions(
                         str, suggestion.getDisplayTextClassifications());
                 textLine2 = str;
             }
         } else if (suggestionType == OmniboxSuggestionType.SEARCH_SUGGEST_PROFILE) {
-            textLine2 = SpannableString.valueOf(suggestion.getDescription());
+            textLine2 = new SuggestionSpannable(suggestion.getDescription());
         }
 
-        final Spannable textLine1 =
+        final SuggestionSpannable textLine1 =
                 getSuggestedQuery(suggestion, suggestion.isUrlSuggestion(), !urlHighlighted);
 
         updateSuggestionIcon(suggestion, model);
@@ -217,10 +215,8 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
                         || suggestionType == OmniboxSuggestionType.CLIPBOARD_IMAGE
                         || suggestionType == OmniboxSuggestionType.CLIPBOARD_TEXT);
         model.set(SuggestionViewProperties.SUGGESTION_TYPE, suggestion.getType());
-        model.set(
-                SuggestionViewProperties.TEXT_LINE_1_TEXT, new SuggestionTextContainer(textLine1));
-        model.set(
-                SuggestionViewProperties.TEXT_LINE_2_TEXT, new SuggestionTextContainer(textLine2));
+        model.set(SuggestionViewProperties.TEXT_LINE_1_TEXT, textLine1);
+        model.set(SuggestionViewProperties.TEXT_LINE_2_TEXT, textLine2);
         fetchSuggestionFavicon(model, suggestion.getUrl(), suggestion.getType());
     }
 
@@ -264,7 +260,7 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
      * @param shouldHighlight Whether the query should be highlighted.
      * @return The first line of text.
      */
-    private Spannable getSuggestedQuery(OmniboxSuggestion suggestion,
+    private SuggestionSpannable getSuggestedQuery(OmniboxSuggestion suggestion,
             boolean showDescriptionIfPresent, boolean shouldHighlight) {
         String userQuery = mUrlBarEditingTextProvider.getTextWithoutAutocomplete();
         String suggestedQuery = null;
@@ -285,7 +281,7 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
                     new OmniboxSuggestion.MatchClassification(0, MatchClassificationStyle.NONE));
         }
 
-        Spannable str = SpannableString.valueOf(suggestedQuery);
+        SuggestionSpannable str = new SuggestionSpannable(suggestedQuery);
         if (shouldHighlight) applyHighlightToMatchRegions(str, classifications);
         return str;
     }
