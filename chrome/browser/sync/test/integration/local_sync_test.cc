7275b69d67f4b255c4da1b2e78e474453ac678b3
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2020-01-03 14:15:32 +0000

    Update UnidoOnSignIn in testing config.
    Bug: 1009516
    Change-Id: If0e14a7940a5dc8b7b0b5002fd4d9bd04bdbb911
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1985842
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728190}

--- chrome/browser/sync/test/integration/local_sync_test.cc
+++ chrome/browser/sync/test/integration/local_sync_test.cc
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/send_tab_to_self/send_tab_to_self_util.h"
+#include "chrome/browser/sharing/features.h"
 #include "chrome/browser/sharing/sharing_service.h"
 #include "chrome/browser/sharing/sharing_service_factory.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
@@ -86,9 +88,11 @@ IN_PROC_BROWSER_TEST_F(LocalSyncTest, ShouldStart) {
 #if !BUILDFLAG(GOOGLE_CHROME_BRANDING)
   // SharingService is only disabled if kSharingDeriveVapidKey is enabled by
   // field trial config, which is never the case for branded builds.
-  EXPECT_EQ(SharingService::State::DISABLED,
-            SharingServiceFactory::GetForBrowserContext(browser()->profile())
-                ->GetStateForTesting());
+  if (base::FeatureList::IsEnabled(kSharingDeriveVapidKey)) {
+    EXPECT_EQ(SharingService::State::DISABLED,
+              SharingServiceFactory::GetForBrowserContext(browser()->profile())
+                  ->GetStateForTesting());
+  }
 #endif  // !BUILDFLAG(GOOGLE_CHROME_BRANDING)
 }
 #endif  // defined(OS_WIN)
