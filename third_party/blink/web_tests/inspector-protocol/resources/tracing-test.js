2d2ce69f778ce3da3ffc194693ea85b0a55f8ae4
Peter Marshall <petermarshall@chromium.org>
2020-01-03 12:32:50 +0000

    DevTools: Try to fix broken page-frames.js test
    The test was flaky a long time ago but now fails consistently.
    The TracingStartedInPage trace event doesn't seem to be emitted anymore
    so I replaced this with the TracingStartedInBrowser event.
    
    That event doesn't emit nodeIds so I updated the expectation to assert
    that they are undfined.
    
    Also a drive-by cleanup to tracing-test which was trying to JSONify
    the wrong variable which meant the output in case the test failed
    was not helpful.
    
    Also clean up the test itself with const, JS style etc.
    
    Bug: 734762
    Change-Id: I3f7f5665ace38525fca5967c5de6f6fba02d3d5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1983172
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728177}

--- third_party/blink/web_tests/inspector-protocol/resources/tracing-test.js
+++ third_party/blink/web_tests/inspector-protocol/resources/tracing-test.js
@@ -100,7 +100,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     var events = this.findEvents(name, ph, condition);
     if (events.length)
       return events[0];
-    throw new Error("Couldn't find event " + name + " / " + ph + "\n\n in " + JSON.stringify(this.devtoolsEvents, null, 2));
+    throw new Error("Couldn't find event " + name + " / " + ph + "\n\n in " + JSON.stringify(this._devtoolsEvents, null, 2));
   }
 
   filterEvents(callback) {
