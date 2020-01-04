e2a81ac294d03d96326a0c925bfb6fd52c6ba854
Peter Williamson <petewil@chromium.org>
2020-01-04 00:24:13 +0000

    Revert "Remove CompositorFrame handling in render_host"
    This reverts commit aad4125594acc4c85916998cf094410a5145019d.
    
    Reason for revert:
    This is being reverted by the build sheriff on suspicion of causing the following break.
    
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20CFI/16082
    
    The following two tests seem to fail consistently after this changelist:
    RenderWidgetHostViewAuraTest.KeyEventRoutingWithKeyboardLockActiveForAllKeys
    
    RenderWidgetHostViewAuraTest.KeyEventRoutingWithKeyboardLockActiveForOneKey
    
    
    Original change's description:
    > Remove CompositorFrame handling in render_host
    > 
    > This CL starts to remove code in content/browser/renderer_host/ that
    > existed to receive CompositorFrames and submit them to the display
    > compositor running on the browser UI thread. The VizDisplayCompositor
    > feature has launched so this code is no longer used.
    > 
    > This CL is a first pass to delete code that doesn't have require
    > significant changes to tests.
    > 
    > TBR=sky@chromium.org
    > 
    > Bug: 759653, 812322, 1038683
    > Change-Id: I7bf29f57e7418eb8d3a16b86169f2b30bb9fbf80
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978997
    > Reviewed-by: Saman Sami <samans@chromium.org>
    > Commit-Queue: kylechar <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#728209}
    
    TBR=sky@chromium.org,kylechar@chromium.org,samans@chromium.org
    
    Change-Id: Ia0b20ebad951e8f22fb0f108cb9d09160e1a645c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 759653, 812322, 1038683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986978
    Reviewed-by: Peter Williamson <petewil@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728341}

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -15,10 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
+#include "components/viz/common/features.h"
 #include "components/viz/common/frame_sinks/copy_output_request.h"
 #include "components/viz/common/frame_sinks/copy_output_result.h"
 #include "components/viz/common/surfaces/parent_local_surface_id_allocator.h"
 #include "components/viz/host/host_frame_sink_manager.h"
+#include "components/viz/service/frame_sinks/compositor_frame_sink_support.h"
+#include "components/viz/service/surfaces/surface.h"
+#include "components/viz/service/surfaces/surface_manager.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
 #include "content/browser/browser_plugin/browser_plugin_guest.h"
 #include "content/browser/compositor/surface_utils.h"
@@ -61,11 +65,13 @@ RenderWidgetHostViewChildFrame::RenderWidgetHostViewChildFrame(
       frame_sink_id_(
           base::checked_cast<uint32_t>(widget_host->GetProcess()->GetID()),
           base::checked_cast<uint32_t>(widget_host->GetRoutingID())),
-      frame_connector_(nullptr) {
+      frame_connector_(nullptr),
+      enable_viz_(features::IsVizDisplayCompositorEnabled()) {
   GetHostFrameSinkManager()->RegisterFrameSinkId(
       frame_sink_id_, this, viz::ReportFirstSurfaceActivation::kNo);
   GetHostFrameSinkManager()->SetFrameSinkDebugLabel(
       frame_sink_id_, "RenderWidgetHostViewChildFrame");
+  CreateCompositorFrameSinkSupport();
 }
 
 RenderWidgetHostViewChildFrame::~RenderWidgetHostViewChildFrame() {
@@ -75,6 +81,7 @@ RenderWidgetHostViewChildFrame::~RenderWidgetHostViewChildFrame() {
   if (frame_connector_)
     DetachFromTouchSelectionClientManagerIfNecessary();
 
+  ResetCompositorFrameSinkSupport();
   if (GetHostFrameSinkManager())
     GetHostFrameSinkManager()->InvalidateFrameSinkId(frame_sink_id_);
 }
@@ -532,6 +539,19 @@ void RenderWidgetHostViewChildFrame::ForwardTouchpadZoomEventIfNecessary(
   NOTREACHED();
 }
 
+void RenderWidgetHostViewChildFrame::DidReceiveCompositorFrameAck(
+    const std::vector<viz::ReturnedResource>& resources) {
+  if (renderer_compositor_frame_sink_)
+    renderer_compositor_frame_sink_->DidReceiveCompositorFrameAck(resources);
+}
+
+void RenderWidgetHostViewChildFrame::DidCreateNewRendererCompositorFrameSink(
+    viz::mojom::CompositorFrameSinkClient* renderer_compositor_frame_sink) {
+  ResetCompositorFrameSinkSupport();
+  renderer_compositor_frame_sink_ = renderer_compositor_frame_sink;
+  CreateCompositorFrameSinkSupport();
+}
+
 void RenderWidgetHostViewChildFrame::SetParentFrameSinkId(
     const viz::FrameSinkId& parent_frame_sink_id) {
   if (parent_frame_sink_id_ == parent_frame_sink_id)
@@ -564,12 +584,17 @@ void RenderWidgetHostViewChildFrame::SubmitCompositorFrame(
     const viz::LocalSurfaceId& local_surface_id,
     viz::CompositorFrame frame,
     base::Optional<viz::HitTestRegionList> hit_test_region_list) {
-  NOTREACHED();
+  DCHECK(!enable_viz_);
+  TRACE_EVENT0("content",
+               "RenderWidgetHostViewChildFrame::OnSwapCompositorFrame");
+  support_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                                  std::move(hit_test_region_list));
 }
 
 void RenderWidgetHostViewChildFrame::OnDidNotProduceFrame(
     const viz::BeginFrameAck& ack) {
-  NOTREACHED();
+  DCHECK(!enable_viz_);
+  support_->DidNotProduceFrame(ack);
 }
 
 void RenderWidgetHostViewChildFrame::TransformPointToRootSurface(
@@ -812,11 +837,36 @@ void RenderWidgetHostViewChildFrame::CopyFromSurface(
                                                  std::move(request));
 }
 
+void RenderWidgetHostViewChildFrame::ReclaimResources(
+    const std::vector<viz::ReturnedResource>& resources) {
+  if (renderer_compositor_frame_sink_)
+    renderer_compositor_frame_sink_->ReclaimResources(resources);
+}
+
+void RenderWidgetHostViewChildFrame::OnBeginFrame(
+    const viz::BeginFrameArgs& args,
+    const viz::FrameTimingDetailsMap& timing_details) {
+  host_->ProgressFlingIfNeeded(args.frame_time);
+  if (renderer_compositor_frame_sink_)
+    renderer_compositor_frame_sink_->OnBeginFrame(args, timing_details);
+}
+
+void RenderWidgetHostViewChildFrame::OnBeginFramePausedChanged(bool paused) {
+  if (renderer_compositor_frame_sink_)
+    renderer_compositor_frame_sink_->OnBeginFramePausedChanged(paused);
+}
+
 void RenderWidgetHostViewChildFrame::OnFirstSurfaceActivation(
     const viz::SurfaceInfo& surface_info) {}
 
 void RenderWidgetHostViewChildFrame::OnFrameTokenChanged(uint32_t frame_token) {
   OnFrameTokenChangedForView(frame_token);
+}
+
+void RenderWidgetHostViewChildFrame::SetNeedsBeginFrames(
+    bool needs_begin_frames) {
+  if (support_)
+    support_->SetNeedsBeginFrame(needs_begin_frames);
 }
 
 TouchSelectionControllerClientManager*
@@ -840,6 +890,11 @@ void RenderWidgetHostViewChildFrame::
     selection_controller_client_->UpdateSelectionBoundsIfNeeded(
         metadata.selection, current_device_scale_factor_);
   }
+}
+
+void RenderWidgetHostViewChildFrame::SetWantsAnimateOnlyBeginFrames() {
+  if (support_)
+    support_->SetWantsAnimateOnlyBeginFrames();
 }
 
 void RenderWidgetHostViewChildFrame::TakeFallbackContentFrom(
@@ -940,6 +995,32 @@ RenderWidgetHostViewChildFrame::DidUpdateVisualProperties(
           &RenderWidgetHostViewChildFrame::OnDidUpdateVisualPropertiesComplete),
       weak_factory_.GetWeakPtr(), metadata);
   return viz::ScopedSurfaceIdAllocator(std::move(allocation_task));
+}
+
+void RenderWidgetHostViewChildFrame::CreateCompositorFrameSinkSupport() {
+  if (enable_viz_)
+    return;
+
+  DCHECK(!support_);
+  constexpr bool is_root = false;
+  support_ = GetHostFrameSinkManager()->CreateCompositorFrameSinkSupport(
+      this, frame_sink_id_, is_root);
+  if (parent_frame_sink_id_.is_valid()) {
+    GetHostFrameSinkManager()->RegisterFrameSinkHierarchy(parent_frame_sink_id_,
+                                                          frame_sink_id_);
+  }
+  if (host()->needs_begin_frames())
+    support_->SetNeedsBeginFrame(true);
+}
+
+void RenderWidgetHostViewChildFrame::ResetCompositorFrameSinkSupport() {
+  if (!support_)
+    return;
+  if (parent_frame_sink_id_.is_valid()) {
+    GetHostFrameSinkManager()->UnregisterFrameSinkHierarchy(
+        parent_frame_sink_id_, frame_sink_id_);
+  }
+  support_.reset();
 }
 
 ui::TextInputType RenderWidgetHostViewChildFrame::GetTextInputType() const {
