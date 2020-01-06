f7f476c2cd67c75ea6d03f9724d32121bf1239d3
Kurt Horimoto <kkhorimoto@chromium.org>
2020-01-06 18:45:21 +0000

    [iOS] Add UI support for infobar banner overlays.
    InfobarBannerOverlayCoordinator is a coordinator that will be shared
    amongst all infobar banners.  InfobarBannerOverlayMediator is a
    superclass that implements behavior shared amongst all InfobarTypes,
    such as setting the consumer, dispatching responses for user
    interaction events, and setting up the overlay completion response.
    
    This CL also introduces the response infos necessary for banners:
    - InfobarBannerConfirmButtonTapped is dispatched to notify the model
      layer that the main button on a confirm infobar banner was tapped.
    - InfobarBannerModalButtonTapped is dispatched to notify the model
      layer that the modal button was tapped.
    - InfobarBannerCompletion is used a completion response and stores
      whether the dismissal was user-initiated.
    
    Bug: 1030357
    Change-Id: Icb19d2a7322fd6c765c19812c02d46dcba07b97c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986449
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728584}

--- ios/chrome/browser/ui/infobars/banners/infobar_banner_delegate.h
+++ ios/chrome/browser/ui/infobars/banners/infobar_banner_delegate.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_INFOBARS_BANNERS_INFOBAR_BANNER_DELEGATE_H_
 #define IOS_CHROME_BROWSER_UI_INFOBARS_BANNERS_INFOBAR_BANNER_DELEGATE_H_
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 // Delegate to handle InfobarBanner actions.
 @protocol InfobarBannerDelegate
