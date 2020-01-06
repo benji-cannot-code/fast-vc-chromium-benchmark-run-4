086336e5cef08b0aa9a9a7fc8621013e05daf211
Nicolás Peña Moreno <npm@chromium.org>
2020-01-06 17:05:17 +0000

    Fix toJSON in WindowPerformance and PerformanceResourceTiming
    Per https://heycam.github.io/webidl/#es-default-tojson, it is correct to
    directly add the attribute values of the interface to the builder
    instead of adding the results of toJSONForBinding().
    
    Change-Id: I07f7ee5992de59938c0116f90dc4b5229c5d6e9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982792
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728540}

--- third_party/blink/web_tests/external/wpt/navigation-timing/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/navigation-timing/idlharness.window.js
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // http://www.w3.org/TR/navigation-timing/
 
 idl_test(
-  ['navigation-timing'],
-  ['resource-timing', 'performance-timeline', 'hr-time', 'dom'],
+  ['hr-time', 'navigation-timing'],
+  ['resource-timing', 'performance-timeline', 'html', 'dom'],
   idl_array => {
     idl_array.add_objects({
       Performance: ['performance'],
