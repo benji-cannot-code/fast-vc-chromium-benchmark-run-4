7483fc03419a54fd0a4e5427b5735f7aa698d13e
Greg Thompson <grt@chromium.org>
2020-01-06 20:20:26 +0000

    Improve stack trimming in Chromium's StackTraceGetter.
    Previously, a hard-coded number of frames was omitted. Such a hard-coded
    constant is dependent on both the build toolchain and the optimization
    level, which makes it flaky. This change instead omits frames based on
    the addresses themselves, which should be much more resilient.
    
    Failures should now mostly show the relevant frames and no less. For
    example, a test with an assertion failure in its main test function will
    show:
    
      base::`anonymous namespace'::BindDeathTest_NullCallback_Test::TestBody [0x00F6BC7C+122] (C:\src\chromium\src\base\bind_unittest.cc:1572)
    
    The exception to this is in debug builds, where there may be additional
    frames, for example:
    
      base::`anonymous namespace'::BindDeathTest_NullCallback_Test::TestBody [0x00B11375+245] (C:\src\chromium\src\base\bind_unittest.cc:1572)
      testing::internal::HandleExceptionsInMethodIfSupported<testing::Test,void> [0x0207725B+107] (C:\src\chromium\src\third_party\googletest\src\googletest\src\gtest.cc:2493)
    
    BUG=1038787
    
    Change-Id: I65aabeaf1c1011e3b7077852c1b1feb85a87c0cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986074
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728609}

--- third_party/googletest/custom/gtest/internal/custom/stack_trace_getter.h
+++ third_party/googletest/custom/gtest/internal/custom/stack_trace_getter.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_GOOGLETEST_CUSTOM_GTEST_INTERNAL_CUSTOM_STACK_TRACE_GETTER_H_
 #define THIRD_PARTY_GOOGLETEST_CUSTOM_GTEST_INTERNAL_CUSTOM_STACK_TRACE_GETTER_H_
 
-#include <stddef.h>
-
+#include "base/debug/stack_trace.h"
+#include "base/optional.h"
 #include "third_party/googletest/src/googletest/src/gtest-internal-inl.h"
 
 // An implementation of Google Test's OsStackTraceGetterInterface that uses
@@ -25,7 +25,7 @@ class StackTraceGetter
   void UponLeavingGTest() override;
 
  private:
-  size_t frame_count_upon_leaving_gtest_ = 0;
+  base::Optional<base::debug::StackTrace> stack_trace_upon_leaving_gtest_;
 };
 
 #endif  // THIRD_PARTY_GOOGLETEST_CUSTOM_GTEST_INTERNAL_CUSTOM_STACK_TRACE_GETTER_H_
