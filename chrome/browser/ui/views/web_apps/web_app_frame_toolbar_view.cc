1b26137f1bf0dec2beeaf0b95a19fdcf03356a1b
Peter Kasting <pkasting@chromium.org>
2020-01-06 00:48:27 +0000

    WebAppFrameToolbarView should respond directly to theme changes.
    Specifically, the caption colors need to update.  On Ash this was happening
    indirectly already, since BrowserNonClientFrameViewAsh::OnThemeChanged() was
    calling UpdateFrameColor().  But on other platforms, it was not.
    
    Handling this eliminates the need to set these colors at construction or in
    BrowserNonClientFrameViewAsh::Init(), since OnThemeChanged() will always be
    called when the frame is added to a widget, which happens after both those.
    This also eliminates the need to null-check the frame view in
    WebAppFrameToolbarView::UpdateCaptionColors(), since by OnThemeChanged() that
    will always be set.  Finally, this eliminates the need for
    BrowserNonClientFrameViewAsh::OnThemeChanged() to indirectly call
    WebAppFrameToolbarView::UpdateCaptionColors(), since that is now called locally.
    
    Also, the BrowserNonClientFrameView need not explicitly SchedulePaint(), since
    if any children of the web app frame actually change color, they will invalidate
    themselves as necessary.
    
    Bug: none
    Change-Id: If0a24ccebcf8ce105dc3662ee6468d6b9b36d419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980166
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728425}

--- chrome/browser/ui/views/web_apps/web_app_frame_toolbar_view.cc
+++ chrome/browser/ui/views/web_apps/web_app_frame_toolbar_view.cc
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/layer_animation_sequence.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/events/event.h"
-#include "ui/gfx/color_palette.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
@@ -651,12 +650,8 @@ void WebAppFrameToolbarView::ToolbarButtonContainer::OnWidgetVisibilityChanged(
 }
 
 WebAppFrameToolbarView::WebAppFrameToolbarView(views::Widget* widget,
-                                               BrowserView* browser_view,
-                                               SkColor active_color,
-                                               SkColor inactive_color)
-    : browser_view_(browser_view),
-      active_color_(active_color),
-      inactive_color_(inactive_color) {
+                                               BrowserView* browser_view)
+    : browser_view_(browser_view) {
   DCHECK(browser_view_);
   DCHECK(web_app::AppBrowserController::IsForWebAppBrowser(
       browser_view_->browser()));
@@ -700,7 +695,6 @@ WebAppFrameToolbarView::WebAppFrameToolbarView(views::Widget* widget,
                                     views::MaximumFlexSizeRule::kPreferred)
                                     .WithOrder(1));
 
-  UpdateChildrenColor();
   UpdateStatusIconsVisibility();
 
   DCHECK(!browser_view_->toolbar_button_provider() ||
@@ -721,10 +715,8 @@ void WebAppFrameToolbarView::UpdateStatusIconsVisibility() {
 void WebAppFrameToolbarView::UpdateCaptionColors() {
   const BrowserNonClientFrameView* frame_view =
       browser_view_->frame()->GetFrameView();
+  DCHECK(frame_view);
 
-  // frame_view is nullptr during BrowserNonClientFrameViewAsh::Init().
-  if (!frame_view)
-    return;
   active_color_ = frame_view->GetCaptionColor(BrowserFrameActiveState::kActive);
   inactive_color_ =
       frame_view->GetCaptionColor(BrowserFrameActiveState::kInactive);
@@ -867,6 +859,10 @@ const char* WebAppFrameToolbarView::GetClassName() const {
 
 void WebAppFrameToolbarView::ChildPreferredSizeChanged(views::View* child) {
   PreferredSizeChanged();
+}
+
+void WebAppFrameToolbarView::OnThemeChanged() {
+  UpdateCaptionColors();
 }
 
 views::View* WebAppFrameToolbarView::GetContentSettingContainerForTesting() {
