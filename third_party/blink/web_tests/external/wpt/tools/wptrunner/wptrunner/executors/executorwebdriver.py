04057c03ea9879cf36ebd5dd538d6e3121ff6a0a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-01-06 21:00:33 +0000

    Import wpt@c9ea677557badbf83258f5e384bf9d3c0efb62cf
    Using wpt-import in Chromium 7b0b72154c5753ab16732df20a53de73d2aacd0d.
    With Chromium commits locally applied on WPT:
    400050e6ae "Fix toJSON in WindowPerformance and PerformanceResourceTiming"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: Ic4739759f628638dffd27478a507d56365ee6620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1988297
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#728627}

--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/executors/executorwebdriver.py
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/executors/executorwebdriver.py
@@ -536,7 +536,7 @@ class WebDriverCrashtestExecutor(CrashtestExecutor):
         if success:
             return self.convert_result(test, data)
 
-        return (test.result_cls(**data), [])
+        return (test.result_cls(*data), [])
 
     def do_crashtest(self, protocol, url, timeout):
         protocol.base.load(url)
