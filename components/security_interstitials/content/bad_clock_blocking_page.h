6099b78348aec5198b86d98aad87225a03790e0e
Colin Blundell <blundell@chromium.org>
2020-01-06 11:33:17 +0000

    Componentize BadClockBlockingPage
    It has been cleaned of //chrome-level dependencies in a predecessor CL,
    so the componentization is trivial.
    
    Bug: 1025059
    Change-Id: I47da26916a614fd8925bdbae1cb6fda9d72623ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1983236
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728484}

--- components/security_interstitials/content/bad_clock_blocking_page.h
+++ components/security_interstitials/content/bad_clock_blocking_page.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_SSL_BAD_CLOCK_BLOCKING_PAGE_H_
-#define CHROME_BROWSER_SSL_BAD_CLOCK_BLOCKING_PAGE_H_
+#ifndef COMPONENTS_SECURITY_INTERSTITIALS_CONTENT_BAD_CLOCK_BLOCKING_PAGE_H_
+#define COMPONENTS_SECURITY_INTERSTITIALS_CONTENT_BAD_CLOCK_BLOCKING_PAGE_H_
 
 #include <memory>
 #include <string>
@@ -71,4 +71,4 @@ class BadClockBlockingPage : public SSLBlockingPageBase {
   DISALLOW_COPY_AND_ASSIGN(BadClockBlockingPage);
 };
 
-#endif  // CHROME_BROWSER_SSL_BAD_CLOCK_BLOCKING_PAGE_H_
+#endif  // COMPONENTS_SECURITY_INTERSTITIALS_CONTENT_BAD_CLOCK_BLOCKING_PAGE_H_
