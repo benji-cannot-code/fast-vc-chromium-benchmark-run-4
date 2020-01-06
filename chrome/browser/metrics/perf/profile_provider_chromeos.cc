98b764c43a403293285464c03b6fe776470312a7
Gabriel Marin <gmx@chromium.org>
2020-01-06 22:13:03 +0000

    metrics/perf: remove the heap collector code
    BUG=1036859,b:147113394
    
    Change-Id: I10422d47e41057e1aca831cd7b65388f75d2fb20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982185
    Reviewed-by: Gabriel Marin <gmx@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Gabriel Marin <gmx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728653}

--- chrome/browser/metrics/perf/profile_provider_chromeos.cc
+++ chrome/browser/metrics/perf/profile_provider_chromeos.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
-#include "base/sampling_heap_profiler/sampling_heap_profiler.h"
-#include "chrome/browser/metrics/perf/heap_collector.h"
 #include "chrome/browser/metrics/perf/metric_provider.h"
 #include "chrome/browser/metrics/perf/perf_events_collector.h"
 #include "chrome/browser/metrics/perf/windowed_incognito_observer.h"
@@ -75,19 +73,6 @@ ProfileProvider::~ProfileProvider() {
 }
 
 void ProfileProvider::Init() {
-#if !defined(MEMORY_TOOL_REPLACES_ALLOCATOR)
-  if (base::FeatureList::IsEnabled(heap_profiling::kOOPHeapProfilingFeature)) {
-    HeapCollectionMode mode = HeapCollector::CollectionModeFromString(
-        base::GetFieldTrialParamValueByFeature(
-            heap_profiling::kOOPHeapProfilingFeature,
-            heap_profiling::kOOPHeapProfilingFeatureMode));
-    if (mode != HeapCollectionMode::kNone) {
-      collectors_.push_back(std::make_unique<MetricProvider>(
-          std::make_unique<HeapCollector>(mode)));
-    }
-  }
-#endif
-
   for (auto& collector : collectors_) {
     collector->Init();
   }
