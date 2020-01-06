6e7d4d23fe87e1e892ff7ee15d8f68145308940b
Toby Huang <tobyhuang@chromium.org>
2020-01-06 20:57:54 +0000

    Remove empty InitSync() method from SupervisedUserService
    This CL is follow-up cleanup work to crrev.com/c/1729631.
    
    Bug: 1035618
    Change-Id: I54cde36e2ace1cd364bcda0dfadd1d75370f9f78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1984707
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728622}

--- chrome/browser/chromeos/login/supervised/supervised_user_constants.h
+++ chrome/browser/chromeos/login/supervised/supervised_user_constants.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-extern const char kSupervisedUserTokenFilename[];
-
 extern const char kCryptohomeSupervisedUserKeyLabel[];
 extern const char kCryptohomeMasterKeyLabel[];
 extern const char kLegacyCryptohomeSupervisedUserKeyLabel[];
