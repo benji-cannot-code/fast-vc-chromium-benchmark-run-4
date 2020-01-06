45b8bc88ddff8bf2ca0c8e02be8728ba9c519796
Alexandre Courbot <acourbot@chromium.org>
2020-01-06 05:34:28 +0000

    media/gpu/v4l2: Use NativePixmapHandle to create GL/EGL images
    We used to pass a vector of DMABuf FDs to the GL/EGL image creation
    functions. This was very error-prone, as these vectors could contain
    less FDs than there were planes in the buffer, and this information
    was used to signal that the last FD contained several planes. The
    function receiving these incomplete FDs would then try to figure out the
    actual planes layout, with more or less luck, often introducing bugs in
    the way.
    
    Solve this issue once and for all by passing all importable buffer
    information within the VDAs through NativePixmapHandles: they always
    contain as many FDs as there are planes, and also have detailed layout
    information. On top of that, they are what we receive when using import
    mode (which means we can pass the exact layout information down to
    GL/EGL), and we already have a function that creates them from a
    VideoFrame. This helps reduce the amount of code, all the while making
    it safer and more readable.
    
    BUG=b:146842214
    TEST=VDAtest passes on Hana.
    TEST=VDAtest passes on Kevin with VD disabled.
    TEST=VDAtest passes on Kukui.
    TEST=crosvideo.appspot.com plays videos properly on Hana.
    TEST=crosvideo.appspot.com plays videos properly on Kevin with VD disabled.
    TEST=crosvideo.appspot.com plays videos properly on Kukui.
    TEST=Youtube Android plays properly on Hana.
    TEST=Youtube Android plays properly on Kevin with VD disabled.
    TEST=Youtube Android plays properly on Kukui.
    
    Change-Id: I88a78b786b1c90820221c9f0770a52a339913537
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982499
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728450}

--- media/gpu/v4l2/tegra_v4l2_device.h
+++ media/gpu/v4l2/tegra_v4l2_device.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "media/gpu/v4l2/v4l2_device.h"
+#include "ui/gfx/native_pixmap_handle.h"
 #include "ui/gl/gl_bindings.h"
 
 namespace media {
@@ -51,11 +52,11 @@ class TegraV4L2Device : public V4L2Device {
                              const gfx::Size& size,
                              unsigned int buffer_index,
                              const Fourcc fourcc,
-                             std::vector<base::ScopedFD>&& dmabuf_fds) override;
+                             gfx::NativePixmapHandle handle) override;
   scoped_refptr<gl::GLImage> CreateGLImage(
       const gfx::Size& size,
       const Fourcc fourcc,
-      std::vector<base::ScopedFD>&& dmabuf_fds) override;
+      gfx::NativePixmapHandle handle) override;
   EGLBoolean DestroyEGLImage(EGLDisplay egl_display,
                              EGLImageKHR egl_image) override;
   GLenum GetTextureTarget() override;
