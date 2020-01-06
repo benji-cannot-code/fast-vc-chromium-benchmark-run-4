ef77b85f83724a2ff40bd991441ed684fca85f5d
Adrienne Walker <enne@chromium.org>
2020-01-06 19:45:20 +0000

    storage: Make IndexedDBDispatcherHost not observe RPH
    This code was added as a part of idb mojo-ification in this patch:
    https://codereview.chromium.org/2727733004#msg2
    
    From comments in that code from three years ago, this cleanup is only a
    potential optimization to close pending transactions that would be
    aborted anyway.  It seems fine in the short term to remove this
    optimization.
    
    This does bring up some future questions about how the renderer/browser
    will communicate with IndexedDBDispatcherHost (which will almost
    live in storage service), and in that world it will be easy to hook this
    back up if needed by adding a mojo disconnect handler.
    
    Bug: 1015212
    Change-Id: Ie02f7c7a925b2296d7403fa542852e912e329246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986915
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728597}

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1546,8 +1546,6 @@ RenderProcessHostImpl::RenderProcessHostImpl(
       /* render_frame_id= */ ChildProcessHost::kInvalidUniqueID,
       storage_partition_impl_->GetServiceWorkerContext()));
 
-  AddObserver(indexed_db_factory_.get());
-
   InitializeChannelProxy();
 
   const int id = GetID();
