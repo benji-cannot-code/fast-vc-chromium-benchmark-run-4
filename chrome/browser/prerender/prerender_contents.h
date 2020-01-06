aaeccaf2b9180b4d498eccab859f3d826081aa6e
Robert Ogden <robertogden@chromium.org>
2020-01-06 17:04:44 +0000

    Remove prerender final status FINAL_STATUS_BAD_DEFERRED_REDIRECT
    This final status is defunct now that prerenders no longer run
    javascript.
    
    Bug: 1038061
    Change-Id: I48f5051deecb465de8c4a9e60088b372db4b093c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981101
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728539}

--- chrome/browser/prerender/prerender_contents.h
+++ chrome/browser/prerender/prerender_contents.h
@@ -42,7 +42,7 @@ namespace content {
 class RenderViewHost;
 class SessionStorageNamespace;
 class WebContents;
-}
+}  // namespace content
 
 namespace history {
 struct HistoryAddPageArgs;
@@ -295,7 +295,6 @@ class PrerenderContents : public content::NotificationObserver,
   // chrome::mojom::PrerenderCanceler:
   void CancelPrerenderForUnsupportedMethod() override;
   void CancelPrerenderForUnsupportedScheme(const GURL& url) override;
-  void CancelPrerenderForSyncDeferredRedirect() override;
 
   mojo::Receiver<chrome::mojom::PrerenderCanceler> prerender_canceler_receiver_{
       this};
