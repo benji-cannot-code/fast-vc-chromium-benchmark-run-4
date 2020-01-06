3da5edaa22f20d74572ebc916458dc5256ebc75b
Lei Zhang <thestig@chromium.org>
2020-01-06 20:49:42 +0000

    Simplify base::ByteSwapUintPtrT().
    Use static_assert() to remove the need for a NOTREACHED() and
    base/logging.h. Then do IWYU for any files that fail to build as a
    result of the header removal.
    
    Change-Id: I2bebcdfce84f9c814a8c38099e0e566812d57d8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977782
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nik Bhagat <nikunjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728619}

--- base/sys_byteorder.h
+++ base/sys_byteorder.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "base/logging.h"
 #include "build/build_config.h"
 
 #if defined(COMPILER_MSVC)
@@ -54,13 +53,11 @@ inline uintptr_t ByteSwapUintPtrT(uintptr_t x) {
   // because these conditionals are constexprs, the irrelevant branches will
   // likely be optimized away, so this construction should not result in code
   // bloat.
-  if (sizeof(uintptr_t) == 4) {
+  static_assert(sizeof(uintptr_t) == 4 || sizeof(uintptr_t) == 8,
+                "Unsupported uintptr_t size");
+  if (sizeof(uintptr_t) == 4)
     return ByteSwap(static_cast<uint32_t>(x));
-  } else if (sizeof(uintptr_t) == 8) {
-    return ByteSwap(static_cast<uint64_t>(x));
-  } else {
-    NOTREACHED();
-  }
+  return ByteSwap(static_cast<uint64_t>(x));
 }
 
 // Converts the bytes in |x| from host order (endianness) to little endian, and
--- components/variations/variations_murmur_hash.cc
+++ components/variations/variations_murmur_hash.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/variations_murmur_hash.h"
 
 #include "base/compiler_specific.h"
+#include "base/logging.h"
 #include "base/sys_byteorder.h"
 #include "build/build_config.h"
 
