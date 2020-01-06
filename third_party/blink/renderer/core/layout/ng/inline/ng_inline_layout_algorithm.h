02667b2dbba868014a7d5a8bdb66a5d9616799d0
Koji Ishii <kojii@chromium.org>
2020-01-06 03:43:21 +0000

    Move inline box placeholders to before children
    This patch changes |NGInlineLayoutAlgorithm| to place inline
    box placeholders to before children.
    
    Before this patch, they were created after children, only
    when the inline box is not culled. This change:
    1. Reduces the insertion to |Vector| in |CreateBoxFragments|,
       which involves memory copy. This is not common today, but
       will be common when |NGFragmentItem| is enabled.
    2. Allows easier implementation to "disable culled inline
       boxes" when |NGFragmetnItem| is enabled.
    
    There should be no behavior changes.
    
    Bug: 982194
    Change-Id: I0d7077e4c9b78b99276afc2e7919258b33bd4ea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980663
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728439}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_layout_algorithm.h
@@ -70,6 +70,7 @@ class CORE_EXPORT NGInlineLayoutAlgorithm final
 
   NGInlineBoxState* HandleOpenTag(const NGInlineItem&,
                                   const NGInlineItemResult&,
+                                  NGLineBoxFragmentBuilder::ChildList*,
                                   NGInlineLayoutStateStack*) const;
   NGInlineBoxState* HandleCloseTag(const NGInlineItem&,
                                    const NGInlineItemResult&,
