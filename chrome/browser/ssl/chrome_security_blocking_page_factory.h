df0a128e704a325a12b9623f97b5febfaee2f2e2
Colin Blundell <blundell@chromium.org>
2020-01-06 08:59:59 +0000

    [SSL] Prepare BadClockBlockingPage for componentization
    This CL prepares BadClockBlockingPage for componentization and sharing
    with WebLayer by cleaning it of //chrome-level dependencies.
    
    Bug: 1025059
    Change-Id: Icacd88b16ad1a50b7b93412986b9a113f138cee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982555
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728470}

--- chrome/browser/ssl/chrome_security_blocking_page_factory.h
+++ chrome/browser/ssl/chrome_security_blocking_page_factory.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SSL_CHROME_SECURITY_BLOCKING_PAGE_FACTORY_H_
 
 #include "base/macros.h"
+#include "chrome/browser/ssl/bad_clock_blocking_page.h"
 #include "components/security_interstitials/content/captive_portal_blocking_page.h"
 #include "components/security_interstitials/content/ssl_blocking_page.h"
 #include "components/security_interstitials/content/ssl_blocking_page_base.h"
@@ -37,6 +38,17 @@ class ChromeSecurityBlockingPageFactory {
       std::unique_ptr<SSLCertReporter> ssl_cert_reporter,
       const net::SSLInfo& ssl_info,
       int cert_error);
+
+  // Creates a bad clock blocking page. The caller is responsible for
+  // ownership of the returned object.
+  static BadClockBlockingPage* CreateBadClockBlockingPage(
+      content::WebContents* web_contents,
+      int cert_error,
+      const net::SSLInfo& ssl_info,
+      const GURL& request_url,
+      const base::Time& time_triggered,
+      ssl_errors::ClockState clock_state,
+      std::unique_ptr<SSLCertReporter> ssl_cert_reporter);
 
   // Does setup on |page| that is specific to the client (Chrome).
   static void DoChromeSpecificSetup(SSLBlockingPageBase* page);
