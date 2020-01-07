c4f9f2df3ff3a49bb64f58d8f7d3588f688a4691
Kurt Horimoto <kkhorimoto@google.com>
2020-01-07 15:04:28 +0000

    [iOS] Update InfobarBadgeTabHelper for banners presented via overlays.
    This CL updates BadgeMediator to add itself as an observer for the
    OverlayPresenter at OverlayModality::kInfobarBanner.  When the
    kInfobarOverlayUI feature is enabled, this implementation will
    update the InfobarBadgeTabHelper state for infobar banner presentations
    and dismissals.
    
    Bug: 1030357
    Change-Id: I2b529e3c015d6ca500691a44b82dc34b5798e7cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1987131
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728920}

---
+++ ios/chrome/browser/infobars/overlays/overlay_request_infobar_type_util.mm
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/infobars/overlays/overlay_request_infobar_type_util.h"
+
+#import "ios/chrome/browser/overlays/public/common/infobars/infobar_overlay_request_config.h"
+#include "ios/chrome/browser/overlays/public/overlay_request.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+InfobarType GetOverlayRequestInfobarType(OverlayRequest* request) {
+  return request->GetConfig<InfobarOverlayRequestConfig>()->infobar_type();
+}
