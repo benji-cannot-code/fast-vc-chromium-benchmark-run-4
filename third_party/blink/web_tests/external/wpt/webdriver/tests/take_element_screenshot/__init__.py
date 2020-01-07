aaec3e69745906861a88f687b2e4f5c1f3405804
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-01-07 17:11:19 +0000

    Import wpt@a74e1f57038e8cb818c2feb9cd879e540d50fcc1
    Using wpt-import in Chromium 9090b121c2952402792d9c7844c3b8db5112ee28.
    With Chromium commits locally applied on WPT:
    9d1727098a "[webnfc] NDEFScanOptions#mediaType being undefined means do not filter"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: Icbfb47bf25dba2679ac972d9d8f434cdaacaf686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989059
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#728965}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/take_element_screenshot/__init__.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/take_element_screenshot/__init__.py
@@ -1,11 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-def element_dimensions(session, element):
-    return tuple(session.execute_script("""
-        const {devicePixelRatio} = window;
-        let {width, height} = arguments[0].getBoundingClientRect();
+def element_rect(session, element):
+    return session.execute_script("""
+        let {devicePixelRatio} = window;
+        let {left, top, width, height} = arguments[0].getBoundingClientRect();
 
-        return [
-          Math.floor(width * devicePixelRatio),
-          Math.floor(height * devicePixelRatio),
-        ];
-        """, args=(element,)))
+        return {
+            x: Math.floor((left + window.pageXOffset) * devicePixelRatio),
+            y: Math.floor((top + window.pageYOffset) * devicePixelRatio),
+            width: Math.floor(width * devicePixelRatio),
+            height: Math.floor(height * devicePixelRatio),
+        };
+        """, args=(element,))
