16c9856c145e11397ea6e6d1de42801364f82ced
Ryan Sturm <ryansturm@chromium.org>
2020-01-07 13:38:28 +0000

    Conditionally removing DSE exception from same origin preconnect
    This behavior was intended to remove wasted preconnects, but we
    underestimated the likelihood of users coming back to DSE from a search
    result.
    
    Bug: 1036489
    Change-Id: Ib5567b70586e0c2f69ada922a0c6c1f02359d4f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1984630
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728901}

--- chrome/browser/navigation_predictor/navigation_predictor_preconnect_client.cc
+++ chrome/browser/navigation_predictor/navigation_predictor_preconnect_client.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
+#include "chrome/browser/navigation_predictor/search_engine_preconnector.h"
 #include "chrome/browser/predictors/loading_predictor.h"
 #include "chrome/browser/predictors/loading_predictor_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -124,7 +125,9 @@ void NavigationPredictorPreconnectClient::MaybePreconnectNow() {
   // On search engine results page, next navigation is likely to be a different
   // origin. Currently, the preconnect is only allowed for same origins. Hence,
   // preconnect is currently disabled on search engine results page.
-  if (IsSearchEnginePage())
+  // If preconnect to DSE is enabled, skip this check.
+  if (!base::FeatureList::IsEnabled(features::kPreconnectToSearch) &&
+      IsSearchEnginePage())
     return;
 
   url::Origin preconnect_origin =
