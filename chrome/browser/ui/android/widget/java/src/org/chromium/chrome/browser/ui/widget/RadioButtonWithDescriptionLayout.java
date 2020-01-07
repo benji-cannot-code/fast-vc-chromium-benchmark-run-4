65f19fd83b248b61db2bfde37157119f89d53be7
Wenyu Fu <wenyufu@google.com>
2020-01-07 01:06:41 +0000

    Fix test failure for RadioButtonWithDescriptionLayoutTest on N-devices
    1. Using toString methods when comparing CharSequences in test, to avoid
    issues for android.text.SpannableStringBuilder#equals;
    
    2. Merge testInflateFromXml with testCombinedRadioButtons and add more
    attribution checks
    
    Along with some misc fixes
    
    Change-Id: I2ebd08c88d29f9fa15066b0478c6c6184979a59d
    Bug: 1039039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1988911
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728731}

--- chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/RadioButtonWithDescriptionLayout.java
+++ chrome/browser/ui/android/widget/java/src/org/chromium/chrome/browser/ui/widget/RadioButtonWithDescriptionLayout.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
 
+import androidx.annotation.VisibleForTesting;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -161,6 +163,7 @@ public final class RadioButtonWithDescriptionLayout
      *
      * @param childIndex Index of the child to select.
      */
+    @VisibleForTesting
     void selectChildAtIndexForTesting(int childIndex) {
         int childCount = getChildCount();
         for (int i = 0; i < childCount; i++) {
