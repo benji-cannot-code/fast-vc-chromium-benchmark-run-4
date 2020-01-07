53acf555380b949a2c9f2e7f9feb8ca63764c747
Avery Musbach <amusbach@chromium.org>
2020-01-07 22:14:42 +0000

    wm: Remove DragWindowDetails::original_window_ and constructor parameter
    The present CL relieves DragWindowController::DragWindowDetails of
    unused stuff. original_window_ is set in CreateDragWindow, and nothing
    else is ever done with it. The constructor has a parameter
    original_window which is not even used to initialize original_window_.
    
    Bug: None
    Change-Id: I803a19d754d419b9a7bd4dc61e8db38a2b991f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989139
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729104}

--- ash/wm/drag_window_controller.cc
+++ ash/wm/drag_window_controller.cc
@@ -77,8 +77,7 @@ float GetDragWindowOpacity(aura::Window* root_window,
 // bounds and opacity based on the current bounds.
 class DragWindowController::DragWindowDetails : public aura::WindowDelegate {
  public:
-  DragWindowDetails(const display::Display& display,
-                    aura::Window* original_window)
+  explicit DragWindowDetails(const display::Display& display)
       : root_window_(Shell::GetRootWindowForDisplayId(display.id())) {}
 
   ~DragWindowDetails() override {
@@ -113,7 +112,6 @@ class DragWindowController::DragWindowDetails : public aura::WindowDelegate {
 
   void CreateDragWindow(aura::Window* original_window) {
     DCHECK(!drag_window_);
-    original_window_ = original_window;
     drag_window_ = window_factory::NewWindow(this).release();
     int parent_id = original_window->parent()->id();
     aura::Window* container = root_window_->GetChildById(parent_id);
@@ -197,8 +195,6 @@ class DragWindowController::DragWindowDetails : public aura::WindowDelegate {
 
   aura::Window* drag_window_ = nullptr;  // Owned by the container.
 
-  aura::Window* original_window_ = nullptr;
-
   // The copy of window_->layer() and its descendants.
   std::unique_ptr<ui::LayerTreeOwner> layer_owner_;
 
@@ -219,8 +215,7 @@ DragWindowController::DragWindowController(aura::Window* window,
   for (const display::Display& display : screen->GetAllDisplays()) {
     if (current.id() == display.id())
       continue;
-    drag_windows_.push_back(
-        std::make_unique<DragWindowDetails>(display, window_));
+    drag_windows_.push_back(std::make_unique<DragWindowDetails>(display));
   }
 }
 
