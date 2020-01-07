f049823dcd113b413b803f95f2aca64a59b862c7
Wez <wez@chromium.org>
2020-01-07 01:03:27 +0000

    [net] Assert expectations are met before returning from RunUntil*() calls.
    Verify that RunUntil*() calls are not returning prematurely, when the required
    conditions have not actually been met, e.g. due to Run() timeout.
    
    Bug: 1032620
    Change-Id: Ia2d2b2028c494705bd4e9502f1d9c19257c58f40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1985605
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728728}

--- net/server/http_server_unittest.cc
+++ net/server/http_server_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/callback_helpers.h"
@@ -220,9 +221,11 @@ class HttpServerTest : public TestWithTaskEnvironment,
       return;
 
     base::RunLoop run_loop;
-    run_loop_quit_func_ = run_loop.QuitClosure();
+    base::AutoReset<base::RepeatingClosure> run_loop_quit_func(
+        &run_loop_quit_func_, run_loop.QuitClosure());
     run_loop.Run();
-    run_loop_quit_func_.Reset();
+
+    ASSERT_EQ(requests_.size(), count);
   }
 
   void RunUntilConnectionIdClosed(int connection_id) {
@@ -234,9 +237,13 @@ class HttpServerTest : public TestWithTaskEnvironment,
     }
 
     base::RunLoop run_loop;
-    run_loop_quit_func_ = run_loop.QuitClosure();
+    base::AutoReset<base::RepeatingClosure> run_loop_quit_func(
+        &run_loop_quit_func_, run_loop.QuitClosure());
     run_loop.Run();
-    run_loop_quit_func_.Reset();
+
+    iter = connection_map_.find(connection_id);
+    ASSERT_TRUE(iter != connection_map_.end());
+    ASSERT_FALSE(iter->second);
   }
 
   HttpServerRequestInfo GetRequest(size_t request_index) {
--- services/network/public/cpp/server/http_server_unittest.cc
+++ services/network/public/cpp/server/http_server_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/run_loop.h"
@@ -229,9 +230,11 @@ class HttpServerTest : public testing::Test, public HttpServer::Delegate {
       return;
 
     base::RunLoop run_loop;
-    run_loop_quit_func_ = run_loop.QuitClosure();
+    base::AutoReset<base::RepeatingClosure> run_loop_quit_func(
+        &run_loop_quit_func_, run_loop.QuitClosure());
     run_loop.Run();
-    run_loop_quit_func_.Reset();
+
+    ASSERT_EQ(requests_.size(), count);
   }
 
   void RunUntilConnectionIdClosed(int connection_id) {
@@ -243,9 +246,13 @@ class HttpServerTest : public testing::Test, public HttpServer::Delegate {
     }
 
     base::RunLoop run_loop;
-    run_loop_quit_func_ = run_loop.QuitClosure();
+    base::AutoReset<base::RepeatingClosure> run_loop_quit_func(
+        &run_loop_quit_func_, run_loop.QuitClosure());
     run_loop.Run();
-    run_loop_quit_func_.Reset();
+
+    iter = connection_map_.find(connection_id);
+    ASSERT_TRUE(iter != connection_map_.end());
+    ASSERT_FALSE(iter->second);
   }
 
   HttpServerRequestInfo GetRequest(size_t request_index) {
