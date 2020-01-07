c6e2359dcff977c98f81ee7da88945c7f93c6592
rbpotter <rbpotter@chromium.org>
2020-01-07 18:44:51 +0000

    Print Preview: Fix issue with Google Drive autoselect hanging
    To support prefetching of destinations, we prefetch the Google Drive
    destination with autoselect=false when the user account changes in
    destination-settings, since it is displayed in the dropdown and its
    availability depends on the account. This caused a hang when Google
    Drive was the most recent printer, because the prefetch with
    autoselect=true triggered by it being the most recent returned too
    early due to the fetch having been already triggered by the account
    change.
    
    Fix by setting the autoselectMatchingDestination_ before returning
    early if the request has already been started, so that when the request
    completes Google Drive will be autoselected instead of just being added
    to the store.
    
    Also adding a test to verify the fix. Since testing this requires
    the cloud print interface stub's printer() method to be async, also
    updating a few other tests to continue working correctly.
    
    Bug: 1038645
    Change-Id: Id8f3f1228694c7ec6981d2d18549506760ec72cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1988521
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729013}

--- chrome/browser/resources/print_preview/data/destination_store.js
+++ chrome/browser/resources/print_preview/data/destination_store.js
@@ -443,18 +443,18 @@ export class DestinationStore extends EventTarget {
    * @private
    */
   fetchPreselectedDestination_(serializedDestination, autoselect) {
-    const key = createRecentDestinationKey(serializedDestination);
-    if (this.inFlightCloudPrintRequests_.has(key)) {
-      // Don't send another request if we are already fetching this
-      // destination.
-      return true;
-    }
-
     const id = serializedDestination.id;
     const origin = serializedDestination.origin;
     if (autoselect) {
       this.autoSelectMatchingDestination_ =
           this.createExactDestinationMatch_(origin, id);
+    }
+
+    const key = createRecentDestinationKey(serializedDestination);
+    if (this.inFlightCloudPrintRequests_.has(key)) {
+      // Don't send another request if we are already fetching this
+      // destination.
+      return true;
     }
 
     let error = false;
@@ -545,7 +545,8 @@ export class DestinationStore extends EventTarget {
     this.nativeLayer_.getEulaUrl(destinationId).then(response => {
       // Check that the currently selected destination ID still matches the
       // destination ID we used to fetch the EULA URL.
-      if (destinationId === this.selectedDestination_.id) {
+      if (this.selectedDestination_ &&
+          destinationId === this.selectedDestination_.id) {
         this.dispatchEvent(new CustomEvent(
             DestinationStore.EventType.DESTINATION_EULA_READY,
             {detail: response}));
@@ -1206,7 +1207,7 @@ export class DestinationStore extends EventTarget {
       this.updateDestination_(dest);
       // <if expr="chromeos">
       // Start the fetch for the PPD EULA URL.
-      this.fetchEulaUrl(this.selectedDestination_.id);
+      this.fetchEulaUrl(dest.id);
       // </if>
     }
   }
--- chrome/test/data/webui/print_preview/print_preview_ui_browsertest.js
+++ chrome/test/data/webui/print_preview/print_preview_ui_browsertest.js
@@ -1073,6 +1073,12 @@ TEST_F('PrintPreviewDestinationSettingsTest', 'GoogleDriveRecent', function() {
   this.runMochaTest(destination_settings_test.TestNames.GoogleDriveRecent);
 });
 
+TEST_F(
+    'PrintPreviewDestinationSettingsTest', 'GoogleDriveAutoselect', function() {
+      this.runMochaTest(
+          destination_settings_test.TestNames.GoogleDriveAutoselect);
+    });
+
 TEST_F('PrintPreviewDestinationSettingsTest', 'SelectSaveAsPdf', function() {
   this.runMochaTest(destination_settings_test.TestNames.SelectSaveAsPdf);
 });
