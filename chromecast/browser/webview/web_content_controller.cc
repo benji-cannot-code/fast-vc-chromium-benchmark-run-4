d93ac6342e63b177249a5da3c3ea01f192ad66e8
Daniel Nicoara <dnicoara@chromium.org>
2020-01-07 00:13:10 +0000

    Revert "Remove WebviewLayoutManager"
    Reason: Breaks Webview sizing.
    
    This reverts commit 35ac419df98d05824192dc776f837911b020f951.
    
    Bug: b/147002958
    Test: Manually
    Change-Id: Iee3eebcd2991036a0592ebf5b91ef7b18a11b0f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1987292
    Reviewed-by: Alex Sakhartchouk <alexst@chromium.org>
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728712}

--- chromecast/browser/webview/web_content_controller.cc
+++ chromecast/browser/webview/web_content_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chromecast/base/version.h"
 #include "chromecast/browser/webview/proto/webview.pb.h"
+#include "chromecast/browser/webview/webview_layout_manager.h"
 #include "chromecast/browser/webview/webview_navigation_throttle.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browsing_data_remover.h"
@@ -139,10 +140,8 @@ void WebContentController::ProcessRequest(
 void WebContentController::AttachTo(aura::Window* window, int window_id) {
   content::WebContents* contents = GetWebContents();
   auto* contents_window = contents->GetNativeView();
+  window->SetLayoutManager(new WebviewLayoutManager(window));
   contents_window->set_id(window_id);
-  contents_window->SetBounds(gfx::Rect(window->bounds().size()));
-  // Set the initial webview size as we depend on the client to do further
-  // sizing.
   contents_window->SetBounds(gfx::Rect(window->bounds().size()));
   // The aura window is hidden to avoid being shown via the usual layer method,
   // instead it is shows via a SurfaceDrawQuad by exo.
