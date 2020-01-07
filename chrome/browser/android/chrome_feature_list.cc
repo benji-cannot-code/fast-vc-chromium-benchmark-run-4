f08c51dbc870e42df9409f59ee85dc0d21a6195f
Jochen Eisinger <jochen@chromium.org>
2020-01-07 08:39:20 +0000

    Add feature flag to control handling of headers in incoming Intents
    Bug: 873178
    Change-Id: Ie8dc708369b4d08903d7ca2af3d147cc40a301b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978796
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728850}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -94,6 +94,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kAdjustWebApkInstallationSpace,
     &kAllowNewIncognitoTabIntents,
     &kAllowRemoteContextForNotifications,
+    &kAndroidBlockIntentNonSafelistedHeaders,
     &kAndroidNightMode,
     &kAndroidNightModeCCT,
     &kAndroidNightModeForQ,
@@ -259,6 +260,10 @@ const base::Feature* FindFeatureExposedToJava(const std::string& feature_name) {
 // Alphabetical:
 const base::Feature kAdjustWebApkInstallationSpace = {
     "AdjustWebApkInstallationSpace", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kAndroidBlockIntentNonSafelistedHeaders{
+    "AndroidBlockIntentNonSafelistedHeaders",
+    base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kAndroidNightMode{"AndroidNightMode",
                                       base::FEATURE_ENABLED_BY_DEFAULT};
