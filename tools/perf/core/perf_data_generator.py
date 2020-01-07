4c31e5a317e57221b6ce4477b859f78889813c43
Haiyang Pan <hypan@google.com>
2020-01-07 23:08:52 +0000

    Revert "[perf] Switch pixel2 webview to Android App Bundles"
    This reverts commit d0f4dea2ef20c53a3ffca5519a158b3226f9a46a.
    
    Reason for revert: Caused flaky test results in chromeperf (crbug.com/1039259)
    
    Original change's description:
    > [perf] Switch pixel2 webview to Android App Bundles
    > 
    > Bug: 986990
    > Change-Id: Ie86239ed57a6cbdc77aed65924c256fd2af21dc3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959661
    > Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    > Commit-Queue: Haiyang Pan <hypan@google.com>
    > Cr-Commit-Position: refs/heads/master@{#728993}
    
    TBR=crouleau@chromium.org,hypan@google.com
    
    Change-Id: Icbb1dcad946d3bdc6a04e3fe32808e5ae01a537b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 986990
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989999
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#729133}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -353,7 +353,7 @@ BUILDERS = {
         'isolate': 'performance_webview_test_suite',
       }
     ],
-    'platform': 'android-webview-google-bundle',
+    'platform': 'android-webview-google',
     'dimension': {
       'pool': 'chrome.tests.perf-webview',
       'os': 'Android',
@@ -465,7 +465,7 @@ BUILDERS = {
         'isolate': 'performance_webview_test_suite',
       }
     ],
-    'platform': 'android-webview-google-bundle',
+    'platform': 'android-webview-google',
     'dimension': {
       'pool': 'chrome.tests.perf-webview',
       'os': 'Android',
