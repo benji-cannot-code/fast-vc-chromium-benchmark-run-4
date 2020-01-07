119119b6d9a449cebdea7a36213fc6ece8cfe171
Mitsuru Oshima <oshima@chromium.org>
2020-01-07 00:31:02 +0000

    Use floating values for apps grid dragging
    This allows drag-scrolling at px boundary.
    
    Bug: 1038998
    Test: manually tested on device
    Change-Id: I6b5d87191bcc97ab5cf770c55fc48c36401b45fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1987753
    Reviewed-by: Jun Mukai <mukai@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728717}

--- ash/app_list/views/apps_grid_view.cc
+++ ash/app_list/views/apps_grid_view.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/paint_info.h"
 #include "ui/views/view_model_utils.h"
 #include "ui/views/widget/widget.h"
+#include "ui/wm/core/coordinate_conversion.h"
 
 namespace ash {
 
@@ -1122,10 +1123,10 @@ void AppsGridView::OnMouseEvent(ui::MouseEvent* event) {
       !event->IsLeftMouseButton()) {
     return;
   }
-
-  gfx::Point point_in_screen = event->location();
-  ConvertPointToScreen(static_cast<views::View*>(event->target()),
-                       &point_in_screen);
+  gfx::PointF point_in_screen = event->root_location_f();
+  auto* view_target = static_cast<views::View*>(event->target());
+  wm::ConvertPointToScreen(view_target->GetWidget()->GetNativeWindow(),
+                           &point_in_screen);
 
   switch (event->type()) {
     case ui::ET_MOUSE_PRESSED:
@@ -1185,8 +1186,8 @@ void AppsGridView::OnMouseEvent(ui::MouseEvent* event) {
       const bool should_handle = ShouldHandleDragEvent(*event);
 
       is_in_mouse_drag_ = false;
-      mouse_drag_start_point_ = gfx::Point();
-      last_mouse_drag_point_ = gfx::Point();
+      mouse_drag_start_point_ = gfx::PointF();
+      last_mouse_drag_point_ = gfx::PointF();
 
       if (!should_handle) {
         gfx::Point drag_location_in_app_list = event->location();
@@ -2780,7 +2781,7 @@ void AppsGridView::TransitionChanged() {
 
   // Sets the transform to locate the scrolled content.
   gfx::Size grid_size = GetTileGridSize();
-  gfx::Vector2d translate;
+  gfx::Vector2dF translate;
   const int dir =
       transition.target_page > pagination_model_.selected_page() ? -1 : 1;
   if (pagination_controller_->scroll_axis() ==
--- ash/public/cpp/pagination/pagination_controller.h
+++ ash/public/cpp/pagination/pagination_controller.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 
 namespace gfx {
-class Vector2d;
+class Vector2dF;
 class Rect;
 }  // namespace gfx
 
@@ -42,7 +42,7 @@ class ASH_PUBLIC_EXPORT PaginationController {
   // Handles a mouse wheel or touchpad scroll event in the area represented by
   // the PaginationModel. |offset| is the number of units scrolled in each axis.
   // Returns true if the event was captured and there was some room to scroll.
-  bool OnScroll(const gfx::Vector2d& offset, ui::EventType type);
+  bool OnScroll(const gfx::Vector2dF& offset, ui::EventType type);
 
   // Handles a touch gesture event in the area represented by the
   // PaginationModel. Returns true if the event was captured.
@@ -50,8 +50,8 @@ class ASH_PUBLIC_EXPORT PaginationController {
 
   // Handles a mouse event in the area represented by the PaginationModel.
   // |drag_offset| should be in screen coordinates.
-  void StartMouseDrag(const gfx::Vector2d& drag_offset);
-  void UpdateMouseDrag(const gfx::Vector2d& drag_offset,
+  void StartMouseDrag(const gfx::Vector2dF& drag_offset);
+  void UpdateMouseDrag(const gfx::Vector2dF& drag_offset,
                        const gfx::Rect& bounds);
   void EndMouseDrag(const ui::MouseEvent& event);
 
