9f17e5c7c6aafbfb1fd948923414d282b2d299ab
nancy <nancylingwang@chromium.org>
2020-01-07 00:23:47 +0000

    Split ArcAppWindowInfo to a separate file.
    This CL is pure refactor, to move ArcAppWindowInfo out of
    AppServiceAppWindowArcTracker to a separate file.
    
    BUG=1011235
    
    Change-Id: I967c43b8a53d7ba57a78c0ca3ab4c8e47803091e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986567
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728716}

--- chrome/browser/ui/ash/launcher/app_service/app_service_app_window_arc_tracker.h
+++ chrome/browser/ui/ash/launcher/app_service/app_service_app_window_arc_tracker.h
@@ -35,6 +35,7 @@ class Time;
 
 class AppServiceAppWindowLauncherController;
 class AppServiceAppWindowLauncherItemController;
+class ArcAppWindowInfo;
 class Profile;
 
 // AppServiceAppWindowArcTracker observes the ArcAppListPrefs to handle ARC app
@@ -91,8 +92,6 @@ class AppServiceAppWindowArcTracker : public ArcAppListPrefs::Observer,
   int active_task_id() const { return active_task_id_; }
 
  private:
-  class ArcAppWindowInfo;
-
   using TaskIdToArcAppWindowInfo =
       std::map<int, std::unique_ptr<ArcAppWindowInfo>>;
 
