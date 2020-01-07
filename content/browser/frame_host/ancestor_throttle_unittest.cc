ad11d14176c7397dff4c6e5f621e62ee5fd5bef3
Lucas Furukawa Gadani <lfg@chromium.org>
2020-01-07 15:43:13 +0000

    CSP: Disable AncestorThrottle.IgnoreWhenFrameAncestorsPresent when OutOfBlinkFrameAncestors is enabled.
    Bug: 759184
    Change-Id: If0ad2bf7fe0a41c3541b71bfb246443dbf78a397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1987196
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728931}

--- content/browser/frame_host/ancestor_throttle_unittest.cc
+++ content/browser/frame_host/ancestor_throttle_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/test_renderer_host.h"
 #include "net/http/http_response_headers.h"
+#include "services/network/public/cpp/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -130,6 +131,13 @@ TEST_F(AncestorThrottleTest, ErrorsParsingXFrameOptions) {
 }
 
 TEST_F(AncestorThrottleTest, IgnoreWhenFrameAncestorsPresent) {
+  // When OutOfBlinkFrameAncestors is enabled frame-ancestors is processed in
+  // the AncestorThrottle and XFO will not be parsed.
+  if (base::FeatureList::IsEnabled(
+          network::features::kOutOfBlinkFrameAncestors)) {
+    return;
+  }
+
   struct TestCase {
     const char* csp;
     AncestorThrottle::HeaderDisposition expected;
