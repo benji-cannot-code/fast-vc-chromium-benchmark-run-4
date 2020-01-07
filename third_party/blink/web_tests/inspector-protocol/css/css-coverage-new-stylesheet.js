c0972a2b9dfd02cd4a5c2ab423821e6db9357e0c
Peter Marshall <petermarshall@chromium.org>
2020-01-07 16:46:53 +0000

    DevTools: Try to fix flaky test css-coverage-new-stylesheet
    The flake no longer reproduces locally after this CL.
    
    The test waited for the onload event to be fired and assumed that
    coverage would include the newly loaded stylesheet, but we
    actually need to wait longer.
    
    We request an animation frame to ensure that the page is re-rendered
    with the newly loaded stylesheet.
    
    Bug: 901314
    Change-Id: I392a98a8e5e2c11af26dc73708a60e57c7e79a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989080
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728946}

--- third_party/blink/web_tests/inspector-protocol/css/css-coverage-new-stylesheet.js
+++ third_party/blink/web_tests/inspector-protocol/css/css-coverage-new-stylesheet.js
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   await dp.CSS.enable();
 
   await dp.CSS.startRuleUsageTracking();
-  await session.evaluateAsync(async function(url) {
+  await session.evaluateAsync(function(url) {
     const div = document.createElement('div');
     div.classList.add('usedAtTheVeryEnd');
     document.body.appendChild(div);
@@ -15,14 +15,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     link.rel = 'stylesheet';
     link.href = url;
     document.head.appendChild(link);
-    await new Promise(fulfill => link.onload = fulfill);
+    return new Promise(fulfill => link.onload = fulfill);
   }, testRunner.url('./resources/coverage2.css'));
+
+  // The onload is not enough to guarantee that the rendering has completed
+  // so we await an animation frame, too.
+  await session.evaluateAsync(function() {
+    let r;
+    const p = new Promise(resolve => r = resolve);
+    window.requestAnimationFrame(r);
+    return p;
+  });
+
   const response = await dp.CSS.stopRuleUsageTracking();
 
   if (response.result.ruleUsage.length === 1) {
-    testRunner.log('Successfully reported CSS coverage.')
+    testRunner.log('Successfully reported CSS coverage.');
   } else {
-    testRunner.log(`ERROR!`);
+    testRunner.log('ERROR!');
     testRunner.log(response.result.ruleUsage);
   }
   testRunner.completeTest();
