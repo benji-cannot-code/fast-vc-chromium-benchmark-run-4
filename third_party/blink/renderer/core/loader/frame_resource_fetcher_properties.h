58b418709a31b977055b5eeb4bb4122deb3f7da6
Tsuyoshi Horo <horo@chromium.org>
2020-01-07 02:16:52 +0000

    Initialize FrameResourceFetcherProperties::web_bundle_physical_url_ in ctor
    When HtmlImportsRequestInitiatorLock is disabled, FrameLoader::document_loader_
    is cleared by FrameLoader::DetachDocument() before FrameLoader::DetachDocument()
    calls Document::Shutdown()
     -> HTMLImportsController::Dispose()
      -> HTMLImportLoader::Dispose()
       -> Document::ClearImportsController()
        -> ResourceFetcher::ClearContext()
         -> DetachableResourceFetcherProperties::Detach()
          -> FrameResourceFetcherProperties::WebBundlePhysicalUrl()
           -> FrameOrImportedDocument::GetMasterDocumentLoader()
            -> FrameLoader::GetDocumentLoader()
    So frame_or_imported_document_->GetMasterDocumentLoader() returns invalid
    reference of DocumentLoader.
    
    This is causing crashes from Dec 20 when HtmlImportsRequestInitiatorLock was
    disabled in M80 using Field Trial Testing Configuration.
    
    To avoid this crash, this cl change FrameResourceFetcherProperties not to
    call ResourceFetcherProperties::WebBundlePhysicalUrl() from
    DetachableResourceFetcherProperties::Detach().
    
    Bug: 1036049
    Change-Id: If6a947ebec0dbbea0e39d5eb979eed8c8d39f018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978340
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#728756}

--- third_party/blink/renderer/core/loader/frame_resource_fetcher_properties.h
+++ third_party/blink/renderer/core/loader/frame_resource_fetcher_properties.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_fetcher_properties.h"
+#include "third_party/blink/renderer/platform/weborigin/kurl.h"
 
 namespace blink {
 
@@ -47,6 +48,7 @@ class CORE_EXPORT FrameResourceFetcherProperties final
  private:
   const Member<FrameOrImportedDocument> frame_or_imported_document_;
   Member<const FetchClientSettingsObject> fetch_client_settings_object_;
+  const KURL web_bundle_physical_url_;
 };
 
 }  // namespace blink
