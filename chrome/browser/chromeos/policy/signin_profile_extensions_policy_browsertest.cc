f7206f1ab1ab7614e4bc32229eeb51b28b0c5e88
Maksim Ivanov <emaxx@chromium.org>
2020-01-07 23:18:06 +0000

    Add test for offline launch of sign-in screen exts
    Add a browser test for the scenario when Chrome is launching without
    network connectivity and sign-in screen extensions are configured via
    device policy.
    
    This tests the functionality added in https://crrev.com/c/1943146.
    
    Bug: 1012892
    Change-Id: I4d3e60606f971f84ecc1e0b613c0cd8d7b9afb9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986079
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729136}

--- chrome/browser/chromeos/policy/signin_profile_extensions_policy_browsertest.cc
+++ chrome/browser/chromeos/policy/signin_profile_extensions_policy_browsertest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
 #include <string>
 
 #include "base/bind.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_source.h"
+#include "content/public/test/test_launcher.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
@@ -339,6 +341,59 @@ IN_PROC_BROWSER_TEST_F(SigninProfileExtensionsPolicyTest,
   EXPECT_NE(default_storage_partition, storage_partition_for_app);
   EXPECT_NE(default_storage_partition, storage_partition_for_extension);
   EXPECT_NE(storage_partition_for_app, storage_partition_for_extension);
+}
+
+// Class for testing the sign-in profile extensions with the simulated absence
+// of network connectivity.
+class SigninProfileExtensionsPolicyOfflineLaunchTest
+    : public SigninProfileExtensionsPolicyTest {
+ protected:
+  void SetUpOnMainThread() override {
+    SigninProfileExtensionsPolicyTest::SetUpOnMainThread();
+
+    test_extension_registry_observer_ =
+        std::make_unique<extensions::TestExtensionRegistryObserver>(
+            extensions::ExtensionRegistry::Get(GetInitialProfile()),
+            kWhitelistedAppId);
+
+    AddExtensionForForceInstallation(kWhitelistedAppId,
+                                     kWhitelistedAppUpdateManifestPath);
+
+    // In the non-PRE test, this simulates inability to make network requests
+    // for fetching the extension update manifest and CRX files. In the PRE test
+    // the server is not shut down, in order to allow the initial installation
+    // of the extension.
+    if (!content::IsPreTest())
+      EXPECT_TRUE(embedded_test_server()->ShutdownAndWaitUntilComplete());
+  }
+
+  void TearDownOnMainThread() override {
+    test_extension_registry_observer_.reset();
+
+    SigninProfileExtensionsPolicyTest::TearDownOnMainThread();
+  }
+
+  void WaitForTestExtensionLoaded() {
+    test_extension_registry_observer_->WaitForExtensionLoaded();
+  }
+
+ private:
+  std::unique_ptr<extensions::TestExtensionRegistryObserver>
+      test_extension_registry_observer_;
+};
+
+// This is the preparation step for the actual test. Here the whitelisted app
+// gets installed into the sign-in profile.
+IN_PROC_BROWSER_TEST_F(SigninProfileExtensionsPolicyOfflineLaunchTest,
+                       PRE_Test) {
+  WaitForTestExtensionLoaded();
+}
+
+// Tests that the whitelisted app gets launched using the cached version even
+// when there's no network connection (i.e., neither the extension update
+// manifest nor the CRX file can be fetched during this browser execution).
+IN_PROC_BROWSER_TEST_F(SigninProfileExtensionsPolicyOfflineLaunchTest, Test) {
+  WaitForTestExtensionLoaded();
 }
 
 }  // namespace policy
