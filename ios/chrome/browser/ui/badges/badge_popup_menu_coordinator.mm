88dd9faa4639b509f1c28c89d745c5dfbd288b77
Chris Lu <thegreenfrog@chromium.org>
2020-01-07 19:44:35 +0000

    [ios] Show Translate Modal when its item is selected in the Badge PopupMenu
    Bug: 1039668
    Change-Id: Iad51cf5284a3f4e3be363226f81cc5ce6f74f764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989847
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729039}

--- ios/chrome/browser/ui/badges/badge_popup_menu_coordinator.mm
+++ ios/chrome/browser/ui/badges/badge_popup_menu_coordinator.mm
@@ -114,6 +114,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [self.dispatcher displayModalInfobar:InfobarType::kInfobarTypeSaveCard];
       break;
     }
+    case PopupMenuActionShowTranslateOptions: {
+      [self.dispatcher displayModalInfobar:InfobarType::kInfobarTypeTranslate];
+      break;
+    }
     default:
       NOTREACHED() << "Unexpected identifier";
       break;
