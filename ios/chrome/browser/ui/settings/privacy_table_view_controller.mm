617ad97b787126eeaa29853631aa77d0ac895980
Huanzhong Huang <huanzhong@google.com>
2020-01-08 11:14:55 +0000

    [iOS] CBD cleanup: remove CBD collection view
    Part of the new clear browsing data dialog cleanup effort.
    This change removes branchings in a data source examining whether the
    consumer is a table view or a collection view. This change also drops
    source code of the collection view and the data item used by the
    collection view.
    
    Bug: 947456
    Change-Id: I25e6d8cd7832574f215d6b7834dcbffd856a7544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1984316
    Commit-Queue: Huanzhong Huang <huanzhong@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729287}

--- ios/chrome/browser/ui/settings/privacy_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/privacy_table_view_controller.mm
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_cell.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
-#import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_collection_view_controller.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_local_commands.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/handoff_table_view_controller.h"
--- ios/chrome/test/earl_grey/chrome_matchers_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_matchers_app_interface.mm
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/cells/clear_browsing_data_constants.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_cell.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
-#import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_collection_view_controller.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_ui_constants.h"
 #import "ios/chrome/browser/ui/settings/credit_card_scanner/credit_card_scanner_view_controller.h"
 #import "ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller_constants.h"
