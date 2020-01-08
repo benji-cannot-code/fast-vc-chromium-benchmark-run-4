f4e2352813931817af42e929aa9f80ed2e34ff77
Hiroki Nakagawa <nhiroki@chromium.org>
2020-01-08 07:45:23 +0000

    ServiceWorker: Clean up ServiceWorkerProviderHost
    This CL randomly cleans up ServiceWorkerProviderHost as follows:
    
    - Removing CreateForServiceWorker() and instead using the bare constructor.
    - Removing IsProviderForServiceWorker() because
      ServiceWorkerProviderHost is created only for service worker execution
      contexts after the previous CL and the check function no longer makes
      sense:
      https://chromium-review.googlesource.com/c/chromium/src/+/1945953
    - Inlining SetWorkerProcessId() in CompleteStartWorkerPreparation().
    
    Bug: 931087
    Change-Id: Ie91c295b56f5f5f5972d694f147be2e420e74484
    Tbr: kinuko@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980253
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729260}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -1695,8 +1695,9 @@ void ServiceWorkerVersion::StartWorkerInternal() {
   auto provider_info =
       blink::mojom::ServiceWorkerProviderInfoForStartWorker::New();
   DCHECK(!provider_host_);
-  provider_host_ = ServiceWorkerProviderHost::CreateForServiceWorker(
-      context(), base::WrapRefCounted(this), &provider_info);
+  provider_host_ = std::make_unique<ServiceWorkerProviderHost>(
+      provider_info->host_remote.InitWithNewEndpointAndPassReceiver(),
+      base::WrapRefCounted(this), context());
 
   auto params = blink::mojom::EmbeddedWorkerStartParams::New();
   params->service_worker_version_id = version_id_;
