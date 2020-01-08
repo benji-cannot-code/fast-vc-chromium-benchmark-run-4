34b8961d785d8a9896db2c7fb52da73479a47f05
Bettina <bdea@chromium.org>
2020-01-08 01:06:56 +0000

    Enable sample pings experiment by default.
    Enable sending sample pings to Safe Browsing for
    allowlist domains in M81 by default.
    
    Bug: 953106
    Change-Id: I5f1ed61e2ba5b692edf4cf19daf41d6d52848e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986793
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729173}

--- components/safe_browsing/core/features.cc
+++ components/safe_browsing/core/features.cc
@@ -51,7 +51,7 @@ const base::Feature kPasswordProtectionForSavedPasswords{
 
 const base::Feature kPasswordProtectionShowDomainsForSavedPasswords{
     "SafeBrowsingPasswordProtectionShowDomainsForSavedPasswords",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kPasswordProtectionForSignedInUsers{
     "SafeBrowsingPasswordProtectionForSignedInUsers",
