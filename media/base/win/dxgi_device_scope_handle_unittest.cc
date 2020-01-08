80bc74c66741a006487bd5a9ecf1eaa117919c81
Frank Li <frankli@microsoft.com>
2020-01-08 23:32:54 +0000

    media: Skip DXGIDeviceScopedHandleTest.UseDXGIDeviceScopedHandle test on Win7
    Bug: 1040237
    Change-Id: I0c29e343b139b3ed8d643ed14dbf3e5ca95e02ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992562
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729540}

--- media/base/win/dxgi_device_scope_handle_unittest.cc
+++ media/base/win/dxgi_device_scope_handle_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <d3d11.h>
 #include <mfapi.h>
 
+#include "base/win/windows_version.h"
 #include "media/base/test_helpers.h"
 #include "media/base/win/mf_helpers.h"
 #include "media/base/win/mf_initializer.h"
@@ -16,11 +17,15 @@ using Microsoft::WRL::ComPtr;
 
 class DXGIDeviceScopedHandleTest : public testing::Test {
  public:
-  DXGIDeviceScopedHandleTest() = default;
+  DXGIDeviceScopedHandleTest()
+      : test_supported_(base::win::GetVersion() >= base::win::Version::WIN10) {}
   ~DXGIDeviceScopedHandleTest() override = default;
 
  protected:
   void SetUp() override {
+    if (!test_supported_)
+      return;
+
     ASSERT_TRUE(InitializeMediaFoundation());
 
     // Get a shared DXGI Device Manager from Media Foundation.
@@ -51,16 +56,23 @@ class DXGIDeviceScopedHandleTest : public testing::Test {
   }
 
   void TearDown() override {
-    ASSERT_HRESULT_SUCCEEDED(MFUnlockDXGIDeviceManager());
+    if (test_supported_) {
+      ASSERT_HRESULT_SUCCEEDED(MFUnlockDXGIDeviceManager());
+    }
   }
 
   Microsoft::WRL::ComPtr<IMFDXGIDeviceManager> dxgi_device_man_ = nullptr;
   UINT device_reset_token_ = 0;
+  const bool test_supported_;
 };
 
 TEST_F(DXGIDeviceScopedHandleTest, UseDXGIDeviceScopedHandle) {
+  if (!test_supported_)
+    return;
+
   {
-    // Create DXGIDeviceScopedHandle in an inner scope without LockDevice call.
+    // Create DXGIDeviceScopedHandle in an inner scope without LockDevice
+    // call.
     mf::DXGIDeviceScopedHandle device_handle_1(dxgi_device_man_.Get());
   }
   {
