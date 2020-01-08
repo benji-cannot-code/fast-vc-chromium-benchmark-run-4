a10a8e14cb3bf15d032b391a5819fca03d5bf98a
Prabir Pradhan <prabirmsp@google.com>
2020-01-08 18:26:42 +0000

    Exo: Re-implement pointer capture using EventHandler
    Pointer Capture in Exo was initially implemented by using SetCapture()
    on the aura::Window. This delegates all input events (touch, keyboard,
    mouse, etc.) to the capture window, meaning that the user will not be
    able to interact with anything outside the window with any form of
    input.
    
    Rather than relying on SetCapture(), we set a pre-target handler that
    receives and consumes all mouse events when pointer capture is enabled.
    
      exo_unittests
    
    Bug: b/146164376
    Test: manual testing on ARC++ P using ArcPointerCaptureTestApp.apk;
    Change-Id: I99bcda823ce662e39a91920af98ae09d715bbc38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1966007
    Auto-Submit: Prabir Pradhan <prabirmsp@chromium.org>
    Commit-Queue: Prabir Pradhan <prabirmsp@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729404}

--- components/exo/pointer.h
+++ components/exo/pointer.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/surface_tree_host.h"
 #include "components/exo/wm_helper.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "ui/aura/client/capture_client_observer.h"
 #include "ui/aura/client/cursor_client_observer.h"
 #include "ui/aura/client/focus_change_observer.h"
 #include "ui/base/cursor/cursor.h"
@@ -49,7 +48,6 @@ class SurfaceTreeHost;
 class Pointer : public SurfaceTreeHost,
                 public SurfaceObserver,
                 public ui::EventHandler,
-                public aura::client::CaptureClientObserver,
                 public aura::client::CursorClientObserver,
                 public aura::client::FocusChangeObserver {
  public:
@@ -82,10 +80,6 @@ class Pointer : public SurfaceTreeHost,
   void OnMouseEvent(ui::MouseEvent* event) override;
   void OnScrollEvent(ui::ScrollEvent* event) override;
   void OnGestureEvent(ui::GestureEvent* event) override;
-
-  // Overridden from aura::client::CaptureClientObserver:
-  void OnCaptureChanged(aura::Window* lost_capture,
-                        aura::Window* gained_capture) override;
 
   // Overridden from aura::client::CursorClientObserver:
   void OnCursorSizeChanged(ui::CursorSize cursor_size) override;
@@ -162,8 +156,9 @@ class Pointer : public SurfaceTreeHost,
   // Moves the cursor to center of the active display.
   void MoveCursorToCenterOfActiveDisplay();
 
-  // Process the delta for relative pointer motion.
-  void HandleRelativePointerMotion(base::TimeTicks time_stamp,
+  // Process the delta for relative pointer motion. Returns true if relative
+  // motion was sent to the delegate, false otherwise.
+  bool HandleRelativePointerMotion(base::TimeTicks time_stamp,
                                    gfx::PointF location_in_target);
 
   // The delegate instance that all events are dispatched to.
@@ -193,7 +188,7 @@ class Pointer : public SurfaceTreeHost,
   // location of a generated move that was sent which should not be forwarded.
   base::Optional<gfx::Point> location_synthetic_move_;
 
-  // The window with input capture. Pointer capture is enabled if and only if
+  // The window with pointer capture. Pointer capture is enabled if and only if
   // this is not null.
   aura::Window* capture_window_ = nullptr;
 
