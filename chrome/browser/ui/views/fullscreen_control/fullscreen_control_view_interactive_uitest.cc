311910953f1dbcc919e17dafc6985d50cbffcaac
Yuwei Huang <yuweih@chromium.org>
2020-01-08 21:41:09 +0000

    [Fullscreen Control] Put mouse in cooldown mode if exit prompt is showing
    Currently if user puts their mouse cursor on the top of the screen and
    press F11 to enter fullscreen mode, the Exit Fullscreen X button and the
    "Press [ESC] to exit fullscreen" prompt will overlap (see bug for demo).
    
    This CL fixes this by putting the fullscreen control X button in
    cooldown mode if the exit prompt is still in showing-animation. In this
    case the user will need to withdraw the cursor from the top buffer area
    then move it back to the top of the screen to trigger the X button.
    Screen cast: https://screencast.googleplex.com/cast/NjU1MTk3NDQzMjQ3MzA4OHwzNTUxNWRmYi1kZA
    
    A simpler alternative design would be just to suppress the X button
    whenever the prompt is showing, and never bother with the cooldown mode,
    but that would end up having the X button immediately pop up even if the
    user just moves the cursor a little bit after the prompt is fully shown.
    
    Bug: 1038026
    Change-Id: If42517c7e87e8b9ab240ac5635f8f7cd33991447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986307
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729495}

--- chrome/browser/ui/views/fullscreen_control/fullscreen_control_view_interactive_uitest.cc
+++ chrome/browser/ui/views/fullscreen_control/fullscreen_control_view_interactive_uitest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/exclusive_access/exclusive_access_bubble_type.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller_test.h"
+#include "chrome/browser/ui/views/exclusive_access_bubble_views.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/fullscreen_control/fullscreen_control_host.h"
 #include "chrome/browser/ui/views/fullscreen_control/fullscreen_control_view.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/base_event_utils.h"
 #include "ui/events/event.h"
 #include "ui/events/keycodes/dom/dom_code.h"
+#include "ui/gfx/animation/animation_test_api.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/view.h"
 #include "url/gurl.h"
@@ -96,6 +98,12 @@ class FullscreenControlViewTest : public InProcessBrowserTest {
     return browser()->exclusive_access_manager();
   }
 
+  ExclusiveAccessBubbleViews* GetExclusiveAccessBubble() {
+    BrowserView* browser_view =
+        BrowserView::GetBrowserViewForBrowser(browser());
+    return browser_view->exclusive_access_bubble();
+  }
+
   KeyboardLockController* GetKeyboardLockController() {
     return GetExclusiveAccessManager()->keyboard_lock_controller();
   }
@@ -115,6 +123,19 @@ class FullscreenControlViewTest : public InProcessBrowserTest {
                                         blink::mojom::FullscreenOptions());
     fullscreen_observer.Wait();
     ASSERT_TRUE(delegate->IsFullscreenForTabOrPending(GetActiveWebContents()));
+  }
+
+  void EnterActiveTabFullscreenAndFinishPromptAnimation() {
+    EnterActiveTabFullscreen();
+    FinishPromptAnimation();
+  }
+
+  void FinishPromptAnimation() {
+    gfx::AnimationTestApi animation_api(
+        GetExclusiveAccessBubble()->animation_for_test());
+    base::TimeTicks far_future =
+        base::TimeTicks::Now() + base::TimeDelta::FromDays(1);
+    animation_api.Step(far_future);
   }
 
   bool EnableKeyboardLock() {
@@ -155,7 +176,7 @@ class FullscreenControlViewTest : public InProcessBrowserTest {
 // immediately created when FullscreenControlHost is created.
 IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest,
                        NoFullscreenPopupOnBrowserFullscreen) {
-  EnterActiveTabFullscreen();
+  EnterActiveTabFullscreenAndFinishPromptAnimation();
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   DCHECK(browser_view);
   ASSERT_TRUE(browser_view->IsFullscreen());
@@ -168,7 +189,7 @@ IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest,
 #if !defined(OS_MACOSX)
 
 IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest, MouseExitFullscreen) {
-  EnterActiveTabFullscreen();
+  EnterActiveTabFullscreenAndFinishPromptAnimation();
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   ASSERT_TRUE(browser_view->IsFullscreen());
 
@@ -197,7 +218,7 @@ IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest, MouseExitFullscreen) {
 
 IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest,
                        MouseExitFullscreen_TimeoutAndRetrigger) {
-  EnterActiveTabFullscreen();
+  EnterActiveTabFullscreenAndFinishPromptAnimation();
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   ASSERT_TRUE(browser_view->IsFullscreen());
 
@@ -249,8 +270,53 @@ IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest,
   ASSERT_TRUE(browser_view->IsFullscreen());
 }
 
-IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest, TouchPopupInteraction) {
+IN_PROC_BROWSER_TEST_F(
+    FullscreenControlViewTest,
+    MouseOnTopWhenPromptIsShowing_ButtonNotShownUntilMouseLeavesBufferArea) {
   EnterActiveTabFullscreen();
+  BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
+  ASSERT_TRUE(browser_view->IsFullscreen());
+
+  FullscreenControlHost* host = GetFullscreenControlHost();
+  host->Hide(false);
+  ASSERT_FALSE(host->IsVisible());
+
+  // Simulate moving the mouse to the top of the screen, which will not trigger
+  // the fullscreen exit UI yet since the prompt is still showing.
+  ui::MouseEvent mouse_move(ui::ET_MOUSE_MOVED, gfx::Point(1, 1), gfx::Point(),
+                            base::TimeTicks(), 0, 0);
+  host->OnMouseEvent(mouse_move);
+  ASSERT_FALSE(host->IsVisible());
+
+  // This still doesn't trigger the UI since the prompt is still showing.
+  mouse_move = ui::MouseEvent(ui::ET_MOUSE_MOVED, gfx::Point(2, 1),
+                              gfx::Point(), base::TimeTicks(), 0, 0);
+  host->OnMouseEvent(mouse_move);
+  ASSERT_FALSE(host->IsVisible());
+
+  FinishPromptAnimation();
+
+  // This still doesn't trigger the UI since it's in cooldown mode.
+  mouse_move = ui::MouseEvent(ui::ET_MOUSE_MOVED, gfx::Point(3, 1),
+                              gfx::Point(), base::TimeTicks(), 0, 0);
+  host->OnMouseEvent(mouse_move);
+  ASSERT_FALSE(host->IsVisible());
+
+  // Move the cursor out of the buffer area, which will have no effect.
+  mouse_move = ui::MouseEvent(ui::ET_MOUSE_MOVED, gfx::Point(2, 1000),
+                              gfx::Point(), base::TimeTicks(), 0, 0);
+  host->OnMouseEvent(mouse_move);
+  ASSERT_FALSE(host->IsVisible());
+
+  // The UI should be triggered now.
+  mouse_move = ui::MouseEvent(ui::ET_MOUSE_MOVED, gfx::Point(1, 1),
+                              gfx::Point(), base::TimeTicks(), 0, 0);
+  host->OnMouseEvent(mouse_move);
+  ASSERT_TRUE(host->IsVisible());
+}
+
+IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest, TouchPopupInteraction) {
+  EnterActiveTabFullscreenAndFinishPromptAnimation();
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   ASSERT_TRUE(browser_view->IsFullscreen());
 
@@ -330,7 +396,7 @@ IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest, TouchPopupInteraction) {
 
 IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest,
                        MouseAndTouchInteraction_NoInterference) {
-  EnterActiveTabFullscreen();
+  EnterActiveTabFullscreenAndFinishPromptAnimation();
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   ASSERT_TRUE(browser_view->IsFullscreen());
 
@@ -405,7 +471,7 @@ IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest,
 #endif
 
 IN_PROC_BROWSER_TEST_F(FullscreenControlViewTest, KeyboardPopupInteraction) {
-  EnterActiveTabFullscreen();
+  EnterActiveTabFullscreenAndFinishPromptAnimation();
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   ASSERT_TRUE(browser_view->IsFullscreen());
 
