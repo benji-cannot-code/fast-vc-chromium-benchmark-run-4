c753af5a12481f3fa73c8647c7951e9dcbc529f3
Josh Simmons <jds@chromium.org>
2020-01-08 00:55:27 +0000

    Adding PerformanceHintsObserver.
    This WebContentsObserver ensures that performance hints are loaded for the
    current page and provides an interface to query those hints.
    
    Bug: 1030813
    Change-Id: I061d6131d00690f7805fdf86e705a3f316ec081d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962630
    Commit-Queue: Josh Simmons <jds@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729168}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/optimization_guide/optimization_guide_web_contents_observer.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_initialize.h"
 #include "chrome/browser/password_manager/chrome_password_manager_client.h"
+#include "chrome/browser/performance_hints/performance_hints_observer.h"
 #include "chrome/browser/permissions/permission_request_manager.h"
 #include "chrome/browser/plugins/pdf_plugin_placeholder_observer.h"
 #include "chrome/browser/predictors/loading_predictor_factory.h"
@@ -245,6 +246,9 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   OutOfMemoryReporter::CreateForWebContents(web_contents);
   chrome::InitializePageLoadMetricsForWebContents(web_contents);
   PDFPluginPlaceholderObserver::CreateForWebContents(web_contents);
+  if (base::FeatureList::IsEnabled(kPerformanceHintsObserver)) {
+    PerformanceHintsObserver::CreateForWebContents(web_contents);
+  }
   PermissionRequestManager::CreateForWebContents(web_contents);
   // The PopupBlockerTabHelper has an implicit dependency on
   // ChromeSubresourceFilterClient being available in its constructor.
