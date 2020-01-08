285e0611a98592ca6d234ae0c9224b2af0cdf582
gogerald <gogerald@google.com>
2020-01-08 19:35:03 +0000

    [StartSurface] Exit fullscreen when there is no tabs in the current tab model
    When start surface is enabled, click back button may close the last tab and show start surface
    
    Bug: 1035338
    Change-Id: I8c00458696f215047af28091ec9d099c3236b939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974811
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729430}

--- chrome/android/java/src/org/chromium/chrome/browser/fullscreen/ChromeFullscreenManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/fullscreen/ChromeFullscreenManager.java
@@ -366,7 +366,7 @@ public class ChromeFullscreenManager extends FullscreenManager
             }
         }
 
-        if (tab == null && mBrowserVisibilityDelegate.get() == BrowserControlsState.SHOWN) {
+        if (tab == null && mBrowserVisibilityDelegate.get() != BrowserControlsState.HIDDEN) {
             setPositionsForTabToNonFullscreen();
         }
     }
