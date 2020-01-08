49533f489d6b5bb3f5039e60866b0fb62ef8d30c
Alexey Baskakov <loyso@chromium.org>
2020-01-08 04:46:40 +0000

    WebApp: Use shared ModelTypeStoreService by default.
    1) Remove kDesktopPWAsUSS flag. The USS-based Web Apps sync works fine.
    Let it be controlled by common kDesktopPWAsWithoutExtensions flag.
    
    2) Add kDesktopPWAsSharedStoreService flag which is enabled by default.
    This flag controls if the Web Apps system uses its own or shared instances of
    ModelTypeStore Keyed Service (the LevelDB persistence layer).
    
    Bug: 877898
    Change-Id: I861e0b4a3523b6bdb8922d0c009bab52dc6811c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981232
    Commit-Queue: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729232}

--- chrome/browser/web_applications/web_app_provider_factory.cc
+++ chrome/browser/web_applications/web_app_provider_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_provider_factory.h"
 
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/sync/model_type_store_service_factory.h"
 #include "chrome/browser/web_applications/components/web_app_utils.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -33,6 +34,7 @@ WebAppProviderFactory::WebAppProviderFactory()
   WebAppProviderBaseFactory::SetInstance(this);
   DependsOn(
       extensions::ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
+  DependsOn(ModelTypeStoreServiceFactory::GetInstance());
 }
 
 WebAppProviderFactory::~WebAppProviderFactory() {
--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -154,7 +154,7 @@ COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDesktopPWAsUnifiedLaunch;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
-extern const base::Feature kDesktopPWAsUSS;
+extern const base::Feature kDesktopPWAsSharedStoreService;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::Feature kDisallowUnsafeHttpDownloads;
