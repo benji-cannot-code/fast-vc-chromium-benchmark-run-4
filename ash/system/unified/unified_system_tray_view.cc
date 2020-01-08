90e1469b7655dee85608e4d1795c1217ececec1e
Ahmed Mehfooz <amehfooz@chromium.org>
2020-01-08 19:07:34 +0000

    Fix transparency for system tray color
    Bug: 1035361
    Change-Id: Ica3738aefd81e287e4fdafd0a46c6afa42fb5d05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986692
    Reviewed-by: Tim Song <tengs@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729420}

--- ash/system/unified/unified_system_tray_view.cc
+++ ash/system/unified/unified_system_tray_view.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/ash_features.h"
 #include "ash/session/session_controller_impl.h"
+#include "ash/shelf/shelf.h"
+#include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/style/ash_color_provider.h"
 #include "ash/style/default_color_constants.h"
@@ -230,9 +232,15 @@ class UnifiedSystemTrayView::FocusSearch : public views::FocusSearch {
 // static
 SkColor UnifiedSystemTrayView::GetBackgroundColor() {
   if (features::IsBackgroundBlurEnabled()) {
-    return AshColorProvider::Get()->DeprecatedGetBaseLayerColor(
-        AshColorProvider::BaseLayerType::kTransparent74,
-        kUnifiedMenuBackgroundColorWithBlur);
+    AshColorProvider::BaseLayerType layer_type =
+        Shelf::ForWindow(Shell::GetPrimaryRootWindow())
+                    ->shelf_widget()
+                    ->GetBackgroundType() == ShelfBackgroundType::kHomeLauncher
+            ? AshColorProvider::BaseLayerType::kTransparent60
+            : AshColorProvider::BaseLayerType::kTransparent74;
+
+    return AshColorProvider::Get()->GetBaseLayerColor(
+        layer_type, AshColorProvider::AshColorMode::kDark);
   }
   return AshColorProvider::Get()->DeprecatedGetBaseLayerColor(
       AshColorProvider::BaseLayerType::kTransparent90,
