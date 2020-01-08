0a1ba0fa93f5c21772713ca9323b395e6fba2a1b
Alex Gough <ajgo@chromium.org>
2020-01-08 18:50:00 +0000

    Remove unused SandboxType::kProfiling
    This supported out of process profiling but this is now back
    in the browser, so this sandbox type is not needed.
    
    Also removes |g_use_localtime_override| from libc interceptions
    which was the only thing controlled by the kProfiling sandbox type.
    This global defaulted to true so it is safe to remove it from the
    compound checks that used it.
    
    BUG=772503
    
    Change-Id: Id1639c595d10ef414be36fc1d62fd58864db002c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1988916
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729412}

--- services/service_manager/sandbox/mac/sandbox_mac.mm
+++ services/service_manager/sandbox/mac/sandbox_mac.mm
@@ -255,7 +255,6 @@ std::string SandboxMac::GetSandboxProfile(SandboxType sandbox_type) {
     case service_manager::SandboxType::kPpapi:
       profile += service_manager::kSeatbeltPolicyString_ppapi;
       break;
-    case service_manager::SandboxType::kProfiling:
     case service_manager::SandboxType::kUtility:
       profile += service_manager::kSeatbeltPolicyString_utility;
       break;
--- services/service_manager/sandbox/sandbox_type.cc
+++ services/service_manager/sandbox/sandbox_type.cc
@@ -37,7 +37,6 @@ bool IsUnsandboxedSandboxType(SandboxType sandbox_type) {
     case SandboxType::kPpapi:
     case SandboxType::kCdm:
     case SandboxType::kPdfCompositor:
-    case SandboxType::kProfiling:
 #if defined(OS_FUCHSIA)
     case SandboxType::kWebContext:
 #endif
@@ -84,7 +83,6 @@ void SetCommandLineFlagsForSandboxType(base::CommandLine* command_line,
     case SandboxType::kNetwork:
     case SandboxType::kCdm:
     case SandboxType::kPdfCompositor:
-    case SandboxType::kProfiling:
     case SandboxType::kAudio:
 #if defined(OS_WIN)
     case SandboxType::kXrCompositing:
@@ -163,8 +161,6 @@ std::string StringFromUtilitySandboxType(SandboxType sandbox_type) {
       return switches::kCdmSandbox;
     case SandboxType::kPdfCompositor:
       return switches::kPdfCompositorSandbox;
-    case SandboxType::kProfiling:
-      return switches::kProfilingSandbox;
     case SandboxType::kUtility:
       return switches::kUtilitySandbox;
     case SandboxType::kAudio:
@@ -213,8 +209,6 @@ SandboxType UtilitySandboxTypeFromString(const std::string& sandbox_string) {
     return SandboxType::kCdm;
   if (sandbox_string == switches::kPdfCompositorSandbox)
     return SandboxType::kPdfCompositor;
-  if (sandbox_string == switches::kProfilingSandbox)
-    return SandboxType::kProfiling;
 #if defined(OS_WIN)
   if (sandbox_string == switches::kXrCompositingSandbox)
     return SandboxType::kXrCompositing;
