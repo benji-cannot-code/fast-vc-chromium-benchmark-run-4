e9f14aac67ab59d8b0c87c33f4691171545015ec
Charlene Yan <cyan@chromium.org>
2020-01-08 21:15:31 +0000

    [Tab Groups] Add interactive browser tests for the tab editor bubble.
    Bug: 1033962
    Change-Id: Idbc5d1108a43d6de1637f40caddadfa42be005fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1990334
    Reviewed-by: Charlene Yan <cyan@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729479}

--- chrome/browser/ui/views/tabs/tab_group_header.h
+++ chrome/browser/ui/views/tabs/tab_group_header.h
@@ -44,6 +44,8 @@ class TabGroupHeader : public TabSlotView {
   void RemoveObserverFromWidget(views::Widget* widget);
 
  private:
+  friend class TabGroupEditorBubbleViewDialogBrowserTest;
+
   // Calculate the width for this View.
   int CalculateWidth() const;
 
@@ -61,6 +63,7 @@ class TabGroupHeader : public TabSlotView {
 
     void Opened(views::Widget* bubble_widget);
     bool is_open() const { return is_open_; }
+    views::Widget* widget() const { return widget_; }
 
     // views::WidgetObserver:
     void OnWidgetDestroyed(views::Widget* widget) override;
