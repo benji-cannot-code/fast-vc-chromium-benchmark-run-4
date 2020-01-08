f11cf27341421ddd416be41c6297f3292ce124ff
Thanh Nguyen <thanhdng@chromium.org>
2020-01-08 04:25:42 +0000

    [smart-charging] Add UKM logger
    This CL creates a UKM logger for smart charging. It takes a
    UserChargingEvent proto and log it using UKM.
    Design doc: go/cros-smart-charging-logging
    
    Bug: 1028853
    Change-Id: I710928118af7941a9742959d1d972d2bf8db2526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1985619
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729223}

---
+++ chrome/browser/chromeos/power/smart_charging/smart_charging_ukm_logger.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_POWER_SMART_CHARGING_SMART_CHARGING_UKM_LOGGER_H_
+#define CHROME_BROWSER_CHROMEOS_POWER_SMART_CHARGING_SMART_CHARGING_UKM_LOGGER_H_
+
+#include "services/metrics/public/cpp/ukm_source_id.h"
+
+namespace chromeos {
+namespace power {
+class UserChargingEvent;
+
+class SmartChargingUkmLogger {
+ public:
+  SmartChargingUkmLogger() = default;
+  ~SmartChargingUkmLogger() = default;
+  SmartChargingUkmLogger(const SmartChargingUkmLogger&) = delete;
+  SmartChargingUkmLogger& operator=(const SmartChargingUkmLogger&) = delete;
+
+  void LogEvent(const UserChargingEvent& user_charging_event) const;
+};
+}  // namespace power
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_POWER_SMART_CHARGING_SMART_CHARGING_UKM_LOGGER_H_
