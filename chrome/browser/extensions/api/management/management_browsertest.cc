19f4dddf083f76b08ae979fbf9f069b49302e020
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-01-08 21:00:10 +0000

    [Extensions Cleanup] Remove WaitForExtensionInstallError()
    Remove ExtensionBrowserTest::WaitForExtensionInstallError() and
    ChromeExtensionTestNotificationObserver::WaitForExtensionInstallError().
    The latter was only called by the former, which in turn was only called
    in a single place.
    
    Inline the (short) implementation directly into the single test that
    called it. In addition to removing cruft from the (very large)
    ExtensionBrowserTest interface, this also fixes a potential issue:
    the proper pattern for these types of observers is:
    <instantiate test observer>
    <trigger behavior>
    <wait with test observer>
    
    Here, it was:
    <trigger behavior>
    <instantiate and wait with test observer>
    
    This is can be racy or incorrect depending on the triggering behavior,
    since it may already finish before the observer is instantiated.
    
    Bug: None
    Change-Id: If35c9faff6ac60e86b476c5064ad0d0d1dd7f739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981050
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729471}

--- chrome/browser/extensions/api/management/management_browsertest.cc
+++ chrome/browser/extensions/api/management/management_browsertest.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/test_utils.h"
 #include "content/public/test/url_loader_interceptor.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_host_observer.h"
@@ -460,8 +461,14 @@ IN_PROC_BROWSER_TEST_F(ExtensionManagementTest, MAYBE_AutoUpdate) {
   extensions::ExtensionUpdater::CheckParams params2;
   params2.callback = base::BindOnce(&NotificationListener::OnFinished,
                                     base::Unretained(&notification_listener));
-  service->updater()->CheckNow(std::move(params2));
-  ASSERT_TRUE(WaitForExtensionInstallError());
+
+  {
+    content::WindowedNotificationObserver install_error_observer(
+        extensions::NOTIFICATION_EXTENSION_INSTALL_ERROR,
+        content::NotificationService::AllSources());
+    service->updater()->CheckNow(std::move(params2));
+    install_error_observer.Wait();
+  }
   ASSERT_TRUE(notification_listener.started());
   ASSERT_TRUE(notification_listener.finished());
   ASSERT_TRUE(base::Contains(notification_listener.updates(),
