3b684f21d3cea6cf54d687c20ffdbd9822f9f52b
David Black <dmblack@google.com>
2020-01-08 21:19:34 +0000

    Close Assistant Web UI container when disabling Assistant.
    Previously, it was possible to open the Assistant Web UI container and
    then disable Assistant in the background which would cause our
    container to persist when it should actually be destroyed.
    
    Bug: b:147344165
    Change-Id: Ia65070fb4d108efb967232f767628e91454d9f8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992312
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#729481}

--- ash/assistant/assistant_web_ui_controller.h
+++ ash/assistant/assistant_web_ui_controller.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/assistant/assistant_controller_observer.h"
 #include "ash/assistant/assistant_web_view_delegate_impl.h"
+#include "ash/public/cpp/assistant/assistant_state.h"
 #include "base/macros.h"
 #include "ui/views/widget/widget_observer.h"
 
@@ -22,7 +23,8 @@ class AssistantWebContainerEventObserver;
 
 // The class to manage Assistant web container view.
 class ASH_EXPORT AssistantWebUiController : public views::WidgetObserver,
-                                            public AssistantControllerObserver {
+                                            public AssistantControllerObserver,
+                                            public AssistantStateObserver {
  public:
   explicit AssistantWebUiController(AssistantController* assistant_controller);
   ~AssistantWebUiController() override;
@@ -31,17 +33,22 @@ class ASH_EXPORT AssistantWebUiController : public views::WidgetObserver,
   void OnWidgetDestroying(views::Widget* widget) override;
 
   // AssistantControllerObserver:
+  void OnAssistantControllerConstructed() override;
   void OnAssistantControllerDestroying() override;
   void OnDeepLinkReceived(
       assistant::util::DeepLinkType type,
       const std::map<std::string, std::string>& params) override;
+
+  // AssistantStateObserver:
+  void OnAssistantSettingsEnabled(bool enabled) override;
 
   void OnBackButtonPressed();
 
   AssistantWebContainerView* GetViewForTest();
 
  private:
-  void ShowUi();
+  void ShowUi(const GURL& url);
+  void CloseUi();
 
   // Constructs/resets |web_container_view_|.
   void CreateWebContainerView();
