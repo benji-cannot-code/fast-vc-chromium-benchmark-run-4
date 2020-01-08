6a2ad677364321c7791a06898e60e0c557dc8da1
Johannes Henkel <johannes@chromium.org>
2020-01-08 02:28:29 +0000

    [DevTools] Roll inspector_protocol (Chromium).
    Upstream PR: https://chromium-review.googlesource.com/c/deps/inspector_protocol/+/1989575
    Remove support for emitting cbor into an std::string.
    
    New Revision: b29d8a5d2bb56b3e555bb27a0e035a9d571c5f28
    
    Change-Id: I242db4b1d6ebf5669a1a7b02b8b19c508a684f62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1990209
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Johannes Henkel <johannes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729194}

--- content/browser/devtools/devtools_protocol_encoding_json_fuzzer.cc
+++ content/browser/devtools/devtools_protocol_encoding_json_fuzzer.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  std::string cbor;
+  std::vector<uint8_t> cbor;
   crdtp::json::ConvertJSONToCBOR(crdtp::span<uint8_t>(data, size), &cbor);
   return 0;
 }
