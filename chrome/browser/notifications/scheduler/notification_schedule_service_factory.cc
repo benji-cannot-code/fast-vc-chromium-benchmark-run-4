a39696a57fcb84712b7a15a39175db90b5e9590e
Hesen Zhang <hesen@google.com>
2020-01-08 17:42:44 +0000

    [Update notificaiton service]: Display flow.
    - Change notification scheduler registar to be able passing
    browser context pointer so that the service calback can be
    injected into client's ctor.
    
    - Implemented BeforeShowNotification in the client code,
    completed display flow.
    
    Bug: 1013685
    Change-Id: Ibfa0cdc43435b31657ac386db11a2069a6144a18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1984746
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Auto-Submit: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729386}

--- chrome/browser/notifications/scheduler/notification_schedule_service_factory.cc
+++ chrome/browser/notifications/scheduler/notification_schedule_service_factory.cc
@@ -24,18 +24,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/scheduler/display_agent_android.h"
 #include "chrome/browser/notifications/scheduler/notification_background_task_scheduler_android.h"
 #include "chrome/browser/updates/update_notification_client.h"
+#include "chrome/browser/updates/update_notification_service_factory.h"
 #endif  // defined(OS_ANDROID)
 
 namespace {
 std::unique_ptr<notifications::NotificationSchedulerClientRegistrar>
-RegisterClients() {
+RegisterClients(content::BrowserContext* context) {
   auto client_registrar =
       std::make_unique<notifications::NotificationSchedulerClientRegistrar>();
   // TODO(xingliu): Register clients here.
 #if defined(OS_ANDROID)
   // Register UpdateNotificationClient.
+  auto update_notification_service_callback = base::BindRepeating(
+      &UpdateNotificationServiceFactory::GetForBrowserContext, context);
   auto chrome_update_client =
-      std::make_unique<updates::UpdateNotificationClient>();
+      std::make_unique<updates::UpdateNotificationClient>(
+          std::move(update_notification_service_callback));
   client_registrar->RegisterClient(
       notifications::SchedulerClientType::kChromeUpdate,
       std::move(chrome_update_client));
@@ -73,7 +77,7 @@ KeyedService* NotificationScheduleServiceFactory::BuildServiceInstanceFor(
   auto* profile = Profile::FromBrowserContext(context);
   base::FilePath storage_dir =
       profile->GetPath().Append(chrome::kNotificationSchedulerStorageDirname);
-  auto client_registrar = RegisterClients();
+  auto client_registrar = RegisterClients(context);
 #if defined(OS_ANDROID)
   auto display_agent = std::make_unique<DisplayAgentAndroid>();
   auto background_task_scheduler =
