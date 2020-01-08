e81105711d81e00cb997c9b4fd0763aad6156ee7
Yan, Shaobo <shaobo.yan@intel.com>
2020-01-08 05:12:56 +0000

    Roll WebGPU CTS
    Roll in copyImageBitmapToTexture cts
    
    Change-Id: Ie4ddf5f7dad5fa6487cbaedc5ca78e6e1d448602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986630
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729235}

--- third_party/blink/web_tests/external/wpt/webgpu/framework/version.js
+++ third_party/blink/web_tests/external/wpt/webgpu/framework/version.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '70754155a0bca2c24e3e3c249bab9ee3dd765a26';
+export const version = 'a5c59f8995f38843e332ff4d869a72f75d2f028d';
