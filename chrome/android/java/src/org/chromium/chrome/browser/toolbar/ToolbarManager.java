3a928802afaaa045f297cf1328162162c03864ab
Mia Glaese <glamia@chromium.org>
2020-01-08 21:52:11 +0000

    [Start Surface] Switch to Homepage when search accelerator is tapped.
    If Tabswitcher is shown, we switch to HomePage which owns fakebox.
    
    Bug: 1039467
    Change-Id: I9a59c9dba89acc9a2644a12663fafaba054d0d95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989867
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Mia Glaese <glamia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729502}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -251,7 +251,9 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
 
     private int mCurrentOrientation;
 
-    /** Runnable for the home button when Start Surface home page is enabled. */
+    /**
+     * Runnable for the home and search accelerator button when Start Surface home page is enabled.
+     */
     private Supplier<Boolean> mShowStartSurfaceSupplier;
 
     /**
@@ -802,6 +804,11 @@ public class ToolbarManager implements ScrimObserver, ToolbarTabController, UrlF
         final OnClickListener searchAcceleratorListener = v -> {
             recordBottomToolbarUseForIPH();
             ACCELERATOR_BUTTON_TAP_ACTION.record();
+
+            // Only switch to HomePage when overview is showing.
+            if (mOverviewModeBehavior.overviewVisible()) {
+                mShowStartSurfaceSupplier.get();
+            }
             setUrlBarFocus(true, LocationBar.OmniboxFocusReason.ACCELERATOR_TAP);
         };
 
