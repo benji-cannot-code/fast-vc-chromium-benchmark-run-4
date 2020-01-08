3ad684bd4e400ce89436aad1d63beebc75b2ab06
Xianzhu Wang <wangxianzhu@chromium.org>
2020-01-08 00:32:30 +0000

    [CompositeAfterPaint] Fix backdrop filter mask
    Same as blend mode, backdrop filter also uses the backdrop as an input,
    so when we synthesize a mask clip for a backdrop filter, we also need
    to move the backdrop filter up to the outermost mask isolation effect
    node. Now use the same code path previously for delegated blend modes
    also for backdrop filters.
    
    Different from blend mode, a backdrop filter has geometry which needs
    a proper transform space which may be different from the transform
    space of the synthesized clip. To deal with this, the synthesized clip
    layer needs to transform the clip operations.
    
    Bug: 923429
    Change-Id: I6294dc6ec68a6fe0e7966bdae69fd5b5754fb965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1977892
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729162}

--- third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
+++ third_party/blink/renderer/platform/graphics/compositing/paint_artifact_compositor.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.h"
 #include "third_party/blink/renderer/platform/graphics/compositing_reasons.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_layer_client.h"
+#include "third_party/blink/renderer/platform/graphics/paint/geometry_mapper.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_controller.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -74,10 +75,10 @@ class SynthesizedClip : private cc::ContentLayerClient {
   }
 
   void UpdateLayer(bool needs_layer,
-                   const FloatRoundedRect& rrect,
-                   scoped_refptr<const RefCountedPath> path);
+                   const ClipPaintPropertyNode&,
+                   const TransformPaintPropertyNode&);
 
-  cc::Layer* Layer() { return layer_.get(); }
+  cc::PictureLayer* Layer() { return layer_.get(); }
   CompositorElementId GetMaskIsolationId() const { return mask_isolation_id_; }
   CompositorElementId GetMaskEffectId() const { return mask_effect_id_; }
 
@@ -92,8 +93,9 @@ class SynthesizedClip : private cc::ContentLayerClient {
 
  private:
   scoped_refptr<cc::PictureLayer> layer_;
-  gfx::Vector2dF layer_origin_;
-  SkRRect local_rrect_ = SkRRect::MakeEmpty();
+  GeometryMapper::Translation2DOrMatrix translation_2d_or_matrix_;
+  bool rrect_is_local_ = false;
+  SkRRect rrect_;
   scoped_refptr<const RefCountedPath> path_;
   CompositorElementId mask_isolation_id_;
   CompositorElementId mask_effect_id_;
@@ -316,6 +318,7 @@ class PLATFORM_EXPORT PaintArtifactCompositor final
   // However, |mask_isolation_id| is always set.
   SynthesizedClip& CreateOrReuseSynthesizedClipLayer(
       const ClipPaintPropertyNode&,
+      const TransformPaintPropertyNode&,
       bool needs_layer,
       CompositorElementId& mask_isolation_id,
       CompositorElementId& mask_effect_id) final;
