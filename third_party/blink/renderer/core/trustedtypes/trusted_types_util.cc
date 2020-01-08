63c4dfba9ce043592b150206867af5d16f9fc7cc
Yifan Luo <lyf@google.com>
2020-01-08 11:50:52 +0000

    [Trusted Types] Change error message for empty default createXYZ policy.
    Bug: 1008022
    Change-Id: If4236bdbbf27be773b86d50097bd695335b214a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989071
    Commit-Queue: Yifan Luo <lyf@google.com>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729297}

--- third_party/blink/renderer/core/trustedtypes/trusted_types_util.cc
+++ third_party/blink/renderer/core/trustedtypes/trusted_types_util.cc
@@ -38,8 +38,11 @@ enum TrustedTypeViolationKind {
   kTrustedScriptAssignment,
   kTrustedScriptURLAssignment,
   kTrustedHTMLAssignmentAndDefaultPolicyFailed,
+  kTrustedHTMLAssignmentAndNoDefaultPolicyExisted,
   kTrustedScriptAssignmentAndDefaultPolicyFailed,
+  kTrustedScriptAssignmentAndNoDefaultPolicyExisted,
   kTrustedScriptURLAssignmentAndDefaultPolicyFailed,
+  kTrustedScriptURLAssignmentAndNoDefaultPolicyExisted,
   kNavigateToJavascriptURL,
   kNavigateToJavascriptURLAndDefaultPolicyFailed,
   kScriptExecution,
@@ -59,12 +62,21 @@ const char* GetMessage(TrustedTypeViolationKind kind) {
     case kTrustedHTMLAssignmentAndDefaultPolicyFailed:
       return "This document requires 'TrustedHTML' assignment and the "
              "'default' policy failed to execute.";
+    case kTrustedHTMLAssignmentAndNoDefaultPolicyExisted:
+      return "This document requires 'TrustedHTML' assignment and no "
+             "'default' policy for 'TrustedHTML' has been defined.";
     case kTrustedScriptAssignmentAndDefaultPolicyFailed:
       return "This document requires 'TrustedScript' assignment and the "
              "'default' policy failed to execute.";
+    case kTrustedScriptAssignmentAndNoDefaultPolicyExisted:
+      return "This document requires 'TrustedScript' assignment and no "
+             "'default' policy for 'TrustedScript' has been defined.";
     case kTrustedScriptURLAssignmentAndDefaultPolicyFailed:
       return "This document requires 'TrustedScriptURL' assignment and the "
              "'default' policy failed to execute.";
+    case kTrustedScriptURLAssignmentAndNoDefaultPolicyExisted:
+      return "This document requires 'TrustedScriptURL' assignment and no "
+             "'default' policy for 'TrustedScriptURL' has been defined.";
     case kNavigateToJavascriptURL:
       return "This document requires 'TrustedScript' assignment. "
              "Navigating to a javascript:-URL is equivalent to a "
@@ -355,7 +367,7 @@ String GetStringFromTrustedHTML(const String& string,
   }
 
   if (!default_policy->HasCreateHTML()) {
-    if (TrustedTypeFail(kTrustedHTMLAssignmentAndDefaultPolicyFailed,
+    if (TrustedTypeFail(kTrustedHTMLAssignmentAndNoDefaultPolicyExisted,
                         execution_context, exception_state, string)) {
       return g_empty_string;
     } else {
@@ -421,7 +433,7 @@ String GetStringFromTrustedScript(const String& potential_script,
   }
 
   if (!default_policy->HasCreateScript()) {
-    if (TrustedTypeFail(kTrustedScriptAssignmentAndDefaultPolicyFailed,
+    if (TrustedTypeFail(kTrustedScriptAssignmentAndNoDefaultPolicyExisted,
                         execution_context, exception_state, potential_script)) {
       return g_empty_string;
     } else {
@@ -477,7 +489,7 @@ String GetStringFromTrustedScriptURL(
   }
 
   if (!default_policy->HasCreateScriptURL()) {
-    if (TrustedTypeFail(kTrustedScriptURLAssignmentAndDefaultPolicyFailed,
+    if (TrustedTypeFail(kTrustedScriptURLAssignmentAndNoDefaultPolicyExisted,
                         execution_context, exception_state, string)) {
       return g_empty_string;
     } else {
