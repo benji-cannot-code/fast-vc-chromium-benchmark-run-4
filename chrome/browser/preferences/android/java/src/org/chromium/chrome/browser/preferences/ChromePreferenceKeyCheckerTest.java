9cdc20e1fdc7303dd9ebf58a0baaf13dd54bf852
Henrique Nakashima <hnakashima@chromium.org>
2020-01-09 19:51:45 +0000

    Support grandfathered prefixes in ChromePreferenceKeys.
    Some SharedPreferences keys used in the past are dynamic. For example,
    in PaymentPreferencesUtil, keys of the format
    "payment_instrument_use_count_[type]" are generated.
    
    These keys should be usable by SharedPreferencesManager, which will
    only allow whitelisted keys to be used. This CL creates a whitelist of
    dynamic key prefixes grandfathered in because they do not follow the
    format "Chrome.[Feature].[KeyPrefix].*".
    
    Bug: 1022108
    Change-Id: Iaf9ebf170c339cc4b6d13705d515c97eef9087cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981262
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729864}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeyCheckerTest.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeyCheckerTest.java
@@ -31,6 +31,7 @@ public class ChromePreferenceKeyCheckerTest {
     private static final KeyPrefix KEY_PREFIX3_NOT_IN_USE =
             new KeyPrefix("Chrome.Feature.KeyPrefix3.*");
     private static final String GRANDFATHERED_KEY_IN_USE = "grandfatheredkey";
+    private static final String GRANDFATHERED_PREFIX_IN_USE = "grandfatheredprefix_";
 
     private ChromePreferenceKeyChecker mSubject;
 
@@ -39,7 +40,10 @@ public class ChromePreferenceKeyCheckerTest {
         List<String> keysInUse = Arrays.asList(KEY1_IN_USE, KEY2_IN_USE,
                 KEY_PREFIX1_IN_USE.pattern(), KEY_PREFIX2_IN_USE.pattern());
         List<String> grandfatheredKeys = Arrays.asList(GRANDFATHERED_KEY_IN_USE);
-        mSubject = new ChromePreferenceKeyChecker(keysInUse, grandfatheredKeys);
+        List<KeyPrefix> grandfatheredPrefixes =
+                Arrays.asList(new KeyPrefix(GRANDFATHERED_PREFIX_IN_USE + "*"));
+        mSubject =
+                new ChromePreferenceKeyChecker(keysInUse, grandfatheredKeys, grandfatheredPrefixes);
     }
 
     @Test
@@ -48,6 +52,7 @@ public class ChromePreferenceKeyCheckerTest {
         mSubject.checkIsKeyInUse(KEY1_IN_USE);
         mSubject.checkIsKeyInUse(KEY2_IN_USE);
         mSubject.checkIsKeyInUse(GRANDFATHERED_KEY_IN_USE);
+        mSubject.checkIsKeyInUse(GRANDFATHERED_PREFIX_IN_USE + "restofkey");
     }
 
     @Test(expected = RuntimeException.class)
