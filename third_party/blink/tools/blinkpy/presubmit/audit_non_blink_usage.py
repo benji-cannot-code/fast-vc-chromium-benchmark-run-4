5de9ba9c83eb407af92a4060c535307d344add53
Yuki Shiino <yukishiino@chromium.org>
2020-01-09 15:38:19 +0000

    v8bindings: Add 'bindings' namespace in the allowed list
    We reached to a consensus that it's okay to introduce 'bindings'
    nested namespace in 'blink' namespace.  This patch registers the
    namespace to the allowed list.
    
    Change-Id: I9b68479dc12e551be3b4d5a0148b6c308b91bb64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1993079
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729758}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -320,6 +320,7 @@ _CONFIG = [
             'url::.+',
 
             # Nested namespaces under the blink namespace
+            'bindings::.+',
             'canvas_heuristic_parameters::.+',
             'compositor_target_property::.+',
             'cors::.+',
