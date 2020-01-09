8b4db6cdfeadf02cba82fee2caed95c90eaa8747
Adrienne Walker <enne@chromium.org>
2020-01-09 00:17:24 +0000

    Remove unneeded IndexedDBContext include
    Bug: 1015214
    Change-Id: I69553d4434f7b8b5d647373869a94705b45a234c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992599
    Auto-Submit: enne <enne@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729558}

--- chrome/browser/ui/webui/cookies_tree_model_util.cc
+++ chrome/browser/ui/webui/cookies_tree_model_util.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browsing_data/cookies_tree_model.h"
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/cache_storage_context.h"
-#include "content/public/browser/indexed_db_context.h"
 #include "content/public/browser/service_worker_context.h"
 #include "content/public/browser/storage_usage_info.h"
 #include "extensions/buildflags/buildflags.h"
