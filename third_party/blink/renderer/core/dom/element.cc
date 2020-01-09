5a0565ab7dad4761aebeaf7cac8b753d7affa503
Vladimir Levin <vmpstr@chromium.org>
2020-01-09 19:26:12 +0000

    RenderSubtree: Remove IsLocked check from IsActivatable implementation.
    This patch makes IsActivatable return the 'correct' value which reflects
    the current state of either the attribute or the CSS tokens, independently
    of whether the context is locked.
    
    This is a first step to us re-locking the context on visibility changes.
    In other words, we need to know whether visibility activation is possible
    even if the context is unlocked
    
    R=chrishtr@chromium.org, rakina@chromium.org
    
    Change-Id: I5d2f5c520b7864fb726cdda01cc94aec5f8da7d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1993433
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729846}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -4366,7 +4366,7 @@ bool Element::ActivateDisplayLockIfNeeded(DisplayLockActivationReason reason) {
     if (auto* context = ancestor_element->GetDisplayLockContext()) {
       // If any of the ancestors is not activatable for the given reason, we
       // can't activate.
-      if (!context->IsActivatable(reason))
+      if (context->IsLocked() && !context->IsActivatable(reason))
         return false;
       activatable_targets.push_back(std::make_pair(
           ancestor_element, &ancestor.GetTreeScope().Retarget(*this)));
@@ -4407,7 +4407,7 @@ bool Element::DisplayLockPreventsActivation(
     if (!current_element)
       continue;
     if (auto* context = current_element->GetDisplayLockContext()) {
-      if (!context->IsActivatable(reason))
+      if (context->IsLocked() && !context->IsActivatable(reason))
         return true;
     }
   }
