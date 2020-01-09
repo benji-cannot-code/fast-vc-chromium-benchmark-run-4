2b64df2d35333640c1f4bb61c3f39eeb18568bb8
Mia Glaese <glamia@chromium.org>
2020-01-09 12:36:34 +0000

    [Start Surface] Change StartSurfaceToolbar with OverviewModeState
    Screenshots here:
    https://drive.google.com/drive/folders/17MCAlDrQidwqYw1sDP1gJAeQZB2FGxbl?usp=sharing
    
    Bug: 982018
    Change-Id: Id682d6544b123fe6bb7c9777c319aff8c85458b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989994
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Mia Glaese <glamia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729714}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/StartSurfaceToolbarCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/StartSurfaceToolbarCoordinator.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewStub;
 
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.toolbar.IncognitoStateProvider;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuButtonHelper;
@@ -102,6 +103,14 @@ class StartSurfaceToolbarCoordinator {
      */
     void onBottomToolbarVisibilityChanged(boolean isVisible) {
         mToolbarMediator.onBottomToolbarVisibilityChanged(isVisible);
+    }
+
+    /**
+     * @param overviewModeBehavior The {@link OverviewModeBehavior} to observe overview state
+     *         changes.
+     */
+    void setOverviewModeBehavior(OverviewModeBehavior overviewModeBehavior) {
+        mToolbarMediator.setOverviewModeBehavior(overviewModeBehavior);
     }
 
     void onNativeLibraryReady() {
