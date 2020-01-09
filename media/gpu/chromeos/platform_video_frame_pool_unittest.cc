a3c91a007bf56f5d8f01aa78fe61d6dbb8cd851e
Miguel Casas <mcasas@chromium.org>
2020-01-09 17:16:40 +0000

    PlatformVideoFramePoolTest: connect the parameterized test cases
    This CL teaches PlatformVideoFramePoolTest to use the parameterization
    defined inside (ToT did not use it). It also removes the surprising
    (unnecessary?) explicit unique_ptr deleter, using PlatformVideoFramePool
    directly, and also takes the chance to remove the private ctor in that
    class, since anyway the Test is a friend and can access the internal
    members (albeit we lose a const).
    
    Test: media_unittests on a target_os="chromeos" build
    Bug: 1040291
    Change-Id: I660e579b34a0ebea213f3c4a9b66b64a17043f3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992762
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729787}

--- media/gpu/chromeos/platform_video_frame_pool_unittest.cc
+++ media/gpu/chromeos/platform_video_frame_pool_unittest.cc
@@ -58,9 +58,9 @@ class PlatformVideoFramePoolTest
   using DmabufId = DmabufVideoFramePool::DmabufId;
 
   PlatformVideoFramePoolTest()
-      : task_environment_(base::test::TaskEnvironment::TimeSource::MOCK_TIME) {
-    pool_.reset(new PlatformVideoFramePool(
-        base::BindRepeating(&CreateDmabufVideoFrame)));
+      : task_environment_(base::test::TaskEnvironment::TimeSource::MOCK_TIME),
+        pool_(new PlatformVideoFramePool(nullptr)) {
+    pool_->create_frame_cb_ = base::BindRepeating(&CreateDmabufVideoFrame);
     pool_->set_parent_task_runner(base::ThreadTaskRunnerHandle::Get());
   }
 
@@ -95,9 +95,7 @@ class PlatformVideoFramePoolTest
 
  protected:
   base::test::TaskEnvironment task_environment_;
-  std::unique_ptr<PlatformVideoFramePool,
-                  std::default_delete<DmabufVideoFramePool>>
-      pool_;
+  std::unique_ptr<PlatformVideoFramePool> pool_;
 
   base::Optional<GpuBufferLayout> layout_;
   gfx::Rect visible_rect_;
@@ -107,11 +105,14 @@ class PlatformVideoFramePoolTest
 INSTANTIATE_TEST_SUITE_P(All,
                          PlatformVideoFramePoolTest,
                          testing::Values(PIXEL_FORMAT_I420,
+                                         PIXEL_FORMAT_YV12,
                                          PIXEL_FORMAT_NV12,
                                          PIXEL_FORMAT_ARGB));
 
-TEST_F(PlatformVideoFramePoolTest, SingleFrameReuse) {
-  RequestFrames(Fourcc(Fourcc::YV12));
+TEST_P(PlatformVideoFramePoolTest, SingleFrameReuse) {
+  const auto fourcc = Fourcc::FromVideoPixelFormat(GetParam());
+  ASSERT_TRUE(fourcc.has_value());
+  RequestFrames(fourcc.value());
   scoped_refptr<VideoFrame> frame = GetFrame(10);
   DmabufId id = DmabufVideoFramePool::GetDmabufId(*frame);
 
@@ -124,8 +125,10 @@ TEST_F(PlatformVideoFramePoolTest, SingleFrameReuse) {
   EXPECT_EQ(id, DmabufVideoFramePool::GetDmabufId(*new_frame));
 }
 
-TEST_F(PlatformVideoFramePoolTest, MultipleFrameReuse) {
-  RequestFrames(Fourcc(Fourcc::YV12));
+TEST_P(PlatformVideoFramePoolTest, MultipleFrameReuse) {
+  const auto fourcc = Fourcc::FromVideoPixelFormat(GetParam());
+  ASSERT_TRUE(fourcc.has_value());
+  RequestFrames(fourcc.value());
   scoped_refptr<VideoFrame> frame1 = GetFrame(10);
   scoped_refptr<VideoFrame> frame2 = GetFrame(20);
   DmabufId id1 = DmabufVideoFramePool::GetDmabufId(*frame1);
@@ -147,8 +150,10 @@ TEST_F(PlatformVideoFramePoolTest, MultipleFrameReuse) {
   CheckPoolSize(2u);
 }
 
-TEST_F(PlatformVideoFramePoolTest, FormatChange) {
-  RequestFrames(Fourcc(Fourcc::YV12));
+TEST_P(PlatformVideoFramePoolTest, RequestFramesWithDifferentFourcc) {
+  const auto fourcc = Fourcc::FromVideoPixelFormat(GetParam());
+  ASSERT_TRUE(fourcc.has_value());
+  RequestFrames(fourcc.value());
   scoped_refptr<VideoFrame> frame_a = GetFrame(10);
   scoped_refptr<VideoFrame> frame_b = GetFrame(10);
 
@@ -162,13 +167,17 @@ TEST_F(PlatformVideoFramePoolTest, FormatChange) {
 
   // Verify that requesting a frame with a different format causes the pool
   // to get drained.
-  RequestFrames(Fourcc(Fourcc::NV12));
+  const Fourcc different_fourcc(Fourcc::NV21);
+  ASSERT_NE(fourcc, different_fourcc);
+  RequestFrames(different_fourcc);
   scoped_refptr<VideoFrame> new_frame = GetFrame(10);
   CheckPoolSize(0u);
 }
 
-TEST_F(PlatformVideoFramePoolTest, UnwrapVideoFrame) {
-  RequestFrames(Fourcc(Fourcc::YV12));
+TEST_P(PlatformVideoFramePoolTest, UnwrapVideoFrame) {
+  const auto fourcc = Fourcc::FromVideoPixelFormat(GetParam());
+  ASSERT_TRUE(fourcc.has_value());
+  RequestFrames(fourcc.value());
   scoped_refptr<VideoFrame> frame_1 = GetFrame(10);
   scoped_refptr<VideoFrame> frame_2 = VideoFrame::WrapVideoFrame(
       frame_1, frame_1->format(), frame_1->visible_rect(),
@@ -181,8 +190,10 @@ TEST_F(PlatformVideoFramePoolTest, UnwrapVideoFrame) {
   EXPECT_FALSE(frame_1->IsSameDmaBufsAs(*frame_3));
 }
 
-TEST_F(PlatformVideoFramePoolTest, FormatNotChange) {
-  RequestFrames(Fourcc(Fourcc::YV12));
+TEST_P(PlatformVideoFramePoolTest, RequestFramesWithSameFourcc) {
+  const auto fourcc = Fourcc::FromVideoPixelFormat(GetParam());
+  ASSERT_TRUE(fourcc.has_value());
+  RequestFrames(fourcc.value());
   scoped_refptr<VideoFrame> frame1 = GetFrame(10);
   DmabufId id1 = DmabufVideoFramePool::GetDmabufId(*frame1);
 
@@ -191,7 +202,7 @@ TEST_F(PlatformVideoFramePoolTest, FormatNotChange) {
   task_environment_.RunUntilIdle();
 
   // Request frame with the same format. The pool should not request new frames.
-  RequestFrames(Fourcc(Fourcc::YV12));
+  RequestFrames(fourcc.value());
 
   scoped_refptr<VideoFrame> frame2 = GetFrame(20);
   DmabufId id2 = DmabufVideoFramePool::GetDmabufId(*frame2);
