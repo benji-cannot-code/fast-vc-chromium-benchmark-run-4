f63b08f5e5e51b60fd4c9aab578575d7171a8285
W. James MacLean <wjmaclean@chromium.org>
2020-01-09 13:19:15 +0000

    Add 'isolation' key to OriginPolicy.
    This CL adds an 'isolation' key to OriginPolicy, as per the proposal at
    https://github.com/domenic/origin-isolation#example.
    
    Bug: 900995
    Change-Id: I8b676070d5a38dfb394427c244cf10936072c229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1975992
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729719}

--- services/network/public/cpp/origin_policy.h
+++ services/network/public/cpp/origin_policy.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/optional.h"
+#include "services/network/public/cpp/isolation_opt_in_hints.h"
 #include "url/gurl.h"
 
 namespace network {
@@ -55,7 +56,8 @@ struct COMPONENT_EXPORT(NETWORK_CPP_BASE) OriginPolicyContents {
   OriginPolicyContents(
       const base::Optional<std::string>& feature_policy,
       const std::vector<std::string>& content_security_policies,
-      const std::vector<std::string>& content_security_policies_report_only);
+      const std::vector<std::string>& content_security_policies_report_only,
+      const base::Optional<IsolationOptInHints>& isolation_optin_hints);
 
   OriginPolicyContents(const OriginPolicyContents& other);
   OriginPolicyContents& operator=(const OriginPolicyContents& other);
@@ -84,6 +86,11 @@ struct COMPONENT_EXPORT(NETWORK_CPP_BASE) OriginPolicyContents {
   // a "report" disposition.
   // https://w3c.github.io/webappsec-csp/#policy-disposition
   std::vector<std::string> content_security_policies_report_only;
+
+  // This field, if present, indicates that the origin is opting in to
+  // origin-based isolation. The int contains zero or more flag bits indicating
+  // what the origin is hoping to achieve through isolation.
+  base::Optional<IsolationOptInHints> isolation_optin_hints;
 };
 
 // Native implementation of mojom::OriginPolicy. This is done so we can pass
