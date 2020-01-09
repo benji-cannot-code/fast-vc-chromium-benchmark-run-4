14ee7629865387e4443a88b4ee2ab2a1cd7badb7
Per Åhgren <peah@chromium.org>
2020-01-09 22:33:33 +0000

    Re-enable keytype suppression for APM in the renderer process
    The activation of the keytype suppression was by mistake removed in
    Chrome for all calls running the processing in the renderer process.
    The CL that caused this was
    https://chromium-review.googlesource.com/c/chromium/src/+/1495528
    
    This CL re-enables keytype suppression.
    
    Bug: chromium:1033455
    Change-Id: Ieaeafc53f2d4b0d8e87400544e859fd0e0e68886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1984305
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Per Åhgren <peah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729943}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -751,6 +751,7 @@ _CONFIG = [
             'webrtc::EchoCanceller3Config',
             'webrtc::EchoCanceller3Factory',
             'webrtc::ExperimentalAgc',
+            'webrtc::ExperimentalNs',
             'webrtc::MediaStreamTrackInterface',
             'webrtc::ObserverInterface',
             'webrtc::StreamConfig',
