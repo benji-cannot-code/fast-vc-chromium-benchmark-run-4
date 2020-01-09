2595334c811c4bec70e71a1d41971d965f2abdee
Gang Wu <gangwu@chromium.org>
2020-01-09 01:11:06 +0000

    Fix resizing image used wrong source image
    https://cs.chromium.org/chromium/src/skia/ext/image_operations.cc?g=0&l=408
    The first parameter should be the source of image, not the destination.
    
    Bug: 1039791
    Change-Id: Ib0fc4cc2b3a15143b4ceb3de7ca558e0d279e3ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1990206
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729578}

--- ui/gfx/image/image_util_unittest.cc
+++ ui/gfx/image/image_util_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkRect.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_unittest_util.h"
+#include "ui/gfx/image/resize_image_dimensions.h"
 
 TEST(ImageUtilTest, JPEGEncodeAndDecode) {
   gfx::Image original = gfx::test::CreateImage(100, 100);
@@ -133,4 +134,16 @@ TEST(ImageUtilTest, GetVisibleMargins) {
     EXPECT_EQ(9, left);
     EXPECT_EQ(8, right);
   }
+}
+
+TEST(ImageUtilTest, ResizedImageForSearchByImage) {
+  // Make sure the image large enough to let ResizedImageForSearchByImage to
+  // resize the image.
+  gfx::Image original_image =
+      gfx::test::CreateImage(gfx::kSearchByImageMaxImageHeight + 10,
+                             gfx::kSearchByImageMaxImageWidth + 10);
+
+  gfx::Image resized_image = gfx::ResizedImageForSearchByImage(original_image);
+  EXPECT_NE(original_image.Size(), resized_image.Size());
+  EXPECT_FALSE(resized_image.IsEmpty());
 }
