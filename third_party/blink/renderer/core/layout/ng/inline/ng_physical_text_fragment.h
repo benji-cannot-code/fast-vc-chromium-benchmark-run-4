e38024d2d2b6d06340b0de4ea041f7121e4064c6
Koji Ishii <kojii@chromium.org>
2020-01-09 10:17:43 +0000

    Use NGTextOffset in NGFragmentItem and NGPhysicalTextFragment
    This patch uses a new struct added in r715013
    <crrev.com/c/1911862>.
    
    This patch has no behavior changes.
    
    Bug: 982194
    Change-Id: Ie15c2c9f71de6037bba5acfbda01c7a1307a0bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1993069
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729686}

--- third_party/blink/renderer/core/layout/ng/inline/ng_physical_text_fragment.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_physical_text_fragment.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_INLINE_NG_PHYSICAL_TEXT_FRAGMENT_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_text_offset.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_ink_overflow.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_fragment.h"
 #include "third_party/blink/renderer/platform/fonts/ng_text_fragment_paint_info.h"
@@ -71,18 +72,19 @@ class CORE_EXPORT NGPhysicalTextFragment final : public NGPhysicalFragment {
 
   bool IsSymbolMarker() const { return TextType() == kSymbolMarker; }
 
-  unsigned TextLength() const { return end_offset_ - start_offset_; }
-  StringView Text() const {
-    return StringView(text_, start_offset_, TextLength());
-  }
   const String& TextContent() const { return text_; }
 
   // ShapeResult may be nullptr if |IsFlowControl()|.
   const ShapeResultView* TextShapeResult() const { return shape_result_.get(); }
 
   // Start/end offset to the text of the block container.
-  unsigned StartOffset() const { return start_offset_; }
-  unsigned EndOffset() const { return end_offset_; }
+  const NGTextOffset& TextOffset() const { return text_offset_; }
+  unsigned StartOffset() const { return text_offset_.start; }
+  unsigned EndOffset() const { return text_offset_.end; }
+  unsigned TextLength() const { return text_offset_.Length(); }
+  StringView Text() const {
+    return StringView(text_, text_offset_.start, TextLength());
+  }
 
   WritingMode GetWritingMode() const { return Style().GetWritingMode(); }
   bool IsHorizontal() const {
@@ -142,8 +144,7 @@ class CORE_EXPORT NGPhysicalTextFragment final : public NGPhysicalFragment {
   const String text_;
 
   // Start and end offset of the parent block text.
-  const unsigned start_offset_;
-  const unsigned end_offset_;
+  const NGTextOffset text_offset_;
   const scoped_refptr<const ShapeResultView> shape_result_;
 
   // Fragments are immutable but allow certain expensive data, specifically ink
--- third_party/blink/renderer/core/layout/ng/inline/ng_text_offset.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_text_offset.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_INLINE_NG_TEXT_OFFSET_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_INLINE_NG_TEXT_OFFSET_H_
 
+#include "base/logging.h"
 #include "third_party/blink/renderer/core/core_export.h"
 
 namespace blink {
@@ -14,10 +15,13 @@ namespace blink {
 struct CORE_EXPORT NGTextOffset {
   NGTextOffset() = default;
   NGTextOffset(unsigned start, unsigned end) : start(start), end(end) {
-    DCHECK_GE(end, start);
+    AssertValid();
   }
 
-  unsigned Length() const { return end - start; }
+  unsigned Length() const {
+    AssertValid();
+    return end - start;
+  }
 
   void AssertValid() const { DCHECK_GE(end, start); }
 
