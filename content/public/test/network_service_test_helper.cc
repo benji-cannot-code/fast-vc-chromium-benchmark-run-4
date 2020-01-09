4c0738d2832163ccfa1a14c403ba0ac26262165a
Matt Menke <mmenke@chromium.org>
2020-01-09 14:54:53 +0000

    Add NetworkIsolationKey to ResolveHost call from Pepper.
    In order to isolate network activity from different sites, we're
    splitting the DNS cache by NetworkIsolationKey. In the case of Pepper,
    this means it just needs to grab the NetworkIsolationKey from the
    RenderFrame associated with the ResolveHost call.
    
    Bug: 997049
    Change-Id: I880c629d42afbe6fa3426f30dca24c3ae6976815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1958498
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729740}

--- content/public/test/network_service_test_helper.cc
+++ content/public/test/network_service_test_helper.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/test/test_host_resolver.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
+#include "net/base/ip_address.h"
 #include "net/cert/mock_cert_verifier.h"
 #include "net/cert/test_root_certs.h"
 #include "net/dns/mock_host_resolver.h"
@@ -116,15 +117,21 @@ class NetworkServiceTestHelper::NetworkServiceTestImpl
         case network::mojom::ResolverType::kResolverTypeFailTimeout:
           host_resolver->AddSimulatedTimeoutFailure(rule->host_pattern);
           break;
-        case network::mojom::ResolverType::kResolverTypeIPLiteral:
-          host_resolver->AddIPLiteralRule(rule->host_pattern, rule->replacement,
-                                          std::string());
+        case network::mojom::ResolverType::kResolverTypeIPLiteral: {
+          net::IPAddress ip_address;
+          DCHECK(ip_address.AssignFromIPLiteral(rule->replacement));
+          host_resolver->AddRuleWithFlags(rule->host_pattern, rule->replacement,
+                                          rule->host_resolver_flags,
+                                          rule->canonical_name);
           break;
+        }
         case network::mojom::ResolverType::kResolverTypeDirectLookup:
           host_resolver->AllowDirectLookup(rule->host_pattern);
           break;
         default:
-          host_resolver->AddRule(rule->host_pattern, rule->replacement);
+          host_resolver->AddRuleWithFlags(rule->host_pattern, rule->replacement,
+                                          rule->host_resolver_flags,
+                                          rule->canonical_name);
           break;
       }
     }
