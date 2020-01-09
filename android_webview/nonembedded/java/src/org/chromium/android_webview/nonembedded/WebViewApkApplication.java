cb385033b06ce240eb15803327f44e6356049869
Robbie McElrath <rmcelrath@chromium.org>
2020-01-09 19:25:07 +0000

    [AW] Set LibraryProcessType before loading library in WebViewApkApplication
    Not setting the process type resulted in an assertion error when loading
    the library before starting the license activity.
    
    Test: Start license activity via adb in trichrome and system webview.
    Bug: 1039896
    Change-Id: I4a5400a8cd6d9644eacb92cca14d59197ad50ebb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1990141
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729844}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/WebViewApkApplication.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/nonembedded/WebViewApkApplication.java
@@ -15,6 +15,7 @@ import org.chromium.base.PathUtils;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.base.library_loader.LibraryLoader;
+import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.components.embedder_support.application.FontPreloadingWorkaround;
 import org.chromium.ui.base.ResourceBundle;
 
@@ -78,6 +79,7 @@ public class WebViewApkApplication extends Application {
             if (LibraryLoader.getInstance().isInitialized()) {
                 return true;
             }
+            LibraryLoader.getInstance().setLibraryProcessType(LibraryProcessType.PROCESS_WEBVIEW);
             LibraryLoader.getInstance().loadNow();
         } catch (Throwable unused) {
             // Happens for WebView Stub. Throws NoClassDefFoundError because of no
