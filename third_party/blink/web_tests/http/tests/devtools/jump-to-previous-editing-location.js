3e73f3c8223dc1ffba934368b771f707fc49cccc
Jack Lynch <jalyn@microsoft.com>
2020-01-09 20:30:48 +0000

    DevTools: Fix and reenable linkifier tests
    This CL fixes tests that will be broken by
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1891546
    
    Bug: 963183
    Change-Id: If58c767086ea176c430cf764fe9245312ebc6ab9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1721843
    Reviewed-by: Robert Paveza <Rob.Paveza@microsoft.com>
    Commit-Queue: Jack Lynch <jalyn@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#729878}

--- third_party/blink/web_tests/http/tests/devtools/jump-to-previous-editing-location.js
+++ third_party/blink/web_tests/http/tests/devtools/jump-to-previous-editing-location.js
@@ -207,7 +207,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       function onScriptSource(uiSourceCode) {
         var linkifier = new Components.Linkifier();
         var anchorURI = uiSourceCode.url();
-        var anchor = linkifier.linkifyScriptLocation(SDK.targetManager.mainTarget(), null, anchorURI, 10, 1);
+        var anchor = linkifier.linkifyScriptLocation(SDK.targetManager.mainTarget(), null, anchorURI, 10, {columnNumber: 1});
         var info = Components.Linkifier._linkInfo(anchor);
         Common.Revealer.reveal(info.uiLocation).then(function() {
           TestRunner.addResult('Selection: ' + panel.visibleView.textEditor.selection().toString());
