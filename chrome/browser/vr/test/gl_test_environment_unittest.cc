71d4e103343df1a41a9c26ba9dd9ed913049ae16
Alex Cooper <alcooper@chromium.org>
2020-01-09 01:12:06 +0000

    Re-Enable vr_pixeltests for Win > 7
    vr_pixeltests were failing on Windows 7, so they were disabled for all
    windows platforms, though the evidence is that they were fine on other
    platforms.  Given that VR is not supported on Win7, only disable these
    tests on that platform.
    
    Fixed: 1035767
    Change-Id: Ic97593f9b324c30111ec1465ae3da41de7aa2c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1985126
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729579}

--- chrome/browser/vr/test/gl_test_environment_unittest.cc
+++ chrome/browser/vr/test/gl_test_environment_unittest.cc
@@ -10,14 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace vr {
 
-// Failing on Win7 Tests (dbg)(1). crbug.com/1035767
-#if defined(OS_WIN)
-#define MAYBE_InitializeAndCleanup DISABLED_InitializeAndCleanup
-#else
-#define MAYBE_InitializeAndCleanup InitializeAndCleanup
-#endif
-
-TEST(GlTestEnvironmentTest, MAYBE_InitializeAndCleanup) {
+TEST(GlTestEnvironmentTest, InitializeAndCleanup) {
   GlTestEnvironment gl_test_environment(gfx::Size(100, 100));
   EXPECT_NE(gl_test_environment.GetFrameBufferForTesting(), 0u);
   EXPECT_EQ(glGetError(), (GLenum)GL_NO_ERROR);
