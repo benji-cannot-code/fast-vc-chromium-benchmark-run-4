103299c5943cdc5bedae6673f5f701a156f11142
Allen Bauer <kylixrd@chromium.org>
2020-01-09 21:41:46 +0000

    Reland "Removed BrowserView::ViewHierarchChanged, moved most code to constructor and widget-specfic code to AddedToWidget."
    This is a reland of f3616c26047a891b122dd12875cb97eacf715e3e
    
    Original change's description:
    > Removed BrowserView::ViewHierarchChanged, moved most code to constructor and widget-specfic code to AddedToWidget.
    > 
    > Bug: 245409
    > Change-Id: I1e606548df4361199862c561ad86f4d40cbaa7df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1979098
    > Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#728998}
    
    Bug: 245409
    Change-Id: Id5317849e130ec8715200f0947bc01f2bc932842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991945
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729914}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -499,6 +499,7 @@ class BrowserView : public BrowserWindow,
   void OnGestureEvent(ui::GestureEvent* event) override;
   void ViewHierarchyChanged(
       const views::ViewHierarchyChangedDetails& details) override;
+  void AddedToWidget() override;
   void PaintChildren(const views::PaintInfo& paint_info) override;
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
   void ChildPreferredSizeChanged(View* child) override;
@@ -579,9 +580,6 @@ class BrowserView : public BrowserWindow,
   // tab navigations and need to give users a visual clue as to what tabs are
   // affected.
   void RevealTabStripIfNeeded();
-
-  // Constructs and initializes the child views.
-  void InitViews();
 
   // Make sure the WebUI tab strip exists if it should.
   void MaybeInitializeWebUITabStrip();
