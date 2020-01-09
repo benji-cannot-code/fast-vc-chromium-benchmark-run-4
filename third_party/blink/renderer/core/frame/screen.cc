654ed3cbd81460e0e1ceb3c499a577bd923d4ae8
Mike Wasserman <msw@chromium.org>
2020-01-09 18:12:41 +0000

    Screen Enumeration: Fix avail[Width|Height] transposition
    Bug: None
    Test: Manual
    Change-Id: I08f2dd1b7925ba2ab5136470a029e6ac462d0698
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992623
    Auto-Submit: Michael Wasserman <msw@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#729808}

--- third_party/blink/renderer/core/frame/screen.cc
+++ third_party/blink/renderer/core/frame/screen.cc
@@ -135,7 +135,7 @@ int Screen::availTop() const {
 int Screen::availHeight() const {
   if (display_) {
     DCHECK(RuntimeEnabledFeatures::ScreenEnumerationEnabled());
-    return display_->work_area.width;
+    return display_->work_area.height;
   }
   LocalFrame* frame = GetFrame();
   if (!frame)
@@ -152,7 +152,7 @@ int Screen::availHeight() const {
 int Screen::availWidth() const {
   if (display_) {
     DCHECK(RuntimeEnabledFeatures::ScreenEnumerationEnabled());
-    return display_->work_area.height;
+    return display_->work_area.width;
   }
   LocalFrame* frame = GetFrame();
   if (!frame)
