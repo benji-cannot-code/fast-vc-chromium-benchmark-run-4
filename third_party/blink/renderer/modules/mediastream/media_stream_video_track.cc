408608c12e1bc0d4d46ce63936ec6b5fe0b05e76
Markus Handell <handellm@google.com>
2020-01-10 12:49:29 +0000

    MediaStreamVideoTrack: survive source getting deallocated.
    This change fixes a problem where MediaStreamVideoTrack would would CHECK-fail
    when the source has been cleared or deallocated AND a call to either SetEnabled,
    AddEncodedSink or RemoveEncodedSink occurs.
    
    The change also includes a new unit test for this case which was previously
    uncovered.
    
    Change-Id: Iab02df69277b2d2a287e2505977ce94345afb848
    Bug: 1013590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1993291
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Markus Handell <handellm@google.com>
    Cr-Commit-Position: refs/heads/master@{#730138}

--- third_party/blink/renderer/modules/mediastream/media_stream_video_track.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream_video_track.cc
@@ -495,7 +495,8 @@ void MediaStreamVideoTrack::AddEncodedSink(WebMediaStreamSink* sink,
   DCHECK_CALLED_ON_VALID_THREAD(main_render_thread_checker_);
   AddSinkInternal(&encoded_sinks_, sink);
   frame_deliverer_->AddEncodedCallback(sink, std::move(callback));
-  source_->UpdateNumEncodedSinks();
+  if (source_)
+    source_->UpdateNumEncodedSinks();
   UpdateSourceHasConsumers();
 }
 
@@ -515,7 +516,8 @@ void MediaStreamVideoTrack::RemoveEncodedSink(WebMediaStreamSink* sink) {
   DCHECK_CALLED_ON_VALID_THREAD(main_render_thread_checker_);
   RemoveSinkInternal(&encoded_sinks_, sink);
   frame_deliverer_->RemoveEncodedCallback(sink);
-  source_->UpdateNumEncodedSinks();
+  if (source_)
+    source_->UpdateNumEncodedSinks();
   UpdateSourceHasConsumers();
 }
 
@@ -533,7 +535,8 @@ void MediaStreamVideoTrack::SetEnabled(bool enabled) {
   // need a new keyframe from the source as we may have dropped data making the
   // stream undecodable.
   bool maybe_await_key_frame = false;
-  if (enabled && source_->SupportsEncodedOutput() && !encoded_sinks_.empty()) {
+  if (enabled && source_ && source_->SupportsEncodedOutput() &&
+      !encoded_sinks_.empty()) {
     source_->RequestRefreshFrame();
     maybe_await_key_frame = true;
   }
