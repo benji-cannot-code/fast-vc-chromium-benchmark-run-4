02120861ec8425dd81103cacbbe8c54e704e2130
Olya Kalitova <okalitova@chromium.org>
2020-01-10 12:46:17 +0000

    Log failure details in case container configuration failed
    Adds failure details in case default Crostini configuration using
    Ansible has failed.
    
    TEST: deployed on DUT and checked that logs are present
    Bug: 1000287
    Change-Id: I4215bb117ed82fe8829ea5375767000645326915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991561
    Reviewed-by: Nic Hollingum <hollingum@google.com>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730136}

--- chrome/browser/chromeos/crostini/crostini_manager.cc
+++ chrome/browser/chromeos/crostini/crostini_manager.cc
@@ -2323,7 +2323,8 @@ void CrostiniManager::OnApplyAnsiblePlaybookProgress(
 
   // TODO(okalitova): Add an observer.
   AnsibleManagementService::GetForProfile(profile_)
-      ->OnApplyAnsiblePlaybookProgress(signal.status());
+      ->OnApplyAnsiblePlaybookProgress(signal.status(),
+                                       signal.failure_details());
 }
 
 void CrostiniManager::OnUpgradeContainerProgress(
