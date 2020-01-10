cc42089acd309d377e50f5d09eb47b0839b90a2c
Olivier Li <olivierli@google.com>
2020-01-10 01:24:26 +0000

    Set browser state builder dir to avoid file leaks in ios tests in /ios/chrome/browser/history
    This cl is generated with 'git cl split'
    
    This CL was uploaded by git cl split.
    
    R=sczs@chromium.org
    
    Bug: 546640
    Change-Id: I52ab81484aa799323192e0535f91dfb52a453082
    Fixed: 1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992546
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Auto-Submit: Oliver Li <olivierli@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730016}

--- ios/chrome/browser/history/history_tab_helper_unittest.mm
+++ ios/chrome/browser/history/history_tab_helper_unittest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind_test_util.h"
@@ -31,6 +32,10 @@ class HistoryTabHelperTest : public PlatformTest {
  public:
   void SetUp() override {
     TestChromeBrowserState::Builder test_cbs_builder;
+
+    ASSERT_TRUE(state_dir_.CreateUniqueTempDir());
+    test_cbs_builder.SetPath(state_dir_.GetPath());
+
     chrome_browser_state_ = test_cbs_builder.Build();
     ASSERT_TRUE(chrome_browser_state_->CreateHistoryService(true));
 
@@ -67,6 +72,11 @@ class HistoryTabHelperTest : public PlatformTest {
   }
 
  protected:
+  // A state directory that outlives |task_environment_| is needed because
+  // CreateHistoryService/CreateBookmarkModel use the directory to host
+  // databases. See https://crbug.com/546640 for more details.
+  base::ScopedTempDir state_dir_;
+
   base::test::TaskEnvironment task_environment_;
   std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
   web::TestWebState web_state_;
