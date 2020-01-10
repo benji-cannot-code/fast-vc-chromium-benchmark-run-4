e5d458fba1e824e81c2308f7285616c4172f9ab9
Julie Jeongeun Kim <jkim@igalia.com>
2020-01-10 01:04:37 +0000

    Use [RaisesException] for immediate promise rejections in streams
    This is a part of effort for using [RaisesException] when synchronously
    rejecting a promise.
    
    It uses [RaisesException] for
    //third_party/blink/renderer/core/streams/.
    
    Bug: 1001114
    Change-Id: I58a9e8d6a65c6c7f33bc61ec39c121202d141cd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989321
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#730010}

--- third_party/blink/renderer/core/streams/transform_stream_test.cc
+++ third_party/blink/renderer/core/streams/transform_stream_test.cc
@@ -266,7 +266,8 @@ TEST_F(TransformStreamTest, EnqueueFromTransform) {
 
   ReadableStream* readable = Stream()->Readable();
   auto* reader = readable->getReader(script_state, ASSERT_NO_EXCEPTION);
-  ScriptPromiseTester tester(script_state, reader->read(script_state));
+  ScriptPromiseTester tester(script_state,
+                             reader->read(script_state, ASSERT_NO_EXCEPTION));
   tester.WaitUntilSettled();
   EXPECT_TRUE(tester.IsFulfilled());
   EXPECT_TRUE(IsIteratorForStringMatching(script_state, tester.Value(), "a"));
@@ -299,7 +300,8 @@ TEST_F(TransformStreamTest, EnqueueFromFlush) {
 
   ReadableStream* readable = Stream()->Readable();
   auto* reader = readable->getReader(script_state, ASSERT_NO_EXCEPTION);
-  ScriptPromiseTester tester(script_state, reader->read(script_state));
+  ScriptPromiseTester tester(script_state,
+                             reader->read(script_state, ASSERT_NO_EXCEPTION));
   tester.WaitUntilSettled();
   EXPECT_TRUE(tester.IsFulfilled());
   EXPECT_TRUE(IsIteratorForStringMatching(script_state, tester.Value(), "a"));
@@ -334,7 +336,8 @@ TEST_F(TransformStreamTest, ThrowFromTransform) {
 
   ReadableStream* readable = Stream()->Readable();
   auto* reader = readable->getReader(script_state, ASSERT_NO_EXCEPTION);
-  ScriptPromiseTester read_tester(script_state, reader->read(script_state));
+  ScriptPromiseTester read_tester(
+      script_state, reader->read(script_state, ASSERT_NO_EXCEPTION));
   read_tester.WaitUntilSettled();
   EXPECT_TRUE(read_tester.IsRejected());
   EXPECT_TRUE(IsTypeError(script_state, read_tester.Value(), kMessage));
@@ -371,7 +374,8 @@ TEST_F(TransformStreamTest, ThrowFromFlush) {
 
   ReadableStream* readable = Stream()->Readable();
   auto* reader = readable->getReader(script_state, ASSERT_NO_EXCEPTION);
-  ScriptPromiseTester read_tester(script_state, reader->read(script_state));
+  ScriptPromiseTester read_tester(
+      script_state, reader->read(script_state, ASSERT_NO_EXCEPTION));
   read_tester.WaitUntilSettled();
   EXPECT_TRUE(read_tester.IsRejected());
   EXPECT_TRUE(IsTypeError(script_state, read_tester.Value(), kMessage));
@@ -442,7 +446,7 @@ TEST_F(TransformStreamTest, WaitInTransform) {
   Stream()
       ->Readable()
       ->getReader(script_state, ASSERT_NO_EXCEPTION)
-      ->read(script_state);
+      ->read(script_state, ASSERT_NO_EXCEPTION);
 
   ScriptPromiseTester write_tester(script_state,
                                    ScriptPromise::Cast(script_state, promise));
@@ -500,7 +504,7 @@ TEST_F(TransformStreamTest, WaitInFlush) {
   Stream()
       ->Readable()
       ->getReader(script_state, ASSERT_NO_EXCEPTION)
-      ->read(script_state);
+      ->read(script_state, ASSERT_NO_EXCEPTION);
 
   ScriptPromiseTester close_tester(script_state,
                                    ScriptPromise::Cast(script_state, promise));
