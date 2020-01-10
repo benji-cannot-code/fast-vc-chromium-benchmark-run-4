7dec4df3a24df1bc90aa506357d13060f51ba384
Mikel Astiz <mastiz@chromium.org>
2020-01-10 07:22:28 +0000

    Extend sync's Javascript API with key versions
    For debugging and future feature development, the web should be allowed
    to pass a key version in addition to key material. If multiple keys are
    involved, only the version of the newest is relevant, so the API is kept
    as simple as possible by plumbing a single integer.
    
    Bug: 1032485
    Change-Id: I0e53c54176e92c1dfa6ffcb496611ae53c8c113f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991432
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730078}

--- components/sync/driver/sync_service_crypto_unittest.cc
+++ components/sync/driver/sync_service_crypto_unittest.cc
@@ -102,7 +102,8 @@ class TestTrustedVaultClient : public TrustedVaultClient {
   }
 
   void StoreKeys(const std::string& gaia_id,
-                 const std::vector<std::vector<uint8_t>>& keys) override {
+                 const std::vector<std::vector<uint8_t>>& keys,
+                 int last_key_version) override {
     gaia_id_to_keys_[gaia_id] = keys;
     observer_list_.Notify();
   }
@@ -195,7 +196,8 @@ TEST_F(SyncServiceCryptoTest,
   // engine (i.e. before SetSyncEngine()).
   crypto_.OnTrustedVaultKeyRequired();
 
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kFetchedKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kFetchedKeys,
+                                  /*last_key_version=*/0);
 
   // Trusted vault keys should be fetched only after the engine initialization
   // is completed.
@@ -234,7 +236,8 @@ TEST_F(SyncServiceCryptoTest,
   EXPECT_CALL(reconfigure_cb_, Run(_)).Times(0);
   ASSERT_FALSE(crypto_.IsTrustedVaultKeyRequired());
 
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kFetchedKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kFetchedKeys,
+                                  /*last_key_version=*/0);
 
   // Mimic the engine determining that trusted vault keys are required.
   crypto_.SetSyncEngine(kSyncingAccount, &engine_);
@@ -299,7 +302,8 @@ TEST_F(SyncServiceCryptoTest, ShouldReadInvalidTrustedVaultKeysFromClient) {
 
   ASSERT_FALSE(crypto_.IsTrustedVaultKeyRequired());
 
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kFetchedKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kFetchedKeys,
+                                  /*last_key_version=*/0);
 
   // Mimic the engine determining that trusted vault keys are required.
   crypto_.SetSyncEngine(kSyncingAccount, &engine_);
@@ -340,7 +344,8 @@ TEST_F(SyncServiceCryptoTest, ShouldRefetchTrustedVaultKeysWhenChangeObserved) {
   const std::vector<std::vector<uint8_t>> kNewKeys = {{0, 1, 2, 3, 4},
                                                       {2, 3, 4, 5}};
 
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kInitialKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kInitialKeys,
+                                  /*last_key_version=*/0);
 
   // The engine replies with OnTrustedVaultKeyAccepted() only if |kNewKeys| are
   // provided.
@@ -365,7 +370,8 @@ TEST_F(SyncServiceCryptoTest, ShouldRefetchTrustedVaultKeysWhenChangeObserved) {
 
   // Mimic keys being added to the vault, which triggers a notification to
   // observers (namely |crypto_|), leading to a second fetch.
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kNewKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kNewKeys,
+                                  /*last_key_version=*/1);
   EXPECT_THAT(trusted_vault_client_.fetch_count(), Eq(2));
   EXPECT_CALL(reconfigure_cb_, Run(CONFIGURE_REASON_CRYPTO));
   EXPECT_TRUE(trusted_vault_client_.CompleteFetchKeysRequest());
@@ -383,7 +389,8 @@ TEST_F(SyncServiceCryptoTest,
   const std::vector<std::vector<uint8_t>> kNewKeys = {{0, 1, 2, 3, 4},
                                                       {2, 3, 4, 5}};
 
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kInitialKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kInitialKeys,
+                                  /*last_key_version=*/0);
 
   // The engine replies with OnTrustedVaultKeyAccepted() only if |kNewKeys| are
   // provided.
@@ -405,7 +412,8 @@ TEST_F(SyncServiceCryptoTest,
 
   // While there is an ongoing fetch, mimic keys being added to the vault, which
   // triggers a notification to observers (namely |crypto_|).
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kNewKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kNewKeys,
+                                  /*last_key_version=*/1);
 
   // Because there's already an ongoing fetch, a second one should not have been
   // triggered yet and should be deferred instead.
@@ -438,7 +446,8 @@ TEST_F(
   const std::vector<std::vector<uint8_t>> kLatestKeys = {
       {0, 1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4}};
 
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kInitialKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kInitialKeys,
+                                  /*last_key_version=*/0);
 
   // The engine replies with OnTrustedVaultKeyAccepted() only if |kLatestKeys|
   // are provided.
@@ -462,12 +471,14 @@ TEST_F(
 
   // Mimic keys being added to the vault, which triggers a notification to
   // observers (namely |crypto_|), leading to a second fetch.
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kIntermediateKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kIntermediateKeys,
+                                  /*last_key_version=*/1);
   EXPECT_THAT(trusted_vault_client_.fetch_count(), Eq(2));
 
   // While the second fetch is ongoing, mimic more keys being added to the
   // vault, which triggers a notification to observers (namely |crypto_|).
-  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kLatestKeys);
+  trusted_vault_client_.StoreKeys(kSyncingAccount.gaia, kLatestKeys,
+                                  /*last_key_version=*/2);
 
   // Because there's already an ongoing fetch, a third one should not have been
   // triggered yet and should be deferred instead.
