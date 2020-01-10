6c36e5dd3366f60daf689b9ce359bab0ae69c7a7
Timothy Loh <timloh@chromium.org>
2020-01-10 07:06:13 +0000

    Minor clean-ups to Plugin VM installer
    - Remove the START_DOWNLOADING state in the UI code.
    - Rename START_DOWNLOADING_DLC to STARTING and fix the initial state to
    be that. While we logically start installation immediately upon opening
    the dialog, we actually wait for AddedToWidget() to be called first, so
    retaining this state allows us to avoid confusion of being in an active
    state without having started the installation.
    - Remove the OnDlcDownloadStarted and OnDownloadStarted callbacks, the
    installer UI doesn't need these.
    - Add TODO suggesting further improvements regarding cancellation.
    
    Change-Id: I83575155047665139f0c1aa2af6b958b52df0b93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994846
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Julian Watson <juwa@google.com>
    Cr-Commit-Position: refs/heads/master@{#730075}

--- chrome/browser/chromeos/plugin_vm/plugin_vm_installer_unittest.cc
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_installer_unittest.cc
@@ -71,14 +71,12 @@ const int kDownloadedPluginVmImageSizeInMb = 123456789u / (1024 * 1024);
 
 class MockObserver : public PluginVmInstaller::Observer {
  public:
-  MOCK_METHOD0(OnDlcDownloadStarted, void());
   MOCK_METHOD2(OnDlcDownloadProgressUpdated,
                void(double progress, base::TimeDelta elapsed_time));
   MOCK_METHOD0(OnDlcDownloadCompleted, void());
   MOCK_METHOD0(OnDlcDownloadCancelled, void());
   MOCK_METHOD1(OnDlcDownloadFailed,
                void(plugin_vm::PluginVmInstaller::FailureReason));
-  MOCK_METHOD0(OnDownloadStarted, void());
   MOCK_METHOD3(OnDownloadProgressUpdated,
                void(uint64_t bytes_downloaded,
                     int64_t content_length,
@@ -378,7 +376,6 @@ class PluginVmInstallerDriveTest : public PluginVmInstallerTestBase {
 TEST_F(PluginVmInstallerDownloadServiceTest, DownloadPluginVmImageParamsTest) {
   SetupConciergeForSuccessfulDiskImageImport(fake_concierge_client_);
 
-  EXPECT_CALL(*observer_, OnDlcDownloadStarted());
   EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   EXPECT_CALL(*observer_, OnDownloadCompleted());
   EXPECT_CALL(*observer_, OnImportProgressUpdated(50.0, _));
@@ -401,7 +398,6 @@ TEST_F(PluginVmInstallerDownloadServiceTest, DownloadPluginVmImageParamsTest) {
 TEST_F(PluginVmInstallerDownloadServiceTest, OnlyOneImageIsProcessedTest) {
   SetupConciergeForSuccessfulDiskImageImport(fake_concierge_client_);
 
-  EXPECT_CALL(*observer_, OnDlcDownloadStarted());
   EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   EXPECT_CALL(*observer_, OnDownloadCompleted());
   EXPECT_CALL(*observer_, OnImportProgressUpdated(50.0, _));
@@ -450,7 +446,6 @@ TEST_F(PluginVmInstallerDownloadServiceTest,
   EXPECT_CALL(*observer_,
               OnDownloadFailed(
                   PluginVmInstaller::FailureReason::DOWNLOAD_FAILED_ABORTED));
-  EXPECT_CALL(*observer_, OnDlcDownloadStarted()).Times(2);
   EXPECT_CALL(*observer_, OnDlcDownloadCompleted()).Times(2);
   EXPECT_CALL(*observer_, OnDownloadCompleted());
   EXPECT_CALL(*observer_, OnImportProgressUpdated(50.0, _));
@@ -518,7 +513,6 @@ TEST_F(PluginVmInstallerDownloadServiceTest, EmptyPluginVmImageUrlTest) {
   EXPECT_CALL(
       *observer_,
       OnDownloadFailed(PluginVmInstaller::FailureReason::INVALID_IMAGE_URL));
-  EXPECT_CALL(*observer_, OnDlcDownloadStarted());
   EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   StartAndRunToCompletion();
 
@@ -545,7 +539,7 @@ TEST_F(PluginVmInstallerDownloadServiceTest, CannotStartIfPluginVmIsDisabled) {
 TEST_F(PluginVmInstallerDriveTest, InvalidDriveUrlTest) {
   SetPluginVmImagePref(kDriveUrl2, kHash);
 
-  EXPECT_CALL(*observer_, OnDownloadStarted());
+  EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   EXPECT_CALL(
       *observer_,
       OnDownloadFailed(PluginVmInstaller::FailureReason::INVALID_IMAGE_URL));
@@ -556,7 +550,7 @@ TEST_F(PluginVmInstallerDriveTest, NoConnectionDriveTest) {
   SetPluginVmImagePref(kDriveUrl, kHash);
   fake_drive_service_->set_offline(true);
 
-  EXPECT_CALL(*observer_, OnDownloadStarted());
+  EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   EXPECT_CALL(*observer_,
               OnDownloadFailed(
                   PluginVmInstaller::FailureReason::DOWNLOAD_FAILED_NETWORK));
@@ -566,7 +560,7 @@ TEST_F(PluginVmInstallerDriveTest, NoConnectionDriveTest) {
 TEST_F(PluginVmInstallerDriveTest, WrongHashDriveTest) {
   SetPluginVmImagePref(kDriveUrl, kHash2);
 
-  EXPECT_CALL(*observer_, OnDownloadStarted());
+  EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   EXPECT_CALL(*observer_, OnDownloadFailed(
                               PluginVmInstaller::FailureReason::HASH_MISMATCH));
 
@@ -577,7 +571,7 @@ TEST_F(PluginVmInstallerDriveTest, DriveDownloadFailedAfterStartingTest) {
   SetPluginVmImagePref(kDriveUrl, kHash);
   SimpleFakeDriveService* fake_drive_service = SetUpSimpleFakeDriveService();
 
-  EXPECT_CALL(*observer_, OnDownloadStarted());
+  EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   EXPECT_CALL(*observer_, OnDownloadProgressUpdated(5, 100, _));
   EXPECT_CALL(*observer_, OnDownloadProgressUpdated(10, 100, _));
   EXPECT_CALL(*observer_,
@@ -601,7 +595,7 @@ TEST_F(PluginVmInstallerDriveTest, CancelledDriveDownloadTest) {
   SetPluginVmImagePref(kDriveUrl, kHash);
   SimpleFakeDriveService* fake_drive_service = SetUpSimpleFakeDriveService();
 
-  EXPECT_CALL(*observer_, OnDownloadStarted());
+  EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   EXPECT_CALL(*observer_, OnDownloadProgressUpdated(5, 100, _));
   EXPECT_CALL(*observer_, OnDownloadCompleted()).Times(0);
 
@@ -618,7 +612,7 @@ TEST_F(PluginVmInstallerDriveTest, CancelledDriveDownloadTest) {
 TEST_F(PluginVmInstallerDriveTest, SuccessfulDriveDownloadTest) {
   SetPluginVmImagePref(kDriveUrl, kHash);
 
-  EXPECT_CALL(*observer_, OnDownloadStarted());
+  EXPECT_CALL(*observer_, OnDlcDownloadCompleted());
   EXPECT_CALL(*observer_, OnDownloadCompleted());
   EXPECT_CALL(*observer_,
               OnDownloadProgressUpdated(_, std::strlen(kContent), _))
--- chrome/browser/ui/views/plugin_vm/plugin_vm_installer_view.cc
+++ chrome/browser/ui/views/plugin_vm/plugin_vm_installer_view.cc
@@ -194,13 +194,12 @@ bool PluginVmInstallerView::Accept() {
 
 bool PluginVmInstallerView::Cancel() {
   switch (state_) {
+    case State::STARTING:
     case State::DOWNLOADING_DLC:
-    case State::START_DLC_DOWNLOADING:
       plugin_vm::RecordPluginVmSetupResultHistogram(
           plugin_vm::PluginVmSetupResult::kUserCancelledDownloadingPluginVmDlc);
       break;
     case State::DOWNLOADING:
-    case State::START_DOWNLOADING:
       plugin_vm::RecordPluginVmSetupResultHistogram(
           plugin_vm::PluginVmSetupResult::
               kUserCancelledDownloadingPluginVmImage);
@@ -224,13 +223,6 @@ gfx::Size PluginVmInstallerView::CalculatePreferredSize() const {
   return gfx::Size(kWindowWidth, kWindowHeight);
 }
 
-void PluginVmInstallerView::OnDlcDownloadStarted() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-
-  state_ = State::DOWNLOADING_DLC;
-  OnStateUpdated();
-}
-
 void PluginVmInstallerView::OnDlcDownloadProgressUpdated(
     double progress,
     base::TimeDelta elapsed_time) {
@@ -244,20 +236,17 @@ void PluginVmInstallerView::OnDlcDownloadCompleted() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   DCHECK_EQ(state_, State::DOWNLOADING_DLC);
 
-  state_ = State::START_DOWNLOADING;
-  OnStateUpdated();
-}
-
-void PluginVmInstallerView::OnDlcDownloadCancelled() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-}
-
-void PluginVmInstallerView::OnDownloadStarted() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  DCHECK_EQ(state_, State::START_DOWNLOADING);
-
   state_ = State::DOWNLOADING;
   OnStateUpdated();
+}
+
+// TODO(timloh): Cancelling the installation immediately closes the dialog, but
+// getting back to a clean state could take several seconds. If a user then
+// re-opens the dialog, it could cause it to fail unexpectedly. We should make
+// use of these callback to avoid this (and possibly merge them into a single
+// callback).
+void PluginVmInstallerView::OnDlcDownloadCancelled() {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 }
 
 void PluginVmInstallerView::OnDownloadProgressUpdated(
@@ -338,9 +327,8 @@ void PluginVmInstallerView::OnImported() {
 
 base::string16 PluginVmInstallerView::GetBigMessage() const {
   switch (state_) {
-    case State::START_DLC_DOWNLOADING:
+    case State::STARTING:
     case State::DOWNLOADING_DLC:
-    case State::START_DOWNLOADING:
     case State::DOWNLOADING:
     case State::IMPORTING:
       return l10n_util::GetStringUTF16(
@@ -361,9 +349,8 @@ base::string16 PluginVmInstallerView::GetBigMessage() const {
 
 base::string16 PluginVmInstallerView::GetMessage() const {
   switch (state_) {
-    case State::START_DLC_DOWNLOADING:
+    case State::STARTING:
     case State::DOWNLOADING_DLC:
-    case State::START_DOWNLOADING:
       return l10n_util::GetStringUTF16(
           IDS_PLUGIN_VM_INSTALLER_START_DOWNLOADING_MESSAGE);
     case State::DOWNLOADING:
@@ -429,9 +416,8 @@ PluginVmInstallerView::~PluginVmInstallerView() {
 
 int PluginVmInstallerView::GetCurrentDialogButtons() const {
   switch (state_) {
-    case State::START_DLC_DOWNLOADING:
+    case State::STARTING:
     case State::DOWNLOADING_DLC:
-    case State::START_DOWNLOADING:
     case State::DOWNLOADING:
     case State::IMPORTING:
       return ui::DIALOG_BUTTON_CANCEL;
@@ -451,9 +437,8 @@ int PluginVmInstallerView::GetCurrentDialogButtons() const {
 base::string16 PluginVmInstallerView::GetCurrentDialogButtonLabel(
     ui::DialogButton button) const {
   switch (state_) {
-    case State::START_DLC_DOWNLOADING:
+    case State::STARTING:
     case State::DOWNLOADING_DLC:
-    case State::START_DOWNLOADING:
     case State::DOWNLOADING:
     case State::IMPORTING: {
       DCHECK_EQ(button, ui::DIALOG_BUTTON_CANCEL);
@@ -490,7 +475,7 @@ void PluginVmInstallerView::AddedToWidget() {
         plugin_vm::PluginVmSetupResult::kPluginVmIsNotAllowed);
   }
 
-  if (state_ == State::START_DOWNLOADING)
+  if (state_ == State::STARTING)
     StartInstallation();
   else
     OnStateUpdated();
@@ -515,8 +500,7 @@ void PluginVmInstallerView::OnStateUpdated() {
   }
 
   const bool progress_bar_visible =
-      state_ == State::START_DLC_DOWNLOADING ||
-      state_ == State::DOWNLOADING_DLC || state_ == State::START_DOWNLOADING ||
+      state_ == State::STARTING || state_ == State::DOWNLOADING_DLC ||
       state_ == State::DOWNLOADING || state_ == State::IMPORTING;
   progress_bar_->SetVisible(progress_bar_visible);
   // Values outside the range [0,1] display an infinite loading animation.
@@ -625,7 +609,7 @@ void PluginVmInstallerView::StartInstallation() {
   // retry button is clicked).
   setup_start_tick_ = base::TimeTicks::Now();
 
-  state_ = State::START_DLC_DOWNLOADING;
+  state_ = State::DOWNLOADING_DLC;
   OnStateUpdated();
 
   plugin_vm_installer_->SetObserver(this);
