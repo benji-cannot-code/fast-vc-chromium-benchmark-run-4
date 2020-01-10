5bcba7633b8deb556427bd830d3b615a78d7b992
Yuta Kitamura <yutak@chromium.org>
2020-01-10 06:21:24 +0000

    Revert "Report save-data savings to LiteMode site-breakdown"
    This reverts commit 733e2fb17b92b0634168deaffb046c56f051f243.
    
    Reason for revert: This CL caused a CHECK failure in the following
    browser_tests:
    
    LazyLoadDisabledBrowserTest.LazyLoadFrame_DisabledInBackgroundTab
    LazyLoadDisabledBrowserTest.LazyLoadFrame_DisabledInIncognito
    
    Sample run: https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests%20%28dbg%29%281%29/85969
    
    CHECK failure log:
    > [67654:67654:0110/151505.705450:FATAL:embedded_test_server.cc(389)]
    > Check failed: !io_thread_.get(). Handlers must be registered before
    > starting the server.
    
    Original change's description:
    > Report save-data savings to LiteMode site-breakdown
    > 
    > This CL gets the estimated data savings due to SaveData and updates as
    > LiteMode savings. The savings are retrieved from field trial as json.
    > 
    > Bug: 923551
    > Change-Id: Ia7c5518edfeaedce33e957c19224f6a2350df6c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1990989
    > Commit-Queue: rajendrant <rajendrant@chromium.org>
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#729972}
    
    TBR=tbansal@chromium.org,rajendrant@chromium.org,ryansturm@chromium.org
    
    Change-Id: I79ebf0d0a25d218ddbe644d708ba65d57061c92a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 923551
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994850
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730072}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_service.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/files/file_path.h"
-#include "base/json/json_reader.h"
 #include "base/location.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_macros.h"
@@ -41,22 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace data_reduction_proxy {
 
-namespace {
-
-base::Optional<base::Value> GetSaveDataSavingsPercentEstimateFromFieldTrial() {
-  if (!base::FeatureList::IsEnabled(features::kReportSaveDataSavings))
-    return base::nullopt;
-  const auto origin_savings_estimate_json =
-      base::GetFieldTrialParamValueByFeature(features::kReportSaveDataSavings,
-                                             "origin_savings_estimate");
-  if (origin_savings_estimate_json.empty())
-    return base::nullopt;
-
-  return base::JSONReader::Read(origin_savings_estimate_json);
-}
-
-}  // namespace
-
 DataReductionProxyService::DataReductionProxyService(
     DataReductionProxySettings* settings,
     PrefService* prefs,
@@ -80,9 +63,7 @@ DataReductionProxyService::DataReductionProxyService(
       data_use_measurement_(data_use_measurement),
       effective_connection_type_(net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN),
       client_(client),
-      channel_(channel),
-      save_data_savings_estimate_dict_(
-          GetSaveDataSavingsPercentEstimateFromFieldTrial()) {
+      channel_(channel) {
   DCHECK(settings);
   DCHECK(network_quality_tracker_);
   DCHECK(network_connection_tracker_);
@@ -425,6 +406,8 @@ void DataReductionProxyService::MarkProxiesAsBad(
   // process (renderer).
 
   if (bypass_duration < base::TimeDelta()) {
+    LOG(ERROR) << "Received bad MarkProxiesAsBad() -- invalid bypass_duration: "
+               << bypass_duration;
     std::move(callback).Run();
     return;
   }
@@ -438,6 +421,8 @@ void DataReductionProxyService::MarkProxiesAsBad(
   // received (FindConfiguredDataReductionProxy() searches recent proxies too).
   for (const auto& proxy : bad_proxies.GetAll()) {
     if (!config_->FindConfiguredDataReductionProxy(proxy)) {
+      LOG(ERROR) << "Received bad MarkProxiesAsBad() -- not a DRP server: "
+                 << proxy.ToURI();
       std::move(callback).Run();
       return;
     }
@@ -564,19 +549,6 @@ void DataReductionProxyService::SetDependenciesForTesting(
   config_client_ = std::move(config_client);
   if (config_client_)
     config_client_->Initialize(url_loader_factory_);
-}
-
-double DataReductionProxyService::GetSaveDataSavingsPercentEstimate(
-    const std::string& origin) const {
-  if (origin.empty() || !save_data_savings_estimate_dict_ ||
-      !save_data_savings_estimate_dict_->is_dict()) {
-    return 0;
-  }
-  const auto savings_percent =
-      save_data_savings_estimate_dict_->FindDoubleKey(origin);
-  if (!savings_percent)
-    return 0;
-  return *savings_percent;
 }
 
 }  // namespace data_reduction_proxy
