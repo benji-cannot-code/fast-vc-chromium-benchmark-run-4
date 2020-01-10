bc9dae3db134693588a0417e304182cc8189271c
Peter Kasting <pkasting@chromium.org>
2020-01-10 21:05:33 +0000

    Compute omnibox background color from toolbar color.
    This makes the omnibox/toolbar contrast ratio 1.3 by default.
    Theme authors can manually override the default colors.
    
    Bug: 878664
    Change-Id: I24b95e8d225fd375b573b48e403fbf147013e940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986244
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730295}

--- ui/gfx/color_utils.cc
+++ ui/gfx/color_utils.cc
@@ -401,6 +401,10 @@ std::string SkColorToRgbString(SkColor color) {
                             SkColorGetB(color));
 }
 
+SkColor GetDarkestColor() {
+  return g_darkest_color;
+}
+
 SkColor SetDarkestColorForTesting(SkColor color) {
   const SkColor previous_darkest_color = g_darkest_color;
   g_darkest_color = color;
