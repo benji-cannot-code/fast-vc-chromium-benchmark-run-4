d69a899ac31727ed02e90cb1b5dc09d13880557a
Sophie Chang <sophiechang@chromium.org>
2020-01-10 20:23:30 +0000

    Expire fetched hints when top host hints are stored
    This also changes it to use the hint's max cache duration if it's
    provided. This value has been populated from the server for awhile.
    
    Bug: 1024236
    Change-Id: I0d5fde6572cf76f6a72357d55e1875245d1e9c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994414
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730284}

--- components/optimization_guide/optimization_guide_store_unittest.cc
+++ components/optimization_guide/optimization_guide_store_unittest.cc
@@ -1419,9 +1419,7 @@ TEST_F(OptimizationGuideStoreTest, FindHintEntryKeyForFetchedHints) {
   InitializeStore(schema_state);
 
   std::unique_ptr<StoreUpdateData> update_data =
-      guide_store()->CreateUpdateDataForFetchedHints(
-          update_time, update_time + optimization_guide::features::
-                                         StoredFetchedHintsFreshnessDuration());
+      guide_store()->CreateUpdateDataForFetchedHints(update_time);
   ASSERT_TRUE(update_data);
   SeedFetchedUpdateData(update_data.get(), update_hint_count);
   UpdateFetchedHints(std::move(update_data));
@@ -1464,10 +1462,7 @@ TEST_F(OptimizationGuideStoreTest,
 
   // Add fetched hints to the store that overlap with the same hosts as the
   // initial set.
-  update_data = guide_store()->CreateUpdateDataForFetchedHints(
-      update_time,
-      update_time +
-          optimization_guide::features::StoredFetchedHintsFreshnessDuration());
+  update_data = guide_store()->CreateUpdateDataForFetchedHints(update_time);
 
   proto::Hint hint;
   hint.set_key("domain2.org");
@@ -1523,8 +1518,7 @@ TEST_F(OptimizationGuideStoreTest, ClearFetchedHints) {
 
   // Add fetched hints to the store that overlap with the same hosts as the
   // initial set.
-  update_data = guide_store()->CreateUpdateDataForFetchedHints(
-      update_time, update_time + base::TimeDelta().FromDays(7));
+  update_data = guide_store()->CreateUpdateDataForFetchedHints(update_time);
 
   proto::Hint fetched_hint1;
   fetched_hint1.set_key("domain2.org");
@@ -1585,10 +1579,7 @@ TEST_F(OptimizationGuideStoreTest, ClearFetchedHints) {
   host_suffix = "host.domain2.org";
   EXPECT_TRUE(guide_store()->FindHintEntryKey(host_suffix, &hint_entry_key));
 
-  update_data = guide_store()->CreateUpdateDataForFetchedHints(
-      update_time,
-      update_time +
-          optimization_guide::features::StoredFetchedHintsFreshnessDuration());
+  update_data = guide_store()->CreateUpdateDataForFetchedHints(update_time);
   proto::Hint new_hint;
   new_hint.set_key("domain1.org");
   new_hint.set_key_representation(proto::HOST_SUFFIX);
@@ -1635,31 +1626,37 @@ TEST_F(OptimizationGuideStoreTest, FetchHintsPurgeExpiredFetchedHints) {
 
   // Add fetched hints to the store that overlap with the same hosts as the
   // initial set.
-  update_data = guide_store()->CreateUpdateDataForFetchedHints(
-      update_time, update_time + base::TimeDelta().FromDays(7));
+  update_data = guide_store()->CreateUpdateDataForFetchedHints(update_time);
 
   proto::Hint fetched_hint1;
   fetched_hint1.set_key("domain2.org");
   fetched_hint1.set_key_representation(proto::HOST_SUFFIX);
+  fetched_hint1.mutable_max_cache_duration()->set_seconds(
+      base::TimeDelta::FromDays(7).InSeconds());
   update_data->MoveHintIntoUpdateData(std::move(fetched_hint1));
   proto::Hint fetched_hint2;
   fetched_hint2.set_key("domain3.org");
   fetched_hint2.set_key_representation(proto::HOST_SUFFIX);
+  fetched_hint1.mutable_max_cache_duration()->set_seconds(
+      base::TimeDelta::FromDays(7).InSeconds());
   update_data->MoveHintIntoUpdateData(std::move(fetched_hint2));
 
   UpdateFetchedHints(std::move(update_data));
 
   // Add expired fetched hints to the store.
-  update_data = guide_store()->CreateUpdateDataForFetchedHints(
-      update_time, update_time - base::TimeDelta().FromDays(7));
+  update_data = guide_store()->CreateUpdateDataForFetchedHints(update_time);
 
   proto::Hint fetched_hint3;
   fetched_hint1.set_key("domain4.org");
   fetched_hint1.set_key_representation(proto::HOST_SUFFIX);
+  fetched_hint1.mutable_max_cache_duration()->set_seconds(
+      base::TimeDelta::FromDays(-7).InSeconds());
   update_data->MoveHintIntoUpdateData(std::move(fetched_hint1));
   proto::Hint fetched_hint4;
   fetched_hint2.set_key("domain5.org");
   fetched_hint2.set_key_representation(proto::HOST_SUFFIX);
+  fetched_hint2.mutable_max_cache_duration()->set_seconds(
+      base::TimeDelta::FromDays(-7).InSeconds());
   update_data->MoveHintIntoUpdateData(std::move(fetched_hint2));
 
   UpdateFetchedHints(std::move(update_data));
@@ -1700,12 +1697,13 @@ TEST_F(OptimizationGuideStoreTest, FetchedHintsLoadExpiredHint) {
   UpdateComponentHints(std::move(update_data));
 
   // Add fetched hints to the store that expired.
-  update_data = guide_store()->CreateUpdateDataForFetchedHints(
-      update_time, update_time - base::TimeDelta().FromDays(10));
+  update_data = guide_store()->CreateUpdateDataForFetchedHints(update_time);
 
   proto::Hint fetched_hint1;
   fetched_hint1.set_key("domain2.org");
   fetched_hint1.set_key_representation(proto::HOST_SUFFIX);
+  fetched_hint1.mutable_max_cache_duration()->set_seconds(
+      base::TimeDelta().FromDays(-10).InSeconds());
   update_data->MoveHintIntoUpdateData(std::move(fetched_hint1));
   proto::Hint fetched_hint2;
   fetched_hint2.set_key("domain3.org");
