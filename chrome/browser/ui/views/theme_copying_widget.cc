5991e686cee0993dc706deb5e2431f9e31bcae41
Thomas Lukaszewicz <tluk@chromium.org>
2020-01-10 22:45:25 +0000

    Revert "Removed profile theme dependency in omnibox_popup_contents_view"
    This reverts commit 14ac66bdf66a001125f2ea079ceddc0374b00391.
    
    Reason for revert:
    Crash caused by views trying to access color through parent before view
    hierarchy is mounted to a widget. See bug for context.
    https://bugs.chromium.org/p/chromium/issues/detail?id=1040926
    
    Original change's description:
    > Removed profile theme dependency in omnibox_popup_contents_view
    >
    > Removed the need to pass down a theme provider obtained through a call to
    > GetThemeProviderForProfile() in omnibox view.
    >
    > Bug: none
    > Change-Id: Ib53f9c70263bc0691c7aa507bfd6ecb301a0fa1d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1987031
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#729058}
    
    TBR=pkasting@chromium.org,tluk@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: none
    Change-Id: Ifa930060f5002366362f45dc6d86081f15d8c45e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1995704
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730323}

--- chrome/browser/ui/views/theme_copying_widget.cc
+++ chrome/browser/ui/views/theme_copying_widget.cc
@@ -13,7 +13,3 @@ ThemeCopyingWidget::~ThemeCopyingWidget() {}
 const ui::NativeTheme* ThemeCopyingWidget::GetNativeTheme() const {
   return role_model_->GetNativeTheme();
 }
-
-const ui::ThemeProvider* ThemeCopyingWidget::GetThemeProvider() const {
-  return role_model_->GetThemeProvider();
-}
