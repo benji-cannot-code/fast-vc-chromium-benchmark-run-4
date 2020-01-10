c297a03a20b75102cfd0594f0314acf89dd791e6
Scott Violet <sky@chromium.org>
2020-01-10 17:15:19 +0000

    sessions: removes SessionTabHelper::SetTabExtensionAppID
    And puts the implementation in the single place that needs it.
    SessionTabHelper is really intended to forward WebContentsObserver
    related calls to SessionService. SetTabExtensionAppID() isn't that.
    As SetTabExtensionAppID() was called in a single place, I moved
    the implementation there.
    
    This is part of a larger refactoring I'm doing in hopes of moving
    SessionTabHelper to a shared location.
    
    BUG=1033924
    TEST=none
    
    Change-Id: Ia70af33908593839d4a22b4ebbc5f9f48c8abc30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994527
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730200}

--- chrome/browser/sessions/session_tab_helper.cc
+++ chrome/browser/sessions/session_tab_helper.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sessions/content/content_serialized_navigation_builder.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/web_contents.h"
+#include "extensions/buildflags/buildflags.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "extensions/common/extension_messages.h"
@@ -24,8 +25,7 @@ SessionTabHelper::SessionTabHelper(content::WebContents* contents)
       session_id_(SessionID::NewUnique()),
       window_id_(SessionID::InvalidValue()) {}
 
-SessionTabHelper::~SessionTabHelper() {
-}
+SessionTabHelper::~SessionTabHelper() = default;
 
 void SessionTabHelper::SetWindowID(const SessionID& id) {
   window_id_ = id;
@@ -113,20 +113,6 @@ void SessionTabHelper::NavigationEntryChanged(
       sessions::ContentSerializedNavigationBuilder::FromNavigationEntry(
           change_details.index, change_details.changed_entry);
   session_service->UpdateTabNavigation(window_id(), session_id(), navigation);
-}
-#endif
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-void SessionTabHelper::SetTabExtensionAppID(
-    const std::string& extension_app_id) {
-#if BUILDFLAG(ENABLE_SESSION_SERVICE)
-  SessionService* session_service = SessionServiceFactory::GetForProfile(
-      Profile::FromBrowserContext(web_contents()->GetBrowserContext()));
-  if (!session_service)
-    return;
-
-  session_service->SetTabExtensionAppID(window_id(), session_id(),
-                                        extension_app_id);
-#endif
 }
 #endif
 
