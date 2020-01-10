692f4ce008352de0eecb82df9a7d221706281b1e
Michael Thiessen <mthiesse@chromium.org>
2020-01-10 18:50:39 +0000

    Move PartnerBrowserCustomizationsUnitTest to unit_tests
    In order to migrate java.net.URI to GURL the junit tests that rely on
    java.net.URI need to be moved to a test apk that supports the native
    library.
    
    This change moves the PartnerBrowserCustomizationsUnitTest unittests
    from junit to unit_tests (Android only). These tests should be just as
    fast as they were as junit tests (and much faster than chrome_test_apk
    unit tests).
    
    Bug: 783819
    Change-Id: I61fe3e37bbf4986b93e1a47d9ee348f19e97372e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991736
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730249}

--- chrome/android/native_java_unittests/src/org/chromium/chrome/browser/partnercustomizations/PartnerBrowserCustomizationsUnitTest.java
+++ chrome/android/native_java_unittests/src/org/chromium/chrome/browser/partnercustomizations/PartnerBrowserCustomizationsUnitTest.java
@@ -6,21 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.partnercustomizations;
 
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.annotation.Config;
 
-import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.CalledByNativeJavaTest;
 import org.chromium.base.test.util.Feature;
 
 /**
  * Unit tests for {@link PartnerBrowserCustomizations}.
  */
-@RunWith(BaseRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
 public class PartnerBrowserCustomizationsUnitTest {
-    @Test
+    @CalledByNative
+    private PartnerBrowserCustomizationsUnitTest() {}
+
     @Feature({"Homepage"})
+    @CalledByNativeJavaTest
     public void testIsValidHomepage() {
         Assert.assertTrue(PartnerBrowserCustomizations.isValidHomepage(
                 "chrome-native://newtab/path#fragment"));
