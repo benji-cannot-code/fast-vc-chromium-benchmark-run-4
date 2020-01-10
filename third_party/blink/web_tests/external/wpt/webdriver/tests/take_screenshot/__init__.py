34a3125b63d477512b4c873ca961b992218344d6
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-01-10 04:23:57 +0000

    Import wpt@2a73e37c60b18846d9d2798c845242f209d74644
    Using wpt-import in Chromium e61ad0df6657d8b26dc6fc2ccf20536a85d4b3f1.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: I21d06d257d78753395d1682736b1f82479004758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1993716
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#730055}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/take_screenshot/__init__.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/take_screenshot/__init__.py
@@ -1,7 +1,19 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def element_dimensions(session, element):
+    return tuple(session.execute_script("""
+        const {devicePixelRatio} = window;
+        let {width, height} = arguments[0].getBoundingClientRect();
+
+        return [
+          Math.floor(width * devicePixelRatio),
+          Math.floor(height * devicePixelRatio),
+        ];
+        """, args=(element,)))
+
+
 def viewport_dimensions(session):
     return tuple(session.execute_script("""
-        let {devicePixelRatio, innerHeight, innerWidth} = window;
+        const {devicePixelRatio, innerHeight, innerWidth} = window;
 
         return [
           Math.floor(innerWidth * devicePixelRatio),
