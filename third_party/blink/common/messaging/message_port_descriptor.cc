6cfa6fd99b2f5e1a61eab64650f9a811e2078438
Chris Hamilton <chrisha@chromium.org>
2020-01-10 19:17:14 +0000

    Create Java counterpart for blink::MessagePortDescriptor.
    A further CL will convert content_public.browser.MessagePort and
    transferable_message.mojom to use content_public.browser.MessagePortDescriptor
    instead of mojom.MessagePipeHandle. This whole migration will allow
    Javascript message ports to be instrumented.
    
    BUG=980533
    
    Change-Id: Ib091b5f12836c2c6e5ff13bfdad8a9ee353bea57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974674
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730260}

--- third_party/blink/common/messaging/message_port_descriptor.cc
+++ third_party/blink/common/messaging/message_port_descriptor.cc
@@ -71,6 +71,12 @@ MessagePortDescriptor& MessagePortDescriptor::operator=(
   return *this;
 }
 
+MojoHandle MessagePortDescriptor::GetMojoHandleForTesting() const {
+  if (!handle_.get())
+    return MOJO_HANDLE_INVALID;
+  return handle_.get().value();
+}
+
 bool MessagePortDescriptor::IsValid() const {
   // |handle_| can be valid or invalid, depending on if we're entangled or
   // not. But everything else should be consistent.
@@ -123,7 +129,7 @@ void MessagePortDescriptor::Reset() {
 }
 
 void MessagePortDescriptor::Init(mojo::ScopedMessagePipeHandle handle,
-                                 base::UnguessableToken id,
+                                 const base::UnguessableToken& id,
                                  uint64_t sequence_number) {
   // Init is only called by deserialization code and thus should only be called
   // on a default initialized descriptor.
@@ -171,6 +177,13 @@ mojo::ScopedMessagePipeHandle MessagePortDescriptor::TakeHandleToEntangle(
   return std::move(handle_);
 }
 
+mojo::ScopedMessagePipeHandle
+MessagePortDescriptor::TakeHandleToEntangleWithEmbedder() {
+  DCHECK(handle_.is_valid());
+  NotifyAttachedToEmbedder();
+  return std::move(handle_);
+}
+
 void MessagePortDescriptor::GiveDisentangledHandle(
     mojo::ScopedMessagePipeHandle handle) {
   // We should only ever be given back the same handle that was taken from us.
@@ -180,6 +193,12 @@ void MessagePortDescriptor::GiveDisentangledHandle(
 #endif
   handle_ = std::move(handle);
   NotifyDetached();
+}
+
+// static
+MessagePortDescriptor::InstrumentationDelegate*
+MessagePortDescriptor::GetInstrumentationDelegate() {
+  return g_instrumentation_delegate;
 }
 
 MessagePortDescriptor::MessagePortDescriptor(
@@ -198,6 +217,14 @@ void MessagePortDescriptor::NotifyAttached(
   if (g_instrumentation_delegate) {
     g_instrumentation_delegate->NotifyMessagePortAttached(
         id_, sequence_number_++, execution_context_id);
+  }
+}
+
+void MessagePortDescriptor::NotifyAttachedToEmbedder() {
+  DCHECK(!id_.is_empty());
+  if (g_instrumentation_delegate) {
+    g_instrumentation_delegate->NotifyMessagePortAttachedToEmbedder(
+        id_, sequence_number_++);
   }
 }
 
@@ -224,8 +251,10 @@ MessagePortDescriptorPair::MessagePortDescriptorPair() {
 
   // Notify the instrumentation that these ports are newly created and peers of
   // each other.
-  if (g_instrumentation_delegate)
-    g_instrumentation_delegate->NotifyMessagePortPairCreated(*this);
+  if (g_instrumentation_delegate) {
+    g_instrumentation_delegate->NotifyMessagePortPairCreated(port0_.id(),
+                                                             port1_.id());
+  }
 }
 
 MessagePortDescriptorPair::~MessagePortDescriptorPair() = default;
