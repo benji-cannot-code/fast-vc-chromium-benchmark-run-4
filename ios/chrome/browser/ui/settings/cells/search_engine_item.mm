1adeff4272a9cd170682271ad5257972c2e36627
Gauthier Ambard <gambard@chromium.org>
2020-01-10 12:32:08 +0000

    [iOS] Add accessibility trait to disabled search engines
    This CL adds the UIAccessibilityTraitNotEnabled to the search engine
    cells when they are not interactable.
    
    Fixed: 1040497
    Change-Id: Iec65f625d7bf3d7f738d82ab86b204c3c58a6099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994284
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#730133}

--- ios/chrome/browser/ui/settings/cells/search_engine_item.mm
+++ ios/chrome/browser/ui/settings/cells/search_engine_item.mm
@@ -55,9 +55,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self.enabled) {
     cell.contentView.alpha = 1.0;
     cell.userInteractionEnabled = YES;
+    cell.accessibilityTraits &= ~UIAccessibilityTraitNotEnabled;
   } else {
     cell.contentView.alpha = 0.4;
     cell.userInteractionEnabled = NO;
+    cell.accessibilityTraits |= UIAccessibilityTraitNotEnabled;
   }
 
   if (styler.cellTitleColor) {
