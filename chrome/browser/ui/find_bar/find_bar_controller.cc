5ea7313cf5678cbc88cf434d9d7d09de90ccfda2
Evan Stade <estade@chromium.org>
2020-01-10 00:54:24 +0000

    Move FindBarController::GetLocationForFindbarView to find_bar_host.cc
    This is the only place it's used, and it's view-specific logic so it
    belongs closer to the view.
    
    Bug: 1038415
    Change-Id: I10bedda65753cff0b883efdff072afc683a7bc7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989884
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730004}

--- chrome/browser/ui/find_bar/find_bar_controller.cc
+++ chrome/browser/ui/find_bar/find_bar_controller.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "base/i18n/rtl.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_util.h"
@@ -26,18 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
-#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/range/range.h"
 
 using content::NavigationController;
 using content::WebContents;
-
-namespace {
-
-// The minimum space between the FindInPage window and the search result.
-constexpr int kMinFindWndDistanceFromSelection = 5;
-
-}  // namespace
 
 FindBarController::FindBarController(std::unique_ptr<FindBar> find_bar,
                                      Browser* browser)
@@ -195,46 +186,6 @@ void FindBarController::Observe(int type,
                      FindBoxResultAction::kClear);
     }
   }
-}
-
-// static
-gfx::Rect FindBarController::GetLocationForFindbarView(
-    gfx::Rect view_location,
-    const gfx::Rect& dialog_bounds,
-    const gfx::Rect& avoid_overlapping_rect) {
-  if (base::i18n::IsRTL()) {
-    int boundary = dialog_bounds.width() - view_location.width();
-    view_location.set_x(std::min(view_location.x(), boundary));
-  } else {
-    view_location.set_x(std::max(view_location.x(), dialog_bounds.x()));
-  }
-
-  gfx::Rect new_pos = view_location;
-
-  // If the selection rectangle intersects the current position on screen then
-  // we try to move our dialog to the left (right for RTL) of the selection
-  // rectangle.
-  if (!avoid_overlapping_rect.IsEmpty() &&
-      avoid_overlapping_rect.Intersects(new_pos)) {
-    if (base::i18n::IsRTL()) {
-      new_pos.set_x(avoid_overlapping_rect.x() +
-                    avoid_overlapping_rect.width() +
-                    (2 * kMinFindWndDistanceFromSelection));
-
-      // If we moved it off-screen to the right, we won't move it at all.
-      if (new_pos.x() + new_pos.width() > dialog_bounds.width())
-        new_pos = view_location;  // Reset.
-    } else {
-      new_pos.set_x(avoid_overlapping_rect.x() - new_pos.width() -
-        kMinFindWndDistanceFromSelection);
-
-      // If we moved it off-screen to the left, we won't move it at all.
-      if (new_pos.x() < 0)
-        new_pos = view_location;  // Reset.
-    }
-  }
-
-  return new_pos;
 }
 
 void FindBarController::OnFindResultAvailable(
