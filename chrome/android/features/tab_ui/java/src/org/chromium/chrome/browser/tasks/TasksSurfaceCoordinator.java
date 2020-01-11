ab77bc2d9f31b8e048dd3d504865d56cbc152b2e
gogerald <gogerald@google.com>
2020-01-11 00:29:30 +0000

    [StartSurface] Lazily initialize MV tiles
    Bug: 1035915
    Change-Id: Id5277007e1771054a298d078b68af9107698ab12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994424
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730355}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/TasksSurfaceCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/TasksSurfaceCoordinator.java
@@ -58,6 +58,11 @@ public class TasksSurfaceCoordinator implements TasksSurface {
 
     /** TasksSurface implementation. */
     @Override
+    public void initialize() {
+        mMostVisitedList.initialize();
+    }
+
+    @Override
     public void setOnTabSelectingListener(TabSwitcher.OnTabSelectingListener listener) {
         mTabSwitcher.setOnTabSelectingListener(listener);
     }
