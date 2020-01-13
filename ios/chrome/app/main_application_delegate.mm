7729dfe7b77496d9ae8d2f66b49ed24b6c20fe16
Avi Drissman <avi@chromium.org>
2020-01-13 18:50:40 +0000

    Revert "Reland of "[iOS][multiball] Move AppURLLoadingServiceDelegate to Scene.""
    This reverts commit 75ed0813a8665848f8b8e012fb7f224995ce8980.
    
    Reason for revert: Breaks compilation on
    
    https://ci.chromium.org/p/chromium/builders/ci/ios-device-xcode-clang
    https://ci.chromium.org/p/chromium/builders/ci/ios-simulator-xcode-clang
    
    Original change's description:
    > Reland of "[iOS][multiball] Move AppURLLoadingServiceDelegate to Scene."
    > 
    > This is a reland of commit 4a58461bc5b79b1d661566b81042c6ae0b3c98bd.
    > 
    > Original change's description:
    > > [iOS][multiball] Move AppURLLoadingServiceDelegate to Scene.
    > >
    > > Moves AppURLLoadingServiceDelegate from Main to Scene Controller.
    > > * adds SceneControllerGuts temporary interface to expose scene-level
    > > internals to MainController while the code is moved.
    > >
    > > Bug: None
    > > Change-Id: I0c7dbe077a94ff5b9c598c401c9c7340cc8f969c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1983226
    > > Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    > > Reviewed-by: Mark Cogan <marq@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#730151}
    > 
    > Change-Id: I378ae83c31c7e2b411407c9b346c6fc342785317
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1993818
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    > Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#730558}
    
    TBR=marq@chromium.org,stkhapugin@chromium.org
    
    Change-Id: I8af804b357abd8f28ec2cea4473b66b023e2adec
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1998385
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730671}

--- ios/chrome/app/main_application_delegate.mm
+++ ios/chrome/app/main_application_delegate.mm
@@ -87,7 +87,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       _sceneController =
           [[SceneController alloc] initWithSceneState:_sceneState];
 
-      // TODO(crbug.com/1040501): remove this.
       // This is temporary plumbing that's not supposed to be here.
       _sceneController.mainController = (id<MainControllerGuts>)_mainController;
       _mainController.sceneController = _sceneController;
--- ios/chrome/browser/ui/main/scene_controller.h
+++ ios/chrome/browser/ui/main/scene_controller.h
@@ -10,20 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/app/application_delegate/tab_switching.h"
 #import "ios/chrome/browser/ui/commands/application_commands.h"
-#import "ios/chrome/browser/ui/main/scene_controller_guts.h"
 #import "ios/chrome/browser/ui/main/scene_state.h"
 #import "ios/chrome/browser/ui/tab_grid/tab_switcher.h"
-#import "ios/chrome/browser/url_loading/app_url_loading_service.h"
 
 @protocol MainControllerGuts;
 
 // The controller object for a scene. Reacts to scene state changes.
-@interface SceneController : NSObject <AppURLLoadingServiceDelegate,
-                                       SceneStateObserver,
+@interface SceneController : NSObject <SceneStateObserver,
                                        ApplicationCommands,
                                        TabSwitcherDelegate,
-                                       TabSwitching,
-                                       SceneControllerGuts>
+                                       TabSwitching>
 
 - (instancetype)init NS_UNAVAILABLE;
 - (instancetype)initWithSceneState:(SceneState*)sceneState
