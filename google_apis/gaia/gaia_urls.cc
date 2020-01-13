9d3ce05c47a796ce950cbdb42f3498392b65e780
Mikel Astiz <mastiz@chromium.org>
2020-01-13 14:00:09 +0000

    Encode platform in Gaia URL for key retrieval
    Follow-up to
    https://chromium-review.googlesource.com/c/chromium/src/+/1897656 where
    a generic "desktop" was adopted for all platforms.
    
    Bug: 1019687
    Change-Id: Ieea3fb1a24ebfaa90c90ea3e2824a44f693441da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992431
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730526}

--- google_apis/gaia/gaia_urls.cc
+++ google_apis/gaia/gaia_urls.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 #include "google_apis/gaia/gaia_switches.h"
 #include "google_apis/google_api_keys.h"
 #include "url/url_canon.h"
@@ -31,7 +32,17 @@ const char kEmbeddedSetupWindowsUrlSuffix[] = "embedded/setup/windows";
 // signs in to Chrome. Note that Gaia will pass this client specified parameter
 // to all URLs that are loaded as part of thi sign-in flow.
 const char kSigninChromeSyncDice[] = "signin/chrome/sync?ssp=1";
+
+#if defined(OS_ANDROID)
+const char kSigninChromeSyncKeysUrl[] = "encryption/unlock/android";
+#elif defined(OS_IOS)
+const char kSigninChromeSyncKeysUrl[] = "encryption/unlock/ios";
+#elif defined(OS_CHROMEOS)
+const char kSigninChromeSyncKeysUrl[] = "encryption/unlock/chromeos";
+#else
 const char kSigninChromeSyncKeysUrl[] = "encryption/unlock/desktop";
+#endif
+
 const char kServiceLoginAuthUrlSuffix[] = "ServiceLoginAuth";
 const char kServiceLogoutUrlSuffix[] = "Logout";
 const char kContinueUrlForLogoutSuffix[] = "chrome/blank.html";
