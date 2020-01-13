b90e762c4000d507670cc33896270bdbd4af9688
Roman Aleksandrov <raleksandrov@google.com>
2020-01-13 11:59:56 +0000

    Packaged License Screen: Focus on Enroll button
    Focus on Enroll button after showing the screen.
    
    Bug: 1039704
    Change-Id: I089819b911ecc0f42070f075bee3ee5d028468cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989537
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#730512}

--- chrome/browser/resources/chromeos/login/packaged_license_screen.js
+++ chrome/browser/resources/chromeos/login/packaged_license_screen.js
@@ -22,6 +22,13 @@ Polymer({
     });
   },
 
+  /**
+   * Returns the control which should receive initial focus.
+   */
+  get defaultControl() {
+    return this.$.packagedLicenseDialog;
+  },
+
   /*
    * Executed on language change.
    */
