f4e4a05d44b7717725101004a3a1662abf7f5150
Dominic Mazzoni <dmazzoni@chromium.org>
2020-01-13 06:35:56 +0000

    ChromeVox should ignore focus events when the event source is touch gesture.
    When you use ChromeVox to navigate to something focusable, like a link or
    button, ChromeVox focuses it. Suppose you navigate to the next object and
    it's not focusable. Focus gets cleared, which is equivalent to moving focus
    back to the body element.
    
    In r716808 (https://crrev.com/c/1895791), focus events on the body were
    fixed to properly report eventFrom. Normally this is good, it allows
    ChromeVox to skip events that were triggered by actions.
    
    This accidentally broke touch navigation, though, because the logic to skip
    events from ChromeVox actions explicitly excluded touch gestures. What that
    meant is that if you used a touch gesture to move to the next element, and
    that moved focus back to the body, ChromeVox would sync back to the document
    body and you'd "lose your place".
    
    As a quick fix, adjust the logic so that focus events are still
    skipped even if they're from touch gestures - when the source of the
    focus event is from a ChromeVox action. As a follow-up we may want to
    improve that logic further.
    
    Bug: 1038121
    Change-Id: Idd1e31f9fd3c93e966f0fc047b767378b216c961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1984475
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730474}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/desktop_automation_handler.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/desktop_automation_handler.js
@@ -135,9 +135,11 @@ DesktopAutomationHandler.prototype = {
     }
 
     // Decide whether to announce and sync this event.
+    let isFocusOnRoot = evt.type == 'focus' && evt.target == evt.target.root;
     if (!DesktopAutomationHandler.announceActions &&
         evt.eventFrom == 'action' &&
-        EventSourceState.get() != EventSourceType.TOUCH_GESTURE) {
+        (EventSourceState.get() != EventSourceType.TOUCH_GESTURE ||
+         isFocusOnRoot)) {
       return;
     }
 
