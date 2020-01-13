61f12a21995e4a15a1c406d6c93c747f41de54e4
Kurt Horimoto <kkhorimoto@google.com>
2020-01-13 20:43:15 +0000

    [iOS] Create PasswordInfobarInteractionHandler
    This interaction handler is provided to the InfobarOverlayBrowserAgent
    so that it update the IOSChromeSavePasswordInfoBarDelegate for
    interaction events with the password infobar overlay UI.
    
    Also in this CL:
    - Creates MockIOSChromeSavePasswordInfoBarDelegate for use in tests.
    
    Bug: 1030357
    Change-Id: I8f17f9d2c28b72a3a2fb845973ff8ed032ac5375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1990220
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730790}

---
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/passwords/password_infobar_interaction_handler.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_PASSWORDS_PASSWORD_INFOBAR_INTERACTION_HANDLER_H_
+#define IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_PASSWORDS_PASSWORD_INFOBAR_INTERACTION_HANDLER_H_
+
+#include <memory>
+
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/infobar_interaction_handler.h"
+
+// An InfobarInteractionHandler that updates the model layer for interaction
+// events with the UI for password infobars.
+class PasswordInfobarInteractionHandler : public InfobarInteractionHandler {
+ public:
+  PasswordInfobarInteractionHandler();
+  ~PasswordInfobarInteractionHandler() override;
+};
+
+#endif  // IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_PASSWORDS_PASSWORD_INFOBAR_INTERACTION_HANDLER_H_
---
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/passwords/password_infobar_interaction_handler.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/passwords/password_infobar_interaction_handler.h"
+
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/passwords/password_infobar_banner_interaction_handler.h"
+#import "ios/chrome/browser/overlays/public/infobar_banner/save_password_infobar_banner_overlay.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+PasswordInfobarInteractionHandler::PasswordInfobarInteractionHandler()
+    : InfobarInteractionHandler(
+          SavePasswordInfobarBannerOverlayRequestConfig::RequestSupport(),
+          std::make_unique<PasswordInfobarBannerInteractionHandler>(),
+          /*sheet_handler=*/nullptr,
+          /*modal_handler=*/nullptr) {
+  // TODO(crbug.com/1033154): Create interaction handlers for detail sheet and
+  // modal.
+}
+
+PasswordInfobarInteractionHandler::~PasswordInfobarInteractionHandler() =
+    default;
