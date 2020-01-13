a717146971c830367390e6e994a8d49aaf35d62f
Greg Thompson <grt@chromium.org>
2020-01-13 16:55:26 +0000

    Prefer the product-neutral enrollment token policy used by Google Update.
    This aligns Chrome's enrollment token discovery with Google Update's.
    
    BUG=970162
    R=zmin@chromium.org
    
    Change-Id: I5a2ca07d4ccd816a4071872170e1059d5138abed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1995325
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730561}

--- chrome/installer/util/install_util.h
+++ chrome/installer/util/install_util.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include "base/command_line.h"
 #include "base/files/file.h"
@@ -177,10 +178,11 @@ class InstallUtil {
       const base::Version& new_version,
       WorkItemList* list);
 
-  // Returns the registry key path and value name where the enrollment token is
-  // stored for machine level user cloud policies.
-  static std::pair<std::wstring, std::wstring>
-  GetCloudManagementEnrollmentTokenRegistryPath();
+  // Returns pairs of registry key paths and value names where the enrollment
+  // token is stored for machine level user cloud policies. The locations are
+  // returned in order of preference.
+  static std::vector<std::pair<std::wstring, std::wstring>>
+  GetCloudManagementEnrollmentTokenRegistryPaths();
 
   // Returns the registry key path and value name where the DM token is stored
   // for machine level user cloud policies.
