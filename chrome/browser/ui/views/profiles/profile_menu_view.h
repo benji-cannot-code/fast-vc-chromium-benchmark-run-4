79c6c4a187c14412357796ab054d19545854076f
Mihai Sardarescu <msarda@chromium.org>
2020-01-13 17:40:00 +0000

    Focus the first profile button when user menu is open with keyboard shortcuts.
    This CL ensures that the first profile button is focused then the user
    uses the keyboard accelerator (Ctrl/Cmd+Shift+M) to open the user menu.
    It also adds back the histogram ProfileChooser.HasProfilesShown that
    seems to have been dropped by mistake.
    
    Bug: 1035656
    Change-Id: Id7e136507332e7f82d121d1944aac5159459d915
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989077
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Thomas Tangl <tangltom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730592}

--- chrome/browser/ui/views/profiles/profile_menu_view.h
+++ chrome/browser/ui/views/profiles/profile_menu_view.h
@@ -43,10 +43,6 @@ class ProfileMenuView : public ProfileMenuViewBase, public AvatarMenuObserver {
  private:
   friend class ProfileMenuViewExtensionsTest;
 
-  // TODO(crbug.com/1021587): Remove after ProfileMenuRevamp.
-  // ProfileMenuViewBase:
-  void FocusButtonOnKeyboardOpen() override;
-
   // views::BubbleDialogDelegateView:
   void OnWidgetClosing(views::Widget* widget) override;
   base::string16 GetAccessibleWindowTitle() const override;
@@ -162,7 +158,6 @@ class ProfileMenuView : public ProfileMenuViewBase, public AvatarMenuObserver {
 
   // TODO(crbug.com/1021587): Remove after ProfileMenuRevamp.
   // Button pointers used in tests.
-  views::Button* first_profile_button_ = nullptr;
   views::Button* lock_button_ = nullptr;
 
   // TODO(crbug.com/1021587): Remove after ProfileMenuRevamp.
