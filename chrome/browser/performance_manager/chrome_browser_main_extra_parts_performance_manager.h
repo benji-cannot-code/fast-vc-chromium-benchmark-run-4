b916d74bc8ab9d0a4390dc733ea6cc7115e80312
Francois Doray <fdoray@chromium.org>
2020-01-13 22:36:20 +0000

    [PM] Create shared workers from Performance Manager registry.
    This removes the dependency from
    //chrome/browser/performance_manager/ on
    //components/performance_manager/shared_worker_watcher.h.
    
    Change-Id: Ic77506305c40e638bc1150405163eee23ea27fbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978945
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730900}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.h
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager_observer.h"
 #include "chrome/browser/profiles/profile_observer.h"
-#include "components/performance_manager/process_node_source.h"
-#include "components/performance_manager/tab_helper_frame_node_source.h"
 
 class Profile;
 
@@ -31,7 +29,6 @@ class Graph;
 class PageLiveStateDecoratorHelper;
 class PerformanceManager;
 class PerformanceManagerRegistry;
-class WorkerWatcher;
 }  // namespace performance_manager
 
 // Handles the initialization of the performance manager and a few dependent
@@ -79,15 +76,6 @@ class ChromeBrowserMainExtraPartsPerformanceManager
       browser_child_process_watcher_;
 
   ScopedObserver<Profile, ProfileObserver> observed_profiles_{this};
-
-  // Needed by the worker watchers to access existing process nodes and frame
-  // nodes.
-  performance_manager::ProcessNodeSource process_node_source_;
-  performance_manager::TabHelperFrameNodeSource frame_node_source_;
-
-  // Observes the lifetime of all types of workers.
-  base::flat_map<Profile*, std::unique_ptr<performance_manager::WorkerWatcher>>
-      worker_watchers_;
 
   // Needed to properly maintain some of the PageLiveStateDecorator' properties.
   std::unique_ptr<performance_manager::PageLiveStateDecoratorHelper>
--- components/performance_manager/performance_manager_registry_impl.h
+++ components/performance_manager/performance_manager_registry_impl.h
@@ -6,12 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PERFORMANCE_MANAGER_PERFORMANCE_MANAGER_REGISTRY_IMPL_H_
 #define COMPONENTS_PERFORMANCE_MANAGER_PERFORMANCE_MANAGER_REGISTRY_IMPL_H_
 
+#include <memory>
+#include <string>
+
+#include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/observer_list.h"
 #include "base/sequence_checker.h"
 #include "components/performance_manager/embedder/performance_manager_registry.h"
 #include "components/performance_manager/performance_manager_tab_helper.h"
+#include "components/performance_manager/process_node_source.h"
 #include "components/performance_manager/render_process_user_data.h"
+#include "components/performance_manager/tab_helper_frame_node_source.h"
+#include "components/performance_manager/worker_watcher.h"
 
 namespace content {
 class RenderProcessHost;
@@ -48,6 +55,10 @@ class PerformanceManagerRegistryImpl
       content::WebContents* web_contents) override;
   void CreateProcessNodeForRenderProcessHost(
       content::RenderProcessHost* render_process_host) override;
+  void NotifyBrowserContextAdded(
+      content::BrowserContext* browser_context) override;
+  void NotifyBrowserContextRemoved(
+      content::BrowserContext* browser_context) override;
   void TearDown() override;
 
   // PerformanceManagerTabHelper::DestructionObserver:
@@ -65,6 +76,16 @@ class PerformanceManagerRegistryImpl
   // data. Used to destroy all user data when the registry is destroyed.
   base::flat_set<content::WebContents*> web_contents_;
   base::flat_set<content::RenderProcessHost*> render_process_hosts_;
+
+  // Map of browser context ids for which a WorkerWatcher exists to
+  // corresponding WorkerWatcher.
+  base::flat_map<std::string, std::unique_ptr<WorkerWatcher>>
+      browser_contexts_with_worker_watcher_;
+
+  // Used by WorkerWatchers to access existing process nodes and frame
+  // nodes.
+  performance_manager::ProcessNodeSource process_node_source_;
+  performance_manager::TabHelperFrameNodeSource frame_node_source_;
 
   base::ObserverList<PerformanceManagerMainThreadObserver> observers_;
 };
