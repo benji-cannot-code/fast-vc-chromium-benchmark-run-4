8411c1aa05b99616daba365445d92289d73dc48f
Oriol Brufau <obrufau@igalia.com>
2020-01-13 14:31:55 +0000

    [css-pseudo] Allow nested ::marker pseudo-elements
    Currently, pseudo-elements can only be originated by elements. However,
    ::before or ::after pseudo-elements can have 'display: list-item', and
    thus should be able to originate a nested ::marker.
    
    This patch adds the changes for making that possible. But note it has
    no effect in practice, since currently list markers are only pseudo-
    elements when their 'content' is not 'normal', and a nested ::marker
    can't be targeted by selectors yet.
    
    However, this change is necessary for implementing 'content: normal'
    markers as real pseudo-elements.
    
    BUG=457718
    
    Change-Id: I81a5c90b7117127e251a296c0bef66a12a95129c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1995329
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#730531}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -5257,8 +5257,11 @@ void Element::UpdatePseudoElement(PseudoId pseudo_id,
                                   const StyleRecalcChange change) {
   PseudoElement* element = GetPseudoElement(pseudo_id);
   if (!element) {
-    if ((element = CreatePseudoElementIfNeeded(pseudo_id)))
+    if ((element = CreatePseudoElementIfNeeded(pseudo_id))) {
+      // ::before and ::after can have a nested ::marker
+      element->CreatePseudoElementIfNeeded(kPseudoIdMarker);
       element->SetNeedsReattachLayoutTree();
+    }
     return;
   }
 
@@ -5276,8 +5279,6 @@ void Element::UpdatePseudoElement(PseudoId pseudo_id,
 }
 
 PseudoElement* Element::CreatePseudoElementIfNeeded(PseudoId pseudo_id) {
-  if (IsPseudoElement())
-    return nullptr;
   if (!CanGeneratePseudoElement(pseudo_id))
     return nullptr;
   if (pseudo_id == kPseudoIdFirstLetter) {
