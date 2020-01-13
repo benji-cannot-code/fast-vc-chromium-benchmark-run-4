a7ca549aec1dda1730d728afaaff40ffb2d69715
Vasilii Sukhanov <vasilii@chromium.org>
2020-01-13 10:17:21 +0000

    Disable ChromeBrowserMainExtraPartsMetricsTest.VerifyTouchEventsEnabledIsRecordedAfterPostBrowserStart on Win
    TBR=gab@chromium.org
    
    Bug: 1026946
    Change-Id: If680beac0d0660bf187ef9c63edac19d12d49339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1996304
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730496}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics_unittest.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "build/build_config.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/display/screen.h"
@@ -126,8 +127,16 @@ TEST_F(ChromeBrowserMainExtraPartsMetricsTest,
 #else
 
 // Verify a TouchEventsEnabled value is recorded during PostBrowserStart.
+// Flaky on Win only.  http://crbug.com/1026946
+#if defined(OS_WIN)
+#define MAYBE_VerifyTouchEventsEnabledIsRecordedAfterPostBrowserStart \
+  DISABLED_VerifyTouchEventsEnabledIsRecordedAfterPostBrowserStart
+#else
+#define MAYBE_VerifyTouchEventsEnabledIsRecordedAfterPostBrowserStart \
+  VerifyTouchEventsEnabledIsRecordedAfterPostBrowserStart
+#endif
 TEST_F(ChromeBrowserMainExtraPartsMetricsTest,
-       VerifyTouchEventsEnabledIsRecordedAfterPostBrowserStart) {
+       MAYBE_VerifyTouchEventsEnabledIsRecordedAfterPostBrowserStart) {
   base::HistogramTester histogram_tester;
   ChromeBrowserMainExtraPartsMetrics test_target;
 
