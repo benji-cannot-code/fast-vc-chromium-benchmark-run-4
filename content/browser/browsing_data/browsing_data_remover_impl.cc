921047b90843014dc2838d1bb6c449526f383a93
Christian Dullweber <dullweber@chromium.org>
2020-01-13 13:09:37 +0000

    Improve metrics for browsing data deletion performance
    Replace History.ClearBrowsingData.Duration.PartialDeletion with a set
    of more detailed metrics.
    
    Change-Id: Iddeb24b7dc30850057a8ba2741a1d2fd2e64c208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1985968
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#730522}

--- content/browser/browsing_data/browsing_data_remover_impl.cc
+++ content/browser/browsing_data/browsing_data_remover_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/callback_helpers.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/stl_util.h"
@@ -305,7 +306,7 @@ void BrowsingDataRemoverImpl::RemoveImpl(
     choice = ONLY_CACHE;
   }
 
-  UMA_HISTOGRAM_ENUMERATION(
+  base::UmaHistogramEnumeration(
       "History.ClearBrowsingData.UserDeletedCookieOrCache", choice,
       MAX_CHOICE_VALUE);
 
@@ -599,18 +600,30 @@ void BrowsingDataRemoverImpl::Notify() {
       observer->OnBrowsingDataRemoverDone();
     }
   }
+
+  base::TimeDelta delta = base::Time::Now() - task.task_started;
   if (task.filter_builder->GetMode() == BrowsingDataFilterBuilder::BLACKLIST) {
-    base::TimeDelta delta = base::Time::Now() - task.task_started;
-    // Full and partial deletions are often implemented differently, so
-    // we track them in seperate metrics.
-    if (task.delete_begin.is_null() && task.delete_end.is_max() &&
-        task.filter_builder->IsEmptyBlacklist()) {
-      UMA_HISTOGRAM_MEDIUM_TIMES(
+    // Full, and time based and filtered deletions are often implemented
+    // differently, so we track them in separate metrics.
+    if (!task.filter_builder->IsEmptyBlacklist()) {
+      base::UmaHistogramMediumTimes(
+          "History.ClearBrowsingData.Duration.FilteredDeletion", delta);
+    } else if (task.delete_begin.is_null() && task.delete_end.is_max()) {
+      base::UmaHistogramMediumTimes(
           "History.ClearBrowsingData.Duration.FullDeletion", delta);
     } else {
-      UMA_HISTOGRAM_MEDIUM_TIMES(
+      base::UmaHistogramMediumTimes(
+          "History.ClearBrowsingData.Duration.TimeRangeDeletion", delta);
+    }
+    // TODO(dullweber): Remove this metric after M83.
+    if (!task.delete_begin.is_null() || !task.delete_end.is_max() ||
+        !task.filter_builder->IsEmptyBlacklist()) {
+      base::UmaHistogramMediumTimes(
           "History.ClearBrowsingData.Duration.PartialDeletion", delta);
     }
+  } else {
+    base::UmaHistogramMediumTimes(
+        "History.ClearBrowsingData.Duration.OriginDeletion", delta);
   }
 
   task_queue_.pop_front();
