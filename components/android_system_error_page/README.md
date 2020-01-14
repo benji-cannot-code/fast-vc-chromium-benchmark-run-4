7e6d22ae4b8799285ee4898a0a975c0dbbfd1a8e
Colin Blundell <blundell@chromium.org>
2020-01-14 14:08:53 +0000

    WebView/WebLayer: Componentize and share network error pages impl
    This CL componentizes WebView's renderer-side code for displaying
    network error pages and replaces WebLayer's fork of the code with
    usage of the shared code.
    
    TBR=jochen@chromium.org
    
    Bug: 1024326
    Change-Id: I6d9c8070d01a68e0e634fb9e72889ce9d2e8bb9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1995323
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731331}

--- components/android_system_error_page/README.md
+++ components/android_system_error_page/README.md
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Contains code and resources for displaying network error pages using an Android
-system style.
+system style. The C++ code is intended to be used from the renderer process.
---
+++ components/android_system_error_page/error_page_populator.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ANDROID_SYSTEM_ERROR_PAGE_ERROR_PAGE_POPULATOR_H_
+#define COMPONENTS_ANDROID_SYSTEM_ERROR_PAGE_ERROR_PAGE_POPULATOR_H_
+
+#include <string>
+
+namespace blink {
+struct WebURLError;
+}
+
+namespace android_system_error_page {
+
+// Populates |error_html| to display an error page with an Android system feel
+// for |error|.
+void PopulateErrorPageHtml(const blink::WebURLError& error,
+                           std::string* error_html);
+
+}  // namespace android_system_error_page
+
+#endif  // COMPONENTS_ANDROID_SYSTEM_ERROR_PAGE_ERROR_PAGE_POPULATOR_H_
