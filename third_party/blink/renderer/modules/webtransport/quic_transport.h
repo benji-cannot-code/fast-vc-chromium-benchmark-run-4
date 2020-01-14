c5208f833d26dc64d0248f28ab278189b740bc70
Adam Rice <ricea@chromium.org>
2020-01-14 12:34:41 +0000

    QuicTransport: Implement datagram send
    Implement the QuicTransport sendDatagrams() method. This gives access to
    a stream for sending datagrams over QuicTransport.
    
    Bug: 1011392
    Change-Id: If0981dd14188a5ed2fe471232ffbdb6d4363b099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959920
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731255}

--- third_party/blink/renderer/modules/webtransport/quic_transport.h
+++ third_party/blink/renderer/modules/webtransport/quic_transport.h
@@ -26,7 +26,9 @@ class ExceptionState;
 class ExecutionContext;
 class ScriptState;
 class WebTransportCloseInfo;
+class WritableStream;
 
+// https://wicg.github.io/web-transport/#quic-transport
 class MODULES_EXPORT QuicTransport final
     : public ScriptWrappable,
       public ActiveScriptWrappable<QuicTransport>,
@@ -47,6 +49,8 @@ class MODULES_EXPORT QuicTransport final
   ~QuicTransport() override;
 
   // QuicTransport IDL implementation.
+  WritableStream* sendDatagrams() { return outgoing_datagrams_; }
+
   void close(const WebTransportCloseInfo*);
 
   // QuicTransportHandshakeClient implementation
@@ -55,6 +59,9 @@ class MODULES_EXPORT QuicTransport final
       mojo::PendingReceiver<network::mojom::blink::QuicTransportClient>)
       override;
   void OnHandshakeFailed() override;
+
+  // QuicTransportClient implementation
+  // TODO(ricea): Add methods.
 
   // Implementation of ContextLifecycleObserver
   void ContextDestroyed(ExecutionContext*) final;
@@ -66,9 +73,18 @@ class MODULES_EXPORT QuicTransport final
   void Trace(Visitor* visitor) override;
 
  private:
+  class DatagramUnderlyingSink;
+
   void Init(const String& url, ExceptionState&);
   void Dispose();
   void OnConnectionError();
+
+  bool cleanly_closed_ = false;
+
+  // This corresponds to the [[SentDatagrams]] internal slot in the standard.
+  Member<WritableStream> outgoing_datagrams_;
+
+  Member<ScriptState> script_state_;
 
   const KURL url_;
   mojo::Remote<network::mojom::blink::QuicTransport> quic_transport_;
