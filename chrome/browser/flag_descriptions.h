5702e0734310c025fe46c8f456d22edfc2a04789
manukh <manukh@chromium.org>
2020-01-14 03:54:13 +0000

    [omnibox] Experiment with midword matching for HistoryQuick suggestions.
    Currently, midword matches disqualify HistoryQuick suggestions. E.g. the
    input 'java script' won't match a suggestion titled 'javascript'. This
    CL adds 2 flags to experiment with alternatives allowing midword
    matching.
    
    1) Midword matches are allowed and scored when the match begins
    immediately after the previous match ends. E.g. 'java script' will match
    but the input 'java cript' won't.
    
    2) Terms with no wordstart matches are ignored, i.e. allowed but not
    scored. E.g. both inputs 'java script' and 'java cript' will match and
    score equivalently.
    
    E.g.,
    
    Suggestion URL:   stackoverflow.com/questions/...
    Suggestion title: javascript ...
    
    Relevance scores:
    
    input           no exp   exp 1    exp 2    exp 1 & 2
    ------------------------------------------------------
    java script     0        1311     1303     1311
    java cript      0        0        1303     1303
    java            1311     1311     1311     1311
    ques tion       0        1311     1311     1311
    ques ion        0        0        1303     1303
    ques            1311     1311     1311     1311
    
    Bug: 591981
    Change-Id: If06e5fc502fde366c1c211c72bfcaa092e0693d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1998169
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731055}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -860,6 +860,14 @@ extern const char kOmniboxDisplayTitleForCurrentUrlDescription[];
 extern const char kOmniboxExperimentalSuggestScoringName[];
 extern const char kOmniboxExperimentalSuggestScoringDescription[];
 
+extern const char
+    kOmniboxHistoryQuickProviderAllowButDoNotScoreMidwordTermsName[];
+extern const char
+    kOmniboxHistoryQuickProviderAllowButDoNotScoreMidwordTermsDescription[];
+extern const char kOmniboxHistoryQuickProviderAllowMidwordContinuationsName[];
+extern const char
+    kOmniboxHistoryQuickProviderAllowMidwordContinuationsDescription[];
+
 extern const char kOmniboxLocalEntitySuggestionsName[];
 extern const char kOmniboxLocalEntitySuggestionsDescription[];
 
