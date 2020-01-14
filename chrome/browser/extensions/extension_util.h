d85054b87e535bb00963041ccc084140ab55f132
Danan S <danan@chromium.org>
2020-01-14 04:52:55 +0000

    Refactor InstallPromptPermissions to make it accessible outside extensions
    This is needed so that the permissions can be rendered in the upcoming
    Parent Permission dialog for extension installs by child accounts.
    
    Bug: 1019908
    Change-Id: I5001d794a20eec492acc115c4a5f1d0999e5918f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986919
    Commit-Queue: Dan S <danan@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731079}

--- chrome/browser/extensions/extension_util.h
+++ chrome/browser/extensions/extension_util.h
@@ -20,11 +20,17 @@ namespace content {
 class BrowserContext;
 }
 
+namespace extensions {
+class PermissionSet;
+}
+
 namespace gfx {
 class ImageSkia;
 }
 
 class GURL;
+
+class Profile;
 
 namespace extensions {
 
@@ -101,6 +107,13 @@ const Extension* GetInstalledPwaForUrl(
     content::BrowserContext* context,
     const GURL& url,
     base::Optional<LaunchContainer> launch_container_filter = base::nullopt);
+
+// Returns a PermissionSet configured with the permissions that should be
+// displayed in an extension installation prompt for the specified |extension|.
+std::unique_ptr<const PermissionSet> GetInstallPromptPermissionSetForExtension(
+    const Extension* extension,
+    Profile* profile,
+    bool include_optional_permissions);
 
 }  // namespace util
 }  // namespace extensions
