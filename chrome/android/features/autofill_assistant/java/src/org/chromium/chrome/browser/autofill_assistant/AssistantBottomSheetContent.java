142ded89335b84946c22024f01aca6c6197164ad
Clemens Arbesser <arbesser@google.com>
2020-01-14 08:44:46 +0000

    [Autofill Assistant] Do not dismiss on scroll.
    Bug: b/147479275
    Change-Id: If091102c93fc9d0450a243ec378f1c22b1dbf151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1996307
    Auto-Submit: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731163}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantBottomSheetContent.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantBottomSheetContent.java
@@ -102,7 +102,7 @@ class AssistantBottomSheetContent implements BottomSheetContent {
 
     @Override
     public boolean hideOnScroll() {
-        return true;
+        return false;
     }
 
     @Override
