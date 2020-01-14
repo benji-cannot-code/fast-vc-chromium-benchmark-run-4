b3dccaa655a4ef4396014e2c42c9fb77e77e11ea
Kent Tamura <tkent@chromium.org>
2020-01-14 08:50:45 +0000

    Remove LayoutSearchField
    It exists only to secure height for -webkit-search-cancel-button. The
    code changes the intrinsic height of the INPUT box if the height of the
    inner_editor box is shorter than the height of the
    -webkit-search-cancel-button. However, it never happens because
    -webkit-search-cancel-button's size alwyas matches to font-size, and we
    ignore line-height property if the line-height is shoter than the
    font-size.
    
    So, we can remove LayoutSearchField, and this CL should have no behavior
    changes.
    
    Bug: 1040828
    Change-Id: I7e35f662cffa757351044be0f3ddee8baaa27dc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1999886
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731168}

--- third_party/blink/renderer/core/html/forms/search_input_type.cc
+++ third_party/blink/renderer/core/html/forms/search_input_type.cc
@@ -32,6 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/forms/search_input_type.h"
 
 #include "third_party/blink/public/platform/task_type.h"
+#include "third_party/blink/renderer/core/css/css_property_names.h"
+#include "third_party/blink/renderer/core/css_value_keywords.h"
+#include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/events/keyboard_event.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
@@ -40,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/shadow/shadow_element_names.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/input_type_names.h"
-#include "third_party/blink/renderer/core/layout/layout_search_field.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
 
@@ -55,11 +57,6 @@ SearchInputType::SearchInputType(HTMLInputElement& element)
 
 void SearchInputType::CountUsage() {
   CountUsageIfVisible(WebFeature::kInputTypeSearch);
-}
-
-LayoutObject* SearchInputType::CreateLayoutObject(const ComputedStyle&,
-                                                  LegacyLayout) const {
-  return new LayoutSearchField(&GetElement());
 }
 
 const AtomicString& SearchInputType::FormControlType() const {
