0ed32c1e2d0f33148a33017ff7ddf3435759f4fa
Koji Ishii <kojii@chromium.org>
2020-01-14 05:55:32 +0000

    [FragmentItem] Fix visual overflow
    This patch fixes two visual overflow issues:
    1. Offsets for inline children were computed relative to
       their inline formatting context. Fixed them to be local
       (relative to themselves.)
    2. Fixing the above issue regressed some tests because
       floats were not included in visual overflow, but these
       tests passed accidentally. Fixed to include floats.
    
    These two changes fix ~30 failures.
    
    Bug: 982194
    Change-Id: If6a2c1c03e50d76e4ad1becc32a56a39f807191a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1996115
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731096}

--- third_party/blink/renderer/core/layout/layout_block_flow_line.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow_line.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/line/word_measurement.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/svg/line/svg_root_inline_box.h"
 #include "third_party/blink/renderer/core/layout/vertical_position_cache.h"
 #include "third_party/blink/renderer/core/paint/ng/ng_paint_fragment.h"
@@ -2385,6 +2386,7 @@ void LayoutBlockFlow::AddVisualOverflowFromInlineChildren() {
       IsRootEditableElement(*GetNode()) && StyleRef().IsLeftToRightDirection())
     end_padding = LayoutUnit(1);
 
+  bool added_inline_children = false;
   if (const NGPaintFragment* paint_fragment = PaintFragment()) {
     for (const NGPaintFragment* child : paint_fragment->Children()) {
       if (child->HasSelfPaintingLayer())
@@ -2395,19 +2397,26 @@ void LayoutBlockFlow::AddVisualOverflowFromInlineChildren() {
         AddContentsVisualOverflow(child_rect);
       }
     }
-  } else if (const NGFragmentItems* items = FragmentItems()) {
-    for (NGInlineCursor cursor(*items); cursor; cursor.MoveToNextSibling()) {
-      const NGFragmentItem* child = cursor.CurrentItem();
-      DCHECK(child);
-      if (child->HasSelfPaintingLayer())
-        continue;
-      PhysicalRect child_rect = child->InkOverflow();
-      if (!child_rect.IsEmpty()) {
-        child_rect.offset += child->Offset();
-        AddContentsVisualOverflow(child_rect);
+    added_inline_children = true;
+  } else if (const NGPhysicalBoxFragment* fragment = CurrentFragment()) {
+    if (const NGFragmentItems* items = fragment->Items()) {
+      for (NGInlineCursor cursor(*items); cursor; cursor.MoveToNextSibling()) {
+        const NGFragmentItem* child = cursor.CurrentItem();
+        DCHECK(child);
+        if (child->HasSelfPaintingLayer())
+          continue;
+        PhysicalRect child_rect = child->InkOverflow();
+        if (!child_rect.IsEmpty()) {
+          child_rect.offset += child->Offset();
+          AddContentsVisualOverflow(child_rect);
+        }
       }
+      if (fragment->HasFloatingDescendantsForPaint())
+        AddVisualOverflowFromFloats(*fragment);
+      added_inline_children = true;
     }
-  } else {
+  }
+  if (!added_inline_children) {
     for (RootInlineBox* curr = FirstRootBox(); curr;
          curr = curr->NextRootBox()) {
       LayoutRect visual_overflow =
