9d3af181b50bb421df47bfdc756f621268e06746
Aaron Tagliaboschi <aarontag@chromium.org>
2020-01-14 20:39:42 +0000

    Unborking the accept-ch-lifetime CH WPT
    Due to some broswer-state related flakiness, the
    "accept-ch-lifetime.tentative.https.html" test was split
    into several different files. Also, all mentions of "lifetime"
    in the tests was removed to keep up to date with the Client Hint
    spec.
    
    Change-Id: I1c06b82d8cbe8bd6e304f5c1fa1d058b100a825c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994422
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731583}

---
+++ third_party/blink/web_tests/external/wpt/client-hints/accept-ch-stickiness/resources/echo-client-hints-received.py
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    """
+    Simple handler that sets a response header based on which client hint
+    request headers were received.
+    """
+
+    response.headers.append("Access-Control-Allow-Origin", "*")
+    response.headers.append("Access-Control-Allow-Headers", "*")
+    response.headers.append("Access-Control-Expose-Headers", "*")
+
+    if "device-memory" in request.headers:
+            response.headers.set("device-memory-received", request.headers.get("device-memory"))
