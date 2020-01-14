a8725e9fc1a0bf96576d560b376d80b7f21ee596
Kurt Horimoto <kkhorimoto@google.com>
2020-01-14 20:40:47 +0000

    [iOS] Create MockOverlayCoordinatorDelegate
    This delegate mocks out the presentation and dismissal callbacks for
    use in tests.
    
    Bug: none
    Change-Id: If6dd38ca262fb94b29c283d8a85e872d8fcf5457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994148
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731585}

---
+++ ios/chrome/browser/ui/overlays/test/mock_overlay_coordinator_delegate.mm
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/overlays/test/mock_overlay_coordinator_delegate.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+MockOverlayRequestCoordinatorDelegate::MockOverlayRequestCoordinatorDelegate() =
+    default;
+
+MockOverlayRequestCoordinatorDelegate::
+    ~MockOverlayRequestCoordinatorDelegate() = default;
