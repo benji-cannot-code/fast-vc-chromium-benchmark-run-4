00d9058a43c654bbff912d917b9f7a8041556736
Vitaly Buka <vitalybuka@chromium.org>
2020-01-14 09:56:24 +0000

    Update Crashpad to 4367e9df172c579ca232ef98d76b1a00d7333e8a
    54bbd7d0d583 Adds support for running GTests on iOS
    0b8741b0740e Remove redundant semicolon in cpu_context_linux.h
    4367e9df172c Increase kAllowedOffset for non-optimized build
    
    Change-Id: I69108474c924708dd60e79f94d562c4d173d71c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1998818
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Vitaly Buka <vitalybuka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731191}

--- third_party/crashpad/crashpad/test/gtest_main.cc
+++ third_party/crashpad/crashpad/test/gtest_main.cc
@@ -22,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gmock/gmock.h"
 #endif  // CRASHPAD_TEST_LAUNCHER_GMOCK
 
+#if defined(OS_IOS)
+#include "test/gtest_runner_ios.h"
+#endif
+
 #if defined(OS_WIN)
 #include "test/win/win_child_process.h"
 #endif  // OS_WIN
@@ -94,5 +98,12 @@ int main(int argc, char* argv[]) {
 #error #define CRASHPAD_TEST_LAUNCHER_GTEST or CRASHPAD_TEST_LAUNCHER_GMOCK
 #endif  // CRASHPAD_TEST_LAUNCHER_GMOCK
 
+#if defined(OS_IOS)
+  // iOS needs to run tests within the context of an app, so call a helper that
+  // invokes UIApplicationMain().  The application delegate will call
+  // RUN_ALL_TESTS() and exit before returning control to this function.
+  crashpad::test::IOSLaunchApplicationAndRunTests(argc, argv);
+#else
   return RUN_ALL_TESTS();
+#endif
 }
