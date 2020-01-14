675cdbe6eb51ea28749a1dcb23cbb3d0ead9e559
vitaliii <vitaliii@chromium.org>
2020-01-14 16:12:05 +0000

    [Sync:Sharing:Prefactor] Commit contribution: take callback, not worker.
    In a followup CL I will propagate failed commit item from
    NonBlockingTypeCommitContribution to ClientTagBasedModelTypeProcessor.
    This will happen through ModelTypeWorker. ModelTypeWorker is too hard
    to mock, so take only callback in NonBlockingTypeCommitContribution
    instead of entire NonBlockingTypeCommitContribution to enable testing
    in the followup CL.
    
    Bug: 1034923
    Change-Id: I7147e295ca17aadd37326117f41875dfd2daeaa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1997304
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731408}

--- components/sync/engine_impl/non_blocking_type_commit_contribution_unittest.cc
+++ components/sync/engine_impl/non_blocking_type_commit_contribution_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/base64.h"
+#include "base/bind_helpers.h"
 #include "base/hash/sha1.h"
 #include "components/sync/base/client_tag_hash.h"
 #include "components/sync/base/model_type.h"
@@ -173,8 +174,8 @@ TEST(NonBlockingTypeCommitContributionTest,
   requests_data.push_back(std::move(request_data));
   NonBlockingTypeCommitContribution contribution(
       PASSWORDS, sync_pb::DataTypeContext(), std::move(requests_data),
-      /*worker=*/nullptr, &cryptographer, PassphraseType::kImplicitPassphrase,
-      &debug_info_emitter,
+      /*on_commit_response_callback=*/base::NullCallback(), &cryptographer,
+      PassphraseType::kImplicitPassphrase, &debug_info_emitter,
       /*only_commit_specifics=*/false);
 
   sync_pb::ClientToServerMessage msg;
@@ -234,8 +235,8 @@ TEST(NonBlockingTypeCommitContributionTest,
   requests_data.push_back(std::move(request_data));
   NonBlockingTypeCommitContribution contribution(
       PASSWORDS, sync_pb::DataTypeContext(), std::move(requests_data),
-      /*worker=*/nullptr, &cryptographer, PassphraseType::kCustomPassphrase,
-      &debug_info_emitter,
+      /*on_commit_response_callback=*/base::NullCallback(), &cryptographer,
+      PassphraseType::kCustomPassphrase, &debug_info_emitter,
       /*only_commit_specifics=*/false);
 
   sync_pb::ClientToServerMessage msg;
