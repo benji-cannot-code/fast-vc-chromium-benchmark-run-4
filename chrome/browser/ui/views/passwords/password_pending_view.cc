adc86ca45735d91fc1f9cf9b099d1fe18a86ef32
Vasilii Sukhanov <vasilii@chromium.org>
2020-01-15 21:20:32 +0000

    Fix button titles in the password bubble.
    Bug: 1042296
    Change-Id: Ib0e3f39b21f700bca27090c3a297250164190d06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002580
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732135}

--- chrome/browser/ui/views/passwords/password_pending_view.cc
+++ chrome/browser/ui/views/passwords/password_pending_view.cc
@@ -307,6 +307,7 @@ PasswordPendingView::PasswordPendingView(content::WebContents* web_contents,
   }
 
   DialogDelegate::SetFootnoteView(CreateFooterView());
+  UpdateDialogButtons();
 }
 
 views::View* PasswordPendingView::GetUsernameTextfieldForTest() const {
