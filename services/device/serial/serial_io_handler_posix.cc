8294619f3675e2625b789e4359a05b43f2294b04
Francois Doray <fdoray@chromium.org>
2020-01-15 21:35:04 +0000

    Add CHECKs in SerialIoHandlerPosix to diagnose FileDescriptorWatcher errors.
    https://crbug.com/996777 suggests that in some cases, event_del()
    fails when resetting the FileDescriptorWatcher in SerialIoHandlerPosix.
    From reading the event_del() code, that can happen if the watched file
    descriptor has been closed. To help diagnose the problem and make sure
    that it doesn't happen again, this CL adds CHECKs that the file
    descriptor is valid before unregistering the watch. Errors generated
    by this CHECK will be more actionnable than the existing reports.
    
    Bug: 996777
    Change-Id: I15515462a59b71846a6a788cee8e77bad74a8cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003248
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732139}

--- services/device/serial/serial_io_handler_posix.cc
+++ services/device/serial/serial_io_handler_posix.cc
@@ -146,13 +146,13 @@ void SerialIoHandlerPosix::WriteImpl() {
 
 void SerialIoHandlerPosix::CancelReadImpl() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  file_read_watcher_.reset();
+  StopWatchingFileRead();
   QueueReadCompleted(0, read_cancel_reason());
 }
 
 void SerialIoHandlerPosix::CancelWriteImpl() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  file_write_watcher_.reset();
+  StopWatchingFileWrite();
   QueueWriteCompleted(0, write_cancel_reason());
 }
 
@@ -344,7 +344,7 @@ void SerialIoHandlerPosix::AttemptRead(bool within_read) {
   } else {
     // Stop watching the fd if we get notifications with no pending
     // reads or writes to avoid starving the message loop.
-    file_read_watcher_.reset();
+    StopWatchingFileRead();
   }
 }
 
@@ -354,7 +354,7 @@ void SerialIoHandlerPosix::RunReadCompleted(bool within_read,
   if (within_read) {
     // Stop watching the fd to avoid more reads until the queued ReadCompleted()
     // completes and releases the pending_read_buffer.
-    file_read_watcher_.reset();
+    StopWatchingFileRead();
 
     QueueReadCompleted(bytes_read, error);
   } else {
@@ -377,7 +377,7 @@ void SerialIoHandlerPosix::OnFileCanWriteWithoutBlocking() {
   } else {
     // Stop watching the fd if we get notifications with no pending
     // writes to avoid starving the message loop.
-    file_write_watcher_.reset();
+    StopWatchingFileWrite();
   }
 }
 
@@ -401,6 +401,26 @@ void SerialIoHandlerPosix::EnsureWatchingWrites() {
         base::BindRepeating(
             &SerialIoHandlerPosix::OnFileCanWriteWithoutBlocking,
             base::Unretained(this)));
+  }
+}
+
+void SerialIoHandlerPosix::StopWatchingFileRead() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (file_read_watcher_) {
+    // Check that file is valid before stopping the watch, to avoid getting a
+    // hard to diagnose crash in MessagePumpLibEvent. https://crbug.com/996777
+    CHECK(file().IsValid());
+    file_read_watcher_.reset();
+  }
+}
+
+void SerialIoHandlerPosix::StopWatchingFileWrite() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (file_write_watcher_) {
+    // Check that file is valid before stopping the watch, to avoid getting a
+    // hard to diagnose crash in MessagePumpLibEvent. https://crbug.com/996777
+    CHECK(file().IsValid());
+    file_write_watcher_.reset();
   }
 }
 
