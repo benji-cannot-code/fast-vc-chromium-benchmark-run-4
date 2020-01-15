996cf5b9cf41ef248a57b1771574a703e534e2c0
Edward Lemur <ehmaldonado@chromium.org>
2020-01-15 02:06:13 +0000

    PRESUBMIT: Fix usage of AffectedFiles.
    The first argument to AffectedFiles is include_deletes,
    so current usage sets it to include deleted files and
    ignores the filter, causing checks to run on unrelated
    files.
    
    Fix it so that the source filter is passed to the right
    parameter.
    
    Bug: 962619
    Change-Id: I94f550a72f2773d5fedc5949848dcd2defd5757e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1610541
    Commit-Queue: Edward Lesmes <ehmaldonado@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731781}

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -3307,7 +3307,7 @@ def _CheckAndroidTestJUnitFrameworkImport(input_api, output_api):
   sources = lambda x: input_api.FilterSourceFile(
       x, white_list=[r'.*\.java$'], black_list=None)
   errors = []
-  for f in input_api.AffectedFiles(sources):
+  for f in input_api.AffectedFiles(file_filter=sources):
     for line_num, line in f.ChangedContents():
       if deprecated_junit_framework_pattern.search(line):
         errors.append("%s:%d" % (f.LocalPath(), line_num))
@@ -3331,7 +3331,7 @@ def _CheckAndroidTestJUnitInheritance(input_api, output_api):
   sources = lambda x: input_api.FilterSourceFile(
       x, white_list=[r'.*Test\.java$'], black_list=None)
   errors = []
-  for f in input_api.AffectedFiles(sources):
+  for f in input_api.AffectedFiles(file_filter=sources):
     if not f.OldContents():
       class_declaration_start_flag = False
       for line_num, line in f.ChangedContents():
@@ -3360,7 +3360,7 @@ def _CheckAndroidTestAnnotationUsage(input_api, output_api):
   sources = lambda x: input_api.FilterSourceFile(
       x, white_list=[r'.*\.java$'], black_list=None)
   errors = []
-  for f in input_api.AffectedFiles(sources):
+  for f in input_api.AffectedFiles(file_filter=sources):
     for line_num, line in f.ChangedContents():
       if deprecated_annotation_import_pattern.search(line):
         errors.append("%s:%d" % (f.LocalPath(), line_num))
