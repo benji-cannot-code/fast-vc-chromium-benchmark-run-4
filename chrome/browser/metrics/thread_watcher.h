a8f29fbe2bede84442338b62ea32b064d70be1b4
François Doray <fdoray@chromium.org>
2020-01-15 17:05:52 +0000

    [hangs] Add time since last memory pressure crash key in hang reports.
    This CL adds a crash key with the number of seconds since the last
    critical memory pressure signal in hang reports. The goal is to
    facilitate filtering of reports that are likely caused by memory
    pressure.
    
    Many things can cause hangs, memory pressure is just one of them.
    More crash keys could be added in separate CLs to get a full
    understanding of hangs.
    
    Change-Id: I0ef482d0b6df7d0661b3d0141c4ee44766942a32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1987204
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732022}

--- chrome/browser/metrics/thread_watcher.h
+++ chrome/browser/metrics/thread_watcher.h
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "base/memory/memory_pressure_listener.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
@@ -185,6 +186,8 @@ class ThreadWatcher {
   FRIEND_TEST_ALL_PREFIXES(ThreadWatcherTest, ThreadNotResponding);
   FRIEND_TEST_ALL_PREFIXES(ThreadWatcherTest, MultipleThreadsResponding);
   FRIEND_TEST_ALL_PREFIXES(ThreadWatcherTest, MultipleThreadsNotResponding);
+  FRIEND_TEST_ALL_PREFIXES(ThreadWatcherTestWithMockTime,
+                           MemoryPressureCrashKey);
 
   // Post constructor initialization.
   void Initialize();
@@ -201,6 +204,9 @@ class ThreadWatcher {
   // This method records watched thread is not responding to the ping message.
   // It increments |unresponsive_count_| by 1.
   void GotNoResponse();
+
+  // Sets a crash key with the time since last critical memory pressure signal.
+  void SetTimeSinceLastCriticalMemoryPressureCrashKey();
 
   // This method returns true if the watched thread has not responded with a
   // pong message for |unresponsive_threshold_| number of ping messages.
@@ -281,6 +287,10 @@ class ThreadWatcher {
   // responsive.
   bool crash_on_hang_;
 
+  // The last time at which a critical memory pressure signal was received, or
+  // null if no signal was ever received. Maintained by ThreadWatcherList.
+  base::TimeTicks last_critical_memory_pressure_;
+
   // We use this factory to create callback tasks for ThreadWatcher object. We
   // use this during ping-pong messaging between WatchDog thread and watched
   // thread.
@@ -359,6 +369,7 @@ class ThreadWatcherList {
   FRIEND_TEST_ALL_PREFIXES(ThreadWatcherListTest, Restart);
   FRIEND_TEST_ALL_PREFIXES(ThreadWatcherTest, ThreadNamesOnlyArgs);
   FRIEND_TEST_ALL_PREFIXES(ThreadWatcherTest, CrashOnHangThreadsAllArgs);
+  FRIEND_TEST_ALL_PREFIXES(ThreadWatcherCrashKeyTest, MemoryPressureCrashKey);
 
   // This singleton holds the global list of registered ThreadWatchers.
   ThreadWatcherList();
@@ -411,6 +422,10 @@ class ThreadWatcherList {
   // |StopWatchingAll|.
   static void SetStopped(bool stopped);
 
+  // Invoked on memory pressure signal.
+  void OnMemoryPressure(
+      base::MemoryPressureListener::MemoryPressureLevel memory_pressure_level);
+
   // The singleton of this class and is used to keep track of information about
   // threads that are being watched.
   static ThreadWatcherList* g_thread_watcher_list_;
@@ -432,6 +447,9 @@ class ThreadWatcherList {
   // Default value for the delay until |InitializeAndStartWatching| is called.
   // Non-const for tests.
   static int g_initialize_delay_seconds;
+
+  // Registration to receive memory pressure signals.
+  base::MemoryPressureListener memory_pressure_listener_;
 
   // Map of all registered watched threads, from thread_id to ThreadWatcher.
   RegistrationList registered_;
