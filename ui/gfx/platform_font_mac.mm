5dace75b7d832b5cd9f5976e2d93c28c373e1e46
Etienne Bergeron <etienneb@chromium.org>
2020-01-15 17:48:47 +0000

    Fix font fallback unittests for Mac
    This is the same issue than this CL, but for MAC:
     https://chromium-review.googlesource.com/c/chromium/src/+/1824104
    
    The MAC API is returning ".Noto Sans Universal" which is not
    recognized by Skia (i.e. SkTypeface). To work with the API
    that converts codepoint to glyphs, the same NativeFont must be kept
    and pass to the SkTypeface_Mac constructor. The goal is to avoid
    loosing information and keep the exact selected fallback font.
    
    Bug: 1022455
    Change-Id: If15d7a1306131fe2469406b45fb614d3d3bcdb39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994158
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732042}

--- ui/gfx/platform_font_mac.mm
+++ ui/gfx/platform_font_mac.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "third_party/skia/include/ports/SkTypeface_mac.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/font_render_params.h"
@@ -313,7 +314,8 @@ NativeFont PlatformFontMac::GetNativeFont() const {
 }
 
 sk_sp<SkTypeface> PlatformFontMac::GetNativeSkTypefaceIfAvailable() const {
-  return nullptr;
+  return sk_sp<SkTypeface>(
+      SkCreateTypefaceFromCTFont(base::mac::NSToCFCast(GetNativeFont())));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
