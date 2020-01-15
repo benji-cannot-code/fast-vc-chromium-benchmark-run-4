c312160307667c2309526336bc4609aea066dc1b
Caleb Rouleau <crouleau@chromium.org>
2020-01-15 22:46:45 +0000

    [Benchmarking] Put performance_test_suite back on the shard maps
    Fix the path to point to browser_tests since that is the name of the binary.
    
    Bug: 1039019
    Change-Id: Ie9a9dc0671f2cf9a16b6ef2162adafb4176620df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003250
    Auto-Submit: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732171}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -758,11 +758,6 @@ BUILDERS = {
           '--assert-gpu-compositing',
         ],
       },
-      {
-        'isolate': 'performance_browser_tests',
-        'num_shards': 1,
-        'type': TEST_TYPES.GTEST,
-      },
     ],
     'platform': 'mac',
     'dimension': {
