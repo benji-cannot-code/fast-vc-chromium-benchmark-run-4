9634db604a005be78a0ce484bd33bbfb8b6a9492
Peter Kotwicz <pkotwicz@chromium.org>
2020-01-15 20:48:44 +0000

    [Android WebAPK] Make WebApkIconHasher use icon cached by ManifestIconDownloader 1/2
    This CL fixes a regression introduced by the "split cache" feature where
    WebApkIconHasher no longer uses the icon cached via ManifestIconDownloader
    earlier in the WebAPK install process. The regression is fixed by setting
    the network_isolation_key on the content::ResourceRequest
    
    BUG=1036372
    TEST=WebApkIconHasherBrowserTest
    
    Change-Id: Ia82ace8d6f2bf70ab5bf8f79c9b2216a03cc8af7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1998821
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732127}

--- chrome/browser/android/webapk/webapk_icon_hasher.cc
+++ chrome/browser/android/webapk/webapk_icon_hasher.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "net/base/data_url.h"
+#include "net/base/network_isolation_key.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
@@ -92,6 +93,9 @@ WebApkIconHasher::WebApkIconHasher(
                  base::Unretained(this)));
 
   auto resource_request = std::make_unique<network::ResourceRequest>();
+  resource_request->trusted_params = network::ResourceRequest::TrustedParams();
+  resource_request->trusted_params->network_isolation_key =
+      net::NetworkIsolationKey(request_initiator, request_initiator);
   resource_request->request_initiator = request_initiator;
   resource_request->url = icon_url;
   simple_url_loader_ = network::SimpleURLLoader::Create(
