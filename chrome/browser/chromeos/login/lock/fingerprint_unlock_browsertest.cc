47248399fdb60756e548eec512be4c8518387d2c
sangwoo <sangwoo.ko@navercorp.com>
2020-01-15 23:39:27 +0000

    Include header file
    It seems .cc file was included by accident.
    
    Bug: NONE
    Change-Id: I09db820dcd46b6898b5da07ddb7baba0a20d1376
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002270
    Auto-Submit: Sang Woo Ko <sangwoo108@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Sang Woo Ko <sangwoo108@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732188}

--- chrome/browser/chromeos/login/lock/fingerprint_unlock_browsertest.cc
+++ chrome/browser/chromeos/login/lock/fingerprint_unlock_browsertest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/lock/screen_locker.h"
 
-#include "base/test/simple_test_clock.cc"
+#include "base/test/simple_test_clock.h"
 #include "base/test/simple_test_tick_clock.h"
 #include "chrome/browser/chromeos/login/lock/screen_locker_tester.h"
 #include "chrome/browser/chromeos/login/quick_unlock/quick_unlock_factory.h"
