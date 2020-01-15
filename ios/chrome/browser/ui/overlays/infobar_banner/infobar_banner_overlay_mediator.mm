9e616ddc191dacad4c6395ce8ee70ee37df05096
Kurt Horimoto <kkhorimoto@chromium.org>
2020-01-15 00:02:44 +0000

    [iOS] Add coordinator and mediator superclasses for infobar modal UI
    These classes implement the behavior shared amongst all infobar modal
    implementations.  The coordinator packages a modal view configured by
    subclasses into a UINavigationController and presents it modally.  The
    mediator conforms to InfobarModalDelegate and handles those callbacks.
    
    Bug: 1030357
    Change-Id: I2c20f8c545daa5038bcccca3c10d7a24a8b3d2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1995687
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731742}

--- ios/chrome/browser/ui/overlays/infobar_banner/infobar_banner_overlay_mediator.mm
+++ ios/chrome/browser/ui/overlays/infobar_banner/infobar_banner_overlay_mediator.mm
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)infobarBannerWasDismissed {
   // Only needed in legacy implementation.  Dismissal completion cleanup occurs
   // in InfobarBannerOverlayCoordinator.
+  // TODO(crbug.com/1041917): Remove once non-overlay implementation is deleted.
 }
 
 @end
--- ios/chrome/browser/ui/overlays/overlay_request_coordinator.mm
+++ ios/chrome/browser/ui/overlays/overlay_request_coordinator.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/ui/overlays/overlay_request_coordinator.h"
+#import "ios/chrome/browser/ui/overlays/overlay_request_coordinator+subclassing.h"
 
 #include "base/logging.h"
 #include "ios/chrome/browser/overlays/public/overlay_request_support.h"
