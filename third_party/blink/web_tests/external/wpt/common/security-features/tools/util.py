d3e617872892edf1f319ba7948203c233fbcc36e
Hiroshige Hayashizaki <hiroshige@chromium.org>
2020-01-15 09:40:06 +0000

    [WPT/common/security-features] Keep field order of spec.src.json
    For easier debugging for [1]:
    [1] will dump merged spec JSONs into files.
    Keeping the field order makes taking diff easy.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1991066
    
    Bug: 906850
    Change-Id: Ibef14a818a52b4c58ee60272ecb2c1c653842a93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991073
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731900}

--- third_party/blink/web_tests/external/wpt/common/security-features/tools/util.py
+++ third_party/blink/web_tests/external/wpt/common/security-features/tools/util.py
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from __future__ import print_function
 
 import os, sys, json, re
+import collections
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 template_directory = os.path.abspath(
@@ -31,7 +32,7 @@ def load_spec_json(path_to_spec):
     re_error_location = re.compile('line ([0-9]+) column ([0-9]+)')
     with open(path_to_spec, "r") as f:
         try:
-            return json.load(f)
+            return json.load(f, object_pairs_hook=collections.OrderedDict)
         except ValueError as ex:
             print(ex.message)
             match = re_error_location.search(ex.message)
