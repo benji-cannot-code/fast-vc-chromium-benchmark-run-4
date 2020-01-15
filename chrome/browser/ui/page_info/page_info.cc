06e5775b35599812743b889c1b2cd52de67840d2
Alex Cooper <alcooper@chromium.org>
2020-01-15 09:02:31 +0000

    Show AR/VR permission status on Desktop PageInfo
    Shows the status of the AR/VR permissions on the Desktop PageInfo
    bubble.
    
    Android Permissions Bubble and site settings for both will be done in a
    future change.
    
    Bug: 1033592
    Change-Id: I0a4ffebbdb92af19dd06ab4092f1c1b4f507ee82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992583
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731889}

--- chrome/browser/ui/page_info/page_info.cc
+++ chrome/browser/ui/page_info/page_info.cc
@@ -143,6 +143,8 @@ ContentSettingsType kPermissionType[] = {
     ContentSettingsType::NATIVE_FILE_SYSTEM_WRITE_GUARD,
 #endif
     ContentSettingsType::BLUETOOTH_SCANNING,
+    ContentSettingsType::VR,
+    ContentSettingsType::AR,
 };
 
 // Checks whether this permission is currently the factory default, as set by
