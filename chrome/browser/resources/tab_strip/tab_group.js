236a8dc418e46466b09491719d4b7408a0bbe417
John Lee <johntlee@chromium.org>
2020-01-15 05:28:48 +0000

    WebUI Tab Strip: Respond to tab grouped and ungrouped events
    Bug: 1027373
    Change-Id: Ie4acd431ce85b62fc0ff961e81e1f6a64dad5500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2000969
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731844}

--- chrome/browser/resources/tab_strip/tab_group.js
+++ chrome/browser/resources/tab_strip/tab_group.js
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {CustomElement} from './custom_element.js';
 
-class TabGroupElement extends CustomElement {
+export class TabGroupElement extends CustomElement {
   static get template() {
     return `{__html_template__}`;
   }
