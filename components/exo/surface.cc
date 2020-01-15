59deacc48ddc290ac4d675c0797b91cb554e20ff
kylechar <kylechar@chromium.org>
2020-01-15 18:46:44 +0000

    Remove unnecessary CrOS deps on viz service
    This CL removes unnecessary deps/includes and tightens DEPS rules. Ash
    and exo should no longer depend on //components/service/viz outside of
    tests.
    
    Bug: 936425
    Change-Id: I5536a187fb3ef06145e96aafcaf403923873c996
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002749
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732074}

--- components/exo/surface.cc
+++ components/exo/surface.cc
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/surface_draw_quad.h"
 #include "components/viz/common/quads/texture_draw_quad.h"
 #include "components/viz/common/resources/single_release_callback.h"
-#include "components/viz/service/surfaces/surface.h"
-#include "components/viz/service/surfaces/surface_manager.h"
 #include "third_party/khronos/GLES2/gl2.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/aura/client/aura_constants.h"
