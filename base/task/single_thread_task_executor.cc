c66a490cca5c5630d34f242ee0caa3c5f927cf0a
Etienne Pierre-doray <etiennep@chromium.org>
2020-01-15 16:29:24 +0000

    Reland "[base] Migrate the Mac/iOS MessagePump to DoSomeWork"
    This is a reland of de5ec16300f6f2a61049849baabafb9a5b1967d4
    
    Reason for revert: rendering.desktop/frame_times regressions
    Fix: SetWorkBatchSize(2) in gpu_main to keep previous behavior.
    
    Original change's description:
    > [base] Migrate the Mac/iOS MessagePump to DoSomeWork
    >
    > Recycling https://chromium-review.googlesource.com/c/chromium/src/+/1470938/13
    > This CL also fixes DetachToBrowserTabDragControllerTest.PressEscapeWhileDetached
    > to make CQ happy.
    >
    > Bug: 885371
    > Change-Id: I0b54ade1fd3481a9248c89ec2993738b20fce002
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1906337
    > Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#722094}
    
    Bug: 885371, 1032188
    Change-Id: Id1d6916a83e1c72cacc554c100d60fd0c78c6b7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992161
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732010}

--- base/task/single_thread_task_executor.cc
+++ base/task/single_thread_task_executor.cc
@@ -44,4 +44,8 @@ scoped_refptr<SingleThreadTaskRunner> SingleThreadTaskExecutor::task_runner()
   return default_task_queue_->task_runner();
 }
 
+void SingleThreadTaskExecutor::SetWorkBatchSize(size_t work_batch_size) {
+  sequence_manager_->SetWorkBatchSize(work_batch_size);
+}
+
 }  // namespace base
