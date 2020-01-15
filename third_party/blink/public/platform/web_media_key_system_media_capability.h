49bfab5e6a5b9222d5e2afd51d23156098e1f31b
John Rummell <jrummell@chromium.org>
2020-01-15 02:02:41 +0000

    Change EncryptionScheme to match latest spec
    The current version of the EME spec
    (https://w3c.github.io/encrypted-media/) has encryptionScheme as a
    DOMString with an additional value allowed ("cbcs-1-9"). Updating
    the code to match this.
    
    This also moves the feature to stable now that it has been approved.
    
    With this change the W3C test
    (https://wpt.live/encrypted-media/clearkey-check-encryption-scheme.https.html)
    passes.
    
    Bug: 1035153
    Test: Updated media_blink_unittests, browser_tests, and WPT tests pass
    Change-Id: I00cb2ce8a4c4f52f95817471226b5e8129660247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972617
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731780}

--- third_party/blink/public/platform/web_media_key_system_media_capability.h
+++ third_party/blink/public/platform/web_media_key_system_media_capability.h
@@ -15,6 +15,8 @@ struct WebMediaKeySystemMediaCapability {
     kNotSpecified,
     kCenc,
     kCbcs,
+    kCbcs_1_9,  // CBCS with a specific encrypt:skip pattern of 1:9.
+    kUnrecognized
   };
 
   WebMediaKeySystemMediaCapability() = default;
