7fa01d4b58a5750a30f070da336acd27cde2c32d
Isabella Scalzi <isabellascalzi@google.com>
2020-01-15 08:27:58 +0000

    [QuickView] Add multi-selection support to Quick View
    It would be good if Quick View supported multi-select (aka file-list
    check-select mode) reading the bug. Let's fix that.
    
    Add multi-selection support to Quick View. Also remove the now
    irrelevant test "cantOpenQuickViewWithMultipleFiles".
    
    Testing of this feature will be the subject of future patches.
    
    Bug: 1038799
    Change-Id: If64871517bb95a7b403df98560d41c27bccf7748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994548
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#731881}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -469,7 +469,6 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("openQuickViewTabIndexText"),
         TestCase("pressEnterOnInfoBoxToOpenClose"),
         TestCase("closeQuickView"),
-        TestCase("cantOpenQuickViewWithMultipleFiles"),
         TestCase("openQuickViewFromDirectoryTree")));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
