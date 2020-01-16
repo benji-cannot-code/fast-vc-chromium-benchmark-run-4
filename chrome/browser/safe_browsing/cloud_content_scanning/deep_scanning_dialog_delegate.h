6306ab96c8e40985c79c1a5cd0504ba0c357a004
Roger Tawa <rogerta@chromium.org>
2020-01-16 20:15:48 +0000

    Take into account the AllowPasswordProtectedFiles policy in Chrome.
    Bug: 1009158
    Change-Id: I975c196c1f92201c1cb98c646bdffb7e12986ad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1994053
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732524}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
 #include "chrome/browser/ui/tab_modal_confirm_dialog.h"
 #include "chrome/browser/ui/tab_modal_confirm_dialog_delegate.h"
+#include "chrome/common/safe_browsing/archive_analyzer_results.h"
 #include "components/safe_browsing/core/proto/webprotect.pb.h"
 #include "content/public/browser/web_contents_view_delegate.h"
 #include "url/gurl.h"
@@ -130,6 +131,9 @@ class DeepScanningDialogDelegate {
           Data,
           CompletionCallback)>;
 
+  using AnalyzeCallback = base::OnceCallback<void(
+      const safe_browsing::ArchiveAnalyzerResults& results)>;
+
   DeepScanningDialogDelegate(const DeepScanningDialogDelegate&) = delete;
   DeepScanningDialogDelegate& operator=(const DeepScanningDialogDelegate&) =
       delete;
@@ -196,6 +200,16 @@ class DeepScanningDialogDelegate {
   // Uploads data for deep scanning.  Returns true if uploading is occurring in
   // the background and false if there is nothing to do.
   bool UploadData();
+
+  // Prepares an upload request for the file at |path|.  If the file
+  // cannot be uploaded it will have a failure verdict added to |result_|.
+  // Virtual so that it can be overridden in tests.
+  virtual void PrepareFileRequest(base::FilePath path,
+                                  AnalyzeCallback callback);
+
+  // Prepares an upload request for the given file.
+  void AnalyzerCallback(int index,
+                        const safe_browsing::ArchiveAnalyzerResults& results);
 
   // Adds required fields to |request| before sending it to the binary upload
   // service.
