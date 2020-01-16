d789161040ddab26c960f76dd45d86ad6cbbacd4
Antonio Gomes <tonikitoo@igalia.com>
2020-01-16 11:00:22 +0000

    Convert FrameMsg_SetNeedsOcclusionTracking to blink mojom RemoteFrame
    BUG=104106
    R=dcheng@chromium.org, dtapuska@chromium.org, haraken@chromium.org
    
    Change-Id: Ib778b3cdb059dd5079f3b9e7ec61af1bb5107cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002750
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#732342}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -3541,8 +3541,7 @@ void RenderFrameHostImpl::SetNeedsOcclusionTracking(bool needs_tracking) {
     return;
   }
 
-  proxy->Send(new FrameMsg_SetNeedsOcclusionTracking(proxy->GetRoutingID(),
-                                                     needs_tracking));
+  proxy->GetAssociatedRemoteFrame()->SetNeedsOcclusionTracking(needs_tracking);
 }
 
 void RenderFrameHostImpl::LifecycleStateChanged(
--- third_party/blink/renderer/core/exported/web_remote_frame_impl.cc
+++ third_party/blink/renderer/core/exported/web_remote_frame_impl.cc
@@ -320,10 +320,6 @@ void WebRemoteFrameImpl::ForwardResourceTimingToParent(
                           mojo::NullReceiver() /* worker_timing_receiver */);
 }
 
-void WebRemoteFrameImpl::SetNeedsOcclusionTracking(bool needs_tracking) {
-  GetFrame()->View()->SetNeedsOcclusionTracking(needs_tracking);
-}
-
 void WebRemoteFrameImpl::DidStartLoading() {
   GetFrame()->SetIsLoading(true);
 }
