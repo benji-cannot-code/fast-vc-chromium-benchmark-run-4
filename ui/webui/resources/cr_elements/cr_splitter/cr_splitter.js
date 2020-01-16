10c91a27a7386e309d6a1497aec66bf09824d0d4
Demetrios Papadopoulos <dpapad@chromium.org>
2020-01-16 22:52:48 +0000

    WebUI: Stop auto-generating Polymer 3 version of cr-splitter.
    The Polymer 2 version is not used anymore, therefore no auto-generation
    is necessary and the v3 version can be directly checked in.
    
    Fixed: 1042545
    Change-Id: I5054477c0b7427f4ec6b760e5da3eac904af2320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003114
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732590}

--- ui/webui/resources/cr_elements/cr_splitter/cr_splitter.js
+++ ui/webui/resources/cr_elements/cr_splitter/cr_splitter.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-(function() {
+import {Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 // TODO(arv): Currently this only supports horizontal layout.
 // TODO(arv): This ignores min-width and max-width of the elements to the
@@ -40,6 +40,8 @@ function getZoomFactor(doc) {
 
 Polymer({
   is: 'cr-splitter',
+
+  _template: null,
 
   properties: {
     resizeNextElement: {
@@ -267,4 +269,3 @@ Polymer({
     this.classList.remove('splitter-active');
   },
 });
-})();
