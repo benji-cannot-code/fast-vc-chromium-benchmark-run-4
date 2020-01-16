91bf4530afb980588383111362cd51c2de3a365f
Kent Tamura <tkent@chromium.org>
2020-01-16 22:09:46 +0000

    Revert "Move LayoutMenuList::DidUpdateActiveOption() to HTMLSelectElement"
    This reverts commit 3c4c13ac59a6afccb37019ac5fdcdfd237f4db5c.
    
    Reason for revert: Made ChromeVoxBackgroundTest.SelectSingleBasic flaky
    
    Original change's description:
    > Move LayoutMenuList::DidUpdateActiveOption() to HTMLSelectElement
    > 
    > Also,
    >  - Remove LayoutMenuList::DidSelectOption().
    >  - Add HTMLFormControlElement::UpdateFromElement() to add
    >   DidUpdateMenuListActiveOption() to each of
    >   LayoutMenuList::UpdateFromElement() calls.
    > 
    > This CL is a preparation to build a LayoutNG counterpart of LayoutMenuList.
    > This CL has no behavior changes.
    > 
    > Bug: 1040828
    > Change-Id: If7917af101119a830e899a50711a6c1491e22eb3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003164
    > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#732313}
    
    TBR=yosin@chromium.org,tkent@chromium.org,kojii@chromium.org
    
    Change-Id: I4f571b72f01f0494fc855c1ceb3841e7ff02a963
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1040828
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006268
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732571}

--- third_party/blink/renderer/core/html/forms/html_select_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element.cc
@@ -358,7 +358,7 @@ void HTMLSelectElement::OptionElementChildrenChanged(
 
   if (GetLayoutObject()) {
     if (option.Selected() && UsesMenuList())
-      UpdateFromElement();
+      GetLayoutObject()->UpdateFromElement();
     if (AXObjectCache* cache =
             GetLayoutObject()->GetDocument().ExistingAXObjectCache())
       cache->ChildrenChanged(this);
@@ -904,8 +904,8 @@ void HTMLSelectElement::SetSuggestedOption(HTMLOptionElement* option) {
     return;
   suggested_option_ = option;
 
-  if (GetLayoutObject()) {
-    UpdateFromElement();
+  if (LayoutObject* layout_object = GetLayoutObject()) {
+    layout_object->UpdateFromElement();
     ScrollToOption(option);
   }
   if (PopupIsVisible())
@@ -1079,7 +1079,8 @@ void HTMLSelectElement::SelectOption(HTMLOptionElement* element,
   }
 
   // For the menu list case, this is what makes the selected element appear.
-  UpdateFromElement();
+  if (LayoutObject* layout_object = GetLayoutObject())
+    layout_object->UpdateFromElement();
   // PopupMenu::UpdateFromElement() posts an O(N) task.
   if (PopupIsVisible() && should_update_popup)
     popup_->UpdateFromElement(PopupMenu::kBySelectionChange);
@@ -1096,9 +1097,8 @@ void HTMLSelectElement::SelectOption(HTMLOptionElement* element,
       // Need to check UsesMenuList() again because event handlers might
       // change the status.
       if (UsesMenuList()) {
-        // DidUpdateMenuListActiveOption() is O(N) because of
-        // HTMLOptionElement::index().
-        DidUpdateMenuListActiveOption(element);
+        // DidSelectOption() is O(N) because of HTMLOptionElement::index().
+        ToLayoutMenuList(layout_object)->DidSelectOption(element);
       }
     }
   }
@@ -1509,27 +1509,6 @@ void HTMLSelectElement::UpdateSelectedState(HTMLOptionElement* clicked_option,
 
   SetActiveSelectionEnd(clicked_option);
   UpdateListBoxSelection(!multi_select);
-}
-
-void HTMLSelectElement::DidUpdateMenuListActiveOption(
-    HTMLOptionElement* option) {
-  if (!GetDocument().ExistingAXObjectCache())
-    return;
-
-  int option_index = option ? option->index() : -1;
-  if (ax_menulist_last_active_index_ == option_index)
-    return;
-  ax_menulist_last_active_index_ = option_index;
-
-  // We skip sending accessiblity notifications for the very first option,
-  // otherwise we get extra focus and select events that are undesired.
-  if (!has_updated_menulist_active_option_) {
-    has_updated_menulist_active_option_ = true;
-    return;
-  }
-
-  GetDocument().ExistingAXObjectCache()->HandleUpdateActiveMenuOption(
-      ToLayoutMenuList(GetLayoutObject()), option_index);
 }
 
 HTMLOptionElement* HTMLSelectElement::EventTargetOption(const Event& event) {
@@ -2024,7 +2003,8 @@ void HTMLSelectElement::PopupDidHide() {
 
 void HTMLSelectElement::SetIndexToSelectOnCancel(int list_index) {
   index_to_select_on_cancel_ = list_index;
-  UpdateFromElement();
+  if (GetLayoutObject())
+    GetLayoutObject()->UpdateFromElement();
 }
 
 HTMLOptionElement* HTMLSelectElement::OptionToBeShown() const {
@@ -2224,20 +2204,6 @@ void HTMLSelectElement::ChangeRendering() {
   DetachLayoutTree();
   SetNeedsStyleRecalc(kLocalStyleChange, StyleChangeReasonForTracing::Create(
                                              style_change_reason::kControl));
-
-  if (UsesMenuList()) {
-    ax_menulist_last_active_index_ = -1;
-    has_updated_menulist_active_option_ = false;
-  }
-}
-
-void HTMLSelectElement::UpdateFromElement() {
-  auto* layout_object = GetLayoutObject();
-  if (!layout_object)
-    return;
-  layout_object->UpdateFromElement();
-  if (UsesMenuList())
-    DidUpdateMenuListActiveOption(OptionToBeShown());
 }
 
 }  // namespace blink
