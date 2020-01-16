ab128d617ffb00af65fd25659a033d2a54168ba7
Md. Hasanur Rashid <hasanur.r@samsung.com>
2020-01-16 12:36:44 +0000

    [IdentityManager] Return one account from GetAccountForDicePromos()
    This change enforces returning of one AccountInfo based on unconsented
    primary account from signin_ui_util::GetAccountForDicePromos().
    
    Bug: 1006167
    Change-Id: I46dbd8e307210f80b1fe3e9e8539ff026f137334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991043
    Commit-Queue: Md. Hasanur Rashid <hasanur.r@samsung.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732359}

--- chrome/browser/ui/views/profiles/profile_menu_view.h
+++ chrome/browser/ui/views/profiles/profile_menu_view.h
@@ -33,8 +33,8 @@ class Browser;
 class ProfileMenuView : public ProfileMenuViewBase {
  public:
   ProfileMenuView(views::Button* anchor_button,
-                     Browser* browser,
-                     signin_metrics::AccessPoint access_point);
+                  Browser* browser,
+                  signin_metrics::AccessPoint access_point);
   ~ProfileMenuView() override;
 
   // ProfileMenuViewBase:
