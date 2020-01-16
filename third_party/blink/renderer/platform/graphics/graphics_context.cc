4d2ed25ae800458010bf812243d90d2a83b62f3e
Ian Vollick <vollick@chromium.org>
2020-01-16 15:50:54 +0000

    Record URL for video elements in paint previews
    With this change, the paint preview tracker will associate the page URL
    with the video rect.
    
    Bug: 1026308
    Change-Id: I074639face279875e5def0bf158cb53a32f2afdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1999292
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732404}

--- third_party/blink/renderer/platform/graphics/graphics_context.cc
+++ third_party/blink/renderer/platform/graphics/graphics_context.cc
@@ -1419,7 +1419,7 @@ void GraphicsContext::SetURLForRect(const KURL& link,
                                     const IntRect& dest_rect) {
   if (ContextDisabled())
     return;
-  DCHECK(canvas_);
+  DCHECK(canvas_ || tracker_);
 
   // Intercept URL rects when painting previews.
   if (IsPaintingPreview() && tracker_) {
@@ -1436,7 +1436,7 @@ void GraphicsContext::SetURLFragmentForRect(const String& dest_name,
                                             const IntRect& rect) {
   if (ContextDisabled())
     return;
-  DCHECK(canvas_);
+  DCHECK(canvas_ || tracker_);
 
   // Intercept URL rects when painting previews.
   if (IsPaintingPreview() && tracker_) {
