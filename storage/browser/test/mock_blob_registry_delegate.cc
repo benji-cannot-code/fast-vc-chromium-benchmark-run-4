79dab1c4b71d57dd545485d0f9dd9fddd2e234b2
Aaron Colwell <acolwell@chromium.org>
2020-01-16 07:10:59 +0000

    Introduce ChildProcessSecurityPolicyImpl::Handle.
    This change introduces a Handle object so that Mojo services can
    preserve the security state beyond the lifetime of the
    RenderProcessHostImpl object. This allows consistent security
    checks to occur even during the period when the renderer process is
    shutting down and there are still pending Mojo operations in flight.
    This will be used to remove all remaining uses of
    ChildProcessSecurityPolicyImpl::HasSecurityState() in follow-up CLs.
    
    - Implements new Handle object that allows security checks to provide
      consistent results after ChildProcessSecurityPolicyImpl::Remove() is
      called.
    - Convert blob code to use Handle instead of the HasSecurityState()
      workaround.
    
    This is an updated version of https://crrev.com/c/1534368 . Further
    discussion of the history and reasons for this CL can be found there.
    
    Bug: 1035399, 943887
    Change-Id: I6165fad4308643a1ddc845690443e8efceac65f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1975165
    Reviewed-by: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732296}

--- storage/browser/test/mock_blob_registry_delegate.cc
+++ storage/browser/test/mock_blob_registry_delegate.cc
@@ -16,8 +16,5 @@ bool MockBlobRegistryDelegate::CanReadFileSystemFile(const FileSystemURL& url) {
 bool MockBlobRegistryDelegate::CanCommitURL(const GURL& url) {
   return can_commit_url_result;
 }
-bool MockBlobRegistryDelegate::IsProcessValid() {
-  return is_process_valid_result;
-}
 
 }  // namespace storage
