948b7edcb7f669c86c4de32c30e3496ada1896c5
Vladimir Levin <vmpstr@chromium.org>
2020-01-16 17:49:27 +0000

    DL/TextFragment: Ensure that we can find text in locked subtrees.
    This patch ensures that we don't enter infinite recursion when trying
    to text-fragment navigate in a document that has locked subtrees.
    
    R=bokan@chromium.org, chrishtr@chromium.org
    
    Bug: 1041866
    Change-Id: I6c0a7be555fb8b78a8332ccbfe9fc09e48c9d7ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2001389
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732451}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -2158,6 +2158,8 @@ class CORE_EXPORT Document : public ContainerNode,
   Member<IntersectionObserver> display_lock_activation_observer_;
 
   bool in_forced_colors_mode_;
+
+  bool applying_scroll_restoration_logic_ = false;
 };
 
 extern template class CORE_EXTERN_TEMPLATE_EXPORT Supplement<Document>;
