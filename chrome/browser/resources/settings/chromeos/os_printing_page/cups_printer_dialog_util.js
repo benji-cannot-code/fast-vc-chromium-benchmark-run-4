d48ace5f37b0de146a9083130ceb47788bc9fd74
Jimmy Gong <jimmyxgong@google.com>
2020-01-16 19:11:22 +0000

    Move findDifference to printer utils
    - Gives shared access to the findDifference function.
    
    Bug: 1015628
    Change-Id: I25fe0954df1e3a6aa784cce9611f2f14086e1f89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2004008
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: jimmy gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732495}

--- chrome/browser/resources/settings/chromeos/os_printing_page/cups_printer_dialog_util.js
+++ chrome/browser/resources/settings/chromeos/os_printing_page/cups_printer_dialog_util.js
@@ -193,6 +193,19 @@ cr.define('settings.printing', function() {
     return first.printerInfo.printerId == second.printerInfo.printerId;
   }
 
+  /**
+   * Finds the printers that are in |firstArr| but not in |secondArr|.
+   * @param {!Array<!PrinterListEntry>} firstArr
+   * @param {!Array<!PrinterListEntry>} secondArr
+   * @return {!Array<!PrinterListEntry>}
+   */
+  function findDifference(firstArr, secondArr) {
+    return firstArr.filter(p1 => {
+      return !secondArr.some(
+          p2 => p2.printerInfo.printerId == p1.printerInfo.printerId);
+    });
+  }
+
   return {
     isNetworkProtocol: isNetworkProtocol,
     isNameAndAddressValid: isNameAndAddressValid,
@@ -204,5 +217,6 @@ cr.define('settings.printing', function() {
     sortPrinters: sortPrinters,
     matchesSearchTerm: matchesSearchTerm,
     arePrinterIdsEqual: arePrinterIdsEqual,
+    findDifference: findDifference,
   };
 });
