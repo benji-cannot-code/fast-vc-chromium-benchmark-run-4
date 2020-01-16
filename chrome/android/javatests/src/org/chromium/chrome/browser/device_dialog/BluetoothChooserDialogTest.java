f4e7556d2c91d59a83c6573f5bd5e0a7793f0db2
Theresa Wellington <twellington@chromium.org>
2020-01-16 02:29:28 +0000

    Disable BluetoothChooserDialogTest#testSelectItem on KitKat
    BUG=1042092
    TBR=juncai@chromium.org
    
    Change-Id: I5ed0d8b0ce326b216fc9527faff72c008292b16b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003302
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732235}

--- chrome/android/javatests/src/org/chromium/chrome/browser/device_dialog/BluetoothChooserDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/device_dialog/BluetoothChooserDialogTest.java
@@ -10,6 +10,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
+import android.os.Build;
 import android.support.test.filters.LargeTest;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
@@ -223,6 +225,8 @@ public class BluetoothChooserDialogTest {
 
     @Test
     @LargeTest
+    @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.LOLLIPOP,
+            message = "https://crbug.com/1042092")
     public void testSelectItem() {
         Dialog dialog = mChooserDialog.mItemChooserDialog.getDialogForTesting();
 
