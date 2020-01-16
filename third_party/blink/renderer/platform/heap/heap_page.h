7c451fc044cd91bc4341ed23b198962a6de61bf5
Anton Bikineev <bikineev@chromium.org>
2020-01-16 12:34:24 +0000

    heap: Implement unmarking phase
    Unmarking phase must be called before major GC to clear all mark bits.
    
    Design doc: https://bit.ly/2ZXGU20
    
    Bug: 1029379
    Change-Id: Ifcfa4af9710f72b9bdea24e89c2c613d0f34eaeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003308
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732357}

--- third_party/blink/renderer/platform/heap/heap_page.h
+++ third_party/blink/renderer/platform/heap/heap_page.h
@@ -467,6 +467,7 @@ class BasePage {
   // Does not create free list entries for empty pages.
   virtual bool Sweep(FinalizeType) = 0;
   virtual void MakeConsistentForMutator() = 0;
+  virtual void Unmark() = 0;
 
   // Calls finalizers after sweeping is done.
   virtual void FinalizeSweep(SweepResult) = 0;
@@ -654,6 +655,7 @@ class PLATFORM_EXPORT NormalPage final : public BasePage {
   void RemoveFromHeap() override;
   bool Sweep(FinalizeType) override;
   void MakeConsistentForMutator() override;
+  void Unmark() override;
   void FinalizeSweep(SweepResult) override;
 #if defined(ADDRESS_SANITIZER)
   void PoisonUnmarkedObjects() override;
@@ -880,6 +882,7 @@ class PLATFORM_EXPORT LargeObjectPage final : public BasePage {
   void RemoveFromHeap() override;
   bool Sweep(FinalizeType) override;
   void MakeConsistentForMutator() override;
+  void Unmark() override;
   void FinalizeSweep(SweepResult) override;
 
   void CollectStatistics(
@@ -946,6 +949,7 @@ class PLATFORM_EXPORT BaseArena {
   virtual void MakeIterable() {}
   virtual void MakeConsistentForGC();
   void MakeConsistentForMutator();
+  void Unmark();
 #if DCHECK_IS_ON()
   virtual bool IsConsistentForGC() = 0;
 #endif
