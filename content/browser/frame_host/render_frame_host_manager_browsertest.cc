4f6823e101b7e58ebe9600253c8eb2fe3ca65a6b
Aaron Colwell <acolwell@chromium.org>
2020-01-16 02:17:27 +0000

    Make noopener partition selection consistent with normal path.
    Fixing an inconsistency in the partition selection in
    WebContentsImpl::CreateNewWindow(). The original code appeared to be
    conflating SiteInstance selection with partition_id selection. This was
    causing crashes when a new SiteInstance, created by the target_url
    in the noopener case, resulted in a site URL that had a different
    storage partition ID.(e.g. chrome-guest://blah). This change avoids
    the crashes by separating the 2 concerns. partition_id selection is
    now the same between noopener and normal paths. The special noopener
    SiteInstance behavior is now handled by not providing a SiteInstance
    in the CreateParams. This triggers the creation of a new SiteInstance,
    that does not share a BrowsingInstance with the original source
    SiteInstance. It also defers setting the site URL until we get the network
    response and are ready to commit. This provides the desired behavior
    and makes this case more consistent with other navigation scenarios.
    
    Bug: 965611
    Change-Id: I054c2f793138a2b373bac6c61b0a85368f0adb4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949285
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Auto-Submit: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732229}

--- content/browser/frame_host/render_frame_host_manager_browsertest.cc
+++ content/browser/frame_host/render_frame_host_manager_browsertest.cc
@@ -395,6 +395,8 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
   scoped_refptr<SiteInstance> noref_blank_site_instance(
       new_shell->web_contents()->GetSiteInstance());
   EXPECT_NE(orig_site_instance, noref_blank_site_instance);
+  EXPECT_FALSE(noref_blank_site_instance->IsRelatedSiteInstance(
+      orig_site_instance.get()));
 }
 
 // Same as above, but for 'noopener'
@@ -448,6 +450,8 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
   scoped_refptr<SiteInstance> noopener_blank_site_instance(
       new_shell->web_contents()->GetSiteInstance());
   EXPECT_NE(orig_site_instance, noopener_blank_site_instance);
+  EXPECT_FALSE(noopener_blank_site_instance->IsRelatedSiteInstance(
+      orig_site_instance.get()));
 }
 
 // 'noopener' also works from 'window.open'
@@ -505,6 +509,8 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
   scoped_refptr<SiteInstance> noopener_blank_site_instance(
       new_shell->web_contents()->GetSiteInstance());
   EXPECT_NE(orig_site_instance, noopener_blank_site_instance);
+  EXPECT_FALSE(noopener_blank_site_instance->IsRelatedSiteInstance(
+      orig_site_instance.get()));
 }
 
 // As of crbug.com/69267, we create a new BrowsingInstance (and SiteInstance)
@@ -551,6 +557,8 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
   scoped_refptr<SiteInstance> noref_blank_site_instance(
       new_shell->web_contents()->GetSiteInstance());
   EXPECT_NE(orig_site_instance, noref_blank_site_instance);
+  EXPECT_FALSE(noref_blank_site_instance->IsRelatedSiteInstance(
+      orig_site_instance.get()));
 }
 
 // Same as above, but for 'noopener'
@@ -596,6 +604,8 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
   scoped_refptr<SiteInstance> noref_blank_site_instance(
       new_shell->web_contents()->GetSiteInstance());
   EXPECT_NE(orig_site_instance, noref_blank_site_instance);
+  EXPECT_FALSE(noref_blank_site_instance->IsRelatedSiteInstance(
+      orig_site_instance.get()));
 }
 
 // Test for crbug.com/24447.  Following a cross-site link with just
