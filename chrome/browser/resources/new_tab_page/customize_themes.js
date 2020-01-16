22d9e27039dae6df58bbe4129d106d112b24083b
Tibor Goldschwendt <tiborg@chromium.org>
2020-01-16 20:42:44 +0000

    [webui-ntp] Move grid of themes into custom element to be reused later
    This prepares for reusing the grid for the backgrounds settings. Also,
    this CL adds support for non-full (i.e. the last row has empty spaces)
    grids.
    
    Bug: 1032328
    Change-Id: Iab76c9bce52b832833d09301c560963b9ab2618c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003294
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732535}

--- chrome/browser/resources/new_tab_page/customize_themes.js
+++ chrome/browser/resources/new_tab_page/customize_themes.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import './grid.js';
 import './theme_icon.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -31,13 +32,6 @@ class CustomizeThemesElement extends PolymerElement {
 
       /** @private {!Array<!newTabPage.mojom.ChromeTheme>} */
       chromeThemes_: Array,
-
-      /** @private {number} */
-      numThemeColumns_: {
-        type: Number,
-        readOnly: true,
-        value: 6,
-      },
     };
   }
 
@@ -152,46 +146,6 @@ class CustomizeThemesElement extends PolymerElement {
    */
   skColorToRgb_(skColor) {
     return skColorToRgb(skColor);
-  }
-
-  /**
-   * @param {!Event} e
-   * @private
-   */
-  onThemesKeyDown_(e) {
-    if (['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'].includes(e.key)) {
-      e.preventDefault();
-      const themeIcons = Array.from(this.shadowRoot.querySelectorAll(
-          '#themesContainer > :-webkit-any(div, ntp-theme-icon)'));
-      const currentIndex = themeIcons.indexOf(e.target);
-      const isRtl = window.getComputedStyle(this)['direction'] === 'rtl';
-      let delta = 0;
-      switch (e.key) {
-        case 'ArrowLeft':
-          delta = isRtl ? 1 : -1;
-          break;
-        case 'ArrowRight':
-          delta = isRtl ? -1 : 1;
-          break;
-        case 'ArrowUp':
-          delta = -this.numThemeColumns_;
-          break;
-        case 'ArrowDown':
-          delta = this.numThemeColumns_;
-          break;
-      }
-      const mod = function(m, n) {
-        return ((m % n) + n) % n;
-      };
-      const newIndex = mod(currentIndex + delta, themeIcons.length);
-      themeIcons[newIndex].focus();
-    }
-
-    if (['Enter', ' '].includes(e.key)) {
-      e.preventDefault();
-      e.stopPropagation();
-      e.target.click();
-    }
   }
 }
 
