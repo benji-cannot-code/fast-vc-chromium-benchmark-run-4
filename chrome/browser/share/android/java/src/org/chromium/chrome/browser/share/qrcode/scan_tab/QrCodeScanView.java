08f181bfb78bf3ee9d6a2c91ae2b3ba41617ee82
Gayane Petrosyan <gayane@chromium.org>
2020-01-16 18:05:18 +0000

    [QRCode Android] Add camera preview overlay.
    screenshot: https://screenshot.googleplex.com/7195BT4gJFQ.png
    
    Bug: 993920
    Change-Id: Id6d013e7046c8cdb59c0cbf79836188fb4d9962c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953568
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732460}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/scan_tab/QrCodeScanView.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/scan_tab/QrCodeScanView.java
@@ -72,6 +72,7 @@ class QrCodeScanView {
         if (mHasCameraPermission) {
             mCameraPreview = new CameraPreview(mContext, mCameraCallback);
             mView.addView(mCameraPreview);
+            mView.addView(new CameraPreviewOverlay(mContext));
 
             updateCameraPreviewState();
         }
