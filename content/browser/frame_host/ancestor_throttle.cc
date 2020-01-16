9f38d53bace935b7f52ce969f28625b1cbcca68c
Jeremy Roman <jbroman@chromium.org>
2020-01-16 16:48:49 +0000

    Use the portal-host-including parent for console logging in AncestorThrottle.
    This is already used for determining whether ancestors are permitted.
    It needs to be used also for figuring out which ancestor's console to
    log in.
    
    A web test that reaches this code using X-Frame-Options: DENY is included.
    Without the change to content/, this test fails with the same stack trace
    as in the linked bug.
    
    Bug: 1038256
    Change-Id: Iad3623d82e0b2192eab626a2184d09d9d3e7be62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002967
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732421}

--- content/browser/frame_host/ancestor_throttle.cc
+++ content/browser/frame_host/ancestor_throttle.cc
@@ -133,6 +133,12 @@ class FrameAncestorCSPContext : public CSPContext {
   RenderFrameHostImpl* navigated_frame_;
 };
 
+// Returns the parent, including outer delegates in the case of portals.
+RenderFrameHostImpl* ParentForAncestorThrottle(RenderFrameHostImpl* frame) {
+  return frame->InsidePortal() ? frame->ParentOrOuterDelegateFrame()
+                               : frame->GetParent();
+}
+
 }  // namespace
 
 // static
@@ -215,9 +221,7 @@ NavigationThrottle::ThrottleCheckResult AncestorThrottle::ProcessResponseImpl(
       // We enforce frame-ancestors in the outer delegate for portals, but not
       // for other uses of inner/outer WebContents (GuestViews).
       RenderFrameHostImpl* parent =
-          is_portal
-              ? request->GetRenderFrameHost()->ParentOrOuterDelegateFrame()
-              : request->GetRenderFrameHost()->GetParent();
+          ParentForAncestorThrottle(request->GetRenderFrameHost());
       while (parent) {
         if (!csp_context.IsAllowedByCsp(
                 network::mojom::CSPDirectiveName::FrameAncestors,
@@ -228,11 +232,7 @@ NavigationThrottle::ThrottleCheckResult AncestorThrottle::ProcessResponseImpl(
                 navigation_handle()->IsFormSubmission())) {
           return NavigationThrottle::BLOCK_RESPONSE;
         }
-        if (parent->InsidePortal()) {
-          parent = parent->ParentOrOuterDelegateFrame();
-        } else {
-          parent = parent->GetParent();
-        }
+        parent = ParentForAncestorThrottle(parent);
       }
       return NavigationThrottle::PROCEED;
     }
@@ -335,7 +335,9 @@ void AncestorThrottle::ParseError(const std::string& value,
 
   // Log a console error in the parent of the current RenderFrameHost (as
   // the current RenderFrameHost itself doesn't yet have a document).
-  navigation_handle()->GetRenderFrameHost()->GetParent()->AddMessageToConsole(
+  auto* frame = static_cast<RenderFrameHostImpl*>(
+      navigation_handle()->GetRenderFrameHost());
+  ParentForAncestorThrottle(frame)->AddMessageToConsole(
       blink::mojom::ConsoleMessageLevel::kError, message);
 }
 
@@ -353,7 +355,9 @@ void AncestorThrottle::ConsoleError(HeaderDisposition disposition) {
 
   // Log a console error in the parent of the current RenderFrameHost (as
   // the current RenderFrameHost itself doesn't yet have a document).
-  navigation_handle()->GetRenderFrameHost()->GetParent()->AddMessageToConsole(
+  auto* frame = static_cast<RenderFrameHostImpl*>(
+      navigation_handle()->GetRenderFrameHost());
+  ParentForAncestorThrottle(frame)->AddMessageToConsole(
       blink::mojom::ConsoleMessageLevel::kError, message);
 }
 
