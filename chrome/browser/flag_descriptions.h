281a3dff7a55416817cc0384135bddd27e9e424c
David Munro <davidmunro@google.com>
2020-01-17 08:41:08 +0000

    crostini: Add a feature flag for username picking.
    Bug: chromium:914515
    Test: CQ
    Change-Id: Ibfacf91020b5bd80c905967accb24c438c0293b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006838
    Commit-Queue: David Munro <davidmunro@google.com>
    Commit-Queue: Julian Watson <juwa@google.com>
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Julian Watson <juwa@google.com>
    Cr-Commit-Position: refs/heads/master@{#732759}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2008,6 +2008,9 @@ extern const char kCrostiniPortForwardingDescription[];
 extern const char kCrostiniDiskResizingName[];
 extern const char kCrostiniDiskResizingDescription[];
 
+extern const char kCrostiniUsernameName[];
+extern const char kCrostiniUsernameDescription[];
+
 extern const char kCrostiniUseBusterImageName[];
 extern const char kCrostiniUseBusterImageDescription[];
 
