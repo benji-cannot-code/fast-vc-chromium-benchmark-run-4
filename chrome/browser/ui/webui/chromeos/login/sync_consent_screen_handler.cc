95ccfb038939e7bb6c2dd289c56032452257786a
James Cook <jamescook@chromium.org>
2020-01-17 03:51:31 +0000

    chromeos: Update strings for OOBE OS sync consent screen
    We now have a final mock at go/cros-sync-mock, so update the strings
    to match. These strings are used by the consent auditor system, so
    add back the consent-description tags. This requires a workaround for
    the "Sync your Chromebook" string, since the device type is
    set at runtime.
    
    Bug: 1023854
    Test: browser_tests
    Change-Id: Idddb83ff66a14922a57303f2064cfb4440791977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006135
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732695}

--- chrome/browser/ui/webui/chromeos/login/sync_consent_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/sync_consent_screen_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/constants/chromeos_features.h"
 #include "components/login/localized_values_builder.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/chromeos/devicetype_utils.h"
 
 namespace {
 
@@ -30,13 +31,20 @@ void GetConsentIDs(const std::unordered_set<int>& known_ids,
     // characters, so we must use base::ReplaceSubstrings* rather than
     // base::ReplaceChars.
     // TODO(alemate): Find a more elegant solution.
-    std::string sanitized_string =
-        base::UTF16ToUTF8(l10n_util::GetStringUTF16(id));
+    base::string16 raw_string;
+    if (id == IDS_LOGIN_OS_SYNC_CONSENT_TOGGLE_NAME) {
+      raw_string = ui::SubstituteChromeOSDeviceType(
+          IDS_LOGIN_OS_SYNC_CONSENT_TOGGLE_NAME);
+    } else {
+      raw_string = l10n_util::GetStringUTF16(id);
+    }
+    std::string sanitized_string = base::UTF16ToUTF8(raw_string);
     base::ReplaceSubstringsAfterOffset(&sanitized_string, 0,
                                        "\u00A0" /* NBSP */, "&nbsp;");
 
     known_strings[sanitized_string] = id;
   }
+
   // The strings returned by the WebUI are not free-form, they must belong into
   // a pre-determined set of strings (stored in |string_to_grd_id_map_|). As
   // this has privacy and legal implications, CHECK the integrity of the strings
@@ -102,6 +110,19 @@ void SyncConsentScreenHandler::DeclareLocalizedValues(
   RememberLocalizedValue("syncConsentAcceptAndContinue",
                          IDS_LOGIN_SYNC_CONSENT_SCREEN_ACCEPT_AND_CONTINUE,
                          builder);
+
+  // SplitSettingsSync strings. The version of the dialog to show is chosen
+  // after the WebUI is loaded, so always supply both sets of strings.
+  RememberLocalizedValue("osSyncConsentTitle", IDS_LOGIN_OS_SYNC_CONSENT_TITLE,
+                         builder);
+  known_string_ids_.insert(IDS_LOGIN_OS_SYNC_CONSENT_TOGGLE_NAME);
+  builder->AddF("osSyncConsentToggleName",
+                IDS_LOGIN_OS_SYNC_CONSENT_TOGGLE_NAME,
+                ui::GetChromeOSDeviceName());
+  RememberLocalizedValue("osSyncConsentToggleDescription",
+                         IDS_LOGIN_OS_SYNC_CONSENT_TOGGLE_DESCRIPTION, builder);
+  RememberLocalizedValue("osSyncConsentContinue",
+                         IDS_LOGIN_OS_SYNC_CONSENT_CONTINUE, builder);
 }
 
 void SyncConsentScreenHandler::Bind(SyncConsentScreen* screen) {
