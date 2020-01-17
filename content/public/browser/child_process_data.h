11b645bf24510ac9e53a5f058c4ded84dfe22bd5
Alex Gough <ajgo@chromium.org>
2020-01-17 05:13:58 +0000

    Adds SandboxType to chrome://sandbox on Windows
    This should be useful as more services with finer grained sandboxing
    are introduced.
    
    Bug: 997273
    Change-Id: I2996c8b32fa9f46bd1a32aa7b80d99d094f020a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006129
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732724}

--- content/public/browser/child_process_data.h
+++ content/public/browser/child_process_data.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process.h"
 #include "base/strings/string16.h"
 #include "content/common/content_export.h"
+#include "content/public/browser/sandbox_type.h"
 
 namespace content {
 
@@ -29,6 +30,10 @@ struct CONTENT_EXPORT ChildProcessData {
   // process ID, and will be unique for all types of child process for
   // one run of the browser.
   int id = 0;
+
+  // The content::SandboxType that this process was launched at. May be
+  // invalid prior to process launch.
+  content::SandboxType sandbox_type;
 
   const base::Process& GetProcess() const { return process_; }
   // Since base::Process is non-copyable, the caller has to provide a rvalue.
