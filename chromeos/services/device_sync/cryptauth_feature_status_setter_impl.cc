8a31cf3d4d0e9117ae22a653f94844ffda0d282a
Josh Nohle <nohle@google.com>
2020-01-17 01:50:11 +0000

    [DeviceSync v2] Add metrics to CryptAuth feature status setter
    Bug: 951969, 936273, 933656
    Change-Id: I9101d47dddd434f9a4c756bc3153d2980d2b2686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1980508
    Commit-Queue: Josh Nohle <nohle@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732658}

--- chromeos/services/device_sync/cryptauth_feature_status_setter_impl.cc
+++ chromeos/services/device_sync/cryptauth_feature_status_setter_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "chromeos/components/multidevice/logging/logging.h"
 #include "chromeos/services/device_sync/async_execution_time_metrics_logger.h"
@@ -25,7 +26,7 @@ namespace device_sync {
 
 namespace {
 
-// Timeout values for asynchronous operation.
+// Timeout values for asynchronous operations.
 // TODO(https://crbug.com/933656): Use async execution time metric to tune these
 // timeout values.
 constexpr base::TimeDelta kWaitingForClientAppMetadataTimeout =
@@ -35,14 +36,27 @@ constexpr base::TimeDelta kWaitingForBatchSetFeatureStatusesResponseTimeout =
 
 void RecordClientAppMetadataFetchMetrics(const base::TimeDelta& execution_time,
                                          CryptAuthAsyncTaskResult result) {
-  // TODO(https://crbug.com/933656, https://crbug.com/936273): Add metrics to
-  // track async execution times and failure rates due to async timeouts.
+  base::UmaHistogramCustomTimes(
+      "CryptAuth.DeviceSyncV2.FeatureStatusSetter.ExecutionTime."
+      "ClientAppMetadataFetch",
+      execution_time, base::TimeDelta::FromSeconds(1) /* min */,
+      kWaitingForClientAppMetadataTimeout /* max */, 100 /* buckets */);
+  LogCryptAuthAsyncTaskSuccessMetric(
+      "CryptAuth.DeviceSyncV2.FeatureStatusSetter.AsyncTaskResult."
+      "ClientAppMetadataFetch",
+      result);
 }
 
 void RecordBatchSetFeatureStatusesMetrics(const base::TimeDelta& execution_time,
                                           CryptAuthApiCallResult result) {
-  // TODO(https://crbug.com/933656, https://crbug.com/936273): Add metrics to
-  // track async execution times and failure rates due to async timeouts.
+  LogAsyncExecutionTimeMetric(
+      "CryptAuth.DeviceSyncV2.FeatureStatusSetter.ExecutionTime."
+      "SetFeatureStatuses",
+      execution_time);
+  LogCryptAuthApiCallSuccessMetric(
+      "CryptAuth.DeviceSyncV2.FeatureStatusSetter.ApiCallResult."
+      "SetFeatureStatuses",
+      result);
 }
 
 }  // namespace
