8a1a8b508b90b875df36ea2d613f3d6a86192d13
Hiroshi Ichikawa <ichikawa@chromium.org>
2020-01-17 20:36:40 +0000

    Delete an unused forward declaration.
    Change-Id: Ieef9a582007b963922931bc607289d7a300bddc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006439
    Reviewed-by: John Wu <jzw@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732994}

--- ios/web_view/public/cwv_navigation_delegate.h
+++ ios/web_view/public/cwv_navigation_delegate.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 NS_ASSUME_NONNULL_BEGIN
 
-@protocol CRIWVTranslateDelegate;
 @class CWVDownloadTask;
 @class CWVSSLStatus;
 @class CWVWebView;
