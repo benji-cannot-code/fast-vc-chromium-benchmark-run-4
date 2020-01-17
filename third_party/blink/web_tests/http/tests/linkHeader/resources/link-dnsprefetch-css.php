bfeab7fa62ac1d8ac03cc02fec741c0614af43cb
Rob Buis <rbuis@igalia.com>
2020-01-17 13:45:28 +0000

    Fix linkHeader tests
    Update some title descriptions and provide a proper stylesheet
    resource for link-dnsprefetch-in-css.html.
    
    Bug: 456173
    Change-Id: I06d2982ded73d6be1e1c9a829c764e3e4a4dfc90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002871
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#732836}

---
+++ third_party/blink/web_tests/http/tests/linkHeader/resources/link-dnsprefetch-css.php
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("Link: <   http://wut.com.test/>; rel=dns-prefetch");
+header("Content-Type: text/css");
+?>
+div { }
