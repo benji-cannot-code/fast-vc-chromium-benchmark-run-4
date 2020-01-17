a75fb3660ba1730b48d127ee2d4a7488df2fe7cc
Nate Fischer <ntfschr@chromium.org>
2020-01-17 22:45:51 +0000

    AW: implement basic dev UI metrics
    No change to logic. This implements 2 histograms:
    
     - Android.WebView.DevUi.DeveloperModeEnabled tracks whether or not
       Developer Mode is enabled.
     - Android.WebView.DevUi.ToggledFlagCount tracks the number of flags we
       will toggle on this run of WebView.
    
    Bug: 1001185
    Test: None (this is in the glue layer, so it's not testable)
    Change-Id: Ic2872e96005a0d24061b678e19e7d5f81bd0f27a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2004064
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733044}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
@@ -51,6 +51,8 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.metrics.CachedMetrics;
+import org.chromium.base.metrics.CachedMetrics.BooleanHistogramSample;
+import org.chromium.base.metrics.CachedMetrics.Count100HistogramSample;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.ScopedSysTraceEvent;
 import org.chromium.base.task.PostTask;
@@ -58,6 +60,8 @@ import org.chromium.base.task.TaskTraits;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
 import org.chromium.net.NetworkChangeNotifier;
 import org.chromium.ui.base.ResourceBundle;
+
+import java.util.Map;
 
 /**
  * Class controlling the Chromium initialization for WebView.
@@ -177,10 +181,21 @@ public class WebViewChromiumAwInit {
             finishVariationsInitLocked();
 
             String webViewPackageName = AwBrowserProcess.getWebViewPackageName();
-            if (DeveloperModeUtils.isDeveloperModeEnabled(webViewPackageName)) {
+            boolean isDeveloperModeEnabled =
+                    DeveloperModeUtils.isDeveloperModeEnabled(webViewPackageName);
+            final BooleanHistogramSample developerModeSample =
+                    new BooleanHistogramSample("Android.WebView.DevUi.DeveloperModeEnabled");
+            developerModeSample.record(isDeveloperModeEnabled);
+            if (isDeveloperModeEnabled) {
                 FlagOverrideHelper helper =
                         new FlagOverrideHelper(ProductionSupportedFlagList.sFlagList);
-                helper.applyFlagOverrides(DeveloperModeUtils.getFlagOverrides(webViewPackageName));
+                Map<String, Boolean> flagOverrides =
+                        DeveloperModeUtils.getFlagOverrides(webViewPackageName);
+                helper.applyFlagOverrides(flagOverrides);
+
+                final Count100HistogramSample flagOverrideSample =
+                        new Count100HistogramSample("Android.WebView.DevUi.ToggledFlagCount");
+                flagOverrideSample.record(flagOverrides.size());
             }
 
             AwBrowserProcess.start();
