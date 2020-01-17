5facf700a3fd25db49a98daea9d858cdea89b543
Rachel Sugrono <rsugrono@google.com>
2020-01-17 06:30:03 +0000

    [quickview] Test that Quick View opens with check-select
    Add two non-consecutive files to a check-select (aka a multi-
    selection). Quick View should open in check-select mode with
    the first file being displayed.
    
    Bug: 1038799
    Change-Id: I1fada68b30ae3bd48df69d96669255aa166787ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006892
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732738}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -470,6 +470,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("openQuickViewTabIndexAudio"),
         TestCase("openQuickViewTabIndexVideo"),
         TestCase("pressEnterOnInfoBoxToOpenClose"),
+        TestCase("openQuickViewWithMultipleFiles"),
         TestCase("closeQuickView"),
         TestCase("openQuickViewFromDirectoryTree")));
 
