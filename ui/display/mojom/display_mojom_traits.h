1b03168d685035c59c3ee6a5ce28445b652d4a79
Christopher Cameron <ccameron@chromium.org>
2020-01-17 16:36:50 +0000

    Add gfx::DisplayColorSpaces and use in display::Display
    The concept of "the color space of the display" falls apart on several
    platforms, because display (or OS) can be configured to handle multiple
    color spaces. Rather than sending a single color space, send a set of
    color spaces to use in various situations (sRGB-only content, SDR WCG
    content, and HDR content).
    
    Use this gfx::DisplayColorSpaces structure in display::Display, instead
    of using gfx::ColorSpace and sdr_white_point. Also communicate this
    via mojo, which had not been done in the past.
    
    For now, only use the "HDR transparent" color space. The next patch
    use this structure in ui::Compositor, viz::Display, and
    viz::SurfaceAggregator. This structure is added to gfx because it is
    imported from these several locations.
    
    Bug: 1037954, 1038770, 1038494, 958166
    Change-Id: I677e3f5c4d01deeb95f76b6463541eec2163b48b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006068
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732874}

--- ui/display/mojom/display_mojom_traits.h
+++ ui/display/mojom/display_mojom_traits.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display.h"
 #include "ui/display/mojom/display.mojom.h"
 #include "ui/gfx/geometry/mojom/geometry_mojom_traits.h"
+#include "ui/gfx/mojom/display_color_spaces_mojom_traits.h"
 
 namespace mojo {
 
@@ -73,6 +74,10 @@ struct StructTraits<display::mojom::DisplayDataView, display::Display> {
 
   static const gfx::Size& maximum_cursor_size(const display::Display& display) {
     return display.maximum_cursor_size();
+  }
+
+  static gfx::DisplayColorSpaces color_spaces(const display::Display& display) {
+    return display.color_spaces();
   }
 
   static int32_t color_depth(const display::Display& display) {
