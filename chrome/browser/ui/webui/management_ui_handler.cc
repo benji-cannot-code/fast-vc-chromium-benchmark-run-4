94f576dbef191047740077c02ea359246f51773c
Anqing Zhao <anqing@google.com>
2020-01-17 00:31:47 +0000

    Add privacy notifications for CBCM reporting
    After CBCM reporting is online, we need notify users that following
    additional data will be collected for privacy purpose on
    chrome://management page.
    - User account information.
    - Installed extensions.
    - Installed Android applications.
    
    Bug: 1010213
    Change-Id: I38dc2094768766f06cbea69f7b2ce158ecf1425e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992067
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@google.com>
    Cr-Commit-Position: refs/heads/master@{#732634}

--- chrome/browser/ui/webui/management_ui_handler.cc
+++ chrome/browser/ui/webui/management_ui_handler.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/callback.h"
+#include "base/feature_list.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/pref_names.h"
 
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
@@ -132,6 +134,9 @@ const char kManagementReportHardwareStatus[] = "managementReportHardwareStatus";
 const char kManagementReportNetworkInterfaces[] =
     "managementReportNetworkInterfaces";
 const char kManagementReportUsers[] = "managementReportUsers";
+const char kManagementReportExtensions[] = "managementReportExtensions";
+const char kManagementReportAndroidApplications[] =
+    "managementReportAndroidApplications";
 const char kManagementPrinting[] = "managementPrinting";
 const char kManagementCrostini[] = "managementCrostini";
 const char kAccountManagedInfo[] = "accountManagedInfo";
@@ -171,7 +176,10 @@ enum class DeviceReportingType {
   kDevice,
   kLogs,
   kPrint,
-  kCrostini
+  kCrostini,
+  kUsername,
+  kExtensions,
+  kAndroidApplication
 };
 
 // Corresponds to DeviceReportingType in management_browser_proxy.js
@@ -191,6 +199,12 @@ std::string ToJSDeviceReportingType(const DeviceReportingType& type) {
       return "print";
     case DeviceReportingType::kCrostini:
       return "crostini";
+    case DeviceReportingType::kUsername:
+      return "username";
+    case DeviceReportingType::kExtensions:
+      return "extension";
+    case DeviceReportingType::kAndroidApplication:
+      return "android application";
     default:
       NOTREACHED() << "Unknown device reporting type";
       return "device";
@@ -257,6 +271,19 @@ void AddDeviceReportingInfo(base::Value* report_sources, Profile* profile) {
           crostini::prefs::kReportCrostiniUsageEnabled)) {
     AddDeviceReportingElement(report_sources, kManagementCrostini,
                               DeviceReportingType::kCrostini);
+  }
+
+  if (g_browser_process->local_state()->GetBoolean(
+          prefs::kCloudReportingEnabled) &&
+      base::FeatureList::IsEnabled(features::kEnterpriseReportingInChromeOS)) {
+    AddDeviceReportingElement(report_sources,
+                              kManagementExtensionReportUsername,
+                              DeviceReportingType::kUsername);
+    AddDeviceReportingElement(report_sources, kManagementReportExtensions,
+                              DeviceReportingType::kExtensions);
+    AddDeviceReportingElement(report_sources,
+                              kManagementReportAndroidApplications,
+                              DeviceReportingType::kAndroidApplication);
   }
 }
 #endif  // defined(OS_CHROMEOS)
