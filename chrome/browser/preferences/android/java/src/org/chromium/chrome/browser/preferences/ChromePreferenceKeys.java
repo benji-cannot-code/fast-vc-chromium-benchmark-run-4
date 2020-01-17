1652c578763750129bc21369b2edf8491a34d3f1
Henrique Nakashima <hnakashima@chromium.org>
2020-01-17 22:39:18 +0000

    Move a SharedPref from HistoryManager to ChromePreferenceKeys
    Register it in ChromePreferenceKeys and use SharedPreferencesManager
    consistently instead of SharedPreferences directly.
    
    Bug: 1022108
    Change-Id: I1c0bea6950d1a52d4213d11d421d89ce3b87ea01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003454
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733040}

--- chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeys.java
+++ chrome/browser/preferences/android/java/src/org/chromium/chrome/browser/preferences/ChromePreferenceKeys.java
@@ -397,6 +397,8 @@ public final class ChromePreferenceKeys {
     public static final String FONT_USER_FONT_SCALE_FACTOR = "user_font_scale_factor";
     public static final String FONT_USER_SET_FORCE_ENABLE_ZOOM = "user_set_force_enable_zoom";
 
+    public static final String HISTORY_SHOW_HISTORY_INFO = "history_home_show_info";
+
     /** Keys used to save settings related to homepage. */
     public static final String HOMEPAGE_CUSTOM_URI = "homepage_custom_uri";
     public static final String HOMEPAGE_ENABLED = "homepage";
@@ -761,6 +763,7 @@ public final class ChromePreferenceKeys {
                 FLAGS_CACHED_TAB_GROUPS_ANDROID_ENABLED,
                 FONT_USER_FONT_SCALE_FACTOR,
                 FONT_USER_SET_FORCE_ENABLE_ZOOM,
+                HISTORY_SHOW_HISTORY_INFO,
                 HOMEPAGE_CUSTOM_URI,
                 HOMEPAGE_ENABLED,
                 HOMEPAGE_USE_DEFAULT_URI,
