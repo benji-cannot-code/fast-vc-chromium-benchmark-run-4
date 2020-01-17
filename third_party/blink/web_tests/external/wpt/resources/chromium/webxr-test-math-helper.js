5efd965f682726e03fa137a3f08b144de4e7ebd4
Piotr Bialecki <bialpio@chromium.org>
2020-01-17 20:02:25 +0000

    CL followup - run ESLint, throw from math helper
    - ran ESLint on webxr-test.js and xr-internal-device-mocking.js
    - xr-internal-device-mocking.js now stores the
      `MockRuntime._injectAdditionalFrameData()` before overwriting it with
      its own implementation, and calls the original implementation as the
      first step
    - throw instead of console.warn when math helper encounters unexpected
      combinations of .w component
    
    Changes: 
    Change-Id: I94c015b5c89cfbc0e1a4b90db6ec44a198f4ce05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008073
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Auto-Submit: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732977}

--- third_party/blink/web_tests/external/wpt/resources/chromium/webxr-test-math-helper.js
+++ third_party/blink/web_tests/external/wpt/resources/chromium/webxr-test-math-helper.js
@@ -24,7 +24,7 @@ class XRMathHelper {
     // .w is treated here like an entity type, 1 signifies points, 0 signifies vectors.
     // point - point, point - vector, vector - vector are ok, vector - point is not.
     if (lhs.w != rhs.w && lhs.w == 0.0) {
-      console.warn("vector - point not allowed: " + toString(lhs) + "-" + toString(rhs));
+      throw new Error("vector - point not allowed: " + toString(lhs) + "-" + toString(rhs));
     }
 
     return {x : lhs.x - rhs.x, y : lhs.y - rhs.y, z : lhs.z - rhs.z, w : lhs.w - rhs.w};
@@ -32,7 +32,7 @@ class XRMathHelper {
 
   static add(lhs, rhs) {
     if (lhs.w == rhs.w && lhs.w == 1.0) {
-      console.warn("point + point not allowed", p1, p2);
+      throw new Error("point + point not allowed", p1, p2);
     }
 
     return {x : lhs.x + rhs.x, y : lhs.y + rhs.y, z : lhs.z + rhs.z, w : lhs.w + rhs.w};
@@ -40,7 +40,7 @@ class XRMathHelper {
 
   static cross(lhs, rhs) {
     if (lhs.w != 0.0 || rhs.w != 0.0) {
-      console.warn("cross product not allowed: " + toString(lhs) + "x" + toString(rhs));
+      throw new Error("cross product not allowed: " + toString(lhs) + "x" + toString(rhs));
     }
 
     return {
@@ -53,7 +53,7 @@ class XRMathHelper {
 
   static dot(lhs, rhs) {
     if (lhs.w != 0 || rhs.w != 0) {
-      console.warn("dot product not allowed: " + toString(lhs) + "x" + toString(rhs));
+      throw new Error("dot product not allowed: " + toString(lhs) + "x" + toString(rhs));
     }
 
     return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
@@ -61,7 +61,7 @@ class XRMathHelper {
 
   static mul(scalar, vector) {
     if (vector.w != 0) {
-      console.warn("scalar * vector not allowed", scalar, vector);
+      throw new Error("scalar * vector not allowed", scalar, vector);
     }
 
     return {x : vector.x * scalar, y : vector.y * scalar, z : vector.z * scalar, w : vector.w};
