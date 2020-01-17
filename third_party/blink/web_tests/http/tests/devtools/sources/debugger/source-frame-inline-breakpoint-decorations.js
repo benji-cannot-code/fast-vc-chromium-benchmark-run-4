e3f8dc324fda3946220513c7224e53a9e7e322e6
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-01-17 17:48:16 +0000

    Revert "Remove event races in two breakpoint decoration tests"
    This reverts commit 61f9138112dc68bc526ba6255e2927e85d3ade01.
    
    Reason for revert: Likely causing flakiness in source-frame-breakpoint-decorations.js: https://crbug.com/1043146
    
    Original change's description:
    > Remove event races in two breakpoint decoration tests
    > 
    > This CL changes two layout tests to follow the pattern of:
    >   1. Install an instrumentation promise
    >   2. Run and await an async action
    >   3. Await that the instrumentation is called
    >   4. Dump breakpoint information
    > 
    > This CL is also in preparation for asyncification of source mapping,
    > hence making the "action" async.
    > 
    > Drive-by: Re-baseline one of the layout tests that seems to be fixed
    > now.
    > 
    > Bug: chromium:1032016
    > Change-Id: Ibad4160fe9ab32aa3f818cdd4937112c3f4e6f08
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2000023
    > Commit-Queue: Simon Zünd <szuend@chromium.org>
    > Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#731171}
    
    TBR=sigurds@chromium.org,szuend@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: chromium:1032016
    Change-Id: I05e46d646d6cbf1516c8d9a2c1eab46cd8d05377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007810
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732905}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger/source-frame-inline-breakpoint-decorations.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger/source-frame-inline-breakpoint-decorations.js
@@ -31,11 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       //# sourceURL=foo.js
     `);
 
-  async function runAsyncBreakpointActionAndDumpDecorations(sourceFrame, action) {
-    const waitPromise = SourcesTestRunner.waitDebuggerPluginBreakpoints(sourceFrame);
-    await action();
-    await waitPromise;
-    SourcesTestRunner.dumpDebuggerPluginBreakpoints(sourceFrame);
+  function waitAndDumpDecorations(sourceFrame) {
+    return SourcesTestRunner.waitDebuggerPluginBreakpoints(sourceFrame)
+        .then(
+            () => SourcesTestRunner.dumpDebuggerPluginBreakpoints(sourceFrame));
   }
 
   Bindings.breakpointManager._storage._breakpoints = new Map();
@@ -47,16 +46,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       function addBreakpoint(sourceFrame) {
         javaScriptSourceFrame = sourceFrame;
         TestRunner.addResult('Setting breakpoint');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.createNewBreakpoint(javaScriptSourceFrame, 11, '', true)
-        ).then(removeBreakpoint);
+        SourcesTestRunner.createNewBreakpoint(javaScriptSourceFrame, 11, '', true)
+            .then(() => waitAndDumpDecorations(javaScriptSourceFrame).then(removeBreakpoint));
       }
 
       function removeBreakpoint() {
         TestRunner.addResult('Toggle breakpoint');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 11)
-        ).then(next);
+        waitAndDumpDecorations(javaScriptSourceFrame).then(() => next());
+        SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 11);
       }
     },
 
@@ -67,16 +64,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       function addBreakpoint(sourceFrame) {
         javaScriptSourceFrame = sourceFrame;
         TestRunner.addResult('Setting breakpoint');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.createNewBreakpoint(javaScriptSourceFrame, 13, '', true)
-        ).then(removeBreakpoint);
+        SourcesTestRunner.createNewBreakpoint(javaScriptSourceFrame, 13, '', true)
+            .then(() => waitAndDumpDecorations(javaScriptSourceFrame).then(removeBreakpoint));
       }
 
       function removeBreakpoint() {
         TestRunner.addResult('Toggle breakpoint');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 13)
-        ).then(next);
+        SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 13);
+        waitAndDumpDecorations(javaScriptSourceFrame).then(() => next());
       }
     },
 
@@ -87,33 +82,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       function addBreakpoint(sourceFrame) {
         javaScriptSourceFrame = sourceFrame;
         TestRunner.addResult('Setting breakpoint');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.createNewBreakpoint(javaScriptSourceFrame, 11, '', true)
-        ).then(clickBySecondLocation);
+        SourcesTestRunner.createNewBreakpoint(javaScriptSourceFrame, 11, '', true)
+            .then(() => waitAndDumpDecorations(javaScriptSourceFrame).then(clickBySecondLocation));
       }
 
       function clickBySecondLocation() {
         TestRunner.addResult('Click by second breakpoint');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.clickDebuggerPluginBreakpoint(
-              javaScriptSourceFrame, 11, 1, next)
-        ).then(clickByFirstLocation);
+        waitAndDumpDecorations(javaScriptSourceFrame).then(clickByFirstLocation);
+        SourcesTestRunner.clickDebuggerPluginBreakpoint(
+            javaScriptSourceFrame, 11, 1, next);
       }
 
       function clickByFirstLocation() {
         TestRunner.addResult('Click by first breakpoint');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.clickDebuggerPluginBreakpoint(
-              javaScriptSourceFrame, 11, 0, next)
-        ).then(clickBySecondLocationAgain);
+        waitAndDumpDecorations(javaScriptSourceFrame).then(clickBySecondLocationAgain);
+        SourcesTestRunner.clickDebuggerPluginBreakpoint(
+            javaScriptSourceFrame, 11, 0, next);
       }
 
       function clickBySecondLocationAgain() {
         TestRunner.addResult('Click by second breakpoint');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.clickDebuggerPluginBreakpoint(
-              javaScriptSourceFrame, 11, 1, next)
-        ).then(next);
+        waitAndDumpDecorations(javaScriptSourceFrame).then(() => next());
+        SourcesTestRunner.clickDebuggerPluginBreakpoint(
+            javaScriptSourceFrame, 11, 1, next);
       }
     },
 
@@ -124,26 +115,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       function addBreakpoint(sourceFrame) {
         javaScriptSourceFrame = sourceFrame;
         TestRunner.addResult('Setting breakpoint in line 4');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 12, false)
-        ).then(toggleBreakpointInAnotherLine);
+        SourcesTestRunner.toggleBreakpoint(sourceFrame, 12, false);
+        waitAndDumpDecorations(javaScriptSourceFrame).then(toggleBreakpointInAnotherLine);
       }
 
       function toggleBreakpointInAnotherLine() {
         TestRunner.addResult('Setting breakpoint in line 3');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-          SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 11, false)
-        ).then(removeBreakpoints);
+        waitAndDumpDecorations(javaScriptSourceFrame).then(removeBreakpoints);
+        SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 11, false);
       }
 
       function removeBreakpoints() {
         TestRunner.addResult('Click by first inline breakpoints');
-        runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () => {
-          SourcesTestRunner.clickDebuggerPluginBreakpoint(
-              javaScriptSourceFrame, 11, 0, next);
-          SourcesTestRunner.clickDebuggerPluginBreakpoint(
-              javaScriptSourceFrame, 12, 0, next);
-        }).then(next);
+        waitAndDumpDecorations(javaScriptSourceFrame).then(() => next());
+        SourcesTestRunner.clickDebuggerPluginBreakpoint(
+            javaScriptSourceFrame, 11, 0, next);
+        SourcesTestRunner.clickDebuggerPluginBreakpoint(
+            javaScriptSourceFrame, 12, 0, next);
       }
     },
 
@@ -151,14 +139,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       let javaScriptSourceFrame = await SourcesTestRunner.showScriptSourcePromise('foo.js');
 
       TestRunner.addResult('Setting breakpoint');
-      await runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-        SourcesTestRunner.createNewBreakpoint(javaScriptSourceFrame, 16, '', true)
-      );
+      await SourcesTestRunner.createNewBreakpoint(javaScriptSourceFrame, 16, '', true)
+      await waitAndDumpDecorations(javaScriptSourceFrame);
 
       TestRunner.addResult('Toggle breakpoint');
-      await runAsyncBreakpointActionAndDumpDecorations(javaScriptSourceFrame, () =>
-        SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 28)
-      );
+      let decorationsPromise = waitAndDumpDecorations(javaScriptSourceFrame);
+      SourcesTestRunner.toggleBreakpoint(javaScriptSourceFrame, 28);
+      await decorationsPromise;
       next();
     }
   ]);
