2709db8427a6abfb68e6db42f4e25038bc809462
Ahmed Mehfooz <amehfooz@chromium.org>
2020-01-17 19:14:34 +0000

    Add themed color support for system menu
    Bug: 1035361
    Change-Id: Ib39b48c2798262c98ded9a2beb796d8f359da925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006370
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732952}

--- ash/system/unified/unified_system_tray_view.cc
+++ ash/system/unified/unified_system_tray_view.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/unified/unified_system_info_view.h"
 #include "ash/system/unified/unified_system_tray_controller.h"
 #include "ash/system/unified/unified_system_tray_model.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/scoped_canvas.h"
 #include "ui/message_center/message_center.h"
@@ -231,20 +232,30 @@ class UnifiedSystemTrayView::FocusSearch : public views::FocusSearch {
 
 // static
 SkColor UnifiedSystemTrayView::GetBackgroundColor() {
-  if (features::IsBackgroundBlurEnabled()) {
-    AshColorProvider::BaseLayerType layer_type =
-        Shelf::ForWindow(Shell::GetPrimaryRootWindow())
-                    ->shelf_widget()
-                    ->GetBackgroundType() == ShelfBackgroundType::kHomeLauncher
-            ? AshColorProvider::BaseLayerType::kTransparent60
-            : AshColorProvider::BaseLayerType::kTransparent74;
-
-    return AshColorProvider::Get()->GetBaseLayerColor(
-        layer_type, AshColorProvider::AshColorMode::kDark);
+  if (!features::IsBackgroundBlurEnabled()) {
+    return AshColorProvider::Get()->DeprecatedGetBaseLayerColor(
+        AshColorProvider::BaseLayerType::kTransparent90,
+        kUnifiedMenuBackgroundColor);
   }
-  return AshColorProvider::Get()->DeprecatedGetBaseLayerColor(
-      AshColorProvider::BaseLayerType::kTransparent90,
-      kUnifiedMenuBackgroundColor);
+
+  AshColorProvider::BaseLayerType layer_type =
+      (Shell::Get()->tablet_mode_controller() &&
+       Shell::Get()->tablet_mode_controller()->InTabletMode())
+          ? AshColorProvider::BaseLayerType::kTransparent60
+          : AshColorProvider::BaseLayerType::kTransparent74;
+
+  auto background_type = Shelf::ForWindow(Shell::GetPrimaryRootWindow())
+                             ->shelf_widget()
+                             ->GetBackgroundType();
+  if (background_type == ShelfBackgroundType::kMaximized ||
+      background_type == ShelfBackgroundType::kInApp) {
+    layer_type = AshColorProvider::BaseLayerType::kTransparent90;
+  }
+
+  SkColor background_color = AshColorProvider::Get()->GetBaseLayerColor(
+      layer_type, AshColorProvider::AshColorMode::kDark);
+
+  return ShelfConfig::Get()->GetThemedColorFromWallpaper(background_color);
 }
 
 // static
