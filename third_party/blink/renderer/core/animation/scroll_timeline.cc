2a8a6d941827284fd0fc3319b6cadc30602eadab
Olga Gerchikov <gerchiko@microsoft.com>
2020-01-17 20:24:36 +0000

    Schedule frame for scroll linked animations only when scrolling changes
    Scheduling frame for scroll linked animations regardless of the scroller
    updates makes scroll animations inefficient. This change schedules next
    frame only when scroll timeline current time changes. The change
    assumes that animation timeline scheduling runs (via
    DocumentAnimations::UpdateAnimations) at the end of any frame in which
    scroll timeline (i.e. its offset, range, overflow) is invalidated.
    
    
    Bug: 1039750
    Change-Id: Ib87a298e6d09cbeb85276cea4c165022b76f3732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1990003
    Commit-Queue: Olga Gerchikov <gerchiko@microsoft.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732987}

--- third_party/blink/renderer/core/animation/scroll_timeline.cc
+++ third_party/blink/renderer/core/animation/scroll_timeline.cc
@@ -144,9 +144,11 @@ ScrollTimeline::InitialStartTimeForAnimations() {
 }
 
 void ScrollTimeline::ScheduleNextService() {
+  DCHECK_EQ(outdated_animation_count_, 0U);
   if (AnimationsNeedingUpdateCount() == 0)
     return;
-  ScheduleServiceOnNextFrame();
+  if (CurrentTimeInternal() != last_current_time_internal_)
+    ScheduleServiceOnNextFrame();
 }
 
 base::Optional<base::TimeDelta> ScrollTimeline::CurrentTimeInternal() {
