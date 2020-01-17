6b3e03c05e74f44e9571f1af2ba6767533951116
Ted Meyer <tmathmeyer@chromium.org>
2020-01-17 02:41:42 +0000

    Create MediaLogMessages and MediaLogEvents
    Share some of the helper macros from MediaLogProperties with these two
    new types in the media_log_type_enforcement.h file, which replaces
    media_log_properties_helper.h
    
    Bug: 794255
    Change-Id: I7c709c5ac881b3bf03ba327d850e0c58824cec57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006130
    Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732672}

--- media/base/media_log_properties.h
+++ media/base/media_log_properties.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/audio_decoder_config.h"
 #include "media/base/media_export.h"
-#include "media/base/media_log_properties_helper.h"
+#include "media/base/media_log_type_enforcement.h"
 #include "media/base/video_decoder_config.h"
 #include "ui/gfx/geometry/size.h"
 
