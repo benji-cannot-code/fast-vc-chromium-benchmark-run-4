551b4766959c4bd0d2ed4ebae530df5035b8fcc2
Elly Fong-Jones <ellyjones@chromium.org>
2020-01-17 19:06:33 +0000

    cbuiv: remove some GetDialogButtons overrides
    They are replaced with the new setters.
    
    Bug: 1011446
    Change-Id: I1c4bcb1853e4ae43d043135bc7f3fbf534aca66f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007649
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732943}

--- chrome/browser/ui/views/apps/app_pause_dialog_view.h
+++ chrome/browser/ui/views/apps/app_pause_dialog_view.h
@@ -29,7 +29,6 @@ class AppPauseDialogView : public views::BubbleDialogDelegateView {
   // views::BubbleDialogDelegateView:
   bool Accept() override;
   gfx::Size CalculatePreferredSize() const override;
-  int GetDialogButtons() const override;
   ui::ModalType GetModalType() const override;
   base::string16 GetWindowTitle() const override;
   bool ShouldShowCloseButton() const override;
