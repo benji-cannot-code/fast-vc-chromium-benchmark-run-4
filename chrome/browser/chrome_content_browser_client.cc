9051317b37579a376183a173c342d3482c3c6b9f
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-01-17 17:08:24 +0000

    Add UMA metrics for Deep Scanning of Paste uploads
    Bug: 1020267
    Change-Id: I98f1a6b785f66225622bacd60721bd84e3d508d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2005688
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732888}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -116,6 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/resource_coordinator/background_tab_navigation_throttle.h"
 #include "chrome/browser/safe_browsing/certificate_reporting_service.h"
 #include "chrome/browser/safe_browsing/certificate_reporting_service_factory.h"
+#include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
 #include "chrome/browser/safe_browsing/safe_browsing_navigation_throttle.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/safe_browsing/ui_manager.h"
@@ -5479,7 +5480,8 @@ void ChromeContentBrowserClient::IsClipboardPasteAllowed(
               std::move(callback).Run(
                   ClipboardPasteAllowed(result.text_results[0]));
             },
-            std::move(callback)));
+            std::move(callback)),
+        safe_browsing::DeepScanAccessPoint::PASTE);
   } else {
     std::move(callback).Run(ClipboardPasteAllowed(true));
   }
--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.cc
@@ -88,7 +88,6 @@ void MaybeReportDeepScanningVerdict(Profile* profile,
 }
 
 std::string DeepScanAccessPointToString(DeepScanAccessPoint access_point) {
-  // TODO(domfc): Add PASTE access point.
   switch (access_point) {
     case DeepScanAccessPoint::DOWNLOAD:
       return "Download";
@@ -96,6 +95,8 @@ std::string DeepScanAccessPointToString(DeepScanAccessPoint access_point) {
       return "Upload";
     case DeepScanAccessPoint::DRAG_AND_DROP:
       return "DragAndDrop";
+    case DeepScanAccessPoint::PASTE:
+      return "Paste";
   }
   NOTREACHED();
   return "";
