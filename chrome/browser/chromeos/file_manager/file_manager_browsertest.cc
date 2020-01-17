65159f1cdb27accb5012a21a6abc5d28203a9dcd
Rachel Sugrono <rsugrono@google.com>
2020-01-17 08:56:39 +0000

    [quickview] Test left/right arrow keys when multiple files are selected
    Add two non-consecutive files to check-select (aka multi-
    select). Quick View should open in check-select mode with
    the first file being opened. Left/right arrow keys should show
    the next selection in check-select.
    
    Bug: 1038799
    Change-Id: I1164a19391920b9cc08d0f622d1c2a14c006b8d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006901
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732763}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -472,6 +472,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("pressEnterOnInfoBoxToOpenClose"),
         TestCase("openQuickViewWithMultipleFiles"),
         TestCase("openQuickViewWithMultipleFilesKeyboardUpDown"),
+        TestCase("openQuickViewWithMultipleFilesKeyboardLeftRight"),
         TestCase("closeQuickView"),
         TestCase("openQuickViewFromDirectoryTree")));
 
