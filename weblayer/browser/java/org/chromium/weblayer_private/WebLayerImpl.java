194155444a8245c4658ee88abe938d1e28477995
Mugdha Lakhani <nator@chromium.org>
2020-01-17 17:40:22 +0000

    [WebLayer] Tie network detection to Browser lifecycle.
    Tying to BrowserFragment lifecycle leaves a loophole. If
    setRetainInstance() is called on BrowserFragment, it will be detached
    and reattached without necessarily being destroyed and re-created.
    
    In the duration the BrowserFragment is alive but not attached, it'll not
    receive any network change notifications. Tying registration to the
    Browser lifecycle fixes that.
    
    Bug: 1018848
    Change-Id: I63be218a0fa2f8422f5cd9cc0d6e2dee2fa2be0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1998398
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732899}

--- weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/WebLayerImpl.java
@@ -103,6 +103,9 @@ public final class WebLayerImpl extends IWebLayer.Stub {
         StrictModeWorkaround.apply();
         init(appContextWrapper, remoteContextWrapper);
 
+        NetworkChangeNotifier.init();
+        WebLayerNetworkChangeNotifierRegistrationPolicy registrationPolicy =
+                new WebLayerNetworkChangeNotifierRegistrationPolicy();
         final ValueCallback<Boolean> loadedCallback = (ValueCallback<Boolean>) ObjectWrapper.unwrap(
                 loadedCallbackWrapper, ValueCallback.class);
         BrowserStartupController.get(LibraryProcessType.PROCESS_WEBLAYER)
@@ -112,7 +115,7 @@ public final class WebLayerImpl extends IWebLayer.Stub {
                             @Override
                             public void onSuccess() {
                                 CrashReporterControllerImpl.getInstance().notifyNativeInitialized();
-                                configureNetworkChangeNotifier();
+                                configureNetworkChangeNotifier(registrationPolicy);
                                 loadedCallback.onReceiveValue(true);
                             }
                             @Override
@@ -136,15 +139,15 @@ public final class WebLayerImpl extends IWebLayer.Stub {
                 .startBrowserProcessesSync(
                         /* singleProcess*/ false);
         CrashReporterControllerImpl.getInstance().notifyNativeInitialized();
-        configureNetworkChangeNotifier();
+        NetworkChangeNotifier.init();
+        configureNetworkChangeNotifier(new WebLayerNetworkChangeNotifierRegistrationPolicy());
     }
 
     // Configure NetworkChangeNotifier to auto detect changes in network
     // connectivity.
-    private void configureNetworkChangeNotifier() {
-        NetworkChangeNotifier.init();
-        NetworkChangeNotifier.setAutoDetectConnectivityState(
-                new WebLayerNetworkChangeNotifierRegistrationPolicy());
+    private void configureNetworkChangeNotifier(
+            WebLayerNetworkChangeNotifierRegistrationPolicy registrationPolicy) {
+        NetworkChangeNotifier.setAutoDetectConnectivityState(registrationPolicy);
     }
 
     private void init(IObjectWrapper appContextWrapper, IObjectWrapper remoteContextWrapper) {
