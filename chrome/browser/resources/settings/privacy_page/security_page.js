cc085758cecbef924e27db562bd7a4a50e757f61
Leonard Grey <lgrey@chromium.org>
2020-01-17 16:12:59 +0000

    [Sheriff]: Revert "More Settings Page Interaction UMA"
    This reverts commit 561bf1af38509f6029ba688219b37f76a4e4f3b4.
    
    Reason for revert:
    Strongly suspect this is causing failures on the debug builders:
    [29058:775:0117/070736.663833:ERROR:CONSOLE(49)] "Mocha test failed: PersonalizationOptionsTests_AllBuilds signinAllowedToggle
    Error: Uncaught TypeError: Cannot read property 'PRIVACY_CHROME_SIGN_IN' of undefined (chrome://settings/privacy_page/personalization_options.js:102)
    
    r732804 was reverted earlier for the same reasons, but there are still failures coming from the settings code, albeit less
    
    Original change's description:
    > More Settings Page Interaction UMA
    > 
    > Move the settings enum and the call to uma to a separate browser proxy.
    > 
    > Add UMA to all site settings subpage interactions and new toggles moved to the main settings card
    > 
    > Bug: 1032584
    > Change-Id: Icadd71d4511fa1f604f2d3acbda92c0e84700173
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967651
    > Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    > Reviewed-by: Dan Beam <dbeam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#732785}
    
    TBR=dbeam@chromium.org,harrisonsean@chromium.org
    
    Change-Id: Id4ee78d89487ecf7e6d1935088d02b29de89c7c8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1032584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007829
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732870}

--- chrome/browser/resources/settings/privacy_page/security_page.js
+++ chrome/browser/resources/settings/privacy_page/security_page.js
@@ -54,8 +54,6 @@ Polymer({
   /** @override */
   ready() {
     this.browserProxy_ = settings.PrivacyPageBrowserProxyImpl.getInstance();
-
-    this.metricsBrowserProxy_ = settings.MetricsBrowserProxyImpl.getInstance();
   },
 
   /**
@@ -99,7 +97,7 @@ Polymer({
     // <if expr="is_win or is_macosx">
     this.browserProxy_.showManageSSLCertificates();
     // </if>
-    this.metricsBrowserProxy_.recordSettingsPageHistogram(
+    this.browserProxy_.recordSettingsPageHistogram(
         settings.SettingsPageInteractions.PRIVACY_MANAGE_CERTIFICATES);
   },
 
@@ -111,7 +109,7 @@ Polymer({
   /** @private */
   onSecurityKeysClick_() {
     settings.Router.getInstance().navigateTo(settings.routes.SECURITY_KEYS);
-    this.metricsBrowserProxy_.recordSettingsPageHistogram(
+    this.browserProxy_.recordSettingsPageHistogram(
         settings.SettingsPageInteractions.PRIVACY_SECURITY_KEYS);
   },
 });
--- chrome/test/data/webui/settings/passwords_leak_detection_toggle_test.js
+++ chrome/test/data/webui/settings/passwords_leak_detection_toggle_test.js
@@ -4,9 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 suite('CrSettingsPasswordsLeakDetectionToggleTest', function() {
-  /** @type {settings.TestMetricsBrowserProxy} */
-  let testMetricsBrowserProxy;
-
   /** @type {settings.TestPrivacyPageBrowserProxy} */
   let privacyPageBrowserProxy;
 
@@ -21,8 +18,6 @@ suite('CrSettingsPasswordsLeakDetectionToggleTest', function() {
     settings.PrivacyPageBrowserProxyImpl.instance_ = privacyPageBrowserProxy;
     syncBrowserProxy = new TestSyncBrowserProxy();
     settings.SyncBrowserProxyImpl.instance_ = syncBrowserProxy;
-    testMetricsBrowserProxy = new TestMetricsBrowserProxy();
-    settings.MetricsBrowserProxyImpl.instance_ = testMetricsBrowserProxy;
     PolymerTest.clearBody();
     testElement =
         document.createElement('settings-passwords-leak-detection-toggle');
@@ -37,20 +32,6 @@ suite('CrSettingsPasswordsLeakDetectionToggleTest', function() {
 
   teardown(function() {
     testElement.remove();
-  });
-
-  test('logPasswordLeakDetectionClick', function() {
-    testElement.set(
-        'prefs.profile.password_manager_leak_detection.value', true);
-    testElement.syncStatus = {signedIn: true};
-    Polymer.dom.flush();
-    console.log(testElement.$$('#passwordsLeakDetectionCheckbox').disabled);
-    testElement.$$('#passwordsLeakDetectionCheckbox').click();
-    return testMetricsBrowserProxy.whenCalled('recordSettingsPageHistogram')
-        .then(result => {
-          assertEquals(
-              settings.SettingsPageInteractions.PRIVACY_PASSWORD_CHECK, result);
-        });
   });
 
   test('leakDetectionToggleSignedOutWithFalsePref', function() {
