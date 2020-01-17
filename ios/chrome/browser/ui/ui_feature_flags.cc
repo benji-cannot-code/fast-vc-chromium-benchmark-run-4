c8f1ed2171e1f582a709e60764f3cfe6742deb51
Nohemi Fernandez <fernandex@chromium.org>
2020-01-17 16:53:45 +0000

    [iOS] Add a flag for new sign-out feature.
    Bug: 1005509
    Change-Id: I7eab2ad6ad082daadff0ab38aafe9ea57d655ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007450
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#732881}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -37,3 +37,6 @@ const base::Feature kForceUnstackedTabstrip{"ForceUnstackedTabstrip",
 
 const base::Feature kContainedBVC{"ContainedBVC",
                                   base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kClearSyncedData{"ClearSyncedData",
+                                     base::FEATURE_DISABLED_BY_DEFAULT};
