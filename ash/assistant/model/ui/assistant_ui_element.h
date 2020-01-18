86fa8268656802f3eefe1f5dc5ae4720cdf74e08
David Black <dmblack@google.com>
2020-01-18 00:48:47 +0000

    Revert "Clean up AssistantResponse processing."
    This reverts commit ff37ce9e23906aa7a6bbc530e8eef1128dbd8c25.
    
    Reason for revert: Breaking browser_tests
    
    Original change's description:
    > Clean up AssistantResponse processing.
    > 
    > This CL:
    > - Removes AssistantCardElement one-off logic from AssistantResponse so
    >   that it will scale to new AssisantUiElement types.
    > - Moves private classes into .cc to clean up .h
    > 
    > Bug: b:145003512
    > Change-Id: If6a9c79a7a6a49c10d903762ac9c1d82a8267c30
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006137
    > Commit-Queue: David Black <dmblack@google.com>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#733011}
    
    TBR=xiaohuic@chromium.org,dmblack@google.com
    
    Change-Id: I5e221abb697434e60e1a6291de22b17887269621
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:145003512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008616
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#733091}

--- ash/assistant/model/ui/assistant_ui_element.h
+++ ash/assistant/model/ui/assistant_ui_element.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_ASSISTANT_MODEL_UI_ASSISTANT_UI_ELEMENT_H_
 #define ASH_ASSISTANT_MODEL_UI_ASSISTANT_UI_ELEMENT_H_
 
-#include "base/callback.h"
 #include "base/component_export.h"
 #include "base/macros.h"
 
@@ -25,17 +24,12 @@ enum class AssistantUiElementType {
 // Base class for a UI element that will be rendered inside of Assistant UI.
 class COMPONENT_EXPORT(ASSISTANT_MODEL) AssistantUiElement {
  public:
-  virtual ~AssistantUiElement();
+  virtual ~AssistantUiElement() = default;
 
   AssistantUiElementType type() const { return type_; }
 
-  // Invoke to being processing the UI element for rendering. Upon completion,
-  // the specified |callback| will be run to indicate success or failure.
-  using ProcessingCallback = base::OnceCallback<void(bool)>;
-  virtual void Process(ProcessingCallback callback);
-
  protected:
-  explicit AssistantUiElement(AssistantUiElementType type);
+  explicit AssistantUiElement(AssistantUiElementType type) : type_(type) {}
 
  private:
   const AssistantUiElementType type_;
