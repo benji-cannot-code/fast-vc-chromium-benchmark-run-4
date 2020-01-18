8a502a1598109b71bf1af3b1b433e2eed3ab9aab
Kai Ninomiya <kainino@chromium.org>
2020-01-18 06:24:36 +0000

    Tools for autogenerating wpt_internal/webgpu/cts.html
    Most of the work that occurs during a roll_webgpu_cts or
    regenerate_internal_cts_html run takes place inside the WebGPU
    conformance test suite repository:
    https://github.com/gpuweb/cts
    
    The first full WebGPU CTS roll created using these tools (there were
    some before this for which I used earlier versions of these tools):
    https://chromium-review.googlesource.com/c/chromium/src/+/1996140
    
    Change-Id: Ifb4c570de5a30e06027fba2eac9629c9992d03a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919211
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733128}

---
+++ third_party/blink/web_tests/webgpu/regenerate_internal_cts_html.sh
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This tool regenerates `wpt_internal/webgpu/cts.html` using the currently
+# checked-out version of the WebGPU CTS. It does the following:
+#
+#   - Extracts the "name" part of every expectation from WebGPUExpectations.
+#   - Prepares the WebGPU checkout to run the gen_wpt_cts_html tool.
+#   - Runs gen_wpt_cts_html with the appropriate metadata and the extracted
+#     expectations names. This generates a cts.html file with the necessary
+#     variants to cover the expectations in WebGPUExpectations.
+#
+# Note: If it fails, it's likely that updates are needed to make
+# WebGPUExpectations match the latest CTS (e.g. perhaps test/case names or
+# parameters have changed). Consult the error message for details.
+
+set -e
+
+cd "$(dirname "$0")"/../../../..  # cd to [chromium]/src/
+
+expectations=$(mktemp)
+trap "{ rm -f $expectations; }" EXIT
+echo $expectations
+
+pushd third_party/blink > /dev/null
+
+  tools/extract_expectation_names.py web_tests/WebGPUExpectations > $expectations
+
+popd > /dev/null
+
+pushd third_party/webgpu-cts/src > /dev/null
+
+  yarn install --frozen-lockfile
+  npx grunt prebuild
+  npx ./tools/gen_wpt_cts_html \
+    ../../blink/web_tests/wpt_internal/webgpu/cts.html \
+    ../../blink/web_tests/webgpu/ctshtml-template.txt \
+    ../../blink/web_tests/webgpu/argsprefixes.txt \
+    $expectations \
+    'wpt_internal/webgpu/cts.html' cts
+
+popd > /dev/null
