4fb7455a7ddbcb086a3b2c704414919e3938dbf7
Demetrios Papadopoulos <dpapad@chromium.org>
2020-01-18 01:12:55 +0000

    [Reland#2] WebUI: Stop auto-generating Polymer 3 version of cr-splitter.
    The Polymer 2 version is not used anymore, therefore no auto-generation
    is necessary and the v3 version can be directly checked in.
    
    Fixed: 1042545
    Change-Id: I4c71ce720d798714e68ee6c7b5fb4458a89a8d24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007517
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733100}

--- chrome/test/data/webui/cr_elements/cr_splitter_test.js
+++ chrome/test/data/webui/cr_elements/cr_splitter_test.js
@@ -3,9 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// clang-format off
-// #import 'chrome://resources/cr_elements/cr_splitter/cr_splitter.m.js';
-// clang-format on
+import 'chrome://resources/cr_elements/cr_splitter/cr_splitter.js';
 
 suite('cr-splitter', function() {
   let crSplitter;
