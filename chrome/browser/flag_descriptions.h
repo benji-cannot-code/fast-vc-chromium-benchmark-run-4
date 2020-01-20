8ef2c21ebbc1a5aa6bc815ac9bdb61b216055e0f
Clemens Backes <clemensb@chromium.org>
2020-01-20 10:43:24 +0000

    [Flags] Add flag for WebAssembly lazy compilation
    Lazy compilation might become one candidate configuration for Liftoff on
    mobile. Hence we add the flag now so it will be available in the next
    beta release for finching.
    
    R=rmcilroy@chromium.org, jochen@chromium.org
    
    Bug: 1040030, 1040061
    Change-Id: Icb86a4d3c075afc111978f6797634aabd5b98020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002568
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733294}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -595,6 +595,9 @@ extern const char kEnableWasmCodeGCDescription[];
 extern const char kEnableWasmCodeCacheName[];
 extern const char kEnableWasmCodeCacheDescription[];
 
+extern const char kEnableWasmLazyCompilationName[];
+extern const char kEnableWasmLazyCompilationDescription[];
+
 extern const char kEnableWasmSimdName[];
 extern const char kEnableWasmSimdDescription[];
 
