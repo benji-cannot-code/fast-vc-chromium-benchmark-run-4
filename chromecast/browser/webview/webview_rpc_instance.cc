d456465af330bb4d2a6dac502d3cc508c7d9e243
Shawn Gallea <sagallea@google.com>
2020-01-20 23:16:27 +0000

    Remove WebviewService
    Webviews are now controlled/created
    by PlatformViewsService
    
    Bug: Internal b/144275588
    Test: NONE
    Change-Id: Id6cb20066a4262f53d6c94d2db3820d06879da96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2011296
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Shawn Gallea <sagallea@google.com>
    Cr-Commit-Position: refs/heads/master@{#733474}

--- chromecast/browser/webview/webview_rpc_instance.cc
+++ chromecast/browser/webview/webview_rpc_instance.cc
@@ -18,26 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromecast {
 
-// TODO(sagallea): Remove this when ready to deprecate WebviewService.
-WebviewRpcInstance::WebviewRpcInstance(
-    webview::WebviewService::AsyncService* service,
-    grpc::ServerCompletionQueue* cq,
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
-    WebviewWindowManager* window_manager)
-    : PlatformViewsRpcInstance(cq, task_runner, window_manager),
-      webview_service_(service),
-      platform_views_service_(nullptr) {
-  webview_service_->RequestCreateWebview(&ctx_, &io_, cq_, cq_,
-                                         &init_callback_);
-}
-
 WebviewRpcInstance::WebviewRpcInstance(
     webview::PlatformViewsService::AsyncService* service,
     grpc::ServerCompletionQueue* cq,
     scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     WebviewWindowManager* window_manager)
     : PlatformViewsRpcInstance(cq, task_runner, window_manager),
-      webview_service_(nullptr),
       platform_views_service_(service) {
   platform_views_service_->RequestCreateWebview(&ctx_, &io_, cq_, cq_,
                                                 &init_callback_);
@@ -46,12 +32,8 @@ WebviewRpcInstance::WebviewRpcInstance(
 WebviewRpcInstance::~WebviewRpcInstance() {}
 
 void WebviewRpcInstance::CreateNewInstance() {
-  if (webview_service_)
-    new WebviewRpcInstance(webview_service_, cq_, task_runner_,
-                           window_manager_);
-  else
-    new WebviewRpcInstance(platform_views_service_, cq_, task_runner_,
-                           window_manager_);
+  new WebviewRpcInstance(platform_views_service_, cq_, task_runner_,
+                         window_manager_);
 }
 
 bool WebviewRpcInstance::Initialize() {
