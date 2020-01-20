a4794e089ff6bc3e052671d601fdfba2c464e3a2
Ionel Popescu <iopopesc@microsoft.com>
2020-01-20 06:47:58 +0000

    Use the 2px inner padding just for checkbox, radio and links.
    *** SHERIFFS: if this CL triggers a flaky layout test, PLEASE do not
    *** revert this CL. Instead, please disable the offending test(s) and
    *** add them to the crbug.com/1035582 section of TestExpectations. I
    *** will then take care of them as I fix up the remainder of these
    *** tests. Feel free to cc me if you create a bug. Thanks!
    
    Prior to this CL the 2px inner padding was used for all the elements.
    Because of issues where due to content overlapping the focus ring is
    not seen completely, the inner 2px padding is changed to be used just
    for small controls (checkbox, radio) and links.
    
    Bug: 1026553, 1043054, 1043064
    Change-Id: I11994e9bfbba6a9764130e314a38e5505808df42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008282
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733260}

--- third_party/blink/renderer/platform/graphics/graphics_context.h
+++ third_party/blink/renderer/platform/graphics/graphics_context.h
@@ -372,6 +372,7 @@ class PLATFORM_EXPORT GraphicsContext {
   void DrawFocusRing(const Vector<IntRect>&,
                      float width,
                      int offset,
+                     int default_offset,
                      float border_radius,
                      float min_border_width,
                      const Color&,
@@ -431,7 +432,10 @@ class PLATFORM_EXPORT GraphicsContext {
                                           FloatPoint& p2,
                                           float stroke_width);
 
-  static int FocusRingOutsetExtent(int offset, int width, bool is_outset);
+  static int FocusRingOutsetExtent(int offset,
+                                   int default_offset,
+                                   int width,
+                                   bool is_outset);
 
   void SetInDrawingRecorder(bool);
   bool InDrawingRecorder() const { return in_drawing_recorder_; }
