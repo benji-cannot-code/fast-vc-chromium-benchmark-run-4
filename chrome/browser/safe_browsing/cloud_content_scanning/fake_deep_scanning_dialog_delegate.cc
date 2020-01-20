d10fe5d00acdaf862ef3bd39e569333f3e182f26
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-01-20 15:28:27 +0000

    Handle non-success results for Deep Scanning of uploads
    Bug: 1042319
    Change-Id: Ie33e4f358d35bb6ed5314e19035536c4860afbd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003349
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733365}

--- chrome/browser/safe_browsing/cloud_content_scanning/fake_deep_scanning_dialog_delegate.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/fake_deep_scanning_dialog_delegate.cc
@@ -7,8 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <base/callback.h>
 #include <base/logging.h>
+#include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
 
 namespace safe_browsing {
+
+BinaryUploadService::Result FakeDeepScanningDialogDelegate::result_ =
+    BinaryUploadService::Result::SUCCESS;
 
 FakeDeepScanningDialogDelegate::FakeDeepScanningDialogDelegate(
     base::RepeatingClosure delete_closure,
@@ -29,6 +33,12 @@ FakeDeepScanningDialogDelegate::FakeDeepScanningDialogDelegate(
 FakeDeepScanningDialogDelegate::~FakeDeepScanningDialogDelegate() {
   if (!delete_closure_.is_null())
     delete_closure_.Run();
+}
+
+// static
+void FakeDeepScanningDialogDelegate::SetResponseResult(
+    BinaryUploadService::Result result) {
+  result_ = result;
 }
 
 // static
@@ -118,9 +128,9 @@ void FakeDeepScanningDialogDelegate::Response(
                                             ? DeepScanningClientResponse()
                                             : status_callback_.Run(path);
   if (path.empty())
-    StringRequestCallback(BinaryUploadService::Result::SUCCESS, response);
+    StringRequestCallback(result_, response);
   else
-    FileRequestCallback(path, BinaryUploadService::Result::SUCCESS, response);
+    FileRequestCallback(path, result_, response);
 }
 
 void FakeDeepScanningDialogDelegate::PrepareFileRequest(
