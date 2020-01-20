0bf80cd8671b336238b9656747ab2c926945ae37
Michael Thiessen <mthiesse@chromium.org>
2020-01-20 21:18:51 +0000

    Introduce DisabledCalledByNativeJavaTest
    This makes tests defined by CalledByNativeJavaTest easier to disable
    by using the DisabledCalledByNativeJavaTest annotation.
    
    Bug: 1042808
    Change-Id: I8e30d5a092e765c786927d753b0a3a22be4a605d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008311
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733453}

--- base/android/jni_generator/jni_generator_tests.py
+++ base/android/jni_generator/jni_generator_tests.py
@@ -827,6 +827,9 @@ scooby doo
       @CalledByNativeJavaTest
       public void testOtherFoo() {}
 
+      @DisabledCalledByNativeJavaTest
+      public void testDisabledFoo() {}
+
       @CalledByNativeJavaTest
       public void testLongNameActionServiceModelProducerDelegateProxyObserverMediatorFactoryConsumerImplForTesting() {}
 
@@ -877,6 +880,19 @@ scooby doo
             env_call=('Void', ''),
             unchecked=False,
             gen_test_method=True,
+        ),
+        CalledByNative(
+            return_type='void',
+            system_class=False,
+            static=False,
+            name='testDisabledFoo',
+            method_id_var_name='testDisabledFoo',
+            java_class_name='',
+            params=[],
+            env_call=('Void', ''),
+            unchecked=False,
+            gen_test_method=True,
+            test_disabled=True,
         ),
         CalledByNative(
             return_type='void',
