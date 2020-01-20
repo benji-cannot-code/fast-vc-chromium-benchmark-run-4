b4c98e2a448f394e8f95afe01cad468ec7fd88f5
Sylvain Defresne <sdefresne@chromium.org>
2020-01-20 13:08:36 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/reading_list
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 1042208
    Change-Id: Id8097e1b29d40d98fb0cef10227d6bf6735a44a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007758
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733328}

--- ios/chrome/browser/reading_list/reading_list_remover_helper.h
+++ ios/chrome/browser/reading_list/reading_list_remover_helper.h
@@ -11,10 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "components/reading_list/core/reading_list_model.h"
 #include "components/reading_list/core/reading_list_model_observer.h"
-
-namespace ios {
-class ChromeBrowserState;
-}
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 class ReadingListDownloadService;
 
