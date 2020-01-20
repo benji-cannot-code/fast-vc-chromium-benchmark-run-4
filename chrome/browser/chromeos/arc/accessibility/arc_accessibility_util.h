85b930527a479f6ca65fb87ffda49ee72d3e6738
Hiroki Sato <hirokisato@google.com>
2020-01-20 01:06:32 +0000

    Set kIgnored state if node has no important property in ARC
    Some node from Android has no important content for assistive technologies.
    These nodes should have kIgnored state.
    
    Previously only the node without ImportantForAccessibility property is set
    kIgnored state. This CL also refactors how to decide which node gets kIgnored
    state in ARC.
    
    Bug: b:142701349
    Test: unit_tests --gtest_filter="AXTreeSourceArcTest.*"
    Test: manual. Open Hangouts/PlayMusic and open drawer menu. Empty item is skipped.
    Change-Id: Ia16c92787d6eb046722d1b2551586d06fc8bdc87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1961352
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Sara Kato <sarakato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733210}

--- chrome/browser/chromeos/arc/accessibility/arc_accessibility_util.h
+++ chrome/browser/chromeos/arc/accessibility/arc_accessibility_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_ARC_ACCESSIBILITY_ARC_ACCESSIBILITY_UTIL_H_
 
 #include <stdint.h>
+#include <string>
 #include <vector>
 
 #include "base/optional.h"
@@ -21,24 +22,38 @@ ax::mojom::Event ToAXEvent(mojom::AccessibilityEventType arc_event_type,
 base::Optional<mojom::AccessibilityActionType> ConvertToAndroidAction(
     ax::mojom::Action action);
 
+bool IsImportantInAndroid(mojom::AccessibilityNodeInfoData* node);
+
+bool HasImportantProperty(mojom::AccessibilityNodeInfoData* node);
+
 // TODO(hirokisato) clean up GetProperty methods in AccessibilityNodeInfoData
 // and AccessibilityWindowInfoData.
 bool GetBooleanProperty(mojom::AccessibilityNodeInfoData* node,
                         mojom::AccessibilityBooleanProperty prop);
 
-template <class InfoDataType, class PropType>
-bool GetIntListProperty(InfoDataType* node,
-                        PropType prop,
-                        std::vector<int32_t>* out_value) {
-  if (!node || !node->int_list_properties)
+template <class PropMTypeMap, class PropType, class OutType>
+bool GetProperty(PropMTypeMap properties, PropType prop, OutType* out_value) {
+  if (!properties)
     return false;
 
-  auto it = node->int_list_properties->find(prop);
-  if (it == node->int_list_properties->end())
+  auto it = properties->find(prop);
+  if (it == properties->end())
     return false;
 
   *out_value = it->second;
   return true;
+}
+
+template <class InfoDataType, class PropType>
+bool HasNonEmptyStringProperty(InfoDataType* node, PropType prop) {
+  if (!node || !node->string_properties)
+    return false;
+
+  auto it = node->string_properties->find(prop);
+  if (it == node->string_properties->end())
+    return false;
+
+  return !it->second.empty();
 }
 
 }  // namespace arc
