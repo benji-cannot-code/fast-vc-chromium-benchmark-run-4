545aa34cca72b8ef2a339cddd836d7008849e780
Sylvain Defresne <sdefresne@chromium.org>
2020-01-20 13:44:11 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/ui/commands
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=gambard@chromium.org
    
    Bug: 1042208
    Change-Id: I1b8e0708a07ca1947843688c953e802ff9c663d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2010823
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733339}

--- ios/chrome/browser/ui/commands/browsing_data_commands.h
+++ ios/chrome/browser/ui/commands/browsing_data_commands.h
@@ -10,11 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/ios/block_types.h"
 #import "components/browsing_data/core/browsing_data_utils.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 #include "ios/chrome/browser/browsing_data/browsing_data_remove_mask.h"
-
-namespace ios {
-class ChromeBrowserState;
-}
 
 // Protocol for commands that relate to browsing data.
 @protocol BrowsingDataCommands<NSObject>
