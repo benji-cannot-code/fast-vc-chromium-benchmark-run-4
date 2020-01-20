9bb8af51bb681fb136cbf989140e172147d32408
Abhijeet Kandalkar <abhijeet@igalia.com>
2020-01-20 10:40:15 +0000

    Remove unused DEFINE_{SVG_PROPERTY|AX_OBJECT}_TYPE_CASTS
    This CL has goal to remove unused
    - DEFINE_SVG_PROPERTY_TYPE_CASTS from t_p/b/renderer/core/svg
    - DEFINE_AX_OBJECT_TYPE_CASTS from t_p/b/renderer/modules/accessibility
    
    Bug: 891908
    Change-Id: I4a6164f88da586f19f748696b294276384cb4da7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2010450
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#733292}

--- third_party/blink/renderer/core/svg/svg_enumeration.cc
+++ third_party/blink/renderer/core/svg/svg_enumeration.cc
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-DEFINE_SVG_PROPERTY_TYPE_CASTS(SVGEnumerationBase);
-
 SVGEnumerationBase::~SVGEnumerationBase() = default;
 
 SVGPropertyBase* SVGEnumerationBase::CloneForAnimation(
