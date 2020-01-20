24f744092bdb64834556692cdd0f981bbd5a1f89
Roman Sorokin <rsorokin@chromium.org>
2020-01-20 10:26:24 +0000

    cros: Add timeout for waiting oauth_code cookie
    Follow-up for CL:1972837
    
    Bug: 1021297
    Change-Id: Ieb8d7f33fb525297436a6c0c32cd96c4c47ec2c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002613
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733290}

--- chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/system/sys_info.h"
 #include "base/task/post_task.h"
+#include "base/timer/timer.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
@@ -121,6 +122,8 @@ enum class ChromeOSSamlApiUsed {
   kSamlApiNotUsed = 2,
   kMaxValue = kSamlApiNotUsed,
 };
+
+constexpr base::TimeDelta kCookieDelay = base::TimeDelta::FromSeconds(20);
 
 void RecordAPILogin(bool is_third_party_idp, bool is_api_used) {
   ChromeOSSamlApiUsed login_type;
@@ -931,6 +934,11 @@ void GaiaScreenHandler::ContinueAuthenticationWhenCookiesAvailable() {
     cookie_manager->AddCookieChangeListener(
         GaiaUrls::GetInstance()->gaia_url(), kOAUTHCodeCookie,
         oauth_code_listener_.BindNewPipeAndPassRemote());
+    cookie_waiting_timer_ = std::make_unique<base::OneShotTimer>();
+    cookie_waiting_timer_->Start(
+        FROM_HERE, kCookieDelay,
+        base::BindOnce(&GaiaScreenHandler::OnCookieWaitTimeout,
+                       weak_factory_.GetWeakPtr()));
   }
 
   const net::CookieOptions cookie_options =
@@ -962,12 +970,24 @@ void GaiaScreenHandler::OnGetCookiesForCompleteAuthentication(
   UserContext user_context = *pending_user_context_;
   pending_user_context_.reset();
   oauth_code_listener_.reset();
+  cookie_waiting_timer_.reset();
 
   user_context.SetAuthCode(auth_code);
   if (!gaps_cookie.empty())
     user_context.SetGAPSCookie(gaps_cookie);
 
   LoginDisplayHost::default_host()->CompleteLogin(user_context);
+}
+
+void GaiaScreenHandler::OnCookieWaitTimeout() {
+  DCHECK(pending_user_context_);
+  pending_user_context_.reset();
+  oauth_code_listener_.reset();
+  cookie_waiting_timer_.reset();
+  LoadAuthExtension(true /* force */, false /* offline */);
+  core_oobe_view_->ShowSignInError(
+      0, l10n_util::GetStringUTF8(IDS_LOGIN_FATAL_ERROR_NO_AUTH_TOKEN),
+      std::string(), HelpAppLauncher::HELP_CANT_ACCESS_ACCOUNT);
 }
 
 void GaiaScreenHandler::HandleCompleteLogin(const std::string& gaia_id,
--- chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/gaia_screen_handler.h
@@ -29,6 +29,7 @@ class AccountId;
 
 namespace base {
 class DictionaryValue;
+class OneShotTimer;
 }  // namespace base
 
 namespace network {
@@ -335,6 +336,8 @@ class GaiaScreenHandler : public BaseScreenHandler,
       const net::CookieStatusList& cookies,
       const net::CookieStatusList& excluded_cookies);
 
+  void OnCookieWaitTimeout();
+
   bool is_security_token_pin_dialog_running() const {
     return !security_token_pin_dialog_closed_callback_.is_null();
   }
@@ -454,6 +457,7 @@ class GaiaScreenHandler : public BaseScreenHandler,
   mojo::Receiver<network::mojom::CookieChangeListener> oauth_code_listener_{
       this};
   std::unique_ptr<UserContext> pending_user_context_;
+  std::unique_ptr<base::OneShotTimer> cookie_waiting_timer_;
 
   base::WeakPtrFactory<GaiaScreenHandler> weak_factory_{this};
 
