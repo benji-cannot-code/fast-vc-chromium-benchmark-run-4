73522f48eaab2b0ad4f5671ffbe5489ac733246b
Yuki Shiino <yukishiino@chromium.org>
2020-01-20 01:59:16 +0000

    bind-gen: Improve NativeValueTraits<IDLNullable<T>>
    This patch renovates NativeValueTraits<IDLNullable<T>> again and
    implements it in a way whether T::IsNull exists or not (String::
    IsNull, ScriptValue::IsNull, AaaOrBbb::IsNull, etc.)
    
    Also implements an optimized version of IDLNullable<Interface>,
    which checks interface type first and then checks null/undefined
    value only when the check of interface type fails.  (Interface
    type is more likely than null value.)
    
    Also canonicalizes IDLDateOrNull into IDLNullable<IDLDate>,
    and IDLStringOrNullV2 into IDLNullable<IDLStringV2>.
    
    Bug: 839389
    Change-Id: Icfc95711c6084a04c06aa6f61b58966bc80a5b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2004828
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733217}

--- third_party/blink/renderer/bindings/core/v8/native_value_traits_test.cc
+++ third_party/blink/renderer/bindings/core/v8/native_value_traits_test.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/native_value_traits.h"
 
 #include <type_traits>
+
 #include "third_party/blink/renderer/bindings/core/v8/idl_types_base.h"
 
 // No gtest tests; only static_assert checks.
