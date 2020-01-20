2a34eba32d8faa1cc5ce8a39a2a73656818e0f87
Yuki Shiino <yukishiino@chromium.org>
2020-01-20 02:40:36 +0000

    IDL compiler: Produce IDL union names compatible to the old bindings generator
    Until the new bindings generator gets enabled for IDL union types,
    makes the IDL union names produced by the new bindings generator
    compatible to the old one as much as possible.
    
    Bug: 839389
    Change-Id: I3033d0e0194e9c6d1930f3654019d52f8027cfc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2009586
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733227}

--- third_party/blink/renderer/bindings/scripts/web_idl/idl_type.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/idl_type.py
@@ -1145,6 +1145,10 @@ class UnionType(IdlType):
 
     @property
     def flattened_member_types(self):
+        return set(self.flattened_member_types_in_original_order)
+
+    @property
+    def flattened_member_types_in_original_order(self):
         def flatten(idl_type):
             if idl_type.is_union:
                 return functools.reduce(
@@ -1157,7 +1161,7 @@ class UnionType(IdlType):
             else:
                 return [idl_type]
 
-        return set(flatten(self))
+        return flatten(self)
 
     @property
     def union_definition_object(self):
