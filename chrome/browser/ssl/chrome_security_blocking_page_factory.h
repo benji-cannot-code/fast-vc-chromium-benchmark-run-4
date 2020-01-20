e4efcf0677f6f65aca7c65866306fc6275ad503f
Colin Blundell <blundell@chromium.org>
2020-01-20 09:40:46 +0000

    [SSL] Create BlockedInterceptionBlockingPage via //chrome factory
    In preparation for componentizing this blocking page, this CL moves
    it to be created via ChromeSecurityBlockingPageFactory and thereby
    cleaned of its //chrome-level dependencies.
    
    Bug: 1030692
    Change-Id: Icb2693512d04fa504e4ac059b06761b287212502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2000743
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733278}

--- chrome/browser/ssl/chrome_security_blocking_page_factory.h
+++ chrome/browser/ssl/chrome_security_blocking_page_factory.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SSL_CHROME_SECURITY_BLOCKING_PAGE_FACTORY_H_
 
 #include "base/macros.h"
+#include "chrome/browser/ssl/blocked_interception_blocking_page.h"
 #include "components/security_interstitials/content/bad_clock_blocking_page.h"
 #include "components/security_interstitials/content/captive_portal_blocking_page.h"
 #include "components/security_interstitials/content/mitm_software_blocking_page.h"
@@ -61,6 +62,15 @@ class ChromeSecurityBlockingPageFactory {
       const net::SSLInfo& ssl_info,
       const std::string& mitm_software_name,
       bool is_enterprise_managed);
+
+  // Creates a blocked interception blocking page. The caller is
+  // responsible for ownership of the returned object.
+  static BlockedInterceptionBlockingPage* CreateBlockedInterceptionBlockingPage(
+      content::WebContents* web_contents,
+      int cert_error,
+      const GURL& request_url,
+      std::unique_ptr<SSLCertReporter> ssl_cert_reporter,
+      const net::SSLInfo& ssl_info);
 
   // Does setup on |page| that is specific to the client (Chrome).
   static void DoChromeSpecificSetup(SSLBlockingPageBase* page);
