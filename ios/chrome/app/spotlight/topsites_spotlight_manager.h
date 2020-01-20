5406cbd994c63ad95a88dd31a6e80766fff29900
Sylvain Defresne <sdefresne@chromium.org>
2020-01-20 14:08:22 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/app/spotlight
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 1042208
    Change-Id: I66eec2bebef6f80951c0537bb067d38ff75cac46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007815
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733349}

--- ios/chrome/app/spotlight/topsites_spotlight_manager.h
+++ ios/chrome/app/spotlight/topsites_spotlight_manager.h
@@ -7,10 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_APP_SPOTLIGHT_TOPSITES_SPOTLIGHT_MANAGER_H_
 
 #import "ios/chrome/app/spotlight/base_spotlight_manager.h"
-
-namespace ios {
-class ChromeBrowserState;
-}  // namespace ios
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 // This spotlight manager handles indexing of sites shown on the NTP. Because of
 // privacy concerns, only sites shown on the NTP are indexed; therefore, this
