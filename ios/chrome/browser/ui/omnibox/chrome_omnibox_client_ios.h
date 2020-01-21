071db44dd9c2b3515ce81fec60bb0abfe6f7010f
Sylvain Defresne <sdefresne@chromium.org>
2020-01-21 17:04:15 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/ui/omnibox
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 1042208
    Change-Id: I6d69a166e7e33905b8ddde566cdd1f411d7a63a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2010842
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733638}

--- ios/chrome/browser/ui/omnibox/chrome_omnibox_client_ios.h
+++ ios/chrome/browser/ui/omnibox/chrome_omnibox_client_ios.h
@@ -11,12 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "components/omnibox/browser/omnibox_client.h"
 #include "ios/chrome/browser/autocomplete/autocomplete_scheme_classifier_impl.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 class WebOmniboxEditController;
-
-namespace ios {
-class ChromeBrowserState;
-}
 
 class ChromeOmniboxClientIOS : public OmniboxClient {
  public:
--- ios/chrome/browser/ui/omnibox/omnibox_coordinator.h
+++ ios/chrome/browser/ui/omnibox/omnibox_coordinator.h
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-namespace ios {
-class ChromeBrowserState;
-}
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
+
 class WebOmniboxEditController;
 @class CommandDispatcher;
 @protocol EditViewAnimatee;
