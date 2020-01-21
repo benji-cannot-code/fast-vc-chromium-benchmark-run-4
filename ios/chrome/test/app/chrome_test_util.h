969d5e358a58f637520c50fec3514a3640897bd0
Sylvain Defresne <sdefresne@chromium.org>
2020-01-21 18:40:10 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/test
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=eugenebut@chromium.org
    
    Bug: 1042208
    Change-Id: I7a7bc2ace673d0c7e57dfc2f2a2e565c44c7c4fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007463
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733677}

--- ios/chrome/test/app/chrome_test_util.h
+++ ios/chrome/test/app/chrome_test_util.h
@@ -9,11 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #import "base/ios/block_types.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 #import "ios/chrome/browser/ui/commands/browser_commands.h"
-
-namespace ios {
-class ChromeBrowserState;
-}
 
 @protocol ApplicationCommands;
 @class DeviceSharingManager;
