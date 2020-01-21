1d1b276aba28d46cd5cba786c07ccfdf84fb3b71
Maksim Ivanov <emaxx@chromium.org>
2020-01-21 19:14:17 +0000

    Show "TPM update required" error on login screen
    Implement displaying the "TPM firmware update required" error on the
    Login Screen, in case this error is received from the cryptohomed
    daemon.
    
    This error is currently only generated when a smart card authentication
    is attempted to be used on a Chromebook with a TPM chip that is known to
    be ROCA-vulnerable (see
    https://www.chromium.org/chromium-os/tpm_firmware_update). In order to
    fix the error the user or the admin need, as described in the error
    message, to install the update of the TPM's firmware according to the
    instructions. Before this CL, the only thing the user would see is that
    the login fails for some unknown reason.
    
    Bug: 1011327
    Test: manual - find a device with a ROCA-vulnerable TPM 1.2 chip, try to add a new user using a smart card, check that the error is displayed
    Change-Id: I0039950903196f702994404304c8d89da7e93879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2010985
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733694}

--- chromeos/login/auth/cryptohome_authenticator.cc
+++ chromeos/login/auth/cryptohome_authenticator.cc
@@ -132,6 +132,8 @@ const char* AuthStateToString(CryptohomeAuthenticator::AuthState state) {
       return "FAILED_PREVIOUS_MIGRATION_INCOMPLETE";
     case CryptohomeAuthenticator::OFFLINE_NO_MOUNT:
       return "OFFLINE_NO_MOUNT";
+    case CryptohomeAuthenticator::TPM_UPDATE_REQUIRED:
+      return "TPM_UPDATE_REQUIRED";
   }
   return "UNKNOWN";
 }
@@ -1032,6 +1034,13 @@ void CryptohomeAuthenticator::Resolve() {
           base::BindOnce(&CryptohomeAuthenticator::OnAuthFailure, this,
                          AuthFailure(AuthFailure::MISSING_CRYPTOHOME)));
       break;
+    case TPM_UPDATE_REQUIRED:
+      current_state_->ResetCryptohomeStatus();
+      task_runner_->PostTask(
+          FROM_HERE,
+          base::BindOnce(&CryptohomeAuthenticator::OnAuthFailure, this,
+                         AuthFailure(AuthFailure::TPM_UPDATE_REQUIRED)));
+      break;
     default:
       NOTREACHED();
       break;
@@ -1111,6 +1120,10 @@ CryptohomeAuthenticator::ResolveCryptohomeFailureState() {
   if (current_state_->cryptohome_code() ==
       cryptohome::MOUNT_ERROR_PREVIOUS_MIGRATION_INCOMPLETE) {
     return FAILED_PREVIOUS_MIGRATION_INCOMPLETE;
+  }
+  if (current_state_->cryptohome_code() ==
+      cryptohome::MOUNT_ERROR_TPM_UPDATE_REQUIRED) {
+    return TPM_UPDATE_REQUIRED;
   }
 
   // Return intermediate states in the following case:
