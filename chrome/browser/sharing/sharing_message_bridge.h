00b747dbb98c229e504c63825753c567264c77bb
Rushan Suleymanov <rushans@google.com>
2020-01-21 14:12:30 +0000

    [Sync] Add SharingMessageBridge implementation.
    Add SharingMessageBridge without wiring it. The bridge does not store any
    data on persistent storage.
    
    Bug: 1034930
    Change-Id: I0b19a136a2cc8a3e9ec11b7c1bb0f0b629996e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2002524
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#733594}

---
+++ chrome/browser/sharing/sharing_message_bridge.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SHARING_SHARING_MESSAGE_BRIDGE_H_
+#define CHROME_BROWSER_SHARING_SHARING_MESSAGE_BRIDGE_H_
+
+#include <memory>
+
+#include "components/sync/protocol/sharing_message_specifics.pb.h"
+
+// Class to provide an interface to send sharing messages using Sync.
+class SharingMessageBridge {
+ public:
+  // TODO(crbug.com/1034930): take callbacks once commit error propagation back
+  // to the bridge is implemented.
+  virtual void SendSharingMessage(
+      std::unique_ptr<sync_pb::SharingMessageSpecifics> specifics) = 0;
+
+  virtual ~SharingMessageBridge() = default;
+};
+
+#endif  // CHROME_BROWSER_SHARING_SHARING_MESSAGE_BRIDGE_H_
