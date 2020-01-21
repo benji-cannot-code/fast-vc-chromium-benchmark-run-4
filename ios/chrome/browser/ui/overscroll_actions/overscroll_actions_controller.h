0f8916c3d1c433eb3d16901a1f45cad7e493214d
Sylvain Defresne <sdefresne@chromium.org>
2020-01-21 17:22:05 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/ui/overscroll_actions
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=kkhorimoto@chromium.org
    
    Bug: 1042208
    Change-Id: I219c99f59c5ffe8b954bdf90667d5c5847536484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007286
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733644}

--- ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.h
+++ ios/chrome/browser/ui/overscroll_actions/overscroll_actions_controller.h
@@ -8,14 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 #import "ios/chrome/browser/ui/overscroll_actions/overscroll_actions_view.h"
 #import "ios/chrome/browser/ui/util/relaxed_bounds_constraints_hittest.h"
 #import "ios/web/public/ui/crw_web_view_scroll_view_proxy.h"
 
 @protocol CRWWebViewProxy;
-namespace ios {
-class ChromeBrowserState;
-}
 @class OverscrollActionsController;
 
 // Describe the current state of the overscroll action controller.
