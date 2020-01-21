78c3a0f1f7577e7d8e0f88f518c8e5f9e6f29d60
Sylvain Defresne <sdefresne@chromium.org>
2020-01-21 19:35:57 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/ntp_tiles
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=mahmadi@chromium.org
    
    Bug: 1042208
    Change-Id: I01d3540bee98e66b436e82135179f05dddc6ee5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007745
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733699}

--- ios/chrome/browser/ntp_tiles/ios_popular_sites_factory.h
+++ ios/chrome/browser/ntp_tiles/ios_popular_sites_factory.h
@@ -8,9 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-namespace ios {
-class ChromeBrowserState;
-}
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 namespace ntp_tiles {
 class PopularSites;
