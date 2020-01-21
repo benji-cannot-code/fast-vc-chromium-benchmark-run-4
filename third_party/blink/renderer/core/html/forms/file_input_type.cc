ae69ae36fd66867e1e639ad6e40ca912a2a029ac
Kent Tamura <tkent@chromium.org>
2020-01-21 04:32:49 +0000

    Merge invalidation request code in FileInputType into one
    They are merged into FileInputType::UpdateView().
    This CL has no behavior changes.
    
    Bug: 1040828
    Change-Id: Ie08598ce562842eee02b15575bcf278c4ce58b0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2011464
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733520}

--- third_party/blink/renderer/core/html/forms/file_input_type.cc
+++ third_party/blink/renderer/core/html/forms/file_input_type.cc
@@ -250,9 +250,8 @@ void FileInputType::SetValue(const String&,
     return;
 
   file_list_->clear();
-  if (auto* layout_object = GetElement().GetLayoutObject())
-    layout_object->SetShouldDoFullPaintInvalidation();
   GetElement().SetNeedsValidityCheck();
+  UpdateView();
 }
 
 FileList* FileInputType::CreateFileList(const FileChooserFileInfoList& files,
@@ -378,10 +377,7 @@ bool FileInputType::SetFiles(FileList* files) {
 
   GetElement().NotifyFormStateChanged();
   GetElement().SetNeedsValidityCheck();
-
-  if (GetElement().GetLayoutObject())
-    GetElement().GetLayoutObject()->SetShouldDoFullPaintInvalidation();
-
+  UpdateView();
   return files_changed;
 }
 
@@ -523,6 +519,11 @@ void FileInputType::WillOpenPopup() {
     UseCounter::Count(GetElement().GetDocument(),
                       WebFeature::kPopupOpenWhileFileChooserOpened);
   }
+}
+
+void FileInputType::UpdateView() {
+  if (auto* layout_object = GetElement().GetLayoutObject())
+    layout_object->SetShouldDoFullPaintInvalidation();
 }
 
 }  // namespace blink
