049badea2ba044d319e4927c7e626dbedd85816f
Steve Becker <stevebe@microsoft.com>
2020-01-21 21:39:14 +0000

    Add meta:timeout=long to FileSystemBaseHandle-postMessage.js tests
    Both the sandboxed and native versions of the NativeFileSystem
    postMessage() test are flaky due to timeouts in the CI.  Looking at
    recent results, these tests take 5 or more seconds to complete
    successfully.  This amount of time is dangerously close to the 7 second
    timeout, which sometimes occurs, causing the flakiness.
    
    This change adds "meta timeout=long" to both test variants to extend the
    7 second timeout to a longer value, which gives these tests enough time
    to pass.
    
    Bug: 1043435
    Bug: 1043028
    Change-Id: I73eab928bce53a36be8c33e4b6e5746e7195f16a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2013580
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#733757}

--- third_party/blink/web_tests/external/wpt/native-file-system/sandboxed_FileSystemBaseHandle-postMessage.tentative.https.window.js
+++ third_party/blink/web_tests/external/wpt/native-file-system/sandboxed_FileSystemBaseHandle-postMessage.tentative.https.window.js
@@ -6,3 +6,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=resources/messaging-blob-helpers.js
 // META: script=resources/messaging-serialize-helpers.js
 // META: script=script-tests/FileSystemBaseHandle-postMessage.js
+// META: timeout=long
