65c22b0c6d40165594857111f51a12310606fd78
Sylvain Defresne <sdefresne@chromium.org>
2020-01-21 05:55:34 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/language
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=dvallet@chromium.org
    
    Bug: 1042208
    Change-Id: Ic014ac6c3dc2cdaec2cbf7a088a32d7d6ce529f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007423
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Commit-Queue: David Vallet <dvallet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733535}

--- ios/chrome/browser/language/language_model_manager_factory.h
+++ ios/chrome/browser/language/language_model_manager_factory.h
@@ -12,10 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-namespace ios {
-class ChromeBrowserState;
-}
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 namespace language {
 class LanguageModelManager;
