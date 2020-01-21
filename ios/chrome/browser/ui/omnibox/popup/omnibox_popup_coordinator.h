fc8a46dbebe7945a30f7ef72f79b0383f5c6934e
Sylvain Defresne <sdefresne@chromium.org>
2020-01-21 18:12:05 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/ui/omnibox/popup
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=stkhapugin@chromium.org
    
    Bug: 1042208
    Change-Id: I9803ea5990e01aac1718b05cf422f125e332eaf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007421
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733667}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_coordinator.h
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_coordinator.h
@@ -10,14 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
+
 @class CommandDispatcher;
 @protocol OmniboxPopupPresenterDelegate;
 @protocol OmniboxFocuser;
 class OmniboxPopupViewIOS;
-
-namespace ios {
-class ChromeBrowserState;
-}
 class WebStateList;
 
 // Coordinator for the Omnibox Popup.
