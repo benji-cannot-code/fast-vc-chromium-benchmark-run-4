1d72ddaf07e0d05c5d450bf2909fb8d4452a9530
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-01-21 08:36:04 +0000

    Reland "[base] Migrate PostTaskAndReplyWithResult to OnceCallbacks"
    This reverts commit 28376809f5d1f04fa10e7ec888fd943405328220.
    
    Reason for revert: This change addresses the compilation error.
    
    Original change's description:
    > Revert "[base] Migrate PostTaskAndReplyWithResult to OnceCallbacks"
    >
    > This reverts commit 0593d72b54b2be3b607934420edb84075f11cb82.
    >
    > Reason for revert: Compiles failing, examples:
    > https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/1791
    > https://ci.chromium.org/p/chrome/builders/ci/win32-builder-perf/148062
    >
    > Looks mostly like ifdefed callsites that weren't migrated.
    >
    > Original change's description:
    > > [base] Migrate PostTaskAndReplyWithResult to OnceCallbacks
    > >
    > > This change migrates users of base::PostTaskAndReplyWithResult to
    > > use base::OnceCallbacks. This allows removing the CallbackType template
    > > from the API, making sure future users are required to use OnceCallbacks.
    > >
    > > TBR=danakj
    > >
    > > Bug: 714018
    > > Change-Id: I8453c351e406a98fc8b9fddce2637331fdbbf5c4
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965664
    > > Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    > > Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    > > Reviewed-by: danakj <danakj@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#732403}
    >
    > TBR=lambroslambrou@chromium.org,danakj@chromium.org,jdoerrie@chromium.org
    >
    > Change-Id: I9a417e1d289c750f5ce5e1f43972bf09c0b23e4f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 714018
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2004968
    > Reviewed-by: Leonard Grey <lgrey@chromium.org>
    > Commit-Queue: Leonard Grey <lgrey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#732414}
    
    TBR=lambroslambrou@chromium.org,danakj@chromium.org,lgrey@chromium.org,jdoerrie@chromium.org
    
    Change-Id: I7cfc4ef768e4020e4d87300f7e8b57cdfff05fb3
    Bug: 714018
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2005028
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733548}

--- chrome/browser/chromeos/policy/status_collector/device_status_collector.cc
+++ chrome/browser/chromeos/policy/status_collector/device_status_collector.cc
@@ -608,8 +608,9 @@ class DeviceStatusCollectorState : public StatusCollectorState {
     base::PostTaskAndReplyWithResult(
         FROM_HERE,
         {base::ThreadPool(), base::MayBlock(), base::TaskPriority::BEST_EFFORT},
-        base::Bind(volume_info_fetcher, mount_points),
-        base::Bind(&DeviceStatusCollectorState::OnVolumeInfoReceived, this));
+        base::BindOnce(volume_info_fetcher, mount_points),
+        base::BindOnce(&DeviceStatusCollectorState::OnVolumeInfoReceived,
+                       this));
   }
 
   // Queues an async callback to query CPU temperature information.
@@ -619,8 +620,9 @@ class DeviceStatusCollectorState : public StatusCollectorState {
     base::PostTaskAndReplyWithResult(
         FROM_HERE,
         {base::ThreadPool(), base::MayBlock(), base::TaskPriority::BEST_EFFORT},
-        cpu_temp_fetcher,
-        base::Bind(&DeviceStatusCollectorState::OnCPUTempInfoReceived, this));
+        base::BindOnce(cpu_temp_fetcher),
+        base::BindOnce(&DeviceStatusCollectorState::OnCPUTempInfoReceived,
+                       this));
   }
 
   bool FetchAndroidStatus(
@@ -989,16 +991,16 @@ DeviceStatusCollector::DeviceStatusCollector(
   base::PostTaskAndReplyWithResult(
       FROM_HERE,
       {base::ThreadPool(), base::MayBlock(), base::TaskPriority::BEST_EFFORT},
-      base::Bind(&chromeos::version_loader::GetVersion,
-                 chromeos::version_loader::VERSION_FULL),
-      base::Bind(&DeviceStatusCollector::OnOSVersion,
-                 weak_factory_.GetWeakPtr()));
+      base::BindOnce(&chromeos::version_loader::GetVersion,
+                     chromeos::version_loader::VERSION_FULL),
+      base::BindOnce(&DeviceStatusCollector::OnOSVersion,
+                     weak_factory_.GetWeakPtr()));
   base::PostTaskAndReplyWithResult(
       FROM_HERE,
       {base::ThreadPool(), base::MayBlock(), base::TaskPriority::BEST_EFFORT},
-      base::Bind(&ReadFirmwareVersion),
-      base::Bind(&DeviceStatusCollector::OnOSFirmware,
-                 weak_factory_.GetWeakPtr()));
+      base::BindOnce(&ReadFirmwareVersion),
+      base::BindOnce(&DeviceStatusCollector::OnOSFirmware,
+                     weak_factory_.GetWeakPtr()));
   chromeos::tpm_util::GetTpmVersion(base::BindOnce(
       &DeviceStatusCollector::OnTpmVersion, weak_factory_.GetWeakPtr()));
 
@@ -1179,9 +1181,9 @@ void DeviceStatusCollector::SampleResourceUsage() {
   base::PostTaskAndReplyWithResult(
       FROM_HERE,
       {base::ThreadPool(), base::MayBlock(), base::TaskPriority::BEST_EFFORT},
-      cpu_statistics_fetcher_,
-      base::Bind(&DeviceStatusCollector::ReceiveCPUStatistics,
-                 weak_factory_.GetWeakPtr()));
+      base::BindOnce(cpu_statistics_fetcher_),
+      base::BindOnce(&DeviceStatusCollector::ReceiveCPUStatistics,
+                     weak_factory_.GetWeakPtr()));
 }
 
 void DeviceStatusCollector::ReceiveCPUStatistics(const std::string& stats) {
