bab11b2a160753f449c1530aea3ce2d1d4bb93e4
Raymond Toy <rtoy@chromium.org>
2020-01-21 23:43:30 +0000

    Allow PFFFT to work on OSX
    Allow compiling PFFFT for OSX.  This is mostly for testing and
    debugging.  Unless PFFFT performance improves, the OSX FFT routines
    are much faster so we should not default to using PFFFT.
    
    Bug: 1042503
    Change-Id: I217686e264eb290a8344afab1ac4da5073e1fe26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2004067
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733804}

--- third_party/blink/renderer/platform/audio/fft_frame.h
+++ third_party/blink/renderer/platform/audio/fft_frame.h
@@ -39,13 +39,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 #include "third_party/blink/renderer/platform/wtf/threading.h"
 
-#if defined(OS_MACOSX)
-#include <Accelerate/Accelerate.h>
-#elif defined(WTF_USE_WEBAUDIO_FFMPEG)
+#if defined(WTF_USE_WEBAUDIO_FFMPEG)
 struct RDFTContext;
 #elif defined(WTF_USE_WEBAUDIO_PFFFT)
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "third_party/pffft/src/pffft.h"
+#elif defined(OS_MACOSX)
+#include <Accelerate/Accelerate.h>
 #endif
 
 namespace blink {
@@ -145,7 +145,7 @@ class PLATFORM_EXPORT FFTFrame {
   AudioFloatArray real_data_;
   AudioFloatArray imag_data_;
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) && !defined(WTF_USE_WEBAUDIO_PFFFT)
   // Thin wrapper around FFTSetup so we can call the appropriate routines to
   // construct or release the FFTSetup objects.
   class FFTSetupDatum {
