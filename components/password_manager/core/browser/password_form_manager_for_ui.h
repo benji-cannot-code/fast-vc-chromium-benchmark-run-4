ea086ebfd16d0a21e34128afa30b91cb6603fda2
Mohamed Amir Yosef <mamir@chromium.org>
2020-01-21 13:43:59 +0000

    [Passwords] PasswordFormManagerForUI::MoveCredentialsToAccountStore()
    This CL introduce the method
    PasswordFormManagerForUI::MoveCredentialsToAccountStore() which would
    be called upon user invocation of the migration flow in the UI, and
    wires the call to the PasswordSaveManager.
    
    This CL doesn't introduce the invocation logic or the UI pieces.
    
    Bug: 1032992
    Change-Id: I6e887bb1a363d45fc15152a52e6cec5db0e00c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2012022
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733588}

--- components/password_manager/core/browser/password_form_manager_for_ui.h
+++ components/password_manager/core/browser/password_form_manager_for_ui.h
@@ -96,6 +96,10 @@ class PasswordFormManagerForUI {
 
   // Called when the passwords were shown on on the bubble without obfuscation.
   virtual void OnPasswordsRevealed() = 0;
+
+  // A user opted to move the credentials used for a successful login from the
+  // profile store to the account store.
+  virtual void MoveCredentialsToAccountStore() = 0;
 };
 
 }  // namespace  password_manager
