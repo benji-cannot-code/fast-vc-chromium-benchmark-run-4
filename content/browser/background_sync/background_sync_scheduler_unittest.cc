69e4f4a35914e96754434ac9aef58b41cc3322c8
Rayan Kanso <rayankans@google.com>
2020-01-22 00:44:16 +0000

    [BackgroundSync] Limit calls to the Background Task Scheduler.
    Keep track of the next wake up time before sending a schedule request to
    the BackgroundSyncBackgroundTaskScheduler.
    
    Bug: 1042041
    Change-Id: I02ce7b418ca2546937d7768e8df2cd53bf15822e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2005230
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733824}

--- content/browser/background_sync/background_sync_scheduler_unittest.cc
+++ content/browser/background_sync/background_sync_scheduler_unittest.cc
@@ -94,6 +94,14 @@ class BackgroundSyncSchedulerTest : public testing::Test {
     return GetController()->GetBrowserWakeupDelay(sync_type);
   }
 
+  base::TimeTicks GetBrowserWakeupTime() {
+    auto* scheduler = BackgroundSyncScheduler::GetFor(&test_browser_context_);
+    DCHECK(scheduler);
+
+    return scheduler
+        ->scheduled_wakeup_time_[blink::mojom::BackgroundSyncType::ONE_SHOT];
+  }
+
   void SetUp() override {
     original_client_ = SetBrowserClientForTesting(&browser_client_);
   }
@@ -282,6 +290,7 @@ TEST_F(BackgroundSyncSchedulerTest,
   base::RunLoop().RunUntilIdle();
   EXPECT_LE(GetBrowserWakeupDelay(blink::mojom::BackgroundSyncType::ONE_SHOT),
             base::TimeDelta::FromSeconds(1));
+  auto wakeup_time1 = GetBrowserWakeupTime();
 
   CancelDelayedProcessing(GURL(kUrl_2),
                           blink::mojom::BackgroundSyncType::ONE_SHOT);
@@ -291,6 +300,7 @@ TEST_F(BackgroundSyncSchedulerTest,
             base::TimeDelta::FromMinutes(1));
   EXPECT_GT(GetBrowserWakeupDelay(blink::mojom::BackgroundSyncType::ONE_SHOT),
             base::TimeDelta::FromSeconds(1));
+  EXPECT_LT(wakeup_time1, GetBrowserWakeupTime());
 }
 
 #endif
