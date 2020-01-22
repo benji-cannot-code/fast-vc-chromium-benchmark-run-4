ffbe885406c9bcf6394a352eddb48858f82bd3a9
Luciano Pacheco <lucmult@chromium.org>
2020-01-22 02:40:20 +0000

    Add Files app extension id to whitelist on feedback logs
    Currently is hard to debug Files app feedback report because Files app
    URL is redacted from logs, the URL is similar to:
    chrome-extension://$EXTENSIO_ID/.
    
    Bug: 979779
    Change-Id: If08ddae7625c431bfe6ca67ac3dd257f14e8410f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2011581
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733850}

--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/common/extensions/extension_constants.h"
+
 #include "base/macros.h"
 #include "build/build_config.h"
-#include "chrome/common/extensions/extension_constants.h"
+#include "extensions/common/constants.h"
 
 namespace extension_urls {
 
@@ -78,6 +80,7 @@ const char* const kBuiltInFirstPartyExtensionIds[] = {
     kAutoclickExtensionId,
     kSelectToSpeakExtensionId,
     kSwitchAccessExtensionId,
+    kFilesManagerAppId,
     kFirstRunDialogId,
     kEspeakSpeechSynthesisExtensionId,
     kGoogleSpeechSynthesisExtensionId,
