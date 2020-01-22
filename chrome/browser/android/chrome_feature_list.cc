831c297ce0f30c4ce504641f4981e25da5e7668f
Henrique Nakashima <hnakashima@chromium.org>
2020-01-22 22:48:09 +0000

    Move ChromeFeatureList to a new chrome/browser/flags:java target.
    This is the first file to be moved into the new flags module.
    
    This is CL 3/3 to move ChromeFeatureList to chrome/browser/flags:java
    
    Bug: 1041468
    Change-Id: Ie52917a32896d7ee721c4b2278fa7bc6884b417d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007315
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734216}

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/stl_util.h"
-#include "chrome/android/chrome_jni_headers/ChromeFeatureList_jni.h"
+#include "chrome/browser/flags/jni_headers/ChromeFeatureList_jni.h"
 #include "chrome/browser/share/features.h"
 #include "chrome/browser/sharing/shared_clipboard/feature_flags.h"
 #include "chrome/common/chrome_features.h"
