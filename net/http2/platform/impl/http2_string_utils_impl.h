4811a867187e6f69f903f771107e40aca37569f8
Bence Béky <bnc@chromium.org>
2020-01-22 20:16:23 +0000

    Roll src/net/third_party/quiche/src/ dfabdfb68..f579b5e51 (5 commits)
    https://quiche.googlesource.com/quiche.git/+log/dfabdfb6884b..f579b5e51aaf
    
    $ git log dfabdfb68..f579b5e51 --date=short --no-merges --format='%ad %ae %s'
    2020-01-21 nharper Refactor TlsServerHandshaker to implement QuicCryptoServerStreamBase directly
    2020-01-21 fayang Disable gfe2_restart_flag_quic_default_on_ietf_loss_detection in QuicEndpointTest.Competition while investigating the issue.
    2020-01-21 wub gfe-relnote: (n/a) Change the return value of QuicUdpSocketApi::ReadMultiplePackets from number of good packets to number of total packets read. Code not used yet.
    2020-01-21 bnc Use quiche_export instead of http2_export.
    2020-01-21 ianswett gfe-relnote: Add a connection option(1ACK) to send only one immediate ACK in QUIC after reordering, instead of 4.  Protected by gfe2_reloadable_flag_quic_one_immediate_ack
    
    Created with:
      roll-dep src/net/third_party/quiche/src
    
    Change-Id: I6a3b22464f673884affc55883c090a8df1dead3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014053
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Auto-Submit: Bence Béky <bnc@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734145}

--- net/http2/platform/impl/http2_string_utils_impl.h
+++ net/http2/platform/impl/http2_string_utils_impl.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/escape.h"
 #include "net/base/hex_utils.h"
 #include "net/third_party/quiche/src/common/platform/api/quiche_string_piece.h"
-#include "net/third_party/quiche/src/http2/platform/api/http2_export.h"
 
 namespace http2 {
 
