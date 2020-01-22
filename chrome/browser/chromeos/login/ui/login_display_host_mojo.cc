ec9af0348b86fe7a4361728d3ccb674f5bac5408
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-01-22 12:43:03 +0000

    [Kiosk] Remove unnsessesary duplications in LoginDisplayHost
    Function calls in OnStart{Web|Arc}Kiosk are identical to
    OnStartAppLaunch. Let's remove them.
    
    Bug: 1043663
    Change-Id: I9f748f4d32374eb9cc622dd6ca80e8375c29c745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2011838
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733961}

--- chrome/browser/chromeos/login/ui/login_display_host_mojo.cc
+++ chrome/browser/chromeos/login/ui/login_display_host_mojo.cc
@@ -251,14 +251,6 @@ void LoginDisplayHostMojo::OnStartAppLaunch() {
   ShowFullScreen();
 }
 
-void LoginDisplayHostMojo::OnStartArcKiosk() {
-  ShowFullScreen();
-}
-
-void LoginDisplayHostMojo::OnStartWebKiosk() {
-  ShowFullScreen();
-}
-
 void LoginDisplayHostMojo::OnBrowserCreated() {
   NOTIMPLEMENTED();
 }
