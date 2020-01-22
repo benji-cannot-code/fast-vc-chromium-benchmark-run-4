f1aa67160a4aa3fdfb65908d883de11845712024
Jochen Eisinger <jochen@chromium.org>
2020-01-22 17:04:32 +0000

    Add about:flags entry for BlockExternalFormRedirectsNoGesture feature
    Bug: 927509
    Change-Id: I40a99ea449f51fa20553a480de18768575290f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2013124
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734044}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1562,6 +1562,9 @@ extern const char kHomepageLocationDescription[];
 extern const char kHomepageSettingsUIConversionName[];
 extern const char kHomepageSettingsUIConversionDescription[];
 
+extern const char kIntentBlockExternalFormRedirectsNoGestureName[];
+extern const char kIntentBlockExternalFormRedirectsNoGestureDescription[];
+
 extern const char kInterestFeedNotificationsName[];
 extern const char kInterestFeedNotificationsDescription[];
 
