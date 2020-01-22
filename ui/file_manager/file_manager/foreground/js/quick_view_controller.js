794fda3828b2dd24adfd07893d4d83f7b7d3026f
Isabella Scalzi <isabellascalzi@google.com>
2020-01-22 06:21:55 +0000

    [quickview] address comments in CL:2011584
    The current code for updating |this.tasks_| does not account for when
    |this.tasks_| is an empty array. Fix this: Only update |this.tasks_| if
    there are tasks to update.
    
    Also remove redundant function |getAvailableTasks_|.
    
    Bug: 1038799
    Change-Id: Iacf74ad6d620ce5001a7de73bfa762b0ea0f0be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014206
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733891}

--- ui/file_manager/file_manager/foreground/js/quick_view_controller.js
+++ ui/file_manager/file_manager/foreground/js/quick_view_controller.js
@@ -295,15 +295,6 @@ class QuickViewController {
   }
 
   /**
-   * @param {!FileEntry} entry
-   * @return {!Promise<!FileTasks>}
-   * @private
-   */
-  getAvailableTasks_(entry) {
-    return this.taskController_.getEntryFileTasks(entry);
-  }
-
-  /**
    * Update quick view using current entries.
    *
    * @return {!Promise} Promise fulfilled after quick view is updated.
@@ -329,7 +320,7 @@ class QuickViewController {
     return Promise
         .all([
           this.metadataModel_.get([entry], ['thumbnailUrl']),
-          this.getAvailableTasks_(entry)
+          this.taskController_.getEntryFileTasks(entry)
         ])
         .then(values => {
           const items = (/**@type{Array<MetadataItem>}*/ (values[0]));
@@ -374,7 +365,10 @@ class QuickViewController {
         browsable: params.browsable || false,
       });
 
-      this.tasks_ = fileTasks;
+      if (params.hasTask) {
+        this.tasks_ = fileTasks;
+      }
+
     });
   }
 
