125d113458ea196669e63248ef820aaaabc97be5
Ben Wagner <bungeman@chromium.org>
2020-01-22 18:07:10 +0000

    Roll src/third_party/freetype/src/ 50b013871..e5038be70 (2 commits)
    https://chromium.googlesource.com/chromium/src/third_party/freetype2.git/+log/50b013871c53..e5038be70414
    
    $ git log 50b013871..e5038be70 --date=short --no-merges --format='%ad %ae %s'
    2020-01-19 wl Update all copyright notices.
    2020-01-04 wl [base] Fix `FREETYPE_PROPERTIES=type1:hinting-engine=adobe`.
    
    Created with:
      roll-dep src/third_party/freetype/src
    R=bungeman@chromium.org,drott@chromium.org
    
    CQ_INCLUDE_TRYBOTS=luci.chromium.try:linux_chromium_msan_rel_ng
    
    Change-Id: Id71ab1c45b05cb211948381805f3fe2a491845d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014054
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734070}

--- third_party/freetype/include/freetype-custom-config/ftoption.h
+++ third_party/freetype/include/freetype-custom-config/ftoption.h
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  *   User-selectable configuration macros (specification only).
  *
- * Copyright (C) 1996-2019 by
+ * Copyright (C) 1996-2020 by
  * David Turner, Robert Wilhelm, and Werner Lemberg.
  *
  * This file is part of the FreeType project, and may only be used,
