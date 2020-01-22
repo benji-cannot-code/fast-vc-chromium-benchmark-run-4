871d3e80fde55e126781a65398761c40a3784a6d
Connie Wan <connily@chromium.org>
2020-01-22 04:36:23 +0000

    Order all TabSlotViews in TabStrip (Tab Group a11y Part 2)
    Part 2 of many.
    
    With this CL, it should be possible to traverse the tab strip via keyboard and interact with both Tabs and TabGroupHeaders in an expected manner.
    
    See Part 1 (crrev.com/c/1998146) for interactions that are still missing.
    
    Bug: 1039921
    Change-Id: I277a6a9cec78b8677e411e248b4ed9adf872e9b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1999088
    Commit-Queue: Connie Wan <connily@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733873}

--- chrome/browser/ui/webui/tab_strip/tab_strip_ui_handler.cc
+++ chrome/browser/ui/webui/tab_strip/tab_strip_ui_handler.cc
@@ -239,6 +239,10 @@ void TabStripUIHandler::OnTabGroupChanged(const TabGroupChange& change) {
       break;
     }
 
+    case TabGroupChange::kMoved: {
+      break;
+    }
+
     case TabGroupChange::kClosed: {
       FireWebUIListener("tab-group-closed",
                         base::Value(change.group.ToString()));
