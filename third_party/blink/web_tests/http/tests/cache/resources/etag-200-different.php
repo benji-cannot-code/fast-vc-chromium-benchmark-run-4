88151ecb995c760333823b68a14ac868230ace7b
Rob Buis <rbuis@igalia.com>
2020-01-22 22:28:47 +0000

    Adjust tests so that response returns text/css
    Adjust tests so that response returns text/css in order
    to not fail these tests once stylesheet loading gets more strict.
    
    Bug: 752830
    
    Change-Id: I51ada4de148c384842911f001761b97f71a9082a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007112
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#734202}

--- third_party/blink/web_tests/http/tests/cache/resources/etag-200-different.php
+++ third_party/blink/web_tests/http/tests/cache/resources/etag-200-different.php
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 header('ETag: foo');
 header('Cache-control: max-age=0');
 
+if ($_GET['type'] == 'css') {
+    header('Content-Type: text/css');
+}
+
 if ($_SERVER['HTTP_IF_NONE_MATCH'] == 'foo') {
     // The body after revalidation.
     echo "/* after revalidation */";
