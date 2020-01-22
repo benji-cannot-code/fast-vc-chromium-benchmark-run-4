5bc492841c096fc954e960e37c7895f2eb493410
Elly Fong-Jones <ellyjones@chromium.org>
2020-01-22 17:19:26 +0000

    a11y: null AXPlatformNodeCocoa node earlier in detach
    Leaving _node set while posting the notification can lead to VoiceOver
    re-entering AXPlatformNodeCocoa while the AXPlatformNodeBase it
    corresponds to is already being destroyed.
    
    Bug: 1037855
    Change-Id: I8ff959998f05821b5f258d32f4a9328cb29d234c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014104
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734055}

--- ui/accessibility/platform/ax_platform_node_mac.mm
+++ ui/accessibility/platform/ax_platform_node_mac.mm
@@ -398,9 +398,9 @@ bool AlsoUseShowMenuActionForDefaultAction(const ui::AXNodeData& data) {
 - (void)detach {
   if (!_node)
     return;
+  _node = nil;
   NSAccessibilityPostNotification(
       self, NSAccessibilityUIElementDestroyedNotification);
-  _node = nil;
 }
 
 - (NSRect)boundsInScreen {
