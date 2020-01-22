fd0d800f2c612ebaf6b0842e7b859742b8fe88ba
Istiaque Ahmed <lazyboy@chromium.org>
2020-01-22 00:30:22 +0000

    Add FilePath canonicalization logic to ComputedHashes.
    Move ComputedHashes's underlying data into its own class ComputedHashes::Data,
    that wraps the canonicalization logic in it.
    
    Adding items to Data would canonicalize paths, obviating the need for
      a) the linear search in case-insensitive OS.
      b) additional dot-space trimming treatment on windows.
    
    Bug: 796395
    Change-Id: I1517f6daa8253562b731cf62a5c54fb95f2bd10e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003261
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733821}

--- extensions/browser/computed_hashes.h
+++ extensions/browser/computed_hashes.h
@@ -14,11 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "base/files/file_path.h"
 #include "base/optional.h"
-
-namespace base {
-class FilePath;
-}
 
 namespace extensions {
 
@@ -30,8 +27,60 @@ using ShouldComputeHashesCallback =
 // computed over the files inside an extension.
 class ComputedHashes {
  public:
-  using HashInfo = std::pair<int, std::vector<std::string>>;
-  using Data = std::map<base::FilePath, HashInfo>;
+  // Hashes data for relative paths.
+  // System specific path canonicalization is taken care of inside this class.
+  class Data {
+   public:
+    struct HashInfo {
+      int block_size;
+      std::vector<std::string> hashes;
+      // The relative unix style path.
+      // Note that we use canonicalized paths as keys to HashInfo's container
+      // |items_|.
+      //
+      // TODO(http://crbug.com/796395#c28): Consider removing this once
+      // ContentVerifier::ShouldVerifyAnyPaths works with canonicalized relative
+      // paths.
+      base::FilePath relative_unix_path;
+      HashInfo(int block_size,
+               std::vector<std::string> hashes,
+               base::FilePath relative_unix_path);
+      ~HashInfo();
+
+      HashInfo(const HashInfo&) = delete;
+      HashInfo& operator=(const HashInfo&) = delete;
+      HashInfo(HashInfo&&);
+      HashInfo& operator=(HashInfo&&);
+    };
+    using Items = std::map<base::FilePath::StringType, HashInfo>;
+
+    Data();
+    ~Data();
+
+    Data(const Data&) = delete;
+    Data& operator=(const Data&) = delete;
+    Data(Data&&);
+    Data& operator=(Data&&);
+
+    // For |relative_path|, adds hash information with |block_size| and
+    // |hashes|.
+    // Note that |relative_path| will be canonicalized.
+    void Add(const base::FilePath& relative_path,
+             int block_size,
+             std::vector<std::string> hashes);
+
+    // Removes the item that corresponds to |relative_path|.
+    void Remove(const base::FilePath& relative_path);
+
+    // Returns HashInfo* for |relative_path| or nullptr if not found.
+    const HashInfo* GetItem(const base::FilePath& relative_path) const;
+
+    const Items& items() const;
+
+   private:
+    // All items, stored by canonicalized FilePath::StringType key.
+    Items items_;
+  };
 
   explicit ComputedHashes(Data&& data);
   ComputedHashes(const ComputedHashes&) = delete;
@@ -80,7 +129,6 @@ class ComputedHashes {
   // added to computed_hashes.json for this resource.
   static base::Optional<std::vector<std::string>> ComputeAndCheckResourceHash(
       const base::FilePath& full_path,
-      const base::FilePath& relative_unix_path,
       int block_size);
 
   Data data_;
