f507f58ef263c79316d520615fc71d79d3cb070c
Sylvain Defresne <sdefresne@chromium.org>
2020-01-22 02:14:44 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/crash_report/breadcrumbs
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=michaeldo@chromium.org
    
    Bug: 1042208
    Change-Id: I859020ffb6010934dd360e4364546c06465d3281
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2010783
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733846}

--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_persistent_storage_keyed_service_factory.h
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_persistent_storage_keyed_service_factory.h
@@ -8,12 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 class BreadcrumbPersistentStorageKeyedService;
-
-namespace ios {
-class ChromeBrowserState;
-}
 
 class BreadcrumbPersistentStorageKeyedServiceFactory
     : public BrowserStateKeyedServiceFactory {
