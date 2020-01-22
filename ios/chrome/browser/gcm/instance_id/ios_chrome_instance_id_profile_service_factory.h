ae8ca1bee9ab47c1973577ac6b16ff669d38e62e
Sylvain Defresne <sdefresne@chromium.org>
2020-01-22 10:39:37 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/gcm
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=jianli@chromium.org
    
    Bug: 1042208
    Change-Id: Iacbce9f831abd0f470636ce16fd153e8e669f5c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007584
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#733930}

--- ios/chrome/browser/gcm/instance_id/ios_chrome_instance_id_profile_service_factory.h
+++ ios/chrome/browser/gcm/instance_id/ios_chrome_instance_id_profile_service_factory.h
@@ -11,13 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 namespace instance_id {
 class InstanceIDProfileService;
-}
-
-namespace ios {
-class ChromeBrowserState;
 }
 
 // Singleton that owns all InstanceIDProfileService and associates them with
