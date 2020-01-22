464605f6b6da9d8c9fdcdbdf1b3b05340a3f80c3
Lukasz Anforowicz <lukasza@chromium.org>
2020-01-22 23:44:48 +0000

    Focus tab contents after drag-and-drop.
    It is desirable (see https://crbug.com/1031119) to focus tab contents
    when a navigation is triggered by dropping a link or a file onto the
    browser window.
    
    Before this CL, tab contents would be focused any time the OpenURL code
    path is used, but not in BeginNavigation code path.  This meant that
    dropping a local file (e.g. navigating to a file: URL) would result
    in focusing the tab contents (possibly moving the focus away from the
    omnibox), but other navigations (e.g. navigating to a https: URL)
    would not affect focus.
    
    After this CL any "drop" focuses the WebContents.
    
    Bug: 1042271
    Change-Id: Ib1db79faec6e32846747d1cde9a78450dc354b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003079
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734245}

--- content/browser/web_contents/web_drag_dest_mac.mm
+++ content/browser/web_contents/web_drag_dest_mac.mm
@@ -314,6 +314,7 @@ content::GlobalRoutingID GetRenderViewHostID(content::RenderViewHost* rvh) {
 
   _currentRVH = NULL;
 
+  _webContents->Focus();
   targetRWH->DragTargetDrop(*_dropDataFiltered, transformedPt,
                             info->location_in_screen, GetModifierFlags());
 
