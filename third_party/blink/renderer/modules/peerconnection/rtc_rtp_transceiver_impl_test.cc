fb1733da038222e2feac6619c61858f1a4f34a47
Steve Anton <steveanton@chromium.org>
2020-01-23 14:23:18 +0000

    Manually define FakeRtpTransceiver copy operator
    WebRTC is moving the implementation of reference counting into the
    base class which means classes inheriting from RefCountInterface
    will no longer be able to define a copy operator.
    
    Bug: webrtc:11308
    Change-Id: I74570c04b7e9724c4a3a4263ea13f3a56667af80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015981
    Auto-Submit: Steve Anton <steveanton@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734450}

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_transceiver_impl_test.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_transceiver_impl_test.cc
@@ -286,10 +286,10 @@ TEST_F(RTCRtpTransceiverImplTest, ModifyTransceiver) {
 
   // Modify the webrtc transceiver and create a new state object for the
   // modified state.
-  *webrtc_transceiver =
+  webrtc_transceiver->ReplaceWith(
       *CreateWebRtcTransceiver(webrtc_sender, webrtc_receiver, "MidyMacMidface",
                                true, webrtc::RtpTransceiverDirection::kInactive,
-                               webrtc::RtpTransceiverDirection::kSendRecv);
+                               webrtc::RtpTransceiverDirection::kSendRecv));
   RtpTransceiverState modified_transceiver_state =
       CreateTransceiverState(webrtc_transceiver, local_track_adapter->Copy(),
                              remote_track_adapter->Copy());
@@ -354,9 +354,9 @@ TEST_F(RTCRtpTransceiverImplTest, ShallowCopy) {
   // shared internal state.
   {
     // Modify webrtc transceiver to be stopped.
-    *webrtc_transceiver = *CreateWebRtcTransceiver(
+    webrtc_transceiver->ReplaceWith(*CreateWebRtcTransceiver(
         webrtc_sender, webrtc_receiver, base::nullopt, true /* stopped */,
-        webrtc::RtpTransceiverDirection::kSendRecv, base::nullopt);
+        webrtc::RtpTransceiverDirection::kSendRecv, base::nullopt));
     RtpTransceiverState transceiver_state =
         CreateTransceiverState(webrtc_transceiver, local_track_adapter->Copy(),
                                remote_track_adapter->Copy());
@@ -391,10 +391,10 @@ TEST_F(RTCRtpTransceiverImplTest, TransceiverStateUpdateModeSetDescription) {
   // Modify the webrtc transceiver and create a new state object for the
   // modified state.
   webrtc_sender->SetTrack(nullptr);
-  *webrtc_transceiver =
+  webrtc_transceiver->ReplaceWith(
       *CreateWebRtcTransceiver(webrtc_sender, webrtc_receiver, "MidyMacMidface",
                                true, webrtc::RtpTransceiverDirection::kInactive,
-                               webrtc::RtpTransceiverDirection::kSendRecv);
+                               webrtc::RtpTransceiverDirection::kSendRecv));
   RtpTransceiverState modified_transceiver_state =
       CreateTransceiverState(webrtc_transceiver, local_track_adapter->Copy(),
                              remote_track_adapter->Copy());
