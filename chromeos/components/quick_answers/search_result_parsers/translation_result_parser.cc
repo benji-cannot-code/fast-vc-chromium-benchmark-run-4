af8e35e75743bc5aeab765ae566639265aa3eb1e
llin@chromium.org <llin@chromium.org>
2020-01-23 02:58:43 +0000

    Update the way of presenting language name.
    The source language localized name might contain bracket already, for example:
    Chinese (Traditional). Checked with UX team and update the way we
    present language name to "[query] · [language name]".
    
    Bug: b/144317438
    Test: unit tests
    Change-Id: Iab209a4eb0d2abdb5ce1b298f7528f751e39cfe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2013893
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734332}

--- chromeos/components/quick_answers/search_result_parsers/translation_result_parser.cc
+++ chromeos/components/quick_answers/search_result_parsers/translation_result_parser.cc
@@ -20,9 +20,7 @@ constexpr char kSourceTextPath[] = "translateResult.sourceText";
 constexpr char kSourceLanguageLocalizedNamePath[] =
     "translateResult.sourceTextLanguageLocalizedName";
 constexpr char kTranslatedTextPath[] = "translateResult.translatedText";
-// TODO(llin): The language localized name might contains bracket, for example
-// Chinese (Traditional). Update the template after UX is finalized.
-constexpr char kSourceTextTemplate[] = "%s (%s)";
+constexpr char kSourceTextTemplate[] = "%s · %s";
 
 }  // namespace
 
