69c434801671e0d4b5ac88e086db2637fb4a17cf
Christopher Cameron <ccameron@chromium.org>
2020-01-23 22:30:55 +0000

    Metal: Test offline-compiled shader link time
    Change metal_util::TestShader to test either compile or link, and
    change the default behavior to link.
    
    Add inline data for a trivial test shader. Add code to modify the
    bitcode, to defeat caching.
    
    Ensure that the Metal calls for the test are invoked on a thread
    that has base::TaskPriority::HIGHEST.
    
    TBR=jvanverth
    
    Bug: 974219
    Change-Id: I35e688c3758af2e03f893a1ccbef7aef6b778b23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015486
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734645}

--- components/metal_util/test_shader.h
+++ components/metal_util/test_shader.h
@@ -13,6 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace metal {
 
+enum class METAL_UTIL_EXPORT TestShaderComponent {
+  // Test a shader compile from source.
+  kCompile,
+  // Test linking a precompiled shader.
+  kLink,
+};
+
 enum class METAL_UTIL_EXPORT TestShaderResult {
   // Not attempted (e.g, because macOS version does not support Metal).
   kNotAttempted,
@@ -38,7 +45,7 @@ constexpr base::TimeDelta kTestShaderTimeForever =
     base::TimeDelta::FromMinutes(3);
 
 // A default delay before attempting to compile the test shader.
-constexpr base::TimeDelta kTestShaderDelay = base::TimeDelta::FromSeconds(90);
+constexpr base::TimeDelta kTestShaderDelay = base::TimeDelta::FromMinutes(3);
 
 // Attempt to asynchronously compile a trivial Metal shader. If |delay| is zero,
 // then compile synchronously, otherwise, post a delayed task to do the compile.
@@ -52,7 +59,8 @@ constexpr base::TimeDelta kTestShaderDelay = base::TimeDelta::FromSeconds(90);
 void METAL_UTIL_EXPORT
 TestShader(TestShaderCallback callback,
            const base::TimeDelta& delay = kTestShaderDelay,
-           const base::TimeDelta& timeout = kTestShaderTimeout);
+           const base::TimeDelta& timeout = kTestShaderTimeout,
+           TestShaderComponent component = TestShaderComponent::kLink);
 
 }  // namespace metal
 
