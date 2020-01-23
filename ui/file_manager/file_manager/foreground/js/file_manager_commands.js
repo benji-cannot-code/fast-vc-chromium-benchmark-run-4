4b6d9947735b22b94b99051b733bf580fa793381
Isabella Scalzi <isabellascalzi@google.com>
2020-01-23 15:19:38 +0000

    [quickview] Add check-select support to get-info context menu
    Quick View can now open when more than one file is selected. Change
    the canExecute code of the |get-info| context menu command to allow
    opening Quick View from the context menu when the number of file
    selections in the file list is 1 or more.
    
    Test: Tests added in CL:2015470 and CL:2015903
    Bug: 1038799
    Change-Id: Id384c256fbc5cce55a12fde8c7f8b84bc45706b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2012370
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734469}

--- ui/file_manager/file_manager/foreground/js/file_manager_commands.js
+++ ui/file_manager/file_manager/foreground/js/file_manager_commands.js
@@ -1503,7 +1503,7 @@ CommandHandler.COMMANDS_['get-info'] = new class extends Command {
       return;
     }
 
-    event.canExecute = entries.length === 1;
+    event.canExecute = entries.length >= 1;
     event.command.setHidden(false);
   }
 };
