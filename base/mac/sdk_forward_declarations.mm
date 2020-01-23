c146b179fd1197bffda1a6391e05e9e5460a48e1
Avi Drissman <avi@chromium.org>
2020-01-23 22:43:16 +0000

    Remove unneeded Mac SDK forward declaration.
    BUG=1044828
    
    Change-Id: I375866984e4878833b19c29a1276af948756b228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016860
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734665}

--- base/mac/sdk_forward_declarations.mm
+++ base/mac/sdk_forward_declarations.mm
@@ -9,8 +9,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_11
 NSString* const CIDetectorTypeText = @"CIDetectorTypeText";
 #endif  // MAC_OS_X_VERSION_10_11
-
-#if !defined(MAC_OS_X_VERSION_10_14) || \
-    MAC_OS_X_VERSION_MIN_REQUIRED < MAC_OS_X_VERSION_10_14
-NSString* const NSAppearanceNameDarkAqua = @"NSAppearanceNameDarkAqua";
-#endif
