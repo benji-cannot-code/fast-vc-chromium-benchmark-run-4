08338e7e6f86f9f29965a543144cf221b2154ffd
David Munro <davidmunro@google.com>
2020-01-23 07:45:02 +0000

    crostini: More metric granularity around container start failures.
    Currently CONTAINER_START_FAILED is our largest bucket of Restarter
    failures, with close to double the failures of every other category
    combined. When OnCreateContainer fails emit CONTAINER_CREATE_FAILED
    (currently one of the smallest buckets) and create a new error for
    setup failing which anecdotally we've seen a lot of.
    
    
    Test: CQ
    Bug: None
    Change-Id: I19d9eceb49c121779c7051e70d25725a557a3d4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015877
    Commit-Queue: David Munro <davidmunro@google.com>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734374}

--- chrome/browser/chromeos/crostini/crostini_manager.cc
+++ chrome/browser/chromeos/crostini/crostini_manager.cc
@@ -431,7 +431,7 @@ class CrostiniManager::CrostiniRestarter
       return;
     }
     if (!success) {
-      FinishRestart(CrostiniResult::CONTAINER_START_FAILED);
+      FinishRestart(CrostiniResult::CONTAINER_SETUP_FAILED);
       return;
     }
 
@@ -2475,7 +2475,7 @@ void CrostiniManager::OnCreateLxdContainer(
     base::Optional<vm_tools::cicerone::CreateLxdContainerResponse> response) {
   if (!response) {
     LOG(ERROR) << "Failed to create lxd container in vm. Empty response.";
-    std::move(callback).Run(CrostiniResult::CONTAINER_START_FAILED);
+    std::move(callback).Run(CrostiniResult::CONTAINER_CREATE_FAILED);
     return;
   }
 
@@ -2493,7 +2493,7 @@ void CrostiniManager::OnCreateLxdContainer(
   if (response->status() !=
       vm_tools::cicerone::CreateLxdContainerResponse::EXISTS) {
     LOG(ERROR) << "Failed to start container: " << response->failure_reason();
-    std::move(callback).Run(CrostiniResult::CONTAINER_START_FAILED);
+    std::move(callback).Run(CrostiniResult::CONTAINER_CREATE_FAILED);
     return;
   }
   std::move(callback).Run(CrostiniResult::SUCCESS);
