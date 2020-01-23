80f403802eb2ee2c1061b17a19fdd0983ec29070
Sylvain Defresne <sdefresne@chromium.org>
2020-01-23 22:21:29 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/sync/consent_auditor_factory.cc
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=jlebel@chromium.org
    
    Bug: 1042208
    Change-Id: I952ce3ca773ec0d693bd4aa31ff4526e55a29f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017484
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734629}

--- ios/chrome/browser/sync/consent_auditor_factory.cc
+++ ios/chrome/browser/sync/consent_auditor_factory.cc
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // static
 consent_auditor::ConsentAuditor* ConsentAuditorFactory::GetForBrowserState(
-    ios::ChromeBrowserState* browser_state) {
+    ChromeBrowserState* browser_state) {
   return static_cast<consent_auditor::ConsentAuditor*>(
       GetInstance()->GetServiceForBrowserState(browser_state, true));
 }
@@ -43,7 +43,7 @@ consent_auditor::ConsentAuditor* ConsentAuditorFactory::GetForBrowserState(
 // static
 consent_auditor::ConsentAuditor*
 ConsentAuditorFactory::GetForBrowserStateIfExists(
-    ios::ChromeBrowserState* browser_state) {
+    ChromeBrowserState* browser_state) {
   return static_cast<consent_auditor::ConsentAuditor*>(
       GetInstance()->GetServiceForBrowserState(browser_state, false));
 }
@@ -65,8 +65,8 @@ ConsentAuditorFactory::~ConsentAuditorFactory() {}
 
 std::unique_ptr<KeyedService> ConsentAuditorFactory::BuildServiceInstanceFor(
     web::BrowserState* browser_state) const {
-  ios::ChromeBrowserState* ios_browser_state =
-      ios::ChromeBrowserState::FromBrowserState(browser_state);
+  ChromeBrowserState* ios_browser_state =
+      ChromeBrowserState::FromBrowserState(browser_state);
 
   std::unique_ptr<consent_auditor::ConsentSyncBridge> consent_sync_bridge;
   syncer::OnceModelTypeStoreFactory store_factory =
