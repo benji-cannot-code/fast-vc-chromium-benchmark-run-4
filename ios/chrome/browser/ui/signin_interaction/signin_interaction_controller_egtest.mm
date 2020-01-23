6a3241dfcd1f88d8549b0c19edea0c8a9b9e9769
Nohemi Fernandez <fernandex@chromium.org>
2020-01-23 22:53:56 +0000

    [iOS] Add a new option to non-managed account Sign-Out.
    Adds a new option for users in non-managed accounts to clear synced
    data from their local device when signing out.
    
    Bug: 1005509
    Change-Id: Iae3b599b3ca3698b2a176bd56b470c6787c74367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007695
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734681}

--- ios/chrome/browser/ui/signin_interaction/signin_interaction_controller_egtest.mm
+++ ios/chrome/browser/ui/signin_interaction/signin_interaction_controller_egtest.mm
@@ -96,7 +96,8 @@ void ChooseImportOrKeepDataSepareteDialog(id<GREYMatcher> choiceButtonMatcher) {
 
   // Sign in to |fakeIdentity1|.
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity1];
-  [SigninEarlGreyUI signOutWithManagedAccount:NO];
+  [SigninEarlGreyUI
+      signOutWithSignOutConfirmation:SignOutConfirmationNonManagedUser];
 
   // Sign in with |fakeIdentity2|.
   [ChromeEarlGreyUI openSettingsMenu];
@@ -171,7 +172,8 @@ void ChooseImportOrKeepDataSepareteDialog(id<GREYMatcher> choiceButtonMatcher) {
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
 
   // Sign out.
-  [SigninEarlGreyUI signOutWithManagedAccount:NO];
+  [SigninEarlGreyUI
+      signOutWithSignOutConfirmation:SignOutConfirmationNonManagedUser];
 }
 
 // Tests that signing out of a managed account from the Settings works
@@ -183,7 +185,8 @@ void ChooseImportOrKeepDataSepareteDialog(id<GREYMatcher> choiceButtonMatcher) {
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity isManagedAccount:YES];
 
   // Sign out.
-  [SigninEarlGreyUI signOutWithManagedAccount:YES];
+  [SigninEarlGreyUI
+      signOutWithSignOutConfirmation:SignOutConfirmationManagedUser];
 
   // Check that there is no signed in user.
   [SigninEarlGreyUtils checkSignedOut];
@@ -267,8 +270,8 @@ void ChooseImportOrKeepDataSepareteDialog(id<GREYMatcher> choiceButtonMatcher) {
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity2];
 
   // Sign out.
-  [SigninEarlGreyUI signOutWithManagedAccount:NO];
-
+  [SigninEarlGreyUI
+      signOutWithSignOutConfirmation:SignOutConfirmationNonManagedUser];
   // Sign in with |fakeIdentity1|.
   [ChromeEarlGreyUI openSettingsMenu];
   [[EarlGrey selectElementWithMatcher:SecondarySignInButton()]
