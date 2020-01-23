8ce98c78e7217bac1e0fa985a752bd64890496e7
Sylvain Defresne <sdefresne@chromium.org>
2020-01-23 22:45:35 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/overlays
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=michaeldo@chromium.org
    
    Bug: 1042208
    Change-Id: I76fab1199331f19d4fda31396e4631a16e2c9242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017343
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734669}

--- ios/chrome/browser/ui/overlays/overlay_request_coordinator.h
+++ ios/chrome/browser/ui/overlays/overlay_request_coordinator.h
@@ -32,8 +32,7 @@ class OverlayRequest;
                                                delegate
     NS_DESIGNATED_INITIALIZER;
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                              browserState:
-                                  (ios::ChromeBrowserState*)browserState
+                              browserState:(ChromeBrowserState*)browserState
     NS_UNAVAILABLE;
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
                                    browser:(Browser*)browser NS_UNAVAILABLE;
