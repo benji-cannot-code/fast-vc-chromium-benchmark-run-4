01b0944a7890c1c6fe2fa39d911ba14cd251cd13
Yoichi Osato <yoichio@chromium.org>
2020-01-23 04:21:17 +0000

    Revert "PPAPI:  Add NetworkIsolationKeys to proxy lookup requests."
    This reverts commit 9939a33b72d7e0bcc86c1b5ea0a83465384cf64f.
    
    Reason for revert: PPAPINaClNewlibTest.NetworkProxy test failing on Mac ASan 64 Tests (1)
    Fixed: 1044845
    
    Original change's description:
    > PPAPI:  Add NetworkIsolationKeys to proxy lookup requests.
    > 
    > This is needed to correctly isolate any DNS requests made by PPAPI in
    > the context of one page from those made in the context of other pages.
    > 
    > Bug: 1021661
    > Change-Id: Ibbca8db7df8c7e61cb9522630e70c8ccd365b5c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008050
    > Commit-Queue: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: Raymes Khoury <raymes@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#734094}
    
    TBR=raymes@chromium.org,mmenke@chromium.org
    
    Change-Id: Ia2c799af73dff4749b1af077b7d976f192d785aa
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1021661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015872
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734343}

--- chrome/test/ppapi/ppapi_browsertest.cc
+++ chrome/test/ppapi/ppapi_browsertest.cc
@@ -1278,9 +1278,7 @@ TEST_PPAPI_NACL_DISALLOWED_SOCKETS(UDPSocketPrivateDisallowed)
 // is present in the DNS cache with the NetworkIsolationKey associated with the
 // foreground WebContents - this is needed so as not to leak what hostnames were
 // looked up across tabs with different first party origins.
-void CheckTestHostNameUsedWithCorrectNetworkIsolationKey(
-    Browser* browser,
-    bool include_canonical_name) {
+void CheckTestHostNameUsedWithCorrectNetworkIsolationKey(Browser* browser) {
   network::mojom::NetworkContext* network_context =
       content::BrowserContext::GetDefaultStoragePartition(browser->profile())
           ->GetNetworkContext();
@@ -1292,7 +1290,7 @@ void CheckTestHostNameUsedWithCorrectNetworkIsolationKey(
   // Cache only lookup.
   params->source = net::HostResolverSource::LOCAL_ONLY;
   // Match the parameters used by the test.
-  params->include_canonical_name = include_canonical_name;
+  params->include_canonical_name = true;
   net::NetworkIsolationKey network_isolation_key =
       browser->tab_strip_model()
           ->GetActiveWebContents()
@@ -1324,8 +1322,7 @@ void CheckTestHostNameUsedWithCorrectNetworkIsolationKey(
 #define RUN_HOST_RESOLVER_SUBTESTS                                             \
   RunTestViaHTTP(LIST_TEST(HostResolver_Empty) LIST_TEST(HostResolver_Resolve) \
                      LIST_TEST(HostResolver_ResolveIPv4));                     \
-  CheckTestHostNameUsedWithCorrectNetworkIsolationKey(                         \
-      browser(), true /* include_canonical_name */)
+  CheckTestHostNameUsedWithCorrectNetworkIsolationKey(browser())
 
 IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest, HostResolverCrash_Basic) {
   if (content::IsInProcessNetworkService())
@@ -2170,33 +2167,7 @@ TEST_PPAPI_NACL(MAYBE_MediaStreamVideoTrack)
 
 TEST_PPAPI_NACL(MouseCursor)
 
-// Proxy configuration test. The PPAPI code used by these tests is in
-// ppapi/tests/test_network_proxy.cc.
-
-IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest, NetworkProxy) {
-  RunTestViaHTTP(STRIP_PREFIXES(NetworkProxy));
-  CheckTestHostNameUsedWithCorrectNetworkIsolationKey(
-      browser(), false /* include_canonical_name */);
-}
-
-IN_PROC_BROWSER_TEST_F(PPAPINaClNewlibTest, NetworkProxy) {
-  RunTestViaHTTP(STRIP_PREFIXES(NetworkProxy));
-  CheckTestHostNameUsedWithCorrectNetworkIsolationKey(
-      browser(), false /* include_canonical_name */);
-}
-
-IN_PROC_BROWSER_TEST_F(PPAPINaClPNaClTest, MAYBE_PPAPI_NACL(NetworkProxy)) {
-  RunTestViaHTTP(STRIP_PREFIXES(NetworkProxy));
-  CheckTestHostNameUsedWithCorrectNetworkIsolationKey(
-      browser(), false /* include_canonical_name */);
-}
-
-IN_PROC_BROWSER_TEST_F(PPAPINaClPNaClNonSfiTest,
-                       MAYBE_PNACL_NONSFI(NetworkProxy)) {
-  RunTestViaHTTP(STRIP_PREFIXES(NetworkProxy));
-  CheckTestHostNameUsedWithCorrectNetworkIsolationKey(
-      browser(), false /* include_canonical_name */);
-}
+TEST_PPAPI_NACL(NetworkProxy)
 
 // TODO(crbug.com/619765): get working on CrOS build.
 #if defined(OS_CHROMEOS)
