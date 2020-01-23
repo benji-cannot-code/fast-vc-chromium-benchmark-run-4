7bc735264a481084ddedae0f586de09a96dc97e7
James Cook <jamescook@chromium.org>
2020-01-23 22:31:59 +0000

    chromeos: Migrate TokenHandleFetcher to unconsented primary account
    SplitSettingsSync will allow the user to opt-out of browser sync.
    However, IdentityManager::GetPrimaryAccount() assumes the user has
    consented to sync. Switch to using the "unconsented" primary account,
    which exists whether or not the user has consented to sync.
    
    This code doesn't add a DCHECK that the unconsented primary account
    exists because there are managed guest session tests that don't
    have a primary account but still exercise this code path.
    
          and on device
    
    Test: existing login browser tests, manual tests of login on linux
    Bug: 1042400
    Change-Id: I14ee7bcc18c294dee31c26fcc9bed537836516b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015589
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734647}

--- chrome/browser/chromeos/login/signin/token_handle_fetcher.cc
+++ chrome/browser/chromeos/login/signin/token_handle_fetcher.cc
@@ -13,9 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/keyed_service/content/browser_context_keyed_service_shutdown_notifier_factory.h"
+#include "components/signin/public/identity_manager/access_token_fetcher.h"
 #include "components/signin/public/identity_manager/access_token_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
-#include "components/signin/public/identity_manager/primary_account_access_token_fetcher.h"
+#include "google_apis/gaia/core_account_id.h"
 #include "google_apis/gaia/gaia_constants.h"
 #include "services/identity/public/cpp/scope_set.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -40,7 +41,7 @@ class TokenHandleFetcherShutdownNotifierFactory
             "TokenHandleFetcher") {
     DependsOn(IdentityManagerFactory::GetInstance());
   }
-  ~TokenHandleFetcherShutdownNotifierFactory() override {}
+  ~TokenHandleFetcherShutdownNotifierFactory() override = default;
 
   DISALLOW_COPY_AND_ASSIGN(TokenHandleFetcherShutdownNotifierFactory);
 };
@@ -51,7 +52,7 @@ TokenHandleFetcher::TokenHandleFetcher(TokenHandleUtil* util,
                                        const AccountId& account_id)
     : token_handle_util_(util), account_id_(account_id) {}
 
-TokenHandleFetcher::~TokenHandleFetcher() {}
+TokenHandleFetcher::~TokenHandleFetcher() = default;
 
 void TokenHandleFetcher::BackfillToken(Profile* profile,
                                        const TokenFetchingCallback& callback) {
@@ -59,7 +60,10 @@ void TokenHandleFetcher::BackfillToken(Profile* profile,
   callback_ = callback;
 
   identity_manager_ = IdentityManagerFactory::GetForProfile(profile);
-  if (!identity_manager_->HasPrimaryAccountWithRefreshToken()) {
+  // This class doesn't care about browser sync consent.
+  CoreAccountId core_account_id =
+      identity_manager_->GetUnconsentedPrimaryAccountId();
+  if (!identity_manager_->HasAccountWithRefreshToken(core_account_id)) {
     profile_shutdown_notification_ =
         TokenHandleFetcherShutdownNotifierFactory::GetInstance()
             ->Get(profile)
@@ -75,14 +79,13 @@ void TokenHandleFetcher::BackfillToken(Profile* profile,
 
   // We can use base::Unretained(this) below because |access_token_fetcher_| is
   // owned by this object (thus destroyed when this object is destroyed) and
-  // PrimaryAccountAccessTokenFetcher guarantees that it doesn't invoke its
-  // callback after it is destroyed.
-  access_token_fetcher_ =
-      std::make_unique<signin::PrimaryAccountAccessTokenFetcher>(
-          kAccessTokenFetchId, identity_manager_, scopes,
-          base::BindOnce(&TokenHandleFetcher::OnAccessTokenFetchComplete,
-                         base::Unretained(this)),
-          signin::PrimaryAccountAccessTokenFetcher::Mode::kWaitUntilAvailable);
+  // AccountAccessTokenFetcher guarantees that it doesn't invoke its callback
+  // after it is destroyed.
+  access_token_fetcher_ = identity_manager_->CreateAccessTokenFetcherForAccount(
+      core_account_id, kAccessTokenFetchId, scopes,
+      base::BindOnce(&TokenHandleFetcher::OnAccessTokenFetchComplete,
+                     base::Unretained(this)),
+      signin::AccessTokenFetcher::Mode::kWaitUntilRefreshTokenAvailable);
 }
 
 void TokenHandleFetcher::OnAccessTokenFetchComplete(
