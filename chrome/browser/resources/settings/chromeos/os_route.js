4afdede8f89c69d054ac44dcfacdaa919b427657
James Cook <jamescook@chromium.org>
2020-01-23 23:47:48 +0000

    chromeos: Fix DCHECK when searching OS settings for not-found string
    The C++ OSSyncHandler is only instantiated when the SplitSettingsSync
    flag is set. However, the new os-sync-controls HTML/JS can be
    instantiated by a search event when the flag is not set. This leads
    to a DCHECK for an unhandled message.
    
    Only instantiate the HTML if the flag is set. Do the same for the
    old UI.
    
    Fixed: 1044369
    Change-Id: If09060e9916405afc524e669f235166cedf8c4eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017986
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734718}

--- chrome/browser/resources/settings/chromeos/os_route.js
+++ chrome/browser/resources/settings/chromeos/os_route.js
@@ -23,7 +23,9 @@ cr.define('settings', function() {
     if (!loadTimeData.getBoolean('isGuest')) {
       r.PEOPLE = r.BASIC.createSection('/people', 'people');
       r.SYNC = r.PEOPLE.createChild('/syncSetup');
-      r.SYNC_ADVANCED = r.SYNC.createChild('/syncSetup/advanced');
+      if (!loadTimeData.getBoolean('splitSettingsSyncEnabled')) {
+        r.SYNC_ADVANCED = r.SYNC.createChild('/syncSetup/advanced');
+      }
     }
 
     r.INTERNET = r.BASIC.createSection('/internet', 'internet');
@@ -103,8 +105,7 @@ cr.define('settings', function() {
     r.ACCESSIBILITY = r.ADVANCED.createSection('/accessibility', 'a11y');
 
     if (!loadTimeData.getBoolean('isGuest')) {
-      if (loadTimeData.valueExists('splitSettingsSyncEnabled') &&
-          loadTimeData.getBoolean('splitSettingsSyncEnabled')) {
+      if (loadTimeData.getBoolean('splitSettingsSyncEnabled')) {
         r.OS_SYNC = r.PEOPLE.createChild('/osSync');
       }
       // Personalization
