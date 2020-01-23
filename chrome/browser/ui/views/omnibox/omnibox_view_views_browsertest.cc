b0df89066f138cf681715e8aaf17ba4f8eee531f
Fergal Daly <fergal@chromium.org>
2020-01-23 14:57:16 +0000

    Replace strings with already-existing constants.
    Follow-up to https://crrev.com/c/2011585
    
    Change-Id: I045e22957c5e3b9ad93ae9604164490febe0fccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015904
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734463}

--- chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
+#include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/omnibox_popup_model.h"
 #include "components/omnibox/browser/test_scheme_classifier.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/url_constants.h"
 #include "ui/accessibility/accessibility_switches.h"
 #include "ui/accessibility/ax_action_data.h"
 #include "ui/accessibility/ax_node_data.h"
@@ -674,14 +676,14 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, ReloadAfterKill) {
       static_cast<OmniboxViewViews*>(omnibox_view);
 
   // Open new tab page.
-  ui_test_utils::NavigateToURL(browser(), GURL("chrome://newtab"));
+  ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUINewTabURL));
 
   content::WebContents* tab =
       browser()->tab_strip_model()->GetActiveWebContents();
   // Kill the tab with chrome://kill
   {
     content::ScopedAllowRendererCrashes scoped_allow_renderer_crashes;
-    ui_test_utils::NavigateToURL(browser(), GURL("chrome://kill"));
+    ui_test_utils::NavigateToURL(browser(), GURL(content::kChromeUIKillURL));
     EXPECT_TRUE(tab->IsCrashed());
   }
 
@@ -691,7 +693,8 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, ReloadAfterKill) {
 
   // Verify the omnibox contents, URL and icon.
   EXPECT_EQ(base::ASCIIToUTF16(""), omnibox_view_views->GetText());
-  EXPECT_EQ(GURL("about:blank"), browser()->location_bar_model()->GetURL());
+  EXPECT_EQ(GURL(url::kAboutBlankURL),
+            browser()->location_bar_model()->GetURL());
 }
 
 // The following set of tests require UIA accessibility support, which only
