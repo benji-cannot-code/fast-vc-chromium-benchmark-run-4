386da2d022c9f9ef5a9b8515e2ceee550a1938fb
Avi Drissman <avi@chromium.org>
2020-01-23 15:34:20 +0000

    Remove unneeded Mac SDK forward declaration.
    BUG=1044828
    
    Change-Id: I9a57f34570ef9b6ab332fed18dd5737de4c00e1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016044
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734475}

--- base/mac/sdk_forward_declarations.h
+++ base/mac/sdk_forward_declarations.h
@@ -38,9 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)animationDidStop:(CAAnimation*)animation finished:(BOOL)finished;
 @end
 
-@protocol CALayerDelegate
-@end
-
 #endif  // MAC_OS_X_VERSION_10_12
 
 // ----------------------------------------------------------------------------
