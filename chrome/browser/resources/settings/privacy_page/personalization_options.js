ed2c53f285c737cb527e6e7dd8bfd8f7d7f42610
Martin Šrámek <msramek@chromium.org>
2020-01-23 11:57:04 +0000

    Remove the Link Doctor toggle as a part of the privacy settings redesign
    The "Show suggestions for similar pages when a page can't be found"
    setting is no longer needed as of crbug.com/1011893#c22. We will remove
    as a part of the next phase of the privacy settings redesign.
    
    Bug: 1032584, 1011893
    Change-Id: I94c4a3747b64417ef7def0fc1d009c1a8bfccf2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007576
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734421}

--- chrome/browser/resources/settings/privacy_page/personalization_options.js
+++ chrome/browser/resources/settings/privacy_page/personalization_options.js
@@ -57,6 +57,14 @@ Polymer({
     showSignoutDialog_: Boolean,
 
     /** @private */
+    privacySettingsRedesignEnabled_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('privacySettingsRedesignEnabled');
+      },
+    },
+
+    /** @private */
     syncFirstSetupInProgress_: {
       type: Boolean,
       value: false,
