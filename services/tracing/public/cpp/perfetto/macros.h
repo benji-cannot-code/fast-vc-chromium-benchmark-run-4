71b8a6c9d92bb87ef1cfcbd558e2a55986005b7c
Eric Seckler <eseckler@chromium.org>
2020-01-23 15:08:43 +0000

    Manually roll src/third_party/perfetto aa94421bfd1f..6ce872b758f4 (8 commits)
    Also includes a manual modification to switch to perfetto's
    EventContext.
    
    https://android.googlesource.com/platform/external/perfetto.git/+log/aa94421bfd1f..6ce872b758f4
    
    git log aa94421bfd1f..6ce872b758f4 --date=short --first-parent --format='%2020-01-23 ddrone@google.com Merge "Add clangd working folder to .gitignore"
    2020-01-23 eseckler@google.com Merge "client lib: Don't depend on default_deps from include/perfetto/tracing"
    2020-01-23 fmayer@google.com Merge "Mark merged callsites as [merged]"
    2020-01-23 fmayer@google.com Fix infinity self size.
    2020-01-23 fmayer@google.com Merge changes I2c6ef9a4,I196e54e8
    2020-01-23 fmayer@google.com Merge "Show heap graphs in UI."
    2020-01-23 treehugger-gerrit@google.com Merge "TrackEvent: Add support for legacy events with arguments"
    2020-01-23 lalitm@google.com tp: add multiple filter benchmark on table
    2020-01-23 eseckler@google.com Revert "build: Remove dep on client lib from Chrome's libperfetto"
    
    Created with:
      gclient setdep -r src/third_party/perfetto@6ce872b758f4
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/perfetto-chromium-autoroll
    Please CC perfetto-bugs@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    Bug: None
    Change-Id: I74f9d9b4c444da09532e4ccf54ec20b9658d3087
    Tbr: perfetto-bugs@google.com
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014931
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734468}

--- services/tracing/public/cpp/perfetto/macros.h
+++ services/tracing/public/cpp/perfetto/macros.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Needed not for this file but for every user of the TRACE_EVENT macros for the
 // lambda definition. So included here for convenience.
-#include "services/tracing/public/cpp/perfetto/event_context.h"
+#include "third_party/perfetto/include/perfetto/tracing/event_context.h"
 #include "third_party/perfetto/protos/perfetto/trace/track_event/track_event.pbzero.h"
 
 #if defined(TRACE_EVENT_BEGIN)
