22da6ce04d7d15378688b6068020effa6186f201
David Black <dmblack@google.com>
2020-01-23 22:06:13 +0000

    Clean up conversation starters feature flag.
    Existing feature implementation has been stable for a long time and
    probably no longer needs a kill switch. We can reuse the flag for
    upcoming experimentation but removing completely in this CL as the
    flag will be re-defined in the same file as all other Assistant
    feature flags.
    
    Bug: b:148219866
    Change-Id: If8e3bca7f141b06c822ae4a8d75bb940c4c37b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017666
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#734603}

--- ash/assistant/assistant_suggestions_controller.cc
+++ ash/assistant/assistant_suggestions_controller.cc
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/assistant/proactive_suggestions.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
-#include "base/feature_list.h"
-#include "base/metrics/field_trial_params.h"
 #include "base/rand_util.h"
 #include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
 #include "chromeos/services/assistant/public/features.h"
@@ -28,36 +26,6 @@ namespace ash {
 namespace {
 
 // Conversation starters -------------------------------------------------------
-
-const base::Feature kConversationStartersFeature{
-    "ChromeOSAssistantConversationStarters", base::FEATURE_ENABLED_BY_DEFAULT};
-
-constexpr base::FeatureParam<bool> kImBoredChipEnabled{
-    &kConversationStartersFeature, "im-bored-chip-enabled", true};
-
-constexpr base::FeatureParam<bool> kOpenFilesChipEnabled{
-    &kConversationStartersFeature, "open-files-chip-enabled", true};
-
-constexpr base::FeatureParam<bool> kPlayMusicChipEnabled{
-    &kConversationStartersFeature, "play-music-chip-enabled", true};
-
-constexpr base::FeatureParam<bool> kSendAnEmailChipEnabled{
-    &kConversationStartersFeature, "send-an-email-chip-enabled", true};
-
-constexpr base::FeatureParam<bool> kSetAReminderChipEnabled{
-    &kConversationStartersFeature, "set-a-reminder-chip-enabled", true};
-
-constexpr base::FeatureParam<bool> kWhatCanYouDoChipEnabled{
-    &kConversationStartersFeature, "what-can-you-do-chip-enabled", true};
-
-constexpr base::FeatureParam<bool> kWhatsOnMyCalendarChipEnabled{
-    &kConversationStartersFeature, "whats-on-my-calendar-chip-enabled", true};
-
-constexpr base::FeatureParam<bool> kWhatsOnMyScreenChipEnabled{
-    &kConversationStartersFeature, "whats-on-my-screen-chip-enabled", true};
-
-constexpr base::FeatureParam<bool> kWhatsTheWeatherChipEnabled{
-    &kConversationStartersFeature, "whats-the-weather-chip-enabled", true};
 
 constexpr int kMaxNumOfConversationStarters = 3;
 
@@ -125,9 +93,6 @@ void AssistantSuggestionsController::OnAssistantContextEnabled(bool enabled) {
 // TODO(dmblack): The conversation starter cache should receive its contents
 // from the server. Hard-coding for the time being.
 void AssistantSuggestionsController::UpdateConversationStarters() {
-  if (!base::FeatureList::IsEnabled(kConversationStartersFeature))
-    return;
-
   using chromeos::assistant::mojom::AssistantSuggestion;
   using chromeos::assistant::mojom::AssistantSuggestionPtr;
   using chromeos::assistant::mojom::AssistantSuggestionType;
@@ -143,13 +108,11 @@ void AssistantSuggestionsController::UpdateConversationStarters() {
     conversation_starters.push_back(std::move(starter));
   };
 
-  // If enabled, always show the "What can you do?" conversation starter.
-  if (kWhatCanYouDoChipEnabled.Get())
-    AddConversationStarter(IDS_ASH_ASSISTANT_CHIP_WHAT_CAN_YOU_DO);
+  // Always show the "What can you do?" conversation starter.
+  AddConversationStarter(IDS_ASH_ASSISTANT_CHIP_WHAT_CAN_YOU_DO);
 
   // If enabled, always show the "What's on my screen?" conversation starter.
-  if (kWhatsOnMyScreenChipEnabled.Get() &&
-      AssistantState::Get()->context_enabled().value_or(false)) {
+  if (AssistantState::Get()->context_enabled().value_or(false)) {
     AddConversationStarter(IDS_ASH_ASSISTANT_CHIP_WHATS_ON_MY_SCREEN,
                            assistant::util::CreateWhatsOnMyScreenDeepLink());
   }
@@ -157,26 +120,13 @@ void AssistantSuggestionsController::UpdateConversationStarters() {
   // The rest of the conversation starters will be shuffled...
   std::vector<int> shuffled_message_ids;
 
-  if (kImBoredChipEnabled.Get())
-    shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_IM_BORED);
-
-  if (kOpenFilesChipEnabled.Get())
-    shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_OPEN_FILES);
-
-  if (kPlayMusicChipEnabled.Get())
-    shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_PLAY_MUSIC);
-
-  if (kSendAnEmailChipEnabled.Get())
-    shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_SEND_AN_EMAIL);
-
-  if (kSetAReminderChipEnabled.Get())
-    shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_SET_A_REMINDER);
-
-  if (kWhatsOnMyCalendarChipEnabled.Get())
-    shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_WHATS_ON_MY_CALENDAR);
-
-  if (kWhatsTheWeatherChipEnabled.Get())
-    shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_WHATS_THE_WEATHER);
+  shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_IM_BORED);
+  shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_OPEN_FILES);
+  shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_PLAY_MUSIC);
+  shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_SEND_AN_EMAIL);
+  shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_SET_A_REMINDER);
+  shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_WHATS_ON_MY_CALENDAR);
+  shuffled_message_ids.push_back(IDS_ASH_ASSISTANT_CHIP_WHATS_THE_WEATHER);
 
   base::RandomShuffle(shuffled_message_ids.begin(), shuffled_message_ids.end());
 
