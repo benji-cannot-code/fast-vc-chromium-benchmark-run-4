a6f0e18d506c88209c2517f589737aac43c02da2
Peter Marshall <petermarshall@chromium.org>
2020-01-23 12:27:44 +0000

    Reland "DevTools: Block SIGPROF during CPU throttling"
    This is a reland of a5afd47a395bcdb2a4a0347b3d9c6dba24daf366
    
    Original change's description:
    > DevTools: Block SIGPROF during CPU throttling
    > 
    > The V8 CPU profiler schedules SIGPROF signals. It is very likely that
    > the signal handler for these will run while the throttling signal
    > handler is already running. V8 can't properly read the stack when
    > there is already a signal handler present.
    > 
    > Fix this by using sa_mask to block V8's SIGPROFs while the
    > throttling handler is running. This makes more sense overall as well.
    > Our CPU samples would be skewed by whichever stacks we encounter
    > during throttling pauses.
    > 
    > Bug: 1027242
    > Change-Id: I80e856ce1e7b4744a9af7e36c2022b6d8610cf60
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2012280
    > Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#733592}
    
    Bug: 1027242
    Change-Id: I2c5c1e462f9f6f16739e773dda112bacd8b354f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014404
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734429}

---
+++ third_party/blink/web_tests/inspector-protocol/cpu-profiler/record-cpu-profile-with-cpu-throttling.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      'Test that the profiler can record a profile with cpu throttling enabled.');
+
+  await dp.Emulation.setCPUThrottlingRate({rate: 4});
+  await dp.Profiler.enable();
+  await dp.Profiler.start();
+
+  // Run some JS for at least a second to give a chance for the two signals to
+  // overlap.
+  await session.evaluate(`
+    let count = 0;
+    const limit = 1e6;
+    let i = 0;
+    let target = new Date();
+    target.setSeconds(target.getSeconds() + 1);
+    let time = new Date();
+    while (i < limit && time < target) {
+      count += i;
+      i++;
+      time = new Date();
+    }
+    window.count = count;
+  `);
+
+  await dp.Profiler.stop();
+
+  testRunner.completeTest();
+})
