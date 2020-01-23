794daa89440b948b83dfd6e4bc69ec2202ebd190
Vladislav Kuzkokov <vkuzkokov@chromium.org>
2020-01-23 14:32:24 +0000

    Reland "[CrOS Printing] Don't send username and filename unencrypted"
    This is a reland of 3e49c929ec101f572a48351547a9d794c319c4e6
    
    Original change's description:
    > [CrOS Printing] Don't send username and filename unencrypted
    >
    > This situation may come up if policy was changed after printer was
    > selected. We reject printing job since silently omitting username and
    > filename could be confusing.
    >
    > Bug: 1042798
    > Change-Id: Iba385503d8f7e960291728d6515ca7c236220847
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2004992
    > Reviewed-by: Sean Kau <skau@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#734128}
    
    TBR=thestig@chromium.org,skau@chromium.org,yoichio@chromium.org
    
    Bug: 1042798
    Change-Id: I75d30cc7c94d843cbbc32318907165bfede6df69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015029
    Reviewed-by: Vladislav Kuzkokov <vkuzkokov@chromium.org>
    Commit-Queue: Vladislav Kuzkokov <vkuzkokov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734453}

--- printing/backend/cups_printer.cc
+++ printing/backend/cups_printer.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+const char kCUPSDeviceUri[] = "device-uri";
 const char kCUPSPrinterInfoOpt[] = "printer-info";
 const char kCUPSPrinterStateOpt[] = "printer-state";
 
@@ -122,6 +123,12 @@ std::string CupsPrinter::GetMakeAndModel() const {
       kDriverNameTagName, destination_->num_options, destination_->options);
 
   return make_and_model ? std::string(make_and_model) : std::string();
+}
+
+std::string CupsPrinter::GetUri() const {
+  const char* uri = cupsGetOption(kCUPSDeviceUri, destination_->num_options,
+                                  destination_->options);
+  return uri ? std::string(uri) : std::string();
 }
 
 bool CupsPrinter::EnsureDestInfo() const {
