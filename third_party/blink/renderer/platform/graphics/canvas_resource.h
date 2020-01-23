d516f11558d3db943bea7564592d108ce59389d2
Khushal <khushalsagar@chromium.org>
2020-01-23 00:18:43 +0000

    Reland blink: Ensure correct shared image lifetime for static bitmap images.
    This reverts commit d196259f3397839f8d431c93384bb98cec436757.
    
    The error here was trying to delete a shared image created using a
    context before a GPU process crash. The service side considers an
    erroneous shared image deletion command as a fatal error which in turn
    destroys the new GpuChannel.
    
    This change now depends on a patchset which ensures that shared images
    are automatically deleted once a context is deleted avoiding the need
    for explicit deletion from the user.
    
    TBR=klausw@chromium.org
    R=kbr@chromium.org
    
    Bug: 1042446,1042481,1036142
    Change-Id: I886460b8b1a62617ae987a1c96e0626fc3eb6e65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008076
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734263}

--- third_party/blink/renderer/platform/graphics/canvas_resource.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource.h
@@ -149,6 +149,10 @@ class PLATFORM_EXPORT CanvasResource
 
   SkImageInfo CreateSkImageInfo() const;
 
+  bool is_cross_thread() const {
+    return base::PlatformThread::CurrentRef() != owning_thread_ref_;
+  }
+
  protected:
   CanvasResource(base::WeakPtr<CanvasResourceProvider>,
                  SkFilterQuality,
@@ -195,7 +199,8 @@ class PLATFORM_EXPORT CanvasResource
   CanvasResourceProvider* Provider() { return provider_.get(); }
   base::WeakPtr<CanvasResourceProvider> WeakProvider() { return provider_; }
 
-  const base::PlatformThreadId owning_thread_id_;
+  const base::PlatformThreadRef owning_thread_ref_;
+  const scoped_refptr<base::SingleThreadTaskRunner> owning_thread_task_runner_;
 
  protected:
   // Returns the texture target for the resource.
@@ -295,9 +300,6 @@ class PLATFORM_EXPORT CanvasResourceSharedImage final : public CanvasResource {
   GLenum TextureTarget() const override { return texture_target_; }
 
   void WillDraw();
-  bool is_cross_thread() const {
-    return base::PlatformThread::CurrentId() != owning_thread_id_;
-  }
   bool has_read_access() const {
     return owning_thread_data().bitmap_image_read_refs > 0u;
   }
@@ -351,11 +353,11 @@ class PLATFORM_EXPORT CanvasResourceSharedImage final : public CanvasResource {
                             uint32_t shared_image_usage_flags);
 
   OwningThreadData& owning_thread_data() {
-    DCHECK_EQ(base::PlatformThread::CurrentId(), owning_thread_id_);
+    DCHECK(!is_cross_thread());
     return owning_thread_data_;
   }
   const OwningThreadData& owning_thread_data() const {
-    DCHECK_EQ(base::PlatformThread::CurrentId(), owning_thread_id_);
+    DCHECK(!is_cross_thread());
     return owning_thread_data_;
   }
 
@@ -388,7 +390,6 @@ class PLATFORM_EXPORT CanvasResourceSharedImage final : public CanvasResource {
   const bool is_accelerated_;
   const bool is_overlay_candidate_;
   const GLenum texture_target_;
-  const scoped_refptr<base::SingleThreadTaskRunner> owning_thread_task_runner_;
 
   OwningThreadData owning_thread_data_;
 };
