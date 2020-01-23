00377bd92244971da0ad4e62d86978e62eff8915
Sylvain Defresne <sdefresne@chromium.org>
2020-01-23 09:43:19 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/chrome/browser/device_sharing
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    R=erikchen@chromium.org
    
    Bug: 1042208
    Change-Id: I9148cc5a2f2ecdfa4d081e27470660b22604f431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007640
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734390}

--- ios/chrome/browser/device_sharing/device_sharing_manager.h
+++ ios/chrome/browser/device_sharing/device_sharing_manager.h
@@ -8,12 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
+
 class GURL;
 @class HandoffManager;
-
-namespace ios {
-class ChromeBrowserState;
-}
 
 // This manager maintains all state related to sharing the active URL to other
 // devices. It has the role of a dispatcher that shares the active URL to
