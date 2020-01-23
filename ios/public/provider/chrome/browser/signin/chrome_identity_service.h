762036f51baac393e7eabcc6529e927d0cef58da
Sylvain Defresne <sdefresne@chromium.org>
2020-01-23 12:03:11 +0000

    [ios] Use chrome_browser_state_forward.h in /ios/public/provider/chrome/browser/signin
    Instead of forward declaring ChromeBrowserState in the ios
    namespace, use a header. This will allow moving the class
    to the global namespace without having to change all the
    forward declaration at the same time.
    
    This CL was uploaded by git cl split.
    
    Bug: 1042208
    Change-Id: Ib442aa79586586b2495dec1df11497713ee7e442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2011800
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734422}

--- ios/public/provider/chrome/browser/signin/chrome_identity_service.h
+++ ios/public/provider/chrome/browser/signin/chrome_identity_service.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/observer_list.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
 
 @class ChromeIdentity;
 @protocol ChromeIdentityBrowserOpener;
@@ -30,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ios {
 
-class ChromeBrowserState;
 class ChromeIdentityService;
 
 // Callback passed to method |GetAccessTokenForScopes()| that returns the
