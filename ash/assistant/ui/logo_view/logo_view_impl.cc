90f56ef15c2ef2dbd15e5bad0ff439978bae98b3
wutao <wutao@chromium.org>
2020-01-23 00:14:28 +0000

    assistant: Stop logo view animation if not drawn
    This cl fixes a bug to stop logo view animation when the view is not
    drawn.
    
    Bug: 1044320
    Test: manual
    Change-Id: Ifa8b3f8ae1cf2c66b58ff2ff778599263ade7166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015608
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734262}

--- ash/assistant/ui/logo_view/logo_view_impl.cc
+++ ash/assistant/ui/logo_view/logo_view_impl.cc
@@ -218,7 +218,7 @@ void LogoViewImpl::OnBoundsChanged(const gfx::Rect& previous_bounds) {
 
 void LogoViewImpl::VisibilityChanged(views::View* starting_from,
                                      bool is_visible) {
-  if (is_visible)
+  if (IsDrawn())
     state_animator_.StartAnimator();
   else
     state_animator_.StopAnimator();
