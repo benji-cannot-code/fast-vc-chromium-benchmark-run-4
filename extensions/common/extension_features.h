417699283874643adf9571ac449b4522c0c995c3
Lukasz Anforowicz <lukasza@chromium.org>
2020-01-23 00:00:47 +0000

    Support populating the CORB/CORS allowlist via field trial parameter.
    In the past, CORB allowlist could be controlled via a field trial
    parameter, but that is no longer possible since r692251.  This CL
    introduces a new, separate field trial parameter that can provide
    similar functionality for the AllowlistAlsoAppliesToOorCors feature.
    
    A *separate* field trial parameter is desirable because we want to
    separately control 1) the old CORB allowlist that is distributed via a
    field trial to old Chrome versions and 2) the new CORB+CORS allowlist
    that will be distributed to new/future Chrome versions.
    
    Bug: 920638
    Change-Id: I65af7ddbc2387765b944bf1dfb05a22b1b8555c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1995925
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734255}

--- extensions/common/extension_features.h
+++ extensions/common/extension_features.h
@@ -20,6 +20,8 @@ extern const char kPrivacyMessage[];
 extern const char kNeutralMessage[];
 
 extern const base::Feature kCorbAllowlistAlsoAppliesToOorCors;
+extern const char kCorbAllowlistAlsoAppliesToOorCorsParamName[];
+
 extern const base::Feature kForceWebRequestProxyForTest;
 
 }  // namespace extensions_features
