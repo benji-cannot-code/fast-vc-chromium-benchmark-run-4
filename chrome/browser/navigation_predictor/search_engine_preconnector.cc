5cfd42080826c98c6ead508b220b14fef99d0bfb
Yuki Shiino <yukishiino@chromium.org>
2020-01-24 02:37:35 +0000

    Revert "Add a feature to limit DSE preconnect to Google only"
    This reverts commit 0abb3399e2a9bb7f9fe3692691b6515621119d1d.
    
    Reason for revert: This patch seems like making SearchEnginePreconnectorGoogleOnlyBrowserTest.GoogleOnly time-out.
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8890428735117785616/+/steps/browser_tests_on__none__GPU_on_Mac_on_Mac-10.11/0/logs/Deterministic_failure:_SearchEnginePreconnectorGoogleOnlyBrowserTest.GoogleOnly__status_TIMEOUT_/0
    
    Original change's description:
    > Add a feature to limit DSE preconnect to Google only
    > 
    > This feature allows us to experiment on Google without possibly creating
    > issues with non-Google DSEs. This will hopefully prevent problems or
    > regressions with other search engines.
    > 
    > Bug: 1036489
    > Change-Id: I499b580b1bc0a2d25bed1c8727ba8b36ef6259b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2013748
    > Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    > Reviewed-by: Robert Ogden <robertogden@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#734601}
    
    TBR=tbansal@chromium.org,robertogden@chromium.org,ryansturm@chromium.org
    
    Change-Id: I8cd7aa05b4428ed5bc6149c23fa52cfea2dfe229
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1036489
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018562
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734787}

--- chrome/browser/navigation_predictor/search_engine_preconnector.cc
+++ chrome/browser/navigation_predictor/search_engine_preconnector.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/loading_predictor_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
-#include "components/google/core/common/google_util.h"
 #include "components/search_engines/template_url_service.h"
 #include "content/public/browser/browser_context.h"
 #include "net/base/features.h"
@@ -22,10 +21,6 @@ namespace features {
 // Feature to control preconnect to search.
 const base::Feature kPreconnectToSearch{"PreconnectToSearch",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
-
-// Feature to limit experimentation to Google search only.
-const base::Feature kPreconnectToSearchNonGoogle{
-    "PreconnectToSearchNonGoogle", base::FEATURE_DISABLED_BY_DEFAULT};
 }  // namespace features
 
 SearchEnginePreconnector::SearchEnginePreconnector(
@@ -42,7 +37,6 @@ SearchEnginePreconnector::SearchEnginePreconnector(
 #endif  // defined(OS_ANDROID)
       browser_context_(browser_context),
       currently_in_foreground_(true) {
-
   DCHECK(!browser_context_->IsOffTheRecord());
 
 #if defined(OS_ANDROID)
@@ -124,13 +118,6 @@ void SearchEnginePreconnector::PreconnectDSE() {
   GURL preconnect_url = GetDefaultSearchEngineOriginURL();
   if (preconnect_url.scheme() != url::kHttpScheme &&
       preconnect_url.scheme() != url::kHttpsScheme) {
-    return;
-  }
-  // Limit experimentation to [www].google.com only.
-  if (!base::FeatureList::IsEnabled(features::kPreconnectToSearchNonGoogle) &&
-      !google_util::IsGoogleDomainUrl(preconnect_url,
-                                      google_util::DISALLOW_SUBDOMAIN,
-                                      google_util::ALLOW_NON_STANDARD_PORTS)) {
     return;
   }
 
