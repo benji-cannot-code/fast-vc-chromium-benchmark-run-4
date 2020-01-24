1da924378b42b6e0bf8d0fc87c4e1ab707c0aa27
Himanshu Jaju <himanshujaju@chromium.org>
2020-01-24 16:59:45 +0000

    Add webrtc metric for parsing ice candidates
    Logs if parsing the ice candidate message was successful.
    
    Bug: 1021984
    Change-Id: I5c7adaed6e3fef5e7e5417f8edd6d11381e6f95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2014942
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734978}

---
+++ chrome/services/sharing/public/cpp/sharing_webrtc_metrics.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_SERVICES_SHARING_PUBLIC_CPP_SHARING_WEBRTC_METRICS_H_
+#define CHROME_SERVICES_SHARING_PUBLIC_CPP_SHARING_WEBRTC_METRICS_H_
+
+// Logs whether adding ice candidate was successful.
+void LogWebRtcAddIceCandidate(bool success);
+
+#endif  // CHROME_SERVICES_SHARING_PUBLIC_CPP_SHARING_WEBRTC_METRICS_H_
--- chrome/services/sharing/webrtc/sharing_webrtc_connection.cc
+++ chrome/services/sharing/webrtc/sharing_webrtc_connection.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/ptr_util.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "chrome/services/sharing/public/cpp/sharing_webrtc_metrics.h"
 #include "third_party/webrtc/api/jsep.h"
 
 namespace {
@@ -455,16 +456,13 @@ void SharingWebRtcConnection::AddIceCandidates(
         webrtc::CreateIceCandidate(
             ice_candidate->sdp_mid, ice_candidate->sdp_mline_index,
             ice_candidate->candidate, /*error=*/nullptr));
+
     if (candidate) {
       peer_connection_->AddIceCandidate(
-          std::move(candidate), [](webrtc::RTCError error) {
-            if (!error.ok()) {
-              LOG(ERROR) << "Failed to add IceCandidate: " << error.message()
-                         << " (" << webrtc::ToString(error.type()) << ")";
-            }
-          });
+          std::move(candidate),
+          [](webrtc::RTCError error) { LogWebRtcAddIceCandidate(error.ok()); });
     } else {
-      LOG(ERROR) << "Skipping invalid IceCandidate";
+      LogWebRtcAddIceCandidate(false);
     }
   }
 }
