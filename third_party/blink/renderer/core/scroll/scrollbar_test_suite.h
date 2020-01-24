426f6e9aea3bf08de4c6cfe999cf381ab44f489f
Joe Downing <joedow@chromium.org>
2020-01-24 22:20:05 +0000

    Revert "[Reland] Make programmatic scrolls respect smooth scroll flag"
    This reverts commit 8d695b074f450251654ea221478f8e6a66fccc87.
    
    Reason for revert: Suspected of causing multiple webkit_layout_test failures involving scrolling on the Mac bots:
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.13%20Tests%20%28dbg%29/16924
    
    Original change's description:
    > [Reland] Make programmatic scrolls respect smooth scroll flag
    > 
    > Programmatic smooth scrolls should also be instant if the smooth scroll
    > flag is explicitly disabled.
    > 
    > Previously landed in https://crrev.com/c/1536759
    > 
    > To reland, this CL force enables the scroll animator in Blink web
    > tests. Without this, the usual flow for determining if the animator is
    > enabled is followed, which leads to inconsistencies. On Mac, the
    > animator is enabled in Chrome but not in content shell. This leads to
    > inconsistencies with other platforms (Mac tests ran without smooth
    > scroll while Linux/Windows ran with) and ensuring we test what we ship.
    > 
    > This change required some minor fixes to tests that assumed instant
    > scroll on Mac (and uncovered one real bug).
    > 
    > Bug: 944583
    > Change-Id: I114d8b882d9855fbe0c43645575a1cdd7c3b6e61
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2009842
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#735006}
    
    TBR=flackr@chromium.org,nasko@chromium.org,bokan@chromium.org
    
    Change-Id: Ifa361e963dbc536830ad245ef31bfae542954c33
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 944583
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020555
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735117}

--- third_party/blink/renderer/core/scroll/scrollbar_test_suite.h
+++ third_party/blink/renderer/core/scroll/scrollbar_test_suite.h
@@ -94,7 +94,7 @@ class MockScrollableArea : public GarbageCollected<MockScrollableArea>,
   CompositorElementId GetScrollElementId() const override {
     return CompositorElementId();
   }
-  bool ScrollAnimatorEnabled() const override { return true; }
+  bool ScrollAnimatorEnabled() const override { return false; }
   int PageStep(ScrollbarOrientation) const override { return 0; }
   void ScrollControlWasSetNeedsPaintInvalidation() override {}
 
@@ -122,12 +122,11 @@ class MockScrollableArea : public GarbageCollected<MockScrollableArea>,
     ScrollableArea::Trace(visitor);
   }
 
- protected:
+ private:
   void SetMaximumScrollOffset(const ScrollOffset& maximum_scroll_offset) {
     maximum_scroll_offset_ = maximum_scroll_offset;
   }
 
- private:
   ScrollOffset scroll_offset_;
   ScrollOffset maximum_scroll_offset_;
   Member<MockPlatformChromeClient> chrome_client_;
