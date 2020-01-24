2951009b5271bc3d421bbeed57d247493bb31e4b
Alexei Svitkine <asvitkine@chromium.org>
2020-01-24 23:57:58 +0000

    Add crash key logging to debug startup locale CHECK.
    Bug: 912320
    Change-Id: I44eceafae715ccedfb258b86d53728efb401d28e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020783
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735160}

--- components/variations/service/variations_service.cc
+++ components/variations/service/variations_service.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/build_time.h"
 #include "base/callback.h"
 #include "base/command_line.h"
+#include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -438,7 +439,17 @@ void VariationsService::EnsureLocaleEquals(const std::string& locale) {
   // Uses a CHECK rather than a DCHECK to ensure that issues are caught since
   // problems in this area may only appear in the wild due to official builds
   // and end user machines.
-  CHECK_EQ(locale, field_trial_creator_.application_locale());
+  if (locale != field_trial_creator_.application_locale()) {
+    // TODO(crbug.com/912320): Report the two values in crash keys.
+    static auto* lhs_key = base::debug::AllocateCrashKeyString(
+        "mismatched_locale_lhs", base::debug::CrashKeySize::Size256);
+    static auto* rhs_key = base::debug::AllocateCrashKeyString(
+        "mismatched_locale_rhs", base::debug::CrashKeySize::Size256);
+    base::debug::ScopedCrashKeyString scoped_lhs(lhs_key, locale);
+    base::debug::ScopedCrashKeyString scoped_rhs(
+        rhs_key, field_trial_creator_.application_locale());
+    CHECK_EQ(locale, field_trial_creator_.application_locale());
+  }
 #endif
 }
 
