6a6cdba3e752ec7cec61f43f5abe709f224f252b
Vladislav Kuzkokov <vkuzkokov@chromium.org>
2020-01-24 18:01:47 +0000

    [CrOS Printing] Allow pin printing through HTTPS.
    Also merge the definition of secure protocol with one used in
    printers_map.cc and exclude kUnknown (i.e. "other") from that list.
    
    Bug: 848942
    Change-Id: I14894d2775c0d6b251866bb0c09b02efc94c3cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2005229
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nikita Podguzov <nikitapodguzov@chromium.org>
    Commit-Queue: Vladislav Kuzkokov <vkuzkokov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735011}

--- chrome/browser/chromeos/printing/printers_map.cc
+++ chrome/browser/chromeos/printing/printers_map.cc
@@ -10,12 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 namespace {
 
-bool IsProtocolSecure(const Printer& printer) {
-  return !printer.HasNetworkProtocol() ||
-         printer.GetProtocol() == Printer::kIpps ||
-         printer.GetProtocol() == Printer::kHttps;
-}
-
 }  // namespace
 
 PrintersMap::PrintersMap() = default;
@@ -108,9 +102,8 @@ std::vector<Printer> PrintersMap::GetSecurePrinters(
   result.reserve(printers_.at(printer_class).size());
   for (const auto& kv : printers_.at(printer_class)) {
     const Printer& printer = kv.second;
-    if (IsProtocolSecure(printer)) {
+    if (printer.HasSecureProtocol())
       result.push_back(printer);
-    }
   }
 
   return result;
