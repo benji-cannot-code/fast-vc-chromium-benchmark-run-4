af926d3da557f12b2684f369b16d2acf90425b77
Joe Downing <joedow@chromium.org>
2020-01-24 22:24:37 +0000

    Revert "Replace drmModeSetCrtc by Atomic APIs"
    This reverts commit bb8e99d0c3be2f2e3a8f1d25f8ea58d172be7db2.
    
    Reason for revert: Suspected of causing ozone_unittest failures on ChromeOS MSAN builder:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/17456
    
    Original change's description:
    > Replace drmModeSetCrtc by Atomic APIs
    > 
    > If the device supports atomic APIs, use drmModeAtomicCommit to modeset
    > and disable instead of drmModeSetCrtc.
    > Abstracted Modeset and Disabled code to HDPM to handle both atomic and
    > legacy call.
    > Updated tests to support Connector and CRTC props.
    > 
    > BUG=987274
    > TEST=HardwareDisplayControllerTest.CheckModesettingSetsProps,
    > HardwareDisplayPlaneManagerLegacyTest.Modeset,
    > HardwareDisplayPlaneManagerLegacyTest.DisableModeset,
    > HardwareDisplayPlaneManagerAtomicTest.DisableModeset, Tested on
    > Nautilus(Intel), Grunt(AMD) and Cave(Legacy).
    > 
    > Change-Id: Ic307c04df4e22d1696abb31ebcd820f6bdcb542a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1715751
    > Commit-Queue: Mark Yacoub <markyacoub@google.com>
    > Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#735013}
    
    TBR=dnicoara@chromium.org,dcastagna@chromium.org,markyacoub@google.com
    
    Change-Id: I05afd1f0419d53814889c4968ecbace723829ff5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 987274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020865
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735118}

--- ui/ozone/platform/drm/gpu/crtc_controller.cc
+++ ui/ozone/platform/drm/gpu/crtc_controller.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/drm/gpu/drm_device.h"
 #include "ui/ozone/platform/drm/gpu/drm_dumb_buffer.h"
 #include "ui/ozone/platform/drm/gpu/drm_framebuffer.h"
-#include "ui/ozone/platform/drm/gpu/drm_gpu_util.h"
 #include "ui/ozone/platform/drm/gpu/hardware_display_plane.h"
 #include "ui/ozone/platform/drm/gpu/page_flip_request.h"
 
@@ -42,16 +41,14 @@ CrtcController::~CrtcController() {
     }
 
     DisableCursor();
-    drm_->plane_manager()->DisableModeset(crtc_, connector_);
+    drm_->DisableCrtc(crtc_);
   }
 }
 
 bool CrtcController::Modeset(const DrmOverlayPlane& plane,
-                             const drmModeModeInfo& mode,
-                             const ui::HardwareDisplayPlaneList& plane_list) {
-  if (!drm_->plane_manager()->Modeset(crtc_,
-                                      plane.buffer->opaque_framebuffer_id(),
-                                      connector_, mode, plane_list)) {
+                             drmModeModeInfo mode) {
+  if (!drm_->SetCrtc(crtc_, plane.buffer->opaque_framebuffer_id(),
+                     std::vector<uint32_t>(1, connector_), &mode)) {
     PLOG(ERROR) << "Failed to modeset: crtc=" << crtc_
                 << " connector=" << connector_
                 << " framebuffer_id=" << plane.buffer->opaque_framebuffer_id()
@@ -78,15 +75,12 @@ bool CrtcController::Disable() {
 
   is_disabled_ = true;
   DisableCursor();
-  return drm_->plane_manager()->DisableModeset(crtc_, connector_);
+  return drm_->DisableCrtc(crtc_);
 }
 
 bool CrtcController::AssignOverlayPlanes(HardwareDisplayPlaneList* plane_list,
-                                         const DrmOverlayPlaneList& overlays,
-                                         bool is_modesetting) {
-  // If we're in the process of modesetting, the CRTC is still disabled.
-  // Once the modeset is done, we expect it to be enabled.
-  DCHECK(is_modesetting || !is_disabled_);
+                                         const DrmOverlayPlaneList& overlays) {
+  DCHECK(!is_disabled_);
 
   const DrmOverlayPlane* primary = DrmOverlayPlane::GetPrimaryPlane(overlays);
   if (primary && !drm_->plane_manager()->ValidatePrimarySize(*primary, mode_)) {
