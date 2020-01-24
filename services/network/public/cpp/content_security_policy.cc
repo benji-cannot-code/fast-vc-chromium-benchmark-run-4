dbuskariol-org/chromium

commit 35a95c7a58d49987f577b72bdb2285f7ea6d1c5a

Unify CSP: Mojoify ContentSecurityPolicyHeader.
Remove content::ContentSecurityPolicyHeader in favor of a mojo struct in
network. This is the last step before being able to replace the
content:: structs by the network::mojom ones for IPC.

See UnifyCSP:
https://docs.google.com/document/d/1v5mJnXJ5dSVXE_rgvJnNM9bzH0ni0YzdhPQ7GLqyhao

Bug: 1021462
Change-Id: Ieba10983f4e5182eee7af656ccc241aff36409b0
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992322
Reviewed-by: Camille Lamy <clamy@chromium.org>
Reviewed-by: Mike West <mkwst@chromium.org>
Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
Cr-Commit-Position: refs/heads/master@{#732043}

services/network/public/cpp/content_security_policy.cc
@@ -299,14 +299,13 @@ void ParseReportDirective(const GURL& request_url,
 }
 
 // Parses the frame-ancestor directive of a Content-Security-Policy header.
-void ParseFrameAncestors(
-    const mojom::ContentSecurityPolicyPtr& content_security_policy_ptr,
-    base::StringPiece frame_ancestors_value) {
+void ParseFrameAncestors(const mojom::ContentSecurityPolicyPtr& policy,
+                         base::StringPiece frame_ancestors_value) {
   // A frame-ancestors directive has already been parsed. Skip further
   // frame-ancestors directives per
   // https://www.w3.org/TR/CSP3/#parse-serialized-policy.
   if (FindDirective(mojom::CSPDirective::Name::FrameAncestors,
-                    &(content_security_policy_ptr->directives))) {
+                    &(policy->directives))) {
     // TODO(arthursonzogni, lfg): Should a warning be fired to the user here?
     return;
   }
@@ -318,23 +317,22 @@ void ParseFrameAncestors(
   if (!source_list)
     return;
 
-  content_security_policy_ptr->directives.push_back(mojom::CSPDirective::New(
+  policy->directives.push_back(mojom::CSPDirective::New(
       mojom::CSPDirective::Name::FrameAncestors, std::move(source_list)));
 }
 
 // Parses the report-uri directive of a Content-Security-Policy header.
-void ParseReportEndpoint(
-    const mojom::ContentSecurityPolicyPtr& content_security_policy_ptr,
-    const GURL& base_url,
-    base::StringPiece header_value,
-    bool using_reporting_api) {
+void ParseReportEndpoint(const mojom::ContentSecurityPolicyPtr& policy,
+                         const GURL& base_url,
+                         base::StringPiece header_value,
+                         bool using_reporting_api) {
   // A report-uri directive has already been parsed. Skip further directives per
   // https://www.w3.org/TR/CSP3/#parse-serialized-policy.
-  if (!content_security_policy_ptr->report_endpoints.empty())
+  if (!policy->report_endpoints.empty())
     return;
 
   ParseReportDirective(base_url, header_value, using_reporting_api,
-                       &(content_security_policy_ptr->report_endpoints));
+                       &(policy->report_endpoints));
 }
 
 }  // namespace
@@ -370,31 +368,32 @@ void ContentSecurityPolicy::Parse(
        base::SplitStringPiece(header_value, ",", base::TRIM_WHITESPACE,
                               base::SPLIT_WANT_NONEMPTY)) {
     DirectivesMap directives = ParseHeaderValue(header);
-    auto content_security_policy_ptr = mojom::ContentSecurityPolicy::New();
-    content_security_policy_ptr->type = type;
+    auto policy = mojom::ContentSecurityPolicy::New();
+
+    policy->header = mojom::ContentSecurityPolicyHeader::New(
+        header_value.as_string(), type,
+        mojom::ContentSecurityPolicySource::kHTTP);
 
     auto frame_ancestors = directives.find("frame-ancestors");
     if (frame_ancestors != directives.end())
-      ParseFrameAncestors(content_security_policy_ptr, frame_ancestors->second);
+      ParseFrameAncestors(policy, frame_ancestors->second);
 
     auto report_endpoints = directives.find("report-to");
     if (report_endpoints != directives.end()) {
-      if (!content_security_policy_ptr->use_reporting_api) {
-        content_security_policy_ptr->use_reporting_api = true;
-        content_security_policy_ptr->report_endpoints.clear();
+      if (!policy->use_reporting_api) {
+        policy->use_reporting_api = true;
+        policy->report_endpoints.clear();
       }
     } else {
       report_endpoints = directives.find("report-uri");
     }
 
     if (report_endpoints != directives.end()) {
-      ParseReportEndpoint(content_security_policy_ptr, base_url,
-                          report_endpoints->second,
-                          content_security_policy_ptr->use_reporting_api);
+      ParseReportEndpoint(policy, base_url, report_endpoints->second,
+                          policy->use_reporting_api);
     }
 
-    content_security_policies_.push_back(
-        std::move(content_security_policy_ptr));
+    content_security_policies_.push_back(std::move(policy));
   }
 }
 

content/common/content_security_policy/content_security_policy.cc
@@ -164,7 +164,7 @@ bool ShouldBypassContentSecurityPolicy(CSPContext* context, const GURL& url) {
 ContentSecurityPolicy::ContentSecurityPolicy() = default;
 
 ContentSecurityPolicy::ContentSecurityPolicy(
-    const ContentSecurityPolicyHeader& header,
+    const network::mojom::ContentSecurityPolicyHeader& header,
     const std::vector<CSPDirective>& directives,
     const std::vector<std::string>& report_endpoints,
     bool use_reporting_api)
@@ -177,7 +177,7 @@ ContentSecurityPolicy::ContentSecurityPolicy(
 // struct.
 ContentSecurityPolicy::ContentSecurityPolicy(
     network::mojom::ContentSecurityPolicyPtr csp)
-    : header("", csp->type, csp->source),
+    : header(*(csp->header)),
       report_endpoints(std::move(csp->report_endpoints)),
       use_reporting_api(csp->use_reporting_api) {
   for (auto& directive : csp->directives)

content/browser/frame_host/form_submission_throttle_unittest.cc
@@ -15,13 +15,8 @@ namespace content {
 class FormSubmissionTest : public RenderViewHostImplTestHarness {
  public:
   void PreventFormSubmission() {
-    std::vector<CSPDirective> directives;
-    directives.push_back(
-        CSPDirective(CSPDirective::FormAction, CSPSourceList()));
-    ContentSecurityPolicy form_action_none(ContentSecurityPolicyHeader(),
-                                           directives,
-                                           std::vector<std::string>(), false);
-
+    ContentSecurityPolicy form_action_none({}, {{CSPDirective::FormAction, {}}},
+                                           {}, false);
     main_test_rfh()->AddContentSecurityPolicy(form_action_none);
   }
 };

content/browser/frame_host/frame_tree_node.cc
@@ -334,11 +334,10 @@ void FrameTreeNode::SetFrameName(const std::string& name,
 }
 
 void FrameTreeNode::AddContentSecurityPolicies(
-    const std::vector<ContentSecurityPolicyHeader>& headers) {
-  replication_state_.accumulated_csp_headers.insert(
-      replication_state_.accumulated_csp_headers.end(), headers.begin(),
-      headers.end());
-  render_manager_.OnDidAddContentSecurityPolicies(headers);
+    std::vector<network::mojom::ContentSecurityPolicyHeaderPtr> headers) {
+  for (auto& header : headers)
+    replication_state_.accumulated_csp_headers.push_back(*header);
+  render_manager_.OnDidAddContentSecurityPolicies(std::move(headers));
 }
 
 void FrameTreeNode::SetInsecureRequestPolicy(

content/browser/frame_host/frame_tree_node.h
@@ -20,6 +20,7 @@
 #include "content/common/content_export.h"
 #include "content/common/frame_owner_properties.h"
 #include "content/common/frame_replication_state.h"
+#include "services/network/public/mojom/content_security_policy.mojom-forward.h"
 #include "third_party/blink/public/common/frame/frame_owner_element_type.h"
 #include "third_party/blink/public/common/frame/frame_policy.h"
 #include "third_party/blink/public/common/frame/user_activation_state.h"
@@ -35,7 +36,6 @@ class NavigationRequest;
 class Navigator;
 class RenderFrameHostImpl;
 class NavigationEntryImpl;
-struct ContentSecurityPolicyHeader;
 
 // When a page contains iframes, its renderer process maintains a tree structure
 // of those frames. We are mirroring this tree in the browser process. This
@@ -184,7 +184,7 @@ class CONTENT_EXPORT FrameTreeNode {
 
   // Add CSP headers to replication state, notify proxies about the update.
   void AddContentSecurityPolicies(
-      const std::vector<ContentSecurityPolicyHeader>& headers);
+      std::vector<network::mojom::ContentSecurityPolicyHeaderPtr> headers);
 
   // Sets the current insecure request policy, and notifies proxies about the
   // update.

content/browser/frame_host/render_frame_host_impl.cc
@@ -2567,12 +2567,13 @@ void RenderFrameHostImpl::DidAddContentSecurityPolicies(
                "RenderFrameHostImpl::OnDidAddContentSecurityPolicies",
                "frame_tree_node", frame_tree_node_->frame_tree_node_id());
 
-  std::vector<ContentSecurityPolicyHeader> headers;
+  std::vector<network::mojom::ContentSecurityPolicyHeaderPtr> headers;
   for (const ContentSecurityPolicy& policy : policies) {
     AddContentSecurityPolicy(policy);
-    headers.push_back(policy.header);
+    headers.push_back(
+        network::mojom::ContentSecurityPolicyHeader::New(policy.header));
   }
-  frame_tree_node()->AddContentSecurityPolicies(headers);
+  frame_tree_node()->AddContentSecurityPolicies(std::move(headers));
 }
 
 void RenderFrameHostImpl::OnOpenURL(const FrameHostMsg_OpenURL_Params& params) {

content/browser/frame_host/render_frame_host_manager.cc
@@ -910,10 +910,10 @@ void RenderFrameHostManager::OnDidUpdateName(const std::string& name,
 }
 
 void RenderFrameHostManager::OnDidAddContentSecurityPolicies(
-    const std::vector<ContentSecurityPolicyHeader>& headers) {
+    std::vector<network::mojom::ContentSecurityPolicyHeaderPtr> headers) {
   for (const auto& pair : proxy_hosts_) {
-    pair.second->Send(new FrameMsg_AddContentSecurityPolicies(
-        pair.second->GetRoutingID(), headers));
+    pair.second->GetAssociatedRemoteFrame()
+        ->AddReplicatedContentSecurityPolicies(mojo::Clone(headers));
   }
 }
 

content/browser/frame_host/render_frame_host_manager.h
@@ -23,6 +23,7 @@
 #include "content/common/content_export.h"
 #include "content/public/browser/global_request_id.h"
 #include "content/public/common/referrer.h"
+#include "services/network/public/mojom/content_security_policy.mojom-forward.h"
 #include "third_party/blink/public/common/frame/user_activation_update_type.h"
 #include "ui/base/page_transition_types.h"
 #include "url/origin.h"
@@ -41,7 +42,6 @@ class RenderViewHost;
 class RenderViewHostImpl;
 class RenderWidgetHostView;
 class TestWebContents;
-struct ContentSecurityPolicyHeader;
 struct FrameOwnerProperties;
 struct FrameReplicationState;
 
@@ -342,7 +342,7 @@ class CONTENT_EXPORT RenderFrameHostManager
 
   // Sends the newly added Content Security Policy headers to all the proxies.
   void OnDidAddContentSecurityPolicies(
-      const std::vector<ContentSecurityPolicyHeader>& headers);
+      std::vector<network::mojom::ContentSecurityPolicyHeaderPtr> headers);
 
   // Resets Content Security Policy in all the proxies.
   void OnDidResetContentSecurityPolicy();

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit e0b349c203f581a710cf4e2e0dda8fd3944e4f8e

Fix wrong header stored in network CSP.
Change
https://chromium.googlesource.com/chromium/src/+/35a95c7a58d49987f577b72bdb2285f7ea6d1c5a
introduced a bug. When parsing a group of CSP policy, it was storing for
each parsed policy the whole header instead of just the header for the
policy itself.

It means when parsing "n" policies, it ends up storing O(n²) bytes.
Clusterfuzz used it to trigger an OOM. Well done ;-)

Bug: 1042706
Change-Id: I6b8b448de0103bc3d1cedb7d01154f46728fed9c
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017434
Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
Commit-Queue: Lucas Gadani <lfg@chromium.org>
Reviewed-by: Lucas Gadani <lfg@chromium.org>
Cr-Commit-Position: refs/heads/master@{#734893}

services/network/public/cpp/content_security_policy.cc
@@ -369,8 +369,7 @@ void ContentSecurityPolicy::Parse(const GURL& base_url,
     auto policy = mojom::ContentSecurityPolicy::New();
 
     policy->header = mojom::ContentSecurityPolicyHeader::New(
-        header_value.as_string(), type,
-        mojom::ContentSecurityPolicySource::kHTTP);
+        header.as_string(), type, mojom::ContentSecurityPolicySource::kHTTP);
 
     auto frame_ancestors = directives.find("frame-ancestors");
     if (frame_ancestors != directives.end())