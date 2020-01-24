c9de7eb663fe3bbb709cc96442e721ebcf190971
Sylvain Defresne <sdefresne@chromium.org>
2020-01-24 16:58:11 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/passwords
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=javierrobles@chromium.org
    
    Bug: 1042208
    Change-Id: Id68870da5b0d31aeccffaedfbc2aead625bcefba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017466
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734976}

--- ios/chrome/browser/ui/passwords/password_breach_coordinator.h
+++ ios/chrome/browser/ui/passwords/password_breach_coordinator.h
@@ -18,8 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NS_UNAVAILABLE;
 // Unavailable, use -initWithBaseViewController:browser:.
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                              browserState:
-                                  (ios::ChromeBrowserState*)browserState
+                              browserState:(ChromeBrowserState*)browserState
     NS_UNAVAILABLE;
 
 @end
