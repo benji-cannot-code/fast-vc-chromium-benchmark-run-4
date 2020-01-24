d356ecd486decc981c5869022d794c39fa174034
Sahel Sharify <sahel@chromium.org>
2020-01-24 23:59:08 +0000

    [payments] Do not allow payment app change during retry.
    With this change only the app selected for the initial "pay" attempt
    will be available during retry.
    
    Bug: 1028098
    Change-Id: I13815f19e86c2ca3413867ce17d04dc48862b98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015826
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735162}

--- chrome/browser/ui/views/payments/payment_method_view_controller.h
+++ chrome/browser/ui/views/payments/payment_method_view_controller.h
@@ -34,8 +34,12 @@ class PaymentMethodViewController : public PaymentRequestSheetController {
   base::string16 GetSecondaryButtonLabel() override;
   int GetSecondaryButtonTag() override;
   int GetSecondaryButtonId() override;
+  bool ShouldShowSecondaryButton() override;
 
   PaymentRequestItemList payment_method_list_;
+
+  // Whether or not adding a new card is allowed.
+  bool enable_add_card_;
 
   DISALLOW_COPY_AND_ASSIGN(PaymentMethodViewController);
 };
