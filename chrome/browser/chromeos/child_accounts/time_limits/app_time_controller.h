b2ea76ee2194994303598179f3e51af9cf3c4b5a
yilkal <yilkal@chromium.org>
2020-01-24 00:09:34 +0000

    Adds App Time limit reached notification.
    This CL adds app time limit notification timers. When an app
    becomes active, it creates a timer. When the timer ticks,
    it checks if the time limit has actually been reached and either
    schedules a new timer or notifies AppTimeController.
    
    Bug: 1034551
    Change-Id: I12ec55724a358a39e7015d70142102cbb38a6a24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1998383
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734740}

--- chrome/browser/chromeos/child_accounts/time_limits/app_time_controller.h
+++ chrome/browser/chromeos/child_accounts/time_limits/app_time_controller.h
@@ -8,13 +8,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/optional.h"
+#include "base/time/default_tick_clock.h"
 #include "base/time/time.h"
+#include "base/timer/timer.h"
+#include "chrome/browser/chromeos/child_accounts/time_limits/app_time_notification_delegate.h"
 
 class Profile;
 class PrefRegistrySimple;
 class PrefChangeRegistrar;
 class PrefService;
 class Profile;
+
+namespace base {
+class OneShotTimer;
+}  // namespace base
 
 namespace chromeos {
 namespace app_time {
@@ -24,7 +32,7 @@ class AppServiceWrapper;
 class WebTimeLimitEnforcer;
 
 // Coordinates per-app time limit for child user.
-class AppTimeController {
+class AppTimeController : public AppTimeNotificationDelegate {
  public:
   // Used for tests to get internal implementation details.
   class TestApi {
@@ -47,13 +55,17 @@ class AppTimeController {
   explicit AppTimeController(Profile* profile);
   AppTimeController(const AppTimeController&) = delete;
   AppTimeController& operator=(const AppTimeController&) = delete;
-  ~AppTimeController();
+  ~AppTimeController() override;
 
   bool IsExtensionWhitelisted(const std::string& extension_id) const;
 
   const WebTimeLimitEnforcer* web_time_enforcer() const {
     return web_time_enforcer_.get();
   }
+
+  // AppTimeNotificationDelegate:
+  void ShowAppTimeLimitNotification(const AppId& app_id,
+                                    AppNotification notification) override;
 
   WebTimeLimitEnforcer* web_time_enforcer() { return web_time_enforcer_.get(); }
 
@@ -68,9 +80,22 @@ class AppTimeController {
   void TimeLimitsPolicyUpdated(const std::string& pref_name);
   void TimeLimitsWhitelistPolicyUpdated(const std::string& pref_name);
 
+  base::Time GetNextResetTime() const;
+
+  void ScheduleForTimeLimitReset();
+  void OnResetTimeReached();
+  void SetLastResetTime(base::Time timestamp);
+
   // The time of the day when app time limits should be reset.
   // Defaults to 6am.
   base::TimeDelta limits_reset_time_ = base::TimeDelta::FromHours(6);
+
+  // The last time when |reset_timer_| fired.
+  base::Optional<base::Time> last_limits_reset_time_;
+
+  // Timer scheduled for the next reset of app time limits.
+  // Only set when |reset_time_| is
+  base::OneShotTimer reset_timer_{base::DefaultTickClock::GetInstance()};
 
   std::unique_ptr<AppServiceWrapper> app_service_wrapper_;
   std::unique_ptr<AppActivityRegistry> app_registry_;
