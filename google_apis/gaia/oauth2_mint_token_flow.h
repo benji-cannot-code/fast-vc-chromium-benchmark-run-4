e1a56eecc36fffdc64ed9bce608ea6452b9417a4
Alex Ilin <alexilin@chromium.org>
2020-01-24 13:27:42 +0000

    [identity] Complete the flow in IdentityGetAuthTokenFunction
    The last step of the browser resolution flow is to make another call
    to IssueToken with the consent result. This call should actually
    generate and return the token. If this call fails, an error will be
    returned to the extension.
    
    Bug: 1026237
    Change-Id: If350d325492d7d88f0583cc1687da7e5f7474d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017486
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734908}

--- google_apis/gaia/oauth2_mint_token_flow.h
+++ google_apis/gaia/oauth2_mint_token_flow.h
@@ -113,6 +113,7 @@ class OAuth2MintTokenFlow : public OAuth2ApiCallFlow {
                const std::string& cid,
                const std::vector<std::string>& scopes_arg,
                const std::string& device_id,
+               const std::string& consent_result,
                Mode mode_arg);
     Parameters(const Parameters& other);
     ~Parameters();
@@ -121,6 +122,7 @@ class OAuth2MintTokenFlow : public OAuth2ApiCallFlow {
     std::string client_id;
     std::vector<std::string> scopes;
     std::string device_id;
+    std::string consent_result;
     Mode mode;
   };
 
