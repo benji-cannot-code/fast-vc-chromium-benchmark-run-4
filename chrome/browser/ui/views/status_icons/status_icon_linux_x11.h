b03fe904c5b70eb4738e46a369d3e480ddc8949c
Maksim Sisov <msisov@igalia.com>
2020-01-24 06:26:12 +0000

    StatusIconLinuxX11: Move X11 calls to X11Window.
    On our way to ozone, we need to stop doing platform native calls outside
    platform (ui/platform_window or ui/ozone).
    
    This CL moves all the X11 bits down to X11Window preserving the same
    logic as it used to be before.
    
    However, the X11Window can fail now. When it fails, it doesn't notify
    the client about accelerated widget (it can only happen when the
    X11Window has wm_role_name set to "status-icon), and the client
    (StatusIconLinuxX11) boils up after checking that the widget still has
    remained null after initialization of the widget.
    
    Bug: 1043089
    Change-Id: Ic70573a45fd2194d9b1cc5371a955cf438c2373b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007571
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#734831}

--- chrome/browser/ui/views/status_icons/status_icon_linux_x11.h
+++ chrome/browser/ui/views/status_icons/status_icon_linux_x11.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
-#include "ui/gfx/x/x11_types.h"
 #include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/menu/menu_runner.h"
