c115695848f74a2bd40e27fc731ffeaac0301c17
Vladimir Levin <vmpstr@chromium.org>
2020-01-24 19:45:54 +0000

    FindInPage: Add UKM metrics for use of find-in-page as well as render-subtree.
    This patch adds a UKM metric that tracks two boolean values recorded
    once per (main frame's) document:
    - Whether find-in-page happened on that page
    - Whether a find-in-page activated a render-subtree element.
    
    R=chrishtr@chromium.org, schenney@chromium.org
    
    Bug: 1044299
    Change-Id: I753702069fdad5d3145f7afe822ca638304470b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2013744
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735061}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1646,6 +1646,9 @@ class CORE_EXPORT Document : public ContainerNode,
 
   void ApplyScrollRestorationLogic();
 
+  void MarkHasFindInPageRequest();
+  void MarkHasFindInPageRenderSubtreeActiveMatch();
+
  protected:
   void ClearXMLVersion() { xml_version_ = String(); }
 
@@ -1663,6 +1666,7 @@ class CORE_EXPORT Document : public ContainerNode,
   friend class NthIndexCache;
   FRIEND_TEST_ALL_PREFIXES(FrameFetchContextSubresourceFilterTest,
                            DuringOnFreeze);
+  FRIEND_TEST_ALL_PREFIXES(DocumentTest, FindInPageUkm);
   class NetworkStateObserver;
 
   Document(const DocumentInit& initization,
@@ -2182,6 +2186,10 @@ class CORE_EXPORT Document : public ContainerNode,
   bool in_forced_colors_mode_;
 
   bool applying_scroll_restoration_logic_ = false;
+
+  // Records find-in-page metrics, which are sent to UKM on shutdown.
+  bool had_find_in_page_request_ = false;
+  bool had_find_in_page_render_subtree_active_match_ = false;
 };
 
 extern template class CORE_EXTERN_TEMPLATE_EXPORT Supplement<Document>;
