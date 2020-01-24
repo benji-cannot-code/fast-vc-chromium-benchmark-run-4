3270565a3b2b26fd1873733da11d3156f0bf927c
Sylvain Defresne <sdefresne@chromium.org>
2020-01-24 06:55:50 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/tab_grid
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=edchin@chromium.org
    
    Bug: 1042208
    Change-Id: Ib0f54beba549ba7fd033a0dd8cd070688e48b1b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017327
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734836}

--- ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.mm
@@ -157,7 +157,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.regularTabsMediator = [[TabGridMediator alloc]
       initWithConsumer:baseViewController.regularTabsConsumer];
-  ios::ChromeBrowserState* regularBrowserState =
+  ChromeBrowserState* regularBrowserState =
       _regularBrowser ? _regularBrowser->GetBrowserState() : nullptr;
   WebStateList* regularWebStateList =
       _regularBrowser ? _regularBrowser->GetWebStateList() : nullptr;
