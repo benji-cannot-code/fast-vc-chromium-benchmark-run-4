a238c8ae450adc5f59cbc13f40860ecac01141be
Alex Newcomer <newcomer@chromium.org>
2020-01-24 20:47:53 +0000

    Cros: Fix icon failing to animate from AppsGridView -> ShelfView
    When the drag completes in ScrollableShelfView, there are two
    possibilities:
    1. The drag was Shelf -> Shelf
    2. The drag was AppList -> Shelf
    
    For scenario 1:
     - ScrollableShelfView owns a DragIconProxy |drag_icon_|
     - ShelfView treats ScrollableShelfView as the
       ApplicationDragAndDropHost.
     - ShelfView makes its DragImageView |drag_image_| invisible.
    When scenario 1's drag completes, there is no issue, ScrollableShelfView
    successfully animates |drag_icon_| to the proper bounds.
    
    For scenario 2:
     - AppsGridView treats ShelfView as the ApplicationDragAndDropHost
     - ShelfView creates a DragIconProxy, |drag_image_|.
     - ScrollableShelfView does not create a DragIconProxy.
     - Animation for the scrollable shelf, implemented in
       ScrollableShelfView fails.
    
    To fix scenario 2, pass ShelfView's DragIconProxy
    (ShelfView::drag_image_|) and use it within ScrollableShelfView.
    
    This change is not intended to be a full fix, just a change that we can
    successfully land without refactoring.
    
    I am avoiding refactoring because:
     - Branch is soon.
     - Kukui is coming out with this milestone
     - This code is already prone to bugs, I am worried there will be
       negative effects to a refactor
    
    I created a follow up bug for this refactoring:
    https://bugs.chromium.org/p/chromium/issues/detail?id=1045186
    
    Also in this change:
     - Use a transform instead of a bounds animation.
     - Simplify DragIconDropAnimationDelegate
    
    Bug: 1041999
    Change-Id: I63a428f103a888ef019da919407a60533da68c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017923
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735082}

--- ash/shelf/shelf_view.h
+++ ash/shelf/shelf_view.h
@@ -240,6 +240,10 @@ class ASH_EXPORT ShelfView : public views::AccessiblePaneView,
       const gfx::Point& origin_in_screen_coordinates) override;
 
   void DestroyDragIconProxy() override;
+
+  // Transfers ownership of |drag_image_|, and cleans up DragIconProxy state.
+  DragImageView* RetrieveDragIconProxyAndClearDragProxyState();
+
   bool StartDrag(const std::string& app_id,
                  const gfx::Point& location_in_screen_coordinates) override;
   bool Drag(const gfx::Point& location_in_screen_coordinates) override;
@@ -301,6 +305,9 @@ class ASH_EXPORT ShelfView : public views::AccessiblePaneView,
   // stops the animation.
   void StopAnimatingViewIfAny(views::View* view);
 
+  // Whether ShelfView is handling a drag and drop.
+  bool IsShelfViewHandlingDragAndDrop() const;
+
   // Return the view model for test purposes.
   const views::ViewModel* view_model_for_test() const {
     return view_model_.get();
@@ -358,6 +365,7 @@ class ASH_EXPORT ShelfView : public views::AccessiblePaneView,
   OverflowBubble* overflow_bubble() { return overflow_bubble_.get(); }
   views::ViewModel* view_model() { return view_model_.get(); }
   bool dragged_off_shelf() const { return dragged_off_shelf_; }
+  ShelfID drag_and_drop_shelf_id() const { return drag_and_drop_shelf_id_; }
 
  private:
   friend class ShelfViewTestAPI;
