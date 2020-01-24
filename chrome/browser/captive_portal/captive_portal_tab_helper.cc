c90aa3191aa310a0447f2b17e0f3160d3301398d
Colin Blundell <blundell@chromium.org>
2020-01-24 08:15:59 +0000

    [Captive Portal] Componentize CaptivePortalLoginDetector
    Needed for componentization of CaptivePortalTabHelper, which is itself
    needed as part of the ongoing effort to componentize //chrome's
    SSLErrorHandler for sharing with WebLayer. The only change needed for
    componentization was injection of the CaptivePortalService instance
    rather than obtaining from its //chrome-level factory.
    
    One oddness here is that there was a comment in the code indicating
    that the presence of the CaptivePortalService from its factory was
    conditional on some logic around the specific runtime flow. In fact,
    the CaptivePortalService is always available from construction of the
    CaptivePortalLoginDetector within the CaptivePortalTabHelper
    constructor.
    
    Bug: 1030692
    Change-Id: I907b3becae90c3b0f28b2584cfc6ac7b968c0364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015244
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734850}

--- chrome/browser/captive_portal/captive_portal_tab_helper.cc
+++ chrome/browser/captive_portal/captive_portal_tab_helper.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/debug/dump_without_crashing.h"
-#include "chrome/browser/captive_portal/captive_portal_login_detector.h"
 #include "chrome/browser/captive_portal/captive_portal_service_factory.h"
 #include "chrome/browser/captive_portal/captive_portal_tab_reloader.h"
 #include "chrome/browser/profiles/profile.h"
@@ -15,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "components/captive_portal/content/captive_portal_login_detector.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
@@ -37,7 +37,8 @@ CaptivePortalTabHelper::CaptivePortalTabHelper(
           base::Bind(&CaptivePortalTabHelper::OpenLoginTabForWebContents,
                      web_contents,
                      false))),
-      login_detector_(new CaptivePortalLoginDetector(profile_)),
+      login_detector_(new CaptivePortalLoginDetector(
+          CaptivePortalServiceFactory::GetForProfile(profile_))),
       is_captive_portal_window_(false),
       subscription_(
           CaptivePortalServiceFactory::GetForProfile(profile_)
