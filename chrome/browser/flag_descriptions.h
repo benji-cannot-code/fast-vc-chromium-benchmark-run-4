bcc426b26a486ace95d6ac960b97fd327ebfac3f
Sammie Quon <sammiequon@chromium.org>
2020-01-24 00:59:00 +0000

    cros: Remove ash-enable-overview-rounded-corners flags and reenable test.
    The rounded corners are here to stay. The test may have been related to
    the old impl of using layer masks. Anyhow, I added more reliable waits
    which also might help with the flakiness.
    
    Test: none
    Bug: 1000730, 941048
    Change-Id: I1ca619f37bc1646c741ba4500707269f17dbee29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018166
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Malay Keshav <malaykeshav@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734757}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1948,9 +1948,6 @@ extern const char kAshDragWindowFromShelfDescription[];
 extern const char kAshEnableDisplayMoveWindowAccelsName[];
 extern const char kAshEnableDisplayMoveWindowAccelsDescription[];
 
-extern const char kAshEnableOverviewRoundedCornersName[];
-extern const char kAshEnableOverviewRoundedCornersDescription[];
-
 extern const char kAshEnablePersistentWindowBoundsName[];
 extern const char kAshEnablePersistentWindowBoundsDescription[];
 
