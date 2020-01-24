85ab89f6b42fc0c8a88efbb44055ae493ec8443f
David Bokan <bokan@chromium.org>
2020-01-24 17:54:17 +0000

    [Reland] Make programmatic scrolls respect smooth scroll flag
    Programmatic smooth scrolls should also be instant if the smooth scroll
    flag is explicitly disabled.
    
    Previously landed in https://crrev.com/c/1536759
    
    To reland, this CL force enables the scroll animator in Blink web
    tests. Without this, the usual flow for determining if the animator is
    enabled is followed, which leads to inconsistencies. On Mac, the
    animator is enabled in Chrome but not in content shell. This leads to
    inconsistencies with other platforms (Mac tests ran without smooth
    scroll while Linux/Windows ran with) and ensuring we test what we ship.
    
    This change required some minor fixes to tests that assumed instant
    scroll on Mac (and uncovered one real bug).
    
    Bug: 944583
    Change-Id: I114d8b882d9855fbe0c43645575a1cdd7c3b6e61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2009842
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735006}

--- third_party/blink/renderer/core/scroll/scrollable_area.cc
+++ third_party/blink/renderer/core/scroll/scrollable_area.cc
@@ -316,7 +316,7 @@ void ScrollableArea::ProgrammaticScrollHelper(const ScrollOffset& offset,
         std::move(callback), WrapWeakPersistent(this)));
   }
 
-  if (scroll_behavior == kScrollBehaviorSmooth) {
+  if (scroll_behavior == kScrollBehaviorSmooth && ScrollAnimatorEnabled()) {
     GetProgrammaticScrollAnimator().AnimateToOffset(offset, is_sequenced_scroll,
                                                     std::move(callback));
   } else {
