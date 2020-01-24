0ab3c53fc36a4f10dda4b654a55ea2521a176775
Julian Pastarmov <pastarmovj@chromium.org>
2020-01-24 16:21:28 +0000

    Add DeviceInfoFetcher implementation for Linux
    This implementation still lacks the serial number and lock screen status
    extraction but provides the rest of the information similar to the other
    platforms' implementations.
    
    BUG=1037702
    TEST=unit_tests
    
    Change-Id: I459eeafe02b495962c5d9f2a6e5eddc7ba90b974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2012956
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734955}

--- chrome/browser/extensions/api/enterprise_reporting_private/device_info_fetcher.cc
+++ chrome/browser/extensions/api/enterprise_reporting_private/device_info_fetcher.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/enterprise_reporting_private/device_info_fetcher_mac.h"
 #elif defined(OS_WIN)
 #include "chrome/browser/extensions/api/enterprise_reporting_private/device_info_fetcher_win.h"
+#elif defined(OS_LINUX)
+#include "chrome/browser/extensions/api/enterprise_reporting_private/device_info_fetcher_linux.h"
 #endif
 
 namespace extensions {
@@ -57,6 +59,8 @@ std::unique_ptr<DeviceInfoFetcher> DeviceInfoFetcher::CreateInstance() {
   return std::make_unique<DeviceInfoFetcherMac>();
 #elif defined(OS_WIN)
   return std::make_unique<DeviceInfoFetcherWin>();
+#elif defined(OS_LINUX)
+  return std::make_unique<DeviceInfoFetcherLinux>();
 #else
   return std::make_unique<StubDeviceFetcher>();
 #endif
