c2a6f5f00bc346ad7aeebfa3777294a2345370fc
Sylvain Defresne <sdefresne@chromium.org>
2020-01-24 12:53:24 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/toolbar_container
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=gambard@chromium.org
    
    Bug: 1042208
    Change-Id: I0b86d018f5f218232271e29445b513ab70c82dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017326
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#734901}

--- ios/chrome/browser/ui/toolbar_container/toolbar_container_coordinator.mm
+++ ios/chrome/browser/ui/toolbar_container/toolbar_container_coordinator.mm
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize type = _type;
 @synthesize started = _started;
 
-- (instancetype)initWithBrowserState:(ios::ChromeBrowserState*)browserState
+- (instancetype)initWithBrowserState:(ChromeBrowserState*)browserState
                                 type:(ToolbarContainerType)type {
   if (self = [super initWithBaseViewController:nil browserState:browserState]) {
     _type = type;
