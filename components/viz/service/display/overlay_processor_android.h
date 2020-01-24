9d5a0419269abafeea4bdf2e06ea8563073fa643
Weiliang Chen <weiliangc@chromium.org>
2020-01-24 20:33:15 +0000

    viz: Wait for Creation of OverlayProcessorOnGpu before Using It
    The creation of OverlayProcessorOnGpu is not synchronous, and we might
    try to use it before the task is run on the gpu thread. Make sure to
    wait for the creation if we try to use it.
    
    
    R=rjkroege
    
    Bug: 1044671
    Change-Id: I9c9f02368ab36b3e328a525612f4fbd481e6df97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015345
    Commit-Queue: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735076}

--- components/viz/service/display/overlay_processor_android.h
+++ components/viz/service/display/overlay_processor_android.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_OVERLAY_PROCESSOR_ANDROID_H_
 #define COMPONENTS_VIZ_SERVICE_DISPLAY_OVERLAY_PROCESSOR_ANDROID_H_
 
+#include "base/synchronization/waitable_event.h"
 #include "components/viz/service/display/overlay_processor_using_strategy.h"
 
 namespace base {
@@ -77,6 +78,9 @@ class VIZ_SERVICE_EXPORT OverlayProcessorAndroid
   bool able_to_create_processor_on_gpu_ = false;
   // This class is created, accessed, and destroyed on the gpu thread.
   std::unique_ptr<OverlayProcessorOnGpu> processor_on_gpu_;
+  base::WaitableEvent gpu_init_event_{
+      base::WaitableEvent::ResetPolicy::MANUAL,
+      base::WaitableEvent::InitialState::NOT_SIGNALED};
 
   OverlayCandidateList overlay_candidates_;
 };
