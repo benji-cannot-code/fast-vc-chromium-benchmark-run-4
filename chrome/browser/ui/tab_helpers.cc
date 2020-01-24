3114788833dcdd97cbaa5ab300ea1c5d2e12c214
rajendrant <rajendrant@chromium.org>
2020-01-24 21:47:53 +0000

    Redirect only the public subresource URLs
    This CL uses optimization guide to fetch the public image URLs as hints
    and sends to renderer where URL loader throttles allow redirect only
    for the subresource URLs in the list.
    
    Change-Id: Id654d041c376420a6e4d2a4daf3fdb51b24056f9
    Bug: 1042869
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1996944
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735105}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ssl/connection_help_tab_helper.h"
 #include "chrome/browser/ssl/security_state_tab_helper.h"
 #include "chrome/browser/subresource_filter/chrome_subresource_filter_client.h"
+#include "chrome/browser/subresource_redirect/subresource_redirect_observer.h"
 #include "chrome/browser/sync/sessions/sync_sessions_router_tab_helper.h"
 #include "chrome/browser/sync/sessions/sync_sessions_web_contents_router_factory.h"
 #include "chrome/browser/sync/sync_encryption_keys_tab_helper.h"
@@ -278,6 +279,8 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   if (SiteEngagementService::IsEnabled())
     SiteEngagementService::Helper::CreateForWebContents(web_contents);
   SoundContentSettingObserver::CreateForWebContents(web_contents);
+  subresource_redirect::SubresourceRedirectObserver::MaybeCreateForWebContents(
+      web_contents);
   sync_sessions::SyncSessionsRouterTabHelper::CreateForWebContents(
       web_contents,
       sync_sessions::SyncSessionsWebContentsRouterFactory::GetForProfile(
--- chrome/renderer/subresource_redirect/subresource_redirect_url_loader_throttle_unittest.cc
+++ chrome/renderer/subresource_redirect/subresource_redirect_url_loader_throttle_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/subresource_redirect/subresource_redirect_url_loader_throttle.h"
 
 #include "base/test/scoped_feature_list.h"
+#include "chrome/renderer/subresource_redirect/subresource_redirect_hints_agent.h"
 #include "chrome/renderer/subresource_redirect/subresource_redirect_util.h"
 #include "content/public/common/previews_state.h"
 #include "content/public/common/resource_type.h"
@@ -16,17 +17,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace subresource_redirect {
 
+int kRenderFrameID = 1;
+
+class TestSubresourceRedirectURLLoaderThrottle
+    : public SubresourceRedirectURLLoaderThrottle {
+ public:
+  explicit TestSubresourceRedirectURLLoaderThrottle(
+      std::vector<std::string> public_image_urls)
+      : SubresourceRedirectURLLoaderThrottle(kRenderFrameID) {
+    subresource_redirect_hints_agent_.SetCompressPublicImagesHints(
+        blink::mojom::CompressPublicImagesHints::New(public_image_urls));
+  }
+
+  SubresourceRedirectHintsAgent* GetSubresourceRedirectHintsAgent() override {
+    return &subresource_redirect_hints_agent_;
+  }
+
+ private:
+  SubresourceRedirectHintsAgent subresource_redirect_hints_agent_;
+};
+
 namespace {
 
 std::unique_ptr<SubresourceRedirectURLLoaderThrottle>
-CreateSubresourceRedirectURLLoaderThrottle(const GURL& url,
-                                           content::ResourceType resource_type,
-                                           int previews_state) {
+CreateSubresourceRedirectURLLoaderThrottle(
+    const GURL& url,
+    content::ResourceType resource_type,
+    int previews_state,
+    const std::vector<std::string>& public_image_urls) {
   blink::WebURLRequest request;
   request.SetUrl(url);
   request.SetPreviewsState(previews_state);
-  return SubresourceRedirectURLLoaderThrottle::MaybeCreateThrottle(
-      request, resource_type);
+  DCHECK(SubresourceRedirectURLLoaderThrottle::MaybeCreateThrottle(
+             request, resource_type, kRenderFrameID)
+             .get() != nullptr);
+
+  return std::make_unique<TestSubresourceRedirectURLLoaderThrottle>(
+      public_image_urls);
 }
 
 TEST(SubresourceRedirectURLLoaderThrottleTest, TestMaybeCreateThrottle) {
@@ -77,29 +104,42 @@ TEST(SubresourceRedirectURLLoaderThrottleTest, TestMaybeCreateThrottle) {
     request.SetUrl(GURL(test_case.url));
     EXPECT_EQ(test_case.expected_is_throttle_created,
               SubresourceRedirectURLLoaderThrottle::MaybeCreateThrottle(
-                  request, test_case.resource_type) != nullptr);
+                  request, test_case.resource_type, kRenderFrameID) != nullptr);
   }
 }
 
 TEST(SubresourceRedirectURLLoaderThrottleTest, TestGetSubresourceURL) {
   struct TestCase {
     GURL original_url;
-    GURL redirected_subresource_url;
+    GURL redirected_subresource_url;  // Empty URL means there will be no
+                                      // redirect.
   };
 
   const TestCase kTestCases[]{
       {
-          GURL("https://www.test.com/test.jpg"),
-          GetSubresourceURLForURL(GURL("https://www.test.com/test.jpg")),
+          GURL("https://www.test.com/public_img.jpg"),
+          GetSubresourceURLForURL(GURL("https://www.test.com/public_img.jpg")),
       },
       {
-          GURL("https://www.test.com/test.jpg#test"),
-          GetSubresourceURLForURL(GURL("https://www.test.com/test.jpg#test")),
-      },
-      {
-          GURL("https://www.test.com/test.jpg?foo=bar&baz"),
+          GURL("https://www.test.com/public_img.jpg#anchor"),
           GetSubresourceURLForURL(
-              GURL("https://www.test.com/test.jpg?foo=bar&baz")),
+              GURL("https://www.test.com/public_img.jpg#anchor")),
+      },
+      {
+          GURL("https://www.test.com/"
+               "public_img.jpg?public_arg1=bar&public_arg2"),
+          GetSubresourceURLForURL(
+              GURL("https://www.test.com/"
+                   "public_img.jpg?public_arg1=bar&public_arg2")),
+      },
+      // Private images will not be redirected.
+      {
+          GURL("https://www.test.com/private_img.jpg"),
+          GURL(),
+      },
+      {
+          GURL("https://www.test.com/public_img.jpg&private_arg1=foo"),
+          GURL(),
       },
   };
   base::test::ScopedFeatureList scoped_feature_list;
@@ -109,16 +149,23 @@ TEST(SubresourceRedirectURLLoaderThrottleTest, TestGetSubresourceURL) {
   for (const TestCase& test_case : kTestCases) {
     auto throttle = CreateSubresourceRedirectURLLoaderThrottle(
         test_case.original_url, content::ResourceType::kImage,
-        content::PreviewsTypes::SUBRESOURCE_REDIRECT_ON);
+        content::PreviewsTypes::SUBRESOURCE_REDIRECT_ON,
+        {"https://www.test.com/public_img.jpg",
+         "https://www.test.com/public_img.jpg#anchor",
+         "https://www.test.com/public_img.jpg?public_arg1=bar&public_arg2"});
     network::ResourceRequest request;
     request.url = test_case.original_url;
     request.resource_type = static_cast<int>(content::ResourceType::kImage);
     request.previews_state = content::PreviewsTypes::SUBRESOURCE_REDIRECT_ON;
-    bool defer = false;
+    bool defer = true;
     throttle->WillStartRequest(&request, &defer);
 
-    EXPECT_FALSE(defer);
-    EXPECT_EQ(request.url, test_case.redirected_subresource_url);
+    EXPECT_EQ(defer, test_case.redirected_subresource_url.is_empty());
+    if (!test_case.redirected_subresource_url.is_empty()) {
+      EXPECT_EQ(request.url, test_case.redirected_subresource_url);
+    } else {
+      EXPECT_EQ(request.url, test_case.original_url);
+    }
   }
 }
 
@@ -129,7 +176,8 @@ TEST(SubresourceRedirectURLLoaderThrottleTest, DeferOverridenToFalse) {
 
   auto throttle = CreateSubresourceRedirectURLLoaderThrottle(
       GURL("https://www.test.com/test.jpg"), content::ResourceType::kImage,
-      content::PreviewsTypes::SUBRESOURCE_REDIRECT_ON);
+      content::PreviewsTypes::SUBRESOURCE_REDIRECT_ON,
+      {"https://www.test.com/test.jpg"});
   network::ResourceRequest request;
   request.url = GURL("https://www.test.com/test.jpg");
   request.resource_type = static_cast<int>(content::ResourceType::kImage);
