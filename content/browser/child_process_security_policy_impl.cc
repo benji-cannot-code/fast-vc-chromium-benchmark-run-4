c29a4e1085aaa8752ca6a30f90043da587baa8ad
Lukasz Anforowicz <lukasza@chromium.org>
2020-01-24 22:35:01 +0000

    Move special-casing of opaque origins into CanAccessDataForOrigin.
    Overview
    ========
    
    Before this CL, opaque origins would be allowed without any further
    checks in the following callers of CanAccessDataForOrigin:
    1. VerifyInitiatorOrigin
    2. GetOriginForURLLoaderFactory
    3. AppCacheSubresourceURLFactory::CreateLoaderAndStart
    4. RenderFrameMessageFilter::OnOpenChannelToPepperPlugin
    
    This CL adds a comment with more details on why special casing of opaque
    origins in VerifyInitiatorOrigin is needed.  This CL removes such
    special casing in GetOriginForURLLoaderFactory, CreateLoaderAndStart
    and OnOpenChannelToPepperPlugin.  This is accomplished by moving the
    special-casing into CanAccessDataForOrigin itself.
    
    
    Special-casing opaque origins without a precursor origin
    ========================================================
    
    A browser-initiated navigation to a data: URL will commit an opaque
    origin without a precursor origin.  Such commit may happen in a renderer
    process locked to a http site URL.  See also
    PDFExtensionTest.EnsureInternalPluginDisabled.
    
    To account for the scenario above, before this CL, opaque/no-precursor
    origins would be allowed to commit via
    ChildProcessSecurityPolicyImpl::CanCommitOriginAndUrl.  After this CL,
    opaque/no-precursor origins are allowed in
    ChildProcessSecurityPolicyImpl::CanAccessDataForOrigin instead.
    
    Before this CL, an origin might be allowed to commit (via
    CanCommitOriginAndUrl) but fail to pass post-commit IPC checks later
    (i.e. fail CanAccessDataForOrigin checks) leading to a renderer kill.
    After this CL, this inconsistency is mitigated, avoiding such renderer
    kills (at least in the scenarios special-cased by this CL).
    
    
    Bug: 1029092
    Change-Id: I552668478a2b27b13e956b13d333393fa844ef28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2007983
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735126}

--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -1329,15 +1329,6 @@ CanCommitStatus ChildProcessSecurityPolicyImpl::CanCommitOriginAndUrl(
     const GURL& url) {
   const url::Origin url_origin = url::Origin::Resolve(url, origin);
   if (!CanAccessDataForOrigin(child_id, url_origin)) {
-    // Allow opaque origins w/o precursors to commit.
-    // TODO(acolwell): Investigate all cases that trigger this path and fix
-    // them so we have precursor information. Remove this logic once that has
-    // been completed.
-    if (url_origin.opaque() &&
-        !url_origin.GetTupleOrPrecursorTupleIfOpaque().IsValid()) {
-      return CanCommitStatus::CAN_COMMIT_ORIGIN_AND_URL;
-    }
-
     // Check for special cases, like blob:null/ and data: URLs, where the
     // origin does not contain information to match against the process lock,
     // but using the whole URL can result in a process lock match.
@@ -1350,17 +1341,8 @@ CanCommitStatus ChildProcessSecurityPolicyImpl::CanCommitOriginAndUrl(
     return CanCommitStatus::CANNOT_COMMIT_URL;
   }
 
-  if (!CanAccessDataForOrigin(child_id, origin)) {
-    // Allow opaque origins w/o precursors to commit.
-    // TODO(acolwell): Investigate all cases that trigger this path and fix
-    // them so we have precursor information. Remove this logic once that has
-    // been completed.
-    if (origin.opaque() &&
-        !origin.GetTupleOrPrecursorTupleIfOpaque().IsValid()) {
-      return CanCommitStatus::CAN_COMMIT_ORIGIN_AND_URL;
-    }
+  if (!CanAccessDataForOrigin(child_id, origin))
     return CanCommitStatus::CANNOT_COMMIT_ORIGIN;
-  }
 
   // Ensure that the origin derived from |url| is consistent with |origin|.
   // Note: We can't use origin.IsSameOriginWith() here because opaque origins
@@ -1409,20 +1391,14 @@ bool ChildProcessSecurityPolicyImpl::CanAccessDataForOrigin(
   if (origin.opaque()) {
     auto precursor_tuple = origin.GetTupleOrPrecursorTupleIfOpaque();
     if (!precursor_tuple.IsValid()) {
-      // We don't have precursor information so we only allow access if
-      // the process lock isn't set yet.
+      // Allow opaque origins w/o precursors (if the security state exists).
+      // TODO(acolwell): Investigate all cases that trigger this path (e.g.,
+      // browser-initiated navigations to data: URLs) and fix them so we have
+      // precursor information (or the process lock is compatible with a missing
+      // precursor). Remove this logic once that has been completed.
       base::AutoLock lock(lock_);
       SecurityState* security_state = GetSecurityState(child_id);
-
-      if (security_state && security_state->origin_lock().is_empty())
-        return true;
-
-      LogCanAccessDataForOriginCrashKeys(
-          /* expected_process_lock= */ "(empty)",
-          GetKilledProcessOriginLock(security_state), origin.GetDebugString(),
-          "opaque_origin_without_precursor_in_locked_process");
-
-      return false;
+      return !!security_state;
     } else {
       url_to_check = precursor_tuple.GetURL();
     }
