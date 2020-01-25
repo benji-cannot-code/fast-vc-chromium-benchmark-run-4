ac8dce0256e63f012a863e976df3f9fd2d268c87
Manuel Rego Casasnovas <rego@igalia.com>
2020-01-25 16:43:26 +0000

    Fix performance regression related to overflow recalc
    In r731227 we started to mark the container chain
    in one extra situation compared with the previous code.
    That introduced performance regressions in a few tests.
    That situation was when SelfPaintingLayerNeedsVisualOverflowRecalc()
    was false, in that case we also called
    MarkContainerChainForOverflowRecalcIfNeeded().
    
    This patch comes back to the previous code when we only call
    MarkContainerChainForOverflowRecalcIfNeeded()
    if SelfNeedsLayoutOverflowRecalc() is false.
    
    This can be confusing when we call SetNeedsOverflowRecalc()
    only for visual overflow, as we don't mark the ancestors
    if SelfNeedsLayoutOverflowRecalc(). However this should
    be safe anyway as MarkSelfPaintingLayerForVisualOverflowRecalc()
    would have been called previously.
    
    Bug: 1042855
    Change-Id: Ifc7c0208ffdcef06a740d8a5091ba3f3b7466623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006549
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735235}

--- third_party/blink/renderer/core/layout/layout_object.cc
+++ third_party/blink/renderer/core/layout/layout_object.cc
@@ -1984,8 +1984,7 @@ void LayoutObject::SetPseudoElementStyle(
 }
 
 void LayoutObject::MarkContainerChainForOverflowRecalcIfNeeded(
-    bool mark_container_chain_layout_overflow_recalc,
-    bool mark_container_chain_visual_overflow_recalc) {
+    bool mark_container_chain_layout_overflow_recalc) {
   LayoutObject* object = this;
   do {
     // Cell and row need to propagate the flag to their containing section and
@@ -1997,8 +1996,7 @@ void LayoutObject::MarkContainerChainForOverflowRecalcIfNeeded(
     if (object) {
       if (mark_container_chain_layout_overflow_recalc)
         object->SetChildNeedsLayoutOverflowRecalc();
-      if (mark_container_chain_visual_overflow_recalc)
-        object->MarkSelfPaintingLayerForVisualOverflowRecalc();
+      object->MarkSelfPaintingLayerForVisualOverflowRecalc();
     }
 
   } while (object);
@@ -2006,13 +2004,11 @@ void LayoutObject::MarkContainerChainForOverflowRecalcIfNeeded(
 
 void LayoutObject::SetNeedsOverflowRecalc(
     OverflowRecalcType overflow_recalc_type) {
-  bool mark_container_chain_layout_overflow_recalc = false;
-  bool mark_container_chain_visual_overflow_recalc = false;
+  bool mark_container_chain_layout_overflow_recalc =
+      !SelfNeedsLayoutOverflowRecalc();
 
   if (overflow_recalc_type ==
       OverflowRecalcType::kLayoutAndVisualOverflowRecalc) {
-    mark_container_chain_layout_overflow_recalc =
-        !SelfNeedsLayoutOverflowRecalc();
     SetSelfNeedsLayoutOverflowRecalc();
   }
 
@@ -2021,15 +2017,12 @@ void LayoutObject::SetNeedsOverflowRecalc(
          overflow_recalc_type ==
              OverflowRecalcType::kLayoutAndVisualOverflowRecalc);
   SetShouldCheckForPaintInvalidation();
-  mark_container_chain_visual_overflow_recalc =
-      !SelfPaintingLayerNeedsVisualOverflowRecalc();
   MarkSelfPaintingLayerForVisualOverflowRecalc();
 
-  if (mark_container_chain_layout_overflow_recalc ||
-      mark_container_chain_visual_overflow_recalc) {
+  if (mark_container_chain_layout_overflow_recalc) {
     MarkContainerChainForOverflowRecalcIfNeeded(
-        mark_container_chain_layout_overflow_recalc,
-        mark_container_chain_visual_overflow_recalc);
+        overflow_recalc_type ==
+        OverflowRecalcType::kLayoutAndVisualOverflowRecalc);
   }
 }
 
