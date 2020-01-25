8f722d23061f42943cb05a7f6bd1df56bace9766
Theresa Wellington <twellington@chromium.org>
2020-01-25 22:28:05 +0000

    Introduce //ui/android/../vr/VrModeProvider and move VrModeObserver
    Add interfaces to //ui/android/ that allow observation of VR mode. This
    is in preparation for moving selectable list widgets to
    //components/browser_ui/, which requires breaking dependencies on
    chrome_java classes.
    
    BUG=1003939
    
    Change-Id: If09ea543a79631c57fbe0a44c58afd5cc4c75b72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020547
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735249}

--- ui/android/java/src/org/chromium/ui/vr/VrModeObserver.java
+++ ui/android/java/src/org/chromium/ui/vr/VrModeObserver.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.vr;
+package org.chromium.ui.vr;
 
 /**
  * Used to observe changes to whether Chrome is currently being viewed in VR.
---
+++ ui/android/java/src/org/chromium/ui/vr/VrModeProvider.java
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.vr;
+
+/**
+ * Provides information about VR mode.
+ */
+public interface VrModeProvider {
+    /**
+     * @return Whether VR mode is currently active.
+     */
+    boolean isInVr();
+
+    /**
+     * Registers the given {@link VrModeObserver}.
+     *
+     * @param observer The VrModeObserver to register.
+     */
+    void registerVrModeObserver(VrModeObserver observer);
+
+    /**
+     * Unregisters the given {@link VrModeObserver}.
+     *
+     * @param observer The VrModeObserver to remove.
+     */
+    void unregisterVrModeObserver(VrModeObserver observer);
+}
