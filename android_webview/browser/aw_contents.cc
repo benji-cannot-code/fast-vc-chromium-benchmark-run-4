bb4c054a348ae806a614f330f63cc91acd340b3f
Tao Bai <michaelbai@chromium.org>
2020-01-25 01:43:54 +0000

    Observe the app state - part 1
    -Add WebViewAppStateObserver.
    -Notify app state change through AwContentsLifecycleNotifier.
    
    Bug: 1042048
    Change-Id: Ide14e8583e78b4597fcd061c9dabacf2cb35fe61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015407
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735197}

--- android_webview/browser/aw_contents.cc
+++ android_webview/browser/aw_contents.cc
@@ -261,7 +261,7 @@ AwContents::AwContents(std::unique_ptr<WebContents> web_contents)
     InitAutofillIfNecessary(autofill_manager_delegate->GetSaveFormData());
   content::SynchronousCompositor::SetClientForWebContents(
       web_contents_.get(), &browser_view_renderer_);
-  AwContentsLifecycleNotifier::OnWebViewCreated();
+  AwContentsLifecycleNotifier::GetInstance().OnWebViewCreated(this);
 }
 
 void AwContents::SetJavaPeers(
@@ -363,7 +363,7 @@ AwContents::~AwContents() {
         base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_CRITICAL);
   }
   browser_view_renderer_.SetCurrentCompositorFrameConsumer(nullptr);
-  AwContentsLifecycleNotifier::OnWebViewDestroyed();
+  AwContentsLifecycleNotifier::GetInstance().OnWebViewDestroyed(this);
 }
 
 base::android::ScopedJavaLocalRef<jobject> AwContents::GetWebContents(
---
+++ android_webview/browser/webview_app_state_observer.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/webview_app_state_observer.h"
+
+namespace android_webview {
+
+WebViewAppStateObserver::WebViewAppStateObserver() = default;
+
+WebViewAppStateObserver::~WebViewAppStateObserver() = default;
+
+}  // namespace android_webview
