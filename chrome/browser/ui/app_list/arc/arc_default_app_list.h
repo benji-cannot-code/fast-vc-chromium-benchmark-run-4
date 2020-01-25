72ee2c57ce02607b5bef4a367068b2050f960abb
Yusuke Sato <yusukes@chromium.org>
2020-01-25 02:55:09 +0000

    arcvm: Support board-specific default apps
    On ARC, to support board-specific default apps, the arc-setup command
    creates a symlink from /var/cache/arc_default_apps to
    /usr/share/google-chrome/extensions/arc/<board>. This doesn't work on
    ARCVM where arc-setup is not in use.
    
    To support ARCVM, this CL checks the current board name in Chrome and
    construct the /usr/share/google-chrome/extensions/arc/<board> path at
    runtime.
    
    BUG=b:144199481
    TEST=try, ARCVM still starts
    
    Change-Id: I8dc3f93010d55324914cf774e90c040ef7f0c66b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020479
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735207}

--- chrome/browser/ui/app_list/arc/arc_default_app_list.h
+++ chrome/browser/ui/app_list/arc/arc_default_app_list.h
@@ -62,6 +62,8 @@ class ArcDefaultAppList {
 
   static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
   static void UseTestAppsDirectory();
+  static std::string GetBoardNameForTesting(
+      const base::FilePath& build_prop_path);
 
   // Returns default app info if it is found in defaults and its package is not
   // marked as uninstalled.
