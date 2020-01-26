b427d94daf075ef57cfdc47809d4f8432a46037d
Sylvain Defresne <sdefresne@chromium.org>
2020-01-26 14:22:50 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/autofill/form_input_accessory
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=javierrobles@chromium.org
    
    Bug: 1042208
    Change-Id: I7502c0433e13fe56e4061a63ed84c52f6a21808d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017402
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735274}

--- ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.h
+++ ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.h
@@ -44,8 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                              browserState:
-                                  (ios::ChromeBrowserState*)browserState
+                              browserState:(ChromeBrowserState*)browserState
     NS_UNAVAILABLE;
 
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
