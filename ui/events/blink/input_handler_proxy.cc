6fe036cf4842a1fb23566dfd0b27f4bb13307c7f
Rahul Arakeri <arakeri@microsoft.com>
2020-01-26 21:55:25 +0000

    [CompositorThreadedScrollbarScrolling] Scrollbar invalidation fix.
    The CL fixes a bug where the scrollbar thumb and arrows were not being
    invalidated when users clicked on them. The root cause here was, the
    MouseEvent(s) were not being passed on deep enough into the main thread
    scrollbar code (where the logic to invalidate exists).
    
    Bug: 1040258
    Change-Id: I3a1b413aea06b437f07e37822e16fcfcab11aef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008132
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735303}

--- ui/events/blink/input_handler_proxy.cc
+++ ui/events/blink/input_handler_proxy.cc
@@ -577,7 +577,6 @@ InputHandlerProxy::RouteToTypeSpecificHandler(
                 original_latency_info, mouse_event.TimeStamp());
           }
 
-          // Mark this event as being handled on the compositor thread.
           if (event_with_callback) {
             event_with_callback
                 ->SetScrollbarManipulationHandledOnCompositorThread();
@@ -603,7 +602,6 @@ InputHandlerProxy::RouteToTypeSpecificHandler(
                                        pointer_result, original_latency_info,
                                        mouse_event.TimeStamp());
 
-          // Mark this event as being handled on the compositor thread.
           if (event_with_callback) {
             event_with_callback
                 ->SetScrollbarManipulationHandledOnCompositorThread();
@@ -623,13 +621,15 @@ InputHandlerProxy::RouteToTypeSpecificHandler(
               gfx::Point(mouse_event.PositionInWidget().x(),
                          mouse_event.PositionInWidget().y()));
       if (pointer_result.type == cc::PointerResultType::kScrollbarScroll) {
-        // Generate a GSU event and add it to the CompositorThreadEventQueue.
-        InjectScrollbarGestureScroll(WebInputEvent::Type::kGestureScrollUpdate,
-                                     mouse_event.PositionInWidget(),
-                                     pointer_result, original_latency_info,
-                                     mouse_event.TimeStamp());
+        // Generate a GSU event and add it to the CompositorThreadEventQueue if
+        // delta is non zero.
+        if (!pointer_result.scroll_offset.IsZero()) {
+          InjectScrollbarGestureScroll(
+              WebInputEvent::Type::kGestureScrollUpdate,
+              mouse_event.PositionInWidget(), pointer_result,
+              original_latency_info, mouse_event.TimeStamp());
+        }
 
-        // Mark this event as being handled on the compositor thread.
         if (event_with_callback) {
           event_with_callback
               ->SetScrollbarManipulationHandledOnCompositorThread();
