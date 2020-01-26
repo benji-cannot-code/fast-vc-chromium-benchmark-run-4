dafe534c5ea0c0415a62e3119a0169ae22d74b41
Sylvain Defresne <sdefresne@chromium.org>
2020-01-26 16:02:16 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/ntp
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=justincohen@chromium.org
    
    Bug: 1042208
    Change-Id: I0ef69eaf7d6bbd882e4cab49441f092e6fb2fa68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017524
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735280}

--- ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
+++ ios/chrome/browser/ui/ntp/new_tab_page_coordinator.mm
@@ -36,7 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark - ChromeCoordinator
 
-- (instancetype)initWithBrowserState:(ios::ChromeBrowserState*)browserState {
+- (instancetype)initWithBrowserState:(ChromeBrowserState*)browserState {
   return [super initWithBaseViewController:nil browserState:browserState];
 }
 
