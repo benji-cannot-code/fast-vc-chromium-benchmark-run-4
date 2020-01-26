4d8109fded51519623fa8190ba9d627b09edfa11
Sylvain Defresne <sdefresne@chromium.org>
2020-01-26 18:39:52 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/history
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=sczs@chromium.org
    
    Bug: 1042208
    Change-Id: I21abc98656bf320207009b9d3b089756eb1222ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017346
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735289}

--- ios/chrome/browser/history/top_sites_factory.h
+++ ios/chrome/browser/history/top_sites_factory.h
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/refcounted_browser_state_keyed_service_factory.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state_forward.h"
+
+class ChromeBrowserState;
 
 namespace history {
 class TopSites;
@@ -22,7 +23,7 @@ namespace ios {
 class TopSitesFactory : public RefcountedBrowserStateKeyedServiceFactory {
  public:
   static scoped_refptr<history::TopSites> GetForBrowserState(
-      ios::ChromeBrowserState* browser_state);
+      ChromeBrowserState* browser_state);
   static TopSitesFactory* GetInstance();
 
  private:
