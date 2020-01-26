b9c023324088ff0827e0046439836bd56a529aff
Christopher Cameron <ccameron@chromium.org>
2020-01-26 06:23:50 +0000

    Bake SDR white level into display::Display's color space
    By doing this, we do not need to adjust the brightness of SDR content.
    The only time that we need to adjust content is when displaying HDR
    video that uses the PQ transfer function.
    
    Adjust the default SDR white level to be 100, so that it will behave
    correctly on macOS, now that Windows is no longer relying on the value.
    
    This fixed the bug where PDFs were double-darkened, and may also fix
    other filtering bugs, though that will need to be verified.
    
    Bug: 1038770
    Change-Id: I22dc9d50bd65897234905390e2c2e2f8416a41eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018339
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735263}

--- ui/gfx/color_space_win.cc
+++ ui/gfx/color_space_win.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/color_space_win.h"
 
+#include "base/logging.h"
+
 namespace gfx {
 
 DXVA2_ExtendedFormat ColorSpaceWin::GetExtendedFormat(
@@ -165,9 +167,16 @@ DXGI_COLOR_SPACE_TYPE ColorSpaceWin::GetDXGIColorSpace(
             color_space.GetTransferID() ==
                 gfx::ColorSpace::TransferID::LINEAR_HDR) {
           return DXGI_COLOR_SPACE_RGB_FULL_G10_NONE_P709;
-        } else {
-          return DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709;
+        } else if (color_space.GetTransferID() ==
+                   gfx::ColorSpace::TransferID::CUSTOM_HDR) {
+          skcms_TransferFunction fn;
+          color_space.GetTransferFunction(&fn);
+          if (fn.g == 1.f)
+            return DXGI_COLOR_SPACE_RGB_FULL_G10_NONE_P709;
+          else
+            DLOG(ERROR) << "Windows HDR only supports gamma=1.0.";
         }
+        return DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709;
       }
     }
   } else {
