189f1ade6f0ec2baed24a60c1ef3884b25ca68b2
Vasiliy Telezhnikov <vasilyt@chromium.org>
2020-01-27 21:54:56 +0000

    Import GLFence in EndOverlayAccess for AHB
    When using SurfaceControl after surface has been returned we need to
    wait until fence provided by system will be signaled. When using GL
    for compositing, this is implemented as importing provided file
    descriptor as egl fence and issue ServerWait().
    
    GLRenderer does it when we get surface back. With SkiaRenderer it
    happens on next BeginWrite for the SharedImage. Import is expensive
    operation and BeginWrite is on critical path.
    
    This CL moves import to EndOverlayAccess as we will do it anyway.
    Then at BeginWrite time we only issue ServerWait().
    
    In local tests with scrolling simple page it saves 200us in
    DrawToSwapUs metric.
    
    Change-Id: I6efb2783d0e287dc5e4fbb5707c507328e75a32f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2019607
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735577}

--- gpu/ipc/common/android/android_image_reader_utils.h
+++ gpu/ipc/common/android/android_image_reader_utils.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/gpu_export.h"
 #include "ui/gl/gl_bindings.h"
 
+namespace gl {
+class GLFence;
+}
+
 namespace gpu {
 
 // Create and inserts an egl fence and exports a ScopedFD from it.
@@ -22,8 +26,13 @@ GPU_EXPORT base::ScopedFD CreateEglFenceAndExportFd();
 bool DeleteAImageAsync(AImage* image,
                        base::android::AndroidImageReader* loader);
 
-// Create and insert an EGL fence and imports the provided fence fd.
+// Create and insert an EGL fence and imports the provided fence fd and issues
+// ServerWait on it.
 GPU_EXPORT bool InsertEglFenceAndWait(base::ScopedFD acquire_fence_fd);
+
+// Create and insert an EGL fence and imports the provided fence fd.
+GPU_EXPORT std::unique_ptr<gl::GLFence> CreateEglFence(
+    base::ScopedFD acquire_fence_fd);
 
 // Create an EGL image from the AImage via AHardwarebuffer. Bind this EGL image
 // to the texture target target_id. This changes the texture binding on the
