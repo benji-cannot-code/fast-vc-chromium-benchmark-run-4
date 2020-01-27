bfec3059faaadaa68d6d7de7181ef5b119e14a68
Armando Miraglia <armax@chromium.org>
2020-01-27 18:17:13 +0000

    [EnumeratDevices] Do not report the device ID without permissions.
    Recently, the spec has changed to guarantee better privacy protection in
    enumerateDevices()[1]. The spec change prescribes that at most one device
    per-type should be shown without ID information if the user did not grant
    permissions.
    
    This CL changes the behavior of enumerateDevice as prescribed but
    keeping it behind a flag to better control the rollout of the change.
    
    [1] https://github.com/w3c/mediacapture-main/commit/837ceeff14509ba1e5200c27b9bd190f91f9977c
    
    Bug: 1019176
    Change-Id: I5a15d15363c50e45d86ddb241eccd9a4bd8698b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1964495
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Armando Miraglia <armax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735462}

--- chrome/browser/media/webrtc/webrtc_getmediadevices_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_getmediadevices_browsertest.cc
@@ -264,6 +264,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcGetMediaDevicesBrowserTest,
   ui_test_utils::NavigateToURL(browser(), url);
   content::WebContents* tab =
       browser()->tab_strip_model()->GetActiveWebContents();
+
+  EXPECT_TRUE(GetUserMediaAndAccept(tab));
+
   std::vector<MediaDeviceInfo> devices;
   EnumerateDevices(tab, &devices);
 
@@ -293,6 +296,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcGetMediaDevicesBrowserTest,
       ->SetDefaultCookieSetting(CONTENT_SETTING_BLOCK);
   content::WebContents* tab1 =
       browser()->tab_strip_model()->GetActiveWebContents();
+
+  EXPECT_TRUE(GetUserMediaAndAccept(tab1));
+
   std::vector<MediaDeviceInfo> devices;
   EnumerateDevices(tab1, &devices);
 
@@ -317,6 +323,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcGetMediaDevicesBrowserTest,
       ->SetDefaultCookieSetting(CONTENT_SETTING_BLOCK);
   content::WebContents* tab =
       browser()->tab_strip_model()->GetActiveWebContents();
+
+  EXPECT_TRUE(GetUserMediaAndAccept(tab));
+
   std::vector<MediaDeviceInfo> devices;
   EnumerateDevices(tab, &devices);
 
