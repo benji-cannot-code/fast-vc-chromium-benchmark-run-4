d809017524d00a4b15dc4859b3ea67a14f046491
David Roger <droger@chromium.org>
2020-01-27 12:12:55 +0000

    [settings] Cleanup shouldShowProfile_ and related code
    This CL removes a reference to diceEnabled in shouldShowProfile_.
    Additionally it removes dead code: on desktop this branch is only used
    when signin is disallowed and the user is signed out.
    
    The tests related to the signout button are converted to use the Dice
    flow.
    
    Bug: 891781
    Change-Id: I227bdbce52c38c24920433d6b2c8d21a4c6f3bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015247
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735370}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -456,6 +456,7 @@ CrSettingsPeoplePageTest.prototype = {
   /** @override */
   extraLibraries: CrSettingsBrowserTest.prototype.extraLibraries.concat([
     '../test_browser_proxy.js',
+    '../test_util.js',
     'sync_test_util.js',
     'test_profile_info_browser_proxy.js',
     'test_sync_browser_proxy.js',
