c5f4960680eba55376bdcd6fdd09af6c01a9d416
Francois Beaufort <beaufort.francois@gmail.com>
2020-01-27 07:58:43 +0000

    Remove Android Beam support in Chrome for Android
    This CL removes support for sharing URLs through NFC aka Android Beam as
    it is being deprecating.
    
    https://android-review.googlesource.com/q/topic:%22Deprecate-android-beam%22)
    
    Change-Id: Idb8445e962cdb8d52d870c2f919b67e1b302520a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1989754
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735331}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -107,7 +107,6 @@ import org.chromium.chrome.browser.metrics.ActivityTabStartupMetricsTracker;
 import org.chromium.chrome.browser.metrics.LaunchMetrics;
 import org.chromium.chrome.browser.metrics.UmaSessionStats;
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
-import org.chromium.chrome.browser.nfc.BeamController;
 import org.chromium.chrome.browser.night_mode.NightModeReparentingController;
 import org.chromium.chrome.browser.ntp.NewTabPage;
 import org.chromium.chrome.browser.ntp.NewTabPageUma;
@@ -1054,16 +1053,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             }
             mUpdateNotificationController.onNewIntent(getIntent());
             UpdateMenuItemHelper.getInstance().registerObserver(mUpdateStateChangedListener);
-        });
-
-        DeferredStartupHandler.getInstance().addDeferredTask(() -> {
-            if (isActivityFinishingOrDestroyed()) return;
-            BeamController.registerForBeam(ChromeActivity.this, () -> {
-                Tab currentTab = getActivityTab();
-                if (currentTab == null) return null;
-                if (!currentTab.isUserInteractable()) return null;
-                return currentTab.getUrl();
-            });
         });
 
         final String simpleName = getClass().getSimpleName();
--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -5,18 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.settings;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.graphics.BitmapFactory;
-import android.nfc.NfcAdapter;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Process;
 import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.Preference;
@@ -110,15 +106,6 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
                     .beginTransaction()
                     .replace(android.R.id.content, fragment)
                     .commit();
-        }
-
-        if (ApiCompatibilityUtils.checkPermission(
-                    this, Manifest.permission.NFC, Process.myPid(), Process.myUid())
-                == PackageManager.PERMISSION_GRANTED) {
-            // Disable Android Beam on JB and later devices.
-            // In ICS it does nothing - i.e. we will send a Play Store link if NFC is used.
-            NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
-            if (nfcAdapter != null) nfcAdapter.setNdefPushMessage(null, this);
         }
 
         Resources res = getResources();
