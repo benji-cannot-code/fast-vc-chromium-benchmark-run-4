254c884c85fdb6581cf1db5521c0fdca39b5cbab
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-01-27 14:31:52 +0000

    Add top images to the Deep Scanning upload UI
    The added images support dark mode and vary depending on if the data
    being scanned is "text" (dragged web content or pasted text) or an
    "upload" (dragged file or file upload via a dialog).
    
    Bug: 999145
    Change-Id: I7f4e583bedf92a308dcc65b90f9c7714b0c891e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1991710
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735391}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_views.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_views.h
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 #include "base/time/time.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.h"
+#include "chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_utils.h"
 #include "ui/views/animation/bounds_animator.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/window/dialog_delegate.h"
@@ -19,6 +21,10 @@ namespace content {
 class WebContents;
 }  // namespace content
 
+namespace gfx {
+class ImageSkia;
+}  // namespace gfx
+
 namespace views {
 class ImageView;
 class Throbber;
@@ -26,13 +32,16 @@ class Widget;
 }  // namespace views
 
 namespace safe_browsing {
+class DeepScanningTopImageView;
 
 // Dialog shown for Deep Scanning to offer the possibility of cancelling the
 // upload to the user.
 class DeepScanningDialogViews : public views::DialogDelegate {
  public:
   DeepScanningDialogViews(std::unique_ptr<DeepScanningDialogDelegate> delegate,
-                          content::WebContents* web_contents);
+                          content::WebContents* web_contents,
+                          base::Optional<DeepScanAccessPoint> access_point,
+                          bool is_file_scan);
 
   // views::DialogDelegate:
   base::string16 GetWindowTitle() const override;
@@ -45,6 +54,9 @@ class DeepScanningDialogViews : public views::DialogDelegate {
   // Updates the dialog with the result, and simply delete it from memory if it
   // nothing should be shown.
   void ShowResult(bool success);
+
+  // Returns the appropriate top image depending on |scan_success_|.
+  const gfx::ImageSkia* GetTopImage() const;
 
  private:
   ~DeepScanningDialogViews() override;
@@ -67,14 +79,17 @@ class DeepScanningDialogViews : public views::DialogDelegate {
   // Returns the appropriate dialog message depending on |scan_success_|.
   base::string16 GetDialogMessage() const;
 
-  // Returns the image's color depending on |scan_success_|.
-  SkColor GetImageColor() const;
-
   // Returns the side image's background circle color.
   SkColor GetSideImageBackgroundColor() const;
 
   // Returns the appropriate dialog message depending on |scan_success_|.
   base::string16 GetCancelButtonText() const;
+
+  // Returns the appropriate paste top image ID depending on |scan_success_|.
+  int GetPasteImageId(bool use_dark) const;
+
+  // Returns the appropriate upload top image ID depending on |scan_success_|.
+  int GetUploadImageId(bool use_dark) const;
 
   // Show the dialog. Sets |shown_| to true.
   void Show();
@@ -85,7 +100,7 @@ class DeepScanningDialogViews : public views::DialogDelegate {
 
   // Views above the buttons. |contents_view_| owns every other view.
   std::unique_ptr<views::View> contents_view_;
-  views::ImageView* image_;
+  DeepScanningTopImageView* image_;
   views::ImageView* side_icon_image_;
   views::Throbber* side_icon_spinner_;
   views::Label* message_;
@@ -103,6 +118,14 @@ class DeepScanningDialogViews : public views::DialogDelegate {
 
   // Used to animate dialog height changes.
   std::unique_ptr<views::BoundsAnimator> bounds_animator_;
+
+  // The access point that caused this dialog to open. This changes what text
+  // and top image are shown to the user.
+  base::Optional<DeepScanAccessPoint> access_point_;
+
+  // Indicates whether the scan being done is for files or for text. This
+  // changes what text and top image are shown to the user.
+  bool is_file_scan_;
 
   base::WeakPtrFactory<DeepScanningDialogViews> weak_ptr_factory_{this};
 };
