9cc29fb7d382fa7de56a391e0cd7bbb83df100a9
Mathias Carlen <mcarlen@chromium.org>
2020-01-27 09:52:48 +0000

    [Direct Action] Fix null tab case.
    This patch fixes a NPE by guarding for null tabs. There are situations
    where a tab is not ready or available and direct action availability
    is queried. In that case a null tab would crash instead of reporting
    that this direct action is simply not yet available.
    
    Bug: 1041669
    Change-Id: Idda1c166405c726cca416898fcd842606e74d3cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016625
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735348}

--- chrome/android/java/src/org/chromium/chrome/browser/directactions/FindInPageDirectActionHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/directactions/FindInPageDirectActionHandler.java
@@ -57,6 +57,6 @@ public class FindInPageDirectActionHandler implements DirectActionHandler {
     /** Returns {@code true} if the action is currently available. */
     private final boolean isAvailable() {
         Tab tab = mTabModelSelector.getCurrentTab();
-        return !tab.isNativePage() && tab.getWebContents() != null;
+        return tab != null && !tab.isNativePage() && tab.getWebContents() != null;
     }
 }
