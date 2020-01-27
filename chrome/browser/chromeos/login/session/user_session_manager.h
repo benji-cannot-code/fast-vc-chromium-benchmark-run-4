10a7e6d24dd3866eaf3b3545830a5fd3486fcac2
Rachel Carpenter <carpenterr@chromium.org>
2020-01-27 23:16:06 +0000

    Preload the help app once profile is loaded.
    This is to avoid the spinner that shows while the Get Help app loads.
    See context in https://crbug.com/1026620#c48.
    
    Bug: b/145710734
    Change-Id: Ifb24fdc6242ea9ac52ba6129ba14ffda641997f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016523
    Commit-Queue: Rachel Carpenter <carpenterr@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735628}

--- chrome/browser/chromeos/login/session/user_session_manager.h
+++ chrome/browser/chromeos/login/session/user_session_manager.h
@@ -449,7 +449,7 @@ class UserSessionManager
   void ActivateWizard(OobeScreenId screen);
 
   // Adds first-time login URLs.
-  void InitializeStartUrls() const;
+  void InitializeStartUrls(Profile* profile) const;
 
   // Perform session initialization and either move to additional login flows
   // such as TOS (public sessions), priority pref sync UI (new users) or
