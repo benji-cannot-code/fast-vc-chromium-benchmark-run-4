1199f0052d919d7b5d2910613382d9515a2e9d06
John Delaney <johnidel@chromium.org>
2020-01-27 16:00:18 +0000

    Implement sqlite storage layer for conversion measurement API
    This change implements a storage layer in sqlite for persisting
    impressions and conversions from the API to disk.
    
    The storage for the API consists of two tables, an impression table and
    a conversion table. Every row in the conversion table is associated with
    an impression in the impression table to reuse common data such as
    conversion and reporting origins.
    
    The storage layer does not implement specific functionality like last
    clicked attribution, bit limits, or reporting delays. Instead it abstracts
    that functionality to an injectable delegate class.
    
    This change also includes some common structs that are used by the API,
    such as a new mojo struct blink::mojom::Conversion.
    
    The functionality of the API is described on
    https://github.com/csharrison/conversion-measurement-api
    
    Reference prototype change:
    https://chromium-review.googlesource.com/c/chromium/src/+/1967220
    
    Bug: 1014604
    Change-Id: Icff2d720fe7c595398835fcfd026f6d321cb9c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1965450
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735418}

---
+++ content/browser/conversions/storable_conversion.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/conversions/storable_conversion.h"
+
+#include "base/logging.h"
+
+namespace content {
+
+StorableConversion::StorableConversion(const std::string& conversion_data,
+                                       const url::Origin& conversion_origin,
+                                       const url::Origin& reporting_origin)
+    : conversion_data_(conversion_data),
+      conversion_origin_(conversion_origin),
+      reporting_origin_(reporting_origin) {
+  DCHECK(!reporting_origin_.opaque());
+  DCHECK(!conversion_origin_.opaque());
+}
+
+StorableConversion::StorableConversion(const StorableConversion& other) =
+    default;
+
+StorableConversion::~StorableConversion() = default;
+
+}  // namespace content
