673d9385c92a1256882bd1d0749070012e00ba3d
Finnur Thorarinsson <finnur@chromium.org>
2020-01-28 10:48:28 +0000

    [Android] Photo Picker: Refactor Video Player out of PickerCategoryView.
    This CL creates a custom View (PickerVideoPlayer) to encapsulate the
    code in PickerCategoryView that relates to playing videos. The xml
    components have already been factored out to a separate xml, and this
    moves the corresponding Java code to a separate class also.
    
    Bug: 895776, 656015
    Change-Id: I4435f3a02bf18bc7c1afb35f35f34cb156908544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016649
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735819}

--- chrome/android/javatests/src/org/chromium/chrome/browser/photo_picker/PhotoPickerDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/photo_picker/PhotoPickerDialogTest.java
@@ -53,7 +53,7 @@ import java.util.concurrent.TimeUnit;
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class PhotoPickerDialogTest implements PhotoPickerListener, SelectionObserver<PickerBitmap>,
                                               DecoderServiceHost.ServiceReadyCallback,
-                                              PickerCategoryView.VideoPlaybackStatusCallback,
+                                              PickerVideoPlayer.VideoPlaybackStatusCallback,
                                               AnimationListener {
     // The timeout (in seconds) to wait for the decoder service to be ready.
     private static final long WAIT_TIMEOUT_SECONDS = 30L;
@@ -111,7 +111,7 @@ public class PhotoPickerDialogTest implements PhotoPickerListener, SelectionObse
         mTestFiles.add(new PickerBitmap(Uri.parse("e"), 1L, PickerBitmap.TileTypes.PICTURE));
         mTestFiles.add(new PickerBitmap(Uri.parse("f"), 0L, PickerBitmap.TileTypes.PICTURE));
         PickerCategoryView.setTestFiles(mTestFiles);
-        PickerCategoryView.setProgressCallback(this);
+        PickerVideoPlayer.setProgressCallback(this);
         PickerBitmapView.setAnimationListenerForTest(this);
 
         DecoderServiceHost.setReadyCallback(this);
@@ -358,7 +358,7 @@ public class PhotoPickerDialogTest implements PhotoPickerListener, SelectionObse
 
             TestThreadUtils.runOnUiThreadBlocking(() -> {
                 View mute = categoryView.findViewById(R.id.mute);
-                categoryView.onClick(mute);
+                categoryView.getVideoPlayerForTesting().onClick(mute);
             });
 
             // Clicking the play button should restart playback.
@@ -366,7 +366,7 @@ public class PhotoPickerDialogTest implements PhotoPickerListener, SelectionObse
 
             TestThreadUtils.runOnUiThreadBlocking(() -> {
                 View playbutton = categoryView.findViewById(R.id.video_player_play_button);
-                categoryView.onClick(playbutton);
+                categoryView.getVideoPlayerForTesting().onClick(playbutton);
             });
 
             onVideoEndedCallback.waitForCallback(callCount, 1);
