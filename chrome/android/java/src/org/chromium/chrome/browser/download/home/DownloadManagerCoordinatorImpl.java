84a15846a78e6bb78a90d98382d88ab210c10178
David Trainor <dtrainor@chromium.org>
2020-01-28 06:51:40 +0000

    Remove dependencies from DownloadsHome on Chrome
    - Remove dependencies on the offlinepages package by passing in an
    ObservableSupplier for whether or not prefetch is enabled.
    
    - Remove dependencies on DownloadSettings (which has dependencies that
    cannot be moved yet) by passing in a Callback<Context> to launch the
    settings screen.
    
    - Remove a dependency on the OfflineContentAggregatorFactory (which
    should stay as glue code) by passing in the OfflineContentProvider
    interface.
    
    Bug: 1046032
    Change-Id: I5d8935661be63e452f08578f18cc33ed735b0536
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023505
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735767}

--- chrome/android/java/src/org/chromium/chrome/browser/download/home/DownloadManagerCoordinatorImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/home/DownloadManagerCoordinatorImpl.java
@@ -6,14 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.download.home;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.Callback;
 import org.chromium.base.ObserverList;
 import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.chrome.browser.download.home.filter.Filters;
 import org.chromium.chrome.browser.download.home.filter.Filters.FilterType;
 import org.chromium.chrome.browser.download.home.list.DateOrderedListCoordinator;
@@ -21,13 +24,11 @@ import org.chromium.chrome.browser.download.home.list.DateOrderedListCoordinator
 import org.chromium.chrome.browser.download.home.list.ListItem;
 import org.chromium.chrome.browser.download.home.snackbars.DeleteUndoCoordinator;
 import org.chromium.chrome.browser.download.home.toolbar.ToolbarCoordinator;
-import org.chromium.chrome.browser.download.items.OfflineContentAggregatorFactory;
-import org.chromium.chrome.browser.settings.SettingsLauncher;
-import org.chromium.chrome.browser.settings.download.DownloadSettings;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.browser.widget.selection.SelectionDelegate;
 import org.chromium.chrome.download.R;
 import org.chromium.components.feature_engagement.Tracker;
+import org.chromium.components.offline_items_collection.OfflineContentProvider;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 
 import java.io.Closeable;
@@ -46,6 +47,7 @@ class DownloadManagerCoordinatorImpl
     private final SelectionDelegate<ListItem> mSelectionDelegate;
 
     private final Activity mActivity;
+    private final Callback<Context> mSettingsLauncher;
 
     private ViewGroup mMainView;
 
@@ -53,13 +55,16 @@ class DownloadManagerCoordinatorImpl
 
     /** Builds a {@link DownloadManagerCoordinatorImpl} instance. */
     public DownloadManagerCoordinatorImpl(Activity activity, DownloadManagerUiConfig config,
-            SnackbarManager snackbarManager, ModalDialogManager modalDialogManager, Tracker tracker,
-            FaviconProvider faviconProvider, LegacyDownloadProvider legacyProvider) {
+            ObservableSupplier<Boolean> isPrefetchEnabledSupplier,
+            Callback<Context> settingsLauncher, SnackbarManager snackbarManager,
+            ModalDialogManager modalDialogManager, Tracker tracker, FaviconProvider faviconProvider,
+            OfflineContentProvider provider, LegacyDownloadProvider legacyProvider) {
         mActivity = activity;
+        mSettingsLauncher = settingsLauncher;
         mDeleteCoordinator = new DeleteUndoCoordinator(snackbarManager);
         mSelectionDelegate = new SelectionDelegate<ListItem>();
         mListCoordinator = new DateOrderedListCoordinator(mActivity, config,
-                OfflineContentAggregatorFactory.get(), legacyProvider,
+                isPrefetchEnabledSupplier, provider, legacyProvider,
                 mDeleteCoordinator::showSnackbar, mSelectionDelegate, this::notifyFilterChanged,
                 createDateOrderedListObserver(), modalDialogManager, faviconProvider);
         mToolbarCoordinator = new ToolbarCoordinator(mActivity, this, mListCoordinator,
@@ -153,7 +158,7 @@ class DownloadManagerCoordinatorImpl
     @Override
     public void openSettings() {
         RecordUserAction.record("Android.DownloadManager.Settings");
-        SettingsLauncher.getInstance().launchSettingsPage(mActivity, DownloadSettings.class);
+        mSettingsLauncher.onResult(mActivity);
     }
 
     private void notifyFilterChanged(@FilterType int filter) {
