68483e20df7ec5e6183239951ab10a52c4ae4e64
Robert Ogden <robertogden@chromium.org>
2020-01-28 20:31:26 +0000

    Add IsolatedPrerenderService to own global classes
    This service is a BrowserContextService which owns the proxy
    configurator, but more singletons will be added to it over the course
    of development.
    
    Bug: 1023483
    Change-Id: I4731a742158133ea304c53e2b54c15063f69302f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020885
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736024}

--- chrome/browser/prerender/isolated/isolated_prerender_params.h
+++ chrome/browser/prerender/isolated/isolated_prerender_params.h
@@ -9,7 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "url/gurl.h"
 
+// Returns true if the Isolated Prerender feature is enabled.
+bool IsolatedPrerenderIsEnabled();
+
 // Returns the URL of the proxy server to use in isolated prerenders, if any.
 base::Optional<GURL> IsolatedPrerenderProxyServer();
+
+// Returns true if the proxy for Isolated Prerenders should replace the DRP
+// custom proxy.
+bool IsolatedPrerenderShouldReplaceDataReductionCustomProxy();
 
 #endif  // CHROME_BROWSER_PRERENDER_ISOLATED_ISOLATED_PRERENDER_PARAMS_H_
