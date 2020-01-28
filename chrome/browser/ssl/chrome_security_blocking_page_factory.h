b3e0c5d151a81630b67549ae490665242c1dd820
Colin Blundell <blundell@chromium.org>
2020-01-28 09:07:04 +0000

    [Captive Portal] Move opening of login tab out of CaptivePortalTabHelper
    This CL starts the componentization of CaptivePortalTabHelper, itself a
    prerequisite to the componentization of SSLErrorHandler for sharing
    with WebLayer. In this CL, we move the functionality of opening a new
    tab via interaction with //chrome's Browser object out of
    CaptivePortalTabHelper into ChromeSecurityBlockingPageFactory. The
    latter is a good landing place for this functionality as it in fact
    already needed to invoke this functionality as part of the configuration
    of CaptivePortalBlockingPage.
    
    Bug: 1030692
    Change-Id: I3fc447a1fe74c8cc7c8172f6bd8433e68d4c854f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017488
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735791}

--- chrome/browser/ssl/chrome_security_blocking_page_factory.h
+++ chrome/browser/ssl/chrome_security_blocking_page_factory.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SSL_CHROME_SECURITY_BLOCKING_PAGE_FACTORY_H_
 
 #include "base/macros.h"
+#include "build/build_config.h"
+#include "components/captive_portal/core/buildflags.h"
 #include "components/security_interstitials/content/bad_clock_blocking_page.h"
 #include "components/security_interstitials/content/blocked_interception_blocking_page.h"
 #include "components/security_interstitials/content/captive_portal_blocking_page.h"
@@ -67,6 +69,12 @@ class ChromeSecurityBlockingPageFactory : public SecurityBlockingPageFactory {
   // Overrides the calculation of whether the app is enterprise-managed for
   // tests.
   static void SetEnterpriseManagedForTesting(bool enterprise_managed);
+
+#if BUILDFLAG(ENABLE_CAPTIVE_PORTAL_DETECTION)
+  // Opens a login tab if the profile's active window doesn't have one already.
+  static void OpenLoginTabForWebContents(content::WebContents* web_contents,
+                                         bool focus);
+#endif
 
  private:
   // Does setup on |page| that is specific to the client (Chrome).
