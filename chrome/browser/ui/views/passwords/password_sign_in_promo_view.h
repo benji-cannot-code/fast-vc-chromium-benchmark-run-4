8ea293bbc6e9b1ef5b016cb06ee503595ca61c1b
Mohamed Amir Yosef <mamir@chromium.org>
2020-01-28 19:11:57 +0000

    [Passwords] Introduce SignInPromoBubbleController
    This CL is one of many refactoring CLs that would split the
    ManagePasswordsBubbleModel into different controllers one per view.
    
    Bug: 1044034
    Change-Id: I89e5ebd4b8a7acdda5f0042d69309a5598426292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020944
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735985}

--- chrome/browser/ui/views/passwords/password_sign_in_promo_view.h
+++ chrome/browser/ui/views/passwords/password_sign_in_promo_view.h
@@ -6,17 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_PASSWORDS_PASSWORD_SIGN_IN_PROMO_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_PASSWORDS_PASSWORD_SIGN_IN_PROMO_VIEW_H_
 
+#include "chrome/browser/ui/passwords/bubble_controllers/sign_in_promo_bubble_controller.h"
 #include "chrome/browser/ui/sync/bubble_sync_promo_delegate.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/view.h"
 
-class ManagePasswordsBubbleModel;
+namespace content {
+class WebContents;
+}
+class SignInPromoBubbleController;
 
 // A view that can show up after saving a password without being signed in to
 // offer signing users in so they can access their credentials across devices.
 class PasswordSignInPromoView : public views::View {
  public:
-  explicit PasswordSignInPromoView(ManagePasswordsBubbleModel* model);
+  explicit PasswordSignInPromoView(content::WebContents* web_contents);
   ~PasswordSignInPromoView() override;
 
  private:
@@ -24,7 +28,7 @@ class PasswordSignInPromoView : public views::View {
   // consistency is enabled.
   class DiceSyncPromoDelegate : public BubbleSyncPromoDelegate {
    public:
-    explicit DiceSyncPromoDelegate(ManagePasswordsBubbleModel* model);
+    explicit DiceSyncPromoDelegate(SignInPromoBubbleController* controller);
     ~DiceSyncPromoDelegate() override;
 
     // BubbleSyncPromoDelegate:
@@ -32,12 +36,12 @@ class PasswordSignInPromoView : public views::View {
                       bool is_default_promo_account) override;
 
    private:
-    ManagePasswordsBubbleModel* model_;
+    SignInPromoBubbleController* controller_;
 
     DISALLOW_COPY_AND_ASSIGN(DiceSyncPromoDelegate);
   };
 
-  ManagePasswordsBubbleModel* const model_;
+  SignInPromoBubbleController controller_;
   std::unique_ptr<DiceSyncPromoDelegate> dice_sync_promo_delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(PasswordSignInPromoView);
