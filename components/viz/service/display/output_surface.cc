39507b48b687696db316de4636df3d3695476338
Saman Sami <samans@chromium.org>
2020-01-28 21:38:06 +0000

    Viz/Ozone/DRM: Provide NativePixmap for the primary plane
    Plumb mailbox for the primary plane to OverlayProcessorOzone and use
    SharedImageInterface to obtain the corresponding NativePixmap.
    
    Bug: 756454
    Change-Id: Ic537d05253bc68c27a8fed961de8547ace359e82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016327
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Saman Sami <samans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736065}

--- components/viz/service/display/output_surface.cc
+++ components/viz/service/display/output_surface.cc
@@ -83,4 +83,9 @@ void OutputSurface::SetGpuVSyncCallback(GpuVSyncCallback callback) {
 void OutputSurface::SetGpuVSyncEnabled(bool enabled) {
   NOTREACHED();
 }
+
+gpu::Mailbox OutputSurface::GetOverlayMailbox() const {
+  return gpu::Mailbox();
+}
+
 }  // namespace viz
--- components/viz/service/display_embedder/buffer_queue.h
+++ components/viz/service/display_embedder/buffer_queue.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "components/viz/service/viz_service_export.h"
 #include "gpu/command_buffer/common/mailbox.h"
+#include "gpu/command_buffer/common/sync_token.h"
 #include "gpu/ipc/common/surface_handle.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/color_space.h"
@@ -123,6 +124,7 @@ class VIZ_SERVICE_EXPORT BufferQueue {
   struct VIZ_SERVICE_EXPORT AllocatedSurface {
     AllocatedSurface(std::unique_ptr<gfx::GpuMemoryBuffer> buffer,
                      const gpu::Mailbox& mailbox,
+                     const gpu::SyncToken& creation_sync_token,
                      const gfx::Rect& rect);
     ~AllocatedSurface();
 
@@ -130,6 +132,9 @@ class VIZ_SERVICE_EXPORT BufferQueue {
     // SurfaceHandle, we don't have to keep track of |buffer|.
     std::unique_ptr<gfx::GpuMemoryBuffer> buffer;
     gpu::Mailbox mailbox;
+    // SyncToken generated when |mailbox| was created. The client of BufferQueue
+    // should insert this token in its command stream before using |mailbox|.
+    gpu::SyncToken creation_sync_token;
     gfx::Rect damage;  // This is the damage for this frame from the previous.
   };
 
@@ -139,11 +144,8 @@ class VIZ_SERVICE_EXPORT BufferQueue {
   void UpdateBufferDamage(const gfx::Rect& damage);
 
   // Return a buffer that is available to be drawn into or nullptr if there is
-  // no available buffer and one cannot be created. If a new buffer is created
-  // *|creation_sync_token| is set to a sync token that the client must wait on
-  // before using the buffer.
-  std::unique_ptr<AllocatedSurface> GetNextSurface(
-      gpu::SyncToken* creation_sync_token);
+  // no available buffer and one cannot be created.
+  std::unique_ptr<AllocatedSurface> GetNextSurface();
 
   gpu::SharedImageInterface* const sii_;
   gfx::Size size_;
