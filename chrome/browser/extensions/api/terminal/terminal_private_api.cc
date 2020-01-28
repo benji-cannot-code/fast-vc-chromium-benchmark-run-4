befef9878dae6fa275870e43464aae803fc34661
Joel Hockey <joelhockey@chromium.org>
2020-01-28 04:03:10 +0000

    Check crostini is allowed before starting vmshell
    This fixes crashes in incognito mode where CrostiniManager is null.
    
    Bug: 1038164
    Change-Id: I9da95285a32a643246c4b16376f0af4fa2c31567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024587
    Reviewed-by: Julian Watson <juwa@google.com>
    Commit-Queue: Julian Watson <juwa@google.com>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735737}

--- chrome/browser/extensions/api/terminal/terminal_private_api.cc
+++ chrome/browser/extensions/api/terminal/terminal_private_api.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/system/sys_info.h"
 #include "base/task/post_task.h"
 #include "base/values.h"
+#include "chrome/browser/chromeos/crostini/crostini_features.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/extensions/api/terminal/crostini_startup_status.h"
 #include "chrome/browser/extensions/api/terminal/terminal_extension_helper.h"
@@ -205,6 +206,11 @@ TerminalPrivateOpenTerminalProcessFunction::Run() {
     }
 
   } else if (params->process_name == kVmShellName) {
+    // Ensure crostini is allowed before starting terminal.
+    Profile* profile = Profile::FromBrowserContext(browser_context());
+    if (!crostini::CrostiniFeatures::Get()->IsAllowed(profile))
+      return RespondNow(Error("vmshell not allowed"));
+
     // command=vmshell: ensure --owner_id, --vm_name, and --target_container are
     // set and the specified vm/container is running.
     base::CommandLine vmshell_cmd({kVmShellCommand});
@@ -224,8 +230,7 @@ TerminalPrivateOpenTerminalProcessFunction::Run() {
     auto open_process =
         base::BindOnce(&TerminalPrivateOpenTerminalProcessFunction::OpenProcess,
                        this, user_id_hash, tab_id, vmshell_cmd.argv());
-    auto* mgr = crostini::CrostiniManager::GetForProfile(
-        Profile::FromBrowserContext(browser_context()));
+    auto* mgr = crostini::CrostiniManager::GetForProfile(profile);
     bool verbose =
         !mgr->GetContainerInfo(crostini::kCrostiniDefaultVmName,
                                crostini::kCrostiniDefaultContainerName)
