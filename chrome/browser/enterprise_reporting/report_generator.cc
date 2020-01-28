73cf4ac73013f65af75f1eb29af69baf6a893d0d
Hiroki Sato <hirokisato@google.com>
2020-01-28 00:11:10 +0000

    Replace CHROMEOS macro with OS_CHROMEOS
    CHROMEOS macro is defined in third_party/webrtc.
    This CL replaces it with more appropriate macro.
    
    Bug: None
    Change-Id: I38fd9e830b8d4b9a6585f7b554377e364daf50e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2022024
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735657}

--- chrome/browser/enterprise_reporting/report_generator.cc
+++ chrome/browser/enterprise_reporting/report_generator.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/wmi.h"
 #endif
 
-#if defined(CHROMEOS)
+#if defined(OS_CHROMEOS)
 #include "chrome/browser/enterprise_reporting/android_app_info_generator.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #endif
