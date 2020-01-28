0fb4df84bee67269894ec0df1175205b55491860
Alex Moshchuk <alexmos@chromium.org>
2020-01-28 03:11:51 +0000

    Clean up a few RPHI functions to not pass BrowserContext unnecessarily.
    A few functions in RenderProcessHostImpl, such as IsSuitableHost, were
    passing both a BrowserContext and either an IsolationContext or
    SiteInstance.  This isn't needed, as both IsolationContext and
    SiteInstance already include BrowserContext information, so this CL
    removes the unnecessary BrowserContext arguments.
    
    Bug: 1015882
    Change-Id: I248b999736adf409bb694af7330af8dcf5161d78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2021337
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735721}

--- content/browser/site_instance_impl.cc
+++ content/browser/site_instance_impl.cc
@@ -266,8 +266,7 @@ bool SiteInstanceImpl::HasProcess() {
   if (has_site_ &&
       RenderProcessHost::ShouldUseProcessPerSite(browser_context, site_) &&
       RenderProcessHostImpl::GetSoleProcessHostForSite(
-          browser_context, GetIsolationContext(), site_, lock_url_,
-          IsGuest())) {
+          GetIsolationContext(), site_, lock_url_, IsGuest())) {
     return true;
   }
 
@@ -308,8 +307,7 @@ RenderProcessHost* SiteInstanceImpl::GetProcess() {
     // at this time, we will register it in SetSite().)
     if (process_reuse_policy_ == ProcessReusePolicy::PROCESS_PER_SITE &&
         has_site_) {
-      RenderProcessHostImpl::RegisterSoleProcessHostForSite(browser_context,
-                                                            process_, this);
+      RenderProcessHostImpl::RegisterSoleProcessHostForSite(process_, this);
     }
 
     TRACE_EVENT2("navigation", "SiteInstanceImpl::GetProcess",
@@ -402,10 +400,9 @@ void SiteInstanceImpl::SetSiteAndLockInternal(const GURL& site_url,
     LockToOriginIfNeeded();
 
     // Ensure the process is registered for this site if necessary.
-    if (should_use_process_per_site) {
-      RenderProcessHostImpl::RegisterSoleProcessHostForSite(browser_context,
-                                                            process_, this);
-    }
+    if (should_use_process_per_site)
+      RenderProcessHostImpl::RegisterSoleProcessHostForSite(process_, this);
+
     MaybeSetBrowsingInstanceDefaultProcess();
   }
 }
@@ -520,8 +517,7 @@ bool SiteInstanceImpl::IsSuitableForURL(const GURL& url) {
   }
 
   return RenderProcessHostImpl::IsSuitableHost(
-      GetProcess(), browsing_instance_->GetBrowserContext(),
-      GetIsolationContext(), site_url, origin_lock, IsGuest());
+      GetProcess(), GetIsolationContext(), site_url, origin_lock, IsGuest());
 }
 
 bool SiteInstanceImpl::RequiresDedicatedProcess() {
