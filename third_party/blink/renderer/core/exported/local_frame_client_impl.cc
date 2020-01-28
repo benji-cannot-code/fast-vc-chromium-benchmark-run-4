6b46c175e98f9f320c3066dd5f9cbf3b60a455fb
Mario Sanchez Prada <mario@igalia.com>
2020-01-28 02:13:12 +0000

    Migrate FrameHostMsg_DidChangeLoadProgress IPC message to Mojo
    Add a new method to blink::mojom::LocalFrameHost interface, to be
    implemented right from RFHI, which will allow us to remove the
    WebLocalFrameClient::DidChangeLoadProgress() API plus the related
    bits under //content/renderer.
    
    Bug: 1008432, 1041076
    Change-Id: Ic5e19292b8c8cce75b519fb516e311fb8d029f9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003309
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Mario Sanchez Prada <mario@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#735703}

--- third_party/blink/renderer/core/exported/local_frame_client_impl.cc
+++ third_party/blink/renderer/core/exported/local_frame_client_impl.cc
@@ -600,11 +600,6 @@ void LocalFrameClientImpl::DidStartLoading() {
   }
 }
 
-void LocalFrameClientImpl::ProgressEstimateChanged(double progress_estimate) {
-  if (web_frame_->Client())
-    web_frame_->Client()->DidChangeLoadProgress(progress_estimate);
-}
-
 void LocalFrameClientImpl::DidStopLoading() {
   if (web_frame_->Client())
     web_frame_->Client()->DidStopLoading();
