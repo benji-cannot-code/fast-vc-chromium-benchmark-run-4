0d076ee94bc447ea6092c66871878e41c2d5f89c
Christopher Thompson <cthomp@chromium.org>
2020-01-28 23:16:43 +0000

    Add LEGACY_TLS error
    This adds a new net error and cert status for when a connection uses an
    obsolete version of TLS (i.e., TLS 1.0 or 1.1 in the default
    configuration). This also adds support for the SSLVersionMin enterprise
    policy setting which version triggers the error (and will thus show an
    interstitial warning), allowing enterprises to opt-out of the legacy TLS
    interstitial.
    
    A new interstitial for this error will be added in
    https://crrev.com/c/1940707. Support for the control site list will be
    added in https://crrev.com/c/1906779.
    
    Bug: 896013, 1039756
    Change-Id: Ib24d4b3efcfbbad1f37a41bfed3690280d4d2b71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1894616
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736130}

--- net/ssl/ssl_config.h
+++ net/ssl/ssl_config.h
@@ -36,6 +36,9 @@ enum {
 // Default minimum protocol version.
 NET_EXPORT extern const uint16_t kDefaultSSLVersionMin;
 
+// Default minimum protocol version to warn about.
+NET_EXPORT extern const uint16_t kDefaultSSLVersionMinWarn;
+
 // Default maximum protocol version.
 NET_EXPORT extern const uint16_t kDefaultSSLVersionMax;
 
