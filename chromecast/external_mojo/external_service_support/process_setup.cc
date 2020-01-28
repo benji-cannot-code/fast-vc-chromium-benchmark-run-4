6f34360d3cea51617a5c3f379572b4d9fbc9b2c6
Jin Chen <jinjiechen@google.com>
2020-01-28 00:31:03 +0000

    [chromecast] Remove crash reporter client on fuchsia.
    * Fuchsia uses a different crash report system (crashpad),
    so remove crash_reporter_client dependencies on fuchsia builds.
    
    R=afelch@google.com, kmackay@chromium.org
    
    Change-Id: I823148bf97d3b930d7ad984e071effc9c61affc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024035
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Commit-Queue: JinJie Chen <jinjiechen@google.com>
    Cr-Commit-Position: refs/heads/master@{#735674}

--- chromecast/external_mojo/external_service_support/process_setup.cc
+++ chromecast/external_mojo/external_service_support/process_setup.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "build/build_config.h"
 
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
 #include "chromecast/external_mojo/external_service_support/crash_reporter_client.h"
 #endif
 
@@ -41,7 +41,7 @@ void CommonProcessInitialization(int argc, char** argv) {
       command_line->GetSwitchValueASCII(switches::kEnableFeatures),
       command_line->GetSwitchValueASCII(switches::kDisableFeatures));
 
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
   CrashReporterClient::InitCrashReporter();
 #endif
 
