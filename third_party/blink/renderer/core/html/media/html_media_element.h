82bca31294d29799be752c4685b89dc09e879122
Will Cassella <cassew@google.com>
2020-01-28 02:34:26 +0000

    Remove undefined method declarations in blink::HTMLMediaElement
    Several methods are declared in blink::HTMLMediaElement, but aren't
    actually defined anywhere. This CL removes their declarations.
    
    Change-Id: I111cbf564c78285f9a9698953ef97e99a80a640a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018256
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Will Cassella <cassew@google.com>
    Cr-Commit-Position: refs/heads/master@{#735712}

--- third_party/blink/renderer/core/html/media/html_media_element.h
+++ third_party/blink/renderer/core/html/media/html_media_element.h
@@ -200,7 +200,6 @@ class CORE_EXPORT HTMLMediaElement
   TimeRanges* seekable() const;
   bool ended() const;
   bool Autoplay() const;
-  bool ShouldAutoplay();
   bool Loop() const;
   void SetLoop(bool);
   ScriptPromise playForBindings(ScriptState*);
@@ -245,11 +244,6 @@ class CORE_EXPORT HTMLMediaElement
 
   TextTrackList* textTracks();
   CueTimeline& GetCueTimeline();
-
-  void addTextTrack(TextTrack*);
-  void RemoveTextTrack(TextTrack*);
-  void TextTracksChanged();
-  void NotifyMediaPlayerOfTextTrackChanges();
 
   // Implements the "forget the media element's media-resource-specific tracks"
   // algorithm in the HTML5 spec.
@@ -469,7 +463,6 @@ class CORE_EXPORT HTMLMediaElement
 
   void Seek(double time);
   void FinishSeek();
-  void CheckIfSeekNeeded();
   void AddPlayedRange(double start, double end);
 
   // FIXME: Rename to scheduleNamedEvent for clarity.
@@ -516,7 +509,6 @@ class CORE_EXPORT HTMLMediaElement
   // This does not stop autoplay visibility observation.
   void PauseInternal();
 
-  void UpdateVolume();
   void UpdatePlayState();
   bool PotentiallyPlaying() const;
   bool StoppedDueToErrors() const;
