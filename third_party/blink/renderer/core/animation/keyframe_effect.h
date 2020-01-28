25d1bcb0913ba590295a23fc72a8df4cda17a7e9
Kevin Ellis <kevers@chromium.org>
2020-01-28 17:50:43 +0000

    Implement commitStyles for web animations.
    CommitStyles provides a convenient way to update the style of an
    element, avoiding the need to keep finished animations persistent
    in order to stack effects.
    
    A typical use case is:
    
    const anim = elem.animate(keyframes, {..., fill: 'forward'});
    anim.finished.then(() => {
      anim.commitStyles();
      anim.cancel();
    });
    
    Spec:
    https://drafts.csswg.org/web-animations-1/#dom-animation-commitstyles
    
    Intent:
    https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/H5sz_dg6fKc/1X7K7U4XCgAJ
    
    Bug: 981905
    Change-Id: I37f61960480517e0a8f3427938cf799de4c6e9c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017673
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735948}

--- third_party/blink/renderer/core/animation/keyframe_effect.h
+++ third_party/blink/renderer/core/animation/keyframe_effect.h
@@ -134,6 +134,8 @@ class CORE_EXPORT KeyframeEffect final : public AnimationEffect {
 
   bool AnimationsPreserveAxisAlignment() const;
 
+  ActiveInterpolationsMap InterpolationsForCommitStyles();
+
  private:
   EffectModel::CompositeOperation CompositeInternal() const;
 
