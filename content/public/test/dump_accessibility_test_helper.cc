791122fe6b06b8548852aba784c5ee549613020c
Aaron Leventhal <aleventhal@google.com>
2020-01-28 05:28:33 +0000

    False positives in dump ax tests related to last lines of expectations
    When an expectations file for accessibility dump tree or event tests
    has more lines than the actual output, the test must fail.
    
    Without this change, the following will produce false positives
    in our test results:
    - Expected events the end of event expectations, but are not fired,
    - Expected objects at the end of the object hierarchy that do not
    exist in the actual object hierarchy.
    
    Bug: None
    Change-Id: I4bb65bb73b0d8af7380c85e59c7891c466c95fc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015429
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735749}

--- content/public/test/dump_accessibility_test_helper.cc
+++ content/public/test/dump_accessibility_test_helper.cc
@@ -19,9 +19,10 @@ namespace content {
 namespace {
 const char kCommentToken = '#';
 const char kMarkSkipFile[] = "#<skip";
-const char kMarkEndOfFile[] = "<-- End-of-file -->";
 const char kSignalDiff[] = "*";
 }  // namespace
+
+const char kMarkEndOfFile[] = "<-- End-of-file -->";
 
 DumpAccessibilityTestHelper::DumpAccessibilityTestHelper(
     AccessibilityTestExpectationsLocator* test_locator)
@@ -123,7 +124,6 @@ bool DumpAccessibilityTestHelper::ValidateAgainstExpectation(
     diff += "------\n";
     diff += base::JoinString(actual_lines, "\n");
     diff += "\n";
-    diff += kMarkEndOfFile;
     LOG(ERROR) << "Diff:\n" << diff;
   } else {
     LOG(INFO) << "Test output matches expectations.";
