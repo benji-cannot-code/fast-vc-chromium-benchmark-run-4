ceb7e069857161972a946810df84475a0ceb28e9
Tanmoy Mollik <triploblastic@chromium.org>
2020-01-28 12:24:34 +0000

    Create flag for enabling SyncErrorInfoBarAndroid feature
    Add SyncErrorInfoBarAndroid to chrome://flags for the testing plan.
    
    Bug: 1044903
    Change-Id: I1bcfbb381b21de92c0295daa6badfdbdd1723771
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016631
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735836}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1214,6 +1214,9 @@ extern const char kSuggestionsWithSubStringMatchDescription[];
 extern const char kSyncDeviceInfoInTransportModeName[];
 extern const char kSyncDeviceInfoInTransportModeDescription[];
 
+extern const char kSyncErrorInfoBarName[];
+extern const char kSyncErrorInfoBarDescription[];
+
 extern const char kSyncSandboxName[];
 extern const char kSyncSandboxDescription[];
 
