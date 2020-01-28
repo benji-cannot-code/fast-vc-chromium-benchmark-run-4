5cb2a350498537313adba0a3e07e5cf534bd080a
Sylvain Defresne <sdefresne@chromium.org>
2020-01-28 14:08:31 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/browser_view/browser_view_controller_dependency_factory.mm
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=marq@chromium.org
    
    Bug: 1042208
    Change-Id: I5c3b376cf001f736937a45c92b9fa2754db64d32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017443
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735859}

--- ios/chrome/browser/ui/browser_view/browser_view_controller_dependency_factory.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller_dependency_factory.mm
@@ -18,11 +18,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 @implementation BrowserViewControllerDependencyFactory {
-  ios::ChromeBrowserState* _browserState;
+  ChromeBrowserState* _browserState;
   WebStateList* _webStateList;
 }
 
-- (id)initWithBrowserState:(ios::ChromeBrowserState*)browserState
+- (id)initWithBrowserState:(ChromeBrowserState*)browserState
               webStateList:(WebStateList*)webStateList {
   self = [super init];
   if (self) {
