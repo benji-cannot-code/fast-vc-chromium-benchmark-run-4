1b2d18f94586b7853d1bbfacd893e1140ba06aac
Akihiro Ota <akihiroota@chromium.org>
2020-01-28 00:57:44 +0000

    Implement AXTreeManager methods in AutomationAXTreeWrapper.
    Makes AutomationAXTreeWrapper a subclass of AXTreeManager.
    This is change lays foundation for AXNodePosition support in automation.
    
    Change-Id: Id5c5eaa0e0829dab599b6f7045f649974334faa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018094
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735684}

--- extensions/renderer/api/automation/automation_ax_tree_wrapper.h
+++ extensions/renderer/api/automation/automation_ax_tree_wrapper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/api/automation.h"
 #include "ui/accessibility/ax_event_generator.h"
 #include "ui/accessibility/ax_tree.h"
+#include "ui/accessibility/ax_tree_manager.h"
 
 struct ExtensionMsg_AccessibilityEventBundleParams;
 
@@ -18,7 +19,8 @@ class AutomationInternalCustomBindings;
 
 // A class that wraps one AXTree and all of the additional state
 // and helper methods needed to use it for the automation API.
-class AutomationAXTreeWrapper : public ui::AXTreeObserver {
+class AutomationAXTreeWrapper : public ui::AXTreeObserver,
+                                public ui::AXTreeManager {
  public:
   AutomationAXTreeWrapper(ui::AXTreeID tree_id,
                           AutomationInternalCustomBindings* owner);
@@ -61,6 +63,14 @@ class AutomationAXTreeWrapper : public ui::AXTreeObserver {
   void EventListenerAdded(ax::mojom::Event event_type, ui::AXNode* node);
   void EventListenerRemoved(ax::mojom::Event event_type, ui::AXNode* node);
   bool HasEventListener(ax::mojom::Event event_type, ui::AXNode* node);
+
+  // AXTreeManager methods.
+  ui::AXNode* GetNodeFromTree(const ui::AXTreeID tree_id,
+                              const int32_t node_id) const override;
+  ui::AXTreeID GetTreeID() const override;
+  ui::AXTreeID GetParentTreeID() const override;
+  ui::AXNode* GetRootAsAXNode() const override;
+  ui::AXNode* GetParentNodeFromParentTreeAsAXNode() const override;
 
  private:
   // AXTreeObserver overrides.
