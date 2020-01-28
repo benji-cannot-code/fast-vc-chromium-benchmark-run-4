4b3b644850b7c0c5a3255e831b285be831bb7e31
Natalie Chouinard <chouinard@chromium.org>
2020-01-28 18:10:17 +0000

    Make AutofillProfileEditorPreference a DialogPreference
    Use OnDisplayPreferenceDialogListener on AutofillProfilesFragment to
    launch the Activity-dependent dialog for
    AutofillProfileEditorPreferences, rather than passing Activity through
    to the preference.
    
    Fixed: 982338
    Change-Id: I4210f717cd3f240a632d79b8a2de881de91f57a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024072
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735953}

--- chrome/android/javatests/src/org/chromium/chrome/browser/settings/autofill/AutofillProfilesFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/settings/autofill/AutofillProfilesFragmentTest.java
@@ -271,7 +271,7 @@ public class AutofillProfilesFragmentTest {
                 InstrumentationRegistry.getInstrumentation(),
                 AutofillProfilesFragment.class.getName());
 
-        PreferenceFragmentCompat fragment = (PreferenceFragmentCompat) activity.getMainFragment();
+        AutofillProfilesFragment fragment = (AutofillProfilesFragment) activity.getMainFragment();
         AutofillProfileEditorPreference addProfile =
                 (AutofillProfileEditorPreference) fragment.findPreference(
                         AutofillProfilesFragment.PREF_NEW_PROFILE);
@@ -279,13 +279,14 @@ public class AutofillProfilesFragmentTest {
 
         // Open AutofillProfileEditorPreference.
         TestThreadUtils.runOnUiThreadBlocking(addProfile::performClick);
-        rule.setEditorDialogAndWait(addProfile.getEditorDialog());
+        rule.setEditorDialogAndWait(fragment.getEditorDialogForTest());
         // The keyboard is shown as soon as AutofillProfileEditorPreference comes into view.
         waitForKeyboardStatus(true, activity);
 
         // Hide the keyboard.
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            List<EditText> fields = addProfile.getEditorDialog().getEditableTextFieldsForTest();
+            List<EditText> fields =
+                    fragment.getEditorDialogForTest().getEditableTextFieldsForTest();
             KeyboardVisibilityDelegate.getInstance().hideKeyboard(fields.get(0));
         });
         // Check that the keyboard is hidden.
@@ -320,7 +321,7 @@ public class AutofillProfilesFragmentTest {
             boolean waitForError) throws TimeoutException {
         TestThreadUtils.runOnUiThreadBlocking(profile::performClick);
 
-        rule.setEditorDialogAndWait(profile.getEditorDialog());
+        rule.setEditorDialogAndWait(profileFragment.getEditorDialogForTest());
         if (values != null) rule.setTextInEditorAndWait(values);
         if (waitForError) {
             rule.clickInEditorAndWaitForValidationError(buttonId);
