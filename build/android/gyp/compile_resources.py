12aa6a8c8d9a0fcf3cf57d6ad4594f7caa729cb2
Chan Li <chanli@chromium.org>
2020-01-28 22:43:27 +0000

    Revert "[Android] don't map zh-hk to zh-tw in bundle builds."
    This reverts commit 43fcd55d34ca96458e15c237d015f1b596daf64a.
    
    Reason for revert: 
    
    Suspecting this change caused the compile failure at https://cr-buildbucket.appspot.com/build/8889983375245058816.
    
    Original change's description:
    > [Android] don't map zh-hk to zh-tw in bundle builds.
    > 
    > To give Clank language parity with Android 27 new locales are being
    > added.  26 of those locales have been added in two previous CLs:
    > 
    >    - https://crrev.com/c/1941058
    >    - https://crrev.com/c/1952288
    > 
    > zh-hk was skipped because currently resources for it are mapped to
    > zh-tw.  This CL skips that mapping for bundle builds where zh-hk is
    > now included. zh-hk is still mapped to zh-tw in apk builds.
    > 
    > There is an increase in size even when language splits are used in
    > bundles.  A bug has been created to look into this:
    > - APK Bundles wastes a lot of space in resources.arsc (400kb)
    > - https://crbug.com/1031645
    > 
    > Bug: 1034774
    > Binary-Size: Size increase is unavoidable (see above).
    > Change-Id: I4edb9d5213e8dd3443522deee5395e500af34371
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970258
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: (gone) Eric Stevenson <estevenson@chromium.org>
    > Commit-Queue: Trevor  Perrier <perrier@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#736071}
    
    TBR=yfriedman@chromium.org,tedchoc@chromium.org,agrieve@chromium.org,estevenson@chromium.org,perrier@chromium.org
    
    Change-Id: I6748ad1e997bb5e2e05d6a99a52fd3e3595253fd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1034774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2026151
    Reviewed-by: Chan Li <chanli@chromium.org>
    Commit-Queue: Chan Li <chanli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736091}

--- build/android/gyp/compile_resources.py
+++ build/android/gyp/compile_resources.py
@@ -698,8 +698,7 @@ def _PackageApk(options, build):
                                            build.deps_dir)
   logging.debug('Applying locale transformations')
   path_info = resource_utils.ResourceInfoFile()
-  if options.support_zh_hk:
-    _DuplicateZhResources(dep_subdirs, path_info)
+  _DuplicateZhResources(dep_subdirs, path_info)
   _RenameLocaleResourceDirs(dep_subdirs, path_info)
 
   _RemoveUnwantedLocalizedStrings(dep_subdirs, options)
