3257fdd532a106de04a58caded41d3773440a358
Jun Mukai <mukai@chromium.org>
2020-01-28 00:32:21 +0000

    setTabletModeEnabled test API to force into the specified mode
    This changes the behavior of autotestPrivate.setTabletModeEnabled;
    it will ensure entering into the specified mode, and will not
    end and will not be affected by peripherals.
    
    As is described in the bug, it is crucial to ensure into a certain
    mode for conducting an integration test or a performance test, but
    sometimes the test scenarios or existence of peripherals can affect
    the intermediate states, which may cause failures when combined
    with multiple tests. This CL simplifies this situation by adding
    a new method to ash::TabletMode.
    
    Note that we can't change the behavior of existing SetEnabledForTest,
    because it is widely used by unittests and browsertests.
    
    Bug: 1040292
    Test: tast run ui.ScreenRotationPerf ui.WindowResizePerf on scarlet
    Change-Id: I343e2b86c145bae789629df1baaf503fa2dfdef5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016193
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Jun Mukai <mukai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735676}

--- chrome/browser/chromeos/arc/input_method_manager/arc_input_method_manager_service_unittest.cc
+++ chrome/browser/chromeos/arc/input_method_manager/arc_input_method_manager_service_unittest.cc
@@ -59,6 +59,8 @@ class FakeTabletMode : public ash::TabletMode {
 
   bool InTabletMode() const override { return in_tablet_mode; }
 
+  void ForceUiTabletModeState(base::Optional<bool> enabled) override {}
+
   void SetEnabledForTest(bool enabled) override {
     bool changed = (in_tablet_mode != enabled);
     in_tablet_mode = enabled;
