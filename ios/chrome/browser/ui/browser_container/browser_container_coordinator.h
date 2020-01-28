6fba017c78552ec09fce4d8f4f80e41f5ff95712
Sylvain Defresne <sdefresne@chromium.org>
2020-01-28 14:42:37 +0000

    [ios] Fix uses of ios::ChromeBrowserState in /ios/chrome/browser/ui/browser_container
    The ChromeBrowserState has been moved from the "ios" namespace
    to the global namespace by http://crrev.com/c/2014913. Fix the
    uses from ios::ChromeBrowserState to ChromeBrowserState.
    
    Also remove includes of chrome_browser_state_forward.h and use
    a simple forward declaration instead (since the old name is no
    longer necessary).
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 1042208
    Change-Id: Id47c687129017c1b84c20fdac2964bbd66850ceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017345
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735875}

--- ios/chrome/browser/ui/browser_container/browser_container_coordinator.h
+++ ios/chrome/browser/ui/browser_container/browser_container_coordinator.h
@@ -19,8 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The BrowserContainerCoordinator must be constructed with a Browser, so the
 // ChromeBrowserState initializer is unavailable.
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                              browserState:
-                                  (ios::ChromeBrowserState*)browserState
+                              browserState:(ChromeBrowserState*)browserState
     NS_UNAVAILABLE;
 
 // The view controller managing the container view.
