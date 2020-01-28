a474a83d746b28bc82cde657f50d567f74792b7a
Charlene Yan <cyan@chromium.org>
2020-01-28 23:31:55 +0000

    [Tab Groups] Add user action metrics as defined in the metrics document.
    This includes all actions from the tab group bubble
    (ungroup, close, color selecting, send feedback) as well as logging
    when a tab that belongs to a group is closed.
    
    Bug: 1040657
    Change-Id: I44cda622d9f8e04adc5c9e76a5e841f821edb88e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2019861
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736140}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1624,6 +1624,8 @@ void TabStrip::CloseTab(Tab* tab, CloseTabSource source) {
   }
 
   UpdateHoverCard(nullptr);
+  if (tab->group().has_value())
+    base::RecordAction(base::UserMetricsAction("CloseGroupedTab"));
   controller_->CloseTab(model_index, source);
 }
 
