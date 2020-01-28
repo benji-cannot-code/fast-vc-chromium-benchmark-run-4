1c21fa8d5845de511bb30bc87cc28d1e13a66a4c
Finnur Thorarinsson <finnur@chromium.org>
2020-01-28 17:52:46 +0000

    [Android] Photo Picker: Add flag for enabling Video Support.
    Bug: 895776, 656015
    Change-Id: I196ae815f631376f0ea6cbb26fdadf4e61e2a38d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023791
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#735949}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1628,6 +1628,9 @@ extern const char kOffliningRecentPagesDescription[];
 extern const char kPasswordManagerOnboardingAndroidName[];
 extern const char kPasswordManagerOnboardingAndroidDescription[];
 
+extern const char kPhotoPickerVideoSupportName[];
+extern const char kPhotoPickerVideoSupportDescription[];
+
 extern const char kProcessSharingWithDefaultSiteInstancesName[];
 extern const char kProcessSharingWithDefaultSiteInstancesDescription[];
 
