833478dd7d5f882b8a2a93047d9f7d3afdedcba2
Sky Malice <skym@chromium.org>
2020-01-29 00:58:31 +0000

    Initial cardrenderer interface.
    This CL should expose enough to allow a sort of hello world example,
    but is missing critical pieces. This is because the design is not yet
    finalized and to try to avoid unnecessary churn.
    
    Bug: 1046069
    Change-Id: I330222affdfe547685cfec5981f438b0e4ab4922
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023849
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736172}

---
+++ chrome/browser/cardrenderer/README.md
@@ -0,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This feature contains interfaces used by downstream code. Changes will likely
+cause build failures. Please do not TBR changes through without engaging OWNERS.
---
+++ chrome/browser/cardrenderer/android/java/src/org/chromium/chrome/browser/cardrenderer/CardRendererAdapterFactory.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.cardrenderer;
+
+import android.content.Context;
+
+/**
+ * Creates CardRendererAdapters on demand.
+ */
+public interface CardRendererAdapterFactory {
+    /**
+     * @param context The context that any new Android UI objects should be created within.
+     * @return A new wrapper capable of making view objects.
+     */
+    CardRendererAdapter createCardRendererAdapter(Context context);
+}
