599430344cde1335d276f5dea178b1620a899dfc
Karan Bhatia <karandeepb@chromium.org>
2020-01-29 00:20:08 +0000

    DNR: Remove the [add/remove/get]AllowedPages API functions.
    These are now made redundant by the "allowAllRequests" action. Note we don't
    officially deprecate the API functions since the API is still in beta.
    
    BUG=1038831, 696822
    
    Change-Id: Ie4dfab84de99184e8c22a3f7f95c66c79cc05d64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2001347
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736152}

--- extensions/browser/extension_api_frame_id_map.h
+++ extensions/browser/extension_api_frame_id_map.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/lazy_instance.h"
 #include "base/macros.h"
-#include "base/optional.h"
-#include "url/gurl.h"
 
 namespace content {
 class NavigationHandle;
@@ -46,12 +44,7 @@ class ExtensionApiFrameIdMap {
   // The data for a RenderFrame. Every RenderFrameIdKey maps to a FrameData.
   struct FrameData {
     FrameData();
-    FrameData(int frame_id,
-              int parent_frame_id,
-              int tab_id,
-              int window_id,
-              GURL last_committed_main_frame_url,
-              base::Optional<GURL> pending_main_frame_url);
+    FrameData(int frame_id, int parent_frame_id, int tab_id, int window_id);
     ~FrameData();
 
     FrameData(const FrameData&);
@@ -70,15 +63,6 @@ class ExtensionApiFrameIdMap {
     // The id of the window that the frame is in, or -1 if the frame isn't in a
     // window.
     int window_id;
-
-    // The last committed url of the main frame to which this frame belongs.
-    // This ignores any same-document navigations.
-    GURL last_committed_main_frame_url;
-
-    // The pending main frame url. This is only non-empty for main frame data
-    // when the main frame is ready to commit navigation but hasn't fully
-    // completed the navigation yet. This ignores any same-document navigations.
-    base::Optional<GURL> pending_main_frame_url;
   };
 
   // An invalid extension API frame ID.
