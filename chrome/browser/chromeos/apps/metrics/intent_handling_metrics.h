12e1434270a8e3022760d8a9f034d130931980c0
alex linker <ajlinker@chromium.org>
2020-01-29 00:58:46 +0000

    Move external protocol dialog to intent handling metrics.
    The external protocol dialog class recorded metrics that fall under
    the purview of the intent handling metrics class, so it has been
    moved to be under that class structure.
    
    BUG=1044710
    TEST=ran IntentHandlingMetrics.* unit tests.
    
    Change-Id: I156617bb999ca6072c744e117306b48287144c28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024388
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Alex Linker <ajlinker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736173}

--- chrome/browser/chromeos/apps/metrics/intent_handling_metrics.h
+++ chrome/browser/chromeos/apps/metrics/intent_handling_metrics.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/intent_helper/apps_navigation_throttle.h"
 #include "chrome/browser/apps/intent_helper/apps_navigation_types.h"
+#include "chrome/browser/chromeos/arc/intent_helper/arc_external_protocol_dialog.h"
 #include "components/arc/metrics/arc_metrics_constants.h"
 
 namespace apps {
@@ -30,6 +31,11 @@ class IntentHandlingMetrics {
       IntentPickerCloseReason close_reason,
       Source source,
       bool should_persist);
+
+  static void RecordExternalProtocolMetrics(arc::Scheme scheme,
+                                            apps::PickerEntryType entry_type,
+                                            bool accepted,
+                                            bool persisted);
 };
 
 }  // namespace apps
