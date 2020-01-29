0cc686ebc7b83c30bb24218126ba4cb8b585e9bb
Kyle Horimoto <khorimoto@chromium.org>
2020-01-29 05:33:27 +0000

    [CrOS Sync] Allow transport in SyncableServiceBasedModelTypeController.
    This CL adds the ability to sync in transport mode via
    SyncableServiceBasedModelTypeController. A follow-up CL converts
    SupervisedUserSyncModelTypeController to use transport mode.
    
    Bug: 1030454, 1043755
    Change-Id: I6a49de5d8d5dd9c24422456fbdbc56683e64c002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016195
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736243}

--- components/sync/driver/syncable_service_based_model_type_controller.cc
+++ components/sync/driver/syncable_service_based_model_type_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "components/sync/model_impl/client_tag_based_model_type_processor.h"
+#include "components/sync/model_impl/forwarding_model_type_controller_delegate.h"
 #include "components/sync/model_impl/syncable_service_based_bridge.h"
 
 namespace syncer {
@@ -80,15 +81,33 @@ SyncableServiceBasedModelTypeController::
         ModelType type,
         OnceModelTypeStoreFactory store_factory,
         base::WeakPtr<SyncableService> syncable_service,
-        const base::RepeatingClosure& dump_stack)
-    : ModelTypeController(
-          type,
-          std::make_unique<ControllerDelegate>(type,
-                                               std::move(store_factory),
-                                               syncable_service,
-                                               dump_stack)) {}
+        const base::RepeatingClosure& dump_stack,
+        DelegateMode delegate_mode)
+    : ModelTypeController(type),
+      delegate_(std::make_unique<ControllerDelegate>(type,
+                                                     std::move(store_factory),
+                                                     syncable_service,
+                                                     dump_stack)) {
+  // Delegate for full sync is always created.
+  auto full_sync_delegate =
+      std::make_unique<syncer::ForwardingModelTypeControllerDelegate>(
+          delegate_.get());
+
+  // Delegate for transport is only created if requested and left null
+  // otherwise.
+  std::unique_ptr<syncer::ForwardingModelTypeControllerDelegate>
+      transport_delegate;
+  if (delegate_mode == DelegateMode::kTransportModeWithSingleModel) {
+    transport_delegate =
+        std::make_unique<syncer::ForwardingModelTypeControllerDelegate>(
+            delegate_.get());
+  }
+
+  InitModelTypeController(std::move(full_sync_delegate),
+                          std::move(transport_delegate));
+}
 
 SyncableServiceBasedModelTypeController::
-    ~SyncableServiceBasedModelTypeController() {}
+    ~SyncableServiceBasedModelTypeController() = default;
 
 }  // namespace syncer
