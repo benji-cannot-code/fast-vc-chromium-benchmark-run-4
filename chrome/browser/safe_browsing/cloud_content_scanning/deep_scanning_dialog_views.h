bef4275d1745db552fe9e5fe7fa4797ce3d396ea
vitaliii <vitaliii@chromium.org>
2020-01-29 10:22:03 +0000

    Revert "Update Deep Scanning upload UI text"
    This reverts commit 75458c57e98598eba51ae20916dd51dd17783b7d.
    
    Reason for revert: to unblock reverting https://chromium-review.googlesource.com/c/chromium/src/+/2020891 (crbug.com/1046693)
    
    Original change's description:
    > Update Deep Scanning upload UI text
    > 
    > Add code to use the appropriate drag-data, drag-file and paste strings
    > instead of just defaulting to the upload strings. Removes the optional
    > part of the access point since it's used to determine the appropriate
    > string now.
    > 
    > Bug: 999145
    > Change-Id: I115cd567f94d9c4ca7a4befdff386ba76163ae20
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016965
    > Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#735943}
    
    TBR=drubery@chromium.org,domfc@chromium.org
    
    Change-Id: I78b6b5c3c3fe10cdd731d84233d20c31cb1a9e00
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 999145,1046693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027547
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736301}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_views.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_views.h
@@ -55,7 +55,7 @@ class DeepScanningDialogViews : public views::DialogDelegate {
   };
   DeepScanningDialogViews(std::unique_ptr<DeepScanningDialogDelegate> delegate,
                           content::WebContents* web_contents,
-                          DeepScanAccessPoint access_point,
+                          base::Optional<DeepScanAccessPoint> access_point,
                           bool is_file_scan);
 
   // views::DialogDelegate:
@@ -121,14 +121,6 @@ class DeepScanningDialogViews : public views::DialogDelegate {
   // Returns the appropriate upload top image ID depending on |dialog_status_|.
   int GetUploadImageId(bool use_dark) const;
 
-  // Returns the appropriate pending message ID depending on |access_point_| and
-  // |is_file_scan_|.
-  int GetPendingMessageId() const;
-
-  // Returns the appropriate failure message ID depending on |access_point_| and
-  // |is_file_scan_|.
-  int GetFailureMessageId() const;
-
   // Show the dialog. Sets |shown_| to true.
   void Show();
 
@@ -157,7 +149,7 @@ class DeepScanningDialogViews : public views::DialogDelegate {
 
   // The access point that caused this dialog to open. This changes what text
   // and top image are shown to the user.
-  DeepScanAccessPoint access_point_;
+  base::Optional<DeepScanAccessPoint> access_point_;
 
   // Indicates whether the scan being done is for files or for text. This
   // changes what text and top image are shown to the user.
