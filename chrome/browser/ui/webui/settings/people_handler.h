285a46a7dcefb7bc3b61d2b80476d0ca9314490d
Monica Basta <msalama@chromium.org>
2020-01-29 09:58:27 +0000

    [SyncSetup]: Allow querying sWAA independently from sync.
    As part of friendly settings project, we communicate the sWAA in sync
    setup page. This CL enables to query the sWAA bit independently from
    sync or history sync status. This is needed for the 2 following
    scenarios during which the |WebHistoryService| is null:
    1- The advanced sync setup flow (click on settings from the turn syn on
    confirmation dialog).
    2- Switching sync history from 'Off' to 'On'. This change does not take
    effect until we close the |syncSetup| settings page.
    
    Bug: 1035421
    Change-Id: Id1651bd0d9508643e6218c6cef0c6f310109bef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2012950
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736295}

--- chrome/browser/ui/webui/settings/people_handler.h
+++ chrome/browser/ui/webui/settings/people_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "chrome/browser/ui/webui/settings/settings_page_ui_handler.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
+#include "components/history/core/browser/web_history_service.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/signin/public/base/signin_buildflags.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
@@ -187,6 +188,7 @@ class PeopleHandler : public SettingsPageUIHandler,
 
   void OnQueryHistoryRecordingCompletion(
       const std::string& webui_callback_id,
+      history::WebHistoryService::Request* request,
       const base::Optional<bool>& history_recording_enabled);
 
   void HandleGetStoredAccounts(const base::ListValue* args);
@@ -231,6 +233,11 @@ class PeopleHandler : public SettingsPageUIHandler,
 
   // Used to listen for pref changes to allow or disallow signin.
   PrefChangeRegistrar profile_pref_registrar_;
+
+  // Pending web and app activity requests to query whether history recording
+  // is enabled or not.
+  std::set<std::unique_ptr<history::WebHistoryService::Request>>
+      web_and_app_activity_requests_;
 
   // Manages observer lifetimes.
   ScopedObserver<signin::IdentityManager, signin::IdentityManager::Observer>
