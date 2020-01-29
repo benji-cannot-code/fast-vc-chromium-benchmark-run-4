1da230abb9ca9dbc5b289d8c00f9ed11cdee41c7
Sylvain Defresne <sdefresne@chromium.org>
2020-01-29 16:17:11 +0000

    [android] Stop recording "Stars.LaunchLocation" metric
    The metric has been obsolete for over a year, and is not logged on
    any other platform. Remove the code to log it and all support code.
    
    Bug: 1046680
    Change-Id: I2c795cf41d223c788214b2a4153050f57416a413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027409
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736392}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManager.java
@@ -524,9 +524,8 @@ public class BookmarkManager
     }
 
     @Override
-    public void openBookmark(BookmarkId bookmark, int launchLocation) {
-        if (BookmarkUtils.openBookmark(
-                    mBookmarkModel, mActivity, bookmark, launchLocation)) {
+    public void openBookmark(BookmarkId bookmark) {
+        if (BookmarkUtils.openBookmark(mBookmarkModel, mActivity, bookmark)) {
             BookmarkUtils.finishActivityOnPhone(mActivity);
         }
     }
