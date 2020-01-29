e5dfec22e03b8b83f3cf99f044bd4353a9ab69ec
Jarryd <jarrydg@chromium.org>
2020-01-29 19:44:16 +0000

    Quota: Add NotifyWriteFailed().
    Add a method that storage backends can call to let QuotaManager know
    that a write error will be or has been thrown. This will be
    used as a hint that the disk storing the user's profile might be
    full. In combination with Quota's knowledge of disk usage, this
    function can be used to trigger any actions that the content embedder
    deems appropriate for storage pressure state, such as notifying the
    user.
    
    Logic for calling this function will be in child CLs.
    
    Bug: 997258
    Change-Id: Iae7f77f546778f426e13b936f6bfeb6284cab3bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969235
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736495}

--- storage/browser/quota/quota_manager_proxy.cc
+++ storage/browser/quota/quota_manager_proxy.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "storage/browser/quota/quota_manager_proxy.h"
 
 #include <stdint.h>
+#include <utility>
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
@@ -99,6 +100,16 @@ void QuotaManagerProxy::NotifyOriginNoLongerInUse(const url::Origin& origin) {
   }
   if (manager_)
     manager_->NotifyOriginNoLongerInUse(origin);
+}
+
+void QuotaManagerProxy::NotifyWriteFailed(const url::Origin& origin) {
+  if (!io_thread_->BelongsToCurrentThread()) {
+    io_thread_->PostTask(
+        FROM_HERE,
+        base::BindOnce(&QuotaManagerProxy::NotifyWriteFailed, this, origin));
+  }
+  if (manager_)
+    manager_->NotifyWriteFailed(origin);
 }
 
 void QuotaManagerProxy::SetUsageCacheEnabled(QuotaClient::ID client_id,
