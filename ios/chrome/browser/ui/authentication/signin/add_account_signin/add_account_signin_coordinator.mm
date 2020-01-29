69db8e2f848bc6ae041393541f6a224c428ffdfe
Jérôme Lebel <jlebel@chromium.org>
2020-01-29 23:43:04 +0000

    [iOS] Adding sub coordinators for the new sign-in UI
    Adding empty implementations for:
     + AddAccountSigninCoordinator
     + AdvancedSettingsSigninCoordinator
     + UserSigninCoordinator
    
    Related to crrev.com/c/2025547
    Based on design: http://go/chrome-ios-signin-migration
    
    Bug: 971989
    Change-Id: Id14dc1ab0cfb18985332cc0c6aac021f4d439673
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2025353
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736616}

---
+++ ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation AddAccountSigninCoordinator
+
+- (instancetype)
+    initWithBaseViewController:(UIViewController*)viewController
+                       browser:(Browser*)browser
+                   accessPoint:(signin_metrics::AccessPoint)accessPoint
+                   promoAction:(signin_metrics::PromoAction)promoAction {
+  self = [super initWithBaseViewController:viewController browser:browser];
+  if (self) {
+    // TODO(crbug.com/971989): Needs implementation.
+    NOTIMPLEMENTED();
+  }
+  return self;
+}
+
+@end
---
+++ ios/chrome/browser/ui/authentication/signin/advanced_settings_signin/advanced_settings_signin_coordinator.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_ADVANCED_SETTINGS_SIGNIN_ADVANCED_SETTINGS_SIGNIN_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_ADVANCED_SETTINGS_SIGNIN_ADVANCED_SETTINGS_SIGNIN_COORDINATOR_H_
+
+#import "ios/chrome/browser/ui/authentication/signin/signin_coordinator.h"
+
+// Coordinates the advanced settings to finish the sign-in flow.
+@interface AdvancedSettingsSigninCoordinator : SigninCoordinator
+
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+    NS_UNAVAILABLE;
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+                              browserState:(ChromeBrowserState*)browserState
+    NS_UNAVAILABLE;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_ADVANCED_SETTINGS_SIGNIN_ADVANCED_SETTINGS_SIGNIN_COORDINATOR_H_
