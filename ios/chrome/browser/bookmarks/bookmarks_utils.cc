00f9a0f9bfc1900a4d86504901b165c3f97cf798
Sylvain Defresne <sdefresne@chromium.org>
2020-01-29 12:08:19 +0000

    Mark "Stars.*" histograms as obsolete as appropriate
    Mark "Stars.*" histogram as obsolete since they are no longer
    recorded in the code (except "Stars.LaunchLocation" on Android
    which will be cleaned up in followup CLs).
    
    Bug: 928484
    Change-Id: Iefb54af2240e1899ee6b00bf658eef1a0de610b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024887
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736323}

--- ios/chrome/browser/bookmarks/bookmarks_utils.cc
+++ ios/chrome/browser/bookmarks/bookmarks_utils.cc
@@ -17,12 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using bookmarks::BookmarkModel;
 using bookmarks::BookmarkNode;
 
-void RecordBookmarkLaunch(BookmarkLaunchLocation launch_location) {
-  DCHECK(launch_location < BOOKMARK_LAUNCH_LOCATION_COUNT);
-  UMA_HISTOGRAM_ENUMERATION("Stars.LaunchLocation", launch_location,
-                            BOOKMARK_LAUNCH_LOCATION_COUNT);
-}
-
 bool RemoveAllUserBookmarksIOS(ChromeBrowserState* browser_state) {
   BookmarkModel* bookmark_model =
       ios::BookmarkModelFactory::GetForBrowserState(browser_state);
--- ios/chrome/browser/ui/omnibox/chrome_omnibox_client_ios.mm
+++ ios/chrome/browser/ui/omnibox/chrome_omnibox_client_ios.mm
@@ -166,10 +166,6 @@ void ChromeOmniboxClientIOS::OnResultChanged(
   }
 }
 
-void ChromeOmniboxClientIOS::OnBookmarkLaunched() {
-  RecordBookmarkLaunch(BOOKMARK_LAUNCH_LOCATION_OMNIBOX);
-}
-
 void ChromeOmniboxClientIOS::DiscardNonCommittedNavigations() {
   controller_->GetWebState()
       ->GetNavigationManager()
