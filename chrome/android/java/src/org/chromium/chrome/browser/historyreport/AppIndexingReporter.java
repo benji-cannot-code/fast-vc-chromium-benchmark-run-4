a52deccaf37dde6c36ba81062a305daac8991d81
Sam Bowen <sgbowen@google.com>
2020-01-29 18:05:32 +0000

    Reland "Rename CopylessPaste to DocumentMetadata to reuse the class for other purposes"
    This is a two-sided patch and will break downstream clank until the following is landed:
    https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/2473783
    
    There are no changes to this reland. The fix for the original revert is
    in the other patch linked above.
    
    This is a reland of da0bf646042d9ee6f8d1c00185be29d6e520506e
    
    Original change's description:
    > Rename CopylessPaste to DocumentMetadata to reuse the class for other purposes
    >
    > Bug: 1044244
    > Change-Id: I7af8208b8c5a25f247b09cc0cbe55d44934f1d2d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020604
    > Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Sam Bowen <sgbowen@google.com>
    > Cr-Commit-Position: refs/heads/master@{#735612}
    
    TBR=dcheng@chromium.org,haraken@chromium.org
    
    Bug: 1044244
    Change-Id: I5348c86f79e39539d21956f82981a3e6d49d7009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2026409
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Sam Bowen <sgbowen@google.com>
    Cr-Commit-Position: refs/heads/master@{#736439}

--- chrome/android/java/src/org/chromium/chrome/browser/historyreport/AppIndexingReporter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/historyreport/AppIndexingReporter.java
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.historyreport;
 
-import org.chromium.blink.mojom.document_metadata.WebPage;
+import org.chromium.blink.mojom.WebPage;
 import org.chromium.chrome.browser.AppHooks;
 
 /** Base class for reporting entities to App Indexing. */
