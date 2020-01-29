a2d877f9ee048a5df55c324199cbaaaccf894c92
Sophie Chang <sophiechang@chromium.org>
2020-01-29 03:15:34 +0000

    Cap number of concurrent page navigation fetches that can happen
    Bug: 1036490
    Change-Id: Ib8bec196e208c0bbb86ec670ba48a7b2c859d565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2025914
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736199}

--- components/optimization_guide/optimization_guide_features.cc
+++ components/optimization_guide/optimization_guide_features.cc
@@ -185,7 +185,15 @@ GetMaxEffectiveConnectionTypeForNavigationHintsFetch() {
 }
 
 base::TimeDelta GetHintsFetchRefreshDuration() {
-  return base::TimeDelta::FromHours(72);
+  return base::TimeDelta::FromHours(GetFieldTrialParamByFeatureAsInt(
+      kRemoteOptimizationGuideFetching, "hints_fetch_refresh_duration_in_hours",
+      72));
+}
+
+size_t MaxConcurrentPageNavigationFetches() {
+  return GetFieldTrialParamByFeatureAsInt(
+      kRemoteOptimizationGuideFetching,
+      "max_concurrent_page_navigation_fetches", 20);
 }
 
 base::TimeDelta StoredHostModelFeaturesFreshnessDuration() {
