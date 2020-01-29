5d1528cf9b90467fb081c5692732debfa2fa3864
Tricia Crichton <triciac@chromium.org>
2020-01-29 19:34:49 +0000

    [ChromeDriver] increase timeout for tests
    Python tests show increased flakiness caused by command timeouts after
    reducing the timeout to 10 seconds. Increasing to 30 seconds to reduce
    timeouts
    
    Bug: 1045241
    Change-Id: I0f564f564155c294ac3e041518096b88a705f94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028174
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tricia Crichton <triciac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736482}

--- chrome/test/chromedriver/client/command_executor.py
+++ chrome/test/chromedriver/client/command_executor.py
@@ -5,7 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import httplib
 import json
+import os
+import sys
 from urlparse import urlparse
+
+_THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+_PARENT_DIR = os.path.join(_THIS_DIR, os.pardir)
+sys.path.insert(1, _PARENT_DIR)
+import util
+sys.path.remove(_PARENT_DIR)
 
 class _Method(object):
   GET = 'GET'
@@ -200,8 +208,12 @@ class CommandExecutor(object):
   def __init__(self, server_url):
     self._server_url = server_url
     parsed_url = urlparse(server_url)
+    timeout = 10
+    # see https://crbug.com/1045241: short timeout seems to introduce flakiness
+    if util.IsMac() or util.IsWindows():
+      timeout = 30
     self._http_client = httplib.HTTPConnection(
-      parsed_url.hostname, parsed_url.port, timeout=10)
+      parsed_url.hostname, parsed_url.port, timeout=timeout)
 
   @staticmethod
   def CreatePath(template_url_path, params):
