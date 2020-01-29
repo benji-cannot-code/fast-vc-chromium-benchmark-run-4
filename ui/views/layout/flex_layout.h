b8775cbc5683eeb72c5b76e72cb6010889b52a05
Peter Kasting <pkasting@chromium.org>
2020-01-29 08:33:47 +0000

    Factor out methods for getting the preferred/current size of a view.
    This makes InitializeChildData() easier to understand, allows a tiny bit
    of code reuse, and will make future changes to both these functions and
    their callers simpler.
    
    Bug: none
    Change-Id: If19e736dbdc00d252c6043d6cf499cffd5d33386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2026352
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736275}

--- ui/views/layout/flex_layout.h
+++ ui/views/layout/flex_layout.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 
+class NormalizedSize;
 class NormalizedSizeBounds;
 class View;
 
@@ -145,6 +146,21 @@ class VIEWS_EXPORT FlexLayout : public LayoutManagerBase {
   // to the indices of child views within that order that can flex.
   // See FlexSpecification::order().
   using FlexOrderToViewIndexMap = std::map<int, std::vector<size_t>>;
+
+  // Returns the preferred size for a given |rule| and |child| given unbounded
+  // space, with the caveat that for vertical layouts the horizontal axis is
+  // bounded to |available_cross| to factor in height-for-width considerations.
+  // This corresponds to the FlexSpecification "preferred size".
+  NormalizedSize GetPreferredSizeForRule(
+      const FlexRule& rule,
+      const View* child,
+      const base::Optional<int>& available_cross) const;
+
+  // Returns the size for a given |rule| and |child| with |available| space.
+  NormalizedSize GetCurrentSizeForRule(
+      const FlexRule& rule,
+      const View* child,
+      const NormalizedSizeBounds& available) const;
 
   // Fills out the child entries for |data| and generates some initial size
   // and visibility data, and stores off information about which views can
