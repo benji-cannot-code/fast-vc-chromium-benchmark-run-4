0f10d414056c342afb7f34e4b8613a6beabfcb4b
shivanigithub <shivanisha@chromium.org>
2020-01-29 23:21:53 +0000

    Add largest contentful paint for pages that load a third-party font
    Change-Id: Ie8ba0b911176f00be1b1cb77c0fedfe09ce229a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008288
    Commit-Queue: Shivani Sharma <shivanisha@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736607}

--- chrome/browser/page_load_metrics/observers/third_party_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/third_party_metrics_observer.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/macros.h"
+#include "components/page_load_metrics/browser/observers/largest_contentful_paint_handler.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
 #include "url/gurl.h"
 #include "url/origin.h"
@@ -26,6 +27,8 @@ class ThirdPartyMetricsObserver
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
   void OnComplete(
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
+  void OnLoadedResource(const page_load_metrics::ExtraRequestCompleteInfo&
+                            extra_request_complete_info) override;
   void OnCookiesRead(const GURL& url,
                      const GURL& first_party_url,
                      const net::CookieList& cookie_list,
@@ -65,16 +68,18 @@ class ThirdPartyMetricsObserver
                                const GURL& first_party_url,
                                bool blocked_by_policy,
                                AccessType access_type);
-  void RecordMetrics();
+  void RecordMetrics(
+      const page_load_metrics::mojom::PageLoadTiming& main_frame_timing);
 
-  // A map of third parties that have read or written cookies, or have accessed
-  // local storage or session storage on this page.
+  // A map of third parties that have read or written cookies, or have
+  // accessed local storage or session storage on this page.
   //
-  // A third party document.cookie / window.localStorage / window.sessionStorage
-  // happens when the context's scheme://eTLD+1 differs from the main frame's.
-  // A third party resource request happens when the URL request's
-  // scheme://eTLD+1 differs from the main frame's. For URLs which have no
-  // registrable domain, the hostname is used instead.
+  // A third party document.cookie / window.localStorage /
+  // window.sessionStorage happens when the context's scheme://eTLD+1
+  // differs from the main frame's. A third party resource request happens
+  // when the URL request's scheme://eTLD+1 differs from the main frame's.
+  // For URLs which have no registrable domain, the hostname is used
+  // instead.
   std::map<GURL, AccessedTypes> third_party_accessed_types_;
 
   // A set of RenderFrameHosts that we've recorded timing data for. The
@@ -85,6 +90,12 @@ class ThirdPartyMetricsObserver
   // block third-party cookies is enabled) then we don't want to record any
   // metrics for the page.
   bool should_record_metrics_ = true;
+
+  // True if this page loaded a third-party font.
+  bool third_party_font_loaded_ = false;
+
+  page_load_metrics::LargestContentfulPaintHandler
+      largest_contentful_paint_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(ThirdPartyMetricsObserver);
 };
