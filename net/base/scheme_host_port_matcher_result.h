2ee3ec18fc8ec485036a53464f76a90e47e19019
Shimi Zhang <ctzsm@chromium.org>
2020-01-29 21:59:02 +0000

    [ProxyBypassRules] Extract ProxyBypassRules::Rule out
    We are going to expose the internal Rule class and some common rules
    sub-classes inside of ProxyBypassRules, so we could build different
    rule set based on different business logic.
    
    Bug: 1030092
    Change-Id: I054e05bf1302506bee77e5c1a8e30ee8f973daea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008012
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736558}

---
+++ net/base/scheme_host_port_matcher_result.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_BASE_SCHEME_HOST_PORT_MATCHER_RESULT_H_
+#define NET_BASE_SCHEME_HOST_PORT_MATCHER_RESULT_H_
+
+namespace net {
+
+// The result of evaluating an URLMatcherRule.
+//
+// Matches can be for including a URL, or for excluding a URL, or neither of
+// them.
+enum class SchemeHostPortMatcherResult {
+  // No match.
+  kNoMatch,
+  // The URL should be included.
+  kInclude,
+  // The URL should be excluded.
+  kExclude,
+};
+
+}  // namespace net
+#endif  // NET_BASE_SCHEME_HOST_PORT_MATCHER_RESULT_H_
