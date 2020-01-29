d279a5d31493d3b905aa131124479a0249aa793d
Owen Min <zmin@chromium.org>
2020-01-29 16:11:25 +0000

    Check extension request policy when generate profile report
    set_extension_request_enabled is not used by any ProfileReportGenerator
    caller. Let the ProfileReportGenerator read the policy value for each
    profile.
    
    Bug: 1006899
    Change-Id: I968656200a214e80e9a1f0502b0e63dddf98c916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024696
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736390}

--- chrome/browser/enterprise_reporting/profile_report_generator_unittest.cc
+++ chrome/browser/enterprise_reporting/profile_report_generator_unittest.cc
@@ -163,7 +163,9 @@ TEST_F(ProfileReportGeneratorTest, PoliciesDisabled) {
 }
 
 TEST_F(ProfileReportGeneratorTest, PendingRequest) {
-  generator_.set_extension_request_enabled(true);
+  profile()->GetTestingPrefService()->SetManagedPref(
+      prefs::kCloudExtensionRequestEnabled,
+      std::make_unique<base::Value>(true));
   std::vector<std::string> ids = {kExtensionId};
   SetExtensionToPendingList(ids);
 
@@ -174,7 +176,9 @@ TEST_F(ProfileReportGeneratorTest, PendingRequest) {
 }
 
 TEST_F(ProfileReportGeneratorTest, NoPendingRequestWhenItsDisabled) {
-  generator_.set_extension_request_enabled(false);
+  profile()->GetTestingPrefService()->SetManagedPref(
+      prefs::kCloudExtensionRequestEnabled,
+      std::make_unique<base::Value>(false));
   std::vector<std::string> ids = {kExtensionId};
   SetExtensionToPendingList(ids);
 
