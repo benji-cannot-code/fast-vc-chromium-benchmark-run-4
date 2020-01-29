46fcb2894882f49e48e0a12c5e489aaa2e13f17a
Hitoshi Yoshida <peria@chromium.org>
2020-01-29 14:35:50 +0000

    bindings: Forward declaration of PointerEventInit
    Without this forward declaration, pointer_event.h and v8_pointer_event.h
    make a cyclic inclusion.  It triggers a compile error in definition of
    PointerEventInit because we don't have declarations of PointerEvent class.
    
    This error happens when we use the new code generator.
    
    Bug: 839389
    Change-Id: I202d1c15dcc82c5800cc4ab8135784135f280086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2026838
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736354}

--- third_party/blink/renderer/core/events/pointer_event.cc
+++ third_party/blink/renderer/core/events/pointer_event.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/events/pointer_event.h"
 
+#include "third_party/blink/renderer/bindings/core/v8/v8_pointer_event_init.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/events/event_dispatcher.h"
 #include "third_party/blink/renderer/core/dom/events/event_path.h"
--- third_party/blink/renderer/core/exported/web_frame_test.cc
+++ third_party/blink/renderer/core/exported/web_frame_test.cc
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_serializer.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_node.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_pointer_event_init.h"
 #include "third_party/blink/renderer/core/clipboard/data_transfer.h"
 #include "third_party/blink/renderer/core/clipboard/system_clipboard.h"
 #include "third_party/blink/renderer/core/css/media_values.h"
