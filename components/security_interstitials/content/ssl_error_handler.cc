074fb22e14777c1a0715b5c7fa6bce3c8979de06
Christopher Thompson <cthomp@chromium.org>
2020-01-29 18:19:41 +0000

    Reland "Add legacy TLS interstitial warning"
    This is a reland of cb98eddd39fc9a8912767086dc55e97eddfa7351
    
    This fixes a crash caused by a call to GetStringFUTF16() with a format string that didn’t have placeholders.
    
    Original change's description:
    > Add legacy TLS interstitial warning
    >
    > This adds a new interstitial page for the legacy TLS warning. It
    > generally follows the SSL interstitial, but with different strings and
    > a different icon (the (i) info icon).
    >
    > The test interstitial is available at chrome://interstitials/legacy-tls
    >
    > Screenshot: https://drive.google.com/file/d/1MkWCqWKtQwo7k-ybYjKotYJDjTzRwcd0/view
    >
    > This also refactors the legacy TLS browsertest classes slightly, basing
    > them on SSLUITest rather than CertVerifierBrowserTest, brings in the
    > mock cert verifier explicitly. It also adds a SetPolicy() method to
    > SSLUITestBase (to complement the EnablePolicy() method, but for policies
    > that take base::Value types, in this case for the SSLVersionMin policy).
    > Overall, this makes for a slightly simpler class structure and lets us
    > reuse all of the infrastructure setup by SSLUITest (since
    > CertVerifierBrowserTest is fairly simple in comparison).
    >
    > The new cert error is introduced in https://crrev.com/c/1894616. Support
    > for the control site list will be added in https://crrev.com/c/1906779.
    >
    > Bug: 896013, 1039756
    > Change-Id: Ic3242178239ac8c68d02fe0a2a21a52638146580
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940707
    > Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#736197}
    
    Bug: 896013, 1039756, 1046696
    Change-Id: I35df3efb3f5e7fcc44211a9497724cb5a68d7d91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027890
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Auto-Submit: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736449}

--- components/security_interstitials/content/ssl_error_handler.cc
+++ components/security_interstitials/content/ssl_error_handler.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/content/blocked_interception_blocking_page.h"
 #include "components/security_interstitials/content/captive_portal_blocking_page.h"
 #include "components/security_interstitials/content/captive_portal_helper.h"
+#include "components/security_interstitials/content/legacy_tls_blocking_page.h"
 #include "components/security_interstitials/content/mitm_software_blocking_page.h"
 #include "components/security_interstitials/content/security_blocking_page_factory.h"
 #include "components/security_interstitials/content/security_interstitial_page.h"
@@ -388,6 +389,8 @@ class SSLErrorHandlerDelegateImpl : public SSLErrorHandler::Delegate {
   void ShowBlockedInterceptionInterstitial() override;
   void ReportNetworkConnectivity(base::OnceClosure callback) override;
   bool HasBlockedInterception() const override;
+  void ShowLegacyTLSInterstitial() override;
+  bool HasLegacyTLS() const override;
 
  private:
   // Calls the |blocking_page_ready_callback_| if it's not null, else calls
@@ -505,6 +508,13 @@ void SSLErrorHandlerDelegateImpl::ShowBlockedInterceptionInterstitial() {
           std::move(ssl_cert_reporter_), ssl_info_));
 }
 
+void SSLErrorHandlerDelegateImpl::ShowLegacyTLSInterstitial() {
+  // Show legacy TLS blocking page. The interstitial owns the blocking page.
+  OnBlockingPageReady(blocking_page_factory_->CreateLegacyTLSBlockingPage(
+      web_contents_, cert_error_, request_url_, std::move(ssl_cert_reporter_),
+      ssl_info_));
+}
+
 void SSLErrorHandlerDelegateImpl::ReportNetworkConnectivity(
     base::OnceClosure callback) {
 #if defined(OS_ANDROID)
@@ -520,6 +530,11 @@ void SSLErrorHandlerDelegateImpl::ReportNetworkConnectivity(
 bool SSLErrorHandlerDelegateImpl::HasBlockedInterception() const {
   return ssl_errors::ErrorInfo::NetErrorToErrorType(cert_error_) ==
          ssl_errors::ErrorInfo::CERT_KNOWN_INTERCEPTION_BLOCKED;
+}
+
+bool SSLErrorHandlerDelegateImpl::HasLegacyTLS() const {
+  return ssl_errors::ErrorInfo::NetErrorToErrorType(cert_error_) ==
+         ssl_errors::ErrorInfo::LEGACY_TLS;
 }
 
 void SSLErrorHandlerDelegateImpl::OnBlockingPageReady(
@@ -665,6 +680,12 @@ void SSLErrorHandler::StartHandlingError() {
 
   if (delegate_->HasBlockedInterception()) {
     return ShowBlockedInterceptionInterstitial();
+  }
+
+  // The legacy TLS interstitial is only shown if no other errors were found.
+  if (ssl_errors::ErrorInfo::NetErrorToErrorType(cert_error_) ==
+      ssl_errors::ErrorInfo::LEGACY_TLS) {
+    return ShowLegacyTLSInterstitial();
   }
 
   if (ssl_errors::ErrorInfo::NetErrorToErrorType(cert_error_) ==
@@ -856,6 +877,15 @@ void SSLErrorHandler::ShowBlockedInterceptionInterstitial() {
   // Show a blocking page. The interstitial owns the blocking page.
   RecordUMA(SHOW_BLOCKED_INTERCEPTION_INTERSTITIAL);
   delegate_->ShowBlockedInterceptionInterstitial();
+  // Once an interstitial is displayed, no need to keep the handler around.
+  // This is the equivalent of "delete this".
+  web_contents_->RemoveUserData(UserDataKey());
+}
+
+void SSLErrorHandler::ShowLegacyTLSInterstitial() {
+  // Show a blocking page. The interstitial owns the blocking page.
+  RecordUMA(SHOW_LEGACY_TLS_INTERSTITIAL);
+  delegate_->ShowLegacyTLSInterstitial();
   // Once an interstitial is displayed, no need to keep the handler around.
   // This is the equivalent of "delete this".
   web_contents_->RemoveUserData(UserDataKey());
