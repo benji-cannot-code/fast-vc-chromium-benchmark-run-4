f5afc6dc20f36396f8e1b9eb4e904c3986c71aa7
Michael Thiessen <mthiesse@chromium.org>
2020-01-29 18:13:12 +0000

    Support feature annotations for native java unittests.
    Native Java Unittests can now simply enable/disable features for
    individual tests using, for example:
    @NativeJavaTestFeatures.Enabled({"Feature1", "Feature2"})
    or
    @NativeJavaTestFeatures.Disabled("Feature3")
    
    This will generate a test method body that creates a ScopedFeatureList
    with the specified feature overrides set.
    
    Bug: 783819
    Change-Id: I6473f6b9584e3faa1c19277a6c8eb6f4cf280677
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015442
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: (gone) Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736444}

---
+++ base/android/jni_generator/TestSampleFeatureList.java
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package foo;
+
+class TestFeatureList {
+    public static final String MY_FEATURE = "MyFeature";
+    public static final String MY_FEATURE_WITH_A_REALLY_REALLY_ABSURDLY_LONG_NAME =
+            "MyFeatureWithAReallyReallyAbsurdlyLongName";
+    public static final String BAD_FEATURE = "BadFeature";
+}
