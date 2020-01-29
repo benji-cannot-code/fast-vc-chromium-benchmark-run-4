754fc1b14100806fb890ae3ca1561c89df901e18
Brandon Wylie <wylieb@chromium.org>
2020-01-29 03:04:31 +0000

    [Assistant Voice Search] Intent to assistant for voice search
    Bug: 1041576
    PRD: go/roti-prd
    Doc: go/roti-chrome-design
    Change-Id: Id5e547f922ce7939563e7a475654a55396954070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1999157
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Ender <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736183}

--- base/android/java/src/org/chromium/base/SysUtils.java
+++ base/android/java/src/org/chromium/base/SysUtils.java
@@ -232,6 +232,11 @@ public class SysUtils {
         return false;
     }
 
+    @VisibleForTesting
+    public static void setAmountOfPhysicalMemoryKBForTesting(int physicalMemoryKB) {
+        sAmountOfPhysicalMemoryKB = physicalMemoryKB;
+    }
+
     @NativeMethods
     interface Natives {
         void logPageFaultCountToTracing();
