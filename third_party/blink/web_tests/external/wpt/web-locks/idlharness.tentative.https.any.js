997c4b88abb6eda2c80f266c530ab851fb0ddfca
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-01-29 16:26:06 +0000

    Import wpt@1c5d0a817201698ce43de1e0bf34c3e612dd2c82
    Using wpt-import in Chromium 5a6ee40fd4b9a6b4bf841db315a03043dc550a25.
    With Chromium commits locally applied on WPT:
    1029b7e95a "SharedWorker: Add WPT for upgrade-insecure-requests"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Ib9bc8dd5e37ff3108b8f264cdcdaf9e827c6f5a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027596
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#736396}

--- third_party/blink/web_tests/external/wpt/web-locks/idlharness.tentative.https.any.js
+++ third_party/blink/web_tests/external/wpt/web-locks/idlharness.tentative.https.any.js
@@ -5,31 +5,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-promise_test(async t => {
-  const srcs = ['./web-locks.idl', '/interfaces/html.idl'];
-  const [weblocks, html] = await Promise.all(
-    srcs.map(i => fetch(i).then(r => r.text())));
+idl_test(
+  ['web-locks'],
+  ['html'],
+  async idl_array => {
+    idl_array.add_objects({
+      LockManager: ['navigator.locks'],
+      Lock: ['lock'],
+    });
 
-  const idl_array = new IdlArray();
-  idl_array.add_idls(weblocks);
-  idl_array.add_dependency_idls(html);
+    if (self.Window) {
+      idl_array.add_objects({ Navigator: ['navigator'] });
+    } else {
+      idl_array.add_objects({ WorkerNavigator: ['navigator'] });
+    }
 
-  try {
-    await navigator.locks.request('name', l => { self.lock = l; });
-  } catch (e) {
-    // Surfaced in idlharness.js's test_object below.
+    try {
+      await navigator.locks.request('name', l => { self.lock = l; });
+    } catch (e) {
+      // Surfaced in idlharness.js's test_object below.
+    }
   }
-
-  idl_array.add_objects({
-    LockManager: ['navigator.locks'],
-    Lock: ['lock'],
-  });
-
-  if (self.Window) {
-    idl_array.add_objects({ Navigator: ['navigator'] });
-  } else {
-    idl_array.add_objects({ WorkerNavigator: ['navigator'] });
-  }
-
-  idl_array.test();
-});
+);
