96fda575c0d01498460170ecc1a6876cf7ac8bd3
Jan Krcal <jkrcal@chromium.org>
2020-01-29 14:47:45 +0000

    Revert "[Sheriff] Disable SingleClientPreferencesSyncTest.ShouldRemoveBadDataWhenRegistering on Windows"
    This reverts commit f86c4c9a8a99305a9aa805b9dca4f8ae3a534792.
    
    Reason for revert: re-enabling the test to gather more debug logs from failures.
    
    Original change's description:
    > [Sheriff] Disable SingleClientPreferencesSyncTest.ShouldRemoveBadDataWhenRegistering on Windows
    > 
    > TBR=jkrcal@chromium.org
    > 
    > Bug: 930482
    > Change-Id: Ifbb07226d94f8b42cbdf7487d20bd8d72811d96b
    > Reviewed-on: https://chromium-review.googlesource.com/c/1462897
    > Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    > Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#630729}
    
    TBR=jkrcal@chromium.org,jdoerrie@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 930482
    Change-Id: I89eab360c7588fb628c94ec89e3876a7da4447c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2026731
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736361}

--- chrome/browser/sync/test/integration/single_client_preferences_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_preferences_sync_test.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/values.h"
-#include "build/build_config.h"
 #include "chrome/browser/sync/test/integration/preferences_helper.h"
 #include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
@@ -76,16 +75,8 @@ IN_PROC_BROWSER_TEST_F(SingleClientPreferencesSyncTest, Sanity) {
   EXPECT_TRUE(BooleanPrefMatches(prefs::kHomePageIsNewTabPage));
 }
 
-// Flaky on Windows. https://crbug.com/930482
-#if defined(OS_WIN)
-#define MAYBE_ShouldRemoveBadDataWhenRegistering \
-  DISABLED_ShouldRemoveBadDataWhenRegistering
-#else
-#define MAYBE_ShouldRemoveBadDataWhenRegistering \
-  ShouldRemoveBadDataWhenRegistering
-#endif
 IN_PROC_BROWSER_TEST_F(SingleClientPreferencesSyncTest,
-                       MAYBE_ShouldRemoveBadDataWhenRegistering) {
+                       ShouldRemoveBadDataWhenRegistering) {
   // Populate the data store with data of type boolean but register as string.
   SetPreexistingPreferencesFileContents(
       0, "{\"testing\":{\"my-test-preference\":true}}");
