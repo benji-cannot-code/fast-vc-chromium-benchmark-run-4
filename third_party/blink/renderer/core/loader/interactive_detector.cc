ba0a51c05eab67ca87d5a8b1b93a828ccd122ce3
Hongbo Song <hbsong@google.com>
2020-01-29 20:45:13 +0000

    Add input delay UKM.
    This CL is to record input delay UKM which is defined as the duration
    between the hardware timestamp and the start of event processing on the
    main thread for the meaningful input.
    
    UKM Privacy Review:
    https://docs.google.com/document/d/1VQvmb3x7uGMmt2JvKt3_4zk1SPWHPB3yJSiyec7hY20
    
    Bug: 1042004
    Change-Id: I69eb91955f116e1f90b9b72a7d3dd13b449fa4ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2008436
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Hongbo Song <hbsong@google.com>
    Cr-Commit-Position: refs/heads/master@{#736527}

--- third_party/blink/renderer/core/loader/interactive_detector.cc
+++ third_party/blink/renderer/core/loader/interactive_detector.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/loader/interactive_detector.h"
 
 #include "base/time/default_tick_clock.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_recorder.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -228,6 +230,13 @@ void InteractiveDetector::HandleForInputDelay(
     page_event_times_.first_input_timestamp = event_timestamp;
     input_delay_metrics_changed = true;
   }
+
+  // Record input delay UKM.
+  ukm::SourceId source_id = GetSupplementable()->UkmSourceID();
+  DCHECK_NE(source_id, ukm::kInvalidSourceId);
+  ukm::builders::InputEvent(source_id)
+      .SetInteractiveTiming_InputDelay(delay.InMilliseconds())
+      .Record(ukm::UkmRecorder::Get());
 
   UMA_HISTOGRAM_CUSTOM_TIMES(kHistogramInputDelay, delay,
                              base::TimeDelta::FromMilliseconds(1),
