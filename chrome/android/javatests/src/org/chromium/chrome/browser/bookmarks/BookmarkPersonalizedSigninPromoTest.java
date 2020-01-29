b2ab2d0c40a86f4c4e52bfb8c56199377f83da98
Yi Gu <yigu@chromium.org>
2020-01-29 22:05:26 +0000

    Disable BookmarkPersonalizedSigninPromoTest#testSigninButtonDefaultAccount
    Similar to crbug.com/1046653, this test is also flaky:
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVylAELEgVGbGFrZSKIAWNocm9taXVtQGNocm9tZV9wdWJsaWNfdGVzdF9hcGtAb3JnLmNocm9taXVtLmNocm9tZS5icm93c2VyLmJvb2ttYXJrcy5Cb29rbWFya1BlcnNvbmFsaXplZFNpZ25pblByb21vVGVzdCN0ZXN0U2lnbmluQnV0dG9uRGVmYXVsdEFjY291bnQM
    
    TBR=mdjones@chromium.org
    
    Bug: 1046652
    Change-Id: I8b80d4d48f68310b7d70a41a50fa452dcaf7972c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028763
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736564}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
@@ -31,6 +31,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.signin.SigninActivityLauncher;
@@ -68,6 +69,7 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/1046652")
     public void testSigninButtonDefaultAccount() {
         doNothing()
                 .when(SigninActivityLauncher.get())
