674d6125d7d1d06a34e063f60cca7bed76fdebf0
Omer Katz <omerkatz@chromium.org>
2020-01-30 16:14:34 +0000

    Replace HeapListHashSet with HeapHashMap
    This is part of an ongoing effort to simplify heap collections
    and limit the types they can support (specifically, this CL
    removes the only existing non-member usage of HeapListHashSet).
    
    Bug: 1047147
    Change-Id: I1c5de9f3a97588c22946eed03048159fd720b502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024896
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736884}

--- third_party/blink/renderer/core/dom/scripted_animation_controller.cc
+++ third_party/blink/renderer/core/dom/scripted_animation_controller.cc
@@ -39,8 +39,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-std::pair<EventTarget*, StringImpl*> EventTargetKey(const Event* event) {
-  return std::make_pair(event->target(), event->type().Impl());
+bool ScriptedAnimationController::InsertToPerFrameEventsMap(
+    const Event* event) {
+  HashSet<const StringImpl*>& set =
+      per_frame_events_.insert(event->target(), HashSet<const StringImpl*>())
+          .stored_value->value;
+  return set.insert(event->type().Impl()).is_new_entry;
+}
+
+void ScriptedAnimationController::EraseFromPerFrameEventsMap(
+    const Event* event) {
+  EventTarget* target = event->target();
+  PerFrameEventsMap::iterator it = per_frame_events_.find(target);
+  if (it != per_frame_events_.end()) {
+    HashSet<const StringImpl*>& set = it->value;
+    set.erase(event->type().Impl());
+    if (set.IsEmpty())
+      per_frame_events_.erase(target);
+  }
 }
 
 ScriptedAnimationController::ScriptedAnimationController(Document* document)
@@ -112,7 +128,7 @@ void ScriptedAnimationController::DispatchEvents(
     HeapVector<Member<Event>> remaining;
     for (auto& event : event_queue_) {
       if (event && event->InterfaceName() == event_interface_filter) {
-        per_frame_events_.erase(EventTargetKey(event.Get()));
+        EraseFromPerFrameEventsMap(event.Get());
         events.push_back(event.Release());
       } else {
         remaining.push_back(event.Release());
@@ -242,7 +258,7 @@ void ScriptedAnimationController::EnqueueEvent(Event* event) {
 }
 
 void ScriptedAnimationController::EnqueuePerFrameEvent(Event* event) {
-  if (!per_frame_events_.insert(EventTargetKey(event)).is_new_entry)
+  if (!InsertToPerFrameEventsMap(event))
     return;
   EnqueueEvent(event);
 }
