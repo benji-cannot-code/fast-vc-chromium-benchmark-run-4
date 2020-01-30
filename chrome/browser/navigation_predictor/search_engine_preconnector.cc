ad3f224e7c4f880636acb9ee22676a2a7b32aa70
Ryan Sturm <ryansturm@chromium.org>
2020-01-30 18:20:44 +0000

    Respect Autocomplete setting for DSE preconnect
    This adds a check to prevent DSE preconnect when the user has opted out
    of DSE automplete and URL uploads.
    
    Bug: 1036489
    Change-Id: I015f4111377657193e6db99ca7bf2f809629270f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028296
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Auto-Submit: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736947}

--- chrome/browser/navigation_predictor/search_engine_preconnector.cc
+++ chrome/browser/navigation_predictor/search_engine_preconnector.cc
@@ -12,7 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/loading_predictor_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/common/pref_names.h"
 #include "components/google/core/common/google_util.h"
+#include "components/prefs/pref_service.h"
 #include "components/search_engines/template_url_service.h"
 #include "content/public/browser/browser_context.h"
 #include "net/base/features.h"
@@ -60,6 +62,12 @@ void SearchEnginePreconnector::PreconnectDSE() {
   DCHECK(!timer_.IsRunning());
 
   if (!base::FeatureList::IsEnabled(features::kPreconnectToSearch))
+    return;
+
+  // Don't preconnect unless the user allows search suggestions.
+  if (!Profile::FromBrowserContext(browser_context_)
+           ->GetPrefs()
+           ->GetBoolean(prefs::kSearchSuggestEnabled))
     return;
 
   GURL preconnect_url = GetDefaultSearchEngineOriginURL();
