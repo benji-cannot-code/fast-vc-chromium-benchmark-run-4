1ee6b175b19e142b87d3cc78393f377d3b4f60f7
Darin Fisher <darin@chromium.org>
2020-01-30 03:11:40 +0000

    Remove unused code that forwards prerender aliases to renderers.
    The only dependency is prerender_dispatcher_unittest.cc, and the only
    purpose of that file is to test that aliases are updated properly. It
    can all just be deleted.
    
    Change-Id: I6114d85cc53333d5712d0f4baceb18ed143a0055
    BUG: 755921
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029271
    Commit-Queue: Darin Fisher <darin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736697}

--- chrome/renderer/prerender/prerender_dispatcher.h
+++ chrome/renderer/prerender/prerender_dispatcher.h
@@ -21,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/web_prerender.h"
 #include "third_party/blink/public/platform/web_prerendering_support.h"
 
-class GURL;
-
 namespace prerender {
 
 // There is one PrerenderDispatcher per render process. It keeps track of which
@@ -36,8 +34,6 @@ class PrerenderDispatcher : public content::RenderThreadObserver,
   PrerenderDispatcher();
   ~PrerenderDispatcher() override;
 
-  bool IsPrerenderURL(const GURL& url) const;
-
   void IncrementPrefetchCount();
   void DecrementPrefetchCount();
 
@@ -48,8 +44,6 @@ class PrerenderDispatcher : public content::RenderThreadObserver,
   void PrerenderStart(int prerender_id) override;
   void PrerenderStopLoading(int prerender_id) override;
   void PrerenderDomContentLoaded(int prerender_id) override;
-  void PrerenderAddAlias(const GURL& alias) override;
-  void PrerenderRemoveAliases(const std::vector<GURL>& aliases) override;
   void PrerenderStop(int prerender_id) override;
 
   void OnPrerenderDispatcherRequest(
@@ -70,10 +64,6 @@ class PrerenderDispatcher : public content::RenderThreadObserver,
 
   // From WebKit, prerender elements launched by renderers in our process.
   std::map<int, blink::WebPrerender> prerenders_;
-
-  // From the browser process, which prerenders are running, indexed by URL.
-  // Updated by the browser processes as aliases are discovered.
-  std::multiset<GURL> running_prerender_urls_;
 
   int prefetch_count_ = 0;
   bool prefetch_finished_ = false;
