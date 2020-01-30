dcb205df4721d1d60c39d7e61c18b86988878ebf
Ella Ge <eirage@chromium.org>
2020-01-30 17:01:25 +0000

    Add SourceIdType::WEBAPK_ID for recording webapk ukm
    Whe recording WebAPK UKM, we use manifest url as the source_url. But UKM
    has a mechanism that filter out the url that are not whitelisted.
    Manifest url does not matches the navigation url or other whitelisted
    url, so the record is drop with DropDataReason::NOT_MATCHED.
    
    This CL adds another SourceIdType WEBAPK_ID for the WebAPK manifests, so
    that these url are whitelisted.
    
    Bug: 1044706
    Change-Id: I988473f52afcf5b25cc5fafd43934e866f628f76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018086
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736903}

--- chrome/browser/android/webapk/webapk_ukm_recorder.cc
+++ chrome/browser/android/webapk/webapk_ukm_recorder.cc
@@ -35,9 +35,8 @@ void WebApkUkmRecorder::RecordInstall(const GURL& manifest_url,
   if (!manifest_url.is_valid())
     return;
 
-  ukm::SourceId source_id = ukm::UkmRecorder::GetNewSourceID();
-  ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
-  ukm_recorder->UpdateSourceURL(source_id, manifest_url);
+  ukm::SourceId source_id =
+      ukm::UkmRecorder::GetSourceIdForWebApkManifestUrl(manifest_url);
 
   // All installs through this method are browser-installs (ie, they should all
   // use the "browser" distributor).
@@ -45,7 +44,7 @@ void WebApkUkmRecorder::RecordInstall(const GURL& manifest_url,
       .SetDistributor(static_cast<int64_t>(WebApkDistributor::BROWSER))
       .SetAppVersion(version_code)
       .SetInstall(1)
-      .Record(ukm_recorder);
+      .Record(ukm::UkmRecorder::Get());
 }
 
 // static
@@ -56,14 +55,13 @@ void WebApkUkmRecorder::RecordSessionDuration(const GURL& manifest_url,
   if (!manifest_url.is_valid())
     return;
 
-  ukm::SourceId source_id = ukm::UkmRecorder::GetNewSourceID();
-  ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
-  ukm_recorder->UpdateSourceURL(source_id, manifest_url);
+  ukm::SourceId source_id =
+      ukm::UkmRecorder::GetSourceIdForWebApkManifestUrl(manifest_url);
   ukm::builders::WebAPK_SessionEnd(source_id)
       .SetDistributor(distributor)
       .SetAppVersion(version_code)
       .SetSessionDuration(ukm::GetExponentialBucketMinForUserTiming(duration))
-      .Record(ukm_recorder);
+      .Record(ukm::UkmRecorder::Get());
 }
 
 // static
@@ -74,15 +72,14 @@ void WebApkUkmRecorder::RecordVisit(const GURL& manifest_url,
   if (!manifest_url.is_valid())
     return;
 
-  ukm::SourceId source_id = ukm::UkmRecorder::GetNewSourceID();
-  ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
-  ukm_recorder->UpdateSourceURL(source_id, manifest_url);
+  ukm::SourceId source_id =
+      ukm::UkmRecorder::GetSourceIdForWebApkManifestUrl(manifest_url);
   ukm::builders::WebAPK_Visit(source_id)
       .SetDistributor(distributor)
       .SetAppVersion(version_code)
       .SetLaunchSource(source)
       .SetLaunch(1)
-      .Record(ukm_recorder);
+      .Record(ukm::UkmRecorder::Get());
 }
 
 // static
@@ -93,9 +90,8 @@ void WebApkUkmRecorder::RecordUninstall(const GURL& manifest_url,
                                         int64_t installed_duration_ms) {
   // UKM metric |launch_count| parameter is enum. '2' indicates >= 2 launches.
   launch_count = base::ClampToRange<int64_t>(launch_count, 0, 2);
-  ukm::SourceId source_id = ukm::UkmRecorder::GetNewSourceID();
-  ukm::UkmRecorder* ukm_recorder = ukm::UkmRecorder::Get();
-  ukm_recorder->UpdateSourceURL(source_id, manifest_url);
+  ukm::SourceId source_id =
+      ukm::UkmRecorder::GetSourceIdForWebApkManifestUrl(manifest_url);
   ukm::builders::WebAPK_Uninstall(source_id)
       .SetDistributor(distributor)
       .SetAppVersion(version_code)
@@ -103,7 +99,7 @@ void WebApkUkmRecorder::RecordUninstall(const GURL& manifest_url,
       .SetInstalledDuration(
           ukm::GetExponentialBucketMinForUserTiming(installed_duration_ms))
       .SetUninstall(1)
-      .Record(ukm_recorder);
+      .Record(ukm::UkmRecorder::Get());
 }
 
 // Called by the Java counterpart to record the Session Duration UKM metric.
--- components/ukm/ukm_recorder_impl_unittest.cc
+++ components/ukm/ukm_recorder_impl_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/metrics/ukm_source_id.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/task_environment.h"
 #include "components/ukm/test_ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_entry_builder.h"
@@ -105,6 +106,24 @@ TEST(UkmRecorderImplTest, PurgeExtensionRecordings) {
   recorder.UpdateSourceURL(id4, GURL("chrome-extension://abc/index.html"));
   EXPECT_FALSE(recorder.extensions_enabled_);
   EXPECT_EQ(2U, recorder.sources().size());
+}
+
+TEST(UkmRecorderImplTest, WebApkSourceUrl) {
+  base::test::TaskEnvironment env;
+  ukm::TestAutoSetUkmRecorder test_ukm_recorder;
+
+  GURL url("https://example_url.com/manifest.json");
+  SourceId id = UkmRecorderImpl::GetSourceIdForWebApkManifestUrl(url);
+
+  ASSERT_NE(kInvalidSourceId, id);
+
+  const auto& sources = test_ukm_recorder.GetSources();
+  ASSERT_EQ(1ul, sources.size());
+  auto it = sources.find(id);
+  ASSERT_NE(sources.end(), it);
+  EXPECT_EQ(url, it->second->url());
+  EXPECT_EQ(1u, it->second->urls().size());
+  EXPECT_EQ(SourceIdType::WEBAPK_ID, GetSourceIdType(id));
 }
 
 }  // namespace ukm
