31774ec691fc04aca87aa800dfebd32742a2ac84
Richard Knoll <knollr@chromium.org>
2020-01-30 20:06:26 +0000

    Hook up Sharing WebRTC send path to flags
    This allows devices to send Sharing messages via WebRTC if they are both
    in the experiment. This allows us to control the rollout more controlled
    and prevents devices not in the experiment to receive WebRTC messages.
    
    Bug: 1021131
    Change-Id: I25f137ed06075c8de7b1e9a5fbc48eb3b89219a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027794
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Himanshu Jaju <himanshujaju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737008}

--- chrome/browser/sharing/webrtc/webrtc_flags.cc
+++ chrome/browser/sharing/webrtc/webrtc_flags.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sharing/webrtc/webrtc_flags.h"
 
-// TODO(crbug.com/1021131) - Enable by default for M80 experimentation.
 const base::Feature kSharingPeerConnectionReceiver{
     "SharingPeerConnectionReceiver", base::FEATURE_DISABLED_BY_DEFAULT};
 
