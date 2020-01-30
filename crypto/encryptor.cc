93de55f780718943c37e6721f6034b7736ebdccf
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-01-30 07:04:12 +0000

    [base] Remove BasicStringPiece::CopyToString
    This change removes CopyToString from base::BasicStringPiece's API.
    Similarly to the recently removed clear() and set() these APIs are
    not present on std::basic_string_view, and thus would block us from
    adopting string_view once we support C++17.
    
    Existing usages of `piece.CopyToString(&str)` are replaced by either
    `str = std::string(piece)` or `str.assign(piece.data(), piece.size())`
    depending on whether move assigning a temporary would result in an
    additional malloc or not.
    
    TBR=dcheng
    
    Bug: 752720
    Change-Id: Ib70a183671d61bbbbf08c2c4a1c7eef573c3c446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027408
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736749}

--- crypto/encryptor.cc
+++ crypto/encryptor.cc
@@ -102,7 +102,7 @@ bool Encryptor::Init(const SymmetricKey* key, Mode mode, base::StringPiece iv) {
 
   key_ = key;
   mode_ = mode;
-  iv.CopyToString(&iv_);
+  iv_ = std::string(iv);
   return true;
 }
 
