95294474076d6ed040c5b13ebad7e229b6857cef
Andres Calderon Jaramillo <andrescj@chromium.org>
2020-01-30 22:31:10 +0000

    viz: Handle empty swaps in GLOutputSurfaceBufferQueue.
    This CL modifies GLOutputSurfaceBufferQueue to handle the case where
    GetOverlayTextureId() and SwapBuffers() are called without first calling
    BindFramebuffer(): this indicates that the client didn't draw but still
    wants to swap. Prior to CL:1977969, on such case, GetOverlayTextureId()
    would return the texture that was obtained in the last call to
    BindFramebuffer().
    
    This CL is intended to do the same: if GetOverlayTextureId() is called,
    and we know that the client is not currently drawing, we still return
    the last texture that was attached to the framebuffer.
    
    Additionally, this CL ensures that we end access to a shared image prior
    to emitting the command buffer command to swap the buffers to avoid
    synchronization issues.
    
    Android Q no longer produces flickering. Inspecting a page using
    added a unit test for empty swaps.
    
    Bug: 1046055
    Test: Visiting https://www.funwithmama.com/bento-boxes-for-kids/ on
    chrome: //inspect on Android Q no longer causes a DCHECK crash. Also
    Change-Id: I86dc72f920a97b598028023f203aa5be4da2a840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024177
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737067}

--- components/viz/service/display_embedder/gl_output_surface_buffer_queue.cc
+++ components/viz/service/display_embedder/gl_output_surface_buffer_queue.cc
@@ -61,6 +61,7 @@ GLOutputSurfaceBufferQueue::~GLOutputSurfaceBufferQueue() {
     gl->DeleteTextures(1u, &buffer_texture.second);
   buffer_queue_textures_.clear();
   current_texture_ = 0u;
+  last_bound_texture_ = 0u;
 
   // Freeing the BufferQueue here ensures that *this is fully alive in case the
   // BufferQueue needs the SyncTokenProvider functionality.
@@ -95,6 +96,7 @@ void GLOutputSurfaceBufferQueue::BindFramebuffer() {
       current_texture_, GL_SHARED_IMAGE_ACCESS_MODE_READWRITE_CHROMIUM);
   gl->FramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
                            texture_target_, current_texture_, 0);
+  last_bound_texture_ = current_texture_;
 
 #if DCHECK_IS_ON() && defined(OS_CHROMEOS)
   const GLenum result = gl->CheckFramebufferStatus(GL_FRAMEBUFFER);
@@ -150,6 +152,7 @@ void GLOutputSurfaceBufferQueue::Reshape(const gfx::Size& size,
         gl->DeleteTextures(1u, &buffer_texture.second);
       buffer_queue_textures_.clear();
       current_texture_ = 0u;
+      last_bound_texture_ = 0u;
     }
   }
 }
@@ -164,14 +167,19 @@ void GLOutputSurfaceBufferQueue::SwapBuffers(OutputSurfaceFrame frame) {
 
   gfx::Rect damage_rect =
       frame.sub_buffer_rect ? *frame.sub_buffer_rect : gfx::Rect(swap_size_);
-  buffer_queue_->SwapBuffers(damage_rect);
-  GLOutputSurface::SwapBuffers(std::move(frame));
+
+  // If the client is currently drawing, we first end access to the
+  // corresponding shared image. Then, we can swap the buffers. That way, we
+  // know that whatever GL commands GLOutputSurface::SwapBuffers() emits can
+  // access the shared image.
   auto* gl = context_provider_->ContextGL();
-  gl->BindFramebuffer(GL_FRAMEBUFFER, 0u);
   if (current_texture_) {
     gl->EndSharedImageAccessDirectCHROMIUM(current_texture_);
+    gl->BindFramebuffer(GL_FRAMEBUFFER, 0u);
     current_texture_ = 0u;
   }
+  buffer_queue_->SwapBuffers(damage_rect);
+  GLOutputSurface::SwapBuffers(std::move(frame));
 }
 
 gfx::Rect GLOutputSurfaceBufferQueue::GetCurrentFramebufferDamage() const {
@@ -188,8 +196,8 @@ bool GLOutputSurfaceBufferQueue::IsDisplayedAsOverlayPlane() const {
 }
 
 unsigned GLOutputSurfaceBufferQueue::GetOverlayTextureId() const {
-  DCHECK(current_texture_);
-  return current_texture_;
+  DCHECK(last_bound_texture_);
+  return last_bound_texture_;
 }
 
 gpu::Mailbox GLOutputSurfaceBufferQueue::GetOverlayMailbox() const {
