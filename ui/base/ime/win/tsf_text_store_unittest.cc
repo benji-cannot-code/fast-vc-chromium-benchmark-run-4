2efe4b65943e87251ff3d356eb41c41fae5455a9
Etienne Bergeron <etienneb@chromium.org>
2020-01-30 03:34:21 +0000

    Fix off-by one with Range::Intersects(...)
    The Intersects function had an inconsistent behavior.
    This CL is fixing the issue and adding tests for these
    cases.
    
    Without the fix:
      Range(0,0) contains Range(0,0)  but they do not Intersects.
    
    Also, for these two ranges:
      r1 = gfx::Range(0, 1);
      r2 = gfx::Range(1, 2);
    
      r1 does not intersects r2   but   r2 does intersect 1.
    
    
    R=pkasting@chromium.org
    
    Bug: 1025561
    Change-Id: Id68da5930d7bfc69c8c97738735acbb22369d2be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1983563
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736701}

--- ui/base/ime/win/tsf_text_store_unittest.cc
+++ ui/base/ime/win/tsf_text_store_unittest.cc
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <OleCtl.h>
 #include <wrl/client.h>
 
-#if defined(OS_WIN)
 #include <vector>
-#endif
 
 #include "base/memory/ref_counted.h"
 #include "base/stl_util.h"
@@ -1333,7 +1331,7 @@ class GetTextExtTestCallback : public TSFTextStoreTestCallback {
     SetInternalState(L"", 0, 0, 0);
     GetTextExtTest(view_cookie, 0, 0, 1, 2, 4, 6);
 
-    // Last character is not availabe due to timing issue of async API.
+    // Last character is not available due to timing issue of async API.
     // In this case, we will get first character bounds instead of whole text
     // bounds.
     SetInternalState(L"abc", 0, 0, 3);
@@ -2896,7 +2894,7 @@ TEST_F(TSFTextStoreTest, RegressionTest3) {
 
 // Due to crbug.com/978678, we should not call TextInputClient::InsertText if
 // provided text is empty. In fact, we should call TextInputClient::InsertText
-// with current composition text to commit compositon without losing text.
+// with current composition text to commit composition without losing text.
 class RegressionTest4Callback : public TSFTextStoreTestCallback {
  public:
   explicit RegressionTest4Callback(TSFTextStore* text_store)
@@ -2992,7 +2990,7 @@ TEST_F(TSFTextStoreTest, RegressionTest4) {
 // regression tests for crbug.com/1006067.
 // We should call |TextInputClient::SetCompositionText()| if ImeTextSpans are
 // changed from previous edit session during same composition even though
-// composition string and composition selection remain unchange.
+// composition string and composition selection remain unchanged.
 class RegressionTest5Callback : public TSFTextStoreTestCallback {
  public:
   explicit RegressionTest5Callback(TSFTextStore* text_store)
@@ -3041,7 +3039,7 @@ class RegressionTest5Callback : public TSFTextStoreTestCallback {
   }
 
   // Only change underline thickness in IME spans. Other states (composition
-  // string, selection) remain unchange.
+  // string, selection) remain unchanged.
   HRESULT LockGranted2(DWORD flags) {
     GetTextTest(0, -1, L"aa", 2);
 
--- ui/gfx/range/range.cc
+++ ui/gfx/range/range.cc
@@ -14,16 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-Range Range::Intersect(const Range& range) const {
-  uint32_t min = std::max(GetMin(), range.GetMin());
-  uint32_t max = std::min(GetMax(), range.GetMax());
-
-  if (min >= max)  // No intersection.
-    return InvalidRange();
-
-  return Range(min, max);
-}
-
 std::string Range::ToString() const {
   return base::StringPrintf("{%" PRIu32 ",%" PRIu32 "}", start(), end());
 }
