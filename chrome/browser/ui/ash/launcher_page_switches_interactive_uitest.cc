c2c685ac736e3bc573cdd260de708bfda8989aea
Toni Barzic <tbarzic@chromium.org>
2020-01-30 23:27:19 +0000

    Use WindowState::Minimize in HomeScreenController::GoHome
    The goal is to make the animation used by the home button consistent
    with the minimization animation used by the back button (which uses
    standard minimization).
    
    BUG=1043265
    
    Change-Id: I6e1b8b447ca86f50a22ec6b981558717d5a7e7d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023518
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Mitsuru Oshima (slow in TOK) <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737104}

--- chrome/browser/ui/ash/launcher_page_switches_interactive_uitest.cc
+++ chrome/browser/ui/ash/launcher_page_switches_interactive_uitest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/test/ui_controls.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
+#include "ui/wm/core/wm_core_switches.h"
 
 namespace {
 
@@ -60,6 +61,13 @@ class LauncherPageSwitchesTest : public UIPerformanceTest,
                             base::TimeDelta::FromSeconds(5));
       run_loop.Run();
     }
+
+    // In tablet mode, the test will wait for the browser window to finish
+    // animating (and for the home screen to become visible) to know when to
+    // continue, so make sure the window has animations.
+    auto* cmd = base::CommandLine::ForCurrentProcess();
+    if (cmd->HasSwitch(wm::switches::kWindowAnimationsDisabled))
+      cmd->RemoveSwitch(wm::switches::kWindowAnimationsDisabled);
 
     ash::ShellTestApi shell_test_api;
 
