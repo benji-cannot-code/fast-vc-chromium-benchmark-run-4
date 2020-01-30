9c25fa6363f09a2a15ba56651ac343c3017b84af
Evan Stade <estade@chromium.org>
2020-01-30 17:37:14 +0000

    Split embedder-specific behavior off from JavaScriptDialogTabHelper
    This will allow us to componentize JavaScriptDialogTabHelper and reuse
    it for WebLayer.
    
    Bug: 1045499,1025256
    Change-Id: I07d6b2123a00898a8aa03400b4b60c203b694252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023397
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736922}

--- chrome/browser/ui/android/javascript_dialog_android.h
+++ chrome/browser/ui/android/javascript_dialog_android.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/javascript_dialogs/javascript_dialog.h"
 #include "content/public/browser/javascript_dialog_manager.h"
 
@@ -23,21 +24,6 @@ using base::android::ScopedJavaGlobalRef;
 class JavaScriptDialogAndroid : public JavaScriptDialog {
  public:
   ~JavaScriptDialogAndroid() override;
-
-  // Note on the two callbacks: |dialog_callback_on_button_clicked| is for the
-  // case where user responds to the dialog. |dialog_callback_on_cancelled| is
-  // for the case where user cancels the dialog without interacting with the
-  // dialog (e.g. clicks the navigate back button on Android).
-  static base::WeakPtr<JavaScriptDialogAndroid> Create(
-      content::WebContents* parent_web_contents,
-      content::WebContents* alerting_web_contents,
-      const base::string16& title,
-      content::JavaScriptDialogType dialog_type,
-      const base::string16& message_text,
-      const base::string16& default_prompt_text,
-      content::JavaScriptDialogManager::DialogClosedCallback
-          callback_on_button_clicked,
-      base::OnceClosure callback_on_cancelled);
 
   // JavaScriptDialog:
   void CloseDialogWithoutCallback() override;
@@ -52,6 +38,7 @@ class JavaScriptDialogAndroid : public JavaScriptDialog {
 
  private:
   friend class JavaScriptDialog;
+  friend class JavaScriptDialogTabHelperDelegateAndroid;
 
   JavaScriptDialogAndroid(content::WebContents* parent_web_contents,
                           content::WebContents* alerting_web_contents,
--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -116,12 +116,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/ui/android/context_menu_helper.h"
 #include "chrome/browser/ui/android/view_android_helper.h"
+#include "chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper_delegate_android.h"
 #else
 #include "chrome/browser/banners/app_banner_manager_desktop.h"
 #include "chrome/browser/safe_browsing/safe_browsing_tab_observer.h"
 #include "chrome/browser/tab_contents/form_interaction_tab_helper.h"
 #include "chrome/browser/ui/bookmarks/bookmark_tab_helper.h"
 #include "chrome/browser/ui/intent_picker_tab_helper.h"
+#include "chrome/browser/ui/javascript_dialogs/javascript_dialog_tab_helper_delegate_desktop.h"
 #include "chrome/browser/ui/sad_tab_helper.h"
 #include "chrome/browser/ui/search/search_tab_helper.h"
 #include "chrome/browser/ui/sync/browser_synced_tab_delegate.h"
@@ -306,7 +308,9 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
     banners::AppBannerManagerAndroid::CreateForWebContents(web_contents);
   }
   ContextMenuHelper::CreateForWebContents(web_contents);
-  JavaScriptDialogTabHelper::CreateForWebContents(web_contents);
+  JavaScriptDialogTabHelper::CreateForWebContents(
+      web_contents,
+      std::make_unique<JavaScriptDialogTabHelperDelegateAndroid>(web_contents));
   if (OomInterventionTabHelper::IsEnabled()) {
     OomInterventionTabHelper::CreateForWebContents(web_contents);
   }
@@ -323,7 +327,9 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   FormInteractionTabHelper::CreateForWebContents(web_contents);
   FramebustBlockTabHelper::CreateForWebContents(web_contents);
   IntentPickerTabHelper::CreateForWebContents(web_contents);
-  JavaScriptDialogTabHelper::CreateForWebContents(web_contents);
+  JavaScriptDialogTabHelper::CreateForWebContents(
+      web_contents,
+      std::make_unique<JavaScriptDialogTabHelperDelegateDesktop>(web_contents));
   ManagePasswordsUIController::CreateForWebContents(web_contents);
   pdf::PDFWebContentsHelper::CreateForWebContentsWithClient(
       web_contents, std::make_unique<ChromePDFWebContentsHelperClient>());
