7eb95c0481f2dc44db99aa4a50b93d1b94be0b80
Josh Simmons <jds@chromium.org>
2020-01-30 18:50:16 +0000

    Updating PerformanceHintsObserver to use the new async OptimizationGuideDecider API.
    Bug: 1030813
    Change-Id: I2d0e73e22ec52f1c09e218e50fe11cd737525d48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029228
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Josh Simmons <jds@google.com>
    Cr-Commit-Position: refs/heads/master@{#736959}

--- chrome/browser/performance_hints/performance_hints_observer.h
+++ chrome/browser/performance_hints/performance_hints_observer.h
@@ -11,7 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
+#include "base/sequence_checker.h"
+#include "components/optimization_guide/optimization_guide_decider.h"
 #include "components/optimization_guide/proto/hints.pb.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -22,7 +25,6 @@ class WebContents;
 }  // namespace content
 
 namespace optimization_guide {
-class OptimizationGuideDecider;
 class URLPatternWithWildcards;
 namespace proto {
 class PerformanceHint;
@@ -45,6 +47,13 @@ class PerformanceHintsObserver
   PerformanceHintsObserver(const PerformanceHintsObserver&) = delete;
   PerformanceHintsObserver& operator=(const PerformanceHintsObserver&) = delete;
 
+  // This callback populates |hints_| with performance information for links on
+  // the current page and is called by |optimization_guide_decider_| when a
+  // definite decision has been reached.
+  void ProcessPerformanceHint(
+      optimization_guide::OptimizationGuideDecision decision,
+      const optimization_guide::OptimizationMetadata& optimization_metadata);
+
   // Returns a PerformanceHint for a link to |url|, if one exists.
   base::Optional<optimization_guide::proto::PerformanceHint> HintForURL(
       const GURL& url) const;
@@ -66,6 +75,10 @@ class PerformanceHintsObserver
   std::vector<std::pair<optimization_guide::URLPatternWithWildcards,
                         optimization_guide::proto::PerformanceHint>>
       hints_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  base::WeakPtrFactory<PerformanceHintsObserver> weak_factory_{this};
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
