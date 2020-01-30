bb0e98ced5524a12c965fb8df7791a5ee5b447d2
Julian Watson <juwa@google.com>
2020-01-30 07:49:20 +0000

    crostini: fix LinuxPackageInfo constructors
    The explicit destructor is pessimising performance by preventing the move constructor from being generated.
    
    Change-Id: Ia595d72a631decd522e22189fe42ec235fd7e381
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029402
    Commit-Queue: Julian Watson <juwa@google.com>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Julian Watson <juwa@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736764}

--- chrome/browser/chromeos/crostini/crostini_simple_types.h
+++ chrome/browser/chromeos/crostini/crostini_simple_types.h
@@ -162,7 +162,10 @@ struct Icon {
 
 struct LinuxPackageInfo {
   LinuxPackageInfo();
+  LinuxPackageInfo(LinuxPackageInfo&&);
   LinuxPackageInfo(const LinuxPackageInfo&);
+  LinuxPackageInfo& operator=(LinuxPackageInfo&&);
+  LinuxPackageInfo& operator=(const LinuxPackageInfo&);
   ~LinuxPackageInfo();
 
   bool success;
