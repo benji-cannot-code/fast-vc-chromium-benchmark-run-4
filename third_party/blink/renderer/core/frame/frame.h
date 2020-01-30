1ec2afed575095dc6aa60baaa69d4ea8ec0a99b1
Charlie Hu <chenleihu@google.com>
2020-01-30 16:18:28 +0000

    Require-Document-Policy header no longer affect current document
    Previously if a http response has both Document-Policy and
    Require-Document-Policy header and they are incompatible, the document
    will be blocked, which is not the correct behavior. This CL corrects
    the behavior of document policy under such condition.
    
    Bug: 993790
    Change-Id: I5fc6b5c213fa7d56d473a8ea7e10235dd418ba42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015443
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736886}

--- third_party/blink/renderer/core/frame/frame.h
+++ third_party/blink/renderer/core/frame/frame.h
@@ -32,7 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/optional.h"
 #include "base/unguessable_token.h"
-#include "third_party/blink/public/common/frame/frame_policy.h"
+#include "third_party/blink/public/common/feature_policy/document_policy.h"
 #include "third_party/blink/public/common/frame/user_activation_state.h"
 #include "third_party/blink/public/common/frame/user_activation_update_source.h"
 #include "third_party/blink/public/mojom/ad_tagging/ad_frame.mojom-blink.h"
@@ -245,10 +245,13 @@ class CORE_EXPORT Frame : public GarbageCollected<Frame> {
     opener_feature_state_ = state;
   }
 
-  const FramePolicy& GetFramePolicy() const { return frame_policy_; }
+  const DocumentPolicy::FeatureState& GetRequiredDocumentPolicy() const {
+    return required_document_policy_;
+  }
 
-  void SetFramePolicy(const FramePolicy& frame_policy) {
-    frame_policy_ = frame_policy;
+  void SetRequiredDocumentPolicy(
+      const DocumentPolicy::FeatureState& required_document_policy) {
+    required_document_policy_ = required_document_policy;
   }
 
   WindowAgentFactory& window_agent_factory() const {
@@ -329,10 +332,12 @@ class CORE_EXPORT Frame : public GarbageCollected<Frame> {
   // frames.
   FeaturePolicy::FeatureState opener_feature_state_;
 
-  // Frame policy of current frame. This can be different to
-  // Owner()->GetFramePolicy(), as the document hosted in the frame can also
-  // further specify frame policy.
-  FramePolicy frame_policy_;
+  // The required document policy for any subframes of this frame.
+  // Note: current frame's document policy might not conform to
+  // |required_document_policy_| here, as the Require-Document-Policy HTTP
+  // header can specify required document policy which only takes effect for
+  // subtree frames.
+  DocumentPolicy::FeatureState required_document_policy_;
 
   Member<WindowAgentFactory> window_agent_factory_;
 
