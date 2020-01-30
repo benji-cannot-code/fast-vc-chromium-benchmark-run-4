3275be62fe10b8811588f364ab2130f8d4cd208a
Zach Trudo <zatrudo@google.com>
2020-01-30 21:39:02 +0000

    Remove Gaia ID from AndroidAppInstallEvent
    Gaia ID was incorrectly reported previously:
    1. It was stored as an int when the obfuscated Gaia ID cannot be
       converted to an int.
    2. It was then converted to an int64 in the proto.
    
    The proto symbol for Gaia ID will be deprecated and it's identifier will
    be reserved for future use.
    
    Removing the Gaia ID prior to deprecation.
    
    Bug: 1045967
    Change-Id: I4c3477005788046408e3ae81f8ee37cfc1df17d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028211
    Commit-Queue: Zach Trudo <zatrudo@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Auto-Submit: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#737028}

--- chrome/browser/chromeos/policy/app_install_event_log_util.cc
+++ chrome/browser/chromeos/policy/app_install_event_log_util.cc
@@ -12,11 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
-#include "chrome/browser/chromeos/profiles/profile_helper.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chromeos/system/statistics_provider.h"
-#include "components/account_id/account_id.h"
-#include "components/user_manager/user.h"
 
 namespace em = enterprise_management;
 
@@ -34,7 +30,6 @@ constexpr char kCloudDpsResponse[] = "clouddpsResponse";
 constexpr char kOnline[] = "online";
 constexpr char kSessionStateChangeType[] = "sessionStateChangeType";
 constexpr char kSerialNumber[] = "serialNumber";
-constexpr char kGaiaId[] = "gaiaId";
 constexpr char kAndroidAppInstallEvent[] = "androidAppInstallEvent";
 constexpr char kTime[] = "time";
 constexpr char kEventId[] = "eventId";
@@ -69,18 +64,6 @@ bool GetHash(const base::Value& event,
 
 }  // namespace
 
-bool GetGaiaId(Profile* profile, int* gaia_id) {
-  if (!profile)
-    return false;
-  const user_manager::User* user =
-      chromeos::ProfileHelper::Get()->GetUserByProfile(profile);
-  if (!user)
-    return false;
-  if (!base::StringToInt(user->GetAccountId().GetGaiaId(), gaia_id))
-    return false;
-  return true;
-}
-
 std::string GetSerialNumber() {
   return chromeos::system::StatisticsProvider::GetInstance()
       ->GetEnterpriseMachineID();
@@ -88,8 +71,7 @@ std::string GetSerialNumber() {
 
 base::Value ConvertProtoToValue(
     const em::AppInstallReportRequest* app_install_report_request,
-    const base::Value& context,
-    Profile* profile) {
+    const base::Value& context) {
   DCHECK(app_install_report_request);
 
   base::Value event_list(base::Value::Type::LIST);
@@ -102,7 +84,7 @@ base::Value ConvertProtoToValue(
       base::Value wrapper;
       wrapper = ConvertEventToValue(
           app_install_report.has_package() ? app_install_report.package() : "",
-          app_install_report_log_event, context, profile);
+          app_install_report_log_event, context);
       auto* id = wrapper.FindStringKey(kEventId);
       if (id) {
         if (seen_ids.find(*id) != seen_ids.end()) {
@@ -122,8 +104,7 @@ base::Value ConvertProtoToValue(
 base::Value ConvertEventToValue(
     const std::string& package,
     const em::AppInstallReportLogEvent& app_install_report_log_event,
-    const base::Value& context,
-    Profile* profile) {
+    const base::Value& context) {
   base::Value event(base::Value::Type::DICTIONARY);
 
   if (!package.empty())
@@ -168,10 +149,6 @@ base::Value ConvertEventToValue(
   }
 
   event.SetStringKey(kSerialNumber, GetSerialNumber());
-
-  int gaia_id;
-  if (GetGaiaId(profile, &gaia_id))
-    event.SetIntKey(kGaiaId, gaia_id);
 
   base::Value wrapper(base::Value::Type::DICTIONARY);
   wrapper.SetKey(kAndroidAppInstallEvent, std::move(event));
