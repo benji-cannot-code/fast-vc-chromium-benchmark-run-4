ecdda96049548ce1a11ca3becbd6769239c3d6a3
Tanya Gupta <tgupta@chromium.org>
2020-01-30 18:33:28 +0000

    [QrCode] Created a new layout to display permissions error. Added ability to prompt user for camera permissions.
    Change-Id: I8d831836a1c90aefc1d52b11e6886e1370c6db7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015401
    Commit-Queue: Tanya Gupta <tgupta@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736954}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/scan_tab/QrCodeScanViewProperties.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/scan_tab/QrCodeScanViewProperties.java
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 package org.chromium.chrome.browser.share.qrcode.scan_tab;
 
 import org.chromium.ui.modelutil.PropertyKey;
@@ -11,8 +12,12 @@ class QrCodeScanViewProperties {
     public static final WritableBooleanPropertyKey HAS_CAMERA_PERMISSION =
             new WritableBooleanPropertyKey();
 
+    public static final WritableBooleanPropertyKey CAN_PROMPT_FOR_PERMISSION =
+            new WritableBooleanPropertyKey();
+
     public static final WritableBooleanPropertyKey IS_ON_FOREGROUND =
             new WritableBooleanPropertyKey();
 
-    public static final PropertyKey[] ALL_KEYS = {HAS_CAMERA_PERMISSION, IS_ON_FOREGROUND};
+    public static final PropertyKey[] ALL_KEYS = {
+            HAS_CAMERA_PERMISSION, CAN_PROMPT_FOR_PERMISSION, IS_ON_FOREGROUND};
 }
