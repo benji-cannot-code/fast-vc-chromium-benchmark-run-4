81301961538d5699215b5988977181822c5b68d0
Jarryd <jarrydg@chromium.org>
2020-01-30 19:37:59 +0000

    Quota: Fix ifndef header guard.
    Change-Id: Id268acba620b57196e06cc1160d95a7d2b036a17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028861
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736993}

--- storage/browser/test/mock_quota_manager.h
+++ storage/browser/test/mock_quota_manager.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_QUOTA_MOCK_QUOTA_MANAGER_H_
-#define CONTENT_BROWSER_QUOTA_MOCK_QUOTA_MANAGER_H_
+#ifndef STORAGE_BROWSER_TEST_MOCK_QUOTA_MANAGER_H_
+#define STORAGE_BROWSER_TEST_MOCK_QUOTA_MANAGER_H_
 
 #include <stdint.h>
 
@@ -148,4 +148,4 @@ class MockQuotaManager : public QuotaManager {
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_QUOTA_MOCK_QUOTA_MANAGER_H_
+#endif  // STORAGE_BROWSER_TEST_MOCK_QUOTA_MANAGER_H_
