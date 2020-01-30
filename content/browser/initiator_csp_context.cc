ba69c9331279db51e146d376cf9e1435d5bd0176
arthursonzogni <arthursonzogni@chromium.org>
2020-01-30 18:26:45 +0000

    Unify CSP: Merge network/ and content/ ContentSecurityPolicy
    This correspond to step 1.d from the "Unify CSP" document:
    https://docs.google.com/document/d/1v5mJnXJ5dSVXE_rgvJnNM9bzH0ni0YzdhPQ7GLqyhao
    
    This merges:
     - content::ContentSecurityPolicy (removed)
     - network::mojom::ContentSecurityPolicy (kept)
    
    Only the mojo one remains: network::mojom::ContentSecurityPolicy;
    
    Future follow-up will:
      * move CSPContext to network.
      * remove content/common/content/security_policy/
    
    With this patch, the goal "have a unique data structure for representing
    CSP outside of blink" is completed \o/
    
    Bug: 1021462
    Change-Id: I262b6c976a9096bc6ff7235ffd56a1985e948d43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028128
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736950}

--- content/browser/initiator_csp_context.cc
+++ content/browser/initiator_csp_context.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 InitiatorCSPContext::InitiatorCSPContext(
-    std::vector<ContentSecurityPolicy> policies,
+    std::vector<network::mojom::ContentSecurityPolicyPtr> policies,
     network::mojom::CSPSourcePtr self_source,
     mojo::PendingRemote<blink::mojom::NavigationInitiator> navigation_initiator)
     : reporting_render_frame_host_impl_(nullptr),
--- content/common/content_security_policy/csp_source_list.h
+++ content/common/content_security_policy/csp_source_list.h
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+// TODO(arthursonzogni): Once CSPContext has been moved to
+// /services/network/public/content_security_policy, this file is going to be
+// moved there as well.
 class CSPContext;
 
 std::string CONTENT_EXPORT
