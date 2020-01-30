efbeb82c7664a5b1e34eba0c4fd24e70c4dace56
Marko Ivanovich <ivanovich@yandex-team.ru>
2020-01-30 12:39:15 +0000

    Fix race condition in background sync tests
    In case of simplest tests RegisterFrom(Un)ControlledDocument,
    message from service worker about tag being removed, is sent before
    promise passed to waitUntil is resolved. There is no guarantee
    that the tag will be removed before test requests list of tags.
    So this is race condition, that can be easily verified by adding
    sleep call to WaitUntilObserver::OnPromiseFulfilled, simulating
    thread interruption by OS scheduler. That is why we should not
    expect tag to be removed after we have received message about
    its removal, by actually wait for it. If test times out,
    it would fail. Similarly we should change some other tests,
    to make them future proof.
    
    Change-Id: I476acc1bececb01acb2d42db670af1a6cb5afa7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2022665
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736821}

--- content/browser/background_sync/one_shot_background_sync_browsertest.cc
+++ content/browser/background_sync/one_shot_background_sync_browsertest.cc
@@ -40,6 +40,7 @@ class OneShotBackgroundSyncBrowserTest : public BackgroundSyncBaseBrowserTest {
   bool RegisterFromServiceWorker(const std::string& tag);
   bool RegisterFromCrossOriginFrame(const std::string& frame_url,
                                     std::string* script_result);
+  void WaitForTagRemoval(const std::string& tag, int64_t pauses_ms = 5);
   bool HasTag(const std::string& tag);
   bool HasTagFromServiceWorker(const std::string& tag);
   bool MatchTags(const std::string& script_result,
@@ -80,6 +81,17 @@ bool OneShotBackgroundSyncBrowserTest::RegisterFromCrossOriginFrame(
   return RunScript(
       BuildScriptString("registerOneShotSyncFromCrossOriginFrame", url.spec()),
       script_result);
+}
+
+void OneShotBackgroundSyncBrowserTest::WaitForTagRemoval(const std::string& tag,
+                                                         int64_t pauses_ms) {
+  while (HasTag(tag)) {
+    base::RunLoop run_loop;
+    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+        FROM_HERE, run_loop.QuitClosure(),
+        base::TimeDelta::FromMilliseconds(pauses_ms));
+    run_loop.Run();
+  }
 }
 
 bool OneShotBackgroundSyncBrowserTest::HasTag(const std::string& tag) {
@@ -145,7 +157,7 @@ IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest,
 
   EXPECT_TRUE(Register("foo"));
   EXPECT_TRUE(PopConsole("foo fired"));
-  EXPECT_FALSE(HasTag("foo"));
+  WaitForTagRemoval("foo");
 }
 
 IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest,
@@ -154,7 +166,7 @@ IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest,
 
   EXPECT_TRUE(Register("foo"));
   EXPECT_TRUE(PopConsole("foo fired"));
-  EXPECT_FALSE(HasTag("foo"));
+  WaitForTagRemoval("foo");
 }
 
 // Verify that Register works in a service worker
@@ -166,7 +178,7 @@ IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest,
   EXPECT_TRUE(RegisterFromServiceWorker("foo_sw"));
   EXPECT_TRUE(PopConsole("ok - foo_sw registered in SW"));
   EXPECT_TRUE(PopConsole("foo_sw fired"));
-  EXPECT_FALSE(HasTag("foo_sw"));
+  WaitForTagRemoval("foo_sw");
 }
 
 IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest,
@@ -183,7 +195,7 @@ IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest,
   // Resume firing by going online.
   background_sync_test_util::SetOnline(web_contents(), true);
   EXPECT_TRUE(PopConsole("foo fired"));
-  EXPECT_FALSE(HasTag("foo"));
+  WaitForTagRemoval("foo");
 }
 
 IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest, WaitUntil) {
@@ -202,7 +214,7 @@ IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest, WaitUntil) {
   EXPECT_TRUE(PopConsole("ok - delay completed"));
 
   // Verify that it finished firing.
-  EXPECT_FALSE(HasTag("delay"));
+  WaitForTagRemoval("delay");
 }
 
 IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest, WaitUntilReject) {
@@ -219,7 +231,7 @@ IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest, WaitUntilReject) {
   // Complete the task.
   EXPECT_TRUE(RejectDelayedSyncEvent());
   EXPECT_TRUE(PopConsole("ok - delay rejected"));
-  EXPECT_FALSE(HasTag("delay"));
+  WaitForTagRemoval("delay");
 }
 
 IN_PROC_BROWSER_TEST_F(OneShotBackgroundSyncBrowserTest, Incognito) {
