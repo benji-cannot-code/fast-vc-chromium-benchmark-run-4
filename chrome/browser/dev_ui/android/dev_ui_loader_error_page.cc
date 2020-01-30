c5681806d24d2d8b9e400b010455fb225a2e7f77
Samuel Huang <huangs@chromium.org>
2020-01-30 19:28:31 +0000

    Revert "[DevUI DFM] Fix crash-on-install caused by stale navigation states."
    This reverts commit 9d43bcbd7ead54f358999d3cffb4caeb168a904d.
    
    Reason for revert: I pressed the wrong button; CL is not ready.
    
    Original change's description:
    > [DevUI DFM] Fix crash-on-install caused by stale navigation states.
    > 
    > Previously, if a failed DevUI DFM install attempt (leading to the error
    > page) is followed by a successful install, then the following problems
    > arise:
    > (A) chrome.send() is unavailable for the loaded DevUI page.
    > (B) Addition refreshes trigger CHECK() and crashes browser.
    > It seems that between showing the error page and showing the DevUI page
    > (after successful install), some stale state persisted so that the
    > active instance of NavigationRequest has bindings() holding a value
    > without BINDINGS_POLICY_WEB_UI, so that chrome.send() would not load,
    > leading to (A). Then (B) is triggered as an after effect.
    > 
    > This CL is a work-around for the bug for the DevUI DFM. On initial
    > DevUI DFM install, instead of resuming navigation to the intended
    > DevUI page, we visit a small shim page that self-refreshes, so that
    > stale states are cleared.
    > 
    > This is not a full fix, since state staleness problem (and crash on
    > refresh) still exists for error pages (in history or other tabs) other
    > than the one that successfully triggers DevUI DFM install.
    > 
    > Bug: 1046159
    > Change-Id: I775491ee165e7d2a809d93d334f93ae4bbf72cb6
    > Fixed: 1046159
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028363
    > Commit-Queue: Samuel Huang <huangs@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#736836}
    
    TBR=creis@chromium.org,huangs@chromium.org,agrieve@chromium.org
    
    Change-Id: I7ff5e5fd7f86ac189bafc4b45ad4e4ade5c61410
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1046159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2031536
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736984}

--- chrome/browser/dev_ui/android/dev_ui_loader_error_page.cc
+++ chrome/browser/dev_ui/android/dev_ui_loader_error_page.cc
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace dev_ui {
 
-std::string BuildInitialInstallRedirectPageHtml() {
-  return "<script>window.location.reload()</script>";
-}
-
 std::string BuildErrorPageHtml() {
   ui::TemplateReplacements replacements;
   replacements["h1"] =
