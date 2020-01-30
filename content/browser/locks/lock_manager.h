585991e40047a0732a66de7fc442c370018c6c2e
Joshua Bell <jsbell@chromium.org>
2020-01-30 00:31:33 +0000

    Web Locks: Make LockManager non-refcounted
    The manager had only a single owner (StoragePartitionImpl) so there
    was no reason it needed to be refcounted. Change ownership to be
    unique_ptr.
    
    Change-Id: Iea1895ecc41cb9614ef0f5624a6b0cfbc037e6bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028121
    Auto-Submit: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736634}

--- content/browser/locks/lock_manager.h
+++ content/browser/locks/lock_manager.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/callback.h"
-#include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
 #include "content/public/browser/browser_context.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
@@ -22,10 +21,10 @@ namespace content {
 // One instance of this exists per StoragePartition, and services multiple
 // child processes/origins. An instance must only be used on the sequence
 // it was created on.
-class LockManager : public base::RefCountedThreadSafe<LockManager>,
-                    public blink::mojom::LockManager {
+class LockManager : public blink::mojom::LockManager {
  public:
   LockManager();
+  ~LockManager() override;
 
   // Binds |receiver| to this LockManager. |receiver| belongs to a frame or
   // worker at |origin| hosted by |render_process_id|. If it belongs to a frame,
@@ -48,10 +47,6 @@ class LockManager : public base::RefCountedThreadSafe<LockManager>,
 
   // Called to request a snapshot of the current lock state for an origin.
   void QueryState(QueryStateCallback callback) override;
-
- protected:
-  friend class base::RefCountedThreadSafe<LockManager>;
-  ~LockManager() override;
 
  private:
   // Internal representation of a lock request or held lock.
