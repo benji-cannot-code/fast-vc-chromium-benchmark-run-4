635389a09f077a338d2a38f2bbe397cfba614f22
Thomas Tellier <tellier@google.com>
2020-01-30 18:22:54 +0000

    Visual revamp of info user bubble on login screen and refacto
    Bug: 1010249
    Change-Id: Ia44fdb3202c54a296fc22ae0d90703d2980d9d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2025549
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Cr-Commit-Position: refs/heads/master@{#736949}

--- ash/login/ui/login_error_bubble.cc
+++ ash/login/ui/login_error_bubble.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/login/ui/login_error_bubble.h"
 
 #include "ash/login/ui/non_accessible_view.h"
+#include "ash/login/ui/views_utils.h"
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/controls/image_view.h"
+#include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
 
@@ -49,10 +51,14 @@ LoginErrorBubble::~LoginErrorBubble() = default;
 
 void LoginErrorBubble::SetContent(views::View* content) {
   if (content_)
-    RemoveChildView(content_);
-
+    delete content_;
   content_ = content;
   AddChildView(content_);
+}
+
+void LoginErrorBubble::SetTextContent(const base::string16& message) {
+  SetContent(
+      login_views_utils::CreateBubbleLabel(message, gfx::kGoogleGrey200, this));
 }
 
 void LoginErrorBubble::SetAccessibleName(const base::string16& name) {
