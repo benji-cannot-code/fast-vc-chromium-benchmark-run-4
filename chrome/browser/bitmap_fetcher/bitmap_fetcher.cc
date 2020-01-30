1e7ca396330865b04468c15aa5b9d02298b8fa3f
Tommy Li <tommycli@chromium.org>
2020-01-30 00:31:47 +0000

    BitmapFetcher: Let BitmapFetcher handle data: URLs correctly
    The omnibox uses BitmapFetcher to get Entity Suggestion images.
    
    For on-focus suggestions, sometimes the images are included inline with
    the Suggest results as data: URLs. This currently fails to render,
    because BitmapFetcher cannot handle them.
    
    This CL makes BitmapFetcher handle them and includes a test.
    
    I implemented this functionality as part of BitmapFetcher instead of
    further upstream, because there are multiple callers to BitmapFetcher
    that may also run into this gotcha in the future, and it would be
    unfortunate if they each had to write their own code to handle data:
    URLs.
    
    Bug: 1015174
    Change-Id: Ibb68f0d0efdcaa8107d486dd88e3e3ccb50e2b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2017962
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Rachel Blum <groby@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736635}

--- chrome/browser/bitmap_fetcher/bitmap_fetcher.cc
+++ chrome/browser/bitmap_fetcher/bitmap_fetcher.cc
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher.h"
 
 #include "base/bind.h"
+#include "base/task/post_task.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/storage_partition.h"
+#include "net/base/data_url.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_context_getter.h"
 #include "net/url_request/url_request_status.h"
+#include "url/url_constants.h"
 
 BitmapFetcher::BitmapFetcher(
     const GURL& url,
@@ -38,9 +41,24 @@ void BitmapFetcher::Init(const std::string& referrer,
 }
 
 void BitmapFetcher::Start(network::mojom::URLLoaderFactory* loader_factory) {
+  network::SimpleURLLoader::BodyAsStringCallback callback = base::BindOnce(
+      &BitmapFetcher::OnSimpleLoaderComplete, weak_factory_.GetWeakPtr());
+
+  // Early exit to handle data URLs.
+  if (url_.SchemeIs(url::kDataScheme)) {
+    std::string mime_type, charset, data;
+    std::unique_ptr<std::string> response_body;
+    if (net::DataURL::Parse(url_, &mime_type, &charset, &data))
+      response_body = std::make_unique<std::string>(std::move(data));
+
+    // Post a task to maintain our guarantee that the delegate will only be
+    // called asynchronously.
+    base::PostTask(FROM_HERE,
+                   BindOnce(std::move(callback), std::move(response_body)));
+    return;
+  }
+
   if (simple_loader_) {
-    network::SimpleURLLoader::BodyAsStringCallback callback = base::BindOnce(
-        &BitmapFetcher::OnSimpleLoaderComplete, base::Unretained(this));
     simple_loader_->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
         loader_factory, std::move(callback));
   }
