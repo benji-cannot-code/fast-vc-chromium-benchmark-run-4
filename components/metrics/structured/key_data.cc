b03df243696f9a4a19053fd74d506ef31ebe26f4
tby <tby@chromium.org>
2020-01-30 11:01:07 +0000

    Reland "[Structured metrics] Rearrange build files"
    This is a reland of cbe2538725a29c5e644f2e87d9f979dca6d68af1
    
    Original change's description:
    > [Structured metrics] Rearrange build files
    > 
    > Currently, the generated classes for each event are created under the
    > //tools/metrics/structured target. I don't think this is the intended
    > use of the tools directory, because chrome targets explicitely can't
    > depend on it, and so can't access the generated events.
    > 
    > This CL moves the codegen targets to //components/metrics/structured,
    > which is accessible from chrome (and just makes more sense).
    > 
    > Bug: 1016655
    > Change-Id: I6a295486a3dad094b44db67d70408c8629728bcc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029531
    > Commit-Queue: Tony Yeoman <tby@chromium.org>
    > Auto-Submit: Tony Yeoman <tby@chromium.org>
    > Reviewed-by: Jia Meng <jiameng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#736753}
    
    Bug: 1016655
    Change-Id: I16f5ddd85b46d0b3420d7a40421a5cba60063f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2030385
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736798}

--- components/metrics/structured/key_data.cc
+++ components/metrics/structured/key_data.cc
@@ -13,10 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
 #include "base/values.h"
+#include "components/metrics/structured/structured_events.h"
 #include "components/prefs/json_pref_store.h"
 #include "crypto/hmac.h"
 #include "crypto/sha2.h"
-#include "tools/metrics/structured/structured_events.h"
 
 namespace metrics {
 namespace structured {
