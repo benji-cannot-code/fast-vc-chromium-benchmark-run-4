c711487b3957ce432335a9c328428411303d55d2
Hidehiko Abe <hidehiko@chromium.org>
2020-01-30 16:05:36 +0000

    Fix hang_watcher's standard lib header includes.
    BUG=None
    TEST=Build locally.
    
    Change-Id: Ibf130dcb59c094cedb6fc94ed62f74d97a299616
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029406
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736877}

--- base/threading/hang_watcher.cc
+++ base/threading/hang_watcher.cc
@@ -4,6 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/threading/hang_watcher.h"
+
+#include <algorithm>
+#include <utility>
+
 #include "base/bind.h"
 #include "base/callback_helpers.h"
 #include "base/no_destructor.h"
--- base/threading/hang_watcher.h
+++ base/threading/hang_watcher.h
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_THREADING_HANG_WATCHER_H_
 #define BASE_THREADING_HANG_WATCHER_H_
 
-#include <exception>
+#include <atomic>
 #include <memory>
+#include <vector>
 
 #include "base/atomicops.h"
 #include "base/callback.h"
