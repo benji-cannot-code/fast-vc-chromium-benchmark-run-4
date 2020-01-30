3cc58373c3601d7a8d317e5429f166d89642510f
Anupam Snigdha <snianu@microsoft.com>
2020-01-30 22:52:29 +0000

    Removed resetting of the keyboard observer while dismissing virtual keyboard.
    On Windows, input service has a callback when the keyboard is
    shown/dismissed so the browser could scroll the content into view
    if its occluded by the VK.
    This callback is an async call and doesn't align with focus
    notification from the renderer process. If we reset the keyboard
    observer before the VK is dismissed, then the callback wouldn't be
    called and the content would be occluded by the VK.
    This fix removes the code that resets the keyboard observer right after
    the dismiss VK is called.
    The observer is reset during DetachFromInputMethod call.
    
    Bug: 1039766, 1046708
    
    Change-Id: I27118daa0c99d35eae9aa80a33ab32a8d155ddf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2021419
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737078}

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -1755,7 +1755,6 @@ void RenderWidgetHostViewAura::FocusedNodeChanged(
     virtual_keyboard_requested_ = false;
     if (auto* controller = GetInputMethod()->GetInputMethodKeyboardController())
       controller->DismissVirtualKeyboard();
-    keyboard_observer_.reset(nullptr);
   }
 #elif defined(OS_FUCHSIA)
   if (!editable && window_) {
@@ -2408,8 +2407,6 @@ void RenderWidgetHostViewAura::OnUpdateTextInputStateCalled(
         keyboard_observer_.reset(new WinScreenKeyboardObserver(this));
         GetInputMethod()->ShowVirtualKeyboardIfEnabled();
         virtual_keyboard_requested_ = keyboard_observer_.get();
-      } else {
-        keyboard_observer_.reset(nullptr);
       }
     }
 #endif
