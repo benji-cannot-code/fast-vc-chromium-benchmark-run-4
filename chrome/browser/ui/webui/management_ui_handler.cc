abde368b3e9456a36892fc42071cec5a16151700
Olya Kalitova <okalitova@chromium.org>
2020-01-30 15:32:43 +0000

    Add message for CrostiniAnsiblePlaybook policy to transparency panel
    Adds message in transparency panel when CrostiniAnsiblePlaybook policy
    is set for user.
    If CrostiniAnsiblePlaybook policy is set and Crostini reporting is
    enabled the new message is set, when only reporting is enabled then old
    message for Crostini reporting is set.
    
    TEST: deployed to DUT and tested using YAPS
    Bug: 1018775
    Change-Id: Ia4df50c3acf69fcc613a8888752706c1e0e6ed0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2000624
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#736868}

--- chrome/browser/ui/webui/management_ui_handler.cc
+++ chrome/browser/ui/webui/management_ui_handler.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/webui/web_ui_util.h"
 
 #if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/crostini/crostini_features.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.h"
@@ -139,6 +140,8 @@ const char kManagementReportAndroidApplications[] =
     "managementReportAndroidApplications";
 const char kManagementPrinting[] = "managementPrinting";
 const char kManagementCrostini[] = "managementCrostini";
+const char kManagementCrostiniContainerConfiguration[] =
+    "managementCrostiniContainerConfiguration";
 const char kAccountManagedInfo[] = "accountManagedInfo";
 const char kDeviceManagedInfo[] = "deviceManagedInfo";
 const char kOverview[] = "overview";
@@ -267,10 +270,18 @@ void AddDeviceReportingInfo(base::Value* report_sources, Profile* profile) {
                               DeviceReportingType::kPrint);
   }
 
-  if (profile->GetPrefs()->GetBoolean(
-          crostini::prefs::kReportCrostiniUsageEnabled)) {
-    AddDeviceReportingElement(report_sources, kManagementCrostini,
-                              DeviceReportingType::kCrostini);
+  if (crostini::CrostiniFeatures::Get()->IsAllowed(profile)) {
+    if (!profile->GetPrefs()
+             ->GetFilePath(crostini::prefs::kCrostiniAnsiblePlaybookFilePath)
+             .empty()) {
+      AddDeviceReportingElement(report_sources,
+                                kManagementCrostiniContainerConfiguration,
+                                DeviceReportingType::kCrostini);
+    } else if (profile->GetPrefs()->GetBoolean(
+                   crostini::prefs::kReportCrostiniUsageEnabled)) {
+      AddDeviceReportingElement(report_sources, kManagementCrostini,
+                                DeviceReportingType::kCrostini);
+    }
   }
 
   if (g_browser_process->local_state()->GetBoolean(
