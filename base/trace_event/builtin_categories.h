25f13118a9ea98b78d9bde75079ae1d9ecfce743
Kramer Ge <fangzhoug@chromium.org>
2020-01-31 16:49:08 +0000

    Tracing category 'gpu_decoder' does not reflect implementation naming
    'gpu.decoder' is used in decoders and mismatches chrome://tracing UI.
    Resulting in gpu.decoder always disabled.
    Rename CL was:
    https://chromium-review.googlesource.com/c/chromium/src/+/1334229
    
    Expected behavior: when recording with gpu.decoder, gpu.device and
    gpu.service, there should be slices with category
    "disabled-by-default-gpu.decoder".
    
    Change-Id: I074a938ce79e05da2b6f931dca35f8f6864fd0c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024070
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737350}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -183,7 +183,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(TRACE_DISABLED_BY_DEFAULT("gpu_cmd_queue"))                          \
   X(TRACE_DISABLED_BY_DEFAULT("gpu.dawn"))                               \
   X(TRACE_DISABLED_BY_DEFAULT("gpu.debug"))                              \
-  X(TRACE_DISABLED_BY_DEFAULT("gpu_decoder"))                            \
+  X(TRACE_DISABLED_BY_DEFAULT("gpu.decoder"))                            \
   X(TRACE_DISABLED_BY_DEFAULT("gpu.device"))                             \
   X(TRACE_DISABLED_BY_DEFAULT("gpu.service"))                            \
   X(TRACE_DISABLED_BY_DEFAULT("histogram_samples"))                      \
