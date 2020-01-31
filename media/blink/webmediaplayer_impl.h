3e712c8ae2436177e7bf964e7334dbaff66e7d1f
Xida Chen <xidachen@chromium.org>
2020-01-31 20:02:02 +0000

    Revert "Switch to using an explicit ended signal for HTMLMediaElement."
    This reverts commit e11c77abc77d53e31107374e2afb9fd7a5c4147c.
    
    Reason for revert:
    Suspect causing layout test failure:
    https://ci.chromium.org/p/chromium/builders/ci/linux-trusty-rel/7513
    
    Original change's description:
    > Switch to using an explicit ended signal for HTMLMediaElement.
    > 
    > We've long used now > dur as an implicit signal, but with poorly muxed
    > media this can be unreliable. Since EndedPlayback() is used for a few
    > important signals, we should ensure it's always right. As such, switch
    > to using the explicit signal known by the WebMediaPlayer.
    > 
    > This has been the source of a lot of flakiness over the years and we've
    > been thinking about doing this since 2014.
    > 
    > The only tricky part is in handling seeks to the end of file correctly;
    > our pipeline previously required that play be called before we would
    > trigger a real pipeline ended event. Instead we relied on HTMLME just
    > declaring the playback ended at now >= dur. Now we will "render" end
    > of stream even if playback hasn't started if we have nothing to play.
    > 
    > Bug: 409280, 1035472
    > Test: Updated unittests, all existing tests pass.
    > 
    > Change-Id: I1fbea34e8ac9e772d5b3573cd57df5d8c065e9c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2021292
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Raymond Toy <rtoy@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: John Rummell <jrummell@chromium.org>
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#737397}
    
    TBR=dalecurtis@chromium.org,jrummell@chromium.org,mlamouri@chromium.org,rtoy@chromium.org,guidou@chromium.org
    
    Change-Id: I99a7000fa2bc33db0b9abc3b257bc56f0778fb2c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 409280, 1035472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033541
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737411}

--- media/blink/webmediaplayer_impl.h
+++ media/blink/webmediaplayer_impl.h
@@ -168,7 +168,6 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   double Duration() const override;
   virtual double timelineOffset() const;
   double CurrentTime() const override;
-  bool IsEnded() const override;
 
   bool PausedWhenHidden() const override;
 
