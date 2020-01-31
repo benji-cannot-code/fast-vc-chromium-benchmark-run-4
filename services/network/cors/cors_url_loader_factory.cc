3ffe13c288121a326bcd2947dd172a273b28b10f
Lukasz Anforowicz <lukasza@chromium.org>
2020-01-31 23:00:10 +0000

    Relax request_initiator_site_lock checks for PDF plugin.
    In some scenarios (e.g. http range requests for linearized PDFs) the PDF
    plugin may need to initiate requests on behalf of the PDF's origin.
    Such requests are currently proxied by the renderer process hosting the
    plugin (i.e. the renderer hosting the PDF extension -
    mhjfbmdgcfjbbpaeojofohoefgiehjai) and therefore go through
    URLLoaderFactory that is locked via request_initiator_site_lock to a
    different origin (i.e. the origin of the PDF extension, rather than the
    origin of the website that hosts the PDF file).
    
    Before this CL, such requests would be classified by
    CorsURLLoaderFactory::IsSane and
    VerifyRequestInitiatorLockWithPluginCheck as incorrect and possibly
    malicious.  After this CL, the request_initiator checks are relaxed by
    PluginServiceImpl::FindOrStartPpapiPluginProcess using a new mechanism
    similar to AddCorbExceptionForPlugin exemption available for Flash.
    
    We don't want to relax request_initiator_site_lock checks for renderer
    processes hosting arbitrary origins from the web, so as a
    defense-in-depth the CL adds a CHECK to FindOrStartPpapiPluginProcess
    that verifies the origin lock of the renderer process that is granted
    the extra capabilities.
    
    Relevant tests:
    - ChromeFindRequestManagerTest.FindInChunkedPDF (tests that range
      request do not result in a request initiator lock mismatch)
    - PDFExtensionTest.EnsureInternalPluginDisabled (tests that the PDF
      plugin may only be embedded in trustworthy contexts)
    - PDFExtensionTest.RedirectsFailInPlugin (the 'noRedirectsSucceed' testcase
      tests non-range requests initiated by the PDF plugin)
    
    Bug: 1027173
    Change-Id: Ib5111fd7a5d5b31727a9754c46c316e75def5143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1986923
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737503}

--- services/network/cors/cors_url_loader_factory.cc
+++ services/network/cors/cors_url_loader_factory.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/cross_origin_read_blocking.h"
 #include "services/network/loader_util.h"
 #include "services/network/network_context.h"
+#include "services/network/network_service.h"
 #include "services/network/public/cpp/cors/cors.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/header_util.h"
@@ -32,28 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace network {
 
 namespace cors {
-
-namespace {
-
-InitiatorLockCompatibility VerifyRequestInitiatorLockWithPluginCheck(
-    uint32_t process_id,
-    const base::Optional<url::Origin>& request_initiator_site_lock,
-    const base::Optional<url::Origin>& request_initiator) {
-  if (process_id == mojom::kBrowserProcessId)
-    return InitiatorLockCompatibility::kBrowserProcess;
-
-  InitiatorLockCompatibility result = VerifyRequestInitiatorLock(
-      request_initiator_site_lock, request_initiator);
-
-  if (result == InitiatorLockCompatibility::kIncorrectLock &&
-      CrossOriginReadBlocking::ShouldAllowForPlugin(process_id)) {
-    result = InitiatorLockCompatibility::kExcludedUniversalAccessPlugin;
-  }
-
-  return result;
-}
-
-}  // namespace
 
 class CorsURLLoaderFactory::FactoryOverride final {
  public:
@@ -306,7 +285,8 @@ bool CorsURLLoaderFactory::IsSane(const NetworkContext* context,
   switch (initiator_lock_compatibility) {
     case InitiatorLockCompatibility::kCompatibleLock:
     case InitiatorLockCompatibility::kBrowserProcess:
-    case InitiatorLockCompatibility::kExcludedUniversalAccessPlugin:
+    case InitiatorLockCompatibility::kExcludedCorbForPlugin:
+    case InitiatorLockCompatibility::kAllowedRequestInitiatorForPlugin:
       break;
 
     case InitiatorLockCompatibility::kNoLock:
@@ -326,9 +306,9 @@ bool CorsURLLoaderFactory::IsSane(const NetworkContext* context,
     case InitiatorLockCompatibility::kIncorrectLock:
       // Requests from the renderer need to always specify a correct initiator.
       //
-      // TODO(lukasza): https://crbug.com/1027173: Reintroduce NOTREACHED below
-      // after relaxing request initiator checks to account for requests issued
-      // by the PDF plugin.
+      // TODO(lukasza): https://crbug.com/920634: Reintroduce NOTREACHED below
+      // after fixing remaining mismatches that are exposed by existing tests
+      // (e.g. ContentScriptApiTest.ExecuteScriptFileSameSiteCookies).
       //
       // TODO(lukasza): https://crbug.com/920634: Report bad message and return
       // false below.
@@ -391,6 +371,32 @@ bool CorsURLLoaderFactory::IsSane(const NetworkContext* context,
   // TODO(yhirano): If the request mode is "no-cors", the redirect mode should
   // be "follow".
   return true;
+}
+
+InitiatorLockCompatibility
+CorsURLLoaderFactory::VerifyRequestInitiatorLockWithPluginCheck(
+    uint32_t process_id,
+    const base::Optional<url::Origin>& request_initiator_site_lock,
+    const base::Optional<url::Origin>& request_initiator) {
+  if (process_id == mojom::kBrowserProcessId)
+    return InitiatorLockCompatibility::kBrowserProcess;
+
+  InitiatorLockCompatibility result = VerifyRequestInitiatorLock(
+      request_initiator_site_lock, request_initiator);
+
+  if (result == InitiatorLockCompatibility::kIncorrectLock &&
+      CrossOriginReadBlocking::ShouldAllowForPlugin(process_id)) {
+    result = InitiatorLockCompatibility::kExcludedCorbForPlugin;
+  }
+
+  if (result == InitiatorLockCompatibility::kIncorrectLock &&
+      request_initiator.has_value() &&
+      context_->network_service()->IsInitiatorAllowedForPlugin(
+          process_id, request_initiator.value())) {
+    result = InitiatorLockCompatibility::kAllowedRequestInitiatorForPlugin;
+  }
+
+  return result;
 }
 
 }  // namespace cors
