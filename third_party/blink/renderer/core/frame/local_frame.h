5e80b73a8d1b944b4843209dae3a4f765a75f02b
Yao Xiao <yaoxia@chromium.org>
2020-01-31 22:30:59 +0000

    Record UseCounter for Large Sticky Ads
    What: Record a use counter whenever a large sticky ad is detected on
    a page. Large sticky ads stands for ad iframes with size >30% browser
    viewport size, and stick to the page regardless of a user’s efforts to
    scroll.
    
    Why: We’d like to learn where and how often they are used, as a signal
    to Centrifuge and eventually to unload them automatically as a browser
    intervention.
    
    How: Add a StickyFrameTracker to FrameView that tracks and stores the
    latest 10 viewport_offset/main_frame_scroll_offset through
    FrameView::UpdateViewportIntersection, which will be called whenever
    there's a scroll on the main page. Apply the detection heuristic
    (see sticky_frame_tracker.h) and record a use counter as necessary.
    
    We calculate/propagate the flag can_skip_sticky_frame_tracking to child
    frames to save the cost of most rendering/sticky-tracking, as most
    frames should be small so we could skip tracking its children. However
    it's still hard to gauge the performance impact of those cases that
    needs the extra renderings, so hide this functionality behind a feature
    flag.
    
    
    Bug: 1013070
    Change-Id: I17d65ac915593ea3496bd6391fe6e163f0c57e82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1852105
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737491}

--- third_party/blink/renderer/core/frame/local_frame.h
+++ third_party/blink/renderer/core/frame/local_frame.h
@@ -347,6 +347,9 @@ class CORE_EXPORT LocalFrame final : public Frame,
   // remote ancestor frames and their respective scroll positions, clips, etc.
   void SetViewportIntersectionFromParent(const ViewportIntersectionState&);
 
+  IntSize GetMainFrameViewportSize() const override;
+  IntPoint GetMainFrameScrollOffset() const override;
+
   // See viewport_intersection_state.h for more info on these methods.
   gfx::Point RemoteViewportOffset() const {
     return intersection_state_.viewport_offset;
@@ -357,7 +360,13 @@ class CORE_EXPORT LocalFrame final : public Frame,
   IntRect RemoteMainFrameDocumentIntersection() const {
     return intersection_state_.main_frame_document_intersection;
   }
+
+  bool CanSkipStickyFrameTracking() const {
+    return intersection_state_.can_skip_sticky_frame_tracking;
+  }
+
   FrameOcclusionState GetOcclusionState() const;
+
   bool NeedsOcclusionTracking() const;
 
   // Replaces the initial empty document with a Document suitable for
