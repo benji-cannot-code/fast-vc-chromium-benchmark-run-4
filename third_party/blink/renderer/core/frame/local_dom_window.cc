73e3f90c36fd56564c8f6c331222d313207986bd
Carlos IL <carlosil@chromium.org>
2020-01-31 01:47:09 +0000

    Add metrics for same origin iframe JS modal dialogs
    This adds use counters for same origin iframe use of alert, confirm,
    print, and prompt.
    
    Bug: 1047451
    Change-Id: Ieee5a69821be5aa8a96af897073e5b8d3deb8cd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032180
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737163}

--- third_party/blink/renderer/core/frame/local_dom_window.cc
+++ third_party/blink/renderer/core/frame/local_dom_window.cc
@@ -707,6 +707,9 @@ void LocalDOMWindow::print(ScriptState* script_state) {
     return;
   }
 
+  if (!GetFrame()->IsMainFrame() && !GetFrame()->IsCrossOriginSubframe()) {
+    document()->CountUse(WebFeature::kSameOriginIframeWindowPrint);
+  }
   document()->CountUseOnlyInCrossOriginIframe(
       WebFeature::kCrossOriginWindowPrint);
 
@@ -744,6 +747,9 @@ void LocalDOMWindow::alert(ScriptState* script_state, const String& message) {
   if (!page)
     return;
 
+  if (!GetFrame()->IsMainFrame() && !GetFrame()->IsCrossOriginSubframe()) {
+    document()->CountUse(WebFeature::kSameOriginIframeWindowAlert);
+  }
   document()->CountUseOnlyInCrossOriginIframe(
       WebFeature::kCrossOriginWindowAlert);
 
@@ -774,6 +780,9 @@ bool LocalDOMWindow::confirm(ScriptState* script_state, const String& message) {
   if (!page)
     return false;
 
+  if (!GetFrame()->IsMainFrame() && !GetFrame()->IsCrossOriginSubframe()) {
+    document()->CountUse(WebFeature::kSameOriginIframeWindowConfirm);
+  }
   document()->CountUseOnlyInCrossOriginIframe(
       WebFeature::kCrossOriginWindowConfirm);
 
@@ -811,6 +820,9 @@ String LocalDOMWindow::prompt(ScriptState* script_state,
                                                    default_value, return_value))
     return return_value;
 
+  if (!GetFrame()->IsMainFrame() && !GetFrame()->IsCrossOriginSubframe()) {
+    document()->CountUse(WebFeature::kSameOriginIframeWindowPrompt);
+  }
   document()->CountUseOnlyInCrossOriginIframe(
       WebFeature::kCrossOriginWindowPrompt);
 
