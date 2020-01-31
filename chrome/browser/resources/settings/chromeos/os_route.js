1b75e9a7c464a2438493f5a633214852d6a3f204
Kyle Horimoto <khorimoto@chromium.org>
2020-01-31 21:26:52 +0000

    [CrOS Search] Add new search directory with SettingsUserAction class.
    This CL introduces Mojo enums for settings section names and page
    visibility as well as a SettingsUserAction class and several derived
    classes which encapsulate user actions in settings.
    
    Bug: 1047062
    Change-Id: I04d1648ade1f8b25dc83c2f32805a22bd4c23458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2030000
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737458}

--- chrome/browser/resources/settings/chromeos/os_route.js
+++ chrome/browser/resources/settings/chromeos/os_route.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 cr.define('settings', function() {
   /**
    * Creates Route objects for each path corresponding to CrOS settings content.
+   * Note that if any new routes are added here, they should also be added to
+   * //chrome/browser/ui/webui/settings/chromeos/search/search.mojom.
    * @return {!SettingsRoutes}
    */
   function createOSSettingsRoutes() {
