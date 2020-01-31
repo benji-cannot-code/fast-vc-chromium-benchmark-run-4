8ce2103a847ecc9b1394eb15aef2ca19f73128bf
Sigurdur Asgeirsson <siggi@chromium.org>
2020-01-31 21:13:10 +0000

    Remove unused persistent browser watcher stability debugging code.
    It's been a couple of years since this code was used, and the oncoming
    refactoring will be easier without this in the way.
    
    Bug: 1044707
    Change-Id: Ic6e07b8f3ce61312d2c5a89d8af9850ea7510a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032230
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Auto-Submit: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737444}

--- components/browser_watcher/features.cc
+++ components/browser_watcher/features.cc
@@ -12,8 +12,4 @@ const base::Feature kExtendedCrashReportingFeature{
 
 const char kInMemoryOnlyParam[] = "in_memory_only";
 
-const char kInitFlushParam[] = "init_flush";
-
-const char kCollectPostmortemParam[] = "collect_postmortem";
-
 }  // namespace browser_watcher
