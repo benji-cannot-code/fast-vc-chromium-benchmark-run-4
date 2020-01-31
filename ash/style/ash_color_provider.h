504b234c539bbb729331c60fc522c0780b4c6aab
Ahmed Mehfooz <amehfooz@chromium.org>
2020-01-31 00:20:45 +0000

    Update kTransparent74 and kShield74 to 80
    Bug: 1046966
    Change-Id: I7611a62da0a084471073546fe8390642b887c206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029490
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737133}

--- ash/style/ash_color_provider.h
+++ ash/style/ash_color_provider.h
@@ -44,7 +44,7 @@ class ASH_EXPORT AshColorProvider {
     kShield20 = 0,
     kShield40,
     kShield60,
-    kShield74,
+    kShield80,
     kShield90,
   };
 
@@ -61,7 +61,7 @@ class ASH_EXPORT AshColorProvider {
     kTransparent20 = 0,
     kTransparent40,
     kTransparent60,
-    kTransparent74,
+    kTransparent80,
     kTransparent90,
 
     // Base layer is opaque.
