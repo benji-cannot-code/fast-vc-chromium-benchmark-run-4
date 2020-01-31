a21ae765f9248874d45b84280873356e3609b8b2
Allen Bauer <kylixrd@chromium.org>
2020-01-31 00:35:05 +0000

    Use the ink drop for hover highlight on MdTextButton.
    Change-Id: Ic7009c85824c74f3dd30dc9d84329c720b572172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032033
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737138}

--- ui/views/controls/button/md_text_button.h
+++ ui/views/controls/button/md_text_button.h
@@ -60,7 +60,6 @@ class VIEWS_EXPORT MdTextButton : public LabelButton {
 
  protected:
   // View:
-  void OnPaintBackground(gfx::Canvas* canvas) override;
   void OnFocus() override;
   void OnBlur() override;
 
