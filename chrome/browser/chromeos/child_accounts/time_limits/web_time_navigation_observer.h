1974bb7f37042d11165b108bc27116e2836c30c4
yilkal <yilkal@chromium.org>
2020-01-31 22:31:08 +0000

    Adds WebTimeActivityProvider.
    This CL adds WebTimeActivityProvider which is
    BrowserListObserver, a TabStripModelObserver,
    AppServiceWrapper::EventListener, and
    WebTimeNavigationObserver::EventListener. It is hosted
    in AppTimeController.
    
    The instance of this class observes
    the TabStripModel of each Browser in BrowserList. It also
    observes the WebTimeNavigationObserver instances of each WebContent
    hosted in each Browser window. The instance also observes
    AppServiceWrapper to learn about the activation/deactivation of
    chrome application windows.
    
    WebTimeActivityProvider informs its observers when Chrome becomes
    Active, Active with whitelisted content or inactive.
    
    Bug: 1034551
    Change-Id: I7d7dd3624ea4fb764c13e35f0ef542759cee8933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020522
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737492}

--- chrome/browser/chromeos/child_accounts/time_limits/web_time_navigation_observer.h
+++ chrome/browser/chromeos/child_accounts/time_limits/web_time_navigation_observer.h
@@ -46,7 +46,7 @@ class WebTimeNavigationObserver
    public:
     virtual void OnWebActivityChanged(const NavigationInfo& info) {}
     virtual void WebTimeNavigationObserverDestroyed(
-        const WebTimeNavigationObserver* observer) {}
+        WebTimeNavigationObserver* observer) {}
   };
 
   static void MaybeCreateForWebContents(content::WebContents* web_contents);
