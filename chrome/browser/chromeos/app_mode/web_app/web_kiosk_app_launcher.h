c551698d036ac8832ffd349122a34c283433ef6d
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-01-31 12:45:24 +0000

    [Kiosk] Do not use launch_url if it has different origin
    To avoid potential risks, do not update launch url if its origin does
    not much the one provided by policy.
    
    Bug: 1046260
    Change-Id: Ifa82de2eb45ada3bd9a68351ffbbf88324d9f9fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027849
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737295}

--- chrome/browser/chromeos/app_mode/web_app/web_kiosk_app_launcher.h
+++ chrome/browser/chromeos/app_mode/web_app/web_kiosk_app_launcher.h
@@ -28,6 +28,8 @@ class WebAppUrlLoader;
 
 namespace chromeos {
 
+class WebKioskAppData;
+
 // Object responsible for preparing and launching web kiosk app. Is destroyed
 // upon app launch.
 class WebKioskAppLauncher {
@@ -59,6 +61,8 @@ class WebKioskAppLauncher {
 
  private:
   void OnAppDataObtained(std::unique_ptr<WebApplicationInfo> app_info);
+
+  const WebKioskAppData* GetCurrentApp() const;
 
   bool is_installed_ = false;  // Whether the installation was completed.
   AccountId account_id_;
