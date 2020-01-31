923267046ae80032d9476af667ccf561ecf8d368
Andres Calderon Jaramillo <andrescj@chromium.org>
2020-01-31 07:56:55 +0000

    Revert "Viz/Ozone/DRM: Provide NativePixmap for the primary plane"
    This reverts commit 0ea417334e8b34e8f0bfbe7e05ce7709c8bda746.
    
    Reason for revert: In this CL, GetOverlayMailbox() calls BufferQueue::GetCurrentBuffer(). Unfortunately, it turns out there are cases where this causes the GPU to draw to a buffer that's being scanned out (see https://crbug.com/1047030 for an example). It may also cause problems with damage tracking. Reverting this CL to try to fix the issue before the M81 branch point :(
    
    Original change's description:
    > Viz/Ozone/DRM: Provide NativePixmap for the primary plane
    > 
    > Plumb mailbox for the primary plane to OverlayProcessorOzone and use
    > SharedImageInterface to obtain the corresponding NativePixmap.
    > 
    > Bug: 756454
    > Change-Id: Ic537d05253bc68c27a8fed961de8547ace359e82
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016327
    > Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    > Commit-Queue: Saman Sami <samans@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#736065}
    
    TBR=rjkroege@chromium.org,samans@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 756454
    Change-Id: I6f42b94d1939597f7c34f0d0433d45a02716aa58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2031723
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737238}

--- components/viz/service/display/output_surface.h
+++ components/viz/service/display/output_surface.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/resources/returned_resource.h"
 #include "components/viz/service/display/software_output_device.h"
 #include "components/viz/service/viz_service_export.h"
-#include "gpu/command_buffer/common/mailbox.h"
 #include "gpu/command_buffer/common/texture_in_use_response.h"
 #include "gpu/ipc/common/surface_handle.h"
 #include "gpu/ipc/gpu_task_scheduler_helper.h"
@@ -146,9 +145,6 @@ class VIZ_SERVICE_EXPORT OutputSurface {
 
   // Get the texture for the main image's overlay.
   virtual unsigned GetOverlayTextureId() const = 0;
-
-  // Returns the |mailbox| corresponding to the main image's overlay.
-  virtual gpu::Mailbox GetOverlayMailbox() const;
 
   // Get the format for the main image's overlay.
   virtual gfx::BufferFormat GetOverlayBufferFormat() const = 0;
