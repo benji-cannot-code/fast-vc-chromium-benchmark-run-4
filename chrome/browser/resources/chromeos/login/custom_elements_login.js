2668dc1b3e79b376d97a0e6af0de331db67ed240
Renato Silva <rrsilva@google.com>
2020-01-31 07:58:35 +0000

    Adapt Chrome OS OOBE ARC Terms of Service to MD
    Improves the overlay dialogs shown on OOBE ARC terms of service screen
    to match MD specs.
    
    Fixed: 1030466, 1032914
    Change-Id: If10fcdec926d724f43c08034519dac5b14eb55c4
    Bug: 1030466
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1953740
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#737239}

--- chrome/browser/resources/chromeos/login/custom_elements_login.js
+++ chrome/browser/resources/chromeos/login/custom_elements_login.js
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="oobe_adb_sideloading_screen.js">
 // <include src="oobe_buttons.js">
 // <include src="oobe_dialog.js">
+// <include src="oobe_help_dialog.js">
 // <include src="oobe_terms_of_service.js">
 // <include src="arc_terms_of_service.js">
 // <include src="oobe_enable_kiosk.js">
