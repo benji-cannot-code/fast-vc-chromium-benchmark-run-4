a8ba88ad7f8a71437dbd337586ee619688535d5b
Andrew Xu <andrewxu@chromium.org>
2020-01-31 21:08:43 +0000

    Keeps auto-hide shelf showing after opening the system tray
    Due to the side effect from https://crrev.com/c/1988855, auto-hide shelf
    is hidden after opening the system tray. Update on the shelf's
    visibility state is triggered by the visibility change of system tray.
    However, calculation on the auto-hide state depends on the state of
    tray bubbles which are created/destructed after the visibility change.
    This CL delays the update on the shelf's state when the visibility of
    system tray changes. In addition, a test case is added for this
    scenario.
    
    Bug: 1046464
    Change-Id: I72d48b5872ca9efc8160c9fd6bba6bd55addfa43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028766
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737437}

--- ash/shelf/scrollable_shelf_view.h
+++ ash/shelf/scrollable_shelf_view.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_container_view.h"
 #include "ash/shelf/shelf_tooltip_delegate.h"
 #include "ash/shelf/shelf_view.h"
+#include "base/cancelable_callback.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/views/animation/ink_drop_host_view.h"
 #include "ui/views/context_menu_controller.h"
