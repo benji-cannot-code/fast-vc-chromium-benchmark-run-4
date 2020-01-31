7ec44dbca5b2a1b845012b27bbbe40c05c3bd9cc
Makoto Shimazu <shimazu@chromium.org>
2020-01-31 09:04:51 +0000

    ServiceWorkerSingleScriptUpdateChecker has credentials
    Before the new byte-for-byte checking, credentials_mode was set to
    kInclude for imported scripts. However,
    ServiceWorkerSingleScriptUpdateChecker sets the flag to kOmit because
    it's missed from the current spec. This CL is to correct the mode to
    fix the unintentional change.
    Spec issue: https://github.com/w3c/ServiceWorker/issues/1497
    
    Bug: 1042159
    Change-Id: I553d7bec015eb4cb80f7f59c640f26491fa02b75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032689
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737253}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/echo-cookie-worker.py
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    headers = [("Content-Type", "text/javascript")]
+
+    values = []
+    for key in request.cookies:
+        for cookie in request.cookies.get_list(key):
+            values.append('"%s": "%s"' % (key, cookie.value))
+
+    # Update the counter to change the script body for every request to trigger
+    # update of the service worker.
+    key = request.GET['key']
+    counter = request.server.stash.take(key)
+    if counter is None:
+        counter = 0
+    counter += 1
+    request.server.stash.put(key, counter)
+
+    body = """
+// %d
+self.addEventListener('message', e => {
+  e.source.postMessage({%s})
+});""" % (counter, ','.join(values))
+
+    return headers, body
