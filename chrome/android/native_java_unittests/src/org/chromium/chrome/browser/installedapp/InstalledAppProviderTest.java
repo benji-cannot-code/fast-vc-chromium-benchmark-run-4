f9063cb03f3d35d1c977e71470392abed7329241
Michael Thiessen <mthiesse@chromium.org>
2020-01-31 18:00:52 +0000

    Migrate installedapp to GURL
    Replaces usages of java.net.URI with GURL.
    
    Bug: 783819
    Change-Id: I1fbcabd96afaa3b55165f1bcff926d9b31d6a3f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2031559
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737377}

--- chrome/android/native_java_unittests/src/org/chromium/chrome/browser/installedapp/InstalledAppProviderTest.java
+++ chrome/android/native_java_unittests/src/org/chromium/chrome/browser/installedapp/InstalledAppProviderTest.java
@@ -21,9 +21,9 @@ import org.chromium.chrome.browser.UnitTestUtils;
 import org.chromium.chrome.browser.instantapps.InstantAppsHandler;
 import org.chromium.installedapp.mojom.InstalledAppProvider;
 import org.chromium.installedapp.mojom.RelatedApplication;
+import org.chromium.url.URI;
 import org.chromium.url.mojom.Url;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -204,7 +204,7 @@ public class InstalledAppProviderTest {
 
         private static AssetManager createAssetManager() {
             try {
-                return (AssetManager) AssetManager.class.getConstructor().newInstance();
+                return AssetManager.class.getConstructor().newInstance();
             } catch (Exception e) {
                 return null;
             }
@@ -247,21 +247,17 @@ public class InstalledAppProviderTest {
         private URI mFrameUrl;
         private boolean mIncognito;
 
-        public FakeFrameUrlDelegate(String frameUrl) {
+        public FakeFrameUrlDelegate(String frameUrl) throws URISyntaxException {
             setFrameUrl(frameUrl);
         }
 
-        public void setFrameUrl(String frameUrl) {
+        public void setFrameUrl(String frameUrl) throws URISyntaxException {
             if (frameUrl == null) {
                 mFrameUrl = null;
                 return;
             }
 
-            try {
-                mFrameUrl = new URI(frameUrl);
-            } catch (URISyntaxException e) {
-                throw new AssertionError(e);
-            }
+            mFrameUrl = new URI(frameUrl);
         }
 
         @Override
@@ -359,7 +355,7 @@ public class InstalledAppProviderTest {
     private InstalledAppProviderTest() {}
 
     @CalledByNative
-    public void setUp() {
+    public void setUp() throws URISyntaxException {
         mFakePackageManager = new FakePackageManager();
         mFrameUrlDelegate = new FakeFrameUrlDelegate(URL_ON_ORIGIN);
         mFakeInstantAppsHandler = new FakeInstantAppsHandler();
@@ -375,7 +371,11 @@ public class InstalledAppProviderTest {
         setAssetStatement(PACKAGE_NAME_1, NAMESPACE_WEB, RELATION_HANDLE_ALL_URLS, ORIGIN);
         RelatedApplication[] expectedInstalledRelatedApps = new RelatedApplication[] {};
 
-        mFrameUrlDelegate.setFrameUrl(ORIGIN_MISSING_SCHEME);
+        try {
+            mFrameUrlDelegate.setFrameUrl(ORIGIN_MISSING_SCHEME);
+        } catch (URISyntaxException e) {
+            mFrameUrlDelegate.setFrameUrl(null);
+        }
         verifyInstalledApps(manifestRelatedApps, expectedInstalledRelatedApps);
 
         mFrameUrlDelegate.setFrameUrl(ORIGIN_MISSING_HOST);
