f9d061a447b612b59e683b7e202443ce904f7a25
Rob Buis <rbuis@igalia.com>
2020-02-01 09:32:19 +0000

    Add WPT tests to verify pings do not send a referrer header
    Add WPT tests to verify pings do not send a referrer header as
    specified in step 3:
    https://html.spec.whatwg.org/multipage/links.html#hyperlink-auditing
    
    Bug: 1046392
    
    Change-Id: Ie0f133be6db34084aac26280a29a90cc9e68e707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024855
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#737596}

--- third_party/blink/renderer/core/loader/ping_loader.cc
+++ third_party/blink/renderer/core/loader/ping_loader.cc
@@ -233,8 +233,6 @@ void PingLoader::SendLinkAuditPing(LocalFrame* frame,
   }
 
   request.SetKeepalive(true);
-  // TODO(domfarolino): Add WPTs ensuring that pings do not have a referrer
-  // header.
   request.SetReferrerString(Referrer::NoReferrer());
   request.SetReferrerPolicy(network::mojom::ReferrerPolicy::kNever);
   request.SetRequestContext(mojom::RequestContextType::PING);
