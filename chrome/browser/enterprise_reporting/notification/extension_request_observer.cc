f8ea383a603d75ad1cf18d111aec4625c45b9829
Owen Min <zmin@chromium.org>
2020-02-01 16:03:47 +0000

    Fix ExtensionRequestObserver crash
    Remove ExtensionManagement Observer crash because Profile instance has been
    deleted already during shutdown process.
    
    This fix check ProfileManager status before delete observer. Please note
    that this won't fix DCHECK failure on observer_list is not empty during
    shutdown but should prevent Canary crash.
    
    The real fix is coming later:
    https://chromium-review.googlesource.com/c/chromium/src/+/2033435
    
    Bug: 1047619
    Change-Id: I71cb961a988ff4475e9fecd04a3be2be4ed23476
    Tbr: pastarmovj@chromium.org:x
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033441
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737611}

--- chrome/browser/enterprise_reporting/notification/extension_request_observer.cc
+++ chrome/browser/enterprise_reporting/notification/extension_request_observer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/enterprise_reporting/notification/extension_request_observer.h"
 
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -21,9 +22,12 @@ ExtensionRequestObserver::ExtensionRequestObserver(Profile* profile)
 }
 
 ExtensionRequestObserver::~ExtensionRequestObserver() {
-  extensions::ExtensionManagementFactory::GetForBrowserContext(profile_)
-      ->RemoveObserver(this);
-  CloseAllNotifications();
+  // If Profile is still available during shutdown
+  if (g_browser_process->profile_manager()) {
+    extensions::ExtensionManagementFactory::GetForBrowserContext(profile_)
+        ->RemoveObserver(this);
+    CloseAllNotifications();
+  }
 }
 
 void ExtensionRequestObserver::OnExtensionManagementSettingsChanged() {
