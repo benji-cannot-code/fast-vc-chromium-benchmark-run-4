174a4e5368b2341b5b828ebc36dbb90f31a01531
Liquan (Max) Gu <maxlg@chromium.org>
2020-02-02 14:22:38 +0000

    [PaymentRequest] Remove Error View from Android
    Before change:
    * With Expandable PH UI enabled, if merchant pages call
    complete("fail"), the screen would be covered under scrim and Clank
    would crash on refresh.
    * With CCT PH UI enabled, if merchant pages call complete("fail"), it
    would open Error View after closing PH UI.
    
    After change:
    The Error View is no longer shown on complete("fail").
    
    Changes:
    * remove Error View from Android Payment Request UI.
    * Tests
     * add a test to ensure http would be rejected for openWindow(url)
     * add a test to cover complete ("unknown").
    
    Bug: 1044077, 1047237, 1042892, 1045474
    
    Change-Id: I0123e163d4cd0a97d1536d5c4baefa0e646c50d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2019922
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737634}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestFailCompleteTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestFailCompleteTest.java
@@ -21,7 +21,6 @@ import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.CreditCard;
 import org.chromium.chrome.browser.payments.PaymentRequestTestRule.MainActivityStartCallback;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.modaldialog.ModalDialogProperties;
 import org.chromium.ui.test.util.DisableAnimationsTestRule;
 
@@ -64,15 +63,6 @@ public class PaymentRequestFailCompleteTest implements MainActivityStartCallback
         mPaymentRequestTestRule.clickCardUnmaskButtonAndWait(
                 ModalDialogProperties.ButtonType.POSITIVE,
                 mPaymentRequestTestRule.getResultReady());
-
-        // Dismiss the error message overlay.
-        int callCount = mPaymentRequestTestRule.getDismissed().getCallCount();
-        TestThreadUtils.runOnUiThreadBlocking(
-                (Runnable) () -> mPaymentRequestTestRule.getPaymentRequestUI()
-                                   .getDialogForTest()
-                                   .findViewById(R.id.ok_button)
-                                   .performClick());
-        mPaymentRequestTestRule.getDismissed().waitForCallback(callCount);
 
         mPaymentRequestTestRule.expectResultContains(new String[] {"Transaction failed"});
     }
