9edd81747a7baad71cee43d9579f3c0d2d82e44a
Ian Clelland <iclelland@chromium.org>
2020-02-02 04:46:51 +0000

    Add use counter to feature policy to gauge web-compatibility of change.
    A proposed change to the semantics of the Feature Policy header could
    change the availability of features in some frames, depending on how the
    Feature-Policy header and allow attribute have been specified. This CL
    adds enough of the proposed logic to be able to tell whether the output
    would change for any given call to IsFeatureEnabled, and counts how
    often that situation occurs.
    
    Bug: 937131
    Change-Id: I2b587546f8d08a0c9d5e0c2bab60b77d771751c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028630
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737617}

--- third_party/blink/renderer/core/execution_context/execution_context.cc
+++ third_party/blink/renderer/core/execution_context/execution_context.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink.h"
 #include "third_party/blink/public/mojom/feature_policy/feature_policy_feature.mojom-blink.h"
 #include "third_party/blink/public/platform/task_type.h"
@@ -333,6 +334,20 @@ bool ExecutionContext::FeaturePolicyFeatureObserved(
   return false;
 }
 
+void ExecutionContext::FeaturePolicyPotentialBehaviourChangeObserved(
+    mojom::blink::FeaturePolicyFeature feature) const {
+  size_t feature_index = static_cast<size_t>(feature);
+  if (feature_policy_behaviour_change_counted_.size() == 0) {
+    feature_policy_behaviour_change_counted_.resize(
+        static_cast<size_t>(mojom::blink::FeaturePolicyFeature::kMaxValue) + 1);
+  } else if (feature_policy_behaviour_change_counted_[feature_index]) {
+    return;
+  }
+  feature_policy_behaviour_change_counted_[feature_index] = true;
+  UMA_HISTOGRAM_ENUMERATION(
+      "Blink.UseCounter.FeaturePolicy.ProposalWouldChangeBehaviour", feature);
+}
+
 bool ExecutionContext::IsFeatureEnabled(
     mojom::blink::FeaturePolicyFeature feature,
     ReportOptions report_on_failure,
@@ -364,6 +379,21 @@ bool ExecutionContext::IsFeatureEnabled(
   base::Optional<mojom::FeaturePolicyDisposition> disposition;
   bool enabled = GetSecurityContext().IsFeatureEnabled(feature, threshold_value,
                                                        &disposition);
+
+  if (enabled) {
+    // Report if the proposed header semantics change would have affected the
+    // outcome. (https://crbug.com/937131)
+    const FeaturePolicy* policy = GetSecurityContext().GetFeaturePolicy();
+    url::Origin origin = GetSecurityOrigin()->ToUrlOrigin();
+    if (policy->GetProposedFeatureValueForOrigin(feature, origin) <
+        threshold_value) {
+      // Count that there was a change in this page load.
+      const_cast<ExecutionContext*>(this)->CountUse(
+          WebFeature::kFeaturePolicyProposalWouldChangeBehaviour);
+      // Record the specific feature whose behaviour was changed.
+      FeaturePolicyPotentialBehaviourChangeObserved(feature);
+    }
+  }
 
   if (disposition && report_on_failure == ReportOptions::kReportOnFailure)
     ReportFeaturePolicyViolation(feature, *disposition, message, source_file);
