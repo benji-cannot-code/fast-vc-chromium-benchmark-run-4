e6537ca532a9f939047ee2d4ccdac3af128fa521
Jason Lin <lxj@google.com>
2020-02-03 05:35:27 +0000

    crostini: select the correct action button
    A recent CL [1] added a second button with the class "action-button" to
    the crostini installer. The existing logic assumed there is only one
    "action-button" which, being no longer always true, sometimes results in
    the wrong button being focussed/clicked. This CL updates the logic to
    select the appropriate button, either next or install, depending on the
    current state of the installation ui.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2004185
    
    Bug: 1016195
    Change-Id: I94cbba0f7610d9f444ee539ddcc910959ee817c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024232
    Reviewed-by: Julian Watson <juwa@google.com>
    Commit-Queue: Jason Lin <lxj@google.com>
    Auto-Submit: Jason Lin <lxj@google.com>
    Cr-Commit-Position: refs/heads/master@{#737675}

--- chrome/browser/resources/chromeos/crostini_installer/app.js
+++ chrome/browser/resources/chromeos/crostini_installer/app.js
@@ -150,7 +150,7 @@ Polymer({
     });
 
     BrowserProxy.getInstance().handler.requestAmountOfFreeDiskSpace();
-    this.$$('.action-button').focus();
+    this.$$('.action-button:not([hidden])').focus();
   },
 
   /** @override */
