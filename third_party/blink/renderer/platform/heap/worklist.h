38383534d3286021160061f52a3e65c4c03e162a
Omer Katz <omerkatz@chromium.org>
2020-02-03 16:37:21 +0000

    heap: Remove worklist(int) ctor
    This CL removes the option to initialize a worklist to less than
    max_tasks. Having a static number of tasks allows replacing some
    DCHECKs with static_asserts.
    
    Bug: 986235
    Change-Id: Ia14792bd946f8ef08606138b56617f90be9cc6ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2030970
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737799}

--- third_party/blink/renderer/platform/heap/worklist.h
+++ third_party/blink/renderer/platform/heap/worklist.h
@@ -32,13 +32,15 @@ namespace blink {
 //
 // Work stealing is best effort, i.e., there is no way to inform other tasks
 // of the need of items.
-template <typename _EntryType, int segment_size, int max_tasks = 2>
+template <typename _EntryType, int segment_size, int num_tasks = 2>
 class Worklist {
   USING_FAST_MALLOC(Worklist);
-  using WorklistType = Worklist<_EntryType, segment_size, max_tasks>;
+  using WorklistType = Worklist<_EntryType, segment_size, num_tasks>;
 
  public:
   using EntryType = _EntryType;
+
+  static constexpr int kNumTasks = num_tasks;
 
   class View {
     DISALLOW_NEW();
@@ -81,11 +83,8 @@ class Worklist {
 
   static constexpr size_t kSegmentCapacity = segment_size;
 
-  Worklist() : Worklist(max_tasks) {}
-
-  explicit Worklist(int num_tasks) : num_tasks_(num_tasks) {
-    CHECK_LE(num_tasks, max_tasks);
-    for (int i = 0; i < num_tasks_; i++) {
+  Worklist() {
+    for (int i = 0; i < kNumTasks; i++) {
       private_push_segment(i) = NewSegment();
       private_pop_segment(i) = NewSegment();
     }
@@ -93,7 +92,7 @@ class Worklist {
 
   ~Worklist() {
     CHECK(IsGlobalEmpty());
-    for (int i = 0; i < num_tasks_; i++) {
+    for (int i = 0; i < kNumTasks; i++) {
       DCHECK(private_push_segment(i));
       DCHECK(private_pop_segment(i));
       delete private_push_segment(i);
@@ -102,7 +101,7 @@ class Worklist {
   }
 
   bool Push(int task_id, EntryType entry) {
-    DCHECK_LT(task_id, num_tasks_);
+    DCHECK_LT(task_id, kNumTasks);
     DCHECK(private_push_segment(task_id));
     if (!private_push_segment(task_id)->Push(entry)) {
       PublishPushSegmentToGlobal(task_id);
@@ -114,7 +113,7 @@ class Worklist {
   }
 
   bool Pop(int task_id, EntryType* entry) {
-    DCHECK_LT(task_id, num_tasks_);
+    DCHECK_LT(task_id, kNumTasks);
     DCHECK(private_pop_segment(task_id));
     if (!private_pop_segment(task_id)->Pop(entry)) {
       if (!private_push_segment(task_id)->IsEmpty()) {
@@ -139,7 +138,7 @@ class Worklist {
   bool IsGlobalPoolEmpty() const { return global_pool_.IsEmpty(); }
 
   bool IsGlobalEmpty() const {
-    for (int i = 0; i < num_tasks_; i++) {
+    for (int i = 0; i < kNumTasks; i++) {
       if (!IsLocalEmpty(i))
         return false;
     }
@@ -166,7 +165,7 @@ class Worklist {
   //
   // Assumes that no other tasks are running.
   void Clear() {
-    for (int i = 0; i < num_tasks_; i++) {
+    for (int i = 0; i < kNumTasks; i++) {
       private_pop_segment(i)->Clear();
       private_push_segment(i)->Clear();
     }
@@ -183,7 +182,7 @@ class Worklist {
   // Assumes that no other tasks are running.
   template <typename Callback>
   void Update(Callback callback) {
-    for (int i = 0; i < num_tasks_; i++) {
+    for (int i = 0; i < kNumTasks; i++) {
       private_pop_segment(i)->Update(callback);
       private_push_segment(i)->Update(callback);
     }
@@ -203,8 +202,6 @@ class Worklist {
   void MergeGlobalPool(Worklist* other) {
     global_pool_.Merge(&other->global_pool_);
   }
-
-  int num_tasks() const { return num_tasks_; }
 
  private:
   FRIEND_TEST_ALL_PREFIXES(WorklistTest, SegmentCreate);
@@ -448,9 +445,8 @@ class Worklist {
     return new Segment();
   }
 
-  PrivateSegmentHolder private_segments_[max_tasks];
+  PrivateSegmentHolder private_segments_[kNumTasks];
   GlobalPool global_pool_;
-  int num_tasks_;
 };
 
 }  // namespace blink
