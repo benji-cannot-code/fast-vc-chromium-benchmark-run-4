aef1b167af155ebddc328bc8378c3e72dacc0653
Andres Calderon Jaramillo <andrescj@chromium.org>
2020-02-03 22:15:43 +0000

    media: Create a fuzzer for VaapiJpegDecoder.
    This CL creates a fuzzer for media::VaapiJpegDecoder. This component
    exposes an interface that takes raw JPEG data and uses libva to decode
    it using the hardware.
    
    Unfortunately, this interface is too simple: most fuzzing inputs will
    probably be invalid because they will fail at the parsing stage even
    before getting to libva. This is not useful because we already have a
    fuzzer for the media::JpegParser.
    
    Instead, we make this fuzzer more structure-aware so that we can inject
    more diverse inputs into the driver and exercise more code paths. The
    rationale is that the driver should fail cleanly on invalid inputs. To
    accomplish this, this CL:
    
    1) Extracts the libva-poking functionality of the VaapiJpegDecoder into
       two private methods: MaybeCreateSurface() and SubmitBuffers().
    
    2) Defines a protobuf that mirrors the structure of the parameters of
       these two methods.
    
    3) Uses libprotobuf-mutator to generate structured data that can be
       directly converted into the input for the two methods above.
    
    Bug: 1028051,1034357
    Test: ./vaapi_jpeg_decoder_fuzzertest pixel-1280x720.textproto on sarien
    Change-Id: I9a1c957558d05343bdc0f371cd68adb2975ad7da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1967203
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737945}

--- media/gpu/vaapi/vaapi_jpeg_decoder.h
+++ media/gpu/vaapi/vaapi_jpeg_decoder.h
@@ -14,8 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vaapi/vaapi_image_decoder.h"
 
 namespace media {
+namespace fuzzing {
+class VaapiJpegDecoderWrapper;
+}  // namespace fuzzing
 
 struct JpegFrameHeader;
+struct JpegParseResult;
 class ScopedVAImage;
 
 // Returns the internal format required for a JPEG image given its parsed
@@ -41,9 +45,20 @@ class VaapiJpegDecoder : public VaapiImageDecoder {
                                           VaapiImageDecodeStatus* status);
 
  private:
+  friend class fuzzing::VaapiJpegDecoderWrapper;
+
   // VaapiImageDecoder implementation.
   VaapiImageDecodeStatus AllocateVASurfaceAndSubmitVABuffers(
       base::span<const uint8_t> encoded_image) override;
+
+  // AllocateVASurfaceAndSubmitVABuffers() is implemented by calling the
+  // following methods. They are here so that a fuzzer can inject (almost)
+  // arbitrary data into libva by skipping the parsing and image support checks
+  // in AllocateVASurfaceAndSubmitVABuffers().
+  bool MaybeCreateSurface(unsigned int picture_va_rt_format,
+                          const gfx::Size& new_coded_size,
+                          const gfx::Size& new_visible_size);
+  bool SubmitBuffers(const JpegParseResult& parse_result);
 
   DISALLOW_COPY_AND_ASSIGN(VaapiJpegDecoder);
 };
