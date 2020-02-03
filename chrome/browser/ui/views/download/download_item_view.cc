c9a51752b1b03ccfc58e1e379cf42dd6b94395c6
Daniel Rubery <drubery@chromium.org>
2020-02-03 17:57:25 +0000

    Use larger size for icon in Advanced Protection scanning prompt.
    Upon feedback from UX, we should be using GetErrorIconSize() in the APP
    deep scanning prompts.
    
    Bug: 1048199
    Screenshot: https://screenshot.googleplex.com/CxJwRFQ66UQ.png
    Change-Id: I9eba847f297520b1f3c5879bf8f748c0c69ad9f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033753
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737829}

--- chrome/browser/ui/views/download/download_item_view.cc
+++ chrome/browser/ui/views/download/download_item_view.cc
@@ -1242,7 +1242,7 @@ gfx::ImageSkia DownloadItemView::GetWarningIcon() {
 
     case download::DOWNLOAD_DANGER_TYPE_PROMPT_FOR_SCANNING:
       return gfx::CreateVectorIcon(
-          vector_icons::kHelpIcon, GetWarningIconSize(),
+          vector_icons::kHelpIcon, GetErrorIconSize(),
           GetNativeTheme()->GetSystemColor(
               ui::NativeTheme::kColorId_DefaultIconColor));
 
