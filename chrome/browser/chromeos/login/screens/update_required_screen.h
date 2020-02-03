d5a597ea1758d6fe6f5ca41f5f05fa285fc333be
Saurabh Nijhara <snijhara@google.com>
2020-02-03 13:06:10 +0000

    Adding browser test for update required screen
    This CL adds a new browser test for captive portal on the update required screen.
    
    Bug: 944838
    Change-Id: Iaf40f8668e762dd33cf5f80372f947a5bed60aae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2019108
    Commit-Queue: Saurabh Nijhara <snijhara@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737746}

--- chrome/browser/chromeos/login/screens/update_required_screen.h
+++ chrome/browser/chromeos/login/screens/update_required_screen.h
@@ -67,6 +67,8 @@ class UpdateRequiredScreen : public BaseScreen,
   // Set a base clock (used to set current time) for testing EOL.
   void SetClockForTesting(base::Clock* clock);
 
+  void SetErrorMessageDelayForTesting(const base::TimeDelta& delay);
+
  private:
   void EnsureScreenIsShown();
 
@@ -125,6 +127,8 @@ class UpdateRequiredScreen : public BaseScreen,
 
   // Overridden for testing EOL by setting the current time.
   base::Clock* clock_;
+
+  base::TimeDelta error_message_delay_;
 
   ErrorScreen::ConnectRequestCallbackSubscription connect_request_subscription_;
 
