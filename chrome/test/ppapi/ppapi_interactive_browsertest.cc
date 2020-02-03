36a7beba9960d78660153ac392c3e7f78745d30d
Marc Treib <treib@chromium.org>
2020-02-03 14:22:18 +0000

    Disable OutOfProcessPPAPITest.MouseLock_SucceedWhenAllowed on Win,Mac
    TBR=lukasza@chromium.org
    
    Bug: 1048148
    Change-Id: I7393ce564ea09535810cf2b77c7854b05af1e1f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033770
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737760}

--- chrome/test/ppapi/ppapi_interactive_browsertest.cc
+++ chrome/test/ppapi/ppapi_interactive_browsertest.cc
@@ -15,12 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Disable tests under ASAN.  http://crbug.com/104832.
 // This is a bit heavy handed, but the majority of these tests fail under ASAN.
 // See bug for history.
-#if defined(ADDRESS_SANITIZER)
+// Flaky on Win/Mac, http://crbug.com/1048148.
+#if defined(ADDRESS_SANITIZER) || defined(OS_WIN) || defined(OS_MACOSX)
 #define MAYBE_MouseLock_SucceedWhenAllowed DISABLED_MouseLock_SucceedWhenAllowed
 #else
 #define MAYBE_MouseLock_SucceedWhenAllowed MouseLock_SucceedWhenAllowed
 #endif  // ADDRESS_SANITIZER
-// Disabled due to timeouts: http://crbug.com/136548
 IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest,
                        MAYBE_MouseLock_SucceedWhenAllowed) {
   RunTestViaHTTP("MouseLock_SucceedWhenAllowed");
