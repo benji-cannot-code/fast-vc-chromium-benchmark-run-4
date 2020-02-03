a2421e640c7f677b9730fee56aee4dd383ca670f
yilkal <yilkal@chromium.org>
2020-02-03 22:24:19 +0000

    Compute web active running time
    This CL ensures that chrome and web apps have a shared time limit
    against which to count their time usage.
    
    Bug: 1034551
    Change-Id: Ia6b647dc8e5151dd95ddeeb96b29c9791ba1ce1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033848
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737951}

--- chrome/browser/chromeos/child_accounts/time_limits/app_time_limit_utils.h
+++ chrome/browser/chromeos/child_accounts/time_limits/app_time_limit_utils.h
@@ -10,8 +10,12 @@ namespace chromeos {
 namespace app_time {
 
 class AppId;
+enum class AppState;
 
 AppId GetChromeAppId();
+
+// Returns true if the application shares chrome's time limit.
+bool ContributesToWebTimeLimit(const AppId& app_id, AppState app_state);
 
 }  // namespace app_time
 }  // namespace chromeos
