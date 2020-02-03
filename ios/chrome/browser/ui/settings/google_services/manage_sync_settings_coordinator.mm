ecd2e6ff3ba08e6241fdc30d410f379fc13218b3
Nazerke <nazerke@google.com>
2020-02-03 12:34:30 +0000

    [iOS][coordinator] Modernize Manage Sync Settings Coordinator.
    This CL modernizes the ManageSyncSettingsCoordinator to remove the
    public property for dispatcher and to update the coordinators
    implementation to get it from self.browser.
    
    Bug: 1029346
    Change-Id: I395a6235682f3983126dd546ca11f2b3192e5e7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033258
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737738}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
@@ -14,12 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
+#import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/signin/authentication_service.h"
 #import "ios/chrome/browser/signin/authentication_service_factory.h"
 #include "ios/chrome/browser/sync/profile_sync_service_factory.h"
 #include "ios/chrome/browser/sync/sync_observer_bridge.h"
 #include "ios/chrome/browser/sync/sync_setup_service_factory.h"
 #import "ios/chrome/browser/ui/commands/application_commands.h"
+#import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/ui/icons/chrome_icon.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_command_handler.h"
@@ -61,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation ManageSyncSettingsCoordinator
 
 - (void)start {
-  DCHECK(self.dispatcher);
   DCHECK(self.navigationController);
   self.mediator = [[ManageSyncSettingsMediator alloc]
       initWithSyncService:self.syncService
@@ -116,7 +117,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     viewController:(UIViewController*)viewController {
   OpenNewTabCommand* command =
       [OpenNewTabCommand commandWithURLFromChrome:net::GURLWithNSURL(url)];
-  [self.dispatcher closeSettingsUIAndOpenURL:command];
+  id<ApplicationCommands> handler = HandlerForProtocol(
+      self.browser->GetCommandDispatcher(), ApplicationCommands);
+  [handler closeSettingsUIAndOpenURL:command];
 }
 
 #pragma mark - ManageSyncSettingsCommandHandler
@@ -133,7 +136,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     controllerToPush = [[SyncEncryptionTableViewController alloc]
         initWithBrowserState:self.browserState];
   }
-  controllerToPush.dispatcher = self.dispatcher;
+  // TODO(crbug.com/1045047): Use HandlerForProtocol after commands protocol
+  // clean up.
+  controllerToPush.dispatcher = static_cast<
+      id<ApplicationCommands, BrowserCommands, BrowsingDataCommands>>(
+      self.browser->GetCommandDispatcher());
   [self.navigationController pushViewController:controllerToPush animated:YES];
 }
 
@@ -155,7 +162,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       GURL(kSyncGoogleDashboardURL),
       GetApplicationContext()->GetApplicationLocale());
   OpenNewTabCommand* command = [OpenNewTabCommand commandWithURLFromChrome:url];
-  [self.dispatcher closeSettingsUIAndOpenURL:command];
+  id<ApplicationCommands> handler = HandlerForProtocol(
+      self.browser->GetCommandDispatcher(), ApplicationCommands);
+  [handler closeSettingsUIAndOpenURL:command];
 }
 
 #pragma mark - SyncObserverModelBridge
