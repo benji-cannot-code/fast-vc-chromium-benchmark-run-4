a878ae0a1dccb52189ddfc657cd0b3bdf3f895c4
Peng Huang <penghuang@chromium.org>
2020-02-03 16:28:04 +0000

    Fix tiling issue with AMD GPU
    The AMD GPU GL driver will only use optimal tiling for a memory
    object, if the memory object is dedicated - the memory object
    only contains one image. We always create dedicated memory objects
    with Vulkan API, however GL doesn't know it. We need to tell GL
    driver about it.
    
    Bug: 1046462
    Change-Id: I4ae6b69f5f8df12d58d5355c5532f974d45b17a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2034033
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737795}

--- ui/gl/gl_bindings_autogen_gl.cc
+++ ui/gl/gl_bindings_autogen_gl.cc
@@ -1994,6 +1994,12 @@ void DriverGL::InitializeDynamicBindings(const GLVersionInfo* ver,
         GetGLProcAddress("glMemoryBarrierEXT"));
   }
 
+  if (ext.b_GL_EXT_memory_object) {
+    fn.glMemoryObjectParameterivEXTFn =
+        reinterpret_cast<glMemoryObjectParameterivEXTProc>(
+            GetGLProcAddress("glMemoryObjectParameterivEXT"));
+  }
+
   if (ver->IsAtLeastGLES(3u, 2u) || ver->IsAtLeastGL(4u, 0u)) {
     fn.glMinSampleShadingFn = reinterpret_cast<glMinSampleShadingProc>(
         GetGLProcAddress("glMinSampleShading"));
@@ -4803,6 +4809,12 @@ void GLApiBase::glMemoryBarrierByRegionFn(GLbitfield barriers) {
 
 void GLApiBase::glMemoryBarrierEXTFn(GLbitfield barriers) {
   driver_->fn.glMemoryBarrierEXTFn(barriers);
+}
+
+void GLApiBase::glMemoryObjectParameterivEXTFn(GLuint memoryObject,
+                                               GLenum pname,
+                                               const GLint* param) {
+  driver_->fn.glMemoryObjectParameterivEXTFn(memoryObject, pname, param);
 }
 
 void GLApiBase::glMinSampleShadingFn(GLfloat value) {
@@ -8413,6 +8425,14 @@ void TraceGLApi::glMemoryBarrierByRegionFn(GLbitfield barriers) {
 void TraceGLApi::glMemoryBarrierEXTFn(GLbitfield barriers) {
   TRACE_EVENT_BINARY_EFFICIENT0("gpu", "TraceGLAPI::glMemoryBarrierEXT")
   gl_api_->glMemoryBarrierEXTFn(barriers);
+}
+
+void TraceGLApi::glMemoryObjectParameterivEXTFn(GLuint memoryObject,
+                                                GLenum pname,
+                                                const GLint* param) {
+  TRACE_EVENT_BINARY_EFFICIENT0("gpu",
+                                "TraceGLAPI::glMemoryObjectParameterivEXT")
+  gl_api_->glMemoryObjectParameterivEXTFn(memoryObject, pname, param);
 }
 
 void TraceGLApi::glMinSampleShadingFn(GLfloat value) {
@@ -12972,6 +12992,15 @@ void LogGLApi::glMemoryBarrierEXTFn(GLbitfield barriers) {
   gl_api_->glMemoryBarrierEXTFn(barriers);
 }
 
+void LogGLApi::glMemoryObjectParameterivEXTFn(GLuint memoryObject,
+                                              GLenum pname,
+                                              const GLint* param) {
+  GL_SERVICE_LOG("glMemoryObjectParameterivEXT"
+                 << "(" << memoryObject << ", " << GLEnums::GetStringEnum(pname)
+                 << ", " << static_cast<const void*>(param) << ")");
+  gl_api_->glMemoryObjectParameterivEXTFn(memoryObject, pname, param);
+}
+
 void LogGLApi::glMinSampleShadingFn(GLfloat value) {
   GL_SERVICE_LOG("glMinSampleShading"
                  << "(" << value << ")");
@@ -16888,6 +16917,12 @@ void NoContextGLApi::glMemoryBarrierByRegionFn(GLbitfield barriers) {
 
 void NoContextGLApi::glMemoryBarrierEXTFn(GLbitfield barriers) {
   NoContextHelper("glMemoryBarrierEXT");
+}
+
+void NoContextGLApi::glMemoryObjectParameterivEXTFn(GLuint memoryObject,
+                                                    GLenum pname,
+                                                    const GLint* param) {
+  NoContextHelper("glMemoryObjectParameterivEXT");
 }
 
 void NoContextGLApi::glMinSampleShadingFn(GLfloat value) {
--- ui/gl/gl_bindings_autogen_mock.cc
+++ ui/gl/gl_bindings_autogen_mock.cc
@@ -3181,6 +3181,14 @@ MockGLInterface::Mock_glMemoryBarrierEXT(GLbitfield barriers) {
   interface_->MemoryBarrierEXT(barriers);
 }
 
+void GL_BINDING_CALL
+MockGLInterface::Mock_glMemoryObjectParameterivEXT(GLuint memoryObject,
+                                                   GLenum pname,
+                                                   const GLint* param) {
+  MakeGlMockFunctionUnique("glMemoryObjectParameterivEXT");
+  interface_->MemoryObjectParameterivEXT(memoryObject, pname, param);
+}
+
 void GL_BINDING_CALL MockGLInterface::Mock_glMinSampleShading(GLfloat value) {
   MakeGlMockFunctionUnique("glMinSampleShading");
   interface_->MinSampleShading(value);
@@ -6088,6 +6096,9 @@ MockGLInterface::GetGLProcAddress(const char* name) {
         Mock_glMemoryBarrierByRegion);
   if (strcmp(name, "glMemoryBarrierEXT") == 0)
     return reinterpret_cast<GLFunctionPointerType>(Mock_glMemoryBarrierEXT);
+  if (strcmp(name, "glMemoryObjectParameterivEXT") == 0)
+    return reinterpret_cast<GLFunctionPointerType>(
+        Mock_glMemoryObjectParameterivEXT);
   if (strcmp(name, "glMinSampleShading") == 0)
     return reinterpret_cast<GLFunctionPointerType>(Mock_glMinSampleShading);
   if (strcmp(name, "glMultiDrawArraysANGLE") == 0)
