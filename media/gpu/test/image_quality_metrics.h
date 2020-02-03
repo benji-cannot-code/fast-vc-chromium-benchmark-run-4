19e80055b82e39a9309341c7bb54ec56d9276270
Hirokazu Honda <hiroh@chromium.org>
2020-02-03 04:18:12 +0000

    media/gpu/test: Move CompareFramesWithErrorDiff out of VideoFrameHelpers
    Bug: 1044452
    Test: compile image_processor_test
    Change-Id: I474c94707f16ef44a495868a8ef8e380fbbb0a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024391
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737666}

---
+++ media/gpu/test/image_quality_metrics.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_GPU_TEST_IMAGE_QUALITY_METRICS_H_
+#define MEDIA_GPU_TEST_IMAGE_QUALITY_METRICS_H_
+
+#include <stdint.h>
+
+namespace media {
+
+class VideoFrame;
+
+namespace test {
+
+// Compare each byte of two VideoFrames, |frame1| and |frame2|, allowing an
+// error up to |tolerance|. Return the number of bytes of which the difference
+// is more than |tolerance|.
+size_t CompareFramesWithErrorDiff(const VideoFrame& frame1,
+                                  const VideoFrame& frame2,
+                                  uint8_t tolerance);
+}  // namespace test
+}  // namespace media
+
+#endif  // MEDIA_GPU_TEST_IMAGE_QUALITY_METRICS_H_
