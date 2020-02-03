be66621d6c7b5bce3d87c15786ca42a3df736c91
Becca Hughes <beccahughes@chromium.org>
2020-02-03 18:45:18 +0000

    [Media History] Make URL unique for playback sessions
    For playback sessions (the continue watching table)
    we actually just want the most recent playback for
    a URL so we can make it unique.
    
    BUG=1041662
    
    Change-Id: Ib7ad1d9f276ffe1b669fff36aa3154daf6b23ffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033908
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737851}

--- chrome/browser/media/history/media_history_session_table.cc
+++ chrome/browser/media/history/media_history_session_table.cc
@@ -32,7 +32,7 @@ sql::InitStatus MediaHistorySessionTable::CreateTableIfNonExistent() {
       DB()->Execute(base::StringPrintf("CREATE TABLE IF NOT EXISTS %s("
                                        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
                                        "origin_id INTEGER NOT NULL,"
-                                       "url TEXT,"
+                                       "url TEXT NOT NULL UNIQUE,"
                                        "duration_ms INTEGER,"
                                        "position_ms INTEGER,"
                                        "last_updated_time_s BIGINT NOT NULL,"
@@ -77,7 +77,7 @@ bool MediaHistorySessionTable::SavePlaybackSession(
   sql::Statement statement(DB()->GetCachedStatement(
       SQL_FROM_HERE,
       base::StringPrintf(
-          "INSERT INTO %s "
+          "REPLACE INTO %s "
           "(origin_id, url, duration_ms, position_ms, last_updated_time_s, "
           "title, artist, album, source_title) "
           "VALUES "
@@ -105,7 +105,12 @@ bool MediaHistorySessionTable::SavePlaybackSession(
   statement.BindString16(7, metadata.album);
   statement.BindString16(8, metadata.source_title);
 
-  return statement.Run();
+  if (statement.Run()) {
+    DCHECK(DB()->GetLastInsertRowId());
+    return true;
+  }
+
+  return false;
 }
 
 base::Optional<MediaHistoryStore::MediaPlaybackSessionList>
@@ -122,16 +127,9 @@ MediaHistorySessionTable::GetPlaybackSessions(
                          kTableName)
           .c_str()));
 
-  std::set<GURL> previous_urls;
   MediaHistoryStore::MediaPlaybackSessionList sessions;
 
   while (statement.Step()) {
-    // Check if we already have a playback session for this url.
-    GURL url(statement.ColumnString(0));
-    if (base::Contains(previous_urls, url))
-      continue;
-    previous_urls.insert(url);
-
     auto duration = base::TimeDelta::FromMilliseconds(statement.ColumnInt64(1));
     auto position = base::TimeDelta::FromMilliseconds(statement.ColumnInt64(2));
 
@@ -140,7 +138,7 @@ MediaHistorySessionTable::GetPlaybackSessions(
       continue;
 
     MediaHistoryStore::MediaPlaybackSession session;
-    session.url = url;
+    session.url = GURL(statement.ColumnString(0));
     session.duration = duration;
     session.position = position;
     session.metadata.title = statement.ColumnString16(3);
