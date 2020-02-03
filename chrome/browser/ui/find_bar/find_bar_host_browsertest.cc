a1da3364e58a53ba9e85102a540125169d0b4307
Finnur Thorarinsson <finnur@chromium.org>
2020-02-03 14:31:45 +0000

    Speculative fix for two FindInPageController tests.
    FindInPageControllerTest.FindMovesOnTabClose_Issue1343052
    FindInPageControllerTest.FindMovesWhenObscuring
    
    This CL effectively reverts two CLs:
    https://chromium-review.googlesource.com/c/chromium/src/+/2031539
    https://chromium-review.googlesource.com/c/chromium/src/+/2028737
    
    If the test fails as a result of this CL then the view.cc changes are
    not to blame for and this CL should be reverted.
    
    TBR: kylixrd
    Bug: 1041614, 1047427
    Change-Id: I66191af28e988ded2a304fa14e2254c1248257be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2035372
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737763}

--- chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
+++ chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
@@ -952,9 +952,7 @@ IN_PROC_BROWSER_TEST_F(FindInPageControllerTest,
 }
 
 // Make sure Find box moves out of the way if it is obscuring the active match.
-// crbug.com/1047427: disabled due to flakiness
-IN_PROC_BROWSER_TEST_F(FindInPageControllerTest,
-                       DISABLED_FindMovesWhenObscuring) {
+IN_PROC_BROWSER_TEST_F(FindInPageControllerTest, FindMovesWhenObscuring) {
   GURL url = GetURL(kMoveIfOver);
   ui_test_utils::NavigateToURL(browser(), url);
 
@@ -1393,9 +1391,8 @@ IN_PROC_BROWSER_TEST_F(FindInPageControllerTest, FitWindow) {
             popup->window()->GetBounds().width());
 }
 
-// crbug.com/1047427: disabled due to flakiness.
 IN_PROC_BROWSER_TEST_F(FindInPageControllerTest,
-                       DISABLED_FindMovesOnTabClose_Issue1343052) {
+                       FindMovesOnTabClose_Issue1343052) {
   EnsureFindBoxOpen();
   content::RunAllPendingInMessageLoop();  // Needed on Linux.
 
