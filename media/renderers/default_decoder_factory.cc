24062660f7e98b3fc00074b78b6fb298d00d21e0
Hirokazu Honda <hiroh@chromium.org>
2020-02-03 05:42:24 +0000

    media/filter: Implement Gav1VideoDecoder
    This introduces VideoDecoder using libgav1 decoder.
    The VideoDecoder is neither enabled nor compiled by default.
    Adding enable_libgav1_video_decoder to gn args is needed to compile,
    and adding --enable-features=Gav1VideoDecoder to chrome option is
    needed to enable the VideoDecoder on chrome.
    The performance differences between Dav1dVideoDecoder and Gav1VideoDecoder
    on kevin are below.
    
    1080p@30 | CPU usage(%) | Drop frame rate(%)
    Dav1d    | 64.0         | 33.7
    Gav1     | 60.4         | 39.0
    
    720p@30  | CPU usage(%) | Drop frame rate(%)
    Dav1d    | 40.52        | 0.0
    Gav1     | 35.85        | 0.0
    
    720p@60  | CPU usage(%) | Drop frame rate(%)
    Dav1d    | 55.50        | 24.8
    Gav1     | 43.7         | 48.45
    
    Bug: 1026522
    Test: Play av1 videos on kevin (ChromeOS ARM) and confirmed Libgav1VD was used.
    Change-Id: I24a83932936f775c364761c88b14c42a29537417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1936445
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: James Zern <jzern@google.com>
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737678}

--- media/renderers/default_decoder_factory.cc
+++ media/renderers/default_decoder_factory.cc
@@ -49,6 +49,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/filters/vpx_video_decoder.h"
 #endif
 
+#if BUILDFLAG(ENABLE_LIBGAV1_DECODER)
+#include "media/filters/gav1_video_decoder.h"
+#endif
+
 namespace media {
 
 DefaultDecoderFactory::DefaultDecoderFactory(
@@ -133,12 +137,20 @@ void DefaultDecoderFactory::CreateVideoDecoders(
   video_decoders->push_back(std::make_unique<OffloadingVpxVideoDecoder>());
 #endif
 
+#if BUILDFLAG(ENABLE_LIBGAV1_DECODER)
+  if (base::FeatureList::IsEnabled(kGav1VideoDecoder)) {
+    video_decoders->push_back(
+        std::make_unique<OffloadingGav1VideoDecoder>(media_log));
+  } else
+#endif  // BUILDFLAG(ENABLE_LIBGAV1_DECODER)
+  {
 #if BUILDFLAG(ENABLE_DAV1D_DECODER)
-  video_decoders->push_back(
-      std::make_unique<OffloadingDav1dVideoDecoder>(media_log));
+    video_decoders->push_back(
+        std::make_unique<OffloadingDav1dVideoDecoder>(media_log));
 #elif BUILDFLAG(ENABLE_LIBAOM_DECODER)
   video_decoders->push_back(std::make_unique<AomVideoDecoder>(media_log));
 #endif
+  }
 
 #if BUILDFLAG(ENABLE_FFMPEG_VIDEO_DECODERS)
   video_decoders->push_back(std::make_unique<FFmpegVideoDecoder>(media_log));
