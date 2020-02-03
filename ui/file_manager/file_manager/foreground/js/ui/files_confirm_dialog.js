f439cd75f2136eb32cd4f8bf284eab2aae64a639
Noel Gordon <noel@chromium.org>
2020-02-03 05:26:03 +0000

    [filesapp] Make the FilesConfirmDialog more communicative
    Some users of FilesConfirmDialog want to know when the dialog is done
    (closed or cancelled). Add an optional this.doneCallback for that.
    
    Some need a call prior to opening the FilesConfirmDialog, so they can
    open a modal HTML <dialog> parent of the FilesConfirmDialog to reveal
    it. Add an optional this.showModalElement for that.
    
    No change in behavior, no new tests.
    
    Bug: 803259
    Change-Id: I36bdf382c3e97bf4c1eaae42c1bb35131271012c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2034386
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737672}

--- ui/file_manager/file_manager/foreground/js/ui/files_confirm_dialog.js
+++ ui/file_manager/file_manager/foreground/js/ui/files_confirm_dialog.js
@@ -8,6 +8,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 class FilesConfirmDialog extends cr.ui.dialogs.ConfirmDialog {
   /**
+   * @param {!HTMLElement} parentElement
+   */
+  constructor(parentElement) {
+    super(parentElement);
+
+    /**
+     * @type {?function()} showModalElement Optional call to show the
+     * modal <dialog> parent of |this| if needed.
+     * @public
+     */
+    this.showModalElement = null;
+
+    /**
+     * @type {?function()} doneCallback Optional callback when |this|
+     * is closed confirmed or cancelled via dialog buttons.
+     * @public
+     */
+    this.doneCallback = null;
+  }
+
+  /**
    * @protected
    * @override
    */
