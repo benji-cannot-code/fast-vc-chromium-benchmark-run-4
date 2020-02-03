9fe16c65ad2242e8cc6c02a2984d7fdbdfd3ea8b
James Cook <jamescook@chromium.org>
2020-02-03 19:20:16 +0000

    cros: Remove --use-unconsented-primary-account
    Replace the only check with a check for SplitSettingsSync. Enough
    features have been converted to work with the unconsented primary
    account that I don't need a separate flag for it any more.
    
    Bug: 1042400
    Test: existing signin tests
    Change-Id: Iaf3690027737fd0ac674dad20d7da6107bb6b1b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2035862
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737870}

--- chromeos/constants/chromeos_switches.h
+++ chromeos/constants/chromeos_switches.h
@@ -199,8 +199,6 @@ extern const char kWaitForInitialPolicyFetchForTest[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) extern const char kWakeOnWifiPacket[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const char kUnfilteredBluetoothDevices[];
-COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
-extern const char kUseUnconsentedPrimaryAccount[];
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -252,9 +250,6 @@ COMPONENT_EXPORT(CHROMEOS_CONSTANTS) bool IsArcCpuRestrictionDisabled();
 
 // Returns true if all Bluetooth devices in UI (System Tray/Settings Page.)
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) bool IsUnfilteredBluetoothDevicesEnabled();
-
-// See kUseUnconsentedPrimaryAccount.
-COMPONENT_EXPORT(CHROMEOS_CONSTANTS) bool UseUnconsentedPrimaryAccount();
 
 }  // namespace switches
 }  // namespace chromeos
