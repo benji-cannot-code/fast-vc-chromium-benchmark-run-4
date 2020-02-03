060f8dabd0a72cec9ac3378c43f4ed4656914586
rajendrant <rajendrant@chromium.org>
2020-02-03 20:46:12 +0000

    Record data use breakdown by resource type UKM metrics
    Records the image bytes, subframe image bytes, media bytes as UKM from
    the PLM infra.
    
    UKM privacy review doc:
    https://docs.google.com/document/d/133SsQTd3L5ryQaI-q1rZsh6EeTsE_HxDFlthjlXi4CU/
    
    Bug: 1047325
    Change-Id: Ib77da45c07a2f55bea77a9750b4c69c7584ecf5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032002
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737906}

--- chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/ukm_page_load_metrics_observer.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/template_url_service.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
+#include "media/base/mime_util.h"
 #include "net/base/load_timing_info.h"
 #include "net/http/http_response_headers.h"
 #include "services/metrics/public/cpp/metrics_utils.h"
@@ -265,6 +266,12 @@ void UkmPageLoadMetricsObserver::OnResourceDataUseObserved(
       js_decoded_bytes_ += resource->decoded_body_length;
       if (resource->decoded_body_length > js_max_decoded_bytes_)
         js_max_decoded_bytes_ = resource->decoded_body_length;
+    } else if (blink::IsSupportedImageMimeType(resource->mime_type)) {
+      image_total_bytes_ += resource->received_data_length;
+      if (!resource->is_main_frame_resource)
+        image_subframe_bytes_ += resource->received_data_length;
+    } else if (media::IsSupportedMediaMimeType(resource->mime_type)) {
+      media_bytes_ += resource->received_data_length;
     }
     if (resource->cache_type !=
         page_load_metrics::mojom::CacheType::kNotCached) {
@@ -399,6 +406,12 @@ void UkmPageLoadMetricsObserver::RecordTimingMetrics(
       ukm::GetExponentialBucketMin(js_decoded_bytes_, 10));
   builder.SetNet_JavaScriptMaxBytes(
       ukm::GetExponentialBucketMin(js_max_decoded_bytes_, 10));
+
+  builder.SetNet_ImageBytes(
+      ukm::GetExponentialBucketMin(image_total_bytes_, 1.3));
+  builder.SetNet_ImageSubframeBytes(
+      ukm::GetExponentialBucketMin(image_subframe_bytes_, 1.3));
+  builder.SetNet_MediaBytes(ukm::GetExponentialBucketMin(media_bytes_, 1.3));
 
   if (main_frame_timing_)
     ReportMainResourceTimingMetrics(timing, &builder);
