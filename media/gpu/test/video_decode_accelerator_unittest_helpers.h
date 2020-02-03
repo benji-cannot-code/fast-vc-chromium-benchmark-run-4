934835389afe7b3eccdc85abfa8968152a2bc136
Hirokazu Honda <hiroh@chromium.org>
2020-02-03 02:46:47 +0000

    media/gpu/test: EncodedDataHelper returns scoped_refptr<DecoderBuffer>
    EncodedDataHelper should return scoped_refptr<DecoderBuffer>
    rather than std::string as this is the input format required
    by VideoDecoder::Decode().
    
    Bug: 1044816
    Test: video.DecodeAccel.* on eve
    Change-Id: Iac47f4e710954efa1dbb7f8d48cdd56bc0f9c963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032543
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737660}

--- media/gpu/test/video_decode_accelerator_unittest_helpers.h
+++ media/gpu/test/video_decode_accelerator_unittest_helpers.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
+#include "media/base/decoder_buffer.h"
 #include "media/base/video_codecs.h"
 #include "media/base/video_frame.h"
 #include "media/base/video_types.h"
@@ -37,7 +38,7 @@ class EncodedDataHelper {
   // Compute and return the next fragment to be sent to the decoder, starting
   // from the current position in the stream, and advance the current position
   // to after the returned fragment.
-  std::string GetBytesForNextData();
+  scoped_refptr<DecoderBuffer> GetNextBuffer();
   static bool HasConfigInfo(const uint8_t* data,
                             size_t size,
                             VideoCodecProfile profile);
@@ -50,9 +51,9 @@ class EncodedDataHelper {
 
  private:
   // For h.264.
-  std::string GetBytesForNextFragment();
+  scoped_refptr<DecoderBuffer> GetNextFragment();
   // For VP8/9.
-  std::string GetBytesForNextFrame();
+  scoped_refptr<DecoderBuffer> GetNextFrame();
 
   // Helpers for GetBytesForNextFragment above.
   size_t GetBytesForNextNALU(size_t pos);
