b5baedd3f5e9a8c132e1fdab58a181a85b152542
Mila Green <milagreen@chromium.org>
2020-02-03 18:00:19 +0000

    updater: Add InstallFromDMG()
    This adds a new function to mount an updater DMG and run its
    installer script, as well as a test to verify that functionality.
    
    Bug: 1031552
    Change-Id: Ica62c90f2a613ed3bd6f1a0dd215dd431fdec58d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018150
    Commit-Queue: Mila Green <milagreen@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#737830}

---
+++ chrome/updater/mac/installer.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_UPDATER_MAC_INSTALLER_H_
+#define CHROME_UPDATER_MAC_INSTALLER_H_
+
+namespace base {
+class FilePath;
+}
+
+namespace updater {
+
+// Mounts the DMG specified by |dmg_file_path|. The install script located at
+// "/.install.sh" in the mounted volume is executed, and then the DMG is
+// un-mounted. Returns false if mounting the dmg or executing the script failed.
+bool InstallFromDMG(const base::FilePath& dmg_file_path);
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_MAC_INSTALLER_H_
--- chrome/updater/run_all_unittests.cc
+++ chrome/updater/run_all_unittests.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
+#include "chrome/common/chrome_paths.h"
 
 int main(int argc, char** argv) {
   base::TestSuite test_suite(argc, argv);
+  chrome::RegisterPathProvider();
   return base::LaunchUnitTests(
       argc, argv,
       base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
