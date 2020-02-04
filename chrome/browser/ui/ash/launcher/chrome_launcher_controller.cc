88767f97c4014ee69ad4479a3f662e2ed6f76d2a
nancy <nancylingwang@chromium.org>
2020-02-04 21:34:55 +0000

    Remove LauncherCrostiniAppUpdater.
    kAppServiceShelf will be removed in M82 release, so this is the
    pareparing for that. I will follow up to remove more stuff, then finally
    remove that flag.
    
    BUG=1016159
    
    Change-Id: I3da4b9e304a182e742f58cf59fc548818fefb13c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037172
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738342}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/launcher/internal_app_window_shelf_controller.h"
 #include "chrome/browser/ui/ash/launcher/launcher_arc_app_updater.h"
 #include "chrome/browser/ui/ash/launcher/launcher_controller_helper.h"
-#include "chrome/browser/ui/ash/launcher/launcher_crostini_app_updater.h"
 #include "chrome/browser/ui/ash/launcher/launcher_extension_app_updater.h"
 #include "chrome/browser/ui/ash/launcher/multi_profile_app_window_launcher_controller.h"
 #include "chrome/browser/ui/ash/launcher/multi_profile_browser_status_monitor.h"
@@ -1377,12 +1376,6 @@ void ChromeLauncherController::AttachProfile(Profile* profile_to_attach) {
       std::unique_ptr<LauncherAppUpdater> arc_app_updater(
           new LauncherArcAppUpdater(this, profile()));
       app_updaters_.push_back(std::move(arc_app_updater));
-    }
-
-    if (crostini::CrostiniFeatures::Get()->IsUIAllowed(profile())) {
-      std::unique_ptr<LauncherAppUpdater> crostini_app_updater(
-          new LauncherCrostiniAppUpdater(this, profile()));
-      app_updaters_.push_back(std::move(crostini_app_updater));
     }
   }
 
