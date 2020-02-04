da9592feae1f108cf04a4b5759f770506dcefbe1
Avery Musbach <amusbach@chromium.org>
2020-02-04 20:04:40 +0000

    overview: Remove default arguments to OverviewSession::AddItem
    Bug: None
    Change-Id: Ib901eeae6a357baaa9d25605aed71c58664b8dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033889
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738297}

--- ash/wm/overview/overview_session_unittest.cc
+++ ash/wm/overview/overview_session_unittest.cc
@@ -1843,7 +1843,8 @@ TEST_P(OverviewSessionTest, NoWindowsIndicatorAddItem) {
   EXPECT_TRUE(overview_session()->no_windows_widget_for_testing());
 
   overview_session()->AddItem(window.get(), /*reposition=*/true,
-                              /*animate=*/false);
+                              /*animate=*/false, /*ignored_items=*/{},
+                              /*index=*/0u);
   EXPECT_FALSE(overview_session()->no_windows_widget_for_testing());
 }
 
