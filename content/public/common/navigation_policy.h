8477caf9f4e01d5cd639695d6f97898214fa883a
Fergal Daly <fergal@chromium.org>
2020-02-04 03:15:16 +0000

    Reland "Stop reviving crashed RenderFrameHost. Get a new one instead."
    This relands https://crrev.com/c/1688957
    
    It also updates it make it controlled by a feature-flag and parameterizes
    all render frame hosts tests to try with the flag on and off.
    
    - https://crrev.com/c/2006996 lands some CHECKs to help debug
      https://crbug.com/1006814
    - https://crrev.com/c/2010511 lands some CHECKs to help debug
      https://crbug.com/1014212
    - https://crrev.com/c/2011585 adds a test for the bug in
      https://crbug.com/993701 however this bug is no longer triggered.
    
    
    > On same-site navigations, the browser reuses the current
    > RenderFrameHost, even if the current RenderFrameHost is crashed.
    >
    > Instead of reviving the crashed one, we should get a new RenderFrameHost.
    >
    > This can be seen as a small step in the direction of RenderDocument, which
    > intend to get a new RenderFrameHost every time, even if the previous
    > RenderFrameHost is not crashed.
    > https://docs.google.com/document/d/1C2VKkFRSc0kdmqjKan1G4NlNlxWZqE4Wam41FNMgnmA/edit
    
    
    Bug: 981339
    Change-Id: I0d89d65d2c12acaccd84454ecb4a1d00901f750b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006890
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738052}

--- content/public/common/navigation_policy.h
+++ content/public/common/navigation_policy.h
@@ -15,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // navigations.
 
 namespace content {
+
+CONTENT_EXPORT bool IsRenderDocumentEnabledForCrashedFrame();
+
 // Navigation type that affects the download decision and relevant metrics to be
 // reported at download-discovery time.
 //
