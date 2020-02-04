e4cfe599cd0ffd8b923e1a0658b3637a3cfa2721
Lijin Shen <lazzzis@google.com>
2020-02-04 19:11:24 +0000

    [duet] start test activity on blank page instead of ntp
    The tests are flaky on Android K & L. It shows the ntp is scrolled down
    on launching the activity, but the reason has not been figured out.
    Launching activity on blank page can prevent the startpage from being
    scrolled down.
    
    Bug: 1046317, 1046316, 1045729, 1045415
    Change-Id: I1d14f15218ad8a1e1ceac9c0a7bbfc1d49008700
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028212
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#738263}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabUiTestHelper.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabUiTestHelper.java
@@ -508,6 +508,7 @@ public class TabUiTestHelper {
             CriteriaHelper.pollUiThread(
                     Criteria.equals(mExpectedCount, () -> adapter.getItemCount()));
 
+            recyclerView.setItemAnimator(null); // Disable animation to reduce flakiness.
             for (int i = 0; i < mExpectedCount; i++) {
                 RecyclerView.ViewHolder viewHolder =
                         recyclerView.findViewHolderForAdapterPosition(i);
