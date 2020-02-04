e36eb4711bb4a7c0a75cf2be9ce094a465703bf4
Paul Moy <pmoy@chromium.org>
2020-02-04 17:38:33 +0000

    cros_healthd: add the AC power routine
    Add the first interactive routine, AC power, to cros_healthd.
    This routine checks the power supply's status and type against
    the routine inputs.
    
    --gtest_filter=CrosHealthdServiceConnectionTest.*
    
    Bug: chromium:1046452
    Test: chromeos_unittests
    Change-Id: I26a317c681781bed096513502240ba4663253fc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033809
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738213}

--- chromeos/services/cros_healthd/public/cpp/service_connection.h
+++ chromeos/services/cros_healthd/public/cpp/service_connection.h
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_SERVICES_CROS_HEALTHD_PUBLIC_CPP_SERVICE_CONNECTION_H_
 
 #include <cstdint>
+#include <string>
 
+#include "base/optional.h"
 #include "chromeos/services/cros_healthd/public/mojom/cros_healthd.mojom.h"
 
 namespace chromeos {
@@ -69,6 +71,15 @@ class ServiceConnection {
   // details.
   virtual void RunSmartctlCheckRoutine(
       mojom::CrosHealthdDiagnosticsService::RunSmartctlCheckRoutineCallback
+          callback) = 0;
+
+  // Requests that cros_healthd runs the AC power routine. See
+  // src/chromeos/service/cros_healthd/public/mojom/cros_healthd.mojom for
+  // details.
+  virtual void RunAcPowerRoutine(
+      mojom::AcPowerStatusEnum expected_status,
+      const base::Optional<std::string>& expected_power_type,
+      mojom::CrosHealthdDiagnosticsService::RunAcPowerRoutineCallback
           callback) = 0;
 
   // Gather pieces of information about the platform. See
