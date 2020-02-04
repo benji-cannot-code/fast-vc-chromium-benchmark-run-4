20f9a619fe88b4e88ce80f2481ff94b0aa28c77d
Samuel Huang <huangs@chromium.org>
2020-02-04 16:29:00 +0000

    Revert "[test] Clean up some incorrect use of ScopedRunTimeout in //chrome."
    This reverts commit 77cffbd1295b7b204310fae56f32ce7676f6544a.
    
    Reason for revert: This causes compile error in linux-chromeos-chrome.
    
    Original change's description:
    > [test] Clean up some incorrect use of ScopedRunTimeout in //chrome.
    > 
    > ScopedRunTimeout is intended to be used to fail tests if a loop is Run()
    > for too long, rather than to Run() the loop for a specific length of
    > time.
    > 
    > Clean up some tests which were using ScopedRunTimeout inappropriately.
    > 
    > Bug: 1021777
    > Change-Id: If6b2ee5f3eab498698d09fdee103e759fff51ead
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033445
    > Auto-Submit: Wez <wez@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Matt Menke <mmenke@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#738180}
    
    TBR=xiyuan@chromium.org,wez@chromium.org,gab@chromium.org,eroman@chromium.org,mmenke@chromium.org
    
    Change-Id: I919d3ce3bdf4430b05664849f8f57b31a21cc3df
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1021777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037315
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738194}

--- chrome/browser/net/network_context_configuration_browsertest.cc
+++ chrome/browser/net/network_context_configuration_browsertest.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_restrictions.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_content_browser_client.h"
@@ -2012,11 +2011,12 @@ class NetworkContextConfigurationProxySettingsBrowserTest
     expected_connections_run_loop.Run();
 
     // Then wait for any remaining connections that we should NOT get.
-    base::RunLoop ugly_100ms_wait;
-    base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
-        FROM_HERE, ugly_100ms_wait.QuitClosure(),
-        base::TimeDelta::FromMilliseconds(100));
-    ugly_100ms_wait.Run();
+    base::RunLoop unexpected_connections_run_loop;
+    base::RunLoop::ScopedRunTimeoutForTest run_timeout(
+        base::TimeDelta::FromMilliseconds(100),
+        base::BindLambdaForTesting(
+            [&]() { unexpected_connections_run_loop.Quit(); }));
+    unexpected_connections_run_loop.Run();
 
     // Stop the server.
     ASSERT_TRUE(embedded_test_server()->ShutdownAndWaitUntilComplete());
