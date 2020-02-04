69e329ff0dc4d108d9b05a1d15a2298115bd17d1
Tibor Goldschwendt <tiborg@chromium.org>
2020-02-04 20:16:43 +0000

    [webui][ntp] Add text animations to voice search overlay
    Bug: 1042534
    Change-Id: Icfeac3fd1d4361227a56119ffeb71c5797a6be5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032431
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738307}

--- chrome/browser/resources/new_tab_page/voice_search_overlay.js
+++ chrome/browser/resources/new_tab_page/voice_search_overlay.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
+import 'chrome://resources/polymer/v3_0/iron-selector/iron-selector.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
