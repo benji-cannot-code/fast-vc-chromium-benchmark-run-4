bdc6028e8fc17c3c6b2cba802c1c97c77026fda1
Alex Cooper <alcooper@chromium.org>
2020-02-04 03:00:29 +0000

    Separate VR DFM install logic from Consent Flow
    Currently GvrConsentHelper also ensures that the VR DFM is or gets
    installed if needed. However, the permissions API bypasses this flow. In
    order to enable the Permissions API logic by default, this logic needs
    to be separated out.
    
    This mimics the pattern of the ArCoreInstallHelper; however, there is an
    outstanding bug which blocks installing VrCore at this time. That will
    be addressed in a separate, future, change.
    
    Also drops the FPS timer in gvr_scheduler_delegate down to DVLOG(2), as
    it's rate of logging proved a bit difficult to debug during development.
    
    Fixed: 1043223
    Change-Id: I2475403b4df62fa2b9db0ca15642be2ff7d6b4ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036643
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738046}

--- chrome/browser/android/vr/gvr_consent_helper.h
+++ chrome/browser/android/vr/gvr_consent_helper.h
@@ -6,13 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ANDROID_VR_GVR_CONSENT_HELPER_H_
 #define CHROME_BROWSER_ANDROID_VR_GVR_CONSENT_HELPER_H_
 
-#include <jni.h>
-#include <memory>
-
 #include "base/android/jni_android.h"
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/android/vr/vr_module_provider.h"
 #include "chrome/browser/vr/service/xr_consent_helper.h"
 
 namespace vr {
@@ -31,11 +27,6 @@ class GvrConsentHelper : public XrConsentHelper {
   void OnUserConsentResult(JNIEnv* env, jboolean is_granted);
 
  private:
-  void OnModuleInstalled(bool success);
-
-  std::unique_ptr<VrModuleProvider> module_delegate_;
-  int render_process_id_;
-  int render_frame_id_;
   XrConsentPromptLevel consent_level_;
 
   OnUserConsentCallback on_user_consent_callback_;
