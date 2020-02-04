9b93605f28bb58f2ac13a9f5e173337baafe774a
Maggie Chen <magchen@chromium.org>
2020-02-04 17:45:18 +0000

    Should not create GpuDataManagerImpl in the IO thread in the content tests
    Some content unit tests create GpuDataManagerImpl in the IO thread.
    PeakGpuMemoryTrackerImpl needs GpuDataManagerImpl and GpuDataManagerImpl
    is created it when PeakGpuMemoryTrackerImpl is running on the IO thread.
    
    This will cause a crash (sequence checker) if the DisplayChange observer is
    added. This CL makes sure GpuDataManagerImpl is created first in the test
    setup so GpuDataManagerImpl will run on the main thread later.
    
    Bug:1042989
    
    Change-Id: I825937f800dfeeddf2d25b03e5e50bf418e6942e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2019882
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738217}

--- content/public/test/test_renderer_host.cc
+++ content/public/test/test_renderer_host.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/frame_tree_node.h"
 #include "content/browser/frame_host/navigation_entry_impl.h"
 #include "content/browser/frame_host/navigation_request.h"
+#include "content/browser/gpu/gpu_data_manager_impl.h"
 #include "content/browser/renderer_host/render_view_host_factory.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/site_instance_impl.h"
@@ -264,6 +265,9 @@ void RenderViewHostTestHarness::SetUp() {
   browser_context_ = CreateBrowserContext();
 
   SetContents(CreateTestWebContents());
+
+  // Create GpuDataManagerImpl here so it always runs on the main thread.
+  GpuDataManagerImpl::GetInstance();
 }
 
 void RenderViewHostTestHarness::TearDown() {
