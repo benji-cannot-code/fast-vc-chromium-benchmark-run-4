60b9d97d0abb46de87cd4eeaa01d87ddc14c2b8e
Marc Treib <treib@chromium.org>
2020-02-04 11:38:06 +0000

    Revert "Call MFShutdown in places where MFStartup is called"
    This reverts commit abf9a1f555cb8fe668d1c5f389811f13c728c9e2.
    
    Reason for revert: Broke tests on Win7 dbg, see linked bug
    
    Original change's description:
    > Call MFShutdown in places where MFStartup is called
    > 
    > Bug: 1012527
    > Fixed: 1012527
    > Change-Id: Id9ef248aadb0aa24704dec973b60ea23c9e1f1ed
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032332
    > Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    > Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#738063}
    
    TBR=xhwang@chromium.org,tmathmeyer@chromium.org
    
    Change-Id: I267213cc06b42769736c7eb337432ade7953de20
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1012527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036057
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738130}

--- media/base/win/mf_initializer.cc
+++ media/base/win/mf_initializer.cc
@@ -9,19 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 
-#include "base/optional.h"
-
 namespace media {
 
-MFSessionLifetime InitializeMediaFoundation() {
-  if (MFStartup(MF_VERSION, MFSTARTUP_LITE) == S_OK)
-    return std::make_unique<MFSession>();
-  DVLOG(1) << "Media Foundation unavailable or it failed to initialize";
-  return nullptr;
-}
-
-MFSession::~MFSession() {
-  MFShutdown();
+bool InitializeMediaFoundation() {
+  static const bool success = MFStartup(MF_VERSION, MFSTARTUP_LITE) == S_OK;
+  DVLOG_IF(1, !success)
+      << "Media Foundation unavailable or it failed to initialize";
+  return success;
 }
 
 }  // namespace media
