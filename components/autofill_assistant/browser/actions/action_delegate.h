1972d38273eaae19ecb087e510743a5f7993b3f3
Mathias Carlen <mcarlen@chromium.org>
2020-02-04 10:09:28 +0000

    [Autofill Assistant] MVP Extension API.
    This patch introduces the chrome.autofillAssistantPrivate extension API.
    Note that this code is gated behind the compile time flag
    enable_autofill_assistant_api and is hence not part of official builds
    and tests.
    
    It supports basic full e2e autofill_assistant flows and integration
    tests for it.
    
    We will add a custom "fyi" tryjob to keep our code green. This patch
    includes a browser smoke test to exercise the new extension code.
    More browser tests will be added as the skeleton is fleshed out
    later.
    
    Bug: 1015753
    Bug: b/143736397
    Change-Id: Ic727e589286bce8b7e43562642d711a6af89fe91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1975884
    Commit-Queue: Mathias Carlen <mcarlen@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738113}

--- components/autofill_assistant/browser/actions/action_delegate.h
+++ components/autofill_assistant/browser/actions/action_delegate.h
@@ -153,7 +153,7 @@ class ActionDelegate {
       const Selector& selector,
       base::OnceCallback<void(const ClientStatus&,
                               const autofill::FormData&,
-                              const autofill::FormFieldData&)> callback);
+                              const autofill::FormFieldData&)> callback) = 0;
 
   // Select the option given by |selector| and the value of the option to be
   // picked.
