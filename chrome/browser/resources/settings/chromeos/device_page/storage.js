1fbc6dad48046f41bdca84a535b5af5fb70cdf1a
Jeremie Boulic <jboulic@chromium.org>
2020-02-04 03:30:07 +0000

    Update Android storage to Apps and extensions
    Before the change, on the storage page, the row "Android storage"
    displayed the size of the android apps. The size of the apps and
    extensions installed from the chrome web store were not reflected
    anywhere on this page.
    
    This row is renamed "Apps and extensions", and displays the size of
    - android apps (including the default ones, which can be deleted if the
    user disables the Play store)
    - apps and extensions installed from the web store, which data is
    located in the Extensions folder of the user's cryptohome, as opposed
    to the system apps/extensions.
    
    The size of web store apps/extensions and android apps are calculated
    separately. The UI is always updated with the sum of both. Bytes
    formatting happen on the C++ side, so summing web store and android
    apps has to happen on the C++ side.
    
    androidRunning_ on the JS side was used to display or hide the
    "Android storage". The "Apps and extensions" row is now always
    visible. androidRunning_ is not needed anymore.
    
    unit_tests --gtest_filter="*StorageHandlerTest.AppsExtensionsSize"
    browser_tests --gtest_filter="*OSSettingsDevicePageTest.StorageTest"
    
    Test: 
    Bug: 1039916
    Change-Id: I4aeffad4a258356ab1cb48834b6bf2cba155f33a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032703
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738058}

--- chrome/browser/resources/settings/chromeos/device_page/storage.js
+++ chrome/browser/resources/settings/chromeos/device_page/storage.js
@@ -38,12 +38,6 @@ Polymer({
     androidEnabled: Boolean,
 
     /** @private */
-    androidRunning_: {
-      type: Boolean,
-      value: false,
-    },
-
-    /** @private */
     showCrostiniStorage_: {
       type: Boolean,
       value: false,
@@ -86,8 +80,7 @@ Polymer({
         'storage-browsing-data-size-changed',
         this.handleBrowsingDataSizeChanged_.bind(this));
     this.addWebUIListener(
-        'storage-android-size-changed',
-        this.handleAndroidSizeChanged_.bind(this));
+        'storage-apps-size-changed', this.handleAppsSizeChanged_.bind(this));
     this.addWebUIListener(
         'storage-crostini-size-changed',
         this.handleCrostiniSizeChanged_.bind(this));
@@ -96,9 +89,6 @@ Polymer({
           'storage-other-users-size-changed',
           this.handleOtherUsersSizeChanged_.bind(this));
     }
-    this.addWebUIListener(
-        'storage-android-running-changed',
-        this.handleAndroidRunningChanged_.bind(this));
   },
 
   ready() {
@@ -152,11 +142,11 @@ Polymer({
   },
 
   /**
-   * Handler for tapping the "Android storage" item.
+   * Handler for tapping the "Apps and Extensions" item.
    * @private
    */
-  onAndroidTap_() {
-    chrome.send('openArcStorage');
+  onAppsTap_() {
+    window.location = 'chrome://os-settings/app-management';
   },
 
   /**
@@ -217,14 +207,12 @@ Polymer({
   },
 
   /**
-   * @param {string} size Formatted string representing the size of Android
-   *     storage.
+   * @param {string} size Formatted string representing the size of Apps and
+   *     extensions storage.
    * @private
    */
-  handleAndroidSizeChanged_(size) {
-    if (this.androidRunning_) {
-      this.$$('#androidSize').subLabel = size;
-    }
+  handleAppsSizeChanged_(size) {
+    this.$$('#appsSize').subLabel = size;
   },
 
   /**
@@ -246,14 +234,6 @@ Polymer({
     if (!this.isGuest_) {
       this.$$('#otherUsersSize').subLabel = size;
     }
-  },
-
-  /**
-   * @param {boolean} running True if Android (ARC) is running.
-   * @private
-   */
-  handleAndroidRunningChanged_(running) {
-    this.androidRunning_ = running;
   },
 
   /**
