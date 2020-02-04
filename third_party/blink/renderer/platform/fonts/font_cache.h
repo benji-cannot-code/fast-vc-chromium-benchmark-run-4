b056549253ea2773fff725e036c1cce529f45013
Dominik Röttsches <drott@chromium.org>
2020-02-04 14:10:23 +0000

    Define DefaultHash and HashTraits for FontCacheKey
    Remove explicit hash and trait references when defining HashMaps based
    on FontCacheKey in place where that is used.
    
    Change-Id: Id77bb4a21102d8ed19c2fba66eda174d6e01ec65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2035999
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738158}

--- third_party/blink/renderer/platform/fonts/font_cache.h
+++ third_party/blink/renderer/platform/fonts/font_cache.h
@@ -96,11 +96,7 @@ typedef HashMap<unsigned,
                 WTF::IntHash<unsigned>,
                 WTF::UnsignedWithZeroKeyHashTraits<unsigned>>
     SizedFontPlatformDataSet;
-typedef HashMap<FontCacheKey,
-                SizedFontPlatformDataSet,
-                FontCacheKeyHash,
-                FontCacheKeyTraits>
-    FontPlatformDataCache;
+typedef HashMap<FontCacheKey, SizedFontPlatformDataSet> FontPlatformDataCache;
 typedef HashMap<FallbackListCompositeKey,
                 std::unique_ptr<ShapeCache>,
                 FallbackListCompositeKeyHash,
