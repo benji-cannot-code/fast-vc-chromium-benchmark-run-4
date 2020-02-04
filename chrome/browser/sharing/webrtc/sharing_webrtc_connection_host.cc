468bb47039f3faaeb7c9d8db89451fb01d8031d5
Himanshu Jaju <himanshujaju@chromium.org>
2020-02-04 18:56:35 +0000

    Track result of receiving message via WebRTC.
    Tracks the result of receiving a SharingMessage via WebRTC. Logs the result
    to UMA.
    
    Bug: 1021984
    Change-Id: I89a87e249204bd8421ef03165f2ad23c361a0af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036001
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738252}

--- chrome/browser/sharing/webrtc/sharing_webrtc_connection_host.cc
+++ chrome/browser/sharing/webrtc/sharing_webrtc_connection_host.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sharing/sharing_message_handler.h"
 #include "chrome/browser/sharing/sharing_metrics.h"
 #include "chrome/browser/sharing/webrtc/webrtc_signalling_host_fcm.h"
+#include "chrome/services/sharing/public/cpp/sharing_webrtc_metrics.h"
 #include "components/gcm_driver/crypto/gcm_decryption_result.h"
 #include "components/gcm_driver/crypto/gcm_encryption_result.h"
 #include "components/gcm_driver/gcm_driver.h"
@@ -91,26 +92,34 @@ void SharingWebRtcConnectionHost::OnMessageDecrypted(
     gcm::GCMDecryptionResult result,
     std::string message) {
   chrome_browser_sharing::WebRtcMessage sharing_message;
-  if (result != gcm::GCMDecryptionResult::DECRYPTED_DRAFT_08 ||
-      !sharing_message.ParseFromString(message)) {
-    // TODO(crbug.com/1021984): replace this with UMA metrics
-    LOG(ERROR) << "Could not parse Sharing message received via WebRTC!";
+  if (result != gcm::GCMDecryptionResult::DECRYPTED_DRAFT_08) {
+    LogSharingWebRtcOnMessageReceivedResult(
+        sharing::WebRtcOnMessageReceivedResult::kDecryptionFailed);
+    return;
+  }
+
+  if (!sharing_message.ParseFromString(message)) {
+    LogSharingWebRtcOnMessageReceivedResult(
+        sharing::WebRtcOnMessageReceivedResult::kParseFailed);
     return;
   }
 
   auto payload_case = sharing_message.message().payload_case();
   if (!IsValidSharingWebRtcPayloadCase(payload_case)) {
-    // TODO(crbug.com/1021984): replace this with UMA metrics
-    LOG(ERROR) << "Unexpected payload case from WebRTC: " << payload_case;
+    LogSharingWebRtcOnMessageReceivedResult(
+        sharing::WebRtcOnMessageReceivedResult::kInvalidPayload);
     return;
   }
 
   auto* handler = handler_registry_->GetSharingHandler(payload_case);
   if (!handler) {
-    // TODO(crbug.com/1021984): replace this with UMA metrics
-    LOG(ERROR) << "No sharing handler for payload_case " << payload_case;
+    LogSharingWebRtcOnMessageReceivedResult(
+        sharing::WebRtcOnMessageReceivedResult::kHandlerNotFound);
     return;
   }
+
+  LogSharingWebRtcOnMessageReceivedResult(
+      sharing::WebRtcOnMessageReceivedResult::kSuccess);
 
   timeout_state_ = sharing::WebRtcTimeoutState::kMessageReceived;
   timeout_timer_.Reset();
--- chrome/browser/sharing/webrtc/webrtc_signalling_host_fcm.cc
+++ chrome/browser/sharing/webrtc/webrtc_signalling_host_fcm.cc
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/sharing/webrtc/webrtc_signalling_host_fcm.h"
 
-#include <memory>
-
+#include "base/bind_helpers.h"
 #include "base/callback.h"
 #include "base/time/time.h"
 #include "chrome/browser/sharing/features.h"
-#include "chrome/browser/sharing/sharing_metrics.h"
+#include "chrome/browser/sharing/proto/sharing_message.pb.h"
+#include "chrome/browser/sharing/sharing_send_message_result.h"
 
 WebRtcSignallingHostFCM::WebRtcSignallingHostFCM(
     mojo::PendingReceiver<sharing::mojom::SignallingSender> signalling_sender,
@@ -74,15 +74,7 @@ void WebRtcSignallingHostFCM::SendIceCandidates(
       *device_info_,
       base::TimeDelta::FromSeconds(kSharingMessageTTLSeconds.Get()),
       std::move(sharing_message), SharingMessageSender::DelegateType::kFCM,
-      base::BindOnce([](SharingSendMessageResult result,
-                        std::unique_ptr<chrome_browser_sharing::ResponseMessage>
-                            response) {
-        if (result != SharingSendMessageResult::kSuccessful) {
-          // TODO(crbug.com/1021984): replace this with UMA metrics
-          LOG(ERROR) << "Error sending Ice Candidate";
-          return;
-        }
-      }));
+      base::DoNothing());
 }
 
 void WebRtcSignallingHostFCM::OnOfferReceived(
