ace8a5a7e8051c2945bac1e84881b5e1909c0d30
Yuki Shiino <yukishiino@chromium.org>
2020-02-04 14:23:48 +0000

    bindings: Add missing 'const' to DOMMatrixReadOnly::Create
    Bug: 839389
    Change-Id: I3c5406b19457ba5427cae24785fbae0a3c161d02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037278
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738159}

--- third_party/blink/renderer/core/geometry/dom_matrix_read_only.cc
+++ third_party/blink/renderer/core/geometry/dom_matrix_read_only.cc
@@ -109,7 +109,7 @@ DOMMatrixReadOnly* DOMMatrixReadOnly::Create(
 
 DOMMatrixReadOnly* DOMMatrixReadOnly::Create(
     ExecutionContext* execution_context,
-    StringOrUnrestrictedDoubleSequence& init,
+    const StringOrUnrestrictedDoubleSequence& init,
     ExceptionState& exception_state) {
   if (init.IsString()) {
     if (!execution_context->IsDocument()) {
