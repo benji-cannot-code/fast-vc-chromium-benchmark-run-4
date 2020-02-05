a2a2c0287966fb689b03d12258df03e87382b85d
gogerald <gogerald@google.com>
2020-02-05 04:05:04 +0000

    [StartSurface] setInitialOverviewState after onStartWithNative
    This CL also fixes the issues that
    1, MobileStartup.UserEnteredTabSwitcher is not recorded when onStartWithNative is
    called earlier than createInitialTab
    
    Bug: 1048318
    Change-Id: I3549ad1fb250046fe293dd2437375abced1254e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036253
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738429}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -892,10 +892,6 @@ public class ChromeTabbedActivity extends ChromeActivity {
         // showing a glimpse of the tab selector during start up.
         if (!mPendingInitialTabCreation) setInitialOverviewState();
 
-        if (isMainIntentFromLauncher(getIntent()) && isInOverviewMode()) {
-            RecordUserAction.record("MobileStartup.UserEnteredTabSwitcher");
-        }
-
         resetSavedInstanceState();
     }
 
@@ -988,6 +984,10 @@ public class ChromeTabbedActivity extends ChromeActivity {
 
         if (getActivityTab() == null && !isOverviewVisible) {
             showOverview(OverviewModeState.SHOWING_START);
+        }
+
+        if (isMainIntentFromLauncher(getIntent()) && mOverviewModeController.overviewVisible()) {
+            RecordUserAction.record("MobileStartup.UserEnteredTabSwitcher");
         }
     }
 
@@ -1194,24 +1194,24 @@ public class ChromeTabbedActivity extends ChromeActivity {
 
         // If the grid tab switcher is enabled and the tab switcher will be shown on start,
         //  do not create a new tab. With the grid, creating a new tab is now a one tap action.
-        if (shouldShowTabSwitcherOnStart() && CachedFeatureFlags.isGridTabSwitcherEnabled()) return;
-
-        String url = HomepageManager.getHomepageUri();
-        if (TextUtils.isEmpty(url)) {
-            url = UrlConstants.NTP_URL;
-        } else {
-            boolean startupHomepageIsNtp = false;
-            // Migrate legacy NTP URLs (chrome://newtab) to the newer format
-            // (chrome-native://newtab)
-            if (NewTabPage.isNTPUrl(url)) {
+        if (!(shouldShowTabSwitcherOnStart() && CachedFeatureFlags.isGridTabSwitcherEnabled())) {
+            String url = HomepageManager.getHomepageUri();
+            if (TextUtils.isEmpty(url)) {
                 url = UrlConstants.NTP_URL;
-                startupHomepageIsNtp = true;
+            } else {
+                boolean startupHomepageIsNtp = false;
+                // Migrate legacy NTP URLs (chrome://newtab) to the newer format
+                // (chrome-native://newtab)
+                if (NewTabPage.isNTPUrl(url)) {
+                    url = UrlConstants.NTP_URL;
+                    startupHomepageIsNtp = true;
+                }
+                RecordHistogram.recordBooleanHistogram(
+                        "MobileStartup.LoadedHomepageOnColdStart", startupHomepageIsNtp);
             }
-            RecordHistogram.recordBooleanHistogram(
-                    "MobileStartup.LoadedHomepageOnColdStart", startupHomepageIsNtp);
-        }
 
-        getTabCreator(false).launchUrl(url, TabLaunchType.FROM_STARTUP);
+            getTabCreator(false).launchUrl(url, TabLaunchType.FROM_STARTUP);
+        }
 
         // If we didn't call setInitialOverviewState() in startWithNative() because
         // mPendingInitialTabCreation was true then do so now.
