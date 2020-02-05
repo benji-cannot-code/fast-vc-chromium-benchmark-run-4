97b72a666d42ff571c77e4e0534ecac75346c113
Ben Mason <benmason@chromium.org>
2020-02-05 06:16:32 +0000

    Revert "Add DomainDiversityReporter for domain diversity metrics"
    This reverts commit 835cde27843d43fc1f2c74ddd98c2c6a373f6e1e.
    
    Reason for revert: Causing crash: crbug/1048145
    
    Original change's description:
    > Add DomainDiversityReporter for domain diversity metrics
    > 
    > This CL adds DomainDiversityReporter to schedule domain metrics
    > computation tasks and emit histograms. The reporting task is
    > executed shortly after startup and then scheduled once per 24
    > hours thereafter.
    > 
    > Bug: 1015424
    > Change-Id: Id4b8043c1a9b9ea0ee70d4be68e9c35017a82de1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1864522
    > Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Mark Pearson <mpearson@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#735713}
    
    TBR=sadrul@chromium.org,sky@chromium.org,mpearson@chromium.org,gab@chromium.org,mjzhang@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1015424
    Change-Id: I8b9d3f2b9866569cf7f55d842cca19bd2c3f65c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037952
    Reviewed-by: Ben Mason <benmason@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738459}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/favicon/history_ui_favicon_request_handler_factory.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/google/google_search_domain_mixing_metrics_emitter_factory.h"
-#include "chrome/browser/history/domain_diversity_reporter_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/history/top_sites_factory.h"
 #include "chrome/browser/invalidation/deprecated_profile_invalidation_provider_factory.h"
@@ -238,7 +237,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   ContentSuggestionsServiceFactory::GetInstance();
   CookieSettingsFactory::GetInstance();
   NotifierStateTrackerFactory::GetInstance();
-  DomainDiversityReporterFactory::GetInstance();
   dom_distiller::DomDistillerServiceFactory::GetInstance();
   DownloadCoreServiceFactory::GetInstance();
   DownloadServiceFactory::GetInstance();
