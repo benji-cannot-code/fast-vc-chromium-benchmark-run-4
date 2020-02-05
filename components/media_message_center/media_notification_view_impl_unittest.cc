b5a22614773bbcf2016ee0ffa645a107bc767da5
Tommy Steimel <steimel@chromium.org>
2020-02-05 21:48:32 +0000

    GMC: Update minimum contrast ratio
    This CL updates the Global Media Controls dialog to use 7 as the
    minimum contrast ratio between the foreground and background colors
    instead of using the default of 4.5.
    
    Bug: 1049242
    Change-Id: I81538040993cdd555cd1e6173554b9021018d18e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2039894
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738688}

--- components/media_message_center/media_notification_view_impl_unittest.cc
+++ components/media_message_center/media_notification_view_impl_unittest.cc
@@ -818,7 +818,7 @@ TEST_F(MAYBE_MediaNotificationViewImplTest, UpdateArtworkFromItem) {
 
   SkBitmap image;
   image.allocN32Pixels(10, 10);
-  image.eraseColor(SK_ColorMAGENTA);
+  image.eraseColor(SK_ColorGREEN);
 
   EXPECT_TRUE(GetArtworkImage().isNull());
 
