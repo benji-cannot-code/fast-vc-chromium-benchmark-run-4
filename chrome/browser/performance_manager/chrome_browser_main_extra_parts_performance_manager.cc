e328e33c4a0e60d6c428f71c901a1b15959139c2
Sebastien Marchand <sebmarchand@chromium.org>
2020-02-05 15:39:42 +0000

    Record Chrome total RSS when receiving a memory pressure signal.
    This metric will be used to understand the state of the system when
    memory pressure happens.
    
    More metrics will be added to this new MemoryPressureMetrics class later
    on.
    
    Change-Id: I2551c1ae5a36913a339ebbe4724d1637044b34f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2028170
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738561}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/performance_manager/graph/policies/policy_features.h"
 #include "chrome/browser/performance_manager/graph/policies/urgent_page_discarding_policy.h"
 #include "chrome/browser/performance_manager/graph/policies/working_set_trimmer_policy.h"
+#include "chrome/browser/performance_manager/metrics/memory_pressure_metrics.h"
 #include "chrome/browser/performance_manager/observers/isolation_context_metrics.h"
 #include "chrome/browser/performance_manager/observers/metrics_collector.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -105,6 +106,9 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
             performance_manager::policies::UrgentPageDiscardingPolicy>());
   }
 #endif  // !defined(OS_ANDROID)
+
+  graph->PassToGraph(
+      std::make_unique<performance_manager::metrics::MemoryPressureMetrics>());
 }
 
 content::LockObserver*
