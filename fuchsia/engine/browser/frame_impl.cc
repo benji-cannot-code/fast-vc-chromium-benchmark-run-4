427453c03c7f004e087329e3ec2687e2284445d5
Wez <wez@chromium.org>
2020-02-05 18:22:03 +0000

    [fuchsia] Rename the WebEngine's LayoutManager.
    Rather than prefixing the name with Fuchsia, prefix with Frame, to
    reflect the scope to which it applies.
    
    Change-Id: I8d946ea9076f3fc04193cfc3ea8a44d830109ac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038775
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738609}

--- fuchsia/engine/browser/frame_impl.cc
+++ fuchsia/engine/browser/frame_impl.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia/base/message_port.h"
 #include "fuchsia/engine/browser/accessibility_bridge.h"
 #include "fuchsia/engine/browser/context_impl.h"
-#include "fuchsia/engine/browser/fuchsia_layout_manager.h"
+#include "fuchsia/engine/browser/frame_layout_manager.h"
 #include "fuchsia/engine/browser/web_engine_devtools_controller.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 #include "mojo/public/cpp/system/platform_handle.h"
@@ -740,7 +740,7 @@ void FrameImpl::SetWindowTreeHost(
 
   wm::SetActivationClient(root_window(), focus_controller_.get());
 
-  layout_manager_ = new FuchsiaLayoutManager;
+  layout_manager_ = new FrameLayoutManager;
   root_window()->SetLayoutManager(layout_manager_);  // Transfers ownership.
   if (!render_size_override_.IsEmpty())
     layout_manager_->ForceContentDimensions(render_size_override_);
--- fuchsia/engine/browser/frame_layout_manager.h
+++ fuchsia/engine/browser/frame_layout_manager.h
@@ -3,10 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FUCHSIA_ENGINE_BROWSER_FUCHSIA_LAYOUT_MANAGER_H_
-#define FUCHSIA_ENGINE_BROWSER_FUCHSIA_LAYOUT_MANAGER_H_
+#ifndef FUCHSIA_ENGINE_BROWSER_FRAME_LAYOUT_MANAGER_H_
+#define FUCHSIA_ENGINE_BROWSER_FRAME_LAYOUT_MANAGER_H_
 
-#include "base/macros.h"
 #include "ui/aura/layout_manager.h"
 #include "ui/aura/window.h"
 
@@ -16,10 +15,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // boxes). These windows get the location and size they request. The main
 // window for the web content is identified by window.type() ==
 // WINDOW_TYPE_CONTROL (set in WebContentsViewAura).
-class FuchsiaLayoutManager : public aura::LayoutManager {
+class FrameLayoutManager : public aura::LayoutManager {
  public:
-  FuchsiaLayoutManager();
-  ~FuchsiaLayoutManager() override;
+  FrameLayoutManager();
+  ~FrameLayoutManager() override;
+
+  FrameLayoutManager(const FrameLayoutManager&) = delete;
+  FrameLayoutManager& operator=(const FrameLayoutManager&) = delete;
 
   // Renders web content within a virtual window of a given |size|, which is
   // proportionately scaled to fit within the View.
@@ -46,8 +48,6 @@ class FuchsiaLayoutManager : public aura::LayoutManager {
   aura::Window* main_child_ = nullptr;
 
   gfx::Size render_size_override_;
-
-  DISALLOW_COPY_AND_ASSIGN(FuchsiaLayoutManager);
 };
 
-#endif  // FUCHSIA_ENGINE_BROWSER_FUCHSIA_LAYOUT_MANAGER_H_
+#endif  // FUCHSIA_ENGINE_BROWSER_FRAME_LAYOUT_MANAGER_H_
