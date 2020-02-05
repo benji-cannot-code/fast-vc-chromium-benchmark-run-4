72bb923f73b09acc93caec5f9dd7cb97393d5c03
Kent Tamura <tkent@chromium.org>
2020-02-05 08:40:18 +0000

    Remove GetText(), ClientPaddingLeft(), and ClientPaddingRight() of LayoutMenuList
    Fold them into their callsites.
    This CL has no behavior changes.
    
    Bug: 1040828
    Change-Id: Iac6c8c1e061993534e7f7ed45d60139ec9cd63e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038196
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738490}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -112,7 +112,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/inspector_issue.h"
 #include "third_party/blink/renderer/core/inspector/main_thread_debugger.h"
 #include "third_party/blink/renderer/core/intersection_observer/intersection_observer.h"
-#include "third_party/blink/renderer/core/layout/layout_menu_list.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_tree_as_text.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
@@ -2942,12 +2941,9 @@ void Internals::forceImageReload(Element* element,
 
 String Internals::selectMenuListText(HTMLSelectElement* select) {
   DCHECK(select);
-  LayoutObject* layout_object = select->GetLayoutObject();
-  if (!layout_object || !layout_object->IsMenuList())
+  if (!select->UsesMenuList())
     return String();
-
-  LayoutMenuList* menu_list = ToLayoutMenuList(layout_object);
-  return menu_list->GetText();
+  return select->InnerElement().innerText();
 }
 
 bool Internals::isSelectPopupVisible(Node* node) {
