79a8e3f92f6721f87e946d83b3c03b23cc7f7dcc
Michael Thiessen <mthiesse@chromium.org>
2020-02-05 22:09:22 +0000

    Migrate webview Webcontents#getVisibleUrl to GURL
    Bug: 783819
    Change-Id: Ib1970a41bb45a31907ea91358fd27bee6c6b20a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2039301
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738694}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
@@ -1097,7 +1097,7 @@ class WebViewChromium implements WebViewProvider, WebViewProvider.ScrollDelegate
             return ret;
         }
         sWebViewApiCallSample.record(ApiCall.GET_URL);
-        return mAwContents.getUrl();
+        return mAwContents.getUrl().getSpec();
     }
 
     @Override
