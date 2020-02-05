1710bb38f915ac8829de981be252a3543a16decc
Hesen Zhang <hesen@google.com>
2020-02-05 22:17:42 +0000

    [Prefetch notification service]: Init setup.
    - Setup prefetch notification service, factory and client code.
    
    Bug: 1047037
    Change-Id: Iccc82d8b49f63213294eb0440fc59138db756912
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2029293
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Auto-Submit: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738697}

---
+++ chrome/browser/offline_pages/prefetch/notifications/prefetch_notification_service.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_OFFLINE_PAGES_PREFETCH_NOTIFICATIONS_PREFETCH_NOTIFICATION_SERVICE_H_
+#define CHROME_BROWSER_OFFLINE_PAGES_PREFETCH_NOTIFICATIONS_PREFETCH_NOTIFICATION_SERVICE_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace offline_pages {
+
+// Service to manage offline prefetch notifications via
+// notifications::NotificationScheduleService.
+class PrefetchNotificationService : public KeyedService {
+ public:
+  ~PrefetchNotificationService() override = default;
+
+ protected:
+  PrefetchNotificationService() = default;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(PrefetchNotificationService);
+};
+
+}  // namespace offline_pages
+
+#endif  // CHROME_BROWSER_OFFLINE_PAGES_PREFETCH_NOTIFICATIONS_PREFETCH_NOTIFICATION_SERVICE_H_
---
+++ chrome/browser/offline_pages/prefetch/notifications/prefetch_notification_service_impl.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/offline_pages/prefetch/notifications/prefetch_notification_service_impl.h"
+
+namespace offline_pages {
+
+PrefetchNotificationServiceImpl::PrefetchNotificationServiceImpl(
+    notifications::NotificationScheduleService* schedule_service)
+    : schedule_service_(schedule_service) {
+  DCHECK(schedule_service_);
+}
+
+PrefetchNotificationServiceImpl::~PrefetchNotificationServiceImpl() = default;
+
+}  // namespace offline_pages
