4e9ad16f1a1482dcb540ead2324082ca4558b629
Theresa Wellington <twellington@chromium.org>
2020-02-05 22:43:53 +0000

    Move three classes to components/browser_ui/widget/
    Move the following classes to components/browser_ui/widget/
     - FeatureHighlightProvider
     - InsetObserverView
     - MenuOrKeyboardActionController
    
    BUG=951455,1010176
    
    Change-Id: I7eac449f45b52f243fea468554b2a9f6c4868d32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040002
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738708}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/InsetObserverView.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/InsetObserverView.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser;
+package org.chromium.components.browser_ui.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
@@ -196,8 +196,9 @@ public class InsetObserverView extends View {
 
             // If the safe area has not changed then we should stop now.
             if (mCurrentSafeArea.left == left && mCurrentSafeArea.top == top
-                    && mCurrentSafeArea.right == right && mCurrentSafeArea.bottom == bottom)
+                    && mCurrentSafeArea.right == right && mCurrentSafeArea.bottom == bottom) {
                 return;
+            }
 
             mCurrentSafeArea.set(left, top, right, bottom);
 
