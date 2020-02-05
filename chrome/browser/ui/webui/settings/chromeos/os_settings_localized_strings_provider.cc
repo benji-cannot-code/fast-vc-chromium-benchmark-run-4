a1cdaa0b298b1cb70006ecb44244034e859e3732
Regan Hsu <hsuregan@google.com>
2020-02-05 02:08:03 +0000

    [CrOS Settings] Add needed OS strings and remove browser string provider
    This CL copies over the final strings that are missing from browser
    settings to OS settings, and removes the browser string provider from
    OS settings.
    
    Bug: 967888
    Change-Id: Iebf0880fb3ef94df67e9bc4fea14ad286b0c18ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036263
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738408}

--- chrome/browser/ui/webui/settings/chromeos/os_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/chromeos/os_settings_localized_strings_provider.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/network_element_localized_strings_provider.h"
 #include "chrome/browser/ui/webui/chromeos/smb_shares/smb_shares_localized_strings_provider.h"
 #include "chrome/browser/ui/webui/management_ui.h"
+#include "chrome/browser/ui/webui/policy_indicator_localized_strings_provider.h"
 #include "chrome/browser/ui/webui/settings/shared_settings_localized_strings_provider.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/chrome_features.h"
@@ -98,6 +99,7 @@ void AddCommonStrings(content::WebUIDataSource* html_source, Profile* profile) {
       {"close", IDS_CLOSE},
       {"confirm", IDS_CONFIRM},
       {"continue", IDS_SETTINGS_CONTINUE},
+      {"controlledByExtension", IDS_SETTINGS_CONTROLLED_BY_EXTENSION},
       {"custom", IDS_SETTINGS_CUSTOM},
       {"delete", IDS_SETTINGS_DELETE},
       {"deviceOff", IDS_SETTINGS_DEVICE_OFF},
@@ -368,6 +370,10 @@ void AddLanguagesStrings(content::WebUIDataSource* html_source) {
       {"moveUp", IDS_SETTINGS_LANGUAGES_LANGUAGES_LIST_MOVE_UP},
   };
   AddLocalizedStringsBulk(html_source, kLocalizedStrings);
+
+  html_source->AddString(
+      "languagesLearnMoreURL",
+      base::ASCIIToUTF16(chrome::kLanguageSettingsLearnMoreUrl));
 }
 
 void AddPersonalizationStrings(content::WebUIDataSource* html_source) {
@@ -813,6 +819,9 @@ void AddChromeOSUserStrings(content::WebUIDataSource* html_source,
                              base::ASCIIToUTF16(chrome::kChromeUISettingsURL)));
   html_source->AddBoolean("isActiveDirectoryUser",
                           user && user->IsActiveDirectoryUser());
+  html_source->AddBoolean(
+      "isSecondaryUser",
+      user && user->GetAccountId() != primary_user->GetAccountId());
   html_source->AddString(
       "secondaryUserBannerText",
       l10n_util::GetStringFUTF16(IDS_SETTINGS_SECONDARY_USER_BANNER,
@@ -2004,6 +2013,10 @@ void AddOsLocalizedStrings(content::WebUIDataSource* html_source,
   AddSearchInSettingsStrings(html_source);
   AddSearchStrings(html_source, profile);
   AddUsersStrings(html_source);
+
+  policy_indicator::AddLocalizedStrings(html_source);
+
+  html_source->UseStringsJs();
 }
 
 }  // namespace settings
