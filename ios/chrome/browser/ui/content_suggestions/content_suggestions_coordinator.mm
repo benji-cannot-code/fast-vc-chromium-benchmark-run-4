09109e35d35d9dfaf4a4824bfeb550f8650e4cbd
Eugene But <eugenebut@google.com>
2020-02-05 22:08:20 +0000

    Improve user actions logging for closing the Tab.
    Add OverscrollActionCloseTab user action.
    Mark MobileTabClosed as not_user_triggered.
    
    This will help to better distinguish different ways
    of closing the tab.
    
    Bug: 1046223
    Change-Id: I58ce36f51bb39f4d52e75633c2c6840ce4283b2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032405
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738693}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.h"
 
 #include "base/mac/foundation_util.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "components/ntp_snippets/content_suggestions_service.h"
 #include "components/ntp_snippets/pref_names.h"
 #include "components/ntp_snippets/remote/remote_suggestions_scheduler.h"
@@ -225,6 +227,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     } break;
     case OverscrollAction::CLOSE_TAB: {
       [_dispatcher closeCurrentTab];
+      base::RecordAction(base::UserMetricsAction("OverscrollActionCloseTab"));
     } break;
     case OverscrollAction::REFRESH:
       [self reload];
