654e7f40cc9d47dc74c3a97b92f27e0c83f3ecbd
Theresa Wellington <twellington@chromium.org>
2020-02-05 18:34:01 +0000

    Remove AppHooks dependency from FeatureHighlightProvider
    Remove FeatureHighlightProvider's dependency on AppHooks in preparation
    for moving to //components/browser_ui/widgets/.
    
    BUG=1010176
    
    Change-Id: Idea815e073fd757140b9ed2b60434dc561b60ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2016325
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738611}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BrowsingModeBottomToolbarMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BrowsingModeBottomToolbarMediator.java
@@ -14,6 +14,7 @@ import androidx.annotation.StringRes;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ThemeColorProvider;
 import org.chromium.chrome.browser.ThemeColorProvider.ThemeColorObserver;
@@ -47,6 +48,8 @@ class BrowsingModeBottomToolbarMediator implements ThemeColorObserver {
     /** A provider that notifies components when the theme color changes.*/
     private ThemeColorProvider mThemeColorProvider;
 
+    private FeatureHighlightProvider mFeatureHighlightProvider;
+
     /**
      * Build a new mediator that handles events from outside the bottom toolbar.
      * @param model The {@link BrowsingModeBottomToolbarModel} that holds all the state for the
@@ -54,6 +57,7 @@ class BrowsingModeBottomToolbarMediator implements ThemeColorObserver {
      */
     BrowsingModeBottomToolbarMediator(BrowsingModeBottomToolbarModel model) {
         mModel = model;
+        mFeatureHighlightProvider = AppHooks.get().createFeatureHighlightProvider();
     }
 
     void setThemeColorProvider(ThemeColorProvider themeColorProvider) {
@@ -118,7 +122,7 @@ class BrowsingModeBottomToolbarMediator implements ThemeColorObserver {
             finalScrimColor = Color.TRANSPARENT;
         }
 
-        FeatureHighlightProvider.getInstance().buildForView(activity, anchor, titleId,
+        mFeatureHighlightProvider.buildForView(activity, anchor, titleId,
                 FeatureHighlightProvider.TextAlignment.CENTER,
                 R.style.TextAppearance_TextLarge_Primary_Light, descId,
                 FeatureHighlightProvider.TextAlignment.CENTER,
@@ -132,7 +136,7 @@ class BrowsingModeBottomToolbarMediator implements ThemeColorObserver {
      * @param activity An activity to attach the IPH to.
      */
     void dismissIPH(AppCompatActivity activity) {
-        FeatureHighlightProvider.getInstance().dismiss(activity);
+        mFeatureHighlightProvider.dismiss(activity);
     }
 
     /**
