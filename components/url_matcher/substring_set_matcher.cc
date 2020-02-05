58bddc6f2faa585eaf5f6c2cf2cb81bfbde2a56e
Karan Bhatia <karandeepb@chromium.org>
2020-02-05 22:45:24 +0000

    UrlMatcher: Add perf test for SubstringSetMatcher.
    Add a simple perf test for SubstringSetMatcher. I plan to change a bit
    of the internal implementation, so this will help to measure any perf
    difference as well as catch future regressions.
    
    BUG=974391
    
    Change-Id: I03ad4d2ae531c524256db095285761ea4529b270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038070
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738709}

--- components/url_matcher/substring_set_matcher.cc
+++ components/url_matcher/substring_set_matcher.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/queue.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
+#include "base/trace_event/memory_usage_estimator.h"
 
 namespace url_matcher {
 
@@ -140,6 +141,11 @@ bool SubstringSetMatcher::IsEmpty() const {
   return patterns_.empty() && tree_.size() == 1u;
 }
 
+size_t SubstringSetMatcher::EstimateMemoryUsage() const {
+  return base::trace_event::EstimateMemoryUsage(tree_) +
+         base::trace_event::EstimateMemoryUsage(patterns_);
+}
+
 void SubstringSetMatcher::RebuildAhoCorasickTree(
     const SubstringPatternVector& sorted_patterns) {
   tree_.clear();
@@ -265,6 +271,11 @@ void SubstringSetMatcher::AhoCorasickNode::AddMatch(StringPattern::ID id) {
 void SubstringSetMatcher::AhoCorasickNode::AddMatches(
     const SubstringSetMatcher::AhoCorasickNode::Matches& matches) {
   matches_.insert(matches.begin(), matches.end());
+}
+
+size_t SubstringSetMatcher::AhoCorasickNode::EstimateMemoryUsage() const {
+  return base::trace_event::EstimateMemoryUsage(edges_) + sizeof(failure_) +
+         base::trace_event::EstimateMemoryUsage(matches_);
 }
 
 }  // namespace url_matcher
