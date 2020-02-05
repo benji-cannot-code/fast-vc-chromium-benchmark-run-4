6e4e7914a22ea6f0f82860728e7eb0eac57120d3
David Tseng <dtseng@chromium.org>
2020-02-05 16:41:12 +0000

    Fixes speak text under mouse in ChromeVox
    Bug: 1047069
    Test: manual (there isn't an easy stable way to test this in either js or interactive ui tests).
    Change-Id: Ia91a585ec314066a4cceea244e4872a15598cc9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037731
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738577}

--- chrome/browser/ui/aura/accessibility/automation_manager_aura.h
+++ chrome/browser/ui/aura/accessibility/automation_manager_aura.h
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/ax_event_observer.h"
 #include "ui/views/accessibility/ax_tree_source_views.h"
 
-class AXRootObjWrapper;
-
 namespace base {
 template <typename T>
 class NoDestructor;
@@ -104,9 +102,6 @@ class AutomationManagerAura : public ui::AXActionHandler,
 
   // Whether automation support for views is enabled.
   bool enabled_ = false;
-
-  // Root object representing the entire desktop. Must outlive |current_tree_|.
-  std::unique_ptr<AXRootObjWrapper> desktop_root_;
 
   // Holds the active views-based accessibility tree. A tree currently consists
   // of all views descendant to a |Widget| (see |AXTreeSourceViews|).
