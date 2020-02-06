5a9ace30568844621d047b99ab4acc48bf039386
Demetrios Papadopoulos <dpapad@chromium.org>
2020-02-06 04:10:24 +0000

    Settings: Port default_browser_page/ to Polymer 3.
    Bug: 1026426
    Change-Id: I384b5f55acc3bbe71b03c039e3cc68782dacc5ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038074
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738830}

--- chrome/browser/resources/settings/default_browser_page/default_browser_browser_proxy.js
+++ chrome/browser/resources/settings/default_browser_page/default_browser_browser_proxy.js
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * to interact with the browser.
  */
 
+// clang-format off
+// #import {addSingletonGetter, sendWithPromise} from 'chrome://resources/js/cr.m.js';
+// clang-format on
+
 /**
  * @typedef {{
  *   canBeDefault: boolean,
@@ -16,11 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   isUnknownError: boolean,
  * }};
  */
-let DefaultBrowserInfo;
+/* #export */ let DefaultBrowserInfo;
 
 cr.define('settings', function() {
   /** @interface */
-  class DefaultBrowserBrowserProxy {
+  /* #export */ class DefaultBrowserBrowserProxy {
     /**
      * Get the initial DefaultBrowserInfo and begin sending updates to
      * 'settings.updateDefaultBrowserState'.
@@ -35,10 +39,8 @@ cr.define('settings', function() {
     setAsDefaultBrowser() {}
   }
 
-  /**
-   * @implements {settings.DefaultBrowserBrowserProxy}
-   */
-  class DefaultBrowserBrowserProxyImpl {
+  /** @implements {settings.DefaultBrowserBrowserProxy} */
+  /* #export */ class DefaultBrowserBrowserProxyImpl {
     /** @override */
     requestDefaultBrowserState() {
       return cr.sendWithPromise('requestDefaultBrowserState');
--- chrome/test/data/webui/settings/default_browser_browsertest.js
+++ chrome/test/data/webui/settings/default_browser_browsertest.js
@@ -3,6 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import {DefaultBrowserBrowserProxyImpl} from 'chrome://settings/settings.js';
+// #import {TestBrowserProxy} from 'chrome://test/test_browser_proxy.m.js';
+// #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// clang-format on
+
 /**
  * A test version of DefaultBrowserBrowserProxy. Provides helper methods
  * for allowing tests to know when a method was called, as well as
