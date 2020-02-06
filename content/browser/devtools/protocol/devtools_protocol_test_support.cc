21b72db7399a9855674297664e316ff65611ebce
Yury Semikhatsky <yurys@chromium.org>
2020-02-06 01:49:07 +0000

    [DevTools] Make Page.crash and Target.targetCrashed work for OOPIFs
    WebContents dispatches RenderProcessGone only for the main frame's
    process. To receive such notifications for out-of-process iframes
    we now subscribe to the frame's process events directly.
    
    Bug: none
    Change-Id: I3cd4b4b7f9a69ed749e624ac82c6feb94111f207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040051
    Commit-Queue: Yury Semikhatsky <yurys@chromium.org>
    Auto-Submit: Yury Semikhatsky <yurys@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738787}

--- content/browser/devtools/protocol/devtools_protocol_test_support.cc
+++ content/browser/devtools/protocol/devtools_protocol_test_support.cc
@@ -20,6 +20,7 @@ namespace content {
 namespace {
 
 const char kIdParam[] = "id";
+const char kSessionIdParam[] = "sessionId";
 const char kMethodParam[] = "method";
 const char kParamsParam[] = "params";
 
@@ -47,9 +48,10 @@ bool DevToolsProtocolTest::DidAddMessageToConsole(
   return true;
 }
 
-base::DictionaryValue* DevToolsProtocolTest::SendCommand(
+base::DictionaryValue* DevToolsProtocolTest::SendSessionCommand(
     const std::string& method,
     std::unique_ptr<base::Value> params,
+    const std::string& session_id,
     bool wait) {
   in_dispatch_ = true;
   base::DictionaryValue command;
@@ -57,6 +59,8 @@ base::DictionaryValue* DevToolsProtocolTest::SendCommand(
   command.SetString(kMethodParam, method);
   if (params)
     command.Set(kParamsParam, std::move(params));
+  if (!session_id.empty())
+    command.SetString(kSessionIdParam, session_id);
 
   std::string json_command;
   base::JSONWriter::Write(command, &json_command);
