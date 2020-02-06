a207f825e52ab668a24f91759cb5746a179f1be6
Wenyu Fu <wenyufu@google.com>
2020-02-06 23:12:37 +0000

    Log EnterpriseChange.IsManaged for UMA metric
    Bug: 964139
    Change-Id: I717f9c07920d7843acb93c88566099d8ee92d90a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040392
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739155}

--- chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaSessionStats.java
+++ chrome/android/java/src/org/chromium/chrome/browser/metrics/UmaSessionStats.java
@@ -20,6 +20,7 @@ import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.chrome.browser.DefaultBrowserInfo;
 import org.chromium.chrome.browser.instantapps.InstantAppsHandler;
+import org.chromium.chrome.browser.policy.EnterpriseInfo;
 import org.chromium.chrome.browser.settings.privacy.PrivacyPreferencesManager;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModel;
@@ -122,6 +123,7 @@ public class UmaSessionStats {
         updatePreferences();
         updateMetricsServiceState();
         DefaultBrowserInfo.logDefaultBrowserStats();
+        EnterpriseInfo.logDeviceEnterpriseInfo();
     }
 
     private static void ensureNativeInitialized() {
