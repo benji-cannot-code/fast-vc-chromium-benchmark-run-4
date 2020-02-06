43ccfe20dece09357ad7c60c638c6c367e8f2eaf
Tim Song <tengs@chromium.org>
2020-02-06 00:57:30 +0000

    Ash Tray: Add detailed metrics for notification click events.
    This is a first pass, adding the plumbing and implementing a simple subset of
    the detailed notification interaction metrics. Follow-up CLs will implement the
    rest of the metrics.
    
    BUG=1047053
    
    Change-Id: Idafc1cdf804097710a53f5d312fd9a30a7bcfa45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037025
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738767}

--- ash/system/message_center/message_center_ui_controller.cc
+++ ash/system/message_center/message_center_ui_controller.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/system/message_center/metrics_utils.h"
 #include "base/observer_list.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/message_center_types.h"
@@ -122,6 +123,14 @@ void MessageCenterUiController::OnNotificationClicked(
     const base::Optional<base::string16>& reply) {
   if (popups_visible_)
     OnMessageCenterChanged();
+
+  // Note: we use |message_center_visible_| instead of |popups_visible_| here
+  // due to timing issues when dismissing the last popup notification.
+  bool is_popup = !message_center_visible_;
+  if (button_index.has_value())
+    metrics_utils::LogClickedActionButton(notification_id, is_popup);
+  else
+    metrics_utils::LogClickedBody(notification_id, is_popup);
 }
 
 void MessageCenterUiController::OnNotificationDisplayed(
