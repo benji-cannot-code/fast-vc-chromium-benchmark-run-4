5ffdeeb543372e90147b48a2e382e8edc3635c4b
Tanmoy Mollik <triploblastic@chromium.org>
2020-02-06 11:01:45 +0000

    [Android] Rename kMiceFeature to kMobileIdentityConsistency
    This CL removes some remaining usages of kMiceFeature and renames the
    feature object to kMobileIdentityConsistency to reduce confusion.
    
    Bug: 1046385, 1048635
    Change-Id: Ia0fd886c9a458060b5794f9a53fcd7c3dbba9236
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033518
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Auto-Submit: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738904}

--- components/signin/public/base/account_consistency_method.cc
+++ components/signin/public/base/account_consistency_method.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace signin {
 
 #if defined(OS_ANDROID)
-const base::Feature kMiceFeature{"MobileIdentityConsistency",
-                                 base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kMobileIdentityConsistency{
+    "MobileIdentityConsistency", base::FEATURE_DISABLED_BY_DEFAULT};
 #endif
 
 }  // namespace signin
