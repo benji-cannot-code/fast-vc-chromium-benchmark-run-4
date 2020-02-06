8fc9961de780e341288e7dbafcb1a98854365ded
Steve Becker <stevebe@microsoft.com>
2020-02-06 08:02:24 +0000

    Fix content_browser_client breaks by forward declaring blink::mojom types
    Removes /third_party/blink/public/mojom/ #includes from
    /content/public/browser/content_browser_client.h.  Instead, forward
    declares blink::mojom types.  This fixes sporadic build breaks where
    unluckly builds fail to find generated headers like
    /third_party/blink/public/mojom/badging/badging.mojom-forward.h
    
    Bug: 1049216
    Change-Id: I8df7bf9d12797917f4aa9b3b0193e76317965229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041033
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#738877}

--- content/public/browser/content_browser_client.h
+++ content/public/browser/content_browser_client.h
@@ -45,9 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/websocket.mojom-forward.h"
 #include "storage/browser/file_system/file_system_context.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
-#include "third_party/blink/public/mojom/badging/badging.mojom-forward.h"
-#include "third_party/blink/public/mojom/credentialmanager/credential_manager.mojom-forward.h"
-#include "third_party/blink/public/mojom/web_feature/web_feature.mojom-forward.h"
 #include "ui/accessibility/ax_mode.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
@@ -84,10 +81,13 @@ class SequencedTaskRunner;
 
 namespace blink {
 namespace mojom {
+class BadgeService;
+class CredentialManager;
 class RendererPreferences;
 class RendererPreferenceWatcher;
 class WebUsbService;
 class WindowFeatures;
+enum class WebFeature : int32_t;
 }  // namespace mojom
 class AssociatedInterfaceRegistry;
 class URLLoaderThrottle;
