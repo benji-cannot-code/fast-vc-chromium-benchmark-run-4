dfa07a47e3ae2bd3da7135aef03245ffd4ae35b8
Wan-Teh Chang <wtc@google.com>
2020-02-06 21:53:18 +0000

    Two cleanup changes in media.
    media/base/video_frame_pool.cc: remove an unnecessary test in a while
    loop.
    
    media/filters/dav1d_video_decoder.h: remove an unneeded #include.
    
    Bug: N/A
    Change-Id: I0014cee024c2321382f018bfbbe9065a9e135b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042675
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739114}

--- media/filters/dav1d_video_decoder.h
+++ media/filters/dav1d_video_decoder.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_decoder.h"
 #include "media/base/video_decoder_config.h"
 #include "media/base/video_frame.h"
-#include "media/base/video_frame_pool.h"
 #include "media/filters/offloading_video_decoder.h"
 
 struct Dav1dContext;
