718e3af8549cfff7597af91ad1d7bc58c224f845
Xianzhu Wang <wangxianzhu@chromium.org>
2020-02-06 19:10:15 +0000

    Ensure cc::Layer's NeedsPushProperties when blink directly sets scroll offset
    When blink directly sets scroll offset in the scroll tree, we need to
    set the associated cc::Layer's NeedsPushProperties to ensure update of
    cc::LayerImpl's scroll related properties
    (see cc::LayerImpl::UpdateScrollable()).
    
    This reverts the change about cc::LayerTreeHost::LayerByElementId() in
    https://chromium-review.googlesource.com/c/chromium/src/+/2020554
    because we still need it in layer list mode.
    
    Bug: 1048113
    Change-Id: I686beebcf54e96cc517700c0f987aaceec880c58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036834
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739043}

--- cc/trees/layer_tree_host_unittest_scroll.cc
+++ cc/trees/layer_tree_host_unittest_scroll.cc
@@ -85,9 +85,8 @@ class LayerTreeHostScrollTest : public LayerTreeTest, public ScrollCallbacks {
                  const base::Optional<TargetSnapAreaElementIds>&
                      snap_target_ids) override {
     // Simulates cc client (e.g Blink) behavior when handling impl-side scrolls.
-    SetScrollOffsetFromImplSide(
-        layer_tree_host()->LayerByElementIdForTesting(element_id),
-        scroll_offset);
+    SetScrollOffsetFromImplSide(layer_tree_host()->LayerByElementId(element_id),
+                                scroll_offset);
 
     if (element_id ==
         layer_tree_host()->OuterViewportScrollLayerForTesting()->element_id()) {
@@ -1624,9 +1623,7 @@ class LayerTreeHostScrollTestLayerStructureChange
       EndTest();
       return;
     }
-    layer_tree_host()
-        ->LayerByElementIdForTesting(element_id)
-        ->RemoveFromParent();
+    layer_tree_host()->LayerByElementId(element_id)->RemoveFromParent();
   }
 
  protected:
