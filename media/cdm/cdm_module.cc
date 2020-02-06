bd36f18694b1261599035a289c2a11d288a84657
Keishi Hattori <keishi@chromium.org>
2020-02-06 04:54:54 +0000

    Revert "media: Add a feature for CdmHostVerification"
    This reverts commit 15a1277e6478aa77d87fab99cd101402de3a45d3.
    
    Reason for revert: Suspecting cause for CDM test failures
    https://ci.chromium.org/p/chromium/builders/ci/Network%20Service%20Linux/396
    
    Original change's description:
    > media: Add a feature for CdmHostVerification
    > 
    > By default it's enabled. No effect if ENABLE_CDM_HOST_VERIFICATION
    > buildflag is false.
    > 
    > To disable this feature for testing, type the following in the command
    > line:
    >   --disable-features=CdmHostVerification
    > 
    > Bug: 658036
    > Test: Manually tested with the feature disabled
    > Change-Id: Ia2d077436a1a8445aec29163a88d1bb4539c4811
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036257
    > Reviewed-by: John Rummell <jrummell@chromium.org>
    > Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#738795}
    
    TBR=xhwang@chromium.org,jrummell@chromium.org,hmchen@chromium.org
    
    Change-Id: I6fadbf600d281e3693001aab5abf9233cfa4c916
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 658036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040642
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738842}

--- media/cdm/cdm_module.cc
+++ media/cdm/cdm_module.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crash/core/common/crash_key.h"
 
 #if BUILDFLAG(ENABLE_CDM_HOST_VERIFICATION)
-#include "base/feature_list.h"
-#include "media/base/media_switches.h"
 #include "media/cdm/cdm_host_files.h"
 #endif  // BUILDFLAG(ENABLE_CDM_HOST_VERIFICATION)
 
@@ -183,8 +181,7 @@ bool CdmModule::Initialize(const base::FilePath& cdm_path) {
 #endif  // defined(OS_WIN)
 
 #if BUILDFLAG(ENABLE_CDM_HOST_VERIFICATION)
-  if (base::FeatureList::IsEnabled(media::kCdmHostVerification))
-    InitCdmHostVerification(library_.get(), cdm_path_, cdm_host_file_paths);
+  InitCdmHostVerification(library_.get(), cdm_path_, cdm_host_file_paths);
 #endif  // BUILDFLAG(ENABLE_CDM_HOST_VERIFICATION)
 
   ReportLoadResult(LoadResult::kLoadSuccess);
