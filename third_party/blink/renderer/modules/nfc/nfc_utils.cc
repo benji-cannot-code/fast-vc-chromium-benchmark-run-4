82fe6ede816c4fc507e8c8cf6664b19c14730abd
Leon Han <leon.han@intel.com>
2020-02-06 10:46:11 +0000

    [webnfc] Remove the limitation on the size of NDEF message
    Per the discussion at
    https://github.com/w3c/web-nfc/issues/523#issuecomment-581785547,
    such size limitation does not make much sense and does not bring much
    benefit to WebDevs.
    
    The spec issue:
    https://github.com/w3c/web-nfc/issues/523
    
    BUG=520391,1048096
    
    Change-Id: Id5fcc8c349ac220c460646f055b78b812da8aeba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037310
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Commit-Queue: Leon Han <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#738900}

--- third_party/blink/renderer/modules/nfc/nfc_utils.cc
+++ third_party/blink/renderer/modules/nfc/nfc_utils.cc
@@ -13,15 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-size_t GetNDEFMessageSize(const device::mojom::blink::NDEFMessage& message) {
-  size_t message_size = 0;
-  for (wtf_size_t i = 0; i < message.data.size(); ++i) {
-    message_size += message.data[i]->media_type.CharactersSizeInBytes();
-    message_size += message.data[i]->data.size();
-  }
-  return message_size;
-}
-
 DOMException* NDEFErrorTypeToDOMException(
     device::mojom::blink::NDEFErrorType error_type,
     const String& error_message) {
