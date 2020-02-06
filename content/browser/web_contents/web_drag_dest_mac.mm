e8dcc41221e9b7eab3fb0fea302dbc6a6856bdf1
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-02-06 16:12:32 +0000

    Hook Deep Scanning drag+drop on MacOS
    This change was started in crrev.com/c/1814619 but is a no-op on Mac
    without the changes in this CL. This only changes the drag+drop
    behavior of users with the appropriate policies enabled and doesn't
    affect other users.
    
    Bug: 1046921
    Change-Id: I3603ae28a730472e6f88f0281c72720c4a71be03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037450
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738971}

--- content/browser/web_contents/web_drag_dest_mac.mm
+++ content/browser/web_contents/web_drag_dest_mac.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/common/web_contents_ns_view_bridge.mojom.h"
 #include "content/public/browser/web_contents_delegate.h"
+#include "content/public/browser/web_contents_view_delegate.h"
 #include "content/public/browser/web_drag_dest_delegate.h"
 #include "content/public/common/child_process_host.h"
 #include "content/public/common/drop_data.h"
@@ -34,6 +35,26 @@ using content::OpenURLParams;
 using content::Referrer;
 using content::WebContentsImpl;
 using remote_cocoa::mojom::DraggingInfo;
+
+namespace content {
+
+DropContext::DropContext(
+    const content::DropData drop_data,
+    const gfx::PointF client_pt,
+    const gfx::PointF screen_pt,
+    int modifier_flags,
+    base::WeakPtr<content::RenderWidgetHostImpl> target_rwh)
+    : drop_data(drop_data),
+      client_pt(client_pt),
+      screen_pt(screen_pt),
+      modifier_flags(modifier_flags),
+      target_rwh(target_rwh) {}
+
+DropContext::DropContext(const DropContext& other) = default;
+
+DropContext::~DropContext() = default;
+
+}  // namespace content
 
 namespace {
 
@@ -67,6 +88,22 @@ int GetModifierFlags() {
 content::GlobalRoutingID GetRenderViewHostID(content::RenderViewHost* rvh) {
   return content::GlobalRoutingID(rvh->GetProcess()->GetID(),
                                   rvh->GetRoutingID());
+}
+
+void DropCompletionCallback(
+    WebDragDest* drag_dest,
+    const content::DropContext context,
+    content::WebContentsViewDelegate::DropCompletionResult result) {
+  // This is an async callback. Make sure RWH is still valid.
+  if (!context.target_rwh ||
+      ![drag_dest isValidDragTarget:context.target_rwh.get()]) {
+    return;
+  }
+
+  bool success =
+      result ==
+      content::WebContentsViewDelegate::DropCompletionResult::kContinue;
+  [drag_dest completeDropAsync:success withContext:context];
 }
 
 }  // namespace
@@ -281,7 +318,9 @@ content::GlobalRoutingID GetRenderViewHostID(content::RenderViewHost* rvh) {
   return _currentOperation;
 }
 
-- (BOOL)performDragOperation:(const DraggingInfo*)info {
+- (BOOL)performDragOperation:(const DraggingInfo*)info
+    withWebContentsViewDelegate:
+        (content::WebContentsViewDelegate*)webContentsViewDelegate {
   gfx::PointF transformedPt;
   content::RenderWidgetHostImpl* targetRWH =
       [self GetRenderWidgetHostAtPoint:info->location_in_view
@@ -309,19 +348,44 @@ content::GlobalRoutingID GetRenderViewHostID(content::RenderViewHost* rvh) {
     }
   }
 
-  if (_delegate)
-    _delegate->OnDrop();
-
   _currentRVH = NULL;
-
   _webContents->Focus();
-  targetRWH->DragTargetDrop(*_dropDataFiltered, transformedPt,
-                            info->location_in_screen, GetModifierFlags());
 
+  if (webContentsViewDelegate) {
+    content::DropContext context(/*drop_data=*/*_dropDataFiltered,
+                                 /*client_pt=*/transformedPt,
+                                 /*screen_pt=*/info->location_in_screen,
+                                 /*modifier_flags=*/GetModifierFlags(),
+                                 /*target_rwh=*/targetRWH->GetWeakPtr());
+
+    webContentsViewDelegate->OnPerformDrop(
+        context.drop_data,
+        base::BindOnce(&DropCompletionCallback, self, context));
+  } else {
+    if (_delegate)
+      _delegate->OnDrop();
+    targetRWH->DragTargetDrop(*_dropDataFiltered, transformedPt,
+                              info->location_in_screen, GetModifierFlags());
+  }
   _dropDataUnfiltered.reset();
   _dropDataFiltered.reset();
 
   return YES;
+}
+
+- (void)completeDropAsync:(BOOL)success
+              withContext:(const content::DropContext)context {
+  if (success) {
+    if (_delegate)
+      _delegate->OnDrop();
+    context.target_rwh->DragTargetDrop(context.drop_data, context.client_pt,
+                                       context.screen_pt,
+                                       context.modifier_flags);
+  } else {
+    if (_delegate)
+      _delegate->OnDragLeave();
+    context.target_rwh->DragTargetDragLeave(gfx::PointF(), gfx::PointF());
+  }
 }
 
 - (content::RenderWidgetHostImpl*)
