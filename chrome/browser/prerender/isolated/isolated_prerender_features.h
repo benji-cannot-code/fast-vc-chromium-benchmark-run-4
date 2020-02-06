923ba34f5cb11af21439bc693b9156d1bcb970f1
Robert Ogden <robertogden@chromium.org>
2020-02-06 02:48:19 +0000

    Add optional origin probing to IsolatedPrerenders
    Blocks navigation to a prefetched page on the success of a probe to the
    origin without the use of a proxy. Default off.
    
    Bug: 1023486
    Change-Id: I0c265bc458c39a0caa4a8da8ceb48c9c326dbba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2025835
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738809}

--- chrome/browser/prerender/isolated/isolated_prerender_features.h
+++ chrome/browser/prerender/isolated/isolated_prerender_features.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 
 extern const base::Feature kIsolatePrerenders;
+extern const base::Feature kIsolatePrerendersMustProbeOrigin;
 extern const base::Feature kIsolatedPrerenderUsesProxy;
 extern const base::Feature kPrefetchSRPNavigationPredictions_HTMLOnly;
 
