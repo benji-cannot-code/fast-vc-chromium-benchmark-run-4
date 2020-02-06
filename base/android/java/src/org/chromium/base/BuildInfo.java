082c6a3d2196c93c152ae0870e62f728f3b6b6fb
Nate Fischer <ntfschr@chromium.org>
2020-02-06 19:11:33 +0000

    AW: disallow secure cookies with HTTP on R
    This adds a targetSdk quirk for WebView's CookieManager API. Starting
    for apps which target Android R, WebView will no longer accept cookies
    for insecure ('http' scheme) URLs which also specify the 'Secure' cookie
    directive.
    
    While this changed for Chrome in M58
    (https://www.chromestatus.com/feature/4506322921848832), WebView has
    been working around this by fixing up the URL's scheme to 'https'.
    WebView will continue to do so for apps targeting < R.
    
    This adds a histogram to track the new case.
    
    Bug: 933981
    Test: run_webview_instrumentation_test_apk -f CookieManagerTest#testSetSecureCookieForHttpUrl*
    Change-Id: Ie31f02911d38984983fdf4afeb4d6e1eb4f7d094
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040685
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739044}

--- base/android/java/src/org/chromium/base/BuildInfo.java
+++ base/android/java/src/org/chromium/base/BuildInfo.java
@@ -85,6 +85,7 @@ public class BuildInfo {
                 buildInfo.resourcesVersion,
                 buildInfo.extractedFileSuffix,
                 isAtLeastQ() ? "1" : "0",
+                targetsAtLeastR() ? "1" : "0",
                 isDebugAndroid() ? "1" : "0",
         };
     }
