512f8b9a9f24ae67f42be29a9ba0882d9078f77f
liberato@chromium.org <liberato@chromium.org>
2020-02-06 22:45:16 +0000

    Added ErrorOr<> for MediaError returns.
    ErrorOr<> makes it easier to return an arbitrary value or an error.
    For example, one might:
    
    ErrorOr<std::unique_ptr<int>> Foo() {
      // return a ptr, or a MediaError explaining why not.
    }
    
    Change-Id: I819b29c1dcc9ba2512ab709da2ae6b267a24d8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040374
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Ted Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739144}

--- media/base/media_error_codes.h
+++ media/base/media_error_codes.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+// NOTE: These numbers are still subject to change.  Do not use for things like
+// UMA yet!
 enum class ErrorCode : uint32_t {
   kOk = 0,
   kCodeOnlyForTesting = std::numeric_limits<uint32_t>::max(),
