689860db7842e1b0e9a870af62b297d3a61c621c
Richard Knoll <knollr@chromium.org>
2020-02-06 09:10:06 +0000

    Cleanup ClickToCall RegexV2 code
    The experiment has been stopped so cleanup code that was used to collect
    metrics on it. Experiment results: go/click-to-call-regex-v2-experiment
    
    Bug: 1001033
    Change-Id: Ib02a56068b2489f46a2d4c89a960cce68b3d997c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038771
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738884}

--- chrome/browser/sharing/click_to_call/click_to_call_metrics.h
+++ chrome/browser/sharing/click_to_call/click_to_call_metrics.h
@@ -6,10 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SHARING_CLICK_TO_CALL_CLICK_TO_CALL_METRICS_H_
 #define CHROME_BROWSER_SHARING_CLICK_TO_CALL_CLICK_TO_CALL_METRICS_H_
 
-#include <string>
-
 #include "base/timer/elapsed_timer.h"
-#include "chrome/browser/sharing/click_to_call/phone_number_regex.h"
 
 namespace content {
 class WebContents;
@@ -37,23 +34,11 @@ enum class SharingClickToCallSelection {
   kMaxValue = kApp,
 };
 
-// Result of comparing the simple regex with one of the available variants.
-// These values are logged to UMA. Entries should not be renumbered and numeric
-// values should never be reused. Please keep in sync with
-// "PhoneNumberRegexVariantResult" in src/tools/metrics/histograms/enums.xml.
-enum class PhoneNumberRegexVariantResult {
-  kNoneMatch = 0,
-  kOnlySimpleMatches = 1,
-  kOnlyVariantMatches = 2,
-  kBothMatch = 3,
-  kMaxValue = kBothMatch,
-};
-
 // TODO(himanshujaju): Make it generic and move to base/metrics/histogram_base.h
 // Used to Log delay in parsing phone number in highlighted text to UMA.
 class ScopedUmaHistogramMicrosecondsTimer {
  public:
-  explicit ScopedUmaHistogramMicrosecondsTimer(PhoneNumberRegexVariant variant);
+  ScopedUmaHistogramMicrosecondsTimer();
   ScopedUmaHistogramMicrosecondsTimer(
       const ScopedUmaHistogramMicrosecondsTimer&) = delete;
   ScopedUmaHistogramMicrosecondsTimer& operator=(
@@ -61,7 +46,6 @@ class ScopedUmaHistogramMicrosecondsTimer {
   ~ScopedUmaHistogramMicrosecondsTimer();
 
  private:
-  const PhoneNumberRegexVariant variant_;
   const base::ElapsedTimer timer_;
 };
 
@@ -72,15 +56,5 @@ void LogClickToCallUKM(content::WebContents* web_contents,
                        bool has_devices,
                        bool has_apps,
                        SharingClickToCallSelection selection);
-
-// Logs the raw phone number length and the number of digits in it.
-void LogClickToCallPhoneNumberSize(const std::string& number,
-                                   SharingClickToCallEntryPoint entry_point,
-                                   bool send_to_device);
-
-// Compares alternative regexes to the default one on a low priority task runner
-// and records the results to UMA.
-void LogPhoneNumberDetectionMetrics(const std::string& selection_text,
-                                    bool sent_to_device);
 
 #endif  // CHROME_BROWSER_SHARING_CLICK_TO_CALL_CLICK_TO_CALL_METRICS_H_
--- chrome/browser/sharing/click_to_call/click_to_call_utils.h
+++ chrome/browser/sharing/click_to_call/click_to_call_utils.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/optional.h"
-#include "chrome/browser/sharing/click_to_call/phone_number_regex.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -29,8 +28,7 @@ base::Optional<std::string> ExtractPhoneNumberForClickToCall(
 // Returns the first possible phone number in |selection_text| given the
 // |regex_variant| to be used or base::nullopt if the regex did not match.
 base::Optional<std::string> ExtractPhoneNumber(
-    const std::string& selection_text,
-    PhoneNumberRegexVariant regex_variant);
+    const std::string& selection_text);
 
 // Unescapes and returns the URL contents.
 std::string GetUnescapedURLContent(const GURL& url);
