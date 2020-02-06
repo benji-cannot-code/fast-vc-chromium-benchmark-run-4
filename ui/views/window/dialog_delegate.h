e21ded9186aa46b1ed6edf0be733537bfecc7efb
Elly Fong-Jones <ellyjones@chromium.org>
2020-02-06 21:09:04 +0000

    views: never deliver multiple dialog closure callbacks
    Currently, if a DialogDelegate's Accept() or Cancel() method leads back
    into its Close(), two of the registered callbacks can be run, which is a
    violation of DialogDelegate's contract and causes confusing workarounds
    in client code. This often happens in code like this:
    
    void SomeBubble::OnDialogAccepted() {
      NavigateTo(kSomeURL);
    }
    
    where NavigateTo causes a page navigation synchronously, which causes
    the involved bubble to be closed.
    
    Instead:
    
    * In the near term, DialogDelegate should ensure that it never delivers
      more than one of these callbacks
    * In the far term, DialogDelegate should enforce that callers do not
      re-enter Close() from inside Accept() or Cancel()
    
    The latter will likely need to wait for the Accept/Cancel/Close callback
    refactor to be finished.
    
    This change also removes the remnants of the GlobalErrorBubbleView unit test
    suite, which were exercising this behavior deliberately. These tests
    were providing very little actual coverage and I have been gradually
    deleting them as I refactor GlobalError anyway.
    
    Bug: 1011446
    Change-Id: Ifd76330c0f79d04ce2e5741c5a1977159ee598d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042252
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739097}

--- ui/views/window/dialog_delegate.h
+++ ui/views/window/dialog_delegate.h
@@ -282,6 +282,10 @@ class VIEWS_EXPORT DialogDelegate : public WidgetDelegate {
   // Close() above.
   bool DefaultClose();
 
+  // Runs a close callback, ensuring that at most one close callback is ever
+  // run.
+  void RunCloseCallback(base::OnceClosure callback);
+
   // The margins between the content and the inside of the border.
   // TODO(crbug.com/733040): Most subclasses assume they must set their own
   // margins explicitly, so we set them to 0 here for now to avoid doubled
@@ -307,6 +311,9 @@ class VIEWS_EXPORT DialogDelegate : public WidgetDelegate {
   base::OnceClosure accept_callback_;
   base::OnceClosure cancel_callback_;
   base::OnceClosure close_callback_;
+
+  // Whether any of the three callbacks just above has been delivered yet.
+  bool callback_delivered_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(DialogDelegate);
 };
