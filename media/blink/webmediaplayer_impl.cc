a8dac62cb5401d2791d45a3d3694a3a0922e6fa8
Thomas Guilbert <tguilbert@chromium.org>
2020-02-06 01:08:15 +0000

    Set OnFramePresentedCB from main thread
    Currently, we jump to the compositor thread in order to set the
    new frame presented callback on the VideoFrameCompositor.
    
    This CL is a small change that allows us to set that callback from the
    main thread. This should help minimize the amount of frames we miss in
    video.rAF calls.
    
    Bug: 1012063
    Change-Id: I521eb0bf0a6e71f59552c60276006e1884c1f989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040436
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#738770}

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -3333,13 +3333,8 @@ base::Optional<viz::SurfaceId> WebMediaPlayerImpl::GetSurfaceId() {
 }
 
 void WebMediaPlayerImpl::RequestAnimationFrame() {
-  vfc_task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(&VideoFrameCompositor::SetOnFramePresentedCallback,
-                     base::Unretained(compositor_.get()),
-                     BindToCurrentLoop(base::BindOnce(
-                         &WebMediaPlayerImpl::OnNewFramePresentedCallback,
-                         weak_factory_.GetWeakPtr()))));
+  compositor_->SetOnFramePresentedCallback(BindToCurrentLoop(base::BindOnce(
+      &WebMediaPlayerImpl::OnNewFramePresentedCallback, weak_this_)));
 }
 
 void WebMediaPlayerImpl::OnNewFramePresentedCallback(
