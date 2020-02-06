5c98ebb181560115be3900a098db877875b720dc
Maria Kazinova <kazinova@google.com>
2020-02-06 10:06:18 +0000

    Added new metric to check how many FormManagers are accumulated.
    Bug: 1047445
    
    Change-Id: I6c90c4525186effe778af2c1b9bb33026774e246
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033251
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Auto-Submit: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/master@{#738892}

--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -319,6 +319,8 @@ void PasswordManager::DidNavigateMainFrame(bool form_may_be_submitted) {
       break;
     }
   }
+  UMA_HISTOGRAM_COUNTS_1000("PasswordManager.NumFormManagersCleared",
+                            form_managers_.size());
   form_managers_.clear();
 
   TryToFindPredictionsToPossibleUsernameData();
