7a40f9b6befa59d0ea69457784188b8503104908
Maksim Moskvitin <mmoskvitin@google.com>
2020-02-06 15:44:56 +0000

    [Invalidations] Do not update subscribed topics on duplicates
    InvalidatorRegistarWithMemory didn't update registrations when there
    are duplicates, but *did* update subscriptions. This behavior is
    confusing and may lead to the future bugs.
    
    This patch makes subscription/registration logic consistent.
    
    Bug: 1020117
    Change-Id: I6ddf5f84f35517570c8c246c415106e71038859b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041470
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/master@{#738964}

--- components/invalidation/impl/fake_invalidation_handler.cc
+++ components/invalidation/impl/fake_invalidation_handler.cc
@@ -9,9 +9,15 @@ namespace syncer {
 
 FakeInvalidationHandler::FakeInvalidationHandler()
     : state_(DEFAULT_INVALIDATION_ERROR),
-      invalidation_count_(0) {}
+      invalidation_count_(0),
+      owner_name_("Fake") {}
 
-FakeInvalidationHandler::~FakeInvalidationHandler() {}
+FakeInvalidationHandler::FakeInvalidationHandler(const std::string& owner_name)
+    : FakeInvalidationHandler() {
+  owner_name_ = owner_name;
+}
+
+FakeInvalidationHandler::~FakeInvalidationHandler() = default;
 
 InvalidatorState FakeInvalidationHandler::GetInvalidatorState() const {
   return state_;
@@ -36,7 +42,9 @@ void FakeInvalidationHandler::OnIncomingInvalidation(
   ++invalidation_count_;
 }
 
-std::string FakeInvalidationHandler::GetOwnerName() const { return "Fake"; }
+std::string FakeInvalidationHandler::GetOwnerName() const {
+  return owner_name_;
+}
 
 bool FakeInvalidationHandler::IsPublicTopic(const syncer::Topic& topic) const {
   return topic == "PREFERENCE";
