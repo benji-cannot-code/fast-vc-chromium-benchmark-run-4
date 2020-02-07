1cc93cd455d93913093121b604a33da324337f82
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-02-07 08:44:43 +0000

    [Passwords] Add Skeleton for Password Check page
    This change adds a skeleton for the upcoming Password Check page and
    provides a flag guarded entry point from chrome://settings/passwords.
    
    Bug: 1047726
    Change-Id: I4f4100d6f1de4021fdf6e9ddaf86bf800949849e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036176
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739307}

--- chrome/browser/resources/settings/autofill_page/passwords_section.js
+++ chrome/browser/resources/settings/autofill_page/passwords_section.js
@@ -88,6 +88,14 @@ Polymer({
     },
 
     /** @private */
+    enablePasswordCheck_: {
+      type: Boolean,
+      value() {
+        return loadTimeData.getBoolean('enablePasswordCheck');
+      }
+    },
+
+    /** @private */
     hidePasswordsLink_: {
       type: Boolean,
       computed: 'computeHidePasswordsLink_(syncPrefs_, syncStatus_)',
@@ -265,6 +273,14 @@ Polymer({
     if (cr.toastManager.getToastManager().isToastOpen) {
       cr.toastManager.getToastManager().hide();
     }
+  },
+
+  /**
+   * Shows the check passwords sub page.
+   * @private
+   */
+  onCheckPasswordsClick_() {
+    settings.Router.getInstance().navigateTo(settings.routes.CHECK_PASSWORDS);
   },
 
   // <if expr="chromeos">
