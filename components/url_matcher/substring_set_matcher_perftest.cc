5b7d99af8df33f34eedbd4b9553d73d91cf66cc3
Karan Bhatia <karandeepb@chromium.org>
2020-02-07 22:54:29 +0000

    UrlMatcher: Correct SubstringSetMatcher::EstimateMemoryUsage semantics.
    EstimateMemoryUsage should only report the dynamically allocated
    memory. Correct SubstringSetMatcher::EstimateMemoryUsage for the
    same.
    
    BUG=974391
    
    Change-Id: I0170be43d88e19d81d563d441b5cb680eb99fbab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042140
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739590}

--- components/url_matcher/substring_set_matcher_perftest.cc
+++ components/url_matcher/substring_set_matcher_perftest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
+#include "base/trace_event/memory_usage_estimator.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/perf/perf_result_reporter.h"
 
@@ -50,14 +51,17 @@ TEST(SubstringSetMatcherPerfTest, HundredThousandKeys) {
     patterns.emplace_back(GetString(kPatternLen), i);
 
   base::ElapsedTimer init_timer;
-  SubstringSetMatcher matcher(patterns);
+
+  // Allocate SubstringSetMatcher on the heap so that EstimateMemoryUsage below
+  // also includes its stack allocated memory.
+  auto matcher = std::make_unique<SubstringSetMatcher>(patterns);
   base::TimeDelta init_time = init_timer.Elapsed();
 
   // Match patterns against a string of 5000 characters.
   const size_t kTextLen = 5000;
   base::ElapsedTimer match_timer;
   std::set<StringPattern::ID> matches;
-  matcher.Match(GetString(kTextLen), &matches);
+  matcher->Match(GetString(kTextLen), &matches);
   base::TimeDelta match_time = match_timer.Elapsed();
 
   const char* kInitializationTime = ".init_time";
@@ -71,8 +75,9 @@ TEST(SubstringSetMatcherPerfTest, HundredThousandKeys) {
 
   reporter.AddResult(kInitializationTime, init_time);
   reporter.AddResult(kMatchTime, match_time);
-  reporter.AddResult(kMemoryUsage,
-                     (matcher.EstimateMemoryUsage() * 1.0 / (1 << 20)));
+  reporter.AddResult(
+      kMemoryUsage,
+      (base::trace_event::EstimateMemoryUsage(matcher) * 1.0 / (1 << 20)));
 }
 
 }  // namespace
