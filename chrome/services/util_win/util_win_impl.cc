de5f802eb3053fa98390b1301963c271cd129f03
Finnur Thorarinsson <finnur@chromium.org>
2020-02-07 09:49:07 +0000

    Revert "Add more System Information collection via WMI"
    This reverts commit e8b2953693c1f232678eaa7097407d99d8255d08.
    
    Reason for revert: Fails consistently on the bots.
    
    See errors in build where CL was introduced...
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%281%29/99608
    
    Original change's description:
    > Add more System Information collection via WMI
    > 
    > This CL adds code to collect metrics regarding the processor that the
    > user is running on.
    > 
    > R=​rockot@chromium.org
    > 
    > Bug: 1032858
    > Change-Id: I38628c722849071c8d0219ce942a257fc0623424
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982237
    > Commit-Queue: Rahul Chaturvedi <rkc@chromium.org>
    > Auto-Submit: Rahul Chaturvedi <rkc@chromium.org>
    > Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#739252}
    
    TBR=rkc@chromium.org,thestig@chromium.org,isherman@chromium.org,rockot@google.com,asvitkine@chromium.org,wfh@chromium.org,sebmarchand@chromium.org
    
    Change-Id: I0e6cb122d9df1c246e1b8ff35e845d5798537104
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1032858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043472
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739324}

--- chrome/services/util_win/util_win_impl.cc
+++ chrome/services/util_win/util_win_impl.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/win/conflicts/module_info_util.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/services/util_win/av_products.h"
-#include "chrome/services/util_win/processor_metrics.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "third_party/metrics_proto/system_profile.pb.h"
 #include "ui/shell_dialogs/execute_select_file_win.h"
@@ -258,13 +257,4 @@ void UtilWinImpl::GetAntiVirusProducts(bool report_full_names,
                                        GetAntiVirusProductsCallback callback) {
   base::win::ScopedCOMInitializer scoped_com_initializer;
   std::move(callback).Run(::GetAntiVirusProducts(report_full_names));
-}
-
-void UtilWinImpl::RecordProcessorMetrics(
-    RecordProcessorMetricsCallback callback) {
-  // TODO(sebmarchand): Check if we should move the ScopedCOMInitializer to the
-  // UtilWinImpl class.
-  base::win::ScopedCOMInitializer scoped_com_initializer;
-  ::RecordProcessorMetrics();
-  std::move(callback).Run();
 }
