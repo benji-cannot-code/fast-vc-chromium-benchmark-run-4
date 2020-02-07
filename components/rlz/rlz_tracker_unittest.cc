42a724f213de468ee83db1dfc4e35c9e02624b9f
Ayu Ishii <ayui@chromium.org>
2020-02-07 17:50:43 +0000

    [Reland] Convert base::Bind and base::Callback in components/rlz to Once/Repeating
    > Original CL: https://crrev.com/c/1935649
    > Revert CL: https://crrev.com/c/1949080
    
    Updated with changes to ios/chrome/browser/rlz/rlz_tracker_delegate_impl.h to
    new signature.
    
    Change-Id: Ic78393d6bfe1d40f0e2721e828c4b00de36350e8
    Bug: 1007725
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1949038
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739456}

--- components/rlz/rlz_tracker_unittest.cc
+++ components/rlz/rlz_tracker_unittest.cc
@@ -50,19 +50,13 @@ class TestRLZTrackerDelegate : public RLZTrackerDelegate {
   }
 
   void SimulateOmniboxUsage() {
-    using std::swap;
-    base::Closure callback;
-    swap(callback, on_omnibox_search_callback_);
-    if (!callback.is_null())
-      callback.Run();
+    if (!on_omnibox_search_callback_.is_null())
+      std::move(on_omnibox_search_callback_).Run();
   }
 
   void SimulateHomepageUsage() {
-    using std::swap;
-    base::Closure callback;
-    swap(callback, on_homepage_search_callback_);
-    if (!callback.is_null())
-      callback.Run();
+    if (!on_homepage_search_callback_.is_null())
+      std::move(on_homepage_search_callback_).Run();
   }
 
   // RLZTrackerDelegate implementation.
@@ -101,14 +95,14 @@ class TestRLZTrackerDelegate : public RLZTrackerDelegate {
 
   bool ClearReferral() override { return true; }
 
-  void SetOmniboxSearchCallback(const base::Closure& callback) override {
+  void SetOmniboxSearchCallback(base::OnceClosure callback) override {
     DCHECK(!callback.is_null());
-    on_omnibox_search_callback_ = callback;
+    on_omnibox_search_callback_ = std::move(callback);
   }
 
-  void SetHomepageSearchCallback(const base::Closure& callback) override {
+  void SetHomepageSearchCallback(base::OnceClosure callback) override {
     DCHECK(!callback.is_null());
-    on_homepage_search_callback_ = callback;
+    on_homepage_search_callback_ = std::move(callback);
   }
 
   // A speculative fix for https://crbug.com/907379.
@@ -119,8 +113,8 @@ class TestRLZTrackerDelegate : public RLZTrackerDelegate {
 
   std::string brand_override_;
   std::string reactivation_brand_override_;
-  base::Closure on_omnibox_search_callback_;
-  base::Closure on_homepage_search_callback_;
+  base::OnceClosure on_omnibox_search_callback_;
+  base::OnceClosure on_homepage_search_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(TestRLZTrackerDelegate);
 };
--- ios/chrome/browser/rlz/rlz_tracker_delegate_impl.cc
+++ ios/chrome/browser/rlz/rlz_tracker_delegate_impl.cc
@@ -102,9 +102,9 @@ bool RLZTrackerDelegateImpl::ClearReferral() {
 }
 
 void RLZTrackerDelegateImpl::SetOmniboxSearchCallback(
-    const base::Closure& callback) {
+    base::OnceClosure callback) {
   DCHECK(!callback.is_null());
-  on_omnibox_search_callback_ = callback;
+  on_omnibox_search_callback_ = std::move(callback);
   on_omnibox_url_opened_subscription_ =
       OmniboxEventGlobalTracker::GetInstance()->RegisterCallback(
           base::Bind(&RLZTrackerDelegateImpl::OnURLOpenedFromOmnibox,
@@ -112,7 +112,7 @@ void RLZTrackerDelegateImpl::SetOmniboxSearchCallback(
 }
 
 void RLZTrackerDelegateImpl::SetHomepageSearchCallback(
-    const base::Closure& callback) {
+    base::OnceClosure callback) {
   NOTREACHED();
 }
 
@@ -131,9 +131,6 @@ void RLZTrackerDelegateImpl::OnURLOpenedFromOmnibox(OmniboxLog* log) {
 
   on_omnibox_url_opened_subscription_.reset();
 
-  using std::swap;
-  base::Closure callback_to_run;
-  swap(callback_to_run, on_omnibox_search_callback_);
-  if (!callback_to_run.is_null())
-    callback_to_run.Run();
+  if (!on_omnibox_search_callback_.is_null())
+    std::move(on_omnibox_search_callback_).Run();
 }
