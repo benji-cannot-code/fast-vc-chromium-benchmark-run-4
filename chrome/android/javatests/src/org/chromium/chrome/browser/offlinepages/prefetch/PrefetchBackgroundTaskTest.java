4aee97227d71404eb548f3e3c164dfce4b8e7a09
Shakti Sahu <shaktisahu@chromium.org>
2020-02-07 11:09:33 +0000

    Background task scheduler : Moved NativeBackgroundTask to components
    This CL moves NativeBackgroundTask to components. The chrome specific
    code is moved to a NativeBackgroundTaskDelegate class which will be
    injected from a factory.
    
    Bug: 1045765
    Change-Id: I53c0e9c73947f9f939e953096d98a2a2412ae4c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024165
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739340}

--- chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchBackgroundTaskTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/offlinepages/prefetch/PrefetchBackgroundTaskTest.java
@@ -23,6 +23,7 @@ import org.chromium.base.task.PostTask;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.ChromeSwitches;
+import org.chromium.chrome.browser.background_task_scheduler.ChromeNativeBackgroundTaskDelegate;
 import org.chromium.chrome.browser.offlinepages.OfflineTestUtil;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ReducedModeNativeTestRule;
@@ -58,6 +59,7 @@ public class PrefetchBackgroundTaskTest {
 
         public TestPrefetchBackgroundTask(TaskInfo taskInfo) {
             mTaskInfo = taskInfo;
+            setDelegate(new ChromeNativeBackgroundTaskDelegate());
         }
 
         public void startTask(Context context, final TaskFinishedCallback callback) {
