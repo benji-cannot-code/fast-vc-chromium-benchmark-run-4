3566ce4a0516f7d80c3aab1dbdddba9f8d61ca76
Yue Zhang <yuezhanggg@chromium.org>
2020-02-07 23:43:34 +0000

    Refactor TabGridIph component to use MessageService APIs
    Bug: 1002678, 1004897
    Change-Id: I10dfc56587b4c5d1c6cc5ddbecd9f40f60ef4368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2026380
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739613}

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphDialogCoordinator.java
@@ -0,0 +1,54 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
+
+/**
+ * Coordinator for the IPH dialog in {@link TabSwitcherMediator}
+ */
+class TabGridIphDialogCoordinator {
+    /**
+     * Interface to control the IPH dialog.
+     */
+    interface IphController {
+        /**
+         * Show the dialog with IPH.
+         */
+        void showIph();
+    }
+    private final PropertyModelChangeProcessor mModelChangeProcessor;
+    private final TabGridIphDialogMediator mMediator;
+    private final TabGridIphDialogParent mIphDialogParent;
+
+    TabGridIphDialogCoordinator(Context context, ViewGroup parent) {
+        PropertyModel iphItemPropertyModel = new PropertyModel(TabGridIphDialogProperties.ALL_KEYS);
+        mIphDialogParent = new TabGridIphDialogParent(context, parent);
+
+        mModelChangeProcessor = PropertyModelChangeProcessor.create(
+                iphItemPropertyModel, mIphDialogParent, TabGridIphDialogViewBinder::bind);
+
+        mMediator = new TabGridIphDialogMediator(iphItemPropertyModel);
+    }
+
+    /**
+     * Get the controller to show IPH dialog.
+     * @return The {@link IphController} used to show IPH.
+     */
+    IphController getIphController() {
+        return mMediator;
+    }
+
+    /** Destroy the IPH component. */
+    public void destroy() {
+        mModelChangeProcessor.destroy();
+        mIphDialogParent.destroy();
+    }
+}
---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIphDialogMediator.java
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import android.view.View;
+
+import org.chromium.chrome.browser.widget.ScrimView;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/**
+ * A mediator for the TabGridIphDialog component, responsible for communicating
+ * with the components' coordinator as well as managing the business logic
+ * for IPH item show/hide.
+ */
+class TabGridIphDialogMediator implements TabGridIphDialogCoordinator.IphController {
+    private PropertyModel mModel;
+
+    TabGridIphDialogMediator(PropertyModel model) {
+        mModel = model;
+
+        View.OnClickListener closeIPHDialogOnClickListener = view -> {
+            mModel.set(TabGridIphDialogProperties.IS_VISIBLE, false);
+        };
+        mModel.set(TabGridIphDialogProperties.CLOSE_BUTTON_LISTENER, closeIPHDialogOnClickListener);
+        ScrimView.ScrimObserver observer = new ScrimView.ScrimObserver() {
+            @Override
+            public void onScrimClick() {
+                mModel.set(TabGridIphDialogProperties.IS_VISIBLE, false);
+            }
+
+            @Override
+            public void onScrimVisibilityChanged(boolean visible) {}
+        };
+        mModel.set(TabGridIphDialogProperties.SCRIM_VIEW_OBSERVER, observer);
+    }
+
+    @Override
+    public void showIph() {
+        mModel.set(TabGridIphDialogProperties.IS_VISIBLE, true);
+    }
+}
