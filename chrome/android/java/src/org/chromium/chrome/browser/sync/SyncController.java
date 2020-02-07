cdf109bf689e6e9408a2089e8d52cbda0c7f0675
Boris Sazonov <bsazonov@chromium.org>
2020-02-07 09:19:58 +0000

    [Signin][Android] Remove unused ChromeSigninController from SyncController
    Removes unused mChromeSigninController field from SyncController.
    
    Bug: 1046412
    Change-Id: Ie7f77000e075bf0dcb3c80b33533ac49a52cd52d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041608
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739319}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/SyncController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/SyncController.java
@@ -23,7 +23,6 @@ import org.chromium.chrome.browser.identity.UniqueIdentificationGeneratorFactory
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.SigninManager;
-import org.chromium.components.signin.ChromeSigninController;
 import org.chromium.components.sync.AndroidSyncSettings;
 import org.chromium.components.sync.ModelType;
 import org.chromium.components.sync.PassphraseType;
@@ -65,12 +64,10 @@ public class SyncController implements ProfileSyncService.SyncStateChangedListen
     private static SyncController sInstance;
     private static boolean sInitialized;
 
-    private final ChromeSigninController mChromeSigninController;
     private final ProfileSyncService mProfileSyncService;
     private final SyncNotificationController mSyncNotificationController;
 
     private SyncController() {
-        mChromeSigninController = ChromeSigninController.get();
         AndroidSyncSettings.get().registerObserver(this);
         mProfileSyncService = ProfileSyncService.get();
         mProfileSyncService.addSyncStateChangedListener(this);
