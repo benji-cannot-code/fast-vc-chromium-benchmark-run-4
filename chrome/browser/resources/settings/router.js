6467cb98250afc0dc394c30c47f5a6d493cefbc4
Kyle Horimoto <khorimoto@chromium.org>
2020-02-07 04:02:31 +0000

    [CrOS Settings] Add OS-specific "path visited" metric
    Before this CL, navigating to a settings subpage on browser and OS
    settings emitted to the same histogram. This caused issues, since some
    separate subpages have the same name in each settings surface. For
    example, the "/printing" subpage means a completely different thing in
    OS vs. browser settings.
    
    Bug: 1049417, 1026353
    Change-Id: Ifbe15866c61377a8da0413694b8578228401a6fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040951
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739264}

--- chrome/browser/resources/settings/router.js
+++ chrome/browser/resources/settings/router.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // #import {assert, assertNotReached} from 'chrome://resources/js/assert.m.js';
+// #import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 
 cr.define('settings', function() {
   /**
@@ -330,8 +331,12 @@ cr.define('settings', function() {
       assert(!urlPath.startsWith('settings'));
       assert(urlPath.startsWith('/'));
       assert(!urlPath.match(/\?/g));
-      chrome.metricsPrivate.recordSparseHashable(
-          'WebUI.Settings.PathVisited', urlPath);
+
+      const metricName = loadTimeData.valueExists('isOSSettings') &&
+              loadTimeData.getBoolean('isOSSettings') ?
+          'ChromeOS.Settings.PathVisited' :
+          'WebUI.Settings.PathVisited';
+      chrome.metricsPrivate.recordSparseHashable(metricName, urlPath);
     }
 
     resetRouteForTesting() {
