c0bd0d33739379418c95d14f813ae0ac133c5056
Dan Harrington <harringtond@chromium.org>
2020-02-07 23:57:30 +0000

    Add placeholder FeedStream
    - Add FeedStream class, which doesn't do much yet...
    - Add a FeedStreamBackground object to own some data like
      an upcoming FeedStore.
    - Add a safe way to access background data, and a safe way
      to delete it.
    - Added FeedStreamApi to provide an external API to FeedStream
      (for other components).
    
    Bug: 1044139
    Change-Id: I0f201e09caccbdc1ccd0a85f92d2ac1a4a9a5f0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2025490
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739625}

---
+++ components/feed/core/v2/feed_stream_api.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FEED_CORE_V2_FEED_STREAM_API_H_
+#define COMPONENTS_FEED_CORE_V2_FEED_STREAM_API_H_
+
+namespace feed {
+
+// This is the public access point for interacting with the Feed stream
+// contents.
+class FeedStreamApi {
+ public:
+  FeedStreamApi() = default;
+  virtual ~FeedStreamApi() = default;
+
+  virtual void SetArticlesListVisible(bool is_visible) = 0;
+  virtual bool IsArticlesListVisible() = 0;
+};
+
+}  // namespace feed
+
+#endif  // COMPONENTS_FEED_CORE_V2_FEED_STREAM_API_H_
---
+++ components/feed/core/v2/feed_stream_background.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/feed/core/v2/feed_stream_background.h"
+
+namespace feed {
+
+FeedStreamBackground::FeedStreamBackground() {
+  // Allow construction on a different thread.
+  DETACH_FROM_SEQUENCE(sequence_checker_);
+}
+
+FeedStreamBackground::~FeedStreamBackground() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
+
+}  // namespace feed
