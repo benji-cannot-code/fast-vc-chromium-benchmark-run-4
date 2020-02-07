aa9c285b82cb54e6cff10c6b3987ae233bd9255f
Lambros Lambrou <lambroslambrou@chromium.org>
2020-02-07 01:19:33 +0000

    [remoting] Guard against null |frame| in OnFrameEncoded().
    WebrtcVideoStream::OnFrameEncoded() expects that the |frame| parameter
    might be null, but it tries to use it anyway. This CL adds a
    null check before the dereference, preventing a host process crash.
    
    Bug: 1049804
    Change-Id: I3e02b7c5bced24611f2ba6a22025ff7ba173bd69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042138
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739209}

--- remoting/protocol/webrtc_video_stream.cc
+++ remoting/protocol/webrtc_video_stream.cc
@@ -269,7 +269,9 @@ void WebrtcVideoStream::OnFrameEncoded(
   // frame.
   // TODO(crbug.com/891571): Remove |quantizer| from the WebrtcVideoEncoder
   // interface, and move this logic to the encoders.
-  current_frame_stats_->frame_quality = (63 - frame->quantizer) * 100 / 63;
+  if (frame) {
+    current_frame_stats_->frame_quality = (63 - frame->quantizer) * 100 / 63;
+  }
 
   HostFrameStats stats;
   scheduler_->OnFrameEncoded(frame.get(), &stats);
