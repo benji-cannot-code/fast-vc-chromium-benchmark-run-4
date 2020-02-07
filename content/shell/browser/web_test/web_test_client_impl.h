6d39582b328c5f0110bb5c7d67cfcb1e196595e0
Gyuyoung Kim <gyuyoung@igalia.com>
2020-02-07 12:36:40 +0000

    Revert "Convert WebTestHostMsg_InitiateCaptureDump to Mojo"
    This reverts commit ec6127ba70797b9493b3e1e66146dd1f5bd8f79d.
    
    Reason for revert: Caused flaky test failure on WebKit Linux Leak.
    
    Original change's description:
    > Convert WebTestHostMsg_InitiateCaptureDump to Mojo
    > 
    > This CL migrates the legacy InitiateCaptureDump IPC message
    > to the new Mojo defined in WebTestClient interface.
    > 
    > Bug: 1039247
    > Change-Id: Idd38ecf67d10593f3bfc6f3c48d9832514cec595
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032634
    > Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#739222}
    
    TBR=dtapuska@chromium.org,mkwst@chromium.org,gyuyoung@igalia.com
    
    Change-Id: Ie0bc76b7d3c8dd67481d67d0ee22b0338d87334f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1039247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043531
    Reviewed-by: Gyuyoung Kim <gyuyoung@igalia.com>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#739357}

--- content/shell/browser/web_test/web_test_client_impl.h
+++ content/shell/browser/web_test/web_test_client_impl.h
@@ -35,8 +35,6 @@ class WebTestClientImpl : public mojom::WebTestClient {
   void SimulateWebContentIndexDelete(const std::string& id) override;
   void BlockThirdPartyCookies(bool block) override;
   void ResetPermissions() override;
-  void InitiateCaptureDump(bool capture_navigation_history,
-                           bool capture_pixels) override;
 };
 
 }  // namespace content
--- content/shell/renderer/web_test/blink_test_runner.cc
+++ content/shell/renderer/web_test/blink_test_runner.cc
@@ -465,9 +465,9 @@ void BlinkTestRunner::TestFinished() {
   if (interfaces->TestRunner()->ShouldDumpAsAudio()) {
     CaptureLocalAudioDump();
 
-    GetWebTestClientRemote().InitiateCaptureDump(
-        browser_should_dump_back_forward_list,
-        /*browser_should_capture_pixels=*/false);
+    Send(new WebTestHostMsg_InitiateCaptureDump(
+        routing_id(), browser_should_dump_back_forward_list,
+        /*browser_should_capture_pixels=*/false));
     return;
   }
 
@@ -478,9 +478,9 @@ void BlinkTestRunner::TestFinished() {
   CaptureLocalLayoutDump();
 
   if (!interfaces->TestRunner()->ShouldGeneratePixelResults()) {
-    GetWebTestClientRemote().InitiateCaptureDump(
-        browser_should_dump_back_forward_list,
-        /*browser_should_capture_pixels=*/false);
+    Send(new WebTestHostMsg_InitiateCaptureDump(
+        routing_id(), browser_should_dump_back_forward_list,
+        /*browser_should_capture_pixels=*/false));
     return;
   }
 
@@ -498,9 +498,9 @@ void BlinkTestRunner::TestFinished() {
           web_frame->GetSelectionBoundsRectForTesting();
     }
   }
-  GetWebTestClientRemote().InitiateCaptureDump(
-      browser_should_dump_back_forward_list,
-      !interfaces->TestRunner()->CanDumpPixelsFromRenderer());
+  Send(new WebTestHostMsg_InitiateCaptureDump(
+      routing_id(), browser_should_dump_back_forward_list,
+      !interfaces->TestRunner()->CanDumpPixelsFromRenderer()));
 }
 
 void BlinkTestRunner::CaptureLocalAudioDump() {
