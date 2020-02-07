506421f91df16d5a6039d80e15080e001ea5bb39
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-02-07 02:07:29 +0000

    Add search term chip in Grid tab switcher (2/2)
    Show a chip on the tab card if that tab has search result page in its
    navigation stack. The chip contains the search term. When clicked,
    the tab would visit the search result page again.
    
    This is behind a Finch parameter "enable_search_term_chip" under
    TabGridLayoutAndroid.
    
    The CL is split into two, and this is part two, which can be verified
    to be behind the flag isSearchTermChipEnabled() by the Formal
    Equivalence Checker in http://crrev.com/c/1934235/4.
    
    See also: part one (http://crrev.com/c/2038458).
    
    Bug: 1048255
    Change-Id: I03d61d97a99964168ebcd758b772231185d9223f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036513
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739230}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherCoordinator.java
@@ -34,6 +34,7 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabCreatorManager;
 import org.chromium.chrome.browser.tabmodel.TabList;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tasks.pseudotab.TabAttributeCache;
 import org.chromium.chrome.browser.tasks.tab_management.suggestions.TabSuggestionsOrchestrator;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.tab_ui.R;
@@ -69,6 +70,7 @@ public class TabSwitcherCoordinator
     private final MessageCardProviderCoordinator mMessageCardProviderCoordinator;
     private TabSuggestionsOrchestrator mTabSuggestionsOrchestrator;
     private NewTabTileCoordinator mNewTabTileCoordinator;
+    private TabAttributeCache mTabAttributeCache;
 
     private final MenuOrKeyboardActionController
             .MenuOrKeyboardActionHandler mTabSwitcherMenuActionHandler =
@@ -186,6 +188,11 @@ public class TabSwitcherCoordinator
                             R.layout.new_tab_tile_card_item, container, false);
                 }, NewTabTileViewBinder::bind);
             }
+        }
+
+        if (TabUiFeatureUtilities.isSearchTermChipEnabled()
+                && mode != TabListCoordinator.TabListMode.CAROUSEL) {
+            mTabAttributeCache = new TabAttributeCache(mTabModelSelector);
         }
 
         mMenuOrKeyboardActionController = menuOrKeyboardActionController;
@@ -388,5 +395,8 @@ public class TabSwitcherCoordinator
         mTabSelectionEditorCoordinator.destroy();
         mMediator.destroy();
         mLifecycleDispatcher.unregister(this);
+        if (mTabAttributeCache != null) {
+            mTabAttributeCache.destroy();
+        }
     }
 }
---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabUiFeatureUtilities.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
+/**
+ * A class to handle the state of flags for tab_management.
+ */
+public class TabUiFeatureUtilities {
+    private static Boolean sSearchTermChipEnabledForTesting;
+
+    /**
+     * Set whether the search term chip in Grid tab switcher is enabled for testing.
+     */
+    public static void setSearchTermChipEnabledForTesting(Boolean enabled) {
+        sSearchTermChipEnabledForTesting = enabled;
+    }
+
+    /**
+     * @return Whether the search term chip in Grid tab switcher is enabled.
+     */
+    public static boolean isSearchTermChipEnabled() {
+        if (sSearchTermChipEnabledForTesting != null) return sSearchTermChipEnabledForTesting;
+        return ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
+                ChromeFeatureList.TAB_GRID_LAYOUT_ANDROID, "enable_search_term_chip", false);
+    }
+}
