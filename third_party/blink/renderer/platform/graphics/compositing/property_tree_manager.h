dda34a7996c041c9a892cb280be199da62390fca
Xianzhu Wang <wangxianzhu@chromium.org>
2020-02-07 01:34:09 +0000

    Reland "Ensure cc::Layer's NeedsPushProperties when blink directly sets scroll offset"
    This reverts commit 5db928ee3c3a91959dae60be942fb21824e09e0e.
    
    The original issue was caused by additional scroll tree scroll offset
    entries. DirectSetScrollOffset should not create new entries, but just
    change existing entries, otherwise after a document lifecycle update,
    the scroll tree may contain entries whose element ids don't correspond
    any scroll node.
    
    Original change's description:
    > Revert "Ensure cc::Layer's NeedsPushProperties when blink directly sets scroll offset"
    > 
    > This reverts commit 617a9c19466ea8fb294f0e904c96f91dc48b8f38.
    > 
    > Reason for revert: Broke some browser tests. The tests flakily crash when pushing scroll properties at impl-side.
    > 
    > Original change's description:
    > > Ensure cc::Layer's NeedsPushProperties when blink directly sets scroll offset
    > > 
    > > When blink directly sets scroll offset in the scroll tree, we need to
    > > set the associated cc::Layer's NeedsPushProperties to ensure update of
    > > cc::LayerImpl's scroll related properties
    > > (see cc::LayerImpl::UpdateScrollable()).
    > > 
    > > This reverts the change about cc::LayerTreeHost::LayerByElementId() in
    > > https://chromium-review.googlesource.com/c/chromium/src/+/2020554
    > > because we still need it in layer list mode.
    > > 
    > > Bug: 1048113
    > > Change-Id: I686beebcf54e96cc517700c0f987aaceec880c58
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036834
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#739043}
    > 
    > TBR=wangxianzhu@chromium.org,pdr@chromium.org
    > 
    > Change-Id: Ib52291c52f8bac3f4a3c9fe0248f55d0162c556c
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1048113
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042763
    > Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#739183}
    
    TBR=wangxianzhu@chromium.org,pdr@chromium.org
    
    Change-Id: Ie72b629bbdb20b0ed4c33746c33828f4d596a884
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1048113
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043056
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739220}

--- third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.h
+++ third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.h
@@ -26,6 +26,10 @@ struct TransformNode;
 enum class RenderSurfaceReason : uint8_t;
 }
 
+namespace gfx {
+class ScrollOffset;
+}
+
 namespace blink {
 
 class ClipPaintPropertyNode;
@@ -126,20 +130,20 @@ class PropertyTreeManager {
   void Finalize();
 
   static bool DirectlyUpdateCompositedOpacityValue(
-      cc::PropertyTrees*,
       cc::LayerTreeHost&,
       const EffectPaintPropertyNode&);
   static bool DirectlyUpdateScrollOffsetTransform(
-      cc::PropertyTrees*,
       cc::LayerTreeHost&,
       const TransformPaintPropertyNode&);
-  static bool DirectlyUpdateTransform(cc::PropertyTrees*,
-                                      cc::LayerTreeHost&,
+  static bool DirectlyUpdateTransform(cc::LayerTreeHost&,
                                       const TransformPaintPropertyNode&);
   static bool DirectlyUpdatePageScaleTransform(
-      cc::PropertyTrees*,
       cc::LayerTreeHost&,
       const TransformPaintPropertyNode&);
+
+  static bool DirectlySetScrollOffset(cc::LayerTreeHost&,
+                                      CompositorElementId,
+                                      const gfx::ScrollOffset&);
 
  private:
   void SetupRootTransformNode();
