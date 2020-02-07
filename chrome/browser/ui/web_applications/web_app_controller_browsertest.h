d863e11b1ff4a989222519db05bab288b73cd567
Eric Willigers <ericwilligers@chromium.org>
2020-02-07 02:39:15 +0000

    Desktop PWAs: Test shortcut apps with BMO
    Various shortcut tests are extracted from hosted_app_browsertest.cc
    They now also run with BMO, i.e. with no dependencies on Extensions.
    
    
    Bug: 1026080
    Change-Id: I6438bcbf38028b700b5f451dd4d781cedaf96d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040593
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739245}

--- chrome/browser/ui/web_applications/web_app_controller_browsertest.h
+++ chrome/browser/ui/web_applications/web_app_controller_browsertest.h
@@ -76,6 +76,7 @@ class WebAppControllerBrowserTest : public WebAppControllerBrowserTestBase {
   net::EmbeddedTestServer* https_server() { return &https_server_; }
 
   GURL GetInstallableAppURL();
+  static const char* GetInstallableAppName();
 
   // ExtensionBrowserTest:
   void SetUpInProcessBrowserTestFixture() override;
