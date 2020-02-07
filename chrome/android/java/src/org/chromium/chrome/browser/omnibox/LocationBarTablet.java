6c28a369bdb94865917d3991325562e2b5c0409d
Brandon Wylie <wylieb@chromium.org>
2020-02-07 19:49:17 +0000

    Ensure Omnibox Url has start spacing
    On tablets, the status icon should always be shown. Setting
    showStatusIcon to true in the constructor.
    
    Bug: 1047500
    Change-Id: I30cd20f84f594ed4fffd862e944d219ad18e30d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042366
    Reviewed-by: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739520}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarTablet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarTablet.java
@@ -116,6 +116,7 @@ public class LocationBarTablet extends LocationBarLayout {
 
         mTargets = new View[] {mUrlBar, mDeleteButton};
         mStatusViewCoordinator.setShowIconsWhenUrlFocused(true);
+        mStatusViewCoordinator.setStatusIconShown(true);
     }
 
     @Override
