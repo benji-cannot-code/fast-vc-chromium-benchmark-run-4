b7ab1c82388a1a25f23e6d16652437d106d335f4
Alexandre Courbot <acourbot@chromium.org>
2020-02-07 10:29:00 +0000

    media/gpu/V4L2Device: constify GL methods
    These methods do not change the state of the V4L2Device and should thus
    be const. They will eventually disappear once GL imports are not
    required anymore, but in the meantime let's make it clear that they can
    be called from any thread.
    
    BUG=None
    TEST=Chromium builds for arm-generic.
    
    Change-Id: I51a58847083165e49f57e75a79650edeb49dc8a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043530
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739330}

--- media/gpu/v4l2/generic_v4l2_device.cc
+++ media/gpu/v4l2/generic_v4l2_device.cc
@@ -197,7 +197,7 @@ std::vector<base::ScopedFD> GenericV4L2Device::GetDmabufsForV4L2Buffer(
   return dmabuf_fds;
 }
 
-bool GenericV4L2Device::CanCreateEGLImageFrom(const Fourcc fourcc) {
+bool GenericV4L2Device::CanCreateEGLImageFrom(const Fourcc fourcc) const {
   static uint32_t kEGLImageDrmFmtsSupported[] = {
     DRM_FORMAT_ARGB8888,
 #if defined(ARCH_CPU_ARM_FAMILY)
@@ -213,13 +213,14 @@ bool GenericV4L2Device::CanCreateEGLImageFrom(const Fourcc fourcc) {
          kEGLImageDrmFmtsSupported + base::size(kEGLImageDrmFmtsSupported);
 }
 
-EGLImageKHR GenericV4L2Device::CreateEGLImage(EGLDisplay egl_display,
-                                              EGLContext /* egl_context */,
-                                              GLuint texture_id,
-                                              const gfx::Size& size,
-                                              unsigned int buffer_index,
-                                              const Fourcc fourcc,
-                                              gfx::NativePixmapHandle handle) {
+EGLImageKHR GenericV4L2Device::CreateEGLImage(
+    EGLDisplay egl_display,
+    EGLContext /* egl_context */,
+    GLuint texture_id,
+    const gfx::Size& size,
+    unsigned int buffer_index,
+    const Fourcc fourcc,
+    gfx::NativePixmapHandle handle) const {
   DVLOGF(3);
 
   if (!CanCreateEGLImageFrom(fourcc)) {
@@ -266,7 +267,7 @@ EGLImageKHR GenericV4L2Device::CreateEGLImage(EGLDisplay egl_display,
 scoped_refptr<gl::GLImage> GenericV4L2Device::CreateGLImage(
     const gfx::Size& size,
     const Fourcc fourcc,
-    gfx::NativePixmapHandle handle) {
+    gfx::NativePixmapHandle handle) const {
   DVLOGF(3);
   DCHECK(CanCreateEGLImageFrom(fourcc));
 
@@ -304,7 +305,7 @@ scoped_refptr<gl::GLImage> GenericV4L2Device::CreateGLImage(
 }
 
 EGLBoolean GenericV4L2Device::DestroyEGLImage(EGLDisplay egl_display,
-                                              EGLImageKHR egl_image) {
+                                              EGLImageKHR egl_image) const {
   DVLOGF(3);
   EGLBoolean result = eglDestroyImageKHR(egl_display, egl_image);
   if (result != EGL_TRUE) {
@@ -313,11 +314,11 @@ EGLBoolean GenericV4L2Device::DestroyEGLImage(EGLDisplay egl_display,
   return result;
 }
 
-GLenum GenericV4L2Device::GetTextureTarget() {
+GLenum GenericV4L2Device::GetTextureTarget() const {
   return GL_TEXTURE_EXTERNAL_OES;
 }
 
-std::vector<uint32_t> GenericV4L2Device::PreferredInputFormat(Type type) {
+std::vector<uint32_t> GenericV4L2Device::PreferredInputFormat(Type type) const {
   if (type == Type::kEncoder)
     return {V4L2_PIX_FMT_NV12M, V4L2_PIX_FMT_NV12};
 
