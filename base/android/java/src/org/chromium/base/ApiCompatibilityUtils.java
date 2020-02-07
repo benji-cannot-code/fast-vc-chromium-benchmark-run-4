fc4fa940da45a28869cd8388d6af0e5100fde5c8
Ben Joyce <bjoyce@google.com>
2020-02-07 18:55:32 +0000

    Skip first time setup when using testharnesses. Only supported with android Q and later.
    Bug: 948969
    Change-Id: I4beba06cea2d13c5323e62cc4363c6dd7e643a98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040443
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: benjamin joyce <bjoyce@google.com>
    Cr-Commit-Position: refs/heads/master@{#739496}

--- base/android/java/src/org/chromium/base/ApiCompatibilityUtils.java
+++ base/android/java/src/org/chromium/base/ApiCompatibilityUtils.java
@@ -56,6 +56,7 @@ import org.chromium.base.annotations.VerifiesOnM;
 import org.chromium.base.annotations.VerifiesOnN;
 import org.chromium.base.annotations.VerifiesOnO;
 import org.chromium.base.annotations.VerifiesOnP;
+import org.chromium.base.annotations.VerifiesOnQ;
 
 import java.io.UnsupportedEncodingException;
 
@@ -67,6 +68,14 @@ import java.io.UnsupportedEncodingException;
  */
 public class ApiCompatibilityUtils {
     private ApiCompatibilityUtils() {
+    }
+
+    @VerifiesOnQ
+    @TargetApi(Build.VERSION_CODES.Q)
+    private static class ApisQ {
+        static boolean isRunningInUserTestHarness() {
+            return ActivityManager.isRunningInUserTestHarness();
+        }
     }
 
     @VerifiesOnP
@@ -678,6 +687,13 @@ public class ApiCompatibilityUtils {
             // https://chromium-review.googlesource.com/c/chromium/src/+/905563/1
             throw new RuntimeException(e);
         }
+    }
+
+    public static boolean isRunningInUserTestHarness() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return ApisQ.isRunningInUserTestHarness();
+        }
+        return false;
     }
 
     private static class LayerDrawableCompat extends LayerDrawable {
--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunFlowSequencer.java
@@ -76,7 +76,8 @@ public abstract class FirstRunFlowSequencer  {
      */
     public void start() {
         if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
-                || ApiCompatibilityUtils.isDemoUser()) {
+                || ApiCompatibilityUtils.isDemoUser()
+                || ApiCompatibilityUtils.isRunningInUserTestHarness()) {
             onFlowIsKnown(null);
             return;
         }
@@ -246,7 +247,8 @@ public abstract class FirstRunFlowSequencer  {
             Intent fromIntent, boolean preferLightweightFre) {
         // If FRE is disabled (e.g. in tests), proceed directly to the intent handling.
         if (CommandLine.getInstance().hasSwitch(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
-                || ApiCompatibilityUtils.isDemoUser()) {
+                || ApiCompatibilityUtils.isDemoUser()
+                || ApiCompatibilityUtils.isRunningInUserTestHarness()) {
             return false;
         }
 
