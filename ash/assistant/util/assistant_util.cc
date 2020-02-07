7dc57e7598d413be8b7e66915ca51ebd28844204
David Black <dmblack@google.com>
2020-02-07 21:17:27 +0000

    Rename Assistant enum values.
    A number of Assistant buttons and entry points have evolved or been
    deprecated.
    
    Bug: b:148080975
    Change-Id: I22dd504a235e1ae26e8a092e5387f086d2c104f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043097
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#739554}

--- ash/assistant/util/assistant_util.cc
+++ ash/assistant/util/assistant_util.cc
@@ -37,9 +37,8 @@ bool IsVoiceEntryPoint(AssistantEntryPoint entry_point, bool prefer_voice) {
   switch (entry_point) {
     case AssistantEntryPoint::kHotword:
       return true;
-    case AssistantEntryPoint::kLauncherSearchBoxMic:
     case AssistantEntryPoint::kHotkey:
-    case AssistantEntryPoint::kLauncherSearchBox:
+    case AssistantEntryPoint::kLauncherSearchBoxIcon:
     case AssistantEntryPoint::kLongPressLauncher:
       return prefer_voice;
     case AssistantEntryPoint::kUnspecified:
@@ -56,14 +55,13 @@ bool ShouldAttemptWarmerWelcome(AssistantEntryPoint entry_point) {
   switch (entry_point) {
     case AssistantEntryPoint::kDeepLink:
     case AssistantEntryPoint::kHotword:
-    case AssistantEntryPoint::kLauncherSearchBoxMic:
+    case AssistantEntryPoint::kLauncherSearchBoxIcon:
     case AssistantEntryPoint::kLauncherSearchResult:
     case AssistantEntryPoint::kProactiveSuggestions:
     case AssistantEntryPoint::kStylus:
       return false;
     case AssistantEntryPoint::kUnspecified:
     case AssistantEntryPoint::kHotkey:
-    case AssistantEntryPoint::kLauncherSearchBox:
     case AssistantEntryPoint::kLongPressLauncher:
     case AssistantEntryPoint::kSetup:
       return true;
