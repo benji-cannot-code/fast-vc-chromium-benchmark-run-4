33833456b7c8113d1dc6182565bb9795582dd28b
Pavel Feldman <pfeldman@chromium.org>
2020-02-07 01:27:42 +0000

    DevTools: introduce Target.createBrowserContext({disposeOnDetach})
    Passing this parameter will desotry all the contexts created by the session
    upon session disconnect.
    
    Change-Id: I6608ef672b41b922308bf71921a074faa037ae7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042279
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739214}

--- headless/test/headless_protocol_browsertest.cc
+++ headless/test/headless_protocol_browsertest.cc
@@ -275,6 +275,9 @@ HEADLESS_PROTOCOL_TEST(MAYBE_VirtualTimeTimerSuspend,
 HEADLESS_PROTOCOL_TEST(HeadlessSessionBasicsTest,
                        "sessions/headless-session-basics.js")
 
+HEADLESS_PROTOCOL_TEST(HeadlessSessionCreateContextDisposeOnDetach,
+                       "sessions/headless-createContext-disposeOnDetach.js")
+
 class HeadlessProtocolCompositorBrowserTest
     : public HeadlessProtocolBrowserTest {
  public:
