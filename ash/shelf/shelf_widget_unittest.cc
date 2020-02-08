073222ff39f7163939a556401c01d2a0cf89d379
Nico Weber <thakis@chromium.org>
2020-02-08 19:58:00 +0000

    Revert "cros: Add metrics for animation smoothness for hotseat transition"
    This reverts commit ab982fafeeb9719f38ea961bfc46139af5ff4a45.
    
    Reason for revert: Caused asan and msan failure:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/36879
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/17669
    
    Original change's description:
    > cros: Add metrics for animation smoothness for hotseat transition
    > 
    > Add metrics to record movement of the hotseat widget.
    > We previously had metrics for animation of the shelfwidget's
    > animating_background_, but no metrics for movement of the hotseat
    > widget.
    > 
    > Also clarify the two metrics descriptions a bit.
    > 
    > Bug: 1049385
    > Change-Id: I712bff95f9fc5fb9109f41f1110af13e1ca285a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042378
    > Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    > Reviewed-by: Mark Pearson <mpearson@chromium.org>
    > Reviewed-by: Manu Cornet <manucornet@chromium.org>
    > Auto-Submit: Alex Newcomer <newcomer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#739583}
    
    TBR=mpearson@chromium.org,newcomer@chromium.org,manucornet@chromium.org
    
    Change-Id: I0c38eb3381455bac270b4a0db09f795149e98c22
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1049385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045471
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739723}

--- ash/shelf/shelf_widget_unittest.cc
+++ ash/shelf/shelf_widget_unittest.cc
@@ -418,7 +418,7 @@ TEST_F(ShelfWidgetTest, OpaqueBackgroundAndDragHandleTransition) {
 
   {
     TransitionAnimationWaiter waiter(
-        GetShelfWidget()->hotseat_transition_animator());
+        GetShelfWidget()->hotseat_transition_animator_for_testing());
     waiter.Wait();
   }
 
