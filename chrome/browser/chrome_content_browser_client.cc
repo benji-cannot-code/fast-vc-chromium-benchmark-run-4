40709532d4e0823549c7e039a83d30403af8a7a0
Chris Blume <cblume@chromium.org>
2020-02-08 02:46:34 +0000

    Enable WCG on Android under flag
    Previously, the flag to enable WCG on Android was inside
    compositor_impl_android.cc. But this is too late in the pipeline.
    The display color gamuts are already set to wide or not before this.
    
    This CL moves the flag forward so the display color spaces are set
    correctly based on what the display actually supports.
    
    Bug: 955158
    Change-Id: I5e81b4d711eced61182f1a0466ddb65b00219af5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036917
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739677}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -5377,7 +5377,8 @@ ui::AXMode ChromeContentBrowserClient::GetAXModeForBrowserContext(
 #if defined(OS_ANDROID)
 content::ContentBrowserClient::WideColorGamutHeuristic
 ChromeContentBrowserClient::GetWideColorGamutHeuristic() {
-  if (UseDisplayWideColorGamut()) {
+  if (UseDisplayWideColorGamut() ||
+      base::FeatureList::IsEnabled(features::kDynamicColorGamut)) {
     return WideColorGamutHeuristic::kUseDisplay;
   }
 
