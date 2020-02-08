fbed994c505fc95222fa73526a6a214e90943a0e
Chase Phillips <cmp@chromium.org>
2020-02-08 02:27:15 +0000

    AppCache: Fix 304 resource response handling
    AppCache previously never implemented 304 response handling for
    resources at all.  The correct handling sequence causes the cached
    item on disk to be updated based on the 304 response's results,
    including its request and response times.
    
    When corrupted cache entries were created due to another bug (issue
    968179), this missing 304 handling resulted in those corrupted cache
    entries persisting indefinitely.
    
    This commit changes the 304 handling sequence for resources
    so any cached entry's info will get updated based on the incoming
    request's info and the entry's data will get copied to that new
    entry.  One implementation detail: it was easier to create a
    new cached entry based on the old entry rather than updating
    the old entry directly, so the change is written to copy cached
    entries over, updating them in-flight based on the response.
    
    As part of this change, this commit adds an UpdateJob unit test to
    verify the cached data is correctly updated by first writing a cached
    entry to disk with broken request and response times, kicking off the
    update, letting it complete, and then reading the newly cached data
    to verify that the request and response times are correctly updated.
    
    This commit doesn't address 2 cases:
    
    * It doesn't address 304 fixes for non-resource handling which will
    remain as it was since the AppCache feature is close to removal.
    
    * It doesn't handle the case where a cached entry has a Vary header
    and the incoming response does not.  In those cases, we'll continue
    to be non-compliant with the spec.  We believe this is acceptable
    since support requires a lot of extra work and we expect it to be a
    rare case.  See the CL for more details.
    
    Bug: 989611
    Change-Id: Ifcb1c853da49d8f3a26104edad96d23386715f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018442
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739668}

--- content/browser/appcache/appcache_update_job.h
+++ content/browser/appcache/appcache_update_job.h
@@ -47,6 +47,7 @@ class AppCacheUpdateJobTest;
 class HostNotifier;
 
 CONTENT_EXPORT extern const base::Feature kAppCacheManifestScopeChecksFeature;
+CONTENT_EXPORT extern const base::Feature kAppCacheUpdateResourceOn304Feature;
 
 // Application cache Update algorithm and state.
 class CONTENT_EXPORT AppCacheUpdateJob
@@ -72,6 +73,7 @@ class CONTENT_EXPORT AppCacheUpdateJob
   friend class content::AppCacheGroupTest;
   friend class content::appcache_update_job_unittest::AppCacheUpdateJobTest;
 
+  class CacheCopier;
   class URLFetcher;
   class UpdateRequestBase;
   class UpdateURLLoaderRequest;
@@ -144,6 +146,8 @@ class CONTENT_EXPORT AppCacheUpdateJob
   void ContinueHandleManifestFetchCompleted(bool changed);
 
   void HandleResourceFetchCompleted(URLFetcher* url_fetcher, int net_error);
+  void ContinueHandleResourceFetchCompleted(const GURL& url,
+                                            URLFetcher* entry_fetcher);
   void HandleNewMasterEntryFetchCompleted(URLFetcher* url_fetcher,
                                           int net_error);
 
@@ -217,6 +221,7 @@ class CONTENT_EXPORT AppCacheUpdateJob
   void ClearPendingMasterEntries();
   void DiscardInprogressCache();
   void DiscardDuplicateResponses();
+  bool IsFinished() const;
 
   void MadeProgress() { last_progress_time_ = base::Time::Now(); }
 
@@ -227,6 +232,8 @@ class CONTENT_EXPORT AppCacheUpdateJob
     return internal_state_ >= AppCacheUpdateJobState::REFETCH_MANIFEST ||
            stored_state_ != UNSTORED;
   }
+
+  AppCache* inprogress_cache() { return inprogress_cache_.get(); }
 
   AppCacheServiceImpl* service_;
   const GURL manifest_url_;  // here for easier access
@@ -245,7 +252,11 @@ class CONTENT_EXPORT AppCacheUpdateJob
 
   // If true, AppCaches will be limited to their determined manifest scope
   // (either the scope of the manifest URL or the override the server gives us).
-  bool manifest_scope_checks_enabled_;
+  const bool manifest_scope_checks_enabled_;
+
+  // If true, AppCache resource fetches that return a 304 response will have
+  // their cache entry copied and updated based on the 304 response.
+  const bool update_resource_on_304_enabled_;
 
   // Defined prior to refs to AppCaches and Groups because destruction
   // order matters, the disabled_storage_reference_ must outlive those
@@ -323,6 +334,9 @@ class CONTENT_EXPORT AppCacheUpdateJob
   // Used to track behavior and conditions found during update for submission
   // to UMA.
   AppCacheUpdateMetricsRecorder update_metrics_;
+
+  // |cache_copier_by_url_| owns all running cache copies, indexed by |url|.
+  std::map<GURL, std::unique_ptr<CacheCopier>> cache_copier_by_url_;
 
   AppCacheStorage* storage_;
   base::WeakPtrFactory<AppCacheUpdateJob> weak_factory_{this};
