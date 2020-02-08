dfefd2ddf2f12c8c7bb22d1fd8b80c8de2b7a7a6
Renjie Tang <renjietang@chromium.org>
2020-02-08 01:24:19 +0000

    Roll src/net/third_party/quiche/src/ d96ecdaaf..051660f24 (22 commits)
    https://quiche.googlesource.com/quiche.git/+log/d96ecdaaf35e..051660f24473
    $ git log d96ecdaaf..051660f24 --date=short --no-merges --format='%ad %ae %s'
    2020-02-06 wub gfe-relnote: Add a quic::test::ServerThread::WaitUntil method and use it in quic end_to_end_test. Test only.
    2020-02-06 bnc Do not call HpackWholeEntryBuffer::error_detected() in HpackDecoder::error_detected().
    2020-02-06 nharper Fix connection close check in quic_client_interop_test_bin
    2020-02-06 vasilvv Fix ODR violations in quic_versions.h
    2020-02-05 ianswett gfe-relnote: No longer send a RESET_STREAM in response to a STOP_SENDING if the stream is write closed.  Protected by disabled gfe2_reloadable_flag_quic_enable_version_99.
    2020-02-05 dschinazi Deprecate quic_allow_very_long_connection_ids
    2020-02-05 fayang gfe-relnote: Initialize stream_count in default constructor of QuicMaxStreamsFrame. Protected by disabled v99 flag.
    2020-02-05 wub gfe-relnote: (n/a) In QuicClientBase, mark version negotiated when the client reconnects. Test only.
    2020-02-05 nharper Fix timeouts and rebinding check in quic_client_interop_test
    2020-02-05 dschinazi Add TLS to QuicIetfTransportErrorCodeString
    2020-02-04 renjietang Insert a greased frame after a HTTP/3 SETTINGS frame is sent.
    2020-02-04 bnc Make bool methods with unused return values void.
    2020-02-04 fayang gfe-relnote: In QUIC, notify frame acked when discarding initial key. Protected by gfe2_reloadable_flag_quic_neuter_unencrypted_control_frames.
    2020-02-04 wub gfe-relnote: (n/a) Increase the time in some quic http end_to_end_tests to reduce flakiness. Test only, not protected.
    2020-02-04 bnc Remove IntervalItem from anonymous namespace.
    2020-02-04 nharper Remove nullptr checks from quic_client_interop_test_bin.cc
    2020-02-04 bnc Set max_outbound_header_list_size_ when receiving SETTINGS_MAX_HEADER_LIST_SIZE.
    2020-02-04 dschinazi Adopt QuicConnectionDebugVisitor from quic_client_interop_test
    2020-02-03 renjietang Clarify QuicSession::CloseStreamInner() parameters.
    2020-02-03 renjietang Add debugging logs for static stream QUIC bug.
    2020-02-03 fayang gfe-relnote: In QUIC, check connected_ when processing MaxStreamsFrame and StreamsBlockedFrame. Protected by disabled v99 flag.
    2020-02-03 fayang gfe-relnote: Deprecate gfe2_reloadable_flag_quic_on_packet_numbers_skipped
    
    Change-Id: I1079a1db479079df3e8f8fefcf0ac36485cff5e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2044758
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739648}

--- net/http2/platform/impl/http2_flags_impl.cc
+++ net/http2/platform/impl/http2_flags_impl.cc
@@ -6,3 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http2/platform/impl/http2_flags_impl.h"
 
 bool FLAGS_http2_varint_decode_64_bits = true;
+
+bool FLAGS_http2_skip_querying_entry_buffer_error = false;
--- net/http2/platform/impl/http2_flags_impl.h
+++ net/http2/platform/impl/http2_flags_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/third_party/quiche/src/common/platform/api/quiche_export.h"
 
 QUICHE_EXPORT_PRIVATE extern bool FLAGS_http2_varint_decode_64_bits;
+QUICHE_EXPORT_PRIVATE extern bool FLAGS_http2_skip_querying_entry_buffer_error;
 
 namespace http2 {
 
