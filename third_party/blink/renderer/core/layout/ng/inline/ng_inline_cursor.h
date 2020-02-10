fd8ed64f20fae83badebbabb2cc08f48b86609f5
Koji Ishii <kojii@chromium.org>
2020-02-10 11:45:15 +0000

    [FragmentItem] Move Style-related functions to |NGInlineCursorPosition|
    This patch moves functions related with the current style to
    |NGInlineCursorPosition|.
    
    |IsHorizontal| was removed because it is just an alias to
    |Style().IsHorizontalWritingMode()|.
    
    This patch has no behavior changes.
    
    Bug: 982194
    Change-Id: I782a90c452aa608a0319edaf13ba9f54205f61f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045618
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739842}

--- third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h
@@ -99,8 +99,10 @@ class CORE_EXPORT NGInlineCursorPosition {
   // end.
   bool IsHiddenForPaint() const;
 
+  // |ComputedStyle| and related functions.
   NGStyleVariant StyleVariant() const;
   bool UsesFirstLineStyle() const;
+  const ComputedStyle& Style() const;
 
   const DisplayItemClient* GetDisplayItemClient() const;
 
@@ -201,9 +203,6 @@ class CORE_EXPORT NGInlineCursor {
   // at end.
   bool IsBeforeSoftLineBreak() const;
 
-  // True if the current position's writing mode in style is horizontal.
-  bool IsHorizontal() const;
-
   // True if the current position is text or atomic inline box.
   // Note: Because of this function is used for caret rect, hit testing, etc,
   // this function returns false for hidden for paint, text overflow ellipsis,
@@ -234,7 +233,6 @@ class CORE_EXPORT NGInlineCursor {
   // call at other than text or atomic inline. Note: <span> doesn't have
   // reserved direction.
   TextDirection CurrentResolvedDirection() const;
-  const ComputedStyle& CurrentStyle() const;
 
   // InkOverflow of itself, including contents if they contribute to the ink
   // overflow of this object (e.g. when not clipped,) in the local coordinate.
