93e041e944984b268815aee194c911a345ee53d6
Daniel Cheng <dcheng@chromium.org>
2020-02-10 15:29:33 +0000

    Directly include histogram_functions.h in //third_party/blink/renderer/modules/canvas
    Include the corresponding //base header directly rather than going
    through Blink's histogram.h, which will be removed.
    
    This also changes the metric to use the function version, since it
    shouldn't get called often enough to warrant caching of the
    histogram pointer.
    
    Bug: 1047547
    Change-Id: I675b3a551dba44cc1122904af5abf875d4a22b9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045537
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739876}

--- third_party/blink/renderer/modules/canvas/htmlcanvas/html_canvas_element_module.cc
+++ third_party/blink/renderer/modules/canvas/htmlcanvas/html_canvas_element_module.cc
@@ -5,13 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/canvas/htmlcanvas/html_canvas_element_module.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_canvas_context_creation_attributes_module.h"
 #include "third_party/blink/renderer/core/dom/dom_node_ids.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context.h"
 #include "third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h"
 #include "third_party/blink/renderer/modules/canvas/htmlcanvas/canvas_context_creation_attributes_helpers.h"
-#include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 
 namespace blink {
 
@@ -52,8 +52,8 @@ OffscreenCanvas* HTMLCanvasElementModule::transferControlToOffscreen(
         execution_context, canvas, exception_state);
   }
 
-  UMA_HISTOGRAM_BOOLEAN("Blink.OffscreenCanvas.TransferControlToOffscreen",
-                        bool(offscreen_canvas));
+  base::UmaHistogramBoolean("Blink.OffscreenCanvas.TransferControlToOffscreen",
+                            !!offscreen_canvas);
   return offscreen_canvas;
 }
 
