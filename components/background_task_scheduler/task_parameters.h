dac980b511cde433af1e28c812d4e3deb008fb6b
Shakti Sahu <shaktisahu@chromium.org>
2020-02-10 23:45:31 +0000

    Background task scheduler: Added public interface
    This CL adds the public interface for the native background task scheduler.
    It also moves the task id and network type enums to native.
    
    TBR=hnakashima@chromium.org
    
    Bug: 1050034
    Change-Id: I73b1bafaea66e9b29804a1392b0089ca81842a9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2030053
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740026}

---
+++ components/background_task_scheduler/task_parameters.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_BACKGROUND_TASK_SCHEDULER_TASK_PARAMETERS_H_
+#define COMPONENTS_BACKGROUND_TASK_SCHEDULER_TASK_PARAMETERS_H_
+
+#include "base/macros.h"
+
+namespace background_task {
+
+// TaskParameters are passed to BackgroundTask whenever they are invoked. It
+// contains the task ID and the extras that the caller has passed in with the
+// TaskInfo.
+struct TaskParameters {
+  TaskParameters();
+  ~TaskParameters();
+
+  int task_id;
+  // TODO(shaktisahu): Add support for extras.
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TaskParameters);
+};
+
+}  // namespace background_task
+
+#endif  // COMPONENTS_BACKGROUND_TASK_SCHEDULER_TASK_PARAMETERS_H_
