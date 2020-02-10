00c893b2cca43e7efe607692f6bb1b6f7032e7d3
James Cook <jamescook@chromium.org>
2020-02-10 19:01:35 +0000

    Rename AuthSyncObserver to AuthErrorObserver, part 2
    Rename the files and fix header guards.
    
    Fixed: 1049678
    Change-Id: If3221e2d608e20c0a67e115aef095da11ee48b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2046716
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739955}

--- chrome/browser/chromeos/login/signin/auth_error_observer.h
+++ chrome/browser/chromeos/login/signin/auth_error_observer.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_LOGIN_SIGNIN_AUTH_SYNC_OBSERVER_H_
-#define CHROME_BROWSER_CHROMEOS_LOGIN_SIGNIN_AUTH_SYNC_OBSERVER_H_
+#ifndef CHROME_BROWSER_CHROMEOS_LOGIN_SIGNIN_AUTH_ERROR_OBSERVER_H_
+#define CHROME_BROWSER_CHROMEOS_LOGIN_SIGNIN_AUTH_ERROR_OBSERVER_H_
 
 #include <string>
 
@@ -59,4 +59,4 @@ class AuthErrorObserver : public KeyedService,
 
 }  // namespace chromeos
 
-#endif  // CHROME_BROWSER_CHROMEOS_LOGIN_SIGNIN_AUTH_SYNC_OBSERVER_H_
+#endif  // CHROME_BROWSER_CHROMEOS_LOGIN_SIGNIN_AUTH_ERROR_OBSERVER_H_
