5543746a0fea0ef982b5254a97443bc32c159e12
Hiroshi Ichikawa <ichikawa@chromium.org>
2020-02-10 06:24:51 +0000

    Add DCHECK for multiple registration of a single property.
    Bug: 1023250
    Change-Id: If221e3d2ab75b4020cfbbb49f722c9dc28f27bbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043258
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739795}

--- ios/web/web_state/ui/crw_properties_store.mm
+++ ios/web/web_state/ui/crw_properties_store.mm
@@ -282,8 +282,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)registerPropertyWithStore:(CRWPropertyStore*)store {
   [_propertyStores addObject:store];
-  _propertyStoreByGetter[[NSValue valueWithPointer:store.getter]] = store;
-  _propertyStoreBySetter[[NSValue valueWithPointer:store.setter]] = store;
+
+  NSValue* getterKey = [NSValue valueWithPointer:store.getter];
+  DCHECK(!_propertyStoreByGetter[getterKey])
+      << "A property with getter "
+      << NSStringFromSelector(store.getter).UTF8String
+      << " has already been registered to the property store";
+  _propertyStoreByGetter[getterKey] = store;
+
+  NSValue* setterKey = [NSValue valueWithPointer:store.setter];
+  DCHECK(!_propertyStoreBySetter[setterKey])
+      << "A property with setter "
+      << NSStringFromSelector(store.setter).UTF8String
+      << " has already been registered to the property store";
+  _propertyStoreBySetter[setterKey] = store;
 }
 
 - (void)savePropertiesFromObject:(id)object {
