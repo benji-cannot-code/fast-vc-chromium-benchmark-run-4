65e6cb92128a383b047229d1d3451ec0603b39b4
Mitsuru Oshima <oshima@chromium.org>
2020-02-10 23:41:09 +0000

    Remove overview focus container
    The widget for focus in overvew should just be excluded from mru
    list so that it won't be focused again.
    
    Bug: 1049316
    Test: Removed unnecessary test. No functional change and all tests should pass.
    Change-Id: I43add420784e059274540ddf3d036275c91eba3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041790
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740019}

--- ash/public/cpp/shell_window_ids.cc
+++ ash/public/cpp/shell_window_ids.cc
@@ -12,7 +12,7 @@ namespace ash {
 
 namespace {
 
-constexpr std::array<int, 19> kActivatableContainersIds = {
+constexpr std::array<int, 18> kActivatableContainersIds = {
     kShellWindowId_OverlayContainer,
     kShellWindowId_LockSystemModalContainer,
     kShellWindowId_AccessibilityPanelContainer,
@@ -35,14 +35,13 @@ constexpr std::array<int, 19> kActivatableContainersIds = {
     kShellWindowId_PipContainer,
     kShellWindowId_ShelfContainer,
     kShellWindowId_ShelfBubbleContainer,
-    kShellWindowId_OverviewFocusContainer,
 };
 
 }  // namespace
 
 // Note: this function avoids having a copy of |kActivatableContainersIds| in
 // each translation unit that references it.
-const std::array<int, 19>& GetActivatableShellWindowIds() {
+const std::array<int, 18>& GetActivatableShellWindowIds() {
   return kActivatableContainersIds;
 }
 
