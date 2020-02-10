3270f748aadf3b47b83b6cbb7593882aac031f68
Olya Kalitova <okalitova@chromium.org>
2020-02-10 11:25:22 +0000

    Add fuzzing of CloudPolicySettings to policy_fuzzer
    Adds fuzzing of CloudPolicySettings to policy_fuzzer.
    
    ASan' out/libfuzzer && ninja -C out/libfuzzer policy_fuzzer &&
    out/libfuzzer/policy_fuzzer
    
    Test: tools/mb/mb.py gen -m chromium.fuzz -b 'Libfuzzer Upload Chrome OS
    Bug: 811690
    Change-Id: Ic0f8b1fa56ac4c49e813375339c7c5ec9a886470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2044275
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Olya Kalitova <okalitova@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739838}

--- components/policy/core/common/policy_proto_decoders.h
+++ components/policy/core/common/policy_proto_decoders.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "components/policy/core/common/policy_types.h"
+#include "components/policy/policy_export.h"
 
 namespace enterprise_management {
 class CloudPolicySettings;
@@ -23,7 +24,7 @@ class PolicyMap;
 // Decode all of the fields in |policy| which are recognized (see the metadata
 // in policy_constants.cc) and store them in the given |map|, with the given
 // |source| and |scope|.
-void DecodeProtoFields(
+POLICY_EXPORT void DecodeProtoFields(
     const enterprise_management::CloudPolicySettings& policy,
     base::WeakPtr<CloudExternalDataManager> external_data_manager,
     PolicySource source,
