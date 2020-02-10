cc0125c4326d845103b452289a382b09fd752b98
Julian Watson <juwa@google.com>
2020-02-10 04:57:45 +0000

    crostini: make RUNNING transition explicit in ExportImportStatusTracker
    Bug: 1049519
    Change-Id: Ib191a360e12af043ab8298e9c05170195a2965f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045614
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Julian Watson <juwa@google.com>
    Cr-Commit-Position: refs/heads/master@{#739789}

--- chrome/browser/chromeos/crostini/crostini_export_import_status_tracker.h
+++ chrome/browser/chromeos/crostini/crostini_export_import_status_tracker.h
@@ -21,6 +21,7 @@ enum class ExportImportType;
 class CrostiniExportImportStatusTracker {
  public:
   enum class Status {
+    NONE,
     RUNNING,
     CANCELLING,
     DONE,
@@ -65,7 +66,7 @@ class CrostiniExportImportStatusTracker {
 
   ExportImportType type_;
   base::FilePath path_;
-  Status status_ = Status::RUNNING;
+  Status status_ = Status::NONE;
 };
 
 }  // namespace crostini
