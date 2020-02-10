72b92b3c518103178c8edd4483147898928598ab
Dominic Battre <battre@chromium.org>
2020-02-10 16:57:50 +0000

    Fix crash caused by <datalist>
    This CL works around a rare crash that occurs in the context
    of <datalist> tags. My hypothesis is that on these sites the
    input element type changes before the datalist is shown. A
    DumpWithoutCrashing was added to understand this better.
    
    Bug: 1048270
    Change-Id: Iaaf1e3b6fdf3b1054370d70a78c4979885414614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047285
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739897}

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/debug/alias.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/i18n/case_conversion.h"
 #include "base/location.h"
@@ -393,7 +395,17 @@ void AutofillAgent::DoAcceptDataListSuggestion(
     return;
 
   WebInputElement* input_element = ToWebInputElement(&element_);
-  DCHECK(input_element);
+  if (!input_element) {
+    // For reasons not understood yet, this is triggered on elements which are
+    // not input elements.
+
+    // TODO(crbug.com/1048270) Gather debug data.
+    DEBUG_ALIAS_FOR_CSTR(element_name, element_.TagName().Latin1().c_str(), 64);
+    base::debug::DumpWithoutCrashing();
+
+    // Keep this return after removing the TODO(crbug.com/1048270) above.
+    return;
+  }
   base::string16 new_value = suggested_value;
   // If this element takes multiple values then replace the last part with
   // the suggestion.
