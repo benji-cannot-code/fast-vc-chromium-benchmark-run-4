0edf14b5f2e0252829a53899326b8a5112d67d30
Jérôme Lebel <jlebel@chromium.org>
2020-02-10 12:06:37 +0000

    Missing -[ChromeCoordinator stop] in SigninInteractionCoordinator
    -[ChromeCoordinator stop] needs to be called before releasing
    -SigninInteractionCoordinator.coordinator.
    
    Related to crrev.com/c/2033145
    
    Bug: 971989
    Change-Id: Ib2d0f70cd25816d7105e2cfa79347a59565fb24f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2046247
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739844}

--- ios/chrome/browser/ui/signin_interaction/signin_interaction_coordinator.mm
+++ ios/chrome/browser/ui/signin_interaction/signin_interaction_coordinator.mm
@@ -113,6 +113,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.coordinator.signinCompletion =
       ^(SigninCoordinatorResult signinResult, ChromeIdentity* identity) {
         completion(signinResult == SigninCoordinatorResultSuccess);
+        [weakSelf.coordinator stop];
         weakSelf.coordinator = nil;
       };
 
