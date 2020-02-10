f4e9f6db244c56468b9fbafc23f84740efe0585a
Victor Costan <pwnall@chromium.org>
2020-02-10 17:56:31 +0000

    Quota: Minor improvements to mock QuotaClient implementation.
    1) Remove inline definition of virtual function.
    2) Add const to a class member that supports it.
    3) Use scoped_refptr instead of raw pointer argument on method that ends
       up holding a reference to the argument.
    
    Bug: 1016065
    Change-Id: Ifd77e8e38b9de05e7b710bfcb1d9c7db9735de0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2046464
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Jarryd Goodman <jarrydg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#739925}

--- storage/browser/test/mock_storage_client.h
+++ storage/browser/test/mock_storage_client.h
@@ -38,10 +38,10 @@ struct MockOriginData {
   int64_t usage;
 };
 
-// Mock storage class for testing.
+// Mock QuotaClient implementation for testing.
 class MockStorageClient : public QuotaClient {
  public:
-  MockStorageClient(QuotaManagerProxy* quota_manager_proxy,
+  MockStorageClient(scoped_refptr<QuotaManagerProxy> quota_manager_proxy,
                     const MockOriginData* mock_data,
                     QuotaClient::ID id,
                     size_t mock_data_size);
@@ -59,9 +59,9 @@ class MockStorageClient : public QuotaClient {
 
   base::Time IncrementMockTime();
 
-  // QuotaClient methods.
+  // QuotaClient.
   QuotaClient::ID id() const override;
-  void OnQuotaManagerDestroyed() override {}
+  void OnQuotaManagerDestroyed() override;
   void GetOriginUsage(const url::Origin& origin,
                       StorageType type,
                       GetUsageCallback callback) override;
@@ -91,7 +91,7 @@ class MockStorageClient : public QuotaClient {
 
   void Populate(const MockOriginData* mock_data, size_t mock_data_size);
 
-  scoped_refptr<QuotaManagerProxy> quota_manager_proxy_;
+  const scoped_refptr<QuotaManagerProxy> quota_manager_proxy_;
   const ID id_;
 
   std::map<std::pair<url::Origin, StorageType>, int64_t> origin_data_;
