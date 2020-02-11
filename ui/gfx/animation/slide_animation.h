fdce067dc03db2edf5a68828c486c8d593cf3b7d
Collin Baker <collinbaker@chromium.org>
2020-02-11 00:32:52 +0000

    Allow SlideAnimation to hide from state between 0 and 1
    If a slide animation is reset to any state between 0 and 1, calls to
    Hide() fail due to an incorrect early return in BeginAnimating(). Note
    that Show() does work in this case.
    
    This CL fixes this behavior and adds a regression test.
    
    Bug: None
    Change-Id: I67cbe2789d80d3f512601335ed67ce860d89de92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045050
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740083}

--- ui/gfx/animation/slide_animation.h
+++ ui/gfx/animation/slide_animation.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_GFX_ANIMATION_SLIDE_ANIMATION_H_
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "ui/gfx/animation/linear_animation.h"
 #include "ui/gfx/animation/tween.h"
 
@@ -78,8 +79,13 @@ class ANIMATION_EXPORT SlideAnimation : public LinearAnimation {
   // IsShowing() will currently return true after the 'show' animation has been
   // completed however IsClosing() will return false after the 'hide' animation
   // has been completed.
-  bool IsShowing() const { return showing_; }
-  bool IsClosing() const { return !showing_ && value_end_ < value_current_; }
+  bool IsShowing() const {
+    return direction_ == Direction::kShowing ||
+           (!direction_ && value_current_ == 1);
+  }
+  bool IsClosing() const {
+    return direction_ == Direction::kHiding && value_end_ < value_current_;
+  }
 
   class TestApi;
 
@@ -88,8 +94,13 @@ class ANIMATION_EXPORT SlideAnimation : public LinearAnimation {
   // is showing or hiding.
   base::TimeDelta GetDuration();
 
+  enum class Direction {
+    kShowing,
+    kHiding,
+  };
+
   // Implementation of Show() and Hide().
-  void BeginAnimating(bool showing);
+  void BeginAnimating(Direction direction);
 
   // Overridden from Animation.
   void AnimateToState(double state) override;
@@ -98,8 +109,8 @@ class ANIMATION_EXPORT SlideAnimation : public LinearAnimation {
 
   Tween::Type tween_type_ = Tween::EASE_OUT;
 
-  // Used to determine which way the animation is going.
-  bool showing_ = false;
+  // Current animation direction, or nullopt if not animating.
+  base::Optional<Direction> direction_;
 
   // Animation values. These are a layer on top of Animation::state_ to
   // provide the reversability.
