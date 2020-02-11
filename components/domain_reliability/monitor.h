e2d725dd5e8986ecf054327b343f35793a8b5a5e
Lily Chen <chlily@chromium.org>
2020-02-11 17:23:33 +0000

    Domain Reliability: Remove header parsing and dynamic config updating
    This change removes the header handling (incl. parsing) code from
    //components/domain_reliability. This removes the ability for
    whitelisted domains to dynamically update their DR configurations.
    
    This feature was not being used, and was nonfunctional for several years
    anyway, as the parsing code was not updated when the header format
    changed.
    
    Bug: 1048340
    Change-Id: I41fbc8c483a1d3f9d66807bd2e09af4544de80a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2048944
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740330}

--- components/domain_reliability/monitor.h
+++ components/domain_reliability/monitor.h
@@ -157,8 +157,6 @@ class DOMAIN_RELIABILITY_EXPORT DomainReliabilityMonitor
 
   void OnRequestLegComplete(const RequestInfo& info);
 
-  void MaybeHandleHeader(const RequestInfo& info);
-
   base::WeakPtr<DomainReliabilityMonitor> MakeWeakPtr();
 
   std::unique_ptr<MockableTime> time_;
