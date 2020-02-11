ae5cfcf5235cfcb24c644ffd5bd5eb27159eac62
Andrey Kosyakov <caseq@chromium.org>
2020-02-11 21:48:10 +0000

    Disable multiple flaky ExtensionsMenuViewBrowserTest
    TBR: rdevlin.cronin@chromium.org
    Bug: 1050712
    Change-Id: I1d2ce1c3f849f9886d704ea9b2dbac7136041ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047496
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740435}

--- chrome/browser/ui/views/extensions/extensions_menu_view_browsertest.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_view_browsertest.cc
@@ -280,7 +280,14 @@ class ExtensionsMenuViewBrowserTest : public ExtensionsToolbarBrowserTest {
   std::string ui_test_name_;
 };
 
-IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest, InvokeUi_default) {
+// TODO(crbug.com/1050712): the test is flaky.
+// TODO(crbug.com/1050712): the test is flaky.
+#if defined(OS_MACOSX) || defined(OS_LINUX)
+#define MAYBE_InvokeUi_default DISABLED_InvokeUi_default
+#else
+#define MAYBE_InvokeUi_default InvokeUi_default
+#endif
+IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest, MAYBE_InvokeUi_default) {
   LoadTestExtension("extensions/uitest/long_name");
   LoadTestExtension("extensions/uitest/window_open");
 
@@ -357,12 +364,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
   ShowAndVerifyUi();
 }
 
-#if (defined(OS_WIN) && defined(MEMORY_SANITIZER))
-#define MAYBE_TriggerPopup DISABLED_TriggerPopup
-#else
-#define MAYBE_TriggerPopup TriggerPopup
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest, TriggerPopup) {
+// TODO(crbug.com/1050712): the test is flaky.
+IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest, DISABLED_TriggerPopup) {
   LoadTestExtension("extensions/simple_with_popup");
   ShowUi("");
   VerifyUi();
@@ -447,7 +450,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
   EXPECT_FALSE(ExtensionsMenuView::IsShowing());
 }
 
-#if defined(OS_WIN)
+// TODO(crbug.com/1050712): the test is flaky.
+#if defined(OS_WIN) || defined(OS_LINUX)
 #define MAYBE_CreatesOneMenuItemPerExtension \
   DISABLED_CreatesOneMenuItemPerExtension
 #else
@@ -464,8 +468,14 @@ IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
   DismissUi();
 }
 
+// TODO(crbug.com/1050712): the test is flaky.
+#if defined(OS_WIN)
+#define MAYBE_PinningDisabledInIncognito DISABLED_PinningDisabledInIncognito
+#else
+#define MAYBE_PinningDisabledInIncognito PinningDisabledInIncognito
+#endif
 IN_PROC_BROWSER_TEST_F(ExtensionsMenuViewBrowserTest,
-                       PinningDisabledInIncognito) {
+                       MAYBE_PinningDisabledInIncognito) {
   LoadTestExtension("extensions/uitest/window_open", true);
   SetUpIncognitoBrowser();
 
