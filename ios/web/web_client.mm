122dfeff6a211659527f3a38e1f6588dac96eba6
Gauthier Ambard <gambard@chromium.org>
2020-02-11 17:13:41 +0000

    [iOS] Request Mobile version of SRP when flag is enabled
    This CL makes sure that, when the desktop version is requested by
    default, when the flag to force the mobile version of the SRP is
    enabled, the mobile version is actually requested.
    
    Fixed: 1043637
    Change-Id: Ie2f267583c88bbfc63d14383f3d87da00da73c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050264
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740325}

--- ios/web/web_client.mm
+++ ios/web/web_client.mm
@@ -121,7 +121,8 @@ bool WebClient::ForceMobileVersionByDefault(const GURL&) {
   return false;
 }
 
-UserAgentType WebClient::GetDefaultUserAgent(UIView* web_view) {
+UserAgentType WebClient::GetDefaultUserAgent(UIView* web_view,
+                                             const GURL& url) {
   return UserAgentType::MOBILE;
 }
 
