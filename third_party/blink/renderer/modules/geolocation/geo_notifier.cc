8a12bb0a1532e70dce4398bfba36ba41b3e11f9d
Daniel Cheng <dcheng@chromium.org>
2020-02-11 00:20:32 +0000

    Use base histogram functions in //third_party/blink/renderer/modules/geolocation
    Blink allows use of the //base histogram helpers, so switch away from
    the Blink-specific histogram helpers and standardize on //base. The
    long-term goal is to remove the Blink helpers, which differ from the
    //base ones in some interesting and surprising ways.
    
    Miscellaneous cleanup:
    - use the histogram functions where appropriate (i.e. for metrics that
      aren't recorded on performance-critical paths)
    - use the histogram helpers for time to improve readability
    - use the correct units for the corresponding metrics in histograms.xml
    
    Bug: 1047547
    Change-Id: Ie6bbebf9d04b38fd1f94ffc59ed535639c000790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2046525
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740072}

--- third_party/blink/renderer/modules/geolocation/geo_notifier.cc
+++ third_party/blink/renderer/modules/geolocation/geo_notifier.cc
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/geolocation/geo_notifier.h"
 
+#include "base/metrics/histogram_functions.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_position_options.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/modules/geolocation/geolocation.h"
 #include "third_party/blink/renderer/modules/geolocation/geolocation_position_error.h"
-#include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
 
 namespace blink {
@@ -31,10 +31,11 @@ GeoNotifier::GeoNotifier(Geolocation* geolocation,
   DCHECK(geolocation_);
   DCHECK(success_callback_);
 
-  DEFINE_STATIC_LOCAL(CustomCountHistogram, timeout_histogram,
-                      ("Geolocation.Timeout", 0,
-                       1000 * 60 * 10 /* 10 minute max */, 20 /* buckets */));
-  timeout_histogram.Count(options_->timeout());
+  base::UmaHistogramCustomTimes(
+      "Geolocation.Timeout",
+      base::TimeDelta::FromMilliseconds(options_->timeout()),
+      base::TimeDelta::FromMilliseconds(1), base::TimeDelta::FromMinutes(10),
+      /* buckets = */ 20);
 }
 
 void GeoNotifier::Trace(blink::Visitor* visitor) {
@@ -137,10 +138,11 @@ void GeoNotifier::TimerFired(TimerBase*) {
                      GeolocationPositionError::kTimeout, "Timeout expired"));
   }
 
-  DEFINE_STATIC_LOCAL(CustomCountHistogram, timeout_expired_histogram,
-                      ("Geolocation.TimeoutExpired", 0,
-                       1000 * 60 * 10 /* 10 minute max */, 20 /* buckets */));
-  timeout_expired_histogram.Count(options_->timeout());
+  base::UmaHistogramCustomTimes(
+      "Geolocation.TimeoutExpired",
+      base::TimeDelta::FromMilliseconds(options_->timeout()),
+      base::TimeDelta::FromMilliseconds(1), base::TimeDelta::FromMinutes(10),
+      /* buckets = */ 20);
 
   geolocation_->RequestTimedOut(this);
 }
