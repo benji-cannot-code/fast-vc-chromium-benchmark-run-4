2625a9a75a12ab3a7a4f50de865b66640622c518
Isabella Scalzi <isabellascalzi@google.com>
2020-02-11 01:35:15 +0000

    [quickview] add openQuickViewDeleteButtonNotShown test
    Add a test that checks that the Quick View delete button is not shown
    when opening Quick View on an entry that cannot be deleted.
    
    Bug: 803259
    Change-Id: Id9ddb021bf1ff28de49820760a8b310aab371039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045694
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Commit-Queue: Isabella Scalzi <isabellascalzi@google.com>
    Cr-Commit-Position: refs/heads/master@{#740126}

--- ui/file_manager/integration_tests/file_manager/quick_view.js
+++ ui/file_manager/integration_tests/file_manager/quick_view.js
@@ -2202,4 +2202,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     // Check: the Quick View dialog should close.
     await waitQuickViewClose(appId);
   };
+
+  /**
+   * Tests that the delete button is not shown if the file displayed in Quick
+   * View cannot be deleted.
+   */
+  testcase.openQuickViewDeleteButtonNotShown = async () => {
+    // Open Files app on My Files
+    const appId = await openNewWindow(RootPath.MYFILES);
+
+    // Wait for the file list to appear.
+    await remoteCall.waitForElement(appId, '#file-list');
+
+    // Check: My Files should contain the expected entries.
+    const expectedRows = [
+      ['Play files', '--', 'Folder'],
+      ['Downloads', '--', 'Folder'],
+      ['Linux files', '--', 'Folder'],
+    ];
+    await remoteCall.waitForFiles(
+        appId, expectedRows, {ignoreLastModifiedTime: true});
+
+    // Open Play files in Quick View, which cannot be deleted.
+    await openQuickView(appId, 'Play files');
+
+    // Check: the delete button should not be shown.
+    const quickViewDeleteButton = ['#quick-view', '#delete-button[hidden]'];
+    await remoteCall.waitForElement(appId, quickViewDeleteButton);
+  };
 })();
