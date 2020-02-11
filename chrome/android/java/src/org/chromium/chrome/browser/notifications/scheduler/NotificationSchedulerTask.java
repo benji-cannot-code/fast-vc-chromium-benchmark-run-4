f0160f3cc660eec2cf3b0412efce045df1245ae7
Andrey Kosyakov <caseq@chromium.org>
2020-02-11 01:55:29 +0000

    Revert "Changing notification scheduler to dep on SimpleKeyedServiceFactory."
    This reverts commit 820c03268b18e7fad3bd6b11fc3d4956230455b4.
    
    Reason for revert: broke nexus 5 release bot (https://ci.chromium.org/p/chromium/builders/ci/Android%20Release%20(Nexus%205X)/28811)
    
    Original change's description:
    > Changing notification scheduler to dep on SimpleKeyedServiceFactory.
    > 
    > - Changing notification schedule service and it's current clients
    > produced from SimpleKeyedServiceFactory rather than depanding on
    > BrowserContext. Since clients may want to start in reduce mode,
    > and loading full browsercontext is not expected.
    > 
    > Bug: 1049878
    > Change-Id: I259261617a2a7a4011dfde7553ec948e862c2d68
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042309
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Auto-Submit: Hesen Zhang <hesen@chromium.org>
    > Commit-Queue: Hesen Zhang <hesen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#740027}
    
    TBR=dtrainor@chromium.org,xingliu@chromium.org,hesen@chromium.org
    
    Change-Id: Idefe8b48bdf7b4657e6bd527296ec15b322c983e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1049878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2048907
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740129}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/scheduler/NotificationSchedulerTask.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/scheduler/NotificationSchedulerTask.java
@@ -13,6 +13,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.background_task_scheduler.BackgroundTaskScheduler;
 import org.chromium.components.background_task_scheduler.BackgroundTaskSchedulerFactory;
 import org.chromium.components.background_task_scheduler.NativeBackgroundTask;
@@ -47,7 +48,7 @@ public class NotificationSchedulerTask extends NativeBackgroundTask {
         };
 
         NotificationSchedulerTaskJni.get().onStartTask(
-                NotificationSchedulerTask.this, taskCallback);
+                NotificationSchedulerTask.this, Profile.getLastUsedRegularProfile(), taskCallback);
     }
 
     @Override
@@ -59,7 +60,8 @@ public class NotificationSchedulerTask extends NativeBackgroundTask {
 
     @Override
     protected boolean onStopTaskWithNative(Context context, TaskParameters taskParameters) {
-        return NotificationSchedulerTaskJni.get().onStopTask(NotificationSchedulerTask.this);
+        return NotificationSchedulerTaskJni.get().onStopTask(
+                NotificationSchedulerTask.this, Profile.getLastUsedRegularProfile());
     }
 
     /**
@@ -94,7 +96,8 @@ public class NotificationSchedulerTask extends NativeBackgroundTask {
 
     @NativeMethods
     interface Natives {
-        void onStartTask(NotificationSchedulerTask caller, Callback<Boolean> callback);
-        boolean onStopTask(NotificationSchedulerTask caller);
+        void onStartTask(
+                NotificationSchedulerTask caller, Profile profile, Callback<Boolean> callback);
+        boolean onStopTask(NotificationSchedulerTask caller, Profile profile);
     }
 }
--- chrome/browser/updates/update_notification_service_bridge_android.cc
+++ chrome/browser/updates/update_notification_service_bridge_android.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/android/chrome_jni_headers/UpdateNotificationServiceBridge_jni.h"
-#include "chrome/browser/android/profile_key_util.h"
 #include "chrome/browser/notifications/scheduler/public/notification_params.h"
-#include "chrome/browser/profiles/profile_key.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_android.h"
 #include "chrome/browser/updates/update_notification_info.h"
 #include "chrome/browser/updates/update_notification_service.h"
 #include "chrome/browser/updates/update_notification_service_factory.h"
@@ -27,13 +27,14 @@ namespace updates {
 //
 void JNI_UpdateNotificationServiceBridge_Schedule(
     JNIEnv* env,
+    const JavaParamRef<jobject>& j_profile,
     const JavaParamRef<jstring>& j_title,
     const JavaParamRef<jstring>& j_message,
     const jint j_state,
     const jboolean j_show_immediately) {
-  ProfileKey* profile_key = ::android::GetLastUsedProfileKey();
+  Profile* profile = ProfileAndroid::FromProfileAndroid(j_profile);
   auto* update_notification_service =
-      UpdateNotificationServiceFactory::GetForKey(profile_key);
+      UpdateNotificationServiceFactory::GetForBrowserContext(profile);
   UpdateNotificationInfo data;
   data.title = ConvertJavaStringToUTF16(env, j_title);
   data.message = ConvertJavaStringToUTF16(env, j_message);
