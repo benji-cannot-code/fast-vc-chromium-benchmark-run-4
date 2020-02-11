656e55d45ee9fe463ac8c333245cd74f24ad08f0
Brandon Wylie <wylieb@chromium.org>
2020-02-11 18:48:43 +0000

    Save tab state after detaching all tabs
    This will prevent TabPersistentStore from restoring a tab we're in the
    process of reparenting.
    
    Bug: 1042565
    Change-Id: I1b7e505c0870e7c6b7d77eaf1012abde8cdd5c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042681
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740370}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorBase.java
@@ -194,6 +194,9 @@ public abstract class TabModelSelectorBase implements TabModelSelector {
     }
 
     @Override
+    public void saveState() {}
+
+    @Override
     public Tab getTabById(int id) {
         for (int i = 0; i < getModels().size(); i++) {
             Tab tab = TabModelUtils.getTabById(mTabModels.get(i), id);
