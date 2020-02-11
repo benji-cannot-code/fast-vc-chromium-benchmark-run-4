2392690d4ec918a04bccd5a947d29c8d4cd526bf
Sigurdur Asgeirsson <siggi@chromium.org>
2020-02-11 21:11:22 +0000

    Refactor extended crash reporting initialization.
    Bug: 1044707
    Change-Id: I51ae9d5fd4d0972f9145927d265f36cef3a4e98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037711
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740420}

--- components/browser_watcher/activity_tracker_annotation_unittest.cc
+++ components/browser_watcher/activity_tracker_annotation_unittest.cc
@@ -10,14 +10,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace browser_watcher {
 
-TEST(ActivityTrackerAnnotationTest, RegistersAtCreation) {
+class ActivityTrackerAnnotationTest : public testing::Test {
+ public:
+  void SetUp() override { crash_reporter::InitializeCrashKeysForTesting(); }
+  void TearDown() override { crash_reporter::ResetCrashKeysForTesting(); }
+};
+
+TEST_F(ActivityTrackerAnnotationTest, RegistersAtCreation) {
   crash_reporter::InitializeCrashKeysForTesting();
+  static const char* kBuffer[128];
+  ActivityTrackerAnnotation annotation;
+  // Validate that the annotation doesn't register on construction.
   EXPECT_EQ("", crash_reporter::GetCrashKeyValue(
                     ActivityTrackerAnnotation::kAnnotationName));
 
-  static const char* kBuffer[128];
-  ActivityTrackerAnnotation annotation(&kBuffer, sizeof(kBuffer));
-
+  annotation.SetValue(&kBuffer, sizeof(kBuffer));
   std::string string_value = crash_reporter::GetCrashKeyValue(
       ActivityTrackerAnnotation::kAnnotationName);
   ASSERT_EQ(sizeof(ActivityTrackerAnnotation::ValueType), string_value.size());
