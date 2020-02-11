7733d52461641f859fd058944b0f87b9d810de3e
Dominik Röttsches <drott@chromium.org>
2020-02-11 20:17:40 +0000

    Replace FallbackLruCache with generic LruCache from WTF
    LruCache was added to WTF which allows us to remove the specific
    instance that was added for OOP font fallback caching.
    
    Bug: 1005234
    Change-Id: Id20e7fb0b99128a4c3d5c4edc5bed5ffe881a17d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050314
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740392}

--- third_party/blink/renderer/platform/fonts/win/fallback_family_style_cache_win.h
+++ third_party/blink/renderer/platform/fonts/win/fallback_family_style_cache_win.h
@@ -8,11 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/fonts/font_description.h"
 #include "third_party/blink/renderer/platform/fonts/font_fallback_priority.h"
-#include "third_party/blink/renderer/platform/fonts/win/fallback_lru_cache_win.h"
+#include "third_party/blink/renderer/platform/wtf/lru_cache.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 
 namespace blink {
+
+using TypefaceVector = Vector<sk_sp<SkTypeface>>;
+using FallbackLruCache = WTF::LruCache<String, TypefaceVector>;
 
 class FallbackFamilyStyleCache {
   USING_FAST_MALLOC(FallbackFamilyStyleCache);
