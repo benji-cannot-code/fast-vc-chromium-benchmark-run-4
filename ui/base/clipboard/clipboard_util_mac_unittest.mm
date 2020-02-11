deb9f273c71fe18f215b23304079f72f6f631b42
Darwin Huang <huangdarwin@chromium.org>
2020-02-11 22:07:22 +0000

    ui clipboard: remove extra ui namespacing
    Remove extraneous namespacing in //ui/base/clipboard.
    
    Refactoring change. No intended functional changes.
    
    Change-Id: I079079bf7eb4d99cb2dacc51cf977b22e58a89e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047988
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740444}

--- ui/base/clipboard/clipboard_util_mac_unittest.mm
+++ ui/base/clipboard/clipboard_util_mac_unittest.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/platform_test.h"
 #include "third_party/mozilla/NSPasteboard+Utils.h"
 
+namespace ui {
 namespace {
 
 class ClipboardUtilMacTest : public PlatformTest {
@@ -35,8 +36,8 @@ TEST_F(ClipboardUtilMacTest, PasteboardItemFromUrl) {
       @"sourceid=chrome&ie=UTF-8";
 
   base::scoped_nsobject<NSPasteboardItem> item(
-      ui::ClipboardUtil::PasteboardItemFromUrl(urlString, nil));
-  scoped_refptr<ui::UniquePasteboard> pasteboard = new ui::UniquePasteboard;
+      ClipboardUtil::PasteboardItemFromUrl(urlString, nil));
+  scoped_refptr<UniquePasteboard> pasteboard = new UniquePasteboard;
   [pasteboard->get() writeObjects:@[ item ]];
 
   NSArray* urls = nil;
@@ -60,8 +61,8 @@ TEST_F(ClipboardUtilMacTest, PasteboardItemWithTitle) {
   NSString* title = @"Burrowing Yams";
 
   base::scoped_nsobject<NSPasteboardItem> item(
-      ui::ClipboardUtil::PasteboardItemFromUrl(urlString, title));
-  scoped_refptr<ui::UniquePasteboard> pasteboard = new ui::UniquePasteboard;
+      ClipboardUtil::PasteboardItemFromUrl(urlString, title));
+  scoped_refptr<UniquePasteboard> pasteboard = new UniquePasteboard;
   [pasteboard->get() writeObjects:@[ item ]];
 
   NSArray* urls = nil;
@@ -86,8 +87,8 @@ TEST_F(ClipboardUtilMacTest, PasteboardItemWithFilePath) {
   NSString* urlString = [url absoluteString];
 
   base::scoped_nsobject<NSPasteboardItem> item(
-      ui::ClipboardUtil::PasteboardItemFromUrl(urlString, nil));
-  scoped_refptr<ui::UniquePasteboard> pasteboard = new ui::UniquePasteboard;
+      ClipboardUtil::PasteboardItemFromUrl(urlString, nil));
+  scoped_refptr<UniquePasteboard> pasteboard = new UniquePasteboard;
   [pasteboard->get() writeObjects:@[ item ]];
 
   NSArray* urls = nil;
@@ -109,7 +110,7 @@ TEST_F(ClipboardUtilMacTest, PasteboardItemWithFilePath) {
 TEST_F(ClipboardUtilMacTest, CheckForLeak) {
   for (int i = 0; i < 10000; ++i) {
     @autoreleasepool {
-      scoped_refptr<ui::UniquePasteboard> pboard = new ui::UniquePasteboard;
+      scoped_refptr<UniquePasteboard> pboard = new UniquePasteboard;
       EXPECT_TRUE(pboard->get());
     }
   }
@@ -119,11 +120,11 @@ TEST_F(ClipboardUtilMacTest, CompareToWriteToPasteboard) {
   NSString* urlString = @"https://www.cnn.com/";
 
   base::scoped_nsobject<NSPasteboardItem> item(
-      ui::ClipboardUtil::PasteboardItemFromUrl(urlString, nil));
-  scoped_refptr<ui::UniquePasteboard> pasteboard = new ui::UniquePasteboard;
+      ClipboardUtil::PasteboardItemFromUrl(urlString, nil));
+  scoped_refptr<UniquePasteboard> pasteboard = new UniquePasteboard;
   [pasteboard->get() writeObjects:@[ item ]];
 
-  scoped_refptr<ui::UniquePasteboard> pboard = new ui::UniquePasteboard;
+  scoped_refptr<UniquePasteboard> pboard = new UniquePasteboard;
   [pboard->get() setDataForURL:urlString title:urlString];
 
   NSDictionary* data1 = DictionaryFromPasteboard(pasteboard->get());
@@ -132,3 +133,4 @@ TEST_F(ClipboardUtilMacTest, CompareToWriteToPasteboard) {
 }
 
 }  // namespace
+}  // namespace ui
