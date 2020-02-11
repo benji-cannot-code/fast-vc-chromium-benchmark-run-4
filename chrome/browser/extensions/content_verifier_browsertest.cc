14be821f3c7aefa975b58c8c5ff00dc59ef56eea
Oleg Davydov <burunduk@chromium.org>
2020-02-11 12:08:33 +0000

    Reland "[Extensions] Enable content verification for new policy non-webstore installs"
    This is a reland of bf17bc2533654630d60214b98c951c6657e46111
    
    Original change's description:
    > [Extensions] Enable content verification for new policy non-webstore installs
    > 
    > After this commit:
    >  * For non-webstore policy force-installed extensions, write
    >  computed_hashes.json on installation.
    >  * If (according to computed_hashes.json) extension's resource is
    >  detected corrupted, this extension will be disabled and  reinstalled.
    >  This applies only for policy force-installed extensions, and for
    >  webstore extensions it worked before this commit.
    > 
    > This commit will NOT change behavior for already-installed extension.
    > So, if some (non-webstore) extension was already corrupted, it will not
    > be detected. The reason for this is that there is no source of truth for
    > resources of already-installed non-webstore extension. It's possible to
    > reinstall all previously-installed non-webstore extensions, but it's out
    > of scope of this commit. See the bug for more details.
    > 
    > Bug: 958794
    > Change-Id: Ie9c014e7fd998f4eae6e852a59bd692d57ff7d8c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2012964
    > Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    > Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    > Reviewed-by: Sergey Poromov <poromov@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#737034}
    
    Bug: 958794
    Change-Id: I90d2c398712b1c716f5299a199c72e12e42b0c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2033160
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Oleg Davydov <burunduk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740269}

--- chrome/browser/extensions/content_verifier_browsertest.cc
+++ chrome/browser/extensions/content_verifier_browsertest.cc
@@ -83,7 +83,7 @@ class ContentVerifierTest : public ExtensionBrowserTest {
     // Override content verification mode before ExtensionSystemImpl initializes
     // ChromeContentVerifierDelegate.
     ChromeContentVerifierDelegate::SetDefaultModeForTesting(
-        ChromeContentVerifierDelegate::ENFORCE);
+        ChromeContentVerifierDelegate::VerifyInfo::Mode::ENFORCE);
 
     ON_CALL(update_service_, StartUpdateCheck)
         .WillByDefault(Invoke(this, &ContentVerifierTest::OnUpdateCheck));
