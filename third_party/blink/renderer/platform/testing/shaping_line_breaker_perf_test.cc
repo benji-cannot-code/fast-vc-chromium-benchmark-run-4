9b09d25e72db5c5cb8ac21fdd4bc9e756aa5dc0a
Brian Sheedy <bsheedy@chromium.org>
2020-02-11 00:33:53 +0000

    Switch blink_platform_perftests to histograms
    Switches uses of PrintResult in blink_platform_perftests to
    PerfResultReporter and whitelists it for conversion to histograms before
    uploading the perf dashboard.
    
    Bug: 923564
    Change-Id: I77b81429b0950d32d5dbe96d0c5454c9e15cb64a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047513
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740084}

--- third_party/blink/renderer/platform/testing/shaping_line_breaker_perf_test.cc
+++ third_party/blink/renderer/platform/testing/shaping_line_breaker_perf_test.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/text/text_run.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
-#include "testing/perf/perf_test.h"
+#include "testing/perf/perf_result_reporter.h"
 
 namespace blink {
 namespace {
@@ -26,6 +26,16 @@ namespace {
 static const int kTimeLimitMillis = 2000;
 static const int kWarmupRuns = 5;
 static const int kTimeCheckInterval = 10;
+
+constexpr char kMetricPrefixShapingLineBreaker[] = "ShapingLineBreaker.";
+constexpr char kMetricThroughput[] = "throughput";
+
+perf_test::PerfResultReporter SetUpReporter(const std::string& story) {
+  perf_test::PerfResultReporter reporter(kMetricPrefixShapingLineBreaker,
+                                         story);
+  reporter.RegisterImportantMetric(kMetricThroughput, "runs/s");
+  return reporter;
+}
 
 struct HarfBuzzShaperCallbackContext {
   const HarfBuzzShaper* shaper;
@@ -165,8 +175,8 @@ TEST_F(ShapingLineBreakerPerfTest, ShapeLatinText) {
     timer_.NextLap();
   } while (!timer_.HasTimeLimitExpired());
 
-  perf_test::PrintResult("ShapingLineBreakerPerfTest", "shape latin text", "",
-                         timer_.LapsPerSecond(), "runs/s", true);
+  SetUpReporter("latin_text")
+      .AddResult(kMetricThroughput, timer_.LapsPerSecond());
 }
 
 }  // namespace blink
