c4ba3011bd8f730fb780172e70991d6a11ddffdb
Gauthier Ambard <gambard@chromium.org>
2020-02-11 09:58:42 +0000

    [iOS] Add HTML injection in ErrorPageHelper
    This CL adds a way of creating JavaScript to replace the content of the
    page with some HTML in ErrorPageHelper.
    
    Bug: 991608
    Change-Id: I31b703fee025387e20215dd6edfa6b8b2e4e8b83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027787
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740213}

--- ios/web/navigation/error_page_helper.mm
+++ ios/web/navigation/error_page_helper.mm
@@ -46,7 +46,7 @@ NSString* InjectedErrorPageFilePath() {
 @interface ErrorPageHelper ()
 @property(nonatomic, strong) NSError* error;
 // The error page HTML to be injected into existing page.
-@property(nonatomic, strong, readonly) NSString* HTMLToInject;
+@property(nonatomic, strong) NSString* automaticReloadJavaScript;
 @property(nonatomic, strong, readonly) NSString* failedNavigationURLString;
 @end
 
@@ -54,8 +54,6 @@ NSString* InjectedErrorPageFilePath() {
 
 @synthesize failedNavigationURL = _failedNavigationURL;
 @synthesize errorPageFileURL = _errorPageFileURL;
-@synthesize HTMLToInject = _HTMLToInject;
-@synthesize scriptToInject = _scriptToInject;
 
 - (instancetype)initWithError:(NSError*)error {
   if (self = [super init]) {
@@ -96,8 +94,8 @@ NSString* InjectedErrorPageFilePath() {
   return _errorPageFileURL;
 }
 
-- (NSString*)HTMLToInject {
-  if (!_HTMLToInject) {
+- (NSString*)automaticReloadJavaScript {
+  if (!_automaticReloadJavaScript) {
     NSString* path = InjectedErrorPageFilePath();
     NSString* HTMLTemplate =
         [NSString stringWithContentsOfFile:path
@@ -105,31 +103,10 @@ NSString* InjectedErrorPageFilePath() {
                                      error:nil];
     NSString* failedNavigationURLString =
         EscapeHTMLCharacters(self.failedNavigationURLString);
-    NSString* errorInfo = EscapeHTMLCharacters(self.error.localizedDescription);
-    _HTMLToInject =
-        [NSString stringWithFormat:HTMLTemplate, failedNavigationURLString,
-                                   failedNavigationURLString, errorInfo];
+    _automaticReloadJavaScript =
+        [NSString stringWithFormat:HTMLTemplate, failedNavigationURLString];
   }
-  return _HTMLToInject;
-}
-
-- (NSString*)scriptToInject {
-  if (!_scriptToInject) {
-    NSString* JSON = [[NSString alloc]
-        initWithData:[NSJSONSerialization
-                         dataWithJSONObject:@[ self.HTMLToInject ]
-                                    options:0
-                                      error:nil]
-            encoding:NSUTF8StringEncoding];
-    NSString* escapedHtml =
-        [JSON substringWithRange:NSMakeRange(1, JSON.length - 2)];
-
-    _scriptToInject =
-        [NSString stringWithFormat:
-                      @"document.open(); document.write(%@); document.close();",
-                      escapedHtml];
-  }
-  return _scriptToInject;
+  return _automaticReloadJavaScript;
 }
 
 #pragma mark - Public
@@ -147,6 +124,29 @@ NSString* InjectedErrorPageFilePath() {
   }
 
   return GURL();
+}
+
+- (NSString*)scriptForInjectingHTML:(NSString*)HTML
+                 addAutomaticReload:(BOOL)addAutomaticReload {
+  NSString* HTMLToInject = HTML;
+  if (addAutomaticReload) {
+    HTMLToInject =
+        [HTMLToInject stringByAppendingString:self.automaticReloadJavaScript];
+  }
+
+  // Serialize as JSON to be able to inject HTML characters.
+  NSString* JSON = [[NSString alloc]
+      initWithData:[NSJSONSerialization dataWithJSONObject:@[ HTMLToInject ]
+                                                   options:0
+                                                     error:nil]
+          encoding:NSUTF8StringEncoding];
+  NSString* escapedHTML =
+      [JSON substringWithRange:NSMakeRange(1, JSON.length - 2)];
+
+  return
+      [NSString stringWithFormat:
+                    @"document.open(); document.write(%@); document.close();",
+                    escapedHTML];
 }
 
 - (BOOL)isErrorPageFileURLForFailedNavigationURL:(NSURL*)URL {
