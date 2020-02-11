8d8e9876acdc44715ba420bd3f1257edbb27c464
Luciano Pacheco <lucmult@chromium.org>
2020-02-11 04:49:54 +0000

    Files app: Closure type check for integration tests file_dialog.js
    Add Closure type check for integration tests file_dialog.js and fix
    error pointed by Closure compiler.
    
    Add typedef for the return type of extractElementInfo() which is a JSON
    object sent from the UI to the test: |ElementObject|. This replaces the
    repeated definition of the same object across different function
    arguments and  make the type check stronger between UI and test.
    
    Change "remote_call.js" to depend on runtime_loaded_test_util, because
    it uses it to make the remote call and to have the |ElementObject|.
    
    Bug: 778674
    Change-Id: Ief15cf5120ea11afafb64a14ceceb3e555265616
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045700
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740189}

--- ui/file_manager/integration_tests/file_manager/file_dialog.js
+++ ui/file_manager/integration_tests/file_manager/file_dialog.js
@@ -237,7 +237,7 @@ async function checkFeedbackDisplayHidden(type) {
 
 /**
  * Test file present in Downloads.
- * @{!string}
+ * @const {!string}
  */
 const TEST_LOCAL_FILE = BASIC_LOCAL_ENTRY_SET[0].targetPath;
 
@@ -306,13 +306,13 @@ testcase.saveFileDialogPanelsDisabled = () => {
 
 /**
  * Test file present in Drive only.
- * @{!string}
+ * @const {!string}
  */
 const TEST_DRIVE_FILE = ENTRIES.hello.targetPath;
 
 /**
  * Test file present in Drive only.
- * @{!string}
+ * @const {!string}
  */
 const TEST_DRIVE_PINNED_FILE = ENTRIES.pinned.targetPath;
 
