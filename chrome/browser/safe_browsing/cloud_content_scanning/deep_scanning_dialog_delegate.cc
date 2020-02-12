df9b782b190f23330260f25c415798c5136bf064
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-02-12 17:37:31 +0000

    Fix FILE_ENCRYPTED usage on Deep Scanning upload
    This result now records metrics and events correctly instead of passing
    as a SUCCESS result.
    
    Bug: 1049712
    Change-Id: Ic104d271e53c13d0b78942dfb57effe59e1ef522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042216
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740727}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.cc
@@ -171,6 +171,12 @@ std::string GetFileMimeType(base::FilePath path) {
   return mime_type;
 }
 
+bool AllowEncryptedFiles() {
+  int state = g_browser_process->local_state()->GetInteger(
+      prefs::kAllowPasswordProtectedFiles);
+  return state == ALLOW_UPLOADS || state == ALLOW_UPLOADS_AND_DOWNLOADS;
+}
+
 }  // namespace
 
 // A BinaryUploadService::Request implementation that gets the data to scan
@@ -270,8 +276,9 @@ bool DeepScanningDialogDelegate::ResultShouldAllowDataUse(
       return true;
 
     case BinaryUploadService::Result::FILE_TOO_LARGE:
-    case BinaryUploadService::Result::FILE_ENCRYPTED:
       return false;
+    case BinaryUploadService::Result::FILE_ENCRYPTED:
+      return AllowEncryptedFiles();
   }
 }
 
@@ -556,13 +563,8 @@ void DeepScanningDialogDelegate::AnalyzerCallback(
   // If the file contains encrypted parts and the user is not allowed to use
   // them, fail the request.
   if (contains_encrypted_parts) {
-    int state = g_browser_process->local_state()->GetInteger(
-        prefs::kAllowPasswordProtectedFiles);
-    BinaryUploadService::Result result =
-        state == ALLOW_UPLOADS || state == ALLOW_UPLOADS_AND_DOWNLOADS
-            ? BinaryUploadService::Result::SUCCESS
-            : BinaryUploadService::Result::FILE_ENCRYPTED;
-    FileRequestCallback(data_.paths[index], result,
+    FileRequestCallback(data_.paths[index],
+                        BinaryUploadService::Result::FILE_ENCRYPTED,
                         DeepScanningClientResponse());
     return;
   }
