4f910bf9fddcdc49c605f7b102c1b1df903a0e53
Rainhard Findling <rainhard@chromium.org>
2020-02-12 23:13:23 +0000

    Add safety check settings UI skeleton behind privacy settings redesign flag
    * Introduces safety check page skeleton
    * Adds safety check element to settings in |basic_page|
    
    Bug: 1015841
    Change-Id: Ib9d74ee53da3fb110a9296f65d3cec2335775143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1959036
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740866}

---
+++ chrome/browser/resources/settings/safety_check_page/safety_check_page.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'settings-safety-check-page' is the settings page containing the browser
+ * safety check.
+ */
+(function() {
+
+Polymer({
+  is: 'settings-safety-check-page',
+
+  /** @private */
+  onRunSafetyCheckClick_: function() {
+    // TODO(crbug.com/1010001) Trigger backend safety check here.
+  },
+});
+})();
