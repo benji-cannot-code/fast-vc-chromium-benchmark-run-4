ca0829c9ed9fccd21a84be35c301bed1e02fc80c
James Cook <jamescook@chromium.org>
2020-02-12 23:52:39 +0000

    Cleanup service_manager references in //chromeos
    Nothing in //chromeos uses the service manager, so clean up the build
    files and eliminate an unnecessary include.
    
    Bug: none
    Change-Id: I0b8262a9c3c7bccdc319329ee6018686555cee18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2052607
    Commit-Queue: Ken Rockot <rockot@google.com>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#740883}

--- chromeos/components/drivefs/drivefs_host.cc
+++ chromeos/components/drivefs/drivefs_host.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/invitation.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
-#include "services/service_manager/public/cpp/connector.h"
 
 namespace drivefs {
 
