00560ef700bd158ce8a4d5903ed82edc448b444e
Robert Ma <robertma@chromium.org>
2020-02-12 21:49:18 +0000

    Roll chromium wpt tools to right before MANIFEST v8
    This will be the last roll before we have to rewrite WPTManifest in
    blinkpy or migrate to wptrunner.
    
    --ignore-glob has been upstreamed in
    https://github.com/web-platform-tests/wpt/pull/20242 so the patch is
    removed.
    
    Change-Id: Id9683a351eb7b465dfebc08aa8ae638f5b791754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050731
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Reviewed-by: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740834}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/wpt.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/wpt.py
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # This file exists to allow `python wpt <command>` to work on Windows:
 # https://github.com/web-platform-tests/wpt/pull/6907
-execfile("wpt")
+exec(compile(open("wpt", "r").read(), "wpt", 'exec'))
