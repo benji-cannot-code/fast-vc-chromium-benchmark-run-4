0df95c5880f0664955288c525acb9b36da214eef
Sky Malice <skym@chromium.org>
2020-02-12 17:36:27 +0000

    Add summary fragments to cardrenderer javadocs.
    Bug: 1050094
    Change-Id: I971bc0bc091b5a32684518ecb14f9527267df1f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050012
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740726}

--- chrome/browser/cardrenderer/android/java/src/org/chromium/chrome/browser/cardrenderer/CardRendererAdapter.java
+++ chrome/browser/cardrenderer/android/java/src/org/chromium/chrome/browser/cardrenderer/CardRendererAdapter.java
@@ -19,7 +19,7 @@ public interface CardRendererAdapter {
     void bind(byte[] protoPayload);
 
     /**
-     * @return The single associated view.
+     * Returns the single associated view.
      */
     View getView();
 }
