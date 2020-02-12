215da84e5b9e06134f45dfc26b078036fd4aad76
Eriko Kurimoto <elkurin@google.com>
2020-02-12 03:14:03 +0000

    SharedWorker: Use CSP headers to outside settings
    This CL passes real ContentSecurityPolicy and ContentSecurityPolicyType
    values to creation_params which are used for SharedWorker scripts fetching.
    
    By this change, outsideSettings CSP is applied to scripts statically
    imported from module SharedWorker top-level scripts.
    This behavior is tested in the static import section in the added test.
    (Previously the CSP was not applied at all for such scripts,
    but it's OK because module SharedWorker is not yet shipped.)
    
    This CL doesn't affect the behavior of classic SharedWorker and
    dynamic imports (either from module or classic SharedWorker).
    
    Web-platform-tests to check its behavior on Module SharedWorker are added.
    WPTs for Classic SharedWorker will be added in the following CLs.
    
    Bug: 1049011, 824646
    Change-Id: I6fa79b0394426f09b19da1abbdf9ecf483d558ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038297
    Commit-Queue: Eriko Kurimoto <elkurin@google.com>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740538}

--- third_party/blink/web_tests/external/wpt/workers/modules/resources/dynamic-import-remote-origin-script-worker.sub.js
+++ third_party/blink/web_tests/external/wpt/workers/modules/resources/dynamic-import-remote-origin-script-worker.sub.js
@@ -1,5 +1,18 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Import a remote origin script.
-import('https://{{domains[www1]}}:{{ports[https][0]}}/workers/modules/resources/export-on-load-script.js')
-  .then(module => postMessage(module.importedModules))
-  .catch(e => postMessage(['ERROR']));
+const importUrl =
+    'https://{{domains[www1]}}:{{ports[https][0]}}/workers/modules/resources/export-on-load-script.js';
+if ('DedicatedWorkerGlobalScope' in self &&
+    self instanceof DedicatedWorkerGlobalScope) {
+  import(importUrl)
+      .then(module => postMessage(module.importedModules))
+      .catch(e => postMessage(['ERROR']));
+} else if (
+    'SharedWorkerGlobalScope' in self &&
+    self instanceof SharedWorkerGlobalScope) {
+  onconnect = e => {
+    import(importUrl)
+        .then(module => e.ports[0].postMessage(module.importedModules))
+        .catch(error => e.ports[0].postMessage(['ERROR']));
+  };
+}
--- third_party/blink/web_tests/external/wpt/workers/modules/resources/static-import-remote-origin-script-worker.sub.js
+++ third_party/blink/web_tests/external/wpt/workers/modules/resources/static-import-remote-origin-script-worker.sub.js
@@ -1,4 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Import a remote origin script.
 import * as module from 'https://{{domains[www1]}}:{{ports[https][0]}}/workers/modules/resources/export-on-load-script.js';
-postMessage(module.importedModules);
+if ('DedicatedWorkerGlobalScope' in self &&
+    self instanceof DedicatedWorkerGlobalScope) {
+  postMessage(module.importedModules);
+} else if (
+    'SharedWorkerGlobalScope' in self &&
+    self instanceof SharedWorkerGlobalScope) {
+  onconnect = e => {
+    e.ports[0].postMessage(module.importedModules);
+  };
+}
