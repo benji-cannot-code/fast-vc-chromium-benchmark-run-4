62f2cea162a403beaff73ebe6b99de7809d15a43
Peter Boström <pbos@chromium.org>
2020-02-12 00:57:11 +0000

    Use platform focus behavior for close-x buttons
    This makes the close-x keyboard focusable in most cases. This serves
    a11y users who can detect its presence and use it, like other buttons,
    without the Esc shortcut.
    
    Adds the Close accessibility name back under Windows where the tooltip
    is removed in favor of ones generated by the platform from HTCLOSE.
    
    Bug: chromium:1019873, chromium:1049667, chromium:1049722
    Change-Id: Ib33b992c1085d9368cc7573785b21e1ff05a9a3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047454
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740499}

--- ui/views/controls/editable_combobox/editable_combobox.cc
+++ ui/views/controls/editable_combobox/editable_combobox.cc
@@ -421,6 +421,10 @@ void EditableCombobox::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   node_data->SetValue(GetText());
 }
 
+void EditableCombobox::RequestFocus() {
+  textfield_->RequestFocus();
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // EditableCombobox, TextfieldController overrides:
 
--- ui/views/controls/editable_combobox/editable_combobox.h
+++ ui/views/controls/editable_combobox/editable_combobox.h
@@ -124,6 +124,7 @@ class VIEWS_EXPORT EditableCombobox : public View,
   void Layout() override;
   void OnThemeChanged() override;
   void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
+  void RequestFocus() override;
 
   // Overridden from TextfieldController:
   void ContentsChanged(Textfield* sender,
