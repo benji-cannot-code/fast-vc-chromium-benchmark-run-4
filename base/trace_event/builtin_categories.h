6924ed97f9b80e3d44099cf3d47b8b33db129ffc
Etienne Bergeron <etienneb@chromium.org>
2020-02-12 18:09:48 +0000

    Add trace events for database functions
    Adding trace events for the database functions.
    
    SQLite is suspected to have higher impact on IO than previously
    expected. We are suspecting LockFile contention and Flush
    contention.
    
    Adding these trace events will help seeing the interaction
    between these tasks and other tasks in chrome via slow-reports.
    
    Bug: 1051180
    Change-Id: Ife21c619bd8fafc6e892d6e9d45818d0813018c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050167
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740745}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -130,6 +130,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("shutdown")                                                          \
   X("SiteEngagement")                                                    \
   X("skia")                                                              \
+  X("sql")                                                               \
   X("startup")                                                           \
   X("sync")                                                              \
   X("sync_lock_contention")                                              \
