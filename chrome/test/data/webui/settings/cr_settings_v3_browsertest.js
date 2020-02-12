8cbd1f48ee18567302a7206d57b00cf9a5e21db7
Takashi Toyoshima <toyoshim@chromium.org>
2020-02-12 08:53:56 +0000

    OOR-CORS: CrSettingsV3BrowserTest inheritances depend on OOR-CORS
    Since CrSettingsV3BrowserTest inheritances depend on OOR-CORS
    as some other WebUI tests do, we need to flip the OOR-CORS to be
    enabled explicitly to run tests.
    
    These failed at linux-blink-cors fyi bots, for instance at
    https://ci.chromium.org/p/chromium/builders/ci/linux-blink-cors-rel/1537
    
    Bug: 1042558
    Change-Id: I2f01ed89e0793ca04f4c4f325755f42c5b7a1704
    Tbr: rbpotter@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050294
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740592}

--- chrome/test/data/webui/settings/cr_settings_v3_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_v3_browsertest.js
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Polymer BrowserTest fixture.
 GEN_INCLUDE(['//chrome/test/data/webui/polymer_browser_test_base.js']);
+GEN('#include "services/network/public/cpp/features.h"');
 
 /** Test fixture for shared Polymer 3 elements. */
 // eslint-disable-next-line no-var
@@ -22,6 +23,11 @@ var CrSettingsV3BrowserTest = class extends PolymerTest {
       '//third_party/mocha/mocha.js',
       '//chrome/test/data/webui/mocha_adapter.js',
     ];
+  }
+
+  /** @override */
+  get featureList() {
+    return {enabled: ['network::features::kOutOfBlinkCors']};
   }
 };
 
