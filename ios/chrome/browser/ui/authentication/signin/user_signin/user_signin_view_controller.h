1cba5e36628adcd0382cba59b4b3a98a7d1e11df
Nohemi Fernandez <fernandex@chromium.org>
2020-02-12 17:46:54 +0000

    [iOS] Create user sign-in flow for sign-in architecture migration.
    This patch embeds the UserConsentViewController into the sign-in view
    host. Interactivity and consent buttons will be added separately.
    
    This CL is part of a series of refactors to move the existing sign-in
    architecture to the Coordinator-Mediator design paradigm.
    
    Fore more information, see go/chrome-ios-signin-migration.
    
    Bug: 971989
    Change-Id: Ie0e87842036c68a72aed2a404dfe6a98a1d8cae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041599
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740732}

---
+++ ios/chrome/browser/ui/authentication/signin/user_signin/user_signin_view_controller.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_USER_SIGNIN_USER_SIGNIN_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_USER_SIGNIN_USER_SIGNIN_VIEW_CONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+
+// View controller used to show sign-in UI.
+@interface UserSigninViewController : UIViewController
+
+// Displays |viewController.view| above the UI displayed by the host.
+- (void)showEmbeddedViewController:(UIViewController*)viewController;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_USER_SIGNIN_USER_SIGNIN_VIEW_CONTROLLER_H_
