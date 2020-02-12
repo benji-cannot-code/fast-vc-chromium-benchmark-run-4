64b4ae2febc4ffb321e21ae9c569047f6a384629
Anand K. Mistry <amistry@chromium.org>
2020-02-12 12:19:28 +0000

    Factor common SMB share parameters into a SmbShareInfo class
    A common set of share parameters needs to be passed around. Use a common
    class instead of repeatedly passing multiple arguments to functions.
    
    Bug: 939235
    Change-Id: Idf41c6e09693c1377cc1613ff18bfe97d7ba3f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2049011
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740632}

--- chrome/browser/chromeos/smb_client/smb_service_unittest.cc
+++ chrome/browser/chromeos/smb_client/smb_service_unittest.cc
@@ -58,6 +58,7 @@ constexpr char kTestPassword[] = "my_secret_password";
 constexpr char kTestDomain[] = "EXAMPLE.COM";
 constexpr char kSharePath[] = "\\\\server\\foobar";
 constexpr char kMountPath[] = "smb://server/foobar";
+constexpr char kDisplayName[] = "My Share";
 
 void SaveMountResult(SmbMountResult* out, SmbMountResult result) {
   *out = result;
@@ -132,6 +133,8 @@ class SmbServiceTest : public testing::Test {
     testing::Mock::AllowLeak(mock_client_);
     chromeos::DBusThreadManager::GetSetterForTesting()->SetSmbProviderClient(
         base::WrapUnique(mock_client_));
+
+    mount_options_.display_name = kDisplayName;
   }
 
   ~SmbServiceTest() override {}
@@ -209,6 +212,8 @@ class SmbServiceTest : public testing::Test {
   file_system_provider::FakeRegistry* registry_;  // Owned by |fsp_service_|.
   // Extension Registry and Registry needed for fsp_service.
   std::unique_ptr<extensions::ExtensionRegistry> extension_registry_;
+
+  chromeos::file_system_provider::MountOptions mount_options_;
 };
 
 TEST_F(SmbServiceTest, InvalidUrls) {
@@ -261,7 +266,7 @@ TEST_F(SmbServiceTest, Mount) {
           })));
 
   smb_service_->Mount(
-      {}, base::FilePath(kSharePath), kTestUser, kTestPassword,
+      mount_options_, base::FilePath(kSharePath), kTestUser, kTestPassword,
       false /* use_chromad_kerberos */,
       false /* should_open_file_manager_after_mount */,
       false /* save_credentials */,
@@ -308,7 +313,7 @@ TEST_F(SmbServiceTest, MountSaveCredentials) {
           })));
 
   smb_service_->Mount(
-      {}, base::FilePath(kSharePath), kTestUser, kTestPassword,
+      mount_options_, base::FilePath(kSharePath), kTestUser, kTestPassword,
       false /* use_chromad_kerberos */,
       false /* should_open_file_manager_after_mount */,
       true /* save_credentials */,
---
+++ chrome/browser/chromeos/smb_client/smb_share_info.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/smb_client/smb_share_info.h"
+
+#include "base/logging.h"
+
+namespace chromeos {
+namespace smb_client {
+
+SmbShareInfo::SmbShareInfo(const SmbUrl& share_url,
+                           const std::string& display_name,
+                           const std::string& username,
+                           const std::string& workgroup,
+                           bool use_kerberos)
+    : share_url_(share_url),
+      display_name_(display_name),
+      username_(username),
+      workgroup_(workgroup),
+      use_kerberos_(use_kerberos) {
+  DCHECK(share_url_.IsValid());
+  DCHECK(!display_name.empty());
+}
+
+SmbShareInfo::~SmbShareInfo() = default;
+
+SmbShareInfo::SmbShareInfo(const SmbShareInfo&) = default;
+
+SmbShareInfo& SmbShareInfo::operator=(const SmbShareInfo&) = default;
+
+}  // namespace smb_client
+}  // namespace chromeos
