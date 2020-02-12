23dabfb5457a2a8fa83a1559eb018165cca81c70
yilkal <yilkal@chromium.org>
2020-02-12 17:44:06 +0000

    Whitelisted web app is not throttled.
    This CL makes sure that whitelisted web app
    opened in a tab will not be blocked when web
    time limit is reached.
    
    Bug: 1015661
    Change-Id: If9ab8bfe5fe51a58e256ee129ee0ba1257d3032d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2048024
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740731}

--- chrome/browser/chromeos/child_accounts/child_user_service.cc
+++ chrome/browser/chromeos/child_accounts/child_user_service.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_activity_registry.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_time_controller.h"
+#include "chrome/browser/chromeos/child_accounts/time_limits/app_types.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/web_time_limit_enforcer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/browser_context.h"
@@ -88,6 +89,13 @@ bool ChildUserService::WebTimeLimitWhitelistedURL(const GURL& url) const {
     return false;
   DCHECK(app_time_controller_->web_time_enforcer());
   return app_time_controller_->web_time_enforcer()->IsURLWhitelisted(url);
+}
+
+bool ChildUserService::AppTimeLimitWhitelistedApp(
+    const app_time::AppId& app_id) const {
+  if (!app_time_controller_)
+    return false;
+  return app_time_controller_->app_registry()->IsWhitelistedApp(app_id);
 }
 
 base::TimeDelta ChildUserService::GetWebTimeLimit() const {
--- chrome/browser/chromeos/child_accounts/child_user_service.h
+++ chrome/browser/chromeos/child_accounts/child_user_service.h
@@ -29,6 +29,7 @@ class GURL;
 
 namespace chromeos {
 namespace app_time {
+class AppId;
 class AppTimeController;
 class WebTimeLimitEnforcer;
 }  // namespace app_time
@@ -76,6 +77,10 @@ class ChildUserService : public KeyedService,
   // Viewing of whitelisted |url| does not count towards usage web time.
   // Always returns false if per-app times limits feature is disabled.
   bool WebTimeLimitWhitelistedURL(const GURL& url) const;
+
+  // Returns whether the application with id |app_id| can be used without any
+  // time restrictions.
+  bool AppTimeLimitWhitelistedApp(const app_time::AppId& app_id) const;
 
   // Returns time limit set for using the web on a given day.
   // Should only be called if |features::kPerAppTimeLimits| and
