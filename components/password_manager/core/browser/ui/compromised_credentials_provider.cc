4899fa6db979e259199e3ed9f6ed159396b53e79
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-02-12 15:24:51 +0000

    [Passwords] Add Skeleton for CompromisedCredentialsProvider
    This change adds skeletons for the CompromisedCredentialsProvider class
    and adds tests for its observer interface.
    
    Bug: 1047726
    Change-Id: I4822125942cce09d25d0cd033f7df66a2442564f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050593
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740675}

---
+++ components/password_manager/core/browser/ui/compromised_credentials_provider.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/password_manager/core/browser/ui/compromised_credentials_provider.h"
+#include "base/logging.h"
+
+namespace password_manager {
+
+CompromisedCredentialsProvider::CompromisedCredentialsProvider() = default;
+
+CompromisedCredentialsProvider::~CompromisedCredentialsProvider() = default;
+
+CompromisedCredentialsProvider::CredentialsView
+CompromisedCredentialsProvider::GetCompromisedCredentials() const {
+  // TODO(crbug.com/1047726): Implement.
+  NOTIMPLEMENTED();
+  return {};
+}
+
+void CompromisedCredentialsProvider::AddObserver(Observer* observer) {
+  observers_.AddObserver(observer);
+}
+
+void CompromisedCredentialsProvider::RemoveObserver(Observer* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+void CompromisedCredentialsProvider::NotifyCompromisedCredentialsChanged(
+    CredentialsView credentials) {
+  for (auto& observer : observers_)
+    observer.OnCompromisedCredentialsChanged(credentials);
+}
+
+}  // namespace password_manager
