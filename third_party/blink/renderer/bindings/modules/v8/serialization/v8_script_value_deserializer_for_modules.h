3a8cfc14bae9811675e21ebd99514de0ccc10795
Reilly Grant <reillyg@chromium.org>
2020-02-12 20:09:09 +0000

    Reland "[Shape Detection] Use IDL dictionaries for result types"
    This reverts commit 00f576f288655f9a52a48caf5216664f56c8df01.
    
    Reason for revert: Failed build was missing the expectation changes from this CL.
    
    Original change's description:
    > Revert "[Shape Detection] Use IDL dictionaries for result types"
    >
    > This reverts commit bae2a7ee57bb5f420d4450234b904425310d303e.
    >
    > Reason for revert: Suspect for failing webkit_layout_tests on (none)
    > GPU on Mac
    > https://ci.chromium.org/p/chromium/builders/ci/Mac10.10%20Tests/51385
    >
    > Original change's description:
    > > [Shape Detection] Use IDL dictionaries for result types
    > >
    > > This change updates the DetectedBarcode, DetectedFace and DetectedText
    > > types to be dictionaries rather than interfaces. This is okay because
    > > they have only attributes and no methods. This reduces the amount of
    > > manually written code necessary to implement them.
    > >
    > > This enables implementing a new field for issue 1023177 without having
    > > to update the manual serialization and deserialization routines.
    > >
    > > Spec PR: https://github.com/WICG/shape-detection-api/pull/86
    > >
    > > Bug: 1023177
    > > Change-Id: I3b93d1869c5457e2ce3da5b5690a5ff968a3c77c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040378
    > > Auto-Submit: Reilly Grant <reillyg@chromium.org>
    > > Reviewed-by: Victor Costan <pwnall@chromium.org>
    > > Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Commit-Queue: Reilly Grant <reillyg@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#740356}
    >
    > TBR=yukishiino@chromium.org,jbroman@chromium.org,reillyg@chromium.org,haraken@chromium.org,pwnall@chromium.org
    >
    > Change-Id: Ib2075266871bdae5651e684ac652617de91e2bb2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1023177
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2049945
    > Reviewed-by: Kristi Park <kristipark@chromium.org>
    > Commit-Queue: Kristi Park <kristipark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#740493}
    
    TBR=yukishiino@chromium.org,jbroman@chromium.org,reillyg@chromium.org,haraken@chromium.org,pwnall@chromium.org,kristipark@chromium.org
    
    Change-Id: Ib66d8b28cff5650c608c4e995fea350cedcf313f
    Bug: 1023177
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2049947
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740786}

--- third_party/blink/renderer/bindings/modules/v8/serialization/v8_script_value_deserializer_for_modules.h
+++ third_party/blink/renderer/bindings/modules/v8/serialization/v8_script_value_deserializer_for_modules.h
@@ -12,9 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class CryptoKey;
-class Landmark;
 class NativeFileSystemHandle;
-class Point2D;
 
 // Extends V8ScriptValueSerializer with support for modules/ types.
 class MODULES_EXPORT V8ScriptValueDeserializerForModules final
@@ -46,8 +44,6 @@ class MODULES_EXPORT V8ScriptValueDeserializerForModules final
     return true;
   }
   CryptoKey* ReadCryptoKey();
-  bool ReadLandmark(Landmark* landmark);
-  bool ReadPoint2D(Point2D* point);
   NativeFileSystemHandle* ReadNativeFileSystemHandle(SerializationTag tag);
 };
 
