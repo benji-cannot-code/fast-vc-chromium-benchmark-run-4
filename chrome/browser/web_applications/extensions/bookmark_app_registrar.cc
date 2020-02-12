6b811027d67a68c6d813ca964b6e513c3e9cf72a
Alan Cutter <alancutter@chromium.org>
2020-02-12 02:48:59 +0000

    Make PWAs and Shortcut apps have the same WebKitPrefs scope
    This CL removes one difference between promotable web apps and
    "Create Shortcut" web apps where Blink's understanding of the app's
    scope was disabled for shortcut apps.
    
    This makes promotable web apps and shortcut apps behave the same for
    autoplay policy, keyboard events in app windows and picture in picture
    mode (see uses of Document::IsInWebAppScope()). This is desirable
    because promotable/non-promotable web apps are effectively identical to
    the user so the fewer hidden behavioural differences the better.
    
    Bug: 910016
    Change-Id: I4d1e29566520369436b58ecdd1ba2eaefa17de31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041379
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740528}

--- chrome/browser/web_applications/extensions/bookmark_app_registrar.cc
+++ chrome/browser/web_applications/extensions/bookmark_app_registrar.cc
@@ -43,13 +43,14 @@ bool BookmarkAppRegistrar::IsInstalled(const web_app::AppId& app_id) const {
 
 bool BookmarkAppRegistrar::IsLocallyInstalled(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
-  return extension && BookmarkAppIsLocallyInstalled(profile(), extension);
+  const Extension* extension = GetEnabledExtension(app_id);
+  return extension && extension->from_bookmark() &&
+         BookmarkAppIsLocallyInstalled(profile(), extension);
 }
 
 bool BookmarkAppRegistrar::WasInstalledByUser(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   return extension && !extension->was_installed_by_default();
 }
 
@@ -95,19 +96,19 @@ void BookmarkAppRegistrar::OnShutdown(ExtensionRegistry* registry) {
 
 std::string BookmarkAppRegistrar::GetAppShortName(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   return extension ? extension->short_name() : std::string();
 }
 
 std::string BookmarkAppRegistrar::GetAppDescription(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   return extension ? extension->description() : std::string();
 }
 
 base::Optional<SkColor> BookmarkAppRegistrar::GetAppThemeColor(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   if (!extension)
     return base::nullopt;
 
@@ -121,18 +122,18 @@ base::Optional<SkColor> BookmarkAppRegistrar::GetAppThemeColor(
 
 const GURL& BookmarkAppRegistrar::GetAppLaunchURL(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   return extension ? AppLaunchInfo::GetLaunchWebURL(extension)
                    : GURL::EmptyGURL();
 }
 
 base::Optional<GURL> BookmarkAppRegistrar::GetAppScopeInternal(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   if (!extension)
     return base::nullopt;
 
-  GURL scope_url = GetScopeURLFromBookmarkApp(GetBookmarkApp(app_id));
+  GURL scope_url = GetScopeURLFromBookmarkApp(GetBookmarkAppDchecked(app_id));
   if (scope_url.is_valid())
     return scope_url;
 
@@ -141,7 +142,7 @@ base::Optional<GURL> BookmarkAppRegistrar::GetAppScopeInternal(
 
 DisplayMode BookmarkAppRegistrar::GetAppDisplayMode(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   if (!extension)
     return DisplayMode::kUndefined;
 
@@ -150,7 +151,7 @@ DisplayMode BookmarkAppRegistrar::GetAppDisplayMode(
 
 DisplayMode BookmarkAppRegistrar::GetAppUserDisplayMode(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   if (!extension)
     return DisplayMode::kStandalone;
 
@@ -170,7 +171,7 @@ DisplayMode BookmarkAppRegistrar::GetAppUserDisplayMode(
 std::vector<WebApplicationIconInfo> BookmarkAppRegistrar::GetAppIconInfos(
     const web_app::AppId& app_id) const {
   std::vector<WebApplicationIconInfo> result;
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   if (!extension)
     return result;
   for (const LinkedAppIcons::IconInfo& icon_info :
@@ -185,7 +186,7 @@ std::vector<WebApplicationIconInfo> BookmarkAppRegistrar::GetAppIconInfos(
 
 std::vector<SquareSizePx> BookmarkAppRegistrar::GetAppDownloadedIconSizes(
     const web_app::AppId& app_id) const {
-  const Extension* extension = GetBookmarkApp(app_id);
+  const Extension* extension = GetBookmarkAppDchecked(app_id);
   return extension ? GetBookmarkAppDownloadedIconSizes(extension)
                    : std::vector<SquareSizePx>();
 }
@@ -219,7 +220,7 @@ const Extension* BookmarkAppRegistrar::FindExtension(
   return ExtensionRegistry::Get(profile())->GetInstalledExtension(app_id);
 }
 
-const Extension* BookmarkAppRegistrar::GetBookmarkApp(
+const Extension* BookmarkAppRegistrar::GetBookmarkAppDchecked(
     const web_app::AppId& app_id) const {
   const Extension* extension = GetEnabledExtension(app_id);
   DCHECK(!extension || extension->from_bookmark());
