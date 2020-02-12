733f8b9eaff495b18d6c21830ddfb9f623e1bc42
Kristi Park <kristipark@chromium.org>
2020-02-12 20:29:06 +0000

    Revert "[ChromeDriver] After test timeout, trigger crash"
    This reverts commit ad16c6ff9c9c27afa201864d63ff9c32574319d6.
    
    Reason for revert: Suspected cause of recent flakes for
    __main__.ChromeDriverSecureContextTest.testAddCredentialBase64Errors
    https://ci.chromium.org/p/chromium/builders/try/mac-rel/301082
    
    Original change's description:
    > [ChromeDriver] After test timeout, trigger crash
    > 
    > After a test times out during a ChromeDriver command, trigger a Chrome
    > crash and a minidump collection for troubleshooting. This is not
    > available on tests connecting to a remote browser, or when running on
    > Android.
    > 
    > 
    > Bug: 1045241
    > Change-Id: I3cb82d95875d0f58a4acee69d249a92801b8024b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032238
    > Reviewed-by: John Chen <johnchen@chromium.org>
    > Commit-Queue: Tricia Crichton <triciac@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#740311}
    
    TBR=johnchen@chromium.org,triciac@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1045241, 1051383
    Change-Id: I851c1b636f7ae43a6c23e01f00f3d093b58cc235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050676
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740794}

--- chrome/test/chromedriver/client/chromedriver.py
+++ chrome/test/chromedriver/client/chromedriver.py
@@ -140,7 +140,6 @@ class ChromeDriver(object):
           ChromeDriver.retried_tests.append(kwargs.get('test_name'))
           self._InternalInit(*args, **kwargs)
         else:
-          self._RequestCrash()
           raise
 
   def _InternalInit(self, server_url, chrome_binary=None, android_package=None,
@@ -278,8 +277,6 @@ class ChromeDriver(object):
       self.w3c_compliant = True
       self._session_id = response['value']['sessionId']
       self.capabilities = self._UnwrapValue(response['value']['capabilities'])
-      self.debuggerAddress = str(
-          self.capabilities['goog:chromeOptions']['debuggerAddress'])
     elif isinstance(response['status'], int):
       self.w3c_compliant = False
       self._session_id = response['sessionId']
@@ -326,13 +323,7 @@ class ChromeDriver(object):
 
   def _ExecuteCommand(self, command, params={}):
     params = self._WrapValue(params)
-    try:
-      response = self._executor.Execute(command, params)
-    except Exception as e:
-      if e.message.startswith('timed out') and self._session_id != None:
-        self._RequestCrash()
-      raise e
-
+    response = self._executor.Execute(command, params)
     if ('status' in response
         and response['status'] != 0):
       raise _ExceptionForLegacyResponse(response)
@@ -340,14 +331,6 @@ class ChromeDriver(object):
           and 'error' in response['value']):
       raise _ExceptionForStandardResponse(response)
     return response
-
-  def _RequestCrash(self):
-    tempDriver = ChromeDriver(self._server_url,
-      debugger_address=self.debuggerAddress, test_name='_forceCrash')
-    tempDriver.SendCommandAndGetResult("Page.crash", {})
-    # allow time to complete writing the minidump
-    time.sleep(5)
-    tempDriver.Quit();
 
   def ExecuteCommand(self, command, params={}):
     params['sessionId'] = self._session_id
