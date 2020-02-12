f49052c1b3d6d568049e580ef0474da3326e3a05
Luke Zielinski <lpz@chromium.org>
2020-02-12 16:05:21 +0000

    [blinkpy] Include trailing_comments in Expectation objects returned from TestExpectations.
    This is part 2/2 of this change, the typ-side change is crrev.com/c/2040012
    
    Change-Id: I4e3ee43399bfd7b3999db9e94e38bbc0adbf3442
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2039999
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740690}

--- third_party/blink/tools/blinkpy/web_tests/models/test_expectations_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/models/test_expectations_unittest.py
@@ -62,7 +62,7 @@ class Base(unittest.TestCase):
         return """
 # results: [ Failure Crash ]
 failures/expected/text.html [ Failure ]
-failures/expected/crash.html [ Crash ]
+failures/expected/crash.html [ Crash ] # foo and bar
 failures/expected/image_checksum.html [ Crash ]
 failures/expected/image.html [ Crash ]
 """
@@ -86,6 +86,9 @@ failures/expected/image.html [ Crash ]
         with self.assertRaises(ParseError):
             self.parse_exp(expectations, is_lint_mode=True, overrides=overrides)
 
+    def assert_trailing_comments(self, test, comments):
+        self.assertEqual(self._exp.get_expectations(test).trailing_comments, comments)
+
 
 class BasicTests(Base):
 
@@ -95,6 +98,7 @@ class BasicTests(Base):
         self.assert_exp_list('failures/expected/image_checksum.html', [ResultType.Crash])
         self.assert_exp('passes/text.html', ResultType.Pass)
         self.assert_exp('failures/expected/image.html', ResultType.Crash)
+        self.assert_trailing_comments('failures/expected/crash.html', ' # foo and bar\n')
 
 
 class FlagExpectationsTests(Base):
@@ -418,4 +422,3 @@ failures/expected/text.html [ Failure ]
         self.parse_exp(exp_str)
         self.assert_exp('failures/expected/text.html', ResultType.Failure)
         self.assert_exp_list('failures/expected/crash.html', [ResultType.Crash])
-
