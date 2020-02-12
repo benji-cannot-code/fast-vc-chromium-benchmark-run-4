24c74dff7840573986ecae3ab0c132078e18ea04
Yutaka Hirano <yhirano@chromium.org>
2020-02-12 16:03:21 +0000

    Move HTTP structured headers implementation to net/
    This is a reland of https://crrev.com/c/2041384 which was reverted
    due to a build failure: see https://crrev.com/c/2051272.
    
    Discussion: https://groups.google.com/a/chromium.org/forum/#!msg/loading-dev/dvEirmhddus/OFjNantxAwAJ
    
    I made some non-trivial edits.
    
     - Changed the file name from structured_header.* to
       structured_headers.* to be aligned with other classes in the
       directory.
     - Changed the namespace from structured_header to
       structured_headers to match with the filename.
     - Changed const base::StringPiece& params to base::StringPiece.
     - Started using std::tie for equality operators.
    
    This CL keeps blink/public/common/http/structured_header.h. I will
    change the existing call sites in a follow-up CL.
    
    Bug: 1049936
    Change-Id: I92b83a56940723c81684da9c37376f01840ea747
    Tbr: kinuko@chromium.org, iclelland@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050418
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740689}

--- net/http/structured_headers.cc
+++ net/http/structured_headers.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/public/common/http/structured_header.h"
+#include "net/http/structured_headers.h"
 
 #include <cmath>
 #include <string>
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 
-namespace blink {
-namespace http_structured_header {
+namespace net {
+namespace structured_headers {
 
 namespace {
 
@@ -61,8 +61,7 @@ class StructuredHeaderParser {
     kDraft09,
     kDraft13,
   };
-  explicit StructuredHeaderParser(const base::StringPiece& str,
-                                  DraftVersion version)
+  explicit StructuredHeaderParser(base::StringPiece str, DraftVersion version)
       : input_(str), version_(version) {
     // [SH09] 4.2 Step 1.
     // [SH13] 4.2 Step 2.
@@ -698,7 +697,7 @@ ParameterisedIdentifier::ParameterisedIdentifier(Item id, const Parameters& ps)
     : identifier(std::move(id)), params(ps) {}
 ParameterisedIdentifier::~ParameterisedIdentifier() = default;
 
-base::Optional<Item> ParseItem(const base::StringPiece& str) {
+base::Optional<Item> ParseItem(base::StringPiece str) {
   StructuredHeaderParser parser(str, StructuredHeaderParser::kDraft13);
   base::Optional<Item> item = parser.ReadItem();
   if (item && parser.FinishParsing())
@@ -707,7 +706,7 @@ base::Optional<Item> ParseItem(const base::StringPiece& str) {
 }
 
 base::Optional<ParameterisedList> ParseParameterisedList(
-    const base::StringPiece& str) {
+    base::StringPiece str) {
   StructuredHeaderParser parser(str, StructuredHeaderParser::kDraft09);
   base::Optional<ParameterisedList> param_list = parser.ReadParameterisedList();
   if (param_list && parser.FinishParsing())
@@ -715,7 +714,7 @@ base::Optional<ParameterisedList> ParseParameterisedList(
   return base::nullopt;
 }
 
-base::Optional<ListOfLists> ParseListOfLists(const base::StringPiece& str) {
+base::Optional<ListOfLists> ParseListOfLists(base::StringPiece str) {
   StructuredHeaderParser parser(str, StructuredHeaderParser::kDraft09);
   base::Optional<ListOfLists> list_of_lists = parser.ReadListOfLists();
   if (list_of_lists && parser.FinishParsing())
@@ -723,7 +722,7 @@ base::Optional<ListOfLists> ParseListOfLists(const base::StringPiece& str) {
   return base::nullopt;
 }
 
-base::Optional<List> ParseList(const base::StringPiece& str) {
+base::Optional<List> ParseList(base::StringPiece str) {
   StructuredHeaderParser parser(str, StructuredHeaderParser::kDraft13);
   base::Optional<List> list = parser.ReadList();
   if (list && parser.FinishParsing())
@@ -745,5 +744,5 @@ base::Optional<std::string> SerializeList(const List& value) {
   return base::nullopt;
 }
 
-}  // namespace http_structured_header
-}  // namespace blink
+}  // namespace structured_headers
+}  // namespace net
--- net/http/structured_headers_unittest.cc
+++ net/http/structured_headers_unittest.cc
@@ -3,15 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/public/common/http/structured_header.h"
+#include "net/http/structured_headers.h"
+
+#include <math.h>
 
 #include <limits>
 #include <string>
 
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace blink {
-namespace http_structured_header {
+namespace net {
+namespace structured_headers {
 namespace {
 
 // Helpers to make test cases clearer
@@ -997,5 +999,5 @@ TEST(StructuredHeaderTest, UnserializableLists) {
   }
 }
 
-}  // namespace http_structured_header
-}  // namespace blink
+}  // namespace structured_headers
+}  // namespace net
