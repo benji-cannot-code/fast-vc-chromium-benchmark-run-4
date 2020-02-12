a784f816cb89fbad693117fa728f103af8e6822c
Jordan Taylor <jortaylo@microsoft.com>
2020-02-12 23:51:33 +0000

    Removed exception for setting playback rate on scroll linked animations
    Initial hold time for scroll-linked animations now accounts for playback
    rate.
    
    Added test for setting playback rate on scroll linked animations
    
    Bug: 916117
    Change-Id: Ibd7e80ce60346d2f13155b13b8cc14252afc0c3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970911
    Commit-Queue: Jordan Taylor <jortaylo@microsoft.com>
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740882}

---
+++ third_party/blink/web_tests/external/wpt/scroll-animations/testcommon.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function createScroller(test) {
+  var scroller = createDiv(test);
+  scroller.innerHTML = "<div class='contents'></div>";
+  scroller.classList.add('scroller');
+  return scroller;
+}
+
+function createScrollTimeline(test) {
+  return new ScrollTimeline({
+    scrollSource: createScroller(test),
+    timeRange: 1000
+  });
+}
+
+function createScrollLinkedAnimation(test, timeline) {
+  if(timeline === undefined)
+    timeline = createScrollTimeline(test);
+  const DURATION = 1000; // ms
+  const KEYFRAMES = { opacity: [1, 0] };
+  return new Animation(
+    new KeyframeEffect(createDiv(test), KEYFRAMES, DURATION), timeline);
+}
