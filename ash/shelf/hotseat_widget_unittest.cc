7b1a89458ce0fe7205048b88efcb710f8f0dc893
Manu Cornet <manucornet@chromium.org>
2020-02-12 19:27:17 +0000

    CrOS Shelf: Replace some deprecated accessors to shelf components
    Change-Id: Iebcfc406962d222d2f652cc21774f60686813dc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050000
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740774}

--- ash/shelf/hotseat_widget_unittest.cc
+++ ash/shelf/hotseat_widget_unittest.cc
@@ -371,7 +371,7 @@ TEST_P(HotseatWidgetTest, SwipeUpInAppShelfShowsHotseat) {
 
   // Swipe down from the hotseat to hide it.
   gfx::Rect hotseat_bounds =
-      GetShelfWidget()->hotseat_widget()->GetWindowBoundsInScreen();
+      GetPrimaryShelf()->hotseat_widget()->GetWindowBoundsInScreen();
   gfx::Point start = hotseat_bounds.top_center();
   gfx::Point end = start + gfx::Vector2d(0, 80);
   const base::TimeDelta kTimeDelta = base::TimeDelta::FromMilliseconds(100);
@@ -1217,7 +1217,7 @@ TEST_P(HotseatWidgetTest, SwipeDownOnFocusedHotseat) {
   // hidden.
   GetPrimaryShelf()->shelf_focus_cycler()->FocusShelf(false /* last_element */);
   gfx::Rect hotseat_bounds =
-      GetShelfWidget()->hotseat_widget()->GetWindowBoundsInScreen();
+      GetPrimaryShelf()->hotseat_widget()->GetWindowBoundsInScreen();
   gfx::Point start = hotseat_bounds.top_center();
   gfx::Point end = start + gfx::Vector2d(0, 80);
   GetEventGenerator()->GestureScrollSequence(
@@ -1243,7 +1243,7 @@ TEST_P(HotseatWidgetTest, ExitOverviewWithClickOnHotseat) {
   // Enter overview, hotseat is visible. Choose the point to the farthest left.
   // This point will not be visible.
   auto* overview_controller = Shell::Get()->overview_controller();
-  auto* hotseat_widget = GetShelfWidget()->hotseat_widget();
+  auto* hotseat_widget = GetPrimaryShelf()->hotseat_widget();
   overview_controller->StartOverview();
   ASSERT_TRUE(overview_controller->InOverviewSession());
   ASSERT_EQ(HotseatState::kExtended, GetShelfLayoutManager()->hotseat_state());
@@ -1668,7 +1668,7 @@ TEST_P(HotseatWidgetTest, HotseatHidesWhenSwipedToBezel) {
 
   gfx::Rect shelf_widget_bounds = GetShelfWidget()->GetWindowBoundsInScreen();
   gfx::Rect hotseat_bounds =
-      GetShelfWidget()->hotseat_widget()->GetWindowBoundsInScreen();
+      GetPrimaryShelf()->hotseat_widget()->GetWindowBoundsInScreen();
   gfx::Point start = hotseat_bounds.CenterPoint();
   const gfx::Point end =
       gfx::Point(shelf_widget_bounds.x() + shelf_widget_bounds.width() / 2,
@@ -1832,7 +1832,7 @@ TEST_P(HotseatWidgetTest, HotseatRemainsHiddenIfPopupLaunched) {
 
   // Hide hotseat by clicking outside its bounds.
   gfx::Rect hotseat_bounds =
-      GetShelfWidget()->hotseat_widget()->GetWindowBoundsInScreen();
+      GetPrimaryShelf()->hotseat_widget()->GetWindowBoundsInScreen();
   gfx::Point start = hotseat_bounds.top_center();
   GetEventGenerator()->GestureTapAt(gfx::Point(start.x() + 1, start.y() - 1));
   EXPECT_EQ(HotseatState::kHidden, GetShelfLayoutManager()->hotseat_state());
