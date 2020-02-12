0f5153b5a18dd8e50fdb2c1250a359ad68319219
En-Shuo Hsu <enshuo@chromium.org>
2020-02-12 22:49:51 +0000

    Add BluetoothBatteryChanged event and interface
    This event and corresponding interface will be used to support battery
    indicator for headphone supporting Hands-Free Profile (HFP).
    
    Design doc: go/cros-bt-bi-hfp
    
    BUG=chromium:785758,b:140783217
    TEST=build chrome and run chromeos_unittests
    
    Change-Id: Ieda4b83f4aa86f21038e7e6a9e9e5aa2b58cba0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041970
    Auto-Submit: En-Shuo Hsu <enshuo@chromium.org>
    Commit-Queue: En-Shuo Hsu <enshuo@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740851}

--- chromeos/audio/cras_audio_handler.cc
+++ chromeos/audio/cras_audio_handler.cc
@@ -91,6 +91,10 @@ void CrasAudioHandler::AudioObserver::OnHotwordTriggered(
     uint64_t /* tv_sec */,
     uint64_t /* tv_nsec */) {}
 
+void CrasAudioHandler::AudioObserver::OnBluetoothBatteryChanged(
+    const std::string& /* address */,
+    uint32_t /* level */) {}
+
 void CrasAudioHandler::AudioObserver::OnOutputStarted() {}
 
 void CrasAudioHandler::AudioObserver::OnOutputStopped() {}
@@ -763,6 +767,12 @@ void CrasAudioHandler::HotwordTriggered(uint64_t tv_sec, uint64_t tv_nsec) {
 
 void CrasAudioHandler::NumberOfActiveStreamsChanged() {
   GetNumberOfOutputStreams();
+}
+
+void CrasAudioHandler::BluetoothBatteryChanged(const std::string& address,
+                                               uint32_t level) {
+  for (auto& observer : observers_)
+    observer.OnBluetoothBatteryChanged(address, level);
 }
 
 void CrasAudioHandler::OnAudioPolicyPrefChanged() {
