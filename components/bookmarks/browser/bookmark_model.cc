99adf688e6623d65da28c16c158bfce7d742374a
Mikel Astiz <mastiz@chromium.org>
2020-02-13 17:47:13 +0000

    bookmarks: Add more fine-grained DCHECKs upon creation
    IsValidIndex() captures the very same preconditions but calling out the
    validity of the parent in dedicated DCHECKs helps readability and allows
    understanding user reports better.
    
    Bug: 1051614
    Change-Id: If10e53fc52b3f6003dba9f9b2bf386ccf75de3e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2054165
    Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741114}

--- components/bookmarks/browser/bookmark_model.cc
+++ components/bookmarks/browser/bookmark_model.cc
@@ -611,6 +611,8 @@ const BookmarkNode* BookmarkModel::AddFolder(
     base::Optional<std::string> guid) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(loaded_);
+  DCHECK(parent);
+  DCHECK(parent->is_folder());
   DCHECK(!is_root_node(parent));
   DCHECK(IsValidIndex(parent, index, true));
 
@@ -641,6 +643,8 @@ const BookmarkNode* BookmarkModel::AddURL(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(loaded_);
   DCHECK(url.is_valid());
+  DCHECK(parent);
+  DCHECK(parent->is_folder());
   DCHECK(!is_root_node(parent));
   DCHECK(IsValidIndex(parent, index, true));
 
