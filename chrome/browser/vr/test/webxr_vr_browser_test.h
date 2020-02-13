713053ac429f8f7f0cc93a865fdac7894442c37b
Alex Cooper <alcooper@chromium.org>
2020-02-13 20:12:50 +0000

    Update xr_browser_tests for permissions prompt
    Updates xr_browser_test infrastructure to appropriately grant (or deny)
    permissions, as well as providing tests with a way to override that.
    This ensures that tests will keep running when the permissions prompt is
    enabled by default and can be cleaned up as time permits.
    
    Additionally, refactors consent flow tests to a base class and two sub-
    classes. These sub classes can be used to explicitly test the custom
    consent flow or permissions prompt behavior. A (local) helper macro was
    created to indicate which of these tests should be run for both versions
    versus which tests were specific only to one or the other. To simplify
    this change (and because the permissions flow is runtime agnostic),
    consent flow tests are now run against only one runtime.
    
    Bug: 1043241
    Change-Id: Ifb9040c46993f8b0375383426a4cafa7e342f6da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055304
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741172}

--- chrome/browser/vr/test/webxr_vr_browser_test.h
+++ chrome/browser/vr/test/webxr_vr_browser_test.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_VR_TEST_WEBXR_VR_BROWSER_TEST_H_
 
 #include "build/build_config.h"
+#include "chrome/browser/permissions/permission_request_manager.h"
 #include "chrome/browser/vr/test/conditional_skipping.h"
 #include "chrome/browser/vr/test/mock_xr_device_hook_base.h"
 #include "chrome/browser/vr/test/webxr_browser_test.h"
@@ -40,6 +41,10 @@ class WebXrVrBrowserTestBase : public WebXrBrowserTestBase {
   void EndSession(content::WebContents* web_contents) override;
   void EndSessionOrFail(content::WebContents* web_contents) override;
 
+  PermissionRequestManager* GetPermissionRequestManager();
+  PermissionRequestManager* GetPermissionRequestManager(
+      content::WebContents* web_contents);
+
   virtual gfx::Vector3dF GetControllerOffset() const;
 
   // Necessary to use the WebContents-less versions of functions.
@@ -49,6 +54,9 @@ class WebXrVrBrowserTestBase : public WebXrBrowserTestBase {
   using WebXrBrowserTestBase::EnterSessionWithUserGestureOrFail;
   using WebXrBrowserTestBase::EndSession;
   using WebXrBrowserTestBase::EndSessionOrFail;
+
+  PermissionRequestManager::AutoResponseType permission_auto_response_ =
+      PermissionRequestManager::ACCEPT_ALL;
 
   // Methods/objects for managing consent. If SetupFakeConsentManager is never
   // called, the test will default to mocking out the consent prompt and always
