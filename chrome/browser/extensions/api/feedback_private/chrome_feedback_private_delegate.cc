7346ca5621cf67547fd7ae07419bc6d6267eea6f
James Cook <jamescook@chromium.org>
2020-02-13 21:26:35 +0000

    chromeos: Convert feedback to use consent aware identity API
    SplitSettingsSync will allow the user to opt-out of browser sync.
    However, IdentityAccessor::GetPrimaryAccountInfo() defaults to
    ConsentLevel::kSync, so it returns an empty struct if the user has not
    consented to the browser sync feature.
    
    Feedback isn't tied to browser sync. It has its own consent text in
    the feedback dialog.
    
    Switch to using the "unconsented" primary account. On Chrome OS this
    account always exists for the logged-in user account, whether or not
    the user consented to browser sync.
    
    go/cros-primary-account and go/consent-aware-api-dd
    
    Bug: 1042400, 1046746
    Test: Send feedback, report shows up in dashboard
    Change-Id: I3f515046994dbb13e826b4c0d62e765ac7025857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053128
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741204}

--- chrome/browser/extensions/api/feedback_private/chrome_feedback_private_delegate.cc
+++ chrome/browser/extensions/api/feedback_private/chrome_feedback_private_delegate.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/simple_message_box.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/feedback/system_logs/system_logs_fetcher.h"
+#include "components/signin/public/identity_manager/consent_level.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_context.h"
@@ -240,8 +241,12 @@ std::string ChromeFeedbackPrivateDelegate::GetSignedInUserEmail(
     content::BrowserContext* context) const {
   auto* identity_manager = IdentityManagerFactory::GetForProfile(
       Profile::FromBrowserContext(context));
-  return identity_manager ? identity_manager->GetPrimaryAccountInfo().email
-                          : std::string();
+  if (!identity_manager)
+    return std::string();
+  // Browser sync consent is not required to use feedback.
+  return identity_manager
+      ->GetPrimaryAccountInfo(signin::ConsentLevel::kNotRequired)
+      .email;
 }
 
 void ChromeFeedbackPrivateDelegate::NotifyFeedbackDelayed() const {
--- chrome/browser/feedback/feedback_uploader_chrome.cc
+++ chrome/browser/feedback/feedback_uploader_chrome.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "components/feedback/feedback_report.h"
+#include "components/signin/public/identity_manager/consent_level.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_access_token_fetcher.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -76,14 +77,18 @@ void FeedbackUploaderChrome::StartDispatchingReport() {
   signin::IdentityManager* identity_manager =
       IdentityManagerFactory::GetForProfile(profile);
 
-  if (identity_manager && identity_manager->HasPrimaryAccount()) {
+  // Sync consent is not required to send feedback because the feedback dialog
+  // has its own privacy notice.
+  if (identity_manager &&
+      identity_manager->HasPrimaryAccount(signin::ConsentLevel::kNotRequired)) {
     identity::ScopeSet scopes;
     scopes.insert("https://www.googleapis.com/auth/supportcontent");
     token_fetcher_ = std::make_unique<signin::PrimaryAccountAccessTokenFetcher>(
         "feedback_uploader_chrome", identity_manager, scopes,
         base::BindOnce(&FeedbackUploaderChrome::AccessTokenAvailable,
                        base::Unretained(this)),
-        signin::PrimaryAccountAccessTokenFetcher::Mode::kImmediate);
+        signin::PrimaryAccountAccessTokenFetcher::Mode::kImmediate,
+        signin::ConsentLevel::kNotRequired);
     return;
   }
 
