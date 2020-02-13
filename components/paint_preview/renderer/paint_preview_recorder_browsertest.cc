a276b5c06a28730e066f5c0772f3930b960e494c
Calder Kitagawa <ckitagawa@chromium.org>
2020-02-13 00:40:45 +0000

    Revert "[CodeHealth] Add embedding token to main frame"
    This reverts commit 4e0b40b1896ad674ce4bc4f487c048c167d7ba4b.
    
    Reason for revert: crbug/1051129 and crbug/1051424
    
    Back-Forward Cache experiment caused crashes with this patch.
    
    This is due to same-document navigation state being different across
    the navigations.
    
    Original change's description:
    > [CodeHealth] Add embedding token to main frame
    > 
    > A relatively simple modification to add embedding tokens to the main
    > frame. This has the same behavior as embedding tokens for remote frames
    > except it doesn't propagate the token to a parent.
    > 
    > Behaviors
    > - Add a token on first navigation.
    > - Swap the token on any cross-process navigation.
    > 
    > Bug: 1045014
    > Change-Id: Ia3a6ba58913b11167e0eb10b5d6f3399f5e3cfcd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032024
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#739504}
    
    TBR=dcheng@chromium.org,alexmos@chromium.org,dtapuska@chromium.org,ckitagawa@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1045014
    Change-Id: I95f7948da12b263ab9adcecf38c36eca217c1029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2052599
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740904}

--- components/paint_preview/renderer/paint_preview_recorder_browsertest.cc
+++ components/paint_preview/renderer/paint_preview_recorder_browsertest.cc
@@ -99,9 +99,7 @@ TEST_F(PaintPreviewRecorderRenderViewTest, TestCaptureMainFrameAndClipping) {
       base::BindOnce(&OnCaptureFinished, mojom::PaintPreviewStatus::kOk,
                      base::Unretained(&out_response)));
 
-  EXPECT_TRUE(out_response->embedding_token.has_value());
-  EXPECT_EQ(frame->GetWebFrame()->GetEmbeddingToken(),
-            out_response->embedding_token.value());
+  EXPECT_FALSE(out_response->embedding_token.has_value());
   EXPECT_EQ(out_response->content_id_to_embedding_token.size(), 0U);
 
   EXPECT_EQ(out_response->links.size(), 1U);
@@ -173,9 +171,7 @@ TEST_F(PaintPreviewRecorderRenderViewTest, TestCaptureFragment) {
       base::BindOnce(&OnCaptureFinished, mojom::PaintPreviewStatus::kOk,
                      &out_response));
 
-  EXPECT_TRUE(out_response->embedding_token.has_value());
-  EXPECT_EQ(frame->GetWebFrame()->GetEmbeddingToken(),
-            out_response->embedding_token.value());
+  EXPECT_FALSE(out_response->embedding_token.has_value());
   EXPECT_EQ(out_response->content_id_to_embedding_token.size(), 0U);
 
   EXPECT_EQ(out_response->links.size(), 1U);
@@ -232,9 +228,7 @@ TEST_F(PaintPreviewRecorderRenderViewTest, TestCaptureMainFrameAndLocalFrame) {
       std::move(params),
       base::BindOnce(&OnCaptureFinished, mojom::PaintPreviewStatus::kOk,
                      base::Unretained(&out_response)));
-  EXPECT_TRUE(out_response->embedding_token.has_value());
-  EXPECT_EQ(frame->GetWebFrame()->GetEmbeddingToken(),
-            out_response->embedding_token.value());
+  EXPECT_FALSE(out_response->embedding_token.has_value());
   EXPECT_EQ(out_response->content_id_to_embedding_token.size(), 0U);
 }
 
