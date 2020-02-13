1761f36ebcbd91b9be3f8b41de226f17e170135f
Avery Musbach <amusbach@chromium.org>
2020-02-13 23:12:49 +0000

    overview: Improve code maintainability
    OverviewWindowDragController::ActivateDraggedWindow relies on the fact
    that kMinimumDragDistanceDp is less than or equal to kWindowMargin. If
    either or both constants were modified so that this condition is not
    met, then there would be an obscure bug where you drag an overview item
    from a display where split view mode is active, and you drag to another
    display but by a distance of less than kMinimumDragDistanceDp. Then the
    window would be snapped on the destination display, but the check for
    snappability would be with respect to the source display, and so you
    could trigger a DCHECK failure by snapping a window on a display where
    it is not snappable.
    
    Bug: 961170
    Change-Id: I0fbf16b50ae2cb5dbd9a5dbefd8317cc9f1e9a11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055349
    Commit-Queue: Avery Musbach <amusbach@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741249}

--- ash/wm/overview/overview_window_drag_controller.cc
+++ ash/wm/overview/overview_window_drag_controller.cc
@@ -312,7 +312,8 @@ void OverviewWindowDragController::ActivateDraggedWindow() {
       split_state == SplitViewController::State::kNoSnap) {
     overview_session_->SelectWindow(item_);
   } else if (split_view_controller->CanSnapWindow(item_->GetWindow())) {
-    SnapWindow(split_state == SplitViewController::State::kLeftSnapped
+    SnapWindow(split_view_controller,
+               split_state == SplitViewController::State::kLeftSnapped
                    ? SplitViewController::RIGHT
                    : SplitViewController::LEFT);
   } else {
@@ -568,7 +569,8 @@ OverviewWindowDragController::CompleteNormalDrag(
 
   // Snap a window if appropriate.
   if (should_allow_split_view_ && snap_position_ != SplitViewController::NONE) {
-    SnapWindow(snap_position_);
+    SnapWindow(SplitViewController::Get(GetRootWindowBeingDraggedIn()),
+               snap_position_);
     overview_session_->PositionWindows(/*animate=*/true);
     return DragResult::kSnap;
   }
@@ -687,6 +689,7 @@ SplitViewController::SnapPosition OverviewWindowDragController::GetSnapPosition(
 }
 
 void OverviewWindowDragController::SnapWindow(
+    SplitViewController* split_view_controller,
     SplitViewController::SnapPosition snap_position) {
   DCHECK_NE(snap_position, SplitViewController::NONE);
 
@@ -694,9 +697,8 @@ void OverviewWindowDragController::SnapWindow(
   DCHECK(!SplitViewController::Get(Shell::GetPrimaryRootWindow())
               ->IsDividerAnimating());
   aura::Window* window = item_->GetWindow();
-  SplitViewController::Get(GetRootWindowBeingDraggedIn())
-      ->SnapWindow(window, snap_position,
-                   /*use_divider_spawn_animation=*/true);
+  split_view_controller->SnapWindow(window, snap_position,
+                                    /*use_divider_spawn_animation=*/true);
   item_ = nullptr;
   wm::ActivateWindow(window);
 }
