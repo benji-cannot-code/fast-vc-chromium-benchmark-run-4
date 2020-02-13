d47335064ac4f7266db78f159db1ff0e364430a7
Ayu Ishii <ayui@chromium.org>
2020-02-13 16:30:56 +0000

    Convert base::Bind and base::Callback in components/autofill_assistant to Once/Repeating
    Bug: 1007669
    Change-Id: If7703f00ebfc63b993a564b2519fb8f4ffdd30a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050822
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741081}

--- components/autofill_assistant/browser/web/web_controller_browsertest.cc
+++ components/autofill_assistant/browser/web/web_controller_browsertest.cc
@@ -125,7 +125,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     run_loop.Run();
   }
 
-  void CheckElementVisibleCallback(const base::Closure& done_callback,
+  void CheckElementVisibleCallback(base::OnceClosure done_callback,
                                    const Selector& selector,
                                    size_t* pending_number_of_checks_output,
                                    bool expected_result,
@@ -133,14 +133,14 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     EXPECT_EQ(expected_result, result.ok()) << "selector: " << selector;
     *pending_number_of_checks_output -= 1;
     if (*pending_number_of_checks_output == 0) {
-      done_callback.Run();
+      std::move(done_callback).Run();
     }
   }
 
-  void ClickElementCallback(const base::Closure& done_callback,
+  void ClickElementCallback(base::OnceClosure done_callback,
                             const ClientStatus& status) {
     EXPECT_EQ(ACTION_APPLIED, status.proto_status());
-    done_callback.Run();
+    std::move(done_callback).Run();
   }
 
   void ClickOrTapElement(const Selector& selector,
@@ -163,10 +163,10 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     run_loop.Run();
   }
 
-  void OnWaitForElementRemove(const base::Closure& done_callback,
+  void OnWaitForElementRemove(base::OnceClosure done_callback,
                               const Selector& selector,
                               const ClientStatus& result) {
-    done_callback.Run();
+    std::move(done_callback).Run();
     if (result.ok()) {
       WaitForElementRemove(selector);
     }
@@ -200,14 +200,14 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     return result;
   }
 
-  void OnClientStatus(base::Closure done_callback,
+  void OnClientStatus(base::OnceClosure done_callback,
                       ClientStatus* result_output,
                       const ClientStatus& status) {
     *result_output = status;
     std::move(done_callback).Run();
   }
 
-  void OnClientStatusAndReadyState(base::Closure done_callback,
+  void OnClientStatusAndReadyState(base::OnceClosure done_callback,
                                    ClientStatus* result_output,
                                    DocumentReadyState* ready_state_out,
                                    const ClientStatus& status,
@@ -240,7 +240,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     return result;
   }
 
-  void OnGetOuterHtml(const base::Closure& done_callback,
+  void OnGetOuterHtml(base::OnceClosure done_callback,
                       ClientStatus* successful_output,
                       std::string* html_output,
                       const ClientStatus& status,
@@ -248,7 +248,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     EXPECT_EQ(ACTION_APPLIED, status.proto_status());
     *successful_output = status;
     *html_output = html;
-    done_callback.Run();
+    std::move(done_callback).Run();
   }
 
   ClientStatus GetElementTag(const Selector& selector,
@@ -263,7 +263,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     return result;
   }
 
-  void OnGetElementTag(const base::Closure& done_callback,
+  void OnGetElementTag(base::OnceClosure done_callback,
                        ClientStatus* successful_output,
                        std::string* element_tag_output,
                        const ClientStatus& status,
@@ -271,7 +271,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     EXPECT_EQ(ACTION_APPLIED, status.proto_status());
     *successful_output = status;
     *element_tag_output = element_tag;
-    done_callback.Run();
+    std::move(done_callback).Run();
   }
 
   void FindElement(const Selector& selector,
@@ -287,13 +287,13 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     run_loop.Run();
   }
 
-  void OnFindElement(const base::Closure& done_callback,
+  void OnFindElement(base::OnceClosure done_callback,
                      ClientStatus* status_out,
                      ElementFinder::Result* result_out,
                      const ClientStatus& status,
                      std::unique_ptr<ElementFinder::Result> result) {
     ASSERT_TRUE(result);
-    done_callback.Run();
+    std::move(done_callback).Run();
     if (status_out)
       *status_out = status;
 
@@ -350,7 +350,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     run_loop.Run();
   }
 
-  void OnGetFieldValue(const base::Closure& done_callback,
+  void OnGetFieldValue(base::OnceClosure done_callback,
                        size_t* pending_number_of_checks_output,
                        const std::string& expected_value,
                        const ClientStatus& status,
@@ -378,7 +378,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     return result;
   }
 
-  void OnSetFieldValue(const base::Closure& done_callback,
+  void OnSetFieldValue(base::OnceClosure done_callback,
                        ClientStatus* result_output,
                        const ClientStatus& status) {
     *result_output = status;
@@ -404,7 +404,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     return SendKeyboardInput(selector, codepoints, -1);
   }
 
-  void OnSendKeyboardInput(const base::Closure& done_callback,
+  void OnSendKeyboardInput(base::OnceClosure done_callback,
                            ClientStatus* result_output,
                            const ClientStatus& status) {
     *result_output = status;
@@ -425,7 +425,7 @@ class WebControllerBrowserTest : public content::ContentBrowserTest,
     return result;
   }
 
-  void OnSetAttribute(const base::Closure& done_callback,
+  void OnSetAttribute(base::OnceClosure done_callback,
                       ClientStatus* result_output,
                       const ClientStatus& status) {
     *result_output = status;
