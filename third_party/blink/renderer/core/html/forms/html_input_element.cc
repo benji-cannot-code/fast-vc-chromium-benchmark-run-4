33b92d9e7c7ee45d8763f0ff0a74e3161d21caf3
Joey Arhar <jarhar@chromium.org>
2020-02-13 23:14:14 +0000

    Revert "Remove inDocument check for emitting change events"
    This reverts commit 07d811aaeb0724d0d019b8d6bdba3d5de1dc9a5e.
    
    The change this reverts fixed http://crbug.com/773680 which was intended
    to fire change events for detached radio and checkbox input elements
    when using input.dispatchEvent(new MouseEvent('click')), but also caused
    input.click() to fire change events. Although these may seem to be the
    same thing, firefox actually fires change events on input.dispatchEvent
    but not input.click(). WebKit, and chrome before the patch this reverts
    fire change events in neither of these cases.
    
    I will file an html spec issue to address this compat issue. For now, it
    seems important to stop firing these change events because it is getting
    more attention than the lack of change events.
    
    Bug: 773680, 1051482
    Change-Id: Ia8217d34bf9a20c50ce008630e748a7dbda52c3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055590
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741251}

--- third_party/blink/renderer/core/html/forms/html_input_element.cc
+++ third_party/blink/renderer/core/html/forms/html_input_element.cc
@@ -978,12 +978,13 @@ bool HTMLInputElement::HasBeenPasswordField() const {
 }
 
 void HTMLInputElement::DispatchChangeEventIfNeeded() {
-  if (input_type_->ShouldSendChangeEventAfterCheckedChanged())
+  if (isConnected() && input_type_->ShouldSendChangeEventAfterCheckedChanged())
     DispatchChangeEvent();
 }
 
 void HTMLInputElement::DispatchInputAndChangeEventIfNeeded() {
-  if (input_type_->ShouldSendChangeEventAfterCheckedChanged()) {
+  if (isConnected() &&
+      input_type_->ShouldSendChangeEventAfterCheckedChanged()) {
     DispatchInputEvent();
     DispatchChangeEvent();
   }
