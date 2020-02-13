5023c01d644b88bd1988721e2f66a064897e5a5e
Nick Diego Yamane <nickdiego@igalia.com>
2020-02-13 08:24:57 +0000

    x11, ozone: Ensure bogus timestamps are corrected when building events
    After PlatformEvent migration CL has landed (crrev.com/c/2022280),
    building ui::Events out of XEvents has been moved exclusively to
    x11_event_translation.{h,cc} helper code. With this change the bogus
    timestamp correction has been mistakenly lost. This CL restores it as
    well as adds a unit test to prevent future similar regressions.
    
    R=sadrul@chromium.org
    
    Bug: 965991
    Test: events_unittests --gtest_filter=XEventTranslationTest.BogusTimestampCorrection
    Change-Id: I238d7dba7586be0dd7cf70475c0705b2303ca225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2040563
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740988}

--- ui/events/x/x11_event_translation.cc
+++ ui/events/x/x11_event_translation.cc
@@ -72,6 +72,12 @@ Event::Properties GetEventPropertiesFromXEvent(EventType type,
   return properties;
 }
 
+base::TimeTicks GetXEventTimestamp(const XEvent& xev) {
+  base::TimeTicks timestamp = EventTimeFromXEvent(xev);
+  ValidateEventTimeClock(&timestamp);
+  return timestamp;
+}
+
 std::unique_ptr<KeyEvent> CreateKeyEvent(EventType event_type,
                                          const XEvent& xev) {
   KeyboardCode key_code = KeyboardCodeFromXKeyEvent(&xev);
@@ -81,14 +87,12 @@ std::unique_ptr<KeyEvent> CreateKeyEvent(EventType event_type,
   // in KeyEvent::ApplyLayout() which makes it possible for CrOS/Linux, for
   // example, to support host system keyboard layouts.
 #if defined(USE_OZONE)
-  auto event = std::make_unique<KeyEvent>(event_type, key_code, event_flags);
+  auto event = std::make_unique<KeyEvent>(event_type, key_code, event_flags,
+                                          GetXEventTimestamp(xev));
 #else
-  DomCode dom_code = CodeFromXEvent(&xev);
-  DomKey dom_key = GetDomKeyFromXEvent(&xev);
-  base::TimeTicks timestamp = EventTimeFromXEvent(xev);
-  ValidateEventTimeClock(&timestamp);
-  auto event = std::make_unique<KeyEvent>(event_type, key_code, dom_code,
-                                          event_flags, dom_key, timestamp);
+  auto event = std::make_unique<KeyEvent>(
+      event_type, key_code, CodeFromXEvent(&xev), event_flags,
+      GetDomKeyFromXEvent(&xev), GetXEventTimestamp(xev));
 #endif
 
   DCHECK(event);
@@ -119,7 +123,7 @@ std::unique_ptr<MouseEvent> CreateMouseEvent(EventType type,
   PointerDetails details{EventPointerType::POINTER_TYPE_MOUSE};
   auto event = std::make_unique<MouseEvent>(
       type, EventLocationFromXEvent(xev), EventSystemLocationFromXEvent(xev),
-      EventTimeFromXEvent(xev), EventFlagsFromXEvent(xev), button_flags,
+      GetXEventTimestamp(xev), EventFlagsFromXEvent(xev), button_flags,
       details);
 
   DCHECK(event);
@@ -135,7 +139,7 @@ std::unique_ptr<MouseWheelEvent> CreateMouseWheelEvent(const XEvent& xev) {
                          : 0;
   auto event = std::make_unique<MouseWheelEvent>(
       GetMouseWheelOffsetFromXEvent(xev), EventLocationFromXEvent(xev),
-      EventSystemLocationFromXEvent(xev), EventTimeFromXEvent(xev),
+      EventSystemLocationFromXEvent(xev), GetXEventTimestamp(xev),
       EventFlagsFromXEvent(xev), button_flags);
 
   DCHECK(event);
@@ -146,7 +150,7 @@ std::unique_ptr<MouseWheelEvent> CreateMouseWheelEvent(const XEvent& xev) {
 std::unique_ptr<TouchEvent> CreateTouchEvent(EventType type,
                                              const XEvent& xev) {
   auto event = std::make_unique<TouchEventX11>(
-      type, EventLocationFromXEvent(xev), EventTimeFromXEvent(xev),
+      type, EventLocationFromXEvent(xev), GetXEventTimestamp(xev),
       GetTouchPointerDetailsFromXEvent(xev));
 #if defined(USE_OZONE)
   // Touch events don't usually have |root_location| set differently than
@@ -170,7 +174,7 @@ std::unique_ptr<ScrollEvent> CreateScrollEvent(EventType type,
                            &y_offset_ordinal, nullptr);
   }
   auto event = std::make_unique<ScrollEvent>(
-      type, EventLocationFromXEvent(xev), EventTimeFromXEvent(xev),
+      type, EventLocationFromXEvent(xev), GetXEventTimestamp(xev),
       EventFlagsFromXEvent(xev), x_offset, y_offset, x_offset_ordinal,
       y_offset_ordinal, finger_count);
 
