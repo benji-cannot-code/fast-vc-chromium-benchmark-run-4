9c269bc5db92ec33462fd5abe1f3213303f8c194
Dong Hwi Lee <leedh@google.com>
2020-02-13 16:31:54 +0000

    Use String.prototype.trim() instead of Regex to trim whitespaces in DocumentXMLTreeViewer.js.
    Change-Id: I58b793e966a226895bf6ef917388a7bcab912be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053165
    Commit-Queue: Dong-Hwi Lee <leedh@google.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741082}

--- third_party/blink/renderer/core/xml/DocumentXMLTreeViewer.js
+++ third_party/blink/renderer/core/xml/DocumentXMLTreeViewer.js
@@ -200,14 +200,9 @@ function processText(parentElement, node)
 
 // Processing utils.
 
-function trim(value)
-{
-    return value.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-}
-
 function isShort(value)
 {
-    return trim(value).length <= 50;
+    return value.trim().length <= 50;
 }
 
 // Tree rendering.
@@ -380,7 +375,6 @@ function initButtons()
         expandButton.onclick = expandFunction(sectionId);
         expandButton.onmousedown = handleButtonMouseDown;
     }
-
 }
 
 function handleButtonMouseDown(e)
