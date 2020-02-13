d991a500e742b0a5018f5e4129bc7df871f1b94d
Katie D <katie@chromium.org>
2020-02-13 21:52:40 +0000

    [Sheriff] Disable flaky PrerenderBrowserTest.PrerenderCancelMainFrameRedirectUnsupportedScheme
    This test appears to be flaking frequently:
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyaAsSBUZsYWtlIl1jaHJvbWl1bUBicm93c2VyX3Rlc3RzQFByZXJlbmRlckJyb3dzZXJUZXN0LlByZXJlbmRlckNhbmNlbE1haW5GcmFtZVJlZGlyZWN0VW5zdXBwb3J0ZWRTY2hlbWUM
    
    TBR=tbansal@chromium.org,darin@chromium.org
    
    Bug: 1050143
    Change-Id: I6fec8b6c4f1262c6884ad05be9be28e69775f805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055670
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741215}

--- chrome/browser/prerender/prerender_browsertest.cc
+++ chrome/browser/prerender/prerender_browsertest.cc
@@ -1757,8 +1757,10 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
 }
 
 // Checks that non-http/https main page redirects cancel the prerender.
-IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
-                       PrerenderCancelMainFrameRedirectUnsupportedScheme) {
+// Disabled for flakes: crbug.com/1050143.
+IN_PROC_BROWSER_TEST_F(
+    PrerenderBrowserTest,
+    DISABLED_PrerenderCancelMainFrameRedirectUnsupportedScheme) {
   // Disable load event checks because they race with cancellation.
   DisableLoadEventCheck();
   GURL url = embedded_test_server()->GetURL(
