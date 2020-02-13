5c409604645f8972d1b043e2c770c2275fff63a0
David Tseng <dtseng@chromium.org>
2020-02-13 21:36:45 +0000

    Add a user selection for number reading style
    Also:
    - fixes the handling of menuItemSelected to only be respected from a focused node (Chrome now fires it more noisely)
    - add a full test suite for options page testing which drives the UI end to end and verifies local ChromeVox state
    (this motivated the above fix)
    - noticed a regression in the way <select> nodes work (TODO'ed and not handled fixing in this change)
    - adds a unit test for verifying number style reading behavior
    
    Test: see above
    Change-Id: I4dce435c6de54399128c1a21f805671b54ac3904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2003895
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741209}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/prefs.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/prefs.js
@@ -203,9 +203,6 @@ ChromeVoxPrefs.DEFAULT_PREFS = {
   'brailleTableType': 'brailleTable8',
   'brailleTable6': 'en-UEB-g2',
   'brailleTable8': 'en-US-comp8',
-  // TODO(dtseng): Leaking state about multiple key maps here until we have a
-  // class to manage multiple key maps. Also, this doesn't belong as a pref;
-  // should just store in local storage.
   'capitalStrategy': 'increasePitch',
   'currentKeyMap': KeyMap.DEFAULT_KEYMAP,
   'cvoxKey': '',
@@ -217,6 +214,7 @@ ChromeVoxPrefs.DEFAULT_PREFS = {
   'focusFollowsMouse': false,
   'granularity': undefined,
   'languageSwitching': false,
+  'numberReadingStyle': 'asWords',
   'position': '{}',
   'siteSpecificEnhancements': true,
   'siteSpecificScriptBase':
