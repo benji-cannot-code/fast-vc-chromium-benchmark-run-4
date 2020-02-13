efb0faca75c21b34af5292cd46f7fae7bcf1ec27
Nazih Almalki <nalmalki@google.com>
2020-02-13 15:33:42 +0000

    Remove Scheduling.Renderer.BeginMainFrameIntervalNotCritical2
    after M81 this metric is no longer valid
    
    Bug: 1037240
    Change-Id: I608f2c22f04891f5c70da82488f897ffe1608da7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2048307
    Commit-Queue: Nazih Almalki <nalmalki@google.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Behdad Bakhshinategh <behdadb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741063}

--- cc/metrics/compositor_timing_history.cc
+++ cc/metrics/compositor_timing_history.cc
@@ -24,8 +24,6 @@ class CompositorTimingHistory::UMAReporter {
 
   // Throughput measurements
   virtual void AddBeginMainFrameIntervalCritical(base::TimeDelta interval) = 0;
-  virtual void AddBeginMainFrameIntervalNotCritical(
-      base::TimeDelta interval) = 0;
   virtual void AddCommitInterval(base::TimeDelta interval) = 0;
   virtual void AddDrawInterval(base::TimeDelta interval) = 0;
 
@@ -302,11 +300,6 @@ class RendererUMAReporter : public CompositorTimingHistory::UMAReporter {
         "Scheduling.Renderer.BeginMainFrameIntervalCritical", interval);
   }
 
-  void AddBeginMainFrameIntervalNotCritical(base::TimeDelta interval) override {
-    UMA_HISTOGRAM_CUSTOM_TIMES_VSYNC_ALIGNED(
-        "Scheduling.Renderer.BeginMainFrameIntervalNotCritical", interval);
-  }
-
   void AddCommitInterval(base::TimeDelta interval) override {
     UMA_HISTOGRAM_CUSTOM_TIMES_VSYNC_ALIGNED(
         "Scheduling.Renderer.CommitInterval", interval);
@@ -410,9 +403,6 @@ class BrowserUMAReporter : public CompositorTimingHistory::UMAReporter {
   // side because browser rendering fps is not at 60.
   void AddBeginMainFrameIntervalCritical(base::TimeDelta interval) override {}
 
-  void AddBeginMainFrameIntervalNotCritical(base::TimeDelta interval) override {
-  }
-
   // CommitInterval is not meaningful to measure on browser side because
   // browser rendering fps is not at 60.
   void AddCommitInterval(base::TimeDelta interval) override {}
@@ -488,8 +478,6 @@ class NullUMAReporter : public CompositorTimingHistory::UMAReporter {
  public:
   ~NullUMAReporter() override = default;
   void AddBeginMainFrameIntervalCritical(base::TimeDelta interval) override {}
-  void AddBeginMainFrameIntervalNotCritical(base::TimeDelta interval) override {
-  }
   void AddCommitInterval(base::TimeDelta interval) override {}
   void AddDrawInterval(base::TimeDelta interval) override {}
   void AddDrawIntervalWithCompositedAnimations(
@@ -823,8 +811,6 @@ void CompositorTimingHistory::DidBeginMainFrame(
           begin_main_frame_end_time - begin_main_frame_end_time_prev_;
       if (begin_main_frame_on_critical_path_)
         uma_reporter_->AddBeginMainFrameIntervalCritical(commit_interval);
-      else
-        uma_reporter_->AddBeginMainFrameIntervalNotCritical(commit_interval);
     }
     begin_main_frame_end_time_prev_ = begin_main_frame_end_time;
   }
