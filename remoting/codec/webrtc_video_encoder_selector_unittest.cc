f83e9f6b7a61bfa79f79f51cfddf12f2b37715c4
Joey Scarr <jsca@google.com>
2020-02-13 03:15:04 +0000

    Convert remoting/codec to Once/RepeatingCallback.
    Fixed: 1007822
    Change-Id: I9d102a066893f5917b14a63952de1092230c4182
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050049
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joey Scarr <jsca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740943}

--- remoting/codec/webrtc_video_encoder_selector_unittest.cc
+++ remoting/codec/webrtc_video_encoder_selector_unittest.cc
@@ -38,10 +38,11 @@ class WebrtcVideoEncoderSelectorTest : public ::testing::Test {
     // Register three encoders.
     for (int i = 0; i < kEncoderCount; i++) {
       int j = selector_.RegisterEncoder(
-          base::Bind(&WebrtcVideoEncoderSelectorTest::IsProfileSupported,
-                     base::Unretained(this), i),
-          base::Bind(&WebrtcVideoEncoderSelectorTest::CreateEncoder,
-                     base::Unretained(this), i));
+          base::BindRepeating(
+              &WebrtcVideoEncoderSelectorTest::IsProfileSupported,
+              base::Unretained(this), i),
+          base::BindRepeating(&WebrtcVideoEncoderSelectorTest::CreateEncoder,
+                              base::Unretained(this), i));
       EXPECT_EQ(i, j);
     }
     // Set the default DesktopFrame.
