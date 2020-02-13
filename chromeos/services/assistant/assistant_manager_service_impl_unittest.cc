6353e214624edc2a1cf94fa780cd2ceb2b1099e5
Kristi Park <kristipark@chromium.org>
2020-02-13 00:10:40 +0000

    Revert "Add a new test case for |AssistantManagerServiceImpl|."
    This reverts commit 64cf44e210d5d658353243c349e1b3fbabbf8812.
    
    Reason for revert: Tentative suspect for
    DriveIntegrationServiceWithGaiaDisabledBrowserTest.DriveDisabled
    linux-chromeos-chrome browser_test failure
    https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/3090
    
    Original change's description:
    > Add a new test case for |AssistantManagerServiceImpl|.
    > 
    > This CL adds a new test case to ensure we update the supported action
    > list stored in |AssistantManagerServiceImpl| when ambient mode state has
    > changed.
    > 
    > Bug: b/149237062
    > Test: run the new unit test added in this change.
    > 
    > Change-Id: Idc3c538e023cb59e76fb6c71219e4846da6d9c30
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1969777
    > Commit-Queue: Meilin Wang <meilinw@chromium.org>
    > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#740817}
    
    TBR=xiaohuic@chromium.org,meilinw@chromium.org,jeroendh@google.com
    
    Change-Id: I596dc72fa1422ee85925f2616557dfbb9b705bad
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/149237062, 1051728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053283
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740893}

--- chromeos/services/assistant/assistant_manager_service_impl_unittest.cc
+++ chromeos/services/assistant/assistant_manager_service_impl_unittest.cc
@@ -222,10 +222,6 @@ class AssistantManagerServiceImplTest : public testing::Test {
     return delegate_->assistant_manager_internal();
   }
 
-  action::CrosActionModule* action_module() {
-    return assistant_manager_service_->action_module_for_testing();
-  }
-
   void Start() {
     assistant_manager_service()->Start("dummy-access-token",
                                        /*enable_hotword=*/false);
@@ -545,17 +541,6 @@ TEST_F(AssistantManagerServiceImplTest,
   EXPECT_CALL(observer, OnStateChanged).Times(0);
 
   Start();
-}
-
-TEST_F(AssistantManagerServiceImplTest,
-       ShouldUpdateActionModuleWhenAmbientModeStateChanged) {
-  EXPECT_FALSE(action_module()->IsAmbientModeEnabledForTesting());
-
-  assistant_manager_service()->EnableAmbientMode(true);
-  EXPECT_TRUE(action_module()->IsAmbientModeEnabledForTesting());
-
-  assistant_manager_service()->EnableAmbientMode(false);
-  EXPECT_FALSE(action_module()->IsAmbientModeEnabledForTesting());
 }
 
 }  // namespace assistant
