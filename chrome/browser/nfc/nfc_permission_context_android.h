3037c4fa56a2bfca78b3b6a1d80e3a37fdb203a8
Natalie Chouinard <chouinard@chromium.org>
2020-02-13 20:16:25 +0000

    [Android] Move NfcSystemLevelSetting and modularize NFC code
    NfcSystemLevelSetting is not a shared class used by multiple Settings
    screens, so move it out of the  org.chromium.chrome.browser.settings
    package to org.chromium.chrome.browser.nfc instead.
    
    Since the classes in org.chromium.chrome.browser.nfc are fairly trivial
    to modularize, organize and build to a separate target in
    //chrome/browser/nfc as part of go/chrome-hoa efforts.
    
    Bug: 1052056
    Change-Id: Ia402340b05596ad880f16aef9521003658c1e2a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032785
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741173}

--- chrome/browser/nfc/nfc_permission_context_android.h
+++ chrome/browser/nfc/nfc_permission_context_android.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NFC_NFC_PERMISSION_CONTEXT_ANDROID_H_
 #define CHROME_BROWSER_NFC_NFC_PERMISSION_CONTEXT_ANDROID_H_
 
-#include "chrome/browser/android/nfc_system_level_setting.h"
+#include "chrome/browser/nfc/android/nfc_system_level_setting.h"
 #include "chrome/browser/nfc/nfc_permission_context.h"
 
 namespace permissions {
