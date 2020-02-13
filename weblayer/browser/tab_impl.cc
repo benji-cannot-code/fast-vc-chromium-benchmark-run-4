56dd7e314f06050d1440620275497393cf519ba9
Evan Stade <estade@chromium.org>
2020-02-13 20:02:35 +0000

    WebLayer: basic JS dialog support for Android.
    This hooks up app-modal JS dialogs without adding any new APIs.
    
    Most functionality is "free" i.e. already componentized, including
    - alerts, prompts, and confirms (such as onbeforeunload)
    - anti-spam suppressions ("prevent this webpage..." checkbox)
    
    We will want to convert all except onBeforeUnload confirmations to
    tab-modal, but we'll still need app-modal for onBeforeUnload, so this
    is a reasonable stepping stone.
    
    TBR=twellington@chromium.org
    
    Bug: 1025256
    Change-Id: I66575619dbfafcc0a575082c23f1e567568c29d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1979154
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741168}

--- weblayer/browser/tab_impl.cc
+++ weblayer/browser/tab_impl.cc
@@ -49,6 +49,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "components/autofill/android/autofill_provider_android.h"
 #include "components/embedder_support/android/delegate/color_chooser_android.h"
+#include "components/javascript_dialogs/android/app_modal_dialog_view_android.h"  // nogncheck
+#include "components/javascript_dialogs/app_modal_dialog_manager.h"  // nogncheck
+#include "ui/android/view_android.h"
 #include "weblayer/browser/controls_visibility_reason.h"
 #include "weblayer/browser/java/jni/TabImpl_jni.h"
 #include "weblayer/browser/top_controls_container_view.h"
@@ -294,6 +297,10 @@ void TabImpl::AttachToView(views::WebView* web_view) {
 }
 #endif
 
+bool TabImpl::IsActive() {
+  return browser_->GetActiveTab() == this;
+}
+
 #if defined(OS_ANDROID)
 // static
 void TabImpl::DisableAutofillSystemIntegrationForTesting() {
@@ -413,6 +420,32 @@ void TabImpl::DidNavigateMainFramePostCommit(
     content::WebContents* web_contents) {
   for (auto& observer : observers_)
     observer.DisplayedUrlChanged(web_contents->GetVisibleURL());
+}
+
+content::JavaScriptDialogManager* TabImpl::GetJavaScriptDialogManager(
+    content::WebContents* web_contents) {
+#if defined(OS_ANDROID)
+  // Simply ignore javascript dialog requests from non-foremost tabs.
+  // TODO(crbug.com/1025256): revisit this behavior.
+  if (!IsActive())
+    return nullptr;
+
+  auto* dialog_manager =
+      javascript_dialogs::AppModalDialogManager::GetInstance();
+  if (dialog_manager->view_factory()->is_null()) {
+    dialog_manager->SetNativeDialogFactory(base::BindRepeating(
+        [](javascript_dialogs::AppModalDialogController* controller)
+            -> javascript_dialogs::AppModalDialogView* {
+          return new javascript_dialogs::AppModalDialogViewAndroid(
+              base::android::AttachCurrentThread(), controller,
+              controller->web_contents()->GetTopLevelNativeWindow());
+        }));
+  }
+
+  return dialog_manager;
+#else
+  return nullptr;
+#endif
 }
 
 content::ColorChooser* TabImpl::OpenColorChooser(
