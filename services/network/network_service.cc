1a6d9bb66803c1bab3e0a214f1c0b5c8501efa3d
Matt Mueller <mattm@chromium.org>
2020-02-13 20:27:26 +0000

    net: proxy netlog messages from browser process to network service process over mojo
    Bug: 1040681
    Change-Id: Iefab63f598fecc3519065b0440815e2d65bb5070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036273
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741179}

--- services/network/network_service.cc
+++ services/network/network_service.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/http_auth_cache_copier.h"
 #include "services/network/legacy_tls_config_distributor.h"
 #include "services/network/net_log_exporter.h"
+#include "services/network/net_log_proxy_sink.h"
 #include "services/network/network_context.h"
 #include "services/network/network_usage_accumulator.h"
 #include "services/network/public/cpp/features.h"
@@ -456,6 +457,15 @@ void NetworkService::StartNetLog(base::File file,
   file_net_log_observer_ = net::FileNetLogObserver::CreateUnboundedPreExisting(
       std::move(file), std::move(constants));
   file_net_log_observer_->StartObserving(net_log_, capture_mode);
+}
+
+void NetworkService::AttachNetLogProxy(
+    mojo::PendingRemote<mojom::NetLogProxySource> proxy_source,
+    mojo::PendingReceiver<mojom::NetLogProxySink> proxy_sink) {
+  if (!net_log_proxy_sink_)
+    net_log_proxy_sink_ = std::make_unique<NetLogProxySink>();
+  net_log_proxy_sink_->AttachSource(std::move(proxy_source),
+                                    std::move(proxy_sink));
 }
 
 void NetworkService::SetSSLKeyLogFile(base::File file) {
