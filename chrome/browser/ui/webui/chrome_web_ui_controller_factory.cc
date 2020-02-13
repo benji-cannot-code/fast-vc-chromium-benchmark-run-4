307bb148f13a406c8fa50a5e6d9dd28880ac9f13
Tommy Steimel <steimel@chromium.org>
2020-02-13 21:28:32 +0000

    Kaleidoscope: Add WebUI logic directly to ChromeWebUIControllerFactory
    This CL adds logic into ChromeWebUIControllerFactory for handling
    Kaleidoscope URLs instead of using the
    KaleidoscopeWebUIControllerFactory.
    
    Bug: 1044741
    Change-Id: I798b39fd8040f43fcfc1d841fea0f82fcf7985f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2050032
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741205}

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -100,6 +100,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/web_dialogs/web_dialog_ui.h"
 #include "url/gurl.h"
 
+#if BUILDFLAG(ENABLE_KALEIDOSCOPE)
+#include "chrome/browser/media/kaleidoscope/internal/kaleidoscope_content_ui.h"
+#include "chrome/browser/media/kaleidoscope/internal/kaleidoscope_ui.h"
+#include "media/base/media_switches.h"
+#endif  // BUILDFLAG(ENABLE_KALEIDOSCOPE)
+
 #if BUILDFLAG(ENABLE_NACL)
 #include "chrome/browser/ui/webui/nacl_ui.h"
 #endif
@@ -670,6 +676,14 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<WelcomeUI>;
 #endif
 
+#if BUILDFLAG(ENABLE_KALEIDOSCOPE)
+  if (base::FeatureList::IsEnabled(media::kKaleidoscope)) {
+    if (url.host_piece() == chrome::kChromeUIKaleidoscopeHost)
+      return &NewWebUI<KaleidoscopeUI>;
+    if (url.host_piece() == chrome::kChromeUIKaleidoscopeContentHost)
+      return &NewWebUI<KaleidoscopeContentUI>;
+  }
+#endif  // BUILDFLAG(ENABLE_KALEIDOSCOPE)
 #if BUILDFLAG(ENABLE_NACL)
   if (url.host_piece() == chrome::kChromeUINaClHost)
     return &NewWebUI<NaClUI>;
