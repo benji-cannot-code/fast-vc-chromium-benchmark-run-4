85c97decf580b055c1bf0eb8b098330d708b063e
Mohit Hotwani <hmohit@chromium.org>
2020-02-13 21:19:26 +0000

    [chromecast] Enable better stack traces in case of FATAL error.
    This gives us offsets in the binary which enables us to run
    symbolizer to get the function name/source line.
    
    After this change stack looks like
    #0 0x00558c83b8ec (foo.bar+0x2428eb)
    #1 0x00558c7e9ac8 (foo.bar+0x1f0ac7)
    #2 0x00558c7ef9b8 (foo.bar+0x1f69b7)
    
    Bug: internal b/148287348
    Change-Id: I654be3afaecd56a06d0339cf0d6cfc4669812c01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053251
    Commit-Queue: Mohit Hotwani <hmohit@chromium.org>
    Reviewed-by: Luke Halliwell (slow) <halliwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741198}

--- chromecast/external_mojo/external_service_support/process_setup.cc
+++ chromecast/external_mojo/external_service_support/process_setup.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
+#include "base/debug/stack_trace.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
 #include "build/build_config.h"
@@ -40,6 +41,9 @@ void CommonProcessInitialization(int argc, char** argv) {
   base::FeatureList::InitializeInstance(
       command_line->GetSwitchValueASCII(switches::kEnableFeatures),
       command_line->GetSwitchValueASCII(switches::kDisableFeatures));
+#if !defined(OFFICIAL_BUILD)
+  base::debug::EnableInProcessStackDumping();
+#endif  // !defined(OFFICIAL_BUILD)
 
 #if !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
   CrashReporterClient::InitCrashReporter();
