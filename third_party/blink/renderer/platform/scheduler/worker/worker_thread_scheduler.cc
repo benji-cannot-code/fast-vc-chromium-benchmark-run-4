40b97f3386ad804856ee487a5f1bd166a98ebc65
Daniel Cheng <dcheng@chromium.org>
2020-02-13 08:49:37 +0000

    [scheduler] Remove unnecessary include of Blink's legacy histogram.h
    Blink allows use of the //base histogram helpers, so the goal is to
    delete the Blink-specific histogram helpers and standardize on //base.
    
    Bug: 1047547
    Change-Id: I6a796dde4437d58b67875edc9b535b761394b509
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053966
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740991}

--- third_party/blink/renderer/platform/scheduler/worker/worker_thread_scheduler.cc
+++ third_party/blink/renderer/platform/scheduler/worker/worker_thread_scheduler.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "third_party/blink/public/common/thread_safe_browser_interface_broker_proxy.h"
 #include "third_party/blink/public/platform/platform.h"
-#include "third_party/blink/renderer/platform/instrumentation/histogram.h"
 #include "third_party/blink/renderer/platform/scheduler/common/features.h"
 #include "third_party/blink/renderer/platform/scheduler/common/process_state.h"
 #include "third_party/blink/renderer/platform/scheduler/common/throttling/task_queue_throttler.h"
