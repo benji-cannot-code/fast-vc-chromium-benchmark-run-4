c81c097751e26e53c3d51447a12ddc67a14d6494
Roland McGrath <mcgrathr@chromium.org>
2020-02-14 20:16:23 +0000

    [Fuchsia] Don't include internal syscalls header
    The <zircon/syscalls/definitions.h> header is an implementation detail
    of the OS that was never meant to be used by application code.
    <zircon/syscalls.h> alone gives access to the Zircon syscall API.
    
    Bug: fuchsia:45844
    Change-Id: I0a1594e36c5eb6a05ff2266461970e49958c34bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055591
    Auto-Submit: Roland McGrath <mcgrathr@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741543}

--- base/debug/stack_trace_fuchsia.cc
+++ base/debug/stack_trace_fuchsia.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unwind.h>
 #include <zircon/process.h>
 #include <zircon/syscalls.h>
-#include <zircon/syscalls/definitions.h>
 #include <zircon/syscalls/port.h>
 #include <zircon/types.h>
 
