7994358cdb9c2e88b1d67a46160987f01016f924
Sammie Quon <sammiequon@chromium.org>
2020-02-14 03:25:17 +0000

    system: Remove remaining ink drop masks.
    Test: manual
    Bug: 1042303
    Change-Id: I9805281283a7293f6accbc8bfe31444b1cf755cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055890
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741333}

--- ash/system/accessibility/autoclick_menu_view.cc
+++ ash/system/accessibility/autoclick_menu_view.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/views/accessibility/view_accessibility.h"
-#include "ui/views/animation/ink_drop_mask.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/separator.h"
@@ -59,13 +58,6 @@ class AutoclickMenuButton : public TopShortcutButton {
 
   // views::Button:
   const char* GetClassName() const override { return "AutoclickMenuButton"; }
-
-  // views::ImageButton:
-  std::unique_ptr<views::InkDropMask> CreateInkDropMask() const override {
-    gfx::Rect bounds = GetContentsBounds();
-    return std::make_unique<views::CircleInkDropMask>(
-        size(), bounds.CenterPoint(), bounds.width() / 2);
-  }
 
   // Set the vector icon shown in a circle.
   void SetVectorIcon(const gfx::VectorIcon& icon) {
--- ash/system/accessibility/autoclick_scroll_view.cc
+++ ash/system/accessibility/autoclick_scroll_view.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/vector_icon_types.h"
+#include "ui/views/controls/highlight_path_generator.h"
 #include "ui/views/masked_targeter_delegate.h"
 #include "ui/views/view.h"
 
@@ -150,6 +151,8 @@ class AutoclickScrollButton : public CustomShapeButton,
 
     SetClipPath(CreateCustomShapePath(gfx::Rect(GetPreferredSize())));
     SetEventTargeter(std::make_unique<views::ViewTargeter>(this));
+
+    views::InstallRoundRectHighlightPathGenerator(this, gfx::Insets(), 0.f);
   }
 
   ~AutoclickScrollButton() override {
