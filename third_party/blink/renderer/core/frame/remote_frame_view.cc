04cee1b02b6407914d604e61f900932e57c888fa
Julie Jeongeun Kim <jkim@igalia.com>
2020-02-14 11:17:08 +0000

    Convert FrameHostMsg_UpdateRenderThrottlingStatus to mojo
    This CL converts FrameHostMsg_UpdateRenderThrottlingStatus to
    RemoteFrameHost blink interface. It removes
    UpdateRenderThrottlingStatus from WebRemoteFrameClient.
    
    Bug: 1047362
    Change-Id: Icc479058169016353dc58039aa4f311c5d1534da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053872
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#741408}

--- third_party/blink/renderer/core/frame/remote_frame_view.cc
+++ third_party/blink/renderer/core/frame/remote_frame_view.cc
@@ -262,10 +262,8 @@ void RemoteFrameView::ParentVisibleChanged() {
 
 void RemoteFrameView::VisibilityForThrottlingChanged() {
   TRACE_EVENT0("blink", "RemoteFrameView::VisibilityForThrottlingChanged");
-  if (!remote_frame_->Client())
-    return;
-  remote_frame_->Client()->UpdateRenderThrottlingStatus(IsHiddenForThrottling(),
-                                                        IsSubtreeThrottled());
+  remote_frame_->GetRemoteFrameHostRemote().UpdateRenderThrottlingStatus(
+      IsHiddenForThrottling(), IsSubtreeThrottled());
 }
 
 void RemoteFrameView::VisibilityChanged(
