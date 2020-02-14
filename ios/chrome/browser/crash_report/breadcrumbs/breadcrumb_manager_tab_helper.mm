a39cc08eb0c0476c673f7ae87958750a5f9f9e98
Eugene But <eugenebut@chromium.org>
2020-02-14 21:21:47 +0000

    Remove BreadcrumbManagerTabHelper::DidChangeBackForwardState
    This callback is called when back/forward buttons visibility state is
    changed. This does not provide any useful signals for finding steps to
    repro.
    
    Bug: 1046223
    Change-Id: Ia93bd11ee1d444de2b9525d5f4781b524184b60e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057117
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741582}

--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.mm
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.mm
@@ -139,11 +139,6 @@ void BreadcrumbManagerTabHelper::PageLoaded(
   LogEvent(base::JoinString(event, " "));
 }
 
-void BreadcrumbManagerTabHelper::DidChangeBackForwardState(
-    web::WebState* web_state) {
-  LogEvent("DidChangeBackForwardState");
-}
-
 void BreadcrumbManagerTabHelper::DidChangeVisibleSecurityState(
     web::WebState* web_state) {
   LogEvent("DidChangeVisibleSecurityState");
