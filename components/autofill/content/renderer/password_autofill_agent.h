63f8517ba5b18bac9c928cea3365a6a5c5637574
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-02-14 18:25:04 +0000

    [Passwords] Don't Trigger Touch To Fill on Sign-Up Forms
    This change modifies PasswordAutofillAgent::TryToShowTouchToFill to not
    trigger Touch To Till in case only fallback data is available. This
    prevents TTF from triggering on sign-up forms, when the user already has
    a credential stored.
    
    Bug: 1050946
    Change-Id: Ie25c6f86335601ba37c705e3c6a9d5c6b0fb418f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057067
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741499}

--- components/autofill/content/renderer/password_autofill_agent.h
+++ components/autofill/content/renderer/password_autofill_agent.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/util/type_safety/strong_alias.h"
 #include "build/build_config.h"
 #include "components/autofill/content/common/mojom/autofill_agent.mojom.h"
 #include "components/autofill/content/common/mojom/autofill_driver.mojom.h"
@@ -111,6 +112,7 @@ class PasswordAutofillAgent : public content::RenderFrameObserver,
                               public FormTracker::Observer,
                               public mojom::PasswordAutofillAgent {
  public:
+  using UseFallbackData = util::StrongAlias<class UseFallbackDataTag, bool>;
   PasswordAutofillAgent(content::RenderFrame* render_frame,
                         blink::AssociatedInterfaceRegistry* registry);
   ~PasswordAutofillAgent() override;
@@ -359,8 +361,11 @@ class PasswordAutofillAgent : public content::RenderFrameObserver,
   // corresponding username, password elements and PasswordInfo into
   // |username_element|, |password_element| and |pasword_info|, respectively.
   // Note, that |username_element->IsNull()| can be true if |element| is a
-  // password.
+  // password. Callers have the chance to restrict the usage of fallback data
+  // by setting |use_fallback_data| to false. In that case data provided via
+  // MaybeStoreFallbackData will be ignored and the function returns early.
   bool FindPasswordInfoForElement(const blink::WebInputElement& element,
+                                  UseFallbackData use_fallback_data,
                                   blink::WebInputElement* username_element,
                                   blink::WebInputElement* password_element,
                                   PasswordInfo** password_info);
