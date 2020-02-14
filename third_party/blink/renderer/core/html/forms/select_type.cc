5f68e68392fbe9775de8ee8f03db71d4365cd794
Kent Tamura <tkent@chromium.org>
2020-02-14 16:50:43 +0000

    Make optimization of blink::SelectType easier
    * Add |final| to SelectType subclasses so that compilers can omit vtbl
      lookup code
    
    * Add |const| to SelectType::select_ to avoid getting its value from
      memory multiple times in a single SelectType method
    
    This CL has no behavior changes.
    
    Bug: 1052232
    Change-Id: I6c941defd9c8a4f428abd2a62e673f026daddb2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057223
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741465}

--- third_party/blink/renderer/core/html/forms/select_type.cc
+++ third_party/blink/renderer/core/html/forms/select_type.cc
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class MenuListSelectType : public SelectType {
+class MenuListSelectType final : public SelectType {
  public:
   explicit MenuListSelectType(HTMLSelectElement& select) : SelectType(select) {}
 
@@ -78,7 +78,7 @@ void MenuListSelectType::DidSelectOption(
 
 // ============================================================================
 
-class ListBoxSelectType : public SelectType {
+class ListBoxSelectType final : public SelectType {
  public:
   explicit ListBoxSelectType(HTMLSelectElement& select) : SelectType(select) {}
 };
