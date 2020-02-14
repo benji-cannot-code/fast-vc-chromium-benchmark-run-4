98cb7ed91af9e928150205cca6e3ad3054123097
Carlos IL <carlosil@chromium.org>
2020-02-14 01:16:50 +0000

    Set mixed autoupgrades to default enabled
    Setting this to default on in preparation for launch, also change
    default behavior to exclude images since that is the mode launching in
    80.
    
    Bug: 1025274
    Change-Id: I6689b4fa90a345e4e6ecc193790757a11e54c6aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2052250
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741289}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -66,7 +66,7 @@ const base::Feature kJSONModules{"JSONModules",
 const base::Feature kLayoutNG{"LayoutNG", base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kMixedContentAutoupgrade{"AutoupgradeMixedContent",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Used to control the collection of anchor element metrics (crbug.com/856683).
 // If kNavigationPredictor is enabled, then metrics of anchor elements
@@ -248,7 +248,7 @@ const base::Feature kPrefetchPrivacyChanges{"PrefetchPrivacyChanges",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
 const char kMixedContentAutoupgradeModeParamName[] = "mode";
-const char kMixedContentAutoupgradeModeNoImages[] = "no-images";
+const char kMixedContentAutoupgradeModeAllPassive[] = "all-passive";
 
 // Decodes jpeg 4:2:0 formatted images to YUV instead of RGBX and stores in this
 // format in the image decode cache. See crbug.com/919627 for details on the
