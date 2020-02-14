9ced96faa6a3c57335583a357d52c56f506fcc57
Elly Fong-Jones <ellyjones@chromium.org>
2020-02-14 20:19:26 +0000

    core: re-enable service process tests
    Two of these were disabled as flaky several years ago.
    They have not flaked on my workstation in a few dozen runs,
    so let's speculatively re-enable them.
    
    Bug: None
    Change-Id: Iea9a6953d71c5935a4b36f99d75bf81ae83eb464
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057286
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741546}

--- chrome/common/service_process_util_unittest.cc
+++ chrome/common/service_process_util_unittest.cc
@@ -43,18 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/auto_start_linux.h"
 #endif
 
-#if defined(USE_AURA)
-// This test fails http://crbug.com/84854, and is very flaky on CrOS and
-// somewhat flaky on other Linux.
-#define MAYBE_ForceShutdown DISABLED_ForceShutdown
-#else
-#if defined(OS_LINUX) || defined(OS_WIN)
-#define MAYBE_ForceShutdown DISABLED_ForceShutdown
-#else
-#define MAYBE_ForceShutdown ForceShutdown
-#endif
-#endif
-
 namespace {
 
 bool g_good_shutdown = false;
@@ -119,8 +107,7 @@ TEST_F(ServiceProcessStateTest, Singleton) {
   LaunchAndWait("ServiceProcessStateTestSingleton");
 }
 
-// http://crbug.com/396390
-TEST_F(ServiceProcessStateTest, DISABLED_ReadyState) {
+TEST_F(ServiceProcessStateTest, ReadyState) {
   ASSERT_FALSE(CheckServiceProcessReady());
   ServiceProcessState state;
   ASSERT_TRUE(state.Initialize());
@@ -198,7 +185,7 @@ TEST_F(ServiceProcessStateTest, SharedMem) {
   ASSERT_EQ(base::GetCurrentProcId(), pid);
 }
 
-TEST_F(ServiceProcessStateTest, MAYBE_ForceShutdown) {
+TEST_F(ServiceProcessStateTest, ForceShutdown) {
   base::Process process = SpawnChild("ServiceProcessStateTestShutdown");
   ASSERT_TRUE(process.IsValid());
   for (int i = 0; !CheckServiceProcessReady() && i < 10; ++i) {
