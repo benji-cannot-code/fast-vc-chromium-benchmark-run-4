bddc1c99f10374e5e809803252d7bf7feff7e763
Manu Cornet <manucornet@chromium.org>
2020-02-14 17:01:18 +0000

    CrOS Shelf: Remove |TargetBounds| struct from layout manager
    Target bounds are now tracked by their relevant shelf components.
    The target opacity is the sole remaining field in this struct, and
    can be kept as a simple float field instead.
    
    Change-Id: I2599fd0994e860d959046b87e860927cbe3b8df2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057106
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741466}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -1068,7 +1068,7 @@ void ShelfLayoutManager::OnDeskSwitchAnimationFinished() {
 }
 
 float ShelfLayoutManager::GetOpacity() const {
-  return target_bounds_.opacity;
+  return target_opacity_;
 }
 
 int ShelfLayoutManager::CalculateHotseatYInScreen(
@@ -1119,10 +1119,6 @@ void ShelfLayoutManager::OnTabletModeEnded() {
 
 ////////////////////////////////////////////////////////////////////////////////
 // ShelfLayoutManager, private:
-
-ShelfLayoutManager::TargetBounds::TargetBounds() : opacity(0.0f) {}
-
-ShelfLayoutManager::TargetBounds::~TargetBounds() = default;
 
 void ShelfLayoutManager::OnCenterVisibilityChanged(
     message_center::Visibility visibility) {
@@ -1424,17 +1420,17 @@ void ShelfLayoutManager::SetDimmed(bool dimmed) {
   const gfx::Tween::Type dim_animation_tween =
       ShelfConfig::Get()->DimAnimationTween();
 
-  AnimateOpacity(shelf_->navigation_widget(), target_bounds_.opacity,
+  AnimateOpacity(shelf_->navigation_widget(), target_opacity_,
                  dim_animation_duration, dim_animation_tween);
 
   AnimateOpacity(shelf_->hotseat_widget(),
                  shelf_->hotseat_widget()->CalculateOpacity(),
                  dim_animation_duration, dim_animation_tween);
 
-  AnimateOpacity(shelf_->status_area_widget(), target_bounds_.opacity,
+  AnimateOpacity(shelf_->status_area_widget(), target_opacity_,
                  dim_animation_duration, dim_animation_tween);
 
-  shelf_widget_->SetLoginShelfButtonOpacity(target_bounds_.opacity);
+  shelf_widget_->SetLoginShelfButtonOpacity(target_opacity_);
 }
 
 void ShelfLayoutManager::UpdateBoundsAndOpacity(bool animate) {
@@ -1449,7 +1445,7 @@ void ShelfLayoutManager::UpdateBoundsAndOpacity(bool animate) {
 
     // Having a window which is visible but does not have an opacity is an
     // illegal state. We therefore hide the shelf here if required.
-    if (!target_bounds_.opacity) {
+    if (!target_opacity_) {
       nav_widget->Hide();
       status_widget->Hide();
     }
@@ -1462,7 +1458,7 @@ void ShelfLayoutManager::UpdateBoundsAndOpacity(bool animate) {
 
     // Nav widget handles its own bounds animations so we use AnimateOpacity to
     // create a separate ScopedLayerAnimationSettings for nav widget opacity.
-    AnimateOpacity(nav_widget, target_bounds_.opacity, animation_duration,
+    AnimateOpacity(nav_widget, target_opacity_, animation_duration,
                    gfx::Tween::EASE_OUT);
 
     // Let the navigation widget handle its own layout changes.
@@ -1507,7 +1503,7 @@ void ShelfLayoutManager::UpdateBoundsAndOpacity(bool animate) {
 
   // Setting visibility during an animation causes the visibility property to
   // animate. Set the visibility property without an animation.
-  if (target_bounds_.opacity) {
+  if (target_opacity_) {
     if (state_.IsActiveSessionState()) {
       nav_widget->ShowInactive();
       hotseat_widget->ShowInactive();
@@ -1546,7 +1542,7 @@ gfx::Insets ShelfLayoutManager::CalculateTargetBounds(
   shelf_->navigation_widget()->CalculateTargetBounds();
   shelf_->hotseat_widget()->CalculateTargetBounds();
 
-  target_bounds_.opacity = ComputeTargetOpacity(state);
+  target_opacity_ = ComputeTargetOpacity(state);
 
   if (drag_status_ == kDragInProgress)
     UpdateTargetBoundsForGesture(hotseat_target_state);
