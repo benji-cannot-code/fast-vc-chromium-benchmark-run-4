a111d74cbe13af030b24ffd72b13d3178c7abcef
Ehsan Kia <ehsankia@google.com>
2020-02-14 21:13:59 +0000

    [QRCode] Improve error detection to handle all different types of camera error
    Bug: 1052026
    Change-Id: Id7e396d64b1c3002472e7b2a694d3e4a719648c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055705
    Commit-Queue: Ehsan Kia <ehsankia@google.com>
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741570}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/scan_tab/CameraPreview.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/scan_tab/CameraPreview.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.share.qrcode.scan_tab;
 
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
@@ -19,6 +20,12 @@ import org.chromium.ui.display.DisplayAndroid;
 /** CameraPreview class controls camera and camera previews. */
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     private static final String THREAD_NAME = "CameraHandlerThread";
+
+    // Extra enums for communicating camera failure modes in the error callback.
+    protected static final int NO_CAMERA_FOUND_ERROR = 1000;
+    protected static final int CAMERA_DISABLED_ERROR = 1001;
+    protected static final int CAMERA_IN_USE_ERROR = 1002;
+    protected static final int CAMERA_FAILED_ERROR = 1003;
 
     private final Context mContext;
     private final Camera.PreviewCallback mPreviewCallback;
@@ -103,7 +110,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
             mCamera.startPreview();
         } catch (Exception e) {
-            mErrorCallback.onError(Camera.CAMERA_ERROR_UNKNOWN, mCamera);
+            mErrorCallback.onError(CAMERA_FAILED_ERROR, mCamera);
         }
     }
 
@@ -180,9 +187,22 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         try {
             camera = Camera.open(cameraId);
         } catch (RuntimeException e) {
-            mErrorCallback.onError(Camera.CAMERA_ERROR_UNKNOWN, null);
+            int error = CAMERA_IN_USE_ERROR;
+            if (cameraId == -1) {
+                error = NO_CAMERA_FOUND_ERROR;
+            } else if (isCameraDisabledByPolicy()) {
+                error = CAMERA_DISABLED_ERROR;
+            }
+            mErrorCallback.onError(error, null);
         }
         return camera;
+    }
+
+    /** Checks whether the device administrator has disabled the camera. */
+    private boolean isCameraDisabledByPolicy() {
+        DevicePolicyManager devicePolicyManager =
+                (DevicePolicyManager) mContext.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        return devicePolicyManager.getCameraDisabled(null);
     }
 
     /** SurfaceHolder.Callback implementation. */
