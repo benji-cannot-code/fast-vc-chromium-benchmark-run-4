0c914ae0a6485955f1b9588a3ed105d41b328dfd
John Delaney <johnidel@chromium.org>
2020-02-14 16:31:40 +0000

    Fix command line documentation for filter tool
    The match command documentation currently asks for "document-origin"
    but actually uses "document_origin", which is hard to recognize.
    
    This CL fixes the documentation and also adds a more verbose error
    for when the required arguments are not supplied.
    
    Change-Id: Idd5f82a61f0628677f8f69633d3e783538a0c253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055690
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741459}

--- components/subresource_filter/tools/filter_tool_main.cc
+++ components/subresource_filter/tools/filter_tool_main.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 
@@ -38,7 +39,7 @@ const char kHelpMsg[] = R"(
   subresource_filter_tool is a utility for querying a ruleset, and provides
   multiple commands:
 
-    * match --document-origin=<origin> --url=<request_url> --type=<request_type>
+    * match --document_origin=<origin> --url=<request_url> --type=<request_type>
         Prints if the request would be blocked or allowed, as well as a
         matching ruleset rule (if one matches). The output format is:
             <BLOCKED/ALLOWED> <UrlRule if any> <document_origin> <request_url>
@@ -131,7 +132,13 @@ int main(int argc, char* argv[]) {
     if (!command_line.HasSwitch(kSwitchOrigin) ||
         !command_line.HasSwitch(kSwitchUrl) ||
         !command_line.HasSwitch(kSwitchType)) {
-      std::cerr << "Missing argument for match command:" << std::endl;
+      std::vector<std::string> missing_args;
+      for (auto* arg : {kSwitchOrigin, kSwitchUrl, kSwitchType}) {
+        if (!command_line.HasSwitch(arg))
+          missing_args.push_back(arg);
+      }
+      std::cerr << "Missing arguments for match command: "
+                << base::JoinString(missing_args, ",") << std::endl;
       PrintHelp();
       return 1;
     }
