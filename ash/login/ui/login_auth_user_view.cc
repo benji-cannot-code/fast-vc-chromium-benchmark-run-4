400bf9617bff13c0df0b4096b39443a2458e505b
Roman Aleksandrov <raleksandrov@google.com>
2020-02-14 18:25:00 +0000

    LoginAuthUserView: Fix crash
    Don't call NotifyAccessibilityEvent on the view being destroyed.
    
    Bug: 1049414
    Change-Id: I06c2a9b24a0f03a948582d9706ae4917e2301a7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057068
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#741498}

--- ash/login/ui/login_auth_user_view.cc
+++ ash/login/ui/login_auth_user_view.cc
@@ -139,6 +139,11 @@ ui::CallbackLayerAnimationObserver* BuildObserverToNotifyA11yLocationChanged(
   return new ui::CallbackLayerAnimationObserver(base::BindRepeating(
       [](views::View* view,
          const ui::CallbackLayerAnimationObserver& observer) {
+        // Don't notify a11y event if the animation is aborted, as |view| may no
+        // longer be valid.
+        if (observer.aborted_count())
+          return true;
+
         view->NotifyAccessibilityEvent(ax::mojom::Event::kLocationChanged,
                                        false /*send_native_event*/);
         return true;
@@ -151,6 +156,11 @@ ui::CallbackLayerAnimationObserver* BuildObserverToNotifyA11yLocationChanged(
   return new ui::CallbackLayerAnimationObserver(base::BindRepeating(
       [](LoginPinView* view,
          const ui::CallbackLayerAnimationObserver& observer) {
+        // Don't notify a11y event if the animation is aborted, as |view| may no
+        // longer be valid.
+        if (observer.aborted_count())
+          return true;
+
         view->NotifyAccessibilityLocationChanged();
         return true;
       },
