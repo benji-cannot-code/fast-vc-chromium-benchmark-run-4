ba130b6b34504da3b810c20faa65196d0f08889e
Mason Freed <masonfreed@chromium.org>
2020-02-14 19:59:40 +0000

    Implement basic declarative Shadow DOM
    This CL adds the DeclarativeShadowDOM runtime enabled feature, and
    implements a basic approach. There are no tests yet, as this is an
    evolving spec/approach. But this allows basic testing of attaching
    the shadow root upon the closing <template> tag.
    
    See the explainer [1], the issue discussion [2], the intent to
    prototype [3], and the chromestatus entry [4].
    
    To test this feature:
      --enable-blink-features=DeclarativeShadowDOM
    
    and then:
      <div>
        <template shadowroot=open>
          <div></div>
          <slot></slot>
        </template>
        <span>
      </div>
    
    [1] https://github.com/mfreed7/declarative-shadow-dom/blob/master/README.md
    [2] https://github.com/whatwg/dom/issues/831
    [3] https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/nJDc-1s3R9U
    [4] https://chromestatus.com/feature/5191745052606464
    
    Bug: 1042130
    
    Change-Id: I5057c932a89fc401735d024348799d30c97e959e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018336
    Auto-Submit: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741538}

--- third_party/blink/renderer/core/html/parser/html_tree_builder.cc
+++ third_party/blink/renderer/core/html/parser/html_tree_builder.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
 #include "third_party/blink/renderer/core/dom/element_traversal.h"
+#include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_element.h"
@@ -910,10 +911,38 @@ bool HTMLTreeBuilder::ProcessTemplateEndTag(AtomicHTMLToken* token) {
   tree_.GenerateImpliedEndTags();
   if (!tree_.CurrentStackItem()->HasTagName(html_names::kTemplateTag))
     ParseError(token);
-  tree_.OpenElements()->PopUntilPopped(html_names::kTemplateTag);
+  tree_.OpenElements()->PopUntil(html_names::kTemplateTag.LocalName());
+  HTMLStackItem* template_stack_item =
+      tree_.OpenElements()->TopRecord()->StackItem();
+  tree_.OpenElements()->Pop();
+  HTMLStackItem* shadow_host_stack_item =
+      tree_.OpenElements()->TopRecord()->StackItem();
   tree_.ActiveFormattingElements()->ClearToLastMarker();
   template_insertion_modes_.pop_back();
   ResetInsertionModeAppropriately();
+  if (RuntimeEnabledFeatures::DeclarativeShadowDOMEnabled() &&
+      template_stack_item && template_stack_item->IsElementNode() &&
+      shadow_host_stack_item && shadow_host_stack_item->IsElementNode()) {
+    // TODO(masonfreed): Add a use counter here.
+    HTMLTemplateElement* template_element =
+        DynamicTo<HTMLTemplateElement>(template_stack_item->GetElement());
+    DCHECK(template_element);
+    Element* shadow_host = shadow_host_stack_item->GetElement();
+    DCHECK(shadow_host);
+    const auto& shadow_mode =
+        template_element->FastGetAttribute(html_names::kShadowrootAttr);
+    if (shadow_mode != g_null_atom) {
+      bool is_open = EqualIgnoringASCIICase(shadow_mode, "open");
+      if (is_open || EqualIgnoringASCIICase(shadow_mode, "closed")) {
+        shadow_host->AttachDeclarativeShadowRoot(
+            template_element,
+            is_open ? ShadowRootType::kOpen : ShadowRootType::kClosed);
+      } else {
+        // TODO(masonfreed): eventually, console warning here.
+        LOG(ERROR) << "Invalid shadowroot value " << shadow_mode;
+      }
+    }
+  }
   return true;
 }
 
