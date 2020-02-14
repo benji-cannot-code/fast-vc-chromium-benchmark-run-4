11582762e4357c6c9c4cd74c1ebebd8809266c10
Josh Karlin <jkarlin@chromium.org>
2020-02-14 12:27:53 +0000

    MakeAsyncTaskId non-copyable non-movable
    What: Make AsyncTaskId non-copyable/non-movable
    
    Why: The address of an AsyncTaskId is what's used to keep track of
    them. So if they move after they've been registered then corruption
    ensues.
    
    How: This is a trivial change. I did have to make a small change to
    XrFrameRequestCallbackCollection to prevent the AsyncTaskId from
    being copied.
    
    Bug: 1051938
    Change-Id: Idf751369c831a9130462b4eb2aa83be121df1fc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053409
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741412}

--- third_party/blink/renderer/modules/xr/xr_frame_request_callback_collection.h
+++ third_party/blink/renderer/modules/xr/xr_frame_request_callback_collection.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_XR_XR_FRAME_REQUEST_CALLBACK_COLLECTION_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_XR_XR_FRAME_REQUEST_CALLBACK_COLLECTION_H_
 
-#include "third_party/blink/renderer/core/probe/async_task_id.h"
 #include "third_party/blink/renderer/platform/bindings/name_client.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 
@@ -16,6 +15,10 @@ class ExecutionContext;
 class V8XRFrameRequestCallback;
 class XRFrame;
 class XRSession;
+
+namespace probe {
+class AsyncTaskId;
+}
 
 class XRFrameRequestCallbackCollection final
     : public GarbageCollected<XRFrameRequestCallbackCollection>,
@@ -42,8 +45,8 @@ class XRFrameRequestCallbackCollection final
            !WTF::IsHashTraitsEmptyValue<Traits, CallbackId>(id);
   }
 
-  using CallbackAndAsyncTask =
-      std::pair<Member<V8XRFrameRequestCallback>, probe::AsyncTaskId>;
+  using CallbackAndAsyncTask = std::pair<Member<V8XRFrameRequestCallback>,
+                                         std::unique_ptr<probe::AsyncTaskId>>;
   using CallbackMap = HeapHashMap<CallbackId, CallbackAndAsyncTask>;
 
   CallbackMap callbacks_;
