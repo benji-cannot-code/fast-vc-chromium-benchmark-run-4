f96d4170f936965bf2a62560035422449e7261a0
Matt Jones <mdjones@chromium.org>
2020-02-14 19:12:51 +0000

    Disable flaky CookieControlsViewTest#testUpdate
    TBR: dullweber@chromium.org
    Bug: 1052395
    Change-Id: Ieecc844418eb9b3ff976306d6af97768351719e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057152
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741513}

--- chrome/android/javatests/src/org/chromium/chrome/browser/page_info/CookieControlsViewTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/page_info/CookieControlsViewTest.java
@@ -25,6 +25,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -120,6 +121,7 @@ import org.chromium.ui.test.util.DisableAnimationsTestRule;
     /**
      * Tests that CookieControlsView updates on navigations.
      */
+    @DisabledTest(message = "https://crbug.com/1052395")
     @Test
     @MediumTest
     public void testUpdate() {
