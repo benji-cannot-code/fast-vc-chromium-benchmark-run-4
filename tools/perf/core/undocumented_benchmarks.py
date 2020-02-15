c0a3e833a850c8a593cdfdbb411e3016ca80fde2
Caleb Rouleau <crouleau@chromium.org>
2020-02-15 00:56:23 +0000

    [Benchmarking] Turn off benchmarks from Win 7 and Nexus 5.
    Per https://groups.google.com/a/google.com/g/chrome-speed/c/J1O_3GXF00Q
    and https://docs.google.com/document/d/1ntZVHyapCVuNMDjbEblI7za7GX_T96oV-FSmKtrEefA/edit#heading=h.a0tfb17lbj4z
    
    Turn off benchmarks that do not explicitly opt in to run on Win 7 Perf, Win 7 Nvidia GPU Perf,
    and Nexus 5.
    
    Also I fixed a bug in bot_platforms.py to make it more usable for this and fixed the
    sharding script deschedule command to additionally work for
    executables (gtests), not just benchmarks.
    
    I ran the following two commands after making the code changes
    to generate the autogenerated files:
    
    ./generate_perf_sharding.py deschedule
    ./generate_perf_sharding.py update-timing --filter-only
    
    Note that after this change these platforms will become extremely unbalanced.
    I will reshard them this weekend to reduce the potential culprit list
    if there is a silicon-lottery related regression as low as possible.
    
    Note that it turns out that
    passthrough_command_buffer_perftests and
    validating_command_buffer_perftests were only scheduled on
    the Win 7 Nvidia GPU Perf bot. Since they were not opted in,
    they became removed from all bots. I sent an email directly to
    the owners of those executables to ask them if they wanted those
    executables scheduled on the win 10 perf bots.
    
    Bug: 1030840
    Change-Id: I66b282ec9a312f58c822c3f06a9f845226b6ce20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2056564
    Commit-Queue: Caleb Rouleau <crouleau@chromium.org>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Auto-Submit: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741679}

--- tools/perf/core/undocumented_benchmarks.py
+++ tools/perf/core/undocumented_benchmarks.py
@@ -20,7 +20,6 @@ UNDOCUMENTED_BENCHMARKS = {
   'memory.desktop',
   'net_perftests',
   'octane',
-  'passthrough_command_buffer_perftests',
   'performance_browser_tests',
   'speedometer',
   'speedometer-future',
@@ -36,6 +35,5 @@ UNDOCUMENTED_BENCHMARKS = {
   'v8.browsing_mobile',
   'v8.browsing_mobile-future',
   'v8.runtime_stats.top_25',
-  'validating_command_buffer_perftests',
   'views_perftests',
 }
