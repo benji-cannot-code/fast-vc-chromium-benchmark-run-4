3ddac91a054cee6d3b658062f86c1770ca151bf2
Sylvain Defresne <sdefresne@chromium.org>
2020-02-15 01:29:21 +0000

    [ios] Add method to initialize Firebase to AppDistributionProvider
    As a first step towards using Firebase via the internal framework,
    and thus removing the dependency upstream on Firebase, add a method
    to AppDistributionProvider to initialize Firebase.
    
    Bug: 1049042
    Change-Id: Ifbbb211b844eeb564462586755f66913a01d5bb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057750
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Peter Lee <pkl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741687}

--- ios/public/provider/chrome/browser/distribution/app_distribution_provider.mm
+++ ios/public/provider/chrome/browser/distribution/app_distribution_provider.mm
@@ -23,6 +23,9 @@ void AppDistributionProvider::ScheduleDistributionNotifications(
 
 void AppDistributionProvider::CancelDistributionNotifications() {}
 
+void AppDistributionProvider::InitializeFirebase(int64_t install_date,
+                                                 bool is_first_run) {}
+
 bool AppDistributionProvider::IsPreFirebaseLegacyUser(int64_t install_date) {
   return false;
 }
