67fd714e1528599590f0cd4571406f210a07cd99
Kurt Horimoto <kkhorimoto@chromium.org>
2020-02-15 01:40:48 +0000

    [iOS] Remove incorrect DCHECK in MainContentUIState.
    This DCHECK assumes that every |-scrollViewDidEndDraggingWithGesture:
    targetContentOffset:| callback is received following a corresponding
    |-scrollViewWillBeginDraggingWithGesture:|.  This is not always the
    case though, since sometimes the begin dragging signals is not sent
    if the drag begins at the same time as WKWebView events like page
    loads or navigations.
    
    This CL removes the DCHECK and updates FullscreenModel to send the
    FullscreenModelObserver::FullscreenModelScrollEventStarted() callback
    when |scrolling_| is set to true, which occurs when the scroll view is
    either dragging or decelerating.  This allows for this callback to be
    handled even if the drag begin callback was skipped.
    
    Bug: 1001872, 1048010
    Change-Id: I6dec6280ba738ecdd4eb035e1133bae7fe7868ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057809
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741691}

--- ios/chrome/browser/ui/main_content/main_content_ui_state.mm
+++ ios/chrome/browser/ui/main_content/main_content_ui_state.mm
@@ -53,11 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)setDecelerating:(BOOL)decelerating {
   if (_decelerating == decelerating)
     return;
-  // If the scroll view is starting to decelerate after a drag, it is expected
-  // that this property is set before |dragging| is reset to NO.  This ensures
-  // that the broadcasted |scrolling| property does not quickly flip to NO when
-  // drag events finish.
-  DCHECK(!decelerating || self.dragging);
   _decelerating = decelerating;
   [self updateIsScrolling];
 }
