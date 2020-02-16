fb5e1462c115a9a284971410ba31b34e3c726485
Christopher Cameron <ccameron@chromium.org>
2020-02-16 07:28:42 +0000

    Remove viz::OutputSurface::GetOverlayBufferFormat()
    This has no callers.
    
    TBR=khushalsagar
    
    Bug: 1049334
    Change-Id: I1f1b17dc3481921bfbeb93efa07fc49d7763bea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058793
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741764}

--- components/viz/service/display_embedder/software_output_surface.cc
+++ components/viz/service/display_embedder/software_output_surface.cc
@@ -92,10 +92,6 @@ unsigned SoftwareOutputSurface::GetOverlayTextureId() const {
   return 0;
 }
 
-gfx::BufferFormat SoftwareOutputSurface::GetOverlayBufferFormat() const {
-  return gfx::BufferFormat::RGBX_8888;
-}
-
 bool SoftwareOutputSurface::HasExternalStencilTest() const {
   return false;
 }
