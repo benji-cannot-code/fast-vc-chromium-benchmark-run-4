1c03c19fb396d88064710ae61e40ab994c1e5abb
Charlie Hu <chenleihu@google.com>
2020-02-16 23:22:14 +0000

    Replace param |disposition| with |should_report| on IsFeatureEnabled
    Previously |disposition| on IsFeatureEnabled have an awkward type
    base::Optional<mojom::FeaturePolicyDisposition>*, which has 3 states
    correspond to FeatureEnabledState:
    - base::nullopt <= kEnabled
    - FeaturePolicyDisposition::kReport <= kReportOnly
    - FeaturePolicyDisposition::kEnforce <= kDisabled
    
    This CL simplifies the representation of this piece of information to
    |should_report|, and removes FeatureEnabledState enum.
    
    The mapping of FeatureEnabledState to new logic:
    kEnabled: enabled=true; should_report=false
    kReportOnly: enabled = true; should_report=true
    kDisabled: enabled = false; should_report=true
    
    Change-Id: I1fa82fb5e9924b8eed448853b139772e024bd531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037326
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741789}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -8259,7 +8259,7 @@ void Document::CountPotentialFeaturePolicyViolation(
 }
 void Document::ReportFeaturePolicyViolation(
     mojom::blink::FeaturePolicyFeature feature,
-    mojom::FeaturePolicyDisposition disposition,
+    mojom::blink::FeaturePolicyDisposition disposition,
     const String& message,
     const String& source_file) const {
   if (!RuntimeEnabledFeatures::FeaturePolicyReportingEnabled(this))
@@ -8271,8 +8271,9 @@ void Document::ReportFeaturePolicyViolation(
   // Construct the feature policy violation report.
   const String& feature_name = GetNameForFeature(feature);
   const String& disp_str =
-      (disposition == mojom::FeaturePolicyDisposition::kReport ? "report"
-                                                               : "enforce");
+      (disposition == mojom::blink::FeaturePolicyDisposition::kReport
+           ? "report"
+           : "enforce");
 
   FeaturePolicyViolationReportBody* body =
       source_file.IsEmpty()
