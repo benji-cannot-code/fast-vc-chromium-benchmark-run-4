df844db91f6ad0d84644d0cdf42867bcf8464dbc
Robbie Gibson <rkgibson@google.com>
2020-02-17 13:02:18 +0000

    [iOS] Fix crash when closing last incognito tab
    Fixed: 1049932
    Change-Id: I2972d4e570e5b472755f6c58d2d699db484fe4ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2060288
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#741899}

--- ios/chrome/browser/ui/find_bar/find_bar_coordinator.mm
+++ ios/chrome/browser/ui/find_bar/find_bar_coordinator.mm
@@ -79,6 +79,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   [self.findBarController findBarViewWillHide];
   [self.presenter dismissAnimated:animated];
+
+  self.mediator = nil;
 }
 
 - (void)showAnimated:(BOOL)animated shouldFocus:(BOOL)shouldFocus {
