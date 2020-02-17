00d79a0127994057ab24fedaf5e1c253fea73d30
Jose Lopes <jabolopes@google.com>
2020-02-17 11:37:24 +0000

    Migrate callbacks in UrlData.
    The redirect callbacks are called only once. This is guaranteed by the std::vector::swap:
    * https://cs.chromium.org/chromium/src/media/blink/url_index.cc?rcl=756282c5ac4947c7329bc8b68711c3898334018f&l=121
    * https://cs.chromium.org/chromium/src/media/blink/url_index.cc?rcl=756282c5ac4947c7329bc8b68711c3898334018f&l=131
    
    This is part of the base::Callback migration.
    
    Context: https://cs.chromium.org/chromium/src/docs/callback.md?rcl=9fcc3764aea8f97e9f6de4a9ee61d554e67edcda&l=40
    
    Bug: 714018
    Change-Id: I9ee6c077341633bdc2bb586dec747bd66d79c3d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2035995
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Jose Lopes <jabolopes@google.com>
    Cr-Commit-Position: refs/heads/master@{#741882}

--- media/blink/multibuffer_data_source.cc
+++ media/blink/multibuffer_data_source.cc
@@ -144,7 +144,7 @@ MultibufferDataSource::MultibufferDataSource(
   DCHECK(url_data_.get());
   url_data_->Use();
   url_data_->OnRedirect(
-      base::Bind(&MultibufferDataSource::OnRedirect, weak_ptr_));
+      base::BindOnce(&MultibufferDataSource::OnRedirect, weak_ptr_));
 }
 
 MultibufferDataSource::~MultibufferDataSource() {
@@ -243,7 +243,7 @@ void MultibufferDataSource::OnRedirect(
 
   if (url_data_) {
     url_data_->OnRedirect(
-        base::Bind(&MultibufferDataSource::OnRedirect, weak_ptr_));
+        base::BindOnce(&MultibufferDataSource::OnRedirect, weak_ptr_));
 
     if (init_cb_) {
       CreateResourceLoader(0, kPositionNotSpecified);
