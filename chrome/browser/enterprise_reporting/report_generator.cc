5d364bc549e823bbaf3309b57eb30a67f928835c
Anqing Zhao <anqing@google.com>
2020-02-17 13:47:21 +0000

    Clear the android_app_infos before each reporting
    Currently, the android_app_infos is not cleaned up properly. The list
    size become bigger and bigger gradually until it exceeds the request
    limit.
    
    This fix is to add a cleanup operation before each reporting. The
    corresponding unit test is also added.
    
    Bug: 1052831
    Change-Id: I98421bd6254f37fa921ba3eee1300b20592697f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2059370
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Anqing Zhao <anqing@google.com>
    Cr-Commit-Position: refs/heads/master@{#741904}

--- chrome/browser/enterprise_reporting/report_generator.cc
+++ chrome/browser/enterprise_reporting/report_generator.cc
@@ -46,7 +46,7 @@ void ReportGenerator::SetMaximumReportSizeForTesting(size_t size) {
 
 void ReportGenerator::CreateBasicRequest() {
 #if defined(OS_CHROMEOS)
-  AppendAndroidAppInfos();
+  SetAndroidAppInfos();
 #else
   basic_request_.set_computer_name(this->GetMachineName());
   basic_request_.set_os_user_name(GetOSUserName());
@@ -85,7 +85,9 @@ std::string ReportGenerator::GetSerialNumber() {
 
 #if defined(OS_CHROMEOS)
 
-void ReportGenerator::AppendAndroidAppInfos() {
+void ReportGenerator::SetAndroidAppInfos() {
+  basic_request_.clear_android_app_infos();
+
   // Android application is only supported for primary profile.
   Profile* primary_profile =
       g_browser_process->profile_manager()->GetPrimaryUserProfile();
