5abda5ffdf38104b6d2be9abe06310ac26096eef
Tobias Sargeant <tobiasjs@chromium.org>
2020-02-17 17:51:53 +0000

    Revert "[wv] PlatformServiceBridge cannot congtain inner classes."
    This reverts commit d1b55f523dfd1f86589d6d3522ad7f5dd4a90d1b.
    
    Reason for revert: Landed build config that enables
    PlatformServiceBridge to have inner classes
    
    Original change's description:
    > [wv] PlatformServiceBridge cannot congtain inner classes.
    > 
    > PlatformServiceBridge.java is compiled twice into two jars for internal
    > builds, and combining the jars is smart enough to remove the duplicate,
    > but cannot do the same for any inner classes. Therefore we must
    > introduce an explicit adapter class to perform the same function.
    > 
    > Bug: 1046782
    > Change-Id: I2a933ced5fa051271f42571eaffea8671a6fb094
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2027810
    > Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    > Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#736476}
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1046782
    Change-Id: If29c419d94c6acfcf922df8d0373cfdf5b2403de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041475
    Commit-Queue: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741953}

--- android_webview/java/src/org/chromium/android_webview/common/PlatformServiceBridge.java
+++ android_webview/java/src/org/chromium/android_webview/common/PlatformServiceBridge.java
@@ -12,6 +12,7 @@ import android.os.HandlerThread;
 import androidx.annotation.NonNull;
 
 import org.chromium.base.Callback;
+import org.chromium.base.Consumer;
 import org.chromium.base.ThreadUtils;
 import org.chromium.components.metrics.AndroidMetricsLogUploader;
 
@@ -30,7 +31,12 @@ public abstract class PlatformServiceBridge {
     private static final Object sHandlerLock = new Object();
 
     protected PlatformServiceBridge() {
-        AndroidMetricsLogUploader.setUploader(new MetricsLogUploaderAdapter(this));
+        AndroidMetricsLogUploader.setUploader(new Consumer<byte[]>() {
+            @Override
+            public void accept(byte[] data) {
+                logMetrics(data);
+            }
+        });
     }
 
     @SuppressWarnings("unused")
