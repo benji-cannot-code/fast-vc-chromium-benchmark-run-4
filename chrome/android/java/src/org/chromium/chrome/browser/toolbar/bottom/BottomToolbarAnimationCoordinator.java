a9d8a5d9fed3b6d51ef842b9f7d0527b2dedfb9d
Mia Glaese <glamia@chromium.org>
2020-02-17 19:52:17 +0000

    Revert "[Start Surface] Add toolbar animations"
    This reverts commit a70166fdf2abec16edf158544f0f2c39e3a3c981.
    
    Reason for revert: May cause crashes on Canary crbug.com/1052744
    
    Original change's description:
    > [Start Surface] Add toolbar animations
    > 
    > Screen capture:
    > https://drive.google.com/drive/folders/1VSZGz0yMwmYBV0uG0Zv5jgtLzKQt8DVM?usp=sharing
    > 
    > Bug: 1025296
    > Change-Id: I6d3671252578fa9f37e5f1173b8d55b364a898c1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1981853
    > Commit-Queue: Mia Glaese <glamia@chromium.org>
    > Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#741772}
    
    TBR=yusufo@chromium.org,gogerald@chromium.org,pnoland@chromium.org,glamia@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1025296
    Change-Id: I1b86c55bf43601536eff5cc3b8f24ce867aed1fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058819
    Reviewed-by: Mia Glaese <glamia@chromium.org>
    Commit-Queue: Mia Glaese <glamia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741973}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BottomToolbarAnimationCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BottomToolbarAnimationCoordinator.java
@@ -17,7 +17,7 @@ import android.view.ViewGroup.MarginLayoutParams;
 
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.compositor.layouts.EmptyOverviewModeObserver;
-import org.chromium.chrome.browser.toolbar.top.TopToolbarCoordinator;
+import org.chromium.chrome.browser.toolbar.ToolbarManager;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -31,7 +31,7 @@ import java.util.List;
  */
 public class BottomToolbarAnimationCoordinator extends EmptyOverviewModeObserver {
     private static final long DURATION =
-            TopToolbarCoordinator.TAB_SWITCHER_MODE_NORMAL_ANIMATION_DURATION_MS;
+            ToolbarManager.TAB_SWITCHER_MODE_NORMAL_ANIMATION_DURATION_MS;
 
     /** The browsing mode bottom toolbar component */
     private final BrowsingModeBottomToolbarCoordinator mBrowsingModeCoordinator;
