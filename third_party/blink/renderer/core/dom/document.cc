653e35345779e74420ba1757de8418b52025cf2e
Takashi Sakamoto <tasak@google.com>
2020-02-17 10:34:14 +0000

    Revert "Replace param |disposition| with |should_report| on IsFeatureEnabled"
    This reverts commit ccf8b883ea63a1494a339123acd9cadc95501814.
    
    Reason for revert: To revert https://chromium-review.googlesource.com/c/chromium/src/+/2028666
    
    https://chromium-review.googlesource.com/c/chromium/src/+/2028666 seems to cause LinuxCFI tests failures.
    
    LinuxCFI tests start failing at
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20CFI/16433
    16433 contains https://chromium-review.googlesource.com/c/chromium/src/+/2028666 
    
    Original change's description:
    > Replace param |disposition| with |should_report| on IsFeatureEnabled
    > 
    > Previously |disposition| on IsFeatureEnabled have an awkward type
    > base::Optional<mojom::FeaturePolicyDisposition>*, which has 3 states
    > correspond to FeatureEnabledState:
    > - base::nullopt <= kEnabled
    > - FeaturePolicyDisposition::kReport <= kReportOnly
    > - FeaturePolicyDisposition::kEnforce <= kDisabled
    > 
    > This CL simplifies the representation of this piece of information to
    > |should_report|, and removes FeatureEnabledState enum.
    > 
    > The mapping of FeatureEnabledState to new logic:
    > kEnabled: enabled=true; should_report=false
    > kReportOnly: enabled = true; should_report=true
    > kDisabled: enabled = false; should_report=true
    > 
    > Change-Id: I1fa82fb5e9924b8eed448853b139772e024bd531
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037326
    > Commit-Queue: Charlie Hu <chenleihu@google.com>
    > Reviewed-by: Ian Clelland <iclelland@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#741789}
    
    TBR=dcheng@chromium.org,iclelland@chromium.org,chenleihu@google.com
    
    Change-Id: Id6531935bbc777662fc3af2af65612495cb5a803
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2059555
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/master@{#741872}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -8259,7 +8259,7 @@ void Document::CountPotentialFeaturePolicyViolation(
 }
 void Document::ReportFeaturePolicyViolation(
     mojom::blink::FeaturePolicyFeature feature,
-    mojom::blink::FeaturePolicyDisposition disposition,
+    mojom::FeaturePolicyDisposition disposition,
     const String& message,
     const String& source_file) const {
   if (!RuntimeEnabledFeatures::FeaturePolicyReportingEnabled(this))
@@ -8271,9 +8271,8 @@ void Document::ReportFeaturePolicyViolation(
   // Construct the feature policy violation report.
   const String& feature_name = GetNameForFeature(feature);
   const String& disp_str =
-      (disposition == mojom::blink::FeaturePolicyDisposition::kReport
-           ? "report"
-           : "enforce");
+      (disposition == mojom::FeaturePolicyDisposition::kReport ? "report"
+                                                               : "enforce");
 
   FeaturePolicyViolationReportBody* body =
       source_file.IsEmpty()
