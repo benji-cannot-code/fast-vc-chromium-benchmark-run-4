b2fd4485f8afbbfef428d3681a26f4945460539e
Nazerke <nazerke@google.com>
2020-02-17 12:40:42 +0000

    [iOS][coordinator] Modernize Repost Form Coordinator.
    This CL modernizes the Repost Form Coordinator to use |browser| in
    the initializer and updates unittests.
    
    Bug: 1029346, 1048668
    Change-Id: I974a12795c4acb8e3ccfa492f1a3b746d57a63cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2044156
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Cr-Commit-Position: refs/heads/master@{#741891}

--- ios/chrome/browser/ui/browser_view/browser_coordinator.mm
+++ ios/chrome/browser/ui/browser_view/browser_coordinator.mm
@@ -562,6 +562,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                      completionHandler:(void (^)(BOOL))completion {
   self.repostFormCoordinator = [[RepostFormCoordinator alloc]
       initWithBaseViewController:self.viewController
+                         browser:self.browser
                   dialogLocation:location
                         webState:webState
                completionHandler:completion];
