2e6b6523df0489ff181fef16298b422c1bda024e
Joey Scarr <jsca@google.com>
2020-02-17 10:58:45 +0000

    Convert DiscardableSharedMemoryHeap to use OnceClosure.
    Bug: 1007682
    Change-Id: If6f048d20652cfa9a8b612dd68f00f9de3beff15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2059359
    Auto-Submit: Joey Scarr <jsca@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741874}

--- components/discardable_memory/common/discardable_shared_memory_heap.h
+++ components/discardable_memory/common/discardable_shared_memory_heap.h
@@ -64,7 +64,7 @@ class DISCARDABLE_MEMORY_EXPORT DiscardableSharedMemoryHeap {
       std::unique_ptr<base::DiscardableSharedMemory> shared_memory,
       size_t size,
       int32_t id,
-      const base::Closure& deleted_callback);
+      base::OnceClosure deleted_callback);
 
   // Merge |span| into the free lists. This will coalesce |span| with
   // neighboring free spans when possible.
@@ -112,7 +112,7 @@ class DISCARDABLE_MEMORY_EXPORT DiscardableSharedMemoryHeap {
         std::unique_ptr<base::DiscardableSharedMemory> shared_memory,
         size_t size,
         int32_t id,
-        const base::Closure& deleted_callback);
+        base::OnceClosure deleted_callback);
     ~ScopedMemorySegment();
 
     bool IsUsed() const;
@@ -134,7 +134,7 @@ class DISCARDABLE_MEMORY_EXPORT DiscardableSharedMemoryHeap {
     std::unique_ptr<base::DiscardableSharedMemory> shared_memory_;
     const size_t size_;
     const int32_t id_;
-    const base::Closure deleted_callback_;
+    base::OnceClosure deleted_callback_;
 
     DISALLOW_COPY_AND_ASSIGN(ScopedMemorySegment);
   };
--- components/discardable_memory/common/discardable_shared_memory_heap_perftest.cc
+++ components/discardable_memory/common/discardable_shared_memory_heap_perftest.cc
@@ -42,7 +42,7 @@ TEST(DiscardableSharedMemoryHeapTest, SearchFreeLists) {
     ASSERT_TRUE(memory->CreateAndMap(segment_size));
     heap.MergeIntoFreeLists(heap.Grow(std::move(memory), segment_size,
                                       next_discardable_shared_memory_id++,
-                                      base::Bind(NullTask)));
+                                      base::BindOnce(NullTask)));
   }
 
   unsigned kSeed = 1;
