1fa25d43d6a45bc94de445c254aff904bcdb5462
Bret Sepulveda <bsep@chromium.org>
2020-02-17 15:17:39 +0000

    Prevent mouse events from triggering hover cards in browser tests.
    If the mouse happens to be in exactly the wrong position it triggers a
    hover card, which can cause strange failures in some tests, especially
    on bots where the mouse can cause the test to fail repeatedly until it
    happens to be moved. To fix this, this patch prevents
    Tab::OnMouseEntered from triggering hover cards when running an
    InProcessBrowserTest. Also changes hover card browser tests to call
    TabStrip::UpdateHoverCard instead of Tab::OnMouseEntered to get around
    that method now being disabled, and does some light refactoring.
    
    Bug: 1050012
    Change-Id: I96523b13659a5ed0cbcedc66d7cdb64c7483df72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2054095
    Commit-Queue: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741922}

--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -110,6 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(TOOLKIT_VIEWS)
+#include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/test/views/accessibility_checker.h"
 #include "ui/views/views_delegate.h"
 #endif
@@ -288,6 +289,13 @@ void InProcessBrowserTest::SetUp() {
                                     default_download_dir_.GetPath()));
 
   AfterStartupTaskUtils::DisableScheduleTaskDelayForTesting();
+
+#if defined(TOOLKIT_VIEWS)
+  // Prevent hover cards from appearing when the mouse is over the tab. Tests
+  // don't typically account for this possibly, so it can cause unrelated tests
+  // to fail. See crbug.com/1050012.
+  Tab::SetShowHoverCardOnMouseHoverForTesting(false);
+#endif  // defined(TOOLKIT_VIEWS)
 
   BrowserTestBase::SetUp();
 }
