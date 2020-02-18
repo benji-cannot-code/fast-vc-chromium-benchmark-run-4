af83887b7addbd13574089396dcfe7ce72338310
Kent Tamura <tkent@chromium.org>
2020-02-18 08:05:42 +0000

    Remove blink::LayoutListBox
    LayoutListBox provided
     * Intrinsic height, and
     * Adjustment of minimum width.
    
    For the former, this CL folds it into LayoutBlock and NGBlockNode
    because it's small.
    
    For the latter, this CL moves it to LayoutBox, and
    ClampIntrinsicBlockSize(), NGLayoutInputNode::ComputeMinMaxSize(), and
    LayoutBox::ComputeLogicalHeight() call it because the code size is not
    small.
    
    This CL has no behavior changes.
    
    Bug: 1040828, 1040826
    Change-Id: I9dd95c0971928a50895c32fa977668421078e780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053648
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742068}

--- third_party/blink/renderer/core/html/forms/html_select_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element.cc
@@ -64,8 +64,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/console_message.h"
 #include "third_party/blink/renderer/core/layout/hit_test_request.h"
 #include "third_party/blink/renderer/core/layout/hit_test_result.h"
-#include "third_party/blink/renderer/core/layout/layout_list_box.h"
+#include "third_party/blink/renderer/core/layout/layout_block_flow.h"
 #include "third_party/blink/renderer/core/layout/layout_menu_list.h"
+#include "third_party/blink/renderer/core/layout/layout_object_factory.h"
 #include "third_party/blink/renderer/core/layout/layout_theme.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/page.h"
@@ -357,11 +358,12 @@ bool HTMLSelectElement::CanSelectAll() const {
   return !UsesMenuList();
 }
 
-LayoutObject* HTMLSelectElement::CreateLayoutObject(const ComputedStyle&,
-                                                    LegacyLayout) {
+LayoutObject* HTMLSelectElement::CreateLayoutObject(
+    const ComputedStyle& style,
+    LegacyLayout legacy_layout) {
   if (UsesMenuList())
     return new LayoutMenuList(this);
-  return new LayoutListBox(this);
+  return LayoutObjectFactory::CreateBlockFlow(*this, style, legacy_layout);
 }
 
 HTMLCollection* HTMLSelectElement::selectedOptions() {
