a49498d86877383e19be85f6cab2c54e9729fb5c
Daniele Castagna <dcastagna@chromium.org>
2020-02-18 21:03:19 +0000

    viz: Change OverlayResources::size_in_pixels to a single Size
    .*DrawQuad::OverlayResources::size_in pixels used to be a vector of 4.
    Looking at the code, we always index the first one and we never use
    the rest of the array.
    
    This reduces the size of TextureDrawQuad that is the biggest
    DrawQuad.
    Since QuadList uses the size of the biggest DrawQuad to allocate any
    quad, this CL will reduce the memory used by QuadList, even if
    TextureDrawQuads are not used.
    
    Change-Id: I554cb1b313519094a893cba6342ad47c15882827
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058834
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742259}

--- components/viz/common/quads/stream_video_draw_quad.h
+++ components/viz/common/quads/stream_video_draw_quad.h
@@ -48,7 +48,7 @@ class VIZ_COMMON_EXPORT StreamVideoDrawQuad : public DrawQuad {
 
   struct OverlayResources {
     OverlayResources();
-    gfx::Size size_in_pixels[Resources::kMaxResourceIdCount];
+    gfx::Size size_in_pixels;
   };
   OverlayResources overlay_resources;
 
@@ -56,7 +56,7 @@ class VIZ_COMMON_EXPORT StreamVideoDrawQuad : public DrawQuad {
 
   ResourceId resource_id() const { return resources.ids[kResourceIdIndex]; }
   const gfx::Size& resource_size_in_pixels() const {
-    return overlay_resources.size_in_pixels[kResourceIdIndex];
+    return overlay_resources.size_in_pixels;
   }
 
  private:
