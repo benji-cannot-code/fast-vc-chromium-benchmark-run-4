3ce3463663a9c81beeba2284d946e604d0664b40
Keishi Hattori <keishi@chromium.org>
2020-02-18 13:58:46 +0000

    Convert IDBTransaction back to ExecutionContextLifecycleObserver
    Convert IDBTransaction back to ExecutionContextLifecycleObserver after crrev.com/c/2060361
    IDBTransaction uses ExecutionContextLifecycleObserver to avoid accessing ExecutionContext from the destructor.
    
    TBR=haraken@chromium.org
    
    No-Tree-Checks: true
    No-Try: true
    Bug: 1052319
    Change-Id: I055550fb310e59ac99438faa7dff268d4efc6003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2060447
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742135}

--- third_party/blink/renderer/modules/indexeddb/idb_transaction.h
+++ third_party/blink/renderer/modules/indexeddb/idb_transaction.h
@@ -65,7 +65,7 @@ class ScriptState;
 class MODULES_EXPORT IDBTransaction final
     : public EventTargetWithInlineData,
       public ActiveScriptWrappable<IDBTransaction>,
-      public ExecutionContextClient {
+      public ExecutionContextLifecycleObserver {
   USING_GARBAGE_COLLECTED_MIXIN(IDBTransaction);
   DEFINE_WRAPPERTYPEINFO();
 
@@ -195,6 +195,8 @@ class MODULES_EXPORT IDBTransaction final
   WebIDBTransaction* transaction_backend() {
     return transaction_backend_.get();
   }
+
+  void ContextDestroyed() override {}
 
  protected:
   // EventTarget
