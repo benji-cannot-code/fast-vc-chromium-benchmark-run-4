451b1e45cb0fc63cbb5112bccb343596b6f7d48a
Hongchan Choi <hongchan@chromium.org>
2020-02-18 22:39:05 +0000

    Use KeepSelfAlive on AudioContext to keep it alive until rendering stops
    When an ExecutionContext is abruptly/unexpectedly destroyed (e.g.
    shutting down of document or iframe), an AudioContext can also
    go away. This type of shutdown can be problematic because the render
    thread still might be touching resources in the AudioContext allocated
    by the main thread.
    
    This CL introduces a self-referencing pointer to the AudioContext,
    and it is cleared after the underlying render thread is stopped. In
    that way, the destruction of AudioContext can be done safely.
    
    Test: Locally confirmed the repro case doesn't crash (UAP) after 1hr.
    Bug: 1043446
    Change-Id: I2e40b7d58ca9d647eed8a5971fc69dc87ee3d1fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2049912
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742338}

--- third_party/blink/renderer/modules/webaudio/base_audio_context.h
+++ third_party/blink/renderer/modules/webaudio/base_audio_context.h
@@ -283,7 +283,7 @@ class MODULES_EXPORT BaseAudioContext
 
   DEFINE_ATTRIBUTE_EVENT_LISTENER(statechange, kStatechange)
 
-  void StartRendering();
+  virtual void StartRendering();
 
   void NotifyStateChange();
 
