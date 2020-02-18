9b5d3a5f269c20f8eb43afcbc594714d814043a2
Fredrik Söderquist <fs@opera.com>
2020-02-18 14:30:38 +0000

    Refactor LayoutSVGResourceGradient::PreparePaintServer
    Split said method into two - one that does a cache lookup and one that
    builds the GradientData.
    
    Reorder the code a bit to eliminate the redundant check for an empty
    object bounding box.
    
    Bug: 109212
    Change-Id: If9bdf1d771478d208debbdddf33188b5db28cf62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2060956
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#742140}

--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_gradient.h
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_gradient.h
@@ -26,19 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_resource_paint_server.h"
 #include "third_party/blink/renderer/core/svg/svg_gradient_element.h"
-#include "third_party/blink/renderer/platform/graphics/gradient.h"
 #include "third_party/blink/renderer/platform/transforms/affine_transform.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 
 namespace blink {
 
-struct GradientData {
-  USING_FAST_MALLOC(GradientData);
-
- public:
-  scoped_refptr<Gradient> gradient;
-  AffineTransform userspace_transform;
-};
+struct GradientData;
 
 class LayoutSVGResourceGradient : public LayoutSVGResourcePaintServer {
  public:
@@ -62,6 +55,9 @@ class LayoutSVGResourceGradient : public LayoutSVGResourcePaintServer {
       SVGSpreadMethodType);
 
  private:
+  std::unique_ptr<GradientData> BuildGradientData(
+      const FloatRect& object_bounding_box);
+
   bool should_collect_gradient_attributes_ : 1;
   using GradientMap = HeapHashMap<Member<const SVGResourceClient>,
                                   std::unique_ptr<GradientData>>;
