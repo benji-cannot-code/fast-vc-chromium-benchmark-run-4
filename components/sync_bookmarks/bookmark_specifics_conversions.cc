2f9d17db91fc04503c0c12f61fa550ee9d3537aa
Mikel Astiz <mastiz@chromium.org>
2020-02-18 12:42:15 +0000

    Revert "Enforce lower-case use of GUIDs for sync"
    This reverts commit b6a3c9f3b6c5762ce7b45ea1b5c06b6c3ed73db2.
    
    Reason for revert: bookmarks created around 2016, between [M44..M51]
    use an uppercase GUID as originator client item ID.
    
    Original change's description:
    > Enforce lower-case use of GUIDs for sync
    > 
    > base::GenerateGUID() produces lower-case GUIDs and they should get
    > exposed to the sync protocol exclusively in this canonical form to avoid
    > running into issues.
    > 
    > This patch replaces all occurrences in sync codebase with:
    > git grep -l 'IsValidGUID' -- components/sync* | \
    >   xargs sed -i 's/IsValidGUID/IsValidGUIDOutputString/g'
    > 
    > In addition, the implicit GUIDs used for bookmark sync, in particular
    > for permanent nodes, are updated to adopt lower-case GUIDs. These are
    > never committed to the server so there are no backward-compatibility
    > considerations.
    > 
    > Change-Id: I0d0468f3475e1a27a161e2af15853c757f53e201
    > Bug: 978430
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2054154
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#741381}
    
    TBR=sky@chromium.org,treib@chromium.org,mastiz@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 978430
    Change-Id: If3a135e24cb61d15cfc7616dd1160e76ce9dd3e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2061749
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742118}

--- components/sync_bookmarks/bookmark_specifics_conversions.cc
+++ components/sync_bookmarks/bookmark_specifics_conversions.cc
@@ -166,7 +166,7 @@ std::string ComputeGuidFromBytes(base::span<const uint8_t> bytes) {
 std::string InferGuidForLegacyBookmark(
     const std::string& originator_cache_guid,
     const std::string& originator_client_item_id) {
-  DCHECK(!base::IsValidGUIDOutputString(originator_client_item_id));
+  DCHECK(!base::IsValidGUID(originator_client_item_id));
 
   const std::string unique_tag =
       base::StrCat({originator_cache_guid, originator_client_item_id});
@@ -176,7 +176,7 @@ std::string InferGuidForLegacyBookmark(
   static_assert(base::kSHA1Length >= 16, "16 bytes needed to infer GUID");
 
   const std::string guid = ComputeGuidFromBytes(base::make_span(hash));
-  DCHECK(base::IsValidGUIDOutputString(guid));
+  DCHECK(base::IsValidGUID(guid));
   return guid;
 }
 
@@ -194,8 +194,7 @@ sync_pb::EntitySpecifics CreateSpecificsFromBookmarkNode(
   }
 
   DCHECK(!node->guid().empty());
-  DCHECK(base::IsValidGUIDOutputString(node->guid()))
-      << "Actual: " << node->guid();
+  DCHECK(base::IsValidGUID(node->guid())) << "Actual: " << node->guid();
 
   if (include_guid) {
     bm_specifics->set_guid(node->guid());
@@ -249,7 +248,7 @@ const bookmarks::BookmarkNode* CreateBookmarkNodeFromSpecifics(
   DCHECK(parent);
   DCHECK(model);
   DCHECK(favicon_service);
-  DCHECK(base::IsValidGUIDOutputString(specifics.guid()));
+  DCHECK(base::IsValidGUID(specifics.guid()));
 
   bookmarks::BookmarkNode::MetaInfoMap metainfo =
       GetBookmarkMetaInfo(specifics);
@@ -286,7 +285,7 @@ void UpdateBookmarkNodeFromSpecifics(
   // resolving any conflict in GUID. Either GUIDs are the same, or the GUID in
   // specifics is invalid, and hence we can ignore it.
   DCHECK(specifics.guid() == node->guid() ||
-         !base::IsValidGUIDOutputString(specifics.guid()) ||
+         !base::IsValidGUID(specifics.guid()) ||
          !base::FeatureList::IsEnabled(
              switches::kUpdateBookmarkGUIDWithNodeReplacement));
 
@@ -310,7 +309,7 @@ const bookmarks::BookmarkNode* ReplaceBookmarkNodeGUID(
     return node;
   }
   const bookmarks::BookmarkNode* new_node;
-  DCHECK(base::IsValidGUIDOutputString(guid));
+  DCHECK(base::IsValidGUID(guid));
 
   if (node->guid() == guid) {
     // Nothing to do.
@@ -342,7 +341,7 @@ bool IsValidBookmarkSpecifics(const sync_pb::BookmarkSpecifics& specifics,
     LogInvalidSpecifics(InvalidBookmarkSpecificsError::kEmptySpecifics);
     is_valid = false;
   }
-  if (!base::IsValidGUIDOutputString(specifics.guid())) {
+  if (!base::IsValidGUID(specifics.guid())) {
     DLOG(ERROR) << "Invalid bookmark: invalid GUID in the specifics.";
     LogInvalidSpecifics(InvalidBookmarkSpecificsError::kInvalidGUID);
     is_valid = false;
@@ -384,7 +383,7 @@ bool IsValidBookmarkSpecifics(const sync_pb::BookmarkSpecifics& specifics,
 bool HasExpectedBookmarkGuid(const sync_pb::BookmarkSpecifics& specifics,
                              const std::string& originator_cache_guid,
                              const std::string& originator_client_item_id) {
-  DCHECK(base::IsValidGUIDOutputString(specifics.guid()));
+  DCHECK(base::IsValidGUID(specifics.guid()));
 
   if (originator_client_item_id.empty()) {
     // This could be a future bookmark with a client tag instead of an
@@ -393,7 +392,7 @@ bool HasExpectedBookmarkGuid(const sync_pb::BookmarkSpecifics& specifics,
     return true;
   }
 
-  if (base::IsValidGUIDOutputString(originator_client_item_id)) {
+  if (base::IsValidGUID(originator_client_item_id)) {
     return specifics.guid() == originator_client_item_id;
   }
 
