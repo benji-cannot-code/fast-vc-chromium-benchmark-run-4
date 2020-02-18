7a76e35ef7fdd24e9f099ee428cb4a6cd2e5a583
Eugene But <eugenebut@chromium.org>
2020-02-18 20:55:26 +0000

    Remove #success logging from BreadcrumbManagerTabHelper::PageLoaded
    "Success" will be the most common page load result and can be omitted as
    default.
    
    Bug: 2056565
    Change-Id: I866aa24ea6da824fd3ff7c11f72fd4ca5acd1ee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057784
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742246}

--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.mm
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.mm
@@ -40,7 +40,6 @@ const char kBreadcrumbDownload[] = "#download";
 const char kBreadcrumbMixedContent[] = "#mixed";
 const char kBreadcrumbNtpNavigation[] = "#ntp";
 const char kBreadcrumbPageLoadFailure[] = "#failure";
-const char kBreadcrumbPageLoadSuccess[] = "#success";
 const char kBreadcrumbRendererInitiatedByUser[] = "#renderer-user";
 const char kBreadcrumbRendererInitiatedByScript[] = "#renderer-script";
 
@@ -136,7 +135,6 @@ void BreadcrumbManagerTabHelper::PageLoaded(
   } else {
     switch (load_completion_status) {
       case web::PageLoadCompletionStatus::SUCCESS:
-        event.push_back(kBreadcrumbPageLoadSuccess);
         break;
       case web::PageLoadCompletionStatus::FAILURE:
         event.push_back(kBreadcrumbPageLoadFailure);
--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper_unittest.mm
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper_unittest.mm
@@ -276,8 +276,6 @@ TEST_F(BreadcrumbManagerTabHelperTest, PageLoadSuccess) {
 
   EXPECT_NE(std::string::npos, events.back().find(kBreadcrumbPageLoaded))
       << events.back();
-  EXPECT_NE(std::string::npos, events.back().find(kBreadcrumbPageLoadSuccess))
-      << events.back();
   EXPECT_EQ(std::string::npos, events.back().find(kBreadcrumbPageLoadFailure))
       << events.back();
 }
@@ -292,8 +290,6 @@ TEST_F(BreadcrumbManagerTabHelperTest, PageLoadFailure) {
   ASSERT_EQ(1ul, events.size());
 
   EXPECT_NE(std::string::npos, events.back().find(kBreadcrumbPageLoaded))
-      << events.back();
-  EXPECT_EQ(std::string::npos, events.back().find(kBreadcrumbPageLoadSuccess))
       << events.back();
   EXPECT_NE(std::string::npos, events.back().find(kBreadcrumbPageLoadFailure))
       << events.back();
