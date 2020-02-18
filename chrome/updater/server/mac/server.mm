53b859afa79af7560500ae2ac8122570b7e0e602
Findit <findit-for-me@appspot.gserviceaccount.com>
2020-02-18 18:32:44 +0000

    Revert "Proof of concept for XPC service in the updater."
    This reverts commit 11357e7b380f7eb0fb77f3c44f1878e02c6b1270.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 742204 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzExMzU3ZTdiMzgwZjdlYjBmYjc3ZjNjNDRmMTg3OGUwMmM2YjEyNzAM
    
    Sample Failed Build: https://ci.chromium.org/b/8888094669312124288
    
    Sample Failed Step: compile
    
    Original change's description:
    > Proof of concept for XPC service in the updater.
    > 
    > Continues https://chromium-review.googlesource.com/c/chromium/src/+/2045058
    > 
    > Rebased. Moved code to the new chrome/updater/server/mac/server.mm::LaunchServer, addressed more feedback.
    > 
    > Need to create a plist for the xpc service. It is not clear yet what executatble will be used.
    > 
    > Bug: 1048653
    > Change-Id: Iadbbd7004afa39fcc902f2ec68297845c2f94571
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055284
    > Commit-Queue: Mila Green <milagreen@chromium.org>
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#742204}
    
    
    Change-Id: I699b45f0e1c87b5a7ccadb9a36d293d9fd7a0484
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1048653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062789
    Cr-Commit-Position: refs/heads/master@{#742209}

--- chrome/updater/server/mac/server.mm
+++ chrome/updater/server/mac/server.mm
@@ -5,35 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/updater/server/mac/server.h"
 
-#import <Foundation/Foundation.h>
-#include <xpc/xpc.h>
-
-#include "base/mac/scoped_nsobject.h"
-#include "base/strings/sys_string_conversions.h"
-#import "chrome/updater/server/mac/service_delegate.h"
+#import "base/logging.h"
 #import "chrome/updater/update_service.h"
-#include "chrome/updater/updater_version.h"
 
 namespace updater {
 
 int RunServer(std::unique_ptr<UpdateService> update_service) {
-  @autoreleasepool {
-    std::string service_name = MAC_BUNDLE_IDENTIFIER_STRING;
-    service_name.append(".UpdaterXPCService");
-    base::scoped_nsobject<NSString> updaterMachServiceName(
-        base::SysUTF8ToNSString(service_name), base::scoped_policy::RETAIN);
-    base::scoped_nsobject<UpdateCheckXPCServiceDelegate> delegate(
-        [[UpdateCheckXPCServiceDelegate alloc]
-            initWithUpdateService:std::move(update_service)]);
-
-    NSXPCListener* listener =
-        [[NSXPCListener alloc] initWithMachServiceName:updaterMachServiceName];
-    listener.delegate = delegate.get();
-
-    [listener resume];
-
-    return 0;
-  }
+  NOTREACHED();
+  return -1;
 }
 
 }  // namespace updater
