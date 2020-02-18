dc1c9ae4e51929f7130d752485f0d8f4e4ff66f2
Clemens Arbesser <arbesser@google.com>
2020-02-18 16:33:11 +0000

    [Autofill Assistant] Fixed animation issues in action carousel.
    Bug: b/144075373
    Change-Id: I559fb7bd9ad4273e3360604c24c230ce5edc0ef9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043799
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#742170}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/carousel/AssistantCarouselModel.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/carousel/AssistantCarouselModel.java
@@ -5,15 +5,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill_assistant.carousel;
 
-import org.chromium.ui.modelutil.ListModel;
+import org.chromium.ui.modelutil.PropertyModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * State for the carousel of the Autofill Assistant.
  */
-public class AssistantCarouselModel {
-    private final ListModel<AssistantChip> mChipsModel = new ListModel<>();
+public class AssistantCarouselModel extends PropertyModel {
+    public static final WritableObjectPropertyKey<List<AssistantChip>> CHIPS =
+            new WritableObjectPropertyKey<>();
 
-    public ListModel<AssistantChip> getChipsModel() {
-        return mChipsModel;
+    public AssistantCarouselModel() {
+        super(CHIPS);
+        set(CHIPS, new ArrayList<>());
+    }
+
+    public void setChips(List<AssistantChip> chips) {
+        set(CHIPS, chips);
     }
 }
