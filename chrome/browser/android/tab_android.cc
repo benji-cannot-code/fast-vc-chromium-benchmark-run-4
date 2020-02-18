ac2ec55b7e4407475f8098e9521b9d02f9f979bf
Michael Thiessen <mthiesse@chromium.org>
2020-02-18 21:02:09 +0000

    Prepare to migrate Tab#getUrl users to GURL (3 of 3)
    Going through each of the usages will take some care, so I want to
    essentially fix the API in a no-op pass and properly migrate each of
    the callsites independently (or in batches).
    
    I'm still working on migrating users of WebContents#getVisibleUrl, but
    some to the APIs those usages call into are shared by users of
    Tab#getUrl, so exposing a GURL Tab#getUrl early makes this less painful
    and requires less duplication and converting.
    
    This will be a 3-sided patch:
    1. Add the getUrlString() method and migrate upstream usage.
    2. Migrate downstream usage to getUrlString().
    3. Change getUrl() to return a GURL (this CL).
    
    Bug: 783819
    Change-Id: If841d509bff8ea9d2da048021da6ba8635a8cd1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2044379
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742257}

--- chrome/browser/android/tab_android.cc
+++ chrome/browser/android/tab_android.cc
@@ -62,6 +62,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/android/view_android.h"
 #include "ui/android/window_android.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "url/android/gurl_android.h"
+#include "url/gurl.h"
 
 using base::android::AttachCurrentThread;
 using base::android::ConvertUTF8ToJavaString;
@@ -163,8 +165,9 @@ bool TabAndroid::IsNativePage() const {
 
 GURL TabAndroid::GetURL() const {
   JNIEnv* env = base::android::AttachCurrentThread();
-  return GURL(base::android::ConvertJavaStringToUTF8(
-      Java_TabImpl_getUrl(env, weak_java_tab_.get(env))));
+  std::unique_ptr<GURL> gurl = url::GURLAndroid::ToNativeGURL(
+      env, Java_TabImpl_getUrl(env, weak_java_tab_.get(env)));
+  return std::move(*gurl);
 }
 
 bool TabAndroid::IsUserInteractable() const {
