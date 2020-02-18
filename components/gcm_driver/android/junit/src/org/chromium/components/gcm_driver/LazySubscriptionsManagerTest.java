07ccaf320d1c1105243414557be8e5ff2a283031
bttk <bttk@chromium.org>
2020-02-18 23:56:25 +0000

    metrics: Refactor CachedMetrics in components/gcm_driver/
    Bug: 1046181
    Change-Id: I58390007cfdc98199a21e2e07df31d7c575b8185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2019477
    Commit-Queue: bttk <bttk@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#742387}

--- components/gcm_driver/android/junit/src/org/chromium/components/gcm_driver/LazySubscriptionsManagerTest.java
+++ components/gcm_driver/android/junit/src/org/chromium/components/gcm_driver/LazySubscriptionsManagerTest.java
@@ -19,7 +19,6 @@ import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.ContextUtils;
-import org.chromium.base.metrics.CachedMetrics;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.test.ShadowRecordHistogram;
 import org.chromium.base.test.BaseRobolectricTestRunner;
@@ -34,8 +33,6 @@ import java.util.Set;
 public class LazySubscriptionsManagerTest {
     @Before
     public void setUp() {
-        // This commits and clears any cached metrics.
-        CachedMetrics.commitCachedMetrics();
         ShadowRecordHistogram.reset();
     }
 
