50b7ae70b973a6e44cd96a6e24b470faafcea60f
Elly Fong-Jones <ellyjones@chromium.org>
2020-02-18 21:41:34 +0000

    views: ax-ignore MenuScrollViewContainer
    Right now, MenuScrollViewContainer claims to be an AXMenuBar, which
    produces the following puzzling AX tree:
    
      AXGroup (menu root)
        AXMenuBar (scroll container)
          AXMenu (actual menu view)
            AXMenuItem ...
    
    An AXMenuBar is something like the top system menu bar, at least on
    macOS, not the "contents of a menu" or anything similar to that. After
    this change, the AX tree is instead:
    
      AXGroup (menu root)
        AXMenu (actual menu view)
          AXMenuItem ...
    
    which makes much more sense.
    
    Bug: 954701
    Change-Id: I00215874d75d9ee4870acd0e2101ef280dc2a199
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1962587
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742311}

--- ui/views/controls/menu/menu_scroll_view_container.cc
+++ ui/views/controls/menu/menu_scroll_view_container.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/color_palette.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/border.h"
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/menu/menu_config.h"
@@ -193,6 +194,11 @@ MenuScrollViewContainer::MenuScrollViewContainer(SubmenuView* content_view)
   arrow_ = BubbleBorderTypeFromAnchor(
       content_view_->GetMenuItem()->GetMenuController()->GetAnchorPosition());
 
+  // The MenuScrollViewContainer is a purely presentational container so it
+  // should not appear in the AX tree. Note that its subviews still appear in
+  // the tree!
+  GetViewAccessibility().OverrideRole(ax::mojom::Role::kIgnored);
+
   CreateBorder();
 }
 
@@ -236,12 +242,6 @@ void MenuScrollViewContainer::OnPaintBackground(gfx::Canvas* canvas) {
       content_view_->GetMenuItem()->GetMenuController());
   GetNativeTheme()->Paint(canvas->sk_canvas(),
       NativeTheme::kMenuPopupBackground, NativeTheme::kNormal, bounds, extra);
-}
-
-void MenuScrollViewContainer::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  // Get the name from the submenu view.
-  content_view_->GetAccessibleNodeData(node_data);
-  node_data->role = ax::mojom::Role::kMenuBar;
 }
 
 void MenuScrollViewContainer::OnBoundsChanged(
