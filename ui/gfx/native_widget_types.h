1805eb8be77f00480dab0c197430d88f4dbf2640
Tom Anderson <thomasanderson@chromium.org>
2020-02-18 21:51:03 +0000

    Reland "Enable ATK and Glib on Linux/Ozone"
    This is a reland of 16c153e6ee336121658c007dc3b2cdd706b2f37e
    
    Original change's description:
    > Enable ATK and Glib on Linux/Ozone
    >
    > BUG=988094,1014934
    > R=sky
    > CC=nickdiego,msisov,adunaev
    >
    > Change-Id: I05b4bae5ccce53053ad4ae8269db849c2db9e569
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2056570
    > Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    > Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#742233}
    
    TBR=sadrul,jamiewalch
    
    Bug: 988094, 1014934
    Change-Id: Ide8b89767eddbdd6e6751db58c92d64e2d756fec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062911
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742315}

--- ui/gfx/native_widget_types.h
+++ ui/gfx/native_widget_types.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "build/build_config.h"
+#include "ui/base/buildflags.h"
 #include "ui/gfx/gfx_export.h"
 
 #if defined(OS_ANDROID)
@@ -103,7 +104,7 @@ class ViewAndroid;
 #endif
 class SkBitmap;
 
-#if defined(USE_X11)
+#if BUILDFLAG(USE_ATK)
 extern "C" {
 struct _AtkObject;
 typedef struct _AtkObject AtkObject;
@@ -206,7 +207,7 @@ typedef NSFont* NativeFont;
 typedef id NativeViewAccessible;
 #else  // Android, Linux, Chrome OS, etc.
 // Linux doesn't have a native font type.
-#if defined(USE_X11)
+#if BUILDFLAG(USE_ATK)
 typedef AtkObject* NativeViewAccessible;
 #else
 typedef struct _UnimplementedNativeViewAccessible
