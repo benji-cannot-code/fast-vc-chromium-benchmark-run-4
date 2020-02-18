2f494c9ddc8f417b4b03553a89867823d7283ecc
Yutaka Hirano <yhirano@chromium.org>
2020-02-18 08:08:45 +0000

    Stop attaching a content type for beacon with ArrayBufferView
    Bug: 1051439, 876671
    Change-Id: Ie6055aa48fc27293a89fd20d4273654553fe85cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2060362
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742069}

--- third_party/blink/renderer/core/loader/ping_loader.cc
+++ third_party/blink/renderer/core/loader/ping_loader.cc
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/loader/ping_loader.h"
 
+#include "base/feature_list.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/platform/web_url_request.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/fileapi/file.h"
@@ -144,9 +146,12 @@ class BeaconDOMArrayBufferView final : public Beacon {
         base::checked_cast<wtf_size_t>(data_->byteLengthAsSizeT()));
     request.SetHttpBody(std::move(entity_body));
 
-    // FIXME: a reasonable choice, but not in the spec; should it give a
-    // default?
-    request.SetHTTPContentType(AtomicString("application/octet-stream"));
+    if (!base::FeatureList::IsEnabled(
+            features::kSuppressContentTypeForBeaconMadeWithArrayBufferView)) {
+      // FIXME: a reasonable choice, but not in the spec; should it give a
+      // default?
+      request.SetHTTPContentType(AtomicString("application/octet-stream"));
+    }
   }
 
   const AtomicString GetContentType() const override { return g_null_atom; }
