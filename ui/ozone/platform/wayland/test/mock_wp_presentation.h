bd5677242410453a7f191eb640702f525d1763f2
Eliot Courtney <edcourtney@google.com>
2020-02-18 13:17:00 +0000

    Send gfx::PresentationFeedback::Failure() for destroyed buffers.
    This CL makes sure presentation callbacks are sent for destroyed buffers
    in WaylandBufferManagerHost. If a buffer is destroyed, we may not
    receive a presentation callback. Instead, send
    gfx::PresentationFeedback::Failure for destroyed buffers.
    
    This CL makes the following changes:
    a. Move feedback from WaylandBuffer to the presentation queue. Feedback
      is a property of presentation, not of the buffer. If it's possible
      for presentation feedback to be delayed then this fixes a potential
      bug where feedback can get lost.
    b. Send gfx::PresentationFeedback::Failure for destroyed buffers.
    c. Adds a test for this behaviour.
    
    Bug: 1051420
    Test: resizing chrome does not cause a DCHECK in OnPresentation.
    Change-Id: I596f0deceadfffe74d35845046b956869908780f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2056169
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#742126}

--- ui/ozone/platform/wayland/test/mock_wp_presentation.h
+++ ui/ozone/platform/wayland/test/mock_wp_presentation.h
@@ -31,7 +31,7 @@ class MockWpPresentation : public GlobalObject {
                     uint32_t callback));
 
   void set_presentation_callback(wl_resource* callback_resource) {
-    DCHECK(!presentation_callback_);
+    DCHECK(!presentation_callback_ || callback_resource == nullptr);
     presentation_callback_ = callback_resource;
   }
 
