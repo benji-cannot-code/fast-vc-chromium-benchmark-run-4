a41a70f6666e44001a9beb6737e458c8bf289960
Mark Cogan <marq@google.com>
2020-02-18 16:35:22 +0000

    [iOS] Create WebStateList observation registrar.
    This CL creates AllWebStateListObservationRegistrar, which can be used
    in any case when an object needs to observe all web state lists for a
    given BrowserState.
    
    This is a generalized version of the snapshot cache tab model observer,
    which this replaces.
    
    For cases where only regular or OTR browsers' WebStateLists should be
    observed, AllWebStateListObservationRegistrar has an optional param that
    can be set at creation time.
    
    To avoid circular dependencies, one target in web_state_list/ had to
    be split from the main target (hence the scattershot BUILD.gn updates).
    
    Change-Id: Ic3977b709d5b089ebf31afd0fa868067833f2249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1997306
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742172}

--- ios/chrome/browser/main/browser_list_factory.h
+++ ios/chrome/browser/main/browser_list_factory.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_MAIN_BROWSER_LIST_FACTORY_H_
 #define IOS_CHROME_BROWSER_MAIN_BROWSER_LIST_FACTORY_H_
 
-#include "base/macros.h"
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
 
@@ -23,6 +22,10 @@ class BrowserListFactory : public BrowserStateKeyedServiceFactory {
   // Getter for singleton instance.
   static BrowserListFactory* GetInstance();
 
+  // Not copyable or moveable.
+  BrowserListFactory(const BrowserListFactory&) = delete;
+  BrowserListFactory& operator=(const BrowserListFactory&) = delete;
+
  private:
   friend class base::NoDestructor<BrowserListFactory>;
 
@@ -34,7 +37,7 @@ class BrowserListFactory : public BrowserStateKeyedServiceFactory {
   web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 
-  DISALLOW_COPY_AND_ASSIGN(BrowserListFactory);
+  void BrowserStateShutdown(web::BrowserState* context) override;
 };
 
 #endif  // IOS_CHROME_BROWSER_MAIN_BROWSER_LIST_FACTORY_H_
