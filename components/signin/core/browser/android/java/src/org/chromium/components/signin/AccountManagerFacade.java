3a365bb4358a073ed223e80d9fb9fe48b0522b15
bttk <bttk@chromium.org>
2020-02-19 10:35:19 +0000

    metrics: Refactor CachedMetrics in AccountManagerFacade
    Bug: 1046181
    Change-Id: I371ad290e454e55c0f8133b4397b26a9b902e3f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2024176
    Auto-Submit: bttk <bttk@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#742548}

--- components/signin/core/browser/android/java/src/org/chromium/components/signin/AccountManagerFacade.java
+++ components/signin/core/browser/android/java/src/org/chromium/components/signin/AccountManagerFacade.java
@@ -31,7 +31,7 @@ import org.chromium.base.Log;
 import org.chromium.base.ObserverList;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.CalledByNative;
-import org.chromium.base.metrics.CachedMetrics;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.components.signin.util.PatternMatcher;
 
@@ -90,8 +90,6 @@ public class AccountManagerFacade {
     private final AtomicReference<AccountManagerResult<List<Account>>> mFilteredAccounts =
             new AtomicReference<>();
     private final CountDownLatch mPopulateAccountCacheLatch = new CountDownLatch(1);
-    private final CachedMetrics.TimesHistogramSample mPopulateAccountCacheWaitingTimeHistogram =
-            new CachedMetrics.TimesHistogramSample("Signin.AndroidPopulateAccountCacheWaitingTime");
 
     private final ArrayList<Runnable> mCallbacksWaitingForCachePopulation = new ArrayList<>();
 
@@ -307,7 +305,8 @@ public class AccountManagerFacade {
                 mPopulateAccountCacheLatch.await();
                 maybeAccounts = mFilteredAccounts.get();
                 if (ThreadUtils.runningOnUiThread()) {
-                    mPopulateAccountCacheWaitingTimeHistogram.record(
+                    RecordHistogram.recordTimesHistogram(
+                            "Signin.AndroidPopulateAccountCacheWaitingTime",
                             SystemClock.elapsedRealtime() - now);
                 }
             } catch (InterruptedException e) {
