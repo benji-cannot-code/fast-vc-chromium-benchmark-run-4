5741af554abfda646dc8b15859eda0e40e277259
Adrienne Walker <enne@chromium.org>
2020-02-19 19:44:02 +0000

    storage: Remove unused IndexedDBContext::CopyOriginData
    The last user is removed via:
    https://chromium-review.googlesource.com/c/chromium/src/+/2048032
    
    Bug: 1015214
    Change-Id: Icb7a3011ba4edfa707d2abd292a1b4b7beda3eb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2048503
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742743}

--- content/public/browser/indexed_db_context.h
+++ content/public/browser/indexed_db_context.h
@@ -19,10 +19,6 @@ namespace base {
 class SequencedTaskRunner;
 }
 
-namespace url {
-class Origin;
-}
-
 namespace content {
 
 // Represents the per-BrowserContext IndexedDB data.
@@ -36,11 +32,6 @@ class IndexedDBContext
  public:
   // Only call the below methods by posting to this IDBTaskRunner.
   virtual base::SequencedTaskRunner* IDBTaskRunner() = 0;
-
-  // Copies the indexed db files from this context to another. The
-  // indexed db directory in the destination context needs to be empty.
-  virtual void CopyOriginData(const url::Origin& origin,
-                              IndexedDBContext* dest_context) = 0;
 
  protected:
   friend class base::RefCountedDeleteOnSequence<IndexedDBContext>;
