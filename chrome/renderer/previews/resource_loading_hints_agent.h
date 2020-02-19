a363aee4c598fb0213ff360a3b121d4a1a66948c
Sergey Poromov <poromov@chromium.org>
2020-02-19 10:42:21 +0000

    [sheriff] Revert "Clear the subresource redirect hints when navigation is started"
    This reverts commit 653f075d6282c7ea33207639d5082689345465b2.
    
    Reason for revert: Consistent failures on Linux:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests
    The first failure:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests/87279
    
    Original change's description:
    > Clear the subresource redirect hints when navigation is started
    > 
    > RenderFrames could be reused for same-origin navigations. This causes
    > the hints from previous navigations to be used in next navigation
    > until the new hints fetch finishes and updates the hints. This CL clears
    > the hints.
    > 
    > Unfortunately this causes race conditions between the image fetch and
    > the hints update, which causes images to not redirected to compressed
    > versions. So the image fetches are delayed a bit to circumvent this.
    > 
    > Bug: 1051283
    > Change-Id: Ie9e5351e68c0081ab9dbfe23faed2c3c5e8e4d42
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2056207
    > Commit-Queue: rajendrant <rajendrant@chromium.org>
    > Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    > Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#742326}
    
    TBR=tbansal@chromium.org,rajendrant@chromium.org,mcrouse@chromium.org
    
    Change-Id: I74441c0600ab5fe5de3016c813590a5d3d21e447
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1051283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063170
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742550}

--- chrome/renderer/previews/resource_loading_hints_agent.h
+++ chrome/renderer/previews/resource_loading_hints_agent.h
@@ -47,9 +47,6 @@ class ResourceLoadingHintsAgent
 
  private:
   // content::RenderFrameObserver:
-  void DidStartNavigation(
-      const GURL& url,
-      base::Optional<blink::WebNavigationType> navigation_type) override;
   void DidCreateNewDocument() override;
   void OnDestruct() override;
 
@@ -67,6 +64,8 @@ class ResourceLoadingHintsAgent
 
   bool IsMainFrame() const;
 
+  bool did_create_new_document_ = false;
+
   std::vector<std::string> subresource_patterns_to_block_;
   base::Optional<int64_t> ukm_source_id_;
 
@@ -75,6 +74,7 @@ class ResourceLoadingHintsAgent
 
   subresource_redirect::SubresourceRedirectHintsAgent
       subresource_redirect_hints_agent_;
+  blink::mojom::CompressPublicImagesHintsPtr images_hints_;
 
   DISALLOW_COPY_AND_ASSIGN(ResourceLoadingHintsAgent);
 };
