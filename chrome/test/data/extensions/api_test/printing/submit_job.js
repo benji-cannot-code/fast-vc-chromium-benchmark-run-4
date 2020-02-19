eafea34c25d81da2f6b8a6159e370c516f41746a
Nikita Podguzov <nikitapodguzov@chromium.org>
2020-02-19 10:10:04 +0000

    Printing API: Add browser test for submitJob()
    Bug: 996785
    Change-Id: Icfce332a4cc763bdabed6a235224a6cea014c203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2015040
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Nikita Podguzov <nikitapodguzov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742545}

---
+++ chrome/test/data/extensions/api_test/printing/submit_job.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.getConfig(function(config) {
+  const url = 'http://localhost:' + config.testServer.port + '/pdf/test.pdf';
+  submitJob('id', 'test job', url, response => {
+    chrome.test.assertTrue(response != undefined);
+    chrome.test.assertTrue(response.status != undefined);
+    chrome.test.assertEq(chrome.printing.SubmitJobStatus.OK, response.status);
+    chrome.test.assertTrue(response.jobId != undefined);
+
+    chrome.test.notifyPass();
+  });
+});
