8b04548379d270d568062221e523e1dddd58ffd3
Pete Williamson <petewil@chromium.org>
2020-02-19 19:28:00 +0000

    Send Feedback Histogram
    Bug: 1992269
    Change-Id: I6223388011d6d54e428a3f5d0228089751351d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2047497
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742733}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/action/FeedActionHandler.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/action/FeedActionHandler.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 
 import androidx.annotation.NonNull;
 
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.feed.FeedLoggingBridge;
 import org.chromium.chrome.browser.feed.FeedOfflineIndicator;
 import org.chromium.chrome.browser.feed.library.api.client.knowncontent.ContentMetadata;
@@ -43,6 +44,13 @@ public class FeedActionHandler implements ActionApi {
     private final Activity mActivity;
     private final Profile mProfile;
     private static final String TAG = "FeedActionHandler";
+
+    // This must match the FeedSendFeedbackType enum in enums.xml.
+    public @interface FeedFeedbackType {
+        int FEEDBACK_TAPPED_ON_CARD = 0;
+        int FEEDBACK_TAPPED_ON_PAGE = 1;
+        int NUM_ENTRIES = 2;
+    }
 
     // Map key names - These will be printed with the values in the feedback report.
     public static final String CARD_URL = "CardUrl";
@@ -130,6 +138,8 @@ public class FeedActionHandler implements ActionApi {
 
     @Override
     public void sendFeedback(ContentMetadata contentMetadata) {
+        RecordHistogram.recordEnumeratedHistogram("ContentSuggestions.Feed.SendFeedback",
+                FeedFeedbackType.FEEDBACK_TAPPED_ON_CARD, FeedFeedbackType.NUM_ENTRIES);
         String feedbackContext = "mobile_browser";
         HashMap<String, String> feedContext = new HashMap<String, String>();
 
