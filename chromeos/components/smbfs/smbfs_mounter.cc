62e39fce85ee2cefdc748d54f4e7885c63418131
Anand K. Mistry <amistry@chromium.org>
2020-02-19 07:06:55 +0000

    Share the smbfs bootstrap message pipe name as a constant in the mojom
    This name is constant and needs to be used by both the browser and
    smbfs. The appropriate place for this is in the mojom file.
    
    Bug: 939235
    Change-Id: I8710ca0ddf9a0ef9fc1abaa0e1a08b9204687355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062109
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742509}

--- chromeos/components/smbfs/smbfs_mounter.cc
+++ chromeos/components/smbfs/smbfs_mounter.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace smbfs {
 
 namespace {
-constexpr char kMessagePipeName[] = "smbfs-bootstrap";
 constexpr char kMountUrlPrefix[] = "smbfs://";
 constexpr base::TimeDelta kMountTimeout = base::TimeDelta::FromSeconds(20);
 }  // namespace
@@ -94,7 +93,7 @@ void SmbFsMounter::Mount(SmbFsMounter::DoneCallback callback) {
     mojo_fd_pending_ = true;
 
     bootstrap_.Bind(mojo::PendingRemote<mojom::SmbFsBootstrap>(
-        bootstrap_invitation_.AttachMessagePipe(kMessagePipeName),
+        bootstrap_invitation_.AttachMessagePipe(mojom::kBootstrapPipeName),
         mojom::SmbFsBootstrap::Version_));
   }
   bootstrap_.set_disconnect_handler(
