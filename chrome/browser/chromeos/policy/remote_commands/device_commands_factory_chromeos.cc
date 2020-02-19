ce95ebd0ef05640823fe549a384be97daebf7b5f
Paul Moy <pmoy@chromium.org>
2020-02-19 18:38:40 +0000

    remote_commands: Add DeviceCommandRunRoutine
    Add a new device command to run a diagnostic routine
    on the device.
    
    DeviceCommandRunRoutineJobTest.*
    
    Bug: chromium:1049762
    Test: unit_tests --gtest_filter=
    Change-Id: I1234854ecc6e8d3c7a452519c865402f74bfe1e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055807
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742715}

--- chrome/browser/chromeos/policy/remote_commands/device_commands_factory_chromeos.cc
+++ chrome/browser/chromeos/policy/remote_commands/device_commands_factory_chromeos.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/remote_commands/device_command_reboot_job.h"
 #include "chrome/browser/chromeos/policy/remote_commands/device_command_refresh_machine_certificate_job.h"
 #include "chrome/browser/chromeos/policy/remote_commands/device_command_remote_powerwash_job.h"
+#include "chrome/browser/chromeos/policy/remote_commands/device_command_run_routine_job.h"
 #include "chrome/browser/chromeos/policy/remote_commands/device_command_screenshot_job.h"
 #include "chrome/browser/chromeos/policy/remote_commands/device_command_set_volume_job.h"
 #include "chrome/browser/chromeos/policy/remote_commands/device_command_start_crd_session_job.h"
@@ -58,6 +59,8 @@ DeviceCommandsFactoryChromeOS::BuildJobForType(em::RemoteCommand_Type type,
       return std::make_unique<DeviceCommandRemotePowerwashJob>(service);
     case em::RemoteCommand_Type_DEVICE_GET_AVAILABLE_DIAGNOSTIC_ROUTINES:
       return std::make_unique<DeviceCommandGetAvailableRoutinesJob>();
+    case em::RemoteCommand_Type_DEVICE_RUN_DIAGNOSTIC_ROUTINE:
+      return std::make_unique<DeviceCommandRunRoutineJob>();
     default:
       // Other types of commands should be sent to UserCommandsFactoryChromeOS
       // instead of here.
