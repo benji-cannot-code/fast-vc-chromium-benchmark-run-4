5b8f8b208a6773d66f9bc978b874b8ad130794f4
Travis Skare <skare@chromium.org>
2020-02-19 21:28:00 +0000

    [QR Generator] Move webauthn QR encoder to common/
    This is towards servicification.
    
    Bug: 1050259
    Change-Id: Ia32c670da08b2026b46b8e64030e1f2054a19a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2045136
    Commit-Queue: Travis Skare <skare@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742798}

--- chrome/browser/ui/views/webauthn/authenticator_qr_sheet_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_qr_sheet_view.cc
@@ -8,13 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64url.h"
 #include "base/rand_util.h"
 #include "base/strings/string_piece.h"
-#include "chrome/browser/ui/views/webauthn/authenticator_qr_code.h"
+#include "chrome/common/qr_code_generator/qr_code_generator.h"
 #include "device/fido/cable/cable_discovery_data.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/view.h"
 
-using QRCode = AuthenticatorQRCode;
+using QRCode = QRCodeGenerator;
 
 namespace {
 
