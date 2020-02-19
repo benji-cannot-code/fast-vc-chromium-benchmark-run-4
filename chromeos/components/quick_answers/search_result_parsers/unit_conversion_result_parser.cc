cab06d267da44ccda8866dcc238c83a79217de98
Xiao Yang <yanxiao@google.com>
2020-02-19 23:08:46 +0000

    update QuickAnswers result
    Update quick answers result returned by search_result_parsers for the
    QuickAnswersView
    
    Bug: b/149841333
    Change-Id: Icabffd2eb337b498c9d7e4376a46b48dd9717ebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2064810
    Commit-Queue: Xiao Yang <yanxiao@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742848}

--- chromeos/components/quick_answers/search_result_parsers/unit_conversion_result_parser.cc
+++ chromeos/components/quick_answers/search_result_parsers/unit_conversion_result_parser.cc
@@ -32,6 +32,9 @@ bool UnitConversionResultParser::Parse(const Value* result,
 
   quick_answer->result_type = ResultType::kUnitConversionResult;
   quick_answer->primary_answer = *dest;
+  quick_answer->first_answer_row.push_back(
+      std::make_unique<QuickAnswerText>(*dest));
+
   return true;
 }
 
