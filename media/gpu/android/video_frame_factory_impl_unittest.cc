4eb2d2b529899f90872f37d8e78d9d7c930e6b19
Jose Lopes <jabolopes@google.com>
2020-02-19 22:26:34 +0000

    Migrate notify promotion callback.
    The callback is repeating because it can be executed in the following
    locations:
    * https://cs.chromium.org/chromium/src/media/gpu/android/codec_image.cc?rcl=ab462b5f5bc23b9366999cd94ea044e61e898660&l=170
    * https://cs.chromium.org/chromium/src/media/gpu/android/codec_image.cc?rcl=ab462b5f5bc23b9366999cd94ea044e61e898660&l=176
    
    invoked by a for loop in:
    * https://cs.chromium.org/chromium/src/components/viz/service/display/overlay_processor_on_gpu.cc?rcl=ab462b5f5bc23b9366999cd94ea044e61e898660&l=56
    * https://cs.chromium.org/chromium/src/components/viz/service/display/overlay_processor_on_gpu.cc?rcl=ab462b5f5bc23b9366999cd94ea044e61e898660&l=74
    
    This is part of the base::Callback migration.
    
    Context: https://cs.chromium.org/chromium/src/docs/callback.md?rcl=9fcc3764aea8f97e9f6de4a9ee61d554e67edcda&l=40
    
    Bug: 714018
    Change-Id: Iad9fb48caa4f49a32a730a0d72881605d1dd9f32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058813
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Jose Lopes <jabolopes@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742831}

--- media/gpu/android/video_frame_factory_impl_unittest.cc
+++ media/gpu/android/video_frame_factory_impl_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/android/video_frame_factory_impl.h"
 
 #include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/single_thread_task_runner.h"
 #include "base/test/gmock_callback_support.h"
 #include "base/test/mock_callback.h"
@@ -117,8 +118,7 @@ class VideoFrameFactoryImplTest : public testing::Test {
 
     impl_->CreateVideoFrame(std::move(output_buffer), base::TimeDelta(),
                             video_frame_params_.natural_size,
-                            PromotionHintAggregator::NotifyPromotionHintCB(),
-                            output_cb_.Get());
+                            base::NullCallback(), output_cb_.Get());
     base::RunLoop().RunUntilIdle();
 
     // TODO(liberato): Verify that it requested a shared image.
@@ -210,9 +210,8 @@ TEST_F(VideoFrameFactoryImplTest, CreateVideoFrameFailsIfUnsupportedFormat) {
   EXPECT_CALL(output_cb, Run(scoped_refptr<VideoFrame>(nullptr)));
   EXPECT_CALL(*image_provider_raw_, MockRequestImage()).Times(0);
 
-  impl_->CreateVideoFrame(
-      std::move(output_buffer), base::TimeDelta(), natural_size,
-      PromotionHintAggregator::NotifyPromotionHintCB(), output_cb.Get());
+  impl_->CreateVideoFrame(std::move(output_buffer), base::TimeDelta(),
+                          natural_size, base::NullCallback(), output_cb.Get());
   base::RunLoop().RunUntilIdle();
 }
 
