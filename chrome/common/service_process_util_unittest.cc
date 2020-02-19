c4c18a63920f652de6bcb8a61106425341ec932b
Sergey Poromov <poromov@chromium.org>
2020-02-19 15:23:33 +0000

    Revert "core: re-enable service process tests"
    This reverts commit 4e60310e71f59211761761e89b036094d1d727d8.
    
    Reason for revert: [sheriff]
    The tests are still flaky:
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=unit_tests&tests=ServiceProcessStateTest.ReadyState
    https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=unit_tests&tests=ServiceProcessStateTest.ForceShutdown
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20Tests
    
    Original change's description:
    > core: re-enable service process tests
    > 
    > Two of these were disabled as flaky several years ago.
    > They have not flaked on my workstation in a few dozen runs,
    > so let's speculatively re-enable them.
    > 
    > Bug: None
    > Change-Id: Iea9a6953d71c5935a4b36f99d75bf81ae83eb464
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057286
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#741546}
    
    TBR=ellyjones@chromium.org,thakis@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: None
    Change-Id: I0c7c550223562e6ce61a797ed51a3e6081fc5981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2064249
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742635}

--- chrome/common/service_process_util_unittest.cc
+++ chrome/common/service_process_util_unittest.cc
@@ -43,6 +43,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/auto_start_linux.h"
 #endif
 
+#if defined(USE_AURA)
+// This test fails http://crbug.com/84854, and is very flaky on CrOS and
+// somewhat flaky on other Linux.
+#define MAYBE_ForceShutdown DISABLED_ForceShutdown
+#else
+#if defined(OS_LINUX) || defined(OS_WIN)
+#define MAYBE_ForceShutdown DISABLED_ForceShutdown
+#else
+#define MAYBE_ForceShutdown ForceShutdown
+#endif
+#endif
+
 namespace {
 
 bool g_good_shutdown = false;
@@ -107,7 +119,8 @@ TEST_F(ServiceProcessStateTest, Singleton) {
   LaunchAndWait("ServiceProcessStateTestSingleton");
 }
 
-TEST_F(ServiceProcessStateTest, ReadyState) {
+// http://crbug.com/396390
+TEST_F(ServiceProcessStateTest, DISABLED_ReadyState) {
   ASSERT_FALSE(CheckServiceProcessReady());
   ServiceProcessState state;
   ASSERT_TRUE(state.Initialize());
@@ -185,7 +198,7 @@ TEST_F(ServiceProcessStateTest, SharedMem) {
   ASSERT_EQ(base::GetCurrentProcId(), pid);
 }
 
-TEST_F(ServiceProcessStateTest, ForceShutdown) {
+TEST_F(ServiceProcessStateTest, MAYBE_ForceShutdown) {
   base::Process process = SpawnChild("ServiceProcessStateTestShutdown");
   ASSERT_TRUE(process.IsValid());
   for (int i = 0; !CheckServiceProcessReady() && i < 10; ++i) {
