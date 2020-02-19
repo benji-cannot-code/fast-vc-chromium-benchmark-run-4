fa77605c8b90c4248c6812a07cb03d60434ab852
Ahmed Fakhry <afakhry@google.com>
2020-02-19 21:21:55 +0000

    Desks: Fix desk bar becoming invisible when active desk is removed.
    Since https://crrev.com/c/2044644, the desk bar is an activatable
    window placed in the active desk's container. Removing the active
    desk from overview means hiding its container, and showing another
    one. The desk bar should be one of those windows that move to the
    newly-activated desk's container when its original (previously-active)
    desk is removed.
    
    BUG=1053771
    TEST=Manually, expanded an existing test.
    
    Change-Id: Id4bb2084ac0117fab4334819c9d79e40e8a565d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063810
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742793}

--- ash/wm/desks/desk.cc
+++ ash/wm/desks/desk.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/public/cpp/app_types.h"
+#include "ash/public/cpp/shell_window_ids.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/shell.h"
 #include "ash/wm/mru_window_tracker.h"
@@ -44,6 +45,12 @@ void UpdateBackdropController(aura::Window* desk_container) {
 // Returns true if |window| can be managed by the desk, and therefore can be
 // moved out of the desk when the desk is removed.
 bool CanMoveWindowOutOfDeskContainer(aura::Window* window) {
+  // The desks bar widget is an activatable window placed in the active desk's
+  // container, therefore it should be allowed to move outside of its desk when
+  // its desk is removed.
+  if (window->id() == kShellWindowId_DesksBarWindow)
+    return true;
+
   // We never move transient descendants directly, this is taken care of by
   // `wm::TransientWindowManager::OnWindowHierarchyChanged()`.
   auto* transient_root = ::wm::GetTransientRoot(window);
@@ -313,6 +320,9 @@ void Desk::MoveWindowToDesk(aura::Window* window, Desk* target_desk) {
   DCHECK(window);
   DCHECK(base::Contains(windows_, window));
   DCHECK(this != target_desk);
+  // The desks bar should not be allowed to move individually to another desk.
+  // Only as part of `MoveWindowsToDesk()` when the desk is removed.
+  DCHECK_NE(window->id(), kShellWindowId_DesksBarWindow);
 
   {
     ScopedWindowPositionerDisabler window_positioner_disabler;
