7bda3224f1c1512f539c83cce90a4cb43a4e7d15
Alison Maher <almaher@microsoft.com>
2020-02-19 20:40:36 +0000

    [LayoutNG] Fieldset fragmentation
    Add support for fieldset fragmentation. This change assumes that
    fieldset content (including the legend) is monolithic. Follow up CLs
    will handle the fragmentation for fieldset content.
    
    Tests added to NGFieldsetLayoutAlgorithmTest.
    
    Bug: 875235
    Change-Id: I08c30d1a99715d3a326b9c2064166a82385048f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057584
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742774}

--- third_party/blink/renderer/core/layout/ng/ng_base_layout_algorithm_test.cc
+++ third_party/blink/renderer/core/layout/ng/ng_base_layout_algorithm_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_block_flow.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_block_layout_algorithm.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_constraint_space_builder.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_fieldset_layout_algorithm.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_length_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
@@ -59,6 +60,22 @@ NGBaseLayoutAlgorithmTest::RunBlockLayoutAlgorithmForElement(Element* element) {
       NGBlockLayoutAlgorithm({node, fragment_geometry, space}).Layout();
   return std::make_pair(To<NGPhysicalBoxFragment>(&result->PhysicalFragment()),
                         std::move(space));
+}
+
+scoped_refptr<const NGPhysicalBoxFragment>
+NGBaseLayoutAlgorithmTest::RunFieldsetLayoutAlgorithm(
+    NGBlockNode node,
+    const NGConstraintSpace& space,
+    const NGBreakToken* break_token) {
+  NGFragmentGeometry fragment_geometry =
+      CalculateInitialFragmentGeometry(space, node);
+
+  scoped_refptr<const NGLayoutResult> result =
+      NGFieldsetLayoutAlgorithm(
+          {node, fragment_geometry, space, To<NGBlockBreakToken>(break_token)})
+          .Layout();
+
+  return To<NGPhysicalBoxFragment>(&result->PhysicalFragment());
 }
 
 scoped_refptr<const NGPhysicalBoxFragment>
