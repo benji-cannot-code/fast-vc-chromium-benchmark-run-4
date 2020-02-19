ec98f937a09485e664202fded673f8c849be452e
Peter Kotwicz <pkotwicz@chromium.org>
2020-02-19 16:28:00 +0000

    [Android Refactor] Share overview theme color computation logic
    This CL introduces
    ToolbarColors#canUseIncognitoToolbarThemeColorInOverview() which
    determines whether the incognito theme color can be used in overview
    mode. The purpose is to share code between
    AppThemeColorProvider#updateTheme() and
    StatusBarColorController#calculateBaseStatusBarColor()
    
    BUG=None
    
    Change-Id: I671a40084f670655a39395fa365bb1b64d8fec52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063872
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742658}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/AppThemeColorProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/AppThemeColorProvider.java
@@ -11,9 +11,6 @@ import org.chromium.chrome.browser.ThemeColorProvider;
 import org.chromium.chrome.browser.compositor.layouts.EmptyOverviewModeObserver;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior;
 import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior.OverviewModeObserver;
-import org.chromium.chrome.browser.device.DeviceClassManager;
-import org.chromium.chrome.browser.flags.CachedFeatureFlags;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.toolbar.IncognitoStateProvider.IncognitoStateObserver;
 import org.chromium.components.browser_ui.styles.ChromeColors;
 
@@ -82,13 +79,9 @@ public class AppThemeColorProvider extends ThemeColorProvider implements Incogni
     }
 
     private void updateTheme() {
-        final boolean isAccessibilityEnabled = DeviceClassManager.enableAccessibilityLayout();
-        final boolean isHorizontalTabSwitcherEnabled =
-                ChromeFeatureList.isEnabled(ChromeFeatureList.HORIZONTAL_TAB_SWITCHER_ANDROID);
-        final boolean isTabGridEnabled = CachedFeatureFlags.isGridTabSwitcherEnabled();
         final boolean shouldUseIncognitoBackground = mIsIncognito
-                && (isAccessibilityEnabled || isHorizontalTabSwitcherEnabled || isTabGridEnabled
-                        || !mIsOverviewVisible);
+                && (!mIsOverviewVisible
+                        || ToolbarColors.canUseIncognitoToolbarThemeColorInOverview());
 
         updatePrimaryColor(
                 shouldUseIncognitoBackground ? mIncognitoPrimaryColor : mStandardPrimaryColor,
