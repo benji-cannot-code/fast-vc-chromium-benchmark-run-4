254fc578baedd4eae13e2e4114dfaab1fb629d56
Rachel Carpenter <carpenterr@chromium.org>
2020-02-19 00:40:21 +0000

    Open V2 Help App post OOBE.
    This happens if the feature is enabled, else fall back to opening the
    genius app.
    
    Also record the app launch for new Help app.
    
    Bug: b/138970611
    Change-Id: I50f2dba79f62d979c43ab6d9d3e8382d8892b9b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2054476
    Commit-Queue: Rachel Carpenter <carpenterr@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742406}

--- chrome/browser/apps/app_service/app_service_metrics.cc
+++ chrome/browser/apps/app_service/app_service_metrics.cc
@@ -33,9 +33,10 @@ enum class DefaultAppName {
   kAudioPlayer = 15,
   kChromeCanvas = 16,
   kCamera = 17,
+  kHelpApp = 18,
   // Add any new values above this one, and update kMaxValue to the highest
   // enumerator value.
-  kMaxValue = kCamera,
+  kMaxValue = kHelpApp,
 };
 
 void RecordDefaultAppLaunch(DefaultAppName default_app_name,
@@ -136,6 +137,8 @@ void RecordAppLaunch(const std::string& app_id,
     RecordDefaultAppLaunch(DefaultAppName::kChromeCanvas, launch_source);
   else if (app_id == extension_misc::kCameraAppId)
     RecordDefaultAppLaunch(DefaultAppName::kCamera, launch_source);
+  else if (app_id == chromeos::default_web_apps::kHelpAppId)
+    RecordDefaultAppLaunch(DefaultAppName::kHelpApp, launch_source);
 
   // Above are default Essential apps; below are built-in apps.
 
