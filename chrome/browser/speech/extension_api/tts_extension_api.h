f12e7f53c18edd742ee880326775bc926929480f
Anand K. Mistry <amistry@chromium.org>
2020-02-19 23:40:13 +0000

    [Extensions Functions] Migrate tts.speak() to ExtensionFunction
    Bug: 634140
    Change-Id: I8f539e99b1a48bdff96e1d100be4e2cdb8ea9be0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2054470
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742864}

--- chrome/browser/speech/extension_api/tts_extension_api.h
+++ chrome/browser/speech/extension_api/tts_extension_api.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "chrome/browser/extensions/chrome_extension_function.h"
 #include "content/public/browser/tts_controller.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
+#include "extensions/browser/extension_function.h"
 
 namespace content {
 class BrowserContext;
@@ -21,10 +21,10 @@ content::TtsEventType TtsEventTypeFromString(const std::string& str);
 
 namespace extensions {
 
-class TtsSpeakFunction : public ChromeAsyncExtensionFunction {
+class TtsSpeakFunction : public ExtensionFunction {
  private:
   ~TtsSpeakFunction() override {}
-  bool RunAsync() override;
+  ResponseAction Run() override;
   DECLARE_EXTENSION_FUNCTION("tts.speak", TTS_SPEAK)
 };
 
