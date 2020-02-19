b9d881a6774b637b98bb7ba99268f69e9ef700b5
Victor Costan <pwnall@chromium.org>
2020-02-19 04:51:39 +0000

    AppCache: Replace AppCacheUpdateJob::IsEmptyTime() with Time::is_null().
    This CL is a follow-up from the review on https://crrev.com/c/2052251.
    
    Bug: 989611
    Change-Id: I251fc8bb4cda138bc0bc94701efc09dc9553650f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058008
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742488}

--- content/browser/appcache/appcache_update_metrics_recorder.cc
+++ content/browser/appcache/appcache_update_metrics_recorder.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-AppCacheUpdateMetricsRecorder::AppCacheUpdateMetricsRecorder() {}
+AppCacheUpdateMetricsRecorder::AppCacheUpdateMetricsRecorder() = default;
 
 void AppCacheUpdateMetricsRecorder::IncrementExistingCorruptionFixedInUpdate() {
 #if DCHECK_IS_ON()
