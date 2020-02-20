8e38b19b3345d3b868524b3ff1ca8c127f433228
Xiaohan Wang <xhwang@chromium.org>
2020-02-20 03:50:17 +0000

    media: Remove LogDXVAError() from mf_helpers.h
    LogDXVAError() reports UMAs that are specific to one single file
    media/gpu/windows/dxva_video_decode_accelerator_win.cc. So it should not
    be in media/base/win/mf_helpers.h which is a common helper for all
    Windows media code.
    
    This CL moves LogDXVAError() and related macros to
    dxva_video_decode_accelerator_win.cc to solve this issue. In the future,
    we should consolidate on fewer macros.
    
    Change-Id: Icea7f0f8c5710bf034fce3dc32c36ccf2cc8e782
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2066057
    Reviewed-by: Ted Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742959}

--- media/base/win/mf_helpers.cc
+++ media/base/win/mf_helpers.cc
@@ -5,17 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/win/mf_helpers.h"
 
-#include "base/metrics/histogram_functions.h"
-
 namespace media {
 
 namespace mf {
-
-void LogDXVAError(int line) {
-  LOG(ERROR) << "Error in dxva_video_decode_accelerator_win.cc on line "
-             << line;
-  base::UmaHistogramSparse("Media.DXVAVDA.ErrorLine", line);
-}
 
 Microsoft::WRL::ComPtr<IMFSample> CreateEmptySampleWithBuffer(
     uint32_t buffer_length,
