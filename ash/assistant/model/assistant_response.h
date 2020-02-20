c15687dbe41c60042389fd432ed1992d0de98a69
David Black <dmblack@google.com>
2020-02-20 19:49:11 +0000

    Modifies AssistantInteractionController for response processing v2.
    In response processing v2, the pending response will be committed at
    the earliest opportunity so that it can begin rendering.
    
    This CL should *not* affect response processing v1.
    
    Follow up CLs will make additional changes to processing logic and
    rendering logic to accommodate this new streaming model.
    
    Bug: b:129411551
    Change-Id: I86c22929eb24945f104a06fbbe5ae0b1468caa5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063264
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743211}

--- ash/assistant/model/assistant_response.h
+++ ash/assistant/model/assistant_response.h
@@ -22,6 +22,7 @@ namespace ash {
 class AssistantResponseObserver;
 class AssistantUiElement;
 
+// TODO(dmblack): Remove ProcessingState after launch of response processing v2.
 // Models a renderable Assistant response.
 // It is refcounted so that views that display the response can safely
 // reference the data inside this response.
