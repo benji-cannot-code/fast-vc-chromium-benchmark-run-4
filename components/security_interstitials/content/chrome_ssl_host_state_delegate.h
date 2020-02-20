d3bf652ca95e199bdd68af74679f00825d25ca41
Colin Blundell <blundell@chromium.org>
2020-02-20 09:04:50 +0000

    [SSL] Simplify //chrome's ChromeSSLHostStateDelegateFactory
    In advance of bringing up a factory for ChromeSSLHostStateDelegate in
    //weblayer, this CL simplifies the one in //chrome by making
    ChromeSSLHostStateDelegate a KeyedService directly rather than having
    a wrapper class inside the factory. This avoids the need to replicate
    the boilerplate in //weblayer's factory.
    
    Bug: 1030692
    Change-Id: I5732f70c5180261c9bbbc385e38da623e01cab86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063172
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743017}

--- components/security_interstitials/content/chrome_ssl_host_state_delegate.h
+++ components/security_interstitials/content/chrome_ssl_host_state_delegate.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/macros.h"
 #include "base/time/time.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/ssl_host_state_delegate.h"
 
 class HostContentSettingsMap;
@@ -35,7 +36,8 @@ class PrefRegistrySyncable;
 //   of time depending on experimental groups)
 // - mixed content exceptions
 // - when errors have recurred multiple times
-class ChromeSSLHostStateDelegate : public content::SSLHostStateDelegate {
+class ChromeSSLHostStateDelegate : public content::SSLHostStateDelegate,
+                                   public KeyedService {
  public:
   enum RecurrentInterstitialMode { PREF, IN_MEMORY, NOT_SET };
 
