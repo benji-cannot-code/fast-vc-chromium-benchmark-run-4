b56fe19769c7fdde3ed018db3fb63deee26be411
Oksana Zhuravlova <oksamyt@chromium.org>
2020-02-20 20:14:19 +0000

    Create contextual search renderer side if build flag set
    This change adds a condition so that the renderer part of the contextual
    search code is only created when the corresponding build flag is defined.
    It also updates the browser-side ContextualSearchJsApiService interface
    binder registration depend on the same build flag rather than the more
    generic OS_ANDROID condition.
    
    Bug: 1047680
    Change-Id: Ia0c88558736b028b96b0092acb6dde4a7a526b87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055992
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743223}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/omnibox/omnibox_ui.h"
 #include "chrome/browser/ui/webui/usb_internals/usb_internals.mojom.h"
 #include "chrome/browser/ui/webui/usb_internals/usb_internals_ui.h"
+#include "components/contextual_search/buildflags.h"
 #include "components/dom_distiller/content/browser/distillability_driver.h"
 #include "components/dom_distiller/content/browser/distiller_javascript_service_impl.h"
 #include "components/dom_distiller/content/common/mojom/distillability_service.mojom.h"
@@ -167,7 +168,7 @@ void BindUnhandledTapWebContentsObserver(
 }
 #endif  // BUILDFLAG(ENABLE_UNHANDLED_TAP)
 
-#if defined(OS_ANDROID)
+#if BUILDFLAG(BUILD_CONTEXTUAL_SEARCH)
 void BindContextualSearchObserver(
     content::RenderFrameHost* const host,
     mojo::PendingReceiver<
@@ -185,7 +186,7 @@ void BindContextualSearchObserver(
         contextual_search_observer->api_handler(), std::move(receiver));
   }
 }
-#endif
+#endif  // BUILDFLAG(BUILD_CONTEXTUAL_SEARCH)
 
 // Forward image Annotator requests to the profile's AccessibilityLabelsService.
 void BindImageAnnotator(
@@ -366,6 +367,7 @@ void PopulateChromeFrameBinders(
   map->Add<blink::mojom::ShareService>(base::BindRepeating(
       &ForwardToJavaWebContents<blink::mojom::ShareService>));
 
+#if BUILDFLAG(BUILD_CONTEXTUAL_SEARCH)
   map->Add<contextual_search::mojom::ContextualSearchJsApiService>(
       base::BindRepeating(&BindContextualSearchObserver));
 
@@ -373,6 +375,7 @@ void PopulateChromeFrameBinders(
   map->Add<blink::mojom::UnhandledTapNotifier>(
       base::BindRepeating(&BindUnhandledTapWebContentsObserver));
 #endif  // BUILDFLAG(ENABLE_UNHANDLED_TAP)
+#endif  // BUILDFLAG(BUILD_CONTEXTUAL_SEARCH)
 
 #if defined(ENABLE_SPATIAL_NAVIGATION_HOST)
   map->Add<blink::mojom::SpatialNavigationHost>(base::BindRepeating(
