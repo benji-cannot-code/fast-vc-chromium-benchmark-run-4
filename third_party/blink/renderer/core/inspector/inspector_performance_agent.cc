8d1e972adbed59fbc707502c2ec34aed832ae077
Peter Kvitek <kvitekp@chromium.org>
2020-02-20 00:40:52 +0000

    Added CaptureSnapshotDuration metric to Performance.getMetrics.
    This metric tracks time spent in DOMSnapshot::captureSnapshot calls.
    
    Drive by: subtracted v8 compilation duration from tasks duration
    when calculating other tasks duration. This ensures correct stacking
    known activities.
    
    Change-Id: I79c6926d516c02717b68ea426e3e99a7d9fd665e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062691
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742888}

--- third_party/blink/renderer/core/inspector/inspector_performance_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_performance_agent.cc
@@ -58,6 +58,7 @@ void InspectorPerformanceAgent::InnerEnable() {
   task_start_ticks_ = base::TimeTicks();
   script_start_ticks_ = base::TimeTicks();
   v8compile_start_ticks_ = base::TimeTicks();
+  capture_snapshot_start_ticks_ = base::TimeTicks();
   thread_time_origin_ = GetThreadTimeNow();
 }
 
@@ -154,6 +155,8 @@ Response InspectorPerformanceAgent::getMetrics(
   AppendMetric(result.get(), "LayoutDuration", layout_duration_.InSecondsF());
   AppendMetric(result.get(), "RecalcStyleDuration",
                recalc_style_duration_.InSecondsF());
+  AppendMetric(result.get(), "CaptureSnapshotDuration",
+               capture_snapshot_duration_.InSecondsF());
 
   base::TimeDelta script_duration = script_duration_;
   if (!script_start_ticks_.is_null())
@@ -174,7 +177,8 @@ Response InspectorPerformanceAgent::getMetrics(
   // Compute task time not accounted for by other metrics.
   base::TimeDelta other_tasks_duration =
       task_duration -
-      (script_duration + recalc_style_duration_ + layout_duration_);
+      (script_duration + v8compile_duration + recalc_style_duration_ +
+       layout_duration_ + capture_snapshot_duration_);
   AppendMetric(result.get(), "TaskOtherDuration",
                other_tasks_duration.InSecondsF());
 
@@ -308,6 +312,18 @@ void InspectorPerformanceAgent::DidProcessTask(base::TimeTicks start_time,
   if (!task_start_ticks_.is_null())
     task_duration_ += GetTimeTicksNow() - task_start_ticks_;
   task_start_ticks_ = base::TimeTicks();
+}
+
+void InspectorPerformanceAgent::WillCaptureSnapshot() {
+  capture_snapshot_start_ticks_ = GetTimeTicksNow();
+}
+
+void InspectorPerformanceAgent::DidCaptureSnapshot() {
+  if (!capture_snapshot_start_ticks_.is_null()) {
+    capture_snapshot_duration_ +=
+        GetTimeTicksNow() - capture_snapshot_start_ticks_;
+  }
+  capture_snapshot_start_ticks_ = base::TimeTicks();
 }
 
 void InspectorPerformanceAgent::Trace(Visitor* visitor) {
