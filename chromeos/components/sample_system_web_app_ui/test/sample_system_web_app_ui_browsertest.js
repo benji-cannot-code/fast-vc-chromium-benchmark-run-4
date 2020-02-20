294d41a6f6c96654046b44131313dcc221efe392
jessing <jessing@google.com>
2020-02-20 06:59:03 +0000

    [system-web-apps] Add chrome-untrusted iframe to sample system web app
    An example of how to embed a chrome-untrusted iframe into a system web
    app.
    
    To do so, we need to add abstract methods in WebUI despite WebUIImpl
    being the only class to actually implement it. This is so the
    WebUIController can add requestable schemes so the WebUI can request
    the chrome-untrusted resource.
    
    Bug: 1048951
    Change-Id: Ia3152e21412845cf421df74bb00a16e0a8f264b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037186
    Commit-Queue: Jessica Huang <jessing@google.com>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Auto-Submit: Jessica Huang <jessing@google.com>
    Cr-Commit-Position: refs/heads/master@{#742999}

--- chromeos/components/sample_system_web_app_ui/test/sample_system_web_app_ui_browsertest.js
+++ chromeos/components/sample_system_web_app_ui/test/sample_system_web_app_ui_browsertest.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 const HOST_ORIGIN = 'chrome://sample-system-web-app';
+const UNTRUSTED_HOST_ORIGIN = 'chrome-untrusted://sample-system-web-app';
 
 var SampleSystemWebAppUIBrowserTest = class extends testing.Test {
   /** @override */
@@ -24,8 +25,40 @@ var SampleSystemWebAppUIBrowserTest = class extends testing.Test {
 // Tests that chrome://sample-system-web-app runs js file and that it goes
 // somewhere instead of 404ing or crashing.
 TEST_F('SampleSystemWebAppUIBrowserTest', 'HasChromeSchemeURL', () => {
-  const header = document.querySelector('h1');
+  const header = document.querySelector('header');
 
   assertEquals(header.innerText, 'Sample System Web App');
   assertEquals(document.location.origin, HOST_ORIGIN);
 });
+
+var SampleSystemWebAppUIUntrustedBrowserTest = class extends testing.Test {
+  /** @override */
+  get browsePreload() {
+    return HOST_ORIGIN + '/sandbox.html';
+  }
+
+  /** @override */
+  get runAccessibilityChecks() {
+    return false;
+  }
+
+  /** @override */
+  get isAsync() {
+    return true;
+  }
+};
+
+// Tests that chrome://sample-system-web-app/sandbox.html embeds a
+// chrome-untrusted:// iframe
+TEST_F(
+    'SampleSystemWebAppUIUntrustedBrowserTest', 'HasChromeUntrustedIframe',
+    () => {
+      const iframe = document.querySelector('iframe');
+      window.onmessage =
+          (event) => {
+            assertEquals(event.origin, UNTRUSTED_HOST_ORIGIN);
+            assertEquals(event.data.success, true);
+            testDone();
+          };
+          iframe.contentWindow.postMessage('hello', UNTRUSTED_HOST_ORIGIN);
+    });
