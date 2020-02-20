8c336f16a1ae41476c01a8da29a75f8abc4f58b6
Ahmed Mehfooz <amehfooz@chromium.org>
2020-02-20 00:18:25 +0000

    Re-enable elide for notification buttons
    The elide for notification buttons was disabled to fix a
    relatively smaller bug. Need it enabled for
    correct functionality with longer labels.
    
    Bug: 1043643
    Change-Id: Ia38f5586d636e1e06033ed468275d2dfe28bf6ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2065549
    Reviewed-by: Tim Song <tengs@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#742878}

--- ui/message_center/views/notification_view_md.cc
+++ ui/message_center/views/notification_view_md.cc
@@ -341,7 +341,6 @@ NotificationButtonMD::NotificationButtonMD(
   set_ink_drop_base_color(SK_ColorBLACK);
   set_ink_drop_visible_opacity(kActionButtonInkDropRippleVisibleOpacity);
   SetEnabledTextColors(kActionButtonTextColor);
-  SetElideBehavior(gfx::NO_ELIDE);
   SetBorder(views::CreateEmptyBorder(kActionButtonPadding));
   SetMinSize(kActionButtonMinSize);
   SetFocusForPlatform();
