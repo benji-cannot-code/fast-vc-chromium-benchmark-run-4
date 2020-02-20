6de7db6c3795ee1eb794c7483941f5fa2aec458f
Sergey Poromov <poromov@chromium.org>
2020-02-20 12:52:42 +0000

    [sheriff] Revert "Skip 3A for still capture if ZSL is enabled"
    This reverts commit e0723ae5e2b23aae6fc0766b4fec2f7ef0e29c13.
    
    Reason for revert: "capture_unittests" failure:
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg/17155
    
    Original change's description:
    > Skip 3A for still capture if ZSL is enabled
    > 
    > This CL skips the wait for 3A-convergence (AE, AF, AWB) when
    > Zero-Shutter Lag (ZSL) is enabled. When configuring streams, camera HAL
    > adapter would add a private usage flag to the configured still capture
    > streams. Video Capture Device (VCD) would then use this private usage
    > flag to determine if ZSL is enabled. If ZSL is enabled, we skip 3A when
    > trying to construct a capture request.
    > 
    > BUG=b:130851309
    > TEST=Tested on kukui and verified VCD is able to get ZSL availability
    > through the private usage flag.
    > 
    > Change-Id: I751a432b099fb1e012fbd65e54f57901d508225c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1919530
    > Commit-Queue: Jasmine Chen <lnishan@google.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Shik Chen <shik@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#743048}
    
    TBR=dcheng@chromium.org,shik@chromium.org,hywu@chromium.org,lnishan@google.com,wtlee@chromium.org
    
    Change-Id: I5386db19de0e1a4e93828c2ab81a53f51ee7a08d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b:130851309
    Bug: 1054385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2066618
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743068}

--- media/capture/video/chromeos/camera_3a_controller.cc
+++ media/capture/video/chromeos/camera_3a_controller.cc
@@ -182,7 +182,7 @@ void Camera3AController::Stabilize3AForStillCapture(
     return;
   }
 
-  if (Is3AStabilized() || zero_shutter_lag_enabled_) {
+  if (Is3AStabilized()) {
     std::move(on_3a_stabilized_callback).Run();
     return;
   }
@@ -500,10 +500,6 @@ void Camera3AController::SetPointOfInterestUnlockAe() {
 
   ae_locked_for_point_of_interest_ = false;
   ClearRepeatingCaptureMetadata();
-}
-
-void Camera3AController::UpdateZeroShutterLagAvailability(bool enabled) {
-  zero_shutter_lag_enabled_ = enabled;
 }
 
 base::WeakPtr<Camera3AController> Camera3AController::GetWeakPtr() {
