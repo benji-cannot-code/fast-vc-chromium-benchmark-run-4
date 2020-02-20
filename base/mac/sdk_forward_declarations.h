5b97be4e5451339582454ecbc707b2f27b5e9e64
Avi Drissman <avi@chromium.org>
2020-02-20 20:12:15 +0000

    One last tweak to the commentary in sdk_forward_declarations.
    Bug: none
    Change-Id: I355076b24666e0b17fe56ca52c7c878e0f1975fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2065854
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743221}

--- base/mac/sdk_forward_declarations.h
+++ base/mac/sdk_forward_declarations.h
@@ -4,10 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // This file contains forward declarations for items in later SDKs than the
-// default one with which Chromium is built (currently 10.10).
-// If you call any function from this header, be sure to check at runtime for
-// respondsToSelector: before calling these functions (else your code will crash
-// on older OS X versions that chrome still supports).
+// default one with which Chromium is built.
 
 #ifndef BASE_MAC_SDK_FORWARD_DECLARATIONS_H_
 #define BASE_MAC_SDK_FORWARD_DECLARATIONS_H_
@@ -30,7 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   b. Include all API_AVAILABLE/NS_CLASS_AVAILABLE_MAC annotations
 //   c. Optionally import frameworks
 // 2. In your source file:
-//   a. Correctly use @available to annotate availability
+//   a. Correctly annotate availability with @available/__builtin_available/
+//      API_AVAILABLE
 //
 // This way, when the SDK is rolled, the section full of definitions
 // corresponding to it can be easily deleted.
