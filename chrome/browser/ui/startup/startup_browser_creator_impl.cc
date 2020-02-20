a48f668d8b4a83aee72d3e62f6c556d4b194980c
Robert Kaplow <rkaplow@chromium.org>
2020-02-20 18:52:29 +0000

    Deprecate some unused metrics and extend one.
    Bug: 1053153, 1037311, 1037121
    Change-Id: Ia5347d3279f2ab52cf21591f7ac9b8ac30321cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062939
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Cait Phillips <caitkp@chromium.org>
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743179}

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/case_conversion.h"
 #include "base/location.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
@@ -366,10 +365,6 @@ StartupBrowserCreatorImpl::~StartupBrowserCreatorImpl() {
 bool StartupBrowserCreatorImpl::Launch(Profile* profile,
                                        const std::vector<GURL>& urls_to_open,
                                        bool process_startup) {
-  UMA_HISTOGRAM_COUNTS_100(
-      "Startup.BrowserLaunchURLCount",
-      static_cast<base::HistogramBase::Sample>(urls_to_open.size()));
-
   DCHECK(profile);
   profile_ = profile;
 
