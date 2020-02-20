67514b939f2e171b5c013b98fb23eaa4484a3401
Luciano Pacheco <lucmult@chromium.org>
2020-02-20 08:34:25 +0000

    Files app: Change directory to update expand icon instead of full rescan
    Change TreeItem classes to avoid doing a full rescan just to work out
    if we should show the expand icon or not. Instead, only scan down to
    the first sub-directory using the new method updateExpandIcon().
    
    Replace calls to updateSubDirectories() with updateExpandIcon() where
    the content of sub-directories aren't needed. The most important call
    replaced is in SubDirectoryItem constructor, because this caused
    scanning of an additional level down from the folder that user was
    interacting with.
    
    This significantly improves the performance of navigating on folders
    that have large sub-folders. For example, the time to run integration
    test directoryTreeExpandFolder() cut down from ~40 secs to ~7 secs,
    where the most of time is spent expanding a sub-folder that contains
    1k children.
    
    Test: browser_tests --gtest_filter="DirectoryTree/FilesAppBrowserTest.Test/directoryTreeExpandFolder"
    Bug: 1039820
    Change-Id: Ib629601f2c795a6dedd80a04f85827fd33e96af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2059729
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743011}

--- ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
+++ ui/file_manager/file_manager/foreground/js/ui/directory_tree.js
@@ -238,6 +238,12 @@ class TreeItem extends cr.ui.TreeItem {
   get labelElement() {
     return this.rowElement.querySelector('.label');
   }
+
+  /**
+   * Updates the expand icon. Defaults to doing nothing for FakeItem and
+   * ShortcutItem that don't have children, thus don't need expand icon.
+   */
+  updateExpandIcon() {}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -402,6 +408,7 @@ class DirectoryItem extends TreeItem {
     let index = 0;
     const tree = this.parentTree_;
     let item;
+
     while (this.entries_[index]) {
       const currentEntry = this.entries_[index];
       const currentElement = this.items[index];
@@ -409,6 +416,7 @@ class DirectoryItem extends TreeItem {
                         tree.volumeManager_.getLocationInfo(currentEntry),
                         currentEntry) ||
           '';
+
 
       if (index >= this.items.length) {
         // If currentEntry carries its navigationModel we generate an item
@@ -432,7 +440,7 @@ class DirectoryItem extends TreeItem {
             // Show the expander even without knowing if there are children.
             currentElement.mayHaveChildren_ = true;
           } else {
-            currentElement.updateSubDirectories(true /* recursive */);
+            currentElement.updateExpandIcon();
           }
         }
         index++;
@@ -650,6 +658,42 @@ class DirectoryItem extends TreeItem {
   }
 
   /**
+   * Updates expand icon.
+   * @override
+   */
+  updateExpandIcon() {
+    if (!this.entry || this.entry.createReader === undefined) {
+      this.hasChildren = false;
+      return;
+    }
+
+    const reader = this.entry.createReader();
+
+    const readEntry = () => {
+      reader.readEntries((results) => {
+        if (!results.length) {
+          // Reached the end without any directory;
+          this.hasChildren = false;
+          return;
+        }
+
+        for (let i = 0; i < results.length; i++) {
+          if (results[i].isDirectory) {
+            // Once the first directory is found we can stop reading.
+            this.hasChildren = true;
+            return;
+          }
+        }
+
+        // Read next batch of entries.
+        readEntry();
+      });
+    };
+
+    readEntry();
+  }
+
+  /**
    * Searches for the changed directory in the current subtree, and if it is
    * found then updates it.
    *
@@ -843,7 +887,7 @@ class SubDirectoryItem extends DirectoryItem {
 
     // Update children now if needed.
     if (parentDirItem.expanded) {
-      this.updateSubDirectories(false /* recursive */);
+      this.updateExpandIcon();
     }
   }
 
@@ -1276,7 +1320,7 @@ class DriveVolumeItem extends VolumeItem {
           const item = new SubDirectoryItem(
               label, sharedDriveGrandRoot, this, this.parentTree_);
           this.addAt(item, 1);
-          item.updateSubDirectories(false);
+          item.updateExpandIcon();
           resolve(item);
           return;
         } else {
@@ -1346,7 +1390,7 @@ class DriveVolumeItem extends VolumeItem {
           // index to place "Computers" at.
           const position = this.computersIndexPosition_();
           this.addAt(item, position);
-          item.updateSubDirectories(false);
+          item.updateExpandIcon();
           resolve(item);
           return;
         } else {
