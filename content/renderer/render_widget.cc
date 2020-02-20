011a69538fe0a4042ae5940b31125b6d9acfb9c8
Chris Blume <cblume@chromium.org>
2020-02-20 21:45:18 +0000

    Set prefers_raster_in_srgb on Android
    Previously, in [1] prefers_raster_in_srgb was set to false by default
    and never enabled. The plan was to launch this first disabled, then
    on Android when the conditions were right, and later expand to other
    platforms.
    
    This CL expands prefers_raster_in_srgb to Android.
    
    [1]: https://chromium-review.googlesource.com/c/chromium/src/+/2045053
    
    Bug: 955158
    Change-Id: Iba9654cb57b71294b2018eef483356f99616c379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055410
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743266}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -123,6 +123,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include <android/keycodes.h>
 #include "base/time/time.h"
+#include "components/viz/common/viz_utils.h"
 #endif
 
 #if defined(OS_POSIX)
@@ -2799,6 +2800,9 @@ cc::LayerTreeSettings RenderWidget::GenerateLayerTreeSettings(
       !cmd.HasSwitch(cc::switches::kDisableCheckerImaging) && is_threaded;
 
 #if defined(OS_ANDROID)
+  if (!viz::AlwaysUseWideColorGamut())
+    settings.prefer_raster_in_srgb = true;
+
   // We can use a more aggressive limit on Android since decodes tend to take
   // longer on these devices.
   settings.min_image_bytes_to_checker = 512 * 1024;  // 512kB
