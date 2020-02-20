d3aed3ec9d1a37914aaecb4601db96f899790c62
Henrique Nakashima <hnakashima@chromium.org>
2020-02-20 23:44:39 +0000

    ChromeFeatureList meta tests demonstrating its behavior in unit tests.
    Bug: 1053463
    Change-Id: I3fd46315c971c6377e194f956b769950b9129d1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2064183
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743325}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -200,6 +200,8 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kTabReparenting,
     &kTabSwitcherOnReturn,
     &kTabToGTSAnimation,
+    &kTestDefaultDisabled,
+    &kTestDefaultEnabled,
     &kTrustedWebActivityPostMessage,
     &kStartSurfaceAndroid,
     &kUmaBackgroundSessions,
@@ -591,6 +593,12 @@ const base::Feature kTabSwitcherOnReturn{"TabSwitcherOnReturn",
 
 const base::Feature kTabToGTSAnimation{"TabToGTSAnimation",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kTestDefaultDisabled{"TestDefaultDisabled",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kTestDefaultEnabled{"TestDefaultEnabled",
+                                        base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kTrustedWebActivityPostMessage{
     "TrustedWebActivityPostMessage", base::FEATURE_DISABLED_BY_DEFAULT};
