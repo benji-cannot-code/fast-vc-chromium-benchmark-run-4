17c8b2eec1f3f9dc2d4b0abd854ecc13738323fe
Jonah Chin <jochin@microsoft.com>
2020-02-20 07:13:37 +0000

    Add a PaintRecorder to CanvasResourceProvider
    This CL is the first step in creating a PaintRecord backed resource
    provider for canvas OOP-R.
    
    Changes of ownership:
    - PaintRecorder entirely moved from Canvas2DLayerBridge to
      CanvasResourceProvider.
    - Management of needs_flush_ for MemoryManagedPaintRecorder moved to
      CanvasResourceProvider.
    - RestoreCanvasMatrixClipStack logic centralized to CanvasResourceHost
      and can be accessed via callback
    - Backing SkiaPaintCanvas now private to CanvasResourceProvider with no
      external access. Any users of the backing SkiaPaintCanvas in
      Canvas2DLayerBridge had their functionality moved to
      CanvasResourceProvider (ie. new RestoreBackBuffer() function)
    
    Updates to test files:
    - Addition of explicit FlushCanvas() calls in tests that used to expect
      SkiaPaintCanvas to immediately update.
    - Some tests expected the CanvasResourceProvider to be created on first
      draw. This expectation has been changed since the
      CanvasResourceProvider now gets set up on SetCanvasResourceHost().
    
    Other new functionality:
    - Display Item List now has a bit that tracks if it contains any draw
      operations. This is used to see if there are draw ops to flush.
    
    Bug: 1019288
    Change-Id: I717b18e22d6699dc876d8f8121a25d147738579d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1891292
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743002}

--- cc/paint/display_item_list.h
+++ cc/paint/display_item_list.h
@@ -88,6 +88,8 @@ class CC_PAINT_EXPORT DisplayItemList
     if (usage_hint_ == kTopLevelDisplayItemList)
       offsets_.push_back(offset);
     const T* op = paint_op_buffer_.push<T>(std::forward<Args>(args)...);
+    if (op->IsDrawOp())
+      has_draw_ops_ = true;
     DCHECK(op->IsValid());
     return offset;
   }
@@ -198,6 +200,7 @@ class CC_PAINT_EXPORT DisplayItemList
                              int max_ops_to_analyze = 1);
 
   std::string ToString() const;
+  bool has_draw_ops() const { return has_draw_ops_; }
 
  private:
   friend class DisplayItemListTest;
@@ -247,6 +250,7 @@ class CC_PAINT_EXPORT DisplayItemList
 #endif
 
   UsageHint usage_hint_;
+  bool has_draw_ops_ = false;
 
   friend class base::RefCountedThreadSafe<DisplayItemList>;
   FRIEND_TEST_ALL_PREFIXES(DisplayItemListTest, BytesUsed);
