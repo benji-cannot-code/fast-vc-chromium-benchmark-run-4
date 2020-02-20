3c79d3b5b2b1f1b7fd39890172001de53cfe374c
Allen Bauer <kylixrd@chromium.org>
2020-02-20 18:51:24 +0000

    Prefer AddChildView<T> with std::unique_ptr<>
    Bug: 648382
    Change-Id: I4db6eb8cfd081c1dde3998db2f1264f0adf42cd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2065350
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743177}

--- ash/assistant/ui/dialog_plate/mic_view.cc
+++ ash/assistant/ui/dialog_plate/mic_view.cc
@@ -58,9 +58,8 @@ void MicView::InitLayout() {
   SetLayoutManager(std::make_unique<views::FillLayout>());
 
   // Logo view container.
-  views::View* logo_view_container = new views::View();
+  auto logo_view_container = std::make_unique<views::View>();
   logo_view_container->set_can_process_events_within_subtree(false);
-  AddChildView(logo_view_container);
 
   views::BoxLayout* layout_manager =
       logo_view_container->SetLayoutManager(std::make_unique<views::BoxLayout>(
@@ -75,6 +74,8 @@ void MicView::InitLayout() {
   // Logo view.
   logo_view_ = logo_view_container->AddChildView(LogoView::Create());
   logo_view_->SetPreferredSize(gfx::Size(kIconSizeDip, kIconSizeDip));
+
+  AddChildView(std::move(logo_view_container));
 
   // Initialize state.
   UpdateState(/*animate=*/false);
