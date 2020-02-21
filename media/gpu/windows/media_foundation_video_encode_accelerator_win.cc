cf17a01f6cddad16054909c123e14b6ad0e240b2
Xiaohan Wang <xhwang@chromium.org>
2020-02-21 18:38:02 +0000

    media: Replace uses of ComPtr::GetAddressOf() with ComPtr::operator&
    Microsoft::WRL::ComPtr<T>::GetAddressOf() is banned due to the ease
    of causing memory leaks.
    
    Also replace CopyTo() with As() where necessary since CopyTo(&foo) does
    not work if we are copying to different interfaces.
    
    BUG=914910
    
    Change-Id: I592388d57375ff25444e7c090af00676dcd10337
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067653
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743611}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
@@ -416,7 +416,7 @@ bool MediaFoundationVideoEncodeAccelerator::InitializeInputOutputSamples(
   }
 
   // Initialize output parameters.
-  hr = MFCreateMediaType(imf_output_media_type_.GetAddressOf());
+  hr = MFCreateMediaType(&imf_output_media_type_);
   RETURN_ON_HR_FAILURE(hr, "Couldn't create media type", false);
   hr = imf_output_media_type_->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video);
   RETURN_ON_HR_FAILURE(hr, "Couldn't set media type", false);
@@ -442,7 +442,7 @@ bool MediaFoundationVideoEncodeAccelerator::InitializeInputOutputSamples(
   RETURN_ON_HR_FAILURE(hr, "Couldn't set output media type", false);
 
   // Initialize input parameters.
-  hr = MFCreateMediaType(imf_input_media_type_.GetAddressOf());
+  hr = MFCreateMediaType(&imf_input_media_type_);
   RETURN_ON_HR_FAILURE(hr, "Couldn't create media type", false);
   hr = imf_input_media_type_->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Video);
   RETURN_ON_HR_FAILURE(hr, "Couldn't set media type", false);
@@ -469,7 +469,7 @@ bool MediaFoundationVideoEncodeAccelerator::SetEncoderModes() {
   RETURN_ON_FAILURE((encoder_.Get() != nullptr),
                     "No HW encoder instance created", false);
 
-  HRESULT hr = encoder_.CopyTo(codec_api_.GetAddressOf());
+  HRESULT hr = encoder_.As(&codec_api_);
   RETURN_ON_HR_FAILURE(hr, "Couldn't get ICodecAPI", false);
   VARIANT var;
   var.vt = VT_UI4;
@@ -540,7 +540,7 @@ void MediaFoundationVideoEncodeAccelerator::EncodeTask(
   DCHECK(encoder_thread_task_runner_->BelongsToCurrentThread());
 
   Microsoft::WRL::ComPtr<IMFMediaBuffer> input_buffer;
-  input_sample_->GetBufferByIndex(0, input_buffer.GetAddressOf());
+  input_sample_->GetBufferByIndex(0, &input_buffer);
 
   {
     MediaBufferScopedPointer scoped_buffer(input_buffer.Get());
@@ -628,7 +628,7 @@ void MediaFoundationVideoEncodeAccelerator::ProcessOutput() {
   DVLOG(3) << "Got encoded data " << hr;
 
   Microsoft::WRL::ComPtr<IMFMediaBuffer> output_buffer;
-  hr = output_sample_->GetBufferByIndex(0, output_buffer.GetAddressOf());
+  hr = output_sample_->GetBufferByIndex(0, &output_buffer);
   RETURN_ON_HR_FAILURE(hr, "Couldn't get buffer by index", );
   DWORD size = 0;
   hr = output_buffer->GetCurrentLength(&size);
