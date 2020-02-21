e92a2993539558ca56017734776acec33b13341d
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-02-21 21:51:27 +0000

    Implement BlockUnsupportedFiletypes policy
    Bug: 1013584
    Change-Id: I7f32e9d5a1e0584e509e5bd08e3346e370bbfcbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067318
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743683}

--- chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.cc
@@ -69,6 +69,8 @@ std::string ResultToString(BinaryUploadService::Result result) {
       return "UNAUTHORIZED";
     case BinaryUploadService::Result::FILE_ENCRYPTED:
       return "FILE_ENCRYPTED";
+    case BinaryUploadService::Result::UNSUPPORTED_FILE_TYPE:
+      return "UNSUPPORTED_FILE_TYPE";
   }
 }
 
