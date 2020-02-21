2c67bfb6638e9363a58d62f9c2ea83c384e95dc2
Eriko Kurimoto <elkurin@google.com>
2020-02-21 06:33:14 +0000

    SharedWorker: Add ES module SharedWorker for WorkerInBackgroundPage
    This CL adds browser tests for 'module' type shared workers in
    WorkerTest.WorkerInBackgroundPage.
    
    Bug: 824646
    Change-Id: I4500087be534bb384bcf4171437b50312b2a7da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2061578
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@google.com>
    Cr-Commit-Position: refs/heads/master@{#743451}

--- chrome/test/data/extensions/api_test/worker/background.js
+++ chrome/test/data/extensions/api_test/worker/background.js
@@ -84,6 +84,10 @@ chrome.test.getConfig(function(config) {
       undefined,
       () => { return new SharedWorker(sharedWorkerUrl) },
       sharedWorkerUrl),
+    noRedirectTest.bind(
+      undefined,
+      () => { return new SharedWorker(sharedWorkerUrl, {type: 'module'}) },
+      sharedWorkerUrl),
 
     sameOriginRedirectTest.bind(
       undefined,
@@ -97,6 +101,12 @@ chrome.test.getConfig(function(config) {
       undefined,
       () => { return new SharedWorker(redirectedSharedWorkerUrl) },
       sharedWorkerUrl),
+    sameOriginRedirectTest.bind(
+      undefined,
+      () => {
+        return new SharedWorker(redirectedSharedWorkerUrl, {type: 'module'});
+      },
+      sharedWorkerUrl),
 
     crossOriginRedirectTest.bind(
       undefined,
@@ -109,5 +119,11 @@ chrome.test.getConfig(function(config) {
     crossOriginRedirectTest.bind(
       undefined,
       () => { return new SharedWorker(crossOriginRedirectedSharedWorkerUrl) }),
+    crossOriginRedirectTest.bind(
+      undefined,
+      () => {
+        return new SharedWorker(crossOriginRedirectedSharedWorkerUrl,
+                                {type: 'module'});
+      }),
   ]);
 });
