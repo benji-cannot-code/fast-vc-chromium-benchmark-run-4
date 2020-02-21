83333a3b6d872c2568ec2e815137d2171f893b18
Wan-Teh Chang <wtc@google.com>
2020-02-21 18:31:25 +0000

    Switch to new libgav1 frame buffer callback API.
    Use VideoFramePool instead of FrameBufferPool.
    
      $ gn args out/Default
      # In the editor, add the following build argument:
      # enable_libgav1_decoder = true
      $ autoninja -C out/Default media_unittests
      $ out/Default/media_unittests
      $ out/Default/media_unittests --enable-features=Gav1VideoDecoder
      $ autoninja -C out/Default chrome
      $ out/Default/chrome --enable-features=Gav1VideoDecoder
      # Open a new incognito window.
      # Visit the AV1 Beta Launch Playlist at Youtube:
      # https://www.youtube.com/playlist?list=PLyqf6gJt7KuHBmeVzZteZUlNUQAVLwrZS
      # Play the first video.
    
    Tested: 
    Bug: 1026522
    Bug: b/145886340
    Change-Id: I8bf47cd1fad3f5610d755c8adee83bc7f68ab5f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042542
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/master@{#743608}

--- media/filters/gav1_video_decoder.h
+++ media/filters/gav1_video_decoder.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "media/base/media_export.h"
 #include "media/base/video_decoder_config.h"
+#include "media/base/video_frame_pool.h"
 #include "media/filters/offloading_video_decoder.h"
 
 namespace libgav1 {
@@ -25,7 +26,6 @@ class Decoder;
 
 namespace media {
 class MediaLog;
-class FrameBufferPool;
 
 class MEDIA_EXPORT Gav1VideoDecoder : public OffloadableVideoDecoder {
  public:
@@ -49,6 +49,10 @@ class MEDIA_EXPORT Gav1VideoDecoder : public OffloadableVideoDecoder {
 
   // OffloadableVideoDecoder implementation.
   void Detach() override;
+
+  scoped_refptr<VideoFrame> CreateVideoFrame(VideoPixelFormat format,
+                                             const gfx::Size& coded_size,
+                                             const gfx::Rect& visible_rect);
 
  private:
   enum class DecoderState {
@@ -85,9 +89,9 @@ class MEDIA_EXPORT Gav1VideoDecoder : public OffloadableVideoDecoder {
   DecoderState state_ = DecoderState::kUninitialized;
 
   // A decoded buffer used in libgav1 is allocated and managed by
-  // |memory_pool_|. The buffer can be reused only if libgav1's decoder doesn't
+  // |frame_pool_|. The buffer can be reused only if libgav1's decoder doesn't
   //  use the buffer and rendering the frame is complete.
-  scoped_refptr<FrameBufferPool> memory_pool_;
+  VideoFramePool frame_pool_;
 
   base::queue<DecodeRequest> decode_queue_;
 
