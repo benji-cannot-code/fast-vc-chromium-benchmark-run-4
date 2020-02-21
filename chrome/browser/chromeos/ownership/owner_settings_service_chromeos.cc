1c2f68d666e6647a65498ccc4e86603dc7a2f8e2
Paul Moy <pmoy@chromium.org>
2020-02-21 17:36:23 +0000

    DeviceStatusCollector: Report memory info using cros_healthd
    Now that cros_healthd collects memory information, use it to
    fetch and report the information in DeviceStatusCollector.
    
    DeviceStatusCollectorTest.TestCrosHealthdInfo
    and chromeos_unittests --gtest_filter=
    CrosHealthdServiceConnectionTest.*
    
    Bug: chromium:1054198
    Test: browser_tests --gtest_filter=
    Change-Id: I23617c2184fc49859b25faf35ae7574af96240c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067252
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743590}

--- chrome/browser/chromeos/ownership/owner_settings_service_chromeos.cc
+++ chrome/browser/chromeos/ownership/owner_settings_service_chromeos.cc
@@ -645,6 +645,7 @@ void OwnerSettingsServiceChromeOS::UpdateDeviceSettings(
     //   kReportDeviceCpuInfo
     //   kReportDeviceHardwareStatus
     //   kReportDeviceLocation
+    //   kReportDeviceMemoryInfo
     //   kReportDeviceNetworkInterfaces
     //   kReportDevicePowerStatus
     //   kReportDeviceStorageStatus
