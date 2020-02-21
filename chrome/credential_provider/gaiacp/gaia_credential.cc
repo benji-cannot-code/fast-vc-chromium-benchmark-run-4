ac589f00f89de33157e2e6ce55164b8f90f8f374
Rakesh Soma <rakeshsoma@google.com>
2020-02-21 20:45:07 +0000

    Add show_tos support in other user flow.
    Bug: 1054703
    Change-Id: Ibb4a81e9bff45e97f9a08ccfddc4105c80ce01ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068163
    Commit-Queue: Rakesh Soma <rakeshsoma@google.com>
    Reviewed-by: Yusuf Sengul <yusufsn@google.com>
    Reviewed-by: Tien Mai <tienmai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743659}

--- chrome/credential_provider/gaiacp/gaia_credential.cc
+++ chrome/credential_provider/gaiacp/gaia_credential.cc
@@ -10,14 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/credential_provider/common/gcp_strings.h"
 #include "chrome/credential_provider/gaiacp/gaia_credential_base.h"
 #include "chrome/credential_provider/gaiacp/gcp_utils.h"
+#include "chrome/credential_provider/gaiacp/gcpw_strings.h"
 #include "chrome/credential_provider/gaiacp/logging.h"
 #include "chrome/credential_provider/gaiacp/mdm_utils.h"
 #include "content/public/common/content_switches.h"
 #include "google_apis/gaia/gaia_switches.h"
 
 namespace credential_provider {
-constexpr char kGaiaSetupPath[] = "embedded/setup/windows";
-
 CGaiaCredential::CGaiaCredential() = default;
 
 CGaiaCredential::~CGaiaCredential() = default;
--- chrome/credential_provider/gaiacp/gaia_credential_other_user.cc
+++ chrome/credential_provider/gaiacp/gaia_credential_other_user.cc
@@ -5,7 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/credential_provider/gaiacp/gaia_credential_other_user.h"
 
+#include "base/command_line.h"
+#include "chrome/credential_provider/common/gcp_strings.h"
+#include "chrome/credential_provider/gaiacp/gcpw_strings.h"
 #include "chrome/credential_provider/gaiacp/logging.h"
+#include "chrome/credential_provider/gaiacp/mdm_utils.h"
 
 namespace credential_provider {
 
@@ -22,4 +26,21 @@ void COtherUserGaiaCredential::FinalRelease() {
   LOGFN(VERBOSE);
 }
 
+HRESULT COtherUserGaiaCredential::GetUserGlsCommandline(
+    base::CommandLine* command_line) {
+  // Don't show tos when GEM isn't enabled.
+  if (IsGemEnabled()) {
+    // In default other user flow, the user has to accept tos
+    // every time. So we need to set the show_tos switch to 1.
+    command_line->AppendSwitchASCII(kShowTosSwitch, "1");
+  }
+
+  HRESULT hr = SetGaiaEndpointCommandLineIfNeeded(
+      L"ep_setup_url", kGaiaSetupPath, IsGemEnabled(), command_line);
+  if (FAILED(hr)) {
+    LOGFN(ERROR) << "Setting gaia url for gaia credential failed";
+    return E_FAIL;
+  }
+  return S_OK;
+}
 }  // namespace credential_provider
