a1b88cf14ed7e8380773f4099c64befa4637f8c1
Side Yilmaz <sideyilmaz@chromium.org>
2020-02-21 09:15:06 +0000

    Update EphemeralTabCoordinator to use incognito profile when called incognito.
    |Profile#getLastUsedProfile| is deprecated and replaced with
    |Profile#getLastUsedRegularProfile|. This CL updates
    EphemeralTabCoordinator class.
    
    This CL creates a profile object based on isIncognito param to pass
    the current profile(i.e., regular or off-the-record profile) for
    TrackerFactory and FaviconLoader,while the profile always regular
    profile in current usage. This CL changes the behavior of code to
    make it run with correct profile.
    
    Bug: 1041781
    Change-Id: Ia412cb5bdf28a269d2b75fac7c8cbf559c36a51d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2051985
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743480}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
@@ -63,6 +63,7 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
     private boolean mOpened;
     private String mUrl;
     private int mCurrentMaxSheetHeight;
+    private Profile mProfile;
 
     /**
      * Constructor.
@@ -139,6 +140,8 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
     public void requestOpenSheet(String url, String title, boolean isIncognito) {
         mUrl = url;
         mIsIncognito = isIncognito;
+        mProfile = isIncognito ? Profile.getLastUsedRegularProfile().getOffTheRecordProfile()
+                               : Profile.getLastUsedRegularProfile();
 
         getContent().loadUrl(url, true);
         getContent().updateBrowserControlsState(true);
@@ -151,7 +154,7 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
 
         mSheetContent.updateTitle(title);
         mBottomSheetController.requestShowContent(mSheetContent, true);
-        Tracker tracker = TrackerFactory.getTrackerForProfile(Profile.getLastUsedProfile());
+        Tracker tracker = TrackerFactory.getTrackerForProfile(mProfile);
         if (tracker.isInitialized()) tracker.notifyEvent(EventConstants.EPHEMERAL_TAB_USED);
 
         // TODO(donnd): Collect UMA with OverlayPanel.StateChangeReason.CLICK.
@@ -307,7 +310,7 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
             }
 
             mCurrentUrl = url;
-            mFaviconLoader.loadFavicon(url, (drawable) -> onFaviconAvailable(drawable));
+            mFaviconLoader.loadFavicon(url, (drawable) -> onFaviconAvailable(drawable), mProfile);
         }
 
         @Override
@@ -382,8 +385,10 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
          * the URL, a default favicon will be shown.
          * @param url The URL for which favicon is to be generated.
          * @param callback The callback to be invoked to display the final image.
+         * @param profile The profile for which favicon service is used.
          */
-        public void loadFavicon(final String url, Callback<Drawable> callback) {
+        public void loadFavicon(final String url, Callback<Drawable> callback, Profile profile) {
+            assert profile != null;
             FaviconHelper.FaviconImageCallback imageCallback = (bitmap, iconUrl) -> {
                 Drawable drawable;
                 if (bitmap != null) {
@@ -397,9 +402,7 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
                 callback.onResult(drawable);
             };
 
-            mFaviconHelper.getLocalFaviconImageForURL(
-                    Profile.getLastUsedProfile(), url, mFaviconSize, imageCallback);
+            mFaviconHelper.getLocalFaviconImageForURL(profile, url, mFaviconSize, imageCallback);
         }
-
     }
 }
