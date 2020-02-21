8860468a18818950cc535d40f0f82a879cf40435
Mike Reed <reed@google.com>
2020-02-21 18:50:25 +0000

    IWYU
    remove legacy matrix44 flag, since SkColorSpace.h no longer needs to
    include SkMatrix44.h
    
    Change-Id: I3f90a6df6ee545fae82584ebeffe19e6c885fc61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068042
    Commit-Queue: Mike Reed <reed@google.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743621}

--- ui/gfx/color_space.h
+++ ui/gfx/color_space.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "third_party/skia/include/core/SkColorSpace.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
+#include "third_party/skia/include/core/SkMatrix44.h"
 #include "ui/gfx/color_space_export.h"
 
 // These forward declarations are used to give IPC code friend access to private
