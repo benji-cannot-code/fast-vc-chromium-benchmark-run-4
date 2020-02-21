4ac4662fb37f1345ed4f997f1f45ab5a5ec597b0
Chris Harrelson <chrishtr@chromium.org>
2020-02-21 16:50:27 +0000

    Only draw base background color into a separate buffer if there is an effect on the root element.
    The only reason to do so is to preserve blending. A more complete
    fix might be to always allocate an isolation effect node for the HTML
    element, but that will cause a very large number of rebaselines and
    introduces some risk of its own. (This CL needs to be merged into M81.)
    While not perfect, it returns us to the situation before
    landing [1].
    
    Bug: 1050462
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/1979258
    
    
    Change-Id: Id7b51837c0d36c426c1042bb9c6c0e5f81e64a36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067288
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743568}

--- third_party/blink/renderer/core/paint/view_painter.cc
+++ third_party/blink/renderer/core/paint/view_painter.cc
@@ -115,6 +115,7 @@ void ViewPainter::PaintBoxDecorationBackground(const PaintInfo& paint_info) {
   base::Optional<ScopedPaintChunkProperties> scoped_properties;
 
   bool painted_separate_backdrop = false;
+  bool painted_separate_effect = false;
 
   bool should_apply_root_background_behavior =
       layout_view_.GetDocument().IsHTMLDocument() ||
@@ -152,6 +153,10 @@ void ViewPainter::PaintBoxDecorationBackground(const PaintInfo& paint_info) {
     // group's. Otherwise we can usually avoid both a separate
     // PaintChunk and a BeginLayer/EndLayer.
     if (document_element_state != root_element_background_painting_state) {
+      if (&document_element_state.Effect() !=
+          &root_element_background_painting_state.Effect())
+        painted_separate_effect = true;
+
       root_element_background_painting_state = document_element_state;
       PaintRootGroup(paint_info, pixel_snapped_background_rect, document,
                      *background_client,
@@ -169,7 +174,8 @@ void ViewPainter::PaintBoxDecorationBackground(const PaintInfo& paint_info) {
 
   if (should_paint_background) {
     PaintRootElementGroup(paint_info, pixel_snapped_background_rect,
-                          *background_client, painted_separate_backdrop);
+                          *background_client, painted_separate_backdrop,
+                          painted_separate_effect);
   }
   if (has_touch_action_rect) {
     BoxPainter(layout_view_)
@@ -224,7 +230,8 @@ void ViewPainter::PaintRootElementGroup(
     const PaintInfo& paint_info,
     const IntRect& pixel_snapped_background_rect,
     const DisplayItemClient& background_client,
-    bool painted_separate_backdrop) {
+    bool painted_separate_backdrop,
+    bool painted_separate_effect) {
   GraphicsContext& context = paint_info.context;
   if (DrawingRecorder::UseCachedDrawingIfPossible(
           context, background_client, DisplayItem::kDocumentBackground)) {
@@ -314,7 +321,7 @@ void ViewPainter::PaintRootElementGroup(
               reversed_paint_list, layout_view_.StyleRef().BackgroundLayers());
   DCHECK(reversed_paint_list.size());
 
-  if (painted_separate_backdrop) {
+  if (painted_separate_effect) {
     should_draw_background_in_separate_buffer = true;
   } else {
     // If the root background color is opaque, isolation group can be skipped
@@ -334,7 +341,7 @@ void ViewPainter::PaintRootElementGroup(
   // painted when there is any effect on the root element, such as a blend
   // mode. An extra BeginLayer will result in incorrect blend isolation if
   // it is added on top of any effect on the root element.
-  if (should_draw_background_in_separate_buffer && !painted_separate_backdrop) {
+  if (should_draw_background_in_separate_buffer && !painted_separate_effect) {
     if (base_background_color.Alpha()) {
       context.FillRect(
           paint_rect, base_background_color,
@@ -379,7 +386,7 @@ void ViewPainter::PaintRootElementGroup(
                                      geometry);
   }
 
-  if (should_draw_background_in_separate_buffer && !painted_separate_backdrop)
+  if (should_draw_background_in_separate_buffer && !painted_separate_effect)
     context.EndLayer();
 }
 
