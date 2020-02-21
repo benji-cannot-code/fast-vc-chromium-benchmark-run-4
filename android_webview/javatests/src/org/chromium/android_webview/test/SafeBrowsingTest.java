d6d12bd3c49bac15ab156811e26f18ef02eb98e4
Justin DeWitt <dewittj@chromium.org>
2020-02-21 01:02:25 +0000

    Disable flaky WebView Safe Browsing Test.
    See e.g. https://ci.chromium.org/p/chromium/builders/ci/Marshmallow%20Tablet%20Tester/13972
    
    Tbr: boliu@chromium.org, carlosil@chromium.org
    Bug: 1047527
    Change-Id: I2513007de4c922183ce88845a29a656df1053e7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067801
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743361}

--- android_webview/javatests/src/org/chromium/android_webview/test/SafeBrowsingTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/SafeBrowsingTest.java
@@ -49,6 +49,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.InMemorySharedPreferences;
 import org.chromium.components.safe_browsing.SafeBrowsingApiBridge;
@@ -1349,6 +1350,7 @@ public class SafeBrowsingTest {
 
     @Test
     @SmallTest
+    @DisabledTest
     @Feature({"AndroidWebView"})
     public void testDestroyWebViewWithInterstitialShowing() throws Throwable {
         loadPathAndWaitForInterstitial(MALWARE_HTML_PATH);
