930885a541c2663d04e28a2daa004891063a32db
Daniel Libby <dlibby@microsoft.com>
2020-02-21 06:47:09 +0000

    Add main thread percentage scrolling for keyboard and scrollbar
    Keyboard arrow scrolling and scrollbar button scrolling should also
    scroll a percentage of the scrollable area when percent based scrolling
    is enabled.
    
    This CL adds plumbing from the base::Feature to blink, and uses 1/8th as
    the percentage to use for directional scrolls via keyboard arrows or
    scrollbar buttons. Adds a test suite that runs scrollbar tests in
    percentage mode. Keyboard is currently handled on the main thread so we
    just use the runtimeFlag toggling via javascript and eventSender.
    
    Bug: 1008153
    Change-Id: I7ea7c5471298187da757cf22748620c4accaf192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057501
    Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743455}

--- cc/input/scrollbar.h
+++ cc/input/scrollbar.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 static constexpr int kPixelsPerLineStep = 40;
 static constexpr float kMinFractionToStepWhenPaging = 0.875f;
 
+// Each directional scroll for percentage-based units should scroll 1/8th of
+// the scrollable area.
+static constexpr float kPercentDeltaForDirectionalScroll = 0.125f;
+
 // Autoscrolling (on the main thread) happens by applying a delta every 50ms.
 // Hence, pixels per second for a autoscroll cc animation can be calculated as:
 // autoscroll velocity = delta / 0.05 sec = delta x 20
