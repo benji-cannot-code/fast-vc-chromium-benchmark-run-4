3dbf73126278fda9c2c491e87360927ec4ff0997
Junyi Qiu <junyiq@google.com>
2020-02-22 02:21:44 +0000

    Support specific referrer-policy in Page.navigate().
    Currently Page.navigate() supports setting the referrer, but not
    referrer-policy. It always uses the default referrer-policy. Some
    clients want set a specific non-default `referrer-policy` along with
    the URL set in referrer field.
    
    BUG=b/149020493
    R=alexmos@chromium.org, caseq@chromium.org, zoeclifford@chromium.org
    
    Change-Id: I90be78f26e1cd1b05517fa3ba93771a545044886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2053170
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743758}

--- content/browser/devtools/protocol/page_handler.h
+++ content/browser/devtools/protocol/page_handler.h
@@ -108,6 +108,7 @@ class PageHandler : public DevToolsDomainHandler,
                 Maybe<std::string> referrer,
                 Maybe<std::string> transition_type,
                 Maybe<std::string> frame_id,
+                Maybe<std::string> referrer_policy,
                 std::unique_ptr<NavigateCallback> callback) override;
   Response StopLoading() override;
 
---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/page/navigate-origin-policy.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests that Page.navigate() returns the results that match referrer-policy configs`);
+
+  await dp.Page.enable();
+  await dp.Network.enable();
+  var navigatePromise = dp.Page.navigate({
+    url: 'http://example.com',
+    referrer: 'https://www.google.com/search?q=bing',
+    referrerPolicy: 'origin',
+  });
+  var requestWillBeSent = dp.Network.onRequestWillBeSent(event => {
+    const request = event.params.request;
+    testRunner.log('Request referrer: ' + request.headers.Referer);
+    testRunner.log('Request referrer-policy: ' + request.referrerPolicy);
+  });
+  const params = (await dp.Network.onceRequestWillBeSent()).params;
+  testRunner.completeTest();
+})
