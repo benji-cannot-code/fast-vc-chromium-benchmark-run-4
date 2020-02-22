52513e816ca8e83f59cd353f4a056c95392691ce
Oystein Eftevaag <oysteine@chromium.org>
2020-02-22 00:15:54 +0000

    Add histogram samples as instant tracing events
    R=ssid@chromium.org
    
    Change-Id: I9d25ace162f0fd575f89474b1a4b95a2993ab53f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068648
    Reviewed-by: ssid <ssid@chromium.org>
    Commit-Queue: oysteine <oysteine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743722}

--- services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source_unittest.cc
@@ -1767,7 +1767,7 @@ TEST_F(TraceEventDataSourceTest, HistogramSample) {
 
   UMA_HISTOGRAM_BOOLEAN("Foo.Bar", true);
 
-  EXPECT_EQ(producer_client()->GetFinalizedPacketCount(), 5u);
+  EXPECT_EQ(producer_client()->GetFinalizedPacketCount(), 4u);
   ExpectStandardPreamble();  // 3 preamble packets.
 
   auto* e_packet = producer_client()->GetFinalizedPacket(3);
