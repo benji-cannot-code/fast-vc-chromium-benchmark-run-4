6b42a60f71a8a667c58e8770a7732e5b3e6c8759
Mehran Mahmoudi <mahmoudi@chromium.org>
2020-02-22 23:43:13 +0000

    Implement equals() and hashCode() for UnguessableToken
    The native implementation of UnguessableToken does not keep a reference
    of its Java counter-part. Therefore, having a Java equals() method comes
    in handy.
    
    Among other things, hashCode() enables UnguessableToken to be used as a
    key in HashMap.
    
    Change-Id: I062d462298990b23da22c87028a5b0520791c00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067670
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743816}

--- components/paint_preview/player/android/junit/src/org/chromium/components/paintpreview/player/frame/PlayerFrameMediatorTest.java
+++ components/paint_preview/player/android/junit/src/org/chromium/components/paintpreview/player/frame/PlayerFrameMediatorTest.java
@@ -87,7 +87,7 @@ public class PlayerFrameMediatorTest {
             if (o.getClass() != this.getClass()) return false;
 
             RequestedBitmap rb = (RequestedBitmap) o;
-            return rb.mClipRect.equals(mClipRect) && rb.mFrameGuid == mFrameGuid
+            return rb.mClipRect.equals(mClipRect) && rb.mFrameGuid.equals(mFrameGuid)
                     && rb.mScaleFactor == mScaleFactor;
         }
 
