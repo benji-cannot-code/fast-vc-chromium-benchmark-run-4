5d1fe11c131617434cbb212cefa47451ee4785ff
Tom Anderson <thomasanderson@chromium.org>
2020-02-22 04:09:53 +0000

    Fix autofill popup separator color
    This CL makes AutofillPopupFooterView respond to theme changes.
    Previously, the separator color was only initialized on creation, when
    the NativeTheme has not yet been set.
    
    R=pkasting
    
    Change-Id: I9bb8a511beca58e4d6f1d40a1f832bce922b6431
    Bug: 914974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063892
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743780}

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views.cc
@@ -152,7 +152,7 @@ class AutofillPopupItemView : public AutofillPopupRowView {
 
   // AutofillPopupRowView:
   void CreateContent() override;
-  void RefreshStyle() final;
+  void RefreshStyle() override;
   std::unique_ptr<views::Background> CreateBackground() final;
 
   int frontend_id() const { return frontend_id_; }
@@ -259,6 +259,7 @@ class AutofillPopupFooterView : public AutofillPopupItemView {
  protected:
   // AutofillPopupItemView:
   void CreateContent() override;
+  void RefreshStyle() override;
   int GetPrimaryTextStyle() override;
   gfx::Font::Weight GetPrimaryTextWeight() const override;
 
@@ -709,13 +710,6 @@ AutofillPopupFooterView* AutofillPopupFooterView::Create(
 }
 
 void AutofillPopupFooterView::CreateContent() {
-  SetBorder(views::CreateSolidSidedBorder(
-      /*top=*/views::MenuConfig::instance().separator_thickness,
-      /*left=*/0,
-      /*bottom=*/0,
-      /*right=*/0,
-      /*color=*/popup_view()->GetSeparatorColor()));
-
   AutofillPopupController* controller = popup_view()->controller();
 
   views::BoxLayout* layout_manager =
@@ -758,6 +752,16 @@ void AutofillPopupFooterView::CreateContent() {
     AddSpacerWithSize(GetHorizontalMargin(), /*resize=*/false, layout_manager);
     AddIcon(icon);
   }
+}
+
+void AutofillPopupFooterView::RefreshStyle() {
+  AutofillPopupItemView::RefreshStyle();
+  SetBorder(views::CreateSolidSidedBorder(
+      /*top=*/views::MenuConfig::instance().separator_thickness,
+      /*left=*/0,
+      /*bottom=*/0,
+      /*right=*/0,
+      /*color=*/popup_view()->GetSeparatorColor()));
 }
 
 int AutofillPopupFooterView::GetPrimaryTextStyle() {
@@ -968,7 +972,6 @@ AutofillPopupRowView::AutofillPopupRowView(
 
 void AutofillPopupRowView::Init() {
   CreateContent();
-  RefreshStyle();
 }
 
 bool AutofillPopupRowView::HandleAccessibleAction(
