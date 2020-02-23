aa5db5f034d4e62d97d5a208a22b13ccbc6bcd76
Michael Spang <spang@chromium.org>
2020-02-23 03:49:22 +0000

    gpu/vulkan: Add binding for vkGetMemoryZirconHandleFUCHSIA
    This function is needed for GL/Vulkan interop on Fuchsia.
    
    Bug: 766360
    
    Change-Id: I37645fc29fbadd49f48eefbaf4849daa1be82a91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2069178
    Commit-Queue: Michael Spang <spang@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743820}

--- gpu/vulkan/vulkan_function_pointers.h
+++ gpu/vulkan/vulkan_function_pointers.h
@@ -199,6 +199,10 @@ struct VulkanFunctionPointers {
 #endif  // defined(OS_FUCHSIA)
 
 #if defined(OS_FUCHSIA)
+  PFN_vkGetMemoryZirconHandleFUCHSIA vkGetMemoryZirconHandleFUCHSIAFn = nullptr;
+#endif  // defined(OS_FUCHSIA)
+
+#if defined(OS_FUCHSIA)
   PFN_vkCreateBufferCollectionFUCHSIA vkCreateBufferCollectionFUCHSIAFn =
       nullptr;
   PFN_vkSetBufferCollectionConstraintsFUCHSIA
@@ -397,6 +401,11 @@ struct VulkanFunctionPointers {
   gpu::GetVulkanFunctionPointers()->vkImportSemaphoreZirconHandleFUCHSIAFn
 #define vkGetSemaphoreZirconHandleFUCHSIA \
   gpu::GetVulkanFunctionPointers()->vkGetSemaphoreZirconHandleFUCHSIAFn
+#endif  // defined(OS_FUCHSIA)
+
+#if defined(OS_FUCHSIA)
+#define vkGetMemoryZirconHandleFUCHSIA \
+  gpu::GetVulkanFunctionPointers()->vkGetMemoryZirconHandleFUCHSIAFn
 #endif  // defined(OS_FUCHSIA)
 
 #if defined(OS_FUCHSIA)
