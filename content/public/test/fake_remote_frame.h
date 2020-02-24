b8cd6ec7f8148acbe391c8e677285cdb3a5b0341
Andrey Kosyakov <caseq@chromium.org>
2020-02-24 18:36:09 +0000

    Revert "Convert FrameMsg_EnforceInsecureRequestPolicy to blink RemoteFrame mojom"
    This reverts commit 99fe56daa5f6cfa28c185388724b20ec1d79258d.
    
    Reason for revert: depends on https://chromium-review.googlesource.com/c/chromium/src/+/2045293 that broke navigation to facebook.com, see bug for details.
    
    BUG=1055450
    
    Original change's description:
    > Convert FrameMsg_EnforceInsecureRequestPolicy to blink RemoteFrame mojom
    > 
    > This CL converts FrameMsg_EnforceInsecureRequestPolicy to method on
    > blink RemoteFrame mojom. It also updates the unit test with
    > intercepting RemoteFrame and removes checking the IPC message.
    > 
    > BUG=1047284
    > 
    > Change-Id: I9578c49cfcc08b3a682ec2899627dc6503965654
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038513
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Julie Kim <jkim@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#743924}
    
    TBR=kinuko@chromium.org,haraken@chromium.org,dtapuska@chromium.org,jkim@igalia.com
    
    Change-Id: I39e9cdaa9e63df07cfdca5105a1933ef3c6a860d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1047284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068683
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743991}

--- content/public/test/fake_remote_frame.h
+++ content/public/test/fake_remote_frame.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/frame/intrinsic_sizing_info.mojom.h"
 #include "third_party/blink/public/mojom/frame/user_activation_update_types.mojom.h"
 #include "third_party/blink/public/mojom/scroll/scroll_into_view_params.mojom.h"
-#include "third_party/blink/public/mojom/security_context/insecure_request_policy.mojom.h"
 #include "ui/events/types/scroll_types.h"
 
 namespace base {
@@ -47,8 +46,6 @@ class FakeRemoteFrame : public blink::mojom::RemoteFrame {
   void EnforceInsecureNavigationsSet(const std::vector<uint32_t>& set) override;
   void SetFrameOwnerProperties(
       blink::mojom::FrameOwnerPropertiesPtr properties) override;
-  void EnforceInsecureRequestPolicy(
-      blink::mojom::InsecureRequestPolicy policy) override;
   void SetReplicatedOrigin(
       const url::Origin& origin,
       bool is_potentially_trustworthy_unique_origin) override;
