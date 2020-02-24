511bb32b4de4c24fc268d5ce36e4b52582df4449
Nina Satragno <nsatragno@chromium.org>
2020-02-24 19:25:55 +0000

    [webauthn] Remove old testing API flag
    This patch removes the --enable-web-authentication-testing-api flag. The
    flag disconnected the real webauthn API and replaced it with a
    simple mock.
    
    This flag had two goals:
      * To allow developers to write tests against webauthn.
        This use has has been superseded by the WebDriver WebAuthn Testing
        API.
      * To allow WPTs to have some sort of mock that avoided timeouts.
        This is now achieved by using the testdriver WebAuthn API.
    
    The patch also removes an inspector-protocol test that relied on a mock
    authenticator being set up automatically when the flag was set.
    
    Fixed: 1054422
    Change-Id: Iddde2b7e84bd6c37eaf5365f52fb0ddd401617e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067088
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744015}

--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -141,7 +141,6 @@ CONTENT_EXPORT extern const char kEnableUserMediaScreenCapturing[];
 CONTENT_EXPORT extern const char kEnableUseZoomForDSF[];
 CONTENT_EXPORT extern const char kEnableViewport[];
 CONTENT_EXPORT extern const char kEnableVtune[];
-CONTENT_EXPORT extern const char kEnableWebAuthTestingAPI[];
 CONTENT_EXPORT extern const char kEnableWebGL2ComputeContext[];
 CONTENT_EXPORT extern const char kEnableWebGLDraftExtensions[];
 CONTENT_EXPORT extern const char kEnableWebGLImageChromium[];
--- content/shell/app/shell_main_delegate.cc
+++ content/shell/app/shell_main_delegate.cc
@@ -261,8 +261,6 @@ bool ShellMainDelegate::BasicStartupComplete(int* exit_code) {
                                    "MAP *.test. 127.0.0.1,"
                                    "MAP *.test 127.0.0.1");
 
-    command_line.AppendSwitch(switches::kEnableWebAuthTestingAPI);
-
     if (!command_line.HasSwitch(switches::kForceGpuRasterization) &&
         !command_line.HasSwitch(switches::kEnableGpuRasterization)) {
       command_line.AppendSwitch(switches::kDisableGpuRasterization);
