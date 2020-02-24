f7797b55239b1a7fbb503926f5c74b7788ad281e
Eric Willigers <ericwilligers@chromium.org>
2020-02-24 05:25:59 +0000

    WebAppUninstallDialogViewBrowserTest does not rely on extensions
    The tests BookmarkAppUninstallDialogViewBrowserTest and
    BookmarkAppUninstallDialogViewInteractiveBrowserTest previously relies
    on extensions methods to install "bookmark apps". We now refer to
    these as web apps, and now use web_app:: functions to install them.
    
    
    Bug: 1052722
    Change-Id: Ie4f81682ffa4282a88f06ca89578b3f093507ea1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067774
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743869}

--- chrome/browser/ui/views/web_apps/web_app_uninstall_dialog_browsertest.cc
+++ chrome/browser/ui/views/web_apps/web_app_uninstall_dialog_browsertest.cc
@@ -10,49 +10,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/optional.h"
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
-#include "chrome/browser/extensions/browsertest_util.h"
-#include "chrome/browser/extensions/extension_service.h"
-#include "chrome/browser/extensions/extension_uninstall_dialog.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/browser/ui/views/web_apps/web_app_uninstall_dialog_view.h"
+#include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/common/web_application_info.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_utils.h"
-#include "extensions/browser/extension_dialog_auto_confirm.h"
-#include "extensions/browser/extension_system.h"
-#include "extensions/common/extension.h"
-#include "extensions/common/extension_builder.h"
-#include "extensions/common/extension_urls.h"
-#include "extensions/common/value_builder.h"
+
+using web_app::AppId;
 
 namespace {
 
-scoped_refptr<const extensions::Extension> BuildTestBookmarkApp() {
-  extensions::ExtensionBuilder extension_builder("foo");
-  extension_builder.AddFlags(extensions::Extension::FROM_BOOKMARK);
-  extension_builder.SetManifestPath({"app", "launch", "web_url"},
-                                    "https://example.com/");
-  return extension_builder.Build();
+AppId InstallTestWebApp(Profile* profile) {
+  const GURL example_url = GURL("http://example.org/");
+
+  auto web_app_info = std::make_unique<WebApplicationInfo>();
+  web_app_info->app_url = example_url;
+  web_app_info->scope = example_url;
+  web_app_info->open_as_window = true;
+  return web_app::InstallWebApp(profile, std::move(web_app_info));
 }
 
 }  // namespace
 
-using BookmarkAppUninstallDialogViewBrowserTest = InProcessBrowserTest;
+using WebAppUninstallDialogViewBrowserTest = InProcessBrowserTest;
 
 // Test that WebAppUninstallDialog cancels the uninstall if the Window
 // which is passed to WebAppUninstallDialog::Create() is destroyed before
 // WebAppUninstallDialogDelegateView is created.
-IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewBrowserTest,
+IN_PROC_BROWSER_TEST_F(WebAppUninstallDialogViewBrowserTest,
                        TrackParentWindowDestruction) {
-  scoped_refptr<const extensions::Extension> extension(BuildTestBookmarkApp());
-  extensions::ExtensionSystem::Get(browser()->profile())
-      ->extension_service()
-      ->AddExtension(extension.get());
+  AppId app_id = InstallTestWebApp(browser()->profile());
 
   std::unique_ptr<web_app::WebAppUninstallDialog> dialog(
       web_app::WebAppUninstallDialog::Create(
@@ -63,7 +56,7 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewBrowserTest,
 
   base::RunLoop run_loop;
   bool was_uninstalled = false;
-  dialog->ConfirmUninstall(extension->id(),
+  dialog->ConfirmUninstall(app_id,
                            base::BindLambdaForTesting([&](bool uninstalled) {
                              was_uninstalled = uninstalled;
                              run_loop.Quit();
@@ -75,12 +68,9 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewBrowserTest,
 // Test that WebAppUninstallDialog cancels the uninstall if the Window
 // which is passed to WebAppUninstallDialog::Create() is destroyed after
 // WebAppUninstallDialogDelegateView is created.
-IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewBrowserTest,
+IN_PROC_BROWSER_TEST_F(WebAppUninstallDialogViewBrowserTest,
                        TrackParentWindowDestructionAfterViewCreation) {
-  scoped_refptr<const extensions::Extension> extension(BuildTestBookmarkApp());
-  extensions::ExtensionSystem::Get(browser()->profile())
-      ->extension_service()
-      ->AddExtension(extension.get());
+  AppId app_id = InstallTestWebApp(browser()->profile());
 
   std::unique_ptr<web_app::WebAppUninstallDialog> dialog(
       web_app::WebAppUninstallDialog::Create(
@@ -89,7 +79,7 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewBrowserTest,
 
   base::RunLoop run_loop;
   bool was_uninstalled = false;
-  dialog->ConfirmUninstall(extension->id(),
+  dialog->ConfirmUninstall(app_id,
                            base::BindLambdaForTesting([&](bool uninstalled) {
                              was_uninstalled = uninstalled;
                              run_loop.Quit();
@@ -102,22 +92,13 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewBrowserTest,
 }
 
 #if defined(OS_CHROMEOS)
-// Test that we don't crash when uninstalling an extension from a bookmark app
-// window in Ash. Context: crbug.com/825554
-IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewBrowserTest,
-                       BookmarkAppWindowAshCrash) {
-  scoped_refptr<const extensions::Extension> extension(BuildTestBookmarkApp());
-  extensions::ExtensionSystem::Get(browser()->profile())
-      ->extension_service()
-      ->AddExtension(extension.get());
-
-  WebApplicationInfo info;
-  info.app_url = GURL("https://test.com/");
-  const extensions::Extension* bookmark_app =
-      extensions::browsertest_util::InstallBookmarkApp(browser()->profile(),
-                                                       info);
-  Browser* app_browser = extensions::browsertest_util::LaunchAppBrowser(
-      browser()->profile(), bookmark_app);
+// Test that we don't crash when uninstalling a web app from a web app window in
+// Ash. Context: crbug.com/825554
+IN_PROC_BROWSER_TEST_F(WebAppUninstallDialogViewBrowserTest,
+                       WebAppWindowAshCrash) {
+  AppId app_id = InstallTestWebApp(browser()->profile());
+  Browser* app_browser =
+      web_app::LaunchWebAppBrowser(browser()->profile(), app_id);
 
   std::unique_ptr<web_app::WebAppUninstallDialog> dialog;
   {
@@ -129,20 +110,17 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewBrowserTest,
 
   {
     base::RunLoop run_loop;
-    dialog->ConfirmUninstall(extension->id(), base::DoNothing());
+    dialog->ConfirmUninstall(app_id, base::DoNothing());
     run_loop.RunUntilIdle();
   }
 }
 #endif  // defined(OS_CHROMEOS)
 
-class BookmarkAppUninstallDialogViewInteractiveBrowserTest
+class WebAppUninstallDialogViewInteractiveBrowserTest
     : public DialogBrowserTest {
  public:
   void ShowUi(const std::string& name) override {
-    extension_ = BuildTestBookmarkApp();
-    extensions::ExtensionSystem::Get(browser()->profile())
-        ->extension_service()
-        ->AddExtension(extension_.get());
+    AppId app_id = InstallTestWebApp(browser()->profile());
 
     dialog_ = web_app::WebAppUninstallDialog::Create(
         browser()->profile(), browser()->window()->GetNativeWindow());
@@ -150,7 +128,7 @@ class BookmarkAppUninstallDialogViewInteractiveBrowserTest
     base::RunLoop run_loop;
     dialog_->SetDialogShownCallbackForTesting(run_loop.QuitClosure());
 
-    dialog_->ConfirmUninstall(extension_->id(), base::DoNothing());
+    dialog_->ConfirmUninstall(app_id, base::DoNothing());
 
     run_loop.Run();
   }
@@ -162,11 +140,10 @@ class BookmarkAppUninstallDialogViewInteractiveBrowserTest
     dialog_.reset();
   }
 
-  scoped_refptr<const extensions::Extension> extension_;
   std::unique_ptr<web_app::WebAppUninstallDialog> dialog_;
 };
 
-IN_PROC_BROWSER_TEST_F(BookmarkAppUninstallDialogViewInteractiveBrowserTest,
+IN_PROC_BROWSER_TEST_F(WebAppUninstallDialogViewInteractiveBrowserTest,
                        InvokeUi_ManualUninstall) {
   ShowAndVerifyUi();
 }
