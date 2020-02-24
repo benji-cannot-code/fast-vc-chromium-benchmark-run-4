5a381d1ac55946e72ca9bb647f08938f4c0d7965
Alex Newcomer <newcomer@chromium.org>
2020-02-24 23:06:33 +0000

    Add test api for overview animation completion
    Check whether the overview animation has finished showing or hiding.
    To be used in CrOS tast tests.
    
    Bug: 1052211
    Change-Id: Ib8063d61302a66412ee0f55b49ae0a5fc5df945a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055694
    Commit-Queue: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Auto-Submit: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744109}

--- extensions/browser/extension_function_histogram_value.h
+++ extensions/browser/extension_function_histogram_value.h
@@ -1506,6 +1506,7 @@ enum HistogramValue {
   AUTOFILLASSISTANTPRIVATE_PROVIDEUSERDATA = 1442,
   PASSWORDSPRIVATE_ISOPTEDINFORACCOUNTSTORAGE = 1443,
   AUTOTESTPRIVATE_PINSHELFICON = 1444,
+  AUTOTESTPRIVATE_WAITFOROVERVIEWSTATE = 1445,
   // Last entry: Add new entries above, then run:
   // python tools/metrics/histograms/update_extension_histograms.py
   ENUM_BOUNDARY
