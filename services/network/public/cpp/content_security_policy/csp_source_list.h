ec936ae5553f9a7e066d47fbfc8e7cb491f712c5
Jose Dapena Paz <jdapena@igalia.com>
2020-02-24 16:24:33 +0000

    IWYU: missing include for std::string in csp_source_list.h
    Build error in GCC:
    ../../services/network/public/cpp/content_security_policy/csp_source_list.h:19:6: error: ‘string’ in namespace ‘std’ does not name a type
    
    Bug: 819294
    Change-Id: I9c8665050cae038530a79323b65932a124adf2a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067074
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#743946}

--- services/network/public/cpp/content_security_policy/csp_source_list.h
+++ services/network/public/cpp/content_security_policy/csp_source_list.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SERVICES_NETWORK_PUBLIC_CPP_CONTENT_SECURITY_POLICY_CSP_SOURCE_LIST_H_
 #define SERVICES_NETWORK_PUBLIC_CPP_CONTENT_SECURITY_POLICY_CSP_SOURCE_LIST_H_
 
+#include <string>
 #include <vector>
 
 #include "base/component_export.h"
