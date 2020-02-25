1382ac293558911d080f808a87eb1ba64632b01d
Lan Wei <lanwei@chromium.org>
2020-02-25 23:09:39 +0000

    Revert "OOBE - Improve reset screen confirmation dialog"
    This reverts commit 8411cc66aab9ea96b3c7ce2f141e562c93e1c3fe.
    
    Reason for revert: <INSERT REASONING HERE>
    ResetFirstAfterBootTest.ViewsLogic fails on Builder linux-chromeos-chrome
    https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome
    
    Original change's description:
    > OOBE - Improve reset screen confirmation dialog
    > 
    > - Replace the dialog on the reset screen with the oobe-help-dialog
    >   that complies with UX specs and reuses available code.
    > 
    > - Refactor the confirmation dialog logic so that it lives in the
    >   main element 'oobe-reset'.
    > 
    > - Remove the intermediate oobe_reset_screen by using LoginScreenBehavior
    > 
    > - Add a waiter to test::OobeJS for waiting upon the presence of an
    >   attribute. Such as waiting on the 'open' attribute of dialogs.
    > 
    > - Add constants and modifications to the browser tests of the reset
    >   screen.
    > 
    > Fixed: 933310
    > Change-Id: I54001745326b60ab5a0a19519923c54dffa5baec
    > Bug: 933310
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041474
    > Commit-Queue: Renato Silva <rrsilva@google.com>
    > Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    > Auto-Submit: Renato Silva <rrsilva@google.com>
    > Cr-Commit-Position: refs/heads/master@{#744288}
    
    TBR=antrim@chromium.org,rsorokin@chromium.org,rrsilva@google.com
    
    Change-Id: I5a98467310e478ccd20c0cb3c9b92461229c5b3f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 933310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2072916
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Lan Wei <lanwei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744419}

--- chrome/browser/resources/chromeos/login/custom_elements_oobe.js
+++ chrome/browser/resources/chromeos/login/custom_elements_oobe.js
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="oobe_terms_of_service.js">
 // <include src="oobe_hid_detection.js">
 // <include src="oobe_reset.js">
+// <include src="oobe_reset_confirmation_overlay.js">
 // <include src="oobe_update.js">
 // <include src="oobe_i18n_dropdown.js">
 // <include src="oobe_welcome_dialog.js">
--- chrome/browser/resources/chromeos/login/md_login.js
+++ chrome/browser/resources/chromeos/login/md_login.js
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include
 // src="../../../../../ui/login/account_picker/chromeos_user_pod_row.js">
 // <include src="cr_ui.js">
+// <include src="oobe_screen_reset.js">
 // <include src="oobe_screen_autolaunch.js">
 // <include src="oobe_screen_supervision_transition.js">
 // <include src="oobe_screen_assistant_optin_flow.js">
@@ -69,6 +70,7 @@ cr.define('cr.ui.Oobe', function() {
       cr.ui.login.DisplayManager.initialize();
       login.WrongHWIDScreen.register();
       login.AccountPickerScreen.register();
+      login.ResetScreen.register();
       login.AutolaunchScreen.register();
       login.ErrorMessageScreen.register();
       login.TPMErrorMessageScreen.register();
