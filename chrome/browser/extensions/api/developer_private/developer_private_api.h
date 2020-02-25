eeab45ca22bd6797eee1722b3730aae8a60f8b44
Anand K. Mistry <amistry@chromium.org>
2020-02-25 23:45:35 +0000

    [Extensions Functions] Migrate developerPrivate API to ExtensionFunction
    Bug: 634140
    Change-Id: Ied0092d83f179dc47130c9bf882cc43aca647da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2066477
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744441}

--- chrome/browser/extensions/api/developer_private/developer_private_api.h
+++ chrome/browser/extensions/api/developer_private/developer_private_api.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observer.h"
 #include "chrome/browser/extensions/api/commands/command_service.h"
 #include "chrome/browser/extensions/api/developer_private/entry_picker.h"
-#include "chrome/browser/extensions/chrome_extension_function.h"
 #include "chrome/browser/extensions/error_console/error_console.h"
 #include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/extensions/extension_uninstall_dialog.h"
@@ -626,8 +625,7 @@ class DeveloperPrivateIsProfileManagedFunction : public ExtensionFunction {
   ResponseAction Run() override;
 };
 
-class DeveloperPrivateLoadDirectoryFunction
-    : public ChromeAsyncExtensionFunction {
+class DeveloperPrivateLoadDirectoryFunction : public ExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("developerPrivate.loadDirectory",
                              DEVELOPERPRIVATE_LOADUNPACKEDCROS)
@@ -638,9 +636,10 @@ class DeveloperPrivateLoadDirectoryFunction
   ~DeveloperPrivateLoadDirectoryFunction() override;
 
   // ExtensionFunction:
-  bool RunAsync() override;
+  ResponseAction Run() override;
 
-  bool LoadByFileSystemAPI(const ::storage::FileSystemURL& directory_url);
+  ResponseAction LoadByFileSystemAPI(
+      const ::storage::FileSystemURL& directory_url);
 
   void ClearExistingDirectoryContent(const base::FilePath& project_path);
 
@@ -680,6 +679,9 @@ class DeveloperPrivateLoadDirectoryFunction
   // This is set to false if any of the copyFile operations fail on
   // call of the API. It is returned as a response of the API call.
   bool success_;
+
+  // Error string if |success_| is false.
+  std::string error_;
 };
 
 class DeveloperPrivateRequestFileSourceFunction
