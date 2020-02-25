c28d59dbd424788c45bef075f5b19b9c974b7316
Erik Jensen <rkjnsn@chromium.org>
2020-02-25 01:42:08 +0000

    remoting: Initial Linux keyboard layout monitor implementation.
    This still has a couple of rough edges:
     * Some layouts lacking a keypad thousands separator will show a decimal
       separator at both NumpadDecimal and NumpadComma.
     * IntlBackslash will appear on layouts where it's not typically
       present (usually generating '<').
     * Japanese and Korean IME keys are not included even where applicable.
     * When the user modifies the enabled keyboard layouts or settings,
       several layout messages can end up being sent in rapid succession as
       the changes are applied.
     * Probably other things that will be revealed during testing.
    
    Bug: 1026029
    Change-Id: Icb511db3d3b5715ab563a0f433443c977e5c805c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063850
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744150}

---
+++ remoting/host/keyboard_layout_monitor_chromeos.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/keyboard_layout_monitor.h"
+
+#include "base/callback.h"
+
+namespace remoting {
+
+namespace {
+
+class KeyboardLayoutMonitorChromeOs : public KeyboardLayoutMonitor {
+ public:
+  explicit KeyboardLayoutMonitorChromeOs(
+      base::RepeatingCallback<void(const protocol::KeyboardLayout&)> callback) {
+  }
+  ~KeyboardLayoutMonitorChromeOs() override = default;
+  void Start() override { NOTIMPLEMENTED(); }
+};
+
+}  // namespace
+
+std::unique_ptr<KeyboardLayoutMonitor> KeyboardLayoutMonitor::Create(
+    base::RepeatingCallback<void(const protocol::KeyboardLayout&)> callback,
+    scoped_refptr<base::SingleThreadTaskRunner> input_task_runner) {
+  return std::make_unique<KeyboardLayoutMonitorChromeOs>(std::move(callback));
+}
+
+}  // namespace remoting
