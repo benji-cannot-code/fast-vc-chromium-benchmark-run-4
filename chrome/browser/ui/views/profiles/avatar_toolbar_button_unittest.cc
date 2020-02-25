da28554c4cd40d609a7f64f047bca07f3f8ec4f2
Peter Kasting <pkasting@chromium.org>
2020-02-25 22:03:55 +0000

    Fix a toolbar button test when the system is in dark mode.
    This was never updated for the identity pill work.
    
    Bug: none
    Change-Id: I129b3184d997612c960253dfd596423d2beca068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071576
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744394}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button_unittest.cc
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_unittest.cc
@@ -5,12 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/profiles/avatar_toolbar_button.h"
 
-#include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/test_with_browser_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_account_icon_container_view.h"
-#include "ui/base/theme_provider.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/views/widget/widget.h"
 
@@ -27,7 +25,7 @@ TEST_F(AvatarToolbarButtonTest, HighlightMeetsMinimumContrast) {
   browser_view()->GetWidget()->GetContentsView()->AddChildView(button.get());
 
   SkColor toolbar_color =
-      button->GetThemeProvider()->GetColor(ThemeProperties::COLOR_TOOLBAR);
+      ToolbarButton::GetDefaultBackgroundColor(button->GetThemeProvider());
   SkColor highlight_color = SkColorSetRGB(0xFE, 0x00, 0x00);
 
   DCHECK_LT(color_utils::GetContrastRatio(highlight_color, toolbar_color),
