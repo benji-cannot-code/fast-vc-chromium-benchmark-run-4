ce8faaa2c7d492fbe48f9be77ba24675cf4b649d
Yusuf Sengul <yusufsn@google.com>
2020-02-25 22:26:36 +0000

    Update mc generated files for gcpw event logs
    Bug: 1054678
    Change-Id: I5df40a6f381c5312df9c95a6a52dafd659e077cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067859
    Reviewed-by: Tien Mai <tienmai@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Yusuf Sengul <yusufsn@google.com>
    Cr-Commit-Position: refs/heads/master@{#744403}

--- third_party/win_build_output/mc/chrome/credential_provider/eventlog/gcp_eventlog_messages.h
+++ third_party/win_build_output/mc/chrome/credential_provider/eventlog/gcp_eventlog_messages.h
@@ -54,7 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // GCPW Events
 //
-#define GCPW_CATEGORY                     ((WORD)0x00000001L)
+#define GCPW_CATEGORY                    ((WORD)0x00000001L)
 
 //
 // MessageId: MSG_LOG_MESSAGE
