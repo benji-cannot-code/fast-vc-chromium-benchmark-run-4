d0fee20167e30063499c54dc28e5ac3ced13081b
Nico Weber <thakis@chromium.org>
2020-02-25 12:57:01 +0000

    ios: Fix a Wrange-loop-analysis warning.
    Bug: 1039697
    Change-Id: Ied0f97df085594347a8e404b91c00f8ba0933690
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071697
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744247}

--- ios/chrome/browser/ui/omnibox/popup/autocomplete_match_formatter.mm
+++ ios/chrome/browser/ui/omnibox/popup/autocomplete_match_formatter.mm
@@ -256,7 +256,7 @@ UIColor* DimColorIncognito() {
   NSMutableAttributedString* result =
       [[NSMutableAttributedString alloc] initWithString:@""];
 
-  for (const auto field : line.text_fields()) {
+  for (const auto& field : line.text_fields()) {
     [result appendAttributedString:
                 [self attributedStringForTextfield:&field
                             useDeemphasizedStyling:useDeemphasizedStyling]];
