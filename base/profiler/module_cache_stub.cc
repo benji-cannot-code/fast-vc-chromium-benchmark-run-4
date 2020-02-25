f09eefabe896fb45ad218e2c257ab1df05d50699
Mike Wittman <wittman@chromium.org>
2020-02-25 23:29:49 +0000

    [Sampling profiler] ModuleCache interface cleanups
    Consistently use 'const Module' rather than 'Module' in the interface.
    Modules are not to be mutated by the ModuleCache or its users.
    
    Rename InjectModuleForTesting to InjectNativeModuleForTesting to be
    clearer about what it does.
    
    Have GetModules() return both native and non-native
    modules to make the behavior consistent with the function
    name. The only current user of GetModules(),
    content::protocol::MemoryHandler::GetBrowserSamplingProfile(), does
    not add any non-native modules to the ModuleCache so returning
    non-native modules from the function will not affect it.
    
    Bug: 1035855
    Change-Id: Ie1cd38e081ee48297df3c7a15b8e4997d1df1589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062883
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744430}

--- base/profiler/module_cache_stub.cc
+++ base/profiler/module_cache_stub.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 // static
-std::unique_ptr<ModuleCache::Module> ModuleCache::CreateModuleForAddress(
+std::unique_ptr<const ModuleCache::Module> ModuleCache::CreateModuleForAddress(
     uintptr_t address) {
   return nullptr;
 }
--- base/profiler/stack_sampler_impl_unittest.cc
+++ base/profiler/stack_sampler_impl_unittest.cc
@@ -148,7 +148,7 @@ class TestModule : public ModuleCache::Module {
 void InjectModuleForContextInstructionPointer(
     const std::vector<uintptr_t>& stack,
     ModuleCache* module_cache) {
-  module_cache->InjectModuleForTesting(
+  module_cache->InjectNativeModuleForTesting(
       std::make_unique<TestModule>(stack[0], sizeof(uintptr_t)));
 }
 
@@ -264,7 +264,8 @@ TEST(StackSamplerImplTest, WalkStack_Completed) {
   RegisterContext thread_context;
   RegisterContextInstructionPointer(&thread_context) =
       GetTestInstructionPointer();
-  module_cache.InjectModuleForTesting(std::make_unique<TestModule>(1u, 1u));
+  module_cache.InjectNativeModuleForTesting(
+      std::make_unique<TestModule>(1u, 1u));
   FakeTestUnwinder native_unwinder({{UnwindResult::COMPLETED, {1u}}});
 
   std::vector<Frame> stack = StackSamplerImpl::WalkStackForTesting(
@@ -279,7 +280,8 @@ TEST(StackSamplerImplTest, WalkStack_Aborted) {
   RegisterContext thread_context;
   RegisterContextInstructionPointer(&thread_context) =
       GetTestInstructionPointer();
-  module_cache.InjectModuleForTesting(std::make_unique<TestModule>(1u, 1u));
+  module_cache.InjectNativeModuleForTesting(
+      std::make_unique<TestModule>(1u, 1u));
   FakeTestUnwinder native_unwinder({{UnwindResult::ABORTED, {1u}}});
 
   std::vector<Frame> stack = StackSamplerImpl::WalkStackForTesting(
@@ -332,7 +334,8 @@ TEST(StackSamplerImplTest, WalkStack_AuxThenNative) {
   // non-native module.
   module_cache.AddNonNativeModule(std::make_unique<TestModule>(0u, 1u, false));
   // Inject a fake native module for the second frame.
-  module_cache.InjectModuleForTesting(std::make_unique<TestModule>(1u, 1u));
+  module_cache.InjectNativeModuleForTesting(
+      std::make_unique<TestModule>(1u, 1u));
 
   FakeTestUnwinder aux_unwinder(
       {{UnwindResult::UNRECOGNIZED_FRAME, {1u}}, false});
@@ -354,8 +357,10 @@ TEST(StackSamplerImplTest, WalkStack_NativeThenAux) {
 
   // Inject fake native modules for the instruction pointer from the context and
   // the third frame.
-  module_cache.InjectModuleForTesting(std::make_unique<TestModule>(0u, 1u));
-  module_cache.InjectModuleForTesting(std::make_unique<TestModule>(2u, 1u));
+  module_cache.InjectNativeModuleForTesting(
+      std::make_unique<TestModule>(0u, 1u));
+  module_cache.InjectNativeModuleForTesting(
+      std::make_unique<TestModule>(2u, 1u));
   // Treat the second frame's pointer as being in the aux unwinder's non-native
   // module.
   module_cache.AddNonNativeModule(std::make_unique<TestModule>(1u, 1u, false));
