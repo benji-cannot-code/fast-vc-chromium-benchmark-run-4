d386043f9e0a32290dd8ca0944f82a1d8654fae9
David Grogan <dgrogan@chromium.org>
2020-02-25 16:31:27 +0000

    LayoutNG: Remove content_size parameter from Resolve{Min,Max}BlockLength
    All the cases that needed content_size were short-circuited when
    https://chromium-review.googlesource.com/c/chromium/src/+/1599691 made
    {min,max}-height: {min,max,fit}-content behave as auto.
    
    Change-Id: Icafee2c35598d92c95703c3eb16e0d43eecde48d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2069506
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744280}

--- third_party/blink/renderer/core/layout/ng/ng_length_utils.cc
+++ third_party/blink/renderer/core/layout/ng/ng_length_utils.cc
@@ -287,7 +287,7 @@ MinMaxSize ComputeMinAndMaxContentContribution(
                                  max_length, LengthResolvePhase::kIntrinsic);
   } else {
     max = ResolveMaxBlockLength(space, style, border_padding, max_length,
-                                content_size, LengthResolvePhase::kIntrinsic);
+                                LengthResolvePhase::kIntrinsic);
   }
   computed_sizes.Constrain(max);
 
@@ -300,7 +300,7 @@ MinMaxSize ComputeMinAndMaxContentContribution(
                                  min_length, LengthResolvePhase::kIntrinsic);
   } else {
     min = ResolveMinBlockLength(space, style, border_padding, min_length,
-                                content_size, LengthResolvePhase::kIntrinsic);
+                                LengthResolvePhase::kIntrinsic);
   }
   computed_sizes.Encompass(min);
 
@@ -487,7 +487,7 @@ LayoutUnit ComputeBlockSizeForFragmentInternal(
         nullptr) {
   LayoutUnit min = ResolveMinBlockLength(
       constraint_space, style, border_padding, style.LogicalMinHeight(),
-      content_size, LengthResolvePhase::kLayout,
+      LengthResolvePhase::kLayout,
       opt_percentage_resolution_block_size_for_min_max);
   const Length& logical_height = style.LogicalHeight();
   // Scrollable percentage-sized children of table cells, in the table
@@ -519,7 +519,7 @@ LayoutUnit ComputeBlockSizeForFragmentInternal(
 
   LayoutUnit max = ResolveMaxBlockLength(
       constraint_space, style, border_padding, style.LogicalMaxHeight(),
-      content_size, LengthResolvePhase::kLayout,
+      LengthResolvePhase::kLayout,
       opt_percentage_resolution_block_size_for_min_max);
 
   return ConstrainByMinMax(extent, min, max);
@@ -566,12 +566,12 @@ void ComputeReplacedSize(const NGLayoutInputNode& node,
   LayoutUnit inline_max = ResolveMaxInlineLength(
       space, style, border_padding, child_min_max_size, style.LogicalMaxWidth(),
       LengthResolvePhase::kLayout);
-  LayoutUnit block_min = ResolveMinBlockLength(
-      space, style, border_padding, style.LogicalMinHeight(),
-      border_padding.BlockSum(), LengthResolvePhase::kLayout);
-  LayoutUnit block_max = ResolveMaxBlockLength(
-      space, style, border_padding, style.LogicalMaxHeight(), LayoutUnit::Max(),
-      LengthResolvePhase::kLayout);
+  LayoutUnit block_min = ResolveMinBlockLength(space, style, border_padding,
+                                               style.LogicalMinHeight(),
+                                               LengthResolvePhase::kLayout);
+  LayoutUnit block_max = ResolveMaxBlockLength(space, style, border_padding,
+                                               style.LogicalMaxHeight(),
+                                               LengthResolvePhase::kLayout);
 
   const Length& inline_length = style.LogicalWidth();
   const Length& block_length = style.LogicalHeight();
