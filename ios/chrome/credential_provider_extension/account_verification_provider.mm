9c41030f4f2e3fb44af062741c2ec35def0134d0
David Jean <djean@chromium.org>
2020-02-25 09:35:51 +0000

    [ios] Add account verification provider for credential ext
    Default implementation returns true.
    
    Bug: 1049638
    Change-Id: Ibf58f726f1bd3252ae18bf3ec18703a2b69d7097
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062209
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744227}

---
+++ ios/chrome/credential_provider_extension/account_verification_provider.mm
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/credential_provider_extension/account_verification_provider.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation AccountVerificationProvider
+
+- (void)isValidProviderAccountID:(NSString*)accountID
+               completionHandler:(void (^)(BOOL, NSError*))completionHandler {
+  // Default implementation return always true.
+  dispatch_async(dispatch_get_main_queue(), ^{
+    completionHandler(YES, nil);
+  });
+}
+
+@end
