1b5977b5d9ff851add3af192d43c6c8195a5ccb6
Xi Han <hanxi@google.com>
2020-02-25 18:03:35 +0000

    Cache default value in StringCachedFieldTrialParameter.
    When a field trial parameter doesn't exist in ChromeFeatureList,
    querying ChromeFeatureList#getFieldTrialParamByFeature() will return
    an empty string. This CL changes to cache the default value of this
    field trial parameter when the parameter doesn't exist.
    
    Bug: 1041865
    Change-Id: I2d13a4c349e554d903e004e805dd8ce9d2f691f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2072119
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744306}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/StringCachedFieldTrialParameter.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/StringCachedFieldTrialParameter.java
@@ -32,6 +32,7 @@ public class StringCachedFieldTrialParameter extends CachedFieldTrialParameter {
     void cacheToDisk() {
         String value =
                 ChromeFeatureList.getFieldTrialParamByFeature(getFeatureName(), getParameterName());
-        SharedPreferencesManager.getInstance().writeString(getSharedPreferenceKey(), value);
+        SharedPreferencesManager.getInstance().writeString(
+                getSharedPreferenceKey(), value.isEmpty() ? getDefaultValue() : value);
     }
 }
