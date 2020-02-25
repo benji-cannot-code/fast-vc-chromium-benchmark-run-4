28c383941c65599b09584ba8906b594c2faa2379
yilkal <yilkal@chromium.org>
2020-02-25 01:38:30 +0000

    Implement App Activity Storage for PerAppTimeLimits.
    This cl also saves app activities for AppActivityRegistry.
    The list of applications are stored. The stored information
    includes: the application ids, the application states, the
    running active time of the applications, and the list of
    their activities (i.e. timestamp ranges when the applications
    were active).
    
    The information is stored every five minutes and it is restored
    when AppActivityRegistry is instantiated.
    
    Bug: 1050876
    Change-Id: I7086f7fbf20c969b58d7af0ef398f7716171607d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063677
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744148}

--- chrome/browser/chromeos/child_accounts/time_limits/app_types.cc
+++ chrome/browser/chromeos/child_accounts/time_limits/app_types.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_types.h"
 
+#include <algorithm>
+
 #include "base/logging.h"
 
 namespace chromeos {
@@ -31,6 +33,25 @@ std::string AppTypeToString(apps::mojom::AppType app_type) {
       return "Mac native";
   }
   NOTREACHED();
+}
+
+// static
+bool CanMerge(const AppActivity::ActiveTime& t1,
+              const AppActivity::ActiveTime& t2) {
+  if (t1.active_from() <= t2.active_from() &&
+      t1.active_to() >=
+          t2.active_from() -
+              AppActivity::ActiveTime::kActiveTimeMergePrecision) {
+    return true;
+  }
+
+  if (t2.active_from() <= t1.active_from() &&
+      t2.active_to() >=
+          t1.active_from() -
+              AppActivity::ActiveTime::kActiveTimeMergePrecision) {
+    return true;
+  }
+  return false;
 }
 
 }  // namespace
@@ -91,6 +112,22 @@ AppLimit& AppLimit::operator=(AppLimit&&) = default;
 
 AppLimit::~AppLimit() = default;
 
+// static
+base::Optional<AppActivity::ActiveTime> AppActivity::ActiveTime::Merge(
+    const ActiveTime& t1,
+    const ActiveTime& t2) {
+  if (!CanMerge(t1, t2))
+    return base::nullopt;
+
+  base::Time active_from = std::min(t1.active_from(), t2.active_from());
+  base::Time active_to = std::max(t1.active_to(), t2.active_to());
+  return AppActivity::ActiveTime(active_from, active_to);
+}
+
+// static
+const base::TimeDelta AppActivity::ActiveTime::kActiveTimeMergePrecision =
+    base::TimeDelta::FromSeconds(1);
+
 AppActivity::ActiveTime::ActiveTime(base::Time start, base::Time end)
     : active_from_(start), active_to_(end) {
   DCHECK_GT(active_to_, active_from_);
@@ -136,6 +173,11 @@ AppActivity::AppActivity(AppState app_state)
     : app_state_(app_state),
       running_active_time_(base::TimeDelta::FromSeconds(0)),
       last_updated_time_ticks_(base::TimeTicks::Now()) {}
+AppActivity::AppActivity(AppState app_state,
+                         base::TimeDelta running_active_time)
+    : app_state_(app_state),
+      running_active_time_(running_active_time),
+      last_updated_time_ticks_(base::TimeTicks::Now()) {}
 AppActivity::AppActivity(const AppActivity&) = default;
 AppActivity& AppActivity::operator=(const AppActivity&) = default;
 AppActivity::AppActivity(AppActivity&&) = default;
@@ -143,20 +185,10 @@ AppActivity& AppActivity::operator=(AppActivity&&) = default;
 AppActivity::~AppActivity() = default;
 
 void AppActivity::SetAppState(AppState app_state) {
-  if (is_active_) {
-    // Log the active time before  updating app_state
-    base::TimeTicks now = base::TimeTicks::Now();
-    base::TimeDelta active_time = now - last_updated_time_ticks_;
-
-    base::Time end_time = base::Time::Now();
-    base::Time start_time = end_time - active_time;
-
-    active_times_.push_back(ActiveTime(start_time, end_time));
-    running_active_time_ += active_time;
-  }
-
   app_state_ = app_state;
-  last_updated_time_ticks_ = base::TimeTicks::Now();
+  CaptureOngoingActivity(base::Time::Now());
+  if (!is_active_)
+    last_updated_time_ticks_ = base::TimeTicks::Now();
 }
 
 void AppActivity::SetAppActive(base::Time timestamp) {
@@ -170,34 +202,13 @@ void AppActivity::SetAppActive(base::Time timestamp) {
 void AppActivity::SetAppInactive(base::Time timestamp) {
   if (!is_active_)
     return;
-
-  base::TimeTicks now = base::TimeTicks::Now();
-  base::TimeDelta active_time = now - last_updated_time_ticks_;
-  base::Time start_time = timestamp - active_time;
+  CaptureOngoingActivity(timestamp);
   is_active_ = false;
-  // Timestamps can be equal if SetAppInactive() is called directly after
-  // SetAppState(). Happens in tests.
-  DCHECK_GE(timestamp, start_time);
-  if (timestamp > start_time)
-    active_times_.push_back(ActiveTime(start_time, timestamp));
-
-  running_active_time_ += active_time;
-  last_updated_time_ticks_ = now;
 }
 
 void AppActivity::ResetRunningActiveTime(base::Time timestamp) {
+  CaptureOngoingActivity(timestamp);
   running_active_time_ = base::TimeDelta::FromMinutes(0);
-
-  if (!is_active_)
-    return;
-
-  // Log the active time before the until the reset.
-  base::TimeTicks now = base::TimeTicks::Now();
-  base::TimeDelta active_time = now - last_updated_time_ticks_;
-  base::Time start_time = timestamp - active_time;
-
-  active_times_.push_back(ActiveTime(start_time, timestamp));
-  last_updated_time_ticks_ = now;
 }
 
 base::TimeDelta AppActivity::RunningActiveTime() const {
@@ -206,6 +217,32 @@ base::TimeDelta AppActivity::RunningActiveTime() const {
 
   return running_active_time_ +
          (base::TimeTicks::Now() - last_updated_time_ticks_);
+}
+
+void AppActivity::CaptureOngoingActivity(base::Time timestamp) {
+  if (!is_active_)
+    return;
+
+  // Log the active time before the until the reset.
+  base::TimeTicks now = base::TimeTicks::Now();
+  base::TimeDelta active_time = now - last_updated_time_ticks_;
+
+  // Update |running_active_time_|.
+  running_active_time_ += active_time;
+
+  base::Time start_time = timestamp - active_time;
+
+  // Timestamps can be equal if SetAppInactive() is called directly after
+  // SetAppState(). Happens in tests.
+  DCHECK_GE(timestamp, start_time);
+  if (timestamp > start_time)
+    active_times_.push_back(ActiveTime(start_time, timestamp));
+
+  last_updated_time_ticks_ = now;
+}
+
+std::vector<AppActivity::ActiveTime> AppActivity::TakeActiveTimes() {
+  return std::move(active_times_);
 }
 
 void AppActivity::RemoveActiveTimeEarlierThan(base::Time timestamp) {
@@ -226,5 +263,4 @@ void AppActivity::RemoveActiveTimeEarlierThan(base::Time timestamp) {
 }
 
 }  // namespace app_time
-
 }  // namespace chromeos
--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -256,6 +256,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cert_provisioning/cert_provisioning_common.h"
 #include "chrome/browser/chromeos/child_accounts/parent_access_code/parent_access_service.h"
 #include "chrome/browser/chromeos/child_accounts/screen_time_controller.h"
+#include "chrome/browser/chromeos/child_accounts/time_limits/app_activity_registry.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_time_controller.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/chromeos/crostini/crostini_registry_service.h"
@@ -997,6 +998,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   certificate_manager::CertificatesHandler::RegisterProfilePrefs(registry);
   chromeos::AccountManager::RegisterPrefs(registry);
   chromeos::ApkWebAppService::RegisterProfilePrefs(registry);
+  chromeos::app_time::AppActivityRegistry::RegisterProfilePrefs(registry);
   chromeos::app_time::AppTimeController::RegisterProfilePrefs(registry);
   chromeos::assistant::prefs::RegisterProfilePrefs(registry);
   chromeos::bluetooth::DebugLogsManager::RegisterPrefs(registry);
