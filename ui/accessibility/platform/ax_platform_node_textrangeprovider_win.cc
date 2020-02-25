060ae7f726188572f816d68a9c0c6c4ef97c4ea7
Benjamin Beaudry <bebeaudr@microsoft.com>
2020-02-25 20:50:03 +0000

    Hide children from UIA for some controls per specs
    According to http://www.w3.org/TR/core-aam-1.1/#exclude_elements2, some
    elements (e.g., buttons) needs to hide their children to accessibility
    APIs. Apparently, this constraint is implemented in Narrator and
    explains why navigating with Scan Mode inside of such controls that
    contains other objects (like an image) fails. To an end user, Narrator
    simply 'gets stuck' on such elements.
    
    In CL:1828124, we added AXPlatformNodeWin::ShouldHideChildrenForUIA and
    more to prevent returning the descendant of a node that should hide its
    children and instead returning the ancestor node that shouldn't have
    children in AXPlatformNodeTextRangeProviderWin::GetEnclosingElement.
    
    In AXPlatformNodeTextRangeProviderWin::GetChildren, I noticed that we
    were still returning the children for a node even though that node
    should actually hide its descendants. This change ensures that we don't
    return any child for a node that shouldn't expose its children.
    
    In short:
      1. Added HasPresentationalChildren to ax_role_properties.cc.
      2. Updated the implementation of
         AXPlatformNodeWin::ShouldHideChildrenForUIA to use that new helper.
      3. Moved and renamed BrowserAccessibility::GetDescendants to
         BrowserAccessibilityWin::GetUIADescendants.
      4. Updated the implementation of GetUIADescendants for two reasons:
         1. Fix issue with ignored nodes being returned.
         2. Hide children that for objects that should hide their children.
      5. Added two unit tests:
         1. One for the new implementation of
            BrowserAccessibilityWin::GetUIADescendants.
         2. One for the end user experience with UIA in
              ax_platform_node_textrangeprovider_win_unittest.cc.
    
    Bug: 928948
    Change-Id: I81f392f5991d9e21f911b802fe0fc69b3ec774d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057863
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#744375}

--- ui/accessibility/platform/ax_platform_node_textrangeprovider_win.cc
+++ ui/accessibility/platform/ax_platform_node_textrangeprovider_win.cc
@@ -882,7 +882,7 @@ HRESULT AXPlatformNodeTextRangeProviderWin::GetChildren(SAFEARRAY** children) {
     delegate = node->GetDelegate();
   }
   if (delegate->GetChildCount())
-    descendants = delegate->GetDescendants();
+    descendants = delegate->GetUIADescendants();
 
   SAFEARRAY* safe_array =
       SafeArrayCreateVector(VT_UNKNOWN, 0, descendants.size());
