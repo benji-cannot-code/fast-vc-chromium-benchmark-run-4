f71939908f81b71e22c9ec75e2aa4cbd2dbde34a
Kehuang Li <kehuangli@.chromium.org>
2020-02-25 23:35:22 +0000

    [Chromecast] Replace boolean with enum for CaptureService message type
    It used to be |has_audio| to differentiate audio message and non-audio
    message. But now we have one more type of message, i.e., metadata
    message. Meanwhile, add new util method to populate metadata message.
    
    Merge-With: eureka-internal/357770
    Bug: internal: 140736325
    Test: On device. Unittests.
    Change-Id: Ic89ffb293b75ce303631dda08ace720f949c318c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2049025
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Kehuang Li <kehuangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744436}

--- chromecast/media/audio/capture_service/message_parsing_utils_unittest.cc
+++ chromecast/media/audio/capture_service/message_parsing_utils_unittest.cc
@@ -22,7 +22,7 @@ constexpr size_t kChannels = 2;
 constexpr StreamInfo kStreamInfo =
     StreamInfo{StreamType::kSoftwareEchoCancelled, kChannels,
                SampleFormat::PLANAR_FLOAT, 16000, kFrames};
-constexpr PacketInfo kPacketInfo = {true /* has_audio */, kStreamInfo, 0};
+constexpr PacketInfo kPacketInfo = {MessageType::kAudio, kStreamInfo, 0};
 
 TEST(MessageParsingUtilsTest, ValidPlanarFloat) {
   size_t data_size = kTotalHeaderBytes / sizeof(float) + kFrames * kChannels;
@@ -40,7 +40,7 @@ TEST(MessageParsingUtilsTest, ValidPlanarFloat) {
       ReadHeader(reinterpret_cast<char*>(data.data()) + sizeof(uint16_t),
                  data_size * sizeof(float) - sizeof(uint16_t), &info);
   EXPECT_TRUE(success);
-  EXPECT_TRUE(info.has_audio);
+  EXPECT_EQ(info.message_type, kPacketInfo.message_type);
   EXPECT_EQ(info.stream_info.stream_type, kStreamInfo.stream_type);
   EXPECT_EQ(info.stream_info.num_channels, kStreamInfo.num_channels);
   EXPECT_EQ(info.stream_info.sample_format, kStreamInfo.sample_format);
@@ -113,7 +113,7 @@ TEST(MessageParsingUtilsTest, InvalidType) {
   size_t data_size = kTotalHeaderBytes / sizeof(float);
   std::vector<float> data(data_size, 1.0f);
   PacketInfo packet_info = kPacketInfo;
-  packet_info.has_audio = false;
+  packet_info.message_type = MessageType::kAck;
   PopulateHeader(reinterpret_cast<char*>(data.data()),
                  data.size() * sizeof(float), packet_info);
   *(reinterpret_cast<uint8_t*>(data.data()) + 3) =
@@ -129,7 +129,7 @@ TEST(MessageParsingUtilsTest, InvalidFormat) {
   size_t data_size = kTotalHeaderBytes / sizeof(float);
   std::vector<float> data(data_size, 1.0f);
   PacketInfo packet_info = kPacketInfo;
-  packet_info.has_audio = false;
+  packet_info.message_type = MessageType::kAck;
   PopulateHeader(reinterpret_cast<char*>(data.data()),
                  data.size() * sizeof(float), packet_info);
   *(reinterpret_cast<uint8_t*>(data.data()) + 5) =
@@ -141,11 +141,11 @@ TEST(MessageParsingUtilsTest, InvalidFormat) {
   EXPECT_FALSE(success);
 }
 
-TEST(MessageParsingUtilsTest, RequestMessage) {
+TEST(MessageParsingUtilsTest, AckMessage) {
   size_t data_size = kTotalHeaderBytes / sizeof(float);
   std::vector<float> data(data_size, 1.0f);
   PacketInfo packet_info = kPacketInfo;
-  packet_info.has_audio = false;
+  packet_info.message_type = MessageType::kAck;
   PopulateHeader(reinterpret_cast<char*>(data.data()),
                  data.size() * sizeof(float), packet_info);
 
@@ -154,7 +154,7 @@ TEST(MessageParsingUtilsTest, RequestMessage) {
       ReadHeader(reinterpret_cast<char*>(data.data()) + sizeof(uint16_t),
                  data_size * sizeof(float) - sizeof(uint16_t), &info);
   EXPECT_TRUE(success);
-  EXPECT_FALSE(info.has_audio);
+  EXPECT_EQ(info.message_type, MessageType::kAck);
   EXPECT_EQ(info.stream_info.stream_type, kStreamInfo.stream_type);
   EXPECT_EQ(info.stream_info.num_channels, kStreamInfo.num_channels);
   EXPECT_EQ(info.stream_info.sample_format, kStreamInfo.sample_format);
