bb9cca8a79b556842e9e7a4fca136dc96727e25a
Sahel Sharify <sahel@chromium.org>
2020-02-25 18:17:47 +0000

    [Payments] Browser tests inherit PaymentRequestPlatformBrowserTestBase
    This cl changes the remaining payments PlatformBrowserTests to use
    utility functions from the newly added PaymentRequestPlatformBrowserTestBase
    rather than redefininig them for each test class.
    
    Bug: 1052169
    Change-Id: I1e621d8ef00e34e55419b51890b79cfe7d83f8e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063666
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744314}

--- chrome/browser/payments/expandable_payment_handler_browsertest.cc
+++ chrome/browser/payments/expandable_payment_handler_browsertest.cc
@@ -4,59 +4,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/test/scoped_feature_list.h"
-#include "build/build_config.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
-#include "chrome/test/base/android/android_browser_test.h"
-#include "chrome/test/base/chrome_test_utils.h"
-#include "chrome/test/payments/payment_request_test_controller.h"
-#include "content/public/browser/web_contents.h"
-#include "content/public/common/content_switches.h"
+#include "chrome/test/payments/payment_request_platform_browsertest_base.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
 namespace payments {
 namespace {
 
-class ExpandablePaymentHandlerBrowserTest : public PlatformBrowserTest {
+class ExpandablePaymentHandlerBrowserTest
+    : public PaymentRequestPlatformBrowserTestBase {
  public:
   ExpandablePaymentHandlerBrowserTest()
-      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS),
-        http_server_(net::EmbeddedTestServer::TYPE_HTTP) {
+      : http_server_(net::EmbeddedTestServer::TYPE_HTTP) {
     scoped_feature_list_.InitWithFeatures(
         /*enabled_features=*/{chrome::android::kScrollToExpandPaymentHandler},
         /*disabled_features=*/{});
   }
 
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    command_line->AppendSwitch(
-        switches::kEnableExperimentalWebPlatformFeatures);
-  }
-
   void SetUpOnMainThread() override {
-    https_server_.ServeFilesFromSourceDirectory(
-        "components/test/data/payments/");
-    ASSERT_TRUE(https_server_.Start());
-    ASSERT_TRUE(content::NavigateToURL(
-        GetActiveWebContents(),
-        https_server_.GetURL("/maxpay.com/merchant.html")));
+    PaymentRequestPlatformBrowserTestBase::SetUpOnMainThread();
+    NavigateTo("/maxpay.com/merchant.html");
+
+    // Start http server.
     http_server_.ServeFilesFromSourceDirectory(
         "components/test/data/payments/");
     ASSERT_TRUE(http_server_.Start());
-    test_controller_.SetUpOnMainThread();
-    PlatformBrowserTest::SetUpOnMainThread();
   }
 
   GURL GetHttpPageUrl() {
     return http_server_.GetURL("/maxpay.com/merchant.html");
   }
 
-  content::WebContents* GetActiveWebContents() {
-    return chrome_test_utils::GetActiveWebContents(this);
-  }
-
-  PaymentRequestTestController test_controller_;
-
  private:
-  net::EmbeddedTestServer https_server_;
   net::EmbeddedTestServer http_server_;
   base::test::ScopedFeatureList scoped_feature_list_;
 };
@@ -70,9 +49,9 @@ IN_PROC_BROWSER_TEST_F(ExpandablePaymentHandlerBrowserTest, ConfirmPayment) {
                 GetActiveWebContents(),
                 "launchAndWaitUntilReady('./payment_handler_window.html')"));
 
-  DCHECK(test_controller_.GetPaymentHandlerWebContents());
+  DCHECK(test_controller()->GetPaymentHandlerWebContents());
   EXPECT_EQ("confirmed",
-            content::EvalJs(test_controller_.GetPaymentHandlerWebContents(),
+            content::EvalJs(test_controller()->GetPaymentHandlerWebContents(),
                             "confirm()"));
   EXPECT_EQ("success", content::EvalJs(GetActiveWebContents(), "getResult()"));
 }
@@ -86,8 +65,8 @@ IN_PROC_BROWSER_TEST_F(ExpandablePaymentHandlerBrowserTest, ClickSecurityIcon) {
                 GetActiveWebContents(),
                 "launchAndWaitUntilReady('./payment_handler_window.html')"));
 
-  DCHECK(test_controller_.GetPaymentHandlerWebContents());
-  EXPECT_TRUE(test_controller_.ClickPaymentHandlerSecurityIcon());
+  DCHECK(test_controller()->GetPaymentHandlerWebContents());
+  EXPECT_TRUE(test_controller()->ClickPaymentHandlerSecurityIcon());
 }
 
 // Make sure merchants can cancel the payment.
@@ -99,9 +78,9 @@ IN_PROC_BROWSER_TEST_F(ExpandablePaymentHandlerBrowserTest, CancelPayment) {
                 GetActiveWebContents(),
                 "launchAndWaitUntilReady('./payment_handler_window.html')"));
 
-  DCHECK(test_controller_.GetPaymentHandlerWebContents());
+  DCHECK(test_controller()->GetPaymentHandlerWebContents());
   EXPECT_EQ("canceled",
-            content::EvalJs(test_controller_.GetPaymentHandlerWebContents(),
+            content::EvalJs(test_controller()->GetPaymentHandlerWebContents(),
                             "cancel()"));
   EXPECT_EQ("unknown", content::EvalJs(GetActiveWebContents(), "getResult()"));
 }
@@ -115,9 +94,9 @@ IN_PROC_BROWSER_TEST_F(ExpandablePaymentHandlerBrowserTest, PaymentFailed) {
                 GetActiveWebContents(),
                 "launchAndWaitUntilReady('./payment_handler_window.html')"));
 
-  DCHECK(test_controller_.GetPaymentHandlerWebContents());
+  DCHECK(test_controller()->GetPaymentHandlerWebContents());
   EXPECT_EQ("failed",
-            content::EvalJs(test_controller_.GetPaymentHandlerWebContents(),
+            content::EvalJs(test_controller()->GetPaymentHandlerWebContents(),
                             "fail()"));
   EXPECT_EQ("fail", content::EvalJs(GetActiveWebContents(), "getResult()"));
 }
@@ -142,9 +121,9 @@ IN_PROC_BROWSER_TEST_F(ExpandablePaymentHandlerBrowserTest, WindowClientReady) {
                 GetActiveWebContents(),
                 "launchAndWaitUntilReady('./payment_handler_window.html')"));
 
-  DCHECK(test_controller_.GetPaymentHandlerWebContents());
+  DCHECK(test_controller()->GetPaymentHandlerWebContents());
   EXPECT_EQ(true,
-            content::EvalJs(test_controller_.GetPaymentHandlerWebContents(),
+            content::EvalJs(test_controller()->GetPaymentHandlerWebContents(),
                             "isWindowClientReady()"));
 }
 }  // namespace
--- chrome/test/payments/payment_request_test_controller.h
+++ chrome/test/payments/payment_request_test_controller.h
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace sync_preferences {
 class TestingPrefServiceSyncable;
 }
-#else
+#endif
+
 namespace content {
 class WebContents;
 }
-#endif
 
 namespace payments {
 
@@ -57,13 +57,14 @@ class PaymentRequestTestController {
   void SetIncognito(bool is_incognito);
   void SetValidSsl(bool valid_ssl);
   void SetCanMakePaymentEnabledPref(bool can_make_payment_enabled);
-#if defined(OS_ANDROID)
-  // Get the WebContents of the Expandable Payment Handler for testing purpose,
-  // or null if nonexistent. To guarantee a non-null return, this function
-  // should be called only if: 1) PaymentRequest UI is opening. 2)
-  // ScrollToExpandPaymentHandler feature is enabled. 3) PaymentHandler is
-  // opening.
+
+  // Get the WebContents of the Payment Handler for testing purpose, or null if
+  // nonexistent. To guarantee a non-null return, this function should be called
+  // only if: 1) PaymentRequest UI is opening. 2) ScrollToExpandPaymentHandler
+  // feature is enabled (on Android). 3) PaymentHandler is opening.
   content::WebContents* GetPaymentHandlerWebContents();
+
+#if defined(OS_ANDROID)
   // Click the security icon on the Expandable Payment Handler toolbar for
   // testing purpose. return whether it's succeeded.
   bool ClickPaymentHandlerSecurityIcon();
