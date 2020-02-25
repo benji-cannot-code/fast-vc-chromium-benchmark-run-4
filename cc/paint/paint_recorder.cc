3f911cd500404ae1e85165bfd2a1bbb2175eb134
Jonah Chin <jochin@microsoft.com>
2020-02-25 02:37:17 +0000

    Reland "Add a PaintRecorder to CanvasResourceProvider"
    This is a reland of 2a3b118991ac0ddd19d5bd5eacb150fc82a6f6b1
    
    To fix the perf regression from the original CL (as seen in
    crbug.com/1054408):
    In Canvas2DLayerBridge::GetPaintCanvas, avoid the call to
    GetOrCreateResourceProvider() to skip the IsValid/ContextLost checks.
    GetPaintCanvas is used in a hot code path that is called by each
    javascript command, so checking the context every time we fetch the
    recording canvas results in unnecessary and expensive work.
    The context does not need to be valid at this point, since only the
    recording canvas is being used.
    
    TBR=fserb@chromium.org
    
    Original change's description:
    > Add a PaintRecorder to CanvasResourceProvider
    >
    > This CL is the first step in creating a PaintRecord backed resource
    > provider for canvas OOP-R.
    >
    > Changes of ownership:
    > - PaintRecorder entirely moved from Canvas2DLayerBridge to
    >   CanvasResourceProvider.
    > - Management of needs_flush_ for MemoryManagedPaintRecorder moved to
    >   CanvasResourceProvider.
    > - RestoreCanvasMatrixClipStack logic centralized to CanvasResourceHost
    >   and can be accessed via callback
    > - Backing SkiaPaintCanvas now private to CanvasResourceProvider with no
    >   external access. Any users of the backing SkiaPaintCanvas in
    >   Canvas2DLayerBridge had their functionality moved to
    >   CanvasResourceProvider (ie. new RestoreBackBuffer() function)
    >
    > Updates to test files:
    > - Addition of explicit FlushCanvas() calls in tests that used to expect
    >   SkiaPaintCanvas to immediately update.
    > - Some tests expected the CanvasResourceProvider to be created on first
    >   draw. This expectation has been changed since the
    >   CanvasResourceProvider now gets set up on SetCanvasResourceHost().
    >
    > Other new functionality:
    > - Display Item List now has a bit that tracks if it contains any draw
    >   operations. This is used to see if there are draw ops to flush.
    >
    > Bug: 1019288
    > Change-Id: I717b18e22d6699dc876d8f8121a25d147738579d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1891292
    > Commit-Queue: Khushal <khushalsagar@chromium.org>
    > Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    > Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    > Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#743002}
    
    Bug: 1019288
    Change-Id: I4ad88fbaeff89cff665ea53038091ec2a814eac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067557
    Commit-Queue: Jonah Chin <jochin@microsoft.com>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Juanmi Huertas <juanmihd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744160}

--- cc/paint/paint_recorder.cc
+++ cc/paint/paint_recorder.cc
@@ -48,4 +48,8 @@ std::unique_ptr<RecordPaintCanvas> PaintRecorder::CreateCanvas(
   return std::make_unique<RecordPaintCanvas>(list, bounds);
 }
 
+bool PaintRecorder::ListHasDrawOps() const {
+  return display_item_list_->has_draw_ops();
+}
+
 }  // namespace cc
