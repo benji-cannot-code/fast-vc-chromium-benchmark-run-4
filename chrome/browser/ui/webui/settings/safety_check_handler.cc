74c95aea1bd9922cc427b953b8ec6b2065bfd904
Rainhard Findling <rainhard@chromium.org>
2020-02-25 08:47:17 +0000

    Safety check browser proxy
    * Adds the JS side of the safety check browser proxy.
    * Introduces the same API on JS side as used on the C++ side
      (https://crrev.com/c/2049982).
    * Adds states for the safety check children elements in the form of
      Polymer properties, which will be used by the safety check children
      elements in the future.
    
    Bug: 1015841
    Change-Id: I4bd95e2970a018b0d3c8e63114582c1f4e2cb60b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2059375
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744217}

--- chrome/browser/ui/webui/settings/safety_check_handler.cc
+++ chrome/browser/ui/webui/settings/safety_check_handler.cc
@@ -15,7 +15,6 @@ namespace {
 
 // Constants for communication with JS.
 static constexpr char kStatusChanged[] = "safety-check-status-changed";
-static constexpr char kInitialize[] = "initializeSafetyCheck";
 static constexpr char kPerformSafetyCheck[] = "performSafetyCheck";
 static constexpr char kSafetyCheckComponent[] = "safetyCheckComponent";
 static constexpr char kNewState[] = "newState";
@@ -55,6 +54,7 @@ SafetyCheckHandler::SafetyCheckHandler()
 SafetyCheckHandler::~SafetyCheckHandler() = default;
 
 void SafetyCheckHandler::PerformSafetyCheck() {
+  AllowJavascript();
   if (!version_updater_) {
     version_updater_.reset(VersionUpdater::Create(web_ui()->GetWebContents()));
   }
@@ -66,10 +66,6 @@ SafetyCheckHandler::SafetyCheckHandler(
     std::unique_ptr<VersionUpdater> version_updater,
     SafetyCheckHandlerObserver* observer)
     : version_updater_(std::move(version_updater)), observer_(observer) {}
-
-void SafetyCheckHandler::HandleInitialize(const base::ListValue* /*args*/) {
-  AllowJavascript();
-}
 
 void SafetyCheckHandler::HandlePerformSafetyCheck(
     const base::ListValue* /*args*/) {
@@ -141,9 +137,6 @@ void SafetyCheckHandler::OnJavascriptAllowed() {}
 void SafetyCheckHandler::OnJavascriptDisallowed() {}
 
 void SafetyCheckHandler::RegisterMessages() {
-  web_ui()->RegisterMessageCallback(
-      kInitialize, base::BindRepeating(&SafetyCheckHandler::HandleInitialize,
-                                       base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
       kPerformSafetyCheck,
       base::BindRepeating(&SafetyCheckHandler::HandlePerformSafetyCheck,
