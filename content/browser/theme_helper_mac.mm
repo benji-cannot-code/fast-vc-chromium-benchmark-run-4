5179a5554fb7013691db077c5b6b2a5d52912803
Rune Lillesveen <futhark@chromium.org>
2020-02-26 13:27:14 +0000

    Load Mac system colors for both light and dark scheme.
    System colors used in CSS should per spec depend on the used
    color-scheme for that element. Since the used color-scheme may not be
    the same as the preferred color-scheme, we need to provide the system
    colors for both light and dark at the same time. This CL temporarily
    sets the appearance to light and dark aqua to retrieve those colors.
    
    TEST=StyleEngineTest.InitialColorChange
    
    Bug: 929098
    Change-Id: I4bb9553cf1b2fc4e98cd1c7c13041adce3e50353
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1860023
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744630}

--- content/browser/theme_helper_mac.mm
+++ content/browser/theme_helper_mac.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/command_line.h"
+#include "base/containers/span.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
@@ -301,7 +302,8 @@ ThemeHelperMac::DuplicateReadOnlyColorMapRegion() {
 ThemeHelperMac::ThemeHelperMac() {
   // Allocate a region for the SkColor value table and map it.
   auto writable_region = base::WritableSharedMemoryRegion::Create(
-      sizeof(SkColor) * blink::kMacSystemColorIDCount);
+      sizeof(SkColor) * blink::kMacSystemColorIDCount *
+      blink::kMacSystemColorSchemeCount);
   writable_color_map_ = writable_region.Map();
   // Downgrade the region to read-only after it has been mapped.
   read_only_color_map_ = base::WritableSharedMemoryRegion::ConvertToReadOnly(
@@ -322,20 +324,8 @@ ThemeHelperMac::~ThemeHelperMac() {
   [theme_observer_ release];
 }
 
-void ThemeHelperMac::LoadSystemColors() {
-  base::span<SkColor> values = writable_color_map_.GetMemoryAsSpan<SkColor>(
-      blink::kMacSystemColorIDCount);
-  // Ensure light mode appearance in web content even if the topchrome is in
-  // dark mode.
-  // TODO(lgrey): Add a second map for content dark mode for the
-  // `prefers-color-scheme` media query: https://crbug.com/889087.
-  NSAppearance* savedAppearance;
-  if (@available(macOS 10.14, *)) {
-    savedAppearance = [NSAppearance currentAppearance];
-    [NSAppearance
-        setCurrentAppearance:[NSAppearance
-                                 appearanceNamed:NSAppearanceNameAqua]];
-  }
+void ThemeHelperMac::LoadSystemColorsForCurrentAppearance(
+    base::span<SkColor> values) {
   for (size_t i = 0; i < blink::kMacSystemColorIDCount; ++i) {
     blink::MacSystemColorID color_id = static_cast<blink::MacSystemColorID>(i);
     switch (color_id) {
@@ -432,9 +422,39 @@ void ThemeHelperMac::LoadSystemColors() {
         break;
     }
   }
+}
+
+void ThemeHelperMac::LoadSystemColors() {
+  static_assert(blink::kMacSystemColorSchemeCount == 2,
+                "Light and dark color scheme system colors loaded.");
+  base::span<SkColor> values = writable_color_map_.GetMemoryAsSpan<SkColor>(
+      blink::kMacSystemColorIDCount * blink::kMacSystemColorSchemeCount);
+
+  NSAppearance* savedAppearance;
   if (@available(macOS 10.14, *)) {
-    [NSAppearance setCurrentAppearance:savedAppearance];
+    savedAppearance = [NSAppearance currentAppearance];
+    // Ensure light mode appearance in web content even if the topchrome is in
+    // dark mode.
+    [NSAppearance
+        setCurrentAppearance:[NSAppearance
+                                 appearanceNamed:NSAppearanceNameAqua]];
   }
+
+  LoadSystemColorsForCurrentAppearance(
+      values.subspan(0, static_cast<size_t>(blink::MacSystemColorID::kCount)));
+
+  if (@available(macOS 10.14, *)) {
+    [NSAppearance
+        setCurrentAppearance:[NSAppearance
+                                 appearanceNamed:NSAppearanceNameDarkAqua]];
+  }
+
+  LoadSystemColorsForCurrentAppearance(
+      values.subspan(static_cast<size_t>(blink::MacSystemColorID::kCount),
+                     static_cast<size_t>(blink::MacSystemColorID::kCount)));
+
+  if (@available(macOS 10.14, *))
+    [NSAppearance setCurrentAppearance:savedAppearance];
 }
 
 void ThemeHelperMac::Observe(int type,
--- third_party/blink/public/platform/mac/web_sandbox_support.h
+++ third_party/blink/public/platform/mac/web_sandbox_support.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_cftyperef.h"
 #include "third_party/blink/public/common/sandbox_support/sandbox_support_mac.h"
+#include "third_party/blink/public/platform/web_color_scheme.h"
 #include "third_party/skia/include/core/SkColor.h"
 
 typedef struct CGFont* CGFontRef;
@@ -57,7 +58,7 @@ class WebSandboxSupport {
       uint32_t* font_id) = 0;
 
   // Returns the system's preferred value for a named color.
-  virtual SkColor GetSystemColor(MacSystemColorID) = 0;
+  virtual SkColor GetSystemColor(MacSystemColorID, WebColorScheme) = 0;
 };
 
 }  // namespace blink
