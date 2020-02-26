b6fefd90c6aa3940947cd24a356cf1a4f8fff22f
Brandon Wylie <wylieb@chromium.org>
2020-02-26 00:03:30 +0000

    Fix for the Assistant voice search service being null
    Returning early from the function if the service is null to guard
    against a potential race condition affecting beta.
    
    Bug: 1055274
    Change-Id: Ibb419037e67b2bce7cc31b3f0371ed1264ca01fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2070823
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744450}

--- chrome/android/junit/src/org/chromium/chrome/browser/omnibox/voice/AssistantVoiceSearchServiceUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omnibox/voice/AssistantVoiceSearchServiceUnitTest.java
@@ -9,10 +9,12 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.drawable.Drawable;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -22,7 +24,9 @@ import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.SysUtils;
@@ -56,8 +60,6 @@ public class AssistantVoiceSearchServiceUnitTest {
     @Mock
     GSAState mGsaState;
     @Mock
-    Context mContext;
-    @Mock
     PackageManager mPackageManager;
     @Mock
     TemplateUrlService mTemplateUrlService;
@@ -65,10 +67,13 @@ public class AssistantVoiceSearchServiceUnitTest {
     ExternalAuthUtils mExternalAuthUtils;
 
     PackageInfo mPackageInfo;
+    Context mContext;
 
     @Before
     public void setUp() throws NameNotFoundException {
         MockitoAnnotations.initMocks(this);
+
+        mContext = Mockito.spy(Robolectric.buildActivity(Activity.class).setup().get());
 
         doReturn(true).when(mExternalAuthUtils).isChromeGoogleSigned();
         doReturn(true).when(mExternalAuthUtils).isGoogleSigned(IntentHandler.PACKAGE_GSA);
@@ -182,5 +187,15 @@ public class AssistantVoiceSearchServiceUnitTest {
     public void getMicButtonColorStateList_ColorfulMicEnabled() {
         mAssistantVoiceSearchService.setColorfulMicEnabledForTesting(true);
         Assert.assertNull(mAssistantVoiceSearchService.getMicButtonColorStateList(0, mContext));
+    }
+
+    @Test
+    @Feature("OmniboxAssistantVoiceSearch")
+    public void getCurrentMicDrawable() {
+        Drawable greyMic = mAssistantVoiceSearchService.getCurrentMicDrawable();
+        mAssistantVoiceSearchService.setColorfulMicEnabledForTesting(true);
+        Drawable colorfulMic = mAssistantVoiceSearchService.getCurrentMicDrawable();
+
+        Assert.assertNotEquals(greyMic, colorfulMic);
     }
 }
