9358a03ff5987851e2ae951e2e29756b35812caf
Side Yilmaz <sideyilmaz@chromium.org>
2020-02-26 09:19:14 +0000

    Replace deprecated getLastUsedProfile function in WebContentsFactory.
    |Profile#getLastUsedProfile| is depreciated and replaced with
    |Profile#getLastUsedRegularProfile|. This CL updates WebContentsFactory
    class.
    
    Note: This change is only code clean-up, does not change any behavior.
    
    Bug: 1041781
    Change-Id: I78995577933aba3cf06e3720ae5ccd0d7b77851f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2069322
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744584}

--- chrome/android/java/src/org/chromium/chrome/browser/WebContentsFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/WebContentsFactory.java
@@ -31,7 +31,7 @@ public class WebContentsFactory {
     // TODO(pshmakov): remove static for unit-testability.
     public static WebContents createWebContents(boolean incognito, boolean initiallyHidden) {
         return WebContentsFactoryJni.get().createWebContents(
-                Profile.getLastUsedProfile(), incognito, initiallyHidden, false);
+                Profile.getLastUsedRegularProfile(), incognito, initiallyHidden, false);
     }
 
     /**
@@ -47,7 +47,7 @@ public class WebContentsFactory {
     public WebContents createWebContentsWithWarmRenderer(
             boolean incognito, boolean initiallyHidden) {
         return WebContentsFactoryJni.get().createWebContents(
-                Profile.getLastUsedProfile(), incognito, initiallyHidden, true);
+                Profile.getLastUsedRegularProfile(), incognito, initiallyHidden, true);
     }
 
     @NativeMethods
