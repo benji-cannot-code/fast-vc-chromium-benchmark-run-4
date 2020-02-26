ee7903b9323e8c8f90e862d9de9bcb0398c9fac5
Kevin Ellis <kevers@chromium.org>
2020-02-26 14:15:19 +0000

    Mark animation outdated after changing playback rate.
    Bug: 1052217
    Change-Id: I9495cf7fb844083c88f796214561cbe0476b19c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2072653
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744649}

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -1333,6 +1333,9 @@ void Animation::updatePlaybackRate(double playback_rate,
       }
       ApplyPendingPlaybackRate();
       UpdateFinishedState(UpdateType::kContinuous, NotificationType::kAsync);
+      SetCompositorPending(false);
+      SetOutdated();
+      NotifyProbe();
       break;
     }
 
@@ -1452,6 +1455,8 @@ void Animation::setPlaybackRate(double playback_rate,
   }
 
   SetCompositorPending(false);
+  SetOutdated();
+  NotifyProbe();
 }
 
 void Animation::ClearOutdated() {
