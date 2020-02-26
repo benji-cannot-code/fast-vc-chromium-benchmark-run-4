78d6c2d66d95d20b43604ad3232fd4de82a214ff
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-02-26 20:11:39 +0000

    Import wpt@8f139108406f740e8c005f8917ddfb2320a9dd34
    Using wpt-import in Chromium 1e0fbe993b324d1be5c9dd2c7f2f2d783f32ce42.
    With Chromium commits locally applied on WPT:
    d5cd6b98a6 "SharedWorker: Assign unique names to SharedWorkers to avoid unintentional matching"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: Ida885dd0df211a3f8f980899ff58e0ef11bbc197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2074755
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#744767}

--- third_party/blink/web_tests/external/wpt/fetch/api/resources/trickle.py
+++ third_party/blink/web_tests/external/wpt/fetch/api/resources/trickle.py
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import time
 
+from six.moves import xrange
+
 def main(request, response):
     delay = float(request.GET.first("ms", 500)) / 1E3
     count = int(request.GET.first("count", 50))
