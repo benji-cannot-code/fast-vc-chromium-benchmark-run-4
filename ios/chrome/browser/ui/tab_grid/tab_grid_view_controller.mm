284b8100db331b5d6a72428336c4a22ca998bf0e
Gauthier Ambard <gambard@chromium.org>
2020-02-26 17:38:39 +0000

    [iOS] Fix bookkeeping for RecentTabs presentation
    This CL makes sure that the animations of the RecentTabs are disabled
    when the recent tabs aren't presented when BVC is contained.
    
    Bug: 1038034
    Change-Id: Ie458cb6b27bcba66637d5b9dad63fc5e6809b8a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071938
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744707}

--- ios/chrome/browser/ui/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_view_controller.mm
@@ -379,6 +379,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   if (base::FeatureList::IsEnabled(kContainedBVC)) {
     [self.incognitoTabsViewController contentWillAppearAnimated:animated];
     [self.regularTabsViewController contentWillAppearAnimated:animated];
+    self.remoteTabsViewController.preventUpdates = NO;
   }
 }
 
@@ -406,6 +407,7 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   if (base::FeatureList::IsEnabled(kContainedBVC)) {
     [self.incognitoTabsViewController contentWillDisappear];
     [self.regularTabsViewController contentWillDisappear];
+    self.remoteTabsViewController.preventUpdates = YES;
   }
 }
 
