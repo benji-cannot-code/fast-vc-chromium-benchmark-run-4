3b5c4d6070ff2253035253f4ff5c0ba4c6fabaa2
Esmael El-Moslimany <aee@chromium.org>
2020-02-26 01:11:20 +0000

    WebUI NTP: add support for chrome-untrusted://new-tab-page/promo
    Bug: 1041127, 1044269
    Change-Id: I8cbb50e4f0b63fe75591e415736684efc4dd2b15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068745
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744478}

--- chrome/common/url_constants.cc
+++ chrome/common/url_constants.cc
@@ -90,6 +90,9 @@ const char kChromeSearchRemoteNtpHost[] = "remote-ntp";
 
 const char kChromeSearchScheme[] = "chrome-search";
 
+const char kChromeUIUntrustedNewTabPageUrl[] =
+    "chrome-untrusted://new-tab-page/";
+
 const char kChromiumProjectURL[] = "https://www.chromium.org/";
 
 const char kCloudPrintLearnMoreURL[] =
