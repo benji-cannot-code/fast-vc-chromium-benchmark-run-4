cbc6db6cc6ae77de6b2480770f32b256e9f3534d
Ken MacKay <kmackay@chromium.org>
2020-02-26 04:41:20 +0000

    [Chromecast] Add ExternalAudioDecoder API
    This CL adds the ability for additional audio decoding to be added as a
    dynamically-loaded library. The default location for the library is
    /system/chrome/lib/libexternal_decoder.so, but it can be changed via the
    --external-audio-decoder-lib-path command-line arg.
    
    The dynamic library must export the correct functions as described in
    external_audio_decoder.h. At runtime, for any audio codec that
    ExternalAudioDecoder_IsSupportedConfig() returns true, an external audio
    decoder will be used (the internal decoder will be bypassed completely
    even if it would also support decoding that codec). See the
    documentation in external_audio_decoder.h for more details.
    
    Bug: internal b/148685511
    Change-Id: I58afbe5a24d1b0cb4751621c75316abd5b2bb97b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2072163
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744546}

--- chromecast/media/cma/decoder/cast_audio_decoder.cc
+++ chromecast/media/cma/decoder/cast_audio_decoder.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromecast/media/cma/base/decoder_buffer_base.h"
 #include "chromecast/media/cma/base/decoder_config_adapter.h"
 #include "chromecast/media/cma/base/decoder_config_logging.h"
+#include "chromecast/media/cma/decoder/external_audio_decoder_wrapper.h"
 #include "media/base/audio_buffer.h"
 #include "media/base/audio_bus.h"
 #include "media/base/cdm_context.h"
@@ -40,17 +41,13 @@ namespace {
 class CastAudioDecoderImpl : public CastAudioDecoder {
  public:
   CastAudioDecoderImpl(scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+                       const media::AudioConfig& config,
                        OutputFormat output_format)
       : task_runner_(std::move(task_runner)),
         output_format_(output_format),
         weak_factory_(this) {
     weak_this_ = weak_factory_.GetWeakPtr();
     DCHECK(task_runner_);
-  }
-
-  bool Initialize(const media::AudioConfig& config) {
-    DCHECK(task_runner_->RunsTasksInCurrentSequence());
-    DCHECK(!initialized_);
 
     input_config_ = config;
     input_config_.encryption_scheme = EncryptionScheme::kUnencrypted;
@@ -71,7 +68,6 @@ class CastAudioDecoderImpl : public CastAudioDecoder {
         base::NullCallback());
     // Unfortunately there is no result from decoder_->Initialize() until later
     // (the pipeline status callback is posted to the task runner).
-    return true;
   }
 
   // CastAudioDecoder implementation:
@@ -297,12 +293,17 @@ std::unique_ptr<CastAudioDecoder> CastAudioDecoder::Create(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     const media::AudioConfig& config,
     OutputFormat output_format) {
-  auto decoder = std::make_unique<CastAudioDecoderImpl>(std::move(task_runner),
-                                                        output_format);
-  if (!decoder->Initialize(config)) {
-    decoder.reset();
+  if (ExternalAudioDecoderWrapper::IsSupportedConfig(config)) {
+    auto external_decoder = std::make_unique<ExternalAudioDecoderWrapper>(
+        std::move(task_runner), config, output_format);
+    if (!external_decoder->initialized()) {
+      return nullptr;
+    }
+    return external_decoder;
   }
-  return decoder;
+
+  return std::make_unique<CastAudioDecoderImpl>(std::move(task_runner), config,
+                                                output_format);
 }
 
 // static
