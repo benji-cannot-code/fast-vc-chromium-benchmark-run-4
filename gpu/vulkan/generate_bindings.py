949bc2b11e9ebec542e2fd9e7d4d32cbdf244edb
Peng Huang <penghuang@chromium.org>
2020-02-26 20:54:49 +0000

    Use macro to simplify class VulkanFunctionPointers
    Bug: None
    Change-Id: Id4ef48633b81f575ff0fb863bbc34fdd929be880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2075048
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744783}

--- gpu/vulkan/generate_bindings.py
+++ gpu/vulkan/generate_bindings.py
@@ -292,13 +292,7 @@ def WriteFunctionDeclarations(file, functions):
   WriteFunctions(file, functions, template)
 
 def WriteTemplateMethods(file, functions):
-  template = Template('''
-template <typename ... Types>
-NO_SANITIZE("cfi-icall")
-auto ${name}Fn(Types ... args) -> decltype(${name}Fn_(args...)) {
-  return ${name}Fn_(args...);
-}
-''')
+  template = Template('  DEFINE_METHOD(${name})\n')
   WriteFunctions(file, functions, template)
 
 def WriteMacros(file, functions):
@@ -403,6 +397,14 @@ class VulkanFunctionPointers {
 
   file.write("""
  public:
+#define DEFINE_METHOD(name)                \
+  template <typename... Types>             \
+  NO_SANITIZE("cfi-icall")                 \
+  auto name ## Fn(Types... args)           \
+      -> decltype(name ## Fn_(args...)) {  \
+    return name ## Fn_(args...);           \
+  }
+
   // Unassociated functions
 """)
   WriteTemplateMethods(file, [{'functions': [ 'vkGetInstanceProcAddr' ,
@@ -424,6 +426,7 @@ class VulkanFunctionPointers {
   WriteTemplateMethods(file, VULKAN_DEVICE_FUNCTIONS)
 
   file.write("""\
+#undef DEFINE_METHOD
 };
 
 }  // namespace gpu
@@ -451,8 +454,7 @@ class VulkanFunctionPointers {
 
   file.write("""\
 
-#endif  // GPU_VULKAN_VULKAN_FUNCTION_POINTERS_H_
-""")
+#endif  // GPU_VULKAN_VULKAN_FUNCTION_POINTERS_H_""")
 
 def WriteFunctionPointerInitialization(file, proc_addr_function, parent,
                                        functions):
