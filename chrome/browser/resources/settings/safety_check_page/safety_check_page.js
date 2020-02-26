09a332598093713f4882a4c009f1014514bb0903
Rainhard Findling <rainhard@chromium.org>
2020-02-26 11:55:45 +0000

    Safety check UI: collapse for children elements
    * Adds the safety check collapse for safety check children elements
    * Adds common styling used by multiple children
    
    Bug: 1015841
    Change-Id: I8dfb3d325d4134d7804155ee3d1b8af822c44cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2070239
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744613}

--- chrome/browser/resources/settings/safety_check_page/safety_check_page.js
+++ chrome/browser/resources/settings/safety_check_page/safety_check_page.js
@@ -156,27 +156,22 @@ Polymer({
         this.updatesStatus_ = status;
         break;
       case SafetyCheckComponent.PASSWORDS:
-        this.passwordsStatus_ = status;
         this.passwordsCompromisedCount_ = event['passwordsCompromised'];
+        this.passwordsStatus_ = status;
         break;
       case SafetyCheckComponent.SAFE_BROWSING:
         this.safeBrowsingStatus_ = status;
         break;
       case SafetyCheckComponent.EXTENSIONS:
-        this.extensionsStatus_ = status;
         this.badExtensionsCount_ = event['badExtensions'];
+        this.extensionsStatus_ = status;
         break;
       default:
         assertNotReached();
     }
 
     // If all children elements received updates: update parent element.
-    if (this.updatesStatus_ != settings.SafetyCheckUpdatesStatus.CHECKING &&
-        this.passwordsStatus_ != settings.SafetyCheckPasswordsStatus.CHECKING &&
-        this.safeBrowsingStatus_ !=
-            settings.SafetyCheckSafeBrowsingStatus.CHECKING &&
-        this.extensionsStatus_ !=
-            settings.SafetyCheckExtensionsStatus.CHECKING) {
+    if (this.areAllChildrenStatesSet_()) {
       this.parentStatus_ = ParentStatus.AFTER;
     }
   },
@@ -185,7 +180,19 @@ Polymer({
    * @private
    * @return {boolean}
    */
-  showParentButton_: function() {
+  areAllChildrenStatesSet_: function() {
+    return this.updatesStatus_ != settings.SafetyCheckUpdatesStatus.CHECKING &&
+        this.passwordsStatus_ != settings.SafetyCheckPasswordsStatus.CHECKING &&
+        this.safeBrowsingStatus_ !=
+        settings.SafetyCheckSafeBrowsingStatus.CHECKING &&
+        this.extensionsStatus_ != settings.SafetyCheckExtensionsStatus.CHECKING;
+  },
+
+  /**
+   * @private
+   * @return {boolean}
+   */
+  shouldShowParentButton_: function() {
     return this.parentStatus_ == ParentStatus.BEFORE;
   },
 
@@ -193,7 +200,7 @@ Polymer({
    * @private
    * @return {boolean}
    */
-  showParentIconButton_: function() {
+  shouldShowParentIconButton_: function() {
     return this.parentStatus_ == ParentStatus.AFTER;
   },
 
@@ -255,11 +262,17 @@ Polymer({
       case ParentStatus.BEFORE:
       case ParentStatus.CHECKING:
         return 'icon-blue';
-      case ParentStatus.AFTER:
-        return '';
       default:
-        assertNotReached();
+        return '';
     }
+  },
+
+  /**
+   * @private
+   * @return {boolean}
+   */
+  shouldShowChildren_: function() {
+    return this.parentStatus_ != ParentStatus.BEFORE;
   },
 });
 })();
