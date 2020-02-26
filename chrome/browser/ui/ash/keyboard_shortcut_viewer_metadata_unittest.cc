d2822975195a24141c2352388dae12133e00337d
Michael Sun <michaelfsun@google.com>
2020-02-26 00:28:10 +0000

    chromeos: add support for more media keys in accelerator
    To support more media controlling features, mainly Bluetooth media
    controllers and HID keyboards, new VKEYs have been added. Including:
    VKEY_MEDIA_PAUSE, VKEY_MEDIA_PLAY, and VKEY_MEDIA_STOP. This patch
    update the accelerator support for these new keys. Furthermore, to help
    Bluetooth device to be qualified for AVRCP (Audio/Video Remote Control
    Profile), add support for "Fast Forward" and "Rewind".
    
    BUG=b:147366755, b:147364903, b:147364132, b:147367726
    
    Change-Id: Ifc8975b05ab237ad379d3c4ae389a89459e0857e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068251
    Commit-Queue: Michael Sun <michaelfsun@google.com>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744460}

--- chrome/browser/ui/ash/keyboard_shortcut_viewer_metadata_unittest.cc
+++ chrome/browser/ui/ash/keyboard_shortcut_viewer_metadata_unittest.cc
@@ -24,9 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // The total number of Ash accelerators.
-constexpr int kAshAcceleratorsTotalNum = 111;
+constexpr int kAshAcceleratorsTotalNum = 116;
 // The hash of Ash accelerators.
-constexpr char kAshAcceleratorsHash[] = "76638d5eb10eb8096571ae42e0e29e6e";
+constexpr char kAshAcceleratorsHash[] = "d436645d30ab3a3b90bfbce57e59191f";
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 // Internal builds add an extra accelerator for the Feedback app.
 // The total number of Chrome accelerators (available on Chrome OS).
