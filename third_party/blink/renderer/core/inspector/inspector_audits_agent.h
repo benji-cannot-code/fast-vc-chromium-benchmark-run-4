1613047f30b336f90957fd401c6a0fbb6ee2a99c
Sigurd Schneider <sigurds@chromium.org>
2020-02-26 20:33:05 +0000

    [devtools] Support restore in InspectorAuditsAgent
    Bug: chromium:1056284
    Also-by: janscheffler@chromium.org
    Change-Id: I65832e22730f98609be3213c8847fcaab47ced88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2075378
    Auto-Submit: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744775}

--- third_party/blink/renderer/core/inspector/inspector_audits_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_audits_agent.h
@@ -30,6 +30,8 @@ class CORE_EXPORT InspectorAuditsAgent final
   protocol::Response enable() override;
   protocol::Response disable() override;
 
+  void Restore() override;
+
   protocol::Response getEncodedResponse(
       const String& request_id,
       const String& encoding,
@@ -40,6 +42,7 @@ class CORE_EXPORT InspectorAuditsAgent final
       int* out_encoded_size) override;
 
  private:
+  void InnerEnable();
   Member<InspectorIssueStorage> inspector_issue_storage_;
   InspectorAgentState::Boolean enabled_;
   Member<InspectorNetworkAgent> network_agent_;
