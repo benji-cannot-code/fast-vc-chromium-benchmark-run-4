49dcb9855c382480c168297e53b3c7133acaec3f
Xiang Ji <jxiang@google.com>
2020-02-26 16:19:31 +0000

    Fix long run safe browsing policy test failure
    Change-Id: I4169a0d8164890d5da815ab6946aeee0b10694c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2073271
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/master@{#744672}

--- chrome/test/enterprise/e2e/policy/safe_browsing/safe_browsing_ui_test.py
+++ chrome/test/enterprise/e2e/policy/safe_browsing/safe_browsing_ui_test.py
@@ -13,7 +13,7 @@ from pywinauto.application import Application
 UnsafePageLink = "http://testsafebrowsing.appspot.com/s/malware.html"
 UnsafePageLinkTabText = "Security error"
 
-UnsafeDownloadLink = "http://testsafebrowsing.appspot.com/s/content.exe"
+UnsafeDownloadLink = "http://testsafebrowsing.appspot.com/s/badrep.exe"
 UnsafeDownloadTextRe = ".* is dangerous,\s*so\s*Chrom.* has blocked it"
 
 
