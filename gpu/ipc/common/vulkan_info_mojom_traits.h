7161874fbb851f78f41b4c05afc83a5fa9fe7f68
Peng Huang <penghuang@chromium.org>
2020-02-27 00:22:15 +0000

    Vulkan: remove device level layers.
    The device level layers are depercated, and there is no device level
    layers for now, so remove it.
    https://www.khronos.org/registry/vulkan/specs/1.2-extensions/html/vkspec.html#extendingvulkan-layers-devicelayerdeprecation
    
    Bug: None
    Change-Id: I9507191ca515876a8d97140261bd4099f284dfe6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2075965
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744857}

--- gpu/ipc/common/vulkan_info_mojom_traits.h
+++ gpu/ipc/common/vulkan_info_mojom_traits.h
@@ -27,11 +27,6 @@ struct StructTraits<gpu::mojom::VulkanPhysicalDeviceInfoDataView,
     return input.extensions;
   }
 
-  static const std::vector<VkLayerProperties>& layers(
-      const gpu::VulkanPhysicalDeviceInfo& input) {
-    return input.layers;
-  }
-
   static const VkPhysicalDeviceFeatures& features(
       const gpu::VulkanPhysicalDeviceInfo& input) {
     return input.features;
@@ -57,8 +52,6 @@ struct StructTraits<gpu::mojom::VulkanPhysicalDeviceInfoDataView,
     if (!data.ReadProperties(&out->properties))
       return false;
     if (!data.ReadExtensions(&out->extensions))
-      return false;
-    if (!data.ReadLayers(&out->layers))
       return false;
     if (!data.ReadFeatures(&out->features))
       return false;
