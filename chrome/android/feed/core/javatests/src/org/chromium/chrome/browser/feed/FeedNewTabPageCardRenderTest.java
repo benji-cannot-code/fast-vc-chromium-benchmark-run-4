2fd14cd1b6a5264de38739051d6720117f1b0ed3
Colin Blundell <blundell@chromium.org>
2020-02-27 13:40:35 +0000

    [Android] Componentize //chrome's UrlConstants.java
    This CL componentizes //chrome's UrlConstants.java for reuse by
    WebLayer. Specifically, this file is used by //chrome's external
    navigation launching code, which we are in the process of componentizing
    for reuse by WebLayer.
    
    As this file references concepts that are conceptually above the
    //content level (e.g., browser-level features), it makes sense for it to
    live in //components. Rather than create a small component just for
    this we placed it in //components/embedder_support. It will shortly be
    joined by UrlUtilities.java.
    
    Bug: 1031465
    Change-Id: I2f24f625d9dc1b5e977ecb5dcbe3dc98a26e67ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2073938
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745031}

--- chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/FeedNewTabPageCardRenderTest.java
+++ chrome/android/feed/core/javatests/src/org/chromium/chrome/browser/feed/FeedNewTabPageCardRenderTest.java
@@ -33,7 +33,6 @@ import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.suggestions.SiteSuggestion;
 import org.chromium.chrome.browser.tab.Tab;
-import org.chromium.chrome.browser.util.UrlConstants;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
@@ -42,6 +41,7 @@ import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.chrome.test.util.browser.RecyclerViewTestUtils;
 import org.chromium.chrome.test.util.browser.suggestions.SuggestionsDependenciesRule;
 import org.chromium.chrome.test.util.browser.suggestions.mostvisited.FakeMostVisitedSites;
+import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.net.test.EmbeddedTestServerRule;
 
 import java.util.List;
