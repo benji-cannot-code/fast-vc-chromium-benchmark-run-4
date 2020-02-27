e2ae0935a46bf9b901826ab650c9d7aa64496f16
dpapad <dpapad@chromium.org>
2020-02-27 18:23:14 +0000

    Settings: Port chrome_cleanup_page/ to Polymer 3.
    Bug: 1026426
    Change-Id: I2e98be132e07c90aa1535cd4edb4cc399fc4a84f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2073226
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745113}

--- chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_proxy.js
+++ chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_proxy.js
@@ -3,9 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import {addSingletonGetter, sendWithPromise} from 'chrome://resources/js/cr.m.js';
+// clang-format on
+
 cr.define('settings', function() {
   /** @interface */
-  class ChromeCleanupProxy {
+  /* #export */ class ChromeCleanupProxy {
     /**
      * Registers the current ChromeCleanupHandler as an observer of
      * ChromeCleanerController events.
@@ -60,7 +64,7 @@ cr.define('settings', function() {
   /**
    * @implements {settings.ChromeCleanupProxy}
    */
-  class ChromeCleanupProxyImpl {
+  /* #export */ class ChromeCleanupProxyImpl {
     /** @override */
     registerChromeCleanerObserver() {
       chrome.send('registerChromeCleanerObserver');
