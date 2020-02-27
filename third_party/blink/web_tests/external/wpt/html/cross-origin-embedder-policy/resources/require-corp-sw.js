63317073a739aac395c09e62ee1bac7da49f0380
Makoto Shimazu <shimazu@chromium.org>
2020-02-27 03:31:30 +0000

    Add tests of requests initiated from COEP-protected service worker
    This CL adds tests to initiate requests from a service worker with
    `require-corp` policy. This currently fails but it'll be fixed once a
    new service worker starts to respect the COEP value.
    
    Bug: 1039613
    Change-Id: Ic610cbe8ceb9c5c0d407507918fcdf2c69eb93b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2065257
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Auto-Submit: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744922}

---
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/resources/require-corp-sw.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Service worker with 'COEP: require-corp' response header.
+// This service worker issues a network request for a resource with or without
+// CORP response header.
+
+importScripts("/common/get-host-info.sub.js");
+
+self.addEventListener('message', e => {
+  e.waitUntil((async () => {
+    let result;
+    try {
+      let url;
+      if (e.data === 'WithCorp') {
+        url = get_host_info().HTTPS_REMOTE_ORIGIN +
+            '/html/cross-origin-embedder-policy/resources/' +
+            'nothing-cross-origin-corp.txt';
+      } else if (e.data === 'WithoutCorp') {
+        url = get_host_info().HTTPS_REMOTE_ORIGIN + '/common/blank.html';
+      }
+      const response = await fetch(url, { mode: 'no-cors' });
+      result = response.type;
+    } catch (error) {
+      result = `${error.name}: ${error.message}`;
+    } finally {
+      e.source.postMessage(result);
+    }
+  })());
+});
