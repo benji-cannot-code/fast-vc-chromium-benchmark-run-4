fe6a17cb1c2ecdb97e7b7183b4b6bde9dcf44204
Nina Satragno <nsatragno@chromium.org>
2020-02-27 20:04:43 +0000

    [webauthn] testdriver API: Add isUserConsenting
    Add `isUserConsenting` to the content shell testdriver API. This lets
    us run the timeout WPTs.
    
    Fixed: 1054810, 1054655
    Change-Id: Id90567818a1fcda44794294fc3527790df056870
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2078833
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745147}

--- third_party/blink/web_tests/resources/testdriver-vendor.js
+++ third_party/blink/web_tests/resources/testdriver-vendor.js
@@ -323,6 +323,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
     mojoOptions.hasResidentKey = options.hasResidentKey;
     mojoOptions.hasUserVerification = options.hasUserVerification;
+    mojoOptions.isUserPresent = options.isUserConsenting;
 
     let authenticator = (await manager.createAuthenticator(mojoOptions)).authenticator;
     return (await authenticator.getUniqueId()).id;
