ff5d6aa9aff1b713678bf9b730ed115cb4117365
Robbie Gibson <rkgibson@google.com>
2020-02-27 17:46:02 +0000

    [iOS] Add accessibility labels to text zoom buttons
    Because the buttons show images, VoiceOver was reading the image
    filename. There should be a real label instead.
    
    Bug: 1028938
    Change-Id: I3fdcb3e325460616d935cc594f4a7489ebbb033f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2078474
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745099}

--- ios/chrome/browser/ui/text_zoom/text_zoom_view_controller.mm
+++ ios/chrome/browser/ui/text_zoom/text_zoom_view_controller.mm
@@ -137,6 +137,8 @@ const CGFloat kDividerWidth = 1;
   if (!_incrementButton) {
     _incrementButton = [self newButtonWithDefaultStyling];
     UIImage* image = [UIImage imageNamed:@"text_zoom_zoom_in"];
+    image.accessibilityLabel =
+        l10n_util::GetNSString(IDS_IOS_TEXT_ZOOM_ZOOM_IN);
     [_incrementButton setImage:image forState:UIControlStateNormal];
     [_incrementButton addTarget:self.zoomHandler
                          action:@selector(zoomIn)
@@ -155,6 +157,8 @@ const CGFloat kDividerWidth = 1;
   if (!_decrementButton) {
     _decrementButton = [self newButtonWithDefaultStyling];
     UIImage* image = [UIImage imageNamed:@"text_zoom_zoom_out"];
+    image.accessibilityLabel =
+        l10n_util::GetNSString(IDS_IOS_TEXT_ZOOM_ZOOM_OUT);
     [_decrementButton setImage:image forState:UIControlStateNormal];
     [_decrementButton addTarget:self.zoomHandler
                          action:@selector(zoomOut)
