cb434f307a321cf816f095de1b9625fb3477a049
ckitagawa <ckitagawa@chromium.org>
2020-02-27 15:24:46 +0000

    [Paint Preview] DirectoryKey for file management
    Refactor FileManager to accept a DirectoryKey rather than a GURL. This
    makes it more ammenable to possibly supporting other keys for creating
    directories such as tab id, UnguessableTokens, etc.
    
    Bug: 1055505
    Change-Id: Id93cc1bcbfdfdd69baf0f57764636dcbcd62c3f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2070737
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745050}

--- components/paint_preview/browser/paint_preview_base_service_unittest.cc
+++ components/paint_preview/browser/paint_preview_base_service_unittest.cc
@@ -161,8 +161,9 @@ TEST_F(PaintPreviewBaseServiceTest, CaptureMainFrame) {
   auto* service = GetService();
   EXPECT_FALSE(service->IsOffTheRecord());
   base::FilePath path;
-  ASSERT_TRUE(service->GetFileManager()->CreateOrGetDirectoryFor(
-      web_contents()->GetLastCommittedURL(), &path));
+  auto* manager = service->GetFileManager();
+  ASSERT_TRUE(manager->CreateOrGetDirectory(
+      manager->CreateKey(web_contents()->GetLastCommittedURL()), &path));
 
   base::RunLoop loop;
   service->CapturePaintPreview(
@@ -213,8 +214,9 @@ TEST_F(PaintPreviewBaseServiceTest, CaptureFailed) {
   auto* service = GetService();
   EXPECT_FALSE(service->IsOffTheRecord());
   base::FilePath path;
-  ASSERT_TRUE(service->GetFileManager()->CreateOrGetDirectoryFor(
-      web_contents()->GetLastCommittedURL(), &path));
+  auto* manager = service->GetFileManager();
+  ASSERT_TRUE(manager->CreateOrGetDirectory(
+      manager->CreateKey(web_contents()->GetLastCommittedURL()), &path));
 
   base::RunLoop loop;
   service->CapturePaintPreview(
@@ -247,8 +249,9 @@ TEST_F(PaintPreviewBaseServiceTest, CaptureDisallowed) {
   auto* service = GetServiceWithRejectionPolicy();
   EXPECT_FALSE(service->IsOffTheRecord());
   base::FilePath path;
-  ASSERT_TRUE(service->GetFileManager()->CreateOrGetDirectoryFor(
-      web_contents()->GetLastCommittedURL(), &path));
+  auto* manager = service->GetFileManager();
+  ASSERT_TRUE(manager->CreateOrGetDirectory(
+      manager->CreateKey(web_contents()->GetLastCommittedURL()), &path));
 
   base::RunLoop loop;
   service->CapturePaintPreview(
