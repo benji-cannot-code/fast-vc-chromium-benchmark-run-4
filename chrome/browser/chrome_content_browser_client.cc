70c1da7e82677ee15b32e2b5f8e0d4a275357ad6
evliu <evliu@google.com>
2020-02-27 21:32:32 +0000

    Add metric to track percentage of caption settings from system settings
    This CL adds a histogram metric to track the percentage of caption
    settings that come from the system settings.
    
    Bug: 1056381
    Change-Id: Ia8e7dee0e521f986e6bcbfcd887702d6ef63dd1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076542
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#745194}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3293,6 +3293,10 @@ void ChromeContentBrowserClient::OverrideWebkitPrefs(
   // Apply system caption style.
   if (!style) {
     style = native_theme->GetSystemCaptionStyle();
+
+    base::UmaHistogramBoolean(
+        "Accessibility.CaptionSettingsLoadedFromSystemSettings",
+        style.has_value());
   }
 
   // Apply caption style from preferences if system caption style is undefined.
