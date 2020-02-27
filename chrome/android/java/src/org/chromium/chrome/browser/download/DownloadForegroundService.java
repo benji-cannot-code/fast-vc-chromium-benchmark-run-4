651b0b27d9bc6a9d4ea835d0f55134b8ada0209a
Xing Liu <xingliu@chromium.org>
2020-02-27 20:09:56 +0000

    Revert "Download notification: Simplify foreground notification events."
    This reverts commit bfb35d53fa9d25d4928309fb8b976c552631e4e3.
    
    Reason for revert: Triggers a bug in the old code. see crbug/1056107.
    
    Original change's description:
    > Download notification: Simplify foreground notification events.
    > 
    > Currently we use DownloadForegroundServiceObservers to broadcast
    > download foreground service events. It uses reflection to cache the
    > class name and then persist to android shared preference.
    > 
    > DownloadNotificationServiceObserver implements
    > DownloadForegroundServiceObservers.Observer, and then call methods of
    > the singleton object of DownloadNotificationService.
    > 
    > Also, onForegroundServiceRestarted() is not getting called, which make
    > the following calls dead code.
    > 
    > This is extremely weird, we can just call the one line singleton
    > method.
    > 
    > Bug: 922929
    > Change-Id: I28ff5552fd7fc0fd5a111cd4971d6bf4514bb45f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063853
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Commit-Queue: Xing Liu <xingliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#743368}
    
    TBR=dtrainor@chromium.org,qinmin@chromium.org,xingliu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 922929
    Change-Id: I467b19ba958a380a08e464339e735d5e30e376c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076977
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745156}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundService.java
@@ -188,7 +188,7 @@ public class DownloadForegroundService extends Service {
     public void onDestroy() {
         DownloadNotificationUmaHelper.recordServiceStoppedHistogram(
                 DownloadNotificationUmaHelper.ServiceStopped.DESTROYED, true /* withForeground */);
-        DownloadNotificationService.getInstance().onForegroundServiceDestroyed();
+        DownloadForegroundServiceObservers.alertObserversServiceDestroyed();
         super.onDestroy();
     }
 
@@ -196,7 +196,7 @@ public class DownloadForegroundService extends Service {
     public void onTaskRemoved(Intent rootIntent) {
         DownloadNotificationUmaHelper.recordServiceStoppedHistogram(
                 DownloadNotificationUmaHelper.ServiceStopped.TASK_REMOVED, true /*withForeground*/);
-        DownloadNotificationService.getInstance().onForegroundServiceTaskRemoved();
+        DownloadForegroundServiceObservers.alertObserversTaskRemoved();
         super.onTaskRemoved(rootIntent);
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundServiceManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundServiceManager.java
@@ -213,6 +213,8 @@ public class DownloadForegroundServiceManager {
                 return;
             }
             mBoundService = ((DownloadForegroundService.LocalBinder) service).getService();
+            DownloadForegroundServiceObservers.addObserver(
+                    DownloadNotificationServiceObserver.class);
             processDownloadUpdateQueue(true /* isProcessingPending */);
         }
 
@@ -283,6 +285,9 @@ public class DownloadForegroundServiceManager {
         mBoundService.stopDownloadForegroundService(
                 stopForegroundStatus, pinnedNotificationId, pinnedNotification);
         ContextUtils.getApplicationContext().unbindService(mConnection);
+
+        DownloadForegroundServiceObservers.removeObserver(
+                DownloadNotificationServiceObserver.class);
     }
 
     /** Helper code for testing. */
