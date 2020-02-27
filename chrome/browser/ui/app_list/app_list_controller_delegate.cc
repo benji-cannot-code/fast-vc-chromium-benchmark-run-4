cfa5c39ea4fb67fa36c15c0da1794905eeb4e942
nancy <nancylingwang@chromium.org>
2020-02-27 21:45:05 +0000

    Remove ExtensionUninstaller
    AppUninstallDialogView replaced ExtensionUninstaller, so
    ExtensionUninstaller can be removed.
    https://cs.chromium.org/chromium/src/chrome/browser/ui/views/apps/app_uninstall_dialog_view.h?l=44
    
    BUG=1010303
    
    Change-Id: I215dad0977b2d37bc126ac68e06689eb0a3b0c3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076857
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745216}

--- chrome/browser/ui/app_list/app_list_controller_delegate.cc
+++ chrome/browser/ui/app_list/app_list_controller_delegate.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/install_tracker_factory.h"
 #include "chrome/browser/extensions/launch_util.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/app_list/extension_uninstaller.h"
 #include "chrome/browser/ui/apps/app_info_dialog.h"
 #include "chrome/browser/ui/ash/tablet_mode_page_behavior.h"
 #include "chrome/browser/ui/chrome_pages.h"
