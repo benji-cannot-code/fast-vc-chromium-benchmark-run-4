f20b1f58ef2ad96ab9462dd987e4701abfdcbb74
Jérôme Lebel <jlebel@chromium.org>
2020-02-27 14:48:49 +0000

    [iOS] Fixing signin interaction coordinator active property
    The sign-in interaction coordinator is active when it is used with
    the sign-in coordinator.
    
    Bug: 971989
    Change-Id: Ic199eb9e7628d6f4195f7bbc5637827eccc38cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2077933
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745045}

--- ios/chrome/browser/ui/signin_interaction/signin_interaction_coordinator.mm
+++ ios/chrome/browser/ui/signin_interaction/signin_interaction_coordinator.mm
@@ -158,7 +158,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Properties
 
 - (BOOL)isActive {
-  return self.controller != nil || self.isSettingsViewPresented;
+  return self.controller != nil || self.coordinator != nil;
 }
 
 - (BOOL)isSettingsViewPresented {
