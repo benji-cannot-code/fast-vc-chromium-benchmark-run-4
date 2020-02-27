477566d2bf8c0ebbb6c4fea60e0fb4bca021b50c
rbpotter <rbpotter@chromium.org>
2020-02-27 21:53:24 +0000

    JS Modulizer: Handle files with non-ascii characters
    This is needed to autogenerate a JS module version of
    chrome/test/data/webui/settings/fake_language_settings_private.js
    
    Bug: 1026426
    Change-Id: I5b3886cde240494235dda367f43af1d0e90a3085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076556
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745218}

---
+++ ui/webui/resources/tools/tests/non_ascii_expected.js
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+export function asciiLanguageName() {
+  return 'English';
+}
+export function nonAsciiLanguageName() {
+  return 'Ελληνικά';
+}
+function baz() {}
