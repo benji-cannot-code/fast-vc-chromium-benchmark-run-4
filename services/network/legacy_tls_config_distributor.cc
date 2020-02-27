79aca0c7efcfaa0c67317b26dccf4f4d795c6769
Christopher Thompson <cthomp@chromium.org>
2020-02-27 22:14:54 +0000

    Legacy TLS: Lookup control sites by eTLD+1
    Bug: 1055906
    Change-Id: I974c8cc5993299a6611c4bd14f515c865d9bef02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2073218
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Christopher Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745226}

--- services/network/legacy_tls_config_distributor.cc
+++ services/network/legacy_tls_config_distributor.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "crypto/sha2.h"
+#include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 
 namespace network {
 
@@ -51,8 +52,16 @@ scoped_refptr<LegacyTLSExperimentConfig> LegacyTLSExperimentConfig::Parse(
 
 bool LegacyTLSExperimentConfig::ShouldSuppressLegacyTLSWarning(
     const std::string& hostname) const {
+  // Match on eTLD+1 rather than full hostname (to account for subdomains and
+  // redirects). If no registrable domain is found, default to using the
+  // hostname as-is.
+  auto domain = net::registry_controlled_domains::GetDomainAndRegistry(
+      hostname, net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
+  if (domain.empty())
+    domain = hostname;
+
   // Convert bytes from crypto::SHA256 so we can compare to the proto contents.
-  std::string host_hash_bytes = crypto::SHA256HashString(hostname);
+  std::string host_hash_bytes = crypto::SHA256HashString(domain);
   std::string host_hash = base::ToLowerASCII(
       base::HexEncode(host_hash_bytes.data(), host_hash_bytes.size()));
   const auto& control_site_hashes = proto_.control_site_hashes();
