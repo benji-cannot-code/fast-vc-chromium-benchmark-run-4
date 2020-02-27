9a1b62d713b87e9cd2f834485c728013ac26066a
James Cook <jamescook@chromium.org>
2020-02-27 21:39:12 +0000

    Fix login crash in assistant service for Active Directory accounts
    The crash is a CHECK failure that the user's email and gaia ID are
    non-empty.
    
    I introduced the regression in this CL:
    cros: Migrate assistant off the identity service
    http://crrev.com/c/2033572
    
    The CHECK was moved to its current location in this CL:
    Reland "assistant: Fix mic button doesn't work with fake gaia account."
    http://crrev.com/c/2053284
    
    It's not clear if Assistant is supposed to work in Active Directory
    accounts. AD accounts don't seed the IdentityManager with an email or
    gaia ID, so it's unclear to me if/how Assistant ever worked.
    
    I did not add a unit test for the fix because it's not clear if the
    empty email/gaia ID is expected behavior or not, so I don't want to
    enforce support for it in tests.
    
    Bug: 1056717
    
    Change-Id: Ib17100c6b2f403d8782f5f7faed79b7d64aac1ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2078841
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/master@{#745205}

--- chrome/browser/chromeos/login/session/user_session_manager.cc
+++ chrome/browser/chromeos/login/session/user_session_manager.cc
@@ -1492,6 +1492,8 @@ void UserSessionManager::InitProfilePreferences(
       user_manager::known_user::UpdateGaiaID(user_context.GetAccountId(),
                                              gaia_id);
     }
+  } else {
+    // Active Directory (non-supervised, non-GAIA) accounts take this path.
   }
 }
 
