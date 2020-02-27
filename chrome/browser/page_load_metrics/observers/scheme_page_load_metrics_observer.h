bb33a586a91630738506b22d53e8d6f3125adb79
Nicolás Peña Moreno <npm@chromium.org>
2020-02-27 18:34:26 +0000

    Remove Time To Interactive plumbing to the browser process
    TTI should not be used as a RUM metric. This CL removes the plumbing of
    TTI and related values from the renderer to the browser process. It also
    removes the UMA and UKM histograms that were using it.
    
    Bug: 1055806
    
    Change-Id: Ia72c454587f633e58882153bd921ddafae3e71f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2070384
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Deep Roy <dproy@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745119}

--- chrome/browser/page_load_metrics/observers/scheme_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/scheme_page_load_metrics_observer.h
@@ -30,8 +30,6 @@ class SchemePageLoadMetricsObserver
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
   void OnFirstMeaningfulPaintInMainFrameDocument(
       const page_load_metrics::mojom::PageLoadTiming& timing) override;
-  void OnPageInteractive(
-      const page_load_metrics::mojom::PageLoadTiming& timing) override;
 
  private:
   // The ui transition for the committed navigation.
