e99fb5332f30404e8b293ac273eee45d46b30635
sandromaggi <sandromaggi@google.com>
2020-02-27 15:40:09 +0000

    [Autofill Assistant] Cleanup and fix element_finder
    While checking an unrelated piece, I found the element_finder to still
    be very confusing. This should CL should clean it up and make it a bit
    more understandable.
    
    It also fixes a minor bug, where the container_frame_selector_index was
    not correctly set for OOPIF.
    
    Bug: none
    Change-Id: I49965dcbc5378a0f73812489b68790b5db9f888f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2072386
    Reviewed-by: Mathias Carlen <mcarlen@chromium.org>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#745055}

--- components/autofill_assistant/browser/web/element_finder.cc
+++ components/autofill_assistant/browser/web/element_finder.cc
@@ -137,6 +137,8 @@ void ElementFinder::Start(Callback callback) {
     return;
   }
 
+  element_result_->container_frame_selector_index = 0;
+  element_result_->container_frame_host = web_contents_->GetMainFrame();
   devtools_client_->GetRuntime()->Evaluate(
       std::string(kGetDocumentElement), /* node_frame_id= */ std::string(),
       base::BindOnce(&ElementFinder::OnGetDocumentElement,
@@ -167,12 +169,6 @@ void ElementFinder::OnGetDocumentElement(
     return;
   }
 
-  element_result_->container_frame_selector_index = index;
-  if (element_result_->container_frame_host == nullptr) {
-    // Don't overwrite results from previous OOPIF passes.
-    element_result_->container_frame_host = web_contents_->GetMainFrame();
-  }
-  element_result_->object_id = std::string();
   RecursiveFindElement(object_id, index);
 }
 
@@ -403,7 +399,7 @@ void ElementFinder::OnDescribeNode(
     return;
   }
 
-  RecursiveFindElement(object_id, ++index);
+  RecursiveFindElement(object_id, index + 1);
 }
 
 void ElementFinder::OnResolveNode(
