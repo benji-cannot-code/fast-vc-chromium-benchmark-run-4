87ffa618de04c082054150c9a213093d70ab70b3
sczs <sczs@chromium.org>
2020-02-27 01:18:49 +0000

    [ios] Removes empty tableFooterViews.
    These were only needed for iOS10
    
    Bug: 863606
    Change-Id: Ie57563c89abc7848d9731d007624c0425205028f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058083
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744882}

--- ios/showcase/uikit_table_view_cell/uikit_table_view_cell_view_controller.mm
+++ ios/showcase/uikit_table_view_cell/uikit_table_view_cell_view_controller.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  self.tableView.tableFooterView = [[UIView alloc] init];
   self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
 }
 
