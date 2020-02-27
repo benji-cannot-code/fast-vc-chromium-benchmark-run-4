a9936e9eb6f90141a3434a3aa9a1aff07191ea64
Rahul Arakeri <arakeri@microsoft.com>
2020-02-27 00:42:10 +0000

    Fix for faded Mac scrollbar hit test.
    This CL fixes an issue where clicking on a faded out scrollbar on Mac
    caused a gesture scroll. This is fixed by querying the opacity of the
    PaintedScrollbarLayerImpl and returning a zero delta if the scrollbar
    opacity is 0.
    
    Bug: 1054155
    Change-Id: I9ae3b8284b1843b99f15e70a66f7336d956aa810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068204
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744864}

--- cc/layers/scrollbar_layer_impl_base.cc
+++ cc/layers/scrollbar_layer_impl_base.cc
@@ -285,6 +285,10 @@ bool ScrollbarLayerImplBase::HasFindInPageTickmarks() const {
   return false;
 }
 
+float ScrollbarLayerImplBase::OverlayScrollbarOpacity() const {
+  return Opacity();
+}
+
 bool ScrollbarLayerImplBase::SupportsDragSnapBack() const {
   return false;
 }
