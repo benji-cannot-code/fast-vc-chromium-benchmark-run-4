a9ed654e165238caaef8779692613260bcbd4803
Alex Keng <shihken@microsoft.com>
2020-02-27 03:04:42 +0000

    video: suppress Aria update when timeline slider is focused
    This CL addresses the issue where when the timeline slider is focused
    while the media is playing, Narrator will announce 'Elapsed time,
    Elapsed time, ..." repeatedly. (NVDA will repeat the whole sentence
    which is also not ideal)
    
    With this change, the behavior is now changed to announce the
    current/elapsed time only once when the slider gets focused.
    
    Note
    1. only Aria updates triggered by the timer are suppressed, so
    user interactions that would move timeline slider, ex mouse clicks,
    left/right keys etc, would still trigger current/elapsed time utterance.
    
    2. we can't use aria-live:off to suppress aria since according to
    the spec [1], aria-live:off doesn't apply to areas that are being focused.
    
    [1] https://www.w3.org/TR/wai-aria-1.1/#aria-live
    
    Bug: 1054885
    Change-Id: I5519793a3d53f1982733f49fd4db882326f5c2f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067381
    Commit-Queue: Alex Keng <shihken@microsoft.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#744920}

--- third_party/blink/renderer/modules/media_controls/elements/media_control_timeline_element.h
+++ third_party/blink/renderer/modules/media_controls/elements/media_control_timeline_element.h
@@ -25,7 +25,7 @@ class MediaControlTimelineElement : public MediaControlSliderElement {
 
   // FIXME: An "earliest possible position" will be needed once that concept
   // is supported by HTMLMediaElement, see https://crbug.com/137275
-  void SetPosition(double);
+  void SetPosition(double, bool suppress_aria = false);
   void SetDuration(double);
 
   void OnMediaKeyboardEvent(Event* event) { DefaultEventHandler(*event); }
@@ -51,6 +51,8 @@ class MediaControlTimelineElement : public MediaControlSliderElement {
   // ended when the touch has ended.
   bool BeginScrubbingEvent(Event&);
   bool EndScrubbingEvent(Event&);
+
+  void UpdateAria();
 
   MediaControlTimelineMetrics metrics_;
 
