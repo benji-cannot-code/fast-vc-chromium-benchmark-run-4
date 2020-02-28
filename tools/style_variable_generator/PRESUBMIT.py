792259f74143e432a50bbc47eb0e9ffc3b556757
Christopher Lam <calamity@chromium.org>
2020-02-28 00:37:07 +0000

    [style_var_gen] Add a CSS Variable Generator.
    This CL adds a python script that generates a file with CSS Variables
    that are specified in a JSON5 file. This will allow unified semantic
    color (and other properties) specification in Chrome OS.
    
    Bug: 1018654
    Change-Id: I3a427cbb04f59659143739f717689810da661fa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041110
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745282}

---
+++ tools/style_variable_generator/PRESUBMIT.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2019 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Presubmit script for changes affecting tools/style_variable_generator/
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+WHITELIST = [r'.+_test.py$']
+
+
+def CheckChangeOnUpload(input_api, output_api):
+    return input_api.canned_checks.RunUnitTestsInDirectory(
+        input_api, output_api, '.', whitelist=WHITELIST)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+    return input_api.canned_checks.RunUnitTestsInDirectory(
+        input_api, output_api, '.', whitelist=WHITELIST)
