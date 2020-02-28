e87bbdc808eb5e3121bebb69a1d5f42f61abea6e
Peter E Conn <peconn@chromium.org>
2020-02-28 11:29:52 +0000

    👮 Disable failing Clipboard tests.
    TBR=dcheng@chromium.org,huangdarwin@chromium.org
    
    Bug: 1056650
    Change-Id: I45ac5d5ac747128f48e6fab502d180479de8822f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080256
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745411}

--- ui/base/clipboard/clipboard_test_template.h
+++ ui/base/clipboard/clipboard_test_template.h
@@ -467,7 +467,7 @@ TYPED_TEST(ClipboardTest, Bitmap_RGBA_Opaque) {
       SkImageInfo::Make(1, 1, kRGBA_8888_SkColorType, kOpaque_SkAlphaType),
       &kRGBAOpaque, &kN32Opaque);
 }
-TYPED_TEST(ClipboardTest, Bitmap_BGRA_Premul) {
+TYPED_TEST(ClipboardTest, DISABLED_Bitmap_BGRA_Premul) {
   TestBitmapWrite(
       &this->clipboard(),
       SkImageInfo::Make(1, 1, kBGRA_8888_SkColorType, kPremul_SkAlphaType),
@@ -479,7 +479,7 @@ TYPED_TEST(ClipboardTest, Bitmap_BGRA_Unpremul) {
       SkImageInfo::Make(1, 1, kBGRA_8888_SkColorType, kUnpremul_SkAlphaType),
       &kBGRAUnpremul, &kN32);
 }
-TYPED_TEST(ClipboardTest, Bitmap_BGRA_Opaque) {
+TYPED_TEST(ClipboardTest, DISABLED_Bitmap_BGRA_Opaque) {
   TestBitmapWrite(
       &this->clipboard(),
       SkImageInfo::Make(1, 1, kBGRA_8888_SkColorType, kOpaque_SkAlphaType),
