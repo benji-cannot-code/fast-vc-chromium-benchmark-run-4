c0025113621cd5350b5a3defefbc56fe7d0e5a78
Karan Bhatia <karandeepb@chromium.org>
2020-02-28 01:44:14 +0000

    DNR: Add a max memory limit for individual regular expression filters.
    This CL adds a maximum memory limit for individual regular expression
    filters using the re2 |max_mem| option. The limit is initially set to 2
    Kb.
    
    Manifest regex rules exceeding the limit are ignored and will cause an
    install warning instead of a hard error. To prevent a lot of warnings,
    we limit the number of such warnings to 10. In case of more than 10 such
    rules,  we'll show a single warning listing all the affected rule IDs.
    
    New dynamic rules added using the
    chrome.declarativeNetRequest.updateDynamicRules call and exceeding the
    limit will cause an error.
    
    Earlier indexed rules (static or dynamic) which now exceed the memory
    limit will be ignored silently. This will be modified subsequently to
    raise an error in the extension console.
    
    BUG=974391
    
    Change-Id: I759d651b851200613c8d3b2e4c7146fdd4749a6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032028
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745317}

--- extensions/browser/api/declarative_net_request/constants.h
+++ extensions/browser/api/declarative_net_request/constants.h
@@ -17,6 +17,7 @@ namespace declarative_net_request {
 // The result of parsing JSON rules provided by an extension. Can correspond to
 // a single or multiple rules.
 enum class ParseResult {
+  NONE,
   SUCCESS,
   ERROR_RESOURCE_TYPE_DUPLICATED,
   ERROR_INVALID_RULE_ID,
@@ -48,6 +49,7 @@ enum class ParseResult {
   ERROR_EMPTY_REGEX_FILTER,
   ERROR_NON_ASCII_REGEX_FILTER,
   ERROR_INVALID_REGEX_FILTER,
+  ERROR_REGEX_TOO_LARGE,
   ERROR_MULTIPLE_FILTERS_SPECIFIED,
   ERROR_REGEX_SUBSTITUTION_WITHOUT_FILTER,
   ERROR_INVALID_REGEX_SUBSTITUTION,
@@ -72,10 +74,11 @@ enum class UpdateDynamicRulesStatus {
   kErrorCreateMatcher_FileReadError = 11,
   kErrorCreateMatcher_ChecksumMismatch = 12,
   kErrorCreateMatcher_VersionMismatch = 13,
+  kErrorRegexTooLarge = 14,
 
   // Magic constant used by histograms code. Should be equal to the largest enum
   // value.
-  kMaxValue = kErrorCreateMatcher_VersionMismatch,
+  kMaxValue = kErrorRegexTooLarge,
 };
 
 // Schemes which can be used as part of url transforms.
@@ -100,6 +103,8 @@ extern const char kErrorJavascriptRedirect[];
 extern const char kErrorMultipleFilters[];
 extern const char kErrorRegexSubstitutionWithoutFilter[];
 extern const char kErrorInvalidAllowAllRequestsResourceType[];
+extern const char kErrorRegexTooLarge[];
+extern const char kErrorRegexesTooLarge[];
 
 extern const char kErrorListNotPassed[];
 
