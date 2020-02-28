e3e87e41c6c657c560ceaaee921f9e2b66e0ac37
Nina Satragno <nsatragno@chromium.org>
2020-02-28 19:26:03 +0000

    [webauthn] Implement UV token for MakeCredential
    Mirror the behaviour of GetAssertion and obtain a UV token for devices
    that support it and have UV configured. Handle retries and fallback to
    PIN.
    
    Bug: 1055497
    Change-Id: I8c3c524db0da848374f2a68a0166a4420bdd248d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2073139
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#745567}

--- device/fido/get_assertion_request_handler.h
+++ device/fido/get_assertion_request_handler.h
@@ -102,6 +102,7 @@ class COMPONENT_EXPORT(DEVICE_FIDO) GetAssertionRequestHandler
   void OnHaveUvToken(FidoAuthenticator* authenticator,
                      CtapDeviceResponseCode status,
                      base::Optional<pin::TokenResponse> response);
+  void DispatchRequestWithToken(pin::TokenResponse token);
 
   CompletionCallback completion_callback_;
   State state_ = State::kWaitingForTouch;
