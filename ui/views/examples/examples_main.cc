824e82db15fa9033eaed2a7a228db0e0c561218a
Robert Liao <robliao@chromium.org>
2020-02-28 03:56:07 +0000

    Fix Examples Main Crash
    In r718224, the GPU Service picked up a dependency on the Task
    Environment. Destroying the Task Environment first leaves some dangling
    objects. Destroying the GPU service first works much better.
    
    Fixed:1056449
    
    Change-Id: I4d0462c5290592f2eb9550019e07d50b48099d35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079891
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745353}

--- ui/views/examples/examples_main.cc
+++ ui/views/examples/examples_main.cc
@@ -96,6 +96,10 @@ int main(int argc, char** argv) {
   // values from TestTimeouts. This ensures they're properly initialized.
   TestTimeouts::Initialize();
 
+  // Viz depends on the task environment to correctly tear down.
+  base::test::TaskEnvironment task_environment(
+      base::test::TaskEnvironment::MainThreadType::UI);
+
   // The ContextFactory must exist before any Compositors are created.
   viz::HostFrameSinkManager host_frame_sink_manager;
   viz::ServerSharedBitmapManager shared_bitmap_manager;
@@ -105,9 +109,6 @@ int main(int argc, char** argv) {
   auto context_factory = std::make_unique<ui::InProcessContextFactory>(
       &host_frame_sink_manager, &frame_sink_manager);
   context_factory->set_use_test_surface(false);
-
-  base::test::TaskEnvironment task_environment(
-      base::test::TaskEnvironment::MainThreadType::UI);
 
   base::i18n::InitializeICU();
 
