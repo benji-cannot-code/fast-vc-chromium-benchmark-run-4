e9bf147c1b88b8ee970316a88a270879a8474d37
Stephen McGruer <smcgruer@chromium.org>
2020-02-28 14:55:39 +0000

    Replace some assert_throws("Something", stuff) calls with assert_throws_dom
    This diff was generated by running:
    
      find . -type f -print0 | xargs -0 perl -pi -e 'BEGIN { $/ = undef; } s/assert_throws\(([ \n]*"[A-Za-z_]*") *(, *.)/assert_throws_dom(\1\2/gs'
    
    Then manually correcting some whitespace errors and one expectation file.
    
    Bug: 1051932
    Change-Id: Ie6beffd8e61d2dfeaa480eb6024d6dea33fefb72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079046
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Luke Z <lpz@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745441}

--- third_party/blink/web_tests/http/tests/security/contentSecurityPolicy/require-sri-for/require-sri-for-sharedworker-blocked.php
+++ third_party/blink/web_tests/http/tests/security/contentSecurityPolicy/require-sri-for/require-sri-for-sharedworker-blocked.php
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <meta http-equiv="Content-Security-Policy" content="require-sri-for script; script-src 'self' 'unsafe-inline'">
 <script>
 	test(function () {
-    	assert_throws("SecurityError",
+    	assert_throws_dom("SecurityError",
     		function() {
     			new SharedWorker("http://127.0.0.1:8000/security/contentSecurityPolicy/require-sri-for/sri-sharedworker.js");
     		},
--- third_party/blink/web_tests/http/tests/security/contentSecurityPolicy/resources/testharness-helper.js
+++ third_party/blink/web_tests/http/tests/security/contentSecurityPolicy/resources/testharness-helper.js
@@ -97,7 +97,7 @@ function assert_worker_is_blocked(url, description) {
 
     // TODO(mkwst): We shouldn't be throwing here. We should be firing an
     // `error` event on the Worker. https://crbug.com/663298
-    assert_throws("SecurityError", function () {
+    assert_throws_dom("SecurityError", function () {
       var w = new Worker(url);
     });
   }, description);
@@ -116,7 +116,7 @@ function assert_shared_worker_is_blocked(url, description) {
 
     // TODO(mkwst): We shouldn't be throwing here. We should be firing an
     // `error` event on the SharedWorker. https://crbug.com/663298
-    assert_throws("SecurityError", function () {
+    assert_throws_dom("SecurityError", function () {
       var w = new SharedWorker(url);
     });
   }, description);
