380b5d2f0f43c31c88fac5bab2ed89c5ed08ad61
Rushan Suleymanov <rushans@google.com>
2020-02-28 11:28:36 +0000

    Reland "Verify consistency between sync and sign-in account IDs"
    This reverts commit 9f5f561e4b2b6cb5a11813bc47d7d08f08a223ea.
    
    Reason for revert: reland this patch after fixing of DCHECK failures at crrev.com/c/2061751.
    
    Original change's description:
    > Revert "Verify consistency between sync and sign-in account IDs"
    > 
    > This reverts commit caf8eebf99fa96125a1b0ae48392e68b7dc2ce0b.
    > 
    > Reason for revert: suspect of running into sync protocol violations and
    > the corresponding DCHECK failures.
    > 
    > This CL reverts a preference being introduced without the corresponding
    > cleanup logic; but we intend to reland this patch anyway.
    > 
    > Original change's description:
    > > Verify consistency between sync and sign-in account IDs
    > >
    > > Local sync metadata belongs to one user account. Due to the distributed
    > > nature of the locally persisted sync metadata, the cache GUID is used
    > > as "epoch" to detect mismatched in edge cases like the browser crashing
    > > during shutdown and before I/O gets flushed.
    > >
    > > However, prior to this patch, the cache GUID itself has no safety
    > > mechanism to detect it maps to the intended account ID. In this patch,
    > > a new SyncPref is introduced to achieve that, in away that both prefs
    > > (cache GUID and account ID) are stored atomically.
    > >
    > > Because the pref is newly-introduced, migration logic is introduced to
    > > populate it for the first time if initially empty.
    > >
    > > Change-Id: I2cdd9f997077c4acd16e9283df8c025f51d40546
    > > Bug: 1046237,1021527
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023528
    > > Reviewed-by: Marc Treib <treib@chromium.org>
    > > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#736848}
    > 
    > TBR=msarda@chromium.org,treib@chromium.org,mastiz@chromium.org
    > 
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    > 
    > Bug: 1046237, 1021527, 1048771
    > Change-Id: Ic9dcaf53780350984c3036f6acdb549f631893f5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2041669
    > Commit-Queue: Rushan Suleymanov <rushans@google.com>
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#739371}
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1046237, 1021527, 1048771
    Change-Id: If1a4a8c897d6e8bb1bae3e54b24c549146063f57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2073757
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#745410}

--- components/sync/driver/profile_sync_service_unittest.cc
+++ components/sync/driver/profile_sync_service_unittest.cc
@@ -230,7 +230,7 @@ class ProfileSyncServiceTest : public ::testing::Test {
     service_.reset();
   }
 
-  void InitializeForNthSync() {
+  void PopulatePrefsForNthSync() {
     // Set first sync time before initialize to simulate a complete sync setup.
     SyncPrefs sync_prefs(prefs());
     sync_prefs.SetCacheGuid(kTestCacheGuid);
@@ -242,6 +242,10 @@ class ProfileSyncServiceTest : public ::testing::Test {
         /*registered_types=*/UserSelectableTypeSet::All(),
         /*selected_types=*/UserSelectableTypeSet::All());
     sync_prefs.SetFirstSetupComplete();
+  }
+
+  void InitializeForNthSync() {
+    PopulatePrefsForNthSync();
     service_->Initialize();
   }
 
@@ -1563,6 +1567,54 @@ TEST_F(ProfileSyncServiceTestWithStopSyncInPausedState,
   SignIn();
   service()->Shutdown();
   EXPECT_FALSE(service()->GetDisableReasons().Empty());
+}
+
+TEST_F(ProfileSyncServiceTest, ShouldPopulateAccountIdCachedInPrefs) {
+  SyncPrefs sync_prefs(prefs());
+
+  SignIn();
+  CreateService(ProfileSyncService::AUTO_START);
+  InitializeForNthSync();
+  ASSERT_EQ(SyncService::TransportState::ACTIVE,
+            service()->GetTransportState());
+  ASSERT_EQ(kTestCacheGuid, sync_prefs.GetCacheGuid());
+  EXPECT_EQ(signin::GetTestGaiaIdForEmail(kTestUser), sync_prefs.GetGaiaId());
+}
+
+TEST_F(ProfileSyncServiceTest,
+       ShouldNotPopulateAccountIdCachedInPrefsWithLocalSync) {
+  SyncPrefs sync_prefs(prefs());
+
+  SignIn();
+  CreateServiceWithLocalSyncBackend();
+  InitializeForNthSync();
+  ASSERT_EQ(SyncService::TransportState::ACTIVE,
+            service()->GetTransportState());
+  ASSERT_EQ(kTestCacheGuid, sync_prefs.GetCacheGuid());
+  EXPECT_TRUE(sync_prefs.GetGaiaId().empty());
+}
+
+// Verifies that local sync transport data is thrown away if there is a mismatch
+// between the account ID cached in SyncPrefs and the actual one.
+TEST_F(ProfileSyncServiceTest,
+       ShouldClearLocalSyncTransportDataDueToAccountIdMismatch) {
+  SyncPrefs sync_prefs(prefs());
+
+  SignIn();
+  CreateService(ProfileSyncService::AUTO_START);
+  PopulatePrefsForNthSync();
+  ASSERT_EQ(kTestCacheGuid, sync_prefs.GetCacheGuid());
+
+  // Manually override the authenticated account ID, which should be detected
+  // during initialization.
+  sync_prefs.SetGaiaId("corrupt_gaia_id");
+
+  service()->Initialize();
+
+  ASSERT_EQ(SyncService::TransportState::ACTIVE,
+            service()->GetTransportState());
+  EXPECT_NE(kTestCacheGuid, sync_prefs.GetCacheGuid());
+  EXPECT_EQ(signin::GetTestGaiaIdForEmail(kTestUser), sync_prefs.GetGaiaId());
 }
 
 }  // namespace
