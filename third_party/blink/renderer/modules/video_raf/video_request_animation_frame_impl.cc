1e369c02c9562f6c5502f21a22b8ca7d317aa61e
Dale Curtis <dalecurtis@chromium.org>
2020-02-28 20:48:57 +0000

    Add a microtask version of the video.rAF for testing.
    We're noticing that running the video.rAF as part of the rendering steps
    isn't always performant since the BeginFrame powering window.rAF is run
    much later than the one powering video rendering - resulting in frequent
    misses.
    
    While trying to diagnose this issue and whether we should switch to
    using a microtask we're running into issues where dev builds are not as
    performant as official builds, so to ease testing and analysis land a
    temporary microtask version of video.rAF behind a flag so we can compare
    using Mac/Win canary builds more effectively.
    
    Bug: 1012063
    Change-Id: Ie0609717d1863800550e858efbe144c8c0dde624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076180
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745602}

--- third_party/blink/renderer/modules/video_raf/video_request_animation_frame_impl.cc
+++ third_party/blink/renderer/modules/video_raf/video_request_animation_frame_impl.cc
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "media/base/media_switches.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_frame_metadata.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/scripted_animation_controller.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/loader/document_loader.h"
 #include "third_party/blink/renderer/modules/video_raf/video_frame_request_callback_collection.h"
+#include "third_party/blink/renderer/platform/bindings/microtask.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 
 namespace blink {
@@ -70,12 +72,24 @@ void VideoRequestAnimationFrameImpl::OnRequestAnimationFrame() {
 
   if (!pending_execution_) {
     pending_execution_ = true;
-    GetSupplementable()
-        ->GetDocument()
-        .GetScriptedAnimationController()
-        .ScheduleVideoRafExecution(
-            WTF::Bind(&VideoRequestAnimationFrameImpl::ExecuteFrameCallbacks,
-                      WrapWeakPersistent(this)));
+    if (base::FeatureList::IsEnabled(media::kUseMicrotaskForVideoRAF)) {
+      auto& time_converter =
+          GetSupplementable()->GetDocument().Loader()->GetTiming();
+      Microtask::EnqueueMicrotask(WTF::Bind(
+          &VideoRequestAnimationFrameImpl::ExecuteFrameCallbacks,
+          WrapWeakPersistent(this),
+          // TODO(crbug.com/1012063): Now is probably not the right value.
+          time_converter
+              .MonotonicTimeToZeroBasedDocumentTime(base::TimeTicks::Now())
+              .InMillisecondsF()));
+    } else {
+      GetSupplementable()
+          ->GetDocument()
+          .GetScriptedAnimationController()
+          .ScheduleVideoRafExecution(
+              WTF::Bind(&VideoRequestAnimationFrameImpl::ExecuteFrameCallbacks,
+                        WrapWeakPersistent(this)));
+    }
   }
 }
 
