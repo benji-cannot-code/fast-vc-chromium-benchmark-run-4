18ac8ed363400c3bf05eed85c90b255b103cab2a
Daniel Clark <daniec@microsoft.com>
2020-02-28 02:16:02 +0000

    Fix null check preventing AXObjectCacheImpl from registering as popup document lifecycle observer
    Fix the polarity on a couple of null checks that were preventing
    AXObjectCacheImpl from registering as a lifecycle notification observer
    for popup documents.  This failure to register was preventing AX focus
    events from being fired when focus changed inside of a popup.
    
    The week-picker-ax.html test was passing prematurely because of focus
    events received when opening the popup, without validating that
    additional events were fired when changing focus in the popup.  Update
    that test to confirm that events are also fired for a focus change in
    the popup itself.
    
    There were a couple of analogous tests (date-picker-ax.html,
    month-picker.html) that were deleted when we purged old date popup tests
    in favor of new tests written for the new popup implementation
    (crrev.com/c/1967608).  I don't see replacements for these in the new
    tests, so this change also restores those tests, updates them for
    the structure of the new form controls, and ensures that they are
    checking that focus events are fired due to changes in the popups and
    not just from events fired when the popups open.
    
    Bug: 1022920
    Change-Id: Ifbccaff74131031ab60d61a715475eecdca0a7a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2074815
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#745325}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -152,7 +152,7 @@ AXObject* AXObjectCacheImpl::Root() {
 }
 
 void AXObjectCacheImpl::InitializePopup(Document* document) {
-  if (!document || documents_.Contains(document) || document->View())
+  if (!document || documents_.Contains(document) || !document->View())
     return;
 
   documents_.insert(document);
@@ -160,7 +160,7 @@ void AXObjectCacheImpl::InitializePopup(Document* document) {
 }
 
 void AXObjectCacheImpl::DisposePopup(Document* document) {
-  if (documents_.Contains(document) || document->View())
+  if (!documents_.Contains(document) || !document->View())
     return;
 
   document->View()->UnregisterFromLifecycleNotifications(this);
