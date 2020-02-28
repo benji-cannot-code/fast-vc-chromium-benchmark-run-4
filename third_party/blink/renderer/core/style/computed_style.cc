f4f4a0b883e6322ed91b38e2e4e6ff318733b337
Ionel Popescu <iopopesc@microsoft.com>
2020-02-28 03:13:18 +0000

    Update default outline-offset for links to 1px.
    This CL updates the default outline-offset for links to 1px
    in order to improve text visibility.
    
    Bug: 1055241
    Change-Id: I8bfa522b9fbe1910e649b653a74261f87ae986ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076664
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745341}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -2217,10 +2217,13 @@ int ComputedStyle::GetDefaultOffsetForFocusRing() const {
   if (!::features::IsFormControlsRefreshEnabled())
     return 0;
 
-  // For FormControlsRefresh checkbox and radio have a 2px inner padding.
   if (EffectiveAppearance() == kCheckboxPart ||
-      EffectiveAppearance() == kRadioPart)
+      EffectiveAppearance() == kRadioPart) {
     return 2;
+  } else if (IsLink()) {
+    return 1;
+  }
+
   return 0;
 }
 
