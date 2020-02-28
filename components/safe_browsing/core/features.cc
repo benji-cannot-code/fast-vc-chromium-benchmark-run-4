45e09134353ff318d1ef1a5ea5241df47ea29d1e
Bettina <bdea@chromium.org>
2020-02-28 00:30:02 +0000

    Remove use local blacklist experiment from Android
    The experiment is no longer used anymore.
    
    Bug: 1054179
    Fixes: 1054179
    Change-Id: Ie00127cd5327e3313e7684db25797cd14d58ca70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2067563
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745271}

--- components/safe_browsing/core/features.cc
+++ components/safe_browsing/core/features.cc
@@ -107,10 +107,6 @@ const base::Feature kThreatDomDetailsTagAndAttributeFeature{
 const base::Feature kTriggerThrottlerDailyQuotaFeature{
     "SafeBrowsingTriggerThrottlerDailyQuota",
     base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::Feature kUseLocalBlacklistsV2{"SafeBrowsingUseLocalBlacklistsV2",
-                                          base::FEATURE_DISABLED_BY_DEFAULT};
-
 const base::Feature kUseNewDownloadWarnings{"UseNewDownloadWarnings",
                                             base::FEATURE_DISABLED_BY_DEFAULT};
 
@@ -142,7 +138,6 @@ constexpr struct {
     {&kSuspiciousSiteTriggerQuotaFeature, true},
     {&kThreatDomDetailsTagAndAttributeFeature, false},
     {&kTriggerThrottlerDailyQuotaFeature, false},
-    {&kUseLocalBlacklistsV2, true},
 };
 
 // Adds the name and the enabled/disabled status of a given feature.
