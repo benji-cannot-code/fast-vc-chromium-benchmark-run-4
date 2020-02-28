4fae35b2f7feecc5d896b51d5e402c70ceaee014
Nicholas Hollingum <hollingum@google.com>
2020-02-28 05:19:39 +0000

    Allow smarter choosing/re-using backup locations during crostini upgrade
    One feature of the mocks that is yet to be implemented is the handling
    of backup-location choice/re-use. In this CL we implement those
    features. Specifically:
     - Backup will use the default location without bringing up a
       file-chooser (unless it is taken, in which case the file chooser is
       brought up to confirm the overwrite/rename)
     - If the user selects "change location" the file chooser will be shown.
     - If backup completed successfully and upgrade fails, restore will
       immediately begin from the known backup location, rather than
       prompting the user to find their backup.
    
    Bug: 1024693
    Change-Id: Iaa57f7162e7c12b483ba30b205c720dd88ea0c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2068400
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745370}

--- chrome/browser/chromeos/crostini/crostini_upgrader.h
+++ chrome/browser/chromeos/crostini/crostini_upgrader.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_CROSTINI_CROSTINI_UPGRADER_H_
 
 #include "base/callback_forward.h"
+#include "base/optional.h"
 #include "base/scoped_observer.h"
 #include "chrome/browser/chromeos/crostini/crostini_export_import.h"
 #include "chrome/browser/chromeos/crostini/crostini_export_import_status_tracker.h"
@@ -39,6 +40,7 @@ class CrostiniUpgrader : public KeyedService,
   void AddObserver(CrostiniUpgraderUIObserver* observer) override;
   void RemoveObserver(CrostiniUpgraderUIObserver* observer) override;
   void Backup(const ContainerId& container_id,
+              bool show_file_chooser,
               content::WebContents* web_contents) override;
   void StartPrechecks() override;
   void Upgrade(const ContainerId& container_id) override;
@@ -65,12 +67,24 @@ class CrostiniUpgrader : public KeyedService,
   static constexpr int64_t kDiskRequired = 1 << 30;
 
  private:
+  void OnBackupPathChecked(const ContainerId& container_id,
+                           content::WebContents* web_contents,
+                           base::FilePath path,
+                           bool path_exists);
+  // Called when backup completes. If backup was completed successfully (which
+  // is different from if |result|==SUCCESS) the |backup_path| will contain a
+  // path to the backup tarball.
+  void OnBackup(CrostiniResult result,
+                base::Optional<base::FilePath> backup_path);
   void OnCancel(CrostiniResult result);
-  void OnBackup(CrostiniResult result);
   void OnBackupProgress(int progress_percent);
   void OnUpgrade(CrostiniResult result);
   void OnAvailableDiskSpace(int64_t bytes);
   void DoPrechecks();
+  void OnRestorePathChecked(const ContainerId& container_id,
+                            content::WebContents* web_contents,
+                            base::FilePath path,
+                            bool path_exists);
   void OnRestore(CrostiniResult result);
   void OnRestoreProgress(int progress_percent);
   CrostiniExportImport::OnceTrackerFactory MakeFactory();
@@ -106,6 +120,11 @@ class CrostiniUpgrader : public KeyedService,
   ScopedObserver<chromeos::PowerManagerClient,
                  chromeos::PowerManagerClient::Observer>
       pmc_observer_;
+
+  // When restoring after a failed upgrade, if the user successfully completed a
+  // backup, we will auto-restore from that (if the file still exists),
+  // otherwise |backup_path_|==nullopt and restore will bring up a file-chooser.
+  base::Optional<base::FilePath> backup_path_;
 
   base::WeakPtrFactory<CrostiniUpgrader> weak_ptr_factory_{this};
 };
--- chrome/browser/resources/chromeos/crostini_upgrader/app.js
+++ chrome/browser/resources/chromeos/crostini_upgrader/app.js
@@ -211,7 +211,7 @@ Polymer({
         }, () => {});
       case State.PROMPT:
         if (this.backupCheckboxChecked_) {
-          this.startBackup_();
+          this.startBackup_(/*showFileChooser=*/ false);
         } else {
           this.startPrechecks_(() => {
             this.startUpgrade_();
@@ -248,9 +248,17 @@ Polymer({
   },
 
   /** @private */
-  startBackup_() {
+  onChangeLocationButtonClick_() {
+    this.startBackup_(/*showFileChooser=*/ true);
+  },
+
+  /**
+   * @param {boolean} showFileChooser
+   * @private
+   */
+  startBackup_(showFileChooser) {
     this.state_ = State.BACKUP;
-    BrowserProxy.getInstance().handler.backup();
+    BrowserProxy.getInstance().handler.backup(showFileChooser);
   },
 
   /** @private */
