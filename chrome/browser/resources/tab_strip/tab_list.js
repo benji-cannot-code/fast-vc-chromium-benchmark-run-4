beb16966a246e891dcf3b796ab1458fcc3ef474b
John Lee <johntlee@chromium.org>
2020-02-28 00:51:42 +0000

    WebUI Tab Strip: Add event for tab group ID change
    This event is necessary as when a tab group is dragged from another
    window, the dragged element will still have the old ID. The dragged
    element's ID needs to be updated with the new ID so that the same
    element can be re-used.
    
    Bug: 1048894
    Change-Id: I5495326a90efd3350b3eedcb1cbb4efda8c69acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076761
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745297}

--- chrome/browser/resources/tab_strip/tab_list.js
+++ chrome/browser/resources/tab_strip/tab_list.js
@@ -286,6 +286,9 @@ class TabListElement extends CustomElement {
           'tab-group-visuals-changed',
           (groupId, visualData) =>
               this.onTabGroupVisualsChanged_(groupId, visualData));
+      this.addWebUIListener_(
+          'tab-group-id-replaced',
+          (oldId, newId) => this.onTabGroupIdReplaced_(oldId, newId));
     });
   }
 
@@ -500,6 +503,18 @@ class TabListElement extends CustomElement {
       return;
     }
     this.placeTabGroupElement(tabGroupElement, index);
+  }
+
+  /**
+   * @param {string} oldId
+   * @param {string} newId
+   * @private
+   */
+  onTabGroupIdReplaced_(oldId, newId) {
+    const tabGroupElement = this.findTabGroupElement_(oldId);
+    if (tabGroupElement) {
+      tabGroupElement.dataset.groupId = newId;
+    }
   }
 
   /**
