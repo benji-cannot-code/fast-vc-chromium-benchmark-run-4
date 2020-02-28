c61f1c417a59ec628fbf9e2fdef7878b2537bc72
Connie Wan <connily@chromium.org>
2020-02-28 01:27:58 +0000

    Refactor Editor Bubble to take the Browser (Editor Bubble refactor Part 1)
    WebUI has access to the Browser, so this should allow interfacing with Mohnstrudel. It also allows us to clean up a good portion of TabStripController that was only being used by the Editor Bubble.
    
    Still left to do: make it so that the dialog doesn't necesesarily need to take a TabGroupHeader.
    
    Bug: 1055537
    Change-Id: Ia341453a092a8083075d7a31b6e39705d883fbfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2072365
    Commit-Queue: Connie Wan <connily@chromium.org>
    Reviewed-by: Charlene Yan <cyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745309}

--- chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h
+++ chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
 
-class TabStripController;
+class Browser;
 
 namespace gfx {
 class Size;
@@ -38,7 +38,7 @@ class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView {
   // Shows the editor for |group|. Returns an *unowned* pointer to the
   // bubble's widget.
   static views::Widget* Show(TabGroupHeader* anchor_view,
-                             TabStripController* tab_strip_controller,
+                             const Browser* browser,
                              const tab_groups::TabGroupId& group);
 
   // views::BubbleDialogDelegateView:
@@ -48,7 +48,7 @@ class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView {
 
  private:
   TabGroupEditorBubbleView(TabGroupHeader* anchor_view,
-                           TabStripController* tab_strip_controller,
+                           const Browser* browser,
                            const tab_groups::TabGroupId& group);
   ~TabGroupEditorBubbleView() override;
 
@@ -58,7 +58,7 @@ class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView {
 
   void OnBubbleClose();
 
-  TabStripController* const tab_strip_controller_;
+  const Browser* const browser_;
   const tab_groups::TabGroupId group_;
 
   class TitleFieldController : public views::TextfieldController {
@@ -81,7 +81,7 @@ class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView {
 
   class ButtonListener : public views::ButtonListener {
    public:
-    explicit ButtonListener(TabStripController* tab_strip_controller,
+    explicit ButtonListener(const Browser* browser,
                             TabGroupHeader* anchor_view,
                             tab_groups::TabGroupId group);
 
@@ -89,7 +89,7 @@ class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView {
     void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
    private:
-    TabStripController* const tab_strip_controller_;
+    const Browser* const browser_;
     TabGroupHeader* anchor_view_;
     const tab_groups::TabGroupId group_;
   };
