933e04446627f4381357833c61866f991070136a
Khushal <khushalsagar@chromium.org>
2020-02-29 03:51:47 +0000

    gpu: Limit Qcomm tiled rendering workaround to webview.
    The crashes which we suspect are related to this extension are present
    only on webview.
    
    R=boliu@chromium.org,zmo@chromium.org
    
    Bug: 1042615,1055629
    Change-Id: I7f797753fde7520e5919c99e026f0a4e90f5d43d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080780
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Auto-Submit: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745740}

--- ui/gl/init/create_gr_gl_interface.h
+++ ui/gl/init/create_gr_gl_interface.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GL_INIT_CREATE_GR_GL_INTERFACE_H_
 #define UI_GL_INIT_CREATE_GR_GL_INTERFACE_H_
 
+#include <vector>
+
 #include "third_party/skia/include/gpu/gl/GrGLInterface.h"
 
 #include "ui/gl/init/gl_init_export.h"
@@ -23,7 +25,8 @@ namespace init {
 GL_INIT_EXPORT sk_sp<GrGLInterface> CreateGrGLInterface(
     const gl::GLVersionInfo& version_info,
     bool use_version_es2,
-    gl::ProgressReporter* progress_reporter = nullptr);
+    gl::ProgressReporter* progress_reporter = nullptr,
+    std::vector<const char*> blacklisted_extensions = {});
 
 }  // namespace init
 }  // namespace gl
