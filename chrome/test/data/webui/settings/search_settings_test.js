f17974d097d4917d870866fe217196e120ed6a5e
rbpotter <rbpotter@chromium.org>
2020-02-29 01:19:04 +0000

    Settings: Migrate search_settings.js to JS modules
    Autogenerate JS module version of search_settings.js and its test, in
    preparation for migrating basic_page/* to Polymer 3.
    
    Bug: 1026426
    Change-Id: I2ec9498dffd3839947f2a365a7200036f781cf8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079901
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745700}

--- chrome/test/data/webui/settings/search_settings_test.js
+++ chrome/test/data/webui/settings/search_settings_test.js
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import {getSearchManager} from 'chrome://settings/settings.js';
+// #import {Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// clang-format on
+
 suite('SearchSettingsTest', function() {
   let searchManager;
 
