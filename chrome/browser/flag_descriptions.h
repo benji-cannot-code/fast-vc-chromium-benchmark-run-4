34bdb5dc29f9c5bd6ffa7e0a8b36e30532b57e2d
Joel Hockey <joelhockey@chromium.org>
2020-03-01 23:18:41 +0000

    Add TerminalSystemAppLegacySettings flag
    The terminal settings page does not yet support all
    settings from the legacy page.  This flag allows
    users to open the legacy settings page.
    
    Bug: 1056444
    Change-Id: I81fe1757f8fa20d85d2547624f375c21f03e6f70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082652
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Jason Lin <lxj@google.com>
    Reviewed-by: Jason Lin <lxj@google.com>
    Cr-Commit-Position: refs/heads/master@{#745816}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1917,6 +1917,9 @@ extern const char kEnableVideoPlayerNativeControlsDescription[];
 extern const char kTerminalSystemAppName[];
 extern const char kTerminalSystemAppDescription[];
 
+extern const char kTerminalSystemAppLegacySettingsName[];
+extern const char kTerminalSystemAppLegacySettingsDescription[];
+
 extern const char kTerminalSystemAppSplitsName[];
 extern const char kTerminalSystemAppSplitsDescription[];
 
