a4b0f74b3854f5fe7f07c1f73388ef576680d653
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2020-03-01 08:00:48 +0000

    system-web-apps: Support chrome-untrusted:// SWAs
    This CL makes four small changes that unblock chrome-untrusted:// SWAs.
    
    1. Change DCHECK in SystemWebAppManager to account for
       chrome-untrusted:// SWAs.
    
    2. Change InstallableManager to consider chrome-untrusted:// as a secure
       transport scheme. Without this, InstallableManager doesn't return a
       manifest.
    
    3. Change convert_web_app.cc to not generate URLHandlers for
       chrome-untrusted:// URLs because Extensions doesn't support
       chrome-untrusted:// URLHandlers.
    
    4. Change app_launch_info.cc to allow chrome-untrusted:// URLs for
       launch_web_url. Making chrome-untrusted:// work with URLPattern has
       many undesirable side-effects e.g. it makes chrome-untrusted://
       URLs scriptable. Given that chrome-untrusted:// support is only
       needed temporarily for Bookmark Apps until they are deprecated, we
       simply check the parsed URL scheme, rather than adding
       chrome-untrusted:// to URLPattern and dealing with all the
       side-effects.
    
    Bug: 1056494
    Change-Id: I62f31fa5309734c890a396758145f6cd14ee2b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076861
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745788}

--- chrome/browser/web_applications/test/test_system_web_app_installation.h
+++ chrome/browser/web_applications/test/test_system_web_app_installation.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_WEB_APPLICATIONS_TEST_TEST_SYSTEM_WEB_APP_INSTALLATION_H_
 
 #include <memory>
+#include <string>
 
 #include "chrome/browser/web_applications/system_web_app_manager.h"
 #include "chrome/browser/web_applications/test/test_system_web_app_web_ui_controller_factory.h"
@@ -32,6 +33,8 @@ class TestSystemWebAppInstallation {
   SetUpAppNotShownInLauncher();
   static std::unique_ptr<TestSystemWebAppInstallation>
   SetUpAppWithAdditionalSearchTerms();
+  static std::unique_ptr<TestSystemWebAppInstallation>
+  SetUpChromeUntrustedApp();
 
   ~TestSystemWebAppInstallation();
 
@@ -51,8 +54,9 @@ class TestSystemWebAppInstallation {
   std::unique_ptr<KeyedService> CreateWebAppProvider(SystemAppInfo info,
                                                      Profile* profile);
   std::unique_ptr<TestWebAppProviderCreator> test_web_app_provider_creator_;
-  SystemAppType type_;
-  TestSystemWebAppWebUIControllerFactory web_ui_controller_factory_;
+  const SystemAppType type_;
+  std::unique_ptr<TestSystemWebAppWebUIControllerFactory>
+      web_ui_controller_factory_;
 };
 
 }  // namespace web_app
