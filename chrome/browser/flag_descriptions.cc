52d19cadafd9f4f9cdfed99d8890064511c51179
Reilly Grant <reillyg@chromium.org>
2020-03-02 21:10:08 +0000

    [webnfc] Remove flag from about:flags
    The flag to enable WebNFC is now the regular experimental web platform
    features flag. The "WebNFC" feature can still be used as a kill-switch
    but should not normally be touched by users or developers.
    
    Bug: 1035048
    Change-Id: I7989ccd610affb5f0b8341f112a583e5dbacc3f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071365
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746090}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -2350,9 +2350,6 @@ const char kEnableOfflinePreviewsName[] = "Offline Page Previews";
 const char kEnableOfflinePreviewsDescription[] =
     "Enable showing offline page previews on slow networks.";
 
-const char kEnableWebNfcName[] = "WebNFC";
-const char kEnableWebNfcDescription[] = "Enable WebNFC support.";
-
 const char kEphemeralTabUsingBottomSheetName[] =
     "An ephemeral Preview Tab using the bottom sheet";
 const char kEphemeralTabUsingBottomSheetDescription[] =
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1363,9 +1363,6 @@ extern const char kEnableRevampedContextMenuDescription[];
 extern const char kEnableOfflinePreviewsName[];
 extern const char kEnableOfflinePreviewsDescription[];
 
-extern const char kEnableWebNfcName[];
-extern const char kEnableWebNfcDescription[];
-
 extern const char kEphemeralTabUsingBottomSheetName[];
 extern const char kEphemeralTabUsingBottomSheetDescription[];
 
