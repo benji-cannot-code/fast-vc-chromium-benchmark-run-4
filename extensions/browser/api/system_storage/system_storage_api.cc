517dd8f7c3a41a939f60b59b36432f25af8f837e
Gabriel Charette <gab@chromium.org>
2020-03-02 15:44:15 +0000

    Migrate off base::ThreadPool()-as-a-trait in /extensions/browser/api/system_storage
    This CL is a logical no-op.
    
    It manually covers the use cases the script
    (https://crbug.com/1026641#c22) didn't cover in
    https://chromium-review.googlesource.com/c/chromium/src/+/2026350.
    
    A few of the affected files were also migrated to
    content::GetUIThreadTaskRunner() whenever surrounding code consistency
    was at stake (but the bulk of that migration is staged to happen after
    this one so this wasn't a priority in this batch).
    
    Most files intentionally kept base/task/post_task.h as an include. It is
    still required because of a cyclical dependency during the migration
    preventing base/task/thread_pool.h from implicitly providing
    base/task/task_traits.h to its users (callers that explicitly store
    TaskTraits and include task_traits.h don't have this problem). The
    post_task.h include will be cleaned up in a later phase.
    
    This CL was uploaded by git cl split.
    
    TBR=ningxin.hu@intel.com
    
    Bug: 1026641
    Change-Id: Ib31ef9cae55f5c6451e047b0fcf22346d080fecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079083
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745941}

--- extensions/browser/api/system_storage/system_storage_api.cc
+++ extensions/browser/api/system_storage/system_storage_api.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/task/post_task.h"
+#include "base/task/thread_pool.h"
 #include "base/task_runner_util.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -99,15 +100,12 @@ void SystemStorageEjectDeviceFunction::HandleResponse(
 
 SystemStorageGetAvailableCapacityFunction::
     SystemStorageGetAvailableCapacityFunction()
-    : query_runner_(base::CreateSequencedTaskRunner(
-          base::TaskTraits(base::ThreadPool(),
-                           base::TaskPriority::BEST_EFFORT,
-                           base::MayBlock(),
-                           base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN))) {}
+    : query_runner_(base::ThreadPool::CreateSequencedTaskRunner(
+          {base::TaskPriority::BEST_EFFORT, base::MayBlock(),
+           base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN})) {}
 
 SystemStorageGetAvailableCapacityFunction::
-    ~SystemStorageGetAvailableCapacityFunction() {
-}
+    ~SystemStorageGetAvailableCapacityFunction() = default;
 
 ExtensionFunction::ResponseAction
 SystemStorageGetAvailableCapacityFunction::Run() {
