a1d61bb895a5d7b18dfafb4d1e30fe1a4aec8275
Rune Lillesveen <futhark@chromium.org>
2020-03-02 07:53:49 +0000

    Add a new Chromium feature for blocking html parser on stylesheets.
    This patch adds a new Chromium feature to control the Blink runtime
    enabled feature BlockHTMLParserOnStyleSheets which blocks the html
    parser while loading stylesheets in <head>.
    
    This allows us to study performance impacts of the feature via finch
    experiments.
    
    Bug: 891767
    Change-Id: Ie61282bb9bdcecbbdb5a1dacb990ee766324e8fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080265
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745873}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -158,6 +158,8 @@ BLINK_COMMON_EXPORT extern const base::Feature kDiscardCodeCacheAfterFirstUse;
 BLINK_COMMON_EXPORT extern const base::Feature
     kSuppressContentTypeForBeaconMadeWithArrayBufferView;
 
+BLINK_COMMON_EXPORT extern const base::Feature kBlockHTMLParserOnStyleSheets;
+
 }  // namespace features
 }  // namespace blink
 
