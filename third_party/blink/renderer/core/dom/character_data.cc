d7ad68f106c39b4c2ed5a37a6f99455324d5b717
Kent Tamura <tkent@chromium.org>
2020-03-02 11:50:47 +0000

    Revert "Menulist SELECT: Avoid updating UA shadow nodes in InsertedInto() and RemovedFrom()"
    This reverts commit 8581152742d5afb7b5101a639f43e4a630e96401.
    
    Reason for revert: Suspected to cause a performance regression.
    
    Original change's description:
    > Menulist SELECT: Avoid updating UA shadow nodes in InsertedInto() and RemovedFrom()
    > 
    > HTMLOptionElement's InsertedInto() and RemovedFrom() called
    > MenuListSelectType::UpdateTextStyleAndContent(), which updates a Text
    > node in a UA shadow tree.  It triggered a DCHECK failure about
    > IsSlotAssignmentRecalcForbidden().  We should not update the DOM tree
    > in these functions.
    > 
    > This CL moves the logic of the InsertedInto() and the RemovedFrom() to
    > HTMLSelectElement::ChildrenChanged() and |HTMLOptGroupElement::
    > ChildrenChanged()|.
    > 
    > As for kAllChildrenRemoved such as |select.innerHTML=''|,
    > ChildrenChanged() had no ways to know removed nodes. This CL adds
    > ChildrenChangedAllChildrenRemovedNeedsList flag, and ContainerNode
    > provides a list of removed nodes if the flag is true.
    > 
    > This CL also updates comments on Node::InsertedInto(),
    > Node::RemovedFrom(), and ContainerNode::ChildrenChanged().
    > 
    > Bug: 1056094
    > Change-Id: I5a28a371b8bacd6551f17acfaf681032d615f1bb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2074840
    > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > Commit-Queue: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#745230}
    
    TBR=yosin@chromium.org,tkent@chromium.org,kojii@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1056094, 1057282
    Change-Id: Id1224c543839b4f6faae50a59586cbc593402e74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082667
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745901}

--- third_party/blink/renderer/core/dom/character_data.cc
+++ third_party/blink/renderer/core/dom/character_data.cc
@@ -234,11 +234,7 @@ void CharacterData::DidModifyData(const String& old_data, UpdateSource source) {
 
   if (parentNode()) {
     ContainerNode::ChildrenChange change = {
-        ContainerNode::kTextChanged,
-        this,
-        previousSibling(),
-        nextSibling(),
-        nullptr,
+        ContainerNode::kTextChanged, this, previousSibling(), nextSibling(),
         ContainerNode::kChildrenChangeSourceAPI};
     parentNode()->ChildrenChanged(change);
   }
