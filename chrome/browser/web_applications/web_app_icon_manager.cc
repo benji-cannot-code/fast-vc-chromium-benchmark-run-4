4c85dfb1dbfdfd74032184fc369fb3f598c654b8
Gabriel Charette <gab@chromium.org>
2020-03-02 04:00:08 +0000

    Migrate off base::ThreadPool()-as-a-trait in /chrome/browser/web_applications
    This CL is a logical no-op.
    
    It manually covers the use cases the script
    (https://crbug.com/1026641#c22) didn't cover in
    https://chromium-review.googlesource.com/c/chromium/src/+/2026350.
    
    A few of the affected files were also migrated to
    content::GetUIThreadTaskRunner() whenever surrounding code consistency
    was at stake (but the bulk of that migration is staged to happen after
    this one so this wasn't a priority in this batch).
    
    Most files intentionally kept base/task/post_task.h as an include. It is
    still required because of a cyclical dependency during the migration
    preventing base/task/thread_pool.h from implicitly providing
    base/task/task_traits.h to its users (callers that explicitly store
    TaskTraits and include task_traits.h don't have this problem). The
    post_task.h include will be cleaned up in a later phase.
    
    This CL was uploaded by git cl split.
    
    R=mgiuca@chromium.org
    
    Bug: 1026641
    Change-Id: Icf9315888817a4dd7309735b6f10df745b81121e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2078666
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745838}

--- chrome/browser/web_applications/web_app_icon_manager.cc
+++ chrome/browser/web_applications/web_app_icon_manager.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
-#include "base/task/post_task.h"
 #include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
 #include "chrome/browser/web_applications/components/web_app_utils.h"
 #include "chrome/browser/web_applications/file_utils_wrapper.h"
 #include "chrome/browser/web_applications/web_app.h"
@@ -261,7 +261,7 @@ std::vector<uint8_t> ReadCompressedIconBlocking(
 }
 
 constexpr base::TaskTraits kTaskTraits = {
-    base::ThreadPool(), base::MayBlock(), base::TaskPriority::USER_VISIBLE,
+    base::MayBlock(), base::TaskPriority::USER_VISIBLE,
     base::TaskShutdownBehavior::BLOCK_SHUTDOWN};
 
 }  // namespace
@@ -280,7 +280,7 @@ void WebAppIconManager::WriteData(AppId app_id,
                                   WriteDataCallback callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  base::PostTaskAndReplyWithResult(
+  base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, kTaskTraits,
       base::BindOnce(WriteDataBlocking, utils_->Clone(), web_apps_directory_,
                      std::move(app_id), std::move(icons)),
@@ -290,7 +290,7 @@ void WebAppIconManager::WriteData(AppId app_id,
 void WebAppIconManager::DeleteData(AppId app_id, WriteDataCallback callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  base::PostTaskAndReplyWithResult(
+  base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, kTaskTraits,
       base::BindOnce(DeleteDataBlocking, utils_->Clone(), web_apps_directory_,
                      std::move(app_id)),
@@ -319,7 +319,7 @@ void WebAppIconManager::ReadIcons(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   DCHECK(HasIcons(app_id, icon_sizes_in_px));
 
-  base::PostTaskAndReplyWithResult(
+  base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, kTaskTraits,
       base::BindOnce(ReadIconsBlocking, utils_->Clone(), web_apps_directory_,
                      app_id, icon_sizes_in_px),
@@ -335,7 +335,7 @@ void WebAppIconManager::ReadAllIcons(const AppId& app_id,
     return;
   }
 
-  base::PostTaskAndReplyWithResult(
+  base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, kTaskTraits,
       base::BindOnce(ReadIconsBlocking, utils_->Clone(), web_apps_directory_,
                      app_id, web_app->downloaded_icon_sizes()),
@@ -351,7 +351,7 @@ void WebAppIconManager::ReadSmallestIcon(const AppId& app_id,
       FindDownloadedSizeInPxMatchBigger(app_id, icon_size_in_px);
   DCHECK(best_size_in_px.has_value());
 
-  base::PostTaskAndReplyWithResult(
+  base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, kTaskTraits,
       base::BindOnce(ReadIconBlocking, utils_->Clone(), web_apps_directory_,
                      app_id, best_size_in_px.value()),
@@ -368,7 +368,7 @@ void WebAppIconManager::ReadSmallestCompressedIcon(
       FindDownloadedSizeInPxMatchBigger(app_id, icon_size_in_px);
   DCHECK(best_size_in_px.has_value());
 
-  base::PostTaskAndReplyWithResult(
+  base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, kTaskTraits,
       base::BindOnce(ReadCompressedIconBlocking, utils_->Clone(),
                      web_apps_directory_, app_id, best_size_in_px.value()),
@@ -397,7 +397,7 @@ void WebAppIconManager::ReadIconAndResize(const AppId& app_id,
 
   DCHECK(best_downloaded_size.has_value());
 
-  base::PostTaskAndReplyWithResult(
+  base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, kTaskTraits,
       base::BindOnce(ReadIconAndResizeBlocking, utils_->Clone(),
                      web_apps_directory_, app_id, best_downloaded_size.value(),
