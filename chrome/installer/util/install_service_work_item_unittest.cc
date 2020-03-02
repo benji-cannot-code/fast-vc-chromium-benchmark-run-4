04f97fcfaf3808921720adf0a784bb1e5226ed34
S. Ganesh <ganesh@chromium.org>
2020-03-02 21:21:44 +0000

    Create Service with new name if creation with original name fails.
    We create the service with a new name if the creation with the original
    name failed. I think this should take care of failures, and may perhaps
    fix issue 1023080 as well. The idea here is that in some cases, a
    deletion of the original service may not be complete and there may still
    has some stale state left over in the SCM database. In such cases, we
    may be unable to open it or recreate it. In these cases, we now will
    create a new name and install a new service.
    
    Bug: 1055470
    Change-Id: I7e0f5e76892fe29fa597c77cd4b64702a3f717c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071387
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746101}

--- chrome/installer/util/install_service_work_item_unittest.cc
+++ chrome/installer/util/install_service_work_item_unittest.cc
@@ -66,7 +66,7 @@ TEST_F(InstallServiceWorkItemTest, Do_MultiSzToVector) {
 }
 
 // This test is flaky, see https://crbug.com/1055470
-TEST_F(InstallServiceWorkItemTest, DISABLED_Do_FreshInstall) {
+TEST_F(InstallServiceWorkItemTest, Do_FreshInstall) {
   auto item = std::make_unique<InstallServiceWorkItem>(
       kServiceName, kServiceDisplayName,
       base::CommandLine(base::FilePath(kServiceProgramPath)));
@@ -80,7 +80,7 @@ TEST_F(InstallServiceWorkItemTest, DISABLED_Do_FreshInstall) {
 }
 
 // This test is flaky, see https://crbug.com/1055470
-TEST_F(InstallServiceWorkItemTest, DISABLED_Do_FreshInstallThenDeleteService) {
+TEST_F(InstallServiceWorkItemTest, Do_FreshInstallThenDeleteService) {
   auto item = std::make_unique<InstallServiceWorkItem>(
       kServiceName, kServiceDisplayName,
       base::CommandLine(base::FilePath(kServiceProgramPath)));
@@ -93,7 +93,7 @@ TEST_F(InstallServiceWorkItemTest, DISABLED_Do_FreshInstallThenDeleteService) {
 }
 
 // This test is flaky, see https://crbug.com/1055470
-TEST_F(InstallServiceWorkItemTest, DISABLED_Do_UpgradeNoChanges) {
+TEST_F(InstallServiceWorkItemTest, Do_UpgradeNoChanges) {
   auto item = std::make_unique<InstallServiceWorkItem>(
       kServiceName, kServiceDisplayName,
       base::CommandLine(base::FilePath(kServiceProgramPath)));
@@ -114,7 +114,7 @@ TEST_F(InstallServiceWorkItemTest, DISABLED_Do_UpgradeNoChanges) {
 }
 
 // This test is flaky, see https://crbug.com/1055470
-TEST_F(InstallServiceWorkItemTest, DISABLED_Do_UpgradeChangedCmdLine) {
+TEST_F(InstallServiceWorkItemTest, Do_UpgradeChangedCmdLine) {
   auto item = std::make_unique<InstallServiceWorkItem>(
       kServiceName, kServiceDisplayName,
       base::CommandLine(base::FilePath(kServiceProgramPath)));
