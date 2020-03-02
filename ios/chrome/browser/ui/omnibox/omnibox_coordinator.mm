4c92936db76c1c934ac396dfad0199a29f902148
Stepan Khapugin <stkhapugin@chromium.org>
2020-03-02 19:00:16 +0000

    [iOS] Fix the bug with suggestion scrolling.
    Fixes a bug that would close the popup when the suggestions are scrolled
    up or down.
    
    scroll the suggestion list. The keyboard should get dismissed on iPhone,
    but the suggestion list should stay on screen until "cancel" is pressed.
    
    Bug: 1039228
    Test: On any page (and NTP), focus the omnibox and type something. Now
    Change-Id: Ie174223e2b963a14eea9fb31ea8ecd8243bbee3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2081415
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#746030}

--- ios/chrome/browser/ui/omnibox/omnibox_coordinator.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_coordinator.mm
@@ -140,6 +140,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)endEditing {
   [self.textField resignFirstResponder];
+  _editView->EndEditing();
 }
 
 - (void)insertTextToOmnibox:(NSString*)text {
