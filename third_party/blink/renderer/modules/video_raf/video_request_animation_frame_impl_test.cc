53aa4458e4e67a81055173d00a43b1ca2f4188e1
Thomas Guilbert <tguilbert@chromium.org>
2020-03-02 23:24:47 +0000

    Fix video-raf test flakiness
    Despite hand coded test values, the difference between
    base::TimeTicks::Now() and the Document's origin time can result in times
    aligned with a 5 microseconds clock. This is a test failure, because we
    don't know whether or not the time values were successfully clamped.
    
    This CL uses the reference monotonic time instead of
    base::TimeTicks::Now() when initializing the test values, which
    guarantees deterministic results.
    
    Bug: 1057319
    Change-Id: I26ee485545a75ba25bc2e9187e78f032fce767e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083737
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746147}

--- third_party/blink/renderer/modules/video_raf/video_request_animation_frame_impl_test.cc
+++ third_party/blink/renderer/modules/video_raf/video_request_animation_frame_impl_test.cc
@@ -59,13 +59,12 @@ class MockFunction : public ScriptFunction {
 class MetadataHelper {
  public:
   static VideoFramePresentationMetadata* GetDefaultMedatada() {
-    InitializeFields();
+    DCHECK(initialized);
     return &metadata_;
   }
 
   static std::unique_ptr<VideoFramePresentationMetadata> CopyDefaultMedatada() {
-    InitializeFields();
-
+    DCHECK(initialized);
     auto copy = std::make_unique<VideoFramePresentationMetadata>();
 
     copy->presented_frames = metadata_.presented_frames;
@@ -79,8 +78,7 @@ class MetadataHelper {
     return copy;
   }
 
- private:
-  static void InitializeFields() {
+  static void InitializeFields(base::TimeTicks now) {
     if (initialized)
       return;
 
@@ -88,7 +86,6 @@ class MetadataHelper {
     // tell whether or not the implementation clamped their values. Therefore,
     // we manually set the values for a deterministic test, and make sure we
     // have sub-microsecond resolution for those values.
-    base::TimeTicks now = base::TimeTicks::Now();
 
     metadata_.presented_frames = 42;
     metadata_.presentation_time =
@@ -112,6 +109,7 @@ class MetadataHelper {
     initialized = true;
   }
 
+ private:
   static bool initialized;
   static VideoFramePresentationMetadata metadata_;
 };
@@ -345,6 +343,7 @@ TEST_F(VideoRequestAnimationFrameImplTest,
 
 TEST_F(VideoRequestAnimationFrameImplTest, VerifyParameters) {
   auto timing = GetDocument().Loader()->GetTiming();
+  MetadataHelper::InitializeFields(timing.ReferenceMonotonicTime());
 
   auto* callback =
       MakeGarbageCollected<VideoRafParameterVerifierCallback>(timing);
