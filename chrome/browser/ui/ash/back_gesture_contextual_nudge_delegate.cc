6f294520947dc6a6a8ee108ceac28b55f5f1cd6e
Findit <findit-for-me@appspot.gserviceaccount.com>
2020-03-02 11:49:41 +0000

    Revert "back gesture nudge: bugs fixing."
    This reverts commit eaa5d421d72c43753dd3f3b6cfec55ea3ce59ec1.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 745863 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtL2VhYTVkNDIxZDcyYzQzNzUzZGQzZjNiNmNmZWM1NWVhM2NlNTllYzEM
    
    Sample Failed Build: https://ci.chromium.org/b/8886950226744430016
    
    Sample Failed Step: ash_unittests
    
    Original change's description:
    > back gesture nudge: bugs fixing.
    > 
    > Fix a few small bugs related to back gesture:
    > - If we have a top active window that can perform back before entering
    > tablet mode, then when entering tablet mode, we should show the nudge
    > above the window.
    > - Fix an issue that if switching to an existing tab that can perform go
    > back operation, the nudge does not show up. We should trigger the
    > navigation status change when active tab changes.
    > - Also fix a crash happens when shutting down chrome when nudge is in
    > animation.
    > - Also use DidFinishNavigation() instead of NavigationEntryCommitted()
    > to inform observers when a navigation happens inside of the current
    > active webcontent.
    > 
    > Bug: 1009005
    > Change-Id: I2cc380b3e40fe63439452361e66da3aac1dec8c5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2081233
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#745863}
    
    
    Change-Id: I2f309dc9ede6fa11f78656d641fecffe31ba58dd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1009005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082888
    Cr-Commit-Position: refs/heads/master@{#745900}

--- chrome/browser/ui/ash/back_gesture_contextual_nudge_delegate.cc
+++ chrome/browser/ui/ash/back_gesture_contextual_nudge_delegate.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "content/public/browser/navigation_details.h"
-#include "content/public/browser/navigation_handle.h"
 #include "ui/aura/window.h"
 
 BackGestureContextualNudgeDelegate::BackGestureContextualNudgeDelegate(
@@ -44,10 +43,9 @@ void BackGestureContextualNudgeDelegate::MaybeStartTrackingNavigation(
   Observe(contents);
 }
 
-void BackGestureContextualNudgeDelegate::DidFinishNavigation(
-    content::NavigationHandle* navigation_handle) {
-  DCHECK(window_);
-  if (navigation_handle->HasCommitted())
+void BackGestureContextualNudgeDelegate::NavigationEntryCommitted(
+    const content::LoadCommittedDetails& load_details) {
+  if (window_)
     controller_->NavigationEntryChanged(window_);
 }
 
@@ -57,8 +55,6 @@ void BackGestureContextualNudgeDelegate::OnTabStripModelChanged(
     const TabStripSelectionChange& selection) {
   const bool active_tab_changed = selection.active_tab_changed();
   if (active_tab_changed) {
-    DCHECK(window_);
-    controller_->NavigationEntryChanged(window_);
     content::WebContents* contents = tab_strip_model->GetActiveWebContents();
     Observe(contents);
   }
