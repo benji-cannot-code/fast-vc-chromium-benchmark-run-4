a27e1035f42971586d81c72a69ff1cd22777827b
Gabriel Charette <gab@chromium.org>
2020-03-02 19:55:41 +0000

    [base] Add TaskRunner::PostTaskAndReplyWithResult
    As discussed @ https://groups.google.com/a/chromium.org/g/scheduler-dev/c/xQkcxeoMuyw
    
    This is blocking migration of
    base::PostTaskAndReplyWithResult(FROM_HERE, {BrowserThread::UI, ...);
    to
    content::GetUIThreadTaskRunner({})->PostTaskAndReplyWithResult(FROM_HERE, ...);
    
    Tests copied from task_runner_util_unittest.cc.
    Will migrate callers in a follow-up.
    
    R=ajwong@chromium.org
    
    Bug: 1026641
    Change-Id: I6fe1b29238a23001d02b9d86da142dd071b39128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076423
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746049}

--- base/task_runner_util.h
+++ base/task_runner_util.h
@@ -10,12 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/bind_helpers.h"
 #include "base/callback.h"
-#include "base/callback_helpers.h"
 #include "base/logging.h"
 #include "base/post_task_and_reply_with_result_internal.h"
 #include "base/task_runner.h"
+
+// TODO: Remove these unused includes after fixing IWYU usage in
+// affected files.
+#include "base/bind_helpers.h"
+#include "base/callback_helpers.h"
 
 namespace base {
 
@@ -33,6 +36,10 @@ namespace base {
 //     FROM_HERE,
 //     BindOnce(&DoWorkAndReturn),
 //     BindOnce(&Callback));
+//
+// DEPRECATED: Prefer calling|task_runner->PostTaskAndReplyWithResult(...)|
+// directly.
+// TODO(gab): Mass-migrate to the member method.
 template <typename TaskReturnType, typename ReplyArgType>
 bool PostTaskAndReplyWithResult(TaskRunner* task_runner,
                                 const Location& from_here,
