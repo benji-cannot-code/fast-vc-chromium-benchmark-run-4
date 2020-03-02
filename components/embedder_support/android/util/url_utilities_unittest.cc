489108a8eb14838e84bdaeba31185432413c45cc
Colin Blundell <blundell@chromium.org>
2020-03-02 09:25:51 +0000

    [Android] Move UrlUtilitiesUnitTest to be on //components-level class
    A previous CL copied UrlUtilities.java into //components in preparation
    for deleting the //chrome-level class once the internal repo had also
    been converted. This CL moves the unittest to be on the
    //components-level class; a followup will delete the //chrome-level
    class. (The move of the test is involved enough that I wanted to focus
    this CL just on that).
    
    Bug: 1031465
    Change-Id: I43aaebb21f7777bbc4cb8c50a3791abd793f0a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080423
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745879}

--- components/embedder_support/android/util/url_utilities_unittest.cc
+++ components/embedder_support/android/util/url_utilities_unittest.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
-#include "chrome/android/native_j_unittests_jni_headers/UrlUtilitiesUnitTest_jni.h"
+#include "components/embedder_support/android/native_j_unittests_jni_headers/UrlUtilitiesUnitTest_jni.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::android::AttachCurrentThread;
