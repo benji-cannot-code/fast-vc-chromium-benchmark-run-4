175e3bd3f09360575cc03053528866d4b1dc7d7b
Fabian Sommer <fabiansommer@chromium.org>
2020-03-02 14:06:41 +0000

    Remove old SecurityTokenRequest PinUI
    Delete dead code which was only used in the old UI for
    SecurityTokenRequests. This includes the Back button in LoginPinView.
    
    Bug: 1001288
    Change-Id: I3b6379597ede5c95bb445e63321db192cfdc2557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080472
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Auto-Submit: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745921}

--- ash/login/ui/lock_contents_view.h
+++ ash/login/ui/lock_contents_view.h
@@ -138,8 +138,6 @@ class ASH_EXPORT LockContentsView
   void ShowAdbEnabled();
   void ShowSystemInfo();
   void ShowParentAccessDialog();
-  void RequestSecurityTokenPin(SecurityTokenPinRequest request);
-  void ClearSecurityTokenPinRequest();
 
   // views::View:
   void Layout() override;
--- ash/login/ui/login_pin_view.h
+++ ash/login/ui/login_pin_view.h
@@ -45,13 +45,8 @@ namespace ash {
 //   |  <-   |  |   0   |  |  ->   |
 //   |       |  |   +   |  |       |
 //    -------    -------    -------
-//    _______
-//   |  BACK |
-//   |       |
-//    -------
 //
 // The <- represents the delete button while -> represents the submit button.
-// The "BACK" button is hidden by default.
 //
 class ASH_EXPORT LoginPinView : public NonAccessibleView {
  public:
@@ -76,7 +71,6 @@ class ASH_EXPORT LoginPinView : public NonAccessibleView {
     views::View* GetButton(int number) const;
     views::View* GetBackspaceButton() const;
     views::View* GetSubmitButton() const;
-    views::View* GetBackButton() const;
 
     // Sets the timers that are used for backspace auto-submit. |delay_timer| is
     // the initial delay before an auto-submit, and |repeat_timer| fires
@@ -91,7 +85,6 @@ class ASH_EXPORT LoginPinView : public NonAccessibleView {
   using OnPinKey = base::RepeatingCallback<void(int value)>;
   using OnPinBackspace = base::RepeatingClosure;
   using OnPinSubmit = base::RepeatingClosure;
-  using OnPinBack = base::RepeatingClosure;
 
   // Creates PIN view with the specified |keyboard_style|.
   // |on_key| is called whenever the user taps one of the pin buttons; must be
@@ -100,13 +93,10 @@ class ASH_EXPORT LoginPinView : public NonAccessibleView {
   // tapped key; must be non-null.
   // |on_submit| is called when the user wants to submit the PIN / password;
   // must be non-null.
-  // |on_back| is called when the user taps the back button; must be non-null
-  // if the back button is shown.
   LoginPinView(Style keyboard_style,
                const OnPinKey& on_key,
                const OnPinBackspace& on_backspace,
-               const OnPinSubmit& on_submit,
-               const OnPinBack& on_back);
+               const OnPinSubmit& on_submit);
   ~LoginPinView() override;
 
   // Notify accessibility that location of rows and LoginPinView changed.
@@ -118,7 +108,6 @@ class ASH_EXPORT LoginPinView : public NonAccessibleView {
   void OnPasswordTextChanged(bool is_empty);
 
  private:
-  class BackButton;
   class BackspacePinButton;
   class SubmitPinButton;
 
@@ -127,13 +116,11 @@ class ASH_EXPORT LoginPinView : public NonAccessibleView {
 
   bool is_back_button_visible_ = false;
 
-  BackButton* back_button_;
   BackspacePinButton* backspace_;
   SubmitPinButton* submit_button_;
   OnPinKey on_key_;
   OnPinBackspace on_backspace_;
   OnPinSubmit on_submit_;
-  OnPinBack on_back_;
 
   std::vector<NonAccessibleView*> rows;
 
