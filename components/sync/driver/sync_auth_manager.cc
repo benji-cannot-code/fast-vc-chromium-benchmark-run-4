43cb32c6125c71204f78df8a3e869b6d76977e22
Vincent Boisselle <vincb@chromium.org>
2020-03-02 19:56:47 +0000

    Fix signin flakes of metrics demographics android browsertests
    The fix consists to let the SyncAuthManager do a retry to fetch the access token when the first request is canceled.
    
    Bug: 1050313
    Change-Id: I173830f5b28b9fe2ec2a2d67032b52fc2eef344e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062294
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746051}

--- components/sync/driver/sync_auth_manager.cc
+++ components/sync/driver/sync_auth_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/time/time.h"
 #include "components/signin/public/identity_manager/access_token_fetcher.h"
@@ -54,6 +55,11 @@ constexpr net::BackoffEntry::Policy kRequestAccessTokenBackoffPolicy = {
 };
 
 }  // namespace
+
+// Enables the retry of the token fetch without backoff on the first fetch
+// cancellation.
+const base::Feature kSyncRetryFirstCanceledTokenFetch = {
+    "SyncRetryFirstCanceledTokenFetch", base::FEATURE_ENABLED_BY_DEFAULT};
 
 SyncAuthManager::SyncAuthManager(
     signin::IdentityManager* identity_manager,
@@ -499,6 +505,17 @@ void SyncAuthManager::AccessTokenFetched(
   DCHECK(ongoing_access_token_fetch_);
   ongoing_access_token_fetch_.reset();
   DCHECK(!request_access_token_retry_timer_.IsRunning());
+
+  // Retry without backoff when the request is canceled for the first time. For
+  // more details, see inline comments of
+  // PrimaryAccountAccessTokenFetcher::OnAccessTokenFetchComplete.
+  if (base::FeatureList::IsEnabled(kSyncRetryFirstCanceledTokenFetch) &&
+      error.state() == GoogleServiceAuthError::REQUEST_CANCELED &&
+      !access_token_retried_) {
+    access_token_retried_ = true;
+    RequestAccessToken();
+    return;
+  }
 
   access_token_ = access_token_info.token;
   partial_token_status_.token_response_time = base::Time::Now();
