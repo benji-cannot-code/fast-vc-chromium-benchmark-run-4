ea5258c24960f2dc684364c005adcfe4e57e447f
Evan Stade <estade@chromium.org>
2020-03-02 18:51:17 +0000

    WebLayer: Add autofill to context menu
    This matches WebView as well as Android TextView.
    
    If a user initially dismisses the Autofill popup, this context menu
    item allows them to access Autofill again.
    
    Bug: 1056468
    Change-Id: I2bee9d5960b92ed6b726402f7f570b09cad2eda5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079039
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746023}

--- components/autofill/android/java/src/org/chromium/components/autofill/AutofillActionModeCallback.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/AutofillActionModeCallback.java
@@ -3,14 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.android_webview;
+package org.chromium.components.autofill;
 
 import android.content.Context;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import org.chromium.components.autofill.AutofillProvider;
 
 /**
  * The class to implement autofill context menu. To match the Android native view behavior, the
