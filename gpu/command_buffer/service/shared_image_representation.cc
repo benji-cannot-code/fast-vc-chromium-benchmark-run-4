b562c23991ff1b9b352589d152856e924c3af779
Vasiliy Telezhnikov <vasilyt@chromium.org>
2020-03-02 20:29:58 +0000

    Hold source texture in SharedImageBackingEglImage
    It seems like on NVidia keeping reference to eglImage itself is not
    enough. When all siblings get deleted all future siblings texture
    are corrupted (like driver reuses this texture memory).
    
    This CL adds gpu driver workaround for NVidia to keep one sibling for
    the time of SharedImage is kept alive by client. After client calls
    DestroySharedImage this extra sibling is deleted allowing to clean up
    memory. As SharedImageBacking is kept alive only by representations
    after this moment there always will be at least one sibling inside them
    to prevent bug to occur.
    
    Bug: 1052114
    Change-Id: Ie1714e1f696e4e59cc5fba12c15e421f694f1a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080488
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746076}

--- gpu/command_buffer/service/shared_image_representation.cc
+++ gpu/command_buffer/service/shared_image_representation.cc
@@ -205,4 +205,8 @@ SharedImageRepresentationDawn::BeginScopedAccess(
       util::PassKey<SharedImageRepresentationDawn>(), this, texture);
 }
 
+SharedImageRepresentationFactoryRef::~SharedImageRepresentationFactoryRef() {
+  backing()->MarkForDestruction();
+}
+
 }  // namespace gpu
