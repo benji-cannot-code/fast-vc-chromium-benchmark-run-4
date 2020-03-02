408c7291c53edc7f9d683181da3c7566cfd9cddf
Yuki Shiino <yukishiino@chromium.org>
2020-03-02 13:05:34 +0000

    bindings: Fix IWYU
    Adds a missing header include to core/testing/type_convertion.h.
    
    Bug: 839389
    Change-Id: I610c1bf60d6b7c3ac0898383b37bb5c232fcb3d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082223
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#745915}

--- third_party/blink/renderer/core/testing/type_conversions.h
+++ third_party/blink/renderer/core/testing/type_conversions.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
