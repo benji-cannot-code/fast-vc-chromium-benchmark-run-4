04c8417cfa1e2683dab01b2faecd6f18ac79dada
Taras Antoshchuk <tantoshchuk@google.com>
2020-03-03 10:19:14 +0000

    Fallback to software TPM if needed
    Use software TPM implementation by default if
    tpm_fallback=true is in build arguments
    
    TEST=manual
    BUG=b:146479130
    
    Change-Id: Ibc6e17c97554ee6c685cedff9ca0b11bea738f2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2062995
    Commit-Queue: Taras Antoshchuk <tantoshchuk@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Auto-Submit: Taras Antoshchuk <tantoshchuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#746308}

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -182,6 +182,14 @@ void StartTPMSlotInitializationOnIOThread(const AccountId& account_id,
       base::BindOnce(&GetTPMInfoForUserOnUIThread, account_id, username_hash));
 }
 
+bool IsTPMTokenEnabledForNSS() {
+#if !defined(TPM_FALLBACK)
+  return crypto::IsTPMTokenEnabledForNSS();
+#else
+  return false;
+#endif
+}
+
 void StartNSSInitOnIOThread(const AccountId& account_id,
                             const std::string& username_hash,
                             const base::FilePath& path) {
@@ -200,7 +208,7 @@ void StartNSSInitOnIOThread(const AccountId& account_id,
 
   crypto::WillInitializeTPMForChromeOSUser(username_hash);
 
-  if (crypto::IsTPMTokenEnabledForNSS()) {
+  if (IsTPMTokenEnabledForNSS()) {
     if (crypto::IsTPMTokenReady(
             base::Bind(&StartTPMSlotInitializationOnIOThread, account_id,
                        username_hash))) {
