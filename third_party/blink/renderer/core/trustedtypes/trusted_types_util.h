cd6b6738e59be1d938813afd2b5d31a951250a6c
Daniel Vogelheim <vogelheim@chromium.org>
2020-03-03 14:38:49 +0000

    [Trusted Types] Remove unused functions.
    Remove unused functions and associated unit tests. This prepares for changes
    to rename & change the trusted_types_util.h interfaces.
    
    Bug: 739170
    Change-Id: I45ddf92d5946cb72a33ae4fba2bfb8d04f8c4d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2078611
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746359}

--- third_party/blink/renderer/core/trustedtypes/trusted_types_util.h
+++ third_party/blink/renderer/core/trustedtypes/trusted_types_util.h
@@ -26,14 +26,6 @@ enum class SpecificTrustedType {
   kTrustedScriptURL,
 };
 
-String CORE_EXPORT GetStringFromTrustedType(
-    const StringOrTrustedHTMLOrTrustedScriptOrTrustedScriptURL&,
-    const ExecutionContext*,
-    ExceptionState&);
-
-String CORE_EXPORT GetStringFromTrustedTypeWithoutCheck(
-    const StringOrTrustedHTMLOrTrustedScriptOrTrustedScriptURL&);
-
 String CORE_EXPORT GetStringFromSpecificTrustedType(
     const StringOrTrustedHTMLOrTrustedScriptOrTrustedScriptURL&,
     SpecificTrustedType,
