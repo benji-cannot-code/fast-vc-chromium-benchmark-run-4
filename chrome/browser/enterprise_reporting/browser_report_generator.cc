3c8056448be1c38cabc084c68e95e979cf8f02a3
Greg Thompson <grt@chromium.org>
2020-03-03 17:11:20 +0000

    Generate and upload enterprise reports following a browser update.
    - ReportGenerator can now optionally generate a report lacking full
      profile info.
    - BrowserReportGenerator and ReportGenerator are now stateless.
    - ReportScheduler now observes BuildState and generates/updates a
      minimal report for desktop Chrome when an update is available. If a
      trigger to generate a report arrives while a report is in-flight, its
      handling is deferred until the previous completes.
    
    BUG=1043624
    R=zmin@chromium.org
    
    Change-Id: I7864ee467bd681869ba0a57015c718cd811dc4e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036072
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746415}

--- chrome/browser/enterprise_reporting/browser_report_generator.cc
+++ chrome/browser/enterprise_reporting/browser_report_generator.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
@@ -46,18 +47,16 @@ BrowserReportGenerator::BrowserReportGenerator() = default;
 BrowserReportGenerator::~BrowserReportGenerator() = default;
 
 void BrowserReportGenerator::Generate(ReportCallback callback) {
-  DCHECK(!callback_);
-  callback_ = std::move(callback);
-
   auto report = std::make_unique<em::BrowserReport>();
   GenerateBasicInfos(report.get());
   GenerateProfileInfos(report.get());
 
   // std::move is required here because the function completes the report
   // asynchronously.
-  GeneratePluginsIfNeeded(std::move(report));
+  GeneratePluginsIfNeeded(std::move(callback), std::move(report));
 }
 
+// static
 void BrowserReportGenerator::GenerateBasicInfos(em::BrowserReport* report) {
 #if !defined(OS_CHROMEOS)
   report->set_browser_version(version_info::GetVersionNumber());
@@ -67,6 +66,7 @@ void BrowserReportGenerator::GenerateBasicInfos(em::BrowserReport* report) {
   report->set_executable_path(GetExecutablePath());
 }
 
+// static
 void BrowserReportGenerator::GenerateProfileInfos(em::BrowserReport* report) {
   for (auto* entry : g_browser_process->profile_manager()
                          ->GetProfileAttributesStorage()
@@ -87,18 +87,20 @@ void BrowserReportGenerator::GenerateProfileInfos(em::BrowserReport* report) {
 }
 
 void BrowserReportGenerator::GeneratePluginsIfNeeded(
+    ReportCallback callback,
     std::unique_ptr<em::BrowserReport> report) {
 #if defined(OS_CHROMEOS) || !BUILDFLAG(ENABLE_PLUGINS)
-  std::move(callback_).Run(std::move(report));
+  std::move(callback).Run(std::move(report));
 #else
-  content::PluginService::GetInstance()->GetPlugins(
-      base::BindOnce(&BrowserReportGenerator::OnPluginsReady,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(report)));
+  content::PluginService::GetInstance()->GetPlugins(base::BindOnce(
+      &BrowserReportGenerator::OnPluginsReady, weak_ptr_factory_.GetWeakPtr(),
+      std::move(callback), std::move(report)));
 #endif
 }
 
 #if BUILDFLAG(ENABLE_PLUGINS)
 void BrowserReportGenerator::OnPluginsReady(
+    ReportCallback callback,
     std::unique_ptr<em::BrowserReport> report,
     const std::vector<content::WebPluginInfo>& plugins) {
   for (content::WebPluginInfo plugin : plugins) {
@@ -109,7 +111,7 @@ void BrowserReportGenerator::OnPluginsReady(
     plugin_info->set_description(base::UTF16ToUTF8(plugin.desc));
   }
 
-  std::move(callback_).Run(std::move(report));
+  std::move(callback).Run(std::move(report));
 }
 #endif  // BUILDFLAG(ENABLE_PLUGINS)
 
