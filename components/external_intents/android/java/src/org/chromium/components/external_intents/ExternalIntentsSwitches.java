d7fd7c654b10977a17dd27b2cc128c21befbf7e8
Colin Blundell <blundell@chromium.org>
2020-03-03 08:18:48 +0000

    [Android] Create external_intents component, populating it with switch
    This CL creates //components/external_intents, initially populating it
    with the previously //chrome-level switch that is used to disable the
    launching of external intents. The ultimate goal is to have //chrome's
    ExternalNavigationHandler.java and friends live inside this component
    for sharing with WebLayer.
    
    Bug: 1031465
    Change-Id: I738f5a9eff6d415aedc85364cb4b8e6c0bb1475b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082377
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746285}

---
+++ components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalIntentsSwitches.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.external_intents;
+
+/**
+ * Contains all of the command line switches for external intent launching.
+ */
+public abstract class ExternalIntentsSwitches {
+    /** Never forward URL requests to external intents. */
+    public static final String DISABLE_EXTERNAL_INTENT_REQUESTS =
+            "disable-external-intent-requests";
+
+    // Prevent instantiation.
+    private ExternalIntentsSwitches() {}
+}
