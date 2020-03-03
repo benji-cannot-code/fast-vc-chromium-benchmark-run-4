c72cdddfa35dc99c3e611554f21ee5ee785f9fd6
arthursonzogni <arthursonzogni@chromium.org>
2020-03-03 09:09:08 +0000

    Cleanup redundant local variable.
    This is a follow-up to haraken@ comment on:
    https://chromium-review.googlesource.com/c/chromium/src/+/1533913/33/content/renderer/render_frame_impl.cc#1517
    
    Bug: None
    Change-Id: I03a15efd4bca486c7400693ac3cdfe9da32b14fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083532
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746296}

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -1503,8 +1503,6 @@ void RenderFrameImpl::CreateFrame(
     // main frame, as in the case where a navigation to the current process'
     // origin replaces a remote main frame (the proxy's web_frame()) with a
     // local one.
-    const bool proxy_is_main_frame = !proxy->web_frame()->Parent();
-
     render_view = proxy->render_view();
     render_frame = RenderFrameImpl::Create(
         render_view, routing_id, std::move(interface_provider),
@@ -1517,7 +1515,7 @@ void RenderFrameImpl::CreateFrame(
         proxy->web_frame(), replicated_state.frame_policy,
         WebString::FromUTF8(replicated_state.name));
     // The new |web_frame| is a main frame iff the proxy's frame was.
-    DCHECK_EQ(proxy_is_main_frame, !web_frame->Parent());
+    DCHECK_EQ(!proxy->web_frame()->Parent(), !web_frame->Parent());
   }
 
   CHECK(render_view);
