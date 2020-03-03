61e61961c18ee7a660748c83ac2ce4ca5a4bd3d1
Xing Liu <xingliu@chromium.org>
2020-03-03 21:14:35 +0000

    Revert "Android: Speculative crash fix for DelayedScreenLockIntentHandler"
    This reverts commit 91ac1e4597667f679f99a9c58ada806b0c5c19b1.
    
    Reason for revert: Breaks downstream test IntentHandlerWithInternalUrlsTest. It still calls static version of the function.
    https://logs.chromium.org/logs/chrome/buildbucket/cr-buildbucket.appspot.com/8886822837374455840/+/steps/compile/0/stdout 
    
    Original change's description:
    > Android: Speculative crash fix for DelayedScreenLockIntentHandler
    > 
    > Stack is:
    > Caused by: android.util.AndroidRuntimeException: Calling startActivity() from outside of an Activity  context requires the FLAG_ACTIVITY_NEW_TASK flag. Is this really what you want?
    >   at android.app.ContextImpl.startActivity	(ContextImpl.java: 952)
    >   at android.content.ContextWrapper.startActivity	(ContextWrapper.java: 405)
    >   at org.chromium.chrome.browser.ChromeApplication.startActivity	(ChromeApplication.java: 206)
    >   at org.chromium.chrome.browser.ChromeApplication.startActivity	(ChromeApplication.java: 199)
    >   at org.chromium.chrome.browser.DelayedScreenLockIntentHandler.onReceive	(DelayedScreenLockIntentHandler.java: 40)
    >   at android.app.LoadedApk$ReceiverDispatcher$Args.lambda$getRunnable$0$LoadedApk$ReceiverDispatcher$Args	(LoadedApk.java: 1550)
    >   ...
    > 
    > From history on DelayedScreenLockIntentHandler, repro steps involve
    > launching a page via NFC while locked.
    > 
    > This restriction is listed as a new one in Android P:
    > https://developer.android.com/about/versions/pie/android-9.0-changes-all#fant-required
    > 
    > Bug: 1034440
    > Change-Id: Ibe13fb99a34d36a15e266d9369393dd6060a1e93
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1972135
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#746442}
    
    TBR=tedchoc@chromium.org,agrieve@chromium.org
    
    Change-Id: Ie8a5c401509df389f4a9cefe9b66b54df2cdad2d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1034440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2085699
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746520}

--- chrome/android/java/src/org/chromium/chrome/browser/DelayedScreenLockIntentHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/DelayedScreenLockIntentHandler.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,8 +12,6 @@ import android.content.IntentFilter;
 import android.os.Handler;
 
 import org.chromium.base.ContextUtils;
-
-import java.lang.ref.WeakReference;
 
 /**
  * Intent handler that is specific for the situation when the screen is unlocked from pin, pattern,
@@ -25,17 +22,15 @@ import java.lang.ref.WeakReference;
 public class DelayedScreenLockIntentHandler extends BroadcastReceiver {
     private static final int VALID_DEFERRED_PERIOD_MS = 10000;
 
-    private final Handler mTaskHandler = new Handler();
-    private final Runnable mUnregisterTask = () -> updateDeferredIntent(null);
-    // Must be an Activity context for deferred intents without FLAG_NEW_TASK on Android P+.
-    // http://crbug.com/1034440
-    private final WeakReference<Activity> mActivity;
-    private Intent mDeferredIntent;
-    private boolean mEnabled;
+    private final Handler mTaskHandler;
+    private final Runnable mUnregisterTask;
 
-    public DelayedScreenLockIntentHandler(Activity activity) {
-        // Use a WeakReference so that Activity is not retained by call to postDelayed.
-        mActivity = new WeakReference<>(activity);
+    private Intent mDeferredIntent;
+    private boolean mReceiverRegistered;
+
+    public DelayedScreenLockIntentHandler() {
+        mTaskHandler = new Handler();
+        mUnregisterTask = () -> updateDeferredIntent(null);
     }
 
     @Override
@@ -43,10 +38,7 @@ public class DelayedScreenLockIntentHandler extends BroadcastReceiver {
         assert Intent.ACTION_USER_PRESENT.equals(intent.getAction());
 
         if (Intent.ACTION_USER_PRESENT.equals(intent.getAction()) && mDeferredIntent != null) {
-            Activity activity = mActivity.get();
-            if (activity != null) {
-                activity.startActivity(mDeferredIntent);
-            }
+            context.startActivity(mDeferredIntent);
             // Prevent the broadcast receiver from firing intent unexpectedly.
             updateDeferredIntent(null);
         }
@@ -56,12 +48,30 @@ public class DelayedScreenLockIntentHandler extends BroadcastReceiver {
      * Update the deferred intent with the target intent, also reset the deferred intent's lifecycle
      * @param intent Target intent
      */
-    public void updateDeferredIntent(Intent intent) {
+    public void updateDeferredIntent(final Intent intent) {
         mTaskHandler.removeCallbacks(mUnregisterTask);
-        mTaskHandler.postDelayed(mUnregisterTask, VALID_DEFERRED_PERIOD_MS);
-        mDeferredIntent = intent;
 
-        setEnabled(intent != null);
+        if (intent == null) {
+            unregisterReceiver();
+            mDeferredIntent = null;
+            return;
+        }
+
+        mDeferredIntent = intent;
+        registerReceiver();
+        mTaskHandler.postDelayed(mUnregisterTask, VALID_DEFERRED_PERIOD_MS);
+    }
+
+    /**
+     * Register to receive ACTION_USER_PRESENT when the screen is unlocked.
+     * The ACTION_USER_PRESENT is sent by platform to indicates when user is present.
+     */
+    private void registerReceiver() {
+        if (mReceiverRegistered) return;
+
+        ContextUtils.getApplicationContext()
+                .registerReceiver(this, new IntentFilter(Intent.ACTION_USER_PRESENT));
+        mReceiverRegistered = true;
     }
 
     /**
@@ -69,17 +79,11 @@ public class DelayedScreenLockIntentHandler extends BroadcastReceiver {
      * - When the deferred intent expires
      * - When updateDeferredIntent(null) called
      * - When the deferred intent has been fired
-     * Register to receive ACTION_USER_PRESENT when the screen is unlocked.
-     * The ACTION_USER_PRESENT is sent by platform to indicates when user is present.
      */
-    private void setEnabled(boolean value) {
-        if (value == mEnabled) return;
-        mEnabled = value;
-        Context applicationContext = ContextUtils.getApplicationContext();
-        if (value) {
-            applicationContext.registerReceiver(this, new IntentFilter(Intent.ACTION_USER_PRESENT));
-        } else {
-            applicationContext.unregisterReceiver(this);
-        }
+    private void unregisterReceiver() {
+        if (!mReceiverRegistered) return;
+
+        ContextUtils.getApplicationContext().unregisterReceiver(this);
+        mReceiverRegistered = false;
     }
 }
