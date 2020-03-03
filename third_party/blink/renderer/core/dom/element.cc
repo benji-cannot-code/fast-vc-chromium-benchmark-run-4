8bba20653d3d578b34adf19e693acd8eaf183217
Xiaocheng Hu <xiaochengh@chromium.org>
2020-03-03 00:38:12 +0000

    Fix tracing of element rare data
    ElementRareData is already traced in Node::Trace(), so there's no need
    to double-trace it in Element::Trace().
    
    By removing the conditional tracing that depends on node flags, this
    also fixes a data race between main thread and GC thread, which should
    not check node flags.
    
    Bug: 1056233
    Change-Id: If51f7946c5c41240cdc28969fb6eaad8891ff66e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083817
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746178}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -6406,8 +6406,6 @@ void Element::LogUpdateAttributeIfIsolatedWorldAndInDocument(
 }
 
 void Element::Trace(Visitor* visitor) {
-  if (HasRareData())
-    visitor->Trace(GetElementRareData());
   visitor->Trace(element_data_);
   ContainerNode::Trace(visitor);
 }
