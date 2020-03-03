8470d79a48c0aebf54b1cd42b7a784a16a9db97b
behdad <behdadb@chromium.org>
2020-03-03 17:31:57 +0000

    Code cleanup for expired MainAndImplFrameTimeDelta2
    Removed the code for the expired metric of MainAndImplFrameTimeDelta2
    in compositor_timing_history.
    
    Bug: chromium:1037242
    Change-Id: Ib5e6537275b4c585a9a4b031801d8ff042f15706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2075042
    Commit-Queue: Behdad Bakhshinategh <behdadb@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746423}

--- cc/scheduler/scheduler.cc
+++ cc/scheduler/scheduler.cc
@@ -633,10 +633,8 @@ void Scheduler::BeginImplFrame(const viz::BeginFrameArgs& args,
     bool has_damage =
         client_->WillBeginImplFrame(begin_impl_frame_tracker_.Current());
 
-    if (!has_damage) {
+    if (!has_damage)
       state_machine_.AbortDraw();
-      compositor_timing_history_->DrawAborted();
-    }
   }
 
   ProcessScheduledActions();
@@ -748,7 +746,6 @@ void Scheduler::DrawIfPossible() {
   state_machine_.DidDraw(result);
   compositor_timing_history_->DidDraw(
       drawing_with_new_active_tree,
-      begin_impl_frame_tracker_.DangerousMethodCurrentOrLast().frame_time,
       client_->CompositedAnimationsCount(),
       client_->MainThreadAnimationsCount(), client_->CurrentFrameHadRAF(),
       client_->NextFrameHasPendingRAF(),
@@ -767,7 +764,6 @@ void Scheduler::DrawForced() {
   state_machine_.DidDraw(result);
   compositor_timing_history_->DidDraw(
       drawing_with_new_active_tree,
-      begin_impl_frame_tracker_.DangerousMethodCurrentOrLast().frame_time,
       client_->CompositedAnimationsCount(),
       client_->MainThreadAnimationsCount(), client_->CurrentFrameHadRAF(),
       client_->NextFrameHasPendingRAF(),
@@ -857,7 +853,6 @@ void Scheduler::ProcessScheduledActions() {
         // No action is actually performed, but this allows the state machine to
         // drain the pipeline without actually drawing.
         state_machine_.AbortDraw();
-        compositor_timing_history_->DrawAborted();
         break;
       }
       case SchedulerStateMachine::Action::BEGIN_LAYER_TREE_FRAME_SINK_CREATION:
