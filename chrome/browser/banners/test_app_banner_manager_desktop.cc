0b0ddd164059af92d52139c002cf2dc8601f9af7
Glen Robertson <glenrob@chromium.org>
2020-03-03 02:00:39 +0000

    desktop-pwas: Remove fake_app_banner_manager and use test_ABM instead.
    Just moved all the methods across for now to ensure tests were not
    affected by this cleanup change. Some duplication (now within TABM)
    could be cleaned up in future.
    
    In prep for making an ABM factory as suggested by bsep@ in
    crrev.com/c/2075001
    
    Change-Id: Idcaad639bcc270a7ed3cc1f77f88b0fd5f2366a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082220
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746196}

--- chrome/browser/banners/test_app_banner_manager_desktop.cc
+++ chrome/browser/banners/test_app_banner_manager_desktop.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/run_loop.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/installable/installable_logging.h"
 #include "content/public/browser/web_contents.h"
 
@@ -46,6 +47,20 @@ bool TestAppBannerManagerDesktop::WaitForInstallableCheck() {
   return *installable_;
 }
 
+void TestAppBannerManagerDesktop::PrepareDone(base::OnceClosure on_done) {
+  on_done_ = std::move(on_done);
+}
+
+AppBannerManager::State TestAppBannerManagerDesktop::state() {
+  return AppBannerManager::state();
+}
+
+void TestAppBannerManagerDesktop::AwaitAppInstall() {
+  base::RunLoop loop;
+  on_install_ = loop.QuitClosure();
+  loop.Run();
+}
+
 void TestAppBannerManagerDesktop::OnDidGetManifest(
     const InstallableData& result) {
   AppBannerManagerDesktop::OnDidGetManifest(result);
@@ -69,11 +84,41 @@ void TestAppBannerManagerDesktop::ResetCurrentPageData() {
     std::move(tear_down_quit_closure_).Run();
 }
 
+void TestAppBannerManagerDesktop::OnInstall(blink::mojom::DisplayMode display) {
+  AppBannerManager::OnInstall(display);
+  if (on_install_)
+    std::move(on_install_).Run();
+}
+
+void TestAppBannerManagerDesktop::DidFinishCreatingWebApp(
+    const web_app::AppId& app_id,
+    web_app::InstallResultCode code) {
+  AppBannerManagerDesktop::DidFinishCreatingWebApp(app_id, code);
+  OnFinished();
+}
+
+void TestAppBannerManagerDesktop::UpdateState(AppBannerManager::State state) {
+  AppBannerManager::UpdateState(state);
+
+  if (state == AppBannerManager::State::PENDING_ENGAGEMENT ||
+      state == AppBannerManager::State::PENDING_PROMPT ||
+      state == AppBannerManager::State::COMPLETE) {
+    OnFinished();
+  }
+}
+
 void TestAppBannerManagerDesktop::SetInstallable(bool installable) {
   DCHECK(!installable_.has_value());
   installable_ = installable;
   if (installable_quit_closure_)
     std::move(installable_quit_closure_).Run();
+}
+
+void TestAppBannerManagerDesktop::OnFinished() {
+  if (on_done_) {
+    base::ThreadTaskRunnerHandle::Get()->PostTask(FROM_HERE,
+                                                  std::move(on_done_));
+  }
 }
 
 }  // namespace banners
