73b060e89d3027332f33ea86458b95a9bf1ac526
David Bokan <bokan@chromium.org>
2020-03-03 16:26:05 +0000

    Remove DCHECKs expecting scroll end
    As a result of pre-commit input suppression (see
    https://crbug.com/958424 for details and CLs), old expectations in the
    compositor that if we receive a ScrollUpdate or ScrollEnd we must have
    seen a ScrollBegin are no longer true. A begin might have been
    suppressed.
    
    Most of input handling code already early-outs and handles this case
    gracefully so we just remove DCHECKs here that guard this behavior.
    ScrollEnd must also early-out.
    
    Bug: 1041666
    Change-Id: Ie3f2b523100f40f59fee121c4be43f4a6870c8df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083934
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746398}

--- ui/events/blink/input_handler_proxy.cc
+++ ui/events/blink/input_handler_proxy.cc
@@ -182,9 +182,6 @@ InputHandlerProxy::InputHandlerProxy(cc::InputHandler* input_handler,
     : client_(client),
       input_handler_(input_handler),
       synchronous_input_handler_(nullptr),
-#if DCHECK_IS_ON()
-      expect_scroll_update_end_(false),
-#endif
       gesture_scroll_on_impl_thread_(false),
       scroll_sequence_ignored_(false),
       touch_result_(kEventDispositionUndefined),
@@ -811,9 +808,6 @@ InputHandlerProxy::EventDisposition InputHandlerProxy::HandleGestureScrollBegin(
   if (scroll_predictor_)
     scroll_predictor_->ResetOnGestureScrollBegin(gesture_event);
 
-#if DCHECK_IS_ON()
-  expect_scroll_update_end_ = true;
-#endif
   cc::ScrollState scroll_state = CreateScrollStateForGesture(gesture_event);
   cc::InputHandler::ScrollStatus scroll_status;
   cc::ElementIdType element_id_type =
@@ -877,10 +871,6 @@ InputHandlerProxy::HandleGestureScrollUpdate(
                -gesture_event.data.scroll_update.delta_x, "dy",
                -gesture_event.data.scroll_update.delta_y);
 
-#if DCHECK_IS_ON()
-  DCHECK(expect_scroll_update_end_);
-#endif
-
   if (scroll_sequence_ignored_) {
     TRACE_EVENT_INSTANT0("input", "Scroll Sequence Ignored",
                          TRACE_EVENT_SCOPE_THREAD);
@@ -899,9 +889,6 @@ InputHandlerProxy::HandleGestureScrollUpdate(
 
   if (snap_fling_controller_->HandleGestureScrollUpdate(
           GetGestureScrollUpdateInfo(gesture_event))) {
-#if DCHECK_IS_ON()
-    expect_scroll_update_end_ = false;
-#endif
     gesture_scroll_on_impl_thread_ = false;
     return DROP_EVENT;
   }
@@ -939,11 +926,6 @@ InputHandlerProxy::HandleGestureScrollUpdate(
 InputHandlerProxy::EventDisposition InputHandlerProxy::HandleGestureScrollEnd(
     const WebGestureEvent& gesture_event) {
   TRACE_EVENT0("input", "InputHandlerProxy::HandleGestureScrollEnd");
-#if DCHECK_IS_ON()
-  DCHECK(expect_scroll_update_end_);
-  expect_scroll_update_end_ = false;
-#endif
-
   if (scroll_sequence_ignored_)
     return DROP_EVENT;
 
