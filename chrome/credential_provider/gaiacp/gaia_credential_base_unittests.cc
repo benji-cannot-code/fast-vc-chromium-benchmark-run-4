209d8a3208455387ccb3b5463b0e10c2f6e465cb
Rakesh Soma <rakeshsoma@google.com>
2020-03-03 02:30:05 +0000

    Support multi-value AD accounts custom attribute instead of single-value.
    Bug: 1057855
    Change-Id: I7a2ffbea114ffffefaaed2c011fb9de5fe7e1923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2084032
    Commit-Queue: Rakesh Soma <rakeshsoma@google.com>
    Auto-Submit: Rakesh Soma <rakeshsoma@google.com>
    Reviewed-by: Yusuf Sengul <yusufsn@google.com>
    Cr-Commit-Position: refs/heads/master@{#746201}

--- chrome/credential_provider/gaiacp/gaia_credential_base_unittests.cc
+++ chrome/credential_provider/gaiacp/gaia_credential_base_unittests.cc
@@ -1441,7 +1441,7 @@ TEST_F(GcpGaiaCredentialBaseAdScenariosTest,
   // Invalid configuration in admin sdk. Don't set the username.
   std::string admin_sdk_response = base::StringPrintf(
       "{\"customSchemas\": {\"Enhanced_desktop_security\": {\"AD_accounts\":"
-      " \"%ls\\\\\"}}}",
+      "[{ \"value\": \"%ls\\\\\" }]}}}",
       domain_name);
   fake_http_url_fetcher_factory()->SetFakeResponse(
       GURL(get_cd_user_url_.c_str()), FakeWinHttpUrlFetcher::Headers(),
@@ -1496,7 +1496,7 @@ TEST_F(GcpGaiaCredentialBaseAdScenariosTest,
   // Set valid response from admin sdk.
   std::string admin_sdk_response = base::StringPrintf(
       "{\"customSchemas\": {\"Enhanced_desktop_security\": {\"AD_accounts\":"
-      " \"%ls\\\\%ls\"}}}",
+      "[{ \"value\": \"%ls\\\\%ls\" }]}}}",
       domain_name, user_name);
   fake_http_url_fetcher_factory()->SetFakeResponse(
       GURL(get_cd_user_url_.c_str()), FakeWinHttpUrlFetcher::Headers(),
