ea7ccc5ad47918906e4e63d1141c3222c2807475
Sylvain Defresne <sdefresne@chromium.org>
2020-03-03 17:33:48 +0000

    [ios/mac] Remove support for scoped_{nsobject,block} with ARC
    As iOS has fully migrated to ARC (automatic reference counting)
    and there is no traction to migration macOS to ARC, remove the
    support allowing to use scoped_{nsobject,block} in files build
    with ARC.
    
    The guards are not necessary as scoped_{nsobject,block} won't
    build with ARC since they contains calls to -release and cast
    blocks to void*. However, they make it easier to understand
    that this is not supported, so add them for documentation.
    
    Bug: 1051997
    Change-Id: I3db04ec645cb1830b92003723631c119e6e4a94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082385
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746425}

--- base/mac/scoped_block.h
+++ base/mac/scoped_block.h
@@ -11,9 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_typeref.h"
 
 #if defined(__has_feature) && __has_feature(objc_arc)
-#define BASE_MAC_BRIDGE_CAST(TYPE, VALUE) (__bridge TYPE)(VALUE)
-#else
-#define BASE_MAC_BRIDGE_CAST(TYPE, VALUE) VALUE
+#error "Cannot include base/mac/scoped_block.h in file built with ARC."
 #endif
 
 namespace base {
@@ -24,13 +22,8 @@ namespace internal {
 template <typename B>
 struct ScopedBlockTraits {
   static B InvalidValue() { return nullptr; }
-  static B Retain(B block) {
-    return BASE_MAC_BRIDGE_CAST(
-        B, Block_copy(BASE_MAC_BRIDGE_CAST(const void*, block)));
-  }
-  static void Release(B block) {
-    Block_release(BASE_MAC_BRIDGE_CAST(const void*, block));
-  }
+  static B Retain(B block) { return Block_copy(block); }
+  static void Release(B block) { Block_release(block); }
 };
 
 }  // namespace internal
@@ -38,36 +31,9 @@ struct ScopedBlockTraits {
 // ScopedBlock<> is patterned after ScopedCFTypeRef<>, but uses Block_copy() and
 // Block_release() instead of CFRetain() and CFRelease().
 template <typename B>
-class ScopedBlock : public ScopedTypeRef<B, internal::ScopedBlockTraits<B>> {
- public:
-  using Traits = internal::ScopedBlockTraits<B>;
-
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-  explicit ScopedBlock(
-      B block = Traits::InvalidValue(),
-      base::scoped_policy::OwnershipPolicy policy = base::scoped_policy::ASSUME)
-      : ScopedTypeRef<B, Traits>(block, policy) {}
-#else
-  explicit ScopedBlock(B block = Traits::InvalidValue())
-      : ScopedTypeRef<B, Traits>(block, base::scoped_policy::RETAIN) {}
-#endif
-
-#if !defined(__has_feature) || !__has_feature(objc_arc)
-  void reset(B block = Traits::InvalidValue(),
-             base::scoped_policy::OwnershipPolicy policy =
-                 base::scoped_policy::ASSUME) {
-    ScopedTypeRef<B, Traits>::reset(block, policy);
-  }
-#else
-  void reset(B block = Traits::InvalidValue()) {
-    ScopedTypeRef<B, Traits>::reset(block, base::scoped_policy::RETAIN);
-  }
-#endif
-};
+using ScopedBlock = ScopedTypeRef<B, internal::ScopedBlockTraits<B>>;
 
 }  // namespace mac
 }  // namespace base
-
-#undef BASE_MAC_BRIDGE_CAST
 
 #endif  // BASE_MAC_SCOPED_BLOCK_H_
