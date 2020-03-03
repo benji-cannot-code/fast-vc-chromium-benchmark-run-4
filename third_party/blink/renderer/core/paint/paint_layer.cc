32600bbd1ec11262a67e100074891e13bfb403ae
Manuel Rego Casasnovas <rego@igalia.com>
2020-03-03 16:27:12 +0000

    Fix performance regression in CompositingInputsRoot optimization
    In r745093 we fixed an issue with sticky elements with
    strict containment. However that introduced a performance regression
    in the following test: perf_tests/paint/contain-update-layer-tree.html
    
    This change applies the fix only for sticky positioned elements
    so the performance tests comes back to previous numbers.
    
    Change-Id: I686e2cfee13a3e44f5f67d05ed7526b6f01d4e5f
    Bug: 1057255,1057257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083030
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746400}

--- third_party/blink/renderer/core/paint/paint_layer.cc
+++ third_party/blink/renderer/core/paint/paint_layer.cc
@@ -1094,8 +1094,9 @@ void PaintLayer::SetNeedsCompositingInputsUpdateInternal() {
     last_ancestor = current;
     current->child_needs_compositing_inputs_update_ = true;
     if (Compositor() &&
-        (current != initial_layer &&
-         current->GetLayoutObject().ShouldApplyStrictContainment()))
+        (current != initial_layer ||
+         !current->GetLayoutObject().IsStickyPositioned()) &&
+        current->GetLayoutObject().ShouldApplyStrictContainment())
       break;
   }
 
