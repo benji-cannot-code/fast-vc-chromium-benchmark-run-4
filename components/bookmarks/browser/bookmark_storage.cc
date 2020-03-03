871215df877472715428fa6d725584f9a1943f51
Mikel Astiz <mastiz@chromium.org>
2020-03-03 18:48:49 +0000

    bookmarks: Remove obsolete sync transaction versions
    The USS implementation of bookmark sync launched with M77 and most code
    has already been cleaned up.
    
    Transaction versions embedded in BookmarkModel are no longer used and
    can also be deleted.
    
    Change-Id: Icfe2ab727edce293c73b7b3ae521df66a71d843a
    Bug: 933756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2081426
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746457}

--- components/bookmarks/browser/bookmark_storage.cc
+++ components/bookmarks/browser/bookmark_storage.cc
@@ -142,8 +142,6 @@ void LoadBookmarks(const base::FilePath& path,
       details->set_ids_reassigned(codec.ids_reassigned());
       details->set_guids_reassigned(codec.guids_reassigned());
       details->set_model_meta_info_map(codec.model_meta_info_map());
-      details->set_model_sync_transaction_version(
-          codec.model_sync_transaction_version());
       UMA_HISTOGRAM_TIMES("Bookmarks.DecodeTime",
                           TimeTicks::Now() - start_time);
       int64_t size = 0;
@@ -203,9 +201,7 @@ void LoadBookmarks(const base::FilePath& path,
 
 BookmarkLoadDetails::BookmarkLoadDetails(BookmarkClient* client)
     : load_managed_node_callback_(client->GetLoadManagedNodeCallback()),
-      index_(std::make_unique<TitledUrlIndex>()),
-      model_sync_transaction_version_(
-          BookmarkNode::kInvalidSyncTransactionVersion) {
+      index_(std::make_unique<TitledUrlIndex>()) {
   // WARNING: do NOT add |client| as a member. Much of this code runs on another
   // thread, and |client_| is not thread safe, and/or may be destroyed before
   // this.
