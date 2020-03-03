bd5ea6b4fe8e3c6c7693e3ec258ffbe7f0b65e87
Tim van der Lippe <tvanderlippe@chromium.org>
2020-03-03 18:34:44 +0000

    Remove DevTools startup layout tests
    This were replaced by e2etests in
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2062588
    and
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2062603
    
    TBR=aerotwist@chromium.org
    
    Bug: 1044632
    Change-Id: I857c07937cb753cf452d9fcef84a9f3f56eee1b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2085191
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746453}

--- third_party/blink/web_tests/http/tests/devtools/startup/resource-tree/resources/main-resource-content-frame-utf8.php
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?
-header('Content-Type: text/html; charset=utf-8');
-?>
-<html>
-<body>
-The following word is written using cyrillic letters and should look like "SUCCESS": SUССЕSS.
-</body>
-</html>
