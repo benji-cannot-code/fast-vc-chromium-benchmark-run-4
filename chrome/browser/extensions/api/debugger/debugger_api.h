3a8c43ee619cd7fd2f9a87ec32396b26358929fe
Anand K. Mistry <amistry@chromium.org>
2020-03-03 23:23:33 +0000

    [Extensions Functions] Migrate debugger API to ExtensionFunction
    Bug: 634140
    Change-Id: I38b8ad3ec7c01c2dedbb65281d7d4541bd553688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2078371
    Commit-Queue: Anand Mistry <amistry@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746576}

--- chrome/browser/extensions/api/debugger/debugger_api.h
+++ chrome/browser/extensions/api/debugger/debugger_api.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "chrome/browser/extensions/chrome_extension_function.h"
 #include "chrome/common/extensions/api/debugger.h"
 #include "content/public/browser/devtools_agent_host.h"
+#include "extensions/browser/extension_function.h"
 
 using extensions::api::debugger::Debuggee;
 
@@ -27,15 +27,15 @@ class DictionaryValue;
 namespace extensions {
 class ExtensionDevToolsClientHost;
 
-class DebuggerFunction : public ChromeAsyncExtensionFunction {
+class DebuggerFunction : public ExtensionFunction {
  protected:
   DebuggerFunction();
   ~DebuggerFunction() override;
 
-  void FormatErrorMessage(const std::string& format);
+  std::string FormatErrorMessage(const std::string& format);
 
-  bool InitAgentHost();
-  bool InitClientHost();
+  bool InitAgentHost(std::string* error);
+  bool InitClientHost(std::string* error);
   ExtensionDevToolsClientHost* FindClientHost();
 
   Debuggee debuggee_;
@@ -54,7 +54,7 @@ class DebuggerAttachFunction : public DebuggerFunction {
   ~DebuggerAttachFunction() override;
 
   // ExtensionFunction:
-  bool RunAsync() override;
+  ResponseAction Run() override;
 };
 
 // Implements the debugger.detach() extension function.
@@ -68,7 +68,7 @@ class DebuggerDetachFunction : public DebuggerFunction {
   ~DebuggerDetachFunction() override;
 
   // ExtensionFunction:
-  bool RunAsync() override;
+  ResponseAction Run() override;
 };
 
 // Implements the debugger.sendCommand() extension function.
@@ -84,7 +84,7 @@ class DebuggerSendCommandFunction : public DebuggerFunction {
   ~DebuggerSendCommandFunction() override;
 
   // ExtensionFunction:
-  bool RunAsync() override;
+  ResponseAction Run() override;
 };
 
 // Implements the debugger.getTargets() extension function.
@@ -98,10 +98,7 @@ class DebuggerGetTargetsFunction : public DebuggerFunction {
   ~DebuggerGetTargetsFunction() override;
 
   // ExtensionFunction:
-  bool RunAsync() override;
-
- private:
-  void SendTargetList(const content::DevToolsAgentHost::List& target_list);
+  ResponseAction Run() override;
 };
 
 }  // namespace extensions
