b2193f9468a167b98606a9f779c3a408c57a0588
Kent Tamura <tkent@chromium.org>
2020-03-03 01:05:16 +0000

    Move some parts of HTMLSelectElement::DispatchBlurEvent() to SelectType
    ... as SelectType::DidBlur().
    
    DispatchEventsIfSelectedOptionChanged() is now used only by
    MenuListSelectType. This CL removes |SelectType::
    DispatchEventsIfSelectedOptionChanged()|, and makes |MenuListSelectType
    ::DispatchEventsIfSelectedOptionChanged()| private.
    
    This CL has no behavior changes.
    
    Bug: 1052232
    Change-Id: I791b4377719cc629d26d96f5c79a4ccd91ac9bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2081721
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746183}

--- third_party/blink/renderer/core/html/forms/select_type.cc
+++ third_party/blink/renderer/core/html/forms/select_type.cc
@@ -69,7 +69,7 @@ class MenuListSelectType final : public SelectType {
   void DidSelectOption(HTMLOptionElement* element,
                        HTMLSelectElement::SelectOptionFlags flags,
                        bool should_update_popup) override;
-  void DispatchEventsIfSelectedOptionChanged() override;
+  void DidBlur() override;
 
   void UpdateTextStyle() override { UpdateTextStyleInternal(); }
   void UpdateTextStyleAndContent() override;
@@ -82,7 +82,7 @@ class MenuListSelectType final : public SelectType {
   bool ShouldOpenPopupForKeyPressEvent(const KeyboardEvent& event);
   // Returns true if this function handled the event.
   bool HandlePopupOpenKeyboardEvent();
-
+  void DispatchEventsIfSelectedOptionChanged();
   String UpdateTextStyleInternal();
   void DidUpdateActiveOption(HTMLOptionElement* option);
 
@@ -294,6 +294,16 @@ void MenuListSelectType::DispatchEventsIfSelectedOptionChanged() {
     select_->DispatchInputEvent();
     select_->DispatchChangeEvent();
   }
+}
+
+void MenuListSelectType::DidBlur() {
+  // We only need to fire change events here for menu lists, because we fire
+  // change events for list boxes whenever the selection change is actually
+  // made.  This matches other browsers' behavior.
+  DispatchEventsIfSelectedOptionChanged();
+  if (select_->PopupIsVisible())
+    select_->HidePopup();
+  SelectType::DidBlur();
 }
 
 String MenuListSelectType::UpdateTextStyleInternal() {
@@ -724,7 +734,9 @@ void SelectType::DidSelectOption(HTMLOptionElement*,
   select_->SetNeedsValidityCheck();
 }
 
-void SelectType::DispatchEventsIfSelectedOptionChanged() {}
+void SelectType::DidBlur() {
+  select_->last_on_change_selection_.clear();
+}
 
 void SelectType::UpdateTextStyle() {}
 
