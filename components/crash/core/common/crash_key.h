d01253111e039f3f8a12a41240a4d9ba926ef38b
Fergal Daly <fergal@chromium.org>
2020-03-04 16:05:31 +0000

    Mention the redaction process in debugging_with_crash_keys.md.
    Also link to debugging_with_crash_keys.md from crash_key.h.
    
    Bug: 1056426
    Change-Id: I8652eae061868e0ec220a89071e738fc2fef7ba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2086388
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746800}

--- components/crash/core/common/crash_key.h
+++ components/crash/core/common/crash_key.h
@@ -20,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Annotation interface. Because not all platforms use Crashpad yet, a
 // source-compatible interface is provided on top of the older Breakpad
 // storage mechanism.
+//
+// See https://cs.chromium.org/chromium/src/docs/debugging_with_crash_keys.md
+// for more information on using this.
 #if BUILDFLAG(USE_CRASHPAD_ANNOTATION) || BUILDFLAG(USE_COMBINED_ANNOTATIONS)
 #include "third_party/crashpad/crashpad/client/annotation.h"  // nogncheck
 #endif
