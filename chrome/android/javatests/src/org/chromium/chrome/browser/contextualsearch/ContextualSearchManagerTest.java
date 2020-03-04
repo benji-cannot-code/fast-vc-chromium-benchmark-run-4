e3ae3a82a6beb61049dae325819fe10cbd5488a7
Christian Dullweber <dullweber@chromium.org>
2020-03-04 13:00:09 +0000

    Disable flaky ContextualSearchManagerTests
    ContextualSearchManagerTest#testTapCausesOneLowPriorityRequest and
    ContextualSearchManagerTest#testAllInternalStatesVisitedResolvingTap are
    flaky.
    
    Tbr: donnd@chromium.org
    Bug: 1058297
    Change-Id: I88391ac0f3fabdfdd54fa894d966e6559c20afce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087582
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746745}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManagerTest.java
@@ -1308,6 +1308,7 @@ public class ContextualSearchManagerTest {
     @Test
     @SmallTest
     @Feature({"ContextualSearch"})
+    @DisabledTest(message = "crbug.com/1058297")
     public void testTapCausesOneLowPriorityRequest() throws TimeoutException {
         mFakeServer.reset();
         clickWordNode("states");
@@ -3011,6 +3012,7 @@ public class ContextualSearchManagerTest {
     @Test
     @SmallTest
     @Feature({"ContextualSearch"})
+    @DisabledTest(message = "crbug.com/1058297")
     public void testAllInternalStatesVisitedResolvingTap()
             throws InterruptedException, TimeoutException {
         // Set up a tracking version of the Internal State Controller.
