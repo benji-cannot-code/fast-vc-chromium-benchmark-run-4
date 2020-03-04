8b01a4c903a96239b0ab9db787ff545339cc6ec8
Henrique Nakashima <hnakashima@chromium.org>
2020-03-04 18:58:23 +0000

    Implement IntCachedFieldTrialParameter.
    This allows caching int-typed field trial parameters for use
    before native is loaded.
    
    Bug: 1041865, 1012975
    Change-Id: I9192d5fc7ee27dd617843e3afc5355d274d6e48f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2086255
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746850}

---
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/IntCachedFieldTrialParameter.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.flags;
+
+import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
+
+/**
+ * An int-type {@link CachedFieldTrialParameter}.
+ */
+public class IntCachedFieldTrialParameter extends CachedFieldTrialParameter {
+    private int mDefaultValue;
+
+    public IntCachedFieldTrialParameter(
+            String featureName, String variationName, int defaultValue) {
+        super(featureName, variationName, FieldTrialParameterType.INT, null);
+        mDefaultValue = defaultValue;
+    }
+
+    public int getDefaultValue() {
+        return mDefaultValue;
+    }
+
+    @Override
+    void cacheToDisk() {
+        int value = ChromeFeatureList.getFieldTrialParamByFeatureAsInt(
+                getFeatureName(), getParameterName(), getDefaultValue());
+        SharedPreferencesManager.getInstance().writeInt(getSharedPreferenceKey(), value);
+    }
+}
