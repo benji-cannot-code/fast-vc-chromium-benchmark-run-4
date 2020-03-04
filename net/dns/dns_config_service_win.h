d8a905434b88195eedc2a5c210b609206ec251b0
Kaustubha Govind <kaustubhag@chromium.org>
2020-03-04 02:30:10 +0000

    Add additional cases to Windows DNS configuration checking code.
    Improve DNS configuration checking to handle additional cases where
    we must defer to platform DNS, such as checking for DNS proxies and VPN
    configurations, and an additional policy that could be used to
    configure NRPT rules.
    
    Bug: 1052139
    Change-Id: I4884f76b576964e976114169113259e28fc78801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2055761
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Kaustubha Govind <kaustubhag@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746643}

--- net/dns/dns_config_service_win.h
+++ net/dns/dns_config_service_win.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // include these headers here.
 #include <winsock2.h>
 #include <iphlpapi.h>
+#include <iptypes.h>
 
 #include <memory>
 #include <string>
@@ -100,7 +101,17 @@ struct NET_EXPORT_PRIVATE DnsSystemSettings {
 
   // True when the Name Resolution Policy Table (NRPT) has at least one rule:
   // SOFTWARE\Policies\Microsoft\Windows NT\DNSClient\DnsPolicyConfig\Rule*
-  bool have_name_resolution_policy;
+  // (or)
+  // SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DnsPolicyConfig\Rule*
+  bool have_name_resolution_policy = false;
+
+  // True when a proxy is configured via at least one rule:
+  // SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DnsConnections
+  // (or)
+  // SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DnsActiveIfs
+  // (or)
+  // SYSTEM\CurrentControlSet\Services\Dnscache\Parameters\DnsConnectionsProxies
+  bool have_proxy = false;
 };
 
 enum ConfigParseWinResult {
