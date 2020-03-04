39bd538fc0509ff35fc96d7724131b2a49541162
Yann Dago <ydago@chromium.org>
2020-03-04 16:30:41 +0000

    User profile: Save the profile creation time in profile prefs
    Profile::GetCreationTime relied on the directory's creation time, which
    is not supported by all the file systems. For example, in POSIX we use
    the st_ctim property, which is the last file status change timestamp.
    Using the prefs will ensure that the creation time never changes if the
    profile directory is copied or if the file creation time is not
    supported by the file system. This also ensures that
    Profile::GetCreationTime works properly across enterprise snapshots.
    
    Bug: 958893
    Change-Id: I4e6189642939bb52524c412a9146087d597320dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079163
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746813}

--- chrome/browser/profiles/profile_impl.h
+++ chrome/browser/profiles/profile_impl.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/optional.h"
+#include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
@@ -182,7 +183,7 @@ class ProfileImpl : public Profile {
   ProfileImpl(const base::FilePath& path,
               Delegate* delegate,
               CreateMode create_mode,
-              base::Time creation_time,
+              base::Time path_creation_time,
               scoped_refptr<base::SequencedTaskRunner> io_task_runner);
 
 #if defined(OS_ANDROID)
@@ -224,7 +225,7 @@ class ProfileImpl : public Profile {
 
   base::FilePath path_;
 
-  base::Time creation_time_;
+  base::Time path_creation_time_;
 
   // Task runner used for file access in the profile path.
   scoped_refptr<base::SequencedTaskRunner> io_task_runner_;
