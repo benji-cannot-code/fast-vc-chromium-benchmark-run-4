ef926ebb1bf3422e8a520fe7c687cad86b236296
Rainhard Findling <rainhard@chromium.org>
2020-03-04 10:15:24 +0000

    Safety check UI: button colors reflect call-to-action
    * Safety check buttons are now either normal (white background)
      or action buttons (blue background).
    * This synchronizes the safety check UI with the latest UI mocks,
      which differentiate different levels of calls-to-action by their
      importance.
    
    Bug: 1015841
    Change-Id: I5c4b4444033b041735656ed58eceb34cfb036942
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080249
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#746716}

--- chrome/browser/resources/settings/safety_check_page/safety_check_page.js
+++ chrome/browser/resources/settings/safety_check_page/safety_check_page.js
@@ -504,6 +504,19 @@ Polymer({
     }
   },
 
+  /**
+   * @private
+   * @return {string}
+   */
+  getPasswordsButtonClass_: function() {
+    switch (this.passwordsStatus_) {
+      case settings.SafetyCheckPasswordsStatus.COMPROMISED:
+        return 'action-button';
+      default:
+        return '';
+    }
+  },
+
   /** @private */
   onPasswordsButtonClick_: function() {
     switch (this.passwordsStatus_) {
@@ -635,6 +648,19 @@ Polymer({
             this.badExtensionsCount_);
       default:
         assertNotReached();
+    }
+  },
+
+  /**
+   * @private
+   * @return {string}
+   */
+  getExtensionsButtonClass_: function() {
+    switch (this.extensionsStatus_) {
+      case settings.SafetyCheckExtensionsStatus.BAD_EXTENSIONS_ON:
+        return 'action-button';
+      default:
+        return '';
     }
   },
 });
