76701835bf1821f75e4f93ce6030adc10142fe4e
Antonio Gomes <tonikitoo@igalia.com>
2020-03-04 17:26:47 +0000

    Reset |local_main_frame_| mojo pipe in RFHI::InvalidateMojoConnection()
    This ensures that the AssociatedRemote<blink::mojom::LocalMainFrame>
    works after a crash.
    
    Analogously, the CL also resets |local_main_frame_host_receiver_|.
    
    BUG=None
    R=arthursonzogni@chromium.org
    
    Change-Id: I5f32afb6824afb30035051ba729377be40b0f374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2085233
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#746831}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -1896,10 +1896,6 @@ void RenderFrameHostImpl::RenderProcessExited(
   // process's channel.
   remote_associated_interfaces_.reset();
 
-  // Ensure that the AssociatedRemote<blink::mojom::LocalFrame> works after a
-  // crash.
-  local_frame_.reset();
-
   // Any termination disablers in content loaded by the new process will
   // be sent again.
   has_before_unload_handler_ = false;
@@ -6013,6 +6009,8 @@ void RenderFrameHostImpl::InvalidateMojoConnection() {
   frame_.reset();
   frame_bindings_control_.reset();
   frame_host_associated_receiver_.reset();
+  local_frame_.reset();
+  local_main_frame_.reset();
   navigation_control_.reset();
   frame_input_handler_.reset();
   find_in_page_.reset();
@@ -6028,6 +6026,7 @@ void RenderFrameHostImpl::InvalidateMojoConnection() {
   sensor_provider_proxy_.reset();
 
   local_frame_host_receiver_.reset();
+  local_main_frame_host_receiver_.reset();
   associated_registry_.reset();
 }
 
