6dc45c1de54c6ff6529ed77e9a288bd75fc88cfe
Nick Diego Yamane <nickdiego@igalia.com>
2020-03-04 20:49:02 +0000

    x11: CHECK for unexpected XWindow destruction during resize closure
    This should help us to determine what exactly caused XWindow
    destruction during DelayedResize closure execution so that we can
    propose a proper fix to it.
    
    R=gab@chromium.org
    
    Bug: 1021490
    Change-Id: Ie8e18d47b4c08d3c8aaf89d4f6dbf230f5c41b7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087664
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#746924}

--- ui/base/x/x11_window.cc
+++ ui/base/x/x11_window.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/location.h"
+#include "base/memory/weak_ptr.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -145,6 +146,7 @@ XWindow::XWindow()
 }
 
 XWindow::~XWindow() {
+  CHECK(!resize_weak_factory_.HasWeakPtrs());
   DCHECK_EQ(xwindow_, x11::None) << "XWindow destructed without calling "
                                     "Close() to release allocated resources.";
 }
@@ -1393,6 +1395,8 @@ void XWindow::DispatchResize() {
 }
 
 void XWindow::DelayedResize(const gfx::Rect& bounds_in_pixels) {
+  base::WeakPtr<XWindow> alive(resize_weak_factory_.GetWeakPtr());
+
   if (configure_counter_value_is_extended_ &&
       (current_counter_value_ % 2) == 0) {
     // Increase the |extended_update_counter_|, so the compositor will know we
@@ -1403,6 +1407,13 @@ void XWindow::DelayedResize(const gfx::Rect& bounds_in_pixels) {
                    ++current_counter_value_);
   }
   NotifyBoundsChanged(bounds_in_pixels);
+
+  // TODO(crbug.com/1021490): Crashes during window re-attaching while dragging
+  // a tab points out to the XWindow instance being destroyed in the middle of
+  // DelayedResize closure execution. This + CHECK'ing weak references in dtor
+  // helps better identify such unexpected scenario.
+  if (!alive)
+    return;
   CancelResize();
 }
 
--- ui/base/x/x11_window.h
+++ ui/base/x/x11_window.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/containers/flat_set.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/strings/string16.h"
 #include "ui/gfx/geometry/insets.h"
@@ -376,6 +377,8 @@ class COMPONENT_EXPORT(UI_BASE_X) XWindow {
   // Keep track of barriers to confine cursor.
   bool has_pointer_barriers_ = false;
   std::array<XID, 4> pointer_barriers_;
+
+  base::WeakPtrFactory<XWindow> resize_weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(XWindow);
 };
