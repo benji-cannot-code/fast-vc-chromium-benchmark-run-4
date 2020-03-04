1f2eb3e3fa26720ca964a7c95674a3e03cd2494f
Nazerke <nazerke@google.com>
2020-03-04 15:47:56 +0000

    [iOS][coordinator] Modernize Selector Coordinator.
    This CL modernizes the SelectorCoordinator to use |browser| in the
    initializer.
    
    Bug: 1029346, 1048675
    Change-Id: Id337cb6313adf9ba58cbb7080a45f3cb23458e1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087217
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Nazerke Kalidolda <nazerke@google.com>
    Cr-Commit-Position: refs/heads/master@{#746794}

--- ios/chrome/browser/ui/elements/selector_coordinator.h
+++ ios/chrome/browser/ui/elements/selector_coordinator.h
@@ -20,6 +20,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Coordinator for displaying UI to allow the user to pick among options.
 @interface SelectorCoordinator : ChromeCoordinator
 
+// Use -initWithBaseViewController:browser:
+- (nonnull instancetype)initWithBaseViewController:
+    (nonnull UIViewController*)viewController NS_UNAVAILABLE;
+
+// Use -initWithBaseViewController:browser:
+- (nonnull instancetype)
+    initWithBaseViewController:(nonnull UIViewController*)viewController
+                  browserState:(nonnull ChromeBrowserState*)browserState
+    NS_UNAVAILABLE;
+
 // Options to present to the user.
 @property(nonatomic, nullable, copy) NSOrderedSet<NSString*>* options;
 
