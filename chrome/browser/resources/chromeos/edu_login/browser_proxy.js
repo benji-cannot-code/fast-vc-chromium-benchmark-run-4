039cec070ada655ad91819f4d7caf762d1e9ad59
Anastasiia Nikolaienko <anastasiian@chromium.org>
2020-03-05 13:38:26 +0000

    Add EDU login flow: Add browser proxy
    Add browser proxy to call the EDU account login handler.
    
    Bug: 1043108
    Change-Id: Ia772b15b73568232ea13e714919adcbe6e98548b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2043470
    Commit-Queue: Anastasiia Nikolaienko <anastasiian@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747244}

---
+++ chrome/browser/resources/chromeos/edu_login/browser_proxy.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {addSingletonGetter, sendWithPromise} from 'chrome://resources/js/cr.m.js';
+import {ParentAccount} from './edu_login_util.js';
+
+/** @interface */
+export class EduAccountLoginBrowserProxy {
+  /**
+   * Send 'getParents' request to the handler. The promise will be resolved
+   * with the list of parents (Array<ParentAccount>).
+   * @return {Promise<Array<ParentAccount>>}
+   */
+  getParents() {}
+
+  /**
+   * Send parent credentials to get the reauth proof token. The promise will be
+   * resolved with the RAPT.
+   *
+   * @param {ParentAccount} parent
+   * @param {String} password parent password
+   * @return {Promise<string>}
+   */
+  parentSignin(parent, password) {}
+}
+
+/**
+ * @implements {EduAccountLoginBrowserProxy}
+ */
+export class EduAccountLoginBrowserProxyImpl {
+  /** @override */
+  getParents() {
+    return sendWithPromise('getParents');
+  }
+
+  /** @override */
+  parentSignin(parent, password) {
+    return sendWithPromise('parentSignin', parent, password);
+  }
+}
+
+addSingletonGetter(EduAccountLoginBrowserProxyImpl);
