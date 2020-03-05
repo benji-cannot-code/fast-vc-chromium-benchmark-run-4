df91b4b4f6673325f794c892e3b0e4f33bb0e727
Chris Letnick <cletnick@google.com>
2020-03-05 06:38:03 +0000

    Make ExternalConnector immediately usable.
    Mojo is designed to be asynchronous and for the user to not have to
    wait for and check connection states.
    
    Merge-With: eureka-internal/370252
    Merge-With: eureka-internal/370569
    Merge-With: eureka-internal/371453
    Merge-With: nest-camera-internal/54520
    
    Bug: internal 150158878
    Test: Verified on device.
    Change-Id: Ibb4fd27f7bfbffa0b9c9c695c6593f5bdbd599c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2086689
    Commit-Queue: Chris Letnick <cletnick@google.com>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Auto-Submit: Chris Letnick <cletnick@google.com>
    Cr-Commit-Position: refs/heads/master@{#747131}

--- chromecast/external_mojo/external_service_support/external_connector.h
+++ chromecast/external_mojo/external_service_support/external_connector.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/callback.h"
+#include "base/callback_list.h"
 #include "chromecast/external_mojo/public/mojom/connector.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -33,13 +33,19 @@ class ExternalConnector {
       const std::string& broker_path,
       base::OnceCallback<void(std::unique_ptr<ExternalConnector>)> callback);
 
+  static std::unique_ptr<ExternalConnector> Create(
+      const std::string& broker_path);
+
   virtual ~ExternalConnector() = default;
 
-  // Sets the callback that will be called if this class loses its connection to
-  // the Mojo broker. Note that once the connection is lost, this instance
-  // becomes nonfunctional (all public methods are no-ops); a new connection
-  // must be made instead.
-  virtual void SetConnectionErrorCallback(base::OnceClosure callback) = 0;
+  // Adds a callback that will be called if this class loses its connection to
+  // the Mojo broker. The calling class must retain the returned Subscription
+  // until it intends to unregister.
+  // By the time |callback| is executed, a new attempt at connecting will be
+  // started, and this object is valid. Note that some prior messages may be
+  // lost.
+  virtual std::unique_ptr<base::CallbackList<void()>::Subscription>
+  AddConnectionErrorCallback(base::RepeatingClosure callback) = 0;
 
   // Registers a service that other Mojo processes/services can bind to. Others
   // can call BindInterface(|service_name|, interface_name) to bind to this
