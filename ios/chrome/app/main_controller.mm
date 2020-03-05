d2f19a1e56ae04cc064a0b0ce7e6cf7fd13ee91b
Rohit Rao <rohitrao@chromium.org>
2020-03-05 16:58:35 +0000

    [ios] Adds OmahaServiceProvider::Stop().
    This will be used to properly remove observers during browser shutdown,
    so that the omaha service will not DCHECK() due to leaving observers
    registered.
    
    Additionally renames methods from Initialize/ShutDown to Start/Stop.
    
    BUG=996148
    
    Change-Id: Ia0e23834f2ec3e11f4e04db2038cc2e144caa2b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1933735
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747302}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -94,6 +94,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/metrics/previous_session_info.h"
 #import "ios/chrome/browser/net/cookie_util.h"
 #include "ios/chrome/browser/ntp_snippets/ios_chrome_content_suggestions_service_factory.h"
+#import "ios/chrome/browser/omaha/omaha_service.h"
 #include "ios/chrome/browser/pref_names.h"
 #import "ios/chrome/browser/reading_list/reading_list_download_service.h"
 #import "ios/chrome/browser/reading_list/reading_list_download_service_factory.h"
@@ -728,6 +729,8 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
   self.sceneController.browserViewWrangler = nil;
 
   // End of per-window code.
+
+  OmahaService::Stop();
 
   [_spotlightManager shutdown];
   _spotlightManager = nil;
--- ios/public/provider/chrome/browser/omaha/omaha_service_provider.mm
+++ ios/public/provider/chrome/browser/omaha/omaha_service_provider.mm
@@ -13,7 +13,17 @@ OmahaServiceProvider::OmahaServiceProvider() {}
 
 OmahaServiceProvider::~OmahaServiceProvider() {}
 
+void OmahaServiceProvider::Start() {
+  Initialize();
+}
+
 void OmahaServiceProvider::Initialize() {}
+
+void OmahaServiceProvider::Stop() {
+  ShutDown();
+}
+
+void OmahaServiceProvider::ShutDown() {}
 
 GURL OmahaServiceProvider::GetUpdateServerURL() const {
   return GURL();
