38c11229c1a34a19a3d88e5d23ca22e4c85773b7
Eugene But <eugenebut@chromium.org>
2020-03-05 19:29:19 +0000

    Add MobileWillEnterForeground user action for iOS
    Logged when app transitioned from background to foreground.
    Used for Breadcrumbs to log steps to reproduce for crashlogs.
    
    Bug: 1046231
    Change-Id: I6cc6b7d22fd19ce71347defbfb9246b3788125db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2085598
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747366}

--- ios/chrome/app/application_delegate/app_state.mm
+++ ios/chrome/app/application_delegate/app_state.mm
@@ -345,6 +345,8 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
     feature_engagement::TrackerFactory::GetForBrowserState(currentBrowserState)
         ->NotifyEvent(feature_engagement::events::kChromeOpened);
   }
+
+  base::RecordAction(base::UserMetricsAction("MobileWillEnterForeground"));
 }
 
 - (void)resumeSessionWithTabOpener:(id<TabOpening>)tabOpener
