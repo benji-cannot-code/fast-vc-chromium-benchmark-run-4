7899afb3bf9f781f4cc2857c8ee51cfd0b496fb7
Artem Sumaneev <asumaneev@google.com>
2020-03-05 11:47:56 +0000

    Add notification requesting user to powerwash
    User must not be able to run ARC applications and Crostini VM
    when following conditions are true:
    1. User is not affiliated
    2. DeviceRebootOnUserSignout policy is set and is not NEVER
    3. Cryptohome state requires powerwash
    
    If all the above is true then when user attempts to run
    ARC application or Crostini VM, notification is shown
    instead of actual run.
    
    BUG=b:146418942
    TEST=Manual and unit tests
    
    Cq-Depend: chromium:2033344
    Change-Id: I3d1a78750d098302c642b44343ab53c6983e80e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1992081
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/master@{#747211}

--- chrome/browser/chromeos/arc/session/arc_session_manager.cc
+++ chrome/browser/chromeos/arc/session/arc_session_manager.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/policy/arc_android_management_checker.h"
 #include "chrome/browser/chromeos/login/demo_mode/demo_resources.h"
 #include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
+#include "chrome/browser/chromeos/policy/powerwash_requirements_checker.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/policy/profile_policy_connector.h"
@@ -751,6 +752,16 @@ bool ArcSessionManager::RequestEnableImpl() {
     // Otherwise, be silent now. Users are notified when clicking ARC app icons.
     if (!start_arc_directly && g_ui_enabled)
       arc::ShowArcMigrationGuideNotification(profile_);
+    return false;
+  }
+
+  // When ARC is blocked because of powerwash request, do not proceed
+  // to starting ARC nor follow further state transitions. Applications are
+  // still visible but replaced with notification requesting powerwash.
+  policy::PowerwashRequirementsChecker pw_checker(
+      policy::PowerwashRequirementsChecker::Context::kArc, profile_);
+  if (pw_checker.GetState() !=
+      policy::PowerwashRequirementsChecker::State::kNotRequired) {
     return false;
   }
 
--- chrome/browser/chromeos/crostini/crostini_manager.cc
+++ chrome/browser/chromeos/crostini/crostini_manager.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/file_manager/path_util.h"
 #include "chrome/browser/chromeos/file_manager/volume_manager.h"
 #include "chrome/browser/chromeos/guest_os/guest_os_share_path.h"
+#include "chrome/browser/chromeos/policy/powerwash_requirements_checker.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/scheduler_configuration_manager.h"
 #include "chrome/browser/chromeos/usb/cros_usb_detector.h"
@@ -1217,6 +1218,17 @@ void CrostiniManager::StartTerminaVm(std::string name,
   if (!GetAnomalyDetectorClient()->IsGuestFileCorruptionSignalConnected()) {
     LOG(ERROR) << "GuestFileCorruptionSignal not connected, will not be "
                   "able to detect file system corruption.";
+    std::move(callback).Run(/*success=*/false);
+    return;
+  }
+
+  // When Crostini is blocked because of powerwash request, do not start VM,
+  // but show notification requesting powerwash.
+  policy::PowerwashRequirementsChecker pw_checker(
+      policy::PowerwashRequirementsChecker::Context::kCrostini, profile_);
+  if (pw_checker.GetState() !=
+      policy::PowerwashRequirementsChecker::State::kNotRequired) {
+    pw_checker.ShowNotification();
     std::move(callback).Run(/*success=*/false);
     return;
   }
