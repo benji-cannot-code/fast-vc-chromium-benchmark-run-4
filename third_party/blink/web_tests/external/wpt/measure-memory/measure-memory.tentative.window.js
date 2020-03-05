e01b2e0df0da6b7e96a75ce35c128da62e75b5f2
Ulan Degenbaev <ulan@chromium.org>
2020-03-05 15:16:59 +0000

    Update performance.measureMemory to the latest proposal
    This changes the result format of the API to the latest version of
    the proposal at https://github.com/WICG/performance-measure-memory.
    
    Specifically, per-origin attribution changed to per-frame attribution
    with one caveat that cross-origin iframes are considered opaque and
    iframes nested in cross-origin iframes do not appear in the result.
    
    The previous version of API:
    {
      bytes: 70*MB,
      breakdown: [
        {bytes: 40*MB, globals: 2, type: 'js', origins: ['foo.com']},
        {bytes: 30*MB, globals: 1, type: 'js', origins: ['bar.com']}
      ]
    }
    
    The current version of the API:
    {
      bytes: 70*MB,
      breakdown: [
        {bytes: 40*MB, type: 'window/js', attribution: ['foo.com']},
        {bytes: 30*MB, type: 'window/js', attribution: ['bar.com']}
      ]
    }
    
    Bug: 1049093
    Change-Id: I3bbf07ad6e978b9b483561c06cedf6c7a135b7e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087627
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747271}

--- third_party/blink/web_tests/external/wpt/measure-memory/measure-memory.tentative.window.js
+++ third_party/blink/web_tests/external/wpt/measure-memory/measure-memory.tentative.window.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 promise_test(async testCase => {
   try {
     let result = await performance.measureMemory();
-    checkMeasureMemory(result);
+    checkMeasureMemory(result, {allowed: [window.location.href]});
   } catch (error) {
     if (!(error instanceof DOMException)) {
       throw error;
