4f548b34f2e4f348ed1a832aef4a2a9aa6f71cd5
Min Qin <qinmin@chromium.org>
2020-03-05 15:54:53 +0000

    Reland "Upstream internal DownloadCollectionBridge code"
    This reverts commit f5bf498e96b6d143869494d55deac834ad75244a.
    
    Reason for revert: The revert will cause downstream bot to fail.
    The issue is in the android perf bot, as it is not picking up
    downstream patches.
    
    Original change's description:
    > Revert "Upstream internal DownloadCollectionBridge code"
    > 
    > This reverts commit 680e32224e520327e455db3be93db13f73c0172d.
    > 
    > Reason for revert: Looks like this broke https://ci.chromium.org/p/chrome/builders/ci/android_arm64-builder-perf/160982
    > 
    > Original change's description:
    > > Upstream internal DownloadCollectionBridge code
    > > 
    > > This will allow download in public chromium build to work on Q with MediaStore.
    > > 
    > > BUG=1052490
    > > 
    > > Change-Id: I31476292b92c7a5882fa4ad80cc60b29f6b14dba
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057806
    > > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > > Commit-Queue: Min Qin <qinmin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#746867}
    > 
    > TBR=yfriedman@chromium.org,dtrainor@chromium.org,qinmin@chromium.org,rsesek@chromium.org,gmariani@google.com
    > 
    > Change-Id: I1547f1132bbddf575eeacfe2498b97ff1d7cc473
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1052490
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089771
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#747233}
    
    TBR=yfriedman@chromium.org,dtrainor@chromium.org,qinmin@chromium.org,rsesek@chromium.org,dullweber@chromium.org,gmariani@google.com
    
    Change-Id: I2df7204834f3357728297df6b80d8b59d946c87f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1052490
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090011
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747286}

---
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/DownloadDelegateImpl.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download;
+
+import android.net.Uri;
+
+import org.chromium.components.download.DownloadDelegate;
+
+/**
+ * Utility class that implements DownloadDelegate.
+ */
+public class DownloadDelegateImpl extends DownloadDelegate {
+    public DownloadDelegateImpl() {}
+
+    @Override
+    public String remapGenericMimeType(String mimeType, String url, String filename) {
+        return MimeUtils.remapGenericMimeType(mimeType, url, filename);
+    }
+
+    @Override
+    public Uri parseOriginalUrl(String originalUrl) {
+        return UriUtils.parseOriginalUrl(originalUrl);
+    }
+
+    @Override
+    public boolean isDownloadOnSDCard(String filePath) {
+        return DownloadDirectoryProvider.isDownloadOnSDCard(filePath);
+    }
+}
