15f417470b66ede94383720f76cf91e2f515d0ba
Mugdha Lakhani <nator@chromium.org>
2020-03-05 12:01:22 +0000

    [WebLayer] Increment version for UrlBarController
    Increment the version UrlBarController is available from.
    
    Bug: 1025607
    Change-Id: I1331921c937883e265b2f6cdf1520039f8f350cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2081416
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747215}

--- weblayer/public/java/org/chromium/weblayer/Browser.java
+++ weblayer/public/java/org/chromium/weblayer/Browser.java
@@ -46,7 +46,7 @@ public class Browser {
 
         try {
             mImpl.setClient(new BrowserClientImpl());
-            if (WebLayer.getSupportedMajorVersionInternal() >= 81) {
+            if (WebLayer.getSupportedMajorVersionInternal() >= 82) {
                 mUrlBarController = new UrlBarController(mImpl.getUrlBarController());
             } else {
                 mUrlBarController = null;
@@ -255,12 +255,12 @@ public class Browser {
 
     /**
      * Returns the UrlBarController.
-     * @since 81
+     * @since 82
      */
     @NonNull
     public UrlBarController getUrlBarController() {
         ThreadCheck.ensureOnUiThread();
-        if (WebLayer.getSupportedMajorVersionInternal() < 81) {
+        if (WebLayer.getSupportedMajorVersionInternal() < 82) {
             throw new UnsupportedOperationException();
         }
         return mUrlBarController;
