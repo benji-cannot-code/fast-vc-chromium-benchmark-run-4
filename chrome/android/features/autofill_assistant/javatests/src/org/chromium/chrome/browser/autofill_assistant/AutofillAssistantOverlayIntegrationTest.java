fe0de716525dc10cabf62c3bca45f1c855f0ed7f
Tomasz Wiszkowski <ender@google.com>
2020-03-05 00:03:17 +0000

    Disable flaky AutofillAssistantOverlayIntegrationTests.
    Link to recent failures: http://shortn/_HrAAo0lrn8
    TBR=sandromaggi@chromium.org
    
    Bug: 1058599
    Change-Id: I71a98fb1b0a38e65eef9b331ac544cf955d3b33b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2088298
    Reviewed-by: Ender <ender@google.com>
    Commit-Queue: Ender <ender@google.com>
    Cr-Commit-Position: refs/heads/master@{#747014}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantOverlayIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantOverlayIntegrationTest.java
@@ -25,6 +25,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.autofill_assistant.proto.ActionProto;
 import org.chromium.chrome.browser.autofill_assistant.proto.ChipProto;
@@ -163,6 +164,7 @@ public class AutofillAssistantOverlayIntegrationTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "flaky on Android L; crbug.com/1058599")
     public void testShowCastOnIFrameElement() throws Exception {
         ElementReferenceProto element = (ElementReferenceProto) ElementReferenceProto.newBuilder()
                                                 .addSelectors("#iframe")
@@ -212,6 +214,7 @@ public class AutofillAssistantOverlayIntegrationTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "flaky on Android L; crbug.com/1058599")
     public void testShowCastOnIFrameElementInScrollIFrame() throws Exception {
         ElementReferenceProto element = (ElementReferenceProto) ElementReferenceProto.newBuilder()
                                                 .addSelectors("#iframe")
