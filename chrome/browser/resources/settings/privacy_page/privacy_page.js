fd7132557c91c285b283bd774ad93ed76d3ea6c6
sauski <sauski@google.com>
2020-03-05 22:49:03 +0000

    Change Settings static element map access to query selector
    CL 2088233 wrapped some elements on the privacy page into dom-if's such
    that they no longer appear in the static element map accessed with .$.
    
    Code that relied on these elements existing in this map thus failed.
    This CL changes these call sites to instead use the query selector $$
    call, which will successfully return the elements inside a dom-if.
    
    Bug: 1058960
    Change-Id: I2e0380a7e5f4251865f5ae809037401fccbc7446
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090456
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747434}

--- chrome/browser/resources/settings/privacy_page/privacy_page.js
+++ chrome/browser/resources/settings/privacy_page/privacy_page.js
@@ -447,7 +447,7 @@ cr.define('settings', function() {
 
     /** @private */
     onDoNotTrackDialogClosed_() {
-      cr.ui.focusWithoutInk(this.$.doNotTrack);
+      cr.ui.focusWithoutInk(assert(this.$$('#doNotTrack')));
     },
 
     /**
@@ -455,7 +455,7 @@ cr.define('settings', function() {
      * @private
      */
     onDoNotTrackDialogConfirm_() {
-      /** @type {!SettingsToggleButtonElement} */ (this.$.doNotTrack)
+      /** @type {!SettingsToggleButtonElement} */ (this.$$('#doNotTrack'))
           .sendPrefChange();
       this.closeDoNotTrackDialog_();
     },
@@ -466,7 +466,7 @@ cr.define('settings', function() {
      * @private
      */
     onDoNotTrackDialogCancel_() {
-      /** @type {!SettingsToggleButtonElement} */ (this.$.doNotTrack)
+      /** @type {!SettingsToggleButtonElement} */ (this.$$('#doNotTrack'))
           .resetToPrefValue();
       this.closeDoNotTrackDialog_();
     },
@@ -544,7 +544,7 @@ cr.define('settings', function() {
     onDialogClosed_() {
       settings.Router.getInstance().navigateTo(
           settings.routes.CLEAR_BROWSER_DATA.parent);
-      cr.ui.focusWithoutInk(assert(this.$.clearBrowsingData));
+      cr.ui.focusWithoutInk(assert(this.$$('#clearBrowsingData')));
     },
 
     /** @private */
