d3d74d1953a88adbcde6925e03f473e53487b53c
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2020-03-05 22:43:37 +0000

    [BrowserSwitcher] Launch IE asynchronously
    Previously, IE would be launched directly from the UI thread. This was
    not only causing jank, but also browser freezes when `DdeConnect()'
    would take a long time to complete (>1min in some cases).
    
    This CL makes `DdeConnect()' and `base::LaunchProcess()' run on a worker
    thread with blocking allowed, so the browser is still responsive in the
    meantime.
    
    Bug: 1054895
    Change-Id: I305063c12ebce67a9feffecfeb9f1d4c75211cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082994
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747424}

--- chrome/browser/browser_switcher/alternative_browser_driver.h
+++ chrome/browser/browser_switcher/alternative_browser_driver.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/callback.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
@@ -40,11 +41,13 @@ enum class BrowserType {
 // - Launching an external process
 class AlternativeBrowserDriver {
  public:
+  using LaunchCallback = base::OnceCallback<void(bool success)>;
+
   virtual ~AlternativeBrowserDriver();
 
   // Tries to launch |browser| at the specified URL, using whatever
   // method is most appropriate.
-  virtual bool TryLaunch(const GURL& url) = 0;
+  virtual void TryLaunch(const GURL& url, LaunchCallback cb) = 0;
 
   // Returns the localized string for the name of the alternative browser, if it
   // was auto-detected. If the name couldn't be auto-detected, returns an empty
@@ -63,8 +66,12 @@ class AlternativeBrowserDriverImpl : public AlternativeBrowserDriver {
   explicit AlternativeBrowserDriverImpl(const BrowserSwitcherPrefs* prefs);
   ~AlternativeBrowserDriverImpl() override;
 
+  AlternativeBrowserDriverImpl(const AlternativeBrowserDriverImpl&) = delete;
+  AlternativeBrowserDriverImpl& operator=(const AlternativeBrowserDriverImpl&) =
+      delete;
+
   // AlternativeBrowserDriver
-  bool TryLaunch(const GURL& url) override;
+  void TryLaunch(const GURL& url, LaunchCallback cb) override;
   std::string GetBrowserName() const override;
   BrowserType GetBrowserType() const override;
 
@@ -75,14 +82,7 @@ class AlternativeBrowserDriverImpl : public AlternativeBrowserDriver {
  private:
   using StringType = base::FilePath::StringType;
 
-#if defined(OS_WIN)
-  bool TryLaunchWithDde(const GURL& url);
-  bool TryLaunchWithExec(const GURL& url);
-#endif
-
   const BrowserSwitcherPrefs* const prefs_;
-
-  DISALLOW_COPY_AND_ASSIGN(AlternativeBrowserDriverImpl);
 };
 
 }  // namespace browser_switcher
