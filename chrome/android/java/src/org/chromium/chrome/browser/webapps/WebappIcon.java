9cca88ba1a978af701992a7af51b2157e181c3f6
Peter Kotwicz <pkotwicz@chromium.org>
2020-03-05 21:52:17 +0000

    [Android WebAPK] Fix WebAPKs with adaptive icons requesting updates daily
    This CL fixes bug where WebAPKs with adaptive icons would incorrectly
    think they need to be updated due to the value of
    WebApkInfo#isIconAdaptive() being incorrect. In particular, the
    WebApkInfo#create() call in WebappLauncherActivity produces a WebApkInfo
    where WebApkInfo#isIconAdaptive() == false if native is not yet loaded.
    
    This CL:
    - Only considers OS version when building WebApkInfo object
    - Modifies WebApkUpdateManager code to enable downgrading a WebAPK from
    an adaptive-primary-icon to a non-adaptive-primary-icon if the
    ChromeFeatureList.WEBAPK_ADAPTIVE_ICON feature is disabled.
    
    BUG=1058066
    TEST=WebApkInfoTest.*
    
    Change-Id: I6a00a3046bf1f82fb5fd50e171d35a49a7b6ecba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2085392
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747400}

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappIcon.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappIcon.java
@@ -10,6 +10,8 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.chrome.browser.ShortcutHelper;
@@ -48,6 +50,11 @@ public class WebappIcon {
             mBitmap = generateBitmap();
         }
         return mBitmap;
+    }
+
+    @VisibleForTesting
+    public int resourceIdForTesting() {
+        return mResourceId;
     }
 
     private Bitmap generateBitmap() {
