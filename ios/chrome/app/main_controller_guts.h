85e96cad5adbc49806c3fd8d8e7b4c17134e4683
Stepan Khapugin <stkhapugin@chromium.org>
2020-03-05 09:57:33 +0000

    [multiball] Move startUpChromeUI to SceneController
    Moves -startUpChromeUIPostCrash:needRestoration: method to
    SceneController.
    
    Bug: 1045658
    Change-Id: I3d3890b1480dfe1facfbfe58e01cc4bb8ffd7521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087614
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747178}

--- ios/chrome/app/main_controller_guts.h
+++ ios/chrome/app/main_controller_guts.h
@@ -10,10 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/ios/block_types.h"
 #include "components/browsing_data/core/browsing_data_utils.h"
+#import "ios/chrome/app/application_delegate/startup_information.h"
 #include "ios/chrome/app/startup/chrome_app_startup_parameters.h"
 #include "ios/chrome/browser/browsing_data/browsing_data_remove_mask.h"
 #import "ios/chrome/browser/crash_report/crash_restore_helper.h"
+#import "ios/chrome/browser/ui/commands/browsing_data_commands.h"
 
+@class AppState;
 @class BrowserViewController;
 class ChromeBrowserState;
 @class TabGridCoordinator;
@@ -22,7 +25,7 @@ class ChromeBrowserState;
 // TODO(crbug.com/1012697): Remove this protocol when SceneController is
 // operational. Move the private internals back into MainController, and pass
 // ownership of Scene-related objects to SceneController.
-@protocol MainControllerGuts
+@protocol MainControllerGuts <StartupInformation, BrowsingDataCommands>
 
 // If YES, the tab switcher is currently active.
 @property(nonatomic, assign, getter=isTabSwitcherActive)
@@ -47,6 +50,8 @@ class ChromeBrowserState;
 - (TabGridCoordinator*)mainCoordinator;
 - (id<BrowserInterfaceProvider>)interfaceProvider;
 - (UIWindow*)window;
+- (NSDictionary*)launchOptions;
+- (AppState*)appState;
 
 - (void)removeBrowsingDataForBrowserState:(ChromeBrowserState*)browserState
                                timePeriod:(browsing_data::TimePeriod)timePeriod
@@ -54,6 +59,9 @@ class ChromeBrowserState;
                           completionBlock:(ProceduralBlock)completionBlock;
 
 - (void)showFirstRunUI;
+- (void)scheduleShowPromo;
+// Returns whether or not the app can launch in incognito mode.
+- (BOOL)canLaunchInIncognito;
 
 @end
 
--- ios/chrome/browser/ui/main/scene_controller_guts.h
+++ ios/chrome/browser/ui/main/scene_controller_guts.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/app/application_delegate/startup_information.h"
 #import "ios/chrome/app/application_delegate/tab_opening.h"
-#include "ios/chrome/app/application_mode.h"
 #import "ios/chrome/browser/procedural_block_types.h"
 #import "ios/chrome/browser/ui/tab_grid/tab_switcher.h"
 #import "ios/chrome/browser/url_loading/app_url_loading_service.h"
@@ -32,7 +31,8 @@ class ChromeBrowserState;
 // browser state level UrlLoadingService instances.
 @property(nonatomic, assign) AppUrlLoadingService* appURLLoadingService;
 
-- (void)createInitialUI:(ApplicationMode)launchMode;
+- (void)startUpChromeUIPostCrash:(BOOL)isPostCrashLaunch
+                 needRestoration:(BOOL)needsRestoration;
 
 - (void)dismissModalDialogsWithCompletion:(ProceduralBlock)completion
                            dismissOmnibox:(BOOL)dismissOmnibox;
