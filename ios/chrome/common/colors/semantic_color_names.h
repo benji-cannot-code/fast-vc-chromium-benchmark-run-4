8662ffa14148973ea8827b6e6f6bd48d6ae3827c
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-03-05 16:41:09 +0000

    [iOS][Common] Delete compatibility header
    Bug: 1052505
    Change-Id: I1af4911a8d927c3530d1d4c70451c556f6e67dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089816
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747298}

--- ios/chrome/common/colors/semantic_color_names.h
+++
@@ -1,7 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// This file is temporal to not break internal.
-#import "ios/chrome/common/ui/colors/semantic_color_names.h"
