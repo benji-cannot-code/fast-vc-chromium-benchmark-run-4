7e414d1e312ae4eabd190aef658aace96a4ac73b
Bettina <bdea@chromium.org>
2020-03-05 23:33:00 +0000

    Set UserPopulation in RealTime Url Service.
    Bug: 1017499
    Fixes: 1017499
    Change-Id: I0b4f6da09b8cb5804fe85e7402f3e1ea83193497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2088502
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747472}

--- components/safe_browsing/core/realtime/url_lookup_service_unittest.cc
+++ components/safe_browsing/core/realtime/url_lookup_service_unittest.cc
@@ -23,6 +23,7 @@ class RealTimeUrlLookupServiceTest : public PlatformTest {
  public:
   void SetUp() override {
     HostContentSettingsMap::RegisterProfilePrefs(test_pref_service_.registry());
+    safe_browsing::RegisterProfilePrefs(test_pref_service_.registry());
     task_environment_ = CreateTestTaskEnvironment(
         base::test::TaskEnvironment::TimeSource::MOCK_TIME);
     PlatformTest::SetUp();
@@ -105,6 +106,8 @@ TEST_F(RealTimeUrlLookupServiceTest, TestFillRequestProto) {
     auto result = FillRequestProto(url);
     EXPECT_EQ(sanitize_url_cases[i].expected_url, result->url());
     EXPECT_EQ(RTLookupRequest::NAVIGATION, result->lookup_type());
+    EXPECT_EQ(ChromeUserPopulation::SAFE_BROWSING,
+              result->population().user_population());
   }
 }
 
