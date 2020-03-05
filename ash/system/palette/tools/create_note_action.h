87fc51ad64a450f88297f180e05a150e569b5a3e
Sean O'Brien <seobrien@google.com>
2020-03-05 23:35:52 +0000

    Allow launching note app with key event
    Add a key event handler to the create note action, looking for events
    with a special flag showing that they were generated by a stylus
    bluetooth button.
    
    This will allow users to open a new note by double clicking the tail
    stylus button
    
    Bug: b:139781900
    Change-Id: Ie77e12b4b5256922f3653bacb425a15fdafcf8b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079836
    Commit-Queue: Sean O'Brien <seobrien@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747473}

--- ash/system/palette/tools/create_note_action.h
+++ ash/system/palette/tools/create_note_action.h
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/system/palette/common_palette_tool.h"
 #include "base/macros.h"
+#include "ui/events/event_handler.h"
 
 namespace ash {
 
 // A button in the ash palette that launches the selected note-taking app when
 // clicked. This action dynamically hides itself if it is not available.
-class ASH_EXPORT CreateNoteAction : public CommonPaletteTool {
+class ASH_EXPORT CreateNoteAction : public CommonPaletteTool,
+                                    public ui::EventHandler {
  public:
   explicit CreateNoteAction(Delegate* delegate);
   ~CreateNoteAction() override;
@@ -28,6 +30,9 @@ class ASH_EXPORT CreateNoteAction : public CommonPaletteTool {
 
   // CommonPaletteTool overrides.
   const gfx::VectorIcon& GetPaletteIcon() const override;
+
+  // ui::EventHandler overrides.
+  void OnKeyEvent(ui::KeyEvent* event) override;
 
   DISALLOW_COPY_AND_ASSIGN(CreateNoteAction);
 };
