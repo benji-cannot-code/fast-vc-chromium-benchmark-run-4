ef1f865e5a5da0d9625f6f7cba175be4e4bc6861
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-03-05 01:02:02 +0000

    [Passwords] Implement passwordsPrivate.removeCompromisedCredential
    This change implements the passwordsPrivate.removeCompromisedCredential
    extension API. This allows callers to remove a specific compromised
    credential from the password store. The caller is informed whether the
    remove operation succeeded via a callback.
    
    Bug: 1047726
    Change-Id: I9b39fa2350d6d87d551bde06e2baab9ec82bdb3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082384
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747033}

--- chrome/browser/extensions/api/passwords_private/passwords_private_apitest.cc
+++ chrome/browser/extensions/api/passwords_private/passwords_private_apitest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/safe_conversions.h"
 #include "base/observer_list.h"
 #include "base/optional.h"
+#include "base/stl_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_piece_forward.h"
 #include "base/strings/utf_string_conversions.h"
@@ -219,6 +220,17 @@ class TestDelegate : public PasswordsPrivateDelegate {
                        [&credential](const auto& compromised_credential) {
                          return compromised_credential.id == credential.id;
                        });
+  }
+
+  // Fake implementation of RemoveCompromisedCredential. This succeeds if the
+  // delegate knows of a compromised credential with the same id.
+  bool RemoveCompromisedCredential(
+      const api::passwords_private::CompromisedCredential& credential)
+      override {
+    return base::EraseIf(compromised_credentials_,
+                         [&credential](const auto& compromised_credential) {
+                           return compromised_credential.id == credential.id;
+                         }) != 0;
   }
 
   void SetOptedInForAccountStorage(bool opted_in) {
@@ -440,6 +452,19 @@ IN_PROC_BROWSER_TEST_F(PasswordsPrivateApiTest,
                        ChangeCompromisedCredentialSucceeds) {
   AddCompromisedCredential(0);
   EXPECT_TRUE(RunPasswordsSubtest("changeCompromisedCredentialSucceeds"))
+      << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(PasswordsPrivateApiTest,
+                       RemoveCompromisedCredentialFails) {
+  EXPECT_TRUE(RunPasswordsSubtest("removeCompromisedCredentialFails"))
+      << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(PasswordsPrivateApiTest,
+                       RemoveCompromisedCredentialSucceeds) {
+  AddCompromisedCredential(0);
+  EXPECT_TRUE(RunPasswordsSubtest("removeCompromisedCredentialSucceeds"))
       << message_;
 }
 
