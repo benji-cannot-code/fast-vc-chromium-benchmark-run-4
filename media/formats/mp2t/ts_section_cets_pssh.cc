1f6b5dc2aabe194a74f1ccdc2fd836d2224f6da6
Jose Lopes <jabolopes@google.com>
2020-03-05 11:29:30 +0000

    media: Migrate RegisterPsshBoxesCb to repeating callback.
    This is a repeating callback because it's called from a call chain that
    links to a loop:
    * https://cs.chromium.org/chromium/src/media/formats/mp2t/mp2t_stream_parser.cc?rcl=385d1322d09f871a327371f0ded2b2d70714bde2&l=873
    * https://cs.chromium.org/chromium/src/media/formats/mp2t/mp2t_stream_parser.cc?rcl=385d1322d09f871a327371f0ded2b2d70714bde2&l=139
    * https://cs.chromium.org/chromium/src/media/formats/mp2t/mp2t_stream_parser.cc?rcl=385d1322d09f871a327371f0ded2b2d70714bde2&l=345
    
    This is part of the base::Callback migration.
    
    Context: https://cs.chromium.org/chromium/src/docs/callback.md?rcl=9fcc3764aea8f97e9f6de4a9ee61d554e67edcda&l=40
    
    Bug: 1007806
    Change-Id: I51a2e7a5b0fe87b114cbedabe2040961d3bb6215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080417
    Commit-Queue: Jose Lopes <jabolopes@google.com>
    Reviewed-by: Albert J. Wong <ajwong@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747205}

--- media/formats/mp2t/ts_section_cets_pssh.cc
+++ media/formats/mp2t/ts_section_cets_pssh.cc
@@ -12,9 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 namespace mp2t {
 
-TsSectionCetsPssh::TsSectionCetsPssh(
-    const RegisterPsshBoxesCb& register_pssh_boxes_cb)
-    : register_pssh_boxes_cb_(register_pssh_boxes_cb) {}
+TsSectionCetsPssh::TsSectionCetsPssh(RegisterPsshBoxesCb register_pssh_boxes_cb)
+    : register_pssh_boxes_cb_(std::move(register_pssh_boxes_cb)) {}
 
 TsSectionCetsPssh::~TsSectionCetsPssh() {}
 
