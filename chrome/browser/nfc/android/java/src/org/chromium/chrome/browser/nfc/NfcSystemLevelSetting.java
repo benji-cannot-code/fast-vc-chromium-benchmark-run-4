ac5adc19a266ce30a701bd5a408c2ebb1f5c9ecc
Francois Beaufort <beaufort.francois@gmail.com>
2020-03-05 10:20:44 +0000

    [WebNFC] Warn user when NFC is not supported
    This CL makes sure a message is shown to the user when NFC is not
    supported on the device while Web NFC access has been granted to a
    website.
    Messages show up in page info popup and site settings UI.
    
    Screenshots: https://bugs.chromium.org/p/chromium/issues/detail?id=1058217#c2
    
    Bug: 1035048, 1055216, 1058217
    Change-Id: I4687a353feedd92e233ae1741738aef9bb774ad0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2077685
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747186}

--- chrome/browser/nfc/android/java/src/org/chromium/chrome/browser/nfc/NfcSystemLevelSetting.java
+++ chrome/browser/nfc/android/java/src/org/chromium/chrome/browser/nfc/NfcSystemLevelSetting.java
@@ -29,10 +29,15 @@ import org.chromium.ui.base.WindowAndroid;
  * This class should be used only on the UI thread.
  */
 public class NfcSystemLevelSetting {
+    private static Boolean sNfcSupportForTesting;
     private static Boolean sSystemNfcSettingForTesting;
 
     @CalledByNative
-    private static boolean isNfcAccessPossible() {
+    public static boolean isNfcAccessPossible() {
+        if (sNfcSupportForTesting != null) {
+            return sNfcSupportForTesting;
+        }
+
         Context context = ContextUtils.getApplicationContext();
         int permission =
                 context.checkPermission(Manifest.permission.NFC, Process.myPid(), Process.myUid());
@@ -89,6 +94,12 @@ public class NfcSystemLevelSetting {
     @VisibleForTesting
     public static void setNfcSettingForTesting(Boolean enabled) {
         sSystemNfcSettingForTesting = enabled;
+    }
+
+    /** Disable/enable Android NFC support for testing use only. */
+    @VisibleForTesting
+    public static void setNfcSupportForTesting(Boolean enabled) {
+        sNfcSupportForTesting = enabled;
     }
 
     @NativeMethods
