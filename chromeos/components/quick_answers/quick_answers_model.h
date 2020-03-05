88a03d880b22898201e380025f6051336f04d9d7
Li Lin <llin@chromium.org>
2020-03-05 22:47:58 +0000

    Update the default style of result text.
    Also added rich UI element for KpEntityResultParser.
    
    Bug: b/150783279
    Test: unit tests
    Change-Id: Ia40b848f9e9caf8b246627d415b4b41fe188b28b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2088212
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747433}

--- chromeos/components/quick_answers/quick_answers_model.h
+++ chromeos/components/quick_answers/quick_answers_model.h
@@ -69,7 +69,7 @@ class QuickAnswerUiElement {
 // class to describe an answer text.
 class QuickAnswerText : public QuickAnswerUiElement {
  public:
-  QuickAnswerText(const std::string& text, SkColor color = SK_ColorBLACK)
+  QuickAnswerText(const std::string& text, SkColor color = gfx::kGoogleGrey900)
       : QuickAnswerUiElement(QuickAnswerUiElementType::kText),
         text_(text),
         color_(color) {}
@@ -78,6 +78,13 @@ class QuickAnswerText : public QuickAnswerUiElement {
 
   // Attributes for text style.
   SkColor color_ = SK_ColorBLACK;
+};
+
+class QuickAnswerResultText : public QuickAnswerText {
+ public:
+  QuickAnswerResultText(const std::string& text,
+                        SkColor color = gfx::kGoogleGrey700)
+      : QuickAnswerText(text, color) {}
 };
 
 class QuickAnswerImage : public QuickAnswerUiElement {
