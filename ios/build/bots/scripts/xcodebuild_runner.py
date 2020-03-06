58c8f2dfa8c02ee325dd986550817621806f2b9f
Lindsay Pasricha <lindsayw@google.com>
2020-03-06 23:41:00 +0000

    Revert "[iOS][code coverage]Move raw coverage data to isolated output in test runner scripts."
    This reverts commit dfe672641edf6212ddd5ac6cd9df22067a346396.
    
    Reason for revert: This breaks on device runs for xcodebuild_runner.
    This revert will land after the revert of crrev/c/2076765.
    
    Original change's description:
    > [iOS][code coverage]Move raw coverage data to isolated output in test runner scripts.
    >
    > - Added function of moving raw coverage data in a new utility file and
    >   corresponding tests.
    > - Added "use_clang_coverage" arg to run.py and all simulator test
    >   runner classes.
    > - Added check of the arg and call to the function in TestRunners after
    >   running unit tests and EG tests.
    >
    > Bug: 943883
    > Change-Id: I5f16c152a38f7a8a3d988aac9e73964d890219b8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2048851
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#744470}
    
    Bug: 943883,1058257
    Change-Id: I4ae784f831c60bf621c8135af490bdeffa3c004e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089404
    Commit-Queue: Lindsay Pasricha <lindsayw@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747930}

--- ios/build/bots/scripts/xcodebuild_runner.py
+++ ios/build/bots/scripts/xcodebuild_runner.py
@@ -13,7 +13,6 @@ import os
 import subprocess
 import time
 
-import coverage_util
 import iossim_util
 import test_apps
 import test_runner
@@ -113,7 +112,6 @@ class LaunchCommand(object):
                shards,
                retries,
                out_dir=os.path.basename(os.getcwd()),
-               use_clang_coverage=False,
                env=None):
     """Initialize launch command.
 
@@ -139,7 +137,6 @@ class LaunchCommand(object):
     self.out_dir = out_dir
     self.logs = collections.OrderedDict()
     self.test_results = collections.OrderedDict()
-    self.use_clang_coverage = use_clang_coverage
     self.env = env
     if distutils.version.LooseVersion('11.0') <= distutils.version.LooseVersion(
         test_runner.get_current_xcode_info()['version']):
@@ -211,13 +208,6 @@ class LaunchCommand(object):
       LOGGER.info('Start test attempt #%d for command [%s]' % (
           attempt, ' '.join(cmd_list)))
       output = self.launch_attempt(cmd_list)
-
-      if self.use_clang_coverage:
-        # out_dir of LaunchCommand object is the TestRunner out_dir joined with
-        # UDID. Use os.path.dirname to retrieve the TestRunner out_dir.
-        coverage_util.move_raw_coverage_data(self.udid,
-                                             os.path.dirname(self.out_dir))
-
       self.test_results['attempts'].append(
           self._log_parser.collect_test_results(outdir_attempt, output))
       if self.retries == attempt or not self.test_results[
@@ -269,7 +259,6 @@ class SimulatorParallelTestRunner(test_runner.SimulatorTestRunner):
                shards=1,
                test_cases=None,
                test_args=None,
-               use_clang_coverage=False,
                env_vars=None):
     """Initializes a new instance of SimulatorParallelTestRunner class.
 
@@ -287,7 +276,6 @@ class SimulatorParallelTestRunner(test_runner.SimulatorTestRunner):
                   None or [] to include all tests.
       test_args: List of strings to pass as arguments to the test when
         launching.
-      use_clang_coverage: Whether code coverage is enabled in this run.
       env_vars: List of environment variables to pass to the test itself.
 
     Raises:
@@ -307,7 +295,6 @@ class SimulatorParallelTestRunner(test_runner.SimulatorTestRunner):
         shards=shards or 1,
         test_args=test_args,
         test_cases=test_cases,
-        use_clang_coverage=use_clang_coverage,
         xctest=False)
     self.set_up()
     self.host_app_path = None
@@ -316,10 +303,6 @@ class SimulatorParallelTestRunner(test_runner.SimulatorTestRunner):
     self._init_sharding_data()
     self.logs = collections.OrderedDict()
     self.test_results['path_delimiter'] = '/'
-    # Do not enable parallel testing when code coverage is enabled, because raw
-    # coverage data won't be produced with parallel testing.
-    if self.use_clang_coverage:
-      self.shards = 1
 
   def _init_sharding_data(self):
     """Initialize sharding data.
@@ -368,7 +351,6 @@ class SimulatorParallelTestRunner(test_runner.SimulatorTestRunner):
               shards=params['shards'],
               retries=self.retries,
               out_dir=os.path.join(self.out_dir, params['udid']),
-              use_clang_coverage=self.use_clang_coverage,
               env=self.get_launch_env()))
 
     thread_pool = pool.ThreadPool(len(launch_commands))
