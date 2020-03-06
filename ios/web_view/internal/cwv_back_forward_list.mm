564adb6103778ca7a510642ff29237834f2274b7
Jiajun Ou <garzonou@google.com>
2020-03-06 06:07:21 +0000

    Implement NSFastEnumeration for BackForwardList API
    Bug: 1045254
    Change-Id: I797f1107a84bd235d0ffd7f69c7d46f11ee1f79a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087232
    Commit-Queue: Jiajun Ou <garzonou@google.com>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747589}

--- ios/web_view/internal/cwv_back_forward_list.mm
+++ ios/web_view/internal/cwv_back_forward_list.mm
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation CWVBackForwardListItemArray {
   BOOL _isBackList;
+
+  CWVBackForwardListItem* _fastEnumerationItemCache;
 }
 
 - (instancetype)initWithBackForwardList:(CWVBackForwardList*)list
@@ -24,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self) {
     _list = list;
     _isBackList = isBackList;
+
+    _fastEnumerationItemCache = nil;
   }
   return self;
 }
@@ -68,6 +72,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       self.list.navigationManager->GetItemAtIndex(internalIndex);
   DCHECK(item);
   return [[CWVBackForwardListItem alloc] initWithNavigationItem:item];
+}
+
+#pragma mark - NSFastEnumeration
+
+- (NSUInteger)countByEnumeratingWithState:(NSFastEnumerationState*)state
+                                  objects:(id __unsafe_unretained[])stackbuf
+                                    count:(NSUInteger)len {
+  DCHECK_GE(self.count, state->state);
+  DCHECK_GE(len, 1UL);
+  if (self.count == state->state) {
+    return 0;
+  }
+
+  // |state| is a pure C-struct so retaining an item in |state| is impossible.
+  // So, |_fastEnumerationItemCache| is designed to retain the returned item.
+  // It is the caller's responsibility to ensure this is not deallocated during
+  // enumeration, so a caller should not use nested for-in statement on the same
+  // list, otherwise it will cause a use-after-free bug.
+  _fastEnumerationItemCache = [self objectAtIndexedSubscript:state->state];
+
+  // This tells Obj-C runtime that |self| is immutable (because
+  // |&state->extra[0]| is constant) during enumeration.
+  state->mutationsPtr = &state->extra[0];
+
+  state->itemsPtr = stackbuf;
+  state->itemsPtr[0] = _fastEnumerationItemCache;
+  ++state->state;
+  return 1;
 }
 
 @end
