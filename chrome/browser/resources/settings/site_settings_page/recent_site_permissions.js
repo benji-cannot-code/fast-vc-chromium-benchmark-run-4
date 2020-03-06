535a68857ae2f2d9d1a1a97c82b56dbe22e69fb2
sauski <sauski@google.com>
2020-03-06 01:26:47 +0000

    Move route observer behavior into Recent Site Permissions element
    Previously the Recent Site Permissions display list update needed to be
    manually triggered (probably by it's parent). This CL extends the element
    to be a route observer so that it can re-trigger updates when appropriate.
    
    Updates are triggered when the route matching that set on the route-path
    attribute of the element is navigated to.
    
    Bug: 1032584
    Change-Id: Id29191b6586c138e1e1191aaab218c5f7ff92a2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2088643
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#747512}

--- chrome/browser/resources/settings/site_settings_page/recent_site_permissions.js
+++ chrome/browser/resources/settings/site_settings_page/recent_site_permissions.js
@@ -7,6 +7,7 @@ Polymer({
   is: 'settings-recent-site-permissions',
 
   behaviors: [
+    settings.RouteObserverBehavior,
     SiteSettingsBehavior,
     WebUIListenerBehavior,
     I18nBehavior,
@@ -29,6 +30,18 @@ Polymer({
       type: Array,
       value: () => [],
     },
+  },
+
+  /**
+   * Reload the site recent site permission list whenever the user navigates
+   * to the site settings page.
+   * @param {!settings.Route} currentRoute
+   * @protected
+   */
+  currentRouteChanged(currentRoute) {
+    if (currentRoute.path == settings.routes.SITE_SETTINGS.path) {
+      this.populateList_();
+    }
   },
 
   /** @override */
@@ -215,7 +228,7 @@ Polymer({
     // and we are currently displaying an incognito entry.
     if (hasIncognito === false &&
         this.recentSitePermissionsList_.some(p => p.incognito)) {
-      this.populateList();
+      this.populateList_();
     }
   },
 
@@ -263,7 +276,7 @@ Polymer({
    * the update of the display list.
    * @private
    */
-  async populateList() {
+  async populateList_() {
     this.recentSitePermissionsList_ =
         await this.browserProxy.getRecentSitePermissions(
             this.getCategoryList(), 3);
