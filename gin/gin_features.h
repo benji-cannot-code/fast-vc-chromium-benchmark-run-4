093db367485d9d19e8d85127309b4fee8d6d3e38
Ulan Degenbaev <ulan@chromium.org>
2020-03-06 12:30:38 +0000

    gin: Add a flag for a Finch experiment V8PerContextMarkingWorklist
    The new feature flag passes --stress-per-context-marking-worklist flag
    to V8, which instructs the GC to use per-context marking worklist.
    
    Bug: 1038180
    Change-Id: Ia7255e0ca70129cbaec7b568667a319cec8a743f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089865
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Commit-Queue: Ulan Degenbaev <ulan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747656}

--- gin/gin_features.h
+++ gin/gin_features.h
@@ -15,6 +15,7 @@ GIN_EXPORT extern const base::Feature kV8OptimizeJavascript;
 GIN_EXPORT extern const base::Feature kV8FlushBytecode;
 GIN_EXPORT extern const base::Feature kV8LazyFeedbackAllocation;
 GIN_EXPORT extern const base::Feature kV8ConcurrentInlining;
+GIN_EXPORT extern const base::Feature kV8PerContextMarkingWorklist;
 
 }  // namespace features
 
