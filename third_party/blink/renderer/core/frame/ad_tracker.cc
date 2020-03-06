95e8377c6d0c39b20289add8ad717c3bf6c487cf
Josh Karlin <jkarlin@chromium.org>
2020-03-06 22:39:04 +0000

    Speed up IsAdScriptInStack by passing ExecutionContext when known
    What: Some callers to IsAdScriptInStack know the ExecutionContext that
    triggered the event. Those callers should pass the context in to
    IsAdScriptInStack as there is a small cost to looking it up.
    
    Why: Some callers (such as core_probes) call frequently and looking up
    the ExecutionContext when it's already known only slows things down.
    
    Bug: 851531
    Change-Id: Id8eadfc9855cdda0d18df34a4829dd47e31ac64f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090232
    Commit-Queue: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747899}

--- third_party/blink/renderer/core/frame/ad_tracker.cc
+++ third_party/blink/renderer/core/frame/ad_tracker.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 #include "third_party/blink/renderer/core/core_probe_sink.h"
 #include "third_party/blink/renderer/core/dom/document.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
@@ -157,7 +158,7 @@ bool AdTracker::CalculateIfAdSubresource(ExecutionContext* execution_context,
   // Check if the document loading the resource is an ad or if any executing
   // script is an ad.
   known_ad = known_ad || IsKnownAdExecutionContext(execution_context) ||
-             IsAdScriptInStack();
+             IsAdScriptInStackForContext(execution_context);
 
   // If it is a script marked as an ad and it's not in an ad context, append it
   // to the known ad script set. We don't need to keep track of ad scripts in ad
@@ -171,12 +172,13 @@ bool AdTracker::CalculateIfAdSubresource(ExecutionContext* execution_context,
   return known_ad;
 }
 
-void AdTracker::DidCreateAsyncTask(probe::AsyncTaskId* task) {
+void AdTracker::DidCreateAsyncTask(probe::AsyncTaskId* task,
+                                   ExecutionContext* execution_context) {
   DCHECK(task);
   if (!async_stack_enabled_)
     return;
 
-  if (IsAdScriptInStack())
+  if (IsAdScriptInStackForContext(execution_context))
     task->SetAdTask();
 }
 
@@ -198,11 +200,11 @@ void AdTracker::DidFinishAsyncTask(probe::AsyncTaskId* task) {
     running_ad_async_tasks_ -= 1;
 }
 
-bool AdTracker::IsAdScriptInStack() {
+bool AdTracker::IsAdScriptInStackForContext(
+    ExecutionContext* execution_context) {
   if (num_ads_in_stack_ > 0 || running_ad_async_tasks_ > 0)
     return true;
 
-  ExecutionContext* execution_context = GetCurrentExecutionContext();
   if (!execution_context)
     return false;
 
@@ -220,6 +222,10 @@ bool AdTracker::IsAdScriptInStack() {
     return true;
 
   return false;
+}
+
+bool AdTracker::IsAdScriptInStackSlow() {
+  return IsAdScriptInStackForContext(GetCurrentExecutionContext());
 }
 
 bool AdTracker::IsKnownAdScript(ExecutionContext* execution_context,
