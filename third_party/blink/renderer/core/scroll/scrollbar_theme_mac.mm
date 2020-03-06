0afa4605260441de5b0efdfd951939027bbd43ec
Rahul Arakeri <arakeri@microsoft.com>
2020-03-06 20:49:07 +0000

    Refactor ThumbOpacity to Opacity.
    This is a follow up CL for crrev.com/c/2068204
    
    Bug: 1055246
    Change-Id: I4b8427f3a5a158e44cef8cb42f8ac77780c77db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2081529
    Commit-Queue: Rahul Arakeri <arakeri@microsoft.com>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747845}

--- third_party/blink/renderer/core/scroll/scrollbar_theme_mac.mm
+++ third_party/blink/renderer/core/scroll/scrollbar_theme_mac.mm
@@ -456,7 +456,7 @@ void ScrollbarThemeMac::UpdateEnabledState(const Scrollbar& scrollbar) {
   [PainterForScrollbar(scrollbar) setEnabled:scrollbar.Enabled()];
 }
 
-float ScrollbarThemeMac::ThumbOpacity(const Scrollbar& scrollbar) const {
+float ScrollbarThemeMac::Opacity(const Scrollbar& scrollbar) const {
   ScrollbarPainter scrollbar_painter = PainterForScrollbar(scrollbar);
   return [scrollbar_painter knobAlpha];
 }
