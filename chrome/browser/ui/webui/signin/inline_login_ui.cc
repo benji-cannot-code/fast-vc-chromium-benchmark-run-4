b8e6f621046c13ed4bcae446c02b52f113f7b048
Anastasiia Nikolaienko <anastasiian@chromium.org>
2020-03-06 12:06:41 +0000

    Add EDU login flow: JS tests
    Add JS tests for edu-login-button component
    
    Bug: 1043108
    Change-Id: Id2b17b91afa5f0810b92ea7ab1d827c5daedbeca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087218
    Commit-Queue: Anastasiia Nikolaienko <anastasiian@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747652}

--- chrome/browser/ui/webui/signin/inline_login_ui.cc
+++ chrome/browser/ui/webui/signin/inline_login_ui.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/ui/webui/chromeos/edu_account_login_handler_chromeos.h"
 #include "chrome/browser/ui/webui/signin/inline_login_handler_chromeos.h"
+#include "ui/resources/grit/webui_resources.h"
 #include "ui/strings/grit/ui_strings.h"
 #else
 #include "chrome/browser/ui/webui/signin/inline_login_handler_impl.h"
@@ -66,6 +67,9 @@ content::WebUIDataSource* CreateWebUIDataSource() {
   source->AddResourcePath("gaia_auth_host.js", IDR_GAIA_AUTH_AUTHENTICATOR_JS);
 
 #if defined(OS_CHROMEOS)
+  source->OverrideContentSecurityPolicyScriptSrc(
+      "script-src chrome://resources chrome://test 'self';");
+
   source->AddResourcePath("edu", IDU_EDU_LOGIN_EDU_LOGIN_HTML);
   source->AddResourcePath("app.js", IDU_EDU_LOGIN_EDU_LOGIN_JS);
   source->AddResourcePath("edu_login_button.js",
@@ -75,6 +79,9 @@ content::WebUIDataSource* CreateWebUIDataSource() {
   source->AddResourcePath("edu_login_css.js", IDR_EDU_LOGIN_EDU_LOGIN_CSS_JS);
   source->AddResourcePath("browser_proxy.js", IDR_EDU_LOGIN_BROWSER_PROXY_JS);
   source->AddResourcePath("edu_login_util.js", IDR_EDU_LOGIN_EDU_LOGIN_UTIL_JS);
+
+  source->AddResourcePath("test_loader.js", IDR_WEBUI_JS_TEST_LOADER);
+  source->AddResourcePath("test_loader.html", IDR_WEBUI_HTML_TEST_LOADER);
 
   AddEduStrings(source);
 #endif
