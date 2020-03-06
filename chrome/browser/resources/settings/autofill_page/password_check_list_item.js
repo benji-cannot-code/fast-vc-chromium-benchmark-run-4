ab659945759856b97e7b2ae070e3171b17b92e7b
Viktor Semeniuk <vsemeniuk@google.com>
2020-03-06 16:42:00 +0000

    [Passwords] Minor fixes to Compromised Credentials List
    Comments after merge from:
    https://chromium-review.googlesource.com/c/chromium/src/+/2078257
    are applied here.
    
    Bug: 1047726
    Change-Id: I130f410e1fdfbf8b44c630279efff0e9618b1fad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089851
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747739}

--- chrome/browser/resources/settings/autofill_page/password_check_list_item.js
+++ chrome/browser/resources/settings/autofill_page/password_check_list_item.js
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'password-check-list-item',
 
-  behaviors: [I18nBehavior],
-
   properties: {
     password_: {
       type: String,
