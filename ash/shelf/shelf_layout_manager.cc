5c907e06bb1825c531046c067fb62f71c96603b7
Toni Barzic <tbarzic@chromium.org>
2020-03-06 03:52:26 +0000

    Update shelf state when shelf config changes
    Shelf config change may cause hotseat state change, so we should update
    the hotseat state (which is done in ShelfLayoutManager::SetState) when
    the shelf config changes, otherwise, the hotseat state change may lag
    behind the shelf layout change.
    
    For example, layout may hide the opaque shelf background if config
    changes to non-in-app shelf, and animating background animation starts
    when the hotseat state changes. So, if the hotseat state change lags
    after the shelf layout change for long enough, we might see a flash of
    animating shelf background after the opaque shelf background has been
    hidden.
    
    This was noticed while implementing swipe to overview/home with VK
    shown (when swiping to go home, the animating shelf would occasionally
    pop up with a delay) - added a test for sequence of events in that case.
    
    (Note that this means that the hotseat state may get calculated before
    app list visibility on tablet mode change, so add back logic that moved
    hotseat in kShownHomeLauncher state if the home should be visible at
    that point in time).
    
    BUG=b:145704293
    
    Change-Id: I4e6ce883eed8674159b03c25336fd82c9d20c50d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090757
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747552}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -1137,6 +1137,7 @@ float ShelfLayoutManager::GetOpacity() const {
 }
 
 void ShelfLayoutManager::OnShelfConfigUpdated() {
+  SetState(state_.visibility_state);
   LayoutShelf(/*animate=*/true);
   MaybeUpdateShelfBackground(AnimationChangeType::IMMEDIATE);
   UpdateContextualNudges();
@@ -1297,7 +1298,9 @@ HotseatState ShelfLayoutManager::CalculateHotseatState(
       ((overview_controller && overview_controller->InOverviewSession()) ||
        overview_mode_will_start_) &&
       !overview_controller->IsCompletingShutdownAnimations();
-  const bool app_list_visible = app_list_controller->GetTargetVisibility();
+  const bool app_list_visible =
+      app_list_controller->GetTargetVisibility() ||
+      app_list_controller->ShouldHomeLauncherBeVisible();
 
   // TODO(https://crbug.com/1058205): Test this behavior.
   if (ShelfConfig::Get()->is_virtual_keyboard_shown())
