c896a25a36020b2cb24eb8e0ee15a862fb0f1399
ckitagawa <ckitagawa@chromium.org>
2020-03-06 21:14:54 +0000

    [Paint Preview] Implement TabService
    This CL introduces a TabService which provides an interface by which to
    capture Paint Previews and hooks to call on tab closure and startup. It
    is largely a wrapper around PaintPreviewBaseService, but handles file
    management for per-tab storage.
    
    Follow-up work is
    - Add a factory for this service.
    - Hook this service up to observers for the events it handles.
    
    Bug: 1058093
    Change-Id: Ic1d7e880052090b5150ee5e63e21def374a20f92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087994
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747864}

--- components/paint_preview/browser/file_manager.h
+++ components/paint_preview/browser/file_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PAINT_PREVIEW_BROWSER_FILE_MANAGER_H_
 #define COMPONENTS_PAINT_PREVIEW_BROWSER_FILE_MANAGER_H_
 
+#include "base/containers/flat_set.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/memory/ref_counted.h"
@@ -63,8 +64,8 @@ class FileManager : public base::RefCountedThreadSafe<FileManager> {
   bool CompressDirectory(const DirectoryKey& key) const;
 
   // Deletes artifacts associated with |key| or |keys|.
-  void DeleteArtifacts(const DirectoryKey& key) const;
-  void DeleteArtifacts(const std::vector<DirectoryKey>& keys) const;
+  void DeleteArtifactSet(const DirectoryKey& key) const;
+  void DeleteArtifactSets(const std::vector<DirectoryKey>& keys) const;
 
   // Deletes all stored paint previews stored in the |root_directory_|.
   void DeleteAll() const;
@@ -79,6 +80,9 @@ class FileManager : public base::RefCountedThreadSafe<FileManager> {
   // found or the proto cannot be parsed.
   std::unique_ptr<PaintPreviewProto> DeserializePaintPreviewProto(
       const DirectoryKey& key) const;
+
+  // Lists the current set of in-use DirectoryKeys.
+  base::flat_set<DirectoryKey> ListUsedKeys() const;
 
  private:
   friend class base::RefCountedThreadSafe<FileManager>;
