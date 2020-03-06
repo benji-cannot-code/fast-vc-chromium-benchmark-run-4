337ead171abec789eed6cd4ef453cfdf0d783098
Mathias Bynens <mathias@chromium.org>
2020-03-06 17:18:01 +0000

    Reland "[DevTools] Support vision deficiency emulation in CDP"
    This reverts commit e4048c2ea939c608dcdbcda11117904a25620191, and
    applies the fix from
    https://chromium-review.googlesource.com/c/chromium/src/+/2087672.
    
    Original change's description:
    > [DevTools] Support vision deficiency emulation in CDP
    >
    > This patch makes it possible to emulate various vision deficiencies,
    > including color vision deficiencies and blurry vision, in the Blink
    > Renderer. The various deficiencies can be triggered by the new
    > Emulation#setEmulatedVisionDeficiency command in the Chrome DevTools
    > Protocol.
    >
    > Design doc: https://goo.gle/devtools-cvd
    >
    > Bug: chromium:1003700
    > Change-Id: Ie86d14b215b93aa219b91b0e7877898d2d47bc5b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2063136
    > Commit-Queue: Mathias Bynens <mathias@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#746715}
    
    TBR=caseq@chromium.org,vogelheim@chromium.org,mathias@chromium.org,futhark@chromium.org
    
    Bug: chromium:1003700
    Change-Id: I3f3e91dd72e7fb479eb7d73ba8452fc876e20b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087347
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747749}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1707,6 +1707,9 @@ class CORE_EXPORT Document : public ContainerNode,
 
   void ColorSchemeChanged();
 
+  // A new vision deficiency is being emulated through DevTools.
+  void VisionDeficiencyChanged();
+
   void ClearIsolatedWorldCSPForTesting(int32_t world_id);
 
   // A META element with name=color-scheme was added, removed, or modified.
