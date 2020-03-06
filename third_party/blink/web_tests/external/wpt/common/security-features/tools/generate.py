f5e1c7fc2dcca45c08b3c261c393c69840d65499
Hiroshige Hayashizaki <hiroshige@chromium.org>
2020-03-06 05:34:06 +0000

    [WPT/common/security-features] Add default spec.src.json
    There were multiple `spec.src.json` for each project directories
    that have many things in common, e.g. lists of subresources,
    source contexts, etc.
    
    This CL introduces `common/security-features/tools/spec.src.json`
    that works as the default `spec.src.json`, and moves
    common things into the default `spec.src.json`.
    By doing this, we can ensure uniform test coverage across projects
    (e.g. the same set of subresource requests are covered in
    `referrer-policy` and `mixed-content` tests) more easily.
    
    To keep the generated tests the same, this CL adds
    exclusion rules named "... not yet tested".
    
    This CL doesn't affect test behavior nor generated results.
    
    Bug: 906850
    Change-Id: Id06bc6e311a482f50531cd36e6da57b2e6df22ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2049953
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747577}

--- third_party/blink/web_tests/external/wpt/common/security-features/tools/generate.py
+++ third_party/blink/web_tests/external/wpt/common/security-features/tools/generate.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from __future__ import print_function
 
 import argparse
+import collections
 import copy
 import json
 import os
@@ -360,8 +361,14 @@ def main():
         print('Error: No spec.src.json is found at %s.' % spec_directory)
         return
 
-    spec_json = util.load_spec_json(spec_filenames[0])
-    for spec_filename in spec_filenames[1:]:
+    # Load the default spec JSON file, ...
+    default_spec_filename = os.path.join(util.script_directory, 'spec.src.json')
+    spec_json = collections.OrderedDict()
+    if os.path.exists(default_spec_filename):
+        spec_json = util.load_spec_json(default_spec_filename)
+
+    # ... and then make spec JSON files in subdirectories override the default.
+    for spec_filename in spec_filenames:
         child_spec_json = util.load_spec_json(spec_filename)
         merge_json(spec_json, child_spec_json)
 
