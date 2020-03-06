011c3198e02700515e6746efb0ea29c6978e3ae2
Sean Harrison <harrisonsean@chromium.org>
2020-03-06 20:44:43 +0000

    Settings: Create new collapsible radio toggle element
    Use the new element to create a new control for safebrowsing that includes their enhanced mode.
    
    Bug: 1032584
    Change-Id: Ife6184d1ed7d427eed64c2a65484e1c3af21588a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2074300
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747840}

---
+++ chrome/browser/resources/settings/privacy_page/collapse_radio_button.js
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+Polymer({
+  is: 'settings-collapse-radio-button',
+
+  behaviors: [
+    CrRadioButtonBehavior,
+  ],
+
+  properties: {
+    expanded: {
+      type: Boolean,
+      notify: true,
+      value: false,
+    },
+
+    label: String,
+
+    subLabel: {
+      type: String,
+      value: '',  // Allows the $hidden= binding to run without being set.
+    },
+  },
+
+  observers: ['onCheckedChanged_(checked)'],
+
+  /** @private */
+  onCheckedChanged_() {
+    // The iron-collapse should open when a user selects the radio button.
+    this.expanded = this.checked;
+  },
+});
