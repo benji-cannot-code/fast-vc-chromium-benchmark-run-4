3603511997c371af8bff83dec545634f42e7c70b
Colin Blundell <blundell@chromium.org>
2020-03-06 09:53:09 +0000

    [Android] Move knowledge of IntentHandler out of ExternalNavHandler
    This CL moves knowledge of the //chrome-level IntentHandler out of
    ExternalNavigationHandler.java. This dependence is to set
    //chrome-level extras on the intent (and set state of IntentHandler
    itself). We abstract this functionality through
    ExternalNavigationDelegate, following the pattern of similar
    already-existing delegate methods.
    
    Bug: 1031465
    Change-Id: I0301abdcf045dfdada1506cf5f14dd7c98315f86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087404
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747621}

--- chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImpl.java
@@ -589,6 +589,16 @@ public class ExternalNavigationDelegateImpl implements ExternalNavigationDelegat
     }
 
     @Override
+    public void maybeSetPendingReferrer(Intent intent, String referrerUrl) {
+        IntentHandler.setPendingReferrer(intent, referrerUrl);
+    }
+
+    @Override
+    public void maybeSetPendingIncognitoUrl(Intent intent) {
+        IntentHandler.setPendingIncognitoUrl(intent);
+    }
+
+    @Override
     public boolean isSerpReferrer() {
         // TODO (thildebr): Investigate whether or not we can use getLastCommittedUrl() instead of
         // the NavigationController.
