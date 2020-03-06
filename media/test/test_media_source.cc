a51afe64fc5410ef06c569735f41d90a2af11066
Jun Cai <juncai@chromium.org>
2020-03-06 20:31:18 +0000

    Convert base::Bind and base::Callback in //media/test to Once or Repeating
    This CL converts base::Bind to Once or Repeating and base::Callback to
    Once or Repeating in //media/test directory.
    
    Bug: 1007812
    Change-Id: I482a583dcf004927c84975c6cb1bcf01adf00b53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082024
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747834}

--- media/test/test_media_source.cc
+++ media/test/test_media_source.cc
@@ -54,7 +54,8 @@ TestMediaSource::TestMediaSource(const std::string& filename,
       initial_sequence_mode_(initial_sequence_mode),
       mimetype_(mimetype),
       chunk_demuxer_(new ChunkDemuxer(
-          base::Bind(&TestMediaSource::DemuxerOpened, base::Unretained(this)),
+          base::BindOnce(&TestMediaSource::DemuxerOpened,
+                         base::Unretained(this)),
           base::DoNothing(),
           base::BindRepeating(&TestMediaSource::OnEncryptedMediaInitData,
                               base::Unretained(this)),
@@ -87,7 +88,8 @@ TestMediaSource::TestMediaSource(scoped_refptr<DecoderBuffer> data,
       initial_sequence_mode_(initial_sequence_mode),
       mimetype_(mimetype),
       chunk_demuxer_(new ChunkDemuxer(
-          base::Bind(&TestMediaSource::DemuxerOpened, base::Unretained(this)),
+          base::BindOnce(&TestMediaSource::DemuxerOpened,
+                         base::Unretained(this)),
           base::DoNothing(),
           base::BindRepeating(&TestMediaSource::OnEncryptedMediaInitData,
                               base::Unretained(this)),
@@ -226,8 +228,8 @@ void TestMediaSource::DemuxerOpenedTask() {
     return;
   }
   chunk_demuxer_->SetTracksWatcher(
-      kSourceId, base::Bind(&TestMediaSource::InitSegmentReceived,
-                            base::Unretained(this)));
+      kSourceId, base::BindRepeating(&TestMediaSource::InitSegmentReceived,
+                                     base::Unretained(this)));
 
   chunk_demuxer_->SetParseWarningCallback(
       kSourceId, base::BindRepeating(&TestMediaSource::OnParseWarningMock,
