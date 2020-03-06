0b97c577cbebfe2a299f19ca49a9948a728487c5
Henrique Nakashima <hnakashima@chromium.org>
2020-03-06 17:49:10 +0000

    Implement DoubleCachedFieldTrialParameter.
    This allows caching double-typed field trial parameters for use
    before native is loaded.
    
    Also implement methods to read and write double-typed values with
    SharedPreferencesManager.
    
    Bug: 1041865, 1012975
    Change-Id: Ieba6e8b41ed566bbd087ddba84c53260a16d481c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087429
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747760}

---
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/DoubleCachedFieldTrialParameter.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.flags;
+
+import org.chromium.chrome.browser.preferences.SharedPreferencesManager;
+
+/**
+ * A double-type {@link CachedFieldTrialParameter}.
+ */
+public class DoubleCachedFieldTrialParameter extends CachedFieldTrialParameter {
+    private double mDefaultValue;
+
+    public DoubleCachedFieldTrialParameter(
+            String featureName, String variationName, double defaultValue) {
+        super(featureName, variationName, FieldTrialParameterType.DOUBLE, null);
+        mDefaultValue = defaultValue;
+    }
+
+    public double getDefaultValue() {
+        return mDefaultValue;
+    }
+
+    @Override
+    void cacheToDisk() {
+        double value = ChromeFeatureList.getFieldTrialParamByFeatureAsDouble(
+                getFeatureName(), getParameterName(), getDefaultValue());
+        SharedPreferencesManager.getInstance().writeDouble(getSharedPreferenceKey(), value);
+    }
+}
