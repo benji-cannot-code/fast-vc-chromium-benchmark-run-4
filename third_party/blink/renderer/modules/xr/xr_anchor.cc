fe804cb353014691aa5f0da497b8f99b661a9615
Piotr Bialecki <bialpio@chromium.org>
2020-03-06 22:40:30 +0000

    WebXR Anchors - update implementation to match the spec draft
    Latest spec: https://immersive-web.github.io/anchors/
    
    - remove last changed time from anchor object
    - anchor space is not nullable on anchor object now
    - anchor objects are not created in zombie state anymore - when anchor
      creation promise resolves, it should receive a fully built object
    - XRAnchor got simplified due to the above
    - XRFrameProvider needs to make sure that JS promises can run between
      presentation frame update and rAF callbacks so that the app has a chance
      to see newly created anchors before they are delivered to it via
      XRFrame.trackedAnchors
    
    Changes: 
    Change-Id: If582552907274d472097f7d7b119bb7ee216736e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2085701
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747901}

--- third_party/blink/renderer/modules/xr/xr_anchor.cc
+++ third_party/blink/renderer/modules/xr/xr_anchor.cc
@@ -11,26 +11,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-XRAnchor::XRAnchor(uint64_t id, XRSession* session)
-    : id_(id), session_(session), anchor_data_(base::nullopt) {}
-
 XRAnchor::XRAnchor(uint64_t id,
                    XRSession* session,
-                   const device::mojom::blink::XRAnchorDataPtr& anchor_data,
-                   double timestamp)
+                   const device::mojom::blink::XRAnchorDataPtr& anchor_data)
     : id_(id),
       session_(session),
-      anchor_data_(base::in_place, anchor_data, timestamp) {}
+      mojo_from_anchor_(std::make_unique<TransformationMatrix>(
+          mojo::ConvertTo<blink::TransformationMatrix>(anchor_data->pose))) {}
 
-void XRAnchor::Update(const device::mojom::blink::XRAnchorDataPtr& anchor_data,
-                      double timestamp) {
-  if (!anchor_data_) {
-    anchor_data_ = AnchorData(anchor_data, timestamp);
-  } else {
-    *anchor_data_->pose_matrix_ =
-        mojo::ConvertTo<blink::TransformationMatrix>(anchor_data->pose);
-    anchor_data_->last_changed_time_ = timestamp;
-  }
+void XRAnchor::Update(
+    const device::mojom::blink::XRAnchorDataPtr& anchor_data) {
+  // Just copy, already allocated.
+  *mojo_from_anchor_ =
+      mojo::ConvertTo<blink::TransformationMatrix>(anchor_data->pose);
 }
 
 uint64_t XRAnchor::id() const {
@@ -38,9 +31,7 @@ uint64_t XRAnchor::id() const {
 }
 
 XRSpace* XRAnchor::anchorSpace() const {
-  if (!anchor_data_) {
-    return nullptr;
-  }
+  DCHECK(mojo_from_anchor_);
 
   if (!anchor_space_) {
     anchor_space_ =
@@ -51,25 +42,9 @@ XRSpace* XRAnchor::anchorSpace() const {
 }
 
 TransformationMatrix XRAnchor::MojoFromObject() const {
-  if (anchor_data_) {
-    return *anchor_data_->pose_matrix_;
-  }
+  DCHECK(mojo_from_anchor_);
 
-  // |poseMatrix()| shouldn't be called by anyone except XRObjectSpace and if
-  // XRObjectSpace already exists for this anchor, then anchor_data_ should also
-  // exist for this anchor.
-  NOTREACHED();
-  return {};
-}
-
-double XRAnchor::lastChangedTime(bool& is_null) const {
-  if (!anchor_data_) {
-    is_null = true;
-    return 0;
-  }
-
-  is_null = false;
-  return anchor_data_->last_changed_time_;
+  return *mojo_from_anchor_;
 }
 
 void XRAnchor::detach() {
@@ -81,12 +56,5 @@ void XRAnchor::Trace(Visitor* visitor) {
   visitor->Trace(anchor_space_);
   ScriptWrappable::Trace(visitor);
 }
-
-XRAnchor::AnchorData::AnchorData(
-    const device::mojom::blink::XRAnchorDataPtr& anchor_data,
-    double timestamp)
-    : pose_matrix_(std::make_unique<TransformationMatrix>(
-          mojo::ConvertTo<blink::TransformationMatrix>(anchor_data->pose))),
-      last_changed_time_(timestamp) {}
 
 }  // namespace blink
