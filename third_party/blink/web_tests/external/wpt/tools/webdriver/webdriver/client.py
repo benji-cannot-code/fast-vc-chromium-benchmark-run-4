7f9b1b93adb242f70c6abb4fdf90705b8c476949
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-03-06 23:10:26 +0000

    Import wpt@8c6d6497707871cf1c073bae65ae06102b569167
    Using wpt-import in Chromium 2bd9927bb715c597e00c0674ce69880c93747eb3.
    With Chromium commits locally applied on WPT:
    d5cd6b98a6 "SharedWorker: Assign unique names to SharedWorkers to avoid unintentional matching"
    acf3302037 "Add testing for the scrolling attribute"
    1aeefa8811 "[UA client hints] Align implementation with PR#46, #48 & #70"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    
    NOAUTOREVERT=true
    TBR=smcgruer
    
    No-Export: true
    Change-Id: I25ff300c5b0d4950eb1b2b20d3c9ab286a25e6e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089410
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#747919}

--- third_party/blink/web_tests/external/wpt/tools/webdriver/webdriver/client.py
+++ third_party/blink/web_tests/external/wpt/tools/webdriver/webdriver/client.py
@@ -445,7 +445,7 @@ class Session(object):
         finally:
             self.session_id = None
 
-    def send_command(self, method, url, body=None):
+    def send_command(self, method, url, body=None, timeout=None):
         """
         Send a command to the remote end and validate its success.
 
@@ -466,7 +466,7 @@ class Session(object):
         response = self.transport.send(
             method, url, body,
             encoder=protocol.Encoder, decoder=protocol.Decoder,
-            session=self)
+            session=self, timeout=timeout)
 
         if response.status != 200:
             err = error.from_response(response)
@@ -494,7 +494,7 @@ class Session(object):
 
         return value
 
-    def send_session_command(self, method, uri, body=None):
+    def send_session_command(self, method, uri, body=None, timeout=None):
         """
         Send a command to an established session and validate its success.
 
@@ -511,7 +511,7 @@ class Session(object):
             an error.
         """
         url = urlparse.urljoin("session/%s/" % self.session_id, uri)
-        return self.send_command(method, url, body)
+        return self.send_command(method, url, body, timeout)
 
     @property
     @command
