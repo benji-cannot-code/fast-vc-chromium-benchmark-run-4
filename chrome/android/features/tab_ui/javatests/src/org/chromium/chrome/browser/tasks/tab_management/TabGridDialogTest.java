8ddd6e6b08f3be532e19b6fc9dc02037cf671a91
Yue Zhang <yuezhanggg@chromium.org>
2020-03-06 20:16:31 +0000

    Temporarily remove appended messages when the last tab is closed
    When the last tab in the grid tab switcher is removed, remove all the
    message items appended. Notice that different from invalidating the
    message items, this would just temporarily remove them and will restore
    message items if needed when the tab switcher is no longer empty.
    
    Bug: 1050443
    Change-Id: Ia5e0aa36c7ac35ad59027fa2ebab25343029e3dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083566
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747825}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -15,7 +15,6 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAct
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtras;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasType;
 import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
-import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
@@ -43,7 +42,6 @@ import static org.chromium.chrome.browser.tasks.tab_management.TabUiTestHelper.v
 import android.content.Intent;
 import android.graphics.Rect;
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.NoMatchingRootException;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.MediumTest;
@@ -266,7 +264,7 @@ public class TabGridDialogTest {
         // Exit dialog, wait for the undo bar showing and undo the closure.
         clickScrimToExitDialog(cta);
         waitForDialogHidingAnimationInTabSwitcher(cta);
-        CriteriaHelper.pollInstrumentationThread(this::verifyUndoBarShowingAndClickUndo);
+        CriteriaHelper.pollInstrumentationThread(TabUiTestHelper::verifyUndoBarShowingAndClickUndo);
 
         // Verify the undo has happened.
         onView(withId(R.id.tab_title)).check((v, noMatchException) -> {
@@ -303,7 +301,7 @@ public class TabGridDialogTest {
         // Exit dialog, wait for the undo bar showing and undo the closure.
         clickScrimToExitDialog(cta);
         waitForDialogHidingAnimation(cta);
-        CriteriaHelper.pollInstrumentationThread(this::verifyUndoBarShowingAndClickUndo);
+        CriteriaHelper.pollInstrumentationThread(TabUiTestHelper::verifyUndoBarShowingAndClickUndo);
 
         // Verify the undo has happened.
         verifyTabStripFaviconCount(cta, 2);
@@ -485,19 +483,6 @@ public class TabGridDialogTest {
                     // Verify if we can grab focus on the editText or not.
                     assertEquals(isEnabled, v.isFocused());
                 });
-    }
-
-    private boolean verifyUndoBarShowingAndClickUndo() {
-        boolean isShowing = true;
-        try {
-            onView(withId(R.id.snackbar_button)).check(matches(isCompletelyDisplayed()));
-            onView(withId(R.id.snackbar_button)).perform(click());
-        } catch (NoMatchingRootException | AssertionError e) {
-            isShowing = false;
-        } catch (Exception e) {
-            assert false : "error when verifying undo snack bar.";
-        }
-        return isShowing;
     }
 
     private void openDialogToolbarMenuAndVerify(ChromeTabbedActivity cta) {
