a17125590be17a46abe95cfc45f32a13b7c7676a
S. Ganesh <ganesh@chromium.org>
2020-03-06 23:33:24 +0000

    Converting DPLOGs to PLOGs for better service install diagnostics.
    Also added a log statement and a cleanup of a value in the unit test.
    
    Bug: 1058506
    Change-Id: I6e43df059f11d7fdba8c430458a1d1f298a56905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090916
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Auto-Submit: S. Ganesh <ganesh@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747927}

--- chrome/installer/util/install_service_work_item_impl.cc
+++ chrome/installer/util/install_service_work_item_impl.cc
@@ -149,7 +149,8 @@ bool InstallServiceWorkItemImpl::DoImpl() {
       return succeeded;
     }
 
-    PLOG(ERROR) << "Failed to install service";
+    PLOG(ERROR) << "Failed to install service "
+                << GetCurrentServiceName().c_str();
     RecordServiceInstallResult(ServiceInstallResult::kFailedFreshInstall);
     RecordWin32ApiErrorCode(kCreateService);
     // Fall through to try installing the service by generating a new name.
@@ -159,6 +160,9 @@ bool InstallServiceWorkItemImpl::DoImpl() {
     // likely to fail. Less intrusive to the SCM and to AV/Anti-malware
     // programs.
     return true;
+  } else {
+    LOG(ERROR) << "Failed to upgrade service "
+               << GetCurrentServiceName().c_str();
   }
 
   // Save the original service name. Then create a new service name so as to not
@@ -184,7 +188,8 @@ bool InstallServiceWorkItemImpl::DoImpl() {
     return true;
   }
 
-  PLOG(ERROR) << "Failed to install service with new name";
+  PLOG(ERROR) << "Failed to install service with new name "
+              << GetCurrentServiceName().c_str();
   RecordServiceInstallResult(
       ServiceInstallResult::kFailedInstallNewAfterFailedUpgrade);
   RecordWin32ApiErrorCode(kCreateService);
@@ -308,7 +313,8 @@ bool InstallServiceWorkItemImpl::GetServiceConfig(ServiceConfig* config) const {
   if (!::QueryServiceConfig(service_.Get(), service_config,
                             kMaxQueryConfigBufferBytes,
                             &bytes_needed_ignored)) {
-    DPLOG(ERROR) << "QueryServiceConfig failed";
+    PLOG(ERROR) << "QueryServiceConfig failed "
+                << GetCurrentServiceName().c_str();
     return false;
   }
 
@@ -341,14 +347,14 @@ bool InstallServiceWorkItemImpl::SetServiceName(
                            KEY_SET_VALUE | KEY_WOW64_32KEY);
   if (result != ERROR_SUCCESS) {
     ::SetLastError(result);
-    DPLOG(ERROR) << "key.Create failed";
+    PLOG(ERROR) << "key.Create failed";
     return false;
   }
 
   result = key.WriteValue(service_name_.c_str(), service_name.c_str());
   if (result != ERROR_SUCCESS) {
     ::SetLastError(result);
-    DPLOG(ERROR) << "key.WriteValue failed";
+    PLOG(ERROR) << "key.WriteValue failed";
     return false;
   }
 
@@ -451,7 +457,8 @@ bool InstallServiceWorkItemImpl::InstallService(const ServiceConfig& config) {
       !config.dependencies.empty() ? config.dependencies.data() : nullptr,
       nullptr, nullptr));
   if (!service.IsValid()) {
-    DPLOG(WARNING) << "Failed to create service";
+    PLOG(WARNING) << "Failed to create service "
+                  << GetCurrentServiceName().c_str();
     return false;
   }
 
@@ -469,7 +476,8 @@ bool InstallServiceWorkItemImpl::ChangeServiceConfig(
           config.cmd_line.c_str(), nullptr, nullptr,
           !config.dependencies.empty() ? config.dependencies.data() : nullptr,
           nullptr, nullptr, nullptr)) {
-    DPLOG(WARNING) << "Failed to change service config";
+    PLOG(WARNING) << "Failed to change service config "
+                  << GetCurrentServiceName().c_str();
     return false;
   }
 
@@ -482,7 +490,7 @@ bool InstallServiceWorkItemImpl::DeleteService(ScopedScHandle service) const {
 
   if (!::DeleteService(service.Get())) {
     DWORD error = ::GetLastError();
-    DPLOG(WARNING) << "DeleteService failed";
+    PLOG(WARNING) << "DeleteService failed " << GetCurrentServiceName().c_str();
     return error == ERROR_SERVICE_MARKED_FOR_DELETE;
   }
 
