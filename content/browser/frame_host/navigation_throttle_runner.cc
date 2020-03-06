6d0f5e8f32dd8d409fa07cbdf4f35ebf0a2b34e5
Alexander Timin <altimin@chromium.org>
2020-03-06 16:59:23 +0000

    Remove HistoryNavigationAblationStudyNavigationThrottle.
    Remove it as it is not being used now.
    
    BUG=954271
    R=arthursonzogni@chromium.org
    
    Change-Id: I7f7e0d0e4d5b1937c9c2a87aaf39f45da6f40bce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2091488
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747742}

--- content/browser/frame_host/navigation_throttle_runner.cc
+++ content/browser/frame_host/navigation_throttle_runner.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/frame_host/ancestor_throttle.h"
 #include "content/browser/frame_host/blocked_scheme_navigation_throttle.h"
 #include "content/browser/frame_host/form_submission_throttle.h"
-#include "content/browser/frame_host/history_navigation_ablation_study_navigation_throttle.h"
 #include "content/browser/frame_host/mixed_content_navigation_throttle.h"
 #include "content/browser/frame_host/navigation_request.h"
 #include "content/browser/frame_host/navigator_delegate.h"
@@ -129,10 +128,6 @@ void NavigationThrottleRunner::RegisterNavigationThrottles() {
        devtools_instrumentation::CreateNavigationThrottles(request)) {
     AddThrottle(std::move(throttle));
   }
-
-  // Delay navigation for an ablation study (if needed).
-  AddThrottle(HistoryNavigationAblationStudyNavigationThrottle::
-                  MaybeCreateForNavigation(request));
 
   // Insert all testing NavigationThrottles last.
   throttles_.insert(throttles_.end(),
