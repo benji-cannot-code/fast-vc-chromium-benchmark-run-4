138a4313fd85c85b18941857b9fd74f34546035b
Josh Simmons <simmonsjosh@google.com>
2020-03-06 02:00:00 +0000

    Add Quick View support for video files on DriveFS
    Treat video files on DriveFS as if they were local when launching Quick
    View. This results in auto-play (where the media is decodable).
    
    DriveFS streams large files so if the user cancels / pauses the playback
    there will not be unnecessary data transferred.
    
    Bug: 1059072
    Test: browser_tests --gtest_filter="openQuickViewVideoOnDrive"
    Change-Id: I0bd6020acdaad8980c7fcf6f48891389d20e8a27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089439
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Commit-Queue: Josh Simmons <simmonsjosh@google.com>
    Cr-Commit-Position: refs/heads/master@{#747526}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -480,6 +480,7 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
         TestCase("openQuickViewBrokenImage"),
         TestCase("openQuickViewImageClick"),
         TestCase("openQuickViewVideo"),
+        TestCase("openQuickViewVideoOnDrive"),
         TestCase("openQuickViewPdf"),
         TestCase("openQuickViewPdfPreviewsDisabled"),
         TestCase("openQuickViewKeyboardUpDownChangesView"),
