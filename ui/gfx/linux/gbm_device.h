b5ba9367cee552e5ed870334e1bae5035bce5941
Bailey Berro <baileyberro@chromium.org>
2020-03-06 01:29:37 +0000

    Revert "Add GetPlaneSurface to ui::GbmBuffer"
    This reverts commit 64d0d2969985113ef7a5663ce2d1a981f9d42f6f.
    
    Reason for revert: crbug.com/1058503
    
    Original change's description:
    > Add GetPlaneSurface to ui::GbmBuffer
    > 
    > There is currently a GetSurface() api but it only gives
    > access to the plane 0.
    > 
    > This new api GetPlaneSurface allows to get access to any
    > planes to support YUV formats.
    > 
    > Bug: 1043007
    > Test: ./out/ozone_build/ozone_unittests
    > Change-Id: I4116affcf168cdaeebf3b405234aa3e5f55cc30f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032060
    > Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    > Reviewed-by: Maksim Sisov <msisov@igalia.com>
    > Commit-Queue: Julien Isorce <julien.isorce@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#740986}
    
    TBR=spang@chromium.org,dnicoara@chromium.org,thomasanderson@chromium.org,msisov@igalia.com,julien.isorce@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1043007
    Change-Id: I014bf490affe19c4ce464cd866276cf287712c78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090358
    Reviewed-by: Bailey Berro <baileyberro@chromium.org>
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747515}

--- ui/gfx/linux/gbm_device.h
+++ ui/gfx/linux/gbm_device.h
@@ -34,7 +34,6 @@ class GbmDevice {
       uint32_t format,
       const gfx::Size& size,
       gfx::NativePixmapHandle handle) = 0;
-  virtual bool IsFormatAndUsageSupported(uint32_t format, uint32_t flags) = 0;
 };
 
 }  // namespace ui
--- ui/ozone/platform/wayland/gpu/wayland_surface_factory_unittest.cc
+++ ui/ozone/platform/wayland/gpu/wayland_surface_factory_unittest.cc
@@ -52,7 +52,6 @@ class FakeGbmBuffer : public GbmBuffer {
   gfx::NativePixmapHandle ExportHandle() const override {
     return gfx::NativePixmapHandle();
   }
-  sk_sp<SkSurface> GetPlaneSurface(size_t plane) override { return nullptr; }
   sk_sp<SkSurface> GetSurface() override { return nullptr; }
 
  private:
@@ -82,9 +81,6 @@ class FakeGbmDevice : public GbmDevice {
       const gfx::Size& size,
       gfx::NativePixmapHandle handle) override {
     return nullptr;
-  }
-  bool IsFormatAndUsageSupported(uint32_t format, uint32_t flags) override {
-    return false;
   }
 
  private:
