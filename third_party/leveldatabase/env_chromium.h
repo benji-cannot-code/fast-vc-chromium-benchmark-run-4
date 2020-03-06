260ce8c58ba5dd9bf4f79e1c99a30183c36bf58f
Ken Rockot <rockot@google.com>
2020-03-06 05:51:43 +0000

    Use FilesystemProxy in LevelDB ChromiumEnv
    This rewrites ChromiumEnv to use the storage::FilesystemProxy
    API for all filesystem access. By default it will use unrestricted
    instances with direct filesystem access, effectively equivalent to
    the status quo. No interesting behavioral or performance changes
    are expected here.
    
    This will allow a follow-up CL to implement sandboxing support for
    LevelDB by constructing a ChromiumEnv instance over a restricted
    FilesystemProxy instance.
    
    Bug: 1052045
    Change-Id: Ic23726b3f5abd3f1a322b49827e80dbf424c3de7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2080673
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747584}

--- third_party/leveldatabase/env_chromium.h
+++ third_party/leveldatabase/env_chromium.h
@@ -36,6 +36,10 @@ class ProcessMemoryDump;
 }  // namespace trace_event
 }  // namespace base
 
+namespace storage {
+class FilesystemProxy;
+}
+
 namespace leveldb_env {
 
 // These entries map to values in tools/metrics/histograms/histograms.xml. New
@@ -158,11 +162,16 @@ class LEVELDB_EXPORT ChromiumEnv : public leveldb::Env,
                                    public UMALogger,
                                    public RetrierProvider {
  public:
+  using ScheduleFunc = void(void*);
+
+  // Constructs a ChromiumEnv instance with an unrestricted FilesystemProxy
+  // instance that performs direct filesystem access.
   ChromiumEnv();
 
-  typedef void(ScheduleFunc)(void*);
+  // Constructs a ChromiumEnv instance with a custom FilesystemProxy instance.
+  explicit ChromiumEnv(std::unique_ptr<storage::FilesystemProxy> filesystem);
 
-  virtual ~ChromiumEnv();
+  ~ChromiumEnv() override;
 
   bool FileExists(const std::string& fname) override;
   leveldb::Status GetChildren(const std::string& dir,
@@ -196,7 +205,13 @@ class LEVELDB_EXPORT ChromiumEnv : public leveldb::Env,
   void SetReadOnlyFileLimitForTesting(int max_open_files);
 
  protected:
+  // Constructs a ChromiumEnv instance with a local unrestricted FilesystemProxy
+  // instance that performs direct filesystem access.
   explicit ChromiumEnv(const std::string& name);
+
+  // Constructs a ChromiumEnv instance with a custom FilesystemProxy instance.
+  ChromiumEnv(const std::string& name,
+              std::unique_ptr<storage::FilesystemProxy> filesystem);
 
   static const char* FileErrorString(base::File::Error error);
 
@@ -207,23 +222,6 @@ class LEVELDB_EXPORT ChromiumEnv : public leveldb::Env,
   void RecordBytesWritten(int amount) const override;
   base::HistogramBase* GetOSErrorHistogram(MethodID method, int limit) const;
   void RemoveBackupFiles(const base::FilePath& dir);
-
-  // File locks may not be exclusive within a process (e.g. on POSIX). Track
-  // locks held by the ChromiumEnv to prevent access within the process.
-  class LockTable {
-   public:
-    bool Insert(const std::string& fname) {
-      leveldb::MutexLock l(&mu_);
-      return locked_files_.insert(fname).second;
-    }
-    bool Remove(const std::string& fname) {
-      leveldb::MutexLock l(&mu_);
-      return locked_files_.erase(fname) == 1;
-    }
-   private:
-    leveldb::port::Mutex mu_;
-    std::set<std::string> locked_files_;
-  };
 
   const int kMaxRetryTimeMillis;
   // BGThread() is the body of the background thread
@@ -239,6 +237,8 @@ class LEVELDB_EXPORT ChromiumEnv : public leveldb::Env,
   base::HistogramBase* GetRetryTimeHistogram(MethodID method) const override;
   base::HistogramBase* GetRecoveredFromErrorHistogram(
       MethodID method) const override;
+
+  const std::unique_ptr<storage::FilesystemProxy> filesystem_;
 
   base::FilePath test_directory_;
 
@@ -256,7 +256,6 @@ class LEVELDB_EXPORT ChromiumEnv : public leveldb::Env,
   };
   using BGQueue = base::circular_deque<BGItem>;
   BGQueue queue_;
-  LockTable locks_;
   std::unique_ptr<leveldb::Cache> file_cache_;
 };
 
