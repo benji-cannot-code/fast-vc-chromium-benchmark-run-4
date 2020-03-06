2a2747a791c043d46442f557b5604daba4b19fb1
Tom Anderson <thomasanderson@chromium.org>
2020-03-06 12:34:51 +0000

    Fix crash in ui::XShmImagePool::DispatchShmCompletionEvent
    The issue can be reproduced by creating and closing windows really
    fast (like when you're using a tiling WM and dragging tabs and it
    sort of bugs out).  In that case the following was getting hit:
      CHECK(!swap_closures_.empty());
    
    The solution is to do loop through |swap_closures_| and remove only
    the matching element.
    
    BUG=1057943
    R=nickdiego
    
    Change-Id: Ib55f6326df6c444adf1bdc6f710785a05cb0d619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090354
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747658}

--- ui/base/x/x11_shm_image_pool.h
+++ ui/base/x/x11_shm_image_pool.h
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_BASE_X_X11_SHM_IMAGE_POOL_H_
 
 #include <cstring>
-#include <queue>
+#include <list>
 #include <vector>
 
 #include "base/callback_forward.h"
@@ -116,7 +116,7 @@ class COMPONENT_EXPORT(UI_BASE_X) XShmImagePool
   std::size_t frame_bytes_ = 0;
   std::vector<FrameState> frame_states_;
   std::size_t current_frame_index_ = 0;
-  std::queue<SwapClosure> swap_closures_;
+  std::list<SwapClosure> swap_closures_;
 
   DISALLOW_COPY_AND_ASSIGN(XShmImagePool);
 };
