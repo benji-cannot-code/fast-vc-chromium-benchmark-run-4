62a5cea78cc922830dfdafc4aef4f977b28e7496
rbpotter <rbpotter@chromium.org>
2020-03-07 01:10:59 +0000

    Polymer modulizer: Support ignore annotations
    Support /* ignore */ annotations in Polymer modulizer, following the
    same approach as the JS modulizer. This will be used to temporarily
    migrate settings_idle_load and settings pages that depend on it, before
    adding lazy loading to the Polymer 3 page.
    
    Bug: 1026426
    Change-Id: Iaf2b3d1e3ba69f97946039522d86ecf2739370cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2092183
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747951}

---
+++ tools/polymer/tests/dom_module_with_ignore_expected.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import {Polymer, html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {PaperRippleBehavior} from 'chrome://resources/polymer/v3_0/paper-behaviors/paper-ripple-behavior.js';
+// import 'chrome://resources/js/ignore_me.m.js';
+import '../shared_vars_css.m.js';
+import './foo.m.js';
+
+Polymer({
+  _template: html`<!--_html_template_start_-->
+    <style>
+      div {
+        font-size: 2rem;
+      }
+    </style>
+    <div>Hello world</div>
+<!--_html_template_end_-->`,
+  is: 'cr-test-foo',
+  behaviors: [PaperRippleBehavior],
+  /** @override */
+  ready() {
+  },
+});
