c0ead384b11470954cfcf67861edfa5ceb741f41
sauski <sauski@google.com>
2020-03-07 03:03:08 +0000

    Move Do Not Track Toggle to Cookies sub page
    As part of the Friendly Settings Privacy settings redesign the "Do not
    track" toggle is being moved to the cookies page.
    
    To make moving cleaner this CL first creates a new custom polymer
    element and tests encompassing the toggle and the associated dialog.
    
    The new element is then placed in both locations in sections whose
    visibility is already controlled by the privacySettingsRedesign flag.
    
    Bug: 1032584
    Change-Id: Id3190592989174fa7a0c51f7263844a1326e7ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2092136
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#747977}

--- chrome/browser/resources/settings/privacy_page/cookies_page.js
+++ chrome/browser/resources/settings/privacy_page/cookies_page.js
@@ -14,6 +14,14 @@ Polymer({
 
   properties: {
     /**
+     * Preferences state.
+     */
+    prefs: {
+      type: Object,
+      notify: true,
+    },
+
+    /**
      * @private {!settings.ContentSettingsTypes}
      */
     ContentSettingsTypes: {type: Object, value: settings.ContentSettingsTypes},
