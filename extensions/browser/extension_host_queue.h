a3ea758235db560f11315f86b9211995bf5c70a8
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-03-07 00:21:35 +0000

    [Extensions] Make ChromeTestExtensionLoader wait for extension readiness
    ChromeTestExtensionLoader is the most common mechanism used to load
    extensions in tests, and is used in both unit and browser tests. It is
    the underlying mechanism used in
    ExtensionServiceTestWithInstall::InstallCRX() (and variations) and also
    ExtensionBrowserTest::LoadExtension() (and variations).
    
    Currently, the ChromeTestExtensionLoader loads the new extension, adds
    it to the profile, and then waits for extension readiness by
    a) waiting for extension content scripts to load, and
    b) waiting for all active extension WebContents to finish loading.
    
    Unfortunately, this can be insufficient if the extension WebContents
    hasn't been created yet. Since we queue ExtensionHost creation, this
    can be an asynchronous process, and can (depending on other factors)
    happen sufficiently late that the ExtensionHost isn't created by the
    time we check whether all WebContents are done loading.
    
    This inconsistency leads many tests to exercise the following pattern
    in their background page, to avoid flakiness:
    
    chrome.<something>.addListener(() => { ... });
    chrome.test.sendMessage('ready');
    
    However, this should be unnecessary, because we should be able to wait
    for the background page to finish its first "spin" before proceeding
    (which would mean we only have to take the "listen for ready" approach
    if an extension has to perform asynchronous set up).
    
    Modify ChromeTestExtensionLoader to also wait for the background page's
    ExtensionHost to be created and finish its first load before proceeding.
    Apply this behavior only if there exist any renderer processes, as a
    proxy for whether this is running in a unit or browser test (since
    waiting on a background page in a unit test will wait forever). Allow
    tests to override this behavior (which is necessary if they never expect
    the page to load).
    
    Add a regression browser test that exposes the previous potential
    failures by incorporating a delay into ExtensionHostQueue.
    
    Bug: 1038051
    
    Change-Id: Ic736ae4990dcc9ec9bc50b79d65768c705b2748b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1982770
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747943}

--- extensions/browser/extension_host_queue.h
+++ extensions/browser/extension_host_queue.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 
 namespace extensions {
 class DeferredStartRenderHost;
@@ -35,6 +36,10 @@ class ExtensionHostQueue {
   // having a chance to start)
   void Remove(DeferredStartRenderHost* host);
 
+  // Adds a delay before starting the next ExtensionHost. This can be used for
+  // testing purposes to help flush out flakes.
+  void SetCustomDelayForTesting(base::TimeDelta delay) { delay_ = delay; }
+
  private:
   // Queues up a delayed task to process the next DeferredStartRenderHost in
   // the queue.
@@ -46,6 +51,10 @@ class ExtensionHostQueue {
   // True if this queue is currently in the process of starting an
   // DeferredStartRenderHost.
   bool pending_create_;
+
+  // The delay before starting the next host. By default, this is 0, meaning we
+  // just wait until the event loop yields.
+  base::TimeDelta delay_;
 
   // The list of DeferredStartRenderHosts waiting to be started.
   std::list<DeferredStartRenderHost*> queue_;
