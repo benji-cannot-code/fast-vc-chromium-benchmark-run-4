3c7ef1f5d86d4d1cf8466e0b33796fa2bc73c523
Ian Wells <iwells@chromium.org>
2020-03-07 03:23:25 +0000

    Feed refactor: Proto conversion
    Add StreamModelUpdateRequest and functions for converting wire protos into store protos
    
    Bug: 1044139
    Change-Id: I243ebabc331884500dec480c69d843d38546a34e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2020545
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#747983}

---
+++ components/feed/core/v2/tools/generate_test_response_binarypb.sh
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+CHROMIUM_SRC=$(realpath $(dirname $(readlink -f $0))/../../../../..)
+OUT_DIR=$CHROMIUM_SRC/components/test/data/feed
+
+if [ ! -d $OUT_DIR ]; then
+  echo "Output directory $OUT_DIR doesn't exist."
+  exit 1
+fi
+
+python3 $CHROMIUM_SRC/components/feed/core/v2/tools/textpb_to_binarypb.py \
+  --chromium_path=$CHROMIUM_SRC \
+  --output_file=$OUT_DIR/response.binarypb \
+  --source_file=\
+$CHROMIUM_SRC/components/feed/core/v2/testdata/response.textproto
