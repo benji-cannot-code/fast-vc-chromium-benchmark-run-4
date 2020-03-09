10fa129650e4cd172c1e1670959a9590f2f36cc2
Nicholas Verne <nverne@chromium.org>
2020-03-09 01:46:15 +0000

    Unmount old Crostini ssh volumes on startup.
    If Chrome crashes while crostini is running, old mount points for the
    sshfs file system are still polluting mtab, and if we don't remove
    them here, then on restarting the VM we still won't be able to use
    Linux files in the files app. Hence we remove the old mounts at session
    startup if it's an unclean startup.
    
    Bug: 1057899
    Change-Id: Ib5082c973e245800ac976efcecfd0aad96fe998d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089538
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Auto-Submit: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/master@{#748081}

--- chrome/browser/chromeos/crostini/crostini_manager.h
+++ chrome/browser/chromeos/crostini/crostini_manager.h
@@ -608,6 +608,7 @@ class CrostiniManager : public KeyedService,
   void EnsureVmRunning(const ContainerId& key, CrostiniResultCallback callback);
   bool IsUncleanStartup() const;
   void SetUncleanStartupForTesting(bool is_unclean_startup);
+  void RemoveUncleanSshfsMounts();
 
  private:
   class CrostiniRestarter;
