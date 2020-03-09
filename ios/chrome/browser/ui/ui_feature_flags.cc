9440f931f38c6d2e9644c5971e2f5b8315088944
Justin Cohen <justincohen@google.com>
2020-03-09 21:56:51 +0000

    [ios] Remove kBrowserContainerKeepsContentView feature flag.
    This feature was dropped in lieu of the KeepRenderProcessAlive flag.
    
    TEST=Does not regress any of the WKWebView hidden test cases.
    
    Bug: 943569, 930123
    Change-Id: Ia6ab0e83e808477d6dbc7498607036ac05a31899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2094468
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748397}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 
-const base::Feature kBrowserContainerKeepsContentView{
-    "BrowserContainerKeepsContentView", base::FEATURE_DISABLED_BY_DEFAULT};
-
 // TODO(crbug.com/945811): Using |-drawViewHierarchyInRect:afterScreenUpdates:|
 // has adverse flickering when taking a snapshot of the NTP while in the app
 // switcher.
