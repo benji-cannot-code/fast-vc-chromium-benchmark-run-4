fe2d9a85e89670d1c98a7be95acdd9fec004396a
Michael Spang <spang@chromium.org>
2020-03-09 14:41:11 +0000

    [fuchsia] Enable ANGLE in web_engine (reland)
    This is not currently compatible with --enforce-vulkan-protected-memory.
    which forces all shared images to be protected, including canvas
    resources.
    
    Bug: 766360
    TBR=sergeyu@chromium.org
    
    Change-Id: I784d2fe690cdced6a733f85d6104fbbaf2532057
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2092871
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Michael Spang <spang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748208}

--- ui/ozone/platform/scenic/scenic_surface_factory.cc
+++ ui/ozone/platform/scenic/scenic_surface_factory.cc
@@ -95,6 +95,7 @@ ScenicSurfaceFactory::GetAllowedGLImplementations() {
   return std::vector<gl::GLImplementation>{
       gl::kGLImplementationSwiftShaderGL,
       gl::kGLImplementationEGLGLES2,
+      gl::kGLImplementationEGLANGLE,
       gl::kGLImplementationStubGL,
   };
 }
@@ -103,6 +104,7 @@ GLOzone* ScenicSurfaceFactory::GetGLOzone(gl::GLImplementation implementation) {
   switch (implementation) {
     case gl::kGLImplementationSwiftShaderGL:
     case gl::kGLImplementationEGLGLES2:
+    case gl::kGLImplementationEGLANGLE:
       return egl_implementation_.get();
     default:
       return nullptr;
