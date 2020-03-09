2d4841c83d2e6002738981eee0639af9291104b2
Peter Kasting <pkasting@chromium.org>
2020-03-09 22:33:38 +0000

    Disable various ash frame tests that fail with kWebUITabStrip.
    I believe these were not caught earlier because we previously called
    MaterialDesignController::Initialize() in every test, which reset
    the controller state to non-touch mode after the TestAPI scoper had
    set it to whatever we were trying to check.
    
    The most problematic of these is AvatarDisplayOnTeleportedWindow,
    which is what the bug here is formally about.  However, the other
    tests disabled here should also be checked: perhaps the test can be
    rewritten in a way that makes sense with the WebUI tabstrip.
    
    This also makes a couple other changes for robustness that should
    have little practical effect: changing an EXPECT to an ASSERT (since
    if we fail to leave immersive mode when we expect, the test has
    already timed out, and we might as well abort), and changing a click
    on the leftmost pixel of a button to a click on the center.
    
    Bug: 1059514
    Change-Id: I7973e043e462b649cca9b4f25214149dcd6bc41a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095855
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748410}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/browser_actions_bar_browsertest.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
@@ -160,8 +161,19 @@ class TopChromeMdParamTest : public BaseTest,
 
  private:
   ui::test::MaterialDesignControllerTestAPI test_api_;
+};
 
-  DISALLOW_COPY_AND_ASSIGN(TopChromeMdParamTest);
+// Template to be used when a test does not work with the webUI tabstrip.
+template <class BaseTest>
+class NoWebUiTabStripTest : public BaseTest {
+ public:
+  NoWebUiTabStripTest() {
+    feature_override_.InitAndDisableFeature(features::kWebUITabStrip);
+  }
+  ~NoWebUiTabStripTest() override = default;
+
+ private:
+  base::test::ScopedFeatureList feature_override_;
 };
 
 // A helper class for immersive mode tests.
@@ -201,7 +213,7 @@ class ImmersiveModeTester : public ImmersiveModeController::Observer {
       fullscreen_loop_ = std::make_unique<base::RunLoop>();
       fullscreen_loop_->Run();
     }
-    EXPECT_FALSE(GetBrowserView()->immersive_mode_controller()->IsEnabled());
+    ASSERT_FALSE(GetBrowserView()->immersive_mode_controller()->IsEnabled());
   }
 
   // ImmersiveModeController::Observer:
@@ -249,8 +261,13 @@ using views::Widget;
 
 using BrowserNonClientFrameViewAshTest =
     TopChromeMdParamTest<InProcessBrowserTest>;
+using BrowserNonClientFrameViewAshTestNoWebUiTabStrip =
+    NoWebUiTabStripTest<BrowserNonClientFrameViewAshTest>;
 
-IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest, NonClientHitTest) {
+// This test does not make sense for the webUI tabstrip, since the window layout
+// is different in that case.
+IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTestNoWebUiTabStrip,
+                       NonClientHitTest) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   Widget* widget = browser_view->GetWidget();
   BrowserNonClientFrameViewAsh* frame_view = GetFrameViewAsh(browser_view);
@@ -275,7 +292,9 @@ IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest, NonClientHitTest) {
 
 // Test that the frame view does not do any painting in non-immersive
 // fullscreen.
-IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest,
+// This test does not make sense for the webUI tabstrip, since the frame is not
+// painted in that case.
+IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTestNoWebUiTabStrip,
                        NonImmersiveFullscreen) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   content::WebContents* web_contents = browser_view->GetActiveWebContents();
@@ -301,7 +320,9 @@ IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest,
 
 // Tests that Avatar icon should show on the top left corner of the teleported
 // browser window on ChromeOS.
-IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTest,
+// TODO(http://crbug.com/1059514): This test should be made to work with the
+// webUI tabstrip.
+IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewAshTestNoWebUiTabStrip,
                        AvatarDisplayOnTeleportedWindow) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   BrowserNonClientFrameViewAsh* frame_view = GetFrameViewAsh(browser_view);
@@ -446,7 +467,13 @@ class ImmersiveModeBrowserViewTest
 
 }  // namespace
 
-IN_PROC_BROWSER_TEST_P(ImmersiveModeBrowserViewTest, ImmersiveFullscreen) {
+using ImmersiveModeBrowserViewTestNoWebUiTabStrip =
+    NoWebUiTabStripTest<ImmersiveModeBrowserViewTest>;
+
+// This test does not make sense for the webUI tabstrip, since the frame is not
+// painted in that case.
+IN_PROC_BROWSER_TEST_P(ImmersiveModeBrowserViewTestNoWebUiTabStrip,
+                       ImmersiveFullscreen) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
   content::WebContents* web_contents = browser_view->GetActiveWebContents();
   BrowserNonClientFrameViewAsh* frame_view = GetFrameViewAsh(browser_view);
@@ -557,7 +584,9 @@ IN_PROC_BROWSER_TEST_P(ImmersiveModeBrowserViewTest,
     tester.RunCommand(datum.command, datum.expected_index);
 }
 
-IN_PROC_BROWSER_TEST_P(ImmersiveModeBrowserViewTest,
+// This test does not make sense for the webUI tabstrip, since the window layout
+// is different in that case.
+IN_PROC_BROWSER_TEST_P(ImmersiveModeBrowserViewTestNoWebUiTabStrip,
                        TestCaptionButtonsReceiveEventsInBrowserImmersiveMode) {
   BrowserView* browser_view = BrowserView::GetBrowserViewForBrowser(browser());
 
@@ -588,7 +617,7 @@ IN_PROC_BROWSER_TEST_P(ImmersiveModeBrowserViewTest,
   gfx::Size button_size = views::GetCaptionButtonLayoutSize(
       views::CaptionButtonLayoutSize::kBrowserCaptionMaximized);
   gfx::Point point_in_restore_button(window->GetBoundsInScreen().top_right());
-  point_in_restore_button.Offset(-2 * button_size.width(),
+  point_in_restore_button.Offset(-button_size.width() * 3 / 2,
                                  button_size.height() / 2);
 
   event_generator.MoveMouseTo(point_in_restore_button);
