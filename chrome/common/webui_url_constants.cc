b17b0f2f440468edf2515fb9601721b286e3cc9a
Vasilii Sukhanov <vasilii@chromium.org>
2020-03-09 11:58:34 +0000

    The password leak check now links to the local Chrome settings page.
    TBR=holte@chromium.org
    
    Bug: 1049200
    Change-Id: I096daf099c8bf97f0699aacb4ca3cd0a58d012d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090430
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748174}

--- chrome/common/webui_url_constants.cc
+++ chrome/common/webui_url_constants.cc
@@ -375,6 +375,7 @@ const char kHandlerSettingsSubPage[] = "handlers";
 const char kImportDataSubPage[] = "importData";
 const char kLanguageOptionsSubPage[] = "languages";
 const char kOnStartupSubPage[] = "onStartup";
+const char kPasswordCheckSubPage[] = "passwords/check";
 const char kPasswordManagerSubPage[] = "passwords";
 const char kPaymentsSubPage[] = "payments";
 const char kPrintingSettingsSubPage[] = "printing";
