d8a44248692901fb8da946ae9173a71f6dd7bbb5
Alain Michaud <alainmichaud@google.com>
2020-03-09 19:19:25 +0000

    Revert [Bluetooth] Ensure update of BlueZ's LTK list after first-time install.
    Reason for revert: The flag has never been used and the underlying fix is
    being replaced by an upstream bluez fix which does not rely on this interface.
    
    Bug: 942089
    Originally Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1652709
    
    Change-Id: I52e6c4f3762f248bdc94383ee5b7f39935db6881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1948095
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Sonny Sasaka <sonnysasaka@chromium.org>
    Commit-Queue: Alain Michaud <alainm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748334}

--- device/bluetooth/bluez/bluetooth_bluez_unittest.cc
+++ device/bluetooth/bluez/bluetooth_bluez_unittest.cc
@@ -326,13 +326,6 @@ class BluetoothBlueZTest : public testing::Test {
     DiscoverDevice("does not exist");
   }
 
-  void ExpectSetLongTermKeysCallCount(uint32_t count) {
-#if defined(OS_CHROMEOS)
-    EXPECT_EQ(count,
-              fake_bluetooth_adapter_client_->set_long_term_keys_call_count());
-#endif
-  }
-
  protected:
   base::test::SingleThreadTaskEnvironment task_environment_;
   bluez::FakeBluetoothAdapterClient* fake_bluetooth_adapter_client_;
@@ -364,13 +357,10 @@ const char BluetoothBlueZTest::kHeadsetUuid[] =
     "00001112-0000-1000-8000-00805f9b34fb";
 
 TEST_F(BluetoothBlueZTest, AlreadyPresent) {
-  ExpectSetLongTermKeysCallCount(0u);
-
   GetAdapter();
 
   // This verifies that the class gets the list of adapters when created;
   // and initializes with an existing adapter if there is one.
-  ExpectSetLongTermKeysCallCount(1u);
   EXPECT_TRUE(adapter_->IsPresent());
   EXPECT_FALSE(adapter_->IsPowered());
   EXPECT_EQ(bluez::FakeBluetoothAdapterClient::kAdapterAddress,
@@ -396,7 +386,6 @@ TEST_F(BluetoothBlueZTest, BecomePresent) {
   fake_bluetooth_adapter_client_->SetVisible(false);
   GetAdapter();
   ASSERT_FALSE(adapter_->IsPresent());
-  ExpectSetLongTermKeysCallCount(0u);
 
   // Install an observer; expect the AdapterPresentChanged to be called
   // with true, and IsPresent() to return true.
@@ -408,7 +397,6 @@ TEST_F(BluetoothBlueZTest, BecomePresent) {
   EXPECT_TRUE(observer.last_present());
 
   EXPECT_TRUE(adapter_->IsPresent());
-  ExpectSetLongTermKeysCallCount(1u);
 
   // We should have had a device announced.
   EXPECT_EQ(2, observer.device_added_count());
@@ -419,14 +407,12 @@ TEST_F(BluetoothBlueZTest, BecomePresent) {
   EXPECT_EQ(0, observer.powered_changed_count());
   EXPECT_EQ(0, observer.discovering_changed_count());
   EXPECT_FALSE(adapter_->IsPowered());
-  EXPECT_FALSE(IsAdapterDiscovering());
-  ExpectSetLongTermKeysCallCount(1u);
+  EXPECT_FALSE(adapter_->IsDiscovering());
 }
 
 TEST_F(BluetoothBlueZTest, BecomeNotPresent) {
   GetAdapter();
   ASSERT_TRUE(adapter_->IsPresent());
-  ExpectSetLongTermKeysCallCount(1u);
 
   // Install an observer; expect the AdapterPresentChanged to be called
   // with false, and IsPresent() to return false.
@@ -455,8 +441,7 @@ TEST_F(BluetoothBlueZTest, BecomeNotPresent) {
   // Other callbacks shouldn't be called since the values are false.
   EXPECT_EQ(0, observer.powered_changed_count());
   EXPECT_FALSE(adapter_->IsPowered());
-  EXPECT_FALSE(IsAdapterDiscovering());
-  ExpectSetLongTermKeysCallCount(1u);
+  EXPECT_FALSE(adapter_->IsDiscovering());
 }
 
 TEST_F(BluetoothBlueZTest, SecondAdapter) {
@@ -511,7 +496,6 @@ TEST_F(BluetoothBlueZTest, SecondAdapter) {
   EXPECT_EQ(0, observer.device_removed_count());
   EXPECT_EQ(0, observer.powered_changed_count());
   EXPECT_EQ(0, observer.discovering_changed_count());
-  ExpectSetLongTermKeysCallCount(1u);
 }
 
 TEST_F(BluetoothBlueZTest, BecomePowered) {
