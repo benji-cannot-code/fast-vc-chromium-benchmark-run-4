93442f8796de89e5d448ea94394f23f5a506733c
Denis Kuznetsov <antrim@google.com>
2020-03-09 17:42:03 +0000

    Set default value for domain on offline sign-in screen
    Polymer v2 evaluates i18nDynamic(as it has defined locale param),
    but does not evaluate !domain as only one undefined param.
    
    Fixed: 1059735
    Change-Id: Ia8ac3a166590e669d77760f1bdd0c1786712c6f7
    Bug: 1059735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095009
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#748281}

--- chrome/browser/chromeos/login/login_browsertest.cc
+++ chrome/browser/chromeos/login/login_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/test/guest_session_mixin.h"
 #include "chrome/browser/chromeos/login/test/login_manager_mixin.h"
 #include "chrome/browser/chromeos/login/test/offline_gaia_test_mixin.h"
+#include "chrome/browser/chromeos/login/test/session_manager_state_waiter.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host_webui.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_constants.h"
@@ -173,10 +174,13 @@ IN_PROC_BROWSER_TEST_F(LoginTest, PRE_GaiaAuthOffline) {
   offline_gaia_test_mixin_.PrepareOfflineGaiaLogin();
 }
 
-// Flaking: https://crbug.com/1023591
 IN_PROC_BROWSER_TEST_F(LoginTest, GaiaAuthOffline) {
   offline_gaia_test_mixin_.GoOffline();
-  offline_gaia_test_mixin_.SignIn(test_user_.account_id, kPassword);
+  offline_gaia_test_mixin_.InitOfflineLogin(test_user_.account_id, kPassword);
+  offline_gaia_test_mixin_.CheckManagedStatus(false);
+  offline_gaia_test_mixin_.SubmitGaiaAuthOfflineForm(
+      test_user_.account_id.GetUserEmail(), kPassword,
+      true /* wait for sign-in */);
   TestSystemTrayIsVisible(false);
 }
 
