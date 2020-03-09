bc566faaf12768d6d4b2032fa87ae3ff6b67eb47
Antonio Gomes <tonikitoo@igalia.com>
2020-03-09 21:31:15 +0000

    [mac] Stop calling EnablePreferredSizeMode() in WebContentsViewMac
    EnablePreferredSizeMode() was enabled for the old cocoa (non-views)
    based mac UI. This code is no longer needed after [1].
    
    What is more, with recent conversion of ViewMsg_EnablePreferredSizeChangedMode
    to mojo [2], we actually fixed the fact that EnablePreferredSizeMode() was being
    called to provisional RenderView instances, and the mode was not
    being effectively transferred when the RenderView instanced transitioned
    to committed state. Prior to [2], then, the mode was simply not working.
    
    However, now that we made calls to EnablePreferredSizeMode() effective
    on Mac for provisional (and committed) RenderViews, a performance drop was
    observed on loading.desktop benchmark - see [2].
    
    [1] https://chromium.googlesource.com/chromium/src/+/1515d1b6e029d3071b15ae2e89991de62db890e3
    [2] https://crrev.com/c/2068699
    [3] https://chromeperf.appspot.com/group_report?bug_id=1058975
    
    This CL fixes the perf regression by removing calling
    EnablePreferredSizeMode() on Mac for provisional RenderViews.
    
    As a follow up WebContentsView::RenderViewCreated() declaration and
    overrides can be removed altogether.
    
    BUG=1058975, 1051155
    R=dtapuska@chromium.org
    
    Change-Id: I14be481949b7e11b6eeba8fe9ac383729ec994b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2092096
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#748386}

--- content/browser/renderer_host/render_view_host_impl.cc
+++ content/browser/renderer_host/render_view_host_impl.cc
@@ -401,9 +401,6 @@ bool RenderViewHostImpl::CreateRenderView(
 void RenderViewHostImpl::SetMainFrameRoutingId(int routing_id) {
   main_frame_routing_id_ = routing_id;
   GetWidget()->UpdatePriority();
-
-  if (enabled_preferred_size_mode_)
-    EnablePreferredSizeMode();
 }
 
 // TODO(https://crbug.com/1006814): Delete this.
@@ -999,7 +996,6 @@ void RenderViewHostImpl::EnablePreferredSizeMode() {
         ->GetAssociatedLocalMainFrame()
         ->EnablePreferredSizeChangedMode();
   }
-  enabled_preferred_size_mode_ = true;
 }
 
 void RenderViewHostImpl::ExecutePluginActionAtLocation(
