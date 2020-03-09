a479c4be954b5099035a255f8de93716fabaa699
Michael Crouse <mcrouse@chromium.org>
2020-03-09 16:35:39 +0000

    Opt-in UKM for XSrcsetDescriptor and WSrcsetDescriptor.
    Enabled UKM collection for the XSrcset and WSrcset tags for image
    srcset. See Opt-in instructions here:
    https://cs.chromium.org/chromium/src/docs/use_counter_wiki.md
    
    Srcset usage is fairly low but is likely above the 1% recommended
    threshold. However, I think several of the other features counted
    are also higher. Would like to have this available as an experiment
    for using lower resolution image versions for data savers is run.
    
    Bug: 1059444
    Change-Id: Id2fba387d85f2c6d8537002d8cf5f6b0e06e36dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2092184
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748246}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -126,6 +126,8 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kRequestedFileSystemTemporary,
           WebFeature::kRequestedFileSystemPersistent,
           WebFeature::kSMSReceiverStart,
+          WebFeature::kSrcsetWDescriptor,
+          WebFeature::kSrcsetXDescriptor,
           WebFeature::kRequestFileSystem,
           WebFeature::kBaseElement,
           WebFeature::kBaseWithCrossOriginHref,
