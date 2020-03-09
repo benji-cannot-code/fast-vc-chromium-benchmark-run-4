e62cb2b8576e24de83205eb68f77f994ffc2a8e6
Adrienne Walker <enne@chromium.org>
2020-03-09 20:12:54 +0000

    indexeddb: pre-fix ambiguities when moving idb to storage service
    This function collides with the quota StatusCallback, so rename
    it to avoid any issues and to make the move cl easier to read.
    
    Bug: 1015214
    Change-Id: I4f82b843ccd27561ed1fb27e76a38b72cf0d2d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2092931
    Auto-Submit: enne <enne@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748349}

--- content/browser/indexed_db/indexed_db_dispatcher_host_unittest.cc
+++ content/browser/indexed_db/indexed_db_dispatcher_host_unittest.cc
@@ -136,9 +136,9 @@ struct TestDatabaseConnection {
   DISALLOW_COPY_AND_ASSIGN(TestDatabaseConnection);
 };
 
-void StatusCallback(base::OnceClosure callback,
-                    blink::mojom::IDBStatus* status_out,
-                    blink::mojom::IDBStatus status) {
+void TestStatusCallback(base::OnceClosure callback,
+                        blink::mojom::IDBStatus* status_out,
+                        blink::mojom::IDBStatus status) {
   *status_out = status;
   std::move(callback).Run();
 }
@@ -611,7 +611,7 @@ TEST_F(IndexedDBDispatcherHostTest, DISABLED_CompactDatabaseWithConnection) {
 
         connection->version_change_transaction->Commit(0);
         idb_mojo_factory_->AbortTransactionsAndCompactDatabase(base::BindOnce(
-            &StatusCallback, std::move(quit_closure2), &callback_result));
+            &TestStatusCallback, std::move(quit_closure2), &callback_result));
       }));
   loop2.Run();
   EXPECT_EQ(blink::mojom::IDBStatus::OK, callback_result);
@@ -688,7 +688,7 @@ TEST_F(IndexedDBDispatcherHostTest, CompactDatabaseWhileDoingTransaction) {
             kObjectStoreId, base::UTF8ToUTF16(kObjectStoreName),
             blink::IndexedDBKeyPath(), false);
         idb_mojo_factory_->AbortTransactionsAndCompactDatabase(base::BindOnce(
-            &StatusCallback, std::move(quit_closure2), &callback_result));
+            &TestStatusCallback, std::move(quit_closure2), &callback_result));
       }));
   loop2.Run();
 
@@ -761,7 +761,7 @@ TEST_F(IndexedDBDispatcherHostTest, CompactDatabaseWhileUpgrading) {
         ASSERT_TRUE(connection->database.is_bound());
         ASSERT_TRUE(connection->version_change_transaction.is_bound());
         idb_mojo_factory_->AbortTransactionsAndCompactDatabase(base::BindOnce(
-            &StatusCallback, std::move(quit_closure2), &callback_result));
+            &TestStatusCallback, std::move(quit_closure2), &callback_result));
       }));
   loop2.Run();
 
@@ -836,7 +836,7 @@ TEST_F(IndexedDBDispatcherHostTest,
         ASSERT_TRUE(connection->version_change_transaction.is_bound());
         connection->version_change_transaction->Commit(0);
         idb_mojo_factory_->AbortTransactionsForDatabase(base::BindOnce(
-            &StatusCallback, std::move(quit_closure2), &callback_result));
+            &TestStatusCallback, std::move(quit_closure2), &callback_result));
       }));
   loop2.Run();
 
@@ -915,7 +915,7 @@ TEST_F(IndexedDBDispatcherHostTest, AbortTransactionsWhileDoingTransaction) {
             kObjectStoreId, base::UTF8ToUTF16(kObjectStoreName),
             blink::IndexedDBKeyPath(), false);
         idb_mojo_factory_->AbortTransactionsForDatabase(base::BindOnce(
-            &StatusCallback, std::move(quit_closure2), &callback_result));
+            &TestStatusCallback, std::move(quit_closure2), &callback_result));
       }));
   loop2.Run();
 
@@ -989,7 +989,7 @@ TEST_F(IndexedDBDispatcherHostTest, AbortTransactionsWhileUpgrading) {
         ASSERT_TRUE(connection->database.is_bound());
         ASSERT_TRUE(connection->version_change_transaction.is_bound());
         idb_mojo_factory_->AbortTransactionsForDatabase(base::BindOnce(
-            &StatusCallback, std::move(quit_closure2), &callback_result));
+            &TestStatusCallback, std::move(quit_closure2), &callback_result));
       }));
   loop2.Run();
 
