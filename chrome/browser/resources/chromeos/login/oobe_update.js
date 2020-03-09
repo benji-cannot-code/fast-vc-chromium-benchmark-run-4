078f9bc7b796c39d3aa93111714f4fe4fa72fde6
Saurabh Nijhara <snijhara@google.com>
2020-03-09 20:18:04 +0000

    Using checking-downloading-update polymer element for Update Required Screen
    This CL makes the the update required screen use the checking-
    downloading-update polymer element so that the UI for this process
    remains in sync with the Update Screen which also uses the same element.
    A new property for controlling the download message in the dialog is
    added in the element as it is different for the UpdateRequiredScreen as
    per the UI mocks.
    
    Bug: 1048607
    Change-Id: I9a9c26f10eff2b64602ffb59b1da1f61d4278b3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2078575
    Commit-Queue: Saurabh Nijhara <snijhara@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748350}

--- chrome/browser/resources/chromeos/login/oobe_update.js
+++ chrome/browser/resources/chromeos/login/oobe_update.js
@@ -39,10 +39,11 @@ Polymer({
     },
 
     /**
-     * Message "3 minutes left".
+     * Estimated time left in seconds.
      */
     estimatedTimeLeft: {
-      type: String,
+      type: Number,
+      value: 0,
     },
 
     /**
@@ -57,13 +58,6 @@ Polymer({
      */
     progressMessage: {
       type: String,
-    },
-
-    /**
-     * Shows progressMessage.
-     */
-    progressMessageShown: {
-      type: Boolean,
     },
 
     /**
