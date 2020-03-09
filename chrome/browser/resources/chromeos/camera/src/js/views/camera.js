be2075d07728bd4396d7a55afe889b0febe244b3
Kuo Jen Wei <inker@google.com>
2020-03-09 09:52:19 +0000

    [CCA] Fix switch resolution race.
    The switch resolution race is caused by logic of enabling switching
    options right after STREAMING ready and before camera configuration
    finishing. This CL fix the race by introducing new CAMERA_CONFIGURING
    state will be flagged during the camera stream reconfiguration process.
    
    Bug: b/147567355
    Test: Manually test clicking on the resolution options when it's
    clickable and won't jump back to previous option after clicked.
    
    Change-Id: I32153ff78bc1bc6a3478225479e289e5617851a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082665
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Kuo Jen Wei <inker@chromium.org>
    Auto-Submit: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748148}

--- chrome/browser/resources/chromeos/camera/src/js/views/camera.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/camera.js
@@ -352,6 +352,7 @@ export class Camera extends View {
         return false;
       }
     }
+    state.set(state.State.CAMERA_CONFIGURING, true);
     this.configuring_ = (async () => {
       try {
         if (state.get(state.State.TAKING)) {
@@ -468,6 +469,7 @@ export class Camera extends View {
         throw new CameraSuspendedError();
       });
       this.configuring_ = null;
+      state.set(state.State.CAMERA_CONFIGURING, false);
 
       return true;
     } catch (error) {
