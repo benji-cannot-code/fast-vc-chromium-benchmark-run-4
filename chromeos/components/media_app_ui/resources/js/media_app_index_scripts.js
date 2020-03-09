1a53859e54435c678123c68c6d4d53f9934a4a87
Zain Afzal <zafzal@google.com>
2020-03-09 02:27:37 +0000

    Moved index.html and app.html scripts into bundled files.
    Previously for every js file we needed in index.html or app.html we would
    manually add a .grd entry, then an addResourcePath call and then a script
    tag in the relevant html file. This was tedious and cluttered up our
    html and cc files.
    This CL adds single js files which bundle together the scripts we need
    in index/app.html, adding more js sources now just involves adding a
    
    `// include<>`
    
    to the relevant bundle file (i.e media_app_(app|index)_scripts.js).
    
    Bug: 1059134, 996088
    Change-Id: I972f538b8414b381529543b8d4692cf6c5390328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093957
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Cr-Commit-Position: refs/heads/master@{#748087}

---
+++ chromeos/components/media_app_ui/resources/js/media_app_index_scripts.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @fileoverview Concatenation of the JS files we use in index.html. */
+
+// <include src="message_pipe.js">
+// <include src="launch.js">
