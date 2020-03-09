40cb590d93144281cf85998def85df4bfad23987
Alice Wang <aliceywang@chromium.org>
2020-03-09 16:11:14 +0000

    [Signin] Remove AndroidTimeBetweenUpdateAccountList recording code
    This CL removes the code that records the histogram
    Signin.AndroidTimeBetweenUpdateAccountList.
    
    Bug: 1048211
    Change-Id: I6948967acd281c921fa4b76aa62c598fafc82d60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093217
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748234}

--- components/signin/internal/identity_manager/profile_oauth2_token_service_delegate_android.h
+++ components/signin/internal/identity_manager/profile_oauth2_token_service_delegate_android.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 #include "base/callback.h"
 #include "base/macros.h"
-#include "base/time/time.h"
 #include "components/signin/internal/identity_manager/account_tracker_service.h"
 #include "components/signin/internal/identity_manager/profile_oauth2_token_service.h"
 #include "components/signin/internal/identity_manager/profile_oauth2_token_service_delegate.h"
@@ -144,7 +143,6 @@ class ProfileOAuth2TokenServiceDelegateAndroid
 
   AccountTrackerService* account_tracker_service_;
   RefreshTokenLoadStatus fire_refresh_token_loaded_;
-  base::Time last_update_accounts_time_;
 
   // For testing, disables the creation of the java counterpart, see
   // set_disable_interaction_with_system_accounts().
