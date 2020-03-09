20b201fbdd6045ac3327a2c5726ea59823d7fdc7
Victor Costan <pwnall@chromium.org>
2020-03-09 18:52:15 +0000

    WebLocks: Minor improvements in browser-side implementation.
    Change-Id: I366028613a73736af28799440cf87bfac5af3aae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093551
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748318}

--- content/browser/locks/lock_manager.h
+++ content/browser/locks/lock_manager.h
@@ -3,13 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_LOCKS_LOCK_CONTEXT_H_
-#define CONTENT_BROWSER_LOCKS_LOCK_CONTEXT_H_
+#ifndef CONTENT_BROWSER_LOCKS_LOCK_MANAGER_H_
+#define CONTENT_BROWSER_LOCKS_LOCK_MANAGER_H_
 
+#include <cstdint>
+#include <map>
 #include <memory>
 #include <string>
 
 #include "base/callback.h"
+#include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
 #include "content/public/browser/browser_context.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
@@ -25,6 +28,9 @@ class LockManager : public blink::mojom::LockManager {
  public:
   LockManager();
   ~LockManager() override;
+
+  LockManager(const LockManager&) = delete;
+  LockManager& operator=(const LockManager&) = delete;
 
   // Binds |receiver| to this LockManager. |receiver| belongs to a frame or
   // worker at |origin| hosted by |render_process_id|. If it belongs to a frame,
@@ -74,10 +80,8 @@ class LockManager : public blink::mojom::LockManager {
 
   SEQUENCE_CHECKER(sequence_checker_);
   base::WeakPtrFactory<LockManager> weak_ptr_factory_{this};
-
-  DISALLOW_COPY_AND_ASSIGN(LockManager);
 };
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_LOCKS_LOCK_CONTEXT_H
+#endif  // CONTENT_BROWSER_LOCKS_LOCK_MANAGER_H
