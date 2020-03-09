e05cb9153373f340391e938c9d9e78a08d13a50d
Adam Langley <agl@chromium.org>
2020-03-09 19:13:48 +0000

    Add sketch of caBLE v2 authenticator support.
    Not currently wired up from anywhere, full of TODOs, and not yet plumbed
    into the GMS FIDO APIs and thus non-functional.
    
    BUG=1002262
    
    Change-Id: I65a01b6cf38ce9897dc83310f3af6c6b3c0c6e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079613
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748329}

---
+++ chrome/android/features/cablev2_authenticator/factory/dummy/java/src/org/chromium/chrome/browser/webauth/authenticator/CableAuthenticatorUIFactory.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webauth.authenticator;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Use {@link #createComponent()} to instantiate a {@link ManualFillingComponent}.
+ */
+public class CableAuthenticatorUIFactory {
+    private CableAuthenticatorUIFactory() {}
+
+    /**
+     * Returns whether this feature is available in the current build.
+     *
+     * If false, no other methods of this class should be called.
+     */
+    public static boolean isAvailable() {
+        return false;
+    }
+
+    /**
+     * Returns the class of {@link CableAuthenticatorUI}.
+     */
+    public static Class<? extends Fragment> getFragmentClass() {
+        return null;
+    }
+}
