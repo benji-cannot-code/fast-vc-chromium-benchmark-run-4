c2b63a323c34ab389bc1915dbbe273974e8f0995
Jia <jiameng@chromium.org>
2020-03-09 00:11:47 +0000

    [cros search service] Add case-insensitive exact match to TokenizedStringMatch
    We're consolidating exact & fuzzy string match to be both supported by
    the search service. In this cl, we add case-insensitive exact match to
    TokenizedStringMatch and remove it from app_search_provider. Note, fuzzy
    string match already performs case-insensitive exact match internally.
    
    Bug: 1018613
    Change-Id: Ia7d17352e3b263ab09fac1e590748a08d7b16eb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089534
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748068}

--- chrome/common/string_matching/tokenized_string_match.cc
+++ chrome/common/string_matching/tokenized_string_match.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/string_search.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/strings/string_util.h"
 #include "chrome/common/string_matching/tokenized_string_char_iterator.h"
 
 namespace {
@@ -195,6 +196,19 @@ bool TokenizedStringMatch::Calculate(const TokenizedString& query,
                                      const TokenizedString& text) {
   relevance_ = kNoMatchScore;
   hits_.clear();
+
+  // If there is an exact match, relevance will be 1.0 and there is only 1 hit
+  // that is the entire text/query.
+  const auto& query_text = query.text();
+  const auto& text_text = text.text();
+  const auto query_size = query_text.size();
+  const auto text_size = text_text.size();
+  if (query_size > 0 && query_size == text_size &&
+      base::EqualsCaseInsensitiveASCII(query_text, text_text)) {
+    hits_.push_back(gfx::Range(0, query_size));
+    relevance_ = 1.0;
+    return true;
+  }
 
   PrefixMatcher matcher(query, text);
   if (matcher.Match()) {
