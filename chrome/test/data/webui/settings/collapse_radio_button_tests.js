16ee13c0fd1ec37ecd74fc9e9d2283e78c011e7f
sauski <sauski@google.com>
2020-03-09 21:54:43 +0000

    Safebrowsing radio toggle addition to the security page
    Adding a new collapsible radio toggle to the security page that includes a third safebrowsing option (enhanced), with more thorough explanations for this third option and the already available standard and disabled options.
    
    Bug: 1032584
    Change-Id: I6e4732ae464120d3d264b141dbb172aa125f9824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2088851
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Auto-Submit: Sean Harrison <harrisonsean@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748394}

--- chrome/test/data/webui/settings/collapse_radio_button_tests.js
+++ chrome/test/data/webui/settings/collapse_radio_button_tests.js
@@ -15,10 +15,6 @@ suite('CrCollapseRadioButton', function() {
     Polymer.dom.flush();
   });
 
-  teardown(function() {
-    collapseRadioButton.remove();
-  });
-
   test('openOnSelection', function() {
     const collapse = collapseRadioButton.$$('iron-collapse');
     collapseRadioButton.checked = false;
