389530d5356681c5c285d86f5debf4a874c674ea
Christian Dullweber <dullweber@chromium.org>
2020-03-10 12:12:34 +0000

    Align cookie controls style with permission rows
    Use MediumThick style for permissions and cookie controls.
    Use tinted icon to align the cookie controls UI with the permission
    UI above.
    
    Screenshot: https://crbug.com/1040091#c22
    Bug: 1040091
    Change-Id: Ie0d2abced7dc63e8c73de9240444cd4215c81044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2091347
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748643}

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/PermissionParamsListBuilder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/PermissionParamsListBuilder.java
@@ -11,7 +11,7 @@ import android.content.pm.PackageManager;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
-import android.text.style.StyleSpan;
+import android.text.style.TextAppearanceSpan;
 
 import androidx.core.app.NotificationManagerCompat;
 
@@ -139,8 +139,9 @@ class PermissionParamsListBuilder {
 
         SpannableStringBuilder builder = new SpannableStringBuilder();
         SpannableString nameString = new SpannableString(permission.name);
-        final StyleSpan boldSpan = new StyleSpan(android.graphics.Typeface.BOLD);
-        nameString.setSpan(boldSpan, 0, nameString.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        final TextAppearanceSpan span =
+                new TextAppearanceSpan(mContext, R.style.TextAppearance_TextMediumThick_Primary);
+        nameString.setSpan(span, 0, nameString.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
 
         builder.append(nameString);
         builder.append(" – "); // en-dash.
