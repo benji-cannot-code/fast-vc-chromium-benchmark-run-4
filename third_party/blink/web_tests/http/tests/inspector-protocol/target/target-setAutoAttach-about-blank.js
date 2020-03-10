c2f206b72ae0c23c5a5081e93c627e68f5f8e889
Yury Semikhatsky <yurys@chromium.org>
2020-03-10 19:44:12 +0000

    DevTools: fix style nits in auto-attach tests
    This is a follow-up fix to address comments from https://crrev.com/c/2042302
    
    Bug: 1051687
    Change-Id: Id6bc1ed057b85d8ed66b42378825a47ce7775170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071360
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Yury Semikhatsky <yurys@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748838}

--- third_party/blink/web_tests/http/tests/inspector-protocol/target/target-setAutoAttach-about-blank.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/target/target-setAutoAttach-about-blank.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
-  var {page, session, dp} = await testRunner.startBlank(
+  await testRunner.startBlank(
       `Tests that browser.Target.setAutoAttach() attaches to new about:blank page.`);
 
   const target = testRunner.browserP().Target;
--- third_party/blink/web_tests/http/tests/inspector-protocol/target/target-setAutoAttach-windowOpen.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/target/target-setAutoAttach-windowOpen.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
-  var {page, session, dp} = await testRunner.startBlank(
+  const {page, session, dp} = await testRunner.startBlank(
       `Tests that browser.Target.setAutoAttach() attaches to window.open targets.`);
 
   const target = testRunner.browserP().Target;
