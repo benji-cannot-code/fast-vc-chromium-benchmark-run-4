ed8fa2698fdc62b066c647625c9cd43b1d3e27da
Wenyu Fu <wenyufu@google.com>
2020-03-10 19:26:58 +0000

    [HomepageConversion] Default enable expriment flag
    Set HomepageConversion expriment flag to default enabled for the
    waterfall rollout.
    
    Bug: 1036470, 1054616
    Change-Id: I0cf98d4be83ebc5e233a89cdbeb88ad6b9246288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096094
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748827}

--- chrome/android/javatests/src/org/chromium/chrome/browser/homepage/HomepagePolicyIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/homepage/HomepagePolicyIntegrationTest.java
@@ -42,6 +42,7 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.chrome.test.util.browser.Features;
+import org.chromium.chrome.test.util.browser.Features.DisableFeatures;
 import org.chromium.chrome.test.util.browser.TabLoadObserver;
 import org.chromium.content_public.browser.test.util.Criteria;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
@@ -178,6 +179,7 @@ public class HomepagePolicyIntegrationTest {
     @Test
     @MediumTest
     @Feature({"Homepage"})
+    @DisableFeatures(ChromeFeatureList.HOMEPAGE_SETTINGS_UI_CONVERSION)
     public void testHomepagePreference() {
         // Launch homepage preference page
         SettingsActivity homepagePreferenceActivity =
