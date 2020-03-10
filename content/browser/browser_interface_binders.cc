bda66b1ddbee554d8c18ac9779eb6f835d110060
Oksana Zhuravlova <oksamyt@chromium.org>
2020-03-10 02:38:29 +0000

    [mojo] Add an empty binder for TextSuggestionHost for non-Android
    Since blink unconditionally requests the TextSuggestionHost interface,
    and the browser only binds it on Android, this change adds an empty
    binder for other OSes.
    
    Bug: 1059728
    Change-Id: Ic9f53b17d9f2abb3d155dd85db098d3b51bfe02d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093913
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748494}

--- content/browser/browser_interface_binders.cc
+++ content/browser/browser_interface_binders.cc
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/geolocation/geolocation_service.mojom.h"
 #include "third_party/blink/public/mojom/idle/idle_manager.mojom.h"
 #include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom.h"
+#include "third_party/blink/public/mojom/input/input_host.mojom.h"
 #include "third_party/blink/public/mojom/insecure_input/insecure_input_service.mojom.h"
 #include "third_party/blink/public/mojom/keyboard_lock/keyboard_lock.mojom.h"
 #include "third_party/blink/public/mojom/loader/navigation_predictor.mojom.h"
@@ -124,7 +125,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/renderer_host/render_widget_host_view_android.h"
 #include "services/device/public/mojom/nfc.mojom.h"
 #include "third_party/blink/public/mojom/hid/hid.mojom.h"
-#include "third_party/blink/public/mojom/input/input_host.mojom.h"
 #include "third_party/blink/public/mojom/unhandled_tap_notifier/unhandled_tap_notifier.mojom.h"
 #endif
 
@@ -727,6 +727,10 @@ void PopulateBinderMapWithContext(
       base::BindRepeating(&BindDateTimeChooserForFrame));
   map->Add<blink::mojom::TextSuggestionHost>(
       base::BindRepeating(&BindTextSuggestionHostForFrame));
+#else
+  // TODO(crbug.com/1060004): add conditions on the renderer side instead.
+  map->Add<blink::mojom::TextSuggestionHost>(base::BindRepeating(
+      &EmptyBinderForFrame<blink::mojom::TextSuggestionHost>));
 #endif  // defined(OS_ANDROID)
 
   map->Add<blink::mojom::ClipboardHost>(
