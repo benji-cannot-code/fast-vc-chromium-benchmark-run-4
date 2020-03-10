cdc318950c730015cb48b36143df12312106e438
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-03-10 14:32:01 +0000

    Import wpt@30cc91f77bce732e9bf145afdc752b9c0918377b
    Using wpt-import in Chromium 29afd4ae2e1e7f383f585168c0bb11ad64b6c7b0.
    With Chromium commits locally applied on WPT:
    d5cd6b98a6 "SharedWorker: Assign unique names to SharedWorkers to avoid unintentional matching"
    acf3302037 "Add testing for the scrolling attribute"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I7df62572e5c012924124e00e09bf1775d8389c00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096509
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#748672}

--- third_party/blink/web_tests/external/wpt/webxr/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/webxr/idlharness.https.window.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 idl_test(
   ['webxr'],
-  ['webgl1', 'html', 'dom'],
+  ['permissions', 'webgl1', 'html', 'dom'],
   async idl_array => {
     idl_array.add_objects({
       Navigator: ['navigator'],
