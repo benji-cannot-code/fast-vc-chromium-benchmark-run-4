16c287064805e90bace6a3b537ecc7d5e5b84b97
Finnur Thorarinsson <finnur@chromium.org>
2020-03-10 10:57:40 +0000

    [Android] PhotoPicker: Re-enable failing test.
    The tests have changed a tiny bit and I can't repro the
    failure locally, despite repeated attempts.
    
    Bug: None
    Change-Id: I527562805cbe66bf3052d945fafac73206549e18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095120
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748622}

--- chrome/android/javatests/src/org/chromium/chrome/browser/photo_picker/DecoderServiceHostTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/photo_picker/DecoderServiceHostTest.java
@@ -19,7 +19,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.browser.ChromeActivity;
@@ -263,7 +262,6 @@ public class DecoderServiceHostTest implements DecoderServiceHost.DecoderStatusC
 
     @Test
     @LargeTest
-    @DisabledTest
     public void testCancelation() throws Throwable {
         DecoderServiceHost host = new DecoderServiceHost(this, mContext);
         host.bind(mContext);
