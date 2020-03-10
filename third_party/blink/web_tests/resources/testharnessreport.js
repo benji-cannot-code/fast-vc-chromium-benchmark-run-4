a3be2a5bc9c32f7eab9a1b2f85981dbe5ad230ed
Navid Zolghadr <nzolghadr@chromium.org>
2020-03-10 21:34:38 +0000

    Remove pointerlock folder from internal automation whitelist
    As pointerlock wpt tests are now all fully automated with testdriver
    APIs there is no need for internal automation whitelisting of
    pointerlock folder.
    
    Change-Id: Id06b0e49a99c95a7c3d1ffe4f8cee44362159d73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2097118
    Reviewed-by: Lan Wei <lanwei@chromium.org>
    Commit-Queue: Navid Zolghadr <nzolghadr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748906}

--- third_party/blink/web_tests/resources/testharnessreport.js
+++ third_party/blink/web_tests/resources/testharnessreport.js
@@ -121,7 +121,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             pathAndBase.startsWith('/css/') ||
             pathAndBase.startsWith('/pointerevents/') ||
             pathAndBase.startsWith('/uievents/') ||
-            pathAndBase.startsWith('/pointerlock/') ||
             pathAndBase.startsWith('/html/') ||
             pathAndBase.startsWith('/input-events/') ||
             pathAndBase.startsWith('/css/selectors/') ||
