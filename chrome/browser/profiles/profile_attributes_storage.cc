3e876033ab7ecbbad8e29e31a20e5300e6a01163
David Roger <droger@chromium.org>
2020-03-10 12:47:18 +0000

    [signin] Cleanup ProfileAvatarDownloader::FetchCompleteCallback
    This changes the callback from RepeatingCallback to OnceCallback,
    and removes the nested callback which was unnecessary and made the code
    hard to understand.
    
    Bug: 1041472
    Change-Id: If93482c0cfa9e676a0b35712bf545d33b60ada89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2094987
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748649}

--- chrome/browser/profiles/profile_attributes_storage.cc
+++ chrome/browser/profiles/profile_attributes_storage.cc
@@ -358,8 +358,9 @@ void ProfileAttributesStorage::DownloadHighResAvatar(
   std::unique_ptr<ProfileAvatarDownloader>& current_downloader =
       avatar_images_downloads_in_progress_[file_name];
   current_downloader.reset(new ProfileAvatarDownloader(
-      icon_index, base::Bind(&ProfileAttributesStorage::SaveAvatarImageAtPath,
-                             AsWeakPtr(), profile_path)));
+      icon_index,
+      base::BindOnce(&ProfileAttributesStorage::SaveAvatarImageAtPathNoCallback,
+                     AsWeakPtr(), profile_path)));
 
   current_downloader->Start();
 }
@@ -434,4 +435,13 @@ void ProfileAttributesStorage::OnAvatarPictureSaved(
     std::move(callback).Run();
 
   NotifyOnProfileHighResAvatarLoaded(profile_path);
+}
+
+void ProfileAttributesStorage::SaveAvatarImageAtPathNoCallback(
+    const base::FilePath& profile_path,
+    gfx::Image image,
+    const std::string& key,
+    const base::FilePath& image_path) {
+  SaveAvatarImageAtPath(profile_path, image, key, image_path,
+                        base::OnceClosure());
 }
