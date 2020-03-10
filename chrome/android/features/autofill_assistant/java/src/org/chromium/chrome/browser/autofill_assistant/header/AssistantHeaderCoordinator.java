ff201fb846caa883e817f9f4f14bde3c5f6c54f4
Tanmoy Mollik <triploblastic@chromium.org>
2020-03-10 10:09:34 +0000

    [Android] Remove ChromeSigninController from AssistantHeaderCoordinator
    Replace ChromeSigninController with IdentityManager in AssistantHeaderCoordinator
    
    Bug: 1058989
    Change-Id: I928b5c41f7a74aa2f410bce72c8a0bae1672adca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089784
    Reviewed-by: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748615}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/header/AssistantHeaderCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/header/AssistantHeaderCoordinator.java
@@ -14,8 +14,9 @@ import android.widget.ImageView;
 import org.chromium.chrome.autofill_assistant.R;
 import org.chromium.chrome.browser.autofill_assistant.header.AssistantHeaderViewBinder.ViewHolder;
 import org.chromium.chrome.browser.signin.DisplayableProfileData;
+import org.chromium.chrome.browser.signin.IdentityServicesProvider;
 import org.chromium.chrome.browser.signin.ProfileDataCache;
-import org.chromium.components.signin.ChromeSigninController;
+import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
 import java.util.Collections;
@@ -47,7 +48,8 @@ public class AssistantHeaderCoordinator implements ProfileDataCache.Observer {
                 R.dimen.autofill_assistant_profile_size);
         mProfileCache = new ProfileDataCache(context, imageSize);
         mProfileView = mView.findViewById(R.id.profile_image);
-        mSignedInAccountName = ChromeSigninController.get().getSignedInAccountName();
+        mSignedInAccountName = CoreAccountInfo.getEmailFrom(
+                IdentityServicesProvider.get().getIdentityManager().getPrimaryAccountInfo());
         setupProfileImage();
 
         // Bind view and mediator through the model.
