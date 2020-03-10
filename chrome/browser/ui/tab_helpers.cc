d656970957d35b3aca7d17d23fe2b697913dcf42
Hailey Wang <haileywang@google.com>
2020-03-10 20:36:30 +0000

    [PM] Set tab properties when creating a PageNode
    Tab properties will be used for upcoming Performance Manager session
    restore policy.
    
    Bug: 1059341
    Change-Id: I81214143b55d747cfbde02d6a6959f5c83c36323
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2091653
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/master@{#748864}

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -103,6 +103,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_pages/buildflags/buildflags.h"
 #include "components/password_manager/core/browser/password_manager.h"
 #include "components/performance_manager/embedder/performance_manager_registry.h"
+#include "components/performance_manager/public/decorators/tab_properties_decorator.h"
 #include "components/permissions/permission_request_manager.h"
 #include "components/subresource_filter/core/browser/subresource_filter_features.h"
 #include "components/tracing/common/tracing_switches.h"
@@ -268,6 +269,7 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   if (auto* performance_manager_registry =
           performance_manager::PerformanceManagerRegistry::GetInstance()) {
     performance_manager_registry->CreatePageNodeForWebContents(web_contents);
+    performance_manager::TabPropertiesDecorator::SetIsTab(web_contents, true);
   }
   permissions::PermissionRequestManager::CreateForWebContents(web_contents);
   // The PopupBlockerTabHelper has an implicit dependency on
