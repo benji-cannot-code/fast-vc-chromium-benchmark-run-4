a5fe25ba3ba60a0380723088969b0650ab66edfa
Joshua Pawlicki <waffles@chromium.org>
2020-03-10 18:08:51 +0000

    Revert "[Autofill Assistant] Added ShowCalendarPopup to UI framework."
    This reverts commit f440b571bb1750b9297e95e2e9efca42e301d386.
    
    Reason for revert: Breaks compile:
    
    [8419/52202] CXX obj/chrome/browser/browser/ui_controller_android_utils.o
    FAILED: obj/chrome/browser/browser/ui_controller_android_utils.o
    /b/s/w/ir/cache/goma/client/gomacc ../../third_party/llvm-build/Release+Asserts/bin/clang++ -MMD -MF...(too long)
    ../../chrome/browser/android/autofill_assistant/ui_controller_android_utils.cc:135:53: error: calling a private constructor of class 'JniIntWrapper'
    Java_AssistantDateTime_Constructor(env, value.year(), value.month(),
    ^
    ../../base/android/jni_int_wrapper.h:46:3: note: declared private here
    JniIntWrapper(long);
    ^
    1 error generated.
    
    Original change's description:
    > [Autofill Assistant] Added ShowCalendarPopup to UI framework.
    > 
    > This adds a new interaction to the framework, which shows a calendar
    > popup and lets the user set or clear a date from the specified range.
    > 
    > Bug: b/145043394
    > Change-Id: I739e6b5ccb5821352b1ce63e63d8573bdde74f8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093224
    > Commit-Queue: Clemens Arbesser <arbesser@google.com>
    > Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    > Cr-Commit-Position: refs/heads/master@{#748718}
    
    TBR=arbesser@google.com,marianfe@google.com,sandromaggi@google.com
    
    Change-Id: I3f9bfc40d3c3f7cec4118eb189fdf48709a6e62e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: b/145043394
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2094710
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748783}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/user_data/AssistantDateTime.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/user_data/AssistantDateTime.java
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.autofill_assistant.user_data;
 
-import org.chromium.base.annotations.CalledByNative;
-import org.chromium.base.annotations.JNINamespace;
-
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -21,7 +18,6 @@ import java.util.TimeZone;
  * Note that this class does not make any guarantees with respect to the validity of the represented
  * date/time.
  */
-@JNINamespace("autofill_assistant")
 public class AssistantDateTime {
     /** Year, e.g., 2019. */
     private int mYear;
@@ -36,7 +32,6 @@ public class AssistantDateTime {
     /** Second in [0-59]. */
     private int mSecond;
 
-    @CalledByNative
     public AssistantDateTime(int year, int month, int day, int hour, int minute, int second) {
         set(year, month, day, hour, minute, second);
     }
@@ -81,17 +76,14 @@ public class AssistantDateTime {
         return calendar.getTimeInMillis();
     }
 
-    @CalledByNative
     public int getYear() {
         return mYear;
     }
 
-    @CalledByNative
     public int getMonth() {
         return mMonth;
     }
 
-    @CalledByNative
     public int getDay() {
         return mDay;
     }
