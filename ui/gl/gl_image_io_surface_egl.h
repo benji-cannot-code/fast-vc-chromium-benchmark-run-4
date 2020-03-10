dce9bc51ce57947c2eb9eb44d508e47d53b57336
Jonah Ryan-Davis <jonahr@google.com>
2020-03-10 17:34:19 +0000

    Reland "Enable GL_TEXTURE_2D" for IO-Surface-backed textures on SwANGLE
    This is a reland of c00d0991f962c743b36d42a11180e4eeeef4152c
    
    I had added a target to "linux_symbols" that was unnecessary.
    
    > Enable GL_TEXTURE_2D for IO-Surface-backed textures on SwANGLE
    >
    > GL_ANGLE_rectangle_texture is unavailable on Swiftshader's Vulkan
    > frontend, so SwANGLE on Mac uses 2D textures instead.
    >
    > Bug: chromium:1015454
    > Change-Id: Id99850fbfae798b33eaff4f3a285deb043b4ae7c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2056931
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: ccameron <ccameron@chromium.org>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    > Cr-Commit-Position: refs/heads/master@{#748322}
    
    Bug: chromium:1015454
    Change-Id: Id198cfe72268cfa496e23bea190aea1893c6718c
    Tbr: dcheng@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095348
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    Reviewed-by: Jonah Ryan-Davis <jonahr@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748754}

--- ui/gl/gl_image_io_surface_egl.h
+++ ui/gl/gl_image_io_surface_egl.h
@@ -24,7 +24,7 @@ class GL_EXPORT GLImageIOSurfaceEGL : public GLImageIOSurface {
 
  protected:
   ~GLImageIOSurfaceEGL() override;
-  bool BindTexImageImpl(unsigned internalformat) override;
+  bool BindTexImageImpl(unsigned target, unsigned internalformat) override;
   bool CopyTexImage(unsigned target) override;
 
  private:
@@ -37,6 +37,7 @@ class GL_EXPORT GLImageIOSurfaceEGL : public GLImageIOSurface {
   EGLDisplay display_;
   EGLSurface pbuffer_;
   EGLConfig dummy_config_;
+  EGLint texture_target_;
   bool texture_bound_;
 };
 
