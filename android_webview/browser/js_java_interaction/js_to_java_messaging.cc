ebf4f3e26ba43db7de9584246b53c7fb89edc323
Shimi Zhang <ctzsm@chromium.org>
2020-03-10 19:46:26 +0000

    [JJI] Pass string "null" as origin for local file schemes
    We want to treat file: and content: schemes as "null" for
    onPostMessage(), this would inconsistent with window.origin result
    if setAllowFileAccessFileUrls() set to true, but that is a more general
    problem in Blink.
    
    Bug: 918065
    Change-Id: If639e3a8be42384258b45e787403f456dca6b3fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090920
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748842}

--- android_webview/browser/js_java_interaction/js_to_java_messaging.cc
+++ android_webview/browser/js_java_interaction/js_to_java_messaging.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "url/origin.h"
+#include "url/url_util.h"
 
 namespace android_webview {
 
@@ -55,10 +56,22 @@ void JsToJavaMessaging::PostMessage(
     int_ports[i] = ports[i].release().value();
   }
 
+  // We want to pass a string "null" for local file schemes, to make it
+  // consistent to the Blink side SecurityOrigin serialization. When both
+  // setAllow{File,Universal}AccessFromFileURLs are false, Blink::SecurityOrigin
+  // will be serialized as string "null" for local file schemes, but when
+  // setAllowFileAccessFromFileURLs is true, Blink::SecurityOrigin will be
+  // serialized as the scheme, which will be inconsistentt to this place. In
+  // this case we want to let developer to know that local files are not safe,
+  // so we still pass "null".
+  std::string origin_string =
+      base::Contains(url::GetLocalSchemes(), source_origin.scheme())
+          ? "null"
+          : source_origin.Serialize();
   JNIEnv* env = base::android::AttachCurrentThread();
   Java_WebMessageListenerHolder_onPostMessage(
       env, listener_ref_, base::android::ConvertUTF16ToJavaString(env, message),
-      base::android::ConvertUTF8ToJavaString(env, source_origin.Serialize()),
+      base::android::ConvertUTF8ToJavaString(env, origin_string),
       web_contents->GetMainFrame() == render_frame_host_,
       base::android::ToJavaIntArray(env, int_ports.data(), int_ports.size()),
       reply_proxy_->GetJavaPeer());
