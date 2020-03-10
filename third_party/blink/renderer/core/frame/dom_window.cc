cf35cf1f4d8f0ea843eb417170ccab6be5cc27cb
Anant Jain <anant90@gmail.com>
2020-03-10 23:20:15 +0000

    Fixes grammatical error in error message
    This change fixes Incorrect error message when trying to close
    a window that was not opened by the current script from
    "Scripts may close only the windows that were opened by it." to
    "Scripts may close only the windows that were opened by them."
    
    Bug: 1059970
    Change-Id: I1b9e3a459755958d320cd6cf07e647f0c4a30c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093160
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748967}

--- third_party/blink/renderer/core/frame/dom_window.cc
+++ third_party/blink/renderer/core/frame/dom_window.cc
@@ -337,7 +337,7 @@ void DOMWindow::Close(LocalDOMWindow* incumbent_window) {
         MakeGarbageCollected<ConsoleMessage>(
             mojom::ConsoleMessageSource::kJavaScript,
             mojom::ConsoleMessageLevel::kWarning,
-            "Scripts may close only the windows that were opened by it."));
+            "Scripts may close only the windows that were opened by them."));
     return;
   }
 
