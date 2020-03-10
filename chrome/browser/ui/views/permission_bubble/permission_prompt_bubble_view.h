f8b1f1f4ffd7ed84e59609195496eb8a641fb543
Yann Dago <ydago@chromium.org>
2020-03-10 16:47:20 +0000

    Add flash deprecation warning on permission prompt
    Screenshot: http://screen/h5v2MCq2nzc
    Bug: 1050721
    Change-Id: If946fd0077aa1db012f44359753f7a01fc6ecb11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2085753
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Kamila Hasanbega <hkamila@google.com>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748729}

--- chrome/browser/ui/views/permission_bubble/permission_prompt_bubble_view.h
+++ chrome/browser/ui/views/permission_bubble/permission_prompt_bubble_view.h
@@ -10,12 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "components/permissions/permission_prompt.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
+#include "ui/views/controls/button/button.h"
 
 class Browser;
 
+namespace views {
+class ImageButton;
+}
+
 // Bubble that prompts the user to grant or deny a permission request from a
 // website.
-class PermissionPromptBubbleView : public views::BubbleDialogDelegateView {
+class PermissionPromptBubbleView : public views::ButtonListener,
+                                   public views::BubbleDialogDelegateView {
  public:
   PermissionPromptBubbleView(Browser* browser,
                              permissions::PermissionPrompt::Delegate* delegate);
@@ -29,6 +35,9 @@ class PermissionPromptBubbleView : public views::BubbleDialogDelegateView {
   bool ShouldShowCloseButton() const override;
   base::string16 GetWindowTitle() const override;
   gfx::Size CalculatePreferredSize() const override;
+
+  // Button Listener
+  void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
  private:
   // Holds the string to be displayed as the origin of the permission prompt,
@@ -51,6 +60,8 @@ class PermissionPromptBubbleView : public views::BubbleDialogDelegateView {
 
   // The requesting domain's name or origin.
   const DisplayNameOrOrigin name_or_origin_;
+
+  views::ImageButton* learn_more_button_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(PermissionPromptBubbleView);
 };
