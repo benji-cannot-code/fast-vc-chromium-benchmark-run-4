c50b5ab3d9dec7e51950d45d9717c01514e92d6f
George Burgess IV <gbiv@chromium.org>
2020-03-10 21:17:45 +0000

    blink/html: convert an enum constant to a constexpr
    This constant is sometimes implicitly converted to a floating-point
    number, which clang complains about, since such conversions are often
    bugs.
    
    Using a `constexpr int` should be equivalent.
    
    Bug: 1058218
    Change-Id: Ic6cafe54b97053b841caaa094a400727eb4cebb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096097
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748893}

--- third_party/blink/renderer/core/html/parser/html_srcset_parser.h
+++ third_party/blink/renderer/core/html/parser/html_srcset_parser.h
@@ -42,7 +42,7 @@ namespace blink {
 
 class Document;
 
-enum { kUninitializedDescriptor = -1 };
+constexpr int kUninitializedDescriptor = -1;
 
 class DescriptorParsingResult {
   STACK_ALLOCATED();
