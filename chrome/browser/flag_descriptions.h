27a550be1fe25a091907faa953a396a8afbab532
Hesen Zhang <hesen@google.com>
2020-03-10 17:01:44 +0000

    Reland "[Prefetch notification service]: Java side and glue setup, feature flag."
    - fix the crash before loading native
    
    This is a reland of 4d4e25dd0b434b813218cf41a2f279fe10b3e856
    
    Original change's description:
    > [Prefetch notification service]: Java side and glue setup, feature flag.
    >
    > - Added a feature flag for the migration.
    >
    > - Added and hid new code path in OfflineNotificationBackgroundTask.
    >
    > - Created JNI bridge for gluing prefetch notification service in native.
    >
    > Bug: 1047037
    > Change-Id: If7b728609f86665cfe6b4c61f521f7362000b292
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082087
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Commit-Queue: Hesen Zhang <hesen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#747323}
    
    Bug: 1047037
    Change-Id: Ifb2d2b97a190c98b00ff2244c2f4d4ae879bdac9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095875
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748738}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1499,6 +1499,9 @@ extern const char kUpdateNotificationSchedulingIntegrationDescription[];
 extern const char kUpdateNotificationServiceImmediateShowOptionName[];
 extern const char kUpdateNotificationServiceImmediateShowOptionDescription[];
 
+extern const char kPrefetchNotificationSchedulingIntegrationName[];
+extern const char kPrefetchNotificationSchedulingIntegrationDescription[];
+
 extern const char kUsageStatsDescription[];
 extern const char kUsageStatsName[];
 
--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -205,6 +205,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kVideoPersistence,
     &kVrBrowsingFeedback,
     &kWebApkAdaptiveIcon,
+    &kPrefetchNotificationSchedulingIntegration,
     &net::features::kSameSiteByDefaultCookies,
     &net::features::kCookiesWithoutSameSiteMustBeSecure,
     &paint_preview::kPaintPreviewCaptureExperiment,
@@ -583,6 +584,10 @@ const base::Feature kUmaBackgroundSessions{"UMABackgroundSessions",
 
 const base::Feature kUpdateNotificationSchedulingIntegration{
     "UpdateNotificationSchedulingIntegration",
+    base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kPrefetchNotificationSchedulingIntegration{
+    "PrefetchNotificationSchedulingIntegration",
     base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kUpdateNotificationScheduleServiceImmediateShowOption{
