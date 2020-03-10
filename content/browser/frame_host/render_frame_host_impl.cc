8b4fe737d925f8855a149d17ba95bf755762fec5
Fergal Daly <fergal@chromium.org>
2020-03-10 16:40:41 +0000

    Stop passing widget routing ID to frame host constructor.
    Currently the ID is used only to signal that a widget exists, however
    this can be deduced by looking at the frame's parent (or lack of) and
    site instances.
    
    Previously the ID for this case was being generated in
    RenderFrameHostManager::CreateRenderFrame and passed all the way down.
    It's simpler to just generate one at the latest point possible.
    
    This is a pre-factor for https://crrev.com/c/2084038/.
    
    Change-Id: I5627e204c7afeab8b05bcb8a5f09a162e051c1c6
    Bug: 1059678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2089546
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#748724}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -869,7 +869,6 @@ RenderFrameHostImpl::RenderFrameHostImpl(
     FrameTree* frame_tree,
     FrameTreeNode* frame_tree_node,
     int32_t routing_id,
-    int32_t widget_routing_id,
     bool renderer_initiated_creation)
     : render_view_host_(std::move(render_view_host)),
       delegate_(delegate),
@@ -940,7 +939,12 @@ RenderFrameHostImpl::RenderFrameHostImpl(
       base::BindRepeating(&RenderFrameHostImpl::BeforeUnloadTimeout,
                           weak_ptr_factory_.GetWeakPtr())));
 
-  if (widget_routing_id != MSG_ROUTING_NONE) {
+  // Local roots are:
+  // - main frames; or
+  // - subframes in a different SiteInstance from their parent.
+  //
+  // Local roots require a RenderWidget for input/layout/painting.
+  if (!parent_ || IsCrossProcessSubframe()) {
     mojo::PendingRemote<mojom::Widget> widget;
     GetRemoteInterfaces()->GetInterface(
         widget.InitWithNewPipeAndPassReceiver());
@@ -951,18 +955,17 @@ RenderFrameHostImpl::RenderFrameHostImpl(
       // RenderWidgetHostImpl, the main render frame should probably start
       // owning the RenderWidgetHostImpl itself.
       DCHECK(GetLocalRenderWidgetHost());
-      DCHECK_EQ(GetLocalRenderWidgetHost()->GetRoutingID(), widget_routing_id);
       DCHECK(!GetLocalRenderWidgetHost()->owned_by_render_frame_host());
 
       // Make the RenderWidgetHostImpl able to call the mojo Widget interface
       // (implemented by the RenderWidgetImpl).
       GetLocalRenderWidgetHost()->SetWidget(std::move(widget));
     } else {
-      // For subframes, the RenderFrameHost directly creates and owns its
-      // RenderWidgetHost.
+      // For local child roots, the RenderFrameHost directly creates and owns
+      // its RenderWidgetHost.
+      int32_t widget_routing_id =
+          site_instance->GetProcess()->GetNextRoutingID();
       DCHECK_EQ(nullptr, GetLocalRenderWidgetHost());
-      DCHECK_EQ(nullptr, RenderWidgetHostImpl::FromID(GetProcess()->GetID(),
-                                                      widget_routing_id));
       owned_render_widget_host_ = RenderWidgetHostFactory::Create(
           frame_tree_->render_widget_delegate(), GetProcess(),
           widget_routing_id, std::move(widget), /*hidden=*/true);
--- content/test/test_render_frame_host_factory.h
+++ content/test/test_render_frame_host_factory.h
@@ -35,7 +35,6 @@ class TestRenderFrameHostFactory : public RenderFrameHostFactory {
       FrameTree* frame_tree,
       FrameTreeNode* frame_tree_node,
       int32_t routing_id,
-      int32_t widget_routing_id,
       bool renderer_initiated_creation) override;
 
  private:
