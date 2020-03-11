1c2bf87cddd0921b4648d4514b226a73b7397792
Ioana Pandele <ioanap@chromium.org>
2020-03-11 17:01:39 +0000

    Ensure that password accessory test doesn't check for the native library
    Currently, testSetsViewAdapterOnTabCreation fails because it reaches a
    feature check which needs the native library to be loaded. This should
    not happen in unit tests, so we explicitly set the feature for test for
    all test cases.
    
    Bug: 1060580
    Change-Id: I688b49b0df49e7262e186bda321f221b7d62be5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093444
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749211}

--- chrome/android/features/keyboard_accessory/junit/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/PasswordAccessorySheetControllerTest.java
+++ chrome/android/features/keyboard_accessory/junit/src/org/chromium/chrome/browser/keyboard_accessory/sheet_tabs/PasswordAccessorySheetControllerTest.java
@@ -76,6 +76,7 @@ public class PasswordAccessorySheetControllerTest {
 
     @Before
     public void setUp() {
+        setAutofillFeature(true);
         ShadowRecordHistogram.reset();
         MockitoAnnotations.initMocks(this);
         mocker.mock(RecordHistogramJni.TEST_HOOKS, mMockRecordHistogramNatives);
