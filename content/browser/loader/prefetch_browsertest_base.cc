e709279fc57f412c13b1536a11f4d932262bf594
Kamila Hasanbega <hkamila@chromium.org>
2020-03-11 08:21:52 +0000

    Revert "WebLocks: Remove unused include from lock_manager.h."
    This reverts commit 264654a0749006cc756035d9ec444d0afeebf36a.
    
    Reason for revert: (sheriff) tentative revert: tree is closed
    
    Original change's description:
    > WebLocks: Remove unused include from lock_manager.h.
    > 
    > //content/browser/locks/lock_manager.h included
    > //content/public/browser/browser_context.h, despite not using
    > BrowserContext at all.
    > 
    > Because lock_manager.h is included in storage_partition_impl.h,
    > browser_context.h was indirectly included in many files. This CL fixes
    > the IWYU violations highlighted by compiler errors.
    > 
    > Change-Id: I46462b337d93a7ae1d60648cfd617b2e69662637
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093553
    > Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    > Auto-Submit: Victor Costan <pwnall@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#749082}
    
    TBR=falken@chromium.org,pwnall@chromium.org
    
    Change-Id: I7079fb9abdad31a204ed8d97af7933ce8c95676f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095733
    Reviewed-by: Kamila Hasanbega <hkamila@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749085}

--- content/browser/loader/prefetch_browsertest_base.cc
+++ content/browser/loader/prefetch_browsertest_base.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/storage_partition_impl.h"
 #include "content/browser/web_package/signed_exchange_handler.h"
 #include "content/browser/web_package/signed_exchange_loader.h"
-#include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
