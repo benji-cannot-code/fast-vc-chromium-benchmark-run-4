37128df56f9a9b76df4f7ba3ea61feac76894d3d
Yoshifumi Inoue <yosin@chromium.org>
2020-03-11 09:50:39 +0000

    [NGFragmentItem] Stop using rounded rect for link highlight when <a> has nested inline
    This patch changes |LinkHighlightImpl::Paint()| to use squesr rect instead of
    rounded rect when <a> has nested inline box, e.g. <a>ABC<b>DEF</b></a> as
    same as |NGPaintFragment|.
    
    This patch makes gesture-tapHighlight-simple-nested.html test to pass.
    
    Bug: 982194
    Change-Id: I824b6833223093892eb1aaa9936fa14c27c76028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2097777
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749101}

--- third_party/blink/renderer/core/paint/link_highlight_impl.cc
+++ third_party/blink/renderer/core/paint/link_highlight_impl.cc
@@ -47,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
 #include "third_party/blink/renderer/core/layout/layout_box_model_object.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.h"
+#include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h"
 #include "third_party/blink/renderer/core/paint/compositing/composited_layer_mapping.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
@@ -287,6 +289,20 @@ void LinkHighlightImpl::Paint(GraphicsContext& context) {
         fragment->PaintOffset(), NGOutlineType::kIncludeBlockVisualOverflow);
     if (rects.size() > 1)
       use_rounded_rects = false;
+
+    // TODO(yosin): We should remove following if-statement once we release
+    // NGFragmentItem to renderer rounded rect even if nested inline, e.g.
+    // <a>ABC<b>DEF</b>GHI</a>.
+    // See gesture-tapHighlight-simple-nested.html
+    if (RuntimeEnabledFeatures::LayoutNGFragmentItemEnabled() &&
+        use_rounded_rects && object->IsLayoutInline()) {
+      NGInlineCursor cursor;
+      cursor.MoveTo(*object);
+      // When |LayoutInline| has more than one children, we render square
+      // rectangle as |NGPaintFragment|.
+      if (cursor && cursor.CurrentItem()->DescendantsCount() > 2)
+        use_rounded_rects = false;
+    }
 
     Path new_path;
     for (auto& rect : rects) {
