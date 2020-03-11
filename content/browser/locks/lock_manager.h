6313f76e4435a0a1e116baa71504c158f48de6a1
Victor Costan <pwnall@chromium.org>
2020-03-11 08:06:41 +0000

    WebLocks: Remove unused include from lock_manager.h.
    //content/browser/locks/lock_manager.h included
    //content/public/browser/browser_context.h, despite not using
    BrowserContext at all.
    
    Because lock_manager.h is included in storage_partition_impl.h,
    browser_context.h was indirectly included in many files. This CL fixes
    the IWYU violations highlighted by compiler errors.
    
    Change-Id: I46462b337d93a7ae1d60648cfd617b2e69662637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093553
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749082}

--- content/browser/locks/lock_manager.h
+++ content/browser/locks/lock_manager.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
-#include "content/public/browser/browser_context.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "third_party/blink/public/mojom/locks/lock_manager.mojom.h"
 #include "url/origin.h"
