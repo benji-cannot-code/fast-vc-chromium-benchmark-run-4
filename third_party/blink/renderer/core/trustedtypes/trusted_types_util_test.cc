e43c1391f12c053a4d3e0768aee117ae3c86f92a
Stefano Sanfilippo <ssanfilippo@chromium.org>
2020-03-11 15:48:49 +0000

    [Trusted Types] Clean up uses of "trusted-types *".
    Change-Id: Id71c80eb058ddd35ecda8156a2faf742d25faf24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095536
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Commit-Queue: Stefano Sanfilippo <ssanfilippo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749182}

--- third_party/blink/renderer/core/trustedtypes/trusted_types_util_test.cc
+++ third_party/blink/renderer/core/trustedtypes/trusted_types_util_test.cc
@@ -24,9 +24,6 @@ namespace blink {
 void TrustedTypesCheckForHTMLThrows(const String& string) {
   auto dummy_page_holder = std::make_unique<DummyPageHolder>(IntSize(800, 600));
   Document& document = dummy_page_holder->GetDocument();
-  document.GetContentSecurityPolicy()->DidReceiveHeader(
-      "trusted-types *", network::mojom::ContentSecurityPolicyType::kEnforce,
-      network::mojom::ContentSecurityPolicySource::kMeta);
   V8TestingScope scope;
   DummyExceptionStateForTesting exception_state;
   ASSERT_FALSE(exception_state.HadException());
@@ -47,9 +44,6 @@ void TrustedTypesCheckForHTMLThrows(const String& string) {
 void TrustedTypesCheckForScriptThrows(const String& string) {
   auto dummy_page_holder = std::make_unique<DummyPageHolder>(IntSize(800, 600));
   Document& document = dummy_page_holder->GetDocument();
-  document.GetContentSecurityPolicy()->DidReceiveHeader(
-      "trusted-types *", network::mojom::ContentSecurityPolicyType::kEnforce,
-      network::mojom::ContentSecurityPolicySource::kMeta);
   V8TestingScope scope;
   DummyExceptionStateForTesting exception_state;
   ASSERT_FALSE(exception_state.HadException());
@@ -72,9 +66,6 @@ void TrustedTypesCheckForScriptThrows(const String& string) {
 void TrustedTypesCheckForScriptURLThrows(const String& string) {
   auto dummy_page_holder = std::make_unique<DummyPageHolder>(IntSize(800, 600));
   Document& document = dummy_page_holder->GetDocument();
-  document.GetContentSecurityPolicy()->DidReceiveHeader(
-      "trusted-types *", network::mojom::ContentSecurityPolicyType::kEnforce,
-      network::mojom::ContentSecurityPolicySource::kMeta);
   V8TestingScope scope;
   DummyExceptionStateForTesting exception_state;
   ASSERT_FALSE(exception_state.HadException());
@@ -99,9 +90,6 @@ void TrustedTypesCheckForScriptWorks(
     String expected) {
   auto dummy_page_holder = std::make_unique<DummyPageHolder>(IntSize(800, 600));
   Document& document = dummy_page_holder->GetDocument();
-  document.GetContentSecurityPolicy()->DidReceiveHeader(
-      "trusted-types *", network::mojom::ContentSecurityPolicyType::kEnforce,
-      network::mojom::ContentSecurityPolicySource::kMeta);
   V8TestingScope scope;
   DummyExceptionStateForTesting exception_state;
   String s = TrustedTypesCheckForScript(
