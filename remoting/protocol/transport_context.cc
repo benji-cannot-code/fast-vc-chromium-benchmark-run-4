39b8b03270cdeee5bed8db2a3cbcf236705321eb
Joe Downing <joedow@google.com>
2020-03-11 17:22:09 +0000

    Adding logging for TURN/STUN candidates
    Change-Id: I0d2e3211720c255647db43ec9bed24f17cf6c5d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096793
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749221}

--- remoting/protocol/transport_context.cc
+++ remoting/protocol/transport_context.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
+#include "remoting/base/logging.h"
 #include "remoting/base/url_request.h"
 #include "remoting/protocol/port_allocator_factory.h"
 #include "third_party/webrtc/rtc_base/socket_address.h"
@@ -31,6 +32,28 @@ namespace {
 // Ensure ICE config is correct at least one hour after session starts.
 constexpr base::TimeDelta kMinimumIceConfigLifetime =
     base::TimeDelta::FromHours(1);
+
+void PrintIceConfig(const IceConfig& ice_config) {
+  HOST_LOG << "Received IceConfig:";
+  HOST_LOG << "  STUN: [";
+  for (auto& stun_server : ice_config.stun_servers) {
+    HOST_LOG << "    " << stun_server.ToString() << ",";
+  }
+  HOST_LOG << "  ]";
+  HOST_LOG << "  TURN: [";
+  for (auto& turn_server : ice_config.turn_servers) {
+    HOST_LOG << "    {";
+    HOST_LOG << "      username: " << turn_server.credentials.username;
+    HOST_LOG << "      password: " << turn_server.credentials.password;
+    for (auto& port : turn_server.ports) {
+      HOST_LOG << "      port: " << port.address.ToString();
+    }
+    HOST_LOG << "    },";
+  }
+  HOST_LOG << "  ]";
+  HOST_LOG << "  expiration time: " << ice_config.expiration_time;
+  HOST_LOG << "  max_bitrate_kbps: " << ice_config.max_bitrate_kbps;
+}
 
 }  // namespace
 
@@ -82,6 +105,7 @@ void TransportContext::EnsureFreshIceConfig() {
   // Don't need to make ICE config request if both STUN and Relay are disabled.
   if ((network_settings_.flags & (NetworkSettings::NAT_TRAVERSAL_STUN |
                                   NetworkSettings::NAT_TRAVERSAL_RELAY)) == 0) {
+    HOST_LOG << "Skipping ICE Config request as STUN and RELAY are disabled";
     return;
   }
 
@@ -108,6 +132,8 @@ void TransportContext::OnIceConfig(RelayMode relay_mode,
                                    const IceConfig& ice_config) {
   ice_config_[relay_mode] = ice_config;
   ice_config_request_[relay_mode].reset();
+
+  PrintIceConfig(ice_config);
 
   auto& callback_list = pending_ice_config_callbacks_[relay_mode];
   while (!callback_list.empty()) {
