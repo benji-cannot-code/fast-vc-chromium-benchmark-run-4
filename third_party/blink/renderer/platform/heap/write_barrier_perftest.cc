8cc661e9f9d574013f8d6bcf958066c9e7c4a852
Brian Sheedy <bsheedy@chromium.org>
2020-03-11 22:00:01 +0000

    Switch blink_heap_unittests to histograms
    Switches uses of PrintResult in blink_heap_unittests to
    PerfResultReporter and whitelists it for conversion to histograms
    before being uploaded to the perf dashboard.
    
    Bug: 923564
    Change-Id: I5278ed703b2644eac7fbe7b547ed8dcc94eb8e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095926
    Reviewed-by: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749349}

--- third_party/blink/renderer/platform/heap/write_barrier_perftest.cc
+++ third_party/blink/renderer/platform/heap/write_barrier_perftest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "testing/perf/perf_test.h"
+#include "testing/perf/perf_result_reporter.h"
 #include "third_party/blink/renderer/platform/heap/heap_test_utilities.h"
 #include "third_party/blink/renderer/platform/heap/persistent.h"
 
@@ -14,6 +14,21 @@ namespace blink {
 class WriteBarrierPerfTest : public TestSupportingGC {};
 
 namespace {
+
+constexpr char kMetricPrefixWriteBarrier[] = "WriteBarrier.";
+constexpr char kMetricWritesDuringGcRunsPerS[] = "writes_during_gc";
+constexpr char kMetricWritesOutsideGcRunsPerS[] = "writes_outside_gc";
+constexpr char kMetricRelativeSpeedDifferenceUnitless[] =
+    "relative_speed_difference";
+
+perf_test::PerfResultReporter SetUpReporter(const std::string& story_name) {
+  perf_test::PerfResultReporter reporter(kMetricPrefixWriteBarrier, story_name);
+  reporter.RegisterImportantMetric(kMetricWritesDuringGcRunsPerS, "runs/s");
+  reporter.RegisterImportantMetric(kMetricWritesOutsideGcRunsPerS, "runs/s");
+  reporter.RegisterImportantMetric(kMetricRelativeSpeedDifferenceUnitless,
+                                   "unitless");
+  return reporter;
+}
 
 class PerfDummyObject : public GarbageCollected<PerfDummyObject> {
  public:
@@ -62,19 +77,16 @@ TEST_F(WriteBarrierPerfTest, MemberWritePerformance) {
   PreciselyCollectGarbage();
 
   // Reporting.
-  perf_test::PrintResult(
-      "WriteBarrierPerfTest", " writes during GC", "",
-      static_cast<double>(kNumElements) / during_gc_duration.InMillisecondsF(),
-      "writes/ms", true);
-  perf_test::PrintResult(
-      "WriteBarrierPerfTest", " writes outside GC", "",
-      static_cast<double>(kNumElements) / outside_gc_duration.InMillisecondsF(),
-      "writes/ms", true);
-  perf_test::PrintResult("WriteBarrierPerfTest", " relative speed difference",
-                         "",
-                         during_gc_duration.InMillisecondsF() /
-                             outside_gc_duration.InMillisecondsF(),
-                         "times", true);
+  auto reporter = SetUpReporter("member_write_performance");
+  reporter.AddResult(
+      kMetricWritesDuringGcRunsPerS,
+      static_cast<double>(kNumElements) / during_gc_duration.InSecondsF());
+  reporter.AddResult(
+      kMetricWritesOutsideGcRunsPerS,
+      static_cast<double>(kNumElements) / outside_gc_duration.InSecondsF());
+  reporter.AddResult(
+      kMetricRelativeSpeedDifferenceUnitless,
+      during_gc_duration.InSecondsF() / outside_gc_duration.InSecondsF());
 }
 
 }  // namespace blink
