b9599b08711723bbc1c4339ffd754c60eaaccff7
Chris Lu <thegreenfrog@chromium.org>
2020-03-11 01:19:06 +0000

    [ios] Add Messages overflow badge metrics
    Adds metrics to track user taps on the overflow badge, the number
    of times the overflow badge is shown, and when a user taps on
    a row in the overflow menu.
    
    
    Fixed: 1055549
    Change-Id: I43fa4fcb2bced7bdcda5209688fba5ffe5066e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2076662
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749009}

--- ios/chrome/browser/ui/badges/badge_popup_menu_coordinator.mm
+++ ios/chrome/browser/ui/badges/badge_popup_menu_coordinator.mm
@@ -6,11 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/badges/badge_popup_menu_coordinator.h"
 
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
+#include "ios/chrome/browser/infobars/infobar_metrics_recorder.h"
 #import "ios/chrome/browser/infobars/infobar_type.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/ui/badges/badge_constants.h"
 #import "ios/chrome/browser/ui/badges/badge_item.h"
 #import "ios/chrome/browser/ui/badges/badge_popup_menu_item.h"
+#import "ios/chrome/browser/ui/badges/badges_histograms.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/infobar_commands.h"
 #import "ios/chrome/browser/ui/popup_menu/public/cells/popup_menu_item.h"
@@ -96,18 +99,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       HandlerForProtocol(self.browser->GetCommandDispatcher(), InfobarCommands);
   switch (item.actionIdentifier) {
     case PopupMenuActionShowSavePasswordOptions: {
+      UMA_HISTOGRAM_ENUMERATION(kInfobarOverflowMenuTappedHistogram,
+                                MobileMessagesInfobarType::SavePassword);
       [handler displayModalInfobar:InfobarType::kInfobarTypePasswordSave];
       break;
     }
     case PopupMenuActionShowUpdatePasswordOptions: {
+      UMA_HISTOGRAM_ENUMERATION(kInfobarOverflowMenuTappedHistogram,
+                                MobileMessagesInfobarType::UpdatePassword);
       [handler displayModalInfobar:InfobarType::kInfobarTypePasswordUpdate];
       break;
     }
     case PopupMenuActionShowSaveCardOptions: {
+      UMA_HISTOGRAM_ENUMERATION(kInfobarOverflowMenuTappedHistogram,
+                                MobileMessagesInfobarType::SaveCard);
       [handler displayModalInfobar:InfobarType::kInfobarTypeSaveCard];
       break;
     }
     case PopupMenuActionShowTranslateOptions: {
+      UMA_HISTOGRAM_ENUMERATION(kInfobarOverflowMenuTappedHistogram,
+                                MobileMessagesInfobarType::Translate);
       [handler displayModalInfobar:InfobarType::kInfobarTypeTranslate];
       break;
     }
