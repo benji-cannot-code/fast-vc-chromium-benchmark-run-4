55418490ab282f272ec71b25335138594763ee47
Dominic Mazzoni <dmazzoni@chromium.org>
2020-03-11 07:26:31 +0000

    Add attributes to PDF structure tree nodes.
    Just like in HTML, some nodes in a PDF structure tree need not only the
    tag name, but also some attributes.
    
    This change adds some of the most generally useful: alt text for images,
    language codes for any element, and various table-related attributes.
    
    Bug: 1039816
    Change-Id: Ib9c8a768f594bdd61cf5848befa09af290dafdc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090621
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749068}

--- headless/lib/headless_web_contents_browsertest.cc
+++ headless/lib/headless_web_contents_browsertest.cc
@@ -567,6 +567,12 @@ const char kExpectedStructTreeJSON[] = R"({
             } ]
          } ]
       } ]
+   }, {
+      "type": "Div",
+      "~children": [ {
+         "alt": "Car at the beach",
+         "type": "Figure"
+      } ]
    } ]
 }
 )";
