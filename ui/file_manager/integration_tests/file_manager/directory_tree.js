8f162b81496b92e4ca198d1a403a90eddf048163
Majid Valipour <majidvp@chromium.org>
2020-03-12 20:31:45 +0000

    Prepare CSSOMViewScrollCoordinates for finch trial
    Following best practice advice from go/finch-blink:
     - Update testing config variation to enable the flag
     - Update file_manager test to expect the new behavior
    
    Bug:721759
    
    Change-Id: I727eef6b50cf09027945bcd645335b39ee83ba2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087422
    Commit-Queue: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749856}

--- ui/file_manager/integration_tests/file_manager/directory_tree.js
+++ ui/file_manager/integration_tests/file_manager/directory_tree.js
@@ -313,13 +313,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     chrome.test.assertTrue(await remoteCall.callRemoteTestUtil(
         'requestAnimationFrame', appId, []));
 
-    // Get the scroll limits: see crbug.com/721759 for RTL |scrollRight|.
     const scrolled = await remoteCall.waitForElementStyles(
         appId, directoryTree, ['scrollLeft']);
-    const scrollRight = scrolled.scrollWidth - scrolled.renderedWidth;
 
     // Check: the directory tree should not be horizontally scrolled.
-    const notScrolled = scrolled.scrollLeft === scrollRight;
+    const notScrolled = scrolled.scrollLeft === 0;
     chrome.test.assertTrue(notScrolled, 'Tree should not scroll right');
   };
 
