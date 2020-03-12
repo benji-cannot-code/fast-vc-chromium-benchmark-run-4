052c0e8e285d4dd3025d6010995711d4b46a1912
Hazem Ashmawy <hazems@google.com>
2020-03-12 18:28:27 +0000

    AW: Fix presubmit warning use setTextAppearance instead of setTextColor
    Use setTextAppearance to set URL text color instead of setTextColor.
    This defines new style resources used by setTextAppearance.
    
    Fixed: 1059458
    Test: Run presubmit upload checks, no warnings show up
    Test: Run the shell and open valid and invalid URLs and notice URL text color
    Change-Id: I55feb310f3315cdd126a46e6f3bd5802b1e5779e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095550
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Auto-Submit: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749787}

--- android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserActivity.java
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/WebViewBrowserActivity.java
@@ -53,7 +53,6 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.PackageManagerUtils;
@@ -745,9 +744,11 @@ public class WebViewBrowserActivity extends AppCompatActivity {
     }
 
     private void setUrlFail(boolean fail) {
-        mUrlBar.setTextColor(fail ?
-            ApiCompatibilityUtils.getColor(getResources(), R.color.url_error_color) :
-            ApiCompatibilityUtils.getColor(getResources(), R.color.url_color));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mUrlBar.setTextAppearance(fail ? R.style.UrlTextError : R.style.UrlText);
+        } else {
+            mUrlBar.setTextAppearance(this, fail ? R.style.UrlTextError : R.style.UrlText);
+        }
     }
 
     /**
