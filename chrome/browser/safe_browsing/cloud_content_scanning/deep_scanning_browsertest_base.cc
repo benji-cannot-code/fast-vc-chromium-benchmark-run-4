4f761eee85dc0bbc94ff26c8dfd1453ab318124e
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-03-12 21:13:19 +0000

    Add browser test for AllowPasswordProtectedFiles policy on upload path
    Bug: 1061015
    Change-Id: I1c08815adc7b470a44e83ef3cb90d694e8c6d4b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2100489
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749880}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_browsertest_base.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_browsertest_base.cc
@@ -82,6 +82,8 @@ void DeepScanningBrowserTestBase::TearDownOnMainThread() {
   SetDlpPolicy(CheckContentComplianceValues::CHECK_NONE);
   SetMalwarePolicy(SendFilesForMalwareCheckValues::DO_NOT_SCAN);
   SetWaitPolicy(DelayDeliveryUntilVerdictValues::DELAY_NONE);
+  SetAllowPasswordProtectedFilesPolicy(
+      AllowPasswordProtectedFilesValues::ALLOW_UPLOADS_AND_DOWNLOADS);
 }
 
 void DeepScanningBrowserTestBase::SetDlpPolicy(
@@ -100,6 +102,12 @@ void DeepScanningBrowserTestBase::SetWaitPolicy(
     DelayDeliveryUntilVerdictValues state) {
   g_browser_process->local_state()->SetInteger(
       prefs::kDelayDeliveryUntilVerdict, state);
+}
+
+void DeepScanningBrowserTestBase::SetAllowPasswordProtectedFilesPolicy(
+    AllowPasswordProtectedFilesValues state) {
+  g_browser_process->local_state()->SetInteger(
+      prefs::kAllowPasswordProtectedFiles, state);
 }
 
 void DeepScanningBrowserTestBase::SetUpDelegate() {
