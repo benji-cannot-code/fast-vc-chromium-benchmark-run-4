affb40434c056cf802e22eef635b9148ac49b53b
Clark DuVall <cduvall@chromium.org>
2020-03-12 18:24:08 +0000

    [WebLayer] Add PermissionDecisionAutoBlockerFactory for weblayer
    This will be used for implementing permissions in WebLayer, and is the
    equivalent of the chrome class of the same name.
    
    Bug: 1025609
    Change-Id: Iff7023262a1593c281ce350ecbd93536ee001db8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2098987
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749783}

---
+++ weblayer/browser/permissions/permission_decision_auto_blocker_factory.cc
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "weblayer/browser/permissions/permission_decision_auto_blocker_factory.h"
+
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "components/permissions/permission_decision_auto_blocker.h"
+#include "weblayer/browser/host_content_settings_map_factory.h"
+
+namespace weblayer {
+
+// static
+permissions::PermissionDecisionAutoBlocker*
+PermissionDecisionAutoBlockerFactory::GetForBrowserContext(
+    content::BrowserContext* browser_context) {
+  return static_cast<permissions::PermissionDecisionAutoBlocker*>(
+      GetInstance()->GetServiceForBrowserContext(browser_context, true));
+}
+
+// static
+PermissionDecisionAutoBlockerFactory*
+PermissionDecisionAutoBlockerFactory::GetInstance() {
+  static base::NoDestructor<PermissionDecisionAutoBlockerFactory> factory;
+  return factory.get();
+}
+
+PermissionDecisionAutoBlockerFactory::PermissionDecisionAutoBlockerFactory()
+    : BrowserContextKeyedServiceFactory(
+          "PermissionDecisionAutoBlocker",
+          BrowserContextDependencyManager::GetInstance()) {
+  DependsOn(HostContentSettingsMapFactory::GetInstance());
+}
+
+PermissionDecisionAutoBlockerFactory::~PermissionDecisionAutoBlockerFactory() =
+    default;
+
+KeyedService* PermissionDecisionAutoBlockerFactory::BuildServiceInstanceFor(
+    content::BrowserContext* context) const {
+  return new permissions::PermissionDecisionAutoBlocker(
+      HostContentSettingsMapFactory::GetForBrowserContext(context));
+}
+
+content::BrowserContext*
+PermissionDecisionAutoBlockerFactory::GetBrowserContextToUse(
+    content::BrowserContext* context) const {
+  return context;
+}
+
+}  // namespace weblayer
