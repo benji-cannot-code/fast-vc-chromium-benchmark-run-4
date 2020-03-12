6918a9c9603e6d0825072d744260357de093714e
bttk <bttk@chromium.org>
2020-03-12 00:06:51 +0000

    metrics: Move CachingUmaRecorder singleton out of RecordHistogram
    In the future it will be used for non-histogram data, namely backing
    RecordUserActions.
    
    Bug: 658300
    Change-Id: I0a872878466f8cf62847ed10da2011fcdbfb07b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079841
    Auto-Submit: bttk <bttk@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749424}

--- base/test/android/javatests/src/org/chromium/base/test/DisableNativeTestRule.java
+++ base/test/android/javatests/src/org/chromium/base/test/DisableNativeTestRule.java
@@ -10,8 +10,8 @@ import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
 import org.chromium.base.Log;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.base.metrics.UmaRecorderHolder;
 
 /**
  * {@link TestRule} to disable native access for testing.
@@ -27,7 +27,7 @@ public final class DisableNativeTestRule implements TestRule {
                 LoadNative loadNative = description.getAnnotation(LoadNative.class);
                 if (loadNative == null) {
                     Log.i(TAG, "Disable RecordHistogram and RecordUserAction for testing");
-                    RecordHistogram.setDisabledForTests(true);
+                    UmaRecorderHolder.setDisabledForTests(true);
                     RecordUserAction.setDisabledForTests(true);
                 } else {
                     Log.i(TAG, "Test will run with native libraries");
@@ -37,7 +37,7 @@ public final class DisableNativeTestRule implements TestRule {
                 } finally {
                     if (loadNative == null) {
                         Log.i(TAG, "Re-enable RecordHistogram and RecordUserAction after test");
-                        RecordHistogram.setDisabledForTests(false);
+                        UmaRecorderHolder.setDisabledForTests(false);
                         RecordUserAction.setDisabledForTests(false);
                     }
                 }
