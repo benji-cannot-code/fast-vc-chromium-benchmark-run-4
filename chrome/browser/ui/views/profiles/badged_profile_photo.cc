393ec3c6a8e8bca8289fec9c4867e7f7b0089670
Thomas Lukaszewicz <tluk@chromium.org>
2020-03-12 18:37:17 +0000

    Fixed view sizing bug for BadgedProfilePhoto view
    Added a call to SizeToPreferredSize() to the badge view wrapping the
    vector icon after the icon has been set. This ensures that the
    encapsulating view is sized correctly.
    
    Bug: 1060554
    Change-Id: I8863d104ee604b7a013805cc6bff72820e28a04c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2099224
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749794}

--- chrome/browser/ui/views/profiles/badged_profile_photo.cc
+++ chrome/browser/ui/views/profiles/badged_profile_photo.cc
@@ -56,7 +56,6 @@ class BadgeView : public ::views::ImageView {
  public:
   explicit BadgeView(BadgedProfilePhoto::BadgeType badge_type)
       : badge_type_(badge_type) {
-    SizeToPreferredSize();
     SetPosition(gfx::Point(kBadgedProfilePhotoWidth - kBadgeIconSize,
                            kBadgedProfilePhotoHeight - kBadgeIconSize));
   }
@@ -107,6 +106,7 @@ class BadgeView : public ::views::ImageView {
         NOTREACHED();
         break;
     }
+    SizeToPreferredSize();
   }
 
  private:
