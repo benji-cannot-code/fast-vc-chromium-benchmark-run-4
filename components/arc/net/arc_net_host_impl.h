01e02a0e17e5efa2e1ee899373d87a928a446978
Hugo Benichi <hugobenichi@google.com>
2020-03-12 02:32:27 +0000

    Reland "arc: net: remove ARC N legacy APIs"
    This is a reland of 956b131c21d79ae311c81f1696a9aef5a3eeae87
    
    The original patch was reverted by markusheintz@ in commit
    a2c713494852d7f71284b7a364c5c24654b85910 because it turned out
    there were still bots running with ARC N.
    
    Original change's description:
    > arc: net: remove ARC N legacy APIs
    >
    > This patch removes net.mojom APIs and fields only used in ARC N:
    >  - NetworkConfiguration mac_address: the host mac_address of a network
    >  is now completely hidden in ARC P.
    >  - GetDefaultNetwork and DefaultNetworkChanged: ARC P is multinetwork
    >  aware and always need the full list of connected services.
    >
    > BUG=b:149716479
    > BUG=b:145960788
    > TEST=Compiled, flashed eve, ran CtsNetTestCases.
    >
    > Change-Id: I3368d620d44dd910aae7a12c84c207c3d5cb6cf3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2086362
    > Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Commit-Queue: Hugo Benichi <hugobenichi@google.com>
    > Cr-Commit-Position: refs/heads/master@{#748094}
    
    Bug: b:149716479, b:145960788
    Change-Id: I198438aec3c5422bbd6b8bf9258f16ab7dd194b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2094759
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hugo Benichi <hugobenichi@google.com>
    Cr-Commit-Position: refs/heads/master@{#749486}

--- components/arc/net/arc_net_host_impl.h
+++ components/arc/net/arc_net_host_impl.h
@@ -97,7 +97,6 @@ class ArcNetHostImpl : public KeyedService,
       const std::vector<const chromeos::NetworkState*>& networks) override;
   void NetworkListChanged() override;
   void DeviceListChanged() override;
-  void GetDefaultNetwork(GetDefaultNetworkCallback callback) override;
 
   // Overriden from chromeos::NetworkConnectionObserver.
   void DisconnectRequested(const std::string& service_path) override;
@@ -108,7 +107,6 @@ class ArcNetHostImpl : public KeyedService,
 
  private:
   const chromeos::NetworkState* GetDefaultNetworkFromChrome();
-  void UpdateDefaultNetwork();
   void UpdateActiveNetworks();
   void DefaultNetworkSuccessCallback(const std::string& service_path,
                                      const base::DictionaryValue& dictionary);
