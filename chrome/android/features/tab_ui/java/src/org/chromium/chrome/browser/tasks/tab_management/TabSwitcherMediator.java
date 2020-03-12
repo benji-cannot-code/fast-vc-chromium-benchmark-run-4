0d0a486eb8d4a56db35317dcf8bbfe8f85388711
Henrique Nakashima <hnakashima@chromium.org>
2020-03-12 20:23:43 +0000

    Move getValue() from CachedFeatureFlags to CachedFieldTrialParameters
    Bug: 1012975
    Change-Id: I1b95e0ab5189b1a8966c317d12792ccfdd0e07b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096879
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749853}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMediator.java
@@ -32,7 +32,6 @@ import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.task.PostTask;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManager;
 import org.chromium.chrome.browser.compositor.layouts.content.TabContentManager;
-import org.chromium.chrome.browser.flags.CachedFeatureFlags;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.fullscreen.ChromeFullscreenManager;
 import org.chromium.chrome.browser.fullscreen.FullscreenManager;
@@ -617,8 +616,7 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
      *  @return whether tabs should show in MRU order
      */
     static boolean isShowingTabsInMRUOrder() {
-        String feature =
-                CachedFeatureFlags.getValue(StartSurfaceConfiguration.START_SURFACE_VARIATION);
+        String feature = StartSurfaceConfiguration.START_SURFACE_VARIATION.getValue();
         return TextUtils.equals(feature, "twopanes");
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabFeatureUtilities.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabFeatureUtilities.java
@@ -65,11 +65,11 @@ public class TabFeatureUtilities {
 
     private static class GridTabSwitcherUtil {
         private static int getMinSdkVersion() {
-            return CachedFeatureFlags.getValue(TabFeatureUtilities.ZOOMING_MIN_SDK);
+            return TabFeatureUtilities.ZOOMING_MIN_SDK.getValue();
         }
 
         private static int getMinMemoryMB() {
-            return CachedFeatureFlags.getValue(TabFeatureUtilities.ZOOMING_MIN_MEMORY);
+            return TabFeatureUtilities.ZOOMING_MIN_MEMORY.getValue();
         }
     }
 }
