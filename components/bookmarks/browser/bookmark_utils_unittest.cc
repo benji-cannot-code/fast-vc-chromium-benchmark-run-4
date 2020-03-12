4aa24ae8b7ee129b9a75244bceab7c7ff49237b4
Mikel Astiz <mastiz@chromium.org>
2020-03-12 08:07:00 +0000

    bookmarks: Simplify constructing the managed node in tests
    Tests can be simplified if TestBookmarkClient offers support for
    constructing the managed bookmarks folder, since it's not supposed to
    be constructed in an arbitrary fashion anyway.
    
    Bug: 1060311
    Change-Id: I388daca001474fab2b0810d3a581b3a36e5f78ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096963
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749570}

--- components/bookmarks/browser/bookmark_utils_unittest.cc
+++ components/bookmarks/browser/bookmark_utils_unittest.cc
@@ -427,11 +427,8 @@ TEST_F(BookmarkUtilsTest, MAYBE_CutToClipboard) {
 
 TEST_F(BookmarkUtilsTest, PasteNonEditableNodes) {
   // Load a model with an managed node that is not editable.
-  std::unique_ptr<TestBookmarkClient> client(new TestBookmarkClient());
-  auto owned_managed_node =
-      std::make_unique<BookmarkPermanentNode>(100, BookmarkNode::FOLDER);
-  BookmarkPermanentNode* managed_node = owned_managed_node.get();
-  client->SetManagedNodeToLoad(std::move(owned_managed_node));
+  auto client = std::make_unique<TestBookmarkClient>();
+  BookmarkNode* managed_node = client->EnableManagedNode();
 
   std::unique_ptr<BookmarkModel> model(
       TestBookmarkClient::CreateModelWithClient(std::move(client)));
@@ -578,11 +575,8 @@ TEST_F(BookmarkUtilsTest, CloneFolderResetsNonClonedKey) {
 
 TEST_F(BookmarkUtilsTest, RemoveAllBookmarks) {
   // Load a model with an managed node that is not editable.
-  std::unique_ptr<TestBookmarkClient> client(new TestBookmarkClient());
-  auto owned_managed_node =
-      std::make_unique<BookmarkPermanentNode>(100, BookmarkNode::FOLDER);
-  BookmarkPermanentNode* managed_node = owned_managed_node.get();
-  client->SetManagedNodeToLoad(std::move(owned_managed_node));
+  auto client = std::make_unique<TestBookmarkClient>();
+  BookmarkNode* managed_node = client->EnableManagedNode();
 
   std::unique_ptr<BookmarkModel> model(
       TestBookmarkClient::CreateModelWithClient(std::move(client)));
