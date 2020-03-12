7c60a6375a7f82a0d112aa0d672f8dfe3a811b3f
Alex Moshchuk <alexmos@chromium.org>
2020-03-12 18:24:46 +0000

    Remove logging code for expired Site Isolation cmdline flag metrics.
    Bug: 1053144
    Change-Id: Ie30d7ba277fac9e87d763a0a7ccadeb163453942
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2098267
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749784}

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -881,9 +881,6 @@ int BrowserMainLoop::PreCreateThreads() {
   // happen.
   SiteIsolationPolicy::ApplyGlobalIsolatedOrigins();
 
-  // Record metrics about which site isolation flags have been turned on.
-  SiteIsolationPolicy::StartRecordingSiteIsolationFlagUsage();
-
   return result_code_;
 }
 
