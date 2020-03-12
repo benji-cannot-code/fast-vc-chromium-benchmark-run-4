21d32d11ea04334720857b96438920e0e6ddb442
Jesse Schettler <jschettler@chromium.org>
2020-03-12 19:14:53 +0000

    Add scanning UI to print management app
    Add a scanning UI that can be opened from chrome://scanning. The UI is
    hidden behind a feature flag that is disabled by default.
    
    Bug: 1059779
    Test: browser_tests --gtest_filter=ScanningUIBrowserTest.All
    
    Change-Id: Ic01e180fe22930e405b9c004fcee7b198e3787ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096788
    Reviewed-by: jimmy gong <jimmyxgong@chromium.org>
    Reviewed-by: Zentaro Kavanagh <zentaro@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749813}

---
+++ chrome/test/data/webui/chromeos/print_management/scanning_ui_test.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(jschettler): use es6 module for mojo binding (crbug/1004256).
+import 'chrome://resources/mojo/mojo/public/js/mojo_bindings_lite.js';
+import 'chrome://scanning/scanning_ui.js';
+
+suite('ScanningUITest', () => {
+  /** @type {?ScanningUIElement} */
+  let page = null;
+
+  setup(function() {
+    PolymerTest.clearBody();
+    page = document.createElement('scanning-ui');
+    document.body.appendChild(page);
+  });
+
+  teardown(function() {
+    page.remove();
+    page = null;
+  });
+
+  test('MainPageLoaded', () => {
+    // TODO(jschettler): Remove this stub test once the UI has more capabilities
+    // to test.
+    assertEquals('Scanning UI', page.$$('#header').textContent);
+  });
+});
