00d90db69b96d0ff5d6d40f7801ddfc3ce433457
Kent Tamura <tkent@chromium.org>
2020-03-12 07:41:01 +0000

    Move HTMLSelectElement::SaveListboxActiveSelection() to SelectType
    Also moves cached_state_for_active_selection_ to ListBoxSelectType.
    This CL has no behavior changes.
    
    Bug: 1052232
    Change-Id: Ie32cdf5eb9cba2daf7031b20dc71a9ca907d92e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2098191
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749564}

--- third_party/blink/renderer/core/html/forms/select_type.h
+++ third_party/blink/renderer/core/html/forms/select_type.h
@@ -46,6 +46,7 @@ class SelectType : public GarbageCollected<SelectType> {
   virtual void MaximumOptionWidthMightBeChanged() const;
 
   virtual void SelectAll();
+  virtual void SaveListboxActiveSelection();
 
   virtual void ShowPopup();
   virtual void HidePopup();
