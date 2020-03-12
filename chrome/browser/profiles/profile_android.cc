65e0f63ff291a1e11001438be7b64d81ddf4aeeb
Theresa <twellington@chromium.org>
2020-03-12 15:03:31 +0000

    Revert "Move //chrome/android/public/ to //chrome/browser/android/"
    This reverts commit a8ed939ac9d7cdb5005b97db2a0695fa48d1e56a.
    
    Reason for revert: Moving to different directory
    
    Original change's description:
    > Move //chrome/android/public/ to //chrome/browser/android/
    > 
    > Change 1 of 2. The next CL will update downstream dependencies.
    > 
    > Note that this will break the downstream build temporarily. Since there
    > are JNI files generated, unless the package is updated we cannot keep
    > a temporary mirrored build target as was done in
    > https://chromium-review.googlesource.com/c/chromium/src/+/2095444
    > 
    > BUG=1052382
    > 
    > Change-Id: Ib4d2e6f8143a7c0c6ead75d7701fea69a21db3a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095760
    > Commit-Queue: Theresa  <twellington@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#749666}
    
    TBR=dtrainor@chromium.org,twellington@chromium.org
    
    Change-Id: Ieb5026e18ccbc629be00c6a10762253908a98ae2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1052382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2100709
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749672}

--- chrome/browser/profiles/profile_android.cc
+++ chrome/browser/profiles/profile_android.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/memory/ptr_util.h"
-#include "chrome/browser/android/profiles/jni_headers/Profile_jni.h"
+#include "chrome/android/public/profiles/jni_headers/Profile_jni.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_destroyer.h"
 #include "chrome/browser/profiles/profile_key.h"
--- chrome/browser/profiles/profile_manager_android.cc
+++ chrome/browser/profiles/profile_manager_android.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile_manager_android.h"
 
-#include "chrome/browser/android/profiles/jni_headers/ProfileManager_jni.h"
+#include "chrome/android/public/profiles/jni_headers/ProfileManager_jni.h"
 #include "chrome/browser/profiles/profile_android.h"
 
 ProfileManagerAndroid::ProfileManagerAndroid() = default;
