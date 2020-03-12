5f1fcbc741cafb19fbec42ac60fefc3b033c264a
Victor Costan <pwnall@chromium.org>
2020-03-12 04:21:48 +0000

    Reland "WebLocks: Remove unused include from lock_manager.h."
    This is a reland of 264654a0749006cc756035d9ec444d0afeebf36a
    
    The CL was reverted speculatively in https://crrev.com/c/2095733 to get
    the tree green. The compilation errors leading to tree closure did not
    seem related to the files changed in this CL.
    
    Original change's description:
    > WebLocks: Remove unused include from lock_manager.h.
    >
    > //content/browser/locks/lock_manager.h included
    > //content/public/browser/browser_context.h, despite not using
    > BrowserContext at all.
    >
    > Because lock_manager.h is included in storage_partition_impl.h,
    > browser_context.h was indirectly included in many files. This CL fixes
    > the IWYU violations highlighted by compiler errors.
    >
    > Change-Id: I46462b337d93a7ae1d60648cfd617b2e69662637
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093553
    > Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    > Auto-Submit: Victor Costan <pwnall@chromium.org>
    > Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#749082}
    
    Tbr: falken
    Change-Id: Iaa1527e4190a577d58355275f6badbd1c26ab44f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2100010
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749512}

--- content/browser/storage_service_restart_browsertest.cc
+++ content/browser/storage_service_restart_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/storage/public/mojom/test_api.test-mojom.h"
 #include "content/browser/dom_storage/dom_storage_context_wrapper.h"
 #include "content/browser/storage_partition_impl.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_test_utils.h"
