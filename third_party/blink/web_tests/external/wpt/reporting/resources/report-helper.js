3bb864ccf93a045c8dd0613ce8838a21519721a1
Rodney Ding <rodneyding@google.com>
2020-03-12 22:19:59 +0000

    Add support for path absolute url to reporting endpoint
    Add wpt tests to cover reports sent out-of-band to
    path-absolute-url endpoints.
    Copying some report infra code from wpt/content-security-policy
    
    Github discussions here https://github.com/w3c/reporting/issues/147
    
    Bug: 1060306
    Change-Id: I293cb686fd60edd15b28b7ccf7a5a9904b7ea588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096846
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749897}

---
+++ third_party/blink/web_tests/external/wpt/reporting/resources/report-helper.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function wait(ms) {
+  return new Promise(resolve => step_timeout(resolve, ms));
+}
+
+async function pollReports(endpoint, id) {
+  const res = await fetch(`${endpoint}?id=${id}`, {cache: 'no-store'});
+  const reports = [];
+  if (res.status === 200) {
+    for (const report of await res.json()) {
+      reports.push(report);
+    }
+  }
+  return reports;
+}
+
+function checkReportExists(reports, type, url) {
+  for (const report of reports) {
+    if (report.type !== type) continue;
+    if (report.body.sourceFile === url) return true;
+  }
+  assert_unreached(`A report of ${type} from ${url} is not found.`);
+}
