05ebbbcb89827ecda735a52ce6c7719cb83c03cc
Sergey Ulanov <sergeyu@chromium.org>
2020-03-12 00:14:53 +0000

    [Fuchsia] Implement Frame/SetPermissionState()
    Now WebEngine implements SetPermissionState() which allows to grant
    permissions for web features.
    
    Bug: 922833
    Change-Id: I356e54646ad870bb7882a5b9a95bc87deccba453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058086
    Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749428}

--- fuchsia/engine/browser/web_engine_browser_context.h
+++ fuchsia/engine/browser/web_engine_browser_context.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 
 class WebEngineNetLogObserver;
-class WebEnginePermissionManager;
+class WebEnginePermissionDelegate;
 
 class WebEngineBrowserContext : public content::BrowserContext {
  public:
@@ -53,7 +53,7 @@ class WebEngineBrowserContext : public content::BrowserContext {
   std::unique_ptr<WebEngineNetLogObserver> net_log_observer_;
   std::unique_ptr<SimpleFactoryKey> simple_factory_key_;
   std::unique_ptr<ResourceContext> resource_context_;
-  std::unique_ptr<WebEnginePermissionManager> permission_manager_;
+  std::unique_ptr<WebEnginePermissionDelegate> permission_delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(WebEngineBrowserContext);
 };
