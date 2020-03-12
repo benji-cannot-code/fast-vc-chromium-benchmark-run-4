24043fb42dbf95c7b3cda30ec5e1947bfea97c4a
Mikhail Khokhlov <khokhlov@google.com>
2020-03-12 13:32:25 +0000

    sqlite: Create an sqlite_dev target for use in developer tools
    We'd like to use some SQLite features currently disabled in Chrome
    in a developer trace-analyzing tool called trace_processor_shell.
    As discussed on the thread [1], it's undesirable to enable them
    Chrome-wide.
    
    This CL adds an "sqlite_dev" build target and third_party/sqlite/dev
    include directory with a separate sqlite library that has most of the
    features enabled.
    
    [1] https://groups.google.com/a/chromium.org/g/storage-dev/c/OkwAfpYWAa0
    
    Bug: b/150127983, b/150125501
    Change-Id: If328bb737679b6d352904bcb543bc6ed1c522b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2082389
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/master@{#749640}

--- third_party/sqlite/dev/sqlite3.h
+++ third_party/sqlite/dev/sqlite3.h
@@ -1,17 +1,19 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This is a shim that injects Chrome-specific defitions into sqlite3.c
-// BUILD.gn uses this instead of building the sqlite3 amalgamation directly.
+#ifndef THIRD_PARTY_SQLITE_DEV_SQLITE3_H_
+#define THIRD_PARTY_SQLITE_DEV_SQLITE3_H_
+
+// This is a shim header to include the right sqlite3 headers.
+// Use this instead of referencing sqlite3 headers directly.
 
 // We prefix chrome_ to SQLite's exported symbols, so that we don't clash with
 // other SQLite libraries loaded by the system libraries. This only matters when
 // using the component build, where our SQLite's symbols are visible to the
 // dynamic library loader.
-#include "third_party/sqlite/amalgamation/rename_exports.h"
+#include "third_party/sqlite/amalgamation_dev/rename_exports.h"
+#include "third_party/sqlite/amalgamation_dev/sqlite3.h"
 
-#include "third_party/sqlite/sqlite3_shim_fixups.h"
-
-#include "third_party/sqlite/amalgamation/sqlite3.c"
+#endif  // THIRD_PARTY_SQLITE_DEV_SQLITE3_H_
