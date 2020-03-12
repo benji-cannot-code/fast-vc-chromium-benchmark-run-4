ff8e8e5f80c25416fa6aaf62ecc60b0fa5d19f98
Yuki Shiino <yukishiino@chromium.org>
2020-03-12 11:15:20 +0000

    bindings: Add base::Optional version of IDL attributes (modules/)
    IDL attributes of type Nullable (e.g. "long?") are currently
    implemented as:
    
      int32_t attrName(bool& is_null);
      void setAttrName(int32_t value, bool is_null);
    
    however, the following APIs are preferable.
    
      base::Optional<int32_t> attrName();
      void setAttrName(base::Optional<int32_t> value);
    
    In order to update bindings APIs of IDL attributes,
    this patch adds the base::Optional version.
    
    Once the bindings generator switches to the new APIs, the
    old explicit is_null version will be removed.
    
    Bug: 1060971, 839389
    Change-Id: Iad728b1b9a7a289dcfa4f637cb84304ff48c4cf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2059032
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749609}

--- third_party/blink/renderer/modules/geolocation/geolocation_coordinates.cc
+++ third_party/blink/renderer/modules/geolocation/geolocation_coordinates.cc
@@ -28,6 +28,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+base::Optional<double> GeolocationCoordinates::altitude() const {
+  if (can_provide_altitude_)
+    return altitude_;
+  return base::nullopt;
+}
+
+base::Optional<double> GeolocationCoordinates::altitudeAccuracy() const {
+  if (can_provide_altitude_accuracy_)
+    return altitude_accuracy_;
+  return base::nullopt;
+}
+
+base::Optional<double> GeolocationCoordinates::heading() const {
+  if (can_provide_heading_)
+    return heading_;
+  return base::nullopt;
+}
+
+base::Optional<double> GeolocationCoordinates::speed() const {
+  if (can_provide_speed_)
+    return speed_;
+  return base::nullopt;
+}
+
 double GeolocationCoordinates::altitude(bool& is_null) const {
   if (can_provide_altitude_)
     return altitude_;
