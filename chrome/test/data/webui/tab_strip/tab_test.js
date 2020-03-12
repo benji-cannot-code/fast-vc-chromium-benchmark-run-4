beadd5db61abdfb990743fdc182d969e6fc4ab83
John Lee <johntlee@chromium.org>
2020-03-12 02:17:28 +0000

    WebUI Tab Strip: Update drag image scale and positioning
    The drag image was previously misaligned because of added padding and
    because ChromeOS automatically scales and offsets drag images. This CL
    fixes the alignment issue and forces the drag image to scale to 110%
    instead as per UX request.
    
    The offsets are calculated by determining the pixels the drag started
    from the center of the element, and then mapping them onto the bounds
    of the drag image.
    
    https://i.imgur.com/yLSJQ47.png
    
    Change-Id: Ifa17ef20d9968cb7b9139bca7391c2c5971c0059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2097187
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749477}

--- chrome/test/data/webui/tab_strip/tab_test.js
+++ chrome/test/data/webui/tab_strip/tab_test.js
@@ -23,6 +23,7 @@ suite('Tab', function() {
     alertStates: [],
     id: 1001,
     networkState: TabNetworkState.NONE,
+    pinned: false,
     title: 'My title',
   };
 
@@ -39,6 +40,7 @@ suite('Tab', function() {
     document.body.innerHTML = '';
 
     // Set CSS variable for animations
+    document.body.style.setProperty('--tabstrip-tab-height', '100px');
     document.body.style.setProperty('--tabstrip-tab-width', '280px');
     document.body.style.setProperty('--tabstrip-tab-spacing', '20px');
 
@@ -61,7 +63,7 @@ suite('Tab', function() {
     const animationPromise = tabElement.slideIn();
     // Before animation completes.
     assertEquals('20px', tabElementStyle.paddingRight);
-    assertEquals('none', tabElementStyle.maxWidth);
+    assertEquals('280px', tabElementStyle.maxWidth);
     assertEquals('matrix(0, 0, 0, 0, 0, 0)', tabElementStyle.transform);
     await animationPromise;
     // After animation completes.
@@ -432,5 +434,20 @@ suite('Tab', function() {
     innerTabElement.dispatchEvent(new KeyboardEvent('keydown', {key: ' '}));
     assertEquals(await testTabsApiProxy.whenCalled('activateTab'), tab.id);
     testTabsApiProxy.reset();
+  });
+
+  test('DragImagePreservesAspectRatio', () => {
+    const originalBoundingBox = tabElement.$('#tab').getBoundingClientRect();
+    const originalAspectRatio =
+        originalBoundingBox.width / originalBoundingBox.height;
+    tabElement.setDragging(true);
+    const dragImageBoundingBox =
+        tabElement.getDragImage().querySelector('#tab').getBoundingClientRect();
+    const dragImageAspectRatio =
+        dragImageBoundingBox.width / dragImageBoundingBox.height;
+    // Check the Math.floor values of these values to prevent possible
+    // flakiness caused by comparing float values.
+    assertEquals(
+        Math.floor(originalAspectRatio), Math.floor(dragImageAspectRatio));
   });
 });
