f634a3da7b34c1f0bc169bd8b4854afd6b8f445b
dalyk <dalyk@google.com>
2020-03-12 23:13:08 +0000

    Add metrics for tracking secure DNS setting UI interactions.
    This cl adds Net.DNS.UI.DropdownSelectionEvent.[Ignored,Selected,
    Unselected], Net.DNS.UI.ProbeAttemptSuccess, and
    Net.DNS.UI.ValidationAttemptSuccess.
    
    Bug: 1037944
    Change-Id: I19ef70e58d370d42111532a192edd2dbc7bbe223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2097000
    Commit-Queue: Katharine Daly <dalyk@google.com>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Auto-Submit: Katharine Daly <dalyk@google.com>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749939}

--- chrome/browser/resources/settings/privacy_page/secure_dns.js
+++ chrome/browser/resources/settings/privacy_page/secure_dns.js
@@ -83,6 +83,13 @@ Polymer({
     resolverOptions_: Array,
 
     /**
+     * Track the selected dropdown option so that it can be logged when a user-
+     * initiated UI dropdown selection change event occurs.
+     * @private
+     */
+    lastResolverOption_: String,
+
+    /**
      * String displaying the privacy policy of the resolver selected in the
      * dropdown menu.
      * @private
@@ -110,6 +117,7 @@ Polymer({
     // to match the underlying host resolver configuration.
     this.browserProxy_.getSecureDnsResolverList().then(resolvers => {
       this.resolverOptions_ = resolvers;
+      this.lastResolverOption_ = this.resolverOptions_[0].value;
       this.browserProxy_.getSecureDnsSetting().then(
           this.onSecureDnsPrefsChanged_.bind(this));
 
@@ -254,6 +262,10 @@ Polymer({
     if (this.$.secureResolverSelect.value === 'custom') {
       this.$.secureDnsInput.focus();
     }
+
+    this.browserProxy_.recordUserDropdownInteraction(
+        this.lastResolverOption_, this.$.secureResolverSelect.value);
+    this.lastResolverOption_ = this.$.secureResolverSelect.value;
   },
 
   /**
@@ -324,12 +336,14 @@ Polymer({
           r => r.value === secureDnsTemplates[0]);
       if (resolver) {
         this.$.secureResolverSelect.value = resolver.value;
+        this.lastResolverOption_ = resolver.value;
         return;
       }
     }
 
     // Otherwise, select the custom option.
     this.$.secureResolverSelect.value = 'custom';
+    this.lastResolverOption_ = 'custom';
 
     // Only update the custom input field if the templates are non-empty.
     // Otherwise, we may be clearing a previous value that the user wishes to
--- chrome/test/data/webui/settings/test_privacy_page_browser_proxy.js
+++ chrome/test/data/webui/settings/test_privacy_page_browser_proxy.js
@@ -16,6 +16,7 @@ class TestPrivacyPageBrowserProxy extends TestBrowserProxy {
       'getSecureDnsSetting',
       'validateCustomDnsEntry',
       'probeCustomDnsTemplate',
+      'recordUserDropdownInteraction',
     ]);
 
     /** @type {!MetricsReporting} */
@@ -120,5 +121,11 @@ class TestPrivacyPageBrowserProxy extends TestBrowserProxy {
   probeCustomDnsTemplate(template) {
     this.methodCalled('probeCustomDnsTemplate', template);
     return Promise.resolve(this.probeSuccess_);
+  }
+
+  /** @override */
+  recordUserDropdownInteraction(oldSelection, newSelection) {
+    this.methodCalled(
+        'recordUserDropdownInteraction', [oldSelection, newSelection]);
   }
 }
