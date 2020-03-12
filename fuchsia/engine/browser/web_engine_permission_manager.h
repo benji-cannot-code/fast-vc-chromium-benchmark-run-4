f292a883e368e5b11283bc8ef32ee3ba87209cf6
Kamila Hasanbega <hkamila@google.com>
2020-03-12 11:13:33 +0000

    Revert "[Fuchsia] Implement Frame/SetPermissionState()"
    This reverts commit 5a7d3fed134d21db7ebd1b808e3076425851958b.
    
    Reason for revert: [Sheriff] Reverting for now, as I suspect the cl is causing failures in https://ci.chromium.org/p/chromium/builders/ci/Fuchsia%20x64/b8886078290056207296
    
    Original change's description:
    > [Fuchsia] Implement Frame/SetPermissionState()
    > 
    > Now WebEngine implements SetPermissionState() which allows to grant
    > permissions for web features.
    > 
    > Bug: 922833
    > Change-Id: I356e54646ad870bb7882a5b9a95bc87deccba453
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2058086
    > Commit-Queue: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: David Dorwin <ddorwin@chromium.org>
    > Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#749428}
    
    TBR=ddorwin@chromium.org,wez@chromium.org,mlamouri@chromium.org,sergeyu@chromium.org
    
    Change-Id: Ib7f824e173044474d281705b865ba8072becd8e0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 922833
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2098787
    Reviewed-by: Kamila Hasanbega <hkamila@google.com>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/master@{#749608}

--- fuchsia/engine/browser/web_engine_permission_manager.h
+++ fuchsia/engine/browser/web_engine_permission_manager.h
@@ -3,22 +3,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_DELEGATE_H_
-#define FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_DELEGATE_H_
+#ifndef FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_MANAGER_H_
+#define FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_MANAGER_H_
 
+#include "base/macros.h"
 #include "content/public/browser/permission_controller_delegate.h"
 
-// PermissionControllerDelegate implementation for WebEngine. It redirects
-// permission redirects all calls to the appropriate FramePermissionController
-// instance.
-class WebEnginePermissionDelegate
+class WebEnginePermissionManager
     : public content::PermissionControllerDelegate {
  public:
-  WebEnginePermissionDelegate();
-  ~WebEnginePermissionDelegate() override;
-
-  WebEnginePermissionDelegate(WebEnginePermissionDelegate&) = delete;
-  WebEnginePermissionDelegate& operator=(WebEnginePermissionDelegate&) = delete;
+  WebEnginePermissionManager();
+  ~WebEnginePermissionManager() override;
 
   // content::PermissionControllerDelegate implementation:
   int RequestPermission(content::PermissionType permission,
@@ -53,6 +48,9 @@ class WebEnginePermissionDelegate
       base::RepeatingCallback<void(blink::mojom::PermissionStatus)> callback)
       override;
   void UnsubscribePermissionStatusChange(int subscription_id) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(WebEnginePermissionManager);
 };
 
-#endif  // FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_DELEGATE_H_
+#endif  // FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_MANAGER_H_
