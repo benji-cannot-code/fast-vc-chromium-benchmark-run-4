aaa055296fbd9137b4d89ee337d5458a1bbef573
Tom Anderson <thomasanderson@chromium.org>
2020-03-12 21:19:23 +0000

    Rename GtkVersionCheck to GtkCheckVersion
    This is for consistency with the GTK_CHECK_VERSION macro.
    
    Also simplify GtkCheckVersion and remove an embarrassing usage of
      if (...) return true; else return false;
    
    BUG=None
    R=nickdiego
    
    Change-Id: Ifc649470db28791438a022ce0b35978c0f9eb6b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2101302
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749885}

--- chrome/browser/ui/gtk/gtk_ui.cc
+++ chrome/browser/ui/gtk/gtk_ui.cc
@@ -145,7 +145,7 @@ class GtkButtonImageSource : public gfx::ImageSkiaSource {
       gfx::Rect focus_rect(width, height);
 
 #if !GTK_CHECK_VERSION(3, 90, 0)
-      if (!GtkVersionCheck(3, 14)) {
+      if (!GtkCheckVersion(3, 14)) {
         gint focus_pad;
         gtk_style_context_get_style(context, "focus-padding", &focus_pad,
                                     nullptr);
@@ -164,7 +164,7 @@ class GtkButtonImageSource : public gfx::ImageSkiaSource {
       }
 #endif
 
-      if (!GtkVersionCheck(3, 20)) {
+      if (!GtkCheckVersion(3, 20)) {
         GtkBorder border;
 #if GTK_CHECK_VERSION(3, 90, 0)
         gtk_style_context_get_border(context, &border);
@@ -238,7 +238,7 @@ int indicators_count;
 const char kUnknownContentType[] = "application/octet-stream";
 
 std::unique_ptr<SettingsProvider> CreateSettingsProvider(GtkUi* gtk_ui) {
-  if (GtkVersionCheck(3, 14))
+  if (GtkCheckVersion(3, 14))
     return std::make_unique<SettingsProviderGtk>(gtk_ui);
 #if defined(USE_GIO)
   return std::make_unique<SettingsProviderGSettings>(gtk_ui);
@@ -297,7 +297,7 @@ gfx::FontRenderParams GetGtkFontRenderParams() {
 }
 
 views::LinuxUI::WindowFrameAction GetDefaultMiddleClickAction() {
-  if (GtkVersionCheck(3, 14))
+  if (GtkCheckVersion(3, 14))
     return views::LinuxUI::WindowFrameAction::kNone;
   std::unique_ptr<base::Environment> env(base::Environment::Create());
   switch (base::nix::GetDesktopEnvironment(env.get())) {
@@ -748,7 +748,7 @@ bool GtkUi::AnimationsEnabled() const {
 }
 
 std::unique_ptr<views::NavButtonProvider> GtkUi::CreateNavButtonProvider() {
-  if (GtkVersionCheck(3, 14))
+  if (GtkCheckVersion(3, 14))
     return std::make_unique<gtk::NavButtonProviderGtk>();
   return nullptr;
 }
@@ -884,11 +884,11 @@ void GtkUi::UpdateColors() {
     colors_[ThemeProperties::COLOR_LOCATION_BAR_BORDER] = location_bar_border;
 
   inactive_selection_bg_color_ = GetSelectionBgColor(
-      GtkVersionCheck(3, 20) ? "GtkTextView#textview.view:backdrop "
+      GtkCheckVersion(3, 20) ? "GtkTextView#textview.view:backdrop "
                                "#text:backdrop #selection:backdrop"
                              : "GtkTextView.view:selected:backdrop");
   inactive_selection_fg_color_ =
-      GetFgColor(GtkVersionCheck(3, 20) ? "GtkTextView#textview.view:backdrop "
+      GetFgColor(GtkCheckVersion(3, 20) ? "GtkTextView#textview.view:backdrop "
                                           "#text:backdrop #selection:backdrop"
                                         : "GtkTextView.view:selected:backdrop");
 
