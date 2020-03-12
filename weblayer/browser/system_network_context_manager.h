35267b411b101e39e2633dca303c3a026c8e28e4
John Abd-El-Malek <jam@chromium.org>
2020-03-12 17:07:44 +0000

    Fix more os_crypt asserts with weblayer on desktop
    Change-Id: Ia53892d8c17d8caa689c1b5e74e4df249626dfaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2099477
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749728}

--- weblayer/browser/system_network_context_manager.h
+++ weblayer/browser/system_network_context_manager.h
@@ -35,6 +35,9 @@ class SystemNetworkContextManager {
   // Destroys the global SystemNetworkContextManager instance.
   static void DeleteInstance();
 
+  static network::mojom::NetworkContextParamsPtr
+  CreateDefaultNetworkContextParams(const std::string& user_agent);
+
   ~SystemNetworkContextManager();
 
   // Returns the System NetworkContext. Does any initialization of the
