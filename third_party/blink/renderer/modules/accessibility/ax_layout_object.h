eb3d4760a8c666bbf7428bd61273e4d93e18c0a7
Aaron Leventhal <aleventhal@chromium.org>
2020-03-12 05:31:22 +0000

    Clean up DOM vs layout traversal logic so it's easier to reason about
    Bug: None
    Change-Id: If1d43b693793dc7a6f2d0645e294e1cb0e1c1f63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2099333
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749530}

--- third_party/blink/renderer/modules/accessibility/ax_layout_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_layout_object.h
@@ -232,6 +232,7 @@ class MODULES_EXPORT AXLayoutObject : public AXNodeObject {
   bool IsPlaceholder() const;
   ax::mojom::Dropeffect ParseDropeffect(String& dropeffect) const;
   bool SelectionShouldFollowFocus() const;
+  bool ShouldUseDOMTraversal() const;
 
   static ax::mojom::TextDecorationStyle
   TextDecorationStyleToAXTextDecorationStyle(
