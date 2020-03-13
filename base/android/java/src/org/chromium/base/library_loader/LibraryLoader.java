81cd70c6c6bf2665075bba63701c3b5bc07f84ce
Benoit L <lizeb@chromium.org>
2020-03-13 19:58:06 +0000

    Reland "base/android: Remove linker tests support."
    This reverts commit 8e6f857273b66b827daf8364b6e53afcc2429b15.
    
    Reason for reland: The dependency which broke was removed in
    https://chromium-review.googlesource.com/c/chromium/src/+/2093606
    
    Original change's description:
    > base/android: Remove linker tests support.
    >
    > The linker tests don't run on bots (and likely not locally either) and
    > do not support the latest library loading patterns. This is the first
    > step to remove them, as they add non-trivial complexity in the codebase.
    >
    > Bug: 1059707
    > Change-Id: Ia14693419792f57dc4f65b2fc7f999e833aec42e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095059
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Reviewed-by: Egor Pasko <pasko@chromium.org>
    > Commit-Queue: Benoit L <lizeb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#748365}
    
    Bug: 1059707
    Change-Id: I3ad7347fdf4868d83ebf1895bee1bd85940ea324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2098785
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750278}

--- base/android/java/src/org/chromium/base/library_loader/LibraryLoader.java
+++ base/android/java/src/org/chromium/base/library_loader/LibraryLoader.java
@@ -257,10 +257,6 @@ public class LibraryLoader {
         return mUseModernLinker;
     }
 
-    public boolean areTestsEnabled() {
-        return NativeLibraries.sEnableLinkerTests;
-    }
-
     @RemovableInRelease
     public void enableJniChecks() {
         if (!BuildConfig.DCHECK_IS_ON) return;
@@ -712,7 +708,8 @@ public class LibraryLoader {
     }
 
     /**
-     * Override the library loader (normally with a mock) for testing.
+     * Overrides the library loader (normally with a mock) for testing.
+     *
      * @param loader the mock library loader.
      */
     @VisibleForTesting
