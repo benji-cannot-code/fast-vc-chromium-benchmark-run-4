f08dc2ac719d94265400b9754aa2d33f5e1fe901
ssid <ssid@chromium.org>
2020-03-13 02:47:45 +0000

    Add user actions for Android application state change events
    Counter is not really a good representation for foregrounded and
    backgrounded notifications in a trace. Add an user action for these
    states since they will be useful to see as a sequence. This will also
    be more clearly visible in traces.
    
    BUG= 1059307
    
    Change-Id: Ic24db48b02a4d97c04649f59d0777eba3ca63da8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090610
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: ssid <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749992}

--- base/android/application_status_listener.cc
+++ base/android/application_status_listener.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_jni_headers/ApplicationStatus_jni.h"
 #include "base/lazy_instance.h"
+#include "base/metrics/user_metrics.h"
 #include "base/observer_list_threadsafe.h"
 
 namespace base {
@@ -81,6 +82,20 @@ std::unique_ptr<ApplicationStatusListener> ApplicationStatusListener::New(
 void ApplicationStatusListener::NotifyApplicationStateChange(
     ApplicationState state) {
   TRACE_COUNTER1("browser", "ApplicationState", static_cast<int>(state));
+  switch (state) {
+    case APPLICATION_STATE_UNKNOWN:
+    case APPLICATION_STATE_HAS_DESTROYED_ACTIVITIES:
+      break;
+    case APPLICATION_STATE_HAS_RUNNING_ACTIVITIES:
+      RecordAction(UserMetricsAction("Android.LifeCycle.HasRunningActivities"));
+      break;
+    case APPLICATION_STATE_HAS_PAUSED_ACTIVITIES:
+      RecordAction(UserMetricsAction("Android.LifeCycle.HasPausedActivities"));
+      break;
+    case APPLICATION_STATE_HAS_STOPPED_ACTIVITIES:
+      RecordAction(UserMetricsAction("Android.LifeCycle.HasStoppedActivities"));
+      break;
+  }
   g_observers.Get().Notify(FROM_HERE, &ApplicationStatusListenerImpl::Notify,
                            state);
 }
