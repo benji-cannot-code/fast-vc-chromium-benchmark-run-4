3d3b486ef8f4337242ae2bd2a3d34899f15f9204
Clark DuVall <cduvall@chromium.org>
2020-03-13 05:45:29 +0000

    Reland "[WebLayer] Use crazy linker for M- and "/." trick for N+"
    This is a reland of 9b6604079eb9000f8fc7b1292de6e9ee058d987f
    
    Original change's description:
    > [WebLayer] Use crazy linker for M- and "/." trick for N+
    >
    > Instead of copying libraries on M-, this changes to using the crazy
    > linker, which loads the library without any knowledge of what libraries
    > have been loaded by the system linker. This requires manual JNI
    > registration. To make sure we only register WebLayer JNI when needed,
    > a minimal set of JNI methods is registered, and the full set is
    > registered later if we require WebView compatibility.
    >
    > In N+, we switch to prepending "/." to library paths instead of using
    > symlinks, which allows us to avoid writing any files to the apps data
    > directory. This simplifies the interface quite a bit, allowing
    > initializeWebViewCompatibilityMode() to be sync instead of async. I will
    > follow up with the Android team to make sure we have a better solution
    > for future versions of Android.
    >
    > The API has not been used in the client app yet, so it should be safe
    > to remove M- support for the copy library approach in the
    > implementation. The old version of initializeWebViewCompatibilityMode()
    > was kept so the WebLayer API version compat tests would still pass.
    >
    > TBR=agrieve@chromium.org
    >
    > Bug: 1051358
    > Change-Id: I2cd5ae0e025feaeb5c41cf9ddfafc91cd09fbad7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090024
    > Commit-Queue: Clark DuVall <cduvall@chromium.org>
    > Reviewed-by: Richard Coles <torne@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#749973}
    
    TBR=torne@chromium.org,agrieve@chromium.org
    
    Bug: 1051358
    Change-Id: Iecccefbbfdaa14c91a8d8b1a50543f4929912704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2101978
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750056}

---
+++ weblayer/browser/web_view_compatibility_helper_impl.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if defined(WEBLAYER_MANUAL_JNI_REGISTRATION)
+#include "base/android/library_loader/library_loader_hooks.h"  // nogncheck
+#include "weblayer/browser/java/jni/WebViewCompatibilityHelperImpl_jni.h"  // nogncheck
+#include "weblayer/browser/java/weblayer_jni_registration.h"  // nogncheck
+#endif
+
+namespace weblayer {
+namespace {
+#if defined(WEBLAYER_MANUAL_JNI_REGISTRATION)
+void RegisterNonMainDexNativesHook() {
+  RegisterNonMainDexNatives(base::android::AttachCurrentThread());
+}
+#endif
+}  // namespace
+
+bool MaybeRegisterNatives() {
+#if defined(WEBLAYER_MANUAL_JNI_REGISTRATION)
+  JNIEnv* env = base::android::AttachCurrentThread();
+  if (Java_WebViewCompatibilityHelperImpl_requiresManualJniRegistration(env)) {
+    if (!RegisterMainDexNatives(env))
+      return false;
+    base::android::SetNonMainDexJniRegistrationHook(
+        RegisterNonMainDexNativesHook);
+  }
+#endif
+  return true;
+}
+
+}  // namespace weblayer
