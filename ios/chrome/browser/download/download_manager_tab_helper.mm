d9bf0fd303d41856df5ad823846d26eb36b6fe3d
Ewann <ewannpv@google.com>
2020-03-13 15:31:09 +0000

    [iOS] Removes alert when persistent download is completed
    Removes alert message saying "Start New Download?" if downloads
    are completed and persistents.
    
    Bug: 1059998
    Change-Id: Icadffc087a372952c01142814541d74905b156ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2100769
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750169}

--- ios/chrome/browser/download/download_manager_tab_helper.mm
+++ ios/chrome/browser/download/download_manager_tab_helper.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #import "ios/chrome/browser/download/download_manager_tab_helper_delegate.h"
 #import "ios/chrome/browser/network_activity/network_activity_indicator_manager.h"
+#include "ios/web/common/features.h"
 #import "ios/web/public/download/download_task.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -41,7 +42,10 @@ void DownloadManagerTabHelper::CreateForWebState(
 void DownloadManagerTabHelper::Download(
     std::unique_ptr<web::DownloadTask> task) {
   __block std::unique_ptr<web::DownloadTask> block_task = std::move(task);
-  if (!task_) {
+  // If downloads are persistent, they cannot be lost once completed.
+  if (!task_ || (base::FeatureList::IsEnabled(
+                     web::features::kEnablePersistentDownloads) &&
+                 task_->GetState() == web::DownloadTask::State::kComplete)) {
     // The task is the first download for this web state.
     DidCreateDownload(std::move(block_task));
     return;
