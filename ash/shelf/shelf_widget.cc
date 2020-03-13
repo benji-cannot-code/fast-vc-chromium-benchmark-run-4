0091e04cfa48a4a903236dfcadf92a87132da862
Toni Barzic <tbarzic@chromium.org>
2020-03-13 04:41:54 +0000

    Updated logic to show and hide drag handle nudge
    Hide the nudge when the user taps it.
    
    Show the nudge when the user taps in-app shelf - previously this was
    restricted to drag handle only, but the desired behavior has since
    changed.
    
    BUG=1061142,1061069
    
    Change-Id: I4fefa2816ff712b54e92535203d87ba592b15d04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2101724
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750043}

--- ash/shelf/shelf_widget.cc
+++ ash/shelf/shelf_widget.cc
@@ -578,7 +578,7 @@ void ShelfWidget::ScheduleShowDragHandleNudge() {
 }
 
 void ShelfWidget::HideDragHandleNudge() {
-  delegate_view_->drag_handle()->HideDragHandleNudge();
+  delegate_view_->drag_handle()->HideDragHandleNudge(false /*hidden_by_tap*/);
 }
 
 void ShelfWidget::SetLoginShelfButtonOpacity(float target_opacity) {
@@ -1032,6 +1032,15 @@ void ShelfWidget::OnGestureEvent(ui::GestureEvent* event) {
   if (HandleLoginShelfGestureEvent(event_in_screen)) {
     event->StopPropagation();
     return;
+  }
+
+  // Tap on in-app shelf should show a contextual nudge for in-app to home
+  // gesture.
+  if (event->type() == ui::ET_GESTURE_TAP && ShelfConfig::Get()->is_in_app()) {
+    if (delegate_view_->drag_handle()->ShowDragHandleNudge()) {
+      event->StopPropagation();
+      return;
+    }
   }
 
   shelf_layout_manager()->ProcessGestureEventFromShelfWidget(&event_in_screen);
