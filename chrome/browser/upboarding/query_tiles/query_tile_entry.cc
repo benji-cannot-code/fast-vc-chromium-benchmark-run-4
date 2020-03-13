8d285ad3e78517bd4cab1166a748ead8f1835cdf
Hesen Zhang <hesen@google.com>
2020-03-13 19:30:16 +0000

    [Upboarding]: Init setup for QueryTileStore.
    - Defined QueryTile proto and in-memory struct.
    - Defined a collection_store interface.
    - Implemented QueryTileStore.
    
    TODO in next CLs: proto_conversion, query_tile_store_unittest,
    query_tile_group_manager(tree operations).
    
    Bug: 1060805
    Change-Id: I6da3c9be67b34843594476ae1c1302206a19f1fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2099228
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750265}

---
+++ chrome/browser/upboarding/query_tiles/query_tile_entry.cc
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/upboarding/query_tiles/query_tile_entry.h"
+
+namespace upboarding {
+
+ImageMetadata::ImageMetadata() = default;
+
+ImageMetadata::ImageMetadata(const std::string& id, const GURL& url)
+    : id(id), url(url) {}
+
+ImageMetadata::~ImageMetadata() = default;
+
+ImageMetadata::ImageMetadata(const ImageMetadata& other) = default;
+
+bool ImageMetadata::operator==(const ImageMetadata& other) const {
+  return id == other.id && url == other.url;
+}
+
+QueryTileEntry::QueryTileEntry() = default;
+
+QueryTileEntry::~QueryTileEntry() = default;
+
+QueryTileEntry::QueryTileEntry(const QueryTileEntry& other) = default;
+
+bool QueryTileEntry::operator==(const QueryTileEntry& other) const {
+  return id == other.id && query_text == other.query_text &&
+         display_text == other.display_text &&
+         accessibility_text == other.accessibility_text &&
+         image_metadatas.size() == other.image_metadatas.size() &&
+         children.size() == other.children.size();
+}
+
+}  // namespace upboarding
