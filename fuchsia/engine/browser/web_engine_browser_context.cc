03a87b9a757ad60408d9333a6e02b22fcd828567
Sergey Ulanov <sergeyu@chromium.org>
2020-03-13 00:03:11 +0000

    [Reland][Fuchsia] Implement Frame/SetPermissionState()
    Now WebEngine implements SetPermissionState() which allows to grant
    permissions for web features.
    
    This is a reland of crrev.com/749428. That change was reverted due to
    a bug in tests which is unrelated to this CL. These tests are being
    fixed in crrev.com/c/2101244 .
    
    TBR=ddorwin@chromium.org,mlamouri@chromium.org
    
    Bug: 922833
    Change-Id: Ibaddd570eee08c5744f71d1b471a9c1544de7154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2101315
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#749971}

--- fuchsia/engine/browser/web_engine_browser_context.cc
+++ fuchsia/engine/browser/web_engine_browser_context.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/resource_context.h"
 #include "fuchsia/engine/browser/web_engine_net_log_observer.h"
-#include "fuchsia/engine/browser/web_engine_permission_manager.h"
+#include "fuchsia/engine/browser/web_engine_permission_delegate.h"
 #include "services/network/public/cpp/network_switches.h"
 
 class WebEngineBrowserContext::ResourceContext
@@ -124,9 +124,9 @@ WebEngineBrowserContext::GetSSLHostStateDelegate() {
 
 content::PermissionControllerDelegate*
 WebEngineBrowserContext::GetPermissionControllerDelegate() {
-  if (!permission_manager_)
-    permission_manager_ = std::make_unique<WebEnginePermissionManager>();
-  return permission_manager_.get();
+  if (!permission_delegate_)
+    permission_delegate_ = std::make_unique<WebEnginePermissionDelegate>();
+  return permission_delegate_.get();
 }
 
 content::ClientHintsControllerDelegate*
--- fuchsia/engine/browser/web_engine_permission_delegate.h
+++ fuchsia/engine/browser/web_engine_permission_delegate.h
@@ -3,17 +3,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_MANAGER_H_
-#define FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_MANAGER_H_
+#ifndef FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_DELEGATE_H_
+#define FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_DELEGATE_H_
 
-#include "base/macros.h"
 #include "content/public/browser/permission_controller_delegate.h"
 
-class WebEnginePermissionManager
+// PermissionControllerDelegate implementation for WebEngine. It redirects
+// permission redirects all calls to the appropriate FramePermissionController
+// instance.
+class WebEnginePermissionDelegate
     : public content::PermissionControllerDelegate {
  public:
-  WebEnginePermissionManager();
-  ~WebEnginePermissionManager() override;
+  WebEnginePermissionDelegate();
+  ~WebEnginePermissionDelegate() override;
+
+  WebEnginePermissionDelegate(WebEnginePermissionDelegate&) = delete;
+  WebEnginePermissionDelegate& operator=(WebEnginePermissionDelegate&) = delete;
 
   // content::PermissionControllerDelegate implementation:
   int RequestPermission(content::PermissionType permission,
@@ -48,9 +53,6 @@ class WebEnginePermissionManager
       base::RepeatingCallback<void(blink::mojom::PermissionStatus)> callback)
       override;
   void UnsubscribePermissionStatusChange(int subscription_id) override;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(WebEnginePermissionManager);
 };
 
-#endif  // FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_MANAGER_H_
+#endif  // FUCHSIA_ENGINE_BROWSER_WEB_ENGINE_PERMISSION_DELEGATE_H_
