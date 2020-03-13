3987723292b64efbedfe5eae0aa7bdbd24e3ab01
Chris Blume <cblume@chromium.org>
2020-03-13 21:10:32 +0000

    Always-wide-color should prevent dynamic color
    Currently, the tests for always-wide and dynamic-wide are
    separate. A bug was found and fixed [1] where the always-wide
    support wasn't being checked when dynamic-wide was enabled.
    
    This CL returns false in the dynamic-wide test if always-wide
    is enabled.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/2102777
    
    Bug: 955158
    Change-Id: I7c5ce5dd2f7704687dee5753d699557450edb177
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103138
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Chris Blume <cblume@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750306}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "build/chromecast_buildflags.h"
 #include "components/viz/common/switches.h"
+#include "components/viz/common/viz_utils.h"
 #include "gpu/config/gpu_finch_features.h"
 
 #if defined(OS_ANDROID)
@@ -98,6 +99,8 @@ bool IsRecordingSkPicture() {
 
 #if defined(OS_ANDROID)
 bool IsDynamicColorGamutEnabled() {
+  if (viz::AlwaysUseWideColorGamut())
+    return false;
   return base::FeatureList::IsEnabled(kDynamicColorGamut);
 }
 #endif
