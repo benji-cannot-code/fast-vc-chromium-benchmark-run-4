71fd288227d83fb04421d2bff5091c5515c771ae
David Black <dmblack@google.com>
2020-03-13 22:52:09 +0000

    Support streaming suggestions/elements in AnimatedContainerView.
    Per UX feedback, this CL also removes the translate up portion of the
    entry animations for both UI elements and suggestion chips.
    
    Still TODO:
    - Add integration tests (tracking in b:149856606).
    - Handle A11Y events in response processing v2.
    
    Bug: b:129411551
    Change-Id: Ieec34aff071abb997cec69f6719131bb83510341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2088405
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/master@{#750334}

--- ash/assistant/ui/main_stage/element_animator.h
+++ ash/assistant/ui/main_stage/element_animator.h
@@ -54,7 +54,6 @@ class ElementAnimator {
   // The view that is being animated.
   virtual views::View* view() const;
 
- protected:
   // The layer that needs to be animated.
   // Used by the default implementations for |FadeOut| and |AbortAnimation|.
   // Defaults to |view()->layer()|.
