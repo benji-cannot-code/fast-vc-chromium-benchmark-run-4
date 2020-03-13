9fca25bf71d1466396c00dea8b797a1690521199
Mark Schillaci <mschillaci@google.com>
2020-03-13 22:24:11 +0000

    Added ACTION_IME_ENTER for Android on R SDK or higher
    This CL continues our previous work on adding the new
    accessibility action "ACTION_IME_ENTER" as an exposed
    action for Android users on R SDK or higher.
    
    We add the action during populate AccessibilityNodeInfo if
    the given node is a descendant of a form (defined
    as any kForm node).
    
    The aciton leverages the existing ImeAdapterImpl to perform
    an editor action based on the current TextInputAction of
    the form field, allowing the ime button to perform next,
    previous, done, etc based on the context of the click.
    
    Bug: 1057254
    Change-Id: I06171a2888d3bd1849f46edb1193ce7b79fe0365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096904
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/master@{#750326}

--- content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityImpl.java
@@ -30,6 +30,7 @@ import android.view.accessibility.AccessibilityManager;
 import android.view.accessibility.AccessibilityManager.AccessibilityStateChangeListener;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.accessibility.AccessibilityNodeProvider;
+import android.view.inputmethod.EditorInfo;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -41,6 +42,7 @@ import org.chromium.content.browser.RenderCoordinatesImpl;
 import org.chromium.content.browser.WindowEventObserver;
 import org.chromium.content.browser.WindowEventObserverManager;
 import org.chromium.content.browser.accessibility.captioning.CaptioningController;
+import org.chromium.content.browser.input.ImeAdapterImpl;
 import org.chromium.content.browser.webcontents.WebContentsImpl;
 import org.chromium.content.browser.webcontents.WebContentsImpl.UserDataFactory;
 import org.chromium.content_public.browser.AccessibilitySnapshotCallback;
@@ -112,7 +114,6 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProvider
     // TODO (mschillaci) - Replace with set IDs once R SDK finalizes values
     private static final int ACTION_IME_ENTER =
             Resources.getSystem().getIdentifier("accessibilityActionImeEnter", "id", "android");
-
     private static final int ACTION_PRESS_AND_HOLD =
             Resources.getSystem().getIdentifier("accessibilityActionPressAndHold", "id", "android");
 
@@ -525,6 +526,17 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProvider
         if (!isAccessibilityEnabled()
                 || !WebContentsAccessibilityImplJni.get().isNodeValid(
                         mNativeObj, WebContentsAccessibilityImpl.this, virtualViewId)) {
+            return false;
+        }
+
+        // TODO (mschillaci) Move this into the switch below once ACTION_IME_ENTER is constant
+        if (action == ACTION_IME_ENTER && ACTION_IME_ENTER != 0) {
+            if (mWebContents != null) {
+                if (ImeAdapterImpl.fromWebContents(mWebContents) != null) {
+                    return ImeAdapterImpl.fromWebContents(mWebContents)
+                            .performEditorAction(EditorInfo.IME_ACTION_NEXT);
+                }
+            }
             return false;
         }
 
@@ -1369,7 +1381,7 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProvider
             boolean canScrollDown, boolean canScrollLeft, boolean canScrollRight, boolean clickable,
             boolean editableText, boolean enabled, boolean focusable, boolean focused,
             boolean isCollapsed, boolean isExpanded, boolean hasNonEmptyValue,
-            boolean hasNonEmptyInnerText, boolean isRangeType) {
+            boolean hasNonEmptyInnerText, boolean isRangeType, boolean isForm) {
         addAction(node, AccessibilityNodeInfo.ACTION_NEXT_HTML_ELEMENT);
         addAction(node, AccessibilityNodeInfo.ACTION_PREVIOUS_HTML_ELEMENT);
         addAction(node, ACTION_SHOW_ON_SCREEN);
@@ -1449,6 +1461,10 @@ public class WebContentsAccessibilityImpl extends AccessibilityNodeProvider
 
         if (isRangeType) {
             addAction(node, ACTION_SET_PROGRESS);
+        }
+
+        if (isForm && ACTION_IME_ENTER != 0) {
+            addAction(node, ACTION_IME_ENTER);
         }
     }
 
