578ee503546fb6ff554869bcaf7b45a2c53164d2
Jinho Bang <jinho.bang@samsung.com>
2020-03-13 04:44:55 +0000

    PaymentRequest: Change PaymentAddressInit to AddressInit
    This is no behavior change. Perhaps the initial implementation used
    the wrong name.
    
    Related spec:
      https://w3c.github.io/payment-request/#addressinit-dictionary
    
    Bug: 984694
    Change-Id: I79967efd9c22c16247d003019cae30e80cf88f4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1940002
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#750045}

--- third_party/blink/renderer/modules/payments/address_init_type_converter.cc
+++ third_party/blink/renderer/modules/payments/address_init_type_converter.cc
@@ -3,15 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/renderer/modules/payments/payment_address_init_type_converter.h"
+#include "third_party/blink/renderer/modules/payments/address_init_type_converter.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace mojo {
 
-payments::mojom::blink::PaymentAddressPtr TypeConverter<
-    payments::mojom::blink::PaymentAddressPtr,
-    blink::PaymentAddressInit*>::Convert(const blink::PaymentAddressInit*
-                                             input) {
+payments::mojom::blink::PaymentAddressPtr
+TypeConverter<payments::mojom::blink::PaymentAddressPtr,
+              blink::AddressInit*>::Convert(const blink::AddressInit* input) {
   payments::mojom::blink::PaymentAddressPtr output =
       payments::mojom::blink::PaymentAddress::New();
   output->country = input->hasCountry() ? input->country() : g_empty_string;
--- third_party/blink/renderer/modules/payments/payment_request_event.cc
+++ third_party/blink/renderer/modules/payments/payment_request_event.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/workers/worker_global_scope.h"
 #include "third_party/blink/renderer/core/workers/worker_location.h"
-#include "third_party/blink/renderer/modules/payments/payment_address_init_type_converter.h"
+#include "third_party/blink/renderer/modules/payments/address_init_type_converter.h"
 #include "third_party/blink/renderer/modules/payments/payments_validators.h"
 #include "third_party/blink/renderer/modules/service_worker/respond_with_observer.h"
 #include "third_party/blink/renderer/modules/service_worker/service_worker_global_scope.h"
@@ -232,7 +232,7 @@ ScriptPromise PaymentRequestEvent::changePaymentMethod(
 
 ScriptPromise PaymentRequestEvent::changeShippingAddress(
     ScriptState* script_state,
-    PaymentAddressInit* shipping_address,
+    AddressInit* shipping_address,
     ExceptionState& exception_state) {
   if (change_payment_request_details_resolver_) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
