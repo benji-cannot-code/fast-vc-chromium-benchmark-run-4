5cf390ec9cd46ce9f8a0c04275e25da54ee3e3ab
Sophie Chang <sophiechang@chromium.org>
2020-03-13 20:45:06 +0000

    Disable all LoadingPredictorWithOptimizationGuide tests on CrOS/Win/Mac
    Bug: 106426
    Change-Id: Ia4b13ebbbc654e14fc547e3641e5b63938bbe173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2097365
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750302}

--- chrome/browser/predictors/loading_predictor_browsertest.cc
+++ chrome/browser/predictors/loading_predictor_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_features.h"
 #include "chrome/browser/metrics/subprocess_metrics_provider.h"
 #include "chrome/browser/navigation_predictor/navigation_predictor_preconnect_client.h"
@@ -1586,10 +1587,16 @@ INSTANTIATE_TEST_SUITE_P(UseLocalPrediction,
                          LoadingPredictorBrowserTestWithOptimizationGuide,
                          ::testing::Bool());
 
-// Disabled test due to consistent failure. crbug.com/1060426
-IN_PROC_BROWSER_TEST_P(
-    LoadingPredictorBrowserTestWithOptimizationGuide,
-    DISABLED_NavigationHasLocalPredictionNoOptimizationHint) {
+// https://crbug.com/1056693
+#if (defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_CHROMEOS))
+#define DISABLE_ON_WIN_MAC_CHROMEOS(x) DISABLED_##x
+#else
+#define DISABLE_ON_WIN_MAC_CHROMEOS(x) x
+#endif
+
+IN_PROC_BROWSER_TEST_P(LoadingPredictorBrowserTestWithOptimizationGuide,
+                       DISABLE_ON_WIN_MAC_CHROMEOS(
+                           NavigationHasLocalPredictionNoOptimizationHint)) {
   // Navigate the first time to fill the predictor's database and the HTTP
   // cache.
   GURL url = embedded_test_server()->GetURL(
@@ -1638,12 +1645,6 @@ IN_PROC_BROWSER_TEST_P(
   }
 }
 
-// https://crbug.com/1056693
-#if (defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_CHROMEOS))
-#define DISABLE_ON_WIN_MAC_CHROMEOS(x) DISABLED_##x
-#else
-#define DISABLE_ON_WIN_MAC_CHROMEOS(x) x
-#endif
 IN_PROC_BROWSER_TEST_P(
     LoadingPredictorBrowserTestWithOptimizationGuide,
     DISABLE_ON_WIN_MAC_CHROMEOS(
@@ -1807,7 +1808,8 @@ IN_PROC_BROWSER_TEST_P(
 
 IN_PROC_BROWSER_TEST_P(
     LoadingPredictorBrowserTestWithOptimizationGuide,
-    OptimizationGuidePredictionsNotAppliedForAlreadyCommittedNavigation) {
+    DISABLE_ON_WIN_MAC_CHROMEOS(
+        OptimizationGuidePredictionsNotAppliedForAlreadyCommittedNavigation)) {
   base::HistogramTester histogram_tester;
 
   GURL url = embedded_test_server()->GetURL("hints.com", "/simple.html");
@@ -1828,7 +1830,8 @@ IN_PROC_BROWSER_TEST_P(
 }
 
 IN_PROC_BROWSER_TEST_P(LoadingPredictorBrowserTestWithOptimizationGuide,
-                       OptimizationGuidePredictionsNotAppliedForRedirect) {
+                       DISABLE_ON_WIN_MAC_CHROMEOS(
+                           OptimizationGuidePredictionsNotAppliedForRedirect)) {
   base::HistogramTester histogram_tester;
 
   GURL destination_url =
