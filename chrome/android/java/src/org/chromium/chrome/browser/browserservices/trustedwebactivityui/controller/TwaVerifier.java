57bdf8002e97dbad4ab7df1981fb0b3129116155
Robbie McElrath <rmcelrath@chromium.org>
2020-03-13 20:31:27 +0000

    [WebLayer] Move browserservices.Origin to //components/embedder_support
    browserservices.Origin is used by Chrome's site settings page, which
    is being componentized so it can be used by WebLayer. This CL moves
    the class to //components/embedder_support.
    
    Bug: 1058597
    Change-Id: Ic47da47f372d1c27238efd2fee8d7bdcba6eb63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2090875
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750294}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/controller/TwaVerifier.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/trustedwebactivityui/controller/TwaVerifier.java
@@ -5,14 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.browserservices.trustedwebactivityui.controller;
 
+import androidx.annotation.Nullable;
+import androidx.browser.customtabs.CustomTabsService;
+
 import org.chromium.base.Promise;
 import org.chromium.chrome.browser.browserservices.BrowserServicesIntentDataProvider;
-import org.chromium.chrome.browser.browserservices.Origin;
 import org.chromium.chrome.browser.browserservices.OriginVerifier;
 import org.chromium.chrome.browser.customtabs.content.CustomTabActivityTabProvider;
 import org.chromium.chrome.browser.dependency_injection.ActivityScope;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.lifecycle.Destroyable;
+import org.chromium.components.embedder_support.util.Origin;
 import org.chromium.content_public.browser.WebContents;
 
 import java.util.HashSet;
@@ -20,9 +23,6 @@ import java.util.List;
 import java.util.Set;
 
 import javax.inject.Inject;
-
-import androidx.annotation.Nullable;
-import androidx.browser.customtabs.CustomTabsService;
 
 /**
  * Provides Trusted Web Activity specific behaviour for the {@link CurrentPageVerifier}.
