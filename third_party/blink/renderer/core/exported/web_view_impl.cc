875ffc1bc049026c3381e7a84bd670ff9ed6d58a
Sinan Sahin <sinansahin@google.com>
2020-03-13 19:19:13 +0000

    [Offline indicator v2] Fix blink layout height and fullscreen
    Today, most parts of the code assume the browser controls min-height
    is 0. This causes issues with the layout size and the fullscreen mode.
    The most obvious issues currently are:
    - If a min-height is set, ChromeFullscreenManager#controlsResizeView()
    always returns true since the controls aren't completely hidden.
    - Similarly if a min-height is set, position: fixed elements are sized
    as if the controls are fully showing.
    
    This CL changes the "browser controls resize/shrink blink" logic to try
    and fix the issues listed above. With the changes in this CL, the
    controlsResizeView bit will be false if the controls are at the
    min-height.
    
    Bug: 1049301
    Change-Id: Ieb95e92c983b56b42a640f726c60c2eec9964064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2048276
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750260}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -1268,7 +1268,8 @@ void WebViewImpl::UpdateICBAndResizeViewport(
   if (GetBrowserControls().PermittedState() ==
           cc::BrowserControlsState::kBoth &&
       !GetBrowserControls().ShrinkViewport()) {
-    icb_size.Expand(0, -GetBrowserControls().TotalHeight());
+    icb_size.Expand(0, -(GetBrowserControls().TotalHeight() -
+                         GetBrowserControls().TotalMinHeight()));
   }
 
   GetPageScaleConstraintsSet().DidChangeInitialContainingBlockSize(icb_size);
