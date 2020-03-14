b895a959bcf2a8b90bec5a828c6cecf594ea0565
Sergey Ulanov <sergeyu@chromium.org>
2020-03-14 07:24:21 +0000

    [Fuchsia] Implement microphone support for WebEngine
    1. Added FuchsiaAudioCapturerSource that implements AudioCapturerSource
       on top of fuchsia::media::AudioCapturer.
    2. Added FuchsiaAudioDeviceFactory to create FuchsiaAudioCapturerSource
       instead of AudioInputDevice.
    3. Updated RendererMainPlatformDelegate to create
       FuchsiaAudioDeviceFactory on Fuchsia.
    4. Added RequestMediaAccessPermission() implementation in FrameImpl.
    
    Bug: 1047390
    Change-Id: Ia9164cc189f58c11ad15f454793efd533fbab96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071042
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750394}

--- fuchsia/engine/browser/frame_impl.h
+++ fuchsia/engine/browser/frame_impl.h
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
-#include "base/memory/platform_shared_memory_region.h"
+#include "base/memory/read_only_shared_memory_region.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "fuchsia/engine/browser/accessibility_bridge.h"
@@ -56,6 +56,9 @@ class FrameImpl : public fuchsia::web::Frame,
             ContextImpl* context,
             fidl::InterfaceRequest<fuchsia::web::Frame> frame_request);
   ~FrameImpl() override;
+
+  FrameImpl(const FrameImpl&) = delete;
+  FrameImpl& operator=(const FrameImpl&) = delete;
 
   uint64_t media_session_id() const { return media_session_id_; }
 
@@ -202,6 +205,10 @@ class FrameImpl : public fuchsia::web::Frame,
                       const gfx::Rect& initial_rect,
                       bool user_gesture,
                       bool* was_blocked) override;
+  void RequestMediaAccessPermission(
+      content::WebContents* web_contents,
+      const content::MediaStreamRequest& request,
+      content::MediaResponseCallback callback) override;
 
   // content::WebContentsObserver implementation.
   void ReadyToCommitNavigation(
@@ -243,8 +250,6 @@ class FrameImpl : public fuchsia::web::Frame,
   gfx::Size render_size_override_;
 
   fidl::Binding<fuchsia::web::Frame> binding_;
-
-  DISALLOW_COPY_AND_ASSIGN(FrameImpl);
 };
 
 #endif  // FUCHSIA_ENGINE_BROWSER_FRAME_IMPL_H_
