74382c3e21d236eaad0b6f02b871599e1b87f856
Rouslan Solomakhin <rouslan@chromium.org>
2020-03-15 11:46:18 +0000

    [Web Payment] Do not dim screen when processing minimal UI.
    Before this patch, Chrome dimmed the screen after the user has payment
    in minimal UI, because the reconciler was not aware that a bottom sheet
    was being displayed.
    
    This patch applies the same bottom sheet logic from reconciler to both
    expandable payment handler UI and minimal UI.
    
    After this patch, Chrome UI is not dimmed after user confirms payment in
    minimal UI.
    
    Bug: 1005076
    Change-Id: Id9e5f220cb8ba7bef4bb06c6f72e725e971ce9e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2097415
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750427}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
@@ -287,7 +287,7 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
 
     /**
      * The top level container of this UI. When needing to call show() or hide(), use {@link
-     * PaymentUisShowStateReconciler}'s showPaymentRequestDialogWhenNoPaymentHandlerUi() and
+     * PaymentUisShowStateReconciler}'s showPaymentRequestDialogWhenNoBottomSheet() and
      * hidePaymentRequestDialog() instead.
      */
     private final DimmingDialog mDialog;
@@ -423,7 +423,7 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
      */
     public void show() {
         mDialog.addBottomSheetView(mRequestView);
-        mPaymentUisShowStateReconciler.showPaymentRequestDialogWhenNoPaymentHandlerUi();
+        mPaymentUisShowStateReconciler.showPaymentRequestDialogWhenNoBottomSheet();
         mClient.getDefaultPaymentInformation(new Callback<PaymentInformation>() {
             @Override
             public void onResult(PaymentInformation result) {
@@ -460,7 +460,7 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
     public void dimBackground() {
         // Intentionally do not add the bottom sheet view to mDialog so that only the scrim part of
         // the dialog will be shown.
-        mPaymentUisShowStateReconciler.showPaymentRequestDialogWhenNoPaymentHandlerUi();
+        mPaymentUisShowStateReconciler.showPaymentRequestDialogWhenNoBottomSheet();
     }
 
     /**
@@ -956,7 +956,7 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
         assert mIsProcessingPayClicked;
         mIsProcessingPayClicked = false;
         changeSpinnerVisibility(false);
-        mPaymentUisShowStateReconciler.showPaymentRequestDialogWhenNoPaymentHandlerUi();
+        mPaymentUisShowStateReconciler.showPaymentRequestDialogWhenNoBottomSheet();
         updatePayButtonEnabled();
     }
 
@@ -978,7 +978,7 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
         assert mIsProcessingPayClicked;
 
         changeSpinnerVisibility(true);
-        mPaymentUisShowStateReconciler.showPaymentRequestDialogWhenNoPaymentHandlerUi();
+        mPaymentUisShowStateReconciler.showPaymentRequestDialogWhenNoBottomSheet();
     }
 
     private void changeSpinnerVisibility(boolean showSpinner) {
@@ -1430,8 +1430,8 @@ public class PaymentRequestUI implements DimmingDialog.OnDismissListener, View.O
 
     /**
      * Set the visibility state of the dialog. Use {@link PaymentUisShowStateReconciler}'s
-     * showPaymentRequestDialogWhenNoPaymentHandlerUi() and hidePaymentRequestDialog() instead of
-     * calling this method directly.
+     * showPaymentRequestDialogWhenNoBottomSheet() and hidePaymentRequestDialog() instead of calling
+     * this method directly.
      * @param visible True to show the dialog, false to hide the dialog.
      */
     public void setVisible(boolean visible) {
