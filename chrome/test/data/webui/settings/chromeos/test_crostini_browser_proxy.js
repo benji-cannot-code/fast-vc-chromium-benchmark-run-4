de5a2c369d80865ee5e757f2a4e4548e0b49e15e
David Munro <davidmunro@google.com>
2020-03-15 22:20:15 +0000

    crostini: Disk Resizing UI in settings.
    Needs some polish but works end-to-end, all the functionality is there.
    This is still behind a flag.
    
    Screenshots:
    * Loading: https://drive.google.com/file/d/1-F7R0fd2FHiTcoOxyWwoFy_W23J7vY_R/view?usp=sharing
    * Resize: https://drive.google.com/file/d/1-7VtbZd2j2fAAYVtB5anV_Ayk6Xty5xx/view?usp=sharing
    * Settings page to open dialogue: https://drive.google.com/file/d/1-HqyovNWKWz4j-l21RkV0aWIUmKRjCko/view?usp=sharing
    
    Test: Browser, unit, manual
    Bug: chromium:858815
    Change-Id: I2967402d2bcd1ebb66e83a5298adedda769deb9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2096155
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: jimmy gong <jimmyxgong@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Auto-Submit: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/master@{#750438}

--- chrome/test/data/webui/settings/chromeos/test_crostini_browser_proxy.js
+++ chrome/test/data/webui/settings/chromeos/test_crostini_browser_proxy.js
@@ -19,9 +19,26 @@ class TestCrostiniBrowserProxy extends TestBrowserProxy {
       'requestCrostiniUpgraderDialogStatus',
       'requestCrostiniContainerUpgradeAvailable',
       'addCrostiniPortForward',
+      'getCrostiniDiskInfo',
+      'resizeCrostiniDisk',
     ]);
     this.sharedUsbDevices = [];
     this.removeSharedPathResult = true;
+    this.methodCalls_ = {};
+  }
+
+  getNewPromiseFor(name) {
+    return new Promise((resolve, reject) => {
+      this.methodCalls_[name] = {name, resolve, reject};
+    });
+  }
+
+  async resolvePromise(name, ...args) {
+    await this.methodCalls_[name].resolve(...args);
+  }
+
+  async rejectPromise(name, ...args) {
+    await this.methodCalls_[name].reject(...args);
   }
 
   /** @override */
@@ -101,5 +118,17 @@ class TestCrostiniBrowserProxy extends TestBrowserProxy {
         'addCrostiniPortForward', vmName, containerName, portNumber,
         protocolIndex, label);
     return Promise.resolve(true);
+  }
+
+  /** @override */
+  getCrostiniDiskInfo(vmName) {
+    this.methodCalled('getCrostiniDiskInfo', vmName);
+    return this.getNewPromiseFor('getCrostiniDiskInfo');
+  }
+
+  /** @override */
+  resizeCrostiniDisk(vmName, newSizeBytes) {
+    this.methodCalled('resizeCrostiniDisk', vmName, newSizeBytes);
+    return this.getNewPromiseFor('resizeCrostiniDisk');
   }
 }
