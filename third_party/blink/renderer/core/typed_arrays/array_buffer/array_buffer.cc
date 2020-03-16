9d3b7b4de6fc277ef2afd6795e0c8ef3d4e5ec7b
Andreas Haas <ahaas@chromium.org>
2020-03-16 20:08:47 +0000

    [arraybuffer] Merge ArrayBufferView into DOMArrayBufferView
    This CL merges ArrayBufferView into DOMArrayBufferView. This requires
    to also merge TypedArray into DOMTypedArray. The merge is done as
    follows:
    1) Move the internal fields of ArrayBufferView to DOMArrayBufferView;
    2) Move the internal fields of TypedArray to DOMTypedArray;
    3) Change all functions in DOMArrayBufferView to use the internal
       fields instead of a reference to an ArrayBufferView;
    4) Same as 3), but for DOMTypedArray;
    5) Adjust all the constructors.
    
    R=ulan@chromium.org, haraken@chromium.org
    
    Bug: chromium:1008840
    Change-Id: Iec9b01546131468a42e1d6644922671d9042141b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2102535
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750689}

--- third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer.cc
+++ third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer.h"
 
 #include "base/memory/scoped_refptr.h"
-#include "third_party/blink/renderer/core/typed_arrays/array_buffer/array_buffer_view.h"
 
 namespace blink {
 
