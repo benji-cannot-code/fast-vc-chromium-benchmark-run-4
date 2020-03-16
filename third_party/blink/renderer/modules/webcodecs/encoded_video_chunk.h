7215ecc69cbd2d0d719d649b39931a5e5841d28b
Andreas Haas <ahaas@chromium.org>
2020-03-16 08:23:28 +0000

    [webcodecs] Replace use of deprecated ArrayBuffer
    This CL replaces the use of ArrayBuffer in EncodedVideoChunk by
    DOMArrayBuffer. ArrayBuffer is deprecated.
    
    R=yukishiino@chromium.org
    
    Bug: chromium:1008840
    Change-Id: I3b1bdefce6a7e60e6b30ddaf6f55dbe1b748da3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2102584
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750489}

--- third_party/blink/renderer/modules/webcodecs/encoded_video_chunk.h
+++ third_party/blink/renderer/modules/webcodecs/encoded_video_chunk.h
@@ -13,15 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class ArrayBuffer;
 class DOMArrayBuffer;
 
 class MODULES_EXPORT EncodedVideoChunk final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  EncodedVideoChunk(EncodedVideoMetadata metadata,
-                    scoped_refptr<ArrayBuffer> buffer);
+  EncodedVideoChunk(EncodedVideoMetadata metadata, DOMArrayBuffer* buffer);
 
   static EncodedVideoChunk* Create(String type,
                                    uint64_t timestamp,
@@ -42,9 +40,14 @@ class MODULES_EXPORT EncodedVideoChunk final : public ScriptWrappable {
   }
   DOMArrayBuffer* data() const;
 
+  void Trace(Visitor* visitor) override {
+    visitor->Trace(buffer_);
+    ScriptWrappable::Trace(visitor);
+  }
+
  private:
   EncodedVideoMetadata metadata_;
-  scoped_refptr<ArrayBuffer> buffer_;
+  Member<DOMArrayBuffer> buffer_;
 };
 
 }  // namespace blink
