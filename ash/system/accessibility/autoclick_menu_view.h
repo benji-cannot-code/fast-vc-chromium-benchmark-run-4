a2abe407ec9ff6ab8171a57624a42b74b9e3f3a8
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-03-16 18:27:48 +0000

    [Ash] Move buttons class from autoclick menu outside of .cc file
    This CL moves out AutoclickMenuButton into a separate class so it can be
    in the future reused by other menus that may want those buttons.
    
    Bug: 1061068
    Change-Id: Ie776231ea5f319f3405eb7d2b04c09497efe7b7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2100792
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750634}

--- ash/system/accessibility/autoclick_menu_view.h
+++ ash/system/accessibility/autoclick_menu_view.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-class AutoclickMenuButton;
+class FloatingMenuButton;
 
 // View for the Automatic Clicks menu bubble, which holds the Automatic Clicks
 // Menu.
@@ -60,13 +60,13 @@ class AutoclickMenuView : public views::View, public views::ButtonListener {
 
  private:
   // Unowned. Owned by views hierarchy.
-  AutoclickMenuButton* left_click_button_;
-  AutoclickMenuButton* right_click_button_;
-  AutoclickMenuButton* double_click_button_;
-  AutoclickMenuButton* drag_button_;
-  AutoclickMenuButton* scroll_button_ = nullptr;
-  AutoclickMenuButton* pause_button_;
-  AutoclickMenuButton* position_button_;
+  FloatingMenuButton* left_click_button_;
+  FloatingMenuButton* right_click_button_;
+  FloatingMenuButton* double_click_button_;
+  FloatingMenuButton* drag_button_;
+  FloatingMenuButton* scroll_button_ = nullptr;
+  FloatingMenuButton* pause_button_;
+  FloatingMenuButton* position_button_;
 
   // The most recently selected event_type_ excluding kNoAction. This is used
   // when the pause button is selected in order to unpause and reset to the
