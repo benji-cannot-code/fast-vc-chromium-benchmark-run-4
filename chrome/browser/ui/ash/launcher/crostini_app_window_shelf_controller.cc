1d482a639647beab0c3a3ea2a03b129c4158d911
Eric Willigers <ericwilligers@chromium.org>
2020-03-16 19:36:26 +0000

    Remove unnecessary extensions includes
    Various includes of extensions headers from
    chrome/browser/ui/ are no longer required.
    
    Change-Id: Ic841cf3ed36b1aa703250c73712e67995d0ba48f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2104738
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750670}

--- chrome/browser/ui/ash/launcher/crostini_app_window_shelf_controller.cc
+++ chrome/browser/ui/ash/launcher/crostini_app_window_shelf_controller.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/exo/permission.h"
 #include "components/exo/shell_surface_util.h"
 #include "components/user_manager/user_manager.h"
-#include "extensions/browser/app_window/app_window.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/env.h"
 #include "ui/base/base_window.h"
--- chrome/browser/ui/web_applications/web_app_launch_utils.cc
+++ chrome/browser/ui/web_applications/web_app_launch_utils.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/strings/string_util.h"
-#include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -23,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
-#include "extensions/browser/extension_registry.h"
-#include "extensions/common/extension.h"
 #include "third_party/blink/public/mojom/renderer_preferences.mojom.h"
 #include "url/gurl.h"
 
