a49f68b1fab26fa98c538bf55e334de52e23d009
Lukasz Anforowicz <lukasza@chromium.org>
2020-03-16 21:57:01 +0000

    Account for CorbAllowlistAlsoAppliesToOorCors in test expectations.
    Some existing extension tests expect the legacy security model, where
    content scripts are able to bypass CORS.  This model will change once
    CorbAllowlistAlsoAppliesToOorCors is enabled by default (soon, early in
    M83).  This CL makes test tweaks that are a prerequisite for enabling
    this feature by default.
    
    Once CorbAllowlistAlsoAppliesToOorCors is enabled by default:
    - Most coverage for the new and legacy mode should be already provided
      by the existing tests under CrossOriginReadBlockingExtensionTest
      suite.
    - Some test coverage is only provided by the tests touched by this CL.
      Temporarily (for a few weeks), this CL tweaks the affected tests, so
      that they continue running in the legacy security mode (to ensure we
      don't loose the test coverage).  Within M83 we should follow-up with a
      separate CL to migrate the tests to the
      CrossOriginReadBlockingExtensionTest suite - this migration will help
      cover allowlisting effects (allowlisted extensions will continue to
      operate in the legacy security mode) and interactions with other
      features covered by the test matric of the
      CrossOriginReadBlockingExtensionTest suite.
    
    Bug: 1061567
    Change-Id: I67214f2a9bcffbeefb7766d8ce23e18c688d69a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103141
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750728}

--- chrome/browser/extensions/background_xhr_browsertest.cc
+++ chrome/browser/extensions/background_xhr_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_with_management_policy_apitest.h"
@@ -35,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/ssl/client_cert_store.h"
 #include "net/ssl/ssl_server_config.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "services/network/public/cpp/features.h"
 #include "url/gurl.h"
 
 namespace extensions {
@@ -106,7 +108,18 @@ IN_PROC_BROWSER_TEST_F(BackgroundXhrTest, HttpAuth) {
 
 class BackgroundXhrWebstoreTest : public ExtensionApiTestWithManagementPolicy {
  public:
-  BackgroundXhrWebstoreTest() = default;
+  BackgroundXhrWebstoreTest() {
+    // TODO(lukasza): https://crbug.com/1061567: Migrate tests related to
+    // cross-origin requests from content scripts into the
+    // CrossOriginReadBlockingExtensionTest suite (which already covers test
+    // matrix of various enabled/disabled features).
+    //
+    // Affected tests:
+    // - BackgroundXhrWebstoreTest.PolicyContentScriptXHR
+    scoped_feature_list_.InitAndDisableFeature(
+        network::features::kCorbAllowlistAlsoAppliesToOorCors);
+  }
+
   ~BackgroundXhrWebstoreTest() override = default;
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -170,6 +183,7 @@ class BackgroundXhrWebstoreTest : public ExtensionApiTestWithManagementPolicy {
   }
 
  private:
+  base::test::ScopedFeatureList scoped_feature_list_;
 
   DISALLOW_COPY_AND_ASSIGN(BackgroundXhrWebstoreTest);
 };
