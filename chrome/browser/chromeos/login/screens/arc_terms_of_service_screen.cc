fcd2cc3e95058a46f9718dc9b61c592698218066
Roman Aleksandrov <raleksandrov@google.com>
2020-03-16 16:05:45 +0000

    ArcTermsOfServiceScreen: Add UMA stats
    Track review following setup box is checked.
    
    Change-Id: I99be8c503159194018e4af1b1f1be91237e5e8f3
    Bug: 1056740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095103
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750568}

--- chrome/browser/chromeos/login/screens/arc_terms_of_service_screen.cc
+++ chrome/browser/chromeos/login/screens/arc_terms_of_service_screen.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/screens/arc_terms_of_service_screen.h"
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/chromeos/login/wizard_controller.h"
 #include "chrome/browser/metrics/metrics_reporting_state.h"
 #include "chrome/browser/profiles/profile.h"
@@ -82,6 +83,8 @@ void ArcTermsOfServiceScreen::OnSkip() {
 }
 
 void ArcTermsOfServiceScreen::OnAccept(bool review_arc_settings) {
+  base::UmaHistogramBoolean("OOBE.ArcTermsOfServiceScreen.ReviewFollowingSetup",
+                            review_arc_settings);
   if (review_arc_settings) {
     Profile* const profile = ProfileManager::GetActiveUserProfile();
     CHECK(profile);
