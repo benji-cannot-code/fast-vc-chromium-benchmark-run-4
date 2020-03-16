fbfbc5216224804a3a50c5b0d31aada15ff4269a
Gabriel Charette <gab@chromium.org>
2020-03-16 15:50:15 +0000

    [blink] Migrate worker_pool:: interface away from base::ThreadPool()-as-a-trait
    base::ThreadPool:: (API as a destination) is the new desired paradigm.
    
    worker_pool:: was already API-as-destination; can remove redundant
    ThreadPool() trait with new API.
    
    R=haraken@chromium.org
    
    Bug: 1026641
    Change-Id: Ida08feab710143da2308ef1361550a9aced39d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105454
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750565}

--- third_party/blink/renderer/platform/scheduler/common/worker_pool.cc
+++ third_party/blink/renderer/platform/scheduler/common/worker_pool.cc
@@ -6,24 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/public/worker_pool.h"
 
 #include "base/location.h"
-#include "base/task/post_task.h"
+#include "base/task/thread_pool.h"
 
 namespace blink {
 
 namespace worker_pool {
 
 void PostTask(const base::Location& location, CrossThreadOnceClosure closure) {
-  PostTask(
-      location,
-      {base::ThreadPool(), base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
-      std::move(closure));
+  PostTask(location, {base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
+           std::move(closure));
 }
 
 void PostTask(const base::Location& location,
               const base::TaskTraits& traits,
               CrossThreadOnceClosure closure) {
-  base::PostTask(location, traits,
-                 ConvertToBaseOnceCallback(std::move(closure)));
+  base::ThreadPool::PostTask(location, traits,
+                             ConvertToBaseOnceCallback(std::move(closure)));
 }
 
 }  // namespace worker_pool
