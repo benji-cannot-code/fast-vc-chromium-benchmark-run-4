3d416571804932b31fc2e3d88e52b352246f2a6a
Mark Yacoub <markyacoub@google.com>
2020-03-16 18:57:36 +0000

    Ozone: Reset Connector Caching when a connector is added/removed
    Refresh the DRM Connectors caching when adding or removing a connector.
    An MST connector creates new connector drm props when connected. It has
    to be cached to be accessed during modeset.
    
    BUG=1056900
    TEST=You're able to connect an MST DP when modeset is atomic.
    HardwareDisplayPlaneManagerTest.ResettingConnectorCache
    
    Change-Id: I6d1a55fd7e1feabe0f66b3305ef5a8400f15aa74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095702
    Commit-Queue: Mark Yacoub <markyacoub@google.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750652}

--- ui/ozone/platform/drm/gpu/mock_drm_device.cc
+++ ui/ozone/platform/drm/gpu/mock_drm_device.cc
@@ -101,6 +101,8 @@ MockDrmDevice::MockDrmDevice(std::unique_ptr<GbmDevice> gbm_device)
   plane_manager_ = std::make_unique<HardwareDisplayPlaneManagerLegacy>(this);
 }
 
+MockDrmDevice::~MockDrmDevice() = default;
+
 // static
 ScopedDrmPropertyBlobPtr MockDrmDevice::AllocateInFormatsBlob(
     uint32_t id,
@@ -146,10 +148,8 @@ bool MockDrmDevice::InitializeStateWithResult(
     const std::vector<PlaneProperties>& plane_properties,
     const std::map<uint32_t, std::string>& property_names,
     bool use_atomic) {
-  crtc_properties_ = crtc_properties;
-  connector_properties_ = connector_properties;
-  plane_properties_ = plane_properties;
-  property_names_ = property_names;
+  UpdateState(crtc_properties, connector_properties, plane_properties,
+              property_names);
   if (use_atomic) {
     plane_manager_ = std::make_unique<HardwareDisplayPlaneManagerAtomic>(this);
   } else {
@@ -159,7 +159,16 @@ bool MockDrmDevice::InitializeStateWithResult(
   return plane_manager_->Initialize();
 }
 
-MockDrmDevice::~MockDrmDevice() = default;
+void MockDrmDevice::UpdateState(
+    const std::vector<CrtcProperties>& crtc_properties,
+    const std::vector<ConnectorProperties>& connector_properties,
+    const std::vector<PlaneProperties>& plane_properties,
+    const std::map<uint32_t, std::string>& property_names) {
+  crtc_properties_ = crtc_properties;
+  connector_properties_ = connector_properties;
+  plane_properties_ = plane_properties;
+  property_names_ = property_names;
+}
 
 ScopedDrmResourcesPtr MockDrmDevice::GetResources() {
   ScopedDrmResourcesPtr resources(DrmAllocator<drmModeRes>());
