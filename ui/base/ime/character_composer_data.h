ca0d8e0b7ce4d4bc8abad09bb23bb2d956e895fe
Kevin Schoedel <kpschoedel@chromium.org>
2020-03-16 14:31:49 +0000

    Fix combining characters for c with acute accent
    The table had ´c map to ç instead of ć, imitating GTK from circa 2008.
    This was overlooked in the conversion, and left no way for users to
    type ć (used in Polish for example).
    
    I speculate that GTK was imitating the Windows ‘US International’ layout,
    which has that mapping. We have a separate section of the combining table
    for the Windows-like ‘US International PC’ that does map ´c map to ç,
    but this should not still happen for other layouts.
    
    Bug: 1061753
    Change-Id: I113b3d6c3fda6b93f7f27296b1a30dae6aa0b668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2102776
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Kevin Schoedel <kpschoedel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750545}

--- ui/base/ime/character_composer_data.h
+++ ui/base/ime/character_composer_data.h
@@ -1074,7 +1074,7 @@ static const uint16_t kCompositionsTree[] = {
     0x0041,  // capital a
     0x00C1,  // -> capital a with acute
     0x0043,  // capital c
-    0x00C7,  // -> capital c with cedilla
+    0x0106,  // -> capital c with acute
     0x0045,  // capital e
     0x00C9,  // -> capital e with acute
     0x0047,  // capital g
@@ -1108,7 +1108,7 @@ static const uint16_t kCompositionsTree[] = {
     0x0061,  // letter a
     0x00E1,  // -> letter a with acute
     0x0063,  // letter c
-    0x00E7,  // -> letter c with cedilla
+    0x0107,  // -> letter c with acute
     0x0065,  // letter e
     0x00E9,  // -> letter e with acute
     0x0067,  // letter g
@@ -17135,7 +17135,7 @@ static const uint16_t kCompositionsTree[] = {
     0x0041,  // capital a
     0x00C1,  // -> capital a with acute
     0x0043,  // capital c
-    0x00C7,  // -> capital c with cedilla
+    0x0106,  // -> capital c with acute
     0x0045,  // capital e
     0x00C9,  // -> capital e with acute
     0x0047,  // capital g
@@ -17171,7 +17171,7 @@ static const uint16_t kCompositionsTree[] = {
     0x0061,  // letter a
     0x00E1,  // -> letter a with acute
     0x0063,  // letter c
-    0x00E7,  // -> letter c with cedilla
+    0x0107,  // -> letter c with acute
     0x0065,  // letter e
     0x00E9,  // -> letter e with acute
     0x0067,  // letter g
