7a4d637a56447b2848ba207691fd803ea9eeda16
Matthew Mourgos <mmourgos@chromium.org>
2020-03-16 22:44:15 +0000

    OOBE: Show pin oobe screen in clamshell for touch centric devices
    Bug: 1060402
    Change-Id: Ie121d9e2a212b34296f65da1dbd592ecbe9ad545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2101970
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750752}

--- chrome/browser/chromeos/login/screens/discover_screen.cc
+++ chrome/browser/chromeos/login/screens/discover_screen.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/users/chrome_user_manager_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/chromeos/login/discover_screen_handler.h"
+#include "chromeos/constants/chromeos_switches.h"
 #include "components/prefs/pref_service.h"
 
 namespace chromeos {
@@ -35,12 +36,20 @@ DiscoverScreen::~DiscoverScreen() {
 void DiscoverScreen::ShowImpl() {
   PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
   if (chrome_user_manager_util::IsPublicSessionOrEphemeralLogin() ||
-      !ash::TabletMode::Get()->InTabletMode() ||
       !chromeos::quick_unlock::IsPinEnabled(prefs) ||
       chromeos::quick_unlock::IsPinDisabledByPolicy(prefs)) {
     exit_callback_.Run();
     return;
   }
+
+  // Skip the screen if the device is not in tablet mode, unless tablet mode
+  // first user run is forced on the device.
+  if (!ash::TabletMode::Get()->InTabletMode() &&
+      !chromeos::switches::ShouldOobeUseTabletModeFirstRun()) {
+    exit_callback_.Run();
+    return;
+  }
+
   view_->Show();
 }
 
