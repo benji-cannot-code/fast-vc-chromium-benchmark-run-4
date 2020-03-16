e329b0e45ff3f41904c2d3642ee6094707708a68
Side Yilmaz <sideyilmaz@chromium.org>
2020-03-16 17:24:15 +0000

    Replace deprecated getLastUsedProfile function in ExploreSitesBackgroundTask.
    Profile#getLastUsedProfile| is deprecated and replaced with
    |Profile#getLastUsedRegularProfile|. This CL updates
    ExploreSitesBackgroundTask class.
    
    Note: This change is only code clean-up, does not change any behavior.
    
    Bug: 1041781
    Change-Id: I8e6be991e2358fdb2130508c8baf0a46861a3e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095131
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750598}

--- chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesBackgroundTask.java
+++ chrome/android/java/src/org/chromium/chrome/browser/explore_sites/ExploreSitesBackgroundTask.java
@@ -43,7 +43,7 @@ public class ExploreSitesBackgroundTask extends NativeBackgroundTask {
 
     @VisibleForTesting
     protected Profile getProfile() {
-        if (mProfile == null) mProfile = Profile.getLastUsedProfile();
+        if (mProfile == null) mProfile = Profile.getLastUsedRegularProfile();
         return mProfile;
     }
 
