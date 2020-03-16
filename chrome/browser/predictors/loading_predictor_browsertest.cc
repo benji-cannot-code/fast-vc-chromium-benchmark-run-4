09ef9d8f876c05ac9caf53ffb2ba57320bd9a3fc
John Budorick <jbudorick@chromium.org>
2020-03-16 21:24:28 +0000

    Fully disable one LoadingPredictorBrowserTestWithOptimizationGuide.
    Bug: 1060966
    Tbr: tbansal@chromium.org
    Change-Id: I6a425285d05b24f4ccaec6b9122f2d01a7294834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106240
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Auto-Submit: John Budorick <jbudorick@chromium.org>
    Reviewed-by: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750719}

--- chrome/browser/predictors/loading_predictor_browsertest.cc
+++ chrome/browser/predictors/loading_predictor_browsertest.cc
@@ -1594,9 +1594,9 @@ INSTANTIATE_TEST_SUITE_P(UseLocalPrediction,
 #define DISABLE_ON_WIN_MAC_CHROMEOS(x) x
 #endif
 
-IN_PROC_BROWSER_TEST_P(LoadingPredictorBrowserTestWithOptimizationGuide,
-                       DISABLE_ON_WIN_MAC_CHROMEOS(
-                           NavigationHasLocalPredictionNoOptimizationHint)) {
+IN_PROC_BROWSER_TEST_P(
+    LoadingPredictorBrowserTestWithOptimizationGuide,
+    DISABLED_NavigationHasLocalPredictionNoOptimizationHint) {
   // Navigate the first time to fill the predictor's database and the HTTP
   // cache.
   GURL url = embedded_test_server()->GetURL(
@@ -1721,10 +1721,10 @@ IN_PROC_BROWSER_TEST_P(
   }
 }
 
+// crbug.com/1060966
 IN_PROC_BROWSER_TEST_P(
     LoadingPredictorBrowserTestWithOptimizationGuide,
-    DISABLE_ON_WIN_MAC_CHROMEOS(
-        NavigationWithNoLocalPredictionsButHasOptimizationHint)) {
+    DISABLED_NavigationWithNoLocalPredictionsButHasOptimizationHint) {
   {
     base::HistogramTester histogram_tester;
 
