0cd0863ef5edba4fd2a531f45972eae53b7ca60c
David Jean <djean@chromium.org>
2020-03-16 12:29:23 +0000

    [ios] Update cred ext account verification api
    New simpler two-step api.
    
    Bug: 1049638
    Change-Id: I8490144f181e140a909d3a740be9035ef0be1465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095056
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750527}

--- ios/chrome/credential_provider_extension/account_verification_provider.h
+++ ios/chrome/credential_provider_extension/account_verification_provider.h
@@ -12,12 +12,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // as @optional, until implemented in all replacement providers.
 @protocol AccountVerificationProvider
 
-// Checks if the given |accountID| is valid, in the sense of
-// credential provider extension should provide or ignore credentials
-// associated with this account.  Completes with YES if account is valid or
+// Completes with a validationID (or nil) that can be used with
+// |validateValidationID:completionHandler:| at a later time.
+- (void)validationIDForAccountID:(NSString*)accountID
+               completionHandler:
+                   (void (^)(NSString*, NSError*))completionHandler;
+
+// Checks if the given |validationID| is valid, in the sense of
+// credential provider extension should provide or ignore/delete credentials
+// associated with this ID.  Completes with YES if account is valid or
 // NO with or without error if it is not.
-- (void)isValidProviderAccountID:(NSString*)accountID
-               completionHandler:(void (^)(BOOL, NSError*))completionHandler;
+- (void)validateValidationID:(NSString*)validationID
+           completionHandler:(void (^)(BOOL, NSError*))completionHandler;
 
 @end
 
--- ios/chrome/credential_provider_extension/account_verification_provider.mm
+++ ios/chrome/credential_provider_extension/account_verification_provider.mm
@@ -11,9 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation AccountVerificationProvider
 
-- (void)isValidProviderAccountID:(NSString*)accountID
-               completionHandler:(void (^)(BOOL, NSError*))completionHandler {
-  // Default implementation return always true.
+- (void)validationIDForAccountID:(NSString*)accountID
+               completionHandler:
+                   (void (^)(NSString*, NSError*))completionHandler {
+  // Default implementation always return nil.
+  dispatch_async(dispatch_get_main_queue(), ^{
+    completionHandler(nil, nil);
+  });
+}
+
+- (void)validateValidationID:(NSString*)validationID
+           completionHandler:(void (^)(BOOL, NSError*))completionHandler {
+  // Default implementation always return true.
   dispatch_async(dispatch_get_main_queue(), ^{
     completionHandler(YES, nil);
   });
