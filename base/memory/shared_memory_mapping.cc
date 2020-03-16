ba32b9f2e2e34fe002275b8598b914b4cef44311
Gauthier Ambard <gambard@chromium.org>
2020-03-16 11:04:08 +0000

    Revert "Set a limit on how shared memory can be mapped in one process."
    This reverts commit 9e64c392b9d6f6e9215cf060ecb5690d3f5cc2eb.
    
    Reason for revert: Failing on iOS device. See crbug.com/1061863
    
    Original change's description:
    > Set a limit on how shared memory can be mapped in one process.
    > 
    > Allowing arbitrary amounts of shared memory to be mapped can make it
    > easier for an attacker to bypass ASLR as documented in
    > https://googleprojectzero.blogspot.com/2019/04/virtually-unlimited-memory-escaping.html
    > 
    > Bug: 1056572
    > Change-Id: I812542e2b3d9e7842a7ae54bdd837fa956a99177
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083447
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#750211}
    
    TBR=dcheng@chromium.org,rkaplow@chromium.org,rsesek@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1056572
    Change-Id: I78af6cfd9737a96cbb120c6e3db769145c244f9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2104050
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750514}

--- base/memory/shared_memory_mapping.cc
+++ base/memory/shared_memory_mapping.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
-#include "base/memory/shared_memory_security_policy.h"
 #include "base/memory/shared_memory_tracker.h"
 #include "base/unguessable_token.h"
 #include "build/build_config.h"
@@ -67,7 +66,6 @@ void SharedMemoryMapping::Unmap() {
   if (!IsValid())
     return;
 
-  SharedMemorySecurityPolicy::ReleaseReservationForMapping(size_);
   SharedMemoryTracker::GetInstance()->DecrementMemoryUsage(*this);
 #if defined(OS_WIN)
   if (!UnmapViewOfFile(memory_))
