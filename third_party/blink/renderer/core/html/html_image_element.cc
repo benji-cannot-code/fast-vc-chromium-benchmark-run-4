0ad4d7e56b07c415c16ec0e369793914cfeea693
Dominic Farolino <dom@chromium.org>
2020-03-17 07:41:21 +0000

    Image Loading: `crossorigin` mutations should queue a microtask
    This CL makes HTMLImageElement aware of `crossorigin` attribute value
    mutations, and respond by invoking ImageLoader::UpdateFromElement if
    the attribute's state changes. This queues a microtask to finish the
    image loading steps.
    
    This change does not influence the crossorigin attribute state used to
    influence any image request, because the crossorigin attribute state
    used to configure image requests (in blink::ConfigureRequest) is already
    referring to the element's latest. Instead, this change simply invokes
    ImageLoader::UpdateFromElement (enqueueing an ImageLoader::Task
    microtask) when an image's crossorigin attribute state changes, even
    after an image has been fetched.
    
    This makes our implementation of the HTML Standard's image element's
    relevant mutations [1] more compliant, passing more relevant mutations
    web-platform-tests. See also [2].
    
    [1]: https://html.spec.whatwg.org/multipage/images.html#relevant-mutations
    [2]: https://docs.google.com/document/d/1Xp34FIbbZnJILl0PNd1sfs_3z1HSriUVyRUgnAesGZk/
    
    Bug: 384251,774706,1061685
    Change-Id: I11e66800309df96aea61226932b9aa40c3cc5d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2102992
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750885}

--- third_party/blink/renderer/core/html/html_image_element.cc
+++ third_party/blink/renderer/core/html/html_image_element.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/deprecation.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/html/canvas/html_canvas_element.h"
+#include "third_party/blink/renderer/core/html/cross_origin_attribute.h"
 #include "third_party/blink/renderer/core/html/forms/form_associated.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_element.h"
 #include "third_party/blink/renderer/core/html/html_dimension.h"
@@ -289,6 +290,24 @@ void HTMLImageElement::ParseAttribute(
     // |importance| attribute to the loading pipeline takes place in
     // ImageLoader.
     UseCounter::Count(GetDocument(), WebFeature::kPriorityHints);
+  } else if (name == html_names::kCrossoriginAttr) {
+    // As per an image's relevant mutations [1], we must queue a new loading
+    // microtask when the `crossorigin` attribute state has changed. Note that
+    // the attribute value can change without the attribute state changing [2].
+    //
+    // [1]:
+    // https://html.spec.whatwg.org/multipage/images.html#relevant-mutations
+    // [2]: https://github.com/whatwg/html/issues/4533#issuecomment-483417499
+    CrossOriginAttributeValue new_crossorigin_state =
+        GetCrossOriginAttributeValue(params.new_value);
+    CrossOriginAttributeValue old_crossorigin_state =
+        GetCrossOriginAttributeValue(params.old_value);
+
+    if (new_crossorigin_state != old_crossorigin_state) {
+      // Update the current state so we can detect future state changes.
+      GetImageLoader().UpdateFromElement(
+          ImageLoader::kUpdateIgnorePreviousError, referrer_policy_);
+    }
   } else {
     HTMLElement::ParseAttribute(params);
   }
