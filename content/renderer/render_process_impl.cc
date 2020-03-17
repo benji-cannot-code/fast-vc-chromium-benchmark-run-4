4228b28af00f8d7a46957e96d20cf3e3355a9aa7
Camillo Bruni <cbruni@chromium.org>
2020-03-17 07:53:35 +0000

    [features] Add kTopLevelAwait feature flag
    The new feature flags enables top-level await for JavaScript modules.
    
    TC39 proposal: https://github.com/tc39/proposal-top-level-await
    
    Bug: 1022182, v8:9344
    Change-Id: Icdba59e1b988c636b041d600e6c003bad2dd2f3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105375
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Camillo Bruni <cbruni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750886}

--- content/renderer/render_process_impl.cc
+++ content/renderer/render_process_impl.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/renderer/content_renderer_client.h"
 #include "services/service_manager/embedder/switches.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/web/web_frame.h"
 #include "v8/include/v8.h"
 
@@ -140,6 +141,9 @@ RenderProcessImpl::RenderProcessImpl()
   SetV8FlagIfFeature(features::kWebAssemblySimd, "--experimental-wasm-simd");
   SetV8FlagIfNotFeature(features::kWebAssemblySimd,
                         "--no-experimental-wasm-simd");
+
+  SetV8FlagIfFeature(blink::features::kTopLevelAwait,
+                     "--harmony-top-level-await");
 
   if (base::FeatureList::IsEnabled(features::kWebAssemblyThreads)) {
     constexpr char kFlags[] =
--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -32,6 +32,7 @@ BLINK_COMMON_EXPORT extern const base::Feature kDisplayLocking;
 BLINK_COMMON_EXPORT extern const base::Feature
     kAssumeOverlapAfterFixedOrStickyPosition;
 BLINK_COMMON_EXPORT extern const base::Feature kJSONModules;
+BLINK_COMMON_EXPORT extern const base::Feature kTopLevelAwait;
 BLINK_COMMON_EXPORT extern const base::Feature kLayoutNG;
 BLINK_COMMON_EXPORT extern const base::Feature kMixedContentAutoupgrade;
 BLINK_COMMON_EXPORT extern const base::Feature kNavigationPredictor;
