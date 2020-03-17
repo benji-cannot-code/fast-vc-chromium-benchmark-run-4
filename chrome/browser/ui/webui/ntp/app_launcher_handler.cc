12c736bf044751c4f7ea6a53837c3fffc3250c6e
Daniel Murphy <dmurph@chromium.org>
2020-03-17 02:52:11 +0000

    Replacing NULL with nullptr in app_launcher_handler.cc
    Prerequisite work to avoid annoying lint errors.
    
    R=loyso@chromium.org
    
    Change-Id: I4495933115f833c0723473b24da3c39afa4a587e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106344
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750839}

--- chrome/browser/ui/webui/ntp/app_launcher_handler.cc
+++ chrome/browser/ui/webui/ntp/app_launcher_handler.cc
@@ -151,9 +151,10 @@ void AppLauncherHandler::CreateAppInfo(const Extension* extension,
            .GetByID(extension->id());
   extensions::GetExtensionBasicInfo(extension, enabled, value);
 
-  value->SetBoolean("mayDisable", extensions::ExtensionSystem::Get(
-      service->profile())->management_policy()->UserMayModifySettings(
-      extension, NULL));
+  value->SetBoolean("mayDisable",
+                    extensions::ExtensionSystem::Get(service->profile())
+                        ->management_policy()
+                        ->UserMayModifySettings(extension, nullptr));
 
   bool is_locally_installed =
       !extension->is_hosted_app() ||
@@ -318,7 +319,7 @@ void AppLauncherHandler::Observe(int type,
                       &app_info);
         web_ui()->CallJavascriptFunctionUnsafe("ntp.appMoved", app_info);
       } else {
-        HandleGetApps(NULL);
+        HandleGetApps(nullptr);
       }
       break;
     }
@@ -469,9 +470,8 @@ void AppLauncherHandler::HandleGetApps(const base::ListValue* args) {
     registrar_.Add(this, chrome::NOTIFICATION_APP_LAUNCHER_REORDERED,
                    content::Source<AppSorting>(
                        ExtensionSystem::Get(profile)->app_sorting()));
-    registrar_.Add(this,
-                   extensions::NOTIFICATION_EXTENSION_INSTALL_ERROR,
-                   content::Source<CrxInstaller>(NULL));
+    registrar_.Add(this, extensions::NOTIFICATION_EXTENSION_INSTALL_ERROR,
+                   content::Source<CrxInstaller>(nullptr));
     registrar_.Add(this,
                    extensions::NOTIFICATION_EXTENSION_LOAD_ERROR,
                    content::Source<Profile>(profile));
@@ -542,7 +542,7 @@ void AppLauncherHandler::HandleLaunchApp(const base::ListValue* args) {
     // it automatically.
     Browser* browser = chrome::FindBrowserWithWebContents(
         web_ui()->GetWebContents());
-    WebContents* old_contents = NULL;
+    WebContents* old_contents = nullptr;
     if (browser)
       old_contents = browser->tab_strip_model()->GetActiveWebContents();
 
@@ -596,8 +596,9 @@ void AppLauncherHandler::HandleUninstallApp(const base::ListValue* args) {
   if (!extension)
     return;
 
-  if (!extensions::ExtensionSystem::Get(extension_service_->profile())->
-          management_policy()->UserMayModifySettings(extension, NULL)) {
+  if (!extensions::ExtensionSystem::Get(extension_service_->profile())
+           ->management_policy()
+           ->UserMayModifySettings(extension, nullptr)) {
     LOG(ERROR) << "Attempt to uninstall an extension that is non-usermanagable "
                << "was made. Extension id : " << extension->id();
     return;
