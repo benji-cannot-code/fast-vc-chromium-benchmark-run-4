fa6a84d8e820a2cfc7f2f27ae14e55eafb561bc0
Theresa Wellington <twellington@chromium.org>
2020-03-17 01:52:05 +0000

    Move //chrome/android/public/ to //chrome/browser/
    This reverts commit eddf4f0182f70e9d13459641703196652747479f.
    
    Change 1 of 2. The next CL will update downstream dependencies.
    
    Moves //chrome/android/public/profiles/ to //chrome/browser/profiles/
    and //chrome/android/public/crypto/ to //chrome/browser/android/crypto/.
    
    Note that this will break the downstream build temporarily. Since there
    are JNI files generated, unless the package is updated we cannot keep
    a temporary mirrored build target as was done in
    https://chromium-review.googlesource.com/c/chromium/src/+/2095444
    
    BUG=1052382
    
    Change-Id: Ia33f81e0d2896d0f5ac7122b941e17a58a79e823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2102758
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750826}

--- chrome/browser/profiles/android/profile_downloader_android.cc
+++ chrome/browser/profiles/android/profile_downloader_android.cc
@@ -74,16 +74,11 @@ class AccountInfoRetriever : public ProfileDownloaderDelegate {
         .get();
   }
 
-  std::string GetCachedPictureURL() const override {
-    return std::string();
-  }
+  std::string GetCachedPictureURL() const override { return std::string(); }
 
-  bool IsPreSignin() const override {
-    return is_pre_signin_;
-  }
+  bool IsPreSignin() const override { return is_pre_signin_; }
 
-  void OnProfileDownloadSuccess(
-      ProfileDownloader* downloader) override {
+  void OnProfileDownloadSuccess(ProfileDownloader* downloader) override {
     base::string16 full_name = downloader->GetProfileFullName();
     base::string16 given_name = downloader->GetProfileGivenName();
     SkBitmap bitmap = downloader->GetProfilePicture();
@@ -138,8 +133,9 @@ JNI_ProfileDownloader_GetCachedFullNameForPrimaryAccount(
 
   base::string16 name;
   ProfileAttributesEntry* entry;
-  if (g_browser_process->profile_manager()->GetProfileAttributesStorage().
-          GetProfileAttributesWithPath(profile->GetPath(), &entry)) {
+  if (g_browser_process->profile_manager()
+          ->GetProfileAttributesStorage()
+          .GetProfileAttributesWithPath(profile->GetPath(), &entry)) {
     name = entry->GetGAIAName();
   }
 
@@ -155,8 +151,9 @@ JNI_ProfileDownloader_GetCachedGivenNameForPrimaryAccount(
 
   base::string16 name;
   ProfileAttributesEntry* entry;
-  if (g_browser_process->profile_manager()->GetProfileAttributesStorage().
-          GetProfileAttributesWithPath(profile->GetPath(), &entry)) {
+  if (g_browser_process->profile_manager()
+          ->GetProfileAttributesStorage()
+          .GetProfileAttributesWithPath(profile->GetPath(), &entry)) {
     name = entry->GetGAIAGivenName();
   }
 
@@ -172,8 +169,9 @@ JNI_ProfileDownloader_GetCachedAvatarForPrimaryAccount(
 
   ScopedJavaLocalRef<jobject> jbitmap;
   ProfileAttributesEntry* entry;
-  if (g_browser_process->profile_manager()->GetProfileAttributesStorage().
-          GetProfileAttributesWithPath(profile->GetPath(), &entry)) {
+  if (g_browser_process->profile_manager()
+          ->GetProfileAttributesStorage()
+          .GetProfileAttributesWithPath(profile->GetPath(), &entry)) {
     gfx::Image avatar_image = entry->GetAvatarIcon();
     if (!avatar_image.IsEmpty() &&
         avatar_image.Width() > profiles::kAvatarIconSize &&
@@ -194,8 +192,7 @@ void JNI_ProfileDownloader_StartFetchingAccountInfoFor(
     jint image_side_pixels,
     jboolean is_pre_signin) {
   Profile* profile = ProfileAndroid::FromProfileAndroid(jprofile);
-  const std::string email =
-      base::android::ConvertJavaStringToUTF8(env, jemail);
+  const std::string email = base::android::ConvertJavaStringToUTF8(env, jemail);
 
   auto maybe_account_info =
       IdentityManagerFactory::GetForProfile(profile)
--- chrome/browser/profiles/android/profile_manager_utils.cc
+++ chrome/browser/profiles/android/profile_manager_utils.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/bind.h"
-#include "chrome/android/public/profiles/jni_headers/ProfileManagerUtils_jni.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/profiles/android/jni_headers/ProfileManagerUtils_jni.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "components/prefs/pref_service.h"
