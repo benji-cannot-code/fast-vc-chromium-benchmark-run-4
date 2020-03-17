aa4155684db348366179318b6a25a66b2a3db1b9
Andrew Xu <andrewxu@chromium.org>
2020-03-17 06:24:44 +0000

    Hide keyboard when showing home launcher in tablet
    When showing home launcher by gesture swipe from shelf, the virtual
    keyboard may show due to the feature called "transient blur" (which
    means that if focus was lost but regained a few seconds later,
    we would show the VK again). So hide the virtual keyboard explicitly
    in this situation.
    
    This CL is for a release-blocking issue of M-81. Add the unit test
    later. Manually tested on kukui and scarlet devices.
    
    TBR=xiyuan@chromium.org
    
    Bug: 1057320
    Change-Id: I56ef6d8799082d6febe7f48497bc2d2263cd6586
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106958
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750874}

--- ash/home_screen/window_scale_animation.cc
+++ ash/home_screen/window_scale_animation.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/home_screen/window_scale_animation.h"
 
+#include "ash/keyboard/ui/keyboard_ui_controller.h"
 #include "ash/public/cpp/shelf_config.h"
 #include "ash/public/cpp/window_backdrop.h"
 #include "ash/public/cpp/window_properties.h"
@@ -74,6 +75,16 @@ void WindowScaleAnimation::OnImplicitAnimationsCompleted() {
   if (scale_type_ == WindowScaleType::kScaleDownToShelf) {
     // Minimize the dragged window after transform animation is completed.
     window_util::MinimizeAndHideWithoutAnimation({window_});
+
+    // When showing home launcher by the gesture swipe up from shelf, one
+    // feature called "transient blur" (which means that if focus was lost but
+    // regained a few seconds later, we would show the virtual keyboard again)
+    // may show the virtual keyboard, which is not what we want. So hide the
+    // virtual keyboard explicitly.
+    keyboard::KeyboardUIController* keyboard_ui_controller =
+        keyboard::KeyboardUIController::Get();
+    if (keyboard_ui_controller)
+      keyboard_ui_controller->HideKeyboardExplicitlyBySystem();
 
     // Reset its transform to identity transform and its original backdrop mode.
     window_->layer()->SetTransform(gfx::Transform());
