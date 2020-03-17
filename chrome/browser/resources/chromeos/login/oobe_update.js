4ed1992215d2ea599677708ceebeb1a3c10c83f1
Saurabh Nijhara <snijhara@google.com>
2020-03-17 10:02:55 +0000

    Fix Regression failure on Multiprofile screen.
    In case of multipofile mode, when account picker is invoked a blank
    white screen appears. Though it is not entirely clear why it happens,
    but removing 'full-screen-dialog' attribute from the oobe-dialogs in
    checking_downloading_update polymer element apparently fixes it. This
    leads to another problem of the update_screen not shown in full screen
    mode. For that we need to call onBeforeShow() of the polymer element
    explicitly so that the oobe_dialog_host_behavior in the
    checking_downlaoding_update can be used to set full screen mode for
    all the oobe dialogs.
    
    Bug: 1061330
    Change-Id: Ic51082b0694f4bed0f357ec7616ab2fbc108fcb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105316
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Saurabh Nijhara <snijhara@google.com>
    Cr-Commit-Position: refs/heads/master@{#750905}

--- chrome/browser/resources/chromeos/login/oobe_update.js
+++ chrome/browser/resources/chromeos/login/oobe_update.js
@@ -91,6 +91,7 @@ Polymer({
       if (behavior.onBeforeShow)
         behavior.onBeforeShow.call(this);
     });
+    this.$['checking-downloading-update'].onBeforeShow();
   },
 
   onBackClicked_() {
