26aa393662da0c136228725583412683a27ebd9b
Ahmed Mehfooz <amehfooz@chromium.org>
2020-03-17 09:02:12 +0000

    Add metric for clicks on gesture education notification
    Bug: 1056451
    Change-Id: I667692eb3dd172921fec09cf93f66e33cd0a7024
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2099475
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750896}

--- chrome/browser/ui/ash/system_tray_client.cc
+++ chrome/browser/ui/ash/system_tray_client.cc
@@ -278,6 +278,7 @@ void SystemTrayClient::ShowAccessibilitySettings() {
 void SystemTrayClient::ShowGestureEducationHelp() {
   chrome::ScopedTabbedBrowserDisplayer displayer(
       ProfileManager::GetActiveUserProfile());
+  base::RecordAction(base::UserMetricsAction("ShowGestureEducationHelp"));
   ShowSingletonTab(displayer.browser(),
                    GURL(chrome::kChromeOSGestureEducationHelpURL));
 }
