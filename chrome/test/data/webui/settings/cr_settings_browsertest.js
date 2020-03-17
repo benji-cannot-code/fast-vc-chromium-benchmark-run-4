ed61f7aa05609fa784f0178258b79b34e0100c4d
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-03-17 20:17:14 +0000

    [Passwords] Reenable Password Check Tests
    This change re-enables the Password Check tests. They were disabled as
    part of https://crbug.com/1061249, however were never actually flaky as
    described in https://crbug.com/1060948#c3.
    
    Bug: 1060948, 1061249
    Change-Id: If69270fa0b19f45b60340e5d0d823bef79e001bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2107582
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751085}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -396,7 +396,7 @@ CrSettingsPasswordsCheckTest.prototype = {
   ]),
 };
 
-TEST_F('CrSettingsPasswordsCheckTest', 'DISABLED_All', function() {
+TEST_F('CrSettingsPasswordsCheckTest', 'All', function() {
   mocha.run();
 });
 
