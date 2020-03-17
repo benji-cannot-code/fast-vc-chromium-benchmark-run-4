1da3147595eb3b11883191004303731e7d0a4387
Nate Chapin <japhet@chromium.org>
2020-03-17 19:20:17 +0000

    Document should not be a ConsoleLogger
    Bug: 1029822
    Change-Id: Ib16193b5bab4aa6d5b807d067843f67670bf87e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087984
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751056}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -7236,15 +7236,6 @@ void Document::AddConsoleMessage(ConsoleMessage* message,
     domWindow()->AddConsoleMessage(message, discard_duplicates);
 }
 
-void Document::AddConsoleMessageImpl(mojom::ConsoleMessageSource source,
-                                     mojom::ConsoleMessageLevel level,
-                                     const String& message,
-                                     bool discard_duplicates) {
-  AddConsoleMessage(
-      MakeGarbageCollected<ConsoleMessage>(source, level, message),
-      discard_duplicates);
-}
-
 void Document::AddInspectorIssue(InspectorIssue* issue) {
   Page* page = GetPage();
 
