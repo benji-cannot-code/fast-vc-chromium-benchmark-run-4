ff30b798eefdb24d8c9a3f8e9330349395d3b3f7
Dan Harrington <harringtond@chromium.org>
2020-03-17 18:58:49 +0000

    Add SurfaceInterface and hook it up
    Now surfaces can be added to FeedStream. These surfaces receive
    updates when the model changes.
    
    Also removed FeedStreamBackground, as we're not going to need it.
    This was originally primarily going to handle storage, but storage
    will provide an async API.
    
    Bug: 2090767
    Change-Id: I043f3e7dc10a4fd3a1c983bd0fb47f2f0b04e696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2102590
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751048}

--- components/feed/core/v2/stream_model.h
+++ components/feed/core/v2/stream_model.h
@@ -25,10 +25,6 @@ namespace feed {
 // An in-memory stream model.
 class StreamModel {
  public:
-  using ContentId = feedwire::ContentId;
-  using ContentRevision = stream_model::ContentRevision;
-  using EphemeralChangeId = stream_model::EphemeralChangeId;
-
   // Information about an update to the model.
   struct UiUpdate {
     struct SharedStateInfo {
@@ -50,15 +46,18 @@ class StreamModel {
 
   class Observer {
    public:
+    virtual ~Observer() = default;
     // Called when the UI model changes.
-    virtual void OnUiUpdate(const UiUpdate&) = 0;
+    virtual void OnUiUpdate(const UiUpdate& update) = 0;
   };
 
-  explicit StreamModel(Observer* observer);
+  explicit StreamModel();
   ~StreamModel();
 
   StreamModel(const StreamModel& src) = delete;
   StreamModel& operator=(const StreamModel&) = delete;
+
+  void SetObserver(Observer* observer);
 
   // Data access.
 
@@ -87,7 +86,7 @@ class StreamModel {
 
   void UpdateFlattenedTree();
 
-  Observer* observer_;  // Unowned.
+  Observer* observer_ = nullptr;  // Unowned.
 
   stream_model::ContentIdMap id_map_;
   stream_model::FeatureTree base_feature_tree_{&id_map_};
--- components/feed/core/v2/stream_model/feature_tree.h
+++ components/feed/core/v2/stream_model/feature_tree.h
@@ -11,15 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/util/type_safety/id_type.h"
 #include "components/feed/core/proto/v2/store.pb.h"
 #include "components/feed/core/v2/proto_util.h"
+#include "components/feed/core/v2/public/feed_stream_api.h"
 
 namespace feed {
 namespace stream_model {
 
 // Uniquely identifies a feedwire::ContentId. Provided by |ContentIdMap|.
 using ContentTag = util::IdTypeU32<class ContentTagClass>;
-// Uniquely identifies a revision of a |feedstore::Content|. If Content changes,
-// it is assigned a new revision number.
-using ContentRevision = util::IdTypeU32<class ContentRevisionClass>;
+using ContentRevision = feed::ContentRevision;
 
 // Maps ContentId into ContentTag, and generates ContentRevision IDs.
 class ContentIdMap {
