b5eb1ee31e7475843ad4b3f6f4bcfbe3f255d8c8
Hesen Zhang <hesen@google.com>
2020-03-17 07:11:18 +0000

    [Upboarding]: ProtoConversion and unittest for QueryTileStore.
    - Implemented proto_conversion for query_tile_entry.
    - Added unit test for proto_conversion and query_tile_store.
    - Fixed some lint/nits.
    
    Bug: 1060805
    Change-Id: Ife1245aa6749ad2e534351c8b66a7e819adf1d75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103656
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#750880}

--- chrome/browser/upboarding/query_tiles/internal/query_tile_store.cc
+++ chrome/browser/upboarding/query_tiles/internal/query_tile_store.cc
@@ -3,20 +3,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/upboarding/query_tiles/internal/query_tile_store.h"
+
 #include <utility>
 
-#include "chrome/browser/upboarding/query_tiles/internal/query_tile_store.h"
+#include "chrome/browser/upboarding/query_tiles/internal/proto_conversion.h"
 
 namespace leveldb_proto {
 
 void DataToProto(upboarding::QueryTileEntry* data,
                  upboarding::query_tiles::proto::QueryTileEntry* proto) {
-  NOTIMPLEMENTED();
+  QueryTileEntryToProto(data, proto);
 }
 
 void ProtoToData(upboarding::query_tiles::proto::QueryTileEntry* proto,
                  upboarding::QueryTileEntry* data) {
-  NOTIMPLEMENTED();
+  QueryTileEntryFromProto(proto, data);
 }
 
 }  // namespace leveldb_proto
@@ -53,7 +55,7 @@ void QueryTileStore::Delete(const std::string& key, DeleteCallback callback) {
 void QueryTileStore::OnDbInitialized(LoadCallback callback,
                                      leveldb_proto::Enums::InitStatus status) {
   if (status != leveldb_proto::Enums::InitStatus::kOK) {
-    std::move(callback).Run(false, {});
+    std::move(callback).Run(false, KeysAndEntries());
     return;
   }
 
