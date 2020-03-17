9f87df6f4cda34636d41d0d782a7008436f2da82
Anders Hartvoll Ruud <andruud@chromium.org>
2020-03-17 19:25:43 +0000

    Disable -webkit-appearance if author provides a border/background
    Blink currently disables the native appearance of a widget if the
    author specifies a border or background that's _different_ from the
    border/background as specified by the UA. This comparison between author
    and UA border/background is problematic, because it means we can toggle
    native appearance of widgets in the wild just by tweaking the UA style.
    Moreover, it leads to behavior that's hard to predict, as Blink may
    behave in different ways depending on the platform.
    
    Therefore, it was suggested to instead disable the native appearance if
    the author specifies _any_ border/background property, i.e. no
    comparison with the UA style. This matches Firefox' current behavior,
    but deviates from Safari (which is doing what Blink currently does).
    
    We currently have two code-paths for style resolution: CSSCascade
    (runtime feature enabled for 'stable'), and the old path. This CL
    affects the CSSCascade path only, since it's likely not worth it to
    implement this new behavior in old path (the plan is anyway to remove
    it).
    
    For the CSSCascade path, the comparison between author border/background
    and UA border/background was implemented by adding -internal-ua-*
    "companion" properties which held the values provided by the UA. (E.g.
    -internal-ua-background-color would hold the "as styled by UA"-value,
    and background-color would be the normal "as styled by author"). After
    the entire cascade was computed, the -internal-ua-* properties would be
    compared with the corresponding normal properties, and we would set the
    HasAuthorBackground/Border flags based on this.
    
    This CL now reverts all that, and instead simply checks if the cascaded
    values for background/border properties have kAuthor origin.
    
    I2S: https://mail.google.com/mail/u/2/#inbox/QgrcJHrtrSkDJhRHmWSHHJQSVjGTFHNHsSq
    Bug: 1061846
    Change-Id: I2e543460fb28bcac81410c6a14891be90d6b1a3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1970312
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751059}

--- third_party/blink/renderer/core/css/resolver/style_resolver_state.h
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.h
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/style/cached_ua_style.h"
-#include "third_party/blink/renderer/core/style/ua_style.h"
 
 namespace blink {
 
@@ -147,10 +146,6 @@ class CORE_EXPORT StyleResolverState {
     return cached_ua_style_.get();
   }
 
-  const UAStyle* GetUAStyle() const { return ua_style_.get(); }
-
-  UAStyle* EnsureUAStyle();
-
   ElementStyleResources& GetElementStyleResources() {
     return element_style_resources_;
   }
@@ -242,7 +237,6 @@ class CORE_EXPORT StyleResolverState {
   FontBuilder font_builder_;
 
   std::unique_ptr<CachedUAStyle> cached_ua_style_;
-  std::unique_ptr<UAStyle> ua_style_;
 
   ElementStyleResources element_style_resources_;
   Element* pseudo_element_;
