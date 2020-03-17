040404f425f11c72188ad8bcb9774b7c2e70c665
Nicholas Hollingum <hollingum@google.com>
2020-03-17 02:27:04 +0000

    Crostini Upgrader: Do not proceed if user cancels backup
    Previously if the user cancelled the backup, we would proceed with the
    upgrade as though the user had elected not to backup at all.
    
    With this change, the flow will instead return to the initial screen
    when the user cancels, rather than proceed.
    
    Bug: 1061203
    Change-Id: I7be99a2c7a6caa4b5fd143def5ba0c6f9b4d5004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2102935
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Nic Hollingum <hollingum@google.com>
    Cr-Commit-Position: refs/heads/master@{#750833}

--- chrome/browser/resources/chromeos/crostini_upgrader/app.js
+++ chrome/browser/resources/chromeos/crostini_upgrader/app.js
@@ -107,7 +107,7 @@ Polymer({
 
     this.listenerIds_ = [
       callbackRouter.onBackupProgress.addListener((percent) => {
-        assert(this.state_ === State.BACKUP);
+        this.state_ = State.BACKUP;
         this.backupProgress_ = percent;
       }),
       callbackRouter.onBackupSucceeded.addListener((wasCancelled) => {
@@ -257,7 +257,6 @@ Polymer({
    * @private
    */
   startBackup_(showFileChooser) {
-    this.state_ = State.BACKUP;
     BrowserProxy.getInstance().handler.backup(showFileChooser);
   },
 
