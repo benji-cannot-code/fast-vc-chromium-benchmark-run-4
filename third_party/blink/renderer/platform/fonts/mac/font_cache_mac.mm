ca018304bacee71c3d61a1268581162450d57bd8
Olivier Yiptong <oyiptong@chromium.org>
2020-03-17 21:17:06 +0000

    FontAccess: Runtime Flag and Chromium Feature mapping
    This adds a runtime flag and a DCHECK for gating Font Access
    functionality. The runtime flag's state is set from the Chromium
    feature flag's state.
    
    Bug: 535764
    Change-Id: Ie756de078cd5063e978c5e3faf9248885f5b3efa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103859
    Commit-Queue: Olivier Yiptong <oyiptong@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751113}

--- third_party/blink/renderer/platform/fonts/mac/font_cache_mac.mm
+++ third_party/blink/renderer/platform/fonts/mac/font_cache_mac.mm
@@ -327,6 +327,7 @@ std::unique_ptr<FontPlatformData> FontCache::CreateFontPlatformData(
 }
 
 std::vector<FontEnumerationEntry> FontCache::EnumeratePlatformAvailableFonts() {
+  DCHECK(RuntimeEnabledFeatures::FontAccessEnabled());
   @autoreleasepool {
     std::vector<FontEnumerationEntry> output;
 
