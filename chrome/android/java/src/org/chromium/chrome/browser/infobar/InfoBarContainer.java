a9fbef63a097d2379d3a85d93a54888c0dd69a87
Pavel Yatsuk <pavely@google.com>
2020-03-17 20:26:53 +0000

    [Modularization] Remove reference to InfoBarContainer from InfoBar
    This CL extracts methods that InfoBar calls on InfoBarContainer into
    InfoBar.Container interface. This breaks InfoBar dependency on
    InfoBarContainer and allows moving InfoBar.java to
    chrome/browser/ui/messages.
    
    BUG=1056346
    R=mdjones@chromium.org
    
    Change-Id: I1b16b6021979dcdfcff1aa5e48cc9aacf613b02b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103140
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751088}

--- chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarContainer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarContainer.java
@@ -38,7 +38,7 @@ import java.util.ArrayList;
  * When initiated from native code, special code is needed to keep the Java and native infobar in
  * sync, see NativeInfoBar.
  */
-public class InfoBarContainer implements UserData, KeyboardVisibilityListener {
+public class InfoBarContainer implements UserData, KeyboardVisibilityListener, InfoBar.Container {
     private static final String TAG = "InfoBarContainer";
 
     private static final Class<InfoBarContainer> USER_DATA_KEY = InfoBarContainer.class;
@@ -292,7 +292,7 @@ public class InfoBarContainer implements UserData, KeyboardVisibilityListener {
         }
 
         infoBar.setContext(mInfoBarContainerView.getContext());
-        infoBar.setInfoBarContainer(this);
+        infoBar.setContainer(this);
 
         // We notify observers immediately (before the animation starts).
         for (InfoBarContainerObserver observer : mObservers) {
@@ -318,10 +318,7 @@ public class InfoBarContainer implements UserData, KeyboardVisibilityListener {
         addInfoBar(infoBar);
     }
 
-    /**
-     * Notifies that an infobar's View ({@link InfoBar#getView}) has changed. If the infobar is
-     * visible, a view swapping animation will be run.
-     */
+    @Override
     public void notifyInfoBarViewChanged() {
         assert !mDestroyed;
         if (mInfoBarContainerView != null) mInfoBarContainerView.notifyInfoBarViewChanged();
@@ -342,12 +339,8 @@ public class InfoBarContainer implements UserData, KeyboardVisibilityListener {
         return mInfoBarContainerView != null ? mInfoBarContainerView.getVisibility() : View.GONE;
     }
 
-    /**
-     * Called by {@link InfoBar} to remove itself from the view hierarchy.
-     *
-     * @param infoBar InfoBar to remove from the View hierarchy.
-     */
-    void removeInfoBar(InfoBar infoBar) {
+    @Override
+    public void removeInfoBar(InfoBar infoBar) {
         assert !mDestroyed;
 
         if (!mInfoBars.remove(infoBar)) {
@@ -365,11 +358,8 @@ public class InfoBarContainer implements UserData, KeyboardVisibilityListener {
         mInfoBarContainerView.removeInfoBar(infoBar);
     }
 
-    /**
-     * @return True when this container has been emptied and its native counterpart has been
-     *         destroyed.
-     */
-    public boolean hasBeenDestroyed() {
+    @Override
+    public boolean isDestroyed() {
         return mDestroyed;
     }
 
@@ -543,13 +533,10 @@ public class InfoBarContainer implements UserData, KeyboardVisibilityListener {
         }
     }
 
-    /**
-     * @return The infobar in front.
-     */
-    @Nullable
-    InfoBar getFrontInfoBar() {
-        if (mInfoBars.isEmpty()) return null;
-        return mInfoBars.get(0);
+    @Override
+    public boolean isFrontInfoBar(InfoBar infoBar) {
+        if (mInfoBars.isEmpty()) return false;
+        return mInfoBars.get(0) == infoBar;
     }
 
     /**
