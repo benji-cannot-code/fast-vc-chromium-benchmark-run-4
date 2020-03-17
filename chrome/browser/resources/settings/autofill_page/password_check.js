63e0ce696758ac7153fb7371f3d3d13bb13afe74
Friedrich Horschig <fhorschig@chromium.org>
2020-03-17 20:56:23 +0000

    [Passwords] Leak check new button and title for idle state
    With this CL, the title during the successful IDLE state changes to
    "Checked Passwords" (not added since it exists for the same context).
    
    Also, the IDLE state shows "Check again" only if a check happened before
    and defaults to "Check Passwords" if there hasn't been a check yet.
    Without an initial check, we also show a different message and banner
    and hide icon and subtitle.
    
    Screenshots are in the linked bug.
    
    Bug: 1062216
    Change-Id: I484504fdb37e0043b553813ff51008085d58488f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106087
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751103}

--- chrome/browser/resources/settings/autofill_page/password_check.js
+++ chrome/browser/resources/settings/autofill_page/password_check.js
@@ -262,7 +262,7 @@ Polymer({
    */
   getStatusIconClass_(status, leakedPasswords) {
     if (!this.hasLeaksOrErrors_(status, leakedPasswords)) {
-      return 'no-leaks';
+      return this.waitsForFirstCheck_() ? 'hidden' : 'no-leaks';
     }
     if (this.hasLeakedCredentials_(leakedPasswords)) {
       return 'has-leaks';
@@ -278,7 +278,9 @@ Polymer({
   getTitle_() {
     switch (this.status_.state) {
       case CheckState.IDLE:
-        return this.i18n('checkPasswords');
+        return this.waitsForFirstCheck_() ?
+            this.i18n('checkPasswordsDescription') :
+            this.i18n('checkedPasswords');
       case CheckState.CANCELED:
         return this.i18n('checkPasswordsCanceled');
       case CheckState.RUNNING:
@@ -333,6 +335,8 @@ Polymer({
   getButtonText_(status) {
     switch (status.state) {
       case CheckState.IDLE:
+        return this.waitsForFirstCheck_() ? this.i18n('checkPasswords') :
+                                            this.i18n('checkPasswordsAgain');
       case CheckState.CANCELED:
       case CheckState.TOO_MANY_PASSWORDS:
         return this.i18n('checkPasswordsAgain');
@@ -348,6 +352,15 @@ Polymer({
         return '';  // Undefined behavior. Don't show any misleading text.
     }
     assertNotReached('Can\'t find a button text for state: ' + status.state);
+  },
+
+  /**
+   * Returns 'action-button' only for the very first check.
+   * @return {string}
+   * @private
+   */
+  getButtonTypeClass_() {
+    return this.waitsForFirstCheck_() ? 'action-button' : ' ';
   },
 
   /**
@@ -383,7 +396,10 @@ Polymer({
    * @private
    */
   bannerImageSrc_(isDarkMode) {
-    const type = this.status_.state == CheckState.IDLE ? 'positive' : 'neutral';
+    const type =
+        (this.status_.state == CheckState.IDLE && !this.waitsForFirstCheck_()) ?
+        'positive' :
+        'neutral';
     const suffix = isDarkMode ? '_dark' : '';
     return `chrome://settings/images/password_check_${type}${suffix}.svg`;
   },
@@ -443,7 +459,7 @@ Polymer({
     }
     switch (this.status_.state) {
       case CheckState.IDLE:
-        return true;
+        return !this.waitsForFirstCheck_();
       case CheckState.CANCELED:
       case CheckState.RUNNING:
       case CheckState.OFFLINE:
@@ -459,6 +475,15 @@ Polymer({
     assertNotReached(
         'Not specified whether to show passwords for state: ' +
         this.status_.state);
+  },
+
+  /**
+   * Returns true iff the leak check was performed at least once before.
+   * @return {boolean}
+   * @private
+   */
+  waitsForFirstCheck_() {
+    return !this.status_.elapsedTimeSinceLastCheck;
   },
 
   /**
