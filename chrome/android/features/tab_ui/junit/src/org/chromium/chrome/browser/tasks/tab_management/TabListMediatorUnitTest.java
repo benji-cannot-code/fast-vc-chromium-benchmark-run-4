7f959243c894646a953d6dbf65eda6d1fbfb7771
Yue Zhang <yuezhanggg@chromium.org>
2020-03-17 22:15:08 +0000

    Revert "Fix a crash in pop-up tab strip"
    This reverts commit fb08e564d1a9b2f02d7d1d197b03670c8e36e03a.
    
    Reason for revert: Workaround introduced didn't fix the crash.
    
    Original change's description:
    > Fix a crash in pop-up tab strip
    > 
    > In the popup TabStrip used by Duet-TabStrip integration, there is a
    > crash in RecycleView when user very intensively clicks on the TabStrip
    > favicon to close multiple tabs. The stack indicates that the
    > recyclerView is trying to remove the same ViewHolder in two consecutive
    > remove operations, and the second call would crash. The fix introduced
    > in this CL is to early end the animation if we know for sure that this
    > item is going to be removed. This should help with better identifying
    > the ViewHolder to remove.
    > 
    > This fix is inspired by the discussion here:
    > https://b.corp.google.com/issues/34184109
    > and the CL here:
    > https://critique.corp.google.com/#review/188055979/depot/google3/java/com/google/android/apps/play/music/app/ui/common/MediaListRecyclerFragment.java
    > Even though we early end the animation, there should be no visible
    > animation change from the UI perspective.
    > 
    > Bug: 1045944
    > Change-Id: I0d8a7b098e17cdc86c4ae3227cd9dc1ba37b2adc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036874
    > Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    > Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#738392}
    
    TBR=yusufo@chromium.org,wychen@chromium.org,yuezhanggg@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1045944
    Change-Id: I733bc7729f9118808999e59695ae01b7194ef0e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2107120
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751146}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediatorUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediatorUnitTest.java
@@ -216,8 +216,6 @@ public class TabListMediatorUnitTest {
     @Mock
     TabListMediator.TabGridAccessibilityHelper mTabGridAccessibilityHelper;
     @Mock
-    TabListMediator.ItemAnimationStopper mItemAnimationStopper;
-    @Mock
     TemplateUrlService mTemplateUrlService;
 
     @Captor
@@ -327,7 +325,7 @@ public class TabListMediatorUnitTest {
         mMediator = new TabListMediator(mContext, mModel, mTabModelSelector,
                 mTabContentManager::getTabThumbnailWithCallback, mTitleProvider,
                 mTabListFaviconProvider, false, null, mGridCardOnClickListenerProvider, null,
-                mItemAnimationStopper, getClass().getSimpleName(), UiType.CLOSABLE);
+                getClass().getSimpleName(), UiType.CLOSABLE);
         mMediator.registerOrientationListener(mGridLayoutManager);
         TrackerFactory.setTrackerForTests(mTracker);
     }
@@ -584,16 +582,6 @@ public class TabListMediatorUnitTest {
                 prepareTab(TAB3_ID, TAB3_TITLE, TAB3_URL), false);
 
         assertThat(mModel.size(), equalTo(2));
-    }
-
-    @Test
-    public void tabClosure_ForceStopItemAnimationForStrip() {
-        setUpForTabGroupOperation(TabListMediatorType.TAB_STRIP);
-
-        mMediatorTabModelObserver.willCloseTab(mTab2, false);
-
-        assertThat(mModel.size(), equalTo(1));
-        verify(mItemAnimationStopper).endItemAnimationForPosition(eq(1));
     }
 
     @Test
@@ -1935,8 +1923,8 @@ public class TabListMediatorUnitTest {
         // Re-initialize the mediator to setup TemplateUrlServiceObserver if needed.
         mMediator = new TabListMediator(mContext, mModel, mTabModelSelector,
                 mTabContentManager::getTabThumbnailWithCallback, mTitleProvider,
-                mTabListFaviconProvider, true, null, null, null, mItemAnimationStopper,
-                getClass().getSimpleName(), TabProperties.UiType.CLOSABLE);
+                mTabListFaviconProvider, true, null, null, null, getClass().getSimpleName(),
+                TabProperties.UiType.CLOSABLE);
 
         initAndAssertAllProperties();
 
@@ -1958,8 +1946,8 @@ public class TabListMediatorUnitTest {
         // Re-initialize the mediator to setup TemplateUrlServiceObserver if needed.
         mMediator = new TabListMediator(mContext, mModel, mTabModelSelector,
                 mTabContentManager::getTabThumbnailWithCallback, mTitleProvider,
-                mTabListFaviconProvider, true, null, null, null, mItemAnimationStopper,
-                getClass().getSimpleName(), TabProperties.UiType.CLOSABLE);
+                mTabListFaviconProvider, true, null, null, null, getClass().getSimpleName(),
+                TabProperties.UiType.CLOSABLE);
 
         initAndAssertAllProperties();
 
@@ -2093,7 +2081,7 @@ public class TabListMediatorUnitTest {
         mMediator = new TabListMediator(mContext, mModel, mTabModelSelector,
                 mTabContentManager::getTabThumbnailWithCallback, mTitleProvider,
                 mTabListFaviconProvider, actionOnRelatedTabs, null, null, handler,
-                mItemAnimationStopper, getClass().getSimpleName(), uiType);
+                getClass().getSimpleName(), uiType);
 
         // There are two TabModelObserver and two TabGroupModelFilter.Observer added when
         // initializing TabListMediator, one set from TabListMediator and the other from
