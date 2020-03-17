b7dbe92d08768f7f4494d925220f8237d2fe92f2
Robert Ogden <robertogden@chromium.org>
2020-03-17 20:05:29 +0000

    Promote DRP ClientConfig fetching to its own feature and enable
    With the pending rollouts of some features that use the client config,
    we should enable everybody to fetch it by default, but leave a kill
    switch.
    
    Bug: 1048736
    Change-Id: I6f88289173425551c10a0fa44dcc144570a2618b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2107561
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751078}

--- chrome/browser/data_reduction_proxy/data_reduction_proxy_browsertest.cc
+++ chrome/browser/data_reduction_proxy/data_reduction_proxy_browsertest.cc
@@ -111,7 +111,6 @@ ClientConfig CreateEmptyConfig() {
   return CreateEmptyProxyConfig(kSessionKey, 1000, 0, 0.5f, false);
 }
 
-
 class TestSettingsObserver : public DataReductionProxySettingsObserver {
  public:
   TestSettingsObserver() = default;
@@ -534,7 +533,6 @@ IN_PROC_BROWSER_TEST_F(DataReductionProxyBrowsertest,
   EXPECT_EQ(result, kDummyBody);
 }
 
-
 IN_PROC_BROWSER_TEST_F(DataReductionProxyBrowsertest,
                        ProxyNotUsedWhenDisabled) {
   net::EmbeddedTestServer test_server;
@@ -628,8 +626,6 @@ IN_PROC_BROWSER_TEST_F(DataReductionProxyBrowsertest,
   EXPECT_EQ(GetBody(), kDummyBody);
 }
 
-
-
 // Test that enabling the holdback disables the proxy and that the client config
 // is fetched when lite page redirect preview is enabled.
 class DataReductionProxyWithHoldbackBrowsertest
@@ -637,39 +633,36 @@ class DataReductionProxyWithHoldbackBrowsertest
       public DataReductionProxyBrowsertest {
  public:
   DataReductionProxyWithHoldbackBrowsertest()
-      : DataReductionProxyBrowsertest(),
-        // Consider the holdback as enabled if holdback is enabled or the
-        // |force_enable_config_service_fetches_| is enabled.
+      :  // Consider the holdback as enabled if holdback is enabled or the
+         // |enable_config_service_fetches_| is enabled.
         data_reduction_proxy_holdback_enabled_(std::get<2>(GetParam()) ||
                                                std::get<0>(GetParam())),
         lite_page_redirect_previews_enabled_(std::get<1>(GetParam())),
-        force_enable_config_service_fetches_(std::get<2>(GetParam())) {}
+        enable_config_service_fetches_(std::get<2>(GetParam())) {}
 
   void SetUp() override {
-    if (force_enable_config_service_fetches_) {
-      scoped_feature_list_.InitAndEnableFeatureWithParameters(
-          data_reduction_proxy::features::kDataReductionProxyHoldback,
-          {{"force_enable_config_service_fetches", "true"}});
-    }
-    if (!force_enable_config_service_fetches_ &&
-        data_reduction_proxy_holdback_enabled_) {
-      scoped_feature_list_.InitAndEnableFeature(
-          data_reduction_proxy::features::kDataReductionProxyHoldback);
-    }
-    if (lite_page_redirect_previews_enabled_) {
-      previews_lite_page_redirect_feature_list_.InitAndEnableFeature(
-          previews::features::kLitePageServerPreviews);
-    }
+    data_reduction_proxy_holdback_feature_list_.InitWithFeatureState(
+        data_reduction_proxy::features::kDataReductionProxyHoldback,
+        data_reduction_proxy_holdback_enabled_);
+
+    fetch_client_config_feature_list_.InitWithFeatureState(
+        data_reduction_proxy::features::kFetchClientConfig,
+        enable_config_service_fetches_);
+
+    previews_lite_page_redirect_feature_list_.InitWithFeatureState(
+        previews::features::kLitePageServerPreviews,
+        lite_page_redirect_previews_enabled_);
 
     InProcessBrowserTest::SetUp();
   }
 
   const bool data_reduction_proxy_holdback_enabled_;
   const bool lite_page_redirect_previews_enabled_;
-  const bool force_enable_config_service_fetches_;
+  const bool enable_config_service_fetches_;
 
  private:
-  base::test::ScopedFeatureList scoped_feature_list_;
+  base::test::ScopedFeatureList data_reduction_proxy_holdback_feature_list_;
+  base::test::ScopedFeatureList fetch_client_config_feature_list_;
   base::test::ScopedFeatureList previews_lite_page_redirect_feature_list_;
 };
 
@@ -718,7 +711,6 @@ class DataReductionProxyExpBrowsertest : public DataReductionProxyBrowsertest {
   }
 };
 
-
 class DataReductionProxyExpFeatureBrowsertest
     : public DataReductionProxyBrowsertest {
  public:
@@ -741,7 +733,6 @@ class DataReductionProxyExpFeatureBrowsertest
 
   const std::string experiment_name = "foo_feature_experiment";
 };
-
 
 // Threadsafe log for recording a sequence of events as newline separated text.
 class EventLog {
@@ -860,7 +851,6 @@ IN_PROC_BROWSER_TEST_F(DataReductionProxyBrowsertest,
     EXPECT_FALSE(observer.last_initiator_origin().has_value());
   }
 
-
   // Simulate clicking on a same-site link.
   {
     content::TestNavigationObserver observer(
@@ -883,6 +873,5 @@ IN_PROC_BROWSER_TEST_F(DataReductionProxyBrowsertest,
               observer.last_initiator_origin());
   }
 }
-
 
 }  // namespace data_reduction_proxy
--- components/data_reduction_proxy/core/common/data_reduction_proxy_features.cc
+++ components/data_reduction_proxy/core/common/data_reduction_proxy_features.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace data_reduction_proxy {
 namespace features {
 
-
 // Enables the data saver promo for low memory Android devices.
 const base::Feature kDataReductionProxyLowMemoryDevicePromo{
     "DataReductionProxyLowMemoryDevicePromo",
@@ -28,9 +27,8 @@ const base::Feature kDataReductionProxyHoldback{
 
 // Enables data reduction proxy when network service is enabled.
 const base::Feature kDataReductionProxyEnabledWithNetworkService{
-  "DataReductionProxyEnabledWithNetworkService",
-      base::FEATURE_ENABLED_BY_DEFAULT
-};
+    "DataReductionProxyEnabledWithNetworkService",
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enables block action of all proxies when 502 is received with no
 // Chrome-Proxy header. The block duration is configurable via field trial with
@@ -67,6 +65,10 @@ const base::Feature kDataReductionProxyAggressiveConfigFetch{
 // savings provided by DataSaver.
 const base::Feature kReportSaveDataSavings{"ReportSaveDataSavings",
                                            base::FEATURE_DISABLED_BY_DEFAULT};
+
+// Enables fetching the Client Config for server-based Lite Mode features.
+const base::Feature kFetchClientConfig{"DataReductionProxyFetchClientConfig",
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 }  // namespace data_reduction_proxy
