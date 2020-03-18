2881ad206d7948bcd80b469dd208948a783f8450
Lukasz Anforowicz <lukasza@chromium.org>
2020-03-18 20:44:39 +0000

    Revert "|request_initiator_site_lock| enforcement - attempt #2."
    This reverts commit 0724e945f0756d77924412075dcfe1e457e2c26a.
    
    Reason for revert: https://crbug.com/1056949 (RBS) and https://crbug.com/1059218
    
    Original change's description:
    > |request_initiator_site_lock| enforcement - attempt #2.
    > 
    > This CL starts treating |request_initiator| that doesn't match
    > |request_initiator_site_lock| as a bad IPC message and ignoring such
    > malformed resource requests.
    > 
    > NetworkService.URLLoader.RequestInitiatorOriginLockCompatibility UMA is
    > non-zero in recent Canary releases.  Therefore it is possible
    > that this CL will get reverted after gathering sufficient number of
    > DumpWithoutCrashing reports to understand why the lock doesn't match the
    > initiator in some real world scenarios.
    > 
    > Bug: 920634
    > Change-Id: If4fbdb6336703f53784c036ca5d9e408ff223d78
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2006113
    > Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    > Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    > Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#739359}
    
    TBR=yhirano@chromium.org,lukasza@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 920634
    Change-Id: Ief1019a74abf8e70364ad2f6aadc2d2216488910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108783
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751470}

--- services/network/crash_keys.h
+++ services/network/crash_keys.h
@@ -16,8 +16,6 @@ struct ResourceRequest;
 
 namespace debug {
 
-base::debug::CrashKeyString* GetRequestInitiatorSiteLockCrashKey();
-
 class ScopedRequestCrashKeys {
  public:
   ScopedRequestCrashKeys(const network::ResourceRequest& request);
