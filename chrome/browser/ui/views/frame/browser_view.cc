9d7c94d983d17cf8572cade2152610d1983d83c7
Ahmed <afakhry@chromium.org>
2020-03-18 19:03:06 +0000

    Ctrl+L to focus omnibox should unhide top-chrome in tablet mode
    When top-chrome is scrolled to be fully hidden in tablet mode,
    using the keyboard shortcut Ctrl+L to focus the omnibox should
    unhide top-chrome and update the constraints such that it remains
    fully shown as long as the omnibox has focus.
    
    BUG=1061253
    TEST=Manually, added a new test.
    
    Change-Id: I93c3ef98af1d17c1980d47448f0eb90a20d9951b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106765
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751434}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -2644,16 +2644,6 @@ void BrowserView::ViewHierarchyChanged(
 void BrowserView::AddedToWidget() {
   views::ClientView::AddedToWidget();
 
-#if defined(OS_CHROMEOS)
-  // TopControlsSlideController must be initialized here in AddedToWidget()
-  // rather than Init() as it depends on the browser frame being ready.
-  if (IsBrowserTypeNormal()) {
-    DCHECK(frame_);
-    top_controls_slide_controller_ =
-        std::make_unique<TopControlsSlideControllerChromeOS>(this);
-  }
-#endif
-
   GetWidget()->AddObserver(this);
 
   // Stow a pointer to this object onto the window handle so that we can get at
@@ -2672,6 +2662,19 @@ void BrowserView::AddedToWidget() {
 #endif
 
   toolbar_->Init();
+
+#if defined(OS_CHROMEOS)
+  // TopControlsSlideController must be initialized here in AddedToWidget()
+  // rather than Init() as it depends on the browser frame being ready.
+  // It also needs to be after the |toolbar_| had been initialized since it uses
+  // the omnibox.
+  if (IsBrowserTypeNormal()) {
+    DCHECK(frame_);
+    DCHECK(toolbar_);
+    top_controls_slide_controller_ =
+        std::make_unique<TopControlsSlideControllerChromeOS>(this);
+  }
+#endif
 
   LoadAccelerators();
 
