693129c0e2db8db39a5443cc9892d2701ba23725
Mark Brand <markbrand@google.com>
2020-03-18 20:32:56 +0000

    Reland "Adding mojolpm fuzzer."
    This is a reland of 96b8d308d93e9f014d795ceb20474b6be9f42c12
    
    Original change's description:
    > Adding mojolpm fuzzer.
    >
    > This adds code that generates libprotobuf-mutator based
    > structure-aware fuzzers as part of the mojom compilation process.
    > (changes in //mojo).
    >
    > It also adds a fuzzer for CodeCacheHost using this framework to demonstrate
    > how it will be used, and documentation explaining that fuzzer and how to use
    > this generator to write new fuzzers. (changes in //content/test).
    >
    > It finally includes a few changes to typemap and traits files that
    > were necessary to smooth over some inconsistencies in existing code
    > using mojo.
    >
    > Bug: 941849
    > Change-Id: I660a57bd90c0963bd9a0b8608c746cdb767edc98
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1978739
    > Commit-Queue: Mark Brand <markbrand@google.com>
    > Auto-Submit: Mark Brand <markbrand@google.com>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Ted Meyer <tmathmeyer@chromium.org>
    > Reviewed-by: Oliver Chang <ochang@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Cr-Commit-Position: refs/heads/master@{#746827}
    
    Bug: 941849
    Change-Id: I01feb96401e1c9cbd3bbb2bf2a22ecc253d36b4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087408
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Oliver Chang <ochang@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Ted Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751464}

--- third_party/blink/public/common/fetch/fetch_api_request_body_mojom_traits.h
+++ third_party/blink/public/common/fetch/fetch_api_request_body_mojom_traits.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "services/network/public/cpp/resource_request_body.h"
 #include "services/network/public/mojom/url_loader.mojom-forward.h"
-#include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom-forward.h"
+#include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom-shared.h"
 
 namespace mojo {
 
