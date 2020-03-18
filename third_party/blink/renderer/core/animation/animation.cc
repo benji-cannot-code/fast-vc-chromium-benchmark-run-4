2b002045a91635594ac3d90f5cfd8c738da88504
Yuki Shiino <yukishiino@chromium.org>
2020-03-18 17:48:20 +0000

    animation: Move global NullValue and IsNull into Timing class
    In third_party/blink/renderer/core/animation/timing.h,
    there are static inline functions defined, which are harmful
    from a PoV of namespace contamination.  (Static inline functions
    in *.h are no good in the first place.)
    
    Moves IsNull and NullValue into Timing struct, and removes
    ValueOrUnresolved.
    
    Change-Id: I777740c4ce020dc78350fb2b5c9322db10660705
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106988
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751391}

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -397,7 +397,7 @@ double Animation::currentTimeForBinding(bool& is_null) {
 }
 
 double Animation::currentTime() const {
-  return currentTimeForBinding().value_or(NullValue());
+  return currentTimeForBinding().value_or(Timing::NullValue());
 }
 
 double Animation::currentTime(bool& is_null) {
@@ -573,7 +573,7 @@ void Animation::NotifyReady(double ready_time) {
 // Refer to Step 8.3 'pending play task' in
 // https://drafts.csswg.org/web-animations/#playing-an-animation-section.
 void Animation::CommitPendingPlay(double ready_time) {
-  DCHECK(!IsNull(ready_time));
+  DCHECK(!Timing::IsNull(ready_time));
   DCHECK(start_time_ || hold_time_);
   DCHECK(pending_play_);
   pending_play_ = false;
@@ -1367,12 +1367,12 @@ void Animation::updatePlaybackRate(double playback_rate,
           timeline_ ? timeline_->CurrentTimeSeconds() : base::nullopt;
       if (playback_rate) {
         if (timeline_time) {
-          start_time_ =
-              (timeline_time && unconstrained_current_time)
-                  ? ValueOrUnresolved((timeline_time.value() -
-                                       unconstrained_current_time.value()) /
-                                      playback_rate)
-                  : base::nullopt;
+          start_time_ = (timeline_time && unconstrained_current_time)
+                            ? base::make_optional<double>(
+                                  (timeline_time.value() -
+                                   unconstrained_current_time.value()) /
+                                  playback_rate)
+                            : base::nullopt;
         }
       } else {
         start_time_ = timeline_time;
@@ -1488,7 +1488,7 @@ void Animation::setPlaybackRate(double playback_rate,
   pending_playback_rate_ = base::nullopt;
   double previous_current_time = currentTime();
   playback_rate_ = playback_rate;
-  if (!IsNull(previous_current_time)) {
+  if (!Timing::IsNull(previous_current_time)) {
     setCurrentTime(previous_current_time, false, exception_state);
   }
 
@@ -1626,7 +1626,7 @@ void Animation::StartAnimationOnCompositor(
     time_offset = time_offset / fabs(EffectivePlaybackRate());
   }
 
-  DCHECK(!start_time || !IsNull(start_time.value()));
+  DCHECK(!start_time || !Timing::IsNull(start_time.value()));
   DCHECK_NE(compositor_group_, 0);
   DCHECK(To<KeyframeEffect>(content_.Get()));
   DCHECK(std::isfinite(time_offset));
