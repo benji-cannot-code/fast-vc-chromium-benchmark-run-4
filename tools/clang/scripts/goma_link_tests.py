3575accf35a3f4f78b588d01ddc619c1d5e11b00
inglorion <inglorion@chromium.org>
2020-03-18 01:38:12 +0000

    Update goma_link_tests to look for '<func>' instead of 'func'
    LLVM D75713 changed llvm-objdump disassembly to display function names
    in angle brackets (e.g. "main" becomes "<main>"). This change
    modifies goma_link_tests.py to match.
    
    Reviewers: akhuang, gbiv, hans
    Change-Id: I2f601a85608561c741c2d4285f17d73b9983faa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106827
    Reviewed-by: George Burgess <gbiv@chromium.org>
    Commit-Queue: Bob Haarman <inglorion@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751208}

--- tools/clang/scripts/goma_link_tests.py
+++ tools/clang/scripts/goma_link_tests.py
@@ -267,7 +267,7 @@ class GomaLdIntegrationTest(unittest.TestCase):
       self.assertFalse(os.path.exists(os.path.join(d, 'lto.main')))
       # Check that main calls foo.
       disasm = subprocess.check_output(['llvm-objdump', '-d', 'main'])
-      main_idx = disasm.index(b' main:\n')
+      main_idx = disasm.index(b' <main>:\n')
       after_main_idx = disasm.index(b'\n\n', main_idx)
       main_disasm = disasm[main_idx:after_main_idx]
       self.assertIn(b'foo', main_disasm)
@@ -291,7 +291,7 @@ class GomaLdIntegrationTest(unittest.TestCase):
       self.assertFalse(os.path.exists(os.path.join(d, 'lto.main')))
       # Check that main does not call foo.
       disasm = subprocess.check_output(['llvm-objdump', '-d', 'main'])
-      main_idx = disasm.index(b' main:\n')
+      main_idx = disasm.index(b' <main>:\n')
       after_main_idx = disasm.index(b'\n\n', main_idx)
       main_disasm = disasm[main_idx:after_main_idx]
       self.assertNotIn(b'foo', main_disasm)
@@ -319,7 +319,7 @@ class GomaLdIntegrationTest(unittest.TestCase):
         self.assertIn('build lto.main/foo.o : codegen ', buildrules)
       # Check that main does not call foo.
       disasm = subprocess.check_output(['llvm-objdump', '-d', 'main'])
-      main_idx = disasm.index(b' main:\n')
+      main_idx = disasm.index(b' <main>:\n')
       after_main_idx = disasm.index(b'\n\n', main_idx)
       main_disasm = disasm[main_idx:after_main_idx]
       self.assertNotIn(b'foo', main_disasm)
@@ -351,7 +351,7 @@ class GomaLdIntegrationTest(unittest.TestCase):
         self.assertIn('build lto.main/foo.o : codegen ', buildrules)
       # Check that main does not call foo.
       disasm = subprocess.check_output(['llvm-objdump', '-d', 'main'])
-      main_idx = disasm.index(b' main:\n')
+      main_idx = disasm.index(b' <main>:\n')
       after_main_idx = disasm.index(b'\n\n', main_idx)
       main_disasm = disasm[main_idx:after_main_idx]
       self.assertNotIn(b'foo', main_disasm)
@@ -389,7 +389,7 @@ class GomaLdIntegrationTest(unittest.TestCase):
         self.assertIn('build lto.main/obj/foo.o : codegen ', buildrules)
       # Check that main does not call foo.
       disasm = subprocess.check_output(['llvm-objdump', '-d', 'main'])
-      main_idx = disasm.index(b' main:\n')
+      main_idx = disasm.index(b' <main>:\n')
       after_main_idx = disasm.index(b'\n\n', main_idx)
       main_disasm = disasm[main_idx:after_main_idx]
       self.assertNotIn(b'foo', main_disasm)
@@ -445,7 +445,7 @@ class GomaLdIntegrationTest(unittest.TestCase):
         self.assertIn('main.split.o', link_text)
       # Check that main does not call foo.
       disasm = subprocess.check_output(['llvm-objdump', '-d', 'main'])
-      main_idx = disasm.index(b' main:\n')
+      main_idx = disasm.index(b' <main>:\n')
       after_main_idx = disasm.index(b'\n\n', main_idx)
       main_disasm = disasm[main_idx:after_main_idx]
       self.assertNotIn(b'foo', main_disasm)
