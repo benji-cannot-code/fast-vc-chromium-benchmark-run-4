7e26cb980538a35e21dbfe405a77cbdeb50ec0f1
Mike Wittman <wittman@chromium.org>
2020-03-18 23:42:53 +0000

    [Sampling profiler] Clean up Unwinder::CanUnwindFrom interface
    Passes |frame| by const reference rather than const pointer, to be
    consistent with style.
    
    Bug: 1035855
    Change-Id: I783441fcfcbca46147bffc428a838a3920856f0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2091650
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751500}

--- base/profiler/native_unwinder_win.cc
+++ base/profiler/native_unwinder_win.cc
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-bool NativeUnwinderWin::CanUnwindFrom(const Frame* current_frame) const {
-  return current_frame->module && current_frame->module->IsNative();
+bool NativeUnwinderWin::CanUnwindFrom(const Frame& current_frame) const {
+  return current_frame.module && current_frame.module->IsNative();
 }
 
 // Attempts to unwind the frame represented by the context values. If
--- base/profiler/stack_sampler_impl_unittest.cc
+++ base/profiler/stack_sampler_impl_unittest.cc
@@ -117,7 +117,7 @@ class TestUnwinder : public Unwinder {
         stack_copy_(stack_copy),
         stack_copy_bottom_(stack_copy_bottom) {}
 
-  bool CanUnwindFrom(const Frame* current_frame) const override { return true; }
+  bool CanUnwindFrom(const Frame& current_frame) const override { return true; }
 
   UnwindResult TryUnwind(RegisterContext* thread_context,
                          uintptr_t stack_top,
@@ -149,7 +149,7 @@ class CallRecordingUnwinder : public Unwinder {
     update_modules_was_invoked_ = true;
   }
 
-  bool CanUnwindFrom(const Frame* current_frame) const override { return true; }
+  bool CanUnwindFrom(const Frame& current_frame) const override { return true; }
 
   UnwindResult TryUnwind(RegisterContext* thread_context,
                          uintptr_t stack_top,
@@ -239,7 +239,7 @@ class FakeTestUnwinder : public Unwinder {
   FakeTestUnwinder(const FakeTestUnwinder&) = delete;
   FakeTestUnwinder& operator=(const FakeTestUnwinder&) = delete;
 
-  bool CanUnwindFrom(const Frame* current_frame) const override {
+  bool CanUnwindFrom(const Frame& current_frame) const override {
     bool can_unwind = results_[current_unwind_].can_unwind;
     // NB: If CanUnwindFrom() returns false then TryUnwind() will not be
     // invoked, so current_unwind_ is guarantee to be incremented only once for
