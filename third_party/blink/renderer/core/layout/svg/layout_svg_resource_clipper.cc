44e19b5ce9fbdac88ebbc4bdb6f44a5f6534ab51
Fredrik Söderqvist <fs@opera.com>
2020-03-18 14:16:40 +0000

    Remove ad-hoc cycle solving code from LayoutSVGResourceClipper
    The generic cycle solver should find (and break) these cycles as well.
    
    Bug: 1028063
    Change-Id: Ic3773ae49f41b3c7e84f6aabe52d954cdf009374
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108502
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#751324}

--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.cc
@@ -107,7 +107,7 @@ Path PathFromElement(const SVGElement& element) {
 }  // namespace
 
 LayoutSVGResourceClipper::LayoutSVGResourceClipper(SVGClipPathElement* node)
-    : LayoutSVGResourceContainer(node), in_clip_expansion_(false) {}
+    : LayoutSVGResourceContainer(node) {}
 
 LayoutSVGResourceClipper::~LayoutSVGResourceClipper() = default;
 
