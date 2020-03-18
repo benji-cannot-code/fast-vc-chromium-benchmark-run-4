77da2012cac9e3fdb689cc750cd8a871ad13e4a7
Nate Fischer <ntfschr@chromium.org>
2020-03-18 18:08:42 +0000

    AW: dismiss dialog when user presses cancel
    Instead of finishing the Activity, this dismisses the dialog when the
    user presses "cancel." This permits the user to see information about
    other crashes, even if they don't want to upload them.
    
    Bug: 1062500
    Test: Manual
    Change-Id: Iee6d8e159aa61e77b1de1c97c7621f69657e9f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108165
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751405}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/devui/CrashesListActivity.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/devui/CrashesListActivity.java
@@ -274,7 +274,7 @@ public class CrashesListActivity extends Activity {
                                         + " Do you want to proceed?")
                                 .setPositiveButton("Upload",
                                         (dialog, id) -> attemptUploadCrash(crashInfo.localId))
-                                .setNegativeButton("Cancel", (dialog, id) -> finish())
+                                .setNegativeButton("Cancel", (dialog, id) -> dialog.dismiss())
                                 .create()
                                 .show();
                     } else {
