b2959d86e35ea2f0c3e8545a793de8e33e5cde3b
Wei Lee <wtlee@google.com>
2020-03-18 07:43:29 +0000

    camera: Add socket information when create/delete socket
    These lines are added to collect more information for debugging
    crbug.com/1053569
    
    Bug: 1053569
    Test: Information appear in logs
    Change-Id: Ifb1ed1c4762f1470bb1f626936a6b9a11f63dffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083979
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751252}

--- media/capture/video/chromeos/camera_hal_dispatcher_impl.cc
+++ media/capture/video/chromeos/camera_hal_dispatcher_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
+#include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/posix/eintr_wrapper.h"
@@ -238,6 +239,17 @@ void CameraHalDispatcherImpl::CreateSocket(base::WaitableEvent* started) {
     return;
   }
 
+  // TODO(crbug.com/1053569): Remove these lines once the issue is solved.
+  base::File::Info info;
+  if (!base::GetFileInfo(socket_path, &info)) {
+    LOG(WARNING) << "Failed to get the socket info after building Mojo channel";
+  } else {
+    LOG(WARNING) << "Building Mojo channel. Socket info:"
+                 << " creation_time: " << info.creation_time
+                 << " last_accessed: " << info.last_accessed
+                 << " last_modified: " << info.last_modified;
+  }
+
   // Change permissions on the socket.
   struct group arc_camera_group;
   struct group* result = nullptr;
@@ -393,6 +405,18 @@ void CameraHalDispatcherImpl::OnCameraHalClientConnectionError(
 
 void CameraHalDispatcherImpl::StopOnProxyThread() {
   DCHECK(proxy_task_runner_->BelongsToCurrentThread());
+
+  // TODO(crbug.com/1053569): Remove these lines once the issue is solved.
+  base::File::Info info;
+  if (!base::GetFileInfo(base::FilePath(kArcCamera3SocketPath), &info)) {
+    LOG(WARNING) << "Failed to get socket info before deleting";
+  } else {
+    LOG(WARNING) << "Delete socket. Socket info:"
+                 << " creation_time: " << info.creation_time
+                 << " last_accessed: " << info.last_accessed
+                 << " last_modified: " << info.last_modified;
+  }
+
   if (!base::DeleteFile(base::FilePath(kArcCamera3SocketPath),
                         /* recursive */ false)) {
     LOG(ERROR) << "Failed to delete " << kArcCamera3SocketPath;
