0b7c3b9f6e103e8fbf9a1c4748559dad11377b6a
Saurabh Nijhara <snijhara@google.com>
2020-03-18 20:22:11 +0000

    Network state handling in update required screen
    This CL adds the following network handling for the update required
    screen :-
    1) Metered(cellular) network at the start - Show dialog to connect to a
    non metered network or accept update over metered. If good network is
    connected, update starts. If accepted to update, update starts.
    2) No network at the start - User is prompted to connect to a network.
    When connected to a good network, update starts.
    3) Good connection is lost and metered connected while updating -
    User prompted to accept update over metered. If accepted, update
    starts.
    
    We register for the network updates only up to starting the update
    process. Post that, the network changes are reflected in the update
    engine result and thus, handled for there.
    
    Bug: 1048607
    Change-Id: I50d939c0152f76f4456830cdd5e07136819b91a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2091534
    Commit-Queue: Saurabh Nijhara <snijhara@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751463}

--- chrome/browser/chromeos/login/screens/update_required_screen_unittest.cc
+++ chrome/browser/chromeos/login/screens/update_required_screen_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/fake_update_engine_client.h"
 #include "chromeos/dbus/update_engine_client.h"
 #include "chromeos/network/network_handler.h"
+#include "chromeos/network/network_state_test_helper.h"
 #include "chromeos/network/portal_detector/mock_network_portal_detector.h"
 #include "chromeos/network/portal_detector/network_portal_detector.h"
 #include "chromeos/tpm/stub_install_attributes.h"
@@ -55,6 +56,7 @@ class UpdateRequiredScreenUnitTest : public testing::Test {
     fake_update_engine_client_ = new FakeUpdateEngineClient();
     DBusThreadManager::GetSetterForTesting()->SetUpdateEngineClient(
         std::unique_ptr<UpdateEngineClient>(fake_update_engine_client_));
+
     NetworkHandler::Initialize();
     mock_network_portal_detector_ = new MockNetworkPortalDetector();
     network_portal_detector::SetNetworkPortalDetector(
@@ -71,6 +73,10 @@ class UpdateRequiredScreenUnitTest : public testing::Test {
 
     update_required_screen_->GetVersionUpdaterForTesting()
         ->set_wait_for_reboot_time_for_testing(base::TimeDelta::FromSeconds(0));
+
+    network_state_test_helper_ =
+        std::make_unique<chromeos::NetworkStateTestHelper>(
+            true /*use_default_devices_and_services*/);
   }
 
   void TearDown() override {
@@ -79,6 +85,7 @@ class UpdateRequiredScreenUnitTest : public testing::Test {
     update_required_screen_.reset();
     mock_error_view_.reset();
     mock_error_screen_.reset();
+    network_state_test_helper_.reset();
 
     network_portal_detector::Shutdown();
     NetworkHandler::Shutdown();
@@ -98,6 +105,8 @@ class UpdateRequiredScreenUnitTest : public testing::Test {
   MockNetworkPortalDetector* mock_network_portal_detector_;
   // Will be deleted in |DBusThreadManager::Shutdown()|.
   FakeUpdateEngineClient* fake_update_engine_client_;
+  // Initializes NetworkStateHandler
+  std::unique_ptr<chromeos::NetworkStateTestHelper> network_state_test_helper_;
 
  private:
   // Test versions of core browser infrastructure.
