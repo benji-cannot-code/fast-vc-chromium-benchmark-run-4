c2c97f5334f9a4e900c2e82d8b6623f3e2be613f
Xiaohan Wang <xhwang@chromium.org>
2020-03-18 04:22:11 +0000

    media: Remove PipelineTestRendererFactory
    PipelineTestRendererFactory is needed by remoting integration test which
    wraps the default renderer. Instead of PipelineTestRendererFactory and
    the subclasses, which are hard to read with implementation inheritance,
    this CL adds WrapRendererCB so remoting integration test can easily wrap
    the default renderer.
    
    This also fixes a bug I accidentally introduced previously that the
    |renderer_factory_| is actually not used by the pipeline. Instead, it
    uses CreateRenderer() directly which ignores the factory.
    
    Bug: 1062130
    Change-Id: I0482922d65fe1727d68936988f04b1e72c048350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106878
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751231}

--- media/remoting/integration_test.cc
+++ media/remoting/integration_test.cc
@@ -17,24 +17,10 @@ namespace {
 
 constexpr int kAppendTimeSec = 1;
 
-class TestRendererFactory final : public PipelineTestRendererFactory {
- public:
-  explicit TestRendererFactory(
-      std::unique_ptr<PipelineTestRendererFactory> renderer_factory)
-      : default_renderer_factory_(std::move(renderer_factory)) {}
-  ~TestRendererFactory() override = default;
-
-  // PipelineTestRendererFactory implementation.
-  std::unique_ptr<Renderer> CreateRenderer() override {
-    return std::make_unique<End2EndTestRenderer>(
-        default_renderer_factory_->CreateRenderer());
-  }
-
- private:
-  std::unique_ptr<PipelineTestRendererFactory> default_renderer_factory_;
-
-  DISALLOW_COPY_AND_ASSIGN(TestRendererFactory);
-};
+std::unique_ptr<Renderer> CreateEnd2EndTestRenderer(
+    std::unique_ptr<Renderer> default_renderer) {
+  return std::make_unique<End2EndTestRenderer>(std::move(default_renderer));
+}
 
 }  // namespace
 
@@ -42,9 +28,7 @@ class MediaRemotingIntegrationTest : public testing::Test,
                                      public PipelineIntegrationTestBase {
  public:
   MediaRemotingIntegrationTest() {
-    std::unique_ptr<PipelineTestRendererFactory> factory =
-        std::move(renderer_factory_);
-    renderer_factory_.reset(new TestRendererFactory(std::move(factory)));
+    SetWrapRendererCB(base::BindRepeating(&CreateEnd2EndTestRenderer));
   }
 
  private:
