8e84a54409134e444beff87a9d3a3034235a03e5
Kent Tamura <tkent@chromium.org>
2020-03-18 05:45:11 +0000

    Remove ShadowRoot check in LayoutBlock::HasLineIfEmpty
    LayoutObject::GetNode() can't be a ShadowRoot.
    This CL has no behavior changes.
    
    Change-Id: I0a29734abf4fbf388b4702038f051910cdd28956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108262
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751242}

--- third_party/blink/renderer/core/layout/layout_block.cc
+++ third_party/blink/renderer/core/layout/layout_block.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
-#include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/editing/drag_caret.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/frame_selection.h"
@@ -1681,11 +1680,6 @@ bool LayoutBlock::HasLineIfEmpty() const {
   if (GetNode()) {
     if (IsRootEditableElement(*GetNode()))
       return true;
-
-    if (auto* shadow_root = DynamicTo<ShadowRoot>(GetNode())) {
-      if (IsA<HTMLInputElement>(shadow_root->host()))
-        return true;
-    }
   }
   return FirstLineStyleRef().HasLineIfEmpty();
 }
