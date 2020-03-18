d9c6e5b877aa0953706ac6fc02ee5b4b710064ed
Omer Katz <omerkatz@chromium.org>
2020-03-18 13:02:00 +0000

    heap: Make IsOufOfLineBuffer an instance method
    Bug: 986235
    Change-Id: I90985578846a64c0da4900b0f600f5ef53720391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106156
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751302}

--- third_party/blink/renderer/platform/wtf/vector.h
+++ third_party/blink/renderer/platform/wtf/vector.h
@@ -603,7 +603,7 @@ class VectorBuffer<T, 0, Allocator> : protected VectorBufferBase<T, Allocator> {
   bool HasOutOfLineBuffer() const {
     // When inlineCapacity is 0 we have an out of line buffer if we have a
     // buffer.
-    return IsOutOfLineBuffer(this, Buffer());
+    return IsOutOfLineBuffer(Buffer());
   }
 
   T** BufferSlot() { return &buffer_; }
@@ -614,9 +614,7 @@ class VectorBuffer<T, 0, Allocator> : protected VectorBufferBase<T, Allocator> {
 
   using Base::size_;
 
-  static bool IsOutOfLineBuffer(const VectorBuffer* vector, const T* buffer) {
-    return buffer;
-  }
+  bool IsOutOfLineBuffer(const T* buffer) const { return buffer; }
 
  private:
   using Base::buffer_;
@@ -890,7 +888,7 @@ class VectorBuffer : protected VectorBufferBase<T, Allocator> {
   using Base::Buffer;
   using Base::capacity;
 
-  bool HasOutOfLineBuffer() const { return IsOutOfLineBuffer(this, Buffer()); }
+  bool HasOutOfLineBuffer() const { return IsOutOfLineBuffer(Buffer()); }
 
   T** BufferSlot() { return &buffer_; }
   const T* const* BufferSlot() const { return &buffer_; }
@@ -900,8 +898,8 @@ class VectorBuffer : protected VectorBufferBase<T, Allocator> {
 
   using Base::size_;
 
-  static bool IsOutOfLineBuffer(const VectorBuffer* vector, const T* buffer) {
-    return buffer && buffer != vector->InlineBuffer();
+  bool IsOutOfLineBuffer(const T* buffer) const {
+    return buffer && buffer != InlineBuffer();
   }
 
  private:
@@ -2048,7 +2046,7 @@ Vector<T, inlineCapacity, Allocator>::Trace(VisitorDispatcher visitor) const {
                 "Garbage collector must be enabled.");
 
   const T* buffer = BufferSafe();
-  if (Base::IsOutOfLineBuffer(this, buffer)) {
+  if (Base::IsOutOfLineBuffer(buffer)) {
     Allocator::TraceVectorBacking(visitor, buffer, Base::BufferSlot());
   } else {
     // We should not visit inline buffers, but we still need to register the
