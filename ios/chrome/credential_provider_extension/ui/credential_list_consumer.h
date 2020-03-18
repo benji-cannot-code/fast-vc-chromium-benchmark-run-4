9cece389d687b5cb4d33c53fa9de3d3d59f76934
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-03-18 23:56:38 +0000

    [iOS][Credential-Provider] Credential list
    Adds the skeleton for the credential list. As this is going to be the
    main view controller it has a Coordinator to control all the
    presentations as well as the mediator and the associated protocols.
    
    In follow ups CLs the view controller will add a table view controller
    and a search bar. The mediator will fetch the passwords, filter and
    provide recommendations based on the service identifiers, as well as
    validate the log in state before providing passwords.
    
    Bug: 1045454
    Change-Id: I5f539c489629d04ebd11a79f49b8b420a24ef1d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2094310
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751524}

---
+++ ios/chrome/credential_provider_extension/ui/credential_list_consumer.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_CREDENTIAL_PROVIDER_EXTENSION_UI_CREDENTIAL_LIST_CONSUMER_H_
+#define IOS_CHROME_CREDENTIAL_PROVIDER_EXTENSION_UI_CREDENTIAL_LIST_CONSUMER_H_
+
+@class UIButton;
+
+@protocol CredentialListConsumerDelegate <NSObject>
+
+// Called when the user taps the cancel button in the navigation bar.
+- (void)navigationCancelButtonWasPressed:(UIButton*)button;
+
+@end
+
+@protocol CredentialListConsumer <NSObject>
+
+// The delegate for the actions in the consumer.
+@property(nonatomic, weak) id<CredentialListConsumerDelegate> delegate;
+
+@end
+
+#endif  // IOS_CHROME_CREDENTIAL_PROVIDER_EXTENSION_UI_CREDENTIAL_LIST_CONSUMER_H_
