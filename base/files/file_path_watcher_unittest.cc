972054f0eb54490e15768aaeb02aabb75c77c8b4
Elly Fong-Jones <ellyjones@google.com>
2020-03-18 18:32:11 +0000

    mac: speculatively re-enable two flaky tests
    These were both marked flaky in 2013. They pass locally, so let's try
    re-enabling them and seeing if they still flake.
    
    Bug: 85930
    Change-Id: I6abe61923c8eb54360ae4ef5d93f08351262bd7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108627
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751420}

--- base/files/file_path_watcher_unittest.cc
+++ base/files/file_path_watcher_unittest.cc
@@ -311,8 +311,7 @@ TEST_F(FilePathWatcherTest, DeleteDuringNotify) {
 
 // Verify that deleting the watcher works even if there is a pending
 // notification.
-// Flaky on MacOS (and ARM linux): http://crbug.com/85930
-TEST_F(FilePathWatcherTest, DISABLED_DestroyWithPendingNotification) {
+TEST_F(FilePathWatcherTest, DestroyWithPendingNotification) {
   std::unique_ptr<TestDelegate> delegate(new TestDelegate(collector()));
   FilePathWatcher watcher;
   ASSERT_TRUE(SetupWatch(test_file(), &watcher, delegate.get(), false));
@@ -387,10 +386,6 @@ TEST_F(FilePathWatcherTest, DirectoryChain) {
   ASSERT_TRUE(WaitForEvents());
 }
 
-#if defined(OS_MACOSX)
-// http://crbug.com/85930
-#define DisappearingDirectory DISABLED_DisappearingDirectory
-#endif
 TEST_F(FilePathWatcherTest, DisappearingDirectory) {
   FilePathWatcher watcher;
   FilePath dir(temp_dir_.GetPath().AppendASCII("dir"));
