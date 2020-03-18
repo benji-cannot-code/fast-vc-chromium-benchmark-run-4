78460370df29b49cb9c6dc25e9a2ae0ffba53d9d
Theresa Wellington <twellington@chromium.org>
2020-03-18 17:32:08 +0000

    Check for null test server in BookmarkTest#teardown
    If the setup fails for some reason, the test server may be null. Check
    for null so that the test failure reason shows the startup error.
    
    BUG=1062642
    
    Change-Id: I2a7710da86f94ae50420f010c4ec74a8070381f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108602
    Reviewed-by: Lijin Shen <lazzzis@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751387}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
@@ -158,7 +158,7 @@ public class BookmarkTest {
 
     @After
     public void tearDown() {
-        mTestServer.stopAndDestroyServer();
+        if (mTestServer != null) mTestServer.stopAndDestroyServer();
     }
 
     @AfterClass
