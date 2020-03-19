f157136795bb661afeb83347860481c01420a5a9
Joshua Pawlicki <waffles@chromium.org>
2020-03-19 19:41:05 +0000

    Run updater tests serially.
    Bug: 1062285
    Change-Id: I0f9d1f97e77dc446a7a622b24c693a285efafef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2110810
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751769}

--- chrome/updater/run_all_unittests.cc
+++ chrome/updater/run_all_unittests.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 int main(int argc, char** argv) {
   base::TestSuite test_suite(argc, argv);
   chrome::RegisterPathProvider();
-  return base::LaunchUnitTests(
+  return base::LaunchUnitTestsSerially(
       argc, argv,
       base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
 }
