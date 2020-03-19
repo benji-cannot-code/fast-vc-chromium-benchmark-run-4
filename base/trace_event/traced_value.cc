aaaf37a6e089238699c2163988572c8a2c399ed0
Xianzhu Wang <wangxianzhu@chromium.org>
2020-03-19 15:50:33 +0000

    Handle nan and infinity values in TracedValueJSON::ToBaseValue
    Bug: 1056841
    Change-Id: I08e2c92b4d58ee751543162aaab2ebe6df1fd20b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2109024
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751689}

--- base/trace_event/traced_value.cc
+++ base/trace_event/traced_value.cc
@@ -270,7 +270,7 @@ class PickleWriter final : public TracedValue::Writer {
           TraceEvent::TraceValue json_value;
           CHECK(it.ReadBool(&json_value.as_bool));
           maybe_append_key_name(state_stack[current_state_index], &it, out);
-          TraceEvent::AppendValueAsJSON(TRACE_VALUE_TYPE_BOOL, json_value, out);
+          json_value.AppendAsJSON(TRACE_VALUE_TYPE_BOOL, out);
           break;
         }
 
@@ -280,7 +280,7 @@ class PickleWriter final : public TracedValue::Writer {
           maybe_append_key_name(state_stack[current_state_index], &it, out);
           TraceEvent::TraceValue json_value;
           json_value.as_int = value;
-          TraceEvent::AppendValueAsJSON(TRACE_VALUE_TYPE_INT, json_value, out);
+          json_value.AppendAsJSON(TRACE_VALUE_TYPE_INT, out);
           break;
         }
 
@@ -288,8 +288,7 @@ class PickleWriter final : public TracedValue::Writer {
           TraceEvent::TraceValue json_value;
           CHECK(it.ReadDouble(&json_value.as_double));
           maybe_append_key_name(state_stack[current_state_index], &it, out);
-          TraceEvent::AppendValueAsJSON(TRACE_VALUE_TYPE_DOUBLE, json_value,
-                                        out);
+          json_value.AppendAsJSON(TRACE_VALUE_TYPE_DOUBLE, out);
           break;
         }
 
@@ -299,8 +298,7 @@ class PickleWriter final : public TracedValue::Writer {
           maybe_append_key_name(state_stack[current_state_index], &it, out);
           TraceEvent::TraceValue json_value;
           json_value.as_string = value.c_str();
-          TraceEvent::AppendValueAsJSON(TRACE_VALUE_TYPE_STRING, json_value,
-                                        out);
+          json_value.AppendAsJSON(TRACE_VALUE_TYPE_STRING, out);
           break;
         }
 
@@ -400,12 +398,23 @@ class PickleWriter final : public TracedValue::Writer {
         } break;
 
         case kTypeDouble: {
-          double value;
-          CHECK(it.ReadDouble(&value));
-          if (cur_dict) {
-            cur_dict->SetDoubleKey(ReadKeyName(it), value);
+          TraceEvent::TraceValue trace_value;
+          CHECK(it.ReadDouble(&trace_value.as_double));
+          Value base_value;
+          if (!std::isfinite(trace_value.as_double)) {
+            // base::Value doesn't support nan and infinity values. Use strings
+            // for them instead. This follows the same convention in
+            // AppendAsTraceFormat(), supported by TraceValue::Append*().
+            std::string value_string;
+            trace_value.AppendAsString(TRACE_VALUE_TYPE_DOUBLE, &value_string);
+            base_value = Value(value_string);
           } else {
-            cur_list->Append(value);
+            base_value = Value(trace_value.as_double);
+          }
+          if (cur_dict) {
+            cur_dict->SetKey(ReadKeyName(it), std::move(base_value));
+          } else {
+            cur_list->Append(std::move(base_value));
           }
         } break;
 
