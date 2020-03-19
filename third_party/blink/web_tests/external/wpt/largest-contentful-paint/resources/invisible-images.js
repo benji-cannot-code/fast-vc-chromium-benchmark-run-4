0ea85d34d3ae0839e10daab191a6ce62a07e073d
Xianzhu Wang <wangxianzhu@chromium.org>
2020-03-19 18:26:34 +0000

    Split external/wpt/largest-contentful-paint/invisible-images-composited.html
    We still have cases that fail in pre-CompositeAfterPaint. Split the test
    into 2 to ensure test coverage of the passing cases.
    
    Bug: 957674
    Change-Id: I951c28eb3f4d44f122c3d0314d24b454a2e56bc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2109144
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751734}

---
+++ third_party/blink/web_tests/external/wpt/largest-contentful-paint/resources/invisible-images.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async_test(t => {
+  assert_precondition(window.LargestContentfulPaint, "LargestContentfulPaint is not implemented");
+  const observer = new PerformanceObserver(
+    t.step_func(entryList => {
+       entryList.getEntries().forEach(entry => {
+       // May receive a text entry. Ignore that entry.
+        if (!entry.url) {
+          return;
+        }
+        // The images should not have caused an entry, so fail test.
+        assert_unreached('Should not have received an entry! Received one with id '
+            + entryList.getEntries()[0].id);
+      });
+    })
+  );
+  observer.observe({type: 'largest-contentful-paint', buffered: true});
+  // Images have been added but should not cause entries to be dispatched.
+  // Wait for 500ms and end test, ensuring no entry was created.
+  t.step_timeout(() => {
+    t.done();
+  }, 500);
+}, 'Images with opacity: 0, visibility: hidden, or display: none are not observable by LargestContentfulPaint.');
