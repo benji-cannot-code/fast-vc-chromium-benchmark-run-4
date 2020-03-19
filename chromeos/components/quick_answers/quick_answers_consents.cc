9499184254681bfd09996ff5abcd47c590cb26b1
Li Lin <llin@chromium.org>
2020-03-19 20:09:23 +0000

    Add Quick Answers consent logging.
    Histogram recorded to understand user's interactions with Quick Answers consent:
    * QuickAnswers.Consent - How many times user has seens the consent before.
    * QuickAnswers.Consent.Duration.[Interaction Type] - How long user has seens the consent before the interaction.
    * QuickAnswers.Consent.Impression.[Interaction Type] - How many times user has seens the consent before the interaction.
    
    The interaction type is one of the following:
    * Accept - click on the "Got it" button.
    * ManageSettings - click on the "Manage settings" button.
    * Dismiss - the consent is dismissed.
    
    Bug: b/151112171
    Test: Manual
    
    Change-Id: I29dedf999bc0f1560d5fce4707eba5229d4cde88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2100937
    Commit-Queue: Li Lin <llin@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751780}

--- chromeos/components/quick_answers/quick_answers_consents.cc
+++ chromeos/components/quick_answers/quick_answers_consents.cc
@@ -11,17 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_prefs.h"
+#include "chromeos/components/quick_answers/utils/quick_answers_metrics.h"
 #include "components/prefs/pref_service.h"
 
 namespace chromeos {
 namespace quick_answers {
-
-namespace {
-
-constexpr int kImpressionCap = 3;
-constexpr int kDurationCap = 8;
-
-}  // namespace
 
 QuickAnswersConsent::QuickAnswersConsent(PrefService* prefs) : prefs_(prefs) {}
 
@@ -30,16 +24,26 @@ QuickAnswersConsent::~QuickAnswersConsent() = default;
 void QuickAnswersConsent::StartConsent() {
   // Increments impression count.
   IncrementPrefCounter(prefs::kQuickAnswersConsentImpressionCount, 1);
+
+  // Logs consent impression with how many times the user has seen the consent.
+  RecordConsentImpression(GetImpressionCount());
+
   start_time_ = base::TimeTicks::Now();
 }
 
 void QuickAnswersConsent::DismissConsent() {
   RecordImpressionDuration();
+  // Logs consent dismissed with impression count and impression duration.
+  RecordConsentInteraction(ConsentInteractionType::kDismiss,
+                           GetImpressionCount(), GetImpressionDuration());
 }
 
 void QuickAnswersConsent::AcceptConsent(ConsentInteractionType interaction) {
-  // TODO(llin): Use |interaction| for user-consent related logging.
   RecordImpressionDuration();
+  // Logs consent accepted with impression count and impression duration.
+  RecordConsentInteraction(interaction, GetImpressionCount(),
+                           GetImpressionDuration());
+
   // Marks the consent as accepted.
   prefs_->SetBoolean(prefs::kQuickAnswersConsented, true);
 }
@@ -54,15 +58,13 @@ bool QuickAnswersConsent::HasConsented() const {
 }
 
 bool QuickAnswersConsent::HasReachedImpressionCap() const {
-  int impression_count =
-      prefs_->GetInteger(prefs::kQuickAnswersConsentImpressionCount);
-  return impression_count + 1 > kImpressionCap;
+  return GetImpressionCount() + 1 > kConsentImpressionCap;
 }
 
 bool QuickAnswersConsent::HasReachedDurationCap() const {
   int duration_secs =
       prefs_->GetInteger(prefs::kQuickAnswersConsentImpressionDuration);
-  return duration_secs >= kDurationCap;
+  return duration_secs >= kConsentDurationCap;
 }
 
 void QuickAnswersConsent::IncrementPrefCounter(const std::string& path,
@@ -71,12 +73,18 @@ void QuickAnswersConsent::IncrementPrefCounter(const std::string& path,
 }
 
 void QuickAnswersConsent::RecordImpressionDuration() {
-  DCHECK(!start_time_.is_null());
-
   // Records duration in pref.
-  base::TimeDelta duration = base::TimeTicks::Now() - start_time_;
   IncrementPrefCounter(prefs::kQuickAnswersConsentImpressionDuration,
-                       duration.InSeconds());
+                       GetImpressionDuration().InSeconds());
+}
+
+int QuickAnswersConsent::GetImpressionCount() const {
+  return prefs_->GetInteger(prefs::kQuickAnswersConsentImpressionCount);
+}
+
+base::TimeDelta QuickAnswersConsent::GetImpressionDuration() const {
+  DCHECK(!start_time_.is_null());
+  return base::TimeTicks::Now() - start_time_;
 }
 
 }  // namespace quick_answers
