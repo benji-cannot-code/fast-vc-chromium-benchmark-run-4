1755a95a62029c7fc68ce6afb919ee5f5d3c661d
Mark Yacoub <markyacoub@google.com>
2020-03-19 18:17:01 +0000

    Reland "Ozone: Implement Atomic Modeset"
    If the device supports KMS Atomic APIs, Enable CRTC using Atomic Modeset
    Commit instead of legacy SetCRTC.
    
    (reverted on crrev.com/c/2080775)
    
    BUG=987274
    TEST=displays should start and continue to function normally for devices
    supporting atomic APIs.
    HardwareDisplayControllerTest.CheckModesettingSetsProps
    HardwareDisplayPlaneManagerAtomicTest.CheckPropsAfterModeset
    
    Change-Id: If08b015d1e62f51e9ec3c243c1e5a2965449345e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106476
    Commit-Queue: Mark Yacoub <markyacoub@google.com>
    Auto-Submit: Mark Yacoub <markyacoub@google.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751717}

--- ui/ozone/platform/drm/gpu/crtc_controller.h
+++ ui/ozone/platform/drm/gpu/crtc_controller.h
@@ -52,7 +52,8 @@ class CrtcController {
   bool Disable();
 
   bool AssignOverlayPlanes(HardwareDisplayPlaneList* plane_list,
-                           const DrmOverlayPlaneList& planes);
+                           const DrmOverlayPlaneList& planes,
+                           bool is_modesetting);
 
   // Returns a vector of format modifiers for the given fourcc format
   // on this CRTCs primary plane. A format modifier describes the
