2c727e509960692f3bdc7e577cce8b27b6461f50
Istiaque Ahmed <lazyboy@chromium.org>
2020-03-19 19:49:15 +0000

    Replace ParseJsonDeprecated with ParseJson.
    The callsites were dereferencing return value of
    ParseJsonDeprecated anyway, so make it use non-deprecated ParseJson.
    
    Bug: 932873
    Test: Unit test cleanup, no behavior change.
    Change-Id: I80fd06bc3ecfa39d0f5cad9cb88631e685bc122c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108004
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751774}

--- components/cast_channel/cast_message_util_unittest.cc
+++ components/cast_channel/cast_message_util_unittest.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/openscreen/src/cast/common/channel/proto/cast_channel.pb.h"
 
 using base::test::IsJson;
-using base::test::ParseJsonDeprecated;
+using base::test::ParseJson;
 
 namespace cast_channel {
 
@@ -39,8 +39,7 @@ TEST(CastMessageUtilTest, GetLaunchSessionResponseOk) {
     }
   )";
 
-  LaunchSessionResponse response =
-      GetLaunchSessionResponse(*ParseJsonDeprecated(payload));
+  LaunchSessionResponse response = GetLaunchSessionResponse(ParseJson(payload));
   EXPECT_EQ(LaunchSessionResponse::Result::kOk, response.result);
   EXPECT_TRUE(response.receiver_status);
 }
@@ -53,8 +52,7 @@ TEST(CastMessageUtilTest, GetLaunchSessionResponseError) {
     }
   )";
 
-  LaunchSessionResponse response =
-      GetLaunchSessionResponse(*ParseJsonDeprecated(payload));
+  LaunchSessionResponse response = GetLaunchSessionResponse(ParseJson(payload));
   EXPECT_EQ(LaunchSessionResponse::Result::kError, response.result);
   EXPECT_FALSE(response.receiver_status);
 }
@@ -69,8 +67,7 @@ TEST(CastMessageUtilTest, GetLaunchSessionResponseUnknown) {
     }
   )";
 
-  LaunchSessionResponse response =
-      GetLaunchSessionResponse(*ParseJsonDeprecated(payload));
+  LaunchSessionResponse response = GetLaunchSessionResponse(ParseJson(payload));
   EXPECT_EQ(LaunchSessionResponse::Result::kUnknown, response.result);
   EXPECT_FALSE(response.receiver_status);
 }
@@ -128,8 +125,8 @@ TEST(CastMessageUtilTest, CreateMediaRequest) {
        "requestId": 123,
     })";
 
-  CastMessage message = CreateMediaRequest(*ParseJsonDeprecated(body), 123,
-                                           "theSourceId", "theDestinationId");
+  CastMessage message = CreateMediaRequest(ParseJson(body), 123, "theSourceId",
+                                           "theDestinationId");
   ASSERT_TRUE(IsCastMessageValid(message));
   EXPECT_EQ(kMediaNamespace, message.namespace_());
   EXPECT_EQ("theSourceId", message.source_id());
@@ -148,7 +145,7 @@ TEST(CastMessageUtilTest, CreateVolumeRequest) {
     })";
 
   CastMessage message =
-      CreateSetVolumeRequest(*ParseJsonDeprecated(body), 123, "theSourceId");
+      CreateSetVolumeRequest(ParseJson(body), 123, "theSourceId");
   ASSERT_TRUE(IsCastMessageValid(message));
   EXPECT_EQ(kReceiverNamespace, message.namespace_());
   EXPECT_EQ("theSourceId", message.source_id());
