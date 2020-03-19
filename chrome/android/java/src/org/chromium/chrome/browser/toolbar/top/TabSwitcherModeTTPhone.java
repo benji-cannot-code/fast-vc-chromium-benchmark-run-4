def2a26c6c95c8dc5ebd60527c7ae79c27b9b0f3
gogerald <gogerald@google.com>
2020-03-19 19:02:02 +0000

    [StartSurface] Remove unused code from TabSwitcherModeTT*
    StartSurfaceToolbar* is used when start surface is enabled right now.
    
    Bug: 990600
    Change-Id: Ib63ae8423c1027cacd27d88179a451f014055f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2094713
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751763}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherModeTTPhone.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherModeTTPhone.java
@@ -33,7 +33,6 @@ import org.chromium.chrome.browser.toolbar.TabCountProvider;
 import org.chromium.chrome.browser.toolbar.bottom.BottomToolbarConfiguration;
 import org.chromium.chrome.browser.toolbar.bottom.BottomToolbarVariationManager;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuButtonHelper;
-import org.chromium.chrome.features.start_surface.StartSurfaceConfiguration;
 import org.chromium.components.browser_ui.styles.ChromeColors;
 import org.chromium.components.browser_ui.widget.animation.CancelAwareAnimatorListener;
 import org.chromium.components.browser_ui.widget.animation.Interpolators;
@@ -386,11 +385,6 @@ public class TabSwitcherModeTTPhone extends OptimizedFrameLayout
     }
 
     private void setIncognitoToggleVisibility(boolean showIncognitoToggle) {
-        // If StartSurface is enabled, the incognito switch is shown and handled
-        // by the IncognitoSwitchCoordinator in the
-        // TabSwitcherModeTTCoordinatorPhone.
-        if (StartSurfaceConfiguration.isStartSurfaceEnabled()) return;
-
         if (mIncognitoToggleTabLayout == null) {
             if (showIncognitoToggle) inflateIncognitoToggle();
         } else {
