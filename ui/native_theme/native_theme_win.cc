8a9a8fc496f49c18eaddcd8fe7c8fa06c39061de
Allen Bauer <kylixrd@chromium.org>
2020-03-19 02:48:05 +0000

    Make SystemDarkModeSupported static since it's a system wide setting.
    TBR=asvitkine@chromium.org
    
    Bug: 1056944
    Change-Id: Id0b82d349e665dc91f21bf4db372fa3e5f7e58ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106813
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751577}

--- ui/native_theme/native_theme_win.cc
+++ ui/native_theme/native_theme_win.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/optional.h"
 #include "base/stl_util.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/win/scoped_gdi_object.h"
@@ -137,6 +138,15 @@ class ScopedCreateDCWithBitmap {
   DISALLOW_COPY_AND_ASSIGN(ScopedCreateDCWithBitmap);
 };
 
+base::win::RegKey OpenThemeRegKey(REGSAM access) {
+  base::win::RegKey hkcu_themes_regkey;
+  hkcu_themes_regkey.Open(HKEY_CURRENT_USER,
+                          L"Software\\Microsoft\\Windows\\CurrentVersion\\"
+                          L"Themes\\Personalize",
+                          access);
+  return hkcu_themes_regkey;
+}
+
 }  // namespace
 
 namespace ui {
@@ -174,6 +184,13 @@ NativeTheme* NativeTheme::GetInstanceForNativeUi() {
 
 NativeTheme* NativeTheme::GetInstanceForDarkUI() {
   return NativeThemeWin::dark_instance();
+}
+
+// static
+bool NativeTheme::SystemDarkModeSupported() {
+  static bool system_supports_dark_mode =
+      ([]() { return OpenThemeRegKey(KEY_READ).Valid(); })();
+  return system_supports_dark_mode;
 }
 
 // static
@@ -273,13 +290,8 @@ NativeThemeWin::NativeThemeWin(bool configure_web_instance,
     // Dark Mode currently targets UWP apps, which means Win32 apps need to use
     // alternate, less reliable means of detecting the state. The following
     // can break in future Windows versions.
-    bool key_open_succeeded =
-        hkcu_themes_regkey_.Open(
-            HKEY_CURRENT_USER,
-            L"Software\\Microsoft\\Windows\\CurrentVersion\\"
-            L"Themes\\Personalize",
-            KEY_READ | KEY_NOTIFY) == ERROR_SUCCESS;
-    if (key_open_succeeded) {
+    hkcu_themes_regkey_ = OpenThemeRegKey(KEY_READ | KEY_NOTIFY);
+    if (hkcu_themes_regkey_.Valid()) {
       UpdateDarkModeStatus();
       RegisterThemeRegkeyObserver();
     }
@@ -716,10 +728,6 @@ bool NativeThemeWin::ShouldUseDarkColors() const {
   if (UsesHighContrastColors() && !IsForcedDarkMode())
     return false;
   return NativeTheme::ShouldUseDarkColors();
-}
-
-bool NativeThemeWin::SystemDarkModeSupported() const {
-  return hkcu_themes_regkey_.Valid();
 }
 
 NativeTheme::PreferredColorScheme
