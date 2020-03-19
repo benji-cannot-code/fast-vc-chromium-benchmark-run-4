61725de53c7f354e863be5ed29586b5dc3565f92
Xing Liu <xingliu@chromium.org>
2020-03-19 18:24:24 +0000

    Upboarding: Setup image cache persistence layer boilerplates.
    This CL adds image cache protobuf and store interfaces. No
    implementation is added yet.
    
    The raw bytes are saved in a separate store, which only supports loading
    one image at a time in its API.
    
    Bug: 1058534
    Change-Id: I0646b9906b81432e39f29dd5f235481d12a756c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108180
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751729}

--- chrome/browser/upboarding/query_tiles/internal/store.h
+++ chrome/browser/upboarding/query_tiles/internal/store.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UPBOARDING_QUERY_TILES_INTERNAL_STORE_H_
 #define CHROME_BROWSER_UPBOARDING_QUERY_TILES_INTERNAL_STORE_H_
 
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
 
+#include "base/callback.h"
 #include "base/macros.h"
 
 namespace upboarding {
