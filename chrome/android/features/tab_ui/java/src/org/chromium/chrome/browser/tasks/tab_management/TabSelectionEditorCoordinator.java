833f76dd63e34a35f816dbd083e3e7bc77643abd
Mei Liang <meiliang@chromium.org>
2020-03-19 03:40:21 +0000

    [TabSelectionEditor] Add List mode supports
    This CL enables TabSelectionEditor to show a List for low-end devices.
    
    Change-Id: Ie60c1e7ddcda09c2f32e23c4de52be7e642e09e0
    Bug: 1042889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103313
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751588}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorCoordinator.java
@@ -10,9 +10,10 @@ import static org.chromium.chrome.browser.tasks.tab_management.TabListModel.Card
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -102,7 +103,7 @@ class TabSelectionEditorCoordinator {
     }
 
     private final Context mContext;
-    private final View mParentView;
+    private final ViewGroup mParentView;
     private final TabModelSelector mTabModelSelector;
     private final TabSelectionEditorLayout mTabSelectionEditorLayout;
     private final TabListCoordinator mTabListCoordinator;
@@ -111,19 +112,18 @@ class TabSelectionEditorCoordinator {
     private final PropertyModelChangeProcessor mTabSelectionEditorLayoutChangeProcessor;
     private final TabSelectionEditorMediator mTabSelectionEditorMediator;
 
-    public TabSelectionEditorCoordinator(Context context, View parentView,
+    public TabSelectionEditorCoordinator(Context context, ViewGroup parentView,
             TabModelSelector tabModelSelector, TabContentManager tabContentManager,
             @Nullable TabSelectionEditorMediator
-                    .TabSelectionEditorPositionProvider positionProvider) {
+                    .TabSelectionEditorPositionProvider positionProvider,
+            @TabListCoordinator.TabListMode int mode) {
         mContext = context;
         mParentView = parentView;
         mTabModelSelector = tabModelSelector;
 
-        // TODO(crbug.com/1007598): construct TabListCoordinator with List mode if it's a low end
-        // device, and TabGroupContinuation is turned on.
-        mTabListCoordinator = new TabListCoordinator(TabListCoordinator.TabListMode.GRID, context,
-                mTabModelSelector, tabContentManager::getTabThumbnailWithCallback, null, false,
-                null, null, TabProperties.UiType.SELECTABLE, this::getSelectionDelegate, null, null,
+        mTabListCoordinator = new TabListCoordinator(mode, context, mTabModelSelector,
+                tabContentManager::getTabThumbnailWithCallback, null, false, null, null,
+                TabProperties.UiType.SELECTABLE, this::getSelectionDelegate, mParentView, null,
                 false, COMPONENT_NAME);
         mTabListCoordinator.registerItemType(TabProperties.UiType.DIVIDER,
                 new LayoutViewBuilder(R.layout.divider_preference),
@@ -197,5 +197,13 @@ class TabSelectionEditorCoordinator {
         mTabSelectionEditorLayout.destroy();
         mTabSelectionEditorMediator.destroy();
         mTabSelectionEditorLayoutChangeProcessor.destroy();
+    }
+
+    /**
+     * @return The {@link TabSelectionEditorLayout} for testing.
+     */
+    @VisibleForTesting
+    public TabSelectionEditorLayout getTabSelectionEditorLayoutForTesting() {
+        return mTabSelectionEditorLayout;
     }
 }
