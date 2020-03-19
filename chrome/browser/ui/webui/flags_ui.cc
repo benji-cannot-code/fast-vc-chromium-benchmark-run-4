ee88cb07e12740a4efd12c79fc97d74c52a47352
Xiyuan Xia <xiyuan@chromium.org>
2020-03-19 23:25:36 +0000

    cros: Show an info bar when flags are ignored
    When chrome crashes more than 3 times within first 60s on start,
    session_manager daemon would restart chrome with "--safe-mode" and
    no longer apply switches for user flags. It is confusing that
    flags UI still shows the user flags as enabled. Show an info bar
    on flags ui to resolve this confusion.
    
    Bug: 1062143, 438436
    Change-Id: I2875d6e6e3905fc10bf74f0b21712c1a034a7c34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111052
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751838}

--- chrome/browser/ui/webui/flags_ui.cc
+++ chrome/browser/ui/webui/flags_ui.cc
@@ -38,15 +38,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 
 #if defined(OS_CHROMEOS)
+#include "base/command_line.h"
 #include "base/system/sys_info.h"
 #include "chrome/browser/chromeos/login/session/user_session_manager.h"
 #include "chrome/browser/chromeos/ownership/owner_settings_service_chromeos.h"
 #include "chrome/browser/chromeos/ownership/owner_settings_service_chromeos_factory.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/settings/owner_flags_storage.h"
+#include "chrome/browser/infobars/infobar_service.h"
+#include "chrome/grit/generated_resources.h"
+#include "chromeos/constants/chromeos_switches.h"
 #include "components/account_id/account_id.h"
+#include "components/infobars/core/simple_alert_infobar_delegate.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/user_manager/user_manager.h"
+#include "components/vector_icons/vector_icons.h"
 #endif
 
 using content::WebContents;
@@ -112,6 +118,17 @@ void FinishInitialization(base::WeakPtr<T> flags_ui,
     dom_handler->Init(
         new flags_ui::PrefServiceFlagsStorage(profile->GetPrefs()),
         flags_ui::kGeneralAccessFlagsOnly);
+  }
+
+  // Show a warning info bar when kSafeMode switch is present.
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          chromeos::switches::kSafeMode)) {
+    SimpleAlertInfoBarDelegate::Create(
+        InfoBarService::FromWebContents(flags_ui->web_ui()->GetWebContents()),
+        infobars::InfoBarDelegate::BAD_FLAGS_INFOBAR_DELEGATE,
+        &vector_icons::kWarningIcon,
+        l10n_util::GetStringUTF16(IDS_FLAGS_IGNORED_DUE_TO_CRASHY_CHROME),
+        /*auto_expire=*/false, /*should_animate=*/true);
   }
 }
 #endif
