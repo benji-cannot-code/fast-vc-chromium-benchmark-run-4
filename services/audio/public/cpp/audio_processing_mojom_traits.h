067385ed9cdac640400cdd04dec3c3cac381a481
Ken Rockot <rockot@google.com>
2020-03-19 00:00:01 +0000

    Migrate Audio service typemaps to GN
    Bug: 1059389
    Change-Id: I0bb19abd33c9aa9e88220d106c208871b11976fd
    Tbr: tommi@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108533
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#751530}

--- services/audio/public/cpp/audio_processing_mojom_traits.h
+++ services/audio/public/cpp/audio_processing_mojom_traits.h
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/audio_processing.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
-#include "services/audio/public/mojom/audio_processing.mojom.h"
+#include "services/audio/public/mojom/audio_processing.mojom-shared.h"
+#include "third_party/webrtc/api/media_stream_interface.h"
 
 namespace mojo {
 
