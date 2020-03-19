a038bdeaa30337968b48dfcfb937b78b085a908a
Eriko Kurimoto <elkurin@google.com>
2020-03-19 06:03:39 +0000

    Extensions: Inherit SecurityOrigin in chrome-extension workers
    This CL changes the WorkerGlobalScope's SecurityOrigin for workers
    constructed in chrome-extension.
    
    Previously, workers didn't inherit the origin from their parents but
    create security origin from their url.
        SecurityOrigin::Create(creation_params->script_url);
    Usually, the result of this is the same with inheriting the parent
    origin since workers are same-origin only. However, in chrome-extension,
    it is allowed to construct a cross-origin worker if the script url has
    the permission. In this case, we should inherit the origin with
    'chrome-extention://example/' so that the |request_initiator| matches to
    what the browser can see.
    
    This CL also adds the test for subresource fetch from dedicated workers
    and shared workers constructed in chrome-extension.
    
    This test covers the case that worker's script url is cross-origin to
    chrome-extension but included in permitted urls by chrome-extension.
    The worker should allow same-origin and cross-origin fetch unless the
    script url is not permitted by chrome-extension.
    
    Bug: 1059218
    Change-Id: I498399275d3b4c2c3085e26a8e37145aa74f1d21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2091327
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@google.com>
    Cr-Commit-Position: refs/heads/master@{#751616}

---
+++ chrome/test/data/extensions/api_test/worker/fetch.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+async function test(url) {
+  let allowed = true;
+  try {
+    await fetch(url);
+  } catch (e) {
+    allowed = false;
+  }
+  return allowed;
+};
+
+if ('DedicatedWorkerGlobalScope' in self &&
+    self instanceof DedicatedWorkerGlobalScope) {
+  onmessage = message => {
+    test(message.data)
+      .then(allowed => postMessage(allowed));
+  };
+} else if (
+    'SharedWorkerGlobalScope' in self &&
+    self instanceof SharedWorkerGlobalScope) {
+  onconnect = e => {
+    e.ports[0].onmessage = message => {
+      test(message.data)
+        .then(allowed => e.ports[0].postMessage(allowed));
+    }
+  };
+}
