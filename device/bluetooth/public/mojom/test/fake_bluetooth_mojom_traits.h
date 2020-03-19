744f06a689c049a78bcf6c7462c3540a81c561a2
Ken Rockot <rockot@google.com>
2020-03-19 04:19:10 +0000

    Convert device typemaps to GN
    Bug: 1059389
    Change-Id: I6fa56c1cdd6bfbc92b6954a05da232bcf61ecec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108617
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#751594}

--- device/bluetooth/public/mojom/test/fake_bluetooth_mojom_traits.h
+++ device/bluetooth/public/mojom/test/fake_bluetooth_mojom_traits.h
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "device/bluetooth/bluetooth_device.h"
-#include "device/bluetooth/public/mojom/test/fake_bluetooth.mojom-forward.h"
+#include "device/bluetooth/public/mojom/test/fake_bluetooth.mojom-shared.h"
+#include "mojo/public/cpp/bindings/struct_traits.h"
 
 namespace mojo {
 
--- device/bluetooth/public/mojom/uuid_mojom_traits.h
+++ device/bluetooth/public/mojom/uuid_mojom_traits.h
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "device/bluetooth/public/cpp/bluetooth_uuid.h"
-#include "device/bluetooth/public/mojom/uuid.mojom-forward.h"
+#include "device/bluetooth/public/mojom/uuid.mojom-shared.h"
+#include "mojo/public/cpp/bindings/struct_traits.h"
 
 namespace mojo {
 
