88c5b3ad9ba74300f80a00b32b2a6046ac5d5c83
Maggie Cai <mxcai@chromium.org>
2020-03-19 07:07:58 +0000

    [IntentHandling] Introduce optimized preferred app storage.
    Currently the storage of preferred app takes up too much space. This CL
    introduce an optimized storage data structure. Start with the add and
    find functionality. This CL also ports in the relevant unittests from
    the original data structure to ensure the test still passed. More unit
    tests will be added in follow up CL.
    
    BUG=853604
    
    Change-Id: I88e57033dde98275a638e64496e0fe86a15e880d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108302
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751621}

--- chrome/services/app_service/public/cpp/intent_filter_util.cc
+++ chrome/services/app_service/public/cpp/intent_filter_util.cc
@@ -5,6 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/services/app_service/public/cpp/intent_filter_util.h"
 
+namespace {
+
+bool ConditionsHaveOverlap(const apps::mojom::ConditionPtr& condition1,
+                           const apps::mojom::ConditionPtr& condition2) {
+  if (condition1->condition_type != condition2->condition_type) {
+    return false;
+  }
+  // If there are same |condition_value| exist in the both |condition|s, there
+  // is an overlap.
+  for (auto& value1 : condition1->condition_values) {
+    for (auto& value2 : condition2->condition_values) {
+      if (value1 == value2) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+}  // namespace
+
 namespace apps_util {
 
 apps::mojom::ConditionValuePtr MakeConditionValue(
@@ -70,6 +91,21 @@ int GetFilterMatchLevel(const apps::mojom::IntentFilterPtr& intent_filter) {
     }
   }
   return match_level;
+}
+
+bool FiltersHaveOverlap(const apps::mojom::IntentFilterPtr& filter1,
+                        const apps::mojom::IntentFilterPtr& filter2) {
+  if (GetFilterMatchLevel(filter1) != GetFilterMatchLevel(filter2)) {
+    return false;
+  }
+  for (size_t i = 0; i < filter1->conditions.size(); i++) {
+    auto& condition1 = filter1->conditions[i];
+    auto& condition2 = filter2->conditions[i];
+    if (!ConditionsHaveOverlap(condition1, condition2)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 }  // namespace apps_util
