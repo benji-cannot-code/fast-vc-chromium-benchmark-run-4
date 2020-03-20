6f73518c020cd5f660116f0ad051b28ecb05674d
Alice Wang <aliceywang@chromium.org>
2020-03-20 18:43:00 +0000

    [Android] Migrate ChromeSigninController usage in ChromeBrowserSyncAdapter
    This CL migrates ChromeSigninController to IdentityManager in
    ChromeBrowserSyncAdapter to get primary account.
    Because IdentityManager is only available after the native library is
    loaded, this also requires changing the setup in
    ChromeBrowserSyncAdapterService.
    
    Bug: 1059005
    Change-Id: I4fdd8e4968b6f29f94540bb237ad19b9588e86e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105298
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752113}

--- chrome/android/java/src/org/chromium/chrome/browser/invalidation/ChromeBrowserSyncAdapterService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/invalidation/ChromeBrowserSyncAdapterService.java
@@ -12,7 +12,7 @@ import android.content.Intent;
 import android.os.IBinder;
 
 import org.chromium.base.ContextUtils;
-import org.chromium.chrome.browser.init.ProcessInitializationHandler;
+import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
 
 /**
  * A Service that provides access to {@link ChromeBrowserSyncAdapter}.
@@ -28,7 +28,7 @@ public class ChromeBrowserSyncAdapterService extends Service {
     private ChromeBrowserSyncAdapter getOrCreateSyncAdapter(Context applicationContext) {
         synchronized (LOCK) {
             if (sSyncAdapter == null) {
-                ProcessInitializationHandler.getInstance().initializePreNative();
+                ChromeBrowserInitializer.getInstance().handleSynchronousStartup();
                 sSyncAdapter = new ChromeBrowserSyncAdapter(applicationContext);
             }
         }
