3e496f92786d2a91c9fff726915a60c9f9e67b45
Tricia Crichton <triciac@chromium.org>
2020-03-20 22:22:16 +0000

    [ChromeDriver] improve security considerations message
    Improve the standard message on the need to protect ChromeDriver. The
    current message is sometimes taken by users as an error statement.
    
    Bug: chromedriver:3351
    Change-Id: Ic4d8f7bd4413a5472288133b52c0d61b97665849
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108786
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tricia Crichton <triciac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752207}

--- chrome/test/chromedriver/logging.cc
+++ chrome/test/chromedriver/logging.cc
@@ -35,8 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const char* GetPortProtectionMessage() {
   static std::string kPortProtectionMessage = base::StringPrintf(
-      "Please protect ports used by %s and related test frameworks to "
-      "prevent access by malicious code.",
+      "Please see https://chromedriver.chromium.org/security-considerations "
+      "for suggestions on keeping %s safe.",
       kChromeDriverProductShortName);
   return kPortProtectionMessage.c_str();
 }
