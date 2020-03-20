5a180a1784b6b4ceab9bfb2c91ffcb990275a256
Felipe Erias <felipeerias@gmail.com>
2020-03-20 01:28:17 +0000

    Implement "auto" value for -webkit-appearance
    Add the "auto" value for the -webkit-appearance property.
    The element will have the appearance returned by
    AutoAppearanceFor() (in layout_theme.cc).
    
    Spec: https://drafts.csswg.org/css-ui/#valdef-appearance-auto
    
    Bug: 965432
    Change-Id: Ia3c829637fcfa8a897b6a8d2e155f5861c3d9c0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108264
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Felipe Erias Morandeira <felipeerias@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#751882}

--- third_party/blink/renderer/core/css/css_primitive_value_mappings.h
+++ third_party/blink/renderer/core/css/css_primitive_value_mappings.h
@@ -227,6 +227,9 @@ inline CSSIdentifierValue::CSSIdentifierValue(ControlPart e)
     case kNoControlPart:
       value_id_ = CSSValueID::kNone;
       break;
+    case kAutoPart:
+      value_id_ = CSSValueID::kAuto;
+      break;
     case kCheckboxPart:
       value_id_ = CSSValueID::kCheckbox;
       break;
@@ -306,8 +309,10 @@ template <>
 inline ControlPart CSSIdentifierValue::ConvertTo() const {
   if (value_id_ == CSSValueID::kNone)
     return kNoControlPart;
+  if (value_id_ == CSSValueID::kAuto)
+    return kAutoPart;
   return ControlPart(static_cast<int>(value_id_) -
-                     static_cast<int>(CSSValueID::kCheckbox) + 1);
+                     static_cast<int>(CSSValueID::kCheckbox) + kCheckboxPart);
 }
 
 template <>
--- third_party/blink/renderer/core/layout/layout_theme.cc
+++ third_party/blink/renderer/core/layout/layout_theme.cc
@@ -183,6 +183,7 @@ ControlPart LayoutTheme::AdjustAppearanceWithElementType(
 
     // Aliases of 'auto'.
     // https://drafts.csswg.org/css-ui-4/#typedef-appearance-compat-auto
+    case kAutoPart:
     case kCheckboxPart:
     case kRadioPart:
     case kPushButtonPart:
@@ -259,6 +260,7 @@ void LayoutTheme::AdjustStyle(ComputedStyle& style, Element* e) {
   ControlPart part = AdjustAppearanceWithAuthorStyle(
       AdjustAppearanceWithElementType(style, e), style);
   style.SetEffectiveAppearance(part);
+  DCHECK_NE(part, kAutoPart);
   if (part == kNoControlPart)
     return;
 
