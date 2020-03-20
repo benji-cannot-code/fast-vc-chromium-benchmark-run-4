39ce08c26eabc494e1b56ce7764c80bd303b415d
Brian White <bcwhite@chromium.org>
2020-03-20 14:40:55 +0000

    Remove unused CreateError histograms for shared memory.
    Bug: 1053139
    Change-Id: Ie9cec438ebfcfe99d1068fec9c89a969e958dd1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108801
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752014}

--- base/memory/platform_shared_memory_region_android.cc
+++ base/memory/platform_shared_memory_region_android.cc
@@ -24,11 +24,6 @@ namespace subtle {
 
 namespace {
 
-// Emits UMA metrics about encountered errors.
-void LogCreateError(PlatformSharedMemoryRegion::CreateError error) {
-  UMA_HISTOGRAM_ENUMERATION("SharedMemory.CreateError", error);
-}
-
 int GetAshmemRegionProtectionMask(int fd) {
   int prot = ashmem_get_prot_region(fd);
   if (prot < 0) {
@@ -146,7 +141,6 @@ bool PlatformSharedMemoryRegion::MapAtInternal(off_t offset,
 PlatformSharedMemoryRegion PlatformSharedMemoryRegion::Create(Mode mode,
                                                               size_t size) {
   if (size == 0) {
-    LogCreateError(PlatformSharedMemoryRegion::CreateError::SIZE_ZERO);
     return {};
   }
 
@@ -155,7 +149,6 @@ PlatformSharedMemoryRegion PlatformSharedMemoryRegion::Create(Mode mode,
   size_t rounded_size = bits::Align(size, GetPageSize());
   if (rounded_size < size ||
       rounded_size > static_cast<size_t>(std::numeric_limits<int>::max())) {
-    LogCreateError(PlatformSharedMemoryRegion::CreateError::SIZE_TOO_LARGE);
     return {};
   }
 
@@ -167,8 +160,6 @@ PlatformSharedMemoryRegion PlatformSharedMemoryRegion::Create(Mode mode,
   int fd = ashmem_create_region(
       SharedMemoryTracker::GetDumpNameForTracing(guid).c_str(), rounded_size);
   if (fd < 0) {
-    LogCreateError(
-        PlatformSharedMemoryRegion::CreateError::CREATE_FILE_MAPPING_FAILURE);
     DPLOG(ERROR) << "ashmem_create_region failed";
     return {};
   }
@@ -176,13 +167,10 @@ PlatformSharedMemoryRegion PlatformSharedMemoryRegion::Create(Mode mode,
   ScopedFD scoped_fd(fd);
   int err = ashmem_set_prot_region(scoped_fd.get(), PROT_READ | PROT_WRITE);
   if (err < 0) {
-    LogCreateError(
-        PlatformSharedMemoryRegion::CreateError::REDUCE_PERMISSIONS_FAILURE);
     DPLOG(ERROR) << "ashmem_set_prot_region failed";
     return {};
   }
 
-  LogCreateError(PlatformSharedMemoryRegion::CreateError::SUCCESS);
   return PlatformSharedMemoryRegion(std::move(scoped_fd), mode, size, guid);
 }
 
