9607c918499875e86c06b1f377cc637eb35a3817
Yulun Wu <yulunwu@chromium.org>
2020-03-20 01:02:05 +0000

    Remove unnecessary include.
    Change-Id: I8be1da447a3728bb48bae48a5fa6018595089f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103365
    Commit-Queue: Manu Cornet <manucornet@chromium.org>
    Auto-Submit: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Manu Cornet <manucornet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751872}

--- ash/shelf/drag_handle.cc
+++ ash/shelf/drag_handle.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "base/bind.h"
-#include "base/debug/stack_trace.h"
 #include "base/timer/timer.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
