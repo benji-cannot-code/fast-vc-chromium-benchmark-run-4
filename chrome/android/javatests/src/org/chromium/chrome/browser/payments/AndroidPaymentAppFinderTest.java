015cc28f7c05b7c658e3832beb68f3734561b690
Liquan (Max) Gu <maxlg@chromium.org>
2020-03-20 14:14:40 +0000

    [PlayBilling] WebPaymentIntentHelper: reject invalid inputs
    Context:
    * WebPaymentIntentHelper#createPayIntent
    * WebPaymentIntentHelper#createIsReadyToPayIntent
    
    Change:
    * throws IllegalArgumentException for these inputs
     - null or empty packageName
     - null or empty activityName
     - null or empty id
     - null merchantName
     - null or empty schemelessOrigin
     - null or empty schemelessIframeOrigin
     - invalid certificateChain
     - invalid methodDataMap
     - null total
     - invalid modifiers
    * test all these above
    
    Bug: 1057488
    
    Change-Id: I5affd2503551f3976287d3f83c07a126bc8a933c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103863
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752006}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/AndroidPaymentAppFinderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/AndroidPaymentAppFinderTest.java
@@ -146,6 +146,18 @@ public class AndroidPaymentAppFinderTest
 
     // PaymentAppFactoryParams implementation.
     @Override
+    public String getTopLevelOrigin() {
+        return "https://top.level.origin";
+    }
+
+    // PaymentAppFactoryParams implementation.
+    @Override
+    public String getPaymentRequestOrigin() {
+        return "https://payment.request.origin";
+    }
+
+    // PaymentAppFactoryParams implementation.
+    @Override
     public Map<String, PaymentMethodData> getMethodData() {
         return mMethodData;
     }
