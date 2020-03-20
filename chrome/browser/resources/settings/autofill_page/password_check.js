c7187ed10354f04624aaa29f1ec5168b25f923fa
Vadym Doroshenko <dvadym@chromium.org>
2020-03-20 10:30:37 +0000

    Revert "[Passwords] No result view fix in password check"
    This reverts commit 39bb3cd1fc375f3ec278bcef594f8de5778358dd.
    
    Reason for revert: It looks it breaks test CrSettingsPasswordsCheckTest.All on Windows
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%28dbg%29%281%29
    
    Original change's description:
    > [Passwords] No result view fix in password check
    > 
    > This CL changes the way visibility of 'No result view' is calculated.
    > The label will be shown only if:
    > - user had successful check with 0 compromised passwords found
    > - user is signed in
    > - breach detection is turned on in settings
    > 
    > Bug: 1061496
    > Change-Id: I9f1e4b5f7aef51749b970964c2b701ee2193ba53
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108501
    > Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    > Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#751674}
    
    TBR=jdoerrie@chromium.org,vsemeniuk@google.com
    
    Change-Id: I20479f59c5a3e97c2f8d70a3a96179faff861624
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1061496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111294
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751971}

--- chrome/browser/resources/settings/autofill_page/password_check.js
+++ chrome/browser/resources/settings/autofill_page/password_check.js
@@ -12,7 +12,6 @@ Polymer({
 
   behaviors: [
     I18nBehavior,
-    PrefsBehavior,
     WebUIListenerBehavior,
   ],
 
@@ -81,13 +80,6 @@ Polymer({
      * @private {?PasswordManagerProxy.CompromisedCredential}
      */
     activePassword_: Object,
-
-    /** @private */
-    showNoCompromisedPasswordsLabel_: {
-      type: Boolean,
-      computed:
-          'computeShowNoCompromisedPasswordsLabel(syncStatus_, prefs.*, status_, leakedPasswords)',
-    }
   },
 
   /**
@@ -533,26 +525,5 @@ Polymer({
     this.leakedPasswords = resultList.concat(addedResults);
   },
 
-  /**
-   * @return {boolean}
-   * @private
-   */
-  computeShowNoCompromisedPasswordsLabel() {
-    // Check if user isn't signed in.
-    if (!this.syncStatus_ || !this.syncStatus_.signedIn) {
-      return false;
-    }
-
-    // Check if breach detection is turned off in settings.
-    if (!this.prefs ||
-        !this.getPref('profile.password_manager_leak_detection').value) {
-      return false;
-    }
-
-    // Return true if there was a successful check and no compromised passwords
-    // were found.
-    return !this.hasLeakedCredentials_(this.leakedPasswords) &&
-        this.showsTimestamp_(this.status_);
-  },
 });
 })();
