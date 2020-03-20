c0f8152bc6540e7c9ead6859fcff20064a5a3217
Mikel Astiz <mastiz@chromium.org>
2020-03-20 17:48:01 +0000

    Fix sync Nigori cache GUID left empty upon mismatch
    In the error-handling codepath where a mismatch is found on the cache
    GUID, when sync is starting for Nigori, the new cache GUID should be
    stored in the ModelTypeState. Otherwise, it would remain empty, which
    means the next sync start the mismatch detection doesn't take place.
    
    This can explain cache-GUID mismatches being ignored, and bad
    consequences such as violating the sync protocol by sending a non-empty
    progress marker despite the sync birthday being empty.
    
    Bug: 1063021
    Change-Id: I05c1c000c17b1f10ff37984d1b8b9f812f02a7f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111301
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752085}

--- components/sync/nigori/nigori_model_type_processor_unittest.cc
+++ components/sync/nigori/nigori_model_type_processor_unittest.cc
@@ -528,6 +528,8 @@ TEST_F(NigoriModelTypeProcessorTest, ShouldResetDataOnCacheGuidMismatch) {
   processor()->OnSyncStarting(request, base::DoNothing());
 
   EXPECT_FALSE(processor()->IsTrackingMetadata());
+  EXPECT_EQ(processor()->GetModelTypeStateForTest().cache_guid(),
+            kOtherCacheGuid);
 
   EXPECT_FALSE(ProcessorHasEntity());
 
