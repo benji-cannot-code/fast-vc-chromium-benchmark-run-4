07d6e6d6f9c62cba9125988eede1c5f7ea87fe58
Martin Kreichgauer <martinkr@google.com>
2020-03-20 02:54:32 +0000

    Fill in makeCredential implementation gaps in caBLEv2 authenticator
    When handling makeCredential requests in the caBLEv2 authenticator, pass
    request arguments from the native side where they are unmarshaled over
    to the Java side, so they can be used to construct the GMSCore FIDO API
    request. When receiving a response from the FIDO API, pass response
    parameters back to native code where they are converted into a CTAP
    response. Both sides were previously using hard-coded fake data.
    
    Move CtapMakeCredentialRequest and CtapGetAssertionRequest parsing code
    out of VirtualCtap2Device so that caBLEv2 authenticator native code can
    reuse them. Add a Parse() factory method to AttestationObject because
    the native code needs to parse the CBOR Java response from Java in order
    to convert it into a CTAP message.
    
    Also switch native code to make use of //base/android JNI type
    conversion helpers throughout.
    
    Bug: 1002262
    Change-Id: Ie24adec8c2b8f64e4330aa1fc4b3bf8a273617e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2109037
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751907}

--- chrome/android/features/cablev2_authenticator/internal/java/src/org/chromium/chrome/browser/webauth/authenticator/BLEHandler.java
+++ chrome/android/features/cablev2_authenticator/internal/java/src/org/chromium/chrome/browser/webauth/authenticator/BLEHandler.java
@@ -506,17 +506,21 @@ class BLEHandler extends BluetoothGattServerCallback implements Closeable {
      * Called by native code to process a makeCredential request.
      */
     @CalledByNative
-    void makeCredential(long client) {
-        mAuthenticator.makeCredential(client);
+    void makeCredential(long client, byte[] clientDataHash, String rpId, byte[] userId,
+            int[] algorithms, byte[][] excludedCredentialIds, boolean residentKeyRequired) {
+        mAuthenticator.makeCredential(client, clientDataHash, rpId, userId, algorithms,
+                excludedCredentialIds, residentKeyRequired);
     }
 
     /**
      * Called by CableAuthenticator to notify native code of an attestation response to a
      * makeCredential request.
      */
-    public void onAuthenticatorAttestationResponse(long client, int ctapStatus) {
-        mTaskRunner.postTask(
-                () -> BLEHandlerJni.get().onAuthenticatorAttestationResponse(client, ctapStatus));
+    public void onAuthenticatorAttestationResponse(
+            long client, int ctapStatus, byte[] attestationObject) {
+        mTaskRunner.postTask(()
+                                     -> BLEHandlerJni.get().onAuthenticatorAttestationResponse(
+                                             client, ctapStatus, attestationObject));
     }
 
     @NativeMethods
@@ -545,6 +549,7 @@ class BLEHandler extends BluetoothGattServerCallback implements Closeable {
         /**
          * Called to alert native code of a response to a makeCredential request.
          */
-        void onAuthenticatorAttestationResponse(long client, int ctapStatus);
+        void onAuthenticatorAttestationResponse(
+                long client, int ctapStatus, byte[] attestationObject);
     }
 }
