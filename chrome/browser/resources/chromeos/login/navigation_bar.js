c9f13afa7d5791bf1055aa8161153f7d90d99462
Denis Kuznetsov <antrim@google.com>
2020-03-20 13:25:31 +0000

    [oobe] Remove last bits of i18n-values usage in polymer elements
    Bug: 955194
    Change-Id: I39b6c4c28ce1ee69f078b3875308b6ca486f749e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2110871
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751999}

--- chrome/browser/resources/chromeos/login/navigation_bar.js
+++ chrome/browser/resources/chromeos/login/navigation_bar.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Polymer({
   is: 'navigation-bar',
 
+  behaviors: [OobeI18nBehavior],
+
   properties: {
     backVisible: {type: Boolean, value: false},
     closeVisible: {type: Boolean, value: false},
