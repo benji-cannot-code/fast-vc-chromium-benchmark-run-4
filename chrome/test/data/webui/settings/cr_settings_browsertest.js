37b171320602df363c469c27a5a253d4407fc034
Demetrios Papadopoulos <dpapad@chromium.org>
2020-03-20 12:27:13 +0000

    Fix and re-enable CrSettingsSiteSettingsPageTest.All test.
    It was previously failing because it was asserting on the value of a
    cr-link-row's sublabel before it was actually updated in the prod code.
    
    Introducing a 'site-settings-list-labels-updated-for-testing' event to give a
    singal on the test code on when to assert fixes the issue.
    
    Bug: 1061249
    Change-Id: Ia9c31229da79ac83acb38254ccee053b26f3ec1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111778
    Commit-Queue: Sean Harrison <harrisonsean@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751988}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -1186,7 +1186,7 @@ CrSettingsSiteSettingsPageTest.prototype = {
   ])
 };
 
-TEST_F('CrSettingsSiteSettingsPageTest', 'DISABLED_All', function() {
+TEST_F('CrSettingsSiteSettingsPageTest', 'All', function() {
   mocha.run();
 });
 
