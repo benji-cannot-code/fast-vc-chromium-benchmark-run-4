81841d4978427c71aae3f008eecd14762660a333
Yann Dago <ydago@chromium.org>
2020-03-20 19:51:07 +0000

    Policy doc: Generate app_config.xml for ios
    Bug: 1056800
    Change-Id: I746aa97362b7089115847ae417cf30c2c0eaf5da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105799
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752140}

--- components/policy/tools/template_writers/test_suite_all.py
+++ components/policy/tools/template_writers/test_suite_all.py
@@ -26,6 +26,7 @@ class TestSuiteAll(unittest.TestSuite):
     import writers.doc_writer_unittest
     import writers.google_adml_writer_unittest
     import writers.google_admx_writer_unittest
+    import writers.ios_app_config_writer_unittest
     import writers.json_writer_unittest
     import writers.plist_strings_writer_unittest
     import writers.plist_writer_unittest
@@ -44,6 +45,7 @@ class TestSuiteAll(unittest.TestSuite):
         writers.doc_writer_unittest.DocWriterUnittest,
         writers.google_adml_writer_unittest.GoogleAdmlWriterUnittest,
         writers.google_admx_writer_unittest.GoogleAdmxWriterUnittest,
+        writers.ios_app_config_writer_unittest.IOSAppConfigWriterUnitTests,
         writers.json_writer_unittest.JsonWriterUnittest,
         writers.plist_strings_writer_unittest.PListStringsWriterUnittest,
         writers.plist_writer_unittest.PListWriterUnittest,
