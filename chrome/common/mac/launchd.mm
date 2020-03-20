11c0f61cd03e24e5a32550b0a403cdaff5b5c286
Joshua Pawlicki <waffles@chromium.org>
2020-03-20 19:51:10 +0000

    Check and clean plist status in updater integration test.
    Change DeletePlist to return true if the plist does not exist. Looking
    through the call sites, this seems to be more correct with their
    assumptions.
    
    Bug: 1062288
    Change-Id: I5d3a7cbe77620de93a5e1378862b4786c7a840ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111903
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752141}

--- chrome/common/mac/launchd.mm
+++ chrome/common/mac/launchd.mm
@@ -150,6 +150,16 @@ bool Launchd::WritePlistToFile(Domain domain,
   }
 }
 
+bool Launchd::PlistExists(Domain domain, Type type, CFStringRef name) {
+  @autoreleasepool {
+    NSURL* ns_url = GetPlistURL(domain, type, name);
+    BOOL is_dir = false;
+    return [[NSFileManager defaultManager] fileExistsAtPath:[ns_url path]
+                                                isDirectory:&is_dir] &&
+           !is_dir;
+  }
+}
+
 bool Launchd::DeletePlist(Domain domain, Type type, CFStringRef name) {
   @autoreleasepool {
     NSURL* ns_url = GetPlistURL(domain, type, name);
@@ -158,8 +168,8 @@ bool Launchd::DeletePlist(Domain domain, Type type, CFStringRef name) {
                                                     error:&err]) {
       if ([err code] != NSFileNoSuchFileError) {
         DLOG(ERROR) << "DeletePlist: " << base::mac::NSToCFCast(err);
+        return false;
       }
-      return false;
     }
     return true;
   }
