3d3888916caf08190141637bc1c4bcc11bab1935
Ahmed <afakhry@chromium.org>
2020-03-20 20:53:37 +0000

    Desks: Fix a crash when a highlighted desk_name_view is removed.
    Tabbing again after removing the desk that corresponds to a
    highlighted desk_name_view used to cause a crash.
    
    This CL fixes this crash.
    It also fixes an issue when multiple highlightable views are
    removed from the same call, where the |delete_index_| (which is
    used to determine where to continue traversing after views are
    removed) is updated properly.
    
    BUG=1063324
    TEST=Added a new test.
    
    Change-Id: I77f21dcec16610ca6d2bc06176753f14f86cdc31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2112817
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752171}

--- ash/wm/overview/overview_test_util.cc
+++ ash/wm/overview/overview_test_util.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+// TODO(sammiequon): Consider adding an overload for this function to trigger
+// the key event |count| times.
 void SendKey(ui::KeyboardCode key, int flags) {
   ui::test::EventGenerator generator(Shell::GetPrimaryRootWindow());
   generator.PressKey(key, flags);
