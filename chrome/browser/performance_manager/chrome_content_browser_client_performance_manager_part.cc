561998c198baadddf77eb60fbf99a6a040e25e7b
Patrick Monette <pmonette@chromium.org>
2020-03-20 21:56:08 +0000

    [PM] Migrate users of GetInstance() to IsAvailable().
    Now that all public functions of PerformanceManagerImpl() are static,
    the only remaining uses of GetInstance() was to check if the performance
    manager was initialized.
    
    Bug: 1013127
    Change-Id: Id919af9cb9051bca45b911e5788fea0f83772ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108090
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752193}

--- chrome/browser/performance_manager/chrome_content_browser_client_performance_manager_part.cc
+++ chrome/browser/performance_manager/chrome_content_browser_client_performance_manager_part.cc
@@ -33,7 +33,7 @@ void BindProcessNode(
       performance_manager::RenderProcessUserData::GetForRenderProcessHost(
           render_process_host);
 
-  DCHECK(performance_manager::PerformanceManagerImpl::GetInstance());
+  DCHECK(performance_manager::PerformanceManagerImpl::IsAvailable());
   performance_manager::PerformanceManagerImpl::CallOnGraphImpl(
       FROM_HERE,
       base::BindOnce(
