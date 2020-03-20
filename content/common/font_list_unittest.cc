1b2cd6782b78176fc48beeba472b394250c8a258
Victor Costan <pwnall@chromium.org>
2020-03-20 02:14:16 +0000

    fonts: Test that GetFontList() doesn't include hidden fonts on Mac.
    Bug: 1062925
    Change-Id: I7210f4557595f2a310b7d6db717d7f23b1ea5077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2109178
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#751897}

--- content/common/font_list_unittest.cc
+++ content/common/font_list_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_piece.h"
+#include "base/strings/string_util.h"
 #include "base/task/post_task.h"
 #include "base/task_runner_util.h"
 #include "base/test/task_environment.h"
@@ -76,3 +77,23 @@ TEST(FontList, GetFontListLocalized) {
   EXPECT_TRUE(HasFontWithName(*ko_fonts, "Malgun Gothic", "맑은 고딕"));
 }
 #endif  // defined(OS_WIN)
+
+#if defined(OS_MACOSX)
+// On some macOS versions, CTFontManager returns LastResort and/or hidden fonts.
+// Ensure that someone (CTFontManager or our FontList code) filters these fonts
+// on all OS versions that we support.
+TEST(FontList, GetFontListDoesNotIncludeHiddenFonts) {
+  std::unique_ptr<base::ListValue> fonts = content::GetFontList_SlowBlocking();
+
+  for (const auto& font : fonts->GetList()) {
+    const auto& font_names = font.GetList();
+    const std::string& font_id = font_names[0].GetString();
+
+    // The checks are inspired by Gecko's gfxMacPlatformFontList::AddFamily.
+    EXPECT_FALSE(base::LowerCaseEqualsASCII(font_id, "lastresort"))
+        << font_id << " seems to be LastResort, which should be filtered";
+    EXPECT_FALSE(font_id[0] == '.')
+        << font_id << " seems like a hidden font, which should be filtered";
+  }
+}
+#endif  // defined(OS_MACOSX)
