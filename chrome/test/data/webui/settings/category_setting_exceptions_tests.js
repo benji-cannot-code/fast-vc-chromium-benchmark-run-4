8d0e44feb78a8244a427801cc06b1ce794b4d7e3
rbpotter <rbpotter@chromium.org>
2020-03-21 01:34:04 +0000

    Settings: Migrate category_settings_exceptions to Polymer 3
    Bug: 1026426
    Change-Id: I99b80c8695e2d56d563f29930f59702147896bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2112907
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752261}

--- chrome/test/data/webui/settings/category_setting_exceptions_tests.js
+++ chrome/test/data/webui/settings/category_setting_exceptions_tests.js
@@ -3,6 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import {ContentSetting,ContentSettingProvider,ContentSettingsTypes,SiteSettingsPrefsBrowserProxyImpl,SiteSettingSource} from 'chrome://settings/lazy_load.js';
+// #import {createContentSettingTypeToValuePair,createDefaultContentSetting,createSiteSettingsPrefs} from 'chrome://test/settings/test_util.m.js';
+// #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// #import {TestSiteSettingsPrefsBrowserProxy} from 'chrome://test/settings/test_site_settings_prefs_browser_proxy.m.js';
+// clang-format on
+
 /** @fileoverview Suite of tests for category-setting-exceptions. */
 suite('CategorySettingExceptions', function() {
   /**
@@ -10,6 +17,12 @@ suite('CategorySettingExceptions', function() {
    * @type {SiteSettingsExceptionsElement}
    */
   let testElement;
+
+  /**
+   * The mock proxy object to use during test.
+   * @type {TestSiteSettingsPrefsBrowserProxy}
+   */
+  let browserProxy = null;
 
   // Initialize a category-setting-exceptions before each test.
   setup(function() {
