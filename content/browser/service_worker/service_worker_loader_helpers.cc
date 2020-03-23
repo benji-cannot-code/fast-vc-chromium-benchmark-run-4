3938ae301c39ae191f0da6ae0f9a0209f1c02be7
Kenichi Ishibashi <bashi@chromium.org>
2020-03-23 02:58:55 +0000

    service worker: Separate CreateHttpResponseInfoAndCheckHeaders
    This CL moves header checks from
    CreateHttpResponseInfoAndCheckHeaders() into a separate function.
    This is a preparation to remove HttpResponseInfo dependencies from
    ServiceWorkerNewScriptLoader and
    ServiceWorkerSingleScriptUpdateChecker. These classes will stop
    converting URLResponseHead but still need to check headers.
    
    
    Bug: 1060076
    Change-Id: I1484e2523f42e8e7779b06f0be6cb0119de6101f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2107032
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752362}

--- content/browser/service_worker/service_worker_loader_helpers.cc
+++ content/browser/service_worker/service_worker_loader_helpers.cc
@@ -18,7 +18,7 @@ namespace content {
 
 namespace service_worker_loader_helpers {
 
-std::unique_ptr<net::HttpResponseInfo> CreateHttpResponseInfoAndCheckHeaders(
+bool CheckResponseHead(
     const network::mojom::URLResponseHead& response_head,
     blink::ServiceWorkerStatusCode* out_service_worker_status,
     network::URLLoaderCompletionStatus* out_completion_status,
@@ -31,7 +31,7 @@ std::unique_ptr<net::HttpResponseInfo> CreateHttpResponseInfoAndCheckHeaders(
         ServiceWorkerConsts::kServiceWorkerBadHTTPResponseError,
         response_head.headers->response_code());
     *out_service_worker_status = blink::ServiceWorkerStatusCode::kErrorNetwork;
-    return nullptr;
+    return false;
   }
 
   if (net::IsCertStatusError(response_head.cert_status) &&
@@ -41,7 +41,7 @@ std::unique_ptr<net::HttpResponseInfo> CreateHttpResponseInfoAndCheckHeaders(
         net::MapCertStatusToNetError(response_head.cert_status));
     *out_error_message = ServiceWorkerConsts::kServiceWorkerSSLError;
     *out_service_worker_status = blink::ServiceWorkerStatusCode::kErrorNetwork;
-    return nullptr;
+    return false;
   }
 
   // Remain consistent with logic in
@@ -58,6 +58,19 @@ std::unique_ptr<net::HttpResponseInfo> CreateHttpResponseInfoAndCheckHeaders(
                   ServiceWorkerConsts::kServiceWorkerBadMIMEError,
                   response_head.mime_type.c_str());
     *out_service_worker_status = blink::ServiceWorkerStatusCode::kErrorSecurity;
+    return false;
+  }
+
+  return true;
+}
+
+std::unique_ptr<net::HttpResponseInfo> CreateHttpResponseInfoAndCheckHeaders(
+    const network::mojom::URLResponseHead& response_head,
+    blink::ServiceWorkerStatusCode* out_service_worker_status,
+    network::URLLoaderCompletionStatus* out_completion_status,
+    std::string* out_error_message) {
+  if (!CheckResponseHead(response_head, out_service_worker_status,
+                         out_completion_status, out_error_message)) {
     return nullptr;
   }
 
