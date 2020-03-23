fd22275da05242f9bb0b5de5cb755d6402a5e695
Olivier Robin <olivierrobin@google.com>
2020-03-23 16:13:59 +0000

    Dismiss omnibox on WebState deactivation.
    There is no callback when a webstate will be deactivated.
    Dismiss the omnibox on the old webstate when switching.
    
    Bug: 1059225, 1062628
    Change-Id: I4120b576e4104bc00b9e474369019e37e875347a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2107595
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752454}

--- ios/chrome/browser/ui/content_suggestions/ntp_home_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_mediator.mm
@@ -195,6 +195,11 @@ const char kNTPHelpURL[] =
   [self setContentOffsetForWebState:webState];
 }
 
+- (void)webStateWasHidden:(web::WebState*)webState {
+  DCHECK_EQ(_webState, webState);
+  [self locationBarDidResignFirstResponder];
+}
+
 - (void)webStateDestroyed:(web::WebState*)webState {
   DCHECK_EQ(self.webState, webState);
   self.webState = nullptr;
