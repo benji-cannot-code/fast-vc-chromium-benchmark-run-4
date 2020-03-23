250f5f8ad74dd9e2ac9855c138b453d353092e95
Fredrik Söderqvist <fs@opera.com>
2020-03-23 11:05:55 +0000

    Wrap an element reference in FEImage
    Rather than having FEImage resolve the element reference on every
    operation that may involve it, have the creator of the FEImage (the
    SVGFEImageElement) resolve the reference and pass it to the FEImage.
    
    Bug: 1028063
    Change-Id: Icabe881d980abe995a96dbf54df81da151e0e610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111157
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#752405}

--- third_party/blink/renderer/core/svg/graphics/filters/svg_fe_image.h
+++ third_party/blink/renderer/core/svg/graphics/filters/svg_fe_image.h
@@ -26,22 +26,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_SVG_GRAPHICS_FILTERS_SVG_FE_IMAGE_H_
 
 #include "third_party/blink/renderer/platform/graphics/filters/filter_effect.h"
-#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
 class Image;
 class LayoutObject;
+class SVGElement;
 class SVGPreserveAspectRatio;
-class TreeScope;
 
 class FEImage final : public FilterEffect {
  public:
   FEImage(Filter*, scoped_refptr<Image>, const SVGPreserveAspectRatio*);
-  FEImage(Filter*,
-          const TreeScope&,
-          const String&,
-          const SVGPreserveAspectRatio*);
+  FEImage(Filter*, const SVGElement*, const SVGPreserveAspectRatio*);
 
   // feImage does not perform color interpolation of any kind, so doesn't
   // depend on the value of color-interpolation-filters.
@@ -66,9 +62,7 @@ class FEImage final : public FilterEffect {
   sk_sp<PaintFilter> CreateImageFilterForLayoutObject(const LayoutObject&);
 
   scoped_refptr<Image> image_;
-
-  Member<const TreeScope> tree_scope_;
-  String href_;
+  Member<const SVGElement> element_;
   Member<const SVGPreserveAspectRatio> preserve_aspect_ratio_;
 };
 
