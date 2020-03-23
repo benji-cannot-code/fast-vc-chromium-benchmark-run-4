04acdd545f17b4c70bc96cc22cd5cd7ffe5b53cf
Joshua Pawlicki <waffles@chromium.org>
2020-03-23 17:45:15 +0000

    Create an updater-specific signing pipeline.
    Bug: 926234
    Change-Id: I453092f47dd059a48ed1579bcb63d700739909eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042217
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752492}

---
+++ chrome/updater/mac/signing/unbranded_config.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from .build_props_config import BuildPropsCodeSignConfig
+
+
+class UnbrandedCodeSignConfig(BuildPropsCodeSignConfig):
+    """A CodeSignConfig used for signing non-official Updater builds."""
+
+    @property
+    def packaging_dir(self):
+        return 'Updater Packaging'
+
+    @property
+    def provisioning_profile_basename(self):
+        return None
