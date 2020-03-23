65254398ff40ecfee6429e2eba5babf33d792822
Andrey Kosyakov <caseq@chromium.org>
2020-03-23 22:13:44 +0000

    Animations: fix animationiteration event to be fired just once per animation
    ... originally regressed by
    https://chromium-review.googlesource.com/c/chromium/src/+/2107302
    
    Bug: 1063506, 1059968
    Change-Id: Id7fd285c9e5cf9a70981e34c4645f778fbf2899f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2113801
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752588}

--- third_party/blink/renderer/core/animation/css/css_animations.cc
+++ third_party/blink/renderer/core/animation/css/css_animations.cc
@@ -1192,8 +1192,11 @@ void CSSAnimations::AnimationEventDelegate::OnEventCondition(
                   event_type_names::kAnimationiteration, elapsed_time);
   }
 
+  previous_iteration_ = current_iteration;
+
   if (previous_phase_ == current_phase)
     return;
+  previous_phase_ = current_phase;
 
   if (current_phase == Timing::kPhaseAfter) {
     MaybeDispatch(Document::kAnimationEndListener,
@@ -1210,9 +1213,6 @@ void CSSAnimations::AnimationEventDelegate::OnEventCondition(
                   event_type_names::kAnimationcancel,
                   AnimationTimeDelta::FromSecondsD(cancel_time));
   }
-
-  previous_phase_ = current_phase;
-  previous_iteration_ = current_iteration;
 }
 
 void CSSAnimations::AnimationEventDelegate::Trace(Visitor* visitor) {
