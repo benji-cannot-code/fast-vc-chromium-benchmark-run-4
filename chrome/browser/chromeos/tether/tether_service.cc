c48381c12a09db20d13adc6919a0a9387535c6b4
Nico Weber <thakis@chromium.org>
2020-03-23 19:34:51 +0000

    cros: Delete some dead code.
    This code has been dead since
    https://chromium-review.googlesource.com/c/chromium/src/+/1372464
    
    Bug: 912743,346399
    Change-Id: I92e690ff6dae9d89ea0fa96b8e8fee8ff4233e2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2115761
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752533}

--- chrome/browser/chromeos/tether/tether_service.cc
+++ chrome/browser/chromeos/tether/tether_service.cc
@@ -622,8 +622,6 @@ TetherService::TetherFeatureState TetherService::GetTetherFeatureState() {
   if (!IsBluetoothPowered())
     return BLUETOOTH_DISABLED;
 
-  // For the cases below, the state is computed differently depending on whether
-  // the MultiDeviceSetup service is active.
   chromeos::multidevice_setup::mojom::FeatureState tether_multidevice_state =
       multidevice_setup_client_->GetFeatureState(
           chromeos::multidevice_setup::mojom::Feature::kInstantTethering);
@@ -661,14 +659,6 @@ TetherService::TetherFeatureState TetherService::GetTetherFeatureState() {
       NOTREACHED();
       return NO_AVAILABLE_HOSTS;
   }
-
-  if (!IsAllowedByPolicy())
-    return PROHIBITED;
-
-  if (!IsEnabledByPreference())
-    return USER_PREFERENCE_DISABLED;
-
-  return ENABLED;
 }
 
 void TetherService::RecordTetherFeatureState() {
