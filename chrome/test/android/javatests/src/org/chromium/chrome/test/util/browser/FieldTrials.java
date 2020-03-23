cceaaa7da59dda26cd4d2aa25753ae3f9911ec8d
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-03-23 20:39:12 +0000

    Force cached flags even when there's no parameters specified
    In instrumented tests, annotations like
    @CommandLineFlags.Add("enable-features=FeatureA") can propagate to
    CachedFeatureFlags.isEnabled("FeatureA"), but "FeatureA<Trial" doesn't.
    
    This CL fixes that.
    
    Bug: 1056327
    Change-Id: I672743ffddd0604bc655def440e8d7c047bf5b65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2113913
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752556}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/FieldTrials.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/FieldTrials.java
@@ -23,8 +23,9 @@ import java.util.Set;
  */
 public class FieldTrials {
     private static FieldTrials sInstance;
-    private Map<String, Map<String, String>> mTrialToParamValueMap = new HashMap<>();
-    private Map<String, Set<String>> mTrialToFeatureNameMap = new HashMap<>();
+    private final Set<String> mEnabledFeatures = new HashSet<>();
+    private final Map<String, Map<String, String>> mTrialToParamValueMap = new HashMap<>();
+    private final Map<String, Set<String>> mTrialToFeatureNameMap = new HashMap<>();
     private static final String TAG = "FieldTrials";
 
     private FieldTrials() {}
@@ -45,12 +46,18 @@ public class FieldTrials {
                 commandLine.getSwitchValue(BaseSwitches.FORCE_FIELD_TRIAL_PARAMS_SWITCH);
         String enableFeatures = commandLine.getSwitchValue(BaseSwitches.ENABLE_FEATURES);
 
+        Set<String> enableFeaturesSet = new HashSet<>();
+        if (enableFeatures != null) {
+            Collections.addAll(enableFeaturesSet, enableFeatures.split(","));
+            for (String enabledFeature : enableFeaturesSet) {
+                mEnabledFeatures.add(cleanupFeatureName(enabledFeature));
+            }
+        }
+
         if (forceFieldTrials == null || forceFieldTrialParams == null || enableFeatures == null) {
             return;
         }
 
-        Set<String> enableFeaturesSet = new HashSet<>();
-        Collections.addAll(enableFeaturesSet, enableFeatures.split(","));
         try {
             updateTrialToParamValueMap(forceFieldTrialParams.split(","));
             updateTrialFeatureMap(forceFieldTrials.split("/"), enableFeaturesSet);
@@ -62,6 +69,11 @@ public class FieldTrials {
                             + forceFieldTrialParams + ".");
             return;
         }
+    }
+
+    private static String cleanupFeatureName(String featureNameWithTrial) {
+        if (!featureNameWithTrial.contains("<")) return featureNameWithTrial;
+        return featureNameWithTrial.split("<")[0];
     }
 
     /**
@@ -134,8 +146,8 @@ public class FieldTrials {
      * in CachedFeatureFlags.
      */
     public void applyFieldTrials() {
-        if (mTrialToFeatureNameMap.isEmpty() || mTrialToParamValueMap.isEmpty()) {
-            return;
+        for (String featureName : mEnabledFeatures) {
+            CachedFeatureFlags.setForTesting(featureName, true);
         }
 
         for (Map.Entry<String, Map<String, String>> entry : mTrialToParamValueMap.entrySet()) {
@@ -144,7 +156,6 @@ public class FieldTrials {
             for (String featureName : featureSet) {
                 Map<String, String> params = entry.getValue();
                 for (Map.Entry<String, String> param : params.entrySet()) {
-                    CachedFeatureFlags.setForTesting(featureName, true);
                     CachedFieldTrialParameter.setForTesting(
                             featureName, param.getKey(), param.getValue());
                 }
