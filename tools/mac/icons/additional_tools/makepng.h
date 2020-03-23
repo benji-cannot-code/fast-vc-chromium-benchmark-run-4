17b5e795c0ca9842a0a745417c95f85a7a66fad1
Avi Drissman <avi@chromium.org>
2020-03-23 17:14:29 +0000

    Add Mac icon building tools
    This documents the Chromium standards for .icns files, and commits the
    tools to use to create them. (For reference, check out
    https://crbug.com/576173 wherein these restrictions were discovered.)
    
    Note also that many of these tools are from https://crbug.com/846451#c10
    which Mark used to create our current set of icons.
    
    Bug: 846451
    Change-Id: I0030e9f883a1c068ef425ee0139b14da112e97d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2101316
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752480}

---
+++ tools/mac/icons/additional_tools/makepng.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MAKEPNG_H_
+#define MAKEPNG_H_
+
+#include <stdint.h>
+
+bool EncodePNG(const char* input_image_path,
+               const char* input_mask_path,
+               const char* output_path,
+               size_t dimension);
+
+#endif  // MAKEPNG_H_
---
+++ tools/mac/icons/additional_tools/unpackicon.h
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UNPACKICON_H_
+#define UNPACKICON_H_
+
+bool UnpackIcon(const char* input_path, const char* output_path, bool skip);
+
+#endif  // UNPACKICON_H_
