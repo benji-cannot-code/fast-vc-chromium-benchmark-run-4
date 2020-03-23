ddcccec9794bd5df64cc800c8a2f9bbc4604f7c9
ckitagawa <ckitagawa@chromium.org>
2020-03-23 14:55:11 +0000

    [Paint Preview] TabbedPaintPreviewPlayer
    TabbedPaintPreviewPlayer glues PlayerManager to PaintPreviewTabService
    to create a class that manages per-tab display of PaintPreviews.
    
    This results in some cascading refactorings:
    - PaintPreviewTabService now caches a list of captures such that
      HasCaptureForTab() synchronous for quick checks.
    - URL is passed to PlayerManager on initialization and propagated to
      ensure that the proto URL matches the URL currently shown in the Tab.
      As a follow-up it may be worth making this optional?
    - Propagate some failures to the Java layer.
    
    Bug: 1062383
    Change-Id: Iafb5913b00732c1078a6a492e828988cbbca6324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106118
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752433}

--- components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/PlayerCompositorDelegateImpl.java
+++ components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/PlayerCompositorDelegateImpl.java
@@ -15,6 +15,7 @@ import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.components.paintpreview.browser.NativePaintPreviewServiceProvider;
+import org.chromium.url.GURL;
 
 import javax.annotation.Nonnull;
 
@@ -25,20 +26,20 @@ import javax.annotation.Nonnull;
 @JNINamespace("paint_preview")
 class PlayerCompositorDelegateImpl implements PlayerCompositorDelegate {
     interface CompositorListener {
-        void onCompositorReady(UnguessableToken rootFrameGuid, UnguessableToken[] frameGuids,
-                int[] frameContentSize, int[] subFramesCount, UnguessableToken[] subFrameGuids,
-                int[] subFrameClipRects);
+        void onCompositorReady(boolean safeToShow, UnguessableToken rootFrameGuid,
+                UnguessableToken[] frameGuids, int[] frameContentSize, int[] subFramesCount,
+                UnguessableToken[] subFrameGuids, int[] subFrameClipRects);
     }
 
     private CompositorListener mCompositorListener;
     private long mNativePlayerCompositorDelegate;
 
-    PlayerCompositorDelegateImpl(NativePaintPreviewServiceProvider service, String directoryKey,
-            @Nonnull CompositorListener compositorListener) {
+    PlayerCompositorDelegateImpl(NativePaintPreviewServiceProvider service, GURL url,
+            String directoryKey, @Nonnull CompositorListener compositorListener) {
         mCompositorListener = compositorListener;
         if (service != null && service.getNativeService() != 0) {
             mNativePlayerCompositorDelegate = PlayerCompositorDelegateImplJni.get().initialize(
-                    this, service.getNativeService(), directoryKey);
+                    this, service.getNativeService(), url.getSpec(), directoryKey);
         }
         // TODO(crbug.com/1021590): Handle initialization errors when
         // mNativePlayerCompositorDelegate == 0.
@@ -47,6 +48,8 @@ class PlayerCompositorDelegateImpl implements PlayerCompositorDelegate {
     /**
      * Called by native when the Paint Preview compositor is ready.
      *
+     * @param safeToShow true if the native initialization of the Paint Preview player succeeded and
+     * the captured result matched the expected URL.
      * @param rootFrameGuid The GUID for the root frame.
      * @param frameGuids Contains all frame GUIDs that are in this hierarchy.
      * @param frameContentSize Contains the content size for each frame. In native, this is called
@@ -69,11 +72,11 @@ class PlayerCompositorDelegateImpl implements PlayerCompositorDelegate {
      * subFrameGuids[4*k+3]}, where {@code k} has the same value as above.
      */
     @CalledByNative
-    void onCompositorReady(UnguessableToken rootFrameGuid, UnguessableToken[] frameGuids,
-            int[] frameContentSize, int[] subFramesCount, UnguessableToken[] subFrameGuids,
-            int[] subFrameClipRects) {
-        mCompositorListener.onCompositorReady(rootFrameGuid, frameGuids, frameContentSize,
-                subFramesCount, subFrameGuids, subFrameClipRects);
+    void onCompositorReady(boolean safeToShow, UnguessableToken rootFrameGuid,
+            UnguessableToken[] frameGuids, int[] frameContentSize, int[] subFramesCount,
+            UnguessableToken[] subFrameGuids, int[] subFrameClipRects) {
+        mCompositorListener.onCompositorReady(safeToShow, rootFrameGuid, frameGuids,
+                frameContentSize, subFramesCount, subFrameGuids, subFrameClipRects);
     }
 
     @Override
@@ -115,7 +118,7 @@ class PlayerCompositorDelegateImpl implements PlayerCompositorDelegate {
     @NativeMethods
     interface Natives {
         long initialize(PlayerCompositorDelegateImpl caller, long nativePaintPreviewBaseService,
-                String directoryKey);
+                String urlSpec, String directoryKey);
         void destroy(long nativePlayerCompositorDelegateAndroid);
         void requestBitmap(long nativePlayerCompositorDelegateAndroid, UnguessableToken frameGuid,
                 Callback<Bitmap> bitmapCallback, Runnable errorCallback, float scaleFactor,
