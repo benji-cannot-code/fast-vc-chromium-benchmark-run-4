38ff1e562cf3aaec2ce46ee30a2ff29ead7ba6db
Nico Weber <thakis@chromium.org>
2020-03-23 22:48:33 +0000

    Enable -Wunreachable-code in (non-arm) Chrome OS builds.
    Bug: 1063529,346399
    Change-Id: I313ebf4cc16885d1f9bdc5a2ed8db524467735f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116590
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752618}

--- ash/display/display_prefs_unittest.cc
+++ ash/display/display_prefs_unittest.cc
@@ -51,12 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 namespace {
-const char kPrimaryIdKey[] = "primary-id";
-const char kPositionKey[] = "position";
-const char kOffsetKey[] = "offset";
-const char kPlacementDisplayIdKey[] = "placement.display_id";
-const char kPlacementParentDisplayIdKey[] = "placement.parent_display_id";
-
 bool IsRotationLocked() {
   return ash::Shell::Get()->screen_orientation_controller()->rotation_locked();
 }
@@ -515,8 +509,13 @@ TEST_F(DisplayPrefsTest, BasicStores) {
   EXPECT_EQ(id2, stored_placement.parent_display_id);
   EXPECT_EQ(id2, stored_layout.primary_id);
 
-  if (true)
-    return;
+  // TODO(oshima): Make the below pass and re-enable, https://crbug.com/1063529
+#if 0
+  const char kPrimaryIdKey[] = "primary-id";
+  const char kPositionKey[] = "position";
+  const char kOffsetKey[] = "offset";
+  const char kPlacementDisplayIdKey[] = "placement.display_id";
+  const char kPlacementParentDisplayIdKey[] = "placement.parent_display_id";
 
   std::string primary_id_str;
   EXPECT_TRUE(layout_value->GetString(kPrimaryIdKey, &primary_id_str));
@@ -610,6 +609,7 @@ TEST_F(DisplayPrefsTest, BasicStores) {
   EXPECT_TRUE(property->GetInteger("height", &height));
   EXPECT_EQ(500, width);
   EXPECT_EQ(400, height);
+#endif
 }
 
 TEST_F(DisplayPrefsTest, PreventStore) {
