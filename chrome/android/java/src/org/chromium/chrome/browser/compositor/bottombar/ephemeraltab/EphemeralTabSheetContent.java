15fb137c11014a58751198ca9894bd672428b954
Clark DuVall <cduvall@chromium.org>
2020-03-23 22:05:30 +0000

    Drop permissions requests in preview tab
    This change allows the preview tab to tell PermissionRequestManager to
    drop any permission requests it receives for the associated WebContents.
    
    TBR=andypaicu@chromium.org
    
    Bug: 1057951
    Change-Id: I65ee35fbaf2081cc99d8384a5b0818fc9f026ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2112812
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752573}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabSheetContent.java
@@ -26,7 +26,6 @@ import org.chromium.chrome.browser.thinwebview.ThinWebViewConstraints;
 import org.chromium.chrome.browser.thinwebview.ThinWebViewFactory;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetContent;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetController;
-import org.chromium.components.browser_ui.modaldialog.AppModalPresenter;
 import org.chromium.components.browser_ui.widget.FadingShadow;
 import org.chromium.components.browser_ui.widget.FadingShadowView;
 import org.chromium.components.embedder_support.delegate.WebContentsDelegateAndroid;
@@ -36,7 +35,6 @@ import org.chromium.components.url_formatter.UrlFormatter;
 import org.chromium.content_public.browser.RenderCoordinates;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.ActivityWindowAndroid;
-import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.url.GURL;
 
 /**
@@ -60,7 +58,6 @@ public class EphemeralTabSheetContent implements BottomSheetContent {
     private FadingShadowView mShadow;
     private Drawable mCurrentFavicon;
     private ImageView mFaviconView;
-    private ModalDialogManager mModalDialogManager;
 
     /**
      * Constructor.
@@ -104,12 +101,8 @@ public class EphemeralTabSheetContent implements BottomSheetContent {
      * bottom sheet.
      */
     private void createThinWebView(int maxSheetHeight) {
-        mThinWebView = ThinWebViewFactory.create(mContext, new ActivityWindowAndroid(mContext) {
-            @Override
-            public @Nullable ModalDialogManager getModalDialogManager() {
-                return EphemeralTabSheetContent.this.getModalDialogManager();
-            }
-        }, new ThinWebViewConstraints());
+        mThinWebView = ThinWebViewFactory.create(
+                mContext, new ActivityWindowAndroid(mContext), new ThinWebViewConstraints());
 
         mSheetContentView = new FrameLayout(mContext);
         mThinWebView.getView().setLayoutParams(new FrameLayout.LayoutParams(
@@ -117,14 +110,6 @@ public class EphemeralTabSheetContent implements BottomSheetContent {
         mSheetContentView.addView(mThinWebView.getView());
 
         mSheetContentView.setPadding(0, mToolbarHeightPx, 0, 0);
-    }
-
-    private ModalDialogManager getModalDialogManager() {
-        if (mModalDialogManager == null) {
-            mModalDialogManager = new ModalDialogManager(
-                    new AppModalPresenter(mContext), ModalDialogManager.ModalDialogType.APP);
-        }
-        return mModalDialogManager;
     }
 
     private void createToolbarView() {
@@ -248,10 +233,6 @@ public class EphemeralTabSheetContent implements BottomSheetContent {
     @Override
     public void destroy() {
         mThinWebView.destroy();
-        if (mModalDialogManager != null) {
-            mModalDialogManager.destroy();
-            mModalDialogManager = null;
-        }
     }
 
     @Override
