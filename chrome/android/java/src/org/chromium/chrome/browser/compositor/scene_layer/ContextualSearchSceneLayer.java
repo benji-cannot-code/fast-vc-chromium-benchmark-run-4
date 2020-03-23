2361d2836f7aababebc89594d8bcae7bf1b5098d
Side Yilmaz <sideyilmaz@chromium.org>
2020-03-23 09:17:06 +0000

    Replace deprecated getLastUsedProfile function in ContextualSearchSceneLayer.
    Profile#getLastUsedProfile| is deprecated and replaced with
    |Profile#getLastUsedRegularProfile|. This CL updates
    ContextualSearchSceneLayer class.
    
    Note: This change is only code clean-up, does not change any behaviour.
    
    Bug: 1041781
    Change-Id: Ic8946777cbc4d6d9d585b89f67578dc6ff930719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2107861
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752394}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/ContextualSearchSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/ContextualSearchSceneLayer.java
@@ -178,7 +178,7 @@ public class ContextualSearchSceneLayer extends SceneOverlayLayer {
                 progressBarHeight * mDpToPx, progressBarOpacity, progressBarCompletion,
                 dividerLineVisibilityPercentage, dividerLineWidth, dividerLineHeight,
                 dividerLineColor, dividerLineXOffset, touchHighlightVisible, touchHighlightXOffset,
-                touchHighlightWidth, Profile.getLastUsedProfile(), roundedBarTopResourceId,
+                touchHighlightWidth, Profile.getLastUsedRegularProfile(), roundedBarTopResourceId,
                 separatorLineColor);
     }
 
