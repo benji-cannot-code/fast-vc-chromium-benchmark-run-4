20422c2faaad779f0c1b010add76a3c43afa291e
Eric Willigers <ericwilligers@chromium.org>
2020-03-23 02:05:05 +0000

    WebApps: Forward declare Profile class
    web_app_file_handler_registration.h now uses a forward declaration
    for Profile instead of including a header.
    
    Change-Id: I7f0175ec47d16f015538ac2981c0e6e89242eeb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2114310
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752355}

--- chrome/browser/web_applications/components/web_app_file_handler_registration_win_unittest.cc
+++ chrome/browser/web_applications/components/web_app_file_handler_registration_win_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_reg_util_win.h"
 #include "base/test/test_timeouts.h"
 #include "base/win/windows_version.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/web_applications/chrome_pwa_launcher/chrome_pwa_launcher_util.h"
