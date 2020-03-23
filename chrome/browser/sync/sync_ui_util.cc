6cf74c7f0e840bb983dd393707dc6dec0ff6c9a4
Mikel Astiz <mastiz@chromium.org>
2020-03-23 22:12:03 +0000

    sync: Allow closing tab when trusted vault key retrieval completes
    When the user goes through a reauth flow to retrieve trusted vault keys,
    the page should be closed upon completion.
    
    In order to achieve this, NavigateParams.created_with_opener is set to
    true, which allows Javascript on the page to close itself. In addition,
    the reuse of the NTP is disallowed because, in many relevant scenarios
    such as FRE, the NTP is the only existing tab and cannot be closed.
    
    As a fallback mechanism, a URL parameter is added to the reauth URL, in
    case the page decides to redirect instead of closing (or if closing the
    tab was rejected).
    
    Change-Id: I124ddd299c47a7c5705b410829a38130ea78e208
    Bug: 1062977
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108731
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752585}

--- chrome/browser/sync/sync_ui_util.cc
+++ chrome/browser/sync/sync_ui_util.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/search_engines/ui_thread_search_terms_data.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/driver/sync_user_settings.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "google_apis/gaia/google_service_auth_error.h"
+#include "net/base/url_util.h"
 
 #if defined(OS_CHROMEOS)
 #include "chromeos/constants/chromeos_features.h"
@@ -178,8 +180,9 @@ void OpenTabForSyncKeyRetrievalWithURL(Browser* browser, const GURL& url) {
   FocusWebContents(browser);
 
   NavigateParams params(GetSingletonTabNavigateParams(browser, url));
-  params.path_behavior = NavigateParams::IGNORE_AND_NAVIGATE;
-  ShowSingletonTabOverwritingNTP(browser, std::move(params));
+  // Allow the window to close itself.
+  params.created_with_opener = true;
+  Navigate(&params);
 }
 
 // Returns true if the user has consented to browser sync-the-feature or
@@ -335,8 +338,14 @@ bool ShouldShowSyncKeysMissingError(const syncer::SyncService* service) {
 }
 
 void OpenTabForSyncKeyRetrieval(Browser* browser) {
-  OpenTabForSyncKeyRetrievalWithURL(
-      browser, GaiaUrls::GetInstance()->signin_chrome_sync_keys_url());
+  const GURL continue_url =
+      GURL(UIThreadSearchTermsData().GoogleBaseURLValue());
+  GURL retrieval_url = GaiaUrls::GetInstance()->signin_chrome_sync_keys_url();
+  if (continue_url.is_valid()) {
+    retrieval_url = net::AppendQueryParameter(retrieval_url, "continue",
+                                              continue_url.spec());
+  }
+  OpenTabForSyncKeyRetrievalWithURL(browser, retrieval_url);
 }
 
 void OpenTabForSyncKeyRetrievalWithURLForTesting(Browser* browser,
