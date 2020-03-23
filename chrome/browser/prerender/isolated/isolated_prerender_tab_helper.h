6620c7328017ee6b25c6448d8e975b57adbec240
Robert Ogden <robertogden@chromium.org>
2020-03-23 18:09:56 +0000

    Use the CONNECT proxy for Isolated Prerender prefetches
    Uses a separate Network Context (only 1 on SRP page loads) with the
    proxy configured to fetch all predicted resources.
    
    Testing for this is a bit limited at the moment. Currently, there is not
    a CONNECT proxy for testing, so only the tunnel request can be verified.
    Working on a testing proxy in a separate cl/bug.
    
    Bug: 1042829
    Change-Id: I9e7a325405ac9660db9afe37ef34efff5c56bed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108534
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752502}

--- chrome/browser/prerender/isolated/isolated_prerender_tab_helper.h
+++ chrome/browser/prerender/isolated/isolated_prerender_tab_helper.h
@@ -19,7 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prerender/isolated/prefetched_mainframe_response_container.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/mojom/network_context.mojom.h"
+#include "services/network/public/mojom/url_response_head.mojom-forward.h"
 #include "url/gurl.h"
 
 class Profile;
@@ -41,8 +44,6 @@ class IsolatedPrerenderTabHelper
  public:
   ~IsolatedPrerenderTabHelper() override;
 
-  void SetURLLoaderFactoryForTesting(
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
   size_t prefetched_responses_size_for_testing() const {
     return page_ ? page_->prefetched_responses.size() : 0;
   }
@@ -58,8 +59,12 @@ class IsolatedPrerenderTabHelper
   std::unique_ptr<PrefetchedMainframeResponseContainer> TakePrefetchResponse(
       const GURL& url);
 
- private:
+ protected:
+  // Exposed for testing.
   explicit IsolatedPrerenderTabHelper(content::WebContents* web_contents);
+  virtual network::mojom::URLLoaderFactory* GetURLLoaderFactory();
+
+ private:
   friend class content::WebContentsUserData<IsolatedPrerenderTabHelper>;
 
   // Owns all per-pageload state in the tab helper so that new navigations only
@@ -82,6 +87,12 @@ class IsolatedPrerenderTabHelper
     // navigation commits.
     std::map<GURL, std::unique_ptr<PrefetchedMainframeResponseContainer>>
         prefetched_responses;
+
+    // The network context and url loader factory that will be used for
+    // prefetches. A separate network context is used so that the prefetch proxy
+    // can be used via a custom porxy configuration.
+    mojo::Remote<network::mojom::URLLoaderFactory> isolated_url_loader_factory;
+    mojo::Remote<network::mojom::NetworkContext> isolated_network_context;
   };
 
   // A helper method to make it easier to tell when prefetching is already
@@ -126,9 +137,10 @@ class IsolatedPrerenderTabHelper
                        const net::CookieStatusList& cookie_with_status_list,
                        const net::CookieStatusList& excluded_cookies);
 
-  Profile* profile_;
+  // Creates the isolated network context and url loader factory for this page.
+  void CreateIsolatedURLLoaderFactory();
 
-  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
+  Profile* profile_;
 
   // Owns all members which need to be reset on a new page load.
   std::unique_ptr<CurrentPageLoad> page_;
