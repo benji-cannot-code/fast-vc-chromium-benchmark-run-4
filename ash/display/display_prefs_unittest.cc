f9aacba4c153b9e275a61ed72d93b45c7a33e418
calamity <calamity@chromium.org>
2020-03-24 04:07:43 +0000

    Revert "Enable -Wunreachable-code in (non-arm) Chrome OS builds."
    This reverts commit 2ff5fdbc7071dbdd8bca67f3cb72cdcb636e543b.
    
    Reason for revert: Causing compile failures on linux-chromeos-chrome
    https://ci.chromium.org/p/chrome/builders/ci/linux-chromeos-chrome/4233
    
    Original change's description:
    > Enable -Wunreachable-code in (non-arm) Chrome OS builds.
    > 
    > Bug: 1063529,346399
    > Change-Id: I313ebf4cc16885d1f9bdc5a2ed8db524467735f2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116590
    > Commit-Queue: Ben Pastene <bpastene@chromium.org>
    > Reviewed-by: Ben Pastene <bpastene@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Auto-Submit: Nico Weber <thakis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#752618}
    
    TBR=oshima@chromium.org,thakis@chromium.org,bpastene@chromium.org
    
    Change-Id: I01a4f609e3936fa98da9127d7f9858a4a61e2ecd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1063529, 346399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2115865
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752723}

--- ash/display/display_prefs_unittest.cc
+++ ash/display/display_prefs_unittest.cc
@@ -51,6 +51,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 namespace {
+const char kPrimaryIdKey[] = "primary-id";
+const char kPositionKey[] = "position";
+const char kOffsetKey[] = "offset";
+const char kPlacementDisplayIdKey[] = "placement.display_id";
+const char kPlacementParentDisplayIdKey[] = "placement.parent_display_id";
+
 bool IsRotationLocked() {
   return ash::Shell::Get()->screen_orientation_controller()->rotation_locked();
 }
@@ -509,13 +515,8 @@ TEST_F(DisplayPrefsTest, BasicStores) {
   EXPECT_EQ(id2, stored_placement.parent_display_id);
   EXPECT_EQ(id2, stored_layout.primary_id);
 
-  // TODO(oshima): Make the below pass and re-enable, https://crbug.com/1063529
-#if 0
-  const char kPrimaryIdKey[] = "primary-id";
-  const char kPositionKey[] = "position";
-  const char kOffsetKey[] = "offset";
-  const char kPlacementDisplayIdKey[] = "placement.display_id";
-  const char kPlacementParentDisplayIdKey[] = "placement.parent_display_id";
+  if (true)
+    return;
 
   std::string primary_id_str;
   EXPECT_TRUE(layout_value->GetString(kPrimaryIdKey, &primary_id_str));
@@ -609,7 +610,6 @@ TEST_F(DisplayPrefsTest, BasicStores) {
   EXPECT_TRUE(property->GetInteger("height", &height));
   EXPECT_EQ(500, width);
   EXPECT_EQ(400, height);
-#endif
 }
 
 TEST_F(DisplayPrefsTest, PreventStore) {
