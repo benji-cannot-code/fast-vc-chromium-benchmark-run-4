8139d43fa90ee7110834f020d59050837a6a27c8
Omer Katz <omerkatz@chromium.org>
2020-03-24 17:52:25 +0000

    heap: Disable concurrent marking tests on non-TSAN builds
    Bug: 986235
    Change-Id: I25fb83b0ec4cd9528c3f9316823e680cb13973d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117736
    Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752911}

--- third_party/blink/renderer/platform/heap/concurrent_marking_test.cc
+++ third_party/blink/renderer/platform/heap/concurrent_marking_test.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#if defined(THREAD_SANITIZER)
+
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/bindings/script_forbidden_scope.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
@@ -482,3 +484,5 @@ TEST_F(ConcurrentMarkingTest, PopFromDeque) {
 
 }  // namespace concurrent_marking_test
 }  // namespace blink
+
+#endif  // defined(THREAD_SANITIZER)
