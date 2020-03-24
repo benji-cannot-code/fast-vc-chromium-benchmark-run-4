1e6a8b67b9692e61a6406d7d8a96980dfb788d33
Xing Liu <xingliu@chromium.org>
2020-03-24 21:12:15 +0000

    Upboarding: Add tile service factory helper.
    This CL adds the dependencies injection entry point for query tile
    service.
    
    Bug: NONE
    Change-Id: Id6f99724fe7061961ddaf534f36f6abe21cbe438
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2118425
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752993}

--- chrome/browser/upboarding/query_tiles/tile_service_factory.cc
+++ chrome/browser/upboarding/query_tiles/tile_service_factory.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/singleton.h"
 #include "chrome/browser/image_fetcher/image_fetcher_service_factory.h"
+#include "chrome/browser/upboarding/query_tiles/tile_service_factory_helper.h"
+#include "components/image_fetcher/core/image_fetcher_service.h"
 #include "components/keyed_service/core/simple_dependency_manager.h"
 
 namespace upboarding {
@@ -32,9 +34,11 @@ TileServiceFactory::~TileServiceFactory() {}
 
 std::unique_ptr<KeyedService> TileServiceFactory::BuildServiceInstanceFor(
     SimpleFactoryKey* key) const {
-  // TODO(shaktisahu): Create TileService instance.
-
-  return nullptr;
+  // TODO(xingliu): Use network only fetcher if needed.
+  auto* image_fetcher =
+      ImageFetcherServiceFactory::GetForKey(key)->GetImageFetcher(
+          image_fetcher::ImageFetcherConfig::kDiskCacheOnly);
+  return CreateTileService(image_fetcher);
 }
 
 }  // namespace upboarding
