1ccc8493f64b9417d915c077a9b043f4bdf6dcbf
Xing Liu <xingliu@chromium.org>
2020-03-24 23:19:18 +0000

    Upboarding: Move image loader classes to internal.
    Image loader classes can be moved to internal, the ImageFetcher can be
    injected from query tiles factory.
    
    TBR=rhalavati@chromium.org
    
    Bug: 1058534
    Change-Id: I3948c8809167f4c00824b26cf4fefc8aaf3a7bce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2118223
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753039}

--- chrome/browser/upboarding/query_tiles/internal/cached_image_loader.h
+++ chrome/browser/upboarding/query_tiles/internal/cached_image_loader.h
@@ -3,12 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UPBOARDING_QUERY_TILES_CACHED_IMAGE_LOADER_H_
-#define CHROME_BROWSER_UPBOARDING_QUERY_TILES_CACHED_IMAGE_LOADER_H_
+#ifndef CHROME_BROWSER_UPBOARDING_QUERY_TILES_INTERNAL_CACHED_IMAGE_LOADER_H_
+#define CHROME_BROWSER_UPBOARDING_QUERY_TILES_INTERNAL_CACHED_IMAGE_LOADER_H_
 
 #include <memory>
 
-#include "chrome/browser/upboarding/query_tiles/image_loader.h"
+#include "chrome/browser/upboarding/query_tiles/internal/image_loader.h"
 #include "components/image_fetcher/core/image_fetcher.h"
 
 namespace image_fetcher {
@@ -35,4 +35,4 @@ class CachedImageLoader : public ImageLoader {
 
 }  // namespace upboarding
 
-#endif  // CHROME_BROWSER_UPBOARDING_QUERY_TILES_CACHED_IMAGE_LOADER_H_
+#endif  // CHROME_BROWSER_UPBOARDING_QUERY_TILES_INTERNAL_CACHED_IMAGE_LOADER_H_
--- chrome/browser/upboarding/query_tiles/internal/image_loader.h
+++ chrome/browser/upboarding/query_tiles/internal/image_loader.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UPBOARDING_QUERY_TILES_IMAGE_LOADER_H_
-#define CHROME_BROWSER_UPBOARDING_QUERY_TILES_IMAGE_LOADER_H_
+#ifndef CHROME_BROWSER_UPBOARDING_QUERY_TILES_INTERNAL_IMAGE_LOADER_H_
+#define CHROME_BROWSER_UPBOARDING_QUERY_TILES_INTERNAL_IMAGE_LOADER_H_
 
 #include <memory>
 #include <string>
@@ -34,4 +34,4 @@ class ImageLoader {
 
 }  // namespace upboarding
 
-#endif  // CHROME_BROWSER_UPBOARDING_QUERY_TILES_IMAGE_LOADER_H_
+#endif  // CHROME_BROWSER_UPBOARDING_QUERY_TILES_INTERNAL_IMAGE_LOADER_H_
