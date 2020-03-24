66d2d8e9510d82c96ff6147ff424a2cada4acb38
calamity <calamity@chromium.org>
2020-03-24 05:59:33 +0000

    Mark testRealPromoDialogDismissWithoutSelection as flaky.
    Bug: 1064092
    TBR=tedchoc@chromium.org
    
    Change-Id: I0a589a9fa7c18206571f1c7876943c29b2cd06d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117411
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752741}

--- chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
@@ -32,6 +32,7 @@ import org.chromium.base.ApplicationStatus;
 import org.chromium.base.Callback;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -446,6 +447,7 @@ public class SearchActivityTest {
 
     @Test
     @SmallTest
+    @FlakyTest(message = "https://crbug.com/1064092")
     public void testRealPromoDialogDismissWithoutSelection() throws Exception {
         // Start the Activity.  It should pause when the promo dialog appears.
         mTestDelegate.shouldShowRealSearchDialog = true;
