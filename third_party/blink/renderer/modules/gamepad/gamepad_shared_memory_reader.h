d191968b18fc7130df34146948cecde49db10c98
Johann <johannkoenig@google.com>
2020-03-24 05:38:26 +0000

    heap mojo: migrate gampad memory reader receiver_
    Use the HeapMojoReceiver wrapper so that Oilpan will GC the object.
    
    Bug: 1049056
    Change-Id: Id01a0242536dca47467421a4a478c03c80263eca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106990
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752738}

--- third_party/blink/renderer/modules/gamepad/gamepad_shared_memory_reader.h
+++ third_party/blink/renderer/modules/gamepad/gamepad_shared_memory_reader.h
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "device/gamepad/public/mojom/gamepad.mojom-blink.h"
 #include "device/gamepad/public/mojom/gamepad_hardware_buffer.h"
-#include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/system/buffer.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
 
 namespace base {
 class ReadOnlySharedMemoryRegion;
@@ -65,7 +65,7 @@ class GamepadSharedMemoryReader
 
   bool ever_interacted_with_ = false;
 
-  mojo::Receiver<device::mojom::blink::GamepadObserver> receiver_{this};
+  HeapMojoReceiver<device::mojom::blink::GamepadObserver> receiver_;
   mojo::Remote<device::mojom::blink::GamepadMonitor> gamepad_monitor_remote_;
   blink::GamepadListener* listener_ = nullptr;
 };
