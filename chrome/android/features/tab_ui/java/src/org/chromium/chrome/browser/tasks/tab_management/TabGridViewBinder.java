115184ca6352a9e9db2479328e729c5aa52f0fb1
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-03-24 00:36:23 +0000

    Simplify search term chip in Grid tab switcher
    Search term chip went through FastPath++, and some tech debt were
    introduced in order to pass formal equivalence checking.
    
    Finch parameters for search term chip in Grid tab switcher
    (enable_search_term_chip and enable_search_term_chip_adaptive_icon) are
    now cached to simplify the tests.
    
    Bug: 1048255
    Change-Id: Ibe1b88a96dae07d3de2f0f49c00e8a53aaa73cde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2114165
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752666}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewBinder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridViewBinder.java
@@ -128,7 +128,7 @@ class TabGridViewBinder {
                         (int) res.getDimension(R.dimen.tab_list_selected_inset));
                 view.setForeground(model.get(TabProperties.IS_SELECTED) ? drawable : null);
             }
-            if (TabUiFeatureUtilities.isSearchTermChipEnabled()) {
+            if (TabUiFeatureUtilities.ENABLE_SEARCH_CHIP.getValue()) {
                 ChipView searchButton = (ChipView) view.fastFindViewById(R.id.search_button);
                 searchButton.getPrimaryTextView().setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
                 searchButton.getPrimaryTextView().setEllipsize(TextUtils.TruncateAt.END);
@@ -195,8 +195,7 @@ class TabGridViewBinder {
             updateColor(view, model.get(TabProperties.IS_INCOGNITO), TabProperties.UiType.CLOSABLE);
         } else if (TabProperties.ACCESSIBILITY_DELEGATE == propertyKey) {
             view.setAccessibilityDelegate(model.get(TabProperties.ACCESSIBILITY_DELEGATE));
-        } else if (TabUiFeatureUtilities.isSearchTermChipEnabled()
-                && TabProperties.SEARCH_QUERY == propertyKey) {
+        } else if (TabProperties.SEARCH_QUERY == propertyKey) {
             String query = model.get(TabProperties.SEARCH_QUERY);
             ChipView searchButton = (ChipView) view.fastFindViewById(R.id.search_button);
             if (TextUtils.isEmpty(query)) {
@@ -205,8 +204,7 @@ class TabGridViewBinder {
                 searchButton.setVisibility(View.VISIBLE);
                 searchButton.getPrimaryTextView().setText(query);
             }
-        } else if (TabUiFeatureUtilities.isSearchTermChipEnabled()
-                && TabProperties.SEARCH_LISTENER == propertyKey) {
+        } else if (TabProperties.SEARCH_LISTENER == propertyKey) {
             TabListMediator.TabActionListener listener = model.get(TabProperties.SEARCH_LISTENER);
             ChipView searchButton = (ChipView) view.fastFindViewById(R.id.search_button);
             if (listener == null) {
@@ -217,8 +215,7 @@ class TabGridViewBinder {
                 int tabId = model.get(TabProperties.TAB_ID);
                 listener.run(tabId);
             });
-        } else if (TabUiFeatureUtilities.isSearchTermChipEnabled()
-                && TabProperties.SEARCH_CHIP_ICON_DRAWABLE_ID == propertyKey) {
+        } else if (TabProperties.SEARCH_CHIP_ICON_DRAWABLE_ID == propertyKey) {
             ChipView searchButton = (ChipView) view.fastFindViewById(R.id.search_button);
             int iconDrawableId = model.get(TabProperties.SEARCH_CHIP_ICON_DRAWABLE_ID);
             boolean shouldTint = iconDrawableId != R.drawable.ic_logo_googleg_24dp;
