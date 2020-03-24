cdc5475538ace43b944729065e16342d16af338b
Peng Huang <penghuang@chromium.org>
2020-03-24 15:27:40 +0000

    Revert "WebGPU: Add early-returns in the request of adapter and device when context is lost"
    This reverts commit 98136c2b4de8e46c94b0501faa64cdfc426a72f5.
    
    Reason for revert: Tests fail on bot Win7 FYI x64 Release
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20FYI%20x64%20Release%20%28NVIDIA%29/8317
    
    Original change's description:
    > WebGPU: Add early-returns in the request of adapter and device when context is lost
    > 
    > This patch adds early-returns in the request of GPUAdapter and GPUDevice
    > when the WebGPU context is lost.
    > 
    > In the following patches we will implement more clean-ups when the
    > WebGPU context is lost.
    > 
    > BUG=chromium:996713
    > 
    > Change-Id: I1e6e4723596ed63c8ab7f9200be41dde68b8815e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2112333
    > Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Reviewed-by: Austin Eng <enga@chromium.org>
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#752767}
    
    TBR=kbr@chromium.org,cwallez@chromium.org,shaobo.yan@intel.com,yunchao.he@intel.com,jiawei.shao@intel.com,kainino@chromium.org,jiajie.hu@intel.com,enga@chromium.org,hao.x.li@intel.com
    
    Change-Id: I563ee6c9dd473e5dad3af13fa5930fb8b3febcbb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:996713
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2118182
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752854}

--- gpu/command_buffer/tests/webgpu_test.h
+++ gpu/command_buffer/tests/webgpu_test.h
@@ -28,7 +28,7 @@ void OnRequestDeviceCallback(bool is_request_device_success,
 
 namespace webgpu {
 
-class WebGPUImplementation;
+class WebGPUInterface;
 
 }  // namespace webgpu
 
@@ -52,7 +52,7 @@ class WebGPUTest : public testing::Test {
 
   void Initialize(const Options& options);
 
-  webgpu::WebGPUImplementation* webgpu() const;
+  webgpu::WebGPUInterface* webgpu() const;
   SharedImageInterface* GetSharedImageInterface() const;
 
   void RunPendingTasks();
@@ -64,14 +64,13 @@ class WebGPUTest : public testing::Test {
   };
   DeviceAndClientID GetNewDeviceAndClientID();
 
-  const uint32_t kAdapterServiceID = 0u;
-
  private:
   std::unique_ptr<viz::TestGpuServiceHolder> gpu_service_holder_;
   std::unique_ptr<WebGPUInProcessContext> context_;
   bool is_initialized_ = false;
 
   webgpu::DawnDeviceClientID next_device_client_id_ = 1;
+  const uint32_t kAdapterServiceID = 0u;
 };
 
 }  // namespace gpu
