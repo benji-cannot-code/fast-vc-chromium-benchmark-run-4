1f1ab128acc13bf1045ba35c1d298d4a0f97caeb
Peter Kasting <pkasting@chromium.org>
2020-03-24 00:10:42 +0000

    Fix DCHECK in ~ExtensionFunction().
    If permission is denied to call an extension function, the function
    is currently destroyed without responding.  This causes a DCHECK
    failure in its destructor.
    
    Instead, ask the function to respond with an error in this case, which
    makes did_respond() return true, which avoids the DCHECK.
    
    This showed up during some trybot runs of an unrelated change, so this
    fixes some test flakiness.
    
    Bug: none
    Change-Id: I6cc5481157161b00a493504b6a403c0e492bef09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116537
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#752655}

--- extensions/browser/extension_function_dispatcher.cc
+++ extensions/browser/extension_function_dispatcher.cc
@@ -52,6 +52,8 @@ using content::BrowserThread;
 namespace extensions {
 namespace {
 
+constexpr char kCreationFailed[] = "Access to extension API denied.";
+
 // Notifies the ApiActivityMonitor that an extension API function has been
 // called. May be called from any thread.
 void NotifyApiFunctionCalled(const std::string& extension_id,
@@ -461,7 +463,7 @@ bool ExtensionFunctionDispatcher::CheckPermissions(
     const ExtensionFunction::ResponseCallback& callback) {
   if (!function->HasPermission()) {
     LOG(ERROR) << "Permission denied for " << params.name;
-    SendAccessDenied(callback);
+    function->RespondWithError(kCreationFailed);
     return false;
   }
   return true;
@@ -482,7 +484,7 @@ ExtensionFunctionDispatcher::CreateExtensionFunction(
       ExtensionFunctionRegistry::GetInstance().NewFunction(params.name);
   if (!function) {
     LOG(ERROR) << "Unknown Extension API - " << params.name;
-    SendAccessDenied(callback);
+    callback.Run(ExtensionFunction::FAILED, base::ListValue(), kCreationFailed);
     return nullptr;
   }
 
@@ -500,13 +502,4 @@ ExtensionFunctionDispatcher::CreateExtensionFunction(
 
   return function;
 }
-
-// static
-void ExtensionFunctionDispatcher::SendAccessDenied(
-    const ExtensionFunction::ResponseCallback& callback) {
-  base::ListValue empty_list;
-  callback.Run(ExtensionFunction::FAILED, empty_list,
-               "Access to extension API denied.");
-}
-
 }  // namespace extensions
