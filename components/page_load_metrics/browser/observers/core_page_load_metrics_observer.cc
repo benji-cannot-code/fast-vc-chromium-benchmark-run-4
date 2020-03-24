25db7d3269ec9df9ebee49aae3e0b1a28a1033aa
Jesse McKenna <jessemckenna@google.com>
2020-03-24 00:06:29 +0000

    Add suffixes OnBattery, NotOnBattery to FCP
    This change adds histogram suffixes
    PageLoad.PaintTiming.NavigationToFirstContentfulPaint.OnBattery and
    PageLoad.PaintTiming.NavigationToFirstContentfulPaint.NotOnBattery,
    which will allow the filtering of FirstContentfulPaint measurements by
    whether the system was on battery or wall power during page load.
    
    This will be useful for filtering FirstContentfulPaint regressions by
    battery-specific Finch experiments (e.g., SlowDCTimerInterruptsWin,
    which slows the system timer-interrupt frequency only when on battery
    power). Filtering will provide a more accurate view of the impact of
    such experiments on their affected clients.
    
    Bug: 1040724
    Change-Id: Ia0845f731c1281bdb508e2360bc393ad33f28095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2025491
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#752650}

--- components/page_load_metrics/browser/observers/core_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/core_page_load_metrics_observer.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/power_monitor/power_monitor.h"
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
 #include "content/public/common/process_type.h"
 #include "net/http/http_response_headers.h"
@@ -136,6 +137,11 @@ const char kHistogramParseBlockedOnScriptExecutionDocumentWrite[] =
 
 const char kHistogramFirstContentfulPaintNoStore[] =
     "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.NoStore";
+
+const char kHistogramFirstContentfulPaintOnBattery[] =
+    "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.OnBattery";
+const char kHistogramFirstContentfulPaintNotOnBattery[] =
+    "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.NotOnBattery";
 
 const char kHistogramLoadTypeFirstContentfulPaintReload[] =
     "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.LoadType."
@@ -406,6 +412,14 @@ void CorePageLoadMetricsObserver::OnFirstContentfulPaintInPage(
 
     if (was_no_store_main_resource_) {
       PAGE_LOAD_HISTOGRAM(internal::kHistogramFirstContentfulPaintNoStore,
+                          timing.paint_timing->first_contentful_paint.value());
+    }
+
+    if (base::PowerMonitor::IsOnBatteryPower()) {
+      PAGE_LOAD_HISTOGRAM(internal::kHistogramFirstContentfulPaintOnBattery,
+                          timing.paint_timing->first_contentful_paint.value());
+    } else {
+      PAGE_LOAD_HISTOGRAM(internal::kHistogramFirstContentfulPaintNotOnBattery,
                           timing.paint_timing->first_contentful_paint.value());
     }
 
--- components/page_load_metrics/browser/observers/core_page_load_metrics_observer_unittest.cc
+++ components/page_load_metrics/browser/observers/core_page_load_metrics_observer_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/test/power_monitor_test_base.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
 #include "components/page_load_metrics/browser/observers/page_load_metrics_observer_content_test_harness.h"
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
@@ -42,6 +43,14 @@ class CorePageLoadMetricsObserverTest
   void SetUp() override {
     page_load_metrics::PageLoadMetricsObserverContentTestHarness::SetUp();
     page_load_metrics::LargestContentfulPaintHandler::SetTestMode(true);
+
+    base::PowerMonitor::Initialize(
+        std::make_unique<base::PowerMonitorTestSource>());
+  }
+
+  void TearDown() override {
+    base::PowerMonitor::ShutdownForTesting();
+    page_load_metrics::PageLoadMetricsObserverContentTestHarness::TearDown();
   }
 
   void OnCpuTimingUpdate(RenderFrameHost* render_frame_host,
