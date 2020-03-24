cb6f5c483097a094068fb050ea976dafd3db817a
Luciano Pacheco <lucmult@chromium.org>
2020-03-24 00:56:49 +0000

    Reland "Files app: Enable flag files-ng by default"
    This reverts commit 7545fd48f9203a24b05300bcd5b10962ca07a5e9.
    
    Reason for revert: We want to enable the flag by default on ToT, the revert was intended to disable the flag on M-82, on ToT it should be
    enabled.
    
    Original change's description:
    > Revert "Files app: Enable flag files-ng by default"
    > 
    > This reverts commit ea497580cfe3df53989772094178d41c088e0e13.
    > 
    > Reason for revert: Feature moved from M-82 to M-83.
    > 
    > Original change's description:
    > > Files app: Enable flag files-ng by default
    > >
    > > Add files-ng classes and attribute to main.html so files-ng styles are
    > > applied during app startup (to avoid a FOUC flash of non-files-ng blue
    > > color).
    > >
    > > Enable file-ng by default in FilesApp and its browser test harness.
    > >
    > > Bug: 992818
    > > Change-Id: If437271c5604d36b4b0908abb663a84e52b6ae35
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2087308
    > > Reviewed-by: Alex Danilo <adanilo@chromium.org>
    > > Commit-Queue: Alex Danilo <adanilo@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#747421}
    > 
    > TBR=noel@chromium.org,lucmult@chromium.org,adanilo@chromium.org
    > 
    > Bug: 992818
    > Change-Id: I8b1c770e2c43823eff9a2a9aceae650fcb25a3f8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2103992
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Reviewed-by: Noel Gordon <noel@chromium.org>
    > Reviewed-by: Alex Danilo <adanilo@chromium.org>
    > Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#750448}
    
    TBR=noel@chromium.org,lucmult@chromium.org,adanilo@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 992818
    Change-Id: I94e63a873d4e2970d201a32dca0944a4e8fa520f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116152
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752674}

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -1560,7 +1560,7 @@ bool FileManagerBrowserTestBase::GetIsOffline() const {
 }
 
 bool FileManagerBrowserTestBase::GetEnableFilesNg() const {
-  return false;
+  return true;
 }
 
 bool FileManagerBrowserTestBase::GetEnableNativeSmb() const {
