7cefb55498c24285c5cd2be2424d1fd655f9c799
Tibor Goldschwendt <tiborg@chromium.org>
2020-03-24 00:42:43 +0000

    [webui][ntp] Fetch WebUI NTP compatible doodle
    Due to postMessage targeting the WebUI NTP requires different query
    params on the doodle URL. The doodle server automatically adds those
    when fetching the doodle info with the async=ntp:2 param appended. This
    CL appends this param for the WebUI NTP.
    
    Bug: 1039910
    Change-Id: I5109bb4c35f1a559834e371a0a320481b6da37a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111063
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752668}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_handler.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_handler.cc
@@ -286,7 +286,7 @@ void NewTabPageHandler::GetDoodle(GetDoodleCallback callback) {
   }
   // This will trigger re-downloading the doodle and caching it. This means that
   // in regular mode a new doodle will be returned on subsequent NTP loads.
-  logo_service_->GetLogo(std::move(callbacks));
+  logo_service_->GetLogo(std::move(callbacks), /*for_webui_ntp=*/true);
 }
 
 void NewTabPageHandler::NtpThemeChanged(const NtpTheme& ntp_theme) {
