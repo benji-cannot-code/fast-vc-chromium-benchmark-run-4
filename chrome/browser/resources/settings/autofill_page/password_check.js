45103573b6342bb069d1640d314f8fb18babefa3
Viktor Semeniuk <vsemeniuk@google.com>
2020-03-24 11:44:14 +0000

    Reland "[Passwords] No result view fix in password check"
    This is a reland of 39bb3cd1fc375f3ec278bcef594f8de5778358dd
    
    Original change's description:
    > [Passwords] No result view fix in password check
    > 
    > This CL changes the way visibility of 'No result view' is calculated.
    > The label will be shown only if:
    > - user had successful check with 0 compromised passwords found
    > - user is signed in
    > - breach detection is turned on in settings
    > 
    > Bug: 1061496
    > Change-Id: I9f1e4b5f7aef51749b970964c2b701ee2193ba53
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108501
    > Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    > Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#751674}
    
    Bug: 1061496
    Change-Id: I838b9383f5a45733aac7dba34875fffc4e12355a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111177
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#752796}

--- chrome/browser/resources/settings/autofill_page/password_check.js
+++ chrome/browser/resources/settings/autofill_page/password_check.js
@@ -12,6 +12,7 @@ Polymer({
 
   behaviors: [
     I18nBehavior,
+    PrefsBehavior,
     WebUIListenerBehavior,
   ],
 
@@ -80,6 +81,13 @@ Polymer({
      * @private {?PasswordManagerProxy.CompromisedCredential}
      */
     activePassword_: Object,
+
+    /** @private */
+    showNoCompromisedPasswordsLabel_: {
+      type: Boolean,
+      computed:
+          'computeShowNoCompromisedPasswordsLabel(syncStatus_, prefs.*, status_, leakedPasswords)',
+    }
   },
 
   /**
@@ -537,5 +545,26 @@ Polymer({
     this.leakedPasswords = resultList.concat(addedResults);
   },
 
+  /**
+   * @return {boolean}
+   * @private
+   */
+  computeShowNoCompromisedPasswordsLabel() {
+    // Check if user isn't signed in.
+    if (!this.syncStatus_ || !this.syncStatus_.signedIn) {
+      return false;
+    }
+
+    // Check if breach detection is turned off in settings.
+    if (!this.prefs ||
+        !this.getPref('profile.password_manager_leak_detection').value) {
+      return false;
+    }
+
+    // Return true if there was a successful check and no compromised passwords
+    // were found.
+    return !this.hasLeakedCredentials_(this.leakedPasswords) &&
+        this.showsTimestamp_(this.status_);
+  },
 });
 })();
