0a328bc06fdac738ec1c379d0689a79a5fcc5ac9
Xinghui Lu <xinghuilu@chromium.org>
2020-03-24 21:07:35 +0000

    Add metrics to count the number of threat infos on each URL check.
    We'd like to know the number of threat infos when safe browsing does a
    real time URL check or a hash based check. The following metrics are
    added:
    
    SafeBrowsing.RT.ThreatInfoSize
    SafeBrowsing.V4LocalDatabaseManager.ThreatInfoSize
    
    Bug: 1062335
    Change-Id: I94568170be737a51964eaed395bafcbb6ba89297
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2113792
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752991}

--- components/safe_browsing/core/db/v4_local_database_manager_unittest.cc
+++ components/safe_browsing/core/db/v4_local_database_manager_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_tokenizer.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -821,6 +822,7 @@ TEST_F(V4LocalDatabaseManagerTest, TestCheckUrlForHCAllowlistUnavailable) {
 }
 
 TEST_F(V4LocalDatabaseManagerTest, TestGetSeverestThreatTypeAndMetadata) {
+  base::HistogramTester histograms;
   WaitForTasksOnTaskRunner();
 
   FullHash fh_malware("Malware");
@@ -865,6 +867,10 @@ TEST_F(V4LocalDatabaseManagerTest, TestGetSeverestThreatTypeAndMetadata) {
   EXPECT_EQ(SB_THREAT_TYPE_URL_MALWARE, result_threat_type);
   EXPECT_EQ("malware_popid", metadata.population_id);
   EXPECT_EQ(fh_malware, matching_full_hash);
+
+  histograms.ExpectUniqueSample(
+      "SafeBrowsing.V4LocalDatabaseManager.ThreatInfoSize",
+      /* sample */ 2, /* expected_count */ 2);
 }
 
 TEST_F(V4LocalDatabaseManagerTest, TestChecksAreQueued) {
--- components/safe_browsing/core/realtime/url_lookup_service_unittest.cc
+++ components/safe_browsing/core/realtime/url_lookup_service_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/safe_browsing/core/realtime/url_lookup_service.h"
 
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
@@ -548,6 +549,7 @@ TEST_F(RealTimeUrlLookupServiceTest, TestCacheInCacheManager) {
 }
 
 TEST_F(RealTimeUrlLookupServiceTest, TestStartLookup_ResponseIsAlreadyCached) {
+  base::HistogramTester histograms;
   EnableRealTimeUrlLookup(/* is_with_token_enabled */ false);
   GURL url("http://example.test/");
   MayBeCacheRealTimeUrlVerdict(url, RTLookupResponse::ThreatInfo::DANGEROUS,
@@ -566,10 +568,17 @@ TEST_F(RealTimeUrlLookupServiceTest, TestStartLookup_ResponseIsAlreadyCached) {
   EXPECT_CALL(response_callback, Run(/* is_rt_lookup_successful */ true, _));
 
   task_environment_->RunUntilIdle();
+
+  // This metric is not recorded because the response is obtained from the
+  // cache.
+  histograms.ExpectUniqueSample("SafeBrowsing.RT.ThreatInfoSize",
+                                /* sample */ 0,
+                                /* expected_count */ 0);
 }
 
 TEST_F(RealTimeUrlLookupServiceTest,
        TestStartLookup_AttachTokenWhenWithTokenIsEnabled) {
+  base::HistogramTester histograms;
   EnableRealTimeUrlLookup(/* is_with_token_enabled */ true);
   SetupPrimaryAccount();
   GURL url("http://example.test/");
@@ -596,6 +605,10 @@ TEST_F(RealTimeUrlLookupServiceTest,
   std::unique_ptr<RTLookupResponse> cache_response =
       GetCachedRealTimeUrlVerdict(url);
   EXPECT_NE(nullptr, cache_response);
+
+  histograms.ExpectUniqueSample("SafeBrowsing.RT.ThreatInfoSize",
+                                /* sample */ 1,
+                                /* expected_count */ 1);
 }
 
 TEST_F(RealTimeUrlLookupServiceTest, TestStartLookup_NoTokenWhenNotSignedIn) {
