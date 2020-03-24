c727744d62fba168f4ba4ff64a4088639ff19010
danakj <danakj@chromium.org>
2020-03-24 16:26:38 +0000

    Remove check for running web tests with content browser tests
    Web tests are run in content shell not in content_browsertests.
    
    R=nasko@chromium.org
    
    Bug: 866140
    Change-Id: Iec4681299aed452e46c6df119ff4c28f06e1f41e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116553
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752874}

--- content/public/test/content_browser_test.cc
+++ content/public/test/content_browser_test.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/browser/shell_browser_context.h"
 #include "content/shell/browser/shell_content_browser_client.h"
 #include "content/shell/common/shell_switches.h"
-#include "content/shell/renderer/web_test/web_test_content_renderer_client.h"
 #include "content/test/test_content_client.h"
 #include "ui/events/platform/platform_event_source.h"
 
@@ -108,11 +107,9 @@ void ContentBrowserTest::PreRunTestOnMainThread() {
   network_change_simulator.InitializeChromeosConnectionType();
 #endif
 
-  if (!switches::IsRunWebTestsSwitchPresent()) {
-    CHECK_EQ(Shell::windows().size(), 1u);
-    shell_ = Shell::windows()[0];
-    SetInitialWebContents(shell_->web_contents());
-  }
+  CHECK_EQ(Shell::windows().size(), 1u);
+  shell_ = Shell::windows()[0];
+  SetInitialWebContents(shell_->web_contents());
 
 #if defined(OS_MACOSX)
   // On Mac, without the following autorelease pool, code which is directly
