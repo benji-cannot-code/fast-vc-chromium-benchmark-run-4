69b1812e9a1d964f20ec128e35d84c99da2f780a
Jinsuk Kim <jinsukkim@chromium.org>
2020-03-24 01:26:55 +0000

    GestureNav: Remove dependency on ChromeActivity
    Removes all the occurrences of TabImpl.getActivity() in gesturenav/.
    Now the dependencies are passed through
    HistoryNavigationCoordinator.create() in the form of supplier,
    runnable, etc.
    
    Bug: 952703
    Change-Id: I722acf5cbaa866556f2b953d7760ac7aa9630604
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106045
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752689}

--- chrome/android/java/src/org/chromium/chrome/browser/gesturenav/TabbedSheetDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gesturenav/TabbedSheetDelegate.java
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.gesturenav;
 
-import org.chromium.chrome.R;
-import org.chromium.chrome.browser.history.HistoryManagerUtils;
+import org.chromium.base.Consumer;
 import org.chromium.chrome.browser.tab.Tab;
-import org.chromium.chrome.browser.tab.TabImpl;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.NavigationEntry;
 import org.chromium.content_public.browser.NavigationHistory;
@@ -22,12 +20,13 @@ public class TabbedSheetDelegate implements NavigationSheet.Delegate {
     private static final int FULL_HISTORY_ENTRY_INDEX = -1;
 
     private final Tab mTab;
+    private final Consumer<Tab> mShowHistoryManager;
     private final String mFullHistoryMenu;
 
-    public TabbedSheetDelegate(Tab tab) {
+    public TabbedSheetDelegate(Tab tab, Consumer<Tab> showHistoryManager, String historyMenu) {
         mTab = tab;
-        mFullHistoryMenu =
-                ((TabImpl) tab).getActivity().getResources().getString(R.string.show_full_history);
+        mShowHistoryManager = showHistoryManager;
+        mFullHistoryMenu = historyMenu;
     }
 
     @Override
@@ -43,7 +42,7 @@ public class TabbedSheetDelegate implements NavigationSheet.Delegate {
     @Override
     public void navigateToIndex(int index) {
         if (index == FULL_HISTORY_ENTRY_INDEX) {
-            HistoryManagerUtils.showHistoryManager(((TabImpl) mTab).getActivity(), mTab);
+            mShowHistoryManager.accept(mTab);
         } else {
             mTab.getWebContents().getNavigationController().goToNavigationIndex(index);
         }
