6f64d558e7155ef194a2fc58e2e09dfaa2a37165
Sammie Quon <sammiequon@chromium.org>
2020-03-24 00:48:09 +0000

    tablet: Speculative fix for signature ui::LayerAnimator::RemoveObserver.
    Stack trace shows window destroying calling |animating_layer_| which is
    the one of the windows associated with the destroyed window. My guess is
    that it might be a dangling pointer, so when window is destroying, set
    it to null.
    
    I tried reproing with a test that destroys the windows at certain points
    of the animation, but no luck :(.
    
    Test: none
    Bug: 1028815
    Change-Id: I5baa2d3784836296c2afe063e6861aa43fb68e5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111059
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#752670}

--- ash/wm/tablet_mode/tablet_mode_controller.h
+++ ash/wm/tablet_mode/tablet_mode_controller.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/power/power_manager_client.h"
 #include "ui/aura/window_occlusion_tracker.h"
 #include "ui/compositor/layer_animation_observer.h"
+#include "ui/compositor/layer_observer.h"
 #include "ui/compositor/layer_tree_owner.h"
 #include "ui/events/devices/input_device_event_observer.h"
 #include "ui/gfx/geometry/vector3d_f.h"
@@ -72,7 +73,8 @@ class ASH_EXPORT TabletModeController
       public WindowTreeHostManager::Observer,
       public SessionObserver,
       public ui::InputDeviceEventObserver,
-      public ui::LayerAnimationObserver {
+      public ui::LayerAnimationObserver,
+      public ui::LayerObserver {
  public:
   // Enable or disable using a screenshot for testing as it makes the
   // initialization flow async, which makes most tests harder to write.
@@ -158,6 +160,9 @@ class ASH_EXPORT TabletModeController
   void OnLayerAnimationEnded(ui::LayerAnimationSequence* sequence) override;
   void OnLayerAnimationAborted(ui::LayerAnimationSequence* sequence) override;
   void OnLayerAnimationScheduled(ui::LayerAnimationSequence* sequence) override;
+
+  // ui::LayerObserver:
+  void LayerDestroyed(ui::Layer* layer) override;
 
   void increment_app_window_drag_count() { ++app_window_drag_count_; }
   void increment_app_window_drag_in_splitview_count() {
@@ -448,6 +453,8 @@ class ASH_EXPORT TabletModeController
   // everything in the screen rotation container except the top window. It helps
   // with animation performance because it fully occludes all windows except the
   // animating window for the duration of the animation.
+  // TODO(sammiequon): See if we can move screenshot and tablet mode transition
+  // animation related code into a separate class/file.
   std::unique_ptr<ui::Layer> screenshot_layer_;
 
   base::ObserverList<TabletModeObserver>::Unchecked tablet_mode_observers_;
