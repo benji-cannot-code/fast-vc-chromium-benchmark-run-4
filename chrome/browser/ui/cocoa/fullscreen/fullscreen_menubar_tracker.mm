9c0ccece81e223022ba96886d4b11b4855e40acf
Leonard Grey <lgrey@chromium.org>
2020-03-25 13:55:52 +0000

    Mac: clean up FullscreenToolbarController
    - Remove some structure that was previously necessary for Cocoa browser
    - the visibility lock controller is vestigial: we release the lock,
    but never acquire it
    
    Bug: 1063417
    Change-Id: I55ccc6b14b0ffed7d81beb640a54ec8b1110e831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117321
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753211}

--- chrome/browser/ui/cocoa/fullscreen/fullscreen_menubar_tracker.mm
+++ chrome/browser/ui/cocoa/fullscreen/fullscreen_menubar_tracker.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/mac_util.h"
 #include "base/stl_util.h"
 #import "chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_controller.h"
-#import "chrome/browser/ui/cocoa/fullscreen/fullscreen_toolbar_visibility_lock_controller.h"
 #include "ui/base/cocoa/appkit_utils.h"
 
 namespace {
@@ -19,6 +18,8 @@ namespace {
 // The event kind value for a undocumented menubar show/hide Carbon event.
 const CGFloat kMenuBarRevealEventKind = 2004;
 
+// TODO(https://crbug.com/1063417): Replace this with something that works
+// on modern macOS versions.
 OSStatus MenuBarRevealHandler(EventHandlerCallRef handler,
                               EventRef event,
                               void* context) {
@@ -51,7 +52,6 @@ OSStatus MenuBarRevealHandler(EventHandlerCallRef handler,
 
 @interface FullscreenMenubarTracker () {
   FullscreenToolbarController* _controller;        // weak
-  id<FullscreenToolbarContextDelegate> _delegate;  // weak
 
   // A Carbon event handler that tracks the revealed fraction of the menubar.
   EventHandlerRef _menubarTrackingHandler;
@@ -71,7 +71,6 @@ OSStatus MenuBarRevealHandler(EventHandlerCallRef handler,
     (FullscreenToolbarController*)controller {
   if ((self = [super init])) {
     _controller = controller;
-    _delegate = [controller delegate];
     _state = FullscreenMenubarState::HIDDEN;
 
     // Install the Carbon event handler for the menubar show, hide and
@@ -113,8 +112,8 @@ OSStatus MenuBarRevealHandler(EventHandlerCallRef handler,
 }
 
 - (void)setMenubarProgress:(CGFloat)progress {
-  if (![_delegate isInAnyFullscreenMode] ||
-      [_delegate isFullscreenTransitionInProgress]) {
+  if (![_controller isInAnyFullscreenMode] ||
+      [_controller isFullscreenTransitionInProgress]) {
     return;
   }
 
@@ -124,7 +123,7 @@ OSStatus MenuBarRevealHandler(EventHandlerCallRef handler,
     return;
 
   // Ignore the menubarFraction changes if the Space is inactive.
-  if (![[_delegate window] isOnActiveSpace])
+  if (![[_controller window] isOnActiveSpace])
     return;
 
   if (ui::IsCGFloatEqual(progress, 1.0))
@@ -138,7 +137,6 @@ OSStatus MenuBarRevealHandler(EventHandlerCallRef handler,
 
   _menubarFraction = progress;
   [_controller layoutToolbar];
-
   // AppKit drives the menu bar animation from a nested run loop. Flush
   // explicitly so that Chrome's UI updates during the animation.
   [CATransaction flush];
@@ -146,14 +144,12 @@ OSStatus MenuBarRevealHandler(EventHandlerCallRef handler,
 
 - (BOOL)isMouseOnScreen {
   return NSMouseInRect([NSEvent mouseLocation],
-                       [[_delegate window] screen].frame, false);
+                       [[_controller window] screen].frame, false);
 }
 
 - (void)activeSpaceDidChange:(NSNotification*)notification {
   _menubarFraction = 0.0;
   _state = FullscreenMenubarState::HIDDEN;
-  [[_controller visibilityLockController] releaseToolbarVisibilityForOwner:self
-                                                             withAnimation:NO];
   [_controller layoutToolbar];
 }
 
