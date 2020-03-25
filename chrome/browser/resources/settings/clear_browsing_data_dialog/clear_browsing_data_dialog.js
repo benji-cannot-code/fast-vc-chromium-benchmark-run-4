3f6df85e6fdd9c3658c994093663ba0678a8b780
Jarryd <jarrydg@chromium.org>
2020-03-25 20:43:15 +0000

    ClearBrowsingData: Add boolean histogram for installed app warnings.
    This histogram tracks whether or not the user even uses the installed
    app warning dialog. This metric will make it easier for us to see if
    it's worth keeping this extra dialog, which is extra friction in the
    clear browsing data flow.
    
    Bug: 997258
    Change-Id: I8e4235ca901a0b44ded4f62efe29388b03e1d2b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116310
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Jarryd Goodman <jarrydg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753357}

--- chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_dialog.js
+++ chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_dialog.js
@@ -384,6 +384,8 @@ Polymer({
 
     const uncheckedAppCount = this.installedApps_.filter(app => !app.isChecked)
         .length;
+    chrome.metricsPrivate.recordBoolean(
+        'History.ClearBrowsingData.InstalledAppExcluded', !!uncheckedAppCount);
     chrome.metricsPrivate.recordCount(
         'History.ClearBrowsingData.InstalledDeselectedNum', uncheckedAppCount);
     chrome.metricsPrivate.recordPercentage(
