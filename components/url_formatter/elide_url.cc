3d309272532c095f6f66ffd8fd65c715482183b8
Peter Kasting <pkasting@chromium.org>
2020-03-25 20:50:40 +0000

    Remove unnecessary ui/gfx #include from url_formatter public header.
    Bug: none
    Change-Id: I5b95f74395d05e0c26a53c7d9419ec0cb1938ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2121039
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753361}

--- components/url_formatter/elide_url.cc
+++ components/url_formatter/elide_url.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_formatter/url_formatter.h"
 #include "net/base/escape.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
+#include "ui/gfx/text_constants.h"
 #include "ui/gfx/text_elider.h"
 #include "ui/gfx/text_utils.h"
 #include "url/gurl.h"
