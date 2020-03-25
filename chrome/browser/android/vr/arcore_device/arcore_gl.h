55168674e5a9cfa0c2654375d47217d0f8d1170e
Klaus Weidner <klausw@chromium.org>
2020-03-25 18:45:23 +0000

    WebXR: adjust AR frame scheduling
    In AR mode, ARCore's blocking update() method was being called too
    early, blocking processing for the current frame. Use a delayed
    PostTask with an estimated arrival time for the next frame to
    avoid blocking the GL thread while waiting.
    
    Also improve event tracing by adding frame numbers and timing counters,
    inspired by those used for the GVR device's VR mode.
    
    Change-Id: Ifece617d6f489366834b96a04f002969bbaea7ed
    Bug: 1064629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2118585
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753292}

--- chrome/browser/android/vr/arcore_device/arcore_gl.h
+++ chrome/browser/android/vr/arcore_device/arcore_gl.h
@@ -159,6 +159,7 @@ class ArCoreGl : public mojom::XRFrameDataProvider,
   void Pause();
   void Resume();
 
+  void FinishFrame(int16_t frame_index);
   bool IsSubmitFrameExpected(int16_t frame_index);
   void ProcessFrame(mojom::XRFrameDataRequestOptionsPtr options,
                     mojom::XRFrameDataPtr frame_data,
@@ -232,6 +233,9 @@ class ArCoreGl : public mojom::XRFrameDataProvider,
 
   bool restrict_frame_data_ = false;
 
+  base::TimeTicks arcore_update_completed_;
+  base::TimeTicks arcore_update_next_expected_;
+  base::TimeDelta arcore_update_interval_;
   FPSMeter fps_meter_;
 
   mojo::Receiver<mojom::XRFrameDataProvider> frame_data_receiver_{this};
