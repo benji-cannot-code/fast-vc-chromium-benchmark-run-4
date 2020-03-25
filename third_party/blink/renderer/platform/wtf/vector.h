5d8e060b195b88389070ab6e07cc3f442d5bdfc3
Omer Katz <omerkatz@chromium.org>
2020-03-25 19:12:11 +0000

    Revert "heap: Trace Vector buffer up to capacity"
    This reverts commit 5dba98706394089ec0b69f033625186bae3a000b.
    
    Reason for revert: quick resolution to regressions in issue 1063593
    
    Original change's description:
    > heap: Trace Vector buffer up to capacity
    >
    > Tracing to capacity means we don't need to check size_ during
    > tracing anymore.
    >
    > Bug: 986235
    > Change-Id: Idae046fcbb6a1705c1522d56479d560091415ea4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2107983
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Omer Katz <omerkatz@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#751353}
    
    TBR=haraken@chromium.org,mlippautz@chromium.org,omerkatz@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 986235
    Change-Id: Ib5b207d4f81966fae2f2522bd1c806071ff82c55
    Tbr: haraken@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119519
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753321}

--- third_party/blink/renderer/platform/wtf/vector.h
+++ third_party/blink/renderer/platform/wtf/vector.h
@@ -629,10 +629,7 @@ class VectorBuffer : protected VectorBufferBase<T, Allocator> {
  public:
   using OffsetRange = typename Base::OffsetRange;
 
-  VectorBuffer() : Base(InlineBuffer(), inlineCapacity) {
-    // Make sure the inline buffer is clear.
-    Base::ClearUnusedSlots(InlineBuffer(), InlineBuffer() + inlineCapacity);
-  }
+  VectorBuffer() : Base(InlineBuffer(), inlineCapacity) {}
 
   VectorBuffer(HashTableDeletedValueType value) : Base(value) {}
   bool IsHashTableDeletedValue() const {
@@ -641,8 +638,6 @@ class VectorBuffer : protected VectorBufferBase<T, Allocator> {
 
   explicit VectorBuffer(wtf_size_t capacity)
       : Base(InlineBuffer(), inlineCapacity) {
-    // Make sure the inline buffer is clear.
-    Base::ClearUnusedSlots(InlineBuffer(), InlineBuffer() + inlineCapacity);
     if (capacity > inlineCapacity)
       Base::AllocateBuffer(capacity);
   }
@@ -2070,7 +2065,7 @@ Vector<T, inlineCapacity, Allocator>::Trace(VisitorDispatcher visitor) const {
       return;
     // Inline buffer requires tracing immediately.
     const T* buffer_begin = buffer;
-    const T* buffer_end = buffer + inlineCapacity;
+    const T* buffer_end = buffer + size();
     if (IsTraceableInCollectionTrait<VectorTraits<T>>::value) {
       for (const T* buffer_entry = buffer_begin; buffer_entry != buffer_end;
            buffer_entry++) {
