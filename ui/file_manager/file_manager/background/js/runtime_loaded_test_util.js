04e94adddbb1908b2dfb180dadda98cf96e492ca
Luciano Pacheco <lucmult@chromium.org>
2020-03-25 05:36:10 +0000

    Add closure type check for some integration tests
    Bug: 1064504
    Change-Id: I1233f791280d3b7aa56b7ee6713ebb2886fa6048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119348
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753133}

--- ui/file_manager/file_manager/background/js/runtime_loaded_test_util.js
+++ ui/file_manager/file_manager/background/js/runtime_loaded_test_util.js
@@ -16,6 +16,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   styles:(Object<string>|undefined),
  *   hidden:boolean,
  *   hasShadowRoot: boolean,
+ *   imageWidth: (number|undefined),
+ *   imageHeight: (number|undefined),
+ *   renderedWidth: (number|undefined),
+ *   renderedHeight: (number|undefined),
+ *   renderedTop: (number|undefined),
+ *   renderedLeft: (number|undefined),
+ *   scrollLeft: (number|undefined),
+ *   scrollTop: (number|undefined),
+ *   scrollWidth: (number|undefined),
+ *   scrollHeight: (number|undefined),
  *  }}
  */
 let ElementObject;
