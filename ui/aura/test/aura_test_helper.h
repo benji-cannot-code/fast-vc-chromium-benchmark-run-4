77a58228e2dfddda6080734630b98030f1e621cd
Peter Kasting <pkasting@chromium.org>
2020-03-25 03:06:24 +0000

    Rename and out-of-line a variety of AuraTestHelper getters.
    This also gets rid of event_sink() entirely, since callers can easily
    get it from the root window.
    
    This is in preparation for making AshTestHelper a subclass of
    AuraTestHelper, which will require these to be virtual.
    
    I did not rename the transitive closure of caller getters to ensure
    everything everywhere was renamed -- that would have required hundreds
    and hundreds of changes and didn't seem worth it.
    
    Bug: none
    Change-Id: Ief901ed0ffd733a553bfccb445c96e920be53499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117348
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753109}

--- ui/aura/test/aura_test_helper.h
+++ ui/aura/test/aura_test_helper.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "ui/aura/window_event_dispatcher.h"
-#include "ui/aura/window_tree_host.h"
 
 namespace ui {
 class ContextFactory;
@@ -27,6 +26,7 @@ namespace aura {
 class Env;
 class TestScreen;
 class Window;
+class WindowTreeHost;
 
 namespace client {
 class CaptureClient;
@@ -58,14 +58,11 @@ class AuraTestHelper {
   // Flushes message loop.
   void RunAllPendingInMessageLoop();
 
-  Window* root_window() { return host_ ? host_->window() : nullptr; }
-  ui::EventSink* event_sink() { return host_->event_sink(); }
-  WindowTreeHost* host() { return host_.get(); }
-
-  TestScreen* test_screen() { return test_screen_.get(); }
-
-  client::FocusClient* focus_client() { return focus_client_.get(); }
-  client::CaptureClient* capture_client();
+  Window* GetContext();
+  WindowTreeHost* GetHost();
+  TestScreen* GetTestScreen();
+  client::FocusClient* GetFocusClient();
+  client::CaptureClient* GetCaptureClient();
 
   Env* GetEnv();
 
