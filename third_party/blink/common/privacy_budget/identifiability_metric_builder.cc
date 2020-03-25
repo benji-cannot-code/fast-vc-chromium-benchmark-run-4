7ac03ae55a86440abfbf6071369674edfc4c38e4
Asanka Herath <asanka@chromium.org>
2020-03-25 21:49:25 +0000

    [privacy_budget] Introduce identifiability metrics builders.
    These metrics builders prepare identifiability metrics that are reported
    via UKM.
    
    Bug: 973801
    Change-Id: I4358eda3737f95f95772a874367ca2ac1fb5d651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2092061
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753389}

---
+++ third_party/blink/common/privacy_budget/identifiability_metric_builder.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
+
+#include "services/metrics/public/cpp/ukm_builders.h"
+
+namespace blink {
+
+IdentifiabilityMetricBuilder::IdentifiabilityMetricBuilder(
+    base::UkmSourceId source_id)
+    : ukm::internal::UkmEntryBuilderBase(
+          source_id,
+          ukm::builders::Identifiability::kEntryNameHash) {}
+
+IdentifiabilityMetricBuilder::~IdentifiabilityMetricBuilder() = default;
+
+IdentifiabilityMetricBuilder& IdentifiabilityMetricBuilder::Set(
+    IdentifiableSurface surface,
+    int64_t value) {
+  SetMetricInternal(surface.ToUkmMetricHash(), value);
+  return *this;
+}
+
+}  // namespace blink
