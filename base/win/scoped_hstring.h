f2bb6973e85a2efca109444d184616afb60b5428
Xiaohan Wang <xhwang@chromium.org>
2020-03-25 16:27:22 +0000

    Warn unused result for ScopedHString::ResolveCoreWinRTStringDelayload()
    Also fix MediaFoundationProtectionManager to check the result of
    ScopedHString::ResolveCoreWinRTStringDelayload().
    
    Change-Id: Ic9e37cfe3a7a53fd38332002d2396345ea1f78cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119373
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753238}

--- base/win/scoped_hstring.h
+++ base/win/scoped_hstring.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <hstring.h>
 
+#include "base/compiler_specific.h"
 #include "base/scoped_generic.h"
 #include "base/strings/string_piece_forward.h"
 
@@ -62,7 +63,7 @@ class BASE_EXPORT ScopedHString
   static ScopedHString Create(StringPiece str);
 
   // Loads all required HSTRING functions, available from Win8 and onwards.
-  static bool ResolveCoreWinRTStringDelayload();
+  static bool ResolveCoreWinRTStringDelayload() WARN_UNUSED_RESULT;
 
   // Returns a view into the memory buffer managed by the instance. The returned
   // StringPiece is only valid during the lifetime of this ScopedHString
