7609ad977151eee2980337ab422b53c2c4e67d73
Caleb Rouleau <crouleau@chromium.org>
2020-03-25 22:07:16 +0000

    [Benchmarking] Replace myself as dummy benchmark owner.
    Change-Id: I9abf963d70f931c4d4b4b252df6535e2ad88dd04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119633
    Auto-Submit: Caleb Rouleau <crouleau@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/master@{#753402}

--- tools/perf/benchmarks/dummy_benchmark.py
+++ tools/perf/benchmarks/dummy_benchmark.py
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Dummy benchmarks for the bisect FYI integration tests.
+"""Dummy benchmarks for the bisect FYI integration tests and other tests.
 
 The number they produce aren't meant to represent any actual performance
 data of the browser. For more information about these dummy benchmarks,
@@ -35,7 +35,9 @@ class _DummyBenchmark(perf_benchmark.PerfBenchmark):
   page_set = dummy_story_set.DummyStorySet
 
 
-@benchmark.Info(emails=['crouleau@chromium.org'], component='Test>Telemetry')
+@benchmark.Info(
+    emails=['johnchen@chromium.org', 'wenbinzhang@google.com'],
+    component='Test>Telemetry')
 class DummyBenchmarkOne(_DummyBenchmark):
   """A low noise benchmark with mean=100 & std=1."""
 
@@ -47,7 +49,9 @@ class DummyBenchmarkOne(_DummyBenchmark):
     return 'dummy_benchmark.stable_benchmark_1'
 
 
-@benchmark.Info(emails=['crouleau@chromium.org'], component='Test>Telemetry')
+@benchmark.Info(
+    emails=['johnchen@chromium.org', 'wenbinzhang@google.com'],
+    component='Test>Telemetry')
 class DummyBenchmarkTwo(_DummyBenchmark):
   """A noisy benchmark with mean=50 & std=20."""
 
