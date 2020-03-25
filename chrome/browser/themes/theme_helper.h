5553d7eaa25547b1fb7d61333fa4fb6d34a0b162
Thomas Lukaszewicz <tluk@chromium.org>
2020-03-25 16:14:26 +0000

    Changed color use for tab groups.
    This change moves color definitions for the tab groups out of
    /components and into /chrome. This change means that each platform
    that wishes to use tab groups will have to define their own color
    mappings to the TabGroupColorId enum. Color should not be defined in
    /components.
    
    Change is intended to align color id definitions of the tab groups
    such that they allow for an easy transition into the Color Pipeline
    way of doing color.
    
    This CL is intended to match the existing functionality as closely
    as possible.
    
    Follow up CLs will address use of global theme accessors used in
    the menu models, fix color semantics and clean up tab group code.
    
    Bug: 1056916
    Change-Id: I29d97c1b4101ed0d80d822f6c2c2d9db9856c81a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2115937
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753235}

--- chrome/browser/themes/theme_helper.h
+++ chrome/browser/themes/theme_helper.h
@@ -164,6 +164,10 @@ class ThemeHelper {
       bool incognito,
       const CustomThemeSupplier* theme_supplier) const;
 
+  SkColor GetTabGroupColor(int id,
+                           bool incognito,
+                           const CustomThemeSupplier* theme_supplier) const;
+
   SEQUENCE_CHECKER(sequence_checker_);
 };
 
