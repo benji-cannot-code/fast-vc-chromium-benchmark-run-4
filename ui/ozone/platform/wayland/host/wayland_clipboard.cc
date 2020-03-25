0706da52eb2d37c11398b8fbbd9ee213b27609a5
Nick Diego Yamane <nickdiego@igalia.com>
2020-03-25 08:54:49 +0000

    ozone/wayland: Fix crashes when primary selection is not available
    Segfaults were observed when running under Wayland compositors that do
    not advertise primary selection protocol extension objects. For now,
    Ozone/Wayland supports only gtk_primary_selection version 1, which is
    likely not available in some compositors, such as, weston, sway, etc.
    This fixes the crashes by properly handling cases where the gtk primary
    selection protocol objects are not in place.
    
    R=msisov@igalia.com
    
    Bug: 1064410
    Change-Id: Ia90b423208e64919f717442309a4c92cdb6749a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2118237
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#753161}

--- ui/ozone/platform/wayland/host/wayland_clipboard.cc
+++ ui/ozone/platform/wayland/host/wayland_clipboard.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/host/wayland_clipboard.h"
 
+#include <string>
+
 #include "ui/ozone/platform/wayland/host/gtk_primary_selection_device.h"
 #include "ui/ozone/platform/wayland/host/gtk_primary_selection_device_manager.h"
 #include "ui/ozone/platform/wayland/host/gtk_primary_selection_source.h"
@@ -39,7 +41,7 @@ void WaylandClipboard::OfferClipboardData(
       clipboard_data_source_ = data_device_manager_->CreateSource();
     data_source = clipboard_data_source_.get();
   } else {
-    if (!primary_selection_device_manager_) {
+    if (!IsPrimarySelectionSupported()) {
       std::move(callback).Run();
       return;
     }
@@ -67,8 +69,10 @@ void WaylandClipboard::RequestClipboardData(
     if (!data_device_->RequestSelectionData(mime_type))
       SetData({}, mime_type);
   } else {
-    if (!primary_selection_device_->RequestSelectionData(mime_type))
+    if (!IsPrimarySelectionSupported() ||
+        !primary_selection_device_->RequestSelectionData(mime_type)) {
       SetData({}, mime_type);
+    }
   }
 }
 
@@ -92,8 +96,10 @@ void WaylandClipboard::GetAvailableMimeTypes(
   if (buffer == ClipboardBuffer::kCopyPaste) {
     std::move(callback).Run(data_device_->GetAvailableMimeTypes());
   } else {
-    DCHECK(primary_selection_device_);
-    std::move(callback).Run(primary_selection_device_->GetAvailableMimeTypes());
+    std::move(callback).Run(
+        IsPrimarySelectionSupported()
+            ? primary_selection_device_->GetAvailableMimeTypes()
+            : std::vector<std::string>{});
   }
 }
 
@@ -127,6 +133,10 @@ void WaylandClipboard::SetData(const std::vector<uint8_t>& contents,
 void WaylandClipboard::UpdateSequenceNumber(ClipboardBuffer buffer) {
   if (!update_sequence_cb_.is_null())
     update_sequence_cb_.Run(buffer);
+}
+
+bool WaylandClipboard::IsPrimarySelectionSupported() const {
+  return primary_selection_device_manager_ && primary_selection_device_;
 }
 
 }  // namespace ui
