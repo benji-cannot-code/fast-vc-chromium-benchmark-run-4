80a8a91789dbb9b100a1eba4b50a447a47e7a1f2
bttk <bttk@chromium.org>
2020-03-25 18:33:36 +0000

    Reland "metrics: Remove CachedMetrics"
    This reverts commit ccb4217d236524652dabb5b3f190d3bf7dff7688.
    
    Reason for revert: crrev.com/i/2797930 has rolled in with commit
    51b4afd9272fa9f4d0cd4fc81d1889ece942db37
    
    Original change's description:
    > Revert "metrics: Remove CachedMetrics"
    >
    > This reverts commit 1231840e8ab486c1af75241b415549eca4c66658.
    >
    > Reason for revert: Causing build failures in android-internal-chromium-tot
    > https://ci.chromium.org/p/chrome/builders/ci/android-internal-chromium-tot
    >
    > Original change's description:
    > > metrics: Remove CachedMetrics
    > >
    > > Bug: 1048429
    > > Change-Id: Ib5b5ee1d07492eb30b754320f6527acd5cda60f4
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079255
    > > Commit-Queue: bttk <bttk@chromium.org>
    > > Reviewed-by: Filip Gorski <fgorski@chromium.org>
    > > Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    > > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#752614}
    >
    > TBR=yfriedman@chromium.org,fgorski@chromium.org,pavely@chromium.org,bttk@chromium.org
    >
    > Change-Id: I3af9a0200e42e332c647ccb3d73f2cd756b0dc04
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1048429
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117153
    > Reviewed-by: Salvador Guerrero <salg@google.com>
    > Commit-Queue: Salvador Guerrero <salg@google.com>
    > Cr-Commit-Position: refs/heads/master@{#752641}
    
    TBR=yfriedman@chromium.org,fgorski@chromium.org,pavely@chromium.org,salg@google.com,bttk@chromium.org
    
    Change-Id: If85fd446d24b74452af55747ad432041e11afa2a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1046181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117056
    Reviewed-by: Salvador Guerrero <salg@google.com>
    Commit-Queue: bttk <bttk@chromium.org>
    Auto-Submit: bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753289}

--- base/android/library_loader/library_loader_hooks.cc
+++ base/android/library_loader/library_loader_hooks.cc
@@ -71,9 +71,6 @@ void RecordLibraryLoaderRendererHistograms() {
   UMA_HISTOGRAM_TIMES(
       "ChromiumAndroidLinker.RendererLoadTime",
       base::TimeDelta::FromMilliseconds(g_renderer_library_load_time_ms));
-
-  Java_LibraryLoader_onUmaRecordingReadyInRenderer(
-      base::android::AttachCurrentThread());
 }
 
 void SetLibraryLoadedHook(LibraryLoadedHook* func) {
--- chrome/android/java/src/org/chromium/chrome/browser/externalauth/UserRecoverableErrorHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalauth/UserRecoverableErrorHandler.java
@@ -13,7 +13,7 @@ import android.content.DialogInterface;
 import com.google.android.gms.common.GoogleApiAvailability;
 
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.metrics.CachedMetrics.ActionEvent;
+import org.chromium.base.metrics.RecordUserAction;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -41,13 +41,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * subclassing this class.
  */
 public abstract class UserRecoverableErrorHandler {
-
-    private static final ActionEvent sModalDialogShownActionEvent =
-            new ActionEvent("Signin_Android_GmsUserRecoverableDialogShown");
-
-    private static final ActionEvent sModalDialogAcceptedActionEvent =
-            new ActionEvent("Signin_Android_GmsUserRecoverableDialogAccepted");
-
     /**
      * Handles the specified error code from Google Play Services.
      * This method must only be called on the UI thread.
@@ -131,7 +124,7 @@ public abstract class UserRecoverableErrorHandler {
             public void onDismiss(DialogInterface dialogInterface) {
                 if (mCancelled) return;
                 // Dialog is being dismissed without being cancelled - user accepted dialog action.
-                sModalDialogAcceptedActionEvent.record();
+                RecordUserAction.record("Signin_Android_GmsUserRecoverableDialogAccepted");
             }
 
             public static void createAndAttachToDialog(Dialog dialog) {
@@ -201,7 +194,7 @@ public abstract class UserRecoverableErrorHandler {
             if (mDialog != null && !mDialog.isShowing()) {
                 mDialog.setCancelable(mCancelable);
                 mDialog.show();
-                sModalDialogShownActionEvent.record();
+                RecordUserAction.record("Signin_Android_GmsUserRecoverableDialogShown");
             }
         }
 
