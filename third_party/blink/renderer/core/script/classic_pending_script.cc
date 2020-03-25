375ddc54e9f7fc5b5de01b596507ce564ef18af1
Nate Chapin <japhet@chromium.org>
2020-03-25 16:08:33 +0000

    Fix crash in subresource integrity reporting on script load finish
    Bug: 1063628
    Test: fast/dom/HTMLScriptElement/reparent-to-new-document-integrity-report-crash.html
    Change-Id: If68846a2a95fd8e144a19b49d4efb6823b08f1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2118696
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753234}

--- third_party/blink/renderer/core/script/classic_pending_script.cc
+++ third_party/blink/renderer/core/script/classic_pending_script.cc
@@ -260,7 +260,7 @@ void ClassicPendingScript::NotifyFinished(Resource* resource) {
   ScriptElementBase* element = GetElement();
   if (element) {
     SubresourceIntegrityHelper::DoReport(
-        *element->GetDocument().ToExecutionContext(),
+        *element->GetDocument().GetExecutionContext(),
         GetResource()->IntegrityReportInfo());
 
     // It is possible to get back a script resource with integrity metadata
