d4abd3de5cbb40ad969af3819b00f1375bf9c3c2
Ewann <ewannpv@google.com>
2020-03-26 18:22:04 +0000

    [iOS] Adds Cookies UI to Incognito NTP
    This CL adds the new Block Third-Party Cookies UI to the
    incognito NTP.
    
    screenshots: https://drive.google.com/drive/folders/1Q5L3S1V5gjU-UyRMMnaVWi9XIGNN3tSA?usp=sharing
    
    Bug: 1063824
    Change-Id: Ia895b23e35614bfcbd1c33e87238b9043d6d1847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116221
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753686}

--- ios/chrome/browser/ui/ntp/incognito_view.mm
+++ ios/chrome/browser/ui/ntp/incognito_view.mm
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/google/core/common/google_util.h"
 #include "components/strings/grit/components_strings.h"
 #include "ios/chrome/browser/application_context.h"
+#import "ios/chrome/browser/ui/ntp/incognito_cookies_view.h"
+#import "ios/chrome/browser/ui/page_info/features.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_constants.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_utils.h"
 #include "ios/chrome/browser/ui/util/rtl_geometry.h"
@@ -178,6 +180,9 @@ NSAttributedString* FormatHTMLListForUILabel(NSString* listString) {
                        afterView:incognitoImageView];
 
     [self addTextSections];
+
+    if (base::FeatureList::IsEnabled(kPageInfoChromeGuard))
+      [self addCoockiesViewController];
 
     // |topGuide| and |bottomGuide| exist to vertically position the stackview
     // inside the container scrollview.
@@ -445,6 +450,11 @@ NSAttributedString* FormatHTMLListForUILabel(NSString* listString) {
          selector:@selector(contentSizeCategoryDidChange)
              name:UIContentSizeCategoryDidChangeNotification
            object:nil];
+}
+
+- (void)addCoockiesViewController {
+  IncognitoCookiesView* cookiesView = [[IncognitoCookiesView alloc] init];
+  [_stackView addArrangedSubview:cookiesView];
 }
 
 @end
