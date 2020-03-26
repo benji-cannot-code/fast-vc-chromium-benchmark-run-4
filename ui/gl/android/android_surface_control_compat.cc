a581d1f81103cb3f68ac5b26d995144e41239cf9
Peng Huang <penghuang@chromium.org>
2020-03-26 20:58:52 +0000

    Disable Android surface control on Android emulator
    The EGL fence cannot be created successfully on Android emulator.
    The EGL fence is need by Android surface control, so disable
    the Android surface control for emulator.
    
    Bug: 1064665
    Change-Id: Ib6624e40d772eeabcb9918250164644db8c79a86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2121472
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753762}

--- ui/gl/android/android_surface_control_compat.cc
+++ ui/gl/android/android_surface_control_compat.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/system/sys_info.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/gfx/color_space.h"
 
@@ -288,6 +289,12 @@ void OnTransactionCompletedOnAnyThread(void* context,
 bool SurfaceControl::IsSupported() {
   if (!base::android::BuildInfo::GetInstance()->is_at_least_q())
     return false;
+
+  // GLFence cannot be created successfully on emulator, and it is needed by
+  // Android surface control.
+  if (base::SysInfo::GetAndroidHardwareEGL() == "emulation")
+    return false;
+
   CHECK(SurfaceControlMethods::Get().supported);
   return true;
 }
