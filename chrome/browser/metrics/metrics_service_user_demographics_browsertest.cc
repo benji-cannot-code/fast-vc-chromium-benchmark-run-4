c683e58610e61968da007f05a1cc6426c26b2bcf
Tommy Nyquist <nyquist@chromium.org>
2020-03-26 22:53:26 +0000

    [android] Disable AddSyncedUserBirthYearAndGenderToProtoData
    Disable on Android since the test is very flaky, particularly on
    android-marshmallow-arm64-rel bot.
    
    Bug: 1016118
    Change-Id: Ie8f1a90a1c26fdc008746a19ef6337ee62661188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122866
    Auto-Submit: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753803}

--- chrome/browser/metrics/metrics_service_user_demographics_browsertest.cc
+++ chrome/browser/metrics/metrics_service_user_demographics_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "chrome/browser/metrics/chrome_metrics_services_manager_client.h"
@@ -116,8 +117,15 @@ class MetricsServiceUserDemographicsBrowserTest
 };
 
 // TODO(crbug/1016118): Add the remaining test cases.
+#if defined(OS_ANDROID)
+#define MAYBE_AddSyncedUserBirthYearAndGenderToProtoData \
+  DISABLED_AddSyncedUserBirthYearAndGenderToProtoData
+#else
+#define MAYBE_AddSyncedUserBirthYearAndGenderToProtoData \
+  AddSyncedUserBirthYearAndGenderToProtoData
+#endif
 IN_PROC_BROWSER_TEST_P(MetricsServiceUserDemographicsBrowserTest,
-                       AddSyncedUserBirthYearAndGenderToProtoData) {
+                       MAYBE_AddSyncedUserBirthYearAndGenderToProtoData) {
   test::DemographicsTestParams param = GetParam();
 
   base::HistogramTester histogram;
