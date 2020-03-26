0dd2a96986c7df9d26b559a3fc4a2f192f0139af
Ahmed Mehfooz <amehfooz@chromium.org>
2020-03-26 22:33:31 +0000

    Add feature flag for mic gain settings in system tray
    Bug: 1065079
    Change-Id: I0646096217a8cdef1715061cb0b60f440c4a6240
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122862
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753793}

--- ash/public/cpp/ash_features.cc
+++ ash/public/cpp/ash_features.cc
@@ -117,6 +117,9 @@ const base::Feature kDragFromShelfToHomeOrOverview{
 const base::Feature kHideShelfControlsInTabletMode{
     "HideShelfControlsInTabletMode", base::FEATURE_ENABLED_BY_DEFAULT};
 
+const base::Feature kSystemTrayMicGainSetting{
+    "SystemTrayMicGainSetting", base::FEATURE_DISABLED_BY_DEFAULT};
+
 bool IsAllowAmbientEQEnabled() {
   return base::FeatureList::IsEnabled(kAllowAmbientEQ);
 }
@@ -244,6 +247,10 @@ bool AreContextualNudgesEnabled() {
 
 bool IsCornerShortcutsEnabled() {
   return base::FeatureList::IsEnabled(kCornerShortcuts);
+}
+
+bool IsSystemTrayMicGainSettingEnabled() {
+  return base::FeatureList::IsEnabled(kSystemTrayMicGainSetting);
 }
 
 namespace {
