3d4f8c360515753e556f3ea2164659fa3cce0c55
Nate Fischer <ntfschr@chromium.org>
2020-03-26 19:09:08 +0000

    AW: migrate Dev UI to bottom nav bar
    This swaps Dev UI's navigation menu for a bottom navigation bar. The
    bottom nav bar is a more intuitive and discoverable UI element, so this
    should improve user experience. This puts "home," "crashes," and "flags"
    in the bottom nav bar (the most prominent tools) and leaves changing
    WebView provider for the top menu (on N+).
    
    I built my own bottom navigation bar because the standard library
    (BottomNavigationView) depends on AppCompat, which was causing crashes
    on L-M and other problems (for standalone WebView) on N+. The custom
    navigation bar is built out of TextViews with compound drawables.
    
    This adds new icon resources from Google's material design library.
    
    No change to the actual Dev UI tools themselves.
    
    Bug: 1017532
    Test: Manual - verify on L and N+
    Test: Manual - verify dark mode on Q
    Change-Id: I235cd9ba0557631a12a6132acd0b1b705227965b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2113799
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753714}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/devui/util/NavigationMenuHelper.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/devui/util/NavigationMenuHelper.java
@@ -4,44 +4,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.android_webview.devui.util;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import org.chromium.android_webview.devui.CrashesListFragment;
-import org.chromium.android_webview.devui.FlagsFragment;
-import org.chromium.android_webview.devui.HomeFragment;
 import org.chromium.android_webview.devui.R;
 
 /**
- * Helper class for navigation menu between activities.
- *
- * TODO(crbug.com/1017532) should be replaced with a navigation drawer.
+ * Helper class for menu to access external tools. Built-in tools should be handled by Fragments in
+ * the MainActivity.
  */
 public final class NavigationMenuHelper {
     /**
      * Inflate the navigation menu in the given {@code activity} options menu.
      *
-     * This should be called inside {@link android.app.Activity#onCreateOptionsMenu} or
-     * {@link Fragment#onCreateOptionsMenu}.
+     * This should be called inside {@link Activity#onCreateOptionsMenu} or
+     * {@link android.support.v4.app.Fragment#onCreateOptionsMenu}.
      */
-    public static void inflate(FragmentActivity activity, Menu menu) {
-        activity.getMenuInflater().inflate(R.menu.navigation_menu, menu);
-
-        // Switching WebView providers is possible only from API >= 24.
+    public static void inflate(Activity activity, Menu menu) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            MenuItem item = menu.findItem(R.id.nav_menu_switch_provider);
-            item.setVisible(false);
-            // No need to call Activity#invalidateOptionsMenu() since this method should be called
-            // inside Activity#onCreateOptionsMenu().
+            return;
         }
+        activity.getMenuInflater().inflate(R.menu.navigation_menu, menu);
     }
 
     /**
@@ -51,25 +40,12 @@ public final class NavigationMenuHelper {
      * @return {@code true} if the item selection event is consumed.
      */
     public static boolean onOptionsItemSelected(FragmentActivity activity, MenuItem item) {
-        Fragment fragment = null;
-        if (item.getItemId() == R.id.nav_menu_crash_ui) {
-            fragment = new CrashesListFragment();
-        } else if (item.getItemId() == R.id.nav_menu_flags_ui) {
-            fragment = new FlagsFragment();
-        } else if (item.getItemId() == R.id.nav_menu_main_ui) {
-            fragment = new HomeFragment();
-        } else if (item.getItemId() == R.id.nav_menu_switch_provider
+        if (item.getItemId() == R.id.nav_menu_switch_provider
                 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             activity.startActivity(new Intent(Settings.ACTION_WEBVIEW_SETTINGS));
             return true;
         }
-        if (fragment == null) return false;
-
-        FragmentManager fm = activity.getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.replace(R.id.content_fragment, fragment);
-        transaction.commit();
-        return true;
+        return false;
     }
 
     // Do not instantiate this class.
