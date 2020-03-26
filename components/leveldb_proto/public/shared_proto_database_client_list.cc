88e85782fd4a35b047ca7c311218615cce357efa
David Maunder <davidjm@chromium.org>
2020-03-26 00:51:28 +0000

    Implement Level DB Database for NonCriticalPersistedTabData.
    NonCriticalPersistedTabData is for metadata
    to build new features where the data needs
    to be persisted across restarts. However,
    the metadata is not pertinent to the running
    of the app as CriticalPersistedTabData
    (tab id, web contents etc.) is.
    
    Bug: 1048712
    Change-Id: Ibcaa202322746d69065546fc3f7ea1d66b443ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2036656
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753458}

--- components/leveldb_proto/public/shared_proto_database_client_list.cc
+++ components/leveldb_proto/public/shared_proto_database_client_list.cc
@@ -80,6 +80,8 @@ std::string SharedProtoDatabaseClientList::ProtoDbTypeToString(
       return "PrintJobDatabase";
     case ProtoDbType::FEED_STREAM_DATABASE:
       return "FeedStreamDatabase";
+    case ProtoDbType::TAB_STATE_DATABASE:
+      return "TabStateDatabase";
     case ProtoDbType::LAST:
       NOTREACHED();
       return std::string();
