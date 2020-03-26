e8570306dc81959c8a25b96ba1646ec48430c603
edchin <edchin@chromium.org>
2020-03-26 19:30:08 +0000

    NULL to nullptr in ManagedBookmarksPolicyHandler
    Change-Id: I7d44615cd725effa9834ba597191c749b9dc0848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122367
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753724}

--- chrome/browser/policy/managed_bookmarks_policy_handler_unittest.cc
+++ chrome/browser/policy/managed_bookmarks_policy_handler_unittest.cc
@@ -36,7 +36,7 @@ class ManagedBookmarksPolicyHandlerTest
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 TEST_F(ManagedBookmarksPolicyHandlerTest, ApplyPolicySettings) {
-  EXPECT_FALSE(store_->GetValue(bookmarks::prefs::kManagedBookmarks, NULL));
+  EXPECT_FALSE(store_->GetValue(bookmarks::prefs::kManagedBookmarks, nullptr));
 
   PolicyMap policy;
   policy.Set(
@@ -81,13 +81,13 @@ TEST_F(ManagedBookmarksPolicyHandlerTest, ApplyPolicySettings) {
                                        "]"),
       nullptr);
   UpdateProviderPolicy(policy);
-  const base::Value* pref_value = NULL;
+  const base::Value* pref_value = nullptr;
   EXPECT_TRUE(
       store_->GetValue(bookmarks::prefs::kManagedBookmarks, &pref_value));
   ASSERT_TRUE(pref_value);
 
   // Make sure the kManagedBookmarksFolderName pref is set correctly.
-  const base::Value* folder_value = NULL;
+  const base::Value* folder_value = nullptr;
   std::string folder_name;
   EXPECT_TRUE(store_->GetValue(bookmarks::prefs::kManagedBookmarksFolderName,
                                &folder_value));
@@ -143,7 +143,7 @@ TEST_F(ManagedBookmarksPolicyHandlerTest, ApplyPolicySettings) {
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 TEST_F(ManagedBookmarksPolicyHandlerTest, ApplyPolicySettingsNoTitle) {
-  EXPECT_FALSE(store_->GetValue(bookmarks::prefs::kManagedBookmarks, NULL));
+  EXPECT_FALSE(store_->GetValue(bookmarks::prefs::kManagedBookmarks, nullptr));
 
   PolicyMap policy;
   policy.Set(key::kManagedBookmarks, POLICY_LEVEL_MANDATORY, POLICY_SCOPE_USER,
@@ -156,13 +156,13 @@ TEST_F(ManagedBookmarksPolicyHandlerTest, ApplyPolicySettingsNoTitle) {
                                               "]"),
              nullptr);
   UpdateProviderPolicy(policy);
-  const base::Value* pref_value = NULL;
+  const base::Value* pref_value = nullptr;
   EXPECT_TRUE(
       store_->GetValue(bookmarks::prefs::kManagedBookmarks, &pref_value));
   ASSERT_TRUE(pref_value);
 
   // Make sure the kManagedBookmarksFolderName pref is set correctly.
-  const base::Value* folder_value = NULL;
+  const base::Value* folder_value = nullptr;
   std::string folder_name;
   EXPECT_TRUE(store_->GetValue(bookmarks::prefs::kManagedBookmarksFolderName,
                                &folder_value));
@@ -195,7 +195,7 @@ TEST_F(ManagedBookmarksPolicyHandlerTest, WrongPolicyType) {
                                            "]"),
              nullptr);
   UpdateProviderPolicy(policy);
-  EXPECT_FALSE(store_->GetValue(bookmarks::prefs::kManagedBookmarks, NULL));
+  EXPECT_FALSE(store_->GetValue(bookmarks::prefs::kManagedBookmarks, nullptr));
 }
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
@@ -213,7 +213,7 @@ TEST_F(ManagedBookmarksPolicyHandlerTest, UnknownKeys) {
                                        "]"),
       nullptr);
   UpdateProviderPolicy(policy);
-  const base::Value* pref_value = NULL;
+  const base::Value* pref_value = nullptr;
   EXPECT_TRUE(
       store_->GetValue(bookmarks::prefs::kManagedBookmarks, &pref_value));
   ASSERT_TRUE(pref_value);
