3ce2eac748c722a27f09bba60a535e3559f2a334
Clemens Arbesser <arbesser@google.com>
2020-03-26 15:07:56 +0000

    [Autofill Assistant] Split java events from native events.
    The groundwork for this was already done a while back. This CL splits
    the creation of interaction events into two independent parts:
      native -> java, with dynamic resolution of views by identifier
      java -> native, with static resolution at time of creation
    
    This has the advantage that all native->java interactions can be created at any time, in particular, before or during views creation. This is in preparation for implicit view interactions, i.e., interactions that are implicitly defined as part of a view definition and which need to be created during view creation.
    
    This is a refactoring only and should not contain user-facing changes.
    
    Bug: b/145043394
    Change-Id: I19fe9e320e8fd7336102ed017eeb730a24a021f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119860
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Cr-Commit-Position: refs/heads/master@{#753614}

--- chrome/browser/android/autofill_assistant/generic_ui_events_android.h
+++ chrome/browser/android/autofill_assistant/generic_ui_events_android.h
@@ -15,11 +15,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill_assistant {
 namespace android_events {
 
-// Sets a click listener for |jview|.
-void SetOnClickListener(JNIEnv* env,
-                        base::android::ScopedJavaGlobalRef<jobject> jview,
-                        base::android::ScopedJavaGlobalRef<jobject> jdelegate,
-                        const OnViewClickedEventProto& proto);
+// Creates java listeners for all view events in |proto| such that |jdelegate|
+// is notified when appropriate (e.g., OnClickListeners). Returns true on
+// success, false on failure.
+bool CreateJavaListenersFromProto(
+    JNIEnv* env,
+    std::map<std::string, base::android::ScopedJavaGlobalRef<jobject>>* views,
+    base::android::ScopedJavaGlobalRef<jobject> jdelegate,
+    const InteractionsProto& proto);
 
 }  // namespace android_events
 }  // namespace autofill_assistant
