212b609754434f11ce39df87b51d445b2756c1a2
rbpotter <rbpotter@chromium.org>
2020-03-26 19:18:54 +0000

    Settings: Migrate site details to Polymer 3
    Bug: 1026426
    Change-Id: I1c9ab419fd1c02d9a8faedde28ecc77fcd536def
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117343
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753717}

--- chrome/test/data/webui/settings/site_details_tests.js
+++ chrome/test/data/webui/settings/site_details_tests.js
@@ -3,6 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// clang-format off
+// #import {ChooserType,ContentSetting,ContentSettingsTypes,SiteSettingSource,SiteSettingsPrefsBrowserProxyImpl,WebsiteUsageBrowserProxyImpl} from 'chrome://settings/lazy_load.js';
+// #import {createContentSettingTypeToValuePair,createRawChooserException,createRawSiteException,createSiteSettingsPrefs} from 'chrome://test/settings/test_util.m.js';
+// #import {flush,Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// #import {isChromeOS} from 'chrome://resources/js/cr.m.js';
+// #import {listenOnce} from 'chrome://resources/js/util.m.js';
+// #import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
+// #import {Route,Router,routes} from 'chrome://settings/settings.js';
+// #import {TestBrowserProxy} from 'chrome://test/test_browser_proxy.m.js';
+// #import {TestSiteSettingsPrefsBrowserProxy} from 'chrome://test/settings/test_site_settings_prefs_browser_proxy.m.js';
+// clang-format on
+
 class TestWebsiteUsageBrowserProxy extends TestBrowserProxy {
   constructor() {
     super(['clearUsage', 'fetchUsageTotal']);
@@ -230,7 +242,7 @@ suite('SiteDetails', function() {
     browserProxy.setPrefs(prefs);
 
     // First, explicitly set all the optional settings to false.
-    for (contentSetting in optionalSiteDetailsContentSettingsTypes) {
+    for (const contentSetting in optionalSiteDetailsContentSettingsTypes) {
       const loadTimeDataOverride = {};
       loadTimeDataOverride
           [optionalSiteDetailsContentSettingsTypes[contentSetting]] = false;
@@ -239,7 +251,7 @@ suite('SiteDetails', function() {
 
     // Iterate over each flag in on / off state, assuming that the on state
     // means the content setting will show, and off hides it.
-    for (contentSetting in optionalSiteDetailsContentSettingsTypes) {
+    for (const contentSetting in optionalSiteDetailsContentSettingsTypes) {
       const numContentSettings =
           Object.keys(settings.ContentSettingsTypes).length -
           nonSiteDetailsContentSettingsTypes.length -
@@ -270,7 +282,7 @@ suite('SiteDetails', function() {
     browserProxy.setPrefs(prefs);
     testElement = createSiteDetails('https://foo.com:443');
     Polymer.dom.flush();
-    assert(!!testElement.$$('#usage'));
+    assertTrue(!!testElement.$$('#usage'));
 
     // When there's no usage, there should be a string that says so.
     assertEquals('', testElement.storedData_);
