5d8d0ff9020739628b2395625f1597d5310a5e2f
Alex Ilin <alexilin@chromium.org>
2020-03-26 10:36:01 +0000

    [Signin] Move Reauth WebContents observer into a separate tab helper
    This CL introduces a new class, ReauthTabHelper, that obsevers
    navigations within the reauth flow. It's done so the tab helper can be
    reused for reauth modal dialog.
    
    Bug: 1045515
    Change-Id: I843f8846aefcc8d67ecc702ba385e166d6ad1af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2112910
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753567}

--- chrome/browser/ui/signin_reauth_popup_delegate.h
+++ chrome/browser/ui/signin_reauth_popup_delegate.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/signin_view_controller_delegate.h"
 #include "content/public/browser/web_contents_observer.h"
-#include "url/gurl.h"
 
 class SigninViewController;
 class Browser;
@@ -36,21 +35,16 @@ class SigninReauthPopupDelegate : public SigninViewControllerDelegate,
   content::WebContents* GetWebContents() override;
 
   // content::WebContentsObserver:
-  void DidFinishNavigation(
-      content::NavigationHandle* navigation_handle) override;
   void WebContentsDestroyed() override;
 
  private:
   void CompleteReauth(signin::ReauthResult result);
   void CloseWebContents();
 
-  const GURL& reauth_url() const;
-
   SigninViewController* signin_view_controller_;
   Browser* const browser_;
   base::OnceCallback<void(signin::ReauthResult)> reauth_callback_;
   content::WebContents* web_contents_;
-  GURL reauth_url_;
 
   base::WeakPtrFactory<SigninReauthPopupDelegate> weak_ptr_factory_{this};
 };
