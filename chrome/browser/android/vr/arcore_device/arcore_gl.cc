1f6427772e34cc1bca6bfb9757a14cee773f3a9c
Piotr Bialecki <bialpio@chromium.org>
2020-03-26 19:05:37 +0000

    WebXR - planes+anchors - Add DVLOGs to aid investigations
    Changes:
    - Pass tracking state to the functions accepted by
      ForEachArCoreAnchor and ForEachArCorePlane in anchor manager and
      plane manager.
    - Add test page for experiments with local space stability & drift,
      the page will not be exposed via proposals/index.html
    
    There should be no functional change introduced by this CL.
    
    Change-Id: Ib325c090f165f306865701987e89c6ad45db5a47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2118990
    Reviewed-by: Klaus Weidner <klausw@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753710}

--- chrome/browser/android/vr/arcore_device/arcore_gl.cc
+++ chrome/browser/android/vr/arcore_device/arcore_gl.cc
@@ -427,6 +427,10 @@ void ArCoreGl::GetFrameData(
   }
 
   // Create the frame data to return to the renderer.
+  if (!pose) {
+    DVLOG(1) << __func__ << ": pose unavailable!";
+  }
+
   frame_data->pose = std::move(pose);
   frame_data->time_delta = now - base::TimeTicks();
 
@@ -829,7 +833,8 @@ void ArCoreGl::ProcessFrame(
     mojom::XRFrameDataRequestOptionsPtr options,
     mojom::XRFrameDataPtr frame_data,
     mojom::XRFrameDataProvider::GetFrameDataCallback callback) {
-  DVLOG(3) << __func__ << " frame=" << frame_data->frame_id;
+  DVLOG(3) << __func__ << " frame=" << frame_data->frame_id << ", pose valid? "
+           << (frame_data->pose ? true : false);
 
   DCHECK(IsOnGlThread());
   DCHECK(is_initialized_);
--- third_party/blink/renderer/modules/xr/xr_frame.cc
+++ third_party/blink/renderer/modules/xr/xr_frame.cc
@@ -43,6 +43,7 @@ XRFrame::XRFrame(XRSession* session, XRWorldInformation* world_information)
 
 XRViewerPose* XRFrame::getViewerPose(XRReferenceSpace* reference_space,
                                      ExceptionState& exception_state) const {
+  DVLOG(3) << __func__;
   if (!is_active_) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidStateError,
                                       kInactiveFrame);
@@ -56,6 +57,7 @@ XRViewerPose* XRFrame::getViewerPose(XRReferenceSpace* reference_space,
   }
 
   if (!reference_space) {
+    DVLOG(1) << __func__ << ": reference space not present, returning null";
     return nullptr;
   }
 
@@ -78,6 +80,10 @@ XRViewerPose* XRFrame::getViewerPose(XRReferenceSpace* reference_space,
 
   // Can only update an XRViewerPose's views with an invertible matrix.
   if (!(offset_space_from_viewer && offset_space_from_viewer->IsInvertible())) {
+    DVLOG(1) << __func__
+             << ": offset_space_from_viewer is invalid or not invertible - "
+                "returning nullptr, offset_space_from_viewer valid? "
+             << (offset_space_from_viewer ? true : false);
     return nullptr;
   }
 
