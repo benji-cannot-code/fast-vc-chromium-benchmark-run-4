9bd8cc88c9857311394090aae07b825af4a342f3
Robbie Gibson <rkgibson@google.com>
2020-03-26 15:59:27 +0000

    [iOS] Persist Google AMP pages text zoom settings separately
    As Text Zoom uses the URL host as its persistence key, Google AMP urls
    are all persisted under "www.google.com," which is not ideal. To fix
    this, if the url is an AMP url, identified using the heuristic that all
    AMP urls are www.<google domain>.com/amp/, the persistence key is
    www.google.com/amp.
    
    Also, when navigating to an AMP page from Google Search/News, no actual
    navigation takes place. Instead, the page is presented using Javascript.
    To make sure that the newly "loaded" page has the correct zoom level,
    the zoom level is set in DidFinishNavigation if the page navigated to
    is a Google AMP page.
    
    Bug: 1063836
    Change-Id: I2282cbacf938eaaf0f7d03925dbe9e60b8fee213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119541
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#753625}

--- ios/chrome/browser/web/font_size_tab_helper.h
+++ ios/chrome/browser/web/font_size_tab_helper.h
@@ -77,15 +77,21 @@ class FontSizeTabHelper : public web::WebStateObserver,
   // 150%) taking all sources into account (system level and user zoom).
   int GetFontSize() const;
 
+  // Set the zoom level correctly for a new navigation.
+  void NewPageZoom();
+
   PrefService* GetPrefService() const;
 
+  // Returns the new multiplier after zooming in the given direction. Returns
+  // nullopt if it is impossible to zoom in the given direction;
   base::Optional<double> NewMultiplierAfterZoom(Zoom zoom) const;
   // Returns the current user zoom multiplier (i.e. not counting any additional
   // zoom due to the system accessibility settings).
   double GetCurrentUserZoomMultiplier() const;
   void StoreCurrentUserZoomMultiplier(double multiplier);
   std::string GetCurrentUserZoomMultiplierKey() const;
-
+  std::string GetUserZoomMultiplierKeyUrlPart() const;
+  bool IsGoogleCachedAMPPage() const;
   bool tab_helper_has_zoomed_ = false;
 
   // web::WebStateObserver overrides:
@@ -93,6 +99,10 @@ class FontSizeTabHelper : public web::WebStateObserver,
       web::WebState* web_state,
       web::PageLoadCompletionStatus load_completion_status) override;
   void WebStateDestroyed(web::WebState* web_state) override;
+  void DidFinishNavigation(web::WebState* web_state,
+                           web::NavigationContext* context) override;
+  void WebFrameDidBecomeAvailable(web::WebState* web_state,
+                                  web::WebFrame* web_frame) override;
 
   // Observer id returned by registering at NSNotificationCenter.
   id content_size_did_change_observer_ = nil;
