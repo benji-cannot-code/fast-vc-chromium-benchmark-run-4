e8838946bbb52907730467eb94593086e1932d62
Artur Kochowski <akochowski@opera.com>
2020-03-26 09:18:21 +0000

    Added OnThemeChanged override to CookieInfoView
    Bug: 1064209
    Change-Id: I08ba090dfbb97efa097e8d574b99a66eaaf00f88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117111
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Artur Kochowski <akochowski@opera.com>
    Cr-Commit-Position: refs/heads/master@{#753553}

--- chrome/browser/ui/views/cookie_info_view.h
+++ chrome/browser/ui/views/cookie_info_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "base/compiler_specific.h"
@@ -44,20 +45,27 @@ class CookieInfoView : public views::ScrollView {
   // Enables or disables the cookie property text fields.
   void EnableCookieDisplay(bool enabled);
 
- private:
-  // Layout helper routines.
-  views::Textfield* AddLabelRow(int layout_id,
-                                views::GridLayout* layout,
-                                int label_message_id);
+  void OnThemeChanged() override;
 
-  // Individual property labels
-  views::Textfield* name_value_field_ = nullptr;
-  views::Textfield* content_value_field_ = nullptr;
-  views::Textfield* domain_value_field_ = nullptr;
-  views::Textfield* path_value_field_ = nullptr;
-  views::Textfield* send_for_value_field_ = nullptr;
-  views::Textfield* created_value_field_ = nullptr;
-  views::Textfield* expires_value_field_ = nullptr;
+ private:
+  enum class CookieProperty {
+    kName,
+    kContent,
+    kDomain,
+    kPath,
+    kSendFor,
+    kCreated,
+    kExpires,
+  };
+
+  // Layout helper routines.
+  views::Textfield* AddTextfieldRow(int layout_id,
+                                    views::GridLayout* layout,
+                                    int label_message_id);
+
+  void SetTextfieldColors();
+
+  std::unordered_map<CookieProperty, views::Textfield*> property_textfields_;
 
   DISALLOW_COPY_AND_ASSIGN(CookieInfoView);
 };
