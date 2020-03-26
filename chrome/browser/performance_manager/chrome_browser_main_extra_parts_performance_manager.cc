e13c17ea020eea6e251bcba42eea9f24b5731278
Hailey Wang <haileywang@google.com>
2020-03-26 17:02:50 +0000

    [PM] Integrate BackgroundTabLoadingPolicy
    Add BackgroundTabLoadingPolicy as an optional feature in Chrome.
    
    Replaces the initialization of TabLoader in SessionRestore when enabled.
    
    Bug: 1059341
    Change-Id: I00437561afe07fd0db50caec667f1bb488e47cca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111577
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753650}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/performance_manager/decorators/helpers/page_live_state_decorator_helper.h"
 #include "chrome/browser/performance_manager/decorators/page_aggregator.h"
 #include "chrome/browser/performance_manager/decorators/process_metrics_decorator.h"
+#include "chrome/browser/performance_manager/graph/policies/background_tab_loading_policy.h"
 #include "chrome/browser/performance_manager/graph/policies/high_pmf_memory_pressure_policy.h"
 #include "chrome/browser/performance_manager/graph/policies/policy_features.h"
 #include "chrome/browser/performance_manager/graph/policies/urgent_page_discarding_policy.h"
@@ -106,6 +107,14 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
     graph->PassToGraph(
         std::make_unique<
             performance_manager::policies::UrgentPageDiscardingPolicy>());
+  }
+
+  if (base::FeatureList::IsEnabled(
+          performance_manager::features::
+              kBackgroundTabLoadingFromPerformanceManager)) {
+    graph->PassToGraph(
+        std::make_unique<
+            performance_manager::policies::BackgroundTabLoadingPolicy>());
   }
 #endif  // !defined(OS_ANDROID)
 
--- chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_unittest.cc
+++ chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_unittest.cc
@@ -63,7 +63,7 @@ class BackgroundTabLoadingPolicyTest : public GraphTestHarness {
   MockPageLoader* mock_loader_;
 };
 
-TEST_F(BackgroundTabLoadingPolicyTest, RestoreTabs) {
+TEST_F(BackgroundTabLoadingPolicyTest, ScheduleLoadForRestoredTabs) {
   std::vector<
       performance_manager::TestNodeWrapper<performance_manager::PageNodeImpl>>
       page_nodes;
@@ -76,11 +76,11 @@ TEST_F(BackgroundTabLoadingPolicyTest, RestoreTabs) {
     EXPECT_CALL(*loader(), LoadPageNode(raw_page_nodes.back()));
 
     // Set |is_tab| property as this is a requirement to pass the PageNode to
-    // RestoreTabs().
+    // ScheduleLoadForRestoredTabs().
     TabPropertiesDecorator::SetIsTabForTesting(raw_page_nodes.back(), true);
   }
 
-  policy()->RestoreTabs(raw_page_nodes);
+  policy()->ScheduleLoadForRestoredTabs(raw_page_nodes);
 }
 
 }  // namespace policies
