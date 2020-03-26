17f373720cd8d3424e77cdf5778df7ab193ca8d5
Lijin Shen <lazzzis@google.com>
2020-03-26 18:29:50 +0000

    Hide "offer to translate" option when it is disabled.
    When the switch option is toggled, the popup menu is not updated. Add a
    new method to notify the adapter of data set changes.
    
    Bug: 1064192
    Change-Id: I638f37157fb8966c200af098589290cde9574c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116925
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#753693}

--- chrome/android/java/src/org/chromium/chrome/browser/language/settings/LanguageListPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/language/settings/LanguageListPreference.java
@@ -179,4 +179,11 @@ public class LanguageListPreference extends Preference {
     void registerActivityLauncher(AddLanguageFragment.Launcher launcher) {
         mLauncher = launcher;
     }
+
+    /**
+     * Notify LanguageListAdapter of pref changes to update list items.
+     */
+    void notifyPrefChanged() {
+        mAdapter.onDataUpdated();
+    }
 }
