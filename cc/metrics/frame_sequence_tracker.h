f6b9304e84c7d1f9db35b460f43e8c2a21fde8c3
Xida Chen <xidachen@chromium.org>
2020-03-26 15:56:22 +0000

    Revert "[throughput] Track last frame"
    This reverts commit fd2cb104b06565a2132877e7336a19f5be44215c.
    
    Reason for revert: causing crash
    
    Original change's description:
    > [throughput] Track last frame
    > 
    > It could happen that after a BeginImpl(Main)Frame is reported, a tracker
    > is scheduled to terminate. In this case, any subsequent handling of
    > that impl(main)-frame will be lost. In other words, we completely lost
    > track of the last frame.
    > 
    > This CL fixes the impl frame, the fix for main frame comes later because
    > this CL is already big.
    > 
    > Bug: 1055844
    > Change-Id: I1cc65e9a5d973ecb67c83105be03f7bc08139dae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2079134
    > Commit-Queue: Xida Chen <xidachen@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#752529}
    
    TBR=flackr@chromium.org,xidachen@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1055844
    Change-Id: I5c96139dd4eb8bd38d2133a1b9d8d4d69bacff18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122041
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753621}

--- cc/metrics/frame_sequence_tracker.h
+++ cc/metrics/frame_sequence_tracker.h
@@ -193,8 +193,6 @@ class CC_EXPORT FrameSequenceTrackerCollection {
   ActiveFrameSequenceTrackers FrameSequenceTrackerActiveTypes();
 
   FrameSequenceTracker* GetTrackerForTesting(FrameSequenceTrackerType type);
-  FrameSequenceTracker* GetRemovalTrackerForTesting(
-      FrameSequenceTrackerType type);
 
   void SetUkmManager(UkmManager* manager);
 
@@ -202,8 +200,6 @@ class CC_EXPORT FrameSequenceTrackerCollection {
   friend class FrameSequenceTrackerTest;
 
   void RecreateTrackers(const viz::BeginFrameArgs& args);
-  // Destroy the trackers that are ready to be terminated.
-  void DestroyTrackers();
 
   const bool is_single_threaded_;
   // The callsite can use the type to manipulate the tracker.
@@ -232,9 +228,9 @@ class CC_EXPORT FrameSequenceTrackerCollection {
 class CC_EXPORT FrameSequenceTracker {
  public:
   enum class TerminationStatus {
-    kActive = 0,
-    kScheduledForTermination = 1,
-    kReadyForTermination = 2,
+    kActive,
+    kScheduledForTermination,
+    kReadyForTermination,
   };
 
   static const char* GetFrameSequenceTrackerTypeName(
@@ -440,11 +436,9 @@ class CC_EXPORT FrameSequenceTracker {
     void Advance(base::TimeTicks new_timestamp);
   } trace_data_;
 
-  // True when an impl-impl is not ended. A tracker is ready for termination
-  // only when the last impl-frame is ended (ReportFrameEnd).
+#if DCHECK_IS_ON()
   bool is_inside_frame_ = false;
 
-#if DCHECK_IS_ON()
   // This stringstream represents a sequence of frame reporting activities on
   // the current tracker. Each letter can be one of the following:
   // {'B', 'N', 'b', 'n', 'S', 'P'}, where
