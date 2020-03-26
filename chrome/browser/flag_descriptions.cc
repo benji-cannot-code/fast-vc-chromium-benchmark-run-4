cd9a5dd63cf4500f569df0ea96d574f7ded745ff
Sophey Dong <sophey@chromium.org>
2020-03-26 23:31:04 +0000

    [SendTabToSelf] Add flag for displaying sending animation in omnibar.
    We are removing the Desktop OS notifications and instead displaying the
    sending animation that is present currently when self-sharing from the
    omnibar icon.
    
    Bug: 1007536
    Change-Id: I44b47e24c14946d1c633ff69abfb07948f07d9a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2111775
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Reviewed-by: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753817}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1646,6 +1646,12 @@ const char kSendTabToSelfBroadcastDescription[] =
     "Allows users to broadcast the tab they send to all of their devices "
     "instead of targetting only one device.";
 
+const char kSendTabToSelfOmniboxSendingAnimationName[] =
+    "Send tab to self omnibox sending animation";
+const char kSendTabToSelfOmniboxSendingAnimationDescription[] =
+    "If enabled, shows Sending... animation in omnibox instead of Desktop OS "
+    "notifications for contextual menu entry points.";
+
 const char kSendTabToSelfWhenSignedInName[] =
     "Send tab to self: enable use when signed-in regardless of sync state";
 const char kSendTabToSelfWhenSignedInDescription[] =
