365c7d7acb93674459e2b4e816d14882b2a7ae07
Brandon Wylie <wylieb@chromium.org>
2020-03-27 01:49:18 +0000

    Fix flakiness in LocationBarTest'is voice search tests
    Bug: 1062844
    Change-Id: I23f94d41cedfc35879130aed14c1c4d1f7599fee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2121500
    Commit-Queue: Brandon Wylie <wylieb@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753872}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
@@ -6,9 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.omnibox;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
+
+import static org.hamcrest.Matchers.not;
 
 import android.support.test.filters.SmallTest;
 import android.view.View;
@@ -165,10 +170,11 @@ public class LocationBarLayoutTest {
     }
 
     private void setUrlBarTextAndFocus(String text) throws ExecutionException {
+        onView(withId(R.id.url_bar)).perform(click());
+
         TestThreadUtils.runOnUiThreadBlocking(new Callable<Void>() {
             @Override
             public Void call() throws InterruptedException {
-                getLocationBar().onUrlFocusChange(true);
                 mActivityTestRule.typeInOmnibox(text, false);
                 return null;
             }
@@ -179,20 +185,22 @@ public class LocationBarLayoutTest {
     @SmallTest
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     public void testNotShowingVoiceSearchButtonIfUrlBarContainsText() throws ExecutionException {
+        // When there is text, the delete button should be visible.
         setUrlBarTextAndFocus("testing");
 
-        Assert.assertEquals(getDeleteButton().getVisibility(), VISIBLE);
-        Assert.assertNotEquals(getMicButton().getVisibility(), VISIBLE);
+        onView(withId(R.id.delete_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.mic_button)).check(matches(not(isDisplayed())));
     }
 
     @Test
     @SmallTest
     @Restriction(UiRestriction.RESTRICTION_TYPE_PHONE)
     public void testShowingVoiceSearchButtonIfUrlBarIsEmpty() throws ExecutionException {
+        // When there's no text, the mic button should be visible.
         setUrlBarTextAndFocus("");
 
-        Assert.assertNotEquals(getDeleteButton().getVisibility(), VISIBLE);
-        Assert.assertEquals(getMicButton().getVisibility(), VISIBLE);
+        onView(withId(R.id.mic_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.delete_button)).check(matches(not(isDisplayed())));
     }
 
     @Test
