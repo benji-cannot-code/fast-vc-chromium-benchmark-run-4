768a16224732e8270923a0df1eb594662c3abf41
Matt Jones <mdjones@chromium.org>
2020-03-27 23:51:53 +0000

    Ensure old scrim hides before showing the new one
    Previously, if another feature tried using the scrim while the old
    one was still visible, the first user's would not be hidden correctly.
    This patch ensures the previous scrim is replaced before showing the
    new one and adds a test for this behavior.
    
    Bug: 1065370
    Change-Id: Iaa0c873a519d41e02b6ab98b87c84a89da06fe0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124983
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754226}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/scrim/ScrimMediator.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/scrim/ScrimMediator.java
@@ -186,6 +186,11 @@ class ScrimMediator implements View.OnClickListener, ScrimCoordinator.TouchEvent
         mOverlayAnimator.start();
     }
 
+    /** @return Whether the scrim is still active (has a non-null model). */
+    boolean isActive() {
+        return mModel != null;
+    }
+
     @Override
     public void onClick(View view) {
         if (mModel.get(ScrimProperties.CLICK_DELEGATE) != null) {
