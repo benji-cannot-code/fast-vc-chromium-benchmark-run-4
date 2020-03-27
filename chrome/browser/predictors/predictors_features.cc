0788e17534b055c63ad453be2e12ac51d7b0e44e
Sophie Chang <sophiechang@chromium.org>
2020-03-27 19:15:10 +0000

    Allow for counterfactual logging the predictions coming from opt guide
    This adds a feature parameter within the
    LoadingPredictorUseOptimizationGuide feature to dictate whether to
    actually use the predictions for preconnect or anything else later.
    
    I tested via browser tests by removing the DISABLED_ locally. I'm not
    remove the DISABLED_ in real code since I still haven't been able to
    repro the flake locally and don't know the root cause of the flake on
    bots.
    
    Bug: 1064121
    Change-Id: I6a47645662b04760520c8eff4dd52ec7f55ca064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119372
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754111}

--- chrome/browser/predictors/predictors_features.cc
+++ chrome/browser/predictors/predictors_features.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/predictors/predictors_features.h"
 
+#include "base/metrics/field_trial_params.h"
+
 namespace features {
 
 // Whether local predictions should be used to make preconnect predictions.
@@ -38,6 +40,15 @@ const base::Feature kLoadingPredictorUseOptimizationGuide{
 
 bool ShouldUseLocalPredictions() {
   return base::FeatureList::IsEnabled(kLoadingPredictorUseLocalPredictions);
+}
+
+bool ShouldUseOptimizationGuidePredictionsToPreconnect() {
+  if (!base::FeatureList::IsEnabled(kLoadingPredictorUseOptimizationGuide))
+    return false;
+
+  return base::GetFieldTrialParamByFeatureAsBool(
+      kLoadingPredictorUseOptimizationGuide, "use_predictions_for_preconnect",
+      true);
 }
 
 }  // namespace features
