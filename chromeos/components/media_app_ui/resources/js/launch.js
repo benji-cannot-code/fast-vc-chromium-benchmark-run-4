e0f3ae8590979cbf447b679c01ef23a8fbe02a18
Zain Afzal <zafzal@google.com>
2020-03-27 09:48:28 +0000

    Add directory navigation functions to receivedFileList.
    The unprivileged frame needs the ability to load the next file in the
    directory as writable. This CL adds functions to expose this
    functionality on receivedFileList.
    
    Bug: 996088
    Change-Id: If18a4141b451a3788ab2e60aa70aff2b88580f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2123688
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753953}

--- chromeos/components/media_app_ui/resources/js/launch.js
+++ chromeos/components/media_app_ui/resources/js/launch.js
@@ -52,7 +52,7 @@ guestMessagePipe.registerHandler(Message.OPEN_FEEDBACK_DIALOG, () => {
 });
 
 guestMessagePipe.registerHandler(Message.OVERWRITE_FILE, async (message) => {
-  const overwrite = /** @type{OverwriteFileMessage} */ (message);
+  const overwrite = /** @type {OverwriteFileMessage} */ (message);
   if (!currentlyWritableFile || overwrite.token !== fileToken) {
     throw new Error('File not current.');
   }
@@ -62,9 +62,8 @@ guestMessagePipe.registerHandler(Message.OVERWRITE_FILE, async (message) => {
   await writer.close();
 });
 
-/** Handler to delete the currently focused file. */
 guestMessagePipe.registerHandler(Message.DELETE_FILE, async (message) => {
-  const deleteMsg = /** @type{DeleteFileMessage} */ (message);
+  const deleteMsg = /** @type {DeleteFileMessage} */ (message);
 
   if (!currentlyWritableFile || deleteMsg.token !== fileToken) {
     throw new Error('File not current for delete.');
@@ -91,6 +90,12 @@ guestMessagePipe.registerHandler(Message.DELETE_FILE, async (message) => {
 
   await currentDirectoryHandle.removeEntry(currentFilename);
   return {deleteResult: DeleteResult.SUCCESS};
+});
+
+guestMessagePipe.registerHandler(Message.NAVIGATE, (message) => {
+  const navigate = /** @type {NavigateMessage} */ (message);
+
+  advance(navigate.direction);
 });
 
 /** Loads the current file list into the guest. */
--- chromeos/components/media_app_ui/resources/js/message_types.js
+++ chromeos/components/media_app_ui/resources/js/message_types.js
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const Message = {
   DELETE_FILE: 'delete-file',
   LOAD_FILES: 'load-files',
+  NAVIGATE: 'navigate',
   OPEN_FEEDBACK_DIALOG: 'open-feedback-dialog',
   OVERWRITE_FILE: 'overwrite-file',
 };
@@ -28,13 +29,24 @@ const DeleteResult = {
   FILE_MOVED: 1,
 };
 
-/** @typedef {{ token: number }} */
+/**
+ * Message sent by the unprivileged context to request the privileged context to
+ * delete the currently writable file.
+ * If the supplied file `token` is invalid the request is rejected.
+ * @typedef {{ token: number }}
+ */
 let DeleteFileMessage;
 
-/** @typedef {{ deleteResult: DeleteResult }}  */
+/**
+ * Response message sent by the privileged context indicating if a requested
+ * delete was successful.
+ * @typedef {{ deleteResult: DeleteResult }}
+ */
 let DeleteFileResponse;
 
 /**
+ * Message sent by the privileged context to the unprivileged context on launch
+ * indicating the initial files available to open.
  * @typedef {{
  *    writableFileIndex: number,
  *    files: !Array<{token: number, file: !File}>
@@ -42,5 +54,18 @@ let DeleteFileResponse;
  */
 let LoadFilesMessage;
 
-/** @typedef {{token: number, blob: !Blob}} */
+/**
+ * Message sent by the unprivileged context to the privileged context requesting
+ * that the currently writable file be overridden with the provided `blob`.
+ * If the supplied file `token` is invalid the request is rejected.
+ * @typedef {{token: number, blob: !Blob}}
+ */
 let OverwriteFileMessage;
+
+/**
+ * Message sent by the unprivileged context to the privileged context requesting
+ * the app be relaunched with the next/previous file in the current directory
+ * set to writable. Direction must be either 'next' or 'prev'
+ * @typedef {{direction: number}}
+ */
+let NavigateMessage;
