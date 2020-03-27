7b4d0532ffeeb9d6df35f8138765c153e92a6038
Rakina Zata Amni <rakina@chromium.org>
2020-03-27 01:45:10 +0000

    Reland "DL: Don't include skip-viewport-activation subtrees in the AX tree"
    This is a reland of https://chromium-review.googlesource.com/c/chromium/src/+/2028883
    
    Currently we shouldn't test behavior with nested locks due to the bug at
    crbug.com/1053526. This CL fixes the flaky test that caused the revert
    in the previous CL (failing on the nested activation case).
    
    Bug: 1001930
    Change-Id: I77c1798724801e5c1be7d52eb14bf746b45aa397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2083977
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753869}

--- third_party/blink/web_tests/inspector-protocol/accessibility/accessibility-getFullAXTree-display-locked.js
+++ third_party/blink/web_tests/inspector-protocol/accessibility/accessibility-getFullAXTree-display-locked.js
@@ -1,16 +1,18 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(testRunner) {
   var {page, session, dp} = await testRunner.startHTML(`
-    <div id='activatable' style='subtree-visibility: hidden-matchable'>
+    <div style='height: 10000px;'>spacer</div>
+    <div id='activatable' style='subtree-visibility: auto'>
       locked
       <div id='child'>
         child
         <div id='grandChild'>grandChild</div>
       </div>
       <div id='invisible' style='display:none'>invisible</div>
-      <div id='nested' style='subtree-visibility: hidden-matchable'>nested</div>
+      <div id='nested' style='subtree-visibility: auto'>nested</div>
       text
     </div>
+    <div id='nonViewportActivatable' style='subtree-visibility: hidden-matchable'>nonViewportActivatable text</div>
     <div id='nonActivatable' style='subtree-visibility: hidden'>nonActivatable text</div>
     <div id='normal'>normal text</div>
   `, 'Tests accessibility values of display locked nodes');
