4eaacb1422086b655aa04b7b196f75fc705126ca
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-03-27 06:58:30 +0000

    [Extensions Menu] Add pinned extension metrics
    Add histograms for the count of pinned extensions, as both an absolute
    count and a percentage of total extensions.
    
    Bug: 1064111
    Change-Id: I3fd6a8808114c2301ce70fdf752048a6f7779c71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116568
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753930}

--- chrome/browser/ui/toolbar/toolbar_actions_model.cc
+++ chrome/browser/ui/toolbar/toolbar_actions_model.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/metrics/histogram_base.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/one_shot_event.h"
 #include "base/single_thread_task_runner.h"
@@ -505,6 +506,19 @@ void ToolbarActionsModel::InitializeActionList() {
     // Set |pinned_action_ids_| directly to avoid notifying observers that they
     // have changed even though they haven't.
     pinned_action_ids_ = GetFilteredPinnedActionIds();
+
+    if (!profile_->IsOffTheRecord()) {
+      base::UmaHistogramCounts100("Extensions.Toolbar.PinnedExtensionCount",
+                                  pinned_action_ids_.size());
+      int percentage = 0;
+      if (!action_ids_.empty()) {
+        double percentage_double =
+            pinned_action_ids_.size() / action_ids_.size() * 100.0;
+        percentage = int{percentage_double};
+      }
+      base::UmaHistogramPercentage(
+          "Extensions.Toolbar.PinnedExtensionPercentage", percentage);
+    }
   }
 }
 
