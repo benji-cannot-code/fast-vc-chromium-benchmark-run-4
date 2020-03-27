ef7630d2e5db647b4360c45b08f021b4abc7edd8
Alex Ilin <alexilin@chromium.org>
2020-03-27 16:41:06 +0000

    [Signin] Live test: sync flow with selecting "This wasn't me"
    This CL adds a new live test for the enable sync flow in a profile that
    already had another sync account before. Chrome should display an email
    confirmation dialog in this case that offers the user an option to
    create a new profile.
    
    The test goes through the following steps:
    - enables sync to account A
    - turns off sync
    - enables sync to account B
    - clicks "This wasn't me" on the email confirmation dialog
    - checks that a new profile was created.
    - Account B has valid cookies and tokens and is syncing in the new
      profile.
    - Account B was removed from the original profile.
    
    SigninEmailConfirmationDialog required modifications in order for the
    test to click through the dialog. Namely, SigninEmailConfirmationDialog
    is moved to be hosted by SigninViewController that keeps a pointer to
    the dialog. SigninViewController instance can be obtained from a Browser
    object.
    
    This CL also largely modifies how LiveSignInTests observes changes in
    IdentityManager. All the tests use a single observer class that allows
    to wait until a specified number of accounts is signed-in. This
    simplifies the code and potentially reduces the test flakiness because
    some observer events may come even if the accounts did not change.
    
    Bug: 1008893
    Change-Id: I39236822cf6160e59f02a8d26fc2c1c8d8f19da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116209
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754048}

--- chrome/browser/ui/signin_view_controller.h
+++ chrome/browser/ui/signin_view_controller.h
@@ -14,6 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_buildflags.h"
 #include "url/gurl.h"
 
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
+#include "chrome/browser/ui/webui/signin/signin_email_confirmation_dialog.h"
+#endif
+
 #if defined(OS_ANDROID)
 #error This file should only be included on desktop.
 #endif
@@ -76,6 +80,14 @@ class SigninViewController {
   void ShowDiceAddAccountTab(Browser* browser,
                              signin_metrics::AccessPoint access_point,
                              const std::string& email_hint);
+
+  // Shows the modal sign-in email confirmation dialog as a tab-modal dialog on
+  // top of the currently displayed WebContents in |browser|.
+  void ShowModalSigninEmailConfirmationDialog(
+      Browser* browser,
+      const std::string& last_email,
+      const std::string& email,
+      base::OnceCallback<void(SigninEmailConfirmationDialog::Action)> callback);
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 
   // Shows the modal sync confirmation dialog as a browser-modal dialog on top
