046f5eb8e7121cf5b7cb0e3de92e2ca64b998792
Minoru Chikamune <chikamune@chromium.org>
2020-03-27 08:01:31 +0000

    heap mojo: Fix Null-dereference READ in blink::SpeechSynthesis::cancel
    `blink::SpeechSynthesis::cancel` calls `mojom_synthesis_->Cancel()`.
    And mojom_synthesis_ is wrapped by HeapMojoRemote.
    
    ```
    void SpeechSynthesis::cancel() {
      // Remove all the items from the utterance queue. The platform
      // may still have references to some of these utterances and may
      // fire events on them asynchronously.
      utterance_queue_.clear();
    
      InitializeMojomSynthesisIfNeeded();
      mojom_synthesis_->Cancel();  // this call caused Null-dereference READ
    }
    ```
    
    When the ExecutionContext was destroyed, HeapMojoRemote will automatically
    reset the mojom_synthesis_ mojo::Remote object.
    
    After that, if the `blink::SpeechSynthesis::cancel` is called,
    Null-dereference READ will be happened because it's already reset.
    
    To avoid this problem, HeapMojoWrapperMode::kWithoutContextObserver have to be specified.
    if this was specified, HeapMojoRemote doesn't reset mojo::Remote object when the ExecutionContext was destroyed.
    
    Bug: 1049056, 1058076
    Change-Id: Iede8d23c7f7016018ace88770a23d80b1b82e7fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2123493
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753937}

--- third_party/blink/renderer/modules/speech/speech_synthesis.h
+++ third_party/blink/renderer/modules/speech/speech_synthesis.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 
 namespace blink {
 
@@ -123,8 +124,12 @@ class MODULES_EXPORT SpeechSynthesis final
   void InitializeMojomSynthesis();
   void InitializeMojomSynthesisIfNeeded();
 
-  HeapMojoReceiver<mojom::blink::SpeechSynthesisVoiceListObserver> receiver_;
-  HeapMojoRemote<mojom::blink::SpeechSynthesis> mojom_synthesis_;
+  HeapMojoReceiver<mojom::blink::SpeechSynthesisVoiceListObserver,
+                   HeapMojoWrapperMode::kWithoutContextObserver>
+      receiver_;
+  HeapMojoRemote<mojom::blink::SpeechSynthesis,
+                 HeapMojoWrapperMode::kWithoutContextObserver>
+      mojom_synthesis_;
   HeapVector<Member<SpeechSynthesisVoice>> voice_list_;
   HeapDeque<Member<SpeechSynthesisUtterance>> utterance_queue_;
   bool is_paused_ = false;
