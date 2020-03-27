f46555c43ce14204e86768e75564ac989e1de3ed
Christian Biesinger <cbiesinger@chromium.org>
2020-03-27 18:29:24 +0000

    [AspectRatio] Add parsing support for aspect-ratio: X / Y
    Does not use the property in layout yet.
    
    https://drafts.csswg.org/css-sizing-4/#aspect-ratio
    
    Bug: 1045668
    Change-Id: I1c6411faaf7eec6c7dc041da5b7e481fb34fe030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122430
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754097}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.h
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.h
@@ -274,6 +274,9 @@ class StyleBuilderConverter {
 
   static LengthSize ConvertIntrinsicSize(StyleResolverState&, const CSSValue&);
 
+  static base::Optional<IntSize> ConvertAspectRatio(StyleResolverState&,
+                                                    const CSSValue&);
+
   static bool ConvertInternalEmptyLineHeight(StyleResolverState& state,
                                              const CSSValue& value);
 
