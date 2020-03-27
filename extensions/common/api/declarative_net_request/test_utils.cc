445e0bb74635952454cfe7dea0436b5e2cb16598
Karandeep Bhatia <karandeepb@chromium.org>
2020-03-27 09:40:13 +0000

    DNR: Simplify WriteManifestAndRuleset.
    The |json_rules_filepath| argument is redundant, remove it. Also,
    introduce a new struct TestRulesetInfo, since we anticipate extending
    WriteManifestAndRuleset to support multiple rulesets.
    
    BUG=754526
    
    Change-Id: I85204f8c0dfabdfde6318e7513d6d590964987f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2120097
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#753949}

--- extensions/common/api/declarative_net_request/test_utils.cc
+++ extensions/common/api/declarative_net_request/test_utils.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/json/json_file_value_serializer.h"
-#include "base/values.h"
 #include "extensions/common/api/declarative_net_request.h"
 #include "extensions/common/api/declarative_net_request/constants.h"
 #include "extensions/common/constants.h"
@@ -235,30 +234,20 @@ std::unique_ptr<base::ListValue> ToListValue(
   return builder.Build();
 }
 
-void WriteManifestAndRuleset(
-    const base::FilePath& extension_dir,
-    const base::FilePath::CharType* json_rules_filepath,
-    const std::string& json_rules_filename,
-    const std::vector<TestRule>& rules,
-    const std::vector<std::string>& hosts,
-    unsigned flags) {
+std::unique_ptr<base::ListValue> ToListValue(const std::vector<TestRule>& vec) {
   ListBuilder builder;
-  for (const auto& rule : rules)
+  for (const TestRule& rule : vec)
     builder.Append(rule.ToValue());
-  WriteManifestAndRuleset(extension_dir, json_rules_filepath,
-                          json_rules_filename, *builder.Build(), hosts, flags);
+  return builder.Build();
 }
 
-void WriteManifestAndRuleset(
-    const base::FilePath& extension_dir,
-    const base::FilePath::CharType* json_rules_filepath,
-    const std::string& json_rules_filename,
-    const base::Value& rules,
-    const std::vector<std::string>& hosts,
-    unsigned flags) {
+void WriteManifestAndRuleset(const base::FilePath& extension_dir,
+                             const TestRulesetInfo& info,
+                             const std::vector<std::string>& hosts,
+                             unsigned flags) {
   // Persist JSON rules file.
-  JSONFileValueSerializer(extension_dir.Append(json_rules_filepath))
-      .Serialize(rules);
+  JSONFileValueSerializer(extension_dir.AppendASCII(info.relative_file_path))
+      .Serialize(info.rules_value);
 
   // Persists a background script if needed.
   if (flags & ConfigFlag::kConfig_HasBackgroundScript) {
@@ -270,7 +259,7 @@ void WriteManifestAndRuleset(
 
   // Persist manifest file.
   JSONFileValueSerializer(extension_dir.Append(kManifestFilename))
-      .Serialize(*CreateManifest(json_rules_filename, hosts, flags));
+      .Serialize(*CreateManifest(info.relative_file_path, hosts, flags));
 }
 
 }  // namespace declarative_net_request
