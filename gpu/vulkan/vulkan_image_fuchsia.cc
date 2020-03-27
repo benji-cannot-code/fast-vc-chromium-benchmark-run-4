acac6668b416a86edecbec47099cd8b3a0d509df
Peng Huang <penghuang@chromium.org>
2020-03-27 16:56:43 +0000

    VulkanImage: support creating from GMB on Linux
    This CL refactors VulkanImage::InitializeFromGpuMemoryBufferHandle()
    to platform specified files (vulkan_image_{android, linux, win, etc}.cc.
    And implements InitializeFromGpuMemoryBufferHandle() for Linux.
    
    Bug: 848385
    Change-Id: I7081d338cfc194185e5cda049fb7eab03fbf9b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124295
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754059}

---
+++ gpu/vulkan/vulkan_image_fuchsia.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/vulkan/vulkan_image.h"
+
+namespace gpu {
+
+bool VulkanImage::InitializeFromGpuMemoryBufferHandle(
+    VulkanDeviceQueue* device_queue,
+    gfx::GpuMemoryBufferHandle gmb_handle,
+    const gfx::Size& size,
+    VkFormat format,
+    VkImageUsageFlags usage,
+    VkImageCreateFlags flags,
+    VkImageTiling image_tiling) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace gpu
