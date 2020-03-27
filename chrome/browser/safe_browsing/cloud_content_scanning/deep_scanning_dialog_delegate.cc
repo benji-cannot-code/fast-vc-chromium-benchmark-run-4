a0d32d349f86f8c971bc4507b58a70cdfd67f033
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-03-27 19:19:38 +0000

    Add EnterpriseConnectorsPolicyHandler
    This handler validates the policy source is clud and it inherits from
    SchemaValidatingPolicyHandler since all of the connectors are configured
    by json.
    
    Bug: 1062338
    Change-Id: I7e2c8a2b9f8618438af9fd8d151e1324482ed968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116010
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754114}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_dialog_delegate.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/services/file_util/public/cpp/sandboxed_zip_analyzer.h"
 #include "components/policy/core/browser/url_blacklist_manager.h"
 #include "components/policy/core/browser/url_util.h"
+#include "components/policy/core/common/chrome_schema.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/safe_browsing/core/features.h"
@@ -410,7 +411,10 @@ DeepScanningDialogDelegate::DeepScanningDialogDelegate(
     : web_contents_(web_contents),
       data_(std::move(data)),
       callback_(std::move(callback)),
-      access_point_(access_point) {
+      access_point_(access_point),
+      handler_("CheckContentCompliance",
+               prefs::kCheckContentCompliance,
+               policy::GetChromeSchema()) {
   DCHECK(web_contents_);
   result_.text_results.resize(data_.text.size(), false);
   result_.paths_results.resize(data_.paths.size(), false);
