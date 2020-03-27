30e9596e8557f68e45d3f6ee6416678e2ba7d27b
David Van Cleve <davidvc@chromium.org>
2020-03-27 18:04:16 +0000

    Trust Tokens: Remove key commitment state from TrustTokenStore
    This is one of a series of CLs updating Trust Tokens key commitment
    state to use a singleton in-memory store owned by NetworkContext and
    updated via Mojo from the component updater. This involves
    
    [1, this CL]: Remove key commitment-related state from the on-disk trust
    token store
    - Remove key commitment setters/getters
    - Remove batch size from the store
    Note: This change is actually a no-op, since no functionality depended
    on being able to store these items over the course of more than one
    operation.
    
    2: Mojoify TrustTokenKeyCommitmentResult and remove commitment fetching
    and parsing
    code.
    3: Clean up the persistent storage schema, swapping in the Mojo
    commitment result struct for existing uses of
    TrustTokenKeyCommitment{Result}.
    4: Add a centralized issuers-to-commitments store in the network
    service.
    5: Add component updater logic to populate this store.
    
    Bug: 1063510
    
    Preliminary: Remove key commitments from TrustTokenStore
    Change-Id: Ib794ba2095b4608ff68eca8ddb42481cd6d2591e
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116646
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754086}

--- services/network/trust_tokens/trust_token_request_redemption_helper.cc
+++ services/network/trust_tokens/trust_token_request_redemption_helper.cc
@@ -203,10 +203,6 @@ void TrustTokenRequestRedemptionHelper::UpdateTokenStoreFromKeyCommitmentResult(
   // this method, and its counterpart in TrustTokenRequestIssuanceHelper, will
   // no longer be needed.
 
-  int batch_size = result.batch_size ? *result.batch_size
-                                     : kDefaultTrustTokenIssuanceBatchSize;
-  token_store_->SetBatchSize(issuer_, batch_size);
-
   std::vector<TrustTokenKeyCommitment> commitments_to_store;
 
   for (const TrustTokenKeyCommitmentResult::Key& key : result.keys) {
@@ -216,8 +212,7 @@ void TrustTokenRequestRedemptionHelper::UpdateTokenStoreFromKeyCommitmentResult(
     commitments_to_store.emplace_back(std::move(to_store));
   }
 
-  token_store_->SetKeyCommitmentsAndPruneStaleState(
-      issuer_, std::move(commitments_to_store));
+  token_store_->PruneStaleIssuerState(issuer_, std::move(commitments_to_store));
 }
 
 base::Optional<TrustToken>
