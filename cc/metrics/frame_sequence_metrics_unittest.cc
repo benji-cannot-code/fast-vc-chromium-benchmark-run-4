1fde6aa02ef5b033929bbb5bca613aac172e6dd3
Xida Chen <xidachen@chromium.org>
2020-03-28 00:45:01 +0000

    [throughput] Compute the slower thread throughput correctly
    We have changed the UMA and UKM to report the percent of frame dropped,
    which is the opposite meaning of throughput. However, we forgot to
    change how the slower thread is computed.
    
    When it was throughput, the slower thread is the smaller throughput
    between the main && impl thread. Now it is percent of frame dropped,
    the slower thread should be the thread with larger value.
    
    Bug: None
    Change-Id: I3a42394c699a6209a0dfca43ae8362b2ac987986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124876
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754250}

--- cc/metrics/frame_sequence_metrics_unittest.cc
+++ cc/metrics/frame_sequence_metrics_unittest.cc
@@ -11,6 +11,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
+TEST(FrameSequenceMetricsTest, SlowerThread) {
+  base::HistogramTester histogram_tester;
+
+  FrameSequenceMetrics first(FrameSequenceTrackerType::kTouchScroll, nullptr);
+  first.impl_throughput().frames_expected = 200;
+  first.impl_throughput().frames_produced = 190;
+  first.main_throughput().frames_expected = 100;
+  first.main_throughput().frames_produced = 50;
+
+  // slower thread throughput is computed at ReportMetrics().
+  first.ReportMetrics();
+  std::string metric =
+      "Graphics.Smoothness.PercentDroppedFrames.SlowerThread.TouchScroll";
+  EXPECT_EQ(histogram_tester.GetBucketCount(metric, 50), 1);
+}
+
 TEST(FrameSequenceMetricsTest, MergeMetrics) {
   // Create a metric with only a small number of frames. It shouldn't report any
   // metrics.
