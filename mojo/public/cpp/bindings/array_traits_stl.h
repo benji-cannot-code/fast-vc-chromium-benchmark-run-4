72900310111280dc580d8c270740acaf968b4677
arthursonzogni <arthursonzogni@chromium.org>
2020-03-28 01:06:24 +0000

    Add missing include for mojo binding.
    The file mojo/public/cpp/bindings/array_traits_stl.h is using the
    definition of std::unordered_set, but it doesn't include it.
    
    array_traits_slt.h was able to compile, because it was always included
    in files where <unordered_set> was included before (directly or
    transitively). It is not able to self-compile. This patch fixes the
    issue.
    
    Bug: None
    Change-Id: I06c028b4842ae449cd3d2bda535c7139405cc145
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124788
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754257}

--- mojo/public/cpp/bindings/array_traits_stl.h
+++ mojo/public/cpp/bindings/array_traits_stl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <map>
 #include <set>
+#include <unordered_set>
 #include <vector>
 
 #include "mojo/public/cpp/bindings/array_traits.h"
