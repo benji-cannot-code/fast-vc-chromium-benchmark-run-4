77f3c033a499790a3be1f39ae56ca842c1afb6eb
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-03-28 01:13:31 +0000

    Re-enable search term tests in StartSurfaceLayoutTests on Pie
    Thumbnail-related checking became flaky, but we are not specifically
    testing thumbnails in the tests for search term chip.
    
    Disable TabToGTSAnimation in the following tests to make them less flaky
    on slower builders:
    - StartSurfaceLayoutTest#testSearchTermChip_noChip
    - StartSurfaceLayoutTest#testSearchTermChip_adaptiveIcon
    - StartSurfaceLayoutTest#testSearchTermChip_withChip
    
    Bug: 1064367
    Change-Id: I705b2141312a23ae5c8acf2fdb6f0683d8a8a90f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2123636
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754259}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceLayoutTest.java
@@ -1432,6 +1432,8 @@ public class StartSurfaceLayoutTest {
 
     @Test
     @MediumTest
+    // Disable TAB_TO_GTS_ANIMATION to make it less flaky.
+    @DisableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION + "<Study")
     @CommandLineFlags.Add({BASE_PARAMS + "/enable_search_term_chip/true"})
     public void testSearchTermChip_noChip() throws InterruptedException {
         assertTrue(TabUiFeatureUtilities.ENABLE_SEARCH_CHIP.getValue());
@@ -1444,6 +1446,8 @@ public class StartSurfaceLayoutTest {
 
     @Test
     @MediumTest
+    // Disable TAB_TO_GTS_ANIMATION to make it less flaky.
+    @DisableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION + "<Study")
     @CommandLineFlags.Add({BASE_PARAMS + "/enable_search_term_chip/true"})
     public void testSearchTermChip_withChip() throws InterruptedException {
         assertTrue(TabUiFeatureUtilities.ENABLE_SEARCH_CHIP.getValue());
@@ -1519,6 +1523,8 @@ public class StartSurfaceLayoutTest {
     @Test
     @MediumTest
     // clang-format off
+    // Disable TAB_TO_GTS_ANIMATION to make it less flaky.
+    @DisableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION + "<Study")
     @CommandLineFlags.Add({BASE_PARAMS +
             "/enable_search_term_chip/true/enable_search_term_chip_adaptive_icon/true"})
     public void testSearchTermChip_adaptiveIcon() throws InterruptedException {
