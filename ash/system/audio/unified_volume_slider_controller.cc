0dbc56bf9dbc14b02ad9a0e29e31f371fec3ca99
Rachel Wong <wrong@chromium.org>
2020-03-28 00:56:19 +0000

    [settings logging] Log volume changes as an audio observer.
    Previously, volume changes were logged from the system tray slider. This
    CL changes the approach to directly observe volume changes, which will
    allow the logger to capture volume changes from the physical keys as
    well as the system tray.
    
    Bug: 1014839
    Change-Id: Id33a196a0537d79d94c4592eae8b4a2437230fed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105683
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754254}

--- ash/system/audio/unified_volume_slider_controller.cc
+++ ash/system/audio/unified_volume_slider_controller.cc
@@ -16,16 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using chromeos::CrasAudioHandler;
 
 namespace ash {
-namespace {
-
-void LogUserVolumeEvent(const int previous_level, const int current_level) {
-  auto* logger = ml::UserSettingsEventLogger::Get();
-  if (logger) {
-    logger->LogVolumeUkmEvent(previous_level, current_level);
-  }
-}
-
-}  // namespace
 
 UnifiedVolumeSliderController::UnifiedVolumeSliderController(
     UnifiedVolumeSliderController::Delegate* delegate)
@@ -45,12 +35,9 @@ void UnifiedVolumeSliderController::ButtonPressed(views::Button* sender,
                                                   const ui::Event& event) {
   if (sender == slider_->button()) {
     bool mute_on = !CrasAudioHandler::Get()->IsOutputMuted();
-    const int volume_level = CrasAudioHandler::Get()->GetOutputVolumePercent();
     if (mute_on) {
-      LogUserVolumeEvent(volume_level, 0);
       base::RecordAction(base::UserMetricsAction("StatusArea_Audio_Muted"));
     } else {
-      LogUserVolumeEvent(0, volume_level);
       base::RecordAction(base::UserMetricsAction("StatusArea_Audio_Unmuted"));
     }
     CrasAudioHandler::Get()->SetOutputMute(mute_on);
@@ -74,7 +61,6 @@ void UnifiedVolumeSliderController::SliderValueChanged(
         UMA_STATUS_AREA_CHANGED_VOLUME_MENU);
   }
 
-  LogUserVolumeEvent(CrasAudioHandler::Get()->GetOutputVolumePercent(), level);
   CrasAudioHandler::Get()->SetOutputVolumePercent(level);
 
   // If the volume is above certain level and it's muted, it should be unmuted.
