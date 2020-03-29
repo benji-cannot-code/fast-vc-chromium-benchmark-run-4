f7be7ec99724cb30888f13511fe5818a120fe1af
Gabriel Charette <gab@chromium.org>
2020-03-29 05:09:43 +0000

    [TaskEnvironment] Make AdvanceClock() schedule ripe delayed tasks
    This not being the case was an oversight and it will soon be required
    by unit tests of the upcoming IO jank metric.
    
    (new test fails without the matching task_environment.cc change)
    
    R=fdoray@chromium.org, olivierli@chromium.org
    
    Bug: 1064645
    Change-Id: I5c57ee647cb72dc5c5c003d9beb3ea24e3dc911f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2120855
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754344}

--- base/test/task_environment_unittest.cc
+++ base/test/task_environment_unittest.cc
@@ -406,7 +406,7 @@ TEST_F(TaskEnvironmentTest,
 // Verify that the TickClock returned by
 // |TaskEnvironment::GetMockTickClock| gets updated when the
 // FastForward(By|UntilNoTasksRemain) functions are called.
-TEST_F(TaskEnvironmentTest, FastForwardAdvanceTickClock) {
+TEST_F(TaskEnvironmentTest, FastForwardAdvancesTickClock) {
   // Use a QUEUED execution-mode environment, so that no tasks are actually
   // executed until RunUntilIdle()/FastForwardBy() are invoked.
   TaskEnvironment task_environment(
@@ -438,7 +438,7 @@ TEST_F(TaskEnvironmentTest, FastForwardAdvanceTickClock) {
   EXPECT_EQ(kLongTaskDelay * 2, tick_clock->NowTicks() - tick_clock_ref);
 }
 
-TEST_F(TaskEnvironmentTest, FastForwardAdvanceMockClock) {
+TEST_F(TaskEnvironmentTest, FastForwardAdvancesMockClock) {
   constexpr base::TimeDelta kDelay = TimeDelta::FromSeconds(42);
   TaskEnvironment task_environment(TaskEnvironment::TimeSource::MOCK_TIME);
 
@@ -449,7 +449,7 @@ TEST_F(TaskEnvironmentTest, FastForwardAdvanceMockClock) {
   EXPECT_EQ(start_time + kDelay, clock->Now());
 }
 
-TEST_F(TaskEnvironmentTest, FastForwardAdvanceTime) {
+TEST_F(TaskEnvironmentTest, FastForwardAdvancesTime) {
   constexpr base::TimeDelta kDelay = TimeDelta::FromSeconds(42);
   TaskEnvironment task_environment(TaskEnvironment::TimeSource::MOCK_TIME);
 
@@ -458,7 +458,7 @@ TEST_F(TaskEnvironmentTest, FastForwardAdvanceTime) {
   EXPECT_EQ(start_time + kDelay, base::Time::Now());
 }
 
-TEST_F(TaskEnvironmentTest, FastForwardAdvanceTimeTicks) {
+TEST_F(TaskEnvironmentTest, FastForwardAdvancesTimeTicks) {
   constexpr base::TimeDelta kDelay = TimeDelta::FromSeconds(42);
   TaskEnvironment task_environment(TaskEnvironment::TimeSource::MOCK_TIME);
 
@@ -467,7 +467,7 @@ TEST_F(TaskEnvironmentTest, FastForwardAdvanceTimeTicks) {
   EXPECT_EQ(start_time + kDelay, base::TimeTicks::Now());
 }
 
-TEST_F(TaskEnvironmentTest, AdvanceClockAdvanceTickClock) {
+TEST_F(TaskEnvironmentTest, AdvanceClockAdvancesTickClock) {
   constexpr base::TimeDelta kDelay = TimeDelta::FromSeconds(42);
   TaskEnvironment task_environment(TaskEnvironment::TimeSource::MOCK_TIME);
 
@@ -478,7 +478,7 @@ TEST_F(TaskEnvironmentTest, AdvanceClockAdvanceTickClock) {
   EXPECT_EQ(start_time + kDelay, tick_clock->NowTicks());
 }
 
-TEST_F(TaskEnvironmentTest, AdvanceClockAdvanceMockClock) {
+TEST_F(TaskEnvironmentTest, AdvanceClockAdvancesMockClock) {
   constexpr base::TimeDelta kDelay = TimeDelta::FromSeconds(42);
   TaskEnvironment task_environment(TaskEnvironment::TimeSource::MOCK_TIME);
 
@@ -489,7 +489,7 @@ TEST_F(TaskEnvironmentTest, AdvanceClockAdvanceMockClock) {
   EXPECT_EQ(start_time + kDelay, clock->Now());
 }
 
-TEST_F(TaskEnvironmentTest, AdvanceClockAdvanceTime) {
+TEST_F(TaskEnvironmentTest, AdvanceClockAdvancesTime) {
   constexpr base::TimeDelta kDelay = TimeDelta::FromSeconds(42);
   TaskEnvironment task_environment(TaskEnvironment::TimeSource::MOCK_TIME);
 
@@ -498,7 +498,7 @@ TEST_F(TaskEnvironmentTest, AdvanceClockAdvanceTime) {
   EXPECT_EQ(start_time + kDelay, base::Time::Now());
 }
 
-TEST_F(TaskEnvironmentTest, AdvanceClockAdvanceTimeTicks) {
+TEST_F(TaskEnvironmentTest, AdvanceClockAdvancesTimeTicks) {
   constexpr base::TimeDelta kDelay = TimeDelta::FromSeconds(42);
   TaskEnvironment task_environment(TaskEnvironment::TimeSource::MOCK_TIME);
 
@@ -522,6 +522,21 @@ TEST_F(TaskEnvironmentTest, AdvanceClockDoesNotRunTasks) {
   // The task is still pending, but is now runnable.
   EXPECT_EQ(1U, task_environment.GetPendingMainThreadTaskCount());
   EXPECT_FALSE(task_environment.NextTaskIsDelayed());
+}
+
+TEST_F(TaskEnvironmentTest, AdvanceClockSchedulesRipeDelayedTasks) {
+  TaskEnvironment task_environment(TaskEnvironment::TimeSource::MOCK_TIME);
+
+  bool ran = false;
+
+  constexpr base::TimeDelta kTaskDelay = TimeDelta::FromDays(1);
+  ThreadPool::PostDelayedTask(
+      FROM_HERE, base::BindLambdaForTesting([&]() { ran = true; }), kTaskDelay);
+
+  task_environment.AdvanceClock(kTaskDelay);
+  EXPECT_FALSE(ran);
+  task_environment.RunUntilIdle();
+  EXPECT_TRUE(ran);
 }
 
 // Verify that FastForwardBy() runs existing immediate tasks before advancing,
