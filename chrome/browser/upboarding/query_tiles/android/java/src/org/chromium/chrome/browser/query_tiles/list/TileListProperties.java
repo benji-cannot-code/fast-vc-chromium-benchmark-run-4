e289aa93853c81574c4c355ad0a7aa9e5363cd80
Shakti Sahu <shaktisahu@chromium.org>
2020-03-29 02:28:03 +0000

    Query tiles : Separate out QueryTileCoordinator from TileProvider
    Change-Id: I20ed0ecc83b0113eb22612bb1a14c9010e2af714
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2123248
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754339}

--- chrome/browser/upboarding/query_tiles/android/java/src/org/chromium/chrome/browser/query_tiles/list/TileListProperties.java
+++ chrome/browser/upboarding/query_tiles/android/java/src/org/chromium/chrome/browser/query_tiles/list/TileListProperties.java
@@ -5,9 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.query_tiles.list;
 
-import android.graphics.Bitmap;
-
 import org.chromium.base.Callback;
+import org.chromium.chrome.browser.query_tiles.QueryTileCoordinator.TileVisualsProvider;
 import org.chromium.chrome.browser.query_tiles.Tile;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
@@ -18,21 +17,13 @@ import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
  * properties that are effectively shared across all list items like callbacks.
  */
 interface TileListProperties {
-    /** A helper interface to support retrieving {@link Bitmap} asynchronously. */
-    @FunctionalInterface
-    interface VisualsProvider {
-        /**
-         * @param tile The {@link Tile} to get the {@link Bitmap} for.
-         * @param callback A {@link Callback} that will be notified on completion.
-         */
-        void getVisuals(Tile tile, Callback<Bitmap> callback);
-    }
 
     /** The callback to run when a {@link Tile} is clicked on the UI. */
     WritableObjectPropertyKey<Callback<Tile>> CLICK_CALLBACK = new WritableObjectPropertyKey<>();
 
     /** The provider to retrieve expensive visuals for a {@link Tile}. */
-    WritableObjectPropertyKey<VisualsProvider> VISUALS_CALLBACK = new WritableObjectPropertyKey<>();
+    WritableObjectPropertyKey<TileVisualsProvider> VISUALS_CALLBACK =
+            new WritableObjectPropertyKey<>();
 
     PropertyKey[] ALL_KEYS = new PropertyKey[] {CLICK_CALLBACK, VISUALS_CALLBACK};
 }
