5c65f4fd88527d15a0de77851d8b4586159c9eba
Krishna Govind <govind@chromium.org>
2020-03-29 04:35:23 +0000

    Revert "Move custom tabs off notSecureIsIntentChromeOrFirstParty()"
    This reverts commit 0bd64fcfb8a4eb82b1014cc5ec1d1578b6df2333.
    
    Reason for revert: Breaks canary, crbug 1065682
    
    Original change's description:
    > Move custom tabs off notSecureIsIntentChromeOrFirstParty()
    > 
    > Instead, use the CCT session token to determine the connected client
    > package and verify using that instead.
    > 
    > Bug: 832124
    > Change-Id: I96f9c6831f7dcbf801e3ee48d59904a4788d97b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1324910
    > Reviewed-by: Peter Conn <peconn@chromium.org>
    > Commit-Queue: Robert Sesek <rsesek@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#754033}
    
    TBR=rsesek@chromium.org,peconn@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 832124
    Change-Id: I0e51471e31a829149e1343611cfa58d78ea49825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2125826
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754343}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -959,7 +959,7 @@ public class CustomTabActivityTest {
 
         // Mark the intent as trusted so it can show more than one action button.
         IntentHandler.addTrustedIntentExtras(intent);
-        Assert.assertTrue(IntentHandler.wasIntentSenderChrome(intent));
+        Assert.assertTrue(IntentHandler.notSecureIsIntentChromeOrFirstParty(intent));
 
         ArrayList<Bundle> toolbarItems = new ArrayList<>(2);
         final PendingIntent pi1 = PendingIntent.getBroadcast(
@@ -1034,7 +1034,7 @@ public class CustomTabActivityTest {
         Intent intent = createMinimalCustomTabIntent();
 
         // By default, the intent should not be trusted.
-        Assert.assertFalse(IntentHandler.wasIntentSenderChrome(intent));
+        Assert.assertFalse(IntentHandler.notSecureIsIntentChromeOrFirstParty(intent));
 
         ArrayList<Bundle> toolbarItems = new ArrayList<>(2);
         final PendingIntent pi = PendingIntent.getBroadcast(
