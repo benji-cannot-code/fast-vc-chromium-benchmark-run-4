f4cf638d9b8592926e32de295a9381dacbc7174e
Rushan Suleymanov <rushans@google.com>
2020-03-30 17:25:31 +0000

    [Sync] Add feature flag to disable cache guid mismatch logic.
    This patch introduces the flag which may be used to disable the logic in
    Nigori which removes metadata on cache guid mismatch.
    
    When the flag is disabled, the cache guid from model_type_state will
    never be changed.
    
    Bug: 1063021
    Change-Id: If46232655e1903a3458a7880cb2d713be7395d14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2127206
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#754525}

--- components/sync/base/sync_base_switches.cc
+++ components/sync/base/sync_base_switches.cc
@@ -7,6 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
+const base::Feature kSyncNigoriRemoveMetadataOnCacheGuidMismatch{
+    "SyncNigoriRemoveMetadataOnCacheGuidMismatch",
+    base::FEATURE_ENABLED_BY_DEFAULT};
+
 // Force disables scrypt key derivation for custom passphrase. If this feature
 // is enabled, scrypt will be considered as an unsupported method, and Chrome
 // will not be able to access data encrypted using scrypt-derived keys (valid
