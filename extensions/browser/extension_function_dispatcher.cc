7ef613a2193b1e13f51dd301ba61d1ceb989fa4e
Peter Kasting <pkasting@chromium.org>
2020-03-30 18:59:46 +0000

    Collapse CheckPermissions() into CreateExtensionFunction().
    This combines both checks for "can the function be created" into one
    place, saving some code and scoping the error more clearly.
    
    HasPermission() only looks at a subset of the fields initialized by
    CreateExtensionFunction(); it does not care about the additional fields
    that were subsequently set in the caller.  It's therefore not worth it
    to pull all the additional data into CreateExtensionFunction() that
    would be needed to hoist those setter calls.
    
    Bug: none
    Change-Id: Ie8169a0c6388245afd399aeca775880bfd891ea0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2125575
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754571}

--- extensions/browser/extension_function_dispatcher.cc
+++ extensions/browser/extension_function_dispatcher.cc
@@ -52,8 +52,6 @@ using content::BrowserThread;
 namespace extensions {
 namespace {
 
-constexpr char kCreationFailed[] = "Access to extension API denied.";
-
 // Notifies the ApiActivityMonitor that an extension API function has been
 // called. May be called from any thread.
 void NotifyApiFunctionCalled(const std::string& extension_id,
@@ -337,9 +335,6 @@ void ExtensionFunctionDispatcher::DispatchWithCallbackInternal(
     function->set_include_incognito_information(true);
   }
 
-  if (!CheckPermissions(function.get(), params, callback))
-    return;
-
   if (!extension) {
     if (function->source_context_type() == Feature::WEBUI_CONTEXT) {
       base::UmaHistogramSparse("Extensions.Functions.WebUICalls",
@@ -457,19 +452,6 @@ ExtensionFunctionDispatcher::GetVisibleWebContents() const {
 }
 
 // static
-bool ExtensionFunctionDispatcher::CheckPermissions(
-    ExtensionFunction* function,
-    const ExtensionHostMsg_Request_Params& params,
-    const ExtensionFunction::ResponseCallback& callback) {
-  if (!function->HasPermission()) {
-    LOG(ERROR) << "Permission denied for " << params.name;
-    function->RespondWithError(kCreationFailed);
-    return false;
-  }
-  return true;
-}
-
-// static
 scoped_refptr<ExtensionFunction>
 ExtensionFunctionDispatcher::CreateExtensionFunction(
     const ExtensionHostMsg_Request_Params& params,
@@ -480,6 +462,8 @@ ExtensionFunctionDispatcher::CreateExtensionFunction(
     ExtensionAPI* api,
     void* profile_id,
     const ExtensionFunction::ResponseCallback& callback) {
+  constexpr char kCreationFailed[] = "Access to extension API denied.";
+
   scoped_refptr<ExtensionFunction> function =
       ExtensionFunctionRegistry::GetInstance().NewFunction(params.name);
   if (!function) {
@@ -499,6 +483,12 @@ ExtensionFunctionDispatcher::CreateExtensionFunction(
   function->set_source_context_type(process_map.GetMostLikelyContextType(
       extension, requesting_process_id, rfh_url));
   function->set_source_process_id(requesting_process_id);
+
+  if (!function->HasPermission()) {
+    LOG(ERROR) << "Permission denied for " << params.name;
+    function->RespondWithError(kCreationFailed);
+    return nullptr;
+  }
 
   return function;
 }
