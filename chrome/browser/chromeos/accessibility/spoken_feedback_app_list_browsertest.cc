06bfef020186e559efcb3c45a5666083948f8037
Johann Koenig <johannkoenig@google.com>
2020-03-30 00:44:40 +0000

    Revert "Remove the blocking apis in SpeechMonitor"
    This reverts commit 4e3c8ff865dc0e719d46be06e0c08397c9641eb3.
    
    Reason for revert: ChromeOS bot failures
    
    Bug: chromium:1065746
    
    Original change's description:
    > Remove the blocking apis in SpeechMonitor
    > 
    > - convert all callers to use the async api
    > - some misc cleanup in SpokenFeedbackBrowserTest
    > 
    > Run these against debug and release; use both 5 and 10 parallel jobs which should load the tests enough to catch flakes.
    > 
    > Test: browser_tests against all relevant suites (SelectToSpeak*, Spoken*, and *.EnableChromeVox).
    > Change-Id: I1b425b8640433b7608d84aa67f1cf5900ce26638
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124701
    > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    > Commit-Queue: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#754319}
    
    TBR=dmazzoni@chromium.org,dtseng@chromium.org,nancylingwang@chromium.org,nancylingwang@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I121b1d503547f3fc54597527ecbdb95ee65b9511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2126328
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#754374}

--- chrome/browser/chromeos/accessibility/spoken_feedback_app_list_browsertest.cc
+++ chrome/browser/chromeos/accessibility/spoken_feedback_app_list_browsertest.cc
@@ -9,12 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/test/test_search_result.h"
 #include "ash/app_list/views/app_list_view.h"
 #include "ash/shell.h"
+#include "base/strings/pattern.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/accessibility/spoken_feedback_browsertest.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/constants/chromeos_switches.h"
+#include "components/account_id/account_id.h"
 #include "components/user_manager/user_names.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -102,8 +105,8 @@ INSTANTIATE_TEST_SUITE_P(TestAsNormalAndGuestUser,
 IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest, LauncherStateTransition) {
   EnableChromeVox();
 
-  sm_.Call(
-      [this]() { EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF)); });
+  EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF));
+
   sm_.ExpectSpeechPattern("Launcher");
   sm_.ExpectSpeech("Button");
   sm_.ExpectSpeech("Shelf");
@@ -136,8 +139,8 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest,
                        DisabledFullscreenExpandButton) {
   EnableChromeVox();
 
-  sm_.Call(
-      [this]() { EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF)); });
+  EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF));
+
   sm_.ExpectSpeech("Shelf");
 
   // Press space on the launcher button in shelf, this opens peeking launcher.
@@ -173,8 +176,8 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest,
 
   EnableChromeVox();
 
-  sm_.Call(
-      [this]() { EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF)); });
+  EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF));
+
   sm_.ExpectSpeech("Press Search plus Space to activate");
   // Press space on the launcher button in shelf, this opens peeking
   // launcher.
@@ -210,8 +213,8 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest,
 
   EnableChromeVox();
 
-  sm_.Call(
-      [this]() { EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF)); });
+  EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF));
+
   sm_.ExpectSpeech("Press Search plus Space to activate");
   // Press space on the launcher button in shelf, this opens peeking
   // launcher.
@@ -255,12 +258,10 @@ IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest,
 IN_PROC_BROWSER_TEST_P(SpokenFeedbackAppListTest, NavigateAppLauncher) {
   EnableChromeVox();
 
-  sm_.Call([this]() {
-    // Add one app to the applist.
-    PopulateApps(1);
+  // Add one app to the applist.
+  PopulateApps(1);
 
-    EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF));
-  });
+  EXPECT_TRUE(PerformAcceleratorAction(ash::FOCUS_SHELF));
 
   // Wait for it to say "Launcher", "Button", "Shelf", "Tool bar".
   sm_.ExpectSpeechPattern("Launcher");
