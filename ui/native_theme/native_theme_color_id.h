1c7abd2ca25e55f5f3f502fc89c45a7d4d32b530
Thomas Lukaszewicz <tluk@chromium.org>
2020-03-30 21:07:32 +0000

    Allow profile avatars to take default icons for empty images
    Added the ability for callers of SetIdentityInfo() to specify which
    vector icon to fall back to in the case that an empty |image| is
    supplied.
    
    This allows the incognito profile menus to specify the incognito
    icon and color required.
    
    This is necessary as previously the incognito image was generated
    prior to having access to the Widget, resulting in incorrect colors.
    
    Bug: 1065294
    Change-Id: I4bc78e692abb7354e6cd5fcdeccd5483dc2c964a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2127410
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754642}

--- ui/native_theme/native_theme_color_id.h
+++ ui/native_theme/native_theme_color_id.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   OP(kColorId_DialogBackground),                                               \
   OP(kColorId_DialogForeground),                                               \
   OP(kColorId_BubbleBackground),                                               \
+  OP(kColorId_BubbleForeground),                                               \
   OP(kColorId_BubbleFooterBackground),                                         \
   /* FocusableBorder */                                                        \
   OP(kColorId_FocusedBorderColor),                                             \
