bc24cd23f8ad723f0181c657338bb7c3dbc52a5c
danakj <danakj@chromium.org>
2020-03-30 21:33:05 +0000

    Remove reset from contructor of TestInterfaces.
    Objects are all set up in a reasonable state at construction time
    already. No need to do a Reset, which also involves some IPCs back up
    to the browser.
    
    Change-Id: Ic4522f40bce99178c838d268af8499d36b0e5325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2128240
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754653}

--- content/shell/test_runner/test_interfaces.cc
+++ content/shell/test_runner/test_interfaces.cc
@@ -34,8 +34,6 @@ TestInterfaces::TestInterfaces()
   blink::SetWebTestMode(true);
   // NOTE: please don't put feature specific enable flags here,
   // instead add them to runtime_enabled_features.json5
-
-  ResetAll();
 }
 
 TestInterfaces::~TestInterfaces() {
