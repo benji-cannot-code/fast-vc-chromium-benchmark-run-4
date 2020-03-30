89db23fdc15eb13dc97e82b9e15e3a2980f4fc66
Sadrul Habib Chowdhury <sadrul@chromium.org>
2020-03-30 21:42:56 +0000

    [throughput] Report scrolling-thread metric in AllInteractions.
    We currently report the 'slower thread' metric in the aggregated
    top-level metrics (AllInteractions and AllSequences). But now that
    we have a 'scrolling thread' metric, report that instead.
    
    BUG=1035460
    
    Change-Id: Ide012ecf9c92aeeaba86fcdfeb812958e85a0656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2128370
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754658}

--- cc/metrics/frame_sequence_tracker.h
+++ cc/metrics/frame_sequence_tracker.h
@@ -72,12 +72,10 @@ class CC_EXPORT FrameSequenceMetrics {
 
     // Returns the throughput in percent, a return value of base::nullopt
     // indicates that no throughput metric is reported.
-    static base::Optional<int> ReportHistogram(
-        ThroughputUkmReporter* ukm_reporter,
-        FrameSequenceTrackerType sequence_type,
-        ThreadType thread_type,
-        int metric_index,
-        const ThroughputData& data);
+    static base::Optional<int> ReportHistogram(FrameSequenceMetrics* metrics,
+                                               ThreadType thread_type,
+                                               int metric_index,
+                                               const ThroughputData& data);
 
     void Merge(const ThroughputData& data) {
       frames_expected += data.frames_expected;
@@ -128,6 +126,11 @@ class CC_EXPORT FrameSequenceMetrics {
     frames_checkerboarded_ += frames;
   }
   uint32_t frames_checkerboarded() const { return frames_checkerboarded_; }
+
+  FrameSequenceTrackerType type() const { return type_; }
+  ThroughputUkmReporter* ukm_reporter() const {
+    return throughput_ukm_reporter_;
+  }
 
  private:
   void ComputeAggregatedThroughput();
