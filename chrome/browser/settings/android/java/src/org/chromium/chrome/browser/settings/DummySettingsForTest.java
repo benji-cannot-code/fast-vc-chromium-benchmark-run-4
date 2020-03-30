9a7fc95d954e29fb539d871f39b9975020a9a2be
Natalie Chouinard <chouinard@chromium.org>
2020-03-30 21:31:50 +0000

    Add tests for Managed Preference toast messages
    Bug: 1044401
    Change-Id: I7b8c69a390f47e86b864360d1913bf593c8b69ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124969
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754650}

---
+++ chrome/browser/settings/android/java/src/org/chromium/chrome/browser/settings/DummySettingsForTest.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.settings;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+/**
+ * A dummy {@link PreferenceFragmentCompat} for use in tests.
+ */
+public class DummySettingsForTest extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String rootKey) {
+        getActivity().setTitle("Dummy Settings");
+
+        Context context = getPreferenceManager().getContext();
+        PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(context);
+        setPreferenceScreen(screen);
+    }
+}
