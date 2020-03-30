f397e4d7db7d9db5f310276c8c12195c07b5cd83
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-03-30 13:14:21 +0000

    Import wpt@d38bfcb1f70b52abc8c7ee02f67ebcbb6057af70
    Using wpt-import in Chromium 5a7a5ec86f3b524b77b1988e0a6497931556e49d.
    With Chromium commits locally applied on WPT:
    d0ad7cf8c4 "UpgradeInsecureRequest: Add WPTs for static-import in dedicated/shared workers"
    6719ca2dab "[AspectRatio] Add parsing support for aspect-ratio: X / Y"
    9e853511d0 "Remove broken icon rendering for loading="lazy" images while loading"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: I961486f981badaf81b16b758cdfd0e103b0eeba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2125666
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#754455}

---
+++ third_party/blink/web_tests/external/wpt/css/css-fonts/support/font-family-keywords.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var kGenericFontFamilyKeywords = ["serif",
+                                  "sans-serif",
+                                  "cursive",
+                                  "fantasy",
+                                  "monospace",
+                                  "system",
+                                  "emoji",
+                                  "math",
+                                  "fangsong",
+                                  "ui-serif",
+                                  "ui-sans-serif",
+                                  "ui-monospace",
+                                  "ui-rounded"];
+
+var kWebKitPrefixKeywords = ["-webkit-body",
+                             "-webkit-standard",
+                             "-webkit-pictograph"];
