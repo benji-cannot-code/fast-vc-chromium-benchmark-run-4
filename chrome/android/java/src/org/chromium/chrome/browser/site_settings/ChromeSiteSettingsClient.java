8f613474f68b0a1d3e7166feff3649213327c253
Robbie McElrath <rmcelrath@chromium.org>
2020-03-30 19:00:18 +0000

    [WebLayer] Create a SiteSettingsClient interface.
    This interface is used by the Site Settings UI to get embedder-specific
    logic, and will be used to abstract Chrome-specific logic so the rest
    of the Site Settings UI can be componentized. The Client is set in the
    Fragment by SettingsActivity.
    
    As of this CL, only the ManagedPreferenceDelegate is accessible through
    this new interface, but other embedder-specific logic will be moved
    behind it as well in future CLs.
    
    See go/weblayer-site-settings for more information.
    
    Bug: 1058597
    Change-Id: I247cdc14fa6ac7cefd4e1b1d64b275b68ca7889e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119259
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754572}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/site_settings/ChromeSiteSettingsClient.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.site_settings;
+
+import androidx.preference.Preference;
+
+import org.chromium.chrome.browser.settings.ChromeManagedPreferenceDelegate;
+import org.chromium.components.browser_ui.settings.ManagedPreferenceDelegate;
+
+/**
+ * A SiteSettingsClient instance that contains Chrome-specific Site Settings logic.
+ */
+public class ChromeSiteSettingsClient implements SiteSettingsClient {
+    private ManagedPreferenceDelegate mManagedPreferenceDelegate;
+
+    @Override
+    public ManagedPreferenceDelegate getManagedPreferenceDelegate() {
+        if (mManagedPreferenceDelegate == null) {
+            mManagedPreferenceDelegate = new ChromeManagedPreferenceDelegate() {
+                @Override
+                public boolean isPreferenceControlledByPolicy(Preference preference) {
+                    return false;
+                }
+            };
+        }
+        return mManagedPreferenceDelegate;
+    }
+}
