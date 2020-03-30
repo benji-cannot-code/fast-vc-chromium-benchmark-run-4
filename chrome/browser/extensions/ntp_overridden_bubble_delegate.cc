54a118bd8a896e5f88c6c08b0d3d993028bfd433
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-03-30 21:44:05 +0000

    [Extensions] Enable the NTP overridden bubble on Mac
    The NTP overridden bubble prompts the user post-installation to ensure
    an extension that overrides the NTP was desired. It is currently enabled
    on ChromeOS and Windows; this CL enables it on MacOS as well.
    
    Already-installed extensions will be auto-acknowledged for the user so
    as to not be disruptive. Only newly-installed extensions will trigger
    the bubble.
    
    Bug: 1064670
    
    Change-Id: I58d8ff4f610def99c28dda7c8ee4d19933870e96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2120744
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754659}

--- chrome/browser/extensions/ntp_overridden_bubble_delegate.cc
+++ chrome/browser/extensions/ntp_overridden_bubble_delegate.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
+#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_web_ui.h"
 #include "chrome/browser/profiles/profile.h"
@@ -29,10 +30,10 @@ const char kDidAcknowledgeExistingNtpExtensions[] =
     "ack_existing_ntp_extensions";
 
 // Whether to acknowledge existing extensions overriding the NTP for the active
-// profile. Active on ChromeOS to rollout the NTP bubble without prompting for
+// profile. Active on MacOS to rollout the NTP bubble without prompting for
 // previously-installed extensions.
 bool g_acknowledge_existing_extensions =
-#if defined(OS_CHROMEOS)
+#if defined(OS_MACOSX)
     true;
 #else
     false;
