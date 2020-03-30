400d0de330ea4a2d2d2250ea09e6eaea272e1331
DongJun Kim <djmix.kim@samsung.com>
2020-03-30 16:00:32 +0000

    Add a new RequestDestination field in content::mojom::BeginNavigationParams
    This patch is step 3 in [1] and we adds a new RequestDestination field
    in the structs / params where only RequestContextType is used.
    If we are finish current step then RequestContextType field will be deprecated.
    
    [1] https://docs.google.com/document/d/1kXXnH_27aJYGqjwbXLW6Bs9fW2xFhvZs8gJcXb4xnhQ/edit
    
    Bug: 889751
    
    Signed-off-by: DongJun Kim <djmix.kim@samsung.com>
    Change-Id: Iedf80fc00e20e73d6780e0e6deb5ced83d04d13d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106708
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754491}

--- content/browser/frame_host/mixed_content_navigation_throttle.h
+++ content/browser/frame_host/mixed_content_navigation_throttle.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/navigation_throttle.h"
+#include "services/network/public/mojom/fetch_api.mojom.h"
 #include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom.h"
 #include "third_party/blink/public/mojom/web_feature/web_feature.mojom.h"
 #include "third_party/blink/public/platform/web_mixed_content_context_type.h"
