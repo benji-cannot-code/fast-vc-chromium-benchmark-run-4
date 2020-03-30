6261ad9db11a82cf850c4eb6d709c165b79c6233
John Z Wu <jzw@chromium.org>
2020-03-30 18:38:22 +0000

    Refactor sync internals web ui code to be shareable with ios/web_view
    Define a provider that needs to be implemented by embedders to provide
    embedder specific values.
    
    Change-Id: I33f929b7a26b997a293dd9e080126214c58fdefe
    Bug: 1062743
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119765
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754556}

---
+++ ios/chrome/browser/ui/webui/chrome_web_ui_provider.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/components/webui/web_ui_provider.h"
+
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/sync/profile_sync_service_factory.h"
+#include "ios/chrome/common/channel_info.h"
+
+namespace web_ui {
+
+syncer::SyncService* GetSyncServiceForWebUI(web::WebUIIOS* web_ui) {
+  ChromeBrowserState* browser_state = ChromeBrowserState::FromWebUIIOS(web_ui);
+  return ProfileSyncServiceFactory::GetForBrowserState(
+      browser_state->GetOriginalChromeBrowserState());
+}
+
+version_info::Channel GetChannel() {
+  return ::GetChannel();
+}
+
+}  // namespace web_ui
--- ios/components/webui/sync_internals/sync_internals_message_handler.mm
+++ ios/components/webui/sync_internals/sync_internals_message_handler.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.h"
+#include "ios/components/webui/sync_internals/sync_internals_message_handler.h"
 
 #include <utility>
 #include <vector>
@@ -22,11 +22,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/engine/cycle/update_counters.h"
 #include "components/sync/engine/events/protocol_event.h"
 #include "components/sync/js/js_event_details.h"
-#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
-#include "ios/chrome/browser/sync/profile_sync_service_factory.h"
-#include "ios/chrome/common/channel_info.h"
+#include "ios/components/webui/web_ui_provider.h"
 #include "ios/web/public/thread/web_thread.h"
 #include "ios/web/public/webui/web_ui_ios.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace {
 
@@ -327,16 +329,13 @@ void SyncInternalsMessageHandler::SendAboutInfo() {
   syncer::SyncService* sync_service = GetSyncService();
   std::unique_ptr<base::DictionaryValue> value =
       syncer::sync_ui_util::ConstructAboutInformation(sync_service,
-                                                      GetChannel());
+                                                      web_ui::GetChannel());
   DispatchEvent(syncer::sync_ui_util::kOnAboutInfoUpdated, *value);
 }
 
 // Gets the SyncService of the underlying original profile. May return null.
 syncer::SyncService* SyncInternalsMessageHandler::GetSyncService() {
-  ChromeBrowserState* browser_state =
-      ChromeBrowserState::FromWebUIIOS(web_ui());
-  return ProfileSyncServiceFactory::GetForBrowserState(
-      browser_state->GetOriginalChromeBrowserState());
+  return web_ui::GetSyncServiceForWebUI(web_ui());
 }
 
 void SyncInternalsMessageHandler::DispatchEvent(
