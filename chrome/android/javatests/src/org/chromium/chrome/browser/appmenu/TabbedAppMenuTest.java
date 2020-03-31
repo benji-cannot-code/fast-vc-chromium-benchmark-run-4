b2c6199707af522825ca5231b463cd8ac5558096
Theresa Wellington <twellington@chromium.org>
2020-03-31 15:30:32 +0000

    Disable TabbedAppMenuTest#testBookmarkMenuItem on tablets
    BUG=1065043
    TBR=mdjones@chromium.org
    
    Change-Id: I9121719064ed24a3a0c66623f6a29f06e4f808e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129782
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754930}

--- chrome/android/javatests/src/org/chromium/chrome/browser/appmenu/TabbedAppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/appmenu/TabbedAppMenuTest.java
@@ -23,6 +23,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
@@ -43,6 +44,7 @@ import org.chromium.content_public.browser.UiThreadTaskTraits;
 import org.chromium.content_public.browser.test.util.Criteria;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.ui.test.util.UiDisableIf;
 import org.chromium.ui.test.util.UiRestriction;
 
 import java.io.IOException;
@@ -232,6 +234,7 @@ public class TabbedAppMenuTest {
     @Test
     @SmallTest
     @Feature({"Browser", "Main", "Bookmark", "RenderTest"})
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // See https://crbug.com/1065043.
     public void testBookmarkMenuItem() throws IOException {
         MenuItem bookmarkStar =
                 AppMenuTestSupport.getMenu(mActivityTestRule.getAppMenuCoordinator())
