a8b4d671e69dac402fbb2047d75177cee4a71c91
Michael Cui <mlcui@google.com>
2020-03-31 02:47:36 +0000

    Demo Mode: Record dwell time metric
    Dwell time is the time the user spent interacting with the current demo
    session, measured from first user activity to last user activity.
    While there is an existing metric which measures this,
    DemoSession.SessionLength, there are some key differences:
    
    - SessionLength measures the duration to logout. DwellTime measures the
      duration to the last user activity.
    - SessionLength is measured to the nearest minute and uses linear
      bucketing up to 2 hours. DwellTime is measured to the nearest second
      and uses exponential bucketing up to 1 hour.
    - SessionLength persists across crashes and is reported at the start of
      the next session (possibly to ensure that metrics are sent correctly),
      while DwellTime is not crash-resistant and is reported at the end of
      the session.
    
    Bug: b/148752593
    Test: ash_unittests --gtest_filter="DemoSessionMetricsRecorderTest.DwellTime"
    Change-Id: I179c59ecd537f33e36e329ba9853b35198ccc793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2114319
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Patti <patricialor@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754786}

--- ash/metrics/demo_session_metrics_recorder.cc
+++ ash/metrics/demo_session_metrics_recorder.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/window_properties.h"
 #include "ash/shelf/shelf_window_watcher.h"
 #include "ash/shell.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/scoped_observer.h"
 #include "base/time/time.h"
@@ -162,6 +163,20 @@ DemoModeApp GetAppFromWindow(const aura::Window* window) {
   return DemoModeApp::kOtherWindow;
 }
 
+// Identical to UmaHistogramLongTimes100, but reports times with second
+// granularity instead of millisecond granularity.
+// This significantly improves the bucketing if millisecond granularity is
+// not required - 90/100 buckets are greater than 10 seconds, compared to
+// 43/100 buckets using millisecond accuracy with min=1ms, or
+// 72/100 buckets using millisecond accuracy with min=1000ms.
+void ReportHistogramLongSecondsTimes100(const char* name,
+                                        base::TimeDelta sample) {
+  // We use a max of 1 hour = 60 * 60 secs.
+  base::UmaHistogramCustomCounts(name,
+                                 base::saturated_cast<int>(sample.InSeconds()),
+                                 /*min=*/1, /*max=*/60 * 60, /*buckets=*/100);
+}
+
 }  // namespace
 
 // Observes for changes in a window's ArcPackageName property for the purpose of
@@ -272,9 +287,14 @@ DemoSessionMetricsRecorder::DemoSessionMetricsRecorder(
 }
 
 DemoSessionMetricsRecorder::~DemoSessionMetricsRecorder() {
+  // TODO(mlcui): Investigate whether the metrics emitted here are gracefully
+  // handled during session / device shutdown.
+
   // Report any remaining stored samples on exit. (If the user went idle, there
   // won't be any.)
   ReportSamples();
+
+  ReportDwellTime();
 
   // Unsubscribe from window activation events.
   activation_client_->RemoveObserver(this);
@@ -354,6 +374,12 @@ void DemoSessionMetricsRecorder::OnWindowActivated(ActivationReason reason,
 }
 
 void DemoSessionMetricsRecorder::OnUserActivity(const ui::Event* event) {
+  // Record the first and last time activity was observed.
+  if (first_user_activity_.is_null()) {
+    first_user_activity_ = base::TimeTicks::Now();
+  }
+  last_user_activity_ = base::TimeTicks::Now();
+
   // Report samples recorded since the last activity.
   ReportSamples();
 
@@ -410,6 +436,18 @@ void DemoSessionMetricsRecorder::ReportUniqueAppsLaunched() {
     UMA_HISTOGRAM_COUNTS_100("DemoMode.UniqueAppsLaunched",
                              unique_apps_launched_.size());
   unique_apps_launched_.clear();
+}
+
+void DemoSessionMetricsRecorder::ReportDwellTime() {
+  if (!first_user_activity_.is_null()) {
+    DCHECK(!last_user_activity_.is_null());
+    DCHECK_LE(first_user_activity_, last_user_activity_);
+
+    base::TimeDelta dwell_time = last_user_activity_ - first_user_activity_;
+    ReportHistogramLongSecondsTimes100("DemoMode.DwellTime", dwell_time);
+  }
+  first_user_activity_ = base::TimeTicks();
+  last_user_activity_ = base::TimeTicks();
 }
 
 }  // namespace ash
