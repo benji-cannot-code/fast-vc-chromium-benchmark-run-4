51d3117496a76eeed98a5bc72f107dedc62f1313
Morten Stenshorne <mstensho@chromium.org>
2020-03-31 01:06:38 +0000

    CSS parser and layout support for the 'page' property.
    There were already some stubs for the 'page' property, but it couldn't
    be used for anything. Move it behind a runtime flag and implement it.
    Also add legacy layout engine support. Will add to LayoutNG once it
    supports printing.
    
    Bug: 1053768
    Change-Id: I2b7eff133c925a67ef36db4b6fa259cf934abf75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2126708
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754753}

--- third_party/blink/renderer/core/layout/layout_state.h
+++ third_party/blink/renderer/core/layout/layout_state.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/geometry/layout_rect.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
+#include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
 
@@ -91,6 +92,8 @@ class LayoutState {
     height_offset_for_table_footers_ = offset;
   }
 
+  const AtomicString& PageName() const { return page_name_; }
+
   const LayoutSize& PaginationOffset() const { return pagination_offset_; }
   bool ContainingBlockLogicalWidthChanged() const {
     return containing_block_logical_width_changed_;
@@ -128,6 +131,8 @@ class LayoutState {
   // The height we need to make available for repeating table footers in
   // paginated layout.
   LayoutUnit height_offset_for_table_footers_;
+
+  AtomicString page_name_;
 
   LayoutObject& layout_object_;
   DISALLOW_COPY_AND_ASSIGN(LayoutState);
