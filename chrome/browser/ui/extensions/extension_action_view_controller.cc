8804f04e6267aac231ff76c6dd977394526c76f7
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-03-31 01:14:11 +0000

    [Extensions UI] Simplify accelerator priority logic
    Extensions used to be able to override the bookmark star view in order
    to provide a custom bookmark manager (this was used for the Stars
    extension). The extension has been deprecated and removed, and the API
    never launched. Clean up the remnants.
    
    When an extension overrode the bookmark shortcuts, the accelerators were
    given normal priority, whereas other extension accelerators are given
    high priority (in order to override the default shortcut in Chrome).
    Now, this can be simplified to have all extension shortcuts be high
    priority. Replace the GetAcceleratorPriority() logic with a constant,
    and update call sites appropriately.
    
    Bug: 894447
    Change-Id: I7a87f323f4c91f8d13bed60c4f845dbe7de45ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2120291
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754756}

--- chrome/browser/ui/extensions/extension_action_view_controller.cc
+++ chrome/browser/ui/extensions/extension_action_view_controller.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_view_host_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/extensions/accelerator_priority.h"
 #include "chrome/browser/ui/extensions/extension_action_platform_delegate.h"
 #include "chrome/browser/ui/extensions/extensions_container.h"
 #include "chrome/browser/ui/extensions/icon_with_badge_image_source.h"
--- chrome/browser/ui/views/extensions/extension_keybinding_registry_views.cc
+++ chrome/browser/ui/views/extensions/extension_keybinding_registry_views.cc
@@ -51,8 +51,8 @@ void ExtensionKeybindingRegistryViews::AddExtensionKeybindings(
       continue;
     const ui::Accelerator &accelerator = iter->second.accelerator();
     if (!IsAcceleratorRegistered(accelerator)) {
-      focus_manager_->RegisterAccelerator(
-          accelerator, GetAcceleratorPriority(accelerator, extension), this);
+      focus_manager_->RegisterAccelerator(accelerator,
+                                          kExtensionAcceleratorPriority, this);
     }
 
     AddEventTarget(accelerator, extension->id(), iter->second.command_name());
@@ -72,14 +72,7 @@ void ExtensionKeybindingRegistryViews::OnShortcutHandlingSuspended(
 
 bool ExtensionKeybindingRegistryViews::AcceleratorPressed(
     const ui::Accelerator& accelerator) {
-  std::string extension_id, command_name;
-  GetFirstTarget(accelerator, &extension_id, &command_name);
-  const ui::AcceleratorManager::HandlerPriority priority =
-      GetAcceleratorPriorityById(accelerator, extension_id, browser_context());
-  // Normal priority shortcuts must be handled via standard browser commands to
-  // be processed at the proper time.
-  return (priority == ui::AcceleratorManager::kHighPriority) &&
-      ExtensionKeybindingRegistry::NotifyEventTargets(accelerator);
+  return ExtensionKeybindingRegistry::NotifyEventTargets(accelerator);
 }
 
 bool ExtensionKeybindingRegistryViews::CanHandleAccelerators() const {
