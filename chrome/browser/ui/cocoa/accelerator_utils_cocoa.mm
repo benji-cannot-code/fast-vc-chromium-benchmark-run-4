5054552b13e4b1006a8bd4c80a393df64178acc0
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-03-31 19:48:29 +0000

    [UI Cleanup] Remove GetPrimaryChromeAcceleratorForBookmarkTab()
    It's no longer used.
    
    Bug: 894447
    Change-Id: Ic81eb4d6bd34ad067e1f753af9300db444dc543b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129283
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755065}

--- chrome/browser/ui/cocoa/accelerator_utils_cocoa.mm
+++ chrome/browser/ui/cocoa/accelerator_utils_cocoa.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Cocoa/Cocoa.h>
 
-#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/global_keyboard_shortcuts_mac.h"
 #include "chrome/browser/ui/cocoa/accelerators_cocoa.h"
 #include "ui/base/accelerators/accelerator.h"
@@ -51,14 +50,6 @@ bool IsChromeAccelerator(const ui::Accelerator& accelerator) {
                                      keyCode:mac_keycode];
 
   return CommandForKeyEvent(event).found();
-}
-
-ui::Accelerator GetPrimaryChromeAcceleratorForBookmarkTab() {
-  const ui::Accelerator* accelerator =
-      AcceleratorsCocoa::GetInstance()->GetAcceleratorForCommand(
-          IDC_BOOKMARK_THIS_TAB);
-
-  return accelerator ? *accelerator : ui::Accelerator();
 }
 
 }  // namespace chrome
--- chrome/browser/ui/views/accelerator_utils_aura.cc
+++ chrome/browser/ui/views/accelerator_utils_aura.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 
-#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/views/accelerator_table.h"
 #include "ui/base/accelerators/accelerator.h"
 
@@ -34,18 +33,6 @@ bool IsChromeAccelerator(const ui::Accelerator& accelerator) {
   }
 
   return false;
-}
-
-ui::Accelerator GetPrimaryChromeAcceleratorForBookmarkTab() {
-  std::vector<AcceleratorMapping> accelerators = GetAcceleratorList();
-  for (size_t i = 0; i < accelerators.size(); ++i) {
-    if (accelerators[i].command_id == IDC_BOOKMARK_THIS_TAB) {
-      return ui::Accelerator(accelerators[i].keycode,
-                             accelerators[i].modifiers);
-    }
-  }
-
-  return ui::Accelerator();
 }
 
 }  // namespace chrome
