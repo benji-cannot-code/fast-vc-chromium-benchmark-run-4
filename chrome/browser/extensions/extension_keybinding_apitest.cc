6f6d98b6bc6f42642a1337695df9afd4b5a40699
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-03-31 19:36:44 +0000

    [Extensions] Remove Chrome UI Overrides, corresponding switches
    The Chrome UI Overrides allowed extensions to override the bookmarks UI,
    but never shipped. This CL removes most of the remaining core bits.
    Specifically,
    - The _manifest_features.json entry
    - The manifest_types.json entry
    - The associated ManifestHandler (UIOverrides)
    - The associated FeatureSwitch (enable_override_bookmarks_ui)
    - The associated constants
    - The associated commandline switches
      ("enable-override-bookmarks-ui")
    
    Bug: 894447
    Change-Id: I673485dcb5f5e414c449476cd6169f862c48aa69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129282
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755056}

--- chrome/browser/extensions/extension_keybinding_apitest.cc
+++ chrome/browser/extensions/extension_keybinding_apitest.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/command_line.h"
 #include "base/macros.h"
 #include "base/stl_util.h"
 #include "base/threading/thread_restrictions.h"
@@ -525,10 +524,6 @@ IN_PROC_BROWSER_TEST_F(CommandsApiTest,
   ASSERT_TRUE(embedded_test_server()->Start());
 
   ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
-
-  // This functionality requires a feature flag.
-  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-      "--enable-override-bookmarks-ui", "1");
 
   ASSERT_TRUE(RunExtensionTest("keybinding/basics"))
       << message_;
--- chrome/common/extensions/chrome_manifest_handlers.cc
+++ chrome/common/extensions/chrome_manifest_handlers.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/manifest_handlers/natively_connectable_handler.h"
 #include "chrome/common/extensions/manifest_handlers/settings_overrides_handler.h"
 #include "chrome/common/extensions/manifest_handlers/theme_handler.h"
-#include "chrome/common/extensions/manifest_handlers/ui_overrides_handler.h"
 #include "extensions/common/manifest_handlers/app_isolation_info.h"
 #include "extensions/common/manifest_handlers/automation.h"
 #include "extensions/common/manifest_handlers/options_page_info.h"
@@ -68,7 +67,6 @@ void RegisterChromeManifestHandlers() {
   registry->RegisterHandler(std::make_unique<SystemIndicatorHandler>());
   registry->RegisterHandler(std::make_unique<ThemeHandler>());
   registry->RegisterHandler(std::make_unique<TtsEngineManifestHandler>());
-  registry->RegisterHandler(std::make_unique<UIOverridesHandler>());
   registry->RegisterHandler(std::make_unique<UrlHandlersParser>());
   registry->RegisterHandler(std::make_unique<URLOverridesHandler>());
 #if defined(OS_CHROMEOS)
