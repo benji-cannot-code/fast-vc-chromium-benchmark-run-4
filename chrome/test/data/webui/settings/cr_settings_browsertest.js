1403d9d506178dbc6ae2a05edb7d0b5e6a73117f
sauski <sauski@google.com>
2020-03-31 19:53:16 +0000

    Re-enable Privacy Page UMA logging tests
    Troublesome Privacy Page UMA logging tests were disabled in anticipation
    of a major UMA rework. That rework has come and gone but erroneously
    did not renable the privacy_page specific tests.
    
    Fixed: 1066182
    Change-Id: I9291aa353aeebd581f164615e9e6033d9e53dbad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129698
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755069}

--- chrome/test/data/webui/settings/cr_settings_browsertest.js
+++ chrome/test/data/webui/settings/cr_settings_browsertest.js
@@ -1051,8 +1051,7 @@ TEST_F(
       runMochaSuite('PrivacyPageSound');
     });
 
-// TODO(sauski): Privacy page UMA changing, tests need updating/removal.
-TEST_F('CrSettingsPrivacyPageTest', 'DISABLED_UMALoggingTests', function() {
+TEST_F('CrSettingsPrivacyPageTest', 'UMALoggingTests', function() {
   runMochaSuite('PrivacyPageUMACheck');
 });
 
