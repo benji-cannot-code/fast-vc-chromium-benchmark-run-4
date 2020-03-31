ac8c8ab42763251548d44aeaeae1b37009601f9d
Timothy Loh <timloh@chromium.org>
2020-03-31 23:00:05 +0000

    Fix routing for Plugin VM OS settings pages
    This CL fixes the routing for the OS settings pages pluginVm/details
    and pluginVm/sharedPaths, which the File Manager relies on.
    
    Bug: b:152708714
    Change-Id: Id5bb0272c70dbf4097bbb675d897dd22af830e10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129453
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755151}

--- chrome/common/webui_url_constants.cc
+++ chrome/common/webui_url_constants.cc
@@ -479,6 +479,8 @@ bool IsOSSettingsSubPage(const std::string& sub_page) {
       kNativePrintingSettingsSubPage,
       kNetworkDetailSubPage,
       kNetworksSubPage,
+      kPluginVmDetailsSubPage,
+      kPluginVmSharedPathsSubPage,
       kPointerOverlaySubPage,
       kPowerSubPage,
       // reset is both an OS and browser sub page, but prefer the OS version
