aadb17e1d0b0a0ede680f04e130858992c75bf8a
Danyao Wang <danyao@chromium.org>
2020-03-31 20:23:18 +0000

    [Payment Request] Move payments JNI header generation into own target.
    This allows adding new payments-specific JNI files without always having
    to get //chrome/android OWNERS review.
    
    Bug: 1063118
    Change-Id: Ia2c86780c7527ea871902664496105908d2532ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129026
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755091}

--- chrome/browser/payments/android/payment_manifest_web_data_service_android.cc
+++ chrome/browser/payments/android/payment_manifest_web_data_service_android.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
-#include "chrome/android/chrome_jni_headers/PaymentManifestWebDataService_jni.h"
+#include "chrome/browser/payments/android/jni_headers/PaymentManifestWebDataService_jni.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/web_data_service_factory.h"
 #include "components/keyed_service/core/service_access_type.h"
--- chrome/browser/payments/android/ssl_validity_checker_android.cc
+++ chrome/browser/payments/android/ssl_validity_checker_android.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
-#include "chrome/android/chrome_jni_headers/SslValidityChecker_jni.h"
+#include "chrome/browser/payments/android/jni_headers/SslValidityChecker_jni.h"
 #include "chrome/browser/payments/ssl_validity_checker.h"
 #include "content/public/browser/web_contents.h"
 
