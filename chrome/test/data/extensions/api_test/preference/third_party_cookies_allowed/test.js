775b04e5cd9f0ef37f0ca1ec580b24c6781e25d6
Christian Dullweber <dullweber@chromium.org>
2020-03-31 20:42:23 +0000

    Set kCookieControlsMode when kBlockThirdParty cookie is set by extension
    An extension can use the privacy.websites.ThirdPartyCookiesAllowed
    extension api to set the kBlockThirdPartyCookies preference.
    kCookieControlsMode needs to be set to the corresponding value if
    kBlockThirdPartyCookies is controlled by extension.
    
    Bug: 1065392
    Change-Id: Ib450349cb33251b3eccc06ff2dcc18a0899a028e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2127034
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#755106}

---
+++ chrome/test/data/extensions/api_test/preference/third_party_cookies_allowed/test.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Content settings API test
+// Run with browser_tests
+// --gtest_filter=ExtensionPreferenceApiTest.ThirdPartyCookiesAllowed
+
+function setTrue() {
+  return new Promise((resolve) => {
+    chrome.privacy.websites.thirdPartyCookiesAllowed.set({ value: true },
+      () => chrome.test.sendMessage("set to true", resolve))
+  });
+}
+
+function setFalse() {
+  return new Promise((resolve) => {
+    chrome.privacy.websites.thirdPartyCookiesAllowed.set({ value: false },
+      () => chrome.test.sendMessage("set to false", resolve))
+  });
+}
+
+function clearPref() {
+  return new Promise((resolve) => {
+    chrome.privacy.websites.thirdPartyCookiesAllowed.clear({},
+      () => chrome.test.sendMessage("cleared", resolve))
+  });
+}
+
+(async () => {
+  await setTrue();
+  await clearPref();
+  await setFalse();
+  chrome.test.sendMessage("done");
+})();
