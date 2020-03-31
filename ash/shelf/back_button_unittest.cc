04fe448dc5c7267b5c5503cfbf76c37da3540030
Anastasia Helfinstein <anastasi@google.com>
2020-03-31 00:56:07 +0000

    Add dialog to confirm before disabling Switch Access
    Relnotes: N/A
    Bug: 996871
    Change-Id: I8232084777882e247676e4e3350d94d11a711418
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122613
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#754748}

--- ash/shelf/back_button_unittest.cc
+++ ash/shelf/back_button_unittest.cc
@@ -127,6 +127,9 @@ class BackButtonVisibilityWithAccessibilityFeaturesTest
       case TestAccessibilityFeature::kSwitchAccess:
         Shell::Get()->accessibility_controller()->SetSwitchAccessEnabled(
             enabled);
+        Shell::Get()
+            ->accessibility_controller()
+            ->no_switch_access_disable_confirmation_dialog_for_testing(true);
         break;
     }
   }
