6c4583f6788aa2ee57a2acb44ca54f6bd6ac7b33
Qijiang Fan <fqj@chromium.org>
2020-03-31 17:04:32 +0000

    Fix integer overflow if microseconds is too small
    microseconds - kMicrosecondsPerMillisecond could cause an overflow if
    microseconds is too small (close to INT64_MIN)
    
    Bug: 1065504
    Change-Id: I827dc3479eabbf1be5fdf964dbcb34bc48d7f401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129788
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Qijiang Fan <fqj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754979}

--- base/time/time_exploded_ios.cc
+++ base/time/time_exploded_ios.cc
@@ -107,9 +107,8 @@ void Time::Explode(bool is_local, Exploded* exploded) const {
   // Calculate milliseconds ourselves, since we rounded the |seconds|, making
   // sure to round towards -infinity.
   exploded->millisecond =
-      (microsecond >= 0) ? microsecond / kMicrosecondsPerMillisecond :
-                           (microsecond - kMicrosecondsPerMillisecond + 1) /
-                               kMicrosecondsPerMillisecond;
+      (microsecond >= 0) ? microsecond / kMicrosecondsPerMillisecond
+                         : ((microsecond + 1) / kMicrosecondsPerMillisecond - 1)
 }
 
 }  // namespace base
