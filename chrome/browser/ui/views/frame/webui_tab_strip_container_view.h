d8685d1e6fa12f043e92d2d5216cc616cf04ba2a
Collin Baker <collinbaker@chromium.org>
2020-03-31 21:53:44 +0000

    Treat up-swipe as a close regardless of container height
    When the user doesn't swipe, the container is animated open or closed based on
    the final height from dragging upon gesture end. Previously, only down-swipes
    were treated specially: swiping down fast enough opened the container regardless
    of its height.
    
    This treats swiping up specially too, causing the container to close.
    
    Bug: None
    Change-Id: I7555e00dbddb21128175d36a2209139c15cf52d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2128968
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755129}

--- chrome/browser/ui/views/frame/webui_tab_strip_container_view.h
+++ chrome/browser/ui/views/frame/webui_tab_strip_container_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_FRAME_WEBUI_TAB_STRIP_CONTAINER_VIEW_H_
 
 #include <memory>
+#include <set>
 
 #include "base/optional.h"
 #include "base/scoped_observer.h"
@@ -93,10 +94,16 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
   // Called as we are dragged open.
   void UpdateHeightForDragToOpen(float height_delta);
 
-  // Called when drag-to-open finishes. If |fling_to_open| is true, the
-  // user released their touch with a high enough velocity that we
-  // should animate open regardless of the final height.
-  void EndDragToOpen(bool fling_to_open);
+  enum class FlingDirection {
+    kUp,
+    kDown,
+  };
+
+  // Called when drag-to-open finishes. If |fling_direction| is present,
+  // the user released their touch with a high velocity. We should use
+  // just this direction to animate open or closed.
+  void EndDragToOpen(
+      base::Optional<FlingDirection> fling_direction = base::nullopt);
 
   void SetContainerTargetVisibility(bool target_visible);
 
