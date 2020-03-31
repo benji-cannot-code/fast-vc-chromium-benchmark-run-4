73d4d8b05c0278d6b4bf88a072c7ca353bda0c6b
Ewann <ewannpv@google.com>
2020-03-31 12:26:58 +0000

    [iOS] Updates privacy coordinator
    This CL refactor the privacy table view controller.
    
    Bug: 1064961
    Change-Id: I2ae5c23305c3d26cd61388b0a3ef925ad146f004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2126233
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754884}

--- ios/chrome/browser/ui/settings/privacy/privacy_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/privacy/privacy_table_view_controller.mm
@@ -19,9 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_cell.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
-#import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_ui_delegate.h"
-#import "ios/chrome/browser/ui/settings/privacy/handoff_table_view_controller.h"
+#import "ios/chrome/browser/ui/settings/privacy/privacy_navigation_commands.h"
 #import "ios/chrome/browser/ui/settings/settings_navigation_controller.h"
 #import "ios/chrome/browser/ui/settings/settings_table_view_controller_constants.h"
 #import "ios/chrome/browser/ui/table_view/cells/table_view_detail_icon_item.h"
@@ -212,32 +211,16 @@ const char kGoogleServicesSettingsURL[] = "settings://open_google_services";
     didSelectRowAtIndexPath:(NSIndexPath*)indexPath {
   [super tableView:tableView didSelectRowAtIndexPath:indexPath];
   NSInteger itemType = [self.tableViewModel itemTypeForIndexPath:indexPath];
-
-  // Items that push a new view controller.
-  UIViewController<SettingsRootViewControlling>* controller;
-
   switch (itemType) {
     case ItemTypeOtherDevicesHandoff:
-      controller = [[HandoffTableViewController alloc]
-          initWithBrowserState:_browserState];
+      [self.handler showHandoff];
       break;
-    case ItemTypeClearBrowsingDataClear: {
-      ClearBrowsingDataTableViewController* clearBrowsingDataViewController =
-          [[ClearBrowsingDataTableViewController alloc]
-              initWithBrowser:_browser];
-      clearBrowsingDataViewController.delegate = self;
-      controller = clearBrowsingDataViewController;
+    case ItemTypeClearBrowsingDataClear:
+      [self.handler showClearBrowsingData];
       break;
-    }
     default:
       break;
   }
-
-  if (controller) {
-    controller.dispatcher = self.dispatcher;
-    [self.navigationController pushViewController:controller animated:YES];
-  }
-
   [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
