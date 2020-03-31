268e2debcbdaf980c30cfd3a4c8043b8fd366231
Clemens Arbesser <arbesser@google.com>
2020-03-31 15:07:36 +0000

    [Autofill Assistant] Added toggle buttons to ui framework.
    Bug: b/152300352
    Change-Id: Idf29c4edce98025f395a91fed76598335065970e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124607
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Cr-Commit-Position: refs/heads/master@{#754923}

--- chrome/browser/android/autofill_assistant/generic_ui_interactions_android.h
+++ chrome/browser/android/autofill_assistant/generic_ui_interactions_android.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/android/autofill_assistant/interaction_handler_android.h"
 #include "components/autofill_assistant/browser/basic_interactions.h"
 #include "components/autofill_assistant/browser/interactions.pb.h"
 
@@ -64,6 +65,20 @@ void SetViewText(
 void SetViewVisibility(
     base::WeakPtr<UserModel> user_model,
     const SetViewVisibilityProto& proto,
+    std::map<std::string, base::android::ScopedJavaGlobalRef<jobject>>* views);
+
+// A simple wrapper around a basic interaction, needed because we can't directly
+// bind a repeating callback to a method with non-void return value.
+void RunConditionalCallback(
+    base::WeakPtr<BasicInteractions> basic_interactions,
+    const std::string& condition_identifier,
+    InteractionHandlerAndroid::InteractionCallback callback);
+
+// Sets the checked state of a toggle button.
+void SetToggleButtonChecked(
+    base::WeakPtr<UserModel> user_model,
+    const std::string& view_identifier,
+    const std::string& model_identifier,
     std::map<std::string, base::android::ScopedJavaGlobalRef<jobject>>* views);
 
 }  // namespace android_interactions
