9078be5f8140cff1827ac008f6cacc1b7c68e491
Ewann <ewannpv@google.com>
2020-03-31 09:30:22 +0000

    [iOS] Adds coordinator to Privacy files
    This CL adds a coordinator to the privacy table view controller.
    Before that the controller was also the coordinator.
    
    Bug: 1064961
    Change-Id: I781924cc24b4c8aae0e957e1e504e95ece8953b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2120423
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#754849}

---
+++ ios/chrome/browser/ui/settings/privacy/privacy_coordinator.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_PRIVACY_PRIVACY_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_PRIVACY_PRIVACY_COORDINATOR_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/browser/ui/coordinators/chrome_coordinator.h"
+
+@class PrivacyCoordinator;
+
+// Delegate that allows to dereference the PrivacyCoordinator.
+@protocol PrivacyCoordinatorDelegate
+
+// Called when the view controller is removed from navigation controller.
+- (void)privacyCoordinatorViewControllerWasRemoved:
+    (PrivacyCoordinator*)coordinator;
+
+@end
+
+// The coordinator for the Privacy screen.
+@interface PrivacyCoordinator : ChromeCoordinator
+
+@property(nonatomic, weak) id<PrivacyCoordinatorDelegate> delegate;
+
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+    NS_UNAVAILABLE;
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+                              browserState:(ChromeBrowserState*)browserState
+    NS_UNAVAILABLE;
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+                                   browser:(Browser*)browser NS_UNAVAILABLE;
+
+- (instancetype)initWithBaseNavigationController:
+                    (UINavigationController*)navigationController
+                                         browser:(Browser*)browser
+    NS_DESIGNATED_INITIALIZER;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_PRIVACY_PRIVACY_COORDINATOR_H_
--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/language/language_settings_mediator.h"
 #import "ios/chrome/browser/ui/settings/language/language_settings_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/password/passwords_table_view_controller.h"
+#import "ios/chrome/browser/ui/settings/privacy/privacy_coordinator.h"
 #import "ios/chrome/browser/ui/settings/privacy/privacy_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/search_engine_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/settings_table_view_controller_constants.h"
@@ -161,6 +162,7 @@ NSString* kDevViewSourceKey = @"DevViewSource";
     GoogleServicesSettingsCoordinatorDelegate,
     IdentityManagerObserverBridgeDelegate,
     PrefObserverDelegate,
+    PrivacyCoordinatorDelegate,
     SettingsControllerProtocol,
     SearchEngineObserving,
     SigninPresenter,
@@ -190,6 +192,9 @@ NSString* kDevViewSourceKey = @"DevViewSource";
   // identity update notifications.
   SigninPromoViewMediator* _signinPromoViewMediator;
   GoogleServicesSettingsCoordinator* _googleServicesSettingsCoordinator;
+
+  // Privacy coordinator.
+  PrivacyCoordinator* _privacyCoordinator;
 
   // Cached resized profile image.
   UIImage* _resizedImage;
@@ -826,8 +831,7 @@ NSString* kDevViewSourceKey = @"DevViewSource";
           initWithPrefs:_browserState->GetPrefs()];
       break;
     case ItemTypePrivacy:
-      controller =
-          [[PrivacyTableViewController alloc] initWithBrowser:_browser];
+      [self showPrivacy];
       break;
     case ItemTypeLanguageSettings: {
       LanguageSettingsMediator* mediator =
@@ -928,6 +932,16 @@ NSString* kDevViewSourceKey = @"DevViewSource";
                                       mode:GoogleServicesSettingsModeSettings];
   _googleServicesSettingsCoordinator.delegate = self;
   [_googleServicesSettingsCoordinator start];
+}
+
+// Shows Privacy screen.
+- (void)showPrivacy {
+  DCHECK(!_privacyCoordinator);
+  _privacyCoordinator = [[PrivacyCoordinator alloc]
+      initWithBaseNavigationController:self.navigationController
+                               browser:_browser];
+  _privacyCoordinator.delegate = self;
+  [_privacyCoordinator start];
 }
 
 // Sets the NSUserDefaults BOOL |value| for |key|.
@@ -1088,9 +1102,14 @@ NSString* kDevViewSourceKey = @"DevViewSource";
 
 - (void)settingsWillBeDismissed {
   DCHECK(!_settingsHasBeenDismissed);
+
   [_googleServicesSettingsCoordinator stop];
   _googleServicesSettingsCoordinator.delegate = nil;
   _googleServicesSettingsCoordinator = nil;
+
+  [_privacyCoordinator stop];
+  _privacyCoordinator = nil;
+
   _settingsHasBeenDismissed = YES;
   [self.signinInteractionCoordinator cancel];
   [_signinPromoViewMediator signinPromoViewIsRemoved];
@@ -1268,6 +1287,15 @@ NSString* kDevViewSourceKey = @"DevViewSource";
   [_googleServicesSettingsCoordinator stop];
   _googleServicesSettingsCoordinator.delegate = nil;
   _googleServicesSettingsCoordinator = nil;
+}
+
+#pragma mark - PrivacyCoordinatorDelegate
+
+- (void)privacyCoordinatorViewControllerWasRemoved:
+    (PrivacyCoordinator*)coordinator {
+  DCHECK_EQ(_privacyCoordinator, coordinator);
+  [_privacyCoordinator stop];
+  _privacyCoordinator = nil;
 }
 
 #pragma mark - IdentityManagerObserverBridgeDelegate
