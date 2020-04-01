34a61f8285ebca9b69ef112215cdcec43270f903
Alexandre Courbot <acourbot@chromium.org>
2020-04-01 04:32:35 +0000

    media/gpu/v4l2: remove unused argument from workarounds
    The V4L2 workaround interface was taking the end position of the encoded
    stream as a writable argument - this is dangerous and it doesn't even
    need it, so remove it.
    
    BUG=b:149663704
    TEST=vdatest passes on Hana.
    
    Change-Id: I6ec9b137e6e762fd999e6ae2462db4564b0e7134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130452
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755246}

--- media/gpu/v4l2/v4l2_stateful_workaround.cc
+++ media/gpu/v4l2/v4l2_stateful_workaround.cc
@@ -35,7 +35,7 @@ class SupportResolutionChecker : public V4L2StatefulWorkaround {
       VideoCodecProfile profile);
   ~SupportResolutionChecker() override = default;
 
-  Result Apply(const uint8_t* data, size_t size, size_t* endpos) override;
+  Result Apply(const uint8_t* data, size_t size) override;
 
  private:
   using SupportedProfileMap = base::small_map<
@@ -95,8 +95,7 @@ SupportResolutionChecker::CreateIfNeeded(V4L2Device::Type device_type,
 
 V4L2StatefulWorkaround::Result SupportResolutionChecker::Apply(
     const uint8_t* data,
-    size_t size,
-    size_t* endpos) {
+    size_t size) {
   Vp8FrameHeader fhdr;
   vp8_parser_->ParseFrame(data, size, &fhdr);
   if (fhdr.IsKeyframe()) {
