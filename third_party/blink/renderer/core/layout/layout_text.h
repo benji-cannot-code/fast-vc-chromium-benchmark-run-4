37c9fece5d3d68f66abcfd0ae8fad61b29085c82
Koji Ishii <kojii@chromium.org>
2020-04-01 07:53:22 +0000

    Make DetachAbstractInlineTextBoxesIfNeeded public
    In preparation to associate |NGFragmentItem| with
    |LayoutObject| in <crrev.com/c/2117558>, it needs to call
    this function from outside of |LayoutText|.
    
    Bug: 982194
    Change-Id: I14a1892d9494ae43748ab4c0145e68e2a21565e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131175
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755277}

--- third_party/blink/renderer/core/layout/layout_text.h
+++ third_party/blink/renderer/core/layout/layout_text.h
@@ -334,6 +334,8 @@ class CORE_EXPORT LayoutText : public LayoutObject {
 
   void InvalidateSubtreeLayoutForFontUpdates() override;
 
+  void DetachAbstractInlineTextBoxesIfNeeded();
+
  protected:
   void WillBeDestroyed() override;
 
@@ -442,7 +444,7 @@ class CORE_EXPORT LayoutText : public LayoutObject {
 
  private:
   ContentCaptureManager* GetContentCaptureManager();
-  void DetachAbstractInlineTextBoxesIfNeeded();
+  void DetachAbstractInlineTextBoxes();
 
   // Used for LayoutNG with accessibility. True if inline fragments are
   // associated to |NGAbstractInlineTextBox|.
@@ -519,6 +521,11 @@ inline float LayoutText::HyphenWidth(const Font& font,
   const ComputedStyle& style = StyleRef();
   return font.Width(ConstructTextRun(font, style.HyphenString().GetString(),
                                      style, direction));
+}
+
+inline void LayoutText::DetachAbstractInlineTextBoxesIfNeeded() {
+  if (UNLIKELY(has_abstract_inline_text_box_))
+    DetachAbstractInlineTextBoxes();
 }
 
 DEFINE_LAYOUT_OBJECT_TYPE_CASTS(LayoutText, IsText());
