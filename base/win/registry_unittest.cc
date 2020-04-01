9c09c124a1593860ec7b7069b6d9fed32e24472a
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-04-01 19:30:52 +0000

    [base] Apply BindOnceRewriter in //base
    This change applies the BindOnceRewriter in //base.
    This rewriter replaces calls to base::Bind and base::BindRepeating with
    calls to base::BindOnce, in case the result is immediately assigned to a
    base::OnceCallback. Given that base::RepeatingCallback is implicitly
    convertible to base::OnceCallback, there is no change in functionality.
    
    Steps:
      1. run_tool.py --tool base_bind_rewriters \
                     --tool-arg='--rewriter=bind_to_bind_once'
      2. git cl format
    
    This CL was uploaded by git cl split.
    
    R=brucedawson@chromium.org
    
    Bug: 714018
    Change-Id: I9b35e5f70260a892ed38255edd632699c293235b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132388
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755487}

--- base/win/registry_unittest.cc
+++ base/win/registry_unittest.cc
@@ -372,7 +372,7 @@ TEST_F(RegistryTest, ChangeCallback) {
             key.Create(HKEY_CURRENT_USER, foo_key.c_str(), KEY_READ));
 
   ASSERT_TRUE(key.StartWatching(
-      BindRepeating(&TestChangeDelegate::OnKeyChanged, Unretained(&delegate))));
+      BindOnce(&TestChangeDelegate::OnKeyChanged, Unretained(&delegate))));
   EXPECT_FALSE(delegate.WasCalled());
 
   // Make some change.
@@ -390,7 +390,7 @@ TEST_F(RegistryTest, ChangeCallback) {
   EXPECT_FALSE(delegate.WasCalled());
 
   ASSERT_TRUE(key.StartWatching(
-      BindRepeating(&TestChangeDelegate::OnKeyChanged, Unretained(&delegate))));
+      BindOnce(&TestChangeDelegate::OnKeyChanged, Unretained(&delegate))));
 
   // Change something else.
   EXPECT_EQ(ERROR_SUCCESS, key2.WriteValue(L"name2", L"data2"));
@@ -398,7 +398,7 @@ TEST_F(RegistryTest, ChangeCallback) {
   ASSERT_TRUE(delegate.WasCalled());
 
   ASSERT_TRUE(key.StartWatching(
-      BindRepeating(&TestChangeDelegate::OnKeyChanged, Unretained(&delegate))));
+      BindOnce(&TestChangeDelegate::OnKeyChanged, Unretained(&delegate))));
   RunLoop().RunUntilIdle();
   EXPECT_FALSE(delegate.WasCalled());
 }
