fe51d486fe1c061f9e80ae5db2124c521f0d840e
Hailey Wang <haileywang@google.com>
2020-04-01 17:07:21 +0000

    [PM] Add calculation of maximum simultaneous load
    Add calculation logic for maximum simultaneous load inside
    BackgroundTabLoadingPolicy.
    
    This will be used for restricting the number of background tabs
    loading in a future CL.
    
    Bug: 1059341
    Change-Id: I7ecd751fafb202ffda09472a2121f72a02a22cb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2120267
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755396}

---
+++ chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_helpers.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PERFORMANCE_MANAGER_GRAPH_POLICIES_BACKGROUND_TAB_LOADING_POLICY_HELPERS_H_
+#define CHROME_BROWSER_PERFORMANCE_MANAGER_GRAPH_POLICIES_BACKGROUND_TAB_LOADING_POLICY_HELPERS_H_
+
+#include <stddef.h>
+
+namespace performance_manager {
+
+namespace policies {
+
+// Helper function for BackgroundTabLoadingPolicy to compute the number of
+// tabs that can load simultaneously.
+size_t CalculateMaxSimultaneousTabLoads(size_t lower_bound,
+                                        size_t upper_bound,
+                                        size_t cores_per_load,
+                                        size_t num_cores);
+
+}  // namespace policies
+
+}  // namespace performance_manager
+
+#endif  // CHROME_BROWSER_PERFORMANCE_MANAGER_GRAPH_POLICIES_BACKGROUND_TAB_LOADING_POLICY_HELPERS_H_
