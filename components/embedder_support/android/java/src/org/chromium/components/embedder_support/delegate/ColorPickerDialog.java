ddcb6f7bba6c9d182a9bbc35ac929e6af679d4bc
Lijin Shen <lazzzis@google.com>
2020-04-01 03:17:06 +0000

    Update button color of Color picker dialog
    Replace "more" button on color picker dialog with ButtonCompat. The
    bg color of the original one does not turn dark on night mode.
    
    Bug: 1045190
    Change-Id: Ifdf2bf60314cbf93b5136ca3007a0cf0204f235c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119370
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#755234}

--- components/embedder_support/android/java/src/org/chromium/components/embedder_support/delegate/ColorPickerDialog.java
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/delegate/ColorPickerDialog.java
@@ -14,6 +14,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.chromium.base.StrictModeContext;
 
 /**
@@ -26,6 +28,8 @@ public class ColorPickerDialog extends AlertDialog implements OnColorChangedList
     private final ColorPickerSimple mSimpleColorPicker;
 
     private final Button mMoreButton;
+
+    private final View mContent;
 
     // The view up in the corner that shows the user the color they've currently selected.
     private final View mCurrentColorView;
@@ -100,11 +104,11 @@ public class ColorPickerDialog extends AlertDialog implements OnColorChangedList
         });
 
         // Initialize main content view
-        View content = inflateView(context, R.layout.color_picker_dialog_content);
-        setView(content);
+        mContent = inflateView(context, R.layout.color_picker_dialog_content);
+        setView(mContent);
 
         // Initialize More button.
-        mMoreButton = (Button) content.findViewById(R.id.more_colors_button);
+        mMoreButton = (Button) mContent.findViewById(R.id.more_colors_button);
         mMoreButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -114,11 +118,11 @@ public class ColorPickerDialog extends AlertDialog implements OnColorChangedList
 
         // Initialize advanced color view (hidden initially).
         mAdvancedColorPicker =
-                (ColorPickerAdvanced) content.findViewById(R.id.color_picker_advanced);
+                (ColorPickerAdvanced) mContent.findViewById(R.id.color_picker_advanced);
         mAdvancedColorPicker.setVisibility(View.GONE);
 
         // Initialize simple color view (default view).
-        mSimpleColorPicker = (ColorPickerSimple) content.findViewById(R.id.color_picker_simple);
+        mSimpleColorPicker = (ColorPickerSimple) mContent.findViewById(R.id.color_picker_simple);
         mSimpleColorPicker.init(suggestions, this);
 
         updateCurrentColor(mInitialColor);
@@ -142,8 +146,7 @@ public class ColorPickerDialog extends AlertDialog implements OnColorChangedList
     private void showAdvancedView() {
         // Only need to hide the borders, not the Views themselves, since the Views are
         // contained within the borders.
-        View buttonBorder = findViewById(R.id.more_colors_button_border);
-        buttonBorder.setVisibility(View.GONE);
+        mMoreButton.setVisibility(View.GONE);
 
         View simpleView = findViewById(R.id.color_picker_simple);
         simpleView.setVisibility(View.GONE);
@@ -167,5 +170,10 @@ public class ColorPickerDialog extends AlertDialog implements OnColorChangedList
     private void updateCurrentColor(int color) {
         mCurrentColor = color;
         if (mCurrentColorView != null) mCurrentColorView.setBackgroundColor(color);
+    }
+
+    @VisibleForTesting
+    public View getContentView() {
+        return mContent;
     }
 }
