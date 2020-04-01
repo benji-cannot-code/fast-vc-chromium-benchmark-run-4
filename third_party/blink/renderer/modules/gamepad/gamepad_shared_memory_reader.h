4afb7ae3ae24add4c2b8a24bcc4e1ec114dc4980
Johann <johannkoenig@google.com>
2020-04-01 22:08:59 +0000

    heap mojo: add mode to gamepad shared memory reader
    The new wrapper changed the behavior slightly during the migration. The
    wrapper defines a default ContextDestroyed method. If some other cleanup
    method tries to access receiver_ it may crash. This change reverts to
    the pre-migration behavior.
    
    BUG=chromium:1049056
    
    Change-Id: I9feafcf13c732664e8b10b6cba1a5258e3012e22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131803
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#755569}

--- third_party/blink/renderer/modules/gamepad/gamepad_shared_memory_reader.h
+++ third_party/blink/renderer/modules/gamepad/gamepad_shared_memory_reader.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/system/buffer.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 
 namespace base {
 class ReadOnlySharedMemoryRegion;
@@ -65,7 +66,9 @@ class GamepadSharedMemoryReader
 
   bool ever_interacted_with_ = false;
 
-  HeapMojoReceiver<device::mojom::blink::GamepadObserver> receiver_;
+  HeapMojoReceiver<device::mojom::blink::GamepadObserver,
+                   HeapMojoWrapperMode::kWithoutContextObserver>
+      receiver_;
   mojo::Remote<device::mojom::blink::GamepadMonitor> gamepad_monitor_remote_;
   blink::GamepadListener* listener_ = nullptr;
 };
