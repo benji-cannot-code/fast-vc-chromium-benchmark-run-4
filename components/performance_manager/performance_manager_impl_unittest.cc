a64658184b0759d165ca022b79833a4912dceeb2
Hailey Wang <haileywang@google.com>
2020-04-01 22:15:36 +0000

    [PM] PageNodeImpl's TimeSinceLastVisibilityChange initial value
    Change the initial value to the LastActiveTime of the web content.
    
    Used inside BackgroundTabLoadingPolicy in order to determine if a
    PageNode should be loaded.
    
    Bug: 1059341
    Change-Id: Ieea245eaaebc3cb71ccaad6bb31695df160e6813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132526
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Sébastien Marchand <sebmarchand@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755575}

--- components/performance_manager/performance_manager_impl_unittest.cc
+++ components/performance_manager/performance_manager_impl_unittest.cc
@@ -56,7 +56,8 @@ TEST_F(PerformanceManagerImplTest, InstantiateNodes) {
   EXPECT_NE(nullptr, process_node.get());
   std::unique_ptr<PageNodeImpl> page_node =
       PerformanceManagerImpl::CreatePageNode(WebContentsProxy(), std::string(),
-                                             GURL(), false, false);
+                                             GURL(), false, false,
+                                             base::TimeTicks::Now());
   EXPECT_NE(nullptr, page_node.get());
 
   // Create a node of each type.
@@ -78,7 +79,8 @@ TEST_F(PerformanceManagerImplTest, BatchDeleteNodes) {
       PerformanceManagerImpl::CreateProcessNode(RenderProcessHostProxy());
   std::unique_ptr<PageNodeImpl> page_node =
       PerformanceManagerImpl::CreatePageNode(WebContentsProxy(), std::string(),
-                                             GURL(), false, false);
+                                             GURL(), false, false,
+                                             base::TimeTicks::Now());
 
   std::unique_ptr<FrameNodeImpl> parent1_frame =
       PerformanceManagerImpl::CreateFrameNode(
@@ -122,7 +124,8 @@ TEST_F(PerformanceManagerImplTest, CallOnGraphImpl) {
   // Create a page node for something to target.
   std::unique_ptr<PageNodeImpl> page_node =
       PerformanceManagerImpl::CreatePageNode(WebContentsProxy(), std::string(),
-                                             GURL(), false, false);
+                                             GURL(), false, false,
+                                             base::TimeTicks::Now());
   base::RunLoop run_loop;
   base::OnceClosure quit_closure = run_loop.QuitClosure();
   EXPECT_FALSE(PerformanceManagerImpl::OnPMTaskRunnerForTesting());
@@ -143,7 +146,8 @@ TEST_F(PerformanceManagerImplTest, CallOnGraphAndReplyWithResult) {
   // Create a page node for something to target.
   std::unique_ptr<PageNodeImpl> page_node =
       PerformanceManagerImpl::CreatePageNode(WebContentsProxy(), std::string(),
-                                             GURL(), false, false);
+                                             GURL(), false, false,
+                                             base::TimeTicks::Now());
   base::RunLoop run_loop;
 
   EXPECT_FALSE(PerformanceManagerImpl::OnPMTaskRunnerForTesting());
