b28b29490b0f9a5ea0da7bd8f96e4f86f37a1c4d
edchin <edchin@chromium.org>
2020-04-01 00:53:38 +0000

    Revert "Reland "[ios] Remove dead code in first_run_util""
    This reverts commit 194955c64ba696ea9a5eb78a9ba4e9bf2a647157.
    
    Reason for revert: 
    
    Mistakenly relanded last CL without change.
    
    Original change's description:
    > Reland "[ios] Remove dead code in first_run_util"
    > 
    > This reverts commit 4edeedc6ed7b608000dfbb42fff07e660291e802.
    > 
    > Reason for revert:
    > 
    > Will fix the error in this reland. 
    > welcome_to_chrome_view_controller.mm should not have been edited. 
    > 
    > Original change's description:
    > > Revert "[ios] Remove dead code in first_run_util"
    > > 
    > > This reverts commit 721f0aa1349d31b49a16df96814b1da47f8391e0.
    > > 
    > > Reason for revert:
    > > suspect this is breaking compilation on ios-simulator
    > > 
    > > Original change's description:
    > > > [ios] Remove dead code in first_run_util
    > > > 
    > > > Removed dead codepaths and marked unused histograms obsolete.
    > > > 
    > > > Change-Id: Ie4f5485ebd9c915dcb36c8ac7d14d2d688b200e7
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129507
    > > > Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    > > > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > > > Commit-Queue: edchin <edchin@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#755163}
    > > 
    > > TBR=isherman@chromium.org,kkhorimoto@chromium.org,mrefaat@chromium.org,edchin@chromium.org
    > > 
    > > Change-Id: I1d3d94ce35b28df80626a1838b3c110957b8757d
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131354
    > > Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    > > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#755166}
    > 
    > TBR=isherman@chromium.org,kkhorimoto@chromium.org,mrefaat@chromium.org,edchin@chromium.org,rbpotter@chromium.org
    > 
    > Change-Id: Ia325e65092b1d7b4cc66e6f2be7d52e93e50f39f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131357
    > Reviewed-by: edchin <edchin@chromium.org>
    > Commit-Queue: edchin <edchin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#755191}
    
    TBR=isherman@chromium.org,kkhorimoto@chromium.org,mrefaat@chromium.org,edchin@chromium.org,rbpotter@chromium.org
    
    Change-Id: Ide9f88a5222c1a44cff70af1c4518b4904ecc703
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131149
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755193}

--- ios/chrome/browser/ui/first_run/first_run_util.h
+++ ios/chrome/browser/ui/first_run/first_run_util.h
@@ -12,6 +12,10 @@ class ChromeBrowserState;
 @class FirstRunConfiguration;
 @protocol SyncPresenter;
 
+namespace base {
+class TimeTicks;
+}
+
 namespace web {
 class WebState;
 }
@@ -39,7 +43,17 @@ void FinishFirstRun(ChromeBrowserState* browserState,
                     FirstRunConfiguration* config,
                     id<SyncPresenter> presenter);
 
+// Records Product tour timing metrics using histogram.
+void RecordProductTourTimingMetrics(NSString* timer_name,
+                                    base::TimeTicks start_time);
+
 // Posts a notification that First Run did finish.
 void FirstRunDismissed();
+
+// Enables or disables the data reduction proxy and also sets a key indicating
+// application is using Data Reduction Proxy.
+void SetDataReductionProxyEnabled(ChromeBrowserState* browserState,
+                                  BOOL enabled,
+                                  BOOL toggled_switch);
 
 #endif  // IOS_CHROME_BROWSER_UI_FIRST_RUN_FIRST_RUN_UTIL_H_
