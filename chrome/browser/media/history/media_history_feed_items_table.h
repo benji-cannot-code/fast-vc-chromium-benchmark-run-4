c2ba4a91774998b13b0893ebe67a57779159f11b
Becca Hughes <beccahughes@chromium.org>
2020-04-01 18:20:07 +0000

    [Media Feeds] Add safe search status field
    Add a field that stores whether the item has been
    checked by Safe Search or not and the result. Adds
    that to the Media Feeds WebUI.
    
    BUG=1066643
    
    Change-Id: I65587885d177b45f95237a22bd08a3e1488f407b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130784
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755448}

--- chrome/browser/media/history/media_history_feed_items_table.h
+++ chrome/browser/media/history/media_history_feed_items_table.h
@@ -40,7 +40,8 @@ class MediaHistoryFeedItemsTable : public MediaHistoryTableBase {
     kBadTVEpisode = 8,
     kBadPlayNextCandidate = 9,
     kBadImages = 10,
-    kMaxValue = kBadImages,
+    kBadSafeSearchResult = 11,
+    kMaxValue = kBadSafeSearchResult,
   };
 
   MediaHistoryFeedItemsTable(const MediaHistoryFeedItemsTable&) = delete;
