b12f8c9ff4b8d4dca239314200fc30c1a88b184a
edchin <edchin@chromium.org>
2020-04-01 00:52:04 +0000

    Reland "[ios] Remove dead code in first_run_util"
    This reverts commit 4edeedc6ed7b608000dfbb42fff07e660291e802.
    
    Reason for revert:
    
    Will fix the error in this reland. 
    welcome_to_chrome_view_controller.mm should not have been edited. 
    
    Original change's description:
    > Revert "[ios] Remove dead code in first_run_util"
    > 
    > This reverts commit 721f0aa1349d31b49a16df96814b1da47f8391e0.
    > 
    > Reason for revert:
    > suspect this is breaking compilation on ios-simulator
    > 
    > Original change's description:
    > > [ios] Remove dead code in first_run_util
    > > 
    > > Removed dead codepaths and marked unused histograms obsolete.
    > > 
    > > Change-Id: Ie4f5485ebd9c915dcb36c8ac7d14d2d688b200e7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129507
    > > Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    > > Reviewed-by: Ilya Sherman <isherman@chromium.org>
    > > Commit-Queue: edchin <edchin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#755163}
    > 
    > TBR=isherman@chromium.org,kkhorimoto@chromium.org,mrefaat@chromium.org,edchin@chromium.org
    > 
    > Change-Id: I1d3d94ce35b28df80626a1838b3c110957b8757d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131354
    > Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#755166}
    
    TBR=isherman@chromium.org,kkhorimoto@chromium.org,mrefaat@chromium.org,edchin@chromium.org,rbpotter@chromium.org
    
    Change-Id: Ia325e65092b1d7b4cc66e6f2be7d52e93e50f39f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131357
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755191}

--- ios/chrome/browser/ui/first_run/first_run_util.mm
+++ ios/chrome/browser/ui/first_run/first_run_util.mm
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/first_run/first_run_configuration.h"
 #include "ios/chrome/browser/first_run/first_run_metrics.h"
 #include "ios/chrome/browser/signin/identity_manager_factory.h"
-#include "ios/chrome/browser/ui/first_run/first_run_histograms.h"
 #import "ios/chrome/browser/ui/settings/sync/utils/sync_util.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #include "ios/web/public/thread/web_thread.h"
@@ -100,17 +99,6 @@ void FinishFirstRun(ChromeBrowserState* browserState,
 
   // Display the sync errors infobar.
   DisplaySyncErrors(browserState, web_state, presenter);
-}
-
-void RecordProductTourTimingMetrics(NSString* timer_name,
-                                    base::TimeTicks start_time) {
-  base::TimeDelta delta = base::TimeTicks::Now() - start_time;
-  NSString* histogramName =
-      [NSString stringWithFormat:@"ProductTour.IOSScreens%@", timer_name];
-  UMA_HISTOGRAM_CUSTOM_TIMES_FIRST_RUN(base::SysNSStringToUTF8(histogramName),
-                                       delta,
-                                       base::TimeDelta::FromMilliseconds(10),
-                                       base::TimeDelta::FromMinutes(3), 50);
 }
 
 void FirstRunDismissed() {
