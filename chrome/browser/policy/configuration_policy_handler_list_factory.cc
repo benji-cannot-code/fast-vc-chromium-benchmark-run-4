72d44bed5ccccf245c0bb02d2955f725840f3980
Varun Khaneja <vakh@chromium.org>
2020-04-01 20:50:30 +0000

    Introduce a new policy to set the Safe Browsing protection level.
    This CL does the following:
    a) Add a new enum policy to set the Safe Browsing protection level.
       This policy is called: SafeBrowsingProtectionLevel
    b) Drops a policy named SafeBrowsingEnhancedProtection
       This policy never shipped.
    c) Deprecates an existing boolean policy named: SafeBrowsingEnabled.
    
    The new policy in a) has three values:
    0 - Safe Browsing gets disabled. This is equivalent to setting
        SafeBrowsingEnabled to false.
    1 - Safe Browsing gets enabled in standard mode. This is equivalent to
        setting SafeBrowsingEnabled to true.
    2 - Safe Browsing Enhanced Protection gets enabled.
    
    This CL also adds a handler class called SafeBrowsingPolicyHandler the
    purpose of which is to map the policy being deprecated
    (i.e. SafeBrowsingEnabled) and the policy being introduced
    (i.e. SafeBrowsingProtectionLevel) to the appropriate prefs.
    
    For details, please see: go/crsb-safer-chrome-policy
    
    Bug: 1066371, 1056483
    Change-Id: I37115f04e07ba190504b9cac85c5e0694f4aec12
    Fixed: 1066371
    TBR: blundell (for '+components/strings/grit/components_strings.h' DEPS)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129284
    Commit-Queue: Varun Khaneja <vakh@chromium.org>
    Auto-Submit: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755526}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/policy_pref_names.h"
 #include "components/policy/core/common/schema.h"
 #include "components/policy/policy_constants.h"
+#include "components/safe_browsing/core/common/safe_browsing_policy_handler.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/search_engines/default_search_policy_handler.h"
 #include "components/security_state/core/security_state_pref_names.h"
@@ -176,9 +177,6 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     base::Value::Type::BOOLEAN },
   { key::kSafeBrowsingEnabled,
     prefs::kSafeBrowsingEnabled,
-    base::Value::Type::BOOLEAN },
-  { key::kSafeBrowsingEnhancedProtection,
-    prefs::kSafeBrowsingEnhanced,
     base::Value::Type::BOOLEAN },
   { key::kSafeBrowsingForTrustedSourcesEnabled,
     prefs::kSafeBrowsingForTrustedSourcesEnabled,
@@ -1361,6 +1359,8 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
   handlers->AddHandler(std::make_unique<JavascriptPolicyHandler>());
   handlers->AddHandler(std::make_unique<NetworkPredictionPolicyHandler>());
   handlers->AddHandler(std::make_unique<RestoreOnStartupPolicyHandler>());
+  handlers->AddHandler(
+      std::make_unique<safe_browsing::SafeBrowsingPolicyHandler>());
   handlers->AddHandler(std::make_unique<syncer::SyncPolicyHandler>());
   handlers->AddHandler(std::make_unique<StringMappingListPolicyHandler>(
       key::kEnableDeprecatedWebPlatformFeatures,
