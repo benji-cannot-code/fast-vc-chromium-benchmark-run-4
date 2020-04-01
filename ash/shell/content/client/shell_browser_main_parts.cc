7f493d61ac9be4e5a1a85ee14db76f303a021569
Peter Kasting <pkasting@chromium.org>
2020-04-01 22:32:14 +0000

    Refactor various common bits up into AshTestHelper.
    Not only is this shorter overall, it's less error-prone, as these were
    mostly aimed at preventing various kinds of test failures.
    
    Bug: none
    Change-Id: Ifaea70d3c03ea809a55eb2be7adcbfb01e572fb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132973
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755586}

--- ash/shell/content/client/shell_browser_main_parts.cc
+++ ash/shell/content/client/shell_browser_main_parts.cc
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/shell/browser/shell_browser_context.h"
 #include "net/base/net_module.h"
-#include "ui/aura/window.h"
-#include "ui/aura/window_tree_host.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/chromeos/events/event_rewriter_chromeos.h"
 #include "ui/views/examples/examples_window_with_content.h"
@@ -97,8 +95,6 @@ void ShellBrowserMainParts::PreMainMessageLoopRun() {
   ash_test_helper_->SetUp(std::move(init_params));
 
   window_watcher_ = std::make_unique<WindowWatcher>();
-
-  Shell::GetPrimaryRootWindow()->GetHost()->Show();
 
   Shell::Get()->InitWaylandServer(nullptr);
 
