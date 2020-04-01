87310c684070dafed0490c6d990b8d795e89253a
Erik Jensen <rkjnsn@google.com>
2020-04-01 23:23:42 +0000

    remoting: Inject key events on UI thread on Mac
    It seems the implementation of CGEventCreateKeyboardEvent has changed,
    and will trigger an assertion failure and crash if called outside of the
    UI thread.
    
    Bug: 1066698
    Change-Id: I2fb66d666c34140fb119a208895a33de6309718e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132939
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755609}

--- remoting/host/input_injector_mac.cc
+++ remoting/host/input_injector_mac.cc
@@ -38,6 +38,7 @@ void SetOrClearBit(uint64_t &value, uint64_t bit, bool set_bit) {
   value = set_bit ? (value | bit) : (value & ~bit);
 }
 
+// Must be called on UI thread.
 void CreateAndPostKeyEvent(int keycode,
                            bool pressed,
                            uint64_t flags,
@@ -71,7 +72,8 @@ using protocol::TouchEvent;
 class InputInjectorMac : public InputInjector {
  public:
   explicit InputInjectorMac(
-      scoped_refptr<base::SingleThreadTaskRunner> task_runner);
+      scoped_refptr<base::SingleThreadTaskRunner> input_thread_task_runner,
+      scoped_refptr<base::SingleThreadTaskRunner> ui_thread_task_runner);
   ~InputInjectorMac() override;
 
   // ClipboardStub interface.
@@ -91,7 +93,9 @@ class InputInjectorMac : public InputInjector {
   // The actual implementation resides in InputInjectorMac::Core class.
   class Core : public base::RefCountedThreadSafe<Core> {
    public:
-    explicit Core(scoped_refptr<base::SingleThreadTaskRunner> task_runner);
+    explicit Core(
+        scoped_refptr<base::SingleThreadTaskRunner> input_thread_task_runner,
+        scoped_refptr<base::SingleThreadTaskRunner> ui_thread_task_runner);
 
     // Mirrors the ClipboardStub interface.
     void InjectClipboardEvent(const ClipboardEvent& event);
@@ -112,7 +116,8 @@ class InputInjectorMac : public InputInjector {
 
     void WakeUpDisplay();
 
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+    scoped_refptr<base::SingleThreadTaskRunner> input_thread_task_runner_;
+    scoped_refptr<base::SingleThreadTaskRunner> ui_thread_task_runner_;
     webrtc::DesktopVector mouse_pos_;
     uint32_t mouse_button_state_;
     std::unique_ptr<Clipboard> clipboard_;
@@ -129,8 +134,9 @@ class InputInjectorMac : public InputInjector {
 };
 
 InputInjectorMac::InputInjectorMac(
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
-  core_ = new Core(task_runner);
+    scoped_refptr<base::SingleThreadTaskRunner> input_thread_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> ui_thread_task_runner) {
+  core_ = new Core(input_thread_task_runner, ui_thread_task_runner);
 }
 
 InputInjectorMac::~InputInjectorMac() {
@@ -163,8 +169,10 @@ void InputInjectorMac::Start(
 }
 
 InputInjectorMac::Core::Core(
-    scoped_refptr<base::SingleThreadTaskRunner> task_runner)
-    : task_runner_(task_runner),
+    scoped_refptr<base::SingleThreadTaskRunner> input_thread_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> ui_thread_task_runner)
+    : input_thread_task_runner_(input_thread_task_runner),
+      ui_thread_task_runner_(ui_thread_task_runner),
       mouse_button_state_(0),
       clipboard_(Clipboard::Create()),
       left_modifiers_(0),
@@ -184,8 +192,8 @@ InputInjectorMac::Core::Core(
 }
 
 void InputInjectorMac::Core::InjectClipboardEvent(const ClipboardEvent& event) {
-  if (!task_runner_->BelongsToCurrentThread()) {
-    task_runner_->PostTask(
+  if (!input_thread_task_runner_->BelongsToCurrentThread()) {
+    input_thread_task_runner_->PostTask(
         FROM_HERE, base::BindOnce(&Core::InjectClipboardEvent, this, event));
     return;
   }
@@ -240,7 +248,9 @@ void InputInjectorMac::Core::InjectKeyEvent(const KeyEvent& event) {
     flags |= kCGEventFlagMaskAlphaShift;
   }
 
-  CreateAndPostKeyEvent(keycode, event.pressed(), flags, base::string16());
+  ui_thread_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(CreateAndPostKeyEvent, keycode, event.pressed(),
+                                flags, base::string16()));
 }
 
 void InputInjectorMac::Core::InjectTextEvent(const TextEvent& event) {
@@ -252,8 +262,12 @@ void InputInjectorMac::Core::InjectTextEvent(const TextEvent& event) {
 
   // Applications that ignore UnicodeString field will see the text event as
   // Space key.
-  CreateAndPostKeyEvent(kVK_Space, /*pressed=*/true, 0, text);
-  CreateAndPostKeyEvent(kVK_Space, /*pressed=*/false, 0, text);
+  ui_thread_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(CreateAndPostKeyEvent, kVK_Space,
+                                /*pressed=*/true, 0, text));
+  ui_thread_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(CreateAndPostKeyEvent, kVK_Space,
+                                /*pressed=*/false, 0, text));
 }
 
 void InputInjectorMac::Core::InjectMouseEvent(const MouseEvent& event) {
@@ -312,8 +326,8 @@ void InputInjectorMac::Core::InjectMouseEvent(const MouseEvent& event) {
 
 void InputInjectorMac::Core::Start(
     std::unique_ptr<protocol::ClipboardStub> client_clipboard) {
-  if (!task_runner_->BelongsToCurrentThread()) {
-    task_runner_->PostTask(
+  if (!input_thread_task_runner_->BelongsToCurrentThread()) {
+    input_thread_task_runner_->PostTask(
         FROM_HERE,
         base::BindOnce(&Core::Start, this, std::move(client_clipboard)));
     return;
@@ -323,8 +337,9 @@ void InputInjectorMac::Core::Start(
 }
 
 void InputInjectorMac::Core::Stop() {
-  if (!task_runner_->BelongsToCurrentThread()) {
-    task_runner_->PostTask(FROM_HERE, base::BindOnce(&Core::Stop, this));
+  if (!input_thread_task_runner_->BelongsToCurrentThread()) {
+    input_thread_task_runner_->PostTask(FROM_HERE,
+                                        base::BindOnce(&Core::Stop, this));
     return;
   }
 
@@ -365,9 +380,10 @@ InputInjectorMac::Core::~Core() {}
 
 // static
 std::unique_ptr<InputInjector> InputInjector::Create(
-    scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
-    scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner) {
-  return base::WrapUnique(new InputInjectorMac(main_task_runner));
+    scoped_refptr<base::SingleThreadTaskRunner> input_thread_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> ui_thread_task_runner) {
+  return base::WrapUnique(
+      new InputInjectorMac(input_thread_task_runner, ui_thread_task_runner));
 }
 
 // static
