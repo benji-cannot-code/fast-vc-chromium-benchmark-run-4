ec876895112c91f295b1847bc71ba81a87486593
Hiroshige Hayashizaki <hiroshige@chromium.org>
2020-04-01 05:27:45 +0000

    Revert "Add a metric to track what resource type triggers SB blocking page."
    This reverts commit b80aea173728e202552061dedee86d95d7436b04.
    
    Reason for revert: Causing use-of-uninitialized-value.
    https://bugs.chromium.org/p/chromium/issues/detail?id=1066737
    
    Original change's description:
    > Add a metric to track what resource type triggers SB blocking page.
    > 
    > We'd like to know for each safe browsing blocking page created, what
    > is the resource type that triggers the page. This information
    > can help us decide the priority of resource types that should be
    > checked in real time.
    > 
    > Metrics added in this CL:
    > SafeBrowsing.BlockingPage.ResourceType
    > 
    > Bug: 1065137
    > Change-Id: I5c5c07c138f2d4f149229af54b03b68e623d44d1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2121735
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#755177}
    
    TBR=holte@chromium.org,drubery@chromium.org,xinghuilu@chromium.org
    
    Change-Id: I29b299e6110511a5435eba174b6ed538dfddc99b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1065137, 1066737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131437
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755251}

--- chrome/browser/safe_browsing/safe_browsing_blocking_page.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/lazy_instance.h"
-#include "base/metrics/histogram_macros.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/interstitials/enterprise_util.h"
@@ -248,8 +247,6 @@ SafeBrowsingBlockingPage* SafeBrowsingBlockingPage::CreateBlockingPage(
     const GURL& main_frame_url,
     const UnsafeResource& unsafe_resource,
     bool should_trigger_reporting) {
-  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.BlockingPage.ResourceType",
-                            unsafe_resource.resource_type);
   const UnsafeResourceList resources{unsafe_resource};
   // Set up the factory if this has not been done already (tests do that
   // before this method is called).
--- components/security_interstitials/core/unsafe_resource.h
+++ components/security_interstitials/core/unsafe_resource.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
-#include "components/safe_browsing/core/common/safebrowsing_constants.h"
 #include "components/safe_browsing/core/db/hit_report.h"
 #include "url/gurl.h"
 
@@ -54,7 +53,6 @@ struct UnsafeResource {
   bool is_subframe;
   safe_browsing::SBThreatType threat_type;
   safe_browsing::ThreatMetadata threat_metadata;
-  safe_browsing::ResourceType resource_type;
   UrlCheckCallback callback;  // This is called back on |callback_thread|.
   scoped_refptr<base::SingleThreadTaskRunner> callback_thread;
   base::RepeatingCallback<content::WebContents*(void)> web_contents_getter;
