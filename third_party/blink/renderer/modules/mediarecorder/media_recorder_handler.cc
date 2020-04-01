3df63713655c85c1b709800abaea229a41efd651
Armando Miraglia <armax@chromium.org>
2020-04-01 09:01:23 +0000

    Reland "[MediaRecorder] Fix corrupted video problem with pause/resume spamming."
    This reverts commit 5f6e2aace852ae047e6d516d3efd61bfddf8be50.
    
    Reason for revert: roll-forward with invalidatino fix and test.
    
    Original change's description:
    > Revert "[MediaRecorder] Fix corrupted video problem with pause/resume spamming."
    >
    > This reverts commit b7dab9dc0c395b471b2c735b0c4de9360f1d7a3c.
    >
    > Reason for revert: caused crbug.com/1065827 which is more severe.
    >
    > Original change's description:
    > > [MediaRecorder] Fix corrupted video problem with pause/resume spamming.
    > >
    > >
    > > This CL fixes an issue caused by the following sequence of events:
    > > 1. MediaRecorderHandler::Pause(): recording_ = false
    > > 2. MediaRecorderHandler::WriteData(): if (!recording_) return
    > >
    > > The problem is that the WriteData() invocation was decided to
    > > happen while the recorder was still resumed. In that state, wholes
    > > would be produced which are not tolerated by vpx_encoder, which
    > > interprets those as corrupted.
    > >
    > > The fix reverts the semantic of the code to when the class was not under
    > > oilpan and would require manual memory management and restores the
    > > "invalidation" semantic from when the class was using a weak ptr.
    > > In fact, the weak reference would be invalidated hence making the
    > > callbacks no-ops. To keep the same semantic, this CL introduces a
    > > boolean and avoids overloading the meaning of 'recording_'
    > >
    > > This should also not require calling Pause() in Stop(), as all callbacks
    > > will not do anything after Stop() is called.
    > >
    > > Fixed: 1060358
    > > Change-Id: I88b0aeb10fd13034412f3689648cebd1df40d6bf
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106072
    > > Commit-Queue: Armando Miraglia <armax@chromium.org>
    > > Reviewed-by: Markus Handell <handellm@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#752838}
    >
    > TBR=armax@chromium.org,handellm@google.com
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Change-Id: Id8a148253433576469cd7958addf48afdabf44eb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2128070
    > Reviewed-by: Armando Miraglia <armax@chromium.org>
    > Commit-Queue: Armando Miraglia <armax@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#754872}
    
    TBR=armax@chromium.org,handellm@google.com
    
    Change-Id: Ie1becac31f0f1a2e5aea92bc59e7e356a2c6e62b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129534
    Reviewed-by: Armando Miraglia <armax@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Armando Miraglia <armax@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755294}

--- third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.cc
+++ third_party/blink/renderer/modules/mediarecorder/media_recorder_handler.cc
@@ -240,6 +240,8 @@ bool MediaRecorderHandler::Start(int timeslice) {
   DCHECK(timeslice_.is_zero());
   DCHECK(!webm_muxer_);
 
+  invalidated_ = false;
+
   timeslice_ = base::TimeDelta::FromMilliseconds(timeslice);
   slice_origin_timestamp_ = base::TimeTicks::Now();
 
@@ -336,8 +338,7 @@ void MediaRecorderHandler::Stop() {
   DCHECK(IsMainThread());
   // Don't check |recording_| since we can go directly from pause() to stop().
 
-  if (recording_)
-    Pause();
+  invalidated_ = true;
 
   recording_ = false;
   timeslice_ = base::TimeDelta::FromMilliseconds(0);
@@ -497,6 +498,10 @@ void MediaRecorderHandler::OnEncodedVideo(
     base::TimeTicks timestamp,
     bool is_key_frame) {
   DCHECK(IsMainThread());
+
+  if (invalidated_)
+    return;
+
   auto params_with_codec = params;
   params_with_codec.codec = MediaVideoCodecFromCodecId(video_codec_id_);
   HandleEncodedVideo(params_with_codec, std::move(encoded_data),
@@ -524,9 +529,6 @@ void MediaRecorderHandler::HandleEncodedVideo(
     base::TimeTicks timestamp,
     bool is_key_frame) {
   DCHECK(IsMainThread());
-
-  if (video_recorders_.IsEmpty())
-    return;
 
   if (UpdateTracksAndCheckIfChanged()) {
     recorder_->OnError("Amount of tracks in MediaStream has changed.");
@@ -558,7 +560,7 @@ void MediaRecorderHandler::OnEncodedAudio(const media::AudioParameters& params,
                                           base::TimeTicks timestamp) {
   DCHECK(IsMainThread());
 
-  if (audio_recorders_.IsEmpty())
+  if (invalidated_)
     return;
 
   if (UpdateTracksAndCheckIfChanged()) {
@@ -577,7 +579,7 @@ void MediaRecorderHandler::OnEncodedAudio(const media::AudioParameters& params,
 void MediaRecorderHandler::WriteData(base::StringPiece data) {
   DCHECK(IsMainThread());
 
-  if (!recording_)
+  if (invalidated_)
     return;
 
   const base::TimeTicks now = base::TimeTicks::Now();
