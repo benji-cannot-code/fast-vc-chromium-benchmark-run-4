22f809cd72edae4bd70246e701926f0a23e520cd
Hwanseung Lee <hs1217.lee@samsung.com>
2020-04-01 00:53:55 +0000

    Convert enum to enum class for views::SliderChangeReason
    Use enum class instead of enum for views::SliderChangeReason.
    enum class is more type safety.
    
    Bug: 940736
    Change-Id: I396a74e79cdf6da52163d875fcceffa20920968f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2128033
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Hwanseung Lee <hs1217.lee@samsung.com>
    Cr-Commit-Position: refs/heads/master@{#755194}

--- ui/views/controls/slider.cc
+++ ui/views/controls/slider.cc
@@ -70,7 +70,7 @@ float Slider::GetValue() const {
 }
 
 void Slider::SetValue(float value) {
-  SetValueInternal(value, VALUE_CHANGED_BY_API);
+  SetValueInternal(value, SliderChangeReason::kByApi);
 }
 
 bool Slider::GetEnableAccessibilityEvents() const {
@@ -181,7 +181,7 @@ void Slider::MoveButtonTo(const gfx::Point& point) {
                    : point.x() - inset.left() - initial_button_offset_;
   SetValueInternal(
       static_cast<float>(amount) / (width() - inset.width() - kThumbWidth),
-      VALUE_CHANGED_BY_USER);
+      SliderChangeReason::kByUser);
 }
 
 void Slider::OnSliderDragStarted() {
@@ -241,7 +241,7 @@ bool Slider::OnKeyPressed(const ui::KeyEvent& event) {
       return false;
   }
   SetValueInternal(value_ + direction * keyboard_increment_,
-                   VALUE_CHANGED_BY_USER);
+                   SliderChangeReason::kByUser);
   return true;
 }
 
