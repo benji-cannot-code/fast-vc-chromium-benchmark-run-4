844dc76452521873fc1f8ac70e601200f529f435
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-04-02 18:46:00 +0000

    Revert "CL to verify that presubmit does not know anythyng about includes"
    This reverts commit 26f39e7805f1165de2b0dbff97753f594fdf87ed.
    
    Reason for revert: it has passed
    
    Original change's description:
    > CL to verify that presubmit does not know anythyng about includes
    > 
    > This CL tests whether we have existing presubmit check to remove
    > includes that are not needed
    > 
    > Change-Id: I821b3cefdc82c8bddf8be20a711af5b121822d82
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1879922
    > Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    > Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    > Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#755881}
    
    TBR=apotapchuk@chromium.org,burunduk@chromium.org
    
    Change-Id: I763613a4d48113c515cae121af544ccc1cb95bdd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133931
    Reviewed-by: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755918}

--- chrome/browser/chromeos/app_mode/web_app/web_kiosk_app_manager.h
+++ chrome/browser/chromeos/app_mode/web_app/web_kiosk_app_manager.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
-#include "base/threading/thread.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_manager_base.h"
 #include "chrome/browser/chromeos/app_mode/web_app/web_kiosk_app_data.h"
 #include "components/account_id/account_id.h"
