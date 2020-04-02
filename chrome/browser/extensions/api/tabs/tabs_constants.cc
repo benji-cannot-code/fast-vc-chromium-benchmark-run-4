39aac5a96fa0e6b9ee8250e581f6dc19760b232a
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-04-02 00:13:49 +0000

    [Extensions Cleanup] Use tabs.TabStatus ubiquitously for tab status
    The tabs.json API schema defined a tab's status both with a
    referenced enum (TabStatus) and an inline enum in two other places.
    This led to both overly-complicated code and an introduced risk for
    these enums (which were conceptually the same) diverging.
    
    Instead, use the same (referenced) enum for all these fields, and
    clean up the code.
    
    Bug: None
    Change-Id: I6a61b019bffa7156cb97a35d576c6f0cada16e70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131414
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755621}

--- chrome/browser/extensions/api/tabs/tabs_constants.cc
+++ chrome/browser/extensions/api/tabs/tabs_constants.cc
@@ -64,9 +64,6 @@ const char kShowStateValueMinimized[] = "minimized";
 const char kShowStateValueMaximized[] = "maximized";
 const char kShowStateValueFullscreen[] = "fullscreen";
 const char kShowStateValueLockedFullscreen[] = "locked-fullscreen";
-const char kStatusValueComplete[] = "complete";
-const char kStatusValueLoading[] = "loading";
-const char kStatusValueUnloaded[] = "unloaded";
 
 const char kWindowTypeValueNormal[] = "normal";
 const char kWindowTypeValuePopup[] = "popup";
