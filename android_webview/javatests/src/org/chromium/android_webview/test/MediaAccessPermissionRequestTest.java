17d8de17ddeca7802f2d638a64d74f7f9b1646e0
Nate Fischer <ntfschr@chromium.org>
2020-04-02 18:41:00 +0000

    AW: re-enable MediaAccessPermissionRequestTest
    This re-enables a flaky test which has been disabled the last few years.
    It seems OK locally, so let's see if it flakes on the bots.
    
    Bug: 653307
    Test: run_webview_instrumentation_test_apk --num_retries=0
    Test: --break-on-failure --repeat=1000
    Test: -f MediaAccessPermissionRequestTest.*
    Change-Id: I5aa806a2087c031ef674278f8c50f2ff4c6eb1f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134575
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755914}

--- android_webview/javatests/src/org/chromium/android_webview/test/MediaAccessPermissionRequestTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/MediaAccessPermissionRequestTest.java
@@ -19,9 +19,7 @@ import org.chromium.android_webview.permission.AwPermissionRequest;
 import org.chromium.android_webview.test.util.CommonResources;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.content_public.common.ContentSwitches;
 import org.chromium.net.test.util.TestWebServer;
 
@@ -81,9 +79,7 @@ public class MediaAccessPermissionRequestTest {
     @Test
     @Feature({"AndroidWebView"})
     @SmallTest
-    @DisableIf.Build(sdk_is_greater_than = 22, message = "crbug.com/623921")
     @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
-    @RetryOnFailure
     public void testGrantAccess() throws Throwable {
         final OnPermissionRequestHelper helper = new OnPermissionRequestHelper();
         TestAwContentsClient contentsClient =
@@ -107,11 +103,8 @@ public class MediaAccessPermissionRequestTest {
     @Test
     @Feature({"AndroidWebView"})
     @SmallTest
-    @DisableIf
-            .Build(sdk_is_greater_than = 22, message = "crbug.com/614347")
-            @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
-            @RetryOnFailure
-            public void testDenyAccess() throws Throwable {
+    @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
+    public void testDenyAccess() throws Throwable {
         final OnPermissionRequestHelper helper = new OnPermissionRequestHelper();
         TestAwContentsClient contentsClient =
                 new TestAwContentsClient() {
@@ -139,11 +132,8 @@ public class MediaAccessPermissionRequestTest {
     @Test
     @Feature({"AndroidWebView"})
     @SmallTest
-    @DisableIf
-            .Build(sdk_is_greater_than = 22, message = "crbug.com/614347")
-            @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
-            @RetryOnFailure
-            public void testDenyAccessByDefault() throws Throwable {
+    @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
+    public void testDenyAccessByDefault() throws Throwable {
         final OnPermissionRequestHelper helper = new OnPermissionRequestHelper();
         TestAwContentsClient contentsClient =
                 new TestAwContentsClient() {
@@ -175,11 +165,8 @@ public class MediaAccessPermissionRequestTest {
     @Test
     @Feature({"AndroidWebView"})
     @SmallTest
-    @DisableIf
-            .Build(sdk_is_greater_than = 22, message = "crbug.com/614347")
-            @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
-            @RetryOnFailure
-            public void testCancelPermission() throws Throwable {
+    @CommandLineFlags.Add(ContentSwitches.USE_FAKE_DEVICE_FOR_MEDIA_STREAM)
+    public void testCancelPermission() throws Throwable {
         final OnPermissionRequestHelper helper = new OnPermissionRequestHelper();
         TestAwContentsClient contentsClient =
                 new TestAwContentsClient() {
