47958bb03bdd6a5d42e925347d970aa90aa3f467
Jeevan Shikaram <jshikaram@chromium.org>
2020-04-02 07:40:06 +0000

    [App Management] Make App Info  BMO friendly.
    Check that WebAppProvider is not null and update chrome_launcher_controller.cc
    
    Bug: 1064877
    Change-Id: Ic53577519d4dc9d47b3b8153391bc9be1d8f225d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129447
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755733}

--- chrome/browser/ui/app_list/app_list_controller_delegate.cc
+++ chrome/browser/ui/app_list/app_list_controller_delegate.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/tablet_mode_page_behavior.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/webui/settings/chromeos/app_management/app_management_uma.h"
+#include "chrome/browser/web_applications/components/app_registrar.h"
 #include "chrome/browser/web_applications/components/web_app_utils.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/common/chrome_features.h"
@@ -105,9 +106,9 @@ void AppListControllerDelegate::DoShowAppInfoFlow(Profile* profile,
 
     chrome::ShowAppManagementPage(profile, app_id);
 
-    web_app::AppRegistrar& registrar =
-        web_app::WebAppProvider::Get(profile)->registrar();
-    if (registrar.IsInstalled(app_id)) {
+    web_app::WebAppProvider* web_app_provider =
+        web_app::WebAppProvider::Get(profile);
+    if (web_app_provider && web_app_provider->registrar().IsInstalled(app_id)) {
       base::UmaHistogramEnumeration(
           kAppManagementEntryPointsHistogramName,
           AppManagementEntryPoint::kAppListContextMenuAppInfoWebApp);
