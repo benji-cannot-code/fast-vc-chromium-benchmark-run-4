ed4be6725285ab583f0c58d9f52ce19224791be1
Andrew Xu <andrewxu@chromium.org>
2020-04-02 16:20:44 +0000

    Shelf icon animation metrics
    This CL records the smoothness for shelf icon animations which are
    usually triggered by adding/removing a shelf icon. In detail, there are
    three kinds of icon animations: (1) Move icons (2) Fade in an icon
    (3) Fade out an icon.
    
    This CL should be landed after https://crbug.com/1051490 is fixed.
    
    Bug: 1049702
    Change-Id: I8c47ba3a3111a2bc8a7361ddcc12420d45308cef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105675
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755859}

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/bind.h"
 #include "base/containers/adapters.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/numerics/ranges.h"
 #include "base/strings/utf_string_conversions.h"
@@ -95,6 +96,19 @@ constexpr SkColor kSeparatorColor = SkColorSetARGB(0x32, 0xFF, 0xFF, 0xFF);
 // items.
 constexpr int kSeparatorSize = 20;
 constexpr int kSeparatorThickness = 1;
+
+constexpr char kShelfIconMoveAnimationHistogram[] =
+    "Ash.ShelfIcon.AnimationSmoothness.Move";
+constexpr char kShelfIconFadeInAnimationHistogram[] =
+    "Ash.ShelfIcon.AnimationSmoothness.FadeIn";
+constexpr char kShelfIconFadeOutAnimationHistogram[] =
+    "Ash.ShelfIcon.AnimationSmoothness.FadeOut";
+
+enum class IconAnimationType {
+  kMoveAnimation,
+  kFadeInAnimation,
+  kFadeOutAnimation
+};
 
 // Helper to check if tablet mode is enabled.
 bool IsTabletModeEnabled() {
@@ -174,6 +188,33 @@ class ShelfFocusSearch : public views::FocusSearch {
   ShelfView* shelf_view_;
 
   DISALLOW_COPY_AND_ASSIGN(ShelfFocusSearch);
+};
+
+class IconAnimationMetricsReporter : public ui::AnimationMetricsReporter {
+ public:
+  explicit IconAnimationMetricsReporter(IconAnimationType type) : type_(type) {}
+  IconAnimationMetricsReporter(const IconAnimationMetricsReporter&) = delete;
+  IconAnimationMetricsReporter& operator=(const IconAnimationMetricsReporter&) =
+      delete;
+  ~IconAnimationMetricsReporter() override = default;
+
+ private:
+  void Report(int value) override {
+    switch (type_) {
+      case IconAnimationType::kMoveAnimation:
+        base::UmaHistogramPercentage(kShelfIconMoveAnimationHistogram, value);
+        break;
+      case IconAnimationType::kFadeInAnimation:
+        base::UmaHistogramPercentage(kShelfIconFadeInAnimationHistogram, value);
+        break;
+      case IconAnimationType::kFadeOutAnimation:
+        base::UmaHistogramPercentage(kShelfIconFadeOutAnimationHistogram,
+                                     value);
+        break;
+    }
+  }
+
+  IconAnimationType type_ = IconAnimationType::kMoveAnimation;
 };
 
 // Returns the id of the display on which |view| is shown.
@@ -319,6 +360,13 @@ int ShelfView::GetSizeOfAppIcons(int count) {
 }
 
 void ShelfView::Init() {
+  move_animation_reporter_ = std::make_unique<IconAnimationMetricsReporter>(
+      IconAnimationType::kMoveAnimation);
+  fade_in_animation_reporter_ = std::make_unique<IconAnimationMetricsReporter>(
+      IconAnimationType::kFadeInAnimation);
+  fade_out_animation_reporter_ = std::make_unique<IconAnimationMetricsReporter>(
+      IconAnimationType::kFadeOutAnimation);
+
   separator_ = new views::Separator();
   separator_->SetColor(kSeparatorColor);
   separator_->SetPreferredHeight(kSeparatorSize);
@@ -1189,6 +1237,7 @@ void ShelfView::OnTabletModeChanged() {
 
 void ShelfView::AnimateToIdealBounds() {
   CalculateIdealBounds();
+  bounds_animator_->SetAnimationMetricsReporter(move_animation_reporter_.get());
 
   for (int i = 0; i < view_model_->view_size(); ++i) {
     View* view = view_model_->view_at(i);
@@ -1211,6 +1260,8 @@ void ShelfView::FadeIn(views::View* view) {
   fade_in_animation_settings.SetPreemptionStrategy(
       ui::LayerAnimator::IMMEDIATELY_SET_NEW_TARGET);
   fade_in_animation_settings.AddObserver(fade_in_animation_delegate_.get());
+  fade_in_animation_settings.SetAnimationMetricsReporter(
+      fade_in_animation_reporter_.get());
   view->layer()->SetOpacity(1.f);
 }
 
@@ -1761,6 +1812,8 @@ void ShelfView::ShelfItemRemoved(int model_index, const ShelfItem& old_item) {
 
     // The first animation fades out the view. When done we'll animate the rest
     // of the views to their target location.
+    bounds_animator_->SetAnimationMetricsReporter(
+        fade_out_animation_reporter_.get());
     bounds_animator_->AnimateViewTo(view.get(), view->bounds());
     bounds_animator_->SetAnimationDelegate(
         view.get(), std::unique_ptr<gfx::AnimationDelegate>(
