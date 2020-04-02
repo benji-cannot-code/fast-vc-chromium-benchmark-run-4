2ffcc423c3e56af6673ba6037a4e03f9b0f34117
Erik Jensen <rkjnsn@google.com>
2020-04-02 03:34:55 +0000

    remoting: Hide continue dialog in destructor on Mac
    This prevents the window from remaining on the screen in a non-
    functioning state.
    
    Bug: 1060792
    Change-Id: Ie0849d306f11cfddf68209239be0d9d378d814ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133421
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755679}

--- remoting/host/continue_window_mac.mm
+++ remoting/host/continue_window_mac.mm
@@ -57,6 +57,9 @@ ContinueWindowMac::ContinueWindowMac() {
 
 ContinueWindowMac::~ContinueWindowMac() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (controller_) {
+    HideUi();
+  }
 }
 
 void ContinueWindowMac::ShowUi() {
@@ -74,6 +77,7 @@ void ContinueWindowMac::HideUi() {
 
   @autoreleasepool {
     [controller_ hide];
+    controller_.reset();
   }
 }
 
