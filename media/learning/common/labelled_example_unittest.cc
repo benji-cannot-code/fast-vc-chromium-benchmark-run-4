1be4f28161fa681d0d7bf47aa6e04f03119ee563
Piotr Tworek <ptworek@vewd.com>
2020-04-02 06:29:41 +0000

    Fix some unittest build failures on libstdc++ based Linux systems.
    The files modified in this patch use either std::find_if or std::sort.
    Both functions should be defined in algorithm header file. None of the
    offending files include this header and as result the code fails to
    build on libstdc++ based Linux systems. The code builds fine when using
    libcxx so probably the necessary headers get pulled in indirectly.
    
    Change-Id: If81a810cdffee26dbcd74e25123e4ecb12ce29ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131917
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#755711}

--- media/learning/common/labelled_example_unittest.cc
+++ media/learning/common/labelled_example_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/learning/common/labelled_example.h"
 
+#include <algorithm>
+
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace media {
--- net/cert/internal/cert_issuer_source_sync_unittest.h
+++ net/cert/internal/cert_issuer_source_sync_unittest.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_CERT_INTERNAL_CERT_ISSUER_SOURCE_SYNC_UNITTEST_H_
 #define NET_CERT_INTERNAL_CERT_ISSUER_SOURCE_SYNC_UNITTEST_H_
 
+#include <algorithm>
+
 #include "net/cert/internal/cert_errors.h"
 #include "net/cert/internal/cert_issuer_source.h"
 #include "net/cert/internal/test_helpers.h"
