e8aabeaec83715ee9f2f5a6aceb74c88f1631872
Anders Hartvoll Ruud <andruud@chromium.org>
2020-04-02 08:55:00 +0000

    DCHECK that custom properties are not added in the UA origin
    This is in preparation of 'revert'. By disallowing custom properties
    in the UA origin, we don't need a HashMap for would-be custom property
    declarations in that origin.
    
    Bug: 579788
    Change-Id: Ida09c0d33b0aea667f16230a517ba65d5270b818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2128053
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755751}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -1013,6 +1013,8 @@ CompositorKeyframeValue* StyleResolver::CreateCompositorKeyframeValueSnapshot(
       auto* set = MakeGarbageCollected<MutableCSSPropertyValueSet>(
           state.GetParserMode());
       set->SetProperty(property.GetCSSProperty().PropertyID(), *value);
+      cascade.MutableMatchResult().FinishAddingUARules();
+      cascade.MutableMatchResult().FinishAddingUserRules();
       cascade.MutableMatchResult().AddMatchedProperties(set);
       cascade.Apply();
     } else {
