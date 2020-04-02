6f49f7891be62f98ccdf73681169eef15d5bb2b9
Monica Basta <msalama@chromium.org>
2020-04-02 21:29:12 +0000

    [SyncSetup]: Add label for two external link buttons for a11y
    This CL adds aria-label for the two external link buttons in the sync
    section in chrome://settings/syncSetup page.
    
    Bug: 1066599
    Change-Id: I95ff9149b9e1e3da0dbea25b067a26aa7af6fd0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132452
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756006}

--- chrome/browser/resources/settings/people_page/sync_page.js
+++ chrome/browser/resources/settings/people_page/sync_page.js
@@ -26,6 +26,12 @@ Polymer({
       notify: true,
     },
 
+    /** @private {!Map<string, (string|Function)>} */
+    focusConfig: {
+      type: Object,
+      observer: 'onFocusConfigChange_',
+    },
+
     /** @private */
     pages_: {
       type: Object,
@@ -233,6 +239,14 @@ Polymer({
   },
 
   /** @private */
+  onFocusConfigChange_() {
+    const router = settings.Router.getInstance();
+    this.focusConfig.set(router.getRoutes().SYNC_ADVANCED, () => {
+      cr.ui.focusWithoutInk(assert(this.$$('#sync-advanced-row')));
+    });
+  },
+
+  /** @private */
   onSetupCancelDialogBack_() {
     /** @type {!CrDialogElement} */ (this.$$('#setupCancelDialog')).cancel();
     chrome.metricsPrivate.recordUserAction(
@@ -382,9 +396,15 @@ Polymer({
   },
 
   /** @private */
-  onActivityControlsTap_() {
+  onActivityControlsClick_() {
     chrome.metricsPrivate.recordUserAction('Sync_OpenActivityControlsPage');
     this.browserProxy_.openActivityControlsUrl();
+    window.open(loadTimeData.getString('activityControlsUrl'));
+  },
+
+  /** @private */
+  onSyncDashboardLinkClick_() {
+    window.open(loadTimeData.getString('syncDashboardUrl'));
   },
 
   /**
@@ -513,7 +533,7 @@ Polymer({
   },
 
   /** @private */
-  onSyncAdvancedTap_() {
+  onSyncAdvancedClick_() {
     const router = settings.Router.getInstance();
     router.navigateTo(router.getRoutes().SYNC_ADVANCED);
   },
