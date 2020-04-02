26bd6bd04009621780df00c4366d166a34e3d6d4
Brook Hong <hzgmaxwell@gmail.com>
2020-04-02 16:55:54 +0000

    Remove Unfreezable TaskRunner on RenderThreadImpl::RemoveRoute
    The task runner (`base::sequence_manager::internal::TaskQueueImpl::TaskRunner`
     -- child of `base::SingleThreadTaskRunner`) being held in
    `unfreezable_message_filter_` refers to an object of
    `base::sequence_manager::internal::TaskQueueImpl::GuardedTaskPoster`,
    which holds an object of `base::internal::OperationsController`, which
    then holds an object of `base::WaitableEvent`, which finnally holds an
    open mach port.
    
    The issue also impacts other systems like Windows / Linux.
    
    Bug: 1063577
    Change-Id: I5e34c71953835d9312360d349072ec4809f00d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132536
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755872}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -904,6 +904,7 @@ void RenderThreadImpl::AddRoute(int32_t routing_id, IPC::Listener* listener) {
 
 void RenderThreadImpl::RemoveRoute(int32_t routing_id) {
   ChildThreadImpl::GetRouter()->RemoveRoute(routing_id);
+  unfreezable_message_filter_->RemoveListenerUnfreezableTaskRunner(routing_id);
   GetChannel()->RemoveListenerTaskRunner(routing_id);
 }
 
@@ -2298,6 +2299,14 @@ void RenderThreadImpl::UnfreezableMessageFilter::
   DCHECK(!base::Contains(unfreezable_task_runners_, routing_id));
   unfreezable_task_runners_.emplace(routing_id,
                                     std::move(unfreezable_task_runner));
+}
+
+// Called on the listener thread.
+void RenderThreadImpl::UnfreezableMessageFilter::
+    RemoveListenerUnfreezableTaskRunner(
+        int32_t routing_id) {
+  base::AutoLock lock(unfreezable_task_runners_lock_);
+  unfreezable_task_runners_.erase(routing_id);
 }
 
 // Called on the I/O thread.
