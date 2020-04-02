287eef582096414cee01cc930ebfd0b040d3cbbe
Yue Li <updowndota@google.com>
2020-04-02 00:48:07 +0000

    [Assistant Opt-in] Fix lottie animation delay for voice match screen
    Bug: b/152592364
    Test: Manual Test
    Change-Id: I4ecac19788bc409f4d3426b62fc8be1be1172ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132687
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755639}

--- chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
+++ chrome/browser/resources/chromeos/assistant_optin/assistant_optin_flow.js
@@ -69,6 +69,7 @@ Polymer({
       case this.FlowType.SPEAKER_ID_ENROLLMENT:
       case this.FlowType.SPEAKER_ID_RETRAIN:
         this.$['value-prop'].hidden = true;
+        this.$['voice-match'].isFirstScreen = true;
         this.showScreen(this.$['voice-match']);
         break;
       default:
