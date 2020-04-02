803e34cc21e61dcfef12aa101df641432caed9d6
Greg Guterman <guterman@google.com>
2020-04-02 22:17:54 +0000

    Remove branches from mb_config_buckets and add feedback
    Different configs for branches are handled by the git branches themselves, so branch-specific config keys are not needed.
    
    Bug: 1062141
    Change-Id: I82832cda02ab798afb77fb26d973f001c53c4bdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133078
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Gregory Guterman <guterman@google.com>
    Cr-Commit-Position: refs/heads/master@{#756032}

--- tools/mb/lib/validation.py
+++ tools/mb/lib/validation.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import ast
 import collections
 import json
+import re
 
 
 def GetAllConfigsMaster(masters):
@@ -170,15 +171,16 @@ def CheckMasterBucketConsistency(errs, masters_file, buckets_file):
 
   # Cross check builders
   configs_by_builder = _GetConfigsByBuilder(master_contents['masters'])
-  for builders in bucket_contents['buckets'].values():
+  for bucketname, builders in bucket_contents['buckets'].items():
     missing = check_missing(builders, configs_by_builder)
-    errs.extend('Builder "%s" from mb_config_buckets.pyl '
-                'not found in mb_config.pyl' % builder for builder in missing)
+    errs.extend('Builder "%s" in bucket "%s" from mb_config_buckets.pyl '
+                'not found in mb_config.pyl' % (builder, bucketname)
+                for builder in missing)
 
     for buildername, config in builders.items():
       if config not in configs_by_builder[buildername]:
-        errs.append('Builder "%s" from mb_config_buckets.pyl '
-                    'doesn\'t match mb_config.pyl' % buildername)
+        errs.append('Builder "%s" in bucket "%s" from mb_config_buckets.pyl '
+                    'doesn\'t match mb_config.pyl' % (buildername, bucketname))
 
   def check_mismatch(bucket_dict, master_dict):
     mismatched = []
