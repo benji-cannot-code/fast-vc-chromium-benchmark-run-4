e795c8ca8263f2a22f4eb59472c2c8f0f32be1e8
Alexander Alekseev <alemate@chromium.org>
2020-04-02 03:08:00 +0000

    Ash,Wallpaper: Add support for reloading WallpaperType::ONE_SHOT.
    Display change (like screen rotation) triggers wallpaper reload.
    But WallpaperType::ONE_SHOT cannot be reloaded, because
    WallpaperController does not have source for it.
    
    This CL caches ONE_SHOT wallpaper value in WallpaperInfo and implements
    reloading of one shot wallpaper.
    
    Bug: 1056565
    Change-Id: I3a63657e37f8a47380f9b865eb3857295a8de028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129851
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755675}

--- ash/public/cpp/wallpaper_info.h
+++ ash/public/cpp/wallpaper_info.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/wallpaper_types.h"
 #include "base/time/time.h"
+#include "ui/gfx/image/image_skia.h"
 
 namespace ash {
 
@@ -37,6 +38,11 @@ struct WallpaperInfo {
   WallpaperLayout layout;
   WallpaperType type;
   base::Time date;
+
+  // Not empty if type == WallpaperType::ONE_SHOT.
+  // This field is filled in by ShowWallpaperImage when image is already
+  // decoded.
+  gfx::ImageSkia one_shot_wallpaper;
 };
 
 }  // namespace ash
