9ce83bd906ca49ee7528e6a31b4aa9a659f74c11
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-04-02 10:48:00 +0000

    [iOS][Credential-Provider] Create support check
    Bug: 1066803
    Change-Id: Ib16222057cc4d1835e4ad73a6ff15cf83f753403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130213
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755773}

---
+++ ios/chrome/browser/credential_provider/credential_provider_support.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/credential_provider/credential_provider_support.h"
+
+#include "ios/chrome/browser/ui/util/credential_provider_buildflags.h"
+
+bool IsCredentialProviderExtensionSupported() {
+  return BUILDFLAG(IOS_CREDENTIAL_PROVIDER_ENABLED);
+}
