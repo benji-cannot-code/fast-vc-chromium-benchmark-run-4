9c9a3da8aec49fcc6a8a32e7ae16080f237803ca
Minoru Chikamune <chikamune@chromium.org>
2020-04-02 00:11:31 +0000

    Migrate NotificationManager to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: I04ae25faa5c1b5fdc00514edb04593e2249c5b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131553
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755620}

--- third_party/blink/renderer/modules/notifications/notification_manager.h
+++ third_party/blink/renderer/modules/notifications/notification_manager.h
@@ -8,12 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
-#include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/notifications/notification_service.mojom-blink.h"
 #include "third_party/blink/public/mojom/permissions/permission.mojom-blink.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_notification_permission_callback.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -94,8 +95,7 @@ class NotificationManager final : public GarbageCollected<NotificationManager>,
 
   // Returns an initialized NotificationService remote. A connection will be
   // established the first time this method is called.
-  const mojo::Remote<mojom::blink::NotificationService>&
-  GetNotificationService();
+  mojom::blink::NotificationService* GetNotificationService();
 
   void OnPermissionRequestComplete(
       ScriptPromiseResolver* resolver,
@@ -105,8 +105,12 @@ class NotificationManager final : public GarbageCollected<NotificationManager>,
   void OnNotificationServiceConnectionError();
   void OnPermissionServiceConnectionError();
 
-  mojo::Remote<mojom::blink::NotificationService> notification_service_;
-  mojo::Remote<mojom::blink::PermissionService> permission_service_;
+  HeapMojoRemote<mojom::blink::NotificationService,
+                 HeapMojoWrapperMode::kWithoutContextObserver>
+      notification_service_;
+  HeapMojoRemote<mojom::blink::PermissionService,
+                 HeapMojoWrapperMode::kWithoutContextObserver>
+      permission_service_;
 
   DISALLOW_COPY_AND_ASSIGN(NotificationManager);
 };
