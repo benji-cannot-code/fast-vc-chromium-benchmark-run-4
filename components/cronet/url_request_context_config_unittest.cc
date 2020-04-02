703d9932f221c69188bcef076bef7105c8905bea
Zhongyi Shi <zhongyi@chromium.org>
2020-04-02 17:53:05 +0000

    Add test coverage to ensure cronet set_quic_flags correctly.
    This change is to verify that integer value of quic flag is handled
    correctly.
    
    Bug: 1005610
    Change-Id: I8cdbf1e365ed4cb3afa3e892b09ef3000fd9d06f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133226
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#755895}

--- components/cronet/url_request_context_config_unittest.cc
+++ components/cronet/url_request_context_config_unittest.cc
@@ -69,6 +69,7 @@ TEST(URLRequestContextConfigTest, TestExperimentalOptionParsing) {
   options.SetPath(
       {"QUIC", "set_quic_flags"},
       base::Value(
+          "FLAGS_quic_max_aggressive_retransmittable_on_wire_ping_count=5,"
           "FLAGS_quic_reloadable_flag_quic_enable_version_t050=true,"
           "FLAGS_quic_reloadable_flag_quic_enable_version_draft_27=true"));
   options.SetPath({"AsyncDNS", "enable"}, base::Value(true));
@@ -142,6 +143,7 @@ TEST(URLRequestContextConfigTest, TestExperimentalOptionParsing) {
   EXPECT_TRUE(base::JSONWriter::Write(options, &options_json));
 
   // Initialize QUIC flags set by the config.
+  FLAGS_quic_max_aggressive_retransmittable_on_wire_ping_count = 0;
   FLAGS_quic_reloadable_flag_quic_enable_version_t050 = false;
   FLAGS_quic_reloadable_flag_quic_enable_version_draft_27 = false;
 
@@ -194,6 +196,7 @@ TEST(URLRequestContextConfigTest, TestExperimentalOptionParsing) {
   quic_connection_options.push_back(quic::kREJ);
   EXPECT_EQ(quic_connection_options, quic_params->connection_options);
 
+  EXPECT_EQ(FLAGS_quic_max_aggressive_retransmittable_on_wire_ping_count, 5);
   EXPECT_TRUE(FLAGS_quic_reloadable_flag_quic_enable_version_t050);
   EXPECT_TRUE(FLAGS_quic_reloadable_flag_quic_enable_version_draft_27);
 
