4374cf43fd28c1d4f2f465bcb9df93bd1e3bfb95
Wenbin Zhang <wenbinzhang@google.com>
2020-04-03 17:23:11 +0000

    [benchmarking] linux-perf-fyi stop uploading perf data
    Stop uploading perf data to perf dashboard on linux fyi.
    This is part of moving the upload workflow to the new VM.
    
    Bug: chromium:1045256
    Change-Id: Ifb09b5ff12a56254c18edf41038f3952f03518ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132961
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756308}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -91,6 +91,10 @@ class TEST_TYPES(object):
 
   ALL = (GENERIC, GTEST, TELEMETRY)
 
+# This is an opt-in list for tester which will skip the perf data handling.
+# The perf data will be handled on a separated 'processor' VM.
+# This list will be removed or replace by an opt-out list.
+LIGHTWEIGHT_TESTERS = ['linux-perf-fyi']
 
 FYI_BUILDERS = {
     'android-nexus5x-perf-fyi': {
@@ -1042,6 +1046,8 @@ def generate_performance_test(tester_config, test, builder_name):
   result['merge'] = {
       'script': '//tools/perf/process_perf_results.py',
   }
+  if builder_name in LIGHTWEIGHT_TESTERS:
+    result['merge']['args'] = ['--skip-perf']
 
   result['swarming'] = {
       # Always say this is true regardless of whether the tester
