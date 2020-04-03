26149150d37aa06c16cbc334a63be815f79f3d26
Hiroki Sato <hirokisato@google.com>
2020-04-03 02:29:30 +0000

    arc-a11y: Handle VIEW_SELECTED event appropriately
    When user press tab in ListView, VIEW_SELECTED event is dispatched.
    This event is not only dispatched from the newly selected node but also
    from List itself and child nodes of it. Previously filtering out these
    event were took place in Android AccessibilityService side.
    This CL migrates the event handling to Chrome side.
    
    Also, this type of event was converted to chrome selection event, but
    focus event is more appropriate. This CL also fixes it.
    
    Bug: b/148837372
    Bug: b/146916101
    Bug: b/143336586
    Bug: b/139645142
    Bug: b/152374820
    Test: unit_tests --gtest_filter="AXTreeSourceArcTest.*"
    Test: manual with ag/10901596. tab navigation works in PlayStore hamburger menu and talkback test appp list menu.
    Change-Id: Ia8d2f3bb71dcb5a9b9b796dc89e4d8d2a733a940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129381
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Sara Kato <sarakato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756140}

--- chrome/browser/chromeos/arc/accessibility/arc_accessibility_util.h
+++ chrome/browser/chromeos/arc/accessibility/arc_accessibility_util.h
@@ -15,9 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_enum_util.h"
 
 namespace arc {
+class AccessibilityInfoDataWrapper;
 
 ax::mojom::Event ToAXEvent(mojom::AccessibilityEventType arc_event_type,
-                           mojom::AccessibilityNodeInfoData* node_info_data);
+                           AccessibilityInfoDataWrapper* source_node,
+                           AccessibilityInfoDataWrapper* focused_node);
 
 base::Optional<mojom::AccessibilityActionType> ConvertToAndroidAction(
     ax::mojom::Action action);
