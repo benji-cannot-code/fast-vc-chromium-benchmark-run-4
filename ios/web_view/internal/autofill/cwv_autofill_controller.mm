932cfa09a288c8fa9b51d46dfc4794f8559a58c1
John Z Wu <jzw@chromium.org>
2020-04-03 04:11:28 +0000

    Define APIs for credit card fix flows
    Bug: 1066690
    Change-Id: I3fdf503aceb45dd93d51187ab30f38c887c2ccdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134958
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756151}

--- ios/web_view/internal/autofill/cwv_autofill_controller.mm
+++ ios/web_view/internal/autofill/cwv_autofill_controller.mm
@@ -38,7 +38,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web_view/internal/autofill/cwv_autofill_form_internal.h"
 #import "ios/web_view/internal/autofill/cwv_autofill_profile_internal.h"
 #import "ios/web_view/internal/autofill/cwv_autofill_suggestion_internal.h"
+#import "ios/web_view/internal/autofill/cwv_credit_card_expiration_fixer_internal.h"
 #import "ios/web_view/internal/autofill/cwv_credit_card_internal.h"
+#import "ios/web_view/internal/autofill/cwv_credit_card_name_fixer_internal.h"
 #import "ios/web_view/internal/autofill/cwv_credit_card_saver_internal.h"
 #import "ios/web_view/internal/autofill/cwv_credit_card_verifier_internal.h"
 #include "ios/web_view/internal/autofill/web_view_autocomplete_history_manager_factory.h"
@@ -423,6 +425,39 @@ fetchNonPasswordSuggestionsForFormWithName:(NSString*)formName
        localSavePromptCallback:std::move(callback)];
   [_delegate autofillController:self saveCreditCardWithSaver:saver];
   _saver = saver;
+}
+
+- (void)
+    confirmCreditCardAccountName:(const base::string16&)name
+                        callback:
+                            (base::OnceCallback<void(const base::string16&)>)
+                                callback {
+  if (![_delegate respondsToSelector:@selector(autofillController:
+                                         confirmCreditCardNameWithFixer:)]) {
+    return;
+  }
+
+  CWVCreditCardNameFixer* fixer = [[CWVCreditCardNameFixer alloc]
+      initWithName:base::SysUTF16ToNSString(name)
+          callback:std::move(callback)];
+  [_delegate autofillController:self confirmCreditCardNameWithFixer:fixer];
+}
+
+- (void)confirmCreditCardExpirationWithCard:(const autofill::CreditCard&)card
+                                   callback:
+                                       (base::OnceCallback<void(
+                                            const base::string16&,
+                                            const base::string16&)>)callback {
+  if (![_delegate respondsToSelector:@selector
+                  (autofillController:confirmCreditCardExpirationWithFixer:)]) {
+    return;
+  }
+
+  CWVCreditCardExpirationFixer* fixer = [[CWVCreditCardExpirationFixer alloc]
+      initWithCreditCard:card
+                callback:std::move(callback)];
+  [_delegate autofillController:self
+      confirmCreditCardExpirationWithFixer:fixer];
 }
 
 - (void)
