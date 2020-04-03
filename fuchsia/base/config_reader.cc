2661e80e53456f35fcd4f45a49efeba346ff2a8e
Wez <wez@chromium.org>
2020-04-03 18:59:38 +0000

    [fuchsia] Cache the package configuration in LoadPackageConfig().
    Package configurations do not change at run-time, so cache it the first
    time it is read, to avoid the cost of repeatedly re-reading it from the
    filesystem.
    
    Change-Id: I53c6ea55be5fca48bf3c81f189e251d8a11a55a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135607
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756349}

--- fuchsia/base/config_reader.cc
+++ fuchsia/base/config_reader.cc
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/json/json_reader.h"
+#include "base/no_destructor.h"
 
 namespace cr_fuchsia {
 
-base::Optional<base::Value> LoadPackageConfig() {
+namespace {
+
+base::Optional<base::Value> ReadPackageConfig() {
   constexpr char kConfigPath[] = "/config/data/config.json";
 
   base::FilePath path(kConfigPath);
@@ -34,6 +37,16 @@ base::Optional<base::Value> LoadPackageConfig() {
                            << path.value();
 
   return std::move(parsed.value());
+}
+
+}  // namespace
+
+const base::Optional<base::Value>& LoadPackageConfig() {
+  // Package configurations do not change at run-time, so read the configuration
+  // on the first call and cache the result.
+  static base::NoDestructor<base::Optional<base::Value>> config(
+      ReadPackageConfig());
+  return *config;
 }
 
 }  // namespace cr_fuchsia
