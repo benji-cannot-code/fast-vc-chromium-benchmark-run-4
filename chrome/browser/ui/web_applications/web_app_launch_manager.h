0e5cfef75fa3488b3b4a8243e75d86182c0efc7b
nancy <nancylingwang@chromium.org>
2020-04-03 06:54:24 +0000

    Remove launch service.
    BUG=1061843
    
    Change-Id: I12efd2899403cb9cc5e9dafc04300a7156342ed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116156
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756170}

--- chrome/browser/ui/web_applications/web_app_launch_manager.h
+++ chrome/browser/ui/web_applications/web_app_launch_manager.h
@@ -8,11 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/apps/launch_service/launch_manager.h"
+#include "components/services/app_service/public/mojom/types.mojom.h"
 
 class Browser;
 enum class WindowOpenDisposition;
 class GURL;
+class Profile;
 
 namespace apps {
 struct AppLaunchParams;
@@ -33,14 +34,13 @@ class WebAppProvider;
 
 // Handles launch requests for Desktop PWAs and bookmark apps.
 // Web applications have type AppType::kWeb in the app registry.
-class WebAppLaunchManager : public apps::LaunchManager {
+class WebAppLaunchManager {
  public:
   explicit WebAppLaunchManager(Profile* profile);
-  ~WebAppLaunchManager() override;
+  ~WebAppLaunchManager();
 
   // apps::LaunchManager:
-  content::WebContents* OpenApplication(
-      const apps::AppLaunchParams& params) override;
+  content::WebContents* OpenApplication(const apps::AppLaunchParams& params);
 
   void LaunchApplication(
       const std::string& app_id,
@@ -57,6 +57,7 @@ class WebAppLaunchManager : public apps::LaunchManager {
                               apps::mojom::LaunchContainer container)>
           callback);
 
+  Profile* const profile_;
   WebAppProvider* const provider_;
 
   base::WeakPtrFactory<WebAppLaunchManager> weak_ptr_factory_{this};
