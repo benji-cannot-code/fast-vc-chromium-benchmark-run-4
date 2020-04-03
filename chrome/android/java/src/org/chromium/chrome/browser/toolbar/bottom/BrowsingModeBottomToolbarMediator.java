f17d8e4d7bfc72bab9c3fd8e88cfcee47306f307
Henry Jian <hzjian@google.com>
2020-04-03 20:25:10 +0000

    [Android] Remove use of callback for feature highlight
    This CL removes the use of callback for feature highlight.
    
    Because the callback is no longer needed, we're removing
    its use now.
    
    Bug: 1057234
    Change-Id: I5ce9ba270851397da926f8c33a4e8c3cbc5d8fbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122619
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Henry Jian <hzjian@google.com>
    Cr-Commit-Position: refs/heads/master@{#756394}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BrowsingModeBottomToolbarMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/bottom/BrowsingModeBottomToolbarMediator.java
@@ -71,10 +71,9 @@ class BrowsingModeBottomToolbarMediator implements ThemeColorObserver {
      * @param activity An activity to attach the IPH to.
      * @param anchor The view to anchor the IPH to.
      * @param tracker A tracker for IPH.
-     * @param completeRunnable The Runnable to be called if the user tab on the view.
      */
     void showIPH(@FeatureConstants String feature, ChromeActivity activity, View anchor,
-            Tracker tracker, Runnable completeRunnable) {
+            Tracker tracker) {
         if (!tracker.shouldTriggerHelpUI(feature) || !anchor.isShown() || !anchor.isEnabled()) {
             return;
         }
@@ -127,8 +126,8 @@ class BrowsingModeBottomToolbarMediator implements ThemeColorObserver {
                 R.style.TextAppearance_TextLarge_Primary_Light, descId,
                 FeatureHighlightProvider.TextAlignment.CENTER,
                 R.style.TextAppearance_TextMedium_Primary_Light, innerBackgroundColor,
-                finalOuterColor, finalScrimColor, FeatureHighlightProvider.NO_TIMEOUT, tapToDismiss,
-                completeRunnable);
+                finalOuterColor, finalScrimColor, FeatureHighlightProvider.NO_TIMEOUT,
+                tapToDismiss);
     }
 
     /**
