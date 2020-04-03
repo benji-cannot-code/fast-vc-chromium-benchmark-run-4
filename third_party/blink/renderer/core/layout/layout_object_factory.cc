276421172feae4f97c95438f2cf41b6fc2cc0312
Kurt Catti-Schmidt <kschmi@microsoft.com>
2020-04-03 15:26:43 +0000

    Initial plumbing for LayoutNG Grid
    This change introduces two new classes that will be used for CSS Grid
    in LayoutNG, as well as the runtime flag "LayoutNGGrid" that enables
    CSS Grid support in LayoutNG.
    
    This is not at all functional yet but will allow for the subsequent
    changes to focus on the implementation instead of boilerplate.
    
    Bug: 1045599
    Change-Id: I04fb4455761b1f66cf567440fc5298a39230cbbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2042056
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#756274}

--- third_party/blink/renderer/core/layout/layout_object_factory.cc
+++ third_party/blink/renderer/core/layout/layout_object_factory.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_fieldset.h"
 #include "third_party/blink/renderer/core/layout/layout_file_upload_control.h"
 #include "third_party/blink/renderer/core/layout/layout_flexible_box.h"
+#include "third_party/blink/renderer/core/layout/layout_grid.h"
 #include "third_party/blink/renderer/core/layout/layout_inside_list_marker.h"
 #include "third_party/blink/renderer/core/layout/layout_list_item.h"
 #include "third_party/blink/renderer/core/layout/layout_outside_list_marker.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_block_flow.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_fieldset.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_flexible_box.h"
+#include "third_party/blink/renderer/core/layout/ng/layout_ng_grid.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_progress.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_table_caption.h"
 #include "third_party/blink/renderer/core/layout/ng/layout_ng_table_cell.h"
@@ -105,6 +107,14 @@ LayoutBlock* LayoutObjectFactory::CreateFlexibleBox(Node& node,
                                                     LegacyLayout legacy) {
   bool disable_ng_for_type = !RuntimeEnabledFeatures::LayoutNGFlexBoxEnabled();
   return CreateObject<LayoutBlock, LayoutNGFlexibleBox, LayoutFlexibleBox>(
+      node, style, legacy, disable_ng_for_type);
+}
+
+LayoutBlock* LayoutObjectFactory::CreateGrid(Node& node,
+                                             const ComputedStyle& style,
+                                             LegacyLayout legacy) {
+  bool disable_ng_for_type = !RuntimeEnabledFeatures::LayoutNGGridEnabled();
+  return CreateObject<LayoutBlock, LayoutNGGrid, LayoutGrid>(
       node, style, legacy, disable_ng_for_type);
 }
 
