a9bca780ac13b12b79904ef4c8ec5774f5112a04
Minoru Chikamune <chikamune@chromium.org>
2020-04-03 08:03:38 +0000

    Migrate KeyboardLayout to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: Iaae3dbd46bc56a2e6b0c0f2d7ed2588dddcb014d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132080
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756191}

--- third_party/blink/renderer/modules/keyboard/keyboard_layout.cc
+++ third_party/blink/renderer/modules/keyboard/keyboard_layout.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
@@ -32,7 +33,7 @@ constexpr char kKeyboardMapRequestFailedErrorMsg[] =
 }  // namespace
 
 KeyboardLayout::KeyboardLayout(ExecutionContext* context)
-    : ExecutionContextClient(context) {}
+    : ExecutionContextClient(context), service_(context) {}
 
 ScriptPromise KeyboardLayout::GetKeyboardLayoutMap(
     ScriptState* script_state,
@@ -76,14 +77,15 @@ bool KeyboardLayout::IsLocalFrameAttached() {
 }
 
 bool KeyboardLayout::EnsureServiceConnected() {
-  if (!service_) {
+  if (!service_.is_bound()) {
     LocalFrame* frame = GetFrame();
     if (!frame) {
       return false;
     }
     frame->GetBrowserInterfaceBroker().GetInterface(
-        service_.BindNewPipeAndPassReceiver());
-    DCHECK(service_);
+        service_.BindNewPipeAndPassReceiver(
+            frame->GetTaskRunner(TaskType::kMiscPlatformAPI)));
+    DCHECK(service_.is_bound());
   }
   return true;
 }
@@ -117,6 +119,7 @@ void KeyboardLayout::GotKeyboardLayoutMap(
 
 void KeyboardLayout::Trace(Visitor* visitor) {
   visitor->Trace(script_promise_resolver_);
+  visitor->Trace(service_);
   ExecutionContextClient::Trace(visitor);
 }
 
