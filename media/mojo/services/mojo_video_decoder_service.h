54ff57b4d52f54c56834666b95e33b6631e477bf
Xiaohan Wang <xhwang@chromium.org>
2020-04-03 00:30:09 +0000

    media: Disallow CDM reset in mojo media services
    In general, after playback starts, we do NOT support removing or
    resetting the CDM if one is already set. There's no use case for this
    at least for now and foreseeable future, and this change would help
    simplify some logic.
    
    Bug: 1066893
    Change-Id: I53ad2445fab79d78b34e76d252c9c069c3e8950d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133173
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756091}

--- media/mojo/services/mojo_video_decoder_service.h
+++ media/mojo/services/mojo_video_decoder_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/unguessable_token.h"
+#include "media/base/cdm_context.h"
 #include "media/base/decode_status.h"
 #include "media/base/overlay_info.h"
 #include "media/base/video_decoder.h"
@@ -111,8 +112,9 @@ class MEDIA_MOJO_EXPORT MojoVideoDecoderService final
   // Helper for reading DecoderBuffer data from the DataPipe.
   std::unique_ptr<MojoDecoderBufferReader> mojo_decoder_buffer_reader_;
 
-  // Holds the CdmContextRef to keep the CdmContext alive for the lifetime of
-  // the |decoder_|.
+  // The CDM ID and the corresponding CdmContextRef, which must be held to keep
+  // the CdmContext alive for the lifetime of the |decoder_|.
+  int cdm_id_ = CdmContext::kInvalidCdmId;
   std::unique_ptr<CdmContextRef> cdm_context_ref_;
 
   std::unique_ptr<media::VideoDecoder> decoder_;
