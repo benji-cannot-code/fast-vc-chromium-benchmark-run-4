f175f436371b3ce65be1b7cb9e5cf5779dfa645e
Becca Hughes <beccahughes@chromium.org>
2020-04-03 18:53:58 +0000

    [Media History] Add histogram for database size
    Add a histogram that records the size of the
    database in KB.
    
    BUG=1024362
    
    Change-Id: I4fac8d7184b0c9e2c53554df0175512c0266a1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2125269
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756345}

--- chrome/browser/media/history/media_history_store.cc
+++ chrome/browser/media/history/media_history_store.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/stringprintf.h"
 #include "base/task_runner_util.h"
@@ -275,6 +276,17 @@ void MediaHistoryStoreInternal::Initialize() {
 
   base::UmaHistogramEnumeration(MediaHistoryStore::kInitResultHistogramName,
                                 MediaHistoryStore::InitResult::kSuccess);
+
+  // Get the size in bytes.
+  int64_t file_size = 0;
+  base::GetFileSize(db_path_, &file_size);
+  DCHECK_NE(0, file_size);
+
+  // Record the size in KB.
+  if (file_size > 0) {
+    base::UmaHistogramMemoryKB(MediaHistoryStore::kDatabaseSizeKbHistogramName,
+                               file_size / 1000);
+  }
 }
 
 sql::InitStatus MediaHistoryStoreInternal::CreateOrUpgradeIfNeeded() {
@@ -724,6 +736,9 @@ const char MediaHistoryStore::kPlaybackWriteResultHistogramName[] =
 
 const char MediaHistoryStore::kSessionWriteResultHistogramName[] =
     "Media.History.Session.WriteResult";
+
+const char MediaHistoryStore::kDatabaseSizeKbHistogramName[] =
+    "Media.History.DatabaseSize";
 
 MediaHistoryStore::MediaHistoryStore(
     Profile* profile,
