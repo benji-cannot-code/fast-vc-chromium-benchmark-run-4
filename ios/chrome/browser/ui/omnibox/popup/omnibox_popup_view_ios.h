500078f6232e2b67c6c6f4806084d6696f831f0a
Orin Jaworski <orinj@chromium.org>
2020-04-03 01:40:40 +0000

    [omnibox] Change popup handler API to pass Selection instead of line only
    This CL starts preparing popup handlers for changes to either line
    index or line state, instead of only considering line and assuming
    the NORMAL state. The interface is changed, but not behavior.
    
    Bug: 1046523
    Change-Id: I1cdf5e288b6be2c488795be2bfba0b276327326e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132769
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Auto-Submit: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756122}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_ios.h
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_view_ios.h
@@ -11,13 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/strings/string16.h"
+#include "components/omnibox/browser/omnibox_popup_model.h"
 #include "components/omnibox/browser/omnibox_popup_view.h"
 #import "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_mediator.h"
 #include "ios/chrome/browser/ui/omnibox/popup/omnibox_popup_provider.h"
 
 class OmniboxEditModel;
 @class OmniboxPopupMediator;
-class OmniboxPopupModel;
 class OmniboxPopupViewSuggestionsDelegate;
 struct AutocompleteMatch;
 
