adc05731f346586c54127e3824f5687942387336
Michael Tang <tangm@microsoft.com>
2020-04-03 23:14:06 +0000

    Implement tag parsing and extra/app arguments
    - Ported Omaha 3's ExtraArgsParser into chrome/updater.
    - (At Sorin's request) Renamed ExtraArgs to something else (currently TagArgs, but I'm open to renaming it or any other item)
    - Ported the Omaha 3 ExtraArgsParser tests
    
    Change-Id: Ief76020716ae35fff26bacd19357847651acad5a
    Bug: 1050738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2081332
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756449}

---
+++ chrome/updater/lib_util.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_UPDATER_LIB_UTIL_H_
+#define CHROME_UPDATER_LIB_UTIL_H_
+
+#include <string>
+
+#include "base/strings/string_piece.h"
+
+namespace updater {
+
+// This is a lightweight implementation of net::UnescapeURLComponent to avoid a
+// dependency on //net using the following rules: NORMAL | SPACES |
+// PATH_SEPARATORS | URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS.
+//
+// |escaped_text| should only contain ASCII text.
+//
+// Returns back |escaped_text| if unescaping failed.
+std::string UnescapeURLComponent(base::StringPiece escaped_text);
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_LIB_UTIL_H_
