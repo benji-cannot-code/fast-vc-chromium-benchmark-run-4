2d78d4b8a08997328e5ddbaae1dbd041b7a5b1dd
Etienne Bergeron <etienneb@chromium.org>
2020-04-03 18:42:24 +0000

    Reduce scope of waitable event in disk_cache BackendImpl
    This is a step forward to remove the waitable event.
    This CL is removing the Waitable event member and reducing
    its scope only to the destructor (where it is used).
    
    R=gab@chromium.org
    
    Bug: 1065004
    Change-Id: I7a813c09a52ccd428c64631bd81fc8a5d710c880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132766
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756335}

--- net/disk_cache/blockfile/backend_impl.cc
+++ net/disk_cache/blockfile/backend_impl.cc
@@ -111,8 +111,10 @@ bool InitExperiment(disk_cache::IndexHeader* header, bool cache_created) {
 }
 
 // A callback to perform final cleanup on the background thread.
-void FinalCleanupCallback(disk_cache::BackendImpl* backend) {
+void FinalCleanupCallback(disk_cache::BackendImpl* backend,
+                          base::WaitableEvent* done) {
   backend->CleanupCache();
+  done->Signal();
 }
 
 class CacheThread : public base::Thread {
@@ -160,9 +162,7 @@ BackendImpl::BackendImpl(
       block_files_(path),
       mask_(0),
       user_flags_(0),
-      net_log_(net_log),
-      done_(base::WaitableEvent::ResetPolicy::MANUAL,
-            base::WaitableEvent::InitialState::NOT_SIGNALED) {
+      net_log_(net_log) {
   TRACE_EVENT0("disk_cache", "BackendImpl::BackendImpl");
 }
 
@@ -178,9 +178,7 @@ BackendImpl::BackendImpl(
       block_files_(path),
       mask_(mask),
       user_flags_(kMask),
-      net_log_(net_log),
-      done_(base::WaitableEvent::ResetPolicy::MANUAL,
-            base::WaitableEvent::InitialState::NOT_SIGNALED) {
+      net_log_(net_log) {
   TRACE_EVENT0("disk_cache", "BackendImpl::BackendImpl");
 }
 
@@ -200,12 +198,15 @@ BackendImpl::~BackendImpl() {
     // Unit tests may use the same sequence for everything.
     CleanupCache();
   } else {
+    // Signals the end of background work.
+    base::WaitableEvent done;
+
     background_queue_.background_thread()->PostTask(
-        FROM_HERE,
-        base::BindOnce(&FinalCleanupCallback, base::Unretained(this)));
+        FROM_HERE, base::BindOnce(&FinalCleanupCallback, base::Unretained(this),
+                                  base::Unretained(&done)));
     // http://crbug.com/74623
     base::ScopedAllowBaseSyncPrimitivesOutsideBlockingScope allow_wait;
-    done_.Wait();
+    done.Wait();
   }
 }
 
@@ -349,7 +350,6 @@ void BackendImpl::CleanupCache() {
   FlushIndex();
   index_ = nullptr;
   ptr_factory_.InvalidateWeakPtrs();
-  done_.Signal();
 }
 
 // ------------------------------------------------------------------------
