bab194d787714f43dcb3087ff5ddad81d7d31587
Andrew Grieve <agrieve@chromium.org>
2020-04-03 02:23:38 +0000

    Android: Fix SuperSize attribution of d8-desugar'ed lambdas
    This involved:
    1) Tweaking R8 to include the outer class name in the lambda name for
    method references. This increases dex size by ~2kb, but is needed for
    attribution (without this they all show in $APK).
    2) Updating supersize to unmangle lambda names.
    
    This patch also re-works how R8 local modifications are stored. It
    changes from a single .patch file to multiple format-patch files.
    This allows each patch to be applied separately (easier conflict
    resolution), and allows a description to live right allon each patch
    (via the commit messages)
    
    Bug: 1032609
    Change-Id: I34c356b542fd665e3b81f7a6d43e0e1dbbf38886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132668
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756136}

--- tools/binary_size/libsupersize/apkanalyzer.py
+++ tools/binary_size/libsupersize/apkanalyzer.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 Assumes that apk_path.mapping and apk_path.jar.info is available.
 """
 
+import collections
 import logging
 import os
 import subprocess
@@ -136,6 +137,13 @@ def UndoHierarchicalSizing(data):
   return nodes
 
 
+def _TruncateFrom(value, delimiter, rfind=False):
+  idx = value.rfind(delimiter) if rfind else value.find(delimiter)
+  if idx != -1:
+    return value[:idx]
+  return value
+
+
 def CreateDexSymbols(apk_path, mapping_path, size_info_prefix):
   source_map = _ParseJarInfoFile(size_info_prefix + '.jar.info')
 
@@ -155,10 +163,33 @@ def CreateDexSymbols(apk_path, mapping_path, size_info_prefix):
   # We have more than 100KB of ids for methods, strings
   id_metadata_overhead_size = dex_expected_size - total_node_size
   symbols = []
+  lambda_by_class_counter = collections.defaultdict(int)
+  lambda_name_to_nested_number = {}
   for _, name, node_size in nodes:
-    package = name.split(' ', 1)[0]
-    class_path = package.split('$')[0]
-    source_path = source_map.get(class_path, '')
+    package = _TruncateFrom(name, ' ')
+    # Make d8 desugared lambdas look the same as Desugar ones.
+    # D8 lambda: -$$Lambda$Promise$Nested1$kjevdDQ8V2zqCrdieLqWLHzk.dex
+    # Desugar lambda: Promise$Nested1$$Lambda$0
+    # 1) Need to prefix with proper class name so that they will show as nested.
+    # 2) Need to suffix with number so that they diff better.
+    # Original name will be kept as "object_path".
+    is_d8_lambda = '-$$Lambda$' in package
+    class_path = package
+    if is_d8_lambda:
+      class_path = package.replace('-$$Lambda$', '')
+      base_name = _TruncateFrom(class_path, '$', rfind=True)
+      # Map all methods of the lambda class to the same nested number.
+      lambda_number = lambda_name_to_nested_number.get(class_path)
+      if lambda_number is None:
+        # First time we've seen this lambda, increment nested class count.
+        lambda_number = lambda_by_class_counter[base_name]
+        lambda_name_to_nested_number[class_path] = lambda_number
+        lambda_by_class_counter[base_name] = lambda_number + 1
+
+      name = '{}$$Lambda${}{}'.format(base_name, lambda_number,
+                                      name[len(package):])
+    # Map all nested classes to outer class.
+    source_path = source_map.get(_TruncateFrom(class_path, '$'), '')
     if source_path:
       object_path = package
     elif package == _TOTAL_NODE_NAME:
