7ec2d8ea840132caaa5204cdfd38391a869f1980
Wanming Lin <wanming.lin@intel.com>
2020-04-03 02:14:40 +0000

    [shapedetection] Upstream TextDetection tests to WPT
    This CL upstreams the reset of shapedetection tests(mainly
    TextDetection tests) to WPT.
    
    - Moves tests under
      third_party/blink/web_tests/shapedetection/,
      third_party/blink/web_tests/fast/shapedetection/,
      third_party/blink/web_tests/http/tests/shapedetection/,
      to third_party/blink/web_tests/external/wpt/shape-detection.
    - Moves third_party/blink/web_tests/shapedetection/resources/mock-textdetection.js
      to third_party/blink/web_tests/external/wpt/resources/chromium/.
    - Removes third_party/blink/web_tests/fast/shapedetection/shapedetection-creation.html
      as which should be covered in idlharness tests.
    - Introduces simulateClosedPipe() to MockBarcodeDetectionProvider to simulate
      'no implementation available' case for
      third_party/blink/web_tests/external/wpt/shape-detection/detection-getSupportedFormats.https.html
    - Improves third_party/blink/web_tests/external/wpt/shape-detection/detection-options.https.html
      by using assert_throws_js BTW.
    
    Bug: 932382
    Change-Id: I2f795e4fa4f23b33c49b7924cdbcbccde5ac6ed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131154
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Wanming Lin <wanming.lin@intel.com>
    Cr-Commit-Position: refs/heads/master@{#756131}

--- third_party/blink/web_tests/external/wpt/shape-detection/resources/shapedetection-helpers.js
+++ third_party/blink/web_tests/external/wpt/shape-detection/resources/shapedetection-helpers.js
@@ -29,8 +29,10 @@ let loadChromiumResources = Promise.resolve().then(() => {
     `${prefix}/barcodedetection_provider.mojom.js`,
     `${prefix}/facedetection.mojom.js`,
     `${prefix}/facedetection_provider.mojom.js`,
+    `${prefix}/textdetection.mojom.js`,
     '/resources/chromium/mock-barcodedetection.js',
     '/resources/chromium/mock-facedetection.js',
+    '/resources/chromium/mock-textdetection.js',
   ].forEach(path => {
     // Use importScripts for workers.
     if (typeof document === 'undefined') {
@@ -52,7 +54,8 @@ let loadChromiumResources = Promise.resolve().then(() => {
 /**
  * @param {String} detectionTestName
  * name of mock shape detection test interface,
- * must be the item of ["FaceDetectionTest", "BarcodeDetectionTest"]
+ * must be the item of ["FaceDetectionTest", "BarcodeDetectionTest",
+ * "TextDetectionTest"]
 */
 async function initialize_detection_tests(detectionTestName) {
   let detectionTest;
