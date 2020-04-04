48f889ca688fc5a903c58cc96fbc13e7675b0fa8
Alex Turner <alexmt@chromium.org>
2020-04-04 00:19:07 +0000

    Use RFH::AddMessageToConsole in ContentSubresourceFilterThrottleManager
    Currently, in ContentSubresourceFilterThrottleManager::
    DidFinishNavigation, there is a call to NavigationConsoleLogger::
    LogMessageOnCommit. This always results in a message being immediately
    added to console, as it is only called for committed navigations.
    
    For simplicity, and as the other functionality of
    NavigationConsoleLogger is unnecessary for this purpose, we change this
    call to a direct call to RenderFrameHost::AddMessageToConsole. This will
    also aid in minor refactoring planned for crbug.com/1055558, which will
    factor out this code path into a function and pass it as a callback for
    a posted task in certain cases. This change avoids the need to pass a
    pointer to a navigation_handle, which may be in the process of being
    deleted, to any posted task.
    
    Bug: 1067010
    Change-Id: I7a3f3a8fed2bfbac23955afe5f45f5a43c22c46f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132779
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756480}

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/traced_value.h"
 #include "components/subresource_filter/content/browser/activation_state_computing_navigation_throttle.h"
 #include "components/subresource_filter/content/browser/async_document_subresource_filter.h"
-#include "components/subresource_filter/content/browser/navigation_console_logger.h"
 #include "components/subresource_filter/content/browser/page_load_statistics.h"
 #include "components/subresource_filter/content/browser/subresource_filter_client.h"
 #include "components/subresource_filter/content/common/subresource_filter_messages.h"
@@ -211,8 +210,8 @@ void ContentSubresourceFilterThrottleManager::DidFinishNavigation(
       if (filter->activation_state().enable_logging) {
         DCHECK(filter->activation_state().activation_level !=
                mojom::ActivationLevel::kDisabled);
-        NavigationConsoleLogger::LogMessageOnCommit(
-            navigation_handle, blink::mojom::ConsoleMessageLevel::kWarning,
+        frame_host->AddMessageToConsole(
+            blink::mojom::ConsoleMessageLevel::kWarning,
             kActivationConsoleMessage);
       }
     }
