8fddf037e5753a59e1896dc1aaa42ea579755b00
Esmael El-Moslimany <aee@chromium.org>
2020-04-04 01:40:08 +0000

    WebUI NTP: daily background refresh for a collection
    - Add backend support for selecting an image, selecting a collection
      for daily refresh and selecting no background
    - Implement the logic to maintain a consistent state for applying
      background selection as well as canceling.
    
    Follow-up CLs will include selecting an image, selecting no background
    and previewing the unapplied selection.
    
    Bug: 1032328, 990963
    Change-Id: I4c4d1a47d67f2fb6cae5735bc7848f61b1c192ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2119206
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756497}

--- chrome/test/data/webui/new_tab_page/app_test.js
+++ chrome/test/data/webui/new_tab_page/app_test.js
@@ -23,6 +23,7 @@ function createTheme() {
     backgroundImageAttribution1: '',
     backgroundImageAttribution2: '',
     backgroundImageAttributionUrl: null,
+    dailyRefreshCollectionId: '',
   };
 }
 
