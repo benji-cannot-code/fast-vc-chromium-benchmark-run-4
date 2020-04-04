7f1d5dfea852d5719e442aac2cf6a652b58fc7a7
Jian Li <jianli@chromium.org>
2020-04-04 00:41:40 +0000

    Implementation of ListContentManager for feed
    ListContentManagerImpl supports both native view and external view.
    
    The methods defined in feed/v2/FeedActionHandler has been merged to
    xsurface/FeedActionsHandler, except navigate* methods that are already
    included in xsurface/SurfaceActionHandler.
    
    Bug: 1044139
    Change-Id: Ic708a1295e2a0e6700f5b72195940a8bd365c76a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133110
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756484}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedStreamSurface.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedStreamSurface.java
@@ -12,6 +12,8 @@ import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.xsurface.FeedActionsHandler;
+import org.chromium.chrome.browser.xsurface.SurfaceActionsHandler;
 import org.chromium.components.feed.proto.FeedUiProto.StreamUpdate;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.ui.base.PageTransition;
@@ -25,7 +27,7 @@ import java.util.List;
  * Created once for each StreamSurfaceMediator corresponding to each NTP/start surface.
  */
 @JNINamespace("feed")
-public class FeedStreamSurface implements FeedActionHandler {
+public class FeedStreamSurface implements SurfaceActionsHandler, FeedActionsHandler {
     private final long mNativeFeedStreamSurface;
     private final ChromeActivity mActivity;
 
@@ -36,6 +38,11 @@ public class FeedStreamSurface implements FeedActionHandler {
     public FeedStreamSurface(ChromeActivity activity) {
         mNativeFeedStreamSurface = FeedStreamSurfaceJni.get().init(FeedStreamSurface.this);
         mActivity = activity;
+
+        FeedListContentManager manager = new FeedListContentManager(this, this);
+
+        // TODO(jianli): Get HybridListRender in order to bind FeedListContentManager to it.
+        // Then add the returned RecyclerView to NTP layout.
     }
 
     /**
@@ -51,7 +58,7 @@ public class FeedStreamSurface implements FeedActionHandler {
     }
 
     @Override
-    public void navigate(String url) {
+    public void navigateTab(String url) {
         LoadUrlParams loadUrlParams = new LoadUrlParams(url);
         loadUrlParams.setTransitionType(PageTransition.AUTO_BOOKMARK);
         mActivity.getActivityTabProvider().get().loadUrl(loadUrlParams);
@@ -60,7 +67,7 @@ public class FeedStreamSurface implements FeedActionHandler {
     }
 
     @Override
-    public void navigateInNewTab(String url) {
+    public void navigateNewTab(String url) {
         TabModelSelector tabModelSelector = mActivity.getTabModelSelector();
         Tab tab = mActivity.getActivityTabProvider().get();
         tabModelSelector.openNewTab(
@@ -72,6 +79,12 @@ public class FeedStreamSurface implements FeedActionHandler {
     @Override
     public void loadMore() {
         FeedStreamSurfaceJni.get().loadMore(mNativeFeedStreamSurface, FeedStreamSurface.this);
+    }
+
+    @Override
+    public void processThereAndBackAgainData(byte[] data) {
+        FeedStreamSurfaceJni.get().processThereAndBackAgain(
+                mNativeFeedStreamSurface, FeedStreamSurface.this, data);
     }
 
     @Override
@@ -93,14 +106,6 @@ public class FeedStreamSurface implements FeedActionHandler {
     public void discardDismissal(int changeId) {
         FeedStreamSurfaceJni.get().discardEphemeralChange(
                 mNativeFeedStreamSurface, FeedStreamSurface.this, changeId);
-    }
-
-    /**
-     * Handles uploading data for ThereAndBackAgain.
-     */
-    public void processThereAndBackAgain(byte[] data) {
-        FeedStreamSurfaceJni.get().processThereAndBackAgain(
-                mNativeFeedStreamSurface, FeedStreamSurface.this, data);
     }
 
     /**
