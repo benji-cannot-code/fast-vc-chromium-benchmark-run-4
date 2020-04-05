5af3a1a463734b88f4e0b7ac9c92d47e817acb3d
Koji Ishii <kojii@chromium.org>
2020-04-05 14:44:57 +0000

    [FragmentItem] Make NGFragmentItem RefCounted
    This patch makes |NGFragmentItem| RefCounted.
    
    Bug: 982194
    Change-Id: I04340c91b52ad4e274c70f3c99f8e4a3e32f4ad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135672
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756542}

--- third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_fragment_item.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_INLINE_NG_FRAGMENT_ITEM_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_INLINE_NG_FRAGMENT_ITEM_H_
 
+#include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/geometry/logical_offset.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_text_offset.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_ink_overflow.h"
 #include "third_party/blink/renderer/platform/graphics/paint/display_item_client.h"
+#include "third_party/blink/renderer/platform/wtf/ref_counted.h"
 
 namespace blink {
 
@@ -27,7 +29,8 @@ struct NGTextFragmentPaintInfo;
 //
 // This class consumes less memory than a full fragment, and can be stored in a
 // flat list (NGFragmentItems) for easier and faster traversal.
-class CORE_EXPORT NGFragmentItem : public DisplayItemClient {
+class CORE_EXPORT NGFragmentItem : public RefCounted<NGFragmentItem>,
+                                   public DisplayItemClient {
  public:
   // Represents regular text that exists in the DOM.
   struct TextItem {
