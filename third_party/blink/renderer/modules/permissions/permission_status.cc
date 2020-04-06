540cc35d6245f598fcc446e00f40254b1d4f8f99
Minoru Chikamune <chikamune@chromium.org>
2020-04-06 12:35:34 +0000

    Migrate PermissionStatus to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: Iecbd1024d347a21fe03a0a08926ee5e0eb1a145d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135553
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756634}

--- third_party/blink/renderer/modules/permissions/permission_status.cc
+++ third_party/blink/renderer/modules/permissions/permission_status.cc
@@ -40,7 +40,8 @@ PermissionStatus::PermissionStatus(ExecutionContext* execution_context,
                                    MojoPermissionDescriptor descriptor)
     : ExecutionContextLifecycleStateObserver(execution_context),
       status_(status),
-      descriptor_(std::move(descriptor)) {}
+      descriptor_(std::move(descriptor)),
+      receiver_(this, execution_context) {}
 
 PermissionStatus::~PermissionStatus() = default;
 
@@ -66,10 +67,6 @@ void PermissionStatus::ContextLifecycleStateChanged(
     StartListening();
   else
     StopListening();
-}
-
-void PermissionStatus::ContextDestroyed() {
-  StopListening();
 }
 
 String PermissionStatus::state() const {
@@ -103,6 +100,7 @@ void PermissionStatus::OnPermissionStatusChange(MojoPermissionStatus status) {
 }
 
 void PermissionStatus::Trace(Visitor* visitor) {
+  visitor->Trace(receiver_);
   EventTargetWithInlineData::Trace(visitor);
   ExecutionContextLifecycleStateObserver::Trace(visitor);
 }
--- third_party/blink/renderer/modules/permissions/permission_status.h
+++ third_party/blink/renderer/modules/permissions/permission_status.h
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_PERMISSIONS_PERMISSION_STATUS_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_PERMISSIONS_PERMISSION_STATUS_H_
 
-#include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/mojom/permissions/permission.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_state_observer.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -57,7 +57,7 @@ class PermissionStatus final : public EventTargetWithInlineData,
 
   // ExecutionContextLifecycleStateObserver implementation.
   void ContextLifecycleStateChanged(mojom::FrameLifecycleState) override;
-  void ContextDestroyed() override;
+  void ContextDestroyed() override {}
 
   String state() const;
 
@@ -73,7 +73,7 @@ class PermissionStatus final : public EventTargetWithInlineData,
 
   MojoPermissionStatus status_;
   MojoPermissionDescriptor descriptor_;
-  mojo::Receiver<mojom::blink::PermissionObserver> receiver_{this};
+  HeapMojoReceiver<mojom::blink::PermissionObserver> receiver_;
 };
 
 }  // namespace blink
