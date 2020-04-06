8bdc3971b9860e2e8d21be306b23c9a17bc04b4e
Ian Wells <iwells@chromium.org>
2020-04-06 23:03:34 +0000

    Add example query textproto and a script to generate a query request URL
    Bug: 1044139
    Change-Id: I6c394dbc3f3c70715134b83a33ecea9388b63c2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137488
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756853}

---
+++ components/feed/core/v2/tools/make_feed_query_request.sh
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Usage: make_feed_query_request.sh request.textproto
+
+CHROMIUM_SRC=$(realpath $(dirname $(readlink -f $0))/../../../../..)
+
+PLD=$(python3 \
+  $CHROMIUM_SRC/components/feed/core/v2/tools/textpb_to_binarypb.py \
+  --chromium_path=$CHROMIUM_SRC \
+  --message=feedwire.Request \
+  --output_format=base64 \
+  --source_file=$1)
+
+BASE_URL="https://www.google.com/httpservice"
+ENDPOINT="TrellisClankService/FeedQuery"
+QUERY_URL="$BASE_URL/retry/$ENDPOINT"
+
+echo "$QUERY_URL?fmt=bin&hl=en-US&reqpld=$PLD"
