0a5874dae48f013196e3ccb88d94b4760839f316
Chris Dalton <csmartdalton@google.com>
2020-04-06 18:02:10 +0000

    Check for ARB_base_instance when initializing GL bindings
    Change-Id: I94ed42c83097bf8c0242e6c05602a65b4c3061fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135425
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Shrek Shao <shrekshao@google.com>
    Commit-Queue: Chris Dalton <csmartdalton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756730}

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -541,7 +541,8 @@ GL_FUNCTIONS = [
 { 'return_type': 'void',
   'known_as': 'glDrawArraysInstancedBaseInstanceANGLE',
   #TODO(shrekshao): workaround when native support not available for cmd decoder
-  'versions' : [{ 'name': 'glDrawArraysInstancedBaseInstance' },
+  'versions' : [{ 'name': 'glDrawArraysInstancedBaseInstance',
+                 'extensions': ['GL_ARB_base_instance'] },
                 { 'name': 'glDrawArraysInstancedBaseInstanceEXT' },
                 { 'name': 'glDrawArraysInstancedBaseInstanceANGLE',
                  'extensions': ['GL_ANGLE_base_vertex_base_instance'] }],
@@ -570,7 +571,8 @@ GL_FUNCTIONS = [
 { 'return_type': 'void',
   'known_as': 'glDrawElementsInstancedBaseVertexBaseInstanceANGLE',
   #TODO(shrekshao): workaround when native support not available for cmd decoder
-  'versions' : [{ 'name': 'glDrawElementsInstancedBaseVertexBaseInstance' },
+  'versions' : [{ 'name': 'glDrawElementsInstancedBaseVertexBaseInstance',
+                 'extensions': ['GL_ARB_base_instance'] },
                 { 'name': 'glDrawElementsInstancedBaseVertexBaseInstanceEXT' },
                 { 'name': 'glDrawElementsInstancedBaseVertexBaseInstanceANGLE',
                  'extensions': ['GL_ANGLE_base_vertex_base_instance'] }],
--- ui/gl/gl_bindings_autogen_gl.cc
+++ ui/gl/gl_bindings_autogen_gl.cc
@@ -310,6 +310,8 @@ void DriverGL::InitializeDynamicBindings(const GLVersionInfo* ver,
       gfx::HasExtension(extensions, "GL_APPLE_vertex_array_object");
   ext.b_GL_ARB_ES2_compatibility =
       gfx::HasExtension(extensions, "GL_ARB_ES2_compatibility");
+  ext.b_GL_ARB_base_instance =
+      gfx::HasExtension(extensions, "GL_ARB_base_instance");
   ext.b_GL_ARB_blend_func_extended =
       gfx::HasExtension(extensions, "GL_ARB_blend_func_extended");
   ext.b_GL_ARB_clear_texture =
@@ -955,7 +957,7 @@ void DriverGL::InitializeDynamicBindings(const GLVersionInfo* ver,
             GetGLProcAddress("glDrawArraysInstancedANGLE"));
   }
 
-  if (ver->IsAtLeastGL(4u, 2u)) {
+  if (ver->IsAtLeastGL(4u, 2u) || ext.b_GL_ARB_base_instance) {
     fn.glDrawArraysInstancedBaseInstanceANGLEFn =
         reinterpret_cast<glDrawArraysInstancedBaseInstanceANGLEProc>(
             GetGLProcAddress("glDrawArraysInstancedBaseInstance"));
@@ -1004,7 +1006,7 @@ void DriverGL::InitializeDynamicBindings(const GLVersionInfo* ver,
             GetGLProcAddress("glDrawElementsInstancedANGLE"));
   }
 
-  if (ver->IsAtLeastGL(4u, 2u)) {
+  if (ver->IsAtLeastGL(4u, 2u) || ext.b_GL_ARB_base_instance) {
     fn.glDrawElementsInstancedBaseVertexBaseInstanceANGLEFn = reinterpret_cast<
         glDrawElementsInstancedBaseVertexBaseInstanceANGLEProc>(
         GetGLProcAddress("glDrawElementsInstancedBaseVertexBaseInstance"));
