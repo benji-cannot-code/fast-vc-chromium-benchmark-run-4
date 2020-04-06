721f52aca53f4e06ce22c548b17f5c462f8d03b3
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-04-06 13:32:10 +0000

    [iOS][Credential-Provider] Credential from Form
    Create credential provider extension objects from password forms.
    
    |recordIdentifier| is built in a similar way to how the logins DB
    identifies credentials. This way Chrome can remove and update entries in
    the credential provider store. This identifier is also used to identify
    indexed credentials in the system for password retrieval.
    
    The keychain identifier will come from the login database in a follow
    up CL. This is the attribute used to retrieve the unencrypted password
    from the keychain.
    
    Favicon and validationID are not related to the password store, and
    will also be implemented in a follow up CL.
    
    Bug: 1066803
    Change-Id: I20d573301cc468e9984f9524239fd47a7826aa85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133982
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756645}

---
+++ ios/chrome/browser/credential_provider/archivable_credential+password_form.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_CREDENTIAL_PROVIDER_ARCHIVABLE_CREDENTIAL_PASSWORD_FORM_H_
+#define IOS_CHROME_BROWSER_CREDENTIAL_PROVIDER_ARCHIVABLE_CREDENTIAL_PASSWORD_FORM_H_
+
+#import "ios/chrome/common/credential_provider/archivable_credential.h"
+
+namespace autofill {
+struct PasswordForm;
+}
+
+// Category for adding convenience logic related to PasswordForms.
+@interface ArchivableCredential (PasswordForm)
+
+// Convenience initializer from a PasswordForm. Will return nil for forms
+// with an empty origin or Android forms.
+- (instancetype)initWithPasswordForm:(const autofill::PasswordForm&)passwordForm
+                             favicon:(NSString*)favicon
+                  keychainIdentifier:(NSString*)keychainIdentifier
+                validationIdentifier:(NSString*)validationIdentifier;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_CREDENTIAL_PROVIDER_ARCHIVABLE_CREDENTIAL_PASSWORD_FORM_H_
