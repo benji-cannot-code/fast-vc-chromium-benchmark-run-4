c97b3502f27eac8d7fd327110e91539f0a4e4df1
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-04-06 03:26:44 +0000

    [CrOS settings] Updated OS route path (PEOPLE) string
    Before this CL some os and browser routes had the same name. This was
    a bit confusing. If a some c++ code called '/people', it is not clear if
    this url is a chrome or browser subpage. A distinction needed to be made.
    This CL renames OS specific route PEOPLE, which is shared with browser
    routes to a more distinguishable name OS_PEOPLE. Allowing this
    bug(990169) to be unblocked.
    
    Bug: 990169
    Change-Id: If1528c70ed31429711044985e9589a6025568dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2117936
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756571}

--- chrome/browser/resources/settings/chromeos/os_settings_routes.js
+++ chrome/browser/resources/settings/chromeos/os_settings_routes.js
@@ -59,7 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *   OS_RESET: !settings.Route,
  *   OS_SEARCH: !settings.Route,
  *   OS_SYNC: !settings.Route,
- *   PEOPLE: !settings.Route,
+ *   OS_PEOPLE: !settings.Route,
  *   PERSONALIZATION: !settings.Route,
  *   PLUGIN_VM: !settings.Route,
  *   PLUGIN_VM_DETAILS: !settings.Route,
--- chrome/common/webui_url_constants.cc
+++ chrome/common/webui_url_constants.cc
@@ -430,6 +430,7 @@ const char kNativePrintingSettingsSubPage[] = "cupsPrinters";
 const char kNetworkDetailSubPage[] = "networkDetail";
 const char kNetworksSubPage[] = "networks";
 const char kOsAccessibilitySubPage[] = "osAccessibility";
+const char kOsPeopleSubPage[] = "osPeople";
 const char kOsPrintingSubPage[] = "osPrinting";
 const char kOsPrivacySubPage[] = "osPrivacy";
 const char kOsResetSubPage[] = "osReset";
@@ -491,6 +492,7 @@ bool IsOSSettingsSubPage(const std::string& sub_page) {
       kOsLanguagesSubPage,
       kOsLanguagesDetailsSubPage,
       kOsLanguagesInputMethodsSubPage,
+      kOsPeopleSubPage,
       kOsPrintingSubPage,
       kOsPrivacySubPage,
       kOsResetSubPage,
