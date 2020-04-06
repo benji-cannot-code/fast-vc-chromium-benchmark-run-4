bfaf613e271d4aef26a615528fe6b790f6071e7a
Xianzhu Wang <wangxianzhu@chromium.org>
2020-04-06 19:40:46 +0000

    Revert "[CompositeAfterPaint] Allow non-atomic effects in multicol"
    This reverts commit 117ea7fd679d8898882f24f685b1cd03983617e9.
    
    Reason for revert: crbug.com/1067845
    
    Original change's description:
    > [CompositeAfterPaint] Allow non-atomic effects in multicol
    > 
    > This is not fully correct, but we have to accept it in our current
    > multicol painting based on FragmentClips.
    > 
    > Actually we have already allowed non-atomic effects in multicol within
    > composited layers since SlimmingPaintV175. A fragmented element with
    > effect applies the effect on each fragment separately. We have to do
    > this because each fragment has its own fragment clip, and its own paint
    > properties because they may depend on the fragment clip.
    > 
    > This CL enables non-atomic composited effects for CompositeAfterPaint.
    > This is not an issue in pre-CompositeAfterPaint which doesn't allow
    > fragmentation of composited layers (which is incorrect).
    > 
    > The root issue will be fully fixed by LayoutNG block fragmentation
    > which doesn't need FragmentClips.
    > 
    > Bug: 1064341
    > Change-Id: Id6ae4c263381f24d037f3c2bc493d73cd050598e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124340
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#754166}
    
    TBR=wangxianzhu@chromium.org,pdr@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1064341,1067845
    Change-Id: I14da5cd66b644c39404446be61fc16a02dabe6f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135834
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756775}

--- third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder.cc
@@ -261,12 +261,6 @@ class FragmentPaintPropertyTreeBuilder {
     full_context_.clip_changed |= cleared;
   }
 
-  CompositorElementId GetCompositorElementId(
-      CompositorElementIdNamespace namespace_id) const {
-    return CompositorElementIdFromUniqueObjectId(fragment_data_.UniqueId(),
-                                                 namespace_id);
-  }
-
   const LayoutObject& object_;
   NGPrePaintInfo* pre_paint_info_;
   // The tree builder context for the whole object.
@@ -541,7 +535,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateStickyTranslation() {
       const auto& box_model = ToLayoutBoxModelObject(object_);
       TransformPaintPropertyNode::State state{
           FloatSize(box_model.StickyPositionOffset())};
-      state.compositor_element_id = GetCompositorElementId(
+      state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+          box_model.UniqueId(),
           CompositorElementIdNamespace::kStickyTranslation);
 
       auto* layer = box_model.Layer();
@@ -807,8 +802,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateTransform() {
           object_.HasHiddenBackface()
               ? TransformPaintPropertyNode::BackfaceVisibility::kHidden
               : TransformPaintPropertyNode::BackfaceVisibility::kVisible;
-      state.compositor_element_id = GetCompositorElementId(
-          CompositorElementIdNamespace::kPrimaryTransform);
+      state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+          object_.UniqueId(), CompositorElementIdNamespace::kPrimaryTransform);
 
       TransformPaintPropertyNode::AnimationState animation_state;
       animation_state.is_running_animation_on_compositor =
@@ -1051,8 +1046,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
 
       CompositorElementId mask_compositor_element_id;
       if (mask_clip || has_spv1_composited_clip_path) {
-        mask_compositor_element_id =
-            GetCompositorElementId(CompositorElementIdNamespace::kEffectMask);
+        mask_compositor_element_id = CompositorElementIdFromUniqueObjectId(
+            object_.UniqueId(), CompositorElementIdNamespace::kEffectMask);
       }
 
       EffectPaintPropertyNode::State state;
@@ -1089,15 +1084,15 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
           full_context_.direct_compositing_reasons &
           CompositingReasonsForEffectProperty();
       if (state.direct_compositing_reasons) {
-        state.compositor_element_id = GetCompositorElementId(
-            CompositorElementIdNamespace::kPrimaryEffect);
+        state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+            object_.UniqueId(), CompositorElementIdNamespace::kPrimaryEffect);
       } else {
         // The effect node CompositorElementId is used to uniquely identify
         // renderpasses so even if we don't need one for animations we still
         // need to set an id. Using kPrimary avoids confusing cc::Animation
         // into thinking the element has been composited for animations.
-        state.compositor_element_id =
-            GetCompositorElementId(CompositorElementIdNamespace::kPrimary);
+        state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+            object_.UniqueId(), CompositorElementIdNamespace::kPrimary);
       }
 
       // TODO(crbug.com/900241): Remove these setters when we can use
@@ -1154,8 +1149,10 @@ void FragmentPaintPropertyTreeBuilder::UpdateEffect() {
         clip_path_state.local_transform_space = context_.current.transform;
         clip_path_state.output_clip = output_clip;
         clip_path_state.blend_mode = SkBlendMode::kDstIn;
-        clip_path_state.compositor_element_id = GetCompositorElementId(
-            CompositorElementIdNamespace::kEffectClipPath);
+        clip_path_state.compositor_element_id =
+            CompositorElementIdFromUniqueObjectId(
+                object_.UniqueId(),
+                CompositorElementIdNamespace::kEffectClipPath);
         OnUpdate(
             properties_->UpdateClipPath(parent, std::move(clip_path_state)));
       } else {
@@ -1269,8 +1266,8 @@ void FragmentPaintPropertyTreeBuilder::UpdateFilter() {
       state.direct_compositing_reasons =
           full_context_.direct_compositing_reasons &
           CompositingReasonsForFilterProperty();
-      state.compositor_element_id =
-          GetCompositorElementId(CompositorElementIdNamespace::kEffectFilter);
+      state.compositor_element_id = CompositorElementIdFromUniqueObjectId(
+          object_.UniqueId(), CompositorElementIdNamespace::kEffectFilter);
 
       // TODO(crbug.com/900241): Remove the setter when we can use
       // state.direct_compositing_reasons to check for active animations.
--- third_party/blink/renderer/core/paint/paint_property_tree_builder_test.cc
+++ third_party/blink/renderer/core/paint/paint_property_tree_builder_test.cc
@@ -4351,6 +4351,10 @@ TEST_P(PaintPropertyTreeBuilderTest, OverflowClipUnderMultiColumn) {
 }
 
 TEST_P(PaintPropertyTreeBuilderTest, CompositedUnderMultiColumn) {
+  // TODO(crbug.com/1064341): This test crashes in CompositeAfterPaint. Fix it.
+  if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled())
+    return;
+
   SetBodyInnerHTML(R"HTML(
     <style>body { margin: 0; }</style>
     <div id='multicol' style='columns:3; column-fill:auto; column-gap: 0;
@@ -4499,6 +4503,10 @@ TEST_P(PaintPropertyTreeBuilderTest, FrameUnderMulticol) {
 }
 
 TEST_P(PaintPropertyTreeBuilderTest, CompositedMulticolFrameUnderMulticol) {
+  // TODO(crbug.com/1064341): This test crashes in CompositeAfterPaint. Fix it.
+  if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled())
+    return;
+
   SetBodyInnerHTML(R"HTML(
     <style>body { margin: 0 }</style>
     <div style='columns: 3; column-gap: 0; column-fill: auto;
@@ -5647,6 +5655,10 @@ TEST_P(PaintPropertyTreeBuilderTest, CompositedLayerSkipsFragmentClip) {
 }
 
 TEST_P(PaintPropertyTreeBuilderTest, CompositedLayerUnderClipUnderMulticol) {
+  // TODO(crbug.com/1064341): This test crashes in CompositeAfterPaint. Fix it.
+  if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled())
+    return;
+
   SetBodyInnerHTML(R"HTML(
     <div id="multicol" style="columns: 2">
       <div id="clip" style="height: 100px; overflow: hidden">
