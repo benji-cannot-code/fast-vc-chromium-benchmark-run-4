62773671aa18c39c7f4fa1d8dd5a5e710f6a8222
Alice Wang <aliceywang@chromium.org>
2020-04-06 16:49:41 +0000

    [Signin][Android] Remove provider in AccountManagerFacade test
    This CL removes AccountManagerFacadeProvider usage in
    AccountManagerFacadeRobolectricTest.
    
    Bug: 1067633
    Change-Id: I2eb5dede88720632817ea05f95fc109a552441b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137341
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756700}

--- components/signin/core/browser/android/junit/src/org/chromium/components/signin/test/AccountManagerFacadeRobolectricTest.java
+++ components/signin/core/browser/android/junit/src/org/chromium/components/signin/test/AccountManagerFacadeRobolectricTest.java
@@ -29,7 +29,6 @@ import org.chromium.base.task.test.CustomShadowAsyncTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.components.signin.AccountManagerDelegateException;
 import org.chromium.components.signin.AccountManagerFacade;
-import org.chromium.components.signin.AccountManagerFacadeProvider;
 import org.chromium.components.signin.AccountUtils;
 import org.chromium.components.signin.ChildAccountStatus;
 import org.chromium.components.signin.ProfileDataSource;
@@ -64,10 +63,8 @@ public class AccountManagerFacadeRobolectricTest {
         mDelegate = new FakeAccountManagerDelegate(
                 FakeAccountManagerDelegate.ENABLE_PROFILE_DATA_SOURCE);
         Assert.assertFalse(mDelegate.isRegisterObserversCalled());
-        // TODO(https://crbug.com/1067633): Remove AccountManagerFacadeProvider in this test
-        AccountManagerFacadeProvider.setInstanceForTests(new AccountManagerFacade(mDelegate));
+        mFacade = new AccountManagerFacade(mDelegate);
         Assert.assertTrue(mDelegate.isRegisterObserversCalled());
-        mFacade = AccountManagerFacadeProvider.getInstance();
     }
 
     private void setAccountRestrictionPatterns(String... patterns) {
@@ -250,7 +247,7 @@ public class AccountManagerFacadeRobolectricTest {
                                        .featureSet(new HashSet<>(Arrays.asList(features)))
                                        .build();
         mDelegate.addAccountHolderExplicitly(holder);
-        Assert.assertFalse(AccountManagerFacadeProvider.getInstance().isUpdatePending().get());
+        Assert.assertFalse(mFacade.isUpdatePending().get());
         return account;
     }
 
@@ -261,7 +258,7 @@ public class AccountManagerFacadeRobolectricTest {
     private void assertChildAccountStatus(
             Account account, @ChildAccountStatus.Status Integer status) {
         final AtomicInteger callCount = new AtomicInteger();
-        AccountManagerFacadeProvider.getInstance().checkChildAccountStatus(account, result -> {
+        mFacade.checkChildAccountStatus(account, result -> {
             callCount.incrementAndGet();
             Assert.assertEquals(result, status);
         });
