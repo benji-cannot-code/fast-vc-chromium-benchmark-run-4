e7be81accaba426dc21ecf539790a5ec0ee585db
Bo Liu <boliu@chromium.org>
2020-04-06 23:32:27 +0000

    weblayer: Remove profile data before deletion
    Commit writes and remove data before deleting the entire profile
    directory. There are already no WebContents associated with the profile.
    This is all to avoid racing with code that tries to write the profile
    during deletion.
    
    Bug: 1065585
    Change-Id: Ic77fde85bccb7c0f79824bfc491ea6994dffdcab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135053
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756869}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ProfileTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/ProfileTest.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.Criteria;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -121,7 +120,6 @@ public class ProfileTest {
 
     @Test
     @SmallTest
-    @DisabledTest
     public void testEnumerateAllProfileNames() throws Exception {
         final String profileName = "TestEnumerateAllProfileNames";
         final WebLayer weblayer = mActivityTestRule.getWebLayer();
