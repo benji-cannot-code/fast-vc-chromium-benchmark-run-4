8c44a737e0738ef6fa83fa9ae3cd428d69b46901
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-04-06 12:15:38 +0000

    [iOS][Credential-Provider] Add default store path.
    Bug: 1045457
    Change-Id: I950bf2a89018d22a5c3ffa1e6d2f08e800d72b20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137320
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756618}

---
+++ ios/chrome/common/credential_provider/constants.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_COMMON_CREDENTIAL_PROVIDER_CONSTANTS_H_
+#define IOS_CHROME_COMMON_CREDENTIAL_PROVIDER_CONSTANTS_H_
+
+#import <Foundation/Foundation.h>
+
+// Path to the persisted file for the credential provider archivable store.
+NSURL* CredentialProviderSharedArchivableStoreURL();
+
+#endif  // IOS_CHROME_COMMON_CREDENTIAL_PROVIDER_CONSTANTS_H_
