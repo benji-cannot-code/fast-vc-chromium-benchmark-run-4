2cc6e26c0c14247a3151bfb6e18b7992828e9f52
Zinovy Nis <zynis@yandex-team.ru>
2020-04-06 07:42:46 +0000

    [clang-tidy] Fix use-after-move in devtools/InputHandler
    Bug: 995566
    Change-Id: Ia71ae2c5b7f423e685d4a88fabc3bc21c22e307a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135694
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Zinovy Nis <zynis@yandex-team.ru>
    Cr-Commit-Position: refs/heads/master@{#756591}

--- content/browser/devtools/protocol/input_handler.cc
+++ content/browser/devtools/protocol/input_handler.cc
@@ -616,9 +616,6 @@ void InputHandler::DispatchKeyEvent(
 void InputHandler::InsertText(const std::string& text,
                               std::unique_ptr<InsertTextCallback> callback) {
   base::string16 text16 = base::UTF8ToUTF16(text);
-  base::OnceClosure closure =
-      base::BindOnce(&InsertTextCallback::sendSuccess, std::move(callback));
-
   if (!host_ || !host_->GetRenderWidgetHost()) {
     callback->sendFailure(Response::InternalError());
     return;
@@ -631,6 +628,9 @@ void InputHandler::InsertText(const std::string& text,
     if (target_host)
       widget_host = target_host;
   }
+
+  base::OnceClosure closure =
+      base::BindOnce(&InsertTextCallback::sendSuccess, std::move(callback));
 
   widget_host->Focus();
   widget_host->GetWidgetInputHandler()->ImeCommitText(
