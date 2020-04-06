c5cbc2eca106af3c5cdd593dba2c8694804fefc1
Friedrich Horschig <fhorschig@chromium.org>
2020-04-06 13:27:40 +0000

    [Passwords] Util to show re-signin for logged-out account-store users
    Adds a util that indicates when to show a re-signin promo for previous
    account-store users. It's true if:
    - no user is logged in
    - a password field is focused
    - any user on this device opted into account-stored passwords before
    
    Showing the actual promo button happens in https://crrev.com/c/2134014
    
    Bug: 1060130
    Change-Id: Ice3ae2b5c8a9995a9433a495404cdbf7f65a1172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133981
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756643}

--- components/password_manager/core/browser/password_feature_manager_impl.h
+++ components/password_manager/core/browser/password_feature_manager_impl.h
@@ -29,6 +29,7 @@ class PasswordFeatureManagerImpl : public PasswordFeatureManager {
 
   bool IsOptedInForAccountStorage() const override;
   bool ShouldShowAccountStorageOptIn() const override;
+  bool ShouldShowAccountStorageReSignin() const override;
   void SetAccountStorageOptIn(bool opt_in) override;
 
   bool ShouldShowPasswordStorePicker() const override;
