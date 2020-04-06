ec5c42f31d64f26d8e6a73dcfa9c56b423c9d620
Tommy Li <tommycli@chromium.org>
2020-04-06 23:17:39 +0000

    [omnibox] Implement a rudimentary views::Label for header text
    This CL uses a rudimentary views::Label to display header text for
    AutocompleteResults that contain matches with headers.
    
    Screenshot: https://i.imgur.com/f61GxWR.png
    
    Bug: 1052522
    Change-Id: I042c21a60a8416e243498793cee5c3dabeaac4f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137673
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756863}

--- chrome/browser/ui/views/omnibox/omnibox_row_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_row_view.cc
@@ -6,7 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/omnibox/omnibox_row_view.h"
 
 #include "chrome/browser/ui/views/omnibox/omnibox_result_view.h"
+#include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
+
+class OmniboxRowView::HeaderView : public views::Label {
+ public:
+  HeaderView() {
+    SetLayoutManager(std::make_unique<views::BoxLayout>(
+        views::BoxLayout::Orientation::kHorizontal));
+
+    header_text_ = AddChildView(std::make_unique<views::Label>());
+  }
+
+  void SetHeaderText(const base::string16& header_text) {
+    header_text_->SetText(header_text);
+  }
+
+ private:
+  // The Label containing the header text. This is never nullptr.
+  views::Label* header_text_;
+};
 
 OmniboxRowView::OmniboxRowView(std::unique_ptr<OmniboxResultView> result_view) {
   DCHECK(result_view);
@@ -17,7 +36,17 @@ OmniboxRowView::OmniboxRowView(std::unique_ptr<OmniboxResultView> result_view) {
   result_view_ = AddChildView(std::move(result_view));
 }
 
-void OmniboxRowView::SetHeader(base::Optional<int> suggestion_group_id,
-                               const base::string16& header_text) {
-  // TODO(tommycli): Create a child header view here. Not implemented yet.
+void OmniboxRowView::ShowHeader(base::Optional<int> suggestion_group_id,
+                                const base::string16& header_text) {
+  // Create the header (at index 0) if it doesn't exist.
+  if (header_view_ == nullptr)
+    header_view_ = AddChildViewAt(std::make_unique<HeaderView>(), 0);
+
+  header_view_->SetHeaderText(header_text);
+  header_view_->SetVisible(true);
+}
+
+void OmniboxRowView::HideHeader() {
+  if (header_view_)
+    header_view_->SetVisible(false);
 }
