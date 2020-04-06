a0f6abf8eb0c78f804db4f9001fcd6213fd0f33b
Maks Orlovich <morlovich@chromium.org>
2020-04-06 21:34:33 +0000

    Move Accept-CH parsing code into services/network/public/cpp
    We need to handle it in the browser for navigation redirects;
    which by upcoming security rules means that the network service
    should parse it, which requires the code to be in services/network
    
    The renderer also needs to parse it, so it needs to be in
    services/network/public/cpp.
    
    Also ParseAceptCH got split into ParseAcceptCH and FilterAcceptCH along the way,
    since it feels like network service shouldn't know about browser
    experiments on what hints to support.
    
    Bug: 1050726
    Change-Id: I4b10ce6ddf97efd852a44d4c8a4f85a9a6a0c404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124734
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756826}

--- third_party/blink/renderer/platform/loader/fetch/client_hints_preferences.cc
+++ third_party/blink/renderer/platform/loader/fetch/client_hints_preferences.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/loader/fetch/client_hints_preferences.h"
 
 #include "base/macros.h"
+#include "services/network/public/cpp/client_hints.h"
 #include "third_party/blink/public/common/client_hints/client_hints.h"
 #include "third_party/blink/renderer/platform/network/http_names.h"
 #include "third_party/blink/renderer/platform/network/http_parsers.h"
@@ -52,9 +53,9 @@ void ClientHintsPreferences::UpdateFromAcceptClientHintsHeader(
 
   // Note: .Ascii() would convert tab to ?, which is undesirable.
   base::Optional<std::vector<network::mojom::WebClientHintsType>> parsed_ch =
-      ParseAcceptCH(header_value.Latin1(),
-                    RuntimeEnabledFeatures::LangClientHintHeaderEnabled(),
-                    RuntimeEnabledFeatures::UserAgentClientHintEnabled());
+      FilterAcceptCH(network::ParseAcceptCH(header_value.Latin1()),
+                     RuntimeEnabledFeatures::LangClientHintHeaderEnabled(),
+                     RuntimeEnabledFeatures::UserAgentClientHintEnabled());
   if (!parsed_ch.has_value())
     return;
 
