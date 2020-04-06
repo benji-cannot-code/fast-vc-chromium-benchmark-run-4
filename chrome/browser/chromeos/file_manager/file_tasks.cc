b53e7878629f8f9ea8d3e5452f0e16d170fbcefb
Alice Boxhall <aboxhall@chromium.org>
2020-04-06 01:12:50 +0000

    Revert "Reland "Remove launch service.""
    This reverts commit a7c7613c007c969890d6a40182d3b7a62a70de38.
    
    Reason for revert: Sorry, this seems to be causing test failures: https://ci.chromium.org/p/chromium/builders/ci/chromeos-kevin-rel/24801
    
    Original change's description:
    > Reland "Remove launch service."
    > 
    > This is a reland of d07c4196274ab9c5b7819cea382cdaafe99e3dcc
    > 
    > TBR=ericwilligers@chromium.org
    > TBR=dominickn@chromium.org
    > 
    > Original change's description:
    > > Remove launch service.
    > >
    > > BUG=1061843
    > >
    > > Change-Id: I12efd2899403cb9cc5e9dafc04300a7156342ed9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116156
    > > Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    > > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    > > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#756170}
    > 
    > Bug: 1061843
    > Change-Id: I020be92154b0f5fad0d45f1f5c83f2d8539684d0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134036
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    > Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    > Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#756556}
    
    TBR=xiyuan@chromium.org,ericwilligers@chromium.org,dominickn@chromium.org,nancylingwang@chromium.org
    
    Change-Id: I5d1c9b2e82f3bbca4d482ab0909639ca553c81a2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1061843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137099
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756559}

--- chrome/browser/chromeos/file_manager/file_tasks.cc
+++ chrome/browser/chromeos/file_manager/file_tasks.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/apps/app_service/app_service_metrics.h"
+#include "chrome/browser/apps/launch_service/launch_service.h"
 #include "chrome/browser/chromeos/crostini/crostini_features.h"
 #include "chrome/browser/chromeos/drive/file_system_util.h"
 #include "chrome/browser/chromeos/extensions/default_web_app_ids.h"
