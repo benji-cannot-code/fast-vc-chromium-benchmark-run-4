4bd52bb3b4ad15d42b581bcf6afd0cbf76967cdd
Maksim Sisov <msisov@igalia.com>
2020-04-06 16:57:15 +0000

    gl: x11: Clean up USE_X11 usage in ui/gl.
    This CL removes some guards, which are not needed, and adds
    some comments for future work.
    
    R=backer@chromium.org
    
    Bug: 1036285
    Change-Id: I37810d5b4abe0675daf121b00cb6b59b4b48db5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116196
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756704}

--- ui/gl/gl_bindings.cc
+++ ui/gl/gl_bindings.cc
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 
-#if defined(USE_X11)
-#include "ui/gfx/x/x11.h"
-#endif  // USE_X11
-
 #if defined(USE_EGL)
 #include <EGL/egl.h>
 #endif
