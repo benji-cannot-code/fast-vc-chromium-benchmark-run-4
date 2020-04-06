6712eac4d18abd0f11536a94eed45382ca9266a9
Johann <johannkoenig@google.com>
2020-04-06 23:41:52 +0000

    presentation controller: remove execution context
    The PresentationController class does not need
    ExecutionContextLifecycleObserver since the mojo::Receiver was migrated
    to HeapMojoReceiver. The replacement wrapper includes management of the
    presentation_controller_receiver_.
    
    Change-Id: I1c96f897a7b604a23d3e7f0bd61a3b0598faa2f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137162
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756871}

--- third_party/blink/renderer/modules/presentation/presentation_controller.cc
+++ third_party/blink/renderer/modules/presentation/presentation_controller.cc
@@ -23,7 +23,6 @@ namespace blink {
 
 PresentationController::PresentationController(LocalFrame& frame)
     : Supplement<LocalFrame>(frame),
-      ExecutionContextLifecycleObserver(frame.GetDocument()),
       presentation_controller_receiver_(this, frame.DomWindow()) {}
 
 PresentationController::~PresentationController() = default;
@@ -61,7 +60,6 @@ void PresentationController::Trace(Visitor* visitor) {
   visitor->Trace(connections_);
   visitor->Trace(availability_state_);
   Supplement<LocalFrame>::Trace(visitor);
-  ExecutionContextLifecycleObserver::Trace(visitor);
 }
 
 void PresentationController::SetPresentation(Presentation* presentation) {
@@ -154,10 +152,10 @@ PresentationController::FindExistingConnection(
 
 mojo::Remote<mojom::blink::PresentationService>&
 PresentationController::GetPresentationService() {
-  if (!presentation_service_remote_ && GetFrame()) {
+  if (!presentation_service_remote_ && GetSupplementable()) {
     scoped_refptr<base::SingleThreadTaskRunner> task_runner =
-        GetFrame()->GetTaskRunner(TaskType::kPresentation);
-    GetFrame()->GetBrowserInterfaceBroker().GetInterface(
+        GetSupplementable()->GetTaskRunner(TaskType::kPresentation);
+    GetSupplementable()->GetBrowserInterfaceBroker().GetInterface(
         presentation_service_remote_.BindNewPipeAndPassReceiver(task_runner));
     presentation_service_remote_->SetController(
         presentation_controller_receiver_.BindNewPipeAndPassRemote(
