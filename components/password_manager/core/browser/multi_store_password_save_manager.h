ca9c0adab921fd3eec9803f6fe33b835f52f5c84
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-04-06 14:46:38 +0000

    Take only account store conflicts into account for generation in b4p
    MultiStorePasswordSaveManager now passes only matches in the account
    store to the PasswordGenerationManager API, causing matches in the
    profile store not to be taken into account, as specified in the b4p
    requirements.
    
    A pre-existing unit test checked that only the account form saver was
    exercised when triggering generation with the account store enabled.
    That test is extended to verify this is still true in the multiple
    possible conflict cases that can exist in each store.
    
    Bug: 1060131
    Change-Id: I19092cf11603c1308172362b181dc505d91986cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132456
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756658}

--- components/password_manager/core/browser/multi_store_password_save_manager.h
+++ components/password_manager/core/browser/multi_store_password_save_manager.h
@@ -47,6 +47,8 @@ class MultiStorePasswordSaveManager : public PasswordSaveManagerImpl {
   FindSimilarSavedFormAndComputeState(
       const autofill::PasswordForm& parsed_submitted_form) const override;
   FormSaver* GetFormSaverForGeneration() override;
+  std::vector<const autofill::PasswordForm*> GetRelevantMatchesForGeneration(
+      const std::vector<const autofill::PasswordForm*>& matches) override;
 
  private:
   bool IsOptedInForAccountStorage();
