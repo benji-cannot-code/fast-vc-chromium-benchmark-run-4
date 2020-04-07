132fb4b75bfcd55ae9ff59798e9b0a66d854697e
Aaron Colwell <acolwell@chromium.org>
2020-04-07 20:09:05 +0000

    Add CrashKey logging for CPSPI::Handle::CanAccessDataForOrigin().
    Adding logging to the "invalid handle" return paths so we get the
    standard CanAccessDataForOrigin crash keys in these cases as well. This
    makes it easier to detect cases in the crash logs where an invalid
    handle was used for an access check.
    
    Bug: 977169
    Change-Id: I63f7bfdf820a3ed69a667b6e3cc88f639107b33e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139417
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Auto-Submit: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757179}

--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -234,8 +234,11 @@ bool ChildProcessSecurityPolicyImpl::Handle::CanReadFileSystemFile(
 
 bool ChildProcessSecurityPolicyImpl::Handle::CanAccessDataForOrigin(
     const GURL& url) {
-  if (child_id_ == ChildProcessHost::kInvalidUniqueID)
+  if (child_id_ == ChildProcessHost::kInvalidUniqueID) {
+    LogCanAccessDataForOriginCrashKeys(
+        "(unknown)", "(unknown)", url.GetOrigin().spec(), "handle_not_valid");
     return false;
+  }
 
   auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
   return policy->CanAccessDataForOrigin(child_id_, url);
@@ -243,8 +246,11 @@ bool ChildProcessSecurityPolicyImpl::Handle::CanAccessDataForOrigin(
 
 bool ChildProcessSecurityPolicyImpl::Handle::CanAccessDataForOrigin(
     const url::Origin& origin) {
-  if (child_id_ == ChildProcessHost::kInvalidUniqueID)
+  if (child_id_ == ChildProcessHost::kInvalidUniqueID) {
+    LogCanAccessDataForOriginCrashKeys(
+        "(unknown)", "(unknown)", origin.GetDebugString(), "handle_not_valid");
     return false;
+  }
 
   auto* policy = ChildProcessSecurityPolicyImpl::GetInstance();
   return policy->CanAccessDataForOrigin(child_id_, origin);
