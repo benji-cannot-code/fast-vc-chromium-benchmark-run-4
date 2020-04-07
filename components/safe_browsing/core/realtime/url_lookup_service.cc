0089f0072a8e4f9af1aa4f90200db569a016909f
Xinghui Lu <xinghuilu@chromium.org>
2020-04-07 01:55:13 +0000

    Attach token only when enhance protection users are signed in.
    It is possible that users are not signed in but enabled enhanced
    protection. In this case, the access token is not available. Add a
    signin status check in this CL.
    
    Bug: 1041912
    Change-Id: I1095a3d26784c6ba4bcf8a768bbed064ae39088e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2138164
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756907}

--- components/safe_browsing/core/realtime/url_lookup_service.cc
+++ components/safe_browsing/core/realtime/url_lookup_service.cc
@@ -408,7 +408,7 @@ bool RealTimeUrlLookupService::CanPerformFullURLLookup() const {
 
 bool RealTimeUrlLookupService::CanPerformFullURLLookupWithToken() const {
   return RealTimePolicyEngine::CanPerformFullURLLookupWithToken(
-      pref_service_, is_off_the_record_, sync_service_);
+      pref_service_, is_off_the_record_, sync_service_, identity_manager_);
 }
 
 bool RealTimeUrlLookupService::IsUserEpOptedIn() const {
