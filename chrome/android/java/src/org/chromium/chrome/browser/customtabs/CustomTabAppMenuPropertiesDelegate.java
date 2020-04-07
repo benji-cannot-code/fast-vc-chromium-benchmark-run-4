e5bc647ad93b80de1c45d487faec0dcb8fa8347c
Theresa Wellington <twellington@chromium.org>
2020-04-07 17:48:16 +0000

    Create AppMenuPropertiesDelegateUnitTest
    Create a new junit test suite with a few simple tests to check whether
    page vs overview menu should be shown and menu items visible on the NTP.
    
    BUG=1066119
    
    Change-Id: I803cd0503b8a254fc9f5a8fb3e3ca5e1b6f65ec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135306
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757105}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
@@ -26,7 +26,6 @@ import org.chromium.chrome.browser.DefaultBrowserInfo;
 import org.chromium.chrome.browser.appmenu.AppMenuPropertiesDelegateImpl;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge;
 import org.chromium.chrome.browser.browserservices.BrowserServicesIntentDataProvider.CustomTabsUiType;
-import org.chromium.chrome.browser.download.DownloadUtils;
 import org.chromium.chrome.browser.firstrun.FirstRunStatus;
 import org.chromium.chrome.browser.multiwindow.MultiWindowModeStateDispatcher;
 import org.chromium.chrome.browser.share.ShareHelper;
@@ -180,7 +179,7 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
 
             MenuItem downloadItem = menu.findItem(R.id.offline_page_id);
             if (downloadItemVisible) {
-                downloadItem.setEnabled(DownloadUtils.isAllowedToDownloadPage(currentTab));
+                downloadItem.setEnabled(shouldEnableDownloadPage(currentTab));
             } else {
                 downloadItem.setVisible(false);
             }
