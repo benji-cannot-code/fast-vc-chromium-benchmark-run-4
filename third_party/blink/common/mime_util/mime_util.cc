686a3f1d2226552df5166380abca1bd50b9e8cc4
Wan-Teh Chang <wtc@google.com>
2020-04-07 02:17:42 +0000

    Use range-based for loop in MimeUtil constructor
    This is suggested by a Tricium Robot Comment from
    ClangTidy/modernize-loop-convert.
    
    Change-Id: I352bc58595382cb5d419c0df52941455e4745d0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2136883
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/master@{#756913}

--- third_party/blink/common/mime_util/mime_util.cc
+++ third_party/blink/common/mime_util/mime_util.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_set>
 
 #include "base/lazy_instance.h"
-#include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "build/build_config.h"
 #include "net/base/mime_util.h"
@@ -132,15 +131,15 @@ class MimeUtil {
 };
 
 MimeUtil::MimeUtil() {
-  for (size_t i = 0; i < base::size(kSupportedNonImageTypes); ++i)
-    non_image_types_.insert(kSupportedNonImageTypes[i]);
-  for (size_t i = 0; i < base::size(kSupportedImageTypes); ++i)
-    image_types_.insert(kSupportedImageTypes[i]);
-  for (size_t i = 0; i < base::size(kUnsupportedTextTypes); ++i)
-    unsupported_text_types_.insert(kUnsupportedTextTypes[i]);
-  for (size_t i = 0; i < base::size(kSupportedJavascriptTypes); ++i) {
-    javascript_types_.insert(kSupportedJavascriptTypes[i]);
-    non_image_types_.insert(kSupportedJavascriptTypes[i]);
+  for (const char* type : kSupportedNonImageTypes)
+    non_image_types_.insert(type);
+  for (const char* type : kSupportedImageTypes)
+    image_types_.insert(type);
+  for (const char* type : kUnsupportedTextTypes)
+    unsupported_text_types_.insert(type);
+  for (const char* type : kSupportedJavascriptTypes) {
+    javascript_types_.insert(type);
+    non_image_types_.insert(type);
   }
 }
 
