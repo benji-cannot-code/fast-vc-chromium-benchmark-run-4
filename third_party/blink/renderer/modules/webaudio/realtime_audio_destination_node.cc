de4d45549bace19c8b886df32a3491ba75c5bb11
Hongchan Choi <hongchan@chromium.org>
2020-04-07 14:56:41 +0000

    Use kInternalMediaRealTime task type for AudioWorklet
    This CL changes the task type of AudioWorklet to improve the
    responsiveness of the task runner when the actual task runner
    implementation can prioritize the real-time tasks.
    
    Bug: 1068311
    Change-Id: Ifa13e535474a979d15738ecaa24ca08c90a77dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2138152
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757055}

--- third_party/blink/renderer/modules/webaudio/realtime_audio_destination_node.cc
+++ third_party/blink/renderer/modules/webaudio/realtime_audio_destination_node.cc
@@ -266,7 +266,7 @@ void RealtimeAudioDestinationHandler::StartPlatformDestination() {
     platform_destination_->StartWithWorkletTaskRunner(
         audio_worklet->GetMessagingProxy()
             ->GetBackingWorkerThread()
-            ->GetTaskRunner(TaskType::kInternalMedia));
+            ->GetTaskRunner(TaskType::kInternalMediaRealTime));
   } else {
     platform_destination_->Start();
   }
