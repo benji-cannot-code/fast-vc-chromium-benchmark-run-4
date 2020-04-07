20dee89478c9e531e0a7481e92de26efe7377a1c
Rayan Kanso <rayankans@google.com>
2020-04-07 10:53:55 +0000

    [WebAPK] Remove the server URL from public chromium
    Tested by running chrome_apk and chrome_public_apk
    
    Bug: 1045009, 911900
    Change-Id: Ib6803cfc0668967efdaac147e4d3c3269634c6f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2126929
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757004}

--- chrome/browser/android/webapk/webapk_installer.cc
+++ chrome/browser/android/webapk/webapk_installer.cc
@@ -539,6 +539,11 @@ void WebApkInstaller::InstallAsync(const ShortcutInfo& shortcut_info,
   finish_callback_ = std::move(finish_callback);
   task_type_ = INSTALL;
 
+  if (!server_url_.is_valid()) {
+    OnResult(WebApkInstallResult::FAILURE);
+    return;
+  }
+
   CheckFreeSpace();
 }
 
@@ -574,6 +579,11 @@ void WebApkInstaller::UpdateAsync(const base::FilePath& update_request_path,
                                   FinishCallback finish_callback) {
   finish_callback_ = std::move(finish_callback);
   task_type_ = UPDATE;
+
+  if (!server_url_.is_valid()) {
+    OnResult(WebApkInstallResult::FAILURE);
+    return;
+  }
 
   base::PostTaskAndReplyWithResult(
       GetBackgroundTaskRunner().get(), FROM_HERE,
@@ -693,6 +703,8 @@ void WebApkInstaller::OnGotIconMurmur2Hashes(
 
 void WebApkInstaller::SendRequest(
     std::unique_ptr<std::string> serialized_proto) {
+  DCHECK(server_url_.is_valid());
+
   timer_.Start(
       FROM_HERE, base::TimeDelta::FromMilliseconds(webapk_server_timeout_ms_),
       base::Bind(&WebApkInstaller::OnResult, weak_ptr_factory_.GetWeakPtr(),
