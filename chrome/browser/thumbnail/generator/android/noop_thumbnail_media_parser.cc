87729f018c1473df90a8e285375f7f93ad01856c
Xing Liu <xingliu@chromium.org>
2020-04-07 20:42:10 +0000

    Media Thumbnail Parser: Use empty implementation when ffmpeg is disabled.
    VideoFrameExtractor should not be built when media_use_ffmpeg build
    flag is false.
    
    This CL splits ThumbnailMediaParser to an interface and use empty
    implementation based on media_use_ffmpeg.
    
    There are other build errors, this CL only fixes the one in
    VideoFrameExtractor use case.
    
    Bug: 1066404
    Change-Id: Ib856899b4d465a817634b468866d3fb62d7c5053
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131036
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757195}

---
+++ chrome/browser/thumbnail/generator/android/noop_thumbnail_media_parser.cc
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/thumbnail/generator/android/noop_thumbnail_media_parser.h"
+
+#include "third_party/skia/include/core/SkBitmap.h"
+
+void NoopThumbnailMediaParser::Start(ParseCompleteCB parse_complete_cb) {
+  DCHECK(parse_complete_cb);
+  std::move(parse_complete_cb)
+      .Run(false /*success*/, chrome::mojom::MediaMetadata::New(), SkBitmap());
+}
