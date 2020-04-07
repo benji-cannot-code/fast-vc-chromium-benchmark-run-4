19bebec26f75f10763f095854e8c4e06edd5867b
Lily Chen <chlily@chromium.org>
2020-04-07 00:25:58 +0000

    Pass NetworkIsolationKey to ReportingCache from ReportingHeaderParser
    ReportingCache is now told what the NIK of each parsed header is.
    This will eventually be passed to the ReportingHeaderParser by the
    caller and will correspond to the NIK of the response sending the
    header.
    
    The NIK fields are still empty for now, except in tests.
    
    Bug: 993805
    Change-Id: I93c30932559338c6ff2173d7ecbedf3541a0d35e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135829
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#756882}

--- net/reporting/reporting_header_parser.cc
+++ net/reporting/reporting_header_parser.cc
@@ -242,9 +242,11 @@ void ReportingHeaderParser::RecordHeaderDiscardedForJsonTooBig() {
 }
 
 // static
-void ReportingHeaderParser::ParseHeader(ReportingContext* context,
-                                        const GURL& url,
-                                        std::unique_ptr<base::Value> value) {
+void ReportingHeaderParser::ParseHeader(
+    ReportingContext* context,
+    const NetworkIsolationKey& network_isolation_key,
+    const GURL& url,
+    std::unique_ptr<base::Value> value) {
   DCHECK(url.SchemeIsCryptographic());
 
   const base::ListValue* group_list = nullptr;
@@ -255,7 +257,6 @@ void ReportingHeaderParser::ParseHeader(ReportingContext* context,
   ReportingCache* cache = context->cache();
 
   url::Origin origin = url::Origin::Create(url);
-  NetworkIsolationKey network_isolation_key = NetworkIsolationKey::Todo();
 
   std::vector<ReportingEndpointGroup> parsed_header;
 
@@ -280,8 +281,8 @@ void ReportingHeaderParser::ParseHeader(ReportingContext* context,
     return;
   }
 
-  // TODO(chlily): Pass NIK to cache.
-  cache->OnParsedHeader(origin, std::move(parsed_header));
+  cache->OnParsedHeader(network_isolation_key, origin,
+                        std::move(parsed_header));
   RecordHeaderOutcome(HeaderOutcome::PARSED);
 }
 
