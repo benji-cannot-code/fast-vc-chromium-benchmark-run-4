81f2f8ae43fc10943f562c38c1af269ec37a5bd0
Joshua Pawlicki <waffles@chromium.org>
2020-04-07 15:04:58 +0000

    Updater docs: Add edge in state diagram for update disabled.
    Change-Id: Ie5915b1d8c8ef6593ae77fa453f73ac288423f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140031
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757056}

--- components/update_client/component.cc
+++ components/update_client/component.cc
@@ -45,8 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //  kUpdateError <------------- [update?] -> [action?] -> kUpToDate  kUpdated
 //     ^                            |           |            ^        ^
 //     |                        yes |           | yes        |        |
-//     |                            V           |            |        |
-//     |                        kCanUpdate      +--------> kRun       |
+//     |     update disabled        V           |            |        |
+//     +-<--------------------- kCanUpdate      +--------> kRun       |
 //     |                            |                                 |
 //     |                no          V                                 |
 //     |               +-<- [differential update?]                    |
