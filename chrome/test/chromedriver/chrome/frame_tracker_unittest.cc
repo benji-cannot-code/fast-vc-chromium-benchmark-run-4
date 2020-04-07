1067d2ac0a943c9e9534b0cf669bd2f5171fa3a0
Shengfa Lin <shengfa@google.com>
2020-04-07 19:03:18 +0000

    [ChromeDriver] Remove TODOs in frame_tracker
    Remove isDefault and frameId for Chrome 53 and type for Chrome 50 parsing
    support
    
    Change-Id: Ia9c630eede1c0af305477b6017d9af7fd72c5641
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2138231
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Shengfa Lin <shengfa@google.com>
    Cr-Commit-Position: refs/heads/master@{#757150}

--- chrome/test/chromedriver/chrome/frame_tracker_unittest.cc
+++ chrome/test/chromedriver/chrome/frame_tracker_unittest.cc
@@ -19,7 +19,8 @@ TEST(FrameTracker, GetContextIdForFrame) {
   ASSERT_TRUE(tracker.GetContextIdForFrame("f", &context_id).IsError());
   ASSERT_EQ(-1, context_id);
 
-  const char context[] = "{\"id\":100,\"frameId\":\"f\"}";
+  const char context[] =
+      "{\"id\":100,\"auxData\":{\"frameId\":\"f\",\"isDefault\":true}}";
   base::DictionaryValue params;
   params.Set("context", base::JSONReader::ReadDeprecated(context));
   ASSERT_EQ(kOk,
@@ -69,7 +70,8 @@ TEST(FrameTracker, CanUpdateFrameContextId) {
   StubDevToolsClient client;
   FrameTracker tracker(&client);
 
-  const char context[] = "{\"id\":1,\"frameId\":\"f\"}";
+  const char context[] =
+      "{\"id\":1,\"auxData\":{\"frameId\":\"f\",\"isDefault\":true}}";
   base::DictionaryValue params;
   params.Set("context", base::JSONReader::ReadDeprecated(context));
   ASSERT_EQ(kOk,
@@ -91,7 +93,8 @@ TEST(FrameTracker, DontTrackContentScriptContexts) {
   StubDevToolsClient client;
   FrameTracker tracker(&client);
 
-  const char context[] = "{\"id\":1,\"frameId\":\"f\"}";
+  const char context[] =
+      "{\"id\":1,\"auxData\":{\"frameId\":\"f\",\"isDefault\":true}}";
   base::DictionaryValue params;
   params.Set("context", base::JSONReader::ReadDeprecated(context));
   ASSERT_EQ(kOk,
@@ -102,7 +105,7 @@ TEST(FrameTracker, DontTrackContentScriptContexts) {
   ASSERT_EQ(1, context_id);
 
   params.SetInteger("context.id", 2);
-  params.SetString("context.type", "Extension");
+  params.SetBoolean("context.auxData.isDefault", false);
   ASSERT_EQ(kOk,
             tracker.OnEvent(&client, "Runtime.executionContextCreated", params)
                 .code());
