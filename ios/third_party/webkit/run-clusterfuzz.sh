dea9eea1fde08e0e0a8740243d0b5193413e8a7b
Ali Juma <ajuma@chromium.org>
2020-04-07 20:29:13 +0000

    [ios fuzzing] Add a script to launch WebKitTestRunner on fuzzer bots
    This adds a run-clusterfuzz script that can be used to launch
    WebKitTestRunner on ClusterFuzz bots. Checking this script into the
    repository will make it easier for buildbots to package the script along
    with the rest of the build when creating builds for fuzzing.
    
    Bug: 1053304
    Change-Id: Ie2e311b862265c008e518b7d00b609265976ec5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140034
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757189}

---
+++ ios/third_party/webkit/run-clusterfuzz.sh
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# This script is used to launch WebKitTestRunner on ClusterFuzz bots.
+
+BASEDIR=$(dirname "$0")
+LIBDIR=$(xcode-select --print-path)/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/10.0.1/lib/darwin
+DYLD_FRAMEWORK_PATH=$BASEDIR DYLD_LIBRARY_PATH=$LIBDIR ./WebKitTestRunner $@
