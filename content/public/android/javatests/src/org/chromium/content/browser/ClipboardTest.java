b3313738aaba467c1b3d5f1f06296a7365b54add
Patrick Noland <pnoland@chromium.org>
2020-04-07 19:04:06 +0000

    Revert "Enable ClipboardTest.testCopyDocumentFragment"
    This reverts commit 29e93125d649f37619a07667800d9c3064e92d56.
    
    Reason for revert: Test is still flaking: 
    
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8883972746869676864/+/steps/content_shell_test_apk/0/logs/Deterministic_failure:_org.chromium.content.browser.ClipboardTest_testCopyDocumentFragment__status_FAILURE_/0
    
    Original change's description:
    > Enable ClipboardTest.testCopyDocumentFragment
    > 
    > Tried 100 times by tools/run-swarmed.py and the test never failed,
    > so enable the test.
    > 
    > Bug:791021
    > 
    > Change-Id: I903fa0abd132aea10e8a2b84b7282c5476bb7487
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2125871
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Commit-Queue: Gang Wu <gangwu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#754804}
    
    TBR=dtrainor@chromium.org,gangwu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 791021
    Change-Id: Ib5aaad028e64231af48f21b1161425f62975a16c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140303
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757151}

--- content/public/android/javatests/src/org/chromium/content/browser/ClipboardTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ClipboardTest.java
@@ -18,6 +18,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content.browser.webcontents.WebContentsImpl;
@@ -61,6 +62,7 @@ public class ClipboardTest {
     @LargeTest
     @Feature({"Clipboard", "TextInput"})
     @RerunWithUpdatedContainerView
+    @DisabledTest(message = "https://crbug.com/791021")
     public void testCopyDocumentFragment() {
         ClipboardManager clipboardManager =
                 TestThreadUtils.runOnUiThreadBlockingNoException(new Callable<ClipboardManager>() {
