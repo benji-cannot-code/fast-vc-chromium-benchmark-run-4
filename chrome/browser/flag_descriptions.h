133af9217e2a8370dfe3f231015ed8b3ecee3b88
Dominic Farolino <dom@chromium.org>
2020-04-07 23:25:48 +0000

    Remove kPrefetchMainResourceNetworkIsolationKey feature
    Since https://crrev.com/c/1943075, the
    kPrefetchMainResourceNetworkIsolationKey Network Service feature I added
    in https://crrev.com/c/1775890 has been enabled by default. This CL
    removes the feature, its usages from the codebase (mostly DCHECKs), and
    fixes/simplifies tests that were relying on it being tied to the
    net::features::kSplitCacheByNetworkIsolationKey feature.
    
    R=kinuko@chromium.org
    
    Bug: 939317
    Change-Id: Ia62ca8fd331a22cdf89a13f1c1006540a598ecbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135907
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757248}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -951,9 +951,6 @@ extern const char kRequestTabletSiteDescription[];
 extern const char kPrefetchPrivacyChangesName[];
 extern const char kPrefetchPrivacyChangesDescription[];
 
-extern const char kPrefetchMainResourceNetworkIsolationKeyName[];
-extern const char kPrefetchMainResourceNetworkIsolationKeyDescription[];
-
 extern const char kPrinterStatusName[];
 extern const char kPrinterStatusDescription[];
 
