bd2749458fa59bc1170b4101a702437bc52bcc4d
Changwan Ryu <changwan@google.com>
2020-04-07 20:19:20 +0000

    Remove unnecessary flags in IME tests
    These features have already launched, so no need to enable them for
    tests.
    
    Bug: 1068182
    Change-Id: I86464e0b420b42b5b945e40166276c1dd6c968b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140294
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757185}

--- content/public/android/javatests/src/org/chromium/content/browser/input/ImeInputActionTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/ImeInputActionTest.java
@@ -14,7 +14,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.test.ContentJUnit4ClassRunner;
 import org.chromium.ui.base.ime.TextInputAction;
@@ -24,7 +23,6 @@ import org.chromium.ui.base.ime.TextInputType;
  * IME (input method editor) and text input tests for enterkeyhint attribute.
  */
 @RunWith(ContentJUnit4ClassRunner.class)
-@CommandLineFlags.Add({"enable-experimental-web-platform-features"})
 public class ImeInputActionTest {
     @Rule
     public ImeActivityTestRule mRule = new ImeActivityTestRule();
