7610ff2f2543b454bfdd89b8f295f4907d016e36
Mitsuru Oshima <oshima@chromium.org>
2020-04-07 14:47:59 +0000

    [CrOS only] Experimental new shortcut handling for non US keyboard layouts
    * This CL changes the accelerator matching logic so that it
      uses DomKey and convert to VKEY+modifier as if it happened on
      US Keyboard Layout. This allows a user to use the key that produces
      the same key on the other layouts, with additional modifiers.
    
    * Reject "ASCII punctuation + shift only" acccelerator because
      this will not work well on non US keyboard. (We don't and shouldn't
      have such shortcut even now but added just in case)
    
    * Add new shortcuts that doesn't rely on ASCII punctuation for
      virtual desks and display zoom (uses Arrow keys)
    
    * Add new shortcuts for shortcuts that uses alt + punctuation,
      because it can cause ghosting when combined with altgr.
      (snap/minimize/maximize)
    
    See bug for design doc and new shortcut key combination.
    
    Bug: 1067269
    Test: covered by unittests. manually tested on chromeos devices.
    
    Change-Id: I1f2eb31923abf9c9b1a901eb6a84689c813d8b53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2124867
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757052}

--- ui/content_accelerators/accelerator_util.cc
+++ ui/content_accelerators/accelerator_util.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/events/blink/blink_event_util.h"
 #include "ui/events/event.h"
 #include "ui/events/event_constants.h"
@@ -15,13 +16,23 @@ namespace ui {
 
 ui::Accelerator GetAcceleratorFromNativeWebKeyboardEvent(
     const content::NativeWebKeyboardEvent& event) {
+#if defined(OS_CHROMEOS)
+  if (::features::IsNewShortcutMappingEnabled()) {
+    // TODO: This must be the same as below and it's simpler.
+    // Cleanup if this change sticks.
+    auto* os_event = static_cast<ui::KeyEvent*>(event.os_event);
+    return ui::Accelerator(*os_event);
+  }
+#endif
   Accelerator::KeyState key_state =
       event.GetType() == blink::WebInputEvent::kKeyUp
           ? Accelerator::KeyState::RELEASED
           : Accelerator::KeyState::PRESSED;
-  return ui::Accelerator(static_cast<ui::KeyboardCode>(event.windows_key_code),
-                         WebEventModifiersToEventFlags(event.GetModifiers()),
-                         key_state, event.TimeStamp());
+  ui::KeyboardCode keyboard_code =
+      static_cast<ui::KeyboardCode>(event.windows_key_code);
+  int modifiers = WebEventModifiersToEventFlags(event.GetModifiers());
+  return ui::Accelerator(keyboard_code, modifiers, key_state,
+                         event.TimeStamp());
 }
 
 }  // namespace ui
