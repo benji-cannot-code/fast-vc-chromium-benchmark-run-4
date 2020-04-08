06baacebec8ec4e0cb76efba293996a0578617f5
Lily Chen <chlily@chromium.org>
2020-04-08 21:08:44 +0000

    HatsSurveyStatusChecker: Remove use of attach_same_site_cookies param
    Bug: 1068792
    Change-Id: I47df5c385a7b5fc67cca14ac92a94b2d1ceb32c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141142
    Reviewed-by: Wei Li <weili@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757566}

--- chrome/browser/ui/hats/hats_survey_status_checker.cc
+++ chrome/browser/ui/hats/hats_survey_status_checker.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "net/base/load_flags.h"
 #include "net/cookies/canonical_cookie.h"
+#include "net/cookies/site_for_cookies.h"
 #include "net/http/http_status_code.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/resource_request.h"
@@ -72,8 +73,9 @@ void HatsSurveyStatusChecker::CheckSurveyStatus(
   auto request = std::make_unique<network::ResourceRequest>();
   std::string url_without_id(HatsSurveyURLWithoutId());
   request->url = GURL(url_without_id + site_id);
+  // Treat this request as same-site for the purposes of cookie inclusion.
+  request->site_for_cookies = net::SiteForCookies::FromUrl(request->url);
   // Send stored cookie along with the request, but don't save any cookie.
-  request->attach_same_site_cookies = true;
   request->load_flags = net::LOAD_BYPASS_CACHE | net::LOAD_DISABLE_CACHE |
                         net::LOAD_DO_NOT_SAVE_COOKIES;
   DCHECK(!url_loader_);
