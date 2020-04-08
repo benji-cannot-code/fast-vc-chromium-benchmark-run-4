cd7481718163509fd0a9a34fb07997c5fdf147b7
Wan-Teh Chang <wtc@google.com>
2020-04-08 01:32:18 +0000

    Add support for decoding the AV1 image format (AVIF).
    Adds libavif library to //third_party and implements a Blink
    ImageDecoder using the library.
    
    Need to do:
    - Implement YUV8 decoding support. Need YUV8 and YUV16 support.
    - Alpha images are not working correctly.
    - Implement support for HBD output for HBD images.
    - Implement color space support.
    - Set duration, disposal method, and alpha blend source from file
      format.
    - Add tests.
    - Support cropping, rotation, and mirroring.
    
    Based on patch set 9 of
    https://chromium-review.googlesource.com/c/chromium/src/+/1707404
    
    Author: Dale Curtis <dalecurtis@chromium.org>
    Contributor: Wan-Teh Chang <wtc@google.com>
    
    Tested:
    blink_common_unittests
    blink_common_unittests --enable-features=AVIF
    blink_platform_unittests
    blink_platform_unittests --enable-features=AVIF
    blink_unittests
    blink_unittests --enable-features=AVIF
    chrome
    chrome --enable-features=AVIF
    
    Bug: 960620
    Change-Id: I82cc50c63c0696bbf4d747df5001213885fc2b86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2101317
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Martin Barbella <mbarbella@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757285}

--- third_party/blink/common/mime_util/mime_util_unittest.cc
+++ third_party/blink/common/mime_util/mime_util_unittest.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/mime_util/mime_util.h"
 
 #include "build/build_config.h"
+#include "media/media_buildflags.h"
 #include "net/base/mime_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/public/common/features.h"
 
 namespace blink {
 
@@ -17,6 +19,15 @@ TEST(MimeUtilTest, LookupTypes) {
 
   EXPECT_TRUE(IsSupportedImageMimeType("image/jpeg"));
   EXPECT_TRUE(IsSupportedImageMimeType("Image/JPEG"));
+#if BUILDFLAG(ENABLE_AV1_DECODER)
+  EXPECT_EQ(IsSupportedImageMimeType("image/avif"),
+            base::FeatureList::IsEnabled(features::kAVIF));
+  EXPECT_EQ(IsSupportedImageMimeType("image/avif-sequence"),
+            base::FeatureList::IsEnabled(features::kAVIF));
+#else
+  EXPECT_FALSE(IsSupportedImageMimeType("image/avif"));
+  EXPECT_FALSE(IsSupportedImageMimeType("image/avif-sequence"));
+#endif
   EXPECT_FALSE(IsSupportedImageMimeType("image/lolcat"));
   EXPECT_FALSE(IsSupportedImageMimeType("Image/LolCat"));
   EXPECT_TRUE(IsSupportedNonImageMimeType("text/html"));
--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -168,6 +168,8 @@ BLINK_COMMON_EXPORT extern const base::Feature kKeepScriptResourceAlive;
 
 BLINK_COMMON_EXPORT extern const base::Feature kAppCache;
 
+BLINK_COMMON_EXPORT extern const base::Feature kAVIF;
+
 }  // namespace features
 }  // namespace blink
 
