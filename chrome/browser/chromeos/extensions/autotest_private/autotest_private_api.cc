61a1e53ac5cac0f094eceb45718c21d690e2b0bb
Timothy Loh <timloh@chromium.org>
2020-04-08 01:43:02 +0000

    Make autotestPrivate.installPluginVM() always pass the disk space check
    As per linked bug, we are getting test flakes as some devices are low
    on disk space and are having the installer pause at the low disk space
    confirmation screen. This CL updates installPluginVM() so always passes
    this check. If there is actually insufficient disk space for Plugin VM,
    the tests will probably fail in importing the VM.
    
    Bug: b/153268443
    Change-Id: I5a07f823eaa11e8ea5c52257a83a7eb5c4f4f5c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139021
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757289}

--- chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
+++ chrome/browser/chromeos/extensions/autotest_private/autotest_private_api.cc
@@ -72,6 +72,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/guest_os/guest_os_registry_service.h"
 #include "chrome/browser/chromeos/guest_os/guest_os_registry_service_factory.h"
 #include "chrome/browser/chromeos/login/lock/screen_locker.h"
+#include "chrome/browser/chromeos/plugin_vm/plugin_vm_installer.h"
+#include "chrome/browser/chromeos/plugin_vm/plugin_vm_installer_factory.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_util.h"
 #include "chrome/browser/chromeos/printing/cups_printers_manager.h"
 #include "chrome/browser/chromeos/settings/stats_reporting_controller.h"
@@ -1837,8 +1839,12 @@ AutotestPrivateInstallPluginVMFunction::Run() {
            << ", " << params->image_hash << ", " << params->license_key;
 
   Profile* profile = Profile::FromBrowserContext(browser_context());
+  plugin_vm::PluginVmInstallerFactory::GetForProfile(profile)
+      ->SetFreeDiskSpaceForTesting(
+          plugin_vm::PluginVmInstaller::kRecommendedFreeDiskSpace);
   plugin_vm::SetFakePluginVmPolicy(profile, params->image_url,
                                    params->image_hash, params->license_key);
+
   plugin_vm::ShowPluginVmInstallerView(profile);
   PluginVmInstallerView::GetActiveViewForTesting()
       ->SetFinishedCallbackForTesting(base::BindOnce(
