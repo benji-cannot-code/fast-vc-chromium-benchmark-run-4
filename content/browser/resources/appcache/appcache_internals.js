4b99865e951fe7da50a8107dfea850a79e77d49c
Adrienne Walker <enne@chromium.org>
2020-04-08 17:00:30 +0000

    appcache: remove token expires from entry in chrome://appcache-internals
    This is a followup to:
    https://chromium-review.googlesource.com/c/chromium/src/+/2132592
    
    That CL showed token_expires on both entries and groups.  It turns out
    that it should be sufficient to check only the group, so the UI on entry
    has been removed to avoid confusing developers.
    
    Bug: 582750
    Change-Id: Ib575241a8f1988ce40b87f9568ff2e4bbc7310a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140104
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757464}

--- content/browser/resources/appcache/appcache_internals.js
+++ content/browser/resources/appcache/appcache_internals.js
@@ -175,7 +175,6 @@ cr.define('appcache', function() {
         properties: properties,
         fileUrl: details.url,
         responseId: details.responseId,
-        tokenExpires: details.tokenExpires,
       });
     }
     return simpleVector;
