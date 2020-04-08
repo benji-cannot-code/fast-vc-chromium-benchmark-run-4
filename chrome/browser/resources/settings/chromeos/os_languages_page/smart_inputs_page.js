de92b5d52e51d8017b169b2afa0ccfb1c3d5846b
My Nguyen <myy@chromium.org>
2020-04-08 01:35:25 +0000

    Add smart inputs setting page in ChromeOS
    The view is currently behind a feature flag.
    
    SmartInputPage view: http://screen/h8Vuy3NdZaz
    SmartInputSection view: http://screen/2kt7NXrDC5h
    
    Bug: 1068379
    Test: browser_test
    Change-Id: I90f2dc3bf85f27af5e2ddb24b5e717f107309a30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132656
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: jimmy gong <jimmyxgong@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757287}

---
+++ chrome/browser/resources/settings/chromeos/os_languages_page/smart_inputs_page.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview 'os-settings-smart-inputs-page' is the settings sub-page
+ * to provide users with assistive or expressive input options.
+ */
+
+Polymer({
+  is: 'os-settings-smart-inputs-page',
+
+  behaviors: [
+    I18nBehavior,
+    PrefsBehavior,
+  ],
+
+  properties: {
+    /** Preferences state. */
+    prefs: {
+      type: Object,
+      notify: true,
+    },
+
+    /** @private */
+    allowAssistivePersonalInfo_: {
+      type: Boolean,
+      value() {
+        return loadTimeData.getBoolean('allowAssistivePersonalInfo');
+      },
+    }
+  },
+
+  /**
+   * Opens Chrome browser's autofill manage addresses setting page.
+   * @private
+   */
+  onManagePersonalInfoClick_() {
+    window.open('chrome://settings/addresses');
+  },
+});
--- chrome/browser/ui/ash/chrome_new_window_client.cc
+++ chrome/browser/ui/ash/chrome_new_window_client.cc
@@ -111,6 +111,8 @@ constexpr std::pair<arc::mojom::ChromePage, const char*> kOSSettingsMapping[] =
      {ChromePage::OSLANGUAGESDETAILS, chrome::kOsLanguagesDetailsSubPage},
      {ChromePage::OSLANGUAGESINPUTMETHODS,
       chrome::kOsLanguagesInputMethodsSubPage},
+     {ChromePage::OSLANGUAGESSMARTINPUTS,
+      chrome::kOsLanguagesSmartInputsSubPage},
      {ChromePage::LOCKSCREEN, chrome::kLockScreenSubPage},
      {ChromePage::MAIN, ""},
      {ChromePage::MANAGEACCESSIBILITY, chrome::kManageAccessibilitySubPage},
