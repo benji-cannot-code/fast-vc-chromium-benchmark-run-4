5c702a35389f790654a33ceb3add339e4f666902
garykac <garykac@chromium.org>
2020-04-08 21:10:43 +0000

    Move ScreenRecording permission check to /ui/base/cocoa
    This needs to move to a shared location so that the same code can
    be used by the Chromoting host.
    
    Change-Id: If860352ef8583d47f268d5cb5b964e3c7f0a2f25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141214
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Gary Kacmarcik <garykac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757568}

---
+++ ui/base/cocoa/permissions_utils.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_BASE_COCOA_PERMISSIONS_UTILS_H_
+#define UI_BASE_COCOA_PERMISSIONS_UTILS_H_
+
+#include "ui/base/ui_base_export.h"
+
+namespace ui {
+
+// Heuristic to check screen capture permission.
+// Starting on macOS 10.15, the ability to screen capture is restricted and
+// requires a permission authorization. There is no direct way to query the
+// permission state, so this uses a heuristic to evaluate whether the permission
+// has been granted.
+UI_BASE_EXPORT bool IsScreenCaptureAllowed();
+
+}  // namespace ui
+
+#endif  // UI_BASE_COCOA_PERMISSIONS_UTILS_H_
