f2182bd597cdcda3722a2be108789a9c408a70be
Rushan Suleymanov <rushans@google.com>
2020-04-08 13:19:59 +0000

    Reland "[Sync] Refactor model type processor: entity tracker lifetime"
    This reverts commit 3926e905dc5048c852b9937f580301b075ba2012.
    
    Reason for revert: reland after M83 BP.
    
    Original change's description:
    > Revert "[Sync] Refactor model type processor: entity tracker lifetime"
    >
    > This reverts commit 83dccf5d084181822af40e66a0f795f9178f740b.
    >
    > Reason for revert: this patch contains refactoring of client tag based
    > model type processor. To prevent too many changes in one release it is
    > better to reland this patch after M83 will be branched.
    >
    > Original change's description:
    > > [Sync] Refactor model type processor: entity tracker lifetime
    > >
    > > Make entity tracker's lifetime same as initial sync done. The tracker
    > > won't exist if we still have no data from the server.
    > >
    > > In this patch also authentication_account_id is not set explicitly for
    > > metadata coming from ModelReadyToSync. The behavior for this field
    > > becomes similar to cache_guid. If authentication_account_id differs from
    > > activation request then the metadata will be reset. Generally it should
    > > not affect anything.
    > >
    > > In the next patch unittests will be added.
    > >
    > > Bug: 947044
    > > Change-Id: I34a75006d1f31888f8ca485a94538b6dd98923c1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2038733
    > > Commit-Queue: Rushan Suleymanov <rushans@google.com>
    > > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#750920}
    >
    > TBR=mastiz@chromium.org,rushans@google.com
    >
    > # Not skipping CQ checks because original CL landed > 1 day ago.
    >
    > Bug: 947044
    > Change-Id: Icc0ae96eb2afa549024bf5b61efb799a6a92969c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130212
    > Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    > Commit-Queue: Rushan Suleymanov <rushans@google.com>
    > Cr-Commit-Position: refs/heads/master@{#755389}
    
    
    Bug: 947044
    Change-Id: I1f391874672cab29b204e32fd21285122a575a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135748
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#757407}

--- components/sync/model_impl/processor_entity_tracker.cc
+++ components/sync/model_impl/processor_entity_tracker.cc
@@ -13,14 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace syncer {
 
-ProcessorEntityTracker::ProcessorEntityTracker(ModelType type) {
-  InitializeMetadata(type);
-}
-
 ProcessorEntityTracker::ProcessorEntityTracker(
+    const sync_pb::ModelTypeState& model_type_state,
     std::map<std::string, std::unique_ptr<sync_pb::EntityMetadata>>
-        metadata_map,
-    const sync_pb::ModelTypeState& model_type_state)
+        metadata_map)
     : model_type_state_(model_type_state) {
   DCHECK(model_type_state.initial_sync_done());
   for (auto& kv : metadata_map) {
@@ -195,11 +191,6 @@ bool ProcessorEntityTracker::HasLocalChanges() const {
 
 size_t ProcessorEntityTracker::size() const {
   return entities_.size();
-}
-
-void ProcessorEntityTracker::InitializeMetadata(ModelType type) {
-  model_type_state_.mutable_progress_marker()->set_data_type_id(
-      GetSpecificsFieldNumberFromModelType(type));
 }
 
 std::vector<const ProcessorEntity*>
