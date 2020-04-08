66da6bc5acc026e102fa4ed6a5554507a96152b9
Bruce Dawson <brucedawson@chromium.org>
2020-04-08 23:47:07 +0000

    Add Windows heap trimming tool
    This is an experimental tool which will inject a thread into a Chrome
    process (tested on the browser and GPU process) and run code to call
    HeapSetInformation with HEAP_OPTIMIZE_RESOURCES_CURRENT_VERSION. This
    tells Windows to trim unnecessary memory from the heaps in that process.
    
    This tool uses sketchy techniques such as copying memory from one
    executable to another (only works if the code is relocatable and has no
    external references), VirtualAllocEx, and CreateRemoteThread. This is
    not for production use.
    
    To build run build.bat
    To run either run it manually (passing the PIDs of the processes of
    interest) or run trim_loop.bat (passing the PIDs of the processes of
    interest) to run it every five minutes in a loop.
    
    While it sometimes frees non-trivial amounts of memory the savings are
    not persistent.
    
    Bug: 1050059, 982452
    Change-Id: I646c515e59598c2c3003500419c154bacd34aeda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139127
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@google.com>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757644}

---
+++ tools/win/trim_heap/build.bat
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+@setlocal
+
+where cl
+if errorlevel 1 goto no_cl
+
+pushd %~dp0
+cl /nologo /Zi /GS- /EHsc trim_heap.cc /link /DEBUG /OPT:REF /OPT:ICF
+popd
+
+exit /b
+
+:no_cl
+@echo Run "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" amd64
+@echo or equivalent to get the 64-bit compiler tools in the path.
+exit /b
---
+++ tools/win/trim_heap/trim_loop.bat
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+call build
+
+@echo off
+
+:top
+echo Trimming heaps at %time%
+trim_heap %*
+sleep 300
+goto top
