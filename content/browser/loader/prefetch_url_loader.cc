32a2a749448ed0b5beff2cb0763aafa0ad9adb25
Dominic Farolino <dom@chromium.org>
2020-04-08 01:11:05 +0000

    Obsolete Prefetch.Redirect Histogram
    This CL "obsoletes" the Prefetch.Redirect histogram, as it was set to
    expire on M83.
    
    R=holte@chromium.org, kinuko@chromium.org
    
    Bug: 988956,1066052
    Change-Id: Iff1967203c3f06e535f12f6bdaa27fc30d0d7e38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130314
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757275}

--- content/browser/loader/prefetch_url_loader.cc
+++ content/browser/loader/prefetch_url_loader.cc
@@ -61,7 +61,6 @@ PrefetchURLLoader::PrefetchURLLoader(
           signed_exchange_utils::IsSignedExchangeHandlingEnabled(
               browser_context)) {
   DCHECK(network_loader_factory_);
-  RecordPrefetchRedirectHistogram(PrefetchRedirect::kPrefetchMade);
 
   if (is_signed_exchange_handling_enabled_) {
     // Set the SignedExchange accept header.
@@ -89,16 +88,6 @@ PrefetchURLLoader::PrefetchURLLoader(
 
 PrefetchURLLoader::~PrefetchURLLoader() = default;
 
-void PrefetchURLLoader::RecordPrefetchRedirectHistogram(
-    PrefetchRedirect event) {
-  // We only want to record prefetch vs prefetch redirects when we're not
-  // experimenting with a request's redirect mode.
-  if (base::FeatureList::IsEnabled(blink::features::kPrefetchPrivacyChanges))
-    return;
-
-  base::UmaHistogramEnumeration("Prefetch.Redirect", event);
-}
-
 void PrefetchURLLoader::FollowRedirect(
     const std::vector<std::string>& removed_headers,
     const net::HttpRequestHeaders& modified_headers,
@@ -109,16 +98,11 @@ void PrefetchURLLoader::FollowRedirect(
   DCHECK(!new_url) << "Redirect with modified URL was not "
                       "supported yet. crbug.com/845683";
   if (signed_exchange_prefetch_handler_) {
-    RecordPrefetchRedirectHistogram(
-        PrefetchRedirect::kPrefetchRedirectedSXGHandler);
-
     // Rebind |client_receiver_| and |loader_|.
     client_receiver_.Bind(signed_exchange_prefetch_handler_->FollowRedirect(
         loader_.BindNewPipeAndPassReceiver()));
     return;
   }
-
-  RecordPrefetchRedirectHistogram(PrefetchRedirect::kPrefetchRedirected);
 
   DCHECK(loader_);
   loader_->FollowRedirect(removed_headers,
