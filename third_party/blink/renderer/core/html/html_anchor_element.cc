042428cdce2c68bae73cd6deeae7e5261bf5756c
Timothy Gu <timothygu@chromium.org>
2020-04-08 21:35:53 +0000

    Treat any HTML element with a tabindex attribute as interactive content
    According to the HTML Standard [1]:
    
    > An element with the tabindex attribute specified is interactive
    > content.
    
    In a subsequent review [2], this clause is reaffirmed by the editor of
    HTML. Thus, we implement this accordingly.
    
    This commit also adds comprehensive testing for all kinds of interactive
    content, which have differential treatment by the <label> element
    relative to other elements.
    
    [1]: https://html.spec.whatwg.org/C/#the-tabindex-attribute
    [2]: https://github.com/whatwg/html/issues/5414
    
    Fixed: 1065048
    Change-Id: I8deff810fead2c9170e18dd4d06dc642f68e6742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122369
    Commit-Queue: Timothy Gu <timothygu@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757590}

--- third_party/blink/renderer/core/html/html_anchor_element.cc
+++ third_party/blink/renderer/core/html/html_anchor_element.cc
@@ -582,7 +582,7 @@ bool HTMLAnchorElement::WillRespondToMouseClickEvents() {
 }
 
 bool HTMLAnchorElement::IsInteractiveContent() const {
-  return IsLink();
+  return IsLink() || HTMLElement::IsInteractiveContent();
 }
 
 Node::InsertionNotificationRequest HTMLAnchorElement::InsertedInto(
