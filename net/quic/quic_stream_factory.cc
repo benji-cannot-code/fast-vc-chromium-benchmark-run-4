d631f43285af11ad91479f0538127a9a8fcc1a56
Renjie Tang <renjietang@chromium.org>
2020-04-08 22:17:29 +0000

    Revert "Initialize QuicClientSessionCache in QuicCryptoClientConfig."
    This reverts commit 5d23438763af908c25c53165436daab62a49496c.
    
    Reason for revert: The session cache needs more implementations. It shouldn't be used right now.
    
    Original change's description:
    > Initialize QuicClientSessionCache in QuicCryptoClientConfig.
    >
    > Change-Id: I1f82aab5bf180adf0dc489b833689fabbf9a1749
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122864
    > Reviewed-by: Nick Harper <nharper@chromium.org>
    > Commit-Queue: Renjie Tang <renjietang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#753821}
    
    TBR=nharper@chromium.org,renjietang@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I49553222e74e22ac194b8cc50f1d11bb28844737
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2142616
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Reviewed-by: Renjie Tang <renjietang@chromium.org>
    Commit-Queue: Renjie Tang <renjietang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757615}

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/quic_chromium_connection_helper.h"
 #include "net/quic/quic_chromium_packet_reader.h"
 #include "net/quic/quic_chromium_packet_writer.h"
-#include "net/quic/quic_client_session_cache.h"
 #include "net/quic/quic_context.h"
 #include "net/quic/quic_crypto_client_stream_factory.h"
 #include "net/quic/quic_http_stream.h"
@@ -326,9 +325,8 @@ class QuicStreamFactory::QuicCryptoClientConfigOwner {
  public:
   QuicCryptoClientConfigOwner(
       std::unique_ptr<quic::ProofVerifier> proof_verifier,
-      std::unique_ptr<QuicClientSessionCache> session_cache,
       QuicStreamFactory* quic_stream_factory)
-      : config_(std::move(proof_verifier), std::move(session_cache)),
+      : config_(std::move(proof_verifier)),
         quic_stream_factory_(quic_stream_factory) {
     DCHECK(quic_stream_factory_);
   }
@@ -2280,7 +2278,7 @@ QuicStreamFactory::CreateCryptoConfigHandle(
               cert_verifier_, ct_policy_enforcer_, transport_security_state_,
               cert_transparency_verifier_,
               HostsFromOrigins(params_.origins_to_force_quic_on)),
-          std::make_unique<QuicClientSessionCache>(), this);
+          this);
 
   quic::QuicCryptoClientConfig* crypto_config = crypto_config_owner->config();
   crypto_config->set_user_agent_id(params_.user_agent_id);
