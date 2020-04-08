ee170fc3e6a59f6643865b450b4ef6a78eab4e20
Julie Jeongeun Kim <jkim@igalia.com>
2020-04-08 11:18:34 +0000

    a11y: AXPlatformNodeBase::GetIndexInParent() returns base::Optional
    This fixes the regression issue from the CL[1] which moved
    the logic of GetIndexInParent to AXPlatformNodeBase.
    
    Before the CL[1], atk_object::GetIndexInParent returns -1 as an
    invalid value but AXPlatformNodeBase::GetIndexInParent returns
    positive numbers or 0 except the case when it's not implemented
    or doesn't have |delegate_|. AXPlatformNodeBase::GetIndexInParent
    is also used for the internal logic in AXPlatformNodeBase with
    the assumption that the return value is not less than 0.
    The CL[1] moved the logic to AXPlatformNodeBase::GetIndexInParent
    with keeping the rule that the return value is not less than 0.
    
    The problem is that screen reader such as Orca expects that it
    returns -1 when it's invalid.
    
    With this CL, AXPlatformNodeBase::GetIndexInParent() returns
    base::Optional and the caller has -1 if GetIndexInParent() returns
    base::nullopt. It also uses GetFirstChild and GetNextSibling for
    children iteration in GetHypertextOffsetFromChild() and
    GetHypertextOffsetFromEndpoint() instead of GetIndexInParent and
    GetChildAt.
    
    [1]https://crrev.com/c/2087234
    
    Bug: 1065534
    Change-Id: I00e2ed421ed263cf379ba22f55049fd52d32df9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129667
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Martin Robinson <mrobinson@igalia.com>
    Reviewed-by: Joanmarie Diggs <jdiggs@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#757381}

--- ui/accessibility/platform/ax_platform_node_mac.h
+++ ui/accessibility/platform/ax_platform_node_mac.h
@@ -28,7 +28,6 @@ class AXPlatformNodeMac : public AXPlatformNodeBase {
 
   // AXPlatformNodeBase.
   void Destroy() override;
-  int GetIndexInParent() override;
 
  protected:
   void AddAttributeToList(const char* name,
