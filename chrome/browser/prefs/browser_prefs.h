818cbb67a0caafac5a5c41cd6a296c29e4a62538
Lei Zhang <thestig@chromium.org>
2020-04-08 16:58:16 +0000

    Clean up local state migration code.
    - Remove the unused Profile* parameter from MigrateObsoleteLocalState().
    - Rename MigrateObsoleteBrowserPrefs() to
      MigrateObsoleteLocalStatePrefs(), to be consistent with other
      functions that operate on Local State prefs. Also rename
      MigrateObsoleteLocalState() to MigrateObsoleteLocalStatePrefs().
    - Then call MigrateObsoleteLocalStatePrefs() once per BrowserProcess
      initialization, instead of once per Profile initialization.
    - Split part of RegisterLocalState() off to
      RegisterLocalStatePrefsForMigration(), like the code does with
      RegisterProfilePrefsForMigration().
    
    Change-Id: Id93c0d912f2418b40de33ed5348e3ffe77fb0f28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2136190
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757459}

--- chrome/browser/prefs/browser_prefs.h
+++ chrome/browser/prefs/browser_prefs.h
@@ -6,10 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_PREFS_BROWSER_PREFS_H_
 #define CHROME_BROWSER_PREFS_BROWSER_PREFS_H_
 
-#include <set>
+#include <string>
 
 #include "build/build_config.h"
-#include "components/prefs/pref_value_store.h"
 
 class PrefRegistrySimple;
 class PrefService;
@@ -42,8 +41,8 @@ void RegisterSigninProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 // Migrate/cleanup deprecated prefs in |local_state|. Over time, long deprecated
 // prefs should be removed as new ones are added, but this call should never go
 // away (even if it becomes an empty call for some time) as it should remain
-// *the* place to drop deprecated browser prefs at.
-void MigrateObsoleteBrowserPrefs(Profile* profile, PrefService* local_state);
+// *the* place to drop deprecated browser-level (Local State) prefs at.
+void MigrateObsoleteLocalStatePrefs(PrefService* local_state);
 
 // Migrate/cleanup deprecated prefs in |profile|'s pref store. Over time, long
 // deprecated prefs should be removed as new ones are added, but this call
