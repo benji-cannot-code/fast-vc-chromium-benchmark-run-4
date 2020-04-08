9da7390783343bc2b586381106bd9c9677dde489
spdonghao <spdonghao@chromium.org>
2020-04-08 23:20:34 +0000

    Introduce MostVisitedSitesFaviconHelper.
    Class to write top sites favicon info to devices.
    
    Bug: 1061906
    Change-Id: Iece46799499890bff10e17bd7f6b7835b851bb51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122848
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757634}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/mostvisited/MostVisitedSitesMetadataUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/mostvisited/MostVisitedSitesMetadataUtils.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.suggestions.mostvisited;
 
 import android.content.Context;
 
+import androidx.annotation.VisibleForTesting;
 import androidx.core.util.AtomicFile;
 
 import org.chromium.base.ContextUtils;
@@ -209,5 +210,10 @@ public class MostVisitedSitesMetadataUtils {
             }
             return sStateDirectory;
         }
+    }
+
+    @VisibleForTesting
+    protected static File getStateDirectoryForTesting() {
+        return sStateDirectory;
     }
 }
