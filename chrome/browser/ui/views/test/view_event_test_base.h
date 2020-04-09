2641b085eb75f3e42086b3a577db77a0a47b4ef5
Peter Kasting <pkasting@chromium.org>
2020-04-09 16:44:41 +0000

    Trivial cleanups noticed while doing other work.
    Bug: none
    Change-Id: Iad1d006fb42d98a583449ff3f4b659fee2fbdf37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143674
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757887}

--- chrome/browser/ui/views/test/view_event_test_base.h
+++ chrome/browser/ui/views/test/view_event_test_base.h
@@ -73,7 +73,7 @@ class ViewEventTestBase : public ChromeViewsTestBase {
 
   static void SetUpTestCase();
 
-  // testing::Test:
+  // ChromeViewsTestBase:
   void SetUp() override;
   void TearDown() override;
 
