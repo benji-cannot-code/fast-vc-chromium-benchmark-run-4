b1408c3dc5f3592b4f5719224706af1302f270b8
Aya ElAttar <ayaelattar@google.com>
2020-04-09 10:49:58 +0000

    Disabled Settings menu item via SystemFeaturesDisableList
    Bug: 1068206
    Change-Id: Iab84006d32e3cefbea3856cc94461c1343aa32d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137329
    Commit-Queue: Aya Elsayed <ayaelattar@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757794}

--- chrome/browser/ui/toolbar/app_menu_model.h
+++ chrome/browser/ui/toolbar/app_menu_model.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "ui/base/accelerators/accelerator.h"
@@ -196,6 +197,12 @@ class AppMenuModel : public ui::SimpleMenuModel,
   // took to select the command.
   void LogMenuMetrics(int command_id);
 
+#if defined(OS_CHROMEOS)
+  // Disables/Enables the settings item based on kSystemFeaturesDisableList
+  // pref.
+  void UpdateSettingsItemState();
+#endif  // defined(OS_CHROMEOS)
+
   // Time menu has been open. Used by LogMenuMetrics() to record the time
   // to action when the user selects a menu item.
   base::ElapsedTimer timer_;
@@ -225,6 +232,8 @@ class AppMenuModel : public ui::SimpleMenuModel,
 
   std::unique_ptr<content::HostZoomMap::Subscription>
       browser_zoom_subscription_;
+
+  PrefChangeRegistrar local_state_pref_change_registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(AppMenuModel);
 };
