85b9a5e304aa2b9c851b7ed3ebdb3ada503a42ff
Tanya Gupta <tgupta@chromium.org>
2020-04-09 19:34:05 +0000

    UI polish including adding an image for dark mode and updated string for permissions dialog.
    Bug: 1066228
    Change-Id: I0580167cc2f39ac6ce1361596118c691c77ff4a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130407
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Commit-Queue: Tanya Gupta <tgupta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757959}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/scan_tab/QrCodeScanView.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/scan_tab/QrCodeScanView.java
@@ -27,6 +27,7 @@ import org.chromium.ui.widget.ButtonCompat;
  */
 class QrCodeScanView {
     public interface PermissionPrompter { void promptForCameraPermission(); }
+
     public interface PermissionPromptAllowedChecker { Boolean canPromptForPermission(); }
 
     private final Context mContext;
@@ -115,8 +116,8 @@ class QrCodeScanView {
     }
 
     /**
-     * Creates a view that opens the settings page for the app and allows the user to
-     * to update permissions including give the app camera permission.
+     * Creates a view that opens the settings page for the app and allows the user to to update
+     * permissions including give the app camera permission.
      */
     private View createOpenSettingsView(Context context) {
         View openSettingsView = (View) LayoutInflater.from(context).inflate(
@@ -152,10 +153,8 @@ class QrCodeScanView {
     }
 
     /**
-     * Update the view based on the latest environment:
-     * - app is in the foreground
-     * - user has given camera permission
-     * - user can be prompted for camera permission
+     * Update the view based on the latest environment: - app is in the foreground - user has given
+     * camera permission - user can be prompted for camera permission
      */
     private void updateView() {
         // The scan tab is not in the foreground so don't do any rendering.
@@ -180,7 +179,7 @@ class QrCodeScanView {
      * to let the user know if the permission has been permanently denied.
      *
      * @param canPromptForPermission Indicates whether the user can be prompted for camera
-     * permission
+     *            permission
      */
     public void canPromptForPermissionChanged(Boolean canPromptForPermission) {
         mCanPromptForPermission = canPromptForPermission;
