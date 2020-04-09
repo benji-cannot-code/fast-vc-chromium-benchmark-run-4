7eee8c56888569497e303765d8983498b8f8703b
Kartik Hegde <khegde@chromium.org>
2020-04-09 21:22:17 +0000

    network_diagnostics: Add initial framework
    BUG=chromium:956783
    TEST=unit_tests --gtest_filter=NetworkDiagnosticsRoutineTest.*
    
    Change-Id: Idaefb63874cb26360a7d22f1ad7ed9cfe4da7810
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2113736
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758023}

---
+++ chrome/browser/chromeos/net/network_diagnostics/network_diagnostics_routine.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/net/network_diagnostics/network_diagnostics_routine.h"
+
+namespace chromeos {
+namespace network_diagnostics {
+
+NetworkDiagnosticsRoutine::NetworkDiagnosticsRoutine() = default;
+
+NetworkDiagnosticsRoutine::~NetworkDiagnosticsRoutine() = default;
+
+void NetworkDiagnosticsRoutine::AnalyzeResultsAndExecuteCallback() {
+  std::move(routine_completed_callback_).Run(verdict());
+}
+
+}  // namespace network_diagnostics
+}  // namespace chromeos
