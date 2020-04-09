3d3574989ec3622eb954f7f1f4860d9466e7c4e8
Liquan (Max) Gu <maxlg@chromium.org>
2020-04-09 01:31:31 +0000

    Reland "Reland "[PlayBilling] Support app-store billing in AndroidPaymentAppFinder""
    Original patch: https://crrev.com/c/2141209
    Revert: https://crrev.com/c/2142334
     Reason for revert:
     ErrorProne usage of array.toString() blocks compilation.
    
    Reason for reland:
     The error get fixed.
     Before: array.toString()
     After: Arrays.toString(array)
    
    Change-Id: Id669ad925c980584911809d83dd10506e0fa9e27
    Bug: 1064740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141244
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757677}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestImpl.java
@@ -2621,6 +2621,12 @@ public class PaymentRequestImpl
         return mRawTotal.amount.currency;
     }
 
+    // PaymentAppFactoryParams implementation.
+    @Override
+    public boolean requestShippingOrPayerContact() {
+        return mRequestShipping || mRequestPayerName || mRequestPayerPhone || mRequestPayerEmail;
+    }
+
     // PaymentAppFactoryDelegate implementation.
     @Override
     public PaymentAppFactoryParams getParams() {
