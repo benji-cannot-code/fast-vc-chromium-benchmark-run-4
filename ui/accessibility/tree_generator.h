2aa15dcfb09c0fbb35c5442e6857c411c8547533
Dominic Mazzoni <dmazzoni@chromium.org>
2020-04-09 20:32:07 +0000

    Add generated tree test to ensure ignored nodes are handled properly.
    First, extends TreeGenerator so that it can not only generate every
    "shape" of tree, but it can then populate that tree with every
    permutation of ignored nodes.
    
    Then, write a new test that tries every possible tree up to a certain
    size and tries every possible source and destination permutation of
    ignored nodes. We call this the "fat" tree. Update the source tree into
    the destination tree and capture the events fired.
    
    Next, build the equivalent "skinny" trees - the same trees but with
    the ignored nodes removed. Now convert the source skinny tree into the
    destination skinny tree.
    
    Assert that the generated events fired are the same in the fat tree as
    the skinny tree. In other words, ignored nodes really are ignored and
    don't affect things. (The only exception is the ignored_changed
    attribute.)
    
    Finally, check every unignored node in the fat tree and make sure that its
    unignored parent, children, siblings, and index in parent are all
    consistent with the equivalent values in the skinny tree.
    
    This test uncovered bugs, which were fixed in http://crrev.com/c/2125235
    before this patch was uploaded for review.
    
    Bug: 1065144
    Change-Id: I82f9b3487485f1024ee066b82c23ab0cd7b60e38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139082
    Reviewed-by: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757989}

--- ui/accessibility/tree_generator.h
+++ ui/accessibility/tree_generator.h
@@ -3,7 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef UI_ACCESSIBILITY_TREE_GENERATOR_H_
+#define UI_ACCESSIBILITY_TREE_GENERATOR_H_
+
 #include <vector>
+
+#include "ui/accessibility/ax_tree_update_forward.h"
 
 namespace ui {
 
@@ -46,13 +51,23 @@ class TreeGenerator {
   TreeGenerator(int max_node_count, bool permutations);
   ~TreeGenerator();
 
+  // Build all unique trees (no nodes ignored).
   int UniqueTreeCount() const;
-
   void BuildUniqueTree(int tree_index, AXTree* out_tree) const;
 
+  // Support for returning every permutation of ignored nodes
+  // (other than the root, which is never ignored) per unique tree.
+  int IgnoredPermutationCountPerUniqueTree(int tree_index) const;
+  void BuildUniqueTreeWithIgnoredNodes(int tree_index,
+                                       int ignored_index,
+                                       AXTree* out_tree) const;
+
  private:
-  void BuildUniqueTreeWithSize(
-      int node_count, int tree_index, AXTree* out_tree) const;
+  void BuildUniqueTreeUpdate(int tree_index,
+                             AXTreeUpdate* out_tree_update) const;
+  void BuildUniqueTreeUpdateWithSize(int node_count,
+                                     int tree_index,
+                                     AXTreeUpdate* out_tree_update) const;
 
   int max_node_count_;
   bool permutations_;
@@ -61,3 +76,5 @@ class TreeGenerator {
 };
 
 }  // namespace ui
+
+#endif  // UI_ACCESSIBILITY_TREE_GENERATOR_H_
