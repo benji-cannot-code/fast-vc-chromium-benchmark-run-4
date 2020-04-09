9be5d2b72c0592f8f87b0171c20d9012a74852a6
Steven Bingler <bingler@chromium.org>
2020-04-09 16:07:25 +0000

    Add a inclusion context getter to SameSiteCookieContext
    Provide a function for consumers of the class to use to retrieve a
    context value when they need to determine a cookie's inclusion status.
    
    While this currently has no meaningful difference between accessing the
    context member directly it will be helpful when the schemeful
    context member is added so that callers will be returned the correct
    value depending on the state of the schemeful same-site feature.
    
    This CL also makes the member variables private.
    
    Bug: 1030938
    Change-Id: I29e30ab1266d1f8195483ae9fbe8e4e63b62e2d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140032
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757871}

--- net/cookies/cookie_util.cc
+++ net/cookies/cookie_util.cc
@@ -107,14 +107,15 @@ CookieOptions::SameSiteCookieContext ComputeSameSiteContext(
     // IsFirstParty().
     if (!initiator ||
         SiteForCookies::FromOrigin(initiator.value()).IsFirstParty(url)) {
-      same_site_type.context =
-          CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_STRICT;
+      same_site_type.set_context(
+          CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_STRICT);
     } else {
-      same_site_type.context =
-          CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_LAX;
+      same_site_type.set_context(
+          CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_LAX);
     }
   }
-  same_site_type.cross_schemeness = ComputeSchemeChange(url, site_for_cookies);
+  same_site_type.set_cross_schemeness(
+      ComputeSchemeChange(url, site_for_cookies));
   return same_site_type;
 }
 
@@ -450,10 +451,10 @@ CookieOptions::SameSiteCookieContext ComputeSameSiteContextForRequest(
   CookieOptions::SameSiteCookieContext same_site_context;
 
   if (attach_same_site_cookies) {
-    same_site_context.context =
-        CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_STRICT;
-    same_site_context.cross_schemeness =
-        ComputeSchemeChange(url, site_for_cookies);
+    same_site_context.set_context(
+        CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_STRICT);
+    same_site_context.set_cross_schemeness(
+        ComputeSchemeChange(url, site_for_cookies));
     return same_site_context;
   }
 
@@ -461,11 +462,11 @@ CookieOptions::SameSiteCookieContext ComputeSameSiteContextForRequest(
 
   // If the method is safe, the context is Lax. Otherwise, make a note that
   // the method is unsafe.
-  if (same_site_context.context ==
+  if (same_site_context.context() ==
           CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_LAX &&
       !net::HttpUtil::IsMethodSafe(http_method)) {
-    same_site_context.context = CookieOptions::SameSiteCookieContext::
-        ContextType::SAME_SITE_LAX_METHOD_UNSAFE;
+    same_site_context.set_context(CookieOptions::SameSiteCookieContext::
+                                      ContextType::SAME_SITE_LAX_METHOD_UNSAFE);
   }
 
   return same_site_context;
@@ -479,8 +480,8 @@ ComputeSameSiteContextForScriptGet(const GURL& url,
   if (attach_same_site_cookies) {
     CookieOptions::SameSiteCookieContext same_site_context(
         CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_STRICT);
-    same_site_context.cross_schemeness =
-        ComputeSchemeChange(url, site_for_cookies);
+    same_site_context.set_cross_schemeness(
+        ComputeSchemeChange(url, site_for_cookies));
     return same_site_context;
   }
   return ComputeSameSiteContext(url, site_for_cookies, initiator);
@@ -495,14 +496,14 @@ CookieOptions::SameSiteCookieContext ComputeSameSiteContextForResponse(
   // |initiator| is here in case it'll be decided to ignore |site_for_cookies|
   // for entirely browser-side requests (see https://crbug.com/958335).
   if (attach_same_site_cookies || site_for_cookies.IsFirstParty(url)) {
-    same_site_context.context =
-        CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_LAX;
+    same_site_context.set_context(
+        CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_LAX);
   } else {
-    same_site_context.context =
-        CookieOptions::SameSiteCookieContext::ContextType::CROSS_SITE;
+    same_site_context.set_context(
+        CookieOptions::SameSiteCookieContext::ContextType::CROSS_SITE);
   }
-  same_site_context.cross_schemeness =
-      ComputeSchemeChange(url, site_for_cookies);
+  same_site_context.set_cross_schemeness(
+      ComputeSchemeChange(url, site_for_cookies));
   return same_site_context;
 }
 
@@ -512,14 +513,14 @@ CookieOptions::SameSiteCookieContext ComputeSameSiteContextForScriptSet(
     bool attach_same_site_cookies) {
   CookieOptions::SameSiteCookieContext same_site_context;
   if (attach_same_site_cookies || site_for_cookies.IsFirstParty(url)) {
-    same_site_context.context =
-        CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_LAX;
+    same_site_context.set_context(
+        CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_LAX);
   } else {
-    same_site_context.context =
-        CookieOptions::SameSiteCookieContext::ContextType::CROSS_SITE;
+    same_site_context.set_context(
+        CookieOptions::SameSiteCookieContext::ContextType::CROSS_SITE);
   }
-  same_site_context.cross_schemeness =
-      ComputeSchemeChange(url, site_for_cookies);
+  same_site_context.set_cross_schemeness(
+      ComputeSchemeChange(url, site_for_cookies));
   return same_site_context;
 }
 
@@ -531,14 +532,14 @@ CookieOptions::SameSiteCookieContext ComputeSameSiteContextForSubresource(
   // If the URL is same-site as site_for_cookies it's same-site as all frames
   // in the tree from the initiator frame up --- including the initiator frame.
   if (attach_same_site_cookies || site_for_cookies.IsFirstParty(url)) {
-    same_site_context.context =
-        CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_STRICT;
+    same_site_context.set_context(
+        CookieOptions::SameSiteCookieContext::ContextType::SAME_SITE_STRICT);
   } else {
-    same_site_context.context =
-        CookieOptions::SameSiteCookieContext::ContextType::CROSS_SITE;
+    same_site_context.set_context(
+        CookieOptions::SameSiteCookieContext::ContextType::CROSS_SITE);
   }
-  same_site_context.cross_schemeness =
-      ComputeSchemeChange(url, site_for_cookies);
+  same_site_context.set_cross_schemeness(
+      ComputeSchemeChange(url, site_for_cookies));
   return same_site_context;
 }
 
