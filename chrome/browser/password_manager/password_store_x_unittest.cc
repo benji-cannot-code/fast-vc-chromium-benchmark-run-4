4095140fc05636c47c367422a757b0b1daea5500
Alan Cutter <alancutter@chromium.org>
2020-04-09 08:43:17 +0000

    Fix PasswordStoreOriginTest instatiation warning
    This CL fixes the unit_tests warning:
    The following parameterized test case is not instantiated: UninstantiatedTypeParamaterizedTestSuite<PasswordStoreOriginTest>
    
    Bug: 1045846
    Change-Id: Iae85d808ae25b83c9473225bcbb671d977573a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143098
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757772}

--- chrome/browser/password_manager/password_store_x_unittest.cc
+++ chrome/browser/password_manager/password_store_x_unittest.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_test_utils.h"
 #include "components/password_manager/core/browser/password_store_change.h"
 #include "components/password_manager/core/browser/password_store_consumer.h"
-#include "components/password_manager/core/browser/password_store_origin_unittest.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -139,7 +138,7 @@ TEST_F(PasswordStoreXTest, MigrationCompleted) {
   // Check the contents are still around.
   MockPasswordStoreConsumer consumer;
   EXPECT_CALL(consumer, OnGetPasswordStoreResultsConstRef(
-                            ElementsAre(Pointee(MakePasswordForm()))));
+                            testing::ElementsAre(Pointee(MakePasswordForm()))));
   store->GetAutofillableLogins(&consumer);
 
   WaitForPasswordStore();
