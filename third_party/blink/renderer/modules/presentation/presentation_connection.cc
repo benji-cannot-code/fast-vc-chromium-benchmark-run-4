0c61834b6dff345e0c40e26d87ce9227b878135a
Johann <johannkoenig@google.com>
2020-04-09 23:21:40 +0000

    Reland "presentation controller: remove execution context"
    This is a reland of ffcf2b75bc29d75ca439ff0e5688319f647e2575. It was
    reverted in a33a9505cf9b7059467c52984b9cd13ffa7a3d34 due to an alert
    triggering for heap allocations. After discussion it sounds like
    this was the straw the forced the camel to allocate a new page and
    had an outsize impact on heap size.
    
    Fixed: chromium:1068911
    
    Original change's description:
    > presentation controller: remove execution context
    >
    > The PresentationController class does not need
    > ExecutionContextLifecycleObserver since the mojo::Receiver was migrated
    > to HeapMojoReceiver. The replacement wrapper includes management of the
    > presentation_controller_receiver_.
    >
    > Change-Id: I1c96f897a7b604a23d3e7f0bd61a3b0598faa2f8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137162
    > Commit-Queue: Johann Koenig <johannkoenig@google.com>
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#756871}
    
    Change-Id: I5270d6ca2483aa2815aaea59cacb2a8f627e5595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2142876
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#758068}

--- third_party/blink/renderer/modules/presentation/presentation_connection.cc
+++ third_party/blink/renderer/modules/presentation/presentation_connection.cc
@@ -241,7 +241,7 @@ ControllerPresentationConnection* ControllerPresentationConnection::Take(
   DCHECK(request);
 
   auto* connection = MakeGarbageCollected<ControllerPresentationConnection>(
-      *controller->GetFrame(), controller, presentation_info.id,
+      *controller->GetSupplementable(), controller, presentation_info.id,
       presentation_info.url);
   controller->RegisterConnection(connection);
 
