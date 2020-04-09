e70877c8c8e5a2151792dbf48f345dc9ecaad6a3
Charlie Hu <chenleihu@google.com>
2020-04-09 21:39:59 +0000

    Use mocking feature in DocumentPolicyParserTest
    This CL makes DocumentPolicyParserTest use mocking feature,
    instead of real feature in document policy.
    
    Change-Id: Ib49b6b0f73f8a5fe85450104d3837f746039a9d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134580
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#758028}

--- third_party/blink/renderer/core/feature_policy/feature_policy_helper.h
+++ third_party/blink/renderer/core/feature_policy/feature_policy_helper.h
@@ -11,15 +11,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/feature_policy/feature_policy_feature.mojom-blink-forward.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
-#include "third_party/blink/renderer/platform/wtf/hash_set.h"
+#include "third_party/blink/renderer/platform/wtf/linked_hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
 using FeatureNameMap = HashMap<String, mojom::blink::FeaturePolicyFeature>;
+
+// TODO(crbug.com/1069021): Use WTF::HashSet as DocumentPolicyFeatureSet
+// container will cause unknown timeout in release build only.
 using DocumentPolicyFeatureSet =
-    HashSet<mojom::blink::DocumentPolicyFeature,
-            IntHash<mojom::blink::DocumentPolicyFeature>>;
+    LinkedHashSet<mojom::blink::DocumentPolicyFeature,
+                  IntHash<mojom::blink::DocumentPolicyFeature>>;
 
 class FeatureContext;
 
