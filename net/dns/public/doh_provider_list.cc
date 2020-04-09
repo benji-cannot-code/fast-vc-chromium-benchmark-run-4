42ddc299c39a157a28ae950c20c616e611f7b75f
Eric Orth <ericorth@chromium.org>
2020-04-09 22:55:18 +0000

    Update Cloudflare DoH privacy policy URL
    Bug: 878582,1060381
    Change-Id: I88445de4b7d19c4006c60d382a9c3c8ddc6fba21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144823
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Eric Orth <ericorth@chromium.org>
    Auto-Submit: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758055}

--- net/dns/public/doh_provider_list.cc
+++ net/dns/public/doh_provider_list.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/dns/public/doh_provider_list.h"
 
+#include <utility>
+
 #include "base/logging.h"
 #include "base/no_destructor.h"
 
@@ -87,8 +89,8 @@ const std::vector<DohProviderEntry>& GetDohProviderList() {
            "1dot1dot1dot1.cloudflare-dns.com"} /* dns_over_tls_hostnames */,
           "https://chrome.cloudflare-dns.com/dns-query",
           "Cloudflare (1.1.1.1)" /* ui_name */,
-          "https://developers.cloudflare.com/1.1.1.1/commitment-to-privacy/"
-          "privacy-policy/privacy-policy/" /* privacy_policy */,
+          "https://developers.cloudflare.com/1.1.1.1/privacy/"
+          "public-dns-resolver/" /* privacy_policy */,
           true /* display_globally */, {} /* display_countries */),
       DohProviderEntry("Comcast", base::nullopt /* provider_id_for_histogram */,
                        {"75.75.75.75", "75.75.76.76", "2001:558:feed::1",
