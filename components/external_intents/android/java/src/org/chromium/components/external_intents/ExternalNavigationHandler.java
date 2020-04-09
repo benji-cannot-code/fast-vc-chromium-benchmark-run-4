d75f92cbcfe518a0e571be83cbf53627246d6613
Colin Blundell <blundell@chromium.org>
2020-04-09 12:09:55 +0000

    [Android] Revert setting ExternalNavigationHandler DEBUG member to true
    I inadvertently landed a change that set this member to true rather
    than false :P. This landed in
    https://chromium-review.googlesource.com/c/chromium/src/+/2132385.
    
    Fortunately the above CL first landed in 84:
    https://storage.googleapis.com/chromium-find-releases-static/955.html#955b7118ab87d23cbb053608efa451ae9bd80d20
    Hence there doesn't need to be any question of whether this change would
    be worth merging to 83 or not.
    
    Bug: 1031465
    Change-Id: I49e0c780f512c8786770851fa56cdcbc03d66608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144051
    Reviewed-by: Mugdha Lakhani <nator@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757805}

--- components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalNavigationHandler.java
+++ components/external_intents/android/java/src/org/chromium/components/external_intents/ExternalNavigationHandler.java
@@ -49,7 +49,7 @@ public class ExternalNavigationHandler {
     private static final String TAG = "UrlHandler";
 
     // Enables debug logging on a local build.
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
 
     private static final String WTAI_URL_PREFIX = "wtai://wp/";
     private static final String WTAI_MC_URL_PREFIX = "wtai://wp/mc;";
