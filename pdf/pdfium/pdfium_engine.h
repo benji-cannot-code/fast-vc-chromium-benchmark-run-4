4be1768c1e55446dadf48b59a402980858cd6a99
Ankit Kumar <ankk@microsoft.com>
2020-04-09 23:35:34 +0000

    Allow focus to move across pages on tab events
    This CL adds tab handling in PDF. A tab event now allows the user to
    move from the last annotation on a page to first annotation on the next
    page.
    
    Unit tests have been added to validate the scenario.
    
    Bug: 989040
    Change-Id: I46106e88946bdc3e667c9897cbcba3ff418aecfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2061634
    Commit-Queue: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#758074}

--- pdf/pdfium/pdfium_engine.h
+++ pdf/pdfium/pdfium_engine.h
@@ -163,6 +163,11 @@ class PDFiumEngine : public PDFEngine,
   FPDF_DOCUMENT doc() const;
   FPDF_FORMHANDLE form() const;
 
+  // State transition when tabbing forward:
+  // None -> Document -> Page -> None (when focusable annotations on all pages
+  // are done).
+  enum class FocusElementType { kNone, kDocument, kPage };
+
  private:
   // This helper class is used to detect the difference in selection between
   // construction and destruction.  At destruction, it invalidates all the
@@ -210,6 +215,7 @@ class PDFiumEngine : public PDFEngine,
   };
 
   friend class FormFillerTest;
+  friend class PDFiumEngineTabbingTest;
   friend class PDFiumFormFiller;
   friend class PDFiumTestBase;
   friend class SelectionChangeInvalidator;
@@ -581,6 +587,15 @@ class PDFiumEngine : public PDFEngine,
   // Retrieves the version of the PDF (e.g. 1.4 or 2.0) as an enum.
   PdfVersion GetDocumentVersion() const;
 
+  // This is a layer between OnKeyDown() and actual tab handling to facilitate
+  // testing.
+  bool HandleTabEvent(uint32_t modifiers);
+
+  // Helper functions to handle tab events.
+  bool HandleTabEventWithModifiers(uint32_t modifiers);
+  bool HandleTabForward(uint32_t modifiers);
+  bool HandleTabBackward(uint32_t modifiers);
+
   PDFEngine::Client* const client_;
 
   // The current document layout.
@@ -681,6 +696,9 @@ class PDFiumEngine : public PDFEngine,
 
   // Timer for touch long press detection.
   base::OneShotTimer touch_timer_;
+
+  // The focus item type for the currently focused object.
+  FocusElementType focus_item_type_ = FocusElementType::kNone;
 
   // Holds the zero-based page index of the last page that had the focused
   // object.
