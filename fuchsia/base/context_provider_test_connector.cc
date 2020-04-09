0b347948819f0536252bade69f45c55237734dcf
Sergey Ulanov <sergeyu@chromium.org>
2020-04-09 22:46:45 +0000

    Reland: [Fuchsia] Cleanup cast_runner integration tests.
    Removed test-only methods from CastRunner, WebContentRunner and
    CastComponent that were previously used in
    cast_runner_integration_tests.
    
    1. Removed CastRunner and WebContentRunner constructors that take
    web.Context. Now context is always created in WebContentRunner.
    2. Removed test-only methods.
    3. Updated TestComponentContext to allow initialization with all
    services cloned from /svc.
    
    The tests now inject a script into the frame in order to test
    functionality that was previously tested with the test-only methods.
    
    Previous attempt to land this CL was reverted due to flake in
    CastRunnerIntegrationTest.CanRecreateContext test. The test kills
    WebEngine and was waiting Component termination and assumed that
    Context and ContextProvider were torn down as well, but they may
    be destroyed with some delay. To fix this issue this version of the
    CL adds explicit wait for Context destruction and removes
    ContextProvider caching from WebContentRunner.
    
    Bug: 1062351
    Change-Id: I18fa11e85d2194bba1ed3896167a64fac8a51c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143494
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758053}

--- fuchsia/base/context_provider_test_connector.cc
+++ fuchsia/base/context_provider_test_connector.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cr_fuchsia {
 
-fuchsia::web::ContextProviderPtr ConnectContextProvider(
+fidl::InterfaceHandle<fuchsia::io::Directory> StartWebEngineForTests(
     fidl::InterfaceRequest<fuchsia::sys::ComponentController>
         component_controller_request,
     const base::CommandLine& command_line) {
@@ -50,12 +50,16 @@ fuchsia::web::ContextProviderPtr ConnectContextProvider(
   launcher->CreateComponent(std::move(launch_info),
                             std::move(component_controller_request));
 
-  sys::ServiceDirectory web_engine_service_dir(
-      std::move(web_engine_services_dir));
+  return web_engine_services_dir;
+}
 
-  fuchsia::web::ContextProviderPtr context_provider;
-  web_engine_service_dir.Connect(context_provider.NewRequest());
-  return context_provider;
+fuchsia::web::ContextProviderPtr ConnectContextProvider(
+    fidl::InterfaceRequest<fuchsia::sys::ComponentController>
+        component_controller_request,
+    const base::CommandLine& command_line) {
+  sys::ServiceDirectory web_engine_service_dir(StartWebEngineForTests(
+      std::move(component_controller_request), command_line));
+  return web_engine_service_dir.Connect<fuchsia::web::ContextProvider>();
 }
 
 }  // namespace cr_fuchsia
