57cd133ba88a8d4aff4d31b38435dd06c95d37c3
Bo Liu <boliu@chromium.org>
2020-04-09 17:12:30 +0000

    weblayer: Factor out profile_disk_operations
    Factor out code that manipulate profile directories into its own file,
    to prepare for upcoming changes to now deleting profile data work.
    
    There are some behavior changes:
    
    The cache directory is created on the UI thread on ProfileImpl
    construction, rather than lazily when first requested. Cache directory
    is likely to be requested soon after profile creation, so should not
    affect performance much.
    
    The cache directory is now empty for incognito. The return value is
    never actually used for incognito profile, so this should not change
    anything.
    
    Bug: 1065585
    Change-Id: I06f86235be34f1bc30247004b57cf9c3762248f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140038
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757901}

--- weblayer/browser/profile_impl.h
+++ weblayer/browser/profile_impl.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
 #include "weblayer/browser/i18n_util.h"
+#include "weblayer/browser/profile_disk_operations.h"
 #include "weblayer/public/profile.h"
 
 #if defined(OS_ANDROID)
@@ -54,7 +55,7 @@ class ProfileImpl : public Profile {
   void DownloadsInitialized();
 
   // Path data is stored at, empty if off-the-record.
-  const base::FilePath& data_path() const { return data_path_; }
+  const base::FilePath& data_path() const { return info_.data_path; }
   DownloadDelegate* download_delegate() { return download_delegate_; }
 
   // Profile implementation:
@@ -108,9 +109,7 @@ class ProfileImpl : public Profile {
   // Callback when the system locale has been updated.
   void OnLocaleChanged();
 
-  const std::string name_;
-
-  base::FilePath data_path_;
+  ProfileInfo info_;
 
   std::unique_ptr<BrowserContextImpl> browser_context_;
 
