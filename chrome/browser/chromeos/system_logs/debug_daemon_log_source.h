1cc8edb6c205a446b5988cad29ac6c3a6e168753
Joon Ahn <joonbug@chromium.org>
2020-04-09 02:27:48 +0000

    logs: Implement size limit for user logs
    Impose 1MiB size limit for user logs and read the logs from back.
    
    Bug: chromium:1066205
    Change-Id: I4c7a2de6f93d87ee0a5a2295dd25b32ec372ac46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133427
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Commit-Queue: Joon Ahn <joonbug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757693}

--- chrome/browser/chromeos/system_logs/debug_daemon_log_source.h
+++ chrome/browser/chromeos/system_logs/debug_daemon_log_source.h
@@ -19,6 +19,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace system_logs {
 
+// Reads the file at |path| into |contents| and returns true on success and
+// false on error. For security reasons a |path| containing path traversal
+// components ('..') is treated as a read error and |contents| is not changed.
+// When the file exceeds |max_size| function returns true with the last
+// |max_size| bytes from the file.
+bool ReadEndOfFile(const base::FilePath& path,
+                   std::string* contents,
+                   size_t max_size);
+
 // Gathers log data from Debug Daemon.
 class DebugDaemonLogSource : public SystemLogsSource {
  public:
