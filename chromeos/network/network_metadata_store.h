86f9cad426ba00bb80b4e2958677dbaa878532a4
Jon Mann <jonmann@chromium.org>
2020-04-09 02:43:25 +0000

    [NetworkMetadataStore] Track networks owned by user.
    This allows us to know which shared networks are
    configured by the logged in user.
    
    Bug: 966270
    Change-Id: Id9c6ee003a3ff7f024ace0e3a30309263cd3b98a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2142614
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757700}

--- chromeos/network/network_metadata_store.h
+++ chromeos/network/network_metadata_store.h
@@ -53,6 +53,8 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) NetworkMetadataStore
   void ConnectSucceeded(const std::string& service_path) override;
 
   // NetworkConfigurationObserver::
+  void OnConfigurationCreated(const std::string& service_path,
+                              const std::string& guid) override;
   void OnConfigurationModified(const std::string& service_path,
                                const std::string& guid,
                                base::DictionaryValue* set_properties) override;
@@ -68,6 +70,9 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) NetworkMetadataStore
 
   // Networks which were added directly from sync data will return true.
   bool GetIsConfiguredBySync(const std::string& network_guid);
+
+  // Networks which were created by the logged in user will return true.
+  bool GetIsCreatedByUser(const std::string& network_guid);
 
   // Manage observers.
   void AddObserver(NetworkMetadataObserver* observer);
