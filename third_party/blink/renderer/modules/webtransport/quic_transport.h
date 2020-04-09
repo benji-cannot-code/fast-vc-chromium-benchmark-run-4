c80ab46a7bba559f50260870d5759b899318125a
Minoru Chikamune <chikamune@chromium.org>
2020-04-09 00:23:32 +0000

    Migrate QuicTransport to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: If151197f5e3061aa1e46484e5720892041367b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137200
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757658}

--- third_party/blink/renderer/modules/webtransport/quic_transport.h
+++ third_party/blink/renderer/modules/webtransport/quic_transport.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/util/type_safety/pass_key.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
-#include "mojo/public/cpp/bindings/receiver.h"
-#include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/system/data_pipe.h"
 #include "services/network/public/mojom/quic_transport.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
@@ -22,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/heap_allocator.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/forward.h"
 
@@ -99,6 +99,8 @@ class MODULES_EXPORT QuicTransport final
   class DatagramUnderlyingSource;
   class ReceivedStreamsUnderlyingSource;
 
+  QuicTransport(ScriptState*, const String& url, ExecutionContext* context);
+
   void Init(const String& url, ExceptionState&);
 
   // Reset the QuicTransport object and all associated streams.
@@ -133,11 +135,12 @@ class MODULES_EXPORT QuicTransport final
               WTF::UnsignedWithZeroKeyHashTraits<uint32_t>>
       stream_map_;
 
-  mojo::Remote<network::mojom::blink::QuicTransport> quic_transport_;
-  mojo::Receiver<network::mojom::blink::QuicTransportHandshakeClient>
-      handshake_client_receiver_{this};
-  mojo::Receiver<network::mojom::blink::QuicTransportClient> client_receiver_{
-      this};
+  HeapMojoRemote<network::mojom::blink::QuicTransport> quic_transport_;
+  HeapMojoReceiver<network::mojom::blink::QuicTransportHandshakeClient,
+                   QuicTransport>
+      handshake_client_receiver_;
+  HeapMojoReceiver<network::mojom::blink::QuicTransportClient, QuicTransport>
+      client_receiver_;
   Member<ScriptPromiseResolver> ready_resolver_;
   ScriptPromise ready_;
   Member<ScriptPromiseResolver> closed_resolver_;
