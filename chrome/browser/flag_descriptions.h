aa093cd6fa6940cbadd3b66455b3e373efb7abe8
Adrienne Walker <enne@chromium.org>
2020-04-09 03:09:07 +0000

    Add chrome://flags entry for disabling AppCache
    AppCache is deprecated and is being removed in a future version.  This
    is to assist developers testing a future without AppCache.
    
    Bug: 1055010
    Change-Id: I6770a160ab29e8914bb0e97be8cc9eb5e2912450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139366
    Commit-Queue: enne <enne@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Auto-Submit: enne <enne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757706}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -97,6 +97,9 @@ extern const char kUseMessagesStagingUrlDescription[];
 extern const char kAndroidPictureInPictureAPIName[];
 extern const char kAndroidPictureInPictureAPIDescription[];
 
+extern const char kAppCacheName[];
+extern const char kAppCacheDescription[];
+
 extern const char kAutofillAlwaysReturnCloudTokenizedCardName[];
 extern const char kAutofillAlwaysReturnCloudTokenizedCardDescription[];
 
