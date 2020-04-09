191ae077692178329010b12eb22728270255dfbf
rbpotter <rbpotter@chromium.org>
2020-04-09 00:02:24 +0000

    Settings: Migrate a11y tests to Polymer 3
    Bug: 1026426
    Change-Id: I328de7bfaf6d6f5501e0fc386dd64e901fa387e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141129
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757651}

---
+++ chrome/test/data/webui/settings/a11y/about_a11y_v3_test.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {routes} from 'chrome://settings/settings.js';
+
+// Set the URL to be that of specific route to load upon injecting
+// settings-ui. Simply calling
+// settings.Router.getInstance().navigateTo(route) prevents use of mock APIs
+// for fake data.
+window.history.pushState('object or string', 'Test', routes.ABOUT.path);
+const settingsUi = document.createElement('settings-ui');
+document.body.appendChild(settingsUi);
+document.dispatchEvent(new CustomEvent('a11y-setup-complete'));
