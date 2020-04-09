c28ee421422246e58bcbebf0699154c12396eea3
Koji Ishii <kojii@chromium.org>
2020-04-09 03:55:29 +0000

    [FragmentItem] Enable simplified layout
    |NGSimplifiedLayoutAlgorithm| was once disabled for
    FragmentItem to focus on working on the correctness.
    
    This patch re-enables it for the performance.
    
    Bug: 982194
    Change-Id: I1bf2d7dc1734c2163a525732a181aa4bdfbbcd0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134185
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#757716}

--- third_party/blink/renderer/core/layout/ng/ng_simplified_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/ng_simplified_layout_algorithm.h
@@ -45,6 +45,8 @@ class CORE_EXPORT NGSimplifiedLayoutAlgorithm
 
   scoped_refptr<const NGLayoutResult> Layout() override;
 
+  NOINLINE scoped_refptr<const NGLayoutResult> LayoutWithItemsBuilder();
+
  private:
   void HandleOutOfFlowPositioned(const NGBlockNode&);
 
