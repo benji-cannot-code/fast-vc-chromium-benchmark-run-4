7852ee97062042d9c50bdc27199f490bebba11b1
Aaron Leventhal <aleventhal@chromium.org>
2020-04-10 01:15:25 +0000

    Don't reuse old ids in table info
    Nodes that are now ignored should not be considered cells.
    
    Bug: 1061248
    Change-Id: Ic0118c58a696dc52f6528e7a9e1da34cc1eef885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144423
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758101}

--- ui/accessibility/ax_table_info.cc
+++ ui/accessibility/ax_table_info.cc
@@ -158,6 +158,8 @@ void AXTableInfo::ClearVectors() {
   unique_cell_ids.clear();
   cell_data_vector.clear();
   row_nodes.clear();
+  cell_id_to_index.clear();
+  row_id_to_index.clear();
 }
 
 void AXTableInfo::BuildCellDataVectorFromRowAndCellNodes(
