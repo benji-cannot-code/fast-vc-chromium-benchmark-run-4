438d80d99b2c78a34e0e20e09832d4491bf507d4
Alan Cutter <alancutter@chromium.org>
2020-04-10 18:42:19 +0000

    Instantiate EndToEndRemoteTest tests
    This CL fixes the warning in mojo_unittests:
    The following parameterized test case is not instantiated: UninstantiatedParamaterizedTestSuite<EndToEndRemoteTest>
    
    Bug: 1045846
    Change-Id: Ic67c23f9cdbc3544c857777451691cecc62de92c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144793
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#758233}

--- mojo/public/cpp/bindings/tests/remote_unittest.cc
+++ mojo/public/cpp/bindings/tests/remote_unittest.cc
@@ -1112,6 +1112,7 @@ TEST_P(RemoteTest, RemoteSet) {
 }
 
 INSTANTIATE_MOJO_BINDINGS_TEST_SUITE_P(RemoteTest);
+INSTANTIATE_MOJO_BINDINGS_TEST_SUITE_P(EndToEndRemoteTest);
 
 }  // namespace
 }  // namespace remote_unittest
