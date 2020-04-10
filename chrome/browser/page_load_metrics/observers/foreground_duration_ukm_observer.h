e0aeac0eaeb6ca65ecfb36ef5b0e945001dc9ccf
Hongbo Song <hbsong@google.com>
2020-04-10 18:01:07 +0000

    Record PageForegroundSession Input Delay UKM.
    We need to know total input delay and number of inputs for each page visit. To better align with `PageForegroundSession.ForegroundDuration`, we plan to add TotalInputDelay, TotalAdjustedInputDelay,  NumInputEvents UKM for each PageForegroundSession.
    
    UKM privacy review: https://docs.google.com/document/d/1F4ZImXniH_YZA6Z4FcONHbwe-ze0q0Bp0W6RKxDf-Kg/edit?usp=sharing
    
    Change-Id: I010d8b7b35af3d3f611f8a29542dd10956c2ab7f
    Bug: 1069304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144362
    Commit-Queue: Hongbo Song <hbsong@google.com>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758228}

--- chrome/browser/page_load_metrics/observers/foreground_duration_ukm_observer.h
+++ chrome/browser/page_load_metrics/observers/foreground_duration_ukm_observer.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer.h"
+#include "components/page_load_metrics/common/page_load_metrics.mojom.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
 // Observer responsible for appending previews information to the PLM UKM
@@ -36,7 +38,10 @@ class ForegroundDurationUKMObserver
   bool currently_in_foreground_ = false;
   base::TimeTicks last_time_shown_;
   ukm::SourceId source_id_ = ukm::kInvalidSourceId;
+  page_load_metrics::mojom::InputTiming last_page_input_timing_;
   void RecordUkmIfInForeground(base::TimeTicks end_time);
+  void RecordInputTimingMetrics(
+      ukm::builders::PageForegroundSession* ukm_builder);
 
   DISALLOW_COPY_AND_ASSIGN(ForegroundDurationUKMObserver);
 };
