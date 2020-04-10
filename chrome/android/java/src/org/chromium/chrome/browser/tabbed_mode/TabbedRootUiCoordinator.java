1cee068d65dd36917dc54b3fe0ae85034247871b
Rakib Hasan <rmhasan@google.com>
2020-04-10 22:03:04 +0000

    Revert "Remove ImmersiveModeManager and related code."
    This reverts commit b5fff4e4ce02e1408654583de4f67bd095c9885e.
    
    Reason for revert: Causing android perf builders to fail compiling Clank. crbug.com/1069913
    
    Original change's description:
    > Remove ImmersiveModeManager and related code.
    > 
    > BUG=949819
    > 
    > Change-Id: I09e3546e3e14f469e3a68fcddfd840ec3fd76e8e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140928
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Commit-Queue: Theresa  <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#758248}
    
    TBR=twellington@chromium.org,mdjones@chromium.org
    
    Change-Id: I2dbc931d813fbbdf804f505632508caff373e439
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 949819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144470
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Commit-Queue: Rakib Hasan <rmhasan@google.com>
    Cr-Commit-Position: refs/heads/master@{#758306}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -21,6 +21,7 @@ import org.chromium.base.supplier.Supplier;
 import org.chromium.base.task.PostTask;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ActivityTabProvider;
+import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.compositor.bottombar.ephemeraltab.EphemeralTabCoordinator;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManager;
@@ -44,6 +45,7 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tasks.tab_management.TabUiFeatureUtilities;
 import org.chromium.chrome.browser.toolbar.ToolbarButtonInProductHelpController;
 import org.chromium.chrome.browser.toolbar.bottom.BottomToolbarConfiguration;
+import org.chromium.chrome.browser.ui.ImmersiveModeManager;
 import org.chromium.chrome.browser.ui.RootUiCoordinator;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuHandler;
 import org.chromium.chrome.browser.ui.tablet.emptybackground.EmptyBackgroundViewWrapper;
@@ -61,6 +63,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator implements Native
     private static final int STATUS_INDICATOR_WAIT_BEFORE_HIDE_DURATION_MS = 2000;
 
     private final ObservableSupplierImpl<EphemeralTabCoordinator> mEphemeralTabCoordinatorSupplier;
+    private @Nullable ImmersiveModeManager mImmersiveModeManager;
     private TabbedSystemUiCoordinator mSystemUiCoordinator;
     private @Nullable EmptyBackgroundViewWrapper mEmptyBackgroundViewWrapper;
 
@@ -92,6 +95,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator implements Native
 
     @Override
     public void destroy() {
+        if (mImmersiveModeManager != null) mImmersiveModeManager.destroy();
         if (mSystemUiCoordinator != null) mSystemUiCoordinator.destroy();
         if (mEmptyBackgroundViewWrapper != null) mEmptyBackgroundViewWrapper.destroy();
 
@@ -112,8 +116,15 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator implements Native
     public void onPostInflationStartup() {
         super.onPostInflationStartup();
 
+        mImmersiveModeManager = AppHooks.get().createImmersiveModeManager(
+                mActivity.getWindow().getDecorView().findViewById(android.R.id.content));
         mSystemUiCoordinator = new TabbedSystemUiCoordinator(mActivity.getWindow(),
-                mActivity.getTabModelSelector(), mActivity.getOverviewModeBehaviorSupplier());
+                mActivity.getTabModelSelector(), mImmersiveModeManager,
+                mActivity.getOverviewModeBehaviorSupplier());
+
+        if (mImmersiveModeManager != null) {
+            getToolbarManager().setImmersiveModeManager(mImmersiveModeManager);
+        }
     }
 
     @Override
