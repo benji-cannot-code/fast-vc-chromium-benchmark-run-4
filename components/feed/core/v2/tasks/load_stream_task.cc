7dafb7a14e14fde177faea9ba59efe0742df3d2c
Ian Wells <iwells@chromium.org>
2020-04-10 20:48:00 +0000

    Fix parsing of feed network responses
    * Encodes request payload as base64
    * Skips the leading varint message size before trying to parse the response
    
    Bug: 1044139
    Change-Id: I4736625ffdec1ba6e9b6ad0d4b64265183bdedc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2142957
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758280}

--- components/feed/core/v2/tasks/load_stream_task.cc
+++ components/feed/core/v2/tasks/load_stream_task.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/time/clock.h"
 #include "base/time/time.h"
+#include "components/feed/core/proto/v2/wire/capability.pb.h"
 #include "components/feed/core/proto/v2/wire/client_info.pb.h"
 #include "components/feed/core/proto/v2/wire/feed_request.pb.h"
 #include "components/feed/core/proto/v2/wire/request.pb.h"
@@ -78,6 +79,8 @@ void LoadStreamTask::LoadFromStoreComplete(
 
   feed_request.mutable_feed_query()->set_reason(
       feedwire::FeedQuery::MANUAL_REFRESH);
+  request.mutable_feed_request()->add_client_capability(
+      feedwire::Capability::BASE_UI);
   if (!result.consistency_token.empty()) {
     feed_request.mutable_consistency_token()->set_token(
         result.consistency_token);
