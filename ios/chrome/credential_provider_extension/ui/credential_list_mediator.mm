acc29cd22f8dd6bf574f2c02d2d0299d4433a202
David Jean <djean@chromium.org>
2020-04-10 14:22:49 +0000

    [ios] Add search to cpe list mediator.
    Bug: 1045454
    Change-Id: I187eec6794ba509f32560c0d422124a628ee4d54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144176
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758195}

--- ios/chrome/credential_provider_extension/ui/credential_list_mediator.mm
+++ ios/chrome/credential_provider_extension/ui/credential_list_mediator.mm
@@ -33,6 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The extension context in which the credential list was started.
 @property(nonatomic, weak) ASCredentialProviderExtensionContext* context;
 
+// List of suggested credentials.
+@property(nonatomic, copy) NSArray<id<Credential>>* suggestedCredentials;
+
+// List of all credentials.
+@property(nonatomic, copy) NSArray<id<Credential>>* allCredentials;
+
 @end
 
 @implementation CredentialListMediator
@@ -57,12 +63,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)fetchCredentials {
   // TODO(crbug.com/1045454): Implement ordering and suggestions.
-  NSArray<id<Credential>>* allCredentials = self.credentialStore.credentials;
-  if (!allCredentials.count) {
+  self.allCredentials = self.credentialStore.credentials;
+  self.suggestedCredentials = nil;
+  if (!self.allCredentials.count) {
     [self.UIHandler showEmptyCredentials];
     return;
   }
-  [self.consumer presentSuggestedPasswords:nil allPasswords:allCredentials];
+  [self.consumer presentSuggestedPasswords:self.suggestedCredentials
+                              allPasswords:self.allCredentials];
 }
 
 #pragma mark - CredentialListConsumerDelegate
@@ -76,7 +84,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)updateResultsWithFilter:(NSString*)filter {
-  // TODO(crbug.com/1045454): Implement this method.
+  NSMutableArray<id<Credential>>* suggested = [[NSMutableArray alloc] init];
+  if (self.suggestedCredentials.count > 0) {
+    for (id<Credential> credential in self.suggestedCredentials) {
+      if ([filter length] == 0 ||
+          [credential.serviceName localizedStandardContainsString:filter] ||
+          [credential.user localizedStandardContainsString:filter]) {
+        [suggested addObject:credential];
+      }
+    }
+  }
+  NSMutableArray<id<Credential>>* all = [[NSMutableArray alloc] init];
+  if (self.allCredentials.count > 0) {
+    for (id<Credential> credential in self.allCredentials) {
+      if ([filter length] == 0 ||
+          [credential.serviceName localizedStandardContainsString:filter] ||
+          [credential.user localizedStandardContainsString:filter]) {
+        [all addObject:credential];
+      }
+    }
+  }
+  [self.consumer presentSuggestedPasswords:suggested allPasswords:all];
 }
 
 - (void)showDetailsForCredential:(id<Credential>)credential {
