95143cb3796562bd945ad1de79e4cb7747fe704e
Esmael El-Moslimany <aee@chromium.org>
2020-04-10 02:24:10 +0000

    WebUI NTP: background image selection
    Frontend for selecting and showing the selection when no background
    and a background image is selected.
    
    
    Bug: 1032328
    Change-Id: Iff7864dd54ef0a2eb7b7bfb186178ef6d4d0b30f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2136035
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758113}

--- chrome/browser/resources/new_tab_page/customize_backgrounds.js
+++ chrome/browser/resources/new_tab_page/customize_backgrounds.js
@@ -8,6 +8,7 @@ import './untrusted_iframe.js';
 
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {BrowserProxy} from './browser_proxy.js';
+import {BackgroundSelection, BackgroundSelectionType} from './customize_dialog.js';
 
 /** Element that lets the user configure the background. */
 class CustomizeBackgroundsElement extends PolymerElement {
@@ -21,6 +22,13 @@ class CustomizeBackgroundsElement extends PolymerElement {
 
   static get properties() {
     return {
+      /** @type {!BackgroundSelection} */
+      backgroundSelection: {
+        type: Object,
+        value: () => ({type: BackgroundSelectionType.NO_SELECTION}),
+        notify: true,
+      },
+
       /** @private {newTabPage.mojom.BackgroundCollection} */
       selectedCollection: {
         notify: true,
@@ -28,6 +36,9 @@ class CustomizeBackgroundsElement extends PolymerElement {
         type: Object,
         value: null,
       },
+
+      /** @type {!newTabPage.mojom.Theme} */
+      theme: Object,
 
       /** @private {!Array<!newTabPage.mojom.BackgroundCollection>} */
       collections_: Array,
@@ -39,10 +50,56 @@ class CustomizeBackgroundsElement extends PolymerElement {
 
   constructor() {
     super();
-    BrowserProxy.getInstance().handler.getBackgroundCollections().then(
-        ({collections}) => {
-          this.collections_ = collections;
-        });
+    /** @private {newTabPage.mojom.PageHandlerRemote} */
+    this.pageHandler_ = BrowserProxy.getInstance().handler;
+    this.pageHandler_.getBackgroundCollections().then(({collections}) => {
+      this.collections_ = collections;
+    });
+  }
+
+  /**
+   * @return {string}
+   * @private
+   */
+  getNoBackgroundClass_() {
+    switch (this.backgroundSelection.type) {
+      case BackgroundSelectionType.NO_BACKGROUND:
+        return 'selected';
+      case BackgroundSelectionType.NO_SELECTION:
+        return this.theme && !this.theme.backgroundImageUrl &&
+                !this.theme.dailyRefreshCollectionId ?
+            'selected' :
+            '';
+      case BackgroundSelectionType.IMAGE:
+      case BackgroundSelectionType.DAILY_REFRESH:
+      default:
+        return '';
+    }
+  }
+
+  /**
+   * @param {number} index
+   * @return {string}
+   * @private
+   */
+  getImageSelectedClass_(index) {
+    const {url} = this.images_[index].imageUrl;
+    switch (this.backgroundSelection.type) {
+      case BackgroundSelectionType.IMAGE:
+        return this.backgroundSelection.image.imageUrl.url === url ?
+            'selected' :
+            '';
+      case BackgroundSelectionType.NO_SELECTION:
+        return this.theme && this.theme.backgroundImageUrl &&
+                this.theme.backgroundImageUrl.url === url &&
+                !this.theme.dailyRefreshCollectionId ?
+            'selected' :
+            '';
+      case BackgroundSelectionType.NO_BACKGROUND:
+      case BackgroundSelectionType.DAILY_REFRESH:
+      default:
+        return '';
+    }
   }
 
   /**
@@ -54,15 +111,29 @@ class CustomizeBackgroundsElement extends PolymerElement {
   }
 
   /** @private */
+  onDefaultClick_() {
+    this.backgroundSelection = {type: BackgroundSelectionType.NO_BACKGROUND};
+  }
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onImageClick_(e) {
+    this.backgroundSelection = {
+      type: BackgroundSelectionType.IMAGE,
+      image: this.$.imagesRepeat.itemForElement(e.target),
+    };
+  }
+
+  /** @private */
   async onSelectedCollectionChange_() {
     this.images_ = [];
     if (!this.selectedCollection) {
       return;
     }
     const collectionId = this.selectedCollection.id;
-    const {images} =
-        await BrowserProxy.getInstance().handler.getBackgroundImages(
-            collectionId);
+    const {images} = await this.pageHandler_.getBackgroundImages(collectionId);
     // We check the IDs match since the user may have already moved to a
     // different collection before the results come back.
     if (!this.selectedCollection ||
