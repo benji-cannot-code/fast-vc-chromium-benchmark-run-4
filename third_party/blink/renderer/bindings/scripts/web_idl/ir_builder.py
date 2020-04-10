019ed8df0717febafe5ef77683f91f3953e6e255
Yuki Shiino <yukishiino@chromium.org>
2020-04-10 08:39:02 +0000

    IDL compiler: Fix special operations
    Stringifier.IR and IndexedAndNamedProperties.IR are not updated
    during the compilation, so these IRs shouldn't be used at all.
    
    Bug: 839389
    Change-Id: Id74cb0f310cd234794f22a675a281ee3b078be42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141500
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758166}

--- third_party/blink/renderer/bindings/scripts/web_idl/ir_builder.py
+++ third_party/blink/renderer/bindings/scripts/web_idl/ir_builder.py
@@ -21,12 +21,10 @@ from .extended_attribute import ExtendedAttribute
 from .extended_attribute import ExtendedAttributes
 from .idl_type import IdlTypeFactory
 from .includes import Includes
-from .interface import IndexedAndNamedProperties
 from .interface import Interface
 from .interface import Iterable
 from .interface import Maplike
 from .interface import Setlike
-from .interface import Stringifier
 from .literal_constant import LiteralConstant
 from .namespace import Namespace
 from .operation import Operation
@@ -100,7 +98,7 @@ class _IRBuilder(object):
 
         child_nodes = list(node.GetChildren())
         inherited = self._take_inheritance(child_nodes)
-        stringifier = self._take_stringifier(child_nodes)
+        stringifier_members = self._take_stringifier(child_nodes)
         iterable = self._take_iterable(child_nodes)
         maplike = self._take_maplike(child_nodes)
         setlike = self._take_setlike(child_nodes)
@@ -112,11 +110,12 @@ class _IRBuilder(object):
                 child, interface_identifier=identifier)
             for child in child_nodes
         ]
+        if stringifier_members:
+            members.extend(filter(None, stringifier_members))
         attributes = []
         constants = []
         constructors = []
         operations = []
-        indexed_and_named_property_operations = []
         for member in members:
             if isinstance(member, Attribute.IR):
                 attributes.append(member)
@@ -126,23 +125,10 @@ class _IRBuilder(object):
                 constructors.append(member)
             elif isinstance(member, Operation.IR):
                 operations.append(member)
-                if member.is_getter or member.is_setter or member.is_deleter:
-                    indexed_and_named_property_operations.append(member)
             else:
                 assert False
 
         named_constructors = self._build_named_constructors(node)
-
-        indexed_and_named_properties = None
-        if indexed_and_named_property_operations:
-            indexed_and_named_properties = IndexedAndNamedProperties.IR(
-                indexed_and_named_property_operations,
-                self._build_debug_info(node))
-
-        if stringifier:
-            operations.append(stringifier.operation)
-            if stringifier.attribute:
-                attributes.append(stringifier.attribute)
 
         return Interface.IR(
             identifier=identifier,
@@ -154,8 +140,6 @@ class _IRBuilder(object):
             constructors=constructors,
             named_constructors=named_constructors,
             operations=operations,
-            indexed_and_named_properties=indexed_and_named_properties,
-            stringifier=stringifier,
             iterable=iterable,
             maplike=maplike,
             setlike=setlike,
@@ -632,11 +616,11 @@ class _IRBuilder(object):
                 component=self._component,
                 debug_info=self._build_debug_info(node))
         operation.is_stringifier = True
-
-        return Stringifier.IR(
-            operation=operation,
-            attribute=attribute,
-            debug_info=self._build_debug_info(node))
+        if attribute:
+            operation.stringifier_attribute = attribute.identifier
+            return (operation, attribute)
+        else:
+            return (operation, )
 
     def _build_type(self,
                     node,
