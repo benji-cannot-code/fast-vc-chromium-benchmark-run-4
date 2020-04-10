d93cb5d4566321a8ac89b935f149400b260abc99
Jaeyong Bae <jdragon.bae@gmail.com>
2020-04-10 14:07:10 +0000

    [Spellcheck] Remove spelling markers when readonly, disabled
    This patch means removing spelling markers
    if the form element is marked as readonly and disabled.
    
    The spec seems pretty clear regarding this [1]:
    The value of textarea elements that do not have a readonly attribute
    and that are not disabled.
    
    [1] https://html.spec.whatwg.org/multipage/interaction.html#spelling-and-grammar-checking
    
    Bug: 687949
    Change-Id: I716d1607d12a603c821211cc0b1933d05b9fdda4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2120992
    Commit-Queue: Jaeyong Bae <jdragon.bae@gmail.com>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758191}

--- third_party/blink/renderer/core/html/forms/text_control_element.cc
+++ third_party/blink/renderer/core/html/forms/text_control_element.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/selection_template.h"
 #include "third_party/blink/renderer/core/editing/serializers/serialization.h"
 #include "third_party/blink/renderer/core/editing/set_selection_options.h"
+#include "third_party/blink/renderer/core/editing/spellcheck/spell_checker.h"
 #include "third_party/blink/renderer/core/editing/text_affinity.h"
 #include "third_party/blink/renderer/core/editing/visible_position.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -774,6 +775,13 @@ void TextControlElement::ParseAttribute(
              params.name == html_names::kDisabledAttr) {
     DisabledOrReadonlyAttributeChanged(params.name);
     HTMLFormControlElementWithState::ParseAttribute(params);
+    if (params.new_value.IsNull())
+      return;
+
+    if (HTMLElement* inner_editor = InnerEditorElement()) {
+      if (auto* frame = GetDocument().GetFrame())
+        frame->GetSpellChecker().RemoveSpellingAndGrammarMarkers(*inner_editor);
+    }
   } else {
     HTMLFormControlElementWithState::ParseAttribute(params);
   }
