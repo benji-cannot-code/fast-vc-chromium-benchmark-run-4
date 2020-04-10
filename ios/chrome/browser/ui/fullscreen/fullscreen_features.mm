443020c35913939ba44d3e1a2e0c8430c397b2ab
Stepan Khapugin <stkhapugin@chromium.org>
2020-04-10 19:04:05 +0000

    [multiball] Force browser-based fullscreen in multiwindow.
    Forces ShouldScopeFullscreenControllerToBrowser to be enabled when in
    multiwindow. This greatly improves usability with multiwindow.
    
    Bug: 1069752, 1060653
    Change-Id: I4521b4814ebd42aaadbe27c5252a8ef710928d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144108
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758238}

--- ios/chrome/browser/ui/fullscreen/fullscreen_features.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_features.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#import "ios/chrome/browser/ui/util/multi_window_support.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -26,6 +27,10 @@ bool ShouldUseSmoothScrolling() {
 }
 
 bool ShouldScopeFullscreenControllerToBrowser() {
+  if (IsMultiwindowSupported()) {
+    return true;
+  }
+
   return base::FeatureList::IsEnabled(kFullscreenControllerBrowserScoped);
 }
 
