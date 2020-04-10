d1ed8fafbdecbe5bc9874269553131b08e052754
Nohemi Fernandez <fernandex@chromium.org>
2020-04-10 08:11:37 +0000

    [iOS] Add ClearSyncedData experiment to field trial testing.
    Updates EarlGrey tests that are impacted by the new default signout UI.
    
    Bug: 1005509
    Change-Id: I688c15b7b3a7b77be6da9e87d2047dd831818aff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139708
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758162}

--- ios/chrome/test/earl_grey/chrome_matchers.mm
+++ ios/chrome/test/earl_grey/chrome_matchers.mm
@@ -196,6 +196,10 @@ id<GREYMatcher> SignOutAccountsButton() {
   return [ChromeMatchersAppInterface signOutAccountsButton];
 }
 
+id<GREYMatcher> SignOutAndClearDataAccountsButton() {
+  return [ChromeMatchersAppInterface signOutAndClearDataAccountsButton];
+}
+
 id<GREYMatcher> ClearBrowsingDataCell() {
   return [ChromeMatchersAppInterface clearBrowsingDataCell];
 }
