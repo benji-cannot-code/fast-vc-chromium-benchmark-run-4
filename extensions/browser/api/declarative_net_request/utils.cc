268bba0436d8bf86b37e5e2ffb357ddc85252077
Karandeep Bhatia <karandeepb@chromium.org>
2020-04-11 10:27:45 +0000

    DNR: Simplify ClearRendererCacheOnNavigation.
    Remove the thread check in ClearRendererCacheOnNavigation. This should
    always be called on the UI thread.
    
    BUG=696822
    
    Change-Id: I25f0059b43dd25bcabf3f52fa1c645bd5a211136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2145699
    Auto-Submit: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758397}

--- extensions/browser/api/declarative_net_request/utils.cc
+++ extensions/browser/api/declarative_net_request/utils.cc
@@ -17,10 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
-#include "base/task/post_task.h"
 #include "components/url_pattern_index/url_pattern_index.h"
 #include "components/web_cache/browser/web_cache_manager.h"
-#include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "extensions/browser/api/declarative_net_request/constants.h"
 #include "extensions/browser/api/declarative_net_request/flat/extension_ruleset_generated.h"
@@ -67,10 +65,6 @@ int GetIndexedRulesetFormatVersion() {
 std::string GetVersionHeader() {
   return base::StringPrintf("---------Version=%d",
                             GetIndexedRulesetFormatVersion());
-}
-
-void ClearRendererCacheOnUI() {
-  web_cache::WebCacheManager::GetInstance()->ClearCacheOnNavigation();
 }
 
 }  // namespace
@@ -158,14 +152,9 @@ bool PersistIndexedRuleset(const base::FilePath& path,
   return true;
 }
 
-// Helper to clear each renderer's in-memory cache the next time it navigates.
 void ClearRendererCacheOnNavigation() {
-  if (content::BrowserThread::CurrentlyOn(content::BrowserThread::UI)) {
-    ClearRendererCacheOnUI();
-  } else {
-    base::PostTask(FROM_HERE, {content::BrowserThread::UI},
-                   base::BindOnce(&ClearRendererCacheOnUI));
-  }
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  web_cache::WebCacheManager::GetInstance()->ClearCacheOnNavigation();
 }
 
 void LogReadDynamicRulesStatus(ReadJSONRulesResult::Status status) {
