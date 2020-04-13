4fd6e584b01df1231c45dfb8b1b52367fd97357f
Koji Ishii <kojii@chromium.org>
2020-04-13 04:53:13 +0000

    [FragmentItem] Make NGFragmentItems flexible array
    This patch changes |NGFragmentItems| to flexible array,
    saving the overhead of |Vector| and possible unused capacity.
    
    Bug: 982194
    Change-Id: I824ab7cfba8e707a2e8d41e2d54cbd85dee6f111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2142888
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758461}

--- third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items_builder.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items_builder.h
@@ -26,6 +26,8 @@ class CORE_EXPORT NGFragmentItemsBuilder {
  public:
   NGFragmentItemsBuilder(NGBoxFragmentBuilder* box_builder) {}
 
+  wtf_size_t Size() const { return items_.size(); }
+
   // Returns true if we have any floating descendants which need to be
   // traversed during the float paint phase.
   bool HasFloatingDescendantsForPaint() const {
