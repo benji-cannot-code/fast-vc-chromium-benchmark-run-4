9b8e4d00f20cf67c80b37490e48dbd02831d1f17
David Van Cleve <davidvc@chromium.org>
2020-04-13 22:29:02 +0000

    Trust Tokens: Construct signing helpers when requested
    This CL adds logic to TrustTokenRequestHelperFactory to vend
    appropriately parameterized TrustTokenRequestSigningHelpers when
    requests bear Trust Tokens parameters with OperationType==kSigning.
    
    This requires that the request's Trust Tokens issuer be a suitable
    Trust Tokens origin.
    
    R=csharrison
    
    constructing a signing helper fails with an unsuitable issuer.
    
    Test: Expand TrustTokenRequestHelperFactory's unit tests to verify that
    Change-Id: Iadbfe160415c97afb68f17a05d98ca71d5bdfbcb
    Fixes: 1068676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140176
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758631}

--- services/network/trust_tokens/trust_token_request_helper_factory.h
+++ services/network/trust_tokens/trust_token_request_helper_factory.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/mojom/trust_tokens.mojom.h"
 #include "services/network/trust_tokens/pending_trust_token_store.h"
+#include "services/network/trust_tokens/suitable_trust_token_origin.h"
 #include "services/network/trust_tokens/trust_token_request_helper.h"
 
 namespace network {
@@ -30,8 +31,8 @@ class TrustTokenRequestHelperFactory {
   // helpers.
   explicit TrustTokenRequestHelperFactory(PendingTrustTokenStore* store);
 
-  TrustTokenRequestHelperFactory() = default;
-  virtual ~TrustTokenRequestHelperFactory() = default;
+  TrustTokenRequestHelperFactory();
+  virtual ~TrustTokenRequestHelperFactory();
 
   // Attempts to create a TrustTokenRequestHelper able to help execute the Trust
   // Tokens protocol operation given by |params| against the request |request|.
@@ -53,7 +54,18 @@ class TrustTokenRequestHelperFactory {
       base::OnceCallback<void(TrustTokenStatusOrRequestHelper)> done);
 
  private:
+  // Continuation of |CreateTrustTokenHelperForRequest|. Uses |store|, alongside
+  // the information provided to |CreateTrustTokenHelperForRequest|, to finish
+  // constructing a store or return an error.
+  void ConstructHelperUsingStore(
+      SuitableTrustTokenOrigin top_frame_origin,
+      mojom::TrustTokenParamsPtr params,
+      base::OnceCallback<void(TrustTokenStatusOrRequestHelper)> done,
+      TrustTokenStore* store);
+
   PendingTrustTokenStore* store_;
+
+  base::WeakPtrFactory<TrustTokenRequestHelperFactory> weak_factory_{this};
 };
 
 class TrustTokenStatusOrRequestHelper {
