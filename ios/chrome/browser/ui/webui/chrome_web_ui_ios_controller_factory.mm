994e85836b320adcd9c66b373833791820585d0c
Kurt Horimoto <kkhorimoto@google.com>
2020-04-13 21:49:43 +0000

    [iOS] Hook up chrome://interstitials page.
    This CL adds support for the chrome://interstitials WebUI, which
    contains links for dummy interstitials.  Currently only a subset of the
    available links are implemented for iOS.
    
    Bug: 1064803
    Change-Id: Ie8fed45623f18319ff1c8a9de5108108a796b2bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2145154
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758619}

--- ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
+++ ios/chrome/browser/ui/webui/chrome_web_ui_ios_controller_factory.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/webui/flags_ui.h"
 #include "ios/chrome/browser/ui/webui/gcm/gcm_internals_ui.h"
 #include "ios/chrome/browser/ui/webui/inspect/inspect_ui.h"
+#include "ios/chrome/browser/ui/webui/interstitials/interstitial_ui.h"
 #include "ios/chrome/browser/ui/webui/net_export/net_export_ui.h"
 #include "ios/chrome/browser/ui/webui/ntp_tiles_internals_ui.h"
 #include "ios/chrome/browser/ui/webui/omaha_ui.h"
@@ -86,6 +87,8 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(const GURL& url) {
     return &NewWebUIIOS<GCMInternalsUI>;
   if (url_host == kChromeUIInspectHost)
     return &NewWebUIIOS<InspectUI>;
+  if (url_host == kChromeUIIntersitialsHost)
+    return &NewWebUIIOS<InterstitialUI>;
   if (url_host == kChromeUINetExportHost)
     return &NewWebUIIOS<NetExportUI>;
   if (url_host == kChromeUINTPTilesInternalsHost)
