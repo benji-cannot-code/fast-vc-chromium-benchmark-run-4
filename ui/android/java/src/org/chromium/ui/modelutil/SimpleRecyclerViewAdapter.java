9631c8cc23ab40cebf08c7c9e7a7f3a052ba58ba
Tomasz Wiszkowski <ender@google.com>
2020-04-13 17:40:34 +0000

    Fix SimpleRecyclerViewAdapter ModelList event propagation.
    This CL addresses the SimpleRecyclerViewAdapter propagating incomplete
    events to the parent RecyclerViewAdapter, resulting in partial UI
    updates.
    
    The CL also adds a few tests that confirm the behavior of the SRVA.
    
    Change-Id: I4c35b54e925853e91d686fde5cc476f3d2efe6f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2146063
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758528}

--- ui/android/java/src/org/chromium/ui/modelutil/SimpleRecyclerViewAdapter.java
+++ ui/android/java/src/org/chromium/ui/modelutil/SimpleRecyclerViewAdapter.java
@@ -74,7 +74,7 @@ public class SimpleRecyclerViewAdapter
         mListObserver = new ListObserver<Void>() {
             @Override
             public void onItemRangeInserted(ListObservable source, int index, int count) {
-                notifyItemInserted(index);
+                notifyItemRangeInserted(index, count);
             }
 
             @Override
@@ -85,7 +85,7 @@ public class SimpleRecyclerViewAdapter
             @Override
             public void onItemRangeChanged(
                     ListObservable<Void> source, int index, int count, @Nullable Void payload) {
-                notifyItemChanged(index);
+                notifyItemRangeChanged(index, count);
             }
 
             @Override
