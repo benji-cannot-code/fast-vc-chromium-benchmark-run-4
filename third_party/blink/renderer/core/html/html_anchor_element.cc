85de7608d5df2511a16c80dbf71b7efc795cfbf8
Timothy Gu <timothygu@chromium.org>
2020-04-13 20:05:33 +0000

    Revert "Treat any HTML element with a tabindex attribute as interactive content"
    This reverts commit 7920b6e87ccb0640fc53ba264b6d1500325cb2f9.
    
    It turns out that this change is not web compatible. In addition to
    reverting the original change, we will seek to change the spec to
    reflect this web reality.
    
    Only the changes to Blink code are fully reverted. The tests are kept,
    but adapted to reflect this change. A comment update is also kept.
    
    Original change's description:
    > Treat any HTML element with a tabindex attribute as interactive content
    >
    > According to the HTML Standard [1]:
    >
    > > An element with the tabindex attribute specified is interactive
    > > content.
    >
    > In a subsequent review [2], this clause is reaffirmed by the editor of
    > HTML. Thus, we implement this accordingly.
    >
    > This commit also adds comprehensive testing for all kinds of interactive
    > content, which have differential treatment by the <label> element
    > relative to other elements.
    >
    > [1]: https://html.spec.whatwg.org/C/#the-tabindex-attribute
    > [2]: https://github.com/whatwg/html/issues/5414
    >
    > Fixed: 1065048
    > Change-Id: I8deff810fead2c9170e18dd4d06dc642f68e6742
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2122369
    > Commit-Queue: Timothy Gu <timothygu@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Reviewed-by: Mason Freed <masonfreed@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#757590}
    
    Bug: 1065048
    Change-Id: Ic32dbb9ac8eafb9b92a131fde71c5459029260e2
    Fixed: 1069615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2146089
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Timothy Gu <timothygu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758568}

--- third_party/blink/renderer/core/html/html_anchor_element.cc
+++ third_party/blink/renderer/core/html/html_anchor_element.cc
@@ -583,7 +583,7 @@ bool HTMLAnchorElement::WillRespondToMouseClickEvents() {
 }
 
 bool HTMLAnchorElement::IsInteractiveContent() const {
-  return IsLink() || HTMLElement::IsInteractiveContent();
+  return IsLink();
 }
 
 Node::InsertionNotificationRequest HTMLAnchorElement::InsertedInto(
