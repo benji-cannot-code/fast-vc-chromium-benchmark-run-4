5ba463f55ff4dc1766c6b61a8c5081641165ae25
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-04-13 06:08:54 +0000

    Import wpt@ec1023a6b5f4dc6c8cbeb3507081e2ac3741adad
    Using wpt-import in Chromium 3d6123e3472d61f05d7a7937dc9b6bb414e6e933.
    With Chromium commits locally applied on WPT:
    d0ad7cf8c4 "UpgradeInsecureRequest: Add WPTs for static-import in dedicated/shared workers"
    c53a40f406 "Update size.attributes.parse.trailingjunk"
    86131ac4be "Add a test for ReplaceTrack that verifies video track content."
    380347e7c2 "[css-masking] Migrate clip-path-columns-svg-clippath-usou.html to WPT"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    rego@igalia.com:
      external/wpt/css/selectors
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I2c3a9f2c8dcd6b478001fea85045e0f68965a93f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144709
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#758466}

--- third_party/blink/web_tests/external/wpt/webxr/idlharness.https.window.js
+++ third_party/blink/web_tests/external/wpt/webxr/idlharness.https.window.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
+// META: timeout=long
 
 'use strict';
 
@@ -8,11 +9,48 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 idl_test(
   ['webxr'],
-  ['permissions', 'webgl1', 'html', 'dom'],
+  ['permissions', 'webgl1', 'geometry', 'html', 'dom'],
   async idl_array => {
     idl_array.add_objects({
       Navigator: ['navigator'],
       XR: ['navigator.xr'],
+      // TODO: XRSystem
+      XRSession: ['xrSession'],
+      XRRenderState: ['xrRenderState'],
+      // TODO: XRFrame
+      // TODO: XRSpace
+      XRReferenceSpace: ['xrReferenceSpace'],
+      // TODO: XRBoundedReferenceSpace
+      // TODO: XRView
+      // TODO: XRViewport
+      XRRigidTransform: ['new XRRigidTransform()'],
+      // TODO: XRPose
+      // TODO: XRViewerPose
+      // TODO: XRInputSource
+      XRInputSourceArray: ['xrInputSourceArray'],
+      XRWebGLLayer: ['xrWebGLLayer'],
+      WebGLRenderingContextBase: ['webGLRenderingContextBase'],
+      XRSessionEvent: ['xrSessionEvent'],
+      // TODO: XRInputSourceEvent
+      XRInputSourcesChangeEvent: ['xrInputSourcesChangeEvent'],
+      // TODO: XRReferenceSpaceEvent
+      // TODO: XRPermissionStatus
     });
+
+    self.xrSession = await navigator.xr.requestSession('inline');
+    self.xrRenderState = self.xrSession.renderState;
+    self.xrReferenceSpace = await self.xrSession.requestReferenceSpace('viewer');
+    self.xrInputSourceArray = self.xrSession.inputSources;
+    self.xrSessionEvent = new XRSessionEvent('end', {session: self.xrSession});
+    self.xrInputSourcesChangeEvent = new XRInputSourcesChangeEvent('inputsourceschange', {
+      session: self.xrSession,
+      added: [],
+      removed: [],
+    });
+
+    // XRWebGLRenderingContext is a typedef to either WebGLRenderingContext or WebGL2RenderingContext.
+    const canvas = document.createElement('canvas');
+    self.webGLRenderingContextBase = canvas.getContext('webgl');
+    self.xrWebGLLayer = new XRWebGLLayer(self.xrSession, self.webGLRenderingContextBase);
   }
 );
