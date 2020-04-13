6d169cb15d1551bcd7edf523e39d64d3a0aa2a92
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-04-13 20:39:14 +0000

    Add basic end-to-end Reader mode tests
    This CL also increases the instruction coverage of
    ReaderModeManager.java from 62% to 87%.
    
    Bug: 1070293
    Change-Id: I11af0f524cd01d0741f9a6c9f4ca17b51c2c145d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143675
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758589}

--- chrome/browser/dom_distiller/tab_utils.cc
+++ chrome/browser/dom_distiller/tab_utils.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "build/build_config.h"
 #include "chrome/browser/dom_distiller/dom_distiller_service_factory.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "components/dom_distiller/content/browser/distiller_page_web_contents.h"
@@ -178,7 +179,9 @@ void DistillCurrentPageAndView(content::WebContents* old_web_contents) {
 
   MaybeStartDistillation(std::move(source_page_handle));
 
+#if !defined(OS_ANDROID)
   dom_distiller::UMAHelper::LogTimeOnDistillablePage(old_web_contents);
+#endif
 }
 
 void DistillCurrentPage(content::WebContents* source_web_contents) {
