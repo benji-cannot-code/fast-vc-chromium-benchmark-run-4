7d1dc2e04b3f71a3ede9d070538597d492c10208
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2020-04-13 21:34:57 +0000

    blink/RtcVideoEncoderFactory: Fix VP9 profile handling
    If the Video Encode hardware accelerator has support for
    VP9Profile0 and VP9Profile1, RTCVideoEncoderFactory creates
    a pipeline for Profile1 which is not supported by blink or webrtc.
    Moreover, it will mess up the h/w encoder by sending an illegal
    combination of chroma-subsampling 4:2:0 raw input and a
    Profile1 config.
    
    This patch is trying to address two issues:
    
    a) make sure that CreateVideoEncoder return successfully once there
    is a match found with SdpVideoFormat, otherwise a video-encoder
    will get created for each supported format and override the previous
    one.
    
    b) Ensure that VEAtoWEBRTCFormat doesn't return a legit profile id
    for VP9 profile1 otherwise, SupportedFormats will get populated with
    wrong sdp_formats against VideoCodecProfile.
    Webrtc supports profile0 & profile2.
    
    BUG = b:153680337
    TEST = Run https://appr.tc/?debug=loopback&vrc=vp9&vsc=vp9 on a platform
    that has more than one profile encode support for VP9. H/W encode will
    fail and fall back to the software encoding.
    
    Change-Id: Ic779375d98fb56d9a1509e042faedfa002f9bf29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2146173
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Sreerenj Balachandran <sreerenj.balachandran@intel.com>
    Cr-Commit-Position: refs/heads/master@{#758615}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_factory.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_factory.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/webrtc/api/video_codecs/video_encoder.h"
 #include "third_party/webrtc/common_video/h264/profile_level_id.h"
 #include "third_party/webrtc/media/base/codec.h"
+#include "third_party/webrtc/media/base/vp9_profile.h"
 
 namespace blink {
 
@@ -114,10 +115,28 @@ base::Optional<webrtc::SdpVideoFormat> VEAToWebRTCFormat(
         {cricket::kH264FmtpPacketizationMode, "1"}};
     return format;
   }
+
   if (profile.profile >= media::VP9PROFILE_MIN &&
       profile.profile <= media::VP9PROFILE_MAX) {
-    return webrtc::SdpVideoFormat("VP9");
+    webrtc::VP9Profile vp9_profile;
+    switch (profile.profile) {
+      case media::VP9PROFILE_PROFILE0:
+        vp9_profile = webrtc::VP9Profile::kProfile0;
+        break;
+      case media::VP9PROFILE_PROFILE2:
+        vp9_profile = webrtc::VP9Profile::kProfile2;
+        break;
+      default:
+        // Unsupported VP9 profiles (profile1 & profile3) in WebRTC.
+        return base::nullopt;
+    }
+    webrtc::SdpVideoFormat format("VP9");
+    format.parameters = {
+        {webrtc::kVP9FmtpProfileId,
+         webrtc::VP9ProfileToString(vp9_profile)}};
+    return format;
   }
+
   return base::nullopt;
 }  // namespace
 
@@ -171,6 +190,7 @@ RTCVideoEncoderFactory::CreateVideoEncoder(
       if (IsSameFormat(format, supported_formats.sdp_formats[i])) {
         encoder = std::make_unique<RTCVideoEncoder>(
             supported_formats.profiles[i], gpu_factories_);
+        break;
       }
     }
   } else {
