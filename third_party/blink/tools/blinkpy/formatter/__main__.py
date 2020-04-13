3e109d47036a5a9f9fada702671e111f5ede01e1
Igor Makarov <igor.makarov@lge.com>
2020-04-13 03:52:41 +0000

    Reformat python code in third_party/blink/tools/blinkpy/formatter for PEP8
    - added .style.yapf file
    - reformat *.py files
    
    Bug: 1051750
    Change-Id: I902200e328a800ec23a4c34ce7896b5d25122b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132791
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758451}

--- third_party/blink/tools/blinkpy/formatter/__main__.py
+++ third_party/blink/tools/blinkpy/formatter/__main__.py
@@ -7,6 +7,5 @@ import sys
 
 from blinkpy.formatter.main import main
 
-
 if __name__ == '__main__':
     sys.exit(main())
--- third_party/blink/tools/blinkpy/formatter/fix_double_quote_strings.py
+++ third_party/blink/tools/blinkpy/formatter/fix_double_quote_strings.py
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """A 2to3 fixer that converts all string literals to use double quotes.
 
 Strings that contain double quotes will not be modified. Prefixed string
