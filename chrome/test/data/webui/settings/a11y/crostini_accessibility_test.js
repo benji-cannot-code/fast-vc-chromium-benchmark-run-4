737b3a26a2579601f12c9fbda18ce49ca511f417
Nicholas Verne <nverne@chromium.org>
2020-04-14 23:34:38 +0000

    Try fixing a11y crostini settings flakes.
    The tests now use FakeCrostiniFeatures to set all things enabled.
    
    Bug: 1067487
    Change-Id: I8e797002be93a830eb2ce1e425b4bd91cf8bffb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147165
    Auto-Submit: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759063}

--- chrome/test/data/webui/settings/a11y/crostini_accessibility_test.js
+++ chrome/test/data/webui/settings/a11y/crostini_accessibility_test.js
@@ -16,6 +16,7 @@ GEN_INCLUDE([
 ]);
 
 GEN('#include "chrome/browser/chromeos/crostini/crostini_pref_names.h"');
+GEN('#include "chrome/browser/chromeos/crostini/fake_crostini_features.h"');
 GEN('#include "chrome/browser/profiles/profile.h"');
 GEN('#include "chrome/browser/ui/browser.h"');
 GEN('#include "chrome/common/chrome_features.h"');
@@ -38,5 +39,7 @@ var CrostiniAccessibilityTest = class extends PolymerTest {
   testGenPreamble() {
     GEN('  browser()->profile()->GetPrefs()->SetBoolean(');
     GEN('      crostini::prefs::kCrostiniEnabled, true);');
+    GEN('  crostini::FakeCrostiniFeatures fake_crostini_features;');
+    GEN('  fake_crostini_features.SetAll(true);');
   }
 };
