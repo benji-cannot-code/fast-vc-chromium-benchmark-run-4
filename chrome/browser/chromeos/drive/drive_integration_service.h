7fed6b7798b1a2520a22cc5899b26d1cdf1772fd
Austin Tankiang <austinct@chromium.org>
2020-04-14 06:53:57 +0000

    Turn DriveIntegrationServiceObserver into a CheckedObserver
    Bug: 1069669
    Change-Id: Iebbe4b6f8461639f780918322da87dd413c4f1a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147989
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758743}

--- chrome/browser/chromeos/drive/drive_integration_service.h
+++ chrome/browser/chromeos/drive/drive_integration_service.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "base/scoped_observer.h"
 #include "chromeos/components/drivefs/drivefs_host.h"
 #include "chromeos/dbus/power/power_manager_client.h"
@@ -67,7 +68,7 @@ struct QuickAccessItem {
 
 // Interface for classes that need to observe events from
 // DriveIntegrationService.  All events are notified on UI thread.
-class DriveIntegrationServiceObserver {
+class DriveIntegrationServiceObserver : public base::CheckedObserver {
  public:
   // Triggered when the file system is mounted.
   virtual void OnFileSystemMounted() {
@@ -80,9 +81,6 @@ class DriveIntegrationServiceObserver {
   // Triggered when mounting the filesystem has failed in a fashion that will
   // not be automatically retried.
   virtual void OnFileSystemMountFailed() {}
-
- protected:
-  virtual ~DriveIntegrationServiceObserver() {}
 };
 
 // DriveIntegrationService is used to integrate Drive to Chrome. This class
@@ -257,7 +255,7 @@ class DriveIntegrationService : public KeyedService,
   std::unique_ptr<internal::ResourceMetadataStorage, util::DestroyHelper>
       metadata_storage_;
 
-  base::ObserverList<DriveIntegrationServiceObserver>::Unchecked observers_;
+  base::ObserverList<DriveIntegrationServiceObserver> observers_;
 
   std::unique_ptr<DriveFsHolder> drivefs_holder_;
   std::unique_ptr<PreferenceWatcher> preference_watcher_;
