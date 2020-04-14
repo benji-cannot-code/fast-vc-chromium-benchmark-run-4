4b048467f1bbdeba18a1324d6e36dc4339502e03
Sigurdur Asgeirsson <siggi@chromium.org>
2020-04-14 20:12:59 +0000

    Fix navigation for the discards tab in chrome://discards.
    Special-casing the 'Discards' tab to the empty tab makes the initial
    page load navigation entry consistent with entry on back navigation.
    
    Bug: 1070554
    Change-Id: Ib1d6f271e12d97db31cd03708f89b41a26009e64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2148539
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758979}

--- chrome/browser/resources/discards/discards_main.js
+++ chrome/browser/resources/discards/discards_main.js
@@ -46,7 +46,10 @@ Polymer({
    */
   selectedChanged_(newValue, oldValue) {
     if (oldValue != undefined) {
-      this.path = '/' + this.tabs[newValue].toLowerCase();
+      // The first tab is special-cased to the empty path.
+      const defaultTab = this.tabs[0].toLowerCase();
+      const tabName = this.tabs[newValue].toLowerCase();
+      this.path = '/' + (tabName === defaultTab ? '' : tabName);
     }
   },
 
