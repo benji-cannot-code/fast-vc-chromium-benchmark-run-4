5b0993fab803796c3220d4f73795ab66701b019f
Peter Kasting <pkasting@chromium.org>
2020-04-14 15:59:40 +0000

    Finish cleaning up references to Blink origin_policy implementation.
    https://chromium-review.googlesource.com/c/chromium/src/+/2079258
    deleted third_party/blink/common/origin_policy, but not the public API.
    Additionally, various OWNERS files were left pointing at a nonexistent
    file.  AFAICT the new canonical home is services/network/origin_policy,
    so point to that.
    
    Bug: none
    Change-Id: I2f27b9a63083e92c708590955849122fef9ffe00
    No-Presubmit: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144106
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758865}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_tick_clock.h"
 #include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/common/features.h"
-#include "third_party/blink/public/common/origin_policy/origin_policy.h"
 #include "third_party/blink/public/mojom/commit_result/commit_result.mojom-blink.h"
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_network_provider.h"
 #include "third_party/blink/public/platform/web_url_request.h"
