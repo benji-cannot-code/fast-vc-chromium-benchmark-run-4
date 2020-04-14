6b3e23a1f858370d7bc54a30a33e8427b48c400e
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-04-14 13:46:08 +0000

    Add b4p badges to the credential manager account chooser UI
    CredentialsItemView now supports setting an optional right-aligned
    G-logo icon to disguish account store forms from profile store
    ones.
    
    The icon is set in the AccountChooserDialogView for credentials
    read from the account store. If the user has not opted in to b4p,
    no forms will satisfy this, so there's no need explicitly check
    the opted-in state of the user to guarantee that no badges will
    be shown for non opted-in users.
    
    Account chooser with badges:
    https://screenshot.googleplex.com/JMiryuMoec8
    
    Bug: 1060134
    Change-Id: Id0a27324ad7955dae366a3e6efa8102c1145633f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2116211
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758817}

--- chrome/browser/ui/views/passwords/credentials_item_view.h
+++ chrome/browser/ui/views/passwords/credentials_item_view.h
@@ -8,13 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "build/branding_buildflags.h"
+#include "build/buildflag.h"
 #include "chrome/browser/ui/passwords/account_avatar_fetcher.h"
+#include "components/autofill/core/common/password_form.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/style/typography.h"
-
-namespace autofill {
-struct PasswordForm;
-}
 
 namespace gfx {
 class ImageSkia;
@@ -45,6 +44,10 @@ class CredentialsItemView : public AccountAvatarFetcherDelegate,
                       int lower_text_style = views::style::STYLE_SECONDARY);
   ~CredentialsItemView() override;
 
+  // If |store| is kAccountStore and the build is official, adds a G logo icon
+  // to the view. If |store| is kProfileStore, removes any existing icon.
+  void SetStoreIndicatorIcon(autofill::PasswordForm::Store store);
+
   const autofill::PasswordForm* form() const { return form_; }
 
   // AccountAvatarFetcherDelegate:
@@ -59,6 +62,13 @@ class CredentialsItemView : public AccountAvatarFetcherDelegate,
   const autofill::PasswordForm* form_;
 
   views::ImageView* image_view_;
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  // Optional right-aligned icon to distinguish account store credentials and
+  // profile store ones.
+  views::ImageView* store_indicator_icon_view_ = nullptr;
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+
   views::Label* upper_label_ = nullptr;
   views::Label* lower_label_ = nullptr;
   views::ImageView* info_icon_ = nullptr;
