39db6343d7c7fa7a4c66880f89c63980e0ab86a1
Pavel Feldman <pfeldman@chromium.org>
2020-04-14 20:08:41 +0000

    CDP: make optional params follow required
    See https://chromium-review.googlesource.com/c/chromium/src/+/2148315
    for details.
    
    Change-Id: I2d1d738ff265505344a394f99a9990429cb96f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149375
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758974}

--- content/browser/devtools/protocol/browser_handler.cc
+++ content/browser/devtools/protocol/browser_handler.cc
@@ -330,9 +330,9 @@ Response BrowserHandler::FindBrowserContext(
 }
 
 Response BrowserHandler::SetPermission(
-    Maybe<std::string> origin,
     std::unique_ptr<protocol::Browser::PermissionDescriptor> permission,
     const protocol::Browser::PermissionSetting& setting,
+    Maybe<std::string> origin,
     Maybe<std::string> browser_context_id) {
   BrowserContext* browser_context = nullptr;
   Response response = FindBrowserContext(browser_context_id, &browser_context);
@@ -374,9 +374,9 @@ Response BrowserHandler::SetPermission(
 }
 
 Response BrowserHandler::GrantPermissions(
-    Maybe<std::string> origin,
     std::unique_ptr<protocol::Array<protocol::Browser::PermissionType>>
         permissions,
+    Maybe<std::string> origin,
     Maybe<std::string> browser_context_id) {
   BrowserContext* browser_context = nullptr;
   Response response = FindBrowserContext(browser_context_id, &browser_context);
