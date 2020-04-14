88dd87070485b797364af8d1bbfdbbdcf6aab8e1
Kevin McNee <mcnee@chromium.org>
2020-04-14 23:02:46 +0000

    Use test assertions in ChildProcessLauncherBrowserTest instead of CHECKs
    Per the style guide, we should use proper test assertions to report
    failures gracefully.
    
    Bug: None
    Change-Id: I84be462364d6f310c08c591785d7f781a92251b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149376
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759052}

--- content/browser/child_process_launcher_browsertest.cc
+++ content/browser/child_process_launcher_browsertest.cc
@@ -71,7 +71,7 @@ IN_PROC_BROWSER_TEST_F(ChildProcessLauncherBrowserTest, ChildSpawnFail) {
   NavigationEntry* last_entry =
       shell()->web_contents()->GetController().GetLastCommittedEntry();
   // Make sure we didn't navigate.
-  CHECK(!last_entry);
+  EXPECT_FALSE(last_entry);
 
   // Navigate again and let the process spawn correctly.
   TestNavigationObserver nav_observer2(window->web_contents(), 1);
@@ -79,8 +79,9 @@ IN_PROC_BROWSER_TEST_F(ChildProcessLauncherBrowserTest, ChildSpawnFail) {
   nav_observer2.Wait();
   last_entry = shell()->web_contents()->GetController().GetLastCommittedEntry();
   // Make sure that we navigated to the proper URL.
-  CHECK(last_entry && last_entry->GetPageType() == PAGE_TYPE_NORMAL);
-  CHECK(shell()->web_contents()->GetLastCommittedURL() == url);
+  ASSERT_TRUE(last_entry);
+  EXPECT_EQ(last_entry->GetPageType(), PAGE_TYPE_NORMAL);
+  EXPECT_EQ(shell()->web_contents()->GetLastCommittedURL(), url);
 
   // Navigate again, using the same renderer.
   url = GURL("data:text/html,dataurl");
@@ -89,8 +90,9 @@ IN_PROC_BROWSER_TEST_F(ChildProcessLauncherBrowserTest, ChildSpawnFail) {
   nav_observer3.Wait();
   last_entry = shell()->web_contents()->GetController().GetLastCommittedEntry();
   // Make sure that we navigated to the proper URL.
-  CHECK(last_entry && last_entry->GetPageType() == PAGE_TYPE_NORMAL);
-  CHECK(shell()->web_contents()->GetLastCommittedURL() == url);
+  ASSERT_TRUE(last_entry);
+  EXPECT_EQ(last_entry->GetPageType(), PAGE_TYPE_NORMAL);
+  EXPECT_EQ(shell()->web_contents()->GetLastCommittedURL(), url);
 }
 
 }  // namespace content
