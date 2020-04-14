259c2cb6a6d41c6d8dae8c239c73c5a0cc2b7933
David Black <dmblack@google.com>
2020-04-14 22:59:48 +0000

    Stub in Assistant search provider.
    The Assistant search provider will be used to provide a single search
    result from its internal cache of conversation starters. This will be
    surfaced as a suggestion chip in the launcher.
    
    This CL simply stubs in the provider (along with a single hardcoded
    result) so as to unblock development of the logic which will allocate
    Assistant a dedicated chip slot.
    
    Changes are gated behind a new feature flag:
      AssistantLauncherChipIntegration
    
    Bug: b:146673437, b:153165833
    Change-Id: I54159c3ee17110c8fad46bbb3cea4a88b3250df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134651
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759050}

---
+++ chrome/browser/ui/app_list/search/assistant_search_provider_unittest.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/app_list/search/assistant_search_provider.h"
+#include "chrome/browser/ui/app_list/app_list_test_util.h"
+#include "chrome/browser/ui/app_list/search/chrome_search_result.h"
+
+namespace app_list {
+namespace test {
+
+// AssistantSearchProviderTest -------------------------------------------------
+
+class AssistantSearchProviderTest : public AppListTestBase {
+ public:
+  AssistantSearchProviderTest() = default;
+  AssistantSearchProviderTest(const AssistantSearchProviderTest&) = delete;
+  AssistantSearchProviderTest& operator=(const AssistantSearchProviderTest&) =
+      delete;
+  ~AssistantSearchProviderTest() override = default;
+
+  AssistantSearchProvider& search_provider() { return search_provider_; }
+
+ private:
+  AssistantSearchProvider search_provider_;
+};
+
+// Tests -----------------------------------------------------------------------
+
+TEST_F(AssistantSearchProviderTest, ShouldHaveAnInitialChipResult) {
+  ASSERT_EQ(1u, search_provider().results().size());
+
+  const ChromeSearchResult& result = *search_provider().results().at(0);
+  EXPECT_EQ(ash::SearchResultDisplayIndex::kFirstIndex, result.display_index());
+  EXPECT_EQ(ash::SearchResultDisplayType::kChip, result.display_type());
+  EXPECT_EQ(ash::AppListSearchResultType::kAssistantChip, result.result_type());
+  EXPECT_EQ(ash::SearchResultType::ASSISTANT, result.GetSearchResultType());
+}
+
+}  // namespace test
+}  // namespace app_list
--- chromeos/services/assistant/public/features.h
+++ chromeos/services/assistant/public/features.h
@@ -33,6 +33,10 @@ extern const base::Feature kAssistantWarmerWelcomeFeature;
 COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
 extern const base::Feature kAssistantAppSupport;
 
+// Enables Assistant launcher chip integration.
+COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
+extern const base::Feature kAssistantLauncherChipIntegration;
+
 // Enables Assistant proactive suggestions.
 COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
 extern const base::Feature kAssistantProactiveSuggestions;
@@ -110,6 +114,9 @@ bool IsConversationStartersV2Enabled();
 COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC) bool IsDspHotwordEnabled();
 
 COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC) bool IsFeedbackUiEnabled();
+
+COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
+bool IsLauncherChipIntegrationEnabled();
 
 COMPONENT_EXPORT(ASSISTANT_SERVICE_PUBLIC)
 bool IsMediaSessionIntegrationEnabled();
