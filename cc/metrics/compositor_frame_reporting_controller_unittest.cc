05642fae5d631adc7d4ce7fbf3057b3c22558162
Sadrul Habib Chowdhury <sadrul@chromium.org>
2020-04-14 15:57:45 +0000

    [code health] Make member variables private.
    The member variables should be private, and can be accessed in
    tests via protected accessors.
    
    BUG=none
    TBR=behdadb@chromium.org
    
    Change-Id: I40e19a3d76323800df9b6c2aa45ad27efe1bc31a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149275
    Reviewed-by: Behdad Bakhshinategh <behdadb@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Behdad Bakhshinategh <behdadb@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Auto-Submit: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758864}

--- cc/metrics/compositor_frame_reporting_controller_unittest.cc
+++ cc/metrics/compositor_frame_reporting_controller_unittest.cc
@@ -38,12 +38,10 @@ class TestCompositorFrameReportingController
   TestCompositorFrameReportingController& operator=(
       const TestCompositorFrameReportingController& controller) = delete;
 
-  std::unique_ptr<CompositorFrameReporter>* reporters() { return reporters_; }
-
   int ActiveReporters() {
     int count = 0;
     for (int i = 0; i < PipelineStage::kNumPipelineStages; ++i) {
-      if (reporters_[i])
+      if (reporters()[i])
         ++count;
     }
     return count;
--- cc/test/fake_compositor_frame_reporting_controller.cc
+++ cc/test/fake_compositor_frame_reporting_controller.cc
@@ -18,14 +18,14 @@ FakeCompositorFrameReportingController::FakeCompositorFrameReportingController()
 
 void FakeCompositorFrameReportingController::WillBeginMainFrame(
     const viz::BeginFrameArgs& args) {
-  if (!reporters_[PipelineStage::kBeginImplFrame])
+  if (!HasReporterAt(PipelineStage::kBeginImplFrame))
     CompositorFrameReportingController::WillBeginImplFrame(args);
   CompositorFrameReportingController::WillBeginMainFrame(args);
 }
 
 void FakeCompositorFrameReportingController::BeginMainFrameAborted(
     const viz::BeginFrameId& id) {
-  if (!reporters_[PipelineStage::kBeginMainFrame]) {
+  if (!HasReporterAt(PipelineStage::kBeginMainFrame)) {
     viz::BeginFrameArgs args = viz::BeginFrameArgs();
     args.frame_id = id;
     args.frame_time = Now();
@@ -36,7 +36,7 @@ void FakeCompositorFrameReportingController::BeginMainFrameAborted(
 }
 
 void FakeCompositorFrameReportingController::WillCommit() {
-  if (!reporters_[PipelineStage::kBeginMainFrame]) {
+  if (!HasReporterAt(PipelineStage::kBeginMainFrame)) {
     viz::BeginFrameArgs args = viz::BeginFrameArgs();
     args.frame_id = viz::BeginFrameId();
     args.frame_time = Now();
@@ -47,19 +47,19 @@ void FakeCompositorFrameReportingController::WillCommit() {
 }
 
 void FakeCompositorFrameReportingController::DidCommit() {
-  if (!reporters_[PipelineStage::kBeginMainFrame])
+  if (!HasReporterAt(PipelineStage::kBeginMainFrame))
     WillCommit();
   CompositorFrameReportingController::DidCommit();
 }
 
 void FakeCompositorFrameReportingController::WillActivate() {
-  if (!reporters_[PipelineStage::kCommit])
+  if (!HasReporterAt(PipelineStage::kCommit))
     DidCommit();
   CompositorFrameReportingController::WillActivate();
 }
 
 void FakeCompositorFrameReportingController::DidActivate() {
-  if (!reporters_[PipelineStage::kCommit])
+  if (!HasReporterAt(PipelineStage::kCommit))
     WillActivate();
   CompositorFrameReportingController::DidActivate();
 }
