6261d6c6f3578d85990861a6f945009309f65734
Katie D <katie@chromium.org>
2020-04-14 02:19:10 +0000

    Turns off tooltip text for caption bubble label.
    Bug: 1055150
    Change-Id: I74fdfd76923d58e3133c80f88de59efb86daaa78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144869
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#758689}

--- chrome/browser/ui/views/accessibility/caption_bubble.cc
+++ chrome/browser/ui/views/accessibility/caption_bubble.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/strings/string16.h"
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/hit_test.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -106,6 +107,7 @@ void CaptionBubble::Init() {
   label->SetBackgroundColor(SK_ColorTRANSPARENT);
   label->SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT);
   label->SetLineHeight(kLineHeightDip);
+  label->SetTooltipText(base::string16());
 
   // TODO(crbug.com/1055150): Respect the user's font size and minimum font size
   // settings rather than having a fixed font size.
