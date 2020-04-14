523c057368f91e75cbe07a10c05d51bbc4cea8bf
Alex Ilin <alexilin@chromium.org>
2020-04-14 13:47:13 +0000

    [identity] Remote consent flow cookie update
    On ChromeOS, new accounts are added through the system settings. Since
    the remote consent flow uses a separate context for showing the consent
    page, an account change in settings won't update cookies automatically.
    
    This CL sets a listener for account updates in GaiaRemoteConsentFlow
    that triggers a cookie update. There is no need to reload the consent
    page because the Gaia OAuth page already handles cookie updates.
    
    Bug: 1026237
    Change-Id: Ie69a21e3a9715ba4993a16b222f1e07af0c4fdd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144201
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758819}

--- chrome/browser/extensions/api/identity/gaia_remote_consent_flow.h
+++ chrome/browser/extensions/api/identity/gaia_remote_consent_flow.h
@@ -9,9 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_list.h"
 
 #include "base/macros.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/extensions/api/identity/extension_token_key.h"
 #include "chrome/browser/extensions/api/identity/web_auth_flow.h"
 #include "components/signin/public/identity_manager/accounts_cookie_mutator.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/signin/public/identity_manager/set_accounts_in_cookie_result.h"
 #include "google_apis/gaia/core_account_id.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "google_apis/gaia/oauth2_mint_token_flow.h"
@@ -20,7 +23,8 @@ namespace extensions {
 
 class GaiaRemoteConsentFlow
     : public WebAuthFlow::Delegate,
-      public signin::AccountsCookieMutator::PartitionDelegate {
+      public signin::AccountsCookieMutator::PartitionDelegate,
+      public signin::IdentityManager::Observer {
  public:
   enum Failure {
     WINDOW_CLOSED,
@@ -61,27 +65,37 @@ class GaiaRemoteConsentFlow
   void OnConsentResultSet(const std::string& consent_result,
                           const std::string& window_id);
 
-  // WebAuthFlow::Delegate implementation.
+  // WebAuthFlow::Delegate:
   void OnAuthFlowFailure(WebAuthFlow::Failure failure) override;
 
-  // AccountsCookieMutator::PartitionDelegate:
+  // signin::AccountsCookieMutator::PartitionDelegate:
   std::unique_ptr<GaiaAuthFetcher> CreateGaiaAuthFetcherForPartition(
       GaiaAuthConsumer* consumer) override;
   network::mojom::CookieManager* GetCookieManagerForPartition() override;
 
+  // signin::IdentityManager::Observer:
+  void OnEndBatchOfRefreshTokenStateChanges() override;
+
   void SetWebAuthFlowForTesting(std::unique_ptr<WebAuthFlow> web_auth_flow);
 
  private:
+  void SetAccountsInCookie();
+
   Delegate* delegate_;
   Profile* profile_;
   CoreAccountId account_id_;
   RemoteConsentResolutionData resolution_data_;
+
   std::unique_ptr<WebAuthFlow> web_flow_;
+  bool web_flow_started_;
+
   std::unique_ptr<signin::AccountsCookieMutator::SetAccountsInCookieTask>
       set_accounts_in_cookie_task_;
   std::unique_ptr<base::CallbackList<void(const std::string&,
                                           const std::string&)>::Subscription>
       identity_api_set_consent_result_subscription_;
+  ScopedObserver<signin::IdentityManager, signin::IdentityManager::Observer>
+      scoped_observer_;
 };
 
 }  // namespace extensions
