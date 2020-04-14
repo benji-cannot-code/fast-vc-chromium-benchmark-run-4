47f068b8a69bd13b18d117191a23833f5dc421eb
Mugdha Lakhani <nator@chromium.org>
2020-04-14 18:10:38 +0000

    [WebLayer] Remove PageInfoController deps on Profile, ChromeActivity.
    PageInfoControllerDelegate has been introduced to allow customization of
    PageInfoController java code, currently focussing on everything that
    requires Profile or ChromeActivity.
    
    The end goal is to have no chrome dependencies in PageInfoController so
    it can be moved to //components.
    
    Bug: 1052375
    Change-Id: I8adf97cb400b99f964b86779c2a3116d1e3b458a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2142261
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758917}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/handler/toolbar/PaymentHandlerToolbarMediator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/handler/toolbar/PaymentHandlerToolbarMediator.java
@@ -12,6 +12,7 @@ import androidx.annotation.DrawableRes;
 
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.offlinepages.OfflinePageUtils;
+import org.chromium.chrome.browser.page_info.ChromePageInfoControllerDelegate;
 import org.chromium.chrome.browser.page_info.PageInfoController;
 import org.chromium.components.omnibox.SecurityStatusIcon;
 import org.chromium.components.security_state.ConnectionSecurityLevel;
@@ -135,6 +136,7 @@ import org.chromium.ui.modelutil.PropertyModel;
         PageInfoController.show(mChromeActivity, mWebContentsRef, null,
                 PageInfoController.OpenedFromSource.TOOLBAR,
                 /*offlinePageLoadUrlDelegate=*/
-                new OfflinePageUtils.WebContentsOfflinePageLoadUrlDelegate(mWebContentsRef));
+                new OfflinePageUtils.WebContentsOfflinePageLoadUrlDelegate(mWebContentsRef),
+                new ChromePageInfoControllerDelegate(mChromeActivity, mWebContentsRef));
     };
 }
