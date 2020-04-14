2e9b47527623808c30b710937b8a83d36d932df9
Yuki Shiino <yukishiino@chromium.org>
2020-04-14 08:41:34 +0000

    bind-gen: Enclose IDL constants in an inner class (namespace)
    Minor style change from V8Foo::kConstantBar to
    V8Foo::Constant::kBar.
    
    Bug: 839389
    Change-Id: Icc2e2e0dae28cb98ea92b80dd3e13ba882d214e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147972
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758764}

--- third_party/blink/renderer/bindings/scripts/bind_gen/interface.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/interface.py
@@ -167,9 +167,7 @@ def constant_name(cg_context):
 
     property_name = cg_context.property_.identifier.lower()
 
-    kind = "Constant"
-
-    return name_style.constant(kind, property_name)
+    return name_style.constant(property_name)
 
 
 def custom_function_name(cg_context):
@@ -1621,7 +1619,7 @@ def make_constant_callback_def(cg_context, function_name):
     body = func_def.body
 
     v8_set_return_value = _format(
-        "bindings::V8SetReturnValue(${info}, ${class_name}::{});",
+        "bindings::V8SetReturnValue(${info}, ${class_name}::Constant::{});",
         constant_name(cg_context))
     body.extend([
         make_runtime_call_timer_scope(cg_context),
@@ -4401,7 +4399,8 @@ def _make_property_entries_and_callback_defs(
         if const_callback_node is None:
             const_callback_name = None
         # IDL constant's C++ constant name
-        const_constant_name = _format("${class_name}::{}", constant_name(cgc))
+        const_constant_name = _format("${class_name}::Constant::{}",
+                                      constant_name(cgc))
 
         callback_def_nodes.extend([
             const_callback_node,
@@ -5879,11 +5878,14 @@ def generate_interface(interface):
         impl_class_def = api_class_def
 
     # Constants
-    constant_defs = ListNode()
-    for constant in interface.constants:
-        cgc = cg_context.make_copy(constant=constant)
-        constant_defs.append(
-            make_constant_constant_def(cgc, constant_name(cgc)))
+    constants_def = None
+    if interface.constants:
+        constants_def = CxxClassDefNode(name="Constant", final=True)
+        constants_def.top_section.append(TextNode("STATIC_ONLY(Constant);"))
+        for constant in interface.constants:
+            cgc = cg_context.make_copy(constant=constant)
+            constants_def.public_section.append(
+                make_constant_constant_def(cgc, constant_name(cgc)))
 
     # Custom callback implementations
     custom_callback_impl_decls = ListNode()
@@ -6215,6 +6217,13 @@ def generate_interface(interface):
     if is_cross_components:
         impl_header_blink_ns.body.append(impl_class_def)
 
+    if constants_def:
+        api_class_def.public_section.extend([
+            TextNode("// Constants"),
+            constants_def,
+            EmptyNode(),
+        ])
+
     api_class_def.public_section.append(get_wrapper_type_info_def)
     api_class_def.public_section.append(EmptyNode())
     api_class_def.public_section.extend([
@@ -6252,13 +6261,6 @@ def generate_interface(interface):
     else:
         api_class_def.public_section.append(installer_function_decls)
         api_class_def.public_section.append(EmptyNode())
-
-    if constant_defs:
-        api_class_def.public_section.extend([
-            TextNode("// Constants"),
-            constant_defs,
-            EmptyNode(),
-        ])
 
     if custom_callback_impl_decls:
         api_class_def.public_section.extend([
