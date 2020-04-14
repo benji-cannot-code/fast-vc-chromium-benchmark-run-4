bcd39fbaca4261660003224b8c788b4087b0d2d7
Sigurdur Asgeirsson <siggi@chromium.org>
2020-04-14 13:06:42 +0000

    [PM] Add node descriptions to graph dump protocol.
    Bug: 1068233
    Change-Id: Ie7b4dd8e6743dd963b8d94056bf71366c8633265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139438
    Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758809}

--- chrome/browser/ui/webui/discards/graph_dump_impl.cc
+++ chrome/browser/ui/webui/discards/graph_dump_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base64.h"
 #include "base/bind.h"
+#include "base/json/json_string_value_serializer.h"
 #include "base/macros.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "base/task/post_task.h"
@@ -20,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/favicon/core/favicon_service.h"
 #include "components/favicon_base/favicon_callback.h"
 #include "components/performance_manager/public/graph/graph.h"
+#include "components/performance_manager/public/graph/node_data_describer.h"
+#include "components/performance_manager/public/graph/node_data_describer_registry.h"
 #include "components/performance_manager/public/performance_manager.h"
 #include "components/performance_manager/public/web_contents_proxy.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -34,6 +37,16 @@ namespace {
 
 int64_t GetSerializationId(const performance_manager::Node* node) {
   return performance_manager::Node::GetSerializationId(node);
+}
+
+// Best effort convert |value| to a string.
+std::string ToJSON(const base::Value& value) {
+  std::string result;
+  JSONStringValueSerializer serializer(&result);
+  if (serializer.Serialize(value))
+    return result;
+
+  return std::string();
 }
 
 }  // namespace
@@ -373,6 +386,8 @@ void DiscardsGraphDumpImpl::SendFrameNotification(
   frame_info->page_id = GetSerializationId(page);
 
   frame_info->url = frame->GetURL();
+  frame_info->description_json = ToJSON(
+      graph_->GetNodeDataDescriberRegistry()->DescribeFrameNodeData(frame));
 
   if (created)
     change_subscriber_->FrameCreated(std::move(frame_info));
@@ -389,6 +404,9 @@ void DiscardsGraphDumpImpl::SendPageNotification(
 
   page_info->id = GetSerializationId(page_node);
   page_info->main_frame_url = page_node->GetMainFrameUrl();
+  page_info->description_json = ToJSON(
+      graph_->GetNodeDataDescriberRegistry()->DescribePageNodeData(page_node));
+
   if (created)
     change_subscriber_->PageCreated(std::move(page_info));
   else
@@ -405,6 +423,9 @@ void DiscardsGraphDumpImpl::SendProcessNotification(
   process_info->id = GetSerializationId(process);
   process_info->pid = process->GetProcessId();
   process_info->private_footprint_kb = process->GetPrivateFootprintKb();
+
+  process_info->description_json = ToJSON(
+      graph_->GetNodeDataDescriberRegistry()->DescribeProcessNodeData(process));
 
   if (created)
     change_subscriber_->ProcessCreated(std::move(process_info));
@@ -435,6 +456,9 @@ void DiscardsGraphDumpImpl::SendWorkerNotification(
        worker->GetChildWorkers()) {
     worker_info->child_worker_ids.push_back(GetSerializationId(child_worker));
   }
+
+  worker_info->description_json = ToJSON(
+      graph_->GetNodeDataDescriberRegistry()->DescribeWorkerNodeData(worker));
 
   if (created)
     change_subscriber_->WorkerCreated(std::move(worker_info));
