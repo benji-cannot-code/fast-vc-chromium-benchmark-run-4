8b49948a4bd74eda2b21a46b656e8a1e3bd1bb31
Ian Prest <iapres@microsoft.com>
2020-04-14 17:27:17 +0000

    SVG: Cache strokes for hit-testing
    This change caches the strokes (converting them to separate paths) used
    for hit-testing in SVG, so they don't need to be recomputed on every
    hit-test / mouse-move.
    
    This change is motivated by a subsequent change that will increase
    hit-testing precision while significantly worsening performance.  By
    caching the stroke path, the aggregate result of the two changes is
    expected to perform better than the current status quo.
    
    Bug: 964614
    Change-Id: I48de70a22c2f8137604f214a699bae258ff741d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140819
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Ian Prest <iapres@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#758897}

--- third_party/blink/renderer/platform/graphics/path.h
+++ third_party/blink/renderer/platform/graphics/path.h
@@ -83,6 +83,7 @@ class PLATFORM_EXPORT Path {
   bool Contains(const FloatPoint&) const;
   bool Contains(const FloatPoint&, WindRule) const;
   bool StrokeContains(const FloatPoint&, const StrokeData&) const;
+  SkPath StrokePath(const StrokeData&) const;
 
   FloatRect BoundingRect() const;
   FloatRect StrokeBoundingRect(const StrokeData&) const;
@@ -193,7 +194,6 @@ class PLATFORM_EXPORT Path {
                   float radius_y,
                   float start_angle,
                   float end_angle);
-  SkPath StrokePath(const StrokeData&) const;
 
   SkPath path_;
 };
