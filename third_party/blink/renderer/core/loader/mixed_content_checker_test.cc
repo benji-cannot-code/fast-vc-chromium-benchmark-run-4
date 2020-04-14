26b747ef106069467af57bdc17f52172ef6da76c
Victor Costan <pwnall@chromium.org>
2020-04-14 13:12:05 +0000

    Roll src/third_party/googletest/src/ 10b1902d8..e3f0319d8 (43 commits)
    https://chromium.googlesource.com/external/github.com/google/googletest.git/+log/10b1902d893e..e3f0319d89f4
    
    This CL also removes references to gmock-generated-matchers.h, which was
    removed from googletest in commit
    41b5f149ab306e96b5b2faf523505d75acffd98a, and to
    gmock-generated-function-mockers.h, which was removed from googletest in
    commit fbf67a70d07543f40832d0cd7a58f304bc6ab1d1.
    
    $ git log 10b1902d8..e3f0319d8 --date=short --no-merges --format='%ad %ae %s'
    2020-04-01 absl-team Googletest export
    2020-03-30 absl-team Googletest export
    2020-03-23 absl-team Googletest export
    2020-03-24 krystian.kuzniarek remove chapters on Autotools, Meson and plain Makefiles
    2020-03-24 krystian.kuzniarek remove dead code in googletest-output-test
    2020-03-24 pkryger Swap settimer and sigaction calls to avoid SIGPROF
    2020-03-20 absl-team Googletest export
    2019-07-11 adam.f.badura Add support for std::function in MockFunction (#2277)
    2019-12-26 adam.f.badura Add tests for MockFunction deduction (#2277)
    2020-03-17 absl-team Googletest export
    2020-03-16 dmauro Googletest export
    2020-03-13 absl-team Googletest export
    2020-03-06 absl-team Googletest export
    2020-03-03 absl-team Googletest export
    2020-03-03 absl-team Googletest export
    2020-03-11 romain.geissler Make sure IsATTY does not clobber errno.
    2020-02-27 absl-team Googletest export
    2020-02-25 absl-team Googletest export
    2020-02-19 absl-team Googletest export
    2020-02-21 johan.mabille Fixed warnings
    2020-02-12 absl-team Googletest export
    2020-02-11 absl-team Googletest export
    2020-02-11 absl-team Googletest export
    2020-02-10 absl-team Googletest export
    2020-02-10 absl-team Googletest export
    2020-02-07 absl-team Googletest export
    2020-02-07 absl-team Googletest export
    2020-02-06 durandal Googletest export
    2020-02-05 absl-team Googletest export
    2020-02-05 absl-team Googletest export
    2020-02-03 absl-team Googletest export
    2020-02-03 absl-team Googletest export
    2020-01-31 absl-team Googletest export
    2020-01-31 absl-team Googletest export
    2020-01-29 absl-team Googletest export
    2020-01-27 absl-team Googletest export
    2020-01-27 absl-team Googletest export
    2020-01-27 absl-team Googletest export
    2020-01-24 absl-team Googletest export
    2020-01-24 absl-team Googletest export
    2020-01-23 absl-team Googletest export
    2020-01-29 krystian.kuzniarek remove a dead reference to the Autotools script
    2019-11-11 krystian.kuzniarek add documentation for the premature-exit-file protocol
    
    Created with:
      roll-dep src/third_party/googletest/src
    
    Change-Id: I61d40d4c3ac7ff0a6dc8f5b04c9b91538c1bebbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2146238
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#758810}

--- third_party/blink/renderer/core/loader/mixed_content_checker_test.cc
+++ third_party/blink/renderer/core/loader/mixed_content_checker_test.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <base/macros.h>
 #include <memory>
 #include "base/memory/scoped_refptr.h"
-#include "testing/gmock/include/gmock/gmock-generated-function-mockers.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/mojom/loader/request_context_frame_type.mojom-blink.h"
 #include "third_party/blink/public/platform/web_mixed_content.h"
