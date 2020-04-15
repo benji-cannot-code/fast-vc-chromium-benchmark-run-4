97ba333f88f25b8eecd4f629a3084bc0f59b918f
dpapad <dpapad@chromium.org>
2020-04-15 00:18:15 +0000

    Migrate chrome://memory-internals to JS modules.
    Also performing several other small cleanups
     1) Eliminate usage of CallJavascriptFunctionUnsafe()
     2) Eliminate global setSaveDumpMessage in favor of a WebUI event.
     3) Eliminate global returnProcessList in favor of a Promise.
     4) Remove bad calls to AllowJavascript(). This method should not
        be called within async C++ callbacks.
     5) Add JS type checking.
     6) Move "Refresh" and "Save" button creation in HTML, no need to
        do this in JS.
     7) Remove unnecessary flattenhtml="true" in grd file.
    
    Bug: 1028829
    Change-Id: I7841982bef7957b7178171ab50211f93deb1d672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1956446
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759075}

--- chrome/browser/resources/memory_internals/memory_internals.js
+++ chrome/browser/resources/memory_internals/memory_internals.js
@@ -3,10 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
+import {addWebUIListener, sendWithPromise} from 'chrome://resources/js/cr.m.js';
+import {$} from 'chrome://resources/js/util.m.js';
 
 function requestProcessList() {
-  chrome.send('requestProcessList');
+  sendWithPromise('requestProcessList').then(onProcessListReceived);
 }
 
 function saveDump() {
@@ -35,11 +36,7 @@ function addListRow(table, celltype, cols) {
   table.appendChild(tr);
 }
 
-function setSaveDumpMessage(data) {
-  $('save_dump_text').innerText = data;
-}
-
-function returnProcessList(data) {
+function onProcessListReceived(data) {
   $('message').innerText = data['message'];
 
   const proclist = $('proclist');
@@ -48,25 +45,7 @@ function returnProcessList(data) {
   const processes = data['processes'];
   if (processes.length == 0) {
     return;
-  }  // No processes to dump, don't make the table and refresh button.
-
-  // Add the refresh and save-dump buttons.
-  const commandsDiv = document.createElement('div');
-  commandsDiv.className = 'commands';
-
-  const refreshButton = document.createElement('button');
-  refreshButton.innerText = '\u21ba Refresh process list';
-  refreshButton.onclick = () => requestProcessList();
-  commandsDiv.appendChild(refreshButton);
-  const saveDumpButton = document.createElement('button');
-  saveDumpButton.innerText = '\u21e9 Save dump';
-  saveDumpButton.onclick = () => saveDump();
-  commandsDiv.appendChild(saveDumpButton);
-  const saveDumpText = document.createElement('div');
-  saveDumpText.id = 'save_dump_text';
-  commandsDiv.appendChild(saveDumpText);
-
-  proclist.appendChild(commandsDiv);
+  }  // No processes to dump, don't make the table.
 
   const table = document.createElement('table');
 
@@ -82,13 +61,11 @@ function returnProcessList(data) {
     const description = document.createTextNode(proc[1]);
     const profiled = proc[2];
 
-    let button = null;
+    const button = document.createElement('button');
     if (profiled) {
-      button = document.createElement('button');
       button.innerText = '\uD83D\uDC1E Report';
       button.onclick = () => reportProcess(procId);
     } else {
-      button = document.createElement('button');
       button.innerText = '\u2600 Start profiling';
       button.onclick = () => startProfiling(procId);
     }
@@ -100,11 +77,20 @@ function returnProcessList(data) {
 }
 
 // Get data and have it displayed upon loading.
-document.addEventListener('DOMContentLoaded', requestProcessList);
+document.addEventListener('DOMContentLoaded', () => {
+  $('refresh').onclick = requestProcessList;
+  $('save').onclick = saveDump;
+
+  addWebUIListener('save-dump-progress', progress => {
+    $('save-dump-text').innerText = progress;
+  });
+
+  requestProcessList();
+});
 
 /* For manual testing.
 function fakeResults() {
-  returnProcessList([
+  onProcessListReceived([
     [ 11234, "Process 11234 [Browser]" ],
     [ 11235, "Process 11235 [Renderer]" ],
     [ 11236, "Process 11236 [Renderer]" ]]);
