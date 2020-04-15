dfdba8f36f9bb7efa82cf0297b1d827e3b15b57d
Roman Sorokin <rsorokin@chromium.org>
2020-04-15 07:08:20 +0000

    Introduce LocalStateMixin
    Bug: 1064271
    Change-Id: Ia618042d9e100c17b90aa3f4f830f97854322836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144152
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759164}

--- chrome/browser/chromeos/login/test/device_state_mixin.h
+++ chrome/browser/chromeos/login/test/device_state_mixin.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/login/demo_mode/demo_session.h"
+#include "chrome/browser/chromeos/login/test/local_state_mixin.h"
 #include "chrome/browser/chromeos/login/test/scoped_policy_update.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 
@@ -31,7 +32,8 @@ namespace chromeos {
 // It will initialized the fake in-memory client in
 // SetUpInProcessBrowserTestFixture(), provided that a session manager was not
 // initialized previously.
-class DeviceStateMixin : public InProcessBrowserTestMixin {
+class DeviceStateMixin : public InProcessBrowserTestMixin,
+                         public LocalStateMixin::Delegate {
  public:
   enum class State {
     BEFORE_OOBE,
@@ -48,8 +50,9 @@ class DeviceStateMixin : public InProcessBrowserTestMixin {
   // InProcessBrowserTestMixin:
   bool SetUpUserDataDirectory() override;
   void SetUpInProcessBrowserTestFixture() override;
-  void CreatedBrowserMainParts(
-      content::BrowserMainParts* browser_main_parts) override;
+
+  // LocalStateMixin::Delegate:
+  void SetUpLocalState() override;
 
   // Returns a ScopedDevicePolicyUpdate instance that can be used to update
   // local device policy blob (kept in fake session manager client).
@@ -113,6 +116,8 @@ class DeviceStateMixin : public InProcessBrowserTestMixin {
   policy::DevicePolicyBuilder device_policy_;
   std::map<std::string, policy::UserPolicyBuilder>
       device_local_account_policies_;
+
+  LocalStateMixin local_state_mixin_;
 
   base::WeakPtrFactory<DeviceStateMixin> weak_factory_{this};
 
--- chrome/browser/chromeos/login/test/login_manager_mixin.h
+++ chrome/browser/chromeos/login/test/login_manager_mixin.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/macros.h"
+#include "chrome/browser/chromeos/login/test/local_state_mixin.h"
 #include "chrome/browser/chromeos/login/test/session_flags_manager.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "components/account_id/account_id.h"
@@ -26,7 +27,8 @@ class UserContext;
 // initializes user manager with a list of pre-registered users.
 // The mixin will mark the OOBE flow as complete during test setup, so it's not
 // suitable for OOBE tests.
-class LoginManagerMixin : public InProcessBrowserTestMixin {
+class LoginManagerMixin : public InProcessBrowserTestMixin,
+                          public LocalStateMixin::Delegate {
  public:
   // Represents test user.
   struct TestUserInfo {
@@ -88,10 +90,11 @@ class LoginManagerMixin : public InProcessBrowserTestMixin {
 
   // InProcessBrowserTestMixin:
   bool SetUpUserDataDirectory() override;
-  void CreatedBrowserMainParts(
-      content::BrowserMainParts* browser_main_parts) override;
   void SetUpOnMainThread() override;
   void TearDownOnMainThread() override;
+
+  // LocalStateMixin::Delegate:
+  void SetUpLocalState() override;
 
   // Starts login attempt for a user, using the stub authenticator provided by
   // |authenticator_builder|.
@@ -126,6 +129,7 @@ class LoginManagerMixin : public InProcessBrowserTestMixin {
   // Whether the user session manager should skip browser launch steps for
   // testing.
   bool should_launch_browser_ = false;
+  LocalStateMixin local_state_mixin_;
 
   DISALLOW_COPY_AND_ASSIGN(LoginManagerMixin);
 };
