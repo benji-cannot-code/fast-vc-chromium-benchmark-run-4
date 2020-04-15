9fe3d419e8e1f5797365692cbd802bae866ddd9e
Aaron Leventhal <aleventhal@google.com>
2020-04-15 17:10:20 +0000

    Helper for debugging tree serialization
    Bug: None
    Change-Id: Idf1b8b56f204165ce60f8b73fd2d993824c44134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2148542
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759303}

--- content/renderer/accessibility/blink_ax_tree_source.h
+++ content/renderer/accessibility/blink_ax_tree_source.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <set>
+#include <string>
 
 #include "base/optional.h"
 #include "content/common/ax_content_node_data.h"
@@ -113,6 +114,7 @@ class BlinkAXTreeSource
   bool IsEqual(blink::WebAXObject node1,
                blink::WebAXObject node2) const override;
   blink::WebAXObject GetNull() const override;
+  std::string GetDebugString(blink::WebAXObject node) const override;
 
   blink::WebDocument GetMainDocument() const;
 
--- ui/views/accessibility/ax_virtual_view_wrapper.h
+++ ui/views/accessibility/ax_virtual_view_wrapper.h
@@ -32,6 +32,7 @@ class AXVirtualViewWrapper : public AXAuraObjWrapper {
   void Serialize(ui::AXNodeData* out_node_data) override;
   int32_t GetUniqueId() const override;
   bool HandleAccessibleAction(const ui::AXActionData& action) override;
+  std::string ToString() const override;
 
  private:
   // Weak.
