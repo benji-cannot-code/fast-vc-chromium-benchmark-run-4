6c2b4ea9bb28db5292ce063ec3c3824d83179e7f
Alan Cutter <alancutter@chromium.org>
2020-04-15 07:59:18 +0000

    Fix warnings for GLImageBindTest and GLImageZeroInitializeTest
    This CL splits the definition of GLImageBindTest and
    GLImageZeroInitializeTest out into their own header files so source
    files that don't use them don't trigger the following warnings in
    gl_unittests:
    The following parameterized test case is not instantiated: UninstantiatedTypeParamaterizedTestSuite<GLImageBindTest>
    The following parameterized test case is not instantiated: UninstantiatedTypeParamaterizedTestSuite<GLImageZeroInitializeTest>
    
    Bug: 1045846
    Change-Id: If64404d069b60405c6333f8a77abda85a5b11bd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143777
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759173}

--- ui/gl/gl_image_ahardwarebuffer_unittest.cc
+++ ui/gl/gl_image_ahardwarebuffer_unittest.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gl/gl_image_ahardwarebuffer.h"
+#include "ui/gl/test/gl_image_bind_test_template.h"
 #include "ui/gl/test/gl_image_test_template.h"
+#include "ui/gl/test/gl_image_zero_initialize_test_template.h"
 
 namespace gl {
 namespace {
