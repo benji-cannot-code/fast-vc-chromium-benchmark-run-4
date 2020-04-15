72d6cb120b2b7832ab5c194065fe7782efc23baf
Yuki Shiino <yukishiino@chromium.org>
2020-04-15 12:39:38 +0000

    v8bindings: Fix History.state === PopStateEvent.state
    "history.state === popStateEvent.state" should be evaluated to
    true, however, the result changes depending on their evaluation
    order.  See the bug for details.
    
    This patch removes the custom bindings of PopStateEvent.state
    and reimplements History.state and PopStateEvent.state in a
    less hacky way, moving caching with V8 private property from
    Blink-V8 bindings into Blink implementation.
    
    WPT failures in this patch are expected.  I'll send a fix of
    WPT separately.
    
    Bug: 1070938
    Change-Id: Iaf9bdbc6545cdbe18d8f3542944dfce61c443da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147488
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759223}

--- third_party/blink/renderer/core/frame/history.h
+++ third_party/blink/renderer/core/frame/history.h
@@ -54,7 +54,7 @@ class CORE_EXPORT History final : public ScriptWrappable,
   explicit History(LocalFrame*);
 
   unsigned length(ExceptionState&) const;
-  ScriptValue state(v8::Isolate*, ExceptionState&);
+  ScriptValue state(ScriptState*, ExceptionState&);
 
   void back(ScriptState*, ExceptionState&);
   void forward(ScriptState*, ExceptionState&);
@@ -75,7 +75,6 @@ class CORE_EXPORT History final : public ScriptWrappable,
   void setScrollRestoration(const String& value, ExceptionState&);
   String scrollRestoration(ExceptionState&);
 
-  bool stateChanged() const;
   bool IsSameAsCurrentState(SerializedScriptValue*) const;
 
   void Trace(Visitor*) override;
