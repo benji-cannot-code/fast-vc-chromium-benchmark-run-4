e2d74a307b308c1b0a684c2b8699163c304d0edf
Rohit Rao <rohitrao@chromium.org>
2020-04-15 02:56:54 +0000

    [ios] Temporary fix for CRWSessionStorage being released early in ClosingWebStateObserver.
    RestoreIOSLiveTab is not compiled with ARC and therefore is not properly
    retaining the CRWSessionStorage* which it is passed. Temporarily work
    around this bug by keeping a strong pointer to the CRWSessionStorage* in
    ClosingWebStateObserver, until RestoreIOSLiveTab can be fixed properly.
    
    BUG=1070852
    
    Change-Id: Ic3d8a4d0cc3c29bfae4f657de06b98aa869b3bc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2150035
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759116}

--- ios/chrome/browser/tabs/closing_web_state_observer.mm
+++ ios/chrome/browser/tabs/closing_web_state_observer.mm
@@ -68,8 +68,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   web::NavigationManager* navigationManager = webState->GetNavigationManager();
   if (navigationManager->IsRestoreSessionInProgress()) {
-    auto live_tab = std::make_unique<sessions::RestoreIOSLiveTab>(
-        webState->BuildSessionStorage());
+    // TODO(crbug.com/1070852): RestoreIOSLiveTab is not compiled with ARC and
+    // therefore is not properly retaining the CRWSessionStorage* which it is
+    // passed. Temporarily work around this bug by keeping a strong pointer to
+    // the CRWSessionStorage* here, until RestoreIOSLiveTab can be fixed
+    // properly.
+    CRWSessionStorage* storage = webState->BuildSessionStorage();
+    auto live_tab = std::make_unique<sessions::RestoreIOSLiveTab>(storage);
     _restoreService->CreateHistoricalTab(live_tab.get(), atIndex);
     return;
   }
