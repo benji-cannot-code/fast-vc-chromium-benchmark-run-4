947e7271d8c3b5df7f70387cdb9b7f0281872db7
Ewann <ewannpv@google.com>
2020-04-15 13:34:31 +0000

    [iOS] Adds Cookies description item
    This CL adds Cokkies description to the Cookies screen.
    screenshot: https://drive.google.com/a/google.com/file/d/1Q_cwV1DR1CFZzFULvq23Rd8c-Z6LPxsC/view?usp=sharing
    
    Bug: 1064961
    Change-Id: I367c2e2e481687bd5bca7874c2ca7562672ba390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2134257
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759236}

--- ios/chrome/browser/ui/settings/privacy/cookies_coordinator.mm
+++ ios/chrome/browser/ui/settings/privacy/cookies_coordinator.mm
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   self.viewController = [[PrivacyCookiesViewController alloc]
-      initWithStyle:UITableViewStylePlain];
+      initWithStyle:UITableViewStyleGrouped];
 
   DCHECK(self.baseNavigationController);
   [self.baseNavigationController pushViewController:self.viewController
