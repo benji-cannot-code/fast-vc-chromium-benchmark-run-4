cb3beb08c16ff2ed94c0aa4e9f3c2c9089df993e
Nathan Zabriskie <nazabris@microsoft.com>
2020-04-15 23:10:27 +0000

    Add WritePixels API to RasterImplementation
    RasterImplementation currently has no API for uploading textures
    directly from CPU memory. The need for an intermediate texture incurs
    unnecessary copies which decrease performance, especially in repeated
    use cases. This CL adds one option for uploading a texture stored in
    CPU memory directly to a shared image mailbox.
    
    Bug: 1063725
    Change-Id: Iaf0ce530650913cd38f17ade3160b5129e96f0a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141068
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Nathan Zabriskie <nazabris@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#759453}

--- gpu/command_buffer/common/raster_cmd_ids_autogen.h
+++ gpu/command_buffer/common/raster_cmd_ids_autogen.h
@@ -32,9 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   OP(DeletePaintCachePathsINTERNALImmediate)     /* 272 */ \
   OP(ClearPaintCacheINTERNAL)                    /* 273 */ \
   OP(CopySubTextureINTERNALImmediate)            /* 274 */ \
-  OP(TraceBeginCHROMIUM)                         /* 275 */ \
-  OP(TraceEndCHROMIUM)                           /* 276 */ \
-  OP(SetActiveURLCHROMIUM)                       /* 277 */
+  OP(WritePixelsINTERNALImmediate)               /* 275 */ \
+  OP(TraceBeginCHROMIUM)                         /* 276 */ \
+  OP(TraceEndCHROMIUM)                           /* 277 */ \
+  OP(SetActiveURLCHROMIUM)                       /* 278 */
 
 enum CommandId {
   kOneBeforeStartPoint =
