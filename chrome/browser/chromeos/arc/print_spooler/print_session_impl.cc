31d849bbd43e9a7eadba55f5e22d447cc1e008d3
Jesse Schettler <jschettler@chromium.org>
2020-04-15 19:35:19 +0000

    Revert "arc: Add logging to investigate ARC print failures"
    This reverts commit f64a6c0af85e65137cf9b4202c00af4c3a4bc573.
    
    Reason for revert: Logging no longer needed.
    
    Original change's description:
    > arc: Add logging to investigate ARC print failures
    > 
    > Print preview occasionally fails in the arc.Print tast test. Add logging
    > to investigate these failures.
    > 
    > Bug: b:152251595
    > Change-Id: I5b8b18213ec8c7fc2a56646c65dfe5262b86f677
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144456
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#758262}
    
    TBR=thestig@chromium.org,jschettler@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: b:152251595
    Change-Id: Iaaa10289a1ba47b7415b1fa820702d9791ad19ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2150817
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759365}

--- chrome/browser/chromeos/arc/print_spooler/print_session_impl.cc
+++ chrome/browser/chromeos/arc/print_spooler/print_session_impl.cc
@@ -147,8 +147,6 @@ base::ReadOnlySharedMemoryRegion ReadPreviewDocument(
   base::PlatformFile platform_file;
   if (mojo::UnwrapPlatformFile(std::move(preview_document), &platform_file) !=
       MOJO_RESULT_OK) {
-    // TODO(jschettler): Remove logging when finished investigating b/152251595.
-    LOG(ERROR) << "Failed to unwrap platform file.";
     return base::ReadOnlySharedMemoryRegion();
   }
 
@@ -264,8 +262,6 @@ void PrintSessionImpl::CreatePreviewDocument(
   mojom::PrintDocumentRequestPtr request =
       PrintDocumentRequestFromJobSettings(job_settings);
   if (!request || !request->attributes) {
-    // TODO(jschettler): Remove logging when finished investigating b/152251595.
-    LOG(ERROR) << "Failed to create print document request.";
     std::move(callback).Run(base::ReadOnlySharedMemoryRegion());
     return;
   }
@@ -285,8 +281,6 @@ void PrintSessionImpl::OnPreviewDocumentCreated(
     int64_t data_size) {
   if (data_size < kMinimumPdfSize ||
       !base::IsValueInRangeForNumericType<size_t>(data_size)) {
-    // TODO(jschettler): Remove logging when finished investigating b/152251595.
-    LOG(ERROR) << "Invalid data size for preview document: " << data_size;
     std::move(callback).Run(base::ReadOnlySharedMemoryRegion());
     return;
   }
@@ -305,8 +299,6 @@ void PrintSessionImpl::OnPreviewDocumentRead(
     CreatePreviewDocumentCallback callback,
     base::ReadOnlySharedMemoryRegion preview_document_region) {
   if (!preview_document_region.IsValid()) {
-    // TODO(jschettler): Remove logging when finished investigating b/152251595.
-    LOG(ERROR) << "Invalid memory region for preview document.";
     std::move(callback).Run(std::move(preview_document_region));
     return;
   }
