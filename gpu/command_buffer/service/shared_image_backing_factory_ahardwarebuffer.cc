64450bc1a83911555e6b644a7571ce42bc9dfde9
Peng Huang <penghuang@chromium.org>
2020-04-15 14:26:50 +0000

    Fix a crash in WrappedSkImage and canvas issues in SharedImageAHB
    wrapped_sk_image()->GetSkSurface() may return nullptr if the deive is
    lost, so we need to check the result before using it to avoid crash.
    
    This CL also improves SharedImageAHB to recreate SkSurface, if surface
    properties are changed. And it also resets the canvas when write access
    is done.
    
    Bug: None
    Change-Id: Ic9a7a966ac7063a74f650c1b04c2ee6ba475d918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2145467
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759251}

--- gpu/command_buffer/service/shared_image_backing_factory_ahardwarebuffer.cc
+++ gpu/command_buffer/service/shared_image_backing_factory_ahardwarebuffer.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/vulkan_image.h"
 #include "gpu/vulkan/vulkan_implementation.h"
 #include "gpu/vulkan/vulkan_util.h"
+#include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkPromiseImageTexture.h"
 #include "third_party/skia/include/gpu/GrBackendSemaphore.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
@@ -307,16 +308,31 @@ class SharedImageRepresentationSkiaVkAHB
     if (!BeginAccess(false /* readonly */, begin_semaphores, end_semaphores))
       return nullptr;
 
-    if (!surface_) {
+    auto* gr_context = context_state_->gr_context();
+    if (gr_context->abandoned()) {
+      LOG(ERROR) << "GrContext is abandoned.";
+      return nullptr;
+    }
+
+    if (!surface_ || final_msaa_count != surface_msaa_count_ ||
+        surface_props != surface_->props()) {
       SkColorType sk_color_type = viz::ResourceFormatToClosestSkColorType(
           /*gpu_compositing=*/true, format());
       surface_ = SkSurface::MakeFromBackendTextureAsRenderTarget(
-          context_state_->gr_context(), promise_texture_->backendTexture(),
+          gr_context, promise_texture_->backendTexture(),
           kTopLeft_GrSurfaceOrigin, final_msaa_count, sk_color_type,
           color_space().ToSkColorSpace(), &surface_props);
+      if (!surface_) {
+        LOG(ERROR) << "MakeFromBackendTextureAsRenderTarget() failed.";
+        return nullptr;
+      }
+      surface_msaa_count_ = final_msaa_count;
     }
 
-    DCHECK(surface_);
+    int count = surface_->getCanvas()->save();
+    DCHECK_EQ(count, 1);
+    ALLOW_UNUSED_LOCAL(count);
+
     return surface_;
   }
 
@@ -326,7 +342,7 @@ class SharedImageRepresentationSkiaVkAHB
 
     surface.reset();
     DCHECK(surface_->unique());
-
+    surface_->getCanvas()->restoreToCount(1);
     EndAccess(false /* readonly */);
   }
 
@@ -452,6 +468,7 @@ class SharedImageRepresentationSkiaVkAHB
   std::unique_ptr<VulkanImage> vulkan_image_;
   sk_sp<SkPromiseImageTexture> promise_texture_;
   RepresentationAccessMode mode_ = RepresentationAccessMode::kNone;
+  int surface_msaa_count_ = 0;
   sk_sp<SkSurface> surface_;
   scoped_refptr<SharedContextState> context_state_;
   VkSemaphore end_access_semaphore_ = VK_NULL_HANDLE;
