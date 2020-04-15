82886b73496a156ba89cab670d5d18bb03609788
Cliff Smolinsky <cliffsmo@microsoft.com>
2020-04-15 16:35:07 +0000

    Add comment for BrowserChangeObserver
    BrowserChangeObserver was just moved to be a publicly available class,
    so this change adds a comment regarding how it should be used (or not
    used, as the case may be).
    
    Bug: 1052096
    Change-Id: I7cdff02f7e2d87b65f17a61600a54e336b0671cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149913
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Cliff Smolinsky <cliffsmo@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#759286}

--- chrome/test/base/ui_test_utils.h
+++ chrome/test/base/ui_test_utils.h
@@ -351,6 +351,8 @@ class HistoryEnumerator {
   DISALLOW_COPY_AND_ASSIGN(HistoryEnumerator);
 };
 
+// In general, tests should use WaitForBrowserToClose() and
+// WaitForBrowserToOpen() rather than instantiating this class directly.
 class BrowserChangeObserver : public BrowserListObserver {
  public:
   enum class ChangeType {
