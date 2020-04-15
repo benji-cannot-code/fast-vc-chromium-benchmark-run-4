4461c7031f72fd4b353b295c74da0c5168ab233f
Brian Manthos <brianman@microsoft.com>
2020-04-15 06:47:20 +0000

    remove kMultiInstallAppGuid
    Change-Id: Ia36d7590b74e3f4a8fb51f66b5ea8d3100baaf0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149888
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759159}

--- chrome/installer/mini_installer/appid.h
+++ chrome/installer/mini_installer/appid.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace google_update {
 
 extern const wchar_t kAppGuid[];
-extern const wchar_t kMultiInstallAppGuid[];
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 extern const wchar_t kBetaAppGuid[];
