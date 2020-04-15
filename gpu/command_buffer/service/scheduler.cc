067046994e19792eb53b2106d15c0b20b51d4354
kylechar <kylechar@chromium.org>
2020-04-15 13:51:10 +0000

    Add GPU.Scheduler.RunTaskTime histogram
    The histogram records the wall time for each GPU task. This will
    eventually be used to add a slow report trigger to capture traces for
    long running tasks from canary.
    
    Bug: 1064662
    Change-Id: I30ec4a7f583fc197f916a55eae7dd3b1fbf57bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144340
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: kylechar <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759239}

--- gpu/command_buffer/service/scheduler.cc
+++ gpu/command_buffer/service/scheduler.cc
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
+#include "base/timer/elapsed_timer.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/traced_value.h"
 #include "gpu/command_buffer/service/sync_point_manager.h"
@@ -519,6 +521,7 @@ void Scheduler::RunNextTask() {
   scheduling_queue_.pop_back();
 
   TRACE_EVENT1("gpu", "Scheduler::RunNextTask", "state", state.AsValue());
+  base::ElapsedTimer task_timer;
 
   Sequence* sequence = GetSequence(state.sequence_id);
   DCHECK(sequence);
@@ -567,6 +570,11 @@ void Scheduler::RunNextTask() {
                      &SchedulingState::Comparator);
     }
   }
+
+  UMA_HISTOGRAM_CUSTOM_MICROSECONDS_TIMES(
+      "GPU.Scheduler.RunTaskTime", task_timer.Elapsed(),
+      base::TimeDelta::FromMicroseconds(10), base::TimeDelta::FromSeconds(30),
+      100);
 
   task_runner_->PostTask(FROM_HERE,
                          base::BindOnce(&Scheduler::RunNextTask, weak_ptr_));
