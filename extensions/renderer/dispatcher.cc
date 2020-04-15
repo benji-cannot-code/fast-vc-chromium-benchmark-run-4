a978440509b1bb3e17014834b75e6fe0f78813c2
dpapad <dpapad@chromium.org>
2020-04-15 21:11:31 +0000

    Replace usages of GetRawDataResource in extensions/renderer/
    GetRawDataResource assumes that underlying files are not compressed,
    whereas LoadDataResourceString automatically decompresses if necessary.
    
    This is one of many CLs, in preparation of compressing all HTML, JS,
    CSS, SVG files by default (meaning without requiring compress="gzip"
    to be specified).
    
    Bug: 1068407
    Change-Id: I59e9f8654e85a780d6152b4e2f9e36e34dc84da4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2145854
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759414}

--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/lazy_instance.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/ref_counted_memory.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
 #include "base/strings/string_piece.h"
@@ -497,9 +498,11 @@ void Dispatcher::WillEvaluateServiceWorkerOnWorkerThread(
   v8::Isolate* isolate = context->isolate();
 
   // Fetch the source code for service_worker_bindings.js.
-  base::StringPiece script_resource =
-      ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
+  scoped_refptr<base::RefCountedMemory> bytes =
+      ui::ResourceBundle::GetSharedInstance().LoadDataResourceBytes(
           IDR_SERVICE_WORKER_BINDINGS_JS);
+  base::StringPiece script_resource(
+      reinterpret_cast<const char*>(bytes->front()), bytes->size());
   v8::Local<v8::String> script =
       v8::String::NewExternalOneByte(
           isolate, new StaticV8ExternalOneByteStringResource(script_resource))
@@ -619,9 +622,9 @@ void Dispatcher::DidCreateDocumentElement(blink::WebLocalFrame* frame) {
       (extension->is_extension() || extension->is_platform_app())) {
     int resource_id = extension->is_platform_app() ? IDR_PLATFORM_APP_CSS
                                                    : IDR_EXTENSION_FONTS_CSS;
-    std::string stylesheet = ui::ResourceBundle::GetSharedInstance()
-                                 .GetRawDataResource(resource_id)
-                                 .as_string();
+    std::string stylesheet =
+        ui::ResourceBundle::GetSharedInstance().LoadDataResourceString(
+            resource_id);
     base::ReplaceFirstSubstringAfterOffset(
         &stylesheet, 0, "$FONTFAMILY", system_font_family_);
     base::ReplaceFirstSubstringAfterOffset(
@@ -638,11 +641,10 @@ void Dispatcher::DidCreateDocumentElement(blink::WebLocalFrame* frame) {
   if (extension && extension->is_extension() &&
       OptionsPageInfo::ShouldUseChromeStyle(extension) &&
       effective_document_url == OptionsPageInfo::GetOptionsPage(extension)) {
-    base::StringPiece extension_css =
-        ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
+    std::string extension_css =
+        ui::ResourceBundle::GetSharedInstance().LoadDataResourceString(
             IDR_EXTENSION_CSS);
-    frame->GetDocument().InsertStyleSheet(
-        WebString::FromUTF8(extension_css.data(), extension_css.length()));
+    frame->GetDocument().InsertStyleSheet(WebString::FromUTF8(extension_css));
   }
 }
 
