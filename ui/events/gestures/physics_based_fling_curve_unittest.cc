dad9e0eef9855054fbf5e0797fac510f256d70d8
Wez <wez@chromium.org>
2020-04-15 13:04:42 +0000

    Revert "FlingBoost with experimental fling animation curve"
    This reverts commit 8e795d5bdcf1d9074040e3278be3e2cf09d0a38f.
    
    Reason for revert: Introduces FlingControllerWithPhysicsBasedFlingTest
    suite, in which ControllerDoesntBoostFinishedFling test flakes across
    Fuchsia, Windows, Linux, and Mac platforms. 
    
    Original change's description:
    > FlingBoost with experimental fling animation curve
    > 
    > Currently, if physics based fling curve is enabled, even if the fling is
    > boosted, the scroll distance is capped by upper bound which is three
    > times the size of viewport. Thus it nullify the effect of fling boosting
    > and travels the same distance as it would if fling is not boosted.
    > 
    > This CL fixes fling boosting with physics based fling curve. It does
    > so by increasing the upper bound every time a fling is boosted.
    > 
    > Bug: 647137
    > Change-Id: Ib2dd51b37df91168ae1f1e8d33888e3b5dc43e23
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2118218
    > Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Reviewed-by: Daniel Libby <dlibby@microsoft.com>
    > Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#758878}
    
    TBR=bokan@chromium.org,nzolghadr@chromium.org,dlibby@microsoft.com,sahir.vellani@microsoft.com
    
    Change-Id: Ibfc9e51bf5f86c7d6a626d761c20b3ce97652af6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 647137, 1070776
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144087
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759229}

--- ui/events/gestures/physics_based_fling_curve_unittest.cc
+++ ui/events/gestures/physics_based_fling_curve_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 const float kDefaultPixelsPerInch = 96.f;
-const float kBoostMultiplierUnboosted = 1.f;
 
 TEST(PhysicsBasedFlingCurveTest, BasicFlingTestVelocityY) {
   const gfx::Vector2dF fling_velocity(0, 5000);
@@ -19,8 +18,7 @@ TEST(PhysicsBasedFlingCurveTest, BasicFlingTestVelocityY) {
                                        kDefaultPixelsPerInch);
   const gfx::Size viewport(1920, 1080);
 
-  PhysicsBasedFlingCurve curve(fling_velocity, now, pixels_per_inch,
-                               kBoostMultiplierUnboosted, viewport);
+  PhysicsBasedFlingCurve curve(fling_velocity, now, pixels_per_inch, viewport);
 
   gfx::Vector2dF offset;
   gfx::Vector2dF velocity;
@@ -69,8 +67,7 @@ TEST(PhysicsBasedFlingCurveTest, BasicFlingTestVelocityX) {
                                        kDefaultPixelsPerInch);
   const gfx::Size viewport(1920, 1080);
 
-  PhysicsBasedFlingCurve curve(fling_velocity, now, pixels_per_inch,
-                               kBoostMultiplierUnboosted, viewport);
+  PhysicsBasedFlingCurve curve(fling_velocity, now, pixels_per_inch, viewport);
 
   gfx::Vector2dF offset;
   gfx::Vector2dF velocity;
@@ -118,8 +115,7 @@ TEST(PhysicsBasedFlingCurveTest, BasicFlingTestVelocityXAndY) {
                                        kDefaultPixelsPerInch);
   const gfx::Size viewport(1920, 1080);
 
-  PhysicsBasedFlingCurve curve(fling_velocity, now, pixels_per_inch,
-                               kBoostMultiplierUnboosted, viewport);
+  PhysicsBasedFlingCurve curve(fling_velocity, now, pixels_per_inch, viewport);
 
   gfx::Vector2dF offset;
   gfx::Vector2dF velocity;
@@ -167,8 +163,7 @@ TEST(PhysicsBasedFlingCurveTest, ControlPointsWithSlopeLessThan1) {
                                        kDefaultPixelsPerInch);
   const gfx::Size viewport(1920, 1080);
 
-  PhysicsBasedFlingCurve curve(velocity, now, pixels_per_inch,
-                               kBoostMultiplierUnboosted, viewport);
+  PhysicsBasedFlingCurve curve(velocity, now, pixels_per_inch, viewport);
 
   EXPECT_EQ(0.20f, curve.p1_for_testing().x());
   EXPECT_NEAR(curve.p1_for_testing().y(), 0.43f, 0.01f);
@@ -183,8 +178,7 @@ TEST(PhysicsBasedFlingCurveTest, ControlPointsWithSlopeGreaterThan1) {
                                        kDefaultPixelsPerInch);
   const gfx::Size viewport(1920, 1080);
 
-  PhysicsBasedFlingCurve curve(velocity, now, pixels_per_inch,
-                               kBoostMultiplierUnboosted, viewport);
+  PhysicsBasedFlingCurve curve(velocity, now, pixels_per_inch, viewport);
 
   EXPECT_NEAR(curve.p1_for_testing().x(), 0.19f, 0.01f);
   EXPECT_EQ(curve.p1_for_testing().y(), 1.0f);
