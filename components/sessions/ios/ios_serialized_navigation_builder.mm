7a5cb7e283191e107638479cc2b6d95aa53d0a71
Rohit Rao <rohitrao@chromium.org>
2020-04-15 14:56:28 +0000

    Compiles //components/sessions/core/ios with ARC.
    These files were inadvertently missing ARC guards and were not being
    compiled with ARC.
    
    BUG=1070852,1070933
    
    Change-Id: I1005d41c26233d858aaee3670d3ce8b69ab561a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2150300
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759257}

--- components/sessions/ios/ios_serialized_navigation_builder.mm
+++ components/sessions/ios/ios_serialized_navigation_builder.mm
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/navigation/referrer.h"
 #include "ios/web/public/session/crw_navigation_item_storage.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace sessions {
 
 // static
