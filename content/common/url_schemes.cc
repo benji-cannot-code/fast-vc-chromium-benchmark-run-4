c9a06987469cf1809f0af141605d8ce0dfb791d5
Mike West <mkwst@chromium.org>
2020-04-15 06:57:00 +0000

    Add `devtools:` as a secure context.
    We trust `devtools:` in the same way we trust `chrome:`. We should
    consider these resources to be secure contexts, as this will enable
    us to harden them with mechanisms like Trusted Types that are only
    available in such contexts.
    
    Bug: 1070526
    Change-Id: I0ac60c642e1afd3f81d669d60a399a94ab0f0751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2145882
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759163}

--- content/common/url_schemes.cc
+++ content/common/url_schemes.cc
@@ -70,6 +70,7 @@ void RegisterContentSchemes() {
   for (auto& scheme : schemes.referrer_schemes)
     url::AddReferrerScheme(scheme.c_str(), url::SCHEME_WITH_HOST);
 
+  schemes.secure_schemes.push_back(kChromeDevToolsScheme);
   schemes.secure_schemes.push_back(kChromeUIScheme);
   schemes.secure_schemes.push_back(kChromeUIUntrustedScheme);
   schemes.secure_schemes.push_back(kChromeErrorScheme);
