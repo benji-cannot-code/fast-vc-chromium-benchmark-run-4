612bd089d88a49732fcc2e2a1a263a20fe2c890a
Vasilii Sukhanov <vasilii@chromium.org>
2020-04-15 15:26:13 +0000

    Add a skeleton controller for the 'compromised passwords' bubble.
    Mocks: https://docs.google.com/presentation/d/1yWuZO-fjXhdRp64nWRbnHEyzpFd1KOMS3u1_1XMdGuA/edit#slide=id.g7c9229dfe2_22_49
    
    Bug: 1049200
    Change-Id: Ifa3e6e0211fb9ed9c4f81ec77d460c6970f5ee97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144017
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759267}

---
+++ chrome/browser/ui/passwords/bubble_controllers/post_save_compromised_bubble_controller.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/passwords/bubble_controllers/post_save_compromised_bubble_controller.h"
+
+PostSaveCompromisedBubbleController::PostSaveCompromisedBubbleController(
+    base::WeakPtr<PasswordsModelDelegate> delegate)
+    : PasswordBubbleControllerBase(
+          std::move(delegate),
+          password_manager::metrics_util::
+              AUTOMATIC_COMPROMISED_CREDENTIALS_REMINDER) {}
+
+PostSaveCompromisedBubbleController::~PostSaveCompromisedBubbleController() {
+  // Make sure the interactions are reported even if Views didn't notify the
+  // controller about the bubble being closed.
+  if (!interaction_reported_)
+    OnBubbleClosing();
+}
+
+base::string16 PostSaveCompromisedBubbleController::GetTitle() const {
+  return base::string16();
+}
+
+void PostSaveCompromisedBubbleController::ReportInteractions() {}
