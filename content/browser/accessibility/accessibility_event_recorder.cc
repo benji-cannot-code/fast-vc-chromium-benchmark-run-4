bee71b8956f1bdcb9496fb1601ef32b7a93954a6
Alexander Surkov <asurkov@igalia.com>
2020-04-15 17:42:20 +0000

    Drop AccessibilityTreeFormatterStub
    No value to produce a bare accessible tree on platforms having no accessibility support. Native blink accessibility tree can be used instead
    
    Bug: 1070310
    Change-Id: I8aa537a6440e6da34f70defb1b8d72b11a1874e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149500
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Alexander Surkkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#759317}

--- content/browser/accessibility/accessibility_event_recorder.cc
+++ content/browser/accessibility/accessibility_event_recorder.cc
@@ -36,7 +36,6 @@ AccessibilityEventRecorder::GetTestPasses() {
 #else   // defined(OS_ANDROID)
   return {
       {"blink", &AccessibilityEventRecorder::Create},
-      {"native", &AccessibilityEventRecorder::Create},
   };
 #endif  // defined(OS_ANDROID)
 }
