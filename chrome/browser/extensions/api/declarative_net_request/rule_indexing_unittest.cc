2a70ad8512f5d6cf6975b225839f122616459fca
Karandeep Bhatia <karandeepb@chromium.org>
2020-04-15 23:53:20 +0000

    DNR: De-flake and re-enable some disabled tests.
    This CL attempts to remove some possible sources of flakiness in the
    declarative net request browsertests by removing all calls to
    content::RunAllTasksUntilIdle() and replacing them with more
    deterministic waiting logic. Also re-enable the tests disabled in
    r758084.
    
    Also, enforce that RulesetManager only has a single test observer at a
    time.
    
    BUG=1069665
    
    Change-Id: I29c646d85322195894d2786fbf01fb7c10842cb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2146335
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759468}

--- chrome/browser/extensions/api/declarative_net_request/rule_indexing_unittest.cc
+++ chrome/browser/extensions/api/declarative_net_request/rule_indexing_unittest.cc
@@ -926,9 +926,9 @@ TEST_P(MultipleRulesetsIndexingTest, EnabledRulesCount) {
   AddRuleset(CreateRuleset("2.json", 200, 20, false));
   AddRuleset(CreateRuleset("3.json", 300, 30, true));
 
-  RulesetCountWaiter waiter(manager());
+  RulesetManagerObserver ruleset_waiter(manager());
   LoadAndExpectSuccess();
-  waiter.WaitForRulesetCount(1);
+  ruleset_waiter.WaitForExtensionsWithRulesetsCount(1);
 
   // Only the first and third rulesets should be enabled.
   CompositeMatcher* composite_matcher =
@@ -958,7 +958,7 @@ TEST_P(MultipleRulesetsIndexingTest, StaticRuleCountExceeded) {
   // Enabled on load.
   AddRuleset(CreateRuleset("4.json", 30, 0, true));
 
-  RulesetCountWaiter waiter(manager());
+  RulesetManagerObserver ruleset_waiter(manager());
   extension_loader()->set_ignore_manifest_warnings(true);
 
   LoadAndExpectSuccess();
@@ -979,7 +979,7 @@ TEST_P(MultipleRulesetsIndexingTest, StaticRuleCountExceeded) {
             Field(&InstallWarning::message, kEnabledRuleCountExceeded)));
   }
 
-  waiter.WaitForRulesetCount(1);
+  ruleset_waiter.WaitForExtensionsWithRulesetsCount(1);
 
   CompositeMatcher* composite_matcher =
       manager()->GetMatcherForExtension(extension_id);
@@ -1007,7 +1007,7 @@ TEST_P(MultipleRulesetsIndexingTest, RegexRuleCountExceeded) {
   // Enabled on load.
   AddRuleset(CreateRuleset("4.json", 20, 20, true));
 
-  RulesetCountWaiter waiter(manager());
+  RulesetManagerObserver ruleset_waiter(manager());
   extension_loader()->set_ignore_manifest_warnings(true);
 
   LoadAndExpectSuccess();
@@ -1020,7 +1020,7 @@ TEST_P(MultipleRulesetsIndexingTest, RegexRuleCountExceeded) {
                                            kEnabledRegexRuleCountExceeded)));
   }
 
-  waiter.WaitForRulesetCount(1);
+  ruleset_waiter.WaitForExtensionsWithRulesetsCount(1);
 
   CompositeMatcher* composite_matcher =
       manager()->GetMatcherForExtension(extension()->id());
