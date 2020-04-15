21f2a074c043fd6f58f55394fbbc77d7032cb588
rbpotter <rbpotter@chromium.org>
2020-04-15 01:13:36 +0000

    Settings: Migrate help page browsertest to Polymer 3
    Bug: 1026426
    Change-Id: I283187f2b4797e2ce88fe7b68c23795971e7cba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2148481
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759093}

---
+++ chrome/test/data/webui/settings/help_page_v3_test.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @fileoverview Suite of tests for the Settings help page. */
+
+// clang-format off
+import 'chrome://settings/settings.js';
+import {getPage, getSection} from 'chrome://test/settings/settings_page_test_util.m.js';
+import {waitBeforeNextRender} from 'chrome://test/test_util.m.js';
+// clang-format on
+
+// Register mocha tests.
+suite('SettingsHelpPage', function() {
+  setup(function() {
+    PolymerTest.clearBody();
+    // The ChromeContentBrowserClient will rewrite chrome://help to
+    // chrome://settings/help.
+    window.history.pushState('', 'Test', 'chrome://settings/help');
+    const settingsUi = document.createElement('settings-ui');
+    document.body.appendChild(settingsUi);
+
+    // Wait for the dom-if.
+    return waitBeforeNextRender(settingsUi);
+  });
+
+  test('about section', async () => {
+    const page = await getPage('about');
+    expectTrue(!!getSection(page, 'about'));
+  });
+});
