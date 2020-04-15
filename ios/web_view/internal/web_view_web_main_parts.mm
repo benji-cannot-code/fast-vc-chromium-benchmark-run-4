507380e086cd74722c2222ab9fbe553b51bb36cb
John Z Wu <jzw@chromium.org>
2020-04-15 03:11:48 +0000

    Remove unneeded call to ContentSettingsPattern
    Change-Id: I1d59001345830e59a0e57834490f9f5afbaa0247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2150126
    Auto-Submit: John Wu <jzw@chromium.org>
    Reviewed-by: Hiroshi Ichikawa <ichikawa@chromium.org>
    Commit-Queue: Hiroshi Ichikawa <ichikawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759120}

--- ios/web_view/internal/web_view_web_main_parts.mm
+++ ios/web_view/internal/web_view_web_main_parts.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
-#include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/sync/driver/sync_driver_switches.h"
 #include "ios/web/public/webui/web_ui_ios_controller_factory.h"
@@ -73,9 +72,6 @@ void WebViewWebMainParts::PreCreateThreads() {
 
 void WebViewWebMainParts::PreMainMessageLoopRun() {
   WebViewTranslateService::GetInstance()->Initialize();
-
-  ContentSettingsPattern::SetNonWildcardDomainNonPortSchemes(
-      /*schemes=*/nullptr, 0);
 
   web::WebUIIOSControllerFactory::RegisterFactory(
       WebViewWebUIIOSControllerFactory::GetInstance());
