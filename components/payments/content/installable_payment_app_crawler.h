088b464f44a9e88e8cda15d917f25efec81f326e
Rouslan Solomakhin <rouslan@chromium.org>
2020-04-15 11:53:42 +0000

    [Web Payment] Single HTTP GET for colocated SW manifests.
    Before this patch, if both payment method manifest and the web app
    manifest of a payment app were located in the same file, then Chrome
    would retrieve the same file twice with two separate HTTP GET requests,
    when looking for installable service worker based payment handlers.
    
    This patch compares the URL of the payment method manifest and the web
    app manifest and, if the two are the same, uses the in-memory copy of
    the payment method manifest content as the web app manifest content
    instead of re-downloading it again.
    
    After this patch, when Chrome is looking for installable service worker
    based payment handlers, if the web app manifest and the payment method
    manifest files are located in the same file, then Chrome performs only
    one HTTP GET request to retrieve it.
    
    This patch does not modify Android payment app signature verification,
    which always makes two separate HTTP GET requests for the payment method
    manifest and the web app manifest, without comparing the two URLs for
    equivalence.
    
    Bug: 1069954
    Change-Id: I83f82058caf3153e8ab2807acea05d350abd9b32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2148114
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759213}

--- components/payments/content/installable_payment_app_crawler.h
+++ components/payments/content/installable_payment_app_crawler.h
@@ -30,7 +30,7 @@ class GURL;
 namespace content {
 class RenderFrameHost;
 class WebContents;
-}
+}  // namespace content
 
 namespace payments {
 
@@ -83,6 +83,7 @@ class InstallablePaymentAppCrawler : public content::WebContentsObserver {
   void OnPaymentMethodManifestParsed(
       const GURL& method_manifest_url,
       const GURL& method_manifest_url_after_redirects,
+      const std::string& content,
       const std::vector<GURL>& default_applications,
       const std::vector<url::Origin>& supported_origins,
       bool all_origins_supported);
---
+++ components/test/data/payments/orenpay.com/README.md
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Just-in-time installable payment handler that serves both the payment method
+manifest and the web app manifest directly at the payment method identifier URL
+https://a.com:<port>/orenpay.com/pay. This payment handler always can make
+payments and responds to a payment request with {"status": "success"}.
