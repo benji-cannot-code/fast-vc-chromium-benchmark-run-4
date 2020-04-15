7f4218eccbd30fb1a851b2f15d96d5f92848f870
Wenyu Fu <wenyufu@google.com>
2020-04-15 00:09:34 +0000

    [HomepagePromo] PromoCard MVC component
    Build a standard promo card component that could be used in different
    area.
    
    Bug: 1068831
    Change-Id: I8f557c602109abfbe5736e82afad7bfea262d236
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141136
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759071}

---
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/promo/PromoCardProperties.java
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.browser_ui.widget.promo;
+
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import org.chromium.base.Callback;
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableBooleanPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
+
+/**
+ * Properties for PromoCardView.
+ */
+public class PromoCardProperties {
+    // Visible related properties
+    public static final WritableBooleanPropertyKey SECONDARY_BUTTON_VISIBLE =
+            new WritableBooleanPropertyKey();
+
+    // View related properties
+    public static final WritableObjectPropertyKey<Drawable> IMAGE =
+            new WritableObjectPropertyKey<>();
+
+    public static final WritableObjectPropertyKey<String> TITLE = new WritableObjectPropertyKey<>();
+
+    public static final WritableObjectPropertyKey<String> DESCRIPTION =
+            new WritableObjectPropertyKey<>();
+
+    public static final WritableObjectPropertyKey<String> PRIMARY_BUTTON_TEXT =
+            new WritableObjectPropertyKey<>();
+
+    public static final WritableObjectPropertyKey<String> SECONDARY_BUTTON_TEXT =
+            new WritableObjectPropertyKey<>();
+
+    // Callback related properties
+    public static final WritableObjectPropertyKey<Callback<View>> PRIMARY_BUTTON_CALLBACK =
+            new WritableObjectPropertyKey<>();
+
+    public static final WritableObjectPropertyKey<Callback<View>> SECONDARY_BUTTON_CALLBACK =
+            new WritableObjectPropertyKey<>();
+
+    public static final WritableObjectPropertyKey<Callback<View>> CLOSE_BUTTON_CALLBACK =
+            new WritableObjectPropertyKey<>();
+
+    /**
+     * All the property keys needed to create the model for {@link PromoCardView}.
+     */
+    public static final PropertyKey[] ALL_KEYS = new PropertyKey[] {SECONDARY_BUTTON_VISIBLE, IMAGE,
+            TITLE, DESCRIPTION, PRIMARY_BUTTON_TEXT, SECONDARY_BUTTON_TEXT, PRIMARY_BUTTON_CALLBACK,
+            SECONDARY_BUTTON_CALLBACK, CLOSE_BUTTON_CALLBACK};
+}
