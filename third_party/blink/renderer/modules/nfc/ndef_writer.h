495cf964accd8e0c3c77a006b5a96ca88fc35506
Johann <johannkoenig@google.com>
2020-04-15 05:33:13 +0000

    heap mojo: migrate NDEFWriter permission_service_
    Use HeapMojoRemote wrapper so that Oilpan will GC the object.
    
    BUG=chromium:1049056
    
    Change-Id: I79e80eecadf9f15c43ef2a1f952440b8b677b6aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2145744
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759142}

--- third_party/blink/renderer/modules/nfc/ndef_writer.h
+++ third_party/blink/renderer/modules/nfc/ndef_writer.h
@@ -6,13 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_NFC_NDEF_WRITER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_NFC_NDEF_WRITER_H_
 
-#include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/permissions/permission.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/nfc/nfc_proxy.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/heap_allocator.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 
 namespace blink {
 
@@ -58,7 +59,9 @@ class NDEFWriter : public ScriptWrappable, public ExecutionContextClient {
                            mojom::blink::PermissionStatus status);
   mojom::blink::PermissionService* GetPermissionService();
 
-  mojo::Remote<mojom::blink::PermissionService> permission_service_;
+  HeapMojoRemote<mojom::blink::PermissionService,
+                 HeapMojoWrapperMode::kWithoutContextObserver>
+      permission_service_;
 
   // |requests_| are kept here to handle Mojo connection failures because
   // in that case the callback passed to Push() won't be called and
