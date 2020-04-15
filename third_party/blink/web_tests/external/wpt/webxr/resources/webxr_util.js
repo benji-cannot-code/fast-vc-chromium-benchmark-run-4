859c40de81b11f52ea0ea290d3d97ef21710babb
Klaus Weidner <klausw@chromium.org>
2020-04-15 02:13:39 +0000

    WebXR WPT: add a xr_debug function for use in tests
    By default, this is a no-op, but test API implementations can override
    it to do console logging or similar.
    
    This is intended to help investigate flaky tests on Windows that are
    currently just being reported as "timed out" with no indication what's
    going wrong.
    
    Includes a fix for ar_dom_overlay_hit_test.https.html, the logs had
    shown that the test wasn't waiting for the test body to execute,
    and an unexpectedly failing assertion wasn't being reached.
    
    Bug: 1054750
    Change-Id: I878c6ea56ebb8e0752027ea13de7106d7f9e7bdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132784
    Commit-Queue: Klaus Weidner <klausw@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759108}

--- third_party/blink/web_tests/external/wpt/webxr/resources/webxr_util.js
+++ third_party/blink/web_tests/external/wpt/webxr/resources/webxr_util.js
@@ -8,13 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 //   --enable-blink-features=MojoJS,MojoJSTest
 
+// Debugging message helper, by default does nothing. Implementations can
+// override this.
+var xr_debug = function(name, msg) {}
+
 function xr_promise_test(name, func, properties) {
   promise_test(async (t) => {
     // Perform any required test setup:
+    xr_debug(name, 'setup');
 
     if (window.XRTest === undefined) {
       // Chrome setup
       await loadChromiumResources;
+      xr_debug = navigator.xr.test.Debug;
     }
 
     // Ensure that any devices are disconnected when done. If this were done in
@@ -23,9 +29,11 @@ function xr_promise_test(name, func, properties) {
     // interfere with the next test.
     t.add_cleanup(async () => {
       // Ensure system state is cleaned up.
+      xr_debug(name, 'cleanup');
       await navigator.xr.test.disconnectAllDevices();
     });
 
+    xr_debug(name, 'main');
     return func(t);
   }, name, properties);
 }
@@ -75,9 +83,12 @@ function xr_session_promise_test(
               })
               .then(() => new Promise((resolve, reject) => {
                       // Perform the session request in a user gesture.
+                      xr_debug(name, 'simulateUserActivation');
                       navigator.xr.test.simulateUserActivation(() => {
+                        xr_debug(name, 'document.hasFocus()=' + document.hasFocus());
                         navigator.xr.requestSession(sessionMode, sessionInit || {})
                             .then((session) => {
+                              xr_debug(name, 'session start');
                               testSession = session;
                               session.mode = sessionMode;
                               let glLayer = new XRWebGLLayer(session, gl, gllayerProperties);
@@ -88,9 +99,11 @@ function xr_session_promise_test(
                                   baseLayer: glLayer
                               });
                               sessionObjects.glLayer = glLayer;
+                              xr_debug(name, 'session.visibilityState=' + session.visibilityState);
                               resolve(func(session, testDeviceController, t, sessionObjects));
                             })
                             .catch((err) => {
+                              xr_debug(name, 'error: ' + err);
                               reject(
                                   'Session with params ' +
                                   JSON.stringify(sessionMode) +
