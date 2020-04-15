1478008ceedabdf9495275c820fadbcdaa3bf4a3
Fredrik Söderqvist <fs@opera.com>
2020-04-15 07:47:10 +0000

    Update <use>'s target reference when adopting to a new document
    When the <use> element moves to a new document, it's both possible that
    the base URL changes and that the transition is from an inactive to an
    active document. In the latter case we need to re-resolve the URL and
    potentially attempt to fetch any external resource again.
    
    Do this by calling UpdateTargetReference() in a DidMoveToNewDocument()
    override. Also add a check for being in an inactive document to avoid
    the unnecessary work in that case, as well as clearing out the resource
    reference if the move is from an active to an inactive document.
    
    Fixed: 1070076
    Change-Id: Ibfcbe5a590d2115eece0d68ed1c03db49ef5f708
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149415
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#759169}

--- third_party/blink/renderer/core/svg/svg_use_element.h
+++ third_party/blink/renderer/core/svg/svg_use_element.h
@@ -79,6 +79,7 @@ class SVGUseElement final : public SVGGraphicsElement,
 
   InsertionNotificationRequest InsertedInto(ContainerNode&) override;
   void RemovedFrom(ContainerNode&) override;
+  void DidMoveToNewDocument(Document&) override;
 
   void SvgAttributeChanged(const QualifiedName&) override;
 
