80cde0c667cf025813c91ca968963cfce7769293
Mugdha Lakhani <nator@chromium.org>
2020-04-16 12:02:39 +0000

    [WebLayer] Move Offline code into Delegate.
    Offline Pages isn't a feature WebLayer intends to support for now.
    Move offline pages related logic from PageInfoController to
    ChromePageInfoControllerDelegate so that the former can be
    componentized.
    
    Bug: 1052375
    Change-Id: I59fc6208a5ce3528c6d0e59da3733679d63ea894
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144029
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759629}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoControllerDelegate.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoControllerDelegate.java
@@ -7,6 +7,8 @@ package org.chromium.components.page_info;
 
 import android.content.Intent;
 
+import androidx.annotation.Nullable;
+
 import org.chromium.base.Consumer;
 import org.chromium.components.omnibox.AutocompleteSchemeClassifier;
 import org.chromium.components.page_info.PageInfoView.PageInfoViewParams;
@@ -67,4 +69,29 @@ public interface PageInfoControllerDelegate {
      * Returns a VrHandler for Page Info UI.
      */
     public VrHandler getVrHandler();
+
+    /**
+     * Gets the Url of the offline page being shown if any. Returns null otherwise.
+     */
+    @Nullable
+    String getOfflinePageUrl();
+
+    /**
+     * Whether the page being shown is an offline page.
+     */
+    boolean isShowingOfflinePage();
+
+    /**
+     * Initialize viewParams with Offline Page UI info, if any.
+     * @param viewParams The PageInfoViewParams to set state on.
+     * @param consumer Used to set "open Online" button callback for offline page.
+     */
+    void initOfflinePageUiParams(PageInfoViewParams viewParams, Consumer<Runnable> runAfterDismiss);
+
+    /**
+     * Return the connection message shown for an offline page, if appropriate.
+     * Returns null if there's no offline page.
+     */
+    @Nullable
+    String getOfflinePageConnectionMessage();
 }
