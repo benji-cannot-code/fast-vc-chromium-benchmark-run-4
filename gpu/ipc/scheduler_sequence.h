c578c7ffc54fa7bc9f0d7835d4581e4a86f3597e
Vasiliy Telezhnikov <vasilyt@chromium.org>
2020-04-16 15:07:26 +0000

    Reland "Limit access to gpu thread in DisplayResourceProvider"
    This is a reland of d60e79d17e7aa5bb43ca392fb9aa047edcbdb658
    
    The problem was that resources were never returned because
    ScopedBatchReturnResources was in outer scope than ScopedAllowGpuAccess
    so the state when we don't batch already, but still have access to gpu
    thread didn't exist.
    
    Solution to merge ScopedBatchReturnResources and ScopedAllowGpuAccess
    to make sure everything happens in right order.
    
    Original change's description:
    > Limit access to gpu thread in DisplayResourceProvider
    >
    > This CL makes sure that DisplayResourceProvider return resources only
    > when GPU Thread is available to make sure ReleaseImageContexts will
    > complete in finite time.
    >
    > This doesn't change anything for Chrome where GPU Thread is always
    > available, but on webview RenderThread that used for gpu access during
    > composition is available only during specific times (init, dtor,
    > DrawAndSwap)
    >
    > Bug: 1068716
    >
    > Change-Id: I8fa482ab7399d6ce7639c1d76187be86ce152059
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2138551
    > Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Jonathan Backer <backer@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#757967}
    
    Bug: 1068716
    Change-Id: Idf531b9ee9e10057b571725a256d2af09efbea69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149377
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759672}

--- gpu/ipc/scheduler_sequence.h
+++ gpu/ipc/scheduler_sequence.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace viz {
 class Display;
+class ScopedAllowGpuAccessForDisplayResourceProvider;
 class OutputSurfaceProviderImpl;
 class OverlayProcessorAndroid;
 }  // namespace viz
@@ -36,6 +37,7 @@ class GL_IN_PROCESS_CONTEXT_EXPORT ScopedAllowScheduleGpuTask {
   // Only add more friend declarations for classes that Android WebView is
   // guaranteed to be able to support. Talk to boliu@ if in doubt.
   friend class viz::Display;
+  friend class viz::ScopedAllowGpuAccessForDisplayResourceProvider;
   friend class viz::OutputSurfaceProviderImpl;
   // Overlay is not supported for WebView. However the initialization and
   // destruction of OverlayProcessor requires posting task to gpu thread, which
