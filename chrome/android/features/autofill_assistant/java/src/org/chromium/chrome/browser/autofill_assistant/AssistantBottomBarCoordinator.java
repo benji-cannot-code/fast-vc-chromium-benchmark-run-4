7563222a3f0b04ae681e2b4576439ff8827650f1
Clemens Arbesser <arbesser@google.com>
2020-04-16 14:56:43 +0000

    [Autofill Assistant] Fixed animations when changing generic UI root view
    Before this change, going from one ShowGenericUiAction to another one
    caused a hard layout transition, rather than a smooth one.
    
    Bug: b/145043394
    Change-Id: I2577c03d9f9c8ebcb9d7f27cb057384f53a3edff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151452
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#759668}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantBottomBarCoordinator.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/AssistantBottomBarCoordinator.java
@@ -26,6 +26,7 @@ import org.chromium.chrome.browser.autofill_assistant.details.AssistantDetailsCo
 import org.chromium.chrome.browser.autofill_assistant.form.AssistantFormCoordinator;
 import org.chromium.chrome.browser.autofill_assistant.form.AssistantFormModel;
 import org.chromium.chrome.browser.autofill_assistant.generic_ui.AssistantGenericUiCoordinator;
+import org.chromium.chrome.browser.autofill_assistant.generic_ui.AssistantGenericUiModel;
 import org.chromium.chrome.browser.autofill_assistant.header.AssistantHeaderCoordinator;
 import org.chromium.chrome.browser.autofill_assistant.header.AssistantHeaderModel;
 import org.chromium.chrome.browser.autofill_assistant.infobox.AssistantInfoBoxCoordinator;
@@ -246,6 +247,12 @@ class AssistantBottomBarCoordinator implements AssistantPeekHeightCoordinator.De
         // Animate when form inputs change.
         model.getFormModel().addObserver((source, propertyKey) -> {
             if (AssistantFormModel.INPUTS == propertyKey) {
+                animateChildren(rootView);
+            }
+        });
+
+        model.getGenericUiModel().addObserver((source, propertyKey) -> {
+            if (AssistantGenericUiModel.VIEW == propertyKey) {
                 animateChildren(rootView);
             }
         });
