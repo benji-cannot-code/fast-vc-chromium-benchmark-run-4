4fd2bf70d687aeb578a4211e98f61cbd67685a88
Mugdha Lakhani <nator@chromium.org>
2020-04-16 09:27:18 +0000

    [WebLayer] InstantAppsHandler, VrModuleProvider to delegate.
    PageInfoController no longer depends on InstantAppsHandler and
    VrModuleProvider, and instead gets this info from
    ChromePageInfoControllerDelegate.
    
    Bug: 1052375
    Change-Id: I7050f105b10d6e78873f185e5516c2df318c8936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144016
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759590}

--- components/page_info/android/java/src/org/chromium/components/page_info/PageInfoControllerDelegate.java
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoControllerDelegate.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.page_info;
 
+import android.content.Intent;
+
 import org.chromium.base.Consumer;
 import org.chromium.components.omnibox.AutocompleteSchemeClassifier;
 import org.chromium.components.page_info.PageInfoView.PageInfoViewParams;
@@ -50,4 +52,19 @@ public interface PageInfoControllerDelegate {
      * Whether Preview page state is INSECURE.
      */
     boolean isPreviewPageInsecure();
+
+    /**
+     * Returns whether or not an instant app is available for |url|.
+     */
+    boolean isInstantAppAvailable(String url);
+
+    /**
+     * Gets the instant app intent for the given URL if one exists.
+     */
+    Intent getInstantAppIntentForUrl(String url);
+
+    /**
+     * Returns a VrHandler for Page Info UI.
+     */
+    public VrHandler getVrHandler();
 }
