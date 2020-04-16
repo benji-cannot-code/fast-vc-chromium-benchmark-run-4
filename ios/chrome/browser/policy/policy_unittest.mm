baeff1cd9e0a2d155ed9f2a5bd768ceab7b24ba2
Tina Wang <tinazwang@google.com>
2020-04-16 20:30:45 +0000

    [iOS Enterprise] Remove useless import and deps
    A small fix cleaning up the password related imports and deps since we automate policy unittests and no longer need them for policy_unittest.mm.
    
    Change-Id: Ic6a57e571a121c486b1edddc7b6d73c8713ff469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2152906
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: sebsg <sebsg@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759781}

--- ios/chrome/browser/policy/policy_unittest.mm
+++ ios/chrome/browser/policy/policy_unittest.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/policy/core/browser/policy_pref_mapping_test.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_map.h"
