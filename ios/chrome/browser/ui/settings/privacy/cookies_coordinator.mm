4cbe0f7516e42b2f8443f51f84b25cd612c961b0
Ewann <ewannpv@google.com>
2020-04-16 08:31:16 +0000

    [iOS] Fixes tableView separator
    This CL removes the top separtor of the Cookies tableView.
    
    Bug: 1064961
    Change-Id: I4a91b8ee567642d838c93034b57bbba87e41d1a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149654
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759574}

--- ios/chrome/browser/ui/settings/privacy/cookies_coordinator.mm
+++ ios/chrome/browser/ui/settings/privacy/cookies_coordinator.mm
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   self.viewController = [[PrivacyCookiesViewController alloc]
-      initWithStyle:UITableViewStyleGrouped];
+      initWithStyle:UITableViewStylePlain];
 
   DCHECK(self.baseNavigationController);
   [self.baseNavigationController pushViewController:self.viewController
