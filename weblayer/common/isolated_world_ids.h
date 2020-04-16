6927a4a7ffa1aa0c653e6250bfa429cd827289c1
Colin Blundell <blundell@chromium.org>
2020-04-16 16:18:42 +0000

    WebLayer: Move isolated_world_ids.h to //weblayer/common
    Following the pattern of //chrome, we will need to add a new world ID
    for translate, and the new ID will need to be accessible from the
    renderer.
    
    Bug: 1025620
    Change-Id: I8154436d61e35a764f79348ac0ca55e641097989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2152373
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759690}

--- weblayer/common/isolated_world_ids.h
+++ weblayer/common/isolated_world_ids.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef WEBLAYER_BROWSER_ISOLATED_WORLD_IDS_H_
-#define WEBLAYER_BROWSER_ISOLATED_WORLD_IDS_H_
+#ifndef WEBLAYER_COMMON_ISOLATED_WORLD_IDS_H_
+#define WEBLAYER_COMMON_ISOLATED_WORLD_IDS_H_
 
 #include "content/public/common/isolated_world_ids.h"
 
@@ -16,4 +16,4 @@ enum IsolatedWorldIDs {
 
 }  // namespace weblayer
 
-#endif  // WEBLAYER_BROWSER_ISOLATED_WORLD_IDS_H_
+#endif  // WEBLAYER_COMMON_ISOLATED_WORLD_IDS_H_
