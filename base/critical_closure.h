93ce9f5493fca7313cb587be37b9ab757860ea36
Justin Cohen <justincohen@google.com>
2020-04-16 15:28:29 +0000

    [ios] Add task name to scoped critical action background task.
    iOS 13.4 added better logging for finding leaked background tasks, but
    it requires setting a name.  This change also adds a metric to track
    what backgroundTimeRemaining is for any tasks initiated in the
    background.
    
    Bug: 1064280
    Change-Id: I074b7a229b4c9ca91b0f37d583d8a07994546178
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1132097
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759678}

--- base/critical_closure.h
+++ base/critical_closure.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/macros.h"
+#include "base/strings/string_piece.h"
 #include "build/build_config.h"
 
 #if defined(OS_IOS)
@@ -30,7 +31,7 @@ bool IsMultiTaskingSupported();
 // |ios::ScopedCriticalAction|.
 class CriticalClosure {
  public:
-  explicit CriticalClosure(OnceClosure closure);
+  explicit CriticalClosure(StringPiece task_name, OnceClosure closure);
   ~CriticalClosure();
   void Run();
 
@@ -52,20 +53,24 @@ class CriticalClosure {
 // Example:
 //   file_task_runner_->PostTask(
 //       FROM_HERE,
-//       MakeCriticalClosure(base::BindOnce(&WriteToDiskTask, path_, data)));
+//       MakeCriticalClosure(task_name,
+//                           base::BindOnce(&WriteToDiskTask, path_, data)));
 //
 // Note new closures might be posted in this closure. If the new closures need
 // background running time, |MakeCriticalClosure| should be applied on them
-// before posting.
+// before posting. |task_name| is used by the platform to identify any tasks
+// that do not complete in time for suspension.
 #if defined(OS_IOS)
-inline OnceClosure MakeCriticalClosure(OnceClosure closure) {
+inline OnceClosure MakeCriticalClosure(StringPiece task_name,
+                                       OnceClosure closure) {
   DCHECK(internal::IsMultiTaskingSupported());
   return base::BindOnce(
       &internal::CriticalClosure::Run,
-      Owned(new internal::CriticalClosure(std::move(closure))));
+      Owned(new internal::CriticalClosure(task_name, std::move(closure))));
 }
 #else  // defined(OS_IOS)
-inline OnceClosure MakeCriticalClosure(OnceClosure closure) {
+inline OnceClosure MakeCriticalClosure(StringPiece task_name,
+                                       OnceClosure closure) {
   // No-op for platforms where the application does not need to acquire
   // background time for closures to finish when it goes into the background.
   return closure;
