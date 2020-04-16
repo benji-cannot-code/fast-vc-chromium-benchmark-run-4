7a974526adf1d470950eaf9a0d6d011c3ab3b4d8
Bo Liu <boliu@chromium.org>
2020-04-16 20:08:13 +0000

    weblayer: Set default encoding
    Read default encoding from components_locale_settings, same as chrome.
    
    Bug: 1052496
    Change-Id: I24e0205ab94e71a3412783d322563ababd64e42d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147684
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759768}

--- weblayer/browser/content_browser_client_impl.cc
+++ weblayer/browser/content_browser_client_impl.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/content/ssl_cert_reporter.h"
 #include "components/security_interstitials/content/ssl_error_handler.h"
 #include "components/security_interstitials/content/ssl_error_navigation_throttle.h"
+#include "components/strings/grit/components_locale_settings.h"
 #include "components/variations/net/variations_http_headers.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_context.h"
@@ -48,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/binder_map.h"
 #include "third_party/blink/public/common/loader/url_loader_throttle.h"
 #include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 #include "url/url_constants.h"
@@ -285,6 +287,7 @@ void ContentBrowserClientImpl::OverrideWebkitPrefs(
   TabImpl* tab = TabImpl::FromWebContents(web_contents);
   prefs->fullscreen_supported = tab && tab->fullscreen_delegate();
   prefs->password_echo_enabled = tab && tab->GetPasswordEchoEnabled();
+  prefs->default_encoding = l10n_util::GetStringUTF8(IDS_DEFAULT_ENCODING);
 }
 
 mojo::Remote<network::mojom::NetworkContext>
