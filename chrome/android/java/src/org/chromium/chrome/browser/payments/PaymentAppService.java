abe4e39faa4e5861d361927d45c2ef1dfcd34e62
Danyao Wang <danyao@chromium.org>
2020-04-16 14:31:19 +0000

    Reland "[Web Payments] Use PaymentAppServiceBridge JNI in PaymentAppService."
    This is a reland of b666bbd09b937399667bbdbfe78fe12925aa7338
    
    Original patch: https://crrev.com/2141436
    Revert: https://crrev.com/2144682
    Reason for revert: broke tests: https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=chrome_public_test_apk&tests=org.chromium.chrome.browser.payments.ExpandablePaymentHandlerChangePaymentMethodTest%23testThrowBasicCard
    Reason for reland: fixed broken test (moving a function call outside of DCHECK)
    
    Original change's description:
    > [Web Payments] Use PaymentAppServiceBridge JNI in PaymentAppService.
    >
    > This CL contains the final steps to make the Java PaymentAppService
    > invoke the C++ PaymentAppService:
    > - Moved remaining PaymentHandlerFinder functionality, namely,
    >   onInstalledPaymentHandlerFounder() and onInstallablePaymentHandler()
    >   from ServiceWorkerPaymentAppBridge to PaymentAppServiceBridge.
    > - Exposed the native method fireCanMakePaymentEvent() via a static
    >   method on ServiceWorkerPaymentAppBridge so it can be used by
    >   PaymentAppServiceBridge.
    > - Proxied ServiceWorkerPaymentAppBridge.setCanMakePaymentForTesting
    >   to PaymentAppServiceBridge. This will be cleaned up in a subsequent
    >   CL.
    > - Fixed a bug from a previous CL to pass payment apps to callbacks by
    >   value instead of by reference.
    >
    > Bug: 1063118
    > Change-Id: I87e254d6dc11e12f890353c9a0a6dde6cca73c9f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141436
    > Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    > Commit-Queue: Danyao Wang <danyao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#757827}
    
    Bug: 1063118
    Change-Id: Ib2eceb850368e3247876ab303b54a1c41b8943ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151091
    Commit-Queue: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759651}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentAppService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentAppService.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.payments;
 
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -28,9 +26,7 @@ public class PaymentAppService implements PaymentAppFactoryInterface {
     /** Prevent instantiation. */
     private PaymentAppService() {
         mFactories.add(new AutofillPaymentAppFactory());
-        if (ChromeFeatureList.isEnabled(ChromeFeatureList.SERVICE_WORKER_PAYMENT_APPS)) {
-            mFactories.add(new ServiceWorkerPaymentAppBridge());
-        }
+        mFactories.add(new PaymentAppServiceBridge());
         mFactories.add(new AndroidPaymentAppFactory());
     }
 
