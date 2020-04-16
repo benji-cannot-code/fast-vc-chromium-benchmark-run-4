bf152f881d693427437e0699ad017df8961c3d68
Abhijeet Singh <siabhijeet@google.com>
2020-04-16 22:14:42 +0000

    Make phonetics optional for Quick Answers
    Make displaying phonetics for the dictionary use-case within Quick
    Answers optional to handle the scenario when one is unavailable or
    multiple are present, parsing failure ends up leading to not displaying
    the definition as well.
    
    Bug: b:152110565
    Test: Tested on Chrome OS VM.
    Change-Id: Ice5a4ed4ee2fd86aaa2c1baeae36c26c2be3262c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151931
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Abhijeet Singh <siabhijeet@google.com>
    Cr-Commit-Position: refs/heads/master@{#759824}

--- chromeos/components/quick_answers/search_result_parsers/definition_result_parser.cc
+++ chromeos/components/quick_answers/search_result_parsers/definition_result_parser.cc
@@ -23,7 +23,8 @@ constexpr char kSensesKey[] = "senses";
 constexpr char kDefinitionPathUnderSense[] = "definition.text";
 constexpr char kPhoneticsKey[] = "phonetics";
 constexpr char kPhoneticsTextKey[] = "text";
-constexpr char kPhoneticsResultTemplate[] = "%s · /%s/";
+constexpr char kDefinitionResultDefaultTemplate[] = "%s · /%s/";
+constexpr char kDefinitionResultNoPhoneticsTemplate[] = "%s";
 
 }  // namespace
 
@@ -36,18 +37,13 @@ bool DefinitionResultParser::Parse(const Value* result,
     return false;
   }
 
-  // Get Definition.
+  // Get definition and phonetics.
   const std::string* definition = ExtractDefinition(first_entry);
   if (!definition) {
     LOG(ERROR) << "Fail in extracting definition";
     return false;
   }
-
   const std::string* phonetics = ExtractPhonetics(first_entry);
-  if (!phonetics) {
-    LOG(ERROR) << "Fail in extracting phonetics";
-    return false;
-  }
 
   const std::string* query_term = result->FindStringPath(kQueryTermPath);
   if (!query_term) {
@@ -55,8 +51,11 @@ bool DefinitionResultParser::Parse(const Value* result,
     return false;
   }
 
-  const std::string& secondary_answer = base::StringPrintf(
-      kPhoneticsResultTemplate, query_term->c_str(), phonetics->c_str());
+  const std::string& secondary_answer =
+      phonetics ? base::StringPrintf(kDefinitionResultDefaultTemplate,
+                                     query_term->c_str(), phonetics->c_str())
+                : base::StringPrintf(kDefinitionResultNoPhoneticsTemplate,
+                                     query_term->c_str());
   quick_answer->result_type = ResultType::kDefinitionResult;
   quick_answer->primary_answer = *definition;
   quick_answer->secondary_answer = secondary_answer;
@@ -91,7 +90,7 @@ const std::string* DefinitionResultParser::ExtractPhonetics(
   const Value* first_phonetics =
       GetFirstListElement(*definition_entry, kPhoneticsKey);
   if (!first_phonetics) {
-    LOG(ERROR) << "Can't find a phonetics.";
+    LOG(WARNING) << "Can't find a phonetics.";
     return nullptr;
   }
 
