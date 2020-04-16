a765547b691a9479f79e371ad459e913ff243815
David Van Cleve <davidvc@chromium.org>
2020-04-16 20:34:53 +0000

    Trust Tokens: Define and register a component installer
    The prototype Trust Token API (https://github.com/wicg/trust-token-api)
    provides new JS functionality with which one can specify certain
    cryptographic operations to execute along outgoing requests.
    
    Executing some of these operations requires that the client possess
    trust token issuers' "key commitments," which are collections of public
    keys and associated metadata. For the initial prototype implementation,
    which will be available to participants in an origin trial, we're
    shipping these key commitments out of band through the component
    updater. This CL implements the client side of this key shipping, namely
    a component installer that will:
    - register itself whenever base::features::kTrustTokens is on;
    - expect an unversioned JSON-encoded collection of commitments from the
    update server; and
    - pass this collection of commitments to the network service via
    NetworkService::SetTrustTokenKeyCommitments, whence network-service side
    code will parse the collection and store the keys in memory.
    
    R=waffles
    
    registration when the feature is off; correctly pass obtained keys to a
    callback). We'll also add a manual test plan once the server-side
    configuration lands.
    
    Test: A couple unit tests verify some aspects of the functionality (no
    Bug: 1068651, b/152525332
    Change-Id: I58c760fe3d5219a61a805923d63fe1d5714450a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144831
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Steven Valdez <svaldez@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759782}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/sth_set_component_remover.h"
 #include "chrome/browser/component_updater/subresource_filter_component_installer.h"
 #include "chrome/browser/component_updater/tls_deprecation_config_component_installer.h"
+#include "chrome/browser/component_updater/trust_token_key_commitments_component_installer.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/pref_names.h"
@@ -145,6 +146,8 @@ void RegisterComponentsForUpdate(bool is_off_the_record_profile,
     RegisterOriginTrialsComponent(cus, path);
 
     RegisterFileTypePoliciesComponent(cus, path);
+
+    RegisterTrustTokenKeyCommitmentsComponentIfTrustTokensEnabled(cus, path);
 
     RegisterSSLErrorAssistantComponent(cus, path);
   }
