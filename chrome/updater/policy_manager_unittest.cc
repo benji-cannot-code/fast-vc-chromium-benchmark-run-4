3628346d6678380685deeeabed011822daaa072a
S. Ganesh <ganesh@chromium.org>
2020-04-16 15:44:41 +0000

    Add base classes for Policy Management in Omaha 4.
    This CL adds a PolicyManagerInterface that all future Policy Managers
    will support, as well as a DefaultPolicyManager.
    
    Bug: 1068797
    Change-Id: Idf70e93a6b084e6a519cb65738f45ed51ecaae29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140994
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759682}

---
+++ chrome/updater/policy_manager_unittest.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/updater/policy_manager.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace updater {
+
+TEST(PolicyManager, GetPolicyManager) {
+  std::unique_ptr<PolicyManagerInterface> policy_manager(GetPolicyManager());
+  ASSERT_FALSE(policy_manager->IsManaged());
+}
+
+}  // namespace updater
