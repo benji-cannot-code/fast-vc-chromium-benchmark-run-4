ce2f970243553380ca67c2cde03e4bea3264c1f8
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-04-16 23:47:10 +0000

    [Extensions] Move views-related extension_action_test_helper files
    Move views-related extension_action_test_helper* files from
    chrome/browser/ui/views/toolbar to chrome/browser/ui/views/extensions.
    They make more sense there.
    
    Bug: None
    Change-Id: I5f8a6c95bdda72a910f9645dc5f3946f0e148bb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153703
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759867}

--- chrome/browser/ui/views/extensions/extension_action_test_helper_views.h
+++ chrome/browser/ui/views/extensions/extension_action_test_helper_views.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_TOOLBAR_EXTENSION_ACTION_TEST_HELPER_VIEWS_H_
-#define CHROME_BROWSER_UI_VIEWS_TOOLBAR_EXTENSION_ACTION_TEST_HELPER_VIEWS_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSION_ACTION_TEST_HELPER_VIEWS_H_
+#define CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSION_ACTION_TEST_HELPER_VIEWS_H_
 
 #include <memory>
 
@@ -63,4 +63,4 @@ class ExtensionActionTestHelperViews : public ExtensionActionTestHelper {
   BrowserActionsContainer* const browser_actions_container_;
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_TOOLBAR_EXTENSION_ACTION_TEST_HELPER_VIEWS_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSION_ACTION_TEST_HELPER_VIEWS_H_
