92a433e43cd7dff4c6e031fc22142ab2a8d10198
Xianzhu Wang <wangxianzhu@chromium.org>
2020-04-16 18:13:25 +0000

    Reland "Collect reasons of disallowing LCD text"
    This reverts commit fc46d7d7bc3ba97b9aa389096897228c66f5e773.
    
    Reason for revert: Fixed telemetry_perf_unittests.
    
    Original change's description:
    > Revert "Collect reasons of disallowing LCD text"
    >
    > This reverts commit b79724f8ed23b9573076a8a92f59fadffb36f609.
    >
    > Reason for revert: Causing flaky failures in Mac10.14 Tests bot.
    >
    > First failure here:
    > https://ci.chromium.org/p/chromium/builders/ci/Mac10.14%20Tests/3374
    >
    >
    > telemetry_perf_unittests on (none) GPU on Mac failed because of:
    > measurements.rasterize_and_record_micro_unittest.RasterizeAndRecordMicroUnitTest.testRasterizeAndRecordMicro
    >
    >
    >
    > Original change's description:
    > > Collect reasons of disallowing LCD text
    > >
    > > (TBR for passive change in tools/perf/measurements/rasterize_and_record_micro.py.)
    > > TBR=wangxianzhu@chromium.org
    > > R=​pdr@chromium.org
    > >
    > > Bug: 642885
    > > Change-Id: Ia5c8313ea9e34139e06ff11fcc5d751ff0768c75
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149914
    > > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#759474}
    >
    > TBR=wangxianzhu@chromium.org,pdr@chromium.org
    >
    > Change-Id: Ic25a88c23e6f504461d3100fbf3bc8f8429c81f2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 642885
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151450
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#759562}
    
    TBR=wangxianzhu@chromium.org,pdr@chromium.org,guidou@chromium.org
    
    Change-Id: Ica3073d071e43b76f763012a2bc6c8403c0695a0
    Bug: 642885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2152771
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759726}

--- cc/benchmarks/rasterize_and_record_benchmark_impl.h
+++ cc/benchmarks/rasterize_and_record_benchmark_impl.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "cc/benchmarks/micro_benchmark_impl.h"
+#include "cc/raster/lcd_text_disallowed_reason.h"
 #include "cc/raster/task_graph_runner.h"
 
 namespace cc {
@@ -42,8 +43,8 @@ class RasterizeAndRecordBenchmarkImpl : public MicroBenchmarkImpl {
     int pixels_rasterized;
     int pixels_rasterized_with_non_solid_color;
     int pixels_rasterized_as_opaque;
-    int visible_pixels_for_lcd_text;
-    int visible_pixels_for_non_lcd_text;
+    int visible_pixels_by_lcd_text_disallowed_reason
+        [kLCDTextDisallowedReasonCount];
     base::TimeDelta total_best_time;
     int total_layers;
     int total_picture_layers;
