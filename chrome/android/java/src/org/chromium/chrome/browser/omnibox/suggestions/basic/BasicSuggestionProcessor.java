521b35a893b2c1be56270a3a75051cec630e2d69
Gang Wu <gangwu@chromium.org>
2020-04-17 17:54:40 +0000

    [Omnibox] Change isUrlSuggestion->isSearchSuggestion
    Change-Id: I9966644225507f51d5d22189b5e391491ed03587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153569
    Reviewed-by: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760130}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/BasicSuggestionProcessor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/basic/BasicSuggestionProcessor.java
@@ -98,13 +98,7 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
      * when we know we have a valid and large enough site favicon to present.
      */
     private @SuggestionIcon int getSuggestionIconType(OmniboxSuggestion suggestion) {
-        if (suggestion.isUrlSuggestion()) {
-            if (suggestion.isStarred()) {
-                return SuggestionIcon.BOOKMARK;
-            } else {
-                return SuggestionIcon.GLOBE;
-            }
-        } else /* Search suggestion */ {
+        if (suggestion.isSearchSuggestion()) {
             switch (suggestion.getType()) {
                 case OmniboxSuggestionType.VOICE_SUGGEST:
                     return SuggestionIcon.VOICE;
@@ -115,6 +109,12 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
 
                 default:
                     return SuggestionIcon.MAGNIFIER;
+            }
+        } else {
+            if (suggestion.isStarred()) {
+                return SuggestionIcon.BOOKMARK;
+            } else {
+                return SuggestionIcon.GLOBE;
             }
         }
     }
@@ -165,7 +165,7 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
         SuggestionSpannable textLine2 = null;
         boolean urlHighlighted = false;
 
-        if (suggestion.isUrlSuggestion()) {
+        if (!suggestion.isSearchSuggestion()) {
             if (!TextUtils.isEmpty(suggestion.getUrl())) {
                 SuggestionSpannable str = new SuggestionSpannable(suggestion.getDisplayText());
                 urlHighlighted = applyHighlightToMatchRegions(
@@ -177,10 +177,10 @@ public class BasicSuggestionProcessor extends BaseSuggestionViewProcessor {
         }
 
         final SuggestionSpannable textLine1 =
-                getSuggestedQuery(suggestion, suggestion.isUrlSuggestion(), !urlHighlighted);
+                getSuggestedQuery(suggestion, !suggestion.isSearchSuggestion(), !urlHighlighted);
 
         updateSuggestionIcon(suggestion, model);
-        model.set(SuggestionViewProperties.IS_SEARCH_SUGGESTION, !suggestion.isUrlSuggestion());
+        model.set(SuggestionViewProperties.IS_SEARCH_SUGGESTION, suggestion.isSearchSuggestion());
         model.set(SuggestionViewProperties.TEXT_LINE_1_TEXT, textLine1);
         model.set(SuggestionViewProperties.TEXT_LINE_2_TEXT, textLine2);
         fetchSuggestionFavicon(model, suggestion.getUrl(), mIconBridgeSupplier.get(), () -> {
