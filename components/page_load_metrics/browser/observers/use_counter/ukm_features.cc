a0ca0f7539034d4e7fe035dafb3092cdf0e8e98c
Ayu Ishii <ayui@chromium.org>
2020-04-17 22:24:16 +0000

    CookieStore: Add UKM use counters for CookieStoreAPI
    This change adds CookieStoreAPI to UKM use counters to record usage
    of the API and observe how it is being used to help with improvements
    to the shape of the API.
    
    CookieStoreAPI Explainer: https://github.com/WICG/cookie-store/blob/master/explainer.md
    
    Change-Id: Ib27ee0d5c228eb8a3e67540a4f0a4bb8a513187d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149866
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760238}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -107,6 +107,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kTextFragmentAnchorMatchFound,
           WebFeature::kCookieNoSameSite,
           WebFeature::kCookieInsecureAndSameSiteNone,
+          WebFeature::kCookieStoreAPI,
           WebFeature::kDeviceOrientationSecureOrigin,
           WebFeature::kDeviceOrientationAbsoluteSecureOrigin,
           WebFeature::kDeviceMotionSecureOrigin,
