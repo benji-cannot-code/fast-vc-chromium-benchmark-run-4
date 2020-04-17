21e1765a063d72e56388562cc9d319eb1be37a89
Mohamed Amir Yosef <mamir@chromium.org>
2020-04-17 17:30:15 +0000

    [Passwords] Introduce PasswordFormManager::IsMovableToAccountStore()
    This method decided whether the pending credentials in the
    password form manager can be moved to the account of the currently
    signed in user.
    
    Bug: 1032992
    Change-Id: I088a24cb99111014c5f87468ac9fc566c6014c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139658
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760121}

--- components/password_manager/core/browser/password_generation_manager.cc
+++ components/password_manager/core/browser/password_generation_manager.cc
@@ -52,6 +52,7 @@ class PasswordDataForUI : public PasswordFormManagerForUI {
   PasswordFormMetricsRecorder* GetMetricsRecorder() override;
   base::span<const InteractionsStats> GetInteractionsStats() const override;
   bool IsBlacklisted() const override;
+  bool IsMovableToAccountStore() const override;
   void Save() override;
   void Update(const PasswordForm& credentials_to_update) override;
   void OnUpdateUsernameFromPrompt(const base::string16& new_username) override;
@@ -127,6 +128,11 @@ base::span<const InteractionsStats> PasswordDataForUI::GetInteractionsStats()
 
 bool PasswordDataForUI::IsBlacklisted() const {
   // 'true' would suppress the bubble.
+  return false;
+}
+
+bool PasswordDataForUI::IsMovableToAccountStore() const {
+  // This is irrelevant for the generation conflict resolution bubble.
   return false;
 }
 
