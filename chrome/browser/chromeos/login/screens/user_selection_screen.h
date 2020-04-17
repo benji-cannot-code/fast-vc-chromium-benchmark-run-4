d1faa18475f335d0d4d93cf07ec3137414cbaf6b
Roman Sorokin <rsorokin@chromium.org>
2020-04-17 11:37:21 +0000

    cros: Honor DeviceLoginScreenInputMethods on the login screen.
    Stop honor the policy on the lock screen.
    Also tweaked LoginScreen*Policy tests a bit
    
    Fixed: 1071012
    Bug: 1067260, 1071010, 1071779
    Change-Id: Icaa5fd108b66dbe22fde738f000b3eff49236347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149357
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760016}

--- chrome/browser/chromeos/login/screens/user_selection_screen.h
+++ chrome/browser/chromeos/login/screens/user_selection_screen.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/screens/base_screen.h"
 #include "chrome/browser/chromeos/login/signin/token_handle_util.h"
 #include "chrome/browser/chromeos/login/ui/login_display.h"
+#include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chromeos/components/proximity_auth/screenlock_bridge.h"
 #include "components/account_id/account_id.h"
 #include "components/user_manager/user.h"
@@ -62,7 +63,6 @@ class UserSelectionScreen
   void CheckUserStatus(const AccountId& account_id);
   void HandleFocusPod(const AccountId& account_id);
   void HandleNoPodFocused();
-  void OnAllowedInputMethodsChanged();
   void OnBeforeShow();
 
   // Build list of users and send it to the webui.
@@ -146,6 +146,7 @@ class UserSelectionScreen
 
   void OnUserStatusChecked(const AccountId& account_id,
                            TokenHandleUtil::TokenHandleStatus status);
+  void OnAllowedInputMethodsChanged();
 
   LoginDisplayWebUIHandler* handler_ = nullptr;
 
@@ -173,6 +174,9 @@ class UserSelectionScreen
   AccountId focused_pod_account_id_;
   // Input Method Engine state used at the user selection screen.
   scoped_refptr<input_method::InputMethodManager::State> ime_state_;
+
+  std::unique_ptr<CrosSettings::ObserverSubscription>
+      allowed_input_methods_subscription_;
 
   base::WeakPtrFactory<UserSelectionScreen> weak_factory_{this};
 
