5f16e93a8d264dcd02e9ac8c853efd74f78351fa
Scott Violet <sky@chromium.org>
2020-04-17 00:24:24 +0000

    weblayer: allow calling Navigate() from onNavigationStarted
    At the time OnNavigationStarted() is called it's not safe to call
    Navigate(). This patch delays the navigate until it is safe (when
    NavigationThrottleImpl::WillStartRequest() is called).
    
    BUG=1070569
    TEST=NavigationBrowserTest.NavigateFromRendererInitiatedNavigation
    
    Change-Id: Ia110bb94e64d6551c9db302ae41b735ad5a8a5e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153758
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759872}

--- weblayer/browser/navigation_controller_impl.h
+++ weblayer/browser/navigation_controller_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 
+#include <memory>
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "build/build_config.h"
@@ -131,7 +132,8 @@ class NavigationControllerImpl : public NavigationController,
 
   void NotifyLoadStateChanged();
 
-  void DoNavigate(content::NavigationController::LoadURLParams&& params);
+  void DoNavigate(
+      std::unique_ptr<content::NavigationController::LoadURLParams> params);
 
   base::ObserverList<NavigationObserver>::Unchecked observers_;
   std::map<content::NavigationHandle*, std::unique_ptr<NavigationImpl>>
