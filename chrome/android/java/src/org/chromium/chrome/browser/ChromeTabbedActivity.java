659428501b0fbd4f420b7bc8c75661b479136c0e
Ted Choc <tedchoc@chromium.org>
2020-04-17 17:10:52 +0000

    Remove no longer needed FirstUserAction.BackgroundTime.MainIntent.* metrics.
    BUG=1037491
    
    Change-Id: Idb59c5fbdc104d2ec6caf552bc0cdcc859339b4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153329
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760106}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -459,7 +459,7 @@ public class ChromeTabbedActivity
      * Constructs a ChromeTabbedActivity.
      */
     public ChromeTabbedActivity() {
-        mMainIntentMetrics = new MainIntentBehaviorMetrics(this);
+        mMainIntentMetrics = new MainIntentBehaviorMetrics();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             mMultiInstanceManager = new MultiInstanceManager(this, mTabModelSelectorSupplier,
                     getMultiWindowModeStateDispatcher(), getLifecycleDispatcher(), this);
@@ -1174,9 +1174,7 @@ public class ChromeTabbedActivity
                     || (shouldShowTabSwitcherOnStart()
                             && !mTabModelSelectorImpl.isIncognitoSelected());
 
-            mMainIntentMetrics.setIgnoreEvents(true);
             mTabModelSelectorImpl.restoreTabs(activeTabBeingRestored);
-            mMainIntentMetrics.setIgnoreEvents(false);
 
             // Only create an initial tab if no tabs were restored and no intent was handled.
             // Also, check whether the active tab was supposed to be restored and that the total
@@ -1191,9 +1189,7 @@ public class ChromeTabbedActivity
 
                 mPendingInitialTabCreation = true;
                 PartnerBrowserCustomizations.getInstance().setOnInitializeAsyncFinished(() -> {
-                    mMainIntentMetrics.setIgnoreEvents(true);
                     createInitialTab();
-                    mMainIntentMetrics.setIgnoreEvents(false);
                 }, INITIAL_TAB_CREATION_TIMEOUT_MS);
             }
         } finally {
@@ -1796,7 +1792,6 @@ public class ChromeTabbedActivity
     }
 
     private void onOmniboxFocusChanged(boolean hasFocus) {
-        mMainIntentMetrics.onOmniboxFocused();
         mTabModalHandler.onOmniboxFocusChanged(hasFocus);
     }
 
