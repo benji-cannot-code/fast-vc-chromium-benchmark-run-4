0e079acb91698ee9086cd3fe14218f8276b4d4a0
Roman Aleksandrov <raleksandrov@google.com>
2020-04-17 16:19:56 +0000

    PackagedLicenseScreen: Skip screen properly.
    Use new method for skipping screen. Add tests for UMA stats.
    
    Bug: 1064561
    Change-Id: I2dd5ec7255fc748c33935c201d29384fbbc3ada2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151445
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760083}

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -778,10 +778,7 @@ void WizardController::ShowDiscoverScreen() {
 }
 
 void WizardController::ShowPackagedLicenseScreen() {
-  if (should_show_packaged_license_screen())
-    SetCurrentScreen(GetScreen(PackagedLicenseView::kScreenId));
-  else
-    ShowLoginScreen();
+  SetCurrentScreen(GetScreen(PackagedLicenseView::kScreenId));
 }
 
 void WizardController::SkipToLoginForTesting() {
@@ -1257,6 +1254,7 @@ void WizardController::OnPackagedLicenseScreenExit(
                PackagedLicenseScreen::GetResultString(result));
   switch (result) {
     case PackagedLicenseScreen::Result::DONT_ENROLL:
+    case PackagedLicenseScreen::Result::NOT_APPLICABLE:
       ShowLoginScreen();
       break;
     case PackagedLicenseScreen::Result::ENROLL:
