e822ddff14bb163311faff49ce66c8e662e30da9
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-04-17 20:21:09 +0000

    [Extensions Menu] Adjust CHECKs in BrowserView
    Adjust the CHECKs in BrowserView to avoid triggering a CHECK failure
    when there is no extensions container (which can happen in the case
    of app windows). This fixes
    AppBrowserControllerBrowserTest.NoExtensionsContainerExists with the
    menu enabled.
    
    Bug: 943702
    Change-Id: Id14113c456934f5e7984d142f97104929080da32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153745
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760187}

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1368,14 +1368,13 @@ void BrowserView::FocusToolbar() {
 }
 
 ExtensionsContainer* BrowserView::GetExtensionsContainer() {
-  ExtensionsToolbarContainer* const extensions_toolbar_container =
-      toolbar_button_provider_->GetExtensionsToolbarContainer();
-  if (extensions_toolbar_container)
-    return extensions_toolbar_container;
+  if (base::FeatureList::IsEnabled(features::kExtensionsToolbarMenu))
+    return toolbar_button_provider_->GetExtensionsToolbarContainer();
 
-  CHECK(!base::FeatureList::IsEnabled(features::kExtensionsToolbarMenu));
   BrowserActionsContainer* container =
       toolbar_button_provider_->GetBrowserActionsContainer();
+  // Note that in some cases (such as an app window), there is no extensions
+  // container.
   return container ? container->toolbar_actions_bar() : nullptr;
 }
 
