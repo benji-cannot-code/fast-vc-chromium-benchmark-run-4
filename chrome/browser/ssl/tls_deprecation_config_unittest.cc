1c6051bf25796a554bfdc6b7a49559acda6526bf
Mohamed Amir Yosef <mamir@chromium.org>
2020-04-17 13:45:17 +0000

    Deflake TLSDeprecationConfigTest.NoProto
    This is a follow up to crrev.com/c/2141099
    
    The test is still flaky because of its dependency on a singleton class.
    
    crrev.com/c/2141099 cleared the state after running each test to make
    sure not other tests are affected.
    
    This CL clears the states before running each test to make sure it's
    not affected by other tests outside
    chrome/browser/ssl/tls_deprecation_config_unittest.cc
    
    TBR= cthomp
    
    Bug: 2141099
    Change-Id: I1bf04ee46023b43796767ca7cf92c1bda0b69cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153437
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760043}

--- chrome/browser/ssl/tls_deprecation_config_unittest.cc
+++ chrome/browser/ssl/tls_deprecation_config_unittest.cc
@@ -17,6 +17,7 @@ using chrome_browser_ssl::LegacyTLSExperimentConfig;
 
 class TLSDeprecationConfigTest : public testing::Test {
  protected:
+  void SetUp() override { ResetTLSDeprecationConfigForTesting(); }
   void TearDown() override { ResetTLSDeprecationConfigForTesting(); }
 };
 
