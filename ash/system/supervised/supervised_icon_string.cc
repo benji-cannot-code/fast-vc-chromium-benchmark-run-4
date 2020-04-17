36533ad9c5ed339671a9b61488f85931917f9611
Xiqi Ruan <xiqiruan@chromium.org>
2020-04-17 20:08:41 +0000

    cros: Update supervised user icon
    Change the supervised user icon used throughout Chrome OS to the new family experience kite icon.
    Rename some files to supervised_user suffix.
    
    Bug: 1058516
    Change-Id: Ief22e51de73b0675c0a9a18969dac8627d9ad30d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2146080
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Xiqi Ruan <xiqiruan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760180}

--- ash/system/supervised/supervised_icon_string.cc
+++ ash/system/supervised/supervised_icon_string.cc
@@ -22,9 +22,9 @@ const gfx::VectorIcon& GetSupervisedUserIcon() {
 
   if (session_controller->IsUserSupervised() &&
       session_controller->IsUserChild())
-    return kSystemMenuChildUserIcon;
+    return kSystemMenuSupervisedUserIcon;
 
-  return kSystemMenuSupervisedUserIcon;
+  return kSystemMenuLegacySupervisedUserIcon;
 }
 
 base::string16 GetSupervisedUserMessage() {
