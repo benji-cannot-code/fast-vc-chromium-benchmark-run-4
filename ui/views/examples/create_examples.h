1e7e0e032bb85d78a4d52db62c06a840a60abee6
Allen Bauer <kylixrd@chromium.org>
2020-04-17 17:35:39 +0000

    Move examples creations to separate module.
    Change-Id: I778aded413a596176c3a2b164856db6b873caf1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149952
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760123}

---
+++ ui/views/examples/create_examples.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_EXAMPLES_CREATE_EXAMPLES_H_
+#define UI_VIEWS_EXAMPLES_CREATE_EXAMPLES_H_
+
+#include <memory>
+#include <vector>
+
+#include "ui/views/examples/example_base.h"
+#include "ui/views/examples/views_examples_export.h"
+
+namespace views {
+namespace examples {
+
+using ExampleVector = std::vector<std::unique_ptr<ExampleBase>>;
+
+// Creates the default set of examples.
+ExampleVector VIEWS_EXAMPLES_EXPORT CreateExamples();
+
+}  // namespace examples
+}  // namespace views
+
+#endif  // UI_VIEWS_EXAMPLES_CREATE_EXAMPLES_H_
