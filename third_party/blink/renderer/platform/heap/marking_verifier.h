e5f73f3d93ca0a1b5e0dfe9d28c0d0835f382dd6
Michael Lippautz <mlippautz@chromium.org>
2020-04-17 14:01:23 +0000

    heap: Remove GetHeapObjectHeader from mixins
    The methods in the macro are put on every GarbageCollectedMixin which
    is expensive in terms of binary size. The HoH getter can be replaced
    with GetTraceDescriptor().
    
    Change-Id: I0daaf3fc51a95f71baf7f6c7bed387e94963601f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139758
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760049}

--- third_party/blink/renderer/platform/heap/marking_verifier.h
+++ third_party/blink/renderer/platform/heap/marking_verifier.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class HeapObjectHeader;
+
 // Marking verifier that checks that a child is marked if its parent is marked.
 class MarkingVerifier final : public Visitor {
  public:
