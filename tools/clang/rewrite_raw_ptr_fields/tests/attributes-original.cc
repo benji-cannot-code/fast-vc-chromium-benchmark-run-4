e51991cabf3f3592adff78436b2e9de21ee3551d
Lukasz Anforowicz <lukasza@chromium.org>
2020-04-17 03:15:48 +0000

    Handling of basics: |Pointee* field| -> |CheckedPtr<Pointee> field|.
    Bug: 1069567
    Change-Id: Ie83b99f25e5d8d13f466c91f8d5118bfd039e856
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144418
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759921}

---
+++ tools/clang/rewrite_raw_ptr_fields/tests/attributes-original.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class SomeClass;
+
+// Based on Chromium's //base/thread_annotations.h
+#define GUARDED_BY(lock) __attribute__((guarded_by(lock)))
+
+class MyClass {
+  // Expected rewrite: CheckedPtr<SomeClass> field GUARDED_BY(lock);
+  SomeClass* field GUARDED_BY(lock);
+  int lock;
+};
---
+++ tools/clang/rewrite_raw_ptr_fields/tests/initializers-expected.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class SomeClass;
+
+SomeClass* GetPointer();
+
+class MyClass {
+  // Expected rewrite: CheckedPtr<SomeClass> raw_ptr_field = GetPointer();
+  CheckedPtr<SomeClass> raw_ptr_field = GetPointer();
+};
