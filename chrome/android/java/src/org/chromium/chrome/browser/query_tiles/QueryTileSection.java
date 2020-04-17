93b3e6cd1a901ec0645a9f4102f206f6f1dee438
Shakti Sahu <shaktisahu@chromium.org>
2020-04-17 22:52:02 +0000

    Query tiles : Implement fake tile provider
    This CL adds a fake tile provider that fakes out the backend and provides
    a set of example topics and images. The images are fetched directly from
    the URLs by image fetcher in Java layer.
    
    TBR=dtrainor@chromium.org
    
    Bug: 1069864
    Change-Id: I441aa5d8e63c6c5924199748f3b7d175228dbee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2145678
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760245}

--- chrome/android/java/src/org/chromium/chrome/browser/query_tiles/QueryTileSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/query_tiles/QueryTileSection.java
@@ -50,6 +50,7 @@ public class QueryTileSection {
         mSubmitQueryCallback = performSearchQueryCallback;
         if (!isFeatureEnabled()) return;
 
+        TileProviderFactory.setFakeTileProvider(new FakeTileProvider());
         mTileProvider = TileProviderFactory.getForProfile(profile);
         TileConfig tileConfig = new TileConfig.Builder().setUmaPrefix(UMA_PREFIX).build();
         mTileCoordinator = ImageTileCoordinatorFactory.create(mQueryTileSectionView.getContext(),
