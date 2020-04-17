d5096e5f8969577332a820fa43b302082ee08b5b
Tao Bai <michaelbai@chromium.org>
2020-04-17 04:16:14 +0000

    Add components_junit_tests target
    - Combined 7 components junit tests into one.
    - Fix a variations test which didn't work with other tests.
    
    Bug: 1069479
    Change-Id: Ife508b4c481805f66a2a655f29224c4c5295e12d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2152987
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#759933}

--- components/variations/android/junit/src/org/chromium/components/variations/firstrun/VariationsSeedFetcherTest.java
+++ components/variations/android/junit/src/org/chromium/components/variations/firstrun/VariationsSeedFetcherTest.java
@@ -20,6 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.os.Looper;
 import android.util.Base64;
 
@@ -48,6 +49,7 @@ public class VariationsSeedFetcherTest {
     private HttpURLConnection mConnection;
     private VariationsSeedFetcher mFetcher;
     private SharedPreferences mPrefs;
+    private Looper mOrigLooper;
 
     private static final String sRestrict = "restricted";
     private static final String sMilestone = "64";
@@ -57,6 +59,10 @@ public class VariationsSeedFetcherTest {
     public void setUp() throws IOException {
         // Pretend we are not on the UI thread, since the class we are testing is supposed to run
         // only on a background thread.
+        Handler handler = ThreadUtils.getUiThreadHandler();
+        if (handler != null) mOrigLooper = handler.getLooper();
+        ThreadUtils.setUiThread(null);
+        ThreadUtils.setWillOverrideUiThread(true);
         ThreadUtils.setUiThread(mock(Looper.class));
         mFetcher = spy(VariationsSeedFetcher.get());
         mConnection = mock(HttpURLConnection.class);
@@ -70,6 +76,11 @@ public class VariationsSeedFetcherTest {
     @After
     public void tearDown() {
         ThreadUtils.setUiThread(null);
+        if (mOrigLooper != null) {
+            ThreadUtils.setUiThread(mOrigLooper);
+            mOrigLooper = null;
+        }
+        ThreadUtils.setWillOverrideUiThread(false);
     }
 
     /**
