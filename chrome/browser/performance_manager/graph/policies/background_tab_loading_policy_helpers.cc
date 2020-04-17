ba3a23539ffd2466a9e56c2404fa9451d5ea7fc2
Stephan Hartmann <stha09@googlemail.com>
2020-04-17 16:04:12 +0000

    IWYU: std::numeric_limits is defined in limits
    Change-Id: Ifbc379adc5bb7242c21e7da0bcfc28f49e4dd06f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153111
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760075}

--- chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_helpers.cc
+++ chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_helpers.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/performance_manager/graph/policies/background_tab_loading_policy_helpers.h"
 
 #include <math.h>
+#include <limits>
 
 #include "base/logging.h"
 
