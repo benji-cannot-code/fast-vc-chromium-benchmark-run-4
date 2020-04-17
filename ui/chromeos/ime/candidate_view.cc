7893e4064222fc61cfa9d3a97257bdbf7982d2cb
Akihiro Ota <akihiroota@chromium.org>
2020-04-17 16:53:39 +0000

    Implement basic accessibility for IME candidate window.
    Before this fix, assistive technologies (I tested with ChromeVox) were
    unable to navigate into the IME candidates. For ChromeVox in particular,
    using the arrow keys to navigate the candidates produced no speech
    output.
    
    With this fix, ChromeVox can navigate into the IME candidates and
    speaks each candidate to the user.
    
    Bug: 1034146
    Change-Id: I77b584fc2a2648bc5f9fca479b2363d476628ac4
    Relnotes: Make Chrome IME accessible to assistive technology.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151672
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760099}

--- ui/chromeos/ime/candidate_view.cc
+++ ui/chromeos/ime/candidate_view.cc
@@ -3,10 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ui/chromeos/ime/candidate_view.h"
+
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "ui/accessibility/ax_enums.mojom.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/base/ime/candidate_window.h"
-#include "ui/chromeos/ime/candidate_view.h"
 #include "ui/chromeos/ime/candidate_window_constants.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/native_theme/native_theme.h"
@@ -178,6 +181,7 @@ void CandidateView::SetHighlighted(bool highlighted) {
 
   highlighted_ = highlighted;
   if (highlighted) {
+    NotifyAccessibilityEvent(ax::mojom::Event::kSelection, false);
     ui::NativeTheme* theme = GetNativeTheme();
     SetBackground(views::CreateSolidBackground(theme->GetSystemColor(
         ui::NativeTheme::kColorId_TextfieldSelectionBackgroundFocused)));
@@ -279,6 +283,11 @@ gfx::Size CandidateView::CalculatePreferredSize() const {
   size.Enlarge(
       kInfolistIndicatorIconWidth + kInfolistIndicatorIconPadding * 2, 0);
   return size;
+}
+
+void CandidateView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  node_data->SetName(candidate_label_->GetText());
+  node_data->role = ax::mojom::Role::kMenuItem;
 }
 
 }  // namespace ime
--- ui/chromeos/ime/candidate_window_view.cc
+++ ui/chromeos/ime/candidate_window_view.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "ui/accessibility/ax_enums.mojom.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/chromeos/ime/candidate_view.h"
 #include "ui/chromeos/ime/candidate_window_constants.h"
 #include "ui/display/display.h"
@@ -410,6 +412,10 @@ void CandidateWindowView::ButtonPressed(views::Button* sender,
       return;
     }
   }
+}
+
+void CandidateWindowView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  node_data->role = ax::mojom::Role::kMenu;
 }
 
 }  // namespace ime
