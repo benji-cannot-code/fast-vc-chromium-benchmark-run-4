7b9911b92052f7382effef1c7dba4bcfbe959edf
Meilin Wang <meilinw@google.com>
2020-04-17 20:46:11 +0000

    Reland "ambient: Show lockscreen contents on detecting user interaction."
    This reverts commit b3f1a19a328c4b5dce8710dab293bb02b5e93c4e.
    
    Reason for revert: The original CL broke the test because the expected
    behavior has been updated for that test case. The try bot didn't alert
    this failure as the failed test was flaky. Patchset 3 fixed this
    temporarily by deleting the obsolete test case. I will add more test
    cases to guard the updated behavior and deflake the original test in a
    follow-up CL.
    
    Original change's description:
    > Revert "ambient: Show lockscreen contents on detecting user interaction."
    >
    > This reverts commit 660c9cefdce872c19a9918e19caf01c18ab86890.
    >
    > Reason for revert: AmbientContainerViewTest.MouseClickClosesWidgetAndStopsTimer failing deterministically on several linux-chromeos bots. Please see https://crbug.com/1068745 for more information.
    >
    > Original change's description:
    > > ambient: Show lockscreen contents on detecting user interaction.
    > >
    > > In ambient mode, when user interacting with the background image by
    > > mouse or gesture, lock screen contents (login pod and media control
    > > view) need be shown on top for users to either do re-authentication,
    > > or media control.
    > >
    > > This CL implements a prototype of this view transaction by:
    > > 1. Show the lock screen contents, and
    > > 2. Fade-out the current shown image.
    > >
    > > Bug: b/149246238
    > > Test: manually.
    > > Change-Id: Ie31c666cdfaddefc863ff8173dc185d73ef19178
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2110689
    > > Commit-Queue: Meilin Wang <meilinw@chromium.org>
    > > Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#757120}
    >
    > Change-Id: I395a7c02c6aa292f66ca5d9b4aefa9156e057773
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: b/149246238, 1068745
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2139369
    > Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    > Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#757223}
    
    Bug: b/149246238, 1068745
    Change-Id: I196c60cb21917dd84f9e7018950d5bf1b788b1ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2141067
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760196}

--- ash/ambient/ambient_controller.cc
+++ ash/ambient/ambient_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ambient/ui/ambient_container_view.h"
 #include "ash/ambient/util/ambient_util.h"
 #include "ash/assistant/assistant_controller_impl.h"
+#include "ash/assistant/util/animation_util.h"
 #include "ash/login/ui/lock_screen.h"
 #include "ash/public/cpp/ambient/ambient_mode_state.h"
 #include "ash/public/cpp/ambient/ambient_prefs.h"
@@ -94,20 +95,17 @@ void AmbientController::OnAmbientModeEnabled(bool enabled) {
 }
 
 void AmbientController::OnLockStateChanged(bool locked) {
-  if (!locked) {
-    // We should already exit ambient mode at this time, as the ambient
-    // container needs to be closed to uncover the login port for
-    // re-authentication.
+  if (locked) {
+    // Show ambient mode when entering lock screen.
     DCHECK(!container_view_);
-    return;
+    Show();
+  } else {
+    // Destroy ambient mode after user re-login.
+    Destroy();
   }
-
-  // Show the ambient container on top of the lock screen.
-  DCHECK(!container_view_);
-  Start();
 }
 
-void AmbientController::Start() {
+void AmbientController::Show() {
   if (!CanStartAmbientMode()) {
     // TODO(wutao): Show a toast to indicate that Ambient mode is not ready.
     return;
@@ -122,14 +120,34 @@ void AmbientController::Start() {
   ambient_state_.SetAmbientModeEnabled(true);
 }
 
-void AmbientController::Stop() {
+void AmbientController::Destroy() {
   ambient_state_.SetAmbientModeEnabled(false);
 }
+
 void AmbientController::Toggle() {
   if (container_view_)
-    Stop();
+    Destroy();
   else
-    Start();
+    Show();
+}
+
+void AmbientController::OnBackgroundPhotoEvents() {
+  refresh_timer_.Stop();
+
+  // Move the |AmbientModeContainer| beneath the |LockScreenWidget| to show the
+  // lock screen contents on top before the fade-out animation.
+  auto* ambient_window = container_view_->GetWidget()->GetNativeWindow();
+  ambient_window->parent()->StackChildAtBottom(ambient_window);
+
+  // Start fading out the current background photo.
+  StartFadeOutAnimation();
+}
+
+void AmbientController::StartFadeOutAnimation() {
+  // We fade out the |PhotoView| on its own layer instead of using the general
+  // layer of the widget, otherwise it will reveal the color of the lockscreen
+  // wallpaper beneath.
+  container_view_->FadeOutPhotoView();
 }
 
 void AmbientController::CreateContainerView() {
@@ -147,9 +165,6 @@ void AmbientController::DestroyContainerView() {
 }
 
 void AmbientController::RefreshImage() {
-  if (!PhotoController::Get())
-    return;
-
   if (photo_model_.ShouldFetchImmediately()) {
     // TODO(b/140032139): Defer downloading image if it is animating.
     base::SequencedTaskRunnerHandle::Get()->PostDelayedTask(
