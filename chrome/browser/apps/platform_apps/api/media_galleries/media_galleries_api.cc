a8e022d9f7bdce0b9ebac41b1a35b774aad1e661
Istiaque Ahmed <lazyboy@chromium.org>
2020-04-17 21:05:40 +0000

    Pass OnceClosure to MediaGalleriesPreferences::EnsureInitialized().
    Instead of a RepeatingClosure, pass OnceClosure as the method
    would call it at most once and is the correct thing to do.
    
    Bug: None
    Change-Id: I9ad50e31117f0fc677e21878b7c4787431cb8422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154586
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760207}

--- chrome/browser/apps/platform_apps/api/media_galleries/media_galleries_api.cc
+++ chrome/browser/apps/platform_apps/api/media_galleries/media_galleries_api.cc
@@ -112,7 +112,7 @@ GalleryWatchManager* gallery_watch_manager() {
 // Checks whether the MediaGalleries API is currently accessible (it may be
 // disallowed even if an extension has the requisite permission). Then
 // initializes the MediaGalleriesPreferences
-bool Setup(Profile* profile, std::string* error, base::Closure callback) {
+bool Setup(Profile* profile, std::string* error, base::OnceClosure callback) {
   if (!ChromeSelectFilePolicy::FileSelectDialogsAllowed()) {
     *error =
         std::string(kDisallowedByPolicy) + prefs::kAllowFileSelectionDialogs;
@@ -121,7 +121,7 @@ bool Setup(Profile* profile, std::string* error, base::Closure callback) {
 
   MediaGalleriesPreferences* preferences =
       media_file_system_registry()->GetPreferences(profile);
-  preferences->EnsureInitialized(callback);
+  preferences->EnsureInitialized(std::move(callback));
   return true;
 }
 
@@ -408,8 +408,9 @@ bool MediaGalleriesGetMediaFileSystemsFunction::RunAsync() {
 
   return Setup(
       GetProfile(), &error_,
-      base::Bind(&MediaGalleriesGetMediaFileSystemsFunction::OnPreferencesInit,
-                 this, interactive));
+      base::BindOnce(
+          &MediaGalleriesGetMediaFileSystemsFunction::OnPreferencesInit, this,
+          interactive));
 }
 
 void MediaGalleriesGetMediaFileSystemsFunction::OnPreferencesInit(
@@ -509,7 +510,7 @@ bool MediaGalleriesAddUserSelectedFolderFunction::RunAsync() {
   ::media_galleries::UsageCount(::media_galleries::ADD_USER_SELECTED_FOLDER);
   return Setup(
       GetProfile(), &error_,
-      base::Bind(
+      base::BindOnce(
           &MediaGalleriesAddUserSelectedFolderFunction::OnPreferencesInit,
           this));
 }
@@ -619,9 +620,10 @@ bool MediaGalleriesGetMetadataFunction::RunAsync() {
   if (!options)
     return false;
 
-  return Setup(GetProfile(), &error_,
-               base::Bind(&MediaGalleriesGetMetadataFunction::OnPreferencesInit,
-                          this, options->metadata_type, blob_uuid));
+  return Setup(
+      GetProfile(), &error_,
+      base::BindOnce(&MediaGalleriesGetMetadataFunction::OnPreferencesInit,
+                     this, options->metadata_type, blob_uuid));
 }
 
 void MediaGalleriesGetMetadataFunction::OnPreferencesInit(
@@ -799,8 +801,8 @@ bool MediaGalleriesAddGalleryWatchFunction::RunAsync() {
       g_browser_process->media_file_system_registry()->GetPreferences(
           GetProfile());
   preferences->EnsureInitialized(
-      base::Bind(&MediaGalleriesAddGalleryWatchFunction::OnPreferencesInit,
-                 this, params->gallery_id));
+      base::BindOnce(&MediaGalleriesAddGalleryWatchFunction::OnPreferencesInit,
+                     this, params->gallery_id));
 
   return true;
 }
@@ -874,9 +876,9 @@ bool MediaGalleriesRemoveGalleryWatchFunction::RunAsync() {
   MediaGalleriesPreferences* preferences =
       g_browser_process->media_file_system_registry()->GetPreferences(
           GetProfile());
-  preferences->EnsureInitialized(
-      base::Bind(&MediaGalleriesRemoveGalleryWatchFunction::OnPreferencesInit,
-                 this, params->gallery_id));
+  preferences->EnsureInitialized(base::BindOnce(
+      &MediaGalleriesRemoveGalleryWatchFunction::OnPreferencesInit, this,
+      params->gallery_id));
   return true;
 }
 
