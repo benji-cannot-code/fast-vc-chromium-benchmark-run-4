f5375d7a0ee1391473951adca30f19d086336fb5
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-04-17 13:36:52 +0000

    Allow mocking of SigninViewController methods
    Unit testing the new AccountStorageAuthHelper requires mocking 2 of the
    methods of SigninViewController. This CL makes such methods virtual and
    adds the basic unit test setup.
    
    Future CLs will:
    1) Refactor AccountStorageAuthHelper so that it can be injected a
    signin_view_controller_getter callback. The controller itself cannot be
    directly injected since its attached to a browser, while the helper is
    per tab.
    2) Add the actual tests.
    
    Bug: 1070944
    Change-Id: I48fe5031e24ee6488be293d7f96f44c4716ae6b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153047
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760039}

--- chrome/browser/ui/signin_view_controller.h
+++ chrome/browser/ui/signin_view_controller.h
@@ -75,8 +75,8 @@ class SigninViewController {
 
   // Shows the Dice "add account" tab, which adds an account to the browser but
   // does not turn sync on. |email_hint| may be empty.
-  void ShowDiceAddAccountTab(signin_metrics::AccessPoint access_point,
-                             const std::string& email_hint);
+  virtual void ShowDiceAddAccountTab(signin_metrics::AccessPoint access_point,
+                                     const std::string& email_hint);
 
   // Shows the modal sign-in email confirmation dialog as a tab-modal dialog on
   // top of the currently displayed WebContents in |browser_|.
@@ -104,7 +104,7 @@ class SigninViewController {
   // indefinitely.
   // The reauth prompt that is currently displayed can be cancelled by
   // CloseModalSignin().
-  void ShowReauthPrompt(
+  virtual void ShowReauthPrompt(
       const CoreAccountId& account_id,
       base::OnceCallback<void(signin::ReauthResult)> reauth_callback);
 
