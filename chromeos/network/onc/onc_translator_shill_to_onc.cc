f7e6f08586d7fe797c30347fe60dd72fa243bbca
Alex Khouderchah <akhouderchah@chromium.org>
2020-04-17 22:11:04 +0000

    Remove references to unused Shill Service properties
    This change removes any remaining references to the following unused
    Shill Service properties:
      * kConnectedProperty ("Connected")
      * kEapCaCertNssProperty ("EAP.CACertNSS")
      * kEapClientCertNssProperty ("EAP.ClientCertNSS")
      * kEapClientCertProperty ("EAP.ClientCert")
      * kEapPrivateKeyPasswordProperty ("EAP.PrivateKeyPassword")
      * kEapPrivateKeyProperty ("EAP.PrivateKey")
      * kIdentityProperty ("Identity")
      * kOpenVPNCaCertNSSProperty ("OpenVPN.CACertNSS")
      * kOpenVPNCaCertProperty ("OpenVPN.CACert")
      * kOpenVPNCertProperty ("OpenVPN.Cert")
      * kOpenVPNClientCertSlotProperty ("OpenVPN.Pkcs11.Slot")
    
    Bug: None
    Change-Id: I69a50ee685fd23230d34d4190884f01c60379401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154544
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Alex Khouderchah <akhouderchah@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760228}

--- chromeos/network/onc/onc_translator_shill_to_onc.cc
+++ chromeos/network/onc/onc_translator_shill_to_onc.cc
@@ -250,7 +250,7 @@ void ShillToONCTranslator::TranslateOpenVPN() {
   }
 
   SetPKCS11Id(shill_dictionary_, shill::kOpenVPNClientCertIdProperty,
-              shill::kOpenVPNClientCertSlotProperty, onc_object_.get());
+              "", onc_object_.get());
 
   TranslateWithTableAndSet(shill::kOpenVPNCompressProperty,
                            kOpenVpnCompressionAlgorithmTable,
