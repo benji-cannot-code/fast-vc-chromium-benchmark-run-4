480c025047b294ed8a501a18cd120699adb69b96
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2020-04-18 00:27:38 +0000

    Add missing algorithm header in crx_install_error.cc
    This is needed for the use of std::find.
    
    Change-Id: I2dc43b3887c467986c5346be5a9e27a987e1e5b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2152333
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#760272}

--- extensions/browser/install/crx_install_error.cc
+++ extensions/browser/install/crx_install_error.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/browser/install/crx_install_error.h"
 
+#include <algorithm>
+
 #include "base/logging.h"
 #include "extensions/browser/install/sandboxed_unpacker_failure_reason.h"
 
