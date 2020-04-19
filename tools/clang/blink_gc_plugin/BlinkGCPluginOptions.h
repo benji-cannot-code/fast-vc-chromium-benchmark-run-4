7eaaaccba4c1dc8537de842180cafda6b19fc73b
Anton Bikineev <bikineev@chromium.org>
2020-04-19 03:40:18 +0000

    Reland "clang plugins: default no-members-in-stack-allocated to on"
    This also adjusts the tests.
    
    Original change's description:
    > clang plugins: default no-members-in-stack-allocated to on
    >
    > Artifacts can be removed after the clang prebuilt is updated.
    >
    > Change-Id: I473c22843445745a39d1fc648a7bbcc336631b8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2152329
    > Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    > Reviewed-by: Anton Bikineev <bikineev@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Johann Koenig <johannkoenig@google.com>
    > Cr-Commit-Position: refs/heads/master@{#759880}
    
    Change-Id: I16e07fe68e308005350c37cf50d0fe5c1e2611e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154788
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760338}

--- tools/clang/blink_gc_plugin/BlinkGCPluginOptions.h
+++ tools/clang/blink_gc_plugin/BlinkGCPluginOptions.h
@@ -13,9 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct BlinkGCPluginOptions {
   bool dump_graph = false;
 
-  // If |true|, disallow Member types in stack allocated classes.
-  bool no_members_in_stack_allocated = false;
-
   // Member<T> fields are only permitted in managed classes,
   // something CheckFieldsVisitor verifies, issuing errors if
   // found in unmanaged classes. WeakMember<T> should be treated
