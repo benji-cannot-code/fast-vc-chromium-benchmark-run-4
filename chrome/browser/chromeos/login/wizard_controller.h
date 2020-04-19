6a06d6171460309148bd9e1bbc81a82aa5874bc3
Roman Aleksandrov <raleksandrov@google.com>
2020-04-19 14:27:10 +0000

    ArcToSScreen: Remove flow related to Skip ToS button
    Remove Skip related methods and tests because there is no more
    Skip button on the ArcToS screen.
    
    Bug: 1069792
    Change-Id: I35362c0c6598ce2b68334ecc1bb06c6d50532632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144960
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760355}

--- chrome/browser/chromeos/login/wizard_controller.h
+++ chrome/browser/chromeos/login/wizard_controller.h
@@ -232,7 +232,6 @@ class WizardController {
   void OnFingerprintSetupScreenExit(FingerprintSetupScreen::Result result);
   void OnDiscoverScreenExit();
   void OnArcTermsOfServiceScreenExit(ArcTermsOfServiceScreen::Result result);
-  void OnArcTermsOfServiceSkipped();
   void OnArcTermsOfServiceAccepted();
   void OnRecommendAppsScreenExit(RecommendAppsScreen::Result result);
   void OnAppDownloadingScreenExit();
