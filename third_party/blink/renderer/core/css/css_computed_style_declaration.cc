b8689b1c16f62cf34b72e151f957b1f051772f12
Rob Buis <rbuis@igalia.com>
2020-04-19 13:18:09 +0000

    [mathml] Implement the math-superscript-shift-style CSS property
    Implement the math-superscript-shift-style CSS property [1] which encapsulates the notion of "cramped" defined in TeX (causing superscripts to raise lower). In OpenType [2], this corresponds to choosing between two parameters: superscriptShiftUp (default shift) and superscriptShiftUpCramped (smaller shift). Default style is non-cramped, property is inherited and becomes cramped in various cases e.g. within subscripts or radicals.
    
    [1] https://mathml-refresh.github.io/mathml-core/#the-math-superscript-shift-style
    [2] https://docs.microsoft.com/en-us/typography/opentype/spec/math
    
    Bug: 6606
    Change-Id: Ie06526e368b185c855ecf13eb2bcbdcac0376c36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144185
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760353}

--- third_party/blink/renderer/core/css/css_computed_style_declaration.cc
+++ third_party/blink/renderer/core/css/css_computed_style_declaration.cc
@@ -204,7 +204,8 @@ const CSSPropertyID kComputedPropertyArray[] = {
     CSSPropertyID::kCy, CSSPropertyID::kX, CSSPropertyID::kY, CSSPropertyID::kR,
     CSSPropertyID::kRx, CSSPropertyID::kRy, CSSPropertyID::kTranslate,
     CSSPropertyID::kRotate, CSSPropertyID::kScale, CSSPropertyID::kCaretColor,
-    CSSPropertyID::kLineBreak, CSSPropertyID::kMathStyle};
+    CSSPropertyID::kLineBreak, CSSPropertyID::kMathStyle,
+    CSSPropertyID::kMathSuperscriptShiftStyle};
 
 CSSValueID CssIdentifierForFontSizeKeyword(int keyword_size) {
   DCHECK_NE(keyword_size, 0);
