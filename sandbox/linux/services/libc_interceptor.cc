8713f9e130a7976adedab190e93a18eac8f24856
Hans Wennborg <hans@chromium.org>
2020-04-20 16:24:51 +0000

    logging.h related include-what-you-use (sandbox/)
    Fix code that was implicitly depending on logging.h or its
    dependencies.
    
    For example, code that uses DISALLOW_COPY_AND_ASSIGN should
    include base/macros.h, but it currently happens to work if
    it pulls in logging.h directly or through some dependency.
    
    This is part of refactoring the codebase to use
    check.h/check_op.h for the CHECK and CHECK_op macro
    instead of logging.h.
    
    Bug: 1031540
    Change-Id: I34bdf1c886255c953706e98910abdb4204efd2de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157068
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760527}

--- sandbox/linux/services/libc_interceptor.cc
+++ sandbox/linux/services/libc_interceptor.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/lazy_instance.h"
+#include "base/logging.h"
 #include "base/pickle.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/posix/global_descriptors.h"
--- sandbox/win/src/process_mitigations.cc
+++ sandbox/win/src/process_mitigations.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/scoped_native_library.h"
 #include "base/win/windows_version.h"
 #include "build/build_config.h"
