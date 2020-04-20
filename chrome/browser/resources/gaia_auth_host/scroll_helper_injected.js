4046f69be4659f5058d2cbaabebe2938f4e5a1b4
Renato Silva <rrsilva@google.com>
2020-04-20 19:54:54 +0000

    OOBE - Fix Gaia Sign-in scroll shadows
    Use the Resize Observer API for the webviews used within OOBE.
    This ensures that the shadows adapt dynamically to content
    changes that might otherwise not generate any event at all.
    
    Fixed: 1068251
    Change-Id: If806c8cedb219d84024030cd9859986bcee626e0
    Bug: 1068251
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151509
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760643}

--- chrome/browser/resources/gaia_auth_host/scroll_helper_injected.js
+++ chrome/browser/resources/gaia_auth_host/scroll_helper_injected.js
@@ -22,22 +22,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
       window.addEventListener('scroll', this.sendScrollInfo_.bind(this));
       window.addEventListener('resize', this.sendScrollInfo_.bind(this));
-
-      // Besides binding to the 'scroll' and 'resize' events, send once the
-      // scroll information when the page if fully loaded.
-      if (document.readyState === 'complete') {
+      this.boundAttachResizeObserver_ = this.attachResizeObserver_.bind(this);
+      window.addEventListener('load', this.boundAttachResizeObserver_);
+      this.resizeObserver = new ResizeObserver(() => {
         this.sendScrollInfo_();
-        return;
-      }
+      });
+    },
 
-      const boundSendScrollInfo = this.sendScrollInfo_.bind(this);
-      window.addEventListener('readystatechange', function listener(event) {
-        if (document.readyState != 'complete') {
-          return;
-        }
-        boundSendScrollInfo();
-        window.removeEventListener(event.type, listener, true);
-      }, true);
+    // Observe when document.body changes in size.
+    attachResizeObserver_(event) {
+      this.resizeObserver.observe(document.body);
+      window.removeEventListener(event.type, this.boundAttachResizeObserver_);
     },
 
     sendScrollInfo_(event) {
