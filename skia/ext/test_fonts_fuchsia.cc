044ad85dc523ca16e297bf0eb4abe2b1994c81f2
Hans Wennborg <hans@chromium.org>
2020-04-20 17:14:24 +0000

    logging.h related include-what-you-use (skia/)
    Fix code that was implicitly depending on logging.h or its
    dependencies.
    
    For example, code that uses DISALLOW_COPY_AND_ASSIGN should
    include base/macros.h, but it currently happens to work if
    it pulls in logging.h directly or through some dependency.
    
    This is part of refactoring the codebase to use
    check.h/check_op.h for the CHECK and CHECK_op macro
    instead of logging.h.
    
    Bug: 1031540
    Change-Id: Ib8732b1732c3a92dc2a81eaf1e58fa063db45f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157007
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760545}

--- skia/ext/test_fonts_fuchsia.cc
+++ skia/ext/test_fonts_fuchsia.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/fuchsia/default_context.h"
 #include "base/fuchsia/file_utils.h"
+#include "base/logging.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
 #include "skia/ext/fontmgr_default.h"
