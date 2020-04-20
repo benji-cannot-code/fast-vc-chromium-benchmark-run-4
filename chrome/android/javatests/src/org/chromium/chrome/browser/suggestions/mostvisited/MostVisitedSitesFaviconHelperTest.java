bbbf251a5f773c064c52cb6f424f5f2d26e4dfd4
spdonghao <spdonghao@chromium.org>
2020-04-20 19:29:58 +0000

    Introduce MostVisitedSitesHost.
    Class for saving most visited sites info.
    
    Bug: 1061906
    Change-Id: Ida98211ed2cf26d9c3302d3efa6e84ff39d1331e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129226
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760620}

--- chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/mostvisited/MostVisitedSitesFaviconHelperTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/suggestions/mostvisited/MostVisitedSitesFaviconHelperTest.java
@@ -88,7 +88,7 @@ public class MostVisitedSitesFaviconHelperTest {
                 urlsToUpdate.size(), () -> getStateDirectorySize() - originalFilesNum));
 
         // The Favicon File lists in the disk.
-        File topSitesDirectory = MostVisitedSitesMetadataUtils.getStateDirectoryForTesting();
+        File topSitesDirectory = MostVisitedSitesMetadataUtils.getStateDirectory();
         Assert.assertNotNull(topSitesDirectory);
         String[] faviconFiles = topSitesDirectory.list();
         Assert.assertNotNull(faviconFiles);
@@ -142,8 +142,7 @@ public class MostVisitedSitesFaviconHelperTest {
     }
 
     private static int getStateDirectorySize() {
-        return Objects
-                .requireNonNull(MostVisitedSitesMetadataUtils.getStateDirectoryForTesting().list())
+        return Objects.requireNonNull(MostVisitedSitesMetadataUtils.getStateDirectory().list())
                 .length;
     }
 }
