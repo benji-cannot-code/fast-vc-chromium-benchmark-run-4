aeeda2ea84ec3f1a3be825bfc6bd26a23b808de5
Jing Wang <jiwan@chromium.org>
2020-04-20 02:46:13 +0000

    Implement keep-typing feature for personal info suggester.
    keep-typing feature: when the suggestion pops up and the user keeps
    typing, if what they type is a prefix of the suggestion, the popup will
    not disappear, and in the suggestion text the typed prefix will be
    displayed in black and bold. The popup will disappear anyway if the user
    has typed more than 10 characters.
    
    Test: tested with Linux emulator
    Bug: 1042084
    Change-Id: I157cd92cf29bb308e09764109d2eb0333183af7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153181
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760375}

--- chrome/browser/chromeos/input_method/assistive_window_controller_impl.h
+++ chrome/browser/chromeos/input_method/assistive_window_controller_impl.h
@@ -38,6 +38,7 @@ class AssistiveWindowControllerImpl
                       const bool show_tab) override;
   void HideSuggestion() override;
   base::string16 GetSuggestionText() const override;
+  size_t GetConfirmedLength() const override;
   void FocusStateChanged() override;
   void OnWidgetClosing(views::Widget* widget) override;
 
@@ -45,6 +46,7 @@ class AssistiveWindowControllerImpl
 
   ui::ime::SuggestionWindowView* suggestion_window_view_ = nullptr;
   base::string16 suggestion_text_;
+  size_t confirmed_length_;
 
   DISALLOW_COPY_AND_ASSIGN(AssistiveWindowControllerImpl);
 };
