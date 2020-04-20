a1b2194384e3b62ce7c9148a2192f95a9ce08be7
Brian Liu Xu <brx@microsoft.com>
2020-04-20 09:17:00 +0000

    Stop scrolling |TreeView| and |TableView| to the top upon focus
    Updates View::Focus() so that |ScrollView| contents do not unexpectedly scroll to the top upon their contents root receiving focus. By default, a |View| scrolls itself to local bounds upon receiving focus, but this does not make sense for the contents root of a |ScrollView|. To avoid this, we modify View::Focus() so that contents roots call their |ScrollView|'s ScrollViewToVisible() instead of their own.
    
    Bug: 811277
    Change-Id: I845115f721ee2fe8f90c016cfc7669edcf451c85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2152626
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Brian Liu Xu <brx@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#760423}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/border.h"
 #include "ui/views/buildflags.h"
 #include "ui/views/context_menu_controller.h"
+#include "ui/views/controls/scroll_view.h"
 #include "ui/views/drag_controller.h"
 #include "ui/views/layout/layout_manager.h"
 #include "ui/views/metadata/metadata_impl_macros.h"
@@ -1973,7 +1974,14 @@ void View::OnBlur() {}
 
 void View::Focus() {
   OnFocus();
-  ScrollViewToVisible();
+
+  // If this is the contents root of a |ScrollView|, focus should bring the
+  // |ScrollView| to visible rather than resetting its content scroll position.
+  ScrollView* scroll_view = ScrollView::GetScrollViewForContents(this);
+  if (scroll_view)
+    scroll_view->ScrollViewToVisible();
+  else
+    ScrollViewToVisible();
 
   for (ViewObserver& observer : observers_)
     observer.OnViewFocused(this);
