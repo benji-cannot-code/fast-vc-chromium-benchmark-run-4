e5eb6bee0f921fe1507d9b31ae86c17bf9a94afd
Matthew Jones <mdjones@chromium.org>
2020-04-20 21:19:20 +0000

    Revert "[Sheriff][Android] Conditionally disable some tests in BottomSheetObserverTest"
    This reverts commit 5d3ed01a127a4934ec5a999ccad9d136a2124811.
    
    Reason for revert: Flake fixes here: https://chromium-review.googlesource.com/c/chromium/src/+/2157592
    
    Original change's description:
    > [Sheriff][Android] Conditionally disable some tests in BottomSheetObserverTest
    > 
    > Reason: flaky on android-arm64-proguard-rel
    > https://ci.chromium.org/p/chromium/builders/ci/android-arm64-proguard-rel/1491.
    > 
    > TBR=mdjones@chromium.org
    > 
    > Bug: 1072455
    > Change-Id: Ic4254f73673041cccd94fbe493d4c6fb8c31a9bb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156988
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#760589}
    
    TBR=mdjones@chromium.org,bsazonov@chromium.org
    
    Change-Id: I4dfb525f4adc728ff02b4becf71af2922faae924
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1072455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157827
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760694}

--- chrome/android/javatests/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetObserverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetObserverTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-import android.os.Build;
 import android.support.test.filters.MediumTest;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +22,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.MathUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.ui.test.util.UiRestriction;
@@ -72,10 +70,7 @@ public class BottomSheetObserverTest {
     /** Test that the onSheetClosed event is triggered if the sheet is closed with animation. */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.LOLLIPOP,
-            sdk_is_less_than = Build.VERSION_CODES.N, message = "https://crbug.com/1072455")
-    public void
-    testCloseEventCalled_withAnimation() throws TimeoutException {
+    public void testCloseEventCalled_withAnimation() throws TimeoutException {
         runCloseEventTest(true, true);
     }
 
@@ -138,20 +133,14 @@ public class BottomSheetObserverTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.LOLLIPOP,
-            sdk_is_less_than = Build.VERSION_CODES.N, message = "https://crbug.com/1072455")
-    public void
-    testOpenedEventCalled_noAnimationNoPeekState() throws TimeoutException {
+    public void testOpenedEventCalled_noAnimationNoPeekState() throws TimeoutException {
         runOpenEventTest(false, false);
     }
 
     /** Test that the onSheetOpened event is triggered if the sheet is opened with animation. */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.LOLLIPOP,
-            sdk_is_less_than = Build.VERSION_CODES.N, message = "https://crbug.com/1072455")
-    public void
-    testOpenedEventCalled_withAnimation() throws TimeoutException {
+    public void testOpenedEventCalled_withAnimation() throws TimeoutException {
         runOpenEventTest(true, true);
     }
 
@@ -210,10 +199,7 @@ public class BottomSheetObserverTest {
      */
     @Test
     @MediumTest
-    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.LOLLIPOP,
-            sdk_is_less_than = Build.VERSION_CODES.N, message = "https://crbug.com/1072455")
-    public void
-    testOffsetChangedEvent() throws TimeoutException {
+    public void testOffsetChangedEvent() throws TimeoutException {
         mBottomSheetTestRule.setSheetState(BottomSheetController.SheetState.FULL, false);
         CallbackHelper callbackHelper = mObserver.mOffsetChangedCallbackHelper;
 
