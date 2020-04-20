16983a73fb525b2f62de576e666a6f4522c143f6
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-04-20 16:13:53 +0000

    [base] Apply BindOnceRewriter in //chrome/browser/devtools
    This change applies the BindOnceRewriter in //chrome/browser/devtools.
    This rewriter replaces calls to base::Bind and base::BindRepeating with
    calls to base::BindOnce, in case the result is immediately assigned to a
    base::OnceCallback. Given that base::RepeatingCallback is implicitly
    convertible to base::OnceCallback, there is no change in functionality.
    
    Steps:
      1. run_tool.py --tool base_bind_rewriters \
                     --tool-arg='--rewriter=bind_to_bind_once'
      2. git cl format
    
    This CL was uploaded by git cl split.
    
    R=pfeldman@chromium.org
    
    Bug: 714018
    Change-Id: I8247fc695ed397a148f7e42f3d2c6529c03673a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2132213
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760522}

--- chrome/browser/devtools/device/usb/usb_device_provider.cc
+++ chrome/browser/devtools/device/usb/usb_device_provider.cc
@@ -45,9 +45,8 @@ void OnRead(net::StreamSocket* socket,
 
   std::string new_data = data + std::string(buffer->data(), result);
   result =
-      socket->Read(buffer.get(),
-                   kBufferSize,
-                   base::Bind(&OnRead, socket, buffer, new_data, callback));
+      socket->Read(buffer.get(), kBufferSize,
+                   base::BindOnce(&OnRead, socket, buffer, new_data, callback));
   if (result != net::ERR_IO_PENDING)
     OnRead(socket, buffer, new_data, callback, result);
 }
@@ -62,9 +61,8 @@ void OpenedForCommand(const UsbDeviceProvider::CommandCallback& callback,
   scoped_refptr<net::IOBuffer> buffer =
       base::MakeRefCounted<net::IOBuffer>(kBufferSize);
   result = socket->Read(
-      buffer.get(),
-      kBufferSize,
-      base::Bind(&OnRead, socket, buffer, std::string(), callback));
+      buffer.get(), kBufferSize,
+      base::BindOnce(&OnRead, socket, buffer, std::string(), callback));
   if (result != net::ERR_IO_PENDING)
     OnRead(socket, buffer, std::string(), callback, result);
 }
@@ -77,8 +75,8 @@ void RunCommand(scoped_refptr<AndroidUsbDevice> device,
     callback.Run(net::ERR_CONNECTION_FAILED, std::string());
     return;
   }
-  int result = socket->Connect(
-      base::Bind(&OpenedForCommand, callback, socket));
+  int result =
+      socket->Connect(base::BindOnce(&OpenedForCommand, callback, socket));
   if (result != net::ERR_IO_PENDING)
     callback.Run(result, std::string());
 }
@@ -124,7 +122,7 @@ void UsbDeviceProvider::OpenSocket(const std::string& serial,
                  base::WrapUnique<net::StreamSocket>(NULL));
     return;
   }
-  int result = socket->Connect(base::Bind(&OnOpenSocket, callback, socket));
+  int result = socket->Connect(base::BindOnce(&OnOpenSocket, callback, socket));
   if (result != net::ERR_IO_PENDING)
     callback.Run(result, base::WrapUnique<net::StreamSocket>(NULL));
 }
