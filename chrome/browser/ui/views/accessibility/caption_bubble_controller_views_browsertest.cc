eac5b83386ca7a975cff9a376732c73937cd06b6
Katie D <katie@chromium.org>
2020-04-20 22:39:26 +0000

    Disable caption positioning test on Mac.
    Failed in Mac10.12 although it passed on other Mac versions.
    Needs investigation separately.
    
    TBR=pdr@chromium.org
    
    Bug: 1072818
    Change-Id: I25cf06ce75235934e4c4cbaf79a9b9192bfb72f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2158015
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760727}

--- chrome/browser/ui/views/accessibility/caption_bubble_controller_views_browsertest.cc
+++ chrome/browser/ui/views/accessibility/caption_bubble_controller_views_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/macros.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/accessibility/caption_bubble.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
@@ -119,7 +120,14 @@ IN_PROC_BROWSER_TEST_F(CaptionBubbleControllerViewsTest,
   EXPECT_FALSE(GetTitle()->GetVisible());
 }
 
-IN_PROC_BROWSER_TEST_F(CaptionBubbleControllerViewsTest, BubblePositioning) {
+// Flaky on Mac 10.12, crbug.com/1072818.
+#if defined(OS_MACOSX)
+#define MAYBE_BubblePositioning DISABLED_BubblePositioning
+#else
+#define MAYBE_BubblePositioning BubblePositioning
+#endif
+IN_PROC_BROWSER_TEST_F(CaptionBubbleControllerViewsTest,
+                       MAYBE_BubblePositioning) {
   views::View* contents_view =
       BrowserView::GetBrowserViewForBrowser(browser())->GetContentsView();
 
