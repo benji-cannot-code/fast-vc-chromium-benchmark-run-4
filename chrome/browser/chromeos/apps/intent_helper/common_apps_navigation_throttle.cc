1f3a9f294c1235725214f47c1d20c742771a424a
Maggie Cai <mxcai@chromium.org>
2020-04-20 08:07:32 +0000

    [IntentHandling] Fix common apps navigation throttle error.
    This CL fixes an error in common apps navigation throttle. After load
    app icons, we should return true to indicate we need to defer the
    navigation. Otherwise it will cause DCHECK error when resume navigation.
    
    BUG=853604
    
    Change-Id: Ic618686070ab7eb237adf7b194de61cdceafcfd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2155764
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760404}

--- chrome/browser/chromeos/apps/intent_helper/common_apps_navigation_throttle.cc
+++ chrome/browser/chromeos/apps/intent_helper/common_apps_navigation_throttle.cc
@@ -240,7 +240,7 @@ bool CommonAppsNavigationThrottle::ShouldDeferNavigation(
       base::BindOnce(
           &CommonAppsNavigationThrottle::OnDeferredNavigationProcessed,
           weak_factory_.GetWeakPtr()));
-  return false;
+  return true;
 }
 
 void CommonAppsNavigationThrottle::OnDeferredNavigationProcessed(
