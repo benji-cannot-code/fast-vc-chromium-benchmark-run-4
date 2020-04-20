ee3515c156646a2f6a90ecb45cc5787a52c496de
Hans Wennborg <hans@chromium.org>
2020-04-20 16:54:49 +0000

    logging.h related include-what-you-use (google_apis/)
    Fix code that was implicitly depending on logging.h or its
    dependencies.
    
    For example, code that uses DISALLOW_COPY_AND_ASSIGN should
    include base/macros.h, but it currently happens to work if
    it pulls in logging.h directly or through some dependency.
    
    This is part of refactoring the codebase to use
    check.h/check_op.h for the CHECK and CHECK_op macro
    instead of logging.h.
    
    Bug: 1031540
    Change-Id: If0ae65768b8b03077e06c70d37a221b7a4392971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157085
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760538}

--- google_apis/gaia/oauth2_id_token_decoder.cc
+++ google_apis/gaia/oauth2_id_token_decoder.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "google_apis/gaia/oauth2_id_token_decoder.h"
 
-#include <base/base64url.h>
-#include <base/json/json_reader.h>
-#include <base/strings/string_split.h>
-#include <base/values.h>
 #include <memory>
+
+#include "base/base64url.h"
+#include "base/json/json_reader.h"
+#include "base/logging.h"
+#include "base/strings/string_split.h"
+#include "base/values.h"
 
 namespace {
 
--- google_apis/gaia/oauth_multilogin_result.cc
+++ google_apis/gaia/oauth_multilogin_result.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/json/json_reader.h"
+#include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/optional.h"
 #include "base/strings/string_number_conversions.h"
