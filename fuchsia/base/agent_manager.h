591f06b89dd7a05a08d53478a19f9aa16c3eccb6
Hans Wennborg <hans@chromium.org>
2020-04-20 18:12:28 +0000

    logging.h related include-what-you-use (fuchsia/)
    Fix code that was implicitly depending on logging.h or its
    dependencies.
    
    For example, code that uses DISALLOW_COPY_AND_ASSIGN should
    include base/macros.h, but it currently happens to work if
    it pulls in logging.h directly or through some dependency.
    
    This is part of refactoring the codebase to use
    check.h/check_op.h for the CHECK and CHECK_op macro
    instead of logging.h.
    
    Bug: 1031540
    Change-Id: I939f1d9d0db3e858c539a436021f47dc619222b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157065
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760572}

--- fuchsia/base/agent_manager.h
+++ fuchsia/base/agent_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/flat_map.h"
 #include "base/fuchsia/default_context.h"
+#include "base/macros.h"
 #include "base/strings/string_piece.h"
 
 namespace cr_fuchsia {
--- fuchsia/base/init_logging.cc
+++ fuchsia/base/init_logging.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "fuchsia/base/init_logging.h"
 
 #include "base/command_line.h"
+#include "base/logging.h"
 
 namespace cr_fuchsia {
 namespace {
