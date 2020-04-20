94cacf192729c20a48a9e3be7b06b5506ae15af0
gogerald <gogerald@google.com>
2020-04-20 18:02:30 +0000

    Fix typo in LayoutManagerChrome
    Change-Id: I8b922761cf9bb18af70e09dfa78b3711fcb067ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157245
    Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760554}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
@@ -194,7 +194,7 @@ public class LayoutManagerChrome
             mOverviewLayout.destroy();
             mOverviewLayout = null;
         }
-        if (mOverviewLayout != null) {
+        if (mOverviewListLayout != null) {
             mOverviewListLayout.destroy();
         }
         if (mToolbarSwipeLayout != null) {
