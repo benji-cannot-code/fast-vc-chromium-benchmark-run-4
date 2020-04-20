7e673b0a6fcc55eac520be10032a2967c1fdbad9
Jian Li <jianli@chromium.org>
2020-04-20 20:20:01 +0000

    Fix indexing in FeedListContentManager
    We used to reserve index 0 for native view. This is not longer
    true. So I fixed the indexing here.
    
    Bug: 1044139
    Change-Id: I5ffd3ea5323f14cf959b051f561fbffd769d42de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157656
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760661}

--- chrome/android/junit/src/org/chromium/chrome/browser/feed/v2/FeedListContentManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/feed/v2/FeedListContentManagerTest.java
@@ -5,16 +5,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.feed.v2;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.test.filters.SmallTest;
+import android.view.View;
 
 import com.google.common.collect.ImmutableList;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
@@ -27,6 +33,7 @@ import java.util.List;
 @Config(manifest = Config.NONE)
 public class FeedListContentManagerTest implements ListContentManagerObserver {
     private FeedListContentManager mManager;
+    private Context mContext;
 
     private boolean mItemRangeInserted;
     private int mItemRangeInsertedStartIndex;
@@ -43,6 +50,7 @@ public class FeedListContentManagerTest implements ListContentManagerObserver {
 
     @Before
     public void setUp() {
+        mContext = Robolectric.buildActivity(Activity.class).get();
         mManager = new FeedListContentManager(null, null);
         mManager.addObserver(this);
     }
@@ -162,6 +170,34 @@ public class FeedListContentManagerTest implements ListContentManagerObserver {
         assertEquals(c1, mManager.getContent(4));
     }
 
+    @Test
+    @SmallTest
+    public void testGetViewData() {
+        FeedListContentManager.FeedContent c1 = createExternalViewContent("foo");
+        View v2 = new View(mContext);
+        FeedListContentManager.FeedContent c2 = createNativeViewContent(v2);
+        View v3 = new View(mContext);
+        FeedListContentManager.FeedContent c3 = createNativeViewContent(v3);
+        FeedListContentManager.FeedContent c4 = createExternalViewContent("hello");
+        View v5 = new View(mContext);
+        FeedListContentManager.FeedContent c5 = createNativeViewContent(v5);
+
+        addContents(0, ImmutableList.of(c1, c2, c3, c4, c5));
+        assertEquals(5, mManager.getItemCount());
+
+        assertFalse(mManager.isNativeView(0));
+        assertTrue(mManager.isNativeView(1));
+        assertTrue(mManager.isNativeView(2));
+        assertFalse(mManager.isNativeView(3));
+        assertTrue(mManager.isNativeView(4));
+
+        assertArrayEquals("foo".getBytes(), mManager.getExternalViewBytes(0));
+        assertEquals(v2, mManager.getNativeView(1, null));
+        assertEquals(v3, mManager.getNativeView(2, null));
+        assertArrayEquals("hello".getBytes(), mManager.getExternalViewBytes(3));
+        assertEquals(v5, mManager.getNativeView(4, null));
+    }
+
     @Override
     public void onItemRangeInserted(int startIndex, int count) {
         mItemRangeInserted = true;
@@ -232,5 +268,9 @@ public class FeedListContentManagerTest implements ListContentManagerObserver {
 
     private FeedListContentManager.FeedContent createExternalViewContent(String s) {
         return new FeedListContentManager.ExternalViewContent(s, s.getBytes());
+    }
+
+    private FeedListContentManager.FeedContent createNativeViewContent(View v) {
+        return new FeedListContentManager.NativeViewContent(v.toString(), v);
     }
 }
