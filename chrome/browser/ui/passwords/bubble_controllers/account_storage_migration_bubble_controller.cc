41b4a16dc228bb86446f6a12ed77e36d34245469
Friedrich Horschig <fhorschig@chromium.org>
2020-04-21 16:24:34 +0000

    [Passwords] Add controller for bubble migrating credentials to account
    This CL adds the controller scaffold of the bubble that proposes to
    users to move their local credential to the account-storage.
    
    See the linked bug for mocks.
    
    Bug: 1060128
    Change-Id: Idb53ef749d0ad18ee4dcb5af5f98cfedecfc7f8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159220
    Commit-Queue: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760992}

---
+++ chrome/browser/ui/passwords/bubble_controllers/account_storage_migration_bubble_controller.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/passwords/bubble_controllers/account_storage_migration_bubble_controller.h"
+
+AccountStorageMigrationBubbleController::
+    AccountStorageMigrationBubbleController(
+        base::WeakPtr<PasswordsModelDelegate> delegate)
+    : PasswordBubbleControllerBase(std::move(delegate),
+                                   password_manager::metrics_util::
+                                       AUTOMATIC_ACCOUNT_MIGRATION_PROPOSAL) {}
+
+AccountStorageMigrationBubbleController::
+    ~AccountStorageMigrationBubbleController() {
+  // Make sure the interactions are reported even if Views didn't notify the
+  // controller about the bubble being closed.
+  if (!interaction_reported_)
+    OnBubbleClosing();
+}
+
+base::string16 AccountStorageMigrationBubbleController::GetTitle() const {
+  return base::string16();
+}
+
+void AccountStorageMigrationBubbleController::ReportInteractions() {}
