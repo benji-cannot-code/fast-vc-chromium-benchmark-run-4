30cdb7d06883de0c161c380daba6367888e32dac
Anders Hartvoll Ruud <andruud@chromium.org>
2020-04-21 19:15:36 +0000

    [@property] Determine the registration
    The current behavior for dealing with multiple registrations on the
    same custom property was highly temporary, and not based on any spec.
    The spec has since been updated to describe how it should work:
    in short, registrations originating from CSS.registerProperty are
    stored independently from those originating from @property, and
    CSS.registerProperty-registrations (if present) win over @property-
    registrations.
    
    Changes in this CL:
    
     - Separates storage for CSS.registerProperty-registrations
       (registered_properties_) and @property-registrations
       (declared_properties_). The old code put @property registrations
       in the same map as CSS.registerProperty-registrations, and only
       if no registration exited already. (Meaning for example that the
       first @property seen would win---opposite of how it's spec'd to
       work now).
     - Determines the registration [1] correctly. I.e. first checks the
       the CSS.registerProperty-registrations, and the @property ones.
     - Adds an iterator which yields the currently active registrations
       across both registration types. (I wanted to do this separately,
       but initial values won't work properly without it, and there's a
       test that relies heavily on that being functional).
    
    Note that it's not explicitly stated in the spec that the storage for
    the two registration types shall be separate, but that _is_ the
    background for the current spec text on the subject. The rationale is
    that it makes it easier to reason about which registration is the
    current one when @property rules can appear and disappear via JS.
    It's also expected that CSS.unregisterProperty will be added in the
    future, making the situation even more unstable.
    
    [1] https://drafts.css-houdini.org/css-properties-values-api-1/#determining-registration
    
    Bug: 973830
    Change-Id: I69b48629034b333ebed5d374f943d08b39579334
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157071
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761075}

--- third_party/blink/renderer/core/css/css_test_helpers.cc
+++ third_party/blink/renderer/core/css/css_test_helpers.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_custom_ident_value.h"
 #include "third_party/blink/renderer/core/css/css_rule_list.h"
 #include "third_party/blink/renderer/core/css/css_style_sheet.h"
+#include "third_party/blink/renderer/core/css/css_syntax_string_parser.h"
 #include "third_party/blink/renderer/core/css/css_variable_data.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_context.h"
 #include "third_party/blink/renderer/core/css/parser/css_parser_local_context.h"
@@ -61,6 +62,14 @@ void TestStyleSheet::AddCSSRules(const String& css_text, bool is_empty_sheet) {
     ASSERT_GT(style_sheet_->length(), sheet_length);
   else
     ASSERT_EQ(style_sheet_->length(), sheet_length);
+}
+
+PropertyRegistration* CreatePropertyRegistration(const String& name) {
+  auto syntax_definition = CSSSyntaxStringParser("*").Parse();
+  DCHECK(syntax_definition);
+  return MakeGarbageCollected<PropertyRegistration>(
+      AtomicString(name), *syntax_definition, false /* inherits */,
+      nullptr /* initial */, nullptr /* initial_variable_data */);
 }
 
 void RegisterProperty(Document& document,
