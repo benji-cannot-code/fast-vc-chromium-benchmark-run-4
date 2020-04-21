24fb11c4af83ac9acf1dc3362d761d579995a2e0
Joey Scarr <jsca@google.com>
2020-04-21 02:00:02 +0000

    Convert Download::Controller::Initialize to take a OnceClosure.
    Bug: 1007685
    Change-Id: I2d3a9b9624bd0fc33aeffa3458719d2ee30df820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2105665
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Joey Scarr <jsca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760787}

--- components/download/internal/background_service/controller_impl.h
+++ components/download/internal/background_service/controller_impl.h
@@ -71,7 +71,7 @@ class ControllerImpl : public Controller,
   ~ControllerImpl() override;
 
   // Controller implementation.
-  void Initialize(const base::Closure& callback) override;
+  void Initialize(base::OnceClosure callback) override;
   State GetState() override;
   void StartDownload(const DownloadParams& params) override;
   void PauseDownload(const std::string& guid) override;
@@ -266,7 +266,7 @@ class ControllerImpl : public Controller,
   std::unique_ptr<FileMonitor> file_monitor_;
 
   // Internal state.
-  base::Closure init_callback_;
+  base::OnceClosure init_callback_;
   State controller_state_;
   StartupStatus startup_status_;
   std::set<std::string> externally_active_downloads_;
