4673e0df90164eb82c17da4f221bae16e4ba940b
Elly Fong-Jones <ellyjones@chromium.org>
2020-04-21 15:40:28 +0000

    sheriff: disable CorbAndCorsExtensionBrowserTest test
    Bug: 1073052
    Change-Id: If3b28d0c2370b8d34aeaa5ff7ad9abb48a231318
    Tbr: karandeepb@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159308
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760966}

--- chrome/browser/extensions/corb_and_cors_extension_browsertest.cc
+++ chrome/browser/extensions/corb_and_cors_extension_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/values.h"
+#include "build/build_config.h"
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -1602,8 +1603,17 @@ IN_PROC_BROWSER_TEST_P(CorbAndCorsExtensionBrowserTest,
   }
 }
 
+#if defined(OS_LINUX)
+// Flaky on Linux, especially under sanitizers: https://crbug.com/1073052
+#define MAYBE_FromBackgroundServiceWorker_NoSniffXml \
+  DISABLED_FromBackgroundServiceWorker_NoSniffXml
+#else
+#define MAYBE_FromBackgroundServiceWorker_NoSniffXml \
+  FromBackgroundServiceWorker_NoSniffXml
+#endif
+
 IN_PROC_BROWSER_TEST_P(CorbAndCorsExtensionBrowserTest,
-                       FromBackgroundServiceWorker_NoSniffXml) {
+                       MAYBE_FromBackgroundServiceWorker_NoSniffXml) {
   // Install the extension with a service worker that can be asked to start a
   // fetch to an arbitrary URL.
   const char kManifestTemplate[] = R"(
