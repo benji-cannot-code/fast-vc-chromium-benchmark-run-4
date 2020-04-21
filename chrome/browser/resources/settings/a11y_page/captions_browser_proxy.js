2e1b0d9a2637a2c2e0bc5a76f387665af82913a9
rbpotter <rbpotter@chromium.org>
2020-04-21 01:44:08 +0000

    Settings: Check in a11y_page/ Polymer 3
    Do not check in the Polymer 3 captions_subpage as this is still used
    by OS settings. Note that captions_browser_proxy is used by a11y_page,
    not captions_subpage, and is Win/Mac specific, so can be checked in.
    
    Bug: 1069258
    Change-Id: I5fb0184d84a1f476552b8f2b09acd96c8044a15e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2155528
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760761}

--- chrome/browser/resources/settings/a11y_page/captions_browser_proxy.js
+++ chrome/browser/resources/settings/a11y_page/captions_browser_proxy.js
@@ -9,12 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // clang-format off
-// #import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
+import {addSingletonGetter} from 'chrome://resources/js/cr.m.js';
 // clang-format on
 
-cr.define('settings', function() {
   /** @interface */
-  /* #export */ class CaptionsBrowserProxy {
+  export class CaptionsBrowserProxy {
     /**
      * Open the native captions system dialog.
      */
@@ -22,20 +21,14 @@ cr.define('settings', function() {
   }
 
   /**
-   * @implements {settings.CaptionsBrowserProxy}
+   * @implements {CaptionsBrowserProxy}
    */
-  /* #export */ class CaptionsBrowserProxyImpl {
+  export class CaptionsBrowserProxyImpl {
     /** @override */
     openSystemCaptionsDialog() {
       chrome.send('openSystemCaptionsDialog');
     }
   }
 
-  cr.addSingletonGetter(CaptionsBrowserProxyImpl);
+  addSingletonGetter(CaptionsBrowserProxyImpl);
 
-  // #cr_define_end
-  return {
-    CaptionsBrowserProxy: CaptionsBrowserProxy,
-    CaptionsBrowserProxyImpl: CaptionsBrowserProxyImpl,
-  };
-});
