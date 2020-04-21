58b84e34d5c990d842e86e3b6b5db097fb7fed60
Hans Wennborg <hans@chromium.org>
2020-04-21 01:49:30 +0000

    logging.h related include-what-you-use (extensions/)
    Fix code that was implicitly depending on logging.h or its
    dependencies.
    
    For example, code that uses DISALLOW_COPY_AND_ASSIGN should
    include base/macros.h, but it currently happens to work if
    it pulls in logging.h directly or through some dependency.
    
    This is part of refactoring the codebase to use
    check.h/check_op.h for the CHECK and CHECK_op macro
    instead of logging.h.
    
    Bug: 1031540
    Change-Id: Iba6f3cdea3a6045e93abb513adddcffb1120e6c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157006
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760770}

--- extensions/browser/verified_contents.cc
+++ extensions/browser/verified_contents.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64url.h"
 #include "base/files/file_util.h"
 #include "base/json/json_reader.h"
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_util.h"
 #include "base/timer/elapsed_timer.h"
--- extensions/renderer/extension_interaction_provider.h
+++ extensions/renderer/extension_interaction_provider.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/renderer/bindings/interaction_provider.h"
 
+#include "base/macros.h"
 #include "base/optional.h"
 #include "v8/include/v8.h"
 
