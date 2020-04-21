63273e835601812c5fb686fd63c8a9e8972c05c2
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-04-21 05:32:49 +0000

    Import wpt@643c701b85063133e0f5c9f5168c7ebfd9fccb0c
    Using wpt-import in Chromium eee38de014c10453c60417a7fab48aaf51763e4f.
    With Chromium commits locally applied on WPT:
    d0ad7cf8c4 "UpgradeInsecureRequest: Add WPTs for static-import in dedicated/shared workers"
    86131ac4be "Add a test for ReplaceTrack that verifies video track content."
    6c8523d285 "[scroll-timeline] Implement element-based scroll offset"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    dtapuska@chromium.org:
      external/wpt/uievents
    fmeawad@chromium.org, panicker@chromium.org:
      external/wpt/device-memory
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    igrigorik@chromium.org, panicker@chromium.org:
      external/wpt/performance-timeline
    jsbell@chromium.org:
      external/wpt/IndexedDB
    ksakamoto@chromium.org, npm@chromium.org:
      external/wpt/resource-timing
    mek@chromium.org, stevebe@microsoft.com:
      external/wpt/native-file-system
    mkwst@chromium.org, andypaicu@chromium.org:
      external/wpt/content-security-policy
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    pwnall@chromium.org, jsbell@chromium.org:
      external/wpt/cookie-store
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Ib4ecdc89972884d1f8fc534505a97ed8818c7ee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2158157
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#760824}

--- third_party/blink/web_tests/external/wpt/html/webappapis/system-state-and-capabilities/the-navigator-object/historical.https.window.js
+++ third_party/blink/web_tests/external/wpt/html/webappapis/system-state-and-capabilities/the-navigator-object/historical.https.window.js
@@ -9,3 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     assert_false(method in self.navigator);
   }, method + "() is removed");
 });
+
+test(() => {
+  let called = false;
+  self.navigator.registerProtocolHandler("web+test", "%s", { toString: () => called = true });
+  assert_false(called);
+}, "registerProtocolHandler has no third argument");
--- third_party/blink/web_tests/external/wpt/pointerevents/idlharness.window.js
+++ third_party/blink/web_tests/external/wpt/pointerevents/idlharness.window.js
@@ -8,9 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 idl_test(
   ['pointerevents'],
-  ['uievents', 'dom', 'html'],
+  ['uievents', 'html', 'dom'],
   idl_array => {
     idl_array.add_objects({
+      Document: ['document'],
       Element: ['document'],
       Window: ['window'],
       Navigator: ['navigator'],
