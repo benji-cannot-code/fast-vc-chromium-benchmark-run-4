e8da2d4b1ecf9ca1be8652d12e12cfa0e588494e
Nohemi Fernandez <fernandex@chromium.org>
2020-04-21 11:08:31 +0000

    [iOS] Fix crash due to multiple completion attempts on interrupt.
    On interrupt action for adding an account during the user sign-in flow
    we currently perform sign-in completion (1) asynchronously in the add
    account mediator during clean-up and (2) in the interrupt action method.
    
    Ensure that we only process the sign-in completion once and that the
    interrupt completion is called afterwards.
    
    Bug: 1005509
    Change-Id: Id96157b68e9173be07758949f1a36914f0e95906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154948
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760891}

--- ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
@@ -43,6 +43,8 @@ using signin_metrics::PromoAction;
 @property(nonatomic, assign) PromoAction promoAction;
 // Add account sign-in intent.
 @property(nonatomic, assign, readonly) AddAccountSigninIntent signinIntent;
+// Called when the sign-in dialog is interrupted.
+@property(nonatomic, copy) ProceduralBlock interruptCompletion;
 
 @end
 
@@ -81,6 +83,12 @@ using signin_metrics::PromoAction;
   }
 
   DCHECK(self.identityInteractionManager);
+  // IdentityInteractionManager |cancelAndDismissAnimated| will trigger the call
+  // to add account completion in the AddAccountMediator, however we must also
+  // ensure that the interrupt completion is called on sign-in completion.
+  // TODO(crbug.com/1072347): Update IdentityInteractionManager dismiss API.
+  self.interruptCompletion = completion;
+  self.mediator.signinInterrupted = YES;
   switch (action) {
     case SigninCoordinatorInterruptActionNoDismiss:
       // SSO doesn't support cancel without dismiss, so to make sure the cancel
@@ -95,14 +103,6 @@ using signin_metrics::PromoAction;
       // dismissed, we need to dismiss without animation.
       [self.identityInteractionManager cancelAndDismissAnimated:NO];
       break;
-  }
-  // The manager UI has been dismissed, it can be set to nil.
-  self.identityInteractionManager = nil;
-  [self runCompletionCallbackWithSigninResult:SigninCoordinatorResultInterrupted
-                                     identity:nil
-                   showAdvancedSettingsSignin:NO];
-  if (completion) {
-    completion();
   }
 }
 
@@ -176,7 +176,12 @@ using signin_metrics::PromoAction;
             (SigninCoordinatorResult)signinResult
                                                 identity:
                                                     (ChromeIdentity*)identity {
-  self.identityInteractionManager = nil;
+  if (!self.identityInteractionManager) {
+    // The IdentityInteractionManager callback might be called after the
+    // interrupt method. If this is the case, the AddAccountSigninCoordinator
+    // is already stopped. This call can be ignored.
+    return;
+  }
   switch (self.signinIntent) {
     case AddAccountSigninIntentReauthPrimaryAccount: {
       [self presentUserConsentWithIdentity:identity];
@@ -196,9 +201,13 @@ using signin_metrics::PromoAction;
                               identity:(ChromeIdentity*)identity {
   DCHECK(!self.alertCoordinator);
   DCHECK(!self.userSigninCoordinator);
+  self.identityInteractionManager = nil;
   [self runCompletionCallbackWithSigninResult:signinResult
                                      identity:identity
                    showAdvancedSettingsSignin:NO];
+  if (self.interruptCompletion) {
+    self.interruptCompletion();
+  }
 }
 
 // Presents the user consent screen with |identity| pre-selected.
--- ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_mediator.mm
+++ ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_mediator.mm
@@ -62,24 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark - Private
 
-// Returns the sign-in result status of the user.
-// |identity| is the identity of the added account.
-// |error| is an error reported by the SSOAuth following adding an account.
-- (SigninCoordinatorResult)signinStateWithIdentity:(ChromeIdentity*)identity
-                                             error:(NSError*)error {
-  DCHECK(self.identityInteractionManager);
-  if (error) {
-    DCHECK(!identity);
-    // Filter out errors handled internally by ChromeIdentity.
-    if (ShouldHandleSigninError(error)) {
-      return SigninCoordinatorResultInterrupted;
-    }
-    return SigninCoordinatorResultCanceledByUser;
-  }
-  DCHECK(identity);
-  return SigninCoordinatorResultSuccess;
-}
-
 // Handles the reauth operation for a user. User account will be
 // automatically populated using the primary user account. In the case of a
 // sign-in error will display a modal alert and abort adding an account.
@@ -116,21 +98,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // if the flow is interrupted by a sign-in error.
 - (void)operationCompletedWithIdentity:(ChromeIdentity*)identity
                                  error:(NSError*)error {
-  SigninCoordinatorResult signinResult = [self signinStateWithIdentity:identity
-                                                                 error:error];
-  switch (signinResult) {
-    case SigninCoordinatorResultSuccess:
-    case SigninCoordinatorResultCanceledByUser: {
-      [self.delegate
-          addAccountSigninMediatorFinishedWithSigninResult:signinResult
-                                                  identity:identity];
-      break;
-    }
-    case SigninCoordinatorResultInterrupted: {
+  SigninCoordinatorResult signinResult;
+  if (error) {
+    DCHECK(!identity);
+    // Filter out errors handled internally by ChromeIdentity.
+    if (ShouldHandleSigninError(error)) {
       [self.delegate addAccountSigninMediatorFailedWithError:error];
-      break;
+      return;
     }
+    signinResult = SigninCoordinatorResultCanceledByUser;
+  } else {
+    DCHECK(identity);
+    signinResult = self.signinInterrupted ? SigninCoordinatorResultInterrupted
+                                          : SigninCoordinatorResultSuccess;
   }
+
+  [self.delegate addAccountSigninMediatorFinishedWithSigninResult:signinResult
+                                                         identity:identity];
 }
 
 // Handles the add account operation for a user. Presents the screen to enter
