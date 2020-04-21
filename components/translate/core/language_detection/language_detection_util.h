9437083ab1967be9a6b17279a35f7d6c337f5fac
Doug Arnett <dougarnett@chromium.org>
2020-04-21 15:21:20 +0000

    Creates a utility service for text language detection
    This is part of supporting translation for sub frames.
    Specifically this moves the analysis of the untrusted contents
    obtained from an accessiblity tree snapshot out of the browser
    process and into a sandboxed utility process.
    
    Bug: 1063520
    Change-Id: I0792d209541558a753f168f78f421e66745748d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2140605
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760958}

--- components/translate/core/language_detection/language_detection_util.h
+++ components/translate/core/language_detection/language_detection_util.h
@@ -12,6 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace translate {
 
+// Returns the ISO 639 language code of the specified |text|, or
+// |translate::kUnknownLanguageCode| if it failed. |is_cld_reliable| will be
+// set as true if CLD says the detection is reliable.
+std::string DetermineTextLanguage(const base::string16& text,
+                                  bool* is_cld_reliable);
+
 // Determines content page language from Content-Language code and contents.
 std::string DeterminePageLanguage(const std::string& code,
                                   const std::string& html_lang,
