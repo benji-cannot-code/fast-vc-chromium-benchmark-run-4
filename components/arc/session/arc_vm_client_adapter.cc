2b1b47327c126330ad06385567f9ba77de1dd138
Edman Anjos <edmanp@google.com>
2020-04-21 20:23:56 +0000

    arcvm: Start arc-keymasterd job during arcvm startup
    arc-keymasterd should run while ARCVM runs in order to serve the
    Keymaster HAL service.
    
    This change starts arc-keymasterd in the per board feature flow,
    ensuring the service is stopped and restarted if it's already running.
    
    The change https://crrev.com/c/2153037 enables the D-Bus policies needed
    and must land before this one.
    
          && autoninja -C out/Default components_unittests
          && out/Default/components_unittests --gtest_filter='ArcVm*'
                 - verify `status arc-keymasterd` says it is running
    
    Bug: b:153278258
    Test: gn gen out/Default --args='target_os = "chromeos"' \
    Test: manual - deploy chrome to an eve-arc-r device and sign in
    Change-Id: Ib79c0e407c3724cc9511162103b1196b09a66fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153038
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761105}

--- components/arc/session/arc_vm_client_adapter.cc
+++ components/arc/session/arc_vm_client_adapter.cc
@@ -48,7 +48,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace arc {
 namespace {
 
+// The "_2d" in job names below corresponds to "-". Upstart escapes characters
+// that aren't valid in D-Bus object paths with underscore followed by its
+// ascii code in hex. So "arc_2dcreate_2ddata" becomes "arc-create-data".
 constexpr const char kArcCreateDataJobName[] = "arc_2dcreate_2ddata";
+constexpr const char kArcKeymasterJobName[] = "arc_2dkeymasterd";
 constexpr const char kArcVmServerProxyJobName[] = "arcvm_2dserver_2dproxy";
 constexpr const char kArcVmPerBoardFeaturesJobName[] =
     "arcvm_2dper_2dboard_2dfeatures";
@@ -460,10 +464,33 @@ class ArcVmClientAdapter : public ArcClientAdapter,
             << (result ? "stopped" : "not running?");
 
     should_notify_observers_ = true;
-    // Always run the |callback| with true ignoring the |result|. |result| can
-    // be false when the proxy job has already been stopped for other reasons,
-    // but it's not considered as an error.
-    std::move(callback).Run(true);
+
+    // Make sure to stop arc-keymasterd if it's already started. Always move
+    // |callback| as is and ignore |result|.
+    chromeos::UpstartClient::Get()->StopJob(
+        kArcKeymasterJobName, /*environment=*/{},
+        base::BindOnce(&ArcVmClientAdapter::OnArcKeymasterJobStopped,
+                       weak_factory_.GetWeakPtr(), std::move(callback)));
+  }
+
+  void OnArcKeymasterJobStopped(chromeos::VoidDBusMethodCallback callback,
+                                bool result) {
+    VLOG(1) << "OnArcKeymasterJobStopped: arc-keymasterd job "
+            << (result ? "stopped" : "not running?");
+
+    // Start arc-keymasterd. Always move |callback| as is and ignore |result|.
+    VLOG(1) << "Starting arc-keymasterd";
+    chromeos::UpstartClient::Get()->StartJob(
+        kArcKeymasterJobName, /*environment=*/{},
+        base::BindOnce(&ArcVmClientAdapter::OnArcKeymasterJobStarted,
+                       weak_factory_.GetWeakPtr(), std::move(callback)));
+  }
+
+  void OnArcKeymasterJobStarted(chromeos::VoidDBusMethodCallback callback,
+                                bool result) {
+    if (!result)
+      LOG(ERROR) << "Failed to start arc-keymasterd job";
+    std::move(callback).Run(result);
   }
 
   void OnConciergeStarted(UpgradeParams params,
