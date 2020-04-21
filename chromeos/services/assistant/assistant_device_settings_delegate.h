0dc085734e580b66c3119572b1d03b7c669a8ad9
Jeroen Dhollander <jeroendh@google.com>
2020-04-21 20:41:24 +0000

    Remove dead code
    This code was meant to handle volume related queries in ChromeOS,
    but these queries are actually handled inside LibAssistant.
    
    The code I removed expects to handles of the volume through a
    |device.MODIFY_SETTING| request.
    
    Instead, LibAssistant gets a |device.UPDATE_VOLUME| request which it
    handles here:
        https://cs.corp.google.com/libassistant/libassistant/internal/assistant/action/device/device_control_module.cc?type=cs&q=libassistant/internal/assistant/action/device/device_control_module.cc+package:%5Elibassistant$&g=0&l=339
    
    After discussing this with the Assistant Device Actions team,
    I learned that the server can either send device.MODIFY_SETTING or
    device.UPDATE_VOLUME, based on the client preference.
    Given that LibAssistant expresses a preference for device.UPDATE_VOLUME,
    we can safely assume we will never receive a device.MODIFY_SETTING
    request.
    
    Test: queries "increase volume", "set volume to 20%", "increase volume by 5".
    Bug: b/154040671
    Change-Id: I763f4d1d1ace0924f81faf7f33eb7cc364e99d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157585
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761116}

--- chromeos/services/assistant/assistant_device_settings_delegate.h
+++ chromeos/services/assistant/assistant_device_settings_delegate.h
@@ -13,10 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/mojom/assistant_controller.mojom-forward.h"
 #include "chromeos/services/assistant/public/mojom/assistant.mojom-forward.h"
 
-namespace assistant_client {
-class VolumeControl;
-}  // namespace assistant_client
-
 namespace assistant {
 namespace api {
 namespace client_op {
@@ -37,9 +33,7 @@ class Setting;
 // the device settings, like Bluetooth or WiFi.
 class AssistantDeviceSettingsDelegate {
  public:
-  explicit AssistantDeviceSettingsDelegate(
-      ServiceContext* context,
-      assistant_client::VolumeControl* volume_control);
+  explicit AssistantDeviceSettingsDelegate(ServiceContext* context);
   AssistantDeviceSettingsDelegate(AssistantDeviceSettingsDelegate&) = delete;
   AssistantDeviceSettingsDelegate& operator=(AssistantDeviceSettingsDelegate&) =
       delete;
