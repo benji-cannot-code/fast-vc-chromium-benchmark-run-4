30fed1df257d70e05ff8c2cb8e38040e5e960fd5
Hans Wennborg <hans@chromium.org>
2020-04-21 18:40:55 +0000

    logging.h related include-what-you-use (cc/)
    Fix code that was implicitly depending on logging.h or its
    dependencies.
    
    For example, code that uses DISALLOW_COPY_AND_ASSIGN should
    include base/macros.h, but it currently happens to work if
    it pulls in logging.h directly or through some dependency.
    
    This is part of refactoring the codebase to use
    check.h/check_op.h for the CHECK and CHECK_op macro
    instead of logging.h.
    
    Bug: 1031540
    Change-Id: Ia68258fa0d399fc9445d09a9ee5e510fd1265923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156985
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761061}

--- cc/input/scroll_snap_data.cc
+++ cc/input/scroll_snap_data.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "cc/input/scroll_snap_data.h"
+
+#include "base/logging.h"
 #include "base/numerics/ranges.h"
 #include "cc/input/snap_selection_strategy.h"
 
