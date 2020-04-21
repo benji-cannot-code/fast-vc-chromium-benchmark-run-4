964ccfcdee96f7db0f0131cc27522911a43f2c82
Istiaque Ahmed <lazyboy@chromium.org>
2020-04-21 10:37:41 +0000

    [Extensions] Change BrowsingDataRemoverFunction to ExtensionFunction.
    ChromeAsyncExtensionFunction is deprecated and will be removed soon.
    This CL makes BrowsingDataRemoverFunction extend ExtensionFunction,
    which is the preferred way.
    
    Bug=634140
    
    Change-Id: I7b575f53559c386bf4af9fd4cc038803e3498ea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2155492
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760885}

--- chrome/browser/extensions/api/browsing_data/browsing_data_api.h
+++ chrome/browser/extensions/api/browsing_data/browsing_data_api.h
@@ -98,7 +98,7 @@ class BrowsingDataSettingsFunction : public ExtensionFunction {
 // Each child class must implement GetRemovalMask(), which returns the bitmask
 // of data types to remove.
 class BrowsingDataRemoverFunction
-    : public ChromeAsyncExtensionFunction,
+    : public ExtensionFunction,
       public content::BrowsingDataRemover::Observer {
  public:
   BrowsingDataRemoverFunction();
@@ -107,7 +107,7 @@ class BrowsingDataRemoverFunction
   void OnBrowsingDataRemoverDone() override;
 
   // ExtensionFunction:
-  bool RunAsync() override;
+  ResponseAction Run() override;
 
  protected:
   ~BrowsingDataRemoverFunction() override;
@@ -138,10 +138,12 @@ class BrowsingDataRemoverFunction
   bool ParseOriginTypeMask(const base::DictionaryValue& options,
                            int* origin_type_mask);
 
-  // Parse the developer-provided list of origins into |result|.
-  // Returns true if parsing was successful.
+  // Parses the developer-provided list of origins into |result|.
+  // Returns whether or not parsing was successful. In case of parse failure,
+  // |error_response| will contain the error response.
   bool ParseOrigins(const base::Value& list_value,
-                    std::vector<url::Origin>* result);
+                    std::vector<url::Origin>* result,
+                    ResponseValue* error_response);
 
   // Called when we're ready to start removing data.
   void StartRemoving();
