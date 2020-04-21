78eac94c84088a94ea3e0d75f4779d3b517a19cb
Olivier Robin <olivierrobin@google.com>
2020-04-21 16:26:08 +0000

    Enable MetricKit payload collection
    Add a flag to save MetricKit payloads.
    Saving the payload will require
    - a compilation flag that will be enabled only on canary.
    - setting an experimental setting
    
    JSON representation of MetricKit payload will be saved in Documents
    directory.
    
    Collecting this data will help developing MetricKit
    support in Chrome.
    
    Bug: 1017434
    Change-Id: I5db5d7112859eb873ee68e8f7159f7ab9fe3dbfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156905
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760994}

---
+++ ios/chrome/app/application_delegate/metric_kit_subscriber.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_APPLICATION_DELEGATE_METRIC_KIT_SUBSCRIBER_H_
+#define IOS_CHROME_APP_APPLICATION_DELEGATE_METRIC_KIT_SUBSCRIBER_H_
+
+#import <Foundation/Foundation.h>
+#import <MetricKit/MetricKit.h>
+
+extern NSString* const kChromeMetricKitPayloadsDirectory;
+
+// A subscriber that save MetricKit reports to the application document
+// directory.
+@interface MetricKitSubscriber : NSObject <MXMetricManagerSubscriber>
++ (instancetype)sharedInstance;
+@end
+
+#endif  // IOS_CHROME_APP_APPLICATION_DELEGATE_METRIC_KIT_SUBSCRIBER_H_
