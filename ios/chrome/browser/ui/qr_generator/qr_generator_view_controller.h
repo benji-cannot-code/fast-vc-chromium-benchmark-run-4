13a1f046bc6a0a9f763dd4d6e74a149deebbfc63
Sebastien Lalancette <seblalancette@chromium.org>
2020-04-21 22:21:16 +0000

    Reuse ConfirmationAlertViewController for QRGeneratorViewController
    Also, allow usage of fixed size image to prevent scaling-up the QR code
    (it becomes too big to be scanned effectively).
    
    Sample:
    https://screenshot.googleplex.com/xYWmsrTzZpD.png
    
    Bug: 1064990
    Change-Id: Ibd1e7f5f1149e436929ca8c33b1dae7807172411
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153391
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761167}

--- ios/chrome/browser/ui/qr_generator/qr_generator_view_controller.h
+++ ios/chrome/browser/ui/qr_generator/qr_generator_view_controller.h
@@ -7,14 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_UI_QR_GENERATOR_QR_GENERATOR_VIEW_CONTROLLER_H_
 
 #import "ios/chrome/browser/ui/commands/qr_generation_commands.h"
+#import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
 
 #import <UIKit/UIKit.h>
 
 // View controller that displays a QR code representing a given website.
-@interface QRGeneratorViewController : UIViewController
-
-// Command handler for communicating with other components of the app.
-@property(nonatomic, weak) id<QRGenerationCommands> handler;
+@interface QRGeneratorViewController : ConfirmationAlertViewController
 
 // URL of the page to generate a QR code for.
 @property(nonatomic, copy) NSURL* pageURL;
