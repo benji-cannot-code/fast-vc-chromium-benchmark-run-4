1a9614bea243cad2a75ca2cb2663bd4a0ba8bfc9
Nicolás Peña Moreno <npm@chromium.org>
2020-04-21 14:22:06 +0000

    Convert some error callbacks to OnceCallback in CastSessionDelegate
    Bug: 1007641
    Change-Id: I03b1387454ce22d11b43730aed7905d1d8d51c67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2106335
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760940}

--- media/cast/cast_sender_impl.h
+++ media/cast/cast_sender_impl.h
@@ -28,7 +28,7 @@ class CastSenderImpl : public CastSender {
                  CastTransport* const transport_sender);
 
   void InitializeAudio(const FrameSenderConfig& audio_config,
-                       const StatusChangeCallback& status_change_cb) final;
+                       StatusChangeOnceCallback status_change_cb) final;
   void InitializeVideo(
       const FrameSenderConfig& video_config,
       const StatusChangeCallback& status_change_cb,
@@ -44,7 +44,7 @@ class CastSenderImpl : public CastSender {
 
  private:
   void ReceivedPacket(std::unique_ptr<Packet> packet);
-  void OnAudioStatusChange(const StatusChangeCallback& status_change_cb,
+  void OnAudioStatusChange(StatusChangeOnceCallback status_change_cb,
                            OperationalStatus status);
   void OnVideoStatusChange(const StatusChangeCallback& status_change_cb,
                            OperationalStatus status);
