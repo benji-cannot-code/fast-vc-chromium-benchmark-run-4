41c7b5b0af08044cc2a04c6b8299150de8b12b51
Fred Mello <fredmello@chromium.org>
2020-04-21 14:58:53 +0000

    Android: Make the DFM initialization call more explicit
    Moved the implicit initialization of DFMs from a logger call
    into ChromeApplication. This is a no-op, but should help in debugging
    future startup issues with DFMs.
    
    Bug: 1022654
    Change-Id: Ia3de76f033058d01468333fed10db108cfc8c316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156607
    Commit-Queue: Fred Mello <fredmello@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760951}

--- components/module_installer/android/java/src/org/chromium/components/module_installer/util/ModuleUtil.java
+++ components/module_installer/android/java/src/org/chromium/components/module_installer/util/ModuleUtil.java
@@ -25,18 +25,6 @@ public class ModuleUtil {
     }
 
     /**
-     * Records the start time in order to later report the install duration via UMA.
-     */
-    public static void recordModuleAvailability() {
-        if (!BundleUtils.isBundle()) return;
-
-        try (Timer timer = new Timer()) {
-            initApplication();
-            SplitAvailabilityLogger.logModuleAvailability();
-        }
-    }
-
-    /**
      * Updates the CrashKey report containing modules currently present.
      */
     public static void updateCrashKeys() {
@@ -56,6 +44,7 @@ public class ModuleUtil {
         try (Timer timer = new Timer()) {
             SplitCompatInitializer.initApplication();
             ActivityObserverUtil.registerDefaultObserver();
+            SplitAvailabilityLogger.logModuleAvailability();
         }
     }
 
