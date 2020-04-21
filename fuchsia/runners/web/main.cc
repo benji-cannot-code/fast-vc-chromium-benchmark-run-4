4c0293fe62f6593ae2978cd17db4f07551dbbe30
Wez <wez@chromium.org>
2020-04-21 22:39:08 +0000

    [fuchsia] Simplify implementation of the CastRunner.
    The implementation of the fuchsia.sys.Runner for Cast apps is refactored
    to be easier to maintain, in the following ways:
    
    1. Instead of deriving from WebContentRunner and overriding certain
       behaviours, the CastRunner now instantiates WebContentRunner instances
       internally, to use to manage CastComponents.
    
    2. CastRunner now implements fuchsia.sys.Runner itself, and manages the
       PendingCastComponents, prior to creating & handing off CastComponents
       to be managed by the WebContentRunner(s).
    
    3. The CreateContextParams for the main and isolated contexts are
       constructed using a common base, rather than copying & filtering the
       main context parameters, to create parameters for isolated contexts.
    
    Bug: 1071544
    Change-Id: Ic66ca0a9805e9b00d213c62845e5101ff9dea76e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153055
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761177}

--- fuchsia/runners/web/main.cc
+++ fuchsia/runners/web/main.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/fuchsia/default_context.h"
 #include "base/fuchsia/file_utils.h"
+#include "base/fuchsia/scoped_service_binding.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_executor.h"
@@ -59,8 +60,9 @@ int main(int argc, char** argv) {
       base::BindRepeating(&GetContextParams);
 
   WebContentRunner runner(std::move(get_context_params_callback));
-  runner.PublishRunnerService(
-      base::fuchsia::ComponentContextForCurrentProcess()->outgoing().get());
+  base::fuchsia::ScopedServiceBinding<fuchsia::sys::Runner> binding(
+      base::fuchsia::ComponentContextForCurrentProcess()->outgoing().get(),
+      &runner);
 
   base::fuchsia::ComponentContextForCurrentProcess()
       ->outgoing()
