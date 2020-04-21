b88472dbcc7d889650fe1a34b05cd3cec1512155
Viktor Semeniuk <vsemeniuk@google.com>
2020-04-21 11:15:48 +0000

    [Password Check] Result view: already changed?
    This change adds "Already changed this password" link to compromised
    credentials items when user clicks "Change password" button.
    
    Screenshot: https://screenshot.googleplex.com/qxmVEm1DyzN.png
    
    Bug: 1047726
    Change-Id: Icdb97d432df2a0c4ec6b2ca38f3f20a1e73feaca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135616
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#760894}

--- chrome/browser/resources/settings/autofill_page/password_check_list_item.js
+++ chrome/browser/resources/settings/autofill_page/password_check_list_item.js
@@ -29,6 +29,11 @@ Polymer({
       type: String,
       computed: 'computePassword_(item.password)',
     },
+
+    clickedChangePassword: {
+      type: Boolean,
+      value: false,
+    }
   },
 
   /**
@@ -64,6 +69,8 @@ Polymer({
    * @private
    */
   onChangePasswordClick_() {
+    this.fire('change-password-clicked', {id: this.item.id});
+
     const url = assert(this.item.changePasswordUrl);
     settings.OpenWindowProxyImpl.getInstance().openURL(url);
 
@@ -136,5 +143,10 @@ Polymer({
     if (this.isPasswordVisible_) {
       this.$$('#leakedPassword').select();
     }
+  },
+
+  /** @private */
+  onAlreadyChangedClick_(e) {
+    e.preventDefault();
   },
 });
