dd187d15401dec3442f6675dfe7e1244f89d5691
Ryan Sturm <ryansturm@chromium.org>
2020-04-21 22:22:16 +0000

    Remove old prerender histograms that are covered by UKM or not needed
    These histograms all aren't important or they have analogs in UKM, which
    provides better understanding of the metrics.
    
    Bug: 975424
    Change-Id: I1d6d9d18dbc08518581843707ed3e31ad04cff23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157903
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761168}

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -950,10 +950,6 @@ void PrerenderManager::PeriodicCleanup() {
   for (auto* contents : prerender_contents)
     contents->DestroyWhenUsingTooManyResources();
 
-  // Measure how long the resource checks took. http://crbug.com/305419.
-  UMA_HISTOGRAM_TIMES("Prerender.PeriodicCleanupResourceCheckTime",
-                      resource_timer.Elapsed());
-
   base::ElapsedTimer cleanup_timer;
 
   // Perform deferred cleanup work.
@@ -965,10 +961,6 @@ void PrerenderManager::PeriodicCleanup() {
   DeleteToDeletePrerenders();
 
   CleanUpOldNavigations(&prefetches_, base::TimeDelta::FromMinutes(30));
-
-  // Measure how long a the various cleanup tasks took. http://crbug.com/305419.
-  UMA_HISTOGRAM_TIMES("Prerender.PeriodicCleanupDeleteContentsTime",
-                      cleanup_timer.Elapsed());
 }
 
 void PrerenderManager::PostCleanupTask() {
--- chrome/browser/prerender/prerender_util.cc
+++ chrome/browser/prerender/prerender_util.cc
@@ -18,31 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace prerender {
 
-namespace {
-
-enum PrerenderSchemeCancelReason {
-  PRERENDER_SCHEME_CANCEL_REASON_EXTERNAL_PROTOCOL,
-  PRERENDER_SCHEME_CANCEL_REASON_DATA,
-  PRERENDER_SCHEME_CANCEL_REASON_BLOB,
-  PRERENDER_SCHEME_CANCEL_REASON_FILE,
-  PRERENDER_SCHEME_CANCEL_REASON_FILESYSTEM,
-  PRERENDER_SCHEME_CANCEL_REASON_WEBSOCKET,
-  PRERENDER_SCHEME_CANCEL_REASON_FTP,
-  PRERENDER_SCHEME_CANCEL_REASON_CHROME,
-  PRERENDER_SCHEME_CANCEL_REASON_CHROME_EXTENSION,
-  PRERENDER_SCHEME_CANCEL_REASON_ABOUT,
-  PRERENDER_SCHEME_CANCEL_REASON_UNKNOWN,
-  PRERENDER_SCHEME_CANCEL_REASON_MAX,
-};
-
-void ReportPrerenderSchemeCancelReason(PrerenderSchemeCancelReason reason) {
-  UMA_HISTOGRAM_ENUMERATION(
-      "Prerender.SchemeCancelReason", reason,
-      PRERENDER_SCHEME_CANCEL_REASON_MAX);
-}
-
-}  // namespace
-
 bool IsGoogleOriginURL(const GURL& origin_url) {
   // ALLOW_NON_STANDARD_PORTS for integration tests with the embedded server.
   if (!google_util::IsGoogleDomainUrl(origin_url,
@@ -53,39 +28,6 @@ bool IsGoogleOriginURL(const GURL& origin_url) {
 
   return (origin_url.path_piece() == "/") ||
          google_util::IsGoogleSearchUrl(origin_url);
-}
-
-void ReportPrerenderExternalURL() {
-  ReportPrerenderSchemeCancelReason(
-      PRERENDER_SCHEME_CANCEL_REASON_EXTERNAL_PROTOCOL);
-}
-
-void ReportUnsupportedPrerenderScheme(const GURL& url) {
-  if (url.SchemeIs(url::kDataScheme)) {
-    ReportPrerenderSchemeCancelReason(PRERENDER_SCHEME_CANCEL_REASON_DATA);
-  } else if (url.SchemeIs(url::kBlobScheme)) {
-    ReportPrerenderSchemeCancelReason(PRERENDER_SCHEME_CANCEL_REASON_BLOB);
-  } else if (url.SchemeIsFile()) {
-    ReportPrerenderSchemeCancelReason(PRERENDER_SCHEME_CANCEL_REASON_FILE);
-  } else if (url.SchemeIsFileSystem()) {
-    ReportPrerenderSchemeCancelReason(
-        PRERENDER_SCHEME_CANCEL_REASON_FILESYSTEM);
-  } else if (url.SchemeIs(url::kWsScheme) || url.SchemeIs(url::kWssScheme)) {
-    ReportPrerenderSchemeCancelReason(PRERENDER_SCHEME_CANCEL_REASON_WEBSOCKET);
-  } else if (url.SchemeIs(url::kFtpScheme)) {
-    ReportPrerenderSchemeCancelReason(PRERENDER_SCHEME_CANCEL_REASON_FTP);
-  } else if (url.SchemeIs(content::kChromeUIScheme)) {
-    ReportPrerenderSchemeCancelReason(PRERENDER_SCHEME_CANCEL_REASON_CHROME);
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-  } else if (url.SchemeIs(extensions::kExtensionScheme)) {
-    ReportPrerenderSchemeCancelReason(
-        PRERENDER_SCHEME_CANCEL_REASON_CHROME_EXTENSION);
-#endif
-  } else if (url.SchemeIs(url::kAboutScheme)) {
-    ReportPrerenderSchemeCancelReason(PRERENDER_SCHEME_CANCEL_REASON_ABOUT);
-  } else {
-    ReportPrerenderSchemeCancelReason(PRERENDER_SCHEME_CANCEL_REASON_UNKNOWN);
-  }
 }
 
 }  // namespace prerender
