3d3ce66a1b68c5278a310dd80d00c93f00a5f87c
Stepan Khapugin <stkhapugin@chromium.org>
2020-04-21 11:22:23 +0000

    Move ContentSuggestionsSchedulerNotifications to NTP Snippets.
    Moves this file from app/startup.
    
    Bug: none
    Change-Id: I827baf0dd50dea155024cee8ce7c4cb985f00513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156411
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760898}

--- ios/chrome/browser/ntp_snippets/content_suggestions_scheduler_notifications.mm
+++ ios/chrome/browser/ntp_snippets/content_suggestions_scheduler_notifications.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/app/startup/content_suggestions_scheduler_notifications.h"
+#import "ios/chrome/browser/ntp_snippets/content_suggestions_scheduler_notifications.h"
 
 #include "components/ntp_snippets/content_suggestions_service.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
