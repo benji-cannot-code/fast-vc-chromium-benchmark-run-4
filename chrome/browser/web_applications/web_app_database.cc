2465449e749a9492a20242342254905403b3abb6
Aya ElAttar <ayaelattar@google.com>
2020-04-21 10:59:19 +0000

    Added is_disabled field to webApps CrOS
    Added is_disabled field to WebApp::chromeos_data in order to be able to
    disable SystemWebApps by an admin policy.
    
    Bug: 1069605
    Change-Id: Ia994622360b372e268dfa88ac7c2ac5993c778ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153151
    Commit-Queue: Aya Elsayed <ayaelattar@google.com>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760889}

--- chrome/browser/web_applications/web_app_database.cc
+++ chrome/browser/web_applications/web_app_database.cc
@@ -132,6 +132,7 @@ std::unique_ptr<WebAppProto> WebAppDatabase::CreateWebAppProto(
     mutable_chromeos_data->set_show_in_search(chromeos_data.show_in_search);
     mutable_chromeos_data->set_show_in_management(
         chromeos_data.show_in_management);
+    mutable_chromeos_data->set_is_disabled(chromeos_data.is_disabled);
   }
 
   local_data->set_is_in_sync_install(web_app.is_in_sync_install());
@@ -256,6 +257,7 @@ std::unique_ptr<WebApp> WebAppDatabase::CreateWebApp(
     chromeos_data->show_in_search = chromeos_data_proto.show_in_search();
     chromeos_data->show_in_management =
         chromeos_data_proto.show_in_management();
+    chromeos_data->is_disabled = chromeos_data_proto.is_disabled();
     web_app->SetWebAppChromeOsData(std::move(chromeos_data));
   }
 
