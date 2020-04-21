f8d664f3667a2e627c79bdb975ca4870eb5c129f
Zain Afzal <zafzal@google.com>
2020-04-21 04:38:47 +0000

    Removed misleading comment
    There is still a comment in the save as handler code in launch.js which
    references the logic of relaunching the app with the new saved file.
    This logic was removed but the comment got left behind. This cl just
    removes this comment.
    
    Change-Id: Idf7083680883cd47e3c97ca0c12a37f3f5403199
    Bug: 996088
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156589
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Cr-Commit-Position: refs/heads/master@{#760819}

--- chromeos/components/media_app_ui/resources/js/launch.js
+++ chromeos/components/media_app_ui/resources/js/launch.js
@@ -146,7 +146,6 @@ guestMessagePipe.registerHandler(Message.SAVE_COPY, async (message) => {
     return 'attemptedCurrentlyWritableFileOverwrite';
   }
 
-  // Load this file into the app.
   await saveToFile(handle, blob);
 });
 
