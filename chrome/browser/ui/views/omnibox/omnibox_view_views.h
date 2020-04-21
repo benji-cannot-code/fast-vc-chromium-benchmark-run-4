fa7321270ca6e8e8679081b2bf00ebf4e47a6431
Dominic Mazzoni <dmazzoni@chromium.org>
2020-04-21 03:37:44 +0000

    Ensure Mac omnibox accessibility announcements come last.
    When arrowing through omnibox suggestions, VoiceOver wasn't always
    announcing the "friendly" suggestion. Use PostTask to ensure that
    notification always comes after any text value change notifications
    to ensure that it's spoken.
    
    Bug: 971386
    Change-Id: Ie108a17a0baecb6b6b959cc5e234ae6c318ac674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156117
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760813}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.h
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.h
@@ -207,6 +207,10 @@ class OmniboxViewViews : public OmniboxView,
   // and returns true.
   bool MaybeTriggerSecondaryButton(const ui::KeyEvent& event);
 
+#if defined(OS_MACOSX)
+  void AnnounceFriendlySuggestionText();
+#endif
+
   // OmniboxView:
   void SetCaretPos(size_t caret_pos) override;
   void UpdatePopup() override;
@@ -373,6 +377,8 @@ class OmniboxViewViews : public OmniboxView,
       send_tab_to_self_sub_menu_model_;
 
   PrefChangeRegistrar pref_change_registrar_;
+
+  base::WeakPtrFactory<OmniboxViewViews> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(OmniboxViewViews);
 };
