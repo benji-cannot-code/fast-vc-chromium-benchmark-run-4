f4a5f374c5db28afe66d3c0a41e46a04a6c69226
Anastasiia Nikolaienko <anastasiian@chromium.org>
2020-04-21 12:17:52 +0000

    Add UMA histograms for consent logging result
    Bug: 1043108, 1070656
    Change-Id: Ib0b9cf4037a6cbfb7b7dd00544bbd931e0e70fc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2130846
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#760913}

--- chrome/browser/ui/webui/signin/inline_login_handler_chromeos.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler_chromeos.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
@@ -206,6 +207,7 @@ class ChildSigninHelper : public SigninHelper {
 
   void OnConsentLogged(const std::string& refresh_token,
                        SecondaryAccountConsentLogger::Result result) {
+    UMA_HISTOGRAM_ENUMERATION("Signin.SecondaryAccountConsentLog", result);
     secondary_account_consent_logger_.reset();
     if (result == SecondaryAccountConsentLogger::Result::kSuccess) {
       UpsertAccount(refresh_token);
