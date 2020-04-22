fecb64afa79d507c91bad34ef537844b80e6d682
Piotr Bialecki <bialpio@chromium.org>
2020-04-22 20:07:40 +0000

    WebXR - AR - add anchors blink feature, feature descriptor and UMAs
    - add experimental blink feature flag to gate anchors API
    - add MeasureAs & corresponding web_feature entries for entry points
    into the anchors API
    - add feature descriptor & feature policy for anchors API
    - modify samples to require appropriate feature
    
    Changes: 
    Change-Id: I4a25e17191eb7a769a522bdce4338d781cfb7abb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153581
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761598}

--- chrome/browser/vr/metrics/webxr_session_tracker.cc
+++ chrome/browser/vr/metrics/webxr_session_tracker.cc
@@ -73,6 +73,7 @@ void WebXRSessionTracker::ReportFeatureUsed(
     case XRSessionFeature::DOM_OVERLAY:
     case XRSessionFeature::HIT_TEST:
     case XRSessionFeature::LIGHT_ESTIMATION:
+    case XRSessionFeature::ANCHORS:
       // Not recording metrics for these features currently.
       // TODO(https://crbug.com/965729): Add metrics for the AR-related features
       // that are enabled by default.
@@ -109,6 +110,7 @@ void WebXRSessionTracker::SetFeatureRequest(
     case XRSessionFeature::DOM_OVERLAY:
     case XRSessionFeature::HIT_TEST:
     case XRSessionFeature::LIGHT_ESTIMATION:
+    case XRSessionFeature::ANCHORS:
       // Not recording metrics for these features currently.
       // TODO(https://crbug.com/965729): Add metrics for the AR-related features
       // that are enabled by default.
