bc96df9e036e630eda3917ccc01b74d7fa6a6ba4
Denis Kuznetsov <antrim@google.com>
2020-04-22 11:02:15 +0000

    OOBE: Introduce UI debugger
    Add debugging interface to OOBE WebUI.
    Interface can only be enabled via command-line switch and only
    for Chromeos-on-Linux and ChromeOS test images.
    
    This initial CL only adds a way to quickly switch OOBE language.
    
    Bug: 1073095
    Change-Id: I341f96a205a0cfd6eeff8bd684f401482cd6a0d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157009
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761372}

---
+++ chrome/browser/resources/chromeos/login/debug/no_debug.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+ * OOBE UI Debugger is not enabled. Please use --show-oobe-dev-overlay command
+ * line switch to enable. Limited to ChromeOS-on-linux and test images only.
+ */
--- chrome/browser/ui/webui/chromeos/login/oobe_ui.cc
+++ chrome/browser/ui/webui/chromeos/login/oobe_ui.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/system/sys_info.h"
 #include "base/values.h"
 #include "build/branding_buildflags.h"
 #include "chrome/browser/browser_process.h"
@@ -128,6 +129,7 @@ constexpr char kArcPlaystoreLogoPath[] = "playstore.svg";
 constexpr char kArcSupervisionIconPath[] = "supervision_icon.png";
 constexpr char kCustomElementsHTMLPath[] = "custom_elements.html";
 constexpr char kCustomElementsJSPath[] = "custom_elements.js";
+constexpr char kDebuggerJSPath[] = "debug.js";
 constexpr char kDiscoverJSPath[] = "discover_app.js";
 constexpr char kKeyboardUtilsJSPath[] = "keyboard_utils.js";
 constexpr char kLoginJSPath[] = "login.js";
@@ -244,6 +246,20 @@ void AddFingerprintResources(content::WebUIDataSource* source) {
   source->AddBoolean("useLottieAnimationForFingerprint", is_lottie_animation);
 }
 
+void AddDebuggerResources(content::WebUIDataSource* source) {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  bool enable_debugger =
+      command_line->HasSwitch(::chromeos::switches::kShowOobeDevOverlay);
+  // TODO(crbug.com/1073095): Also enable for ChromeOS test images.
+  // Enable for ChromeOS-on-linux for developers.
+  bool test_mode = !base::SysInfo::IsRunningOnChromeOS();
+  if (enable_debugger && test_mode) {
+    source->AddResourcePath(kDebuggerJSPath, IDR_OOBE_DEBUGGER_JS);
+  } else {
+    source->AddResourcePath(kDebuggerJSPath, IDR_OOBE_DEBUGGER_STUB_JS);
+  }
+}
+
 // Default and non-shared resource definition for kOobeDisplay display type.
 // chrome://oobe/oobe
 void AddOobeDisplayTypeDefaultResources(content::WebUIDataSource* source) {
@@ -305,6 +321,8 @@ content::WebUIDataSource* CreateOobeUIDataSource(
   AddAssistantScreensResources(source);
   AddGestureNavigationResources(source);
   AddMarketingOptInResources(source);
+
+  AddDebuggerResources(source);
 
   source->AddResourcePath(kKeyboardUtilsJSPath, IDR_KEYBOARD_UTILS_JS);
   source->OverrideContentSecurityPolicyObjectSrc(
