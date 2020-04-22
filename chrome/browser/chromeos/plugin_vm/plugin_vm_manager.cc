2cecfed0fede77d5ad5cf286c45e0e4cca0fd0ca
Dmitry Torokhov <dtor@chromium.org>
2020-04-22 17:53:53 +0000

    Pass user's UI language preference when starting Plugin VM
    When starting Plugin VM dispatcher (via debugd) pass user's system UI
    language preference so that VM UI can use the same.
    
    Note that to avoid introducing new DBus method with new argument for
    the language, we are trying to send it in the old method, and if that
    fails with "invalid arguments" error, we will try the same method, but
    without sending language argument. This fallback will be removed once
    Chrome OS updates its implementation of StartVmPluginDispatcher method.
    
    Bug: b/150910107
    Change-Id: I10d907749cf7c15cd5ffde00d63a4addefdd9a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154102
    Commit-Queue: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761523}

--- chrome/browser/chromeos/plugin_vm/plugin_vm_manager.cc
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/notification_utils.h"
 #include "base/bind_helpers.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/guest_os/guest_os_share_path.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_engagement_metrics_service.h"
 #include "chrome/browser/chromeos/plugin_vm/plugin_vm_files.h"
@@ -269,10 +270,11 @@ void PluginVmManager::UpdateVmState(
   chromeos::DBusThreadManager::Get()
       ->GetDebugDaemonClient()
       ->StartPluginVmDispatcher(
-          owner_id_, base::BindOnce(&PluginVmManager::OnStartDispatcher,
-                                    weak_ptr_factory_.GetWeakPtr(),
-                                    std::move(success_callback),
-                                    std::move(error_callback)));
+          owner_id_, g_browser_process->GetApplicationLocale(),
+          base::BindOnce(&PluginVmManager::OnStartDispatcher,
+                         weak_ptr_factory_.GetWeakPtr(),
+                         std::move(success_callback),
+                         std::move(error_callback)));
 }
 
 void PluginVmManager::OnStartDispatcher(
--- chromeos/dbus/debug_daemon/fake_debug_daemon_client.cc
+++ chromeos/dbus/debug_daemon/fake_debug_daemon_client.cc
@@ -257,7 +257,8 @@ void FakeDebugDaemonClient::StopConcierge(ConciergeCallback callback) {
 }
 
 void FakeDebugDaemonClient::StartPluginVmDispatcher(
-    const std::string& owner_id,
+    const std::string& /* owner_id */,
+    const std::string& /* lang */,
     PluginVmDispatcherCallback callback) {
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE, base::BindOnce(std::move(callback), true));
