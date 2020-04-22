85aad7240419075e49ce35dc8b408d304e1a589d
Aaron Leventhal <aleventhal@google.com>
2020-04-22 17:56:50 +0000

    Improve debugging string for AXObject
    Bug: None
    Change-Id: Iab691bbd137b94acf453137368e83498842f3d74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153788
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761524}

--- third_party/blink/renderer/modules/accessibility/ax_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_object.h
@@ -1049,7 +1049,7 @@ class MODULES_EXPORT AXObject : public GarbageCollected<AXObject> {
   int GetDOMNodeId() const;
 
   // Returns a string representation of this object.
-  String ToString() const;
+  String ToString(bool verbose = false) const;
 
  protected:
   AXID id_;
