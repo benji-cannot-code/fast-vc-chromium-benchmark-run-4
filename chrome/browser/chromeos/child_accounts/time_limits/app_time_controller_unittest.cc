bac63d569994d9d4fbb8b5e9493d62059ab745d6
yilkal <yilkal@chromium.org>
2020-04-22 16:22:13 +0000

    No time limit set for chrome if WebTimeLimit is not enabled.
    This cl ensures that no time limit is set for chrome
    and web apps if web time limit feature flag is not enabled.
    
    Bug: 1072037
    Change-Id: If8b79b3f3f2c849fffa7507acf717ee8f7fd7a11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159950
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761493}

--- chrome/browser/chromeos/child_accounts/time_limits/app_time_controller_unittest.cc
+++ chrome/browser/chromeos/child_accounts/time_limits/app_time_controller_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/app_service/app_service_test.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_activity_registry.h"
+#include "chrome/browser/chromeos/child_accounts/time_limits/app_time_limit_utils.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_time_limits_policy_builder.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_time_test_utils.h"
 #include "chrome/browser/chromeos/child_accounts/time_limits/app_types.h"
@@ -115,6 +116,8 @@ class AppTimeControllerTest : public testing::Test {
 
   void EnablePerAppTimeLimits();
 
+  void DisableWebTimeLimit();
+
   void CreateActivityForApp(const AppId& app_id,
                             base::TimeDelta active_time,
                             base::TimeDelta time_limit);
@@ -197,6 +200,16 @@ void AppTimeControllerTest::TearDown() {
 
 void AppTimeControllerTest::EnablePerAppTimeLimits() {
   scoped_feature_list_.InitAndEnableFeature(features::kPerAppTimeLimits);
+}
+
+void AppTimeControllerTest::DisableWebTimeLimit() {
+  scoped_feature_list_.InitWithFeatures(
+      /* enabled_features */ {{features::kPerAppTimeLimits}},
+      /* disabled_features */ {{features::kWebTimeLimits}});
+
+  // Recreate app time controller.
+  DeleteController();
+  InstantiateController();
 }
 
 void AppTimeControllerTest::CreateActivityForApp(const AppId& app_id,
@@ -604,6 +617,22 @@ TEST_F(AppTimeControllerTest, SetLastResetTimeTest) {
 
   test_api()->SetLastResetTime(prev_midnight + kDay);
   EXPECT_EQ(test_api()->GetLastResetTime(), reset_time);
+}
+
+TEST_F(AppTimeControllerTest, WebTimeLimitDisabled) {
+  DisableWebTimeLimit();
+
+  // Simulate chrome install
+  controller()->app_registry()->OnAppInstalled(GetChromeAppId());
+  controller()->app_registry()->OnAppAvailable(GetChromeAppId());
+
+  // Set the time limit to 30 minutes but run chrome for 1 hour.
+  CreateActivityForApp(GetChromeAppId(), kOneHour, kOneHour / 2);
+
+  EXPECT_EQ(controller()->app_registry()->GetWebTimeLimit(), base::nullopt);
+
+  EXPECT_EQ(controller()->app_registry()->GetAppState(GetChromeAppId()),
+            AppState::kAvailable);
 }
 
 }  // namespace app_time
