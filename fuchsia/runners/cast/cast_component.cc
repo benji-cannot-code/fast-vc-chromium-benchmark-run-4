ce90eaf1ac78162f90586ffc364b0150b65b3339
Rakina Zata Amni <rakina@chromium.org>
2020-04-22 04:54:46 +0000

    Revert "[fuchsia] Simplify implementation of the CastRunner."
    This reverts commit e15433f371a787bcdc26fff9c2ef8ccceb6ba454.
    
    Reason for revert: Probably causes failures in https://ci.chromium.org/p/chromium/builders/ci/Fuchsia%20ARM64/102925
    
    Original change's description:
    > [fuchsia] Simplify implementation of the CastRunner.
    > 
    > The implementation of the fuchsia.sys.Runner for Cast apps is refactored
    > to be easier to maintain, in the following ways:
    > 
    > 1. Instead of deriving from WebContentRunner and overriding certain
    >    behaviours, the CastRunner now instantiates WebContentRunner instances
    >    internally, to use to manage CastComponents.
    > 
    > 2. CastRunner now implements fuchsia.sys.Runner itself, and manages the
    >    PendingCastComponents, prior to creating & handing off CastComponents
    >    to be managed by the WebContentRunner(s).
    > 
    > 3. The CreateContextParams for the main and isolated contexts are
    >    constructed using a common base, rather than copying & filtering the
    >    main context parameters, to create parameters for isolated contexts.
    > 
    > Bug: 1071544
    > Change-Id: Ic66ca0a9805e9b00d213c62845e5101ff9dea76e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153055
    > Commit-Queue: Wez <wez@chromium.org>
    > Reviewed-by: David Dorwin <ddorwin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#761177}
    
    TBR=ddorwin@chromium.org,wez@chromium.org
    
    Change-Id: I34fe26155b03455dc1c5b9da9a052b8fa6fd9d57
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1071544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160286
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761308}

--- fuchsia/runners/cast/cast_component.cc
+++ fuchsia/runners/cast/cast_component.cc
@@ -47,13 +47,10 @@ bool CastComponent::Params::AreComplete() const {
   return true;
 }
 
-CastComponent::CastComponent(WebContentRunner* runner,
-                             CastComponent::Params params,
-                             bool is_headless)
+CastComponent::CastComponent(CastRunner* runner, CastComponent::Params params)
     : WebComponent(runner,
                    std::move(params.startup_context),
                    std::move(params.controller_request)),
-      is_headless_(is_headless),
       agent_manager_(std::move(params.agent_manager)),
       application_config_(std::move(params.application_config)),
       url_rewrite_rules_provider_(std::move(params.url_rewrite_rules_provider)),
@@ -67,10 +64,6 @@ CastComponent::CastComponent(WebContentRunner* runner,
 }
 
 CastComponent::~CastComponent() = default;
-
-void CastComponent::SetOnDestroyedCallback(base::OnceClosure on_destroyed) {
-  on_destroyed_ = std::move(on_destroyed);
-}
 
 void CastComponent::StartComponent() {
   if (application_config_.has_enable_remote_debugging() &&
@@ -124,11 +117,13 @@ void CastComponent::StartComponent() {
   }
 }
 
+CastRunner* CastComponent::runner() const {
+  return static_cast<CastRunner*>(WebComponent::runner());
+}
+
 void CastComponent::DestroyComponent(int termination_exit_code,
                                      fuchsia::sys::TerminationReason reason) {
   DCHECK(!constructor_active_);
-
-  std::move(on_destroyed_).Run();
 
   WebComponent::DestroyComponent(termination_exit_code, reason);
 }
@@ -153,7 +148,7 @@ void CastComponent::CreateView(
     zx::eventpair view_token,
     fidl::InterfaceRequest<fuchsia::sys::ServiceProvider> incoming_services,
     fidl::InterfaceHandle<fuchsia::sys::ServiceProvider> outgoing_services) {
-  if (is_headless_) {
+  if (runner()->is_headless()) {
     // For headless CastComponents, |view_token| does not actually connect to a
     // Scenic View. It is merely used as a conduit for propagating termination
     // signals.
@@ -173,7 +168,7 @@ void CastComponent::CreateView(
 void CastComponent::OnZxHandleSignalled(zx_handle_t handle,
                                         zx_signals_t signals) {
   DCHECK_EQ(signals, ZX_SOCKET_PEER_CLOSED);
-  DCHECK(is_headless_);
+  DCHECK(runner()->is_headless());
 
   frame()->DisableHeadlessRendering();
 }
--- fuchsia/runners/web/main.cc
+++ fuchsia/runners/web/main.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/fuchsia/default_context.h"
 #include "base/fuchsia/file_utils.h"
-#include "base/fuchsia/scoped_service_binding.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_executor.h"
@@ -60,9 +59,8 @@ int main(int argc, char** argv) {
       base::BindRepeating(&GetContextParams);
 
   WebContentRunner runner(std::move(get_context_params_callback));
-  base::fuchsia::ScopedServiceBinding<fuchsia::sys::Runner> binding(
-      base::fuchsia::ComponentContextForCurrentProcess()->outgoing().get(),
-      &runner);
+  runner.PublishRunnerService(
+      base::fuchsia::ComponentContextForCurrentProcess()->outgoing().get());
 
   base::fuchsia::ComponentContextForCurrentProcess()
       ->outgoing()
