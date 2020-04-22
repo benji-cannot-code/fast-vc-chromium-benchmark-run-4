a7603abce450439311d9750d6f60ad968077306a
Clemens Arbesser <arbesser@google.com>
2020-04-22 10:32:12 +0000

    [Autofill Assistant] Fixed dividers that were not cleaned up.
    Bug: b/152500114
    Change-Id: I8a7e2a1a21b03fb7499f74588e355d9695aa519e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159435
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#761366}

--- chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/user_data/additional_sections/AssistantAdditionalSectionContainer.java
+++ chrome/android/features/autofill_assistant/java/src/org/chromium/chrome/browser/autofill_assistant/user_data/additional_sections/AssistantAdditionalSectionContainer.java
@@ -55,6 +55,7 @@ public class AssistantAdditionalSectionContainer {
                     sections.get(i).createSection(mContext, mParent, index, mDelegate);
             section.setDelegate(mDelegate);
             mSections.add(section);
+            mDividers.add(divider);
         }
         Collections.reverse(mSections);
     }
