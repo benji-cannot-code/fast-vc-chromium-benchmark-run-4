6af78d5b747b0dcd01e60e01ef79e7f733e6453e
Esmael El-Moslimany <aee@chromium.org>
2020-04-22 01:14:00 +0000

    WebUI NTP: most visited edit title does not use computed text direction
    Bug: 1060919
    Change-Id: I1bc1053e2582b41a68281c4ffc6f15682324f53b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160072
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761248}

--- chrome/browser/resources/new_tab_page/most_visited.js
+++ chrome/browser/resources/new_tab_page/most_visited.js
@@ -97,9 +97,6 @@ class MostVisitedElement extends PolymerElement {
       dialogTileTitle_: String,
 
       /** @private */
-      dialogTileTitleDirectionClass_: String,
-
-      /** @private */
       dialogTileUrl_: String,
 
       /** @private */
@@ -485,7 +482,6 @@ class MostVisitedElement extends PolymerElement {
   onAdd_() {
     this.dialogTitle_ = loadTimeData.getString('addLinkTitle');
     this.dialogTileTitle_ = '';
-    this.dialogTileTitleDirectionClass_ = '';
     this.dialogTileUrl_ = '';
     this.dialogTileUrlInvalid_ = false;
     this.adding_ = true;
@@ -586,8 +582,6 @@ class MostVisitedElement extends PolymerElement {
     this.dialogTitle_ = loadTimeData.getString('editLinkTitle');
     const tile = this.tiles_[this.actionMenuTargetIndex_];
     this.dialogTileTitle_ = tile.title;
-    this.dialogTileTitleDirectionClass_ =
-        this.getTileTitleDirectionClass_(tile);
     this.dialogTileUrl_ = tile.url.url;
     this.dialogTileUrlInvalid_ = false;
     this.$.dialog.showModal();
