b2d6804a042517bbd060c1ad46c29aecfc0a7907
Hans Wennborg <hans@chromium.org>
2020-04-22 17:43:31 +0000

    logging.h related include-what-you-use (content/)
    Fix code that was implicitly depending on logging.h or its
    dependencies.
    
    For example, code that uses DISALLOW_COPY_AND_ASSIGN should
    include base/macros.h, but it currently happens to work if
    it pulls in logging.h directly or through some dependency.
    
    This is part of refactoring the codebase to use
    check.h/check_op.h for the CHECK and CHECK_op macro
    instead of logging.h.
    
    Bug: 1031540
    Change-Id: Ic7acb86282462f129ef926d5879c14df0a486a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156948
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761520}

--- content/browser/renderer_host/direct_manipulation_test_helper_win.cc
+++ content/browser/renderer_host/direct_manipulation_test_helper_win.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/browser/renderer_host/direct_manipulation_test_helper_win.h"
+#include "base/logging.h"
 
 namespace content {
 MockDirectManipulationContent::MockDirectManipulationContent() {}
--- content/public/browser/mhtml_generation_result.h
+++ content/public/browser/mhtml_generation_result.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_MHTML_GENERATION_RESULT_H_
 #define CONTENT_PUBLIC_BROWSER_MHTML_GENERATION_RESULT_H_
 
+#include "base/callback_forward.h"
 #include "base/optional.h"
 #include "content/common/content_export.h"
 
