b6eb199e6a3d34ed91f51b371ef6647b92cac550
Jesse McKenna <jessemckenna@google.com>
2020-04-22 21:49:51 +0000

    Revert "Reland "boilerplate.py: Ignore current-dir prefixes .\, ./""
    This reverts commit 7c8c6f1ead8a3318a93cf4d41f9d30fba349d3fa.
    
    Reason for revert: I didn't mean to reland this yet... =_=
    
    Original change's description:
    > Reland "boilerplate.py: Ignore current-dir prefixes .\, ./"
    > 
    > This reverts commit f909a6a231ec78eb10f433f0599d4053ab02b73e.
    > 
    > Reason for revert: _RemoveCurrentDirectoryPrefix was missing a
    > line to return the original argument if it was not modified,
    > i.e. the function was returning None if no modification took
    > place. This reland adds the missing return statement.
    > 
    > Original change's description:
    > > Revert "boilerplate.py: Ignore current-dir prefixes .\, ./"
    > > 
    > > This reverts commit 9d14b9ec3607ce5cc9a9f88ddf55e39378742515.
    > > 
    > > Reason for revert: Change is breaking calls without .\ prefix.
    > > 
    > > Original change's description:
    > > > boilerplate.py: Ignore current-dir prefixes .\, ./
    > > > 
    > > > This change makes boilerplate.py trim a leading ".\" (Windows's
    > > > current-directory prefix) or "./" (Unix's current-directory prefix)
    > > > from the filename argument, if present.
    > > > 
    > > > This is a convenience change for cases where the current-directory
    > > > prefix is prepended. For example, Windows PowerShell automatically
    > > > prepends ".\" to a path when tab completion is used (e.g., typing
    > > > "chrome/b" and pressing tab results in ".\chrome\browser"). This
    > > > change will remove artifacts of this for users who use tab completion
    > > > while typing the filename argument.
    > > > 
    > > > Currently:
    > > > `tools/boilerplate.py .\test.h` produces include guard `__TEST_H_`
    > > > `tools/boilerplate.py ./test.cc` or `.mm` produces `#include "./test.h"`
    > > > 
    > > > With this change:
    > > > `tools/boilerplate.py .\test.h` produces include guard `TEST_H_`
    > > > `tools/boilerplate.py ./test.cc` or `.mm` produces `#include "test.h"`
    > > > 
    > > > Change-Id: I19fd2e72c58135b5e87d4a1e22ec8bc76ef326aa
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157876
    > > > Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    > > > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#761085}
    > > 
    > > TBR=rsesek@chromium.org,jessemckenna@google.com
    > > 
    > > Change-Id: Ic02537133fd1548f5279ead83abb0492d6657aaa
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159993
    > > Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    > > Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#761185}
    > 
    > TBR=rsesek@chromium.org,jessemckenna@google.com
    > 
    > Change-Id: I224cbe046d85af6d4c6d542fb2780fbb1794d331
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161312
    > Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    > Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    > Cr-Commit-Position: refs/heads/master@{#761650}
    
    TBR=rsesek@chromium.org,jessemckenna@google.com
    
    Change-Id: Ic45f66ec6a9803902951dd40eea5b1ba03443cb9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161868
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/master@{#761655}

--- tools/boilerplate.py
+++ tools/boilerplate.py
@@ -56,15 +56,6 @@ def _CppHeader(filename):
   ])
 
 
-def _RemoveCurrentDirectoryPrefix(filename):
-  current_dir_prefixes = [os.curdir + os.sep]
-  if os.altsep is not None:
-    current_dir_prefixes.append(os.curdir + os.altsep)
-  for prefix in current_dir_prefixes:
-    if filename.startswith(prefix):
-      return filename[len(prefix):]
-
-
 def _RemoveTestSuffix(filename):
   base, _ = os.path.splitext(filename)
   suffixes = [ '_test', '_unittest', '_browsertest' ]
@@ -104,8 +95,6 @@ def _ObjCppImplementation(filename):
 
 
 def _CreateFile(filename):
-  filename = _RemoveCurrentDirectoryPrefix(filename)
-
   contents = _GetHeader(filename) + '\n'
 
   if filename.endswith('.h'):
