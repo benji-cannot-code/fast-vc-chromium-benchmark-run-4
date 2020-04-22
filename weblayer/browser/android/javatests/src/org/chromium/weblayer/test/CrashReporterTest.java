84a4bd2eeb46295db770e08bd2027b54468d38c8
Tim Volodine <timvolodine@chromium.org>
2020-04-22 15:26:32 +0000

    [WebLayer] Disable flaky CrashReporterTest#testCrashReporterLoading on P+
    Flaky on android-pie-x86-rel
    
    BUG=1073464
    TBR=tobiasjs@chromium.org,torne@chromium.org
    
    Change-Id: I9314fd8c1b2b120f2b12d0515bdcd9e489c9ab20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161029
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761462}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/CrashReporterTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/CrashReporterTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.weblayer.test;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
@@ -17,6 +18,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.weblayer.CrashReporterCallback;
 import org.chromium.weblayer.CrashReporterController;
@@ -75,7 +77,10 @@ public class CrashReporterTest {
 
     @Test
     @SmallTest
-    public void testCrashReporterLoading() throws Exception {
+    @DisableIf.Build(sdk_is_greater_than = Build.VERSION_CODES.O_MR1,
+            message = "Flaky on P, crbug.com/1073464")
+    public void
+    testCrashReporterLoading() throws Exception {
         BundleCallbackHelper callbackHelper = new BundleCallbackHelper();
         CallbackHelper deleteHelper = new CallbackHelper();
 
