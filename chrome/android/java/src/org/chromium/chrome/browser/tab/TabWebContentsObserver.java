da4add2df140f83e732feb41826a68d7cdb638e3
Jinsuk Kim <jinsukkim@chromium.org>
2020-04-22 22:38:09 +0000

    Android: Null check in TabWebContentsObserver
    The reported crash suggests that there are situations where Activity
    destruction can happen after WebContents is already nulled out,
    causing the TabWebContentsObserver.cleanupWebContents to be triggered
    after the internal WebContentsObserver field is already cleared. This
    CL adds a guard against it to avoid the NPE.
    
    Bug: 1073008
    Change-Id: Id01d5070e81e30f6ab9e3aee54322e6619b722f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2158204
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761679}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsObserver.java
@@ -135,8 +135,10 @@ public class TabWebContentsObserver extends TabWebContentsUserData {
 
     @Override
     public void cleanupWebContents(WebContents webContents) {
-        mObserver.destroy();
-        mObserver = null;
+        if (mObserver != null) {
+            mObserver.destroy();
+            mObserver = null;
+        }
     }
 
     @VisibleForTesting
