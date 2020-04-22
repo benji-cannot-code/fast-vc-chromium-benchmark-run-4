5cadebdd534583a4bf3b096af72b11bfbc830fa5
Esmael El-Moslimany <aee@chromium.org>
2020-04-22 20:52:18 +0000

    WebUI NTP: layer logo on top of OGB
    Before this CL, the OGB with an initial z-level of 0 is layered on top
    of the logo which is preventing the interactive logo from accepting mouse
    events. This CL increases the z-level of the logo which fixes the
    immediate issue which we can look into why the logo is layered
    differently from the rest of the NTP content which layers on top of the
    OGB correctly.
    
    Change-Id: I95c40bf3acfedfb213567a283737855073d432eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161354
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761619}

--- chrome/browser/resources/new_tab_page/logo.js
+++ chrome/browser/resources/new_tab_page/logo.js
@@ -50,7 +50,7 @@ class LogoElement extends PolymerElement {
       /** @private */
       loaded_: Boolean,
 
-      /** @private */
+      /** @private {newTabPage.mojom.Doodle} */
       doodle_: Object,
 
       /** @private */
