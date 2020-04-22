4f8f8e9103e08a0b082c4593db783565bf99d33e
Rayan Kanso <rayankans@google.com>
2020-04-22 20:09:11 +0000

    [gIRA] Disable getInstalledRelatedApps on WebView
    Adds a feature kill switch, and disables it for WebView.
    
    Change-Id: Ieeb4079596c0b2d1f3fc79a17f62f785fff81112
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2156991
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761599}

--- third_party/blink/public/platform/web_runtime_features.h
+++ third_party/blink/public/platform/web_runtime_features.h
@@ -233,6 +233,8 @@ class WebRuntimeFeatures {
 
   BLINK_PLATFORM_EXPORT static void EnableTrustTokens(bool);
 
+  BLINK_PLATFORM_EXPORT static void EnableInstalledApp(bool);
+
  private:
   WebRuntimeFeatures();
 };
