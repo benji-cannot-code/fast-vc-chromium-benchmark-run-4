718d89cf69ad945dffb6b6f42b62178a7acd1f48
Chris Lu <thegreenfrog@chromium.org>
2020-04-22 21:08:05 +0000

    Set TranslateInfobarDelegate’s step when it changes
    This change will be helpful in the new Tranlsate Overlay framework
    migration, but when the state changes, its properties should be
    updated as well.
    
    Bug: 1071914
    Change-Id: Ib8b22b635634c39030cadc1a04a2e2e5ba821228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157778
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761629}

--- components/translate/core/browser/translate_infobar_delegate.cc
+++ components/translate/core/browser/translate_infobar_delegate.cc
@@ -114,6 +114,7 @@ void TranslateInfoBarDelegate::Create(
 
   // Try to reuse existing translate infobar delegate.
   if (old_delegate) {
+    old_delegate->step_ = step;
     for (auto& observer : old_delegate->observers_) {
       observer.OnTranslateStepChanged(step, error_type);
     }
