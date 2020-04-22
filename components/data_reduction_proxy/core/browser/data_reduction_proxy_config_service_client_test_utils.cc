849b845dc85d5b95edb8c55faad6632e083163b2
Michael Crouse <mcrouse@chromium.org>
2020-04-22 18:52:29 +0000

    Deprecate ProxyConfig and ProxyServer from ClientConfig.
    This change deprecates the proxy from the client config while
    maintaining the client config for continued use.
    
    Bug: 1064131, 1073510
    Change-Id: I9c184b330e6dbad3eb0a05c4730a0c29dc16768d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157717
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Auto-Submit: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761552}

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client_test_utils.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_config_service_client_test_utils.cc
@@ -14,53 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace data_reduction_proxy {
 
-// Creates a new ClientConfig from the given parameters.
-ClientConfig CreateConfig(const std::string& session_key,
-                          int64_t expire_duration_seconds,
-                          int64_t expire_duration_nanoseconds,
-                          ProxyServer_ProxyScheme primary_scheme,
-                          const std::string& primary_host,
-                          int primary_port,
-                          ProxyServer_ProxyScheme secondary_scheme,
-                          const std::string& secondary_host,
-                          int secondary_port,
-                          float reporting_fraction,
-                          bool ignore_long_term_black_list_rules) {
-  ClientConfig config;
-
-  config.set_session_key(session_key);
-  config.mutable_refresh_duration()->set_seconds(expire_duration_seconds);
-  config.mutable_refresh_duration()->set_nanos(expire_duration_nanoseconds);
-
-  // Leave the pageload_metrics_config empty when |reporting_fraction| is not
-  // inclusively between zero and one.
-  if (reporting_fraction >= 0.0f && reporting_fraction <= 1.0f) {
-    config.mutable_pageload_metrics_config()->set_reporting_fraction(
-        reporting_fraction);
-  }
-  config.set_ignore_long_term_black_list_rules(
-      ignore_long_term_black_list_rules);
-  ProxyServer* primary_proxy =
-      config.mutable_proxy_config()->add_http_proxy_servers();
-  primary_proxy->set_scheme(primary_scheme);
-  primary_proxy->set_host(primary_host);
-  primary_proxy->set_port(primary_port);
-
-  ProxyServer* secondary_proxy =
-      config.mutable_proxy_config()->add_http_proxy_servers();
-  secondary_proxy->set_scheme(secondary_scheme);
-  secondary_proxy->set_host(secondary_host);
-  secondary_proxy->set_port(secondary_port);
-
-  return config;
-}
-
-// Creates a new ClientConfig with no proxies from the given parameters.
-ClientConfig CreateEmptyProxyConfig(const std::string& session_key,
-                                    int64_t expire_duration_seconds,
-                                    int64_t expire_duration_nanoseconds,
-                                    float reporting_fraction,
-                                    bool ignore_long_term_black_list_rules) {
+// Creates a new ClientConfig.
+ClientConfig CreateClientConfig(const std::string& session_key,
+                                int64_t expire_duration_seconds,
+                                int64_t expire_duration_nanoseconds,
+                                float reporting_fraction,
+                                bool ignore_long_term_black_list_rules) {
   ClientConfig config;
 
   config.set_session_key(session_key);
@@ -89,9 +48,8 @@ std::string EncodeConfig(const ClientConfig& config) {
 }
 
 std::string DummyBase64Config() {
-  ClientConfig config = CreateConfig(
-      "secureSessionKey", 600, 0, ProxyServer_ProxyScheme_HTTPS, "origin.net",
-      443, ProxyServer_ProxyScheme_HTTP, "fallback.net", 80, 0.5f, false);
+  ClientConfig config =
+      CreateClientConfig("secureSessionKey", 600, 0, 0.5f, false);
   return EncodeConfig(config);
 }
 
