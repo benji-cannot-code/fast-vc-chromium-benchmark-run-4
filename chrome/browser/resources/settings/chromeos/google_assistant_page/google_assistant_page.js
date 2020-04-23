995f49465888b5db5daeff05da7183204e3a57c7
Kyle Horimoto <khorimoto@chromium.org>
2020-04-23 00:44:13 +0000

    [CrOS Settings] Remove unnecessary isAssistantAllowed_ property
    <os-settings-search-page> guards <settings-google-assistant-page> with a
    dom-if preventing it from being created if the isAssistantAllowed field
    is false [1]. But, within <settings-google-assistant-page>, we guard
    more elements by this same check. Since the entire subpage is never
    created if the field is false, this check is unnecessary.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/master:chrome/browser/resources/settings/chromeos/os_search_page/os_search_page.html;l=94-116;drc=c803dcbb1b80adc2e6832db6712ebeb6d818ba0f
    
    Bug: 1069849
    Change-Id: I8be1b610ce2a0434a7d7608377810972dd3f09f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161870
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761738}

--- chrome/browser/resources/settings/chromeos/google_assistant_page/google_assistant_page.js
+++ chrome/browser/resources/settings/chromeos/google_assistant_page/google_assistant_page.js
@@ -46,14 +46,6 @@ Polymer({
 
   properties: {
     /** @private */
-    isAssistantAllowed_: {
-      type: Boolean,
-      value() {
-        return loadTimeData.getBoolean('isAssistantAllowed');
-      },
-    },
-
-    /** @private */
     shouldShowVoiceMatchSettings_: {
       type: Boolean,
       value: false,
