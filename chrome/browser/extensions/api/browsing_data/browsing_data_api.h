d4126bb33e12a944a28a7bcbe36ba95ae7aace54
Istiaque Ahmed <lazyboy@chromium.org>
2020-04-23 10:43:50 +0000

    [Extensions] Fix include files in browsing_data API.
    https://chromium-review.googlesource.com/c/chromium/src/+/2155492
    removed the use of ChromeAsyncExtensionFunction from
    browsing_data API, but left its #include behind.
    
    This CL changes the include to the correct one:
    "extensions/browser/extension_function.h"
    
    Bug: 634140
    Change-Id: Id67995a732f5b16f3a1dc75bafc1eaffce607b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161791
    Auto-Submit: Istiaque Ahmed <lazyboy@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761868}

--- chrome/browser/extensions/api/browsing_data/browsing_data_api.h
+++ chrome/browser/extensions/api/browsing_data/browsing_data_api.h
@@ -14,11 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/scoped_observer.h"
-#include "chrome/browser/extensions/chrome_extension_function.h"
 #include "components/browsing_data/core/browsing_data_utils.h"
 #include "components/signin/core/browser/account_reconcilor.h"
 #include "content/public/browser/browsing_data_filter_builder.h"
 #include "content/public/browser/browsing_data_remover.h"
+#include "extensions/browser/extension_function.h"
 #include "ppapi/buildflags/buildflags.h"
 
 class PluginPrefs;
