aafcab8cf7653468b317404c904fce15f17cbfab
Johann <johannkoenig@google.com>
2020-04-23 12:58:03 +0000

    heap mojo: migrate battery dispatcher
    Use HeapMojoRemote wrapper so that Oilpan will GC the object.
    
    Bug: chromium:1049056
    
    Change-Id: If91318eb651fa32a7a2a3c37ce1d21c9f4d24f5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143419
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#761892}

--- third_party/blink/renderer/modules/battery/battery_dispatcher.h
+++ third_party/blink/renderer/modules/battery/battery_dispatcher.h
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_BATTERY_BATTERY_DISPATCHER_H_
 
 #include "base/macros.h"
-#include "mojo/public/cpp/bindings/remote.h"
 #include "services/device/public/mojom/battery_monitor.mojom-blink.h"
 #include "third_party/blink/renderer/core/frame/platform_event_dispatcher.h"
 #include "third_party/blink/renderer/modules/battery/battery_manager.h"
 #include "third_party/blink/renderer/modules/battery/battery_status.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 
 namespace blink {
 
@@ -30,6 +30,8 @@ class MODULES_EXPORT BatteryDispatcher final
     return has_latest_data_ ? &battery_status_ : nullptr;
   }
 
+  void Trace(Visitor*) override;
+
  private:
   void QueryNextStatus();
   void OnDidChange(device::mojom::blink::BatteryStatusPtr);
@@ -39,7 +41,7 @@ class MODULES_EXPORT BatteryDispatcher final
   void StartListening(LocalFrame* frame) override;
   void StopListening() override;
 
-  mojo::Remote<device::mojom::blink::BatteryMonitor> monitor_;
+  HeapMojoRemote<device::mojom::blink::BatteryMonitor> monitor_;
   BatteryStatus battery_status_;
   bool has_latest_data_;
 
