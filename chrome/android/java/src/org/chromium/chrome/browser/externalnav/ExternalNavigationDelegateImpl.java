dd07d7625ecf3eae4a46059d0b13c7722e646410
Colin Blundell <blundell@chromium.org>
2020-04-23 14:29:24 +0000

    Android: Dedupe //chrome and //weblayer isPdfDownload() functions
    The //chrome and //weblayer ExternalNavigationDelegate implementations
    have identical isPdfDownload() functions. This CL dedupes these
    functions into the shared ExternalNavigationHandler.java.
    
    Bug: 1071390
    Change-Id: Iebde2257f10a8704b79a76a2fa00b6d6f04845c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161014
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761914}

--- chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/externalnav/ExternalNavigationDelegateImpl.java
@@ -20,7 +20,6 @@ import android.os.StrictMode;
 import android.provider.Browser;
 import android.text.TextUtils;
 import android.view.WindowManager.BadTokenException;
-import android.webkit.MimeTypeMap;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
@@ -73,8 +72,6 @@ import java.util.List;
  * The main implementation of the {@link ExternalNavigationDelegate}.
  */
 public class ExternalNavigationDelegateImpl implements ExternalNavigationDelegate {
-    private static final String PDF_EXTENSION = "pdf";
-
     protected final Context mApplicationContext;
     private final Tab mTab;
     private final TabObserver mTabObserver;
@@ -442,14 +439,6 @@ public class ExternalNavigationDelegateImpl implements ExternalNavigationDelegat
         if (context instanceof ChromeActivity) {
             ((ChromeActivity) context).getTabModelSelector().closeTab(mTab);
         }
-    }
-
-    @Override
-    public boolean isPdfDownload(String url) {
-        String fileExtension = MimeTypeMap.getFileExtensionFromUrl(url);
-        if (TextUtils.isEmpty(fileExtension)) return false;
-
-        return PDF_EXTENSION.equals(fileExtension);
     }
 
     @Override
