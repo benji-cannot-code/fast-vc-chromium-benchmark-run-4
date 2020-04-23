c49e76bcc2f780cb079b82720f8ba991f79cde0c
David Bertoni <dbertoni@chromium.org>
2020-04-23 21:22:34 +0000

    [Extensions] Fixed a broken and flaky test.
    The ExtensionApiTest.ChromeRuntimeReloadDisable test was broken due
    to underlying changes in the code that terminates extensions that
    reload themselves too frequently. It was also flaky.
    
    The new version is fixed and is also no longer flaky.
    
    Bug: 366181
    Change-Id: I8b11cb5b2db1ee17885e3270cf41037faebb0923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2150164
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762108}

--- extensions/browser/api/runtime/runtime_api.cc
+++ extensions/browser/api/runtime/runtime_api.cc
@@ -165,6 +165,9 @@ BrowserContextKeyedAPIFactory<RuntimeAPI>* RuntimeAPI::GetFactoryInstance() {
   return g_factory.Pointer();
 }
 
+constexpr int RuntimeAPI::kFastReloadCount;
+constexpr int RuntimeAPI::kUnpackedFastReloadCount;
+
 // static
 void RuntimeAPI::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kPrefLastRestartWasDueToDelayedRestartApi,
