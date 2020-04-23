cae35527bcf87c388f315df30bd8e6a189786899
Collin Baker <collinbaker@chromium.org>
2020-04-23 21:23:35 +0000

    Don't immediately layout BrowserView on ChildPreferredSizeChanged
    When a child's preferred size changes, it already calls
    InvalidateLayout(). Immediately calling Layout() shouldn't be necessary.
    It causes several redundant layouts in many cases.
    
    This CL changes tests to not depend on BrowserView synchronously laying
    out itself. Instead, these tests trigger a layout when they need it. A
    method RunScheduledLayouts() is added to InProcessBrowserTest to run all
    scheduled layouts when needed.
    
    Bug: 1054489
    Change-Id: Ie3d6197b86d8f9e82fe31fa19d80fa794d751d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2133172
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762109}

--- chrome/browser/ui/views/extensions/extension_action_test_helper_views.h
+++ chrome/browser/ui/views/extensions/extension_action_test_helper_views.h
@@ -37,6 +37,7 @@ class ExtensionActionTestHelperViews : public ExtensionActionTestHelper {
   ExtensionsContainer* GetExtensionsContainer() override;
   std::unique_ptr<ExtensionActionTestHelper> CreateOverflowBar(
       Browser* browser) override;
+  void LayoutForOverflowBar() override;
   gfx::Size GetMinPopupSize() override;
   gfx::Size GetMaxPopupSize() override;
   gfx::Size GetToolbarActionSize() override;
--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -94,6 +94,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS)
 #include "ash/public/cpp/test/shell_test_api.h"
+#include "ash/shell.h"
 #include "base/system/sys_info.h"
 #include "chrome/browser/chromeos/input_method/input_method_configuration.h"
 #include "chromeos/constants/chromeos_switches.h"
@@ -110,9 +111,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(TOOLKIT_VIEWS)
+#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/test/views/accessibility_checker.h"
+#include "ui/views/test/widget_test.h"
 #include "ui/views/views_delegate.h"
+#include "ui/views/widget/widget.h"
 #endif
 
 namespace {
@@ -165,6 +169,26 @@ InProcessBrowserTest::InProcessBrowserTest(
   views_delegate_ = std::move(views_delegate);
 }
 #endif
+
+void InProcessBrowserTest::RunScheduledLayouts() {
+#if defined(TOOLKIT_VIEWS)
+  views::Widget::Widgets widgets_to_layout;
+
+#if defined(OS_CHROMEOS)
+  // WidgetTest::GetAllWidgets() doesn't work for ChromeOS in a production
+  // environment. We must get the Widgets ourself.
+  for (aura::Window* root_window : ash::Shell::GetAllRootWindows())
+    views::Widget::GetAllChildWidgets(root_window, &widgets_to_layout);
+#else
+  widgets_to_layout = views::test::WidgetTest::GetAllWidgets();
+#endif  // defined(OS_CHROMEOS)
+
+  for (views::Widget* widget : widgets_to_layout)
+    widget->LayoutRootViewIfNecessary();
+#endif  // defined(TOOLKIT_VIEWS)
+}
+
+// defined(TOOLKIT_VIEWS)
 
 void InProcessBrowserTest::Initialize() {
   CreateTestServer(GetChromeTestDataDir());
