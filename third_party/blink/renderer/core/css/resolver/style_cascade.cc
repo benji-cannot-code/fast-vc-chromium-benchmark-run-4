2e434a37841f78eeee75870bd9e875a8e0b8284a
Anders Hartvoll Ruud <andruud@chromium.org>
2020-04-23 09:44:06 +0000

    Add use counter for 'revert'
    Bug: 579788
    Change-Id: I7f442468b9da0b5f3a11923961f82d1f1fc4b140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161078
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761854}

--- third_party/blink/renderer/core/css/resolver/style_cascade.cc
+++ third_party/blink/renderer/core/css/resolver/style_cascade.cc
@@ -32,8 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/resolver/style_resolver_state.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
+#include "third_party/blink/renderer/core/frame/web_feature.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/core/style_property_shorthand.h"
+#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
@@ -711,6 +713,8 @@ const CSSValue* StyleCascade::ResolvePendingSubstitution(
 const CSSValue* StyleCascade::ResolveRevert(const CSSProperty& property,
                                             CascadeOrigin origin,
                                             CascadeResolver& resolver) {
+  GetDocument().CountUse(WebFeature::kCSSKeywordRevert);
+
   CascadeOrigin target_origin = TargetOriginForRevert(origin);
   const CSSProperty* target_property = &property;
 
