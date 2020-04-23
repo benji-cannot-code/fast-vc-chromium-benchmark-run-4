b6a8fa27c78a842f3d7f2b6d960e09fba5eb7bef
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-04-23 00:30:11 +0000

    [Extensions] Move IDR_EXTENSIONS_FAVICON resource
    Move the IDR_EXTENSIONS_FAVICON resource to //extensions. This will
    allow it to be used by files at //extensions, rather than just those
    at the //chrome layer.
    
    The favicon was also, implicitly, used for the plugins favicon. Keep
    a copy at the //chrome layer, but name it appropriately
    (favicon_plugins.png)
    
    Bug: 1073590
    Change-Id: Ide5b1ac2a147a515a5eaf38fba2ec24a6dc80e66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160347
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761729}

--- chrome/browser/task_manager/providers/web_contents/extension_task.cc
+++ chrome/browser/task_manager/providers/web_contents/extension_task.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/chrome_pages.h"
-#include "chrome/grit/theme_resources.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/process_manager.h"
@@ -19,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest_handlers/icons_handler.h"
 #include "extensions/common/view_type.h"
+#include "extensions/grit/extensions_browser_resources.h"
 
 namespace task_manager {
 
