4a1dde69ea6fac09482102728e7ec2f4b8b5846e
Akihiro Ota <akihiroota@chromium.org>
2020-04-23 20:58:10 +0000

    ChromeVox: Give phonetic readings for IME candidates.
    This change:
    1. Adds a new enum to ax_enums.mojom.
    2. Creates a new output rule for IME candidates.
    3. Adds a new method to PhoneticData to get phonetics for text.
    4. Cleans up the goog.require() statements in output.js.
    
    Bug: 1034146, 1056062
    Relnotes: ChromeVox gives phonetic readings for IME candidates.
    
    Change-Id: Idc620f2ef4b06dfcbf6f494ac900f92ee92831d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2151852
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762092}

--- third_party/closure_compiler/externs/automation.js
+++ third_party/closure_compiler/externs/automation.js
@@ -190,6 +190,7 @@ chrome.automation.RoleType = {
   IGNORED: 'ignored',
   IMAGE: 'image',
   IMAGE_MAP: 'imageMap',
+  IME_CANDIDATE: 'imeCandidate',
   INLINE_TEXT_BOX: 'inlineTextBox',
   INPUT_TIME: 'inputTime',
   KEYBOARD: 'keyboard',
--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -5221,6 +5221,7 @@ int AXPlatformNodeWin::MSAARole() {
       // hierarchy, matching the HWND.
       return ROLE_SYSTEM_PANE;
 
+    case ax::mojom::Role::kImeCandidate:
     case ax::mojom::Role::kIgnored:
     case ax::mojom::Role::kKeyboard:
     case ax::mojom::Role::kNone:
@@ -5776,6 +5777,10 @@ base::string16 AXPlatformNodeWin::UIAAriaRole() {
 
     case ax::mojom::Role::kImageMap:
       return L"document";
+
+    case ax::mojom::Role::kImeCandidate:
+      // Internal role, not used on Windows.
+      return L"group";
 
     case ax::mojom::Role::kInputTime:
       return L"group";
@@ -6704,6 +6709,7 @@ LONG AXPlatformNodeWin::ComputeUIAControlType() {  // NOLINT(runtime/int)
     case ax::mojom::Role::kPane:
     case ax::mojom::Role::kWindow:
     case ax::mojom::Role::kIgnored:
+    case ax::mojom::Role::kImeCandidate:
     case ax::mojom::Role::kKeyboard:
     case ax::mojom::Role::kNone:
     case ax::mojom::Role::kPresentational:
