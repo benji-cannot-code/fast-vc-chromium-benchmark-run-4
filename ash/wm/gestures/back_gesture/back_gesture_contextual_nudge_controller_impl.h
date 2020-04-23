f2fb814e4f2bbd77ca815f999c72872e31128340
Yulun Wu <yulunwu@chromium.org>
2020-04-23 19:52:46 +0000

    Hide back gesture nudge when shelf controls are visible.
    Enabling shelf controls disables gestures and should hide contextual
    nudges related to gestures.
    This CL changes the back gesture nudge controller to observe the shelf config and hide/disable the nudge when shelf controls are enabled.
    
    Bug: 1071248
    Change-Id: If43f22bb108660f8725b6d660e849d0317ddcd7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161734
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762067}

--- ash/wm/gestures/back_gesture/back_gesture_contextual_nudge_controller_impl.h
+++ ash/wm/gestures/back_gesture/back_gesture_contextual_nudge_controller_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/public/cpp/back_gesture_contextual_nudge_controller.h"
+#include "ash/public/cpp/shelf_config.h"
 #include "ash/public/cpp/tablet_mode_observer.h"
 #include "ash/session/session_observer.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
@@ -30,7 +31,8 @@ class ASH_EXPORT BackGestureContextualNudgeControllerImpl
     : public SessionObserver,
       public TabletModeObserver,
       public wm::ActivationChangeObserver,
-      public BackGestureContextualNudgeController {
+      public BackGestureContextualNudgeController,
+      public ShelfConfig::Observer {
  public:
   BackGestureContextualNudgeControllerImpl();
   BackGestureContextualNudgeControllerImpl(
@@ -60,6 +62,9 @@ class ASH_EXPORT BackGestureContextualNudgeControllerImpl
 
   // BackGestureContextualNudgeController:
   void NavigationEntryChanged(aura::Window* window) override;
+
+  // ShelfConfig::Observer:
+  void OnShelfConfigUpdated() override;
 
   bool is_monitoring_windows() const { return is_monitoring_windows_; }
   BackGestureContextualNudge* nudge() { return nudge_.get(); }
@@ -108,6 +113,9 @@ class ASH_EXPORT BackGestureContextualNudgeControllerImpl
 
   // True if we're currently monitoring window activation changes.
   bool is_monitoring_windows_ = false;
+
+  // Tracks the visibility of shelf controls.
+  bool shelf_control_visible_ = false;
 
   // Timer to automatically show nudge ui in 24 hours.
   base::OneShotTimer auto_show_timer_;
