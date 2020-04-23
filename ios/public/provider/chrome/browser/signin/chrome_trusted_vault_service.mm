7f93aadbeedba69a4ac38edc5edaee615ef35a81
Jérôme Lebel <jlebel@chromium.org>
2020-04-23 05:00:41 +0000

    [iOS] Reauth for trusted vault service
    Adding virtual methods in ChromeTrustedVaultService class to support
    re-authentication.
    
    To present the reauth UI:
      void ChromeTrustedVaultService::Reauthentication()
    To dismiss the reauth UI:
      void ChromeTrustedVaultService::CancelReauthentication()
    
    See ChromeTrustedVaultServiceImpl for the real implementation (in
    crrev.com/i/2863263).
    
    Related to:
    => crrev.com/c/2149330 Adding APIs in ChromeTrustedVaultService
       crrev.com/i/2863263 Implementation in ChromeTrustedVaultServiceImpl
       crrev.com/c/2153071 Adding coordinator to present the view
       crrev.com/c/2152788 Using the coordinator to present the view
    
    Bug: 1019685
    Change-Id: I935098793ead1f726d7f220e9bb886afb14baa7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2149330
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761800}

--- ios/public/provider/chrome/browser/signin/chrome_trusted_vault_service.mm
+++ ios/public/provider/chrome/browser/signin/chrome_trusted_vault_service.mm
@@ -17,10 +17,4 @@ ChromeTrustedVaultService::ChromeTrustedVaultService() {}
 
 ChromeTrustedVaultService::~ChromeTrustedVaultService() {}
 
-void ChromeTrustedVaultService::FetchKeys(
-    ChromeIdentity* chrome_identity,
-    base::OnceCallback<void(const TrustedVaultSharedKeyList&)> callback) {
-  NOTREACHED();
-}
-
 }  // namespace ios
