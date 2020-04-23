3296cb50751a9f4361b7932e1a21d4442dcc1a04
Asami Doi <asamidoi@chromium.org>
2020-04-23 08:09:07 +0000

    wpt: Add WPT tests for navigation preload and same site cookies.
    WPT tests to check if the navigation request handled by navigation
    preload contains cookies in its headers. There are 3 tests depending on
    the `SameSite` attribute.
    
    Bug: 920488
    Change-Id: I0a2c539443f9216c7d5fabd714e9cf4a3ff3d0a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147522
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761837}

---
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/navigation-preload/resources/cookie.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+def main(request, response):
+    """
+    Returns a response with a Set-Cookie header based on the query params.
+    The body will be "1" if the cookie is present in the request and `drop` parameter is "0",
+    otherwise the body will be "0".
+    """
+    same_site = request.GET.first("same-site")
+    cookie_name = request.GET.first("cookie-name")
+    drop = request.GET.first("drop")
+    cookie_in_request = "0"
+    cookie = "%s=1; Secure; SameSite=%s" % (cookie_name, same_site)
+
+    if drop == "1":
+        cookie += "; Max-Age=0"
+
+    if request.cookies.get(cookie_name):
+        cookie_in_request = request.cookies[cookie_name].value
+
+    headers = [('Content-Type', 'text/html'), ('Set-Cookie', cookie)]
+    return (200, headers, cookie_in_request)
