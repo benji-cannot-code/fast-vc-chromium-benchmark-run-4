12217934f6d02491d5625ce600be30cd113c8aff
Peng Huang <penghuang@chromium.org>
2020-04-23 15:26:06 +0000

    Fix memory and fps regressions
    The regressions is because we call sk_surface_->getCanvas(), it
    will create a SkCanvas and cache it. Creating SkCanvas is not cost
    free, so remove it for now.
    
    Bug: 1071557,1071481
    Change-Id: Ic21c8f066c4e4b3ece497bb9483f97bf012b462c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157286
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761939}

--- gpu/command_buffer/service/shared_image_backing_factory_ahardwarebuffer.cc
+++ gpu/command_buffer/service/shared_image_backing_factory_ahardwarebuffer.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/vulkan_image.h"
 #include "gpu/vulkan/vulkan_implementation.h"
 #include "gpu/vulkan/vulkan_util.h"
-#include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkPromiseImageTexture.h"
 #include "third_party/skia/include/gpu/GrBackendSemaphore.h"
 #include "third_party/skia/include/gpu/GrBackendSurface.h"
@@ -328,11 +327,6 @@ class SharedImageRepresentationSkiaVkAHB
       }
       surface_msaa_count_ = final_msaa_count;
     }
-
-    int count = surface_->getCanvas()->save();
-    DCHECK_EQ(count, 1);
-    ALLOW_UNUSED_LOCAL(count);
-
     return surface_;
   }
 
@@ -342,7 +336,12 @@ class SharedImageRepresentationSkiaVkAHB
 
     surface.reset();
     DCHECK(surface_->unique());
-    surface_->getCanvas()->restoreToCount(1);
+    // TODO(penghuang): reset canvas cached in |surface_|, when skia provides an
+    // API to do it.
+    // Currently, the |surface_| is only used with SkSurface::draw(ddl), it
+    // doesn't create a canvas and change the state of it, so we don't get any
+    // render issues. But we shouldn't assume this backing will only be used in
+    // this way.
     EndAccess(false /* readonly */);
   }
 
