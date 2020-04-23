bc4af2d20dadfc30c8e7eb673bdffd5b98887efd
Kenichi Ishibashi <bashi@chromium.org>
2020-04-23 01:58:07 +0000

    Add ServiceWorkerResponseWriter::WriteResponseHead()
    This method wraps WriteInfo() to hide HttpResponseInfo dependencies
    in ServiceWorkerDiskCache. Once ServiceWorkerDiskCache is moved into
    the Storage Service, //content side service worker implementation
    wouldn't depend on HttpResponseInfo.
    
    ServiceWorkerCacheWriter uses the new method.
    
    Bug: 1060076
    Change-Id: Ibc43c5fdaa441160a543d9f3d871eaf5bd73cbdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108211
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761771}

--- content/browser/service_worker/service_worker_disk_cache.h
+++ content/browser/service_worker/service_worker_disk_cache.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/appcache/appcache_disk_cache.h"
 #include "content/browser/appcache/appcache_disk_cache_ops.h"
 #include "content/common/content_export.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 
 namespace content {
 
@@ -38,6 +39,16 @@ class CONTENT_EXPORT ServiceWorkerResponseReader
 
 class CONTENT_EXPORT ServiceWorkerResponseWriter
     : public AppCacheResponseWriter {
+ public:
+  // Writes response headers for a service worker script to storage. Currently
+  // this just converts |response_head| to HttpResponseInfo and calls
+  // WriteInfo(). |response_head| must be examined by
+  // service_worker_loader_helpers::CheckResponseHead() before calling this
+  // method.
+  void WriteResponseHead(const network::mojom::URLResponseHead& response_head,
+                         int response_data_size,
+                         net::CompletionOnceCallback callback);
+
  protected:
   // Should only be constructed by the storage class.
   friend class ServiceWorkerStorage;
