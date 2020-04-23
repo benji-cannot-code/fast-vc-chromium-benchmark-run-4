debfe435b16a42cdfe3d107f9ef23bea02283143
Ankit Kumar <ankk@microsoft.com>
2020-04-23 09:35:29 +0000

    Forward tab events to PDF plugin from blink
    Blink brings plugin to focus but does not forward the tab event to
    plugin. Simulating tab to enable seamless tabbing across UI and plugin.
    
    Bug: 989046
    Change-Id: Ie156c66a25a87afe9b56083f98876cc6e75917e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2061636
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Commit-Queue: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#761853}

--- content/renderer/pepper/pepper_plugin_instance_impl.cc
+++ content/renderer/pepper/pepper_plugin_instance_impl.cc
@@ -2230,6 +2230,11 @@ void PepperPluginInstanceImpl::OnHiddenForPlaceholder(bool hidden) {
   UpdateLayer(false /* device_changed */);
 }
 
+bool PepperPluginInstanceImpl::SupportsKeyboardFocus() {
+  // Only PDF plugin supports keyboard focus.
+  return LoadPdfInterface();
+}
+
 void PepperPluginInstanceImpl::AddPluginObject(PluginObject* plugin_object) {
   DCHECK(live_plugin_objects_.find(plugin_object) ==
          live_plugin_objects_.end());
