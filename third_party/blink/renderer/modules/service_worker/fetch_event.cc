9a6f4b751a00ad0641504d13663de23e9819b67a
Minoru Chikamune <chikamune@chromium.org>
2020-04-23 11:31:18 +0000

    Migrate FetchEvent to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: I4ba32734f307ac5226a99171568b6ee02890e78c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160954
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#761875}

--- third_party/blink/renderer/modules/service_worker/fetch_event.cc
+++ third_party/blink/renderer/modules/service_worker/fetch_event.cc
@@ -96,7 +96,10 @@ FetchEvent::FetchEvent(ScriptState* script_state,
       observer_(respond_with_observer),
       preload_response_property_(MakeGarbageCollected<PreloadResponseProperty>(
           ExecutionContext::From(script_state))),
-      worker_timing_remote_(std::move(worker_timing_remote)) {
+      worker_timing_remote_(ExecutionContext::From(script_state)) {
+  worker_timing_remote_.Bind(std::move(worker_timing_remote),
+                             ExecutionContext::From(script_state)
+                                 ->GetTaskRunner(TaskType::kNetworking));
   if (!navigation_preload_sent)
     preload_response_property_->ResolveWithUndefined();
 
@@ -206,7 +209,7 @@ void FetchEvent::OnNavigationPreloadComplete(
 }
 
 void FetchEvent::addPerformanceEntry(PerformanceMark* performance_mark) {
-  if (worker_timing_remote_) {
+  if (worker_timing_remote_.is_bound()) {
     auto mojo_performance_mark =
         performance_mark->ToMojoPerformanceMarkOrMeasure();
     worker_timing_remote_->AddPerformanceEntry(
@@ -215,7 +218,7 @@ void FetchEvent::addPerformanceEntry(PerformanceMark* performance_mark) {
 }
 
 void FetchEvent::addPerformanceEntry(PerformanceMeasure* performance_measure) {
-  if (worker_timing_remote_) {
+  if (worker_timing_remote_.is_bound()) {
     auto mojo_performance_measure =
         performance_measure->ToMojoPerformanceMarkOrMeasure();
     worker_timing_remote_->AddPerformanceEntry(
@@ -228,6 +231,7 @@ void FetchEvent::Trace(Visitor* visitor) {
   visitor->Trace(request_);
   visitor->Trace(preload_response_property_);
   visitor->Trace(body_completion_notifier_);
+  visitor->Trace(worker_timing_remote_);
   ExtendableEvent::Trace(visitor);
   ExecutionContextClient::Trace(visitor);
 }
