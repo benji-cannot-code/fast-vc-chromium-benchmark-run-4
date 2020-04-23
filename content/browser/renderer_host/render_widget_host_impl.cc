ef3472d64522474a5df79bd2a2616c1d9c076d26
Antonio Gomes <tonikitoo@igalia.com>
2020-04-23 03:56:59 +0000

    Convert WidgetHostMsg_AnimateDoubleTapZoomInMainFrame to mojo
    Target interface: blink::mojom::FrameWidgetHost.
    
    BUG=1071597
    R=dtapuska@chromium.org
    
    Change-Id: I666dc15f80fbcac7c349f6dafd7fcb121c6cc8ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157249
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#761793}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -634,8 +634,6 @@ bool RenderWidgetHostImpl::OnMessageReceived(const IPC::Message &msg) {
                         OnHasTouchEventHandlers)
     IPC_MESSAGE_HANDLER(WidgetHostMsg_IntrinsicSizingInfoChanged,
                         OnIntrinsicSizingInfoChanged)
-    IPC_MESSAGE_HANDLER(WidgetHostMsg_AnimateDoubleTapZoomInMainFrame,
-                        OnAnimateDoubleTapZoomInMainFrame)
     IPC_MESSAGE_HANDLER(WidgetHostMsg_ZoomToFindInPageRectInMainFrame,
                         OnZoomToFindInPageRectInMainFrame)
     IPC_MESSAGE_UNHANDLED(handled = false)
@@ -3262,7 +3260,23 @@ bool TransformPointAndRectToRootView(RenderWidgetHostViewBase* view,
 
 }  // namespace
 
-void RenderWidgetHostImpl::OnAnimateDoubleTapZoomInMainFrame(
+void RenderWidgetHostImpl::OnZoomToFindInPageRectInMainFrame(
+    const gfx::Rect& rect_to_zoom) {
+  if (!view_)
+    return;
+
+  auto* root_view = view_->GetRootView();
+  gfx::Rect transformed_rect_to_zoom(rect_to_zoom);
+  if (!TransformPointAndRectToRootView(view_.get(), root_view, nullptr,
+                                       &transformed_rect_to_zoom)) {
+    return;
+  }
+
+  auto* root_rvhi = RenderViewHostImpl::From(root_view->GetRenderWidgetHost());
+  root_rvhi->ZoomToFindInPageRect(transformed_rect_to_zoom);
+}
+
+void RenderWidgetHostImpl::AnimateDoubleTapZoomInMainFrame(
     const gfx::Point& point,
     const gfx::Rect& rect_to_zoom) {
   if (!view_)
@@ -3279,22 +3293,6 @@ void RenderWidgetHostImpl::OnAnimateDoubleTapZoomInMainFrame(
 
   auto* root_rvhi = RenderViewHostImpl::From(root_view->GetRenderWidgetHost());
   root_rvhi->AnimateDoubleTapZoom(transformed_point, transformed_rect_to_zoom);
-}
-
-void RenderWidgetHostImpl::OnZoomToFindInPageRectInMainFrame(
-    const gfx::Rect& rect_to_zoom) {
-  if (!view_)
-    return;
-
-  auto* root_view = view_->GetRootView();
-  gfx::Rect transformed_rect_to_zoom(rect_to_zoom);
-  if (!TransformPointAndRectToRootView(view_.get(), root_view, nullptr,
-                                       &transformed_rect_to_zoom)) {
-    return;
-  }
-
-  auto* root_rvhi = RenderViewHostImpl::From(root_view->GetRenderWidgetHost());
-  root_rvhi->ZoomToFindInPageRect(transformed_rect_to_zoom);
 }
 
 gfx::Size RenderWidgetHostImpl::GetRootWidgetViewportSize() {
--- third_party/blink/renderer/core/frame/web_frame_widget_base.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_base.cc
@@ -372,6 +372,11 @@ Page* WebFrameWidgetBase::GetPage() const {
   return View()->GetPage();
 }
 
+const mojo::AssociatedRemote<mojom::blink::FrameWidgetHost>&
+WebFrameWidgetBase::GetAssociatedFrameWidgetHost() const {
+  return frame_widget_host_;
+}
+
 void WebFrameWidgetBase::DidAcquirePointerLock() {
   GetPage()->GetPointerLockController().DidAcquirePointerLock();
 
