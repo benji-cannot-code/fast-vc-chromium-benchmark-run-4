1bf8ed0bb183211db1031dc6046fe242fd3be61e
Jesse Schettler <jschettler@chromium.org>
2020-04-23 23:21:18 +0000

    arcvm: Move FakeCrosConfig
    Move the FakeCrosConfig class into its own .cc/.h files so that it can
    be used by other tests and fuzzers.
    
    Bug: b:148803654
    Test: components_unittests --gtest_filter=ArcPropertyUtilTest.*
    Change-Id: I3bddeff8d55fe741290e5c766703b574c9d8bf58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163729
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762163}

---
+++ components/arc/test/fake_cros_config.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/arc/test/fake_cros_config.h"
+
+namespace arc {
+
+FakeCrosConfig::FakeCrosConfig() = default;
+
+FakeCrosConfig::~FakeCrosConfig() = default;
+
+bool FakeCrosConfig::GetString(const std::string& path,
+                               const std::string& property,
+                               std::string* val_out) {
+  auto it = overrides_.find(property);
+  if (it != overrides_.end()) {
+    *val_out = it->second;
+    return true;
+  }
+  return arc::CrosConfig::GetString(path, property, val_out);
+}
+
+void FakeCrosConfig::SetString(const std::string& path,
+                               const std::string& property,
+                               const std::string& value) {
+  overrides_.emplace(property, value);
+}
+
+}  // namespace arc
