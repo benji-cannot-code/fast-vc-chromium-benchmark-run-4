a820060030261761f13075c997ac416c648e0285
Xing Liu <xingliu@chromium.org>
2020-04-23 22:53:13 +0000

    Query Tiles: Removes image id.
    This is not needed. The only data needed by image cache is the URL.
    
    Bug: 1058534
    Change-Id: I7eecc924923efdf842f3475f82406d449187ca84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163158
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762150}

--- chrome/browser/upboarding/query_tiles/tile.h
+++ chrome/browser/upboarding/query_tiles/tile.h
@@ -15,18 +15,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace upboarding {
 
-// Metadata of an query tile image.
+// Metadata of a tile image.
 struct ImageMetadata {
   ImageMetadata();
-  ImageMetadata(const std::string& id, const GURL& url);
+  explicit ImageMetadata(const GURL& url);
   ~ImageMetadata();
   ImageMetadata(const ImageMetadata& other);
   bool operator==(const ImageMetadata& other) const;
 
-  // Unique Id for image.
-  std::string id;
-
-  // Origin URL the image fetched from.
+  // URL of the image.
   GURL url;
 };
 
