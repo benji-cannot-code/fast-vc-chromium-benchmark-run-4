6b16e2eaa77f16ab493b0f90dd8c567769e5a20b
Johann <johannkoenig@google.com>
2020-04-23 12:57:29 +0000

    heap mojo: migrate gamepad_monitor_remote_
    Use HeapMojoRemote wrapper so that Oilpan will GC the object.
    
    BUG=chromium:1049056
    
    Change-Id: Ibd29c294c0d4cb17144f49c3f82c514e794b21d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147976
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#761891}

--- third_party/blink/renderer/modules/gamepad/gamepad_shared_memory_reader.cc
+++ third_party/blink/renderer/modules/gamepad/gamepad_shared_memory_reader.cc
@@ -18,29 +18,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 GamepadSharedMemoryReader::GamepadSharedMemoryReader(LocalFrame& frame)
-    : receiver_(this, frame.DomWindow()) {
-  frame.GetBrowserInterfaceBroker().GetInterface(
-      gamepad_monitor_remote_.BindNewPipeAndPassReceiver());
+    : receiver_(this, frame.DomWindow()),
+      gamepad_monitor_remote_(frame.DomWindow()) {
   // See https://bit.ly/2S0zRAS for task types
   scoped_refptr<base::SingleThreadTaskRunner> task_runner =
       frame.GetTaskRunner(TaskType::kMiscPlatformAPI);
+  frame.GetBrowserInterfaceBroker().GetInterface(
+      gamepad_monitor_remote_.BindNewPipeAndPassReceiver(task_runner));
   gamepad_monitor_remote_->SetObserver(
       receiver_.BindNewPipeAndPassRemote(task_runner));
 }
 
 void GamepadSharedMemoryReader::Trace(Visitor* visitor) {
   visitor->Trace(receiver_);
+  visitor->Trace(gamepad_monitor_remote_);
 }
 
 void GamepadSharedMemoryReader::SendStartMessage() {
-  if (gamepad_monitor_remote_) {
+  if (gamepad_monitor_remote_.is_bound()) {
     gamepad_monitor_remote_->GamepadStartPolling(
         &renderer_shared_buffer_region_);
   }
 }
 
 void GamepadSharedMemoryReader::SendStopMessage() {
-  if (gamepad_monitor_remote_) {
+  if (gamepad_monitor_remote_.is_bound()) {
     gamepad_monitor_remote_->GamepadStopPolling();
   }
 }
--- third_party/blink/renderer/modules/gamepad/gamepad_shared_memory_reader.h
+++ third_party/blink/renderer/modules/gamepad/gamepad_shared_memory_reader.h
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "device/gamepad/public/mojom/gamepad.mojom-blink.h"
 #include "device/gamepad/public/mojom/gamepad_hardware_buffer.h"
-#include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/system/buffer.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 
 namespace base {
@@ -70,7 +70,9 @@ class GamepadSharedMemoryReader
                    GamepadSharedMemoryReader,
                    HeapMojoWrapperMode::kWithoutContextObserver>
       receiver_;
-  mojo::Remote<device::mojom::blink::GamepadMonitor> gamepad_monitor_remote_;
+  HeapMojoRemote<device::mojom::blink::GamepadMonitor,
+                 HeapMojoWrapperMode::kWithoutContextObserver>
+      gamepad_monitor_remote_;
   blink::GamepadListener* listener_ = nullptr;
 };
 
