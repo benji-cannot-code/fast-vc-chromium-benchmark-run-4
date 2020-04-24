fcd45fec38058685d72a65ae2831147cf6ebcb6b
Wez <wez@chromium.org>
2020-04-24 17:31:17 +0000

    [blink] Initialize null, about and src-doc KURL statics per-thread.
    Blink has static null, "about" and src-doc instances of KURL. These were
    previously non-thread-safe, leading to unsafe initialization, and to
    unsafe post-initialization arising from the non-thread-safe Strings
    inside each KURL, which are referenced when the KURL is copy-constructed
    from an existing instance.
    
    This resolves thread-check failures in DCHECK-enabled builds and may
    also resolve some small number of crashes in production, since there is
    no longer unsafe ref-counting going on.
    
    Bug: 1049834
    Change-Id: I9b3b0e9af8a59d182dabba3d3b47611b1f44bfc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162734
    Commit-Queue: Wez <wez@chromium.org>
    Auto-Submit: Wez <wez@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762406}

--- third_party/blink/renderer/platform/weborigin/kurl_test.cc
+++ third_party/blink/renderer/platform/weborigin/kurl_test.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/stl_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "url/url_util.h"
@@ -872,6 +873,43 @@ TEST(KURLTest, strippedForUseAsReferrer) {
     const KURL kurl(referrer_cases[i].input);
     EXPECT_EQ(referrer_cases[i].output, kurl.StrippedForUseAsReferrer().Utf8());
   }
+}
+
+TEST(KURLTest, ThreadSafesStaticKurlGetters) {
+#if DCHECK_IS_ON()
+  // Simulate the static getters being called during/after threads have been
+  // started, so that StaticSingleton's thread checks will be applied.
+  WTF::WillCreateThread();
+#endif
+
+  // Take references to the static KURLs, so that each has two references to
+  // its internal StringImpl, rather than one.
+  KURL blank_url = BlankURL();
+  EXPECT_FALSE(blank_url.IsEmpty());
+  KURL srcdoc_url = SrcdocURL();
+  EXPECT_FALSE(srcdoc_url.IsEmpty());
+  KURL null_url = NullURL();
+  EXPECT_TRUE(null_url.IsNull());
+
+  auto thread =
+      Thread::CreateThread(ThreadCreationParams(ThreadType::kTestThread));
+  thread->GetTaskRunner()->PostTask(FROM_HERE, base::BindOnce([]() {
+                                      // Reference each of the static KURLs
+                                      // again, from the background thread,
+                                      // which should succeed without thread
+                                      // verifier checks firing.
+                                      KURL blank_url = BlankURL();
+                                      EXPECT_FALSE(blank_url.IsEmpty());
+                                      KURL srcdoc_url = SrcdocURL();
+                                      EXPECT_FALSE(srcdoc_url.IsEmpty());
+                                      KURL null_url = NullURL();
+                                      EXPECT_TRUE(null_url.IsNull());
+                                    }));
+
+#if DCHECK_IS_ON()
+  // Restore the IsBeforeThreadCreated() flag.
+  WTF::SetIsBeforeThreadCreatedForTest();
+#endif
 }
 
 enum class PortIsValid {
