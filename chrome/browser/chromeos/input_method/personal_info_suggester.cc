59fa25adbcc95fb64e360df066d54cb7b2ea4cb2
Jing Wang <jiwan@chromium.org>
2020-04-24 02:01:33 +0000

    Add unit tests for PersonalInfoSuggester
    Run the test: autoninja -C out/Default unit_tests &&
    out/Default/unit_tests --gtest_filter="PersonalInfoSuggesterTest*"
    
    Test: tested with Linux emulator.
    Bug: 1042084
    Change-Id: I79473502d84c6e3a2a24aa69fbfb136cc478658a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157331
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762221}

--- chrome/browser/chromeos/input_method/personal_info_suggester.cc
+++ chrome/browser/chromeos/input_method/personal_info_suggester.cc
@@ -47,12 +47,16 @@ AssistiveType ProposeAssistiveAction(const base::string16& text) {
   return action;
 }
 
-PersonalInfoSuggester::PersonalInfoSuggester(InputMethodEngine* engine,
-                                             Profile* profile)
-    : engine_(engine),
+PersonalInfoSuggester::PersonalInfoSuggester(
+    SuggestionHandlerInterface* suggestion_handler,
+    Profile* profile,
+    autofill::PersonalDataManager* personal_data_manager)
+    : suggestion_handler_(suggestion_handler),
       profile_(profile),
       personal_data_manager_(
-          autofill::PersonalDataManagerFactory::GetForProfile(profile)) {}
+          personal_data_manager
+              ? personal_data_manager
+              : autofill::PersonalDataManagerFactory::GetForProfile(profile)) {}
 
 PersonalInfoSuggester::~PersonalInfoSuggester() {}
 
@@ -146,7 +150,8 @@ void PersonalInfoSuggester::ShowSuggestion(const base::string16& text,
                                            const size_t confirmed_length) {
   std::string error;
   suggestion_shown_ = true;
-  engine_->SetSuggestion(context_id_, text, confirmed_length, true, &error);
+  suggestion_handler_->SetSuggestion(context_id_, text, confirmed_length, true,
+                                     &error);
   if (!error.empty()) {
     LOG(ERROR) << "Fail to show suggestion. " << error;
   }
@@ -159,7 +164,7 @@ AssistiveType PersonalInfoSuggester::GetProposeActionType() {
 void PersonalInfoSuggester::AcceptSuggestion() {
   std::string error;
   suggestion_shown_ = false;
-  engine_->AcceptSuggestion(context_id_, &error);
+  suggestion_handler_->AcceptSuggestion(context_id_, &error);
   if (!error.empty()) {
     LOG(ERROR) << "Failed to accept suggestion. " << error;
   }
@@ -168,7 +173,7 @@ void PersonalInfoSuggester::AcceptSuggestion() {
 void PersonalInfoSuggester::DismissSuggestion() {
   std::string error;
   suggestion_shown_ = false;
-  engine_->DismissSuggestion(context_id_, &error);
+  suggestion_handler_->DismissSuggestion(context_id_, &error);
   if (!error.empty()) {
     LOG(ERROR) << "Failed to dismiss suggestion. " << error;
   }
--- chrome/browser/chromeos/input_method/personal_info_suggester.h
+++ chrome/browser/chromeos/input_method/personal_info_suggester.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "chrome/browser/chromeos/input_method/input_method_engine.h"
 #include "chrome/browser/chromeos/input_method/suggester.h"
 #include "chrome/browser/chromeos/input_method/suggestion_enums.h"
+#include "chrome/browser/chromeos/input_method/suggestion_handler_interface.h"
 #include "chrome/browser/ui/input_method/input_method_engine_base.h"
 
 namespace autofill {
@@ -27,7 +27,12 @@ AssistiveType ProposeAssistiveAction(const base::string16& text);
 // dismiss the suggestion according to the user action.
 class PersonalInfoSuggester : public Suggester {
  public:
-  explicit PersonalInfoSuggester(InputMethodEngine* engine, Profile* profile);
+  // If |personal_data_manager| is nullptr, we will obtain it from
+  // |PersonalDataManagerFactory| according to |profile|.
+  PersonalInfoSuggester(
+      SuggestionHandlerInterface* suggestion_handler,
+      Profile* profile,
+      autofill::PersonalDataManager* personal_data_manager = nullptr);
   ~PersonalInfoSuggester() override;
 
   // Suggester overrides:
@@ -49,7 +54,7 @@ class PersonalInfoSuggester : public Suggester {
 
   void AcceptSuggestion();
 
-  InputMethodEngine* const engine_;
+  SuggestionHandlerInterface* const suggestion_handler_;
 
   // ID of the focused text field, 0 if none is focused.
   int context_id_ = -1;
