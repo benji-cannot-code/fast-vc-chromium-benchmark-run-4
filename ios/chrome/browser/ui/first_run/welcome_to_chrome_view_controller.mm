cfd12dee1e4ee4f0acf11d034da7f905193c2ef3
edchin <edchin@chromium.org>
2020-04-24 23:54:27 +0000

    [iOS] FRE TOS string change
    Required by Legal.
    
    Change-Id: I958992ce2dc614338bbc15789865cc6cac76ee5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165711
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762593}

--- ios/chrome/browser/ui/first_run/welcome_to_chrome_view_controller.mm
+++ ios/chrome/browser/ui/first_run/welcome_to_chrome_view_controller.mm
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #include "ios/chrome/common/string_util.h"
-#include "ios/chrome/grit/ios_chromium_strings.h"
+#include "ios/chrome/grit/ios_strings.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 #include "net/base/mac/url_conversions.h"
