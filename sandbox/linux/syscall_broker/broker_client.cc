93639cafe8a9d1b26045b32fa0c264c14efd726a
Piotr Tworek <ptworek@vewd.com>
2020-04-24 16:06:32 +0000

    Add a bunch of missing string.h / cstring includes in //sandbox/linux.
    There is a lot of code in the tree that uses various functions defined
    in string.h (memcpy, strlen, strnlen, memmove, etc), but never includes
    the necessary header file. At least on Linux it still works fine with
    bundled sysroots, or when using system headers based on glibc 2.29.
    Starting with glibc 2.30 this is no longer the case. There are quite a
    few files which fail to compile because the compiler can't see the
    definitions of some functions normally defined in string.h.
    
    Bug: 1074286
    Change-Id: I0c4956ec1ad53596d76d2ed3355462a739324a12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164647
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#762381}

--- sandbox/linux/syscall_broker/broker_client.cc
+++ sandbox/linux/syscall_broker/broker_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 
