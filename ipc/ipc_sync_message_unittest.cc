36e68cbd7dedd6b7184e614577338640a511d211
Wenbin Zhang <wenbinzhang@google.com>
2020-04-24 21:47:20 +0000

    Android builders on perf waterfall started failing since this commit. Reverting this change to mitigate.
    https://ci.chromium.org/p/chrome/g/chrome.perf/console?limit=200
    
    Revert "Remove/replace unnecessary logging.h includes in .cc files (ipc)"
    
    This reverts commit ba6e417c4da5629fb66f27fddf7d159f548a59bd.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Remove/replace unnecessary logging.h includes in .cc files (ipc)
    > 
    > CHECK, CHECK_EQ etc., and NOTREACHED/NOTIMPLEMENTED have moved
    > to the much smaller headers check.h, check_op.h, and notreached.h,
    > respectively.
    > 
    > This CL updates .cc files to use those headers instead when
    > possible, with the purpose of saving compile time.
    > 
    > (Split out from https://crrev.com/c/2164525 which also has
    > notes on how the change was generated.)
    > 
    > Bug: 1031540
    > Change-Id: I9b41171cc43f427005f9a9d78e5cd61341ebeb40
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164512
    > Commit-Queue: Tom Sepez <tsepez@chromium.org>
    > Auto-Submit: Hans Wennborg <hans@chromium.org>
    > Reviewed-by: Tom Sepez <tsepez@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#762412}
    
    TBR=hans@chromium.org,tsepez@chromium.org
    
    Change-Id: I21ebe546731e74bec8178d6bca889c1aeb4239c0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1031540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165458
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/master@{#762523}

--- ipc/ipc_sync_message_unittest.cc
+++ ipc/ipc_sync_message_unittest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // works.  This ensures that the macros and templates were defined correctly.
 // Doesn't test the IPC channel mechanism.
 
-#include "base/check_op.h"
+#include "base/logging.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
