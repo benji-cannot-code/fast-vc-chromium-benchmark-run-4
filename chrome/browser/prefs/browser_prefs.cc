202d501b03877c4fe9932b73d0c23f1ee9ed7681
Anastasiia Nikolaienko <anastasiian@chromium.org>
2020-04-24 14:17:03 +0000

    Move secondary account consent logger
    Move secondary_account_consent_logger to
    chrome/browser/chromeos/child_accounts/ folder.
    
    Change-Id: I64dbc8269ffb6c64b1b8e6819b2c447923f348f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160908
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762349}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -279,6 +279,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(USE_CUPS)
 #include "chrome/browser/chromeos/extensions/printing/printing_api_handler.h"
 #endif
+#include "chrome/browser/chromeos/child_accounts/secondary_account_consent_logger.h"
 #include "chrome/browser/chromeos/file_system_provider/registry.h"
 #include "chrome/browser/chromeos/first_run/first_run.h"
 #include "chrome/browser/chromeos/guest_os/guest_os_pref_names.h"
@@ -327,7 +328,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_assets_manager_chromeos.h"
 #include "chrome/browser/media/protected_media_identifier_permission_context.h"
 #include "chrome/browser/metrics/chromeos_metrics_provider.h"
-#include "chrome/browser/supervised_user/child_accounts/secondary_account_consent_logger.h"
 #include "chrome/browser/ui/app_list/app_list_syncable_service.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ui/app_list/search/arc/arc_app_reinstall_search_provider.h"
