82f9896a42b7e94766db9556b14a5046e58ae232
Minoru Chikamune <chikamune@chromium.org>
2020-04-24 00:14:49 +0000

    Migrate BeforeInstallPromptEvent to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: Ida25a173d6b6083c8f1b03f1d12abbbebc58a194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162429
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762182}

--- third_party/blink/renderer/modules/app_banner/before_install_prompt_event.h
+++ third_party/blink/renderer/modules/app_banner/before_install_prompt_event.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_APP_BANNER_BEFORE_INSTALL_PROMPT_EVENT_H_
 
 #include <utility>
-#include "mojo/public/cpp/bindings/receiver.h"
-#include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/app_banner/app_banner.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/active_script_wrappable.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
@@ -17,6 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/modules/event_modules.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 
 namespace blink {
 
@@ -33,7 +34,6 @@ class BeforeInstallPromptEvent final
       public ActiveScriptWrappable<BeforeInstallPromptEvent>,
       public ExecutionContextClient {
   DEFINE_WRAPPERTYPEINFO();
-  USING_PRE_FINALIZER(BeforeInstallPromptEvent, Dispose);
   USING_GARBAGE_COLLECTED_MIXIN(BeforeInstallPromptEvent);
 
  public:
@@ -66,8 +66,6 @@ class BeforeInstallPromptEvent final
                                                           name, init);
   }
 
-  void Dispose();
-
   Vector<String> platforms() const;
   ScriptPromise userChoice(ScriptState*, ExceptionState&);
   ScriptPromise prompt(ScriptState*, ExceptionState&);
@@ -85,8 +83,13 @@ class BeforeInstallPromptEvent final
   void BannerAccepted(const String& platform) override;
   void BannerDismissed() override;
 
-  mojo::Remote<mojom::blink::AppBannerService> banner_service_remote_;
-  mojo::Receiver<mojom::blink::AppBannerEvent> receiver_{this};
+  HeapMojoRemote<mojom::blink::AppBannerService,
+                 HeapMojoWrapperMode::kWithoutContextObserver>
+      banner_service_remote_;
+  HeapMojoReceiver<mojom::blink::AppBannerEvent,
+                   BeforeInstallPromptEvent,
+                   HeapMojoWrapperMode::kWithoutContextObserver>
+      receiver_;
   Vector<String> platforms_;
   Member<UserChoiceProperty> user_choice_;
 };
