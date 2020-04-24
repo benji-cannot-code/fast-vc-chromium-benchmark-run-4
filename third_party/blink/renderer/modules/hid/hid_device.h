ab6f1acacf45c803cbbdbd1e22cf8c0ae2ae99f4
Minoru Chikamune <chikamune@chromium.org>
2020-04-24 00:17:59 +0000

    Migrate HIDDevice to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: I02e8a42e40d62212dc709ab68d6b91c568b2f585
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162428
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762185}

--- third_party/blink/renderer/modules/hid/hid_device.h
+++ third_party/blink/renderer/modules/hid/hid_device.h
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_HID_HID_DEVICE_H_
 
 #include "mojo/public/cpp/bindings/pending_remote.h"
-#include "mojo/public/cpp/bindings/receiver.h"
-#include "mojo/public/cpp/bindings/remote.h"
 #include "services/device/public/mojom/hid.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/hid/hid.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/array_buffer_or_array_buffer_view.h"
@@ -19,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/heap_allocator.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -36,7 +36,6 @@ class MODULES_EXPORT HIDDevice
       public device::mojom::blink::HidConnectionClient {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(HIDDevice);
-  USING_PRE_FINALIZER(HIDDevice, Dispose);
 
  public:
   HIDDevice(HID* parent,
@@ -74,7 +73,6 @@ class MODULES_EXPORT HIDDevice
   void ContextDestroyed() override;
 
   void Trace(Visitor*) override;
-  void Dispose();
 
  private:
   bool EnsureNoDeviceChangeInProgress(ScriptPromiseResolver* resolver) const;
@@ -98,8 +96,9 @@ class MODULES_EXPORT HIDDevice
 
   Member<HID> parent_;
   device::mojom::blink::HidDeviceInfoPtr device_info_;
-  mojo::Remote<device::mojom::blink::HidConnection> connection_;
-  mojo::Receiver<device::mojom::blink::HidConnectionClient> receiver_{this};
+  HeapMojoRemote<device::mojom::blink::HidConnection> connection_;
+  HeapMojoReceiver<device::mojom::blink::HidConnectionClient, HIDDevice>
+      receiver_;
   HeapHashSet<Member<ScriptPromiseResolver>> device_requests_;
   HeapVector<Member<HIDCollectionInfo>> collections_;
   bool device_state_change_in_progress_ = false;
