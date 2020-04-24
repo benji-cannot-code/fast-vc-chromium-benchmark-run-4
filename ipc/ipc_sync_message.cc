101942eaabcc2c83a2e35f8b47d619aa92017992
Hans Wennborg <hans@chromium.org>
2020-04-24 17:39:09 +0000

    Remove/replace unnecessary logging.h includes in .cc files (ipc)
    CHECK, CHECK_EQ etc., and NOTREACHED/NOTIMPLEMENTED have moved
    to the much smaller headers check.h, check_op.h, and notreached.h,
    respectively.
    
    This CL updates .cc files to use those headers instead when
    possible, with the purpose of saving compile time.
    
    (Split out from https://crrev.com/c/2164525 which also has
    notes on how the change was generated.)
    
    Bug: 1031540
    Change-Id: I9b41171cc43f427005f9a9d78e5cd61341ebeb40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164512
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762412}

--- ipc/ipc_sync_message.cc
+++ ipc/ipc_sync_message.cc
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/atomic_sequence_num.h"
-#include "base/logging.h"
+#include "base/check.h"
+#include "base/notreached.h"
 #include "build/build_config.h"
 
 namespace {
