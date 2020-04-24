16d8950a7f501c427bd43e3183dfb8bb3f4ed08d
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-04-24 13:44:30 +0000

    [iOS][Credential-Provider] Move account validator to common 3/3
    Moves files to common to be used in Chrome.
    
    Bug: 1066803
    Change-Id: I931d8394fd673982d9f5267d723bb2ef5b2c0212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160987
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762340}

--- ios/chrome/credential_provider_extension/account_verification_provider.h
+++
@@ -1,3 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Compatibility file. To be removed after internal is updated.
-#import "ios/chrome/common/credential_provider/account_verification_provider.h"
