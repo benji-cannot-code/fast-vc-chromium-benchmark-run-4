caf5628f9c1d6882062c23c2f93f6738fa98ebac
Jinsuk Kim <jinsukkim@chromium.org>
2020-04-24 02:45:29 +0000

    Android: TabDelegateFactory.createNativePage
    Replaces TabDelegateFactory.getNativePageFactory with createNativePage,
    and makes NativePageFactory an implementation detail. This is to remove
    TabDelegateFactory's dependency on the target chrome_java.
    
    Bug: 952703
    Change-Id: Id5e45f52d4826676618d4ee98415a040830ba1b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159157
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762231}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabDelegateFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabDelegateFactory.java
@@ -35,7 +35,6 @@ import org.chromium.chrome.browser.externalauth.ExternalAuthUtils;
 import org.chromium.chrome.browser.externalnav.ExternalNavigationDelegateImpl;
 import org.chromium.chrome.browser.flags.ActivityType;
 import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
-import org.chromium.chrome.browser.native_page.NativePageFactory;
 import org.chromium.chrome.browser.share.ShareDelegate;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabAssociatedApp;
@@ -48,6 +47,7 @@ import org.chromium.chrome.browser.tab_activity_glue.ActivityTabWebContentsDeleg
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.document.AsyncTabCreationParams;
 import org.chromium.chrome.browser.tabmodel.document.TabDelegate;
+import org.chromium.chrome.browser.ui.native_page.NativePage;
 import org.chromium.chrome.browser.webapps.WebDisplayMode;
 import org.chromium.chrome.browser.webapps.WebappActivity;
 import org.chromium.chrome.browser.webapps.WebappExtras;
@@ -476,7 +476,7 @@ public class CustomTabDelegateFactory implements TabDelegateFactory {
     }
 
     @Override
-    public NativePageFactory getNativePageFactory() {
+    public NativePage createNativePage(String url, NativePage candidatePage, Tab tab) {
         // Custom tab does not create native pages.
         return null;
     }
