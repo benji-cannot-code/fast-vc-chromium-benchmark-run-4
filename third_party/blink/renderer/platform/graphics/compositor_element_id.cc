0aca82aba6cf99100d8da722997b85c70b9261a2
Piotr Tworek <ptworek@vewd.com>
2020-04-24 14:58:02 +0000

    Add missing limits include in compositor_element_id.cc
    This is required for std::numeric_limits which the code uses. Without
    the header the code fails to build against recent versions of libstdc++.
    
    Change-Id: Ibf03e3b26dd0a9a659527c87f92661809f4c1905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164630
    Reviewed-by: Saman Sami <samans@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#762361}

--- third_party/blink/renderer/platform/graphics/compositor_element_id.cc
+++ third_party/blink/renderer/platform/graphics/compositor_element_id.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/compositor_element_id.h"
 
+#include <limits>
+
 namespace blink {
 
 UniqueObjectId NewUniqueObjectId() {
