98ab6792707f4e7048a8b769a9b84f838a45d55c
Etienne Bergeron <etienneb@chromium.org>
2020-04-24 03:37:49 +0000

    Add trace event for TimeChange notification
    Add a trace event to ease investigation with slow-reports.
    
    R=gab@chromium.org, mark@chromium.org
    CC=joenotcharles@chromium.org
    
    Bug: 1074036
    Change-Id: I0725591990ebc35bb4b004bb20d4434ba4cd7a9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163810
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762239}

--- services/device/time_zone_monitor/time_zone_monitor_win.cc
+++ services/device/time_zone_monitor/time_zone_monitor_win.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/macros.h"
+#include "base/trace_event/trace_event.h"
 #include "third_party/icu/source/i18n/unicode/timezone.h"
 #include "ui/gfx/win/singleton_hwnd_observer.h"
 
@@ -32,7 +33,7 @@ class TimeZoneMonitorWin : public TimeZoneMonitor {
     if (message != WM_TIMECHANGE) {
       return;
     }
-
+    TRACE_EVENT0("browser", "TimeZoneMonitorWin::UpdateIcuAndNotifyClients");
     UpdateIcuAndNotifyClients(DetectHostTimeZoneFromIcu());
   }
 
