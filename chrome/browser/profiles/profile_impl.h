74b026a0fbd01aa6dcd58fc15642bb8d69157627
Ramin Halavati <rhalavati@chromium.org>
2020-04-24 05:28:55 +0000

    Remove IndependentOTRProfileManager.
    All use cases of IndependentOTRProfileManager are moved to the new APIs.
    The class and and its tests are removed.
    |IsIndependentOffTheRecordProfile| is replaced with
    |!IsPrimaryOTRProfile|.
    
    [This CL doesn't change any behavior.]
    
    Bug: 1033903
    Change-Id: I2ed2d3b3ad6283e89c5e8549cb2feb98c4783cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159718
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Brandon Tolsch <btolsch@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762262}

--- chrome/browser/profiles/profile_impl.h
+++ chrome/browser/profiles/profile_impl.h
@@ -131,7 +131,6 @@ class ProfileImpl : public Profile {
   bool IsSupervised() const override;
   bool IsChild() const override;
   bool IsLegacySupervised() const override;
-  bool IsIndependentOffTheRecordProfile() override;
   bool AllowsBrowserWindows() const override;
   ExtensionSpecialStoragePolicy* GetExtensionSpecialStoragePolicy() override;
   PrefService* GetPrefs() override;
