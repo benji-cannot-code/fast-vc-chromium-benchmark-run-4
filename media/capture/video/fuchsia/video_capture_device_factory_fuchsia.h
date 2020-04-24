6b5ae4d75c14be1d413ffab4265e34842d386b54
Sergey Ulanov <sergeyu@chromium.org>
2020-04-24 22:50:51 +0000

    [Reland][Fuchsia] Implement camera device enumeration
    Added camera device enumeration in VideoCaptureDeviceFactoryFuchsia
    using fuchsia.camera3 API.
    Also updated CMX files for runners to list fuchsia.camera3.DeviceWatcher
    service.
    
    Originally review
    https://chromium-review.googlesource.com/c/chromium/src/+/2157870
    
    TBR=ddorwin@chromium.org, nasko@chromium.org, guidou@chromium.org
    
    Bug: 1064731
    Change-Id: I884c0335ff10b2b4445d1037d8794590914facae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166342
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762560}

--- media/capture/video/fuchsia/video_capture_device_factory_fuchsia.h
+++ media/capture/video/fuchsia/video_capture_device_factory_fuchsia.h
@@ -6,6 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_CAPTURE_VIDEO_FUCHSIA_VIDEO_CAPTURE_DEVICE_FACTORY_FUCHSIA_H_
 #define MEDIA_CAPTURE_VIDEO_FUCHSIA_VIDEO_CAPTURE_DEVICE_FACTORY_FUCHSIA_H_
 
+#include <fuchsia/camera3/cpp/fidl.h>
+
+#include <map>
+
+#include "base/containers/small_map.h"
+#include "base/optional.h"
+#include "base/run_loop.h"
 #include "media/capture/video/video_capture_device_factory.h"
 
 namespace media {
@@ -16,6 +23,12 @@ class CAPTURE_EXPORT VideoCaptureDeviceFactoryFuchsia
   VideoCaptureDeviceFactoryFuchsia();
   ~VideoCaptureDeviceFactoryFuchsia() override;
 
+  VideoCaptureDeviceFactoryFuchsia(const VideoCaptureDeviceFactoryFuchsia&) =
+      delete;
+  VideoCaptureDeviceFactoryFuchsia& operator=(
+      const VideoCaptureDeviceFactoryFuchsia&) = delete;
+
+  // VideoCaptureDeviceFactory implementation.
   std::unique_ptr<VideoCaptureDevice> CreateDevice(
       const VideoCaptureDeviceDescriptor& device_descriptor) override;
   void GetDeviceDescriptors(
@@ -24,7 +37,26 @@ class CAPTURE_EXPORT VideoCaptureDeviceFactoryFuchsia
                            VideoCaptureFormats* supported_formats) override;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(VideoCaptureDeviceFactoryFuchsia);
+  // Helper class used to fetch per-device information.
+  class DeviceInfoFetcher;
+
+  void Initialize();
+
+  void OnDeviceWatcherDisconnected(zx_status_t status);
+
+  void WatchDevices();
+  void OnWatchDevicesResult(
+      std::vector<fuchsia::camera3::WatchDevicesEvent> events);
+
+  fuchsia::camera3::DeviceWatcherPtr device_watcher_;
+  base::small_map<std::map<uint64_t, std::unique_ptr<DeviceInfoFetcher>>>
+      devices_;
+
+  // RunLoop used to wait for the first WatchDevices() response. Currently
+  // required because GetDeviceDescriptors() is synchronous.
+  // TODO(crbug.com/1072932) Refactor interface to allow asynchronous
+  // enumeration and remove this hack.
+  base::Optional<base::RunLoop> first_update_run_loop_;
 };
 
 }  // namespace media
