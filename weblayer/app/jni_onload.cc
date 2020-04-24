946d09a95e4e6e8b7c4a530d79f283dde35ed32d
Hans Wennborg <hans@chromium.org>
2020-04-24 18:43:17 +0000

    Remove/replace unnecessary logging.h includes in .cc files (weblayer)
    CHECK, CHECK_EQ etc., and NOTREACHED/NOTIMPLEMENTED have moved
    to the much smaller headers check.h, check_op.h, and notreached.h,
    respectively.
    
    This CL updates .cc files to use those headers instead when
    possible, with the purpose of saving compile time.
    
    (Split out from https://crrev.com/c/2164525 which also has
    notes on how the change was generated.)
    
    Bug: 1031540
    Change-Id: I2dc41b82bfbc3cb425f5f9147fd5afeaef480fd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164640
    Commit-Queue: Richard Coles <torne@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762424}

--- weblayer/app/jni_onload.cc
+++ weblayer/app/jni_onload.cc
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/android/library_loader/library_loader_hooks.h"
-#include "base/logging.h"
 #include "components/version_info/version_info.h"
 #include "components/version_info/version_info_values.h"
 #include "content/public/app/content_jni_onload.h"
--- weblayer/browser/profile_disk_operations_unittests.cc
+++ weblayer/browser/profile_disk_operations_unittests.cc
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/check.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
-#include "base/logging.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
 #include "build/build_config.h"
