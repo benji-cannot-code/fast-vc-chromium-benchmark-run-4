c7476254347b73ace0e3f5ff97cebf878272dbbf
Hans Wennborg <hans@chromium.org>
2020-04-24 21:33:44 +0000

    Remove/replace unnecessary logging.h includes in .cc files (sql)
    CHECK, CHECK_EQ etc., and NOTREACHED/NOTIMPLEMENTED have moved
    to the much smaller headers check.h, check_op.h, and notreached.h,
    respectively.
    
    This CL updates .cc files to use those headers instead when
    possible, with the purpose of saving compile time.
    
    (Split out from https://crrev.com/c/2164525 which also has
    notes on how the change was generated.)
    
    Bug: 1031540
    Change-Id: I91305fe44026e637eaf7f2c543a9ad159e86e60a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164812
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762517}

--- sql/meta_table.cc
+++ sql/meta_table.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "base/logging.h"
+#include "base/check_op.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
 #include "sql/database.h"
--- sql/recover_module/record.cc
+++ sql/recover_module/record.cc
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <type_traits>
 
-#include "base/logging.h"
+#include "base/check_op.h"
+#include "base/notreached.h"
 #include "sql/recover_module/integers.h"
 #include "sql/recover_module/payload.h"
 #include "third_party/sqlite/sqlite3.h"
