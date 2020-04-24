8638edc2446712adaef65f2e357cf894504d5d35
David Grogan <dgrogan@chromium.org>
2020-04-24 05:37:55 +0000

    [css-flexbox] Implement gap/gutters
    This only works when you enable "Enable experimental web platform
    features" in about:flags.
    
    This patch doesn't implement a weird case that firefox supports:
    resolving a percent column-gap when the flexbox's height is indefinite.
    This patch resolves it to 0. We may wait until that is fixed before
    exposing this to the web without the about:flag. The relevant test is
    http://wpt.live/css/vendor-imports/mozilla/mozilla-central-reftests/flexbox/flexbox-column-row-gap-004.html
    The spec issue is https://github.com/w3c/csswg-drafts/issues/4664.
    
    This patch does not include the few engine-specific bits in legacy, so
    gap only works in FlexNG.
    
    Change-Id: I691889cf4b9346e94c83ef655dc9fd6eebca640d
    Bug: 762679, 845235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162149
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762264}

--- third_party/blink/renderer/core/layout/flexible_box_algorithm.h
+++ third_party/blink/renderer/core/layout/flexible_box_algorithm.h
@@ -360,7 +360,10 @@ class FlexLayoutAlgorithm {
   DISALLOW_NEW();
 
  public:
-  FlexLayoutAlgorithm(const ComputedStyle*, LayoutUnit line_break_length);
+  FlexLayoutAlgorithm(const ComputedStyle*,
+                      LayoutUnit line_break_length,
+                      LayoutUnit gap_between_items,
+                      LayoutUnit gap_between_lines);
 
   template <typename... Args>
   FlexItem& emplace_back(Args&&... args) {
@@ -429,6 +432,8 @@ class FlexLayoutAlgorithm {
   void LayoutColumnReverse(LayoutUnit main_axis_content_size,
                            LayoutUnit border_scrollbar_padding_before);
   bool IsNGFlexBox() const;
+  const LayoutUnit gap_between_items_;
+  const LayoutUnit gap_between_lines_;
 
  private:
   EOverflow MainAxisOverflowForChild(const LayoutBox& child) const;
