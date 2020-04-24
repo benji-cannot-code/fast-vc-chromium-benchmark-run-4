b3c9bf812d4228559ee718d6c9bab398702d5779
Bo Liu <boliu@chromium.org>
2020-04-24 22:16:18 +0000

    android: Avoid WeakReference gc churn
    Add ImmutableWeakReference that throws in clear().
    Then use it in WindowAndroid and keep returning the same instance
    to clients to avoid gc churn.
    
    Change-Id: I568d5b5047214ebf8c6df776407b3c5fb368a6d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165148
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762539}

---
+++ ui/android/java/src/org/chromium/ui/base/ImmutableWeakReference.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.base;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * A WeakReference sublcass that's immutable.
+ * This is so that it's safe to pass the same instance to multiple
+ * clients without worrying about modification.
+ */
+public class ImmutableWeakReference<T> extends WeakReference<T> {
+    public ImmutableWeakReference(T referent) {
+        super(referent);
+    }
+
+    @Override
+    public final void clear() {
+        throw new UnsupportedOperationException("clear WeakReference banned");
+    }
+}
