be80b4f2b53ec12b21e59804da5bf8ccdce0c68f
Doug Arnett <dougarnett@chromium.org>
2020-04-25 14:25:57 +0000

    Adds Translate.LanguageDetection.ContentLength histogram
    This will be used to compare how much content is used with subframe
    language detection vs. just with main frame.
    
    
    Bug: 797328
    Change-Id: I30ce39cd119e3b04941e1a6d89c467872e40ea55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163726
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Commit-Queue: Doug Arnett <dougarnett@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762659}

--- chrome/browser/translate/chrome_translate_client.cc
+++ chrome/browser/translate/chrome_translate_client.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/translate/core/browser/language_state.h"
 #include "components/translate/core/browser/page_translated_details.h"
 #include "components/translate/core/browser/translate_accept_languages.h"
+#include "components/translate/core/browser/translate_browser_metrics.h"
 #include "components/translate/core/browser/translate_download_manager.h"
 #include "components/translate/core/browser/translate_infobar_delegate.h"
 #include "components/translate/core/browser/translate_manager.h"
@@ -343,6 +344,9 @@ void ChromeTranslateClient::WebContentsDestroyed() {
 
 void ChromeTranslateClient::OnLanguageDetermined(
     const translate::LanguageDetectionDetails& details) {
+  translate::TranslateBrowserMetrics::ReportLanguageDetectionContentLength(
+      details.contents.size());
+
   // TODO(268984): Remove translate notifications and have the clients be
   // ContentTranslateDriver::Observer directly instead.
   content::NotificationService::current()->Notify(
