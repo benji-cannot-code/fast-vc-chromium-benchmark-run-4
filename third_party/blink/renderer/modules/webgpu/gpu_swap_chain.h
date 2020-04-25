c319b7703265dce83795a656d867e2fa518766a0
Kai Ninomiya <kainino@chromium.org>
2020-04-25 01:12:24 +0000

    Allow only bgra8unorm and (deprecated) rgba8unorm swap chains
    rgba8unorm swap chains are enforced to be only used with
    OUTPUT_ATTACHMENT. Other usages will behave incorrectly, because the
    internal format is actually bgra8unorm on Mac.
    
    rgba8unorm swap chains are deprecated, for now, as support is not
    required IIRC (only bgra8unorm and rgba16float are core).
    
    Also make the logic in SharedImageBackingFactoryIOSurface more explicit.
    This shouldn't change the result of the logic at all.
    
    Change-Id: I23c1f1172511fd63fdf01000b571f59b142cb275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163041
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762623}

--- third_party/blink/renderer/modules/webgpu/gpu_swap_chain.h
+++ third_party/blink/renderer/modules/webgpu/gpu_swap_chain.h
@@ -17,7 +17,6 @@ namespace blink {
 
 class GPUCanvasContext;
 class GPUDevice;
-class GPUSwapChainDescriptor;
 class GPUTexture;
 
 class GPUSwapChain : public ScriptWrappable,
@@ -26,9 +25,11 @@ class GPUSwapChain : public ScriptWrappable,
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  explicit GPUSwapChain(GPUCanvasContext* context,
-                        const GPUSwapChainDescriptor* descriptor,
-                        SkFilterQuality filter_quality);
+  explicit GPUSwapChain(GPUCanvasContext*,
+                        GPUDevice*,
+                        WGPUTextureUsage,
+                        WGPUTextureFormat,
+                        SkFilterQuality);
   ~GPUSwapChain() override;
 
   void Trace(Visitor* visitor) override;
