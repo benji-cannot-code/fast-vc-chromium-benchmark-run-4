702cf23ed5b6956bd33aab62194eeaeb05a82378
Eugene Zemtsov <eugene@chromium.org>
2020-04-25 21:17:52 +0000

    [webcodecs] webcodecs/VideoEncoder can actually encode frames
    1. Generic interface for VideoEncoder in media.
    2. Software VpxEncoder in media. (No fancy options are supported yet)
    3. webcodecs/VideoEncoder can call media::VideoEncoder to encode frames.
       But there is no frame queue (only one frame can be encoded at once)
       and everything is happening on the main thread.
    
    Bug: 897297, 1045248
    Change-Id: I0665c1c4f6cf8cfb47a15ece6511a3f03ff64ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2148643
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762670}

---
+++ media/base/video_encoder.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/video_encoder.h"
+
+#include "media/base/video_frame.h"
+
+namespace media {
+
+VideoEncoderOutput::VideoEncoderOutput() = default;
+VideoEncoderOutput::VideoEncoderOutput(VideoEncoderOutput&&) = default;
+VideoEncoderOutput::~VideoEncoderOutput() = default;
+
+VideoEncoder::VideoEncoder() = default;
+VideoEncoder::~VideoEncoder() = default;
+
+VideoEncoder::Options::Options() = default;
+VideoEncoder::Options::~Options() = default;
+
+}  // namespace media
--- third_party/blink/renderer/modules/webcodecs/video_encoder.h
+++ third_party/blink/renderer/modules/webcodecs/video_encoder.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBCODECS_VIDEO_ENCODER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBCODECS_VIDEO_ENCODER_H_
 
+#include <memory>
+
+#include "base/optional.h"
+#include "media/base/status.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_video_encoder_output_callback.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_web_codecs_error_callback.h"
@@ -13,9 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/webcodecs/video_frame.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 
+namespace media {
+class VideoEncoder;
+struct VideoEncoderOutput;
+}  // namespace media
+
 namespace blink {
 
 class ExceptionState;
+enum class DOMExceptionCode;
 class VideoEncoderInit;
 class VideoEncoderTuneOptions;
 class VideoEncoderEncodeOptions;
@@ -25,31 +35,64 @@ class MODULES_EXPORT VideoEncoder final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static VideoEncoder* Create(ScriptState*, VideoEncoderInit*, ExceptionState&);
-  VideoEncoder(ScriptState*, VideoEncoderInit*, ExceptionState&);
+  static VideoEncoder* Create(ScriptState*, ExceptionState&);
+  VideoEncoder(ScriptState*, ExceptionState&);
   ~VideoEncoder() override;
 
   // video_encoder.idl implementation.
-  ScriptPromise tune(const VideoEncoderTuneOptions* params, ExceptionState&);
   ScriptPromise encode(const VideoFrame* frame,
                        const VideoEncoderEncodeOptions*,
                        ExceptionState&);
+
+  ScriptPromise configure(const VideoEncoderInit* init, ExceptionState&);
+
+  ScriptPromise tune(const VideoEncoderTuneOptions* params, ExceptionState&);
+
   ScriptPromise close();
+
+  ScriptPromise flush();
 
   // GarbageCollected override.
   void Trace(Visitor*) override;
 
  private:
-  void DoEncoding(const VideoFrame* frame, bool force_keyframe);
+  struct Request : public GarbageCollected<Request> {
+    enum class Type {
+      kConfigure,
+      kEncode,
+      kFlush,
+      kClose,
+    };
+
+    void Trace(Visitor*);
+    DOMException* Reject(DOMExceptionCode code, const String& message);
+    void Resolve();
+
+    Type type;
+    Member<const VideoEncoderInit> config;               // used by kConfigure
+    Member<const VideoFrame> frame;                      // used by kEncode
+    Member<const VideoEncoderEncodeOptions> encodeOpts;  // used by kEncode
+    Member<ScriptPromiseResolver> resolver;
+  };
 
   void CallOutputCallback(EncodedVideoChunk* chunk);
+  void CallErrorCallback(DOMException* ex);
+  ScriptPromise EnqueueRequest(Request* request);
+  void ProcessRequests();
+  void ProcessEncode(Request* request);
+  void ProcessConfigure(Request* request);
+  void ProcessClose(Request* request);
+  void ProcessFlush(Request* request);
+
+  void MediaEncoderOutputCallback(media::VideoEncoderOutput output);
 
   gfx::Size frame_size_;
+  std::unique_ptr<media::VideoEncoder> media_encoder_;
 
   Member<ScriptState> script_state_;
-
   Member<V8VideoEncoderOutputCallback> output_callback_;
   Member<V8WebCodecsErrorCallback> error_callback_;
+  HeapDeque<Member<Request>> requests_;
   SEQUENCE_CHECKER(sequence_checker_);
 };
 
