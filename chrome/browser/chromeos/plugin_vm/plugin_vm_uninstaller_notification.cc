42feb556f01072368e4acd16c534c828a5cefcaf
Timothy Loh <timloh@chromium.org>
2020-04-27 08:11:02 +0000

    Use a placeholder for 'Plugin VM' and update strings in chromeos_strings.grdp
    Instead of having 'Plugin VM' explicitly written out in the various
    strings, we should use a placeholder and have the name in a single
    place. This ensures the translations of this name will be identical.
    
    Bug: 1073795
    Change-Id: I76f1ef2d81fc7baa69e2becc3af8381071315eaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162386
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Julian Watson <juwa@google.com>
    Cr-Commit-Position: refs/heads/master@{#762746}

--- chrome/browser/chromeos/plugin_vm/plugin_vm_uninstaller_notification.cc
+++ chrome/browser/chromeos/plugin_vm/plugin_vm_uninstaller_notification.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/message_center/public/cpp/message_center_constants.h"
 
+namespace {
+
 constexpr char kNotifierPluginVmUninstallOperation[] =
     "plugin_vm.uninstall_operation";
 
@@ -26,6 +28,12 @@ std::string GetUniqueNotificationId() {
                             next_notification_id_++);
 }
 
+base::string16 PluginVmAppName() {
+  return l10n_util::GetStringUTF16(IDS_PLUGIN_VM_APP_NAME);
+}
+
+}  // namespace
+
 PluginVmUninstallerNotification::PluginVmUninstallerNotification(
     Profile* profile)
     : profile_(profile) {
@@ -35,13 +43,15 @@ PluginVmUninstallerNotification::PluginVmUninstallerNotification(
   rich_notification_data.pinned = true;
   rich_notification_data.never_timeout = true;
 
+  base::string16 app_name = PluginVmAppName();
   notification_ = std::make_unique<message_center::Notification>(
       message_center::NOTIFICATION_TYPE_PROGRESS, GetUniqueNotificationId(),
-      l10n_util::GetStringUTF16(
-          IDS_PLUGIN_VM_REMOVING_NOTIFICATION_IN_PROGRESS_MESSAGE),  // title
-      base::string16(),                                              // message
-      gfx::Image(),                                                  // icon
-      l10n_util::GetStringUTF16(IDS_PLUGIN_VM_APP_NAME),
+      l10n_util::GetStringFUTF16(
+          IDS_PLUGIN_VM_REMOVING_NOTIFICATION_IN_PROGRESS_MESSAGE,
+          app_name),     // title
+      base::string16(),  // message
+      gfx::Image(),      // icon
+      app_name,
       GURL(),  // origin_url
       message_center::NotifierId(message_center::NotifierType::SYSTEM_COMPONENT,
                                  kNotifierPluginVmUninstallOperation),
@@ -56,8 +66,8 @@ PluginVmUninstallerNotification::~PluginVmUninstallerNotification() = default;
 
 void PluginVmUninstallerNotification::SetFailed() {
   notification_->set_type(message_center::NOTIFICATION_TYPE_SIMPLE);
-  notification_->set_title(l10n_util::GetStringUTF16(
-      IDS_PLUGIN_VM_REMOVING_NOTIFICATION_FAILED_MESSAGE));
+  notification_->set_title(l10n_util::GetStringFUTF16(
+      IDS_PLUGIN_VM_REMOVING_NOTIFICATION_FAILED_MESSAGE, PluginVmAppName()));
   notification_->set_pinned(false);
   notification_->set_never_timeout(false);
   notification_->set_accent_color(ash::kSystemNotificationColorCriticalWarning);
@@ -67,8 +77,8 @@ void PluginVmUninstallerNotification::SetFailed() {
 
 void PluginVmUninstallerNotification::SetCompleted() {
   notification_->set_type(message_center::NOTIFICATION_TYPE_SIMPLE);
-  notification_->set_title(l10n_util::GetStringUTF16(
-      IDS_PLUGIN_VM_REMOVING_NOTIFICATION_COMPLETE_MESSAGE));
+  notification_->set_title(l10n_util::GetStringFUTF16(
+      IDS_PLUGIN_VM_REMOVING_NOTIFICATION_COMPLETE_MESSAGE, PluginVmAppName()));
   notification_->set_pinned(false);
   notification_->set_never_timeout(false);
 
