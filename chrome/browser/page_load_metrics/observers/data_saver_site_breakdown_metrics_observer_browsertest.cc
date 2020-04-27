d108913a41f2957e4a8ca7ae1db6a5aa2178d5a3
Scott Little <sclittle@chromium.org>
2020-04-27 09:37:32 +0000

    Remove dependency on WaitForScrollOffset from LazyLoad data savings browser tests.
    Other LazyLoad browser tests were having trouble with flakiness caused
    by RenderFrameSubmissionObserver::WaitForScrollOffset sometimes hanging
    (see https://crbug.com/1035057), so this CL removes these unneeded
    dependencies in order to reduce the flakiness of these data savings
    browser tests.
    
    Fixed: 1074594
    Change-Id: I2d568fbc77c4c576cc1b912fef54aa413231132b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166635
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Scott Little <sclittle@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762768}

--- chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/data_saver_site_breakdown_metrics_observer_browsertest.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_base.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
+#include "net/nqe/effective_connection_type.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
@@ -133,7 +134,14 @@ class LazyLoadWithoutLiteModeBrowserTest
     scoped_feature_list_.InitWithFeaturesAndParameters(
         {{features::kLazyImageLoading,
           {{"automatic-lazy-load-images-enabled", "true"},
-           {"lazy_image_first_k_fully_load", "4G:0"}}},
+           {"lazy_image_first_k_fully_load",
+            base::StringPrintf("%s:0,%s:0,%s:0,%s:0,%s:0,%s:0",
+                               net::kEffectiveConnectionTypeUnknown,
+                               net::kEffectiveConnectionTypeOffline,
+                               net::kEffectiveConnectionTypeSlow2G,
+                               net::kEffectiveConnectionType2G,
+                               net::kEffectiveConnectionType3G,
+                               net::kEffectiveConnectionType4G)}}},
          {features::kLazyFrameLoading,
           {{"automatic-lazy-load-frames-enabled", "true"}}}},
         {});
@@ -151,7 +159,14 @@ class LazyLoadWithLiteModeBrowserTest
     scoped_feature_list_.InitWithFeaturesAndParameters(
         {{features::kLazyImageLoading,
           {{"automatic-lazy-load-images-enabled", "true"},
-           {"lazy_image_first_k_fully_load", "4G:0"}}},
+           {"lazy_image_first_k_fully_load",
+            base::StringPrintf("%s:0,%s:0,%s:0,%s:0,%s:0,%s:0",
+                               net::kEffectiveConnectionTypeUnknown,
+                               net::kEffectiveConnectionTypeOffline,
+                               net::kEffectiveConnectionTypeSlow2G,
+                               net::kEffectiveConnectionType2G,
+                               net::kEffectiveConnectionType3G,
+                               net::kEffectiveConnectionType4G)}}},
          {features::kLazyFrameLoading,
           {{"automatic-lazy-load-frames-enabled", "true"}}}},
         {});
@@ -210,7 +225,9 @@ class LazyLoadWithLiteModeBrowserTest
     waiter->Wait();
 
     // Scroll to remove data savings by loading the images.
-    ScrollToAndWaitForScroll(10000);
+    EXPECT_EQ(nullptr, content::EvalJs(
+                           browser()->tab_strip_model()->GetActiveWebContents(),
+                           "document.body.scrollIntoView({block: 'end'});"));
 
     waiter->AddMinimumCompleteResourcesExpectation(
         expected_initial_resources + expected_resources_post_scroll);
@@ -224,15 +241,6 @@ class LazyLoadWithLiteModeBrowserTest
   }
 
  private:
-  void ScrollToAndWaitForScroll(unsigned int scroll_offset) {
-    ASSERT_TRUE(content::ExecuteScript(
-        browser()->tab_strip_model()->GetActiveWebContents(),
-        base::StringPrintf("window.scrollTo(0, %d);", scroll_offset)));
-    content::RenderFrameSubmissionObserver observer(
-        browser()->tab_strip_model()->GetActiveWebContents());
-    observer.WaitForScrollOffset(gfx::Vector2dF(0, scroll_offset));
-  }
-
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
