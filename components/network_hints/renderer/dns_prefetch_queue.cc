7f0580f796b0e3cb0e435b05d5a6df4a8702de0a
Piotr Tworek <ptworek@vewd.com>
2020-04-27 14:14:10 +0000

    Add missing include in //components/network_hints
    The code in one of the source files of this module uses strlen but does
    not include cstring header. On Linux systems using glibc 2.30 or newer
    this leads to a build failure.
    
    Bug: 1074286
    Change-Id: I9240e1846308613a071ebce4458430e64b1c59de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165893
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#762835}

--- components/network_hints/renderer/dns_prefetch_queue.cc
+++ components/network_hints/renderer/dns_prefetch_queue.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/network_hints/renderer/dns_prefetch_queue.h"
 
+#include <cstring>
+
 #include "base/logging.h"
 
 namespace network_hints {
