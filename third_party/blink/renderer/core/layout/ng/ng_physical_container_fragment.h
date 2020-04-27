969efe293c9820d01c731a97ce313e8527a6ba43
Stephan Hartmann <stha09@googlemail.com>
2020-04-27 12:05:48 +0000

    libstdc++: make NGPhysicalContainerFragment::ConstIterator usable with std::all_of
    Use of std::all_of requires STL compliant iterator. However,
    NGPhysicalContainerFragment::ConstIterator does not define
    iterator_tag and therefore is no STL iterator. To make it
    compliant derive NGPhysicalContainerFragment::ConstIterator from
    std::iterator.
    
    Bug: 957519
    Change-Id: Id042f987ca093ece6cd1cd10f601eb1909700d08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2153148
    Reviewed-by: Aleks Totic <atotic@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762806}

--- third_party/blink/renderer/core/layout/ng/ng_physical_container_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_container_fragment.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_NG_PHYSICAL_CONTAINER_FRAGMENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_NG_PHYSICAL_CONTAINER_FRAGMENT_H_
 
+#include <iterator>
+
 #include "base/containers/span.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_rect.h"
@@ -32,7 +34,8 @@ class CORE_EXPORT NGPhysicalContainerFragment : public NGPhysicalFragment {
     PostLayoutChildLinkList(wtf_size_t count, const NGLink* buffer)
         : count_(count), buffer_(buffer) {}
 
-    class ConstIterator {
+    class ConstIterator
+        : public std::iterator<std::input_iterator_tag, NGLink> {
       STACK_ALLOCATED();
 
      public:
