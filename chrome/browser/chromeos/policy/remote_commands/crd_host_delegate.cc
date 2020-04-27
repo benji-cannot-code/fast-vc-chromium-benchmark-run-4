2e48432af38080b9fdc14bcbb2a2a2f68624dfab
Joe Downing <joedow@chromium.org>
2020-04-27 17:08:47 +0000

    Removing kUseFtlSignalingForCrdHostDelegate feature
    The ChromeOS CRD Host has been using FTL for almost a year
    now so it's time to clean up the flag we used for feature
    testing.
    
    Change-Id: I06654cb91f14c19e27faa6756a980e7b0e849cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164693
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762894}

--- chrome/browser/chromeos/policy/remote_commands/crd_host_delegate.cc
+++ chrome/browser/chromeos/policy/remote_commands/crd_host_delegate.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/remote_commands/crd_host_delegate.h"
 
 #include "base/bind.h"
-#include "base/feature_list.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/task/post_task.h"
@@ -17,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/device_identity/device_oauth2_token_service.h"
 #include "chrome/browser/device_identity/device_oauth2_token_service_factory.h"
-#include "chrome/common/chrome_features.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
@@ -150,15 +148,9 @@ void CRDHostDelegate::FetchOAuthToken(
   DeviceOAuth2TokenService* oauth_service =
       DeviceOAuth2TokenServiceFactory::Get();
 
-  OAuth2AccessTokenManager::ScopeSet scopes;
-  scopes.insert(GaiaConstants::kGoogleUserInfoEmail);
-  scopes.insert(kCloudDevicesOAuth2Scope);
-
-  if (base::FeatureList::IsEnabled(
-          features::kUseFtlSignalingForCrdHostDelegate)) {
-    scopes.insert(kChromotingRemoteSupportOAuth2Scope);
-    scopes.insert(kTachyonOAuth2Scope);
-  }
+  OAuth2AccessTokenManager::ScopeSet scopes{
+      GaiaConstants::kGoogleUserInfoEmail, kCloudDevicesOAuth2Scope,
+      kChromotingRemoteSupportOAuth2Scope, kTachyonOAuth2Scope};
 
   oauth_success_callback_ = std::move(success_callback);
   error_callback_ = std::move(error_callback);
