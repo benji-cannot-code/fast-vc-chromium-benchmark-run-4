057ac1a6be71e905f5edb93ea1b5becf7b341066
Rachel Carpenter <carpenterr@chromium.org>
2020-04-27 07:06:17 +0000

    Add user account type information to help_app loadtimedata.
    Bug: b/153377178
    Change-Id: I64beda05358e880c49d52194bd90c435dd903689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166229
    Reviewed-by: dstockwell <dstockwell@chromium.org>
    Commit-Queue: Rachel Carpenter <carpenterr@google.com>
    Cr-Commit-Position: refs/heads/master@{#762740}

--- chrome/browser/chromeos/web_applications/chrome_help_app_ui_delegate.cc
+++ chrome/browser/chromeos/web_applications/chrome_help_app_ui_delegate.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/assistant/assistant_util.h"
 #include "chrome/browser/chromeos/login/quick_unlock/quick_unlock_utils.h"
+#include "chrome/browser/policy/profile_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/common/pref_names.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/multidevice_setup/public/cpp/prefs.h"
 #include "chromeos/system/statistics_provider.h"
 #include "components/prefs/pref_service.h"
+#include "components/user_manager/user_manager.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "ui/events/devices/device_data_manager.h"
 #include "url/gurl.h"
@@ -92,4 +94,12 @@ void ChromeHelpAppUIDelegate::PopulateLoadTimeData(
                      arc::IsArcPlayStoreEnabledForProfile(profile));
   source->AddBoolean("pinEnabled",
                      chromeos::quick_unlock::IsPinEnabled(pref_service));
+
+  // Data about what type of account/login this is.
+  user_manager::UserManager* user_manager = user_manager::UserManager::Get();
+  source->AddBoolean("isManagedDevice",
+                     profile->GetProfilePolicyConnector()->IsManaged());
+  source->AddInteger("userType", user_manager->GetActiveUser()->GetType());
+  source->AddBoolean("isEphemeralUser",
+                     user_manager->IsCurrentUserNonCryptohomeDataEphemeral());
 }
