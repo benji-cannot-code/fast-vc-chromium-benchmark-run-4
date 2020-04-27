3b47c1e19d6d11a4d3439d1598d6736c12cfabc3
Ken MacKay <kmackay@chromium.org>
2020-04-27 23:50:57 +0000

    [Chromecast] Move wav_header.h to new location
    Merge-With: eureka-internal/391730
    
    Bug: none
    Test: build
    Change-Id: Id1259c7e63e9dc618dcd829c0de377fa3173c9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168763
    Commit-Queue: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Yuchen Liu <yucliu@chromium.org>
    Auto-Submit: Kenneth MacKay <kmackay@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763100}

--- chromecast/media/audio/wav_header.h
+++ chromecast/media/audio/wav_header.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROMECAST_MEDIA_CMA_BACKEND_CPLAY_WAV_HEADER_H_
-#define CHROMECAST_MEDIA_CMA_BACKEND_CPLAY_WAV_HEADER_H_
+#ifndef CHROMECAST_MEDIA_AUDIO_WAV_HEADER_H_
+#define CHROMECAST_MEDIA_AUDIO_WAV_HEADER_H_
 
 #include <stdint.h>
 
@@ -82,4 +82,4 @@ WavHeader::WavHeader() = default;
 }  // namespace media
 }  // namespace chromecast
 
-#endif  // CHROMECAST_MEDIA_CMA_BACKEND_CPLAY_WAV_HEADER_H_
+#endif  // CHROMECAST_MEDIA_AUDIO_WAV_HEADER_H_
