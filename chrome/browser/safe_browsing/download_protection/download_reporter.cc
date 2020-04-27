dc0b146156337d6a8e38d5e070f7936403cf2454
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-04-27 22:20:56 +0000

    Add Dlp Verdict to warning bypass event on uploads
    Bug: 1069109
    Change-Id: I79f2c77ee4624cf200f9baee49239a3956ab5eb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154847
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763062}

--- chrome/browser/safe_browsing/download_protection/download_reporter.cc
+++ chrome/browser/safe_browsing/download_protection/download_reporter.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/common/download_danger_type.h"
 #include "components/download/public/common/download_item.h"
 #include "components/download/public/common/simple_download_manager_coordinator.h"
+#include "components/safe_browsing/core/proto/webprotect.pb.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_item_utils.h"
 
@@ -91,9 +92,12 @@ void ReportSensitiveDataWarningBypassed(download::DownloadItem* download) {
   Profile* profile = Profile::FromBrowserContext(browser_context);
   if (profile) {
     std::string raw_digest_sha256 = download->GetHash();
+    // TODO(crbug/1069109): Use actual DlpDeepScanningVerdict that triggered the
+    // original warning here.
     extensions::SafeBrowsingPrivateEventRouterFactory::GetForProfile(profile)
         ->OnSensitiveDataWarningBypassed(
-            download->GetURL(), download->GetTargetFilePath().AsUTF8Unsafe(),
+            DlpDeepScanningVerdict(), download->GetURL(),
+            download->GetTargetFilePath().AsUTF8Unsafe(),
             base::HexEncode(raw_digest_sha256.data(), raw_digest_sha256.size()),
             download->GetMimeType(),
             extensions::SafeBrowsingPrivateEventRouter::kTriggerFileDownload,
