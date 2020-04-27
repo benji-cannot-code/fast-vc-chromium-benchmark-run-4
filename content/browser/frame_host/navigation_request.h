74a6491f8a0d2c1feb7e71545ee5b0ff6b57623c
W. James MacLean <wjmaclean@chromium.org>
2020-04-27 21:05:57 +0000

    Use session history walk to find non-isolated instances of newly opted-in origins.
    For implementing origin-isolation opt-in (via OriginPolicy or headers),
    in order to maintain consistency within a BrowsingInstance, when an
    origin opts in to isolation, we need to be able to track where we have
    previously visited that origin in a non-isolated state to prevent us from
    isolating future visits in the same BrowsingInstance, since those
    documents may need the ability to script each other.
    
    Since doing so with a special purpose data structure might be costly in
    terms of memory, and since the information should already exist in the
    session histories, this CL implements a traversal of the session
    histories that determines if a given origin has previously been seen or
    not. This traversal is done the first time an origin opts in to
    isolation, populating the non-isolated lists in other BrowsingInstances
    that have already loaded non-isolated instances of the origin, found by
    searching FrameNavigationEntries across all WebContents.
    
    In a followup CL, we will handle cases where there is no
    FrameNavigationEntry, as well as pending navigation commits.
    
    Bug: 1042415
    Change-Id: I83e623e585eff55d32fc586dadd2a6be9d7b9964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2018252
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763006}

--- content/browser/frame_host/navigation_request.h
+++ content/browser/frame_host/navigation_request.h
@@ -203,6 +203,10 @@ class CONTENT_EXPORT NavigationRequest
 
   ~NavigationRequest() override;
 
+  // Returns true if this navigation request is requesting opt-in
+  // origin-isolation, via Origin Policy or headers.
+  bool IsOptInIsolationRequested();
+
   // NavigationHandle implementation:
   int64_t GetNavigationId() override;
   const GURL& GetURL() override;
@@ -601,8 +605,7 @@ class CONTENT_EXPORT NavigationRequest
   // Checks if the response requests an isolated origin (using either origin
   // policy or the Origin-Isolation header), and if so opts in the origin to be
   // isolated.
-  void CheckForIsolationOptIn(const GURL& url,
-                              const network::mojom::URLResponseHead* response);
+  void CheckForIsolationOptIn(const GURL& url);
 
   // NavigationURLLoaderDelegate implementation.
   void OnRequestRedirected(
