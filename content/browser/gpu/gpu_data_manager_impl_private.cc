9dc227ce1ef02d4fe9cfe209e926264f7d26ff9f
Maggie Chen <magchen@chromium.org>
2020-04-27 17:19:06 +0000

    Reland "Separate GpuInfoService from GpuService"
    This is a reland of 3d3e44d2bd3f807e5b37ea8bc3d17cdc0a83d15d
    
    Skip adding memory pressure listener if it's the info-collection GPU
    process.
    
    Original change's description:
    > Separate GpuInfoService from GpuService
    >
    > GpuInfoService is used for GPU info collection and is running on the
    > short-lived info-collection GPU process only.
    > This CL separates GpuInfoService from the regular GpuService because it
    > is not running on the same GPU process.
    >
    > Bug:1048903
    >
    > Change-Id: I6f11cbfec8b17d4e5c4ac9865b26542aa6dd619d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154388
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Khushal <khushalsagar@chromium.org>
    > Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    > Commit-Queue: Maggie Chen <magchen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#762068}
    
    TBR=dcheng@chromium.org
    Bug:1048903,1074216
    
    Change-Id: Id5c67baeae58000bee89cf5e5758f788cb24071c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165868
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762900}

--- content/browser/gpu/gpu_data_manager_impl_private.cc
+++ content/browser/gpu/gpu_data_manager_impl_private.cc
@@ -643,7 +643,7 @@ void GpuDataManagerImplPrivate::RequestDxDiagNodeData() {
     }
 
     manager->UpdateDxDiagNodeRequestStatus(true);
-    host->gpu_service()->RequestCompleteGpuInfo(
+    host->info_collection_gpu_service()->RequestDxDiagNodeInfo(
         base::BindOnce([](const gpu::DxDiagNode& dx_diagnostics) {
           GpuDataManagerImpl* manager = GpuDataManagerImpl::GetInstance();
           manager->UpdateDxDiagNode(dx_diagnostics);
@@ -700,8 +700,8 @@ void GpuDataManagerImplPrivate::RequestGpuSupportedRuntimeVersion(
         }
 
         manager->UpdateDx12VulkanRequestStatus(true);
-        host->gpu_service()->GetGpuSupportedRuntimeVersionAndDevicePerfInfo(
-            base::BindOnce(
+        host->info_collection_gpu_service()
+            ->GetGpuSupportedRuntimeVersionAndDevicePerfInfo(base::BindOnce(
                 [](const gpu::Dx12VulkanVersionInfo& dx12_vulkan_info,
                    const gpu::DevicePerfInfo& device_perf_info) {
                   GpuDataManagerImpl* manager =
