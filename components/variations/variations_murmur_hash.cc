7c792b00de74b32e37801fb268fc67f25b6b4252
Piotr Tworek <ptworek@vewd.com>
2020-04-27 17:38:55 +0000

    Add missing string.h include in //components/variations.
    The code in variations_murmur_hash.cc uses memcpy function but does not
    include header defining it. This leads to a build failure on Linux
    systems using glibc 2.30, or newer.
    
    Bug: 1074286
    Change-Id: Ieb175d873b7b65e99ea1356414fe7ff65d5825ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165794
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762918}

--- components/variations/variations_murmur_hash.cc
+++ components/variations/variations_murmur_hash.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/variations/variations_murmur_hash.h"
 
+#include <string.h>
+
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/sys_byteorder.h"
