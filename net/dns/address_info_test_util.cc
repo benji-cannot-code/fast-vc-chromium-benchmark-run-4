c859e8fcf3c00296843989780092e69c5431d854
Piotr Tworek <ptworek@vewd.com>
2020-04-27 08:21:35 +0000

    Add missing cstring include in net/dns/address_info_test_util.cc
    The code uses memset and memcpy provided by string.h. It does not
    include this file however. On Linux this fails to build starting with
    glibc 2.30.
    
    Bug: 1074286
    Change-Id: I549b02aad39ba0f022a2c84bfc2bb36c2ff31f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164508
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#762748}

--- net/dns/address_info_test_util.cc
+++ net/dns/address_info_test_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/dns/address_info_test_util.h"
 
+#include <cstring>
+
 #include "base/logging.h"
 #include "base/sys_byteorder.h"
 #include "net/base/sys_addrinfo.h"
