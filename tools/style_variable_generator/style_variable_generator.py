7a8c0889b3a6405f64dc42287be38f14742209dc
Christopher Lam <calamity@chromium.org>
2020-04-27 05:31:36 +0000

    [cros_colors] Generate semantic colors for AshColorProvider.
    This CL plugs cros_colors.json5 into the AshColorProvider by generating a
    C++ header that maps color names to their final values at compile time.
    
    Bug: 1018654
    Change-Id: I52f21e859be704504ce430b5d558c96b60b1fb0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2154350
    Commit-Queue: calamity <calamity@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762729}

--- tools/style_variable_generator/style_variable_generator.py
+++ tools/style_variable_generator/style_variable_generator.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import argparse
 import sys
 from css_generator import CSSStyleGenerator
+from views_generator import ViewsStyleGenerator
 
 
 def main():
@@ -14,7 +15,7 @@ def main():
 
     parser.add_argument(
         '--generator',
-        choices=['CSS'],
+        choices=['CSS', 'Views'],
         required=True,
         help='type of file to generate')
     parser.add_argument('--out-file', help='file to write output to')
@@ -25,8 +26,13 @@ def main():
     if args.generator == 'CSS':
         style_generator = CSSStyleGenerator()
 
+    if args.generator == 'Views':
+        style_generator = ViewsStyleGenerator()
+
     for t in args.targets:
         style_generator.AddJSONFileToModel(t)
+
+    style_generator.out_file_path = args.out_file
 
     if args.out_file:
         with open(args.out_file, 'w') as f:
