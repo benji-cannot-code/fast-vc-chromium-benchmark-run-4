6a249c9d6c8669bf9fcd15bfeee750f42a6cb4f8
Piotr Tworek <ptworek@vewd.com>
2020-04-27 08:44:48 +0000

    Add missing cstring includes in //mojo
    The two affected files use memcpy and memset. Both functions are
    supposed to be defined in string.h / cstring. Unfortunately the affected
    files do not pull in this header. On Linux this works fine up until
    glibc 2.29. Starting with 2.30 the code fails to build. Most probably
    string.h was being pulled indirectly in glibc 2.29 and older.
    
    Bug: 1074286
    Change-Id: Ia79cb76bb3cfb0d4062aec1a7688cc6e516b5bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164885
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#762755}

--- mojo/public/cpp/bindings/lib/buffer.cc
+++ mojo/public/cpp/bindings/lib/buffer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/bindings/lib/buffer.h"
 
+#include <cstring>
+
 #include "base/logging.h"
 #include "base/numerics/safe_math.h"
 #include "mojo/public/c/system/message_pipe.h"
--- mojo/public/cpp/system/message_pipe.cc
+++ mojo/public/cpp/system/message_pipe.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "mojo/public/cpp/system/message_pipe.h"
 
+#include <cstring>
+
 #include "base/numerics/safe_math.h"
 
 namespace mojo {
