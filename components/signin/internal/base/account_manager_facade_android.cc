d6496e78bd23ea1ba473386475ea01a0dc4f92a8
Pâris Meuleman <pmeuleman@chromium.org>
2020-04-27 10:58:15 +0000

    [Signin][Android] Move AccountManagerFacade.java and dependants to ../../../public/android
    This CL is a second step towards reorganizing //components/signin java files, this one moves the files from core/browser/android to public/android when relevant.
    
    Bug: 934688
    Change-Id: I0ac30a3d94fa0a09f645b70c1ddc6c5345b94441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814826
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#762795}

--- components/signin/internal/base/account_manager_facade_android.cc
+++ components/signin/internal/base/account_manager_facade_android.cc
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/signin/internal/base/account_manager_facade_android.h"
 
-#include "components/signin/core/browser/android/jni_headers/AccountManagerFacadeProvider_jni.h"
+#include "components/signin/public/android/jni_headers/AccountManagerFacadeProvider_jni.h"
 
 base::android::ScopedJavaLocalRef<jobject>
 AccountManagerFacadeAndroid::GetJavaObject() {
-  return Java_AccountManagerFacadeProvider_getInstance(
+  return signin::Java_AccountManagerFacadeProvider_getInstance(
       base::android::AttachCurrentThread());
 }
