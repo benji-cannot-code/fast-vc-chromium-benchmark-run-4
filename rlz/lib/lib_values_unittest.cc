473400f31ffd3d15ce749d8f9ba6188da25809b4
Hans Wennborg <hans@chromium.org>
2020-04-28 18:56:36 +0000

    Remove/replace unnecessary logging.h includes in .cc files (rlz)
    CHECK, CHECK_EQ etc., and NOTREACHED/NOTIMPLEMENTED have moved
    to the much smaller headers check.h, check_op.h, and notreached.h,
    respectively.
    
    This CL updates .cc files to use those headers instead when
    possible, with the purpose of saving compile time.
    
    (Split out from https://crrev.com/c/2164525 which also has
    notes on how the change was generated.)
    
    Bug: 1031540
    Change-Id: I054e9ccfcbe6417607237e46a42622194956f1eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164513
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763464}

--- rlz/lib/lib_values_unittest.cc
+++ rlz/lib/lib_values_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "rlz/lib/lib_values.h"
 
-#include "base/logging.h"
 #include "rlz/lib/assert.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
--- rlz/lib/rlz_lib_test.cc
+++ rlz/lib/rlz_lib_test.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/bind.h"
-#include "base/logging.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/strings/stringprintf.h"
