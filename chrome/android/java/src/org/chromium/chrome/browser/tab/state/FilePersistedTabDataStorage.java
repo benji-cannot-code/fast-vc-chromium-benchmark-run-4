c551d3b6498ef9c4ef59cede45b46207c5e2ce9f
Ben Joyce <bjoyce@google.com>
2020-04-28 19:19:06 +0000

    Add skip jetify to speed up compile time.
    Needs this CL to land first:
    https://chromium-review.googlesource.com/c/chromium/src/+/2166636
    
    Bug: 1069613
    Change-Id: I20d705ff00ed038f506315b8a22728fd4feec857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2169847
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: benjamin joyce <bjoyce@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763476}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/state/FilePersistedTabDataStorage.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/state/FilePersistedTabDataStorage.java
@@ -5,9 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tab.state;
 
-import android.support.v4.util.AtomicFile;
-
 import androidx.annotation.VisibleForTesting;
+import androidx.core.util.AtomicFile;
 
 import org.chromium.base.Callback;
 import org.chromium.base.Log;
