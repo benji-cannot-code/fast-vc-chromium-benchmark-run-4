995b31086efc965ad390341359a6e14842d87b6d
Mehran Mahmoudi <mahmoudi@chromium.org>
2020-04-28 15:07:29 +0000

    [Tab View] Add TabViewManager and TabViewProvider
    This CL implements a new system for managing custom Views for Tab.
    The TabViewManager class acts as the entry point for all users who want
    to display a View on top of Tab's Content view.
    
    TabViewManager knows the priority of all its users and always displays
    the TabViewProvider with the highest priority.
    
    Internal design doc: http://go/tab-view-clients
    
    Bug: 1073948
    Change-Id: I5d5d0384b512d6722dd0357295de5542b9b77306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159248
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763351}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TabContentManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/content/TabContentManager.java
@@ -35,12 +35,11 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.BooleanCachedFieldTrialParameter;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
-import org.chromium.chrome.browser.tab.SadTab;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tab.TabViewManager;
 import org.chromium.chrome.browser.tasks.tab_management.TabUiFeatureUtilities;
 import org.chromium.chrome.browser.ui.native_page.FrozenNativePage;
 import org.chromium.chrome.browser.ui.native_page.NativePage;
-import org.chromium.chrome.browser.usage_stats.SuspendedTab;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.display.DisplayAndroid;
 
@@ -281,7 +280,7 @@ public class TabContentManager {
 
         View viewToDraw = null;
         if (isNativeViewShowing) {
-            viewToDraw = tab.getContentView();
+            viewToDraw = tab.getView();
         } else if (!(nativePage instanceof FrozenNativePage)) {
             viewToDraw = nativePage.getView();
         }
@@ -645,7 +644,7 @@ public class TabContentManager {
     }
 
     private boolean isNativeViewShowing(Tab tab) {
-        return tab != null && (SadTab.isShowing(tab) || SuspendedTab.isShowing(tab));
+        return tab != null && TabViewManager.get(tab).getCurrentTabViewProvider() != null;
     }
 
     @NativeMethods
--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
@@ -91,6 +91,9 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
     /** The parent view of the ContentView and the InfoBarContainer. */
     private ContentView mContentView;
 
+    /** The view provided by {@link TabViewManager} to be shown on top of Content view. */
+    private View mCustomView;
+
     /** A list of Tab observers.  These are used to broadcast Tab events to listeners. */
     private final ObserverList<TabObserver> mObservers = new ObserverList<>();
 
@@ -335,6 +338,14 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
         }
     }
 
+    /**
+     * Sets a custom {@link View} for this {@link Tab} that replaces Content view.
+     */
+    void setCustomView(@Nullable View view) {
+        mCustomView = view;
+        notifyContentChanged();
+    }
+
     @Override
     public ContentView getContentView() {
         return mContentView;
@@ -342,7 +353,11 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
 
     @Override
     public View getView() {
-        return mNativePage != null ? mNativePage.getView() : mContentView;
+        if (mCustomView != null) return mCustomView;
+
+        if (mNativePage != null) return mNativePage.getView();
+
+        return mContentView;
     }
 
     @Override
@@ -791,7 +806,7 @@ public class TabImpl implements Tab, TabObscuringHandler.Observer {
 
         WebContentsAccessibility wcax = getWebContentsAccessibility(getWebContents());
         if (wcax != null) {
-            boolean isWebContentObscured = isObscured || SadTab.isShowing(this);
+            boolean isWebContentObscured = isObscured || mCustomView != null;
             wcax.setObscuredByAnotherView(isWebContentObscured);
         }
     }
