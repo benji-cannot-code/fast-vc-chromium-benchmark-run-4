24309a9ab66b2ffdfcb3f3833e52648059df156c
Vaclav Brozek <vabr@chromium.org>
2020-04-28 18:59:50 +0000

    Make DoCanonicalizeFileSystemURL perform path tests on the new path
    DoCanonicalizeFileSystemURL transforms a filesystem URL and also
    checks that the path of the inner URL (i.e., the path of the URL
    following the filesystem: prefix) has length at least 2. At the same
    time, canonicalizing the inner path can decrease the URL length, e.g.,
    "/." becomes "/".
    
    So if the path length is checked on the non-canonicalized URL, the
    sanity check passes, whereas on the canonicalized it does not. The
    former situation is what happens before this patch. As a result,
    canonicalizing the URL twice (e.g., once when constructing and the
    second time when applying even empty replacements) results in a valid
    URL the first time and an invalid one the second time. This is a bug,
    because canonicalizing a canonicalized URL should not change its
    properties.
    
    Therefore this CL ensures that the sanity check is done on the already
    canonicalized path. This fixes the above described bug. Some URLs
    which used to be considered as valid (only) on the first
    canonicalization, are now considered invalid every time. Example:
    "filesystem:http://e:/.".
    
    Also, this CL does a minor clean-up in DoCanonicalizeFileSystemURL:
    The function cached a const version of the parsed.inner_parsed()
    pointer, but then sometimes used the cached const version, and
    sometimes the original non-const. The CL turns all of the latter cases
    into the former one.
    
    Bug: 1075515
    Change-Id: I48ae1efc6de9207ea29ae774180a9e2662def9f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2169891
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763469}

--- url/url_canon_filesystemurl.cc
+++ url/url_canon_filesystemurl.cc
@@ -40,7 +40,7 @@ bool DoCanonicalizeFileSystemURL(const CHAR* spec,
   output->Append("filesystem:", 11);
   new_parsed->scheme.len = 10;
 
-  if (!parsed.inner_parsed() || !parsed.inner_parsed()->scheme.is_valid())
+  if (!inner_parsed || !inner_parsed->scheme.is_valid())
     return false;
 
   bool success = true;
@@ -58,8 +58,8 @@ bool DoCanonicalizeFileSystemURL(const CHAR* spec,
       inner_scheme_type = SCHEME_WITH_HOST_AND_PORT;
     }
     success = CanonicalizeStandardURL(
-        spec, parsed.inner_parsed()->Length(), *parsed.inner_parsed(),
-        inner_scheme_type, charset_converter, output, &new_inner_parsed);
+        spec, inner_parsed->Length(), *inner_parsed, inner_scheme_type,
+        charset_converter, output, &new_inner_parsed);
   } else {
     // TODO(ericu): The URL is wrong, but should we try to output more of what
     // we were given?  Echoing back filesystem:mailto etc. doesn't seem all that
@@ -67,7 +67,7 @@ bool DoCanonicalizeFileSystemURL(const CHAR* spec,
     return false;
   }
   // The filesystem type must be more than just a leading slash for validity.
-  success &= parsed.inner_parsed()->path.len > 1;
+  success &= new_inner_parsed.path.len > 1;
 
   success &= CanonicalizePath(source.path, parsed.path, output,
                               &new_parsed->path);
