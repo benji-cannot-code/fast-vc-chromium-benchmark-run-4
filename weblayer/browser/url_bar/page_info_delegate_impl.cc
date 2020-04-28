8095f4fd2570f3071d5422178e9b92522423200d
Mugdha Lakhani <nator@chromium.org>
2020-04-28 09:01:20 +0000

    [WebLayer] Return StatefulSSLHostStateDelegate from delegateimpl.
    Implement PageInfoDelegateImpl::GetStatefulSSLHostStateDelegate().
    A simple browser test verifies that.
    
    Bug: 1052375
    Change-Id: If831ca5d188590250eece4fb6de0f89d0cf27ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166262
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763257}

--- weblayer/browser/url_bar/page_info_delegate_impl.cc
+++ weblayer/browser/url_bar/page_info_delegate_impl.cc
@@ -5,10 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "weblayer/browser/url_bar/page_info_delegate_impl.h"
 
+#include "components/security_interstitials/content/stateful_ssl_host_state_delegate.h"
 #include "components/security_state/content/content_utils.h"
+#include "content/public/browser/browser_context.h"
+#include "weblayer/browser/stateful_ssl_host_state_delegate_factory.h"
 
 PageInfoDelegateImpl::PageInfoDelegateImpl(content::WebContents* web_contents)
-    : web_contents_(web_contents) {}
+    : web_contents_(web_contents) {
+  DCHECK(web_contents_);
+}
 
 permissions::ChooserContextBase* PageInfoDelegateImpl::GetChooserContext(
     ContentSettingsType type) {
@@ -68,9 +73,8 @@ PageInfoDelegateImpl::GetPermissionDecisionAutoblocker() {
 
 StatefulSSLHostStateDelegate*
 PageInfoDelegateImpl::GetStatefulSSLHostStateDelegate() {
-  // TODO(crbug.com/1052375): Implement.
-  NOTREACHED();
-  return nullptr;
+  return weblayer::StatefulSSLHostStateDelegateFactory::GetInstance()
+      ->GetForBrowserContext(GetBrowserContext());
 }
 
 HostContentSettingsMap* PageInfoDelegateImpl::GetContentSettings() {
@@ -103,4 +107,8 @@ PageInfoDelegateImpl::GetTabSpecificContentSettingsDelegate() {
   // TODO(crbug.com/1052375): Implement.
   NOTREACHED();
   return nullptr;
+}
+
+content::BrowserContext* PageInfoDelegateImpl::GetBrowserContext() const {
+  return web_contents_->GetBrowserContext();
 }
