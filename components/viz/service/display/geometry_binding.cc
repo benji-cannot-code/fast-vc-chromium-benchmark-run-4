f580e07875007f3b65ad123aa105446a85ac288f
Piotr Tworek <ptworek@vewd.com>
2020-04-28 08:21:27 +0000

    Add missing string.h include //components/viz/service.
    The code in display/geometry_binding.cc uses memset function, but does
    not include header declaring it. This leads to build failures on Linux
    systems using glibc 2.30, or newer.
    
    Bug: 1074286
    Change-Id: I55ab981118398b2af741bcd76a065536e31faa61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165737
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#763249}

--- components/viz/service/display/geometry_binding.cc
+++ components/viz/service/display/geometry_binding.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/display/geometry_binding.h"
 
 #include <stdint.h>
+#include <string.h>
 
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "ui/gfx/geometry/rect_f.h"
