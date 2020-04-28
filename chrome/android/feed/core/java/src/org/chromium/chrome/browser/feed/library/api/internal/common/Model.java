af139fa02163890069e190b46dcb78be3d8bf471
Carlos Knippschild <carlosk@chromium.org>
2020-04-28 01:59:26 +0000

    Fixes and improvements to uses of Collections methods
    This fixes one case of creating an unmodifiable Set that might be
    modified later and a couple cases where things could be a bit simpler.
    
    Bug: 1070417
    Change-Id: Ib5ed886ec794599e7c091320d45e071e388d4bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166471
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763158}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/library/api/internal/common/Model.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/library/api/internal/common/Model.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.feed.library.api.internal.common;
 
 import org.chromium.components.feed.core.proto.libraries.api.internal.StreamDataProto.StreamDataOperation;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,8 +20,8 @@ public final class Model {
     public final int schemaVersion;
 
     private Model(List<StreamDataOperation> streamDataOperations, int schemaVersion) {
-        this.streamDataOperations = Collections.unmodifiableList(
-                Collections.list(Collections.enumeration(streamDataOperations)));
+        this.streamDataOperations =
+                Collections.unmodifiableList(new ArrayList(streamDataOperations));
         this.schemaVersion = schemaVersion;
     }
 
