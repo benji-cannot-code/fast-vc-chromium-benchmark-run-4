cfb6da18f21d631b5f7908c6e8725e12a4d2e654
Chris Mumford <cmumford@chromium.org>
2020-04-28 00:24:59 +0000

    Stop logging Net.FileSVGZLoadCount UMA metric in test.
    The Net.FileSVGZLoadCount metric logging was added in r611842 and
    was intended to be removed in M73. This wasn't done, but it was removed
    in r695192 during a refactoring (from production code), but the logging
    was moved into a class used only by tests. This change removes the
    logging from the test-only function.
    
    More details at https://crbug.com/857117.
    
    Bug: 930904
    Change-Id: I0760907752c66928a5a1aab96f6e4f378f83425e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163891
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/master@{#763118}

--- net/test/url_request/url_request_test_job_backed_by_file.cc
+++ net/test/url_request/url_request_test_job_backed_by_file.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/compiler_specific.h"
 #include "base/files/file_util.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/synchronization/lock.h"
@@ -163,7 +162,6 @@ URLRequestTestJobBackedByFile::SetUpSourceStream() {
   if (!base::LowerCaseEqualsASCII(file_path_.Extension(), ".svgz"))
     return source;
 
-  UMA_HISTOGRAM_BOOLEAN("Net.FileSVGZLoadCount", true);
   return GzipSourceStream::Create(std::move(source), SourceStream::TYPE_GZIP);
 }
 
