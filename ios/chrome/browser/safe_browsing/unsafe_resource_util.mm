1164f546cfffba2b899cdc28a700d4c3883b443b
Kurt Horimoto <kkhorimoto@chromium.org>
2020-04-28 23:20:07 +0000

    [iOS] UrlCheckerDelegateImpl::StartDisplayingBlockingPageHelper() impl
    Implements StartDisplayingBlockingPageHelper() for the iOS
    UrlCheckerDelegate.  On content//, this call is forwarded to the
    SafeBrowsingService's UI manager.  However, with the committed
    interstitial behavior, the UI manager class is largely just used to
    implement the whitelist and unsafe resource copying functionality,
    which is implemented by SafeBrowsingUrlAllowList and
    SafeBrowsingUnsafeResourceStack on iOS.
    
    This CL updates UrlCheckerDelegateImpl to execute the UnsafeResource
    callbacks and modify the allow list and unsafe resource stack.  After
    this fix, the SafeBrowsingTabHelper will receive do-not-proceed
    signals from the URL checker and can use them to cancel unsafe
    navigations.
    
    A follow-up CL will use the information stored in the unsafe
    resource stack to populate the error page shown for the cancelled
    navigations.
    
    This CL also adds the RunUnsafeResourceCallback() utility function
    which executes the unsafe resource's callback on the appropriate
    thread if it is non-null.
    
    Bug: 1064803
    Change-Id: I22ec557785799452cc3a026a088c9a3863617623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2167005
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763607}

---
+++ ios/chrome/browser/safe_browsing/unsafe_resource_util.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/safe_browsing/unsafe_resource_util.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+void RunUnsafeResourceCallback(
+    const security_interstitials::UnsafeResource& resource,
+    bool proceed,
+    bool showed_interstitial) {
+  DCHECK(resource.callback_thread);
+  DCHECK(!resource.callback.is_null());
+  resource.callback_thread->PostTask(
+      FROM_HERE,
+      base::BindOnce(resource.callback, proceed, showed_interstitial));
+}
