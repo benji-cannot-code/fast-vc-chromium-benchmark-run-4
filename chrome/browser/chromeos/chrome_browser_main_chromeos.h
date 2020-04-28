c476cda464b1df8463efb6361e2456b0a4790d1b
Trent Begin <tbegin@google.com>
2020-04-28 17:54:26 +0000

    network_health: add minimal mojo interface
    This change adds a basic mojo interface to query NetworkHealth for the
    cached active networks and devices.
    
    Bug: chromium:1057739
    Change-Id: I5c7437a522535f8a3154e3ec74bc17d67f4b2dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161701
    Commit-Queue: Trent Begin <tbegin@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763424}

--- chrome/browser/chromeos/chrome_browser_main_chromeos.h
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.h
@@ -52,7 +52,6 @@ class LoginScreenExtensionsLifetimeManager;
 class LoginScreenExtensionsStorageCleaner;
 class LowDiskNotification;
 class NetworkChangeManagerClient;
-class NetworkHealth;
 class NetworkPrefStateObserver;
 class NetworkThrottlingObserver;
 class PowerMetricsReporter;
@@ -71,6 +70,10 @@ class ExternalLoader;
 namespace internal {
 class DBusServices;
 }  // namespace internal
+
+namespace network_health {
+class NetworkHealth;
+}  // namespace network_health
 
 namespace power {
 class SmartChargingManager;
@@ -115,7 +118,7 @@ class ChromeBrowserMainPartsChromeos : public ChromeBrowserMainPartsLinux {
 
  private:
   std::unique_ptr<default_app_order::ExternalLoader> app_order_loader_;
-  std::unique_ptr<NetworkHealth> network_health_;
+  std::unique_ptr<network_health::NetworkHealth> network_health_;
   std::unique_ptr<NetworkPrefStateObserver> network_pref_state_observer_;
   std::unique_ptr<IdleActionWarningObserver> idle_action_warning_observer_;
   std::unique_ptr<RendererFreezer> renderer_freezer_;
