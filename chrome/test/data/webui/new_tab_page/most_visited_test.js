d65f604e70516dfe3e8a25a4eb4ec2981c58b36d
Esmael El-Moslimany <aee@chromium.org>
2020-04-28 19:58:00 +0000

    WebUI NTP: add title for most-visted tile button
    Split action menu button and remove button into two buttons.
    
    Bug: 1064850
    Change-Id: Iee82ff988201cd5c9cdb44054b3d81addd03d4d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168832
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763502}

--- chrome/test/data/webui/new_tab_page/most_visited_test.js
+++ chrome/test/data/webui/new_tab_page/most_visited_test.js
@@ -430,7 +430,7 @@ suite('NewTabPageMostVisitedTest', () => {
     await addTiles(2);
     const {actionMenu, dialog} = mostVisited.$;
     assertFalse(actionMenu.open);
-    queryTiles()[0].querySelector('cr-icon-button').click();
+    queryTiles()[0].querySelector('#actionMenuButton').click();
     assertTrue(actionMenu.open);
     assertFalse(dialog.open);
     mostVisited.$.actionMenuEdit.click();
@@ -465,7 +465,7 @@ suite('NewTabPageMostVisitedTest', () => {
       cancelButton = dialog.querySelector('.cancel-button');
       await addTiles(2);
       tile = queryTiles()[1];
-      actionMenuButton = tile.querySelector('cr-icon-button');
+      actionMenuButton = tile.querySelector('#actionMenuButton');
       actionMenuButton.click();
       mostVisited.$.actionMenuEdit.click();
     });
@@ -527,7 +527,7 @@ suite('NewTabPageMostVisitedTest', () => {
     const {actionMenu, actionMenuRemove: removeButton} = mostVisited.$;
     await addTiles(2);
     const secondTile = queryTiles()[1];
-    const actionMenuButton = secondTile.querySelector('cr-icon-button');
+    const actionMenuButton = secondTile.querySelector('#actionMenuButton');
 
     assertFalse(actionMenu.open);
     actionMenuButton.click();
@@ -542,7 +542,7 @@ suite('NewTabPageMostVisitedTest', () => {
 
   test('remove with icon button (customLinksEnabled=false)', async () => {
     await addTiles(1, /* customLinksEnabled */ false);
-    const removeButton = queryTiles()[0].querySelector('cr-icon-button');
+    const removeButton = queryTiles()[0].querySelector('#removeButton');
     const deleteCalled = testProxy.handler.whenCalled('deleteMostVisitedTile');
     removeButton.click();
     assertEquals('https://a/', (await deleteCalled).url);
