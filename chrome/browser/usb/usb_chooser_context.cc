8ae5775daf10f217d9405d7b34894442783c6fa3
Reilly Grant <reillyg@chromium.org>
2020-04-28 23:52:46 +0000

    [usb] Remove temporary logging for debugging issue 1041804
    This change reverts r761214 and r759527 to remove the log messages added
    to debug issue 1041804. These are no longer needed and would contribute
    to log spam.
    
    Bug: 1041804
    Change-Id: Ie6d6227bad24abf87f32ad7daf2581060953c2c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171799
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763615}

--- chrome/browser/usb/usb_chooser_context.cc
+++ chrome/browser/usb/usb_chooser_context.cc
@@ -239,7 +239,6 @@ void UsbChooserContext::OnDeviceInfoRefreshed(
 #endif
 
 UsbChooserContext::~UsbChooserContext() {
-  LOG(INFO) << "USB chooser context being destroyed.";
   OnDeviceManagerConnectionError();
 }
 
@@ -604,7 +603,6 @@ void UsbChooserContext::OnDeviceRemoved(
 }
 
 void UsbChooserContext::OnDeviceManagerConnectionError() {
-  LOG(INFO) << "Got disconnected from USB device service.";
   device_manager_.reset();
   client_receiver_.reset();
   devices_.clear();
