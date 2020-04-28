1edbcff47dbcde29555ffd481186a47b435ae57f
Chris Palmer <palmer@chromium.org>
2020-04-28 02:49:22 +0000

    Re-add `DFAKE_SCOPED_LOCK_THREAD_LOCKED` checks to ref_counted.h.
    Fixed a test call site to work with the checks.
    
    Bug: None
    Change-Id: I3d8ab9ad6835a720f5a51c6cc0b2b92d13616306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1622475
    Commit-Queue: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763175}

--- base/deferred_sequenced_task_runner_unittest.cc
+++ base/deferred_sequenced_task_runner_unittest.cc
@@ -23,7 +23,8 @@ namespace {
 
 class DeferredSequencedTaskRunnerTest : public testing::Test {
  public:
-  class ExecuteTaskOnDestructor : public RefCounted<ExecuteTaskOnDestructor> {
+  class ExecuteTaskOnDestructor
+      : public RefCountedThreadSafe<ExecuteTaskOnDestructor> {
    public:
     ExecuteTaskOnDestructor(
         DeferredSequencedTaskRunnerTest* executor,
@@ -32,7 +33,7 @@ class DeferredSequencedTaskRunnerTest : public testing::Test {
           task_id_(task_id) {
     }
   private:
-   friend class RefCounted<ExecuteTaskOnDestructor>;
+   friend class RefCountedThreadSafe<ExecuteTaskOnDestructor>;
    virtual ~ExecuteTaskOnDestructor() { executor_->ExecuteTask(task_id_); }
    DeferredSequencedTaskRunnerTest* executor_;
    int task_id_;
