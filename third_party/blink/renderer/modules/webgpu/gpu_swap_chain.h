658984d133819c826ccb7440f054302f25ebd265
Yan, Shaobo <shaobo.yan@intel.com>
2020-04-28 10:01:59 +0000

    Reland implement CopyImageBitmapToTexture part II : Enable GPU T2T copy path
    Reland with a fix for doing memcpy on a temp variable in
    DawnTextureFromImageBitmapTest_VerifyAccessTexture. Fix this by storing
    the temp mailbox used in ProduceDawnTextureFromImageBitmap
    into a new added class member.
    
    > Revert "Implement CopyImageBitmapToTexture part II : Enable GPU T2T copy path"
    
    > This reverts commit 15e959f31ba16b708a38099a73c4deeb856da7f9.
    
    > Reason for revert:
    > Introduced asan failure in DawnTextureFromImageBitmapTest.VerifyAccessTexture
    > See: https://ci.chromium.org/p/chromium/builders/ci/win-asan/11729
    
    Original change's description:
    > Implement CopyImageBitmapToTexture part II : Enable GPU T2T copy path
    >
    > This patch added the path that uploading imageBitmap contents to WebGPU texture if the backed resource
    > of imageBitmap is on GPU.
    >
    > It leverage shared image system to share content from image bitmap to a dawn texture as intermediate resource
    > and than starts a encoder and submit a T2T copy.
    >
    > But this path has lots of constraints and may failed. When it is not possible to using GPU uploading path, the API
    > will fallback to CPU path.
    >
    > BUG=966582
    
    Change-Id: Icfb0a2b153cdcf95101106f849dfdd55dce8d83a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163761
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Shaobo Yan <shaobo.yan@intel.com>
    Cr-Commit-Position: refs/heads/master@{#763269}

--- third_party/blink/renderer/modules/webgpu/gpu_swap_chain.h
+++ third_party/blink/renderer/modules/webgpu/gpu_swap_chain.h
@@ -52,6 +52,7 @@ class GPUSwapChain : public ScriptWrappable,
   Member<GPUDevice> device_;
   Member<GPUCanvasContext> context_;
   WGPUTextureUsage usage_;
+  WGPUTextureFormat format_;
 
   Member<GPUTexture> texture_;
 };
