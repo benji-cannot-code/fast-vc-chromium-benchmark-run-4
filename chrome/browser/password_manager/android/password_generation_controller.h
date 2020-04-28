0e66ada651e597523c1dff7810fabe8041e06869
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-04-28 16:40:20 +0000

    Cleanup in password client and password generation controller
    The CL removes an unnecessary function call for desktop in the client
    and a duplicated forward declaration in the generation controller.
    
    Bug: None
    Change-Id: Ic1b805741722da112073f4a0b9001967c01b7000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162755
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#763390}

--- chrome/browser/password_manager/android/password_generation_controller.h
+++ chrome/browser/password_manager/android/password_generation_controller.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace password_manager {
 class PasswordManagerDriver;
-class PasswordManagerDriver;
 }  // namespace password_manager
 
 // Interface for the controller responsible for overseeing the UI flow for
