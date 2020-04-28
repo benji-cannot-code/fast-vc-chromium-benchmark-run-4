99f927f5e5f976ea567785912706bc9ac562e1ba
Morten Stenshorne <mstensho@chromium.org>
2020-04-28 07:18:50 +0000

    [FragmentItem] Prevent dangling LayoutObject ptrs in block fragmentation.
    NGInlineCursor doesn't work with block fragmentation, so we'd end up
    with NGFragmentItem objects with dangling pointers to LayoutObject. To
    fix this, perform a full walk through all fragment items in all
    fragments, if it's likely that we're in block fragmentation.
    
    CL:2152824 caused two tests to crash because of this, and it therefore
    got reverted. Add another test now, which would also crash without the
    aforementioned CL.
    
    Bug: 1075517
    Change-Id: I97e2fae63d89031ea258ba146898e7fcfe56bd45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165800
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763233}

--- third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items.cc
+++ third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items.cc
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items.h"
 
+#include "third_party/blink/renderer/core/layout/layout_block.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items_builder.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 
 namespace blink {
 
@@ -105,6 +107,22 @@ void NGFragmentItems::ClearAssociatedFragments() const {
 // static
 void NGFragmentItems::LayoutObjectWillBeMoved(
     const LayoutObject& layout_object) {
+  if (UNLIKELY(layout_object.IsInsideFlowThread())) {
+    // TODO(crbug.com/829028): Make NGInlineCursor handle block
+    // fragmentation. For now, perform a slow walk here manually.
+    const LayoutBlock& container = *layout_object.ContainingBlock();
+    for (wtf_size_t idx = 0; idx < container.PhysicalFragmentCount(); idx++) {
+      const NGPhysicalBoxFragment& fragment =
+          *container.GetPhysicalFragment(idx);
+      DCHECK(fragment.Items());
+      for (const auto& item : fragment.Items()->Items()) {
+        if (item->GetLayoutObject() == &layout_object)
+          item->LayoutObjectWillBeMoved();
+      }
+    }
+    return;
+  }
+
   NGInlineCursor cursor;
   cursor.MoveTo(layout_object);
   for (; cursor; cursor.MoveToNextForSameLayoutObject()) {
@@ -116,6 +134,22 @@ void NGFragmentItems::LayoutObjectWillBeMoved(
 // static
 void NGFragmentItems::LayoutObjectWillBeDestroyed(
     const LayoutObject& layout_object) {
+  if (UNLIKELY(layout_object.IsInsideFlowThread())) {
+    // TODO(crbug.com/829028): Make NGInlineCursor handle block
+    // fragmentation. For now, perform a slow walk here manually.
+    const LayoutBlock& container = *layout_object.ContainingBlock();
+    for (wtf_size_t idx = 0; idx < container.PhysicalFragmentCount(); idx++) {
+      const NGPhysicalBoxFragment& fragment =
+          *container.GetPhysicalFragment(idx);
+      DCHECK(fragment.Items());
+      for (const auto& item : fragment.Items()->Items()) {
+        if (item->GetLayoutObject() == &layout_object)
+          item->LayoutObjectWillBeDestroyed();
+      }
+    }
+    return;
+  }
+
   NGInlineCursor cursor;
   cursor.MoveTo(layout_object);
   for (; cursor; cursor.MoveToNextForSameLayoutObject()) {
