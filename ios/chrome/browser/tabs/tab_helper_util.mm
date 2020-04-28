3a6e1564e6b301478d29a27c45170ee6aa534d0b
Kurt Horimoto <kkhorimoto@chromium.org>
2020-04-28 18:39:15 +0000

    [iOS] Create SafeBrowsingUnsafeResourceContainer
    This tab helper is used to store copies of UnsafeResources that are
    detected by the URL checks kicked off by the SafeBrowsingTabHelper.
    It will be populated with UnsafeResources by the UrlCheckerDelegateImpl
    before calling the SafeBrowsingTabHelper::UrlCheckerClient's completion
    callback.  When the WebClient is preparing the error page for the
    unsafe navigation, it can pop the UnsafeResource copy and use it to
    populate the WebUI.
    
    For unsafe resources in the main frame, a copy is stored directly in
    the tab helper, as the associated pending NavigationItem may be deleted
    due to the navigation cancellation.
    
    For unsafe subresources, a copy is stored in the NavigationItem's user
    data.  When the SafeBrowsingTabHelper detects an unsafe subresource, it
    will kick off a reload of the NavigationItem and use the presence of
    the unsafe subresource in the stack to cancel the main frame navigation
    and display the error page.
    
    Bug: 1064803
    Change-Id: I96c99e0d5ca23443400a493023edb9f13a4fd393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166128
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763455}

--- ios/chrome/browser/tabs/tab_helper_util.mm
+++ ios/chrome/browser/tabs/tab_helper_util.mm
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/reading_list/reading_list_model_factory.h"
 #import "ios/chrome/browser/reading_list/reading_list_web_state_observer.h"
 #import "ios/chrome/browser/safe_browsing/safe_browsing_tab_helper.h"
+#import "ios/chrome/browser/safe_browsing/safe_browsing_unsafe_resource_container.h"
 #import "ios/chrome/browser/safe_browsing/safe_browsing_url_allow_list.h"
 #import "ios/chrome/browser/search_engines/search_engine_tab_helper.h"
 #import "ios/chrome/browser/sessions/ios_chrome_session_tab_helper.h"
@@ -126,9 +127,11 @@ void AttachTabHelpers(web::WebState* web_state, bool for_prerender) {
           safe_browsing::kSafeBrowsingAvailableOnIOS)) {
     SafeBrowsingTabHelper::CreateForWebState(web_state);
     if (!for_prerender) {
-      // Unsafe navigations will never be allowed for prerender WebStates, so
-      // the allow list does not need to be created.
+      // Unsafe navigations will never be allowed for prerender WebStates, and
+      // error pages will never be shown, so the allow list and unsafe resource
+      // container do not need to be created.
       SafeBrowsingUrlAllowList::CreateForWebState(web_state);
+      SafeBrowsingUnsafeResourceContainer::CreateForWebState(web_state);
     }
   }
 
