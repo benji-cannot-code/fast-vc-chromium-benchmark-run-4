04f8eacc88a955d87026fee2abd7ccf46587d80a
Hesen Zhang <hesen@google.com>
2020-04-28 18:20:05 +0000

    [Upboarding]: Move ProtoDb forward declaration to factory helper.
    - Fix the potential broken when moving the project to components/.
    
    Bug: 1074682
    Change-Id: I1301e6e45facee1837e718f375bd4feb77ef5556
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168242
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763444}

--- chrome/browser/upboarding/query_tiles/tile_service_factory_helper.cc
+++ chrome/browser/upboarding/query_tiles/tile_service_factory_helper.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/upboarding/query_tiles/internal/config.h"
 #include "chrome/browser/upboarding/query_tiles/internal/tile_manager.h"
 #include "chrome/browser/upboarding/query_tiles/internal/tile_service_impl.h"
-#include "chrome/browser/upboarding/query_tiles/internal/tile_store.h"
 #include "components/image_fetcher/core/image_fetcher_service.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/leveldb_proto/public/proto_database_provider.h"
--- chrome/browser/upboarding/query_tiles/tile_service_factory_helper.h
+++ chrome/browser/upboarding/query_tiles/tile_service_factory_helper.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/files/file_path.h"
+#include "chrome/browser/upboarding/query_tiles/internal/tile_store.h"
 
 namespace leveldb_proto {
 class ProtoDatabaseProvider;
