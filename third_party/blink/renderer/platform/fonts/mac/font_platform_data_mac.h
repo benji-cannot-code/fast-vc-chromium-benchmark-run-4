e6ac569b6acdeca9317d5e00d3a4f56ec3f0af76
Dominik Röttsches <drott@chromium.org>
2020-04-28 09:53:25 +0000

    Avoid redundant makeClone() calls in Mac system font instantiation
    The optical sizing (`opsz`) and other axis parameters are directly
    compared against the base font instance. Variable axis limits are not
    taken into account. The San Francisco system font of Mac OS 10.14 and
    above has an optical size axis of 19.9 min and 20 max value, so we're
    calling makeClone() too often for usual font sizes of 12, 16 etc. as 12
    != 19.9 but the optical size is always set to that value by default for
    font sizes under 19.9.
    
    Introduce a clamped comparison function which clamps the value to be set
    to limits first, then performs the comparison to what the font instance
    has.
    
    Bug: 1075572
    Change-Id: I0d5e6eda96babcc2c0c48e12d57ab83337283759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144086
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Ben Wagner <bungeman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763267}

--- third_party/blink/renderer/platform/fonts/mac/font_platform_data_mac.h
+++ third_party/blink/renderer/platform/fonts/mac/font_platform_data_mac.h
@@ -33,16 +33,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_MAC_FONT_PLATFORM_DATA_MAC_H_
 
 #include "third_party/blink/renderer/platform/fonts/font_optical_sizing.h"
+#include "third_party/blink/renderer/platform/platform_export.h"
 
 #include <memory>
 
 @class NSFont;
+class SkTypeface;
+typedef uint32_t SkFourByteTag;
 
 namespace blink {
 
 enum class FontOrientation;
 class FontPlatformData;
 class FontVariationSettings;
+
+bool PLATFORM_EXPORT VariableAxisChangeEffective(SkTypeface* typeface,
+                                                 SkFourByteTag axis,
+                                                 float new_value);
 
 std::unique_ptr<FontPlatformData> FontPlatformDataFromNSFont(
     NSFont*,
