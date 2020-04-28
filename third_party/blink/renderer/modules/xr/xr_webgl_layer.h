c90ae77d412ae022d0dd2cfa21f573639a0066d1
Brandon Jones <bajones@chromium.org>
2020-04-28 17:42:02 +0000

    Added XRLayer interface to match latest WebXR spec change
    Implements https://immersive-web.github.io/webxr/#xrlayer-interface
    
    New interface added to serve as a common base class between the existing
    WebGL layer and the WIP layers module. No changes in functionality.
    
    Bug: 1074150
    Change-Id: I695ff842cce5d38cf37b263acecf95dafc21d572
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164146
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763414}

--- third_party/blink/renderer/modules/xr/xr_webgl_layer.h
+++ third_party/blink/renderer/modules/xr/xr_webgl_layer.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/webgl_rendering_context_or_webgl2_rendering_context.h"
 #include "third_party/blink/renderer/modules/webgl/webgl2_rendering_context.h"
 #include "third_party/blink/renderer/modules/webgl/webgl_rendering_context.h"
+#include "third_party/blink/renderer/modules/xr/xr_layer.h"
 #include "third_party/blink/renderer/modules/xr/xr_view.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/graphics/gpu/xr_webgl_drawing_buffer.h"
@@ -28,7 +29,7 @@ class WebGLRenderingContextBase;
 class XRSession;
 class XRViewport;
 
-class XRWebGLLayer final : public ScriptWrappable {
+class XRWebGLLayer final : public XRLayer {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
@@ -45,8 +46,6 @@ class XRWebGLLayer final : public ScriptWrappable {
       const WebGLRenderingContextOrWebGL2RenderingContext&,
       const XRWebGLLayerInit*,
       ExceptionState&);
-
-  XRSession* session() const { return session_; }
 
   WebGLRenderingContextBase* context() const { return webgl_context_; }
 
@@ -80,8 +79,6 @@ class XRWebGLLayer final : public ScriptWrappable {
   void Trace(Visitor*) override;
 
  private:
-  const Member<XRSession> session_;
-
   Member<XRViewport> left_viewport_;
   Member<XRViewport> right_viewport_;
 
