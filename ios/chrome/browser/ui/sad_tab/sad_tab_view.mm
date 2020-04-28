e78a187d02324f76cc86883d6a07e81b70266c56
edchin <edchin@chromium.org>
2020-04-28 02:38:55 +0000

    [iOS] Add hover pointer support to SadTab
    iOS 13.4 introduces new APIs for mouse cursor support on iPad.
    
    Buttons should react to pointer hover. This CL enables the default
    hover effect on sad tab buttons.
    
    Test: iPad only. Enable pointer support flag. Ensure sad tab buttons react to pointer hover.
    Bug: 1073961
    Change-Id: I519901577b84f5c4d3324ad375d75b59b2aa07a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168010
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763171}

--- ios/chrome/browser/ui/sad_tab/sad_tab_view.mm
+++ ios/chrome/browser/ui/sad_tab/sad_tab_view.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/sad_tab/sad_tab_view.h"
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/sys_string_conversions.h"
 #include "components/grit/components_scaled_resources.h"
@@ -13,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/chrome_url_constants.h"
 #import "ios/chrome/browser/ui/colors/MDCPalette+CrAdditions.h"
 #import "ios/chrome/browser/ui/commands/application_commands.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/label_link_controller.h"
 #include "ios/chrome/browser/ui/util/rtl_geometry.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
@@ -561,6 +563,13 @@ NSString* const kMessageTextViewBulletRTLFormat = @"\u202E%@\u202C";
     [_actionButton addTarget:self
                       action:@selector(handleActionButtonTapped)
             forControlEvents:UIControlEventTouchUpInside];
+#if defined(__IPHONE_13_4)
+    if (@available(iOS 13.4, *)) {
+      if (base::FeatureList::IsEnabled(kPointerSupport)) {
+        _actionButton.pointerInteractionEnabled = YES;
+      }
+    }
+#endif  // defined(__IPHONE_13_4)
   }
   return _actionButton;
 }
