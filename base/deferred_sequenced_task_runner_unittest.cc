3407a41b1e2f7c6092e5f4661e89576f417f5161
Yuki Shiino <yukishiino@chromium.org>
2020-04-28 06:20:51 +0000

    Revert "Re-add `DFAKE_SCOPED_LOCK_THREAD_LOCKED` checks to ref_counted.h."
    This reverts commit 64f222e1805a468245d42df1e0a8389b34c95b12.
    
    Reason for revert: Suspicious about causing test failures on bots.
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-dbg/18361
    
    Original change's description:
    > Re-add `DFAKE_SCOPED_LOCK_THREAD_LOCKED` checks to ref_counted.h.
    > 
    > Fixed a test call site to work with the checks.
    > 
    > Bug: None
    > Change-Id: I3d8ab9ad6835a720f5a51c6cc0b2b92d13616306
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1622475
    > Commit-Queue: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#763175}
    
    TBR=rohitrao@chromium.org,maruel@chromium.org,danakj@chromium.org,palmer@chromium.org,msarda@chromium.org,eugenebut@chromium.org
    
    Change-Id: I8900a6da32d4b27d3ab812b754e5f591430621e6
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2169531
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763222}

--- base/deferred_sequenced_task_runner_unittest.cc
+++ base/deferred_sequenced_task_runner_unittest.cc
@@ -23,8 +23,7 @@ namespace {
 
 class DeferredSequencedTaskRunnerTest : public testing::Test {
  public:
-  class ExecuteTaskOnDestructor
-      : public RefCountedThreadSafe<ExecuteTaskOnDestructor> {
+  class ExecuteTaskOnDestructor : public RefCounted<ExecuteTaskOnDestructor> {
    public:
     ExecuteTaskOnDestructor(
         DeferredSequencedTaskRunnerTest* executor,
@@ -33,7 +32,7 @@ class DeferredSequencedTaskRunnerTest : public testing::Test {
           task_id_(task_id) {
     }
   private:
-   friend class RefCountedThreadSafe<ExecuteTaskOnDestructor>;
+   friend class RefCounted<ExecuteTaskOnDestructor>;
    virtual ~ExecuteTaskOnDestructor() { executor_->ExecuteTask(task_id_); }
    DeferredSequencedTaskRunnerTest* executor_;
    int task_id_;
