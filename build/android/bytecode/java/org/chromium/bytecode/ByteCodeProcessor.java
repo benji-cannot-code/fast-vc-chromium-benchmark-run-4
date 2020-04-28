7900e2af92130267b2fa59bf5f5745ec5c5b635c
Andrew Grieve <agrieve@chromium.org>
2020-04-28 04:47:29 +0000

    Android: Remove BuildHooksAndroid.java
    It was needed for a now-obsolete downstream feature.
    
    References to no_build_hooks to be cleaned up in a follow-up.
    
    Bug: 1042017
    Change-Id: I4ecb7eee9321b179a47908c7ba01075f52306732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165665
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763199}

--- build/android/bytecode/java/org/chromium/bytecode/ByteCodeProcessor.java
+++ build/android/bytecode/java/org/chromium/bytecode/ByteCodeProcessor.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.bytecode;
 
-import static org.objectweb.asm.ClassWriter.COMPUTE_FRAMES;
-
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
@@ -46,9 +44,6 @@ import java.util.zip.ZipOutputStream;
 /**
  * Java application that takes in an input jar, performs a series of bytecode transformations,
  * and generates an output jar.
- *
- * One type of transformation are performed:
- * 1) Providing support for custom resources via {@link CustomResourcesClassAdapter}
  */
 class ByteCodeProcessor {
     private static final String CLASS_FILE_SUFFIX = ".class";
@@ -56,7 +51,6 @@ class ByteCodeProcessor {
     private static final int BUFFER_SIZE = 16384;
     private static boolean sVerbose;
     private static boolean sIsPrebuilt;
-    private static boolean sShouldUseCustomResources;
     private static boolean sShouldUseThreadAnnotations;
     private static boolean sShouldCheckClassPath;
     private static ClassLoader sDirectClassPathClassLoader;
@@ -99,16 +93,7 @@ class ByteCodeProcessor {
                     sFullClassPathClassLoader, sFullClassPathJarPaths, sIsPrebuilt, sVerbose);
         }
 
-        ClassWriter writer;
-        if (sShouldUseCustomResources) {
-            // Use the COMPUTE_FRAMES flag to have asm figure out the stack map frames.
-            // This is necessary because GCMBaseIntentService in android_gcm_java contains
-            // incorrect stack map frames. This option slows down processing time by 2x.
-            writer = new CustomClassLoaderClassWriter(
-                    sFullClassPathClassLoader, reader, COMPUTE_FRAMES);
-        } else {
-            writer = new ClassWriter(reader, 0);
-        }
+        ClassWriter writer = new ClassWriter(reader, 0);
         ClassVisitor chain = writer;
         /* DEBUGGING:
          To see objectweb.asm code that will generate bytecode for a given class:
@@ -127,10 +112,6 @@ class ByteCodeProcessor {
         */
         if (sShouldUseThreadAnnotations) {
             chain = new ThreadAssertionClassAdapter(chain);
-        }
-        if (sShouldUseCustomResources) {
-            chain = new CustomResourcesClassAdapter(
-                    chain, reader.getClassName(), reader.getSuperName(), sFullClassPathClassLoader);
         }
         reader.accept(chain, 0);
         byte[] patchedByteCode = writer.toByteArray();
@@ -248,7 +229,6 @@ class ByteCodeProcessor {
         String outputJarPath = args[currIndex++];
         sVerbose = args[currIndex++].equals("--verbose");
         sIsPrebuilt = args[currIndex++].equals("--is-prebuilt");
-        sShouldUseCustomResources = args[currIndex++].equals("--enable-custom-resources");
         sShouldUseThreadAnnotations = args[currIndex++].equals("--enable-thread-annotations");
         sShouldCheckClassPath = args[currIndex++].equals("--enable-check-class-path");
         int sdkJarsLength = Integer.parseInt(args[currIndex++]);
--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -29,7 +29,6 @@ import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
-import org.chromium.build.BuildHooksAndroid;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.AfterStartupTaskUtils;
 import org.chromium.chrome.browser.AppHooks;
@@ -428,9 +427,6 @@ public class ProcessInitializationHandler {
 
         deferredStartupHandler.addDeferredTask(
                 ProcessInitializationHandler::logEGLShaderCacheSizeHistogram);
-
-        deferredStartupHandler.addDeferredTask(
-                BuildHooksAndroid::maybeRecordResourceMetrics);
 
         deferredStartupHandler.addDeferredTask(
                 () -> MediaViewerUtils.updateMediaLauncherActivityEnabled());
