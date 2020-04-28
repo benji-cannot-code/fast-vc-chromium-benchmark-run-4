cc864b2fb69ef84e982e71eee5f6b58a2a70cd97
Henrique Ferreiro <hferreiro@igalia.com>
2020-04-28 16:21:09 +0000

    Make //ui/base/x and //ui/gfx/x export their configs
    Targets depending on //ui/base/x need to use "linux:xrandr" config and
    the ones depending on //ui/gfx/x, "linux:x11". This CL makes those
    configs public in targets where they are used, so that they can be
    omitted everywhere else.
    
    Change-Id: Ied1f5cc85e9af4172b1472a75d53a71526d491f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162864
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763380}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -137,12 +137,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/diagnostics/diagnostics_writer.h"
 #endif
 
-#if defined(USE_X11)
-#include <stdlib.h>
-#include <string.h>
-#include "ui/base/x/x11_util.h"  // nogncheck
-#endif
-
 #if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_ANDROID)
 #include "components/crash/core/app/breakpad_linux.h"
 #include "v8/include/v8-wasm-trap-handler-posix.h"
