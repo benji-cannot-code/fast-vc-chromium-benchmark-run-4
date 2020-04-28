23b9f36e7f5448d1bf1787b850155cc08bd81a55
Piotr Tworek <ptworek@vewd.com>
2020-04-28 06:51:04 +0000

    Add missing string.h include in ui/gfx/x/x11_types.cc
    The code uses memset function defined in string.h. It does not include
    the necessary header. On Linux this fails to build starting with glibc
    2.30.
    
    Bug: 1074286
    Change-Id: I0c9f3b0965e0f46114dcaff0f2841a9ece8a461a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164609
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#763228}

--- ui/gfx/x/x11_types.cc
+++ ui/gfx/x/x11_types.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/x/x11_types.h"
 
 #include <X11/Xlib.h>
+#include <string.h>
 
 #include "base/command_line.h"
 #include "base/logging.h"
