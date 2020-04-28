c67135dea2ee440409d52cdbc58883207ad502c3
Akihiro Ota <akihiroota@chromium.org>
2020-04-28 21:11:02 +0000

    Enable ChromeVox search menus by default.
    Bug: 684762
    Change-Id: Iba524b75bbceafd69ca0af04461b5badb1a2ebd7
    Relnotes: Launch ChromeVox search menus feature.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168795
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763543}

--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/panel.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/panel.js
@@ -154,12 +154,12 @@ Panel = class {
      * @type {boolean}
      * @private
      */
-    Panel.menuSearchEnabled_ = false;
+    Panel.menuSearchEnabled_ = true;
 
     chrome.commandLinePrivate.hasSwitch(
-        'enable-experimental-accessibility-chromevox-search-menus',
-        function(enabled) {
-          Panel.menuSearchEnabled_ = enabled;
+        'disable-experimental-accessibility-chromevox-search-menus',
+        (enabled) => {
+          Panel.menuSearchEnabled_ = !enabled;
         });
   }
 
@@ -322,6 +322,7 @@ Panel = class {
       Panel.clearMenus();
       Panel.pendingCallback_ = null;
 
+      // Build the top-level menus.
       const searchMenu = (Panel.menuSearchEnabled_) ?
           Panel.addSearchMenu('panel_search_menu') :
           null;
