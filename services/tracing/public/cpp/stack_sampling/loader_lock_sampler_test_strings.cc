928c1083998474a15f91c0cc65f0657f99a122ec
Joe Mason <joenotcharles@google.com>
2020-04-29 19:17:27 +0000

    Reland "Sample the loader lock when taking stack samples."
    This is a reland of 696784436e1576f4d4d2ae0dfc740bb04202eac4 with
    modifications:
    
    * Added missed NTAPI calling convention.
    * Loader lock sampling is only enabled on 64-bit Windows builds due to
      failing tests on 32-bit builds.
    
    Original change's description:
    > Sample the loader lock when taking stack samples.
    >
    > Bug: 1065879
    >
    > Change-Id: I05ca50043d8efbcdc3ae6d4767fb289162d85198
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2127911
    > Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Reviewed-by: oysteine <oysteine@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#762064}
    
    Bug: 1065879
    Change-Id: Ie660d4d954c493b32342074b062ee6585450a8d4
    Cq-Include-Trybots: chromium/try:win7-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166417
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763943}

---
+++ services/tracing/public/cpp/stack_sampling/loader_lock_sampler_test_strings.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/tracing/public/cpp/stack_sampling/loader_lock_sampler_test_strings.h"
+
+namespace tracing {
+
+namespace loader_lock_sampler_test {
+
+const wchar_t kWaitForLockEventName[] =
+    L"LoaderLockSamplerTestWaitForLockEvent";
+const wchar_t kDropLockEventName[] = L"LoaderLockSamplerTestDropLockEvent";
+
+}  // namespace loader_lock_sampler_test
+
+}  // namespace tracing
