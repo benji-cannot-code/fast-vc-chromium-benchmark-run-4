364d3f9b49eccafec68496d47e67fca9dbd5b5b9
dpapad <dpapad@chromium.org>
2020-04-29 22:12:48 +0000

    WebUI: Remove TestBrowserProxy#whenCalled() 2nd parameter.
    Seems unnecessary, as there is only a single usage of it, and it can
    be replaced with a getCallCount() call instead.
    
    Bug: None
    Change-Id: I08574147206ded1fa029028138680bbb3e43964b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168807
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764014}

--- chrome/test/data/webui/new_tab_page/most_visited_test.js
+++ chrome/test/data/webui/new_tab_page/most_visited_test.js
@@ -100,7 +100,7 @@ suite('NewTabPageMostVisitedTest', () => {
     updateScreenWidth(true, true);
   }
 
-  setup(async () => {
+  setup(() => {
     PolymerTest.clearBody();
 
     testProxy = createTestProxy();
@@ -131,8 +131,7 @@ suite('NewTabPageMostVisitedTest', () => {
     BrowserProxy.instance_ = testProxy;
     mostVisited = document.createElement('ntp-most-visited');
     document.body.appendChild(mostVisited);
-
-    await testProxy.whenCalled('matchMedia', 2);
+    assertEquals(2, testProxy.getCallCount('matchMedia'));
     wide();
   });
 
