5e5aeabddce941926f8f371b5b349c865f696b95
Vladimir Levin <vmpstr@chromium.org>
2020-04-29 18:30:02 +0000

    CSSContentVisibility: Fix erroneous DCHECK in visual overflow recalc.
    This patch fixes an erroneous DCHECK in the visual overflow recalc.
    
    The intent of the DCHECK was to ensure that we don't call the function
    in the locked subtree that prevented prepaint. However, as it stands,
    forced updates cause it to trigger, since it only checks that we are
    in a locked subtree.
    
    The patch updates this to instead check that we are in a locked subtree
    that prevents pre-paint.
    
    R=chrishtr@chromium.org
    
    Change-Id: Ib2f23529a86a0adf15beff1590388bf7387a4db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173150
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763919}

--- third_party/blink/renderer/core/layout/layout_block.cc
+++ third_party/blink/renderer/core/layout/layout_block.cc
@@ -438,7 +438,7 @@ void LayoutBlock::UpdateBlockLayout(bool) {
 void LayoutBlock::AddVisualOverflowFromChildren() {
   // It is an error to call this function on a LayoutBlock that it itself inside
   // a display-locked subtree.
-  DCHECK(!DisplayLockUtilities::NearestLockedExclusiveAncestor(*this));
+  DCHECK(!DisplayLockUtilities::LockedAncestorPreventingPrePaint(*this));
   if (PrePaintBlockedByDisplayLock(DisplayLockLifecycleTarget::kChildren))
     return;
 
@@ -2121,8 +2121,7 @@ void LayoutBlock::RecalcChildVisualOverflow() {
   DCHECK(!IsTable());
   // It is an error to call this function on a LayoutBlock that it itself inside
   // a display-locked subtree.
-  DCHECK(!DisplayLockUtilities::NearestLockedExclusiveAncestor(*this));
-
+  DCHECK(!DisplayLockUtilities::LockedAncestorPreventingPrePaint(*this));
   if (PrePaintBlockedByDisplayLock(DisplayLockLifecycleTarget::kChildren))
     return;
 
