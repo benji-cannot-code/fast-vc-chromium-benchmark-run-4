fa4b65a6e3a0005488dca0d176e43eba5fd434e6
Becca Hughes <beccahughes@chromium.org>
2020-04-29 21:19:52 +0000

    Reland "[Media Feeds] Use associated origin for reset"
    This is a reland of 64faa6fdc94e97300f450e93ac25a7f05b0f19de
    
    TBR=steimel@chromium.org
    
    Original change's description:
    > [Media Feeds] Use associated origin for reset
    >
    > Change the reset API to use associated origins
    > and also add the feed origin as an associated
    > origin once we have stored content. This allows
    > us to easily reset based on origin.
    >
    > BUG=1072702
    >
    > Change-Id: Ideb8386af7a9a4acf7ade4f17dc70114b53a1e04
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2170587
    > Reviewed-by: Tommy Steimel <steimel@chromium.org>
    > Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#763905}
    
    Bug: 1072702
    Change-Id: I24aebf2974c64535340532e9807b4cb5865d5b03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173402
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764001}

--- chrome/browser/media/history/media_history_feed_associated_origins_table.cc
+++ chrome/browser/media/history/media_history_feed_associated_origins_table.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media_history {
 
+const char MediaHistoryFeedAssociatedOriginsTable::kTableName[] =
+    "mediaFeedAssociatedOrigin";
+
 MediaHistoryFeedAssociatedOriginsTable::MediaHistoryFeedAssociatedOriginsTable(
     scoped_refptr<base::UpdateableSequencedTaskRunner> db_task_runner)
     : MediaHistoryTableBase(std::move(db_task_runner)) {}
@@ -39,6 +42,13 @@ MediaHistoryFeedAssociatedOriginsTable::CreateTableIfNonExistent() {
     success = DB()->Execute(
         "CREATE INDEX IF NOT EXISTS mediaFeedAssociatedOrigin_feed_index ON "
         "mediaFeedAssociatedOrigin (feed_id)");
+  }
+
+  if (success) {
+    success = DB()->Execute(
+        "CREATE UNIQUE INDEX IF NOT EXISTS "
+        "mediaFeedAssociatedOrigin_unique_index ON "
+        "mediaFeedAssociatedOrigin(feed_id, origin)");
   }
 
   if (!success) {
@@ -102,6 +112,23 @@ std::vector<url::Origin> MediaHistoryFeedAssociatedOriginsTable::Get(
   }
 
   return origins;
+}
+
+std::set<int64_t> MediaHistoryFeedAssociatedOriginsTable::GetFeeds(
+    const url::Origin& origin) {
+  std::set<int64_t> feeds;
+  if (!CanAccessDatabase())
+    return feeds;
+
+  sql::Statement statement(DB()->GetCachedStatement(
+      SQL_FROM_HERE,
+      "SELECT feed_id FROM mediaFeedAssociatedOrigin WHERE origin = ?"));
+  statement.BindString(0, MediaHistoryOriginTable::GetOriginForStorage(origin));
+
+  while (statement.Step())
+    feeds.insert(statement.ColumnInt64(0));
+
+  return feeds;
 }
 
 }  // namespace media_history
--- chrome/browser/media/history/media_history_store.cc
+++ chrome/browser/media/history/media_history_store.cc
@@ -666,7 +666,7 @@ void MediaHistoryStore::StoreMediaFeedFetchResult(
     const bool was_fetched_from_cache,
     const std::vector<media_feeds::mojom::MediaImagePtr>& logos,
     const std::string& display_name,
-    const std::vector<url::Origin>& associated_origins) {
+    const std::set<url::Origin>& associated_origins) {
   DCHECK(db_task_runner_->RunsTasksInCurrentSequence());
   if (!CanAccessDatabase())
     return;
@@ -678,6 +678,18 @@ void MediaHistoryStore::StoreMediaFeedFetchResult(
     LOG(ERROR) << "Failed to begin the transaction.";
     return;
   }
+
+  std::set<url::Origin> origins_set;
+  for (auto& origin : associated_origins)
+    origins_set.insert(origin);
+
+  // Get the origin for the feed.
+  auto origin = feeds_table_->GetOrigin(feed_id);
+  if (!origin.has_value()) {
+    DB()->RollbackTransaction();
+    return;
+  }
+  origins_set.insert(*origin);
 
   // Remove all the items currently associated with this feed.
   if (!feed_items_table_->DeleteItems(feed_id)) {
@@ -729,7 +741,7 @@ void MediaHistoryStore::StoreMediaFeedFetchResult(
   }
 
   // Store associated origins.
-  for (auto& origin : associated_origins) {
+  for (auto& origin : origins_set) {
     if (!feed_origins_table_->Add(origin, feed_id)) {
       DB()->RollbackTransaction();
       return;
@@ -887,7 +899,7 @@ void MediaHistoryStore::UpdateMediaFeedDisplayTime(const int64_t feed_id) {
   DB()->CommitTransaction();
 }
 
-void MediaHistoryStore::ResetMediaFeed(const int64_t feed_id,
+void MediaHistoryStore::ResetMediaFeed(const url::Origin& origin,
                                        media_feeds::mojom::ResetReason reason) {
   if (!CanAccessDatabase())
     return;
@@ -900,16 +912,27 @@ void MediaHistoryStore::ResetMediaFeed(const int64_t feed_id,
     return;
   }
 
-  // Remove all the items currently associated with this feed.
-  if (!feeds_table_->Reset(feed_id, reason)) {
-    DB()->RollbackTransaction();
-    return;
-  }
+  // Get all the feeds with |origin| as an associated origin.
+  std::set<int64_t> feed_ids = feed_origins_table_->GetFeeds(origin);
 
-  // Remove all the items currently associated with this feed.
-  if (!feed_items_table_->DeleteItems(feed_id)) {
-    DB()->RollbackTransaction();
-    return;
+  for (auto& feed_id : feed_ids) {
+    // Remove all the items currently associated with this feed.
+    if (!feeds_table_->Reset(feed_id, reason)) {
+      DB()->RollbackTransaction();
+      return;
+    }
+
+    // Remove all the items currently associated with this feed.
+    if (!feed_items_table_->DeleteItems(feed_id)) {
+      DB()->RollbackTransaction();
+      return;
+    }
+
+    // Clear any old associated origins.
+    if (!feed_origins_table_->Clear(feed_id)) {
+      DB()->RollbackTransaction();
+      return;
+    }
   }
 
   DB()->CommitTransaction();
