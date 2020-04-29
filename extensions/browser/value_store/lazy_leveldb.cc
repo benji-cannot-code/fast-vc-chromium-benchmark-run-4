fe8ca2de14900fb31c80b306aaf388441090bd62
Chris Mumford <cmumford@chromium.org>
2020-04-29 17:08:20 +0000

    leveldb: Stop logging *.IOError.BFE.* metrics.
    These metrics are no longer used.
    
    Bug: 1037137
    Change-Id: I708b03091ea3e1d34bc0a0ee38487d204deec45a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168219
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/master@{#763869}

--- extensions/browser/value_store/lazy_leveldb.cc
+++ extensions/browser/value_store/lazy_leveldb.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/json/json_reader.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram.h"
 #include "base/strings/string_util.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "content/public/browser/browser_thread.h"
