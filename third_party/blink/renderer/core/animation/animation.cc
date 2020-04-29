b3b64efc70ffbd5941b2d3b18416b84cd9d44f6f
Yi Gu <yigu@chromium.org>
2020-04-29 16:51:22 +0000

    [ScrollTimeline] Composite scroll-linked Web Animations
    After crrev.com/c/2106820 scroll-linked animations are fully supported
    on the compositor. This patch turns on the switch to composite them.
    
    Bug: 1023508
    Change-Id: Ie7cafaf6a94e550df161ccb450ff436f127bf9fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2170847
    Reviewed-by: Majid Valipour <majidvp@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763859}

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -1703,10 +1703,6 @@ Animation::CheckCanStartAnimationOnCompositorInternal() const {
   if (!Playing())
     reasons |= CompositorAnimations::kInvalidAnimationOrEffect;
 
-  // TODO(crbug.com/916117): Support accelerated scroll linked animations.
-  if (timeline_->IsScrollTimeline())
-    reasons |= CompositorAnimations::kInvalidAnimationOrEffect;
-
   return reasons;
 }
 
