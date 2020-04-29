8730fdd9ca18012c260d2f0ffe87f05b433bfd5f
Katie D <katie@chromium.org>
2020-04-29 18:21:33 +0000

    Show the focus ring around the captions bubble when focused per spec.
    The arrow keys move the caption bubble when focused on all platforms.
    In Linux and Chrome, hitting "escape" returns focus to the parent
    page (Mac is a TODO).
    
    Prepares to expose accessibility information to screen readers
    but currently exposes the whole label until a screen reader design
    is decided on.
    
    Makes the test an interactive_ui_test so that we can try sending
    keystroke events.
    
    Spec: go/chrome-captions-ux
    This CL on linux: https://screenshot.googleplex.com/BpkBAR7gXnh
    
    Bug: 1055150
    Change-Id: I91e9b254ec9217cbd55043107b7c948581992046
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161366
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#763914}

--- chrome/browser/ui/views/accessibility/caption_bubble.h
+++ chrome/browser/ui/views/accessibility/caption_bubble.h
@@ -17,7 +17,12 @@ class ImageButton;
 class ImageView;
 }
 
+namespace ui {
+struct AXNodeData;
+}
+
 namespace captions {
+class CaptionBubbleFrameView;
 
 ///////////////////////////////////////////////////////////////////////////////
 // Caption Bubble
@@ -49,6 +54,11 @@ class CaptionBubble : public views::BubbleDialogDelegateView,
   gfx::Rect GetBubbleBounds() override;
   void OnWidgetBoundsChanged(views::Widget* widget,
                              const gfx::Rect& new_bounds) override;
+  void OnKeyEvent(ui::KeyEvent* event) override;
+  bool AcceleratorPressed(const ui::Accelerator& accelerator) override;
+  void OnFocus() override;
+  void OnBlur() override;
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
 
   // Views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
@@ -64,6 +74,7 @@ class CaptionBubble : public views::BubbleDialogDelegateView,
   views::Label* error_message_;
   views::ImageView* error_icon_;
   views::ImageButton* close_button_;
+  CaptionBubbleFrameView* frame_;
 
   bool has_error_ = false;
 
