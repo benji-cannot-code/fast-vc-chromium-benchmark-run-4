a9e713c07567282fdeb418d17732f9b10981322a
Xianzhu Wang <wangxianzhu@chromium.org>
2020-04-29 00:27:00 +0000

    Add use counter for percent length for custom scrollbars
    If the number is low enough, we can remove support of percent length
    which will greatly simplify the code.
    
    Change-Id: Ib47bfdd89b2fbf98a503bf0fd7b782a1c013a5c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168352
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763631}

--- third_party/blink/renderer/core/layout/layout_custom_scrollbar_part.h
+++ third_party/blink/renderer/core/layout/layout_custom_scrollbar_part.h
@@ -106,6 +106,8 @@ class LayoutCustomScrollbarPart final : public LayoutBlock {
 
   bool AllowsOverflowClip() const override { return false; }
 
+  void RecordPercentLengthStats() const;
+
   UntracedMember<ScrollableArea> scrollable_area_;
   UntracedMember<CustomScrollbar> scrollbar_;
   ScrollbarPart part_;
