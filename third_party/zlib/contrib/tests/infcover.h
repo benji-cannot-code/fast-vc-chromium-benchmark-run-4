6b7133f95105cfaf804b7533411c27200d7b5233
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2020-04-29 03:44:04 +0000

    [zlib] Re-land import canonical zlib inflate cover tests
    There is a test suite in Canonical zlib that ensures correctness and
    coverage of inflate/deflate: the inflate cover (infcover.c) suite.
    
    This patch imports the inflate cover suite with the necessary changes
    to make it build and run under Chromes's C++11 GTEST test system.
    
    There are minimal changes (i.e. undefined 'local', miscellaneous C
    casts to use C++ casts, etc) so we can build it as C++ code and hook
    into GTEST e.g. use EXPECT_TRUE instead of C assert from <assert.h>.
    
    Adds a patch file for these changes, that can be applied on Canonical
    infcover should we need to sync with a new Canonical zlib release.
    
    TODO: there's one test accessing a symbol that causes link errors so
    comment that code out for now.
    
    Bug: 1032721
    Change-Id: I004a1357d46d909ad3a5a5bacc5f9ab9bfcb7809
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168792
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763696}

---
+++ third_party/zlib/contrib/tests/infcover.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#ifndef __INF_COVER_H__
+#define __INF_COVER_H__
+
+void cover_support(void);
+void cover_wrap(void);
+void cover_back(void);
+void cover_inflate(void);
+void cover_trees(void);
+void cover_fast(void);
+
+#endif
