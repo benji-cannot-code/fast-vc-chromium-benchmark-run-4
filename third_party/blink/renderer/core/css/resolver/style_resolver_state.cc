72134d873ba205033878dc498e00f867312e1f7b
Rune Lillesveen <futhark@chromium.org>
2020-04-29 15:06:01 +0000

    Support UA sheet -internal-light-dark() for FillImages.
    This allows using -internal-light-dark() for specifying e.g. a
    background image per color-scheme.
    
    This is in preparation for color-scheme specific background images in
    the UA stylesheet for media controls.
    
    Bug: 929098
    Change-Id: Id0fcc73d19203d69833b39eec5fe0e597d1b2ed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165726
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763820}

--- third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/resolver/style_resolver_state.h"
 
 #include "third_party/blink/renderer/core/animation/css/css_animations.h"
+#include "third_party/blink/renderer/core/css/css_light_dark_value_pair.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
 #include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
@@ -216,6 +217,19 @@ const Element* StyleResolverState::GetAnimatingElement() const {
     return &GetElement();
   DCHECK_EQ(AnimatingElementType::kPseudoElement, animating_element_type_);
   return pseudo_element_;
+}
+
+const CSSValue& StyleResolverState::ResolveLightDarkPair(
+    const CSSProperty& property,
+    const CSSValue& value) {
+  if (const auto* pair = DynamicTo<CSSLightDarkValuePair>(value)) {
+    if (!property.IsInherited())
+      Style()->SetHasNonInheritedLightDarkValue();
+    if (Style()->UsedColorScheme() == WebColorScheme::kLight)
+      return pair->First();
+    return pair->Second();
+  }
+  return value;
 }
 
 }  // namespace blink
