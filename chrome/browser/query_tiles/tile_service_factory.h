85c565a5492ee472249b7305c18012c1c10326f2
Shakti Sahu <shaktisahu@chromium.org>
2020-04-29 01:47:17 +0000

    Query Tiles : Move to components/
    This CL moves the most of the files from chrome/browser/upboarding to
    components/query_tiles. Only a few factory classes are left in
    chrome layer at chrome/browser_query_tiles. This was required as a prerequisite to be
    able to build omnibox suggestions from query tiles.
    
    TBR=rhalavati@chromium.org
    
    Bug: 1074744
    Change-Id: I84ed780fe4c2a71f24f8b8fa935e8e3f47815d5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2167011
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763666}

---
+++ chrome/browser/query_tiles/tile_service_factory.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_QUERY_TILES_TILE_SERVICE_FACTORY_H_
+#define CHROME_BROWSER_QUERY_TILES_TILE_SERVICE_FACTORY_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "components/keyed_service/core/simple_keyed_service_factory.h"
+#include "components/query_tiles/tile_service.h"
+
+namespace base {
+template <typename T>
+struct DefaultSingletonTraits;
+}  // namespace base
+
+namespace upboarding {
+
+class TileService;
+
+// A factory to create one unique TileService.
+class TileServiceFactory : public SimpleKeyedServiceFactory {
+ public:
+  static TileServiceFactory* GetInstance();
+  static TileService* GetForKey(SimpleFactoryKey* key);
+
+ private:
+  friend struct base::DefaultSingletonTraits<TileServiceFactory>;
+
+  TileServiceFactory();
+  ~TileServiceFactory() override;
+
+  std::unique_ptr<KeyedService> BuildServiceInstanceFor(
+      SimpleFactoryKey* key) const override;
+
+  DISALLOW_COPY_AND_ASSIGN(TileServiceFactory);
+};
+
+}  // namespace upboarding
+
+#endif  // CHROME_BROWSER_QUERY_TILES_TILE_SERVICE_FACTORY_H_
---
+++ components/query_tiles/internal/tile_fetcher_unittest.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/query_tiles/internal/tile_fetcher.h"
+
+#include "base/test/task_environment.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace upboarding {
+class TileFetcherTest : public testing::Test {
+ public:
+  TileFetcherTest();
+  ~TileFetcherTest() override = default;
+
+  TileFetcherTest(const TileFetcherTest& other) = delete;
+  TileFetcherTest& operator=(const TileFetcherTest& other) = delete;
+};
+
+}  // namespace upboarding
