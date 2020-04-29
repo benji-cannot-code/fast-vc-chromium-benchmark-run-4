33652dae0e26ff94b0e316cfda7b0941da34ade7
Piotr Tworek <ptworek@vewd.com>
2020-04-29 14:41:14 +0000

    Add missing string.h include in //device/fido/cable/noise.cc
    The file in question uses memcpy but fails to include header declaring
    this function. This leads to build failures on Linux systems using glibc
    newer than 2.29.
    
    Bug: 1074286
    Change-Id: I9192c1d801fcbd8075ac1d3732b13c5260e38b8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171478
    Reviewed-by: Kim Paulhamus <kpaulhamus@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#763816}

--- device/fido/cable/noise.cc
+++ device/fido/cable/noise.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/fido/cable/noise.h"
 
+#include <string.h>
+
 #include "crypto/aead.h"
 #include "crypto/sha2.h"
 #include "third_party/boringssl/src/include/openssl/digest.h"
