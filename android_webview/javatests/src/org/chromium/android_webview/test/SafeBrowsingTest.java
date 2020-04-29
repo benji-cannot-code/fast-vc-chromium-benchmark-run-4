0d521ddb120745008a289e6c05136c6857f04baf
Carlos IL <carlosil@chromium.org>
2020-04-29 22:19:34 +0000

    Clean up kCommittedSBInterstitials flag.
    This removes the flag for committed safe browsing interstitials, which
    launched on M81. safe_browsing_blocking_page_unittest.cc tests all
    relied on overlay interstitials, so this removes that file. Tests that
    didn't have existing coverage as a browser test were added to
    safe_browsing_blocking_page_test.cc
    
    Bug: 1039443
    Change-Id: I6d5b9a1a9a8ab3b72d002acdce805f9fdfcc87fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2138506
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764026}

--- android_webview/javatests/src/org/chromium/android_webview/test/SafeBrowsingTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/SafeBrowsingTest.java
@@ -31,8 +31,6 @@ import org.chromium.android_webview.AwContents.InternalAccessDelegate;
 import org.chromium.android_webview.AwContents.NativeDrawFunctorFactory;
 import org.chromium.android_webview.AwContentsClient;
 import org.chromium.android_webview.AwContentsStatics;
-import org.chromium.android_webview.AwFeatureList;
-import org.chromium.android_webview.AwFeatures;
 import org.chromium.android_webview.AwSettings;
 import org.chromium.android_webview.AwWebContentsObserver;
 import org.chromium.android_webview.ErrorCodeConversionHelper;
@@ -438,18 +436,13 @@ public class SafeBrowsingTest {
                 mWebContentsObserver.getAttachedInterstitialPageHelper().getCallCount();
         final String responseUrl = mTestServer.getURL(path);
         mActivityTestRule.loadUrlAsync(mAwContents, responseUrl);
-        if (AwFeatureList.isEnabled(AwFeatures.SAFE_BROWSING_COMMITTED_INTERSTITIALS)) {
-            // Subresource triggered interstitials will trigger after the page containing the
-            // subresource has loaded (and displayed), so we first wait for the interstitial to be
-            // attached to the web contents, then for a visual state callback to allow the
-            // interstitial to render.
-            CriteriaHelper.pollUiThread(() -> mAwContents.isDisplayingInterstitialForTesting());
-            // Wait for the interstitial to actually render.
-            mActivityTestRule.waitForVisualStateCallback(mAwContents);
-        } else {
-            mWebContentsObserver.getAttachedInterstitialPageHelper().waitForCallback(
-                    interstitialCount);
-        }
+        // Subresource triggered interstitials will trigger after the page containing the
+        // subresource has loaded (and displayed), so we first wait for the interstitial to be
+        // attached to the web contents, then for a visual state callback to allow the
+        // interstitial to render.
+        CriteriaHelper.pollUiThread(() -> mAwContents.isDisplayingInterstitialForTesting());
+        // Wait for the interstitial to actually render.
+        mActivityTestRule.waitForVisualStateCallback(mAwContents);
     }
 
     private void assertTargetPageHasLoaded(int pageColor) throws Exception {
@@ -702,10 +695,7 @@ public class SafeBrowsingTest {
         // For subresources, the initial site finishes loading before the interstitial is shown,
         // causing an extra onPageFinished call if committed interstitials are enabled (since the
         // proceed action triggers a reload).
-        int numNavigations =
-                AwFeatureList.isEnabled(AwFeatures.SAFE_BROWSING_COMMITTED_INTERSTITIALS) ? 2 : 1;
-        mContentsClient.getOnPageFinishedHelper().waitForCallback(
-                pageFinishedCount, numNavigations);
+        mContentsClient.getOnPageFinishedHelper().waitForCallback(pageFinishedCount, 2);
         assertTargetPageHasLoaded(IFRAME_EMBEDDER_BACKGROUND_COLOR);
     }
 
@@ -739,13 +729,7 @@ public class SafeBrowsingTest {
         OnReceivedError2Helper errorHelper = mContentsClient.getOnReceivedError2Helper();
         int errorCount = errorHelper.getCallCount();
         clickBackToSafety();
-        if (AwFeatureList.isEnabled(AwFeatures.SAFE_BROWSING_COMMITTED_INTERSTITIALS)) {
-            errorHelper.waitForCallback(errorCount);
-        } else {
-            mWebContentsObserver.getDetachedInterstitialPageHelper().waitForCallback(
-                    interstitialCount);
-        }
-
+        errorHelper.waitForCallback(errorCount);
         mActivityTestRule.waitForVisualStateCallback(mAwContents);
         assertTargetPageNotShowing(MALWARE_PAGE_BACKGROUND_COLOR);
         assertGreenPageShowing();
@@ -763,13 +747,7 @@ public class SafeBrowsingTest {
         OnReceivedError2Helper errorHelper = mContentsClient.getOnReceivedError2Helper();
         int errorCount = errorHelper.getCallCount();
         clickBackToSafety();
-        if (AwFeatureList.isEnabled(AwFeatures.SAFE_BROWSING_COMMITTED_INTERSTITIALS)) {
-            errorHelper.waitForCallback(errorCount);
-        } else {
-            mWebContentsObserver.getDetachedInterstitialPageHelper().waitForCallback(
-                    interstitialCount);
-        }
-
+        errorHelper.waitForCallback(errorCount);
         mActivityTestRule.waitForVisualStateCallback(mAwContents);
         assertTargetPageNotShowing(IFRAME_EMBEDDER_BACKGROUND_COLOR);
         assertGreenPageShowing();
@@ -1104,13 +1082,8 @@ public class SafeBrowsingTest {
         int interstitialCount =
                 mWebContentsObserver.getAttachedInterstitialPageHelper().getCallCount();
         mActivityTestRule.loadUrlAsync(mAwContents, WEB_UI_MALWARE_URL);
-        if (AwFeatureList.isEnabled(AwFeatures.SAFE_BROWSING_COMMITTED_INTERSTITIALS)) {
-            // Wait for the interstitial to actually render.
-            mActivityTestRule.waitForVisualStateCallback(mAwContents);
-        } else {
-            mWebContentsObserver.getAttachedInterstitialPageHelper().waitForCallback(
-                    interstitialCount);
-        }
+        // Wait for the interstitial to actually render.
+        mActivityTestRule.waitForVisualStateCallback(mAwContents);
         waitForInterstitialDomToLoad();
     }
 
@@ -1122,13 +1095,8 @@ public class SafeBrowsingTest {
         int interstitialCount =
                 mWebContentsObserver.getAttachedInterstitialPageHelper().getCallCount();
         mActivityTestRule.loadUrlAsync(mAwContents, WEB_UI_PHISHING_URL);
-        if (AwFeatureList.isEnabled(AwFeatures.SAFE_BROWSING_COMMITTED_INTERSTITIALS)) {
-            // Wait for the interstitial to actually render.
-            mActivityTestRule.waitForVisualStateCallback(mAwContents);
-        } else {
-            mWebContentsObserver.getAttachedInterstitialPageHelper().waitForCallback(
-                    interstitialCount);
-        }
+        // Wait for the interstitial to actually render.
+        mActivityTestRule.waitForVisualStateCallback(mAwContents);
         waitForInterstitialDomToLoad();
     }
 
@@ -1141,13 +1109,8 @@ public class SafeBrowsingTest {
         int interstitialCount =
                 mWebContentsObserver.getAttachedInterstitialPageHelper().getCallCount();
         mActivityTestRule.loadUrlAsync(mAwContents, WEB_UI_MALWARE_URL);
-        if (AwFeatureList.isEnabled(AwFeatures.SAFE_BROWSING_COMMITTED_INTERSTITIALS)) {
-            // Wait for the interstitial to actually render.
-            mActivityTestRule.waitForVisualStateCallback(mAwContents);
-        } else {
-            mWebContentsObserver.getAttachedInterstitialPageHelper().waitForCallback(
-                    interstitialCount);
-        }
+        // Wait for the interstitial to actually render.
+        mActivityTestRule.waitForVisualStateCallback(mAwContents);
         waitForInterstitialDomToLoad();
     }
 
@@ -1243,16 +1206,12 @@ public class SafeBrowsingTest {
         int pageFinishedCount = mContentsClient.getOnPageFinishedHelper().getCallCount();
         clickLinkById(linkId);
         mContentsClient.getOnPageFinishedHelper().waitForCallback(pageFinishedCount);
-        if (AwFeatureList.isEnabled(AwFeatures.SAFE_BROWSING_COMMITTED_INTERSTITIALS)) {
-            // Some click tests involve URLs that redirect and mAwContents.getUrl() sometimes
-            // returns the post-redirect URL, so we instead check with ShouldInterceptRequest.
-            AwContentsClient.AwWebResourceRequest requestsForUrl =
-                    mContentsClient.getShouldInterceptRequestHelper().getRequestsForUrl(linkUrl);
-            // Make sure the URL was seen for a main frame navigation.
-            Assert.assertTrue(requestsForUrl.isMainFrame);
-        } else {
-            Assert.assertEquals(new GURL(linkUrl), mAwContents.getUrl());
-        }
+        // Some click tests involve URLs that redirect and mAwContents.getUrl() sometimes
+        // returns the post-redirect URL, so we instead check with ShouldInterceptRequest.
+        AwContentsClient.AwWebResourceRequest requestsForUrl =
+                mContentsClient.getShouldInterceptRequestHelper().getRequestsForUrl(linkUrl);
+        // Make sure the URL was seen for a main frame navigation.
+        Assert.assertTrue(requestsForUrl.isMainFrame);
     }
 
     @Test
--- chrome/browser/safe_browsing/v4_embedded_test_server_browsertest.cc
+++ chrome/browser/safe_browsing/v4_embedded_test_server_browsertest.cc
@@ -28,16 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 bool IsShowingInterstitial(content::WebContents* contents) {
-  if (base::FeatureList::IsEnabled(safe_browsing::kCommittedSBInterstitials)) {
-    security_interstitials::SecurityInterstitialTabHelper* helper =
-        security_interstitials::SecurityInterstitialTabHelper::FromWebContents(
-            contents);
-    return helper &&
-           (helper
-                ->GetBlockingPageForCurrentlyCommittedNavigationForTesting() !=
-            nullptr);
-  }
-  return contents->GetInterstitialPage() != nullptr;
+  security_interstitials::SecurityInterstitialTabHelper* helper =
+      security_interstitials::SecurityInterstitialTabHelper::FromWebContents(
+          contents);
+  return helper &&
+         (helper->GetBlockingPageForCurrentlyCommittedNavigationForTesting() !=
+          nullptr);
 }
 
 }  // namespace
