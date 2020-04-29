e88170e5e8d8a09b6ac06c1b2494142ed9f6d3a5
Antonio Gomes <tonikitoo@igalia.com>
2020-04-29 21:06:12 +0000

    Convert WidgetHostMsg_HasTouchEventHandlers to mojo
    As part of this conversion, mojo::AssociatedReceiver<> has got
    a new method that allows remotes to operate without an actual
    message pipe endpoint:
    BindNewEndpointAndPassDedicatedRemoteForTesting().
    It is analogous to mojom::AssociatedRemote<>'s
    BindNewEndpointAndPassDedicatedReceiverForTesting().
    
    Also, this CL does not completely eliminates
    RenderWidget::SetHasTouchEventHandlers() method, for atomicity
    reasons. In a follow up CL, RenderWidget::render_widget_scheduling_state_
    will move to WebFrameWidgetBase class.
    
    BUG=1071594
    R=avi@chromium.org, dtapuska@chromium.org
    
    Change-Id: I4a13a89f1aa0d8745a365bfc535d14b4d62bdfef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165906
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#763992}

--- content/public/test/render_view_test.cc
+++ content/public/test/render_view_test.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/use_zoom_for_dsf_policy.h"
 #include "content/public/renderer/content_renderer_client.h"
 #include "content/public/renderer/render_view_visitor.h"
+#include "content/public/test/fake_render_widget_host.h"
 #include "content/public/test/frame_load_waiter.h"
 #include "content/renderer/history_serialization.h"
 #include "content/renderer/loader/resource_dispatcher.h"
@@ -380,6 +381,8 @@ void RenderViewTest::SetUp() {
   if (!render_thread_)
     render_thread_ = std::make_unique<MockRenderThread>();
 
+  render_widget_host_.reset(new FakeRenderWidgetHost());
+
   // Blink needs to be initialized before calling CreateContentRendererClient()
   // because it uses blink internally.
   blink_platform_impl_.Initialize();
@@ -464,6 +467,8 @@ void RenderViewTest::SetUp() {
   view_params->hidden = false;
   view_params->never_composited = false;
   view_params->visual_properties = InitialVisualProperties();
+  std::tie(view_params->frame_widget_host, view_params->frame_widget) =
+      render_widget_host_->BindNewFrameWidgetInterfaces();
 
   RenderViewImpl* view = RenderViewImpl::Create(
       compositor_deps_.get(), std::move(view_params),
--- content/public/test/render_view_test.h
+++ content/public/test/render_view_test.h
@@ -45,6 +45,7 @@ class ContentBrowserClient;
 class ContentClient;
 class ContentRendererClient;
 class CompositorDependencies;
+class FakeRenderWidgetHost;
 class PageState;
 class RendererMainPlatformDelegate;
 class RendererBlinkPlatformImpl;
@@ -220,6 +221,7 @@ class RenderViewTest : public testing::Test {
   std::unique_ptr<ContentBrowserClient> content_browser_client_;
   std::unique_ptr<ContentRendererClient> content_renderer_client_;
   std::unique_ptr<MockRenderThread> render_thread_;
+  std::unique_ptr<FakeRenderWidgetHost> render_widget_host_;
 
   // Used to setup the process so renderers can run.
   std::unique_ptr<RendererMainPlatformDelegate> platform_;
