8d2ce0ab24caa44ef86041c9fd0d58da4146abb4
Bo Liu <boliu@chromium.org>
2020-04-29 20:11:53 +0000

    viz: Remove copy in CopyOutputSkBitmapResult deserialization
    Change the mojo type to pass pixel data over shared memory, and then
    directly adopt the shared memory as pixel data in the SkBitmap in
    deserialization.
    
    Bug: 1073601
    Change-Id: I21784c2076acd23b263c3c990651aad84c72325c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163425
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763963}

--- services/viz/public/cpp/compositing/copy_output_result_mojom_traits.h
+++ services/viz/public/cpp/compositing/copy_output_result_mojom_traits.h
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/common/mailbox_mojom_traits.h"
 #include "gpu/ipc/common/sync_token_mojom_traits.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
+#include "services/viz/public/cpp/compositing/bitmap_in_shared_memory_mojom_traits.h"
 #include "services/viz/public/mojom/compositing/copy_output_result.mojom-shared.h"
 #include "services/viz/public/mojom/compositing/texture_releaser.mojom.h"
-#include "skia/public/mojom/bitmap_skbitmap_mojom_traits.h"
 #include "ui/gfx/geometry/mojom/geometry_mojom_traits.h"
 #include "ui/gfx/ipc/color/gfx_param_traits.h"
 #include "ui/gfx/mojom/color_space_mojom_traits.h"
@@ -38,7 +38,7 @@ struct StructTraits<viz::mojom::CopyOutputResultDataView,
   static const gfx::Rect& rect(
       const std::unique_ptr<viz::CopyOutputResult>& result);
 
-  static const SkBitmap& bitmap(
+  static base::Optional<SkBitmap> bitmap(
       const std::unique_ptr<viz::CopyOutputResult>& result);
 
   static base::Optional<gpu::Mailbox> mailbox(
