0e53cf4fe84a7536f19439010272aa5c9a51dbac
Rayan Kanso <rayankans@chromium.org>
2020-04-29 12:07:04 +0000

    Revert "Reland "[Signin][Android] Move AccountManagerFacade.java and dependants to ../../../public/android""
    This reverts commit ccaa32108312bb9db81ca58a132e909182f87613.
    
    Reason for revert: Still causing dep cycles
    https://ci.chromium.org/p/chrome/builders/ci/arm64-unpublished-builder-rel/13942
    https://ci.chromium.org/p/chrome/builders/ci/android-internal-chromium-tot/58310?
    
    Original change's description:
    > Reland "[Signin][Android] Move AccountManagerFacade.java and dependants to ../../../public/android"
    > 
    > This is a reland of c714a458b35e034d435d72e9f578802d90bd69ee
    > 
    > Original change's description:
    > > [Signin][Android] Move AccountManagerFacade.java and dependants to ../../../public/android
    > > 
    > > This CL is a second step towards reorganizing //components/signin java files, this one moves the files from core/browser/android to public/android when relevant.
    > > 
    > > Bug: 934688
    > > Change-Id: I0ac30a3d94fa0a09f645b70c1ddc6c5345b94441
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1814826
    > > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > > Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    > > Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#762795}
    > 
    > Bug: 934688
    > Change-Id: Ib9c404955177997ee70304d3a96dad66cfce7b66
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2167759
    > Auto-Submit: Pâris Meuleman <pmeuleman@chromium.org>
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Commit-Queue: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#763743}
    
    TBR=blundell@chromium.org,bsazonov@chromium.org,arthursonzogni@chromium.org,pmeuleman@chromium.org
    
    Change-Id: I177aa57f88bc6f9fede7ecdaf5003172b892b21c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 934688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172684
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763778}

--- components/signin/internal/identity_manager/child_account_info_fetcher_android.cc
+++ components/signin/internal/identity_manager/child_account_info_fetcher_android.cc
@@ -10,9 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/memory/ptr_util.h"
+#include "components/signin/core/browser/android/jni_headers/ChildAccountInfoFetcher_jni.h"
 #include "components/signin/internal/identity_manager/account_fetcher_service.h"
 #include "components/signin/internal/identity_manager/account_tracker_service.h"
-#include "components/signin/public/android/jni_headers/ChildAccountInfoFetcher_jni.h"
 
 using base::android::JavaParamRef;
 
@@ -33,7 +33,7 @@ ChildAccountInfoFetcherAndroid::Create(AccountFetcherService* service,
 }
 
 void ChildAccountInfoFetcherAndroid::InitializeForTests() {
-  signin::Java_ChildAccountInfoFetcher_initializeForTests(
+  Java_ChildAccountInfoFetcher_initializeForTests(
       base::android::AttachCurrentThread());
 }
 
@@ -42,19 +42,16 @@ ChildAccountInfoFetcherAndroid::ChildAccountInfoFetcherAndroid(
     const CoreAccountId& account_id,
     const std::string& account_name) {
   JNIEnv* env = base::android::AttachCurrentThread();
-  j_child_account_info_fetcher_.Reset(
-      signin::Java_ChildAccountInfoFetcher_create(
-          env, reinterpret_cast<jlong>(service),
-          base::android::ConvertUTF8ToJavaString(env, account_id.ToString()),
-          base::android::ConvertUTF8ToJavaString(env, account_name)));
+  j_child_account_info_fetcher_.Reset(Java_ChildAccountInfoFetcher_create(
+      env, reinterpret_cast<jlong>(service),
+      base::android::ConvertUTF8ToJavaString(env, account_id.ToString()),
+      base::android::ConvertUTF8ToJavaString(env, account_name)));
 }
 
 ChildAccountInfoFetcherAndroid::~ChildAccountInfoFetcherAndroid() {
-  signin::Java_ChildAccountInfoFetcher_destroy(
-      base::android::AttachCurrentThread(), j_child_account_info_fetcher_);
+  Java_ChildAccountInfoFetcher_destroy(base::android::AttachCurrentThread(),
+                                       j_child_account_info_fetcher_);
 }
-
-namespace signin {
 
 // TODO(crbug.com/1028580) Pass |j_account_id| as a
 // org.chromium.components.signin.identitymanager.CoreAccountId and convert it
@@ -71,4 +68,3 @@ void JNI_ChildAccountInfoFetcher_SetIsChildAccount(
           base::android::ConvertJavaStringToUTF8(env, j_account_id)),
       is_child_account);
 }
-}  // namespace signin
