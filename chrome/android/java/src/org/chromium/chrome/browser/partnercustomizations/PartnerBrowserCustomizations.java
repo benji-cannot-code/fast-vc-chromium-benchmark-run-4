a20320655fec237891b701bb72886b1107429e31
bttk <bttk@chromium.org>
2020-04-29 04:44:54 +0000

    Don't call getCustomizationProvider on the main thread
    Bug: 1063948
    Change-Id: Ia7dce21334b9cbafdd7ef30adae81edc016336d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164325
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: bttk <bttk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763709}

--- chrome/android/java/src/org/chromium/chrome/browser/partnercustomizations/PartnerBrowserCustomizations.java
+++ chrome/android/java/src/org/chromium/chrome/browser/partnercustomizations/PartnerBrowserCustomizations.java
@@ -288,12 +288,11 @@ public class PartnerBrowserCustomizations {
     @VisibleForTesting
     void initializeAsync(final Context context, long timeoutMs) {
         mIsInitialized = false;
-        Provider provider = AppHooks.get().getCustomizationProvider();
         // Setup an initializing async task.
         final AsyncTask<Void> initializeAsyncTask = new AsyncTask<Void>() {
             private boolean mHomepageUriChanged;
 
-            private void refreshHomepage() {
+            private void refreshHomepage(Provider provider) {
                 try {
                     String homepage = provider.getHomepage();
                     if (!isValidHomepage(homepage)) {
@@ -308,7 +307,7 @@ public class PartnerBrowserCustomizations {
                 }
             }
 
-            private void refreshIncognitoModeDisabled() {
+            private void refreshIncognitoModeDisabled(Provider provider) {
                 try {
                     mIncognitoModeDisabled = provider.isIncognitoModeDisabled();
                 } catch (Exception e) {
@@ -316,7 +315,7 @@ public class PartnerBrowserCustomizations {
                 }
             }
 
-            private void refreshBookmarksEditingDisabled() {
+            private void refreshBookmarksEditingDisabled(Provider provider) {
                 try {
                     mBookmarksEditingDisabled = provider.isBookmarksEditingDisabled();
                 } catch (Exception e) {
@@ -336,20 +335,17 @@ public class PartnerBrowserCustomizations {
                         return null;
                     }
 
-                    if (isCancelled()) {
-                        return null;
-                    }
-                    refreshIncognitoModeDisabled();
+                    if (isCancelled()) return null;
+                    Provider provider = AppHooks.get().getCustomizationProvider();
 
-                    if (isCancelled()) {
-                        return null;
-                    }
-                    refreshBookmarksEditingDisabled();
+                    if (isCancelled()) return null;
+                    refreshIncognitoModeDisabled(provider);
 
-                    if (isCancelled()) {
-                        return null;
-                    }
-                    refreshHomepage();
+                    if (isCancelled()) return null;
+                    refreshBookmarksEditingDisabled(provider);
+
+                    if (isCancelled()) return null;
+                    refreshHomepage(provider);
                 } catch (Exception e) {
                     Log.w(TAG, "Fetching partner customizations failed", e);
                 }
