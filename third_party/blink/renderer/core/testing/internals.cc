a4d478c2a3437e1381cf6100e1cb6668eeeacd24
Michael Lippautz <mlippautz@chromium.org>
2020-04-29 17:39:03 +0000

    bindings: Remove internals.scheduleBlinkGC
    Users should invoke window.gc().
    
    For microbenchmarking we can take dromaeo benchmarks and look at
    Blink-portion of GC slices.
    
    Bug: chromium:1074061
    Change-Id: I31b026d6067dadf19ae4a3cd745dfb23a1a035e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172320
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763886}

--- third_party/blink/renderer/core/testing/internals.cc
+++ third_party/blink/renderer/core/testing/internals.cc
@@ -3211,10 +3211,6 @@ void Internals::revealSelection() {
   GetFrame()->Selection().RevealSelection(ScrollAlignment::ToEdgeIfNeeded());
 }
 
-void Internals::scheduleBlinkGC() {
-  ThreadState::Current()->ScheduleForcedGCForTesting();
-}
-
 String Internals::selectedHTMLForClipboard() {
   if (!GetFrame())
     return String();
