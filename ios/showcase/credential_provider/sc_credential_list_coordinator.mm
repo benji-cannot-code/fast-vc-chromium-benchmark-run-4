d509b97a708a3512c0e47da389fb17f14808340e
David Jean <djean@chromium.org>
2020-04-29 15:41:11 +0000

    [ios] fix cpe tooltip crash
    Bug: 1076000
    Change-Id: I8c1889b01fe65a7c8a6ce7f9c2b20c1c2e69fb38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172960
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763832}

--- ios/showcase/credential_provider/sc_credential_list_coordinator.mm
+++ ios/showcase/credential_provider/sc_credential_list_coordinator.mm
@@ -69,8 +69,6 @@ NSArray<id<Credential>>* allPasswords = @[
 @interface SCCredentialListCoordinator () <CredentialDetailsConsumerDelegate,
                                            CredentialListConsumerDelegate>
 @property(nonatomic, strong) CredentialListViewController* viewController;
-@property(nonatomic, strong)
-    CredentialDetailsViewController* detailsViewController;
 @end
 
 @implementation SCCredentialListCoordinator
@@ -82,10 +80,6 @@ NSArray<id<Credential>>* allPasswords = @[
   self.viewController.delegate = self;
   [self.baseViewController setHidesBarsOnSwipe:NO];
   [self.baseViewController pushViewController:self.viewController animated:YES];
-
-  self.detailsViewController = [[CredentialDetailsViewController alloc] init];
-  self.detailsViewController.title = @"CPE Password Details";
-  self.detailsViewController.delegate = self;
 
   [self.viewController presentSuggestedPasswords:suggestedPasswords
                                     allPasswords:allPasswords];
@@ -117,8 +111,11 @@ NSArray<id<Credential>>* allPasswords = @[
 }
 
 - (void)showDetailsForCredential:(id<Credential>)credential {
-  [self.detailsViewController presentCredential:credential];
-  [self.baseViewController pushViewController:self.detailsViewController
+  CredentialDetailsViewController* detailsViewController =
+      [[CredentialDetailsViewController alloc] init];
+  detailsViewController.delegate = self;
+  [detailsViewController presentCredential:credential];
+  [self.baseViewController pushViewController:detailsViewController
                                      animated:YES];
 }
 
