03230ebded8c419c97f79baf01dcc65fbae774ac
Stephen Nusko <nuskos@chromium.org>
2020-04-29 09:29:28 +0000

    Add GestureScrollId to LatencyInfo so we can tie all GestureScroll events together.
    Its useful for traces (system traces) where there are multiple chrome
    traces to be able to tell which events go together. This takes the first
    Async InputLatency::GestureScroll.* event, and stores the trace_id until the scroll ends.
    
    This will allow us to tie together begin and end events in lab metrics (TBMv3).
    
    See internal bug: b/150944635
    
    Change-Id: I177d559a7d98d98ceab028604501c6c5c087d747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2167764
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Timothy Dresser <tdresser@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763748}

--- ui/latency/latency_info.cc
+++ ui/latency/latency_info.cc
@@ -189,6 +189,7 @@ void LatencyInfo::CopyLatencyFrom(const LatencyInfo& other,
   }
 
   coalesced_ = other.coalesced();
+  gesture_scroll_id_ = other.gesture_scroll_id();
   scroll_update_delta_ = other.scroll_update_delta();
   // TODO(tdresser): Ideally we'd copy |began_| here as well, but |began_|
   // isn't very intuitive, and we can actually begin multiple times across
@@ -213,6 +214,7 @@ void LatencyInfo::AddNewLatencyFrom(const LatencyInfo& other) {
   }
 
   coalesced_ = other.coalesced();
+  gesture_scroll_id_ = other.gesture_scroll_id();
   scroll_update_delta_ = other.scroll_update_delta();
   // TODO(tdresser): Ideally we'd copy |began_| here as well, but |began_| isn't
   // very intuitive, and we can actually begin multiple times across copied
@@ -353,6 +355,9 @@ LatencyInfo::AsTraceableData() {
   }
   record_data->SetDouble("trace_id", static_cast<double>(trace_id_));
   record_data->SetBoolean("is_coalesced", coalesced_);
+  if (gesture_scroll_id_ > 0) {
+    record_data->SetDouble("gesture_scroll_id", gesture_scroll_id_);
+  }
   return LatencyInfoTracedValue::FromValue(std::move(record_data));
 }
 
