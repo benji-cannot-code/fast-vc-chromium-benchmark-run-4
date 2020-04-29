8dc7349824718bfb69b56aa0161e349a8ae94350
Peter Kotwicz <pkotwicz@chromium.org>
2020-04-29 00:46:53 +0000

    [Android Webapps] Remove ActivityAssigner usage
    This CL deletes the Kitkat activity creation logic in
    WebappLauncherActivity.java and LaunchIntentDispatcher.java now that we
    will not be shipping new versions of Chrome for Kitkat
    
    BUG=1068178
    
    Change-Id: I20de318602deeb505353985c0c133c609e4c295b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166808
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763636}

--- chrome/android/javatests/src/org/chromium/chrome/browser/display_cutout/WebappDisplayCutoutTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/display_cutout/WebappDisplayCutoutTestRule.java
@@ -14,7 +14,7 @@ import org.junit.runners.model.Statement;
 
 import org.chromium.chrome.browser.ShortcutHelper;
 import org.chromium.chrome.browser.webapps.WebDisplayMode;
-import org.chromium.chrome.browser.webapps.WebappActivity0;
+import org.chromium.chrome.browser.webapps.WebappActivity;
 import org.chromium.content_public.browser.test.util.Criteria;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
 
@@ -24,9 +24,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Custom test rule for simulating a {@link WebappActivity0} with a Display Cutout.
+ * Custom test rule for simulating a {@link WebappActivity} with a Display Cutout.
  */
-public class WebappDisplayCutoutTestRule extends DisplayCutoutTestRule<WebappActivity0> {
+public class WebappDisplayCutoutTestRule extends DisplayCutoutTestRule<WebappActivity> {
     /** Test data for the test webapp. */
     private static final String WEBAPP_ID = "webapp_id";
     private static final String WEBAPP_NAME = "webapp name";
@@ -36,7 +36,7 @@ public class WebappDisplayCutoutTestRule extends DisplayCutoutTestRule<WebappAct
     private static final long STARTUP_TIMEOUT = 10000L;
 
     /**
-     * Contains test specific configuration for launching {@link WebappActivity0}.
+     * Contains test specific configuration for launching {@link WebappActivity}.
      */
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
@@ -46,7 +46,7 @@ public class WebappDisplayCutoutTestRule extends DisplayCutoutTestRule<WebappAct
     }
 
     public WebappDisplayCutoutTestRule() {
-        super(WebappActivity0.class);
+        super(WebappActivity.class);
     }
 
     @Override
@@ -67,8 +67,8 @@ public class WebappDisplayCutoutTestRule extends DisplayCutoutTestRule<WebappAct
 
     private void startWebappActivity(@WebDisplayMode int displayMode) {
         Intent intent =
-                new Intent(InstrumentationRegistry.getTargetContext(), WebappActivity0.class);
-        intent.setData(Uri.parse(WebappActivity0.WEBAPP_SCHEME + "://" + WEBAPP_ID));
+                new Intent(InstrumentationRegistry.getTargetContext(), WebappActivity.class);
+        intent.setData(Uri.parse(WebappActivity.WEBAPP_SCHEME + "://" + WEBAPP_ID));
         intent.putExtra(ShortcutHelper.EXTRA_ID, WEBAPP_ID);
         intent.putExtra(ShortcutHelper.EXTRA_URL, getTestURL());
         intent.putExtra(ShortcutHelper.EXTRA_NAME, WEBAPP_NAME);
