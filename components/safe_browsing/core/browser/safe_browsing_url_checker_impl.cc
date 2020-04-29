21c1faaaba2eacc1ec5267b0c1bacbbf454a63e4
Mustafa Emre Acer <meacer@chromium.org>
2020-04-29 22:33:24 +0000

    [SB Delayed Warnings] Don't delay the interstitial on prerender pages
    This CL properly handles prerender pages when the SafeBrowsing Delayed
    Warning experiment is enabled.
    
    It also moves SafeBrowsing related Prerender tests into a new class
    and parametrizes the class so that we can test Prerender & SafeBrowsing
    interaction with and without the experiment
    
    Bug: 1057157
    Change-Id: I1f00c00478e94d71ca3cb79ccca945b706a8a6e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2148286
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764032}

--- components/safe_browsing/core/browser/safe_browsing_url_checker_impl.cc
+++ components/safe_browsing/core/browser/safe_browsing_url_checker_impl.cc
@@ -202,9 +202,11 @@ void SafeBrowsingUrlCheckerImpl::OnUrlResult(const GURL& url,
 
   TRACE_EVENT_ASYNC_END1("safe_browsing", "CheckUrl", this, "url", url.spec());
 
+  const bool is_prefetch = (load_flags_ & net::LOAD_PREFETCH);
+
   // Handle main frame and subresources. We do this to catch resources flagged
   // as phishing even if the top frame isn't flagged.
-  if (threat_type == SB_THREAT_TYPE_URL_PHISHING &&
+  if (!is_prefetch && threat_type == SB_THREAT_TYPE_URL_PHISHING &&
       base::FeatureList::IsEnabled(kDelayedWarnings)) {
     if (state_ != STATE_DELAYED_BLOCKING_PAGE) {
       // Delayed warnings experiment delays the warning until a user interaction
@@ -239,7 +241,7 @@ void SafeBrowsingUrlCheckerImpl::OnUrlResult(const GURL& url,
     return;
   }
 
-  if (load_flags_ & net::LOAD_PREFETCH) {
+  if (is_prefetch) {
     // Destroy the prefetch with FINAL_STATUS_SAFEBROSWING.
     if (resource_type_ == ResourceType::kMainFrame) {
       url_checker_delegate_->MaybeDestroyPrerenderContents(
@@ -445,7 +447,8 @@ void SafeBrowsingUrlCheckerImpl::BlockAndProcessUrls(bool showed_interstitial) {
 void SafeBrowsingUrlCheckerImpl::OnBlockingPageComplete(
     bool proceed,
     bool showed_interstitial) {
-  DCHECK_EQ(STATE_DISPLAYING_BLOCKING_PAGE, state_);
+  DCHECK(state_ == STATE_DISPLAYING_BLOCKING_PAGE ||
+         state_ == STATE_DELAYED_BLOCKING_PAGE);
 
   if (proceed) {
     state_ = STATE_NONE;
