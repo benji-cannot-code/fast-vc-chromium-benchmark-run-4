38574eaa7b14072e9dfd3b172105fe4ec5853f84
Theresa Wellington <twellington@chromium.org>
2020-04-29 17:20:56 +0000

    Remove ChromeFullscreenManager.FullscreenListener#onUpdateViewportSize
    Remove the FullscreenListener method in favor of a delegate. The
    viewport isn't actually resized until ChromeFullscreenManager processes
    the event (so it's the delegate).
    
    There's more refactoring that could be done here to make this even
    better, but this is a step in the right direction.
    
    Also removes AssistantOverlayDrawable#onUpdateViewportSize since it
    was determined that's not needed due to other viewport size polling
    logic.
    
    BUG=995914
    
    Change-Id: I82b0c48863ab428abf5a4ae78f2e0654ad5c185b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2170560
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Theresa  <twellington@chromium.org>
    Auto-Submit: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763880}

--- chrome/android/java/src/org/chromium/chrome/browser/fullscreen/ChromeFullscreenManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/fullscreen/ChromeFullscreenManager.java
@@ -110,6 +110,7 @@ public class ChromeFullscreenManager extends FullscreenManager
     private ContentView mContentView;
 
     private final ArrayList<FullscreenListener> mListeners = new ArrayList<>();
+    private Runnable mViewportSizeDelegate;
 
     /** The animator for slide-in animation on the Android controls. */
     private ValueAnimator mControlsAnimator;
@@ -169,11 +170,6 @@ public class ChromeFullscreenManager extends FullscreenManager
          * Called when the height of the top controls are changed.
          */
         default void onTopControlsHeightChanged(int topControlsHeight, int topControlsMinHeight) {}
-
-        /**
-         * Called when the viewport size of the active content is updated.
-         */
-        default void onUpdateViewportSize() {}
 
         /**
          * Called when entering fullscreen mode.
@@ -692,6 +688,13 @@ public class ChromeFullscreenManager extends FullscreenManager
     }
 
     /**
+     * @param delegate A Runnable to be executed when the WebContents viewport should be updated.
+     */
+    public void setViewportSizeDelegate(Runnable delegate) {
+        mViewportSizeDelegate = delegate;
+    }
+
+    /**
      * Updates viewport size to have it render the content correctly.
      */
     public void updateViewportSize() {
@@ -703,7 +706,7 @@ public class ChromeFullscreenManager extends FullscreenManager
 
         mControlsResizeView = getContentOffset() > getTopControlsMinHeight()
                 || getBottomContentOffset() > getBottomControlsMinHeight();
-        for (FullscreenListener listener : mListeners) listener.onUpdateViewportSize();
+        if (mViewportSizeDelegate != null) mViewportSizeDelegate.run();
     }
 
     /**
