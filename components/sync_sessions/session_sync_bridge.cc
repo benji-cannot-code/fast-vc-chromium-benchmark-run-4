c0d1f7ff4b03369629b7d40a0277eff09330c155
Jan Krcal <jkrcal@chromium.org>
2020-04-29 12:06:26 +0000

    [Session sync] Replace HistoryService getter by it's one function
    This CL stops exposing the full HistoryService in SyncSessionsClient and
    exposes the only public function that is needed.
    
    Bug: 978775
    Change-Id: I564f8f55be7de82a6a87282326a615a94e7042cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162870
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763777}

--- components/sync_sessions/session_sync_bridge.cc
+++ components/sync_sessions/session_sync_bridge.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "base/time/time.h"
-#include "components/history/core/browser/history_service.h"
 #include "components/sync/base/client_tag_hash.h"
 #include "components/sync/base/time.h"
 #include "components/sync/model/data_type_activation_request.h"
@@ -287,11 +286,7 @@ void SessionSyncBridge::ApplyStopSyncChanges(
     // synced history data, especially by HistoryUiFaviconRequestHandler. We do
     // it upon disabling of sessions sync to have symmetry with the condition
     // checked inside that layer to allow downloads (sessions sync enabled).
-    history::HistoryService* history_service =
-        sessions_client_->GetHistoryService();
-    if (history_service) {
-      history_service->ClearAllOnDemandFavicons();
-    }
+    sessions_client_->ClearAllOnDemandFavicons();
   }
   syncing_.reset();
 }
