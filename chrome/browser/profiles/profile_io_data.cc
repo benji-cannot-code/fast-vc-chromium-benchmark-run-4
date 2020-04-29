06b0ed7aeed479a910cef1dfb0e1ee93ff7243ff
Matt Mueller <mattm@chromium.org>
2020-04-29 15:12:41 +0000

    Remove unused include
    Change-Id: Ic486ea95c41fc59eb359b6ba837786d75b837d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171836
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#763821}

--- chrome/browser/profiles/profile_io_data.cc
+++ chrome/browser/profiles/profile_io_data.cc
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "extensions/buildflags/buildflags.h"
-#include "net/ssl/client_cert_store.h"
 #include "services/network/network_service.h"
 #include "services/network/public/cpp/features.h"
 #include "third_party/blink/public/public_buildflags.h"
