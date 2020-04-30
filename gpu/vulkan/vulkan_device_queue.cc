c4206818e37066506135c73137584ffc7d863cd4
Piotr Tworek <ptworek@vewd.com>
2020-04-30 13:28:42 +0000

    Add missing cstring include in //gpu/vulkan/vulkan_device_queue.cc
    The code in this file uses std::strcmp, but fails to include header
    declaring this function. This leads to build failures on Linux systems
    using glibc 2.30, or newer.
    
    Bug: 1074286
    Change-Id: I6b1cfa6194db8a19c26726508ebff853f1b2e4db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171485
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#764232}

--- gpu/vulkan/vulkan_device_queue.cc
+++ gpu/vulkan/vulkan_device_queue.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/vulkan/vulkan_device_queue.h"
 
+#include <cstring>
 #include <unordered_set>
 #include <utility>
 #include <vector>
