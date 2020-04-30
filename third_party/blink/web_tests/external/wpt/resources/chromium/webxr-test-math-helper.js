8f63eb2f324eb99a07d0773961c667cbdc3b0da4
Alexander Cooper <alcooper@chromium.org>
2020-04-30 21:04:05 +0000

    Update pointerOrigin usage
    Updates pointerOrigin to be represented as mojoFromPointer as the test
    API dictates. This required changing input pointer transforms to be
    transformed by their corresponding grip transforms in a few tests where
    the output matrices relied on the incorrect behavior (e.g. assuming that
    the pointerOrigin was actually inputFromPointer). This required
    simplifying getMojoFromInputSource, and it was moved onto inputSource,
    as a more logical home for the method.
    
    Unfortunately the assumption that pointerOrigin was inputFromPointer was
    pretty integral in the assumptions of the getInputPose_pointer test, so
    it had to be updated more thoroughly.
    
    Change-Id: Ied17b523640fa9a2f73c3d4422b341f4b5412212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173519
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764414}

--- third_party/blink/web_tests/external/wpt/resources/chromium/webxr-test-math-helper.js
+++ third_party/blink/web_tests/external/wpt/resources/chromium/webxr-test-math-helper.js
@@ -239,6 +239,15 @@ class XRMathHelper {
 
     return result;
   }
+
+  static identity() {
+    return [
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1
+    ];
+  };
 }
 
 XRMathHelper.EPSILON = 0.001;
