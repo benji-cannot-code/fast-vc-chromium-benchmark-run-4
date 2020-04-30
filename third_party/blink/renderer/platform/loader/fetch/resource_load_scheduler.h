b3d17b230250324fbbc2e1487f6b3a70dd71ea6b
Takashi Toyoshima <toyoshim@chromium.org>
2020-04-30 08:30:27 +0000

    ResourceLoadScheduler: remove traffic monitoring metrics
    As these are expiring and not used recently, let's remove
    them all.
    
    Bug: 1037587
    Change-Id: I5f6b925b0a3d01f3362d6746f97b986eb6da7c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2037188
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764180}

--- third_party/blink/renderer/platform/loader/fetch/resource_load_scheduler.h
+++ third_party/blink/renderer/platform/loader/fetch/resource_load_scheduler.h
@@ -227,8 +227,6 @@ class PLATFORM_EXPORT ResourceLoadScheduler final
   }
 
  private:
-  class TrafficMonitor;
-
   class ClientIdWithPriority {
    public:
     struct Compare {
@@ -344,9 +342,6 @@ class PLATFORM_EXPORT ResourceLoadScheduler final
   // Remembers elapsed times in seconds when the top request in each queue is
   // processed.
   std::map<ThrottleOption, base::Time> pending_queue_update_times_;
-
-  // Holds an internal class instance to monitor and report traffic.
-  std::unique_ptr<TrafficMonitor> traffic_monitor_;
 
   // Handle to throttling observer.
   std::unique_ptr<FrameOrWorkerScheduler::LifecycleObserverHandle>
