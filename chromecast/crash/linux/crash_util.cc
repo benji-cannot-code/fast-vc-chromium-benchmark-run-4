99bc91931e28b7958ca13d93801e7bc643606a72
Joshua LeVasseur <jlevasseur@chromium.org>
2020-04-30 20:35:43 +0000

    Chromecast: refactor dumpstate collection logic
    Add some crash helper methods.
    
    Bug: internal b/153016480
    Test: Ensure that a dumpstate is collected upon a crash.
    Change-Id: I602d755a4267e4687cdef2cf33ea5c1047eb5ba7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173580
    Reviewed-by: Luke Halliwell (slow) <halliwell@chromium.org>
    Commit-Queue: Joshua LeVasseur <jlevasseur@chromium.org>
    Auto-Submit: Joshua LeVasseur <jlevasseur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764402}

--- chromecast/crash/linux/crash_util.cc
+++ chromecast/crash/linux/crash_util.cc
@@ -5,12 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/crash/linux/crash_util.h"
 
+#include <string>
+
 #include "base/bind.h"
-#include "base/files/file_path.h"
+#include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "chromecast/base/path_utils.h"
+#include "chromecast/base/process_utils.h"
 #include "chromecast/base/version.h"
 #include "chromecast/crash/app_state_tracker.h"
 #include "chromecast/crash/linux/dummy_minidump_generator.h"
@@ -19,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromecast {
 
 namespace {
+const char kDumpStateSuffix[] = ".txt.gz";
+const char kMinidumpsDir[] = "minidumps";
+const size_t kMaxFilesInMinidumpsDir = 22;  // 10 crashes
 
 // This can be set to a callback for testing. This allows us to inject a fake
 // dumpstate routine to avoid calling an executable during an automated test.
@@ -27,6 +33,43 @@ namespace {
 static base::OnceCallback<int(const std::string&)>* g_dumpstate_cb = nullptr;
 
 }  // namespace
+
+// static
+bool CrashUtil::HasSpaceToCollectCrash() {
+  // Normally the path is protected by a file lock, but we don't need to acquire
+  // that just to make an estimate of the space consumed.
+  base::FilePath dump_dir(GetHomePathASCII(kMinidumpsDir));
+  base::FileEnumerator file_enumerator(dump_dir, false /* recursive */,
+                                       base::FileEnumerator::FILES);
+  size_t total = 0;
+  for (base::FilePath name = file_enumerator.Next(); !name.empty();
+       name = file_enumerator.Next()) {
+    total++;
+  }
+  return total <= kMaxFilesInMinidumpsDir;
+}
+
+// static
+bool CrashUtil::CollectDumpstate(const base::FilePath& minidump_path,
+                                 base::FilePath* dumpstate_path) {
+  std::vector<std::string> argv = {
+      chromecast::GetBinPathASCII("dumpstate").value(),
+      "-w",
+      "crash-request",
+      "-z",
+      "-o",
+      minidump_path.value() /* dumpstate appends ".txt.gz" to the filename */
+  };
+
+  std::string log;
+  if (!chromecast::GetAppOutput(argv, &log)) {
+    LOG(ERROR) << "failed to execute dumpstate";
+    return false;
+  }
+
+  *dumpstate_path = minidump_path.AddExtensionASCII(kDumpStateSuffix);
+  return true;
+}
 
 // static
 uint64_t CrashUtil::GetCurrentTimeMs() {
