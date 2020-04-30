8ff372a6d0c3be6d6ac8a50d2bfe50f7c97b886c
Amr Aboelkher <amraboelkher@google.com>
2020-04-30 00:45:33 +0000

    Support PSM client library in chromium
    Private Set Membership (PSM) is a cryptographic protocol that
    allows users to privately query whether the user's identifier is a
    member of a set of identifiers held by a server in a
    privacy-preserving manner.
    
    This CL does the following:
    - Adds the required dependencies files, for the library to be
    compiled successfully in chromium
    - Migrating the library into C++14 instead of C++17 pure features
    -- According to the guide in go/chrome-porting-psm-libs
    - Fixing the conflicts between casting of several StatusOr
    - Disable, temporarily, the possibility of adding PSM client
    directly from chromium
    - Neglect formatting rules for sorting headers and the
    specific style format as well
    - Overrides include_rules section for PSM client
    - Adds the library as another client for abseil-cpp
    - Adds the owners of the library
    - Adds the README.chromium
    -- Include overall explanation about the library
    -- Include instructions how to update the library
    
    Bug: chromium:1072795
    Change-Id: I563fb14fd1075202d39a4109be23ea69bfab5b86
    
    TBR=thakis
    
    Change-Id: I563fb14fd1075202d39a4109be23ea69bfab5b86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157072
    Reviewed-by: Amr Aboelkher <amraboelkher@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Amr Aboelkher <amraboelkher@google.com>
    Cr-Commit-Position: refs/heads/master@{#764076}

---
+++ third_party/private_membership/src/internal/testing/constants.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#ifndef THIRD_PARTY_PRIVATE_MEMBERSHIP_SRC_INTERNAL_TESTING_CONSTANTS_H_
+#define THIRD_PARTY_PRIVATE_MEMBERSHIP_SRC_INTERNAL_TESTING_CONSTANTS_H_
+
+#include <openssl/obj_mac.h>
+
+namespace private_membership {
+namespace rlwe {
+
+// Identifier of the elliptic curve used in tests.
+constexpr int kTestCurveId = NID_X9_62_prime256v1;
+
+}  // namespace rlwe
+}  // namespace private_membership
+
+#endif  // THIRD_PARTY_PRIVATE_MEMBERSHIP_SRC_INTERNAL_TESTING_CONSTANTS_H_
