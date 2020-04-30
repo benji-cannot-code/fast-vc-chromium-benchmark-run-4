4c6243c53b4ebe4c70d841ca8f26f600abe5b72a
Piotr Tworek <ptworek@vewd.com>
2020-04-30 16:00:18 +0000

    Add missing limits.h include in //components/prefs/pref_value_map.cc.
    This is needed to get the definition of CHAR_MAX. This popped up after
    I've updated one of my Linux systems to glibc 2.30. My guess is the
    header was somehow included indirectly by older glibc versions.
    
    Bug: 1074286
    Change-Id: I07b154c51877c7c8d752f7f164a0a86815fd72b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172787
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764287}

--- components/prefs/pref_value_map.cc
+++ components/prefs/pref_value_map.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/prefs/pref_value_map.h"
 
+#include <limits.h>
 #include <map>
 #include <memory>
 #include <utility>
