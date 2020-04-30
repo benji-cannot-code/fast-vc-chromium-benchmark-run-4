d3ff8ba5afe492ce6940757a51e6af419e635d6c
dpapad <dpapad@chromium.org>
2020-04-30 18:07:55 +0000

    Settings: Remove various obsolete Polymer2 leftovers from tests.
    Bug: 1069258
    Change-Id: I260b71f6cd6b188ef4b8c74e2049dd49e3fd077f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173725
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764333}

--- chrome/test/data/webui/settings/all_sites_tests.js
+++ chrome/test/data/webui/settings/all_sites_tests.js
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // clang-format off
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
-import {PromiseResolver} from 'chrome://resources/js/promise_resolver.m.js';
 import {beforeNextRender,flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {ContentSetting,ContentSettingsTypes,LocalDataBrowserProxyImpl,SiteSettingsPrefsBrowserProxyImpl} from 'chrome://settings/lazy_load.js';
 import {CrSettingsPrefs, Router,routes} from 'chrome://settings/settings.js';
@@ -135,22 +134,13 @@ suite('AllSites', function() {
     setUpAllSites(prefsVarious);
     testElement.populateList_();
     return browserProxy.whenCalled('getAllSites').then(() => {
-      // Use resolver to ensure that the list container is populated.
-      const resolver = new PromiseResolver();
-      // In Polymer2, we need to wait until after the next render for the list
-      // to be populated.
-      beforeNextRender(testElement, () => {
-        resolver.resolve();
-      });
-      return resolver.promise.then(() => {
-        assertEquals(3, testElement.siteGroupMap.size);
+      assertEquals(3, testElement.siteGroupMap.size);
 
-        // Flush to be sure list container is populated.
-        flush();
-        const siteEntries =
-            testElement.$.listContainer.querySelectorAll('site-entry');
-        assertEquals(3, siteEntries.length);
-      });
+      // Flush to be sure list container is populated.
+      flush();
+      const siteEntries =
+          testElement.$.listContainer.querySelectorAll('site-entry');
+      assertEquals(3, siteEntries.length);
     });
   });
 
