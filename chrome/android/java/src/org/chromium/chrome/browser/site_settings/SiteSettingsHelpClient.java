c88760aeae01afcc985797c51d4a1a9a243364c9
Robbie McElrath <rmcelrath@chromium.org>
2020-04-30 21:18:07 +0000

    [WebLayer] Control site settings help menu option with SiteSettingsClient.
    This CL introduces a SiteSettingsHelpClient interface that pulls in the
    existing help related SiteSettingsClient methods, and introduces a new
    method to control the visibility of help menu items. The help menu
    behavior is currently Chrome-specific, so this provides us a mechanism
    to hide the options for non-Chrome embedders.
    
    Bug: 1058595
    Change-Id: I4c3db35655fa1f237e019baced141120de1ce44a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2164028
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764418}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/site_settings/SiteSettingsHelpClient.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.site_settings;
+
+import android.app.Activity;
+
+/**
+ * An interface that allows the Site Settings UI to link to and open embedder-specific help pages.
+ */
+public interface SiteSettingsHelpClient {
+    /**
+     * @return true if Help and Feedback links and menu items should be shown to the user.
+     */
+    boolean isHelpAndFeedbackEnabled();
+
+    /**
+     * Launches a support page relevant to settings UI pages.
+     *
+     * @see org.chromium.chrome.browser.help.HelpAndFeedback#show
+     */
+    void launchSettingsHelpAndFeedbackActivity(Activity currentActivity);
+
+    /**
+     * Launches a support page related to protected content.
+     *
+     * @see org.chromium.chrome.browser.help.HelpAndFeedback#show
+     */
+    void launchProtectedContentHelpAndFeedbackActivity(Activity currentActivity);
+}
