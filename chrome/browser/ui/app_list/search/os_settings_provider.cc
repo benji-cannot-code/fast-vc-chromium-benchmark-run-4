5863b463351c3c9170ec97d288d16e929202a1d7
tby <tby@chromium.org>
2020-04-30 00:13:00 +0000

    [Launcher settings] Fix results persisting to zero-state.
    I forgot to clear the results at the start of a search without a query,
    meaning the results would persist to the next zero-state list after a
    query containing settings results. This CL fixes that.
    
    Bug: 1068851
    Change-Id: If71cc054e500ae83b3f703a86983b68ac48a6e21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172265
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764067}

--- chrome/browser/ui/app_list/search/os_settings_provider.cc
+++ chrome/browser/ui/app_list/search/os_settings_provider.cc
@@ -83,6 +83,8 @@ OsSettingsProvider::OsSettingsProvider(Profile* profile)
 OsSettingsProvider::~OsSettingsProvider() = default;
 
 void OsSettingsProvider::Start(const base::string16& query) {
+  ClearResultsSilently();
+
   // This provider does not handle zero-state.
   if (query.empty())
     return;
