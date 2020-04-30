a7e7b1226251f0038c9cf7f3a76f19c006b18012
Eugene But <eugenebut@chromium.org>
2020-04-30 05:04:00 +0000

    Revert "Use module_cache_mac.cc instead of module_cache_stub.cc on iOS"
    This reverts commit 294c8d0bcda1c26e868caeb497e4f086af27866b.
    
    Reason for revert: Breaks Cronet build, which supports 32 bit
    
    Original change's description:
    > Use module_cache_mac.cc instead of module_cache_stub.cc on iOS
    > 
    > This will allow to return valid module from CreateModuleForAddress() as
    > required by HeapProfilerController.
    > 
    > Bug: 1075702
    > Change-Id: I7a949754ed8a3fd7df7679e6de6a2d722cece925
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171465
    > Auto-Submit: Eugene But <eugenebut@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Commit-Queue: Eugene But <eugenebut@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#763679}
    
    TBR=eugenebut@chromium.org,mark@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1075702
    Change-Id: I78b15a6d0a64b19ce1e95982e1f8e193fa333abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173705
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764119}

---
+++ base/profiler/module_cache_stub.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/profiler/module_cache.h"
+
+namespace base {
+
+// static
+std::unique_ptr<const ModuleCache::Module> ModuleCache::CreateModuleForAddress(
+    uintptr_t address) {
+  return nullptr;
+}
+
+}  // namespace base
