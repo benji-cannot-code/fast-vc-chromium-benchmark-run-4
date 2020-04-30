9f42f1b5d046b30637397e11fd2b01b94d17ed3c
Kurt Horimoto <kkhorimoto@chromium.org>
2020-04-30 05:35:33 +0000

    [iOS] Add Safe Browsing opt out setting.
    Bug: none
    Change-Id: I15f78a700cfc24ba3dd0209be3a182f61b38e396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172652
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764141}

--- ios/chrome/browser/prefs/browser_prefs.mm
+++ ios/chrome/browser/prefs/browser_prefs.mm
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/voice/voice_search_prefs_registration.h"
 #import "ios/chrome/browser/web/font_size_tab_helper.h"
 #include "ios/public/provider/chrome/browser/chrome_browser_provider.h"
+#import "ios/web/common/features.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
