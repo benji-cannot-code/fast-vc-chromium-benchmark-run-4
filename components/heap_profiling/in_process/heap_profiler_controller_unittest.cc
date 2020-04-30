75be8de5892493006a697d5b27b839b583b27940
Eugene But <eugenebut@chromium.org>
2020-04-30 16:12:25 +0000

    Componetize HeapProfilerController to use on iOS
    This CL only moves files and HeapProfilerController compilation
    is still turned off on iOS, because fixing compilation requires
    changes in base. There will be a few follow up CLs to:
     - separate iOS and non-iOS feature flags
     - compile HeapProfilerController on iOS
     - instantiate and start HeapProfilerController on iOS
    
    Bug: 1075702
    Change-Id: I01ecbe24f104807a534681cb1d483cdb509569f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168923
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jesse Doherty <jwd@chromium.org>
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764289}

--- components/heap_profiling/in_process/heap_profiler_controller_unittest.cc
+++ components/heap_profiling/in_process/heap_profiler_controller_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/common/heap_profiler_controller.h"
+#include "components/heap_profiling/in_process/heap_profiler_controller.h"
 
 #include "base/sampling_heap_profiler/sampling_heap_profiler.h"
 #include "base/test/bind_test_util.h"
