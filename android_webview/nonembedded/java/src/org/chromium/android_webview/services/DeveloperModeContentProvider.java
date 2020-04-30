26949cb6f32b4e42623965be0414abd8e7de3692
Nate Fischer <ntfschr@chromium.org>
2020-04-30 22:59:53 +0000

    [AW][Dev-UI] persist flags across reboots/updates
    This persists flags across reboots, updates, force-stops, etc. (anything
    which would kill the service process). The main motivation is to make it
    easier to dogfood features.
    
    Fixed: 1048405
    Test: toggle flags, force-stop dev UI, restart dev UI to observe flags
    Test: I also tested rebooting the device and installing updates
    Test: locally remove flags from allowlist, verify dev mode is disabled
    Change-Id: I270681f82c34a74180c3605ef9f368a91c74d49d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163858
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764464}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/DeveloperModeContentProvider.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/DeveloperModeContentProvider.java
@@ -4,11 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.android_webview.services;
 
-import android.content.ComponentName;
 import android.content.ContentProvider;
 import android.content.ContentValues;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.net.Uri;
@@ -56,22 +53,9 @@ public final class DeveloperModeContentProvider extends ContentProvider {
                 boolean enabled = entry.getValue();
                 cursor.addRow(new Object[] {flagName, enabled ? 1 : 0});
             }
-            if (flagOverrides.isEmpty()) {
-                disableDeveloperMode();
-            }
             return cursor;
         }
         return null;
-    }
-
-    private void disableDeveloperMode() {
-        ComponentName developerModeState =
-                new ComponentName(getContext(), DeveloperModeUtils.DEVELOPER_MODE_STATE_COMPONENT);
-        getContext().getPackageManager().setComponentEnabledSetting(developerModeState,
-                PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, PackageManager.DONT_KILL_APP);
-
-        // Stop the service explicitly, in case it's running. NOOP if the service is not running.
-        getContext().stopService(new Intent(getContext(), DeveloperUiService.class));
     }
 
     @Override
