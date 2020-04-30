21f7de4830b48e4d7b5b9962476eb4de7b8c7fac
Michael Lippautz <mlippautz@chromium.org>
2020-04-30 06:20:27 +0000

    heap: Remove scheduled precise GCs
    The code has been dead since unified heap launched as regular
    scheduled precise GCs were used as non-critical follow up GCs to V8.
    
    Bug: chromium:1074061
    Change-Id: Ic50d3838cd5a0a8b148f7e44cd2731c42e953064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172906
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764148}

--- third_party/blink/renderer/platform/heap/thread_state.cc
+++ third_party/blink/renderer/platform/heap/thread_state.cc
@@ -591,12 +591,6 @@ void ThreadState::ScheduleConcurrentAndLazySweep() {
           WTF::CrossThreadUnretained(this))));
 }
 
-void ThreadState::SchedulePreciseGC() {
-  DCHECK(CheckThread());
-  CompleteSweep();
-  SetGCState(kPreciseGCScheduled);
-}
-
 namespace {
 
 #define UNEXPECTED_GCSTATE(s)                                   \
@@ -607,7 +601,6 @@ namespace {
 void UnexpectedGCState(ThreadState::GCState gc_state) {
   switch (gc_state) {
     UNEXPECTED_GCSTATE(kNoGCScheduled);
-    UNEXPECTED_GCSTATE(kPreciseGCScheduled);
     UNEXPECTED_GCSTATE(kForcedGCForTestingScheduled);
     UNEXPECTED_GCSTATE(kIncrementalMarkingStepPaused);
     UNEXPECTED_GCSTATE(kIncrementalMarkingStepScheduled);
@@ -628,13 +621,13 @@ void ThreadState::SetGCState(GCState gc_state) {
   switch (gc_state) {
     case kNoGCScheduled:
       DCHECK(CheckThread());
-      VERIFY_STATE_TRANSITION(
-          gc_state_ == kNoGCScheduled || gc_state_ == kPreciseGCScheduled ||
-          gc_state_ == kForcedGCForTestingScheduled ||
-          gc_state_ == kIncrementalMarkingStepPaused ||
-          gc_state_ == kIncrementalMarkingStepScheduled ||
-          gc_state_ == kIncrementalMarkingFinalizeScheduled ||
-          gc_state_ == kIncrementalGCScheduled);
+      VERIFY_STATE_TRANSITION(gc_state_ == kNoGCScheduled ||
+                              gc_state_ == kForcedGCForTestingScheduled ||
+                              gc_state_ == kIncrementalMarkingStepPaused ||
+                              gc_state_ == kIncrementalMarkingStepScheduled ||
+                              gc_state_ ==
+                                  kIncrementalMarkingFinalizeScheduled ||
+                              gc_state_ == kIncrementalGCScheduled);
       break;
     case kIncrementalMarkingStepScheduled:
       DCHECK(CheckThread());
@@ -648,7 +641,6 @@ void ThreadState::SetGCState(GCState gc_state) {
       VERIFY_STATE_TRANSITION(gc_state_ == kIncrementalMarkingStepScheduled);
       break;
     case kForcedGCForTestingScheduled:
-    case kPreciseGCScheduled:
       DCHECK(CheckThread());
       DCHECK(!IsSweepingInProgress());
       VERIFY_STATE_TRANSITION(gc_state_ == kNoGCScheduled ||
@@ -656,7 +648,6 @@ void ThreadState::SetGCState(GCState gc_state) {
                               gc_state_ == kIncrementalMarkingStepScheduled ||
                               gc_state_ ==
                                   kIncrementalMarkingFinalizeScheduled ||
-                              gc_state_ == kPreciseGCScheduled ||
                               gc_state_ == kForcedGCForTestingScheduled ||
                               gc_state_ == kIncrementalGCScheduled);
       break;
@@ -711,12 +702,6 @@ void ThreadState::RunScheduledGC(BlinkGC::StackState stack_state) {
   switch (GetGCState()) {
     case kForcedGCForTestingScheduled:
       CollectAllGarbageForTesting();
-      break;
-    case kPreciseGCScheduled:
-      CollectGarbage(BlinkGC::CollectionType::kMajor,
-                     BlinkGC::kNoHeapPointersOnStack, BlinkGC::kAtomicMarking,
-                     BlinkGC::kConcurrentAndLazySweeping,
-                     BlinkGC::GCReason::kPreciseGC);
       break;
     default:
       break;
