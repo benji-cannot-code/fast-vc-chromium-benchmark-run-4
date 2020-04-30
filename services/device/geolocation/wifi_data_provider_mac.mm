bfbe81c318b8ef4e82a997075b2757542063ac3b
Chris Mumford <cmumford@chromium.org>
2020-04-30 20:15:23 +0000

    [geolocation] Stop logging Net.Wifi.* UMA metrics.
    Stop logging the following disused UMA metrics:
    
        Net.Wifi.InterfaceCount
        Net.Wifi.LbsLatency
        Net.Wifi.ScanLatency
    
    Bug: 1036866
    Change-Id: Icfdc281cb35304bb2813edb58141f308c8add8ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173145
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: James Hollyer <jameshollyer@google.com>
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Cr-Commit-Position: refs/heads/master@{#764393}

--- services/device/geolocation/wifi_data_provider_mac.mm
+++ services/device/geolocation/wifi_data_provider_mac.mm
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_nsobject.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/strings/sys_string_conversions.h"
 #include "services/device/geolocation/wifi_data_provider_common.h"
 #include "services/device/geolocation/wifi_data_provider_manager.h"
@@ -42,8 +41,6 @@ bool CoreWlanApi::GetAccessPointData(WifiData::AccessPointDataSet* data) {
     NSArray<CWInterface*>* interfaces = [wifi_client_ interfaces];
     NSUInteger interface_error_count = 0;
     for (CWInterface* interface in interfaces) {
-      const base::TimeTicks start_time = base::TimeTicks::Now();
-
       NSError* err = nil;
       NSSet<CWNetwork*>* scan = [interface scanForNetworksWithName:nil
                                                              error:&err];
@@ -57,12 +54,6 @@ bool CoreWlanApi::GetAccessPointData(WifiData::AccessPointDataSet* data) {
         ++interface_error_count;
         continue;
       }
-
-      const base::TimeDelta duration = base::TimeTicks::Now() - start_time;
-
-      UMA_HISTOGRAM_CUSTOM_TIMES("Net.Wifi.ScanLatency", duration,
-                                 base::TimeDelta::FromMilliseconds(1),
-                                 base::TimeDelta::FromMinutes(1), 100);
 
       DVLOG(1) << interface.interfaceName << ": found " << count << " wifi APs";
 
@@ -83,10 +74,6 @@ bool CoreWlanApi::GetAccessPointData(WifiData::AccessPointDataSet* data) {
         data->insert(access_point_data);
       }
     }
-
-    UMA_HISTOGRAM_CUSTOM_COUNTS("Net.Wifi.InterfaceCount",
-                                [interfaces count] - interface_error_count, 1,
-                                5, 6);
 
     // Return true even if some interfaces failed to scan, so long as at least
     // one interface did not fail.
