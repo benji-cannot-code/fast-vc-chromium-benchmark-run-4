02e631c9b6f902d16664231dab846e9ade7db49b
Elly Fong-Jones <ellyjones@chromium.org>
2020-04-30 18:34:08 +0000

    cbuiv: remove WidgetDelegate overrides from GlobalErrorBubbleView
    This change also adds callbacks for each step of the WidgetDelegate
    teardown process to WidgetDelegate.
    
    Bug: 1075649
    Change-Id: Ifcabfd42cf39a70bbfd01bbff2304537bbfa812e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173626
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764345}

--- chrome/browser/ui/views/global_error_bubble_view.h
+++ chrome/browser/ui/views/global_error_bubble_view.h
@@ -27,13 +27,8 @@ class GlobalErrorBubbleView : public views::BubbleDialogDelegateView,
       const base::WeakPtr<GlobalErrorWithStandardBubble>& error);
   ~GlobalErrorBubbleView() override;
 
-  // views::WidgetDelegate implementation.
-  base::string16 GetWindowTitle() const override;
-  void WindowClosing() override;
-
   // views::BubbleDialogDelegateView implementation.
   void Init() override;
-  bool ShouldShowCloseButton() const override;
   void OnDialogInitialized() override;
 
   // GlobalErrorBubbleViewBase implementation.
