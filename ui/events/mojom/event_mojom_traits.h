962a75e03b18a98760c7b49228f95d5be2a27684
Ken Rockot <rockot@google.com>
2020-04-30 20:05:46 +0000

    Migrate remaining //ui typemaps to GN
    Bug: 1059389
    Change-Id: Ie8bae98b838cd1f051eaaf234aa4fdbc61052bea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173663
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764388}

--- ui/events/mojom/event_mojom_traits.h
+++ ui/events/mojom/event_mojom_traits.h
@@ -15,12 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/event_constants.h"
 #include "ui/events/mojom/event.mojom.h"
 #include "ui/events/mojom/event_constants.mojom.h"
+#include "ui/events/pointer_details.h"
 #include "ui/events/types/event_type.h"
 
 namespace ui {
 class Event;
 class LatencyInfo;
-struct PointerDetails;
 }
 
 namespace mojo {
