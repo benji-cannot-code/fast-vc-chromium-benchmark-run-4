b0ad054d04a31d71e86ff758e1819859e24d9e5b
Toby Huang <tobyhuang@chromium.org>
2020-04-30 00:48:52 +0000

    Ensure supervised users can view the HaTS survey more than once
    After the user completes a Happiness Tracking Survey (HaTS), the
    survey stores a cookie to prevent another survey from showing again
    for a period of time (30 days). Since we use a Finch experiment to
    control the random survey sampling in ChromeOS, we want to discard
    this cookie. Regular users and Googlers open the HaTS survey in an
    incognito dialog to avoid storing the cookie. However, incognito mode
    is disabled for supervised users, so a different workaround is needed.
    
    With the current setup, there shouldn't be a problem for supervised
    users because the HaTS survey only re-appears after at least 90 days,
    and this cookie expires naturally after 30 days. This CL adds some
    assertions to ensure that the survey doesn't re-appear before 30 days
    for supervised users.
    
    Bug: 1055644
    Change-Id: I0e5eb8e80e1f230e8b83c6fe4eed19de3b4021c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2104012
    Reviewed-by: Malay Keshav <malaykeshav@chromium.org>
    Commit-Queue: Toby Huang <tobyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764078}

--- chrome/browser/chromeos/hats/hats_notification_controller.cc
+++ chrome/browser/chromeos/hats/hats_notification_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/logging.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "chrome/app/vector_icons/vector_icons.h"
@@ -48,6 +49,16 @@ constexpr base::TimeDelta kHatsThreshold = base::TimeDelta::FromDays(90);
 
 // The threshold for a Googler is less.
 constexpr base::TimeDelta kHatsGooglerThreshold = base::TimeDelta::FromDays(30);
+
+// For supervised users, the HaTS survey stores a cookie that expires in this
+// many days.
+constexpr base::TimeDelta kCookieExpiry = base::TimeDelta::FromDays(30);
+
+// If the survey is shown again before the cookie expires, supervised users will
+// just see a blank dialog. We want to avoid this bug.
+static_assert(kCookieExpiry < kHatsThreshold,
+              "The show again threshold shouldn't be less than the cookie "
+              "expiration for supervised users");
 
 // Minimum amount of time after initial login or oobe after which we can show
 // the HaTS notification.
@@ -167,6 +178,14 @@ bool HatsNotificationController::ShouldShowSurveyToProfile(Profile* profile) {
   // |threshold_time| time delta.
   if (DidShowSurveyToProfileRecently(profile, threshold_time))
     return false;
+
+  // If this profile is a supervised user, ensure we don't show the survey again
+  // before the HaTS cookie expires. Otherwise, the supervised user will just
+  // see a blank dialog.
+  DCHECK(!profile->IsChild() ||
+         !DidShowSurveyToProfileRecently(profile, kCookieExpiry))
+      << "Don't show the HaTS survey to supervised users again before the "
+         "cookie expires";
 
   return true;
 }
