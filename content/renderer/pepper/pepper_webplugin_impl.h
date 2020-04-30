9cd0677a1076fceb447f119a22284a8216a61edc
Roger Tawa <rogerta@chromium.org>
2020-04-30 20:36:37 +0000

    Fix cut and paste command in PDF viewer.
    Recently the clipboard was moved from being owned by the render process
    host to being owned by the render frame host:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/1877532
    
    The above CL did not update the PDF viewer implementation so broke
    support for cut amd paste in PDF files.
    
    The clipboard handing code was consolidated into WebPluginContainerImpl
    whereas before it was splite between that class and PepperWebPluginImpl,
    so should be easier to follow.  This also does not require changing the
    WebPlugin interface in order to pass more data from the container to
    properly implement paste.
    
    Bug: 1072737
    Change-Id: Ibfb305688cc55287d6bbcf0f0c522fb6b1641ec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168838
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764403}

--- content/renderer/pepper/pepper_webplugin_impl.h
+++ content/renderer/pepper/pepper_webplugin_impl.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequenced_task_runner_helpers.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "ppapi/c/pp_var.h"
-#include "third_party/blink/public/mojom/clipboard/clipboard.mojom.h"
 #include "third_party/blink/public/mojom/input/focus_type.mojom-forward.h"
 #include "third_party/blink/public/web/web_plugin.h"
 #include "ui/gfx/geometry/rect.h"
@@ -109,7 +108,6 @@ class PepperWebPluginImpl : public blink::WebPlugin {
   gfx::Rect plugin_rect_;
   PP_Var instance_object_;
   blink::WebPluginContainer* container_;
-  mojo::Remote<blink::mojom::ClipboardHost> clipboard_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperWebPluginImpl);
 };
