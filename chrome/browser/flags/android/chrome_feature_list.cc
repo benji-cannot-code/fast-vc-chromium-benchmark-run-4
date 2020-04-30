1a9b665c66538e741a28d5c502daaf1034d278e5
Filip Gorski <fgorski@chromium.org>
2020-04-30 15:58:37 +0000

    [OSL-RV] Adding a feature flag to enable switching to RecyclerView
    Adds a flag to enable switching to recycler view and controlling it
    from variations server.
    
    Bug: 1075602
    Change-Id: Ic5f667cecb44fd7e4d0f0f34ca18f6452cefc3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173542
    Auto-Submit: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764286}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -240,6 +240,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &omnibox::kHideSteadyStateUrlTrivialSubdomains,
     &omnibox::kOmniboxAssistantVoiceSearch,
     &omnibox::kOmniboxSearchEngineLogo,
+    &omnibox::kOmniboxSuggestionsRecyclerView,
     &omnibox::kQueryInOmnibox,
     &password_manager::features::kGooglePasswordManager,
     &password_manager::features::kPasswordEditingAndroid,
