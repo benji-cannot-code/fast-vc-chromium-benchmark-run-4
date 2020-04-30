1057f457980b10fe5ee2b7979a948a79584194cb
Marc Grimme <marcgrimme@chromium.org>
2020-04-30 14:02:40 +0000

    Re-add crash reporting info to chrome://management
    This CL re-adds the falsely removed crash reporting information to the
    ManagementUIHandler::AddDeviceReportingInfo() method.
    
    Change that falsely removed 6bf5b850cc852f52b17bd82cdf59f9fe352508c0
    
    Change that introduced this functionality
    c64221d9332abdc5d6a37809db7a1d41a59ae12a
    
    Bug: 1076348
    Change-Id: I1281273b9a62900ff81473370134ca61aba60d5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172784
    Commit-Queue: Marc Grimme <marcgrimme@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764240}

--- chrome/browser/ui/webui/management_ui_handler.cc
+++ chrome/browser/ui/webui/management_ui_handler.cc
@@ -528,6 +528,10 @@ void ManagementUIHandler::AddDeviceReportingInfo(
                               kManagementReportNetworkInterfaces,
                               DeviceReportingType::kDevice);
   }
+  if (collector->ShouldReportCrashReportInfo()) {
+    AddDeviceReportingElement(report_sources, kManagementReportCrashReports,
+                              DeviceReportingType::kCrashReport);
+  }
   if (uploader->upload_enabled()) {
     AddDeviceReportingElement(report_sources, kManagementLogUploadEnabled,
                               DeviceReportingType::kLogs);
