64ba616c270ad6f415fed934f6e9c43efbf861e9
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-04-30 20:35:28 +0000

    Import wpt@4d413415dd8c6a0dc18d173071525dc0d12b11a6
    Using wpt-import in Chromium e0482d8e6e76b7b16dd4a2688aa2df0a20bec865.
    With Chromium commits locally applied on WPT:
    694e2d71ee "Add referrerpolicy to relevant mutations"
    f347930c06 "Add about:blank iframe referrer tests"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    nzolghadr@chromium.org, yosin@chromium.org:
      external/wpt/input-events
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=foolip
    
    No-Export: true
    Change-Id: I3c832182a8a7fe69492438ed7d26587756586ff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174698
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#764401}

--- third_party/blink/web_tests/external/wpt/periodic-background-sync/idlharness.https.any.js
+++ third_party/blink/web_tests/external/wpt/periodic-background-sync/idlharness.https.any.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-// https://github.com/WICG/BackgroundSync/blob/master/spec/PeriodicBackgroundSync-index.html
+// https://wicg.github.io/periodic-background-sync/
 
 const idl = `
 partial interface ServiceWorkerGlobalScope {
