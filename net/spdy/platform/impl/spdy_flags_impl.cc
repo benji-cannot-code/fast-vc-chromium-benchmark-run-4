193c2c4c6a05254deccae9b60c52cb2c664baf7b
Bence Béky <bnc@chromium.org>
2020-04-30 18:15:10 +0000

    Remove unused spdy::spdy_enable_granular_decompress_errors flag.
    All uses of the flag have been removed at
    https://quiche.googlesource.com/quiche/+/260f2e4e5763e56d0b824119af655cfddc655f78.
    This flag has been defined twice in Chromium: once in quic namespace,
    once in spdy namespace.  The former definition has already been removed
    at https://crrev.com/c/2170589
    
    Change-Id: I9e3b5d8caffddbc7178739910b1f07fc5450aea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173561
    Commit-Queue: Zhongyi Shi <zhongyi@chromium.org>
    Auto-Submit: Bence Béky <bnc@chromium.org>
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764336}

--- net/spdy/platform/impl/spdy_flags_impl.cc
+++ net/spdy/platform/impl/spdy_flags_impl.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/spdy/platform/impl/spdy_flags_impl.h"
 
-bool spdy_enable_granular_decompress_errors = true;
-
 // If true, use indexed name if possible when sending
 // Literal Header Field without Indexing instruction.
 bool spdy_hpack_use_indexed_name = true;
--- net/spdy/platform/impl/spdy_flags_impl.h
+++ net/spdy/platform/impl/spdy_flags_impl.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/base/net_export.h"
 
-NET_EXPORT_PRIVATE extern bool spdy_enable_granular_decompress_errors;
 NET_EXPORT_PRIVATE extern bool spdy_hpack_use_indexed_name;
 
 inline bool GetSpdyReloadableFlagImpl(bool flag) {
