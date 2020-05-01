72fa33dc809cf89aeed1d904b44ba3d3509c6cc2
Kyle Horimoto <khorimoto@chromium.org>
2020-05-01 15:02:25 +0000

    [CrOS Settings] Add routes.mojom
    (1) Adds string constants for every route in OS settings. Previously,
        constants only existed for a subset of routes.
    (2) Adds enums corresponding to settings Sections and Subpages. These
        will be used to record metrics related to which search results have
        been selected.
    (3) Updates os_route.js to utilize these constants. This ensures that
        the routes in JS and C++ stay in sync.
    
    These enums will be used in follow-up CLs to implement settings search.
    
    Note that the alternate C++ constants are removed in follow-up CL
    https://chromium-review.googlesource.com/c/chromium/src/+/2173586.
    
    Bug: 1076268
    Change-Id: Iee6b56173ca53c117d04352d9e4ee99966389919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172125
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764651}

--- chrome/test/data/webui/settings/chromeos/os_settings_page_test.js
+++ chrome/test/data/webui/settings/chromeos/os_settings_page_test.js
@@ -85,8 +85,10 @@ suite('OSSettingsPage', function() {
     Polymer.dom.flush();
     await test_util.flushTasks();
 
-    const sectionNames =
-        ['osPrivacy', 'osLanguages', 'files', 'osReset', 'dateTime', 'a11y'];
+    const sectionNames = [
+      'osPrivacy', 'osLanguages', 'files', 'osReset', 'dateTime',
+      'osAccessibility'
+    ];
 
     for (const name of sectionNames) {
       const section = settingsPage.shadowRoot.querySelector(
@@ -114,7 +116,7 @@ suite('OSSettingsPage', function() {
 
     const visibleSections = [
       'internet', 'bluetooth', 'device', 'osSearch', 'apps', 'osPrivacy',
-      'osLanguages', 'files', 'osReset', 'dateTime', 'a11y'
+      'osLanguages', 'files', 'osReset', 'dateTime', 'osAccessibility'
     ];
     for (const name of visibleSections) {
       const section = settingsPage.shadowRoot.querySelector(
