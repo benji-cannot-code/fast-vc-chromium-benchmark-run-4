a3e10b9ea931c19e275015b30da0a083f86db34f
Mustafa Emre Acer <meacer@chromium.org>
2020-05-01 00:26:04 +0000

    [SB Delayed Warnings] Show the warning when the page enters fullscreen
    This CL observes fullscreen events and immediately shows the delayed
    warning on the page. It also exits the fullscreen after showing the
    interstitial.
    
    Bug: 1057157
    Change-Id: Ibba15d4f83c7c4b1c48d2cd933722aeaaf0828b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172201
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764507}

--- chrome/browser/safe_browsing/user_interaction_observer.h
+++ chrome/browser/safe_browsing/user_interaction_observer.h
@@ -35,7 +35,10 @@ enum class DelayedWarningEvent {
   kWarningNotTriggeredOnMouseClick = 3,
   // User clicked on the page and the warning was shown.
   kWarningShownOnMouseClick = 4,
-  kMaxValue = kWarningShownOnMouseClick,
+  // The page tried to enter fullscreen mode.
+  kWarningShownOnFullscreenAttempt = 5,
+
+  kMaxValue = kWarningShownOnFullscreenAttempt,
 };
 
 // Name of the histogram.
@@ -73,6 +76,8 @@ class SafeBrowsingUserInteractionObserver
                              content::RenderViewHost* new_host) override;
   void WebContentsDestroyed() override;
   void DidStartNavigation(content::NavigationHandle* handle) override;
+  void DidToggleFullscreenModeForTab(bool entered_fullscreen,
+                                     bool will_cause_resize) override;
 
  private:
   static SafeBrowsingUserInteractionObserver* FromWebContents(
