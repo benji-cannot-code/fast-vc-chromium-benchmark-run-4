6d8726ef538a8cbae87b45d2c5c681dbb3a53ade
Chris Hall <chrishall@chromium.org>
2020-05-01 07:31:19 +0000

    Add presubmit check for relnotes field in accessibility changes.
    R=akihiroota,dmazzoni,jam,dpranke
    
    Change-Id: I37d5035967941da1d2e3d671426133668b87f0f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162427
    Commit-Queue: Chris Hall <chrishall@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764594}

--- PRESUBMIT_test_mocks.py
+++ PRESUBMIT_test_mocks.py
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+from collections import defaultdict
 import fnmatch
 import json
 import os
@@ -241,6 +242,7 @@ class MockChange(object):
 
   def __init__(self, changed_files):
     self._changed_files = changed_files
+    self.footers = defaultdict(list)
 
   def LocalPaths(self):
     return self._changed_files
@@ -248,3 +250,7 @@ class MockChange(object):
   def AffectedFiles(self, include_dirs=False, include_deletes=True,
                     file_filter=None):
     return self._changed_files
+
+  def GitFootersFromDescription(self):
+    return self.footers
+
