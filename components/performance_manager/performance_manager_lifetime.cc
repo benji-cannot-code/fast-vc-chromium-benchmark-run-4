c0bd649a25ce1445dc03fe974f2e5c8de4ce1f1b
Etienne Bergeron <etienneb@chromium.org>
2020-05-01 22:54:37 +0000

    [PM] Initial implementation of WorkerNodeImplDescriber
    R=chrisha
    
    Bug: 1077305
    Change-Id: I420afe1dea8f01661656ece5fa22ef2c8491a87b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175544
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764830}

--- components/performance_manager/performance_manager_lifetime.cc
+++ components/performance_manager/performance_manager_lifetime.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "components/performance_manager/decorators/page_load_tracker_decorator.h"
+#include "components/performance_manager/graph/worker_node_impl_describer.h"
 #include "components/performance_manager/performance_manager_impl.h"
 #include "components/performance_manager/public/decorators/tab_properties_decorator.h"
 #include "components/performance_manager/public/graph/graph.h"
@@ -20,6 +21,7 @@ void DefaultGraphCreatedCallback(
     GraphImpl* graph) {
   graph->PassToGraph(std::make_unique<PageLoadTrackerDecorator>());
   graph->PassToGraph(std::make_unique<TabPropertiesDecorator>());
+  graph->PassToGraph(std::make_unique<WorkerNodeImplDescriber>());
   std::move(external_graph_created_callback).Run(graph);
 }
 
