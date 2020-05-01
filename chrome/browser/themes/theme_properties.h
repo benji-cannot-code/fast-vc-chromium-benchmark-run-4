e72df34bfb458081ea370db099642e705b87c6ae
Evan Stade <estade@chromium.org>
2020-05-01 23:47:38 +0000

    Fix deps of //chrome/browser:theme_properties
    Bug: 1077322
    Change-Id: Iece3b4fd8f45dada49819a06c306cfa0359712f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176314
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764860}

--- chrome/browser/themes/theme_properties.h
+++ chrome/browser/themes/theme_properties.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "build/build_config.h"
-#include "chrome/common/buildflags.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/color_utils.h"
 
