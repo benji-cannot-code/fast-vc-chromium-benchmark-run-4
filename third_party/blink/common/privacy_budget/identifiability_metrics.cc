5d6fd48621d3e92d49dc13e84059466288e8d715
Caleb Raitto <caraitto@chromium.org>
2020-05-01 20:30:03 +0000

    Add a family of identifiability digest helpers.
    This will simplify and standardize digest calculation by providing a
    helper templated function that accepts a wide variety of types and
    number of arguments and produces a digest.
    
    The set of supported types can be extended by declaring overloads of
    IdentifiabilityDigestHelper() -- such declarations should be included
    before identifiability_digest_helpers.h so that they can be they can be
    used in the parameter pack overload.
    
    See example intended use of these helpers in crrev.com/c/2094474/5.
    
    Bug: 973801
    Change-Id: I9b58764545a7f1a6c9ca4dbbced0d455b64a1100
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2125447
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764769}

--- third_party/blink/common/privacy_budget/identifiability_metrics.cc
+++ third_party/blink/common/privacy_budget/identifiability_metrics.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-uint64_t DigestForMetrics(base::span<const uint8_t> in) {
+uint64_t IdentifiabilityDigestOfBytes(base::span<const uint8_t> in) {
   // NOTE: As documented at the point of declaration, the digest calculated here
   // should be stable once released.
   return base::PersistentHash(in);
