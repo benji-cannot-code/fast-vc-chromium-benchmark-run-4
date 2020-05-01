b198d6839018d9ce208b0d3a5e529392ce0eedb0
Colin Blundell <blundell@chromium.org>
2020-05-01 12:47:08 +0000

    Remove //chrome's AuthenticatorNavigationInterceptor
    This is no longer referenced downstream and hence can safely be removed
    without breaking the downstream build. We change the AppHooks method
    that is no longer being overridden downstream to talk in terms of the
    //components-level interface; hence, I can follow up with a downstream
    CL that implements that method and kick off the process of removing the
    V2() version that I had to add during the componentization.
    
    Bug: 1071390
    Change-Id: Ic8cd8c5734635a4b3708a9ef40f68c0596a1a61f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174427
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764630}

--- chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
+++ chrome/android/java/src/org/chromium/chrome/browser/AppHooks.java
@@ -52,6 +52,7 @@ import org.chromium.chrome.browser.webauth.Fido2ApiHandler;
 import org.chromium.chrome.browser.xsurface.ProcessScope;
 import org.chromium.chrome.browser.xsurface.SurfaceDependencyProvider;
 import org.chromium.components.browser_ui.widget.FeatureHighlightProvider;
+import org.chromium.components.external_intents.AuthenticatorNavigationInterceptor;
 import org.chromium.components.signin.AccountManagerDelegate;
 import org.chromium.components.signin.SystemAccountManagerDelegate;
 import org.chromium.policy.AppRestrictionsProvider;
@@ -109,11 +110,8 @@ public abstract class AppHooks {
     /**
      * Return a {@link AuthenticatorNavigationInterceptor} for the given {@link Tab}.
      * This can be null if there are no applicable interceptor to be built.
-     * NOTE: This method will be transitioned to talk in terms of the //components-level interface
-     * once downstream has been transitioned.
      */
-    public org.chromium.chrome.browser.tab.AuthenticatorNavigationInterceptor
-    createAuthenticatorNavigationInterceptor(Tab tab) {
+    public AuthenticatorNavigationInterceptor createAuthenticatorNavigationInterceptor(Tab tab) {
         return null;
     }
 
@@ -123,8 +121,7 @@ public abstract class AppHooks {
      * NOTE: This method exists only to allow downstream to transition to talking in terms of the
      * //components-level interface. It will be deleted once the transition is complete.
      */
-    public org.chromium.components.external_intents.AuthenticatorNavigationInterceptor
-    createAuthenticatorNavigationInterceptorV2(Tab tab) {
+    public AuthenticatorNavigationInterceptor createAuthenticatorNavigationInterceptorV2(Tab tab) {
         return null;
     }
 
