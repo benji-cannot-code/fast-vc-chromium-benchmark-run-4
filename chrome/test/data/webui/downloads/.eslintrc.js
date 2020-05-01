88727f84b7b993e5783da56c885b876c2fc668c2
rbpotter <rbpotter@chromium.org>
2020-05-01 20:09:59 +0000

    Web UI: Change !=/== to !==/=== for history, downloads, extensions tests
    This is a portion of
    https://chromium-review.googlesource.com/c/chromium/src/+/2003795
    by dbeam@ generated using the same command line, but only updating the
    history/, downloads/, and extensions/ folders in chrome/test/data/webui
    instead of the entire directory.
    
    Planning to split changes to other folders into additional CLs.
    
    Bug: 720034
    Change-Id: Ia1e675df9ad17602cc88e4acbdaeab126e183f54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173955
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764763}

--- chrome/test/data/webui/downloads/.eslintrc.js
+++ chrome/test/data/webui/downloads/.eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
   'rules': {
     'no-var': 'error',
     'prefer-const': 'error',
+    'eqeqeq': ['error', 'always', {'null': 'ignore'}],
   },
 };
--- chrome/test/data/webui/extensions/error_page_test.js
+++ chrome/test/data/webui/extensions/error_page_test.js
@@ -97,7 +97,7 @@ suite(extension_error_page_tests.suiteName, function() {
     let error = errorElements[0];
     expectEquals(
         'message', error.querySelector('.error-message').textContent.trim());
-    expectTrue(error.querySelector('iron-icon').icon == 'cr:error');
+    expectTrue(error.querySelector('iron-icon').icon === 'cr:error');
 
     const manifestError = Object.assign(
         {
@@ -115,7 +115,7 @@ suite(extension_error_page_tests.suiteName, function() {
     expectEquals(
         'invalid key',
         error.querySelector('.error-message').textContent.trim());
-    expectTrue(error.querySelector('iron-icon').icon == 'cr:warning');
+    expectTrue(error.querySelector('iron-icon').icon === 'cr:warning');
 
     mockDelegate.testClickingCalls(
         error.querySelector('.icon-delete-gray'), 'deleteErrors',
