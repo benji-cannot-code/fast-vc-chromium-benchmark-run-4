0d9c19f233a9ac1dc7a00d4a2eec8387e0761112
Jiewei Qian <qjw@chromium.org>
2020-05-01 02:15:14 +0000

    WebUI: add AllowlistProvider to auto-grant permissions to WebUI
    This CL adds a new WebUIAllowlistProvider to handle auto-granted
    permissions for WebUI.
    
    This removes the need to hard-code WebUI URLs and permissions in
    ContentSettingsRegistry, and fixes the layering violation (from
    components/ into chrome/).
    
    WebUIAllowlistProvider is implemented by a WebUIAllowlist, which stores
    the permission registry (e.g. origin and permissions to granted).
    
    This CL also renames SettingSource::SETTING_SOURCE_WHITELIST enum to
    SETTING_SOURCE_ALLOWLIST for consistency.
    
    Followup CLs will be made to deprecate force_allowed_origins, support
    additional schemes (if necessary), and deprecate whitelisted_schemes.
    
    Bug: 1054261
    Change-Id: Iff37e249911d3cc8a16258d01c35ba2572d781fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162426
    Commit-Queue: Jiewei Qian  <qjw@chromium.org>
    Reviewed-by: calamity <calamity@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764537}

--- chrome/browser/content_settings/host_content_settings_map_factory.cc
+++ chrome/browser/content_settings/host_content_settings_map_factory.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/off_the_record_profile_impl.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/ui/webui/webui_allowlist_provider.h"
 #include "chrome/common/buildflags.h"
 #include "components/content_settings/core/browser/content_settings_pref_provider.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
@@ -88,6 +89,12 @@ scoped_refptr<RefcountedKeyedService>
       /*store_last_modified=*/true,
       base::FeatureList::IsEnabled(
           permissions::features::kPermissionDelegation)));
+
+  auto allowlist_provider = std::make_unique<WebUIAllowlistProvider>(
+      WebUIAllowlist::GetOrCreate(profile));
+  settings_map->RegisterProvider(
+      HostContentSettingsMap::WEBUI_ALLOWLIST_PROVIDER,
+      std::move(allowlist_provider));
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   // These must be registered before before the HostSettings are passed over to
