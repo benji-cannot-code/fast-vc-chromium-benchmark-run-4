afd36c5686e3a2d1458d3c2cca90c6d022276d09
Steven Bennetts <stevenjb@chromium.org>
2020-05-01 22:20:24 +0000

    Network config: Support multiple Cellular GUIDs
    This also ensures that GUID is correctly populated in Shill
    properties for chrome://network debugging.
    
    BUG=b/154014577
    
    Change-Id: I15114e8222e16183d8522b2d8f14e9161d2abd28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174992
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764804}

--- chrome/browser/extensions/api/networking_private/networking_private_chromeos_apitest.cc
+++ chrome/browser/extensions/api/networking_private/networking_private_chromeos_apitest.cc
@@ -612,8 +612,6 @@ IN_PROC_BROWSER_TEST_F(NetworkingPrivateChromeOSApiTest,
           ->network_state_handler()
           ->FirstNetworkByType(chromeos::NetworkTypePattern::Cellular());
   ASSERT_TRUE(cellular);
-  std::string cellular_guid = std::string(kCellular1ServicePath) + "_guid";
-  EXPECT_EQ(cellular_guid, cellular->guid());
   // Remove the Cellular service. This should create a default Cellular network.
   service_test_->RemoveService(kCellular1ServicePath);
   content::RunAllPendingInMessageLoop();
@@ -621,7 +619,6 @@ IN_PROC_BROWSER_TEST_F(NetworkingPrivateChromeOSApiTest,
                  ->network_state_handler()
                  ->FirstNetworkByType(chromeos::NetworkTypePattern::Cellular());
   ASSERT_TRUE(cellular);
-  EXPECT_EQ(cellular_guid, cellular->guid());
   EXPECT_TRUE(RunNetworkingSubtest("getPropertiesCellularDefault")) << message_;
 }
 
