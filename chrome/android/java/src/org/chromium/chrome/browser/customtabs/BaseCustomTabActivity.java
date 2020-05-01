b1070e97af2b285edd5e2c9d855efa6e72ae4e44
Peter Kotwicz <pkotwicz@chromium.org>
2020-05-01 16:25:57 +0000

    [Android Refactor Webapp] Move webapp-finding methods to dedicated class
    This CL moves:
    - WebappActivity#findWebappActivityWithTabId()
    - WebappActivity#findRunningWebappActivityWithId()
    to a dedicated class WebappLocator.java
    
    BUG=1077142
    
    Change-Id: Ie2ff7e0f355f0ad0d175fedb11cc6689e5dd4652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172329
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764677}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/BaseCustomTabActivity.java
@@ -39,6 +39,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
 import org.chromium.chrome.browser.ui.RootUiCoordinator;
 import org.chromium.chrome.browser.webapps.SameTaskWebApkActivity;
 import org.chromium.chrome.browser.webapps.WebappActivityCoordinator;
+import org.chromium.components.embedder_support.delegate.WebContentsDelegateAndroid;
 
 /**
  * Contains functionality which is shared between {@link WebappActivity} and
@@ -48,6 +49,7 @@ import org.chromium.chrome.browser.webapps.WebappActivityCoordinator;
  */
 public abstract class BaseCustomTabActivity<C extends BaseCustomTabActivityComponent>
         extends ChromeActivity<C> {
+    protected CustomTabDelegateFactory mDelegateFactory;
     protected CustomTabToolbarCoordinator mToolbarCoordinator;
     protected CustomTabActivityNavigationController mNavigationController;
     protected CustomTabActivityTabProvider mTabProvider;
@@ -101,6 +103,7 @@ public abstract class BaseCustomTabActivity<C extends BaseCustomTabActivityCompo
      * Called when the {@link BaseCustomTabActivityComponent} was created.
      */
     protected void onComponentCreated(BaseCustomTabActivityComponent component) {
+        mDelegateFactory = component.resolveTabDelegateFactory();
         mToolbarCoordinator = component.resolveToolbarCoordinator();
         mNavigationController = component.resolveNavigationController();
         mTabProvider = component.resolveTabProvider();
@@ -310,5 +313,10 @@ public abstract class BaseCustomTabActivity<C extends BaseCustomTabActivityCompo
             return true;
         }
         return super.onMenuOrKeyboardAction(id, fromMenu);
+    }
+
+    public WebContentsDelegateAndroid getWebContentsDelegate() {
+        assert mDelegateFactory != null;
+        return mDelegateFactory.getWebContentsDelegate();
     }
 }
