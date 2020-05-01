e2e0e0fc7075e650f7f61a8cdf4b0fe2799c61d4
Maggie Cai <mxcai@chromium.org>
2020-05-01 05:06:09 +0000

    [IntentHandling] Fix flakiness for unit test.
    This CL changes all FlushMojoCallsForTesting back to RunUntilIdle() for
    AppServiceImplTest.PreferredApps. Hope this can solve the flakiness.
    
    BUG=1074596
    
    Change-Id: Id097cf282645d6bafe717a2e0a6880ed3e52a158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174028
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764571}

--- chrome/services/app_service/app_service_impl_unittest.cc
+++ chrome/services/app_service/app_service_impl_unittest.cc
@@ -281,19 +281,19 @@ TEST_F(AppServiceImplTest, PreferredApps) {
 
   // Add one subscriber.
   FakeSubscriber sub0(&impl);
-  impl.FlushMojoCallsForTesting();
+  base::RunLoop().RunUntilIdle();
   EXPECT_EQ(sub0.PreferredApps().GetValue(),
             impl.GetPreferredAppsForTesting().GetValue());
 
   // Add another subscriber.
   FakeSubscriber sub1(&impl);
-  impl.FlushMojoCallsForTesting();
+  base::RunLoop().RunUntilIdle();
   EXPECT_EQ(sub1.PreferredApps().GetValue(),
             impl.GetPreferredAppsForTesting().GetValue());
 
   FakePublisher pub0(&impl, apps::mojom::AppType::kArc,
                      std::vector<std::string>{kAppId1, kAppId2});
-  impl.FlushMojoCallsForTesting();
+  base::RunLoop().RunUntilIdle();
 
   // Test sync preferred app to all subscribers.
   filter_url = GURL("https://www.abc.com/");
@@ -319,7 +319,6 @@ TEST_F(AppServiceImplTest, PreferredApps) {
                        another_intent_filter->Clone(),
                        apps_util::CreateIntentFromUrl(another_filter_url),
                        /*from_publisher=*/true);
-  impl.FlushMojoCallsForTesting();
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(kAppId2, sub0.PreferredApps().FindPreferredAppForUrl(filter_url));
   EXPECT_EQ(kAppId2, sub1.PreferredApps().FindPreferredAppForUrl(filter_url));
@@ -330,7 +329,7 @@ TEST_F(AppServiceImplTest, PreferredApps) {
 
   // Test that uninstall removes all the settings for the app.
   pub0.UninstallApps(std::vector<std::string>{kAppId2}, &impl);
-  impl.FlushMojoCallsForTesting();
+  base::RunLoop().RunUntilIdle();
   EXPECT_EQ(base::nullopt,
             sub0.PreferredApps().FindPreferredAppForUrl(filter_url));
   EXPECT_EQ(base::nullopt,
@@ -347,7 +346,7 @@ TEST_F(AppServiceImplTest, PreferredApps) {
                        another_intent_filter->Clone(),
                        apps_util::CreateIntentFromUrl(another_filter_url),
                        /*from_publisher=*/true);
-  impl.FlushMojoCallsForTesting();
+  base::RunLoop().RunUntilIdle();
 
   EXPECT_EQ(kAppId2, sub0.PreferredApps().FindPreferredAppForUrl(filter_url));
   EXPECT_EQ(kAppId2, sub1.PreferredApps().FindPreferredAppForUrl(filter_url));
@@ -359,7 +358,7 @@ TEST_F(AppServiceImplTest, PreferredApps) {
   // Test that remove setting for one filter.
   impl.RemovePreferredAppForFilter(apps::mojom::AppType::kUnknown, kAppId2,
                                    intent_filter->Clone());
-  impl.FlushMojoCallsForTesting();
+  base::RunLoop().RunUntilIdle();
   EXPECT_EQ(base::nullopt,
             sub0.PreferredApps().FindPreferredAppForUrl(filter_url));
   EXPECT_EQ(base::nullopt,
