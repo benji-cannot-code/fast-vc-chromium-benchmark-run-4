b5c6ffb0cd9e729b92e7b8239adcfd2f7d262861
Livvie Lin <livvielin@chromium.org>
2020-05-01 18:20:53 +0000

    Reland "[iOS] Trigger a generic error page on lookalikes of top sites"
    This is a reland of 66a6a9d911658ac13fd7725c1ca0f8b565dd4b42
    
    Original change's description:
    > [iOS] Trigger a generic error page on lookalikes of top sites
    >
    > This currently triggers a net error since this is required for
    > GetErrorPage, but will later be updated to pass a custom NSError.
    >
    > Follow-up CLs will add the actual blocking page UI and incorporate
    > site engagement once it's been turned into a component.
    >
    > Bug: 1058898
    > Change-Id: I36af8be93a8a42a4127ff5365d26c2a1aea5087a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166356
    > Commit-Queue: Livvie Lin <livvielin@chromium.org>
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#764518}
    
    Bug: 1058898
    Change-Id: I4f1a8c5386e1fa1f2147ebfadd1b60fb9cce7499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175576
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764713}

---
+++ ios/components/security_interstitials/lookalikes/lookalike_url_tab_allow_list.mm
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/components/security_interstitials/lookalikes/lookalike_url_tab_allow_list.h"
+
+#import "ios/web/public/web_state.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+WEB_STATE_USER_DATA_KEY_IMPL(LookalikeUrlTabAllowList)
+
+LookalikeUrlTabAllowList::LookalikeUrlTabAllowList(web::WebState* web_state) {}
+
+LookalikeUrlTabAllowList::LookalikeUrlTabAllowList(
+    LookalikeUrlTabAllowList&& other) = default;
+
+LookalikeUrlTabAllowList& LookalikeUrlTabAllowList::operator=(
+    LookalikeUrlTabAllowList&& other) = default;
+
+LookalikeUrlTabAllowList::~LookalikeUrlTabAllowList() = default;
+
+bool LookalikeUrlTabAllowList::IsDomainAllowed(const std::string& domain) {
+  return allowed_domains_.find(domain) != allowed_domains_.end();
+}
+
+void LookalikeUrlTabAllowList::AllowDomain(const std::string& domain) {
+  allowed_domains_.insert(domain);
+}
