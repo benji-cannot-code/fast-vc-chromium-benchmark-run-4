8b0bd0c5a04e30287bdb9b927110f1da3eb214d4
Ali Juma <ajuma@chromium.org>
2020-05-01 20:22:13 +0000

    [iOS] Use result of main-frame Safe Browsing queries to cancel navigations
    This CL makes SafeBrowsingTabHelper keep track of pending
    main-frame queries issued during ShouldAllowRequest(),
    and uses the results of these queries when producing the
    decision for ShouldAllowResponse(). If the queried URL is
    determined to be unsafe, the navigation is not allowed.
    In the event that a Safe Browsing query has not completed
    in time for ShouldAllowResponse(), the callback for this
    method isn't invoked until a response to the query has
    been received.
    
    Bug: 1049340
    Change-Id: Ib58246846bac37306247466aaf5049fd6ac21540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173407
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764766}

--- ios/chrome/browser/safe_browsing/fake_safe_browsing_service.mm
+++ ios/chrome/browser/safe_browsing/fake_safe_browsing_service.mm
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-// A SafeBrowsingUrlCheckerImpl that treats all URLs as safe.
+// A SafeBrowsingUrlCheckerImpl that treats all URLs as safe, unless they have
+// host safe.browsing.unsafe.chromium.test.
 class FakeSafeBrowsingUrlCheckerImpl
     : public safe_browsing::SafeBrowsingUrlCheckerImpl {
  public:
@@ -36,11 +37,21 @@ class FakeSafeBrowsingUrlCheckerImpl
       const std::string& method,
       safe_browsing::SafeBrowsingUrlCheckerImpl::NativeCheckUrlCallback
           callback) override {
+    if (url.host() == FakeSafeBrowsingService::kUnsafeHost) {
+      std::move(callback).Run(/*slow_check_notifier=*/nullptr,
+                              /*proceed=*/false,
+                              /*showed_interstitial=*/true);
+      return;
+    }
     std::move(callback).Run(/*slow_check_notifier=*/nullptr, /*proceed=*/true,
                             /*showed_interstitial=*/false);
   }
 };
 }
+
+// static
+const std::string FakeSafeBrowsingService::kUnsafeHost =
+    "safe.browsing.unsafe.chromium.test";
 
 FakeSafeBrowsingService::FakeSafeBrowsingService() = default;
 
