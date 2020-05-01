ba0bc64767ab886026f896fa00c2e21ef58b7fbc
Julian Watson <juwa@google.com>
2020-05-01 07:18:40 +0000

    plugin_vm: alert user if files cant be opened
    Bug: 1049453
    Change-Id: I70d8bb9d692808533aed38c047b2437690d76e9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172263
    Commit-Queue: Julian Watson <juwa@google.com>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764593}

--- chrome/browser/chromeos/file_manager/guest_os_file_tasks.h
+++ chrome/browser/chromeos/file_manager/guest_os_file_tasks.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "chrome/browser/chromeos/file_manager/file_tasks.h"
+#include "chrome/browser/chromeos/guest_os/guest_os_registry_service.h"
 
 class Profile;
 
@@ -30,10 +31,12 @@ constexpr char kGuestOsAppActionID[] = "open-with";
 
 // Finds the Guest OS |app_ids| and |app_names| that can handle |entries|.
 // VisibleForTesting.  Called by |FindGuestOsTasks|.
-void FindGuestOsApps(Profile* profile,
-                     const std::vector<extensions::EntryInfo>& entries,
-                     std::vector<std::string>* app_ids,
-                     std::vector<std::string>* app_names);
+void FindGuestOsApps(
+    Profile* profile,
+    const std::vector<extensions::EntryInfo>& entries,
+    std::vector<std::string>* app_ids,
+    std::vector<std::string>* app_names,
+    std::vector<guest_os::GuestOsRegistryService::VmType>* vm_types);
 
 // Finds the Guest OS tasks that can handle |entries|, appends them to
 // |result_list|, and calls back to |callback| once finished.
