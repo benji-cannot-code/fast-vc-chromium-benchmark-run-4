94544a83423f5976d185c50fb5ce8a326f039f69
Maks Orlovich <morlovich@chromium.org>
2020-05-02 20:08:21 +0000

    Client hints: http-equiv accept-ch should not persist
    Change-Id: I53e21a16d5e83590b99f6efb6d165d6a67336092
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172586
    Commit-Queue: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764943}

--- third_party/blink/renderer/core/loader/http_equiv.h
+++ third_party/blink/renderer/core/loader/http_equiv.h
@@ -46,8 +46,6 @@ class HttpEquiv {
       const AtomicString& equiv,
       const AtomicString& content);
   static void ProcessHttpEquivAcceptCH(Document&, const AtomicString& content);
-  static void ProcessHttpEquivAcceptCHLifetime(Document&,
-                                               const AtomicString& content);
 };
 
 }  // namespace blink
