92ba86e338e6b08e30d1dbb8ba07f4d6345fb96e
Tanmoy Mollik <triploblastic@chromium.org>
2020-05-02 12:51:37 +0000

    [Android] Disable flaky test in ManageSyncSettingsTest
    Disable testAdvancedSyncFlowBottomView and testAdvancedSyncFlowTopView
    in ManageSyncSettingsTest
    
    TBR=bsazonov@chromium.org
    
    Bug: 1066541
    Change-Id: I16f513dfc8a89db97c8d4bfb0c6a5d8aababc908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174487
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764923}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
@@ -469,6 +469,7 @@ public class ManageSyncSettingsTest {
     @LargeTest
     @Feature({"Sync", "RenderTest"})
     @Features.EnableFeatures(ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY)
+    @FlakyTest(message = "https://crbug.com/1066541")
     public void testAdvancedSyncFlowTopView() throws Exception {
         mSyncTestRule.setUpTestAccountAndSignIn();
         SyncTestUtil.waitForSyncActive();
@@ -481,6 +482,7 @@ public class ManageSyncSettingsTest {
     @LargeTest
     @Feature({"Sync", "RenderTest"})
     @Features.EnableFeatures(ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY)
+    @FlakyTest(message = "https://crbug.com/1066541")
     public void testAdvancedSyncFlowBottomView() throws Exception {
         mSyncTestRule.setUpTestAccountAndSignIn();
         SyncTestUtil.waitForSyncActive();
