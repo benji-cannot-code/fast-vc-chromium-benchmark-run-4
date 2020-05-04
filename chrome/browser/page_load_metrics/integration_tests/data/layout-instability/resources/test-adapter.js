abeaabe12ba0b90d8b57fba880f55348f7037b6e
Steve Kobes <skobes@chromium.org>
2020-05-04 18:02:42 +0000

    Convert a CLS end to end test to wrap a WPT.
    This unifies MetricIntegrationTest.LayoutInstability with the nearly
    identical simple-block-movement.html, and introduces an abstraction for
    communicating expectations from the WPT to the wrapper, so that they can
    be verified in a generic way against the trace data and UMA/UKM results.
    
    We'll use this technique for CLSAttribution_* in a follow-up.
    
    Bug: 1045064
    Change-Id: I401af5cd0186ad3de930130694ff2fcf7b43ed7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175636
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765156}

---
+++ chrome/browser/page_load_metrics/integration_tests/data/layout-instability/resources/test-adapter.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+let cls_expectations = [];
+
+cls_expect = (watcher, expectation) => {
+  assert_equals(watcher.score, expectation.score);
+  if (expectation.score > 0)
+    cls_expectations.push(expectation);
+};
+
+cls_run_tests = new Promise((resolve, reject) => {
+  add_completion_callback((tests, harness_status) => {
+    if (harness_status.status != 0) {
+      reject(harness_status.message);
+      return;
+    }
+    for (let test of tests) {
+      if (test.status != 0 /* PASS */) {
+        reject(test.message);
+        return;
+      }
+    }
+    resolve(cls_expectations);
+  });
+});
---
+++ third_party/blink/web_tests/external/wpt/layout-instability/resources/test-adapter.js
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Abstracts expectations for reuse in different test frameworks.
+
+cls_expect = (watcher, expectation) => {
+  assert_equals(watcher.score, expectation.score);
+};
