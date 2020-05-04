eca5a1ff9f47824af9637ad7a776d84075b2f4d5
Robert Phillips <robertphillips@google.com>
2020-05-04 13:37:42 +0000

    Skia now supports BGR10_A2 - allow it to be tested
    The Skia-side CL https://skia-review.googlesource.com/c/skia/+/286036 (Add BGR_10A2 support to Ganesh (take 2)) added BGR10_A2 support. It has rolled into Chrome and appears to have stuck.
    
    Bug: 1068416
    Change-Id: I5041d0dd711d189ca17e7501e376add560bd0260
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174733
    Commit-Queue: Jonathan Backer <backer@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765046}

--- gpu/command_buffer/service/shared_image_backing_factory_gl_texture_unittest.cc
+++ gpu/command_buffer/service/shared_image_backing_factory_gl_texture_unittest.cc
@@ -275,10 +275,6 @@ TEST_P(SharedImageBackingFactoryGLTextureTest, Basic) {
     gl_representation.reset();
   }
 
-  // Skia does not support BGRA_1010102 as render target, only RGBA_1010102.
-  if (format == viz::ResourceFormat::BGRA_1010102)
-    return;
-
   // Finally, validate a SharedImageRepresentationSkia.
   auto skia_representation = shared_image_representation_factory_->ProduceSkia(
       mailbox, context_state_.get());
@@ -392,10 +388,6 @@ TEST_P(SharedImageBackingFactoryGLTextureTest, Image) {
     EXPECT_EQ(usage, gl_representation->usage());
     gl_representation.reset();
   }
-
-  // Skia does not support BGRA_1010102 as render target, only RGBA_1010102.
-  if (format == viz::ResourceFormat::BGRA_1010102)
-    return;
 
   // Finally, validate a SharedImageRepresentationSkia.
   auto skia_representation = shared_image_representation_factory_->ProduceSkia(
