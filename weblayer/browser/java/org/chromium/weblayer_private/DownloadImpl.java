a726b94015a8be7280f3a4761dadf46f0bb37159
Clark DuVall <cduvall@chromium.org>
2020-05-04 23:21:03 +0000

    [WebLayer] Fix crash when other shared resource library is loaded
    This changes the hardcoded package ID from 0x02 to 0x05, which allows
    for other shared libraries to be loaded before WebLayer.
    
    This change calls addAssetPath on the AssetManager to increment the
    package ID until we hit our magic 0x05. Each call needs to use a
    different path and the path has to be a valid asset path, so we prepend
    a string of "/." to the WebView package's source dir.
    
    This also fixes crbug.com/1075704 by hardcoding the package IDs of the
    locale resources to 0x05, which will load them at the same ID the main
    APK will be loaded. This also prevents each split from taking a new
    dynamic ID.
    
    Bug: 1075704, 1075789
    Change-Id: I671d0ad64a3025409a62ecebbdbc584819d68ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174964
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765318}

--- weblayer/browser/java/org/chromium/weblayer_private/DownloadImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/DownloadImpl.java
@@ -20,7 +20,6 @@ import org.chromium.base.Log;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
-import org.chromium.components.browser_ui.notifications.NotificationBuilder;
 import org.chromium.components.browser_ui.notifications.NotificationManagerProxy;
 import org.chromium.components.browser_ui.notifications.NotificationManagerProxyImpl;
 import org.chromium.components.browser_ui.notifications.NotificationMetadata;
@@ -346,8 +345,8 @@ public final class DownloadImpl extends IDownload.Stub {
                 ? WebLayerNotificationChannels.ChannelId.COMPLETED_DOWNLOADS
                 : WebLayerNotificationChannels.ChannelId.ACTIVE_DOWNLOADS;
 
-        NotificationBuilder builder =
-                new NotificationBuilder(context, channelId, channelsInitializer,
+        WebLayerNotificationBuilder builder =
+                new WebLayerNotificationBuilder(context, channelId, channelsInitializer,
                         new NotificationMetadata(0, NOTIFICATION_TAG, mNotificationId));
         builder.setOngoing(true)
                 .setDeleteIntent(deletePendingIntent)
