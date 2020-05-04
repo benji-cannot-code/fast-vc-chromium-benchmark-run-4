5b46aa086101382473e2236562aec1eba0d8497a
Elly Fong-Jones <ellyjones@chromium.org>
2020-05-04 21:39:51 +0000

    views: remove WidgetDelegate::ShouldCenterWindowTitleText
    This change removes the only override of it, then makes it non-virtual.
    
    Bug: 1075649
    Change-Id: I2e3f31d686e58d2887e5ed7177b58414f125a77b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175744
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765269}

--- ui/views/widget/widget_delegate.h
+++ ui/views/widget/widget_delegate.h
@@ -118,9 +118,6 @@ class VIEWS_EXPORT WidgetDelegate {
   // Returns true if the window should show a title in the title bar.
   virtual bool ShouldShowWindowTitle() const;
 
-  // Returns true if the title text should be centered. Default is false.
-  virtual bool ShouldCenterWindowTitleText() const;
-
   // Returns true if the window should show a close button in the title bar.
   virtual bool ShouldShowCloseButton() const;
 
@@ -251,6 +248,9 @@ class VIEWS_EXPORT WidgetDelegate {
   // Call this to notify the WidgetDelegate that its Widget is about to start
   // closing.
   void WindowWillClose();
+
+  // Returns true if the title text should be centered.
+  bool ShouldCenterWindowTitleText() const;
 
  protected:
   virtual ~WidgetDelegate();
