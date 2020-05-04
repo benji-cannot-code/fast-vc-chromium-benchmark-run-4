e81ebcd784334d33f394a5ee59c1bdec2c3f276b
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-05-04 14:49:59 +0000

    [iOS][Credential-Provider] ASPasswordCredentialIdentity from Credentials
    Create and test conveninece initializer from Credentials.
    
    Bug: 1077747
    Change-Id: Ie04ca8de21bc44df8b9c2818cd6ecc6b63bbd9b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2179504
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765059}

---
+++ ios/chrome/common/credential_provider/as_password_credential_identity+credential.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/common/credential_provider/as_password_credential_identity+credential.h"
+
+#import "ios/chrome/common/credential_provider/credential.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation ASPasswordCredentialIdentity (Credential)
+
+- (instancetype)initWithCredential:(id<Credential>)credential {
+  ASCredentialServiceIdentifier* serviceIdentifier =
+      [[ASCredentialServiceIdentifier alloc]
+          initWithIdentifier:credential.serviceIdentifier
+                        type:ASCredentialServiceIdentifierTypeURL];
+  return [self initWithServiceIdentifier:serviceIdentifier
+                                    user:credential.user
+                        recordIdentifier:credential.recordIdentifier];
+}
+
+@end
