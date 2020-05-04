cabbd70736cf7d963da26ab189005126ccb6906c
Dave Tapuska <dtapuska@chromium.org>
2020-05-04 20:44:03 +0000

    Turn DidOverscrollParams into aggregate class.
    Removing the empty ctor/dtor allows us to braces to initialize the
    class.
    
    BUG=1073560
    
    Change-Id: I9a6941c0910c71524923674e33abf41cedbbddbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176418
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765243}

--- ui/events/blink/did_overscroll_params.h
+++ ui/events/blink/did_overscroll_params.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 struct DidOverscrollParams {
-  DidOverscrollParams();
-  ~DidOverscrollParams();
   gfx::Vector2dF accumulated_overscroll;
   gfx::Vector2dF latest_overscroll_delta;
   gfx::Vector2dF current_fling_velocity;
