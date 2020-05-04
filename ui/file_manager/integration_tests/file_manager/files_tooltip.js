97cec7211e34d4357518f1b15c64f6b30e2a2f39
Omid Tourzan <oto@chromium.org>
2020-05-04 02:43:45 +0000

    [tooltip] Fix readonly bubble tooltip width and wrap rules.
    Set max-width as advised by UX. Update readonly bubble tooltip text to
    remove <br> tag. Use CSS max-width to control the tooltip width.
    
    Bug: 1058549
    Change-Id: Ia2db406b65a5e7a84d181f46a9050968d96d276c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175940
    Commit-Queue: Omid Tourzan <oto@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764979}

--- ui/file_manager/integration_tests/file_manager/files_tooltip.js
+++ ui/file_manager/integration_tests/file_manager/files_tooltip.js
@@ -107,7 +107,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         await remoteCall.waitForElement(appId, [tooltipQueryVisible, '#label']);
     chrome.test.assertEq(
         'Files in this folders are read only.' +
-            'Some activities are not supported.',
+            ' Some activities are not supported.',
         label.text);
     chrome.test.assertEq('card-tooltip', tooltip.attributes.class);
     chrome.test.assertEq('card-label', label.attributes.class);
