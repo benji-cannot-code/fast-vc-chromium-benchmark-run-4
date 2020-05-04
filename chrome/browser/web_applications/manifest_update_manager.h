5d863a214e3ad7899eb8a1da9112d092d5098231
Alan Cutter <alancutter@chromium.org>
2020-05-04 07:04:47 +0000

    desktop-pwas: Retire DesktopPWAsLocalUpdatingThrottlePersistence flag
    This CL removes the DesktopPWAsLocalUpdatingThrottlePersistence flag as
    it has expired and no longer needed.
    This flag governed whether we persisted the "last update check time" to
    prefs so that it survived a browser restart. This turned out to not be
    necessary and only made checking for PWA update bugs harder so it was
    replaced with an in memory equivalent.
    
    Bug: 926083
    Change-Id: Id5b6190a49425f1305d0fc6d98ad6edf8a1d4413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2178473
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765000}

--- chrome/browser/web_applications/manifest_update_manager.h
+++ chrome/browser/web_applications/manifest_update_manager.h
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/components/web_app_id.h"
 #include "chrome/browser/web_applications/manifest_update_task.h"
 
-class Profile;
-
 namespace content {
 class WebContents;
 }
@@ -43,7 +41,7 @@ class SystemWebAppManager;
 // of being triggered by page loads.
 class ManifestUpdateManager final : public AppRegistrarObserver {
  public:
-  explicit ManifestUpdateManager(Profile* profile);
+  ManifestUpdateManager();
   ~ManifestUpdateManager() override;
 
   void SetSubsystems(AppRegistrar* registrar,
@@ -84,7 +82,6 @@ class ManifestUpdateManager final : public AppRegistrarObserver {
                        ManifestUpdateResult result);
   void NotifyResult(const GURL& url, ManifestUpdateResult result);
 
-  Profile* const profile_ = nullptr;
   AppRegistrar* registrar_ = nullptr;
   AppIconManager* icon_manager_ = nullptr;
   WebAppUiManager* ui_manager_ = nullptr;
