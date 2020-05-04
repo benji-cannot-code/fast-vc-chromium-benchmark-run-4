e06f7359fe2e2e01533acb59cc0c7da0cb4df8a3
Elly Fong-Jones <ellyjones@chromium.org>
2020-05-04 23:31:48 +0000

    views: don't override ShouldShowCloseButton in BubbleDialogDelegateView
    This override prevents any subclass of BubbleDialogDelegateView from
    using WidgetDelegate::SetShowCloseButton(), which is annoying. Replacing
    it with a use of the new setter lets subclasses use either approach.
    
    Bug: 1077366
    Change-Id: Idf08cfd6fc5b359f1c97340d1e736142e2f650b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2179929
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765323}

--- ui/views/bubble/bubble_dialog_delegate_view.cc
+++ ui/views/bubble/bubble_dialog_delegate_view.cc
@@ -191,6 +191,8 @@ BubbleDialogDelegateView::BubbleDialogDelegateView(View* anchor_view,
                                                    BubbleBorder::Arrow arrow,
                                                    BubbleBorder::Shadow shadow)
     : shadow_(shadow) {
+  WidgetDelegate::SetShowCloseButton(false);
+
   SetArrow(arrow);
   LayoutProvider* provider = LayoutProvider::Get();
   // An individual bubble should override these margins if its layout differs
@@ -210,10 +212,6 @@ BubbleDialogDelegateView::~BubbleDialogDelegateView() {
 
 BubbleDialogDelegateView* BubbleDialogDelegateView::AsBubbleDialogDelegate() {
   return this;
-}
-
-bool BubbleDialogDelegateView::ShouldShowCloseButton() const {
-  return false;
 }
 
 NonClientFrameView* BubbleDialogDelegateView::CreateNonClientFrameView(
