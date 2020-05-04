d8abe1290b16469a098a878dcd655196b96386e6
Natalie Chouinard <chouinard@chromium.org>
2020-05-04 21:15:20 +0000

    Revert "[Android] Disable flaky test in ManageSyncSettingsTest"
    This reverts commit e74c73d0000f81b3f40a513176c8d024bba57d28.
    
    Reason for revert: Re-enable tests, suspecting failures on CQ were valid.
    
    Original change's description:
    > [Android] Disable flaky test in ManageSyncSettingsTest
    >
    > Disable testAdvancedSyncFlowBottomView and testAdvancedSyncFlowTopView
    > in ManageSyncSettingsTest
    >
    > TBR=bsazonov@chromium.org
    >
    > Bug: 1066541
    > Change-Id: I16f513dfc8a89db97c8d4bfb0c6a5d8aababc908
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174487
    > Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    > Commit-Queue: Tanmoy Mollik <triploblastic@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#764923}
    
    TBR=bsazonov@chromium.org,triploblastic@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1066541
    Change-Id: I64284cae86be4083c04638bd139accd7dbb56a0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2180910
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765257}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
@@ -469,7 +469,6 @@ public class ManageSyncSettingsTest {
     @LargeTest
     @Feature({"Sync", "RenderTest"})
     @Features.EnableFeatures(ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY)
-    @FlakyTest(message = "https://crbug.com/1066541")
     public void testAdvancedSyncFlowTopView() throws Exception {
         mSyncTestRule.setUpTestAccountAndSignIn();
         SyncTestUtil.waitForSyncActive();
@@ -482,7 +481,6 @@ public class ManageSyncSettingsTest {
     @LargeTest
     @Feature({"Sync", "RenderTest"})
     @Features.EnableFeatures(ChromeFeatureList.MOBILE_IDENTITY_CONSISTENCY)
-    @FlakyTest(message = "https://crbug.com/1066541")
     public void testAdvancedSyncFlowBottomView() throws Exception {
         mSyncTestRule.setUpTestAccountAndSignIn();
         SyncTestUtil.waitForSyncActive();
