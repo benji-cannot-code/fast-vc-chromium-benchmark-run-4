6a7cd3a2a5e8c23aed118a1854357d8234089910
Olesia Marukhno <olesiamarukhno@google.com>
2020-05-04 09:34:00 +0000

    Add a feature flag for permissions chip
    Bug: 1019129
    Change-Id: I958bcfe117c55df088f7b1c051fed0ba794f6751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165743
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765016}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -944,6 +944,9 @@ extern const char kPercentBasedScrollingDescription[];
 extern const char kPerMethodCanMakePaymentQuotaName[];
 extern const char kPerMethodCanMakePaymentQuotaDescription[];
 
+extern const char kPermissionChipName[];
+extern const char kPermissionChipDescription[];
+
 extern const char kPointerLockOptionsName[];
 extern const char kPointerLockOptionsDescription[];
 
