892b1446d1b595a709a8eef6784410a31d186280
Jeevan Shikaram <jshikaram@chromium.org>
2020-05-04 22:31:08 +0000

    [PWA-in-Play] Move digital_asset_links folder to chrome/browser/installable.
    This CL moves the digital_asset_links folder from chrome/browser/android
    to chrome/browser/installable as it will now be used on Chrome OS as well.
    
    Bug: 1026469
    Change-Id: I31941fa2dc4cf97e33d27d6784a1571ffc9300e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162402
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Jeevan Shikaram <jshikaram@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765300}

--- chrome/browser/android/installedapp/installed_app_verifier.cc
+++ chrome/browser/android/installedapp/installed_app_verifier.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "chrome/android/chrome_jni_headers/InstalledAppProviderImpl_jni.h"
-#include "chrome/browser/android/digital_asset_links/digital_asset_links_handler.h"
+#include "chrome/browser/installable/digital_asset_links/digital_asset_links_handler.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_android.h"
 #include "content/public/browser/browser_context.h"
