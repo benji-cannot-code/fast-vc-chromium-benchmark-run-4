81d9e1d6dc9505838cbd5652b805af5a5b77b986
Anastasia Helfinstein <anastasi@google.com>
2020-05-04 22:58:57 +0000

    Add ArrayUtil class for accessibility component extensions
    Currently has only one method, contentsAreEqual, which compares arrays
    based on whether they have equal elements in the same order. This is not
    currently used, but will be used by Switch Access in a follow-up change.
    
    AX-Relnotes: n/a.
    Bug: None
    Change-Id: Ib80a33ab4ac400d07794c5a1355b460f6b070a77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176673
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765308}

---
+++ chrome/browser/resources/chromeos/accessibility/common/array_util.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** A collection of helper functions when dealing with arrays. */
+const ArrayUtil = {
+  /**
+   * @param {Array=} array1
+   * @param {Array=} array2
+   * @return {boolean}
+   */
+  contentsAreEqual: (array1, array2) => {
+    if (!array1 || !array2 || array1.length !== array2.length) {
+      return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  },
+};
