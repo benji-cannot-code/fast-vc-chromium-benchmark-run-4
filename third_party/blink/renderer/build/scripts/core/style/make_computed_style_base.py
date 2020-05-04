b8f4917bf24956fd8abef5a12a9658b5080bbca6
Dominik Röttsches <drott@chromium.org>
2020-05-04 15:16:13 +0000

    Parse text-decoration-thickness
    Implement parsing support for text-decoration-thickness, which allows us
    to pass text-decoration-thickness-(computed|valid|invalid) as well as
    the text-decoration-thickness.html CSSOM tests, which test for the CSS
    availability of the property, not full drawing support yet.
    
    Bug: 785230
    Change-Id: I0a3535e474131f4997ba3b6fd4d7be8dc802f523
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2178888
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765066}

--- third_party/blink/renderer/build/scripts/core/style/make_computed_style_base.py
+++ third_party/blink/renderer/build/scripts/core/style/make_computed_style_base.py
@@ -51,6 +51,7 @@ ALIGNMENT_ORDER = [
     'FillLayer',
     'NinePieceImage',
     'IntrinsicLength',
+    'TextDecorationThickness',
     # Aligns like float
     'StyleOffsetRotation',
     'TransformOrigin',
