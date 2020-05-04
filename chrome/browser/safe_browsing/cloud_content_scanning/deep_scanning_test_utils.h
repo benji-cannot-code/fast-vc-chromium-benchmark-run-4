035fb93b32b11326717c8f1c4eb5df54d2e50803
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-05-04 18:41:38 +0000

    Reland "Add helper functions to set Connectors prefs"
    Reland of crrev.com/c/2173341. Differences are:
    - Pointers initialized to nullptr
    - Added "ForConnectors" suffix to avoid future name conflicts with
      existing code.
    
    Bug: 1076083
    Change-Id: I820dfa71779d22125bb967799abcb4c2738ae739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173618
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765189}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/optional.h"
+#include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/safe_browsing/core/proto/webprotect.pb.h"
 
 namespace base {
@@ -105,6 +106,27 @@ class EventReportValidator {
 
   base::RepeatingClosure done_closure_;
 };
+
+// Helper functions that set matching connector policies values from legacy
+// policy values.
+void SetDlpPolicyForConnectors(CheckContentComplianceValues state);
+void SetMalwarePolicyForConnectors(SendFilesForMalwareCheckValues state);
+void SetDelayDeliveryUntilVerdictPolicyForConnectors(
+    DelayDeliveryUntilVerdictValues state);
+void SetAllowPasswordProtectedFilesPolicyForConnectors(
+    AllowPasswordProtectedFilesValues state);
+void SetBlockUnsupportedFileTypesPolicyForConnectors(
+    BlockUnsupportedFiletypesValues state);
+void SetBlockLargeFileTransferPolicyForConnectors(
+    BlockLargeFileTransferValues state);
+void AddUrlsToCheckComplianceOfDownloadsForConnectors(
+    const std::vector<std::string>& urls);
+void AddUrlsToNotCheckComplianceOfUploadsForConnectors(
+    const std::vector<std::string>& urls);
+void AddUrlsToCheckForMalwareOfUploadsForConnectors(
+    const std::vector<std::string>& urls);
+void AddUrlsToNotCheckForMalwareOfDownloadsForConnectors(
+    const std::vector<std::string>& urls);
 
 }  // namespace safe_browsing
 
