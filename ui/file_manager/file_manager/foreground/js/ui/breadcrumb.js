47a20bb4e487afd311a0f06a12fdaacd91c777fa
Alex Danilo <adanilo@chromium.org>
2020-05-04 06:55:20 +0000

    [menus] Add files-ng ripple to elider drop down
    Adds a child <paper-ripple> element to the buttons in the elider drop
    down menu.
    
    Sets 'position: relative' on the buttons to contain the ripple to the
    bounds of the button.
    
    Enables the ripple to only be visible when '.pointer-active' is set on
    the parent document root.
    
    Tbr: noel
    Bug: 1002396
    Change-Id: I91eed9cfbb0687e4270fd40dc747bde0c15247a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2178331
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#764998}

--- ui/file_manager/file_manager/foreground/js/ui/breadcrumb.js
+++ ui/file_manager/file_manager/foreground/js/ui/breadcrumb.js
@@ -103,6 +103,14 @@ const breadCrumbTemplate = `
       width: 36px;
     }
 
+    button.dropdown-item {
+      position: relative;
+    }
+
+    :host-context(:root:not(.pointer-active)) button.dropdown-item > paper-ripple {
+      display: none;
+    }
+
     button:not([disabled]):not(:active):hover {
       background-color: rgba(0, 0, 0, 4%);
     }
@@ -293,7 +301,8 @@ class BreadCrumb extends HTMLElement {
 
     let elidedParts = '';
     for (let i = 1; i < parts.length - 2; ++i) {
-      elidedParts += `<button class='dropdown-item'>${parts[i]}</button>`;
+      elidedParts += `<button class='dropdown-item'>${
+          parts[i]}<paper-ripple></paper-ripple></button>`;
     }
 
     const menu = this.shadowRoot.querySelector('cr-action-menu');
