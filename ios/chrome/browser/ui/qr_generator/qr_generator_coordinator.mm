49d2b66a5c48be098071268ffd8307eb83bb0486
Sebastien Lalancette <seblalancette@chromium.org>
2020-05-04 20:50:47 +0000

    [iOS] Hooked-up QR Generator Share Button To Share Image
    Tapping on the button will now initiate the "ShareImage" flow of
    an ActivityServiceCoordinator using the generated QR code image.
    
    Bug: 1064990
    Change-Id: Ib82e63bfc0bcd017028050fe6519b4ec1a27519f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174793
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765246}

--- ios/chrome/browser/ui/qr_generator/qr_generator_coordinator.mm
+++ ios/chrome/browser/ui/qr_generator/qr_generator_coordinator.mm
@@ -6,18 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/qr_generator/qr_generator_coordinator.h"
 
+#include "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/main/browser.h"
+#import "ios/chrome/browser/ui/activity_services/activity_service_coordinator.h"
+#import "ios/chrome/browser/ui/activity_services/requirements/activity_service_positioner.h"
+#import "ios/chrome/browser/ui/activity_services/requirements/activity_service_presentation.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
 #import "ios/chrome/browser/ui/commands/qr_generation_commands.h"
 #import "ios/chrome/browser/ui/qr_generator/qr_generator_view_controller.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
+#include "ios/chrome/grit/ios_strings.h"
 #import "net/base/mac/url_conversions.h"
+#include "ui/base/l10n/l10n_util_mac.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-@interface QRGeneratorCoordinator () <ConfirmationAlertActionHandler> {
+@interface QRGeneratorCoordinator () <ActivityServicePositioner,
+                                      ActivityServicePresentation,
+                                      ConfirmationAlertActionHandler> {
   // URL of a page to generate a QR code for.
   GURL _URL;
 }
@@ -27,6 +35,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // View controller used to display the QR code and actions.
 @property(nonatomic, strong) QRGeneratorViewController* viewController;
+
+// Coordinator for the activity view brought up when the user wants to share
+// the QR code.
+@property(nonatomic, strong)
+    ActivityServiceCoordinator* activityServiceCoordinator;
 
 // Title of a page to generate a QR code for.
 @property(nonatomic, copy) NSString* title;
@@ -70,6 +83,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.viewController dismissViewControllerAnimated:YES completion:nil];
   self.viewController = nil;
 
+  [self.activityServiceCoordinator stop];
+  self.activityServiceCoordinator = nil;
+
   [super stop];
 }
 
@@ -80,13 +96,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)confirmationAlertPrimaryAction {
-  // No-op.
-  // TODO crbug.com/1064990: Add sharing action.
+  self.activityServiceCoordinator = [[ActivityServiceCoordinator alloc]
+      initWithBaseViewController:self.viewController
+                         browser:self.browser];
+
+  self.activityServiceCoordinator.positionProvider = self;
+  self.activityServiceCoordinator.presentationProvider = self;
+
+  // Configure the image sharing scenario.
+  self.activityServiceCoordinator.image = self.viewController.content;
+  self.activityServiceCoordinator.title = l10n_util::GetNSStringF(
+      IDS_IOS_QR_CODE_ACTIVITY_TITLE, base::SysNSStringToUTF16(self.title));
+
+  [self.activityServiceCoordinator start];
 }
 
 - (void)confirmationAlertLearnMoreAction {
   // No-op.
   // TODO crbug.com/1064990: Add learn more behavior.
+}
+
+#pragma mark - ActivityServicePositioner
+
+- (UIView*)shareButtonView {
+  return [self.viewController primaryActionButton];
+}
+
+#pragma mark - ActivityServicePresentation
+
+- (void)activityServiceDidEndPresenting {
+  [self.activityServiceCoordinator stop];
+  self.activityServiceCoordinator = nil;
 }
 
 @end
