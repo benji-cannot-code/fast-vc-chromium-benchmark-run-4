f7499bf4313cb9e64d3b3b0d901367c8cd113a6a
Peng Huang <penghuang@chromium.org>
2020-05-04 21:49:16 +0000

    vulkan: fix pdf render problem with vulkan
    The problem is because PDF render uses GLES2Interface for
    uploading textures. The GLESInterface reports GL_BGRA8_EXT is
    supported, but ExternalVkImageBacking doesn't support BGRA format
    properly. It always uses RGBA format with texture swizzle. However BGRA
    format are supported by some GL implementation, and texture swizzle is
    not supported for GL < 3.3 or GLES < 3.0. So it may not render BGRA
    shared image properly. Fix the problem by checking if BGRA8 texture is
    supported by platform opengl. If it does, use that format, If not,
    but texture swizzle is supported, we will use RGBA8 format with
    texture swizzle.
    
    Bug: 1077616,1001533
    Change-Id: Idaa844161b760fac0e9076aa84d911f8c420a73d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2177971
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Jonathan Backer <backer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765279}

--- gpu/command_buffer/service/external_vk_image_gl_representation.cc
+++ gpu/command_buffer/service/external_vk_image_gl_representation.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/posix/eintr_wrapper.h"
 #include "build/build_config.h"
 #include "gpu/vulkan/vulkan_function_pointers.h"
 #include "gpu/vulkan/vulkan_implementation.h"
@@ -82,14 +81,7 @@ bool ExternalVkImageGLRepresentationShared::BeginAccess(GLenum mode) {
          mode == GL_SHARED_IMAGE_ACCESS_MODE_READWRITE_CHROMIUM);
   const bool readonly = (mode == GL_SHARED_IMAGE_ACCESS_MODE_READ_CHROMIUM);
 
-  if (!readonly && backing_impl()->format() == viz::ResourceFormat::BGRA_8888) {
-    NOTIMPLEMENTED()
-        << "BeginAccess write on a BGRA_8888 backing is not supported.";
-    return false;
-  }
-
   std::vector<SemaphoreHandle> handles;
-
   if (!backing_impl()->BeginAccess(readonly, &handles, true /* is_gl */))
     return false;
 
