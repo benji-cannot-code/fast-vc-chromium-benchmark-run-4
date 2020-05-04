9390619bc00a4266e1a990c95eda112b10236cb7
Eugene But <eugenebut@chromium.org>
2020-05-04 22:20:06 +0000

    Remove expired ignores-viewport-scale-limits flag
    The feature has been enabled by default for multiple releases and the
    flag expired in M80.
    
    Bug: None
    Change-Id: I1656d0d1874d4ac205f73676b80e02c6de2b807b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176290
    Auto-Submit: Eugene But <eugenebut@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765294}

--- ios/web/common/features.mm
+++ ios/web/common/features.mm
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace web {
 namespace features {
 
-const base::Feature kIgnoresViewportScaleLimits{
-    "IgnoresViewportScaleLimits", base::FEATURE_ENABLED_BY_DEFAULT};
-
 const base::Feature kCrashOnUnexpectedURLChange{
     "CrashOnUnexpectedURLChange", base::FEATURE_ENABLED_BY_DEFAULT};
 
