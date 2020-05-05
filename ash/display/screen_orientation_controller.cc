b4a9bf4364ff6b709dd0e99554a12d4e37fcf6d2
Ahmed Fakhry <afakhry@chromium.org>
2020-05-05 20:20:06 +0000

    Re-enable OverviewButtonTrayHideAnimationAlwaysCompletes
    The test was flaking on msan builds, due to a use after free
    of the TabletModeController in ~Shell() in ShelfConfig.
    
    This CL makes ShelfConfig track the tablet mode state locally
    so as not to depend on the lifetime of the TabletModeController.
    
    BUG=1077712
    
    Change-Id: I7926b5ffaad5c19e5faa81c9b3c63b89532441b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2180975
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765726}

--- ash/display/screen_orientation_controller.cc
+++ ash/display/screen_orientation_controller.cc
@@ -169,7 +169,6 @@ bool IsPortraitOrientation(OrientationLockType type) {
 }
 
 OrientationLockType GetCurrentScreenOrientation() {
-  DCHECK(Shell::Get()->tablet_mode_controller()->InTabletMode());
   // ScreenOrientationController might be nullptr during shutdown.
   // TODO(xdai|sammiequon): See if we can reorder so that users of the function
   // |SplitViewController::Get| get shutdown before screen orientation
