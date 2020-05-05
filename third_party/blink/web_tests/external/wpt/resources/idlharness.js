5463426802c7fddaf2826d6266ad13741a742549
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-05-05 21:49:01 +0000

    Import wpt@9f85748a60c4c1fb1fd910a3ea21708418e01584
    Using wpt-import in Chromium a374c57704748a11a18c30b860f37e820d63f078.
    With Chromium commits locally applied on WPT:
    4aa9d9f35a "Idle: Add notification permission check"
    59c99fcd65 "Clipboard API Tests: Automate Web Platform Tests"
    ef74ae8108 "[idle] Change threshold option from seconds to milliseconds"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I033ed0b37f6469b7389d16461111e4aaa6210d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181734
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#765773}

--- third_party/blink/web_tests/external/wpt/resources/idlharness.js
+++ third_party/blink/web_tests/external/wpt/resources/idlharness.js
@@ -1221,6 +1221,7 @@ IdlArray.prototype.assert_type_is = function(value, type)
         case "Float64Array":
         case "ArrayBuffer":
         case "DataView":
+        case "Function":
             assert_true(value instanceof self[type], "wrong type: not a " + type);
             return;
 
