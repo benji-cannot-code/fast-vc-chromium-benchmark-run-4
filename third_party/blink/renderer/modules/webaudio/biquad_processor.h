519745c5dedcea2c4623f02c332b628edc89bed0
Raymond Toy <rtoy@chromium.org>
2020-05-05 16:51:17 +0000

    Handle k-rate AudioParam inputs for BiquadFilterNode
    Support k-rate input to AudioParams for a BiquadFilterNode.
    Previously, inputs to a k-rate AudioParam was ignored, which is
    incorrect.  The input must be summed with any timeline value.
    
    Manually ran the new test case with Chrome stable and all the tests fail
    as expected.
    
    Bug: 1015760
    Test: k-rate-biquad-connections.html
    Change-Id: Iec7b57fe3b730d4f5984e223fde9cef9a4402103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144687
    Commit-Queue: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765606}

--- third_party/blink/renderer/modules/webaudio/biquad_processor.h
+++ third_party/blink/renderer/modules/webaudio/biquad_processor.h
@@ -83,6 +83,7 @@ class BiquadProcessor final : public AudioDSPKernelProcessor {
 
   bool FilterCoefficientsDirty() const { return filter_coefficients_dirty_; }
   bool HasSampleAccurateValues() const { return has_sample_accurate_values_; }
+  bool IsAudioRate() const { return is_audio_rate_; }
 
   AudioParamHandler& Parameter1() { return *parameter1_; }
   AudioParamHandler& Parameter2() { return *parameter2_; }
@@ -105,6 +106,9 @@ class BiquadProcessor final : public AudioDSPKernelProcessor {
 
   // Set to true if any of the filter parameters are sample-accurate.
   bool has_sample_accurate_values_;
+
+  // Set to true if any of the filter parameters are a-rate.
+  bool is_audio_rate_;
 };
 
 }  // namespace blink
