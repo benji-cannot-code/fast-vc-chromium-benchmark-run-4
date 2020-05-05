804bbeacd36c8854f4cc999b6e3c066ed3e7fa93
Kevin Ellis <kevers@chromium.org>
2020-05-05 13:03:47 +0000

    Fix flake in style-change-events.html due to float point errors
    Fixes a test flake caused by float point errors. The start time is
    expressed as a double, but timelines use TimeDelta. This discrepancy
    leads to non-deterministic behavior when indirectly setting the current
    time to zero by setting start time to timeline time. The value of
    current time ends up being set to  0 +/- epsilon.  Snapping to timeline
    time for small deltas (approx 2e-17) ensures consistent behavior.
    
    Bug: 1064463
    Change-Id: I971fd7cfc9b273dc4d303bbd6b25bcf0c964686a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173163
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765503}

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/pending_animations.h"
 #include "third_party/blink/renderer/core/animation/scroll_timeline.h"
 #include "third_party/blink/renderer/core/animation/scroll_timeline_util.h"
+#include "third_party/blink/renderer/core/animation/timing_calculations.h"
 #include "third_party/blink/renderer/core/css/properties/css_property_ref.h"
 #include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
 #include "third_party/blink/renderer/core/css/style_change_reason.h"
@@ -756,8 +757,15 @@ void Animation::setStartTime(base::Optional<double> start_time_ms,
 
   // 5. Set animation’s start time to new start time.
   base::Optional<double> new_start_time;
-  if (start_time_ms)
+  if (start_time_ms) {
     new_start_time = MillisecondsToSeconds(start_time_ms.value());
+    // Snap to timeline time if within floating point tolerance to ensure
+    // deterministic behavior in phase transitions.
+    if (timeline_time && IsWithinAnimationTimeEpsilon(timeline_time.value(),
+                                                      new_start_time.value())) {
+      new_start_time = timeline_time.value();
+    }
+  }
   start_time_ = new_start_time;
 
   // 6. Update animation’s hold time based on the first matching condition from
