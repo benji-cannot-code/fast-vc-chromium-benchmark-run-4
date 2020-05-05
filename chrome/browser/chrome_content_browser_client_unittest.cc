52811df1339c762d15b4227a008eebc1db804736
Yoav Weiss <yoavweiss@chromium.org>
2020-05-05 18:23:35 +0000

    UserAgent tests - fix the test-o-phone bot
    Bug: 1077717
    Change-Id: I5202e4ebd2fc636b305af59854a6c65dfa51da2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181205
    Commit-Queue: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765659}

--- chrome/browser/chrome_content_browser_client_unittest.cc
+++ chrome/browser/chrome_content_browser_client_unittest.cc
@@ -193,9 +193,8 @@ void CheckUserAgentStringOrdering(bool mobile_device) {
   ASSERT_GE(3u, pieces.size());
   ASSERT_EQ("Linux", pieces[0]);
   std::string model;
-  if (pieces.size() > 2) {
+  if (pieces.size() > 2)
     model = pieces[2];
-  }
 
   pieces = base::SplitStringUsingSubstr(pieces[1], " ", base::KEEP_WHITESPACE,
                                         base::SPLIT_WANT_ALL);
@@ -209,11 +208,10 @@ void CheckUserAgentStringOrdering(bool mobile_device) {
   }
 
   if (!model.empty()) {
-    if (base::SysInfo::GetAndroidBuildCodename() == "REL") {
+    if (base::SysInfo::GetAndroidBuildCodename() == "REL")
       ASSERT_EQ(base::SysInfo::HardwareModelName(), model);
-    } else {
+    else
       ASSERT_EQ("", model);
-    }
   }
 #elif defined(OS_FUCHSIA)
   // X11; Fuchsia
