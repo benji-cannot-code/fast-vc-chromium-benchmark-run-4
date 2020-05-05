e963f8f00cfa24710fabc36ccda529fb235ec27b
Regan Hsu <hsuregan@google.com>
2020-05-05 14:45:07 +0000

    [DlcSettingsUi] Add purge DLC method to DevicePageBrowserProxy.
    Adds a purgeDlc() method that will purge a DLC when provided the ID.
    This will be used by the DLC subpage to allow users to purge any given
    DLC on the device.
    
    Bug: 1070712
    Change-Id: Id6b980b2d3e11bfba002ee79bddc13785d5d2cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173589
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765546}

--- chrome/browser/resources/settings/chromeos/device_page/device_page_browser_proxy.js
+++ chrome/browser/resources/settings/chromeos/device_page/device_page_browser_proxy.js
@@ -194,6 +194,13 @@ cr.define('settings', function() {
      * @return {!Promise<!Array<!settings.DlcMetadata>>} A list of DLC metadata.
      */
     getDlcList() {}
+
+    /**
+     * Purges the DLC with the provided |dlcId| from the device.
+     * @param {string} dlcId The ID of the DLC to purge from the device.
+     * @return {!Promise<boolean>} Whether purging of DLC was successful.
+     */
+    purgeDlc(dlcId) {}
   }
 
   /**
@@ -293,6 +300,11 @@ cr.define('settings', function() {
     /** @override */
     getDlcList() {
       return cr.sendWithPromise('getDlcList');
+    }
+
+    /** @override */
+    purgeDlc(dlcId) {
+      return cr.sendWithPromise('purgeDlc', dlcId);
     }
   }
 
