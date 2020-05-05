0c5698090a29a1cd92a861e82469341d137ca620
John Delaney <johnidel@chromium.org>
2020-05-05 18:41:22 +0000

    Add Conversion Measurement internals page
    This CL adds a chrome://conversion-internals page which exposes internal
    state for the Conversion Measurement API's. A new WebUI-only mojo
    interface is added to send persisted data to WebUI.
    
    The initial UI will dump the impression and conversion report tables to
    the page, as well as the feature status for the API. Future changes are
    planned to extend functionality:
     - force send pending reports
     - clearing storage
    
    Bug: 1057240
    Change-Id: I0d0f0d61dd6587a9e35eb100e1834d9dcd9524f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2135057
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765670}

--- content/browser/conversions/conversion_manager.h
+++ content/browser/conversions/conversion_manager.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_CONVERSIONS_CONVERSION_MANAGER_H_
 #define CONTENT_BROWSER_CONVERSIONS_CONVERSION_MANAGER_H_
 
+#include <vector>
+
 #include "base/callback.h"
 #include "base/callback_forward.h"
 #include "content/browser/conversions/conversion_policy.h"
+#include "content/browser/conversions/conversion_report.h"
 #include "content/browser/conversions/storable_conversion.h"
 #include "content/browser/conversions/storable_impression.h"
 #include "content/common/content_export.h"
@@ -48,6 +51,17 @@ class CONTENT_EXPORT ConversionManager {
   // Notify storage to delete the given |conversion_id| when it's associated
   // report has been sent.
   virtual void HandleSentReport(int64_t conversion_id) = 0;
+
+  // Get all impressions that are currently stored in this partition. Used for
+  // populating WebUI.
+  virtual void GetActiveImpressionsForWebUI(
+      base::OnceCallback<void(std::vector<StorableImpression>)> callback) = 0;
+
+  // Get all pending reports that are currently stored in this partition. Used
+  // for populating WebUI.
+  virtual void GetReportsForWebUI(
+      base::OnceCallback<void(std::vector<ConversionReport>)> callback,
+      base::Time max_report_time) = 0;
 
   // Returns the ConversionPolicy that is used to control API policies such
   // as noise.
