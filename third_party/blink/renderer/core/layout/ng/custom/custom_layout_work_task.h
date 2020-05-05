837e77aee79e1211e7f6e5914ff8e4d3e81c946b
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-05-05 22:56:48 +0000

    [FlexNG] Make ComputeMinMaxSizes cache more precise.
    Previously our cache was imprecise, we recalculate our min/max sizes
    when we didn't strictly need to, as we didn't know if we'd have a
    %-block-size replaced element beneath us.
    
    This patch changes ComputeMinMaxSizes to return a MinMaxSizesResult
    (instead of just MinMaxSizes). This contains an additional bit of
    information to indicate if this sub-tree depended on the %-block-size
    given to it.
    
    For most things, this will return false. For legacy layout roots we
    always assume that they have children which depend on the %-block-size
    (and why we initialize:
     intrinsic_logical_widths_depends_on_percentage_block_size_ to true).
    
    Along with this change, the cache changes in the following way:
     - We store an addition bit of information on LayoutBox to store the bit
       of information from ComputeMinMaxSizes.
     - We calculate the %-block-size for our children up-front, this means
       each of the algorithms don't have to do this.
     - With these changes, the cache can now hit, when the %-block-size
       given to us changes.
    
    \o/
    
    Bug: 845235
    Change-Id: I93504721dbdd13bef58917b1fab2cabbcd1ce51e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165676
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765797}

--- third_party/blink/renderer/core/layout/ng/custom/custom_layout_work_task.h
+++ third_party/blink/renderer/core/layout/ng/custom/custom_layout_work_task.h
@@ -48,7 +48,8 @@ class CustomLayoutWorkTask {
   // Runs this work task.
   void Run(const NGConstraintSpace& parent_space,
            const ComputedStyle& parent_style,
-           const LayoutUnit child_percentage_resolution_block_size_for_min_max);
+           const LayoutUnit child_percentage_resolution_block_size_for_min_max,
+           bool* child_depends_on_percentage_block_size = nullptr);
 
  private:
   Persistent<CustomLayoutChild> child_;
@@ -64,7 +65,8 @@ class CustomLayoutWorkTask {
   void RunIntrinsicSizesTask(
       const ComputedStyle& parent_style,
       const LayoutUnit child_percentage_resolution_block_size_for_min_max,
-      NGLayoutInputNode child);
+      NGLayoutInputNode child,
+      bool* child_depends_on_percentage_block_size);
 };
 
 }  // namespace blink
