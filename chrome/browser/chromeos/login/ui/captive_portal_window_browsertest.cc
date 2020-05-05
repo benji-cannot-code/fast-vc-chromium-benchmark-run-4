39e07bd5ed145ad715ce46b987489e490a86374f
Roman Sorokin <rsorokin@chromium.org>
2020-05-05 13:45:43 +0000

    cros: Enable CaptivePortalWindowCtorDtorTest.OpenPortalDialog
    Looks good with the local runs
    
    Bug: 1005456
    Change-Id: I5ce31b1a105306f2d00cc4878f44239f824cc6be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174490
    Reviewed-by: Roman Aleksandrov <raleksandrov@google.com>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765521}

--- chrome/browser/chromeos/login/ui/captive_portal_window_browsertest.cc
+++ chrome/browser/chromeos/login/ui/captive_portal_window_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/login_manager_test.h"
 #include "chrome/browser/chromeos/login/screens/error_screen.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
+#include "chrome/browser/chromeos/login/test/device_state_mixin.h"
 #include "chrome/browser/chromeos/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/chromeos/login/ui/captive_portal_window_proxy.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host.h"
@@ -207,18 +208,13 @@ class CaptivePortalWindowCtorDtorTest : public LoginManagerTest {
  private:
   NetworkPortalDetectorTestImpl* network_portal_detector_;
 
+  DeviceStateMixin device_state_{
+      &mixin_host_, DeviceStateMixin::State::OOBE_COMPLETED_UNOWNED};
+
   DISALLOW_COPY_AND_ASSIGN(CaptivePortalWindowCtorDtorTest);
 };
 
-// Flaky. https://crbug.com/1005456.
-IN_PROC_BROWSER_TEST_F(CaptivePortalWindowCtorDtorTest,
-                       DISABLED_PRE_OpenPortalDialog) {
-  StartupUtils::MarkOobeCompleted();
-}
-
-// Flaky. https://crbug.com/1005456.
-IN_PROC_BROWSER_TEST_F(CaptivePortalWindowCtorDtorTest,
-                       DISABLED_OpenPortalDialog) {
+IN_PROC_BROWSER_TEST_F(CaptivePortalWindowCtorDtorTest, OpenPortalDialog) {
   LoginDisplayHost* host = LoginDisplayHost::default_host();
   ASSERT_TRUE(host);
   OobeUI* oobe = host->GetOobeUI();
