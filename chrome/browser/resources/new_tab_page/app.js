b7032e0ef79b9121b08251003e7462ce6f990e34
Tibor Goldschwendt <tiborg@chromium.org>
2020-05-05 03:54:27 +0000

    [webui][ntp] Perf traces for applying theme, loading background image
    Adds traces to measure the duration until the theme is set and the
    background image is loaded. Since the background image is loaded in an
    iframe, the load event is sent back to the NTP via a postMessage and
    logged there. If we logged the background image load time in the iframe,
    the event would be logged with respect to the iframe's load time and,
    thus, wouldn't be comparable to other NTP traces.
    
    Bug: 1061078
    Change-Id: Iaf5ceec2c6ee44e0f838cf25ae2c1cbbe0f03a58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181522
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765387}

--- chrome/browser/resources/new_tab_page/app.js
+++ chrome/browser/resources/new_tab_page/app.js
@@ -215,6 +215,7 @@ class AppElement extends PolymerElement {
     super.connectedCallback();
     this.setThemeListenerId_ =
         this.callbackRouter_.setTheme.addListener(theme => {
+          performance.measure('theme-set');
           this.theme_ = theme;
         });
     this.eventTracker_.add(window, 'message', ({data}) => {
@@ -228,6 +229,8 @@ class AppElement extends PolymerElement {
           this.handlePromoMessage_(data);
         } else if (data.frameType === 'one-google-bar') {
           this.handleOneGoogleBarMessage_(data);
+        } else if (data.frameType === 'background-image') {
+          this.handleBackgroundImageMessage_(data);
         }
       }
     });
@@ -507,6 +510,7 @@ class AppElement extends PolymerElement {
         path = '';
     }
     if (path && this.$.backgroundImage.path !== path) {
+      performance.mark('background-image-load-start');
       this.$.backgroundImage.path = path;
     }
   }
@@ -570,6 +574,19 @@ class AppElement extends PolymerElement {
       $$(this, '#oneGoogleBar').style.zIndex = '1000';
     } else if (data.messageType === 'deactivate') {
       $$(this, '#oneGoogleBar').style.zIndex = '0';
+    }
+  }
+
+  /**
+   * @param {!Object} data
+   * @private
+   */
+  handleBackgroundImageMessage_(data) {
+    if (data.src === this.theme_.backgroundImageUrl.url &&
+        data.messageType === 'loaded') {
+      performance.measure(
+          'background-image-load', 'background-image-load-start');
+      performance.measure('background-image-loaded');
     }
   }
 
