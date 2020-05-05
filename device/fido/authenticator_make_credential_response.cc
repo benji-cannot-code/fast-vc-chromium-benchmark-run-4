f32fbb31752cc6033d2dbc8913ae67ce31f017f0
Adam Langley <agl@chromium.org>
2020-05-05 14:40:12 +0000

    device/fido: rename ECPublicKey to P256PublicKey.
    ECPublicKey is used for U2F credentials and thus is exactly P-256 ECDSA
    and not generic to elliptic curves in general.
    
    Change-Id: I011ca244d408e446b832726af5d92604a41855e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2177190
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#765544}

--- device/fido/authenticator_make_credential_response.cc
+++ device/fido/authenticator_make_credential_response.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/attested_credential_data.h"
 #include "device/fido/authenticator_data.h"
 #include "device/fido/client_data.h"
-#include "device/fido/ec_public_key.h"
 #include "device/fido/fido_parsing_utils.h"
+#include "device/fido/p256_public_key.h"
 
 namespace device {
 
@@ -24,7 +24,7 @@ AuthenticatorMakeCredentialResponse::CreateFromU2fRegisterResponse(
     base::Optional<FidoTransportProtocol> transport_used,
     base::span<const uint8_t, kRpIdHashLength> relying_party_id_hash,
     base::span<const uint8_t> u2f_data) {
-  auto public_key = ECPublicKey::ExtractFromU2fRegistrationResponse(
+  auto public_key = P256PublicKey::ExtractFromU2fRegistrationResponse(
       fido_parsing_utils::kEs256, u2f_data);
   if (!public_key)
     return base::nullopt;
