9721755bf31e95c627868f9986df345022f4cabb
David Lei <dlei@google.com>
2020-05-05 06:22:42 +0000

    Remove logging to console in browsertests for chrome://media-app.
    Remove console.logs I left in crrev/c/2174019 oops.
    
    Change-Id: I85b95956b87a978229c9ed00c21cabd68eb1047d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181349
    Auto-Submit: David Lei <dlei@google.com>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765420}

--- chromeos/components/media_app_ui/test/media_app_ui_browsertest.js
+++ chromeos/components/media_app_ui/test/media_app_ui_browsertest.js
@@ -278,8 +278,6 @@ TEST_F('MediaAppUIBrowserTest', 'NavigateIPC', async () => {
 // in the untrusted context.
 TEST_F('MediaAppUIBrowserTest', 'RenameOriginalIPC', async () => {
   const firstFile = await createTestImageFile();
-  console.log('Rename DLEI firstFile size: ' + firstFile.size);
-  console.log(firstFile.name);
   const directory = await createMockTestDirectory([firstFile]);
   // Simulate steps taken to load a file via a launch event.
   const firstFileHandle = directory.files[0];
