e3e9ee27f57c173506c3e34d6547e91c33261004
Dave Tapuska <dtapuska@chromium.org>
2020-05-05 16:45:13 +0000

    Move a few more helper methods in blink_event_util.h into blink.
    This will allow moving InputHandlerProxy into blink.
    
    BUG=1073560
    
    Change-Id: I925e8100190796a77e9bf9b46403ea7d1184a181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2180291
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765601}

--- third_party/blink/public/common/input/web_input_event.h
+++ third_party/blink/public/common/input/web_input_event.h
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "third_party/blink/public/common/common_export.h"
 #include "third_party/blink/public/mojom/input/input_event.mojom-shared.h"
+#include "ui/events/types/event_type.h"
+#include "ui/events/types/scroll_input_type.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 
@@ -330,6 +332,15 @@ class BLINK_COMMON_EXPORT WebInputEvent {
 
   // Merge the current event with attributes from |event|.
   virtual void Coalesce(const WebInputEvent& event) = 0;
+
+  // Returns the scroll input type if this is a scroll event; otherwise,
+  // returns base::nullopt.
+  virtual base::Optional<ui::ScrollInputType> GetScrollInputType() const;
+
+  // Convert this WebInputEvent::Type to a ui::EventType. Note that this is
+  // not a 1:1 relationship. Multiple blink types convert to the same
+  // ui::EventType and not all types do convert.
+  ui::EventType GetTypeAsUiEventType() const;
 
  protected:
   // The root frame scale.
