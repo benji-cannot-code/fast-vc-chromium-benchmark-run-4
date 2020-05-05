10befd1dc38e4f51e95d21f2380be82e74de1282
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-05-05 18:40:52 +0000

    Import wpt@664d6c5556092f71949ebc50f77724ea35f28d27
    Using wpt-import in Chromium 70e8360ab5ded321b9b19654aca0e80b63dd8da7.
    With Chromium commits locally applied on WPT:
    4aa9d9f35a "Idle: Add notification permission check"
    59c99fcd65 "Clipboard API Tests: Automate Web Platform Tests"
    aef378ec15 "Treat -webkit-writing-mode as animation-affecting"
    88f4429867 "Add WPT tests for bug 1074317"
    2202100baf "[EventTiming] Add a test for auxclick"
    a32837d3a3 "Add rAF delays before cross-origin clicks to fix flakiness."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    foolip@chromium.org, robertma@chromium.org, smcgruer@chromium.org:
      external/wpt/infrastructure
    jsbell@chromium.org:
      external/wpt/resources
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Iee7a5dc191a26c96fe2d656446982716270e18f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181311
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#765669}

--- third_party/blink/web_tests/external/wpt/resources/testharness.js
+++ third_party/blink/web_tests/external/wpt/resources/testharness.js
@@ -1868,23 +1868,10 @@ policies and contribution forms [3].
      */
     function assert_implements_optional(condition, description) {
         if (!condition) {
-            // Due to the difficulty of changing logging statuses, we re-use
-            // the PRECONDITION_FAILED status for assert_implements_optional.
-            // See the RFC: https://github.com/web-platform-tests/rfcs/pull/48
-            //
-            // TODO(smcgruer): Once assert_precondition is removed, rename the
-            // exception and move this comment to where PRECONDITION_FAILED is used.
-            throw new PreconditionFailedError(description);
+            throw new OptionalFeatureUnsupportedError(description);
         }
     }
     expose(assert_implements_optional, "assert_implements_optional")
-
-    function assert_precondition(precondition, description) {
-        if (!precondition) {
-            throw new PreconditionFailedError(description);
-        }
-    }
-    expose(assert_precondition, "assert_precondition");
 
     function Test(name, properties)
     {
@@ -1991,7 +1978,7 @@ policies and contribution forms [3].
             if (this.phase >= this.phases.HAS_RESULT) {
                 return;
             }
-            var status = e instanceof PreconditionFailedError ? this.PRECONDITION_FAILED : this.FAIL;
+            var status = e instanceof OptionalFeatureUnsupportedError ? this.PRECONDITION_FAILED : this.FAIL;
             var message = String((typeof e === "object" && e !== null) ? e.message : e);
             var stack = e.stack ? e.stack : null;
 
@@ -2577,7 +2564,7 @@ policies and contribution forms [3].
             try {
                 func();
             } catch (e) {
-                this.status.status = e instanceof PreconditionFailedError ? this.status.PRECONDITION_FAILED : this.status.ERROR;
+                this.status.status = e instanceof OptionalFeatureUnsupportedError ? this.status.PRECONDITION_FAILED : this.status.ERROR;
                 this.status.message = String(e);
                 this.status.stack = e.stack ? e.stack : null;
                 this.complete();
@@ -3119,14 +3106,14 @@ policies and contribution forms [3].
         status_text_harness[harness_status.OK] = "OK";
         status_text_harness[harness_status.ERROR] = "Error";
         status_text_harness[harness_status.TIMEOUT] = "Timeout";
-        status_text_harness[harness_status.PRECONDITION_FAILED] = "Precondition Failed";
+        status_text_harness[harness_status.PRECONDITION_FAILED] = "Optional Feature Unsupported";
 
         var status_text = {};
         status_text[Test.prototype.PASS] = "Pass";
         status_text[Test.prototype.FAIL] = "Fail";
         status_text[Test.prototype.TIMEOUT] = "Timeout";
         status_text[Test.prototype.NOTRUN] = "Not Run";
-        status_text[Test.prototype.PRECONDITION_FAILED] = "Precondition Failed";
+        status_text[Test.prototype.PRECONDITION_FAILED] = "Optional Feature Unsupported";
 
         var status_number = {};
         forEach(tests,
@@ -3508,12 +3495,12 @@ policies and contribution forms [3].
         return lines.slice(i).join("\n");
     }
 
-    function PreconditionFailedError(message)
+    function OptionalFeatureUnsupportedError(message)
     {
         AssertionError.call(this, message);
     }
-    PreconditionFailedError.prototype = Object.create(AssertionError.prototype);
-    expose(PreconditionFailedError, "PreconditionFailedError");
+    OptionalFeatureUnsupportedError.prototype = Object.create(AssertionError.prototype);
+    expose(OptionalFeatureUnsupportedError, "OptionalFeatureUnsupportedError");
 
     function make_message(function_name, description, error, substitutions)
     {
@@ -3743,17 +3730,17 @@ policies and contribution forms [3].
 
     if (global_scope.addEventListener) {
         var error_handler = function(error, message, stack) {
-            var precondition_failed = error instanceof PreconditionFailedError;
+            var optional_unsupported = error instanceof OptionalFeatureUnsupportedError;
             if (tests.file_is_test) {
                 var test = tests.tests[0];
                 if (test.phase >= test.phases.HAS_RESULT) {
                     return;
                 }
-                var status = precondition_failed ? test.PRECONDITION_FAILED : test.FAIL;
+                var status = optional_unsupported ? test.PRECONDITION_FAILED : test.FAIL;
                 test.set_status(status, message, stack);
                 test.phase = test.phases.HAS_RESULT;
             } else if (!tests.allow_uncaught_exception) {
-                var status = precondition_failed ? tests.status.PRECONDITION_FAILED : tests.status.ERROR;
+                var status = optional_unsupported ? tests.status.PRECONDITION_FAILED : tests.status.ERROR;
                 tests.status.status = status;
                 tests.status.message = message;
                 tests.status.stack = stack;
@@ -3869,11 +3856,11 @@ tr.notrun > td:first-child {\
     color:blue;\
 }\
 \
-tr.preconditionfailed > td:first-child {\
+tr.optionalunsupported > td:first-child {\
     color:blue;\
 }\
 \
-.pass > td:first-child, .fail > td:first-child, .timeout > td:first-child, .notrun > td:first-child, .preconditionfailed > td:first-child {\
+.pass > td:first-child, .fail > td:first-child, .timeout > td:first-child, .notrun > td:first-child, .optionalunsupported > td:first-child {\
     font-variant:small-caps;\
 }\
 \
