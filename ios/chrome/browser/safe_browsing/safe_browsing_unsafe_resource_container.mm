fdab928cae22e334040b66acb3494af04fce4ef3
Kurt Horimoto <kkhorimoto@chromium.org>
2020-05-05 23:34:58 +0000

    [iOS] Release main frame unsafe resource when kicking off new request
    As part of the placeholder navigation flow, navigating back from a safe
    browsing error page then back forward causes two ShouldAllowRequest()
    calls for the same URL.  The first navigation is cancelled before the
    error page can be shown, so the UnsafeResource is stored in the
    container twice.  This CL releases the main frame unsafe resource so
    that the container always holds the unsafe resource for the most
    recent request.
    
    Bug: 1064803
    Change-Id: I25ddb860c2a87985149ccd24e9e72dd8fac750f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176655
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765808}

--- ios/chrome/browser/safe_browsing/safe_browsing_unsafe_resource_container.mm
+++ ios/chrome/browser/safe_browsing/safe_browsing_unsafe_resource_container.mm
@@ -41,8 +41,10 @@ class UnsafeSubresourceContainer : public base::SupportsUserData::Data {
     return stack;
   }
 
-  // Whether the container is holding an unsafe resource.
-  bool HasUnsafeResource() const { return unsafe_subresource_.get(); }
+  // Returns a pointer to unsafe resource, or null if one is not stored.
+  const security_interstitials::UnsafeResource* GetUnsafeResource() const {
+    return unsafe_subresource_.get();
+  }
 
   // Stores a copy of |subresource| in the container.
   void StoreUnsafeSubresource(std::unique_ptr<UnsafeResource> subresource) {
@@ -92,7 +94,7 @@ void SafeBrowsingUnsafeResourceContainer::StoreUnsafeResource(
     // For main frame navigations, the copy is stored in
     // |main_frame_unsafe_resource_| it corresponds with the pending
     // NavigationItem, which is discarded when the navigation is cancelled.
-    DCHECK(!HasMainFrameUnsafeResource());
+    DCHECK(!GetMainFrameUnsafeResource());
     main_frame_unsafe_resource_ = std::move(resource_copy);
   } else {
     // Unsafe subresources are caused by loads triggered by the committed main
@@ -100,13 +102,14 @@ void SafeBrowsingUnsafeResourceContainer::StoreUnsafeResource(
     // that they persist past reloads.
     web::NavigationItem* item =
         web_state_->GetNavigationManager()->GetLastCommittedItem();
-    DCHECK(!HasSubFrameUnsafeResource(item));
+    DCHECK(!GetSubFrameUnsafeResource(item));
     UnsafeSubresourceContainer::FromNavigationItem(item)
         ->StoreUnsafeSubresource(std::move(resource_copy));
   }
 }
 
-bool SafeBrowsingUnsafeResourceContainer::HasMainFrameUnsafeResource() const {
+const security_interstitials::UnsafeResource*
+SafeBrowsingUnsafeResourceContainer::GetMainFrameUnsafeResource() const {
   return main_frame_unsafe_resource_.get();
 }
 
@@ -115,10 +118,11 @@ SafeBrowsingUnsafeResourceContainer::ReleaseMainFrameUnsafeResource() {
   return std::move(main_frame_unsafe_resource_);
 }
 
-bool SafeBrowsingUnsafeResourceContainer::HasSubFrameUnsafeResource(
+const security_interstitials::UnsafeResource*
+SafeBrowsingUnsafeResourceContainer::GetSubFrameUnsafeResource(
     web::NavigationItem* item) const {
   return UnsafeSubresourceContainer::FromNavigationItem(item)
-      ->HasUnsafeResource();
+      ->GetUnsafeResource();
 }
 
 std::unique_ptr<security_interstitials::UnsafeResource>
