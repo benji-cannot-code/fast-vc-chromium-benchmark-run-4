fa9dde839600928198cb9802d9d558dbe41e7f2b
Mugdha Lakhani <nator@chromium.org>
2020-05-05 12:21:46 +0000

    [WebLayer] Embedder can show Page Info from url text.
    Before this CL, only the security status icon of WebLayer Url Bar was
    clickable. After this, the default is still the same, but the embedder
    can make the URL bar text clickable as well through a UrlBarOptions
    option.
    
    Videos of UI possible after this change can be seen here:
    https://drive.google.com/drive/folders/1UE9aOdAwuf9BTwKoPK1Vot52kJ21E_tC?usp=sharing
    
    Bug: 1075773
    Change-Id: I068358f75cc23cfcd4b0f1b20bd96e904d04d55b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172083
    Commit-Queue: Mugdha Lakhani <nator@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765495}

---
+++ weblayer/browser/java/org/chromium/weblayer_private/interfaces/UrlBarOptionsKeys.java
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer_private.interfaces;
+
+/** Keys for the Bundle of arguments with which BrowserFragments are created. */
+public interface UrlBarOptionsKeys {
+    String URL_TEXT_SIZE = "UrlTextSize";
+    String SHOW_PAGE_INFO_WHEN_URL_TEXT_CLICKED = "ShowPageInfoWhenUrlTextClicked";
+}
