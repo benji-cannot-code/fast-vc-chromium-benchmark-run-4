a52deae4f2a48131903550424c0970791efae675
Miyoung Shin <myid.shin@igalia.com>
2020-05-05 16:49:18 +0000

    Hide the popup menu if the mojo connection is still open
    AwAutofillTest#testSelectControlChangeNotification is flaky because
    the the mojo connect can be created rapidly before the previous mojo
    connection is connected, so this CL hides the popup menu if the
    previous mojo connection is still open.
    
    Bug: 1074279
    Change-Id: Ibb2d344d098be802e4bc0573f576f4259f30e4b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2179682
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765604}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwAutofillTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwAutofillTest.java
@@ -1276,7 +1276,6 @@ public class AwAutofillTest {
     }
 
     @Test
-    @FlakyTest(message = "https://crbug.com/1074279")
     @SmallTest
     @Feature({"AndroidWebView"})
     @DisableIf.Build(sdk_is_less_than = Build.VERSION_CODES.P,
