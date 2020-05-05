02c07afe40134422ab8a94ebcd27a4a8c4024435
danakj <danakj@chromium.org>
2020-05-05 14:16:17 +0000

    Move the beforeinstallprompt code out of BlinkTestRunner.
    The code makes a fake service-side object and sends a message to the
    RenderFrame. This doesn't need to be in BlinkTestRunner or attached to
    per-RenderView things.
    
    R=avi@chromium.org
    
    Bug: 866140, 1069111
    Change-Id: I499b842e25cfc172151838770cd36a2bd59568b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2180903
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765532}

--- content/shell/renderer/web_test/blink_test_runner.cc
+++ content/shell/renderer/web_test/blink_test_runner.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_thread.h"
 #include "content/public/renderer/render_view.h"
 #include "content/shell/common/shell_switches.h"
-#include "content/shell/renderer/web_test/app_banner_service.h"
 #include "content/shell/renderer/web_test/blink_test_helpers.h"
 #include "content/shell/renderer/web_test/gamepad_controller.h"
 #include "content/shell/renderer/web_test/pixel_dump.h"
@@ -60,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
-#include "third_party/blink/public/mojom/app_banner/app_banner.mojom.h"
 #include "third_party/blink/public/platform/file_path_conversion.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
@@ -420,27 +418,6 @@ void BlinkTestRunner::SetPermission(const std::string& name,
 
 void BlinkTestRunner::ResetPermissions() {
   GetWebTestClientRemote()->ResetPermissions();
-}
-
-void BlinkTestRunner::DispatchBeforeInstallPromptEvent(
-    const std::vector<std::string>& event_platforms,
-    base::OnceCallback<void(bool)> callback) {
-  app_banner_service_.reset(new AppBannerService());
-  web_view_test_proxy_->GetMainRenderFrame()->BindLocalInterface(
-      blink::mojom::AppBannerController::Name_,
-      app_banner_service_->controller()
-          .BindNewPipeAndPassReceiver()
-          .PassPipe());
-  app_banner_service_->SendBannerPromptRequest(event_platforms,
-                                               std::move(callback));
-}
-
-void BlinkTestRunner::ResolveBeforeInstallPromptPromise(
-    const std::string& platform) {
-  if (app_banner_service_) {
-    app_banner_service_->ResolvePromise(platform);
-    app_banner_service_.reset(nullptr);
-  }
 }
 
 void BlinkTestRunner::SetScreenOrientationChanged() {
--- content/shell/renderer/web_test/test_runner.cc
+++ content/shell/renderer/web_test/test_runner.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/use_zoom_for_dsf_policy.h"
 #include "content/shell/common/web_test/web_test_constants.h"
 #include "content/shell/common/web_test/web_test_string_util.h"
+#include "content/shell/renderer/web_test/app_banner_service.h"
 #include "content/shell/renderer/web_test/blink_test_helpers.h"
 #include "content/shell/renderer/web_test/blink_test_runner.h"
 #include "content/shell/renderer/web_test/layout_dump.h"
@@ -43,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/object_template_builder.h"
 #include "gin/wrappable.h"
 #include "services/network/public/mojom/cors.mojom.h"
+#include "third_party/blink/public/mojom/app_banner/app_banner.mojom.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 #include "third_party/blink/public/platform/web_data.h"
 #include "third_party/blink/public/platform/web_url_response.h"
@@ -300,6 +302,7 @@ class TestRunnerBindings : public gin::Wrappable<TestRunnerBindings> {
   RenderFrame* const frame_;
   SpellCheckClient* const spell_check_;
   TestPreferences prefs_;
+  std::unique_ptr<AppBannerService> app_banner_service_;
 
   DISALLOW_COPY_AND_ASSIGN(TestRunnerBindings);
 };
@@ -425,6 +428,7 @@ gin::ObjectTemplateBuilder TestRunnerBindings::GetObjectTemplateBuilder(
                  &TestRunnerBindings::DisableMockScreenOrientation)
       .SetMethod("setDisallowedSubresourcePathSuffixes",
                  &TestRunnerBindings::SetDisallowedSubresourcePathSuffixes)
+      // Causes the beforeinstallprompt event to be sent to the renderer.
       .SetMethod("dispatchBeforeInstallPromptEvent",
                  &TestRunnerBindings::DispatchBeforeInstallPromptEvent)
       .SetMethod("dumpAsMarkup", &TestRunnerBindings::DumpAsMarkup)
@@ -1465,19 +1469,55 @@ void TestRunnerBindings::SetPermission(const std::string& name,
                                 GURL(embedding_origin));
 }
 
+static void DispatchBeforeInstallPromptCallback(
+    base::WeakPtr<TestRunner> test_runner,
+    TestRunnerForSpecificView* view_runner,
+    RenderFrame* frame,
+    v8::UniquePersistent<v8::Function> blink_callback,
+    bool cancelled) {
+  if (!test_runner)
+    return;
+
+  v8::Isolate* isolate = blink::MainThreadIsolate();
+  v8::HandleScope handle_scope(isolate);
+
+  v8::Local<v8::Context> context =
+      frame->GetWebFrame()->MainWorldScriptContext();
+  if (context.IsEmpty())
+    return;
+
+  v8::Context::Scope context_scope(context);
+  v8::Local<v8::Value> arg;
+  arg = v8::Boolean::New(isolate, cancelled);
+
+  view_runner->PostV8CallbackWithArgs(std::move(blink_callback), 1, &arg);
+}
+
 void TestRunnerBindings::DispatchBeforeInstallPromptEvent(
     const std::vector<std::string>& event_platforms,
     v8::Local<v8::Function> callback) {
-  return view_runner_->DispatchBeforeInstallPromptEvent(event_platforms,
-                                                        callback);
+  app_banner_service_ = std::make_unique<AppBannerService>();
+  frame_->BindLocalInterface(blink::mojom::AppBannerController::Name_,
+                             app_banner_service_->controller()
+                                 .BindNewPipeAndPassReceiver()
+                                 .PassPipe());
+
+  auto blink_callback = v8::UniquePersistent<v8::Function>(
+      blink::MainThreadIsolate(), std::move(callback));
+  auto reply_callback =
+      base::BindOnce(&DispatchBeforeInstallPromptCallback, runner_,
+                     view_runner_, frame_, std::move(blink_callback));
+
+  app_banner_service_->SendBannerPromptRequest(event_platforms,
+                                               std::move(reply_callback));
 }
 
 void TestRunnerBindings::ResolveBeforeInstallPromptPromise(
     const std::string& platform) {
-  if (!runner_)
-    return;
-
-  runner_->ResolveBeforeInstallPromptPromise(platform);
+  if (app_banner_service_) {
+    app_banner_service_->ResolvePromise(platform);
+    app_banner_service_.reset();
+  }
 }
 
 void TestRunnerBindings::RunIdleTasks(v8::Local<v8::Function> callback) {
@@ -2546,11 +2586,6 @@ void TestRunner::SetPermission(const std::string& name,
                                const GURL& origin,
                                const GURL& embedding_origin) {
   blink_test_runner_->SetPermission(name, value, origin, embedding_origin);
-}
-
-void TestRunner::ResolveBeforeInstallPromptPromise(
-    const std::string& platform) {
-  blink_test_runner_->ResolveBeforeInstallPromptPromise(platform);
 }
 
 void TestRunner::SetPOSIXLocale(const std::string& locale) {
