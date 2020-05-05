8242facc0698be277d609c26604f075385884be7
edchin <edchin@chromium.org>
2020-05-05 15:46:06 +0000

    [ios] Add pointer interaction to NTP omnibox
    iOS 13.4 introduces new APIs for mouse cursor support on iPad.
    
    This CL enables a hover tint effect on the NTP omnibox, and also
    changes the pointer to an I-beam on hover.
    
    Test: iPad only. Enable pointer support flag. Ensure NTP omnibox reacts to pointer hover.
    Bug: 1073961
    Change-Id: Ie72b07e9d3e3521adbf7c0da4c65d7559758a937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181139
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765564}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_view_controller.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/ntp/new_tab_page_header_constants.h"
 #import "ios/chrome/browser/ui/toolbar/public/fakebox_focuser.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_utils.h"
+#include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/named_guide.h"
 #include "ios/chrome/browser/ui/util/ui_util.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
@@ -39,6 +40,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using base::UserMetricsAction;
+
+#if defined(__IPHONE_13_4)
+@interface ContentSuggestionsHeaderViewController (Pointer) <
+    UIPointerInteractionDelegate>
+@end
+#endif  // defined(__IPHONE_13_4)
 
 @interface ContentSuggestionsHeaderViewController () <
     UserAccountImageUpdateDelegate>
@@ -288,6 +295,15 @@ using base::UserMetricsAction;
   [self.fakeOmnibox addSubview:self.accessibilityButton];
   self.accessibilityButton.translatesAutoresizingMaskIntoConstraints = NO;
   AddSameConstraints(self.fakeOmnibox, self.accessibilityButton);
+
+#if defined(__IPHONE_13_4)
+  if (@available(iOS 13.4, *)) {
+    if (base::FeatureList::IsEnabled(kPointerSupport)) {
+      [self.fakeOmnibox
+          addInteraction:[[UIPointerInteraction alloc] initWithDelegate:self]];
+    }
+  }
+#endif  // defined(__IPHONE_13_4)
 
   [self.headerView addViewsToSearchField:self.fakeOmnibox];
 
@@ -576,5 +592,37 @@ using base::UserMetricsAction;
   self.identityDiscButton.imageView.layer.cornerRadius = image.size.width / 2;
   self.identityDiscButton.imageView.layer.masksToBounds = YES;
 }
+
+#if defined(__IPHONE_13_4)
+#pragma mark UIPointerInteractionDelegate
+
+- (UIPointerRegion*)pointerInteraction:(UIPointerInteraction*)interaction
+                      regionForRequest:(UIPointerRegionRequest*)request
+                         defaultRegion:(UIPointerRegion*)defaultRegion
+    API_AVAILABLE(ios(13.4)) {
+  return defaultRegion;
+}
+
+- (UIPointerStyle*)pointerInteraction:(UIPointerInteraction*)interaction
+                       styleForRegion:(UIPointerRegion*)region
+    API_AVAILABLE(ios(13.4)) {
+  UIBezierPath* path = [UIBezierPath
+      bezierPathWithRoundedRect:interaction.view.bounds
+                   cornerRadius:interaction.view.bounds.size.height];
+  UIPreviewParameters* parameters = [[UIPreviewParameters alloc] init];
+  parameters.visiblePath = path;
+  UITargetedPreview* preview =
+      [[UITargetedPreview alloc] initWithView:interaction.view
+                                   parameters:parameters];
+  UIPointerHoverEffect* effect =
+      [UIPointerHoverEffect effectWithPreview:preview];
+  effect.prefersScaledContent = NO;
+  effect.prefersShadow = NO;
+  UIPointerShape* shape = [UIPointerShape
+      beamWithPreferredLength:interaction.view.bounds.size.height / 2
+                         axis:UIAxisVertical];
+  return [UIPointerStyle styleWithEffect:effect shape:shape];
+}
+#endif  // defined(__IPHONE_13_4)
 
 @end
