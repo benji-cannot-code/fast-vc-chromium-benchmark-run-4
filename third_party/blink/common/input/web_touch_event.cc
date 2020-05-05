58ca14be152377256d9c215f7680fc6118bc53a0
Piotr Tworek <ptworek@vewd.com>
2020-05-05 06:06:35 +0000

    Add missing bitset include in web_touch_event.cc.
    The code uses std::bitset, but does not include header declaring it. It
    works when using bundled copy of libcxx, but fails when using system
    libstdc++ on Linux.
    
    Change-Id: I8a88078e110b27623b3cdea38c94012ba4050ee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2179123
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#765415}

--- third_party/blink/common/input/web_touch_event.cc
+++ third_party/blink/common/input/web_touch_event.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <bitset>
+
 #include "third_party/blink/public/common/input/web_touch_event.h"
 
 namespace blink {
