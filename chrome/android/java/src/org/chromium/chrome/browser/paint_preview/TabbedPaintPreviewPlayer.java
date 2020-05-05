665768cd307b0b6d92df93f8fcf55282f810a352
ckitagawa <ckitagawa@chromium.org>
2020-05-05 21:02:47 +0000

    [Paint Preview] Properly destroy PlayerManager
    PlayerManager#destroy was not called in several places leaving the
    native delegate in a leaked state.
    
    Change-Id: I3736fb0e8eb9def13f494724519a89fc39d07a8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181661
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765744}

--- chrome/android/java/src/org/chromium/chrome/browser/paint_preview/TabbedPaintPreviewPlayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/paint_preview/TabbedPaintPreviewPlayer.java
@@ -58,6 +58,7 @@ public class TabbedPaintPreviewPlayer {
         if (mTab == null || mPlayerManager == null) return;
 
         mTab.getContentView().removeView(mPlayerManager.getView());
+        mPlayerManager.destroy();
         mPlayerManager = null;
         mTab = null;
     }
