6be9099a8ce93cb48ba9f78f5f82bd991c4a6e99
Aaron Colwell <acolwell@chromium.org>
2020-05-05 19:15:59 +0000

    Fix the only ShouldUseProcessPerSite() call not using a site URL.
    All calls but one were passing a site URL to
    ShouldUseProcessPerSite(). This change fixes that one call
    so it passes in a site URL. A number of parameter names in
    related functions were also updated to make it clear that
    a site URL is expected to be passed to those functions.
    
    Bug: 1015882
    Change-Id: Ia344debe8237b635cc377bed8200349a4b2c24f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174875
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765693}

--- chrome/browser/search/search_unittest.cc
+++ chrome/browser/search/search_unittest.cc
@@ -127,7 +127,7 @@ TEST_F(SearchTest, ShouldAssignURLToInstantRenderer) {
   }
 }
 
-TEST_F(SearchTest, ShouldUseProcessPerSiteForInstantURL) {
+TEST_F(SearchTest, ShouldUseProcessPerSiteForInstantSiteURL) {
   const SearchTestCase kTestCases[] = {
       {"chrome-search://local-ntp", true, "Local NTP"},
       {"chrome-search://remote-ntp", true, "Remote NTP"},
@@ -146,8 +146,8 @@ TEST_F(SearchTest, ShouldUseProcessPerSiteForInstantURL) {
 
   for (size_t i = 0; i < base::size(kTestCases); ++i) {
     const SearchTestCase& test = kTestCases[i];
-    EXPECT_EQ(test.expected_result,
-              ShouldUseProcessPerSiteForInstantURL(GURL(test.url), profile()))
+    EXPECT_EQ(test.expected_result, ShouldUseProcessPerSiteForInstantSiteURL(
+                                        GURL(test.url), profile()))
         << test.url << " " << test.comment;
   }
 }
