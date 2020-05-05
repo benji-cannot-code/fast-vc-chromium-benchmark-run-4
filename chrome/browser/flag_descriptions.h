309c5e63d8c465b4e398bfd9b3cd4b4aa08ae214
Gang Wu <gangwu@chromium.org>
2020-05-05 17:53:03 +0000

    [Omnibox] Enable Omnibox clipboard suggestions removal by default
    The reason is http://go/cl/309481467
    
    Bug: 977402
    Change-Id: I807a687b07f5f3f2a4633875e2fc7e582d43ce6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176281
    Reviewed-by: Ender <ender@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765636}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -809,9 +809,6 @@ extern const char kOmniboxOnFocusSuggestionsDescription[];
 extern const char kOmniboxOnFocusSuggestionsContextualWebName[];
 extern const char kOmniboxOnFocusSuggestionsContextualWebDescription[];
 
-extern const char kOmniboxRemoveSuggestionsFromClipboardName[];
-extern const char kOmniboxRemoveSuggestionsFromClipboardDescription[];
-
 extern const char kOmniboxShortBookmarkSuggestionsName[];
 extern const char kOmniboxShortBookmarkSuggestionsDescription[];
 
