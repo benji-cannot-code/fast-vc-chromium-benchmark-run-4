0d6717f64757094860e24e0cb916f7b392d28220
Arthur Wang <wuwang@chromium.org>
2020-05-05 03:50:05 +0000

    Add takeScreenshot support to ChromeUiAutomatorTestRule
    Bug: 1060399
    Change-Id: I90c035e1ee543ba2920c0bd9bf97db68e64b6257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181568
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Reviewed-by: Andrew Luo <aluo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765379}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/pagecontroller/rules/ChromeUiAutomatorTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/pagecontroller/rules/ChromeUiAutomatorTestRule.java
@@ -5,11 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.test.pagecontroller.rules;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
 import org.chromium.base.Log;
 import org.chromium.chrome.test.pagecontroller.utils.UiAutomatorUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Custom Rule that logs useful information for debugging UiAutomator
@@ -24,5 +31,19 @@ public class ChromeUiAutomatorTestRule extends TestWatcher {
         Log.e(TAG, description.toString() + " failed", e);
         UiAutomatorUtils utils = UiAutomatorUtils.getInstance();
         utils.printWindowHierarchy("UI hierarchy when " + description.toString() + " failed");
+    }
+
+    /**
+     * Creates a Bitmap from the UiAutomatorUtils (use this for RenderTest).
+     */
+    public Bitmap takeScreenshot(Context context) throws IOException {
+        UiAutomatorUtils utils = UiAutomatorUtils.getInstance();
+
+        File outputDir = context.getCacheDir();
+        File outputFile = File.createTempFile("screenshot", "png", outputDir);
+        outputFile.deleteOnExit();
+
+        utils.takeScreenShot(outputFile);
+        return BitmapFactory.decodeFile(outputFile.getAbsolutePath());
     }
 }
