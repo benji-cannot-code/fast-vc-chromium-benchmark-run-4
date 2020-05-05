349f1110423b9fc171e069671ed77110866c87fc
Sam Bowen <sgbowen@google.com>
2020-05-05 17:51:04 +0000

    Parse associated origins from the schema.org feed
    Bug: 1072702
    Change-Id: I1a204ed2278f60ccc7dfd3c414c496a30436edb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175747
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Sam Bowen <sgbowen@google.com>
    Cr-Commit-Position: refs/heads/master@{#765634}

--- chrome/browser/media/feeds/media_feeds_converter.cc
+++ chrome/browser/media/feeds/media_feeds_converter.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/schema_org/schema_org_entity_names.h"
 #include "components/schema_org/schema_org_enums.h"
 #include "components/schema_org/schema_org_property_names.h"
+#include "url/url_constants.h"
 
 namespace media_feeds {
 
@@ -38,6 +39,7 @@ static int constexpr kMaxRatings = 5;
 static int constexpr kMaxGenres = 3;
 static int constexpr kMaxInteractionStatistics = 3;
 static int constexpr kMaxImages = 5;
+static int constexpr kMaxAssociatedOrigins = 5;
 
 // Gets the property of entity with corresponding name. May be null if not found
 // or if the property has no values.
@@ -380,6 +382,56 @@ bool ValidateProvider(
 
   result->display_name = name->values->string_values[0];
   result->logos = std::move(maybe_images.value());
+
+  return true;
+}
+
+// Gets the associated origin URLs and places a duplicate-free set in the result
+// struct.
+bool GetAssociatedOriginURLs(
+    const Property& property,
+    media_history::MediaHistoryKeyedService::MediaFeedFetchResult* result) {
+  if (property.values->url_values.empty())
+    return false;
+
+  for (const auto& url : property.values->url_values) {
+    // Exceeded the maximum allowed number of associated origins.
+    if (result->associated_origins.size() == kMaxAssociatedOrigins)
+      break;
+
+    if (!url.SchemeIs(url::kHttpsScheme))
+      continue;
+
+    result->associated_origins.insert(url::Origin::Create(url));
+  }
+
+  return true;
+}
+
+// Gets the associated origins information and validates against the spec.
+bool GetAssociatedOrigins(
+    const Property& property,
+    media_history::MediaHistoryKeyedService::MediaFeedFetchResult* result) {
+  if (property.values->entity_values.empty())
+    return false;
+
+  auto& entity = property.values->entity_values[0];
+  if (!entity || entity->type != schema_org::entity::kPropertyValue)
+    return false;
+
+  if (!ValidateProperty(entity.get(), schema_org::property::kName,
+                        base::BindOnce([](const Property& name) {
+                          return !name.values->string_values.empty() &&
+                                 name.values->string_values[0] ==
+                                     "associatedOrigin";
+                        }))) {
+    return false;
+  }
+
+  if (!ConvertProperty(entity.get(), result, schema_org::property::kValue,
+                       false, base::BindOnce(&GetAssociatedOriginURLs))) {
+    return false;
+  }
 
   return true;
 }
@@ -1118,6 +1170,12 @@ bool ConvertMediaFeed(
   auto* provider = GetProperty(entity.get(), schema_org::property::kProvider);
   if (!ValidateProvider(*provider, result))
     return false;
+
+  if (!ConvertProperty(entity.get(), result,
+                       schema_org::property::kAdditionalProperty, false,
+                       base::BindOnce(&GetAssociatedOrigins))) {
+    return false;
+  }
 
   auto data_feed_items = std::find_if(
       entity->properties.begin(), entity->properties.end(),
