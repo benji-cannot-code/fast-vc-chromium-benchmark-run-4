5ac513a320d46e4e3fb3b58f6ce406b6dea20e00
Joe Downing <joedow@google.com>
2020-05-05 21:10:46 +0000

    Adding an 'Echo' message and logic to Chromium FTL signaling classes
    This CL adds a new message which can be used to check whether a CRD
    endpoint is receiving and responding to FTL messages.  In order to make
    this as simple as possible, I didn't want to reuse the existing message
    payloads which may have side-effects or require special processing.
    
    The main use case is for the client to be able to ping the host to see
    if it is online before connecting.  I think we will also use it for
    checking to see if a reconnect is possible after a connection drops.
    
    Change-Id: I8825451bebc41c4219186bf7e54d79e3229d9717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174966
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765750}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/desktop_environment.h"
 #include "remoting/host/desktop_environment_options.h"
 #include "remoting/host/desktop_session_connector.h"
+#include "remoting/host/ftl_echo_message_listener.h"
 #include "remoting/host/ftl_host_change_notification_listener.h"
 #include "remoting/host/ftl_signaling_connector.h"
 #include "remoting/host/heartbeat_sender.h"
@@ -409,6 +410,7 @@ class HostProcess : public ConfigWatcher::Delegate,
   std::unique_ptr<HeartbeatSender> heartbeat_sender_;
   std::unique_ptr<FtlHostChangeNotificationListener>
       ftl_host_change_notification_listener_;
+  std::unique_ptr<FtlEchoMessageListener> ftl_echo_message_listener_;
 
   std::unique_ptr<HostStatusLogger> host_status_logger_;
   std::unique_ptr<HostEventLogger> host_event_logger_;
@@ -1499,6 +1501,9 @@ void HostProcess::StartHost() {
   ftl_host_change_notification_listener_ =
       std::make_unique<FtlHostChangeNotificationListener>(
           this, signal_strategy_.get());
+
+  ftl_echo_message_listener_ = std::make_unique<FtlEchoMessageListener>(
+      host_owner_, signal_strategy_.get());
 
   // Set up reporting the host status notifications.
 #if defined(REMOTING_MULTI_PROCESS)
