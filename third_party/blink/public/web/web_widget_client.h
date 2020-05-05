bbaa77d75c1f469d914f7f3e6ca13ad5b1616c1c
Antonio Gomes <tonikitoo@igalia.com>
2020-05-05 11:44:37 +0000

    Remove RenderWidget::SetHasTouchEventHandlers()
    This blink::WebWidgetClient method had two overrides:
    RenderWidget and one in web_view_tests.cc.
    
    The former got moved down to
    WebFrameWidgetBase::SetEventListenerProperties(), while the
    later was replaced by a blink::mojom::FrameWidgetHost override.
    
    BUG=1071594
    R=dtapuska@chromium.org
    
    Change-Id: Ie605a62e2a3b2ba8fb7fa049bd8246620469f2ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2180943
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765484}

--- third_party/blink/public/web/web_widget_client.h
+++ third_party/blink/public/web/web_widget_client.h
@@ -174,9 +174,6 @@ class WebWidgetClient {
   // Called to update if pointerrawupdate events should be sent.
   virtual void SetHasPointerRawUpdateEventHandlers(bool) {}
 
-  // Called to update if touch events should be sent.
-  virtual void SetHasTouchEventHandlers(bool) {}
-
   // Called to update whether low latency input mode is enabled or not.
   virtual void SetNeedsLowLatencyInput(bool) {}
 
