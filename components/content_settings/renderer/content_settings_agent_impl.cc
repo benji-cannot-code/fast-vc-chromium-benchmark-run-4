1573ccd0cc637999cb4248047459667307c152f8
Hans Wennborg <hans@chromium.org>
2020-05-05 18:23:40 +0000

    De-templatize ContentSettingsAgentImpl::GetContentSettingFromRules
    The template definition was not in the header, so callers from outside
    content_settings_agent_impl.cc could not instantiate the template,
    leading to link errors in some configs (see bug).
    
    Instead, provide overloads for the two types of URL parameter, and
    use a template internally (in the .cc file) as it was before
    crrev.com/759360.
    
    Bug: 1077605
    Change-Id: I5c6f1e60ab694d60f7c20ce77a435a1b03e32e08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181364
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765660}

--- components/content_settings/renderer/content_settings_agent_impl.cc
+++ components/content_settings/renderer/content_settings_agent_impl.cc
@@ -170,7 +170,7 @@ void ContentSettingsAgentImpl::DidBlockContentType(
 }
 
 template <typename URL>
-ContentSetting ContentSettingsAgentImpl::GetContentSettingFromRules(
+ContentSetting GetContentSettingFromRulesImpl(
     const ContentSettingsForOneType& rules,
     const WebFrame* frame,
     const URL& secondary_url) {
@@ -191,6 +191,20 @@ ContentSetting ContentSettingsAgentImpl::GetContentSettingFromRules(
   }
   NOTREACHED();
   return CONTENT_SETTING_DEFAULT;
+}
+
+ContentSetting ContentSettingsAgentImpl::GetContentSettingFromRules(
+    const ContentSettingsForOneType& rules,
+    const WebFrame* frame,
+    const GURL& secondary_url) {
+  return GetContentSettingFromRulesImpl(rules, frame, secondary_url);
+}
+
+ContentSetting ContentSettingsAgentImpl::GetContentSettingFromRules(
+    const ContentSettingsForOneType& rules,
+    const WebFrame* frame,
+    const blink::WebURL& secondary_url) {
+  return GetContentSettingFromRulesImpl(rules, frame, secondary_url);
 }
 
 void ContentSettingsAgentImpl::BindContentSettingsManager(
