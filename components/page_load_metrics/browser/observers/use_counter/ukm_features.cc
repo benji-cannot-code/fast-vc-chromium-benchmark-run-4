9e69603aaa346bdcdada623099fdb32b4e88ae02
Thomas Guilbert <tguilbert@chromium.org>
2020-05-06 23:40:15 +0000

    Add video-rVFC UseCounter
    This CL adds the necessary calls to measure the usage of
    video.requestVideoFrameCallback.
    
    Bug: 1012063
    Change-Id: Ie91b09a4dd129f6ca81c4d007d30514171a42e92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174958
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766210}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -166,6 +166,8 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kTrustTokenFetch,
           WebFeature::kTrustTokenIframe,
           WebFeature::kV8Document_HasTrustToken_Method,
+          WebFeature::kV8HTMLVideoElement_RequestVideoFrameCallback_Method,
+          WebFeature::kV8HTMLVideoElement_CancelVideoFrameCallback_Method,
       }));
   return *opt_in_features;
 }
