5b7c4108108b0834cad4dad4a6c3d1abadc87bb8
nancy <nancylingwang@chromium.org>
2020-05-06 15:57:05 +0000

    Remove ExtensionAppItem.
    ExtensionAppItem is replaced by AppServiceAppItem, so it can be removed.
    
    TBR=ortuno@chromium.org
    
    BUG=1016159
    
    Change-Id: Ia0f6804f353903f8a03851e4ab5ecbacca435d0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2183651
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766012}

--- chrome/browser/ui/app_list/app_list_syncable_service.cc
+++ chrome/browser/ui/app_list/app_list_syncable_service.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ui/app_list/chrome_app_list_item.h"
 #include "chrome/browser/ui/app_list/chrome_app_list_model_updater.h"
-#include "chrome/browser/ui/app_list/extension_app_item.h"
 #include "chrome/browser/ui/app_list/page_break_app_item.h"
 #include "chrome/browser/ui/app_list/page_break_constants.h"
 #include "chrome/common/chrome_features.h"
--- chrome/browser/web_applications/system_web_app_manager_browsertest.cc
+++ chrome/browser/web_applications/system_web_app_manager_browsertest.cc
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/system_features_disable_list_policy_handler.h"
 #include "chrome/browser/ui/app_list/app_list_client_impl.h"
 #include "chrome/browser/ui/app_list/app_list_model_updater.h"
-#include "chrome/browser/ui/app_list/extension_app_item.h"
 #include "chrome/browser/ui/app_list/test/chrome_app_list_test_support.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "components/policy/core/common/policy_pref_names.h"
