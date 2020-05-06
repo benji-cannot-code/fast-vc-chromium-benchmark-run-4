d425e2778c3b36eaf75a12d72c6cea50689fa127
Zach Trudo <zatrudo@google.com>
2020-05-06 20:39:52 +0000

    Make PluginVmUserId a User Policy
    PluginVmUserId should have been a user policy not a device policy.
    
    File changes:
    ~ chrome/browser/chromeos/
    | ~ plugin_vm/
    | | ~ plugin_vm_pref_names.cc/h
    | | | Add PluginVmUserId pref name. Alphabetically sorted pref names.
    | | | Move pref descriptions to header.
    | | ~ plugin_vm_util.cc/h
    | | | Update GetPluginVmUserId, IsPluginVmAllowedForProfile.
    | ~ settings/device_settings_provider.cc
    | | No longer set the value for the PluginVmUserId.
    ~ chrome/browser/policy/configuration_policy_handler_list_factory.cc
    | Add key::kPluginUserVmId -> plugin_vm::prefs::kPluginVmUserId mapping
    ~ chrome/test/data/policy/policy_test_cases.json
    | Move test to a user policy test.
    ~ chromeos/settings/cros_settings_names.cc/h
    | Remove kPluginVmImageId setting.
    ~ components/policy/proto/chrome_device_policy.proto
    | Remove protos related to PluginVmUserId
    ~ components/policy/resources/policy_templates.json
    | Remove Device only flag for PluginVmUserId
    
    
    Bug: b:153882286
    Change-Id: I60e51482be3119e61993e20ae6e5e1b31fd33791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176695
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Olya Kalitova <okalitova@chromium.org>
    Commit-Queue: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#766140}

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -108,7 +108,6 @@ const char* const kKnownSettings[] = {
     kMinimumChromeVersionEnforced,
     kPluginVmAllowed,
     kPluginVmLicenseKey,
-    kPluginVmUserId,
     kPolicyMissingMitigationMode,
     kRebootOnShutdown,
     kReleaseChannel,
@@ -837,14 +836,6 @@ void DecodeGenericPolicies(const em::ChromeDeviceSettingsProto& policy,
     if (container.has_plugin_vm_license_key()) {
       new_values_cache->SetValue(
           kPluginVmLicenseKey, base::Value(container.plugin_vm_license_key()));
-    }
-  }
-
-  if (policy.has_plugin_vm_user_id()) {
-    const em::PluginVmUserIdProto& container(policy.plugin_vm_user_id());
-    if (container.has_plugin_vm_user_id()) {
-      new_values_cache->SetValue(kPluginVmUserId,
-                                 base::Value(container.plugin_vm_user_id()));
     }
   }
 
