b0aa64a654f5cfa2ba35afc7e96459e9ff10c5be
Harald Alvestrand <hta@chromium.org>
2020-05-06 18:10:18 +0000

    Guard MediaStreamAudioDestinationNode() against null context
    Adds test.
    Drive-by: DCHECK that context is valid for MediaStream; have not
    yet found an execution path that will invalidate EC here.
    
    Bug: chromium:1076744
    Change-Id: I665ac0a5df5be1f9082db3a825f17059aa1358f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2184234
    Commit-Queue: Harald Alvestrand <hta@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766066}

--- third_party/blink/renderer/modules/mediastream/media_stream.cc
+++ third_party/blink/renderer/modules/mediastream/media_stream.cc
@@ -57,11 +57,13 @@ MediaStream* MediaStream::Create(ExecutionContext* context) {
   MediaStreamTrackVector audio_tracks;
   MediaStreamTrackVector video_tracks;
 
+  DCHECK(context);
   return MakeGarbageCollected<MediaStream>(context, audio_tracks, video_tracks);
 }
 
 MediaStream* MediaStream::Create(ExecutionContext* context,
                                  MediaStream* stream) {
+  DCHECK(context);
   DCHECK(stream);
 
   MediaStreamTrackVector audio_tracks;
@@ -81,6 +83,7 @@ MediaStream* MediaStream::Create(ExecutionContext* context,
   MediaStreamTrackVector audio_tracks;
   MediaStreamTrackVector video_tracks;
 
+  DCHECK(context);
   for (MediaStreamTrack* track : tracks) {
     ProcessTrack(track, track->kind() == "audio" ? audio_tracks : video_tracks);
   }
