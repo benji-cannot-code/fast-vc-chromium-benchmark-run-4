df8067fefaffacf0cff6fffcbc780d1a35754c83
Richard Knoll <knollr@chromium.org>
2020-05-06 19:37:53 +0000

    [Nearby] NearbyConnections interface skeleton
    This adds an empty NearbyConnections mojo interface and all the wiring
    to get an instance of it running in a sandboxed process. Actual methods
    will be added in future CLs.
    
    Bug: 1073803
    Change-Id: I615f724ae5c02ed6e3ef5ff3073bfe44bbd54f75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2182655
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766105}

---
+++ chrome/services/sharing/nearby/nearby_connections.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/sharing/nearby/nearby_connections.h"
+
+namespace location {
+namespace nearby {
+namespace connections {
+
+NearbyConnections::NearbyConnections(
+    mojo::PendingReceiver<mojom::NearbyConnections> nearby_connections,
+    mojo::PendingRemote<mojom::NearbyConnectionsHost> host,
+    base::OnceClosure on_disconnect)
+    : nearby_connections_(this, std::move(nearby_connections)),
+      host_(std::move(host)),
+      on_disconnect_(std::move(on_disconnect)) {
+  nearby_connections_.set_disconnect_handler(base::BindOnce(
+      &NearbyConnections::OnDisconnect, weak_ptr_factory_.GetWeakPtr()));
+  host_.set_disconnect_handler(base::BindOnce(&NearbyConnections::OnDisconnect,
+                                              weak_ptr_factory_.GetWeakPtr()));
+}
+
+NearbyConnections::~NearbyConnections() = default;
+
+void NearbyConnections::OnDisconnect() {
+  if (on_disconnect_)
+    std::move(on_disconnect_).Run();
+  // Note: |this| might be destroyed here.
+}
+
+}  // namespace connections
+}  // namespace nearby
+}  // namespace location
--- chrome/services/sharing/sharing_impl.cc
+++ chrome/services/sharing/sharing_impl.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/services/sharing/sharing_impl.h"
 
 #include "base/callback.h"
+#include "chrome/services/sharing/nearby/nearby_connections.h"
 #include "chrome/services/sharing/webrtc/sharing_webrtc_connection.h"
 #include "jingle/glue/thread_wrapper.h"
 #include "third_party/webrtc/api/peer_connection_interface.h"
@@ -40,6 +41,24 @@ void SharingImpl::CreateSharingWebRtcConnection(
   SharingWebRtcConnection* sharing_connection_ptr = sharing_connection.get();
   sharing_webrtc_connections_.emplace(sharing_connection_ptr,
                                       std::move(sharing_connection));
+}
+
+void SharingImpl::CreateNearbyConnections(
+    mojo::PendingRemote<NearbyConnectionsHostMojom> host,
+    CreateNearbyConnectionsCallback callback) {
+  // Reset old instance of Nearby Connections stack.
+  nearby_connections_.reset();
+
+  mojo::PendingRemote<NearbyConnectionsMojom> remote;
+  nearby_connections_ = std::make_unique<NearbyConnections>(
+      remote.InitWithNewPipeAndPassReceiver(), std::move(host),
+      base::BindOnce(&SharingImpl::NearbyConnectionsDisconnected,
+                     weak_ptr_factory_.GetWeakPtr()));
+  std::move(callback).Run(std::move(remote));
+}
+
+void SharingImpl::NearbyConnectionsDisconnected() {
+  nearby_connections_.reset();
 }
 
 size_t SharingImpl::GetWebRtcConnectionCountForTesting() const {
