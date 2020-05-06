6726ace3024fda40b24179110d28956f03db2810
Eero Häkkinen <eero.hakkinen@intel.com>
2020-05-06 12:53:00 +0000

    [ImageCapture] Implement platform support for Windows MediaFoundation backend
    Image capture functionality is already present on Windows in DirectShow
    backend and on Linux and CrOS. This CL adds the platform support also to
    the MediaFoundation backend for Windows.
    
    Spec: https://w3c.github.io/mediacapture-image/
    
    Bug: 1049100
    Change-Id: I3712ec76efec7fa4ecd40da3661bf0fa40966cdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162745
    Reviewed-by: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Eero Häkkinen <eero.hakkinen@intel.com>
    Cr-Commit-Position: refs/heads/master@{#765958}

--- media/capture/video/win/video_capture_device_mf_win.h
+++ media/capture/video/win/video_capture_device_mf_win.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mfidl.h>
 #include <mfreadwrite.h>
 #include <stdint.h>
+#include <strmif.h>
 #include <wrl/client.h>
 
 #include <vector>
@@ -132,12 +133,18 @@ class CAPTURE_EXPORT VideoCaptureDeviceMFWin : public VideoCaptureDevice {
 
   std::unique_ptr<VideoCaptureDevice::Client> client_;
   const Microsoft::WRL::ComPtr<IMFMediaSource> source_;
+  Microsoft::WRL::ComPtr<IAMCameraControl> camera_control_;
+  Microsoft::WRL::ComPtr<IAMVideoProcAmp> video_control_;
   Microsoft::WRL::ComPtr<IMFCaptureEngine> engine_;
   std::unique_ptr<CapabilityWin> selected_video_capability_;
   CapabilityList photo_capabilities_;
   std::unique_ptr<CapabilityWin> selected_photo_capability_;
   bool is_started_;
   bool has_sent_on_started_to_client_;
+  // These flags keep the manual/auto mode between cycles of SetPhotoOptions().
+  bool exposure_mode_manual_;
+  bool focus_mode_manual_;
+  bool white_balance_mode_manual_;
   base::queue<TakePhotoCallback> video_stream_take_photo_callbacks_;
 
   SEQUENCE_CHECKER(sequence_checker_);
