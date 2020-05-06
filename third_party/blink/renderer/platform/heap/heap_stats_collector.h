219fee5f63e1f95fc2af90123aec76a42627d75a
Michael Lippautz <mlippautz@chromium.org>
2020-05-06 13:45:16 +0000

    heap: Remove support for stand-alone GCs when attached v8::Isolate
    When ThreadState is attached to a v8::Isolate all garbage collections
    should use unified heap. This has long been the case for production code
    but only recently changed for all testing codepaths as well.
    
    Bug: chromium:1074061
    Change-Id: Ie6183f6f18fa52c59e1a874a2d25131fe436b117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2179484
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765974}

--- third_party/blink/renderer/platform/heap/heap_stats_collector.h
+++ third_party/blink/renderer/platform/heap/heap_stats_collector.h
@@ -62,7 +62,6 @@ class PLATFORM_EXPORT ThreadHeapStatsObserver {
   V(MarkWeakProcessing)               \
   V(UnifiedMarkingStep)               \
   V(VisitCrossThreadPersistents)      \
-  V(VisitDOMWrappers)                 \
   V(VisitPersistentRoots)             \
   V(VisitPersistents)                 \
   V(VisitRoots)                       \
