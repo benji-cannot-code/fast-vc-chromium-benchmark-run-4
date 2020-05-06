42645e550a6cfe5609fa5a28d3ff76bf47d58baa
Kyle Horimoto <khorimoto@chromium.org>
2020-05-06 00:14:12 +0000

    [CrOS Settings] Add AppsSection search tags
    Generated by running a script to parse go/cros-settings-search-tags.
    This CL also migrates handlers for this section.
    
    Bug: 1076268
    Change-Id: I65d5b6dae7578d20b1335de2f57bf45df5e29df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181079
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765820}

--- chrome/browser/ui/webui/settings/chromeos/os_settings_ui.cc
+++ chrome/browser/ui/webui/settings/chromeos/os_settings_ui.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/browser_lifetime_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/accessibility_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/account_manager_handler.h"
-#include "chrome/browser/ui/webui/settings/chromeos/android_apps_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/cups_printers_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/date_time_handler.h"
 #include "chrome/browser/ui/webui/settings/chromeos/device_storage_handler.h"
@@ -226,8 +225,6 @@ void OSSettingsUI::InitOSWebUIHandlers(content::WebUIDataSource* html_source) {
 
   web_ui()->AddMessageHandler(
       std::make_unique<chromeos::settings::AccessibilityHandler>(profile));
-  web_ui()->AddMessageHandler(
-      std::make_unique<chromeos::settings::AndroidAppsHandler>(profile));
   web_ui()->AddMessageHandler(
       chromeos::settings::CupsPrintersHandler::Create(web_ui()));
   web_ui()->AddMessageHandler(base::WrapUnique(
