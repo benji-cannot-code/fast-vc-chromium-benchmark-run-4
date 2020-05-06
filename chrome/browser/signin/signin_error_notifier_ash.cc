8713e90003520d27dcd9777dc1133de5972d0b07
Roman Sorokin <rsorokin@chromium.org>
2020-05-06 06:36:09 +0000

    cros: Do not use SigninProfile for TokenHandleUtil in the session
    Leverage user profile instead for the url loader factory
    
    Fixed: 1032516
    Change-Id: Ia14f91c795dd5ea00ab53d5931c8a80f970e5006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2173177
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#765897}

--- chrome/browser/signin/signin_error_notifier_ash.cc
+++ chrome/browser/signin/signin_error_notifier_ash.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/chromeos/account_manager/account_manager_util.h"
+#include "chrome/browser/chromeos/login/reauth_stats.h"
 #include "chrome/browser/chromeos/login/user_flow.h"
 #include "chrome/browser/chromeos/login/users/chrome_user_manager.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
@@ -93,8 +94,9 @@ SigninErrorNotifier::SigninErrorNotifier(SigninErrorController* controller,
   if (TokenHandleUtil::HasToken(account_id)) {
     token_handle_util_ = std::make_unique<TokenHandleUtil>();
     token_handle_util_->CheckToken(
-        account_id, base::Bind(&SigninErrorNotifier::OnTokenHandleCheck,
-                               weak_factory_.GetWeakPtr()));
+        account_id, profile->GetURLLoaderFactory(),
+        base::Bind(&SigninErrorNotifier::OnTokenHandleCheck,
+                   weak_factory_.GetWeakPtr()));
   }
   OnErrorChanged();
 }
@@ -104,6 +106,7 @@ void SigninErrorNotifier::OnTokenHandleCheck(
     TokenHandleUtil::TokenHandleStatus status) {
   if (status != TokenHandleUtil::INVALID)
     return;
+  RecordReauthReason(account_id, chromeos::ReauthReason::INVALID_TOKEN_HANDLE);
   HandleDeviceAccountError();
 }
 
@@ -138,9 +141,12 @@ void SigninErrorNotifier::OnErrorChanged() {
       return;
   }
 
+  const AccountId account_id =
+      multi_user_util::GetAccountIdFromProfile(profile_);
   if (!chromeos::IsAccountManagerAvailable(profile_)) {
     // If this flag is disabled, Chrome OS does not have a concept of Secondary
     // Accounts. Preserve existing behavior.
+    RecordReauthReason(account_id, chromeos::ReauthReason::SYNC_FAILED);
     HandleDeviceAccountError();
     return;
   }
@@ -150,6 +156,7 @@ void SigninErrorNotifier::OnErrorChanged() {
       identity_manager_->GetPrimaryAccountId(
           signin::ConsentLevel::kNotRequired);
   if (error_account_id == primary_account_id) {
+    RecordReauthReason(account_id, chromeos::ReauthReason::SYNC_FAILED);
     HandleDeviceAccountError();
   } else {
     HandleSecondaryAccountError(error_account_id);
