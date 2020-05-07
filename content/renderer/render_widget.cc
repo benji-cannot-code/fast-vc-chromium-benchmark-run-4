f12c77e356f05c1abca37f1805b43ae3a83faf10
Abhijeet Kandalkar <abhijeet@igalia.com>
2020-05-07 15:20:32 +0000

    Convert WidgetMsg_UpdateRenderThrottlingStatus to FrameWidget mojom
    This CL defines frame-widget-specific methods that will be invoked from
    the browser process to toggle render throttling for an out-of-process
    iframe.
    
    Bug: 1071586
    Change-Id: Iaedb484cd16268c6ca7fc8d479915797b9396225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162430
    Commit-Queue: Abhijeet Kandalkar <abhijeet@igalia.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766416}

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -575,8 +575,6 @@ bool RenderWidget::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER(WidgetMsg_SetViewportIntersection,
                         OnSetViewportIntersection)
     IPC_MESSAGE_HANDLER(WidgetMsg_SetIsInert, OnSetIsInert)
-    IPC_MESSAGE_HANDLER(WidgetMsg_UpdateRenderThrottlingStatus,
-                        OnUpdateRenderThrottlingStatus)
     IPC_MESSAGE_HANDLER(WidgetMsg_WaitForNextFrameForTests,
                         OnWaitNextFrameForTests)
     IPC_MESSAGE_HANDLER(DragMsg_TargetDragEnter, OnDragTargetDragEnter)
@@ -2108,12 +2106,6 @@ void RenderWidget::OnSetViewportIntersection(
 void RenderWidget::OnSetIsInert(bool inert) {
   if (auto* frame_widget = GetFrameWidget())
     frame_widget->SetIsInert(inert);
-}
-
-void RenderWidget::OnUpdateRenderThrottlingStatus(bool is_throttled,
-                                                  bool subtree_throttled) {
-  if (auto* frame_widget = GetFrameWidget())
-    frame_widget->UpdateRenderThrottlingStatus(is_throttled, subtree_throttled);
 }
 
 void RenderWidget::OnDragTargetDragEnter(
