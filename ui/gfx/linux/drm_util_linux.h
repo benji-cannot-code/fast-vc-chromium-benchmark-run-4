d4e12429e66272fb98545e2063f421ba40296dbc
Stephan Hartmann <stha09@googlemail.com>
2020-05-07 16:08:25 +0000

    IWYU: uint32_t is defined in cstdint
    This is required for gcc-10.
    
    Change-Id: I6659a14f885011941887702171f6e49b8740f049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2174486
    Reviewed-by: Michael Spang <spang@chromium.org>
    Commit-Queue: Robert Kroeger <rjkroege@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766427}

--- ui/gfx/linux/drm_util_linux.h
+++ ui/gfx/linux/drm_util_linux.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_GFX_LINUX_DRM_UTIL_LINUX_H_
 #define UI_GFX_LINUX_DRM_UTIL_LINUX_H_
 
+#include <cstdint>
+
 #include "ui/gfx/buffer_types.h"
 
 namespace ui {
