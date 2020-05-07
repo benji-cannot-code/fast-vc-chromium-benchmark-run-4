3c85a020e74b07c8328eea93b68d7972486203d2
Matthias Körber <koerber@google.com>
2020-05-07 14:32:47 +0000

    [Autofill] Translate the country from a form using the page language.
    Currently, autofill assumes that the language of country names found in
    a form that is imported to an address profile machtes the app_locale.
    
    With this change, autofill also tries to use the page language to
    understand localized country name in forms to import addresses into new
    profiles.
    
    Change-Id: I409c08633d82e340bd9cc4a776aae0d571264c03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165895
    Commit-Queue: Matthias Körber <koerber@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766400}

--- components/autofill/core/browser/test_autofill_client.h
+++ components/autofill/core/browser/test_autofill_client.h
@@ -49,6 +49,7 @@ class TestAutofillClient : public AutofillClient {
   ukm::SourceId GetUkmSourceId() override;
   AddressNormalizer* GetAddressNormalizer() override;
   security_state::SecurityLevel GetSecurityLevelForUmaHistograms() override;
+  std::string GetPageLanguage() const override;
 #if !defined(OS_ANDROID) && !defined(OS_IOS)
   std::vector<std::string> GetMerchantWhitelistForVirtualCards() override;
   std::vector<std::string> GetBinRangeWhitelistForVirtualCards() override;
@@ -158,6 +159,11 @@ class TestAutofillClient : public AutofillClient {
     payments_client_ = std::move(payments_client);
   }
 
+  // Sets the page language that is retrieved by |GetPageLanguage()|.
+  void set_page_language(std::string page_language) {
+    page_language_ = page_language;
+  }
+
   void set_test_form_data_importer(
       std::unique_ptr<TestFormDataImporter> form_data_importer) {
     form_data_importer_ = std::move(form_data_importer);
@@ -248,6 +254,9 @@ class TestAutofillClient : public AutofillClient {
   base::Optional<SaveCreditCardOptions> save_credit_card_options_;
 
   std::vector<std::string> migration_card_selection_;
+
+  // The language that is returned by |GetPageLanguage()|.
+  std::string page_language_;
 
 #if !defined(OS_ANDROID) && !defined(OS_IOS)
   std::vector<std::string> merchant_whitelist_;
