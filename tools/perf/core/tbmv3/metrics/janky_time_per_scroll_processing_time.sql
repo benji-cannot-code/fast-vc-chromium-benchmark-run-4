9c9931e730b6e3af63a778ae75a955d26a77b1ab
Stephen Nusko <nuskos@chromium.org>
2020-05-07 17:15:26 +0000

    Fix janky_time metric by remove extra brackets.
    Somehow I missed this and added extra brackets. This fixes the syntax issue.
    
    Change-Id: I364267b21fa01168cbeb0f63d4270a4b97794d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2187173
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766454}

--- tools/perf/core/tbmv3/metrics/janky_time_per_scroll_processing_time.sql
+++ tools/perf/core/tbmv3/metrics/janky_time_per_scroll_processing_time.sql
@@ -218,7 +218,7 @@ CREATE TABLE ScrollJanksMaybeNull AS
       currGestureScrollId != prevGestureScrollId OR
       prevTs IS NULL OR
       prevTs < currScrollBeginTs OR
-      prevTs > currScrollEndTs))
+      prevTs > currScrollEndTs
     THEN
       0 ELSE
       currScrollFramesExact > prevScrollFramesExact + 0.5
