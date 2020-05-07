1a4337725fed6ebaa94d227a4fb72ab42abc4b48
Marijn Kruisselbrink <mek@chromium.org>
2020-05-07 21:07:58 +0000

    Revert "DevTools: add tests for extensions on DOM UI pages"
    This reverts commit 3c0f2556708b39f7cb223ea33306a7fbb10ca01f.
    
    Reason for revert: test is extremely flaky
    
    Per https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyUgsSBUZsYWtlIkdjaHJvbWl1bUBicm93c2VyX3Rlc3RzQERldlRvb2xzRXh0ZW5zaW9uVGVzdC5UZXN0RXZhbHVhdGVPbkNocm9tZVNjaGVtZQw this flakily fails about 10 times an hour.
    
    Original change's description:
    > DevTools: add tests for extensions on DOM UI pages
    > 
    > This is the chrome-side counterpart of
    > https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2128732
    > 
    > Bug: 1059577, 795595
    > Change-Id: Iec2ee772a42b4c7bc2249627c0839f7506f0cd1d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2129344
    > Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#756375}
    
    TBR=dgozman@chromium.org,rdevlin.cronin@chromium.org,caseq@chromium.org,bmeurer@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1059577, 795595
    Change-Id: I2919088167b064086b315d8c3a64df569d95c844
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2188512
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766575}

--- chrome/browser/devtools/devtools_sanity_browsertest.cc
+++ chrome/browser/devtools/devtools_sanity_browsertest.cc
@@ -247,7 +247,7 @@ class DevToolsSanityTest : public InProcessBrowserTest {
   }
 
   void LoadTestPage(const std::string& test_page) {
-    GURL url(spawned_test_server()->GetURL("").Resolve(test_page));
+    GURL url = spawned_test_server()->GetURL(test_page);
     ui_test_utils::NavigateToURL(browser(), url);
   }
 
@@ -1501,11 +1501,6 @@ IN_PROC_BROWSER_TEST_F(DevToolsExtensionTest,
                        TestConsoleContextNames) {
   LoadExtension("simple_content_script");
   RunTest("testConsoleContextNames", kPageWithContentScript);
-}
-
-IN_PROC_BROWSER_TEST_F(DevToolsExtensionTest, TestEvaluateOnChromeScheme) {
-  LoadExtension("chrome_scheme");
-  RunTest("waitForTestResultsAsMessage", std::string());
 }
 
 // Tests that scripts are not duplicated after Scripts Panel switch.
