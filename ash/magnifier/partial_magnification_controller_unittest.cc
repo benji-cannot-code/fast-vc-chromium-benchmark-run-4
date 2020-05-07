40dc1fd9ee7797f9db1c8e3d55278d5b8f208514
Xiyuan Xia <xiyuan@chromium.org>
2020-05-07 22:12:12 +0000

    ash: Magnifier for movable partial screenshot
    - Refactor PartialMagnificationController to extract magnifier
      glass widget code into a MagnifierGlass class;
    - Use MagnifierGlass for movable partial screenshot;
    - Fix pseudo cursor off when it is on the left/top of the border;
    
    Bug: 1076605
    Change-Id: I70c4cdc06e8a59b949a5c9dfb51e9a87134b4369
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2185436
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766603}

--- ash/magnifier/partial_magnification_controller_unittest.cc
+++ ash/magnifier/partial_magnification_controller_unittest.cc
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/magnifier/magnification_controller.h"
 #include "ash/magnifier/partial_magnification_controller.h"
+
+#include "ash/magnifier/magnification_controller.h"
+#include "ash/magnifier/magnifier_glass.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "ui/display/manager/display_manager.h"
@@ -24,7 +26,9 @@ class PartialMagnificationControllerTestApi {
 
   bool is_enabled() const { return controller_->is_enabled_; }
   bool is_active() const { return controller_->is_active_; }
-  views::Widget* host_widget() const { return controller_->host_widget_; }
+  views::Widget* host_widget() const {
+    return controller_->magnifier_glass_->host_widget_;
+  }
 
   gfx::Point GetWidgetOrigin() const {
     return host_widget()->GetWindowBoundsInScreen().origin();
