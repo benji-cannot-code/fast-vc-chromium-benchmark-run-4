de401ca10521241d5aa295f535e7ff6f792ff793
evliu <evliu@google.com>
2020-05-07 17:24:11 +0000

    Update the Speech Recognition sandbox to accommodate Cloud Speech-to-Text
    The Speech On-Device API (SODA) will not be available on Windows until
    later in the year, so the Live Caption feature will initially launch
    using the Cloud Speech-to-Text API. This CL updates the sandbox of the
    speech recognition service to use the kUtility sandbox in the event
    SODA is not available, as the Cloud Speech-to-Text implementation will
    not require the permissions allowed by the kSpeechRecognition sandbox
    type.
    
    Bug: 1072846
    Change-Id: I7b0ce61850115b476f1f38e81790a0b205ec56c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2157997
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#766458}

--- chrome/browser/speech/speech_recognition_service.cc
+++ chrome/browser/speech/speech_recognition_service.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/speech/speech_recognition_service.h"
 
 #include "chrome/grit/generated_resources.h"
+#include "chrome/services/speech/buildflags.h"
 #include "content/public/browser/service_process_host.h"
 
 namespace speech {
@@ -29,7 +30,13 @@ void SpeechRecognitionService::LaunchIfNotRunning() {
       speech_recognition_service_.BindNewPipeAndPassReceiver(),
       content::ServiceProcessHost::Options()
           .WithDisplayName(IDS_UTILITY_PROCESS_SPEECH_RECOGNITION_SERVICE_NAME)
+  // Use the custom speech recognition sandbox type if the Speech On-Device API
+  // is enabled. Otherwise, use the utility sandbox type.
+#if BUILDFLAG(ENABLE_SODA)
           .WithSandboxType(service_manager::SandboxType::kSpeechRecognition)
+#else
+          .WithSandboxType(service_manager::SandboxType::kUtility)
+#endif  // BUILDFLAG(ENABLE_SODA)
           .Pass());
 
   // Ensure that if the interface is ever disconnected (e.g. the service
