e44d40e03a201106179074e19698d7c57b9de9e8
Dylan Cutler <dylancutler@google.com>
2020-05-07 17:46:12 +0000

    Instrument history.length to see if it leaks entropy about users' cross-domain identity.
    Bug: 973801
    Change-Id: I61319729a0fd1938264370ee1359dff75cfcc225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171969
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/master@{#766470}

--- third_party/blink/renderer/core/frame/history.cc
+++ third_party/blink/renderer/core/frame/history.cc
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/history.h"
 
+#include "third_party/blink/public/common/privacy_budget/identifiability_metric_builder.h"
+#include "third_party/blink/public/common/privacy_budget/identifiability_metrics.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/frame_console.h"
@@ -77,7 +79,17 @@ unsigned History::length(ExceptionState& exception_state) const {
         "fully active");
     return 0;
   }
-  return GetFrame()->Client()->BackForwardLength();
+
+  unsigned result = GetFrame()->Client()->BackForwardLength();
+  Document* document = DomWindow()->document();
+  IdentifiabilityMetricBuilder(
+      base::UkmSourceId::FromInt64(document->UkmSourceID()))
+      .Set(IdentifiableSurface::FromTypeAndInput(
+               IdentifiableSurface::Type::kWebFeature,
+               static_cast<uint64_t>(WebFeature::kHistoryLength)),
+           IdentifiabilityDigestHelper(result))
+      .Record(document->UkmRecorder());
+  return result;
 }
 
 ScriptValue History::state(ScriptState* script_state,
