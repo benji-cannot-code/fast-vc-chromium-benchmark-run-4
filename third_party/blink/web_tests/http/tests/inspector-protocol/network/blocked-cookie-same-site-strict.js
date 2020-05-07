dfbd759e5c8bd423d7cc6b7e77a42dc34fa2dfa3
Sigurd Schneider <sigurds@chromium.org>
2020-05-07 12:05:23 +0000

    [devtools] Add structured request cookies to requestWillBeSentExtraInfo
    This allows the devtools front-end to provide better information and
    to support more accurate filtering in the network view.
    
    Previously, cookie information for requests was loaded separately
    from the cookie store, and in some cases this caused the cookie
    information to be out-of-date or inaccurate, because the headers
    only mention the cookie name, but to identify a cookie in the cookie
    store, the triple (name,domain,path) is required.
    
    Additionally, this ensures that full request cookie information is
    present in the request without consulting the back-end again.
    
    Bug: chromium:1027752, chromium:1051163
    Change-Id: I95e1ba7993eaeeebcfdec33e3ff571efb9466473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2183937
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766376}

--- third_party/blink/web_tests/http/tests/inspector-protocol/network/blocked-cookie-same-site-strict.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/blocked-cookie-same-site-strict.js
@@ -17,17 +17,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // navigate to a different domain and back from the browser and see that the cookie is not blocked
   await helper.navigateWithExtraInfo(thirdPartyUrl);
   var {requestExtraInfo, responseExtraInfo} = await helper.navigateWithExtraInfo(firstPartyUrl);
-  testRunner.log(`Browser initiated navigation blocked cookies: ${JSON.stringify(requestExtraInfo.params.blockedCookies, null, 2)}\n`);
+  testRunner.log(`Browser initiated navigation blocked cookies: ${JSON.stringify(requestExtraInfo.params.associatedCookies, null, 2)}\n`);
 
   // navigate to a different domain and back from javascript and see that the cookie is blocked
   await helper.navigateWithExtraInfo(thirdPartyUrl);
   var {requestExtraInfo, responseExtraInfo} = await helper.jsNavigateWithExtraInfo(firstPartyUrl);
-  testRunner.log(`Javascript initiated navigation blocked cookies: ${JSON.stringify(requestExtraInfo.params.blockedCookies, null, 2)}\n`);
+  testRunner.log(`Javascript initiated navigation blocked cookies: ${JSON.stringify(requestExtraInfo.params.associatedCookies, null, 2)}\n`);
 
   // navigate away and make a subresource request from javascript, see that the cookie is blocked
   await helper.navigateWithExtraInfo(thirdPartyUrl);
   await helper.fetchWithExtraInfo(firstPartyUrl);
-  testRunner.log(`Javascript initiated subresource blocked cookies: ${JSON.stringify(requestExtraInfo.params.blockedCookies, null, 2)}`);
+  testRunner.log(`Javascript initiated subresource blocked cookies: ${JSON.stringify(requestExtraInfo.params.associatedCookies, null, 2)}`);
 
   testRunner.completeTest();
 })
