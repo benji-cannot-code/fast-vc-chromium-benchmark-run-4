5e9d2702b4600d4726ce07278782071d391ed450
Mike Wittman <wittman@chromium.org>
2020-05-07 21:12:25 +0000

    [Sampling profiler] Provide DFM function pointers through Java
    Replaces the manual loading of the dynamic feature module native library
    with use of the Java DFM interfaces. Supplies the function pointers for
    the native functionality esposed by the module via JNI.
    
    Bug: 1055945
    Change-Id: I014eda3a1e2a8b2e0277ae66f30fddd393b9e180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176893
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766583}

---
+++ chrome/android/modules/stack_unwinder/internal/java/src/org/chromium/chrome/modules/stack_unwinder/StackUnwinderModuleContentsImpl.java
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.modules.stack_unwinder;
+
+import org.chromium.base.annotations.MainDex;
+import org.chromium.base.annotations.NativeMethods;
+import org.chromium.base.annotations.UsedByReflection;
+
+/**
+ * Provides access to the stack unwinder native code functions within the dynamic feature module.
+ */
+@UsedByReflection("StackUnwinderModule")
+@MainDex
+public class StackUnwinderModuleContentsImpl implements StackUnwinderModuleContents {
+    /**
+     * Returns the pointer to the CreateMemoryRegionsMap native function within the module, encoded
+     * as a long.
+     */
+    @Override
+    public long getCreateMemoryRegionsMapFunction() {
+        return StackUnwinderModuleContentsImplJni.get().getCreateMemoryRegionsMapFunction();
+    }
+
+    /**
+     * Returns the pointer to the CreateNativeUnwinder native function within the module, encoded as
+     * a long.
+     */
+    @Override
+    public long getCreateNativeUnwinderFunction() {
+        return StackUnwinderModuleContentsImplJni.get().getCreateNativeUnwinderFunction();
+    }
+
+    @NativeMethods
+    interface Natives {
+        long getCreateMemoryRegionsMapFunction();
+        long getCreateNativeUnwinderFunction();
+    }
+}
