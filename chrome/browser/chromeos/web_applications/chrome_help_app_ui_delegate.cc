d216328b04accdad4a3c699ee41f64d015de85ae
Rachel Carpenter <carpenterr@chromium.org>
2020-05-07 06:38:03 +0000

    Add a boolean for showing the migration notice.
    This should be shown to all users that are not new, ie they have
    probably seen the previous app.
    
    Also fix hasTouchScreen - the boolean was around the wrong way.
    
    Bug: b/155145109
    Change-Id: I80c4fe0a339636d5ca2e405632a8f10617af5027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181700
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Patti <patricialor@chromium.org>
    Commit-Queue: Rachel Carpenter <carpenterr@google.com>
    Cr-Commit-Position: refs/heads/master@{#766306}

--- chrome/browser/chromeos/web_applications/chrome_help_app_ui_delegate.cc
+++ chrome/browser/chromeos/web_applications/chrome_help_app_ui_delegate.cc
@@ -79,7 +79,7 @@ void ChromeHelpAppUIDelegate::PopulateLoadTimeData(
   // Checks if there are active touch screens.
   source->AddBoolean(
       "hasTouchScreen",
-      ui::DeviceDataManager::GetInstance()->GetTouchscreenDevices().empty());
+      !ui::DeviceDataManager::GetInstance()->GetTouchscreenDevices().empty());
   // Checks if the Google Assistant is allowed on this device by going through
   // policies.
   ash::mojom::AssistantAllowedState assistant_allowed_state =
@@ -102,4 +102,9 @@ void ChromeHelpAppUIDelegate::PopulateLoadTimeData(
   source->AddInteger("userType", user_manager->GetActiveUser()->GetType());
   source->AddBoolean("isEphemeralUser",
                      user_manager->IsCurrentUserNonCryptohomeDataEphemeral());
+
+  // Hardcoding the version number of first 84 dev.
+  bool is_new_in_84 = profile->WasCreatedByVersionOrLater("84.0.4129.0");
+  // Show a notice that the app has changed to users that are not new.
+  source->AddBoolean("shouldShowMigrationNotice", !is_new_in_84);
 }
