3a2b6641afb81f593bc5a0470a2d0e117fb7e451
Eugene Zemtsov <eugene@chromium.org>
2020-05-07 01:47:25 +0000

    Expose video playback roughness metric in medialog
    Also exposing effective fps in medialog.
    
    Bug: 1042111
    Change-Id: I751dce80e6ce2937ab0b141476f68b0d423a761d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2184054
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Ted Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766257}

--- media/base/media_log_properties.cc
+++ media/base/media_log_properties.cc
@@ -33,6 +33,8 @@ std::string MediaLogPropertyKeyToString(MediaLogProperty property) {
     STRINGIFY(kIsPlatformAudioDecoder);
     STRINGIFY(kAudioTracks);
     STRINGIFY(kVideoTracks);
+    STRINGIFY(kFramerate);
+    STRINGIFY(kVideoPlaybackRoughness);
   }
 #undef STRINGIFY
 }
