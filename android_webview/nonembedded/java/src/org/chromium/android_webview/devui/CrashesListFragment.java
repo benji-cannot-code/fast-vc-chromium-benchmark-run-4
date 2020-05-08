e7a416c3c75dd8b9a96faaecc4ed68aff313e80f
Hazem Ashmawy <hazems@google.com>
2020-05-08 04:29:02 +0000

    [AW][DevUI] Add triage steps to crash bug template
    Add go link to bug triage guide to the crash bug template.
    
    Bug: 1072792
    Test: Press report bug button and make sure redirection work after signing in
    Change-Id: Idfcd9b360f32037d60201df678c6ba8a56228ad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2187794
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Auto-Submit: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766723}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/devui/CrashesListFragment.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/devui/CrashesListFragment.java
@@ -90,7 +90,9 @@ public class CrashesListFragment extends DevUiBaseFragment {
             + "\n"
             + "\n"
             + "****DO NOT CHANGE BELOW THIS LINE****\n"
-            + "Crash ID: http://crash/%s\n";
+            + "Crash ID: http://crash/%s\n"
+            + "Instructions for triaging this report (Chromium members only): "
+            + "https://bit.ly/2SM1Y9t\n";
 
     @Override
     public void onAttach(Context context) {
