cd344db532ca12bbfc63a5b8aac175c8ebf10e1a
Paul Moy <pmoy@chromium.org>
2020-05-08 02:37:11 +0000

    cros_healthd: add support for lid events
    Expose the new lid observer through cros_healthd's
    ServiceConnection.
    
    Bug: chromium:1067644
    Change-Id: I64e3e56f89275e887eb8306ebba40e9406e5e744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2188442
    Commit-Queue: Paul Moy <pmoy@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766700}

--- chromeos/dbus/cros_healthd/fake_cros_healthd_client.h
+++ chromeos/dbus/cros_healthd/fake_cros_healthd_client.h
@@ -62,6 +62,9 @@ class COMPONENT_EXPORT(CROS_HEALTHD) FakeCrosHealthdClient
   // observers.
   void EmitAdapterAddedEventForTesting();
 
+  // Calls the lid event OnLidClosed on all registered lid observers.
+  void EmitLidClosedEventForTesting();
+
  private:
   FakeCrosHealthdService fake_service_;
   mojo::Receiver<mojom::CrosHealthdServiceFactory> receiver_{&fake_service_};
--- chromeos/dbus/cros_healthd/fake_cros_healthd_service.h
+++ chromeos/dbus/cros_healthd/fake_cros_healthd_service.h
@@ -86,6 +86,7 @@ class FakeCrosHealthdService final
   // CrosHealthdEventService overrides:
   void AddBluetoothObserver(
       mojom::CrosHealthdBluetoothObserverPtr observer) override;
+  void AddLidObserver(mojom::CrosHealthdLidObserverPtr observer) override;
   void AddPowerObserver(mojom::CrosHealthdPowerObserverPtr observer) override;
 
   // CrosHealthdProbeService overrides:
@@ -118,6 +119,9 @@ class FakeCrosHealthdService final
   // observers.
   void EmitAdapterAddedEventForTesting();
 
+  // Calls the lid event OnLidClosed for all registered lid observers.
+  void EmitLidClosedEventForTesting();
+
  private:
   // Used as the response to any GetAvailableRoutines IPCs received.
   std::vector<mojom::DiagnosticRoutineEnum> available_routines_;
@@ -138,6 +142,8 @@ class FakeCrosHealthdService final
 
   // Collection of registered Bluetooth observers.
   mojo::RemoteSet<mojom::CrosHealthdBluetoothObserver> bluetooth_observers_;
+  // Collection of registered lid observers.
+  mojo::RemoteSet<mojom::CrosHealthdLidObserver> lid_observers_;
   // Collection of registered power observers.
   mojo::RemoteSet<mojom::CrosHealthdPowerObserver> power_observers_;
 
