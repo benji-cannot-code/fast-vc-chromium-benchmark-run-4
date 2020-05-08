7ead907ced369b49447dd7a4b18e68d2c5188bee
Natalie Chouinard <chouinard@chromium.org>
2020-05-08 18:15:05 +0000

    Modularize PrefetchConfiguration
    This is a necessary prerequisite to modularizing DownloadSettings.
    
    Bug: 1047896
    Change-Id: I068d7e641348d1e69accdf6d68a60f2e69ccef0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186466
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766901}

--- chrome/browser/offline_pages/android/prefetch_configuration_impl_android.cc
+++ chrome/browser/offline_pages/android/prefetch_configuration_impl_android.cc
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
-#include "chrome/android/chrome_jni_headers/PrefetchConfiguration_jni.h"
+#include "chrome/browser/offline_pages/android/jni_headers/PrefetchConfiguration_jni.h"
 #include "chrome/browser/profiles/profile_key.h"
 #include "chrome/browser/profiles/profile_key_android.h"
 #include "components/offline_pages/core/offline_page_feature.h"
