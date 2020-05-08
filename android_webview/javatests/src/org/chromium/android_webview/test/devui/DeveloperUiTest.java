0a7efe45854ac45aacbd9c0a08bba14aa296f437
Hazem Ashmawy <hazems@google.com>
2020-05-08 00:44:52 +0000

    [AW][DevUI] Add priority, bug type and OS labels to bug URL
    Add priority, bug type and OS labels to bug URL submitted via the
    three dotted menu so that the bug would appear in WebView bugcop
    teriage queue.
    
    Bug: 1077399
    Test: File a test bug
    Test: run_webview_instrumentation_test_apk
    Change-Id: I40bdcc8fc062769bfa65662c0166b3b24d9ded76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2187634
    Commit-Queue: Hazem Ashmawy <hazems@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Hazem Ashmawy <hazems@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766665}

--- android_webview/javatests/src/org/chromium/android_webview/test/devui/DeveloperUiTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/devui/DeveloperUiTest.java
@@ -162,7 +162,8 @@ public class DeveloperUiTest {
                 IntentMatchers.hasData(hasHost("bugs.chromium.org")),
                 IntentMatchers.hasData(hasPath("/p/chromium/issues/entry")),
                 IntentMatchers.hasData(hasParamWithValue("template", "Webview+Bugs")),
-                IntentMatchers.hasData(hasParamWithValue("labels", "Via-WebView-DevTools"))));
+                IntentMatchers.hasData(hasParamWithValue(
+                        "labels", "Via-WebView-DevTools,Pri-3,Type-Bug,OS-Android"))));
     }
 
     @Test
