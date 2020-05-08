44f506be1eba7f99ec057f3f0f7be0050b50aef5
Regan Hsu <hsuregan@google.com>
2020-05-08 23:41:49 +0000

    [OsSettingsSearchUi] Address and reassign remaining TODOs for 1056909
    * Remove fake data
    * Reassign TODOs for removing old search code to 1080777
    * Add remaining TODO tests.
    
    Fixed: 1056909
    Change-Id: I46c84d275973b2af38297087fe18b42bd1572a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191193
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767049}

--- chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.js
+++ chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.js
@@ -240,7 +240,7 @@ cr.define('settings', function() {
       }
 
       if (loadTimeData.getBoolean('newOsSettingsSearch')) {
-        // TODO(crbug/1056909): Remove when new os settings search complete.
+        // TODO(crbug/1080777): Remove when new os settings search complete.
         // This block prevents the old settings search code from being executed.
         return;
       }
@@ -260,7 +260,7 @@ cr.define('settings', function() {
               toolbar.getSearchField());
 
       if (!searchField) {
-        // TODO(crbug/1056909): Remove this and surrounding code when new os
+        // TODO(crbug/1080777): Remove this and surrounding code when new os
         // settings search complete. If the search field has not been rendered
         // yet, do not continue. crbug/1056909 changes the toolbar search field
         // to an optional value, so the element is not attached to the DOM the
@@ -313,7 +313,7 @@ cr.define('settings', function() {
 
     /**
      * Handles the 'search-changed' event fired from the toolbar.
-     * TODO(crbug/1056909): Remove when new settings search complete.
+     * TODO(crbug/1080777): Remove when new settings search complete.
      * @param {!Event} e
      * @private
      */
