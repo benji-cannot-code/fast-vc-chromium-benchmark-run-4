92a1d9a17939e9c09e3b95c43ba6f3572fc9250f
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-05-08 23:13:14 +0000

    Import wpt@c07eb06124e4fe32a6269b29b2521dad71189f73
    Using wpt-import in Chromium b25c957279f646893f9d269eaf5d708dde42839d.
    With Chromium commits locally applied on WPT:
    59c99fcd65 "Clipboard API Tests: Automate Web Platform Tests"
    a39b0b7621 "Move some internal scroll anchoring tests to wpt"
    7a588d8c6c "[client hints] Match update behavior to the spec."
    46619c7315 "[client hints] Enforce the top-frame requirement."
    2fc36d6315 "Fugu: Add a permission for Screen Enumeration & Window Placement"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    binji@chromium.org:
      external/wpt/wasm
    creis@chromium.org, lukasza@chromium.org:
      external/wpt/fetch/cross-origin-resource-policy
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    yhirano@chromium.org, mkwst@chromium.org, japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: Iade191cb9ac22a3d000db639756244c7c911b936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189816
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#767035}

---
+++ third_party/blink/web_tests/external/wpt/wasm/jsapi/memory/constructor-shared.tentative.any.js
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,jsshell
+// META: script=/wasm/jsapi/assertions.js
+// META: script=/wasm/jsapi/memory/assertions.js
+
+test(() => {
+  assert_throws_js(TypeError, () => new WebAssembly.Memory({ "initial": 10, "shared": true }));
+}, "Shared memory without maximum");
+
+test(t => {
+  const order = [];
+
+  new WebAssembly.Memory({
+    get maximum() {
+      order.push("maximum");
+      return {
+        valueOf() {
+          order.push("maximum valueOf");
+          return 1;
+        },
+      };
+    },
+
+    get initial() {
+      order.push("initial");
+      return {
+        valueOf() {
+          order.push("initial valueOf");
+          return 1;
+        },
+      };
+    },
+
+    get shared() {
+      order.push("shared");
+      return {
+        valueOf: t.unreached_func("should not call shared valueOf"),
+      };
+    },
+  });
+
+  assert_array_equals(order, [
+    "initial",
+    "initial valueOf",
+    "maximum",
+    "maximum valueOf",
+    "shared",
+  ]);
+}, "Order of evaluation for descriptor (with shared)");
+
+test(() => {
+  const argument = { "initial": 4, "maximum": 10, shared: true };
+  const memory = new WebAssembly.Memory(argument);
+  assert_Memory(memory, { "size": 4, "shared": true });
+}, "Shared memory");
