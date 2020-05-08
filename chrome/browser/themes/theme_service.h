7fa1a64a5090d9b72b4275ac3e3b50ea21654755
Ghazale Hosseinabadi <ghazale@chromium.org>
2020-05-08 00:10:59 +0000

    remove NOTIFICATION_EXTENSIONS_READY_DEPRECATED from theme_service
    Since theme_service still uses the deprecated extension
    notification, i.e., NOTIFICATION_EXTENSIONS_READY_DEPRECATED, we
    remove the deprecated notification in this CL.
    
    Bug: 411566
    Change-Id: I1d37307c8702baec6eef772ae8e2b91530739396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2180858
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766650}

--- chrome/browser/themes/theme_service.h
+++ chrome/browser/themes/theme_service.h
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/themes/theme_helper.h"
 #include "chrome/common/buildflags.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 #include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension_id.h"
 #include "ui/base/theme_provider.h"
@@ -45,8 +43,7 @@ class BrowserThemeProviderDelegate {
   virtual const CustomThemeSupplier* GetThemeSupplier() const = 0;
 };
 
-class ThemeService : public content::NotificationObserver,
-                     public KeyedService,
+class ThemeService : public KeyedService,
                      public ui::NativeThemeObserver,
                      public BrowserThemeProviderDelegate {
  public:
@@ -82,11 +79,6 @@ class ThemeService : public content::NotificationObserver,
 
   // KeyedService:
   void Shutdown() override;
-
-  // Overridden from content::NotificationObserver:
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override;
 
   // Overridden from ui::NativeThemeObserver:
   void OnNativeThemeUpdated(ui::NativeTheme* observed_theme) override;
@@ -278,8 +270,6 @@ class ThemeService : public content::NotificationObserver,
 
   // The number of infobars currently displayed.
   int number_of_reinstallers_ = 0;
-
-  content::NotificationRegistrar registrar_;
 
   std::unique_ptr<ThemeSyncableService> theme_syncable_service_;
 
