e3e9fb12df3ee8d2fd12268c5cef537cf4f50513
Clark DuVall <cduvall@chromium.org>
2020-05-08 22:48:31 +0000

    [WebLayer] Deflake CrashReporterTest#testCrashReporterLoading
    Switching to callbackHelper.waitForFirst() makes sure we don't wait if
    the callback was already called.
    
    Bug: 1073464
    Change-Id: Iebe88ad68882456a2023d3f264bbee35274435b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191148
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767019}

--- weblayer/browser/android/javatests/src/org/chromium/weblayer/test/CrashReporterTest.java
+++ weblayer/browser/android/javatests/src/org/chromium/weblayer/test/CrashReporterTest.java
@@ -17,7 +17,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.weblayer.CrashReporterCallback;
 import org.chromium.weblayer.CrashReporterController;
@@ -76,7 +75,6 @@ public class CrashReporterTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "Flaky on crbug.com/1073464")
     public void testCrashReporterLoading() throws Exception {
         BundleCallbackHelper callbackHelper = new BundleCallbackHelper();
         CallbackHelper deleteHelper = new CallbackHelper();
@@ -111,7 +109,7 @@ public class CrashReporterTest {
             crashReporterController.checkForPendingCrashReports();
         });
         // Expect that a Bundle containing { "foo": "bar" } is returned.
-        callbackHelper.waitForCallback(callbackHelper.getCallCount());
+        callbackHelper.waitForFirst();
         Bundle crashKeys = callbackHelper.getResult();
         Assert.assertArrayEquals(crashKeys.keySet().toArray(new String[0]), new String[] {"foo"});
         Assert.assertEquals(crashKeys.getString("foo"), "bar");
