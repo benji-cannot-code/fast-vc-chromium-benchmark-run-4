9f5e808c40490d66bd6e885be07773b8a0cad05d
Stephen McGruer <smcgruer@chromium.org>
2020-05-08 22:11:51 +0000

    Mark two idlharness.html tests as timeout=long
    These are symptomatic of crbug.com/1047818, but to stop the pain for
    sheriffs lets mark them timeout=long for now.
    
    Bug: 1079238, 1080608
    Change-Id: I71e2ce51e4a6807dcdea54765ff9c96afbf2696d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191076
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Robert Ma <robertma@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766994}

--- third_party/blink/web_tests/external/wpt/push-api/idlharness.https.any.js
+++ third_party/blink/web_tests/external/wpt/push-api/idlharness.https.any.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/WebIDLParser.js
 // META: script=/resources/idlharness.js
 // META: script=/service-workers/service-worker/resources/test-helpers.sub.js
+// META: timeout=long
 
 // https://w3c.github.io/push-api/
 
