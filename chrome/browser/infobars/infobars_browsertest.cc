15714d5ae4cc1dacc791ad9ff72370ab219a7d09
Peter Kasting <pkasting@chromium.org>
2020-05-08 08:03:56 +0000

    InfoBar-related cleanup, part 2: Simplify ExtensionDevToolsInfoBar API.
    Replaces a bespoke map with base::CallbackList.
    
    Bug: none
    Change-Id: I90bec595089856129334c39e574d1cf6552ad6d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189952
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766767}

--- chrome/browser/infobars/infobars_browsertest.cc
+++ chrome/browser/infobars/infobars_browsertest.cc
@@ -229,7 +229,7 @@ void InfoBarUiTest::ShowUi(const std::string& name) {
       break;
 
     case IBD::EXTENSION_DEV_TOOLS_INFOBAR_DELEGATE:
-      extensions::ExtensionDevToolsInfoBar::Create("id", "Extension", nullptr,
+      extensions::ExtensionDevToolsInfoBar::Create("id", "Extension",
                                                    base::DoNothing());
       break;
 
