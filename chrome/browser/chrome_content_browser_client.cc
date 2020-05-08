fabfc3521949aeb22be36b1a3ddc090243116bdf
Aaron Tagliaboschi <aarontag@chromium.org>
2020-05-08 20:03:17 +0000

    [client-hints] Lower entropy for Sec-CH-UA-Arch to most common cases
    Bug: 1078270
    Change-Id: Ia5a7213b78ec51a86af809ab1081ee3aa577cc03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2182790
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Yoav Weiss <yoavweiss@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Commit-Queue: Aaron Tagliaboschi <aarontag@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766949}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1132,7 +1132,7 @@ blink::UserAgentMetadata GetUserAgentMetadata() {
   metadata.platform_version =
       content::GetOSVersion(content::IncludeAndroidBuildNumber::Exclude,
                             content::IncludeAndroidModel::Exclude);
-  metadata.architecture = content::BuildCpuInfo();
+  metadata.architecture = content::GetLowEntropyCpuArchitecture();
   metadata.model = content::BuildModelInfo();
 
   metadata.mobile = false;
