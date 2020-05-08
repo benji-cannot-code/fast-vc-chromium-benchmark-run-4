cbc7fbe0d70dc166319e6577bad655b195576b15
Thomas Lukaszewicz <tluk@chromium.org>
2020-05-08 21:45:04 +0000

    Instantiate the Login Bubble Dialog example
    Added the necessary code to instantiate the dialog in the
    views_examples target. Added the necessary string IDS.
    
    Bug: None
    Change-Id: Id03807c002af368d192157893143f0f2ed1d01ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168770
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766980}

--- ui/views/examples/create_examples.cc
+++ ui/views/examples/create_examples.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/examples/flex_layout_example.h"
 #include "ui/views/examples/label_example.h"
 #include "ui/views/examples/link_example.h"
+#include "ui/views/examples/login_bubble_dialog.h"
 #include "ui/views/examples/menu_example.h"
 #include "ui/views/examples/message_box_example.h"
 #include "ui/views/examples/multiline_example.h"
@@ -53,6 +54,7 @@ ExampleVector CreateExamples(ExampleVector extra_examples) {
   examples.push_back(std::make_unique<FlexLayoutExample>());
   examples.push_back(std::make_unique<LabelExample>());
   examples.push_back(std::make_unique<LinkExample>());
+  examples.push_back(std::make_unique<LoginBubbleDialogExample>());
   examples.push_back(std::make_unique<MenuExample>());
   examples.push_back(std::make_unique<MessageBoxExample>());
   examples.push_back(std::make_unique<MultilineExample>());
