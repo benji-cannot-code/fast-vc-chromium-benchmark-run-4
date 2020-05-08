cd58939cec5fbf377f2643e971d48729c8e18995
Xing Liu <xingliu@chromium.org>
2020-05-08 17:32:34 +0000

    Download Later: Add a feature flag.
    This CL adds the feature flag for download later feature on Android.
    
    Bug: 1078454
    Change-Id: I9d7726b7f428f5c4dbd1d9fedb2366683eee94a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189056
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766887}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -463,6 +463,9 @@ extern const char kMemlogStackModePseudo[];
 extern const char kDownloadAutoResumptionNativeName[];
 extern const char kDownloadAutoResumptionNativeDescription[];
 
+extern const char kDownloadLaterName[];
+extern const char kDownloadLaterDescription[];
+
 extern const char kDuetTabStripIntegrationAndroidName[];
 extern const char kDuetTabStripIntegrationAndroidDescription[];
 
