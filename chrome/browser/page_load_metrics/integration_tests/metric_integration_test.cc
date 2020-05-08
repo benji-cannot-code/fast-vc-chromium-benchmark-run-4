072e4aefd75507163ec622e4b1e051526d7dbbc3
Steve Kobes <skobes@chromium.org>
2020-05-08 20:24:06 +0000

    Convert CLS source attribution integration tests to WPT.
    Bug: 1053510
    Change-Id: Ia1549ffa6d0c5d3e6f3dbdca79c83e4a66304852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189951
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766956}

--- chrome/browser/page_load_metrics/integration_tests/metric_integration_test.cc
+++ chrome/browser/page_load_metrics/integration_tests/metric_integration_test.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/tracing_controller.h"
+#include "content/public/common/content_switches.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
@@ -111,6 +112,7 @@ WebContents* MetricIntegrationTest::web_contents() const {
 void MetricIntegrationTest::SetUpCommandLine(CommandLine* command_line) {
   // Set a default window size for consistency.
   command_line->AppendSwitchASCII(switches::kWindowSize, "800,600");
+  command_line->AppendSwitch(switches::kEnableExperimentalWebPlatformFeatures);
 }
 
 std::unique_ptr<HttpResponse> MetricIntegrationTest::HandleRequest(
