af0f4b977a87c523f98a863f2df8052e60b3c9e9
Ian Clelland <iclelland@chromium.org>
2020-05-08 16:04:28 +0000

    Actually disable ScrollLatencyNotRecordedIfGSUIgnored test on MacOS
    The previous CL (https://crrev.com/c/2187202) didn't actually disable the test,
    so this fixes that.
    
    Bug: 1067492
    Change-Id: I2b61e054efb88a3e00102ba6a95bc06e61acebef
    TBR: sadrul@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190048
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766858}

--- content/browser/renderer_host/input/scroll_latency_browsertest.cc
+++ content/browser/renderer_host/input/scroll_latency_browsertest.cc
@@ -228,7 +228,7 @@ IN_PROC_BROWSER_TEST_F(ScrollLatencyBrowserTest, MultipleWheelScrollOnMain) {
   ScrollLatencyNotRecordedIfGSUIgnored
 #endif
 IN_PROC_BROWSER_TEST_F(ScrollLatencyBrowserTest,
-                       ScrollLatencyNotRecordedIfGSUIgnored) {
+                       MAYBE_ScrollLatencyNotRecordedIfGSUIgnored) {
   LoadURL();
   auto scroll_update_watcher = std::make_unique<InputMsgWatcher>(
       GetWidgetHost(), blink::WebInputEvent::Type::kGestureScrollUpdate);
