b02f9f5d9946eb7af3b97caf575eb636eca82270
Caleb Raitto <caraitto@chromium.org>
2020-05-08 21:43:54 +0000

    Make 0 max budget ClientPaintCache instances disable Put().
    This will be used to compute hash digests on PaintOps produced by HTML
    canvas operations.
    
    Bug: 973801
    Change-Id: I0aff1255797890fce994d78b58d1916668eac326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2188903
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766979}

--- cc/paint/paint_cache.cc
+++ cc/paint/paint_cache.cc
@@ -35,6 +35,8 @@ bool ClientPaintCache::Get(PaintCacheDataType type, PaintCacheId id) {
 void ClientPaintCache::Put(PaintCacheDataType type,
                            PaintCacheId id,
                            size_t size) {
+  if (max_budget_ == kNoCachingBudget)
+    return;
   auto key = std::make_pair(type, id);
   DCHECK(cache_map_.Peek(key) == cache_map_.end());
 
