4af4366ada9c6620ba4766144a03dc81895e61f3
Ted Meyer <tmathmeyer@chromium.org>
2020-05-08 21:03:52 +0000

    Make the Wait a TimeoutWait
    This won't fix the root cause of the issue - and 10 seconds might not be
    the right amount of time to wait for; I suspect that it could be lower.
    
    This should fix the watchdog killing the thread though, and will at
    least allow some kind of fallback to potentially occur.
    
    Bug: 1076817
    Change-Id: I0390dc13b2d4f0ae05caea07fd7ba824b2a91553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189636
    Reviewed-by: Markus Handell <handellm@google.com>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Ted Meyer <tmathmeyer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766962}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_decoder_adapter.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread_restrictions.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "media/base/media_log.h"
 #include "media/base/media_switches.h"
@@ -206,7 +207,9 @@ bool RTCVideoDecoderAdapter::InitializeSync(
           CrossThreadBindOnce(&RTCVideoDecoderAdapter::InitializeOnMediaThread,
                               CrossThreadUnretained(this), config,
                               std::move(init_cb)))) {
-    waiter.Wait();
+    // TODO(crbug.com/1076817) Remove if a root cause is found.
+    if (!waiter.TimedWait(base::TimeDelta::FromSeconds(10)))
+      return false;
   }
   return result;
 }
