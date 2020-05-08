987ecdf16ffd2ae08c14842809b6156e3ee66962
Xiaohan Wang <xhwang@chromium.org>
2020-05-08 22:24:05 +0000

    media: Use MFSessionLifetime in MediaFoundationRenderer
    Use MFSessionLifetime so MFShutdown() will be automatically called. This
    is consistent with the rest of media code.
    
    Bug: 999747
    Change-Id: I917bbc48aa31b7a3449348db3e63cb19bca807a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191194
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767003}

--- media/renderers/win/media_foundation_renderer.h
+++ media/renderers/win/media_foundation_renderer.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/pipeline_status.h"
 #include "media/base/renderer.h"
 #include "media/base/renderer_client.h"
+#include "media/base/win/mf_initializer.h"
 #include "media/renderers/win/media_engine_extension.h"
 #include "media/renderers/win/media_engine_notify_impl.h"
 #include "media/renderers/win/media_foundation_protection_manager.h"
@@ -106,7 +107,9 @@ class MediaFoundationRenderer : public Renderer,
   // This is used for testing.
   const bool force_dcomp_mode_for_testing_;
 
-  bool mf_started_ = false;
+  // Keep this here so it's destroyed after all Media Foundation members below.
+  MFSessionLifetime mf_session_life_time_;
+
   RendererClient* renderer_client_;
 
   Microsoft::WRL::ComPtr<IMFMediaEngine> mf_media_engine_;
