98058e7e7c5e6dd23843dd038a4cd73b377bab4b
Yue Li <updowndota@google.com>
2020-05-08 23:56:28 +0000

    Replace usages of HTTP related constants
    Replace the use cases to use the shared library instead.
    
    Bug: None
    Test: Run existing unit tests
    Change-Id: I18e2fa8669811674cf7ad5334009225a4a32d2e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189358
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767057}

--- chrome/browser/ui/ash/assistant/search_and_assistant_enabled_checker.cc
+++ chrome/browser/ui/ash/assistant/search_and_assistant_enabled_checker.cc
@@ -12,18 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/json/json_writer.h"
 #include "base/strings/string_util.h"
-#include "build/buildflag.h"
-#include "chromeos/assistant/buildflags.h"
 #include "chromeos/services/assistant/public/cpp/assistant_prefs.h"
+#include "chromeos/services/assistant/public/shared/constants.h"
 #include "net/base/url_util.h"
 #include "services/data_decoder/public/cpp/data_decoder.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "url/gurl.h"
-
-#if BUILDFLAG(ENABLE_CROS_LIBASSISTANT)
-#include "chromeos/assistant/internal/internal_constants.h"
-#endif
 
 namespace {
 
@@ -49,14 +44,10 @@ SearchAndAssistantEnabledChecker::~SearchAndAssistantEnabledChecker() {}
 void SearchAndAssistantEnabledChecker::SyncSearchAndAssistantState() {
   auto resource_request = std::make_unique<network::ResourceRequest>();
   resource_request->url =
-#if BUILDFLAG(ENABLE_CROS_LIBASSISTANT)
       net::AppendOrReplaceQueryParameter(
           GURL(chromeos::assistant::kServiceIdEndpoint),
           chromeos::assistant::kPayloadParamName,
           chromeos::assistant::kServiceIdRequestPayload);
-#else
-      GURL();
-#endif
   url_loader_ = network::SimpleURLLoader::Create(std::move(resource_request),
                                                  NO_TRAFFIC_ANNOTATION_YET);
   url_loader_->DownloadToString(
--- chrome/browser/ui/ash/assistant/search_and_assistant_enabled_checker_unittest.cc
+++ chrome/browser/ui/ash/assistant/search_and_assistant_enabled_checker_unittest.cc
@@ -10,26 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
-#include "build/buildflag.h"
 #include "chrome/test/base/chrome_ash_test_base.h"
-#include "chromeos/assistant/buildflags.h"
+#include "chromeos/services/assistant/public/shared/constants.h"
 #include "services/data_decoder/public/cpp/test_support/in_process_data_decoder.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if BUILDFLAG(ENABLE_CROS_LIBASSISTANT)
-#include "chromeos/assistant/internal/internal_constants.h"
-#endif
-
 namespace {
-
-const std::string kURL =
-#if BUILDFLAG(ENABLE_CROS_LIBASSISTANT)
-    chromeos::assistant::kSampleServiceIdRequest;
-#else
-    "";
-#endif
 
 constexpr char kFalseResponse[] = R"()]}'
   {
@@ -82,14 +70,16 @@ class SearchAndAssistantEnabledCheckerTest : public ChromeAshTestBase {
 };
 
 TEST_F(SearchAndAssistantEnabledCheckerTest, TrueResponse) {
-  test_url_loader_factory_.AddResponse(kURL, kTrueResponse);
+  test_url_loader_factory_.AddResponse(
+      chromeos::assistant::kSampleServiceIdRequest, kTrueResponse);
   sender_->SyncSearchAndAssistantState();
   EXPECT_CALL(delegate_, OnSearchAndAssistantStateReceived(true));
   base::RunLoop().RunUntilIdle();
 }
 
 TEST_F(SearchAndAssistantEnabledCheckerTest, FalseResponse) {
-  test_url_loader_factory_.AddResponse(kURL, kFalseResponse);
+  test_url_loader_factory_.AddResponse(
+      chromeos::assistant::kSampleServiceIdRequest, kFalseResponse);
   sender_->SyncSearchAndAssistantState();
   EXPECT_CALL(delegate_, OnSearchAndAssistantStateReceived(false));
   EXPECT_CALL(delegate_, OnError()).Times(0);
@@ -98,7 +88,8 @@ TEST_F(SearchAndAssistantEnabledCheckerTest, FalseResponse) {
 
 TEST_F(SearchAndAssistantEnabledCheckerTest, NetworkError) {
   test_url_loader_factory_.AddResponse(
-      GURL(kURL), network::mojom::URLResponseHead::New(), std::string(),
+      GURL(chromeos::assistant::kSampleServiceIdRequest),
+      network::mojom::URLResponseHead::New(), std::string(),
       network::URLLoaderCompletionStatus(net::HTTP_NOT_FOUND));
   sender_->SyncSearchAndAssistantState();
   EXPECT_CALL(delegate_, OnError());
@@ -106,7 +97,8 @@ TEST_F(SearchAndAssistantEnabledCheckerTest, NetworkError) {
 }
 
 TEST_F(SearchAndAssistantEnabledCheckerTest, InvalidResponse) {
-  test_url_loader_factory_.AddResponse(kURL, "");
+  test_url_loader_factory_.AddResponse(
+      chromeos::assistant::kSampleServiceIdRequest, "");
   sender_->SyncSearchAndAssistantState();
   EXPECT_CALL(delegate_, OnError());
   base::RunLoop().RunUntilIdle();
