5b3b9bcacb14619e11cdb9e30e89604dacd74fa5
Bo Liu <boliu@chromium.org>
2020-05-08 00:19:25 +0000

    weblayer: Obscure webcontent for a11y when dialog shows
    Follow similar logic in ChromeTabModalPresenter and obscure
    WebContentsAccessibility when a tab-modal dialog is displayed.
    
    Bug: 1078845
    Change-Id: If2f6cfe99fd5385ba04f3028cf958833907b59e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2188831
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#766653}

--- weblayer/browser/java/org/chromium/weblayer_private/ContentView.java
+++ weblayer/browser/java/org/chromium/weblayer_private/ContentView.java
@@ -52,6 +52,7 @@ public class ContentView extends RelativeLayout
 
     private TabImpl mTab;
     private WebContents mWebContents;
+    private boolean mIsObscuredForAccessibility;
     private final ObserverList<OnHierarchyChangeListener> mHierarchyChangeListeners =
             new ObserverList<>();
     private final ObserverList<OnSystemUiVisibilityChangeListener> mSystemUiChangeListeners =
@@ -136,13 +137,27 @@ public class ContentView extends RelativeLayout
         boolean wasFocused = isFocused();
         boolean wasWindowFocused = hasWindowFocus();
         boolean wasAttached = isAttachedToWindow();
+        boolean wasObscured = mIsObscuredForAccessibility;
         if (wasFocused) onFocusChanged(false, View.FOCUS_FORWARD, null);
         if (wasWindowFocused) onWindowFocusChanged(false);
         if (wasAttached) onDetachedFromWindow();
+        if (wasObscured) setIsObscuredForAccessibility(false);
         mWebContents = mTab != null ? mTab.getWebContents() : null;
         if (wasFocused) onFocusChanged(true, View.FOCUS_FORWARD, null);
         if (wasWindowFocused) onWindowFocusChanged(true);
         if (wasAttached) onAttachedToWindow();
+        if (wasObscured) setIsObscuredForAccessibility(true);
+    }
+
+    /**
+     * Control whether WebContentsAccessibility will respond to accessibility requests.
+     */
+    public void setIsObscuredForAccessibility(boolean isObscured) {
+        if (mIsObscuredForAccessibility == isObscured) return;
+        mIsObscuredForAccessibility = isObscured;
+        WebContentsAccessibility wcax = getWebContentsAccessibility();
+        if (wcax == null) return;
+        wcax.setObscuredByAnotherView(mIsObscuredForAccessibility);
     }
 
     @Override
