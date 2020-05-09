6c098514f10fdf8c7c36aae904b9b2df8aa82c49
Brian Sheedy <bsheedy@chromium.org>
2020-05-09 00:42:31 +0000

    Enable more GPU PyLint errors
    Enables too-many-lines, too-many-return-statements, and too-many-locals
    PyLint errors on //content/test/gpu and fixes the resulting errors.
    
    Bug: 1076144
    Change-Id: I064a2d4e6769e19358379374e1cc4d671df28ec9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2183470
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Rakib Hasan <rmhasan@google.com>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767068}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -3,6 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# This is more akin to a .pyl/JSON file, so it's expected to be long.
+# pylint: disable=too-many-lines
+
 import datetime
 
 from gpu_tests import skia_gold_matching_algorithms as algo
