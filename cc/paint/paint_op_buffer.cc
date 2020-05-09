4c5d013cbf32997d48e2b8a632710a88c492c6f0
Caleb Raitto <caraitto@chromium.org>
2020-05-09 02:21:12 +0000

    Remove DCHECK for |options.transfer_cache|.
    This used to be required for many operations, but it isn't anymore. When
    performing serialization for identifiability metrics, we won't use a
    transfer cache as it's not needed for web content.
    
    Bug: 973801
    Change-Id: I29f512b6a99657f62c3067e1b18599965a26cb76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191210
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767086}

--- cc/paint/paint_op_buffer.cc
+++ cc/paint/paint_op_buffer.cc
@@ -2004,7 +2004,6 @@ void PaintOp::Raster(SkCanvas* canvas, const PlaybackParams& params) const {
 size_t PaintOp::Serialize(void* memory,
                           size_t size,
                           const SerializeOptions& options) const {
-  DCHECK(options.transfer_cache);
   DCHECK(options.canvas);
 
   // Need at least enough room for a skip/type header.
