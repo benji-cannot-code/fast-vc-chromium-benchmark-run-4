339534b6594bee8d8113bb3345d0c2591ca82830
Shakti Sahu <shaktisahu@chromium.org>
2020-05-09 03:31:50 +0000

    Query tile : Handle empty URL
    We see some of the tiles don't have a URL and crashes, due to some
    server issues which is being fixed. Nevertheless, the client should be
    prepared to handle if there is no URL.
    
    Change-Id: Ia95da992aa6dcf57e629d560b0638096bafb56bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190620
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767096}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxQueryTileCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxQueryTileCoordinator.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import org.chromium.base.Callback;
+import org.chromium.base.task.PostTask;
 import org.chromium.chrome.browser.GlobalDiscardableReferencePool;
 import org.chromium.chrome.browser.image_fetcher.ImageFetcher;
 import org.chromium.chrome.browser.image_fetcher.ImageFetcherConfig;
@@ -22,6 +23,7 @@ import org.chromium.components.browser_ui.widget.image_tiles.ImageTileCoordinato
 import org.chromium.components.browser_ui.widget.image_tiles.ImageTileCoordinatorFactory;
 import org.chromium.components.browser_ui.widget.image_tiles.TileConfig;
 import org.chromium.components.query_tiles.QueryTile;
+import org.chromium.content_public.browser.UiThreadTaskTraits;
 import org.chromium.ui.UiUtils;
 
 import java.util.ArrayList;
@@ -98,6 +100,10 @@ public class OmniboxQueryTileCoordinator {
         }
 
         QueryTile queryTile = (QueryTile) tile;
+        if (queryTile.urls.isEmpty()) {
+            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> callback.onResult(null));
+            return;
+        }
         getImageFetcher().fetchImage(queryTile.urls.get(0), ImageFetcher.QUERY_TILE_UMA_CLIENT_NAME,
                 mTileWidth, mTileWidth, bitmap -> callback.onResult(Arrays.asList(bitmap)));
     }
--- chrome/android/java/src/org/chromium/chrome/browser/query_tiles/QueryTileSection.java
+++ chrome/android/java/src/org/chromium/chrome/browser/query_tiles/QueryTileSection.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 
 import org.chromium.base.Callback;
+import org.chromium.base.task.PostTask;
 import org.chromium.chrome.browser.GlobalDiscardableReferencePool;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.image_fetcher.ImageFetcher;
@@ -27,6 +28,7 @@ import org.chromium.components.browser_ui.widget.image_tiles.TileConfig;
 import org.chromium.components.query_tiles.QueryTile;
 import org.chromium.components.query_tiles.TileProvider;
 import org.chromium.components.query_tiles.TileUmaLogger;
+import org.chromium.content_public.browser.UiThreadTaskTraits;
 import org.chromium.ui.display.DisplayAndroid;
 import org.chromium.ui.display.DisplayUtil;
 
@@ -140,6 +142,11 @@ public class QueryTileSection {
     }
 
     private void fetchImage(QueryTile queryTile, int size, Callback<Bitmap> callback) {
+        if (queryTile.urls.isEmpty()) {
+            PostTask.postTask(UiThreadTaskTraits.DEFAULT, () -> callback.onResult(null));
+            return;
+        }
+
         String url = queryTile.urls.get(0);
         // TODO(crbug.com/1058534) : Replace with correct API.
         mImageFetcher.fetchImage(
