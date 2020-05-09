714c57b3b0af8d383c673018fe8d176653766642
Piotr Bialecki <bialpio@chromium.org>
2020-05-09 02:53:30 +0000

    WebXR - blink - move assumption about mojo == local/unbounded
    This moves the assumption about mojo space being equivalent to
    local/unbounded space. The assumption is still present since the devices
    still send the same data over through mojo, but it makes it so that
    blink is aware that local and unbounded reference spaces can dynamically
    change.
    
    The bounded reference space was already assumed to be dynamically
    changing so no modifications are required there.
    
    Bug: 1070380
    Change-Id: I62c33561659a6f32016565c3646cdcfec79ee4c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189350
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767092}

--- third_party/blink/renderer/modules/xr/xr_session.h
+++ third_party/blink/renderer/modules/xr/xr_session.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/xr/xr_frame_request_callback_collection.h"
 #include "third_party/blink/renderer/modules/xr/xr_input_source.h"
 #include "third_party/blink/renderer/modules/xr/xr_input_source_array.h"
+#include "third_party/blink/renderer/modules/xr/xr_reference_space.h"
 #include "third_party/blink/renderer/platform/geometry/double_size.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/transforms/transformation_matrix.h"
@@ -289,9 +290,14 @@ class XRSession final
 
   bool CanReportPoses() const;
 
-  // Returns current transform from mojo space to viewer space. May return
-  // nullopt if poses cannot be reported or if the transform is unknown.
-  base::Optional<TransformationMatrix> MojoFromViewer() const;
+  // Returns current transform from mojo space to the space of the passed in
+  // type. May return nullopt if poses cannot be reported or if the transform is
+  // unknown.
+  // Note: currently, the information about the mojo_from_-floor-type spaces is
+  // stored elsewhere, this method will not work for those reference space
+  // types.
+  base::Optional<TransformationMatrix> GetMojoFrom(
+      XRReferenceSpace::Type space_type);
 
   // Creates presentation frame based on current state of the session.
   // State currently used in XRFrame creation is mojo_from_viewer_ and
