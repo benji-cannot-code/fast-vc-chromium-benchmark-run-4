6196965a43c5fd6ab4c6587f559c754edbcd1df2
Rouslan Solomakhin <rouslan@chromium.org>
2020-05-10 16:54:18 +0000

    [Web Payment] Componentize PaymentApp.java
    This patch moves PaymentApp.java from //chrome to //components.
    
    Bug: 1081024
    Change-Id: I0cdf50bd4c360ace049d7bfe35aac13397294d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191783
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Auto-Submit: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767148}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentAppFactoryDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentAppFactoryDelegate.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.payments;
 
+import org.chromium.components.payments.PaymentApp;
+
 /**
  * Interface for providing information to a payment app factory and receiving the list of payment
  * apps.
--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentAppService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentAppService.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.payments;
 
+import org.chromium.components.payments.PaymentApp;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
