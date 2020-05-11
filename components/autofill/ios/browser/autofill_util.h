dbb9a1998aca505038c995e0fba7fbb84fc41b04
Maria Kazinova <kazinova@google.com>
2020-05-11 10:54:58 +0000

    [iOS] Using stable unique form and field IDs in password form filling.
    So far it does not apply for generated password filling.
    
    Bug: 1075444
    Change-Id: I5e5896086942fdeaf1c85650efaca60ebdf71e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190636
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Vadym Doroshenko  <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767250}

--- components/autofill/ios/browser/autofill_util.h
+++ components/autofill/ios/browser/autofill_util.h
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @class CRWJSInjectionReceiver;
 class GURL;
 
+namespace {
+constexpr int kNotSetRendererID = -1;
+}
+
 namespace base {
 class DictionaryValue;
 }
--- components/autofill/ios/browser/autofill_util.mm
+++ components/autofill/ios/browser/autofill_util.mm
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 // The timeout for any JavaScript call in this file.
 const int64_t kJavaScriptExecutionTimeoutInSeconds = 5;
-
-constexpr int kNotSetRendererID = -1;
 }
 
 namespace autofill {
