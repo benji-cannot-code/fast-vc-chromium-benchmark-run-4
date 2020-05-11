3064622a3987597ef49b9b8ab3200103f76c7298
Steven Bingler <bingler@chromium.org>
2020-05-11 16:58:34 +0000

    Add same-site context downgrade warnings to DevTools
    Add the schemeful same-site context downgrade warnings to DevTools to
    support showing warnings in the Issue Pane.
    
    Bug: 1071618
    Change-Id: I877a7b279e2a37c847d75549c7311b19fadd4ed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181024
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767353}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/same-site-issue-warn-cookie-context-downgrade.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  const {page, session, dp} = await testRunner.startBlank(
+      `Verifies that accessing a cookie in a breaking schemeful context downgrading situation triggers an inspector issue.\n`);
+
+  await dp.Network.enable();
+  await dp.Audits.enable();
+
+  await session.navigate('http://cookie.test:8000/inspector-protocol/resources/empty.html');
+
+  const setCookieUrl = 'https://cookie.test:8443/inspector-protocol/network/resources/set-cookie.php?cookie='
+      + encodeURIComponent('name=value; SameSite=Strict');
+  await session.evaluate(`fetch('${setCookieUrl}', {method: 'POST', credentials: 'include'})`);
+  const issue = await dp.Audits.onceIssueAdded();
+  // Clear request id.
+  const replacer = (key, value) => {
+    if (key === "requestId") return "<request-id>";
+    return value;
+  };
+  testRunner.log(`Inspector issue: ${JSON.stringify(issue.params, replacer, 2)}\n`);
+
+  testRunner.completeTest();
+});
