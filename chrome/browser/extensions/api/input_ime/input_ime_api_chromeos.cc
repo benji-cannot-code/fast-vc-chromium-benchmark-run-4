9c34d2ce56c3b7a4fa367007d3fbe123882dccb2
My Nguyen <myy@chromium.org>
2020-05-11 05:12:00 +0000

    Store candidate_window_property with engine_id
    As there are more optional fields added to CandidateWindowProperty,
    there is a need to distinguish property for different engineID. This
    parameter is already passed in from the API, we only haven't used it in
    our code yet.
    
    Bug: 1074369
    Change-Id: If9f3860e37c4b31e29876091787ad5c6228f55bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2182201
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767184}

--- chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api_chromeos.cc
@@ -615,7 +615,7 @@ InputImeSetCandidateWindowPropertiesFunction::Run() {
   }
 
   InputMethodEngine::CandidateWindowProperty properties_out =
-      engine->GetCandidateWindowProperty();
+      engine->GetCandidateWindowProperty(params.engine_id);
   bool modified = false;
 
   if (properties.cursor_visible) {
@@ -664,7 +664,7 @@ InputImeSetCandidateWindowPropertiesFunction::Run() {
   }
 
   if (modified) {
-    engine->SetCandidateWindowProperty(properties_out);
+    engine->SetCandidateWindowProperty(params.engine_id, properties_out);
   }
 
   return RespondNow(OneArgument(std::make_unique<base::Value>(true)));
