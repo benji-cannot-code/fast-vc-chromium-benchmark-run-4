7bad3c9502a7a7517f00022378098d831d781a91
Julie Jeongeun Kim <jkim@igalia.com>
2020-05-11 23:16:44 +0000

    [a11y] Check if |sibling| is null on looping using GetNextSibling
    This CL checks if |sibling| is null on looping using GetNextSibling
    at AXPlatformNodeBase::GetHypertextOffsetFromChild.
    
    AX-Relnotes: N/A
    Bug: 1080631
    Change-Id: Iab5e34594852db1cdb575ee351fe1f3d57ee1ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2192575
    Commit-Queue: Julie Kim <jkim@igalia.com>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767558}

--- ui/accessibility/platform/ax_platform_node_base.cc
+++ ui/accessibility/platform/ax_platform_node_base.cc
@@ -1441,9 +1441,8 @@ int32_t AXPlatformNodeBase::GetHypertextOffsetFromChild(
   // cross-tree traversal is necessary.
   if (child->IsTextOnlyObject()) {
     int32_t hypertext_offset = 0;
-    for (AXPlatformNodeBase* sibling = GetFirstChild(); sibling != child;
-         sibling = sibling->GetNextSibling()) {
-      DCHECK(sibling);
+    for (AXPlatformNodeBase* sibling = GetFirstChild();
+         sibling && sibling != child; sibling = sibling->GetNextSibling()) {
       if (sibling->IsTextOnlyObject()) {
         hypertext_offset += (int32_t)sibling->GetHypertext().size();
       } else {
