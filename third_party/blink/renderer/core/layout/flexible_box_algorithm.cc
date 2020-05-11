07146827da98610f864bc8e6eddef3b71203dce1
David Grogan <dgrogan@chromium.org>
2020-05-11 19:35:19 +0000

    [css-flexbox] Add finch flag for flex gaps
    Gaps will be enabled if you're running with
    experimental-web-platform-features regardless of the state of the
    server-controlled flag.
    
    I tested with these command lines. First two showed gaps, last didn't.
    
    out/Release/chrome --enable-features=FlexGaps
    out/Release/chrome --enable-experimental-web-platform-features
    out/Release/chrome
    
    Bug: 762679
    Change-Id: Id7445db345689f6c8f20e9b86c008ae28b2e3d83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2192674
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767435}

--- third_party/blink/renderer/core/layout/flexible_box_algorithm.cc
+++ third_party/blink/renderer/core/layout/flexible_box_algorithm.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_flexible_box.h"
 #include "third_party/blink/renderer/core/layout/min_max_sizes.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_box_fragment.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 namespace {
@@ -584,6 +585,8 @@ void FlexLine::ComputeLineItemsPosition(LayoutUnit main_axis_start_offset,
 LayoutUnit FlexLayoutAlgorithm::GapBetweenItems(
     const ComputedStyle& style,
     LogicalSize percent_resolution_sizes) {
+  if (!RuntimeEnabledFeatures::FlexGapsEnabled())
+    return LayoutUnit();
   DCHECK_GE(percent_resolution_sizes.inline_size, 0);
   if (IsColumnFlow(style)) {
     if (LIKELY(style.RowGap().IsNormal()))
@@ -602,6 +605,8 @@ LayoutUnit FlexLayoutAlgorithm::GapBetweenItems(
 LayoutUnit FlexLayoutAlgorithm::GapBetweenLines(
     const ComputedStyle& style,
     LogicalSize percent_resolution_sizes) {
+  if (!RuntimeEnabledFeatures::FlexGapsEnabled())
+    return LayoutUnit();
   DCHECK_GE(percent_resolution_sizes.inline_size, 0);
   if (!IsColumnFlow(style)) {
     if (LIKELY(style.RowGap().IsNormal()))
