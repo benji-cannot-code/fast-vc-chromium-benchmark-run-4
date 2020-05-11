ee2aa56081e7b2c76cc51ffc62c907978c7e87e7
Oksana Zhuravlova <oksamyt@chromium.org>
2020-05-11 23:06:45 +0000

    [mojo] Remove RenderFrameHostImpl::RegisterMojoInterfaces()
    This change removes RegisterMojoInterfaces() method from
    RenderFrameHostImpl and moves the remaining code to SetUpMojoIfNeeded().
    
    Bug: 718652
    
    Change-Id: I5d132c3bc9e4120842de8592902acf0522e2689f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189450
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767550}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -5004,13 +5004,6 @@ void RenderFrameHostImpl::HandleAXLocationChanges(
   }
 }
 
-void RenderFrameHostImpl::RegisterMojoInterfaces() {
-  file_system_manager_.reset(new FileSystemManagerImpl(
-      GetProcess()->GetID(),
-      GetProcess()->GetStoragePartition()->GetFileSystemContext(),
-      ChromeBlobStorageContext::GetFor(GetProcess()->GetBrowserContext())));
-}
-
 media::MediaMetricsProvider::RecordAggregateWatchTimeCallback
 RenderFrameHostImpl::GetRecordAggregateWatchTimeCallback() {
   return delegate_->GetRecordAggregateWatchTimeCallback();
@@ -6139,7 +6132,11 @@ void RenderFrameHostImpl::SetUpMojoIfNeeded() {
       },
       base::Unretained(this)));
 
-  RegisterMojoInterfaces();
+  file_system_manager_.reset(new FileSystemManagerImpl(
+      GetProcess()->GetID(),
+      GetProcess()->GetStoragePartition()->GetFileSystemContext(),
+      ChromeBlobStorageContext::GetFor(GetProcess()->GetBrowserContext())));
+
   mojo::PendingRemote<mojom::FrameFactory> frame_factory;
   GetProcess()->BindReceiver(frame_factory.InitWithNewPipeAndPassReceiver());
   mojo::Remote<mojom::FrameFactory>(std::move(frame_factory))
