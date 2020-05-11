26ce3c815907f557f8bbfacf3fb2884f3f32ec8d
Kramer Ge <fangzhoug@chromium.org>
2020-05-11 16:48:37 +0000

    [ozone]Remove ipc/ dependencies from ui/ozone/
    This is the last CL(?) to remove legacy IPC from Ozone
    
    Bug: 806092
    Change-Id: I189bbdd07c2950e2da648e6242b7da71f182256b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2188050
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767350}

--- ui/ozone/public/gpu_platform_support_host.h
+++ ui/ozone/public/gpu_platform_support_host.h
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
-#include "ipc/ipc_listener.h"
-#include "ipc/ipc_sender.h"
 #include "mojo/public/cpp/system/message_pipe.h"
 
 namespace ui {
