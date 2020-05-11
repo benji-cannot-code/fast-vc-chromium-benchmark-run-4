454288a8783293f4711caf75441cd0e409602f07
Mitsuru Oshima <oshima@chromium.org>
2020-05-11 18:04:23 +0000

    ash::ViewTreeHostWidget
    This is based on the FastInkView but uses back buffers.
    
    I'll add front buffer rendering & partial content update later
    and replace FastInkView.
    Here are use cases:
    * Performance Dev Tools: This bypasses cc and directly submits a frame
     to viz, and can promote it to HW overlay. This allow the tool to
     show performance related stats with minimum impact on the performance.
    * Once partial content update and front buffer rendering is supported,
     this will replace FastInkView that is used for low-latency UI such as
     laser pointer / software cursor.
    
    Bug: 1054474
    Test: ash hud display (ctrl-alt-shift-g) works as expected.
    
    Change-Id: I5de40ed217212a790ea96f6f0719ccfa7cf50239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2071181
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767393}

---
+++ ash/fast_ink/view_tree_host_widget.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_FAST_INK_VIEW_TREE_HOST_WIDGET_H_
+#define ASH_FAST_INK_VIEW_TREE_HOST_WIDGET_H_
+
+#include "ui/views/widget/widget.h"
+
+namespace ash {
+
+// Creates a Widget that creates a CompositorFrame from a view tree within the
+// widget and submits it directly w/o going through cc.
+views::Widget* CreateViewTreeHostWidget(views::Widget::InitParams params);
+
+}  // namespace ash
+
+#endif  // ASH_FAST_INK_VIEW_TREE_HOST_WIDGET_H_
