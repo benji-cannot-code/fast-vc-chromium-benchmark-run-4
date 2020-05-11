5ffe751223459605cb521b94e639e06b34a5aae6
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-05-11 19:22:25 +0000

    [Ash] Replace back button in floating menu
    This cl changes the name and icon of the back button in the detailed
    view of floating menu. Now it is a fully-fledged close button, with own
    icon and description. Also, it also adds some strings and hides
    alrady disabled help button.
    
    Bug: 1015383
    Change-Id: Ia4d911160aeb80756654a8cf1bc6c3a825c823ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190695
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767432}

--- ash/system/accessibility/floating_accessibility_detailed_controller.h
+++ ash/system/accessibility/floating_accessibility_detailed_controller.h
@@ -40,6 +40,7 @@ class ASH_EXPORT FloatingAccessibilityDetailedController
   // DetailedViewDelegate:
   void CloseBubble() override;
   void TransitionToMainView(bool restore_focus) override;
+  base::string16 GetAccessibleNameForBubble() override;
 
   void OnAccessibilityStatusChanged();
 
@@ -47,6 +48,7 @@ class ASH_EXPORT FloatingAccessibilityDetailedController
   friend class FloatingAccessibilityControllerTest;
   class DetailedBubbleView;
   // DetailedViewDelegate:
+  views::Button* CreateBackButton(views::ButtonListener* listener) override;
   views::Button* CreateHelpButton(views::ButtonListener* listener) override;
   // TrayBubbleView::Delegate:
   void BubbleViewDestroyed() override;
