d9b7c195090d9a2c9ef01317b6cf295d82264e35
Rouslan Solomakhin <rouslan@chromium.org>
2020-05-11 09:17:17 +0000

    [Web Payment] Cleanup non-componentized PaymentApp.java
    Bug: 1081024
    Change-Id: I1b3d6507327699854eb76485bebf656e79915f44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191784
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Sahel Sharify <sahel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767227}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentAppFactoryParams.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentAppFactoryParams.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.payments;
 
 import androidx.annotation.Nullable;
 
-import org.chromium.chrome.browser.payments.PaymentApp.PaymentRequestUpdateEventCallback;
 import org.chromium.components.payments.PaymentApp.PaymentRequestUpdateEventListener;
 import org.chromium.content_public.browser.RenderFrameHost;
 import org.chromium.content_public.browser.WebContents;
@@ -90,13 +89,6 @@ public interface PaymentAppFactoryParams {
      * @return The listener for payment method, shipping address, and shipping option change events.
      */
     default PaymentRequestUpdateEventListener getPaymentRequestUpdateEventListener() {
-        return null;
-    }
-
-    /**
-     * Stub method to be removed after resolving clank dependencies.
-     */
-    default PaymentRequestUpdateEventCallback getPaymentRequestUpdateEventCallback() {
         return null;
     }
 
