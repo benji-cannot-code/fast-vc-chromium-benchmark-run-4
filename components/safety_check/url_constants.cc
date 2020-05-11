6326f6abacf86a9cef9aa38e749f3f41dae99b8d
Andrey Zaytsev <andzaytsev@google.com>
2020-05-11 09:25:17 +0000

    Safety check: added an extra connectivity check on VersionUpdater generic error
    Bug: 1078760, 1015841
    Change-Id: I45a294faa179560adbf9ab94cc438cb43814459a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2184231
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767230}

---
+++ components/safety_check/url_constants.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/safety_check/url_constants.h"
+
+namespace safety_check {
+
+const char kConnectivityCheckUrl[] =
+    "https://connectivitycheck.gstatic.com/generate_204";
+
+}  // namespace safety_check
