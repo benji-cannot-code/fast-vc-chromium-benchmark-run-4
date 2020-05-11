cb30d78aa0b4dff50e72068a4ab9d98a42617172
Zentaro Kavanagh <zentaro@google.com>
2020-05-11 15:30:23 +0000

    EventRewriterChromeOS: Cleanup includes
    - Remove redundant includes
    
    Bug: None
    Test: unittests
    Change-Id: I9b1a1da19ef85133d5f4062004b4974be719e364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191130
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Zentaro Kavanagh <zentaro@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767315}

--- ui/chromeos/events/event_rewriter_chromeos.cc
+++ ui/chromeos/events/event_rewriter_chromeos.cc
@@ -8,10 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fcntl.h>
 #include <stddef.h>
 
-#include <vector>
-
-#include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/files/file_path.h"
 #include "base/files/scoped_file.h"
 #include "base/logging.h"
 #include "base/metrics/user_metrics.h"
@@ -29,10 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/chromeos/events/modifier_key.h"
 #include "ui/chromeos/events/pref_names.h"
 #include "ui/events/devices/device_data_manager.h"
-#include "ui/events/event.h"
 #include "ui/events/event_utils.h"
 #include "ui/events/keycodes/dom/dom_code.h"
-#include "ui/events/keycodes/dom/dom_key.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
 #include "ui/events/ozone/evdev/event_device_info.h"
--- ui/chromeos/events/event_rewriter_chromeos.h
+++ ui/chromeos/events/event_rewriter_chromeos.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/files/file_path.h"
 #include "base/macros.h"
 #include "ui/events/devices/input_device.h"
 #include "ui/events/event.h"
