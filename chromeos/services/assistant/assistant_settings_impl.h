50c2b7db9fb18e7f371fecbdab83b3751470e66d
David Black <dmblack@google.com>
2020-05-11 19:34:14 +0000

    Conditionally ignore response from GetSettingsUi request.
    Bug: b:151321970
    Change-Id: I0236c4f1f378fa44ce63feb5e6a1363b4eb5fbef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186649
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#767434}

--- chromeos/services/assistant/assistant_settings_impl.h
+++ chromeos/services/assistant/assistant_settings_impl.h
@@ -23,6 +23,7 @@ class AssistantStateBase;
 namespace assistant_client {
 struct SpeakerIdEnrollmentStatus;
 struct SpeakerIdEnrollmentUpdate;
+struct VoicelessResponse;
 }  // namespace assistant_client
 
 namespace chromeos {
@@ -62,6 +63,8 @@ class AssistantSettingsImpl : public AssistantSettings {
       const assistant_client::SpeakerIdEnrollmentStatus& status);
   void HandleDeviceAppsStatusSync(base::OnceCallback<void(bool)> callback,
                                   const std::string& settings);
+
+  bool ShouldIgnoreResponse(const assistant_client::VoicelessResponse&) const;
 
   ash::AssistantStateBase* assistant_state();
   ash::AssistantController* assistant_controller();
