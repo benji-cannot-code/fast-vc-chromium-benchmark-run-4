cfb028d2ce03dd15d52ad83a4c06ab1eb332013e
Minoru Chikamune <chikamune@chromium.org>
2020-05-11 15:24:35 +0000

    Migrate XRSystem to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: Ic0470437d823c723bec4e2ac429423d53a22cad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2192714
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767309}

--- third_party/blink/renderer/modules/xr/xr_system.cc
+++ third_party/blink/renderer/modules/xr/xr_system.cc
@@ -710,6 +710,9 @@ XRSystem::XRSystem(LocalFrame& frame, int64_t ukm_source_id)
     : ExecutionContextLifecycleObserver(frame.DomWindow()),
       FocusChangedObserver(frame.GetPage()),
       ukm_source_id_(ukm_source_id),
+      service_(frame.DomWindow()),
+      environment_provider_(frame.DomWindow()),
+      receiver_(this, frame.DomWindow()),
       navigation_start_(
           frame.Loader().GetDocumentLoader()->GetTiming().NavigationStart()),
       feature_handle_for_scheduler_(frame.GetFrameScheduler()->RegisterFeature(
@@ -746,10 +749,9 @@ XRFrameProvider* XRSystem::frameProvider() {
   return frame_provider_;
 }
 
-const mojo::AssociatedRemote<
-    device::mojom::blink::XREnvironmentIntegrationProvider>&
+device::mojom::blink::XREnvironmentIntegrationProvider*
 XRSystem::xrEnvironmentProviderRemote() {
-  return environment_provider_;
+  return environment_provider_.get();
 }
 
 void XRSystem::AddEnvironmentProviderErrorHandler(
@@ -802,7 +804,7 @@ void XRSystem::ExitPresent(base::OnceClosure on_exited) {
     }
   }
 
-  if (service_) {
+  if (service_.is_bound()) {
     service_->ExitPresent(std::move(on_exited));
   } else {
     // The service was already shut down, run the callback immediately.
@@ -814,7 +816,7 @@ void XRSystem::SetFramesThrottled(const XRSession* session, bool throttled) {
   // The service only cares if the immersive session is throttling frames.
   if (session->immersive()) {
     // If we have an immersive session, we should have a service.
-    DCHECK(service_);
+    DCHECK(service_.is_bound());
     service_->SetFramesThrottled(throttled);
   }
 }
@@ -888,7 +890,7 @@ ScriptPromise XRSystem::InternalIsSessionSupported(
   // If TryEnsureService() doesn't set |service_|, then we don't have any WebXR
   // hardware, so we need to reject as being unsupported.
   TryEnsureService();
-  if (!service_) {
+  if (!service_.is_bound()) {
     query->Resolve(false, &exception_state);
     return promise;
   }
@@ -939,7 +941,7 @@ void XRSystem::RequestImmersiveSession(LocalFrame* frame,
   // If TryEnsureService() doesn't set |service_|, then we don't have any WebXR
   // hardware.
   TryEnsureService();
-  if (!service_) {
+  if (!service_.is_bound()) {
     query->RejectWithDOMException(DOMExceptionCode::kNotSupportedError,
                                   kNoDevicesMessage, exception_state);
     return;
@@ -1004,7 +1006,8 @@ void XRSystem::RequestInlineSession(LocalFrame* frame,
   // If no sensors are requested, or if we don't have a service and sensors are
   // not required, then just create a sensorless session.
   if (sensor_requirement == SensorRequirement::kNone ||
-      (!service_ && sensor_requirement != SensorRequirement::kRequired)) {
+      (!service_.is_bound() &&
+       sensor_requirement != SensorRequirement::kRequired)) {
     query->Resolve(CreateSensorlessInlineSession());
     return;
   }
@@ -1012,7 +1015,7 @@ void XRSystem::RequestInlineSession(LocalFrame* frame,
   // If we don't have a service, then we don't have any WebXR hardware.
   // If we didn't already create a sensorless session, we can't create a session
   // without hardware, so just reject now.
-  if (!service_) {
+  if (!service_.is_bound()) {
     DVLOG(2) << __func__ << ": rejecting session - no service";
     query->RejectWithDOMException(DOMExceptionCode::kNotSupportedError,
                                   kSessionNotSupported, exception_state);
@@ -1372,7 +1375,7 @@ void XRSystem::AddedEventListener(
   // If we're adding an event listener we should spin up the service, if we can,
   // so that we can actually register for notifications.
   TryEnsureService();
-  if (!service_)
+  if (!service_.is_bound())
     return;
 
   if (event_type == event_type_names::kDevicechange) {
@@ -1470,7 +1473,7 @@ void XRSystem::OnEnvironmentProviderDisconnect() {
 
 void XRSystem::TryEnsureService() {
   // If we already have a service, there's nothing to do.
-  if (service_)
+  if (service_.is_bound())
     return;
 
   // If the service has been disconnected in the past or our context has been
@@ -1495,6 +1498,9 @@ void XRSystem::TryEnsureService() {
 void XRSystem::Trace(Visitor* visitor) {
   visitor->Trace(frame_provider_);
   visitor->Trace(sessions_);
+  visitor->Trace(service_);
+  visitor->Trace(environment_provider_);
+  visitor->Trace(receiver_);
   visitor->Trace(outstanding_support_queries_);
   visitor->Trace(outstanding_request_queries_);
   visitor->Trace(fullscreen_event_manager_);
