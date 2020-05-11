b64fe3eec0da7b2ca36be573a5fdfc4da184540a
Ayu Ishii <ayui@chromium.org>
2020-05-11 19:59:33 +0000

    Idle: Add UKM use counters for IdleDetection
    This change adds IdleDetectionStart to UKM use counters to record usage
    of the API and observe how it is being used to help with improvements
    to the shape of the API.
    
    IdleDetection Explainer: https://github.com/samuelgoto/idle-detection
    
    Change-Id: Ifba08f6816a3a9158ef79f2989b0a7d3fa1c3d4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171709
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767458}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -169,6 +169,7 @@ UseCounterPageLoadMetricsObserver::GetAllowedUkmFeatures() {
           WebFeature::kV8HTMLVideoElement_RequestVideoFrameCallback_Method,
           WebFeature::kV8HTMLVideoElement_CancelVideoFrameCallback_Method,
           WebFeature::kSchemefulSameSiteContextDowngrade,
+          WebFeature::kIdleDetectionStart,
       }));
   return *opt_in_features;
 }
