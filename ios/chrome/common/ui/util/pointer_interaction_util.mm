db361344b28044699381868b77add6df5103020b
edchin <edchin@chromium.org>
2020-05-11 17:35:54 +0000

    [ios] Needs ostream in addition to check_op.h
    Change-Id: I19691f68ac1c9fe06879c8c01c4ea6ad0203c762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2194293
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Auto-Submit: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767374}

--- ios/chrome/common/ui/util/pointer_interaction_util.mm
+++ ios/chrome/common/ui/util/pointer_interaction_util.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/common/ui/util/pointer_interaction_util.h"
 
+#include <ostream>
+
 #include "base/check_op.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
