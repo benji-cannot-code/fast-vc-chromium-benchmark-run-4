b7e5efb308d09fef637d8cf331a74b94b83acdce
Minoru Chikamune <chikamune@chromium.org>
2020-05-11 09:27:30 +0000

    Migrate Lock to use GC mojo wrappers.
    No behavior change. This CL reduces potential risks of use-after-free bugs.
    
    Bug: 1049056
    Change-Id: Ifa6c63b49fd305e5c3a1d109aab01e6cc2261a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186838
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767233}

--- third_party/blink/renderer/modules/locks/lock.h
+++ third_party/blink/renderer/modules/locks/lock.h
@@ -6,13 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_LOCKS_LOCK_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_LOCKS_LOCK_H_
 
-#include "mojo/public/cpp/bindings/associated_remote.h"
-#include "mojo/public/cpp/bindings/remote.h"
 #include "third_party/blink/public/mojom/feature_observer/feature_observer.mojom-blink.h"
 #include "third_party/blink/public/mojom/locks/lock_manager.mojom-blink.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_associated_remote.h"
+#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -27,7 +27,7 @@ class Lock final : public ScriptWrappable,
                    public ExecutionContextLifecycleObserver {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(Lock);
-  USING_PRE_FINALIZER(Lock, Dispose);
+
  public:
   Lock(ScriptState*,
        const String& name,
@@ -38,8 +38,6 @@ class Lock final : public ScriptWrappable,
   ~Lock() override;
 
   void Trace(Visitor*) override;
-
-  void Dispose();
 
   // Lock.idl
   String name() const { return name_; }
@@ -69,9 +67,9 @@ class Lock final : public ScriptWrappable,
 
   // An opaque handle; this one end of a mojo pipe. When this is closed,
   // the lock is released by the back end.
-  mojo::AssociatedRemote<mojom::blink::LockHandle> handle_;
+  HeapMojoAssociatedRemote<mojom::blink::LockHandle> handle_;
 
-  mojo::Remote<mojom::blink::ObservedFeature> lock_lifetime_;
+  HeapMojoRemote<mojom::blink::ObservedFeature> lock_lifetime_;
 
   // LockManager::OnLockReleased() is called when this lock is released, to
   // stop artificially keeping this instance alive. It is necessary in the
