1ad7443056a65af42b2214562fcb08da888ae9e2
Lei Zhang <thestig@chromium.org>
2020-05-11 19:21:21 +0000

    Switch some content/ tests to use simpler base::WriteFile() variant.
    Bug: 418837
    Change-Id: I98f0b1206f622930740109660c649ad3b7ac333d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191574
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767430}

--- content/browser/native_file_system/native_file_system_file_writer_impl_browsertest.cc
+++ content/browser/native_file_system/native_file_system_file_writer_impl_browsertest.cc
@@ -63,8 +63,7 @@ class NativeFileSystemFileWriterBrowserTest : public ContentBrowserTest {
     base::FilePath test_file;
     EXPECT_TRUE(
         base::CreateTemporaryFileInDir(temp_dir_.GetPath(), &test_file));
-    EXPECT_EQ(int{contents.size()},
-              base::WriteFile(test_file, contents.data(), contents.size()));
+    EXPECT_TRUE(base::WriteFile(test_file, contents));
 
     ui::SelectFileDialog::SetFactory(
         new FakeSelectFileDialogFactory({test_file}));
@@ -90,9 +89,7 @@ class NativeFileSystemFileWriterBrowserTest : public ContentBrowserTest {
     base::FilePath test_file =
         temp_dir_.GetPath().AppendASCII("to_be_quarantined.exe");
     std::string file_data = "hello world!";
-    int file_size = static_cast<int>(file_data.size());
-    EXPECT_EQ(file_size,
-              base::WriteFile(test_file, file_data.c_str(), file_size));
+    EXPECT_TRUE(base::WriteFile(test_file, file_data));
 
     ui::SelectFileDialog::SetFactory(
         new FakeSelectFileDialogFactory({test_file}));
