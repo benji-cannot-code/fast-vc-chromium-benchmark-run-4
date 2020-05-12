5da6ae7dcd03c3ecf69de5f41f0f8458e7c86fa8
Etienne Pierre-Doray <etiennep@chromium.org>
2020-05-12 15:24:01 +0000

    Revert "IsUIAControl to return false for ignored & invisible nodes"
    This reverts commit d4da4b0e3d43e97025e72ab87f9b0767d9448a45.
    
    Reason for revert: DumpAccessibilityEventsTest.AccessibilityEventsMenuListCollapse failing on win (crbug.com/1081662)
    
    Original change's description:
    > IsUIAControl to return false for ignored & invisible nodes
    > 
    > Consider the following buttons that are hidden in the a11y tree:
    >   <button aria-hidden="true">button</button>
    > 
    > Whose a11y tree is the following
    >   rootWebArea
    >   ++button invisible "button"
    > 
    > Currently, Windows Narrator is able to perceive an aria-hidden="true"
    > button. The expected behavior is that Narrator should not see the
    > button.
    > 
    > The issue here is that Windows Narrator looks at UIA Control View to
    > determine if an element should be announced. AXPlatformNodeWin::
    > IsUIAControl() determines if a node should be in the control view.
    > Currently, IsUIAControl does not take into account of ignored or
    > invisible state for web content elements.
    > 
    > This CL fixes this Narrator issue by making IsUIAControl return false
    > for all web content elements with invisible or ignored state, thus
    > excluding them from the UIA Control view.
    > 
    > This change also revealed one quirk with UIA. If an element is marked
    > as ignored or invisible (i.e. IsUIAControlElement return false), in
    > the UIA event model, UIA event listener will normalize the event
    > source to be the parent of the ignored or invisible element. I also
    > updated some of the UIA event tests due to this UIA quirk.
    > 
    > Bug: 651614
    > Change-Id: Icc997c09da29cbfdbcee2be3cc1026957c9a7e41
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2182168
    > Commit-Queue: Victor Fei <vicfei@microsoft.com>
    > Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    > Reviewed-by: Ian Prest <iapres@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#767491}
    
    TBR=kbabbitt@microsoft.com,kschmi@microsoft.com,iapres@microsoft.com,vicfei@microsoft.com
    
    Change-Id: Ied8eb32fc277bb5ef2639402cccfea23814b1cae
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 651614, 1081662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196425
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767801}

--- ui/accessibility/platform/ax_platform_node_win.cc
+++ ui/accessibility/platform/ax_platform_node_win.cc
@@ -6801,12 +6801,7 @@ bool AXPlatformNodeWin::IsUIAControl() const {
   // UIA provides multiple "views": raw, content and control. We only want to
   // populate the content and control views with items that make sense to
   // traverse over.
-
   if (GetDelegate()->IsWebContent()) {
-    // Invisible or ignored elements should not show up in control view at all.
-    if (IsInvisibleOrIgnored())
-      return false;
-
     if (IsTextOnlyObject()) {
       // A text leaf can be a UIAControl, but text inside of a heading, link,
       // button, etc. where the role allows the name to be generated from the
@@ -6846,8 +6841,7 @@ bool AXPlatformNodeWin::IsUIAControl() const {
         }
         parent = FromNativeViewAccessible(parent->GetParent());
       }
-    }  // end of text only case.
-
+    }
     const AXNodeData& data = GetData();
     // https://docs.microsoft.com/en-us/windows/win32/winauto/uiauto-treeoverview#control-view
     // The control view also includes noninteractive UI items that contribute
@@ -6899,10 +6893,9 @@ bool AXPlatformNodeWin::IsUIAControl() const {
         !data.HasState(ax::mojom::State::kFocusable) && !data.IsClickable()) {
       return false;
     }
-
     return true;
-  }  // end of web-content only case.
-
+  }
+  // non web-content case.
   const AXNodeData& data = GetData();
   return !((IsReadOnlySupported(data.role) && data.IsReadOnlyOrDisabled()) ||
            data.HasState(ax::mojom::State::kInvisible) ||
