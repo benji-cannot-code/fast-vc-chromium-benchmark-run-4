666d3a8d2951116c62ed743b541d0d04e3a20e70
Sam Bowen <sgbowen@google.com>
2020-05-12 21:45:45 +0000

    [Media Feeds] Fix two bugs related to content attributes.
    * The current max entity nesting depth in the extractor is not deep
      enough to support content attributes nested inside episodes. Increase
      it to a safe upper bound.
    
    * Play next candidate should use the new image conversion method to
      preserve content attributes.
    
    Bug: 1081448
    Change-Id: I2beb18cb1a143c8263aa6646cc7c27f923b83609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196815
    Reviewed-by: Becca Hughes <beccahughes@chromium.org>
    Commit-Queue: Sam Bowen <sgbowen@google.com>
    Cr-Commit-Position: refs/heads/master@{#767978}

--- chrome/browser/media/history/media_history_feed_items_table.cc
+++ chrome/browser/media/history/media_history_feed_items_table.cc
@@ -147,13 +147,6 @@ void FillLiveDetails(const media_feeds::mojom::LiveDetailsPtr& live_details,
         live_details->end_time->ToDeltaSinceWindowsEpoch().InSeconds());
 }
 
-void FillImage(const media_feeds::mojom::MediaImagePtr& image,
-               media_feeds::Image* proto) {
-  proto->set_url(image->src.spec());
-  proto->set_width(image->size.width());
-  proto->set_height(image->size.height());
-}
-
 }  // namespace
 
 const char MediaHistoryFeedItemsTable::kTableName[] = "mediaFeedItem";
@@ -384,7 +377,7 @@ bool MediaHistoryFeedItemsTable::SaveItem(
       FillIdentifier(identifier, play_next_candidate.add_identifier());
 
     for (auto& image : item->play_next_candidate->images)
-      FillImage(image, play_next_candidate.add_image());
+      media_feeds::MediaImageToProto(play_next_candidate.add_image(), image);
 
     BindProto(statement, 19, play_next_candidate);
   } else {
