76ce92e9c887bed2d9328e4e7850e6ee8a4761fa
Kazuki Takise <takise@google.com>
2020-05-12 03:25:32 +0000

    Make frame type committable
    Currently frame type is not synchronous with other committable
    properties, so we have a few issues (e.g. b/140201157) and random
    hacks (e.g. b/138996293) in exo.
    
    Now that we can break ARC NYC, we make it committable to fix those
    issues. As ARC P already has support to do so, we can safely make
    change this in client controlled shell surface. Note that we keep
    this change inside client controlled shell surface as this can
    break crostini.
    
    BUG=b:140201157
    BUG=b:141151475
    BUG=b:138996293
    TEST=Manually confirmed PIP didn't have an incorrect bounds when
         exiting PIP.
    TEST=Manually confirmed ARC windows had the correct shadow bounds.
    
    Change-Id: If2c58cdd67beefbf1947db02cd2b638ab5332840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2093964
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767655}

--- components/exo/shell_surface_base.h
+++ components/exo/shell_surface_base.h
@@ -219,6 +219,8 @@ class ShellSurfaceBase : public SurfaceTreeHost,
   // |shadow_bounds_|.
   void UpdateShadow();
 
+  virtual void UpdateFrameType();
+
   // Applies |system_modal_| to |widget_|.
   void UpdateSystemModal();
 
@@ -280,7 +282,7 @@ class ShellSurfaceBase : public SurfaceTreeHost,
 
   // Commit is deferred if this returns false.
   virtual bool OnPreWidgetCommit() = 0;
-  virtual void OnPostWidgetCommit() = 0;
+  virtual void OnPostWidgetCommit();
 
   void CommitWidget();
 
