53d828f515fd3e6ff1835a732323f503f077098c
Mugdha Lakhani <nator@chromium.org>
2020-05-12 18:29:04 +0000

    [WebLayer] Componentize SettingsLauncher.
    Move SettingsLauncher to components/ and update references.
    WebLayer needs to implement this.
    
    Bug: 1058595, 1075773
    Change-Id: Ifd461449d4fa27daca6331b08091372bc17db632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2185072
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Auto-Submit: Mugdha Lakhani <nator@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767886}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsLauncherImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsLauncherImpl.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import org.chromium.base.IntentUtils;
+import org.chromium.components.browser_ui.settings.SettingsLauncher;
 
 /**
  * Implementation class for launching a {@link SettingsActivity}.
