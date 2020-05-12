317c1c5abe671f898556be8a2661379bcea7964e
Lei Zhang <thestig@chromium.org>
2020-05-12 08:20:30 +0000

    Clean up os_crypt usage in headless/.
    Change-Id: I5b9704f908e2917ac218ad975b3a9d077462fb71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191585
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767693}

--- headless/app/headless_shell.cc
+++ headless/app/headless_shell.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "cc/base/switches.h"
-#include "components/os_crypt/os_crypt_switches.h"
 #include "components/viz/common/switches.h"
 #include "content/public/app/content_main.h"
 #include "content/public/browser/browser_thread.h"
@@ -55,6 +54,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/crash/core/app/crash_switches.h"
 #include "components/crash/core/app/run_as_crashpad_handler_win.h"
 #include "sandbox/win/src/sandbox_types.h"
+#endif
+
+#if defined(OS_MACOSX)
+#include "components/os_crypt/os_crypt_switches.h"
 #endif
 
 namespace headless {
--- headless/lib/browser/headless_request_context_manager.cc
+++ headless/lib/browser/headless_request_context_manager.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
-#include "components/os_crypt/key_storage_config_linux.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/cors_exempt_headers.h"
 #include "content/public/browser/network_service_instance.h"
@@ -29,7 +28,7 @@ namespace headless {
 namespace {
 
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-static char kProductName[] = "HeadlessChrome";
+constexpr char kProductName[] = "HeadlessChrome";
 #endif
 
 net::NetworkTrafficAnnotationTag GetProxyConfigTrafficAnnotationTag() {
