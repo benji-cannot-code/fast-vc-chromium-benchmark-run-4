c190434adc886306e551214a3878469b680d6103
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-05-12 23:13:42 +0000

    Add use counters for %-based row-gaps within grid and flex.
    As above. Additionally this adds a usecounter for when the initial
    height is indefinite, and we can't initially resolve the row-gap.
    
    Change-Id: Ic3fef9eaa48a3b3cc40e89d5429e80dd049a7e26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196641
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Manuel Rego <rego@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#768024}

--- third_party/blink/renderer/core/layout/layout_grid.cc
+++ third_party/blink/renderer/core/layout/layout_grid.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "third_party/blink/public/mojom/web_feature/web_feature.mojom-blink.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/layout/grid_layout_utils.h"
 #include "third_party/blink/renderer/core/layout/layout_state.h"
@@ -377,6 +378,15 @@ void LayoutGrid::UpdateBlockLayout(bool relayout_children) {
 
     LayoutGridItems();
     track_sizing_algorithm_.Reset();
+
+    if (NumTracks(kForRows, *grid_) > 1u && !StyleRef().RowGap().IsNormal() &&
+        StyleRef().RowGap().GetLength().IsPercentOrCalc()) {
+      UseCounter::Count(GetDocument(), WebFeature::kGridRowGapPercent);
+      if (!CachedHasDefiniteLogicalHeight()) {
+        UseCounter::Count(GetDocument(),
+                          WebFeature::kGridRowGapPercentIndefinite);
+      }
+    }
 
     if (Size() != previous_size)
       relayout_children = true;
