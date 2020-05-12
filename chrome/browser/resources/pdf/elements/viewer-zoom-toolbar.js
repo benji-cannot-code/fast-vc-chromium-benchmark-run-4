2a4a95033bcaa325488b8959866067b606b7d49c
rbpotter <rbpotter@chromium.org>
2020-05-12 22:50:10 +0000

    PDF viewer: Exclude resources in Print Preview
    Add a separate index.html for the PDF viewer in Print Preview vs the
    normal viewer. This allows resources not used in Print Preview to be
    excluded from the data source, and not imported at runtime, saving
    ~18ms (5%) of the time to load the viewer in an optimized build.
    
    Also add a few tests for the viewer in Print Preview.
    
    Change-Id: I6599bb61d061cf42b71ef26b48736bf7d69cdfaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2183405
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768013}

--- chrome/browser/resources/pdf/elements/viewer-zoom-toolbar.js
+++ chrome/browser/resources/pdf/elements/viewer-zoom-toolbar.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/hidden_style_css.m.js';
 import 'chrome://resources/cr_elements/icons.m.js';
 import './icons.js';
 import './viewer-zoom-button.js';
