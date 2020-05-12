c354f794090e68e1afb21a99490b82f9bfb7a621
Michael Lippautz <mlippautz@chromium.org>
2020-05-12 11:49:42 +0000

    heap: Rework visitation for WTF collections
    In the past blink::Visitor has provided Trace*() methods specifically
    for backing stores that bundle multiple uses into single methods.
    E.g., VisitBackingStoreWeakly would implement weakness handling and
    register slots for movable references.
    
    With the move into V8, we split the methods apart to avoid
    specializing for a WTF use case. In practice this means that slot
    registration for movable references is kept separate from weakness
    handling.
    
    Change-Id: I699de9a3d1d63d34d6d2cc705ebbbecb8cb1616a
    Bug: chromium:1056170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2193796
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Ulan Degenbaev <ulan@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767751}

--- third_party/blink/renderer/platform/heap/incremental_marking_test.cc
+++ third_party/blink/renderer/platform/heap/incremental_marking_test.cc
@@ -69,18 +69,6 @@ class BackingVisitor : public Visitor {
                  const void* object_weak_ref,
                  TraceDescriptor desc,
                  WeakCallback callback) final {}
-  void VisitBackingStoreStrongly(const void* object,
-                                 const void* const* object_slot,
-                                 TraceDescriptor desc) final {}
-  void VisitBackingStoreWeakly(const void*,
-                               const void* const*,
-                               TraceDescriptor,
-                               TraceDescriptor,
-                               WeakCallback,
-                               const void*) final {}
-  void VisitBackingStoreOnly(const void*, const void* const*) final {}
-  void RegisterBackingStoreCallback(const void* slot,
-                                    MovingObjectCallback) final {}
   void RegisterWeakCallback(WeakCallback, const void*) final {}
   void Visit(const TraceWrapperV8Reference<v8::Value>&) final {}
 
@@ -1542,7 +1530,7 @@ TEST_F(IncrementalMarkingTest, WeakHashMapHeapCompaction) {
   driver.FinishGC();
 
   // Weak callback should register the slot.
-  EXPECT_EQ(driver.GetHeapCompactLastFixupCount(), 2u);
+  EXPECT_EQ(1u, driver.GetHeapCompactLastFixupCount());
 }
 
 TEST_F(IncrementalMarkingTest, ConservativeGCWhileCompactionScheduled) {
