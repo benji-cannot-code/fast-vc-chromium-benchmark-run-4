44af824fc103e6899540a683387f0c293865dde4
David Tseng <dtseng@chromium.org>
2020-05-12 23:42:11 +0000

    Re-enable ChromeVoxEditingTest.BackwardWordDelete
    This test appears to no longer be flakey. Tentitively re-enabling. e.g. on chromeos-dbg
    
    TBR=dtseng@chromium.org
    
    Bug: 1033983
    Change-Id: I3400f7cc1eafe07e7ed8bd69872c54c101dd72e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2197504
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768039}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing_test.js
@@ -1193,8 +1193,7 @@ TEST_F('ChromeVoxEditingTest', 'TelTrimsWhitespace', function() {
       });
 });
 
-// TODO(https://crbug.com/1033983) flaky on linux-chromeos-rel/dbg.
-TEST_F('ChromeVoxEditingTest', 'DISABLED_BackwardWordDelete', function() {
+TEST_F('ChromeVoxEditingTest', 'BackwardWordDelete', function() {
   const mockFeedback = this.createMockFeedback();
   this.runWithLoadedTree(
       `
