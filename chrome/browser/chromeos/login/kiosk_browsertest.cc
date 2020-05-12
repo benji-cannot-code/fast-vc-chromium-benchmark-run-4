ff16709d82ea7649609325ea501b4b0c4064aed3
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-05-12 13:05:48 +0000

    [Kiosk] Do not allow non-app type browser windows in kiosk mode
    Just to be on a safe side, if the newly created browser type is not app,
    we should close it. This way we will protect ourselves from extensions
    trying to open random whitelisted webpages in a normal browser.
    
    Bug: 1015383
    Change-Id: I186c959575fa136eabfd981f1e40c69846e36574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195241
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767762}

--- chrome/browser/chromeos/login/kiosk_browsertest.cc
+++ chrome/browser/chromeos/login/kiosk_browsertest.cc
@@ -1400,6 +1400,7 @@ IN_PROC_BROWSER_TEST_F(KioskTest, SettingsWindow) {
     content::TestNavigationObserver settings_navigation_observer(web_contents,
                                                                  1);
     NavigateParams params(profile, page2, ui::PAGE_TRANSITION_AUTO_BOOKMARK);
+    params.disposition = WindowOpenDisposition::NEW_POPUP;
     Navigate(&params);
     // Wait for browser to be handled.
     base::RunLoop waiter;
