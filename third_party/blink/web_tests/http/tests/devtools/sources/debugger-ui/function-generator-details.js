7f8ce290df866bd921d52fff7a987ba438532418
Jack Franklin <jacktfranklin@chromium.org>
2020-05-12 13:58:38 +0000

    Remove use of toTitleCase in layout tests
    It's a method that's added to the String prototype by DevTools frontend,
    but we are removing that in a CL [1]. The layout tests need to be
    updated to not depend on it. They don't need the expression to be in
    title case so we'll just change the tests to use the expression as is.
    
    [1]: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2193590
    
    Change-Id: I7ac36003423ded30ec42072a0667d66af590215c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195962
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767773}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger-ui/function-generator-details.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger-ui/function-generator-details.js
@@ -87,7 +87,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ];
 
   function createTestSuiteFunction(expression) {
-    var functionName = 'test' + expression.toTitleCase();
+    var functionName = 'test' + expression;
     return eval(
         'function ' + functionName + '(next)\n' +
         '{\n' +
