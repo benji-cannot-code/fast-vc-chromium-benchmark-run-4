7f59cc944efe782185bc1beb65d3ae3899837c2a
Victor Costan <pwnall@chromium.org>
2020-05-12 00:27:40 +0000

    Roll src/third_party/leveldatabase/src/ a0191e556..5bd5f0f67 (10 commits)
    https://chromium.googlesource.com/external/leveldb.git/+log/a0191e5563b7..5bd5f0f67a5e
    
    This CL also removes kLittleEndian from port_chromium.h.
    
    $ git log a0191e556..5bd5f0f67 --date=short --no-merges --format='%ad %ae %s'
    2020-04-30 costan Fix Travis CI build.
    2020-04-30 costan Fix accidental double std:: qualifiers.
    2020-04-29 costan Add some std:: qualifiers to types and functions.
    2020-04-29 costan Switch from C headers to C++ headers.
    2020-04-28 costan Fix C++11 build.
    2020-04-17 no-reply change const to constexpr
    2020-04-24 imjunwang remove unnessary status judge
    2020-04-13 costan Remove leveldb::port::kLittleEndian.
    2020-04-13 costan Use LLVM 10 on Travis CI.
    2020-01-10 costan Remove Windows workarounds in some tests.
    
    Created with:
      roll-dep src/third_party/leveldatabase/src
    
    Change-Id: I11cf3f1aed658bd63ee0ddce1347f6bec4c58d9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2194591
    Reviewed-by: Chris Mumford <cmumford@google.com>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767596}

--- third_party/leveldatabase/port/port_chromium.h
+++ third_party/leveldatabase/port/port_chromium.h
@@ -21,9 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace leveldb {
 namespace port {
 
-// Chromium only supports little endian.
-static const bool kLittleEndian = true;
-
 class LOCKABLE Mutex {
  public:
   Mutex() = default;
