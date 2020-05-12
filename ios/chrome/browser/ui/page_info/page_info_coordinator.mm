e6e88df2d404b48512df2ed0136160f53c03937b
Ewann <ewannpv@google.com>
2020-05-12 11:19:42 +0000

    [iOS][Page-Info] Clean page info
    This CL cleans the new implementation of the page info because the new design has been changed.
    
    Bug: 1038919
    Change-Id: If95d38659ccb6e6d1293a5cbf8f31f098cee75ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2193373
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767742}

--- ios/chrome/browser/ui/page_info/page_info_coordinator.mm
+++ ios/chrome/browser/ui/page_info/page_info_coordinator.mm
@@ -7,14 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/reading_list/offline_page_tab_helper.h"
-#import "ios/chrome/browser/ui/commands/browser_commands.h"
-#import "ios/chrome/browser/ui/commands/command_dispatcher.h"
-#import "ios/chrome/browser/ui/page_info/page_info_cookies_view_controller.h"
-#import "ios/chrome/browser/ui/page_info/page_info_mediator.h"
-#import "ios/chrome/browser/ui/page_info/page_info_navigation_commands.h"
 #import "ios/chrome/browser/ui/page_info/page_info_site_security_description.h"
 #import "ios/chrome/browser/ui/page_info/page_info_site_security_mediator.h"
-#import "ios/chrome/browser/ui/page_info/page_info_site_security_view_controller.h"
 #import "ios/chrome/browser/ui/page_info/page_info_view_controller.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #include "ios/web/public/navigation/navigation_item.h"
@@ -25,11 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
-@interface PageInfoCoordinator () <PageInfoNavigationCommands>
+@interface PageInfoCoordinator ()
 
 @property(nonatomic, strong) CommandDispatcher* dispatcher;
 @property(nonatomic, strong) PageInfoViewController* viewController;
-@property(nonatomic, strong) PageInfoMediator* mediator;
 @property(nonatomic, strong) UINavigationController* navigationController;
 
 @end
@@ -42,20 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ChromeCoordinator
 
 - (void)start {
-  self.dispatcher = self.browser->GetCommandDispatcher();
-  [self.dispatcher
-      startDispatchingToTarget:self
-                   forProtocol:@protocol(PageInfoNavigationCommands)];
-
   self.viewController =
       [[PageInfoViewController alloc] initWithStyle:UITableViewStylePlain];
-  self.viewController.handler =
-      HandlerForProtocol(self.dispatcher, PageInfoNavigationCommands);
-
-  web::WebState* webState =
-      self.browser->GetWebStateList()->GetActiveWebState();
-  self.mediator = [[PageInfoMediator alloc] initWithWebState:webState];
-  self.mediator.consumer = self.viewController;
 
   self.navigationController = [[UINavigationController alloc]
       initWithRootViewController:self.viewController];
@@ -66,46 +47,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stop {
-  [self.dispatcher stopDispatchingToTarget:self];
   [self.baseViewController.presentedViewController
       dismissViewControllerAnimated:YES
                          completion:nil];
-  self.dispatcher = nil;
   self.navigationController = nil;
-  self.mediator.consumer = nil;
-  self.mediator = nil;
   self.viewController = nil;
-}
-
-#pragma mark - PageInfoNavigationCommands
-
-- (void)showSiteSecurityInfo {
-  web::WebState* webState =
-      self.browser->GetWebStateList()->GetActiveWebState();
-  web::NavigationItem* navItem =
-      webState->GetNavigationManager()->GetVisibleItem();
-
-  bool offlinePage =
-      OfflinePageTabHelper::FromWebState(webState)->presenting_offline_page();
-
-  PageInfoSiteSecurityDescription* description =
-      [PageInfoSiteSecurityMediator configurationForURL:navItem->GetURL()
-                                              SSLStatus:navItem->GetSSL()
-                                            offlinePage:offlinePage];
-
-  PageInfoSiteSecurityViewController* viewController =
-      [[PageInfoSiteSecurityViewController alloc]
-          initWitDescription:description];
-
-  viewController.handler = HandlerForProtocol(self.dispatcher, BrowserCommands);
-  [self.navigationController pushViewController:viewController animated:YES];
-}
-
-- (void)showCookiesInfo {
-  PageInfoCookiesViewController* viewController =
-      [[PageInfoCookiesViewController alloc]
-          initWithStyle:UITableViewStylePlain];
-  [self.navigationController pushViewController:viewController animated:YES];
 }
 
 @end
