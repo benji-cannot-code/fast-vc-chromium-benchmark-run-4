16de460b3a11bde0bee7ef2dbb778df5f1e5c042
Ian Kilpatrick <ikilpatrick@chromium.org>
2020-05-12 04:43:53 +0000

    [cleanup] Remove NGConstraintSpace::ForceTruncateAtLineClamp
    This patch removes the ForceTruncateAtLineClamp flag.
    Previously this flag would typically be set to true if line-clamp was
    present, and then if we didn't and any additional content, we'd relayout
    again with this flag set to false.
    
    This patch removes this flag, in favour of a top level switch:
    NGBlockLayoutAlgorithm::ignore_line_clamp_.
    
    If this flag is set to true, we simply ignore the line-clamp property,
    which causes no line-clamping to occur.
    
    Change-Id: Ifce9ad7b9cb2d59830d030e16b8c6f43a8578f27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191979
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767667}

--- third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
+++ third_party/blink/renderer/core/layout/ng/ng_constraint_space.h
@@ -559,10 +559,6 @@ class CORE_EXPORT NGConstraintSpace final {
     return HasRareData() ? rare_data_->ClearanceOffset() : LayoutUnit::Min();
   }
 
-  bool ForceTruncateAtLineClamp() const {
-    return HasRareData() ? rare_data_->ForceTruncateAtLineClamp() : true;
-  }
-
   base::Optional<int> LinesUntilClamp() const {
     return HasRareData() ? rare_data_->LinesUntilClamp() : base::nullopt;
   }
@@ -836,16 +832,6 @@ class CORE_EXPORT NGConstraintSpace final {
       EnsureBlockData()->lines_until_clamp = value;
     }
 
-    int ForceTruncateAtLineClamp() const {
-      return data_union_type == kBlockData
-                 ? block_data_.force_truncate_at_line_clamp
-                 : true;
-    }
-
-    void SetForceTruncateAtLineClamp(bool value) {
-      EnsureBlockData()->force_truncate_at_line_clamp = value;
-    }
-
     NGBoxStrut TableCellBorders() const {
       return data_union_type == kTableCellData
                  ? table_cell_data_.table_cell_borders
@@ -935,13 +921,11 @@ class CORE_EXPORT NGConstraintSpace final {
    private:
     struct BlockData {
       bool MaySkipLayout(const BlockData& other) const {
-        return lines_until_clamp == other.lines_until_clamp &&
-               force_truncate_at_line_clamp ==
-                   other.force_truncate_at_line_clamp;
+        return lines_until_clamp == other.lines_until_clamp;
       }
 
       bool IsInitialForMaySkipLayout() const {
-        return !lines_until_clamp.has_value() && force_truncate_at_line_clamp;
+        return !lines_until_clamp.has_value();
       }
 
       NGMarginStrut margin_strut;
@@ -949,9 +933,6 @@ class CORE_EXPORT NGConstraintSpace final {
       base::Optional<LayoutUnit> forced_bfc_block_offset;
       LayoutUnit clearance_offset = LayoutUnit::Min();
       base::Optional<int> lines_until_clamp;
-      // If true and |lines_until_clamp| == 1, then the line should be truncated
-      // regardless of whether there is more text that follows on the line.
-      bool force_truncate_at_line_clamp = true;
     };
 
     struct TableCellData {
