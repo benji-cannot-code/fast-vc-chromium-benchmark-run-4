38a97d1b809e334ed8856cf9b4fcdf8f1fb414a9
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-05-12 22:54:43 +0000

    [Kiosk] Turn non-app browser windows in app windows in kiosk session
    Turns out some a11y extensions are using apis which create normal
    browsers instead of app browser(Select-to-Speak). And this is widely
    used inside of the OS Settings page. We need to make it work for these
    extensions by closing the non-app type browser and create a new, browser
    type one.
    
    Bug: 1015383
    Change-Id: I320cb87758c30e67ecd05f2fe2c2c526774c00c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196488
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768015}

--- chrome/browser/chromeos/app_mode/app_session.h
+++ chrome/browser/chromeos/app_mode/app_session.h
@@ -47,7 +47,7 @@ class AppSession : public KioskSessionPluginHandlerDelegate {
   void SetAttemptUserExitForTesting(base::OnceClosure closure);
 
   Browser* GetSettingsBrowserForTesting() { return settings_browser_; }
-  void SetOnHandleBrowserCallbackForTesting(base::OnceClosure closure);
+  void SetOnHandleBrowserCallbackForTesting(base::RepeatingClosure closure);
 
  private:
   // AppWindowHandler watches for app window and exits the session when the
@@ -77,10 +77,12 @@ class AppSession : public KioskSessionPluginHandlerDelegate {
   // KioskSettingsNavigationThrottle.
   Browser* settings_browser_ = nullptr;
 
+  Profile* profile_ = nullptr;
+
   base::OnceClosure attempt_user_exit_;
   // Is called whenever a new browser creation was handled by the
   // BrowserWindowHandler.
-  base::OnceClosure on_handle_browser_callback_;
+  base::RepeatingClosure on_handle_browser_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(AppSession);
 };
