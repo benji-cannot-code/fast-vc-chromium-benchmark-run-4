fa40e9092993b745c1e6f06f38d3326f7fdec3ef
Trent Apted <tapted@chromium.org>
2020-05-12 23:08:31 +0000

    Refresh open files from handles on navigation.
    Files can be changed or renamed or moved while chrome://media-app is
    running, and this currently breaks its ability to navigate. The DOM
    File machinery checks modification timestamps and requires a File to be
    reopened if it changes.
    
    Rather than keeping an opened File in the file descriptors held in the
    privileged context, we set them all to null whenever they are sent to
    the guest context. Navigation reopens (but doesn't read) the files to
    ensure recent handles are passed.
    
    Bug: b/154550689
    Change-Id: I0fa19efd03752069f168015a0210c412cd553640
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186873
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768021}

--- chromeos/components/media_app_ui/resources/js/receiver.js
+++ chromeos/components/media_app_ui/resources/js/receiver.js
@@ -12,14 +12,15 @@ const parentMessagePipe = new MessagePipe('chrome://media-app', window.parent);
  */
 class ReceivedFile {
   /**
-   * @param {!File} file The received file.
+   * @param {?File} file The received file.
    * @param {number} token A token that identifies the file.
+   * @param {string} fallbackName The name to use when `file` is null/empty.
    */
-  constructor(file, token) {
-    this.blob = file;
-    this.name = file.name;
-    this.size = file.size;
-    this.mimeType = file.type;
+  constructor(file, token, fallbackName) {
+    this.blob = file || new File([], fallbackName);
+    this.name = this.blob.name;
+    this.size = this.blob.size;
+    this.mimeType = this.blob.type;
     this.token = token;
   }
 
@@ -94,7 +95,7 @@ class ReceivedFileList {
 
     this.length = files.length;
     /** @type {!Array<!ReceivedFile>} */
-    this.files = files.map(f => new ReceivedFile(f.file, f.token));
+    this.files = files.map(f => new ReceivedFile(f.file, f.token, f.name));
     /** @type {number} */
     this.writableFileIndex = 0;
   }
