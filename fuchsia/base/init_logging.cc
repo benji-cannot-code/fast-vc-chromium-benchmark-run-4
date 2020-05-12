a107bd92e6f85ae5cf67f7503fb5e8a52a5154db
Chong Gu <chonggu@google.com>
2020-05-12 02:44:24 +0000

    [Fuchsia] Flag support for web_engine_shell.
    If additional flags are passed in, create a private WebEngine instance and pass the flags to it directly.
    
    Bug: 1075696
    Change-Id: I673b8a9e025530a6ecf477846c5bb19a1b2e2aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168351
    Commit-Queue: Chong Gu <chonggu@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767646}

--- fuchsia/base/init_logging.cc
+++ fuchsia/base/init_logging.cc
@@ -9,13 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 
 namespace cr_fuchsia {
-namespace {
 
-// These are intended to match those in content_switches.cc.
 constexpr char kEnableLogging[] = "enable-logging";
 constexpr char kLogFile[] = "log-file";
-
-}  // namespace
 
 bool InitLoggingFromCommandLine(const base::CommandLine& command_line) {
   logging::LoggingSettings settings;
