059b804043f925bede115ede3b6172f1ee4e4de2
nancy <nancylingwang@chromium.org>
2020-05-12 17:10:08 +0000

    Fix the shelf icon load issue when the app is not pinned to the shelf.
    If the unpinned app has been launched for the second user, when switch
    to the second user, AppServiceAppIconLoader is called to load the icon
    at very early stage for the launched apps due to the session restore
    process. But ChromeLauncherController::ActiveUserChanged is not called
    yet, so AppServiceAppIconLoader for the first user is still used to
    load icons. Then ChromeLauncherController::ActiveUserChanged is called,
    and AppServiceAppIconLoader for the first user is destroyed, so the load
    icon callback can't be called to load icons.
    
    Modify ChromeLauncherController's app_icon_loaders_ to use a per profile
    based map. When loading icons, select the correct profile's
    app_icon_loaders_ to load icons.
    
    Modify ChromeLauncherController's OnAppInstalled and OnAppUpdated to
    reload icons for both the pinned app and launched unpinned app.
    
    Modify LauncherControllerHelper::GetAppTitle to use extensions to set
    the title for extensions only.
    
    BUG=1076540
    
    Change-Id: I432047a4c965422f4721acb1fa6d6885f96faa01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2189999
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#767849}

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller.h
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_ASH_LAUNCHER_CHROME_LAUNCHER_CONTROLLER_H_
 #define CHROME_BROWSER_UI_ASH_LAUNCHER_CHROME_LAUNCHER_CONTROLLER_H_
 
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -364,6 +365,9 @@ class ChromeLauncherController
   void CloseWindowedAppsFromRemovedExtension(const std::string& app_id,
                                              const Profile* profile);
 
+  // Add the app updater and the app icon loder for a specific profile.
+  void AddAppUpdaterAndIconLoader(Profile* profile);
+
   // Attach to a specific profile.
   void AttachProfile(Profile* profile_to_attach);
 
@@ -399,6 +403,11 @@ class ChromeLauncherController
   // multi-profile use cases this might change over time.
   Profile* profile_ = nullptr;
 
+  // The profile used to load icons and get the app update information. This is
+  // the latest active user's profile when switch users in multi-profile use
+  // cases.
+  Profile* latest_active_profile_ = nullptr;
+
   // The ShelfModel instance owned by ash::Shell's ShelfController.
   ash::ShelfModel* model_;
 
@@ -428,7 +437,8 @@ class ChromeLauncherController
   std::unique_ptr<DiscoverWindowObserver> discover_window_observer_;
 
   // Used to load the images for app items.
-  std::vector<std::unique_ptr<AppIconLoader>> app_icon_loaders_;
+  std::map<Profile*, std::vector<std::unique_ptr<AppIconLoader>>>
+      app_icon_loaders_;
 
   // Direct access to app_id for a web contents.
   // NOTE: This tracks all WebContents, not just those associated with an app.
@@ -442,7 +452,8 @@ class ChromeLauncherController
   ArcAppWindowLauncherController* arc_app_window_controller_ = nullptr;
 
   // Used to handle app load/unload events.
-  std::vector<std::unique_ptr<LauncherAppUpdater>> app_updaters_;
+  std::map<Profile*, std::vector<std::unique_ptr<LauncherAppUpdater>>>
+      app_updaters_;
 
   PrefChangeRegistrar pref_change_registrar_;
 
