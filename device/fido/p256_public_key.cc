d4091f9ac2ae9fccbcf16cbac0774573394f36ca
Adam Langley <agl@chromium.org>
2020-05-12 00:14:28 +0000

    device/fido: hold SPKI versions of public keys.
    In preparation for https://github.com/w3c/webauthn/pull/1395, transform
    credential public keys into SPKI form.
    
    Change-Id: Iabbadd7802788bb323711ca754d1f04634ecfb6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2194374
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/master@{#767583}

--- device/fido/p256_public_key.cc
+++ device/fido/p256_public_key.cc
@@ -12,7 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/fido/fido_constants.h"
 #include "device/fido/public_key.h"
 #include "third_party/boringssl/src/include/openssl/bn.h"
+#include "third_party/boringssl/src/include/openssl/bytestring.h"
 #include "third_party/boringssl/src/include/openssl/ec.h"
+#include "third_party/boringssl/src/include/openssl/evp.h"
+#include "third_party/boringssl/src/include/openssl/mem.h"
 #include "third_party/boringssl/src/include/openssl/obj.h"
 
 namespace device {
@@ -26,6 +29,31 @@ constexpr size_t kFieldElementLength = 32;
 // P-256. It's one byte of type information followed by two field elements (x
 // and y).
 constexpr size_t kUncompressedPointLength = 1 + 2 * kFieldElementLength;
+
+namespace {
+
+// DERFromEC_POINT returns the ASN.1, DER, SubjectPublicKeyInfo for a given
+// elliptic-curve point.
+static std::vector<uint8_t> DERFromEC_POINT(const EC_POINT* point) {
+  bssl::UniquePtr<EC_KEY> ec_key(
+      EC_KEY_new_by_curve_name(NID_X9_62_prime256v1));
+  CHECK(EC_KEY_set_public_key(ec_key.get(), point));
+  bssl::UniquePtr<EVP_PKEY> pkey(EVP_PKEY_new());
+  CHECK(EVP_PKEY_assign_EC_KEY(pkey.get(), ec_key.release()));
+
+  bssl::ScopedCBB cbb;
+  uint8_t* der_bytes = nullptr;
+  size_t der_bytes_len = 0;
+  CHECK(CBB_init(cbb.get(), /* initial size */ 128) &&
+        EVP_marshal_public_key(cbb.get(), pkey.get()) &&
+        CBB_finish(cbb.get(), &der_bytes, &der_bytes_len));
+
+  std::vector<uint8_t> ret(der_bytes, der_bytes + der_bytes_len);
+  OPENSSL_free(der_bytes);
+  return ret;
+}
+
+}  // namespace
 
 // static
 std::unique_ptr<PublicKey> P256PublicKey::ExtractFromU2fRegistrationResponse(
@@ -92,7 +120,8 @@ std::unique_ptr<PublicKey> P256PublicKey::ExtractFromCOSEKey(
     return nullptr;
   }
 
-  return std::make_unique<PublicKey>(algorithm, cbor_bytes);
+  return std::make_unique<PublicKey>(algorithm, cbor_bytes,
+                                     DERFromEC_POINT(point.get()));
 }
 
 // static
@@ -129,7 +158,8 @@ std::unique_ptr<PublicKey> P256PublicKey::ParseX962Uncompressed(
   const std::vector<uint8_t> cbor_bytes(
       std::move(cbor::Writer::Write(cbor::Value(std::move(map))).value()));
 
-  return std::make_unique<PublicKey>(algorithm, cbor_bytes);
+  return std::make_unique<PublicKey>(algorithm, cbor_bytes,
+                                     DERFromEC_POINT(point.get()));
 }
 
 }  // namespace device
