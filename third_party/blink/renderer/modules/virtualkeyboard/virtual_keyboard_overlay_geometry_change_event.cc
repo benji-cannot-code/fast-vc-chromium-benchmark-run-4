ec89a6db82cc68d27c24c2c98af858d03b78ba38
Hitoshi Yoshida <peria@chromium.org>
2020-05-13 05:30:36 +0000

    IDL: Update list of IDL and generated files
    - Drops a build target which builds already removed files
    - Update IDL files and generated files from them
    - Fix include path in a newly generated file
    
    Bug: 839389
    Change-Id: I8b62e3c78eada63e0b45e173f6cacdeb69c6f26c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196887
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768150}

--- third_party/blink/renderer/modules/virtualkeyboard/virtual_keyboard_overlay_geometry_change_event.cc
+++ third_party/blink/renderer/modules/virtualkeyboard/virtual_keyboard_overlay_geometry_change_event.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/virtualkeyboard/virtual_keyboard_overlay_geometry_change_event.h"
 
+#include "third_party/blink/renderer/bindings/modules/v8/v8_virtual_keyboard_overlay_geometry_change_event_init.h"
 #include "third_party/blink/renderer/core/geometry/dom_rect.h"
-#include "third_party/blink/renderer/modules/virtualkeyboard/virtual_keyboard_overlay_geometry_change_event_init.h"
 
 namespace blink {
 
