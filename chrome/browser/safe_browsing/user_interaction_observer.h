50a9bfadc2be4a71e1a190bd23a818b9d7dd70de
Mustafa Emre Acer <meacer@chromium.org>
2020-05-13 04:29:38 +0000

    [SB Delayed Warnings] Handle permission prompts
    This CL observers the creation of permission request prompts. When it
    detects one, it automatically denies the request and immediately shows
    the delayed warning. A new histogram bin is also added to record this
    event (kPermissionRequestCancelled).
    
    Bug: 1057157, 1081312
    Change-Id: If5eca1d827705e53160f6546c4c3a4127c7dac0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2194910
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768140}

--- chrome/browser/safe_browsing/user_interaction_observer.h
+++ chrome/browser/safe_browsing/user_interaction_observer.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SAFE_BROWSING_USER_INTERACTION_OBSERVER_H_
 
 #include "chrome/browser/safe_browsing/ui_manager.h"
+#include "components/permissions/permission_request_manager.h"
 #include "components/security_interstitials/core/unsafe_resource.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -40,7 +41,10 @@ enum class DelayedWarningEvent {
   // The page tried to initiate a download and we cancelled it. This doesn't
   // show an interstitial.
   kDownloadCancelled = 6,
-  kMaxValue = kDownloadCancelled,
+  // The page triggered a permission request. It was denied and the warning was
+  // shown.
+  kWarningShownOnPermissionRequest = 7,
+  kMaxValue = kWarningShownOnPermissionRequest,
 };
 
 // Name of the histogram.
@@ -52,7 +56,8 @@ extern const char kDelayedWarningsHistogram[];
 // shown, or the tab is closed or navigated away.
 class SafeBrowsingUserInteractionObserver
     : public base::SupportsUserData::Data,
-      public content::WebContentsObserver {
+      public content::WebContentsObserver,
+      public permissions::PermissionRequestManager::Observer {
  public:
   // Creates an observer for given |web_contents|. |resource| is the unsafe
   // resource for which a delayed interstitial will be displayed.
@@ -83,6 +88,9 @@ class SafeBrowsingUserInteractionObserver
   void DidFinishNavigation(content::NavigationHandle* handle) override;
   void DidToggleFullscreenModeForTab(bool entered_fullscreen,
                                      bool will_cause_resize) override;
+
+  // permissions::PermissionRequestManager::Observer methods:
+  void OnBubbleAdded() override;
 
  private:
   bool HandleKeyPress(const content::NativeWebKeyboardEvent& event);
