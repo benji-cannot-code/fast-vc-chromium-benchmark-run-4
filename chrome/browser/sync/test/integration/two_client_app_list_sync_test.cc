5ea92fa85d52de8778335f5563e85c70bebe605a
Ghazale Hosseinabadi <ghazale@chromium.org>
2020-05-13 18:13:44 +0000

    remove deprecated extension notification from a sync_integration_test
    Since two_client_app_list_sync_test still uses the deprecated
    extension notification (NOTIFICATION_EXTENSIONS_READY_DEPRECATED),
    we remove the deprecated notification in this CL.
    
    Bug: 411566
    Change-Id: I65b4252d6e23872be9dc0050f3e969fbcd1a0844
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191125
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Ghazale Hosseinabadi <ghazale@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768360}

--- chrome/browser/sync/test/integration/two_client_app_list_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_app_list_sync_test.cc
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include "base/macros.h"
+#include "base/one_shot_event.h"
+#include "base/run_loop.h"
 #include "build/build_config.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/test/integration/apps_helper.h"
@@ -24,9 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/pref_names.h"
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/driver/sync_user_settings.h"
-#include "content/public/browser/notification_service.h"
 #include "content/public/test/browser_test.h"
-#include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_system.h"
 
@@ -106,10 +105,10 @@ class TwoClientAppListSyncTest : public SyncTest {
         extensions::ExtensionSystem::Get(profile)->extension_service();
     if (extension_service && extension_service->is_ready())
       return;
-    content::WindowedNotificationObserver extensions_loaded_observer(
-        extensions::NOTIFICATION_EXTENSIONS_READY_DEPRECATED,
-        content::NotificationService::AllSources());
-    extensions_loaded_observer.Wait();
+    base::RunLoop run_loop;
+    extensions::ExtensionSystem::Get(profile)->ready().Post(
+        FROM_HERE, run_loop.QuitClosure());
+    run_loop.Run();
   }
 
   DISALLOW_COPY_AND_ASSIGN(TwoClientAppListSyncTest);
