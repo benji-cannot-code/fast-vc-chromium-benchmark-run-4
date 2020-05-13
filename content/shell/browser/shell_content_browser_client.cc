473a7515436ab60106cc2f9fd5c798189fd8e356
Chris Hamilton <chrisha@chromium.org>
2020-05-13 13:54:31 +0000

    [PM] Create TabLoadingFrameNavigationScheduler.
    This is the mechanism half of the TabLoadingFrameNavigation feature.
    
    Design doc:
    
    https://docs.google.com/document/d/141gYr7s2m0rPgzDxzAN0PDMtwT2diHEaa7-sZ0V_Q_M/edit#
    
    BUG=1064763
    
    Change-Id: Ib22b4516ac95740d9519ca061b67fd4b9e7ebbc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2191128
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Commit-Queue: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768260}

--- content/shell/browser/shell_content_browser_client.cc
+++ content/shell/browser/shell_content_browser_client.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/client_certificate_delegate.h"
 #include "content/public/browser/cors_exempt_headers.h"
 #include "content/public/browser/login_delegate.h"
+#include "content/public/browser/navigation_throttle.h"
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/render_process_host.h"
@@ -313,6 +314,15 @@ void ShellContentBrowserClient::OpenURL(
       Shell::CreateNewWindow(site_instance->GetBrowserContext(), params.url,
                              nullptr, gfx::Size())
           ->web_contents());
+}
+
+std::vector<std::unique_ptr<NavigationThrottle>>
+ShellContentBrowserClient::CreateThrottlesForNavigation(
+    NavigationHandle* navigation_handle) {
+  std::vector<std::unique_ptr<NavigationThrottle>> empty_throttles;
+  if (create_throttles_for_navigation_callback_)
+    return create_throttles_for_navigation_callback_.Run(navigation_handle);
+  return empty_throttles;
 }
 
 std::unique_ptr<LoginDelegate> ShellContentBrowserClient::CreateLoginDelegate(
