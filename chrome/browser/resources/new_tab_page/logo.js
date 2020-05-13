3921cb4bbe5246dee3aaa9b3a9816126b907271e
Tibor Goldschwendt <tiborg@chromium.org>
2020-05-13 20:22:02 +0000

    [webui][ntp] Log NewTabPage.Logo[Click|Shown]* histograms
    Logs
    - NewTabPage.LogoClick
    - NewTabPage.LogoShown
    - NewTabPage.LogoShown.FromCache
    - NewTabPage.LogoShownTime2
    on chrome://new-tab-page.
    
    NewTabPage.LogoShown.Fresh is not logged because the WebUI NTP always
    shows cached doodles.
    
    Fixed: 1072869
    Change-Id: I3afdbebb9596b42053634693bb3d23945260ab72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2197200
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768426}

--- chrome/browser/resources/new_tab_page/logo.js
+++ chrome/browser/resources/new_tab_page/logo.js
@@ -122,7 +122,9 @@ class LogoElement extends PolymerElement {
     super();
     /** @private {!EventTracker} */
     this.eventTracker_ = new EventTracker();
-    BrowserProxy.getInstance().handler.getDoodle().then(({doodle}) => {
+    /** @private {newTabPage.mojom.PageHandlerRemote} */
+    this.pageHandler_ = BrowserProxy.getInstance().handler;
+    this.pageHandler_.getDoodle().then(({doodle}) => {
       this.doodle_ = doodle;
       this.loaded_ = true;
       if (this.doodle_ && this.doodle_.content.interactiveDoodle) {
@@ -191,13 +193,25 @@ class LogoElement extends PolymerElement {
    * @private
    */
   onImageClick_() {
-    if (!this.showAnimation_ && this.doodle_ &&
-        this.doodle_.content.imageDoodle.animationUrl) {
+    if (this.isCtaImageShown_()) {
       this.showAnimation_ = true;
+      this.pageHandler_.onDoodleImageClicked(
+          newTabPage.mojom.DoodleImageType.CTA);
       return;
     }
+    this.pageHandler_.onDoodleImageClicked(
+        this.showAnimation_ ? newTabPage.mojom.DoodleImageType.ANIMATION :
+                              newTabPage.mojom.DoodleImageType.STATIC);
     BrowserProxy.getInstance().open(
         this.doodle_.content.imageDoodle.onClickUrl.url);
+  }
+
+  /** @private */
+  onImageLoad_() {
+    this.pageHandler_.onDoodleImageRendered(
+        this.isCtaImageShown_() ? newTabPage.mojom.DoodleImageType.CTA :
+                                  newTabPage.mojom.DoodleImageType.STATIC,
+        BrowserProxy.getInstance().now());
   }
 
   /**
@@ -208,6 +222,15 @@ class LogoElement extends PolymerElement {
     if ([' ', 'Enter'].includes(e.key)) {
       this.onImageClick_();
     }
+  }
+
+  /**
+   * @returns {boolean}
+   * @private
+   */
+  isCtaImageShown_() {
+    return !this.showAnimation_ && !!this.doodle_ &&
+        !!this.doodle_.content.imageDoodle.animationUrl;
   }
 
   /**
