fbbbc64287b4a828ac108649839f598dc0eb073b
Takumi Fujimoto <takumif@chromium.org>
2020-05-13 01:33:29 +0000

    Cleanup: Remove MediaRouterContextualMenu::menu_model_
    Remove the menu_model_ field that is no longer necessary. Instead,
    instantiate a ui::SimpleMenuModel whenever CreateMenuModel() is called.
    
    Bug: 1078871, b/155892635
    Change-Id: Iacbcf50c8d0a2529385d7724abb7e84f53a67d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2183395
    Reviewed-by: Brandon Tolsch <btolsch@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768091}

--- chrome/browser/ui/toolbar/media_router_contextual_menu_unittest.cc
+++ chrome/browser/ui/toolbar/media_router_contextual_menu_unittest.cc
@@ -128,7 +128,7 @@ TEST_F(MediaRouterContextualMenuUnitTest, Basic) {
   int expected_number_items = 9;
 
   MediaRouterContextualMenu menu(browser(), kShownByUser, &observer_);
-  ui::SimpleMenuModel* model = menu.menu_model();
+  std::unique_ptr<ui::SimpleMenuModel> model = menu.CreateMenuModel();
   // Verify the number of menu items, including separators.
   EXPECT_EQ(model->GetItemCount(), expected_number_items);
 
@@ -158,7 +158,7 @@ TEST_F(MediaRouterContextualMenuUnitTest, Basic) {
 // incognito.
 TEST_F(MediaRouterContextualMenuUnitTest, EnableAndDisableReportIssue) {
   MediaRouterContextualMenu menu(browser(), kShownByPolicy, &observer_);
-  EXPECT_NE(-1, menu.menu_model()->GetIndexOfCommandId(
+  EXPECT_NE(-1, menu.CreateMenuModel()->GetIndexOfCommandId(
                     IDC_MEDIA_ROUTER_REPORT_ISSUE));
 
   std::unique_ptr<BrowserWindow> window(CreateBrowserWindow());
@@ -168,7 +168,7 @@ TEST_F(MediaRouterContextualMenuUnitTest, EnableAndDisableReportIssue) {
 
   MediaRouterContextualMenu incognito_menu(incognito_browser.get(),
                                            kShownByPolicy, &observer_);
-  EXPECT_EQ(-1, incognito_menu.menu_model()->GetIndexOfCommandId(
+  EXPECT_EQ(-1, incognito_menu.CreateMenuModel()->GetIndexOfCommandId(
                     IDC_MEDIA_ROUTER_REPORT_ISSUE));
 }
 
@@ -250,7 +250,7 @@ TEST_F(MediaRouterContextualMenuUnitTest, ActionShownByPolicy) {
   EXPECT_FALSE(menu.IsCommandIdEnabled(IDC_MEDIA_ROUTER_SHOWN_BY_POLICY));
 
   // The checkbox item "Always show icon" should not be shown.
-  EXPECT_FALSE(HasCommandId(menu.menu_model(),
+  EXPECT_FALSE(HasCommandId(menu.CreateMenuModel().get(),
                             IDC_MEDIA_ROUTER_ALWAYS_SHOW_TOOLBAR_ACTION));
 }
 
@@ -258,9 +258,10 @@ TEST_F(MediaRouterContextualMenuUnitTest, NotifyActionController) {
   EXPECT_CALL(observer_, OnContextMenuShown());
   auto menu = std::make_unique<MediaRouterContextualMenu>(
       browser(), kShownByUser, &observer_);
-  menu->OnMenuWillShow(menu->menu_model());
+  std::unique_ptr<ui::SimpleMenuModel> model = menu->CreateMenuModel();
+  menu->OnMenuWillShow(model.get());
 
   EXPECT_CALL(observer_, OnContextMenuHidden());
-  menu->MenuClosed(menu->menu_model());
+  menu->MenuClosed(model.get());
   menu.reset();
 }
