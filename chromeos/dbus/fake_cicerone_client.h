3ab850ca2ae01e9c26af663edb17147378a52a95
John Abd-El-Malek <jam@chromium.org>
2020-05-13 00:28:01 +0000

    Fix use after free in FakeCiceroneClient.
    This was causing failures in CrostiniManagerRestartTest.RestartDelayAndSuccessWhenVmStopping.
    
    Bug: 1081232
    Change-Id: I7984423f5aa77709b5ab9621c6a37af4612cb6fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2197165
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768065}

--- chromeos/dbus/fake_cicerone_client.h
+++ chromeos/dbus/fake_cicerone_client.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_DBUS_FAKE_CICERONE_CLIENT_H_
 #define CHROMEOS_DBUS_FAKE_CICERONE_CLIENT_H_
 
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "chromeos/dbus/cicerone_client.h"
 
@@ -394,6 +395,8 @@ class COMPONENT_EXPORT(CHROMEOS_DBUS) FakeCiceroneClient
   UninstallPackageOwningFileCallback uninstall_package_owning_file_callback_;
 
   base::ObserverList<Observer>::Unchecked observer_list_;
+
+  base::WeakPtrFactory<FakeCiceroneClient> weak_factory_{this};
 
   DISALLOW_COPY_AND_ASSIGN(FakeCiceroneClient);
 };
