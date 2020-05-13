68f464e70b34e087cdb2bfb869f07b9df34ef0a8
Luciano Pacheco <lucmult@chromium.org>
2020-05-13 09:35:34 +0000

    Files app: Suggest App Dialog footer for FilesNG
    Change the sizing, padding and colors for the footer icon and link in
    Suggest App Dialog.
    
    Add the CSS class "button2" to apply in the footer link the desired
    typography (font face, size and weight) from the spec.
    
    Bug: 992827
    Change-Id: I09fd106f7852326708ea562d6dd7dffa2b239261
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198275
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768193}

--- ui/file_manager/file_manager/cws_widget/cws_widget_container.js
+++ ui/file_manager/file_manager/cws_widget/cws_widget_container.js
@@ -112,6 +112,7 @@ class CWSWidgetContainer {
     this.webstoreButton_ = document.createElement('div');
     this.webstoreButton_.hidden = true;
     this.webstoreButton_.setAttribute('role', 'button');
+    this.webstoreButton_.classList.add('button2');
     this.webstoreButton_.tabIndex = 0;
 
     /**
