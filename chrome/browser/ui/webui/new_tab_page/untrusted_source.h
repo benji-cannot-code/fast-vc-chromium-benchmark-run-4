f1648df34c18122ae2af295b6fb7da7df4be4ebd
John Abd-El-Malek <jam@chromium.org>
2020-05-13 04:16:27 +0000

    Reland "Move WebUI code to run on the UI thread only."
    This is a reland of 2dd9b658bb333128b1c24dd4f1a0559d21570535. There's no change, the fix was in different code in r768065.
    
    TBR=nasko@chromium.org
    
    Original change's description:
    > Move WebUI code to run on the UI thread only.
    >
    > The optimization to run it on the IO thread was for the non-network service path. We can now simplify this.
    >
    > InstantIOContext can be removed, I'll leave that for a follow-up as this is already big.
    >
    > Bug: 908955,1062873
    > Change-Id: Ia209d22575f49159227e49accf249dccb4fbaffe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190744
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#767182}
    
    Bug: 908955, 1062873
    Change-Id: Iae51e1c9b17a9c9d68bdfebd3a07387f34070339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2197500
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768133}

--- chrome/browser/ui/webui/new_tab_page/untrusted_source.h
+++ chrome/browser/ui/webui/new_tab_page/untrusted_source.h
@@ -55,7 +55,7 @@ class UntrustedSource : public content::URLDataSource,
   std::string GetContentSecurityPolicyFrameAncestors() override;
   bool ShouldReplaceExistingSource() override;
   bool ShouldServiceRequest(const GURL& url,
-                            content::ResourceContext* resource_context,
+                            content::BrowserContext* browser_context,
                             int render_process_id) override;
 
  private:
--- content/public/browser/url_data_source.cc
+++ content/public/browser/url_data_source.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ptr_util.h"
 #include "base/task_runner_util.h"
-#include "content/browser/resource_context_impl.h"
 #include "content/browser/webui/url_data_manager.h"
 #include "content/browser/webui/url_data_manager_backend.h"
 #include "content/browser/webui/url_data_source_impl.h"
@@ -21,20 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-namespace {
-
-URLDataSource* GetSourceForURLHelper(ResourceContext* resource_context,
-                                     const GURL& url) {
-  DCHECK_CURRENTLY_ON(BrowserThread::IO);
-
-  URLDataSourceImpl* source =
-      GetURLDataManagerForResourceContext(resource_context)
-          ->GetDataSourceFromURL(url);
-  return source->source();
-}
-
-}  // namespace
-
 // static
 void URLDataSource::Add(BrowserContext* browser_context,
                         std::unique_ptr<URLDataSource> source) {
@@ -42,15 +27,11 @@ void URLDataSource::Add(BrowserContext* browser_context,
 }
 
 // static
-void URLDataSource::GetSourceForURL(
-    BrowserContext* browser_context,
-    const GURL& url,
-    base::OnceCallback<void(URLDataSource*)> callback) {
-  base::PostTaskAndReplyWithResult(
-      GetIOThreadTaskRunner({}).get(), FROM_HERE,
-      base::BindOnce(&GetSourceForURLHelper,
-                     browser_context->GetResourceContext(), url),
-      std::move(callback));
+URLDataSource* URLDataSource::GetSourceForURL(BrowserContext* browser_context,
+                                              const GURL& url) {
+  return URLDataManagerBackend::GetForBrowserContext(browser_context)
+      ->GetDataSourceFromURL(url)
+      ->source();
 }
 
 // static
@@ -64,11 +45,6 @@ std::string URLDataSource::URLToRequestPath(const GURL& url) {
     return spec.substr(offset);
 
   return std::string();
-}
-
-scoped_refptr<base::SingleThreadTaskRunner>
-URLDataSource::TaskRunnerForRequestPath(const std::string& path) {
-  return GetUIThreadTaskRunner({});
 }
 
 bool URLDataSource::ShouldReplaceExistingSource() {
@@ -118,7 +94,7 @@ bool URLDataSource::ShouldDenyXFrameOptions() {
 }
 
 bool URLDataSource::ShouldServiceRequest(const GURL& url,
-                                         ResourceContext* resource_context,
+                                         BrowserContext* browser_context,
                                          int render_process_id) {
   return url.SchemeIs(kChromeDevToolsScheme) || url.SchemeIs(kChromeUIScheme) ||
          url.SchemeIs(kChromeUIUntrustedScheme);
