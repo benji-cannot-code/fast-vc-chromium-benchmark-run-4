292aaa8e2805b16ac4ec3bd0b6d734851104be37
Mikhail Khokhlov <khokhlov@google.com>
2020-05-13 15:24:39 +0000

    [tools/perf] Output an empty JSON object from upload_trace_processor
    This is to fix errors when running the script on the bot.
    
    Bug: 1050517
    Change-Id: I14ec26f53334a8c47eb4ef369c62c01bd87157b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199058
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/master@{#768284}

--- tools/perf/core/perfetto_binary_roller/upload_trace_processor.py
+++ tools/perf/core/perfetto_binary_roller/upload_trace_processor.py
@@ -39,7 +39,7 @@ def main(args):
   parser.add_argument(
       '--revision',
       help=('Perfetto revision. '
-            'If not supplied, will try to infer from //third_party/perfetto.'))
+            'If not supplied, will try to infer from DEPS file.'))
 
   # When this script is invoked on a CI bot, there are some extra arguments
   # that we have to ignore.
@@ -49,6 +49,9 @@ def main(args):
 
   binary_deps_manager.UploadHostBinary(trace_processor.TP_BINARY_NAME,
                                        args.path, revision)
+
+  # CI bot expects a valid JSON object as script output.
+  sys.stdout.write('{}\n')
 
 
 if __name__ == '__main__':
