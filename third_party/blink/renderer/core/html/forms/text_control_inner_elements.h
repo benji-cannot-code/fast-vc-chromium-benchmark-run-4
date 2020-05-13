c5275137f3d886692fcb22f10c05316db40a9770
Rune Lillesveen <futhark@chromium.org>
2020-05-13 07:22:56 +0000

    Invalidate style for inner editor when focus changes.
    The inner editor style invalidation relied on marking the element style
    dirty during style recalc if the host input style changed, but that
    might not be the case if the author overrides any focus styles.
    
    Bug: 1078663
    Change-Id: I2914ff9bebc4bc239ca29ced7365b2c9155ebbb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196583
    Reviewed-by: Ionel Popescu <iopopesc@microsoft.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768172}

--- third_party/blink/renderer/core/html/forms/text_control_inner_elements.h
+++ third_party/blink/renderer/core/html/forms/text_control_inner_elements.h
@@ -52,6 +52,7 @@ class TextControlInnerEditorElement final : public HTMLDivElement {
   void DefaultEventHandler(Event&) override;
 
   void SetVisibility(bool is_visible);
+  void FocusChanged();
   scoped_refptr<ComputedStyle> CreateInnerEditorStyle() const;
 
  private:
