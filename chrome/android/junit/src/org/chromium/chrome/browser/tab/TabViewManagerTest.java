eb45f0d859196d9fb0fc89b174891aeffa190d2d
Mehran Mahmoudi <mahmoudi@chromium.org>
2020-05-13 22:39:25 +0000

    [TabViewManager] Add controls height as margins to the current view
    This addresses a problem where views shown by TabViewManager don't
    respect the toolbar height and and partially clipped by it.
    
    Bug: 1073948,1081628
    Change-Id: I6d499a67c1378e8c18c1f4bc7849d9622cabbf41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2188212
    Commit-Queue: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768471}

--- chrome/android/junit/src/org/chromium/chrome/browser/tab/TabViewManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tab/TabViewManagerTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tab;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -17,6 +18,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.annotation.Config;
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 
@@ -24,7 +28,23 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
  * Unit tests for the {@link TabViewManager} class.
  */
 @RunWith(BaseRobolectricTestRunner.class)
+@Config(shadows = {TabViewManagerTest.TabBrowserControlsOffsetHelperShadow.class})
 public class TabViewManagerTest {
+    /**
+     * A shadow implementation of {@link TabBrowserControlsOffsetHelper}.
+     */
+    @Implements(TabBrowserControlsOffsetHelper.class)
+    public static class TabBrowserControlsOffsetHelperShadow {
+        @Implementation
+        public static TabBrowserControlsOffsetHelper get(Tab tab) {
+            TabBrowserControlsOffsetHelper mockTabBrowserControlsOffsetHelper =
+                    mock(TabBrowserControlsOffsetHelper.class);
+            when(mockTabBrowserControlsOffsetHelper.contentOffset()).thenReturn(0);
+            when(mockTabBrowserControlsOffsetHelper.bottomControlsOffset()).thenReturn(0);
+            return mockTabBrowserControlsOffsetHelper;
+        }
+    }
+
     @Mock
     private TabImpl mTab;
     @Mock
