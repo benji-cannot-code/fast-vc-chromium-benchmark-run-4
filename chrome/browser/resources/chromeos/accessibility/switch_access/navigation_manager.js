8e828a0fdba2529bcdda46c6c001024646162d1e
Anastasia Helfinstein <anastasi@google.com>
2020-05-13 22:34:53 +0000

    [Switch Access] Clean up NavigationManager tests
    Cleans up the Navigation Manager tests a bit in preparation for an
    upcoming refactor of the code.
    
    AX-Relnotes: n/a.
    Bug: None
    Change-Id: Ia564c9ed7444232426cb1c5d46ce10ac06809ad8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2197861
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#768463}

--- chrome/browser/resources/chromeos/accessibility/switch_access/navigation_manager.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/navigation_manager.js
@@ -374,7 +374,8 @@ class NavigationManager {
   }
 
   /**
-   * Set |this.group_| to |group|.
+   * Set |this.group_| to |group|, and optionally sets |this.node_| to the
+   * group's first child.
    * @param {!SARootNode} group
    * @param {boolean} shouldSetNode
    * @private
