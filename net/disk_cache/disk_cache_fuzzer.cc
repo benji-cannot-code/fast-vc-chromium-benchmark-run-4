2bba0d5b65f026d6d133a23d3e9292572db1a0d6
danakj <danakj@chromium.org>
2020-05-13 19:51:52 +0000

    Initialize TestTimeouts in the disk_cache_fuzzer.
    It should be initialized in all tests that use TaskEnvironment to run
    posted tasks.
    
    R=jkarlin@chromium.org
    
    Bug: 1076737
    Change-Id: I949316fc6ce8f8abd78ae458ebd7fab203dd7a3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198391
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768408}

--- net/disk_cache/disk_cache_fuzzer.cc
+++ net/disk_cache/disk_cache_fuzzer.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/numerics/checked_math.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/task_environment.h"
+#include "base/test/test_timeouts.h"
 #include "base/time/time.h"
 #include "net/base/cache_type.h"
 #include "net/base/interval.h"
@@ -91,6 +92,10 @@ struct InitGlobals {
     base::CommandLine::Init(0, nullptr);
 
     print_comms_ = ::getenv("LPM_DUMP_NATIVE_INPUT");
+
+    // TaskEnvironment requires TestTimeouts initialization to watch for
+    // problematic long-running tasks.
+    TestTimeouts::Initialize();
 
     // Mark this thread as an IO_THREAD with MOCK_TIME, and ensure that Now()
     // is driven from the same mock clock.
