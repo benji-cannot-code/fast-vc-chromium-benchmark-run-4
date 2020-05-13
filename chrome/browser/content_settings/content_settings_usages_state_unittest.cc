d26bdea398958a9c3fdb1c079a8408e2bba8cd99
Illia Klimov <elklm@google.com>
2020-05-13 12:01:22 +0000

    ContentSettingsUsagesState migration.
    This patch migrates ContentSettingsUsagesState from
    components/content_settings/core/browser/ to
    components/content_settings/browser/.
    
    The reason for migration is to avoid cyclic dependencies that occurred
    during fixing a bug.
    
    Bug: 106862
    TBR: raymes@chromium.org
    Change-Id: I971fdc5c8917e213d6e40f46e65f3af7b83067a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199098
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Illia Klimov <elklm@google.com>
    Cr-Commit-Position: refs/heads/master@{#768222}

--- chrome/browser/content_settings/content_settings_usages_state_unittest.cc
+++ chrome/browser/content_settings/content_settings_usages_state_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/content_settings/core/browser/content_settings_usages_state.h"
+#include "components/content_settings/browser/content_settings_usages_state.h"
 
 #include <string>
 
