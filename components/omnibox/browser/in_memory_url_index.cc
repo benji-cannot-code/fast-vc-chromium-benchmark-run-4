a6ac1c293941963e9e603e2f8dbbe3f99286314a
Bruce Dawson <brucedawson@chromium.org>
2020-05-13 20:38:18 +0000

    Better leaking of URLIndexPrivateData
    When working on crrev.com/c/2188517 (already landed) I misunderstood a
    suggestion about using base::NoDestructor. This change uses that type
    to leak URLIndexPrivateData more cleanly.
    
    Bug: 1025646, 1032679
    Change-Id: Ic60066b3dbfc93fd86ba0a875f9cd3e936bc7c4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198928
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Auto-Submit: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768436}

--- components/omnibox/browser/in_memory_url_index.cc
+++ components/omnibox/browser/in_memory_url_index.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/files/file_util.h"
+#include "base/no_destructor.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/post_task.h"
@@ -300,13 +301,13 @@ void InMemoryURLIndex::Shutdown() {
                          &URLIndexPrivateData::WritePrivateDataToCacheFileTask),
                      private_data_, path));
 #ifndef LEAK_SANITIZER
-  // Intentionally allocate and then leak a scoped_refptr to private_data_. This
+  // Intentionally create and then leak a scoped_refptr to private_data_. This
   // permanently raises the reference count so that the URLIndexPrivateData
   // destructor won't run during browser shutdown. This saves having to walk the
   // maps to free their memory, which saves time and avoids shutdown hangs,
   // especially if some of the memory has been paged out.
-  auto* leak_pointer = new scoped_refptr<URLIndexPrivateData>(private_data_);
-  ANALYZER_ALLOW_UNUSED(leak_pointer);
+  base::NoDestructor<scoped_refptr<URLIndexPrivateData>> leak_reference(
+      private_data_);
 #endif
   needs_to_be_cached_ = false;
 }
