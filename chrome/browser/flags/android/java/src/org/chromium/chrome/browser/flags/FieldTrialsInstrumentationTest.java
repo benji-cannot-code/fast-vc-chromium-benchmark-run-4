f3ab159708df3cea4dc51baf34a0e42d6d52c1e0
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-05-13 22:37:14 +0000

    Move FieldTrialsTest to chrome/browser/flags module
    Bug: 1041468
    Change-Id: I5244a6eed73b9cbeb6336ef43d8550d4961dcc01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172619
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768468}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/FieldTrialsInstrumentationTest.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/FieldTrialsInstrumentationTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser;
+package org.chromium.chrome.browser.flags;
 
 import android.support.test.filters.SmallTest;
 
@@ -15,13 +15,8 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.chrome.browser.flags.BooleanCachedFieldTrialParameter;
-import org.chromium.chrome.browser.flags.CachedFeatureFlags;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
-import org.chromium.chrome.browser.flags.ChromeSwitches;
-import org.chromium.chrome.browser.flags.DoubleCachedFieldTrialParameter;
-import org.chromium.chrome.browser.flags.IntCachedFieldTrialParameter;
-import org.chromium.chrome.browser.flags.StringCachedFieldTrialParameter;
+import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.browser.Features;
@@ -32,7 +27,7 @@ import org.chromium.chrome.test.util.browser.FieldTrials;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add(ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE)
-public final class FieldTrialsTest {
+public final class FieldTrialsInstrumentationTest {
     private static final String sFeature1 = ChromeFeatureList.TEST_DEFAULT_DISABLED;
     private static final String sFeature2 = ChromeFeatureList.TEST_DEFAULT_ENABLED;
 
