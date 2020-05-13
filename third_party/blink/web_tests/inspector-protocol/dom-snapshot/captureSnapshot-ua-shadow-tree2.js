0c84f754edc3f9ee2ccd0be0b2d56d33b0b46927
Andrey Kosyakov <caseq@chromium.org>
2020-05-13 00:39:33 +0000

    DevTools/DOMSnapshot: use flat iterator over DOM tree instead of recursion
    ... this makes it easier to profile the code, as children of VisitNode()
    are meaningfully aggregated.
    
    Change-Id: I37532964d8cefb6723e732c04a28f97190e337a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195562
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768066}

---
+++ third_party/blink/web_tests/inspector-protocol/dom-snapshot/captureSnapshot-ua-shadow-tree2.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startHTML(`
+  <html>
+  <style>
+   body { font: 10px Ahem; }
+  </style>
+  <body>
+  <img id="one" src="http://example.com/A.png"/>
+  <img id="two" src="http://example.com/B.png"/>
+  <img id="three" src="http://example.com/C.png"/>
+  </body>
+  </html>`, 'Tests that DOMSnapshot skips UA shadow root when traversing the DOM tree');
+
+  var response = await dp.DOMSnapshot.captureSnapshot({computedStyles: []});
+  if (response.error) {
+    testRunner.log(response);
+  } else {
+    response.result.strings[response.result.documents[0].documentURL] = '';
+    response.result.strings[response.result.documents[0].frameId] = '';
+    testRunner.log(
+        response.result, undefined,
+        ['documentURL', 'frameId', 'backendNodeId']);
+  }
+  testRunner.completeTest();
+})
