923c1f8e69999dcca81b6703f5fa19a27e4f9252
edchin <edchin@chromium.org>
2020-05-14 19:39:49 +0000

    [ios] Reduce pointer boilerplate in Popup Menu
    crrev.com/c/2199891 introduces a reusable class for pointer
    interactions for non-button views. This class internally handles
    all the delegate callbacks. So usage is simplified to a single line.
    
    This CL should have no impact/change on the UI.
    
    Bug: 1073961
    Change-Id: Ida7b1ae9e356d09a9faa666c0aaf4e39ef331b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2200136
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768921}

--- ios/chrome/browser/ui/popup_menu/public/popup_menu_table_view_controller.mm
+++ ios/chrome/browser/ui/popup_menu/public/popup_menu_table_view_controller.mm
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/table_view/chrome_table_view_styler.h"
 #include "ios/chrome/browser/ui/ui_feature_flags.h"
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/ui/util/pointer_interaction_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -26,11 +27,6 @@ const CGFloat kFooterHeight = 21;
 const CGFloat kPopupMenuVerticalInsets = 7;
 const CGFloat kScrollIndicatorVerticalInsets = 11;
 }  // namespace
-
-#if defined(__IPHONE_13_4)
-@interface PopupMenuTableViewController (Pointer) <UIPointerInteractionDelegate>
-@end
-#endif  // defined(__IPHONE_13_4)
 
 @interface PopupMenuTableViewController ()
 // Whether the -viewDidAppear: callback has been called.
@@ -228,8 +224,7 @@ const CGFloat kScrollIndicatorVerticalInsets = 11;
   if (@available(iOS 13.4, *)) {
     if (base::FeatureList::IsEnabled(kPointerSupport)) {
       if (![self.cellsInMemory containsObject:cell]) {
-        [cell addInteraction:[[UIPointerInteraction alloc]
-                                 initWithDelegate:self]];
+        [cell addInteraction:[[ViewPointerInteraction alloc] init]];
         [self.cellsInMemory addObject:cell];
       }
     }
@@ -237,29 +232,6 @@ const CGFloat kScrollIndicatorVerticalInsets = 11;
 #endif  // defined(__IPHONE_13_4)
   return cell;
 }
-
-#if defined(__IPHONE_13_4)
-
-#pragma mark UIPointerInteractionDelegate
-
-- (UIPointerRegion*)pointerInteraction:(UIPointerInteraction*)interaction
-                      regionForRequest:(UIPointerRegionRequest*)request
-                         defaultRegion:(UIPointerRegion*)defaultRegion
-    API_AVAILABLE(ios(13.4)) {
-  return defaultRegion;
-}
-
-- (UIPointerStyle*)pointerInteraction:(UIPointerInteraction*)interaction
-                       styleForRegion:(UIPointerRegion*)region
-    API_AVAILABLE(ios(13.4)) {
-  UIPointerHoverEffect* effect = [UIPointerHoverEffect
-      effectWithPreview:[[UITargetedPreview alloc]
-                            initWithView:interaction.view]];
-  effect.prefersScaledContent = NO;
-  effect.prefersShadow = NO;
-  return [UIPointerStyle styleWithEffect:effect shape:nil];
-}
-#endif  // defined(__IPHONE_13_4)
 
 #pragma mark - Private
 
