c543ea1d581a011cb8fbe6214ea76a991ba593ea
Roman Sorokin <rsorokin@chromium.org>
2020-05-14 09:35:20 +0000

    cros enrollment: Do not switch to Mojo when autolaunch scheduled
    Caused issues with CFM enrollment - Mojo host does not support all
    kiosk apps yet.
    
    Bug: 1077009
    Change-Id: I00639777f9fa45d85bf459148ff4a1e1ffc50d6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199063
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#768707}

--- chrome/browser/chromeos/login/enrollment/enrollment_screen.cc
+++ chrome/browser/chromeos/login/enrollment/enrollment_screen.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/chromeos/login/configuration_keys.h"
 #include "chrome/browser/chromeos/login/enrollment/enrollment_uma.h"
-#include "chrome/browser/chromeos/login/login_wizard.h"
 #include "chrome/browser/chromeos/login/screen_manager.h"
 #include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host.h"
@@ -325,19 +324,8 @@ void EnrollmentScreen::OnConfirmationClosed() {
   // either case, passing exit_callback_ directly should be safe.
   ClearAuth(base::BindRepeating(exit_callback_, Result::COMPLETED));
 
-  if (ShouldAttemptRestart()) {
+  if (ShouldAttemptRestart())
     chrome::AttemptRestart();
-    return;
-  }
-
-  // Could be not managed in tests.
-  if (g_browser_process->platform_part()
-          ->browser_policy_connector_chromeos()
-          ->IsEnterpriseManaged()) {
-    DCHECK_EQ(LoginDisplayHost::default_host()->GetOobeUI()->display_type(),
-              OobeUI::kOobeDisplay);
-    SwitchWebUItoMojo();
-  }
 }
 
 void EnrollmentScreen::OnAuthError(const GoogleServiceAuthError& error) {
