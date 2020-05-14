dd002cce849dddf538c217136e5200c314a5919a
Andrey Kosyakov <caseq@chromium.org>
2020-05-14 07:08:59 +0000

    Reland "DevTools/DOMSnapshot: use flat iterator over DOM tree instead of recursion"
    This reverts commit 32e8ec94d2d7c8d4ecd3502a65ce2fcefd5ebffc.
    
    Reason for revert: fixed the regression.
    
    Original change's description:
    > Revert "DevTools/DOMSnapshot: use flat iterator over DOM tree instead of recursion"
    > 
    > This reverts commit d349e910bc8a68c07e4fcf037f6fef1a87fef756.
    > 
    > Reason for revert: broke clients, apparently we skip some nodes that were previously returned.
    > 
    > Original change's description:
    > > DevTools/DOMSnapshot: use flat iterator over DOM tree instead of recursion
    > > 
    > > ... this makes it easier to profile the code, as children of VisitNode()
    > > are meaningfully aggregated.
    > > 
    > > Change-Id: I37532964d8cefb6723e732c04a28f97190e337a7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195562
    > > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > > Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#768066}
    > 
    > TBR=caseq@chromium.org,pfeldman@chromium.org,kvitekp@chromium.org
    > 
    > Change-Id: Ieca37c61af707468a19a21fbdf3da78965f3af7e
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199478
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#768327}
    
    TBR=caseq@chromium.org,pfeldman@chromium.org,kvitekp@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Change-Id: I81e72c72a0ed69c03b6828145f6e29c0fa6de479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2200459
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768677}

--- third_party/blink/renderer/core/inspector/legacy_dom_snapshot_agent.cc
+++ third_party/blink/renderer/core/inspector/legacy_dom_snapshot_agent.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/html_link_element.h"
 #include "third_party/blink/renderer/core/html/html_template_element.h"
 #include "third_party/blink/renderer/core/input_type_names.h"
+#include "third_party/blink/renderer/core/inspector/dom_traversal_utils.h"
 #include "third_party/blink/renderer/core/inspector/identifiers_factory.h"
 #include "third_party/blink/renderer/core/inspector/inspector_dom_agent.h"
 #include "third_party/blink/renderer/core/inspector/inspector_dom_debugger_agent.h"
@@ -304,16 +305,16 @@ LegacyDOMSnapshotAgent::VisitContainerChildren(
     bool include_user_agent_shadow_tree) {
   auto children = std::make_unique<protocol::Array<int>>();
 
-  if (!InspectorDOMSnapshotAgent::HasChildren(*container,
-                                              include_user_agent_shadow_tree))
+  if (!blink::dom_traversal_utils::HasChildren(*container,
+                                               include_user_agent_shadow_tree))
     return nullptr;
 
-  Node* child = InspectorDOMSnapshotAgent::FirstChild(
+  Node* child = blink::dom_traversal_utils::FirstChild(
       *container, include_user_agent_shadow_tree);
   while (child) {
     children->emplace_back(VisitNode(child, include_event_listeners,
                                      include_user_agent_shadow_tree));
-    child = InspectorDOMSnapshotAgent::NextSibling(
+    child = blink::dom_traversal_utils::NextSibling(
         *child, include_user_agent_shadow_tree);
   }
 
