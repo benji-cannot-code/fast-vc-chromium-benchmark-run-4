b270ee4cf34da5c941d5730141d2a8455c35a596
Hitoshi Yoshida <peria@chromium.org>
2020-05-14 03:08:28 +0000

    bindings: Update include files of IDL dicitonaries
    Running IDL compiler generates code for IDL dictionaries in
    {core,modules}/<module>/foo.{cc,h} and
    bindings/{core,modules}/v8/v8_foo.{cc,h}
    But the new IDL compiler will generate only
    bindings/{core,modules}/v8/v8_foo.{cc,h}
    
    This CL updates included file paths to work for the migration.
    
    
    Bug: 839389
    Change-Id: I4c862fe476e7aa2f000d3dbbfff848eaed2126f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198333
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768615}

--- third_party/blink/renderer/modules/clipboard/clipboard.cc
+++ third_party/blink/renderer/modules/clipboard/clipboard.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_clipboard_item_options.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
-#include "third_party/blink/renderer/modules/clipboard/clipboard_item_options.h"
 #include "third_party/blink/renderer/modules/clipboard/clipboard_promise.h"
 
 namespace blink {
