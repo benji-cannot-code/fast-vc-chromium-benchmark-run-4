b15683958d87574f15c45d66e8c56cb0bac1a356
edchin <edchin@chromium.org>
2020-05-14 19:45:28 +0000

    [ios] Reduce pointer boilerplate in Omnibox Popup
    crrev.com/c/2199891 introduces a reusable class for pointer
    interactions for non-button views. This class internally handles
    all the delegate callbacks. So usage is simplified to a single line.
    
    This CL should have no impact/change on the UI.
    
    Bug: 1073961
    Change-Id: I8d1f00801685267ca065b7e4565dbc08f77c07c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199728
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: edchin <edchin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768926}

--- ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row_cell.mm
+++ ios/chrome/browser/ui/omnibox/popup/omnibox_popup_row_cell.mm
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/dynamic_color_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/util/pointer_interaction_util.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ios/chrome/grit/ios_theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -36,12 +37,6 @@ const CGFloat kTrailingButtonTrailingMargin = 14;
 NSString* const kOmniboxPopupRowSwitchTabAccessibilityIdentifier =
     @"OmniboxPopupRowSwitchTabAccessibilityIdentifier";
 }  // namespace
-
-#if defined(__IPHONE_13_4)
-@interface OmniboxPopupRowCell (PointerInteraction) <
-    UIPointerInteractionDelegate>
-@end
-#endif  // defined(__IPHONE_13_4)
 
 @interface OmniboxPopupRowCell ()
 
@@ -130,8 +125,7 @@ NSString* const kOmniboxPopupRowSwitchTabAccessibilityIdentifier =
 #if defined(__IPHONE_13_4)
     if (@available(iOS 13.4, *)) {
       if (base::FeatureList::IsEnabled(kPointerSupport)) {
-        [self addInteraction:[[UIPointerInteraction alloc]
-                                 initWithDelegate:self]];
+        [self addInteraction:[[ViewPointerInteraction alloc] init]];
       }
     }
 #endif  // defined(__IPHONE_13_4)
@@ -509,27 +503,5 @@ NSString* const kOmniboxPopupRowSwitchTabAccessibilityIdentifier =
 - (void)trailingButtonTapped {
   [self.delegate trailingButtonTappedForCell:self];
 }
-
-#if defined(__IPHONE_13_4)
-
-#pragma mark UIPointerInteractionDelegate
-
-- (UIPointerRegion*)pointerInteraction:(UIPointerInteraction*)interaction
-                      regionForRequest:(UIPointerRegionRequest*)request
-                         defaultRegion:(UIPointerRegion*)defaultRegion
-    API_AVAILABLE(ios(13.4)) {
-  return defaultRegion;
-}
-
-- (UIPointerStyle*)pointerInteraction:(UIPointerInteraction*)interaction
-                       styleForRegion:(UIPointerRegion*)region
-    API_AVAILABLE(ios(13.4)) {
-  UIPointerHoverEffect* effect = [UIPointerHoverEffect
-      effectWithPreview:[[UITargetedPreview alloc] initWithView:self]];
-  effect.prefersScaledContent = NO;
-  effect.prefersShadow = NO;
-  return [UIPointerStyle styleWithEffect:effect shape:nil];
-}
-#endif  // defined(__IPHONE_13_4)
 
 @end
