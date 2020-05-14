cc9ddfe5712878d007195da5cff54d6efd853dd8
Moe Ahmadi <mahmadi@chromium.org>
2020-05-14 03:16:36 +0000

    [WebUI][NTP] Fixes the realbox flicker when matching omnibox's theme
    It does so by keeping the realbox hidden until the theme arrives.
    
    Bug: 1074448
    Change-Id: I6b72b6f6cbcc3ef2a09c6d4a25e3031bb089b65c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2200071
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768618}

--- chrome/browser/resources/new_tab_page/app.js
+++ chrome/browser/resources/new_tab_page/app.js
@@ -162,6 +162,12 @@ class AppElement extends PolymerElement {
         value: () => loadTimeData.getBoolean('realboxEnabled'),
       },
 
+      /** @private */
+      realboxShown_: {
+        type: Boolean,
+        computed: 'computeRealboxShown_(theme_)',
+      },
+
       /**
        * If true, renders additional elements that were not deemed crucial to
        * to show up immediately on load.
@@ -409,6 +415,17 @@ class AppElement extends PolymerElement {
       default:
         return '';
     }
+  }
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  computeRealboxShown_() {
+    // If realbox is to match the Omnibox's theme, keep it hidden until the
+    // theme arrives. Otherwise mismatching colors will cause flicker.
+    return !loadTimeData.getBoolean('realboxMatchOmniboxTheme') ||
+        !!this.theme_;
   }
 
   /** @private */
