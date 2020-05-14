e1769fd6748588a34ebe219179894fbe1bef0a7c
Daniele Castagna <dcastagna@chromium.org>
2020-05-14 22:24:36 +0000

    Revert "viz: Re-enable ignore_undamaged on platforms using GL buffer queue"
    This reverts commit a79f5b3b7907dec4a46f06b15834374ccf32511c.
    
    Reason for revert: Enabled the optimization on the wrong platforms and it causes flickering (crbug.com/1082947)
    
    Original change's description:
    > viz: Re-enable ignore_undamaged on platforms using GL buffer queue
    > 
    > SurfaceAggregator has an option to output only the quads that
    > intersect the damage rect computed by SA itself.
    > 
    > This optimization was disabled on devices with HW overlays or where
    > the renderer might expand the damage, since SA would not know the
    > final damage rect.
    > 
    > This CL re-enables the optimization on devices where we can estimate
    > a bounding rect of the renderer expanded damage.
    > 
    > This is achieved by asking to the renderer for a bounding rect that
    > represents the maximum area that can be expanded.
    > SurfaceAggregator will aggregate the boundingrect union the damage.
    > 
    > The damage on the root render pass will be unchanged, so that HW
    > overlays damage computation won't be affected.
    > 
    > 
    > Bug: 1077210
    > Test: viz_unittest (new test and Display tests)
    > Change-Id: Ie94d1f09923959e2e025845f89a9b3eae759a64a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2179763
    > Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Reviewed-by: kylechar <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#768572}
    
    TBR=dcastagna@chromium.org,sunnyps@chromium.org,kylechar@chromium.org
    
    Change-Id: Id7453435c8b50981e5b7752855e3fd2190076676
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1077210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203219
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769005}

--- components/viz/service/display/overlay_processor_using_strategy.h
+++ components/viz/service/display/overlay_processor_using_strategy.h
@@ -67,7 +67,6 @@ class VIZ_SERVICE_EXPORT OverlayProcessorUsingStrategy
 
   ~OverlayProcessorUsingStrategy() override;
 
-  gfx::Rect GetPreviousFrameOverlaysBoundingRect() const final;
   gfx::Rect GetAndResetOverlayDamage() final;
 
   // Override OverlayProcessor.
