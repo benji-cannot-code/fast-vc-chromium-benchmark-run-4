436f4df089c36ede6f9bc8098bbd936bb65e3fda
Eugene But <eugenebut@chromium.org>
2020-05-14 17:46:17 +0000

    Allow finch-configured heap profiler collection interval
    Default interval on iOS was set to 30 minutes, but it's unclear if
    the value is ideal for iOS. Too frequent sampling will put unnecessary
    load on the server, while to rare sampling will not allow us to
    collect enough data.
    
    Default interval on Android was set to 24 hours, which seems to be
    too rare, so we don't collect enough data.
    
    Configurable sampling interval will allow us to find the best sampling
    rate for iOS and Android.
    
    Bug: 1075702
    TBR: rockot@chromium.org
    Change-Id: I6e820486240e2b3552146d0b066d411b79a12a6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2194593
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768838}

--- ios/chrome/browser/ios_chrome_main_parts.mm
+++ ios/chrome/browser/ios_chrome_main_parts.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "base/path_service.h"
 #include "base/sequenced_task_runner.h"
-#include "base/strings/string_number_conversions.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
 #include "base/time/default_tick_clock.h"
@@ -162,20 +161,7 @@ void IOSChromeMainParts::PreCreateThreads() {
   metrics::CallStackProfileBuilder::SetBrowserProcessReceiverCallback(
       base::BindRepeating(
           &metrics::CallStackProfileMetricsProvider::ReceiveProfile));
-
-  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  std::string collection_interval_switch = "heap-profiler-collection-interval";
-  int collection_interval = 30;
-
-  if (!command_line->HasSwitch(collection_interval_switch) ||
-      !base::StringToInt(
-          command_line->GetSwitchValueASCII(collection_interval_switch),
-          &collection_interval)) {
-    collection_interval = 30;
-  }
-  DCHECK_GT(collection_interval, 0);
-  heap_profiler_controller_->Start(
-      base::TimeDelta::FromMinutes(collection_interval));
+  heap_profiler_controller_->Start();
 #endif
 
   variations::InitCrashKeys();
