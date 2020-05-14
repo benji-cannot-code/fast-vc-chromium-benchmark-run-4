81a9b94d14d4eea6a1c66aff9396283e4c43d5c9
Stepan Khapugin <stkhapugin@chromium.org>
2020-05-14 10:13:19 +0000

    [iOS] Rename getViewControllerToPresent to viewControllerToPresent.
    New name better matches style guide.
    
    Bug: none
    Change-Id: I8fdf0bf918cb0f2042acebf47ef01d224fe1dd7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2193074
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768716}

--- ios/chrome/browser/ui/qr_scanner/qr_scanner_legacy_coordinator.mm
+++ ios/chrome/browser/ui/qr_scanner/qr_scanner_legacy_coordinator.mm
@@ -58,7 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   self.viewController.modalPresentationStyle = UIModalPresentationFullScreen;
 
   [self.baseViewController
-      presentViewController:[self.viewController getViewControllerToPresent]
+      presentViewController:[self.viewController viewControllerToPresent]
                    animated:YES
                  completion:nil];
 }
