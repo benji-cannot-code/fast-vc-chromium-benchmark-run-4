4719401ed9e6072689813b9cc18bf0948a83464e
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-05-14 17:15:56 +0000

    Import wpt@81726995f2fad2a715242162562e2010e6c12ae8
    Using wpt-import in Chromium 435ac43b2eac2546f9fd7877d5d382f598ef56a0.
    With Chromium commits locally applied on WPT:
    59c99fcd65 "Clipboard API Tests: Automate Web Platform Tests"
    7a588d8c6c "[client hints] Match update behavior to the spec."
    46619c7315 "[client hints] Enforce the top-frame requirement."
    3c2baf6442 "[Client-Hints] Fix cross-origin redirect leak"
    c15bc63cdb "[EventTiming] Test more event types"
    6640d9f951 "[EventTiming] Wrap lines better in event-timing-test-utils"
    200214d5ef "[client-hints] Ensure by-default&legacy hints behave with XO redirects"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: I248b1c93988422efce6357ba48e46053f741d0a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2201457
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#768793}

--- third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/resources/script-factory.js
+++ third_party/blink/web_tests/external/wpt/html/cross-origin-embedder-policy/resources/script-factory.js
@@ -2,8 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This creates a serialized <script> element that is useful for blob/data/srcdoc-style tests.
 
 function createScript(sameOrigin, crossOrigin, type="parent", id="") {
-  return `<script>
-const data = { id: "${id}",
+  return `const data = { id: "${id}",
                opener: !!window.opener,
                origin: window.origin,
                sameOriginNoCORPSuccess: false,
@@ -28,6 +27,5 @@ Promise.all(records).then(() => {
   } else {
     window.${type}.postMessage(data, "*");
   }
-});
-<\/script>`;
+});`;
 }
