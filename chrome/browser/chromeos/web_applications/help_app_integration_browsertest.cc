e89e8f80ed91f0a082ab3f5613d184b558a00f0d
Callistus <callistus@google.com>
2020-05-14 00:37:03 +0000

    Set default and minimum window size for the Help app.
    Carbon specs: https://carbon.googleplex.com/cros-ux/pages/show-off/app-structure#3e13a03d-5684-488c-b935-4129aedd6a68
    Demo: https://screencast.googleplex.com/cast/NTE1NzAyMzQ5Njc5ODIwOHxjY2U4OGY5Ni1iYw
    
    Bug: b/154777087
    Change-Id: Ibd79be825751334be65025155d0a1b17a807dbeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198058
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Rachel Carpenter <carpenterr@chromium.org>
    Commit-Queue: Callistus Tan <callistus@google.com>
    Cr-Commit-Position: refs/heads/master@{#768554}

--- chrome/browser/chromeos/web_applications/help_app_integration_browsertest.cc
+++ chrome/browser/chromeos/web_applications/help_app_integration_browsertest.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/chromeos/web_applications/system_web_app_integration_test.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/web_applications/system_web_app_manager.h"
 #include "chrome/browser/web_applications/system_web_app_manager_browsertest.h"
 #include "chromeos/components/help_app_ui/url_constants.h"
@@ -20,7 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/constants/chromeos_features.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/display/screen.h"
 #include "ui/display/types/display_constants.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/size.h"
 
 class HelpAppIntegrationTest : public SystemWebAppIntegrationTest {
  public:
@@ -46,6 +51,25 @@ IN_PROC_BROWSER_TEST_P(HelpAppIntegrationTest, HelpAppV2SearchInLauncher) {
   EXPECT_EQ(
       std::vector<std::string>({"Get Help", "Perks", "Offers"}),
       GetManager().GetAdditionalSearchTerms(web_app::SystemAppType::HELP));
+}
+
+// Test that the Help App has a minimum window size of 600x320.
+IN_PROC_BROWSER_TEST_P(HelpAppIntegrationTest, HelpAppV2MinWindowSize) {
+  WaitForSystemAppInstallAndLaunch(web_app::SystemAppType::HELP);
+  auto app_id = LaunchParamsForApp(web_app::SystemAppType::HELP).app_id;
+  EXPECT_EQ(GetManager().GetMinimumWindowSize(app_id), gfx::Size(600, 320));
+}
+
+// Test that the Help App has a default size of 960x600 and is in the center of
+// the screen.
+IN_PROC_BROWSER_TEST_P(HelpAppIntegrationTest, HelpAppV2DefaultWindowBounds) {
+  auto* browser =
+      WaitForSystemAppInstallAndLaunch(web_app::SystemAppType::HELP);
+  gfx::Rect work_area =
+      display::Screen::GetScreen()->GetDisplayForNewWindows().work_area();
+  int x = (work_area.width() - 960) / 2;
+  int y = (work_area.height() - 600) / 2;
+  EXPECT_EQ(browser->window()->GetBounds(), gfx::Rect(x, y, 960, 600));
 }
 
 // Test that the Help App logs metric when launching the app using the
--- chrome/browser/ui/web_applications/app_browser_controller.cc
+++ chrome/browser/ui/web_applications/app_browser_controller.cc
@@ -90,6 +90,7 @@ namespace {
 constexpr gfx::Rect TERMINAL_DEFAULT_BOUNDS(gfx::Point(64, 64),
                                             gfx::Size(652, 484));
 constexpr gfx::Size TERMINAL_SETTINGS_DEFAULT_SIZE(768, 512);
+constexpr gfx::Size HELP_DEFAULT_SIZE(960, 600);
 }  // namespace
 
 // static
@@ -318,6 +319,12 @@ gfx::Rect AppBrowserController::GetDefaultBounds() const {
       return bounds;
     }
     return TERMINAL_DEFAULT_BOUNDS;
+  } else if (system_app_type_ == SystemAppType::HELP) {
+    // Help app is centered.
+    gfx::Rect bounds =
+        display::Screen::GetScreen()->GetDisplayForNewWindows().work_area();
+    bounds.ClampToCenteredSize(HELP_DEFAULT_SIZE);
+    return bounds;
   }
   return gfx::Rect();
 }
