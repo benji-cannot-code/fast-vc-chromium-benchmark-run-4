d6e9700e85a4080aa68f6d966d03cb690a20e162
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-05-14 23:05:00 +0000

    [Extensions UI] Add an NTP-specific settings overridden dialog
    Add a specialization of the settings overridden dialog for extensions
    that override the New Tab Page. This involves adding a generic
    controller for the settings overridden dialog to handle extension-
    controlled settings (which will be re-used for additional controlled
    settings) and an NTP-specific provider to handle the NTP bits.
    
    Add unit tests for both the NTP-specific provider and the generic
    extension-controlled settings dialog controller, and a UI browser
    test for the NTP overridden dialog.
    
    Currently, this is only used in testing; it is not hooked up to any
    production codepaths.
    
    Bug: 1079364
    Change-Id: I1f360bd603b6514fb492acc0b065d1b6d5236a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186642
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769024}

--- chrome/browser/ui/views/extensions/settings_overridden_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/extensions/settings_overridden_dialog_view_browsertest.cc
@@ -5,11 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/extensions/settings_overridden_dialog_view.h"
 
+#include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/extensions/chrome_test_extension_loader.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/extensions/extension_settings_overridden_dialog.h"
 #include "chrome/browser/ui/extensions/settings_overridden_dialog_controller.h"
+#include "chrome/browser/ui/extensions/settings_overridden_params_providers.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
+#include "chrome/common/chrome_paths.h"
 #include "content/public/test/browser_test.h"
 
 namespace {
@@ -36,8 +42,15 @@ class TestDialogController : public SettingsOverriddenDialogController {
 
 class SettingsOverriddenDialogViewBrowserTest : public DialogBrowserTest {
   void ShowUi(const std::string& name) override {
-    ASSERT_EQ("SimpleDialog", name);
+    if (name == "SimpleDialog")
+      ShowSimpleDialog();
+    else if (name == "NtpOverriddenDialog")
+      ShowNtpOverriddenDialog();
+    else
+      NOTREACHED() << name;
+  }
 
+  void ShowSimpleDialog() {
     SettingsOverriddenDialogController::ShowParams params{
         base::ASCIIToUTF16("Settings overridden dialog title"),
         base::ASCIIToUTF16(
@@ -47,9 +60,34 @@ class SettingsOverriddenDialogViewBrowserTest : public DialogBrowserTest {
         std::make_unique<TestDialogController>(std::move(params)));
     dialog->Show(browser()->window()->GetNativeWindow());
   }
+
+  void ShowNtpOverriddenDialog() {
+    base::FilePath test_root_path;
+    ASSERT_TRUE(base::PathService::Get(chrome::DIR_TEST_DATA, &test_root_path));
+
+    Profile* const profile = browser()->profile();
+    scoped_refptr<const extensions::Extension> extension =
+        extensions::ChromeTestExtensionLoader(profile).LoadExtension(
+            test_root_path.AppendASCII("extensions/api_test/override/newtab"));
+    ASSERT_TRUE(extension);
+
+    base::Optional<ExtensionSettingsOverriddenDialog::Params> params =
+        settings_overridden_params::GetNtpOverriddenParams(profile);
+    ASSERT_TRUE(params);
+    auto controller = std::make_unique<ExtensionSettingsOverriddenDialog>(
+        std::move(*params), profile);
+
+    auto* dialog = new SettingsOverriddenDialogView(std::move(controller));
+    dialog->Show(browser()->window()->GetNativeWindow());
+  }
 };
 
 IN_PROC_BROWSER_TEST_F(SettingsOverriddenDialogViewBrowserTest,
                        InvokeUi_SimpleDialog) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_F(SettingsOverriddenDialogViewBrowserTest,
+                       InvokeUi_NtpOverriddenDialog) {
   ShowAndVerifyUi();
 }
