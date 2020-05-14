4435b1ad28c4a36782e6a9f19d243fd230421dc5
Roman Sorokin <rsorokin@chromium.org>
2020-05-14 18:20:10 +0000

    cros auth: Extract auth domain from the top level url
    Current flow relies on the page loaded event. SAML page might contain
    embedded pages which are hosted on separate domains (e.g. Google
    captcha). That might lead to wrong SAML notice "This sign-in service is
    hosted by ...".
    
    This CL extracts domain from the top level url on each loadcommit
    
    Fixed: 1055791
    Change-Id: I882a62dc29200c6eb6a569defb89f25c7db2de91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2201761
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768859}

--- chrome/browser/resources/gaia_auth_host/authenticator.js
+++ chrome/browser/resources/gaia_auth_host/authenticator.js
@@ -173,6 +173,16 @@ cr.define('cr.login', function() {
     'samlAclUrl',
   ];
 
+  /**
+   * Extract domain name from an URL.
+   * @param {string} url An URL string.
+   * @return {string} The host name of the URL.
+   */
+  function extractDomain(url) {
+    const a = document.createElement('a');
+    a.href = url;
+    return a.hostname;
+  }
 
   /**
    * Handlers for the HTML5 messages received from Gaia.
@@ -1134,7 +1144,6 @@ cr.define('cr.login', function() {
         return;
       }
 
-      this.authDomain = this.samlHandler_.authDomain;
       this.authFlow = AuthFlow.SAML;
 
       this.webview_.focus();
@@ -1263,6 +1272,9 @@ cr.define('cr.login', function() {
     onLoadCommit_(e) {
       if (this.gaiaId_) {
         this.maybeCompleteAuth_();
+      }
+      if (e.isTopLevel) {
+        this.authDomain = extractDomain(e.url);
       }
     }
 
