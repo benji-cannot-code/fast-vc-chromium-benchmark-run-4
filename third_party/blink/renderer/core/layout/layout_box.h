51b588da6666c64a2d4edbd47fb51efde1edf432
Xianzhu Wang <wangxianzhu@chromium.org>
2020-05-14 00:38:07 +0000

    cc::Layer::contents_opaque_for_text() to improve LCD text
    The flag is separated from cc::Layer::contents_opaque() for LCD text.
    By default it has the same value as contents_opaque(), but when
    contents_opaque() is false, blink can set contents_opaque_for_text()
    to true if all texts will be painted on opaque background, e.g.
      <div style="overflow: hidden; background: white; border-radius: 10px">
        TEXT
      </div>
    
    This reduces 30%~40% non-LCD-text pixels for contents not opaque reason
    (except background is not opaque), and 6%~7% overall non-LCD-text
    pixels:
    https://ct.skia.org/results/cluster-telemetry/tasks/chromium_perf_runs/wangxianzhu-ChromiumPerf-4900/html/index.html
    https://ct.skia.org/results/cluster-telemetry/tasks/chromium_perf_runs/wangxianzhu-ChromiumPerf-4901/html/index.html
    
    Bug: 642885
    Change-Id: I5566e7111ba711c4e50f9a03a0fb0500462f92b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2163035
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768555}

--- third_party/blink/renderer/core/layout/layout_box.h
+++ third_party/blink/renderer/core/layout/layout_box.h
@@ -221,6 +221,7 @@ class CORE_EXPORT LayoutBox : public LayoutBoxModelObject {
 
   bool BackgroundIsKnownToBeOpaqueInRect(
       const PhysicalRect& local_rect) const override;
+  bool TextIsKnownToBeOnOpaqueBackground() const override;
 
   virtual bool BackgroundShouldAlwaysBeClipped() const { return false; }
 
