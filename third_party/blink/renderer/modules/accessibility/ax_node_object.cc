8586f722e443e97d9d14960bcffd46b4ce11ddd7
Chris Hall <chrishall@chromium.org>
2020-05-14 03:34:03 +0000

    Remove 2 'magic' string constants from AXNodeObject.
     - Remove "TIME" and "math" magic string constants
    
    Written collaboratively with Aboxhall and Meredithl.
    
    Change-Id: Ife90662708ee5edcb1da73f1ecbf83d7c7db8e61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190590
    Commit-Queue: Chris Hall <chrishall@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768623}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -82,6 +82,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_table.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
+#include "third_party/blink/renderer/core/mathml_names.h"
 #include "third_party/blink/renderer/core/page/focus_controller.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/core/svg/svg_element.h"
@@ -795,7 +796,7 @@ ax::mojom::Role AXNodeObject::NativeRoleIgnoringAria() const {
   if (GetNode()->HasTagName(html_names::kDtTag))
     return ax::mojom::Role::kDescriptionListTerm;
 
-  if (GetNode()->nodeName() == "math")
+  if (GetNode()->nodeName() == mathml_names::kMathTag.LocalName())
     return ax::mojom::Role::kMath;
 
   if (GetNode()->HasTagName(html_names::kRpTag) ||
@@ -890,7 +891,7 @@ ax::mojom::Role AXNodeObject::NativeRoleIgnoringAria() const {
   if (GetNode()->HasTagName(html_names::kFigureTag))
     return ax::mojom::Role::kFigure;
 
-  if (GetNode()->nodeName() == "TIME")
+  if (GetNode()->HasTagName(html_names::kTimeTag))
     return ax::mojom::Role::kTime;
 
   if (IsA<HTMLPlugInElement>(GetNode())) {
