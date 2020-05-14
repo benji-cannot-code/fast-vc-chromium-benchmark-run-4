0ac8f7df5b2fa4c15acf501a6e8708100c8a3653
Yuzu Saijo <yuzus@chromium.org>
2020-05-14 05:02:09 +0000

    [content] Manage ManifestManagerHost per-document
    This CL converts ManifestManagerHost class away from being owned by
    WebContents and makes it a part of RenderDocumentHostUserData.
    
    We used to create an instance per-WebContents, but now it is created for
    document, and only for main-frame.
    
    Bug: 1077170
    Change-Id: I2a185a6cd6f290d3b904d359b55638bf09eaa79f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2147485
    Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#768647}

--- content/browser/manifest/manifest_manager_host.cc
+++ content/browser/manifest/manifest_manager_host.cc
@@ -10,25 +10,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/render_frame_host.h"
-#include "content/public/browser/web_contents.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "third_party/blink/public/common/manifest/manifest.h"
 
 namespace content {
 
-ManifestManagerHost::ManifestManagerHost(WebContents* web_contents)
-    : WebContentsObserver(web_contents),
-      manifest_url_change_observer_receivers_(web_contents, this) {}
+ManifestManagerHost::ManifestManagerHost(RenderFrameHost* render_frame_host)
+    : manifest_manager_frame_(render_frame_host) {
+  // Check that |manifest_manager_frame_| is a main frame.
+  DCHECK(!manifest_manager_frame_->GetParent());
+}
 
 ManifestManagerHost::~ManifestManagerHost() {
   OnConnectionError();
 }
 
-void ManifestManagerHost::RenderFrameDeleted(
-    RenderFrameHost* render_frame_host) {
-  if (render_frame_host == manifest_manager_frame_)
-    OnConnectionError();
+void ManifestManagerHost::BindObserver(
+    mojo::PendingAssociatedReceiver<blink::mojom::ManifestUrlChangeObserver>
+        receiver) {
+  manifest_url_change_observer_receiver_.Bind(std::move(receiver));
+}
+
+ManifestManagerHost* ManifestManagerHost::GetOrCreateForCurrentDocument(
+    RenderFrameHostImpl* rfh) {
+  DCHECK(rfh->is_main_frame());
+  if (!GetForCurrentDocument(rfh))
+    CreateForCurrentDocument(rfh);
+  return GetForCurrentDocument(rfh);
 }
 
 void ManifestManagerHost::GetManifest(GetManifestCallback callback) {
@@ -46,11 +55,7 @@ void ManifestManagerHost::RequestManifestDebugInfo(
 }
 
 blink::mojom::ManifestManager& ManifestManagerHost::GetManifestManager() {
-  if (manifest_manager_frame_ != web_contents()->GetMainFrame())
-    OnConnectionError();
-
   if (!manifest_manager_) {
-    manifest_manager_frame_ = web_contents()->GetMainFrame();
     manifest_manager_frame_->GetRemoteInterfaces()->GetInterface(
         manifest_manager_.BindNewPipeAndPassReceiver());
     manifest_manager_.set_disconnect_handler(base::BindOnce(
@@ -60,8 +65,6 @@ blink::mojom::ManifestManager& ManifestManagerHost::GetManifestManager() {
 }
 
 void ManifestManagerHost::OnConnectionError() {
-  manifest_manager_frame_ = nullptr;
-  manifest_manager_.reset();
   std::vector<GetManifestCallback> callbacks;
   for (CallbackMap::iterator it(&callbacks_); !it.IsAtEnd(); it.Advance()) {
     callbacks.push_back(std::move(*it.GetCurrentValue()));
@@ -69,6 +72,10 @@ void ManifestManagerHost::OnConnectionError() {
   callbacks_.Clear();
   for (auto& callback : callbacks)
     std::move(callback).Run(GURL(), blink::Manifest());
+
+  if (GetForCurrentDocument(manifest_manager_frame_)) {
+    DeleteForCurrentDocument(manifest_manager_frame_);
+  }
 }
 
 void ManifestManagerHost::OnRequestManifestResponse(
@@ -82,12 +89,16 @@ void ManifestManagerHost::OnRequestManifestResponse(
 
 void ManifestManagerHost::ManifestUrlChanged(
     const base::Optional<GURL>& manifest_url) {
-  if (manifest_url_change_observer_receivers_.GetCurrentTargetFrame() !=
-      web_contents()->GetMainFrame()) {
+  if (!manifest_manager_frame_->IsCurrent())
     return;
-  }
-  static_cast<WebContentsImpl*>(web_contents())
+
+  // TODO(yuzus): |NotifyManifestUrlChanged| should start taking a
+  // |RenderFrameHost| parameter.
+  WebContents* web_contents =
+      WebContents::FromRenderFrameHost(manifest_manager_frame_);
+  static_cast<WebContentsImpl*>(web_contents)
       ->NotifyManifestUrlChanged(manifest_url);
 }
 
+RENDER_DOCUMENT_HOST_USER_DATA_KEY_IMPL(ManifestManagerHost)
 }  // namespace content
