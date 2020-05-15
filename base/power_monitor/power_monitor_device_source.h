e5c11a2161f5de79fd5088e7b56708765e9e7e96
Miguel Casas-Sanchez <mcasas@chromium.org>
2020-05-15 17:16:02 +0000

    base/power_monitor: Add NotifyThermalStateChange.
    This CL adds a PowerObserver::OnThermalStateChange and associated
    DeviceThermalState enum, and wires them for MacOS (CrOs and Linux
    need some more work but the "API" would be the same).
    
    power_monitor_device_source_mac.mm uses a new ThermalStateObserver
    class to wrap around the thermal state change notification. This has
    been verified using a Chromium build and also a mini unit test.
    
    Bug: 1071431
    Test: ./out/gn/Chromium.app/Contents/MacOS/Chromium -vmodule=*power_monitor*=2,*thermal*=2
    while e.g. compiling chrome in parallel, and see the thermal
    state climb up as e.g. the fan kicks in (MBPro)
    
    Change-Id: I16991e674e84d2266bac98c73b3229bf85b25d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195334
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769316}

--- base/power_monitor/power_monitor_device_source.h
+++ base/power_monitor/power_monitor_device_source.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_cftyperef.h"
 #include "base/mac/scoped_ionotificationportref.h"
+#include "base/power_monitor/thermal_state_observer_mac.h"
 #endif
 
 #if defined(OS_IOS)
@@ -97,6 +98,9 @@ class BASE_EXPORT PowerMonitorDeviceSource : public PowerMonitorSource {
 
   // Run loop source to observe power-source-change events.
   ScopedCFTypeRef<CFRunLoopSourceRef> power_source_run_loop_source_;
+
+  // Observer of thermal state events: critical temperature etc.
+  std::unique_ptr<ThermalStateObserverMac> thermal_state_observer_;
 #endif
 
 #if defined(OS_IOS)
