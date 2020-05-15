eac745c59666b52cfd0aea39dcd34b38c939b9de
danakj <danakj@chromium.org>
2020-05-15 21:40:12 +0000

    Don't leak in view source tests.
    When a test turns on view source mode (due to being in /viewsource/) we
    don't ever turn it off for that frame. Previously it seems we disabled
    view source mode somehow by applying page visibility in between tests.
    
    R=nasko@chromium.org
    
    Bug: 866140, 1069111
    Change-Id: Icb56c91f62a16b4647f97ddb8c977ef82bfdf314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204626
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769438}

--- content/shell/renderer/web_test/test_runner.cc
+++ content/shell/renderer/web_test/test_runner.cc
@@ -1938,6 +1938,7 @@ void TestRunner::ResetWebFrame(WebFrameTestProxy* web_frame_test_proxy) {
   if (web_frame_test_proxy->IsMainFrame()) {
     web_frame->SetMainFrameOverlayColor(SK_ColorTRANSPARENT);
   }
+  web_frame->EnableViewSourceMode(false);
 }
 
 void TestRunner::SetTestIsRunning(bool running) {
