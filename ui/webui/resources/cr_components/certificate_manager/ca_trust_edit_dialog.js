06a4da8b28a7f5747175d18eb2e9b3beb0758043
dpapad <dpapad@chromium.org>
2020-05-15 00:17:32 +0000

    WebUI: Remove unnecessary absolute URLs from certificate_manager/
    Bug: 866236
    Change-Id: Id2fb17b6809631cdf2e1ec62217e5190ede6a79d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202498
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769073}

--- ui/webui/resources/cr_components/certificate_manager/ca_trust_edit_dialog.js
+++ ui/webui/resources/cr_components/certificate_manager/ca_trust_edit_dialog.js
@@ -9,15 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *    imported.
  *  - edit the trust level of an already existing certificate authority.
  */
-import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
-import 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.m.js';
-import 'chrome://resources/cr_elements/cr_dialog/cr_dialog.m.js';
+import '../../cr_elements/cr_button/cr_button.m.js';
+import '../../cr_elements/cr_checkbox/cr_checkbox.m.js';
+import '../../cr_elements/cr_dialog/cr_dialog.m.js';
 import 'chrome://resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
 import './certificate_shared_css.js';
 
-import {I18nBehavior} from 'chrome://resources/js/i18n_behavior.m.js';
-import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {I18nBehavior} from '../../js/i18n_behavior.m.js';
+import {loadTimeData} from '../../js/load_time_data.m.js';
 
 import {CaTrustInfo, CertificatesBrowserProxy, CertificatesBrowserProxyImpl, CertificateSubnode, NewCertificateSubNode} from './certificates_browser_proxy.js';
 
--- ui/webui/resources/cr_components/certificate_manager/certificates_browser_proxy.js
+++ ui/webui/resources/cr_components/certificate_manager/certificates_browser_proxy.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 // clang-format off
-import {addSingletonGetter, sendWithPromise} from 'chrome://resources/js/cr.m.js';
+import {addSingletonGetter, sendWithPromise} from '../../js/cr.m.js';
 // clang-format on
 
 /**
