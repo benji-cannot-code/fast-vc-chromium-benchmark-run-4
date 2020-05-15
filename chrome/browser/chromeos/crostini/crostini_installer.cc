e4207131a32ccbc20f77a0b314ba829327cf9525
Jason Lin <lxj@google.com>
2020-05-15 05:03:01 +0000

    crostini: installer set disk recommended size as per UX review
    see https://docs.google.com/presentation/d/1FWFaZ_t9lYT-VB7OFwDbMqHkPJebcCAuB5Um1zBCJEg/edit?ts=5e54a041#slide=id.g853c006a44_0_0
    
    Bug: 1082616
    Change-Id: I8fff7a28e0da428e7338ea44da2f52ba399ac3a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203037
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: Jason Lin <lxj@google.com>
    Cr-Commit-Position: refs/heads/master@{#769161}

--- chrome/browser/chromeos/crostini/crostini_installer.cc
+++ chrome/browser/chromeos/crostini/crostini_installer.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
 #include "chrome/browser/chromeos/crostini/ansible/ansible_management_service_factory.h"
+#include "chrome/browser/chromeos/crostini/crostini_disk.h"
 #include "chrome/browser/chromeos/crostini/crostini_features.h"
 #include "chrome/browser/chromeos/crostini/crostini_manager_factory.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
@@ -683,8 +684,10 @@ void CrostiniInstaller::OnAvailableDiskSpace(int64_t bytes) {
   free_disk_space_ = bytes;
   // Don't enforce minimum disk size on dev box or trybots because
   // base::SysInfo::AmountOfFreeDiskSpace returns zero in testing.
-  if (free_disk_space_ < kMinimumFreeDiskSpace &&
-      base::SysInfo::IsRunningOnChromeOS()) {
+  if (base::SysInfo::IsRunningOnChromeOS() &&
+      free_disk_space_ < restart_options_.disk_size_bytes.value_or(
+                             crostini::disk::kDiskHeadroomBytes +
+                             crostini::disk::kMinimumDiskSizeBytes)) {
     HandleError(InstallerError::kErrorInsufficientDiskSpace);
     return;
   }
