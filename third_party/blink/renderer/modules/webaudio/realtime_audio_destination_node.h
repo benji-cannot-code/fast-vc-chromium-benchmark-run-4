1f3a1f0ae622616f0f2e6d36ec8bc7b72c7dce0f
Hongchan Choi <hongchan@chromium.org>
2020-05-15 07:16:47 +0000

    Use SetDetectSlience(bool) in Web Audio
    This is a follow-up of
    https://chromium-review.googlesource.com/c/chromium/src/+/2194925
    
    With this CL, Web Audio uses the SetDetectSilience() method chain
    to signal WebAudioDevice when there is one or more "automatic pull"
    AudioNodes exist in the audio graph.
    
    - The automatic pull node check happens every render quantum, but
      the switchover of silence detection is limited to happening once
      per 30 seconds.
    - The transition from SilentSinkSuspender to real AudioOutputDevice
      thread will reply the captured buffer. Transition from a real AOD
      to SSS just drops the buffer since it is silence.
    - The start up cost for SilentSinkSuspender varies between 10-100ms,
      but this is safe because the transition is limited to once per
      30 seconds.
    
    Bug: 766198, 1030950
    Change-Id: Ia0bf520c6a6a6eae744beb36639e6f2f9ca84ee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2197816
    Auto-Submit: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769184}

--- third_party/blink/renderer/modules/webaudio/realtime_audio_destination_node.h
+++ third_party/blink/renderer/modules/webaudio/realtime_audio_destination_node.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <atomic>
 #include <memory>
+#include "base/memory/weak_ptr.h"
 #include "third_party/blink/public/platform/web_audio_latency_hint.h"
 #include "third_party/blink/renderer/modules/webaudio/audio_destination_node.h"
 #include "third_party/blink/renderer/platform/audio/audio_callback_metric_reporter.h"
@@ -41,8 +42,10 @@ class AudioContext;
 class ExceptionState;
 class WebAudioLatencyHint;
 
-class RealtimeAudioDestinationHandler final : public AudioDestinationHandler,
-                                              public AudioIOCallback {
+class RealtimeAudioDestinationHandler final
+    : public AudioDestinationHandler,
+      public AudioIOCallback,
+      public base::SupportsWeakPtr<RealtimeAudioDestinationHandler> {
  public:
   static scoped_refptr<RealtimeAudioDestinationHandler> Create(
       AudioNode&,
@@ -86,6 +89,9 @@ class RealtimeAudioDestinationHandler final : public AudioDestinationHandler,
     return allow_pulling_audio_graph_.load(std::memory_order_acquire);
   }
 
+  // Sets the detect silence flag for the platform destination.
+  void SetDetectSilence(bool detect_silence);
+
  private:
   explicit RealtimeAudioDestinationHandler(AudioNode&,
                                            const WebAudioLatencyHint&,
@@ -94,6 +100,11 @@ class RealtimeAudioDestinationHandler final : public AudioDestinationHandler,
   void CreatePlatformDestination();
   void StartPlatformDestination();
   void StopPlatformDestination();
+
+  // Checks the current silent detection condition (e.g. the number of
+  // automatic pull nodes) and flips the switch if necessary. Called within the
+  // Render() method.
+  void SetDetectSilenceIfNecessary(bool has_automatic_pull_nodes);
 
   // Should only be called from StartPlatformDestination.
   void EnablePullingAudioGraph() {
@@ -121,6 +132,11 @@ class RealtimeAudioDestinationHandler final : public AudioDestinationHandler,
   // DisablePullingAudioGraph) .  This is modified only by the main threda and
   // the audio thread only reads this.
   std::atomic_bool allow_pulling_audio_graph_;
+
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+
+  // The current condition of silence detection.
+  bool is_detecting_silence_ = false;
 };
 
 // -----------------------------------------------------------------------------
