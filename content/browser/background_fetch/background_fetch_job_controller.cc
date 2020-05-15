352aba7238edb4f7011e19c8251c8a2992c172e7
George Burgess IV <gbiv@chromium.org>
2020-05-15 17:13:32 +0000

    background_fetch: return after running our callback
    Running twice seems like a bug, given that we `std::move(callback)` in
    the `if` here.
    
    Bug: None
    Change-Id: Idf21045b20d11495523f5362278d54098f19e329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202945
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: George Burgess <gbiv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769314}

--- content/browser/background_fetch/background_fetch_job_controller.cc
+++ content/browser/background_fetch/background_fetch_job_controller.cc
@@ -378,6 +378,7 @@ void BackgroundFetchJobController::DidGetUploadData(
     Abort(BackgroundFetchFailureReason::SERVICE_WORKER_UNAVAILABLE,
           base::DoNothing());
     std::move(callback).Run(nullptr);
+    return;
   }
 
   DCHECK(blob);
