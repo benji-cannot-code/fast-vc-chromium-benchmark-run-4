46e284af2221a8af0ba28b91eb097cff5e8a9000
Julie Jeongeun Kim <jkim@igalia.com>
2020-05-15 16:53:59 +0000

    [printing] Mojofy PrintHostMsg_PrintPreviewInvalidPrinterSettings
    This CL replaces the PrintHostMsg_PrintPreviewInvalidPrinterSettings
    IPC message with a new PrinterSettingsInvalid Mojo method in the
    PrintPreviewUI interface.
    
    Bug: 1008939
    Change-Id: I73499267fa0af1ec2320b2cc9630c2f24febd4ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2200556
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769309}

--- chrome/browser/ui/webui/print_preview/print_preview_ui.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui.cc
@@ -726,10 +726,6 @@ void PrintPreviewUI::OnPrintPreviewFailed(int request_id) {
   handler_->OnPrintPreviewFailed(request_id);
 }
 
-void PrintPreviewUI::OnInvalidPrinterSettings(int request_id) {
-  handler_->OnInvalidPrinterSettings(request_id);
-}
-
 void PrintPreviewUI::OnHidePreviewDialog() {
   WebContents* preview_dialog = web_ui()->GetWebContents();
   BackgroundPrintingManager* background_printing_manager =
@@ -778,6 +774,12 @@ void PrintPreviewUI::PrintPreviewCancelled(int32_t document_cookie,
   // Always need to stop the worker.
   StopWorker(document_cookie);
   handler_->OnPrintPreviewCancelled(request_id);
+}
+
+void PrintPreviewUI::PrinterSettingsInvalid(int32_t document_cookie,
+                                            int32_t request_id) {
+  StopWorker(document_cookie);
+  handler_->OnInvalidPrinterSettings(request_id);
 }
 
 // static
