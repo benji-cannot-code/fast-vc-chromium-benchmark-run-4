cbe6097f0288e83eb2f5c2d13c64266a7ef7c10f
Anastasia Helfinstein <anastasi@google.com>
2020-05-15 19:04:49 +0000

    [Switch Access] Validate that group node is visible
    AX-Relnotes: n/a.
    Bug: None
    Change-Id: If02b6d90ac839e0367bb3a493aba89ae0963c501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203139
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769366}

--- chrome/browser/resources/chromeos/accessibility/switch_access/nodes/group_node.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/nodes/group_node.js
@@ -104,7 +104,7 @@ class GroupNode extends SAChildNode {
   isValidAndVisible() {
     for (const child of this.children_) {
       if (child.isValidAndVisible()) {
-        return true;
+        return !!this.location;
       }
     }
     return false;
