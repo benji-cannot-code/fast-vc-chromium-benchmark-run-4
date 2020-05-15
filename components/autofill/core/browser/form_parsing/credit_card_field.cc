2309f32766826edd28f937879808bdd8e4d06427
Uday Agarwal <udagarwa@microsoft.com>
2020-05-15 09:53:34 +0000

    [AF] Parse expiry year fields with 2 digit length
    Allow individual expiry year select fields to have 2 digit length, so
    they can be identified while parsing a form.
    
    Bug: 991484
    Change-Id: I41160e7aea64369d39f4b7d56cd4925dde03efa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190065
    Commit-Queue: Uday Agarwal <udagarwa@microsoft.com>
    Reviewed-by: Jared Saul <jsaul@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769215}

--- components/autofill/core/browser/form_parsing/credit_card_field.cc
+++ components/autofill/core/browser/form_parsing/credit_card_field.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/field_filler.h"
 #include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_parsing/autofill_scanner.h"
+#include "components/autofill/core/browser/form_parsing/form_field.h"
 #include "components/autofill/core/common/autofill_clock.h"
 #include "components/autofill/core/common/autofill_regex_constants.h"
 #include "components/autofill/core/common/autofill_regexes.h"
@@ -213,7 +214,7 @@ std::unique_ptr<FormField> CreditCardField::Parse(AutofillScanner* scanner,
       continue;
     }
 
-    if (credit_card_field->ParseExpirationDate(scanner)) {
+    if (credit_card_field->ParseExpirationDate(scanner, log_manager)) {
       nb_unknown_fields = 0;
       continue;
     }
@@ -309,7 +310,8 @@ bool CreditCardField::LikelyCardMonthSelectField(AutofillScanner* scanner) {
 }
 
 // static
-bool CreditCardField::LikelyCardYearSelectField(AutofillScanner* scanner) {
+bool CreditCardField::LikelyCardYearSelectField(AutofillScanner* scanner,
+                                                LogManager* log_manager) {
   if (scanner->IsEnd())
     return false;
 
@@ -318,18 +320,48 @@ bool CreditCardField::LikelyCardYearSelectField(AutofillScanner* scanner) {
                               MATCH_SELECT | MATCH_SEARCH))
     return false;
 
+  // Filter out days - elements for date entries would have
+  // numbers 1 to 9 as well in them, which we can filter on.
+  const base::string16 kSingleDigitDateRe = base::ASCIIToUTF16("\\b[1-9]\\b");
+  for (const auto& value : field->option_contents) {
+    if (MatchesPattern(value, kSingleDigitDateRe)) {
+      return false;
+    }
+  }
+
+  // Another way to eliminate days - filter out 'day' fields.
+  if (FormField::ParseFieldSpecifics(scanner, base::UTF8ToUTF16(kDayRe),
+                                     MATCH_DEFAULT | MATCH_SELECT, nullptr,
+                                     {log_manager, "kDayRe"})) {
+    return false;
+  }
+
+  // Filter out birth years - a website would not offer 1999 as a credit card
+  // expiration year, but show it in the context of a birth year selector.
+  const base::string16 kBirthYearRe = base::ASCIIToUTF16("(1999|99)");
+  for (const auto& value : field->option_contents) {
+    if (MatchesPattern(value, kBirthYearRe)) {
+      return false;
+    }
+  }
+
   const base::Time time_now = AutofillClock::Now();
   base::Time::Exploded time_exploded;
   time_now.UTCExplode(&time_exploded);
 
   const int kYearsToMatch = 3;
-  std::vector<base::string16> years_to_check;
+  std::vector<base::string16> years_to_check_4_digit;
+  std::vector<base::string16> years_to_check_2_digit;
   for (int year = time_exploded.year; year < time_exploded.year + kYearsToMatch;
        ++year) {
-    years_to_check.push_back(base::NumberToString16(year));
+    years_to_check_4_digit.push_back(base::NumberToString16(year));
+    years_to_check_2_digit.push_back(base::NumberToString16(year).substr(2));
   }
-  return (FindConsecutiveStrings(years_to_check, field->option_values) ||
-          FindConsecutiveStrings(years_to_check, field->option_contents));
+  return (
+      FindConsecutiveStrings(years_to_check_4_digit, field->option_values) ||
+      FindConsecutiveStrings(years_to_check_4_digit, field->option_contents) ||
+      FindConsecutiveStrings(years_to_check_2_digit, field->option_values) ||
+      FindConsecutiveStrings(years_to_check_2_digit, field->option_contents));
 }
 
 // static
@@ -428,7 +460,8 @@ void CreditCardField::AddClassifications(
   }
 }
 
-bool CreditCardField::ParseExpirationDate(AutofillScanner* scanner) {
+bool CreditCardField::ParseExpirationDate(AutofillScanner* scanner,
+                                          LogManager* log_manager) {
   if (!expiration_date_ && base::LowerCaseEqualsASCII(
                                scanner->Cursor()->form_control_type, "month")) {
     expiration_date_ = scanner->Cursor();
@@ -448,7 +481,7 @@ bool CreditCardField::ParseExpirationDate(AutofillScanner* scanner) {
   if (LikelyCardMonthSelectField(scanner)) {
     expiration_month_ = scanner->Cursor();
     scanner->Advance();
-    if (LikelyCardYearSelectField(scanner)) {
+    if (LikelyCardYearSelectField(scanner, log_manager)) {
       expiration_year_ = scanner->Cursor();
       scanner->Advance();
       return true;
