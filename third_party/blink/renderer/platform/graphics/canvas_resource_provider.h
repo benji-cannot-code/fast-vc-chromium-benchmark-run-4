9a82618ac06b03a001df36b239f6dc2ea970a1e6
Caleb Raitto <caraitto@chromium.org>
2020-05-15 16:00:03 +0000

    Compute canvas operation digest using PaintOps.
    JavaScript performs drawing operations on 2D HTMLCanvas objects via the
    canvas' context object.
    
    These context operations get translated into buffers of PaintOps.
    Serialize and hash these PaintOps to compute a digest of operations
    performed.
    
    Text must be handled separately, since font fallback logic occurs before
    PaintOp production.
    
    Bug: 973801
    Change-Id: Id0ab36d64a68954a4fe01ea4e72026a937adb8bc
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2185756
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769289}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/raster/playback_image_provider.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource.h"
+#include "third_party/blink/renderer/platform/graphics/identifiability_paint_op_digest.h"
 #include "third_party/blink/renderer/platform/graphics/image_orientation.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_recorder.h"
 #include "third_party/skia/include/core/SkSurface.h"
@@ -247,6 +248,10 @@ class PLATFORM_EXPORT CanvasResourceProvider
 
   void OnDestroyResource();
 
+  // Returns the identifiability digest computed from the set of PaintOps
+  // flushed from FlushCanvas().
+  uint64_t GetIdentifiabilityDigest();
+
  protected:
   class CanvasImageProvider;
 
@@ -337,6 +342,10 @@ class PLATFORM_EXPORT CanvasResourceProvider
   size_t max_inflight_resources_ = 0;
 
   RestoreMatrixClipStackCb restore_clip_stack_callback_;
+
+  // For identifiability metrics -- PaintOps are serialized so that digests can
+  // be calculated using hashes of the serialized output.
+  IdentifiabilityPaintOpDigest identifiability_paint_op_digest_;
 
   base::WeakPtrFactory<CanvasResourceProvider> weak_ptr_factory_{this};
 
