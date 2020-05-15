697ebb6b84b93db78e4741d042ba65f2b27556c7
Roman Aleksandrov <raleksandrov@google.com>
2020-05-15 11:53:20 +0000

    EnrollmentScreen: New error message when an enterpise consumer enrolls.
    Add handling new status code 905 from DMServer on an erollment request.
    It occurs when not-activated (or in other words inelibile) enterprise
    account is used to enroll the device.
    
    Bug: 1053829
    Change-Id: If01d81c68cd76f2156632aa9a6a4081c6c34b93d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199061
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Cr-Commit-Position: refs/heads/master@{#769239}

--- components/policy/core/common/cloud/device_management_service.cc
+++ components/policy/core/common/cloud/device_management_service.cc
@@ -124,6 +124,7 @@ const int DeviceManagementService::kServiceUnavailable;
 const int DeviceManagementService::kPolicyNotFound;
 const int DeviceManagementService::kDeprovisioned;
 const int DeviceManagementService::kArcDisabled;
+const int DeviceManagementService::kInvalidDomainlessCustomer;
 
 // static
 std::string DeviceManagementService::JobConfiguration::GetJobTypeAsString(
