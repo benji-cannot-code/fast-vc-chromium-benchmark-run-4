71ed79baeafa0fd29edc7a8debd6a8fa329c96af
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-05-15 11:16:39 +0000

    [Arc kiosk] Add a check for nullptr for ArcKioskAppService::Delegate
    The delegate of ArcKioskAppService sometimes is invalidated non only
    in the destructor of ArcKioskController. This led to the state where
    we are calling a delegate which is nullptr. Let's add a check for it.
    
    Bug: 1083184, b/156207090
    Change-Id: I69498d0d8f5f45ef668ee322bbe3e7deb0e395f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202861
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769229}

--- chrome/browser/chromeos/app_mode/arc/arc_kiosk_app_service.cc
+++ chrome/browser/chromeos/app_mode/arc/arc_kiosk_app_service.cc
@@ -132,7 +132,8 @@ void ArcKioskAppService::OnIconUpdated(ArcAppIcon* icon) {
   AccountId account_id = multi_user_util::GetAccountIdFromProfile(profile_);
   app_manager_->UpdateNameAndIcon(account_id, app_info_->name,
                                   app_icon_->image_skia());
-  delegate_->OnAppDataUpdated();
+  if (delegate_)
+    delegate_->OnAppDataUpdated();
 }
 
 void ArcKioskAppService::OnArcSessionRestarting() {
