e1a75c526e3219dbfe99a43d46844ae23aaa4efb
Hiroki Nakagawa <nhiroki@chromium.org>
2020-05-15 08:04:44 +0000

    ServiceWorker: Move mojom::ServiceWorkerContainerInfoForClient
    This is a follow-up CL for https://crrev.com/c/2200478
    
    This moves mojom::ServiceWorkerContainerInfoForClient struct from
    service_worker_provider.mojom to service_worker_container.mojom. The
    struct contains information for ServiceWorkerContainer, not for
    ServiceWorkerProvider, so service_worker_container.mojom is more
    appropriate place to put the definition.
    
    Bug: 931087
    Change-Id: I2d4465c519cc850fb4d7a9125e2a74307f15e4cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203509
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769189}

--- content/browser/worker_host/mock_shared_worker.h
+++ content/browser/worker_host/mock_shared_worker.h
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/messaging/message_port_channel.h"
 #include "third_party/blink/public/mojom/renderer_preferences.mojom.h"
 #include "third_party/blink/public/mojom/service_worker/controller_service_worker.mojom.h"
-#include "third_party/blink/public/mojom/service_worker/service_worker_provider.mojom.h"
+#include "third_party/blink/public/mojom/service_worker/service_worker_container.mojom.h"
 #include "third_party/blink/public/mojom/worker/shared_worker_factory.mojom.h"
 #include "third_party/blink/public/mojom/worker/worker_content_settings_proxy.mojom.h"
 
