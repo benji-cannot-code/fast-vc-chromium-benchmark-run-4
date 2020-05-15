e2d3fe1b862d65a406fc2d83a49ac5190bee5f12
Ewann <ewannpv@google.com>
2020-05-15 12:23:56 +0000

    [iOS][Page-Info] Adds PageInfo NavBar
    This CL adds a 'Done' button to the navBar that dismiss the PageInfo.
    The URL of the visited site is also displayed at the top of the navBar.
    
    screenshot:
    https://drive.google.com/file/d/1Q4r_RY4rtRhgD64d-egsS60XN5MCllju/view?usp=sharing
    
    Bug: 1038919
    Change-Id: I889cb85f310e32d8961d4cab3b777f2ab5407a78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195264
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769244}

--- ios/chrome/browser/ui/page_info/page_info_coordinator.mm
+++ ios/chrome/browser/ui/page_info/page_info_coordinator.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/page_info/page_info_site_security_description.h"
 #import "ios/chrome/browser/ui/page_info/page_info_site_security_mediator.h"
 #import "ios/chrome/browser/ui/page_info/page_info_view_controller.h"
+#import "ios/chrome/browser/ui/table_view/table_view_navigation_controller.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 #include "ios/web/public/navigation/navigation_item.h"
 #include "ios/web/public/navigation/navigation_manager.h"
@@ -23,16 +24,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface PageInfoCoordinator ()
 
+@property(nonatomic, strong)
+    TableViewNavigationController* navigationController;
 @property(nonatomic, strong) CommandDispatcher* dispatcher;
 @property(nonatomic, strong) PageInfoViewController* viewController;
-@property(nonatomic, strong) UINavigationController* navigationController;
 
 @end
 
 @implementation PageInfoCoordinator
 
 @synthesize presentationProvider = _presentationProvider;
-@synthesize navigationController = _navigationController;
 
 #pragma mark - ChromeCoordinator
 
@@ -54,12 +55,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.viewController = [[PageInfoViewController alloc]
       initWithSiteSecurityDescription:description];
+  self.navigationController =
+      [[TableViewNavigationController alloc] initWithTable:self.viewController];
+
   self.dispatcher = self.browser->GetCommandDispatcher();
   self.viewController.handler =
       HandlerForProtocol(self.dispatcher, BrowserCommands);
 
-  self.navigationController = [[UINavigationController alloc]
-      initWithRootViewController:self.viewController];
   [self.baseViewController presentViewController:self.navigationController
                                         animated:YES
                                       completion:nil];
