58e302b1d2abeeec04b03b6a356a54c9dd6dbef6
Marina Ciocea <marinaciocea@chromium.org>
2020-05-15 16:06:37 +0000

    Use WebRTC mock in RTCEncodedVideoUnderlyingSink/SourceTest tests.
    Remove fake implementation of TransformableVideoFrameInterface from
    Blink and use the mock defined in WebRTC instead.
    
    Bug: 1069295
    Change-Id: I3a9a77cb1e706b019b6f868ab5c4b78d63a72e03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198955
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769295}

--- third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_underlying_source_test.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_encoded_video_underlying_source_test.cc
@@ -15,27 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/streams/readable_stream_default_controller_with_script_scope.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/webrtc/api/frame_transformer_interface.h"
+#include "third_party/webrtc/api/test/mock_transformable_video_frame.h"
 
 namespace blink {
-
-namespace {
-class FakeTransformableFrame : public webrtc::TransformableVideoFrameInterface {
- public:
-  FakeTransformableFrame() = default;
-  ~FakeTransformableFrame() override = default;
-
-  rtc::ArrayView<const uint8_t> GetData() const override { return nullptr; }
-
-  void SetData(rtc::ArrayView<const uint8_t> data) override {}
-
-  uint32_t GetTimestamp() const override { return 0; }
-  uint32_t GetSsrc() const override { return 0; }
-  bool IsKeyFrame() const override { return false; }
-  std::vector<uint8_t> GetAdditionalData() const override {
-    return std::vector<uint8_t>();
-  }
-};
-}  // namespace
 
 class RTCEncodedVideoUnderlyingSourceTest : public testing::Test {
  public:
@@ -62,7 +44,8 @@ TEST_F(RTCEncodedVideoUnderlyingSourceTest,
   ScriptPromiseTester read_tester(script_state,
                                   reader->read(script_state, exception_state));
   EXPECT_FALSE(read_tester.IsFulfilled());
-  source->OnFrameFromSource(std::make_unique<FakeTransformableFrame>());
+  source->OnFrameFromSource(
+      std::make_unique<webrtc::MockTransformableVideoFrame>());
   read_tester.WaitUntilSettled();
   EXPECT_TRUE(read_tester.IsFulfilled());
 
@@ -91,12 +74,14 @@ TEST_F(RTCEncodedVideoUnderlyingSourceTest, QueuedFramesAreDroppedWhenOverflow) 
   for (int i = 0; i > RTCEncodedVideoUnderlyingSource::kMinQueueDesiredSize;
        --i) {
     EXPECT_EQ(source->Controller()->DesiredSize(), i);
-    source->OnFrameFromSource(std::make_unique<FakeTransformableFrame>());
+    source->OnFrameFromSource(
+        std::make_unique<webrtc::MockTransformableVideoFrame>());
   }
   EXPECT_EQ(source->Controller()->DesiredSize(),
             RTCEncodedVideoUnderlyingSource::kMinQueueDesiredSize);
 
-  source->OnFrameFromSource(std::make_unique<FakeTransformableFrame>());
+  source->OnFrameFromSource(
+      std::make_unique<webrtc::MockTransformableVideoFrame>());
   EXPECT_EQ(source->Controller()->DesiredSize(),
             RTCEncodedVideoUnderlyingSource::kMinQueueDesiredSize);
 
