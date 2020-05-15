c678b70eb50b3c2ba5f406be9026fe6ddbcb9a24
Bailey Berro <baileyberro@chromium.org>
2020-05-15 00:54:26 +0000

    Add DisplayObserver::DISPLAY_METRIC_REFRESH_RATE
    This change ensures that when just the refresh rate is changed on a
    display, the proper observer events fire.
    
    Bug: 1024113
    Change-Id: Id654d75beb14b1f7a9f345af3953dc5cb8726bb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199716
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769096}

--- ui/display/manager/display_manager.cc
+++ ui/display/manager/display_manager.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/display/display_observer.h"
 #include "ui/display/display_switches.h"
 #include "ui/display/manager/display_layout_store.h"
+#include "ui/display/manager/display_util.h"
 #include "ui/display/manager/managed_display_info.h"
 #include "ui/display/screen.h"
 #include "ui/display/types/display_snapshot.h"
@@ -48,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/system/devicemode.h"
 #include "ui/display/manager/display_change_observer.h"
 #include "ui/display/manager/display_configurator.h"
-#include "ui/display/manager/display_util.h"
 #include "ui/display/types/native_display_delegate.h"
 #include "ui/events/devices/touchscreen_device.h"
 #endif
@@ -985,6 +985,11 @@ void DisplayManager::UpdateDisplaysWith(
 
       if (current_display.rotation() != new_display.rotation())
         metrics |= DisplayObserver::DISPLAY_METRIC_ROTATION;
+
+      if (!WithinEpsilon(current_display.display_frequency(),
+                         new_display.display_frequency())) {
+        metrics |= DisplayObserver::DISPLAY_METRIC_REFRESH_RATE;
+      }
 
       if (metrics != DisplayObserver::DISPLAY_METRIC_NONE) {
         display_changes.insert(
