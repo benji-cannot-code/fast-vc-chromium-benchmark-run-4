b9829ec563e9650d277b50d21846c81c34a56af0
Ionel Popescu <iopopesc@microsoft.com>
2020-05-15 18:40:46 +0000

    Update eye dropper icon and ensure that it is closed when the popup is dismissed
    This CL contains the following fixes:
    
    1. updates the eye dropper icon to match the spec [1].
    
    2. ensures that the eye dropper is closed as soon as the popup is dismissed.
    
    [1] https://bugs.chromium.org/p/chromium/issues/detail?id=992297#c10
    
    Bug: 992297
    Change-Id: I99149ee34017107f717ad9add6385d2ec37bd542
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203258
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#769354}

--- third_party/blink/renderer/core/html/forms/color_chooser_popup_ui_controller.cc
+++ third_party/blink/renderer/core/html/forms/color_chooser_popup_ui_controller.cc
@@ -226,6 +226,7 @@ void ColorChooserPopupUIController::SetValue(const String& value) {
 
 void ColorChooserPopupUIController::DidClosePopup() {
   popup_ = nullptr;
+  eye_dropper_chooser_.reset();
 
   if (!chooser_)
     EndChooser();
