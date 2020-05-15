4c4f5a93331f0ab8b45ba02f8a18e18ec98a8a4b
Thomas Lukaszewicz <tluk@chromium.org>
2020-05-15 23:34:58 +0000

    Revert "add test for web_app::WebAppUninstallDialogDelegateView"
    This reverts commit 7bde7700a69deccfd4f2830460012dd64bd12b23.
    
    Reason for revert:
    
    Deterministic build failure on Win7 Tests bot for the following:
    
    WebAppUninstallDialogViewBrowserTest.TestWebAppUninstallDialogDelegateViewAccept
    
    Original change's description:
    > add test for web_app::WebAppUninstallDialogDelegateView
    > 
    > This adds test for the flow of initiating uninstall and accept/cancel
    > the uninstall action. It also fixes a bug that the autoconfirm for testing is called
    > before the widget is instantiated by
    > |constrained_window::CreateBrowserModalDialogViews|
    > 
    > Bug: 1073258
    > Change-Id: Ifad3298f892be3e1e1875cb83e29cf87ce9f8b34
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198369
    > Reviewed-by: Alan Cutter <alancutter@chromium.org>
    > Commit-Queue: Phillis Tang <phillis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#769406}
    
    TBR=alancutter@chromium.org,phillis@chromium.org
    
    Change-Id: I65c61881adfccb7fe7acf94f00cc0f43a4309219
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1073258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204933
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769489}

--- chrome/browser/ui/views/web_apps/web_app_uninstall_dialog_view.h
+++ chrome/browser/ui/views/web_apps/web_app_uninstall_dialog_view.h
@@ -47,7 +47,6 @@ class WebAppUninstallDialogDelegateView : public views::DialogDelegateView {
   WebAppUninstallDialogDelegateView& operator=(
       const WebAppUninstallDialogDelegateView&) = delete;
   ~WebAppUninstallDialogDelegateView() override;
-  void ProcessAutoConfirmValue();
 
  private:
   // views::DialogDelegateView:
@@ -61,6 +60,7 @@ class WebAppUninstallDialogDelegateView : public views::DialogDelegateView {
   // Uninstalls the web app. Returns true on success.
   bool Uninstall();
   void ClearWebAppSiteData();
+  void ProcessAutoConfirmValue();
 
   void OnDialogAccepted();
   void OnDialogCanceled();
