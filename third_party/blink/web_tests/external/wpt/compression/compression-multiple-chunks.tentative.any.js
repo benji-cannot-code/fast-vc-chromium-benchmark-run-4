9fb8255467772ef73e4adbcdf62d753a5cd40475
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-05-15 01:30:02 +0000

    Import wpt@fc9010a1907e210fc8304673d59eb8a99e1e8791
    Using wpt-import in Chromium 73b89f4c19560bde49971f7a998868aa3fb73add.
    With Chromium commits locally applied on WPT:
    3c2baf6442 "[Client-Hints] Fix cross-origin redirect leak"
    6640d9f951 "[EventTiming] Wrap lines better in event-timing-test-utils"
    200214d5ef "[client-hints] Ensure by-default&legacy hints behave with XO redirects"
    b7641670c0 "Added in special case for timeline inclusive end point"
    050a13e644 "[LayoutNG] Change NGBlockLayoutAlgorithm::FinishInflow DCHECK."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=lpz
    
    No-Export: true
    Change-Id: I0ac94aec7f4838577105ea54ea39b078ba8427fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202879
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#769111}

--- third_party/blink/web_tests/external/wpt/compression/compression-multiple-chunks.tentative.any.js
+++ third_party/blink/web_tests/external/wpt/compression/compression-multiple-chunks.tentative.any.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=window,worker
-// META: script=pako/pako_inflate.min.js
+// META: script=third_party/pako/pako_inflate.min.js
 // META: timeout=long
 
 'use strict';
