03228fefa831677c6e7d2af6af90334ecde92400
Jonah Chin <jochin@microsoft.com>
2020-05-15 18:02:42 +0000

    Add set_texture_image() API to PaintImageBuilder
    Adding a DCHECK to PaintImageBuilder::set_image() ensures texture backed
    SkImages are not used to create a PaintImages from the renderer process.
    This is to align with the eventual goal of removing all skia usage from
    the client.
    
    This change moves valid callers to the new
    PaintImageBuilder::set_texture_image(). Eventually this API  should only
    be called from the GPU process. However, a few non-GPU process callers
    currently exist (see ImagePaintFilter and PaintShader). These callers
    will be removed once we fully transition to OOPR.
    
    For details about this overall PaintImage effort, see crbug.com/1023259
    
    This is being done as part of the OOPR-Canvas2D project. For more info
    about that project see the tracking bug here: crbug.com/1018894
    
    Bug: 1031050
    Change-Id: I2516da5e0bc034bd785acc8324d7535a2d93508e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186709
    Commit-Queue: Jonah Chin <jochin@microsoft.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769341}

--- cc/test/skia_common.cc
+++ cc/test/skia_common.cc
@@ -236,7 +236,7 @@ PaintImage CreateNonDiscardablePaintImage(const gfx::Size& size) {
   bitmap.eraseColor(SK_AlphaTRANSPARENT);
   return PaintImageBuilder::WithDefault()
       .set_id(PaintImage::GetNextId())
-      .set_image(
+      .set_texture_image(
           SkImage::MakeFromBitmap(bitmap)->makeTextureImage(context.get()),
           PaintImage::GetNextContentId())
       .TakePaintImage();
