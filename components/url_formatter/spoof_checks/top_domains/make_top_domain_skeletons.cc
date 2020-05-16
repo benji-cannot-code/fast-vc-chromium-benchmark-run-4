5f8c9ac3bded6b402f7a4b67df2a41af605ac5ab
Joe DeBlasio <jdeblasio@chromium.org>
2020-05-16 00:54:44 +0000

    [Top Domains] Permit top 500 list to be... less than 500 domains.
    This CL makes some small changes to allow the top 500 list to be less
    than 500 domains long. This allows us to remove domains that should no
    longer be in the list without having to regenerate the whole thing.
    
    This CL also incidentally fixes a bug wherein some domains were treated
    as top-500 domains even though they weren't.
    
    This CL keeps some statically-defined variables that are defined in a
    generated .cc file as having 500 entries, even though they actually have
    fewer than that. This prevents us from having to regenerate the header
    file as well, but eventually that's the right answer. I'm hoping we can
    merge this CL to M84, which necessitates simplicity and safety over
    elegance.
    
    Bug: 1083489,1083487
    Change-Id: I45a5425cd4a8c4a9135d6f723b08d628f164f7df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204554
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769507}

--- components/url_formatter/spoof_checks/top_domains/make_top_domain_skeletons.cc
+++ components/url_formatter/spoof_checks/top_domains/make_top_domain_skeletons.cc
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/icu/source/common/unicode/utypes.h"
 #include "third_party/icu/source/i18n/unicode/uspoof.h"
 
+const char* kTop500Separator = "###END_TOP_500###";
+
 base::FilePath GetPath(base::StringPiece basename) {
   base::FilePath path;
   base::PathService::Get(base::DIR_SOURCE_ROOT, &path);
@@ -75,6 +77,13 @@ int GenerateSkeletons(const char* input_file_name,
   size_t max_labels = 0;
   std::string domain_with_max_labels;
   while (std::getline(input, domain)) {
+    base::TrimWhitespaceASCII(domain, base::TRIM_ALL, &domain);
+
+    if (domain == kTop500Separator) {
+      output += std::string(kTop500Separator) + "\n";
+      continue;
+    }
+
     if (domain[0] == '#')
       continue;
 
