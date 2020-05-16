476ff761709f6855aac7f40db6b7736bd18a277e
David Tseng <dtseng@chromium.org>
2020-05-16 01:17:20 +0000

    Plumb through an api to get display cell size from brltty
    R=dmazzoni@chromium.org
    
    Test: manual; verify a cell size of 8 on a display, when call chrome.brailleDisplayPrivate.getDisplayState, on a 8-dot celled braille display.
    Change-Id: I25a2517e2a6ff29b38746a7ddfb8294669476d9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2194339
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769511}

--- chrome/browser/extensions/api/braille_display_private/braille_display_private_apitest.cc
+++ chrome/browser/extensions/api/braille_display_private/braille_display_private_apitest.cc
@@ -51,6 +51,7 @@ struct MockBrlapiConnectionData {
   bool connected;
   size_t display_columns;
   size_t display_rows;
+  size_t cell_size;
   brlapi_error_t error;
   std::vector<std::string> written_content;
   // List of brlapi key codes.  A negative number makes the connection mock
@@ -126,6 +127,11 @@ class MockBrlapiConnection : public BrlapiConnection {
     }
   }
 
+  bool GetCellSize(unsigned int* cell_size) override {
+    *cell_size = data_->cell_size;
+    return true;
+  }
+
  private:
   void NotifyDataReady() {
     on_data_ready_.Run();
@@ -153,6 +159,7 @@ class BrailleDisplayPrivateApiTest : public ExtensionApiTest {
         base::Bind(
             &BrailleDisplayPrivateApiTest::CreateBrlapiConnection,
             base::Unretained(this)));
+    BrailleControllerImpl::GetInstance()->skip_libbrlapi_so_load_ = true;
     DisableAccessibilityManagerBraille();
   }
 
@@ -260,6 +267,7 @@ IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, KeyEvents) {
 IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, DisplayStateChanges) {
   connection_data_.display_columns = 11;
   connection_data_.display_rows = 1;
+  connection_data_.cell_size = 6;
   connection_data_.pending_keys.push_back(kErrorKeyCode);
   connection_data_.reappear_on_disconnect = true;
   ASSERT_TRUE(RunComponentExtensionTest(
