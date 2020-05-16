3f1b9ae48b97b64a5aa4041b5219f787bb9a8c36
Rakesh Soma <rakeshsoma@google.com>
2020-05-16 01:56:56 +0000

    Define and implement the mojom interface for GcpwHidBroker
    Change-Id: Ia9b9677a9b5b33f86691a9da3e2d2055c14c03fc
    Bug: 1079070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2184954
    Reviewed-by: Rakesh Soma <rakeshsoma@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Rakesh Soma <rakeshsoma@google.com>
    Cr-Commit-Position: refs/heads/master@{#769518}

---
+++ base/win/scoped_devinfo.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_WIN_SCOPED_DEVINFO_H_
+#define BASE_WIN_SCOPED_DEVINFO_H_
+
+#include <setupapi.h>
+
+#include "base/scoped_generic.h"
+
+namespace base {
+namespace win {
+
+struct DevInfoScopedTraits {
+  static HDEVINFO InvalidValue() { return INVALID_HANDLE_VALUE; }
+  static void Free(HDEVINFO h) { SetupDiDestroyDeviceInfoList(h); }
+};
+using ScopedDevInfo = base::ScopedGeneric<HDEVINFO, DevInfoScopedTraits>;
+
+}  // namespace win
+}  // namespace base
+
+#endif  // BASE_WIN_SCOPED_DEVINFO_H_
