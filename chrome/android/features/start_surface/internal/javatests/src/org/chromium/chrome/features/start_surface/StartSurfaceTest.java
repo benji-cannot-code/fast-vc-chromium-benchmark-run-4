ce05edf8354c5b178c26050e40dea24f731414c5
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-05-16 01:29:05 +0000

    Selectively disable StartSurfaceTest#testScroll_OmniboxOnly
    StartSurfaceTest#testScroll_OmniboxOnly fails on Lollipop builders
    as well. Disable it on all builders below Pie.
    
    TBR=peconn@chromium.org
    
    Bug: 1083174
    Change-Id: I74a634296ef3230ff9e3bc34d31ce3c3aeb6a39f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204534
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769513}

--- chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/internal/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.features.start_surface;
 
+import static android.os.Build.VERSION_CODES.P;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -60,7 +61,6 @@ import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -211,7 +211,6 @@ public class StartSurfaceTest {
     @MediumTest
     @Feature({"StartSurface"})
     @DisableIf.Build(hardware_is = "bullhead", message = "crbug.com/1081657")
-    @DisabledTest(message = "crbug.com/1083174")
     // clang-format off
     @CommandLineFlags.Add({BASE_PARAMS + "/omniboxonly" +
             "/hide_switch_when_no_incognito_tabs/true"})
@@ -797,7 +796,7 @@ public class StartSurfaceTest {
     @MediumTest
     @Feature({"StartSurface"})
     // clang-format off
-    @DisableIf.Build(hardware_is = "bullhead", message = "crbug.com/1081657")
+    @DisableIf.Build(sdk_is_less_than = P, message = "crbug.com/1083174")
     @CommandLineFlags.Add({BASE_PARAMS + "/omniboxonly" +
         "/hide_switch_when_no_incognito_tabs/true/omnibox_scroll_mode/quick"})
     public void testScroll_OmniboxOnly_Quick() {
