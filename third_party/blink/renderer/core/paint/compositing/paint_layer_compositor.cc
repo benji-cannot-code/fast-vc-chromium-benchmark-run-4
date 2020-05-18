525b171651e03f5286aa282f8e4a65c0345d4e3a
Xianzhu Wang <wangxianzhu@chromium.org>
2020-05-18 18:06:27 +0000

    Put mask layer in GraphicsLayer tree hierarchy
    After BlinkGenPropertyTrees launched, we no longer need to handle mask
    layers specially in GraphicsLayer tree. The associated effect node
    specifies a special blending mode for the mask layer.
    
    Change-Id: Ib3043caf6a5d0a71d39449b564455f9aaba780a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2205973
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769759}

--- third_party/blink/renderer/core/paint/compositing/paint_layer_compositor.cc
+++ third_party/blink/renderer/core/paint/compositing/paint_layer_compositor.cc
@@ -734,9 +734,6 @@ static void UpdateTrackingRasterInvalidationsRecursive(
 
   for (wtf_size_t i = 0; i < graphics_layer->Children().size(); ++i)
     UpdateTrackingRasterInvalidationsRecursive(graphics_layer->Children()[i]);
-
-  if (GraphicsLayer* mask_layer = graphics_layer->MaskLayer())
-    UpdateTrackingRasterInvalidationsRecursive(mask_layer);
 }
 
 void PaintLayerCompositor::UpdateTrackingRasterInvalidations() {
