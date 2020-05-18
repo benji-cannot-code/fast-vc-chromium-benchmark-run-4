2f9a690046a5ef144331cbeac8460bb5c46444b3
Thomas Lukaszewicz <tluk@chromium.org>
2020-05-18 23:49:21 +0000

    Revert "Reland: Plumb through an api to get display cell size from brltty"
    This reverts commit cfba7924aef8ad27580055a4cfd3b785443a8ae3.
    
    Reason for revert:
    Tests failing on the Linux ChromiumOS MSan Tests bot:
    
    - BrailleDisplayPrivateApiTest.WriteDots
    - BrailleDisplayPrivateApiTest.KeyEvents
    
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/19188
    
    Original change's description:
    > Reland: Plumb through an api to get display cell size from brltty
    > 
    > |cell_size| was not initialized during the test which expected it to be. The parameter is optional according to idl, but in this context, it probably should be initialized.
    > 
    > Original change
    > https://chromium-review.googlesource.com/c/chromium/src/+/2206198
    > 
    > TBR=dtseng@chromium.org
    > 
    > Change-Id: I744c7b567cf2c4d27653f4368edc9c0f635d7d8d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207412
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Commit-Queue: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#769736}
    
    TBR=dtseng@chromium.org
    
    Change-Id: I07143ad331dcb627ddd4a768525c1caf5330a70c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208103
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769904}

--- chrome/browser/extensions/api/braille_display_private/braille_display_private_apitest.cc
+++ chrome/browser/extensions/api/braille_display_private/braille_display_private_apitest.cc
@@ -51,7 +51,6 @@ struct MockBrlapiConnectionData {
   bool connected;
   size_t display_columns;
   size_t display_rows;
-  size_t cell_size;
   brlapi_error_t error;
   std::vector<std::string> written_content;
   // List of brlapi key codes.  A negative number makes the connection mock
@@ -127,11 +126,6 @@ class MockBrlapiConnection : public BrlapiConnection {
     }
   }
 
-  bool GetCellSize(unsigned int* cell_size) override {
-    *cell_size = data_->cell_size;
-    return true;
-  }
-
  private:
   void NotifyDataReady() {
     on_data_ready_.Run();
@@ -159,7 +153,6 @@ class BrailleDisplayPrivateApiTest : public ExtensionApiTest {
         base::Bind(
             &BrailleDisplayPrivateApiTest::CreateBrlapiConnection,
             base::Unretained(this)));
-    BrailleControllerImpl::GetInstance()->skip_libbrlapi_so_load_ = true;
     DisableAccessibilityManagerBraille();
   }
 
@@ -267,7 +260,6 @@ IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, KeyEvents) {
 IN_PROC_BROWSER_TEST_F(BrailleDisplayPrivateApiTest, DisplayStateChanges) {
   connection_data_.display_columns = 11;
   connection_data_.display_rows = 1;
-  connection_data_.cell_size = 6;
   connection_data_.pending_keys.push_back(kErrorKeyCode);
   connection_data_.reappear_on_disconnect = true;
   ASSERT_TRUE(RunComponentExtensionTest(
