6a4d03a11f1c8402c953bca9b7e169723893a7e9
Hesen Zhang <hesen@google.com>
2020-05-18 23:59:22 +0000

    [Query Tiles]: bug fix and unit test for scheduler and fetcher.
    - Fixed fetcher crash because ResourceRequest was null if a second
    request was asked but it was build in ctor.
    - Fixed a wrong usage of OneOffTaskInfo window.
    - Added unit test to verify the start/end window and backoff delay
    in task info.
    
    - If debug switch instant fetch on, we always schedule a fetch task
    on service startup once, don't do recurring fetching task.
    
    Bug: 1083532
    Change-Id: I8c1a29bc7c7692b6c59a2de958589b41ca7957c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207616
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Auto-Submit: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769910}

--- components/query_tiles/internal/tile_service_scheduler.cc
+++ components/query_tiles/internal/tile_service_scheduler.cc
@@ -19,6 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace query_tiles {
 namespace {
 
+// Schedule window params in instant schedule mode.
+const int kInstantScheduleWindowStartMs = 10 * 1000;  // 10 seconds
+const int kInstantScheduleWindowEndMs = 20 * 1000;    // 20 seconds
+
 class TileServiceSchedulerImpl : public TileServiceScheduler {
  public:
   TileServiceSchedulerImpl(
@@ -43,6 +47,9 @@ class TileServiceSchedulerImpl : public TileServiceScheduler {
   }
 
   void OnFetchCompleted(TileInfoRequestStatus status) override {
+    if (IsInstantFetchMode())
+      return;
+
     if (status == TileInfoRequestStatus::kShouldSuspend) {
       MaximizeBackoff();
       ScheduleTask(false);
@@ -57,6 +64,12 @@ class TileServiceSchedulerImpl : public TileServiceScheduler {
   }
 
   void OnTileManagerInitialized(TileGroupStatus status) override {
+    if (IsInstantFetchMode()) {
+      ResetBackoff();
+      ScheduleTask(true);
+      return;
+    }
+
     if (status == TileGroupStatus::kNoTiles && !is_suspend_) {
       ResetBackoff();
       ScheduleTask(true);
@@ -69,9 +82,7 @@ class TileServiceSchedulerImpl : public TileServiceScheduler {
 
   void ScheduleTask(bool is_init_schedule) {
     background_task::OneOffInfo one_off_task_info;
-    bool instant_fetch_task = base::CommandLine::ForCurrentProcess()->HasSwitch(
-        switches::kQueryTilesInstantBackgroundTask);
-    if (instant_fetch_task) {
+    if (IsInstantFetchMode()) {
       GetInstantTaskWindow(&one_off_task_info.window_start_time_ms,
                            &one_off_task_info.window_end_time_ms,
                            is_init_schedule);
@@ -120,22 +131,20 @@ class TileServiceSchedulerImpl : public TileServiceScheduler {
   void GetInstantTaskWindow(int64_t* start_time_ms,
                             int64_t* end_time_ms,
                             bool is_init_schedule) {
-    auto now = clock_->Now().ToDeltaSinceWindowsEpoch().InMilliseconds();
     if (is_init_schedule) {
-      *start_time_ms = now + 10 * 1000;
+      *start_time_ms = kInstantScheduleWindowStartMs;
     } else {
       *start_time_ms = GetDelaysFromBackoff();
     }
-    *end_time_ms = now + 20 * 1000;
+    *end_time_ms = kInstantScheduleWindowEndMs;
   }
 
   void GetTaskWindow(int64_t* start_time_ms,
                      int64_t* end_time_ms,
                      bool is_init_schedule) {
-    auto now = clock_->Now().ToDeltaSinceWindowsEpoch().InMilliseconds();
     if (is_init_schedule) {
       *start_time_ms =
-          now + TileConfig::GetScheduleIntervalInMs() +
+          TileConfig::GetScheduleIntervalInMs() +
           base::RandGenerator(TileConfig::GetMaxRandomWindowInMs());
     } else {
       *start_time_ms = GetDelaysFromBackoff();
@@ -161,6 +170,11 @@ class TileServiceSchedulerImpl : public TileServiceScheduler {
     std::unique_ptr<base::Value> value =
         net::BackoffEntrySerializer::SerializeToValue(*backoff, clock_->Now());
     prefs_->Set(kBackoffEntryKey, *value);
+  }
+
+  bool IsInstantFetchMode() const {
+    return base::CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kQueryTilesInstantBackgroundTask);
   }
 
   // Native Background Scheduler instance.
