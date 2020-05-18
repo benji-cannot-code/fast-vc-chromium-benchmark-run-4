b3c987ade356e1a5f64c2e2bbfc6a64f5568a3bf
My Nguyen <myy@chromium.org>
2020-05-18 23:51:37 +0000

    Add EmojiSuggestAdditionEnabled Pref
    As an enterprise policy is required for emoji suggest addition, adding
    preference's value first.
    
    Bug: 1077629
    Change-Id: I4a55ddc861c4c2216742c92cd90bd0c892ae0bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175762
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769906}

--- chrome/browser/chromeos/input_method/assistive_suggester.h
+++ chrome/browser/chromeos/input_method/assistive_suggester.h
@@ -17,14 +17,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-// Check if any assistive feature is enabled.
-bool IsAssistiveFeatureEnabled();
+const char kEmojiSuggestAdditionEnabledPrefName[] =
+    "emoji_suggest_addition_enabled";
 
 // An agent to suggest assistive information when the user types, and adopt or
 // dismiss the suggestion according to the user action.
 class AssistiveSuggester {
  public:
   AssistiveSuggester(InputMethodEngine* engine, Profile* profile);
+
+  bool IsAssistiveFeatureEnabled();
 
   // Called when a text field gains focus, and suggester starts working.
   void OnFocus(int context_id);
@@ -60,6 +62,9 @@ class AssistiveSuggester {
   // Check if suggestion is being shown.
   bool IsSuggestionShown();
 
+  bool IsEmojiSuggestAdditionEnabled();
+
+  Profile* profile_;
   PersonalInfoSuggester personal_info_suggester_;
   EmojiSuggester emoji_suggester_;
 
--- chrome/browser/chromeos/input_method/native_input_method_engine.cc
+++ chrome/browser/chromeos/input_method/native_input_method_engine.cc
@@ -138,14 +138,14 @@ void NativeInputMethodEngine::ImeObserver::OnActivate(
 
 void NativeInputMethodEngine::ImeObserver::OnFocus(
     const IMEEngineHandlerInterface::InputContext& context) {
-  if (IsAssistiveFeatureEnabled())
+  if (assistive_suggester_->IsAssistiveFeatureEnabled())
     assistive_suggester_->OnFocus(context.id);
 
   base_observer_->OnFocus(context);
 }
 
 void NativeInputMethodEngine::ImeObserver::OnBlur(int context_id) {
-  if (IsAssistiveFeatureEnabled())
+  if (assistive_suggester_->IsAssistiveFeatureEnabled())
     assistive_suggester_->OnBlur();
 
   base_observer_->OnBlur(context_id);
@@ -155,7 +155,7 @@ void NativeInputMethodEngine::ImeObserver::OnKeyEvent(
     const std::string& engine_id,
     const InputMethodEngineBase::KeyboardEvent& event,
     ui::IMEEngineHandlerInterface::KeyEventDoneCallback callback) {
-  if (IsAssistiveFeatureEnabled()) {
+  if (assistive_suggester_->IsAssistiveFeatureEnabled()) {
     if (assistive_suggester_->OnKeyEvent(event)) {
       std::move(callback).Run(true);
       return;
@@ -202,7 +202,7 @@ void NativeInputMethodEngine::ImeObserver::OnSurroundingTextChanged(
     int offset_pos) {
   assistive_suggester_->RecordAssistiveCoverageMetrics(text, cursor_pos,
                                                        anchor_pos);
-  if (IsAssistiveFeatureEnabled()) {
+  if (assistive_suggester_->IsAssistiveFeatureEnabled()) {
     // If |assistive_suggester_| changes the surrounding text, no longer need
     // to call the following function, as the information is out-dated.
     if (assistive_suggester_->OnSurroundingTextChanged(text, cursor_pos,
