25dde715b464920240d480fb7731e8052502414a
Jeffrey Cohen <jeffreycohen@google.com>
2020-05-18 18:15:40 +0000

    [ShareButtonInToolbar] Fix for crashing bug
    This change adds a guard to prevent the Share Button from causing a
    crash on Tablets.
    
    Bug: 1083851
    Change-Id: Ic110e9821d43c0ccbab04dcb1ae05ae9ed5d50ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207552
    Commit-Queue: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769763}

--- chrome/android/java/src/org/chromium/chrome/browser/share/ShareButtonController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/share/ShareButtonController.java
@@ -83,8 +83,10 @@ public class ShareButtonController implements ButtonDataProvider {
             assert shareDelegate
                     != null : "Share delegate became null after share button was displayed";
             if (shareDelegate == null) return;
-            RecordUserAction.record("MobileTopToolbarShareButton");
             Tab tab = mTabProvider.get();
+            assert tab != null : "Tab became null after share button was displayed";
+            if (tab == null) return;
+            RecordUserAction.record("MobileTopToolbarShareButton");
             shareDelegate.share(tab, /*shareDirectly=*/false);
         });
 
@@ -136,7 +138,8 @@ public class ShareButtonController implements ButtonDataProvider {
     }
 
     private void updateButtonVisibility(Tab tab) {
-        if (tab == null || tab.getWebContents() == null
+        if (tab == null || tab.getWebContents() == null || mTabProvider == null
+                || mTabProvider.get() == null
                 || !ChromeFeatureList.isEnabled(ChromeFeatureList.SHARE_BUTTON_IN_TOP_TOOLBAR)) {
             mButtonData.canShow = false;
             return;
