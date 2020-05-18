9581f9072dfd8d0ad648a637ca37930c95c2693c
Kong, Fanchen <fanchen.kong@intel.com>
2020-05-18 09:21:32 +0000

    Seccomp Opt-out SSBD
    This patch is to opt-out the mitigation of Spectre Variant 4 (Speculative Store Bypass Disable, SSBD) on Linux and ChromeOS platform.
    
    Chrome uses Seccomp Filter Sandbox as a Layer-2 sandbox on Linux related platforms. The Seccomp will automatically enable the mitigation of Spectre Variant 4 which is not necessary for platforms where site isolation is applied.
    
    Bug: chromium:1029470
    Change-Id: Ib8515922a4ca3df97094360a20f908fbff175bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186360
    Commit-Queue: Fanchen Kong <fanchen.kong@intel.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769643}

--- sandbox/linux/system_headers/linux_seccomp.h
+++ sandbox/linux/system_headers/linux_seccomp.h
@@ -68,6 +68,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IPC_64
 #define IPC_64                   0x0100
 #endif
+#ifndef PR_SET_SPECULATION_CTRL
+#define PR_SET_SPECULATION_CTRL 53
+#define PR_GET_SPECULATION_CTRL 52
+#endif
+#ifndef PR_SPEC_INDIRECT_BRANCH
+#define PR_SPEC_INDIRECT_BRANCH 1
+#endif
+#ifndef PR_SPEC_PRCTL
+#define PR_SPEC_PRCTL (1UL << 0)
+#endif
+#ifndef PR_SPEC_FORCE_DISABLE
+#define PR_SPEC_FORCE_DISABLE (1UL << 3)
+#endif
 
 // In order to build will older tool chains, we currently have to avoid
 // including <linux/seccomp.h>. Until that can be fixed (if ever). Rely on
@@ -86,6 +99,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #ifndef SECCOMP_FILTER_FLAG_TSYNC
 #define SECCOMP_FILTER_FLAG_TSYNC 1
+#endif
+#ifndef SECCOMP_FILTER_FLAG_SPEC_ALLOW
+#define SECCOMP_FILTER_FLAG_SPEC_ALLOW (1UL << 2)
 #endif
 
 #ifndef SECCOMP_RET_KILL
