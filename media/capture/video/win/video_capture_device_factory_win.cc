c57bc791bcf7103990a43b51a0dc35df0d4520c7
Reilly Grant <reillyg@chromium.org>
2020-05-18 10:29:16 +0000

    Boost thread priority to load Media Foundation DLLs
    While investigating crash/0befa4916cf9467e I noticed that
    LoadMediaFoundationDlls() was holding the loader lock on a background
    thread while the main thread was trying to call GetProcAddress() and
    another thread was flagged as hung because of how long it had been
    waiting.
    
    This macro temporarily boosts the priority of the thread while loading
    these DLLs to avoid this kind of jank.
    
    Bug: 973868
    Change-Id: Ida0a2e96b9b2b0a63dcfb93088f31dbf27eee52c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204817
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769656}

--- media/capture/video/win/video_capture_device_factory_win.cc
+++ media/capture/video/win/video_capture_device_factory_win.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
+#include "base/threading/scoped_thread_priority.h"
 #include "base/win/core_winrt_util.h"
 #include "base/win/scoped_co_mem.h"
 #include "base/win/scoped_variant.h"
@@ -150,6 +151,10 @@ bool LoadMediaFoundationDlls() {
       L"%WINDIR%\\system32\\mf.dll", L"%WINDIR%\\system32\\mfplat.dll",
       L"%WINDIR%\\system32\\mfreadwrite.dll",
       L"%WINDIR%\\system32\\MFCaptureEngine.dll"};
+
+  // Mitigate the issues caused by loading DLLs on a background thread
+  // (http://crbug/973868).
+  SCOPED_MAY_LOAD_LIBRARY_AT_BACKGROUND_PRIORITY_REPEATEDLY();
 
   for (const wchar_t* kMfDLL : kMfDLLs) {
     wchar_t path[MAX_PATH] = {0};
