36a22e0e0c119ee00fbe86cc80d8b50e4ee80ade
Ramin Halavati <rhalavati@chromium.org>
2020-05-18 05:26:13 +0000

    Rename VariationsClient::IsIncognito to IsOffTheRecord.
    VariationsClient::IsIncognito returns true for all off-the-record
    profiles and is always initialized with BrowserContext::IsOffTheRecord.
    To remove misinterpretations, the function is renamed to IsOffTheRecord.
    
    This CL does not create any behavioral change.
    
    Bug: 968028, 1074201
    Change-Id: I19a0a81794e7838c8f882faa6aeab9d7b085f756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202980
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769614}

--- content/browser/field_trial_synchronizer.cc
+++ content/browser/field_trial_synchronizer.cc
@@ -111,7 +111,7 @@ void FieldTrialSynchronizer::UpdateRendererVariationsHeader(
       host->GetBrowserContext()->GetVariationsClient();
 
   // |client| might be null in tests.
-  if (!client || client->IsIncognito())
+  if (!client || client->IsOffTheRecord())
     return;
 
   mojo::AssociatedRemote<mojom::RendererVariationsConfiguration>
--- weblayer/browser/browser_context_impl.cc
+++ weblayer/browser/browser_context_impl.cc
@@ -262,7 +262,7 @@ class BrowserContextImpl::WebLayerVariationsClient
 
   ~WebLayerVariationsClient() override = default;
 
-  bool IsIncognito() const override {
+  bool IsOffTheRecord() const override {
     return browser_context_->IsOffTheRecord();
   }
 
