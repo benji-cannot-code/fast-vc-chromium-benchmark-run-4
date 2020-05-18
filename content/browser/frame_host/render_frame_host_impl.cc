f9a8dded0b1a5257a81056dbc017d48d3195cf66
Sreeja Kamishetty <sreejakshetty@chromium.org>
2020-05-18 11:29:06 +0000

    [content] Fix LeaveInPendingDeletionState for sub-frames
    Currently, LeaveInPendingDeletionState doesn't work for leaving
    the RenderFrameHost which are sub-frames in pending deletion state.
    
    This CL proposes a fix to this case.
    
    Bug: 1067674
    Change-Id: I86a7d740aae6db6d5e8534ace0cf996e7ddc0919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2144121
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769667}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -2571,6 +2571,11 @@ void RenderFrameHostImpl::OnDetach() {
     return;
   }
 
+  // Ignore FrameHostMsg_Detach IPC message, if the RenderFrameHost should be
+  // left in pending deletion state.
+  if (do_not_delete_for_testing_)
+    return;
+
   if (IsPendingDeletion()) {
     // The frame is pending deletion. FrameHostMsg_Detach is used to confirm
     // its unload handlers ran. Note that it is possible for a frame to already
@@ -3548,6 +3553,10 @@ bool RenderFrameHostImpl::IsBeforeUnloadHangMonitorDisabledForTesting() {
 
 void RenderFrameHostImpl::DoNotDeleteForTesting() {
   do_not_delete_for_testing_ = true;
+}
+
+void RenderFrameHostImpl::ResumeDeletionForTesting() {
+  do_not_delete_for_testing_ = false;
 }
 
 bool RenderFrameHostImpl::IsFeatureEnabled(
