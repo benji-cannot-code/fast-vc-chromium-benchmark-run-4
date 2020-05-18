40e54f154f66d7f7ceac1bc73942c208730afda3
Ramin Halavati <rhalavati@chromium.org>
2020-05-18 12:06:10 +0000

    Update TestingProfile to support non-primary OTRs.
    TestingProfile class is updated to support multiple off-the-record
    profiles.
    
    Bug: 1033903
    Change-Id: I23dbb8abbf6008ed0bde484446515ffa36809431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2193652
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769673}

--- chrome/test/base/testing_profile.h
+++ chrome/test/base/testing_profile.h
@@ -154,12 +154,12 @@ class TestingProfile : public Profile {
     // Creates the TestingProfile using previously-set settings.
     std::unique_ptr<TestingProfile> Build();
 
-    // Build an incognito profile, owned by |original_profile|. Note: unless you
-    // need to customize the Builder, or access TestingProfile member functions,
-    // you can use original_profile->GetOffTheRecordProfile().
-    //
-    // TODO(https://crbug.com/1033903): Add BuildOffTheRecord to add possibility
-    // of creating non-primary OTRs.
+    // Build an OffTheRecord profile, owned by |original_profile|. Note: unless
+    // you need to customize the Builder, or access TestingProfile member
+    // functions, you can use original_profile->GetOffTheRecordProfile().
+    TestingProfile* BuildOffTheRecord(TestingProfile* original_profile,
+                                      const OTRProfileID& otr_profile_id);
+
     TestingProfile* BuildIncognito(TestingProfile* original_profile);
 
    private:
@@ -216,7 +216,8 @@ class TestingProfile : public Profile {
                  std::unique_ptr<policy::PolicyService> policy_service,
                  TestingFactories testing_factories,
                  const std::string& profile_name,
-                 base::Optional<bool> override_policy_connector_is_managed);
+                 base::Optional<bool> override_policy_connector_is_managed,
+                 base::Optional<OTRProfileID> otr_profile_id);
 
   ~TestingProfile() override;
 
@@ -271,6 +272,7 @@ class TestingProfile : public Profile {
   // Called on the parent of an OffTheRecord |otr_profile|. Usually called from
   // the constructor of an OffTheRecord TestingProfile, but can also be used by
   // tests to provide an OffTheRecordProfileImpl instance.
+  // |otr_profile| cannot be empty.
   void SetOffTheRecordProfile(std::unique_ptr<Profile> otr_profile);
 
   void SetSupervisedUserId(const std::string& id);
@@ -449,7 +451,7 @@ class TestingProfile : public Profile {
   std::unique_ptr<net::CookieStore, content::BrowserThread::DeleteOnIOThread>
       extensions_cookie_store_;
 
-  std::unique_ptr<Profile> incognito_profile_;
+  std::map<OTRProfileID, std::unique_ptr<Profile>> otr_profiles_;
   TestingProfile* original_profile_;
 
   bool guest_session_;
@@ -498,6 +500,7 @@ class TestingProfile : public Profile {
   std::string profile_name_;
 
   base::Optional<bool> override_policy_connector_is_managed_;
+  base::Optional<OTRProfileID> otr_profile_id_;
 
 #if defined(OS_CHROMEOS)
   std::unique_ptr<chromeos::ScopedCrosSettingsTestHelper>
