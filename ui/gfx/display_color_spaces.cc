917f9a0707cbe0d2499c004e465f5942afdc39b5
Christopher Cameron <ccameron@chromium.org>
2020-05-18 19:24:12 +0000

    Use ifdef for gfx::DisplayColorSpaces gfx::BufferFormat
    ChromeOS uses default-initialized display::Displays, with a therefore
    default-initialized gfx::DisplayColorSpaces in several places. This is
    resulting in a trickle of bugs where the default gfx::BufferFormat of
    RGBA_8888 is being passed to something that expects BGRA_8888.
    
    Change the gfx::DisplayColorSpaces default constructor to have ifdef
    based on OS that will select BGRA_8888 on ChromeOS.
    
    Bug: 1073237
    Change-Id: I422153a933d3b8958675a06c3e562f88a9b09eda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2205976
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769791}

--- ui/gfx/display_color_spaces.cc
+++ ui/gfx/display_color_spaces.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/display_color_spaces.h"
 
+#include "build/build_config.h"
+
 namespace gfx {
 
 namespace {
@@ -14,6 +16,16 @@ const ContentColorUsage kAllColorUsages[] = {
     ContentColorUsage::kWideColorGamut,
     ContentColorUsage::kHDR,
 };
+
+gfx::BufferFormat DefaultBufferFormat() {
+  // ChromeOS expects the default buffer format be BGRA_8888 in several places.
+  // https://crbug.com/1057501, https://crbug.com/1073237
+#if defined(OS_CHROMEOS)
+  return gfx::BufferFormat::BGRA_8888;
+#else
+  return gfx::BufferFormat::RGBA_8888;
+#endif
+}
 
 size_t GetIndex(ContentColorUsage color_usage, bool needs_alpha) {
   switch (color_usage) {
@@ -32,7 +44,7 @@ DisplayColorSpaces::DisplayColorSpaces() {
   for (auto& color_space : color_spaces_)
     color_space = gfx::ColorSpace::CreateSRGB();
   for (auto& buffer_format : buffer_formats_)
-    buffer_format = gfx::BufferFormat::RGBA_8888;
+    buffer_format = DefaultBufferFormat();
 }
 
 DisplayColorSpaces::DisplayColorSpaces(const gfx::ColorSpace& c)
