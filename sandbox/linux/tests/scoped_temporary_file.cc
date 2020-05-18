f44abd3c97b072dea8126498d28770f384d7a37b
Piotr Tworek <ptworek@vewd.com>
2020-05-18 12:49:48 +0000

    Include string.h in sandbox/linux/tests/scoped_temporary_file.cc.
    The code in this file uses memcpy function but does not include the
    header file declaring it. This leads to build failures on Linux systems
    using recent glibc versions.
    
    Bug: 1074286
    Change-Id: I1ebc63729145a341d25e8299337d15b7696571da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2206797
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Piotr Tworek <ptworek@vewd.com>
    Cr-Commit-Position: refs/heads/master@{#769680}

--- sandbox/linux/tests/scoped_temporary_file.cc
+++ sandbox/linux/tests/scoped_temporary_file.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/linux/tests/scoped_temporary_file.h"
 
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "base/check_op.h"
