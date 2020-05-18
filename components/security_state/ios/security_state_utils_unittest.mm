87d2e93a787d95c8c6a6ae6a7ccf4f130e648a3d
Kurt Horimoto <kkhorimoto@chromium.org>
2020-05-18 20:31:50 +0000

    [iOS] Account for safe browsing errors in security state icon.
    When UnsafeResources are added to the allow list as pending or allowed
    decisions, the WebState's visible security state is updated.  This CL
    adds GetMaliciousContentStatus() to security_state_utils to calculate
    the malicious content state based on Safe Browsing unsafe resource
    state.  This malicious content status is added to the
    VisibleSecurityState returned by GetVisibleSecurityStateForWebState(),
    which is used to compute the correct icon to show in the omnibox when
    malicious content is loaded.
    
    Since the security_state_utils functions take const WebStates,
    AreUnsafeNavigationsAllowed() and IsUnsafeNavigationDecisionPending()
    are updated to be const functions.
    
    The LocationBarModelImpl was also updated to remove an ifdef for iOS
    because billing safe browsing error pages are now supported.
    
    To prevent GN deps cycles introduced by the security_state/ =>
    safe_browsing/ dependency, the allow list was moved to a separate
    source set.
    
    Finally, the SafeBrowsingBlockingPage was updated to remove pending
    unsafe resource decisions upon its destruction to match the
    functionality of other platforms.
    
    Bug: 1081926
    Change-Id: I773cbee072a2d75d0f965a048edb933f2383a065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2200085
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769813}

--- components/security_state/ios/security_state_utils_unittest.mm
+++ components/security_state/ios/security_state_utils_unittest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/security_state/core/security_state.h"
 #import "components/security_state/ios/insecure_input_tab_helper.h"
+#import "ios/chrome/browser/safe_browsing/safe_browsing_url_allow_list.h"
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/navigation/navigation_manager.h"
 #include "ios/web/public/security/ssl_status.h"
@@ -23,11 +24,13 @@ class SecurityStateUtilsTest : public web::WebTestWithWebState {
  protected:
   void SetUp() override {
     web::WebTestWithWebState::SetUp();
+    SafeBrowsingUrlAllowList::CreateForWebState(web_state());
     InsecureInputTabHelper::CreateForWebState(web_state());
     insecure_input_ = InsecureInputTabHelper::FromWebState(web_state());
     ASSERT_TRUE(insecure_input_);
 
-    LoadHtml(@"<html><body></body></html>", GURL("http://chromium.test"));
+    url_ = GURL("http://chromium.test");
+    LoadHtml(@"<html><body></body></html>", url_);
   }
 
   // Returns the InsecureInputEventData for current WebState().
@@ -38,8 +41,8 @@ class SecurityStateUtilsTest : public web::WebTestWithWebState {
 
   InsecureInputTabHelper* insecure_input() { return insecure_input_; }
 
- private:
   InsecureInputTabHelper* insecure_input_;
+  GURL url_;
 };
 
 // Ensures that |insecure_field_edited| is set only when an editing event has
@@ -69,4 +72,53 @@ TEST_F(SecurityStateUtilsTest, InsecureInputClearedOnRenavigation) {
   LoadHtml(@"<html><body></body></html>", GURL("http://chromium.test"));
   events = GetInsecureInputEventData();
   EXPECT_FALSE(events.insecure_field_edited);
+}
+
+// Verifies GetMaliciousContentStatus() return values.
+TEST_F(SecurityStateUtilsTest, GetMaliciousContentStatus) {
+  SafeBrowsingUrlAllowList* allow_list =
+      SafeBrowsingUrlAllowList::FromWebState(web_state());
+  std::map<security_state::MaliciousContentStatus,
+           std::vector<safe_browsing::SBThreatType>>
+      threats_types_for_content_statuses = {
+          {security_state::MALICIOUS_CONTENT_STATUS_SOCIAL_ENGINEERING,
+           {safe_browsing::SB_THREAT_TYPE_UNUSED,
+            safe_browsing::SB_THREAT_TYPE_SAFE,
+            safe_browsing::SB_THREAT_TYPE_URL_PHISHING,
+            safe_browsing::SB_THREAT_TYPE_URL_CLIENT_SIDE_PHISHING}},
+          {security_state::MALICIOUS_CONTENT_STATUS_MALWARE,
+           {safe_browsing::SB_THREAT_TYPE_URL_MALWARE,
+            safe_browsing::SB_THREAT_TYPE_URL_CLIENT_SIDE_MALWARE}},
+          {security_state::MALICIOUS_CONTENT_STATUS_UNWANTED_SOFTWARE,
+           {safe_browsing::SB_THREAT_TYPE_URL_UNWANTED}},
+          {security_state::MALICIOUS_CONTENT_STATUS_BILLING,
+           {safe_browsing::SB_THREAT_TYPE_SAVED_PASSWORD_REUSE,
+            safe_browsing::SB_THREAT_TYPE_SIGNED_IN_SYNC_PASSWORD_REUSE,
+            safe_browsing::SB_THREAT_TYPE_SIGNED_IN_NON_SYNC_PASSWORD_REUSE,
+            safe_browsing::SB_THREAT_TYPE_ENTERPRISE_PASSWORD_REUSE,
+            safe_browsing::SB_THREAT_TYPE_BILLING}}};
+
+  for (auto& pair : threats_types_for_content_statuses) {
+    security_state::MaliciousContentStatus status = pair.first;
+    for (auto& threat : pair.second) {
+      allow_list->RemovePendingUnsafeNavigationDecisions(url_);
+      allow_list->AddPendingUnsafeNavigationDecision(url_, threat);
+      EXPECT_EQ(status, security_state::GetMaliciousContentStatus(web_state()))
+          << "Unexpected MaliciousContentStatus for SBThreatType: " << threat;
+    }
+  }
+}
+
+// Verifies GetSecurityLevelForWebState() for malicious content.
+TEST_F(SecurityStateUtilsTest, GetSecurityLevelForWebStateMaliciousContent) {
+  // The test fixture loads an http page, so the initial state is WARNING.
+  ASSERT_EQ(security_state::WARNING,
+            security_state::GetSecurityLevelForWebState(web_state()));
+
+  SafeBrowsingUrlAllowList* allow_list =
+      SafeBrowsingUrlAllowList::FromWebState(web_state());
+  allow_list->AddPendingUnsafeNavigationDecision(
+      url_, safe_browsing::SB_THREAT_TYPE_URL_MALWARE);
+  EXPECT_EQ(security_state::DANGEROUS,
+            security_state::GetSecurityLevelForWebState(web_state()));
 }
