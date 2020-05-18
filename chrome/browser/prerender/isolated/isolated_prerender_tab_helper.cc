36c38285341270b77acaa7415f5d4c8638047dd5
Robert Ogden <robertogden@chromium.org>
2020-05-18 21:29:56 +0000

    Add Accept-Language header to IsolatedPrerender requests
    This header is copied from prefs, per the design doc.
    
    Bug: 1084186
    Change-Id: Id99652bbf573b6aa6ede9230a7d6da5c23904acf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207752
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769841}

--- chrome/browser/prerender/isolated/isolated_prerender_tab_helper.cc
+++ chrome/browser/prerender/isolated/isolated_prerender_tab_helper.cc
@@ -28,7 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.h"
 #include "components/google/core/common/google_util.h"
+#include "components/language/core/browser/pref_names.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
+#include "components/prefs/pref_service.h"
 #include "components/search_engines/template_url_service.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/navigation_handle.h"
@@ -42,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/cookies/cookie_store.h"
 #include "net/http/http_status_code.h"
+#include "net/http/http_util.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/resource_request.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -788,6 +791,8 @@ void IsolatedPrerenderTabHelper::CreateIsolatedURLLoaderFactory() {
 
   auto context_params = network::mojom::NetworkContextParams::New();
   context_params->user_agent = ::GetUserAgent();
+  context_params->accept_language = net::HttpUtil::GenerateAcceptLanguageHeader(
+      profile_->GetPrefs()->GetString(language::prefs::kAcceptLanguages));
   context_params->initial_custom_proxy_config =
       isolated_prerender_service->proxy_configurator()
           ->CreateCustomProxyConfig();
