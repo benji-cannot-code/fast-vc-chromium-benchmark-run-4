158fef531cfe66f51e114bf47230a04313f06695
Nohemi Fernandez <fernandex@chromium.org>
2020-05-18 15:57:49 +0000

    [iOS] Update ChromeIdentityInteractionManager API.
    Following the update of internal account management APIs, this patch
    updates the relevant code necessary to use the new standard for adding
    an account to Chrome.
    
    Bug: 1078815
    Change-Id: I486b6561d9e291608bdb12ab8642351168769ef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2206814
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769721}

--- ios/public/provider/chrome/browser/signin/chrome_identity_interaction_manager.mm
+++ ios/public/provider/chrome/browser/signin/chrome_identity_interaction_manager.mm
@@ -22,17 +22,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)addAccountWithCompletion:(SigninCompletionCallback)completion {
   NOTREACHED() << "Subclasses must override this";
-  completion(nil, nil);
 }
 
 - (void)reauthenticateUserWithID:(NSString*)userID
                            email:(NSString*)userEmail
                       completion:(SigninCompletionCallback)completion {
   NOTREACHED() << "Subclasses must override this";
-  completion(nil, nil);
 }
 
 - (void)cancelAndDismissAnimated:(BOOL)animated {
+  NOTREACHED() << "Subclasses must override this";
+}
+
+- (void)addAccountWithPresentingViewController:(UIViewController*)viewController
+                                    completion:
+                                        (SigninCompletionCallback)completion {
+  NOTREACHED() << "Subclasses must override this";
+}
+
+- (void)addAccountWithPresentingViewController:(UIViewController*)viewController
+                                     userEmail:(NSString*)userEmail
+                                    completion:
+                                        (SigninCompletionCallback)completion {
+  NOTREACHED() << "Subclasses must override this";
+}
+
+- (void)cancelAddAccountWithAnimation:(BOOL)animated {
   NOTREACHED() << "Subclasses must override this";
 }
 
