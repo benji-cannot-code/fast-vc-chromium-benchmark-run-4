c150a51684835870e54b0525b3c684e380fd50dc
Jerry Lin <wahahab11@gmail.com>
2020-05-18 08:14:27 +0000

    Replace usage of ResourceType with RequestDestination
    Add new service worker util function 'IsMainRequestDestination' to
    check request type and replace usage of IsMainResourceType with it in
    ServiceWorkerNavigationLoader::StartRequest.
    
    Use destination of ResourceRequest to check corresponding request type
    is kScript instead of using resource_type in
    ServiceWorkerSubresourceLoader::StartResponse.
    
    Bug: 1062579
    Change-Id: I90e44ad7f1e06dfe65fb03e4cbba83dfbd0bbf8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2108444
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Jerry Lin <wahahab11@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#769636}

--- content/renderer/service_worker/service_worker_subresource_loader_unittest.cc
+++ content/renderer/service_worker/service_worker_subresource_loader_unittest.cc
@@ -553,8 +553,7 @@ class ServiceWorkerSubresourceLoaderTest : public ::testing::Test {
     network::ResourceRequest request;
     request.url = url;
     request.method = "GET";
-    request.resource_type =
-        static_cast<int>(blink::mojom::ResourceType::kSubResource);
+    request.destination = network::mojom::RequestDestination::kEmpty;
     return request;
   }
 
@@ -602,8 +601,7 @@ class ServiceWorkerSubresourceLoaderTest : public ::testing::Test {
         CreateSubresourceLoaderFactory();
     network::ResourceRequest request =
         CreateRequest(GURL("https://www.example.com/big-file"));
-    request.resource_type =
-        static_cast<int>(blink::mojom::ResourceType::kMedia);
+    request.destination = network::mojom::RequestDestination::kVideo;
     request.headers.SetHeader("Range", range_header);
     mojo::Remote<network::mojom::URLLoader> loader;
     std::unique_ptr<network::TestURLLoaderClient> client;
@@ -1027,7 +1025,7 @@ TEST_F(ServiceWorkerSubresourceLoaderTest, BlobResponse) {
   // Perform the request.
   network::ResourceRequest request =
       CreateRequest(GURL("https://www.example.com/foo.js"));
-  request.resource_type = static_cast<int>(blink::mojom::ResourceType::kScript);
+  request.destination = network::mojom::RequestDestination::kScript;
   mojo::Remote<network::mojom::URLLoader> loader;
   std::unique_ptr<network::TestURLLoaderClient> client;
   StartRequest(factory, request, &loader, &client);
@@ -1084,7 +1082,7 @@ TEST_F(ServiceWorkerSubresourceLoaderTest, BlobResponseWithoutMetadata) {
   // Perform the request.
   network::ResourceRequest request =
       CreateRequest(GURL("https://www.example.com/foo.js"));
-  request.resource_type = static_cast<int>(blink::mojom::ResourceType::kScript);
+  request.destination = network::mojom::RequestDestination::kScript;
   mojo::Remote<network::mojom::URLLoader> loader;
   std::unique_ptr<network::TestURLLoaderClient> client;
   StartRequest(factory, request, &loader, &client);
@@ -1131,8 +1129,7 @@ TEST_F(ServiceWorkerSubresourceLoaderTest, BlobResponseNonScript) {
   // Perform the request.
   network::ResourceRequest request =
       CreateRequest(GURL("https://www.example.com/foo.txt"));
-  request.resource_type =
-      static_cast<int>(blink::mojom::ResourceType::kSubResource);
+  request.destination = network::mojom::RequestDestination::kEmpty;
   mojo::Remote<network::mojom::URLLoader> loader;
   std::unique_ptr<network::TestURLLoaderClient> client;
   StartRequest(factory, request, &loader, &client);
