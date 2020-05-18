a241d3c624a7da73382aa4ffb2536ece4a6bbfca
Glen Robertson <glenrob@chromium.org>
2020-05-18 04:34:34 +0000

    Revert "Reland "Implement capture of metrics events for web app daily"
    This reverts commit 91cc0984da8b798caaf5ba0cb6e9dc1845fc916d.
    
    Reason for revert: Other tests still flaking on mac
    eg. https://ci.chromium.org/p/chromium/builders/ci/Mac10.11%20Tests/50389
    
    Original change's description:
    > Reland "Implement capture of metrics events for web app daily
    > interaction."
    > 
    > Disabled 3 tests on Mac as they are failing/flaky.
    > 
    > Original CL: crrev.com/c/2160638
    > Revert CL: crrev.com/c/2203240
    > 
    > Original change's description:
    > > Implement capture of metrics events for web app daily interaction.
    > >
    > > Captures basic static information on all web apps used and more detailed
    > > sums of foreground/background duration and session count for installed
    > > web apps that are set to open in a window.
    > >
    > > WebAppMetrics now listens on BrowserList events and TabStripModel
    > > changes in order to determine when web apps move into the foreground or
    > > background. It keeps a record of the last time each web app was seen so
    > > that intervening time can be counted as foreground time if switching
    > > away from the web app (and background time if switching to the web app).
    > >
    > > Tbr: achuith@chromium.org
    > > Bug: 1026148
    > > Bug: 1081187
    > > Change-Id: Iae6460c678b59380cf0fad8408e2014918bc2864
    > > Fixed: 1026148
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2160638
    > > Commit-Queue: Glen Robertson <glenrob@chromium.org>
    > > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > > Reviewed-by: Alan Cutter <alancutter@chromium.org>
    > > Auto-Submit: Glen Robertson <glenrob@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#769125}
    > 
    > TBR=achuith@chromium.org,dominickn@chromium.org
    > 
    > Fixed: 1026148
    > Bug: 1026148
    > Bug: 1081187
    > Bug: 1083813
    > Change-Id: I38d92958255c4db055ddfa3b731de9309dd7219c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2206060
    > Commit-Queue: Glen Robertson <glenrob@chromium.org>
    > Reviewed-by: Glen Robertson <glenrob@chromium.org>
    > Reviewed-by: Alan Cutter <alancutter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#769600}
    
    TBR=achuith@chromium.org,alancutter@chromium.org,dominickn@chromium.org,glenrob@chromium.org
    
    Change-Id: I7d5ca8e984f305f0a56d0f6f49941e26beb816f7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1026148, 1081187, 1083813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2206294
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769608}

--- chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
+++ chrome/browser/ui/views/page_action/pwa_install_view_browsertest.cc
@@ -282,8 +282,7 @@ IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest,
 
   EXPECT_EQ(
       result.app_banner_manager->GetInstallableWebAppCheckResultForTesting(),
-      banners::AppBannerManager::InstallableWebAppCheckResult::
-          kNoAlreadyInstalled);
+      banners::AppBannerManager::InstallableWebAppCheckResult::kNo);
   EXPECT_FALSE(pwa_install_view_->GetVisible());
 }
 
@@ -306,8 +305,7 @@ IN_PROC_BROWSER_TEST_P(PwaInstallViewBrowserTest,
   // The nested PWA should now not be installable.
   EXPECT_EQ(
       result.app_banner_manager->GetInstallableWebAppCheckResultForTesting(),
-      banners::AppBannerManager::InstallableWebAppCheckResult::
-          kNoAlreadyInstalled);
+      banners::AppBannerManager::InstallableWebAppCheckResult::kNo);
   EXPECT_FALSE(pwa_install_view_->GetVisible());
 }
 
