7a9e693a181dbf1eff5f23a7aab63afcf252c46f
Lily Chen <chlily@chromium.org>
2020-05-18 16:30:38 +0000

    Remove force_ignore_site_for_cookies use from Wilco Dtc Supportd test
    The parameter is unnecessary.
    
    Bug: 1068792
    Change-Id: I55a31de56fd3f4440b2d4a863a76181835fa9678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2165287
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769730}

--- chrome/browser/chromeos/wilco_dtc_supportd/wilco_dtc_supportd_web_request_service_browsertest.cc
+++ chrome/browser/chromeos/wilco_dtc_supportd/wilco_dtc_supportd_web_request_service_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/simple_url_loader_test_helper.h"
+#include "net/cookies/site_for_cookies.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
@@ -127,9 +128,8 @@ class ContextRequestPerformer {
 
     auto request = std::make_unique<network::ResourceRequest>();
     request->url = url;
-    // TODO(crbug.com/993801): This probably isn't needed here and can be
-    // removed if the test sets an appropriate site_for_cookies instead.
-    request->force_ignore_site_for_cookies = true;
+    // Allow access to SameSite cookies.
+    request->site_for_cookies = net::SiteForCookies::FromUrl(url);
 
     auto url_loader = network::SimpleURLLoader::Create(
         std::move(request), TRAFFIC_ANNOTATION_FOR_TESTS);
@@ -377,7 +377,7 @@ IN_PROC_BROWSER_TEST_F(WilcoDtcSupportdNetworkContextTest,
 }
 
 // Verifies that WilcoDtcSupportdWebRequestService
-//     1) niether saves nor sends cookies;
+//     1) neither saves nor sends cookies;
 //     2) uses neither profile nor system network context cookies.
 IN_PROC_BROWSER_TEST_F(WilcoDtcSupportdNetworkContextTest, NoCookies) {
   ASSERT_NO_FATAL_FAILURE(StartServer());
