41c5b7489be08dc13f0ba0afd44dacadf1a899a4
Ramin Halavati <rhalavati@chromium.org>
2020-05-18 05:27:53 +0000

    Remove DestroyOffTheRecordProfile() function.
    Profile::DestroyOffTheRecordProfile function without arguments is
    deprecated and replaced with a function that receives the OTR profile.
    The few remaining use cases of the function are replaced with the new
    API and the old function is removed.
    
    Bug: 1033903
    Change-Id: Ie417f4ef12f449774c658228dc3850b65811b3bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196168
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769615}

--- chrome/browser/extensions/extension_context_menu_browsertest.cc
+++ chrome/browser/extensions/extension_context_menu_browsertest.cc
@@ -905,7 +905,8 @@ IN_PROC_BROWSER_TEST_P(ExtensionContextMenuLazyTest,
                                                  false);
 
   // Create an incognito profile.
-  ASSERT_TRUE(browser()->profile()->GetOffTheRecordProfile());
+  Profile* incognito = browser()->profile()->GetPrimaryOTRProfile();
+  ASSERT_TRUE(incognito);
   ASSERT_TRUE(LoadContextMenuExtensionWithIncognitoFlags("incognito"));
 
   // Wait for the extension's processes to tell us they've created an item.
@@ -913,7 +914,7 @@ IN_PROC_BROWSER_TEST_P(ExtensionContextMenuLazyTest,
   ASSERT_TRUE(created_incognito.WaitUntilSatisfied());
   ASSERT_EQ(2u, GetItems().size());
 
-  browser()->profile()->DestroyOffTheRecordProfile();
+  browser()->profile()->DestroyOffTheRecordProfile(incognito);
   ASSERT_EQ(1u, GetItems().size());
 }
 
