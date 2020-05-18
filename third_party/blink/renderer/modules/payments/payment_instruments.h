b4c20cb7a8e3df2160d07f8100e44ff37b40cf69
Hitoshi Yoshida <peria@chromium.org>
2020-05-18 12:29:57 +0000

    bindings: Drop wrapping of IDL dictionary
    This CL is a revert of http://crrev.com/580435
    IDL dictionary is now a GCed class, thus no need to make a GCed
    copy to bind to a function.
    
    
    Bug: 839389
    Change-Id: I629b53fa3e6ae23e7531b1106392a15b2bae39a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2206197
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Hitoshi Yoshida <peria@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769676}

--- third_party/blink/renderer/modules/payments/payment_instruments.h
+++ third_party/blink/renderer/modules/payments/payment_instruments.h
@@ -23,7 +23,6 @@ class PaymentInstrument;
 class ScriptPromise;
 class ScriptPromiseResolver;
 class ScriptState;
-class PaymentInstrumentParameter;
 
 class MODULES_EXPORT PaymentInstruments final : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
@@ -56,7 +55,7 @@ class MODULES_EXPORT PaymentInstruments final : public ScriptWrappable {
   mojom::blink::PermissionService* GetPermissionService(ScriptState*);
   void OnRequestPermission(ScriptPromiseResolver*,
                            const String&,
-                           PaymentInstrumentParameter*,
+                           const PaymentInstrument*,
                            mojom::blink::PermissionStatus);
 
   void onDeletePaymentInstrument(ScriptPromiseResolver*,
