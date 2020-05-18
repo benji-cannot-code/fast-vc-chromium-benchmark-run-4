cb6064c654e95271e4adf6124425b14b2d172fb4
= <wfurr@google.com>
2020-05-18 21:41:13 +0000

    Update Ink binaries from google3.
    This update brings performance, low latency, and API improvements.
    Testing on a Pixelbook shows that the inking surface receives a hardware
    overlay in all orientations.
    
    The new binaries are smaller.  The new WebAssembly binary is 6.8 MB
    compared to 5.5 MB for the old one, plus a 2 MB memory initialization
    file that's no longer required.  That's a net reduction of 0.7 MB.  The
    JavaScript is smaller as well; 217 KB to 183 KB for the WebAssembly
    loader and bindings script, and 350 KB to 89 KB for the Ink closure JS
    binary.  Also, only the threaded build is required now that threads are
    stable in Chrome, which eliminates the entire 7.8 MB non-threaded
    WebAssembly and script files.  Total savings: 8.8 MB.
    
    The Ink exported binary uses a new interface with narrowly defined
    externs and no longer exports 'goog' among other globals from the
    previous automatic externs. It may be possible in a future change to
    remove the iframe sandboxing from the Ink binary.
    
    Change-Id: Ieba6513b723f91d67c445246e7ba4e08f113469e
    Bug: 1084273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204141
    Commit-Queue: William Furr <wfurr@google.com>
    Reviewed-by: dstockwell <dstockwell@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769842}

--- chrome/browser/resources/pdf/elements/viewer-pdf-toolbar.js
+++ chrome/browser/resources/pdf/elements/viewer-pdf-toolbar.js
@@ -256,7 +256,7 @@ Polymer({
     const tool = element.id;
     const options = element.querySelector('viewer-pen-options') || {
       selectedSize: 1,
-      selectedColor: null,
+      selectedColor: undefined,
     };
     const attributeStyleMap = element.attributeStyleMap;
     attributeStyleMap.set('--pen-tip-fill', options.selectedColor);
