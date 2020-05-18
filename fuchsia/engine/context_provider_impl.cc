cb7c116816838d3e359490e0e3446e24982f1333
Fabrice de Gans-Riberi <fdegans@chromium.org>
2020-05-18 11:46:14 +0000

    [fuchsia] Remove the custom flag to enable Cast Streaming Receiver
    * Fuchsia does not allow to set custom flags in FIDL bit fields.
    * Add the --enable-cast-streaming-receiver command-line switch to the
      allow list for the config file. This switch will be passed to the
      ContextProvider, which will pass it to the Context.
    
    Bug: 1078919
    Change-Id: Icd541639920cfeaa7101626ad1f62925e5e8a9b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202856
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769670}

--- fuchsia/engine/context_provider_impl.cc
+++ fuchsia/engine/context_provider_impl.cc
@@ -159,6 +159,8 @@ bool MaybeAddCommandLineArgsFromConfig(const base::Value& config,
       switches::kAcceleratedCanvas2dMSAASampleCount,
       switches::kDisableFeatures,
       switches::kDisableGpuWatchdog,
+      // TODO(crbug.com/1082821): Remove this switch from the allow-list.
+      switches::kEnableCastStreamingReceiver,
       switches::kEnableFeatures,
       switches::kEnableLowEndDeviceMode,
       switches::kForceGpuMemAvailableMb,
@@ -211,12 +213,6 @@ bool IsFuchsiaCdmSupported() {
   return false;
 #endif
 }
-
-// Use the most significant bit to enable cast streaming receiver features.
-// TODO(crbug.com/1078919): Remove this when we have a better way of enabling
-// this feature.
-constexpr auto kCastStreamingFeatureFlag =
-    static_cast<fuchsia::web::ContextFeatureFlags>(1ULL << 63);
 
 }  // namespace
 
@@ -460,11 +456,6 @@ void ContextProviderImpl::Create(
 
     launch_command.AppendSwitch(switches::kDisableSoftwareVideoDecoders);
   }
-
-  const bool enable_cast_streaming_receiver =
-      (features & kCastStreamingFeatureFlag) == kCastStreamingFeatureFlag;
-  if (enable_cast_streaming_receiver)
-    launch_command.AppendSwitch(switches::kEnableCastStreamingReceiver);
 
   // Validate embedder-supplied product, and optional version, and pass it to
   // the Context to include in the UserAgent.
