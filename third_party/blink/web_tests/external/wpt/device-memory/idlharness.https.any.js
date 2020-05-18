cf10ae76b5c94c8435200cb7f05d81bb9c564798
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-05-18 16:12:42 +0000

    Import wpt@b60726a4394d1c1cadd0d7e4201088499d019f78
    Using wpt-import in Chromium 6f03162f56f64f8e6ece2eaa935f80b63ae75e5e.
    With Chromium commits locally applied on WPT:
    f6984fd94c "De-flaky pointerlock/pointerevent_pointermove_in_pointerlock.html"
    6c948200b9 "webrtc wpt: add test for ability to connect to two dtls certs"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    fmeawad@chromium.org, panicker@chromium.org:
      external/wpt/device-memory
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    
    NOAUTOREVERT=true
    TBR=raphael.kubo.da.costa
    
    No-Export: true
    Change-Id: Id7998e9f8fe48a6a319865d4eb3162d0fb2f8a29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207015
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#769726}

---
+++ third_party/blink/web_tests/external/wpt/device-memory/idlharness.https.any.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+// META: timeout=long
+
+// https://w3c.github.io/device-memory/
+
+"use strict";
+
+idl_test(
+  ['device-memory'],
+  ['html'],
+  async idl_array => {
+    if (self.GLOBAL.isWorker()) {
+      idl_array.add_objects({ WorkerNavigator: ['navigator'] });
+    } else {
+      idl_array.add_objects({ Navigator: ['navigator'] });
+    }
+  }
+);
--- third_party/blink/web_tests/external/wpt/webrtc/RTCDTMFSender-helper.js
+++ third_party/blink/web_tests/external/wpt/webrtc/RTCDTMFSender-helper.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // The following helper functions are called from RTCPeerConnection-helper.js:
 //   getTrackFromUserMedia
-//   doSignalingHandshake
+//   exchangeOfferAnswer
 
 // Create a RTCDTMFSender using getUserMedia()
 // Connect the PeerConnection to another PC and wait until it is
@@ -27,7 +27,7 @@ function createDtmfSender(pc = new RTCPeerConnection()) {
     const pc2 = new RTCPeerConnection();
     Object.defineProperty(pc, 'otherPc', { value: pc2 });
     exchangeIceCandidates(pc, pc2);
-    return doSignalingHandshake(pc, pc2);
+    return exchangeOfferAnswer(pc, pc2);
   }).then(() => {
     if (!('canInsertDTMF' in dtmfSender)) {
       return Promise.resolve();
