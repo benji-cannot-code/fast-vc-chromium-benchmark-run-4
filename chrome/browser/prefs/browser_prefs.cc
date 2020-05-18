e94975c21dcdfdd7fd55b51c883b3bf9bc710daf
Mason Freed <masonfreed@chromium.org>
2020-05-18 08:15:48 +0000

    Remove the WebComponentsV0Enabled Enterprise Policy
    This policy allowed Enterprise users to re-enable the Web Components
    V0 features for several milestones. It was supported until M84,
    which has now branched for Beta.
    
    Bug: 911943, 937746
    Change-Id: I0bb06e5cddc282cc1b31b45ff49f05c1ab004a5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204306
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769637}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -93,7 +93,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_ui_features_prefs.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/secure_origin_whitelist.h"
-#include "chrome/common/web_components_prefs.h"
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/browsing_data/core/pref_names.h"
 #include "components/certificate_transparency/pref_names.h"
@@ -892,7 +891,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   syncer::SyncPrefs::RegisterProfilePrefs(registry);
   syncer::PerUserTopicSubscriptionManager::RegisterProfilePrefs(registry);
   syncer::InvalidatorRegistrarWithMemory::RegisterProfilePrefs(registry);
-  web_components_prefs::RegisterProfilePrefs(registry);
   TemplateURLPrepopulateData::RegisterProfilePrefs(registry);
   translate::TranslatePrefs::RegisterProfilePrefs(registry);
   omnibox::RegisterProfilePrefs(registry);
