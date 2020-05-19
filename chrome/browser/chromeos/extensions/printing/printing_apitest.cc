36bf4c3ff1635d5d8b95cacd83e7bcbdb02fe6e5
Nikita Podguzov <nikitapodguzov@chromium.org>
2020-05-19 17:07:38 +0000

    Printing API: Add browser test missing include statement
    As far as we don't have build bot with use_cups=true this bug slipped
    under our radars.
    
    Bug: 996785
    Change-Id: Ia6cfcaed81330407fe26c9c139bf631b30a32c5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207460
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770149}

--- chrome/browser/chromeos/extensions/printing/printing_apitest.cc
+++ chrome/browser/chromeos/extensions/printing/printing_apitest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chromeos/printing/printer_configuration.h"
+#include "content/public/test/browser_test.h"
 #include "extensions/test/result_catcher.h"
 #include "printing/backend/print_backend.h"
 #include "printing/backend/test_print_backend.h"
