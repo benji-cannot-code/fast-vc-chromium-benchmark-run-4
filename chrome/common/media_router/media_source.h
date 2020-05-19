04f8e5041af4ef15eb1ebcf3d9924a154af08f34
mark a. foltz <mfoltz@chromium.org>
2020-05-19 19:25:07 +0000

    Revert "[Media Router] Consolidate origin checks for using Cast streaming apps."
    This reverts commit 34fae769ce322627328f3787f383387f66027d07.
    
    Reason for revert: Seems to have broken tab mirroring.
    
    Original change's description:
    > [Media Router] Consolidate origin checks for using Cast streaming apps.
    >
    > Previously, the origin check was only done for sink availability and not
    > at route creation.  It also did some brittle string parsing of the
    > media source ID.
    >
    > The updated check covers all streaming app IDs, adds the meet.google.com
    > origin, and checks origins for both availability and route creation.
    >
    > Follow up changes will add additional checks to prevent opaque origins
    > from also launching streaming apps (see bug for details).
    >
    > Bug: 1047834,1081503
    > Change-Id: I081822796524ec2c72af29c544382cb4c9d9542d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2095928
    > Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    > Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#769512}
    
    TBR=mfoltz@chromium.org,takumif@chromium.org,jophba@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1047834, 1081503, 1084643
    Change-Id: I328fe04b0d0541a86649e84f06a44a30e128051b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207748
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770231}

--- chrome/common/media_router/media_source.h
+++ chrome/common/media_router/media_source.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/hash.h"
 #include "url/gurl.h"
 
-// TODO(takumif): Move all Chromecast-specific constants out of this file, since
-// they are not directly related to MediaSource.
-
 namespace media_router {
 
 // URL schemes used by Presentation URLs for Cast and DIAL.
@@ -32,6 +29,9 @@ constexpr char kLegacyCastPresentationUrlPrefix[] =
 constexpr char kMirroringAppUri[] = "cast:0F5096E8";
 
 // Strings used in presentation IDs by the Cast SDK implementation.
+// TODO(takumif): Move them out of this file, since they are not directly
+// related to MediaSource.
+//
 // This value must be the same as |chrome.cast.AUTO_JOIN_PRESENTATION_ID| in the
 // component extension.
 constexpr char kAutoJoinPresentationId[] = "auto-join";
--- chrome/common/media_router/providers/cast/cast_media_source.h
+++ chrome/common/media_router/providers/cast/cast_media_source.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/media_router/media_source.h"
 #include "components/cast_channel/cast_message_util.h"
 #include "components/cast_channel/cast_socket.h"
-#include "url/origin.h"
 
 using cast_channel::ReceiverAppType;
 
@@ -147,14 +146,6 @@ class CastMediaSource {
 
   // Returns a list of App IDs in this CastMediaSource.
   std::vector<std::string> GetAppIds() const;
-
-  // Returns a list of origins that are allowed to use this source.  An empty
-  // list means any origin is allowed.  For now, non-empty origins are websites
-  // allowed to use this source via the Presentation API.
-  std::vector<url::Origin> GetAllowedOrigins() const;
-
-  // Returns true if |origin| is allowed according to GetAllowedOrigins().
-  bool IsAllowedOrigin(const url::Origin& origin) const;
 
   const MediaSource::Id& source_id() const { return source_id_; }
   const std::vector<CastAppInfo>& app_infos() const { return app_infos_; }
