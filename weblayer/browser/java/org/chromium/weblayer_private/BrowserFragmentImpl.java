69e8b9ba5e88cd2e4af465bee74a4b7cc9a12f86
Robbie McElrath <rmcelrath@chromium.org>
2020-05-19 20:28:50 +0000

    [WebLayer] Implement the Site Settings UI in WebLayer.
    This CL hooks the componentized Site Settings Fragments into the
    SiteSettingsFragmentImpl.
    
    Binary-Size: Site Settings client interfaces require lots of methods
    Bug: 1049683
    Change-Id: I66afa208bd81ca50052040aabbf427adb37b6be3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2184200
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770265}

--- weblayer/browser/java/org/chromium/weblayer_private/BrowserFragmentImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/BrowserFragmentImpl.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.view.ViewGroup;
 
 import org.chromium.components.browser_ui.styles.R;
 import org.chromium.components.embedder_support.application.ClassLoaderContextWrapperFactory;
@@ -74,7 +75,7 @@ public class BrowserFragmentImpl extends RemoteFragmentImpl {
     }
 
     @Override
-    public View onCreateView() {
+    public View onCreateView(ViewGroup container, Bundle savedInstanceState) {
         StrictModeWorkaround.apply();
         return mBrowser.getFragmentView();
     }
