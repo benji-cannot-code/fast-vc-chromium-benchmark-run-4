d117859037288515117bd536b7b5268b0465e43b
Moe Ahmadi <mahmadi@chromium.org>
2020-05-19 22:57:57 +0000

    [NTP][Realbox] Adds a11y label/tooltip to suggestion group toggle button
    The a11y label changes between "Hide the 'X' section" and
    "Show the 'X' section" depending on the suggestion group visibility where
    X is the suggestion group header (e.g., 'Recommended for you').
    
    The tooltip changes between "Hide Suggestions" and "Show Suggestions"
    depending on the suggestion group visibility.
    
    Bug: 1052514
    Change-Id: Ib7bce19b8d203e57b9b916fbcc089a58593bdd68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2206222
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770341}

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -1683,13 +1683,13 @@ function renderAutocompleteMatches(matches, suggestionGroupsMap) {
   suggestionGroupElsMap = {};
 
   /**
-   * Creates and returns a remove button that once clicked invokes |callback|.
+   * Creates and returns an action button that once clicked invokes |callback|.
    * @param {!function()} callback
    */
-  function createRemoveButton(callback) {
+  function createActionButton(callback) {
     const icon = document.createElement('button');
-    icon.title = configData.translatedStrings.removeSuggestion;
     icon.classList.add(CLASSES.REMOVE_ICON);
+    icon.tabIndex = -1;
     icon.onmousedown = e => {
       e.preventDefault();  // Stops default browser action (focus)
     };
@@ -1704,10 +1704,13 @@ function renderAutocompleteMatches(matches, suggestionGroupsMap) {
       e.preventDefault();  // Stops default browser action (navigation)
     };
 
-    const remove = document.createElement('div');
-    remove.classList.add(CLASSES.REMOVE_MATCH);
-    remove.appendChild(icon);
-    return remove;
+    const action = document.createElement('div');
+
+    action.classList.add(CLASSES.REMOVE_MATCH);
+    action.tabIndex = 0;
+    action.setAttribute('role', 'button');
+    action.appendChild(icon);
+    return action;
   }
 
   /**
@@ -1722,6 +1725,21 @@ function renderAutocompleteMatches(matches, suggestionGroupsMap) {
 
     const suggestionGroup = assert(suggestionGroupsMap[suggestionGroupId]);
 
+    /**
+     * Updates the tooltip and a11y label of the suggestion group toggle button.
+     * @param {!Element} toggleButtonEl
+     * @param {boolean} groupIsHidden
+     */
+    function updateToggleButtonA11y(toggleButtonEl, groupIsHidden) {
+      toggleButtonEl.title = groupIsHidden ?
+          configData.translatedStrings.showSuggestions :
+          configData.translatedStrings.hideSuggestions;
+      toggleButtonEl.ariaLabel = utils.substituteString(
+          groupIsHidden ? configData.translatedStrings.showSection :
+                          configData.translatedStrings.hideSection,
+          suggestionGroup.header);
+    }
+
     const groupEl = document.createElement('div');
     groupEl.classList.toggle(CLASSES.COLLAPSED, suggestionGroup.hidden);
     const headerEl = document.createElement('a');
@@ -1731,12 +1749,15 @@ function renderAutocompleteMatches(matches, suggestionGroupsMap) {
     headerEl.tabIndex = -1;
     headerEl.append(document.createTextNode(suggestionGroup.header));
     if (configData.suggestionTransparencyEnabled) {
-      const remove = createRemoveButton(() => {
+      const toggle = createActionButton(() => {
         groupEl.classList.toggle(CLASSES.COLLAPSED);
+        updateToggleButtonA11y(
+            toggle, groupEl.classList.contains(CLASSES.COLLAPSED));
         window.chrome.embeddedSearch.searchBox
             .toggleSuggestionGroupIdVisibility(suggestionGroupId);
       });
-      headerEl.appendChild(remove);
+      updateToggleButtonA11y(toggle, suggestionGroup.hidden);
+      headerEl.appendChild(toggle);
       realboxMatchesEl.classList.add(CLASSES.REMOVABLE);
     }
     groupEl.appendChild(headerEl);
@@ -1833,9 +1854,10 @@ function renderAutocompleteMatches(matches, suggestionGroupsMap) {
     }
 
     if (match.supportsDeletion && configData.suggestionTransparencyEnabled) {
-      const remove = createRemoveButton(() => {
+      const remove = createActionButton(() => {
         window.chrome.embeddedSearch.searchBox.deleteAutocompleteMatch(i);
       });
+      remove.title = configData.translatedStrings.removeSuggestion;
       matchEl.appendChild(remove);
       realboxMatchesEl.classList.add(CLASSES.REMOVABLE);
     }
