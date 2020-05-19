df7d4055e40e5c7aa70c974c1af4057880761c6a
Charlene Yan <cyan@chromium.org>
2020-05-19 03:51:03 +0000

    [Tab Groups Collapse] Use TabAnimationState rather than a new boolean for the collapsed TabWidthConstraint.
    When the tab group collapses, the available tabstrip width is not
    updated to reflect the collapsed group.
    Also easier and less code >_>
    
    Bug: 1018230
    Change-Id: If3e0907ce3b881e736c748a6db7b040e66a5c172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207617
    Commit-Queue: Charlene Yan <cyan@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769980}

--- chrome/browser/ui/views/tabs/tab_width_constraints.cc
+++ chrome/browser/ui/views/tabs/tab_width_constraints.cc
@@ -11,12 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 TabWidthConstraints::TabWidthConstraints(
     const TabAnimationState& state,
     const TabLayoutConstants& layout_constants,
-    const TabSizeInfo& size_info,
-    bool is_collapsed)
+    const TabSizeInfo& size_info)
     : state_(state),
       layout_constants_(layout_constants),
-      size_info_(size_info),
-      is_collapsed_(is_collapsed) {}
+      size_info_(size_info) {}
 
 float TabWidthConstraints::GetMinimumWidth() const {
   const float min_width = gfx::Tween::FloatValueBetween(
