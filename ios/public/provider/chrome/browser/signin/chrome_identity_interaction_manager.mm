982a1b5e7039d100ed4069857e2c9cb735bc29cb
Nohemi Fernandez <fernandex@chromium.org>
2020-05-19 09:25:21 +0000

    [iOS] Add completion parameter to ChromeIdentityService cancel API.
    In response to b/149180809 an additional parameter was added to the
    cancel add account flow. Update service accordingly.
    
    Bug: 1078815
    Change-Id: I34bd4aa9a68bd949ce78d366afcee0f7b3fc423f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207158
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770049}

--- ios/public/provider/chrome/browser/signin/chrome_identity_interaction_manager.mm
+++ ios/public/provider/chrome/browser/signin/chrome_identity_interaction_manager.mm
@@ -47,7 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   NOTREACHED() << "Subclasses must override this";
 }
 
-- (void)cancelAddAccountWithAnimation:(BOOL)animated {
+- (void)cancelAddAccountWithAnimation:(BOOL)animated
+                           completion:(void (^)(void))completion {
   NOTREACHED() << "Subclasses must override this";
 }
 
