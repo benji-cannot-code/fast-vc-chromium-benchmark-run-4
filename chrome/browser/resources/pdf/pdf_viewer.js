57bcac4211c944a45325ca9fcb57f26f9e5cb238
Ankit Kumar <ankk@microsoft.com>
2020-05-19 06:21:27 +0000

    Reland "Add PDFExtensionTest for opening context menu from touch selection menu"
    This CL adds a browser test for TouchSelectionMenu in PDF. There is no
    utility class which could help in determining when touch selection menu
    is opened. This CL adds TouchSelectionMenuWaiter() to determine when
    touch selection menu is shown and helps in executing commands on the
    touch selection menu.
    
    Fix some nits in TouchSelectionMenuViews along the way.
    
    This CL originally landed as https://crrev.com/c/2082750 but got
    reverted in https://crrev.com/c/2178584 due to failures on bots with CFI
    enabled. The static cast from View to TouchSelectionMenuViews should be
    safe but for some reason CFI bot isn't happy. Given we don't need
    TouchSelectionMenuViews and View can satisfy the test, using View
    instead of casting to TouchSelectionMenuViews.
    
    CQ_INCLUDE_TRYBOTS=luci.chromium.try:linux_chromium_cfi_rel_ng
    
    Bug: 1027842
    Change-Id: Ia055dd15caf894fad64e2a35fed524244ed598f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203785
    Commit-Queue: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770002}

--- chrome/browser/resources/pdf/pdf_viewer.js
+++ chrome/browser/resources/pdf/pdf_viewer.js
@@ -1287,6 +1287,11 @@ export class PDFViewer {
         this.isFormFieldFocused_ =
             /** @type {{ focused: boolean }} */ (data).focused;
         return;
+      case 'touchSelectionOccurred':
+        this.sendScriptingMessage_({
+          type: 'touchSelectionOccurred',
+        });
+        return;
     }
     assertNotReached('Unknown message type received: ' + data.type);
   }
