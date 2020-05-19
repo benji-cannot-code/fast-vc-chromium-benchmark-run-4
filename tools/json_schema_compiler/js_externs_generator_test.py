97844fb2533cfb3adfa2861b2b1bf1c124902319
dpapad <dpapad@chromium.org>
2020-05-19 16:57:17 +0000

    Extensions JS externs: Handle "!" when converting to nullable type.
    When creating externs for an optional non-final parameter, to appear
    as a nullable required parameter, handle the case where it is
    explicitly non-nullable indicated by the "!" operator.
    
    Otherwise an invalid JS type is created as
    ?!chrome.fakeApi.Baz|undefined
    instead of
    ?chrome.fakeApi.Baz|undefined
    
    Bug: 469920
    Change-Id: I93ce8952586aa92e8ad3f1176643677ec4d6525b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208185
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770145}

--- tools/json_schema_compiler/js_externs_generator_test.py
+++ tools/json_schema_compiler/js_externs_generator_test.py
@@ -85,6 +85,7 @@ namespace fakeApi {
                                        optional double num,
                                        object obj,
                                        optional boolean bool,
+                                       optional Baz baz,
                                        VoidCallback callback);
 
     static void multipleOptionalParams(optional DOMString param1,
@@ -228,10 +229,11 @@ chrome.fakeApi.optionalParam = function(callback) {};
  * @param {?number|undefined} num
  * @param {Object} obj
  * @param {?boolean|undefined} bool
+ * @param {?chrome.fakeApi.Baz|undefined} baz
  * @param {function():void} callback
  * @see https://developer.chrome.com/extensions/fakeApi#method-nonFinalOptionalParams
  */
-chrome.fakeApi.nonFinalOptionalParams = function(string, num, obj, bool, callback) {};
+chrome.fakeApi.nonFinalOptionalParams = function(string, num, obj, bool, baz, callback) {};
 
 /**
  * @param {string=} param1
