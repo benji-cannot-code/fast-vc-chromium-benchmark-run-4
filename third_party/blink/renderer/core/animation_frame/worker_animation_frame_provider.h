89afff3dea7923205a014427903521e57614ddc9
Yuki Yamada <yukiy@chromium.org>
2020-05-19 06:24:18 +0000

    Remove WeakPtrFactory for WorkerAnimationFrameProvider
    Removes a WeakPtrFactory for WorkerAnimationFrameProvider which is a
    BlinkGC class.
    
    Change-Id: I18d0af057804cef01abefe3229627d706d6cf90d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208153
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770003}

--- third_party/blink/renderer/core/animation_frame/worker_animation_frame_provider.h
+++ third_party/blink/renderer/core/animation_frame/worker_animation_frame_provider.h
@@ -58,8 +58,6 @@ class CORE_EXPORT WorkerAnimationFrameProvider
   HeapLinkedHashSet<WeakMember<OffscreenCanvas>> offscreen_canvases_;
 
   Member<ExecutionContext> context_;
-
-  base::WeakPtrFactory<WorkerAnimationFrameProvider> weak_factory_{this};
 };
 
 }  // namespace blink
