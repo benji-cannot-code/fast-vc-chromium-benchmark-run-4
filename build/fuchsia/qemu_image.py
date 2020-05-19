c9b38b067db5d0087a0148be5f9766f5cecb595b
Stephen Roe <steveroe@google.com>
2020-05-19 19:25:55 +0000

    [fuchsia] Add diagnostics for qemu-img fuchsia image generation errors.
    Reports qemu-img exit code and command output.  The exit code allows
    locating these intermittent errors via BigQuery.  Logging the command
    output will give more information on the failure.
    
    See http://crbug.com/1063106#c7
    
    Bug: 1063106
    Change-Id: I60d7240eb4183e70ae25aa8efb514ba05ac356df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208109
    Commit-Queue: Stephen Roe <steveroe@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770232}

--- build/fuchsia/qemu_image.py
+++ build/fuchsia/qemu_image.py
@@ -19,6 +19,7 @@ TODO(crbug.com/1046861): Remove this workaround when the bug is fixed.
 
 import logging
 import subprocess
+import tempfile
 import time
 
 
@@ -34,7 +35,9 @@ def _ExecQemuImgWithTimeout(command):
   """
 
   logging.info('qemu-img starting')
-  p = subprocess.Popen(command)
+  command_output_file = tempfile.NamedTemporaryFile('w')
+  p = subprocess.Popen(command, stdout=command_output_file,
+                       stderr=subprocess.STDOUT)
   start_sec = time.time()
   while p.poll() is None and time.time() - start_sec < QEMU_IMG_TIMEOUT_SEC:
     time.sleep(1)
@@ -42,10 +45,17 @@ def _ExecQemuImgWithTimeout(command):
   logging.info('qemu-img duration: %f' % float(stop_sec - start_sec))
 
   if p.poll() is None:
+    returncode = None
     p.kill()
-    return None
+    p.wait()
+  else:
+    returncode = p.returncode
 
-  return p.returncode
+  log_level = logging.WARN if returncode else logging.DEBUG
+  for line in open(command_output_file.name, 'r'):
+    logging.log(log_level, 'qemu-img stdout: ' + line.strip())
+
+  return returncode
 
 
 def ExecQemuImgWithRetry(command):
