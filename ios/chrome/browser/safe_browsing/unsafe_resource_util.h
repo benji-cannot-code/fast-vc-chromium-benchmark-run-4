b025d2c9efdd7ef4157b954bd543cf885111ef5f
Kurt Horimoto <kkhorimoto@chromium.org>
2020-05-19 00:03:30 +0000

    [iOS] Create PendingUnsafeResourceStorage
    This helper class stores a copy of an UnsafeResource while its allow
    list decision is pending.  When the pending decision is finished (either
    by allowing or disallowing the threat), the resource is reset.
    
    This CL also adds a new util function that returns the allow list for
    an UnsafeResource.
    
    Bug: 1083557
    Change-Id: Iec9e32fdc68a59f43c98c33e2f04f1ee2f5f5bdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2205115
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#769911}

--- ios/chrome/browser/safe_browsing/unsafe_resource_util.h
+++ ios/chrome/browser/safe_browsing/unsafe_resource_util.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/core/base_safe_browsing_error_ui.h"
 #include "components/security_interstitials/core/unsafe_resource.h"
 
+class SafeBrowsingUrlAllowList;
+
 // Runs |resource|'s callback on the appropriate thread.
 void RunUnsafeResourceCallback(
     const security_interstitials::UnsafeResource& resource,
@@ -22,6 +24,10 @@ GetUnsafeResourceInterstitialReason(
 
 // Returns the metric prefix for error pages for |resource|.
 std::string GetUnsafeResourceMetricPrefix(
+    const security_interstitials::UnsafeResource& resource);
+
+// Returns the SafeBrowsingUrlAllowList for |resource|.
+SafeBrowsingUrlAllowList* GetAllowListForResource(
     const security_interstitials::UnsafeResource& resource);
 
 #endif  // IOS_CHROME_BROWSER_SAFE_BROWSING_UNSAFE_RESOURCE_UTIL_H_
