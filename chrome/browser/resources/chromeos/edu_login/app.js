97910d661305885a4641961ed285b6fb139f48da
yilkal <yilkal@chromium.org>
2020-05-19 19:41:17 +0000

    Removes the welcome screen of EDU coexistence flow.
    This cl removes the welcome screen of EDU coexistence flow.
    This means that the first page that a supervised user sees in the
    flow allows the user to select which parent to use to add the EDU
    account. The strings in this current first screen have been updated.
    
    A screenshot can be found here:
    https://drive.google.com/file/d/164-O5p1tixfWCxzU0T7yWoZQcPzCksyb/view?usp=sharing
    
    Bug: 1082336
    Change-Id: Ife1f09a29babd8185199f28bf9856af97df15405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204816
    Commit-Queue: Yilkal Abe <yilkal@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770241}

--- chrome/browser/resources/chromeos/edu_login/app.js
+++ chrome/browser/resources/chromeos/edu_login/app.js
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './edu_login_welcome.js';
 import './edu_login_parents.js';
 import './edu_login_parent_signin.js';
 import './edu_login_parent_info.js';
@@ -16,7 +15,6 @@ import {EduLoginParams, ParentAccount} from './edu_login_util.js';
 
 /** @enum {string} */
 const Steps = {
-  WELCOME: 'welcome',
   PARENTS: 'parents',
   PARENT_SIGNIN: 'parent-signin',
   PARENT_INFO: 'parent-info',
