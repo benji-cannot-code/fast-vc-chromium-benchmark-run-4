db811d39a08fb19661b40e47d211a41bfef47ee0
Andy Paicu <andypaicu@chromium.org>
2020-05-19 18:43:44 +0000

    Updated android permissions tests that relied on infobars to ...
    ... check modals instead.
    
    Since modals have been the default for a while, all these test were
    disabled and needed reworking.
    
    Bug: 935900
    Change-Id: I47e706dc288e24369a2b6e1f1af5af24deb7bfbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2171490
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770200}

--- chrome/android/javatests/src/org/chromium/chrome/browser/permissions/QuotaTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/permissions/QuotaTest.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -58,8 +57,7 @@ public class QuotaTest {
     @Test
     @MediumTest
     @Feature({"QuotaPermissions"})
-    @DisabledTest(message = "Modals are now enabled and test needs to be reworked crbug.com/935900")
-    public void testQuotaShowsInfobar() throws Exception {
-        testQuotaPermissionsPlumbing("initiate_requestQuota(1024)", 1, false, false);
+    public void testQuotaPermissionRequestShowsModal() throws Exception {
+        testQuotaPermissionsPlumbing("initiate_requestQuota(1024)", 1, false, true);
     }
 }
