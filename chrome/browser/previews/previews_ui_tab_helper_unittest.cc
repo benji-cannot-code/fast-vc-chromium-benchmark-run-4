1a515a1612e2ebbc09193593e5ce4f7d1d123410
Tarun Bansal <tbansal@chromium.org>
2020-05-19 20:06:07 +0000

    Remove initialization of chrome data use measurement by DRP
    Chrome data use measurement singleton object is currently created
    at 2 different places. This CL removes the creation
    of data use measurement singleton object by DRP (Data Reduction
    Proxy). Instead, the class object would only be created by
    chrome/browser/chrome_content_browser_client.cc
    
    Change-Id: I638614f9cc4c71c9ba8a9b4dca683820ae8d2c91
    Bug: 1084194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2205513
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770254}

--- chrome/browser/previews/previews_ui_tab_helper_unittest.cc
+++ chrome/browser/previews/previews_ui_tab_helper_unittest.cc
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/data_reduction_proxy/data_reduction_proxy_chrome_settings.h"
 #include "chrome/browser/data_reduction_proxy/data_reduction_proxy_chrome_settings_factory.h"
+#include "chrome/browser/data_use_measurement/chrome_data_use_measurement.h"
 #include "chrome/browser/previews/previews_https_notification_infobar_decider.h"
 #include "chrome/browser/previews/previews_service.h"
 #include "chrome/browser/previews/previews_service_factory.h"
@@ -71,6 +73,11 @@ class PreviewsUITabHelperUnitTest : public ChromeRenderViewHostTestHarness {
     test_handle_->set_redirect_chain(redirect_chain);
     content::RenderFrameHostTester::For(main_rfh())
         ->InitializeRenderFrameIfNeeded();
+
+    if (!data_use_measurement::ChromeDataUseMeasurement::GetInstance()) {
+      data_use_measurement::ChromeDataUseMeasurement::CreateInstance(
+          g_browser_process->local_state());
+    }
 
     DataReductionProxyChromeSettingsFactory::GetForBrowserContext(profile())
         ->InitDataReductionProxySettings(
--- components/data_use_measurement/core/data_use_measurement_unittest.cc
+++ components/data_use_measurement/core/data_use_measurement_unittest.cc
@@ -22,10 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace data_use_measurement {
 
-class DataUseMeasurementTest : public testing::Test {
+class DataUseMeasurementTest {
  public:
   DataUseMeasurementTest()
       : data_use_measurement_(
+            nullptr,
             network::TestNetworkConnectionTracker::GetInstance()) {
     // During the test it is expected to not have cellular connection.
     DCHECK(!net::NetworkChangeNotifier::IsConnectionCellular(
@@ -60,22 +61,27 @@ class DataUseMeasurementTest : public testing::Test {
 // foreground or the OS is not Android.
 // TODO(amohammadkhan): Add tests for Cellular/non-cellular connection types
 // when support for testing is provided in its class.
-TEST_F(DataUseMeasurementTest, UserNotUserTest) {
+TEST(DataUseMeasurementTest, UserNotUserTest) {
+  DataUseMeasurementTest data_use_measurement_test;
 #if defined(OS_ANDROID)
-  data_use_measurement()->OnApplicationStateChangeForTesting(
-      base::android::APPLICATION_STATE_HAS_RUNNING_ACTIVITIES);
+  data_use_measurement_test.data_use_measurement()
+      ->OnApplicationStateChangeForTesting(
+          base::android::APPLICATION_STATE_HAS_RUNNING_ACTIVITIES);
 #endif
-  TestForAUserRequest("Foreground.");
+  data_use_measurement_test.TestForAUserRequest("Foreground.");
 }
 
 #if defined(OS_ANDROID)
 // This test function tests recording of data use information in UMA histogram
 // when packet is originated from user or services when the app is in the
 // background and OS is Android.
-TEST_F(DataUseMeasurementTest, ApplicationStateTest) {
-  data_use_measurement()->OnApplicationStateChangeForTesting(
-      base::android::APPLICATION_STATE_HAS_STOPPED_ACTIVITIES);
-  TestForAUserRequest("Background.");
+TEST(DataUseMeasurementTest, ApplicationStateTest) {
+  DataUseMeasurementTest data_use_measurement_test;
+
+  data_use_measurement_test.data_use_measurement()
+      ->OnApplicationStateChangeForTesting(
+          base::android::APPLICATION_STATE_HAS_STOPPED_ACTIVITIES);
+  data_use_measurement_test.TestForAUserRequest("Background.");
 }
 #endif
 
