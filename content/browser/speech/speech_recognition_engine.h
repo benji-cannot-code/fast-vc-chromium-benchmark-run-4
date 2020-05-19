291a236f921b256d2880d1092399e4b6c7f18324
evliu <evliu@google.com>
2020-05-19 18:22:32 +0000

    Refactor the SpeechRecognitionEngine
    This CL splits out the UpstreamLoader and DownstreamLoader from the
    SpeechRecognitionEngine, allowing it to be reused by other components.
    
    Bug: 1082419
    Change-Id: I446735a223820489b59a4272182efdfe2e0acff2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199976
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#770191}

--- content/browser/speech/speech_recognition_engine.h
+++ content/browser/speech/speech_recognition_engine.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
 #include "base/strings/string_piece.h"
+#include "components/speech/downstream_loader.h"
+#include "components/speech/downstream_loader_client.h"
+#include "components/speech/upstream_loader.h"
+#include "components/speech/upstream_loader_client.h"
 #include "content/browser/speech/audio_encoder.h"
 #include "content/browser/speech/chunked_byte_buffer.h"
 #include "content/common/content_export.h"
@@ -60,7 +64,9 @@ struct SpeechRecognitionError;
 // EndRecognition. If a recognition was started, the caller can free the
 // SpeechRecognitionEngine only after calling EndRecognition.
 
-class CONTENT_EXPORT SpeechRecognitionEngine {
+class CONTENT_EXPORT SpeechRecognitionEngine
+    : public speech::UpstreamLoaderClient,
+      public speech::DownstreamLoaderClient {
  public:
   class Delegate {
    public:
@@ -105,7 +111,7 @@ class CONTENT_EXPORT SpeechRecognitionEngine {
   SpeechRecognitionEngine(
       scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory,
       const std::string& accept_language);
-  ~SpeechRecognitionEngine();
+  ~SpeechRecognitionEngine() override;
 
   // Sets the URL requests are sent to for tests.
   static void set_web_service_base_url_for_tests(
@@ -120,8 +126,8 @@ class CONTENT_EXPORT SpeechRecognitionEngine {
   int GetDesiredAudioChunkDurationMs() const;
 
  private:
-  class UpstreamLoader;
-  class DownstreamLoader;
+  friend class speech::UpstreamLoaderClient;
+  friend class speech::DownstreamLoader;
 
   Delegate* delegate_;
 
@@ -172,10 +178,12 @@ class CONTENT_EXPORT SpeechRecognitionEngine {
     DISALLOW_COPY_AND_ASSIGN(FSMEventArgs);
   };
 
-  void OnUpstreamDataComplete(bool success, int response_code);
+  // speech::UpstreamLoaderClient
+  void OnUpstreamDataComplete(bool success, int response_code) override;
 
-  void OnDownstreamDataReceived(base::StringPiece new_response_data);
-  void OnDownstreamDataComplete(bool success, int response_code);
+  // speech::DownstreamLoaderClient
+  void OnDownstreamDataReceived(base::StringPiece new_response_data) override;
+  void OnDownstreamDataComplete(bool success, int response_code) override;
 
   // Entry point for pushing any new external event into the recognizer FSM.
   void DispatchEvent(const FSMEventArgs& event_args);
@@ -205,8 +213,8 @@ class CONTENT_EXPORT SpeechRecognitionEngine {
   void UploadAudioChunk(const std::string& data, FrameType type, bool is_final);
 
   Config config_;
-  std::unique_ptr<UpstreamLoader> upstream_loader_;
-  std::unique_ptr<DownstreamLoader> downstream_loader_;
+  std::unique_ptr<speech::UpstreamLoader> upstream_loader_;
+  std::unique_ptr<speech::DownstreamLoader> downstream_loader_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
   const std::string accept_language_;
   std::unique_ptr<AudioEncoder> encoder_;
