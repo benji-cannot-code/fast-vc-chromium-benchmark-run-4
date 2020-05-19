eb3af93e7b93a8972748a42e3900d729d7900477
danakj <danakj@chromium.org>
2020-05-19 21:12:23 +0000

    Attempt to deflake webgl-composite-modes-tabswitching.html
    The test is failing because it is performing a compositor frame while
    hidden. The test uses runAfterLayoutAndPaint() in ways it does not need
    to, so move it to use requestAnimationFrame() directly to see if that
    helps. While doing so, I noticed that the triangles drawn while hidden
    are not lost if a requestAnimationFrame() draws while inside the event
    listener for page visibility. That is now documented in a comment, with
    a double call to requestAnimationFrame() to work around it.
    
    R=kbr@chromium.org
    
    Bug: 1083585
    Change-Id: Iefb9a49f7ef337b86f9de898fa71a96cf65d0513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209331
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770286}

--- third_party/blink/web_tests/resources/visibility.js
+++ third_party/blink/web_tests/resources/visibility.js
@@ -2,11 +2,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 function setMainWindowHidden(hidden) {
-  if (window.testRunner)
-    testRunner.setMainWindowHidden(hidden);
   return new Promise((resolve, reject) => {
-    if (document.visibilityState == (hidden ? "hidden" : "visible"))
+    if (!window.testRunner) {
+      reject("no window.testRunner present");
+      return;
+    }
+    if (document.visibilityState == (hidden ? "hidden" : "visible")) {
       reject("setMainWindowHidden(" + hidden + ") called but already " + hidden);
+      return;
+    }
     document.addEventListener("visibilitychange", resolve, {once:true});
+    testRunner.setMainWindowHidden(hidden);
   });
 }
