d07635379c5c3289b12188be1f485da0a506c692
Nikita Podguzov <nikitapodguzov@chromium.org>
2020-05-19 22:27:25 +0000

    Policy: Disable "Save link as" button if it violates URLBlacklist policy
    Bug: 1079177
    Change-Id: I99fa1ae741d13ea808e0a92c119de042a72e5cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207453
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Nikita Podguzov <nikitapodguzov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770329}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/content/browser/content_password_manager_driver.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
+#include "components/policy/content/policy_blacklist_service.h"
 #include "components/prefs/pref_member.h"
 #include "components/prefs/pref_service.h"
 #include "components/search_engines/template_url.h"
@@ -2575,6 +2576,13 @@ bool RenderViewContextMenu::IsSaveLinkAsEnabled() const {
   // Test if file-selection dialogs are forbidden by policy.
   if (!local_state->GetBoolean(prefs::kAllowFileSelectionDialogs))
     return false;
+
+  PolicyBlacklistService* service =
+      PolicyBlacklistFactory::GetForBrowserContext(browser_context_);
+  if (service->GetURLBlacklistState(params_.link_url) ==
+      policy::URLBlacklist::URLBlacklistState::URL_IN_BLACKLIST) {
+    return false;
+  }
 
   return params_.link_url.is_valid() &&
       ProfileIOData::IsHandledProtocol(params_.link_url.scheme());
