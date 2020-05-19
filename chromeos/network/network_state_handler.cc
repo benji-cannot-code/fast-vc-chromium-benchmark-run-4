fff838b63cdb1e64e0caf37a27d1fcfff9313255
Steven Bennetts <stevenjb@chromium.org>
2020-05-19 22:43:00 +0000

    CrOS Networking: Use Metered for detecting metered networks
    The Shill algorithm for identifying metered networks is slightly more
    accurate than the Chrome logic, and has the potential to improve
    further in the future.
    
    There is also the possibility of allowing users to override this
    property, in which case we will want to use the override value.
    
    Bug: 1078427
    Change-Id: I85ad477a27e981be45ec77e8d060eb299bc78065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2186112
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770336}

--- chromeos/network/network_state_handler.cc
+++ chromeos/network/network_state_handler.cc
@@ -900,7 +900,7 @@ void NetworkStateHandler::SetTetherNetworkStateConnecting(
     NET_LOG(EVENT) << "Connecting to Tether network when there is currently no "
                    << "default network; setting as new default network. GUID: "
                    << guid;
-    default_network_path_ = guid;
+    SetDefaultNetworkValues(guid, /*metered=*/true);
   }
 
   SetTetherNetworkStateConnectionState(guid, shill::kStateConfiguration);
@@ -944,7 +944,7 @@ void NetworkStateHandler::SetTetherNetworkStateConnectionState(
                    << "New state: " << connection_state;
     if (!tether_network_state->IsConnectingOrConnected() &&
         tether_network_state->path() == default_network_path_) {
-      default_network_path_.clear();
+      SetDefaultNetworkValues(/*path=*/std::string(), /*metered=*/false);
       NotifyDefaultNetworkChanged(kReasonTether);
     }
     OnNetworkConnectionStateChanged(tether_network_state);
@@ -1361,10 +1361,16 @@ void NetworkStateHandler::UpdateNetworkStateProperties(
   bool network_property_updated = false;
   std::string prev_connection_state = network->connection_state();
   bool prev_is_captive_portal = network->is_captive_portal();
+  bool metered = false;
   for (const auto iter : properties.DictItems()) {
     if (network->PropertyChanged(iter.first, iter.second))
       network_property_updated = true;
+    if (iter.first == shill::kMeteredProperty)
+      metered = iter.second.is_bool() && iter.second.GetBool();
   }
+  if (network->path() == default_network_path_)
+    default_network_is_metered_ = metered && network->IsConnectedState();
+
   if (network->Matches(NetworkTypePattern::WiFi()))
     network_property_updated |= UpdateBlockedByPolicy(network);
   network_property_updated |= network->InitialPropertiesReceived(properties);
@@ -1715,7 +1721,7 @@ void NetworkStateHandler::DefaultNetworkServiceChanged(
                  << NetworkPathId(service_path);
   if (new_service_path.empty()) {
     // Notify that there is no default network.
-    default_network_path_.clear();
+    SetDefaultNetworkValues(/*path=*/std::string(), /*metered=*/false);
     NotifyDefaultNetworkChanged(kReasonChange);
     return;
   }
@@ -1726,7 +1732,8 @@ void NetworkStateHandler::DefaultNetworkServiceChanged(
     // they will be notified when the state is received.
     NET_LOG(EVENT) << "Default NetworkState not available: "
                    << NetworkPathId(service_path);
-    default_network_path_ = service_path;
+    // Metered will be updated to the correct value when properties arrive.
+    SetDefaultNetworkValues(service_path, /*metered=*/false);
     return;
   }
 
@@ -1738,8 +1745,8 @@ void NetworkStateHandler::DefaultNetworkServiceChanged(
     // default network to be the associated Tether network instead.
     network = GetNetworkStateFromGuid(network->tether_guid());
     if (default_network_path_ != network->path()) {
-      default_network_path_ = network->path();
       NET_LOG(DEBUG) << "Tether network is default: " << NetworkId(network);
+      SetDefaultNetworkValues(network->path(), /*metered=*/true);
       NotifyDefaultNetworkChanged(kReasonChange);
     }
     return;
@@ -1747,7 +1754,8 @@ void NetworkStateHandler::DefaultNetworkServiceChanged(
 
   // Request the updated default network properties which will trigger
   // NotifyDefaultNetworkChanged().
-  default_network_path_ = service_path;
+  // Metered will be updated to the correct value when properties arrive.
+  SetDefaultNetworkValues(service_path, /*metered=*/false);
   RequestUpdateForNetwork(service_path);
 }
 
@@ -2099,6 +2107,12 @@ std::vector<std::string> NetworkStateHandler::GetTechnologiesForType(
 
   CHECK_GT(technologies.size(), 0ul);
   return technologies;
+}
+
+void NetworkStateHandler::SetDefaultNetworkValues(const std::string& path,
+                                                  bool metered) {
+  default_network_path_ = path;
+  default_network_is_metered_ = metered;
 }
 
 }  // namespace chromeos
