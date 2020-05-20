e04f556d14e84c6aa7e04168d7d3f01043f3cfda
Mei Liang <meiliang@chromium.org>
2020-05-20 00:12:44 +0000

    Extend MessageCardProvider component to support grouping suggestion
    This CL modifies the TabSuggestionMessageCardViewModel to build a
    PropertyModel based on the TabSuggestionAction.
    
    In addition, this CL updates the relationship between the shown Message
    and the available Message in MessageCardProviderMediator. Prior to this
    change, each MessageType is allowed to live in either the shown Message
    queue or the available Message queue, not both. After this CL, each
    MessageType can live in both. The available Message for the same
    MessageType can be queued up, and each MessageType will have one Shown
    Message.
    
    Change-Id: Icb57147823bbcf81679d38c600d894ed6e47317c
    Bug: 1076538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196845
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770370}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/MessageCardProviderTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/MessageCardProviderTest.java
@@ -29,6 +29,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.tasks.tab_management.suggestions.TabSuggestion;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.chrome.test.util.browser.Features;
 import org.chromium.content_public.browser.test.util.CriteriaHelper;
@@ -42,7 +43,7 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * Integration tests for TabGridMessageCardProvider component.
+ * Integration tests for MessageCardProvider component.
  */
 @Features.DisableFeatures(ChromeFeatureList.TAB_TO_GTS_ANIMATION)
 public class MessageCardProviderTest extends DummyUiActivityTestCase {
@@ -136,6 +137,9 @@ public class MessageCardProviderTest extends DummyUiActivityTestCase {
         mCoordinator = new MessageCardProviderCoordinator(getActivity(), mUiDismissActionProvider);
         mCoordinator.subscribeMessageService(mTestingService);
         mCoordinator.subscribeMessageService(mSuggestionService);
+
+        when(mTabSuggestionMessageData.getActionType())
+                .thenReturn(TabSuggestion.TabSuggestionAction.CLOSE);
     }
 
     @Test
