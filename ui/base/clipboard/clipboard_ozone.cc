e768cf4611a4855aa5f2cc02768485504c0fb704
Darwin Huang <huangdarwin@chromium.org>
2020-05-20 02:05:14 +0000

    ui/base/clipboard: Rename AuraClipboard to ClipboardNonBacked.
    This is more accurate, as AuraClipboard is no longer tied to Aura.
    
    Changes include:
    - Filenames: clipboard_aura -> clipboard_non_backed
    - Classnames: ClipboardAura -> ClipboardNonBacked
                  AuraClipboard -> ClipboardInternal
    
    Renaming change. No intended functionality changes.
    
    Bug: 990033
    Change-Id: Iaf81975909d7184649f1532333347447a557df4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203642
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770415}

--- ui/base/clipboard/clipboard_ozone.cc
+++ ui/base/clipboard/clipboard_ozone.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_CHROMEOS) && BUILDFLAG(OZONE_PLATFORM_X11)
 #include "base/command_line.h"
-#include "ui/base/clipboard/clipboard_aura.h"
+#include "ui/base/clipboard/clipboard_non_backed.h"
 #include "ui/base/ui_base_switches.h"
 #endif
 
@@ -297,12 +297,12 @@ class ClipboardOzone::AsyncClipboardOzone {
 
 // Clipboard factory method.
 Clipboard* Clipboard::Create() {
-// linux-chromeos uses aura clipboard by default, but supports ozone x11
+// linux-chromeos uses non-backed clipboard by default, but supports ozone x11
 // with flag --use-system-clipbboard.
 #if defined(OS_CHROMEOS) && BUILDFLAG(OZONE_PLATFORM_X11)
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kUseSystemClipboard)) {
-    return new ClipboardAura;
+    return new ClipboardNonBacked;
   }
 #endif
   return new ClipboardOzone;
@@ -470,8 +470,8 @@ void ClipboardOzone::WritePortableRepresentations(ClipboardBuffer buffer,
 
   async_clipboard_ozone_->OfferData(buffer);
 
-  // Just like Aura/X11 implementation does, copy text data from the copy/paste
-  // selection to the primary selection.
+  // Just like Non-Backed/X11 implementation does, copy text data from the
+  // copy/paste selection to the primary selection.
   if (buffer == ClipboardBuffer::kCopyPaste) {
     auto text_iter = objects.find(PortableFormat::kText);
     if (text_iter != objects.end()) {
