2e6bd1fd4ffc47c1902c746d8ae5ed9c4d31e9dc
Anders Hartvoll Ruud <andruud@chromium.org>
2020-05-20 17:22:10 +0000

    [mpc] Limit number of dependencies in cache
    Enforce a limit on how many dependencies a single entry can hold.
    It's common to define hundreds of custom properties at :root, and
    we do not want to cache such entries for memory reasons.
    
    I've experimented with YouTube and other sites, and 8 dependencies is
    enough to catch ~90% of all cases, while avoiding extremes such as
    137 dependencies (which was observed for YouTube).
    
    Bug: 1057072
    Change-Id: Ia92d6c53a29bb3784a491fc2efbcef228d5a8caa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208985
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770645}

--- third_party/blink/renderer/core/css/resolver/matched_properties_cache.cc
+++ third_party/blink/renderer/core/css/resolver/matched_properties_cache.cc
@@ -65,6 +65,7 @@ void CachedMatchedProperties::Set(
   DCHECK(RuntimeEnabledFeatures::MPCDependenciesEnabled() ||
          dependencies.IsEmpty());
   if (dependencies.size()) {
+    DCHECK(dependencies.size() <= MatchedPropertiesCache::kMaxDependencies);
     // Plus one for g_null_atom.
     this->dependencies =
         std::make_unique<AtomicString[]>(dependencies.size() + 1);
@@ -254,7 +255,8 @@ bool MatchedPropertiesCache::IsCacheable(const StyleResolverState& state) {
     return true;
   }
 
-  return !state.HasIncomparableDependency();
+  return state.Dependencies().size() <= kMaxDependencies &&
+         !state.HasIncomparableDependency();
 }
 
 void MatchedPropertiesCache::Trace(Visitor* visitor) const {
