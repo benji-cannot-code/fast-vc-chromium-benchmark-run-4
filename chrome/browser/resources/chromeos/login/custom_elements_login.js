3c2c62759f2306903adaaad7bae8d36703808631
Denis Kuznetsov <antrim@google.com>
2020-05-20 11:04:07 +0000

    OOBE: Update device disabled screen according to mocks
    Bug: 1007876
    Change-Id: If20a2e559c49d5df22e71b2a49ad74c4fbd9b460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209065
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770539}

--- chrome/browser/resources/chromeos/login/custom_elements_login.js
+++ chrome/browser/resources/chromeos/login/custom_elements_login.js
@@ -50,3 +50,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="security_token_pin.js">
 // <include src="gesture_navigation.js">
 // <include src="screen_tpm_error.js">
+// <include src="device_disabled.js">
--- chrome/browser/resources/chromeos/login/oobe.js
+++ chrome/browser/resources/chromeos/login/oobe.js
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="screen_wrong_hwid.js">
 // <include src="screen_confirm_password.js">
 // <include src="screen_fatal_error.js">
-// <include src="screen_device_disabled.js">
 // <include src="screen_active_directory_password_change.js">
 // <include src="screen_encryption_migration.js">
 // <include src="screen_update_required.js">
@@ -83,7 +82,6 @@ cr.define('cr.ui.Oobe', function() {
       login.AppLaunchSplashScreen.register();
       login.ConfirmPasswordScreen.register();
       login.FatalErrorScreen.register();
-      login.DeviceDisabledScreen.register();
       login.ActiveDirectoryPasswordChangeScreen.register(/* lazyInit= */ true);
       login.SupervisionTransitionScreen.register();
       login.DemoSetupScreen.register();
