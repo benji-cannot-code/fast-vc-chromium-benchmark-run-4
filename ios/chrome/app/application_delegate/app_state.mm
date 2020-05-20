bd91c6952c86227ca80e5384902dffb94487d0a9
Stepan Khapugin <stkhapugin@chromium.org>
2020-05-20 16:55:29 +0000

    [multiball] Fix the bug with modal UI.
    The first scene should not show the overlay when there's no blocking UI.
    
    Bug: 1084493
    Change-Id: I9d760dc12e5bb07f797f488d470f17484d4080ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210244
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770638}

--- ios/chrome/app/application_delegate/app_state.mm
+++ ios/chrome/app/application_delegate/app_state.mm
@@ -619,6 +619,7 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
       [self.observers appState:self
            firstSceneActivated:sceneDelegate.sceneState];
       sceneDelegate.sceneState.presentingModalOverlay =
+          self.sceneShowingBlockingUI &&
           (self.sceneShowingBlockingUI != sceneDelegate.sceneState);
     }
   }
