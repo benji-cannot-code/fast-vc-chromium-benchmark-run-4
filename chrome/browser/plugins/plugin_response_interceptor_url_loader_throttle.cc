7f5eaf5849fb9ad473880ae8b1500773baa0b968
Eric Lawrence <ericlaw@microsoft.com>
2020-05-20 06:00:11 +0000

    [PDF] Ignore Content-Security-Policy on PDF responses
    Starting around Chrome 76, the browser started respecting CSP headers on
    PDF file responses. This broke display of PDFs because the
    MimeHandlerViewAttachHelper::OverrideBodyForInterceptedResponse function
    that generates a wrapper page around the PDF plugin embed uses inline
    styles to size the embed, and these inline styles may be forbidden by
    the server's CSP.
    
    To correct that, strip Content-Security-Policy headers from PDF
    responses, because the PDF plugin does not use the headers anyway.
    
    Bug: 271452
    Change-Id: I58b2763e34b7d614e88059bf92b16e9cdf2d5fd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176415
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Eric Lawrence [MSFT] <ericlaw@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#770476}

--- chrome/browser/plugins/plugin_response_interceptor_url_loader_throttle.cc
+++ chrome/browser/plugins/plugin_response_interceptor_url_loader_throttle.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/plugins/plugin_response_interceptor_url_loader_throttle.h"
 
+#include <utility>
+
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/guid.h"
@@ -17,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/transferrable_url_loader.mojom.h"
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_attach_helper.h"
+#include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest_handlers/mime_types_handler.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -55,6 +58,14 @@ void PluginResponseInterceptorURLLoaderThrottle::WillProcessResponse(
 
   if (extension_id.empty())
     return;
+
+  // Chrome's PDF Extension does not work properly in the face of a restrictive
+  // Content-Security-Policy, and does not currently respect the policy anyway.
+  // Ignore CSP served on a PDF response. https://crbug.com/271452
+  if (extension_id == extension_misc::kPdfExtensionId &&
+      response_head->headers) {
+    response_head->headers->RemoveHeader("Content-Security-Policy");
+  }
 
   MimeTypesHandler::ReportUsedHandler(extension_id);
 
