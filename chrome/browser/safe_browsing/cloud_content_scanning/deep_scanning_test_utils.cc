765936e69c832c02557b8fbdc405c1cf0a70c556
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-05-20 20:29:17 +0000

    Reuse upload browsertests for corresponding Enterprise Connectors
    This reuses tests by having the policy setters of
    DeepScanningBrowserTestBase set either legacy or Connector policies, and
    then making every upload browsertest parametrized with a bool so both
    policy sets get tested.
    
    The DeepScanningDialogViews test also change a bit, the ScanType test
    parameter is removed since this dialog doesn't depend on the type of
    scanning being done. This avoids having a very large number of
    combinations for that test that don't cover different code paths.
    
    Bug: 1076083
    Change-Id: I419b21de0095add8b5b214993be010d022941202
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210782
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770745}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.cc
@@ -32,6 +32,8 @@ base::Value MakeListValue(const std::vector<std::string>& elements) {
 base::Value DefaultConnectorSettings() {
   base::Value settings(base::Value::Type::DICTIONARY);
 
+  settings.SetKey(enterprise_connectors::kKeyServiceProvider,
+                  base::Value("google"));
   settings.SetKey(enterprise_connectors::kKeyEnable,
                   base::Value(base::Value::Type::LIST));
   settings.SetKey(enterprise_connectors::kKeyDisable,
@@ -74,9 +76,47 @@ void AddConnectorUrlPattern(enterprise_connectors::AnalysisConnector connector,
   list->Append(std::move(list_element));
 }
 
+void ClearConnectorUrlPattern(
+    enterprise_connectors::AnalysisConnector connector,
+    bool enable,
+    base::Value tags) {
+  ListPrefUpdate settings_list(g_browser_process->local_state(),
+                               ConnectorPref(connector));
+  DCHECK(settings_list.Get());
+  if (settings_list->empty())
+    return;
+
+  base::Value* settings = nullptr;
+  DCHECK(settings_list->Get(0, &settings));
+  DCHECK(settings);
+  DCHECK(settings->is_dict());
+
+  base::Value* list =
+      settings->FindListPath(enable ? enterprise_connectors::kKeyEnable
+                                    : enterprise_connectors::kKeyDisable);
+  if (!list)
+    return;
+
+  DCHECK(list->is_list());
+  for (const base::Value& pattern : list->GetList()) {
+    DCHECK(pattern.is_dict());
+
+    const base::Value* pattern_tags =
+        pattern.FindKey(enterprise_connectors::kKeyTags);
+    if (!pattern_tags)
+      continue;
+
+    DCHECK(pattern_tags->is_list());
+    if (*pattern_tags == tags) {
+      list->EraseListValue(pattern);
+    }
+  }
+}
+
+template <typename T>
 void SetConnectorField(enterprise_connectors::AnalysisConnector connector,
                        const char* key,
-                       bool value) {
+                       T value) {
   InitConnectorPrefIfEmpty(connector);
   ListPrefUpdate settings_list(g_browser_process->local_state(),
                                ConnectorPref(connector));
@@ -84,7 +124,7 @@ void SetConnectorField(enterprise_connectors::AnalysisConnector connector,
   DCHECK(settings_list->Get(0, &settings));
   DCHECK(settings);
   DCHECK(settings->is_dict());
-  settings->SetKey(key, base::Value(std::move(value)));
+  DCHECK(settings->SetKey(key, base::Value(std::move(value))));
 }
 
 }  // namespace
@@ -354,7 +394,6 @@ void SetDlpPolicyForConnectors(CheckContentComplianceValues state) {
   // wildcard pattern should also be included in the disable list if the policy
   // is disabled for downloads since no scan can occur with the legacy policy
   // when it is disabled.
-
   bool enable_uploads =
       state == CHECK_UPLOADS || state == CHECK_UPLOADS_AND_DOWNLOADS;
 
@@ -406,10 +445,11 @@ void SetMalwarePolicyForConnectors(SendFilesForMalwareCheckValues state) {
 
 void SetDelayDeliveryUntilVerdictPolicyForConnectors(
     DelayDeliveryUntilVerdictValues state) {
-  bool delay_uploads =
-      state == DELAY_UPLOADS || state == DELAY_UPLOADS_AND_DOWNLOADS;
-  bool delay_downloads =
-      state == DELAY_DOWNLOADS || state == DELAY_UPLOADS_AND_DOWNLOADS;
+  int delay_uploads =
+      (state == DELAY_UPLOADS || state == DELAY_UPLOADS_AND_DOWNLOADS) ? 1 : 0;
+  int delay_downloads =
+      (state == DELAY_DOWNLOADS || state == DELAY_UPLOADS_AND_DOWNLOADS) ? 1
+                                                                         : 0;
   SetConnectorField(enterprise_connectors::AnalysisConnector::BULK_DATA_ENTRY,
                     enterprise_connectors::kKeyBlockUntilVerdict,
                     delay_uploads);
@@ -429,10 +469,10 @@ void SetAllowPasswordProtectedFilesPolicyForConnectors(
       state == ALLOW_DOWNLOADS || state == ALLOW_UPLOADS_AND_DOWNLOADS;
   SetConnectorField(enterprise_connectors::AnalysisConnector::FILE_ATTACHED,
                     enterprise_connectors::kKeyBlockPasswordProtected,
-                    allow_uploads);
+                    !allow_uploads);
   SetConnectorField(enterprise_connectors::AnalysisConnector::FILE_DOWNLOADED,
                     enterprise_connectors::kKeyBlockPasswordProtected,
-                    allow_downloads);
+                    !allow_downloads);
 }
 
 void SetBlockUnsupportedFileTypesPolicyForConnectors(
@@ -496,6 +536,20 @@ void AddUrlsToNotCheckForMalwareOfDownloadsForConnectors(
   AddConnectorUrlPattern(
       enterprise_connectors::AnalysisConnector::FILE_DOWNLOADED, false,
       MakeListValue(urls), MakeListValue({"malware"}));
+}
+
+void ClearUrlsToCheckComplianceOfDownloadsForConnectors() {
+  ClearConnectorUrlPattern(
+      enterprise_connectors::AnalysisConnector::FILE_DOWNLOADED, true,
+      MakeListValue({"dlp"}));
+}
+
+void ClearUrlsToCheckForMalwareOfUploadsForConnectors() {
+  for (auto connector :
+       {enterprise_connectors::AnalysisConnector::FILE_ATTACHED,
+        enterprise_connectors::AnalysisConnector::BULK_DATA_ENTRY}) {
+    ClearConnectorUrlPattern(connector, true, MakeListValue({"malware"}));
+  }
 }
 
 }  // namespace safe_browsing
