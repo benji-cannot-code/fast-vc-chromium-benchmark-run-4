2ce9b6b628c93aeb70b29c14f0c0ec9d5805682f
Minoru Chikamune <chikamune@chromium.org>
2020-05-20 22:25:31 +0000

    Fix Null-dereference READ in scoped_refptr<mojo::SimpleWatcher::Context>::operator bool
    ClusterFuzz found null-dereference READ in
    scoped_refptr<mojo::SimpleWatcher::Context>::operator bool.
    This bug already existed before HeapMojo wrappers were introduced.
    We are not sure the root cause, but HeapMojo wrappers could sanitize
    the issue.
    
    Bug: 1049056, 1079286
    Change-Id: I9f0fae6921024525891841bc942ac3a521a0a015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208780
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770808}

--- third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h
+++ third_party/blink/renderer/modules/filesystem/file_system_dispatcher.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_unique_receiver_set.h"
-#include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace WTF {
@@ -201,17 +200,12 @@ class FileSystemDispatcher : public GarbageCollected<FileSystemDispatcher>,
 
   void Prefinalize();
 
-  HeapMojoRemote<mojom::blink::FileSystemManager,
-                 HeapMojoWrapperMode::kWithoutContextObserver>
-      file_system_manager_;
+  HeapMojoRemote<mojom::blink::FileSystemManager> file_system_manager_;
   using OperationsMap =
       HashMap<int, mojo::Remote<mojom::blink::FileSystemCancellableOperation>>;
   OperationsMap cancellable_operations_;
   int next_operation_id_;
-  HeapMojoUniqueReceiverSet<
-      mojom::blink::FileSystemOperationListener,
-      std::default_delete<mojom::blink::FileSystemOperationListener>,
-      HeapMojoWrapperMode::kWithoutContextObserver>
+  HeapMojoUniqueReceiverSet<mojom::blink::FileSystemOperationListener>
       op_listeners_;
 };
 
