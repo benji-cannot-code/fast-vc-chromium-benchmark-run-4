c4746759a5f3408251a748871b5f1005b7acf473
Evan Stade <estade@chromium.org>
2020-05-20 18:02:20 +0000

    Componentize ForegroundServiceUtils.java
    It's needed for media session notifications.
    
    Bug: 1066263
    Change-Id: I9239ac5cbe7b8d6f071a8aa019947b606a55158f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209116
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770671}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundService.java
@@ -23,7 +23,7 @@ import androidx.core.app.ServiceCompat;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
-import org.chromium.chrome.browser.notifications.ForegroundServiceUtils;
+import org.chromium.components.browser_ui.notifications.ForegroundServiceUtils;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
