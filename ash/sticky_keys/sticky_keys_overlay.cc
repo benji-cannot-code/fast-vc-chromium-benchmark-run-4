514b6129604942b41919828995571debd6a309d0
Wei Li <weili@chromium.org>
2020-05-20 17:16:49 +0000

    Let sticky keys overlay widget to own its view
    Sticky key overlay view is always created and used along with the
    overlay widget. It is better to be owned by the widget instead of
    using set_owned_by_client() to maintain the ownership.
    
    BUG=1044687
    
    Change-Id: If179b9316b25d3d21ed03166aece9fdcea4b5c40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207575
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770643}

--- ash/sticky_keys/sticky_keys_overlay.cc
+++ ash/sticky_keys/sticky_keys_overlay.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/sticky_keys/sticky_keys_overlay.h"
 
+#include <memory>
+
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "ash/sticky_keys/sticky_keys_controller.h"
@@ -101,11 +103,7 @@ void StickyKeyOverlayLabel::SetKeyState(StickyKeyState state) {
 //  StickyKeysOverlayView
 class StickyKeysOverlayView : public views::View {
  public:
-  // This object is not owned by the views hiearchy or by the widget. The
-  // StickyKeysOverlay is the only class that should create and destroy this
-  // view.
   StickyKeysOverlayView();
-
   ~StickyKeysOverlayView() override;
 
   // views::View overrides:
@@ -128,9 +126,6 @@ class StickyKeysOverlayView : public views::View {
 };
 
 StickyKeysOverlayView::StickyKeysOverlayView() {
-  // The parent StickyKeysOverlay object owns this view.
-  set_owned_by_client();
-
   const gfx::Font& font =
       ui::ResourceBundle::GetSharedInstance().GetFont(kKeyLabelFontStyle);
   int font_size = font.GetFontSize();
@@ -203,10 +198,10 @@ void StickyKeysOverlayView::AddKeyLabel(ui::EventFlags modifier,
 
 ///////////////////////////////////////////////////////////////////////////////
 //  StickyKeysOverlay
-StickyKeysOverlay::StickyKeysOverlay()
-    : is_visible_(false),
-      overlay_view_(new StickyKeysOverlayView),
-      widget_size_(overlay_view_->GetPreferredSize()) {
+StickyKeysOverlay::StickyKeysOverlay() {
+  auto overlay_view = std::make_unique<StickyKeysOverlayView>();
+  widget_size_ = overlay_view->GetPreferredSize();
+
   views::Widget::InitParams params;
   params.type = views::Widget::InitParams::TYPE_POPUP;
   params.opacity = views::Widget::InitParams::WindowOpacity::kTranslucent;
@@ -219,7 +214,7 @@ StickyKeysOverlay::StickyKeysOverlay()
   overlay_widget_.reset(new views::Widget);
   overlay_widget_->Init(std::move(params));
   overlay_widget_->SetVisibilityChangedAnimationsEnabled(false);
-  overlay_widget_->SetContentsView(overlay_view_.get());
+  overlay_view_ = overlay_widget_->SetContentsView(std::move(overlay_view));
   overlay_widget_->GetNativeView()->SetName("StickyKeysOverlay");
 }
 
