0fc5a13f0b26d490faae9e3a68640eb6cdc8f9cd
Kyle Horimoto <khorimoto@chromium.org>
2020-05-20 00:38:33 +0000

    [CrOS Settings] Enable new search by default
    We still have some polish work to go, but we should be ready for more
    feedback now that we've addressed all of the obvious bugs.
    
    Change-Id: I52997f9acf6426f39eb7227aebad4d371f00bf5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208025
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770377}

--- chrome/browser/ui/webui/settings/chromeos/search/search_tag_registry_unittest.cc
+++ chrome/browser/ui/webui/settings/chromeos/search/search_tag_registry_unittest.cc
@@ -6,13 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/chromeos/search/search_tag_registry.h"
 
 #include "base/no_destructor.h"
-#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/chromeos/local_search_service/index.h"
 #include "chrome/browser/chromeos/local_search_service/local_search_service.h"
 #include "chrome/browser/ui/webui/settings/chromeos/constants/routes.mojom.h"
 #include "chrome/browser/ui/webui/settings/chromeos/search/search_concept.h"
 #include "chrome/grit/generated_resources.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chromeos {
@@ -59,13 +57,10 @@ class SearchTagRegistryTest : public testing::Test {
 
   // testing::Test:
   void SetUp() override {
-    scoped_feature_list_.InitAndEnableFeature(
-        chromeos::features::kNewOsSettingsSearch);
     index_ = local_search_service_.GetIndex(
         local_search_service::IndexId::kCrosSettings);
   }
 
-  base::test::ScopedFeatureList scoped_feature_list_;
   local_search_service::LocalSearchService local_search_service_;
   SearchTagRegistry search_tag_registry_;
   local_search_service::Index* index_;
