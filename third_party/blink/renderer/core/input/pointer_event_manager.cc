38bb54cf6dfc52027e2250f168e764dd6fd46a35
Eric Willigers <ericwilligers@chromium.org>
2020-05-20 14:48:43 +0000

    Retire UnifiedTouchAdjustment flag
    The Blink flag UnifiedTouchAdjustment has been stable since 2018-05.
    
    Bug: 625198
    Change-Id: I7ee53c7c1953df4d14f952378905b3572b57a9a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208836
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770584}

--- third_party/blink/renderer/core/input/pointer_event_manager.cc
+++ third_party/blink/renderer/core/input/pointer_event_manager.cc
@@ -343,8 +343,7 @@ bool PointerEventManager::ShouldAdjustPointerEvent(
       !frame_->GetSettings()->GetTouchAdjustmentEnabled())
     return false;
 
-  return RuntimeEnabledFeatures::UnifiedTouchAdjustmentEnabled() &&
-         pointer_event.pointer_type ==
+  return pointer_event.pointer_type ==
              WebPointerProperties::PointerType::kTouch &&
          pointer_event.GetType() == WebInputEvent::Type::kPointerDown &&
          pointer_event_factory_.IsPrimary(pointer_event);
