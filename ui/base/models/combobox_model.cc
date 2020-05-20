b1b7188a7c4989f6a7a4180ad5760cf28b6c6d0c
Mohamed Amir Yosef <mamir@chromium.org>
2020-05-20 23:47:11 +0000

    [UI] Combobox can show different text and icon from the dropdown
    This CL adds support to the Combobox model to show different text
    and icon inside the combobox from that shown inside the dropdown menu.
    
    Bug: 1044038
    Change-Id: Iad33be358e24f212db92fd5e774d52ec9bdd772d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207168
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770846}

--- ui/base/models/combobox_model.cc
+++ ui/base/models/combobox_model.cc
@@ -9,12 +9,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
+base::string16 ComboboxModel::GetDropDownTextAt(int index) {
+  return GetItemAt(index);
+}
+
 base::string16 ComboboxModel::GetDropDownSecondaryTextAt(int index) const {
   return base::string16();
 }
 
 ImageModel ComboboxModel::GetIconAt(int index) const {
   return ui::ImageModel();
+}
+
+ImageModel ComboboxModel::GetDropDownIconAt(int index) const {
+  return GetIconAt(index);
 }
 
 bool ComboboxModel::IsItemSeparatorAt(int index) {
