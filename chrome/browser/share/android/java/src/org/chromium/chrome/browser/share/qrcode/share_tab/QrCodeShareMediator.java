1999eb943675a519b2fcb18c4a4c8f60c7c9eef4
Gayane Petrosyan <gayane@chromium.org>
2020-05-20 23:28:54 +0000

    [QRCode Android] Fix crash getting current url.
    Bug: 1085209
    Change-Id: I9868b9a7012e9dc7d23a710fea21186d8b73107a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210798
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770828}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/share_tab/QrCodeShareMediator.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/qrcode/share_tab/QrCodeShareMediator.java
@@ -21,6 +21,7 @@ import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.share.SaveImageNotificationManager;
 import org.chromium.chrome.browser.share.qrcode.QRCodeGenerationRequest;
+import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.browser_ui.share.ShareImageFileUtils;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.widget.Toast;
@@ -50,8 +51,11 @@ class QrCodeShareMediator implements ShareImageFileUtils.OnImageSaveListener {
 
         // TODO(crbug.com/1083351): Get URL from Sharing Hub.
         if (context instanceof ChromeActivity) {
-            mUrl = ((ChromeActivity) context).getActivityTabProvider().get().getUrl().getSpec();
-            refreshQrCode(mUrl);
+            Tab tab = ((ChromeActivity) context).getActivityTabProvider().get();
+            if (tab != null) {
+                mUrl = tab.getUrl().getSpec();
+                refreshQrCode(mUrl);
+            }
         }
     }
 
