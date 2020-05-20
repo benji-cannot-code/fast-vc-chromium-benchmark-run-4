4eeccacc74c1c03ef928056b050953d61a905127
Sorin Jianu <sorin@chromium.org>
2020-05-20 00:40:48 +0000

    Rename --ua to --wake for chrome/updater.
    This is a mechanical change to lexically remove references
    to --ua and UpdateApps and replace them with --wake and
    Wake.
    
    Bug: 1084010
    Change-Id: I1c0a8fa33f160f2d769549026725546c75c54854
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209619
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770381}

--- chrome/updater/updater.cc
+++ chrome/updater/updater.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "chrome/updater/app/app.h"
 #include "chrome/updater/app/app_uninstall.h"
-#include "chrome/updater/app/app_update_all.h"
+#include "chrome/updater/app/app_wake.h"
 #include "chrome/updater/configurator.h"
 #include "chrome/updater/constants.h"
 #include "chrome/updater/crash_client.h"
@@ -112,8 +112,8 @@ int HandleUpdaterCommands(const base::CommandLine* command_line) {
   if (command_line->HasSwitch(kUninstallSwitch))
     return AppUninstallInstance()->Run();
 
-  if (command_line->HasSwitch(kUpdateAppsSwitch)) {
-    return AppUpdateAllInstance()->Run();
+  if (command_line->HasSwitch(kWakeSwitch)) {
+    return AppWakeInstance()->Run();
   }
 
   VLOG(1) << "Unknown command line switch.";
