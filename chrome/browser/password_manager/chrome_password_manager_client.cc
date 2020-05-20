7c211dcff271dc79fd556f6f6bb2ce91168e34d8
Mustafa Emre Acer <meacer@chromium.org>
2020-05-20 02:11:18 +0000

    Record a histogram when the user focuses on a password field
    This CL records a new histogram called
    Navigation.PasswordFocus.SiteEngagementLevel the first time a user
    focuses on a password field.
    
    Bug: 1073604
    Change-Id: I0288ab5ab29955b25818e60af953c3c47cdb12df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208108
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Christopher Thompson <cthomp@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770419}

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/singleton.h"
 #include "base/metrics/field_trial.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "build/build_config.h"
@@ -198,20 +199,31 @@ class NavigationPasswordMetricsRecorder
   explicit NavigationPasswordMetricsRecorder(content::WebContents* web_contents)
       : web_contents_(web_contents) {}
 
+  void OnUserFocusedPasswordFieldFirstTime(
+      const GURL& main_frame_url) override {
+    RecordEngagementLevel(main_frame_url,
+                          "Security.PasswordFocus.SiteEngagementLevel");
+  }
+
   void OnUserModifiedPasswordFieldFirstTime(
       const GURL& main_frame_url) override {
+    RecordEngagementLevel(main_frame_url,
+                          "Security.PasswordEntry.SiteEngagementLevel");
+  }
+
+ private:
+  void RecordEngagementLevel(const GURL& main_frame_url,
+                             const char* histogram_name) {
     if (main_frame_url.SchemeIsHTTPOrHTTPS()) {
       SiteEngagementService* site_engagement_service =
           SiteEngagementService::Get(
               Profile::FromBrowserContext(web_contents_->GetBrowserContext()));
       blink::mojom::EngagementLevel engagement_level =
           site_engagement_service->GetEngagementLevel(main_frame_url);
-      UMA_HISTOGRAM_ENUMERATION("Security.PasswordEntry.SiteEngagementLevel",
-                                engagement_level);
+      base::UmaHistogramEnumeration(histogram_name, engagement_level);
     }
   }
 
- private:
   content::WebContents* web_contents_;
 };
 
