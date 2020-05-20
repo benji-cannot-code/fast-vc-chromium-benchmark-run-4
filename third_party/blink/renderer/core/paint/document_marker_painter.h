a9cba331a601b963616a6025ec5ca539174de998
Anupam Snigdha <snianu@microsoft.com>
2020-05-20 01:25:52 +0000

    Removed painting styleable marker for underline style none.
    On Windows, dictation comes as a composition and in some cases
    the underline style is set as TF_LS_NONE to differentiate between
    high confidence and low confidence words. This patch skips the
    painting of underline if the style is set as none.
    
    Bug: 1033764
    
    Change-Id: I68688cdf4318b5c0249b551efa06d1d3e910956f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208187
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770396}

--- third_party/blink/renderer/core/paint/document_marker_painter.h
+++ third_party/blink/renderer/core/paint/document_marker_painter.h
@@ -42,6 +42,7 @@ class DocumentMarkerPainter {
   static TextPaintStyle ComputeTextPaintStyleFrom(const ComputedStyle& style,
                                                   const TextMarkerBase& marker,
                                                   bool in_forced_colors_mode);
+  static bool ShouldPaintMarkerUnderline(const StyleableMarker& marker);
 };
 
 }  // namespace blink
