fad383e0ac3c9221482a20605e1e7f35e6eed812
Erik Chen <erikchen@chromium.org>
2020-05-20 01:09:11 +0000

    Prevent lacros from uninstalling on login.
    The lacros loader was being instantiated at the wrong location. It was
    called relatively early in the sign-in process, before feature flags are
    picked up. This means that the lacros loader detected that lacros was
    disabled, and would uninstall it and delete the user-data-dir. Shortly
    thereafter, the binary is redownloaded and the user-data-dir is remade.
    
    This CL moves the initializer to the right location.
    
    Bug: 1084701
    Change-Id: I3df9d4e652f2aa28ed42dd83604feb2931f75742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209497
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Erik Chen <erikchen@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770388}

--- chrome/browser/chromeos/login/session/user_session_initializer.h
+++ chrome/browser/chromeos/login/session/user_session_initializer.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/session_manager/core/session_manager_observer.h"
 #include "components/user_manager/user.h"
 
+class LacrosLoader;
 class Profile;
 
 namespace user_manager {
@@ -41,6 +42,7 @@ class UserSessionInitializer : public session_manager::SessionManagerObserver {
 
   // session_manager::SessionManagerObserver:
   void OnUserProfileLoaded(const AccountId& account_id) override;
+  void OnUserSessionStarted(bool is_primary_user) override;
 
   // Initialize child user profile services that depend on the policy.
   void InitializeChildUserServices(Profile* profile);
@@ -70,6 +72,8 @@ class UserSessionInitializer : public session_manager::SessionManagerObserver {
 
   // Initializes RLZ. If |disabled| is true, RLZ pings are disabled.
   void InitRlzImpl(Profile* profile, const RlzInitParams& params);
+
+  std::unique_ptr<LacrosLoader> lacros_loader_;
 
   base::OnceClosure init_rlz_impl_closure_for_testing_;
 
