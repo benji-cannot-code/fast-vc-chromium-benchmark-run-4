c5fdc8174b683721621fccf015df22b3cae965fc
Scott Little <sclittle@chromium.org>
2020-05-20 22:53:38 +0000

    Prevent integer overflow in translate::ReportForceTriggerOnEnglishPages()
    Previously, it was theoretically possible for this method to encounter
    an integer overflow. This CL prevents that by capping the value at max
    int.
    
    Bug: 1085171
    Change-Id: Ib4e7208a8f4322c3ccaedc092fe853593df2cb01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210934
    Auto-Submit: Scott Little <sclittle@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Reviewed-by: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770819}

--- components/translate/core/browser/translate_prefs.cc
+++ components/translate/core/browser/translate_prefs.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/translate/core/browser/translate_prefs.h"
 
 #include <algorithm>
+#include <limits>
 #include <memory>
 #include <set>
 #include <utility>
@@ -804,7 +805,7 @@ int TranslatePrefs::GetForceTriggerOnEnglishPagesCount() const {
 
 void TranslatePrefs::ReportForceTriggerOnEnglishPages() {
   int current_count = GetForceTriggerOnEnglishPagesCount();
-  if (current_count != -1)
+  if (current_count != -1 && current_count < std::numeric_limits<int>::max())
     prefs_->SetInteger(kForceTriggerTranslateCount, current_count + 1);
 }
 
--- components/translate/core/browser/translate_prefs_unittest.cc
+++ components/translate/core/browser/translate_prefs_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/translate/core/browser/translate_prefs.h"
 
 #include <algorithm>
+#include <limits>
 #include <string>
 #include <utility>
 #include <vector>
@@ -42,6 +43,9 @@ const char* kPreferredLanguagesPref = nullptr;
 const char kAcceptLanguagesPref[] = "intl.accept_languages";
 
 const char kTranslateBlockedLanguagesPref[] = "translate_blocked_languages";
+
+const char kForceTriggerTranslateCountPref[] =
+    "translate_force_trigger_on_english_count_for_backoff_1";
 
 }  // namespace
 
@@ -983,4 +987,30 @@ TEST_F(TranslatePrefsTest, CanTranslateLanguage) {
   EXPECT_TRUE(translate_prefs_->CanTranslateLanguage(
       &translate_accept_languages, "en"));
 }
+
+TEST_F(TranslatePrefsTest, ForceTriggerOnEnglishPagesCount) {
+  prefs_->SetInteger(kForceTriggerTranslateCountPref,
+                     std::numeric_limits<int>::max() - 1);
+  EXPECT_EQ(std::numeric_limits<int>::max() - 1,
+            translate_prefs_->GetForceTriggerOnEnglishPagesCount());
+
+  // The count should increment up to max int.
+  translate_prefs_->ReportForceTriggerOnEnglishPages();
+  EXPECT_EQ(std::numeric_limits<int>::max(),
+            translate_prefs_->GetForceTriggerOnEnglishPagesCount());
+
+  // The count should not increment past max int.
+  translate_prefs_->ReportForceTriggerOnEnglishPages();
+  EXPECT_EQ(std::numeric_limits<int>::max(),
+            translate_prefs_->GetForceTriggerOnEnglishPagesCount());
+
+  translate_prefs_->ReportAcceptedAfterForceTriggerOnEnglishPages();
+  EXPECT_EQ(-1, translate_prefs_->GetForceTriggerOnEnglishPagesCount());
+
+  // Incrementing after force triggering has already been accepted should have
+  // no effect.
+  translate_prefs_->ReportForceTriggerOnEnglishPages();
+  EXPECT_EQ(-1, translate_prefs_->GetForceTriggerOnEnglishPagesCount());
+}
+
 }  // namespace translate
