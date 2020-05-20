e1fa7c89c96330d5225c81341c8518de776bf4b4
Robbie McElrath <rmcelrath@chromium.org>
2020-05-20 21:36:46 +0000

    [WebLayer] Polish the Site Settings UI
    This CL fixes the following Site Settings issues in WebLayer:
     * Fix launching system settings from certain links
     * Apply themes to the UI, copied from Chrome's
     * Hide unsupported settings categories
     * Finish implementing pref-based settings
     * Properly handle multiple settings instances by closing one
    
    Bug: 1049683
    Change-Id: I9c8afa1b5a8faa9d2429d6c34b171225cc5f5af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209598
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770784}

--- weblayer/browser/java/org/chromium/weblayer_private/SiteSettingsFragmentImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/SiteSettingsFragmentImpl.java
@@ -92,6 +92,16 @@ public class SiteSettingsFragmentImpl extends RemoteFragmentImpl {
         }
 
         @Override
+        public Context getApplicationContext() {
+            return getEmbedderActivity().getApplicationContext();
+        }
+
+        @Override
+        public void startActivity(Intent intent) {
+            getEmbedderActivity().startActivity(intent);
+        }
+
+        @Override
         public void setTitle(int titleId) {
             getEmbedderActivity().setTitle(mFragmentImpl.getWebLayerContext().getString(titleId));
         }
@@ -217,7 +227,7 @@ public class SiteSettingsFragmentImpl extends RemoteFragmentImpl {
 
         mEmbedderContext = context;
         mContext = new ContextThemeWrapper(
-                ClassLoaderContextWrapperFactory.get(context), R.style.Theme_BrowserUI);
+                ClassLoaderContextWrapperFactory.get(context), R.style.Theme_WebLayer_SiteSettings);
         mFragmentController =
                 FragmentController.createController(new SiteSettingsFragmentHostCallback(this));
     }
