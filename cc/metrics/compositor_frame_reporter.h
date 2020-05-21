5f40ced064ba727b1c0820e90a7cef1f5ff3126b
Mohsen Izadi <mohsen@chromium.org>
2020-05-21 05:02:03 +0000

    Add Viz breakdown to EventLatency trace events
    Please see a screenshot from a sample trace at:
    https://drive.google.com/file/d/1rTdrSmfPoM07ycf76cpLAC1zHZrfwus0/view
    
    Bug: 1054009
    Change-Id: Ia24056e6a9951dae4e4be00f1e58aa6c93ff2706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166637
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770946}

--- cc/metrics/compositor_frame_reporter.h
+++ cc/metrics/compositor_frame_reporter.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <bitset>
 #include <memory>
+#include <utility>
 #include <vector>
 
 #include "base/optional.h"
@@ -214,15 +215,9 @@ class CC_EXPORT CompositorFrameReporter {
                                    int stage_type_index,
                                    base::TimeDelta latency) const;
 
-  // Generate a trace event corresponding to a Viz breakdown under
-  // SubmitCompositorFrameToPresentationCompositorFrame stage in
-  // PipelineReporter. This function only generates trace events and does not
-  // report histograms.
-  void ReportVizBreakdownTrace(VizBreakdown substage,
-                               const base::TimeTicks start_time,
-                               const base::TimeTicks end_time) const;
-
-  void ReportAllTraceEvents(const char* termination_status_str) const;
+  void ReportCompositorLatencyTraceEvents(
+      const char* termination_status_str) const;
+  void ReportEventLatencyTraceEvents() const;
 
   void EnableReportType(FrameReportType report_type) {
     report_types_.set(static_cast<size_t>(report_type));
@@ -250,7 +245,7 @@ class CC_EXPORT CompositorFrameReporter {
 
   viz::FrameTimingDetails viz_breakdown_;
   base::TimeTicks viz_start_time_;
-  base::Optional<base::TimeDelta>
+  base::Optional<std::pair<base::TimeTicks, base::TimeTicks>>
       viz_breakdown_list_[static_cast<int>(VizBreakdown::kBreakdownCount)];
 
   // Stage data is recorded here. On destruction these stages will be reported
