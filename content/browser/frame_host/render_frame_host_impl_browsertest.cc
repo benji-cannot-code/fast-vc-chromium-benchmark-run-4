4b1076f470798bbea9a81167ea6abb997c2a2d3e
Avi Drissman <avi@chromium.org>
2020-05-21 03:09:01 +0000

    Followup to remove an include no longer needed.
    A test moved in bd3e986441c5d3dc38a97e76081f507501bc1236 and therefore
    this include is no longer needed.
    
    Tbr: nasko@chromium.org
    Bug: 1076947
    Change-Id: Id926e1b0c1759b5d1bcac1d33231ad443832cc58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210027
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770907}

--- content/browser/frame_host/render_frame_host_impl_browsertest.cc
+++ content/browser/frame_host/render_frame_host_impl_browsertest.cc
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "third_party/blink/public/mojom/browser_interface_broker.mojom-test-utils.h"
-#include "third_party/blink/public/mojom/choosers/file_chooser.mojom.h"
 #include "third_party/blink/public/mojom/frame/frame.mojom-test-utils.h"
 #include "url/gurl.h"
 #include "url/origin.h"
