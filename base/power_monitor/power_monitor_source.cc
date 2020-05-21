3cd9aac6064d73f97ac1242340177d8933b24dc9
Miguel Casas-Sanchez <mcasas@chromium.org>
2020-05-21 15:08:39 +0000

    MediaStreamManager: listen to and log OnThermalStateChange
    crrev.com/c/2195334 just landed PowerObserver::OnThermalStateChange,
    this CL listens to it from MediaStreamManager and logs it in the
    WebRTC log.
    
    The utility function PowerMonitorSource::DeviceThermalStateToString
    gets #if DCHECK_IS_ON() removed to be used from the new code, which
    runs also in non-dcheck_always_on builds.
    
    Bug: 1071431
    Change-Id: I5641ae5a96e4ca9504498821ffb3ea91adbec18a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207410
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771045}

--- base/power_monitor/power_monitor_source.cc
+++ base/power_monitor/power_monitor_source.cc
@@ -76,9 +76,8 @@ void PowerMonitorSource::SetInitialOnBatteryPowerState(bool on_battery_power) {
   on_battery_power_ = on_battery_power;
 }
 
-#if DCHECK_IS_ON()
 // static
-std::string PowerMonitorSource::DeviceThermalStateToString(
+const char* PowerMonitorSource::DeviceThermalStateToString(
     PowerObserver::DeviceThermalState state) {
   switch (state) {
     case PowerObserver::DeviceThermalState::kUnknown:
@@ -95,6 +94,5 @@ std::string PowerMonitorSource::DeviceThermalStateToString(
   NOTREACHED();
   return "Unknown";
 }
-#endif
 
 }  // namespace base
