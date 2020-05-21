e467d5414f5a69b87ae8a6757c66156432b3596f
Vasilii Sukhanov <vasilii@chromium.org>
2020-05-21 07:13:38 +0000

    Revert "Reuse upload browsertests for corresponding Enterprise Connectors"
    This reverts commit 8a90504926e943b15950d6a8667993c3764d2790.
    
    Reason for revert: consistent failure on CrOS
    https://ci.chromium.org/p/chromium/builders/ci/linux-chromeos-rel/38104?blamelist=1#blamelist-tab
    
    Original change's description:
    > Reuse upload browsertests for corresponding Enterprise Connectors
    > 
    > This reuses tests by having the policy setters of
    > DeepScanningBrowserTestBase set either legacy or Connector policies, and
    > then making every upload browsertest parametrized with a bool so both
    > policy sets get tested.
    > 
    > The DeepScanningDialogViews test also change a bit, the ScanType test
    > parameter is removed since this dialog doesn't depend on the type of
    > scanning being done. This avoids having a very large number of
    > combinations for that test that don't cover different code paths.
    > 
    > Bug: 1076083
    > Change-Id: I419b21de0095add8b5b214993be010d022941202
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210782
    > Reviewed-by: Roger Tawa <rogerta@chromium.org>
    > Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#770745}
    
    TBR=rogerta@chromium.org,domfc@chromium.org
    
    Change-Id: Ifbd58dd8403a58b48b2d4911387b2ecbbd5bf70a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1076083
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209222
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770969}

--- chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/deep_scanning_test_utils.h
@@ -127,8 +127,6 @@ void AddUrlsToCheckForMalwareOfUploadsForConnectors(
     const std::vector<std::string>& urls);
 void AddUrlsToNotCheckForMalwareOfDownloadsForConnectors(
     const std::vector<std::string>& urls);
-void ClearUrlsToCheckComplianceOfDownloadsForConnectors();
-void ClearUrlsToCheckForMalwareOfUploadsForConnectors();
 
 }  // namespace safe_browsing
 
