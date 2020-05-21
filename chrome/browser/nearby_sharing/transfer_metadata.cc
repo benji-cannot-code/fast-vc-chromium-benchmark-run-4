3818bd4eddb8a9fc63109cde6bf6540ac1cc2cfe
Alex Chau <alexchau@chromium.org>
2020-05-21 10:51:29 +0000

    High level interface for NearbySharingService
    - Methods are a mirror of NearbySharingChimeraService in Android
    - Added a few data types and callback to define NearbySharingService
      methods
    - Only data related methods are introduced for now. Implementations are
      empty
    
    Bug: 1084283
    Change-Id: I18e691b76bc4872bb4a4f4771af1b368b07af6e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207374
    Commit-Queue: Alex Chau <alexchau@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770997}

---
+++ chrome/browser/nearby_sharing/transfer_metadata.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/nearby_sharing/transfer_metadata.h"
+
+TransferMetadata::TransferMetadata(Status status,
+                                   float progress,
+                                   base::Optional<std::string> token,
+                                   bool is_original,
+                                   bool is_final_status)
+    : status_(status),
+      progress_(progress),
+      token_(std::move(token)),
+      is_original_(is_original),
+      is_final_status_(is_final_status) {}
+
+TransferMetadata::~TransferMetadata() = default;
