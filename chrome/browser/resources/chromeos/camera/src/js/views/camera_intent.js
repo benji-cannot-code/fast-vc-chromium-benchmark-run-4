35caf0d0f3b113d1edff2ab07266ca6cd420843e
Shik Chen <shik@chromium.org>
2020-05-21 05:40:57 +0000

    CCA: Use the recording start timestamp for file name
    This CL removes the renaming behavior for video recording to simplify
    the logic, and mitigate thumbnail regeneration timeout issue in Files
    app for long video.
    
    Bug: 1058325, 1080869
    Test: Verify the file name manually, and pass CCAUIRecordVideo and CCAUIIntent
    Change-Id: I074885d95e8cf41d85cf46ef866c47500726708b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210201
    Commit-Queue: Shik Chen <shik@chromium.org>
    Commit-Queue: Kuo Jen Wei <inker@chromium.org>
    Auto-Submit: Shik Chen <shik@chromium.org>
    Reviewed-by: Kuo Jen Wei <inker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770953}

--- chrome/browser/resources/chromeos/camera/src/js/views/camera_intent.js
+++ chrome/browser/resources/chromeos/camera/src/js/views/camera_intent.js
@@ -66,7 +66,7 @@ export class CameraIntent extends Camera {
       startSaveVideo: async () => {
         return await VideoSaver.createForIntent(intent);
       },
-      finishSaveVideo: async (video, savedName) => {
+      finishSaveVideo: async (video) => {
         this.videoResultFile_ = await video.endWrite();
       },
     });
@@ -121,10 +121,10 @@ export class CameraIntent extends Camera {
   /**
    * @override
    */
-  async doSaveVideo_(result, name) {
+  async doSaveVideo_(result) {
     this.videoResult_ = result;
     try {
-      await this.resultSaver_.finishSaveVideo(result.videoSaver, name);
+      await this.resultSaver_.finishSaveVideo(result.videoSaver);
     } catch (e) {
       toast.show('error_msg_save_file_failed');
       throw e;
