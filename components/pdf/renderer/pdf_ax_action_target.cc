baeb4def6884a4e5bdfd6ec1cb8eb67f2947b1fe
Mansi Awasthi <maawas@microsoft.com>
2020-05-21 21:07:42 +0000

    Reland: Supporting context menu on PDF via AT clients
    Original CL: http://crrev.com/c/2172854
    Revert: http://crrev.com/c/2204841
    
    The original CL got reverted because following browser test was failing
    on the Mac10.12 tests bot -
    PDFExtensionTest.PdfAccessibilityContextMenuAction
    
    Added missing ContextMenuWaiter in the test to fix the issue.
    
    Original description:
    This CL adds support for invoking context menu on PDF via AT clients. To
    show context menu for PDF plugin, the context menu action can be invoked
    on respective EMBED node. This internally takes care of showing plugin
    related context menu option(See ContextMenuController::ShowContextMenu()
    for reference).
    
    Implemented PdfAXActionTarget::ShowContextMenu() that forwards the call
    to RenderAccessibilityImpl::ShowPluginContextMenu() to find the EMBED
    object in accessibility tree and invoke ShowContextMenu() on EMBED node.
    
    This CL also includes browser test to validate E2E scenario.
    
    CQ_INCLUDE_TRYBOTS=luci.chromium.try:mac_chromium_dbg_ng
    
    Bug: 1023283
    Change-Id: Ie710fd24232b8dfeb3ce4bc6c0b56250ec9fbf0d
    
    Tbr: dmazzoni@chromium.org,jochen@chromium.org
    Change-Id: Ie710fd24232b8dfeb3ce4bc6c0b56250ec9fbf0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210080
    Commit-Queue: Mansi Awasthi <maawas@microsoft.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#771188}

--- components/pdf/renderer/pdf_ax_action_target.cc
+++ components/pdf/renderer/pdf_ax_action_target.cc
@@ -158,7 +158,7 @@ bool PdfAXActionTarget::SetValue(const std::string& value) const {
 }
 
 bool PdfAXActionTarget::ShowContextMenu() const {
-  return false;
+  return pdf_accessibility_tree_source_->ShowContextMenu();
 }
 
 bool PdfAXActionTarget::ScrollToMakeVisible() const {
