b9bfc92cd1b0f0dfff3cfe7e6da7f3d57d1861ac
Khushal Sagar <khushalsagar@chromium.org>
2020-05-21 03:42:37 +0000

    cc: Set up finch config to disable synchronized scrolling.
    Add a finch feature flag to disable delaying scroll response in the
    compositor for scroll handlers.
    
    R=sadrul@chromium.org,szager@chromium.org
    
    Bug: 1080911
    Change-Id: Ida54b5f160b4438898cde48e59037d5c5254b4cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209688
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770926}

--- cc/base/features.h
+++ cc/base/features.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace features {
 
 CC_BASE_EXPORT extern const base::Feature kImpulseScrollAnimations;
+CC_BASE_EXPORT extern const base::Feature kSynchronizedScrolling;
 CC_BASE_EXPORT extern const base::Feature kTextureLayerSkipWaitForActivation;
 
 #if !defined(OS_ANDROID)
