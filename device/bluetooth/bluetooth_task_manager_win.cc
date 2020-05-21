f5c75e9de27897b43d95c785def2580e5f8a156c
Reilly Grant <reillyg@chromium.org>
2020-05-21 20:49:48 +0000

    [bluetooth] Boost priority while calling WSALookupService*()
    These functions may cause DLLs to be loaded for socket providers in the
    process of enumerating device services. This macro temporarily boosts
    this background thread's priority in order to avoid a priority inversion
    where the main thread (and others) hang while this low priority thread
    holds the dynamic library loader lock.
    
    Observed this hang in crash/08cf98f7a1b73d12.
    
    Bug: 973868
    Change-Id: Ibb80c37c735471958f7b41460831aad2891ccf78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212671
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771181}

--- device/bluetooth/bluetooth_task_manager_win.cc
+++ device/bluetooth/bluetooth_task_manager_win.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/task/post_task.h"
 #include "base/task/thread_pool.h"
+#include "base/threading/scoped_thread_priority.h"
 #include "device/bluetooth/bluetooth_classic_win.h"
 #include "device/bluetooth/bluetooth_device.h"
 #include "device/bluetooth/bluetooth_init_win.h"
@@ -645,6 +646,10 @@ int BluetoothTaskManagerWin::DiscoverClassicDeviceServicesWorker(
     const GUID& protocol_uuid,
     bool search_cached_services_only,
     std::vector<std::unique_ptr<ServiceRecordState>>* service_record_states) {
+  // Mitigate the issues caused by loading DLLs on a background thread
+  // (http://crbug/973868).
+  SCOPED_MAY_LOAD_LIBRARY_AT_BACKGROUND_PRIORITY();
+
   // Bluetooth and WSAQUERYSET for Service Inquiry. See http://goo.gl/2v9pyt.
   WSAQUERYSET sdp_query;
   ZeroMemory(&sdp_query, sizeof(sdp_query));
