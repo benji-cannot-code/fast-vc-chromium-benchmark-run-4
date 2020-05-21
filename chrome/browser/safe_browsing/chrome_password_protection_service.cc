5662cd5f12f6c1020bcb991e47a5b034bbeeb2ec
Xinghui Lu <xinghuilu@chromium.org>
2020-05-21 23:00:36 +0000

    Revert "Move ChromePasswordProtectionService to be a KeyedService."
    This reverts commit 596bbbfeb885bd7c43739b4ecbe20397dbb6248c.
    
    Reason for revert: Check failed: checker.CalledOnValidSequence() during destruction.
    
    TBR=drubery@chromium.org
    
    Original change's description:
    > Move ChromePasswordProtectionService to be a KeyedService.
    >
    > Bug: 1058431
    > Change-Id: Id831530b726e63cfcba71dbcb86af8bf00e11dd0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198536
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#769913}
    
    TBR=drubery@chromium.org,xinghuilu@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1058431,1085536
    Change-Id: I1047ec0f415a4504d13edb52f8809d6c946073a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212817
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771236}

--- chrome/browser/safe_browsing/chrome_password_protection_service.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service.cc
@@ -295,12 +295,10 @@ void ChromePasswordProtectionService::Init() {
 #endif
 }
 
-void ChromePasswordProtectionService::Shutdown() {
+ChromePasswordProtectionService::~ChromePasswordProtectionService() {
   if (pref_change_registrar_)
     pref_change_registrar_->RemoveAll();
 }
-
-ChromePasswordProtectionService::~ChromePasswordProtectionService() = default;
 
 // static
 ChromePasswordProtectionService*
--- chrome/browser/safe_browsing/chrome_password_protection_service_browsertest.cc
+++ chrome/browser/safe_browsing/chrome_password_protection_service_browsertest.cc
@@ -738,7 +738,6 @@ IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
                        OnEnterpriseTriggerOffGSuite) {
-  GetService(/*is_incognito=*/false);  // Create a service to listen to events.
   ConfigureEnterprisePasswordProtection(
       /*is_gsuite=*/true, PasswordProtectionTrigger::PHISHING_REUSE);
   Profile* profile = browser()->profile();
@@ -764,7 +763,6 @@ IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(ChromePasswordProtectionServiceBrowserTest,
                        OnEnterpriseTriggerOff) {
-  GetService(/*is_incognito=*/false);  // Create a service to listen to events.
   ConfigureEnterprisePasswordProtection(
       /*is_gsuite=*/false, PasswordProtectionTrigger::PHISHING_REUSE);
   Profile* profile = browser()->profile();
