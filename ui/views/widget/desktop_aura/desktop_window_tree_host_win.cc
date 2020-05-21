26504a3fc1ca9f620cbf07978c9bf0f9e541ccdb
Liviu Tinta <liviutinta@chromium.org>
2020-05-21 13:51:40 +0000

    Ignore native system mouse events during testing
    A bunch of tests synthesize input events during testing, and this
    ranges from unit-tests, interactive-ui-tests, telemetry tests etc.
    During such tests, it is possible that chrome also receives unexpected
    input events from the native system (e.g. if the window happens to show
    up under the mouse cursor), causing unexpected failures (e.g. the
    event-stream becomes invalid, or the tooltip shows up, etc.). Ignoring
    all input events from the system during these tests should make these
    tests more deterministic.
    
    Bug: 766450, 883710
    Change-Id: Ib7fccaf7a0495207cc9b097bfe25862cad302dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2176697
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Navid Zolghadr <nzolghadr@chromium.org>
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771020}

--- ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/keyboard_hook.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/dom_keyboard_layout_map.h"
+#include "ui/events/platform/platform_event_source.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/vector2d.h"
 #include "ui/gfx/native_widget_types.h"
@@ -926,6 +927,9 @@ void DesktopWindowTreeHostWin::HandleNativeBlur(HWND focused_window) {
 }
 
 bool DesktopWindowTreeHostWin::HandleMouseEvent(ui::MouseEvent* event) {
+  // Ignore native platform events for test purposes
+  if (ui::PlatformEventSource::ShouldIgnoreNativePlatformEvents())
+    return true;
   // Mouse events in occluded windows should be very rare. If this stat isn't
   // very close to 0, that would indicate that windows are incorrectly getting
   // marked occluded, or getting stuck in the occluded state. Event can cause
