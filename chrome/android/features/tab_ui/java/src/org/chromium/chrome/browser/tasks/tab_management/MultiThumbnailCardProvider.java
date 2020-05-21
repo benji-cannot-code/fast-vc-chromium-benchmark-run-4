2a026f79f948442b8471570b476a68d0d121a5aa
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2020-05-21 20:26:49 +0000

    Fix pre-restoration behavior in PseudoTab
    Before this CL, existence of TabModelFilter was used to determine
    whether we should use pre-native PseudoTab or not, but having a
    TabModelFilter doesn't necessarily mean it's already restored.
    
    This CL uses TabModelSelector#isTabStateInitialized instead.
    
    Bug: 1080810
    Change-Id: Id8aa5e1a61cfa563f4f56b64d6c09ea0fd2b277c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2206223
    Auto-Submit: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771173}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/MultiThumbnailCardProvider.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/MultiThumbnailCardProvider.java
@@ -89,8 +89,7 @@ public class MultiThumbnailCardProvider implements TabListMediator.ThumbnailProv
             mCanvas.drawColor(Color.TRANSPARENT);
 
             // Initialize Tabs.
-            List<PseudoTab> relatedTabList =
-                    PseudoTab.getRelatedTabs(tab, mTabModelSelector.getTabModelFilterProvider());
+            List<PseudoTab> relatedTabList = PseudoTab.getRelatedTabs(tab, mTabModelSelector);
             if (relatedTabList.size() <= 4) {
                 mThumbnailsToFetch.set(relatedTabList.size());
 
@@ -307,10 +306,7 @@ public class MultiThumbnailCardProvider implements TabListMediator.ThumbnailProv
     public void getTabThumbnailWithCallback(
             int tabId, Callback<Bitmap> finalCallback, boolean forceUpdate, boolean writeToCache) {
         PseudoTab tab = PseudoTab.fromTabId(tabId);
-        if (tab == null
-                || PseudoTab.getRelatedTabs(tab, mTabModelSelector.getTabModelFilterProvider())
-                                .size()
-                        == 1) {
+        if (tab == null || PseudoTab.getRelatedTabs(tab, mTabModelSelector).size() == 1) {
             mTabContentManager.getTabThumbnailWithCallback(
                     tabId, finalCallback, forceUpdate, writeToCache);
             return;
