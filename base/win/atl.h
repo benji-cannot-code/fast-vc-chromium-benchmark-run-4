af71ddfc6321a9182f2473194b07d05775ed1843
Nico Weber <thakis@chromium.org>
2020-05-21 17:44:23 +0000

    Revert "Crash fast in case of an exception from ATL."
    This reverts commit add913dca4c5acc0e402cc41c76bbf6c1b86d871.
    
    Reason for revert: closed tree due to
    https://ci.chromium.org/p/chromium/builders/ci/win32-archive-rel/13297?
    
    Original change's description:
    > Crash fast in case of an exception from ATL.
    > 
    > BUG=108176
    > TBR=fdoray@chromium.org
    > 
    > Change-Id: I9723b07932b35fd098e40965735d1e92cb0c8946
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210466
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#771073}
    
    TBR=fdoray@chromium.org,grt@chromium.org,nicolaso@chromium.org
    
    Change-Id: Idaa6e98d787d8dc33c08aede2c33dca919d2a612
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 108176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212255
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771095}

--- base/win/atl.h
+++ base/win/atl.h
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #ifndef BASE_WIN_ATL_H_
 #define BASE_WIN_ATL_H_
 
@@ -10,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_defines.inc"
 // Undefine before windows header will make the poisonous defines
 #include "base/win/windows_undefines.inc"
+
+#ifndef _ATL_NO_EXCEPTIONS
+#define _ATL_NO_EXCEPTIONS
+#endif
 
 // atlwin.h relies on std::void_t, but libc++ doesn't define it unless
 // _LIBCPP_STD_VER > 14.  Workaround this by manually defining it.
@@ -20,9 +23,6 @@ template <class...>
 using void_t = void;
 }
 #endif
-
-// Declare our own exception thrower (atl_throw.h includes atldef.h).
-#include "base/win/atl_throw.h"
 
 #include <atlbase.h>
 #include <atlcom.h>
