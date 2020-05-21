fcb590fc9d524c475f4cac3dc4751882d1807838
Hiroki Nakagawa <nhiroki@chromium.org>
2020-05-21 05:40:02 +0000

    Loader: Rename LinkFetchResource to LinkPrefetchResource
    For code cleanup, this CL renames LinkFetchResource that was used only
    for <link rel='prefetch'> to LinkPrefetchResource.
    
    This also renames LinkResourceFactory to Factory. That is the internal
    class of LinkPrefetchResource, so just naming it "Factory" would be
    simpler and still understandable.
    
    Bug: 1075582
    Change-Id: If057c89242a41ea1969045ba19f1ceef6d4195a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210202
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770952}

--- third_party/blink/renderer/core/loader/resource/link_prefetch_resource.h
+++ third_party/blink/renderer/core/loader/resource/link_prefetch_resource.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_RESOURCE_LINK_FETCH_RESOURCE_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_RESOURCE_LINK_FETCH_RESOURCE_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_RESOURCE_LINK_PREFETCH_RESOURCE_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_RESOURCE_LINK_PREFETCH_RESOURCE_H_
 
 #include "third_party/blink/renderer/platform/loader/fetch/resource.h"
 
@@ -14,19 +14,17 @@ class FetchParameters;
 class ResourceFetcher;
 
 // This is the implementation of Resource for <link rel='prefetch'>.
-// TODO(nhiroki): Rename this to LinkPrefetchResource.
-class LinkFetchResource final : public Resource {
+class LinkPrefetchResource final : public Resource {
  public:
   static Resource* Fetch(FetchParameters&, ResourceFetcher*);
 
-  LinkFetchResource(const ResourceRequest&,
-                    const ResourceLoaderOptions&);
-  ~LinkFetchResource() override;
+  LinkPrefetchResource(const ResourceRequest&, const ResourceLoaderOptions&);
+  ~LinkPrefetchResource() override;
 
  private:
-  class LinkResourceFactory final : public NonTextResourceFactory {
+  class Factory final : public NonTextResourceFactory {
    public:
-    LinkResourceFactory();
+    Factory();
 
     Resource* Create(const ResourceRequest& request,
                      const ResourceLoaderOptions& options) const override;
@@ -35,4 +33,4 @@ class LinkFetchResource final : public Resource {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_RESOURCE_LINK_FETCH_RESOURCE_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LOADER_RESOURCE_LINK_PREFETCH_RESOURCE_H_
