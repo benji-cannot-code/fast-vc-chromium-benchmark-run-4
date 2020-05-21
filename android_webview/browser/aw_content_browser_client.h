c4ffda8dfdf357de4c4fe26d9e88aab1b128c7ca
Jinsong Fan <jinsong.van@gmail.com>
2020-05-21 09:34:04 +0000

    AW: Remove NetLog from AwContentBrowserClient
    The NetLog is now an internal detail of the network service.
    And this CL: https://chromium-review.googlesource.com/c/chromium/src/+/1695293
    still left dead codes about NetLog. The CL removes it for code health.
    
    Bug: 963875
    Change-Id: I01c246b95a961b219bb4c93fd4b40743f9ba5e20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2192536
    Auto-Submit: Jinsong Fan <jinsong.van@gmail.com>
    Commit-Queue: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: Tobias Sargeant <tobiasjs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#770992}

--- android_webview/browser/aw_content_browser_client.h
+++ android_webview/browser/aw_content_browser_client.h
@@ -234,8 +234,6 @@ class AwContentBrowserClient : public content::ContentBrowserClient {
   content::SpeechRecognitionManagerDelegate*
   CreateSpeechRecognitionManagerDelegate() override;
 
-  net::NetLog* GetNonNetworkServiceNetLog();
-
   static void DisableCreatingThreadPool();
 
  private:
