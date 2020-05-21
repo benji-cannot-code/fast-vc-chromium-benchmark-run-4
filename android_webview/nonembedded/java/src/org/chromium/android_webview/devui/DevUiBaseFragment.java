177c8c7ef8881833948f4c919898184f3caca052
Nate Fischer <ntfschr@chromium.org>
2020-05-21 19:24:15 +0000

    [AW][Dev-UI] log duration for user session
    No change to logic. This adds logs to capture the duration of the user's
    session with each tool. This captures each tool in a different
    histogram, measuring from onResume until onPause (so we intentionally
    don't include time when the app is in the background).
    
    Fixed: 1084177
    Test: Add a log statement and verify it fires as expected
    Change-Id: I9933a97c5c99d123440ed4e46b94bc056964ada3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2211146
    Reviewed-by: Hazem Ashmawy <hazems@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771143}

--- android_webview/nonembedded/java/src/org/chromium/android_webview/devui/DevUiBaseFragment.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/devui/DevUiBaseFragment.java
@@ -4,12 +4,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.android_webview.devui;
 
+import android.os.SystemClock;
+
 import androidx.fragment.app.Fragment;
+
+import org.chromium.base.metrics.RecordHistogram;
 
 /**
  * A base class for all fragments in the UI.
  */
 public abstract class DevUiBaseFragment extends Fragment {
+    private long mStartOfSession;
+
     /**
      * Called by {@link MainActivity} to to give the fragment an opportunity to show an error
      * message. It's up to the {@link MainActivity} to call this or not.
@@ -19,5 +25,33 @@ public abstract class DevUiBaseFragment extends Fragment {
      */
     /* package */ void maybeShowErrorView(PersistentErrorView errorView) {
         errorView.hide();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mStartOfSession = SystemClock.elapsedRealtime();
+    }
+
+    private void logSessionDuration() {
+        String suffix = "Unknown";
+        if (this instanceof HomeFragment) {
+            suffix = "HomeFragment";
+        } else if (this instanceof FlagsFragment) {
+            suffix = "FlagsFragment";
+        } else if (this instanceof CrashesListFragment) {
+            suffix = "CrashesListFragment";
+        }
+        // Note: keep this if-else ladder synchronized with the AndroidWebViewFragments
+        // histogram_suffix
+        long endOfSession = SystemClock.elapsedRealtime();
+        RecordHistogram.recordTimesHistogram(
+                "Android.WebView.DevUi.SessionDuration." + suffix, endOfSession - mStartOfSession);
+    }
+
+    @Override
+    public void onPause() {
+        logSessionDuration();
+        super.onPause();
     }
 }
