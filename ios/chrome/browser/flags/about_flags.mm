bf930d4974507680791972755c0dea9a7e9bccb2
adamta <adamta@google.com>
2020-05-21 03:12:21 +0000

    Feature Flag for Discover Feed in Bling NTP
    Change-Id: I04d3b1315656d9adedd6871435ef0686924bb41a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202387
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/master@{#770913}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/passwords/password_manager_features.h"
 #include "ios/chrome/browser/policy/policy_features.h"
 #include "ios/chrome/browser/system_flags.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
 #import "ios/chrome/browser/ui/download/features.h"
 #import "ios/chrome/browser/ui/fullscreen/fullscreen_features.h"
 #import "ios/chrome/browser/ui/infobars/infobar_feature.h"
@@ -633,7 +634,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
     {"safety-check-ios", flag_descriptions::kSafetyCheckIOSName,
      flag_descriptions::kSafetyCheckIOSDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kSafetyCheckIOS)},
-
+    {"discover-feed-ntp-ios", flag_descriptions::kDiscoverFeedInNtpName,
+     flag_descriptions::kDiscoverFeedInNtpDescription, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kDiscoverFeedInNtp)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
---
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_feature.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// Feature disabled by default to keep showing old Zine feed.
+const base::Feature kDiscoverFeedInNtp{"DiscoverFeedInNtp",
+                                       base::FEATURE_DISABLED_BY_DEFAULT};
+
+bool IsDiscoverFeedEnabled() {
+  return base::FeatureList::IsEnabled(kDiscoverFeedInNtp);
+}
