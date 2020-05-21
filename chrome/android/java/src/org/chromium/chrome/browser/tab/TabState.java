b0fad62f30f90300a8ec6434c6f45ee94fc0b308
Henrique Nakashima <hnakashima@chromium.org>
2020-05-21 19:38:53 +0000

    Remove JellyBean workaround in TabState.
    A bug in JellyBean with rewinding buffers required a workaround.
    JellyBean has not been supported for a while, so this can be removed.
    
    Change-Id: I99aa7ea55ded13e9748b34c3902ce085fade4c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212448
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771153}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabState.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabState.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab;
 
 import android.graphics.Color;
-import android.os.Build;
 import android.util.Pair;
 
 import androidx.annotation.Nullable;
@@ -302,15 +301,8 @@ public class TabState {
 
     public static byte[] getContentStateByteArray(ByteBuffer buffer) {
         byte[] contentsStateBytes = new byte[buffer.limit()];
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            buffer.rewind();
-            buffer.get(contentsStateBytes);
-        } else {
-            // For JellyBean and below a bug in MappedByteBufferAdapter causes rewind to not be
-            // propagated to the underlying ByteBuffer, and results in an underflow exception. See:
-            // http://b.android.com/53637.
-            for (int i = 0; i < buffer.limit(); i++) contentsStateBytes[i] = buffer.get(i);
-        }
+        buffer.rewind();
+        buffer.get(contentsStateBytes);
         return contentsStateBytes;
     }
 
