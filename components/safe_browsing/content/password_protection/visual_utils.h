bc968a7096a512e2d15ecedf2966da1b9a4981ca
Daniel Rubery <drubery@chromium.org>
2020-05-21 17:13:16 +0000

    Implementing utility to check if a screenshot matches a VisualTarget
    This CL adds and tests a utility function that checks if the captured
    screenshot matches a given VisualTarget from the VisualModel.
    
    Bug: 1068617
    Change-Id: I3cad89f7ce6aa16a0e97ac31e30c8205fb93d3d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209475
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771089}

--- components/safe_browsing/content/password_protection/visual_utils.h
+++ components/safe_browsing/content/password_protection/visual_utils.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "components/safe_browsing/core/proto/client_model.pb.h"
 #include "components/safe_browsing/core/proto/csd.pb.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 
@@ -39,6 +40,9 @@ bool GetBlurredImage(const SkBitmap& image,
 // image has pixels the average of the pixels in each block.
 std::unique_ptr<SkBitmap> BlockMeanAverage(const SkBitmap& image,
                                            int block_size);
+
+// Returns whether the given |image| is a match for the |target|.
+bool IsVisualMatch(const SkBitmap& image, const VisualTarget& target);
 
 }  // namespace visual_utils
 }  // namespace safe_browsing
