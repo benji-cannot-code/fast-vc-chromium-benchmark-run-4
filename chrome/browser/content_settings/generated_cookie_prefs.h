b1cbd6bd94b5213afae0abaa021bfd2872eec384
sauski <sauski@google.com>
2020-05-24 20:21:39 +0000

    Settings Virtual Prefs: Primary Cookie Setting Radio Buttons
    Introduces new generated preference to support the Quad state radio
    button which controls the primary cookie settings on the new Cookies
    page.
    
    This CL creates the generated preference and extends
    settings_private.idl with a new userSelectableValues attribute to
    support the complex partial management states this preference must be
    capable of representing.
    
    The new preference is not used in production code and only enough
    management state functionality is included to test the additions to the
    SettingsPrivate API.
    
    Bug: 1063265
    Change-Id: I15ac79021530efe6af11cb000f536549216263ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2199461
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#771432}

--- chrome/browser/content_settings/generated_cookie_prefs.h
+++ chrome/browser/content_settings/generated_cookie_prefs.h
@@ -16,6 +16,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content_settings {
 
 extern const char kCookieSessionOnly[];
+extern const char kCookiePrimarySetting[];
+
+// Must be kept in sync with the CookiesControl enum located in
+// chrome/browser/resources/settings/privacy_page/cookies_page.js
+// TODO(crbug.com/1063265): Rename JS enum to match this one when the generated
+//    preference is used in production code.
+enum class CookiePrimarySetting {
+  ALLOW_ALL,
+  BLOCK_THIRD_PARTY_INCOGNITO,
+  BLOCK_THIRD_PARTY,
+  BLOCK_ALL
+};
 
 // The base class for generated preferences which support WebUI cookie controls
 // that do not not map completely to individual preferences or content settings.
@@ -31,6 +43,7 @@ class GeneratedCookiePrefBase
                                const ContentSettingsPattern& secondary_pattern,
                                ContentSettingsType content_type,
                                const std::string& resource_identifier) override;
+  void OnCookiePreferencesChanged();
 
  protected:
   GeneratedCookiePrefBase(Profile* profile, const std::string& pref_name_);
@@ -39,6 +52,18 @@ class GeneratedCookiePrefBase
   const std::string pref_name_;
   ScopedObserver<HostContentSettingsMap, content_settings::Observer>
       content_settings_observer_{this};
+  PrefChangeRegistrar user_prefs_registrar_;
+};
+
+class GeneratedCookiePrimarySettingPref : public GeneratedCookiePrefBase {
+ public:
+  explicit GeneratedCookiePrimarySettingPref(Profile* profile);
+
+  // Generated Preference Interface.
+  extensions::settings_private::SetPrefResult SetPref(
+      const base::Value* value) override;
+  std::unique_ptr<extensions::api::settings_private::PrefObject> GetPrefObject()
+      const override;
 };
 
 class GeneratedCookieSessionOnlyPref : public GeneratedCookiePrefBase {
--- third_party/closure_compiler/externs/settings_private.js
+++ third_party/closure_compiler/externs/settings_private.js
@@ -58,6 +58,7 @@ chrome.settingsPrivate.Enforcement = {
  *   controlledByName: (string|undefined),
  *   enforcement: (!chrome.settingsPrivate.Enforcement|undefined),
  *   recommendedValue: (*|undefined),
+ *   userSelectableValues: (!Array<*>|undefined),
  *   userControlDisabled: (boolean|undefined),
  *   extensionId: (string|undefined),
  *   extensionCanBeDisabled: (boolean|undefined)
