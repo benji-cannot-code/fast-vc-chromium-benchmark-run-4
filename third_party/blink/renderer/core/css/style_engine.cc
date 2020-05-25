bab3184f002f4e89b994841ebc3b1b525fe04a96
Rune Lillesveen <futhark@chromium.org>
2020-05-25 14:40:32 +0000

    Set preferred color-scheme to no-preference for printing.
    There's no need to use a lot of ink for printing dark even if the user
    has a dark preference for their screen.
    
    Bug: 1084465
    Change-Id: Ibac200703c4505ed2838c9d40545041ad9609170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214525
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771566}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -2119,7 +2119,8 @@ void StyleEngine::UpdateColorScheme() {
     // darkening is enabled.
     preferred_color_scheme_ = PreferredColorScheme::kNoPreference;
   }
-
+  if (GetDocument().Printing())
+    preferred_color_scheme_ = PreferredColorScheme::kNoPreference;
   if (forced_colors_ != old_forced_colors ||
       preferred_color_scheme_ != old_preferred_color_scheme)
     PlatformColorsChanged();
@@ -2198,6 +2199,13 @@ void StyleEngine::PropagateWritingModeAndDirectionToHTMLRoot() {
   if (HTMLHtmlElement* root_element =
           DynamicTo<HTMLHtmlElement>(GetDocument().documentElement()))
     root_element->PropagateWritingModeAndDirectionFromBody();
+}
+
+void StyleEngine::PrintingStateChanged() {
+  ColorSchemeChanged();
+  MarkViewportStyleDirty();
+  MarkAllElementsForStyleRecalc(StyleChangeReasonForTracing::Create(
+      style_change_reason::kStyleSheetChange));
 }
 
 void StyleEngine::Trace(Visitor* visitor) const {
