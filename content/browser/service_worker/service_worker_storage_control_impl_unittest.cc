95ef1f2bdd51a27fa535c5aed8623a4747475ba3
Kenichi Ishibashi <bashi@chromium.org>
2020-05-25 23:03:01 +0000

    Add ServiceWorkerLiveVersionRef
    This CL introduces a mojo interface that will be used to keep track
    of running service workers in browser and embedder. The storage
    service wouldn't start purging resources until there is no running
    service workers.
    
    See [1] for the motivation of this change.
    
    [1] https://docs.google.com/document/d/1VnZN2k9YbpX1Xy-Yvo93DKDnW4-M2W-rwjkGF1swqLo/edit?usp=sharing
    
    Bug: 1055677
    Change-Id: Ie84892b98edf4f789f9c1073343489aa63ff5b6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2200717
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771649}

--- content/browser/service_worker/service_worker_storage_control_impl_unittest.cc
+++ content/browser/service_worker/service_worker_storage_control_impl_unittest.cc
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 using DatabaseStatus = storage::mojom::ServiceWorkerDatabaseStatus;
+using RegistrationData = storage::mojom::ServiceWorkerRegistrationDataPtr;
+using ResourceRecord = storage::mojom::ServiceWorkerResourceRecordPtr;
 using FindRegistrationResult =
     storage::mojom::ServiceWorkerFindRegistrationResultPtr;
 
@@ -191,7 +193,7 @@ class ServiceWorkerStorageControlImplTest : public testing::Test {
     storage()->FindRegistrationForClientUrl(
         client_url,
         base::BindLambdaForTesting([&](FindRegistrationResult result) {
-          return_value = result.Clone();
+          return_value = std::move(result);
           loop.Quit();
         }));
     loop.Run();
@@ -203,7 +205,7 @@ class ServiceWorkerStorageControlImplTest : public testing::Test {
     base::RunLoop loop;
     storage()->FindRegistrationForScope(
         scope, base::BindLambdaForTesting([&](FindRegistrationResult result) {
-          return_value = result.Clone();
+          return_value = std::move(result);
           loop.Quit();
         }));
     loop.Run();
@@ -217,7 +219,7 @@ class ServiceWorkerStorageControlImplTest : public testing::Test {
     storage()->FindRegistrationForId(
         registration_id, origin,
         base::BindLambdaForTesting([&](FindRegistrationResult result) {
-          return_value = result.Clone();
+          return_value = std::move(result);
           loop.Quit();
         }));
     loop.Run();
@@ -244,7 +246,7 @@ class ServiceWorkerStorageControlImplTest : public testing::Test {
   }
 
   DatabaseStatus StoreRegistration(
-      storage::mojom::ServiceWorkerRegistrationDataPtr registration,
+      RegistrationData registration,
       std::vector<storage::mojom::ServiceWorkerResourceRecordPtr> resources) {
     DatabaseStatus out_status;
     base::RunLoop loop;
@@ -512,17 +514,14 @@ class ServiceWorkerStorageControlImplTest : public testing::Test {
     return return_value;
   }
 
-  // Create a registration with a single resource and stores the registration.
-  DatabaseStatus CreateAndStoreRegistration(int64_t registration_id,
-                                            int64_t version_id,
-                                            int64_t resource_id,
-                                            const GURL& scope,
-                                            const GURL& script_url,
-                                            int64_t script_size) {
-    std::vector<storage::mojom::ServiceWorkerResourceRecordPtr> resources;
-    resources.push_back(storage::mojom::ServiceWorkerResourceRecord::New(
-        resource_id, script_url, script_size));
-
+  // Creates a registration with the given resource records.
+  RegistrationData CreateRegistrationData(
+      int64_t registration_id,
+      int64_t version_id,
+      const GURL& scope,
+      const GURL& script_url,
+      const std::vector<storage::mojom::ServiceWorkerResourceRecordPtr>&
+          resources) {
     auto data = storage::mojom::ServiceWorkerRegistrationData::New();
     data->registration_id = registration_id;
     data->version_id = version_id;
@@ -537,9 +536,51 @@ class ServiceWorkerStorageControlImplTest : public testing::Test {
     }
     data->resources_total_size_bytes = resources_total_size_bytes;
 
+    return data;
+  }
+
+  // Creates a registration with a single resource and stores the registration.
+  DatabaseStatus CreateAndStoreRegistration(int64_t registration_id,
+                                            int64_t version_id,
+                                            int64_t resource_id,
+                                            const GURL& scope,
+                                            const GURL& script_url,
+                                            int64_t script_size) {
+    std::vector<storage::mojom::ServiceWorkerResourceRecordPtr> resources;
+    resources.push_back(storage::mojom::ServiceWorkerResourceRecord::New(
+        resource_id, script_url, script_size));
+
+    RegistrationData data = CreateRegistrationData(
+        registration_id, version_id, scope, script_url, resources);
+
     DatabaseStatus status =
         StoreRegistration(std::move(data), std::move(resources));
     return status;
+  }
+
+  int WriteResource(int64_t resource_id, const std::string& data) {
+    auto response_head = network::mojom::URLResponseHead::New();
+    response_head->headers = base::MakeRefCounted<net::HttpResponseHeaders>(
+        net::HttpUtil::AssembleRawHeaders(
+            "HTTP/1.1 200 OK\n"
+            "Content-Type: application/javascript\n"));
+    response_head->headers->GetMimeType(&response_head->mime_type);
+
+    mojo::Remote<storage::mojom::ServiceWorkerResourceWriter> writer =
+        CreateResourceWriter(resource_id);
+    int result = WriteResponseHead(writer.get(), std::move(response_head));
+    if (result < 0)
+      return result;
+
+    mojo_base::BigBuffer buffer(base::as_bytes(base::make_span(data)));
+    result = WriteResponseData(writer.get(), std::move(buffer));
+    return result;
+  }
+
+  std::string ReadResource(int64_t resource_id, int data_size) {
+    mojo::Remote<storage::mojom::ServiceWorkerResourceReader> reader =
+        CreateResourceReader(resource_id);
+    return ReadResponseData(reader.get(), data_size);
   }
 
   mojo::Remote<storage::mojom::ServiceWorkerResourceReader>
@@ -1246,6 +1287,95 @@ TEST_F(ServiceWorkerStorageControlImplTest, ApplyPolicyUpdates) {
   {
     FindRegistrationResult result = FindRegistrationForScope(kScope2);
     ASSERT_EQ(result->status, DatabaseStatus::kErrorNotFound);
+  }
+}
+
+TEST_F(ServiceWorkerStorageControlImplTest, TrackRunningVersion) {
+  const GURL kScope("https://www.example.com/");
+  const GURL kScriptUrl("https://www.example.com/sw.js");
+  const GURL kImportedScriptUrl("https://www.example.com/imported.js");
+
+  LazyInitializeForTest();
+
+  // Preparation: Create a registration with two resources (The main script and
+  // an imported script).
+  int result;
+  std::vector<ResourceRecord> resources;
+  const int64_t resource_id1 = GetNewResourceId();
+  const std::string resource_data1 = "main script data";
+  result = WriteResource(resource_id1, resource_data1);
+  ASSERT_GT(result, 0);
+  resources.push_back(storage::mojom::ServiceWorkerResourceRecord::New(
+      resource_id1, kScriptUrl, resource_data1.size()));
+
+  const int64_t resource_id2 = GetNewResourceId();
+  const std::string resource_data2 = "imported script data";
+  result = WriteResource(resource_id2, resource_data2);
+  ASSERT_GT(result, 0);
+  resources.push_back(storage::mojom::ServiceWorkerResourceRecord::New(
+      resource_id2, kImportedScriptUrl, resource_data2.size()));
+
+  const int64_t registration_id = GetNewRegistrationId();
+  const int64_t version_id = GetNewVersionId();
+  RegistrationData registration_data = CreateRegistrationData(
+      registration_id, version_id, kScope, kScriptUrl, resources);
+  DatabaseStatus status =
+      StoreRegistration(std::move(registration_data), std::move(resources));
+  ASSERT_EQ(status, DatabaseStatus::kOk);
+
+  // Create two references.
+  mojo::Remote<storage::mojom::ServiceWorkerLiveVersionRef> reference1;
+  {
+    FindRegistrationResult result =
+        FindRegistrationForId(registration_id, kScope.GetOrigin());
+    ASSERT_EQ(result->status, DatabaseStatus::kOk);
+    ASSERT_TRUE(result->version_reference);
+    reference1.Bind(std::move(result->version_reference));
+  }
+
+  mojo::Remote<storage::mojom::ServiceWorkerLiveVersionRef> reference2;
+  {
+    FindRegistrationResult result =
+        FindRegistrationForId(registration_id, kScope.GetOrigin());
+    ASSERT_EQ(result->status, DatabaseStatus::kOk);
+    ASSERT_TRUE(result->version_reference);
+    reference2.Bind(std::move(result->version_reference));
+  }
+
+  // Drop the first reference and delete the registration.
+  reference1.reset();
+  {
+    DeleteRegistrationResult result =
+        DeleteRegistration(registration_id, kScope.GetOrigin());
+    ASSERT_EQ(result.status, DatabaseStatus::kOk);
+  }
+
+  // Make sure all tasks are ran.
+  // TODO(bashi): Don't rely on RunAllTasksUntilIdle()?
+  content::RunAllTasksUntilIdle();
+
+  // Resources shouldn't be purged because there is an active reference.
+  {
+    std::string read_resource_data1 =
+        ReadResource(resource_id1, resource_data1.size());
+    ASSERT_EQ(read_resource_data1, resource_data1);
+    std::string read_resource_data2 =
+        ReadResource(resource_id2, resource_data2.size());
+    ASSERT_EQ(read_resource_data2, resource_data2);
+  }
+
+  // Drop the second reference.
+  reference2.reset();
+  content::RunAllTasksUntilIdle();
+
+  // Resources should have been purged.
+  {
+    std::string read_resource_data1 =
+        ReadResource(resource_id1, resource_data1.size());
+    ASSERT_EQ(read_resource_data1, "");
+    std::string read_resource_data2 =
+        ReadResource(resource_id2, resource_data2.size());
+    ASSERT_EQ(read_resource_data2, "");
   }
 }
 
