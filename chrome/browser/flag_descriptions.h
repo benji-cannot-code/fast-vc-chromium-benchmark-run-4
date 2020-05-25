acb09d8c5c50f3e93956c675ef6517b1fdb1c46f
Joel Hockey <joelhockey@chromium.org>
2020-05-25 01:02:23 +0000

    Removed unused terminal-system-app-splits flag
    Change-Id: I94f00041f8f0a738f9e8659839c6e3821c8a3228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214581
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Julian Watson <juwa@google.com>
    Reviewed-by: Julian Watson <juwa@google.com>
    Cr-Commit-Position: refs/heads/master@{#771446}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2089,9 +2089,6 @@ extern const char kTerminalSystemAppDescription[];
 extern const char kTerminalSystemAppLegacySettingsName[];
 extern const char kTerminalSystemAppLegacySettingsDescription[];
 
-extern const char kTerminalSystemAppSplitsName[];
-extern const char kTerminalSystemAppSplitsDescription[];
-
 extern const char kTrimOnFreezeName[];
 extern const char kTrimOnFreezeDescription[];
 
