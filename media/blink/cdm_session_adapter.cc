8ea9e460be0eec10ac2e9709d11c0ba5b0b3ac25
Vi Nguyen <ving@microsoft.com>
2020-05-25 18:50:04 +0000

    Pass MediaKeySessionType in createSession()
    Courtesy of jrummell's TODO: Now that there are 2 types of persistent
    sessions, the session type should be passed from blink. Type should also
    be passed in the constructor (and removed from initializeNewSession()).
    
    Bug: 1085463
    Change-Id: Ic6f5f496d7f696d1563c0af0c80e42c8d54ec060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212685
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Vi Nguyen <ving@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#771612}

--- media/blink/cdm_session_adapter.cc
+++ media/blink/cdm_session_adapter.cc
@@ -77,8 +77,10 @@ void CdmSessionAdapter::GetStatusForPolicy(
 }
 
 std::unique_ptr<WebContentDecryptionModuleSessionImpl>
-CdmSessionAdapter::CreateSession() {
-  return std::make_unique<WebContentDecryptionModuleSessionImpl>(this);
+CdmSessionAdapter::CreateSession(
+    blink::WebEncryptedMediaSessionType session_type) {
+  return std::make_unique<WebContentDecryptionModuleSessionImpl>(this,
+                                                                 session_type);
 }
 
 bool CdmSessionAdapter::RegisterSession(
