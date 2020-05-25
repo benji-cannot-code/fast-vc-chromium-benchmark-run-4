0f0a3455c5e0e57bcd0e8af331118f2a519bd05d
arthursonzogni <arthursonzogni@chromium.org>
2020-05-25 12:41:49 +0000

    [OutOfBlinkSandbox] Compute sandbox flags in RenderFrameHost
    Compute the sandbox policy of a document in the RenderFrameHost.
    
    For now, this is simply used to double check the value computed from the
    renderer. If this works correctly, the browser process will use it and
    stop depending on the renderer process.
    
    Ideally, this patch must land for M84, in order to get useful reports.
    
    This patch is a step in the following direction:
    ~~
    Stop parsing the HTTP CSP sandbox header in blink. Parse it in the
    network service CSP parser. This allows the browser process to compute
    the Origin of the new document before loading it.
    ~~
    https://docs.google.com/document/d/1PechV73KKMF8leh7uTlyGkR32kD5qILvu3ZeG8TYGfk/edit?usp=sharing
    
    Bug: 1041376
    Change-Id: I26d0e3744bc1b5b2d893fd01b6e6ecea35cf47e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190680
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771551}

--- content/browser/frame_host/navigation_request.h
+++ content/browser/frame_host/navigation_request.h
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "services/network/public/cpp/origin_policy.h"
 #include "services/network/public/mojom/blocked_by_response_reason.mojom-shared.h"
+#include "services/network/public/mojom/web_sandbox_flags.mojom-shared.h"
 
 #if defined(OS_ANDROID)
 #include "base/android/scoped_java_ref.h"
@@ -615,6 +616,14 @@ class CONTENT_EXPORT NavigationRequest
   std::vector<mojo::PendingReceiver<network::mojom::CookieAccessObserver>>
   TakeCookieObservers() WARN_UNUSED_RESULT;
 
+  // The sandbox policy of the document to be loaded. This returns nullopt for
+  // navigations that haven't reached the 'ReadyToCommit' stage yet. In
+  // particular, this returns nullopt for same-document navigations.
+  //
+  // TODO(arthursonzogni): After RenderDocument, this can be computed and stored
+  // directly into the RenderDocumentHost.
+  base::Optional<network::mojom::WebSandboxFlags> SandboxFlagsToCommit();
+
  private:
   friend class NavigationRequestTest;
 
@@ -956,6 +965,10 @@ class CONTENT_EXPORT NavigationRequest
   // Convenience function to return the NavigationControllerImpl this
   // NavigationRequest is in.
   NavigationControllerImpl* GetNavigationController();
+
+  // Compute the sandbox policy of the document to be loaded. Called once when
+  // reaching the 'ReadyToCommit' stage.
+  network::mojom::WebSandboxFlags ComputeSandboxFlagsToCommit();
 
   FrameTreeNode* frame_tree_node_;
 
@@ -1308,6 +1321,10 @@ class CONTENT_EXPORT NavigationRequest
   // Observers listening to cookie access notifications for the network requests
   // made by this navigation.
   mojo::ReceiverSet<network::mojom::CookieAccessObserver> cookie_observers_;
+
+  // The sandbox flags of the document to be loaded. This is computed at
+  // 'ReadyToCommit' time.
+  base::Optional<network::mojom::WebSandboxFlags> sandbox_flags_to_commit_;
 
   base::WeakPtrFactory<NavigationRequest> weak_factory_{this};
 
