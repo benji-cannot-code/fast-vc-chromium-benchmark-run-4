1d1332974efcb121ac44f72d47c5d0fa21b0c9bc
Yuki Shiino <yukishiino@chromium.org>
2020-05-25 15:46:48 +0000

    bind-gen: Use SetAcceptAnyReceiver appropriately
    Bug: 839389
    Change-Id: I54aadee2177b0470a76e909581862c8fe90e5706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212186
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771576}

--- third_party/blink/renderer/bindings/scripts/bind_gen/interface.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/interface.py
@@ -4127,6 +4127,24 @@ def _make_property_entry_check_receiver(property_):
         return "V8DOMConfiguration::kCheckHolder"
 
 
+def _make_property_entry_check_cross_origin_access(property_,
+                                                   is_get=False,
+                                                   is_set=False):
+    constants = {
+        True: "V8DOMConfiguration::kDoNotCheckAccess",
+        False: "V8DOMConfiguration::kCheckAccess",
+    }
+    if "CrossOrigin" not in property_.extended_attributes:
+        return constants[False]
+    values = property_.extended_attributes.values_of("CrossOrigin")
+    if is_get:
+        return constants[not values or "Getter" in values]
+    elif is_set:
+        return constants["Setter" in values]
+    else:
+        return constants[True]
+
+
 def _make_property_entry_has_side_effect(property_):
     if property_.extended_attributes.value_of("Affects") == "Nothing":
         return "V8DOMConfiguration::kHasNoSideEffect"
@@ -4179,8 +4197,9 @@ def _make_attribute_registration_table(table_name, attribute_entries):
                    "{v8_property_attribute}, "
                    "{on_which_object}, "
                    "{check_receiver}, "
+                   "{check_cross_origin_get_access}, "
+                   "{check_cross_origin_set_access}, "
                    "{has_side_effect}, "
-                   "V8DOMConfiguration::kAlwaysCallGetter, "
                    "{world}"
                    "}},")
         text = _format(
@@ -4196,6 +4215,12 @@ def _make_attribute_registration_table(table_name, attribute_entries):
                 entry.property_),
             check_receiver=_make_property_entry_check_receiver(
                 entry.property_),
+            check_cross_origin_get_access=(
+                _make_property_entry_check_cross_origin_access(entry.property_,
+                                                               is_get=True)),
+            check_cross_origin_set_access=(
+                _make_property_entry_check_cross_origin_access(entry.property_,
+                                                               is_set=True)),
             has_side_effect=_make_property_entry_has_side_effect(
                 entry.property_),
             world=_make_property_entry_world(entry.world))
@@ -4328,7 +4353,7 @@ def _make_operation_registration_table(table_name, operation_entries):
                    "{v8_property_attribute}, "
                    "{on_which_object}, "
                    "{check_receiver}, "
-                   "V8DOMConfiguration::kDoNotCheckAccess, "
+                   "{check_cross_origin_access}, "
                    "{has_side_effect}, "
                    "{world}"
                    "}}, ")
@@ -4343,6 +4368,9 @@ def _make_operation_registration_table(table_name, operation_entries):
                 entry.property_),
             check_receiver=_make_property_entry_check_receiver(
                 entry.property_),
+            check_cross_origin_access=(
+                _make_property_entry_check_cross_origin_access(
+                    entry.property_)),
             has_side_effect=_make_property_entry_has_side_effect(
                 entry.property_),
             world=_make_property_entry_world(entry.world))
@@ -6122,7 +6150,7 @@ def generate_interface(interface):
                     attribute_set=True,
                     arg_decls=[
                         "v8::Local<v8::Value>",
-                        "const v8::PropertyCallbackInfo<v8::Value>&",
+                        "const v8::PropertyCallbackInfo<void>&",
                     ])
     for operation_group in interface.operation_groups:
         if "Custom" in operation_group.extended_attributes:
