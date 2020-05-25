dcca12215334107f1cf9515344f46808b4dae955
Marina Ciocea <marinaciocea@chromium.org>
2020-05-25 14:05:42 +0000

    Revert "Re-enable ChromeVoxBackground*.NavigationSyncSelect"
    This reverts commit f0f922f740a33fe7876b4c27e8eb360564e51a6d.
    
    Reason for revert: Failing flakily on Linux Chromium OS ASan LSan Tests (1):
    - https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/37738
    - https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/37731
    - https://ci.chromium.org/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/37729
    
    Original change's description:
    > Re-enable ChromeVoxBackground*.NavigationSyncSelect
    >
    > Mostly speculative; pass with
    >
    > testing/xvfb.py out/d/browser_tests --gtest_filter=ChromeVox*.NavigationSyncsSelect
    > !! & !! & !! & !! &
    >
    > TBR=dtseng@chromium.org
    >
    > Fixed: 976105
    > Change-Id: Ieb646dd0ab22d4b45f2355c89542a59c51af5888
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209684
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Commit-Queue: David Tseng <dtseng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#770789}
    
    TBR=dtseng@chromium.org
    
    Change-Id: I32c4f0d36e7334cd99e51a25b960d9b56fa32d17
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2213524
    Reviewed-by: Marina Ciocea <marinaciocea@chromium.org>
    Commit-Queue: Marina Ciocea <marinaciocea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771563}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
@@ -1583,7 +1583,7 @@ TEST_F('ChromeVoxBackgroundTest', 'NavigationEscapesEdit', function() {
       });
 });
 
-TEST_F('ChromeVoxBackgroundTest', 'NavigationSyncsSelect', function() {
+TEST_F('ChromeVoxBackgroundTest', 'DISABLED_NavigationSyncsSelect', function() {
   const mockFeedback = this.createMockFeedback();
   this.runWithLoadedTree(
       `
