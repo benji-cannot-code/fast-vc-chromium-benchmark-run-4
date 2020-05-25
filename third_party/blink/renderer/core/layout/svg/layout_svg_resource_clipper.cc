fadbe1ce947d179155fec91d6596ea930c8820c2
Fredrik Söderqvist <fs@opera.com>
2020-05-25 09:18:25 +0000

    Add SVGAnimatedEnumeration<Enum>::CurrentEnumValue helper
    Everything that accesses SVGAnimatedEnumeration<Enum> will read the
    current enum value, so cut directly to the chase instead and drop the
    unnecessary CurrentValue/BaseValue overloads.
    
    Change-Id: Ib7b56ae106ca6531e461e3b47973782112860ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207450
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#771518}

--- third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_resource_clipper.cc
@@ -217,8 +217,7 @@ void LayoutSVGResourceClipper::CalculateLocalClipBounds() {
 SVGUnitTypes::SVGUnitType LayoutSVGResourceClipper::ClipPathUnits() const {
   return To<SVGClipPathElement>(GetElement())
       ->clipPathUnits()
-      ->CurrentValue()
-      ->EnumValue();
+      ->CurrentEnumValue();
 }
 
 AffineTransform LayoutSVGResourceClipper::CalculateClipTransform(
--- third_party/blink/renderer/core/layout/svg/svg_text_layout_engine.cc
+++ third_party/blink/renderer/core/layout/svg/svg_text_layout_engine.cc
@@ -186,8 +186,7 @@ void SVGTextLayoutEngine::BeginTextPathLayout(SVGInlineFlowBox* flow_box) {
   if (SVGTextContentElement* text_content_element =
           SVGTextContentElement::ElementFromLineLayoutItem(text_path)) {
     SVGLengthContext length_context(text_content_element);
-    length_adjust =
-        text_content_element->lengthAdjust()->CurrentValue()->EnumValue();
+    length_adjust = text_content_element->lengthAdjust()->CurrentEnumValue();
     if (text_content_element->TextLengthIsSpecifiedByUser())
       desired_text_length =
           text_content_element->textLength()->CurrentValue()->Value(
@@ -253,7 +252,7 @@ static bool DefinesTextLengthWithSpacing(const InlineFlowBox* start) {
       SVGTextContentElement::ElementFromLineLayoutItem(
           start->GetLineLayoutItem());
   return text_content_element &&
-         text_content_element->lengthAdjust()->CurrentValue()->EnumValue() ==
+         text_content_element->lengthAdjust()->CurrentEnumValue() ==
              kSVGLengthAdjustSpacing &&
          text_content_element->TextLengthIsSpecifiedByUser();
 }
