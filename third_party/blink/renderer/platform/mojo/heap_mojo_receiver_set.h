a79c0c978d2e851e9e946112e5652a75a2b66593
Minoru Chikamune <chikamune@chromium.org>
2020-05-25 20:51:43 +0000

    Add ContextType to HeapMojoReceiverSet as a template parameter.
    The original mojo::ReceiverSet can be specified ContextType.
    This CL add the same functionarity to  HeapMojoReceiverSet.
    This CL also adds ContextType related functionarity.
    
    Bug: 1049056
    Change-Id: I8639e34760fd16c6e70302c03ddadfa853ae2ecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214854
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771631}

--- third_party/blink/renderer/platform/mojo/heap_mojo_receiver_set.h
+++ third_party/blink/renderer/platform/mojo/heap_mojo_receiver_set.h
@@ -25,11 +25,14 @@ namespace blink {
 // that the interface is not used after ContextDestroyed().
 template <typename Interface,
           typename Owner,
-          HeapMojoWrapperMode Mode = HeapMojoWrapperMode::kWithContextObserver>
+          HeapMojoWrapperMode Mode = HeapMojoWrapperMode::kWithContextObserver,
+          typename ContextType = void>
 class HeapMojoReceiverSet {
   DISALLOW_NEW();
 
  public:
+  using ContextTraits = mojo::ReceiverSetContextTraits<ContextType>;
+  using Context = typename ContextTraits::Type;
   explicit HeapMojoReceiverSet(Owner* owner, ContextLifecycleNotifier* context)
       : wrapper_(MakeGarbageCollected<Wrapper>(owner, context)) {
     static_assert(std::is_base_of<Interface, Owner>::value,
@@ -48,6 +51,14 @@ class HeapMojoReceiverSet {
                                         task_runner);
   }
 
+  mojo::ReceiverId Add(mojo::PendingReceiver<Interface> receiver,
+                       Context context,
+                       scoped_refptr<base::SequencedTaskRunner> task_runner) {
+    DCHECK(task_runner);
+    return wrapper_->receiver_set().Add(wrapper_->owner(), std::move(receiver),
+                                        std::move(context), task_runner);
+  }
+
   bool Remove(mojo::ReceiverId id) {
     return wrapper_->receiver_set().Remove(id);
   }
@@ -56,6 +67,12 @@ class HeapMojoReceiverSet {
 
   bool HasReceiver(mojo::ReceiverId id) {
     return wrapper_->receiver_set().HasReceiver(id);
+  }
+
+  bool empty() const { return wrapper_->receiver_set().empty(); }
+  size_t size() const { return wrapper_->receiver_set().size(); }
+  const Context& current_context() const {
+    return wrapper_->receiver_set().current_context();
   }
 
   void Trace(Visitor* visitor) const { visitor->Trace(wrapper_); }
@@ -83,7 +100,9 @@ class HeapMojoReceiverSet {
 
     void Dispose() { receiver_set_.Clear(); }
 
-    mojo::ReceiverSet<Interface>& receiver_set() { return receiver_set_; }
+    mojo::ReceiverSet<Interface, ContextType>& receiver_set() {
+      return receiver_set_;
+    }
     Owner* owner() { return owner_; }
 
     // ContextLifecycleObserver methods
@@ -94,7 +113,7 @@ class HeapMojoReceiverSet {
 
    private:
     Member<Owner> owner_;
-    mojo::ReceiverSet<Interface> receiver_set_;
+    mojo::ReceiverSet<Interface, ContextType> receiver_set_;
   };
 
   Member<Wrapper> wrapper_;
