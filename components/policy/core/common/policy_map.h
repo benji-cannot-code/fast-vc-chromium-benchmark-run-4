2b34b4c6bd9e3fe77e2b92e90514d2e6a13883da
Aya ElAttar <ayaelattar@google.com>
2020-05-25 21:18:55 +0000

    Add new PolicyMap::Entry constructor
    Added new PolicyMap::Entry constructor to accept moved
    base::Value instead of moving unique_ptr to be consistent
    with the base:: Value new design.
    The old constructor should be removed when all its references
    are fixed accordingly.
    
    Change-Id: I8e520bac0fc0e60b6e117014b6d85781c9a21cdb
    Bug: 646113, 1083984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207214
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Aya Elsayed <ayaelattar@google.com>
    Cr-Commit-Position: refs/heads/master@{#771636}

--- components/policy/core/common/policy_map.h
+++ components/policy/core/common/policy_map.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "base/optional.h"
 #include "base/values.h"
 #include "components/policy/core/common/external_data_fetcher.h"
 #include "components/policy/core/common/policy_types.h"
@@ -49,6 +50,11 @@ class POLICY_EXPORT PolicyMap {
           PolicyScope scope,
           PolicySource source,
           std::unique_ptr<base::Value> value,
+          std::unique_ptr<ExternalDataFetcher> external_data_fetcher);
+    Entry(PolicyLevel level,
+          PolicyScope scope,
+          PolicySource source,
+          base::Optional<base::Value> value,
           std::unique_ptr<ExternalDataFetcher> external_data_fetcher);
     ~Entry();
 
