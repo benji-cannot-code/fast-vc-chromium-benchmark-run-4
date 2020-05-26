5b6261822214b3b6e68c7a3e4c4d32204fac09c9
jorgegil@google.com <jorgegil@google.com>
2020-05-26 17:00:23 +0000

    Add is_tracking_occlusion to the surface state
    Fixes issue where occlusion tracking could start sending
    updates before the surface state had been committed. In
    ARC, this meant that occlusion updates were sent before
    the surface bounds were first set, leading to incorrect
    "full occlusion (1.0f) updates".
    
    Bug: b:149229321
    Test: exo_unittests
    Change-Id: Ia885a44cf38658cc82d87273f70395c8bc2fcf72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212921
    Commit-Queue: Jorge Gil <jorgegil@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771850}

--- components/exo/surface.h
+++ components/exo/surface.h
@@ -282,7 +282,7 @@ class Surface final : public ui::PropertyHandler {
   void OnWindowOcclusionChanged();
 
   // True if the window for this surface has its occlusion tracked.
-  bool is_tracking_occlusion() const { return is_tracking_occlusion_; }
+  bool IsTrackingOcclusion();
 
   // Sets the |surface_hierarchy_content_bounds_|.
   void SetSurfaceHierarchyContentBoundsForTest(const gfx::Rect& content_bounds);
@@ -310,6 +310,7 @@ class Surface final : public ui::PropertyHandler {
     float alpha = 1.0f;
     gfx::Vector2d offset;
     gfx::ColorSpace color_space;
+    bool is_tracking_occlusion = false;
   };
   class BufferAttachment {
    public:
@@ -451,9 +452,6 @@ class Surface final : public ui::PropertyHandler {
 
   // Surface observer list. Surface does not own the observers.
   base::ObserverList<SurfaceObserver, true>::Unchecked observers_;
-
-  // Whether this surface is tracking occlusion for the client.
-  bool is_tracking_occlusion_ = false;
 
 #if defined(OS_CHROMEOS)
   std::unique_ptr<ash::OutputProtectionDelegate> output_protection_;
--- components/exo/wayland/zaura_shell_unittest.cc
+++ components/exo/wayland/zaura_shell_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desks_util.h"
 #include "ash/wm/window_util.h"
 #include "base/time/time.h"
+#include "components/exo/buffer.h"
 #include "components/exo/test/exo_test_base.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/window_occlusion_tracker.h"
@@ -68,7 +69,13 @@ class ZAuraSurfaceTest : public test::ExoTestBase,
   void SetUp() override {
     test::ExoTestBase::SetUp();
 
+    gfx::Size buffer_size(10, 10);
+    std::unique_ptr<Buffer> buffer(
+        new Buffer(exo_test_helper()->CreateGpuMemoryBuffer(buffer_size)));
+
     surface_.reset(new Surface);
+    surface_->Attach(buffer.get());
+
     aura_surface_.reset(new TestAuraSurface(surface_.get()));
 
     gfx::Transform transform;
@@ -129,8 +136,25 @@ class ZAuraSurfaceTest : public test::ExoTestBase,
   DISALLOW_COPY_AND_ASSIGN(ZAuraSurfaceTest);
 };
 
+TEST_F(ZAuraSurfaceTest, OcclusionTrackingStartsAfterCommit) {
+  surface().OnWindowOcclusionChanged();
+
+  EXPECT_EQ(-1.0f, aura_surface().last_sent_occlusion_fraction());
+  EXPECT_EQ(0, aura_surface().num_occlusion_updates());
+  EXPECT_FALSE(surface().IsTrackingOcclusion());
+
+  auto widget = CreateOpaqueWidget(gfx::Rect(0, 0, 10, 10));
+  widget->Show();
+  surface().Commit();
+
+  EXPECT_EQ(0.2f, aura_surface().last_sent_occlusion_fraction());
+  EXPECT_EQ(1, aura_surface().num_occlusion_updates());
+  EXPECT_TRUE(surface().IsTrackingOcclusion());
+}
+
 TEST_F(ZAuraSurfaceTest,
        LosingActivationWithNoAnimatingWindowsSendsCorrectOcclusionFraction) {
+  surface().Commit();
   EXPECT_EQ(0.0f, aura_surface().last_sent_occlusion_fraction());
   EXPECT_EQ(1, aura_surface().num_occlusion_updates());
   ::wm::ActivateWindow(parent_widget().GetNativeWindow());
@@ -147,6 +171,7 @@ TEST_F(ZAuraSurfaceTest,
 
 TEST_F(ZAuraSurfaceTest,
        LosingActivationWithAnimatingWindowsSendsTargetOcclusionFraction) {
+  surface().Commit();
   EXPECT_EQ(0.0f, aura_surface().last_sent_occlusion_fraction());
   EXPECT_EQ(1, aura_surface().num_occlusion_updates());
   ::wm::ActivateWindow(parent_widget().GetNativeWindow());
@@ -195,6 +220,7 @@ TEST_F(ZAuraSurfaceTest,
 
 TEST_F(ZAuraSurfaceTest,
        LosingActivationByTriggeringTheLockScreenDoesNotSendOccludedFraction) {
+  surface().Commit();
   EXPECT_EQ(0.0f, aura_surface().last_sent_occlusion_fraction());
   EXPECT_EQ(1, aura_surface().num_occlusion_updates());
   ::wm::ActivateWindow(parent_widget().GetNativeWindow());
@@ -228,9 +254,16 @@ TEST_F(ZAuraSurfaceTest,
 
 TEST_F(ZAuraSurfaceTest, OcclusionIncludesOffScreenArea) {
   UpdateDisplay("150x150");
+
+  gfx::Size buffer_size(80, 100);
+  std::unique_ptr<Buffer> buffer(
+      new Buffer(exo_test_helper()->CreateGpuMemoryBuffer(buffer_size)));
   // This is scaled by 1.5 - set the bounds to (-60, 75, 120, 150) in screen
   // coordinates so 75% of it is outside of the 100x100 screen.
   surface().window()->SetBounds(gfx::Rect(-40, 50, 80, 100));
+  surface().Attach(buffer.get());
+  surface().Commit();
+
   surface().OnWindowOcclusionChanged();
 
   EXPECT_EQ(0.75f, aura_surface().last_sent_occlusion_fraction());
@@ -239,6 +272,7 @@ TEST_F(ZAuraSurfaceTest, OcclusionIncludesOffScreenArea) {
 TEST_F(ZAuraSurfaceTest, ZeroSizeWindowSendsZeroOcclusionFraction) {
   // Zero sized window should not be occluded.
   surface().window()->SetBounds(gfx::Rect(0, 0, 0, 0));
+  surface().Commit();
   surface().OnWindowOcclusionChanged();
   EXPECT_EQ(0.0f, aura_surface().last_sent_occlusion_fraction());
 }
