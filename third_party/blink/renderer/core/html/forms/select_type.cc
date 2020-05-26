a8e2b7666df7121000b637405da3904ad0a23f1c
Kent Tamura <tkent@chromium.org>
2020-05-26 04:42:50 +0000

    Make SelectType::DidSelectOption() a pure virtual
    The content of the function was very small. This CL folds
    SelectType::DidSelectOption() into call sites, and makes it
    a pure virtual function.
    
    Note that ScrollToSelection() for MenuListSelectType is empty.
    So MenuListSelectType::DidSelectOption() doesn't need it.
    
    This CL has no behavior changes.
    
    Bug: 1052232
    Change-Id: I1b483118546c15788d683b97e83da931619fe18b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215146
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771684}

--- third_party/blink/renderer/core/html/forms/select_type.cc
+++ third_party/blink/renderer/core/html/forms/select_type.cc
@@ -366,7 +366,7 @@ void MenuListSelectType::DidSelectOption(
   if (PopupIsVisible() && should_update_popup)
     popup_->UpdateFromElement(PopupMenu::kBySelectionChange);
 
-  SelectType::DidSelectOption(element, flags, should_update_popup);
+  select_->SetNeedsValidityCheck();
 
   if (should_dispatch_events) {
     select_->DispatchInputEvent();
@@ -934,7 +934,8 @@ void ListBoxSelectType::DidSelectOption(
       select_->SetActiveSelectionEnd(element);
   }
 
-  SelectType::DidSelectOption(element, flags, should_update_popup);
+  ScrollToSelection();
+  select_->SetNeedsValidityCheck();
 }
 
 void ListBoxSelectType::OptionRemoved(HTMLOptionElement& option) {
@@ -1243,13 +1244,6 @@ void SelectType::WillBeDestroyed() {
 
 void SelectType::Trace(Visitor* visitor) const {
   visitor->Trace(select_);
-}
-
-void SelectType::DidSelectOption(HTMLOptionElement*,
-                                 HTMLSelectElement::SelectOptionFlags,
-                                 bool) {
-  ScrollToSelection();
-  select_->SetNeedsValidityCheck();
 }
 
 void SelectType::OptionRemoved(HTMLOptionElement& option) {}
