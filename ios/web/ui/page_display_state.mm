7c97920e124e4efb65efb5153431646d7cc43afa
Gauthier Ambard <gambard@chromium.org>
2020-05-26 10:20:27 +0000

    [iOS] Remove deprecated keys for PageDisplayState
    Fixed: 926041
    Change-Id: Ib40621f5039fcc6440968a397da1438aca7fbb78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215841
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771739}

--- ios/web/ui/page_display_state.mm
+++ ios/web/ui/page_display_state.mm
@@ -20,10 +20,6 @@ NSString* const kContentInsetKey = @"contentInset";
 NSString* const kMinZoomKey = @"minZoom";
 NSString* const kMaxZoomKey = @"maxZoom";
 NSString* const kZoomKey = @"zoom";
-// Deprecated serialization keys.
-// TODO(crbug.com/926041): Remove these keys.
-NSString* const kDeprecatedXOffsetKey = @"scrollX";
-NSString* const kDeprecatedYOffsetKey = @"scrollY";
 // Invalid consts.
 const CGPoint kInvalidContentOffset = {NAN, NAN};
 const UIEdgeInsets kInvalidContentInset = {NAN, NAN, NAN, NAN};
@@ -63,10 +59,7 @@ inline CGPoint GetContentOffset(NSDictionary* serialization) {
   NSValue* value = serialization[kContentOffsetKey];
   if (value)
     return [value CGPointValue];
-  // TODO(crbug.com/926041): Return kInvalidContentOffset when legacy keys are
-  // removed.
-  return CGPointMake(GetFloatValue(kDeprecatedXOffsetKey, serialization),
-                     GetFloatValue(kDeprecatedYOffsetKey, serialization));
+  return kInvalidContentOffset;
 }
 // Returns the contentInset stored in |serialization|, or a NAN inset if it is
 // not set.
@@ -74,14 +67,6 @@ inline UIEdgeInsets GetContentInset(NSDictionary* serialization) {
   NSValue* value = serialization[kContentInsetKey];
   if (value)
     return [value UIEdgeInsetsValue];
-  if (serialization[kDeprecatedXOffsetKey] &&
-      serialization[kDeprecatedYOffsetKey]) {
-    // When restoring PageScrollStates created using the deprecated
-    // serialization keyes, use UIEdgeInsetsZero as default.
-    // TODO(crbug.com/926041): Just return kInvalidContentInset when legacy keys
-    // are removed.
-    return UIEdgeInsetsZero;
-  }
   // Return an invalid inset if neither the new nor legacy keys were contained.
   return kInvalidContentInset;
 }
