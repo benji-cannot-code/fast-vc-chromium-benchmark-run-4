50946070acb338d4e518842be6e91dc5f0399764
Ben Kelly <wanderview@chromium.org>
2020-05-26 19:48:52 +0000

    CacheStorage: Persist explicit response mime type.
    This CL fixes a small interop issue where chromium would always
    recompute the mime type for a response after reading it from
    cache_storage.  This was incorrect because the mime type should be
    fixed at original response construction time even if its headers
    are later mutated.
    
    This CL also causes service workers to use the mime type originally
    computed for the Response instead of re-extracting the value from the
    content-type header.
    
    Bug: 938939
    Change-Id: I941c1b1e3863e41da49128ce814ac0f1762dae70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2211949
    Commit-Queue: Ben Kelly <wanderview@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771921}

--- third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/mime-sniffing-worker.js
+++ third_party/blink/web_tests/external/wpt/service-workers/service-worker/resources/mime-sniffing-worker.js
@@ -1,6 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 self.addEventListener('fetch', function(event) {
-    var res = new Response('<!DOCTYPE html>\n<h1 id=\'testid\'>test</h1>');
-    res.headers.delete('content-type');
+    // Use an empty content-type value to force mime-sniffing.  Note, this
+    // must be passed to the constructor since the mime-type of the Response
+    // is fixed and cannot be later changed.
+    var res = new Response('<!DOCTYPE html>\n<h1 id=\'testid\'>test</h1>', {
+      headers: { 'content-type': '' }
+    });
     event.respondWith(res);
   });
