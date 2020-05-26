6f26a10e5ae6a80a0dfed2f68d08fadf47e42bd3
Joel Fernandes <joelaf@google.com>
2020-05-26 22:59:29 +0000

    CrOS: Change core-scheduling prctl value to a placeholder-value
    The prctl in upstream kernel keeps changing as new prctls are added.
    Numbers 57 and 58 are already taken. Let us choose 0x200 (512) to be
    safe.
    
    While the core-scheduling patchset is making great upstream progress,
    the prctl(2) interface to use it will take time upstream and we might
    have to carry it in product kernels for a while. Let us change it to
    0x200 to keep products working. We can always change it to the final one
    later.
    
    BUG=b:152605392
    
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: I15d7a534cab35678b017991ac67bae54128b43a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2213330
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772005}

--- chromeos/system/core_scheduling.cc
+++ chromeos/system/core_scheduling.cc
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/field_trial_params.h"
 
 #ifndef PR_SET_CORE_SCHED
-#define PR_SET_CORE_SCHED 57
+// Setup core-scheduling for the task. This value is a temporary
+// place holder till the final upstream value is known.
+#define PR_SET_CORE_SCHED 0x200
 #endif
 
 namespace chromeos {
