551c63602b47074be03af80d40ba3aec9c6ff59b
Dominick Ng <dominickn@chromium.org>
2020-05-26 07:57:46 +0000

    Enable app_search_provider_unittest.cc with BMO.
    Call TestWebAppProvider::Start() in the tests to stop the App Service
    waiting for web apps to be published.
    
    BUG=1082884
    
    Change-Id: I051a43fa56e739e22e9b1a70e6edf58f0a49b600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214587
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771712}

--- chrome/browser/ui/app_list/search/tests/app_search_provider_unittest.cc
+++ chrome/browser/ui/app_list/search/tests/app_search_provider_unittest.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/search/search_result_ranker/ranking_item_util.h"
 #include "chrome/browser/ui/app_list/test/fake_app_list_model_updater.h"
 #include "chrome/browser/ui/app_list/test/test_app_list_controller_delegate.h"
+#include "chrome/browser/web_applications/test/test_web_app_provider.h"
 #include "chrome/browser/web_applications/test/web_app_test.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_features.h"
@@ -725,6 +726,7 @@ TEST_P(AppSearchProviderCrostiniTest, CrostiniTerminal) {
   EXPECT_EQ("", RunQuery("linux"));
 
   // This both allows Crostini UI and enables Crostini.
+  web_app::TestWebAppProvider::Get(testing_profile())->Start();
   crostini::CrostiniTestHelper crostini_test_helper(testing_profile());
   crostini_test_helper.ReInitializeAppServiceIntegration();
   CreateSearch();
@@ -752,6 +754,7 @@ TEST_P(AppSearchProviderCrostiniTest, CrostiniTerminal) {
 
 TEST_P(AppSearchProviderCrostiniTest, CrostiniApp) {
   // This both allows Crostini UI and enables Crostini.
+  web_app::TestWebAppProvider::Get(testing_profile())->Start();
   crostini::CrostiniTestHelper crostini_test_helper(testing_profile());
   crostini_test_helper.ReInitializeAppServiceIntegration();
   CreateSearch();
@@ -1056,10 +1059,10 @@ INSTANTIATE_TEST_SUITE_P(
     ::testing::ValuesIn({TestArcAppInstallType::CONTROLLED_BY_POLICY,
                          TestArcAppInstallType::INSTALLED_BY_DEFAULT}));
 
-// TODO(crbug.com/1082884): Test with BMO enabled.
 INSTANTIATE_TEST_SUITE_P(All,
                          AppSearchProviderCrostiniTest,
-                         ::testing::Values(ProviderType::kBookmarkApps),
+                         ::testing::Values(ProviderType::kBookmarkApps,
+                                           ProviderType::kWebApps),
                          web_app::ProviderTypeParamToString);
 
 }  // namespace test
