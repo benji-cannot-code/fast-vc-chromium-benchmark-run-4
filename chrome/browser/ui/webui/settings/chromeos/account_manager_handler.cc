038f8b0ed520bc5d6c237bc740b615d18e5010a8
Kush Sinha <sinhak@chromium.org>
2020-05-26 16:42:00 +0000

    Add metrics for crOS account addition/re-auth trigger
    Add metrics to identify the source of account additions and re-auth on
    Chrome OS.
    
    Bug: 1082780
    Test: Manually
    Change-Id: I1f3df57f5b5ef3c731f75156e61992812c7f78b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214531
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771843}

--- chrome/browser/ui/webui/settings/chromeos/account_manager_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/account_manager_handler.cc
@@ -344,7 +344,8 @@ base::ListValue AccountManagerUIHandler::GetSecondaryGaiaAccounts(
 
 void AccountManagerUIHandler::HandleAddAccount(const base::ListValue* args) {
   AllowJavascript();
-  InlineLoginHandlerDialogChromeOS::Show();
+  InlineLoginHandlerDialogChromeOS::Show(
+      InlineLoginHandlerDialogChromeOS::Source::kSettingsAddAccountButton);
 }
 
 void AccountManagerUIHandler::HandleReauthenticateAccount(
@@ -354,7 +355,9 @@ void AccountManagerUIHandler::HandleReauthenticateAccount(
   CHECK(!args->GetList().empty());
   const std::string& account_email = args->GetList()[0].GetString();
 
-  InlineLoginHandlerDialogChromeOS::Show(account_email);
+  InlineLoginHandlerDialogChromeOS::Show(
+      account_email,
+      InlineLoginHandlerDialogChromeOS::Source::kSettingsReauthAccountButton);
 }
 
 void AccountManagerUIHandler::HandleMigrateAccount(
--- chrome/browser/ui/webui/signin/inline_login_handler_dialog_chromeos.cc
+++ chrome/browser/ui/webui/signin/inline_login_handler_dialog_chromeos.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/json/json_writer.h"
 #include "base/macros.h"
+#include "base/metrics/histogram_functions.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/chromeos/system_web_dialog_delegate.h"
@@ -38,6 +39,9 @@ namespace {
 InlineLoginHandlerDialogChromeOS* dialog = nullptr;
 constexpr int kSigninDialogWidth = 768;
 constexpr int kSigninDialogHeight = 640;
+
+constexpr char kAccountAdditionSource[] =
+    "AccountManager.AccountAdditionSource";
 
 // Keep in sync with resources/chromeos/account_manager_error.js
 enum class AccountManagerErrorType {
@@ -94,6 +98,7 @@ GURL GetInlineLoginUrl(const std::string& email,
 // static
 void InlineLoginHandlerDialogChromeOS::Show(const std::string& email,
                                             const Source& source) {
+  base::UmaHistogramEnumeration(kAccountAdditionSource, source);
   // If the dialog was triggered as a response to background request, it could
   // get displayed on the lock screen. In this case it is safe to ignore it,
   // since in this case user will get it again after a request to Google
@@ -115,6 +120,10 @@ void InlineLoginHandlerDialogChromeOS::Show(const std::string& email,
   // Chrome OS is defined.
   ash::WindowBackdrop::Get(dialog->dialog_window())
       ->SetBackdropType(ash::WindowBackdrop::BackdropType::kSemiOpaque);
+}
+
+void InlineLoginHandlerDialogChromeOS::Show(const Source& source) {
+  Show(/* email= */ std::string(), source);
 }
 
 void InlineLoginHandlerDialogChromeOS::AdjustWidgetInitParams(
