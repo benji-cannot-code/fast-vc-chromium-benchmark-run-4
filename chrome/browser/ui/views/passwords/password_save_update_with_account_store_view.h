5200ea455304133d047ee9a9438c242b84b9a136
Mohamed Amir Yosef <mamir@chromium.org>
2020-05-26 12:18:24 +0000

    [Passwords] Use AnimatingLayoutManager instead of LayerAnimation ...
    ... to animate password save/update bubble.
    
    The reason for that is GetWidget()->GetLayer() only works for animating
    entire windows if the underlying widget is a `NativeWidgetAura`.
    DesktopNativeWidgetAura and NativeWidgetMac have an OS-provided "frame"
    that we can not animate in the same way.
    
    Implementing the animation on MacOS and Windows is more complex
    After CL the animation works on MacOS and Windows in the same way
    as on Linux.
    
    Here is a screencast on MacOS
    https://drive.google.com/file/d/133bqSrjVA9PIuSDjQNKmfsCHBMXOAo5w/view?usp=sharing
    
    Follow up CLs will fix the flickering of the dropdown menu.
    
    Bug: 1044038
    Change-Id: Id7a24bfb3049e3577c4ea7e2089b6679c41269d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195967
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771757}

--- chrome/browser/ui/views/passwords/password_save_update_with_account_store_view.h
+++ chrome/browser/ui/views/passwords/password_save_update_with_account_store_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/combobox/combobox_listener.h"
 #include "ui/views/controls/editable_combobox/editable_combobox_listener.h"
+#include "ui/views/layout/animating_layout_manager.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -39,8 +40,7 @@ class PasswordSaveUpdateWithAccountStoreView
   }
 
  private:
-  class BubbleExpansionObserver;
-
+  class AutoResizingLayout;
   ~PasswordSaveUpdateWithAccountStoreView() override;
 
   // PasswordBubbleViewBase
@@ -75,12 +75,6 @@ class PasswordSaveUpdateWithAccountStoreView
   void UpdateDialogButtonsAndAccountPickerVisiblity();
   std::unique_ptr<views::View> CreateFooterView();
 
-  // Starts animating the bubble to expand or contract it.
-  void StartResizing();
-  // Called when the expansion animation is finished. When the bubble it fully
-  // expanded, the account picker is shown.
-  void OnBubbleExpansionFinished();
-
   SaveUpdateWithAccountStoreBubbleController controller_;
 
   // True iff it is an update password bubble on creation. False iff it is a
@@ -94,14 +88,6 @@ class PasswordSaveUpdateWithAccountStoreView
 
   // The view for the password value.
   views::EditableCombobox* password_dropdown_;
-
-  // The height of widget layer when the account picker is visible.
-  int expanded_bubble_height_ = -1;
-
-  // Observers the animation of the bubbling during expansion and makes sure the
-  // OnBubbleExpansionFinished() is called when the bubble is fully expanded.
-  std::unique_ptr<BubbleExpansionObserver> expansion_observer_;
-
   bool are_passwords_revealed_;
 };
 
