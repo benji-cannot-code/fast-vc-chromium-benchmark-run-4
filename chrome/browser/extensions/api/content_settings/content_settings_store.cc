be90ef7d2722a3f5bbe809945534f7f89ddf366f
Ravjit Singh Uppal <ravjit@chromium.org>
2020-05-26 16:13:05 +0000

    Disallow wildcard patterns for plugins in extension policy provider
    Exceptions defined via extensions for plugin content settings, that have
    wildcard in their patterns will be completely ignored after this CL
    
    Bug: 1073883
    Change-Id: I47ee2a335101eb6a1660e5d1676cd4827c99809e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214529
    Commit-Queue: Ravjit Singh Uppal <ravjit@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771830}

--- chrome/browser/extensions/api/content_settings/content_settings_store.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_store.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/content_settings_utils.h"
 #include "components/content_settings/core/browser/website_settings_info.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/permissions/features.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -107,6 +108,12 @@ void ContentSettingsStore::SetExtensionContentSetting(
     const content_settings::ResourceIdentifier& identifier,
     ContentSetting setting,
     ExtensionPrefsScope scope) {
+  if (base::FeatureList::IsEnabled(
+          content_settings::kDisallowWildcardsInPluginContentSettings) &&
+      type == ContentSettingsType::PLUGINS && primary_pattern.HasWildcards()) {
+    return;
+  }
+
   {
     base::AutoLock lock(lock_);
     OriginIdentifierValueMap* map = GetValueMap(ext_id, scope);
--- chrome/browser/extensions/api/content_settings/content_settings_store_unittest.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_store_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/browser/content_settings_rule.h"
 #include "components/content_settings/core/browser/content_settings_utils.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
+#include "components/content_settings/core/common/features.h"
 #include "components/content_settings/core/test/content_settings_test_utils.h"
 #include "components/permissions/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -373,6 +374,40 @@ TEST_F(ContentSettingsStoreTest, RemoveEmbedded) {
 
   Mock::VerifyAndClear(&observer);
   store()->RemoveObserver(&observer);
+}
+
+TEST_F(ContentSettingsStoreTest, DisallowWildcardsInFlash) {
+  // Enabling the feature which disallows wildcard matching for Plugin content
+  // settings.
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(
+      content_settings::kDisallowWildcardsInPluginContentSettings);
+
+  // Register extension.
+  std::string ext_id("my_extension");
+  RegisterExtension(ext_id);
+  ContentSettingsPattern primary_pattern =
+      ContentSettingsPattern::FromString("https://[*.]google.com");
+  ContentSettingsPattern secondary_pattern = ContentSettingsPattern::Wildcard();
+  store()->SetExtensionContentSetting(
+      ext_id, primary_pattern, secondary_pattern, ContentSettingsType::PLUGINS,
+      std::string(), CONTENT_SETTING_ALLOW, kExtensionPrefsScopeRegular);
+  store()->SetExtensionContentSetting(
+      ext_id, primary_pattern, secondary_pattern, ContentSettingsType::COOKIES,
+      std::string(), CONTENT_SETTING_ALLOW, kExtensionPrefsScopeRegular);
+
+  std::vector<content_settings::Rule> rules;
+  rules = GetSettingsForOneTypeFromStore(store(), ContentSettingsType::PLUGINS,
+                                         std::string(), false);
+  // Number of rules will be zero because we tried to add a pattern with
+  // wildcards.
+  ASSERT_EQ(rules.size(), 0u);
+
+  rules = GetSettingsForOneTypeFromStore(store(), ContentSettingsType::COOKIES,
+                                         std::string(), false);
+  // Here we will have one rule because wildcard patterns are allowed for
+  // ContentSettingsType::COOKIES.
+  ASSERT_EQ(rules.size(), 1u);
 }
 
 }  // namespace extensions
