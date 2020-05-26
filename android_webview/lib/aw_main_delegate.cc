9fffab87a4092fefa85d6a8fb8f0bdc0fd649574
Tao Bai <michaelbai@chromium.org>
2020-05-26 17:59:17 +0000

    Autofill: Enable the kAutofillExtractAllDatalists feature
    Also included a refactorying to grouped all autofill features together.
    
    Bug: 949555
    Change-Id: I9b72005db703677d71d03ff01f0e4c3322a4b603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212758
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771874}

--- android_webview/lib/aw_main_delegate.cc
+++ android_webview/lib/aw_main_delegate.cc
@@ -168,9 +168,14 @@ bool AwMainDelegate::BasicStartupComplete(int* exit_code) {
 #if BUILDFLAG(ENABLE_SPELLCHECK)
     features.EnableIfNotSet(spellcheck::kAndroidSpellCheckerNonLowEnd);
 #endif  // ENABLE_SPELLCHECK
-
-    features.EnableIfNotSet(
-        autofill::features::kAutofillSkipComparingInferredLabels);
+    if (base::android::BuildInfo::GetInstance()->sdk_int() >=
+        base::android::SDK_VERSION_OREO) {
+      features.EnableIfNotSet(autofill::features::kAutofillExtractAllDatalists);
+      features.EnableIfNotSet(
+          autofill::features::kAutofillSkipComparingInferredLabels);
+      features.DisableIfNotSet(
+          autofill::features::kAutofillRestrictUnownedFieldsToFormlessCheckout);
+    }
 
     if (cl->HasSwitch(switches::kWebViewLogJsConsoleMessages)) {
       features.EnableIfNotSet(::features::kLogJsConsoleMessages);
@@ -201,9 +206,6 @@ bool AwMainDelegate::BasicStartupComplete(int* exit_code) {
 
     // WebView does not support Picture-in-Picture yet.
     features.DisableIfNotSet(media::kPictureInPictureAPI);
-
-    features.DisableIfNotSet(
-        autofill::features::kAutofillRestrictUnownedFieldsToFormlessCheckout);
 
     features.DisableIfNotSet(::features::kBackgroundFetch);
 
