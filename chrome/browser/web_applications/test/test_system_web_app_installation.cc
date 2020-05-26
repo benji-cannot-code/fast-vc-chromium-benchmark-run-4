2c57731d9386bb769217d4917a6b2662b135eb56
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2020-05-26 03:39:56 +0000

    system-web-apps: Add test for uninstallation
    Follow up to https://crrev.com/c/2176571 to ensure there are no
    regressions.
    
    Bug: 1081090,1084939
    Change-Id: I220e5ab122d5421df73748547f26e7ab76a8239f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2213653
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771674}

--- chrome/browser/web_applications/test/test_system_web_app_installation.cc
+++ chrome/browser/web_applications/test/test_system_web_app_installation.cc
@@ -101,11 +101,29 @@ TestSystemWebAppInstallation::TestSystemWebAppInstallation(SystemAppType type,
                      base::Unretained(this), info));
 }
 
+TestSystemWebAppInstallation::TestSystemWebAppInstallation() {
+  test_web_app_provider_creator_ = std::make_unique<
+      TestWebAppProviderCreator>(base::BindOnce(
+      &TestSystemWebAppInstallation::CreateWebAppProviderWithNoSystemWebApps,
+      // base::Unretained is safe here. This callback is called
+      // at TestingProfile::Init, which is at test startup.
+      // TestSystemWebAppInstallation is intended to have the
+      // same lifecycle as the test, it won't be destroyed before
+      // the test finishes.
+      base::Unretained(this)));
+}
+
 TestSystemWebAppInstallation::~TestSystemWebAppInstallation() {
   if (web_ui_controller_factory_.get()) {
     content::WebUIControllerFactory::UnregisterFactoryForTesting(
         web_ui_controller_factory_.get());
   }
+}
+
+// static
+std::unique_ptr<TestSystemWebAppInstallation>
+TestSystemWebAppInstallation::SetUpWithoutApps() {
+  return base::WrapUnique(new TestSystemWebAppInstallation());
 }
 
 // static
@@ -207,7 +225,7 @@ TestSystemWebAppInstallation::CreateWebAppProvider(SystemAppInfo info,
 
   auto provider = std::make_unique<TestWebAppProvider>(profile);
   auto system_web_app_manager = std::make_unique<SystemWebAppManager>(profile);
-  system_web_app_manager->SetSystemAppsForTesting({{type_, info}});
+  system_web_app_manager->SetSystemAppsForTesting({{type_.value(), info}});
   system_web_app_manager->SetUpdatePolicyForTesting(update_policy_);
   provider->SetSystemWebAppManager(std::move(system_web_app_manager));
   provider->Start();
@@ -217,6 +235,19 @@ TestSystemWebAppInstallation::CreateWebAppProvider(SystemAppInfo info,
   for (const auto& permission : auto_granted_permissions_)
     allowlist->RegisterAutoGrantedPermission(app_origin, permission);
 
+  return provider;
+}
+
+std::unique_ptr<KeyedService>
+TestSystemWebAppInstallation::CreateWebAppProviderWithNoSystemWebApps(
+    Profile* profile) {
+  profile_ = profile;
+  auto provider = std::make_unique<TestWebAppProvider>(profile);
+  auto system_web_app_manager = std::make_unique<SystemWebAppManager>(profile);
+  system_web_app_manager->SetSystemAppsForTesting({});
+  system_web_app_manager->SetUpdatePolicyForTesting(update_policy_);
+  provider->SetSystemWebAppManager(std::move(system_web_app_manager));
+  provider->Start();
   return provider;
 }
 
@@ -237,7 +268,7 @@ void TestSystemWebAppInstallation::WaitForAppInstall() {
 AppId TestSystemWebAppInstallation::GetAppId() {
   return WebAppProvider::Get(profile_)
       ->system_web_app_manager()
-      .GetAppIdForSystemApp(type_)
+      .GetAppIdForSystemApp(type_.value())
       .value();
 }
 
@@ -246,7 +277,7 @@ const GURL& TestSystemWebAppInstallation::GetAppUrl() {
 }
 
 SystemAppType TestSystemWebAppInstallation::GetType() {
-  return type_;
+  return type_.value();
 }
 
 void TestSystemWebAppInstallation::SetManifest(std::string manifest) {
