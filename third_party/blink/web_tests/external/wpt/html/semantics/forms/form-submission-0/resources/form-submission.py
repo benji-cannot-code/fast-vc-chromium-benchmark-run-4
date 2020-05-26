6433a3b5bd9839e5d99aca725d84787baf3299b6
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-05-26 15:24:39 +0000

    Import wpt@2228dbdd370235f8bad3330aaec613f9d3ec6ac7
    Using wpt-import in Chromium 290dd0f07d36caa951b87942124c5ea8ae8d0713.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    mkwst@chromium.org:
      external/wpt/cookies
    
    NOAUTOREVERT=true
    TBR=smcgruer
    
    No-Export: true
    Change-Id: I08096e6e3bde44d014d94ef132e2ebc797ea7aba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215907
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#771815}

--- third_party/blink/web_tests/external/wpt/html/semantics/forms/form-submission-0/resources/form-submission.py
+++ third_party/blink/web_tests/external/wpt/html/semantics/forms/form-submission-0/resources/form-submission.py
@@ -1,13 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
-    if request.headers.get('Content-Type') == 'application/x-www-form-urlencoded':
-        result = request.body == 'foo=bara'
-    elif request.headers.get('Content-Type') == 'text/plain':
-        result = request.body == 'qux=baz\r\n'
+    if request.headers.get(b'Content-Type') == b'application/x-www-form-urlencoded':
+        result = request.body == b'foo=bara'
+    elif request.headers.get(b'Content-Type') == b'text/plain':
+        result = request.body == b'qux=baz\r\n'
     else:
-        result = request.POST.first('foo') == 'bar'
+        result = request.POST.first(b'foo') == b'bar'
 
-    result = result and request.url_parts.query == 'query=1'
+    result = result and request.url_parts.query == u'query=1'
 
-    return ([("Content-Type", "text/plain")],
-            "OK" if result else "FAIL")
+    return ([(b"Content-Type", b"text/plain")],
+            b"OK" if result else b"FAIL")
