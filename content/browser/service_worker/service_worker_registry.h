0c91ecfed92c2e71ea9ebcf89ebdbc00b3c74c3a
Kenichi Ishibashi <bashi@chromium.org>
2020-05-26 04:25:28 +0000

    Partial revert of crrev.com/c/2214582
    This CL partially reverts crrev.com/c/2214582 not to use
    |remote_storage_control_| yet. Looks like we need to bind the
    remote on a separate sequence.
    
    Bug: 1055677
    Change-Id: I6c82d6c7150bd5405a92d5bac2ff0517f7aff925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215386
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771682}

--- content/browser/service_worker/service_worker_registry.h
+++ content/browser/service_worker/service_worker_registry.h
@@ -348,8 +348,6 @@ class CONTENT_EXPORT ServiceWorkerRegistry {
   void OnStoragePolicyChanged();
   bool ShouldPurgeOnShutdown(const url::Origin& origin);
 
-  void BindRemoteStorageControlIfNeeded();
-
   // The ServiceWorkerContextCore object must outlive this.
   ServiceWorkerContextCore* const context_;
 
