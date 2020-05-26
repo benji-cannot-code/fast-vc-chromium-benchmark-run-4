82671ead1caa2e3a721c0cbd1dc915518c52b183
sandromaggi <sandromaggi@google.com>
2020-05-26 06:34:54 +0000

    [Autofill Assistant] UT IsCompleteX
    This adds unit tests for
    * IsCompleteContact
    * IsCompleteShippingAddress
    * IsCompleteCreditCard
    
    This change rearranges the |IsCompleteCreditCard| checks to be more
    easily understandable without changing the behaviour of the function
    as a whole.
    
    Bug: none
    Change-Id: I0ed6bc657737b69e362e588156d8befbbadacfd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214889
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#771701}

--- components/autofill_assistant/browser/user_data_util.cc
+++ components/autofill_assistant/browser/user_data_util.cc
@@ -318,7 +318,8 @@ bool IsCompleteCreditCard(
     return true;
   }
 
-  if (!credit_card || !billing_profile) {
+  if (!credit_card || !billing_profile ||
+      credit_card->billing_address_id().empty()) {
     return false;
   }
 
@@ -335,8 +336,7 @@ bool IsCompleteCreditCard(
     return false;
   }
 
-  if (!credit_card->HasValidExpirationDate() ||
-      credit_card->billing_address_id().empty()) {
+  if (!credit_card->HasValidExpirationDate()) {
     return false;
   }
 
@@ -350,6 +350,7 @@ bool IsCompleteCreditCard(
           collect_user_data_options.supported_basic_card_networks.end()) {
     return false;
   }
+
   return true;
 }
 
