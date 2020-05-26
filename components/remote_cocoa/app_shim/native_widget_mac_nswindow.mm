a4d8dd55e1d05e2d9a71d425e9fa8fae99faf7a2
Leonard Grey <lgrey@chromium.org>
2020-05-26 17:10:06 +0000

    Mac: Don't manually start frame drag on 10.12+
    On 10.12, the window server is fine initiating a drag without this, so
    we end up with two drags starting.
    
    This actually doesn't fix the teleporting windows bug, but it can't help!
    
    Bug: 897274
    Change-Id: I2e181659c504c90db0a0928747f5485419728fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207980
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771854}

--- components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
+++ components/remote_cocoa/app_shim/native_widget_mac_nswindow.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/remote_cocoa/app_shim/native_widget_mac_nswindow.h"
 
 #include "base/mac/foundation_util.h"
+#include "base/mac/mac_util.h"
 #include "base/trace_event/trace_event.h"
 #import "components/remote_cocoa/app_shim/native_widget_ns_window_bridge.h"
 #include "components/remote_cocoa/app_shim/native_widget_ns_window_host_helper.h"
@@ -60,7 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation NativeWidgetMacNSWindowTitledFrame
 - (void)mouseDown:(NSEvent*)event {
-  if (self.window.isMovable)
+  if (base::mac::IsAtMostOS10_11() && self.window.isMovable)
     [self cr_mouseDownOnFrameView:event];
   [super mouseDown:event];
 }
