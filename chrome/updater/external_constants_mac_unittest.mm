f6521af8b515c0a2117dd63963738b03ab87ef90
Michael Chang <donchan@microsoft.com>
2020-05-26 21:55:53 +0000

    Fix NSUserDefaults bug in //chrome/updater
    initWithSuiteName for NSUserDefaults should not use its own app
    bundle identifier. initWithSuiteName should use a suite that is
    distinct from the apps own main bundle identifier. Reference:
    https://developer.apple.com/documentation/foundation/nsuserdefaults/1409957-initwithsuitename?language=objc
    
    Bug: 1086669
    Change-Id: Iea3316ac8c6752bab3fa0644fd18b7dbdd018390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2213787
    Commit-Queue: Michael Chang <donchan@microsoft.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771975}

--- chrome/updater/external_constants_mac_unittest.mm
+++ chrome/updater/external_constants_mac_unittest.mm
@@ -21,11 +21,10 @@ namespace {
 
 void ClearUserDefaults() {
   @autoreleasepool {
-    base::scoped_nsobject<NSUserDefaults> user_defaults([[NSUserDefaults alloc]
-        initWithSuiteName:@MAC_BUNDLE_IDENTIFIER_STRING]);
-    [user_defaults
+    NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults
         removeObjectForKey:[NSString stringWithUTF8String:kDevOverrideKeyUrl]];
-    [user_defaults
+    [userDefaults
         removeObjectForKey:[NSString
                                stringWithUTF8String:kDevOverrideKeyUseCUP]];
   }
@@ -45,11 +44,10 @@ TEST_F(DevOverrideTest, TestDevOverrides) {
   std::unique_ptr<ExternalConstants> consts = CreateExternalConstants();
 
   @autoreleasepool {
-    base::scoped_nsobject<NSUserDefaults> user_defaults([[NSUserDefaults alloc]
-        initWithSuiteName:@MAC_BUNDLE_IDENTIFIER_STRING]);
-    [user_defaults setURL:[NSURL URLWithString:@"http://localhost:8080"]
-                   forKey:[NSString stringWithUTF8String:kDevOverrideKeyUrl]];
-    [user_defaults
+    NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setURL:[NSURL URLWithString:@"http://localhost:8080"]
+                  forKey:[NSString stringWithUTF8String:kDevOverrideKeyUrl]];
+    [userDefaults
         setBool:NO
          forKey:[NSString stringWithUTF8String:kDevOverrideKeyUseCUP]];
   }
