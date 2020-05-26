9453641d9de7ec718f976a39dc64ff31784b64c3
Kenichi Ishibashi <bashi@chromium.org>
2020-05-26 10:47:51 +0000

    Remove unused include from ServiceWorkerScriptLoaderFactory
    Bug: N/A
    Change-Id: I1ef2b3b1256b33ab458ec6316416836de9b5638d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215400
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#771744}

--- content/browser/service_worker/service_worker_script_loader_factory.cc
+++ content/browser/service_worker/service_worker_script_loader_factory.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
-#include "base/debug/crash_logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "content/browser/service_worker/service_worker_cache_writer.h"
 #include "content/browser/service_worker/service_worker_consts.h"
