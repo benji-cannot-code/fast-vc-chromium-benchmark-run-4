abf9c8374c583a0422567901a333ba3436314833
Anastasiia Nikolaienko <anastasiian@chromium.org>
2020-05-27 14:05:42 +0000

    Add account_manager/ folder in ui/webui/chromeos
    Move account_manager_* and account_migration_* files to
    ui/webui/chromeos/account_manager.
    
    Bug: 1083147
    Change-Id: I2f61237cf4b781a8ddd0ed8837e690e7f59adac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207226
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772224}

--- chrome/browser/ui/webui/chromeos/account_manager/account_migration_welcome_ui.h
+++ chrome/browser/ui/webui/chromeos/account_manager/account_migration_welcome_ui.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_ACCOUNT_MIGRATION_WELCOME_UI_H_
-#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_ACCOUNT_MIGRATION_WELCOME_UI_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_ACCOUNT_MANAGER_ACCOUNT_MIGRATION_WELCOME_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_CHROMEOS_ACCOUNT_MANAGER_ACCOUNT_MIGRATION_WELCOME_UI_H_
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
@@ -27,4 +27,4 @@ class AccountMigrationWelcomeUI : public ui::WebDialogUI {
 
 }  // namespace chromeos
 
-#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_ACCOUNT_MIGRATION_WELCOME_UI_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_CHROMEOS_ACCOUNT_MANAGER_ACCOUNT_MIGRATION_WELCOME_UI_H_
--- chrome/browser/ui/webui/chromeos/account_manager/account_migration_welcome_ui_test.cc
+++ chrome/browser/ui/webui/chromeos/account_manager/account_migration_welcome_ui_test.cc
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/chromeos/account_migration_welcome_ui_test.h"
+#include "chrome/browser/ui/webui/chromeos/account_manager/account_migration_welcome_ui_test.h"
 
 #include "base/macros.h"
-#include "chrome/browser/ui/webui/chromeos/account_migration_welcome_dialog.h"
+#include "chrome/browser/ui/webui/chromeos/account_manager/account_migration_welcome_dialog.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
