259ae23d7c99735cf6a5b6acd37c13c944591f5a
dpapad <dpapad@chromium.org>
2020-05-27 17:35:18 +0000

    WebUI: Add JS type checking for more cr_elements/ tests, part 2.
    Specifically:
     - cr_drawer_tests.js
     - cr_expand_button_focus_tests.js
     - cr_expand_button_tests.js
    
    Bug: 1000989
    Change-Id: I5b7bb247e645d795dd45387d4b9d80a2e02e6c12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215459
    Commit-Queue: dpapad <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772302}

--- chrome/test/data/webui/cr_elements/cr_drawer_tests.js
+++ chrome/test/data/webui/cr_elements/cr_drawer_tests.js
@@ -8,13 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // #import {eventToPromise} from '../test_util.m.js';
 // #import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+// #import {assertEquals, assertFalse, assertNotEquals, assertTrue, assertThrows} from '../chai_assert.js';
 // clang-format on
 
 suite('cr-drawer', function() {
   setup(function() {
-    PolymerTest.clearBody();
+    document.body.innerHTML = '';
   });
 
+  /**
+   * @param {string} align
+   * @return {!CrDrawerElement}
+   */
   function createDrawer(align) {
     document.body.innerHTML = `
       <cr-drawer id="drawer" align="${align}">
@@ -23,7 +28,7 @@ suite('cr-drawer', function() {
       </cr-drawer>
     `;
     Polymer.dom.flush();
-    return document.getElementById('drawer');
+    return /** @type {!CrDrawerElement} */ (document.getElementById('drawer'));
   }
 
   test('open and close', function() {
@@ -40,7 +45,7 @@ suite('cr-drawer', function() {
           document.querySelector('.drawer-content').click();
 
           const whenClosed = test_util.eventToPromise('close', drawer);
-          drawer.$.dialog.dispatchEvent(new MouseEvent('click', {
+          drawer.$$('#dialog').dispatchEvent(new MouseEvent('click', {
             bubbles: true,
             cancelable: true,
             clientX: 300,  // Must be larger than the drawer width (256px).
@@ -72,7 +77,7 @@ suite('cr-drawer', function() {
     await test_util.eventToPromise('cr-drawer-opened', drawer);
 
     // Clicking the icon closes the drawer.
-    drawer.$.iconButton.click();
+    drawer.$$('#iconButton').click();
     await test_util.eventToPromise('close', drawer);
     assertFalse(drawer.open);
     assertTrue(drawer.wasCanceled());
@@ -82,7 +87,7 @@ suite('cr-drawer', function() {
     const drawer = createDrawer('ltr');
     drawer.openDrawer();
     return test_util.eventToPromise('cr-drawer-opened', drawer).then(() => {
-      const rect = drawer.$.dialog.getBoundingClientRect();
+      const rect = drawer.$$('#dialog').getBoundingClientRect();
       assertEquals(0, rect.left);
       assertNotEquals(0, rect.right);
     });
@@ -92,14 +97,14 @@ suite('cr-drawer', function() {
     const drawer = createDrawer('rtl');
     drawer.openDrawer();
     return test_util.eventToPromise('cr-drawer-opened', drawer).then(() => {
-      const rect = drawer.$.dialog.getBoundingClientRect();
+      const rect = drawer.$$('#dialog').getBoundingClientRect();
       assertNotEquals(0, rect.left);
       assertEquals(window.innerWidth, rect.right);
     });
   });
 
   test('close and cancel', () => {
-    const drawer = createDrawer();
+    const drawer = createDrawer('ltr');
     drawer.openDrawer();
     return test_util.eventToPromise('cr-drawer-opened', drawer)
         .then(() => {
@@ -126,7 +131,7 @@ suite('cr-drawer', function() {
   });
 
   test('openDrawer/close/toggle can be called multiple times in a row', () => {
-    const drawer = createDrawer();
+    const drawer = createDrawer('ltr');
     drawer.openDrawer();
     drawer.close();
     drawer.close();
@@ -139,7 +144,7 @@ suite('cr-drawer', function() {
   });
 
   test('cannot set open', () => {
-    const drawer = createDrawer();
+    const drawer = createDrawer('ltr');
     assertThrows(() => {
       drawer.open = true;
     });
--- chrome/test/data/webui/cr_elements/cr_expand_button_focus_tests.js
+++ chrome/test/data/webui/cr_elements/cr_expand_button_focus_tests.js
@@ -9,15 +9,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {eventToPromise} from '../test_util.m.js';
 // #import {getDeepActiveElement} from 'chrome://resources/js/util.m.js';
 // #import {pressAndReleaseKeyOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
+// #import {assertEquals, assertFalse, assertNotEquals, assertTrue} from '../chai_assert.js';
 // clang-format on
 
 suite('cr-expand-button-focus-tests', () => {
+  /** @type {!CrExpandButtonElement} */
   let button;
 
-  /** @param {boolean} */
+  /** @type {!CrIconButtonElement} */
+  let icon;
+
+  /** @param {boolean} rippleShown */
   function assertRippleState(rippleShown) {
-    assertEquals(button.$.icon, getDeepActiveElement());
-    assertEquals(rippleShown, button.$.icon.getRipple().holdDown);
+    assertEquals(icon, getDeepActiveElement());
+    assertEquals(rippleShown, icon.getRipple().holdDown);
   }
 
   function assertRipple() {
@@ -28,41 +33,50 @@ suite('cr-expand-button-focus-tests', () => {
     assertRippleState(false);
   }
 
-  /** @param {!Function} toggler */
-  async function waitForExpansion(toggler) {
+  /**
+   * @param {!Function} toggler
+   * @return {!Promise<void>}
+   */
+  function waitForExpansion(toggler) {
     const wait = test_util.eventToPromise('expanded-changed', button);
     toggler();
-    await wait;
+    return wait;
   }
 
-  async function click() {
-    await waitForExpansion(() => {
+  /** @return {!Promise<void>} */
+  function click() {
+    return waitForExpansion(() => {
       // This is used in cr.ui.focusWithoutInk to change mode into ink hidden
       // when focused.
       button.dispatchEvent(new PointerEvent('pointerdown'));
       // Used to simulate releasing the mouse button.
-      button.$.icon.fire('up');
+      icon.fire('up');
       // When the mouse is pressed and released, it will also emit a 'click'
       // event which is used in cr-expand-button to toggle expansion.
       button.fire('click');
     });
   }
 
-  async function enter() {
-    await waitForExpansion(() => {
-      MockInteractions.pressAndReleaseKeyOn(button.$.icon, '', '', 'Enter');
+  /** @return {!Promise<void>} */
+  function enter() {
+    return waitForExpansion(() => {
+      MockInteractions.pressAndReleaseKeyOn(icon, 0, '', 'Enter');
     });
   }
 
-  async function space() {
-    await waitForExpansion(() => {
-      MockInteractions.pressAndReleaseKeyOn(button.$.icon, '', '', ' ');
+  /** @return {!Promise<void>} */
+  function space() {
+    return waitForExpansion(() => {
+      MockInteractions.pressAndReleaseKeyOn(icon, 0, '', ' ');
     });
   }
 
   setup(() => {
-    document.body.innerHTML = '<cr-expand-button></cr-expand-button>';
-    button = document.body.querySelector('cr-expand-button');
+    document.body.innerHTML = '';
+    button = /** @type {!CrExpandButtonElement} */ (
+        document.createElement('cr-expand-button'));
+    document.body.appendChild(button);
+    icon = /** @type {!CrIconButtonElement} */ (button.$$('#icon'));
   });
 
   test('focus, ripple', () => {
