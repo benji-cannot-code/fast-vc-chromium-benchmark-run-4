e8e1c454f9df38b9f57ebea1f8b2e81f51288613
Maksim Ivanov <emaxx@chromium.org>
2020-05-27 10:30:03 +0000

    policy_fuzzer: Reduce log spam
    Disable the non-fatal logging, because fuzzing triggers tons of such
    messages, which are useless and only slow down the fuzzing.
    
    Note that some of the logging spam - mainly caused by the SYSLOG macros
    - stays intact, since there's no easy way to disable those.
    
    Bug: 811690
    Change-Id: I9f93b0e2450d8ff9ec7c7a9a8c88c3c23947e7ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215827
    Reviewed-by: Olya Kalitova <okalitova@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772179}

--- chrome/browser/chromeos/policy/fuzzer/policy_fuzzer.cc
+++ chrome/browser/chromeos/policy/fuzzer/policy_fuzzer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/command_line.h"
+#include "base/logging.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/policy/device_policy_decoder_chromeos.h"
 #include "chrome/browser/chromeos/policy/fuzzer/policy_fuzzer.pb.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace policy {
 
 bool FuzzTestInitializer() {
+  logging::SetMinLogLevel(logging::LOG_FATAL);
   base::CommandLine::Init(0, nullptr);
   chromeos::DBusThreadManager::Initialize();
   return true;
