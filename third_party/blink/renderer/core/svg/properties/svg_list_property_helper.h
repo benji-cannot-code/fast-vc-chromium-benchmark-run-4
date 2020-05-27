e4ff9860bf4e4c260e3c2ba95984947f0692961c
Fredrik Söderqvist <fs@opera.com>
2020-05-27 18:31:39 +0000

    Mark argument to SVGListPropertyHelper::DeepCopy const
    The argument is the list to copy from, marking it const makes that clear
    and reduces the risk for accidental mutations.
    
    Change-Id: I38fbcc64dc9dd6d2a54444ccd8c76c1959ccda7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218272
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772336}

--- third_party/blink/renderer/core/svg/properties/svg_list_property_helper.h
+++ third_party/blink/renderer/core/svg/properties/svg_list_property_helper.h
@@ -108,7 +108,7 @@ class SVGListPropertyHelper : public SVGPropertyHelper<Derived> {
   }
 
  protected:
-  void DeepCopy(Derived*);
+  void DeepCopy(const Derived*);
 
   bool AdjustFromToListValues(Derived* from_list,
                               Derived* to_list,
@@ -228,7 +228,8 @@ bool SVGListPropertyHelper<Derived, ItemProperty>::CheckIndexBound(
 }
 
 template <typename Derived, typename ItemProperty>
-void SVGListPropertyHelper<Derived, ItemProperty>::DeepCopy(Derived* from) {
+void SVGListPropertyHelper<Derived, ItemProperty>::DeepCopy(
+    const Derived* from) {
   Clear();
   for (const auto& from_value : from->values_)
     Append(from_value->Clone());
