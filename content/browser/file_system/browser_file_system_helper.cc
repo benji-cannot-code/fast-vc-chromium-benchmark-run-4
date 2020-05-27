ad74b04838e9d649973a5bc564e50650fb540ec4
Gabriel Charette <gab@chromium.org>
2020-05-27 17:01:14 +0000

    [base] Make PostTask destination explicit in /content/browser/file_system
    Task APIs v3 will all have an explicit destination. In the past
    "default traits" meant ThreadPool. These CLs are thus a no-op
    except for the few callers that clearly wanted to post a QuitClosure()
    on the current thread (posting a QuitClosure() to a parallel task will
    merely bounce it back to the current thread anyways). Those few callers
    were migrated to ThreadTaskRunnerHandle::Get().
    
    These changes are thus required to phase out task APIs v2 (post_task.h)
    Design doc: https://docs.google.com/document/d/1tssusPykvx3g0gvbvU4HxGyn3MjJlIylnsH13-Tv6s4/edit
    
    This specific change is a subset of
    https://chromium-review.googlesource.com/c/chromium/src/+/2216733
    
    This CL was uploaded by git cl split.
    
    R=mek@chromium.org
    
    Bug: 1026641
    Change-Id: I3c723c41dc313d8eaa664260c7569a7bde398076
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216737
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772281}

--- content/browser/file_system/browser_file_system_helper.cc
+++ content/browser/file_system/browser_file_system_helper.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/lazy_thread_pool_task_runner.h"
 #include "base/task/post_task.h"
+#include "base/task/thread_pool.h"
 #include "content/browser/child_process_security_policy_impl.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -169,8 +170,8 @@ void SyncGetPlatformPath(storage::FileSystemContext* context,
   if (!FileSystemURLIsValid(context, url)) {
     // Note: Posting a task here so this function always returns
     // before the callback is called no matter which path is taken.
-    base::PostTask(FROM_HERE,
-                   base::BindOnce(std::move(callback), base::FilePath()));
+    base::ThreadPool::PostTask(
+        FROM_HERE, base::BindOnce(std::move(callback), base::FilePath()));
     return;
   }
 
