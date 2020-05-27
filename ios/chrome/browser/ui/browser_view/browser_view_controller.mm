c1baaf90f927e1cfeacc7b8658814c5cfddb427f
Kurt Horimoto <kkhorimoto@chromium.org>
2020-05-27 15:23:06 +0000

    [iOS] Reset BVC._voiceSearchController to null in |-shutdown|.
    The VoiceSearchController created by VoiceSearchProvider may depend on
    state that is no longer valid after shut down.  Reset BVC's pointer to
    it early so that shutdown can occur properly when the BVC is later
    destroyed.
    
    Bug: 1083208
    Change-Id: I05dffce9eb79d353443eae759bc990fb61d74308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216804
    Auto-Submit: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772250}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -1322,8 +1322,10 @@ NSString* const kBrowserViewControllerSnackbarCategory =
   _sideSwipeController = nil;
   _webStateListObserver.reset();
   _allWebStateObservationForwarder = nullptr;
-  if (_voiceSearchController)
+  if (_voiceSearchController) {
     _voiceSearchController->SetDispatcher(nil);
+    _voiceSearchController = nullptr;
+  }
   [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
