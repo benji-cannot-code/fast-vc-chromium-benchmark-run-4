872bf5739660f703eb9a72a7d421c98b329a00dd
Alex Gough <ajgo@chromium.org>
2020-05-27 23:06:40 +0000

    Supply sandbox for PaintPreviewCompositorCollection
    Adds service_sandbox_type.h header for components to consume and uses it
    to supply sandbox type for PaintPreviewCompositorCollection.
    
    Prelude to removal of WithSandboxType API. See linked bug for
    rationale.  Does not change behavior.
    
    Tests: browser_tests PaintPreviewCompositorBrowserTest.*
    Bug: 1065087
    Change-Id: Ie7e13e884df5c59edc850b3546d6d9b655102f79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202663
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772447}

--- components/paint_preview/browser/compositor_utils.cc
+++ components/paint_preview/browser/compositor_utils.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "build/build_config.h"
 #include "components/discardable_memory/service/discardable_shared_memory_manager.h"
+#include "components/paint_preview/browser/service_sandbox_type.h"
 #include "components/services/paint_preview_compositor/public/mojom/paint_preview_compositor.mojom.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -41,12 +42,10 @@ CreateCompositorCollection() {
 
 void CreateCompositorCollectionPending(
     mojo::PendingReceiver<mojom::PaintPreviewCompositorCollection> collection) {
-  // TODO(crbug/1074323): Investigate using a different SandboxType.
   content::ServiceProcessHost::Launch<mojom::PaintPreviewCompositorCollection>(
       std::move(collection),
       content::ServiceProcessHost::Options()
           .WithDisplayName(IDS_PAINT_PREVIEW_COMPOSITOR_SERVICE_DISPLAY_NAME)
-          .WithSandboxType(service_manager::SandboxType::kPrintCompositor)
           .Pass());
 }
 
