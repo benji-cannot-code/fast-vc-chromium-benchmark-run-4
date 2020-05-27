3f0b243fd3c908676e27a04cb33b3a3d843fdc14
Rushan Suleymanov <rushans@google.com>
2020-05-27 18:07:54 +0000

    [Sync] Report detailed server errors on each commit
    This CL breaks down previously reported sync commit failures per data
    type into particular syncer errors.
    
    Bug: 1086877
    Change-Id: I3ff3290b0857624d9a08f645d10f29e382f992eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218111
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#772325}

--- components/sync/engine_impl/syncer.cc
+++ components/sync/engine_impl/syncer.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/engine_impl/syncer.h"
 
 #include <memory>
+#include <string>
 
 #include "base/auto_reset.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/trace_event/trace_event.h"
 #include "components/sync/base/cancelation_signal.h"
 #include "components/sync/engine/sync_engine_switches.h"
@@ -164,6 +166,12 @@ SyncerError Syncer::BuildAndPostCommits(const ModelTypeSet& request_types,
     SyncerError error = commit->PostAndProcessResponse(
         nudge_tracker, cycle, cycle->mutable_status_controller(),
         cycle->context()->extensions_activity());
+    base::UmaHistogramEnumeration("Sync.CommitResponse", error.value());
+    for (ModelType type : commit->GetContributingDataTypes()) {
+      const std::string kPrefix = "Sync.CommitResponse.";
+      base::UmaHistogramEnumeration(kPrefix + ModelTypeToHistogramSuffix(type),
+                                    error.value());
+    }
     commit->CleanUp();
     if (error.value() != SyncerError::SYNCER_OK) {
       return error;
