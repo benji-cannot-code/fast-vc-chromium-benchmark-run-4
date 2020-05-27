85dc05d845504822f89d31c1c05f3a8781510017
Rune Lillesveen <futhark@chromium.org>
2020-05-27 08:50:52 +0000

    Reduce calls to LoadPendingImages.
    There is no need for the extra call after ApplyAnimatedStandardProperties
    now that we trigger loading at the end of StyleForElement and
    PseudoStyleForElement. Also, we only need to have the extra loading for
    MatchedPropertiesCache case if when we actually add the ComputedStyle to
    the cache.
    
    Change-Id: I943c24f42f8ec945b901cdbf7572361dc68e280b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216076
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772159}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -1465,9 +1465,6 @@ bool StyleResolver::ApplyAnimatedStandardProperties(
                                                           standard_transitions);
   }
 
-  // Start loading resources used by animations.
-  state.LoadPendingResources();
-
   DCHECK(!state.GetFontBuilder().FontDirty());
 
   return true;
@@ -1901,6 +1898,7 @@ void StyleResolver::MaybeAddToMatchedPropertiesCache(
       MatchedPropertiesCache::IsCacheable(state)) {
     INCREMENT_STYLE_STATS_COUNTER(GetDocument().GetStyleEngine(),
                                   matched_property_cache_added, 1);
+    state.LoadPendingResources();
     matched_properties_cache_.Add(cache_success.key, *state.Style(),
                                   *state.ParentStyle(), state.Dependencies());
   }
@@ -2078,7 +2076,6 @@ void StyleResolver::ApplyMatchedLowPriorityProperties(
         state, match_result, apply_inherited_only, needs_apply_pass);
   }
 
-  state.LoadPendingResources();
   MaybeAddToMatchedPropertiesCache(state, cache_success, match_result);
 
   DCHECK(!state.GetFontBuilder().FontDirty());
@@ -2178,7 +2175,6 @@ void StyleResolver::CascadeAndApplyMatchedProperties(StyleResolverState& state,
     cascade.Apply();
   }
 
-  state.LoadPendingResources();
   MaybeAddToMatchedPropertiesCache(state, cache_success, result);
 
   DCHECK(!state.GetFontBuilder().FontDirty());
