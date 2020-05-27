fd40c4101323815f602252af8e0a5cc10652927f
Peng Huang <penghuang@chromium.org>
2020-05-27 17:02:02 +0000

    Reland "vma_wrapper: create VmaAllocator for vulkan 1.1.0"
    This is a reland of de6734f75c24c2055fa7cbe24f77d8147340c51e
    
    Original change's description:
    > vma_wrapper: create VmaAllocator for vulkan 1.1.0
    > 
    > For vulkan 1.1.0, VmaAllocator will use dedicated memory
    > allocation for some big vulkan memory request, and free
    > it when the allocation is released. Without this change,
    > the VmaAllocator will allocate memory from self managed
    > memory blocks for all vulkan memory request. And the
    > memory may not be released when the allocation is
    > released. VmaAllocator may keep the memory for later
    > usage. It could be the reason for the memory regression
    > in fuchsia.
    > 
    > Bug: fuchsia:52463
    > Change-Id: I7b905435d31b927b2344724f8a2e1738e0544410
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215414
    > Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Auto-Submit: Peng Huang <penghuang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#771927}
    
    Bug: fuchsia:52463
    Change-Id: I4da1f291a574f72ad16811ffafffb3be36392af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218247
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772282}

--- gpu/vulkan/generate_bindings.py
+++ gpu/vulkan/generate_bindings.py
@@ -23,6 +23,8 @@ from reg import Registry
 registry = Registry()
 registry.loadFile(open(path.join(vulkan_reg_path, "vk.xml")))
 
+VULKAN_REQUIRED_API_VERSION = 'VK_API_VERSION_1_1'
+
 VULKAN_UNASSOCIATED_FUNCTIONS = [
   {
     'functions': [
@@ -44,9 +46,11 @@ VULKAN_INSTANCE_FUNCTIONS = [
       'vkEnumerateDeviceLayerProperties',
       'vkEnumeratePhysicalDevices',
       'vkGetDeviceProcAddr',
-      'vkGetPhysicalDeviceFeatures',
+      'vkGetPhysicalDeviceFeatures2',
       'vkGetPhysicalDeviceFormatProperties',
+      'vkGetPhysicalDeviceImageFormatProperties2',
       'vkGetPhysicalDeviceMemoryProperties',
+      'vkGetPhysicalDeviceMemoryProperties2',
       'vkGetPhysicalDeviceProperties',
       'vkGetPhysicalDeviceQueueFamilyProperties',
     ]
@@ -98,22 +102,6 @@ VULKAN_INSTANCE_FUNCTIONS = [
       'vkCreateImagePipeSurfaceFUCHSIA',
     ]
   },
-  {
-  'min_api_version': 'VK_API_VERSION_1_1',
-    'functions': [
-      'vkGetPhysicalDeviceImageFormatProperties2',
-    ]
-  },
-  {
-    # vkGetPhysicalDeviceFeatures2() is defined in Vulkan 1.1 or suffixed in the
-    # VK_KHR_get_physical_device_properties2 extension.
-    'min_api_version': 'VK_API_VERSION_1_1',
-    'extension': 'VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME',
-    'extension_suffix': 'KHR',
-    'functions': [
-      'vkGetPhysicalDeviceFeatures2',
-    ]
-  },
 ]
 
 VULKAN_DEVICE_FUNCTIONS = [
@@ -124,7 +112,9 @@ VULKAN_DEVICE_FUNCTIONS = [
       'vkAllocateMemory',
       'vkBeginCommandBuffer',
       'vkBindBufferMemory',
+      'vkBindBufferMemory2',
       'vkBindImageMemory',
+      'vkBindImageMemory2',
       'vkCmdBeginRenderPass',
       'vkCmdCopyBuffer',
       'vkCmdCopyBufferToImage',
@@ -165,9 +155,12 @@ VULKAN_DEVICE_FUNCTIONS = [
       'vkFreeMemory',
       'vkInvalidateMappedMemoryRanges',
       'vkGetBufferMemoryRequirements',
+      'vkGetBufferMemoryRequirements2',
       'vkGetDeviceQueue',
+      'vkGetDeviceQueue2',
       'vkGetFenceStatus',
       'vkGetImageMemoryRequirements',
+      'vkGetImageMemoryRequirements2',
       'vkMapMemory',
       'vkQueueSubmit',
       'vkQueueWaitIdle',
@@ -176,14 +169,6 @@ VULKAN_DEVICE_FUNCTIONS = [
       'vkUnmapMemory',
       'vkUpdateDescriptorSets',
       'vkWaitForFences',
-    ]
-  },
-  {
-    'min_api_version': 'VK_API_VERSION_1_1',
-    'functions': [
-      'vkGetDeviceQueue2',
-      'vkGetBufferMemoryRequirements2',
-      'vkGetImageMemoryRequirements2',
     ]
   },
   {
@@ -409,6 +394,8 @@ namespace gpu {
 
 struct VulkanFunctionPointers;
 
+constexpr uint32_t kVulkanRequiredApiVersion = %s;
+
 COMPONENT_EXPORT(VULKAN) VulkanFunctionPointers* GetVulkanFunctionPointers();
 
 struct COMPONENT_EXPORT(VULKAN) VulkanFunctionPointers {
@@ -463,7 +450,7 @@ struct COMPONENT_EXPORT(VULKAN) VulkanFunctionPointers {
   // Unassociated functions
   VulkanFunction<PFN_vkGetInstanceProcAddr> vkGetInstanceProcAddr;
 
-""")
+""" % VULKAN_REQUIRED_API_VERSION)
 
   WriteFunctionDeclarations(file, VULKAN_UNASSOCIATED_FUNCTIONS)
 
@@ -586,6 +573,7 @@ bool VulkanFunctionPointers::BindInstanceFunctionPointers(
     VkInstance vk_instance,
     uint32_t api_version,
     const gfx::ExtensionSet& enabled_extensions) {
+  DCHECK_GE(api_version, kVulkanRequiredApiVersion);
 """)
 
   WriteInstanceFunctionPointerInitialization(file, VULKAN_INSTANCE_FUNCTIONS);
@@ -599,6 +587,7 @@ bool VulkanFunctionPointers::BindDeviceFunctionPointers(
     VkDevice vk_device,
     uint32_t api_version,
     const gfx::ExtensionSet& enabled_extensions) {
+  DCHECK_GE(api_version, kVulkanRequiredApiVersion);
   // Device functions
 """)
   WriteDeviceFunctionPointerInitialization(file, VULKAN_DEVICE_FUNCTIONS)
