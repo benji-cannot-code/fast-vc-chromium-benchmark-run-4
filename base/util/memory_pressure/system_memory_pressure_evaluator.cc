78058fde3c41f1952ce9438d98a021f32a194062
Brian Geffon <bgeffon@chromium.org>
2020-05-27 19:14:59 +0000

    CrOS: Fix error logs on linux-chromeos.
    We shouldn't log errors for CrOS specific behavior when run on
    linux-chromeos.
    
    Bug: 1085453
    Change-Id: Ibc0bf32a2fd6488eee224a9efd796a7114d51bdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216378
    Reviewed-by: François Doray <fdoray@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772358}

--- base/util/memory_pressure/system_memory_pressure_evaluator.cc
+++ base/util/memory_pressure/system_memory_pressure_evaluator.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #if defined(OS_CHROMEOS)
+#include "base/system/sys_info.h"
 #include "base/util/memory_pressure/system_memory_pressure_evaluator_chromeos.h"
 #elif defined(OS_FUCHSIA)
 #include "base/util/memory_pressure/system_memory_pressure_evaluator_fuchsia.h"
@@ -29,8 +30,9 @@ SystemMemoryPressureEvaluator::CreateDefaultSystemEvaluator(
     return std::make_unique<util::chromeos::SystemMemoryPressureEvaluator>(
         monitor->CreateVoter());
   }
-  LOG(ERROR) << "No MemoryPressureMonitor created because the kernel does "
-                "not support notifications.";
+  LOG_IF(ERROR, base::SysInfo::IsRunningOnChromeOS())
+      << "No MemoryPressureMonitor created because the kernel does not have "
+         "support.";
 #elif defined(OS_FUCHSIA)
   return std::make_unique<util::SystemMemoryPressureEvaluatorFuchsia>(
       monitor->CreateVoter());
