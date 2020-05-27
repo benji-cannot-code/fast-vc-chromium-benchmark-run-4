83f5d77d381d5cfbcdac8252d6d258d7b354779b
David Van Cleve <davidvc@chromium.org>
2020-05-27 14:33:03 +0000

    fetch: Remove a VLOG accidentally added in a prior CL
    I accidentally included a VLOG statement from local debugging in a prior
    change; this CL removes it.
    
    R=jbroman
    
    Bug: None
    Change-Id: If942046836e306696547e163c18c6a0d84f5805b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218229
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772232}

--- third_party/blink/renderer/core/fetch/request.cc
+++ third_party/blink/renderer/core/fetch/request.cc
@@ -534,8 +534,6 @@ Request* Request::CreateRequestWithRequestOrString(
       return nullptr;
     }
 
-    VLOG(1) << "a";
-
     if (params.type == TrustTokenOperationType::kIssuance &&
         !IsTrustTokenIssuanceAvailableInExecutionContext(*execution_context)) {
       exception_state.ThrowTypeError(
