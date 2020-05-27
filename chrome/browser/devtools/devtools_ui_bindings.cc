554fcb87962daef6d7dc930627a413a2dc0450a3
Jack Lynch <jalyn@microsoft.com>
2020-05-27 22:51:04 +0000

    DevTools: Add DevTools.KeybindSetSettingChanged histogram
    This CL adds a new histogram that is recorded whenever a user changes
    which keyboard shortcut presets they are using in the DevTools, an
    experimental feature that will be introduced by this [1] CL. A test for
    this histogram is included in this [2] devtools-frontend CL.
    
    Custom keyboard shortcuts design doc:
    https://docs.google.com/document/d/1oOPSWPxCHvMoBZ0Fw9jwFZt6gP4lrsrsl8DEAp-Hy7o/edit
    
    [1] https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2095859
    [2] https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2173300
    
    Bug: 174309
    Change-Id: I00206839d4d55031539efbdc02b4f776cf4f054c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2172132
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Vidal Diazleal <vidorteg@microsoft.com>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Jack Lynch <jalyn@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#772437}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -104,6 +104,8 @@ static const char kDevToolsKeyboardShortcutFiredHistogram[] =
     "DevTools.KeyboardShortcutFired";
 static const char kDevToolsIssuesPanelOpenedFromHistogram[] =
     "DevTools.IssuesPanelOpenedFrom";
+static const char kDevToolsKeybindSetSettingChanged[] =
+    "DevTools.KeybindSetSettingChanged";
 
 static const char kRemotePageActionInspect[] = "inspect";
 static const char kRemotePageActionReload[] = "reload";
@@ -1244,6 +1246,8 @@ void DevToolsUIBindings::RecordEnumeratedHistogram(const std::string& name,
   else if (name == kDevToolsKeyboardShortcutFiredHistogram)
     UMA_HISTOGRAM_EXACT_LINEAR(name, sample, boundary_value);
   else if (name == kDevToolsIssuesPanelOpenedFromHistogram)
+    UMA_HISTOGRAM_EXACT_LINEAR(name, sample, boundary_value);
+  else if (name == kDevToolsKeybindSetSettingChanged)
     UMA_HISTOGRAM_EXACT_LINEAR(name, sample, boundary_value);
   else
     frontend_host_->BadMessageRecieved();
