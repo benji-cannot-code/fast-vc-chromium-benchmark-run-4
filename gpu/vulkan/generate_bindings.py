25556682bcdb00273fd54642f31a54ae852df780
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-05-27 08:14:16 +0000

    Revert "vma_wrapper: create VmaAllocator for vulkan 1.1.0"
    This reverts commit de6734f75c24c2055fa7cbe24f77d8147340c51e.
    
    Reason for revert: Likely culprit for https://crbug.com/1086863.
    
    Original change's description:
    > vma_wrapper: create VmaAllocator for vulkan 1.1.0
    > 
    > For vulkan 1.1.0, VmaAllocator will use dedicated memory
    > allocation for some big vulkan memory request, and free
    > it when the allocation is released. Without this change,
    > the VmaAllocator will allocate memory from self managed
    > memory blocks for all vulkan memory request. And the
    > memory may not be released when the allocation is
    > released. VmaAllocator may keep the memory for later
    > usage. It could be the reason for the memory regression
    > in fuchsia.
    > 
    > Bug: fuchsia:52463
    > Change-Id: I7b905435d31b927b2344724f8a2e1738e0544410
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215414
    > Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Auto-Submit: Peng Huang <penghuang@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#771927}
    
    TBR=penghuang@chromium.org,vasilyt@chromium.org
    
    Change-Id: I2bc3a572ff9e95b4dcb3cdd8bf7a9f6bb5969c0a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: fuchsia:52463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216254
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772152}

--- gpu/vulkan/generate_bindings.py
+++ gpu/vulkan/generate_bindings.py
@@ -23,8 +23,6 @@ from reg import Registry
 registry = Registry()
 registry.loadFile(open(path.join(vulkan_reg_path, "vk.xml")))
 
-VULKAN_REQUIRED_API_VERSION = 'VK_API_VERSION_1_1'
-
 VULKAN_UNASSOCIATED_FUNCTIONS = [
   {
     'functions': [
@@ -411,8 +409,6 @@ namespace gpu {
 
 struct VulkanFunctionPointers;
 
-constexpr uint32_t kVulkanRequiredApiVersion = %s;
-
 COMPONENT_EXPORT(VULKAN) VulkanFunctionPointers* GetVulkanFunctionPointers();
 
 struct COMPONENT_EXPORT(VULKAN) VulkanFunctionPointers {
@@ -467,7 +463,7 @@ struct COMPONENT_EXPORT(VULKAN) VulkanFunctionPointers {
   // Unassociated functions
   VulkanFunction<PFN_vkGetInstanceProcAddr> vkGetInstanceProcAddr;
 
-""" % VULKAN_REQUIRED_API_VERSION)
+""")
 
   WriteFunctionDeclarations(file, VULKAN_UNASSOCIATED_FUNCTIONS)
 
@@ -590,7 +586,6 @@ bool VulkanFunctionPointers::BindInstanceFunctionPointers(
     VkInstance vk_instance,
     uint32_t api_version,
     const gfx::ExtensionSet& enabled_extensions) {
-  DCHECK_GE(api_version, kVulkanRequiredApiVersion);
 """)
 
   WriteInstanceFunctionPointerInitialization(file, VULKAN_INSTANCE_FUNCTIONS);
@@ -604,7 +599,6 @@ bool VulkanFunctionPointers::BindDeviceFunctionPointers(
     VkDevice vk_device,
     uint32_t api_version,
     const gfx::ExtensionSet& enabled_extensions) {
-  DCHECK_GE(api_version, kVulkanRequiredApiVersion);
   // Device functions
 """)
   WriteDeviceFunctionPointerInitialization(file, VULKAN_DEVICE_FUNCTIONS)
