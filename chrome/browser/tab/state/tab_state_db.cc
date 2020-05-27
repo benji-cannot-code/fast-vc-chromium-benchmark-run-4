7f3ca00349d3bb9c083158fd8b231bb16c1f2c07
Gabriel Charette <gab@chromium.org>
2020-05-27 20:37:41 +0000

    [base] Make PostTask destination explicit in /chrome
    Task APIs v3 will all have an explicit destination. In the past
    "default traits" meant ThreadPool. These CLs are thus a no-op
    except for the few callers that clearly wanted to post a QuitClosure()
    on the current thread (posting a QuitClosure() to a parallel task will
    merely bounce it back to the current thread anyways). Those few callers
    were migrated to ThreadTaskRunnerHandle::Get().
    
    These changes are thus required to phase out task APIs v2 (post_task.h)
    Design doc: https://docs.google.com/document/d/1tssusPykvx3g0gvbvU4HxGyn3MjJlIylnsH13-Tv6s4/edit
    
    This specific change is a subset of
    https://chromium-review.googlesource.com/c/chromium/src/+/2216733
    
    This CL was uploaded by git cl split.
    
    R=avi@chromium.org
    
    Bug: 1026641
    Change-Id: I00577ff6e5b1883bbdb62f9e2dfb95e8f8758841
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215914
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772382}

--- chrome/browser/tab/state/tab_state_db.cc
+++ chrome/browser/tab/state/tab_state_db.cc
@@ -38,8 +38,9 @@ void TabStateDB::LoadContent(const std::string& key, LoadCallback callback) {
         base::BindOnce(&TabStateDB::LoadContent, weak_ptr_factory_.GetWeakPtr(),
                        std::move(key), std::move(callback)));
   } else if (FailedToInit()) {
-    base::PostTask(FROM_HERE, base::BindOnce(std::move(callback), false,
-                                             std::vector<KeyAndValue>()));
+    base::ThreadPool::PostTask(
+        FROM_HERE,
+        base::BindOnce(std::move(callback), false, std::vector<KeyAndValue>()));
   } else {
     storage_database_->LoadEntriesWithFilter(
         base::BindRepeating(&DatabasePrefixFilter, key), CreateReadOptions(),
@@ -57,7 +58,8 @@ void TabStateDB::InsertContent(const std::string& key,
         &TabStateDB::InsertContent, weak_ptr_factory_.GetWeakPtr(),
         std::move(key), std::move(value), std::move(callback)));
   } else if (FailedToInit()) {
-    base::PostTask(FROM_HERE, base::BindOnce(std::move(callback), false));
+    base::ThreadPool::PostTask(FROM_HERE,
+                               base::BindOnce(std::move(callback), false));
   } else {
     auto contents_to_save = std::make_unique<ContentEntry>();
     tab_state_db::TabStateContentProto proto;
@@ -79,7 +81,8 @@ void TabStateDB::DeleteContent(const std::string& key,
         &TabStateDB::DeleteContent, weak_ptr_factory_.GetWeakPtr(),
         std::move(key), std::move(callback)));
   } else if (FailedToInit()) {
-    base::PostTask(FROM_HERE, base::BindOnce(std::move(callback), false));
+    base::ThreadPool::PostTask(FROM_HERE,
+                               base::BindOnce(std::move(callback), false));
   } else {
     storage_database_->UpdateEntriesWithRemoveFilter(
         std::make_unique<ContentEntry>(),
@@ -95,7 +98,8 @@ void TabStateDB::DeleteAllContent(OperationCallback callback) {
         base::BindOnce(&TabStateDB::DeleteAllContent,
                        weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
   } else if (FailedToInit()) {
-    base::PostTask(FROM_HERE, base::BindOnce(std::move(callback), false));
+    base::ThreadPool::PostTask(FROM_HERE,
+                               base::BindOnce(std::move(callback), false));
   } else {
     storage_database_->Destroy(std::move(callback));
   }
