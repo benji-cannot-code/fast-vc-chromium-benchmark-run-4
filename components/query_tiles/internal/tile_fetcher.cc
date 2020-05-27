e67f63363bae629baa4ac76b5889705633bdf0ad
Tommy Nyquist <nyquist@chromium.org>
2020-05-27 15:31:47 +0000

    Revert "Reland "Query Tiles : Integration test""
    This reverts commit bee2010fe3bf981dfd725aa1c6ea43434edb17a8.
    
    Reason for revert: OmniboxQueryTileSuggestionTest was added by this
    CL and is failing on:
    https://ci.chromium.org/p/chromium/builders/ci/android-marshmallow-arm64-rel/21620
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-x86-rel
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-arm64-rel
    https://ci.chromium.org/p/chromium/builders/ci/android-arm64-proguard-rel
    
    Original change's description:
    > Reland "Query Tiles : Integration test"
    > 
    > This is a reland of d95d22482d232f727e60b2d509ffd02606467765
    > 
    > Original change's description:
    > > Query Tiles : Integration test
    > > 
    > > This CL adds end-to-end Integration tests for query tiles. The network
    > > layer is substituted by a embedded test server that serves a fake
    > > server response. Added omnibox javatests that use this mechanism to
    > > test the full flow.
    > > 
    > > Bug: 1083807
    > > Change-Id: Ida65dd9e093ce2311def892158c6bbe4de1fe3c3
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2206153
    > > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > > Reviewed-by: Hesen Zhang <hesen@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#770512}
    > 
    > Bug: 1083807
    > Change-Id: I7f65bfb79006201bdaa545c0aba8dd7cf2b4a243
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210654
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Reviewed-by: David Trainor <dtrainor@chromium.org>
    > Reviewed-by: Hesen Zhang <hesen@chromium.org>
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#772139}
    
    TBR=dtrainor@chromium.org,qinmin@chromium.org,shaktisahu@chromium.org,xingliu@chromium.org,hesen@chromium.org
    
    Change-Id: I345b9c70b3de4e964f3e309117928992b69deb0c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1083807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218490
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772254}

--- components/query_tiles/internal/tile_fetcher.cc
+++ components/query_tiles/internal/tile_fetcher.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "base/lazy_instance.h"
 #include "components/query_tiles/internal/stats.h"
 #include "net/base/url_util.h"
 #include "net/http/http_request_headers.h"
@@ -19,9 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace query_tiles {
 namespace {
-
-// An override server URL for testing.
-base::LazyInstance<GURL>::Leaky g_override_url_for_testing;
 
 const char kRequestContentType[] = "application/x-protobuf";
 
@@ -99,10 +95,6 @@ class TileFetcherImpl : public TileFetcher {
       request->headers.SetHeader(net::HttpRequestHeaders::kAcceptLanguage,
                                  accept_languages_);
     }
-
-    if (!g_override_url_for_testing.Get().is_empty())
-      request->url = g_override_url_for_testing.Get();
-
     return request;
   }
 
@@ -182,11 +174,6 @@ std::unique_ptr<TileFetcher> TileFetcher::Create(
   return std::make_unique<TileFetcherImpl>(url, country_code, accept_languages,
                                            api_key, experiment_tag,
                                            client_version, url_loader_factory);
-}
-
-// static
-void TileFetcher::SetOverrideURLForTesting(const GURL& url) {
-  g_override_url_for_testing.Get() = url;
 }
 
 TileFetcher::TileFetcher() = default;
