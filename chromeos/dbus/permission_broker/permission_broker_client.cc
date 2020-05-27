0ffb64f3626813b326961977cf52f72c610c8fdc
Nicholas Verne <nverne@chromium.org>
2020-05-27 07:37:50 +0000

    Track allowed interfaces for sharing USB devices to guests.
    Some interfaces on multi-interface USB devices should be blocked from
    sharing with guests. The mechanism for this is a mask parameter sent to
    PermissionBroker for use when it opens the raw USB fd for the guest to use.
    The mask is a "drop privileges" mask that can only be used to narrow the
    privileges given to the fd.
    
    Change-Id: Ia37726384666a1b24e336d1e8b4ec66117b94384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2196885
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Fergus Dall <sidereal@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/master@{#772143}

--- chromeos/dbus/permission_broker/permission_broker_client.cc
+++ chromeos/dbus/permission_broker/permission_broker_client.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using permission_broker::kCheckPathAccess;
 using permission_broker::kOpenPath;
+using permission_broker::kOpenPathWithDroppedPrivileges;
 using permission_broker::kPermissionBrokerInterface;
 using permission_broker::kPermissionBrokerServiceName;
 using permission_broker::kPermissionBrokerServicePath;
@@ -63,6 +64,24 @@ class PermissionBrokerClientImpl : public PermissionBrokerClient {
     dbus::MethodCall method_call(kPermissionBrokerInterface, kOpenPath);
     dbus::MessageWriter writer(&method_call);
     writer.AppendString(path);
+    proxy_->CallMethodWithErrorCallback(
+        &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+        base::BindOnce(&PermissionBrokerClientImpl::OnOpenPathResponse,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(callback)),
+        base::BindOnce(&PermissionBrokerClientImpl::OnError,
+                       weak_ptr_factory_.GetWeakPtr(),
+                       std::move(error_callback)));
+  }
+
+  void OpenPathWithDroppedPrivileges(const std::string& path,
+                                     uint32_t allowed_interfaces_mask,
+                                     OpenPathCallback callback,
+                                     ErrorCallback error_callback) override {
+    dbus::MethodCall method_call(kPermissionBrokerInterface,
+                                 kOpenPathWithDroppedPrivileges);
+    dbus::MessageWriter writer(&method_call);
+    writer.AppendString(path);
+    writer.AppendUint32(allowed_interfaces_mask);
     proxy_->CallMethodWithErrorCallback(
         &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
         base::BindOnce(&PermissionBrokerClientImpl::OnOpenPathResponse,
