280e05b004b2ac617fe924cf66b2707a7074a5ca
nancylingwang@google.com <nancylingwang@chromium.org>
2020-05-27 18:05:55 +0000

    Create AppRegistryCacheWrapper to access AppRegistryCache.
    Ash(ShelfController) should observe AppRegistryCache to get the app
    notification badging information. Ash can't depend on chrome/*, so we
    can't use AppServiceProxy to get AppRegistryCache, and we can't use
    profile as well, because that is in chrome/browser/chromeos directory.
    
    Create a new AppRegistryCacheWrapper to map account id to
    AppRegistryCache, so that Ash or other external browser components can
    access AppRegistryCache. This could help offload AppServiceProxy, extend
    AppRegistryCache to be used out of chrome/browser, and benefit for the
    future if we have an AppRegistryCache out of AppServiceProxy.
    
    BUG=1068884
    
    Change-Id: Ib5caa94d944a0d031df9dbee0e3d2c00587ab402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212285
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772324}

--- chrome/browser/apps/app_service/app_service_proxy.cc
+++ chrome/browser/apps/app_service/app_service_proxy.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_features.h"
 #include "components/services/app_service/app_service_impl.h"
+#include "components/services/app_service/public/cpp/app_registry_cache_wrapper.h"
 #include "components/services/app_service/public/cpp/intent_filter_util.h"
 #include "components/services/app_service/public/cpp/intent_util.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
@@ -31,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/supervised_user/grit/supervised_user_unscaled_resources.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "components/user_manager/user.h"
+#include "components/user_manager/user_manager.h"
 #include "extensions/common/constants.h"
 #endif
 
@@ -94,7 +97,11 @@ AppServiceProxy::AppServiceProxy(Profile* profile)
   Initialize();
 }
 
-AppServiceProxy::~AppServiceProxy() = default;
+AppServiceProxy::~AppServiceProxy() {
+#if defined(OS_CHROMEOS)
+  AppRegistryCacheWrapper::Get().RemoveAppRegistryCache(&cache_);
+#endif
+}
 
 // static
 void AppServiceProxy::RegisterProfilePrefs(PrefRegistrySimple* registry) {
@@ -122,6 +129,15 @@ void AppServiceProxy::Initialize() {
   if (profile_->IsOffTheRecord() && !profile_->IsGuestSession()) {
     return;
   }
+
+#if defined(OS_CHROMEOS)
+  if (user_manager::UserManager::Get() &&
+      user_manager::UserManager::Get()->GetActiveUser()) {
+    AppRegistryCacheWrapper::Get().AddAppRegistryCache(
+        user_manager::UserManager::Get()->GetActiveUser()->GetAccountId(),
+        &cache_);
+  }
+#endif
 
   browser_app_launcher_ = std::make_unique<apps::BrowserAppLauncher>(profile_);
 
