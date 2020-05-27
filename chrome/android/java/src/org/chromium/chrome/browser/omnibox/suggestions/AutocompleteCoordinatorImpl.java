ea6714562d369d2ad3906166a078f4a48dc32a4a
Gang Wu <gangwu@chromium.org>
2020-05-27 18:29:14 +0000

    Fix image search from search activity
    SearchActivity did not handle image search data since image search need
    post data, so the fix is add the post data into intent to pass to chrome.
    
    Bug: 1078773
    Change-Id: I04efdad0bba315508e0b05d3d9e444d8c1a5d955
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2187191
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772335}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinatorImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinatorImpl.java
@@ -47,6 +47,7 @@ import org.chromium.ui.ViewProvider;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modelutil.LazyConstructionPropertyMcp;
 import org.chromium.ui.modelutil.MVCListAdapter;
+import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
 
 import java.util.ArrayList;
@@ -335,6 +336,11 @@ public class AutocompleteCoordinatorImpl implements AutocompleteCoordinator {
     @VisibleForTesting
     OnSuggestionsReceivedListener getSuggestionsReceivedListenerForTest() {
         return mMediator;
+    }
+
+    @VisibleForTesting
+    ModelList getSuggestionModelList() {
+        return mMediator.getSuggestionModelList();
     }
 
     private void onTileSelected(QueryTile queryTile) {
