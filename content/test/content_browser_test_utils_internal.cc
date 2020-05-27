a85574a7014b59c0a3aa57adf4a631548f93fb39
Gabriel Charette <gab@chromium.org>
2020-05-27 16:53:06 +0000

    [base] Make PostTask destination explicit in /content/test
    Task APIs v3 will all have an explicit destination. In the past
    "default traits" meant ThreadPool. These CLs are thus a no-op
    except for the few callers that clearly wanted to post a QuitClosure()
    on the current thread (posting a QuitClosure() to a parallel task will
    merely bounce it back to the current thread anyways). Those few callers
    were migrated to ThreadTaskRunnerHandle::Get().
    
    These changes are thus required to phase out task APIs v2 (post_task.h)
    Design doc: https://docs.google.com/document/d/1tssusPykvx3g0gvbvU4HxGyn3MjJlIylnsH13-Tv6s4/edit
    
    This specific change is a subset of
    https://chromium-review.googlesource.com/c/chromium/src/+/2216733
    
    This CL was uploaded by git cl split.
    
    R=sky@chromium.org
    
    Bug: 1026641
    Change-Id: I54a1ce913a2b19504b20494a930810d1a50f9139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215919
    Auto-Submit: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772273}

--- content/test/content_browser_test_utils_internal.cc
+++ content/test/content_browser_test_utils_internal.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/stack.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
-#include "base/task/post_task.h"
+#include "base/task/thread_pool.h"
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "content/browser/frame_host/frame_tree_node.h"
@@ -477,8 +477,8 @@ bool ObserveMessageFilter::OnMessageReceived(const IPC::Message& message) {
     // Exit the Wait() method if it's being used, but in a fresh stack once the
     // message is actually handled.
     if (quit_closure_ && !received_) {
-      base::PostTask(FROM_HERE,
-                     base::BindOnce(&ObserveMessageFilter::QuitWait, this));
+      base::ThreadPool::PostTask(
+          FROM_HERE, base::BindOnce(&ObserveMessageFilter::QuitWait, this));
     }
     received_ = true;
   }
