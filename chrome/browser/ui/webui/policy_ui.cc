bc64c658a1de89da70c7fb035a8c99dd8aa27763
Jérôme Gingras <jgingras@chromium.org>
2020-05-27 16:35:50 +0000

    Add a button to copy a policy's value on the chrome://policy page.
    Bug: 1020213
    Change-Id: Ib637c351af63496ba9b7bb538b0df0639fd18f96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212630
    Commit-Queue: Jerome Gingras <jgingras@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772268}

--- chrome/browser/ui/webui/policy_ui.cc
+++ chrome/browser/ui/webui/policy_ui.cc
@@ -58,6 +58,7 @@ content::WebUIDataSource* CreatePolicyUIHtmlSource() {
     {"noPoliciesSet", IDS_POLICY_NO_POLICIES_SET},
     {"offHoursActive", IDS_POLICY_OFFHOURS_ACTIVE},
     {"offHoursNotActive", IDS_POLICY_OFFHOURS_NOT_ACTIVE},
+    {"policyCopyValue", IDS_POLICY_COPY_VALUE},
     {"policiesPushOff", IDS_POLICY_PUSH_POLICIES_OFF},
     {"policiesPushOn", IDS_POLICY_PUSH_POLICIES_ON},
     {"policyLearnMore", IDS_POLICY_LEARN_MORE},
@@ -92,5 +93,4 @@ PolicyUI::PolicyUI(content::WebUI* web_ui) : WebUIController(web_ui) {
                                 CreatePolicyUIHtmlSource());
 }
 
-PolicyUI::~PolicyUI() {
-}
+PolicyUI::~PolicyUI() = default;
