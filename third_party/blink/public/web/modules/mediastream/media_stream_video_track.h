db00bb453dbcd72c084a18a7e82a349060e44b56
Antonio Gomes <tonikitoo@igalia.com>
2020-05-27 11:52:28 +0000

    Remove unused MediaStreamVideoTrack ctor
    BUG=919392
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: Ia1d237955345a66881d75627f18b734d243b69c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216593
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#772195}

--- third_party/blink/public/web/modules/mediastream/media_stream_video_track.h
+++ third_party/blink/public/web/modules/mediastream/media_stream_video_track.h
@@ -46,11 +46,6 @@ class BLINK_MODULES_EXPORT MediaStreamVideoTrack
       MediaStreamVideoSource::ConstraintsOnceCallback callback,
       bool enabled);
   static blink::WebMediaStreamTrack CreateVideoTrack(
-      const blink::WebString& id,
-      MediaStreamVideoSource* source,
-      MediaStreamVideoSource::ConstraintsOnceCallback callback,
-      bool enabled);
-  static blink::WebMediaStreamTrack CreateVideoTrack(
       MediaStreamVideoSource* source,
       const VideoTrackAdapterSettings& adapter_settings,
       const base::Optional<bool>& noise_reduction,
