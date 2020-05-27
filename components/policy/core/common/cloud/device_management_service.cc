278e6a85f624fe9523773b195e40e0c894d20095
Roman Aleksandrov <raleksandrov@google.com>
2020-05-27 18:32:45 +0000

    EnrollmentScreen: New error message when an enterpise consumer enrolls.
    Add handling new status code 906 from DMServer on an erollment request.
    It occurs when user tries to enroll device while the administrator has
    not accepted enterprise ToS.
    
    Bug: 1053831
    Change-Id: I2984ae671727771b1c6e8bf538a6fd8a37ee7df4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2213528
    Commit-Queue: Roman Aleksandrov <raleksandrov@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772338}

--- components/policy/core/common/cloud/device_management_service.cc
+++ components/policy/core/common/cloud/device_management_service.cc
@@ -125,6 +125,7 @@ const int DeviceManagementService::kPolicyNotFound;
 const int DeviceManagementService::kDeprovisioned;
 const int DeviceManagementService::kArcDisabled;
 const int DeviceManagementService::kInvalidDomainlessCustomer;
+const int DeviceManagementService::kTosHasNotBeenAccepted;
 
 // static
 std::string DeviceManagementService::JobConfiguration::GetJobTypeAsString(
