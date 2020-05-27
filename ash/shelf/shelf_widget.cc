45ae8a1a3dd8c1760596b97d7cb4054f42116523
Andrew Xu <andrewxu@chromium.org>
2020-05-27 02:38:03 +0000

    Re-enable hotseat browser test
    Re-enable HotseatShelfAppBrowserTest.TappingAppIconsHidesHotseat. Fix
    one things: In ShelfWidget, contextual nudge related function should
    not be called when the contextual nudge feature is disabled.
    
    Verify this CL with MSAN test.
    
    Bug: 1021011
    Change-Id: Iff1755001e41e28cfe0490e014502ad711e0b90c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207613
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772081}

--- ash/shelf/shelf_widget.cc
+++ ash/shelf/shelf_widget.cc
@@ -1007,7 +1007,8 @@ void ShelfWidget::OnGestureEvent(ui::GestureEvent* event) {
 
   // Tap on in-app shelf should show a contextual nudge for in-app to home
   // gesture.
-  if (event->type() == ui::ET_GESTURE_TAP && ShelfConfig::Get()->is_in_app()) {
+  if (event->type() == ui::ET_GESTURE_TAP && ShelfConfig::Get()->is_in_app() &&
+      features::AreContextualNudgesEnabled()) {
     if (delegate_view_->drag_handle()->MaybeShowDragHandleNudge()) {
       event->StopPropagation();
       return;
