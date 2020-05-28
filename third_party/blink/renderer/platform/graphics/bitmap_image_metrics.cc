d79985f4f257b0662ca790b84e4b2f6859b8559b
Wan-Teh Chang <wtc@google.com>
2020-05-28 21:43:29 +0000

    Add AVIF to the Blink.DecodedImageType histogram
    Also document a common use of the return value of
    blink::ImageDecoder::FilenameExtension().
    
    Tested:
    blink_platform_unittests --gtest_filter=*DecodedImageTypeHistogramTest*
    blink_platform_unittests --gtest_filter=*DecodedImageTypeHistogramTest* --enable-features=AVIF
    
    Bug: 960620
    Change-Id: Id42036fe60e9b2b91308bcfbb7275c6e5224d1f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219100
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772898}

--- third_party/blink/renderer/platform/graphics/bitmap_image_metrics.cc
+++ third_party/blink/renderer/platform/graphics/bitmap_image_metrics.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_base.h"
 #include "base/numerics/safe_conversions.h"
+#include "media/media_buildflags.h"
 #include "third_party/blink/renderer/platform/geometry/int_size.h"
 #include "third_party/blink/renderer/platform/graphics/color_space_gamut.h"
 #include "third_party/blink/renderer/platform/instrumentation/histogram.h"
@@ -29,7 +30,11 @@ void BitmapImageMetrics::CountDecodedImageType(const String& type) {
                                   ? kImageICO
                                   : type == "bmp"
                                         ? kImageBMP
-                                        : DecodedImageType::kImageUnknown;
+#if BUILDFLAG(ENABLE_AV1_DECODER)
+                                        : type == "avif"
+                                              ? kImageAVIF
+#endif
+                                              : DecodedImageType::kImageUnknown;
 
   DEFINE_THREAD_SAFE_STATIC_LOCAL(
       EnumerationHistogram, decoded_image_type_histogram,
--- third_party/blink/renderer/platform/graphics/bitmap_image_metrics.h
+++ third_party/blink/renderer/platform/graphics/bitmap_image_metrics.h
@@ -30,7 +30,8 @@ class PLATFORM_EXPORT BitmapImageMetrics {
     kImageWebP = 4,
     kImageICO = 5,
     kImageBMP = 6,
-    kDecodedImageTypeEnumEnd = kImageBMP + 1
+    kImageAVIF = 7,
+    kDecodedImageTypeEnumEnd = kImageAVIF + 1
   };
 
   // Values synced with 'Gamma' in src/tools/metrics/histograms/enums.xml. These
@@ -71,6 +72,7 @@ class PLATFORM_EXPORT BitmapImageMetrics {
     kMaxValue = kYCbCrOther,
   };
 
+  // |type| is the return value of ImageDecoder::FilenameExtension().
   static void CountDecodedImageType(const String& type);
   static void CountImageOrientation(const ImageOrientationEnum);
   static void CountImageDensityCorrection(bool densityCorrectionPresent);
