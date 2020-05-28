8926f5f49fd5f035bc154430f2baf07a5072c4be
Gabriel Charette <gab@chromium.org>
2020-05-28 19:44:35 +0000

    [base] Clarify RunLoop docs
    Legacy comments and modern comments in the same header
    were self contradictory.
    
    R=chrisha@chromium.org, fdoray@chromium.org
    
    Bug: 1085416
    Change-Id: Ia024b399360a2f30db84b508a870cebc3e50c257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219022
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Reviewed-by: Oliver Li <olivierli@google.com>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772834}

--- base/run_loop.cc
+++ base/run_loop.cc
@@ -137,7 +137,7 @@ void RunLoop::RunUntilIdle() {
 void RunLoop::Quit() {
   // Thread-safe.
 
-  // This can only be hit if run_loop->Quit() is called directly (QuitClosure()
+  // This can only be hit if RunLoop::Quit() is called directly (QuitClosure()
   // proxies through ProxyToTaskRunner() as it can only deref its WeakPtr on
   // |origin_task_runner_|).
   if (!origin_task_runner_->RunsTasksInCurrentSequence()) {
@@ -156,7 +156,7 @@ void RunLoop::Quit() {
 void RunLoop::QuitWhenIdle() {
   // Thread-safe.
 
-  // This can only be hit if run_loop->QuitWhenIdle() is called directly
+  // This can only be hit if RunLoop::QuitWhenIdle() is called directly
   // (QuitWhenIdleClosure() proxies through ProxyToTaskRunner() as it can only
   // deref its WeakPtr on |origin_task_runner_|).
   if (!origin_task_runner_->RunsTasksInCurrentSequence()) {
@@ -169,9 +169,9 @@ void RunLoop::QuitWhenIdle() {
 }
 
 RepeatingClosure RunLoop::QuitClosure() {
-  // Obtaining the QuitClosure() is not thread-safe; either post the
-  // QuitClosure() from the run thread or invoke Quit() directly (which is
-  // thread-safe).
+  // Obtaining the QuitClosure() is not thread-safe; either obtain the
+  // QuitClosure() from the owning thread before Run() or invoke Quit() directly
+  // (which is thread-safe).
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   allow_quit_current_deprecated_ = false;
 
@@ -181,9 +181,9 @@ RepeatingClosure RunLoop::QuitClosure() {
 }
 
 RepeatingClosure RunLoop::QuitWhenIdleClosure() {
-  // Obtaining the QuitWhenIdleClosure() is not thread-safe; either post the
-  // QuitWhenIdleClosure() from the run thread or invoke QuitWhenIdle() directly
-  // (which is thread-safe).
+  // Obtaining the QuitWhenIdleClosure() is not thread-safe; either obtain the
+  // QuitWhenIdleClosure() from the owning thread before Run() or invoke
+  // QuitWhenIdle() directly (which is thread-safe).
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   allow_quit_current_deprecated_ = false;
 
