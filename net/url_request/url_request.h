2ea462cbcfa711999eca45df160627ef08496792
Lei Zhang <thestig@chromium.org>
2020-05-28 23:48:26 +0000

    Remove |net::URLRequest::leak_tracker_|.
    It's probably not useful, since LeakTracker::CheckForLeaks() isn't being
    called.
    
    Change-Id: I96b226e43698a859c934e45916514d5369927ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219177
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772946}

--- net/url_request/url_request.h
+++ net/url_request/url_request.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/debug/leak_tracker.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
@@ -928,8 +927,6 @@ class NET_EXPORT URLRequest : public base::SupportsUserData {
   // |this| is currently being blocked by.
   std::string blocked_by_;
   bool use_blocked_by_as_load_param_;
-
-  base::debug::LeakTracker<URLRequest> leak_tracker_;
 
   // Safe-guard to ensure that we do not send multiple "I am completed"
   // messages to network delegate.
