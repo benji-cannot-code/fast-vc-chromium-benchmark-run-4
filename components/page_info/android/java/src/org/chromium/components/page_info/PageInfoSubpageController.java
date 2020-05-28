05af52b123ea49ff106b7147793ac1a0684036e5
Ehimare Okoyomon <eokoyomon@chromium.org>
2020-05-28 15:00:55 +0000

    [Android] Add a base controller and subpage template for page info v2
    Create PageInfoSubpageController and PageInfoSubpage for control
    of a specific section of the page info v2. Componentized the cookies
    section as an example.
    https://screenshot.googleplex.com/XnhMwuBkVzG.png
    
    Bug: 1077766
    Change-Id: I7bf71c62859bcae7b2f24bd9509947c003a51c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209070
    Commit-Queue: Ehimare Okoyomon <eokoyomon@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772697}

---
+++ components/page_info/android/java/src/org/chromium/components/page_info/PageInfoSubpageController.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.page_info;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Interface for a page info subpage controller.
+ */
+public interface PageInfoSubpageController {
+    /**
+     * Returns a title string for the page info subpage.
+     */
+    String getSubpageTitle();
+
+    /**
+     * Returns a personalized subview to be used inside of the page info subpage.
+     */
+    View createViewForSubpage(ViewGroup parent);
+
+    /**
+     * Called before the subpage closes in order to perform any necessary cleanup.
+     */
+    void willRemoveSubpage();
+}
