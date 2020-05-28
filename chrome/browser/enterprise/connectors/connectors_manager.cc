63128950b713c663af229044067de7b3dc06ae5a
Roger Tawa <rogerta@chromium.org>
2020-05-28 17:58:52 +0000

    Change SafeBrowsingPrivateEventRouter to use ConnectorsManager.
    Bug: 1069068
    Change-Id: Ia6c57279558b0a29e06cc949bef05bf3c92947f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212985
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772780}

--- chrome/browser/enterprise/connectors/connectors_manager.cc
+++ chrome/browser/enterprise/connectors/connectors_manager.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/browser/url_util.h"
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
-#include "components/safe_browsing/core/features.h"
 #include "components/url_matcher/url_matcher.h"
 
 namespace enterprise_connectors {
@@ -393,6 +392,12 @@ ConnectorsManager::GetReportingSettingsFromConnectorPolicy(
 base::Optional<ReportingSettings>
 ConnectorsManager::GetReportingSettingsFromLegacyPolicies(
     ReportingConnector connector) const {
+  if (!g_browser_process || !g_browser_process->local_state() ||
+      !g_browser_process->local_state()->GetBoolean(
+          prefs::kUnsafeEventsReportingEnabled)) {
+    return base::nullopt;
+  }
+
   return ReportingSettings();
 }
 
--- chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h
+++ chrome/browser/extensions/api/safe_browsing_private/safe_browsing_private_event_router.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/prefs/pref_change_registrar.h"
 
 namespace content {
 class BrowserContext;
@@ -216,12 +215,6 @@ class SafeBrowsingPrivateEventRouter : public KeyedService {
   // with CBCM and the appropriate policies are enabled.
   void InitRealtimeReportingClient();
 
-  // Initialize DeviceManagementService and |client_| after validating the
-  // browser can upload data.
-  void InitRealtimeReportingClientCallback(
-      policy::DeviceManagementService* device_management_service,
-      bool authorized);
-
   // Continues execution if the client is authorized to do so.
   void IfAuthorized(base::OnceCallback<void(bool)> cont);
 
@@ -272,7 +265,6 @@ class SafeBrowsingPrivateEventRouter : public KeyedService {
   // The |private_client_| is used on platforms where we cannot just get a
   // client and we create our own (used through |client_|).
   std::unique_ptr<policy::CloudPolicyClient> private_client_;
-  PrefChangeRegistrar registrar_;
 
   base::WeakPtrFactory<SafeBrowsingPrivateEventRouter> weak_ptr_factory_{this};
   DISALLOW_COPY_AND_ASSIGN(SafeBrowsingPrivateEventRouter);
