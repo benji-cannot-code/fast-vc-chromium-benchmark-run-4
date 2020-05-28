18dce2822cebb39aa979262d756eec583f3cf09f
Li Lin <llin@chromium.org>
2020-05-28 23:27:00 +0000

    Enable Assistant Media Session integration by default.
    Bug: b/120559778
    Test: Manual
    Change-Id: I4498ec1160508afd6fccc9489a6e1fbe96bcc127
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219352
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772926}

--- chromeos/services/assistant/public/cpp/features.cc
+++ chromeos/services/assistant/public/cpp/features.cc
@@ -48,8 +48,7 @@ const base::Feature kEnableOnDeviceAssistant{"ChromeOSOnDeviceAssistant",
                                              base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kEnableMediaSessionIntegration{
-    "AssistantEnableMediaSessionIntegration",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    "AssistantEnableMediaSessionIntegration", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Disable voice match for test purpose.
 const base::Feature kDisableVoiceMatch{"DisableVoiceMatch",
