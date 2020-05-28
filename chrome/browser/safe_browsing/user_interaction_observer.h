bff989a9b3596d3c08676bad01e8740a872517dc
Mustafa Emre Acer <meacer@chromium.org>
2020-05-28 22:36:36 +0000

    [SB Delayed Warnings] Reject password save and autofill requests
    When the Safe Browsing Delayed Warnings experiment delays a SafeBrowsing
    warning on a page, passwords shouldn't be saved or autofilled on that
    page. This CL implements that, and records a metric the first time a
    request is rejected.
    
    Bug: 1085217
    Change-Id: Ieae193beccd93062af366088cd3b42b313ef3a45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215246
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772914}

--- chrome/browser/safe_browsing/user_interaction_observer.h
+++ chrome/browser/safe_browsing/user_interaction_observer.h
@@ -47,7 +47,10 @@ enum class DelayedWarningEvent {
   // The page tried to display a JavaScript dialog (alert/confirm/prompt). It
   // was blocked and the warning was shown.
   kWarningShownOnJavaScriptDialog = 8,
-  kMaxValue = kWarningShownOnJavaScriptDialog,
+  // The page was denied a password save or autofill request. This doesn't show
+  // an interstitial and is recorded once per navigation.
+  kPasswordSaveOrAutofillDenied = 9,
+  kMaxValue = kPasswordSaveOrAutofillDenied,
 };
 
 // Name of the histogram.
@@ -99,6 +102,9 @@ class SafeBrowsingUserInteractionObserver
   // show a JavaScript dialog (alert, confirm or prompt). Shows the
   // delayed interstitial immediately.
   void OnJavaScriptDialog();
+  // Called when a password save or autofill request is denied to the current
+  // page. Records a metric once per navigation.
+  void OnPasswordSaveOrAutofillDenied();
 
  private:
   bool HandleKeyPress(const content::NativeWebKeyboardEvent& event);
@@ -116,6 +122,7 @@ class SafeBrowsingUserInteractionObserver
   scoped_refptr<SafeBrowsingUIManager> ui_manager_;
   bool interstitial_shown_ = false;
   bool mouse_click_with_no_warning_recorded_ = false;
+  bool password_save_or_autofill_denied_metric_recorded_ = false;
   // This will be set to true if the initial navigation that caused this
   // observer to be created has finished. We need this extra bit because
   // observers can only detect download navigations in DidFinishNavigation.
