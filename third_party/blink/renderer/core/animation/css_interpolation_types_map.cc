c8f68b1b1bd47b30165f830d394dc7ac5df91928
George Steel <gtsteel@google.com>
2020-05-28 17:35:41 +0000

    Add interpolation for font-stretch
    Uses font-weight as a template for the implementation as the value is
    also a scalar with keywords.
    
    Bug: 924353
    Change-Id: If635e279f369f7b1433b98d9cfb7f6423fbcb3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209389
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: George Steel <gtsteel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772767}

--- third_party/blink/renderer/core/animation/css_interpolation_types_map.cc
+++ third_party/blink/renderer/core/animation/css_interpolation_types_map.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/css_default_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_filter_list_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_font_size_interpolation_type.h"
+#include "third_party/blink/renderer/core/animation/css_font_stretch_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_font_variation_settings_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_font_weight_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_image_interpolation_type.h"
@@ -47,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/animation/css_translate_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_var_cycle_interpolation_type.h"
 #include "third_party/blink/renderer/core/animation/css_visibility_interpolation_type.h"
+#include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/css_syntax_definition.h"
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
 #include "third_party/blink/renderer/core/css/property_registry.h"
@@ -254,6 +256,10 @@ const InterpolationTypes& CSSInterpolationTypesMap::Get(
       case CSSPropertyID::kFontWeight:
         applicable_types->push_back(
             std::make_unique<CSSFontWeightInterpolationType>(used_property));
+        break;
+      case CSSPropertyID::kFontStretch:
+        applicable_types->push_back(
+            std::make_unique<CSSFontStretchInterpolationType>(used_property));
         break;
       case CSSPropertyID::kFontVariationSettings:
         applicable_types->push_back(
