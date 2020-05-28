dcf14ff5a2d44dba782fcb5321f921698eaf4e9f
Balazs Engedy <engedy@chromium.org>
2020-05-28 21:58:15 +0000

    Do not animate notifications icon for abusive requests.
    On sites known to show abusive notification requests, the quiet UI icon
    should be static, not animated.
    
    Bug: 1081233, 1087561
    Change-Id: I6f5fdbacaf4475f273238b0d27c7e46a11ed1d79
    TBR: hkamila@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218770
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772903}

--- chrome/browser/ui/content_settings/content_setting_image_model_unittest.cc
+++ chrome/browser/ui/content_settings/content_setting_image_model_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/browser/tab_specific_content_settings.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/permissions/features.h"
+#include "components/permissions/notification_permission_ui_selector.h"
 #include "components/permissions/permission_request.h"
 #include "components/permissions/permission_request_manager.h"
 #include "components/permissions/permission_uma_util.h"
@@ -50,6 +51,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content_settings::TabSpecificContentSettings;
 
 namespace {
+
+class TestQuietNotificationPermissionUiSelector
+    : public permissions::NotificationPermissionUiSelector {
+ public:
+  explicit TestQuietNotificationPermissionUiSelector(
+      QuietUiReason simulated_reason_for_quiet_ui)
+      : simulated_reason_for_quiet_ui_(simulated_reason_for_quiet_ui) {}
+  ~TestQuietNotificationPermissionUiSelector() override = default;
+
+ protected:
+  // permissions::NotificationPermissionUiSelector:
+  void SelectUiToUse(permissions::PermissionRequest* request,
+                     DecisionMadeCallback callback) override {
+    std::move(callback).Run(
+        Decision(simulated_reason_for_quiet_ui_, base::nullopt));
+  }
+
+ private:
+  QuietUiReason simulated_reason_for_quiet_ui_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestQuietNotificationPermissionUiSelector);
+};
 
 class ContentSettingImageModelTest : public BrowserWithTestWindowTest {
  public:
@@ -420,8 +443,8 @@ TEST_F(ContentSettingImageModelTest, NotificationsIconVisibility) {
   EXPECT_FALSE(content_setting_image_model->is_visible());
 }
 
-TEST_F(ContentSettingImageModelTest, NotificationsPrompt) {
 #if !defined(OS_ANDROID)
+TEST_F(ContentSettingImageModelTest, NotificationsPrompt) {
   base::test::ScopedFeatureList feature_list;
   feature_list.InitWithFeatures(
       {features::kQuietNotificationPrompts},
@@ -441,11 +464,54 @@ TEST_F(ContentSettingImageModelTest, NotificationsPrompt) {
   EXPECT_TRUE(manager_->ShouldCurrentRequestUseQuietUI());
   content_setting_image_model->Update(web_contents());
   EXPECT_TRUE(content_setting_image_model->is_visible());
+  EXPECT_NE(0, content_setting_image_model->explanatory_string_id());
   manager_->Accept();
   EXPECT_FALSE(manager_->ShouldCurrentRequestUseQuietUI());
   content_setting_image_model->Update(web_contents());
   EXPECT_FALSE(content_setting_image_model->is_visible());
-#endif  // !defined(OS_ANDROID)
 }
+
+TEST_F(ContentSettingImageModelTest, NotificationsPromptCrowdDeny) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(features::kQuietNotificationPrompts);
+
+  auto content_setting_image_model =
+      ContentSettingImageModel::CreateForContentType(
+          ContentSettingImageModel::ImageType::NOTIFICATIONS_QUIET_PROMPT);
+  EXPECT_FALSE(content_setting_image_model->is_visible());
+  manager_->set_notification_permission_ui_selector_for_testing(
+      std::make_unique<TestQuietNotificationPermissionUiSelector>(
+          permissions::NotificationPermissionUiSelector::QuietUiReason::
+              kTriggeredByCrowdDeny));
+  manager_->AddRequest(&request_);
+  WaitForBubbleToBeShown();
+  EXPECT_TRUE(manager_->ShouldCurrentRequestUseQuietUI());
+  content_setting_image_model->Update(web_contents());
+  EXPECT_TRUE(content_setting_image_model->is_visible());
+  EXPECT_EQ(0, content_setting_image_model->explanatory_string_id());
+  manager_->Accept();
+}
+
+TEST_F(ContentSettingImageModelTest, NotificationsPromptAbusive) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(features::kQuietNotificationPrompts);
+
+  auto content_setting_image_model =
+      ContentSettingImageModel::CreateForContentType(
+          ContentSettingImageModel::ImageType::NOTIFICATIONS_QUIET_PROMPT);
+  EXPECT_FALSE(content_setting_image_model->is_visible());
+  manager_->set_notification_permission_ui_selector_for_testing(
+      std::make_unique<TestQuietNotificationPermissionUiSelector>(
+          permissions::NotificationPermissionUiSelector::QuietUiReason::
+              kTriggeredDueToAbusiveRequests));
+  manager_->AddRequest(&request_);
+  WaitForBubbleToBeShown();
+  EXPECT_TRUE(manager_->ShouldCurrentRequestUseQuietUI());
+  content_setting_image_model->Update(web_contents());
+  EXPECT_TRUE(content_setting_image_model->is_visible());
+  EXPECT_EQ(0, content_setting_image_model->explanatory_string_id());
+  manager_->Accept();
+}
+#endif  // !defined(OS_ANDROID)
 
 }  // namespace
