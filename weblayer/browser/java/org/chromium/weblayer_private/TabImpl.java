baef5ab843e325c1c96a7d730d2e6d74ab7dca84
Scott Violet <sky@chromium.org>
2020-05-28 03:50:46 +0000

    weblayer: makes browser controls not hide when a11y is enabled
    The controls shouldn't scroll if a11y is enabled.
    
    I plan to merge this to 84, so that I'm forking AccessibilityUtil.
    The plan it to move AccessibilityUtil to ui so that it can be shared.
    
    BUG=1085210
    TEST=BrowserControlsTest.testAccessibility
    
    Change-Id: Icc04354639d3ae1b4f64773752705b37117e9f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219008
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772549}

--- weblayer/browser/java/org/chromium/weblayer_private/TabImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/TabImpl.java
@@ -113,6 +113,8 @@ public final class TabImpl extends ITab.Stub {
 
     private boolean mPostContainerViewInitDone;
 
+    private AccessibilityUtil.Observer mAccessibilityObserver;
+
     private static class InternalAccessDelegateImpl
             implements ViewEventSink.InternalAccessDelegate {
         @Override
@@ -228,6 +230,13 @@ public final class TabImpl extends ITab.Stub {
                 new InterceptNavigationDelegateImpl(mInterceptNavigationDelegateClient);
         mInterceptNavigationDelegateClient.initializeWithDelegate(mInterceptNavigationDelegate);
         sTabMap.put(mId, this);
+
+        mAccessibilityObserver = (boolean enabled) -> {
+            setBrowserControlsVisibilityConstraint(ImplControlsVisibilityReason.ACCESSIBILITY,
+                    enabled ? BrowserControlsState.SHOWN : BrowserControlsState.BOTH);
+        };
+        // addObserver() calls to observer when added.
+        WebLayerAccessibilityUtil.get().addObserver(mAccessibilityObserver);
     }
 
     private void doInitAfterSettingContainerView() {
@@ -703,6 +712,8 @@ public final class TabImpl extends ITab.Stub {
         mWebContents.removeObserver(mWebContentsObserver);
         TabImplJni.get().deleteTab(mNativeTab);
         mNativeTab = 0;
+
+        WebLayerAccessibilityUtil.get().removeObserver(mAccessibilityObserver);
     }
 
     @CalledByNative
@@ -752,6 +763,11 @@ public final class TabImpl extends ITab.Stub {
         onBrowserControlsStateUpdated(mBrowserControlsVisibility.get());
     }
 
+    @VisibleForTesting
+    public boolean canBrowserControlsScrollForTesting() {
+        return mBrowserControlsVisibility.get() == BrowserControlsState.BOTH;
+    }
+
     private void onBrowserControlsStateUpdated(int state) {
         // If something has overridden the FIP's SHOWN constraint, cancel FIP. This causes FIP to
         // dismiss when entering fullscreen.
@@ -767,7 +783,8 @@ public final class TabImpl extends ITab.Stub {
         // happen). For js dialogs, the renderer's update will come when the dialog is hidden, and
         // since that animates from 0 height, it causes a flicker since the override is already set
         // to fully show. Thus, disable animation.
-        if (state == BrowserControlsState.SHOWN && mBrowser.getActiveTab() == this
+        if (state == BrowserControlsState.SHOWN && mBrowser != null
+                && mBrowser.getActiveTab() == this
                 && !TabImplJni.get().isRendererControllingBrowserControlsOffsets(mNativeTab)) {
             mViewAndroidDelegate.setIgnoreRendererUpdates(true);
             getViewController().showControls();
