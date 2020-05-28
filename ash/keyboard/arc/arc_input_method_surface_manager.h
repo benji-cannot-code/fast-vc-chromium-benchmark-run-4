02c6801aae97fac050e5cc6bbd445be994bc8581
Yuichiro Hanada <yhanada@google.com>
2020-05-28 12:30:46 +0000

    Introduce ArcInputMethodBoundsTracker interface in ash/public.
    The interface allows classes outside of ash/ to observe any bounds
    changes of ARC IME window.
    This CL should not cause any behavior change.
    
    Bug: 1060272
    Change-Id: I3b5b0652c89fd87ffe93de4903dae11523b30dae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2217827
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772651}

--- ash/keyboard/arc/arc_input_method_surface_manager.h
+++ ash/keyboard/arc/arc_input_method_surface_manager.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_KEYBOARD_ARC_ARC_INPUT_METHOD_SURFACE_MANAGER_H_
 #define ASH_KEYBOARD_ARC_ARC_INPUT_METHOD_SURFACE_MANAGER_H_
 
+#include "ash/public/cpp/keyboard/arc/arc_input_method_bounds_tracker.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "components/exo/input_method_surface_manager.h"
@@ -13,15 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-class ArcInputMethodSurfaceManager : public exo::InputMethodSurfaceManager {
+class ArcInputMethodSurfaceManager : public exo::InputMethodSurfaceManager,
+                                     public ArcInputMethodBoundsTracker {
  public:
-  class Observer : public base::CheckedObserver {
-   public:
-    ~Observer() override = default;
-    virtual void OnArcInputMethodSurfaceBoundsChanged(
-        const gfx::Rect& bounds) = 0;
-  };
-
   ArcInputMethodSurfaceManager();
   ~ArcInputMethodSurfaceManager() override;
 
@@ -31,14 +26,8 @@ class ArcInputMethodSurfaceManager : public exo::InputMethodSurfaceManager {
   void RemoveSurface(exo::InputMethodSurface* surface) override;
   void OnTouchableBoundsChanged(exo::InputMethodSurface* surface) override;
 
-  // Management of the observer list.
-  void AddObserver(Observer* observer);
-  void RemoveObserver(Observer* observer);
-
  private:
   exo::InputMethodSurface* input_method_surface_ = nullptr;  // Not owned
-
-  base::ObserverList<Observer> observers_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcInputMethodSurfaceManager);
 };
---
+++ ash/public/cpp/keyboard/arc/arc_input_method_bounds_tracker.cc
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/public/cpp/keyboard/arc/arc_input_method_bounds_tracker.h"
+
+namespace ash {
+
+namespace {
+
+ArcInputMethodBoundsTracker* g_instance = nullptr;
+
+}  // namespace
+
+// static
+ArcInputMethodBoundsTracker* ArcInputMethodBoundsTracker::Get() {
+  return g_instance;
+}
+
+ArcInputMethodBoundsTracker::ArcInputMethodBoundsTracker() {
+  DCHECK(!g_instance);
+  g_instance = this;
+}
+
+ArcInputMethodBoundsTracker::~ArcInputMethodBoundsTracker() {
+  DCHECK_EQ(g_instance, this);
+  g_instance = nullptr;
+}
+
+void ArcInputMethodBoundsTracker::AddObserver(Observer* observer) {
+  observers_.AddObserver(observer);
+}
+
+void ArcInputMethodBoundsTracker::RemoveObserver(Observer* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+void ArcInputMethodBoundsTracker::NotifyArcInputMethodBoundsChanged(
+    const gfx::Rect& bounds) {
+  for (Observer& observer : observers_)
+    observer.OnArcInputMethodBoundsChanged(bounds);
+}
+
+}  // namespace ash
