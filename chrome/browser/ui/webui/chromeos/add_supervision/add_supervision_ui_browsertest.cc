035c50e1a59aacf11ccfe147e925d142cdd461e9
Danan S <danan@chromium.org>
2020-05-28 22:14:47 +0000

    Remove ASAN/LSAN/MSAN guards in AddSupervisionBrowserTests
    The underlying issue causing the tests to fail was fixed in
    crrev.com/c/1278287
    
    I tested this by running the tests locally with msan, and lsan+asan GN
    args enabled.
    
    Bug: 1002560,1004237
    Change-Id: Iea2693d8962624576b34526d1f8ae1c0aeb286d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219150
    Auto-Submit: Dan S <danan@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772908}

--- chrome/browser/ui/webui/chromeos/add_supervision/add_supervision_ui_browsertest.cc
+++ chrome/browser/ui/webui/chromeos/add_supervision/add_supervision_ui_browsertest.cc
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-// NOTE: This test is flaky and therefore disabled under MSAN:
-// https://crbug.com/1002560
-#if !defined(MEMORY_SANITIZER)
-
 namespace {
 
 const char kGetAddSupervisionUIElementJS[] =
@@ -95,14 +91,7 @@ class AddSupervisionBrowserTest : public InProcessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(AddSupervisionBrowserTest);
 };
 
-// Disabled on ASan and LSAn builds, because it's very flaky. See
-// crbug.com/1004237
-#if defined(ADDRESS_SANITIZER) || defined(LEAK_SANITIZER)
-#define MAYBE_URLParameters DISABLED_URLParameters
-#else
-#define MAYBE_URLParameters URLParameters
-#endif
-IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest, MAYBE_URLParameters) {
+IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest, URLParameters) {
   // Open the Add Supervision URL.
   ui_test_utils::NavigateToURL(browser(), add_supervision_webui_url());
   content::WaitForLoadStop(contents());
@@ -173,15 +162,7 @@ IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest, ShowOfflineScreen) {
                                std::string(".webviewDiv")));
 }
 
-// Disabled on ASan and LSAn builds, because it's very flaky. See
-// crbug.com/1004237
-#if defined(ADDRESS_SANITIZER) || defined(LEAK_SANITIZER)
-#define MAYBE_ShowConfirmSignoutDialog DISABLED_ShowConfirmSignoutDialog
-#else
-#define MAYBE_ShowConfirmSignoutDialog ShowConfirmSignoutDialog
-#endif
-IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest,
-                       MAYBE_ShowConfirmSignoutDialog) {
+IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest, ShowConfirmSignoutDialog) {
   // Open the Add Supervision URL.
   ui_test_utils::NavigateToURL(browser(), add_supervision_webui_url());
   content::WaitForLoadStop(contents());
@@ -207,14 +188,7 @@ IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest,
   ASSERT_TRUE(ConfirmSignoutDialog::IsShowing());
 }
 
-// Disabled on ASan and LSAn builds, because it's very flaky. See
-// crbug.com/1004237
-#if defined(ADDRESS_SANITIZER) || defined(LEAK_SANITIZER)
-#define MAYBE_UMATest DISABLED_UMATest
-#else
-#define MAYBE_UMATest UMATest
-#endif
-IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest, MAYBE_UMATest) {
+IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest, UMATest) {
   base::HistogramTester histogram_tester;
   base::UserActionTester user_action_tester;
 
@@ -246,7 +220,5 @@ IN_PROC_BROWSER_TEST_F(AddSupervisionBrowserTest, MAYBE_UMATest) {
                 "AddSupervisionDialog_EnrollmentCompleted"),
             1);
 }
-
-#endif  // !defined(MEMORY_SANITIZER)
 
 }  // namespace chromeos
