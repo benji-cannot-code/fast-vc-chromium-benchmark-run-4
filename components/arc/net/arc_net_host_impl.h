33109813abe937876ca62d0db444fb3b70746e49
Hugo Benichi <hugobenichi@google.com>
2020-05-28 02:04:12 +0000

    arc: net: remove force network property updates
    Now that ArcNetHostImpl overrides chromeos::networkStatehandlerObserver
    NetworkPropertiesUpdated and fetches shill properties asynchronously, it
    is not necessary anymore to force NetworkState updates via
    chromeos::NetworkStateHandler to obtain properties that may be missing
    like IP configuration data.
    
    This patch removes this logic and partially reverts Change-Id
    I24d26d6492f133e0c38949c8c3b4403687bfac21 and Change-Id
    I2a39ba9c8d14cf42b6b0dbdd10c885bf5be09cfa.
    
    BUG=b:145960788
    BUG=b:143258259
    TEST=Flashed chrome, tested various connectivity setup with multiple
    networks, including L2TP connections and Chrome 3p VPN connections.
    
    Change-Id: I030a706740986d30830454a3308ea13a05790ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2217276
    Commit-Queue: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772521}

--- components/arc/net/arc_net_host_impl.h
+++ components/arc/net/arc_net_host_impl.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 #include <map>
 #include <memory>
-#include <set>
 #include <string>
 #include <vector>
 
@@ -151,8 +150,6 @@ class ArcNetHostImpl : public KeyedService,
       const std::string& error_name,
       std::unique_ptr<base::DictionaryValue> error_data);
 
-  // Request properties of the Service corresponding to |service_path|.
-  void RequestUpdateForNetwork(const std::string& service_path);
   // Callback for chromeos::NetworkHandler::GetShillProperties
   void ReceiveShillProperties(const std::string& service_path,
                               const base::DictionaryValue& shill_properties);
@@ -162,9 +159,6 @@ class ArcNetHostImpl : public KeyedService,
   // True if the chrome::NetworkStateHandler is currently being observed for
   // state changes.
   bool observing_network_state_ = false;
-  // Contains all service paths for which a property update request is
-  // currently scheduled.
-  std::set<std::string> pending_service_property_requests_;
   // Cached shill properties for all active networks, keyed by Service path.
   std::map<std::string, base::Value> shill_network_properties_;
 
