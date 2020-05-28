29e83aa939d816d23e9a8aa4681282eaed092945
Brian Sheedy <bsheedy@chromium.org>
2020-05-28 22:09:09 +0000

    Remove -stable GPU Mac OS usage
    Removes the usage of the Mac -stable OS dimensions from
    //testing/buildbot. This should be the last remaining usage of the
    synthetic swarming dimensions, so they should be able to be removed from
    swarming after this.
    
    Also removes the uses of synthetic dimensions from the unittests and
    the associated generate_buildbot_json.py code.
    
    Bug: 1085617
    Change-Id: Iaebcb10d416dad3f773462e3bfb3b3871a91d653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219042
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772907}

--- testing/buildbot/generate_buildbot_json_unittest.py
+++ testing/buildbot/generate_buildbot_json_unittest.py
@@ -382,7 +382,7 @@ NVIDIA_GPU_TELEMETRY_TEST_WATERFALL = """\
         'swarming': {
           'dimension_sets': [
             {
-              'gpu': 'nvidia-quadro-p400-win10-stable',
+              'gpu': '10de:1cb3-26.21.14.3102',
             },
           ],
         },
@@ -408,7 +408,7 @@ INTEL_GPU_TELEMETRY_TEST_WATERFALL = """\
         'swarming': {
           'dimension_sets': [
             {
-              'gpu': 'intel-hd-630-win10-stable',
+              'gpu': '8086:5912-24.20.100.6286',
             },
           ],
         },
@@ -434,7 +434,7 @@ INTEL_UHD_GPU_TELEMETRY_TEST_WATERFALL = """\
         'swarming': {
           'dimension_sets': [
             {
-              'gpu': 'intel-uhd-630-win10-stable',
+              'gpu': '8086:3e92-24.20.100.6286',
             },
           ],
         },
@@ -1613,7 +1613,7 @@ NVIDIA_GPU_TELEMETRY_TEST_OUTPUT = """\
           "can_use_on_swarming_builders": true,
           "dimension_sets": [
             {
-              "gpu": "nvidia-quadro-p400-win10-stable"
+              "gpu": "10de:1cb3-26.21.14.3102"
             }
           ],
           "idempotent": false
@@ -1655,7 +1655,7 @@ INTEL_GPU_TELEMETRY_TEST_OUTPUT = """\
           "can_use_on_swarming_builders": true,
           "dimension_sets": [
             {
-              "gpu": "intel-hd-630-win10-stable"
+              "gpu": "8086:5912-24.20.100.6286"
             }
           ],
           "idempotent": false
@@ -1697,7 +1697,7 @@ INTEL_UHD_GPU_TELEMETRY_TEST_OUTPUT = """\
           "can_use_on_swarming_builders": true,
           "dimension_sets": [
             {
-              "gpu": "intel-uhd-630-win10-stable"
+              "gpu": "8086:3e92-24.20.100.6286"
             }
           ],
           "idempotent": false
