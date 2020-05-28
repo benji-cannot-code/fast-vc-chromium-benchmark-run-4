fb7232805a26b8cfdf89a71eaa350f56d246b316
Kurt Catti-Schmidt <kschmi@microsoft.com>
2020-05-28 00:14:03 +0000

    [GridNG] Moving ng-grid files to core/layout/ng/grid/
    Per https://chromium-review.googlesource.com/c/chromium/src/+/2163429,
    this change moves GridNG files to their own directory. This will help
    organize subsequent changes where we will be introducing classes
    specific to GridNG.
    
    There should be no behavioral changes associated with this PR.
    
    Bug: 1045599
    Change-Id: I50c2c98a7903904bf80663801a80ed1874bb0f1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218616
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772477}

--- third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.h
+++ third_party/blink/renderer/core/layout/ng/grid/layout_ng_grid.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_LAYOUT_NG_GRID_H_
-#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_LAYOUT_NG_GRID_H_
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_GRID_LAYOUT_NG_GRID_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_GRID_LAYOUT_NG_GRID_H_
 
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/layout_block.h"
@@ -29,4 +29,4 @@ class CORE_EXPORT LayoutNGGrid : public LayoutNGMixin<LayoutBlock> {
 
 }  // namespace blink
 
-#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_LAYOUT_NG_GRID_H_
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_NG_GRID_LAYOUT_NG_GRID_H_
