ff373b3df5749eb63208b7516be99f3965317c22
Rushan Suleymanov <rushans@google.com>
2020-05-28 12:48:20 +0000

    [Sync] Extend device info specifics with invalidations token
    This patch changes only Sync protocol. It extends device info specifics
    to support sync invalidations with an invalidations token.
    
    Bug: 1082117
    Change-Id: I861bf0d959932b7270c626f7b3445cf5a40ac543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214887
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772655}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -327,6 +327,7 @@ VISIT_PROTO_FIELDS(const sync_pb::DeviceInfoSpecifics& proto) {
   VISIT(last_updated_timestamp);
   VISIT(feature_fields);
   VISIT(sharing_fields);
+  VISIT(invalidation_fields);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::FeatureSpecificFields& proto) {
@@ -527,6 +528,10 @@ VISIT_PROTO_FIELDS(const sync_pb::HistoryDeleteDirectiveSpecifics& proto) {
 
 VISIT_PROTO_FIELDS(const sync_pb::HistoryDeleteDirectives& proto) {
   VISIT(enabled);
+}
+
+VISIT_PROTO_FIELDS(const sync_pb::InvalidationSpecificFields& proto) {
+  VISIT(instance_id_token);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::KeystoreEncryptionFlags& proto) {
