53030e52d6fbff1c99da8d8a8718165db03d2a53
John Abd-El-Malek <jam@chromium.org>
2020-05-28 07:17:09 +0000

    Mark YouTube safe search header as CORS exempt.
    Bug: 1085309
    Change-Id: Idb58fa6e32de68c4b92760fcc8ca6298be0212f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216853
    Auto-Submit: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772585}

--- chrome/common/google_url_loader_throttle.cc
+++ chrome/common/google_url_loader_throttle.cc
@@ -31,6 +31,8 @@ void GoogleURLLoaderThrottle::UpdateCorsExemptHeader(
     network::mojom::NetworkContextParams* params) {
   params->cors_exempt_header_list.push_back(
       safe_search_util::kGoogleAppsAllowedDomains);
+  params->cors_exempt_header_list.push_back(
+      safe_search_util::kYouTubeRestrictHeaderName);
 #if defined(OS_ANDROID)
   params->cors_exempt_header_list.push_back(kCCTClientDataHeader);
 #endif
@@ -71,7 +73,7 @@ void GoogleURLLoaderThrottle::WillStartRequest(
       dynamic_params_.youtube_restrict <
           safe_search_util::YOUTUBE_RESTRICT_COUNT) {
     safe_search_util::ForceYouTubeRestrict(
-        request->url, &request->headers,
+        request->url, &request->cors_exempt_headers,
         static_cast<safe_search_util::YouTubeRestrictMode>(
             dynamic_params_.youtube_restrict));
   }
@@ -126,7 +128,7 @@ void GoogleURLLoaderThrottle::WillRedirectRequest(
       dynamic_params_.youtube_restrict <
           safe_search_util::YOUTUBE_RESTRICT_COUNT) {
     safe_search_util::ForceYouTubeRestrict(
-        redirect_info->new_url, modified_headers,
+        redirect_info->new_url, modified_cors_exempt_headers,
         static_cast<safe_search_util::YouTubeRestrictMode>(
             dynamic_params_.youtube_restrict));
   }
