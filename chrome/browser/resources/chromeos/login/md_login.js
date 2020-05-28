a3f1ce2a2a91f494515cdd9904a3add91b6d9c5c
Denis Kuznetsov <antrim@google.com>
2020-05-28 14:32:17 +0000

    OOBE: Reorganize code, make it explicit which screens are login/oobe specific.
    Bug: 1084940, 1078294
    Change-Id: I475453adce7fc98d96279434754cb3f09dac50e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218036
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772686}

--- chrome/browser/resources/chromeos/login/md_login.js
+++ chrome/browser/resources/chromeos/login/md_login.js
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="screen_multidevice_setup.js">
 
 // <include src="../../gaia_auth_host/authenticator.js">
-// <include src="notification_card.js">
 
 /**
  * Ensures that the pin keyboard is loaded.
