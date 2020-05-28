7fe5201d71b0be2af445f9934b926130fa7a9fae
Rachel Wong <wrong@chromium.org>
2020-05-28 00:23:08 +0000

    [consent toggle] Refactor privacy view into parent and child classes.
    Currently, privacy_info_view is a launcher notice about Assistant privacy.
    
    This CL separates privacy_info_view into two classes:
    1) privacy_info_view.*, a parent class for privacy notices in the launcher;
    2) assistant/assistant_privacy_info_view.*, which contains all the Assistant-specific methods and strings.
    
    This allows us to create a new privacy notice for Suggested Content, which uses the same parent view but has its own methods and strings.
    
    This CL should not make any functionality changes.
    
    Bug: 1079169
    Change-Id: I28324a9196ef2643e5fdcef122c13a8113ee2603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2198848
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772481}

--- ash/app_list/views/search_result_page_view.cc
+++ ash/app_list/views/search_result_page_view.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/app_list/app_list_util.h"
 #include "ash/app_list/app_list_view_delegate.h"
 #include "ash/app_list/views/app_list_main_view.h"
+#include "ash/app_list/views/assistant/assistant_privacy_info_view.h"
 #include "ash/app_list/views/contents_view.h"
-#include "ash/app_list/views/privacy_info_view.h"
 #include "ash/app_list/views/search_box_view.h"
 #include "ash/app_list/views/search_result_base_view.h"
 #include "ash/app_list/views/search_result_list_view.h"
@@ -176,7 +176,8 @@ SearchResultPageView::SearchResultPageView(AppListViewDelegate* view_delegate,
       views::BoxLayout::Orientation::kVertical, gfx::Insets(), 0));
 
   if (view_delegate_->ShouldShowAssistantPrivacyInfo()) {
-    assistant_privacy_info_view_ = new PrivacyInfoView(view_delegate_, this);
+    assistant_privacy_info_view_ =
+        new AssistantPrivacyInfoView(view_delegate_, this);
     contents_view_->AddChildView(assistant_privacy_info_view_);
   }
 
