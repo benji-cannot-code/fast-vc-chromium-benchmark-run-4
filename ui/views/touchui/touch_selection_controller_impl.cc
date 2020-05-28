00a8644f74ac19f6d4d494c509644b249d915790
Thomas Lukaszewicz <tluk@chromium.org>
2020-05-28 19:03:17 +0000

    Removed unused methods from TouchEditingController
    This is part of a larger cleanup of TouchSelectionControllerImpl in
    effots to remove the use of set_owned_by_client().
    
    Bug: 1044687
    Change-Id: I24a9a4971ed85687875032bc4b7e0e4e267f25fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216538
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772812}

--- ui/views/touchui/touch_selection_controller_impl.cc
+++ ui/views/touchui/touch_selection_controller_impl.cc
@@ -230,7 +230,7 @@ class TouchSelectionControllerImpl::EditingHandleView
     set_owned_by_client();
   }
 
-  ~EditingHandleView() override { SetWidgetVisible(false, false); }
+  ~EditingHandleView() override { SetWidgetVisible(false); }
 
   gfx::SelectionBound::Type selection_bound_type() {
     return selection_bound_.type();
@@ -298,11 +298,9 @@ class TouchSelectionControllerImpl::EditingHandleView
 
   bool IsWidgetVisible() const { return widget_->IsVisible(); }
 
-  void SetWidgetVisible(bool visible, bool quick) {
+  void SetWidgetVisible(bool visible) {
     if (widget_->IsVisible() == visible)
       return;
-    widget_->SetVisibilityAnimationDuration(
-        quick ? base::TimeDelta::FromMilliseconds(50) : base::TimeDelta());
     if (visible)
       widget_->Show();
     else
@@ -498,28 +496,18 @@ void TouchSelectionControllerImpl::SelectionChanged() {
     // Check if there is any selection at all.
     if (screen_bound_anchor.edge_start() == screen_bound_focus.edge_start() &&
         screen_bound_anchor.edge_end() == screen_bound_focus.edge_end()) {
-      selection_handle_1_->SetWidgetVisible(false, false);
-      selection_handle_2_->SetWidgetVisible(false, false);
+      selection_handle_1_->SetWidgetVisible(false);
+      selection_handle_2_->SetWidgetVisible(false);
       SetHandleBound(cursor_handle_.get(), anchor, screen_bound_anchor_clipped);
       return;
     }
 
-    cursor_handle_->SetWidgetVisible(false, false);
+    cursor_handle_->SetWidgetVisible(false);
     SetHandleBound(selection_handle_1_.get(), anchor,
                    screen_bound_anchor_clipped);
     SetHandleBound(selection_handle_2_.get(), focus,
                    screen_bound_focus_clipped);
   }
-}
-
-bool TouchSelectionControllerImpl::IsHandleDragInProgress() {
-  return !!dragging_handle_;
-}
-
-void TouchSelectionControllerImpl::HideHandles(bool quick) {
-  selection_handle_1_->SetWidgetVisible(false, quick);
-  selection_handle_2_->SetWidgetVisible(false, quick);
-  cursor_handle_->SetWidgetVisible(false, quick);
 }
 
 void TouchSelectionControllerImpl::ShowQuickMenuImmediatelyForTesting() {
@@ -576,7 +564,7 @@ void TouchSelectionControllerImpl::SetHandleBound(
     EditingHandleView* handle,
     const gfx::SelectionBound& bound,
     const gfx::SelectionBound& bound_in_screen) {
-  handle->SetWidgetVisible(ShouldShowHandleFor(bound), false);
+  handle->SetWidgetVisible(ShouldShowHandleFor(bound));
   handle->SetBoundInScreen(bound_in_screen, handle->IsWidgetVisible());
 }
 
