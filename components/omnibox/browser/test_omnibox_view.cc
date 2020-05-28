86f1dc8bc7d06c389dc28f2479936ffea52f36ce
manuk <manukh@chromium.org>
2020-05-28 08:49:24 +0000

    [omnibox] [rich-autocompletion] Add multi-sel & comp-text to OmniboxView
    *Rich autocompletion background*
    
    Currently, autocompletion is limited to when the user types a prefix of
    the URL. E.g. 'wiki' can autocomplete 'wiki.org/thecookinglist - top
    awesome recipes'.
    
    Rich autocompletion will allow autocompletion even when the input is
    from the suggestion title or middle of the URL. E.g. 'thecoo' and
    'recip' will also autocomplete 'wiki.org/thecookinglist - top awesome
    recipes'.
    
    Design doc with further details: go/richomniboxautocompletion
    
    Previous CLs:
    - crrev.com/c/2107796 added chrome://flags flags
    - crrev.com/c/2108780 supported multiselection in textfield views.
    - crrev.com/c/2199859 Add complementary omnibox view.
    
    *This CL*
    
    This is the 4th CL for rich autocompletion. It wires in the omnibox view
    to textfield's multiselection & location bar's complementary text.
    
    This CL has no user facing changes. Followup CLs will wire in rich
    autocompletion to make use of these changes.
    
    Bug: 1062446
    Change-Id: I919965049837fd5421fd2b8f0dabc6d3489dabf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204545
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772612}

--- components/omnibox/browser/test_omnibox_view.cc
+++ components/omnibox/browser/test_omnibox_view.cc
@@ -9,6 +9,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/native_widget_types.h"
 
+// static
+OmniboxView::State TestOmniboxView::CreateState(std::string text,
+                                                size_t sel_start,
+                                                size_t sel_end,
+                                                size_t all_sel_length) {
+  OmniboxView::State state;
+  state.text = base::UTF8ToUTF16(text);
+  state.sel_start = sel_start;
+  state.sel_end = sel_end;
+  state.all_sel_length = all_sel_length;
+  return state;
+}
+
 void TestOmniboxView::SetModel(std::unique_ptr<OmniboxEditModel> model) {
   model_ = std::move(model);
 }
@@ -17,10 +30,12 @@ base::string16 TestOmniboxView::GetText() const {
   return text_;
 }
 
-void TestOmniboxView::SetWindowTextAndCaretPos(const base::string16& text,
-                                               size_t caret_pos,
-                                               bool update_popup,
-                                               bool notify_text_changed) {
+void TestOmniboxView::SetWindowTextAndCaretPos(
+    const base::string16& text,
+    size_t caret_pos,
+    bool update_popup,
+    bool notify_text_changed,
+    const base::string16& additional_text) {
   text_ = text;
   selection_ = gfx::Range(caret_pos);
 }
@@ -32,6 +47,10 @@ bool TestOmniboxView::IsSelectAll() const {
 void TestOmniboxView::GetSelectionBounds(size_t* start, size_t* end) const {
   *start = selection_.start();
   *end = selection_.end();
+}
+
+size_t TestOmniboxView::GetAllSelectionsLength() const {
+  return 0;
 }
 
 void TestOmniboxView::SelectAll(bool reversed) {
@@ -52,9 +71,11 @@ void TestOmniboxView::OnTemporaryTextMaybeChanged(
     saved_temporary_selection_ = selection_;
 }
 
-bool TestOmniboxView::OnInlineAutocompleteTextMaybeChanged(
+void TestOmniboxView::OnInlineAutocompleteTextMaybeChanged(
     const base::string16& display_text,
-    size_t user_text_length) {
+    size_t user_text_length,
+    size_t user_text_start,
+    const base::string16& additional_text) {
   const bool text_changed = text_ != display_text;
   text_ = display_text;
   inline_autocomplete_text_ = display_text.substr(user_text_length);
@@ -63,8 +84,6 @@ bool TestOmniboxView::OnInlineAutocompleteTextMaybeChanged(
   // actually changed.
   if (text_changed)
     selection_ = gfx::Range(text_.size(), user_text_length);
-
-  return text_changed;
 }
 
 void TestOmniboxView::OnInlineAutocompleteTextCleared() {
