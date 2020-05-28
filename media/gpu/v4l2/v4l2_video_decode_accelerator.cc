847330cf81cb84a4436e837845c1b2c0430f8227
Alexandre Courbot <acourbot@chromium.org>
2020-05-28 09:55:54 +0000

    media/gpu/v4l2: add QueueDMABuf() method for VideoFrame
    Add a new QueueDMABuf() method taking a VideoFrame as argument. This
    method queues the VideoFrame's DMABufs, but will also keep a reference
    to the VideoFrame itself, which can be retrieved when the buffer is
    dequeued using V4L2ReadableBufferRef::GetVideoFrame(). This makes it
    easier to manage imported DMAbufs with V4L2, since clients don't have to
    keep that reference themselves (usually using a map of buffer IDs to
    VideoFrames).
    
    BUG=b:149663704
    TEST=VDAtest --use_vd passes on Kukui.
    
    Change-Id: Id3fd7c5b62b16978553f3c99664ba688ba61451e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2143427
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#772622}

--- media/gpu/v4l2/v4l2_video_decode_accelerator.cc
+++ media/gpu/v4l2/v4l2_video_decode_accelerator.cc
@@ -1524,8 +1524,7 @@ bool V4L2VideoDecodeAccelerator::EnqueueOutputRecord(
       ret = std::move(buffer).QueueMMap();
       break;
     case V4L2_MEMORY_DMABUF:
-      ret = std::move(buffer).QueueDMABuf(
-          output_record.output_frame->DmabufFds());
+      ret = std::move(buffer).QueueDMABuf(output_record.output_frame);
       break;
     default:
       NOTREACHED();
