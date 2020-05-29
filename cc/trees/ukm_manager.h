f5d5c3936aa0202f0e80c6d93915c88d79c4f353
Mohsen Izadi <mohsen@chromium.org>
2020-05-29 17:30:28 +0000

    Record EventLatency UKM metrics
    The new Graphics.Smoothness.EventLatency event reports the latency of
    each pipeline stage with fields for input event type and scroll event
    input type.
    
    A sampling strategy similar to sampling strategy of existing
    Graphics.Smoothness.Latency event is used.
    
    The UKM collection request has been approved here:
    https://docs.google.com/document/d/1SDxdua997Gm7ihJFVjinHz9qTdBzTJrEzC7GEeQHs-8/edit#
    
    Bug: 1054019
    Change-Id: I2d36ab61210ccf085fe81c8328097e5c84bcd2fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2175324
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Commit-Queue: Mohsen Izadi <mohsen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773242}

--- cc/trees/ukm_manager.h
+++ cc/trees/ukm_manager.h
@@ -6,9 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_TREES_UKM_MANAGER_H_
 #define CC_TREES_UKM_MANAGER_H_
 
+#include <memory>
+#include <vector>
+
 #include "cc/cc_export.h"
 #include "cc/metrics/compositor_frame_reporter.h"
+#include "cc/metrics/event_metrics.h"
 #include "cc/metrics/frame_sequence_metrics.h"
+#include "components/viz/common/frame_timing_details.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "url/gurl.h"
 
@@ -49,10 +54,15 @@ class CC_EXPORT UkmManager {
                            int64_t throughput) const;
   void RecordAggregateThroughput(AggregationType aggregation_type,
                                  int64_t throughput_percent) const;
-  void RecordLatencyUKM(
+  void RecordCompositorLatencyUKM(
       CompositorFrameReporter::FrameReportType report_type,
       const std::vector<CompositorFrameReporter::StageData>& stage_history,
       const CompositorFrameReporter::ActiveTrackers& active_trackers,
+      const viz::FrameTimingDetails& viz_breakdown) const;
+
+  void RecordEventLatencyUKM(
+      const std::vector<EventMetrics>& events_metrics,
+      const std::vector<CompositorFrameReporter::StageData>& stage_history,
       const viz::FrameTimingDetails& viz_breakdown) const;
 
   ukm::UkmRecorder* recorder_for_testing() { return recorder_.get(); }
