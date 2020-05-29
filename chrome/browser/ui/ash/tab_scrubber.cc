b389055982f767195a5c9367d25cdf57aef9729b
Joel Hockey <joelhockey@chromium.org>
2020-05-29 23:41:31 +0000

    Allow TabScrubber for all tabbed browsers
    Now that we support tabs in apps, FEATURE_TABSTRIP is the correct
    way to determine if a browser has tabs.
    
    Bug: 1078907
    Change-Id: Ia977b9df45a63eb08156673db858c053513dfe18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222075
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773417}

--- chrome/browser/ui/ash/tab_scrubber.cc
+++ chrome/browser/ui/ash/tab_scrubber.cc
@@ -196,7 +196,7 @@ void TabScrubber::OnTabRemoved(int index) {
 
 Browser* TabScrubber::GetActiveBrowser() {
   Browser* browser = chrome::FindLastActive();
-  if (!browser || !browser->is_type_normal() ||
+  if (!browser || !browser->SupportsWindowFeature(Browser::FEATURE_TABSTRIP) ||
       !browser->window()->IsActive()) {
     return nullptr;
   }
