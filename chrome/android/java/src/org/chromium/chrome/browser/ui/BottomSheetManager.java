9edbc2cfd8d5a6ca74a248f39bb3674317bbe974
Matt Jones <mdjones@chromium.org>
2020-05-29 20:09:30 +0000

    Tab obscuring no longer requires access to BottomSheet view
    The setIsObscuringAllTabs API on BottomSheetController was originally
    necessary because the TabObscuringHandler needed access to the view
    being obscured. The handler has changed such that is is now token based
    and therefore no longer needs to be an API on the controller.
    
    Bug: 100227
    Change-Id: Iff028b627833ceaa4cdd424c6e5305dde7b4dfd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212512
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773309}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/BottomSheetManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/BottomSheetManager.java
@@ -43,6 +43,9 @@ class BottomSheetManager extends EmptyBottomSheetObserver implements Destroyable
     /** A delegate that provides the functionality of obscuring all tabs. */
     private TabObscuringHandler mTabObscuringHandler;
 
+    /** A token held while the bottom sheet is obscuring all visible tabs. */
+    private int mTabObscuringToken;
+
     /**
      * Used to track whether the active content has a custom scrim lifecycle. This is kept here
      * because there are some instances where the active content is changed prior to the close event
@@ -59,6 +62,7 @@ class BottomSheetManager extends EmptyBottomSheetObserver implements Destroyable
         mDialogManager = dialogManager;
         mSnackbarManager = snackbarManagerSupplier;
         mTabObscuringHandler = obscuringDelegate;
+        mTabObscuringToken = TokenHolder.INVALID_TOKEN;
 
         mSheetController.addObserver(this);
     }
@@ -81,7 +85,7 @@ class BottomSheetManager extends EmptyBottomSheetObserver implements Destroyable
             return;
         }
 
-        mSheetController.setIsObscuringAllTabs(mTabObscuringHandler, true);
+        setIsObscuringAllTabs(true);
 
         assert mAppModalToken == TokenHolder.INVALID_TOKEN;
         assert mTabModalToken == TokenHolder.INVALID_TOKEN;
@@ -102,7 +106,7 @@ class BottomSheetManager extends EmptyBottomSheetObserver implements Destroyable
             return;
         }
 
-        mSheetController.setIsObscuringAllTabs(mTabObscuringHandler, false);
+        setIsObscuringAllTabs(false);
 
         // Tokens can be invalid if the sheet has a custom lifecycle.
         if (mDialogManager.get() != null
@@ -116,6 +120,20 @@ class BottomSheetManager extends EmptyBottomSheetObserver implements Destroyable
         }
         mAppModalToken = TokenHolder.INVALID_TOKEN;
         mTabModalToken = TokenHolder.INVALID_TOKEN;
+    }
+
+    /**
+     * Set whether the bottom sheet is obscuring all tabs.
+     * @param isObscuring Whether the bottom sheet is considered to be obscuring.
+     */
+    private void setIsObscuringAllTabs(boolean isObscuring) {
+        if (isObscuring) {
+            assert mTabObscuringToken == TokenHolder.INVALID_TOKEN;
+            mTabObscuringToken = mTabObscuringHandler.obscureAllTabs();
+        } else {
+            mTabObscuringHandler.unobscureAllTabs(mTabObscuringToken);
+            mTabObscuringToken = TokenHolder.INVALID_TOKEN;
+        }
     }
 
     @Override
