d25a235e8e643fade5646a8d40fff8d83ed7ccdb
Jing Wang <jiwan@chromium.org>
2020-05-29 04:51:34 +0000

    Add a flag for IME options page in ChromeOS settings.
    Implemented a UI change behind the flag:
    UI with flag disabled: https://screenshot.googleplex.com/osdAbRsrOdw
    UI with flag enabled: https://screenshot.googleplex.com/k9waaRkjG4x
    
    Test: tested with Chrome on Linux
    Bug: 1086786
    Change-Id: Id3b35306289715e8761e874a6b0fcdfed12205ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216139
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773047}

--- chrome/browser/resources/settings/chromeos/os_languages_page/os_languages_page.js
+++ chrome/browser/resources/settings/chromeos/os_languages_page/os_languages_page.js
@@ -67,6 +67,14 @@ cr.define('settings', function() {
           return loadTimeData.getBoolean('isGuest');
         },
       },
+
+      /** @private */
+      imeOptionsInSettings_: {
+        type: Boolean,
+        value() {
+          return loadTimeData.getBoolean('imeOptionsInSettings');
+        },
+      }
     },
 
     /** @private {boolean} */
