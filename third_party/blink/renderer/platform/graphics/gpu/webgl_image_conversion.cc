ec9a435a8457d4180021cb61615e7aee81b23e6f
Kenneth Russell <kbr@chromium.org>
2020-05-29 18:03:07 +0000

    Check for presence of Skia image when determining colorspace conversion.
    Regression test being added to WebGL conformance suite in
    https://github.com/KhronosGroup/WebGL/pull/3087 .
    
    Bug: 1085044
    Change-Id: I2bf2f58a61e42dc360fe6e9c2fb54a4ec896dc5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220812
    Reviewed-by: Shrek Shao <shrekshao@google.com>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773254}

--- third_party/blink/renderer/platform/graphics/gpu/webgl_image_conversion.cc
+++ third_party/blink/renderer/platform/graphics/gpu/webgl_image_conversion.cc
@@ -2816,7 +2816,7 @@ void WebGLImageConversion::ImageExtractor::ExtractImage(
   bool has_alpha = skia_image ? !skia_image->isOpaque() : true;
 
   bool need_unpremultiplied = has_alpha && !premultiply_alpha;
-  bool need_color_conversion = !ignore_color_space &&
+  bool need_color_conversion = !ignore_color_space && skia_image &&
                                skia_image->colorSpace() &&
                                !skia_image->colorSpace()->isSRGB();
   if ((!skia_image || ignore_color_space || need_unpremultiplied ||
