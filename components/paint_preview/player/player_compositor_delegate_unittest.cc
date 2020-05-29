ad35372390d2b762cc9fea2ef69e524630abbd8c
ckitagawa <ckitagawa@chromium.org>
2020-05-29 16:08:21 +0000

    [Paint Preview] Disable PlayerCompositorDelegate.OnClick
    Flaking due to a bug. I know the root cause, but will address in a
    separate CL.
    
    TBR=mahmoudi@chromium.org
    
    Bug: 1087768
    Change-Id: If25f17e5c9e4a6c0e2fdfaa4d1d199f4f78434e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222543
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773202}

--- components/paint_preview/player/player_compositor_delegate_unittest.cc
+++ components/paint_preview/player/player_compositor_delegate_unittest.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace paint_preview {
 
-TEST(PlayerCompositorDelegate, OnClick) {
+TEST(PlayerCompositorDelegate, DISABLED_OnClick) {
   base::test::TaskEnvironment env;
   base::ScopedTempDir temp_dir;
   ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
