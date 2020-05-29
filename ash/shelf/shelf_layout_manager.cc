48f81accfc42c786919eb9526f26c1fd8235351b
Yulun Wu <yulunwu@chromium.org>
2020-05-29 23:50:50 +0000

    Shelf layout manager should not explicitly handle shelf dimming.
    The shelf auto dim handler should observe the shelf's auto hide state
    to handle opacity adjustments and should not be done explicitly by
    ShelfLayoutManger::SetState().
    
    Bug: 1086632
    Change-Id: I719b08243ed560f7aa6fd886e639ad6904e7ab90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216698
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773424}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -1314,10 +1314,6 @@ void ShelfLayoutManager::SetState(ShelfVisibilityState visibility_state) {
       observer.OnHotseatStateChanged(previous_hotseat_state, hotseat_state());
   }
 
-  // Shelf should be undimmed when it transitions into a visible state.
-  if (state_.IsShelfVisible())
-    SetDimmed(false);
-
   UpdateContextualNudges();
 }
 
