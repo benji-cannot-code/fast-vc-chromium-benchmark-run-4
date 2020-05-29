0c45d4f48392d645752946d2578965c49d54c477
Sunny Sachanandani <sunnyps@chromium.org>
2020-05-29 03:44:33 +0000

    Use DWM API to get vsync interval for the GPU vsync BFS on Windows
    VSyncProviderWin through DWM provides a more accurate vsync interval
    than getting the refresh rate from the Win32 GetMonitorInfo() API.
    However, the vsync phase from DWM via VSyncProviderWin is noisy
    especially when any kind of mouse movement occurs, so the logic for
    vsync time is not changed.
    
    Bug: 1085548
    Change-Id: If74d927c1e19864cf245b3bda82327a8219a5e9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2217065
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773032}

--- ui/gl/vsync_thread_win.h
+++ ui/gl/vsync_thread_win.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/threading/thread.h"
 #include "ui/gl/gl_export.h"
+#include "ui/gl/vsync_provider_win.h"
 
 namespace base {
 template <typename T>
@@ -47,6 +48,7 @@ class GL_EXPORT VSyncThreadWin {
   base::Thread vsync_thread_;
 
   // Used on vsync thread only after initialization.
+  VSyncProviderWin vsync_provider_;
   const Microsoft::WRL::ComPtr<ID3D11Device> d3d11_device_;
   HMONITOR primary_monitor_ = nullptr;
   Microsoft::WRL::ComPtr<IDXGIOutput> primary_output_;
