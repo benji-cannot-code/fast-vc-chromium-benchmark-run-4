16fc47e6b0e54649ab77989a4a167977d8f943bf
Arnaud Mandy <arnaud.mandy@intel.com>
2020-05-29 21:14:21 +0000

    enable wpt tests with mock screen enumeration
    This CL enables possibility to isolate blink implementation of
    screen enumeration from the rest of the stack.
    
    It provides the possibility to create mock displays at test conveniences
    by rerouting the GetDisplay call to the JS implementation in the test.
    
    Existing test has been modified to use this test framework.
    
    Bug: 1075656
    Change-Id: I1a38426deaccff14036f00806ef7dfc878bcd4ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214538
    Commit-Queue: Arnaud Mandy <arnaud.mandy@intel.com>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773339}

---
+++ third_party/blink/web_tests/external/wpt/screen_enumeration/screen_enumeration_permission.window.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+"use strict";
+
+promise_test(async t => {
+  await test_driver.set_permission({ name: "window-placement" }, "denied", false);
+
+  const status = await navigator.permissions.query({ name:"window-placement" });
+  assert_class_string(status, "PermissionStatus");
+  assert_equals(status.state, "denied");
+}, "Deny window placement permission should work.");
+
+promise_test(async t => {
+  await test_driver.set_permission({ name: "window-placement" }, "granted", false);
+
+  const status = await navigator.permissions.query({ name: "window-placement" });
+  assert_class_string(status, "PermissionStatus");
+  assert_equals(status.state, "granted");
+}, "Grant window placement permission should work.");
