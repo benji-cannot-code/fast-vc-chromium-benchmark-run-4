13947d1adc9953f6456433883a105b6135f02332
Mike Klein <mtklein@google.com>
2020-05-29 13:42:28 +0000

    fix name of Display P3 gamut
    The gamut named "SkNamedGamut::kDCIP3" is really the slightly different
    Display P3 gamut.  This is a no-op refactor; the matrices are bitwise
    identical in Skia.
    
    Bug: skia:9792
    Change-Id: I4405206337b919453d9cb58a09d9331f54591305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216159
    Reviewed-by: ccameron <ccameron@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Mike Klein <mtklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#773153}

--- third_party/blink/renderer/platform/graphics/canvas_color_params.cc
+++ third_party/blink/renderer/platform/graphics/canvas_color_params.cc
@@ -128,7 +128,7 @@ sk_sp<SkColorSpace> CanvasColorParams::GetSkColorSpace() const {
       transferFn = SkNamedTransferFn::kLinear;
       break;
     case CanvasColorSpace::kP3:
-      gamut = SkNamedGamut::kDCIP3;
+      gamut = SkNamedGamut::kDisplayP3;
       transferFn = SkNamedTransferFn::kLinear;
       break;
   }
@@ -218,7 +218,7 @@ CanvasColorParams::CanvasColorParams(const sk_sp<SkColorSpace> color_space,
   } else if (SkColorSpace::Equals(
                  color_space.get(),
                  SkColorSpace::MakeRGB(SkNamedTransferFn::kLinear,
-                                       SkNamedGamut::kDCIP3)
+                                       SkNamedGamut::kDisplayP3)
                      .get())) {
     color_space_ = CanvasColorSpace::kP3;
   }
--- ui/gfx/color_space.cc
+++ ui/gfx/color_space.cc
@@ -631,7 +631,7 @@ sk_sp<SkColorSpace> ColorSpace::ToSkColorSpace() const {
       gamut = SkNamedGamut::kAdobeRGB;
       break;
     case PrimaryID::SMPTEST432_1:
-      gamut = SkNamedGamut::kDCIP3;
+      gamut = SkNamedGamut::kDisplayP3;
       break;
     case PrimaryID::BT2020:
       gamut = SkNamedGamut::kRec2020;
