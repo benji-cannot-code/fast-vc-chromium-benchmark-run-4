02d72cbf717796c86a01a7f5841d061a3f45313f
Thomas Guilbert <tguilbert@chromium.org>
2020-05-29 19:28:08 +0000

    Mark PauseOnHeadsetUnplugTest as flaky
    This test has been flaking consistently on android-pie-arm64-rel and a
    few other bots.
    
    TBR=mlamouri
    
    Bug: 1087794
    Change-Id: I2655b11bf7b5580d497df0958e3ea9a8564cb9c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222184
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773293}

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/PauseOnHeadsetUnplugTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/PauseOnHeadsetUnplugTest.java
@@ -18,6 +18,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
@@ -52,6 +53,7 @@ public class PauseOnHeadsetUnplugTest {
 
     @Test
     @SmallTest
+    @FlakyTest
     @RetryOnFailure
     public void testPause() throws IllegalArgumentException, TimeoutException {
         Tab tab = mActivityTestRule.getActivity().getActivityTab();
