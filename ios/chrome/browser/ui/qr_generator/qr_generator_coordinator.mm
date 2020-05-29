ed9567e6be046c96a8cc2eb1268ac6236d1bad7b
Sebastien Lalancette <seblalancette@chromium.org>
2020-05-29 15:43:48 +0000

    [iOS] Add "Learn More" Button to QR Code View
    UX-requested functionality which allows users to learn more about what
    they can do with a QR code. When tapping on the "Learn More" button, a
    popover is shown with the help text.
    
    Bug: 1064990
    Change-Id: I1cc65f29607089e95c766c518fe0af526061aeb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218705
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773194}

--- ios/chrome/browser/ui/qr_generator/qr_generator_coordinator.mm
+++ ios/chrome/browser/ui/qr_generator/qr_generator_coordinator.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/qr_generation_commands.h"
 #import "ios/chrome/browser/ui/qr_generator/qr_generator_view_controller.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
+#import "ios/chrome/common/ui/elements/popover_label_view_controller.h"
 #include "ios/chrome/grit/ios_strings.h"
 #import "net/base/mac/url_conversions.h"
 #include "ui/base/l10n/l10n_util_mac.h"
@@ -46,6 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Title of a page to generate a QR code for.
 @property(nonatomic, copy) NSString* title;
 
+// Popover used to show learn more info, not nil when presented.
+@property(nonatomic, strong)
+    PopoverLabelViewController* learnMoreViewController;
+
 @end
 
 @implementation QRGeneratorCoordinator
@@ -74,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.viewController setPageURL:net::NSURLWithGURL(_URL)];
   [self.viewController setTitleString:self.title];
   [self.viewController setActionHandler:self];
+  [self.viewController setHelpButtonAvailable:YES];
 
   [self.baseViewController presentViewController:self.viewController
                                         animated:YES
@@ -84,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)stop {
   [self.viewController dismissViewControllerAnimated:YES completion:nil];
   self.viewController = nil;
+  self.learnMoreViewController = nil;
 
   [self.activityServiceCoordinator stop];
   self.activityServiceCoordinator = nil;
@@ -116,8 +123,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)confirmationAlertLearnMoreAction {
-  // No-op.
-  // TODO crbug.com/1064990: Add learn more behavior.
+  NSString* message =
+      l10n_util::GetNSString(IDS_IOS_QR_CODE_LEARN_MORE_MESSAGE);
+  self.learnMoreViewController =
+      [[PopoverLabelViewController alloc] initWithMessage:message];
+
+  self.learnMoreViewController.popoverPresentationController.barButtonItem =
+      self.viewController.helpButton;
+  self.learnMoreViewController.popoverPresentationController
+      .permittedArrowDirections = UIPopoverArrowDirectionUp;
+
+  [self.viewController presentViewController:self.learnMoreViewController
+                                    animated:YES
+                                  completion:nil];
 }
 
 #pragma mark - ActivityServicePositioner
