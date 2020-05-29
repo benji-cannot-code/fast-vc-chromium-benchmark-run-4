5ef1a2478526ca4e287d2cecfe519910fd55c234
Omid Tourzan <oto@chromium.org>
2020-05-29 06:45:14 +0000

    [file-operations-handler] Fix ES6 lint nits: replace var with let/const
    Bug: 953308
    Change-Id: I13b337fe205e897e0350a007d690a24ce582787f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220409
    Reviewed-by: Noel Gordon <noel@chromium.org>
    Commit-Queue: Noel Gordon <noel@chromium.org>
    Commit-Queue: Omid Tourzan <oto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773065}

--- ui/file_manager/file_manager/background/js/file_operation_handler.js
+++ ui/file_manager/file_manager/background/js/file_operation_handler.js
@@ -217,7 +217,7 @@ class FileOperationHandler {
     if (event.reason === fileOperationUtil.EventRouter.EventType.ERROR) {
       switch (event.error.code) {
         case util.FileOperationErrorType.TARGET_EXISTS:
-          var name = event.error.data.name;
+          let name = event.error.data.name;
           if (event.error.data.isDirectory) {
             name += '/';
           }
@@ -258,7 +258,7 @@ class FileOperationHandler {
           }
       }
     } else if (event.status.numRemainingItems === 1) {
-      var name = event.status.processingEntryName;
+      const name = event.status.processingEntryName;
       switch (event.status.operationType) {
         case 'COPY':
           return strf('COPY_FILE_NAME', name);
