ab77359f199d61f72627712a98aaf68afec6fc90
Bo Liu <boliu@chromium.org>
2020-05-29 22:31:28 +0000

    weblayer: Implement surfaceRedrawNeededAsync
    Implement surfaceRedrawNeededAsync for SurfaceView. This is meant to
    ensure that after SurfaceView is resized, a partially drawn surface is
    not shown to the user. The implementation is roughly copied from
    CompositorView in chrome.
    
    Bug: 1084500
    Change-Id: Ic8272f680488222f25c74fda8c82ece144e1c8d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222142
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773381}

--- weblayer/browser/java/org/chromium/weblayer_private/ContentViewRenderView.java
+++ weblayer/browser/java/org/chromium/weblayer_private/ContentViewRenderView.java
@@ -87,6 +87,7 @@ public class ContentViewRenderView extends RelativeLayout {
                 int width, int height);
         // |cacheBackBuffer| will delay destroying the EGLSurface until after the next swap.
         void surfaceDestroyed(boolean cacheBackBuffer);
+        void surfaceRedrawNeededAsync(Runnable drawingFinished);
     }
 
     private final ArrayList<TrackedRunnable> mPendingRunnables = new ArrayList<>();
@@ -161,6 +162,11 @@ public class ContentViewRenderView extends RelativeLayout {
                     mNativeContentViewRenderView, cacheBackBuffer);
             mCompositorHasSurface = false;
         }
+
+        @Override
+        public void surfaceRedrawNeededAsync(Runnable drawingFinished) {
+            assert false; // NOTREACHED.
+        }
     }
 
     // Abstract differences between SurfaceView and TextureView behind this class.
@@ -221,6 +227,7 @@ public class ContentViewRenderView extends RelativeLayout {
         private final TextureViewSurfaceTextureListener mSurfaceTextureListener;
 
         private final ArrayList<ValueCallback<Boolean>> mModeCallbacks = new ArrayList<>();
+        private ArrayList<Runnable> mSurfaceRedrawNeededCallbacks;
 
         public SurfaceData(@Mode int mode, FrameLayout parent, SurfaceEventListener listener,
                 int backgroundColor, Runnable evict) {
@@ -303,6 +310,7 @@ public class ContentViewRenderView extends RelativeLayout {
                 mListener.surfaceDestroyed(mCachedSurfaceNeedsEviction);
                 mNeedsOnSurfaceDestroyed = false;
             }
+            runSurfaceRedrawNeededCallbacks();
 
             if (mMode == MODE_SURFACE_VIEW) {
                 mSurfaceView.getHolder().removeCallback(mSurfaceCallback);
@@ -404,6 +412,15 @@ public class ContentViewRenderView extends RelativeLayout {
             return false;
         }
 
+        public void runSurfaceRedrawNeededCallbacks() {
+            ArrayList<Runnable> callbacks = mSurfaceRedrawNeededCallbacks;
+            mSurfaceRedrawNeededCallbacks = null;
+            if (callbacks == null) return;
+            for (Runnable r : callbacks) {
+                r.run();
+            }
+        }
+
         private void destroyPreviousData() {
             if (mPrevSurfaceDataNeedsDestroy != null) {
                 mPrevSurfaceDataNeedsDestroy.destroy();
@@ -446,6 +463,22 @@ public class ContentViewRenderView extends RelativeLayout {
             assert mNeedsOnSurfaceDestroyed;
             mListener.surfaceDestroyed(cacheBackBuffer);
             mNeedsOnSurfaceDestroyed = false;
+            runSurfaceRedrawNeededCallbacks();
+        }
+
+        @Override
+        public void surfaceRedrawNeededAsync(Runnable drawingFinished) {
+            if (mMarkedForDestroy) {
+                drawingFinished.run();
+                return;
+            }
+            assert mNativeContentViewRenderView != 0;
+            assert this == ContentViewRenderView.this.mCurrent;
+            if (mSurfaceRedrawNeededCallbacks == null) {
+                mSurfaceRedrawNeededCallbacks = new ArrayList<>();
+            }
+            mSurfaceRedrawNeededCallbacks.add(drawingFinished);
+            ContentViewRenderViewJni.get().setNeedsRedraw(mNativeContentViewRenderView);
         }
 
         private void runCallbacks() {
@@ -471,7 +504,7 @@ public class ContentViewRenderView extends RelativeLayout {
     }
 
     // Adapter for SurfaceHoolder.Callback.
-    private static class SurfaceHolderCallback implements SurfaceHolder.Callback {
+    private static class SurfaceHolderCallback implements SurfaceHolder.Callback2 {
         private final SurfaceEventListener mListener;
 
         public SurfaceHolderCallback(SurfaceEventListener listener) {
@@ -491,6 +524,16 @@ public class ContentViewRenderView extends RelativeLayout {
         @Override
         public void surfaceDestroyed(SurfaceHolder holder) {
             mListener.surfaceDestroyed(false /* cacheBackBuffer */);
+        }
+
+        @Override
+        public void surfaceRedrawNeeded(SurfaceHolder holder) {
+            // Intentionally not implemented.
+        }
+
+        @Override
+        public void surfaceRedrawNeededAsync(SurfaceHolder holder, Runnable drawingFinished) {
+            mListener.surfaceRedrawNeededAsync(drawingFinished);
         }
     }
 
@@ -720,6 +763,13 @@ public class ContentViewRenderView extends RelativeLayout {
         return mCurrent.didSwapFrame();
     }
 
+    @CalledByNative
+    private void didSwapBuffers(boolean sizeMatches) {
+        assert mCurrent != null;
+        if (!sizeMatches) return;
+        mCurrent.runSurfaceRedrawNeededCallbacks();
+    }
+
     private void evictCachedSurface() {
         if (mNativeContentViewRenderView == 0) return;
         ContentViewRenderViewJni.get().evictCachedSurface(mNativeContentViewRenderView);
@@ -753,6 +803,7 @@ public class ContentViewRenderView extends RelativeLayout {
         void surfaceDestroyed(long nativeContentViewRenderView, boolean cacheBackBuffer);
         void surfaceChanged(long nativeContentViewRenderView, boolean canBeUsedWithSurfaceControl,
                 int format, int width, int height, Surface surface);
+        void setNeedsRedraw(long nativeContentViewRenderView);
         void evictCachedSurface(long nativeContentViewRenderView);
         ResourceManager getResourceManager(long nativeContentViewRenderView);
     }
