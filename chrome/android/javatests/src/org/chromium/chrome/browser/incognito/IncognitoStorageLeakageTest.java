19b87e3312edf333cc2b49a41e7814e95161436a
Thomas Guilbert <tguilbert@chromium.org>
2020-05-29 21:51:02 +0000

    [Sheriff] Retry IncognitoStorageLeakageTest
    Add @RetryOnFailure to
    IncognitoStorageLeakageTest#testStorageDoesNotLeakFromActivityToActivity
    
    Although there are talk about removing the RetyOnFailure flag (see
    crbug.com/797002) for now this should be slightly less expensive than
    re-running the entire test suite.
    
    TBR: tedchoch@chromium.org
    Bug: 1087796
    Change-Id: I2f76509aa7f5aecf9f7554cc3d825149055b428d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222727
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773362}

--- chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoStorageLeakageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoStorageLeakageTest.java
@@ -22,6 +22,7 @@ import org.chromium.base.test.params.ParameterAnnotations.UseMethodParameter;
 import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.customtabs.CustomTabActivityTestRule;
 import org.chromium.chrome.browser.customtabs.CustomTabIncognitoManager;
@@ -121,6 +122,7 @@ public class IncognitoStorageLeakageTest {
 
     @Test
     @LargeTest
+    @RetryOnFailure
     @UseMethodParameter(TestParams.AllTypesToAllTypes.class)
     public void testStorageDoesNotLeakFromActivityToActivity(
             String activityType1, String activityType2) throws TimeoutException {
