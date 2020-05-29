442d3eab6d449137fee209a873bfeedff043e47c
danakj <danakj@chromium.org>
2020-05-29 22:36:17 +0000

    Fix http/tests/loading/bad-server-subframe.html for site isolation.
    Provisional frames don't have a name, so the logging is different, but
    we can print the name of the frame they will be replacing. That name is
    assigned at swap in, but the tests can get to it before-hand.
    
    Avoid sending a DidFailProvisionalLoad() notification during unload
    of a frame being swapped out. We were sending this unconditionally on
    unload even if the navigation succeeded, because the frame being
    unloaded has no idea. The navigation is happening in the provisional
    frame. This makes the RenderFrameObserver expectations match with site
    isolation and without.
    
    R=dgozman@chromium.org
    
    Bug: 765779
    Change-Id: I2c160091e06a890470746356b6fcfed3cc977948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219088
    Auto-Submit: danakj <danakj@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773384}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -489,7 +489,11 @@ void LocalFrame::DetachImpl(FrameDetachType type) {
   inspector_task_runner_->Dispose();
 
   PluginScriptForbiddenScope forbid_plugin_destructor_scripting;
-  loader_.StopAllLoaders();
+  // In a kSwap detach, if we have a navigation going, its moved to the frame
+  // being swapped in, so we don't need to notify the client about the
+  // navigation stopping here. That will be up to the provisional frame being
+  // swapped in, which knows the actual state of the navigation.
+  loader_.StopAllLoaders(/*abort_client=*/type == FrameDetachType::kRemove);
   // Don't allow any new child frames to load in this frame: attaching a new
   // child frame during or after detaching children results in an attached
   // frame on a detached DOM tree, which is bad.
@@ -1886,7 +1890,7 @@ void LocalFrame::ForciblyPurgeV8Memory() {
 
   WindowProxyManager* window_proxy_manager = GetWindowProxyManager();
   window_proxy_manager->ClearForV8MemoryPurge();
-  Loader().StopAllLoaders();
+  Loader().StopAllLoaders(/*abort_client=*/true);
 }
 
 void LocalFrame::OnPageLifecycleStateUpdated() {
@@ -2321,7 +2325,7 @@ void LocalFrame::AddInspectorIssue(mojom::blink::InspectorIssueInfoPtr info) {
 }
 
 void LocalFrame::StopLoading() {
-  Loader().StopAllLoaders();
+  Loader().StopAllLoaders(/*abort_client=*/true);
 
   // The stopLoading handler may run script, which may cause this frame to be
   // detached/deleted. If that happens, return immediately.
