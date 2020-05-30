eb4d1d1c66f6836cd15c7e4e37a60d822863eb46
dpapad <dpapad@chromium.org>
2020-05-30 00:46:28 +0000

    WebUI: Add JS type checking for more cr_elements/ tests, part 5.
    Specifically:
      - cr_policy_indicator_behavior_tests.js
      - cr_policy_indicator_tests.js
      - cr_policy_pref_indicator_tests.js
    
    Bug: 1000989
    Change-Id: Iac363bccb89ca2508727ed8334b194dc867c60fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220280
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773445}

--- chrome/test/data/webui/cr_elements/cr_policy_indicator_behavior_tests.js
+++ chrome/test/data/webui/cr_elements/cr_policy_indicator_behavior_tests.js
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {CrPolicyIndicatorBehavior, CrPolicyIndicatorType} from 'chrome://resources/cr_elements/policy/cr_policy_indicator_behavior.m.js';
 // #import {Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 // #import {isChromeOS} from 'chrome://resources/js/cr.m.js';
-// #import 'chrome://test/cr_elements/cr_policy_strings.js';
+// #import './cr_policy_strings.js';
+// #import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
 // clang-format on
 
 suite('CrPolicyIndicatorBehavior', function() {
@@ -21,10 +22,13 @@ suite('CrPolicyIndicatorBehavior', function() {
     });
   });
 
+  /** @type {!TestIndicatorElement} */
   let indicator;
+
   setup(function() {
-    PolymerTest.clearBody();
-    indicator = document.createElement('test-indicator');
+    document.body.innerHTML = '';
+    indicator = /** @type {!TestIndicatorElement} */ (
+        document.createElement('test-indicator'));
     document.body.appendChild(indicator);
   });
 
--- chrome/test/data/webui/cr_elements/cr_policy_indicator_tests.js
+++ chrome/test/data/webui/cr_elements/cr_policy_indicator_tests.js
@@ -7,27 +7,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // #import {CrPolicyIndicatorType} from 'chrome://resources/cr_elements/policy/cr_policy_indicator_behavior.m.js';
 // #import 'chrome://resources/cr_elements/policy/cr_policy_indicator.m.js';
 // #import {isChromeOS} from 'chrome://resources/js/cr.m.js';
-// #import 'chrome://test/cr_elements/cr_policy_strings.js';
+// #import './cr_policy_strings.js';
+// #import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
 // clang-format on
 
 /** @fileoverview Suite of tests for cr-policy-indicator. */
 suite('CrPolicyIndicator', function() {
-  /** @type {!CrPolicyIndicatorElement|undefined} */
+  /** @type {!CrPolicyIndicatorElement} */
   let indicator;
 
-  /** @type {!CrTooltipIconElement|undefined} */
+  /** @type {!CrTooltipIconElement} */
   let icon;
 
   setup(function() {
-    PolymerTest.clearBody();
+    document.body.innerHTML = '';
 
-    indicator = document.createElement('cr-policy-indicator');
+    indicator = /** @type {!CrPolicyIndicatorElement} */ (
+        document.createElement('cr-policy-indicator'));
     document.body.appendChild(indicator);
-    icon = indicator.$$('cr-tooltip-icon');
-  });
-
-  teardown(function() {
-    PolymerTest.clearBody();  // crbug.com/680169
+    icon =
+        /** @type {!CrTooltipIconElement} */ (indicator.$$('cr-tooltip-icon'));
   });
 
   test('none', function() {
