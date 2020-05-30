db06742975cad149f6d2b5026d5f050651aca069
Daniel Hosseinian <dhoss@chromium.org>
2020-05-30 05:57:09 +0000

    Honor JavaScript content settings in the PDF extension
    Use a feature flag to allow for runtime switching between the existing
    and new behaviors.
    
    When |chrome_pdf::features::kPdfHonorJsContentSettings| is disabled,
    JavaScript will be allowed to execute regardless of the content
    settings.
    
    Bug: 696650
    Change-Id: Ifc45058e8dffd579952added95ac0c4630cbf350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202973
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773491}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -911,10 +911,7 @@ class PDFExtensionContentSettingJSTest
   }
 
   std::string GetDisabledJsTestFile() const {
-    // TODO(crbug.com/696650): Run nobeep_test.js when
-    // |honor_js_content_settings_| once JS content settings get respected in
-    // the PDF extension.
-    return "beep_test.js";
+    return ShouldHonorJsContentSettings() ? "nobeep_test.js" : "beep_test.js";
   }
 
  private:
