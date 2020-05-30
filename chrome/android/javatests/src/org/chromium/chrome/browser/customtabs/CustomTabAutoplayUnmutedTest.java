63efb42cdd84d7cce85fc02e999e53797e3fd859
Peter Kotwicz <pkotwicz@chromium.org>
2020-05-30 03:11:31 +0000

    Try fixing testAutoplayUnmuted flakiness
    This CL changes WebappActivityTestRule#startWebappActivity() to wait till the page is loaded (as opposed to the tab not loading
    which also occurs prior to the tab starting loading)
    
    BUG=1081847
    
    Change-Id: Ic49b39e49aa83428c05d9d5570076e2ec7f6bbc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209614
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773471}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabAutoplayUnmutedTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabAutoplayUnmutedTest.java
@@ -18,7 +18,6 @@ import org.chromium.base.test.params.ParameterAnnotations.UseRunnerDelegate;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.flags.ActivityType;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeActivityTestRule;
@@ -57,7 +56,6 @@ public final class CustomTabAutoplayUnmutedTest {
      */
     @Test
     @LargeTest
-    @DisabledTest(message = "crbug.com/1081847")
     public void testAutoplayUnmuted() throws Exception {
         String testPageUrl = mActivityTestRule.getTestServer().getURL(
                 "/chrome/test/data/android/media/video-autoplay.html");
