d4db83c646f0105fbbe9d83740e589142393968d
Domenic Denicola <domenic@chromium.org>
2020-06-01 17:16:08 +0000

    Origin isolation: parse the header
    Bug: 1066930
    Change-Id: Ib1c79f8c9218821c7da3640e012cf042666e6d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222692
    Commit-Queue: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773706}

---
+++ services/network/public/cpp/origin_isolation_parser.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_PUBLIC_CPP_ORIGIN_ISOLATION_PARSER_H_
+#define SERVICES_NETWORK_PUBLIC_CPP_ORIGIN_ISOLATION_PARSER_H_
+
+#include <string>
+#include "base/component_export.h"
+
+namespace network {
+
+// Parsing is done following the Origin-Isolation spec draft:
+// https://github.com/whatwg/html/pull/5545
+//
+// See the comment in network::PopulateParsedHeaders for restrictions on this
+// function.
+COMPONENT_EXPORT(NETWORK_CPP)
+bool ParseOriginIsolation(const std::string&);
+
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_PUBLIC_CPP_ORIGIN_ISOLATION_PARSER_H_
--- services/network/public/cpp/parsed_headers.cc
+++ services/network/public/cpp/parsed_headers.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/cross_origin_embedder_policy_parser.h"
 #include "services/network/public/cpp/cross_origin_opener_policy_parser.h"
 #include "services/network/public/cpp/features.h"
+#include "services/network/public/cpp/origin_isolation_parser.h"
 
 namespace network {
 
@@ -30,6 +31,10 @@ mojom::ParsedHeadersPtr PopulateParsedHeaders(
       ParseCrossOriginEmbedderPolicy(*headers);
   parsed_headers->cross_origin_opener_policy =
       ParseCrossOriginOpenerPolicy(*headers);
+
+  std::string origin_isolation;
+  if (headers->GetNormalizedHeader("Origin-Isolation", &origin_isolation))
+    parsed_headers->origin_isolation = ParseOriginIsolation(origin_isolation);
 
   std::string accept_ch;
   if (headers->GetNormalizedHeader("Accept-CH", &accept_ch))
