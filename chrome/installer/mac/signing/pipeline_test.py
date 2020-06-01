9f8a6318819c1733aac820184a9281161acaf000
Robert Sesek <rsesek@chromium.org>
2020-06-01 17:43:17 +0000

    mac: Staple notarized XPC services.
    Older versions of the stapler did not support XPC services, but the ones
    that are part of Xcode 11 do support it. This reverts
    4312c1f9a6954f2520bbd0afdc94dd272e7e2997.
    
    Bug: 979725
    Change-Id: I0f9cd92e56119ca9ff33ce17896f20c9fe1e2f7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224821
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773718}

--- chrome/installer/mac/signing/pipeline_test.py
+++ chrome/installer/mac/signing/pipeline_test.py
@@ -239,6 +239,9 @@ class TestPipelineHelpers(unittest.TestCase):
 
         self.assertEqual(staple.mock_calls, [
             mock.call(
+                '$W/App Product.app/Contents/Frameworks/Product Framework.framework/XPCServices/AlertNotificationService.xpc'
+            ),
+            mock.call(
                 '$W/App Product.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper.app'
             ),
             mock.call(
@@ -264,6 +267,9 @@ class TestPipelineHelpers(unittest.TestCase):
         pipeline._staple_chrome(paths, dist.to_config(test_config.TestConfig()))
 
         self.assertEqual(staple.mock_calls, [
+            mock.call(
+                '$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/XPCServices/AlertNotificationService.xpc'
+            ),
             mock.call(
                 '$W/App Product Canary.app/Contents/Frameworks/Product Framework.framework/Helpers/Product Helper.app'
             ),
