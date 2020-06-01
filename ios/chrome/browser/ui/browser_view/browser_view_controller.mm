efe82d4fb8ba07e8e25181d0fcf316877e50daf3
Chris Lu <thegreenfrog@chromium.org>
2020-06-01 19:43:35 +0000

    [ios] Do not update fullscreen viewport insets when flag is on
    When FullscreenController is scoped to the Browser, fullscreen
    viewport inset updates are handled downstream, so BVC no longer
    has to respond to FullscreenUIElement callbacks by updating
    the viewport insets.
    
    Bug: 790886, 1090044
    Change-Id: I52e08694b38d36ae6137b641b2b9be07a7bd4191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222292
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773782}

--- ios/chrome/browser/ui/browser_view/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view/browser_view_controller.mm
@@ -3502,7 +3502,9 @@ NSString* const kBrowserViewControllerSnackbarCategory =
 - (void)updateForFullscreenProgress:(CGFloat)progress {
   [self updateHeadersForFullscreenProgress:progress];
   [self updateFootersForFullscreenProgress:progress];
-  [self updateBrowserViewportForFullscreenProgress:progress];
+  if (!fullscreen::features::ShouldScopeFullscreenControllerToBrowser()) {
+    [self updateBrowserViewportForFullscreenProgress:progress];
+  }
 }
 
 - (void)updateForFullscreenEnabled:(BOOL)enabled {
