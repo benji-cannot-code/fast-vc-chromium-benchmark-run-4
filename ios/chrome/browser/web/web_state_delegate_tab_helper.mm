a069d4c05bdc4ef93dcc2115dfe399700634c0ab
Kurt Horimoto <kkhorimoto@chromium.org>
2020-06-01 23:02:08 +0000

    [iOS] Move HTTP auth completion callback to standalone C function.
    This was previously handled by a WebStateDelegateTabHelper instance
    method that was bound with a weak pointer.  If the WebState is destroyed
    before the completion callback is executed, this means that the
    WebKit-provided completion block will not be executed since the weak
    pointer makes the callback a no-op.  This CL moves this logic into a
    standalone C function that will execute the WebKit callback regardless
    of whether the WebState has been destroyed.
    
    Bug: 1087858
    Change-Id: Id1a8ccfbead397d04b923b04e069589ea8e83967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225461
    Commit-Queue: Kurt Horimoto <kkhorimoto@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773886}

--- ios/chrome/browser/web/web_state_delegate_tab_helper.mm
+++ ios/chrome/browser/web/web_state_delegate_tab_helper.mm
@@ -19,10 +19,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error "This file requires ARC support."
 #endif
 
+namespace {
+// Callback for HTTP authentication dialogs.  This callback is a standalone
+// function rather than an instance method.  This is to ensure that the callback
+// can be executed regardless of whether the tab helper has been destroyed.
+void OnHTTPAuthOverlayFinished(web::WebStateDelegate::AuthCallback callback,
+                               OverlayResponse* response) {
+  if (response) {
+    HTTPAuthOverlayResponseInfo* auth_info =
+        response->GetInfo<HTTPAuthOverlayResponseInfo>();
+    if (auth_info) {
+      callback.Run(base::SysUTF8ToNSString(auth_info->username()),
+                   base::SysUTF8ToNSString(auth_info->password()));
+      return;
+    }
+  }
+  callback.Run(nil, nil);
+}
+}  // namespace
+
 WEB_STATE_USER_DATA_KEY_IMPL(WebStateDelegateTabHelper)
 
-WebStateDelegateTabHelper::WebStateDelegateTabHelper(web::WebState* web_state)
-    : weak_factory_(this) {
+WebStateDelegateTabHelper::WebStateDelegateTabHelper(web::WebState* web_state) {
   web_state->AddObserver(this);
 }
 
@@ -49,8 +67,7 @@ void WebStateDelegateTabHelper::OnAuthRequired(
           nsurlprotectionspace_util::RequesterOrigin(protection_space), message,
           default_username);
   request->GetCallbackManager()->AddCompletionCallback(
-      base::BindOnce(&WebStateDelegateTabHelper::OnHTTPAuthOverlayFinished,
-                     weak_factory_.GetWeakPtr(), callback));
+      base::BindOnce(&OnHTTPAuthOverlayFinished, callback));
   OverlayRequestQueue::FromWebState(source, OverlayModality::kWebContentArea)
       ->AddRequest(std::move(request));
 }
@@ -61,21 +78,3 @@ void WebStateDelegateTabHelper::WebStateDestroyed(web::WebState* web_state) {
   web_state->RemoveObserver(this);
 }
 
-#pragma mark - Overlay Callbacks
-
-void WebStateDelegateTabHelper::OnHTTPAuthOverlayFinished(
-    web::WebStateDelegate::AuthCallback callback,
-    OverlayResponse* response) {
-  if (!response) {
-    callback.Run(nil, nil);
-    return;
-  }
-  HTTPAuthOverlayResponseInfo* auth_info =
-      response->GetInfo<HTTPAuthOverlayResponseInfo>();
-  if (!auth_info) {
-    callback.Run(nil, nil);
-    return;
-  }
-  callback.Run(base::SysUTF8ToNSString(auth_info->username()),
-               base::SysUTF8ToNSString(auth_info->password()));
-}
