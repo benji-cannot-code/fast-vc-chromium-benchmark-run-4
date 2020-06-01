3ccb309bb33beb753f9349a28caf9383de33021b
Maxime Charland <mcharland@google.com>
2020-06-01 19:47:21 +0000

    Add Feature flag for new Chrome Empties UI
    This flag will be used to show or hide the new UI in Chrome empty states.
    
    Bug: 1086688
    Change-Id: I885d642f1f2e87f9b3b3346f3c1c7f8f80f064dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216547
    Commit-Queue: Maxime Charland <mcharland@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773786}

--- ios/chrome/browser/ui/ui_feature_flags.cc
+++ ios/chrome/browser/ui/ui_feature_flags.cc
@@ -57,3 +57,6 @@ const base::Feature kEnableIOSManagedSettingsUI{
 
 const base::Feature kSafetyCheckIOS{"SafetyCheckIOS",
                                     base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kIllustratedEmptyStates{"IllustratedEmptyStates",
+                                            base::FEATURE_DISABLED_BY_DEFAULT};
