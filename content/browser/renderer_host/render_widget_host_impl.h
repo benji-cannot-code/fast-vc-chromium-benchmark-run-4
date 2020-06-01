682bfbdffb1e0f234fd77b1834f902a7c84e2f4d
Haiyang Pan <hypan@google.com>
2020-06-01 18:07:49 +0000

    Revert "Add window segments concept to the content layer"
    This reverts commit 7a213fdac0a454a10b234f22678af47c1bf62cbc.
    
    Reason for revert: The test RenderWidgetHostViewChildFrameBrowserTest.VisualPropertiesPropagation_RootWindowSegments failed quite often on "Android CFI" and android-pie-x86-rel builders, and flaky on android-asan.
    See http://shortn/_a3kmhb2wuI for more details of the flakiness
    
    Original change's description:
    > Add window segments concept to the content layer
    > 
    > This change adds a the window segments concept to the
    > SynchronizeVisualProperties cascade. A display feature splits a view by,
    > e.g., a fold or a mask - an accessor is added to RenderWidgetHostViewBase
    > to provide this information during the calculation of visual properties.
    > When the root render widget is split by the display feature, there are
    > multiple 'window segments' that web authors will be able to use to split
    > their view and have a logical separation of content (either through
    > JavaScript or CSS).
    > 
    > The root widget window segments are computed by the root
    > RenderWidgetHostImpl, based on the display feature on the
    > RenderWidgetHostView. The segments are then propagated to child local
    > frame roots via RenderFrameProxy and CrossProcessFrameConnector via the
    > new member on VisualProperties.
    > 
    > Follow-up changes will finish hooking this information up to Blink in
    > order to expose it to the web, and also integrate with the platform
    > specific APIs in order to get the display feature information from the
    > underlying operating system.
    > 
    > Explainers:
    > https://github.com/webscreens/window-segments/blob/master/EXPLAINER.md
    > https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/master/Foldables/explainer.md
    > 
    > i2p:
    > https://groups.google.com/a/chromium.org/d/msg/blink-dev/_1_ljYsaqrU/qOoHJ7MhAgAJ
    > https://groups.google.com/a/chromium.org/d/msg/blink-dev/KsPRO3hkOGw/md4jgIAaAgAJ
    > 
    > Bug: 1039050 1039051
    > Change-Id: I6805cda5946bd5379ad6dcd68e092e5d7d9be42d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2205123
    > Commit-Queue: Daniel Libby <dlibby@microsoft.com>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#773545}
    
    TBR=danakj@chromium.org,kinuko@chromium.org,dlibby@microsoft.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1039050 1039051
    Change-Id: I74b881c8548cedfe1c694dbdee1ce617491bf75b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225141
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#773727}

--- content/browser/renderer_host/render_widget_host_impl.h
+++ content/browser/renderer_host/render_widget_host_impl.h
@@ -108,7 +108,6 @@ class SyntheticGestureController;
 class TimeoutMonitor;
 class TouchEmulator;
 class WebCursor;
-struct DisplayFeature;
 struct VisualProperties;
 struct ScreenInfo;
 struct TextInputState;
@@ -628,8 +627,7 @@ class CONTENT_EXPORT RenderWidgetHostImpl
       float page_scale_factor,
       bool is_pinch_gesture_active,
       const gfx::Size& visible_viewport_size,
-      const gfx::Rect& compositor_viewport,
-      std::vector<gfx::Rect> root_widget_window_segments);
+      const gfx::Rect& compositor_viewport);
 
   // Indicates if the render widget host should track the render widget's size
   // as opposed to visa versa.
@@ -845,14 +843,6 @@ class CONTENT_EXPORT RenderWidgetHostImpl
   // ---------------------------------------------------------------------------
 
   bool IsMouseLocked() const;
-
-  // Computes logical segments of the |visible_viewport_size|, based on the
-  // optional DisplayFeature. These segments are in DIPs relative to the widget
-  // origin. If a DisplayFeature is not provided, a vector with a single rect,
-  // the size of the visible viewport will be returned.
-  std::vector<gfx::Rect> ComputeRootWindowSegments(
-      const gfx::Size& visible_viewport_size,
-      const DisplayFeature* display_feature) const;
 
   // The View associated with the RenderWidgetHost. The lifetime of this object
   // is associated with the lifetime of the Render process. If the Renderer
@@ -1144,9 +1134,6 @@ class CONTENT_EXPORT RenderWidgetHostImpl
   // that the renderer receives updates in an atomic fashion along with a
   // synchronization token for the compositor in a LocalSurfaceIdAllocation.
   struct MainFramePropagationProperties {
-    MainFramePropagationProperties();
-    ~MainFramePropagationProperties();
-
     // The page-scale factor of the main-frame.
     float page_scale_factor = 1.f;
 
@@ -1157,10 +1144,6 @@ class CONTENT_EXPORT RenderWidgetHostImpl
     gfx::Size visible_viewport_size;
 
     gfx::Rect compositor_viewport;
-
-    // The logical segments of the root widget, in DIPs relative to the root
-    // RenderWidgetHost.
-    std::vector<gfx::Rect> root_widget_window_segments;
   } properties_from_parent_local_root_;
 
   bool waiting_for_screen_rects_ack_ = false;
