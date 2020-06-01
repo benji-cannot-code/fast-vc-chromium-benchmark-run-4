48045eed0bf832624a0509fefa3dfb62f1129574
Nate Chapin <japhet@chromium.org>
2020-06-01 20:38:38 +0000

    Remove BindCSPImmediately(), it's no longer necessary
    Change-Id: I770dded68d2ff6de14897b5a1666f5931fcfc450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224830
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773811}

--- third_party/blink/renderer/core/execution_context/security_context.h
+++ third_party/blink/renderer/core/execution_context/security_context.h
@@ -192,8 +192,6 @@ class CORE_EXPORT SecurityContext {
     secure_context_mode_ = mode;
   }
 
-  bool BindCSPImmediately() const { return bind_csp_immediately_; }
-
  protected:
   network::mojom::blink::WebSandboxFlags sandbox_flags_;
   scoped_refptr<SecurityOrigin> security_origin_;
@@ -213,7 +211,6 @@ class CORE_EXPORT SecurityContext {
   Member<Agent> agent_;
   SecureContextMode secure_context_mode_;
   Member<OriginTrialContext> origin_trial_context_;
-  bool bind_csp_immediately_ = false;
   DISALLOW_COPY_AND_ASSIGN(SecurityContext);
 };
 
