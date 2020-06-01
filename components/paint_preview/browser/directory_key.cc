248f3be919509e67295c5349e78c2d260d120d11
ckitagawa <ckitagawa@chromium.org>
2020-06-01 19:11:46 +0000

    Reland "[Paint Preview] Create total size budget"
    This is a reland of 09cf456a8e3b89f0f56a25e01c1deef03fae5300
    
    Failures were due to flakiness in FileManagerTest.OldestFileForCleanup
    
    Timing of last modified time seems to be racy leading to inconsistent
    behavior. The fix for this is manually setting the modified time to
    something longer.
    
    Original change's description:
    > [Paint Preview] Create total size budget
    >
    > This CL introduces a mechanism to determine the oldest files and
    > enforce a total disk size budget for captures. By default this is set to
    > 25 MB for the tab service. This will ensure we don't use excessive disk
    > space.
    >
    > Bug: 1086963
    > Change-Id: I71bd8efa5985214b322a4dc3048afbe4d0b717f2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218548
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#772809}
    
    Bug: 1086963, 1087592
    Change-Id: Id95442afcdeb55e60cafc4a8bf6cebef84b3fc8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220483
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773764}

--- components/paint_preview/browser/directory_key.cc
+++ components/paint_preview/browser/directory_key.cc
@@ -11,4 +11,8 @@ bool operator<(const DirectoryKey& a, const DirectoryKey& b) {
   return a.AsciiDirname() < b.AsciiDirname();
 }
 
+bool operator==(const DirectoryKey& a, const DirectoryKey& b) {
+  return a.AsciiDirname() == b.AsciiDirname();
+}
+
 }  // namespace paint_preview
