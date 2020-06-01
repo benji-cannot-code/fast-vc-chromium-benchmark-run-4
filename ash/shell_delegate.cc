f93739fa4c3242c1b58bc7b0cb163cdc810c3b4e
Xiaoqian Dai <xdai@chromium.org>
2020-06-01 21:52:17 +0000

    back gesture: Disable back gesture for {touch-action:none} web pages/apps.
    Bug: 1063144
    Change-Id: I85379cc2f4aded28ac9eefb09e2086d446f42168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166360
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773852}

--- ash/shell_delegate.cc
+++ ash/shell_delegate.cc
@@ -7,6 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+bool ShellDelegate::AllowDefaultTouchActions(gfx::NativeWindow window) {
+  return true;
+}
+
+bool ShellDelegate::ShouldWaitForTouchPressAck(gfx::NativeWindow window) {
+  return false;
+}
+
 bool ShellDelegate::IsTabDrag(const ui::OSExchangeData& drop_data) {
   return false;
 }
