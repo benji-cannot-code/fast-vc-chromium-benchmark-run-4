f8155fefba97c98b5bc29390265ae508cc85e5cb
Alex Gough <ajgo@chromium.org>
2020-06-02 05:51:51 +0000

    Remove policy size metric and associated plumbing.
    Note: check for successful call to Done() before calling AddTarget() is
    retained.
    
    Tests: removing functionality.
    Bug: 1059129, 1089326
    Change-Id: If500d2c2b8aedaa1925fcbaf5b6fa9d32c718a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225223
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774019}

--- sandbox/win/src/policy_low_level.h
+++ sandbox/win/src/policy_low_level.h
@@ -97,10 +97,6 @@ class LowLevelPolicy {
   // passed on the constructor. Returns false on error.
   bool Done();
 
-  // Returns the size that could hold all rules, valid after Done() has
-  // packed them.
-  size_t GetPolicyGlobalSize();
-
  private:
   struct RuleNode {
     const PolicyRule* rule;
@@ -108,7 +104,6 @@ class LowLevelPolicy {
   };
   std::list<RuleNode> rules_;
   PolicyGlobal* policy_store_;
-  size_t policy_global_size_;
   DISALLOW_IMPLICIT_CONSTRUCTORS(LowLevelPolicy);
 };
 
