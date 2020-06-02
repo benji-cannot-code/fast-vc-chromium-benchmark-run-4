15c37b0d1700088c1d94d8287be37c55d7cfa468
Gyuyoung Kim <gyuyoung@igalia.com>
2020-06-02 03:09:27 +0000

    Migrate CameraService interface to the new Mojo types
    This CL converts the uses of CameraService interface as below,
    
      - Convert CameraServicePtr to mojo::Remote
      - Convert CameraServicePtrInfo to mojo::PendingRemote
    
    Bug: 955171
    Change-Id: I5200a2773dfa9ef121de645bedb73d5238a823f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2222087
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#773975}

--- components/arc/camera/arc_camera_bridge.cc
+++ components/arc/camera/arc_camera_bridge.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/arc/session/arc_bridge_service.h"
 #include "crypto/random.h"
 #include "media/capture/video/chromeos/camera_hal_dispatcher_impl.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/platform/platform_channel.h"
 #include "mojo/public/cpp/system/invitation.h"
 #include "mojo/public/cpp/system/platform_handle.h"
@@ -54,9 +55,10 @@ class ArcCameraBridge::PendingStartCameraServiceResult {
       mojo::ScopedMessagePipeHandle pipe,
       ArcCameraBridge::StartCameraServiceCallback callback)
       : owner_(owner),
-        service_(mojom::CameraServicePtrInfo(std::move(pipe), 0u)),
+        service_(
+            mojo::PendingRemote<mojom::CameraService>(std::move(pipe), 0u)),
         callback_(std::move(callback)) {
-    service_.set_connection_error_handler(
+    service_.set_disconnect_handler(
         base::BindOnce(&PendingStartCameraServiceResult::OnError,
                        weak_ptr_factory_.GetWeakPtr()));
     service_.QueryVersion(
@@ -79,13 +81,13 @@ class ArcCameraBridge::PendingStartCameraServiceResult {
   // Runs the callback and removes this object from the owner.
   void Finish() {
     DCHECK(callback_);
-    std::move(callback_).Run(std::move(service_));
+    std::move(callback_).Run(service_.Unbind());
     // Destructs |this|.
     owner_->pending_start_camera_service_results_.erase(this);
   }
 
   ArcCameraBridge* const owner_;
-  mojom::CameraServicePtr service_;
+  mojo::Remote<mojom::CameraService> service_;
   ArcCameraBridge::StartCameraServiceCallback callback_;
   base::WeakPtrFactory<PendingStartCameraServiceResult> weak_ptr_factory_{this};
 
