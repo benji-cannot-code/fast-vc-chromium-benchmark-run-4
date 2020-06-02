b840922ab50b6c337d5f443cdb64fcd74a7e4c50
Timothy Gu <timothygu@chromium.org>
2020-06-02 16:11:19 +0000

    v8bindings: Remove custom bindings of window.event
    Currently, window.event is implemented as a private symbol property
    stored on the Window JavaScript object itself. While doing so may have
    simplified memory management when the code was first written versus
    storing the event in C++, it does not anymore with Oilpan and unified
    heap. This also has no performance benefits, since each call to event()
    has had to jump through multiple C++/JS boundaries.
    
    The custom setter for Window.event was previous removed in r538391. This
    CL removes the custom getter.
    
    Bug: 236420
    Change-Id: I67e862dda8c5a0248436ea90553fbf0b9b37aca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225505
    Commit-Queue: Timothy Gu <timothygu@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774170}

--- third_party/blink/renderer/core/frame/local_dom_window.h
+++ third_party/blink/renderer/core/frame/local_dom_window.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_LOCAL_DOM_WINDOW_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_LOCAL_DOM_WINDOW_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/events/event_target.h"
@@ -354,6 +355,11 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
 
   void AcceptLanguagesChanged();
 
+  // https://dom.spec.whatwg.org/#dom-window-event
+  ScriptValue event(ScriptState*) const;
+  Event* CurrentEvent() const;
+  void SetCurrentEvent(Event*);
+
   TrustedTypePolicyFactory* trustedTypes() const;
 
   // Returns true if this window is cross-site to the main frame. Defaults to
@@ -435,6 +441,10 @@ class CORE_EXPORT LocalDOMWindow final : public DOMWindow,
   scoped_refptr<SerializedScriptValue> pending_state_object_;
 
   HeapHashSet<WeakMember<EventListenerObserver>> event_listener_observers_;
+
+  // https://dom.spec.whatwg.org/#window-current-event
+  // We represent the "undefined" value as nullptr.
+  Member<Event> current_event_;
 
   mutable Member<TrustedTypePolicyFactory> trusted_types_;
 
