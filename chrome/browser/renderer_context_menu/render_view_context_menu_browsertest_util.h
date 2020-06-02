e1c5f60abbaa3c984529dd3a6331cf266ed2f40d
Dominic Mazzoni <dmazzoni@chromium.org>
2020-06-02 23:17:03 +0000

    Revert "Main frame's context menu should exclude frame-specific commands."
    This reverts commit cd9df201100da0d8dd3a584d93d02ac3160f34eb.
    
    Reason for revert: Failing on Mac, see bug
    
    Original change's description:
    > Main frame's context menu should exclude frame-specific commands.
    > 
    > If content::ContextMenuParams includes a non-empty |frame_url|, then the
    > context menu (the menu shown after right clicking in a frame) includes
    > frame-specific commands like "View frame source" and/or "Reload frame".
    > Before r749036 / 82.0.4084.0, context menu shown for the main frame
    > would exclude such frame-specific items.  This behavior has regressed
    > in r749036, because it failed to preserve the following logic from
    > //third_party/blink/renderer/core/page/context_menu_controller.cc:
    > 
    >   if (selected_frame != page_->MainFrame())
    >     data.frame_url = WebURL(UrlFromFrame(selected_frame));
    > 
    > and instead started to unconditionally populate the |frame_url| field in
    > RenderFrameHostImpl::OnContextMenu.  This CL fixes the regression.
    > 
    > Bug: 1085040
    > Change-Id: I149447654bee858037a4230828e2c4a7b3c7c0bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2212995
    > Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#774333}
    
    TBR=nasko@chromium.org,lazyboy@chromium.org,lukasza@chromium.org,lazyboy@google.com
    
    Change-Id: I39ca90cca707409aede5a46f27f300528b4d5d57
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1085040, 1090564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225246
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774387}

--- chrome/browser/renderer_context_menu/render_view_context_menu_browsertest_util.h
+++ chrome/browser/renderer_context_menu/render_view_context_menu_browsertest_util.h
@@ -37,7 +37,6 @@ class ContextMenuWaiter {
   ~ContextMenuWaiter();
 
   content::ContextMenuParams& params();
-  const std::vector<int>& GetCapturedCommandIds() const;
 
   // Wait until the context menu is opened and closed.
   void WaitForMenuOpenAndClose();
@@ -48,8 +47,6 @@ class ContextMenuWaiter {
   void Cancel(RenderViewContextMenu* context_menu);
 
   content::ContextMenuParams params_;
-  std::vector<int> captured_command_ids_;
-
   base::RunLoop run_loop_;
   base::Optional<int> maybe_command_to_execute_;
 
