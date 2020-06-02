a6cbddcdc2404bdac939d5b04202d60e3d7e7e81
Alice Wang <aliceywang@chromium.org>
2020-06-02 12:25:20 +0000

    [Android][Test] Fix flaky bookmark signin promo tests on tablet
    This CL fixes the flaky bookmark signin promo tests on tablet by
    destroying the activity in SyncTestRule before tearing down the
    mock signin environment.
    
    Bug: 1088464, 1088730
    Change-Id: Ide2179edc3f4e2ddd1e115d7b8aa512976dca548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224258
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774096}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
@@ -32,7 +32,6 @@ import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.signin.SigninActivityLauncher;
@@ -41,7 +40,6 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.BookmarkTestRule;
 import org.chromium.chrome.test.util.browser.signin.SigninTestUtil;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
-import org.chromium.ui.test.util.UiDisableIf;
 
 /**
  * Tests for the personalized signin promo on the Bookmarks page.
@@ -67,7 +65,6 @@ public class BookmarkPersonalizedSigninPromoTest {
         BookmarkPromoHeader.forcePromoStateForTests(
                 BookmarkPromoHeader.PromoState.PROMO_SIGNIN_PERSONALIZED);
         SigninActivityLauncher.setLauncherForTest(mMockSigninActivityLauncher);
-        mSyncTestRule.startMainActivityFromLauncher();
     }
 
     @After
@@ -78,7 +75,6 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/1088464
     public void testSigninButtonDefaultAccount() {
         doNothing()
                 .when(SigninActivityLauncher.get())
@@ -93,7 +89,6 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/1088464
     public void testSigninButtonNotDefaultAccount() {
         doNothing()
                 .when(SigninActivityLauncher.get())
@@ -108,7 +103,6 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/1088464
     public void testSigninButtonNewAccount() {
         doNothing()
                 .when(SigninActivityLauncher.get())
