c22b9e0c48a0abfa4e1c9bdd5c0148b836044b4b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-06-02 17:16:03 +0000

    Import wpt@d5aae4ab7f99e024f70aa0d93ca3748809995263
    Using wpt-import in Chromium 216407de1639733ed592963fb2f29c5d05720029.
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org, lpz@chromium.org, robertma@chromium.org:
      external/wpt/tools
    
    NOAUTOREVERT=true
    TBR=robertma
    
    No-Export: true
    Change-Id: I660ebcfaade8bdd4891e15d92a1b35ea44725b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225047
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#774195}

--- third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/browsers/sauce.py
+++ third_party/blink/web_tests/external/wpt/tools/wptrunner/wptrunner/browsers/sauce.py
@@ -70,9 +70,6 @@ def get_capabilities(**kwargs):
         "prerun": prerun_script.get(browser_name)
     }
 
-    if browser_name == 'MicrosoftEdge':
-        capabilities['selenium-version'] = '2.4.8'
-
     return capabilities
 
 
