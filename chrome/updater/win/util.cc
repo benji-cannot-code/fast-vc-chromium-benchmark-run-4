2add1c9042bfca7c9f354ced6d0412e3543a1458
Sorin Jianu <sorin@chromium.org>
2020-06-02 20:44:52 +0000

    Collect external installer progress and propagate it to installer.
    This code polls for the installer progress, collects it, and invokes
    the callback to have the progress injected in the installer stack, and
    propagate it to the UI.
    
    Bug: 1014594
    Change-Id: I0d457e8680dcc065fac7b8c842e1db6340a52032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2226299
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774318}

--- chrome/updater/win/util.cc
+++ chrome/updater/win/util.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/win/registry.h"
 #include "chrome/updater/constants.h"
 #include "chrome/updater/win/constants.h"
 #include "chrome/updater/win/user_info.h"
@@ -320,6 +321,42 @@ int GetDownloadProgress(int64_t downloaded_bytes, int64_t total_bytes) {
   DCHECK_LE(downloaded_bytes, total_bytes);
   return 100 *
          base::ClampToRange(double{downloaded_bytes} / total_bytes, 0.0, 1.0);
+}
+
+// Reads the installer progress from the registry value at:
+// {HKLM|HKCU}\Software\Google\Update\ClientState\<appid>\InstallerProgress.
+int GetInstallerProgress(const std::string& app_id) {
+  base::string16 subkey;
+  if (!base::UTF8ToUTF16(app_id.c_str(), app_id.size(), &subkey)) {
+    return -1;
+  }
+  constexpr REGSAM kRegSam = KEY_READ | KEY_WOW64_32KEY;
+  base::win::RegKey key(HKEY_CURRENT_USER,
+                        base::ASCIIToUTF16(CLIENT_STATE_KEY).c_str(), kRegSam);
+  if (key.OpenKey(subkey.c_str(), kRegSam) != ERROR_SUCCESS) {
+    return -1;
+  }
+  DWORD progress = 0;
+  if (key.ReadValueDW(kRegistryValueInstallerProgress, &progress) !=
+      ERROR_SUCCESS) {
+    return -1;
+  }
+  return base::ClampToRange(progress, DWORD{0}, DWORD{100});
+}
+
+bool DeleteInstallerProgress(const std::string& app_id) {
+  base::string16 subkey;
+  if (!base::UTF8ToUTF16(app_id.c_str(), app_id.size(), &subkey)) {
+    return false;
+  }
+  constexpr REGSAM kRegSam = KEY_SET_VALUE | KEY_WOW64_32KEY;
+  base::win::RegKey key(HKEY_CURRENT_USER,
+                        base::ASCIIToUTF16(CLIENT_STATE_KEY).c_str(), kRegSam);
+  if (key.OpenKey(subkey.c_str(), kRegSam) != ERROR_SUCCESS) {
+    return false;
+  }
+
+  return key.DeleteValue(kRegistryValueInstallerProgress) == ERROR_SUCCESS;
 }
 
 }  // namespace updater
