9bb8946304a7fdf1dc590054a907587269a9adfe
François Doray <fdoray@chromium.org>
2020-06-02 19:49:42 +0000

    Revert "dpwa: Implement backward compatibility for Bookmark Apps."
    This reverts commit 30e4bc3995f5fffc23f20de5507d0ec17a60a8a5.
    
    Reason for revert: Suspected culprit for crbug.com/1090427
    
    Original change's description:
    > dpwa: Implement backward compatibility for Bookmark Apps.
    > 
    > Implement backward compatibility of Web Apps via
    > shadow Bookmark Apps.
    > 
    > On a web app install/update/uninstall we also install/update/uninstall
    > the shadow bookmark app.
    > 
    > Any install/uninstall changes for the extensions registry get
    > propagated to the sync server (syncer::ModelType::APPS) and to all
    > devices where BMO is disabled.
    > 
    > Design doc: go/chrome-bmo-migration.
    > 
    > Bug: 1020037
    > Fixed: 1086909
    > Change-Id: I29c673f8eb83373f4abb1d984c91e8a436d00410
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2162507
    > Commit-Queue: Owen Min <zmin@chromium.org>
    > Auto-Submit: Alexey Baskakov <loyso@chromium.org>
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Reviewed-by: Alan Cutter <alancutter@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#774134}
    
    TBR=alancutter@chromium.org,loyso@chromium.org,zmin@chromium.org
    
    Change-Id: I8e43156b19eccc9d4dd550d946e7551ade9cc54d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1020037, 1090427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2226060
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774287}

--- chrome/browser/policy/extension_policy_browsertest.cc
+++ chrome/browser/policy/extension_policy_browsertest.cc
@@ -226,12 +226,6 @@ class ExtensionPolicyTest : public PolicyTest {
   }
 
   const web_app::AppId InstallWebApp() {
-    // Waits for the shadow bookmark app to be installed to avoid extension
-    // system races.
-    content::WindowedNotificationObserver observer(
-        extensions::NOTIFICATION_CRX_INSTALLER_DONE,
-        content::NotificationService::AllSources());
-
     std::unique_ptr<WebApplicationInfo> web_application =
         std::make_unique<WebApplicationInfo>();
     web_application->title = base::ASCIIToUTF16("Web App");
@@ -248,9 +242,6 @@ class ExtensionPolicyTest : public PolicyTest {
               loop.Quit();
             }));
     loop.Run();
-
-    observer.Wait();
-
     return return_app_id;
   }
 
--- chrome/browser/web_applications/web_app_install_finalizer.h
+++ chrome/browser/web_applications/web_app_install_finalizer.h
@@ -27,10 +27,8 @@ class WebAppRegistrar;
 
 class WebAppInstallFinalizer final : public InstallFinalizer {
  public:
-  // |legacy_finalizer| can be nullptr (optional argument).
   WebAppInstallFinalizer(Profile* profile,
-                         WebAppIconManager* icon_manager,
-                         std::unique_ptr<InstallFinalizer> legacy_finalizer);
+                         WebAppIconManager* icon_manager);
   ~WebAppInstallFinalizer() override;
 
   // InstallFinalizer:
@@ -96,8 +94,6 @@ class WebAppInstallFinalizer final : public InstallFinalizer {
                                   InstallResultCode code);
 
   WebAppRegistrar& GetWebAppRegistrar() const;
-
-  std::unique_ptr<InstallFinalizer> legacy_finalizer_;
 
   Profile* const profile_;
   WebAppIconManager* const icon_manager_;
