aa1ede3113a6b3093b3cd8d80f315de62a42f719
Tom Anderson <thomasanderson@chromium.org>
2020-06-02 21:42:26 +0000

    Reland "[XProto] Remove all remaining non-event usages of XRandR"
    This is a reland of 0af7d7aa93b368435a008af9b19d50db17d6a365
    
    Original change's description:
    > [XProto] Remove all remaining non-event usages of XRandR
    >
    > BUG=1066670
    > R=msisov
    > CC=sky
    >
    > Change-Id: Iecd593c0c77878a80bf84e9e3c504fd90815db6c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220686
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Reviewed-by: Maksim Sisov <msisov@igalia.com>
    > Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#773876}
    
    Bug: 1066670
    R=msisov
    TBR=jamiewalch
    
    Change-Id: Id24ace289778fc9f6feb8de9ed2744a2630be766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2226785
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774348}

--- ui/gfx/x/connection.cc
+++ ui/gfx/x/connection.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/x/connection.h"
 
 #include <X11/Xlib-xcb.h>
+#include <X11/Xlib.h>
 #include <xcb/xcb.h>
 
 #include <algorithm>
@@ -52,25 +53,25 @@ XDisplay* OpenNewXDisplay() {
 }  // namespace
 
 Connection* Connection::Get() {
-  static Connection* instance = new Connection(OpenNewXDisplay());
+  static Connection* instance = new Connection;
   return instance;
 }
 
-Connection::Connection(XDisplay* display) : XProto(this), display_(display) {
+Connection::Connection() : XProto(this), display_(OpenNewXDisplay()) {
   if (!display_)
     return;
 
-  setup_ = std::make_unique<x11::Setup>(x11::Read<x11::Setup>(
+  setup_ = std::make_unique<Setup>(Read<Setup>(
       reinterpret_cast<const uint8_t*>(xcb_get_setup(XcbConnection()))));
-  default_screen_ = &setup_->roots[DefaultScreen(display)];
+  default_screen_ = &setup_->roots[DefaultScreen(display_)];
   default_root_depth_ = &*std::find_if(
       default_screen_->allowed_depths.begin(),
-      default_screen_->allowed_depths.end(), [&](const x11::Depth& depth) {
+      default_screen_->allowed_depths.end(), [&](const Depth& depth) {
         return depth.depth == default_screen_->root_depth;
       });
   defualt_root_visual_ = &*std::find_if(
       default_root_depth_->visuals.begin(), default_root_depth_->visuals.end(),
-      [&](const x11::VisualType visual) {
+      [&](const VisualType visual) {
         return visual.visual_id == default_screen_->root_visual;
       });
 
@@ -81,7 +82,10 @@ Connection::Connection(XDisplay* display) : XProto(this), display_(display) {
   }
 }
 
-Connection::~Connection() = default;
+Connection::~Connection() {
+  if (display_)
+    XCloseDisplay(display_);
+}
 
 xcb_connection_t* Connection::XcbConnection() {
   if (!display())
