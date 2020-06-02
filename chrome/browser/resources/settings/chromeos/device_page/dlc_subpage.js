ddb0411a5cfac7fbb45adc4a15d383b682837450
Regan Hsu <hsuregan@google.com>
2020-06-02 19:48:45 +0000

    [DlcSettingsUi] Default feature flag to on, and remove fake dlc results.
    Fixed: 1070712
    Change-Id: I7f3fc2c906112d615f22b8298f093cd6c02277f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225482
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774286}

--- chrome/browser/resources/settings/chromeos/device_page/dlc_subpage.js
+++ chrome/browser/resources/settings/chromeos/device_page/dlc_subpage.js
@@ -8,48 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * 'os-settings-dlc-subpage' is the Downloaded Content subpage.
  */
 cr.define('settings', function() {
-  /**
-   * TODO(hsuregan): Delete when DLC subpage is finished.
-   * Whether to fetch fake DLC list.
-   * @type {boolean}
-   */
-  const USE_FAKE_DLC_LIST = true;
-
-  /**
-   * TODO(hsuregan): Delete when DLC subpage is finished.
-   * Fake function to simulate async DevicePageBrowserProxy getDlcList().
-   * @return {!Promise<!Array<!settings.DlcMetadata>>} A list of DLC metadata.
-   */
-  function getFakeDlcList() {
-    /**
-     * @param {number} max The upper bound integer.
-     * @return {number} A random in the range [0, max).
-     */
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
-
-    const fakeRandomResults =
-        [...Array(getRandomInt(getRandomInt(40))).keys()].map((idx) => {
-          const dlcDescription = getRandomInt(2) ?
-              `fake description ${idx} `.repeat(getRandomInt(500)) :
-              '';
-          return {
-            id: `fake id ${idx}`,
-            name: `fake name ${idx}`,
-            description: dlcDescription,
-            diskUsageLabel: `fake diskUsageLabel`,
-          };
-        });
-
-    console.log(fakeRandomResults);
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(fakeRandomResults);
-      }, 0);
-    });
-  }
-
   Polymer({
     is: 'os-settings-dlc-subpage',
 
@@ -78,11 +36,6 @@ cr.define('settings', function() {
 
     /** @override */
     attached() {
-      if (USE_FAKE_DLC_LIST) {
-        getFakeDlcList().then(this.onDlcListChanged_.bind(this));
-        return;
-      }
-
       this.addWebUIListener(
           'dlc-list-changed', this.onDlcListChanged_.bind(this));
       this.browserProxy_.notifyDlcSubpageReady();
