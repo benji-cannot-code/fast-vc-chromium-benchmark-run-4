2768a6814fb5eaf8b8e08f82c86ae1ab97617f40
Peter Marshall <petermarshall@chromium.org>
2020-06-02 11:18:36 +0000

    Revert "Mac: don't use a custom tracking area in BridgedContentView"
    This reverts commit 7e18556bc311219566b5e40af3cded316052a566.
    
    Reason for revert: Breaks devtools overlay: crbug.com/1090280
    and link hovering: https://bugs.chromium.org/p/chromium/issues/detail?id=841792#c24
    
    Original change's description:
    > Mac: don't use a custom tracking area in BridgedContentView
    > 
    > Currently, in BridgedContentView we "opt out"[1] of BaseView's tracking area to install a different one which:
    > - Specifies `NSTrackingCursorUpdate`
    > - As a consequence, only tracks while app is active, since the docs say that always tracking is incompatible with cursor updates
    > 
    > However:
    > - Empirically, cursor updates work fine without this (some test cases: move mouse between Omnibox and other parts of topchrome, move from webcontent links/inputs to topchrome and vice-versa).
    > - Only tracking while app is active means we don't get entered/exited events from app switching. This means:
    >   - Switching away from Chrome while showing a topchrome hover tooltip keeps the hover on and makes the tooltip stuck
    >   - Under some circumstances, hover stops working on switching back to Chrome (see bug)
    >   - Even though we allow mouse clicks to function when Chrome is inactive (that is, we return `YES` from `acceptsFirstMouse:`, we don't process hovers in this case, contrary to other apps on the system.
    > 
    > This change removes BridgedContentView's tracking area in favor of the default one and gives BaseView some explicit enable/disables, mostly to make tests happy.
    > 
    > [1] By overriding a private method with a no-op!
    > 
    > Bug: 841792
    > 
    > Change-Id: Ifb5d65e22299959b8cac25a9e0a0407650a40746
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216530
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Leonard Grey <lgrey@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#773848}
    
    TBR=ellyjones@chromium.org,avi@chromium.org,lgrey@chromium.org
    
    Change-Id: Ic1344adb7cdf271636026a2b1e6def014852416f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 841792, 1090280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2223949
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774088}

--- components/remote_cocoa/app_shim/bridged_content_view.h
+++ components/remote_cocoa/app_shim/bridged_content_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string16.h"
 #include "components/remote_cocoa/app_shim/remote_cocoa_app_shim_export.h"
 #import "ui/base/cocoa/tool_tip_base_view.h"
+#import "ui/base/cocoa/tracking_area.h"
 
 namespace remote_cocoa {
 class NativeWidgetNSWindowBridge;
@@ -31,6 +32,9 @@ REMOTE_COCOA_APP_SHIM_EXPORT
  @private
   // Weak, reset by clearView.
   remote_cocoa::NativeWidgetNSWindowBridge* _bridge;
+
+  // A tracking area installed to enable mouseMoved events.
+  ui::ScopedCrTrackingArea _cursorTrackingArea;
 
   // The keyDown event currently being handled, nil otherwise.
   NSEvent* _keyDownEvent;
