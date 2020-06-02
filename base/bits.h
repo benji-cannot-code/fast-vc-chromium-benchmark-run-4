68d2a66c6f8b9042cd5727cfef99500012adb525
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2020-06-02 13:00:41 +0000

    base/bits: Include <limits.h> for CHAR_BIT
    IWYU. Explicitly include the right header to fix the build with libstdc++
    after commit 02b94c59050a5 ("Introduce the bits::LeftmostBit<T> function").
    
    Bug: 957519
    Change-Id: Idfe6303428ba306bca6b6e821fd197371fe7f377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225403
    Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774104}

--- base/bits.h
+++ base/bits.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_BITS_H_
 #define BASE_BITS_H_
 
+#include <limits.h>
 #include <stddef.h>
 #include <stdint.h>
 
