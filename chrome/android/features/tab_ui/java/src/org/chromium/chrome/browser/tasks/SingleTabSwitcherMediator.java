8559eb02ec5bfa519e7768e155008a9d4ca46530
Xi Han <hanxi@google.com>
2020-06-02 20:26:10 +0000

    [Instant Start] StartSurface will hide after navigating from the Omnibox
    Currently, typing an URL in the fake Omnibox doesn't trigger the Start
    Surface to hide and navigate to a new Tab when instant start is
    enabled. This is because the TabModelObserver is registered before the
    Tab State is initialized. Thus, the observer is registered to an empty
    TabModel, rather than the correct one. This CL caches the pending
    request, and registers the observer after native/Tab State is
    initialized.
    
    Bug: 1086608
    Change-Id: I734cd9b3622f37cf672ba54a639ab8f7085ce0f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218468
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774305}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/SingleTabSwitcherMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/SingleTabSwitcherMediator.java
@@ -42,6 +42,7 @@ class SingleTabSwitcherMediator implements TabSwitcher.Controller {
     private TabSwitcher.OnTabSelectingListener mTabSelectingListener;
     private boolean mShouldIgnoreNextSelect;
     private boolean mSelectedTabDidNotChangedAfterShown;
+    private boolean mAddNormalTabModelObserverPending;
 
     SingleTabSwitcherMediator(PropertyModel propertyModel, TabModelSelector tabModelSelector,
             TabListFaviconProvider tabListFaviconProvider) {
@@ -80,6 +81,11 @@ class SingleTabSwitcherMediator implements TabSwitcher.Controller {
             @Override
             public void onTabStateInitialized() {
                 TabModel normalTabModel = mTabModelSelector.getModel(false);
+                if (mAddNormalTabModelObserverPending) {
+                    mAddNormalTabModelObserverPending = false;
+                    normalTabModel.addObserver(mNormalTabModelObserver);
+                }
+
                 int selectedTabIndex = normalTabModel.index();
                 if (selectedTabIndex != TabList.INVALID_TAB_INDEX) {
                     assert normalTabModel.getCount() > 0;
@@ -134,17 +140,12 @@ class SingleTabSwitcherMediator implements TabSwitcher.Controller {
     @Override
     public void showOverview(boolean animate) {
         mSelectedTabDidNotChangedAfterShown = true;
-        TabModel normalTabModel = mTabModelSelector.getModel(false);
-        if (normalTabModel != null) {
-            normalTabModel.addObserver(mNormalTabModelObserver);
-        } else {
-            assert CachedFeatureFlags.isEnabled(ChromeFeatureList.INSTANT_START)
-                : "Normal tab model should exist except for instant start";
-        }
         mTabModelSelector.addObserver(mTabModelSelectorObserver);
 
         if (CachedFeatureFlags.isEnabled(ChromeFeatureList.INSTANT_START)
                 && !mTabModelSelector.isTabStateInitialized()) {
+            mAddNormalTabModelObserverPending = true;
+
             PseudoTab activeTab;
             try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
                 activeTab = PseudoTab.getActiveTabFromStateFile();
@@ -153,6 +154,9 @@ class SingleTabSwitcherMediator implements TabSwitcher.Controller {
                 mPropertyModel.set(TITLE, activeTab.getTitle());
             }
         } else {
+            TabModel normalTabModel = mTabModelSelector.getModel(false);
+            normalTabModel.addObserver(mNormalTabModelObserver);
+
             int selectedTabIndex = normalTabModel.index();
             if (selectedTabIndex != TabList.INVALID_TAB_INDEX) {
                 assert normalTabModel.getCount() > 0;
