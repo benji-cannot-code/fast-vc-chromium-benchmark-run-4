96c1309b1e257b13856653d7f46f4c5998a205b6
Samuel Tang <samtan@microsoft.com>
2020-06-02 17:46:07 +0000

    Desktop PWAs: Add protocol_handler parsing to manifest_parser
    Manifest parser can now parse protocol_handlers
    
    This feature allows the blink parser to parse protocol_handlers
    defined in its app manifest. It is checked in behind the
    flag ParseUrlProtocolHandler.
    
    The current CL does not include semantic validation of the scheme.
    We will include it in a subsequent CL to keep CLs small and scoped.
    
    This feature is part of a Chrome experiment, see the feature page:
    https://chromestatus.com/features/5151703944921088
    
    The URLProtocolHandler explainer can be found here:
    https://github.com/MicrosoftEdge/MSEdgeExplainers/blob/master/URLProtocolHandler/explainer.md
    
    The main bug link is: https://bugs.chromium.org/p/chromium/issues/detail?id=1019239
    
    The blinkdev proposal is here: https://groups.google.com/a/chromium.org/forum/?hl=en#!topic/blink-dev/x4Ev_l9Oj2U
    
    Bug: 1019239
    Change-Id: I85a99064485f12e7c224bdaf491ad413e451229c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2211200
    Reviewed-by: Alex Russell <slightlyoff@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Samuel Tang <samtan@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#774211}

--- third_party/blink/renderer/modules/manifest/manifest_type_converters.cc
+++ third_party/blink/renderer/modules/manifest/manifest_type_converters.cc
@@ -53,6 +53,11 @@ TypeConverter<blink::Manifest, blink::mojom::blink::ManifestPtr>::Convert(
     output.file_handlers.push_back(entry.To<blink::Manifest::FileHandler>());
   }
 
+  for (auto& uri_protocol : input->protocol_handlers) {
+    output.protocol_handlers.push_back(
+        uri_protocol.To<blink::Manifest::ProtocolHandler>());
+  }
+
   for (auto& related_application : input->related_applications) {
     output.related_applications.push_back(
         related_application.To<blink::Manifest::RelatedApplication>());
@@ -223,6 +228,18 @@ TypeConverter<blink::Manifest::FileHandler,
       extensions.push_back(blink::WebString(extension).Utf16());
   }
 
+  return output;
+}
+
+blink::Manifest::ProtocolHandler
+TypeConverter<blink::Manifest::ProtocolHandler,
+              blink::mojom::blink::ManifestProtocolHandlerPtr>::
+    Convert(const blink::mojom::blink::ManifestProtocolHandlerPtr& input) {
+  blink::Manifest::ProtocolHandler output;
+  if (input.is_null())
+    return output;
+  output.protocol = blink::WebString(input->protocol).Utf16();
+  output.url = input->url;
   return output;
 }
 
