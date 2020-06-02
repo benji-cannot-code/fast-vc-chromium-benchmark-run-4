a451e9f6770cb6f5b6dedb17152ed514e1335fbf
Maksim Moskvitin <mmoskvitin@google.com>
2020-06-02 12:31:35 +0000

    Remove SyncEncryptionHandlerImpl
    SyncEncryptionHandlerImpl is a part of Directory Nigori implementation
    that is now replaced by USS.
    
    Bug: 1083924
    Change-Id: I5f3f26b05ba5189213e602128eb8a5549d9cde94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2223942
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774097}

--- components/sync/driver/glue/sync_engine_backend.cc
+++ components/sync/driver/glue/sync_engine_backend.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/engine/sync_backend_registrar.h"
 #include "components/sync/engine/sync_manager.h"
 #include "components/sync/engine/sync_manager_factory.h"
-#include "components/sync/engine_impl/sync_encryption_handler_impl.h"
 #include "components/sync/model_impl/forwarding_model_type_controller_delegate.h"
 #include "components/sync/nigori/nigori_model_type_processor.h"
 #include "components/sync/nigori/nigori_storage_impl.h"
