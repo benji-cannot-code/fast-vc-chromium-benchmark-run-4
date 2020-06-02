8b5725ad57e1ff6569feec8bb31ca3801fc4e0b5
Thiemo Nagel <tnagel@chromium.org>
2020-06-02 08:50:45 +0000

    Revert "[bfcache] Blocklist documents/frames using PaymentManager"
    This reverts commit 40342cdf31b8b29ca79374abb8c7e70ccc5d63bb.
    
    Reason for revert: It seems the CL has introduced high levels of flakiness in BackForwardCacheBrowserTest.DoesNotCachePaymentManager
    
    See e.g.: https://chromium-swarm.appspot.com/task?id=4c8c1a19978bfe10
    
    Mac10.10 Tests has failed six times:
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.10%20Tests?limit=200
    
    Original change's description:
    > [bfcache] Blocklist documents/frames using PaymentManager
    > 
    > As a note - this patch jumps from 47 to 49 in WebSchedulerTrackedFeature so as to prevent merge conflicts with the patch here:
    > https://chromium-review.googlesource.com/c/chromium/src/+/2212934
    > 
    > Some background on why we sometimes need to disable bfcache:
    > https://docs.google.com/document/d/1NjZeusdS1kyEkZyfLggndU1A6qVt0Y1sa-LRUxnMoK8
    > 
    > Change-Id: I884e7b5262938c453cea7e1792b505c3a55447bb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225355
    > Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    > Reviewed-by: Alexander Timin <altimin@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#773982}
    
    TBR=nasko@chromium.org,haraken@chromium.org,altimin@chromium.org,hbolaria@google.com
    
    Change-Id: Id7b7cdaaed0815addf65795cb664d33520e6d841
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2223943
    Reviewed-by: Thiemo Nagel <tnagel@chromium.org>
    Commit-Queue: Thiemo Nagel <tnagel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774057}

--- content/browser/frame_host/back_forward_cache_impl.cc
+++ content/browser/frame_host/back_forward_cache_impl.cc
@@ -156,8 +156,7 @@ uint64_t GetDisallowedFeatures(RenderFrameHostImpl* rfh) {
       FeatureToBit(WebSchedulerTrackedFeature::kWebDatabase) |
       FeatureToBit(WebSchedulerTrackedFeature::kPictureInPicture) |
       FeatureToBit(WebSchedulerTrackedFeature::kPortal) |
-      FeatureToBit(WebSchedulerTrackedFeature::kSpeechRecognizer) |
-      FeatureToBit(WebSchedulerTrackedFeature::kPaymentManager);
+      FeatureToBit(WebSchedulerTrackedFeature::kSpeechRecognizer);
 
   uint64_t result = kAlwaysDisallowedFeatures;
 
