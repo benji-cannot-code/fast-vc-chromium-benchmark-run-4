a37818f4f4b9402f7645d965fc7cae54ca1d1aa3
Xida Chen <xidachen@chromium.org>
2020-06-02 18:36:44 +0000

    [throughput] Report median throughput to UKM
    Right now we report throughput to UKM system at:
      1. 5 seconds after a sequence starts.
      2. Every 500 seconds after that as long as the sequence lasts.
    The problem is that in many cases users stay on a page less than 500
    seconds, and thus we would only get the report for the first 5 seconds,
    which can be heavily dominated by the page loading period.
    
    This CL changes it such that we record the throughput every 5 seconds,
    and then report the median throughput to the UKM when the page shuts
    down. At this moment, this applies to the kUniversal tracker only,
    which tracks all kinds of frame productions. For other types of
    trackers, such as the trackers tracks animation/scroll, etc, this
    change will be applied later.
    
    Bug: 1040634
    Change-Id: I452e16c45187393f29abf1bdf3650a4e5ffb45fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2112953
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774242}

--- cc/metrics/frame_sequence_tracker_unittest.cc
+++ cc/metrics/frame_sequence_tracker_unittest.cc
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "cc/metrics/compositor_frame_reporting_controller.h"
 #include "cc/metrics/frame_sequence_tracker_collection.h"
+#include "cc/metrics/throughput_ukm_reporter.h"
+#include "cc/trees/ukm_manager.h"
+#include "components/ukm/test_ukm_recorder.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -255,12 +258,27 @@ class FrameSequenceTrackerTest : public testing::Test {
     return tracker_->ignored_frame_tokens_;
   }
 
+  FrameSequenceMetrics::ThroughputData& ImplThroughput(
+      FrameSequenceTracker* tracker) const {
+    return tracker->impl_throughput();
+  }
+
   FrameSequenceMetrics::ThroughputData& ImplThroughput() const {
     return tracker_->impl_throughput();
   }
 
+  FrameSequenceMetrics::ThroughputData& MainThroughput(
+      FrameSequenceTracker* tracker) const {
+    return tracker->main_throughput();
+  }
+
   FrameSequenceMetrics::ThroughputData& MainThroughput() const {
     return tracker_->main_throughput();
+  }
+
+  FrameSequenceMetrics::ThroughputData& AggregatedThroughput(
+      FrameSequenceTracker* tracker) const {
+    return tracker->aggregated_throughput();
   }
 
   FrameSequenceMetrics::ThroughputData& AggregatedThroughput() const {
@@ -1868,6 +1886,29 @@ TEST_F(FrameSequenceTrackerTest, TrackerTypeEncoding) {
   ActiveFrameSequenceTrackers active_encoded =
       collection_.FrameSequenceTrackerActiveTypes();
   EXPECT_EQ(active_encoded, 16);  // 1 << 4
+}
+
+TEST_F(FrameSequenceTrackerTest, UniversalTrackerSubmitThroughput) {
+  auto recorder = std::make_unique<ukm::TestUkmRecorder>();
+  auto ukm_manager = std::make_unique<UkmManager>(std::move(recorder));
+
+  collection_.ClearAll();
+  collection_.SetUkmManager(ukm_manager.get());
+  collection_.StartSequence(FrameSequenceTrackerType::kUniversal);
+  FrameSequenceTracker* tracker =
+      collection_.GetTrackerForTesting(FrameSequenceTrackerType::kUniversal);
+  ImplThroughput(tracker).frames_expected = 200u;
+  ImplThroughput(tracker).frames_produced = 190u;
+  MainThroughput(tracker).frames_expected = 100u;
+  MainThroughput(tracker).frames_produced = 50u;
+  AggregatedThroughput(tracker).frames_produced = 150u;
+
+  collection_.ComputeUniversalThroughputForTesting();
+  DCHECK(collection_.HasThroughputData());
+  EXPECT_EQ(collection_.TakeLastAggregatedPercent(), 25);
+  EXPECT_EQ(collection_.TakeLastImplPercent(), 5);
+  EXPECT_EQ(collection_.TakeLastMainPercent().value(), 50);
+  EXPECT_FALSE(collection_.HasThroughputData());
 }
 
 TEST_F(FrameSequenceTrackerTest, CustomTrackers) {
--- cc/trees/layer_tree_host_client.h
+++ cc/trees/layer_tree_host_client.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "cc/input/browser_controls_state.h"
 #include "cc/metrics/frame_sequence_tracker_collection.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 #include "ui/gfx/geometry/scroll_offset.h"
 #include "ui/gfx/geometry/vector2d_f.h"
 
@@ -172,6 +173,12 @@ class LayerTreeHostClient {
   // RecordEndOfFrameMetrics.
   virtual std::unique_ptr<BeginMainFrameMetrics> GetBeginMainFrameMetrics() = 0;
   virtual void NotifyThroughputTrackerResults(CustomTrackerResults results) = 0;
+  // LayerTreeHost calls this when there is new throughput data. The client send
+  // the data to the browser process who will aggregate them and report to UKM.
+  virtual void SubmitThroughputData(ukm::SourceId source_id,
+                                    int aggregated_percent,
+                                    int impl_percent,
+                                    base::Optional<int> main_percent) = 0;
 
  protected:
   virtual ~LayerTreeHostClient() = default;
