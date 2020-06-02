d37be5110f672258ff26cd6a4202e3de17b2c639
phweiss <phweiss@google.com>
2020-06-02 07:27:25 +0000

    Hide ARC++ Windows with the same logical_window_id
    Windows are considered one "logical window" if they are created
    with the same |shelf_group_id| and |logical_window_id|.
    The consequence: only one window per logical window is shown in
    the shelf, the alt-tab menu, and the desk mini view.
    
    The use case are windows across multiple screen, which are not possible
    in Chrome OS. So the app creates one window per screen and marks them
    as one logical window.
    
    This is done by the |ArcAppWindowInfo|, which stores the IDs mentioned
    above. It is setting the |HideInDeskMiniView| property, the
    |HideInOverview| property and the new |HideInShelf| property on a
    window. The AppWindowLauncherItemController pays attention to the
    HideInShelf property and will not use hidden windows as
    |last_active_window|s, nor return them via |GetAppMenuItems|.
    
    When a window closes, it might have been the visible window of a
    logical window, so the |AppServiceAppWindowArcTracker| checks this and
    tells a different app of the same logical window to become visible.
    
    browser_tests --gtest_filter=ArcAppLauncherBrowserTest.LogicalWindow
    
    Test: 
    Bug: b/148875131
    Change-Id: Ide57ac8dbc77137d4678763bdc88806f3937c0dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2207316
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Commit-Queue: Philipp Weiß <phweiss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774043}

--- chrome/browser/ui/ash/launcher/extension_app_window_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/extension_app_window_launcher_controller.cc
@@ -231,7 +231,8 @@ void ExtensionAppWindowLauncherController::UnregisterApp(aura::Window* window) {
   ExtensionAppWindowLauncherItemController* controller =
       app_controller_iter->second;
 
-  controller->RemoveWindow(controller->GetAppWindow(window));
+  controller->RemoveWindow(
+      controller->GetAppWindow(window, true /*include_hidden*/));
   if (controller->window_count() == 0) {
     // If this is the last window associated with the app window shelf id,
     // close the shelf item.
