3083368aafb66009c3a6c8949ddc2f3ef60f5202
Joel Hockey <joelhockey@chromium.org>
2020-06-02 00:39:35 +0000

    Terminal CSP requires style and font to load web fonts
    Bug: 1085324
    Change-Id: I69984c8fa234da941382779544bbcd90a45f4ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224891
    Commit-Queue: Jason Lin <lxj@google.com>
    Reviewed-by: Jason Lin <lxj@google.com>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773929}

--- chrome/browser/chromeos/web_applications/terminal_source.cc
+++ chrome/browser/chromeos/web_applications/terminal_source.cc
@@ -157,5 +157,5 @@ std::string TerminalSource::GetContentSecurityPolicyImgSrc() {
 }
 
 std::string TerminalSource::GetContentSecurityPolicyStyleSrc() {
-  return "style-src 'self' 'unsafe-inline';";
+  return "style-src * 'unsafe-inline'; font-src *;";
 }
