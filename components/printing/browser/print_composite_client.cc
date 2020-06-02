9ddd48445a7c09cc68a16390b9aa24345fd32ee4
Alex Gough <ajgo@chromium.org>
2020-06-02 19:10:28 +0000

    Supply PrintCompositor sandbox from service_sandbox_type.h
    Prelude to removal of WithSandboxType API. See linked bug for
    rationale.  Does not change behavior.
    
    Tests: browser_tests PrintBrowserTest.*
    Bug: 1065087
    Change-Id: Idaf185273813b4fc75cad3897f0507569135134d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202423
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774263}

--- components/printing/browser/print_composite_client.cc
+++ components/printing/browser/print_composite_client.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "build/build_config.h"
 #include "components/discardable_memory/service/discardable_shared_memory_manager.h"
+#include "components/printing/browser/service_sandbox_type.h"
 #include "components/printing/common/print_messages.h"
 #include "components/services/print_compositor/public/cpp/print_service_mojo_types.h"
 #include "components/strings/grit/components_strings.h"
@@ -323,7 +324,6 @@ PrintCompositeClient::CreateCompositeRequest() {
   auto compositor = content::ServiceProcessHost::Launch<mojom::PrintCompositor>(
       content::ServiceProcessHost::Options()
           .WithDisplayName(IDS_PRINT_COMPOSITOR_SERVICE_DISPLAY_NAME)
-          .WithSandboxType(service_manager::SandboxType::kPrintCompositor)
           .Pass());
 
   mojo::PendingRemote<discardable_memory::mojom::DiscardableSharedMemoryManager>
---
+++ components/printing/browser/service_sandbox_type.h
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PRINTING_BROWSER_SERVICE_SANDBOX_TYPE_H_
+#define COMPONENTS_PRINTING_BROWSER_SERVICE_SANDBOX_TYPE_H_
+
+#include "content/public/browser/sandbox_type.h"
+#include "content/public/browser/service_process_host.h"
+
+// This file maps service classes to sandbox types.  Services which
+// require a non-utility sandbox can be added here.  See
+// ServiceProcessHost::Launch() for how these templates are consumed.
+
+// printing::mojom::PrintCompositor
+namespace printing {
+namespace mojom {
+class PrintCompositor;
+}
+}  // namespace printing
+
+template <>
+inline content::SandboxType
+content::GetServiceSandboxType<printing::mojom::PrintCompositor>() {
+  return content::SandboxType::kPrintCompositor;
+}
+
+#endif  // COMPONENTS_PRINTING_BROWSER_SERVICE_SANDBOX_TYPE_H_
