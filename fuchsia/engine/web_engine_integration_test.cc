df2870a14caf5178ecb7620d96546633588341e5
Thomas Guilbert <tguilbert@chromium.org>
2020-06-02 00:17:48 +0000

    [Sheriff] Disable WebIntegrationTest.PlayVideo
    This newly added test is timing out.
    
    TBR=sergeyu@chromium.org
    
    Bug: 1090159
    Change-Id: I5dcc92935e12664770a17ce9f70ec03c3c9710bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225595
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#773914}

--- fuchsia/engine/web_engine_integration_test.cc
+++ fuchsia/engine/web_engine_integration_test.cc
@@ -525,7 +525,7 @@ TEST_F(WebEngineIntegrationTest, PlayAudio) {
   EXPECT_FALSE(fake_audio_consumer_service_->instance(0)->is_muted());
 }
 
-TEST_F(WebEngineIntegrationTest, PlayVideo) {
+TEST_F(WebEngineIntegrationTest, DISABLED_PlayVideo) {
   StartWebEngine();
   CreateContextAndFrame(ContextParamsWithAudioAndTestData());
 
