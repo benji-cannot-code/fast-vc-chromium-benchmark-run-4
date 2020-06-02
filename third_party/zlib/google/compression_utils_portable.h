2d43268066fa70fc8b369b1a1d60ac68b92dbea1
Findit <findit-for-me@appspot.gserviceaccount.com>
2020-06-02 21:37:58 +0000

    Revert "Re-land migrate md5sum tool to portable zlib utils"
    This reverts commit 00c7f91f09462399fff5e5266eeddcd6e0c9405c.
    
    Reason for revert:
    
    Findit (https://goo.gl/kROfz5) identified CL at revision 774325 as the
    culprit for failures in the build cycles as shown on:
    https://analysis.chromium.org/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzAwYzdmOTFmMDk0NjIzOTlmZmY1ZTUyNjZlZWRkY2Q2ZTBjOTQwNWMM
    
    Sample Failed Build: https://ci.chromium.org/b/8878570367609188432
    
    Sample Failed Step: compile
    
    Original change's description:
    > Re-land migrate md5sum tool to portable zlib utils
    > 
    > Use the set of portable code from zlib that shouldn't have
    > extra dependencies (e.g. 'base').
    > 
    > Also allow building the tool for other targets (e.g. Linux,
    > OSX, etc) as that allows easier testing and profiling.
    > 
    > Compilation is disabled for Windows, see crbug.com/1090428.
    > 
    > Bug: 1076580, 1087999
    > Change-Id: Ic036685029be99b06753ea18f9fc62ce7c1a6540
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225526
    > Reviewed-by: Tao Bai <michaelbai@chromium.org>
    > Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#774325}
    
    
    Change-Id: I90e7158af6e9484a63f523cc75f5bfc52288262b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1076580, 1087999
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227311
    Cr-Commit-Position: refs/heads/master@{#774345}

--- third_party/zlib/google/compression_utils_portable.h
+++ third_party/zlib/google/compression_utils_portable.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 #if defined(USE_SYSTEM_ZLIB)
 #include <zlib.h>
-/* AOSP build requires relative paths. */
 #else
 #include "zlib.h"
 #endif
