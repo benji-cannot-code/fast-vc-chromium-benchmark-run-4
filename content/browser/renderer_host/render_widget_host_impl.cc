4e7f40c0456d4127ec23424322b28a7d4fc01d46
Fergal Daly <fergal@chromium.org>
2020-06-02 05:02:33 +0000

    Remove debugging for crbug.com/1006814
    The crash has not occurred since https://crrev.com/c/2186901,
    submitted on May 12.
    
    Some of the dchecks etc remain and some todos updated to point to the
    main RenderDocument bug.
    
    Bug: 1006814
    Change-Id: Id078eed913addbc4444050dff5f76e357e8a3e40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214766
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774008}

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -517,7 +517,7 @@ void RenderWidgetHostImpl::UpdatePriority() {
 void RenderWidgetHostImpl::Init() {
   DCHECK(process_->IsInitializedAndNotDead());
 
-  SetRendererInitialized(true, RendererInitializer::kInit);
+  set_renderer_initialized(true);
 
   SendScreenRects();
   SynchronizeVisualProperties();
@@ -578,7 +578,7 @@ void RenderWidgetHostImpl::BindFrameWidgetInterfaces(
 
 void RenderWidgetHostImpl::InitForFrame() {
   DCHECK(process_->IsInitializedAndNotDead());
-  SetRendererInitialized(true, RendererInitializer::kInitForFrame);
+  set_renderer_initialized(true);
 
   if (view_)
     view_->OnRenderWidgetInit();
@@ -1937,7 +1937,7 @@ void RenderWidgetHostImpl::RendererExited() {
 
   // Clearing this flag causes us to re-create the renderer when recovering
   // from a crashed renderer.
-  SetRendererInitialized(false, RendererInitializer::kUnknown);
+  set_renderer_initialized(false);
 
   // After the renderer crashes, the view is destroyed and so the
   // RenderWidgetHost cannot track its visibility anymore. We assume such
@@ -3334,18 +3334,6 @@ gfx::Size RenderWidgetHostImpl::GetRootWidgetViewportSize() {
     return gfx::Size();
 
   return root_view->GetVisibleViewportSize();
-}
-
-void RenderWidgetHostImpl::SetRendererInitialized(
-    bool renderer_initialized,
-    RendererInitializer initializer) {
-  renderer_initialized_ = renderer_initialized;
-  // We want to record the first initializer. If this is called again, don't
-  // overwrite the existing value.
-  if (initializer != RendererInitializer::kUnknown &&
-      initializer_ != RendererInitializer::kUnknown)
-    return;
-  initializer_ = initializer;
 }
 
 }  // namespace content
