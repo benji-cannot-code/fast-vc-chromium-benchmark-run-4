e6855feb19d4fe9e9d642cfa20a44641b1ad4001
Sebastien Marchand <sebmarchand@chromium.org>
2020-06-02 15:46:29 +0000

    [PM] Tie the lifetime of the SiteDataCacheFactory to the Facade factory
    These 2 objects should always have the same lifetime (the facade factory
    is the UI thread counterpart of the PM cache factory).
    
    Bug: 1064859
    Change-Id: Id68549fce593de5d473940902f9460e4b1a72210
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2137521
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774160}

--- components/performance_manager/public/performance_manager.h
+++ components/performance_manager/public/performance_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
+#include "base/sequenced_task_runner.h"
 
 namespace content {
 class RenderFrameHost;
@@ -135,6 +136,8 @@ class PerformanceManager {
         PerformanceManagerRegisteredImpl<DerivedType>::TypeId());
     return static_cast<DerivedType*>(object);
   }
+
+  static scoped_refptr<base::SequencedTaskRunner> GetTaskRunner();
 
  protected:
   PerformanceManager();
