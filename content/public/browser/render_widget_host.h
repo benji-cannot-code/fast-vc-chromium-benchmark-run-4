e0268ad49136dde4564cfece814ad25354e5aa6d
Xiaoqian Dai <xdai@chromium.org>
2020-06-03 22:20:33 +0000

    [reland] back gesture: Disable back gesture for {touch-action:none} web pages/apps.
    Bug: 1063144
    Change-Id: Iea6a7b5ecf1089eef11237c401433ca9a59e9e65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2166360
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#773852}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227239
    Cr-Commit-Position: refs/heads/master@{#774834}

--- content/public/browser/render_widget_host.h
+++ content/public/browser/render_widget_host.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/i18n/rtl.h"
+#include "base/optional.h"
 #include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/native_web_keyboard_event.h"
@@ -29,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 class WebMouseEvent;
 class WebMouseWheelEvent;
+}
+
+namespace cc {
+enum class TouchAction;
 }
 
 namespace gfx {
@@ -278,6 +283,9 @@ class CONTENT_EXPORT RenderWidgetHost : public IPC::Sender {
 
   // Get the screen info corresponding to this render widget.
   virtual void GetScreenInfo(ScreenInfo* result) = 0;
+
+  // Get the allowed touch action corresponding to this render widget.
+  virtual base::Optional<cc::TouchAction> GetAllowedTouchAction() = 0;
 
   // Drag-and-drop drop target messages that get sent to Blink.
   virtual void DragTargetDragEnter(
