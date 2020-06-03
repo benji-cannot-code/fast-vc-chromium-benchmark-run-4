c66ef4ac4b18fbaf7a87e47ce343f726380780b9
Xing Liu <xingliu@chromium.org>
2020-06-03 21:54:12 +0000

    Download later: Add download later dialog resources.
    This CL mainly adds the download later dialog UI xml file and the string
    resources.
    
    The view logic is not implemented. A very basic instrumentation test is
    added to test the download later dialog Java code.
    
    A Jni bridge will eventually own DownloadLaterDialogCoordinator.
    
    Bug: 1078454
    Change-Id: I3508415e437371b38facd7c9ce78b3276f7fa7e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2223001
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774824}

---
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadLaterDialogProperties.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download.dialogs;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/**
+ * Contains all the properties for the download later dialog {@link PropertyModel}.
+ */
+public class DownloadLaterDialogProperties {
+    /** The selection to define when to start the download. */
+    public static final PropertyModel.WritableIntPropertyKey DOWNLOAD_TIME_SELECTION =
+            new PropertyModel.WritableIntPropertyKey();
+
+    public static final PropertyKey[] ALL_KEYS = new PropertyKey[] {DOWNLOAD_TIME_SELECTION};
+}
