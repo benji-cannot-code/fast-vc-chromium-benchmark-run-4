a733d700595d9eee077d19db11f28ef05e1e72cc
Lukasz Anforowicz <lukasza@chromium.org>
2020-06-03 21:12:36 +0000

    Rewriting |printf("%p", s.ptr_field)| into |...s.ptr_field.get()...|.
    Bug: 1069567
    Change-Id: Ib1711e1ee47ca14d448b32fb680d061c658a2eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159578
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774790}

---
+++ tools/clang/rewrite_raw_ptr_fields/tests/affected-expr-original.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class SomeClass;
+
+struct MyStruct {
+  SomeClass* ptr;
+  SomeClass* ptr2;
+};
+
+int ConvertSomeClassToInt(SomeClass* some_class) {
+  return 123;
+}
+
+void MyPrintf(const char* fmt, ...) {}
+
+void foo() {
+  MyStruct s;
+
+  // Expected rewrite: MyPrintf("%p", s.ptr.get());
+  MyPrintf("%p", s.ptr);
+
+  // Test - all arguments are rewritten.
+  // Expected rewrite: MyPrintf("%p, %p", s.ptr.get(), s.ptr2.get());
+  MyPrintf("%p, %p", s.ptr, s.ptr2);
+
+  // Test - only |s.ptr|-style arguments are rewritten.
+  // Expected rewrite: MyPrintf("%d, %p", 123, s.ptr.get());
+  MyPrintf("%d, %p", 123, s.ptr);
+
+  // Test - |s.ptr| is deeply nested.
+  // No rewrite expected.
+  MyPrintf("%d", ConvertSomeClassToInt(s.ptr));
+}
