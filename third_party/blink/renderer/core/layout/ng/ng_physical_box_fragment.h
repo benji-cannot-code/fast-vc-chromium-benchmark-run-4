59683c1135ea6673b806b24f3a2eda84bd01f236
Rob Buis <rbuis@igalia.com>
2020-06-03 09:04:18 +0000

    [mathml] Extend the MathML painter class
    Extend the MathML painter class to be more generic and offer
    an API through NGMathMLPaintInfo to paint MathML fragments
    besides fractions, i.e. the upcoming operators and radicals.
    
    Bug: 6606
    Change-Id: I8b143fc02965fa2d757b19d026d99c693dc33f5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190676
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774547}

--- third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h
+++ third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/layout/ng/geometry/ng_box_strut.h"
 #include "third_party/blink/renderer/core/layout/ng/inline/ng_fragment_items.h"
+#include "third_party/blink/renderer/core/layout/ng/mathml/ng_mathml_paint_info.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_container_fragment.h"
 #include "third_party/blink/renderer/platform/graphics/scroll_types.h"
 #include "third_party/blink/renderer/platform/wtf/casting.h"
@@ -30,6 +31,7 @@ class CORE_EXPORT NGPhysicalBoxFragment final
                         NGBoxFragmentBuilder* builder,
                         const NGPhysicalBoxStrut& borders,
                         const NGPhysicalBoxStrut& padding,
+                        std::unique_ptr<NGMathMLPaintInfo>& mathml_paint_info,
                         WritingMode block_or_line_writing_mode);
 
   scoped_refptr<const NGLayoutResult> CloneAsHiddenForPaint() const;
@@ -37,6 +39,8 @@ class CORE_EXPORT NGPhysicalBoxFragment final
   ~NGPhysicalBoxFragment() {
     if (has_fragment_items_)
       ComputeItemsAddress()->~NGFragmentItems();
+    if (ink_overflow_computed_or_mathml_paint_info_)
+      ComputeMathMLPaintInfoAddress()->~NGMathMLPaintInfo();
     for (const NGLink& child : Children())
       child.fragment->Release();
   }
@@ -153,15 +157,21 @@ class CORE_EXPORT NGPhysicalBoxFragment final
                                     bool check_same_block_size) const;
 #endif
 
+  bool HasExtraMathMLPainting() const {
+    return IsMathMLFraction() || ink_overflow_computed_or_mathml_paint_info_;
+  }
+
  private:
   const NGFragmentItems* ComputeItemsAddress() const {
-    DCHECK(has_fragment_items_ || has_borders_ || has_padding_);
+    DCHECK(has_fragment_items_ || has_borders_ || has_padding_ ||
+           ink_overflow_computed_or_mathml_paint_info_);
     const NGLink* children_end = children_ + Children().size();
     return reinterpret_cast<const NGFragmentItems*>(children_end);
   }
 
   const NGPhysicalBoxStrut* ComputeBordersAddress() const {
-    DCHECK(has_borders_ || has_padding_);
+    DCHECK(has_borders_ || has_padding_ ||
+           ink_overflow_computed_or_mathml_paint_info_);
     const NGFragmentItems* items = ComputeItemsAddress();
     if (!has_fragment_items_)
       return reinterpret_cast<const NGPhysicalBoxStrut*>(items);
@@ -170,9 +180,17 @@ class CORE_EXPORT NGPhysicalBoxFragment final
   }
 
   const NGPhysicalBoxStrut* ComputePaddingAddress() const {
-    DCHECK(has_padding_);
+    DCHECK(has_padding_ || ink_overflow_computed_or_mathml_paint_info_);
     const NGPhysicalBoxStrut* address = ComputeBordersAddress();
     return has_borders_ ? address + 1 : address;
+  }
+
+  NGMathMLPaintInfo* ComputeMathMLPaintInfoAddress() const {
+    DCHECK(ink_overflow_computed_or_mathml_paint_info_);
+    NGPhysicalBoxStrut* address =
+        const_cast<NGPhysicalBoxStrut*>(ComputePaddingAddress());
+    return has_padding_ ? reinterpret_cast<NGMathMLPaintInfo*>(address + 1)
+                        : reinterpret_cast<NGMathMLPaintInfo*>(address);
   }
 
 #if DCHECK_IS_ON()
