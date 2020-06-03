d292a2e631160b13175d0ba65fefe33ac240580b
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2020-06-03 01:58:10 +0000

    Revert "[signin] Add browser tests for the reauth dialog"
    This reverts commit 98d003741bff0b0fc76493c8c909bab6227806f4.
    
    Reason for revert:
    Both of these are failing on MSAN:
    SigninReauthViewControllerBrowserTest.CancelReauthDialog
    SigninReauthViewControllerBrowserTest.ConfirmReauthDialog
    
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests
    
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests/24165
    
    https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8878588233120522160/+/steps/browser_tests/0/logs/Deterministic_failure:_SigninReauthViewControllerBrowserTest.CancelReauthDialog__status_CRASH_/0
    
    Original change's description:
    > [signin] Add browser tests for the reauth dialog
    > 
    > This CL adds tests for the first step of the reauth flow, the reauth
    > confirmation dialog. The tests check that clicking on all buttons
    > produces expected results.
    > 
    > The tests for the second step of the reauth flow will be added in
    > follow-up CLs.
    > 
    > Bug: 1045515
    > Change-Id: I4266f29ea03080d3d6612099a0b9a49cff5cdf09
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219897
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: Alex Ilin <alexilin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#774122}
    
    TBR=droger@chromium.org,alexilin@chromium.org
    
    Change-Id: I08bfa073e1b51c4dbb3125232cc4b60880deb293
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1045515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227932
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774447}

--- chrome/browser/ui/signin_view_controller_interactive_uitest.cc
+++ chrome/browser/ui/signin_view_controller_interactive_uitest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
+#include "base/test/mock_callback.h"
 #include "build/build_config.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/reauth_result.h"
@@ -66,6 +67,19 @@ IN_PROC_BROWSER_TEST_F(SignInViewControllerBrowserTest, Accelerators) {
   wait_for_new_tab.Wait();
 
   EXPECT_EQ(2, browser()->tab_strip_model()->count());
+}
+
+IN_PROC_BROWSER_TEST_F(SignInViewControllerBrowserTest, AbortOngoingReauth) {
+  CoreAccountId account_id = signin::SetUnconsentedPrimaryAccount(
+                                 GetIdentityManager(), "alice@gmail.com")
+                                 .account_id;
+  base::MockCallback<base::OnceCallback<void(signin::ReauthResult)>>
+      reauth_callback;
+  std::unique_ptr<SigninViewController::ReauthAbortHandle> abort_handle =
+      browser()->signin_view_controller()->ShowReauthPrompt(
+          account_id, reauth_callback.Get());
+  EXPECT_CALL(reauth_callback, Run(signin::ReauthResult::kCancelled));
+  abort_handle.reset();
 }
 
 // Tests that the confirm button is focused by default in the reauth dialog.
