0c82a51ff902b6ab55e7d35c04a6a1e38ebf41a5
Hitoshi Yoshida <peria@chromium.org>
2020-06-03 05:05:24 +0000

    bindings: Fix how to check a member is missing (2/N)
    IDL dictionary provides a way to check if a member is missing or not.
    This CL fixes a wrong way of the check in TrustedTypePolicy.
    
    Bug: 839389
    Change-Id: I47206875af24089fb29fd1d92e3a82c97050ae1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227937
    Auto-Submit: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774507}

--- third_party/blink/renderer/core/trustedtypes/trusted_type_policy.cc
+++ third_party/blink/renderer/core/trustedtypes/trusted_type_policy.cc
@@ -46,7 +46,7 @@ TrustedHTML* TrustedTypePolicy::CreateHTML(v8::Isolate* isolate,
                                            const String& input,
                                            const HeapVector<ScriptValue>& args,
                                            ExceptionState& exception_state) {
-  if (!policy_options_->createHTML()) {
+  if (!policy_options_->hasCreateHTML()) {
     exception_state.ThrowTypeError(
         "Policy " + name_ +
         "'s TrustedTypePolicyOptions did not specify a 'createHTML' member.");
@@ -67,7 +67,7 @@ TrustedScript* TrustedTypePolicy::CreateScript(
     const String& input,
     const HeapVector<ScriptValue>& args,
     ExceptionState& exception_state) {
-  if (!policy_options_->createScript()) {
+  if (!policy_options_->hasCreateScript()) {
     exception_state.ThrowTypeError(
         "Policy " + name_ +
         "'s TrustedTypePolicyOptions did not specify a 'createScript' member.");
@@ -90,7 +90,7 @@ TrustedScriptURL* TrustedTypePolicy::CreateScriptURL(
     const String& input,
     const HeapVector<ScriptValue>& args,
     ExceptionState& exception_state) {
-  if (!policy_options_->createScriptURL()) {
+  if (!policy_options_->hasCreateScriptURL()) {
     exception_state.ThrowTypeError("Policy " + name_ +
                                    "'s TrustedTypePolicyOptions did not "
                                    "specify a 'createScriptURL' member.");
@@ -109,15 +109,15 @@ TrustedScriptURL* TrustedTypePolicy::CreateScriptURL(
 }
 
 bool TrustedTypePolicy::HasCreateHTML() {
-  return policy_options_->createHTML();
+  return policy_options_->hasCreateHTML();
 }
 
 bool TrustedTypePolicy::HasCreateScript() {
-  return policy_options_->createScript();
+  return policy_options_->hasCreateScript();
 }
 
 bool TrustedTypePolicy::HasCreateScriptURL() {
-  return policy_options_->createScriptURL();
+  return policy_options_->hasCreateScriptURL();
 }
 
 String TrustedTypePolicy::name() const {
