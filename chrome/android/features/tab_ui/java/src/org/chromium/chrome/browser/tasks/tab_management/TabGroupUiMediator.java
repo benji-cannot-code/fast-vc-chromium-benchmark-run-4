20cc8f4ead322315f7009366d4f95b7e7ad9bf80
Yue Zhang <yuezhanggg@chromium.org>
2020-06-03 17:33:10 +0000

    Properly destroy TabModelSelectorTabObserver in TabGroupUiMediator
    Currently, when the activity is destroyed and re-created due to
    dark/light mode switching, the TabModelSelectorTabObserver in
    TabGroupUiMediator is not properly destroyed. This CL fixes this issue
    by destroying it when tab is detached from activity.
    
    Bug: 1087826
    Change-Id: Ia11138923cb9d4e16e2ce35db18ca74afa50412f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227890
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774692}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupUiMediator.java
@@ -47,6 +47,7 @@ import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.LoadUrlParams;
+import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modelutil.PropertyModel;
 
 import java.util.ArrayList;
@@ -109,13 +110,13 @@ public class TabGroupUiMediator implements SnackbarManager.SnackbarController {
     private final TabGridDialogMediator.DialogController mTabGridDialogController;
     private final ThemeColorProvider.ThemeColorObserver mThemeColorObserver;
     private final ThemeColorProvider.TintObserver mTintObserver;
-    private final TabModelSelectorTabObserver mTabModelSelectorTabObserver;
     private final TabModelSelectorObserver mTabModelSelectorObserver;
     private final ActivityLifecycleDispatcher mActivityLifecycleDispatcher;
     private final SnackbarManager.SnackbarManageable mSnackbarManageable;
     private final Snackbar mUndoClosureSnackBar;
     private TabGroupModelFilter.Observer mTabGroupModelFilterObserver;
     private PauseResumeWithNativeObserver mPauseResumeWithNativeObserver;
+    private TabModelSelectorTabObserver mTabModelSelectorTabObserver;
     private boolean mIsTabGroupUiVisible;
     private boolean mIsShowingOverViewMode;
     private boolean mActivatedButNotShown;
@@ -261,6 +262,16 @@ public class TabGroupUiMediator implements SnackbarManager.SnackbarController {
                 // This is set to zero because the UI is hidden.
                 if (!mIsTabGroupUiVisible || numTabs == 1) numTabs = 0;
                 RecordHistogram.recordCountHistogram("TabStrip.TabCountOnPageLoad", numTabs);
+            }
+
+            @Override
+            public void onActivityAttachmentChanged(Tab tab, WindowAndroid window) {
+                // Remove this when tab is detached since the TabModelSelectorTabObserver is not
+                // properly destroyed when there is a normal/night mode switch.
+                if (window == null) {
+                    this.destroy();
+                    mTabModelSelectorTabObserver = null;
+                }
             }
         };
 
@@ -489,10 +500,12 @@ public class TabGroupUiMediator implements SnackbarManager.SnackbarController {
         if (mPauseResumeWithNativeObserver != null) {
             mActivityLifecycleDispatcher.unregister(mPauseResumeWithNativeObserver);
         }
+        if (mTabModelSelectorTabObserver != null) {
+            mTabModelSelectorTabObserver.destroy();
+        }
         mOverviewModeBehavior.removeOverviewModeObserver(mOverviewModeObserver);
         mThemeColorProvider.removeThemeColorObserver(mThemeColorObserver);
         mThemeColorProvider.removeTintObserver(mTintObserver);
-        mTabModelSelectorTabObserver.destroy();
     }
 
     private void maybeActivateConditionalTabStrip(@ReasonToShow int reason) {
