97ca6b1f5eb4e85f8aeca391e2c7b069cdbd4d4a
Minoru Chikamune <chikamune@chromium.org>
2020-06-03 04:37:36 +0000

    Supply ExecutionContext to PushMessagingBridge's HeapMojo wrappers.
    HeapMojo wrappers require ExecutionContext to reset mojo objects when ExecutionContext is detached.
    
    Bug: 1049056
    Change-Id: I8881349b0cc5870be87d5f465a12cc6eb6d9986a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220105
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774495}

--- third_party/blink/renderer/modules/push_messaging/push_messaging_bridge.cc
+++ third_party/blink/renderer/modules/push_messaging/push_messaging_bridge.cc
@@ -43,7 +43,7 @@ PushMessagingBridge* PushMessagingBridge::From(
 PushMessagingBridge::PushMessagingBridge(
     ServiceWorkerRegistration& registration)
     : Supplement<ServiceWorkerRegistration>(registration),
-      permission_service_(nullptr) {}
+      permission_service_(registration.GetExecutionContext()) {}
 
 PushMessagingBridge::~PushMessagingBridge() = default;
 
