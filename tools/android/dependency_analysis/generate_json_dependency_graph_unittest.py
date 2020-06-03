955485bf9d609795f9514259d455d5da5e8634c8
James Long <yjlong@google.com>
2020-06-03 16:33:54 +0000

    [Android] Add command-line tools for auditing dependencies
    Three command-line tools created:
    - generate_json_dependency_graph (renamed from process_jdeps)
    - print_class_dependencies
    - print_package_dependencies
    
    The README file has been updated with information about tool usage, and
    the tools can all be run with --help flags for more information.
    
    Bug: 1081895
    Change-Id: I07e5e13adbec029b54cce89e3f243526823279eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224651
    Commit-Queue: James Long <yjlong@google.com>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774672}

--- tools/android/dependency_analysis/generate_json_dependency_graph_unittest.py
+++ tools/android/dependency_analysis/generate_json_dependency_graph_unittest.py
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""Unit tests for dependency_analysis.process_jdeps."""
+"""Unit tests for dependency_analysis.generate_json_dependency_graph."""
 
 import unittest
-import process_jdeps
+import generate_json_dependency_graph
 
 
 class TestHelperFunctions(unittest.TestCase):
@@ -14,25 +14,26 @@ class TestHelperFunctions(unittest.TestCase):
     def test_class_is_interesting(self):
         """Tests that the helper identifies a valid Chromium class name."""
         self.assertTrue(
-            process_jdeps.class_is_interesting(
+            generate_json_dependency_graph.class_is_interesting(
                 'org.chromium.chrome.browser.Foo'))
 
     def test_class_is_interesting_longer(self):
         """Tests that the helper identifies a valid Chromium class name."""
         self.assertTrue(
-            process_jdeps.class_is_interesting(
+            generate_json_dependency_graph.class_is_interesting(
                 'org.chromium.chrome.browser.foo.Bar'))
 
     def test_class_is_interesting_negative(self):
         """Tests that the helper ignores a non-Chromium class name."""
         self.assertFalse(
-            process_jdeps.class_is_interesting(
+            generate_json_dependency_graph.class_is_interesting(
                 'org.notchromium.chrome.browser.Foo'))
 
     def test_class_is_interesting_not_interesting(self):
         """Tests that the helper ignores a builtin class name."""
         self.assertFalse(
-            process_jdeps.class_is_interesting('java.lang.Object'))
+            generate_json_dependency_graph.class_is_interesting(
+                'java.lang.Object'))
 
 
 class TestJavaClassJdepsParser(unittest.TestCase):
@@ -41,7 +42,7 @@ class TestJavaClassJdepsParser(unittest.TestCase):
     """
     def setUp(self):
         """Sets up a new JavaClassJdepsParser."""
-        self.parser = process_jdeps.JavaClassJdepsParser()
+        self.parser = generate_json_dependency_graph.JavaClassJdepsParser()
 
     def test_parse_line(self):
         """Tests that new nodes + edges are added after a successful parse."""
---
+++ tools/android/dependency_analysis/print_dependencies_helper.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2020 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Various helpers for printing dependencies."""
+
+from typing import List
+
+
+def get_valid_keys_matching_input(all_keys: List, input_key: str) -> List[str]:
+    """Return a list of valid keys into the graph's nodes based on an input.
+
+    For our use case (matching user input to graph nodes),
+    a valid key is one that ends with the input, case insensitive.
+    For example, 'apphooks' matches 'org.chromium.browser.AppHooks'.
+    """
+    input_key_lower = input_key.lower()
+    return [key for key in all_keys if key.lower().endswith(input_key_lower)]
