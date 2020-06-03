484ce1110a55da7d321fc2f8af14ac0d85769233
Johann <johannkoenig@google.com>
2020-06-03 23:36:40 +0000

    remove ExecutionContextLifecycleObserver from Bluetooth
    Bluetooth does not need the Observer part of the class as
    there is no cleanup done OnContextDestroyed.
    
    Add a Member<LocalDOMWindow> for the cases which need
    ExecutionContext. This also allows removing some
    To<LocalDOMWindow> casts.
    
    Change-Id: I7fac59360b213d869e9f1e19fab116d5309dc24e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2192465
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#774858}

--- third_party/blink/renderer/modules/bluetooth/bluetooth.h
+++ third_party/blink/renderer/modules/bluetooth/bluetooth.h
@@ -25,14 +25,13 @@ class ScriptPromise;
 class ScriptState;
 
 class Bluetooth final : public EventTargetWithInlineData,
-                        public ExecutionContextLifecycleObserver,
                         public PageVisibilityObserver,
                         public mojom::blink::WebBluetoothAdvertisementClient {
   DEFINE_WRAPPERTYPEINFO();
   USING_GARBAGE_COLLECTED_MIXIN(Bluetooth);
 
  public:
-  explicit Bluetooth(ExecutionContext*);
+  explicit Bluetooth(LocalDOMWindow*);
   ~Bluetooth() override;
 
   // IDL exposed interface:
@@ -57,9 +56,6 @@ class Bluetooth final : public EventTargetWithInlineData,
 
   // GC
   void Trace(Visitor*) const override;
-
-  // ExecutionContextLifecycleObserver
-  void ContextDestroyed() override {}
 
   DEFINE_ATTRIBUTE_EVENT_LISTENER(advertisementreceived, kAdvertisementreceived)
 
@@ -93,6 +89,8 @@ class Bluetooth final : public EventTargetWithInlineData,
   // Ensures only one BluetoothDevice instance represents each
   // Bluetooth device inside a single global object.
   HeapHashMap<String, Member<BluetoothDevice>> device_instance_map_;
+
+  Member<LocalDOMWindow> window_;
 
   HeapMojoAssociatedReceiverSet<mojom::blink::WebBluetoothAdvertisementClient,
                                 Bluetooth>
