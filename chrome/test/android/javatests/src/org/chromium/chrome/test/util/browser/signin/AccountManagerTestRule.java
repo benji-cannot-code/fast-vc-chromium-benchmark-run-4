19fe61deef6c08920baa55798f8a7de61b0e19a7
Alice Wang <aliceywang@chromium.org>
2020-06-03 18:11:52 +0000

    [Android] Move AccountManagerTestRule to the package of SigninTestUtil
    This CL moves AccountManagerTestRule to the package of SigninTestUtil
    so we can use SigninTestUtil.signIn/signOut methods in the rule later.
    
    Bug: 1004418
    Change-Id: I82204f8661ecda00ebbf1e741ce7ae5bec793a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209214
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774712}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/signin/AccountManagerTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/signin/AccountManagerTestRule.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.components.signin.test.util;
+package org.chromium.chrome.test.util.browser.signin;
 
 import android.accounts.Account;
 
@@ -16,6 +16,8 @@ import org.junit.runners.model.Statement;
 import org.chromium.components.signin.AccountManagerFacadeProvider;
 import org.chromium.components.signin.AccountUtils;
 import org.chromium.components.signin.ProfileDataSource;
+import org.chromium.components.signin.test.util.FakeAccountManagerFacade;
+import org.chromium.components.signin.test.util.FakeProfileDataSource;
 
 /**
  * JUnit4 rule for overriding behaviour of {@link AccountManagerFacade} for tests.
