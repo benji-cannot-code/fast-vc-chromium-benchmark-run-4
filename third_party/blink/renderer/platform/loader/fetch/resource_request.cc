6730023d46af5cca98871a48e50acd79bcafa077
Yoichi Osato <yoichio@chromium.org>
2020-06-03 00:33:00 +0000

    Reland "[fetch] upload streaming basic implementation under flag"
    This CL relands the patch crrev.com/c/2172724, which was reverted
    as crrev.com/c/2224218 due to
    1. linux-blink-cors-rel failure
    2. http/tests/loading/bad-server-subframe.html failing on mac bots
    
    I fixed #1 by updating FlagExpectations/disable-features=OutOfBlinkCors.
    For #2, it is reported flaky:
    https://analysis.chromium.org/p/chromium/flake-portal/flakes/occurrences?key=ag9zfmZpbmRpdC1mb3ItbWVyTwsSBUZsYWtlIkRjaHJvbWl1bUBibGlua193ZWJfdGVzdHNAaHR0cC90ZXN0cy9sb2FkaW5nL2JhZC1zZXJ2ZXItc3ViZnJhbWUuaHRtbAw
    (guessed crrev.com/c/2219088 the culprit). Now this CL passes the bots.
    
    Bug: 688906
    Cq-Include-Trybots: chromium/try:mac10.10-blink-rel,mac10.11-blink-rel
    Change-Id: I9d64f917014096f79b224369fc719e9743086b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2226103
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774418}

--- third_party/blink/renderer/platform/loader/fetch/resource_request.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_request.cc
@@ -98,14 +98,19 @@ ResourceRequestBody::ResourceRequestBody(
     : form_body_(form_body) {}
 
 ResourceRequestBody::ResourceRequestBody(ResourceRequestBody&& src)
-    : ResourceRequestBody(std::move(src.form_body_)) {}
+    : form_body_(std::move(src.form_body_)),
+      stream_body_(std::move(src.stream_body_)) {}
 
-ResourceRequestBody& ResourceRequestBody::operator=(ResourceRequestBody&& src) {
-  form_body_ = std::move(src.form_body_);
-  return *this;
-}
+ResourceRequestBody& ResourceRequestBody::operator=(ResourceRequestBody&& src) =
+    default;
 
 ResourceRequestBody::~ResourceRequestBody() = default;
+
+void ResourceRequestBody::SetStreamBody(
+    mojo::PendingRemote<network::mojom::blink::ChunkedDataPipeGetter>
+        stream_body) {
+  stream_body_ = std::move(stream_body);
+}
 
 ResourceRequest::ResourceRequest() : ResourceRequestHead(NullURL()) {}
 
@@ -118,6 +123,8 @@ ResourceRequest::ResourceRequest(const ResourceRequestHead& head)
     : ResourceRequestHead(head) {}
 
 ResourceRequest& ResourceRequest::operator=(const ResourceRequest& src) {
+  DCHECK(!body_.StreamBody().is_valid());
+  DCHECK(!src.body_.StreamBody().is_valid());
   this->ResourceRequestHead::operator=(src);
   body_.SetFormBody(src.body_.FormBody());
   return *this;
@@ -130,6 +137,8 @@ ResourceRequest& ResourceRequest::operator=(ResourceRequest&&) = default;
 ResourceRequest::~ResourceRequest() = default;
 
 void ResourceRequest::CopyFrom(const ResourceRequest& src) {
+  DCHECK(!body_.StreamBody().is_valid());
+  DCHECK(!src.body_.StreamBody().is_valid());
   *this = src;
 }
 
