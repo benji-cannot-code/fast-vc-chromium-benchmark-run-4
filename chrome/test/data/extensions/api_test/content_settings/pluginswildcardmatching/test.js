bf54179ea5d05279d1ee44332ff98e7c0ea53849
Ravjit Singh Uppal <ravjit@chromium.org>
2020-06-03 11:47:45 +0000

    Api test for plugins wildcard matching
    Bug: 1073883
    Change-Id: I98964fe27b8a3f5bc247291e0b810e5e74c04a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224863
    Commit-Queue: Ravjit Singh Uppal <ravjit@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774575}

---
+++ chrome/test/data/extensions/api_test/content_settings/pluginswildcardmatching/test.js
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var cs = chrome.contentSettings;
+
+function expect(expected, message) {
+  return chrome.test.callbackPass(function(value) {
+    chrome.test.assertEq(expected, value, message);
+  });
+}
+
+chrome.test.runTests([function setAndCheckContentSettings() {
+  cs['plugins'].set({
+    'primaryPattern': 'http://drive.google.com:443/*',
+    'secondaryPattern': '<all_urls>',
+    'setting': 'allow'
+  });
+  cs['plugins'].set({
+    'primaryPattern': 'http://*.google.com:443/*',
+    'secondaryPattern': '<all_urls>',
+    'setting': 'allow'
+  });
+  cs['plugins'].get(
+      {'primaryUrl': 'http://drive.google.com:443/'},
+      expect(
+          {'setting': 'allow'},
+          'Flash should be allowed on this ' +
+              'page'));
+  cs['plugins'].get(
+      {'primaryUrl': 'http://mail.google.com:443/'},
+      expect(
+          {'setting': 'block'},
+          'Flash should be blocked on this page' +
+              ' because wildcards are not allowed'));
+}]);
