7bcf97f723e461e951fcc1ffa2026e2f39b53e56
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-06-03 23:08:49 +0000

    [b4p/Settings] Prepare for chrome://settings/passwords/device
    This CL prepares for the introduction of the new page, in order to make
    the diff smaller when the page functionality is actually added. It adds
    the necessary strings, the new '/device' route and the settings-subpage
    component where the new page will be injected.
    
    The route only exists for users with the kEnablePasswordsAccountStorage
    feature enabled. The page is not searchable from settings. This is to
    avoid users who are not eligible/opted-in to b4p accessing it by
    mistake. The page can still be accessed by those users if they type the
    URL on the omnibar, as happens for other pages in Settings.
    
    Bug: 1049141
    Change-Id: I726549953b28abf9da0d764629d0fab529d9b877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228618
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774848}

--- chrome/browser/resources/settings/settings_routes.js
+++ chrome/browser/resources/settings/settings_routes.js
@@ -34,6 +34,7 @@ import {Route} from './router.m.js';
  *   MANAGE_PROFILE: !Route,
  *   ON_STARTUP: !Route,
  *   PASSWORDS: !Route,
+ *   DEVICE_PASSWORDS: !Route,
  *   PAYMENTS: !Route,
  *   PEOPLE: !Route,
  *   PRINTING: !Route,
