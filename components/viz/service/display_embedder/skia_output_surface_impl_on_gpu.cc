4ee1987ec0a8bca29a0f118b8e138d0a1684550f
Rafael Cintron <rafael.cintron@microsoft.com>
2020-06-03 19:21:13 +0000

    Make SkiaRenderer/Dawn/D3D12 run within GPU sandbox
    D3D12 requires that we use flip model swap chains. Flip swap chains
    require that the swap chain be connected with DWM. DWM requires that
    the rendering windows are owned by the process that's currently doing
    the rendering.
    
    To workaround this problem, we use gl::ChildWindowWin to create a window
    that is reparented by the browser to be a child of its window.
    
    Bug: 1071458
    Change-Id: I212df12ec21f3fbc00e08b63a741b05b14771c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2223371
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774742}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -84,7 +84,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(SKIA_USE_DAWN)
 #include "components/viz/common/gpu/dawn_context_provider.h"
+#if defined(OS_WIN)
 #include "components/viz/service/display_embedder/skia_output_device_dawn.h"
+#endif
 #endif
 
 namespace viz {
@@ -1627,11 +1629,20 @@ bool SkiaOutputSurfaceImplOnGpu::InitializeForDawn() {
     output_device_ = std::make_unique<SkiaOutputDeviceX11>(
         context_state_, dependency_->GetSurfaceHandle(), memory_tracker_.get(),
         did_swap_buffer_complete_callback_);
+#elif defined(OS_WIN)
+    std::unique_ptr<SkiaOutputDeviceDawn> output_device =
+        std::make_unique<SkiaOutputDeviceDawn>(
+            dawn_context_provider_, dependency_->GetSurfaceHandle(),
+            gfx::SurfaceOrigin::kTopLeft, memory_tracker_.get(),
+            did_swap_buffer_complete_callback_);
+    const gpu::SurfaceHandle child_surface_handle =
+        output_device->GetChildSurfaceHandle();
+    DidCreateAcceleratedSurfaceChildWindow(dependency_->GetSurfaceHandle(),
+                                           child_surface_handle);
+    output_device_ = std::move(output_device);
 #else
-    output_device_ = std::make_unique<SkiaOutputDeviceDawn>(
-        dawn_context_provider_, dependency_->GetSurfaceHandle(),
-        gfx::SurfaceOrigin::kTopLeft, memory_tracker_.get(),
-        did_swap_buffer_complete_callback_);
+    NOTREACHED();
+    return false;
 #endif
   }
 #endif
