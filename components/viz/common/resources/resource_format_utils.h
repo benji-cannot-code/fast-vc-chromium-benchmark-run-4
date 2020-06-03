743d950e3c1383a15ffdf9eaa949d462829276ff
Christopher Cameron <ccameron@chromium.org>
2020-06-03 22:50:47 +0000

    SkiaRenderer/macOS: Make Metal work
    Create a viz::ToMTLPixelFormat helper function, and use it in the
    shared image factory, and in the output surface allocation.
    
    Update the default gfx::BufferFormat for SkiaOutputDeviceBufferQueue
    for macOS, and add a TODO noting that the correct value should be
    determined at the Reshape call, and not at initialization.
    
    This is enough to get --enable-features=SkiaRenderer,Metal to draw
    the browser and some simple pages.
    
    Bug: 894929
    Change-Id: I3fde39349fecaea856293609e5b877c49e46d0e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225485
    Commit-Queue: ccameron <ccameron@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774843}

--- components/viz/common/resources/resource_format_utils.h
+++ components/viz/common/resources/resource_format_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_VIZ_COMMON_RESOURCES_RESOURCE_FORMAT_UTILS_H_
 #define COMPONENTS_VIZ_COMMON_RESOURCES_RESOURCE_FORMAT_UTILS_H_
 
+#include "build/build_config.h"
 #include "components/viz/common/resources/resource_format.h"
 #include "components/viz/common/viz_resource_format_export.h"
 #include "gpu/vulkan/buildflags.h"
@@ -73,6 +74,10 @@ VIZ_RESOURCE_FORMAT_EXPORT wgpu::TextureFormat ToDawnFormat(
 // WGPUTextureFormat instead.
 VIZ_RESOURCE_FORMAT_EXPORT WGPUTextureFormat
 ToWGPUFormat(ResourceFormat format);
+
+#if defined(OS_MACOSX)
+VIZ_RESOURCE_FORMAT_EXPORT unsigned int ToMTLPixelFormat(ResourceFormat format);
+#endif
 
 }  // namespace viz
 
