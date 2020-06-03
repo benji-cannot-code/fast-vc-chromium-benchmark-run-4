7356bbbedd82e82499250df5551910a85be016b3
Antonio Sartori <antoniosartori@chromium.org>
2020-06-03 12:56:39 +0000

    Fix regression in system_health.memory_mobile
    https://crrev.com/770126 introduced a regression in memory usage. I
    think this regression was due to storing the whole redirect chain of a
    ResourceRequestHead alongside with it. Although this seemed like a
    clean idea from the code point of view, this causes heap allocation
    for information which we do not really need. In this change I get rid
    of it and only store the first and previous urls in the
    ResourceRequestHead.
    
    Change-Id: I2ee5c122142d732d1496e6606615d83e028581f4
    Bug: 1084972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214542
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774588}

--- third_party/blink/renderer/core/loader/base_fetch_context.h
+++ third_party/blink/renderer/core/loader/base_fetch_context.h
@@ -38,7 +38,7 @@ class CORE_EXPORT BaseFetchContext : public FetchContext {
       const KURL&,
       const ResourceLoaderOptions&,
       ReportingDisposition,
-      const Vector<KURL>&) const override;
+      const base::Optional<ResourceRequest::RedirectInfo>&) const override;
   base::Optional<ResourceRequestBlockedReason> CheckCSPForRequest(
       mojom::RequestContextType,
       network::mojom::RequestDestination request_destination,
@@ -79,7 +79,7 @@ class CORE_EXPORT BaseFetchContext : public FetchContext {
   // registration endpoint.
   virtual bool SendConversionRequestInsteadOfRedirecting(
       const KURL& url,
-      const Vector<KURL>& redirect_chain,
+      const base::Optional<ResourceRequest::RedirectInfo>& redirect_info,
       ReportingDisposition reporting_disposition) const;
 
   virtual const ContentSecurityPolicy* GetContentSecurityPolicy() const = 0;
@@ -104,7 +104,7 @@ class CORE_EXPORT BaseFetchContext : public FetchContext {
   virtual bool IsSVGImageChromeClient() const = 0;
   virtual bool ShouldBlockFetchByMixedContentCheck(
       mojom::blink::RequestContextType request_context,
-      const Vector<KURL>& redirect_chain,
+      const base::Optional<ResourceRequest::RedirectInfo>& redirect_info,
       const KURL& url,
       ReportingDisposition reporting_disposition,
       const base::Optional<String>& devtools_id) const = 0;
@@ -129,7 +129,7 @@ class CORE_EXPORT BaseFetchContext : public FetchContext {
       const KURL&,
       const ResourceLoaderOptions&,
       ReportingDisposition,
-      const Vector<KURL>& redirect_chain) const;
+      const base::Optional<ResourceRequest::RedirectInfo>& redirect_info) const;
 
   base::Optional<ResourceRequestBlockedReason> CheckCSPForRequestInternal(
       mojom::RequestContextType,
