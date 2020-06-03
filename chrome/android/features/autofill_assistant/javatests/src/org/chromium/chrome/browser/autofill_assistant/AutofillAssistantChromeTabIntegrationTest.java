648f4e8332ca639cb3ee678ccbf3b9f823a448ed
sandromaggi <sandromaggi@google.com>
2020-06-03 09:42:49 +0000

    [Autofill Assistant] Re-enable integration test
    This test was failing as a result of crbug/1090314. Re-enabling this
    test since the cause has been fixed.
    
    Bug: 1090215
    Change-Id: I60ec44e694ef35a408712ee8d0d8e725b57a7e36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228838
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Cr-Commit-Position: refs/heads/master@{#774558}

--- chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantChromeTabIntegrationTest.java
+++ chrome/android/features/autofill_assistant/javatests/src/org/chromium/chrome/browser/autofill_assistant/AutofillAssistantChromeTabIntegrationTest.java
@@ -420,7 +420,6 @@ public class AutofillAssistantChromeTabIntegrationTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1090215")
     public void interactingWithLocationBarHidesAutofillAssistant() {
         ArrayList<ActionProto> list = new ArrayList<>();
         list.add((ActionProto) ActionProto.newBuilder()
