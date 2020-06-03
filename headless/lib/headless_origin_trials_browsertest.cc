29ccd50c7c4180bf86956d78dcdf19a7cb9295e0
Francois Doray <fdoray@chromium.org>
2020-06-03 14:54:46 +0000

    [sheriff] Disable headless tests on Win Dbg.
    Affected tests:
    HeadlessOriginTrialsBrowserTest.TrialsDisabledByDefault
    HeadlessOriginTrialsBrowserTest.WebComponentsV0CustomElements
    HeadlessOriginTrialsBrowserTest.TrialsCanBeEnabled
    HeadlessWebContentsScreenshotWindowPositionTests/HeadlessWebContentsScreenshotWindowPositionTest.RunAsyncTest/0
    HeadlessWebContentsScreenshotWindowPositionTests/HeadlessWebContentsScreenshotWindowPositionTest.RunAsyncTest/1
    HeadlessWebContentsTaggedPDFTest.RunAsyncTest
    
    TBR=fdoray@chromium.org
    
    Bug: 1090801
    Change-Id: I28769af93ffc772a36a3104f3e16c1c758a956a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229062
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: François Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774626}

--- headless/lib/headless_origin_trials_browsertest.cc
+++ headless/lib/headless_origin_trials_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/strcat.h"
 #include "base/test/bind_test_util.h"
+#include "build/build_config.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/url_loader_interceptor.h"
@@ -54,7 +55,14 @@ class HeadlessOriginTrialsBrowserTest : public HeadlessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(HeadlessOriginTrialsBrowserTest);
 };
 
-IN_PROC_BROWSER_TEST_F(HeadlessOriginTrialsBrowserTest, TrialsCanBeEnabled) {
+// Flaky on Windows Debug https://crbug.com/1090801
+#if defined(OS_WIN) && !defined(NDEBUG)
+#define MAYBE_TrialsCanBeEnabled DISABLED_TrialsCanBeEnabled
+#else
+#define MAYBE_TrialsCanBeEnabled TrialsCanBeEnabled
+#endif
+IN_PROC_BROWSER_TEST_F(HeadlessOriginTrialsBrowserTest,
+                       MAYBE_TrialsCanBeEnabled) {
   HeadlessBrowserContext* browser_context =
       browser()->CreateBrowserContextBuilder().Build();
 
@@ -77,8 +85,14 @@ IN_PROC_BROWSER_TEST_F(HeadlessOriginTrialsBrowserTest, TrialsCanBeEnabled) {
                   ->GetBool());
 }
 
+// Flaky on Windows Debug https://crbug.com/1090801
+#if defined(OS_WIN) && !defined(NDEBUG)
+#define MAYBE_TrialsDisabledByDefault DISABLED_TrialsDisabledByDefault
+#else
+#define MAYBE_TrialsDisabledByDefault TrialsDisabledByDefault
+#endif
 IN_PROC_BROWSER_TEST_F(HeadlessOriginTrialsBrowserTest,
-                       TrialsDisabledByDefault) {
+                       MAYBE_TrialsDisabledByDefault) {
   HeadlessBrowserContext* browser_context =
       browser()->CreateBrowserContextBuilder().Build();
 
@@ -100,8 +114,15 @@ IN_PROC_BROWSER_TEST_F(HeadlessOriginTrialsBrowserTest,
           ->GetBool());
 }
 
+// Flaky on Windows Debug https://crbug.com/1090801
+#if defined(OS_WIN) && !defined(NDEBUG)
+#define MAYBE_WebComponentsV0CustomElements \
+  DISABLED_WebComponentsV0CustomElements
+#else
+#define MAYBE_WebComponentsV0CustomElements WebComponentsV0CustomElements
+#endif
 IN_PROC_BROWSER_TEST_F(HeadlessOriginTrialsBrowserTest,
-                       WebComponentsV0CustomElements) {
+                       MAYBE_WebComponentsV0CustomElements) {
   HeadlessBrowserContext* browser_context =
       browser()->CreateBrowserContextBuilder().Build();
 
