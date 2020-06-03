8d62199c783b86960475d41efe5865c01e40f19c
Dominic Farolino <dom@chromium.org>
2020-06-03 22:51:54 +0000

    Remove kLazyLoad and kLoadingFrameDefaultEager feature policies
    Bug: 1079372
    Change-Id: Ie1a116405cf89a4fa07e1716ec36170c75ba55fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190034
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774844}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -1562,7 +1562,6 @@ class CORE_EXPORT Document : public ContainerNode,
   }
 
   bool IsVerticalScrollEnforced() const { return is_vertical_scroll_enforced_; }
-  bool IsLazyLoadPolicyEnforced() const;
   bool IsFocusAllowed() const;
 
   NavigationInitiatorImpl& NavigationInitiator();
