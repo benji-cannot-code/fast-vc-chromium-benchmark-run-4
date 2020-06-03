c60551b3dec8f70f0009acc8e0482c7ee4487ab8
Yusuke Sato <yusukes@chromium.org>
2020-06-03 18:58:16 +0000

    arcvm: Don't generate /run/chrome/is_arcvm
    The file is unused now.
    
    BUG=b:154633986
    TEST=arc.Boot[.vm]
    
    Change-Id: I70b375feb99e47e8dc9a007265a8a2b1eca1eac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219567
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Commit-Queue: Yusuke Sato <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774733}

--- chrome/browser/chromeos/arc/session/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/session/arc_service_launcher.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/check_op.h"
 #include "base/files/file_util.h"
-#include "base/task/post_task.h"
-#include "base/task/thread_pool.h"
 #include "chrome/browser/apps/app_service/arc_apps_factory.h"
 #include "chrome/browser/chromeos/apps/apk_web_app_service.h"
 #include "chrome/browser/chromeos/arc/accessibility/arc_accessibility_helper_bridge.h"
@@ -88,10 +86,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace arc {
 namespace {
 
-// The file that specifies if the environment is ARCVM or ARC. It contains 1 in
-// ARCVM and 0 in ARC.
-constexpr base::FilePath::CharType kIsArcVm[] = "/run/chrome/is_arcvm";
-
 // ChromeBrowserMainPartsChromeos owns.
 ArcServiceLauncher* g_arc_service_launcher = nullptr;
 
@@ -111,14 +105,6 @@ ArcServiceLauncher::ArcServiceLauncher(
       scheduler_configuration_manager_(scheduler_configuration_manager) {
   DCHECK(g_arc_service_launcher == nullptr);
   g_arc_service_launcher = this;
-
-  // Write kIsArcVm file to be 1 or 0.
-  base::ThreadPool::PostTask(
-      FROM_HERE, {base::MayBlock()},
-      base::BindOnce([](const base::FilePath& filename, const char* data,
-                        int size) { base::WriteFile(filename, data, size); },
-                     base::FilePath(kIsArcVm),
-                     arc::IsArcVmEnabled() ? "1" : "0", 1));
 }
 
 ArcServiceLauncher::~ArcServiceLauncher() {
