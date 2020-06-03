9018b8cc0ade30a7650ff23699f33170527781fc
Scott Violet <sky@chromium.org>
2020-06-03 00:55:06 +0000

    android: separates GestureStateListener into two interfaces
    To fully support onScrollOffsetOrExtentChanged() comes at a performance
    cost. As very few places actually need this, this creates a new interface
    with onScrollOffsetOrExtentChanged() and converts the places that need it
    to the new interface.
    
    BUG=1087480
    TEST=none
    
    Change-Id: I1f54bdf74ee0b6006fc92284bc761ed6992a0d55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225548
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774422}

--- chrome/android/java/src/org/chromium/chrome/browser/banners/SwipableOverlayView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/banners/SwipableOverlayView.java
@@ -22,7 +22,7 @@ import androidx.annotation.IntDef;
 
 import org.chromium.base.MathUtils;
 import org.chromium.content_public.browser.GestureListenerManager;
-import org.chromium.content_public.browser.GestureStateListener;
+import org.chromium.content_public.browser.GestureStateListenerWithScroll;
 import org.chromium.content_public.browser.WebContents;
 
 import java.lang.annotation.Retention;
@@ -59,7 +59,7 @@ public abstract class SwipableOverlayView extends FrameLayout {
     private static final long ANIMATION_DURATION_MS = 250;
 
     /** Detects when the user is dragging the WebContents. */
-    private final GestureStateListener mGestureStateListener;
+    private final GestureStateListenerWithScroll mGestureStateListener;
 
     /** Listens for changes in the layout. */
     private final View.OnLayoutChangeListener mLayoutChangeListener;
@@ -203,10 +203,10 @@ public abstract class SwipableOverlayView extends FrameLayout {
     /**
      * Creates a listener than monitors the WebContents for scrolls and flings.
      * The listener updates the location of this View to account for the user's gestures.
-     * @return GestureStateListener to send to the WebContents.
+     * @return GestureStateListenerWithScroll to send to the WebContents.
      */
-    private GestureStateListener createGestureStateListener() {
-        return new GestureStateListener() {
+    private GestureStateListenerWithScroll createGestureStateListener() {
+        return new GestureStateListenerWithScroll() {
             /** Tracks the previous event's scroll offset to determine if a scroll is up or down. */
             private int mLastScrollOffsetY;
 
