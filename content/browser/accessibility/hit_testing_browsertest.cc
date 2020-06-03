7ec1ddb7de9113d1db0099d591171e9edeb7d757
Dominic Mazzoni <dmazzoni@chromium.org>
2020-06-03 17:43:59 +0000

    Add async accessibility hit testing API
    Previously, in order to do a hit test for accessibility we'd
    send an action to the renderer with the x,y coordinates, and
    the renderer would fire an accessibility event on the matching
    node.
    
    It'd be more flexible if we had an API that could do a hit
    test and call a callback function with the result. That
    eliminates the need for keeping track of requests and matching
    them up with events.
    
    This patch refactors the way hit testing works so that a
    hit testing request may contain a request for an accessibility
    event to fire, or a callback function to call, or both.
    
    Existing tests are extended to cover both mechanisms.
    
    Bug: 1083834
    
    
    Change-Id: Idb4c1f302361a0eb6d6344bcee9da7f086caf3c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2209630
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Auto-Submit: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774697}

--- content/browser/accessibility/hit_testing_browsertest.cc
+++ content/browser/accessibility/hit_testing_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/accessibility/hit_testing_browsertest.h"
 
 #include "base/check.h"
+#include "base/test/bind_test_util.h"
 #include "build/build_config.h"
 #include "build/chromecast_buildflags.h"
 #include "content/browser/accessibility/accessibility_tree_formatter_blink.h"
@@ -139,7 +140,8 @@ AccessibilityHitTestingBrowserTest::HitTestAndWaitForResultWithEvent(
   action_data.action = ax::mojom::Action::kHitTest;
   action_data.target_point = CSSToFramePoint(point);
   action_data.hit_test_event_to_fire = event_to_fire;
-  manager->delegate()->AccessibilityPerformAction(action_data);
+  manager->delegate()->AccessibilityHitTest(CSSToFramePoint(point),
+                                            event_to_fire, 0, {});
   event_waiter.WaitForNotification();
 
   RenderFrameHostImpl* target_frame = event_waiter.event_render_frame_host();
@@ -154,6 +156,30 @@ BrowserAccessibility*
 AccessibilityHitTestingBrowserTest::HitTestAndWaitForResult(
     const gfx::Point& point) {
   return HitTestAndWaitForResultWithEvent(point, ax::mojom::Event::kHover);
+}
+
+BrowserAccessibility*
+AccessibilityHitTestingBrowserTest::AsyncHitTestAndWaitForCallback(
+    const gfx::Point& point) {
+  BrowserAccessibilityManager* manager = GetRootBrowserAccessibilityManager();
+
+  gfx::Point target_point = CSSToFramePoint(point);
+  base::RunLoop run_loop;
+  BrowserAccessibilityManager* hit_manager = nullptr;
+  int hit_node_id = 0;
+
+  auto callback = [&](BrowserAccessibilityManager* manager, int node_id) {
+    hit_manager = manager;
+    hit_node_id = node_id;
+    run_loop.QuitClosure().Run();
+  };
+  manager->delegate()->AccessibilityHitTest(
+      target_point, ax::mojom::Event::kNone, 0,
+      base::BindLambdaForTesting(callback));
+  run_loop.Run();
+
+  BrowserAccessibility* hit_node = hit_manager->GetFromID(hit_node_id);
+  return hit_node;
 }
 
 BrowserAccessibility*
@@ -363,6 +389,10 @@ IN_PROC_BROWSER_TEST_P(AccessibilityHitTestingBrowserTest, HitTest) {
     BrowserAccessibility* expected_node =
         FindNode(ax::mojom::Role::kGenericContainer, "rect2");
     EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_2_point, expected_node, hit_node);
+
+    // Try callback API.
+    hit_node = AsyncHitTestAndWaitForCallback(rect_2_point);
+    EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_2_point, expected_node, hit_node);
   }
 
   // Test a hit on a rect in the iframe.
@@ -371,6 +401,10 @@ IN_PROC_BROWSER_TEST_P(AccessibilityHitTestingBrowserTest, HitTest) {
     BrowserAccessibility* hit_node = HitTestAndWaitForResult(rect_b_point);
     BrowserAccessibility* expected_node =
         FindNode(ax::mojom::Role::kGenericContainer, "rectB");
+    EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_b_point, expected_node, hit_node);
+
+    // Try callback API.
+    hit_node = AsyncHitTestAndWaitForCallback(rect_b_point);
     EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_b_point, expected_node, hit_node);
 
     // Test with a different event.
@@ -401,7 +435,14 @@ IN_PROC_BROWSER_TEST_P(AccessibilityHitTestingBrowserTest,
   EXPECT_TRUE(NavigateToURL(shell(), url));
   waiter.WaitForNotification();
 
-  BrowserAccessibility* hit_node = HitTestAndWaitForResult(gfx::Point(-1, -1));
+  gfx::Point out_of_bounds_point(-1, -1);
+
+  BrowserAccessibility* hit_node = HitTestAndWaitForResult(out_of_bounds_point);
+  ASSERT_TRUE(hit_node != nullptr);
+  ASSERT_EQ(ax::mojom::Role::kRootWebArea, hit_node->GetRole());
+
+  // Try callback API.
+  hit_node = AsyncHitTestAndWaitForCallback(out_of_bounds_point);
   ASSERT_TRUE(hit_node != nullptr);
   ASSERT_EQ(ax::mojom::Role::kRootWebArea, hit_node->GetRole());
 }
@@ -449,6 +490,10 @@ IN_PROC_BROWSER_TEST_P(AccessibilityHitTestingCrossProcessBrowserTest,
     BrowserAccessibility* expected_node =
         FindNode(ax::mojom::Role::kGenericContainer, "rectB");
     EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_b_point, expected_node, hit_node);
+
+    // Try callback API.
+    hit_node = AsyncHitTestAndWaitForCallback(rect_b_point);
+    EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_b_point, expected_node, hit_node);
   }
 
   // Scroll div up 100px.
@@ -468,6 +513,10 @@ IN_PROC_BROWSER_TEST_P(AccessibilityHitTestingCrossProcessBrowserTest,
     BrowserAccessibility* hit_node = HitTestAndWaitForResult(rect_g_point);
     BrowserAccessibility* expected_node =
         FindNode(ax::mojom::Role::kGenericContainer, "rectG");
+    EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_g_point, expected_node, hit_node);
+
+    // Try callback API.
+    hit_node = AsyncHitTestAndWaitForCallback(rect_g_point);
     EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_g_point, expected_node, hit_node);
   }
 }
@@ -596,6 +645,10 @@ IN_PROC_BROWSER_TEST_P(AccessibilityHitTestingBrowserTest,
     BrowserAccessibility* expected_node =
         FindNode(ax::mojom::Role::kGenericContainer, "rect2");
     EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_2_point, expected_node, hit_node);
+
+    // Try callback API.
+    hit_node = AsyncHitTestAndWaitForCallback(rect_2_point);
+    EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_2_point, expected_node, hit_node);
   }
 
   // Test a hit on a rect in the iframe.
@@ -604,6 +657,10 @@ IN_PROC_BROWSER_TEST_P(AccessibilityHitTestingBrowserTest,
     BrowserAccessibility* hit_node = HitTestAndWaitForResult(rect_b_point);
     BrowserAccessibility* expected_node =
         FindNode(ax::mojom::Role::kGenericContainer, "rectB");
+    EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_b_point, expected_node, hit_node);
+
+    // Try callback API.
+    hit_node = AsyncHitTestAndWaitForCallback(rect_b_point);
     EXPECT_ACCESSIBILITY_HIT_TEST_RESULT(rect_b_point, expected_node, hit_node);
   }
 }
