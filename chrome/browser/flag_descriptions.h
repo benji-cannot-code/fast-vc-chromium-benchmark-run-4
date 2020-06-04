ec5e3a23eed2a7e38530cc01115344e4d3f360f5
Behnood Momenzadeh <behnoodm@google.com>
2020-06-04 18:35:35 +0000

    [HouseCall] Add a feature flag to use lookalikes to suggest navigations.
    This CL will add a flag to enable suggesting navigations based on
    lookalikes. Lookalikes are currently used to detect spoofs, but it could
    be utilized to suggest navigations to users who might have typos in
    their navigation URL.
    
    Bug: 1076154
    Change-Id: I4daf8061a3b52db970588f8a16b7fa9d19e38628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228985
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Behnood Momenzadeh <behnoodm@google.com>
    Cr-Commit-Position: refs/heads/master@{#775193}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -101,6 +101,9 @@ extern const char kAudioWorkletRealtimeThreadDescription[];
 extern const char kUpdatedCellularActivationUiName[];
 extern const char kUpdatedCellularActivationUiDescription[];
 
+extern const char kUseLookalikesForNavigationSuggestionsName[];
+extern const char kUseLookalikesForNavigationSuggestionsDescription[];
+
 extern const char kUseMessagesStagingUrlName[];
 extern const char kUseMessagesStagingUrlDescription[];
 
