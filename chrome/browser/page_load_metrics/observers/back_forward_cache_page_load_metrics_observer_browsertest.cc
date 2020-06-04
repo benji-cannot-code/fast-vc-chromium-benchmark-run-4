24b996f6f1bd9195add44f36046ff23825dcb4fb
Jonathan Ross <jonross@chromium.org>
2020-06-04 18:45:12 +0000

    Disabling flaky BackForwardCachePageLoadMetricsObserverBrowserTest
    On Mac we are seeing FirstPaintAfterBackForwardCacheRestore flake.
    This disables the test on just Mac until the root cause can be
    addressed
    
    TBR=npm@chromium.org
    TEST=BackForwardCachePageLoadMetricsObserverBrowserTest.FirstPaintAfterBackForwardCacheRestore
    
    Bug: 1075936
    Change-Id: Ia6ad11517e81edb9d3832468e8db4881eec7dd26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231284
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Nicolás Peña Moreno <npm@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775201}

--- chrome/browser/page_load_metrics/observers/back_forward_cache_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/back_forward_cache_page_load_metrics_observer_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -61,8 +62,13 @@ class BackForwardCachePageLoadMetricsObserverBrowserTest
 
 }  // namespace
 
+#if defined(OS_MACOSX)
+#define MAYBE_FirstPaintAfterBackForwardCacheRestore DISABLED_FirstPaintAfterBackForwardCacheRestore
+#else
+#define MAYBE_FirstPaintAfterBackForwardCacheRestore FirstPaintAfterBackForwardCacheRestore
+#endif
 IN_PROC_BROWSER_TEST_F(BackForwardCachePageLoadMetricsObserverBrowserTest,
-                       FirstPaintAfterBackForwardCacheRestore) {
+                       MAYBE_FirstPaintAfterBackForwardCacheRestore) {
   ASSERT_TRUE(embedded_test_server()->Start());
   GURL url_a(embedded_test_server()->GetURL("a.com", "/title1.html"));
   GURL url_b(embedded_test_server()->GetURL("b.com", "/title1.html"));
