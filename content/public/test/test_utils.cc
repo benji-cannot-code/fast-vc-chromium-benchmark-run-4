919a82f6eb0152ec629ad347420882c8c1fa2d42
Rakina Zata Amni <rakina@chromium.org>
2020-06-04 05:33:05 +0000

    Add SameSite level to ProactivelySwapBrowsingInstance
    Adds the "SameSite" level to the ProactivelySwapBrowsingInstance feature
    flag. This CL also adds helper functions (to parametrize tests, etc.).
    The implementation for same-site proactive BI swap is done in another
    CL - crrev.com/c/2121522.
    Non-content uses of the functions: crrev.com/c/2210605,
    crrev.com/c/2196105, and a few others.
    
    For more details, see doc: https://docs.google.com/document/d/1lHdkKLUe8H6ZP6ALwj-dsus7oYcuc93HkSCHCcerItg/edit?usp=sharing
    
    Bug: 977562
    Change-Id: I283ad31297ce6dd93bb6f02e8159488d62c5d989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214588
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774980}

--- content/public/test/test_utils.cc
+++ content/public/test/test_utils.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "content/browser/frame_host/render_frame_host_delegate.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
+#include "content/common/content_navigation_policy.h"
 #include "content/public/browser/browser_child_process_host_iterator.h"
 #include "content/public/browser/browser_plugin_guest_delegate.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -208,6 +209,12 @@ bool AreDefaultSiteInstancesEnabled() {
 
 void IsolateAllSitesForTesting(base::CommandLine* command_line) {
   command_line->AppendSwitch(switches::kSitePerProcess);
+}
+
+bool CanSameSiteMainFrameNavigationsChangeRenderFrameHosts() {
+  // TODO(crbug.com/936696): Also return true when RenderDocument for main frame
+  // is enabled.
+  return IsProactivelySwapBrowsingInstanceOnSameSiteNavigationEnabled();
 }
 
 GURL GetWebUIURL(const std::string& host) {
