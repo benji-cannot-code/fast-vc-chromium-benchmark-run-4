3151eb73a8b2d7b53003842ae4d185e8256ac842
Stephen McGruer <smcgruer@chromium.org>
2020-06-04 02:59:20 +0000

    Disable h2 server from running via run_blink_wptserve.py
    The h2 server turns itself on by default in wptserve, but we don't
    support it in blinkpy yet. To avoid quietly grabbing port 9000, pass
    --no-h2 to wpt for now.
    
    Bug: 1090491
    Change-Id: Ie326b86dd1305d1173740c85afc8eecded17e277
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229582
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774951}

--- third_party/blink/tools/blinkpy/web_tests/servers/wptserve_unittest.py
+++ third_party/blink/tools/blinkpy/web_tests/servers/wptserve_unittest.py
@@ -34,6 +34,7 @@ class TestWPTServe(LoggingTestCase):
             server._config_file,
             '--doc_root',
             '/test.checkout/wtests/external/wpt',
+            '--no-h2',
         ])
 
     def test_init_start_cmd_with_ws_handlers(self):
@@ -49,6 +50,7 @@ class TestWPTServe(LoggingTestCase):
             server._config_file,
             '--doc_root',
             '/test.checkout/wtests/external/wpt',
+            '--no-h2',
             '--ws_doc_root',
             '/test.checkout/wtests/external/wpt/websockets/handlers',
         ])
