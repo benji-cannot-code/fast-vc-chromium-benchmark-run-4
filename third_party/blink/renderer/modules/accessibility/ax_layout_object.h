46ad00e3b110f0735679838794ac4fc914d21220
Chris Hall <chrishall@chromium.org>
2020-06-04 02:43:47 +0000

    Remove IsDefault from AXLayoutObject.
    Mark AXNodeObject::IsDefault as final.
    
    Written collaboratively with Aboxhall and Meredithl.
    
    Change-Id: I201b2579fde618ab2858442f0ab8da7b88dc68f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227724
    Reviewed-by: Meredith Lane <meredithl@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Chris Hall <chrishall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774945}

--- third_party/blink/renderer/modules/accessibility/ax_layout_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_layout_object.h
@@ -85,7 +85,6 @@ class MODULES_EXPORT AXLayoutObject : public AXNodeObject {
 
   // Check object role or purpose.
   bool IsAutofillAvailable() const override;
-  bool IsDefault() const final;
   bool IsEditable() const override;
   bool IsRichlyEditable() const override;
   bool IsLineBreakingObject() const override;
