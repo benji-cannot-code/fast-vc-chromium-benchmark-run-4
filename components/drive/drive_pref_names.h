da0880a603c0fa4ffb0481dbcd6d4290122f0de6
Austin Tankiang <austinct@chromium.org>
2020-06-04 05:54:48 +0000

    Add pref to control verbose logging in DriveFS
    Bug: 1049071
    Change-Id: I4156e737f1cd760d783f086be2d058609a50b4fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2208327
    Reviewed-by: Stuart Langley <slangley@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774989}

--- components/drive/drive_pref_names.h
+++ components/drive/drive_pref_names.h
@@ -13,6 +13,7 @@ namespace prefs {
 
 extern const char kDisableDrive[];
 extern const char kDisableDriveOverCellular[];
+extern const char kDriveFsEnableVerboseLogging[];
 extern const char kDriveFsProfileSalt[];
 extern const char kDriveFsPinnedMigrated[];
 extern const char kDriveFsWasLaunchedAtLeastOnce[];
