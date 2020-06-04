46154bfc341f16f654cc66e2a4140c7730f7792f
Jan Wilken Dörrie <jdoerrie@chromium.org>
2020-06-04 11:46:25 +0000

    [base] Re-organize Windows only string utils
    This change moves the Windows only code in base/strings/{strcat,
    string_util, string_split}.h to their own _win.h headers, and introduces
    appropriate _internal.h headers to be able to share the templated code
    between Windows and cross-platform code.
    
    This change is purely organizational, and there is no functionality
    added or removed.
    
    Bug: 911896
    Change-Id: I3d2eae1e665cfcf48e727d6d71ca1975c441a370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228622
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775075}

---
+++ base/strings/strcat_win.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/strings/strcat_win.h"
+
+#include <string>
+
+#include "base/containers/span.h"
+#include "base/strings/strcat_internal.h"
+#include "base/strings/string_piece.h"
+
+namespace base {
+
+#if defined(BASE_STRING16_IS_STD_U16STRING)
+
+std::wstring StrCat(span<const WStringPiece> pieces) {
+  return internal::StrCatT(pieces);
+}
+
+std::wstring StrCat(span<const std::wstring> pieces) {
+  return internal::StrCatT(pieces);
+}
+
+void StrAppend(std::wstring* dest, span<const WStringPiece> pieces) {
+  internal::StrAppendT(dest, pieces);
+}
+
+void StrAppend(std::wstring* dest, span<const std::wstring> pieces) {
+  internal::StrAppendT(dest, pieces);
+}
+
+#endif
+
+}  // namespace base
