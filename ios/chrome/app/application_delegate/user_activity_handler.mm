a32e0af916e361bf6c7764fd8aeba0512b1d125e
Olivier Robin <olivierrobin@google.com>
2020-06-04 10:53:43 +0000

    Handle ShortcutItem in multiwindow
    As a completionHandler has to be called after handling the item, it is
    not clear how to do just set an item in SceneState and call the
    observers.
    
    Bug: 1069748
    Change-Id: I02b8e5e4031323cbe7b60bea5045da16aab7c9d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228533
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775065}

--- ios/chrome/app/application_delegate/user_activity_handler.mm
+++ ios/chrome/app/application_delegate/user_activity_handler.mm
@@ -240,7 +240,9 @@ NSString* const kShortcutQRScanner = @"OpenQRScanner";
                                 browserState:interfaceProvider.currentInterface
                                                  .browserState];
   }
-  completionHandler(handledShortcutItem);
+  if (completionHandler) {
+    completionHandler(handledShortcutItem);
+  }
 }
 
 + (BOOL)willContinueUserActivityWithType:(NSString*)userActivityType {
--- ios/chrome/browser/ui/main/scene_controller.h
+++ ios/chrome/browser/ui/main/scene_controller.h
@@ -45,6 +45,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, strong, readonly) id<BrowserInterfaceProvider>
     interfaceProvider;
 
+// Handler for the UIWindowSceneDelegate callback with the same selector.
+- (void)performActionForShortcutItem:(UIApplicationShortcutItem*)shortcutItem
+                   completionHandler:(void (^)(BOOL succeeded))completionHandler
+    API_AVAILABLE(ios(13));
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_MAIN_SCENE_CONTROLLER_H_
