4e6a20b9b1a43144a516179ba3b7b064b46007d4
Dan Zhang <danzh@chromium.org>
2020-06-04 17:42:36 +0000

    Roll src/net/third_party/quiche/src/ 4026f6e15..d7b17f31c (13 commits)
    https://quiche.googlesource.com/quiche.git/+log/4026f6e15a63..d7b17f31c55ae
    
    $ git log 4026f6e15..d7b17f31c --date=short --no-merges --format='%ad %ae %s'
    2020-06-02 bnc Deprecate gfe2_reloadable_flag_spdy_hpack_use_indexed_name.
    2020-06-02 renjietang Add << operator for TransmissionType to make debugging easier.
    2020-06-02 fayang Deflake //third_party/quic/core/http:end_to_end_test.
    2020-06-02 fayang In quic, add num_draining_outgoing_streams_ to quicsession, and this counter will be used to determine shouldkeepconnectionalive in quicspdyclientsessionbase. client side only. not protected.
    2020-06-02 fayang In quic, do not arm pto if the only in flight packet is half rtt data before handshake completion. protected by gfe2_reloadable_flag_quic_fix_server_pto_timeout.
    2020-06-02 fayang Log max_packet_length and header when failed to serialize packet.
    2020-06-02 nharper Improve QuicCryptoServerStreamBase functions for querying resumption status
    2020-06-02 renjietang Internal QUICHE change
    2020-06-02 fayang In quicsession, unify num_*_incoming_streams_ and num_*_outgoing_streams_. refactoring only, not protected.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    substitute GetNumDrainingStreams and GetNumOpenDynamicStreams
    
    R=dschinazi@chromium.org
    
    Change-Id: I65bf6eb1a694d62ba4bc25a1142f587595de79aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229719
    Commit-Queue: Dan Zhang <danzh@chromium.org>
    Auto-Submit: Dan Zhang <danzh@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775161}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -1865,8 +1865,7 @@ void QuicChromiumClientSession::MigrateSessionOnWriteError(
   if (migrate_idle_session_ && CheckIdleTimeExceedsIdleMigrationPeriod())
     return;
 
-  if (!migrate_idle_session_ && GetNumActiveStreams() == 0 &&
-      GetNumDrainingStreams() == 0) {
+  if (!migrate_idle_session_ && !HasActiveRequestStreams()) {
     // connection close packet to be sent since socket may be borked.
     connection()->CloseConnection(quic::QUIC_PACKET_WRITE_ERROR,
                                   "Write error for non-migratable session",
@@ -2014,8 +2013,7 @@ void QuicChromiumClientSession::OnProbeSucceeded(
   // Close streams that are not migratable to the probed |network|.
   ResetNonMigratableStreams();
 
-  if (!migrate_idle_session_ && GetNumActiveStreams() == 0 &&
-      GetNumDrainingStreams() == 0) {
+  if (!migrate_idle_session_ && !HasActiveRequestStreams()) {
     // If idle sessions won't be migrated, close the connection.
     CloseSessionOnErrorLater(
         ERR_NETWORK_CHANGED,
@@ -2216,8 +2214,7 @@ void QuicChromiumClientSession::MigrateNetworkImmediately(
   // - otherwise, it's brought to default network, cancel the running timer to
   //   migrate back.
 
-  if (!migrate_idle_session_ && GetNumActiveStreams() == 0 &&
-      GetNumDrainingStreams() == 0) {
+  if (!migrate_idle_session_ && !HasActiveRequestStreams()) {
     HistogramAndLogMigrationFailure(MIGRATION_STATUS_NO_MIGRATABLE_STREAMS,
                                     connection_id(), "No active streams");
     CloseSessionOnErrorLater(
@@ -2311,7 +2308,7 @@ void QuicChromiumClientSession::OnPathDegrading() {
         GetNumActiveStreams());
     UMA_HISTOGRAM_COUNTS_1M(
         "Net.QuicSession.DrainingStreamsOnGoAwayAfterPathDegrading",
-        GetNumDrainingStreams());
+        num_outgoing_draining_streams());
     return;
   }
 
@@ -2339,11 +2336,6 @@ void QuicChromiumClientSession::OnPathDegrading() {
   HistogramAndLogMigrationFailure(MIGRATION_STATUS_PATH_DEGRADING_NOT_ENABLED,
                                   connection_id(),
                                   "Migration on path degrading not enabled");
-}
-
-bool QuicChromiumClientSession::ShouldKeepConnectionAlive() const {
-  return quic::QuicSpdySession::ShouldKeepConnectionAlive() ||
-         GetNumDrainingOutgoingStreams() > 0;
 }
 
 void QuicChromiumClientSession::OnProofValid(
@@ -2547,8 +2539,7 @@ ProbingResult QuicChromiumClientSession::MaybeStartProbing(
 
   CHECK_NE(NetworkChangeNotifier::kInvalidNetworkHandle, network);
 
-  if (!migrate_idle_session_ && GetNumActiveStreams() == 0 &&
-      GetNumDrainingStreams() == 0) {
+  if (!migrate_idle_session_ && !HasActiveRequestStreams()) {
     HistogramAndLogMigrationFailure(MIGRATION_STATUS_NO_MIGRATABLE_STREAMS,
                                     connection_id(), "No active streams");
     CloseSessionOnErrorLater(
@@ -2692,7 +2683,7 @@ bool QuicChromiumClientSession::CheckIdleTimeExceedsIdleMigrationPeriod() {
   if (!migrate_idle_session_)
     return false;
 
-  if (GetNumActiveStreams() != 0 || GetNumDrainingStreams() != 0) {
+  if (HasActiveRequestStreams()) {
     return false;
   }
 
@@ -3030,8 +3021,7 @@ MigrationResult QuicChromiumClientSession::Migrate(
   if (network != NetworkChangeNotifier::kInvalidNetworkHandle) {
     // This is a migration attempt from connection migration.
     ResetNonMigratableStreams();
-    if (!migrate_idle_session_ && GetNumActiveStreams() == 0 &&
-        GetNumDrainingStreams() == 0) {
+    if (!migrate_idle_session_ && !HasActiveRequestStreams()) {
       // If idle sessions can not be migrated, close the session if needed.
       if (close_session_on_error) {
         CloseSessionOnErrorLater(
