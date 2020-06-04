eba9228c7d7fdd6dfcead91f9a2712aaefe6034e
Liquan (Max) Gu <maxlg@chromium.org>
2020-06-04 18:46:52 +0000

    [ExpandablePaymentHandler] Test UI appearance and user interaction
    This CL adds test coverage for the appearance of the Expandable
    Payment Handler UI and its user interaction.
    
    Changes:
    * Moved the original content of ExpandablePaymentHandlerTest.java into
    ExpandablePaymentHandlerCoordinatorTest.java (and change to small-test).
    * Added test cases in ExpandablePaymentHandlerTest.java.
    * Changed payment method from "/pay" to the "payment_method.json".
    This is because /pay was not pointing to a payment method manifest, so
    JIT installation was disabled.
    * merchant.html: given ids to the buttons so that the tests can
    simulate users' clicking on the buttons.
    
    Bug: 1042892, 1084705, 1084707, 1084708, 1084711, 1084721, 1068741
    
    Change-Id: I4943932632cc446c75144bcdf7f9d9f9e06881be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227512
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775203}

--- components/test/data/payments/maxpay.com/installer.js
+++ components/test/data/payments/maxpay.com/installer.js
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-const methodName = window.location.origin + '/pay';
+const methodName = window.location.origin +
+    '/components/test/data/payments/maxpay.com/payment_method.json';
 const swSrcUrl = './payment_handler_sw.js';
 let resultPromise;
 
