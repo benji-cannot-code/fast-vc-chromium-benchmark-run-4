ba38b4111eb62aa12a03ac7366caf3638a8e9dbc
Yuki Shiino <yukishiino@chromium.org>
2020-06-04 05:01:43 +0000

    bind-gen: Implement bind_gen.TaskQueue
    Bug: 839389
    Change-Id: I4921bd4b970fdd65fd2561f22bc2832768c28063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228742
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#774974}

--- third_party/blink/renderer/bindings/scripts/bind_gen/enumeration.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/enumeration.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import web_idl
+
 from . import name_style
 from .blink_v8_bridge import blink_class_name
 from .code_node import EmptyNode
@@ -24,6 +26,7 @@ from .codegen_utils import make_header_include_directives
 from .codegen_utils import write_code_node_to_file
 from .mako_renderer import MakoRenderer
 from .path_manager import PathManager
+from .task_queue import TaskQueue
 
 
 def make_factory_methods(cg_context):
@@ -356,6 +359,9 @@ def generate_enumeration(enumeration):
     write_code_node_to_file(source_node, path_manager.gen_path_to(source_path))
 
 
-def generate_enumerations(web_idl_database):
+def generate_enumerations(task_queue, web_idl_database):
+    assert isinstance(task_queue, TaskQueue)
+    assert isinstance(web_idl_database, web_idl.Database)
+
     for enumeration in web_idl_database.enumerations:
-        generate_enumeration(enumeration)
+        task_queue.post_task(generate_enumeration, enumeration)
