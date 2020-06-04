5dcb9ddbdc1ecf9ddef987f7f6341734ae3f9760
evliu <evliu@google.com>
2020-06-04 18:58:40 +0000

    Supply speech recognition sandbox from service_sandbox_type.h
    This CL adds a header that was missed in this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/2206918.
    
    Bug: 1091051
    Change-Id: Ifa742b5197b4deba6873a0c90ac180478b91d9a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228061
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/master@{#775209}

--- chrome/browser/speech/speech_recognition_service.cc
+++ chrome/browser/speech/speech_recognition_service.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/speech/speech_recognition_service.h"
 
+#include "chrome/browser/service_sandbox_type.h"
 #include "chrome/grit/generated_resources.h"
 #include "content/public/browser/service_process_host.h"
 
