aa61ec65806af3ab801176399bcdf3715eb07e21
dpapad <dpapad@chromium.org>
2020-06-04 08:52:23 +0000

    WebUI: Add JS type checking for more cr_elements/ tests, part 8.
    Specifically:
     - cr_toast_manager_test.js
     - cr_toast_test.js
     - cr_toggle_test.js
     - cr_toolbar_search_field_tests.js
    
    Bug: 1000989
    Change-Id: I9d745caca24c46945e64faca96d9b87fcecd1af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227238
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775025}

--- chrome/test/data/webui/cr_elements/cr_toast_manager_test.js
+++ chrome/test/data/webui/cr_elements/cr_toast_manager_test.js
@@ -6,14 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // clang-format off
 // #import {getToastManager} from 'chrome://resources/cr_elements/cr_toast/cr_toast_manager.m.js';
 // #import {eventToPromise} from '../test_util.m.js';
+// #import {assertEquals, assertFalse, assertTrue} from '../chai_assert.js';
 // clang-format on
 
 suite('cr-toast-manager', () => {
+  /** @type {!CrToastManagerElement} */
   let toastManager;
 
   suiteSetup(() => {
-    PolymerTest.clearBody();
-    toastManager = document.createElement('cr-toast-manager');
+    document.body.innerHTML = '';
+    toastManager = /** @type {!CrToastManagerElement} */ (
+        document.createElement('cr-toast-manager'));
     document.body.appendChild(toastManager);
   });
 
@@ -46,6 +49,7 @@ suite('cr-toast-manager', () => {
 
   test('duration passed through to toast', () => {
     toastManager.duration = 3;
-    assertEquals(3, toastManager.$.toast.duration);
+    assertEquals(
+        3, /** @type {!CrToastElement} */ (toastManager.$$('#toast').duration));
   });
 });
--- chrome/test/data/webui/mock_timer.js
+++ chrome/test/data/webui/mock_timer.js
@@ -24,10 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     /**
      * Details for active timers.
-     * @type {Array<{callback: Function,
+     * @type {!Array<{callback: Function,
      *                delay: number,
      *                key: number,
-     *                repeats: boolean}>}
+     *                repeats: boolean}|undefined>}
      * @private
      */
     this.timers_ = [];
