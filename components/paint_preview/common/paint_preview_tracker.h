9fc95ffb3baf9c6216d5189e8dc89d91b6b3e570
Calder Kitagawa <ckitagawa@chromium.org>
2020-06-04 18:51:40 +0000

    Revert "[Paint Preview] Account for scroll offset in subframes"
    This reverts commit 17ae1743f3fc2c4e897f8195e21ce20a8caaa06d.
    
    Reason for revert: MSAN failure https://logs.chromium.org/logs/chromium/buildbucket/cr-buildbucket.appspot.com/8878408074967908800/+/steps/components_unittests/0/logs/Deterministic_failure:PaintPreviewRecorderUtilsTest.TestSerializeAsSkPicture__status_CRASH/0
    
    Original change's description:
    > [Paint Preview] Account for scroll offset in subframes
    > 
    > Scroll position isn't accounted for in the clip rect for subframes. It
    > needs to be manually corrected via the tracker when serializing.
    > 
    > Bug: 1090920
    > Change-Id: I93e316016832132cbfdfec833249ed454c060b9c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228577
    > Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    > Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#775098}
    
    TBR=mahmoudi@chromium.org,ckitagawa@chromium.org
    
    Change-Id: I2b2e7f5ff847c95c0c329bea0a54dc11c3810310
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1090920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231262
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775205}

--- components/paint_preview/common/paint_preview_tracker.h
+++ components/paint_preview/common/paint_preview_tracker.h
@@ -51,9 +51,6 @@ class PaintPreviewTracker {
       const gfx::Rect& rect,
       const base::UnguessableToken& embedding_token);
 
-  // Sets the scroll position for the top layer frame.
-  void SetScrollForFrame(const SkISize& scroll);
-
   // Adds the glyphs in |blob| to the glyph usage tracker for the |blob|'s
   // associated typface.
   void AddGlyphs(const SkTextBlob* blob);
@@ -86,8 +83,6 @@ class PaintPreviewTracker {
   const base::UnguessableToken guid_;
   const base::Optional<base::UnguessableToken> embedding_token_;
   const bool is_main_frame_;
-
-  SkISize scroll_;
 
   std::vector<mojom::LinkDataPtr> links_;
   PictureSerializationContext content_id_to_embedding_token_;
