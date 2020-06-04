8631a033b273d5970d656cfbad14dd5ddf3a8fa5
Manuel Rego Casasnovas <rego@igalia.com>
2020-06-04 09:45:37 +0000

    Enable transformed rasterization for kScrollDependentPosition
    BUG=1076257
    TEST=CompositedLayerMappingTest.TransformedRasterizationForScrollDependentPosition
    
    Change-Id: I1ec70d65a533b14e3b6911de29830cbe334c66b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215335
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775040}

--- third_party/blink/renderer/platform/graphics/compositing_reasons.h
+++ third_party/blink/renderer/platform/graphics/compositing_reasons.h
@@ -133,6 +133,9 @@ class PLATFORM_EXPORT CompositingReason {
     kComboAllDirectReasons = kComboAllDirectStyleDeterminedReasons |
                              kComboAllDirectNonStyleDeterminedReasons,
 
+    kComboTransformedRasterizationDisallowedReasons =
+        kComboAllDirectReasons & ~kScrollDependentPosition,
+
     kComboAllCompositedScrollingDeterminedReasons =
         kScrollDependentPosition | kOverflowScrolling,
 
