6669fc138049efaab74a249676a9383db0d6a810
Lei Zhang <thestig@chromium.org>
2020-06-04 16:52:56 +0000

    Remove PDFAnnotations flag altogether.
    Just assume the feature is always enabled on Chrome OS.
    
    Bug: 1010663
    Change-Id: I562d18a782dc8ba0e938698697f3beeccf2919d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229317
    Reviewed-by: dstockwell <dstockwell@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775138}

--- pdf/out_of_process_instance.cc
+++ pdf/out_of_process_instance.cc
@@ -1499,7 +1499,6 @@ void OutOfProcessInstance::SaveToBuffer(const std::string& token) {
     }
   } else {
 #if defined(OS_CHROMEOS)
-    DCHECK(base::FeatureList::IsEnabled(features::kPDFAnnotations));
     uint32_t length = engine_->GetLoadedByteSize();
     if (IsSaveDataSizeValid(length)) {
       pp::VarArrayBuffer buffer(length);
