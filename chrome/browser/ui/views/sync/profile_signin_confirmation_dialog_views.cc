328064b47b316fd649a8744d9977e1833d06e6b6
David Roger <droger@chromium.org>
2020-06-04 17:22:53 +0000

    Fix extra call to Cancel() in ProfileSigninConfirmationDialogViews
    Bug: 1091232
    Change-Id: Idfbecb611c0bde730a375a3bf3059c19c4d115ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231221
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775155}

--- chrome/browser/ui/views/sync/profile_signin_confirmation_dialog_views.cc
+++ chrome/browser/ui/views/sync/profile_signin_confirmation_dialog_views.cc
@@ -255,10 +255,6 @@ void ProfileSigninConfirmationDialogViews::ViewHierarchyChanged(
                          kPreferredWidth, explanation_label_height);
 }
 
-void ProfileSigninConfirmationDialogViews::WindowClosing() {
-  Cancel();
-}
-
 void ProfileSigninConfirmationDialogViews::StyledLabelLinkClicked(
     views::StyledLabel* label,
     const gfx::Range& range,
