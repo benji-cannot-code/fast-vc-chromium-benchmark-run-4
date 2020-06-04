bdc582c42487e982ab1765ccee54bf2f35199499
Gyuyoung Kim <gyuyoung@igalia.com>
2020-06-04 00:39:37 +0000

    Convert MidisServer interface to the new Mojo
    This CL applies mojo::PendingReceiver to all uses of
    MidisServerRequest.
    
    Bug: 955171
    Change-Id: I3faf2160dd85f4d9e7855e3d72685c1b01c8a624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227943
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#774898}

--- components/arc/midis/arc_midis_bridge.h
+++ components/arc/midis/arc_midis_bridge.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "components/arc/mojom/midis.mojom.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 namespace content {
@@ -35,13 +36,14 @@ class ArcMidisBridge : public KeyedService,
   ~ArcMidisBridge() override;
 
   // Midis Mojo host interface
-  void Connect(mojom::MidisServerRequest request,
+  void Connect(mojo::PendingReceiver<mojom::MidisServer> receiver,
                mojom::MidisClientPtr client_ptr) override;
 
  private:
-  void OnBootstrapMojoConnection(mojom::MidisServerRequest request,
-                                 mojom::MidisClientPtr client_ptr,
-                                 bool result);
+  void OnBootstrapMojoConnection(
+      mojo::PendingReceiver<mojom::MidisServer> receiver,
+      mojom::MidisClientPtr client_ptr,
+      bool result);
   void OnMojoConnectionError();
 
   ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
