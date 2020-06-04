2a7c7c678758bb90b6df5b890b3023a12a17f983
Torne (Richard Coles) <torne@google.com>
2020-06-04 15:17:49 +0000

    Reland "webview: make extra header handling more sensible."
    Reland the change to extra header handling with the actual behaviour
    change behind a disabled-by-default base::Feature. UMA metrics are
    collected even when the feature is disabled, simulating what would have
    happened had the feature been enabled, so we can better gauge the
    compatibility impact on applications.
    
    Bug: 1038002
    Change-Id: I3d722060cc4526a14adac076bbf4eec0a0992b70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229420
    Commit-Queue: Richard Coles <torne@chromium.org>
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775115}

--- android_webview/common/aw_features.h
+++ android_webview/common/aw_features.h
@@ -18,6 +18,7 @@ namespace features {
 extern const base::Feature kWebViewBrotliSupport;
 extern const base::Feature kWebViewCollectNonembeddedMetrics;
 extern const base::Feature kWebViewConnectionlessSafeBrowsing;
+extern const base::Feature kWebViewExtraHeadersSameOriginOnly;
 extern const base::Feature kWebViewSniffMimeType;
 extern const base::Feature kWebViewWideColorGamutSupport;
 
--- android_webview/java/src/org/chromium/android_webview/common/AwFeatures.java
+++ android_webview/java/src/org/chromium/android_webview/common/AwFeatures.java
@@ -18,5 +18,8 @@ public final class AwFeatures {
     public static final String WEBVIEW_CONNECTIONLESS_SAFE_BROWSING =
             "WebViewConnectionlessSafeBrowsing";
 
+    public static final String WEBVIEW_EXTRA_HEADERS_SAME_ORIGIN_ONLY =
+            "WebViewExtraHeadersSameOriginOnly";
+
     private AwFeatures() {}
 }
