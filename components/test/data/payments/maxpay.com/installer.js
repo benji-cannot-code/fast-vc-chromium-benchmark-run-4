4380caf0694e9e2a36611f2bcf0af8516776e6f4
Liquan (Max) Gu <maxlg@chromium.org>
2020-06-05 21:08:37 +0000

    Revert "[Reland][ExpandablePaymentHandler] Test UI appearance and user interaction"
    This reverts commit 098a11183b6f9ba00f021129ddd73ac7de01fb40.
    
    Reason for revert: This reland CL is still flaky[1].
    
    [1] https://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=chrome_public_test_apk%20(with%20patch)&tests=org.chromium.chrome.browser.payments.ExpandablePaymentHandlerTest
    
    Original change's description:
    > [Reland][ExpandablePaymentHandler] Test UI appearance and user interaction
    >
    > original CL: crrev.com/c/2227512
    > revert CL: crrev.com/c/2231122
    > revert reason:
    > showPaymentHandlerUi was flaky. It's very likely because the animation
    > took so long to finish that that it hits the timeout. Many screenshots
    > prove that because the payment sheet still had a thin scrim on it.
    >
    > change:
    > Compared to the original CL, the new one removes the animation.
    >
    > Bug: 1042892, 1084705, 1084707, 1084708, 1084711, 1084721, 1068741
    > Change-Id: Ib3401e3e422e37a733f85eb56e6d08598d3ade1f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2232679
    > Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    > Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#775685}
    
    TBR=rouslan@chromium.org,maxlg@chromium.org
    
    Change-Id: I59ddce5a193b105e8d2e9f99a3891efbf6a0f178
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1042892, 1084705, 1084707, 1084708, 1084711, 1084721, 1068741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233682
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Liquan (Max) Gu <maxlg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775762}

--- components/test/data/payments/maxpay.com/installer.js
+++ components/test/data/payments/maxpay.com/installer.js
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-const methodName = window.location.origin +
-    '/components/test/data/payments/maxpay.com/payment_method.json';
+const methodName = window.location.origin + '/pay';
 const swSrcUrl = './payment_handler_sw.js';
 let resultPromise;
 
