043a6c6393e5b7830609795044e9038a21fd61d2
Robert Ogden <robertogden@chromium.org>
2020-06-05 00:28:10 +0000

    Remove code for NoStatePrefetch fetching fonts
    This is not having any impact on metrics, so we're not launching this.
    
    Bug: 1021749
    Change-Id: I42a13ff73419d2af9d30899fe328a41394568c04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231549
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775370}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -83,8 +83,6 @@ BLINK_COMMON_EXPORT extern const base::Feature
 BLINK_COMMON_EXPORT extern const base::Feature kAudioWorkletRealtimeThread;
 
 BLINK_COMMON_EXPORT extern const base::Feature kLightweightNoStatePrefetch;
-BLINK_COMMON_EXPORT extern const base::Feature
-    kLightweightNoStatePrefetch_FetchFonts;
 
 BLINK_COMMON_EXPORT extern const base::Feature kSaveDataImgSrcset;
 
