5f8a414aadd4209a3f7853068dd1253a7e43ab1c
Maksim Ivanov <emaxx@chromium.org>
2020-06-05 19:32:20 +0000

    DCHECK on self-assignment of PolicyMap
    Add debug assertions against misuses of PolicyMap in self-assignment or
    self-modifying constructions.
    
    This CL doesn't bring proper support of such scenarios, but should at
    least make it more obvious that they're not supported (by documenting
    it and by catching during automated tests).
    
    Bug: none
    Change-Id: I9e8a60e48986fd5e3b61b15792b2a24cd6067b9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233238
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@google.com>
    Cr-Commit-Position: refs/heads/master@{#775711}

--- components/policy/core/common/policy_map.cc
+++ components/policy/core/common/policy_map.cc
@@ -285,6 +285,8 @@ void PolicyMap::Swap(PolicyMap* other) {
 }
 
 void PolicyMap::CopyFrom(const PolicyMap& other) {
+  DCHECK_NE(this, &other);
+
   Clear();
   for (const auto& it : other)
     Set(it.first, it.second.DeepCopy());
@@ -297,6 +299,8 @@ std::unique_ptr<PolicyMap> PolicyMap::DeepCopy() const {
 }
 
 void PolicyMap::MergeFrom(const PolicyMap& other) {
+  DCHECK_NE(this, &other);
+
   for (const auto& policy_and_entry : other) {
     Entry* current_policy = GetMutableUntrusted(policy_and_entry.first);
     Entry other_policy = policy_and_entry.second.DeepCopy();
