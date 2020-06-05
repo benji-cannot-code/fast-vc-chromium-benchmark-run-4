5f8bd4e6d9dbfc21f3086ffd86218ce98a343607
Maksim Moskvitin <mmoskvitin@google.com>
2020-06-05 13:29:01 +0000

    Remove obsolete NOTIMPLEMENTED() in nigori_state
    Removed NOTIMPLEMENTED() refers to per-type encryption settings.
    Per-type encryption settings aren't supported anymore (replaced by
    |encrypt_everything|).
    
    Bug: 1033020
    Change-Id: I308f2da0419f03c5a7d301714cfa8ab24d46ae02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2232323
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775538}

--- components/sync/nigori/nigori_state.cc
+++ components/sync/nigori/nigori_state.cc
@@ -274,8 +274,6 @@ sync_pb::NigoriSpecifics NigoriState::ToSpecificsProto() const {
     specifics.set_custom_passphrase_time(
         TimeToProtoTime(custom_passphrase_time));
   }
-  // TODO(crbug.com/922900): add other fields support.
-  NOTIMPLEMENTED();
   return specifics;
 }
 
