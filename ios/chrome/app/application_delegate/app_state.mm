3d609d81910703f9b875e648a8f7dd32211f45c9
Stepan Khapugin <stkhapugin@chromium.org>
2020-06-05 17:33:21 +0000

    [multiwindow] Make safe mode a blocking UI.
    Makes safe mode a blocking UI. Also fixes a bug when you quit safe mode
    after viewing other windows, which might make window 0 not initialized
    when the safe mode completes, resulting in a nullptr dereference.
    
    Bug: 1072408
    Change-Id: I9b1dba2f806fe716e49c9c3a48f767691ab37d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218320
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775641}

--- ios/chrome/app/application_delegate/app_state.mm
+++ ios/chrome/app/application_delegate/app_state.mm
@@ -546,6 +546,7 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
 #pragma mark - SafeModeCoordinatorDelegate Implementation
 
 - (void)coordinatorDidExitSafeMode:(nonnull SafeModeCoordinator*)coordinator {
+  self.sceneShowingBlockingUI = nil;
   self.safeModeCoordinator = nil;
   self.inSafeMode = NO;
   [_browserLauncher startUpBrowserToStage:INITIALIZATION_STAGE_FOREGROUND];
@@ -569,6 +570,8 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
   [self.foregroundActiveScene.window makeKeyAndVisible];
 
   [self.safeModeCoordinator start];
+
+  self.sceneShowingBlockingUI = self.foregroundActiveScene;
 }
 
 - (void)initializeUI {
@@ -613,13 +616,14 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
 - (void)sceneDidActivate:(NSNotification*)notification {
   DCHECK(IsSceneStartupSupported());
   if (@available(iOS 13, *)) {
+    UIWindowScene* scene =
+        base::mac::ObjCCastStrict<UIWindowScene>(notification.object);
+    SceneDelegate* sceneDelegate =
+        base::mac::ObjCCastStrict<SceneDelegate>(scene.delegate);
+
     if (!self.firstSceneHasActivated) {
       self.firstSceneHasActivated = YES;
 
-      UIWindowScene* scene =
-          base::mac::ObjCCastStrict<UIWindowScene>(notification.object);
-      SceneDelegate* sceneDelegate =
-          base::mac::ObjCCastStrict<SceneDelegate>(scene.delegate);
       [self.observers appState:self
            firstSceneActivated:sceneDelegate.sceneState];
 
@@ -628,10 +632,10 @@ initWithBrowserLauncher:(id<BrowserLauncher>)browserLauncher
         // safe mode has been postponed until now.
         [self startSafeMode];
       }
-      sceneDelegate.sceneState.presentingModalOverlay =
-          self.sceneShowingBlockingUI &&
-          (self.sceneShowingBlockingUI != sceneDelegate.sceneState);
     }
+    sceneDelegate.sceneState.presentingModalOverlay =
+        self.sceneShowingBlockingUI &&
+        (self.sceneShowingBlockingUI != sceneDelegate.sceneState);
   }
 }
 
