519647971d91c75d31b9b5cb5b3576d27e6c3c08
Yoshifumi Inoue <yosin@chromium.org>
2020-06-05 19:33:01 +0000

    [EditingNG] Make empty editable flex box has height
    This patch makes empty editable flex box to have height to pass the test[1]
    for enabling EditingNG.
    
    In legacy layout, this is implemented in |LayoutFlexibleBox::LayoutFlexItems()|
      if (HasLineIfEmpty()) {
        // Even if ComputeNextFlexLine returns true, the flexbox might not have
        // a line because all our children might be out of flow positioned.
        // Instead of just checking if we have a line, make sure the flexbox
        // has at least a line's worth of height to cover this case.
        LayoutUnit min_height = MinimumLogicalHeightForEmptyLine();
        if (Size().Height() < min_height)
          SetLogicalHeight(min_height);
      }
    
    Note: CL[2] introduces |LayoutBox::LogicalHeightForEmptyLine()| by extracting
    from |MinimumLogicalHeightForEmptyLine()|
    
    [1] fast/events/key-events-in-editable-flexbox.html
    [2] http://crrev.com/c/2232242 [EditingNG] Introduce LayoutBox::
    LogicalHeightForEmptyLine()
    
    Bug: 707656
    Change-Id: I1d8071902027b42203238f464e0caef332b401cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230552
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775712}

--- third_party/blink/renderer/core/layout/layout_block.h
+++ third_party/blink/renderer/core/layout/layout_block.h
@@ -402,6 +402,8 @@ class CORE_EXPORT LayoutBlock : public LayoutBox {
   void UpdateAfterLayout() override;
   MinMaxSizes PreferredLogicalWidths() const override;
 
+  virtual bool HasLineIfEmpty() const;
+
  protected:
   MinMaxSizes ComputeIntrinsicLogicalWidths() const override;
   void ComputeChildPreferredLogicalWidths(
@@ -430,8 +432,6 @@ class CORE_EXPORT LayoutBlock : public LayoutBox {
   // hasOverflowClip() will be false and we won't create scrollable area for
   // this object even if overflow is non-visible.
   virtual bool AllowsOverflowClip() const;
-
-  virtual bool HasLineIfEmpty() const;
 
   bool SimplifiedLayout();
   virtual void SimplifiedNormalFlowLayout();
