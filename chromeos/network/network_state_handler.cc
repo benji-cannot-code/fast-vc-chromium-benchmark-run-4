da1032b2a40a73fbcfc55c242cd99a8819d4240a
Steven Bennetts <stevenjb@chromium.org>
2020-06-05 18:54:53 +0000

    Support Cellular service with no device
    While swapping SIM cards a Cellular Device may not be available, but
    Shill will start persisiting the Service while the Device resets.
    
    This removes the requirement that all Cellular Network mojo instances
    have an associated device.
    
    Bug: b:154014577
    Change-Id: Id9c982f3752dbccce38201cb4acd221e5d0cc6b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227066
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775681}

--- chromeos/network/network_state_handler.cc
+++ chromeos/network/network_state_handler.cc
@@ -377,8 +377,10 @@ void NetworkStateHandler::SetProhibitedTechnologies(
 const DeviceState* NetworkStateHandler::GetDeviceState(
     const std::string& device_path) const {
   const DeviceState* device = GetModifiableDeviceState(device_path);
-  if (device && !device->update_received())
+  if (device && !device->update_received()) {
+    NET_LOG(DEBUG) << "Device exists but update not received: " << device_path;
     return nullptr;
+  }
   return device;
 }
 
@@ -1465,7 +1467,7 @@ void NetworkStateHandler::UpdateNetworkServiceProperty(
       key == shill::kNetworkTechnologyProperty ||
       (key == shill::kDeviceProperty && value_str == "/")) {
     // Uninteresting update. This includes 'Device' property changes to "/"
-    // (occurs before just a service is removed).
+    // (occurs just before a service is removed).
     // For non active networks do not log or send any notifications.
     if (!network->IsActive())
       return;
