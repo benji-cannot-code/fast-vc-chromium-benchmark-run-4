dbb7fea5bed1f83f18b56e76643230612cb07752
Andrew Xu <andrewxu@chromium.org>
2020-06-05 19:35:25 +0000

    Calculate Hotseat state transition
    This CL does the following things:
    (1) Create a structure which defines the hotseat state transition, such
    as the transition between the home launcher state and the hidden state.
    Implement the member function of HotseatWidget to calculate the state
    transition.
    
    (2) Add a new enum type to HotseatState to indicate the empty state.
    
    (3) Refactor the code block which layouts the hotseat widget so that
    the hotseat animation is set only when the animation is used.
    
    This CL should not introduce any noticeable difference.
    
    Bug: 1042911
    Change-Id: Ied8997e8e25b67e954620b95d83e59247b3155c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231257
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775714}

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -1475,7 +1475,7 @@ HotseatState ShelfLayoutManager::CalculateHotseatState(
       return hotseat_state();
   }
   NOTREACHED();
-  return HotseatState::kShownHomeLauncher;
+  return HotseatState::kNone;
 }
 
 ShelfVisibilityState ShelfLayoutManager::CalculateShelfVisibility() {
@@ -2050,7 +2050,8 @@ bool ShelfLayoutManager::ShouldHomeGestureHandleEvent(float scroll_y) const {
 
   if (IsHotseatEnabled()) {
     if (features::IsDragFromShelfToHomeOrOverviewEnabled() &&
-        hotseat_state() != HotseatState::kShownHomeLauncher) {
+        hotseat_state() != HotseatState::kShownHomeLauncher &&
+        hotseat_state() != HotseatState::kNone) {
       // If hotseat is hidden or extended (in-app or in-overview), do not let
       // HomeLauncherGestureHandler to handle the events.
       return false;
