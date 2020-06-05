411ef1d244725f5e73b2592276e79bd5ccff89d4
Jia <jiameng@chromium.org>
2020-06-05 05:39:39 +0000

    [cros search service] Remove old APIs
    Also remove unnecessary prefix that was used when we still have mojo
    version of the functions.
    
    Bug: 1090132
    Change-Id: I00416ae10fa756e6107f60af915b6f2a56c08e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230424
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775440}

--- chrome/browser/chromeos/local_search_service/index_unittest.cc
+++ chrome/browser/chromeos/local_search_service/index_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/local_search_service/index.h"
-#include "chrome/browser/chromeos/local_search_service/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace local_search_service {
@@ -183,9 +182,9 @@ TEST_F(IndexTest, MaxResults) {
 }
 
 TEST_F(IndexTest, ResultFound) {
-  // Register search tags but not contents to test backward compatibility.
-  const std::map<std::string, std::vector<std::string>> data_to_register = {
-      {"id1", {"id1", "tag1a", "tag1b"}}, {"xyz", {"xyz"}}};
+  const std::map<std::string, std::vector<ContentWithId>> data_to_register = {
+      {"id1", {{"cid1", "id1"}, {"cid2", "tag1a"}, {"cid3", "tag1b"}}},
+      {"xyz", {{"cid4", "xyz"}}}};
   std::vector<Data> data = CreateTestData(data_to_register);
   EXPECT_EQ(data.size(), 2u);
 
@@ -193,7 +192,7 @@ TEST_F(IndexTest, ResultFound) {
   EXPECT_EQ(index_.GetSize(), 2u);
 
   // Find result with query "id1". It returns an exact match.
-  const std::vector<ResultWithIds> expected_results = {{"id1", {""}}};
+  const std::vector<ResultWithIds> expected_results = {{"id1", {"cid1"}}};
   FindAndCheckResults(&index_, "id1",
                       /*max_results=*/-1, ResponseStatus::kSuccess,
                       expected_results);
