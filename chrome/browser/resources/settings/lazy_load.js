3b58b8094a5473e49ee983e56fdc89a00e6dc689
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-06-05 18:41:44 +0000

    [b4p/Settings] Introduce chrome://settings/passwords/device
    Screenshot of chrome://settings/passwords/device:
    https://screenshot.googleplex.com/Ox5jLPWSoAs
    
    Screenshot of chrome://settings/passwords (not changed):
    https://screenshot.googleplex.com/bUgjeiyHSvh
    
    We also profit of the occasion to modernize the code of a helper in
    passwords_section_test that is now also used in the tests for the new
    page.
    
    Bug: 1049141
    Change-Id: I9ff66231b7fc8ed888599842067bfe4760f1595a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228159
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/master@{#775673}

--- chrome/browser/resources/settings/lazy_load.js
+++ chrome/browser/resources/settings/lazy_load.js
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './appearance_page/appearance_fonts_page.js';
 import './autofill_page/password_check.js';
 import './autofill_page/passwords_section.js';
+import './autofill_page/passwords_device_section.js';
 import './autofill_page/payments_section.js';
 import './clear_browsing_data_dialog/clear_browsing_data_dialog.js';
 import './search_engines_page/search_engines_page.js';
