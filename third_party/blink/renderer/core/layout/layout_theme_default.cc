66c3e11313c5ca68a41db19e9100b4ce32e0b648
Ionel Popescu <iopopesc@microsoft.com>
2020-06-05 22:33:14 +0000

    Update select behavior for Enter key.
    This CL updates the select behavior for Enter key to pre-M83 where:
    1. on Mac, Enter key submits the form
    2. on other platforms, Enter key opens the popup
    
    Bug: 1087503
    Change-Id: Id3ca7171cc752e01db3c8c9bc23bbbb0a067d650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231456
    Commit-Queue: Ionel Popescu <iopopesc@microsoft.com>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775792}

--- third_party/blink/renderer/core/layout/layout_theme_default.cc
+++ third_party/blink/renderer/core/layout/layout_theme_default.cc
@@ -242,7 +242,7 @@ void LayoutThemeDefault::AdjustInnerSpinButtonStyle(
 }
 
 bool LayoutThemeDefault::PopsMenuByReturnKey() const {
-  return !features::IsFormControlsRefreshEnabled();
+  return true;
 }
 
 bool LayoutThemeDefault::ShouldOpenPickerWithF4Key() const {
