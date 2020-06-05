c4604410c0c93ee830e6ccc7ab2419ba5ede1fae
Yue Zhang <yuezhanggg@chromium.org>
2020-06-05 21:11:54 +0000

    Enable undo closure snack bar for tab strip in accessibility mode
    Currently, the undo closure snack bar is disabled when accessibility
    mode is enabled, and undo closure is handled in accessibility tab
    switcher. Since conditional tab strip allows users to close a tab
    when overview mode is not showing, this CL enables undo closure snack
    bar for accessibility users with this feature.
    
    Bug: 1090067
    Change-Id: I4427c8305f5706c4fc99f09393de1b323612a8f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231899
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775765}

--- chrome/android/java/src/org/chromium/chrome/browser/accessibility_tab_switcher/AccessibilityTabModelListItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/accessibility_tab_switcher/AccessibilityTabModelListItem.java
@@ -644,7 +644,7 @@ public class AccessibilityTabModelListItem extends FrameLayout implements OnClic
     }
 
     @VisibleForTesting
-    View getCloseButtonForTests() {
+    public View getCloseButtonForTests() {
         return mCloseButton;
     }
 }
