19f5651ee9a0b30e94c6fa6c84bf683604fb692e
Nate Fischer <ntfschr@chromium.org>
2020-06-05 19:30:50 +0000

    Componentize ReportAboutFlagsHistogram
    No change to logic. This componentizes ReportAboutFlagsHistogram (which
    is responsible for logging the Launch.FlagsAtStartup and
    Login.FlagsAtStartup histograms). This will be used later by Android
    WebView to log a similar histogram.
    
    This does not componentize AboutFlagsHistogramTest because this still
    depends on iterating over Chrome's kFeatureEntries list.
    
    Bug: 1084178
    Test: unit_tests -f 'AboutFlagsHistogramTest.*'
    Change-Id: If9f0f844d1e3b8e9dd7b4b7207b6c82996d88d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231446
    Commit-Queue: Theresa  <twellington@chromium.org>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775708}

--- chrome/browser/chromeos/login/session/user_session_manager.cc
+++ chrome/browser/chromeos/login/session/user_session_manager.cc
@@ -125,6 +125,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/settings/cros_settings_names.h"
 #include "components/account_id/account_id.h"
 #include "components/component_updater/component_updater_service.h"
+#include "components/flags_ui/flags_ui_metrics.h"
 #include "components/flags_ui/pref_service_flags_storage.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
@@ -847,7 +848,7 @@ bool UserSessionManager::RestartToApplyPerSessionFlagsIfNeed(
 
   LogCustomSwitches(command_line_difference);
 
-  about_flags::ReportAboutFlagsHistogram(
+  flags_ui::ReportAboutFlagsHistogram(
       "Login.CustomFlags", command_line_difference, std::set<std::string>());
 
   base::CommandLine::StringVector flags;
