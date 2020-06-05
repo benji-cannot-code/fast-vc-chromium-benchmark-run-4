7c692d87193bff190face0e6c5f9717f3813fe49
Collin Baker <collinbaker@chromium.org>
2020-06-05 18:53:50 +0000

    Autoclose WebUI tab strip only on tap in Omnibox or tab contents
    The previous behavior was to autoclose for any tap outside of the tab
    strip, or its control buttons.
    
    In addition, now only one tap is required to close the tab strip and
    focus the Omnibox.
    
    Change-Id: Iaef938fc073f26db8bedec9b9f0e5f1f10678ee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216603
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775680}

--- chrome/browser/ui/views/frame/webui_tab_strip_container_view.h
+++ chrome/browser/ui/views/frame/webui_tab_strip_container_view.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "chrome/browser/ui/webui/tab_strip/tab_strip_ui.h"
 #include "chrome/browser/ui/webui/tab_strip/tab_strip_ui_embedder.h"
+#include "chrome/browser/ui/webui/tab_strip/tab_strip_ui_metrics.h"
 #include "chrome/common/buildflags.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "ui/events/event_handler.h"
@@ -47,7 +48,8 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
  public:
   WebUITabStripContainerView(Browser* browser,
                              views::View* tab_contents_container,
-                             views::View* drag_handle);
+                             views::View* drag_handle,
+                             views::View* omnibox);
   ~WebUITabStripContainerView() override;
 
   static bool UseTouchableTabStrip();
@@ -98,13 +100,8 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
 
   void SetContainerTargetVisibility(bool target_visible);
 
-  // When the container is open, it intercepts most tap and click
-  // events. This checks if each event should be intercepted or passed
-  // through to its target.
-  bool EventShouldPropagate(const ui::Event& event);
-
   // Passed to the AutoCloser to handle closing.
-  void CloseForEventOutsideTabStrip();
+  void CloseForEventOutsideTabStrip(TabStripUICloseAction reason);
 
   // TabStripUI::Embedder:
   const ui::AcceleratorProvider* GetAcceleratorProvider() const override;
@@ -119,8 +116,6 @@ class WebUITabStripContainerView : public TabStripUIEmbedder,
   SkColor GetColor(int id) const override;
 
   // views::View:
-  void AddedToWidget() override;
-  void RemovedFromWidget() override;
   int GetHeightForWidth(int w) const override;
 
   gfx::Size FlexRule(const views::View* view,
