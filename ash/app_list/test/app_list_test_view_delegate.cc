e93811b111220322921c739d13f3b7b28ea2b4ad
Yulun Wu <yulunwu@chromium.org>
2020-06-05 21:30:35 +0000

    AppListView should not create accessibility event if AppListViewState is unchanged.
    Bug: 1005539
    Change-Id: Icb089f71cdd35f8b7c6005e83322a997270a4753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225379
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775774}

--- ash/app_list/test/app_list_test_view_delegate.cc
+++ ash/app_list/test/app_list_test_view_delegate.cc
@@ -208,6 +208,10 @@ int AppListTestViewDelegate::GetShelfSize() {
   return 56;
 }
 
+bool AppListTestViewDelegate::AppListTargetVisibility() const {
+  return true;
+}
+
 bool AppListTestViewDelegate::IsInTabletMode() {
   return is_tablet_mode_;
 }
