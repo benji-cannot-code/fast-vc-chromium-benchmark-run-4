dc1e29dc14f95ebfd5dbb60f4a428e99bee8aa12
Andrew Grieve <agrieve@chromium.org>
2020-06-05 16:05:09 +0000

    Android: Remove FileNotFoundException logged on start-up
    This exception was being logged during normal start-up flow
    of bundles. It's a common cause of misdirection when trying
    to investigate start-up crashes.
    
    Change-Id: I30913ff2fa67198c276f7813d17c86ee87b64257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203619
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775590}

--- ui/base/resource/resource_bundle_android.cc
+++ ui/base/resource/resource_bundle_android.cc
@@ -60,11 +60,10 @@ bool LoadFromApkOrFile(const char* apk_path,
 int LoadLocalePakFromApk(const std::string& app_locale,
                          bool in_split,
                          base::MemoryMappedFile::Region* out_region) {
+  bool log_error = true;
   std::string locale_path_within_apk =
-      GetPathForAndroidLocalePakWithinApk(app_locale, in_split);
+      GetPathForAndroidLocalePakWithinApk(app_locale, in_split, log_error);
   if (locale_path_within_apk.empty()) {
-    LOG(WARNING) << "locale_path_within_apk.empty() for locale "
-                 << app_locale;
     return -1;
   }
   return base::android::OpenApkAsset(locale_path_within_apk, out_region);
@@ -99,10 +98,13 @@ void ResourceBundle::LoadCommonResources() {
 
 // static
 bool ResourceBundle::LocaleDataPakExists(const std::string& locale) {
-  if (g_locale_paks_in_apk) {
-    return !GetPathForAndroidLocalePakWithinApk(locale, false).empty();
+  bool log_error = false;
+  bool in_split = !g_locale_paks_in_apk;
+  if (!in_split) {
+    return !GetPathForAndroidLocalePakWithinApk(locale, in_split, log_error)
+                .empty();
   }
-  if (!GetPathForAndroidLocalePakWithinApk(locale, true).empty())
+  if (!GetPathForAndroidLocalePakWithinApk(locale, in_split, log_error).empty())
     return true;
   const auto path = GetLocaleFilePath(locale);
   return !path.empty() && base::PathExists(path);
@@ -268,11 +270,13 @@ int GetSecondaryLocalePackFd(base::MemoryMappedFile::Region* out_region) {
 }
 
 std::string GetPathForAndroidLocalePakWithinApk(const std::string& locale,
-                                                bool in_bundle) {
+                                                bool in_bundle,
+                                                bool log_error) {
   JNIEnv* env = base::android::AttachCurrentThread();
   base::android::ScopedJavaLocalRef<jstring> ret =
       Java_ResourceBundle_getLocalePakResourcePath(
-          env, base::android::ConvertUTF8ToJavaString(env, locale), in_bundle);
+          env, base::android::ConvertUTF8ToJavaString(env, locale), in_bundle,
+          log_error);
   if (ret.obj() == nullptr) {
     return std::string();
   }
