febcdb572152ba02e0fba53feff222a0c6799fbf
Jonathan Ross <jonross@chromium.org>
2020-06-05 15:38:11 +0000

    Reland "Fields in system headers (and generated code) are not really affected."
    This reverts commit 990655e19332f14aced359b49528f1826eeee9fe.
    
    Reason for revert: I was incorrect, this did not resolve the failure. Build with this revert landed and failure still present: https://ci.chromium.org/p/chromium/builders/ci/Linux%20FYI%20GPU%20TSAN%20Release/87055
    
    Original change's description:
    > Revert "Fields in system headers (and generated code) are not really affected."
    > 
    > This reverts commit 1772227e54e3aa696f34a3f279e522c8d9722ff4.
    > 
    > Reason for revert: Suspect that this change has lead to TSAN errors in angle_unittests crbug.com/1091723
    > 
    > Original change's description:
    > > Fields in system headers (and generated code) are not really affected.
    > > 
    > > Fields in system headers (and generated) code were not rewritten even
    > > before this CL, because |apply_edits.py| ignores edits that apply to
    > > files that |git| doesn't know anything about.  But, before this CL, such
    > > fields would be considered "affected" and therefore Chromium code might
    > > see |.get()| appended when using such fields in |printf|,
    > > |reinterpret_cast|, etc.  Such inconsistency would lead to build errors
    > > after the rewrite.
    > > 
    > > After this CL, fields in system headers (and generated code) are
    > > explicitly excluded (early on, in the |field_decl_matcher|).
    > > 
    > > Bug: 1069567
    > > Change-Id: Icc9735b94558828c002572b38dcef12a812de35c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2204305
    > > Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    > > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#775250}
    > 
    > TBR=dcheng@chromium.org,lukasza@chromium.org
    > 
    > Change-Id: I4d547b32db1c159a6df5ae7a4a57ecb825d13abc
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1069567
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231310
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Commit-Queue: Jonathan Ross <jonross@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#775542}
    
    TBR=dcheng@chromium.org,jonross@chromium.org,lukasza@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1069567
    Change-Id: I954ae59c623031e7b5acd31093460a3d3bc84f0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230225
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775582}

---
+++ tools/clang/rewrite_raw_ptr_fields/tests/gen/generated_header.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef TOOLS_CLANG_REWRITE_RAW_PTR_FIELDS_TESTS_GEN_GENERATED_HEADER_H_
+#define TOOLS_CLANG_REWRITE_RAW_PTR_FIELDS_TESTS_GEN_GENERATED_HEADER_H_
+
+class SomeClass;
+
+// This file simulates a header that was generated during build.  This is
+// simulated by virtue of being located inside a directory named "gen".
+struct GeneratedStruct {
+  // No rewrite expected inside generated code.
+  int* ptr_field;
+  SomeClass* ptr_field2;
+};
+
+#endif  // TOOLS_CLANG_REWRITE_RAW_PTR_FIELDS_TESTS_GEN_GENERATED_HEADER_H_
