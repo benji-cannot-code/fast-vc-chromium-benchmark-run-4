a83dd29beba7614c107d6d5a54b41a787a551f2a
Sorin Jianu <sorin@chromium.org>
2020-06-05 22:37:57 +0000

    Expedite writing the prefs changes in the COM service.
    Without the change, the prefs changes made by the update client were
    never persisted, since the process terminated before such changes were
    lazily committed by the prefs service.
    
    With the change, the prefs are committed with each update complete
    notification for any app. They are also committed when the Stop()
    function is called.
    
    Since the ComServer is a leaky singleton, the implementation of Stop()
    acts like a finalizer, and resets the members of the ComServer instance.
    
    Bug: 1091532
    Change-Id: I07dd0b3d989ad383a0a39e43ed6d47164c389d25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233609
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775793}

--- chrome/updater/server/win/server.cc
+++ chrome/updater/server/win/server.cc
@@ -29,9 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/server/win/com_classes.h"
 #include "chrome/updater/server/win/com_classes_legacy.h"
 #include "chrome/updater/update_service_in_process.h"
+#include "components/prefs/pref_service.h"
 
 namespace updater {
 
+// Returns a leaky singleton of the App instance.
 scoped_refptr<App> AppServerInstance() {
   return AppInstance<ComServerApp>();
 }
@@ -129,7 +131,14 @@ void ComServerApp::CreateWRLModule() {
 void ComServerApp::Stop() {
   VLOG(2) << __func__ << ": COM server is shutting down.";
   UnregisterClassObjects();
-  Shutdown(0);
+  main_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce([]() {
+        auto this_server = ComServerApp::Instance();
+        this_server->config_->GetPrefService()->CommitPendingWrite();
+        this_server->service_ = nullptr;
+        this_server->config_ = nullptr;
+        this_server->Shutdown(0);
+      }));
 }
 
 void ComServerApp::Initialize() {
--- chrome/updater/server/win/server.h
+++ chrome/updater/server/win/server.h
@@ -40,6 +40,9 @@ scoped_refptr<App> AppServerInstance();
 //
 // This class is responsible for the lifetime of the COM server, as well as
 // class factory registration.
+//
+// The instance of the this class is managed by a singleton and it leaks at
+// runtime.
 class ComServerApp : public App {
  public:
   ComServerApp();
@@ -75,7 +78,8 @@ class ComServerApp : public App {
   // Creates an out-of-process WRL Module.
   void CreateWRLModule();
 
-  // Handles object unregistration then triggers program shutdown.
+  // Handles object unregistration then triggers program shutdown. This
+  // function runs on a COM RPC thread when the WRL module is destroyed.
   void Stop();
 
   // Identifier of registered class objects used for unregistration.
