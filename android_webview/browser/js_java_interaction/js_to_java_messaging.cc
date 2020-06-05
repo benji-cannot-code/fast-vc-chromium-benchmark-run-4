ed9f7e7c782f23924149a72467d12b2c5735790e
Scott Violet <sky@chromium.org>
2020-06-05 21:07:03 +0000

    webview: remove unnecessary include
    AFAICT this class doesn't depend upon AwContents.
    
    BUG=none
    TEST=none
    
    Change-Id: I0a0427986e71e31625d58aaa6b6c6840597d1ac2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2232762
    Auto-Submit: Scott Violet <sky@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775760}

--- android_webview/browser/js_java_interaction/js_to_java_messaging.cc
+++ android_webview/browser/js_java_interaction/js_to_java_messaging.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/js_java_interaction/js_to_java_messaging.h"
 
-#include "android_webview/browser/aw_contents.h"
 #include "android_webview/browser_jni_headers/WebMessageListenerHolder_jni.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
