35848167ae104f6e66bf985d047a2922c71d637d
Ella Ge <eirage@chromium.org>
2020-06-05 19:26:44 +0000

    Reland "Update AndroidX.Browser dependency"
    This reverts commit 0ea86868e6cd7c159067da433c1d16f84182842f.
    
    Reason for revert: reland with fix
    
    Original change's description:
    > Revert "Update AndroidX.Browser dependency"
    > 
    > This reverts commit 28b4bf48fb421c92d4884855a1cb9dbe57d5d5d8.
    > 
    > Reason for revert: Compile failure on https://ci.chromium.org/p/chromium/builders/ci/android-archive-rel/12150?
    > 
    > Original change's description:
    > > Update AndroidX.Browser dependency
    > > 
    > > 
    > > Bug: 1069506
    > > Change-Id: I6e6fd53dcb6d8bae3022c1d5d4c5834723156ba0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230159
    > > Commit-Queue: Ella Ge <eirage@chromium.org>
    > > Reviewed-by: Peter Conn <peconn@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#775573}
    > 
    > TBR=peconn@chromium.org,eirage@chromium.org
    > 
    > Change-Id: Ib0c067f24efe4def48a42e47656bcae759206dc1
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1069506
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231823
    > Reviewed-by: Yi Gu <yigu@chromium.org>
    > Commit-Queue: Yi Gu <yigu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#775585}
    
    TBR=peconn@chromium.org,yigu@chromium.org,eirage@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1069506
    Change-Id: Ia8d51c97613028798db4c192979f17afee837fb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2232740
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775702}

--- chrome/browser/android/examples/custom_tabs_client/src/java/org/chromium/customtabsclient/SessionHelper.java
+++ chrome/browser/android/examples/custom_tabs_client/src/java/org/chromium/customtabsclient/SessionHelper.java
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.customtabsclient;
 
-import android.support.annotation.Nullable;
-
+import androidx.annotation.Nullable;
 import androidx.browser.customtabs.CustomTabsSession;
 
 import java.lang.ref.WeakReference;
