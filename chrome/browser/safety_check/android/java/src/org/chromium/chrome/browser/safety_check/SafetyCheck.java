c9b965cfc565d3d708672559b9d52e69f26b51dd
Andrey Zaytsev <andzaytsev@google.com>
2020-06-05 17:37:40 +0000

    Safety check on Android: implemented Safe Browsing bridge
    Bug: 1070620
    Change-Id: I449f09340cf0f834a96d5966b3086702834c18e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2219500
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/master@{#775642}

---
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheck.java
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.safety_check;
+
+import org.chromium.chrome.browser.safety_check.SafetyCheckBridge.SafetyCheckCommonObserver;
+
+/**
+ * Main class for all Safety check related logic.
+ */
+public class SafetyCheck implements SafetyCheckCommonObserver {
+    private SafetyCheckBridge mSafetyCheckBridge;
+
+    public SafetyCheck() {
+        mSafetyCheckBridge = new SafetyCheckBridge(SafetyCheck.this);
+    }
+
+    /**
+     * Triggers a Safe Browsing status check.
+     */
+    public void checkSafeBrowsing() {
+        mSafetyCheckBridge.checkSafeBrowsing();
+    }
+
+    /**
+     * Gets invoked once the Safe Browsing check is completed.
+     * @param status SafetyCheck::SafeBrowsingStatus enum value representing the Safe Browsing
+     *         state (see //components/safety_check/safety_check.h).
+     */
+    @Override
+    public void onSafeBrowsingCheckResult(@SafeBrowsingStatus int status) {}
+}
