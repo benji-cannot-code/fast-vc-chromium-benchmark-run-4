8b29589a730811fa43cdd148913b684acb5c896b
Chris Lu <thegreenfrog@chromium.org>
2020-06-05 14:52:07 +0000

    [ios] Add TranslateInfobarInteractionHandler to Overlay framework
    Bug: 1071914
    Change-Id: I28d0e7f6837188ea87c3a6a0144f7476ebe30c4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2181048
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775564}

--- ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.mm
+++ ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent_util.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/infobars/overlays/browser_agent/infobar_overlay_browser_agent.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/confirm/confirm_infobar_interaction_handler.h"
 #import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/passwords/password_infobar_interaction_handler.h"
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/translate/translate_infobar_interaction_handler.h"
 #import "ios/chrome/browser/ui/infobars/infobar_feature.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -25,6 +26,8 @@ void AttachInfobarOverlayBrowserAgent(Browser* browser) {
       std::make_unique<PasswordInfobarInteractionHandler>(browser));
   browser_agent->AddInfobarInteractionHandler(
       std::make_unique<ConfirmInfobarInteractionHandler>());
+  browser_agent->AddInfobarInteractionHandler(
+      std::make_unique<TranslateInfobarInteractionHandler>());
   // TODO(crbug.com/1030357): Add InfobarInteractionHandlers for each
   // InfobarType when implemented.
 }
