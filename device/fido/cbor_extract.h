2b8e14b8636ff7ad2b05f983ed302bbb22984ea4
Bartek Nowierski <bartekn@chromium.org>
2020-06-05 13:26:50 +0000

    Add CheckedPtr support for cbor_extract::Element
    This is preparation for converting raw pointers to CheckedPtr. See go/miracleptr for details.
    
    Bug: 1080832
    Change-Id: I2bce7496db00bd86acbd31492d8468687bbe44b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224954
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775534}

--- device/fido/cbor_extract.h
+++ device/fido/cbor_extract.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_forward.h"
 #include "base/component_export.h"
 #include "base/containers/span.h"
+#include "base/memory/checked_ptr.h"
 #include "components/cbor/values.h"
 
 namespace device {
@@ -217,8 +218,23 @@ constexpr StepOrByte<S> Element(const Is required,
 }
 
 template <typename S>
+constexpr StepOrByte<S> Element(
+    const Is required,
+    CheckedPtr<const std::vector<uint8_t>> S::*member,
+    uintptr_t offset) {
+  return ElementImpl<S>(required, offset, internal::Type::kBytestring);
+}
+
+template <typename S>
 constexpr StepOrByte<S> Element(const Is required,
                                 const std::string* S::*member,
+                                uintptr_t offset) {
+  return ElementImpl<S>(required, offset, internal::Type::kString);
+}
+
+template <typename S>
+constexpr StepOrByte<S> Element(const Is required,
+                                CheckedPtr<const std::string> S::*member,
                                 uintptr_t offset) {
   return ElementImpl<S>(required, offset, internal::Type::kString);
 }
@@ -232,8 +248,23 @@ constexpr StepOrByte<S> Element(const Is required,
 
 template <typename S>
 constexpr StepOrByte<S> Element(const Is required,
+                                CheckedPtr<const int64_t> S::*member,
+                                uintptr_t offset) {
+  return ElementImpl<S>(required, offset, internal::Type::kInt);
+}
+
+template <typename S>
+constexpr StepOrByte<S> Element(const Is required,
                                 const std::vector<cbor::Value>* S::*member,
                                 uintptr_t offset) {
+  return ElementImpl<S>(required, offset, internal::Type::kArray);
+}
+
+template <typename S>
+constexpr StepOrByte<S> Element(
+    const Is required,
+    CheckedPtr<const std::vector<cbor::Value>> S::*member,
+    uintptr_t offset) {
   return ElementImpl<S>(required, offset, internal::Type::kArray);
 }
 
@@ -246,7 +277,21 @@ constexpr StepOrByte<S> Element(const Is required,
 
 template <typename S>
 constexpr StepOrByte<S> Element(const Is required,
+                                CheckedPtr<const cbor::Value> S::*member,
+                                uintptr_t offset) {
+  return ElementImpl<S>(required, offset, internal::Type::kValue);
+}
+
+template <typename S>
+constexpr StepOrByte<S> Element(const Is required,
                                 const bool* S::*member,
+                                uintptr_t offset) {
+  return ElementImpl<S>(required, offset, internal::Type::kBoolean);
+}
+
+template <typename S>
+constexpr StepOrByte<S> Element(const Is required,
+                                CheckedPtr<const bool> S::*member,
                                 uintptr_t offset) {
   return ElementImpl<S>(required, offset, internal::Type::kBoolean);
 }
