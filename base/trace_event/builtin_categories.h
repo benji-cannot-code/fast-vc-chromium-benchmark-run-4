8e86a47c98a109f3d800f726ce9c910a8cb514f9
Andreas Haas <ahaas@chromium.org>
2020-06-05 10:35:01 +0000

    [tracing] Split v8.wasm category
    This CL follows https://crrev.com/c/2210230 to split the v8.wasm tracing
    category into v8.wasm and v8.wasm.detailed, where v8.wasm.detailed is
    disabled by default.
    
    The v8.wasm category will contain basic events of WebAssembly
    compilation and execution, which can be used in Telemetry benchmarks to
    define metrics. v8.wasm.detailed contains more detailed debugging
    information, but generates too much data for bigger WebAssembly modules
    to be useful in Telemetry benchmarks.
    
    R=eseckler@chromium.org
    
    Bug: chromium:1084929
    Change-Id: Iece9c0a5e1558a8e4eebf32b0468eafc03363890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210246
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775503}

--- base/trace_event/builtin_categories.h
+++ base/trace_event/builtin_categories.h
@@ -153,6 +153,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X("ui")                                                                \
   X("v8")                                                                \
   X("v8.execute")                                                        \
+  X("v8.wasm")                                                           \
   X("ValueStoreFrontend::Backend")                                       \
   X("views")                                                             \
   X("views.frame")                                                       \
@@ -237,7 +238,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime_stats"))                       \
   X(TRACE_DISABLED_BY_DEFAULT("v8.runtime_stats_sampling"))              \
   X(TRACE_DISABLED_BY_DEFAULT("v8.turbofan"))                            \
-  X(TRACE_DISABLED_BY_DEFAULT("v8.wasm"))                                \
+  X(TRACE_DISABLED_BY_DEFAULT("v8.wasm.detailed"))                       \
   X(TRACE_DISABLED_BY_DEFAULT("video_and_image_capture"))                \
   X(TRACE_DISABLED_BY_DEFAULT("viz.debug.overlay_planes"))               \
   X(TRACE_DISABLED_BY_DEFAULT("viz.hit_testing_flow"))                   \
