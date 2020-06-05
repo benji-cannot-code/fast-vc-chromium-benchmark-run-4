d2351ee84a555820b97c9daddb13bc886f2e9c9b
Nate Chapin <japhet@chromium.org>
2020-06-05 01:12:41 +0000

    Ensure document.cloneNode() doesn't crash when document is inactive
    Bug: 1091357
    Test: dom/nodes/Node-cloneNode-on-inactive-document-crash.html
    Change-Id: Ice51a2a24b0625f12000b6d448920842132ffb18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230777
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775391}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -5054,6 +5054,9 @@ bool Document::CanAcceptChild(const Node& new_child,
 Node* Document::Clone(Document& factory, CloneChildrenFlag flag) const {
   DCHECK_EQ(this, &factory)
       << "Document::Clone() doesn't support importNode mode.";
+
+  if (!execution_context_)
+    return nullptr;
   Document* clone = CloneDocumentWithoutChildren();
   clone->CloneDataFromDocument(*this);
   if (flag != CloneChildrenFlag::kSkip)
