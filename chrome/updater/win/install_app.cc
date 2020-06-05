5e266eb0b4f3667245a7ced9a52391e0d27d4dca
Sorin Jianu <sorin@chromium.org>
2020-06-05 15:02:42 +0000

    Use CommitPendingWrite to expedite flushing of prefs file in updater.
    This change is intended to reduce the lag between scheduling the
    prefs writes and the writes landing on persistent storage.
    
    This CL fixes the app install case. The service case is going to
    be addressed in a future CL, as part as the on-demand work.
    
    Bug: 1091532
    Change-Id: I08d0929a97183b4fe3ffc588b8db8c28dcf2fe1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231806
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775570}

--- chrome/updater/win/install_app.cc
+++ chrome/updater/win/install_app.cc
@@ -433,7 +433,7 @@ class InstallAppController
   void DoInstallApp();
   void InstallComplete();
   void HandleInstallResult(const update_client::CrxUpdateItem& update_item);
-  void FlushPrefs();
+  void PrefsCommit();
 
   // Returns the thread id of the thread which owns the progress window.
   DWORD GetUIThreadID() const;
@@ -547,7 +547,7 @@ void InstallAppController::DoInstallApp() {
 // by calling UpdateClient::GetCrxUpdateState.
 void InstallAppController::InstallComplete() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  FlushPrefs();
+  PrefsCommit();
   update_client_ = nullptr;
 }
 
@@ -682,9 +682,9 @@ BOOL InstallAppController::PreTranslateMessage(MSG* msg) {
   return false;
 }
 
-void InstallAppController::FlushPrefs() {
+void InstallAppController::PrefsCommit() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  config_->GetPrefService()->SchedulePendingLossyWrites();
+  config_->GetPrefService()->CommitPendingWrite();
 }
 
 DWORD InstallAppController::GetUIThreadID() const {
