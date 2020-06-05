02fba2baac469811245cfc83c37f6d27ef17440c
Jun Kokatsu <Jun.Kokatsu@microsoft.com>
2020-06-05 21:04:58 +0000

    Trusted Type various WebUI
    This change removes innerHTML usage in various WebUI pages.
    
    Bug: 41905
    Change-Id: I195b7d0bbf040f0bf1443222da75cb8a1734c16f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2226302
    Commit-Queue: Jun Kokatsu <Jun.Kokatsu@microsoft.com>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775757}

--- content/browser/resources/gpu/info_view.js
+++ content/browser/resources/gpu/info_view.js
@@ -538,7 +538,7 @@ cr.define('gpu', function() {
         throw new Error('Node ' + outputElementId + ' not found');
       }
 
-      peg.innerHTML = '';
+      peg.innerHTML = trustedTypes.emptyHTML;
       peg.appendChild(template);
     }
   };
