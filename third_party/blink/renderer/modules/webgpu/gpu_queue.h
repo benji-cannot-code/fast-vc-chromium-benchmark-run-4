7b47f630c975d8916d04972f3517a0a8b0c5efaf
Kai Ninomiya <kainino@chromium.org>
2020-06-05 02:33:27 +0000

    WebGPU: support more array buffer types in writeBuffer
    The fully general [AllowShared] BufferSource, or any of its equivalents:
    - union of ([AllowShared] ArrayBufferView or ArrayBuffer or SharedArrayBuffer)
    - overloads for those three types
    aren't currently possible in Blink's bindings generation. Also:
      - union of ([AllowShared] ArrayBufferView or ArrayBuffer)
    doesn't work due to another bindings generation issue.
    
    This compromise mostly works, except that it doesn't allow SharedArrayBuffer.
    (Shared ArrayBufferView types work, though.)
    
    Spec change: https://github.com/gpuweb/gpuweb/pull/832
    Tested locally on a modified version of the computeBoids sample.
    
    Bug: 1069302, 1088107
    Change-Id: I0ba5f1deebedd2f8ae60f3f1e93183c7a2cbbffb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224995
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775406}

--- third_party/blink/renderer/modules/webgpu/gpu_queue.h
+++ third_party/blink/renderer/modules/webgpu/gpu_queue.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_QUEUE_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBGPU_GPU_QUEUE_H_
 
+#include "third_party/blink/renderer/core/typed_arrays/array_buffer_view_helpers.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_object.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 
@@ -13,7 +14,6 @@ namespace blink {
 
 class CanvasColorParams;
 class DawnTextureFromImageBitmap;
-class DOMArrayBuffer;
 class ExceptionState;
 class GPUBuffer;
 class GPUCommandBuffer;
@@ -36,21 +36,34 @@ class GPUQueue : public DawnObject<WGPUQueue> {
   void signal(GPUFence* fence, uint64_t signal_value);
   GPUFence* createFence(const GPUFenceDescriptor* descriptor);
   void writeBuffer(GPUBuffer* buffer,
-                   uint64_t bufferOffset,
-                   const DOMArrayBuffer* data,
-                   uint64_t dataOffset,
+                   uint64_t buffer_offset,
+                   const MaybeShared<DOMArrayBufferView>& data,
+                   uint64_t data_byte_offset,
                    ExceptionState& exception_state);
   void writeBuffer(GPUBuffer* buffer,
-                   uint64_t bufferOffset,
-                   const DOMArrayBuffer* data,
-                   uint64_t dataOffset,
-                   uint64_t size,
+                   uint64_t buffer_offset,
+                   const MaybeShared<DOMArrayBufferView>& data,
+                   uint64_t data_byte_offset,
+                   uint64_t byte_size,
+                   ExceptionState& exception_state);
+  void writeBuffer(GPUBuffer* buffer,
+                   uint64_t buffer_offset,
+                   const DOMArrayBufferBase* data,
+                   uint64_t data_byte_offset,
+                   ExceptionState& exception_state);
+  void writeBuffer(GPUBuffer* buffer,
+                   uint64_t buffer_offset,
+                   const DOMArrayBufferBase* data,
+                   uint64_t data_byte_offset,
+                   uint64_t byte_size,
                    ExceptionState& exception_state);
   void WriteBufferImpl(GPUBuffer* buffer,
                        uint64_t buffer_offset,
-                       const DOMArrayBuffer* data,
-                       uint64_t data_offset,
-                       base::Optional<uint64_t> size,
+                       uint64_t data_byte_length,
+                       const void* data_base_ptr,
+                       unsigned data_bytes_per_element,
+                       uint64_t data_byte_offset,
+                       base::Optional<uint64_t> byte_size,
                        ExceptionState& exception_state);
   void copyImageBitmapToTexture(
       GPUImageBitmapCopyView* source,
