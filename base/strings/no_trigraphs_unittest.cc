1491fedbb3ff17d0a767504f0bee3a8e319a21f5
Daniel Cheng <dcheng@chromium.org>
2020-06-06 05:41:34 +0000

    Disable trigraphs in pre-C++17 code.
    Bug: 752720
    Change-Id: Iffdd54f4e4305aadfb5d4ed60d0899c33926cef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231592
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775870}

---
+++ base/strings/no_trigraphs_unittest.cc
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/strings/strcat.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(NoTrigraphs, Basic) {
+  EXPECT_EQ("??=", base::StrCat({"?", "?", "="}));
+}
