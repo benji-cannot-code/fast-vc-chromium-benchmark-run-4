a2ca0765fca227af7ba05c5e043aba3d9793b9f6
Sammie Quon <sammiequon@chromium.org>
2020-06-06 00:32:31 +0000

    overview: Make sure to hide all excluded windows.
    Popups like the wifi dialog are excluded from overview, but weren't
    being passed to the scoped object that handles hidding excluded windows.
    
    Fixed: ash_unittests OverviewControllerTest.ExcludedWindowsHidden
    Bug: 1000596
    Change-Id: If52f2e3be2d99f483fd59c8b479a6aa0c0cdcece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228261
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775806}

--- ash/wm/overview/scoped_overview_hide_windows.h
+++ ash/wm/overview/scoped_overview_hide_windows.h
@@ -7,10 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_OVERVIEW_SCOPED_OVERVIEW_HIDE_WINDOWS_H_
 
 #include <map>
-#include <vector>
 
-#include "ash/ash_export.h"
-#include "base/macros.h"
 #include "ui/aura/window_observer.h"
 
 namespace aura {
@@ -21,13 +18,16 @@ namespace ash {
 
 // ScopedOverviewHideWindows hides the list of windows in overview mode,
 // remembers their visibility and recovers the visibility after overview mode.
-class ASH_EXPORT ScopedOverviewHideWindows : public aura::WindowObserver {
+class ScopedOverviewHideWindows : public aura::WindowObserver {
  public:
   // |windows| the list of windows to hide in overview mode. If |force_hidden|
   // is true, the hidden windows may have their visibility altered during
   // overview, but we want to keep them hidden.
   ScopedOverviewHideWindows(const std::vector<aura::Window*>& windows,
                             bool force_hidden);
+  ScopedOverviewHideWindows(const ScopedOverviewHideWindows&) = delete;
+  ScopedOverviewHideWindows& operator=(const ScopedOverviewHideWindows&) =
+      delete;
   ~ScopedOverviewHideWindows() override;
 
   // aura::WindowObserver:
@@ -37,8 +37,6 @@ class ASH_EXPORT ScopedOverviewHideWindows : public aura::WindowObserver {
  private:
   std::map<aura::Window*, bool> window_visibility_;
   bool force_hidden_;
-
-  DISALLOW_COPY_AND_ASSIGN(ScopedOverviewHideWindows);
 };
 
 }  // namespace ash
