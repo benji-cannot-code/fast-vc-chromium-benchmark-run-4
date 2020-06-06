c18ffff6ed58430b3bd70304ad313064d871bf46
Yuheng Huang <yuhengh@chromium.org>
2020-06-06 04:00:35 +0000

    Settings: Add mechanism for preserving subpage search across navigations.
    In <settings-subpage>, if preserve-search-term boolean is set,
    settings-subpage will save the subpage input term to the URL,
    similar to what settings page does to the settings search input.
    When user hits the back button, it will preserve the search input
    from the URL.
    
    At this moment only
    chrome://settings/siteData
    and
    chrome://settings/content/all
    have this flag turned on since they are the only places of search
    results link to another subpage.
    
    Bug: 1049887
    Change-Id: I92782876f2e0ac3167fce6c4980914b4dab9b249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2210731
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775844}

--- chrome/browser/resources/settings/settings_page/settings_subpage.js
+++ chrome/browser/resources/settings/settings_page/settings_subpage.js
@@ -69,6 +69,14 @@ Polymer({
       value: null,
     },
 
+    /**
+     * Whether the subpage search term should be preserved across navigations.
+     */
+    preserveSearchTerm: {
+      type: Boolean,
+      value: false,
+    },
+
     /** @private */
     active_: {
       type: Boolean,
@@ -99,6 +107,53 @@ Polymer({
     }
   },
 
+  /**
+   * @return {!Promise<!CrSearchFieldElement>}
+   * @private
+   */
+  getSearchField_() {
+    let searchField = this.$$('cr-search-field');
+    if (searchField) {
+      return Promise.resolve(searchField);
+    }
+
+    return new Promise(resolve => {
+      listenOnce(this, 'dom-change', () => {
+        searchField = this.$$('cr-search-field');
+        resolve(assert(searchField));
+      });
+    });
+  },
+
+  /**
+   * Restore search field value from URL search param
+   * @private
+   */
+  restoreSearchInput_() {
+    const searchField = this.$$('cr-search-field');
+    if (assert(searchField)) {
+      const urlSearchQuery =
+          settings.Router.getInstance().getQueryParameters().get(
+              'searchSubpage') ||
+          '';
+      this.searchTerm = urlSearchQuery;
+      searchField.setValue(urlSearchQuery);
+    }
+  },
+
+  /**
+   * Preserve search field value to URL search param
+   * @private
+   */
+  preserveSearchInput_() {
+    const query = this.searchTerm;
+    const searchParams = query.length > 0 ?
+        new URLSearchParams('searchSubpage=' + encodeURIComponent(query)) :
+        undefined;
+    const currentRoute = settings.Router.getInstance().getCurrentRoute();
+    settings.Router.getInstance().navigateTo(currentRoute, searchParams);
+  },
+
   /** Focuses the back button when page is loaded. */
   initialFocus() {
     if (this.hideCloseButton) {
@@ -111,6 +166,9 @@ Polymer({
   /** @protected */
   currentRouteChanged(route) {
     this.active_ = this.getAttribute('route-path') == route.path;
+    if (this.active_ && this.searchLabel && this.preserveSearchTerm) {
+      this.getSearchField_().then(() => this.restoreSearchInput_());
+    }
   },
 
   /** @private */
@@ -157,7 +215,14 @@ Polymer({
 
   /** @private */
   onSearchChanged_(e) {
+    if (this.searchTerm === e.detail) {
+      return;
+    }
+
     this.searchTerm = e.detail;
+    if (this.preserveSearchTerm && this.active_) {
+      this.preserveSearchInput_();
+    }
   },
 
   /** @private */
