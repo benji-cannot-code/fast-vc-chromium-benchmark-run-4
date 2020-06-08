122f3284ec1db7f2fb8075248b5ff5c2be50e4c7
Lucas Gadani <lfg@chromium.org>
2020-06-08 18:58:10 +0000

    Portals: XFO sameorigin now traverses the portal hierarchy.
    This allows portals to support XFO sameorigin policies.
    
    Bug: 1083329
    Change-Id: I922fd762ed643c1381706145aa3a017254456ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216537
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776135}

--- content/browser/frame_host/ancestor_throttle.cc
+++ content/browser/frame_host/ancestor_throttle.cc
@@ -306,7 +306,8 @@ AncestorThrottle::CheckResult AncestorThrottle::EvaluateXFrameOptions(
 
     case HeaderDisposition::SAMEORIGIN: {
       // Block the request when any ancestor is not same-origin.
-      RenderFrameHostImpl* parent = request->GetParentFrame();
+      RenderFrameHostImpl* parent = ParentForAncestorThrottle(
+          request->frame_tree_node()->current_frame_host());
       url::Origin current_origin =
           url::Origin::Create(navigation_handle()->GetURL());
       while (parent) {
@@ -328,7 +329,7 @@ AncestorThrottle::CheckResult AncestorThrottle::EvaluateXFrameOptions(
 
           return CheckResult::BLOCK;
         }
-        parent = parent->GetParent();
+        parent = ParentForAncestorThrottle(parent);
       }
       RecordXFrameOptionsUsage(XFrameOptionsHistogram::SAMEORIGIN);
       return CheckResult::PROCEED;
