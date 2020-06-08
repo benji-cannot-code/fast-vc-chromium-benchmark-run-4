b946d2b2f828d22cdf85fec305d087f235330256
Marc Treib <treib@chromium.org>
2020-06-08 12:48:11 +0000

    Clear account storage settings when account is removed from cookies
    When an account is fully removed from the signin cookies (i.e. doesn't
    show up in either signed-in or signed-out accounts), then any
    account-storage-related settings should also be cleared, most notably
    the opt-in bit.
    
    Bug: 1052005, 1079297
    Change-Id: I6b5441abd60fb05e8001e770e46f72b8d083ea04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2232327
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776006}

--- components/password_manager/core/browser/sync/password_model_type_controller.cc
+++ components/password_manager/core/browser/sync/password_model_type_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/password_manager/core/browser/password_manager_features_util.h"
 #include "components/prefs/pref_service.h"
+#include "components/signin/public/identity_manager/accounts_in_cookie_jar_info.h"
 #include "components/sync/base/model_type.h"
 #include "components/sync/driver/sync_client.h"
 #include "components/sync/driver/sync_service.h"
@@ -95,6 +96,29 @@ void PasswordModelTypeController::OnStateChanged(syncer::SyncService* sync) {
   DCHECK(CalledOnValidThread());
   sync_service_->DataTypePreconditionChanged(syncer::PASSWORDS);
   state_changed_callback_.Run();
+}
+
+void PasswordModelTypeController::OnAccountsInCookieUpdated(
+    const signin::AccountsInCookieJarInfo& accounts_in_cookie_jar_info,
+    const GoogleServiceAuthError& error) {
+  // If the account information is stale, do nothing for now - wait until there
+  // is fresh information.
+  if (!accounts_in_cookie_jar_info.accounts_are_fresh) {
+    return;
+  }
+  // Collect all the known accounts (signed-in or signed-out).
+  std::vector<std::string> gaia_ids;
+  for (const gaia::ListedAccount& account :
+       accounts_in_cookie_jar_info.signed_in_accounts) {
+    gaia_ids.push_back(account.gaia_id);
+  }
+  for (const gaia::ListedAccount& account :
+       accounts_in_cookie_jar_info.signed_out_accounts) {
+    gaia_ids.push_back(account.gaia_id);
+  }
+  // Keep any account-storage settings only for known accounts.
+  features_util::KeepAccountStorageSettingsOnlyForUsers(pref_service_,
+                                                        gaia_ids);
 }
 
 void PasswordModelTypeController::OnAccountsCookieDeletedByUserAction() {
