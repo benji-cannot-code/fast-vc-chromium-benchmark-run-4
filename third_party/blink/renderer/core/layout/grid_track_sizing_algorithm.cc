0a686bb8056c6b67e5bd7ed3418bdbe805b4cbe4
Javier Fernandez <jfernandez@igalia.com>
2020-06-08 14:57:55 +0000

    [css-grid] Considering baseline shim when computing max-content size
    When computing the MaxContentForChild size for the tracks by the
    IndefiniteStrategy we were not considering the baseline shim, since we
    assumed it was enough to do it for the MinContentSize.
    
    However, there are possible definitions of the grid track that cause
    the MinContentForChild function to not be called, leading to an
    incorrect intrinsic size for the grid.
    
    Bug: 1091944
    Change-Id: I91a35852dd044ded0fc14f03e97d3e140d80ba12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233239
    Reviewed-by: Manuel Rego <rego@igalia.com>
    Commit-Queue: Javier Fernandez <jfernandez@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#776040}

--- third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.cc
+++ third_party/blink/renderer/core/layout/grid_track_sizing_algorithm.cc
@@ -799,7 +799,9 @@ LayoutUnit IndefiniteSizeStrategy::MaxContentForChild(LayoutBox& child) const {
   DCHECK(GridLayoutUtils::IsOrthogonalChild(*GetLayoutGrid(), child));
 
   return child.LogicalHeight() +
-         GridLayoutUtils::MarginLogicalHeightForChild(*GetLayoutGrid(), child);
+         GridLayoutUtils::MarginLogicalHeightForChild(*GetLayoutGrid(), child) +
+         algorithm_.BaselineOffsetForChild(child,
+                                           GridAxisForDirection(Direction()));
 }
 
 bool IndefiniteSizeStrategy::IsComputingSizeContainment() const {
