2fa93bd91df9e2ca272aa438afcf2aae1481f591
Jian Li <jianli@chromium.org>
2020-06-08 17:37:10 +0000

    Remove the construction of FeedStreamSurface that is not needed
    Bug: none
    Change-Id: Ia397468378c7a94d04024fb3f444995c1003efea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2234548
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776101}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceCoordinator.java
@@ -40,8 +40,6 @@ import org.chromium.chrome.browser.feed.shared.stream.Header;
 import org.chromium.chrome.browser.feed.shared.stream.NonDismissibleHeader;
 import org.chromium.chrome.browser.feed.shared.stream.Stream;
 import org.chromium.chrome.browser.feed.tooltip.BasicTooltipApi;
-import org.chromium.chrome.browser.feed.v2.FeedServiceBridge;
-import org.chromium.chrome.browser.feed.v2.FeedStreamSurface;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.native_page.ContextMenuManager;
 import org.chromium.chrome.browser.native_page.NativePageNavigationDelegate;
@@ -82,7 +80,6 @@ public class FeedSurfaceCoordinator implements FeedSurfaceProvider {
     private final int mDefaultMargin;
     private final int mWideMargin;
     private final FeedSurfaceMediator mMediator;
-    private FeedStreamSurface mFeedStreamSurface;
 
     private UiConfig mUiConfig;
     private FrameLayout mRootView;
@@ -344,15 +341,6 @@ public class FeedSurfaceCoordinator implements FeedSurfaceProvider {
             mHomepagePromoController =
                     new HomepagePromoController(mActivity, mSnackbarManager, mTracker, mMediator);
             mMediator.onHomepagePromoStateChange();
-        }
-
-        // Native should already have been loaded because of FeedSurfaceMediator.
-        if (FeedServiceBridge.isEnabled()) {
-            // TODO(jianli): Temporary: simulate opening the feed V2 surface. This should probably
-            // move to FeedSurfaceMediator.
-            mFeedStreamSurface = new FeedStreamSurface(
-                    tabModelSelector, tabProvider, mActivity, mSnackbarManager);
-            mFeedStreamSurface.surfaceOpened();
         }
 
         mUserEducationHelper = new UserEducationHelper(mActivity);
