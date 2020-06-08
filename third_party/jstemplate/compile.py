7ed86f1a2309697f6ed50928f2f936dda9705b02
Jun Kokatsu <Jun.Kokatsu@microsoft.com>
2020-06-08 03:38:10 +0000

    Create Trusted Type policy for jstemplate
    This change adds Trusted Type policy for jstemplate.
    
    Bug: 41905
    Change-Id: I0cd5fccd5e58ee47cd5e7a1d23c65069487b72a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215706
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Commit-Queue: Jun Kokatsu <Jun.Kokatsu@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#775923}

--- third_party/jstemplate/compile.py
+++ third_party/jstemplate/compile.py
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Combines the javascript files needed by jstemplate into a single file."""
 
 import httplib
+import sys
 import urllib
 
 
