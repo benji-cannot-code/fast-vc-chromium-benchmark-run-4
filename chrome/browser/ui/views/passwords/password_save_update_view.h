d0258da8795d214c37a7b3042eaf7d5072c8ee43
Mohamed Amir Yosef <mamir@chromium.org>
2020-06-08 13:09:13 +0000

    [Passwords] Clean up obsolete PASSWORD_STORE_SELECT_ENABLED
    Bug: 1044038
    Change-Id: I01eb844b2d73563e5e394d6ab725bc2e9d056928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2232840
    Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776009}

--- chrome/browser/ui/views/passwords/password_save_update_view.h
+++ chrome/browser/ui/views/passwords/password_save_update_view.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 class EditableCombobox;
 class ToggleImageButton;
-
-#if defined(PASSWORD_STORE_SELECT_ENABLED)
-class Checkbox;
-#endif  // defined(PASSWORD_STORE_SELECT_ENABLED)
 }  // namespace views
 
 class PasswordSignInPromoView;
@@ -86,10 +82,6 @@ class PasswordSaveUpdateView : public PasswordBubbleViewBase,
 
   // The view for the password value.
   views::EditableCombobox* password_dropdown_;
-
-#if defined(PASSWORD_STORE_SELECT_ENABLED)
-  views::Checkbox* account_store_checkbox_ = nullptr;
-#endif  // defined(PASSWORD_STORE_SELECT_ENABLED)
 
   bool are_passwords_revealed_;
 };
