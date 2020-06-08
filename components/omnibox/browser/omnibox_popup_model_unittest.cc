c13a626968eb554695c6e0b58e4ff4371055c6b8
Tommy Li <tommycli@chromium.org>
2020-06-08 22:03:57 +0000

    [omnibox] OmniboxPopupModel: Fold SetSelectedLine into SetSelection
    Currently, there's all three methods:
     1. SetSelection
     2. SetSelectedLine
     3. SetSelectedLineState
    
    SetSelection calls both SetSelectedLine and SetSelectedLineState.
    That has worked well so far, but this will cause a problem for section
    headers.
    
    The problem is: SetSelectedLine is what updates the textbox inline
    autocomplete. With Headers, we may need to update the inline
    autocomplete when the line state changes (from header to match), rather
    than when the line number changes.
    
    Philosophically, I think this is also a good change, because in the
    omnibox code, there should be one, and preferably only one, obvious way
    to do something.
    
    This CL is not intended to change any user facing behavior but it does
    change some of the logic of what happens in some circumstances or in
    what order. So it's possible that some unanticipated changes may arise.
    If so, they will be addressed in subsequent CLs.
    
    I'll fix the actual inline autocomplete behavior for headers in a
    followup CL.
    
    Bug: 1078183, 1052522
    Change-Id: I3c086de080acf03eeaba72a6ac358408ca3462f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2227453
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776216}

--- components/omnibox/browser/omnibox_popup_model_unittest.cc
+++ components/omnibox/browser/omnibox_popup_model_unittest.cc
@@ -99,9 +99,9 @@ TEST_F(OmniboxPopupModelTest, SetSelectedLine) {
   result->SortAndCull(input, nullptr);
   popup_model()->OnResultChanged();
   EXPECT_TRUE(popup_model()->SelectionOnInitialLine());
-  popup_model()->SetSelectedLine(0, true, false);
+  popup_model()->SetSelection(OmniboxPopupModel::Selection(0), true, false);
   EXPECT_TRUE(popup_model()->SelectionOnInitialLine());
-  popup_model()->SetSelectedLine(0, false, false);
+  popup_model()->SetSelection(OmniboxPopupModel::Selection(0), false, false);
   EXPECT_TRUE(popup_model()->SelectionOnInitialLine());
 }
 
@@ -125,11 +125,11 @@ TEST_F(OmniboxPopupModelTest, SetSelectedLineWithNoDefaultMatches) {
   EXPECT_EQ(OmniboxPopupModel::kNoMatch, popup_model()->selected_line());
   EXPECT_TRUE(popup_model()->SelectionOnInitialLine());
 
-  popup_model()->SetSelectedLine(0, false, false);
+  popup_model()->SetSelection(OmniboxPopupModel::Selection(0), false, false);
   EXPECT_EQ(0U, popup_model()->selected_line());
   EXPECT_FALSE(popup_model()->SelectionOnInitialLine());
 
-  popup_model()->SetSelectedLine(1, false, false);
+  popup_model()->SetSelection(OmniboxPopupModel::Selection(1), false, false);
   EXPECT_EQ(1U, popup_model()->selected_line());
   EXPECT_FALSE(popup_model()->SelectionOnInitialLine());
 
@@ -513,12 +513,12 @@ TEST_F(OmniboxPopupModelTest, TestFocusFixing) {
   result->AppendMatches(input, matches);
   result->SortAndCull(input, nullptr);
   popup_model()->OnResultChanged();
-  popup_model()->SetSelectedLine(0, true, false);
+  popup_model()->SetSelection(OmniboxPopupModel::Selection(0), true, false);
   // The default state should be unfocused.
   EXPECT_EQ(OmniboxPopupModel::NORMAL, popup_model()->selected_line_state());
 
   // Focus the selection.
-  popup_model()->SetSelectedLine(0, false, false);
+  popup_model()->SetSelection(OmniboxPopupModel::Selection(0));
   popup_model()->SetSelectedLineState(OmniboxPopupModel::BUTTON_FOCUSED);
   EXPECT_EQ(OmniboxPopupModel::BUTTON_FOCUSED,
             popup_model()->selected_line_state());
@@ -536,17 +536,7 @@ TEST_F(OmniboxPopupModelTest, TestFocusFixing) {
             popup_model()->selected_line_state());
 
   // Changing selection should change focused state.
-  popup_model()->SetSelectedLine(1, false, false);
-  EXPECT_EQ(OmniboxPopupModel::NORMAL, popup_model()->selected_line_state());
-
-  // Changing selection to same selection might change state.
-  popup_model()->SetSelectedLineState(OmniboxPopupModel::BUTTON_FOCUSED);
-  // Letting routine filter selecting same line should not change it.
-  popup_model()->SetSelectedLine(1, false, false);
-  EXPECT_EQ(OmniboxPopupModel::BUTTON_FOCUSED,
-            popup_model()->selected_line_state());
-  // Forcing routine to handle selecting same line should change it.
-  popup_model()->SetSelectedLine(1, false, true);
+  popup_model()->SetSelection(OmniboxPopupModel::Selection(1));
   EXPECT_EQ(OmniboxPopupModel::NORMAL, popup_model()->selected_line_state());
 
   // Adding a match at end will reset selection to first, so should change
@@ -575,7 +565,8 @@ TEST_F(OmniboxPopupModelTest, TestFocusFixing) {
 
   // Selecting |kNoMatch| should clear focus.
   popup_model()->SetSelectedLineState(OmniboxPopupModel::BUTTON_FOCUSED);
-  popup_model()->SetSelectedLine(OmniboxPopupModel::kNoMatch, false, false);
+  popup_model()->SetSelection(
+      OmniboxPopupModel::Selection(OmniboxPopupModel::kNoMatch));
   popup_model()->OnResultChanged();
   EXPECT_EQ(OmniboxPopupModel::NORMAL, popup_model()->selected_line_state());
 }
