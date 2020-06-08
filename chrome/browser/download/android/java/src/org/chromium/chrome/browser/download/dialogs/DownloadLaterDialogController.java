67a9fd0df82f25b566d58a7fee7e6fdebf5eae62
Xing Liu <xingliu@chromium.org>
2020-06-08 03:30:09 +0000

    Download later: Implement selection logic for download later dialog.
    Implements the selection logic for download later dialog.
    
    Bug: 1078454
    Change-Id: I81bd1d0d38abe29c499d795959f2e2da5af064b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231848
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775909}

---
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadLaterDialogController.java
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download.dialogs;
+
+/**
+ * Receives events from download later dialog.
+ */
+public interface DownloadLaterDialogController {
+    /**
+     * Called when the selection changed in the download later dialog.
+     * @param choice The selection of the download time in the download later dialog.
+     */
+    void onChoiceChanged(@DownloadLaterDialogChoice int choice);
+}
