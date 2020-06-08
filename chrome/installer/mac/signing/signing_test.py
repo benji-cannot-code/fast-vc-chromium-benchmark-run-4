7d0f83ba1352ee394e05d3874b0733a5f59190c4
Robert Sesek <rsesek@chromium.org>
2020-06-08 15:45:05 +0000

    mac: Fix signing script when using a relative --output dir.
    This makes the model.Paths run os.path.abspath on its arguments. This is a
    large mechanical change because tests did not use absolute paths before;
    most of the CL was generated by:
      gsed -i -E -e 's@\$(W|I|O)@/$\1@g' chrome/installer/mac/signing/*_test.py
    
    Bug: 964608
    Change-Id: Iac8d1213e47fd8f08daf4962a756e47dab26d64e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231440
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776054}

--- chrome/installer/mac/signing/signing_test.py
+++ chrome/installer/mac/signing/signing_test.py
@@ -20,7 +20,7 @@ except NameError:
 class TestSignPart(unittest.TestCase):
 
     def setUp(self):
-        self.paths = model.Paths('$I', '$O', '$W')
+        self.paths = model.Paths('/$I', '/$O', '/$W')
         self.config = test_config.TestConfig()
 
     def test_sign_part(self, run_command):
@@ -28,7 +28,7 @@ class TestSignPart(unittest.TestCase):
         signing.sign_part(self.paths, self.config, part)
         run_command.assert_called_once_with([
             'codesign', '--sign', '[IDENTITY]', '--timestamp', '--requirements',
-            '=designated => identifier "test.signing.app"', '$W/Test.app'
+            '=designated => identifier "test.signing.app"', '/$W/Test.app'
         ])
 
     def test_sign_part_no_notary(self, run_command):
@@ -37,7 +37,7 @@ class TestSignPart(unittest.TestCase):
         signing.sign_part(self.paths, config, part)
         run_command.assert_called_once_with([
             'codesign', '--sign', '[IDENTITY]', '--requirements',
-            '=designated => identifier "test.signing.app"', '$W/Test.app'
+            '=designated => identifier "test.signing.app"', '/$W/Test.app'
         ])
 
     def test_sign_part_no_identifier_requirement(self, run_command):
@@ -45,7 +45,7 @@ class TestSignPart(unittest.TestCase):
             'Test.app', 'test.signing.app', identifier_requirement=False)
         signing.sign_part(self.paths, self.config, part)
         run_command.assert_called_once_with(
-            ['codesign', '--sign', '[IDENTITY]', '--timestamp', '$W/Test.app'])
+            ['codesign', '--sign', '[IDENTITY]', '--timestamp', '/$W/Test.app'])
 
     def test_sign_with_identifier(self, run_command):
         part = model.CodeSignedProduct(
@@ -54,7 +54,7 @@ class TestSignPart(unittest.TestCase):
         run_command.assert_called_once_with([
             'codesign', '--sign', '[IDENTITY]', '--timestamp', '--identifier',
             'test.signing.app', '--requirements',
-            '=designated => identifier "test.signing.app"', '$W/Test.app'
+            '=designated => identifier "test.signing.app"', '/$W/Test.app'
         ])
 
     def test_sign_with_identifier_no_requirement(self, run_command):
@@ -66,7 +66,7 @@ class TestSignPart(unittest.TestCase):
         signing.sign_part(self.paths, self.config, part)
         run_command.assert_called_once_with([
             'codesign', '--sign', '[IDENTITY]', '--timestamp', '--identifier',
-            'test.signing.app', '$W/Test.app'
+            'test.signing.app', '/$W/Test.app'
         ])
 
     def test_sign_part_with_options(self, run_command):
@@ -79,7 +79,7 @@ class TestSignPart(unittest.TestCase):
         run_command.assert_called_once_with([
             'codesign', '--sign', '[IDENTITY]', '--timestamp', '--requirements',
             '=designated => identifier "test.signing.app"', '--options',
-            'restrict,library', '$W/Test.app'
+            'restrict,library', '/$W/Test.app'
         ])
 
     def test_sign_part_with_entitlements(self, run_command):
@@ -91,7 +91,7 @@ class TestSignPart(unittest.TestCase):
         signing.sign_part(self.paths, self.config, part)
         run_command.assert_called_once_with([
             'codesign', '--sign', '[IDENTITY]', '--timestamp', '--entitlements',
-            '$W/entitlements.plist', '$W/Test.app'
+            '/$W/entitlements.plist', '/$W/Test.app'
         ])
 
     def test_verify_part(self, run_command):
@@ -100,9 +100,9 @@ class TestSignPart(unittest.TestCase):
         self.assertEqual(run_command.mock_calls, [
             mock.call([
                 'codesign', '--display', '--verbose=5', '--requirements', '-',
-                '$W/Test.app'
+                '/$W/Test.app'
             ]),
-            mock.call(['codesign', '--verify', '--verbose=6', '$W/Test.app']),
+            mock.call(['codesign', '--verify', '--verbose=6', '/$W/Test.app']),
         ])
 
     def test_verify_part_with_options(self, run_command):
@@ -115,10 +115,10 @@ class TestSignPart(unittest.TestCase):
         self.assertEqual(run_command.mock_calls, [
             mock.call([
                 'codesign', '--display', '--verbose=5', '--requirements', '-',
-                '$W/Test.app'
+                '/$W/Test.app'
             ]),
             mock.call([
                 'codesign', '--verify', '--verbose=6', '--deep',
-                '--ignore-resources', '$W/Test.app'
+                '--ignore-resources', '/$W/Test.app'
             ]),
         ])
