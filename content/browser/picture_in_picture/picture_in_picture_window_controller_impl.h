f6f9bc6bc25dca99708faefcf9bdded9a4c531fe
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2020-06-08 16:53:56 +0000

    Sort out naming of play/pause button visibility functions
    SetAlwaysHidePlayPauseButton() was confusing, because it's actually used
    to set visibility rather than invisibility.
    
    Also, SetShowPlayPauseButton() doesn't need to be a part of the
    PictureInPictureWindowController interface.
    
    Change-Id: I2dd32b64b44d3318a6cc1906070b439190e162b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231068
    Reviewed-by: François Beaufort <beaufort.francois@gmail.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Cr-Commit-Position: refs/heads/master@{#776070}

--- content/browser/picture_in_picture/picture_in_picture_window_controller_impl.h
+++ content/browser/picture_in_picture/picture_in_picture_window_controller_impl.h
@@ -66,7 +66,6 @@ class CONTENT_EXPORT PictureInPictureWindowControllerImpl
   bool TogglePlayPause() override;
   void UpdatePlaybackState(bool is_playing,
                            bool reached_end_of_stream) override;
-  void SetAlwaysHidePlayPauseButton(bool is_visible) override;
   void SkipAd() override;
   void NextTrack() override;
   void PreviousTrack() override;
@@ -87,6 +86,8 @@ class CONTENT_EXPORT PictureInPictureWindowControllerImpl
   // Embeds a surface in the Picture-in-Picture window.
   void EmbedSurface(const viz::SurfaceId& surface_id,
                     const gfx::Size& natural_size);
+
+  void SetShowPlayPauseButton(bool show_play_pause_button);
 
   // Called by PictureInPictureServiceImpl when a session request is received.
   // The call should return the |session_remote| and |window_size| as out
@@ -154,7 +155,7 @@ class CONTENT_EXPORT PictureInPictureWindowControllerImpl
   // Used to hide play/pause button if video is a MediaStream or has infinite
   // duration. Play/pause button visibility can be overridden by the Media
   // Session API in UpdatePlayPauseButtonVisibility().
-  bool always_hide_play_pause_button_ = false;
+  bool always_show_play_pause_button_ = false;
 
   // Session currently associated with the Picture-in-Picture window. The
   // session object makes the bridge with the renderer process by handling
