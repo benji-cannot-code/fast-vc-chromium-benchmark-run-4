a2d85daedff0cd6d5608737817b6200995a122c5
Kent Tamura <tkent@chromium.org>
2020-06-08 08:56:03 +0000

    RubyNG: Implement annotation space sharing with previous lines
    - A line layout provides logical bottom line space via NGLayoutResult
    
    - NGBlockLayoutAlgorithm passes it to the NGConstraintSpace for the next
      line layout
    
    - The next line doesn't expand line space if the bottom line space of
      the previous line is enough.
    
    
    Note that this CL does not support annotation space sharing with next
    lines, and does not support FlippedLinesWritingMode yet.
    
    Bug: 1069817
    Change-Id: Id642a170379995985ce15c1ecbc3d2fb9a417bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230437
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#775974}

--- third_party/blink/renderer/core/layout/ng/ng_container_fragment_builder.h
+++ third_party/blink/renderer/core/layout/ng/ng_container_fragment_builder.h
@@ -179,6 +179,11 @@ class CORE_EXPORT NGContainerFragmentBuilder : public NGFragmentBuilder {
     annotation_overflow_ = overflow;
   }
 
+  // See NGLayoutRsult::BlockEndAnnotatioSpace().
+  void SetBlockEndAnnotationSpace(LayoutUnit space) {
+    block_end_annotation_space_ = space;
+  }
+
   const NGConstraintSpace* ConstraintSpace() const { return space_; }
 
 #if DCHECK_IS_ON()
@@ -233,6 +238,8 @@ class CORE_EXPORT NGContainerFragmentBuilder : public NGFragmentBuilder {
 
   // See NGLayoutResult::AnnotationOverflow().
   LayoutUnit annotation_overflow_;
+  // See NGLayoutResult::BlockEndAnotationSpace().
+  LayoutUnit block_end_annotation_space_;
 
   NGAdjoiningObjectTypes adjoining_object_types_ = kAdjoiningNone;
   bool has_adjoining_object_descendants_ = false;
