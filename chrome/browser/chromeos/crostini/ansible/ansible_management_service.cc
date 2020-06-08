a7c8fd85a0fb0ec68b75d180a5cfb141ba966dc9
Joel Hockey <joelhockey@chromium.org>
2020-06-08 23:29:49 +0000

    Define crostini string constants in cc rather than h
    Change-Id: I5c6a219afe20f27d441baac09c63105c613dfec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235167
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776261}

--- chrome/browser/chromeos/crostini/ansible/ansible_management_service.cc
+++ chrome/browser/chromeos/crostini/ansible/ansible_management_service.cc
@@ -23,6 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace crostini {
 
+const char kCrostiniDefaultAnsibleVersion[] =
+    "ansible;2.2.1.0-2+deb9u1;all;debian-stable-main";
+
 namespace {
 
 chromeos::CiceroneClient* GetCiceroneClient() {
