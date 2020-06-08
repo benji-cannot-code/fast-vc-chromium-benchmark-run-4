6d618858470b2e996c06fe66a9a0f5b666e1c34b
Simon Pelchat <spelchat@chromium.org>
2020-06-08 19:02:02 +0000

    Add variation ID for subresource redirect tests.
    The litepages server will soon start blocking requests that don't have a GWS ID
    from one of the explicitly allowed GWS IDs. GWS ID -1 will be a special GWS ID
    used for prober requests and integration tests and will always be allowed by the
    server.
    
    Bug: 1092538
    Change-Id: I0ef8e8c034847a944c5dbd750df78911f0f3da6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236046
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Auto-Submit: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: rajendrant <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776140}

--- tools/chrome_proxy/webdriver/subresource_redirect.py
+++ tools/chrome_proxy/webdriver/subresource_redirect.py
@@ -15,6 +15,7 @@ class SubresourceRedirect(IntegrationTest):
 
   def enableSubresourceRedirectFeature(self, test_driver):
     test_driver.EnableChromeFeature('SubresourceRedirect<SubresourceRedirect')
+    test_driver.AddChromeArg('--force-variation-ids=-1')
     test_driver.AddChromeArg('--force-fieldtrials=SubresourceRedirect/Enabled')
     test_driver.AddChromeArg(
         '--force-fieldtrial-params='
