91acaf74a9d212ed95b4da4e229cd08815880aef
Roman Sorokin <rsorokin@chromium.org>
2020-06-08 12:23:49 +0000

    Reland "cros login: Always create WC in LDHMojo ctor"
    This is a reland of 8745743cba4ea5b5bf9929d4a07a246499dcb303
    
    Original change's description:
    > cros login: Always create WC in LDHMojo ctor
    > 
    > Always create WizardController in LoginDisplayHostMojo. It creates all
    > the screen objects. Postpone initiliazation until first screen should be
    > shown.
    > 
    > Not needed for LoginDisplayHostWebui - WizardController is already
    > always created there.
    > 
    > Bug: 1079199
    > Change-Id: I989322ed98eae5edafa334d20fccea7bd1a498d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230472
    > Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    > Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#775526}
    
    Bug: 1079199
    Change-Id: Ibfbf6c46f592a479fbf8aa8b28d7ea0fd84aec68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233439
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776004}

--- chrome/browser/chromeos/login/active_directory_login_browsertest.cc
+++ chrome/browser/chromeos/login/active_directory_login_browsertest.cc
@@ -104,7 +104,6 @@ class ActiveDirectoryLoginAutocompleteTest : public ActiveDirectoryLoginTest {
 
 // Test successful Active Directory login.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, LoginSuccess) {
-  OobeBaseTest::WaitForSigninScreen();
   ASSERT_TRUE(InstallAttributes::Get()->IsActiveDirectoryManaged());
   ad_login_.TestNoError();
   ad_login_.TestDomainHidden();
@@ -116,7 +115,6 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, LoginSuccess) {
 // Tests that the Kerberos SSO environment variables are set correctly after
 // an Active Directory log in.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, KerberosVarsCopied) {
-  OobeBaseTest::WaitForSigninScreen();
   ad_login_.TestNoError();
   ad_login_.TestDomainHidden();
   ad_login_.SubmitActiveDirectoryCredentials(test_user_, kPassword);
@@ -135,7 +133,6 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, KerberosVarsCopied) {
 
 // Test different UI errors for Active Directory login.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, LoginErrors) {
-  OobeBaseTest::WaitForSigninScreen();
   ASSERT_TRUE(InstallAttributes::Get()->IsActiveDirectoryManaged());
   ad_login_.TestNoError();
   ad_login_.TestDomainHidden();
@@ -176,7 +173,6 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, LoginErrors) {
 
 // Test successful Active Directory login from the password change screen.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, PasswordChange_LoginSuccess) {
-  OobeBaseTest::WaitForSigninScreen();
   ASSERT_TRUE(InstallAttributes::Get()->IsActiveDirectoryManaged());
   ad_login_.TestLoginVisible();
   ad_login_.TestDomainHidden();
@@ -193,7 +189,6 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, PasswordChange_LoginSuccess) {
 
 // Test different UI errors for Active Directory password change screen.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, PasswordChange_UIErrors) {
-  OobeBaseTest::WaitForSigninScreen();
   ASSERT_TRUE(InstallAttributes::Get()->IsActiveDirectoryManaged());
   ad_login_.TestLoginVisible();
   ad_login_.TestDomainHidden();
@@ -228,7 +223,6 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest, PasswordChange_UIErrors) {
 // Test reopening Active Directory password change screen clears errors.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest,
                        PasswordChange_ReopenClearErrors) {
-  OobeBaseTest::WaitForSigninScreen();
   ASSERT_TRUE(InstallAttributes::Get()->IsActiveDirectoryManaged());
   ad_login_.TestLoginVisible();
   ad_login_.TestDomainHidden();
@@ -247,7 +241,6 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginTest,
 
 // Tests that autocomplete works. Submits username without domain.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginAutocompleteTest, LoginSuccess) {
-  OobeBaseTest::WaitForSigninScreen();
   ASSERT_TRUE(InstallAttributes::Get()->IsActiveDirectoryManaged());
   ad_login_.TestNoError();
   ad_login_.TestDomainVisible();
@@ -260,7 +253,6 @@ IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginAutocompleteTest, LoginSuccess) {
 
 // Tests that user could override autocomplete domain.
 IN_PROC_BROWSER_TEST_F(ActiveDirectoryLoginAutocompleteTest, TestAutocomplete) {
-  OobeBaseTest::WaitForSigninScreen();
   ASSERT_TRUE(InstallAttributes::Get()->IsActiveDirectoryManaged());
 
   ad_login_.TestLoginVisible();
