23c1a8f47bbf4362fa5aeb289b900dbb64d7e800
Anastasia Helfinstein <anastasi@google.com>
2020-06-09 01:43:33 +0000

    [Switch Access] Add fallback behavior for custom sliders
    Currently, there is no possible way for custom sliders to expose an
    increment/decrement action to the automation tree. This adds a fallback
    behavior of using simulated arrow key presses.
    
    AX-Relnotes: n/a.
    Bug: 958251
    Change-Id: I2990a420228ee15fa94b58c29b0a586c17b2c639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230936
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/master@{#776332}

--- chrome/browser/resources/chromeos/accessibility/switch_access/navigation_manager.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/navigation_manager.js
@@ -110,7 +110,9 @@ class NavigationManager {
    */
   static forceFocusedNode(node) {
     const navigator = NavigationManager.instance;
-    navigator.setNode_(node);
+    if (!navigator.node_.equals(node)) {
+      navigator.setNode_(node);
+    }
   }
 
   /**
