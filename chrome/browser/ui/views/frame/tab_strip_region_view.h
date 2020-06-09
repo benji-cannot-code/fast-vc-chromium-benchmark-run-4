1ae3e276a8c03ffd96fdbe36adcc22254a8f260c
Taylor Bergquist <tbergquist@chromium.org>
2020-06-09 22:54:00 +0000

    Basic tab scrolling prototype. Guarded by a flag.
    Many rough edges, which I'll document somewhere and fix.
    Crashes unless the #scroll-unification flag is enabled.
    
    Bug: 951078
    Change-Id: If8570648b62fe435b9ca9c03b483e7a503012362
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236237
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776747}

--- chrome/browser/ui/views/frame/tab_strip_region_view.h
+++ chrome/browser/ui/views/frame/tab_strip_region_view.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_FRAME_TAB_STRIP_REGION_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_FRAME_TAB_STRIP_REGION_VIEW_H_
 
+#include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "ui/views/view.h"
 
 // Container for the tabstrip, new tab button, and reserved grab handle space.
@@ -15,6 +16,9 @@ class TabStripRegionView final : public views::View {
   TabStripRegionView();
   ~TabStripRegionView() override;
 
+  // Takes ownership of the tabstrip.
+  TabStrip* AddTabStrip(std::unique_ptr<TabStrip> tab_strip);
+
   // views::View overrides:
   const char* GetClassName() const override;
   void ChildPreferredSizeChanged(views::View* child) override;
@@ -23,6 +27,10 @@ class TabStripRegionView final : public views::View {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(TabStripRegionView);
+
+  int CalculateTabStripAvailableWidth();
+
+  views::View* tab_strip_container_;
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_FRAME_TAB_STRIP_REGION_VIEW_H_
