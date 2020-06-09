42323d92293d497626bccdba33d145d3a7115244
Victor-Gabriel Savu <vsavu@google.com>
2020-06-09 19:34:19 +0000

    set a default empty policy_test_server.
    Set a default empty policy file for policy_test_server. Only
    warn when the file is missing.
    
    Bug: chromium:668719
    Change-Id: I474587cdf810716d485c3dca1aefb4f513cbbe22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2215050
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Victor-Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/heads/master@{#776640}

--- components/policy/test_support/local_policy_test_server.cc
+++ components/policy/test_support/local_policy_test_server.cc
@@ -69,6 +69,10 @@ bool IsUnsafeCharacter(char c) {
 LocalPolicyTestServer::LocalPolicyTestServer()
     : LocalPolicyTestServer(base::FilePath()) {
   config_file_ = server_data_dir_.GetPath().Append(kPolicyFileName);
+
+  if (!PathExists(config_file_)) {
+    SetEmptyConfig();
+  }
 }
 
 LocalPolicyTestServer::LocalPolicyTestServer(const base::FilePath& config_file)
@@ -77,6 +81,10 @@ LocalPolicyTestServer::LocalPolicyTestServer(const base::FilePath& config_file)
   base::ScopedAllowBlockingForTesting allow_blocking;
   CHECK(server_data_dir_.CreateUniqueTempDir());
   client_state_file_ = server_data_dir_.GetPath().Append(kClientStateFileName);
+
+  if (!config_file_.empty() && !PathExists(config_file_)) {
+    SetEmptyConfig();
+  }
 }
 
 LocalPolicyTestServer::LocalPolicyTestServer(
@@ -287,6 +295,12 @@ std::string LocalPolicyTestServer::GetSelector(const std::string& type,
     selector = base::StringPrintf("%s/%s", type.c_str(), entity_id.c_str());
   std::replace_if(selector.begin(), selector.end(), IsUnsafeCharacter, '_');
   return selector;
+}
+
+void LocalPolicyTestServer::SetEmptyConfig() {
+  if (!SetConfig(base::Value(base::Value::Type::DICTIONARY))) {
+    LOG(ERROR) << "Failed to write empty config " << config_file_.value();
+  }
 }
 
 }  // namespace policy
