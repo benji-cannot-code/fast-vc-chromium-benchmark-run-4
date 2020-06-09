3943b593181f58e23542e16aaf6b0951add1bc81
Weilun Shi <sweilun@chromium.org>
2020-06-09 01:47:46 +0000

    Pretty print the merged xml before writing it
    Pretty print the merged xml to make sure it follows our xml style before
    sending to our server.
    
    Bug: b/158215587
    Change-Id: I6fd0776bd7a1db2e2f89db26399acdb2b1e36496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236681
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Auto-Submit: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776333}

--- tools/metrics/histograms/merge_xml.py
+++ tools/metrics/histograms/merge_xml.py
@@ -13,6 +13,7 @@ import xml.dom.minidom
 
 import expand_owners
 import extract_histograms
+import histograms_print_style
 import populate_enums
 
 
@@ -105,12 +106,18 @@ def MergeFiles(filenames=[], files=[]):
   return MergeTrees(trees)
 
 
+def PrettyPrintMergedFiles(filenames=[], files=[]):
+  return histograms_print_style.GetPrintStyle().PrettyPrintXml(
+      MergeFiles(filenames=filenames, files=files))
+
+
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('inputs', nargs="+")
   parser.add_argument('--output', required=True)
   args = parser.parse_args()
-  MergeFiles(args.inputs).writexml(open(args.output, 'w'))
+  with open(args.output, 'w') as f:
+    f.write(PrettyPrintMergedFiles(args.inputs))
 
 
 if __name__ == '__main__':
