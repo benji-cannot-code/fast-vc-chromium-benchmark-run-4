09dbfe08ae24658f205b9fd059627d8cf5f5207c
Patrick Noland <pnoland@chromium.org>
2020-06-09 13:06:50 +0000

    Destroy BookmarkBridge in ChromeActivity#onDestroy
    Failing to do this leaves behind a native observer of bookmark events
    that can hold onto a profile that subsequently gets deleted. When the
    observer is triggered, accessing the deleted profile causes a crash.
    
    Bug: 1092391
    Change-Id: I7864d2c63ae4952aee64cb28574c0404cb868dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236092
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776489}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -1256,6 +1256,12 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             if (selector != null) selector.destroy();
         }
 
+        if (mBookmarkBridgeSupplier != null) {
+            BookmarkBridge bookmarkBridge = mBookmarkBridgeSupplier.get();
+            if (bookmarkBridge != null) bookmarkBridge.destroy();
+            mBookmarkBridgeSupplier = null;
+        }
+
         mActivityTabProvider.destroy();
 
         mComponent = null;
