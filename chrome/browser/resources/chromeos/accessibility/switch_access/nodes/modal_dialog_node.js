68b387f2618878b2989bc33adb697d964396b4e7
Anastasia Helfinstein <anastasi@google.com>
2020-06-09 18:09:10 +0000

    [Switch Access] Jump to status tray when it opens
    To allow Switch Access to automatically move the user's focus into the
    status tray when it's opened, an accessibility event must be fired when
    the status tray bubble is created.
    
    The event kShow was selected because, although the status tray is
    treated like a menu by Switch Access, ChromeVox expects that kMenuStart
    would only be fired on a menu item.
    
    AX-Relnotes: n/a.
    Bug: 866173
    Change-Id: I5cd1ac908c6df473135ac2676e3ec5706577b2be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231903
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776598}

---
+++ chrome/browser/resources/chromeos/accessibility/switch_access/nodes/modal_dialog_node.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** This class represents the group rooted at a modal dialog. */
+class ModalDialogRootNode extends RootNodeWrapper {
+  /** @override */
+  onExit() {
+    // To close a modal dialog, we need to send an escape key event.
+    EventHelper.simulateKeyPress(EventHelper.KeyCode.ESC);
+  }
+
+  /**
+   * Creates the tree structure for a modal dialog.
+   * @param {!AutomationNode} dialogNode
+   * @return {!ModalDialogRootNode}
+   */
+  static buildTree(dialogNode) {
+    const root = new ModalDialogRootNode(dialogNode);
+    const childConstructor = (node) => NodeWrapper.create(node, root);
+
+    RootNodeWrapper.findAndSetChildren(root, childConstructor);
+    return root;
+  }
+}
