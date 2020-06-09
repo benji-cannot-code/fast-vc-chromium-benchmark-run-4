f6ff3837986b620e227bbe640dbe53b4207e0cdf
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-06-09 12:03:00 +0000

    [b4p/Settings] Add exceptions to 'passwords on this device' page
    This CL uses the new MergeExceptionsStoreCopiesBehavior to display
    deduplicated exceptions in the new page (exceptions are websites where
    passwords are never saved). The page now has two new subsections, one for
    device-only exceptions and one for device-and-account ones, similar to
    what was done for passwords in crrev.com/c/2228159. We also use the
    occasion to simplify the code a bit:
    
    a) The properties hasDevice*Passwords_ are now just inlined calls of the
    isNotEmpty_() function.
    b) Since exceptions and passwords share a common class, the code to
    filter device-only/device-and-account entries can be shared, cf the
    getDevice*Entries_() functions.
    c) Remove unnecessary ids in the HTML file.
    
    Screenshot after CL:
    https://screenshot.googleplex.com/mGDT0DPj3X6
    
    Screenshot before CL:
    https://screenshot.googleplex.com/Smq5nxgAt07
    
    Bug: 1049141
    Change-Id: I27cf33848188801ef2aa3ce1c01ee0e04e105f45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236392
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776482}

--- chrome/test/data/webui/settings/passwords_device_section_test.js
+++ chrome/test/data/webui/settings/passwords_device_section_test.js
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {MultiStorePasswordUiEntry, PasswordManagerImpl, PasswordManagerProxy} from 'chrome://settings/settings.js';
-import {createMultiStorePasswordEntry, createPasswordEntry} from 'chrome://test/settings/passwords_and_autofill_fake_data.js';
+import {MultiStoreExceptionEntry, MultiStorePasswordUiEntry, PasswordManagerImpl, PasswordManagerProxy} from 'chrome://settings/settings.js';
+import {createExceptionEntry, createMultiStoreExceptionEntry, createMultiStorePasswordEntry, createPasswordEntry} from 'chrome://test/settings/passwords_and_autofill_fake_data.js';
 import {simulateStoredAccounts, simulateSyncStatus} from 'chrome://test/settings/sync_test_util.m.js';
 import {TestPasswordManagerProxy} from 'chrome://test/settings/test_password_manager_proxy.js';
 
@@ -16,10 +16,13 @@ import {TestPasswordManagerProxy} from 'chrome://test/settings/test_password_man
  * Sets the fake data and creates the element for testing.
  * @param {!TestPasswordManagerProxy} passwordManager
  * @param {!Array<!chrome.passwordsPrivate.PasswordUiEntry>} passwordList
+ * @param {!Array<!chrome.passwordsPrivate.ExceptionEntry>} exceptionList
  * @return {!Object}
  */
-function createPasswordsDeviceSection(passwordManager, passwordList) {
+function createPasswordsDeviceSection(
+    passwordManager, passwordList, exceptionList) {
   passwordManager.data.passwords = passwordList;
+  passwordManager.data.exceptions = exceptionList;
   const passwordsDeviceSection =
       document.createElement('passwords-device-section');
   document.body.appendChild(passwordsDeviceSection);
@@ -49,6 +52,27 @@ function validatePasswordsSubsection(subsection, expectedPasswords) {
   }
 }
 
+/**
+ * @param {!Element} subsection
+ * @param {!Array<!MultiStoreExceptionEntry} expectedExceptions
+ */
+function validateExceptionsSubsection(subsection, expectedExceptions) {
+  assertTrue(!!subsection);
+  const listItemElements = subsection.querySelectorAll('.list-item');
+  assertEquals(expectedExceptions.length, listItemElements.length);
+  for (let index = 0; index < expectedExceptions.length; ++index) {
+    const expectedException = expectedExceptions[index];
+    const listItemElement = listItemElements[index];
+    assertTrue(!!listItemElement);
+    assertEquals(
+        expectedException.urls.shown,
+        listItemElement.querySelector('#exception').innerText);
+    assertEquals(
+        expectedException.urls.link,
+        listItemElement.querySelector('#exception').href);
+  }
+}
+
 suite('PasswordsDeviceSection', function() {
   /** @type {TestPasswordManagerProxy} */
   let passwordManager = null;
@@ -74,16 +98,22 @@ suite('PasswordsDeviceSection', function() {
     passwordManager.setIsOptedInForAccountStorageAndNotify(false);
   });
 
-  // Test verifies that the fallback text is displayed when passwords are not
-  // present.
+  // Test verifies that the fallback text is displayed when passwords/exceptions
+  // are not present.
   test('verifyPasswordsEmptySubsections', function() {
     const passwordsDeviceSection =
-        createPasswordsDeviceSection(passwordManager, []);
+        createPasswordsDeviceSection(passwordManager, [], []);
+    assertFalse(passwordsDeviceSection.shadowRoot
+                    .querySelector('#noDeviceOnlyPasswordsLabel')
+                    .hidden);
     assertFalse(passwordsDeviceSection.shadowRoot
                     .querySelector('#noDeviceAndAccountPasswordsLabel')
                     .hidden);
     assertFalse(passwordsDeviceSection.shadowRoot
-                    .querySelector('#noDeviceAndAccountPasswordsLabel')
+                    .querySelector('#noDeviceOnlyExceptionsLabel')
+                    .hidden);
+    assertFalse(passwordsDeviceSection.shadowRoot
+                    .querySelector('#noDeviceAndAccountExceptionsLabel')
                     .hidden);
   });
 
@@ -101,13 +131,15 @@ suite('PasswordsDeviceSection', function() {
         {username: 'both', frontendId: 42, id: 3, fromAccountStore: true});
 
     // Shuffle entries a little.
-    const passwordsDeviceSection =
-        createPasswordsDeviceSection(passwordManager, [
+    const passwordsDeviceSection = createPasswordsDeviceSection(
+        passwordManager,
+        [
           devicePassword,
           deviceCopyPassword,
           accountPassword,
           accountCopyPassword,
-        ]);
+        ],
+        []);
 
     validatePasswordsSubsection(
         passwordsDeviceSection.$.deviceOnlyPasswordList, [
@@ -119,11 +151,17 @@ suite('PasswordsDeviceSection', function() {
               {username: 'both', deviceId: 2, accountId: 3}),
         ]);
     assertTrue(passwordsDeviceSection.shadowRoot
-                   .querySelector('#noDeviceAndAccountPasswordsLabel')
+                   .querySelector('#noDeviceOnlyPasswordsLabel')
                    .hidden);
     assertTrue(passwordsDeviceSection.shadowRoot
                    .querySelector('#noDeviceAndAccountPasswordsLabel')
                    .hidden);
+    assertFalse(passwordsDeviceSection.shadowRoot
+                    .querySelector('#noDeviceOnlyExceptionsLabel')
+                    .hidden);
+    assertFalse(passwordsDeviceSection.shadowRoot
+                    .querySelector('#noDeviceAndAccountExceptionsLabel')
+                    .hidden);
   });
 
   // Test verifies that removing the device copy of a duplicated password
@@ -135,7 +173,7 @@ suite('PasswordsDeviceSection', function() {
     ];
 
     const passwordsDeviceSection =
-        createPasswordsDeviceSection(passwordManager, passwordList);
+        createPasswordsDeviceSection(passwordManager, passwordList, []);
     validatePasswordsSubsection(
         passwordsDeviceSection.$.deviceOnlyPasswordList, []);
     validatePasswordsSubsection(
@@ -163,7 +201,7 @@ suite('PasswordsDeviceSection', function() {
     ];
 
     const passwordsDeviceSection =
-        createPasswordsDeviceSection(passwordManager, passwordList);
+        createPasswordsDeviceSection(passwordManager, passwordList, []);
     validatePasswordsSubsection(
         passwordsDeviceSection.$.deviceOnlyPasswordList, []);
     validatePasswordsSubsection(
@@ -182,4 +220,48 @@ suite('PasswordsDeviceSection', function() {
     validatePasswordsSubsection(
         passwordsDeviceSection.$.deviceAndAccountPasswordList, []);
   });
+
+  // Test verifies that account exceptions are not displayed, whereas
+  // device-only and device-and-account ones end up in the correct subsection.
+  test('verifyExceptionsFilledSubsections', function() {
+    const deviceException = createExceptionEntry(
+        {url: 'device.com', id: 0, fromAccountStore: false});
+    const accountException = createExceptionEntry(
+        {url: 'account.com', id: 1, fromAccountStore: true});
+    // Create duplicate that gets merged.
+    const deviceCopyException = createExceptionEntry(
+        {url: 'both.com', frontendId: 42, id: 2, fromAccountStore: false});
+    const accountCopyException = createExceptionEntry(
+        {url: 'both.com', frontendId: 42, id: 3, fromAccountStore: true});
+
+    // Shuffle entries a little.
+    const passwordsDeviceSection =
+        createPasswordsDeviceSection(passwordManager, [], [
+          deviceException,
+          deviceCopyException,
+          accountException,
+          accountCopyException,
+        ]);
+
+    validateExceptionsSubsection(
+        passwordsDeviceSection.$.deviceOnlyExceptionsList,
+        [createMultiStoreExceptionEntry({url: 'device.com', deviceId: 0})]);
+    validateExceptionsSubsection(
+        passwordsDeviceSection.$.deviceAndAccountExceptionsList,
+        [createMultiStoreExceptionEntry(
+            {url: 'both.com', deviceId: 2, accountId: 3})]);
+    assertFalse(passwordsDeviceSection.shadowRoot
+                    .querySelector('#noDeviceOnlyPasswordsLabel')
+                    .hidden);
+    assertFalse(passwordsDeviceSection.shadowRoot
+                    .querySelector('#noDeviceAndAccountPasswordsLabel')
+                    .hidden);
+    assertTrue(passwordsDeviceSection.shadowRoot
+                   .querySelector('#noDeviceOnlyExceptionsLabel')
+                   .hidden);
+    assertTrue(passwordsDeviceSection.shadowRoot
+                   .querySelector('#noDeviceAndAccountExceptionsLabel')
+                   .hidden);
+  });
+
 });
