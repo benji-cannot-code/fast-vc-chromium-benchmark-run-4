e4dfdcb7fb81a0d840d269b212e7f48a7551d3cb
Stephen Nusko <nuskos@chromium.org>
2020-06-09 12:53:28 +0000

    Ensure that TouchEvents are always async after scrolling has started.
    Currently we can flip flop back and forth between TouchMoves being sent
    async or sync which can cause some pretty noticeable cases of jank. This
    ensures that we are always consistently scrolling after deciding
    TouchMoves can be async.
    
    Bug: 1072364
    Change-Id: I3ef1ed6715d4b1fd2d3e1377ebd6b0c8bcc02872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2182426
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776487}

--- content/browser/renderer_host/input/passthrough_touch_event_queue.cc
+++ content/browser/renderer_host/input/passthrough_touch_event_queue.cc
@@ -157,6 +157,7 @@ void PassthroughTouchEventQueue::ProcessTouchAck(
 
 void PassthroughTouchEventQueue::OnGestureScrollEvent(
     const GestureEventWithLatencyInfo& gesture_event) {
+  // Turn events sent during gesture scrolls to be async.
   if (gesture_event.event.GetType() ==
       blink::WebInputEvent::Type::kGestureScrollUpdate) {
     send_touch_events_async_ = true;
@@ -166,11 +167,12 @@ void PassthroughTouchEventQueue::OnGestureScrollEvent(
 void PassthroughTouchEventQueue::OnGestureEventAck(
     const GestureEventWithLatencyInfo& event,
     blink::mojom::InputEventResultState ack_result) {
-  // Turn events sent during gesture scrolls to be async.
-  if (event.event.GetType() ==
-      blink::WebInputEvent::Type::kGestureScrollUpdate) {
-    send_touch_events_async_ =
-        (ack_result == blink::mojom::InputEventResultState::kConsumed);
+  // When the scroll finishes allow TouchEvents to be blocking again.
+  if (event.event.GetType() == blink::WebInputEvent::Type::kGestureScrollEnd) {
+    send_touch_events_async_ = false;
+  } else if (event.event.GetType() ==
+             blink::WebInputEvent::Type::kGestureScrollUpdate) {
+    send_touch_events_async_ = true;
   }
 }
 
