db668b22144d02ca206c989eeef0137e2006959c
Sreeja Kamishetty <sreejakshetty@chromium.org>
2020-06-09 11:52:50 +0000

    [content] Add WebContentsImpl::RenderFrameHostStateChanged
    This CL adds a new method WebContentsImpl::
    RenderFrameHostStateChanged. This is called whenever the
    RenderFrameHostImpl::LifecycleState changes.
    
    One pager doc explaining RenderFrameHostStateChanged usage:
    https://docs.google.com/document/d/1vH_41CU61disTGWB2aFcP8-weHwfzHjEH7fbqdf3Ff8/edit?usp=sharing
    
    This is useful to make sure all the UI elements like popups/dialogs
    are closed when the RenderFrameHostImpl leave the kActive state.
    
    One of the reasons why we want to use RenderFrameHostStateChanged
    instead of WebContentsObserver::DidFinishNavigation is because a
    committed navigation changes state of not just the navigating frame
    but all of its subframes as well.  So, determining whether a particular
    frame's UI element needs to be closed would require listening to
    DidFinishNavigation in that frame as well as all of its ancestor frames.
    
    This CL closes the color chooser popup which rely on a signal coming
    from the renderer triggered by the document destruction which don't
    happen in the case of BackForwardCache and would be problematic,
    instead we close it when the LifecycleState changes from kActive state.
    
    Bug: 1073144
    Change-Id: I44eab243d5581e6e639406054d79da9efc444b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2159444
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776476}

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -9010,7 +9010,10 @@ void RenderFrameHostImpl::SetLifecycleState(LifecycleState state) {
           << "Unexpected LifeCycleState " << int(lifecycle_state_);
       break;
   }
+  LifecycleState old_state = lifecycle_state_;
   lifecycle_state_ = state;
+  // Notify the delegate about change in |lifecycle_state_|.
+  delegate_->RenderFrameHostStateChanged(this, old_state, lifecycle_state_);
 }
 
 void RenderFrameHostImpl::BindReportingObserver(
