cf9f8a1ec543a653dc0ad5b0827068eee9017fef
Denis Kuznetsov <antrim@google.com>
2020-06-09 18:43:09 +0000

    Replace last gaia-input with cr-input in OOBE
    Fixed: 1071778
    Bug: 856310
    Change-Id: I3c4d0beb18a81edff017fc51159d05d214f4f1d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2195243
    Commit-Queue: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776616}

--- chrome/browser/resources/chromeos/login/offline_gaia.js
+++ chrome/browser/resources/chromeos/login/offline_gaia.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 {
   const DEFAULT_EMAIL_DOMAIN = '@gmail.com';
+  const INPUT_EMAIL_PATTERN = '^[a-zA-Z0-9.!#$%&\'*+=?^_`{|}~-]+(@[^\\s@]+)?$';
 
   /** @enum */
   const TRANSITION_TYPE = {FORWARD: 0, BACKWARD: 1, NONE: 2};
@@ -20,14 +21,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         value: false,
       },
 
-      /** @type {?string} */
+      /**
+       * Management domain.
+       * @type {?string}
+       */
       domain: {
         type: String,
-        value: null,
+        value: '',
       },
 
-      /** @type {?string} */
-      emailDomain: String,
+      /**
+       * E-mail domain including initial '@' sign.
+       * @type {?string}
+       */
+      emailDomain: {
+        type: String,
+        value: '',
+      },
+
+      /**
+       * |domain| or empty string, depending on |email_| value.
+       */
+      displayDomain_: {
+        type: String,
+        computed: 'computeDomain_(emailDomain, email_)',
+      },
+
+      /**
+       * Current value of e-mail input field.
+       */
+      email_: String,
+
+      /**
+       * Current value of password input field.
+       */
+      password_: String,
+
+      /**
+       * Proper e-mail with domain, displayed on password page.
+       */
+      fullEmail_: String,
 
       activeSection: {
         type: String,
@@ -43,10 +76,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     focus() {
-      if (this.isEmailSectionActive_())
-        this.$$('#emailInput').focus();
-      else
-        this.$$('#passwordInput').focus();
+      if (this.isEmailSectionActive_()) {
+        this.$.emailInput.focusInput();
+      } else {
+        this.$.passwordInput.focusInput();
+      }
     },
 
     back() {
@@ -55,22 +89,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
     onBeforeShow() {
       cr.ui.login.invokePolymerMethod(this.$.dialog, 'onBeforeShow');
+      this.$.emailInput.pattern = INPUT_EMAIL_PATTERN;
     },
 
     reset() {
       this.disabled = false;
-      this.emailDomain = null;
-      this.domain = null;
+      this.emailDomain = '';
+      this.domain = '';
+      this.email_ = '';
+      this.fullEmail_ = '';
     },
 
     onForgotPasswordClicked_() {
       this.disabled = true;
       this.fire('dialogShown');
-      this.$$('#forgotPasswordDlg').showModal();
+      this.$.forgotPasswordDlg.showModal();
     },
 
     onForgotPasswordCloseTap_() {
-      this.$$('#forgotPasswordDlg').close();
+      this.$.forgotPasswordDlg.close();
     },
 
     onDialogOverlayClosed_() {
@@ -82,12 +119,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if (email) {
         if (this.emailDomain)
           email = email.replace(this.emailDomain, '');
-
         this.switchToPasswordCard(email, false /* animated */);
-        this.$$('#passwordInput').isInvalid = true;
+        this.$.passwordInput.isInvalid = true;
         this.fire('backButton', true);
       } else {
-        this.$$('#emailInput').value = '';
+        this.email_ = '';
         this.switchToEmailCard(false /* animated */);
       }
     },
@@ -101,9 +137,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     switchToEmailCard(animated) {
-      this.$$('#passwordInput').value = '';
-      this.$$('#passwordInput').isInvalid = false;
-      this.$$('#emailInput').isInvalid = false;
+      this.$.emailInput.isInvalid = false;
+      this.$.passwordInput.isInvalid = false;
+      this.password_ = '';
       if (this.isEmailSectionActive_())
         return;
 
@@ -112,14 +148,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     switchToPasswordCard(email, animated) {
-      this.$$('#emailInput').value = email;
+      this.email_ = email;
       if (email.indexOf('@') === -1) {
         if (this.emailDomain)
           email = email + this.emailDomain;
         else
           email = email + DEFAULT_EMAIL_DOMAIN;
       }
-      this.$$('#passwordHeader').email = email;
+      this.fullEmail_ = email;
+
       if (!this.isEmailSectionActive_())
         return;
 
@@ -133,23 +170,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     },
 
     onEmailSubmitted_() {
-      if (this.$$('#emailInput').checkValidity()) {
-        this.switchToPasswordCard(
-            this.$$('#emailInput').value, true /* animated */);
+      if (this.$.emailInput.validate()) {
+        this.switchToPasswordCard(this.email_, true /* animated */);
       } else {
-        this.$$('#emailInput').focus();
+        this.$.emailInput.focusInput();
       }
     },
 
     onPasswordSubmitted_() {
-      if (!this.$$('#passwordInput').checkValidity())
+      if (!this.$.passwordInput.validate())
         return;
       var msg = {
         'useOffline': true,
-        'email': this.$$('#passwordHeader').email,
-        'password': this.$$('#passwordInput').value
+        'email': this.email_,
+        'password': this.password_,
       };
-      this.$$('#passwordInput').value = '';
+      this.password_ = '';
       this.fire('authCompleted', msg);
     },
 
@@ -167,6 +203,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         return;
       }
       this.onPasswordSubmitted_();
+    },
+
+    computeDomain_(domain, email) {
+      if (email && email.indexOf('@') !== -1)
+        return '';
+      return domain;
     },
   });
 }
--- chrome/browser/resources/chromeos/login/structure/components_common.js
+++ chrome/browser/resources/chromeos/login/structure/components_common.js
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // <include src="../gaia_card.js">
 // <include src="../gaia_header.js">
 // <include src="../gaia_input_form.js">
-// <include src="../gaia_input.js">
 // <include src="../navigation_bar.js">
 
 // <include src="../oobe_a11y_option.js">
