6d6ca314ca1f1d88a17f43f3c693b84d6ba9deee
Asanka Herath <asanka@chromium.org>
2020-06-09 22:34:31 +0000

    [privacy_budget] Incremental token generation.
    A prior CL introduced a concept of an IdentifiableToken. This CL
    introduces an incremental token builder that can be used to generate
    such a token based on disjoint buffers or objects.
    
    Bug: 973801
    Change-Id: I9394878f36748fd2d97f2b220d1d57444bf7600f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214426
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776734}

--- third_party/blink/common/privacy_budget/identifiability_metrics_unittest.cc
+++ third_party/blink/common/privacy_budget/identifiability_metrics_unittest.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/privacy_budget/identifiability_metrics.h"
 
 #include <cstdint>
-#include <limits>
 #include <vector>
 
 #include "base/strings/string_piece_forward.h"
