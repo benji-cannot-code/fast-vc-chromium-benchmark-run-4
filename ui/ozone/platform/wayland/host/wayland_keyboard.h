f105fddd78e3bb6b53eb8e8bb5f6126ff8f7adbb
Eliot Courtney <edcourtney@google.com>
2020-06-09 06:40:43 +0000

    Add zcr_extended_keyboard implementation to ozone Wayland backend.
    This CL adds the extended keyboard custom Wayland protocol to the Ozone
    Wayland backend. This lets Ctrl-W close tabs rather than the
    entire window.
    
    Bug: 1087479
    Test: Ctrl-W works properly with lacros.
    Test: Keyboard input works on Wayland Ozone (via Weston)
    Change-Id: I4784cc3e81e503df13a40a373ba7ffc2c1fdb52e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230139
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776414}

--- ui/ozone/platform/wayland/host/wayland_keyboard.h
+++ ui/ozone/platform/wayland/host/wayland_keyboard.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_OZONE_PLATFORM_WAYLAND_HOST_WAYLAND_KEYBOARD_H_
 #define UI_OZONE_PLATFORM_WAYLAND_HOST_WAYLAND_KEYBOARD_H_
 
+#include <keyboard-extension-unstable-v1-client-protocol.h>
 #include <wayland-client.h>
 
 #include "base/time/time.h"
@@ -31,6 +32,7 @@ class WaylandKeyboard : public EventAutoRepeatHandler::Delegate {
   class Delegate;
 
   WaylandKeyboard(wl_keyboard* keyboard,
+                  zcr_keyboard_extension_v1* keyboard_extension_v1,
                   WaylandConnection* connection,
                   KeyboardLayoutEngine* keyboard_layout_engine,
                   Delegate* delegate);
@@ -89,6 +91,7 @@ class WaylandKeyboard : public EventAutoRepeatHandler::Delegate {
                    int flags) override;
 
   wl::Object<wl_keyboard> obj_;
+  wl::Object<zcr_extended_keyboard_v1> extended_keyboard_v1_;
   WaylandConnection* const connection_;
   Delegate* const delegate_;
 
@@ -111,12 +114,18 @@ class WaylandKeyboard::Delegate {
   virtual void OnKeyboardDestroyed(WaylandKeyboard* keyboard) = 0;
   virtual void OnKeyboardFocusChanged(WaylandWindow* window, bool focused) = 0;
   virtual void OnKeyboardModifiersChanged(int modifiers) = 0;
-  virtual void OnKeyboardKeyEvent(EventType type,
-                                  DomCode dom_code,
-                                  DomKey dom_key,
-                                  KeyboardCode key_code,
-                                  bool repeat,
-                                  base::TimeTicks timestamp) = 0;
+  // Returns a mask of ui::PostDispatchAction indicating how the event was
+  // dispatched.
+  virtual uint32_t OnKeyboardKeyEvent(EventType type,
+                                      DomCode dom_code,
+                                      DomKey dom_key,
+                                      KeyboardCode key_code,
+                                      bool repeat,
+                                      base::TimeTicks timestamp) = 0;
+
+ protected:
+  // Prevent deletion through a WaylandKeyboard::Delegate pointer.
+  virtual ~Delegate() = default;
 };
 
 }  // namespace ui
