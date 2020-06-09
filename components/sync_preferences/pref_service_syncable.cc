567ba9e14496e9451bac6ffd3454dc35556603b8
Nigel Tao <nigeltao@chromium.org>
2020-06-09 10:31:40 +0000

    Delete unused include "base/value_conversions.h"
    Bug: 1086733
    Change-Id: I41f294fac67e865f4ac968b42bd32225905a32fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235254
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776458}

--- components/sync_preferences/pref_service_syncable.cc
+++ components/sync_preferences/pref_service_syncable.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/callback.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/value_conversions.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/default_pref_store.h"
 #include "components/prefs/in_memory_pref_store.h"
