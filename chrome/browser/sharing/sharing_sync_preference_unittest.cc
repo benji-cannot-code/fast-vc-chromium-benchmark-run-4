0cfc82e3171feb40bd79d572f58f86a045a8b6aa
Nigel Tao <nigeltao@chromium.org>
2020-06-09 10:34:02 +0000

    Upgrade chrome/browser/sharing from deprecated Value-conversion API
    Bug: 1086733
    Change-Id: I61d6bf6cace0b29de75c2754e0bfdfc70d30b801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235161
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776462}

--- chrome/browser/sharing/sharing_sync_preference_unittest.cc
+++ chrome/browser/sharing/sharing_sync_preference_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/guid.h"
 #include "base/time/time.h"
-#include "base/value_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/sharing/fake_device_info.h"
 #include "chrome/browser/sharing/proto/sharing_message.pb.h"
