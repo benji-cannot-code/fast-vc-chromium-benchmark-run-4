451407eed8087687510be3204385d2007dad9093
Anders Hartvoll Ruud <andruud@chromium.org>
2020-06-09 11:46:16 +0000

    Add missing animation-timeline property to css_proto_converter.cc
    Bug: 1091415, 1074052
    Change-Id: Ife168acada6aa37502b1bf2ff891700896531610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2232341
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776474}

--- third_party/blink/renderer/core/css/parser/css_proto_converter.cc
+++ third_party/blink/renderer/core/css/parser/css_proto_converter.cc
@@ -1518,6 +1518,7 @@ const std::string Converter::kPropertyLookupTable[] = {
     "overscroll-behavior-block",
     "overscroll-behavior-x",
     "overscroll-behavior-y",
+    "animation-timeline",
     "INVALID_PROPERTY",
 };
 
