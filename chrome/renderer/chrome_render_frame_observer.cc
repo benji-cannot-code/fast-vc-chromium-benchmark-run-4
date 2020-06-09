8881c9a212a6d43a7cf0396a9b172622d535c15a
Bettina <bdea@chromium.org>
2020-06-09 19:55:53 +0000

    Remove SAFE_BROWSING_CSD.
    SAFE_BROWSING_CSD has the same values as
    FULL_SAFE_BROWSING so removing the former
    would remove unnecessary code.
    
    Bug: 1078195
    Change-Id: I0b388cdd51a1f6b719eba8c74e0c45938f8dba56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236194
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776652}

--- chrome/renderer/chrome_render_frame_observer.cc
+++ chrome/renderer/chrome_render_frame_observer.cc
@@ -131,7 +131,7 @@ ChromeRenderFrameObserver::ChromeRenderFrameObserver(
   if (!render_frame->IsMainFrame())
     return;
 
-#if BUILDFLAG(SAFE_BROWSING_CSD)
+#if BUILDFLAG(FULL_SAFE_BROWSING)
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
   if (!command_line.HasSwitch(switches::kDisableClientSidePhishingDetection))
@@ -474,7 +474,7 @@ void ChromeRenderFrameObserver::GetMediaFeedURL(
 }
 
 void ChromeRenderFrameObserver::SetClientSidePhishingDetection() {
-#if BUILDFLAG(SAFE_BROWSING_CSD)
+#if BUILDFLAG(FULL_SAFE_BROWSING)
   phishing_classifier_ = safe_browsing::PhishingClassifierDelegate::Create(
       render_frame(), nullptr);
 #endif
@@ -510,7 +510,7 @@ void ChromeRenderFrameObserver::CapturePageText(TextCaptureType capture_type) {
 
     // Don't capture contents unless there is either a translate agent or a
     // phishing classifier to consume them.
-#if BUILDFLAG(SAFE_BROWSING_CSD)
+#if BUILDFLAG(FULL_SAFE_BROWSING)
   if (!translate_agent_ && !phishing_classifier_)
     return;
 #else
@@ -540,7 +540,7 @@ void ChromeRenderFrameObserver::CapturePageText(TextCaptureType capture_type) {
 
   TRACE_EVENT0("renderer", "ChromeRenderFrameObserver::CapturePageText");
 
-#if BUILDFLAG(SAFE_BROWSING_CSD)
+#if BUILDFLAG(FULL_SAFE_BROWSING)
   // Will swap out the string.
   if (phishing_classifier_)
     phishing_classifier_->PageCaptured(&contents,
