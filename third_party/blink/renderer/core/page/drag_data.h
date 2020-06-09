6dd7c1558ec6def01c26b02e7634f3ef1774eec7
Henrique Ferreiro <hferreiro@igalia.com>
2020-06-09 08:53:26 +0000

    Remove unused blink::DragApplicationFlags
    Bug: 1039255.
    Change-Id: I1cc739ae66e4f1580d2ae73f01423678f47e9c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2234512
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#776437}

--- third_party/blink/renderer/core/page/drag_data.h
+++ third_party/blink/renderer/core/page/drag_data.h
@@ -41,14 +41,6 @@ class DataObject;
 class DocumentFragment;
 class LocalFrame;
 
-enum DragApplicationFlags {
-  kDragApplicationNone = 0,
-  kDragApplicationIsModal = 1,
-  kDragApplicationIsSource = 2,
-  kDragApplicationHasAttachedSheet = 4,
-  kDragApplicationIsCopyKeyDown = 8
-};
-
 class CORE_EXPORT DragData {
   STACK_ALLOCATED();
 
@@ -60,11 +52,9 @@ class CORE_EXPORT DragData {
   DragData(DataObject*,
            const FloatPoint& client_position,
            const FloatPoint& global_position,
-           DragOperation,
-           DragApplicationFlags = kDragApplicationNone);
+           DragOperation);
   const FloatPoint& ClientPosition() const { return client_position_; }
   const FloatPoint& GlobalPosition() const { return global_position_; }
-  DragApplicationFlags Flags() const { return application_flags_; }
   DataObject* PlatformData() const { return platform_drag_data_; }
   DragOperation DraggingSourceOperationMask() const {
     return dragging_source_operation_mask_;
@@ -90,7 +80,6 @@ class CORE_EXPORT DragData {
   const FloatPoint global_position_;
   DataObject* const platform_drag_data_;
   const DragOperation dragging_source_operation_mask_;
-  const DragApplicationFlags application_flags_;
 
   bool ContainsHTML() const;
 };
