7d768f00b25381054f8571f02f0c7861d3ea61a4
Olesia Marukhno <olesiamarukhno@google.com>
2020-06-09 19:24:54 +0000

    Add setting custom padding to md text button
    It is used in the permission chip to get proper padding.
    
    Bug: 1019129
    Change-Id: I33b5ed4599d65a3d32f3177c9b47d2ec35451604
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235042
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776634}

---
+++ ui/views/controls/button/md_text_button_unittest.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/views/controls/button/md_text_button.h"
+
+#include "ui/views/test/views_test_base.h"
+
+namespace views {
+
+using MdTextButtonTest = ViewsTestBase;
+
+TEST_F(MdTextButtonTest, CustomPadding) {
+  const base::string16 text = base::ASCIIToUTF16("abc");
+  std::unique_ptr<MdTextButton> button =
+      MdTextButton::Create(nullptr, text, views::style::CONTEXT_BUTTON_MD);
+
+  const gfx::Insets custom_padding(10, 20);
+  ASSERT_NE(button->GetInsets(), custom_padding);
+
+  button->SetCustomPadding(custom_padding);
+  EXPECT_EQ(button->GetInsets(), custom_padding);
+}
+
+}  // namespace views
