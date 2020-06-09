fc7b7fa4d18445cef20d3eead3b88d3b62852c6a
Marc Treib <treib@chromium.org>
2020-06-09 13:11:24 +0000

    Revert "Add browser test for permission prompt."
    This reverts commit bb3eba3325c8b2393ed595971b72cc5e62a8c433.
    
    Reason for revert: New tests are failing on Mac, see e.g.:
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.11%20Tests/51174
    All failing bots:
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.11%20Tests
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.12%20Tests
    https://ci.chromium.org/p/chromium/builders/ci/Mac10.14%20Tests
    
    Original change's description:
    > Add browser test for permission prompt.
    > 
    > Bug: 1019129
    > Change-Id: I08a2c1d038f353b0f84264d0a3ddf26f601347a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2214536
    > Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    > Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#776457}
    
    TBR=bsep@chromium.org,olesiamarukhno@google.com
    
    Change-Id: I071939c3e57947489513b14ef49f9ff1037c5617
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1019129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235847
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776491}

--- chrome/browser/ui/views/permission_bubble/permission_prompt_impl.h
+++ chrome/browser/ui/views/permission_bubble/permission_prompt_impl.h
@@ -20,8 +20,7 @@ class WebContents;
 // This object will create or trigger UI to reflect that a website is requesting
 // a permission. The UI is usually a popup bubble, but may instead be a location
 // bar icon (the "quiet" prompt).
-class PermissionPromptImpl : public permissions::PermissionPrompt,
-                             public views::WidgetObserver {
+class PermissionPromptImpl : public permissions::PermissionPrompt {
  public:
   PermissionPromptImpl(Browser* browser,
                        content::WebContents* web_contents,
@@ -35,9 +34,6 @@ class PermissionPromptImpl : public permissions::PermissionPrompt,
   PermissionPromptBubbleView* prompt_bubble_for_testing() {
     return prompt_bubble_;
   }
-
-  // views::WidgetObserver:
-  void OnWidgetClosing(views::Widget* widget) override;
 
  private:
   enum class PromptStyle;
