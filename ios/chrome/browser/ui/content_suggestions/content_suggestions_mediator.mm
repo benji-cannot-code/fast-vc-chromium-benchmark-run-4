5d37eb8cf6fb9e365d4b13db501c99a498c7d226
adamta <adamta@google.com>
2020-06-09 15:26:10 +0000

    [iOS] Discover feed item in NTP
    Adds the Discover feed coming from the Discover provider to the NTP
    ContentSuggestions collection view. The feed is shown based on the
    feature flag, as well as the "Article Suggestions" setting in the
    settings menu. Will replace the Zine feed if the flag is active.
    
    Bug: 1085419
    Change-Id: Iad9e94af68ad74e2d9028152cf696c7cb69d733e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2230765
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Adam Trudeau-Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/master@{#776532}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/ntp_tiles/most_visited_sites_observer_bridge.h"
 #include "ios/chrome/browser/pref_names.h"
+#import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_discover_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_learn_more_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited_action_item.h"
@@ -116,6 +117,8 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
 // reading list count.
 @property(nonatomic, strong)
     ContentSuggestionsMostVisitedActionItem* readingListItem;
+// Item for the Discover feed.
+@property(nonatomic, strong) ContentSuggestionsDiscoverItem* discoverItem;
 // Number of unread items in reading list model.
 @property(nonatomic, assign) NSInteger readingListUnreadCount;
 
@@ -135,7 +138,8 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
            mostVisitedSite:
                (std::unique_ptr<ntp_tiles::MostVisitedSites>)mostVisitedSites
           readingListModel:(ReadingListModel*)readingListModel
-               prefService:(PrefService*)prefService {
+               prefService:(PrefService*)prefService
+              discoverFeed:(UIViewController*)discoverFeed {
   self = [super init];
   if (self) {
     _contentArticlesEnabled =
@@ -158,6 +162,8 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
     _learnMoreItem = [[ContentSuggestionsLearnMoreItem alloc] init];
 
     _discoverSectionInfo = DiscoverSectionInformation();
+    _discoverItem = [[ContentSuggestionsDiscoverItem alloc] init];
+    _discoverItem.discoverFeed = discoverFeed;
 
     _notificationPromo = std::make_unique<NotificationPromoWhatsNew>(
         GetApplicationContext()->GetLocalState());
@@ -228,7 +234,8 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
 
   [sectionsInfo addObject:self.learnMoreSectionInfo];
 
-  if (IsDiscoverFeedEnabled()) {
+  if (IsDiscoverFeedEnabled() &&
+      self.contentArticlesEnabled->GetValue()->GetBool()) {
     [sectionsInfo addObject:self.discoverSectionInfo];
   }
 
@@ -257,6 +264,8 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
     [convertedSuggestions addObjectsFromArray:self.actionButtonItems];
   } else if (sectionInfo == self.learnMoreSectionInfo) {
     [convertedSuggestions addObject:self.learnMoreItem];
+  } else if (sectionInfo == self.discoverSectionInfo) {
+    [convertedSuggestions addObject:self.discoverItem];
   } else {
     ntp_snippets::Category category =
         [[self categoryWrapperForSectionInfo:sectionInfo] category];
@@ -621,26 +630,43 @@ const NSInteger kMaxNumMostVisitedTiles = 4;
 // ntp_snippets doesn't differentiate between disabled vs collapsed, so if
 // the status is |CATEGORY_EXPLICITLY_DISABLED|, check the value of
 // |contentArticlesEnabled|.
+// The Articles category is always ignored if the Discover feed flag is
+// active.
 - (BOOL)isCategoryInitOrAvailable:(ntp_snippets::Category)category {
   ntp_snippets::CategoryStatus status =
       self.contentService->GetCategoryStatus(category);
-  if (category.IsKnownCategory(ntp_snippets::KnownCategories::ARTICLES) &&
-      status == ntp_snippets::CategoryStatus::CATEGORY_EXPLICITLY_DISABLED)
-    return self.contentArticlesEnabled->GetValue()->GetBool();
-  else
+  if (category.IsKnownCategory(ntp_snippets::KnownCategories::ARTICLES)) {
+    if (status == ntp_snippets::CategoryStatus::CATEGORY_EXPLICITLY_DISABLED) {
+      return self.contentArticlesEnabled->GetValue()->GetBool() &&
+             !IsDiscoverFeedEnabled();
+    } else {
+      return IsCategoryStatusInitOrAvailable(
+                 self.contentService->GetCategoryStatus(category)) &&
+             !IsDiscoverFeedEnabled();
+    }
+  } else {
     return IsCategoryStatusInitOrAvailable(
         self.contentService->GetCategoryStatus(category));
+  }
 }
 
 // ntp_snippets doesn't differentiate between disabled vs collapsed, so if
 // the status is |CATEGORY_EXPLICITLY_DISABLED|, check the value of
 // |contentArticlesEnabled|.
+// The Articles category is always ignored if the Discover feed flag is
+// active.
 - (BOOL)isCategoryAvailable:(ntp_snippets::Category)category {
   ntp_snippets::CategoryStatus status =
       self.contentService->GetCategoryStatus(category);
-  if (category.IsKnownCategory(ntp_snippets::KnownCategories::ARTICLES) &&
-      status == ntp_snippets::CategoryStatus::CATEGORY_EXPLICITLY_DISABLED) {
-    return self.contentArticlesEnabled->GetValue()->GetBool();
+  if (category.IsKnownCategory(ntp_snippets::KnownCategories::ARTICLES)) {
+    if (status == ntp_snippets::CategoryStatus::CATEGORY_EXPLICITLY_DISABLED) {
+      return self.contentArticlesEnabled->GetValue()->GetBool() &&
+             !IsDiscoverFeedEnabled();
+    } else {
+      return IsCategoryStatusAvailable(
+                 self.contentService->GetCategoryStatus(category)) &&
+             !IsDiscoverFeedEnabled();
+    }
   } else {
     return IsCategoryStatusAvailable(
         self.contentService->GetCategoryStatus(category));
