d06868f9dba662b43279ec4e3781033e7ffd4cc3
Matt Jones <mdjones@chromium.org>
2020-06-09 22:51:55 +0000

    Add BottomSheetControllerFactory, hide impl from everything else
    Bug: 1002277
    Change-Id: I70219c5eef14630f8fd1584d2af085c4a9c3ae21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236503
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776746}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -66,7 +66,7 @@ import org.chromium.chrome.browser.vr.VrModuleProvider;
 import org.chromium.chrome.browser.widget.ScrimView;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetController;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetController.SheetState;
-import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetControllerImpl;
+import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetControllerFactory;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetControllerInternal;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetObserver;
 import org.chromium.chrome.browser.widget.bottomsheet.EmptyBottomSheetObserver;
@@ -127,7 +127,7 @@ public class RootUiCoordinator
     private VrModeObserver mVrModeObserver;
 
     private BottomSheetManager mBottomSheetManager;
-    private BottomSheetControllerImpl mBottomSheetController;
+    private BottomSheetControllerInternal mBottomSheetController;
     private SnackbarManager mBottomSheetSnackbarManager;
 
     private ScrimView mScrimView;
@@ -639,9 +639,8 @@ public class RootUiCoordinator
         Supplier<OverlayPanelManager> panelManagerSupplier = ()
                 -> mActivity.getCompositorViewHolder().getLayoutManager().getOverlayPanelManager();
 
-        mBottomSheetController = new BottomSheetControllerImpl(()
-                                                                       -> mScrimCoordinator,
-                sheetViewSupplier, mActivity.getWindow(),
+        mBottomSheetController = BottomSheetControllerFactory.createBottomSheetController(
+                () -> mScrimCoordinator, sheetViewSupplier, mActivity.getWindow(),
                 mActivity.getWindowAndroid().getKeyboardDelegate());
 
         mBottomSheetManager = new BottomSheetManager(mBottomSheetController, mActivityTabProvider,
