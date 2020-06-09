45d1eb0053e096bc2a73500e5ec907260d660a8c
Clark DuVall <cduvall@chromium.org>
2020-06-09 02:59:37 +0000

    [WebLayer] Run weblayer_unittests on Android
    The weblayer unittests needed a small fix to be able to run on Android,
    these should be running on the bots. This will be useful for adding site
    isolation unittests, which should be tested on both Android and desktop.
    
    Bug: 1058364
    Change-Id: I7188a6140cff2811594bc7ab6d2e9b2dd58366ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235941
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776359}

--- weblayer/browser/profile_disk_operations_unittests.cc
+++ weblayer/browser/profile_disk_operations_unittests.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/base_paths.h"
 #include "base/check.h"
 #include "base/files/file_util.h"
-#include "base/files/scoped_temp_dir.h"
-#include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/scoped_path_override.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "weblayer/browser/profile_disk_operations.h"
@@ -20,20 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace weblayer {
 
 class ProfileDiskOperationsTest : public testing::Test {
- public:
-  void SetUp() override {
-    CHECK(data_temp_dir_.CreateUniqueTempDir());
-    base::PathService::Override(DIR_USER_DATA, data_temp_dir_.GetPath());
-#if defined(OS_POSIX)
-    CHECK(cache_temp_dir_.CreateUniqueTempDir());
-    base::PathService::Override(base::DIR_CACHE, cache_temp_dir_.GetPath());
-#endif
-  }
-
  protected:
-  base::ScopedTempDir data_temp_dir_;
+  base::ScopedPathOverride data_dir_override_{DIR_USER_DATA};
 #if defined(OS_POSIX)
-  base::ScopedTempDir cache_temp_dir_;
+  base::ScopedPathOverride cache_dir_override_{base::DIR_CACHE};
 #endif
 };
 
