55901ce285778e11303b226d9123257817c63003
Xianzhu Wang <wangxianzhu@chromium.org>
2020-06-09 06:50:31 +0000

    Don't add overflow from floats if display locked for children
    A LayoutBlockFlow may have floating children. Previously during
    LayoutBlockFlow::ComputeVisualOverflow() and ComputeLayoutOverflow(),
    we always added overflow from floating children, which might access
    dirty layout data because we may have skipped layout of the floating
    children.
    
    Now skip floating children during ComputeVisualOverflow() and
    ComputeLayoutOverflow(). Also add CHECKs to check dirty layout during
    ComputeVisualOverflow().
    
    Bug: 1086968
    
    Change-Id: I816adaf57f8940e272709328d8b4c5f84981e8dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236043
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: vmpstr <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776420}

--- third_party/blink/renderer/core/layout/layout_block_flow.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow.cc
@@ -2386,8 +2386,12 @@ EBreakBetween LayoutBlockFlow::BreakAfter() const {
 }
 
 void LayoutBlockFlow::AddVisualOverflowFromFloats() {
-  if (!floating_objects_)
+  if (PrePaintBlockedByDisplayLock(DisplayLockLifecycleTarget::kChildren) ||
+      !floating_objects_)
     return;
+
+  // TODO(crbug.com/1086968): Change to DCHECK after diagnosing the bug.
+  CHECK(!NeedsLayout());
 
   for (auto& floating_object : floating_objects_->Set()) {
     if (floating_object->IsDescendant()) {
@@ -2401,7 +2405,11 @@ void LayoutBlockFlow::AddVisualOverflowFromFloats() {
 
 void LayoutBlockFlow::AddVisualOverflowFromFloats(
     const NGPhysicalContainerFragment& fragment) {
+  // TODO(crbug.com/1086968): Change to DCHECK after diagnosing the bug.
+  CHECK(!NeedsLayout());
+  DCHECK(!PrePaintBlockedByDisplayLock(DisplayLockLifecycleTarget::kChildren));
   DCHECK(fragment.HasFloatingDescendantsForPaint());
+
   for (const NGLink& child : fragment.Children()) {
     if (child->HasSelfPaintingLayer())
       continue;
@@ -2421,7 +2429,8 @@ void LayoutBlockFlow::AddVisualOverflowFromFloats(
 }
 
 void LayoutBlockFlow::AddLayoutOverflowFromFloats() {
-  if (!floating_objects_)
+  if (LayoutBlockedByDisplayLock(DisplayLockLifecycleTarget::kChildren) ||
+      !floating_objects_)
     return;
 
   for (auto& floating_object : floating_objects_->Set()) {
@@ -2446,6 +2455,9 @@ const NGFragmentItems* LayoutBlockFlow::FragmentItems() const {
 
 void LayoutBlockFlow::ComputeVisualOverflow(
     bool recompute_floats) {
+  // TODO(crbug.com/1086968): Change to DCHECK after diagnosing the bug.
+  CHECK(!SelfNeedsLayout());
+
   LayoutRect previous_visual_overflow_rect = VisualOverflowRect();
   ClearVisualOverflow();
   AddVisualOverflowFromChildren();
@@ -2457,6 +2469,7 @@ void LayoutBlockFlow::ComputeVisualOverflow(
       (recompute_floats || CreatesNewFormattingContext() ||
        HasSelfPaintingLayer()))
     AddVisualOverflowFromFloats();
+
   if (VisualOverflowRect() != previous_visual_overflow_rect) {
     InvalidateIntersectionObserverCachedRects();
     SetShouldCheckForPaintInvalidation();
