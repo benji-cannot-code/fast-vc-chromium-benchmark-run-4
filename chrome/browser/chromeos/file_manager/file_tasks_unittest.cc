26d44516a30ea933eb5a4da04bf306cfe9c1fd03
Joel Hockey <joelhockey@chromium.org>
2020-06-09 01:53:54 +0000

    Check that files can be shared with VMs to allow open-with
    Bug: 1091611
    Change-Id: Ibbdae7456f83b43cfbba08b4b9962785583cd62c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2234041
    Reviewed-by: François Degros <fdegros@chromium.org>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776338}

--- chrome/browser/chromeos/file_manager/file_tasks_unittest.cc
+++ chrome/browser/chromeos/file_manager/file_tasks_unittest.cc
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_builder.h"
 #include "extensions/common/manifest.h"
 #include "google_apis/drive/drive_api_parser.h"
+#include "net/base/escape.h"
+#include "storage/browser/file_system/external_mount_points.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
 #include "url/gurl.h"
@@ -1359,6 +1361,25 @@ class FileManagerFileTasksCrostiniTest
         ->UpdateMimeTypes(mime_types_list);
   }
 
+  void SetUp() override {
+    storage::ExternalMountPoints::GetSystemInstance()->RegisterFileSystem(
+        util::GetDownloadsMountPointName(&test_profile_),
+        storage::kFileSystemTypeNativeLocal, storage::FileSystemMountOption(),
+        util::GetMyFilesFolderForProfile(&test_profile_));
+  }
+
+  void TearDown() override {
+    storage::ExternalMountPoints::GetSystemInstance()->RevokeFileSystem(
+        util::GetDownloadsMountPointName(&test_profile_));
+  }
+
+  GURL PathToURL(const std::string& path) {
+    std::string virtual_path = net::EscapeUrlEncodedData(
+        util::GetDownloadsMountPointName(&test_profile_) + "/" + path,
+        /*use_plus=*/false);
+    return GURL("filesystem:chrome-extension://id/external/" + virtual_path);
+  }
+
   crostini::CrostiniTestHelper crostini_test_helper_;
   base::FilePath crostini_folder_;
   std::string text_app_id_;
@@ -1370,8 +1391,7 @@ class FileManagerFileTasksCrostiniTest
 TEST_F(FileManagerFileTasksCrostiniTest, BasicFiles) {
   std::vector<extensions::EntryInfo> entries{
       {crostini_folder_.Append("foo.txt"), "text/plain", false}};
-  std::vector<GURL> file_urls{
-      GURL("filesystem:chrome-extension://id/dir/foo.txt")};
+  std::vector<GURL> file_urls{PathToURL("dir/foo.txt")};
 
   std::vector<FullTaskDescriptor> tasks;
   FindAllTypesOfTasksSynchronousWrapper().Call(&test_profile_, entries,
@@ -1381,7 +1401,7 @@ TEST_F(FileManagerFileTasksCrostiniTest, BasicFiles) {
 
   // Multiple text files
   entries.emplace_back(crostini_folder_.Append("bar.txt"), "text/plain", false);
-  file_urls.emplace_back("filesystem:chrome-extension://id/dir/bar.txt");
+  file_urls.emplace_back(PathToURL("dir/bar.txt"));
   FindAllTypesOfTasksSynchronousWrapper().Call(&test_profile_, entries,
                                                file_urls, &tasks);
   ASSERT_EQ(1U, tasks.size());
@@ -1391,14 +1411,14 @@ TEST_F(FileManagerFileTasksCrostiniTest, BasicFiles) {
 TEST_F(FileManagerFileTasksCrostiniTest, Directories) {
   std::vector<extensions::EntryInfo> entries{
       {crostini_folder_.Append("dir"), "", true}};
-  std::vector<GURL> file_urls{GURL("filesystem:chrome-extension://id/dir/dir")};
+  std::vector<GURL> file_urls{PathToURL("dir/dir")};
   std::vector<FullTaskDescriptor> tasks;
   FindAllTypesOfTasksSynchronousWrapper().Call(&test_profile_, entries,
                                                file_urls, &tasks);
   EXPECT_EQ(0U, tasks.size());
 
   entries.emplace_back(crostini_folder_.Append("foo.txt"), "text/plain", false);
-  file_urls.emplace_back("filesystem:chrome-extension://id/dir/foo.txt");
+  file_urls.emplace_back(PathToURL("dir/foo.txt"));
   FindAllTypesOfTasksSynchronousWrapper().Call(&test_profile_, entries,
                                                file_urls, &tasks);
   EXPECT_EQ(0U, tasks.size());
@@ -1408,9 +1428,8 @@ TEST_F(FileManagerFileTasksCrostiniTest, MultipleMatches) {
   std::vector<extensions::EntryInfo> entries{
       {crostini_folder_.Append("foo.gif"), "image/gif", false},
       {crostini_folder_.Append("bar.gif"), "image/gif", false}};
-  std::vector<GURL> file_urls{
-      GURL("filesystem:chrome-extension://id/dir/foo.gif"),
-      GURL("filesystem:chrome-extension://id/dir/bar.gif")};
+  std::vector<GURL> file_urls{PathToURL("dir/foo.gif"),
+                              PathToURL("dir/bar.gif")};
 
   std::vector<FullTaskDescriptor> tasks;
   FindAllTypesOfTasksSynchronousWrapper().Call(&test_profile_, entries,
@@ -1427,9 +1446,8 @@ TEST_F(FileManagerFileTasksCrostiniTest, MultipleTypes) {
   std::vector<extensions::EntryInfo> entries{
       {crostini_folder_.Append("foo.gif"), "image/gif", false},
       {crostini_folder_.Append("bar.png"), "image/png", false}};
-  std::vector<GURL> file_urls{
-      GURL("filesystem:chrome-extension://id/dir/foo.gif"),
-      GURL("filesystem:chrome-extension://id/dir/bar.png")};
+  std::vector<GURL> file_urls{PathToURL("dir/foo.gif"),
+                              PathToURL("dir/bar.png")};
 
   std::vector<FullTaskDescriptor> tasks;
   FindAllTypesOfTasksSynchronousWrapper().Call(&test_profile_, entries,
@@ -1438,7 +1456,7 @@ TEST_F(FileManagerFileTasksCrostiniTest, MultipleTypes) {
   EXPECT_EQ(image_app_id_, tasks[0].task_descriptor().app_id);
 
   entries.emplace_back(crostini_folder_.Append("qux.mp4"), "video/mp4", false);
-  file_urls.emplace_back("filesystem:chrome-extension://id/dir/qux.mp4");
+  file_urls.emplace_back(PathToURL("dir/qux.mp4"));
   FindAllTypesOfTasksSynchronousWrapper().Call(&test_profile_, entries,
                                                file_urls, &tasks);
   EXPECT_EQ(0U, tasks.size());
@@ -1448,9 +1466,8 @@ TEST_F(FileManagerFileTasksCrostiniTest, AlternateMimeTypes) {
   std::vector<extensions::EntryInfo> entries{
       {crostini_folder_.Append("bar1.foo"), "text/plain", false},
       {crostini_folder_.Append("bar2.foo"), "application/octet-stream", false}};
-  std::vector<GURL> file_urls{
-      GURL("filesystem:chrome-extension://id/dir/bar1.foo"),
-      GURL("filesystem:chrome-extension://id/dir/bar2.foo")};
+  std::vector<GURL> file_urls{PathToURL("dir/bar1.foo"),
+                              PathToURL("dir/bar2.foo")};
 
   std::vector<FullTaskDescriptor> tasks;
   FindAllTypesOfTasksSynchronousWrapper().Call(&test_profile_, entries,
