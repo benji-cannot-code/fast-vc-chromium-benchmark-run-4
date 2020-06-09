2f36baacc904caf6ba333c9d01e5ef835c430c93
Andrew Xu <andrewxu@chromium.org>
2020-06-09 16:24:56 +0000

    Polish hotseat animation during overview transition
    When hotseat enters extended mode from home launcher mode, hotseat's
    bounds undergo the following changes:
    (1) The origin of the hotseat bounds moves to left.
    (2) The hotseat bounds become wider.
    
    Meanwhile, the hotseat background's origin is related to hotseat bounds.
    Denote the background origin by |background_origin|; denote the hotseat
    bounds by |bounds_rect|. Then  background_origin|.x is calculated
    roughly by the equation below:
    (1) |padding_inset| := (|bounds_rect|.width - space_taken_by_icons) / 2
    (2) |background_origin|.x := padding_inset + |bounds_rect|.x
    
    |bounds_rect|.x becomes smaller while |padding_inset| becomes bigger. It
    is the reason why the hotseat bounds move back and forth.
    
    In this CL, the custom layer element is created for the transition
    between home launcher mode and extended mode. It ensures that the
    hotseat background varies linearly in screen coordinates.
    
    Bug: 1042911
    Change-Id: Id91ce3ecf2bbee8509a3f794c6e943c6bf1d6ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225590
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776552}

--- ash/shelf/scrollable_shelf_view.cc
+++ ash/shelf/scrollable_shelf_view.cc
@@ -714,6 +714,22 @@ bool ScrollableShelfView::RequiresScrollingForItemSize(
   return !CanFitAllAppsWithoutScrolling(target_size, icons_preferred_size);
 }
 
+gfx::Insets ScrollableShelfView::GetTotalEdgePadding() const {
+  return extra_padding_insets_ + base_padding_insets_;
+}
+
+gfx::Insets ScrollableShelfView::CalculateTotalEdgePaddingInTargetBounds()
+    const {
+  return CalculateExtraEdgePadding(/*use_target_bounds=*/true) +
+         base_padding_insets_;
+}
+
+void ScrollableShelfView::SetTotalPaddingInsets(
+    const gfx::Insets& padding_insets) {
+  extra_padding_insets_ = padding_insets - base_padding_insets_;
+  shelf_view_->LayoutIfAppIconsOffsetUpdates();
+}
+
 views::View* ScrollableShelfView::GetShelfContainerViewForTest() {
   return shelf_container_view_;
 }
@@ -1432,10 +1448,6 @@ gfx::Insets ScrollableShelfView::CalculateExtraEdgePadding(
   return padding_insets;
 }
 
-gfx::Insets ScrollableShelfView::GetTotalEdgePadding() const {
-  return extra_padding_insets_ + base_padding_insets_;
-}
-
 int ScrollableShelfView::GetStatusWidgetSizeOnPrimaryAxis(
     bool use_target_bounds) const {
   const gfx::Size status_widget_size =
@@ -2089,8 +2101,10 @@ int ScrollableShelfView::CalculateScrollDistanceAfterAdjustment(
 }
 
 void ScrollableShelfView::UpdateAvailableSpace() {
-  extra_padding_insets_ =
-      CalculateExtraEdgePadding(/*use_target_bounds=*/false);
+  if (!is_padding_configured_externally_) {
+    extra_padding_insets_ =
+        CalculateExtraEdgePadding(/*use_target_bounds=*/false);
+  }
   base_padding_insets_ = CalculateBaseEdgePadding();
 
   available_space_ = GetLocalBounds();
