02e0e6f8bab6c4f5a9014bb8b4cbaf5c22d99405
Alexander Dunaev <adunaev@igalia.com>
2020-06-09 06:46:49 +0000

    Reland "[ozone/x11] Moved the DnD handling from X11WindowOzone to X11Window."
    This is a reland of 6cb8f1eedd730c494659b66b108f425e943ab540
    
    The previous version caused flakiness in interactive_ui_tests, namely
    BookmarkBarViewTest22.CloseSourceBrowserDuringDrag.  Most probably, that
    had been caused by mixing Ozone and non-Ozone logic and therefore having
    two paths for handling the XDND events.
    
    This CL guards Ozone code with USE_OZONE so X11 path is not touched.
    
    Original change's description:
    > [ozone/x11] Moved the DnD handling from X11WindowOzone to X11Window.
    >
    > This CL is the first step that prepares us to get rid of DWTHX11.
    >
    > As we proceed with that, at some point we will have to throw off the
    > DesktopDragDropClientAuraX11 because it is used by DWTHX11, and migrate
    > DnD to DDDCOzone.  As the latter will be used on both Ozone and
    > non-Ozone, it needs the X11window to handle DnD.
    >
    > Hence this change.  I move the DnD-related code from X11WindowOzone into
    > X11Window.
    >
    > Change-Id: I682c794d9cfab4b7ad6b6a9b89331526f31d227a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2213578
    > Reviewed-by: Maksim Sisov <msisov@igalia.com>
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#774026}
    
    Change-Id: I58b16b7ef5cad3c93fd9070d457f915c14ea41d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2226853
    Commit-Queue: Alexander Dunaev <adunaev@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776418}

--- ui/base/x/x11_drag_drop_client.cc
+++ ui/base/x/x11_drag_drop_client.cc
@@ -677,7 +677,7 @@ void XDragDropClient::SendXdndDrop(XID dest_window) {
 
 void XDragDropClient::EndMoveLoop() {
   StopEndMoveLoopTimer();
-  delegate_->EndMoveLoop();
+  delegate_->EndDragLoop();
 }
 
 }  // namespace ui
