051164ce0e2e0d414f08e02fcb6ff53e893081a7
Dan Harrington <harringtond@chromium.org>
2020-06-09 01:51:37 +0000

    feedv2: don't create service if disabled
    We were accidentally creating the service to check if it
    should be enabled.
    
    Bug: 1092633
    Change-Id: I63013b4b11da3f5ce39f78cd0f3895a50e7af316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236638
    Auto-Submit: Dan H <harringtond@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776335}

--- chrome/browser/android/feed/v2/feed_service_bridge.cc
+++ chrome/browser/android/feed/v2/feed_service_bridge.cc
@@ -42,8 +42,7 @@ DisplayMetrics FeedServiceBridge::GetDisplayMetrics() {
 
 bool FeedServiceBridge::IsEnabled() {
   Profile* profile = ProfileManager::GetLastUsedProfile();
-  return FeedServiceFactory::GetForBrowserContext(profile)->IsEnabled(
-      *profile->GetPrefs());
+  return FeedService::IsEnabled(*profile->GetPrefs());
 }
 
 }  // namespace feed
