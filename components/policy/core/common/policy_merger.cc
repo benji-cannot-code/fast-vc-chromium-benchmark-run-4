7db6c5afedb43ce2893c8bb66c380d8b34aba629
Aya ElAttar <ayaelattar@google.com>
2020-06-10 13:29:44 +0000

    Remove old PolicyMap::Entry::get_value
    1. Removed PolicyMap::Entry::get_value which accepts unique_ptr.
    2. Changed all its references to use the new one with
    base::Optional instead.
    
    Bug: 1092471
    Change-Id: Iaa1246f55e63bc3737e36141bb349905d48335e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2237974
    Auto-Submit: Aya Elsayed <ayaelattar@google.com>
    Commit-Queue: Aya Elsayed <ayaelattar@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776926}

--- components/policy/core/common/policy_merger.cc
+++ components/policy/core/common/policy_merger.cc
@@ -120,7 +120,7 @@ void PolicyListMerger::DoMerge(PolicyMap::Entry* policy) const {
     for (const base::Value* it : merged_values)
       new_value.Append(it->Clone());
 
-    policy->set_value(base::Value::ToUniquePtrValue(std::move(new_value)));
+    policy->set_value(std::move(new_value));
   }
   policy->ClearConflicts();
   policy->AddConflictingPolicy(std::move(new_conflict));
@@ -209,10 +209,9 @@ void PolicyDictionaryMerger::DoMerge(PolicyMap::Entry* policy) const {
   }
 
   auto new_conflict = policy->DeepCopy();
-  if (value_changed) {
-    policy->set_value(
-        base::Value::ToUniquePtrValue(std::move(merged_dictionary)));
-  }
+  if (value_changed)
+    policy->set_value(std::move(merged_dictionary));
+
   policy->ClearConflicts();
   policy->AddConflictingPolicy(std::move(new_conflict));
   policy->source = POLICY_SOURCE_MERGED;
