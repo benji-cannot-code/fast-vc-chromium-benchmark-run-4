20195a1a7bdd9d0f642a7c16cd2927700dea73b7
Fergal Daly <fergal@chromium.org>
2020-06-10 16:26:36 +0000

    Followup nits from https://crrev.com/c/2239738
    TBR=altimin@chromium.org
    
    Change-Id: Iafd0d8223663fe9e36b4a0767d3b6a04670d97f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239906
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776966}

--- tools/get_swarming_logs/get_one.sh
+++ tools/get_swarming_logs/get_one.sh
@@ -4,13 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Downloads all the log from a CQ run. You can find the task ID by e.g. clicking
+# Download the log from a CQ run. You can find the task ID by e.g. clicking
 # through to the CQ run from gerrit and finding, e.g. the line
 #
 # Swarming Task: 4cb6401085894f10
 #
-# This will create a .txt for this log in the output dir. If you want all the
-# logs of subtasks, see get_all.sh
+# The script will create a .txt for this log in the output dir. If you want all
+# the logs of subtasks, see get_all.sh
 #
 # Usage:
 #   get_one.sh <output_dir> <task ID>
