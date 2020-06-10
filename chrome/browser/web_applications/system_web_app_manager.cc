ff1781f209b96c7787cc992729f1ae46079d67a3
Wei Lee <wtlee@google.com>
2020-06-10 02:11:51 +0000

    Reland "[CCA WebUI] Move CCA and its WebUI to chromeos/components"
    This is a reland of c027dce360f73c49a97330b47994481fbd763937
    
    Original change's description:
    > [CCA WebUI] Move CCA and its WebUI to chromeos/components
    >
    > In this CL, we move some CCA related components to
    > //chromeos/components/camera_app_ui
    >
    > to make it consistent to Help app and Media app.
    >
    > 1. CCA resources
    >   From: //chrome/browser/resources/chromeos/camera/
    >   To: //chromeos/components/camera_app_ui/resources/
    >
    > 2. CCA WebUI
    >   From: //chrome/browser/ui/webui/chromeos/camera/
    >   To: //chromeos/components/camera_app_ui/
    >
    > 3. Camera App Helper Implementation
    >   From: //components/chromeos_camera/
    >   To: //chromeos/components/camera_app_ui/
    >
    > Bug: 980846
    > Test: Launch CCA and CCA works normally as a platform app
    > Test: tast run [DUT] camera.CCAUI*
    > Change-Id: Ic70bd54b35194e3c3398e9f3de9af200346491d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2131542
    > Commit-Queue: Wei Lee <wtlee@chromium.org>
    > Reviewed-by: Shik Chen <shik@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@google.com>
    > Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Dominick Ng <dominickn@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: calamity <calamity@chromium.org>
    > Auto-Submit: Wei Lee <wtlee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#775987}
    
    Bug: 980846
    Change-Id: If8421c04573c567c2f2e7d8842f022609fab2a4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235263
    Auto-Submit: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776804}

--- chrome/browser/web_applications/system_web_app_manager.cc
+++ chrome/browser/web_applications/system_web_app_manager.cc
@@ -95,8 +95,9 @@ base::flat_map<SystemAppType, SystemAppInfo> CreateSystemWebApps() {
   }
 
   if (SystemWebAppManager::IsAppEnabled(SystemAppType::CAMERA)) {
-    infos.emplace(SystemAppType::CAMERA,
-                  SystemAppInfo("Camera", GURL("chrome://camera/pwa.html")));
+    infos.emplace(
+        SystemAppType::CAMERA,
+        SystemAppInfo("Camera", GURL("chrome://camera-app/pwa.html")));
     infos.at(SystemAppType::CAMERA).uninstall_and_replace = {
         extension_misc::kCameraAppId};
   }
--- chromeos/components/camera_app_ui/resources/utils/cca.py
+++ chromeos/components/camera_app_ui/resources/utils/cca.py
@@ -20,7 +20,7 @@ import time
 
 @functools.lru_cache(1)
 def get_chromium_root():
-    path = os.path.realpath('../../../../../')
+    path = os.path.realpath('../../../../')
     assert os.path.basename(path) == 'src'
     return path
 
@@ -285,7 +285,7 @@ def parse_args(args):
 
 def main(args):
     cca_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    assert os.path.basename(cca_root) == 'camera'
+    assert os.path.basename(cca_root) == 'resources'
     os.chdir(cca_root)
 
     args = parse_args(args)
