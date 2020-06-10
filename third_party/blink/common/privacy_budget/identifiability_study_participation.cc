51e9f0d4ba96c202a4f5b14836be44dbd8ba07c9
Asanka Herath <asanka@chromium.org>
2020-06-10 15:14:54 +0000

    [privacy_budget] Introduce IdentifiabilityStudySettings.
    On the renderer there needs to be a method for determining whether the
    identifiability study is active. In addition to being a performance
    tweak -- no need to generate samples that would never be used -- this
    facility can also be used to turn down misbehaving samples.
    
    Bug: 973801
    Change-Id: I717f3abc4fbda1c97f5d6518c4f1f7d1ca174c66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225537
    Commit-Queue: Asanka Herath <asanka@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Maksim Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776944}

--- third_party/blink/common/privacy_budget/identifiability_study_participation.cc
+++ third_party/blink/common/privacy_budget/identifiability_study_participation.cc
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/privacy_budget/identifiability_study_participation.h"
 
+#include "third_party/blink/public/common/privacy_budget/identifiability_study_settings.h"
+
 namespace blink {
 
 bool IsUserInIdentifiabilityStudy() {
-  // TODO(crbug.com/973801): Implement.
-  return false;
+  return IdentifiabilityStudySettings::Get()->IsActive();
 }
 
 }  // namespace blink
