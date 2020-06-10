fe70bafb862922917e9a891712d8e136f8e35516
Joey Arhar <jarhar@chromium.org>
2020-06-10 15:56:40 +0000

    Don't make Document::Shutdown cancel form submissions
    I added this line in Document::Shutdown in crrev.com/c/2048035
    because I was trying to recreate logic from NavigationScheduler, and I
    noticed that there was a line there to to cancel scheduled http
    refreshes, which is that NavigationScheduler was renamed to.
    
    I thought this would help make the async form submission behavior more
    like the correct behavior we used to have, but it turns out that back
    when we had async form submissions (before crrev.com/c/1520353),
    Document::Shutdown actually didn't cancel pending form submissions. The
    http refresh canceling was actually added to avoid a crash after the
    http refresh scheduler was moved from Frame to Document.
    
    Now that I moved pending form submissions from the Document containing
    the form which initiated the submission to the Frame which is being
    navigated in crrev.com/c/2212458, it introduced a race condition where
    the subframe being navigated would cancel its pending form submission in
    Document::Shutdown.
    
    This patch stops canceling form submissions in Document::Shutdown. I
    don't think this will cause problems now that pending form submissions
    are no longer stored in Document.
    
    I wasn't able to add a test because the undesired behavior was extremely
    flaky. The reporter provided a test which they said failed on their
    computer, but I couldn't get it to fail on mine or any of the cq+1
    trybots. I was only able to reproduce the bug on their production
    website which they mentioned in the bug, which is how I figured out how
    to fix this.
    
    Bug: 1092200
    Change-Id: I59b8cf81a5629d5eba09c55e6f320874f3f0ad72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236218
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Mason Freed <masonfreed@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776953}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -3281,7 +3281,6 @@ void Document::Shutdown() {
 
   CancelPendingJavaScriptUrls();
   http_refresh_scheduler_->Cancel();
-  GetFrame()->CancelFormSubmission();
 
   DetachCompositorTimeline(Timeline().CompositorTimeline());
 
