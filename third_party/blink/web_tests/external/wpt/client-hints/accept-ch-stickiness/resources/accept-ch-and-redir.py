bb56c7c51a357368f8c58b33166179fe1b31c0b4
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-06-10 17:05:51 +0000

    Import wpt@4d4166da585da2a0b00c123ae4fe853f107669e1
    Using wpt-import in Chromium 84a440a4818bae43a972cfc7c7c67ba4a0318003.
    With Chromium commits locally applied on WPT:
    9c24e63c71 "Check for first frame in CreateImageBitmap"
    a8a2808358 "[Security][Coop] Browsing context switch reporting WPT"
    b2436c3bfe "Ensure cloning a template element into an inactive document does not crash."
    027aefc6b5 "OPTION element: Update OPTION label even if it has invalid markup"
    0776aa6ddc "Mark pointerevent_pointermove_in_pointerlock test non flaky"
    d2ee98f482 "Move manual offscreen wpt tests to own folder"
    458dc853b0 "WPT: Migrate content-visibility tests to wpt."
    84f61c816c "Exclude non relevants reports from the COOP-opener-breakage tests."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Change-Id: I10d45089057aad743e2b4aca2465a6928edeced7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239528
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#776992}

--- third_party/blink/web_tests/external/wpt/client-hints/accept-ch-stickiness/resources/accept-ch-and-redir.py
+++ third_party/blink/web_tests/external/wpt/client-hints/accept-ch-stickiness/resources/accept-ch-and-redir.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
-    url = ''
-    if 'url' in request.GET:
-        url = request.GET['url']
-    return 301, [('Location', url),('Accept-CH', 'device-memory, DPR')], ''
+    url = b''
+    if b'url' in request.GET:
+        url = request.GET[b'url']
+    return 301, [(b'Location', url),(b'Accept-CH', b'device-memory, DPR')], u''
--- third_party/blink/web_tests/external/wpt/client-hints/resources/sec-ch-ua.py
+++ third_party/blink/web_tests/external/wpt/client-hints/resources/sec-ch-ua.py
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
-    ua = request.headers.get('sec-ch-ua', '')
-    response.headers.set("Content-Type", "text/html")
-    response.headers.set("Accept-CH", "UA")
-    response.headers.set("Accept-CH-Lifetime", "10")
-    response.content = '''
+    ua = request.headers.get(b'sec-ch-ua', b'')
+    response.headers.set(b"Content-Type", b"text/html")
+    response.headers.set(b"Accept-CH", b"UA")
+    response.headers.set(b"Accept-CH-Lifetime", b"10")
+    response.content = b'''
 <script>
   window.opener.postMessage({ header: '%s' }, "*");
 </script>
