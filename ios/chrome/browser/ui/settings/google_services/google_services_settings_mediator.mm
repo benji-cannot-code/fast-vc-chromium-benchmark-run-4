016058d4a10b69e75d3a4a3431a9189ddb93dc8e
Ali Juma <ajuma@chromium.org>
2020-06-10 16:53:10 +0000

    Reland: [iOS] Make password leak checks toggleable when Safe Browsing is not available
    This relands r776646 (previously reviewed at http://crrev.com/c/2237716),
    with test fixes:
    1. On the settings pages, scroll up when needed (on small devices)
       to make the Safe Browsing and password leak check items visible.
    2. In testTogglePasswordLeakCheckWhenSafeBrowsingNotAvailable,
       add coverage for the case where a user opts-out of Safe Browsing
       on another device.
    
    Description of previous CL:
    This CL fixes a regression from http://crrev.com/c/2174995
    that prevents users who do not yet have Safe Browsing
    rolled out to them from toggling the password leak check setting.
    
    This CL ensures that the value of the Safe Browsing opt-out
    pref is always initialized in GoogleServicesSettingsMediator,
    regardless of whether Safe Browsing has been rolled out. With
    this change, the other logic changes in
    http://crrev.com/c/2174995 work as expected, whether or not
    Safe Browsing is available.
    
    Bug: 1090406, 1093306
    Change-Id: Id511dc339be146bcc15d865d90feb5cf93369b51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239300
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776981}

--- ios/chrome/browser/ui/settings/google_services/google_services_settings_mediator.mm
+++ ios/chrome/browser/ui/settings/google_services/google_services_settings_mediator.mm
@@ -188,12 +188,10 @@ NSString* kGoogleServicesSyncErrorImage = @"google_services_sync_error";
         initWithPrefService:userPrefService
                    prefName:prefs::kSearchSuggestEnabled];
     _autocompleteSearchPreference.observer = self;
-    if (base::FeatureList::IsEnabled(kSafeBrowsingAvailableOnIOS)) {
-      _safeBrowsingPreference = [[PrefBackedBoolean alloc]
-          initWithPrefService:userPrefService
-                     prefName:prefs::kSafeBrowsingEnabled];
-      _safeBrowsingPreference.observer = self;
-    }
+    _safeBrowsingPreference = [[PrefBackedBoolean alloc]
+        initWithPrefService:userPrefService
+                   prefName:prefs::kSafeBrowsingEnabled];
+    _safeBrowsingPreference.observer = self;
     _sendDataUsagePreference = [[PrefBackedBoolean alloc]
         initWithPrefService:localPrefService
                    prefName:metrics::prefs::kMetricsReportingEnabled];
@@ -598,6 +596,8 @@ NSString* kGoogleServicesSyncErrorImage = @"google_services_sync_error";
                   detailStringID:
                       IDS_IOS_GOOGLE_SERVICES_SETTINGS_SAFE_BROWSING_DETAIL
                         dataType:0];
+      safeBrowsingItem.accessibilityIdentifier =
+          kSafeBrowsingItemAccessibilityIdentifier;
       [items addObject:safeBrowsingItem];
     }
     [items addObject:self.passwordLeakCheckItem];
@@ -632,7 +632,7 @@ NSString* kGoogleServicesSyncErrorImage = @"google_services_sync_error";
         l10n_util::GetNSString(IDS_IOS_LEAK_CHECK_SWITCH);
     passwordLeakCheckItem.on = [self passwordLeakCheckItemOnState];
     passwordLeakCheckItem.accessibilityIdentifier =
-        @"passwordLeakCheckItem_switch";
+        kPasswordLeakCheckItemAccessibilityIdentifier;
     passwordLeakCheckItem.enabled = self.isAuthenticated;
     _passwordLeakCheckItem = passwordLeakCheckItem;
   }
