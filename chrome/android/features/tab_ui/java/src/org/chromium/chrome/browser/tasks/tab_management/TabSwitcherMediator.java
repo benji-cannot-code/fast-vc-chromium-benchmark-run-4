50c9ab11ddadb58d6275e8175f27367ab4115826
Yue Zhang <yuezhanggg@chromium.org>
2020-06-10 21:17:34 +0000

    Reland "Skip showing tab switcher message card in multi-window mode"
    This reverts commit 2b6086756c9e39aac2cebb15f05d52a940ec0991.
    
    Reason for revert: Reproduced the flakiness with swarm bots, the fix
    is verified with swarm bots as well.
    
    Original change's description:
    > Revert "Skip showing tab switcher message card in multi-window mode"
    >
    > This reverts commit 3d506040da339ad7c9dd2752efc7eb0306f2469e.
    >
    > Reason for revert: The test fails on multiple builders. E.g.:
    > https://ci.chromium.org/p/chromium/builders/ci/Marshmallow%2064%20bit%20Tester/32695
    >
    > The call stack is:
    > android.support.test.espresso.base.DefaultFailureHandler$AssertionFailedWithCauseError: View is present in the hierarchy: MessageCardView{id=2131428697, res-name=tab_grid_message_item, visibility=VISIBLE, width=1038, height=156, has-focus=false, has-focusable=true, has-window-focus=true, is-clickable=false, is-enabled=true, is-focused=false, is-focusable=false, is-layout-requested=false, is-selected=false, root-is-layout-requested=false, has-input-connection=false, x=21.0, y=603.0, child-count=3}
    > Expected: is <false>
    >      Got: <true>
    >
    > 	at dalvik.system.VMStack.getThreadStackTrace(Native Method)
    > 	at java.lang.Thread.getStackTrace(Thread.java:580)
    > 	at android.support.test.espresso.base.DefaultFailureHandler.getUserFriendlyError(DefaultFailureHandler.java:82)
    > 	at android.support.test.espresso.base.DefaultFailureHandler.handle(DefaultFailureHandler.java:53)
    > 	at android.support.test.espresso.ViewInteraction.runSynchronouslyOnUiThread(ViewInteraction.java:184)
    > 	at android.support.test.espresso.ViewInteraction.check(ViewInteraction.java:158)
    > 	at org.chromium.chrome.browser.tasks.tab_management.TabGridIphTest.testNotShowIPHInMultiWindowMode(TabGridIphTest.java:344)
    >
    >
    > Original change's description:
    > > Skip showing tab switcher message card in multi-window mode
    > >
    > > Bug: 1085630
    > > Change-Id: If0e6e852458aedf448647e3ab8320519c4775bd2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2216630
    > > Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > > Reviewed-by: Mei Liang <meiliang@chromium.org>
    > > Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#776308}
    >
    > TBR=wychen@chromium.org,meiliang@chromium.org,yuezhanggg@chromium.org
    >
    > Change-Id: I60c9ff0219832ccda197550ee8de92b6bb91fb54
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1085630
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236502
    > Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    > Commit-Queue: Pavel Yatsuk <pavely@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#776586}
    
    TBR=pavely@chromium.org,wychen@chromium.org,meiliang@chromium.org,yuezhanggg@chromium.org
    
    # Not skipping CQ checks because this is a reland.
    
    Bug: 1085630
    Change-Id: Ifa62e5909b152603abe964df302cbbc7e1ac237f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238503
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777107}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMediator.java
@@ -36,6 +36,7 @@ import org.chromium.chrome.browser.flags.CachedFeatureFlags;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.fullscreen.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.init.FirstDrawDetector;
+import org.chromium.chrome.browser.multiwindow.MultiWindowModeStateDispatcher;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabCreationState;
 import org.chromium.chrome.browser.tab.TabHidingType;
@@ -95,6 +96,8 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
     private final BrowserControlsStateProvider.Observer mBrowserControlsObserver;
     private final ViewGroup mContainerView;
     private final TabContentManager mTabContentManager;
+    private final MultiWindowModeStateDispatcher mMultiWindowModeStateDispatcher;
+    private final MultiWindowModeStateDispatcher.MultiWindowModeObserver mMultiWindowModeObserver;
 
     private Integer mSoftCleanupDelayMsForTesting;
     private Integer mCleanupDelayMsForTesting;
@@ -190,17 +193,21 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
      * @param browserControlsStateProvider {@link BrowserControlsStateProvider} to use.
      * @param containerView The container {@link ViewGroup} to use.
      * @param tabContentManager The {@link TabContentManager} for first meaningful paint event.
+     * @param multiWindowModeStateDispatcher The {@link MultiWindowModeStateDispatcher} to observe
+     *         for multi-window related changes.
      * @param mode One of the {@link TabListCoordinator.TabListMode}.
      */
     TabSwitcherMediator(ResetHandler resetHandler, PropertyModel containerViewModel,
             TabModelSelector tabModelSelector,
             BrowserControlsStateProvider browserControlsStateProvider, ViewGroup containerView,
             TabContentManager tabContentManager, MessageItemsController messageItemsController,
+            MultiWindowModeStateDispatcher multiWindowModeStateDispatcher,
             @TabListCoordinator.TabListMode int mode) {
         mResetHandler = resetHandler;
         mContainerViewModel = containerViewModel;
         mTabModelSelector = tabModelSelector;
         mBrowserControlsStateProvider = browserControlsStateProvider;
+        mMultiWindowModeStateDispatcher = multiWindowModeStateDispatcher;
         mMode = mode;
 
         mTabModelSelectorObserver = new EmptyTabModelSelectorObserver() {
@@ -365,6 +372,15 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
         // TODO(crbug.com/982018): Let the start surface pass in the parameter and add unit test for
         // it. This is a temporary solution to keep this change minimum.
         mShowTabsInMruOrder = isShowingTabsInMRUOrder();
+
+        mMultiWindowModeObserver = isInMultiWindowMode -> {
+            if (isInMultiWindowMode) {
+                messageItemsController.removeAllAppendedMessage();
+            } else {
+                messageItemsController.restoreAllAppendedMessage();
+            }
+        };
+        mMultiWindowModeStateDispatcher.addObserver(mMultiWindowModeObserver);
     }
 
     /**
@@ -725,6 +741,7 @@ class TabSwitcherMediator implements TabSwitcher.Controller, TabListRecyclerView
         mBrowserControlsStateProvider.removeObserver(mBrowserControlsObserver);
         mTabModelSelector.getTabModelFilterProvider().removeTabModelFilterObserver(
                 mTabModelObserver);
+        mMultiWindowModeStateDispatcher.removeObserver(mMultiWindowModeObserver);
     }
 
     void setOnTabSelectingListener(TabSwitcher.OnTabSelectingListener listener) {
