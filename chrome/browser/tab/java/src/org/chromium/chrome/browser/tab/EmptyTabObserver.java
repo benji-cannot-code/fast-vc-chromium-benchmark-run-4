66b41d6bace5beb66cdae9bf467036ae0dffea40
Jinsuk Kim <jinsukkim@chromium.org>
2020-06-10 05:07:10 +0000

    Reland "Android: Use ActivityTabTabObserver in FulscreenManager"
    The CL was reverted due to a bug when used in Tablet which
    was fixed by https://crrev.com/c/2237012. Now can be landed
    again as is.
    
    This reverts commit c90c90f17b98ac146aba27c85420f04d7d71f523.
    
    TBR=twellington@chromium.org
    
    Bug: 966272, 1092390
    Change-Id: I4a6c671afa6544ccc2846214e1e841200e0083c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2237536
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776838}

--- chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/EmptyTabObserver.java
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/EmptyTabObserver.java
@@ -153,5 +153,5 @@ public class EmptyTabObserver implements TabObserver {
             int bottomControlsMinHeightOffsetY) {}
 
     @Override
-    public void onContentViewScrollingStateChanged(Tab tab, boolean scrolling) {}
+    public void onContentViewScrollingStateChanged(boolean scrolling) {}
 }
