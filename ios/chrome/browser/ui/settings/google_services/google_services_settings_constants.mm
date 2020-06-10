5d1f4e9b70d571530268e25a236d079bb554f316
Stepan Khapugin <stkhapugin@chromium.org>
2020-06-10 13:34:41 +0000

    Revert "[iOS] Make password leak checks toggleable when Safe Browsing is not available"
    This reverts commit 096c17627aa9ce3cb136009a27096f83ee1e1c33.
    
    Reason for revert: new tests fail on iphone-simulator (see crbug.com/1093306)
    
    Original change's description:
    > [iOS] Make password leak checks toggleable when Safe Browsing is not available
    > 
    > This CL fixes a regression from http://crrev.com/c/2174995
    > that prevents users who do not yet have Safe Browsing
    > rolled out to them from toggling the password leak check setting.
    > 
    > This CL ensures that the value of the Safe Browsing opt-out
    > pref is always initialized in GoogleServicesSettingsMediator,
    > regardless of whether Safe Browsing has been rolled out. With
    > this change, the other logic changes in
    > http://crrev.com/c/2174995 work as expected, whether or not
    > Safe Browsing is available.
    > 
    > Bug: 1090406
    > Change-Id: I901f04faad60f136b89161091eeb6504415718a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2237716
    > Commit-Queue: Ali Juma <ajuma@chromium.org>
    > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#776646}
    
    TBR=ajuma@chromium.org,sczs@chromium.org
    
    Change-Id: Iac9d50cbbdd03ee5098565cb83d5a3c2ae4b6deb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1090406
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239841
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776927}

--- ios/chrome/browser/ui/settings/google_services/google_services_settings_constants.mm
+++ ios/chrome/browser/ui/settings/google_services/google_services_settings_constants.mm
@@ -17,9 +17,3 @@ NSString* const kManageSyncCellAccessibilityIdentifier =
 
 NSString* const kAccountListItemAccessibilityIdentifier =
     @"AccountListItemAccessibilityIdentifier";
-
-NSString* const kPasswordLeakCheckItemAccessibilityIdentifier =
-    @"PasswordLeakCheckItemAccessibilityIdentifier";
-
-NSString* const kSafeBrowsingItemAccessibilityIdentifier =
-    @"SafeBrowsingItemAccessibilityIdentifier";
