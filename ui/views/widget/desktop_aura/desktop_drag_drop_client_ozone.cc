ebf14b37f7c7fccc197381820509bdfd16fe8903
Alexander Dunaev <adunaev@igalia.com>
2020-06-10 16:14:50 +0000

    [ozone] Replaced a DCHECK with if.
    This issue has been found by a test:
    MenuViewDragAndDropTestTestInMenuDrag.TestInMenuDrag
    
    The grag_context_ can become nullptr before the task comes to execution.  This CL fixes that.
    
    Bug: 1014860
    Change-Id: I279a4bec0274cc50c8f745361463a787471fca9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238149
    Auto-Submit: Alexander Dunaev <adunaev@igalia.com>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776960}

--- ui/views/widget/desktop_aura/desktop_drag_drop_client_ozone.cc
+++ ui/views/widget/desktop_aura/desktop_drag_drop_client_ozone.cc
@@ -355,7 +355,8 @@ DesktopDragDropClientOzone::UpdateTargetAndCreateDropEvent(
 }
 
 void DesktopDragDropClientOzone::UpdateDragWidgetLocation() {
-  DCHECK(drag_context_);
+  if (!drag_context_)
+    return;
 
   float scale_factor =
       ui::GetScaleFactorForNativeView(drag_context_->widget->GetNativeWindow());
