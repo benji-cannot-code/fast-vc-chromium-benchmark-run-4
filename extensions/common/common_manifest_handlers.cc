c9794d2a24d81ac44d3df8f5744522be82fe31b7
Rahul Singh <rahsin@microsoft.com>
2020-06-10 22:54:21 +0000

    DPWA: Add web_app_linked_shortcut_items manifest handler.
    This CL lays the ground work for migrating Extensions based Shortcuts
    Menu data to a WebApp based representation. As part of this,
    web_app_linked_shortcut_icons has transformed into
    web_app_linked_shortcut_items. This reflects a move towards storing
    information at the shortcut level instead of the shortcut icon level.
    This CL renames and modifies the web_app_linked_shortcut_icons* files
    added in this CL: https://crrev.com/c/2183713
    
    With the previous structure, we needed to store the shortcut item index
    and name for the shortcut that the icon was associated with. That design
    was a flat list of all shortcut icons. As a result, shortcut item name
    and index would be repeated in the list when multiple icons were
    specified for a shortcut. This was necessary to associate an icon with
    the right shortcut. The new web_app_linked_shortcut_items representation
    maintains a list of all shortcuts. Each shortcut in turn has a list of
    shortcut_item_icon_infos, each with their respective size and url.
    
    The previous structure was aligned with how we store app icon info in
    the extensions manifest. This turned out to be problematic from a
    migration perspective because:
    1. We need to move over shortcuts AND their icons. Not just the icons.
    2. We were lacking shortcut_url info.
    3. The migration code wouldn't have a 1:1 mapping when moving data from
       the extensions representation to the WebApp one. This would make for
       migration code that is hard to read and test.
    
    To that last point, WebApp::WebAppShortcutsMenuItemInfo contains all the
    info from WebApplicationShortcutInfo except shortcut_icon_bitmaps. The
    structure of WebAppShortcutsMenuItemInfo is the same as
    WebApplicationShortcutInfo minus the shortcut_icon_bitmaps vector. So on
    the extensions side it makes sense to align to this with
    web_app_linked_shortcut_items.
    
    On the WebApp side, we store downloaded icon sizes for each icon for a
    shortcut, across all shortcuts in:
    std::vector<std::vector<SquareSizePx>>
    downloaded_shortcuts_menu_icons_sizes_. During migration we can easily
    populate this by iterating over WebAppShortcutIconsHandler on the
    extensions side. So, it makes sense to keep WebAppShortcutIconsHandler
    separate from web_app_linked_shortcut_items.
    
    With this change when migrating shortcuts menu, we would:
    1. Use web_app_linked_shortcut_items to populate
       WebApp::WebAppShortcutsMenuItemInfo. This would store broader
       shortcuts info.
    2. Use WebAppShortcutIconsHandler to populate
       downloaded_shortcuts_menu_icons_sizes_. This would store shortcuts
       icons info.
    
    This design should make migration easier to reason over. Tangentially,
    good alignment with WebApplicationShortcutInfo has helped simplify test
    code in convert_web_app_unittests.cc.
    
    Note:
    With this change, shortcuts menu data for existing extensions based PWAs
    won't migrate to WebApps. However, as the Shortcuts Menu feature is
    under development and DISABLED_BY_DEFAULT this should have a very
    limited impact.
    
    Testing:
    I've updated existing tests and added some new test cases for
    web_app_linked_shortcut_items.
    
    Bug: 1069316
    Change-Id: I66edfac17cb8b8bf2c3998f8038d037765449a1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220624
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Alexey Baskakov <loyso@chromium.org>
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#777148}

--- extensions/common/common_manifest_handlers.cc
+++ extensions/common/common_manifest_handlers.cc
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest_handlers/shared_module_info.h"
 #include "extensions/common/manifest_handlers/web_accessible_resources_info.h"
 #include "extensions/common/manifest_handlers/web_app_file_handler.h"
-#include "extensions/common/manifest_handlers/web_app_linked_shortcut_icons.h"
+#include "extensions/common/manifest_handlers/web_app_linked_shortcut_items.h"
 #include "extensions/common/manifest_handlers/web_app_shortcut_icons_handler.h"
 #include "extensions/common/manifest_handlers/webview_info.h"
 #include "extensions/common/manifest_url_handlers.h"
@@ -83,7 +83,7 @@ void RegisterCommonManifestHandlers() {
   registry->RegisterHandler(std::make_unique<WebAccessibleResourcesHandler>());
   registry->RegisterHandler(std::make_unique<WebAppFileHandlersParser>());
   registry->RegisterHandler(
-      std::make_unique<WebAppLinkedShortcutIconsHandler>());
+      std::make_unique<WebAppLinkedShortcutItemsHandler>());
   registry->RegisterHandler(std::make_unique<WebAppShortcutIconsHandler>());
   registry->RegisterHandler(std::make_unique<WebviewHandler>());
 }
