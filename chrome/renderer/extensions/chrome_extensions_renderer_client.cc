a9f9f42cf9fcfc6ad59a382d1793c49942532470
mark a. foltz <mfoltz@chromium.org>
2020-06-10 20:09:38 +0000

    [Media Router] Delete chrome.cast.streaming APIs.
    These APIs were used to initiate and control tab and desktop streaming
    to Chromecast devices from extensions.  All of this functionality was
    migrated to //components/mirroring which launched in M81; therefore
    these APIs are obsolete.
    
    They are whitelisted to Media Router extensions and manifest permissions
    for them were removed in M84, so they are safe to delete now.
    
    performance_test.cc is kept because it is a general test for tab
    capture performance.  It needs to be updated to no longer rely on
    extension APIs and re-enabled, that is being tracked under
    https://crbug.com/974427
    
    Bug: 986981
    Change-Id: I74b9e4e8393d095baeca5bb6d5279247175bcb31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225821
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777074}

--- chrome/renderer/extensions/chrome_extensions_renderer_client.cc
+++ chrome/renderer/extensions/chrome_extensions_renderer_client.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/extensions/extension_process_policy.h"
 #include "chrome/renderer/extensions/renderer_permissions_policy_delegate.h"
 #include "chrome/renderer/extensions/resource_request_policy.h"
-#include "chrome/renderer/media/cast_ipc_dispatcher.h"
 #include "content/public/common/content_constants.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/renderer/render_frame.h"
@@ -167,7 +166,6 @@ void ChromeExtensionsRendererClient::RenderThreadStarted() {
 
   thread->AddObserver(extension_dispatcher_.get());
   thread->AddObserver(guest_view_container_dispatcher_.get());
-  thread->AddFilter(new CastIPCDispatcher(thread->GetIOTaskRunner()));
 }
 
 void ChromeExtensionsRendererClient::RenderFrameCreated(
