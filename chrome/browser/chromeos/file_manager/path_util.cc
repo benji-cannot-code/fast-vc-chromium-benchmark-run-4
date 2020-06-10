3910ac8bd4b35918c0241366cff84bcf4fae12d7
Joel Hockey <joelhockey@chromium.org>
2020-06-10 19:54:27 +0000

    Share /media/archive with VMs
    Bug: 1091611
    Change-Id: Ib304ef0c6ee2e357585eec66ed4465dda2ce04a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233943
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777067}

--- chrome/browser/chromeos/file_manager/path_util.cc
+++ chrome/browser/chromeos/file_manager/path_util.cc
@@ -151,6 +151,9 @@ const base::FilePath::CharType kAndroidFilesPath[] =
 const base::FilePath::CharType kSystemFontsPath[] =
     FILE_PATH_LITERAL("/usr/share/fonts");
 
+const base::FilePath::CharType kArchiveMountPath[] =
+    FILE_PATH_LITERAL("/media/archive");
+
 base::FilePath GetDownloadsFolderForProfile(Profile* profile) {
   // Check if FilesApp has a registered path already.  This happens for tests.
   const std::string mount_point_name =
@@ -365,6 +368,10 @@ bool ConvertFileSystemURLToPathInsideCrostini(
   } else if (id == GetAndroidFilesMountPointName()) {
     *inside = crostini::ContainerChromeOSBaseDirectory().Append(
         kCrostiniMapPlayFiles);
+  } else if (id == chromeos::kSystemMountNameArchive) {
+    // Archive.
+    *inside = crostini::ContainerChromeOSBaseDirectory().Append(
+        chromeos::kSystemMountNameArchive);
   } else {
     return false;
   }
@@ -603,6 +610,12 @@ std::string GetPathDisplayTextForSettings(Profile* profile,
                                .value(),
                            "")) {
     // Strip prefix of "/media/removable/" including trailing slash.
+  } else if (ReplacePrefix(&result,
+                           base::FilePath(kArchiveMountPath)
+                               .AsEndingWithSeparator()
+                               .value(),
+                           "")) {
+    // Strip prefix of "/media/archive/" including trailing slash.
   }
 
   base::ReplaceChars(result, "/", " \u203a ", &result);
