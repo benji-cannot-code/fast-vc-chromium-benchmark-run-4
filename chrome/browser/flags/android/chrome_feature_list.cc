a16575cb4a84a6117346d1854c7252b9df8f7d04
Ben Schwartz <bemasc@google.com>
2020-06-10 18:43:27 +0000

    Expose Secure DNS functions to Java
    This is a prerequisite for an Android implementation of the Secure DNS
    settings UI.
    
    Bug: 1040146
    Change-Id: Iebfa1b0423d75860b5f7db9c5a700fcd5647cef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229303
    Commit-Queue: Ben Schwartz <bemasc@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Auto-Submit: Ben Schwartz <bemasc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777034}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -214,6 +214,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kVrBrowsingFeedback,
     &kWebApkAdaptiveIcon,
     &kPrefetchNotificationSchedulingIntegration,
+    &features::kDnsOverHttps,
     &net::features::kSameSiteByDefaultCookies,
     &net::features::kCookiesWithoutSameSiteMustBeSecure,
     &paint_preview::kPaintPreviewCaptureExperiment,
