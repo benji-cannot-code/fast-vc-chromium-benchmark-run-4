a336f5233ab3dded96cf9021f4676b42547822e7
Sophey Dong <sophey@chromium.org>
2020-06-10 20:07:18 +0000

    [Sharing Hub] Add image entry point.
    Bug: 1079467
    Change-Id: I44b799bb5971b55469e00f82c94f1f128717f125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231847
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Kyle Milka <kmilka@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777072}

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/ChromeContextMenuPopulator.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.contextmenu;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -22,6 +23,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
+import org.chromium.base.ContextUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.base.metrics.RecordHistogram;
@@ -65,6 +67,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -738,9 +741,22 @@ public class ChromeContextMenuPopulator implements ContextMenuPopulator {
      * it will use the right activity set when the menu was displayed.
      * @param renderFrameHost {@link RenderFrameHost} to get the encoded images from.
      */
-    void shareImage(RenderFrameHost renderFrameHost) {
-        retrieveImage(renderFrameHost, ContextMenuImageFormat.ORIGINAL,
-                (Uri imageUri) -> { ShareHelper.shareImage(getWindow(), null, imageUri); });
+    private void shareImage(RenderFrameHost renderFrameHost) {
+        retrieveImage(renderFrameHost, ContextMenuImageFormat.ORIGINAL, (Uri imageUri) -> {
+            if (!ChromeFeatureList.isEnabled(ChromeFeatureList.CHROME_SHARING_HUB_V15)) {
+                ShareHelper.shareImage(getWindow(), null, imageUri);
+                return;
+            }
+            ContentResolver contentResolver =
+                    ContextUtils.getApplicationContext().getContentResolver();
+            ShareParams imageShareParams =
+                    new ShareParams.Builder(getWindow(), /*title=*/"", /*url=*/"")
+                            .setFileUris(new ArrayList<>(Collections.singletonList(imageUri)))
+                            .setFileContentType(contentResolver.getType(imageUri))
+                            .build();
+            mShareDelegateSupplier.get().share(imageShareParams,
+                    new ChromeShareExtras.Builder().setSaveLastUsed(true).build());
+        });
     }
 
     @Override
