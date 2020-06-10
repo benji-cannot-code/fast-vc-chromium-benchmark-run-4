14355cc84875acb766c1523365f2937ccf97150a
Tomasz Wiszkowski <ender@google.com>
2020-06-10 01:24:22 +0000

    Extract DropdownViewInfoListBuilder from AutocompleteMediator.
    Change-Id: I736c4531e8dd7f29c6b7cbce49522e9bde0dcf3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229598
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776796}

--- chrome/test/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinatorTestUtils.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinatorTestUtils.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox.suggestions;
 
-import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteMediator.DropdownItemViewInfo;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 
 /**
