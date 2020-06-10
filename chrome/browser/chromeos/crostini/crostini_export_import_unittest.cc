b2cc9c6ff80668ce37218adc2ee0f650198ac972
Joel Hockey <joelhockey@chromium.org>
2020-06-10 03:16:49 +0000

    Clean up tests to revoke system external mount points
    I noticed these when I was having some failures in
    FileManagerPathUtilTest.  These were not actually causing any issues,
    but it is still better to revoke any registered system mount points at
    the end of tests.
    
    Change-Id: Ic58ff6c560de35a4839d58483d5f1d0dde13152d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2237434
    Commit-Queue: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776823}

--- chrome/browser/chromeos/crostini/crostini_export_import_unittest.cc
+++ chrome/browser/chromeos/crostini/crostini_export_import_unittest.cc
@@ -147,9 +147,7 @@ class CrostiniExportImportTest : public testing::Test {
     profile()->GetPrefs()->SetBoolean(
         crostini::prefs::kUserCrostiniExportImportUIAllowedByPolicy, true);
 
-    storage::ExternalMountPoints* mount_points =
-        storage::ExternalMountPoints::GetSystemInstance();
-    mount_points->RegisterFileSystem(
+    storage::ExternalMountPoints::GetSystemInstance()->RegisterFileSystem(
         file_manager::util::GetDownloadsMountPointName(profile()),
         storage::kFileSystemTypeNativeLocal, storage::FileSystemMountOption(),
         file_manager::util::GetMyFilesFolderForProfile(profile()));
@@ -158,6 +156,8 @@ class CrostiniExportImportTest : public testing::Test {
   }
 
   void TearDown() override {
+    storage::ExternalMountPoints::GetSystemInstance()->RevokeFileSystem(
+        file_manager::util::GetDownloadsMountPointName(profile()));
     crostini_export_import_.reset();
     // If the file has been created (by an export), then delete it, but first
     // shutdown GuestOsSharePath to ensure watchers are destroyed, otherwise
