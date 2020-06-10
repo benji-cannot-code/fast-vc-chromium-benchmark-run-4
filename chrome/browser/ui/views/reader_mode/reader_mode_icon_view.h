b2a5267f5bd8f73f5117f3e89467b4c8e9714f03
Katie D <katie@chromium.org>
2020-06-10 17:20:27 +0000

    Remove reader mode page from navigation stack on desktop.
    This means users will not be able to navigate to Reader Mode pages
    using "back" or "forward" buttons, matching behavior in Android.
    
    Also improves the ReaderModeIconViewBrowserTest by resolving a TODO.
    
    AX-Relnotes: n/a. Reader mode is still behind a flag.
    Bug: 1090588, 1087949
    Change-Id: I016a02b95b1a3acbff9299c518e8bf2780379d32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235988
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777001}

--- chrome/browser/ui/views/reader_mode/reader_mode_icon_view.h
+++ chrome/browser/ui/views/reader_mode/reader_mode_icon_view.h
@@ -55,6 +55,8 @@ class ReaderModeIconView : public PageActionIconView,
   void OnResult(const dom_distiller::DistillabilityResult& result) override;
 
  private:
+  friend class ReaderModeIconViewBrowserTest;
+
   PrefService* pref_service_;
 
   DISALLOW_COPY_AND_ASSIGN(ReaderModeIconView);
