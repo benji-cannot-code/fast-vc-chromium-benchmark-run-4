9436923be653b114a56a23698a71e9337b51b0e8
Yue Zhang <yuezhanggg@chromium.org>
2020-06-10 18:01:36 +0000

    Update tab strip UI for dark mode
    * Update the tap size of the items in strip from 46dpX48dp to 48dpX48dp.
    * Add a 20dpX20dp white circle as the background of each favicon in tab
      strip.
    * Change the background color of the selected favicon in tab strip to
      transparent.
    * Update the selected ring for tab strip.
    * Change the size of the favicon in tab strip from 16dpX16dp to
      14dpX14dp.
    
    Bug: 1091566
    Change-Id: Iaacd146242e0ce16dd53f732a4514a8a649f1607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236902
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777011}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/SingleTabSwitcherCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/SingleTabSwitcherCoordinator.java
@@ -40,7 +40,7 @@ class SingleTabSwitcherCoordinator implements TabSwitcher {
         container.addView(singleTabView);
         mPropertyModelChangeProcessor = PropertyModelChangeProcessor.create(
                 propertyModel, singleTabView, SingleTabViewBinder::bind);
-        mTabListFaviconProvider = new TabListFaviconProvider(activity);
+        mTabListFaviconProvider = new TabListFaviconProvider(activity, false);
         mMediator = new SingleTabSwitcherMediator(
                 propertyModel, activity.getTabModelSelector(), mTabListFaviconProvider);
 
