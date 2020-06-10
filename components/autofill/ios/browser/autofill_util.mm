d4a95ae7392044629ee653170d4624aa8875c664
Maria Kazinova <kazinova@google.com>
2020-06-10 07:41:11 +0000

    [iOS] Not setting a unique_renderer_id of a form or field if JavaScript returned an invalid value.
    Spotted a bug, that kNotSetRendererId value was treated as valid in
    Objective-C part.
    
    Bug: 1075444
    Change-Id: Id81c7057075ca4388952248d3f480b504817f1dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238166
    Reviewed-by: Vadym Doroshenko  <dvadym@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/master@{#776869}

--- components/autofill/ios/browser/autofill_util.mm
+++ components/autofill/ios/browser/autofill_util.mm
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/gurl.h"
 #include "url/origin.h"
 
+using base::NumberToString;
+using base::StringToUint;
+
 namespace {
 // The timeout for any JavaScript call in this file.
 const int64_t kJavaScriptExecutionTimeoutInSeconds = 5;
@@ -118,9 +121,9 @@ bool ExtractFormData(const base::Value& form_value,
 
   std::string unique_renderer_id;
   form_dictionary->GetString("unique_renderer_id", &unique_renderer_id);
-  if (!unique_renderer_id.empty()) {
-    base::StringToUint(unique_renderer_id,
-                       &form_data->unique_renderer_id.value());
+  if (!unique_renderer_id.empty() &&
+      unique_renderer_id != NumberToString(kNotSetRendererID)) {
+    StringToUint(unique_renderer_id, &form_data->unique_renderer_id.value());
   } else {
     form_data->unique_renderer_id = FormRendererId();
   }
@@ -164,9 +167,9 @@ bool ExtractFormFieldData(const base::DictionaryValue& field,
 
   std::string unique_renderer_id;
   field.GetString("unique_renderer_id", &unique_renderer_id);
-  if (!unique_renderer_id.empty()) {
-    base::StringToUint(unique_renderer_id,
-                       &field_data->unique_renderer_id.value());
+  if (!unique_renderer_id.empty() &&
+      unique_renderer_id != NumberToString(kNotSetRendererID)) {
+    StringToUint(unique_renderer_id, &field_data->unique_renderer_id.value());
   } else {
     field_data->unique_renderer_id = FieldRendererId();
   }
