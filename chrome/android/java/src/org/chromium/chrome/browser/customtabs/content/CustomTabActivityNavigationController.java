fbf0da7b528656cae8043d9afd6dcf3c93643477
Peter E Conn <peconn@chromium.org>
2020-06-10 07:52:43 +0000

    🤝 Finish Custom Tabs early when last tab is closed.
    Bug: 1087108
    Change-Id: Ibf1775d96ea5600a1c2d73ee31ef4db8fbe307e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228617
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776873}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityNavigationController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityNavigationController.java
@@ -96,9 +96,6 @@ public class CustomTabActivityNavigationController implements StartStopWithNativ
     private ToolbarManager mToolbarManager;
 
     @Nullable
-    private BackHandler mBackHandler;
-
-    @Nullable
     private FinishHandler mFinishHandler;
 
     private boolean mIsFinishing;
@@ -205,26 +202,19 @@ public class CustomTabActivityNavigationController implements StartStopWithNativ
             return true;
         }
 
-        if (mBackHandler != null
-                && mBackHandler.handleBackPressed(this::executeDefaultBackHandling)) {
-            return true;
+        if (mToolbarManager != null && mToolbarManager.back()) return true;
+
+        if (mTabController.onlyOneTabRemaining()) {
+            // If we're closing the last tab, just finish the Activity manually. If we had called
+            // mTabController.closeTab() and waited for the Activity to close as a result we would
+            // have a visual glitch: https://crbug.com/1087108.
+            finish(USER_NAVIGATION);
+        } else {
+            mTabController.closeTab();
         }
 
-        executeDefaultBackHandling();
         return true;
     }
-
-    private void executeDefaultBackHandling() {
-        if (mToolbarManager != null && mToolbarManager.back()) return;
-
-        // mTabController.closeTab may result in either closing the only tab (through the back
-        // button or the close button), or swapping to the previous tab. In the first case we need
-        // finish to be called with USER_NAVIGATION reason.
-        mIsHandlingUserNavigation = true;
-        mTabController.closeTab();
-        mIsHandlingUserNavigation = false;
-    }
-
     /**
      * Handles close button navigation.
      */
@@ -309,14 +299,6 @@ public class CustomTabActivityNavigationController implements StartStopWithNativ
         if (mFinishHandler != null) {
             mFinishHandler.onFinish(reason);
         }
-    }
-
-    /**
-     * See {@link BackHandler}. Only one BackHandler at a time should be set.
-     */
-    public void setBackHandler(BackHandler handler) {
-        assert mBackHandler == null : "Multiple BackHandlers not supported";
-        mBackHandler = handler;
     }
 
     /**
