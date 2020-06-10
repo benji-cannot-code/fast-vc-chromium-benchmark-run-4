ef7d645c36718a392995f894cedf47170a71d499
David Bertoni <dbertoni@chromium.org>
2020-06-10 07:43:42 +0000

    [Extensions] Add Bookmarks API test for SW-based extensions.
    This CL adds testing for Service Worked-based extensions through
    parameterized test fixtures.
    
    Bug: 1093066
    Change-Id: Ie94d9eaa00a15e3a48be91505e02b04796055b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238947
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#776871}

--- chrome/browser/extensions/api/bookmarks/bookmark_apitest.cc
+++ chrome/browser/extensions/api/bookmarks/bookmark_apitest.cc
@@ -20,12 +20,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/common/scoped_worker_based_extensions_channel.h"
 
 using bookmarks::BookmarkModel;
 
 namespace extensions {
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Bookmarks) {
+using ContextType = ExtensionApiTest::ContextType;
+
+class BookmarksApiTest : public ExtensionApiTest,
+                         public testing::WithParamInterface<ContextType> {
+ public:
+  void SetUp() override {
+    ExtensionApiTest::SetUp();
+    // Service Workers are currently only available on certain channels, so set
+    // the channel for those tests.
+    if (GetParam() == ContextType::kServiceWorker)
+      current_channel_ = std::make_unique<ScopedWorkerBasedExtensionsChannel>();
+  }
+
+ private:
+  std::unique_ptr<ScopedWorkerBasedExtensionsChannel> current_channel_;
+};
+
+INSTANTIATE_TEST_SUITE_P(EventPage,
+                         BookmarksApiTest,
+                         ::testing::Values(ContextType::kEventPage));
+INSTANTIATE_TEST_SUITE_P(ServiceWorker,
+                         BookmarksApiTest,
+                         ::testing::Values(ContextType::kServiceWorker));
+
+IN_PROC_BROWSER_TEST_P(BookmarksApiTest, Bookmarks) {
   // Add test managed bookmarks to verify that the bookmarks API can read them
   // and can't modify them.
   Profile* profile = browser()->profile();
@@ -46,7 +71,13 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Bookmarks) {
   profile->GetPrefs()->Set(bookmarks::prefs::kManagedBookmarks, list);
   ASSERT_EQ(2u, managed->managed_node()->children().size());
 
-  ASSERT_TRUE(RunExtensionTest("bookmarks")) << message_;
+  if (GetParam() == ContextType::kEventPage) {
+    ASSERT_TRUE(RunExtensionTest("bookmarks")) << message_;
+  } else {
+    ASSERT_TRUE(RunExtensionTestWithFlags(
+        "bookmarks", kFlagRunAsServiceWorkerBasedExtension, kFlagNone))
+        << message_;
+  }
 }
 
 }  // namespace extensions
