d1142677b7db14e6491351c406d4321ba4f0cd0b
Weilun Shi <sweilun@chromium.org>
2020-06-11 03:21:54 +0000

    Implement SparseHistogram's GetCountAndBucketData and GetParameters
    This cl implements GetCountAndBucketData and GetParameters function for
    the SparseHistogram class. These two functions are required in
    HistogramBase::WriteJSON.
    
    Bug: 990131
    Change-Id: I023b40ce1a25142b3142f1a97db719333098ec49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233924
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777248}

--- base/metrics/sparse_histogram.h
+++ base/metrics/sparse_histogram.h
@@ -76,10 +76,8 @@ class BASE_EXPORT SparseHistogram : public HistogramBase {
       base::PickleIterator* iter);
   static HistogramBase* DeserializeInfoImpl(base::PickleIterator* iter);
 
+  // Writes the type of the sparse histogram in the |params|.
   void GetParameters(DictionaryValue* params) const override;
-  void GetCountAndBucketData(Count* count,
-                             int64_t* sum,
-                             ListValue* buckets) const override;
 
   // Helpers for emitting Ascii graphic.  Each method appends data to output.
   void WriteAsciiBody(const HistogramSamples& snapshot,
@@ -91,7 +89,7 @@ class BASE_EXPORT SparseHistogram : public HistogramBase {
   void WriteAsciiHeader(const HistogramSamples& snapshot,
                         std::string* output) const;
 
-  // For constuctor calling.
+  // For constructor calling.
   friend class SparseHistogramTest;
 
   // Protects access to |samples_|.
