65a09b33437547600e6498477a99583dab20bf78
Xianzhu Wang <wangxianzhu@chromium.org>
2020-06-11 22:17:45 +0000

    Fix relationship among transformed rasterization, lcd text and contents opaque
    - Turned out that we can't enable LCD text with transformed
    rasterization because the latter will cause translucent pixels along the
    edges of the layer on which text may draw. This CL partly reverts
    crrev.com/c/2229243 which enabled LCD text with transformed
    rasterization.
    
    - When enabling transformed rasterization, we need to not only set
    contents_opaque to false, but also need to let RasterSource clear canvas
    before drawing because of the translucent pixels at edges.
    
    - Refactor to make contents_opaque() consistent in the main-thread and
    impl-side.
    
    Bug: 1077071
    Change-Id: Ie08d8d7bdeb46fa6dcc16560369cfd3dfedd14a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238519
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777523}

--- cc/layers/picture_layer.h
+++ cc/layers/picture_layer.h
@@ -59,6 +59,9 @@ class CC_EXPORT PictureLayer : public Layer {
   RecordingSource* GetRecordingSourceForTesting() {
     return recording_source_.get();
   }
+  bool ShouldUseTransformedRasterizationForTesting() const {
+    return ShouldUseTransformedRasterization();
+  }
 
   const DisplayItemList* GetDisplayItemList();
 
