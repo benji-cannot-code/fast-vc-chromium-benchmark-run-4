fbfd9d8963590cea323a1002457a80343f3b880b
Renjie Tang <renjietang@chromium.org>
2020-06-11 23:05:38 +0000

    Reland "Default enable QUIC in Cronet Android and IOS." This CL also patches in crrev.com/c/2194176, which is a fix to the original CL.
    This is a reland of 652185a088229532f81f9f30e41a0498694fd8e6
    
    Original change's description:
    > Default enable QUIC in Cronet Android and IOS.
    >
    > This change introduces no API changes. But API documents need to be updated.
    >
    > Change-Id: Ibc347df79066de704c5be0683c5b658ba6e1896a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2168553
    > Commit-Queue: Renjie Tang <renjietang@chromium.org>
    > Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    > Reviewed-by: Misha Efimov <mef@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#765334}
    
    Change-Id: I37f62746a35d634641febf16f29dfd1608b1e483
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2235981
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777549}

--- components/cronet/android/api/src/org/chromium/net/CronetEngine.java
+++ components/cronet/android/api/src/org/chromium/net/CronetEngine.java
@@ -65,7 +65,7 @@ public abstract class CronetEngine {
         /**
          * Constructs a {@link Builder} object that facilitates creating a
          * {@link CronetEngine}. The default configuration enables HTTP/2 and
-         * disables QUIC and the HTTP cache.
+         * QUIC, but disables the HTTP cache.
          *
          * @param context Android {@link Context}, which is used by
          *                {@link Builder} to retrieve the application
@@ -143,7 +143,7 @@ public abstract class CronetEngine {
 
         /**
          * Sets whether <a href="https://www.chromium.org/quic">QUIC</a> protocol
-         * is enabled. Defaults to disabled. If QUIC is enabled, then QUIC User Agent Id
+         * is enabled. Defaults to enabled. If QUIC is enabled, then QUIC User Agent Id
          * containing application name and Cronet version is sent to the server.
          * @param value {@code true} to enable QUIC, {@code false} to disable.
          * @return the builder to facilitate chaining.
