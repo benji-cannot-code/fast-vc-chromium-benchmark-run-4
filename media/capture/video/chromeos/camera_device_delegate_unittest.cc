25b4e0b8ce279c14f78c2c6c772c289343a0b6b8
Hirokazu Honda <hiroh@chromium.org>
2020-06-11 08:12:04 +0000

    media/capture: Use SCANOUT_VEA_READ_CAMERA_AND_CPU_READ_WRITE for capture usecase
    We have used gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE for
    camera capture use case. A content of the buffer is written by
    the camera service, rendered by GPU and also might be read by
    a software encoder and a hardware-accelerated video encoder.
    
    A hardware-accelerated video encoder requires their specific
    alignment on an input buffer. The above buffer must satisfy the
    alignment condition if the input buffer is encoded by the
    hardware-accelerated video encoder, which is the Zero-Copy use
    scenario (go/cros-vide0Capture). The buffer must be allocated
    with HW_VIDEO_ENCODER mask to request a buffer allocation stack
    (e.g. minigbm) the encoder's alignment.
    
    SCANOUT_CAMERA_READ_WRITE usage doesn't contain HW_VIDEO_ENCODER
    mask. This CL replaces it in capture stack with
    SCANOUT_VEA_READ_CAMERA_AND_CPU_READ_WRITE. It is equivalent to
    SCANOUT_CAMERA_READ_WRITE + HW_VIDEO_ENCODER mask.
    
    Bug: 982201
    Test: tast run camera.* on krane
    Test: Camera preview and recorded video look good with CCA on krane
    Change-Id: If45d4a44aaead8de497571274f14563a89415077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2234684
    Reviewed-by: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777287}

--- media/capture/video/chromeos/camera_device_delegate_unittest.cc
+++ media/capture/video/chromeos/camera_device_delegate_unittest.cc
@@ -375,9 +375,10 @@ class CameraDeviceDelegateTest : public ::testing::Test {
             Invoke(this, &CameraDeviceDelegateTest::ConfigureFakeStreams));
     EXPECT_CALL(
         mock_gpu_memory_buffer_manager_,
-        CreateGpuMemoryBuffer(_, gfx::BufferFormat::YUV_420_BIPLANAR,
-                              gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE,
-                              gpu::kNullSurfaceHandle))
+        CreateGpuMemoryBuffer(
+            _, gfx::BufferFormat::YUV_420_BIPLANAR,
+            gfx::BufferUsage::SCANOUT_VEA_READ_CAMERA_AND_CPU_READ_WRITE,
+            gpu::kNullSurfaceHandle))
         .Times(1)
         .WillOnce(Invoke(&unittest_internal::MockGpuMemoryBufferManager::
                              CreateFakeGpuMemoryBuffer));
@@ -391,10 +392,11 @@ class CameraDeviceDelegateTest : public ::testing::Test {
                              CreateFakeGpuMemoryBuffer));
     EXPECT_CALL(
         mock_gpu_memory_buffer_manager_,
-        CreateGpuMemoryBuffer(gfx::Size(kDefaultWidth, kDefaultHeight),
-                              gfx::BufferFormat::YUV_420_BIPLANAR,
-                              gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE,
-                              gpu::kNullSurfaceHandle))
+        CreateGpuMemoryBuffer(
+            gfx::Size(kDefaultWidth, kDefaultHeight),
+            gfx::BufferFormat::YUV_420_BIPLANAR,
+            gfx::BufferUsage::SCANOUT_VEA_READ_CAMERA_AND_CPU_READ_WRITE,
+            gpu::kNullSurfaceHandle))
         .Times(1)
         .WillOnce(Invoke(&unittest_internal::MockGpuMemoryBufferManager::
                              CreateFakeGpuMemoryBuffer));
