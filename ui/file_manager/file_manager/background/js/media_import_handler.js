7e3a699556960f87dac74bc3876cd627712faef5
Omid Tourzan <oto@chromium.org>
2020-06-11 03:13:38 +0000

    [files-progress] Add speedometer to drive sync and media importer.
    The buffer length set to 30 as they operate on external storages and
    there are more chance of fluctuations.
    
    Add a rate limiter to progress update for drive sync as it updates
    foreground continuously.
    
    Disable eslint no-var rule as var can't change to let/const in
    namespace redeclaration like here.
    
    Bug: 953308
    Change-Id: I6aff7e5467ff68cdc016d00c090d2d31b17ba1bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2237489
    Commit-Queue: Omid Tourzan <oto@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777246}

--- ui/file_manager/file_manager/background/js/media_import_handler.js
+++ ui/file_manager/file_manager/background/js/media_import_handler.js
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // Namespace
-var importer = importer || {};
+window.importer = window.importer || {};
 
 importer.MediaImportHandler = importer.MediaImportHandler || {};
 importer.MediaImportHandler.ImportTask =
@@ -47,6 +47,20 @@ importer.MediaImportHandlerImpl = class {
 
     /** @private {!importer.DispositionChecker.CheckerFunction} */
     this.getDisposition_ = dispositionChecker;
+
+    /**
+     * Number of progress item sequence used in calculating moving average
+     * speed of task.
+     * @private {number}
+     */
+    this.SPEED_BUFFER_WINDOW_ = 30;
+
+    /**
+     * Speedometer object used to calculate and track speed and remaining time.
+     * @protected {fileOperationUtil.Speedometer}
+     */
+    this.speedometer_ =
+        new fileOperationUtil.Speedometer(this.SPEED_BUFFER_WINDOW_);
 
     /** @private {!DriveSyncHandler} */
     this.driveSyncHandler_ = driveSyncHandler;
@@ -100,6 +114,7 @@ importer.MediaImportHandlerImpl = class {
       // TODO(kenobi): Might need a different progress item type here.
       item.type = ProgressItemType.COPY;
       item.progressMax = task.totalBytes;
+      this.speedometer_.setTotalBytes(item.progressMax);
       item.cancelCallback = () => {
         task.requestCancel();
       };
@@ -111,6 +126,10 @@ importer.MediaImportHandlerImpl = class {
             strf('CLOUD_IMPORT_ITEMS_REMAINING', task.remainingFilesCount);
         item.progressValue = task.processedBytes;
         item.state = ProgressItemState.PROGRESSING;
+        this.speedometer_.update(item.progressValue);
+        item.currentSpeed = this.speedometer_.getCurrentSpeed();
+        item.averageSpeed = this.speedometer_.getAverageSpeed();
+        item.remainingTime = this.speedometer_.getRemainingTime();
         break;
 
       case UpdateType.COMPLETE:
