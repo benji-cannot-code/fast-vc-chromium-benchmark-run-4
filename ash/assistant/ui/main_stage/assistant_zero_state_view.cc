3685e5cfb2fdeb9f79bd0d917cde7a5277fe289b
David Black <dmblack@google.com>
2020-06-11 21:05:37 +0000

    Don't show conversation starters if showing onboarding.
    Bug: b:157689497
    Change-Id: Ib69db89261ff4751d8722463060f21f24312f2e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240306
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777484}

--- ash/assistant/ui/main_stage/assistant_zero_state_view.cc
+++ ash/assistant/ui/main_stage/assistant_zero_state_view.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/ui/assistant_ui_constants.h"
 #include "ash/assistant/ui/assistant_view_ids.h"
 #include "ash/assistant/ui/main_stage/assistant_onboarding_view.h"
+#include "ash/assistant/util/assistant_util.h"
 #include "ash/public/cpp/assistant/controller/assistant_ui_controller.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "chromeos/services/assistant/public/cpp/features.h"
@@ -97,12 +98,11 @@ void AssistantZeroStateView::InitLayout() {
       l10n_util::GetStringUTF16(IDS_ASH_ASSISTANT_PROMPT_DEFAULT));
 }
 
-// TODO(dmblack): Update conditions under which onboarding view is shown.
 void AssistantZeroStateView::UpdateLayout() {
   if (!IsBetterOnboardingEnabled())
     return;
 
-  constexpr bool show_onboarding = true;
+  const bool show_onboarding = assistant::util::ShouldShowOnboarding();
   onboarding_view_->SetVisible(show_onboarding);
   greeting_label_->SetVisible(!show_onboarding);
 }
--- ash/assistant/util/assistant_util.cc
+++ ash/assistant/util/assistant_util.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/model/assistant_ui_model.h"
 #include "base/strings/string_util.h"
 #include "chromeos/constants/devicetype.h"
+#include "chromeos/services/assistant/public/cpp/features.h"
 
 namespace {
 
@@ -68,6 +69,10 @@ bool ShouldAttemptWarmerWelcome(AssistantEntryPoint entry_point) {
     case AssistantEntryPoint::kSetup:
       return true;
   }
+}
+
+bool ShouldShowOnboarding() {
+  return chromeos::assistant::features::IsBetterOnboardingEnabled();
 }
 
 bool IsGoogleDevice() {
