289e8142aa8622c095be1b8780c277dc7183909e
Ramin Halavati <rhalavati@chromium.org>
2020-06-11 10:52:37 +0000

    Revert "Remove two Autofill OffTheRecord metrics."
    This reverts commit 030a0e3eb0ddaafba352f84feb47e177bd6525a5.
    
    Reason for revert: Removed metrics are needed for other studies.
    
    Original change's description:
    > Remove two Autofill OffTheRecord metrics.
    > 
    > Removes the following expired metrics and cleans up:
    >  - Autofill.SuggestionShown.OffTheRecord
    >  - Autofill.SuggestionAccepted.OffTheRecord
    > 
    > Bug: 1088632, 1088634
    > Change-Id: Ic5e0d5e821b6de743d769e6828d0828511142499
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2228549
    > Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#774971}
    
    TBR=holte@chromium.org,cfroussios@chromium.org,rhalavati@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1088632, 1088634
    Change-Id: Ia5a72d7161990c5457a20d99c19ce424fcabe128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236476
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777308}

--- components/autofill/core/browser/autofill_metrics.cc
+++ components/autofill/core/browser/autofill_metrics.cc
@@ -1777,6 +1777,9 @@ void AutofillMetrics::LogAutofillSuggestionAcceptedIndex(int index,
   }
 
   base::RecordAction(base::UserMetricsAction("Autofill_SelectedSuggestion"));
+
+  base::UmaHistogramBoolean("Autofill.SuggestionAccepted.OffTheRecord",
+                            off_the_record);
 }
 
 // static
@@ -2046,7 +2049,8 @@ void AutofillMetrics::FormInteractionsUkmLogger::LogInteractedWithForm(
 void AutofillMetrics::FormInteractionsUkmLogger::LogSuggestionsShown(
     const FormStructure& form,
     const AutofillField& field,
-    const base::TimeTicks& form_parsed_timestamp) {
+    const base::TimeTicks& form_parsed_timestamp,
+    bool off_the_record) {
   if (!CanLog())
     return;
 
@@ -2059,6 +2063,9 @@ void AutofillMetrics::FormInteractionsUkmLogger::LogSuggestionsShown(
       .SetMillisecondsSinceFormParsed(
           MillisecondsSinceFormParsed(form_parsed_timestamp))
       .Record(ukm_recorder_);
+
+  base::UmaHistogramBoolean("Autofill.SuggestionShown.OffTheRecord",
+                            off_the_record);
 }
 
 void AutofillMetrics::FormInteractionsUkmLogger::LogDidFillSuggestion(
