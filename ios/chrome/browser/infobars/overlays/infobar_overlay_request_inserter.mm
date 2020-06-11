cfcd2251c9eb71f94157760f5fcb30c7856adece
Chris Lu <thegreenfrog@chromium.org>
2020-06-11 18:49:13 +0000

    [ios] Add modal placeholder to banner queue when presenting modal.
    This change will prevent banners from being presented behind a modal.
    
    Bug: 1071914
    Change-Id: I22b3635b42acd4a145542978b6440e741318d9e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233700
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777419}

--- ios/chrome/browser/infobars/overlays/infobar_overlay_request_inserter.mm
+++ ios/chrome/browser/infobars/overlays/infobar_overlay_request_inserter.mm
@@ -10,9 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/infobars/infobar_ios.h"
 #import "ios/chrome/browser/infobars/overlays/infobar_banner_overlay_request_cancel_handler.h"
 #import "ios/chrome/browser/infobars/overlays/infobar_modal_completion_notifier.h"
+#import "ios/chrome/browser/infobars/overlays/infobar_modal_overlay_request_cancel_handler.h"
 #import "ios/chrome/browser/infobars/overlays/infobar_overlay_request_cancel_handler.h"
 #import "ios/chrome/browser/infobars/overlays/infobar_overlay_request_factory.h"
 #import "ios/chrome/browser/overlays/public/common/infobars/infobar_overlay_request_config.h"
+#import "ios/chrome/browser/overlays/public/infobar_banner/infobar_banner_placeholder_request_config.h"
 #include "ios/chrome/browser/overlays/public/overlay_modality.h"
 #include "ios/chrome/browser/overlays/public/overlay_request.h"
 #import "ios/chrome/browser/overlays/public/overlay_request_queue.h"
@@ -92,8 +94,22 @@ void InfobarOverlayRequestInserter::InsertOverlayRequest(
       break;
     case InfobarOverlayType::kDetailSheet:
     case InfobarOverlayType::kModal:
+      // Add placeholder request in front of banner queue so no banner get
+      // presented behind the modal.
       cancel_handler = std::make_unique<InfobarOverlayRequestCancelHandler>(
           request.get(), queue, infobar_ios);
+      OverlayRequestQueue* banner_queue =
+          queues_.at(InfobarOverlayType::kBanner);
+      std::unique_ptr<OverlayRequest> request =
+          OverlayRequest::CreateWithConfig<
+              InfobarBannerPlaceholderRequestConfig>(infobar_ios);
+      std::unique_ptr<InfobarModalOverlayRequestCancelHandler>
+          modal_cancel_handler =
+              std::make_unique<InfobarModalOverlayRequestCancelHandler>(
+                  request.get(), banner_queue, infobar_ios,
+                  modal_completion_notifier_.get());
+      banner_queue->InsertRequest(0, std::move(request),
+                                  std::move(modal_cancel_handler));
       break;
   }
   for (auto& observer : observers_) {
