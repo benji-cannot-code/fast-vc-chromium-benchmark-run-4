e71015554f2dc9a82cc24027a973dd725012e131
Ana Salazar <anasalazar@chromium.org>
2020-06-11 14:44:40 +0000

    Add chrome://flags#enable-launcher-app-paging
    This change adds a flag to enable the new launcher app drag redesign.
    Design doc:
    https://docs.google.com/document/d/1klNglz8LFuCoHQDHo43SQaBMxnvCRSDAl_BakCSpVto/edit
    
    Bug: 996822:
    Change-Id: I890a35a579b5ce83ef96d2027a0091c503391793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240030
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777343}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2067,6 +2067,9 @@ extern const char kEnableHeuristicStylusPalmRejectionDescription[];
 extern const char kEnableHighResolutionMouseScrollingName[];
 extern const char kEnableHighResolutionMouseScrollingDescription[];
 
+extern const char kNewDragSpecInLauncherName[];
+extern const char kNewDragSpecInLauncherDescription[];
+
 extern const char kEnableNeuralStylusPalmRejectionName[];
 extern const char kEnableNeuralStylusPalmRejectionDescription[];
 
