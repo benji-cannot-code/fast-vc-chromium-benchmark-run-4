7e2804a371b008e83cdd68ab5d64d1f80fc833c9
Mei Liang <meiliang@chromium.org>
2020-06-11 22:28:40 +0000

    Deflake garbage collection test in TabSelectionEditorTest
    The flaky test,
    TabSelectionEditorTest.testTabSelectionEditorLayoutCanBeGarbageCollected
    is reproduced locally, and noticed a longer timeout is
    needed. This CL extends the time out by using the
    CriteriaHelper.pollUiThread with
    GarbageCollectionTestUtils.canBeGarbageCollected.
    
    Change-Id: Id82312493e52601609ca4cbbd63bd68637f45373
    Bug: 1075816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241345
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777526}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSelectionEditorTest.java
@@ -11,7 +11,6 @@ import android.support.test.filters.MediumTest;
 import android.view.ViewGroup;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,7 +22,6 @@ import org.chromium.base.GarbageCollectionTestUtils;
 import org.chromium.base.SysUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -467,7 +465,6 @@ public class TabSelectionEditorTest {
     @Test
     @MediumTest
     @DisableFeatures({ChromeFeatureList.TAB_GROUPS_ANDROID})
-    @DisabledTest(message = "crbug.com/1075816")
     public void testTabSelectionEditorLayoutCanBeGarbageCollected() {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             mTabSelectionEditorCoordinator.destroy();
@@ -477,7 +474,9 @@ public class TabSelectionEditorTest {
         });
 
         InstrumentationRegistry.getInstrumentation().waitForIdleSync();
-        Assert.assertTrue(GarbageCollectionTestUtils.canBeGarbageCollected(mRef));
+
+        // A longer timeout is needed. Achieve that by using the CriteriaHelper.pollUiThread.
+        CriteriaHelper.pollUiThread(() -> GarbageCollectionTestUtils.canBeGarbageCollected(mRef));
     }
 
     private List<Tab> getTabsInCurrentTabModel() {
