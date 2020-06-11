30379223dad8344adf22e70d090e3f86e6d677d9
Scott Violet <sky@chromium.org>
2020-06-11 22:15:05 +0000

    webview: changes how AwOriginMatcher is serialized
    This makes the format better match the internal representation.
    There are now two distinct types:
    . a type for matching anything.
    . a type for matching the scheme, host and port.
    
    BUG=1042771
    TEST=AwOriginMatcherTest*
    
    Change-Id: I15beac188beab06a977d25b7a58cec6491f0025a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240066
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Shimi Zhang <ctzsm@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777520}

--- components/js_injection/common/aw_origin_matcher.h
+++ components/js_injection/common/aw_origin_matcher.h
@@ -6,16 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_JS_INJECTION_COMMON_AW_ORIGIN_MATCHER_H_
 #define COMPONENTS_JS_INJECTION_COMMON_AW_ORIGIN_MATCHER_H_
 
+#include <memory>
 #include <string>
-
-#include "net/base/scheme_host_port_matcher.h"
-#include "url/origin.h"
+#include <vector>
 
 namespace url {
 class Origin;
 }  // namespace url
 
 namespace js_injection {
+
+class OriginMatcherRule;
 
 // An url origin matcher allows wildcard subdomain matching. It supports two
 // types of rules.
@@ -40,6 +41,8 @@ namespace js_injection {
 // correspondingly).
 class AwOriginMatcher {
  public:
+  using RuleList = std::vector<std::unique_ptr<OriginMatcherRule>>;
+
   AwOriginMatcher() = default;
   // Allow copy and assign.
   AwOriginMatcher(const AwOriginMatcher& rhs);
@@ -49,6 +52,8 @@ class AwOriginMatcher {
 
   ~AwOriginMatcher() = default;
 
+  void SetRules(RuleList rules);
+
   // Adds a rule given by the string |raw|. Returns true if the rule was
   // successfully added.
   bool AddRuleFromString(const std::string& raw);
@@ -57,13 +62,13 @@ class AwOriginMatcher {
   bool Matches(const url::Origin& origin) const;
 
   // Returns the current list of rules.
-  const net::SchemeHostPortMatcher::RuleList& rules() const { return rules_; }
+  const RuleList& rules() const { return rules_; }
 
   // Returns string representation of this origin matcher.
   std::vector<std::string> Serialize() const;
 
  private:
-  net::SchemeHostPortMatcher::RuleList rules_;
+  RuleList rules_;
 };
 
 }  // namespace js_injection
