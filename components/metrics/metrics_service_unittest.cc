36272d995c66ce295bf99c5a612e2356a1228425
Weilun Shi <sweilun@chromium.org>
2020-06-11 20:32:32 +0000

    Use LogStoreForTest instead of the protected log_store() in test
    Removed unused log_manager() function in MetricsService class and use
    LogStoreForTest instead of the protected log_store() in the
    MetricsServiceTest.
    
    Bug: 1034679
    Change-Id: Ib5b3bd252fc867f0602a790d19bdec28103941be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238343
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Weilun Shi <sweilun@chromium.org>
    Auto-Submit: Weilun Shi <sweilun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777473}

--- components/metrics/metrics_service_unittest.cc
+++ components/metrics/metrics_service_unittest.cc
@@ -78,8 +78,6 @@ class TestMetricsService : public MetricsService {
       : MetricsService(state_manager, client, local_state) {}
   ~TestMetricsService() override = default;
 
-  using MetricsService::log_manager;
-  using MetricsService::log_store;
   using MetricsService::RecordCurrentEnvironmentHelper;
 
   // MetricsService:
@@ -278,9 +276,9 @@ TEST_F(MetricsServiceTest, InitialStabilityLogAtProviderRequest) {
   service.InitializeMetricsRecordingState();
 
   // The initial stability log should be generated and persisted in unsent logs.
-  MetricsLogStore* log_store = service.log_store();
-  EXPECT_TRUE(log_store->has_unsent_logs());
-  EXPECT_FALSE(log_store->has_staged_log());
+  MetricsLogStore* test_log_store = service.LogStoreForTest();
+  EXPECT_TRUE(test_log_store->has_unsent_logs());
+  EXPECT_FALSE(test_log_store->has_staged_log());
 
   // Ensure that HasPreviousSessionData() is always called on providers,
   // for consistency, even if other conditions already indicate their presence.
@@ -292,11 +290,11 @@ TEST_F(MetricsServiceTest, InitialStabilityLogAtProviderRequest) {
   EXPECT_TRUE(test_provider->provide_stability_metrics_called());
 
   // Stage the log and retrieve it.
-  log_store->StageNextLog();
-  EXPECT_TRUE(log_store->has_staged_log());
+  test_log_store->StageNextLog();
+  EXPECT_TRUE(test_log_store->has_staged_log());
 
   ChromeUserMetricsExtension uma_log;
-  EXPECT_TRUE(DecodeLogDataToProto(log_store->staged_log(), &uma_log));
+  EXPECT_TRUE(DecodeLogDataToProto(test_log_store->staged_log(), &uma_log));
 
   EXPECT_TRUE(uma_log.has_client_id());
   EXPECT_TRUE(uma_log.has_session_id());
@@ -341,9 +339,9 @@ TEST_F(MetricsServiceTest, InitialStabilityLogAfterCrash) {
   service.InitializeMetricsRecordingState();
 
   // The initial stability log should be generated and persisted in unsent logs.
-  MetricsLogStore* log_store = service.log_store();
-  EXPECT_TRUE(log_store->has_unsent_logs());
-  EXPECT_FALSE(log_store->has_staged_log());
+  MetricsLogStore* test_log_store = service.LogStoreForTest();
+  EXPECT_TRUE(test_log_store->has_unsent_logs());
+  EXPECT_FALSE(test_log_store->has_staged_log());
 
   // Ensure that HasPreviousSessionData() is always called on providers,
   // for consistency, even if other conditions already indicate their presence.
@@ -355,11 +353,11 @@ TEST_F(MetricsServiceTest, InitialStabilityLogAfterCrash) {
   EXPECT_TRUE(test_provider->provide_stability_metrics_called());
 
   // Stage the log and retrieve it.
-  log_store->StageNextLog();
-  EXPECT_TRUE(log_store->has_staged_log());
+  test_log_store->StageNextLog();
+  EXPECT_TRUE(test_log_store->has_staged_log());
 
   ChromeUserMetricsExtension uma_log;
-  EXPECT_TRUE(DecodeLogDataToProto(log_store->staged_log(), &uma_log));
+  EXPECT_TRUE(DecodeLogDataToProto(test_log_store->staged_log(), &uma_log));
 
   EXPECT_TRUE(uma_log.has_client_id());
   EXPECT_TRUE(uma_log.has_session_id());
