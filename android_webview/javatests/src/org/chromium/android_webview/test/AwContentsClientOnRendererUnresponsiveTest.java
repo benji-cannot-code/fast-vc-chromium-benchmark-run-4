e8eccbf4b7754a77f9128ccfc2f752feb1c95220
Torne (Richard Coles) <torne@google.com>
2020-06-11 22:34:20 +0000

    Deflake AwContentsClientOnRendererUnresponsiveTest.
    The test was flaking because sometimes the renderer was not evaluating
    the JS code used to block the blink thread until *after* we had already
    sent the input event to the frame, and thus the hang detection never
    kicked in.
    
    Add explicit synchronization in JSBlocker so that the test thread can
    wait until the blink thread is actually blocked before continuing.
    
    Fixed: 1092707
    Change-Id: Ib993b32ffc06b49c8015b6c248477ee91079c575
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238866
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Auto-Submit: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777531}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientOnRendererUnresponsiveTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientOnRendererUnresponsiveTest.java
@@ -21,7 +21,6 @@ import org.chromium.android_webview.AwRenderProcess;
 import org.chromium.android_webview.AwRenderProcessGoneDetail;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.test.util.CallbackHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.UiThreadTaskTraits;
 import org.chromium.content_public.common.ContentUrlConstants;
@@ -40,18 +39,30 @@ public class AwContentsClientOnRendererUnresponsiveTest {
     private static final String TAG = "AwRendererUnresponsive";
 
     private static class JSBlocker {
-        private CountDownLatch mLatch;
+        // The Blink thread waits on this in block(),
+        // until the test thread calls releaseBlock().
+        private CountDownLatch mBlockingLatch;
+        // The test thread waits on this in waitUntilBlocked(),
+        // until the Blink thread calls block().
+        private CountDownLatch mThreadWasBlockedLatch;
+
         JSBlocker() {
-            mLatch = new CountDownLatch(1);
+            mBlockingLatch = new CountDownLatch(1);
+            mThreadWasBlockedLatch = new CountDownLatch(1);
         }
 
         public void releaseBlock() {
-            mLatch.countDown();
+            mBlockingLatch.countDown();
         }
 
         @JavascriptInterface
         public void block() throws Exception {
-            mLatch.await(AwActivityTestRule.WAIT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+            mThreadWasBlockedLatch.countDown();
+            mBlockingLatch.await(AwActivityTestRule.WAIT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        }
+
+        public void waitUntilBlocked() throws Exception {
+            mThreadWasBlockedLatch.await(AwActivityTestRule.WAIT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
         }
     }
 
@@ -67,9 +78,10 @@ public class AwContentsClientOnRendererUnresponsiveTest {
             mBlocker = new JSBlocker();
         }
 
-        void transientlyBlockBlinkThread(final AwContents awContents) {
+        void transientlyBlockBlinkThread(final AwContents awContents) throws Exception {
             PostTask.runOrPostTask(UiThreadTaskTraits.DEFAULT,
                     () -> { awContents.evaluateJavaScript("blocker.block();", null); });
+            mBlocker.waitUntilBlocked();
         }
 
         void awaitRecovery() throws Exception {
@@ -106,15 +118,18 @@ public class AwContentsClientOnRendererUnresponsiveTest {
 
         private CallbackHelper mUnresponsiveCallbackHelper;
         private CallbackHelper mTerminatedCallbackHelper;
+        private JSBlocker mBlocker;
 
         public RendererUnresponsiveTestAwContentsClient() {
             mUnresponsiveCallbackHelper = new CallbackHelper();
             mTerminatedCallbackHelper = new CallbackHelper();
+            mBlocker = new JSBlocker();
         }
 
-        void permanentlyBlockBlinkThread(final AwContents awContents) {
+        void permanentlyBlockBlinkThread(final AwContents awContents) throws Exception {
             PostTask.runOrPostTask(UiThreadTaskTraits.DEFAULT,
                     () -> { awContents.evaluateJavaScript("blocker.block();", null); });
+            mBlocker.waitUntilBlocked();
         }
 
         void awaitRendererTermination() throws Exception {
@@ -126,6 +141,10 @@ public class AwContentsClientOnRendererUnresponsiveTest {
             mTerminatedCallbackHelper.waitForCallback(
                     0, 1, AwActivityTestRule.WAIT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
             Assert.assertEquals(1, mTerminatedCallbackHelper.getCallCount());
+        }
+
+        JSBlocker getBlocker() {
+            return mBlocker;
         }
 
         @Override
@@ -159,7 +178,6 @@ public class AwContentsClientOnRendererUnresponsiveTest {
     @Feature({"AndroidWebView"})
     @LargeTest
     @OnlyRunIn(MULTI_PROCESS)
-    @DisabledTest(message = "crbug.com/1093270")
     public void testOnRendererUnresponsive() throws Throwable {
         RendererUnresponsiveTestAwContentsClient contentsClient =
                 new RendererUnresponsiveTestAwContentsClient();
@@ -168,7 +186,7 @@ public class AwContentsClientOnRendererUnresponsiveTest {
         final AwContents awContents = testView.getAwContents();
 
         AwActivityTestRule.enableJavaScriptOnUiThread(awContents);
-        addJsBlockerInterface(awContents, new JSBlocker());
+        addJsBlockerInterface(awContents, contentsClient.getBlocker());
         mActivityTestRule.loadUrlSync(awContents, contentsClient.getOnPageFinishedHelper(),
                 ContentUrlConstants.ABOUT_BLANK_DISPLAY_URL);
 
@@ -182,7 +200,6 @@ public class AwContentsClientOnRendererUnresponsiveTest {
     @Test
     @Feature({"AndroidWebView"})
     @LargeTest
-    @DisabledTest(message = "crbug.com/1093270")
     public void testTransientUnresponsiveness() throws Throwable {
         RendererTransientlyUnresponsiveTestAwContentsClient contentsClient =
                 new RendererTransientlyUnresponsiveTestAwContentsClient();
