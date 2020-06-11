8e1f0f246a1838aeed2445d0bceb538ad4c503d2
Ramin Halavati <rhalavati@chromium.org>
2020-06-11 04:59:07 +0000

    Update OffTheRecord profile API usage in AccessibilityManager.
    Profile API is being updated (issue 1033903) and the usage of
    GetOffTheRecordProfile and HasOffTheRecordProfile without argument is
    getting deprecated. Use cases of this call that ONLY refer to incognito
    or guest mode can use [Get/Has]PrimaryOTRProfile() instead.
    
    Use cases of these functions in AccessibilityManager are updated to use
    the new APIs.
    
    This CL does not change any behavior.
    
    Bug: 1060940
    Change-Id: I51877f1b5d4e8d520b640ef91134a3291dc59bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239845
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777262}

--- chrome/browser/chromeos/accessibility/accessibility_manager.cc
+++ chrome/browser/chromeos/accessibility/accessibility_manager.cc
@@ -243,8 +243,8 @@ class ChromeVoxDeferredLoader : public content::UtteranceEventDelegate,
 
   void SendWarmupUtterance() {
     Profile* profile = manager_->profile();
-    if (profile->HasOffTheRecordProfile())
-      profile = profile->GetOffTheRecordProfile();
+    if (profile->HasPrimaryOTRProfile())
+      profile = profile->GetPrimaryOTRProfile();
     std::unique_ptr<content::TtsUtterance> utterance =
         content::TtsUtterance::Create(profile);
     utterance->SetEventDelegate(this);
@@ -256,8 +256,8 @@ class ChromeVoxDeferredLoader : public content::UtteranceEventDelegate,
 
   bool IsGoogleTtsVoiceAvailable() const {
     Profile* profile = manager_->profile();
-    if (profile->HasOffTheRecordProfile())
-      profile = profile->GetOffTheRecordProfile();
+    if (profile->HasPrimaryOTRProfile())
+      profile = profile->GetPrimaryOTRProfile();
     std::vector<content::VoiceData> voices;
     content::TtsController::GetInstance()->GetVoices(profile, &voices);
     return std::any_of(voices.begin(), voices.end(), [](const auto& voice) {
