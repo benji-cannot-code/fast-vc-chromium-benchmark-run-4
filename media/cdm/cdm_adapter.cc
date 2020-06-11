5f9a4fb8100bb97e7801282c7494559b0c119cda
Xiaohan Wang <xhwang@chromium.org>
2020-06-11 20:15:14 +0000

    media: Provide verbose option to DecoderBuffer::AsHumanReadableString()
    Currently DecoderBuffer::AsHumanReadableString() prints all fields by
    default which is not necessary in most debugging scenarios. This CL
    adds a verbose option so we can control what to display. By default
    verbose is false.
    
    Change-Id: If0478b73e9a08b23323a2a5f282f61d7d7a9fb70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229590
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777465}

--- media/cdm/cdm_adapter.cc
+++ media/cdm/cdm_adapter.cc
@@ -439,7 +439,8 @@ void CdmAdapter::RegisterNewKeyCB(StreamType stream_type,
 void CdmAdapter::Decrypt(StreamType stream_type,
                          scoped_refptr<DecoderBuffer> encrypted,
                          DecryptCB decrypt_cb) {
-  DVLOG(3) << __func__ << ": " << encrypted->AsHumanReadableString();
+  DVLOG(3) << __func__ << ": "
+           << encrypted->AsHumanReadableString(/*verbose=*/true);
   DCHECK(task_runner_->BelongsToCurrentThread());
 
   ScopedCrashKeyString scoped_crash_key(&g_origin_crash_key, cdm_origin_);
@@ -558,7 +559,8 @@ void CdmAdapter::InitializeVideoDecoder(const VideoDecoderConfig& config,
 
 void CdmAdapter::DecryptAndDecodeAudio(scoped_refptr<DecoderBuffer> encrypted,
                                        const AudioDecodeCB& audio_decode_cb) {
-  DVLOG(3) << __func__ << ": " << encrypted->AsHumanReadableString();
+  DVLOG(3) << __func__ << ": "
+           << encrypted->AsHumanReadableString(/*verbose=*/true);
   DCHECK(task_runner_->BelongsToCurrentThread());
 
   ScopedCrashKeyString scoped_crash_key(&g_origin_crash_key, cdm_origin_);
@@ -596,7 +598,8 @@ void CdmAdapter::DecryptAndDecodeAudio(scoped_refptr<DecoderBuffer> encrypted,
 
 void CdmAdapter::DecryptAndDecodeVideo(scoped_refptr<DecoderBuffer> encrypted,
                                        const VideoDecodeCB& video_decode_cb) {
-  DVLOG(3) << __func__ << ": " << encrypted->AsHumanReadableString();
+  DVLOG(3) << __func__ << ": "
+           << encrypted->AsHumanReadableString(/*verbose=*/true);
   DCHECK(task_runner_->BelongsToCurrentThread());
 
   ScopedCrashKeyString scoped_crash_key(&g_origin_crash_key, cdm_origin_);
