e5ab2763c7a5c110e34bfe38f522681739301286
Koji Ishii <kojii@chromium.org>
2020-06-11 18:25:26 +0000

    Update baseline after simplified layout
    This patch changes |NGSimplifiedLayoutAlgorithm| to update
    the inline block baseline when needed.
    
    |NGSimplifiedLayoutAlgorithm| can be used when the block
    height is changed. In that case, and if the inline block
    baselins should be computed from the logical bottom margin
    edge, the inline block baseline needs to be re-computed.
    
    Bug: 1088340
    Change-Id: Ia3c88776b7633b35790c05be210fd28dc759fd10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2237442
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777408}

--- third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.cc
+++ third_party/blink/renderer/core/layout/ng/ng_box_fragment_builder.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/ng_break_token.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_fragmentation_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_layout_result.h"
+#include "third_party/blink/renderer/core/layout/ng/ng_length_utils.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_positioned_float.h"
 #include "third_party/blink/renderer/core/paint/ng/ng_paint_fragment.h"
@@ -451,6 +452,20 @@ void NGBoxFragmentBuilder::ComputeInlineContainerGeometry(
                                             inline_containing_block_map,
                                             &containing_linebox_map);
   }
+}
+
+void NGBoxFragmentBuilder::SetLastBaselineToBlockEndMarginEdgeIfNeeded() {
+  if (ConstraintSpace()->BaselineAlgorithmType() !=
+      NGBaselineAlgorithmType::kInlineBlock)
+    return;
+
+  if (!node_.UseBlockEndMarginEdgeForInlineBlockBaseline())
+    return;
+
+  // When overflow is present (within an atomic-inline baseline context) we
+  // should always use the block-end margin edge as the baseline.
+  NGBoxStrut margins = ComputeMarginsForSelf(*ConstraintSpace(), Style());
+  SetLastBaseline(BlockSize() + margins.block_end);
 }
 
 #if DCHECK_IS_ON()
