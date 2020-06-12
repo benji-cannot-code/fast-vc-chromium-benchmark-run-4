5be1e51e45b9ff720fba5c286a83f89cf612af11
My Nguyen <myy@chromium.org>
2020-06-12 03:47:07 +0000

    Show emoji suggestion regardless of the word casing
    Currently emoji suggestion only shows up when the word is lowercase.
    Update the code to show it regardless of casing.
    
    Bug: 1087213
    Change-Id: Ia4debbbbd35ccd3c9d909938ae52a4793059695f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2217678
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777668}

--- chrome/browser/chromeos/input_method/emoji_suggester_unittest.cc
+++ chrome/browser/chromeos/input_method/emoji_suggester_unittest.cc
@@ -37,6 +37,10 @@ TEST_F(EmojiSuggesterTest, SuggestWhenStringEndsWithSpace) {
   EXPECT_TRUE(emoji_suggester_->Suggest(base::UTF8ToUTF16("happy ")));
 }
 
+TEST_F(EmojiSuggesterTest, SuggestWhenStringEndsWithSpaceAndIsUppercase) {
+  EXPECT_TRUE(emoji_suggester_->Suggest(base::UTF8ToUTF16("HAPPY ")));
+}
+
 TEST_F(EmojiSuggesterTest, DoNotSuggestWhenStringEndsWithNewLine) {
   EXPECT_FALSE(emoji_suggester_->Suggest(base::UTF8ToUTF16("happy\n")));
 }
