5f5c2c9071922d7ef2c4217344e99875d8619042
Alison Maher <almaher@microsoft.com>
2020-06-12 17:30:28 +0000

    Remove use of PreferredColorScheme::kNoPreference in Forced Colors Mode
    The 'no-preference' value was removed from the spec. As such, remove
    the use of PreferredColorScheme::kNoPreference when in Forced Colors
    Mode. This is a follow-up to https://crrev.com/c/2232922
    
    The algorithm for calculating the PreferredColorScheme on Windows now
    more closely matches the spec expectation (ie. 'dark' if Canvas L<33%,
    otherwise 'light'). https://www.w3.org/TR/css-color-adjust-1/#forced
    
    Bug: 970285
    Change-Id: Ia41e3f92a582252ae7430ffa2f2772a818eabfce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240037
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#777861}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3455,7 +3455,6 @@ void ChromeContentBrowserClient::OverrideWebkitPrefs(
       web_prefs->preferred_color_scheme = blink::PreferredColorScheme::kDark;
       break;
     case ui::NativeTheme::PreferredColorScheme::kLight:
-    case ui::NativeTheme::PreferredColorScheme::kNoPreference:
       web_prefs->preferred_color_scheme = blink::PreferredColorScheme::kLight;
       break;
   }
