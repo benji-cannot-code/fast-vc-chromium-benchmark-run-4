a721ae49d10da14e57191be33856797b66535537
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-06-12 04:27:52 +0000

    Import wpt@ce024853ffad6fcd8e58bf7be539e26bb30d8ec0
    Using wpt-import in Chromium 757ef2e0484f7b83acd6db415708646076995ddb.
    With Chromium commits locally applied on WPT:
    9c24e63c71 "Check for first frame in CreateImageBitmap"
    d2ee98f482 "Move manual offscreen wpt tests to own folder"
    458dc853b0 "WPT: Migrate content-visibility tests to wpt."
    84f61c816c "Exclude non relevants reports from the COOP-opener-breakage tests."
    e106756f1b "[COOP] Access reporting. Update the test."
    4fc2994513 "Split up canvas test yaml files"
    964749c70d "[NativeFS] Mark a bunch of tests as slow."
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    mek@chromium.org, stevebe@microsoft.com:
      external/wpt/native-file-system
    
    NOAUTOREVERT=true
    TBR=foolip@google.com
    
    No-Export: true
    Change-Id: Idc8aa460fff25d76d7479ddac08c9eb3d01c4dfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242175
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#777677}

--- third_party/blink/web_tests/external/wpt/native-file-system/idlharness.https.any.js
+++ third_party/blink/web_tests/external/wpt/native-file-system/idlharness.https.any.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 idl_test(
   ['native-file-system'],
-  ['html', 'dom'],
+  ['streams', 'html', 'dom'],
   idl_array => {
     idl_array.add_objects({
       // TODO: Add instances of FileSystemHandle, FileSystemFileHandle,
