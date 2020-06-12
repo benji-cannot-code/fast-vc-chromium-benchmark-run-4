dae49d036b4b74a64e98387f31bca3c31a554e9b
Hitoshi Yoshida <peria@chromium.org>
2020-06-12 00:40:13 +0000

    bindings: Drop a test case of NotificationDirection
    An invalid value for an IDL enum is expected to be TypeError in bindings
    layer, and thus it must be valid in Blink layer.
    This CL removes a test case that checks a behavior for an invalid value
    in Blink layer.  Also adds NOTREACHED() to confirm there are no invalid
    value inputs.
    
    
    Bug: 839389
    Change-Id: I878829b69a0a8302b6bd1f7b4fd777fa383f12df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241197
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Auto-Submit: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777597}

--- third_party/blink/renderer/modules/notifications/notification_data.cc
+++ third_party/blink/renderer/modules/notifications/notification_data.cc
@@ -23,13 +23,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 namespace {
 
+// TODO(crbug.com/1092328): Use V8NotificationDirection.
 mojom::blink::NotificationDirection ToDirectionEnumValue(
     const String& direction) {
   if (direction == "ltr")
     return mojom::blink::NotificationDirection::LEFT_TO_RIGHT;
   if (direction == "rtl")
     return mojom::blink::NotificationDirection::RIGHT_TO_LEFT;
-
+  if (direction == "auto")
+    return mojom::blink::NotificationDirection::AUTO;
+  NOTREACHED() << "Unknown direction: " << direction;
   return mojom::blink::NotificationDirection::AUTO;
 }
 
