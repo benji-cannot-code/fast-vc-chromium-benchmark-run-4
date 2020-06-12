49414335af9ffa8831ab0086a6c2c42f15810a7a
Christopher Cameron <ccameron@chromium.org>
2020-06-12 18:14:19 +0000

    MacPWAs: Fix popup windows
    At some point, popup RenderWidgetHostViews stopped being told
    which process they should appear in. Fix this.
    
    Note that popups are strange in that the RenderWidgetHostView
    doesn't have a parent WebContentsView, rather, it is just
    the content view for the NSWindow.
    
    Add a sentinel remote_cocoa::kInvalidNSViewId to specify to
    RenderWidgetHostViewMac::MigrateNSViewBridge to indicate
    that we shouldn't mess with the app-side NSView hierarchy.
    
    Bug: 1091179
    Change-Id: I905877e7b306c9f648734e4a433859f7d636356c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242851
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: ccameron <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777880}

--- components/remote_cocoa/browser/ns_view_ids.h
+++ components/remote_cocoa/browser/ns_view_ids.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remote_cocoa {
 
+constexpr uint64_t kInvalidNSViewId = 0;
+
 // Return a new unique is to be used with ScopedNSViewIdMapping and
 // GetNSViewFromId in various app shim processes.
 uint64_t REMOTE_COCOA_BROWSER_EXPORT GetNewNSViewId();
