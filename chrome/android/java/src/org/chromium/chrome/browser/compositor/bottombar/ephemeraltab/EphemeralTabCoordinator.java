ae4382ca8052e83a4f910c7a25c3e4b2b790318f
Jinsuk Kim <jinsukkim@chromium.org>
2020-06-12 02:08:06 +0000

    Revert "PreviewTab: Fix metric 'closed' being logged too few times"
    This CL reverts the culprit for the linked bug, and took a
    different approach addressing the original issue without suffering
    the crash in ProfileDestroyer. Since the destruction order matters
    for the associated profile, preview tab destruction is done on
    |onSheetContentChanged| as before, but handles the missing state
    transition to hidden simply by doing the logging task in the event.
    
    This reverts commit 620109beefc1926e15c40a0a29d3a3b1eb99505f.
    
    Bug: 1093240
    Change-Id: Ibac78816b0f262fae827b9ba6e7a59b023a54ea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239395
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777638}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/bottombar/ephemeraltab/EphemeralTabCoordinator.java
@@ -11,7 +11,6 @@ import static org.chromium.chrome.browser.dependency_injection.ChromeCommonQuali
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.os.Handler;
 import android.view.View;
 
 import org.chromium.base.Callback;
@@ -35,6 +34,7 @@ import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetController;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetController.SheetState;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetController.StateChangeReason;
 import org.chromium.chrome.browser.widget.bottomsheet.EmptyBottomSheetObserver;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
 import org.chromium.components.browser_ui.widget.RoundedIconGenerator;
 import org.chromium.components.embedder_support.view.ContentView;
 import org.chromium.components.feature_engagement.EventConstants;
@@ -58,7 +58,6 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
     private final Context mContext;
     private final ActivityWindowAndroid mWindow;
     private final View mLayoutView;
-    private final Handler mHandler = new Handler();
     private final ActivityTabProvider mTabProvider;
     private final Supplier<TabCreator> mTabCreator;
     private final BottomSheetController mBottomSheetController;
@@ -145,6 +144,15 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
                 private int mCloseReason;
 
                 @Override
+                public void onSheetContentChanged(BottomSheetContent newContent) {
+                    if (newContent != mSheetContent) {
+                        mMetrics.recordMetricsForClosed(mCloseReason);
+                        mOpened = false;
+                        destroyWebContents();
+                    }
+                }
+
+                @Override
                 public void onSheetStateChanged(int newState) {
                     if (mSheetContent == null) return;
                     switch (newState) {
@@ -158,14 +166,6 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
                             if (!mFullStateLogged) {
                                 mMetrics.recordMetricsForOpened();
                                 mFullStateLogged = true;
-                            }
-                            break;
-                        case SheetState.HIDDEN:
-                            // TODO(donnd): move the close reason to onSheetStateChanged so it's
-                            // more accurate.  See http://crbug.com/986310.
-                            if (mOpened) {
-                                mMetrics.recordMetricsForClosed(mCloseReason);
-                                mOpened = false;
                             }
                             break;
                     }
@@ -186,7 +186,7 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
             };
             mBottomSheetController.addObserver(mSheetObserver);
             mSheetContent = new EphemeralTabSheetContent(mContext, this::openInNewTab,
-                    this::onToolbarClick, this::close, this::destroyContent, getMaxSheetHeight());
+                    this::onToolbarClick, this::close, getMaxSheetHeight());
             mMediator.init(mWebContents, mContentView, mSheetContent, profile);
             mLayoutView.addOnLayoutChangeListener(this);
         }
@@ -214,7 +214,9 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
         ContentUtils.setUserAgentOverride(mWebContents);
     }
 
-    private void destroyContent() {
+    private void destroyWebContents() {
+        mSheetContent = null; // Will be destroyed by BottomSheet controller.
+
         if (mWebContents != null) {
             mWebContents.destroy();
             mWebContents = null;
@@ -224,12 +226,7 @@ public class EphemeralTabCoordinator implements View.OnLayoutChangeListener {
         if (mMediator != null) mMediator.destroyContent();
 
         mLayoutView.removeOnLayoutChangeListener(this);
-
-        // Delay the clean-up till the state transitions are completed.
-        mHandler.post(() -> {
-            if (mSheetObserver != null) mBottomSheetController.removeObserver(mSheetObserver);
-            mSheetContent = null;
-        });
+        if (mSheetObserver != null) mBottomSheetController.removeObserver(mSheetObserver);
     }
 
     private void openInNewTab() {
