8e84ebb9d2bd7474be69853273127c912e99f232
Alan Cutter <alancutter@chromium.org>
2020-06-12 08:34:25 +0000

    Re-enable WebAppLinkCapturingBrowserTest test with AppServiceIntentHandling flag
    This CL re-enables the experimental tabbed PWA link capturing logic
    with the CommonAppsNavigationThrottle class that's used by the
    AppServiceIntentHandling flag.
    
    The previous placement of the check happened after a more general app
    scope check that exits whenever the navigation is in scope of an app.
    It should come before as it is a very specific check that can be
    followed by the more general one.
    
    Bug: 1092789
    Change-Id: I112fb632d8ecc30a037e7c17931cd98a7b69fdb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242693
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Maggie Cai <mxcai@chromium.org>
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777721}

--- chrome/browser/apps/intent_helper/apps_navigation_throttle.cc
+++ chrome/browser/apps/intent_helper/apps_navigation_throttle.cc
@@ -500,6 +500,9 @@ AppsNavigationThrottle::HandleRequest() {
   if (!ShouldOverrideUrlLoading(starting_url_, url))
     return content::NavigationThrottle::PROCEED;
 
+  if (CaptureExperimentalTabStripWebAppScopeNavigations(web_contents, handle))
+    return content::NavigationThrottle::CANCEL_AND_IGNORE;
+
   if (ShouldDeferNavigation(handle)) {
     // Handling is now deferred to ArcIntentPickerAppFetcher, which
     // asynchronously queries ARC for apps, and runs
@@ -510,9 +513,6 @@ AppsNavigationThrottle::HandleRequest() {
     ui_displayed_ = true;
     return content::NavigationThrottle::DEFER;
   }
-
-  if (CaptureExperimentalTabStripWebAppScopeNavigations(web_contents, handle))
-    return content::NavigationThrottle::CANCEL_AND_IGNORE;
 
   // We didn't query ARC, so proceed with the navigation and query if we have an
   // installed desktop PWA to handle the URL.
