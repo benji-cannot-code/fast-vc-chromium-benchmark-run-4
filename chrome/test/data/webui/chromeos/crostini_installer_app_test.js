e8e0fce819b5ae0d3fb7b3cf2f6f72051b253759
David Munro <davidmunro@google.com>
2020-06-12 02:52:55 +0000

    crostini: Use dom-if instead of hidden to hide an element.
    The element wasn't being hidden properly, and it's a custom element so
    the style guide requires it be inside a dom-if template anyway.
    
    Test: Manual
    Bug: None
    Change-Id: I5b1397bac525ac67c2917465488fc278e04b5bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239694
    Commit-Queue: David Munro <davidmunro@google.com>
    Auto-Submit: David Munro <davidmunro@google.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777653}

--- chrome/test/data/webui/chromeos/crostini_installer_app_test.js
+++ chrome/test/data/webui/chromeos/crostini_installer_app_test.js
@@ -216,7 +216,7 @@ suite('<crostini-installer-app>', () => {
     await clickNext();
     await flushTasks();
     expectFalse(app.$$('#configure-message').hidden);
-    expectFalse(app.$$('#low-free-space-warning').hidden);
+    expectFalse(isHidden(app.$$('#low-free-space-warning')));
   });
 
   diskTicks.forEach(async (_, defaultIndex) => {
@@ -230,7 +230,7 @@ suite('<crostini-installer-app>', () => {
       await flushTasks();
 
       expectFalse(app.$$('#configure-message').hidden);
-      expectTrue(app.$$('#low-free-space-warning').hidden);
+      expectTrue(isHidden(app.$$('#low-free-space-warning')));
       expectTrue(isHidden(app.$$('#diskSlider')));
 
       await clickInstall();
@@ -253,7 +253,7 @@ suite('<crostini-installer-app>', () => {
     await flushTasks();
 
     expectFalse(app.$$('#configure-message').hidden);
-    expectTrue(app.$$('#low-free-space-warning').hidden);
+    expectTrue(isHidden(app.$$('#low-free-space-warning')));
     expectFalse(isHidden(app.$$('#diskSlider')));
 
     app.$$('#diskSlider').value = 1;
