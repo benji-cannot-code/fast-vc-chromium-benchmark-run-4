a4dcd472d4a5af79787b5dc5e9f35149247dd1ec
Xing Liu <xingliu@chromium.org>
2020-06-12 23:16:41 +0000

    Download later: Wire download later dialog to DownloadDialogBridge.
    This CL implements the download later flow logic in
    DownloadDialogBridge.
    
    Bug: 1078454
    Change-Id: I1a0e6470a81ebda2add4a069addf21258c67c83c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240544
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778003}

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/dialogs/DownloadLaterDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/dialogs/DownloadLaterDialogTest.java
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.download.dialogs;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import android.app.Activity;
 import android.support.test.filters.MediumTest;
 
 import org.junit.Assert;
@@ -27,10 +27,14 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.components.browser_ui.widget.RadioButtonWithDescription;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modaldialog.ModalDialogProperties;
+import org.chromium.ui.modaldialog.ModalDialogProperties.ButtonType;
 import org.chromium.ui.modelutil.PropertyModel;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Test to verify download later dialog.
@@ -48,7 +52,7 @@ public class DownloadLaterDialogTest {
     private DownloadLaterDialogController mController;
 
     @Mock
-    private ModalDialogProperties.Controller mModalDialogController;
+    private WindowAndroid mWindowAndroid;
 
     private ModalDialogManager getModalDialogManager() {
         return mActivityTestRule.getActivity().getModalDialogManager();
@@ -64,27 +68,37 @@ public class DownloadLaterDialogTest {
         MockitoAnnotations.initMocks(this);
         mActivityTestRule.startMainActivityOnBlankPage();
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            Assert.assertNotNull(mController);
+            when(mWindowAndroid.getActivity())
+                    .thenReturn(new WeakReference<Activity>(mActivityTestRule.getActivity()));
             mModel = new PropertyModel.Builder(DownloadLaterDialogProperties.ALL_KEYS)
                              .with(DownloadLaterDialogProperties.DOWNLOAD_TIME_INITIAL_SELECTION,
                                      DownloadLaterDialogChoice.ON_WIFI)
-                             .with(DownloadLaterDialogProperties.CONTROLLER, mController)
                              .build();
-
-            mDialogCoordinator = new DownloadLaterDialogCoordinator(mActivityTestRule.getActivity(),
-                    getModalDialogManager(), mModel, mModalDialogController);
+            mDialogCoordinator = new DownloadLaterDialogCoordinator();
+            Assert.assertNotNull(mController);
+            mDialogCoordinator.initialize(mController);
         });
+    }
+
+    private void showDialog() {
+        mDialogCoordinator.showDialog(mWindowAndroid, mModel);
+    }
+
+    private void clickPositiveButton() {
+        PropertyModel modalDialogModel = getModalDialogManager().getCurrentDialogForTest();
+        modalDialogModel.get(ModalDialogProperties.CONTROLLER)
+                .onClick(modalDialogModel, ButtonType.POSITIVE);
     }
 
     @Test
     @MediumTest
     public void testShowDialogThenDismiss() {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            mDialogCoordinator.showDialog();
+            showDialog();
             Assert.assertTrue(mActivityTestRule.getActivity().getModalDialogManager().isShowing());
             mDialogCoordinator.dismissDialog(DialogDismissalCause.UNKNOWN);
             Assert.assertFalse(getModalDialogManager().isShowing());
-            verify(mModalDialogController).onDismiss(any(), anyInt());
+            verify(mController).onDownloadLaterDialogCanceled();
         });
     }
 
@@ -92,7 +106,7 @@ public class DownloadLaterDialogTest {
     @MediumTest
     public void testShowDialogThenDestroy() {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            mDialogCoordinator.showDialog();
+            showDialog();
             Assert.assertTrue(getModalDialogManager().isShowing());
             mDialogCoordinator.destroy();
         });
@@ -100,9 +114,15 @@ public class DownloadLaterDialogTest {
 
     @Test
     @MediumTest
+    public void testDestroyWithoutShowDialog() {
+        TestThreadUtils.runOnUiThreadBlocking(() -> { mDialogCoordinator.destroy(); });
+    }
+
+    @Test
+    @MediumTest
     public void testSelectRadioButton() {
         TestThreadUtils.runOnUiThreadBlocking(() -> {
-            mDialogCoordinator.showDialog();
+            showDialog();
 
             // Verify the initial selection of the dialog. The controller should not get an event
             // for the initial setup.
@@ -118,7 +138,9 @@ public class DownloadLaterDialogTest {
             Assert.assertNotNull(downloadNowButton);
             downloadNowButton.setChecked(true);
             getDownloadLaterDialogView().onCheckedChanged(null, -1);
-            verify(mController).onChoiceChanged(eq(DownloadLaterDialogChoice.DOWNLOAD_NOW));
+            clickPositiveButton();
+            verify(mController)
+                    .onDownloadLaterDialogComplete(eq(DownloadLaterDialogChoice.DOWNLOAD_NOW));
         });
     }
 }
--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadLocationDialogCoordinator.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadLocationDialogCoordinator.java
@@ -133,7 +133,7 @@ public class DownloadLocationDialogCoordinator implements ModalDialogProperties.
             if (dir.type == DirectoryOption.DownloadLocationDirectoryType.DEFAULT) {
                 assert (!TextUtils.isEmpty(dir.location));
                 DownloadDialogBridge.setDownloadAndSaveFileDefaultDirectory(dir.location);
-                mController.onComplete(mSuggestedPath);
+                mController.onDownloadLocationDialogComplete(mSuggestedPath);
             }
             return;
         }
@@ -213,8 +213,9 @@ public class DownloadLocationDialogCoordinator implements ModalDialogProperties.
                 directoryOption.type, DirectoryOption.DownloadLocationDirectoryType.NUM_ENTRIES);
 
         File file = new File(directoryOption.location, fileName);
+
         assert mController != null;
-        mController.onComplete(file.getAbsolutePath());
+        mController.onDownloadLocationDialogComplete(file.getAbsolutePath());
 
         // Update preference to show prompt based on whether checkbox is checked only when the user
         // click the positive button.
@@ -227,6 +228,6 @@ public class DownloadLocationDialogCoordinator implements ModalDialogProperties.
 
     private void cancel() {
         assert mController != null;
-        mController.onCancel();
+        mController.onDownloadLocationDialogCanceled();
     }
 }
