284d44e0bf405cc01ea4d6c0560eaaa07c6913d2
Manuel Rego Casasnovas <rego@igalia.com>
2020-06-12 22:45:47 +0000

    Enable transformed rasterization for kBackfaceVisibilityHidden
    BUG=1078401
    TEST=CompositedLayerMappingTest.TransformedRasterizationForBackfaceVisibilityHidden
    
    Change-Id: I3ae4a581d247537d1211ebbc4688c6c8e1056a06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238926
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777987}

--- third_party/blink/renderer/platform/graphics/compositing_reasons.h
+++ third_party/blink/renderer/platform/graphics/compositing_reasons.h
@@ -136,7 +136,7 @@ class PLATFORM_EXPORT CompositingReason {
 
     kComboTransformedRasterizationDisallowedReasons =
         kComboAllDirectReasons & ~kScrollDependentPosition &
-        ~kTrivial3DTransform,
+        ~kTrivial3DTransform & ~kBackfaceVisibilityHidden,
 
     kComboAllCompositedScrollingDeterminedReasons =
         kScrollDependentPosition | kOverflowScrolling,
