3090cded14fcc2f9966405e79a5038407d314978
Dave Tapuska <dtapuska@chromium.org>
2020-06-12 16:59:36 +0000

    Typemap text direction in blink as well.
    base::i18n::TextDirection is already used some spots in blink. Typemap
    it for blink as well.
    
    BUG=1073560
    
    Change-Id: I6360ff3bee9a5a1be862f0bdf1673ac790c288ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241945
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777842}

--- third_party/blink/public/web/web_local_frame.h
+++ third_party/blink/public/web/web_local_frame.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/optional.h"
 #include "base/unguessable_token.h"
-#include "mojo/public/mojom/base/text_direction.mojom-forward.h"
 #include "services/network/public/mojom/fetch_api.mojom-shared.h"
 #include "services/network/public/mojom/web_sandbox_flags.mojom-shared.h"
 #include "third_party/blink/public/common/css/page_size_type.h"
@@ -444,7 +443,8 @@ class WebLocalFrame : public WebFrame {
   // (i.e its anchor is its start).
   virtual bool IsSelectionAnchorFirst() const = 0;
   // Changes the text direction of the selected input node.
-  virtual void SetTextDirectionForTesting(mojo_base::mojom::TextDirection) = 0;
+  virtual void SetTextDirectionForTesting(
+      base::i18n::TextDirection direction) = 0;
 
   // Selection -----------------------------------------------------------
 
--- third_party/blink/renderer/core/frame/web_local_frame_impl.cc
+++ third_party/blink/renderer/core/frame/web_local_frame_impl.cc
@@ -1183,7 +1183,7 @@ bool WebLocalFrameImpl::IsSelectionAnchorFirst() const {
 }
 
 void WebLocalFrameImpl::SetTextDirectionForTesting(
-    mojo_base::mojom::blink::TextDirection direction) {
+    base::i18n::TextDirection direction) {
   frame_->SetTextDirection(direction);
 }
 
