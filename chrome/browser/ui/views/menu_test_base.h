69b9cbe2d68b2cc6eef89c7e553e67a799c26ca0
Aaron Leventhal <aleventhal@google.com>
2020-06-12 19:36:03 +0000

    Correct menu events and remove redundant hide event
    Menustart/menuend events are fired at start/end of menu interaction.
    Menupopupstart/menupopupend are fired the same number of times, when
    menu is shown or hidden.
    Menupopuphide is now redundant with menupopupend.
    
    TBR=mkwst@chromium.org
    
    Bug: 1039422
    Change-Id: I5446e6ee0c09e0ed5c7b57fcc84ae35c38003917
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238018
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777916}

--- chrome/browser/ui/views/menu_test_base.h
+++ chrome/browser/ui/views/menu_test_base.h
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/macros.h"
 #include "chrome/browser/ui/views/test/view_event_test_base.h"
+#include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/events/keycodes/keyboard_codes.h"
+#include "ui/views/accessibility/ax_event_observer.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/menu/menu_delegate.h"
 
@@ -36,11 +38,15 @@ class MenuRunner;
 // MenuItemView prevents repeated activation of a menu by clicks too
 // close in time.
 class MenuTestBase : public ViewEventTestBase,
+                     public views::AXEventObserver,
                      public views::ButtonListener,
                      public views::MenuDelegate {
  public:
   MenuTestBase();
   ~MenuTestBase() override;
+
+  // AXEventObserver overrides.
+  void OnViewEvent(views::View*, ax::mojom::Event event_type) override;
 
   // Generate a mouse click and run |next| once the event has been processed.
   virtual void Click(views::View* view, base::OnceClosure next);
@@ -82,6 +88,8 @@ class MenuTestBase : public ViewEventTestBase,
   // views::MenuDelegate implementation
   void ExecuteCommand(int id) override;
 
+  int GetAXEventCount(ax::mojom::Event event_type) const;
+
  private:
   views::MenuButton* button_ = nullptr;
   views::MenuItemView* menu_ = nullptr;
@@ -89,6 +97,11 @@ class MenuTestBase : public ViewEventTestBase,
 
   // The command id of the last pressed menu item since the menu was opened.
   int last_command_;
+
+  // The number of AX events fired by type.
+  static constexpr int kNumEvents =
+      static_cast<size_t>(ax::mojom::Event::kMaxValue) + 1;
+  std::array<int, kNumEvents> ax_event_counts_;
 
   DISALLOW_COPY_AND_ASSIGN(MenuTestBase);
 };
