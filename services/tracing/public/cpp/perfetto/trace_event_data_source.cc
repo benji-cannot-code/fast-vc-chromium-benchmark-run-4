16476baabff6b6b6b5e3e8be31be1424eff62ec7
Eric Seckler <eseckler@chromium.org>
2020-06-12 05:22:29 +0000

    Revert "tracing: Support for typed events in //base."
    This reverts commit bb130954bcc5e3e5c5eec9554ae2352b486f7c81.
    
    Reason for revert: breaks ios x86 cronet builds: crbug.com/1093843
    
    Original change's description:
    > tracing: Support for typed events in //base.
    >
    > Moves the stop-gap implementation of typed trace events from
    > //services/tracing/public/cpp/perfetto into //base/trace_event and
    > extends //base's stub implementation to cover typed event macros, too.
    >
    > This adds a dependency on //third_party/perfetto to //base in the case
    > when the enable_base_tracing gn flag is set to true. Base embedders can
    > set this arg to false to disable tracing support and avoid the
    > dependency on //third_party/perfetto.
    >
    > Design doc:
    > https://docs.google.com/document/d/1f7tt4cb-JcA5bQFR1oXk60ncJPpkL02_Hi_Bc6MfTQk/edit
    >
    > Test: add a new typed event to //base (not submitted), run
    > services_unittests, base_unittests (the latter also with
    > enable_base_tracing = false).
    >
    > Change-Id: I1f54b9e6b4b9f26aff5b85d2e3d18aaceb752936
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2203380
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Commit-Queue: Eric Seckler <eseckler@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#776515}
    
    TBR=gab@chromium.org,skyostil@chromium.org,eseckler@chromium.org
    
    # Skipping CQ because this is blocking the CQ for some folks and
    # non-win bots were passing.
    NOTRY=true
    
    Change-Id: I59989ae80ff73454e0642e82bf9bc7f55e6f1b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241953
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777687}

--- services/tracing/public/cpp/perfetto/trace_event_data_source.cc
+++ services/tracing/public/cpp/perfetto/trace_event_data_source.cc
@@ -459,13 +459,11 @@ TraceEventDataSource::~TraceEventDataSource() = default;
 
 void TraceEventDataSource::RegisterStartupHooks() {
   RegisterTracedValueProtoWriter();
-  base::trace_event::EnableTypedTraceEvents(
-      &TraceEventDataSource::OnAddTypedTraceEvent);
 }
 
 void TraceEventDataSource::RegisterWithTraceLog() {
   TraceLog::GetInstance()->SetAddTraceEventOverrides(
-      &TraceEventDataSource::OnAddLegacyTraceEvent,
+      &TraceEventDataSource::OnAddTraceEvent,
       &TraceEventDataSource::FlushCurrentThread,
       &TraceEventDataSource::OnUpdateDuration);
   base::AutoLock l(lock_);
@@ -501,7 +499,8 @@ void TraceEventDataSource::OnStopTracingDone() {
 }
 
 // static
-TrackEventThreadLocalEventSink* TraceEventDataSource::GetOrPrepareEventSink() {
+TrackEventThreadLocalEventSink* TraceEventDataSource::GetOrPrepareEventSink(
+    bool thread_will_flush) {
   // Avoid re-entrancy, which can happen during PostTasks (the taskqueue can
   // emit trace events). We discard the events in this case, as any PostTasking
   // to deal with these events later would break the event ordering that the
@@ -538,7 +537,8 @@ TrackEventThreadLocalEventSink* TraceEventDataSource::GetOrPrepareEventSink() {
   }
 
   if (!thread_local_event_sink) {
-    thread_local_event_sink = GetInstance()->CreateThreadLocalEventSink();
+    thread_local_event_sink =
+        GetInstance()->CreateThreadLocalEventSink(thread_will_flush);
     ThreadLocalEventSinkSlot()->Set(thread_local_event_sink);
   }
 
@@ -930,7 +930,7 @@ TraceEventDataSource::CreateTraceWriterLocked() {
 }
 
 TrackEventThreadLocalEventSink*
-TraceEventDataSource::CreateThreadLocalEventSink() {
+TraceEventDataSource::CreateThreadLocalEventSink(bool thread_will_flush) {
   AutoLockWithDeferredTaskPosting lock(lock_);
   uint32_t session_id =
       session_flags_.load(std::memory_order_relaxed).session_id;
@@ -946,27 +946,12 @@ TraceEventDataSource::CreateThreadLocalEventSink() {
 }
 
 // static
-void TraceEventDataSource::OnAddLegacyTraceEvent(
+void TraceEventDataSource::OnAddTraceEvent(
     TraceEvent* trace_event,
     bool thread_will_flush,
     base::trace_event::TraceEventHandle* handle) {
-  auto* thread_local_event_sink = GetOrPrepareEventSink();
-  if (thread_local_event_sink) {
-    AutoThreadLocalBoolean thread_is_in_trace_event(
-        GetThreadIsInTraceEventTLS());
-    thread_local_event_sink->AddLegacyTraceEvent(trace_event, handle);
-  }
-}
-
-// static
-base::trace_event::TrackEventHandle TraceEventDataSource::OnAddTypedTraceEvent(
-    base::trace_event::TraceEvent* trace_event) {
-  auto* thread_local_event_sink = GetOrPrepareEventSink();
-  if (thread_local_event_sink) {
-    // GetThreadIsInTraceEventTLS() is handled by the sink for typed events.
-    return thread_local_event_sink->AddTypedTraceEvent(trace_event);
-  }
-  return base::trace_event::TrackEventHandle();
+  OnAddTraceEvent(trace_event, thread_will_flush, handle, perfetto::Track(),
+                  [](perfetto::EventContext) {});
 }
 
 // static
