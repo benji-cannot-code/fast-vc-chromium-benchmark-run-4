bde90bb1749ac3403ede3dd1430fe88e2b7aaf28
David Bertoni <dbertoni@chromium.org>
2020-06-12 16:50:00 +0000

    [Extensions] Add Native Messaging API tests for SW-based extensions.
    This CL adds testing for Service Worked-based extensions through
    parameterized test fixtures.
    
    Bug: 1093066
    Change-Id: I60e8605a8c0adc1addad4fe5fc72f72d698b6105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240297
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777835}

--- chrome/test/data/extensions/api_test/service_worker/messaging/send_native_message/service_worker_background.js
+++ chrome/test/data/extensions/api_test/service_worker/messaging/send_native_message/service_worker_background.js
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const appName = 'com.google.chrome.test.echo';
 const kExtensionURL  = 'chrome-extension://knldjmfmopnpolahpmmgbagdohdnhkik/';
 
-// NOTE: These tests are based on (copied from =P)
-// chrome/test/data/extensions/api_test/native_messaging/test.js [1].
-// TODO(lazyboy): We should run tests with and without Service Worker based
-// background from the same place! This will require some tweaking.
+// TODO(crbug.com/1094027): We should run tests with and without Service Worker
+// based background from the same place! This will require some tweaking.
 chrome.test.runTests([
   function invalidHostName() {
     console.log('invalid host -name test');
