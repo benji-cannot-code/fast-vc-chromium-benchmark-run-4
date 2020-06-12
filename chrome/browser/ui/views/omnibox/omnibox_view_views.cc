185b91a2f930f524f97ac6947dfd8e273da43bc9
Angela Yoeurng <yoangela@chromium.org>
2020-06-12 21:11:11 +0000

    [omnibox] Make Enter/Space trigger Suggestion Button Row buttons
    There was no case to handle KeyEvents for the Button Row buttons
    previously. This was added so Enter and Space will trigger the button's
    actions.
    
    This CL moves some of the business logic to PopupModel, merging the
    handling of Mouse/ButtonPressed events and Key events
    
    Keyword entry method was also changed to Tab rather than Keyboard shortcut.
    
    Bug: 1091351
    Change-Id: I2818e47860d8993bbb99836c40d60197b8ef7e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2233780
    Commit-Queue: Angela Yoeurng <yoangela@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777957}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -1726,8 +1726,8 @@ bool OmniboxViewViews::HandleKeyEvent(views::Textfield* textfield,
   switch (event.key_code()) {
     case ui::VKEY_RETURN: {
       OmniboxPopupModel* popup_model = model()->popup_model();
-      if (popup_model &&
-          popup_model->TriggerSelectionAction(popup_model->selection())) {
+      if (popup_model && popup_model->TriggerSelectionAction(
+                             popup_model->selection(), event.time_stamp())) {
         return true;
       } else if (MaybeTriggerSecondaryButton(event)) {
         return true;
@@ -1882,8 +1882,8 @@ bool OmniboxViewViews::HandleKeyEvent(views::Textfield* textfield,
     case ui::VKEY_SPACE: {
       if (!control && !alt && !shift && SelectionAtEnd()) {
         OmniboxPopupModel* popup_model = model()->popup_model();
-        if (popup_model &&
-            popup_model->TriggerSelectionAction(popup_model->selection())) {
+        if (popup_model && popup_model->TriggerSelectionAction(
+                               popup_model->selection(), event.time_stamp())) {
           return true;
         }
 
