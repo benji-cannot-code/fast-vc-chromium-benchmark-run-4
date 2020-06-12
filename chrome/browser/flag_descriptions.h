cda085d9d37a292df856ffc0ec2392bfe9caab57
nancylingwang@google.com <nancylingwang@chromium.org>
2020-06-12 03:13:37 +0000

    Add AppService adaptive icon flag.
    BUG=1083331
    
    Change-Id: Ie1d8457d1eb296882cf08d2b3bb71350b424d0d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241397
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777659}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1866,6 +1866,9 @@ extern const char kAllowDisableMouseAccelerationDescription[];
 extern const char kAllowScrollSettingsName[];
 extern const char kAllowScrollSettingsDescription[];
 
+extern const char kAppServiceAdaptiveIconName[];
+extern const char kAppServiceAdaptiveIconDescription[];
+
 extern const char kAppServiceInstanceRegistryName[];
 extern const char kAppServiceInstanceRegistryDescription[];
 
