52831cbb0663393da212f3a3eb1f7f066a36f141
John Palmer <jopalmer@chromium.org>
2020-06-12 06:41:37 +0000

    Quickly add undo icon as unicode - will replace with proper icon
    Bug: 1060433
    Change-Id: I7f0614dd592d330945302f58266c592ee2b9d619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241062
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777696}

--- chrome/browser/chromeos/input_method/ui/undo_window.cc
+++ chrome/browser/chromeos/input_method/ui/undo_window.cc
@@ -15,7 +15,7 @@ namespace ime {
 
 namespace {
 constexpr int kUndoWindowCornerRadius = 5;
-const char kUndoButtonText[] = "Undo";
+const char kUndoButtonText[] = "⮌ Undo";
 
 class UndoWindowBorder : public views::BubbleBorder {
  public:
