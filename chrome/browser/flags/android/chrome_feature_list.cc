70e40eff7c7384bbbf4bcf564d8231b3b0d5ff2f
Tommy Li <tommycli@chromium.org>
2020-06-12 01:21:17 +0000

    [omnibox] Delete Query in Omnibox
    This feature is not very promising, and we are deleting it now.
    
    This feature showed the search query, e.g. "harry potter" instead of the
    URL, e.g. "https://www.google.com/search?q=harry+potter" when the user
    was on the default search provider search results page.
    
    Bug: 874592
    Change-Id: Ic6be7933858ca900f9057a67358b19cb8877b60b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238345
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777619}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -241,7 +241,6 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &omnibox::kOmniboxSearchEngineLogo,
     &omnibox::kOmniboxSuggestionsRecyclerView,
     &omnibox::kOmniboxSuggestionsWrapAround,
-    &omnibox::kQueryInOmnibox,
     &password_manager::features::kGooglePasswordManager,
     &password_manager::features::kPasswordEditingAndroid,
     &password_manager::features::kPasswordManagerOnboardingAndroid,
