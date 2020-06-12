00dfadc3e8d6b80fad19a6a82a090d981ae59576
Marijn Kruisselbrink <mek@chromium.org>
2020-06-12 03:16:12 +0000

    [NativeFS] Cleanup NFS RuntimeEnabledFeatures.
    Collapse features that are no longer needed as separate ones into the
    main NativeFileSystem feature. Also add a new LegacyNativeFileSystem
    feature for API surface that is available for the duration of the origin
    trial, but won't be available when the API ships (because it was
    replaced with different API surface).
    
    Change-Id: I40a2c2dd8e87b192e1e8e0c07e05f8d7a636decd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241938
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777661}

--- third_party/blink/renderer/bindings/modules/v8/serialization/v8_script_value_deserializer_for_modules.cc
+++ third_party/blink/renderer/bindings/modules/v8/serialization/v8_script_value_deserializer_for_modules.cc
@@ -311,7 +311,7 @@ CryptoKey* V8ScriptValueDeserializerForModules::ReadCryptoKey() {
 NativeFileSystemHandle*
 V8ScriptValueDeserializerForModules::ReadNativeFileSystemHandle(
     SerializationTag tag) {
-  if (!RuntimeEnabledFeatures::CloneableNativeFileSystemHandlesEnabled(
+  if (!RuntimeEnabledFeatures::NativeFileSystemEnabled(
           ExecutionContext::From(GetScriptState()))) {
     return nullptr;
   }
