f82d7c37560d5cf6898077c38e1118470c131ae8
Jon Mann <jonmann@chromium.org>
2020-06-12 01:18:56 +0000

    Wi-Fi Sync: handle invalid inputs for NetworkIdentifier appropriately.
    There are several places that construct NetworkIdentifiers for input
    that isn't validated, which makes it possible that there isn't a valid
    SSID or security type.  This removes the assumption that it will always
    have a valid input.
    
    Bug: 966270
    Change-Id: I8d37ea53867076b848afc750fca6d11f289217b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239198
    Commit-Queue: Jon Mann <jonmann@chromium.org>
    Reviewed-by: James Vecore <vecore@google.com>
    Cr-Commit-Position: refs/heads/master@{#777614}

--- chromeos/components/sync_wifi/network_type_conversions.cc
+++ chromeos/components/sync_wifi/network_type_conversions.cc
@@ -38,7 +38,6 @@ std::string SecurityTypeStringFromMojo(
       return shill::kSecurityWep;
     default:
       // Only PSK and WEP secured networks are supported by sync.
-      NOTREACHED();
       return "";
   }
 }
