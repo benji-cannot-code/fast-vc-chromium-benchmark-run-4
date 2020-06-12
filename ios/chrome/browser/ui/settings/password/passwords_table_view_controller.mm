2ced13b5bd3877887d405fd47378ab8ad521eda6
Tina Wang <tinazwang@google.com>
2020-06-12 20:07:31 +0000

    [SettingsUI] Show pop-up bubble when clicking on the info button
    Add a customized PopoverEnterpriseInfoViewController.
    - Adjusted PopoverLabelViewController and use it as a generic bubble view controller.
    - PopoverEnterpriseInfoViewController inherits PopoverLabelViewController and reformats the secondary label.
    - Added enterprise icon.
    - Added corresponding strings.
    
    Screenshot: https://screenshot.googleplex.com/iVO01su8CTs
    
    Bug: 1085202
    Change-Id: Ia6f0b3e4a81a55835a9592dd5361ab3ac71a90a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2229999
    Reviewed-by: sebsg <sebsg@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777935}

--- ios/chrome/browser/ui/settings/password/passwords_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/passwords_table_view_controller.mm
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/cells/settings_managed_item.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_cell.h"
 #import "ios/chrome/browser/ui/settings/cells/settings_switch_item.h"
+#import "ios/chrome/browser/ui/settings/elements/enterprise_info_popover_view_controller.h"
 #import "ios/chrome/browser/ui/settings/password/password_details_table_view_controller.h"
 #import "ios/chrome/browser/ui/settings/password/password_details_table_view_controller_delegate.h"
 #import "ios/chrome/browser/ui/settings/password/password_exporter.h"
@@ -60,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/UIColor+cr_semantic_colors.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
-#import "ios/chrome/common/ui/elements/popover_label_view_controller.h"
 #import "ios/chrome/common/ui/reauthentication/reauthentication_module.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #include "ios/chrome/grit/ios_strings.h"
@@ -546,13 +546,14 @@ std::vector<std::unique_ptr<autofill::PasswordForm>> CopyOf(
 // Being called when the user clicks on the information button of the managed
 // setting's UI. Shows a textual bubble with the information of the enterprise.
 - (void)didTapManagedUIInfoButton:(UIButton*)buttonView {
-  // TODO(crbug.com/1085202): show customized text bubble.
-  // Now showing a PopoverLabelView as a placeholder.
-  NSString* text = l10n_util::GetNSString(
-      IDS_IOS_TOGGLE_SETTING_MANAGED_INFO_BUBBLE_MESSAGE);
-  PopoverLabelViewController* bubbleViewController =
-      [[PopoverLabelViewController alloc] initWithMessage:text];
+  EnterpriseInfoPopoverViewController* bubbleViewController =
+      [[EnterpriseInfoPopoverViewController alloc] initWithEnterpriseName:nil];
   [self presentViewController:bubbleViewController animated:YES completion:nil];
+
+  // Disable the button when showing the bubble.
+  buttonView.enabled = NO;
+
+  // Set the anchor and arrow direction of the bubble.
   bubbleViewController.popoverPresentationController.sourceView = buttonView;
   bubbleViewController.popoverPresentationController.sourceRect =
       buttonView.bounds;
--- ios/chrome/common/ui/elements/popover_label_view_controller.h
+++ ios/chrome/common/ui/elements/popover_label_view_controller.h
@@ -11,13 +11,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Static popover presenting a simple message.
 @interface PopoverLabelViewController : UIViewController
 
-// |Presenter| is used to dismiss this view controller when done.
+// Init with only a main message shown as the primary label.
 - (instancetype)initWithMessage:(NSString*)message NS_DESIGNATED_INITIALIZER;
+
+// Init with primary string and an attributed string set to secondary text.
+- (instancetype)initWithPrimaryAttributedString:
+                    (NSAttributedString*)primaryAttributedString
+                      secondaryAttributedString:
+                          (NSAttributedString*)secondaryAttributedString
+    NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)init NS_UNAVAILABLE;
 - (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
 - (instancetype)initWithNibName:(NSString*)nibNameOrNil
                          bundle:(NSBundle*)nibBundleOrNil NS_UNAVAILABLE;
+
 @end
 
 #endif  // IOS_CHROME_COMMON_UI_ELEMENTS_POPOVER_LABEL_VIEW_CONTROLLER_H_
