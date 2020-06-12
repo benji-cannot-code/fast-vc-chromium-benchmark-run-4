fc3fc71b0c43fcd6e6a5782a8a0afab8d66374a0
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2020-06-12 05:49:16 +0000

    Fix missing relative include path on utests
    This change will fix an include path that was missing to allow
    building zlib's unit tests outside of Chromium tree.
    
    Bug: 1032721
    Change-Id: I9b4e4a0b816e716b7e1cda9172544d02127f0d32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242125
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#777690}

--- third_party/zlib/contrib/tests/infcover.cc
+++ third_party/zlib/contrib/tests/infcover.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define local static
 
 /* XXX: hacking C assert and plugging into GTest. */
-#include "testing/gtest/include/gtest/gtest.h"
+#include "gtest.h"
 #if defined(assert)
 #undef assert
 #define assert EXPECT_TRUE
