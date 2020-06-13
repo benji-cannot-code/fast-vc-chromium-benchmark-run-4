f3a380487316f52f079f2e40200c1aaf30d858be
Xing Liu <xingliu@chromium.org>
2020-06-13 04:39:10 +0000

    Download later: Fix a windows compiling issue for DownloadSchedule.
    This CL adds DownloadSchedule to InProgressInfo.
    
    We need to ignore MSVC compiler warning 4217. Since download schedule is
    exported to other dll files but also used in
    components/download/database.
    
    Bug: 1078454
    Change-Id: Icce888153c9dedf56f74e60fd2839827512e892d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242000
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778075}

--- components/download/database/in_progress/in_progress_info.h
+++ components/download/database/in_progress/in_progress_info.h
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/optional.h"
 #include "components/download/public/common/download_danger_type.h"
 #include "components/download/public/common/download_item.h"
+#include "components/download/public/common/download_schedule.h"
 #include "components/download/public/common/download_url_parameters.h"
 #include "url/gurl.h"
 
@@ -121,6 +123,9 @@ struct InProgressInfo {
 
   // Whether the download is initiated on a metered network
   bool metered = false;
+
+  // When to start the download. Used by download later feature.
+  base::Optional<DownloadSchedule> download_schedule;
 };
 
 }  // namespace download
