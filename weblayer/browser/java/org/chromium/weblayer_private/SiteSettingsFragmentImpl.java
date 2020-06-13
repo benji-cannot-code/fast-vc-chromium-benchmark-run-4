812bcc3fd6c78d514415390f9de9b31cc343b736
Robbie McElrath <rmcelrath@chromium.org>
2020-06-13 00:35:31 +0000

    [WebLayer] Fix Site Settings dialogs on newest AndroidX version
    Site Settings dialogs crash on open with newer versions of the AndroidX
    fragment library. This is due to them expecting the LayoutInflater
    returned by the FragmentHostCallback to not have a Factory2 associated
    with it yet. To get around this, this CL makes FragmentHostCallback's
    onGetLayoutInflater method clone the Context's LayoutInflater.
    
    This CL also prevents onStart from calling dispatchActivityCreated more
    that once in case the Fragment gets stopped and restarted.
    
    Bug: 1085553
    Change-Id: I8b392fdd6b6c8231470698e76d90eae07734a818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2244094
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778040}

--- weblayer/browser/java/org/chromium/weblayer_private/SiteSettingsFragmentImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/SiteSettingsFragmentImpl.java
@@ -60,6 +60,7 @@ public class SiteSettingsFragmentImpl extends RemoteFragmentImpl {
     // resource IDs.
     private Context mContext;
 
+    private boolean mStarted;
     private FragmentController mFragmentController;
 
     /**
@@ -183,8 +184,9 @@ public class SiteSettingsFragmentImpl extends RemoteFragmentImpl {
 
         @Override
         public LayoutInflater onGetLayoutInflater() {
-            return (LayoutInflater) mFragmentImpl.getWebLayerContext().getSystemService(
-                    Context.LAYOUT_INFLATER_SERVICE);
+            Context context = mFragmentImpl.getWebLayerContext();
+            return ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                    .cloneInContext(context);
         }
 
         @Override
@@ -299,7 +301,11 @@ public class SiteSettingsFragmentImpl extends RemoteFragmentImpl {
     @Override
     public void onStart() {
         super.onStart();
-        mFragmentController.dispatchActivityCreated();
+
+        if (!mStarted) {
+            mStarted = true;
+            mFragmentController.dispatchActivityCreated();
+        }
         mFragmentController.noteStateNotSaved();
         mFragmentController.execPendingActions();
         mFragmentController.dispatchStart();
