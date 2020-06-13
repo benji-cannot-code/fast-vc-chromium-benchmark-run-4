4ebe58beed6aba8c6725b6024a57e5957a06c509
David Grogan <dgrogan@chromium.org>
2020-06-13 21:44:37 +0000

    [FlexNG] Fix double reversal for wrap-reverse
    We were double flipping some static positions for abspos flex children
    with align-items: flex-{start,end}.
    
    Included is an extensive test of static positions for abspos flex
    children.
    
    Fixed: 1090613
    Bug: 845235
    Change-Id: I1abd5d114c6740f3736d27aafdd4836b296d5d7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2244590
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778112}

--- third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/ng/flex/ng_flex_layout_algorithm.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/ng/ng_physical_box_fragment.h"
 #include "third_party/blink/renderer/core/layout/ng/ng_space_utils.h"
 #include "third_party/blink/renderer/core/paint/paint_layer_scrollable_area.h"
+#include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
@@ -103,15 +104,20 @@ AxisEdge CrossAxisStaticPositionEdge(const ComputedStyle& style,
                                      const ComputedStyle& child_style) {
   ItemPosition alignment =
       FlexLayoutAlgorithm::AlignmentForChild(style, child_style);
-  bool is_wrap_reverse = style.FlexWrap() == EFlexWrap::kWrapReverse;
+  // AlignmentForChild already accounted for wrap-reverse for kFlexStart and
+  // kFlexEnd, but not kStretch. kStretch is supposed to act like kFlexStart.
+  if (style.FlexWrap() == EFlexWrap::kWrapReverse &&
+      alignment == ItemPosition::kStretch) {
+    return AxisEdge::kEnd;
+  }
 
   if (alignment == ItemPosition::kFlexEnd)
-    return is_wrap_reverse ? AxisEdge::kStart : AxisEdge::kEnd;
+    return AxisEdge::kEnd;
 
   if (alignment == ItemPosition::kCenter)
     return AxisEdge::kCenter;
 
-  return is_wrap_reverse ? AxisEdge::kEnd : AxisEdge::kStart;
+  return AxisEdge::kStart;
 }
 
 }  // namespace
