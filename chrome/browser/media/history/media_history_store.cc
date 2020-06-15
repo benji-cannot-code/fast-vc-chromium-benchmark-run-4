5133323ce1d8b968715282cb9c0362642fdea83d
Becca Hughes <beccahughes@chromium.org>
2020-06-15 18:54:01 +0000

    [Media Feeds] Add safe search check at feed level
    We already check the feed items against Safe Search
    but we should also check the feed itself so we
    can avoid fetching the feed if it is unsafe.
    
    BUG=1085095
    
    Change-Id: I8546c372e6915512c7bb5ddde8ce379cb8cd14c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2220275
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778395}

--- chrome/browser/media/history/media_history_store.cc
+++ chrome/browser/media/history/media_history_store.cc
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr int kCurrentVersionNumber = 3;
+constexpr int kCurrentVersionNumber = 4;
 constexpr int kCompatibleVersionNumber = 1;
 
 constexpr base::FilePath::CharType kMediaHistoryDatabaseName[] =
@@ -112,6 +112,26 @@ int MigrateFrom2To3(sql::Database* db, sql::MetaTable* meta_table) {
     return target_version;
   }
   return 2;
+}
+
+int MigrateFrom3To4(sql::Database* db, sql::MetaTable* meta_table) {
+  // Version 4 adds a new column to mediaFeed.
+  const int target_version = 3;
+
+  // The mediaFeed table might not exist if the feature is disabled.
+  if (!db->DoesTableExist("mediaFeed")) {
+    meta_table->SetVersionNumber(target_version);
+    return target_version;
+  }
+
+  static const char k3To4Sql[] =
+      "ALTER TABLE mediaFeed ADD COLUMN safe_search_result INTEGER DEFAULT 0;";
+  sql::Transaction transaction(db);
+  if (transaction.Begin() && db->Execute(k3To4Sql) && transaction.Commit()) {
+    meta_table->SetVersionNumber(target_version);
+    return target_version;
+  }
+  return 3;
 }
 
 bool IsCauseFromExpiration(const net::CookieChangeCause& cause) {
@@ -386,6 +406,8 @@ sql::InitStatus MediaHistoryStore::CreateOrUpgradeIfNeeded() {
     cur_version = MigrateFrom1To2(db_.get(), meta_table_.get());
   if (cur_version == 2)
     cur_version = MigrateFrom2To3(db_.get(), meta_table_.get());
+  if (cur_version == 3)
+    cur_version = MigrateFrom3To4(db_.get(), meta_table_.get());
 
   if (cur_version == kCurrentVersionNumber)
     return sql::INIT_OK;
@@ -828,14 +850,19 @@ MediaHistoryKeyedService::PendingSafeSearchCheckList
 MediaHistoryStore::GetPendingSafeSearchCheckMediaFeedItems() {
   DCHECK(db_task_runner_->RunsTasksInCurrentSequence());
 
-  if (!CanAccessDatabase() || !feed_items_table_)
+  if (!CanAccessDatabase() || !feed_items_table_ || !feeds_table_)
     return MediaHistoryKeyedService::PendingSafeSearchCheckList();
 
-  return feed_items_table_->GetPendingSafeSearchCheckItems();
+  auto items = feeds_table_->GetPendingSafeSearchCheckItems();
+  for (auto& item : feed_items_table_->GetPendingSafeSearchCheckItems())
+    items.push_back(std::move(item));
+
+  return items;
 }
 
 void MediaHistoryStore::StoreMediaFeedItemSafeSearchResults(
-    std::map<int64_t, media_feeds::mojom::SafeSearchResult> results) {
+    std::map<MediaHistoryKeyedService::SafeSearchID,
+             media_feeds::mojom::SafeSearchResult> results) {
   DCHECK(db_task_runner_->RunsTasksInCurrentSequence());
   if (!CanAccessDatabase())
     return;
@@ -850,8 +877,19 @@ void MediaHistoryStore::StoreMediaFeedItemSafeSearchResults(
 
   std::set<int64_t> feed_ids;
   for (auto& entry : results) {
-    auto feed_id =
-        feed_items_table_->StoreSafeSearchResult(entry.first, entry.second);
+    if (entry.first.first ==
+        MediaHistoryKeyedService::SafeSearchCheckedType::kFeed) {
+      if (!feeds_table_->StoreSafeSearchResult(entry.first.second,
+                                               entry.second)) {
+        DB()->RollbackTransaction();
+        return;
+      }
+
+      continue;
+    }
+
+    auto feed_id = feed_items_table_->StoreSafeSearchResult(entry.first.second,
+                                                            entry.second);
 
     if (!feed_id.has_value()) {
       DB()->RollbackTransaction();
