4704ddd07cf7b5ffb460be6a39525fc13db46c2e
alanlxl <alanlxl@chromium.org>
2020-06-15 00:01:15 +0000

    Add tracking_id count features extraction code.
    This CL adds two new features to neural_stylus_palm_detection_filter: the
    tracking id count within a session, and the current active tracking id count.
    They are controlled by two new switches in
    NeuralStylusPalmDetectionFilterModelConfig.
    
    Bug: 1088366
    Test: Passed the unit tests.
    Change-Id: I0ff59164a1078ccbec3394cf1e590d22215f1857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224488
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778155}

--- ui/events/ozone/evdev/touch_filter/neural_stylus_palm_detection_filter.h
+++ ui/events/ozone/evdev/touch_filter/neural_stylus_palm_detection_filter.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <memory>
 #include <string>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -83,6 +84,9 @@ class COMPONENT_EXPORT(EVDEV) NeuralStylusPalmDetectionFilter
   std::bitset<kNumTouchEvdevSlots> is_palm_;
   std::bitset<kNumTouchEvdevSlots> is_delay_;
   std::map<int, PalmFilterStroke> strokes_;
+  base::TimeTicks previous_report_time_;
+  std::unordered_set<int> active_tracking_ids_;
+  int tracking_ids_count_within_session_;
   int tracking_ids_[kNumTouchEvdevSlots];
   const PalmFilterDeviceInfo palm_filter_dev_info_;
   std::unique_ptr<NeuralStylusPalmDetectionFilterModel> model_;
