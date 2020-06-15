1843620e4e37ed8d2450d25f2c73f98ad919939b
Nicholas Verne <nverne@chromium.org>
2020-06-15 02:25:10 +0000

    crostini: CrosUSBAllowUnsupported is default enabled
    The flag is ignored in code. The next CL will remove it altogether.
    
    Bug: 1094742
    Change-Id: Ib9f5ff685f59b758a0dde6d635dac7f2d9e3f9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245412
    Auto-Submit: Nicholas Verne <nverne@chromium.org>
    Commit-Queue: David Munro <davidmunro@google.com>
    Reviewed-by: David Munro <davidmunro@google.com>
    Cr-Commit-Position: refs/heads/master@{#778168}

--- chrome/browser/chromeos/usb/cros_usb_detector_unittest.cc
+++ chrome/browser/chromeos/usb/cros_usb_detector_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/chromeos/crostini/crostini_manager.h"
 #include "chrome/browser/chromeos/crostini/crostini_pref_names.h"
 #include "chrome/browser/chromeos/crostini/crostini_test_helper.h"
@@ -135,8 +134,6 @@ class CrosUsbDetectorTest : public BrowserWithTestWindowTest {
   void SetUp() override {
     BrowserWithTestWindowTest::SetUp();
     crostini_test_helper_.reset(new crostini::CrostiniTestHelper(profile()));
-    scoped_feature_list_.InitWithFeatures(
-        {chromeos::features::kCrostiniUsbAllowUnsupported}, {});
 
     TestingBrowserProcess::GetGlobal()->SetSystemNotificationHelper(
         std::make_unique<SystemNotificationHelper>());
@@ -155,7 +152,6 @@ class CrosUsbDetectorTest : public BrowserWithTestWindowTest {
   }
 
   void TearDown() override {
-    scoped_feature_list_.Reset();
     crostini_test_helper_.reset();
     BrowserWithTestWindowTest::TearDown();
   }
@@ -221,8 +217,6 @@ class CrosUsbDetectorTest : public BrowserWithTestWindowTest {
 
   TestCrosUsbDeviceObserver usb_device_observer_;
   std::unique_ptr<chromeos::CrosUsbDetector> cros_usb_detector_;
-
-  base::test::ScopedFeatureList scoped_feature_list_;
 
   std::unique_ptr<crostini::CrostiniTestHelper> crostini_test_helper_;
 
@@ -727,7 +721,6 @@ TEST_F(CrosUsbDetectorTest, AttachingAlreadyAttachedDeviceIsANoOp) {
 }
 
 TEST_F(CrosUsbDetectorTest, DeviceCanBeAttachedToArcVmWhenCrostiniIsDisabled) {
-  scoped_feature_list_.Reset();  // Clears Crostini flags.
   ConnectToDeviceManager();
   base::RunLoop().RunUntilIdle();
 
