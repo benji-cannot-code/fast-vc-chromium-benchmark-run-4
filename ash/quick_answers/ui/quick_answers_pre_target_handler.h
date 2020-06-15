1bdd19df25bbb9d0c41cb876fcfa71078ecc4e23
Abhijeet Singh <siabhijeet@google.com>
2020-06-15 21:06:14 +0000

    Move menu-cancel logic to ~QuickAnswersPreTargetHandler
    Currently QuickAnswersPreTargetHandler cancels the active menu when its
    associated view is deleting, but never gets the chance to observe the
    deletion because it gets destroyed as a member field of the view before
    the view has had a chance to call the base class's ~View() that notifies
    the observers of deletion.
    
    This CL moves the menu-canceling logic to the event-handler's destructor
    instead of observing the view's deletion. This way the logic is executed
    when the view and along with it the handler is destroyed.
    
    Bug: b:158767170
    Test: Tested on Chrome OS VM.
    Change-Id: I782b63d058083405d2d992ec8b23c6b5f0cfaaf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242039
    Commit-Queue: Abhijeet Singh <siabhijeet@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778492}

--- ash/quick_answers/ui/quick_answers_pre_target_handler.h
+++ ash/quick_answers/ui/quick_answers_pre_target_handler.h
@@ -6,10 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_QUICK_ANSWERS_UI_QUICK_ANSWERS_PRE_TARGET_HANDLER_H_
 #define ASH_QUICK_ANSWERS_UI_QUICK_ANSWERS_PRE_TARGET_HANDLER_H_
 
-#include "base/scoped_observer.h"
 #include "ui/events/event_handler.h"
 #include "ui/views/view.h"
-#include "ui/views/view_observer.h"
 
 namespace ui {
 class LocatedEvent;
@@ -30,8 +28,7 @@ class UserConsentView;
 // This class handles mouse events, and update background color or
 // dismiss quick answers view.
 // TODO (siabhijeet): Migrate to using two-phased event dispatching.
-class QuickAnswersPreTargetHandler : public ui::EventHandler,
-                                     public views::ViewObserver {
+class QuickAnswersPreTargetHandler : public ui::EventHandler {
  public:
   explicit QuickAnswersPreTargetHandler(QuickAnswersView* view);
   explicit QuickAnswersPreTargetHandler(quick_answers::UserConsentView* view);
@@ -46,9 +43,6 @@ class QuickAnswersPreTargetHandler : public ui::EventHandler,
   // ui::EventHandler:
   void OnEvent(ui::Event* event) override;
 
-  // views::ViewObserver:
-  void OnViewIsDeleting(views::View* observed_view) override;
-
   void set_dismiss_anchor_menu_on_view_closed(bool dismiss) {
     dismiss_anchor_menu_on_view_closed_ = dismiss;
   }
@@ -62,7 +56,6 @@ class QuickAnswersPreTargetHandler : public ui::EventHandler,
 
   // Associated view handled by this class.
   views::View* const view_;
-  ScopedObserver<views::View, views::ViewObserver> view_observer_{this};
 
   // Whether any active menus, |view_| is a companion Quick-Answers related view
   // of which, should be dismissed when it is deleted.
