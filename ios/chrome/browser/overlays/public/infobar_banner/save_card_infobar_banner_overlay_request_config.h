51f04a99831a9e6ca83d4d815d9244b93f4fa89f
Chris Lu <thegreenfrog@chromium.org>
2020-06-15 21:38:25 +0000

    [ios] Show SaveCard modal if upload is on
    This change overrides bannerInfobarButtonWasPressed: to add a modal
    request if upload is on. This keeps consistency with existing
    implementations of save card.
    
    Bug: 1071914
    Change-Id: Ia82426b15cd6d2aeb733934d085faead88bfb24c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241866
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778510}

--- ios/chrome/browser/overlays/public/infobar_banner/save_card_infobar_banner_overlay_request_config.h
+++ ios/chrome/browser/overlays/public/infobar_banner/save_card_infobar_banner_overlay_request_config.h
@@ -35,6 +35,9 @@ class SaveCardBannerRequestConfig
   // The name of the icon image.
   NSString* icon_image_name() const { return icon_image_name_; }
 
+  // Whether the action is an upload or a local save.
+  bool should_upload_credentials() const { return should_upload_credentials_; }
+
  private:
   OVERLAY_USER_DATA_SETUP(SaveCardBannerRequestConfig);
   explicit SaveCardBannerRequestConfig(infobars::InfoBar* infobar);
@@ -49,6 +52,7 @@ class SaveCardBannerRequestConfig
   base::string16 card_label_;
   base::string16 button_label_text_;
   NSString* icon_image_name_ = nil;
+  bool should_upload_credentials_ = false;
 };
 
 }  // namespace save_card_infobar_overlays
