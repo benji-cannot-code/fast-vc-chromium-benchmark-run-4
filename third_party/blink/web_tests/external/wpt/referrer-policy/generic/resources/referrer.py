e421513cddcf8fe63d315af09ad3508c4a5b9bf5
Dominic Farolino <dom@chromium.org>
2020-06-15 12:36:51 +0000

    Fix iframe referrer upgrade tests
    This CL fixes some tests added in https://crrev.com/c/2236900.
    
    Specifically it:
     (1) Renames the tests to make them more clear
     (2) Fixes one test to make it actually run. Previously it wasn't
         running because the test harness was being requested from an
         absolute substituted URL, so for some reason the runner wasn't
         picking it up as a real test.
     (3) Uses the window 'message' event handler so the test can be notified
         of the referrer that the iframe was fetched with, instead of
         querying the iframe's contentDocument
    
    Bug: 1090391,1093160
    Change-Id: I1117d4ae65308ba8b1acb0b6a3b4896b57e7eeb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245153
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778263}

--- third_party/blink/web_tests/external/wpt/referrer-policy/generic/resources/referrer.py
+++ third_party/blink/web_tests/external/wpt/referrer-policy/generic/resources/referrer.py
@@ -1,4 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 def main(request, response):
     response_headers = [(b"Access-Control-Allow-Origin", b"*")]
-    return (200, response_headers, request.headers.get("referer", ""))
+    body = """
+    <p id=referrer>%s</p>
+    <script>
+      const referrer_text = referrer.textContent;
+      window.parent.postMessage(referrer_text, "*");
+    </script>
+    """ % request.headers.get("referer", "")
+    return (200, response_headers, body)
