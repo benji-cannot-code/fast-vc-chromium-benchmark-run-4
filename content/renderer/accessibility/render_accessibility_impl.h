716d1980f55404d5cd2bb872b6df6f9b8626387d
Dominic Mazzoni <dmazzoni@chromium.org>
2020-06-15 19:25:14 +0000

    Add UKM for elapsed time in accessibility renderer code.
    This will enable us to track the urls where users are experiencing the
    slowest times serializing the accessibility tree. See bug for link to
    UKM collection review.
    
    UKM review doc:
    https://docs.google.com/document/d/1xQTzJy_6m019gRt1orEsOeQ10Og9j5G-D__TMhMhJFI/edit
    
    Bug: 1092944
    Change-Id: I14f443414d804f05c62131505c3a64cfd4439938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2237912
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778422}

--- content/renderer/accessibility/render_accessibility_impl.h
+++ content/renderer/accessibility/render_accessibility_impl.h
@@ -38,6 +38,10 @@ class AXActionTarget;
 struct AXEvent;
 }
 
+namespace ukm {
+class MojoUkmRecorder;
+}
+
 namespace content {
 
 class AXImageAnnotator;
@@ -282,6 +286,8 @@ class CONTENT_EXPORT RenderAccessibilityImpl : public RenderAccessibility,
 
   // The specified page language, or empty if unknown.
   std::string page_language_;
+
+  std::unique_ptr<ukm::MojoUkmRecorder> ukm_recorder_;
 
   // So we can queue up tasks to be executed later.
   base::WeakPtrFactory<RenderAccessibilityImpl>
