f48117d70b77e1e70467c5e864a60d698cd86d15
Robert Flack <flackr@chromium.org>
2020-06-15 18:18:12 +0000

    Revert "[css-pseudo] Set default ::marker styles in a real UA sheet"
    This reverts commit a53560e3c83ab4d7b379247d16995ca774e772ba.
    
    Reason for revert: Suspect this caused marker elements in blink expectations to disappear
    
    First failing build:
    https://ci.chromium.org/p/chromium/builders/ci/WebKit%20Linux%20Leak/15587
    Blink web test failures:
    https://test-results.appspot.com/data/layout_results/WebKit_Linux_Leak/15587/blink_web_tests/layout-test-results/results.html
    
    Original change's description:
    > [css-pseudo] Set default ::marker styles in a real UA sheet
    > 
    > Before this patch, default styles for ::marker were assigned manually
    > when resolving the style. The problem was that, since the rules didn't
    > actually exist in any stylesheet, they were not exposed in devtools.
    > Also, devtools assume that if a pseudo-element exists, it must have some
    > rule. This could not hold for ::marker, and then trying to add styles
    > to it would fail.
    > 
    > This patch fixes that by making the styles actual UA rules. However,
    > adding a '::marker' selector directly to the UA sheet for elements would
    > be bad: it would match every single element (even non-list-items) due to
    > the universal selector, and then it would set an internal bit for the
    > marker. So HasAnyPseudoElementStyles() would return true, which means
    > extra work when handling style changes.
    > 
    > Therefore, this patch adds a new UA sheet just for pseudo-elements. This
    > sheet is not used for elements, ensuring that HasAnyPseudoElementStyles
    > is still false if there are no author styles for a pseudo-element.
    > 
    > The selector parser was treating ::marker as invalid if the runtime flag
    > CSSMarkerPseudoElement was disabled. But now it's accepted in that case
    > for UA styles.
    > 
    > There already was a kHasMarkerPseudoElement counter to track how widely
    > used is ::marker on the web. Now this counter will only be incremented
    > for author rules.
    > 
    > Bug: 457718, 1082377
    > 
    > TEST=blink_unittests --gtest_filter=StyleResolverTest.CSSMarkerPseudoElement
    > TEST=blink_unittests --gtest_filter=UseCounterHelperTest.CSSMarkerPseudoElementUA
    > TEST=blink_unittests --gtest_filter=UseCounterHelperTest.CSSMarkerPseudoElementAuthor
    > TEST=http/tests/devtools/elements/styles-2/pseudo-elements.js
    > 
    > Change-Id: Ib5583dc1952bfec2cce8f33fe8332cc39016c34a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239579
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Commit-Queue: Oriol Brufau <obrufau@igalia.com>
    > Cr-Commit-Position: refs/heads/master@{#778303}
    
    TBR=mkwst@chromium.org,futhark@chromium.org,andruud@chromium.org,obrufau@igalia.com,yoavweiss@chromium.org
    
    Change-Id: I372feba40cccb0a6e028c5480cd81ec421fa7a57
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 457718, 1082377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246860
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778378}

--- third_party/blink/renderer/core/css/css_default_style_sheets.h
+++ third_party/blink/renderer/core/css/css_default_style_sheets.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/macros.h"
 #include "third_party/blink/renderer/core/core_export.h"
-#include "third_party/blink/renderer/core/style/computed_style_constants.h"
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -46,7 +45,6 @@ class CSSDefaultStyleSheets final
   CSSDefaultStyleSheets();
 
   bool EnsureDefaultStyleSheetsForElement(const Element&);
-  bool EnsureDefaultStyleSheetsForPseudoElement(PseudoId);
   bool EnsureDefaultStyleSheetForXrOverlay();
   void EnsureDefaultStyleSheetForFullscreen();
 
@@ -58,9 +56,6 @@ class CSSDefaultStyleSheets final
   RuleSet* DefaultViewSourceStyle();
   RuleSet* DefaultForcedColorStyle() {
     return default_forced_color_style_.Get();
-  }
-  RuleSet* DefaultPseudoElementStyle() {
-    return default_pseudo_element_style_.Get();
   }
 
   StyleSheetContents* EnsureMobileViewportStyleSheet();
@@ -77,7 +72,6 @@ class CSSDefaultStyleSheets final
   StyleSheetContents* FullscreenStyleSheet() {
     return fullscreen_style_sheet_.Get();
   }
-  StyleSheetContents* MarkerStyleSheet() { return marker_style_sheet_.Get(); }
 
   CORE_EXPORT void PrepareForLeakDetection();
 
@@ -109,7 +103,6 @@ class CSSDefaultStyleSheets final
   Member<RuleSet> default_print_style_;
   Member<RuleSet> default_view_source_style_;
   Member<RuleSet> default_forced_color_style_;
-  Member<RuleSet> default_pseudo_element_style_;
 
   Member<StyleSheetContents> default_style_sheet_;
   Member<StyleSheetContents> mobile_viewport_style_sheet_;
@@ -122,7 +115,6 @@ class CSSDefaultStyleSheets final
   Member<StyleSheetContents> text_track_style_sheet_;
   Member<StyleSheetContents> fullscreen_style_sheet_;
   Member<StyleSheetContents> webxr_overlay_style_sheet_;
-  Member<StyleSheetContents> marker_style_sheet_;
 
   std::unique_ptr<UAStyleSheetLoader> media_controls_style_sheet_loader_;
   DISALLOW_COPY_AND_ASSIGN(CSSDefaultStyleSheets);
--- third_party/blink/renderer/core/css/style_engine.h
+++ third_party/blink/renderer/core/css/style_engine.h
@@ -272,7 +272,6 @@ class CORE_EXPORT StyleEngine final : public GarbageCollected<StyleEngine>,
   void EnsureUAStyleForFullscreen();
   void EnsureUAStyleForXrOverlay();
   void EnsureUAStyleForElement(const Element&);
-  void EnsureUAStyleForPseudoElement(PseudoId);
 
   void PlatformColorsChanged();
 
