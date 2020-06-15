2daa5ddaa82ee8d0ad4e7e02d959036385e8f704
Stephen McGruer <smcgruer@chromium.org>
2020-06-15 19:07:10 +0000

    Roll WPT internal tools to 3016dfa710e2114595431f6506f1c7d5729c2484
    Bug: None
    Change-Id: Ic6f6068da5df1336bac63524489eeccd8ff8ed12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246619
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778404}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/manifest/manifest.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/manifest/manifest.py
@@ -3,6 +3,7 @@ import io
 import itertools
 import json
 import os
+from atomicwrites import atomic_write
 from copy import deepcopy
 from multiprocessing import Pool, cpu_count
 from six import (
@@ -16,8 +17,16 @@ from six import (
 )
 
 from . import vcs
-from .item import (ConformanceCheckerTest, ManifestItem, ManualTest, RefTest, SupportFile,
-                   TestharnessTest, VisualTest, WebDriverSpecTest, CrashTest)
+from .item import (ConformanceCheckerTest,
+                   CrashTest,
+                   ManifestItem,
+                   ManualTest,
+                   PrintRefTest,
+                   RefTest,
+                   SupportFile,
+                   TestharnessTest,
+                   VisualTest,
+                   WebDriverSpecTest)
 from .log import get_logger
 from .sourcefile import SourceFile
 from .typedata import TypeData
@@ -58,6 +67,7 @@ class ManifestVersionMismatch(ManifestError):
 
 item_classes = {"testharness": TestharnessTest,
                 "reftest": RefTest,
+                "print-reftest": PrintRefTest,
                 "crashtest": CrashTest,
                 "manual": ManualTest,
                 "wdspec": WebDriverSpecTest,
@@ -377,6 +387,8 @@ def load_and_update(tests_root,  # type: bytes
                              allow_cached=allow_cached)
         except ManifestVersionMismatch:
             logger.info("Manifest version changed, rebuilding")
+        except ManifestError:
+            logger.warning("Failed to load manifest, rebuilding")
 
         if manifest is not None and manifest.url_base != url_base:
             logger.info("Manifest url base did not match, rebuilding")
@@ -403,7 +415,7 @@ def write(manifest, manifest_path):
     dir_name = os.path.dirname(manifest_path)
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
-    with open(manifest_path, "w") as f:
+    with atomic_write(manifest_path, overwrite=True) as f:
         # Use ',' instead of the default ', ' separator to prevent trailing
         # spaces: https://docs.python.org/2/library/json.html#json.dump
         json.dump(manifest.to_json(caller_owns_obj=True), f,
