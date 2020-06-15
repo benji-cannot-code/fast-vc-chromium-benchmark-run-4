38d027a479fdcd3a27966fb3080b92f2442edeff
Viktor Semeniuk <vsemeniuk@google.com>
2020-06-15 15:09:39 +0000

    [Password Check] Obj-C bridge for Password Check Manager Observer
    This change adds Objective-C protocol mirroring
    IOSChromePasswordCheckManager::Observer. It can be used directly from
    ViewControllers.
    
    Bug: 1075494
    Change-Id: Ied49d5098563736ad93ceacbabe8cc73330f7c95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218017
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/master@{#778292}

---
+++ ios/chrome/browser/passwords/password_check_observer_bridge.mm
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/passwords/password_check_observer_bridge.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+PasswordCheckObserverBridge::PasswordCheckObserverBridge(
+    id<PasswordCheckObserver> delegate,
+    IOSChromePasswordCheckManager* manager)
+    : delegate_(delegate) {
+  DCHECK(manager);
+  password_check_manager_observer_.Add(manager);
+}
+
+PasswordCheckObserverBridge::~PasswordCheckObserverBridge() = default;
+
+void PasswordCheckObserverBridge::PasswordCheckStatusChanged(
+    PasswordCheckState status) {
+  [delegate_ passwordCheckStateDidChange:status];
+}
+
+void PasswordCheckObserverBridge::CompromisedCredentialsChanged(
+    password_manager::CompromisedCredentialsManager::CredentialsView
+        credentials) {
+  [delegate_ compromisedCredentialsDidChange:credentials];
+}
