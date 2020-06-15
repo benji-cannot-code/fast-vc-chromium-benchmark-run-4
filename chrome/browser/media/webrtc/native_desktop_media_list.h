250862c4d4f81dbc08b97082939491e105d286d6
Erik Chen <erikchen@chromium.org>
2020-06-15 20:35:31 +0000

    Avoid segfault if platform does not support desktop/window capture.
    Platforms that do not support desktop/window capture return a nullptr
    from CreateWindowCapturer and CreateScreenCapturer. The previous
    implementation of DesktopMediaPickerFactoryImpl::CreateMediaList ignored
    this and created an instance of NativeDesktopMediaList. The
    implementation of NativeDesktopMediaList assumes that the capturer
    exists, resulting in nullptr dereference.
    
    This CL makes two small changes:
    * The implementation of CreateMediaList avoids making an instance of
      NativeDesktopMediaList if the capturer is nullptr.
    * The constructor of NativeDesktopMediaList DCHECKs that the capturer
      exists. This eases future debugging.
    
    Change-Id: I22804d9a30c5892564c4e4221651defa95d642fd
    Bug: 1094416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2243572
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778466}

--- chrome/browser/media/webrtc/native_desktop_media_list.h
+++ chrome/browser/media/webrtc/native_desktop_media_list.h
@@ -22,6 +22,7 @@ class DesktopCapturer;
 // native windows.
 class NativeDesktopMediaList : public DesktopMediaListBase {
  public:
+  // |capturer| must exist.
   NativeDesktopMediaList(content::DesktopMediaID::Type type,
                          std::unique_ptr<webrtc::DesktopCapturer> capturer);
   ~NativeDesktopMediaList() override;
