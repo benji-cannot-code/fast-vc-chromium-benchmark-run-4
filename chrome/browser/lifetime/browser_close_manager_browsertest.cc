5f1a55cdb275496a925a81463d04b903b2fa18ae
Ramin Halavati <rhalavati@chromium.org>
2020-06-15 05:27:04 +0000

    Update downloads usage of OffTheRecord profiles.
    Profile API is being updated (issue 1033903) and the usage of
    GetOffTheRecordProfile and HasOffTheRecordProfile without argument is
    getting deprecated. Use cases of this call that ONLY refer to incognito
    or guest mode can use [Get/Has]PrimaryOTRProfile() instead.
    
    This CL updates the references to OffTheRecord profiles in downloads to
    cover all OTR profiles.
    It also updates related browser tests to use the new APIs to access
    incognito browser.
    
    Bug: 1060940
    Change-Id: Ib0d862c6fcb4005934898090a518710270e998bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2237939
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778187}

--- chrome/browser/lifetime/browser_close_manager_browsertest.cc
+++ chrome/browser/lifetime/browser_close_manager_browsertest.cc
@@ -1006,7 +1006,7 @@ IN_PROC_BROWSER_TEST_F(BrowserCloseManagerBrowserTest, TestWithDownloads) {
 // Test shutdown with a download in progress in an off-the-record profile.
 IN_PROC_BROWSER_TEST_F(BrowserCloseManagerBrowserTest,
                        TestWithOffTheRecordDownloads) {
-  Profile* otr_profile = browser()->profile()->GetOffTheRecordProfile();
+  Profile* otr_profile = browser()->profile()->GetPrimaryOTRProfile();
   Browser* otr_browser = CreateBrowser(otr_profile);
   {
     browser()->window()->Close();
@@ -1038,7 +1038,7 @@ IN_PROC_BROWSER_TEST_F(BrowserCloseManagerBrowserTest,
 // call to AttemptClose.
 IN_PROC_BROWSER_TEST_F(BrowserCloseManagerBrowserTest,
                        DISABLED_TestWithOffTheRecordWindowAndRegularDownload) {
-  Profile* otr_profile = browser()->profile()->GetOffTheRecordProfile();
+  Profile* otr_profile = browser()->profile()->GetPrimaryOTRProfile();
   Browser* otr_browser = CreateBrowser(otr_profile);
   ASSERT_NO_FATAL_FAILURE(CreateStalledDownload(browser()));
 
