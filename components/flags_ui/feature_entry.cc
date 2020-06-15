0d62a1f9e7091a478f5095bf2abbae6113dd0a5c
Jeremy Roman <jbroman@chromium.org>
2020-06-15 17:55:43 +0000

    Move mutually exclusive flag fields into a union.
    This saves about 10 kB of APK size on Android.
    
    C++20 designated initializers (which Chromium pre-adopts) makes it
    straightforward to target the correct union fields. And because
    most use of this goes through macros, this change is fairly contained.
    
    Finally, two adjacent fields (supported platforms and type) are
    narrowed to save an extra 4 bytes per feature entry.
    
    Change-Id: I02ddec892efc184366f3ed5bca5660cedcc6bcc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2243265
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778368}

--- components/flags_ui/feature_entry.cc
+++ components/flags_ui/feature_entry.cc
@@ -45,7 +45,21 @@ bool FeatureEntry::InternalNameMatches(const std::string& name) const {
       return name.size() > internal_name_length + 1 &&
              name[internal_name_length] == kMultiSeparatorChar &&
              base::StringToInt(name.substr(internal_name_length + 1), &index) &&
-             index >= 0 && index < num_options;
+             index >= 0 && index < NumOptions();
+  }
+}
+
+int FeatureEntry::NumOptions() const {
+  switch (type) {
+    case ENABLE_DISABLE_VALUE:
+    case FEATURE_VALUE:
+      return 3;
+    case MULTI_VALUE:
+      return choices.size();
+    case FEATURE_WITH_PARAMS_VALUE:
+      return 3 + feature.feature_variations.size();
+    default:
+      return 0;
   }
 }
 
@@ -54,7 +68,7 @@ std::string FeatureEntry::NameForOption(int index) const {
          type == FeatureEntry::ENABLE_DISABLE_VALUE ||
          type == FeatureEntry::FEATURE_VALUE ||
          type == FeatureEntry::FEATURE_WITH_PARAMS_VALUE);
-  DCHECK_LT(index, num_options);
+  DCHECK_LT(index, NumOptions());
   return std::string(internal_name) + testing::kMultiSeparator +
          base::NumberToString(index);
 }
@@ -64,7 +78,7 @@ base::string16 FeatureEntry::DescriptionForOption(int index) const {
          type == FeatureEntry::ENABLE_DISABLE_VALUE ||
          type == FeatureEntry::FEATURE_VALUE ||
          type == FeatureEntry::FEATURE_WITH_PARAMS_VALUE);
-  DCHECK_LT(index, num_options);
+  DCHECK_LT(index, NumOptions());
   const char* description = nullptr;
   if (type == FeatureEntry::ENABLE_DISABLE_VALUE ||
       type == FeatureEntry::FEATURE_VALUE) {
@@ -78,16 +92,16 @@ base::string16 FeatureEntry::DescriptionForOption(int index) const {
       description = kGenericExperimentChoiceDefault;
     } else if (index == 1) {
       description = kGenericExperimentChoiceEnabled;
-    } else if (index < num_options - 1) {
+    } else if (index < NumOptions() - 1) {
       // First two options do not have variations params.
       int variation_index = index - 2;
       return base::ASCIIToUTF16(
                  base::StringPiece(kGenericExperimentChoiceEnabled)) +
              base::ASCIIToUTF16(" ") +
              base::ASCIIToUTF16(
-                 feature_variations[variation_index].description_text);
+                 feature.feature_variations[variation_index].description_text);
     } else {
-      DCHECK_EQ(num_options - 1, index);
+      DCHECK_EQ(NumOptions() - 1, index);
       description = kGenericExperimentChoiceDisabled;
     }
   } else {
@@ -98,7 +112,7 @@ base::string16 FeatureEntry::DescriptionForOption(int index) const {
 
 const FeatureEntry::Choice& FeatureEntry::ChoiceForOption(int index) const {
   DCHECK_EQ(FeatureEntry::MULTI_VALUE, type);
-  DCHECK_LT(index, num_options);
+  DCHECK_LT(index, NumOptions());
 
   return choices[index];
 }
@@ -106,11 +120,11 @@ const FeatureEntry::Choice& FeatureEntry::ChoiceForOption(int index) const {
 FeatureEntry::FeatureState FeatureEntry::StateForOption(int index) const {
   DCHECK(type == FeatureEntry::FEATURE_VALUE ||
          type == FeatureEntry::FEATURE_WITH_PARAMS_VALUE);
-  DCHECK_LT(index, num_options);
+  DCHECK_LT(index, NumOptions());
 
   if (index == 0)
     return FeatureEntry::FeatureState::DEFAULT;
-  if (index == num_options - 1)
+  if (index == NumOptions() - 1)
     return FeatureEntry::FeatureState::DISABLED;
   return FeatureEntry::FeatureState::ENABLED;
 }
@@ -119,14 +133,14 @@ const FeatureEntry::FeatureVariation* FeatureEntry::VariationForOption(
     int index) const {
   DCHECK(type == FeatureEntry::FEATURE_VALUE ||
          type == FeatureEntry::FEATURE_WITH_PARAMS_VALUE);
-  DCHECK_LT(index, num_options);
+  DCHECK_LT(index, NumOptions());
 
   if (type == FeatureEntry::FEATURE_WITH_PARAMS_VALUE && index > 1 &&
-      index < num_options - 1) {
+      index < NumOptions() - 1) {
     // We have no variations for FEATURE_VALUE type. Option at |index|
     // corresponds to variation at |index| - 2 as the list starts with "Default"
     // and "Enabled" (with default parameters).
-    return &feature_variations[index - 2];
+    return &feature.feature_variations[index - 2];
   }
   return nullptr;
 }
