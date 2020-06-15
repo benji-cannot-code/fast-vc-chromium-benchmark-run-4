e1c3c7e61168f5bddebcd39f3ac6ceaada797c2e
Gayane Petrosyan <gayane@chromium.org>
2020-06-15 16:43:44 +0000

    [QRCode Android] Download QR code with download manager
    Download QR code bitmap using Chrome's download manager.
    Demo: https://drive.google.com/file/d/1KE4G57GBfYBfyh-JJBq4I0YiN5uE3QXj/view?usp=sharing
    
    This CL also removes notifications, toasts and histograms that used to
    trigger with the saving bitmap success or failure. Additional cleanup
    might be necessary.
    
    Bug: 1079021
    Change-Id: I7138f3efe8add621d482b005d554b2f47ee7b4d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2224030
    Commit-Queue: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Travis Skare <skare@chromium.org>
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778333}

---
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/BitmapDownloadRequest.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.share;
+
+import android.graphics.Bitmap;
+
+import org.chromium.base.annotations.NativeMethods;
+
+/**
+ * A Java API for requesting bitmap download from Chrome's download manager service.
+ */
+public class BitmapDownloadRequest {
+    public static void downloadBitmap(String fileName, Bitmap bitmap) {
+        BitmapDownloadRequestJni.get().downloadBitmap(fileName, bitmap);
+    }
+
+    @NativeMethods
+    interface Natives {
+        void downloadBitmap(String fileName, Bitmap bitmap);
+    }
+}
