8eda673f59421b406edd783ea9ac8d15904b64af
Etienne Bergeron <etienneb@chromium.org>
2020-06-15 22:16:29 +0000

    Allow delegate post-task only when Thread Pool is available
    In some unittests, the ThreadPool is not created and tasks can't be
    posted.
    
    Typically, unittests should instantiate a TaskEnvironment to allows
    posting tasks to the pool. The GPU tests are wrapped and doesn't provide
    an easy place to instantiate that environment.
    
    As an example, this is how the angle platform methods 'gles2_conform_test':
    
            angle::InitializePlatform [0x00007FF924B66670+112] (C:\src\chromium\src\ui\gl\angle_platform_impl.cc:150)
            gl::GLSurfaceEGL::InitializeDisplay [0x00007FF924B79497+4943] (C:\src\chromium\src\ui\gl\gl_surface_egl.cc:1206)
            gl::GLSurfaceEGL::InitializeOneOff [0x00007FF924B78110+64] (C:\src\chromium\src\ui\gl\gl_surface_egl.cc:856)
            gl::init::InitializeGLOneOffPlatform [0x00007FF9540E764A+102] (C:\src\chromium\src\ui\gl\init\gl_initializer_win.cc:195)
            gl::init::InitializeGLOneOffPlatformImplementation [0x00007FF9540E607E+60] (C:\src\chromium\src\ui\gl\init\gl_factory.cc:178)
            gl::init::`anonymous namespace'::InitializeGLOneOffPlatformHelper [0x00007FF9540E5CE7+276] (C:\src\chromium\src\ui\gl\init\gl_factory.cc:107)
            gl::init::InitializeGLNoExtensionsOneOff [0x00007FF9540E5E4B+104] (C:\src\chromium\src\ui\gl\init\gl_factory.cc:134)
            egl::ThreadState::Get [0x00007FF6C17C68D7+623] (C:\src\chromium\src\gpu\gles2_conform_support\egl\thread_state.cc:82)
            eglGetDisplay [0x00007FF6C17C6036+21] (C:\src\chromium\src\gpu\gles2_conform_support\egl\egl.cc:23)
            GTFInitEGL [0x00007FF6C17A731D+218] (C:\src\chromium\src\third_party\gles2_conform\GTF_ES\glsl\GTF\Source\GTFInitEGL.c:310)
            GTFMain [0x00007FF6C17A9E1C+2073] (C:\src\chromium\src\third_party\gles2_conform\GTF_ES\glsl\GTF\Source\GTFMain.c:406)
            main [0x00007FF6C17C78BE+422] (C:\src\chromium\src\gpu\gles2_conform_support\native\main.cc:56)
            __scrt_common_main_seh [0x00007FF6C17E01E4+268] (d:\agent\_work\3\s\src\vctools\crt\vcstartup\src\startup\exe_common.inl:288)
            BaseThreadInitThunk [0x00007FF962404034+20]
            RtlUserThreadStart [0x00007FF964443691+33]
    
    R=sunnyps@chromium.org
    
    Bug: 1091259
    Change-Id: I037a4bdcaf525c6c8b3bce7dd6e76252c231a792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246869
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778525}

--- ui/gl/angle_platform_impl.cc
+++ ui/gl/angle_platform_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/task/post_task.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/trace_event/trace_event.h"
 #include "third_party/angle/include/platform/PlatformMethods.h"
 #include "ui/gl/gl_bindings.h"
@@ -171,7 +172,11 @@ bool InitializePlatform(EGLDisplay display) {
       ANGLEPlatformImpl_monotonicallyIncreasingTime;
   platformMethods->updateTraceEventDuration =
       ANGLEPlatformImpl_updateTraceEventDuration;
-  platformMethods->postWorkerTask = ANGLEPlatformImpl_postWorkerTask;
+
+  // Initialize the delegate to allow posting tasks in the Chromium thread pool.
+  // The thread pool is not available in some unittests.
+  if (base::ThreadPoolInstance::Get())
+    platformMethods->postWorkerTask = ANGLEPlatformImpl_postWorkerTask;
   return true;
 }
 
