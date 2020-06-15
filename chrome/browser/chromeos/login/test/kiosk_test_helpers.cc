e86e7a09cb7eb5b00093deb3c89e1935f7a40be9
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-06-15 07:31:42 +0000

    [Kiosk] Add test to verify auto launched kiosk after enrollment
    Recently this has been broken. Let's add a browser test for this.
    
    Bug: 1082666,1046364
    Change-Id: I569bbe30684676180c382e647034f40a04f0327c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2202056
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778207}

---
+++ chrome/browser/chromeos/login/test/kiosk_test_helpers.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/login/test/kiosk_test_helpers.h"
+
+namespace chromeos {
+
+KioskSessionInitializedWaiter::KioskSessionInitializedWaiter() {
+  scoped_observer_.Add(KioskAppManager::Get());
+}
+
+KioskSessionInitializedWaiter::~KioskSessionInitializedWaiter() = default;
+
+void KioskSessionInitializedWaiter::Wait() {
+  if (KioskAppManager::Get()->app_session())
+    return;
+
+  run_loop_.Run();
+}
+
+void KioskSessionInitializedWaiter::OnKioskSessionInitialized() {
+  run_loop_.Quit();
+}
+
+}  // namespace chromeos
