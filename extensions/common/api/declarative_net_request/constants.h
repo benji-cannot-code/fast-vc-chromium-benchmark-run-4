a21f9fbd5fdc631d17a4cd2731f9787c7f852a02
Kelvin Jiang <kelvinjiang@chromium.org>
2020-06-15 23:34:02 +0000

    [DNR] Add support for append/set operations in RequestAction
    This CL adds support for the append/set modifyHeaders operations in
    RequestAction, along with a header value. Now, matching modifyHeaders
    rules will also have the appended/set values for its headers added to
    RequestActions.
    
    Evaluation of these operations will be done in a follow up.
    
    Bug: 1081544
    Change-Id: Id716b556bb088049223471b6702009ac300d4916
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242657
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778557}

--- extensions/common/api/declarative_net_request/constants.h
+++ extensions/common/api/declarative_net_request/constants.h
@@ -80,6 +80,7 @@ extern const char kRequestHeadersPath[];
 extern const char kResponseHeadersPath[];
 extern const char kHeaderNameKey[];
 extern const char kHeaderOperationKey[];
+extern const char kHeaderValueKey[];
 
 }  // namespace declarative_net_request
 }  // namespace extensions
--- extensions/common/api/declarative_net_request/test_utils.cc
+++ extensions/common/api/declarative_net_request/test_utils.cc
@@ -148,8 +148,12 @@ std::unique_ptr<base::DictionaryValue> TestRuleRedirect::ToValue() const {
   return dict;
 }
 
-TestHeaderInfo::TestHeaderInfo(std::string header, std::string operation)
-    : header(std::move(header)), operation(std::move(operation)) {}
+TestHeaderInfo::TestHeaderInfo(std::string header,
+                               std::string operation,
+                               base::Optional<std::string> value)
+    : header(std::move(header)),
+      operation(std::move(operation)),
+      value(std::move(value)) {}
 TestHeaderInfo::~TestHeaderInfo() = default;
 TestHeaderInfo::TestHeaderInfo(const TestHeaderInfo&) = default;
 TestHeaderInfo& TestHeaderInfo::operator=(const TestHeaderInfo&) = default;
@@ -158,6 +162,7 @@ std::unique_ptr<base::DictionaryValue> TestHeaderInfo::ToValue() const {
   auto dict = std::make_unique<base::DictionaryValue>();
   SetValue(dict.get(), kHeaderNameKey, header);
   SetValue(dict.get(), kHeaderOperationKey, operation);
+  SetValue(dict.get(), kHeaderValueKey, value);
   return dict;
 }
 
