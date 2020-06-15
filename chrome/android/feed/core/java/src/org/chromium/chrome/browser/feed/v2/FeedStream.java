d6249db5cb219e33429b41fde7bd33fca7869d6d
Jian Li <jianli@chromium.org>
2020-06-15 19:28:07 +0000

    Restore feed scroll state
    Bug: none
    Change-Id: I0f9747e0f6a4356bcfc03ce202909a507417fd6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241869
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778424}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedStream.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedStream.java
@@ -12,6 +12,10 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.chromium.base.Log;
 import org.chromium.base.ObserverList;
 import org.chromium.chrome.browser.feed.shared.stream.Header;
 import org.chromium.chrome.browser.feed.shared.stream.Stream;
@@ -29,6 +33,8 @@ import java.util.List;
  */
 public class FeedStream implements Stream {
     private static final String TAG = "FeedStream";
+    private static final String SCROLL_POSITION = "scroll_pos";
+    private static final String SCROLL_OFFSET = "scroll_off";
 
     private final Activity mActivity;
     private final FeedStreamSurface mFeedStreamSurface;
@@ -53,7 +59,9 @@ public class FeedStream implements Stream {
     @Override
     public void onCreate(@Nullable String savedInstanceState) {
         setupRecyclerView();
-        // TODO(jianli): Restore scroll state.
+        if (savedInstanceState != null && !savedInstanceState.isEmpty()) {
+            restoreScrollState(savedInstanceState);
+        }
     }
 
     @Override
@@ -69,8 +77,28 @@ public class FeedStream implements Stream {
 
     @Override
     public String getSavedInstanceStateString() {
-        // TODO(jianli): Return saved scroll state as serializedf string;
-        return "";
+        LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+        if (layoutManager == null) {
+            return "";
+        }
+        int firstItemPosition = layoutManager.findFirstVisibleItemPosition();
+        if (firstItemPosition == RecyclerView.NO_POSITION) {
+            return "";
+        }
+        View firstVisibleView = layoutManager.findViewByPosition(firstItemPosition);
+        if (firstVisibleView == null) {
+            return "";
+        }
+        int firstVisibleTop = firstVisibleView.getTop();
+
+        JSONObject jsonSavedState = new JSONObject();
+        try {
+            jsonSavedState.put(SCROLL_POSITION, firstItemPosition);
+            jsonSavedState.put(SCROLL_OFFSET, firstVisibleTop);
+        } catch (JSONException e) {
+            Log.d(TAG, "Unable to write to a JSONObject.");
+        }
+        return jsonSavedState.toString();
     }
 
     @Override
@@ -184,5 +212,19 @@ public class FeedStream implements Stream {
                 }
             }
         });
+    }
+
+    private void restoreScrollState(String savedInstanceState) {
+        try {
+            JSONObject jsonSavedState = new JSONObject(savedInstanceState);
+            LinearLayoutManager layoutManager =
+                    (LinearLayoutManager) mRecyclerView.getLayoutManager();
+            if (layoutManager != null) {
+                layoutManager.scrollToPositionWithOffset(jsonSavedState.getInt(SCROLL_POSITION),
+                        jsonSavedState.getInt(SCROLL_OFFSET));
+            }
+        } catch (JSONException e) {
+            Log.d(TAG, "Unable to parse a JSONObject from a string.");
+        }
     }
 }
