08df2bc087fa8663fe18c2a13cb95a7f76a49d44
Adam Rice <ricea@chromium.org>
2020-06-15 13:38:51 +0000

    [XHR] Use XMLHttpRequestBodyInit in IDL
    https://github.com/whatwg/xhr/pull/279 modified the XHR standard to use
    the type XMLHttpRequestBodyInit in the send() method. Update our IDL to
    match.
    
    Unlike in the standard, we define XMLHttpRequestBodyInit in
    xml_http_request.idl rather than fetch.idl.
    
    No behavioural changes.
    
    Add a web platform test for sending strings containing invalid unicode
    to verify that the behaviour has not changed.
    
    BUG=1091120
    
    Change-Id: I7f26160ad925d5956bdf05b1b69572735b7d0907
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2239433
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778272}

--- third_party/blink/renderer/core/xmlhttprequest/xml_http_request.cc
+++ third_party/blink/renderer/core/xmlhttprequest/xml_http_request.cc
@@ -38,8 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink.h"
 #include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom-blink.h"
 #include "third_party/blink/public/platform/web_url_request.h"
-#include "third_party/blink/renderer/bindings/core/v8/array_buffer_or_array_buffer_view_or_blob_or_document_or_string_or_form_data_or_url_search_params.h"
 #include "third_party/blink/renderer/bindings/core/v8/array_buffer_or_array_buffer_view_or_blob_or_usv_string.h"
+#include "third_party/blink/renderer/bindings/core/v8/document_or_xml_http_request_body_init.h"
 #include "third_party/blink/renderer/core/dom/document_init.h"
 #include "third_party/blink/renderer/core/dom/document_parser.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
@@ -762,13 +762,23 @@ bool XMLHttpRequest::InitSend(ExceptionState& exception_state) {
 }
 
 void XMLHttpRequest::send(
-    const ArrayBufferOrArrayBufferViewOrBlobOrDocumentOrStringOrFormDataOrURLSearchParams&
+    const DocumentOrBlobOrArrayBufferOrArrayBufferViewOrFormDataOrURLSearchParamsOrUSVString&
         body,
     ExceptionState& exception_state) {
   probe::WillSendXMLHttpOrFetchNetworkRequest(GetExecutionContext(), Url());
 
   if (body.IsNull()) {
     send(String(), exception_state);
+    return;
+  }
+
+  if (body.IsDocument()) {
+    send(body.GetAsDocument(), exception_state);
+    return;
+  }
+
+  if (body.IsBlob()) {
+    send(body.GetAsBlob(), exception_state);
     return;
   }
 
@@ -782,16 +792,6 @@ void XMLHttpRequest::send(
     return;
   }
 
-  if (body.IsBlob()) {
-    send(body.GetAsBlob(), exception_state);
-    return;
-  }
-
-  if (body.IsDocument()) {
-    send(body.GetAsDocument(), exception_state);
-    return;
-  }
-
   if (body.IsFormData()) {
     send(body.GetAsFormData(), exception_state);
     return;
@@ -802,8 +802,8 @@ void XMLHttpRequest::send(
     return;
   }
 
-  DCHECK(body.IsString());
-  send(body.GetAsString(), exception_state);
+  DCHECK(body.IsUSVString());
+  send(body.GetAsUSVString(), exception_state);
 }
 
 bool XMLHttpRequest::AreMethodAndURLValidForSend() {
--- third_party/blink/renderer/core/xmlhttprequest/xml_http_request.h
+++ third_party/blink/renderer/core/xmlhttprequest/xml_http_request.h
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class
-    ArrayBufferOrArrayBufferViewOrBlobOrDocumentOrStringOrFormDataOrURLSearchParams;
+    DocumentOrBlobOrArrayBufferOrArrayBufferViewOrFormDataOrURLSearchParamsOrUSVString;
 class Blob;
 class BlobDataHandle;
 class DOMArrayBuffer;
@@ -137,7 +137,7 @@ class XMLHttpRequest final : public XMLHttpRequestEventTarget,
             bool async,
             ExceptionState&);
   void send(
-      const ArrayBufferOrArrayBufferViewOrBlobOrDocumentOrStringOrFormDataOrURLSearchParams&,
+      const DocumentOrBlobOrArrayBufferOrArrayBufferViewOrFormDataOrURLSearchParamsOrUSVString&,
       ExceptionState&);
   void abort();
   void Dispose();
