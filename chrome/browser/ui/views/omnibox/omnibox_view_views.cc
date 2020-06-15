9a47e3cf54ebd0bd658523b08e892d40a3168bc5
Emily Stark <estark@google.com>
2020-06-15 22:14:39 +0000

    Add unit tests for path color in path-hiding experiments
    The main goal of this CL is to add a regression test for
    https://crbug.com/1093748. To do that, it adds an
    OmniboxViewViews::SetPathColor() method that is overridden by
    TestingOmniboxView to check that the path is not set to transparent
    when it shouldn't be. The same method is used to test that the path is
    set to transparent when the path-hiding field trials are enabled.
    
    This approach waas complicated by the fact that the unit tests don't
    actually call the real EmphasizeURLComponents(), which is where the
    bug was located. TestingOmniboxView had an EmphasizeURLComponents()
    override that did a subset of what the real version does. The real
    EmphasizeURLComponents() was not immediately usable from unit tests
    because it had an early-return when there was a null
    location_bar_view_, which is the case in tests. However, I couldn't
    find any reason why a non-null location_bar_view_ was needed, so I
    simply removed that check and made the unit tests use the real
    EmphasizeURLComponents().
    
    The last wrinkle in this plan is that using the real
    EmphasizeURLComponents() caused OmniboxViewViewsTest.OnBlur to start
    failing. AFAICT this is due to either a real bug related to how RTL
    URLs are scrolled in focus mode, or an incorrect assertion. I
    commented out the assertion for now and filed
    https://bugs.chromium.org/p/chromium/issues/detail?id=1094386.
    
    Bug: 1092651,1093748
    Change-Id: I8b4690251d5d82fc28f4141bb37087ca730726fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2243491
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778523}

--- chrome/browser/ui/views/omnibox/omnibox_view_views.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -374,9 +374,6 @@ bool OmniboxViewViews::SelectionAtEnd() const {
 }
 
 void OmniboxViewViews::EmphasizeURLComponents() {
-  if (!location_bar_view_)
-    return;
-
   // Cancel any existing path fading animation. The path style will be reset
   // in the following lines, so there should be no ill effects from cancelling
   // the animation midway.
@@ -411,7 +408,7 @@ void OmniboxViewViews::EmphasizeURLComponents() {
     // If reveal-on-hover is enabled and hide-on-interaction is disabled, hide
     // the path now.
     if (CanFadePath())
-      ApplyColor(SK_ColorTRANSPARENT, GetPathBounds());
+      SetPathColor(SK_ColorTRANSPARENT);
   }
 }
 
@@ -635,6 +632,10 @@ void OmniboxViewViews::AddedToWidget() {
 void OmniboxViewViews::RemovedFromWidget() {
   views::Textfield::RemovedFromWidget();
   scoped_compositor_observer_.RemoveAll();
+}
+
+void OmniboxViewViews::SetPathColor(SkColor color) {
+  ApplyColor(color, GetPathBounds());
 }
 
 void OmniboxViewViews::OnThemeChanged() {
@@ -1592,7 +1593,7 @@ void OmniboxViewViews::OnBlur() {
         !OmniboxFieldTrial::ShouldHidePathQueryRefOnInteraction()) {
       ResetPathFadeInAnimation();
       if (CanFadePath())
-        ApplyColor(SK_ColorTRANSPARENT, GetPathBounds());
+        SetPathColor(SK_ColorTRANSPARENT);
     } else if (OmniboxFieldTrial::ShouldHidePathQueryRefOnInteraction()) {
       ResetToHideOnInteraction();
     }
@@ -1622,7 +1623,7 @@ void OmniboxViewViews::DidFinishNavigation(
   if (navigation->IsSameDocument()) {
     // Make sure the path is not re-shown for same-document navigations.
     if (CanFadePath())
-      ApplyColor(SK_ColorTRANSPARENT, GetPathBounds());
+      SetPathColor(SK_ColorTRANSPARENT);
     return;
   }
   // Once a cross-document navigation finishes, show the path and reset state so
@@ -2159,9 +2160,8 @@ void OmniboxViewViews::ResetToHideOnInteraction() {
       std::make_unique<PathFadeAnimation>(this, dimmed_text_color,
                                           SK_ColorTRANSPARENT, 0);
   if (CanFadePath()) {
-    ApplyColor(GetOmniboxColor(GetThemeProvider(),
-                               OmniboxPart::LOCATION_BAR_TEXT_DIMMED),
-               GetPathBounds());
+    SetPathColor(GetOmniboxColor(GetThemeProvider(),
+                                 OmniboxPart::LOCATION_BAR_TEXT_DIMMED));
   }
 }
 
