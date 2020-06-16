7f601ea1a876488a85a7e212ef76e06a218f7140
Maggie Cai <mxcai@chromium.org>
2020-06-16 08:15:08 +0000

    [IntentHandling] Fix navigation throttle return value.
    The original code was incorrectly DEFER the navigation when it should
    actually CANCEL_AND_IGNORE the navigation. This CL fixes this.
    BUG=853604
    
    Change-Id: I6fa1e7bda97c38d446bf8e7496440e0ff6ce55b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247434
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778699}

--- chrome/browser/apps/intent_helper/apps_navigation_throttle.cc
+++ chrome/browser/apps/intent_helper/apps_navigation_throttle.cc
@@ -404,6 +404,11 @@ bool AppsNavigationThrottle::ShouldShowPersistenceOptions(
   return !ContainsOnlyPwasAndMacApps(apps);
 }
 
+bool AppsNavigationThrottle::ShouldCancelNavigation(
+    content::NavigationHandle* handle) {
+  return false;
+}
+
 bool AppsNavigationThrottle::ShouldDeferNavigation(
     content::NavigationHandle* handle) {
   return false;
@@ -502,6 +507,12 @@ AppsNavigationThrottle::HandleRequest() {
 
   if (CaptureExperimentalTabStripWebAppScopeNavigations(web_contents, handle))
     return content::NavigationThrottle::CANCEL_AND_IGNORE;
+
+  // Handles apps that are automatically launched and the navigation needs to be
+  // cancelled.
+  if (ShouldCancelNavigation(handle)) {
+    return content::NavigationThrottle::CANCEL_AND_IGNORE;
+  }
 
   if (ShouldDeferNavigation(handle)) {
     // Handling is now deferred to ArcIntentPickerAppFetcher, which
