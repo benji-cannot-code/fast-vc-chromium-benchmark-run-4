085d134137233a3b49ee714d95f3535af1fd50e4
Omar Morsi <omorsi@google.com>
2020-06-16 22:44:39 +0000

    Extend getKeyPair API function to retrieve EC keys
    chrome.platformKeys.getKeyPair(certificate, parameters, callback) passes the key
    pair of the certificate to the callback so as it can be used with
    platformKeys.subtleCrypto functions. For more information please refer to:
    https://developer.chrome.com/extensions/platformKeys#method-getKeyPair
    
    This CL extends this function so that it accepts a certificate for an EC key.
    
    Bug: 1059209, 980395
    Change-Id: Ic34294cbb380ac3fe5da63ccebfa4e0a6f8fcf14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2231046
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Omar Morsi <omorsi@google.com>
    Cr-Commit-Position: refs/heads/master@{#779045}

--- chrome/renderer/resources/extensions/platform_keys/get_public_key.js
+++ chrome/renderer/resources/extensions/platform_keys/get_public_key.js
@@ -16,7 +16,10 @@ function normalizeImportParams(importParams) {
     throw new Error('Algorithm: name: Missing or not a String');
   }
 
-  var filteredParams = { name: importParams.name };
+  var filteredParams = {
+    name: importParams.name,
+    namedCurve: importParams.namedCurve
+  };
 
   var hashIsNone = false;
   if (importParams.hash) {
@@ -30,9 +33,13 @@ function normalizeImportParams(importParams) {
     }
   }
 
+  if (importParams.name === 'ECDSA' && importParams.namedCurve !== 'P-256') {
+    throw new Error('Only P-256 named curve is supported.');
+  }
+
   // Apply WebCrypto's algorithm normalization.
   var resultParams = normalizeAlgorithm(filteredParams, 'ImportKey');
-  if (!resultParams ) {
+  if (!resultParams) {
     throw new Error('A required parameter was missing or out-of-range');
   }
   if (hashIsNone) {
@@ -48,7 +55,9 @@ function combineAlgorithms(algorithm, importParams) {
     algorithm.publicExponent = new Uint8Array(algorithm.publicExponent);
   }
 
-  algorithm.hash = importParams.hash;
+  if (importParams.hash) {
+    algorithm.hash = importParams.hash;
+  }
   return algorithm;
 }
 
