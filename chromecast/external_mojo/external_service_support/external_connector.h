d0bb75b975f462c75f5c49e717fdaa5e46a19e23
Antoine Desbois <antoinedesbois@google.com>
2020-06-16 01:16:07 +0000

    Add WARN_UNUSED_RESULT qualifier
    This prevent a user from adding a callback and discarding the return
    value which in turns causes the callback not to be called later on.
    
    Merge-With: eureka-internal/407264
    
    Change-Id: I4b1fb4570c6709297f783d488f5505b4102573ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240294
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Kenneth MacKay <kmackay@chromium.org>
    Commit-Queue: Antoine Desbois <antoinedesbois@google.com>
    Cr-Commit-Position: refs/heads/master@{#778594}

--- chromecast/external_mojo/external_service_support/external_connector.h
+++ chromecast/external_mojo/external_service_support/external_connector.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback_list.h"
+#include "base/compiler_specific.h"
 #include "chromecast/external_mojo/public/mojom/connector.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -45,7 +46,8 @@ class ExternalConnector {
   // started, and this object is valid. Note that some prior messages may be
   // lost.
   virtual std::unique_ptr<base::CallbackList<void()>::Subscription>
-  AddConnectionErrorCallback(base::RepeatingClosure callback) = 0;
+  AddConnectionErrorCallback(base::RepeatingClosure callback)
+      WARN_UNUSED_RESULT = 0;
 
   // Registers a service that other Mojo processes/services can bind to. Others
   // can call BindInterface(|service_name|, interface_name) to bind to this
