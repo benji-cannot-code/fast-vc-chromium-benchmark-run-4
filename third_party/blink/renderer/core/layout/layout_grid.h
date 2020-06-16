277de0026d83d47b861a1a22bb05fba6713dea1c
Patrick Brosset <patrick.brosset@microsoft.com>
2020-06-16 10:28:48 +0000

    DevTools: Calculate Grid row and column Negative number line positions
    This adds a new option to the GridHighlightConfig Overlay domain
    protocol type. When set to true, this option instructs the domain agent
    to calculate the positions for showing css grid negative line numbers
    in the grid overlay.
    
    The logic is in BuildGridNegativeLineNumberOffsets and is similar to
    how we do it for positive line numbers in
    BuildGridPositiveLineNumberOffsets.
    
    The difference is negative line numbers always start at the first line
    (from the top or left in default writing direction) and go to the last
    explicit line.
    
    This CL has a corresponding frontend CL:
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2239367
    
    With the frontend CL applied: https://i.imgur.com/riUXwz4.jpg
    
    Bug: 1047356
    Change-Id: I9b1a2f4ab50f5a926081f6aff1d18406be4df07a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2242952
    Commit-Queue: Patrick Brosset <patrick.brosset@microsoft.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Javier Fernandez <jfernandez@igalia.com>
    Reviewed-by: Brandon Goddard <brgoddar@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#778729}

--- third_party/blink/renderer/core/layout/layout_grid.h
+++ third_party/blink/renderer/core/layout/layout_grid.h
@@ -119,6 +119,8 @@ class LayoutGrid final : public LayoutBlock {
 
   StyleContentAlignmentData ContentAlignment(GridTrackSizingDirection) const;
 
+  size_t ExplicitGridEndForDirection(GridTrackSizingDirection) const;
+
   // Exposed for testing *ONLY*.
   Grid* InternalGrid() const { return grid_.get(); }
 
