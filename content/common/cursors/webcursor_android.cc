a9de52cab399e3267839f3090038050ba27beccd
Henrique Ferreiro <hferreiro@igalia.com>
2020-06-16 12:21:52 +0000

    Remove WebCursor::IsPlatformDataEqual()
    All platform's implementation of this method are equal and just return
    true.
    
    Bug: 1029142.
    Change-Id: I16e73326e904040e48c6d217e008b78bed624895
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245630
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778750}

--- content/common/cursors/webcursor_android.cc
+++ content/common/cursors/webcursor_android.cc
@@ -19,10 +19,6 @@ gfx::NativeCursor WebCursor::GetNativeCursor() {
 void WebCursor::SetDisplayInfo(const display::Display& display) {}
 #endif
 
-bool WebCursor::IsPlatformDataEqual(const WebCursor& other) const {
-  return true;
-}
-
 void WebCursor::CleanupPlatformData() {}
 
 void WebCursor::CopyPlatformData(const WebCursor& other) {}
