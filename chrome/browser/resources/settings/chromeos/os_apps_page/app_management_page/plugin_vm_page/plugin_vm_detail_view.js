0b97aba4b8494d6dbb93d76cb778178ba24d6cff
Dmitry Torokhov <dtor@chromium.org>
2020-06-16 21:02:21 +0000

    plugin_vm: Add UI and flag for microphone permission
    This adds microphone to the list of permissions for Plugin VM. It is
    hidden behind a feature flag for now.
    
    Bug: chromium:1071872
    Change-Id: Ibd54d52ac0d89ddda1f4c39b350d3c348694faa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2244574
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Reviewed-by: Greg Kerr <kerrnel@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Ng <danielng@google.com>
    Commit-Queue: Dmitry Torokhov <dtor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778988}

--- chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/plugin_vm_page/plugin_vm_detail_view.js
+++ chrome/browser/resources/settings/chromeos/os_apps_page/app_management_page/plugin_vm_page/plugin_vm_detail_view.js
@@ -26,6 +26,17 @@ Polymer({
         return loadTimeData.getBoolean('showPluginVmCameraPermissions');
       },
     },
+
+    /**
+     * Whether the microphone permissions should be shown.
+     * @private {boolean}
+     */
+    showMicrophonePermissions_: {
+      type: Boolean,
+      value() {
+        return loadTimeData.getBoolean('showPluginVmMicrophonePermissions');
+      },
+    },
   },
 
   attached() {
