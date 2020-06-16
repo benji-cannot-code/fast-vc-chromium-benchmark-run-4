0ba401f8d2fb591838dd26edda016f32866fef97
Maggie Cai <mxcai@chromium.org>
2020-06-16 08:06:22 +0000

    [IntentHandling] Only update ARC action send preferred apps when flag is on.
    This CL skips the add/delete of preferred apps from ARC for action send
    and send multiple when the IntentHandlingSharing flag is off. Otherwise
    this might generate empty intent filters that matches with all intents.
    
    BUG=1092784
    
    Change-Id: Iee7e98e936d1706c610253765b5ea56c8e654b4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247462
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778693}

--- chrome/browser/apps/app_service/arc_apps.cc
+++ chrome/browser/apps/app_service/arc_apps.cc
@@ -427,6 +427,16 @@ void RequestDomainVerificationStatusUpdate(ArcAppListPrefs* prefs) {
   instance->RequestDomainVerificationStatusUpdate();
 }
 
+bool ShouldSkipFilter(const arc::IntentFilter& arc_intent_filter) {
+  return !base::FeatureList::IsEnabled(features::kIntentHandlingSharing) &&
+         std::any_of(arc_intent_filter.actions().begin(),
+                     arc_intent_filter.actions().end(),
+                     [](const std::string& action) {
+                       return action == arc::kIntentActionSend ||
+                              action == arc::kIntentActionSendMultiple;
+                     });
+}
+
 }  // namespace
 
 namespace apps {
@@ -987,6 +997,10 @@ void ArcApps::OnPreferredAppsChanged() {
       intent_helper_bridge->GetAddedPreferredApps();
 
   for (auto& added_preferred_app : added_preferred_apps) {
+    if (ShouldSkipFilter(added_preferred_app)) {
+      continue;
+    }
+
     constexpr bool kFromPublisher = true;
     // TODO(crbug.com/853604): Currently only handles one App ID per package.
     // If need to handle multiple activities per package, will need to
@@ -1007,6 +1021,9 @@ void ArcApps::OnPreferredAppsChanged() {
       intent_helper_bridge->GetDeletedPreferredApps();
 
   for (auto& deleted_preferred_app : deleted_preferred_apps) {
+    if (ShouldSkipFilter(deleted_preferred_app)) {
+      continue;
+    }
     // TODO(crbug.com/853604): Currently only handles one App ID per package.
     // If need to handle multiple activities per package, will need to
     // update ARC to send through the corresponding activity and ensure this
@@ -1270,13 +1287,7 @@ void ArcApps::UpdateAppIntentFilters(
   const std::vector<arc::IntentFilter>& arc_intent_filters =
       intent_helper_bridge->GetIntentFilterForPackage(package_name);
   for (auto& arc_intent_filter : arc_intent_filters) {
-    if (!base::FeatureList::IsEnabled(features::kIntentHandlingSharing) &&
-        std::any_of(arc_intent_filter.actions().begin(),
-                    arc_intent_filter.actions().end(),
-                    [](const std::string& action) {
-                      return action == arc::kIntentActionSend ||
-                             action == arc::kIntentActionSendMultiple;
-                    })) {
+    if (ShouldSkipFilter(arc_intent_filter)) {
       continue;
     }
     intent_filters->push_back(ConvertArcIntentFilter(arc_intent_filter));
