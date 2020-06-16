509bb308dac08863c5e9b1e76163c5a4ff1198ee
Khushal Sagar <khushalsagar@chromium.org>
2020-06-16 18:17:28 +0000

    cc: Ensure img.decode API with a bitmap backed image works.
    We currently assume that a decode task can always be created for every
    decode request. But this is not the case for bitmaps (where no decoding
    is needed) or hardware accelerated decodes. Ensure that this case is
    handled correctly.
    
    R=cblume@chromium.org
    
    Bug: 1083100
    Change-Id: Idf3812ee8005369d335ff8887391ea3ad1ca0735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246796
    Reviewed-by: Chris Blume <cblume@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778892}

--- cc/tiles/gpu_image_decode_cache_unittest.cc
+++ cc/tiles/gpu_image_decode_cache_unittest.cc
@@ -430,7 +430,6 @@ class GpuImageDecodeCacheTest
   }
 
   PaintImage CreateBitmapImageInternal(const gfx::Size& size) {
-    DCHECK(!do_yuv_decode_);
     return CreateBitmapImage(size, color_type_);
   }
 
@@ -2926,6 +2925,20 @@ TEST_P(GpuImageDecodeCacheTest, GetBorderlineLargeDecodedImageForDraw) {
   EXPECT_FALSE(cache->DiscardableIsLockedForTesting(draw_image));
 
   cache->DrawWithImageFinished(draw_image, decoded_draw_image);
+  cache->UnrefImage(draw_image);
+}
+
+TEST_P(GpuImageDecodeCacheTest, OutOfRasterDecodeForBitmaps) {
+  auto cache = CreateCache();
+
+  PaintImage image = CreateBitmapImageInternal(GetNormalImageSize());
+  DrawImage draw_image = CreateDrawImageInternal(image);
+  ImageDecodeCache::TaskResult result =
+      cache->GetOutOfRasterDecodeTaskForImageAndRef(draw_image);
+  EXPECT_TRUE(result.need_unref);
+  EXPECT_FALSE(result.task);
+  EXPECT_FALSE(result.is_at_raster_decode);
+  EXPECT_FALSE(result.can_do_hardware_accelerated_decode);
   cache->UnrefImage(draw_image);
 }
 
