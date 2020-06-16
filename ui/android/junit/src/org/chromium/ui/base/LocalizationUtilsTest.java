b63389590f8cce1cc90bbe2bd0650b4d357e4980
Ben Joyce <bjoyce@google.com>
2020-06-16 14:02:18 +0000

    Migrate test filters to androidx.
    Bug: 1005891
    Change-Id: If167ef14eebdb488bd9bd08e7ce931929f057c10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247161
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778774}

--- ui/android/junit/src/org/chromium/ui/base/LocalizationUtilsTest.java
+++ ui/android/junit/src/org/chromium/ui/base/LocalizationUtilsTest.java
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.ui.base;
 
-import android.support.test.filters.SmallTest;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import androidx.test.filters.SmallTest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
