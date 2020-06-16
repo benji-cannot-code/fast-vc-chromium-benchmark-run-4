59f063842724e203802b62fa5d0a5882e1345043
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-06-16 18:54:14 +0000

    [iOS][a11y] Add accessibility label to more info button
    Add a11y label to the ConfirmationAlertViewController help button.
    
    Merge after: https://chromium-review.googlesource.com/c/chromium/src/+/2241999
    
    Bug: N/A
    Change-Id: I5387d0cc732aee368c91d612ba3b2a6fab8e1bf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241972
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778914}

--- ios/chrome/browser/ui/passwords/password_breach_view_controller.mm
+++ ios/chrome/browser/ui/passwords/password_breach_view_controller.mm
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)loadView {
   self.image = [UIImage imageNamed:@"password_breach_illustration"];
   self.helpButtonAvailable = YES;
+  self.helpButtonAccessibilityLabel =
+      l10n_util::GetNSString(IDS_IOS_HELP_ACCESSIBILITY_LABEL);
 
 #if defined(__IPHONE_13_4)
   if (@available(iOS 13.4, *)) {
