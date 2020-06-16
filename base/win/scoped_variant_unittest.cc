2ec674cad090f84ecb6557d8feb215a19f613541
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2020-06-16 23:16:48 +0000

    Move DispatchStub out of scoped_variant_unittest.cc
    Minor change to move the test-only class DispatchStub into its
    own file so it may be shared between multiple unittest files.
    
    Split out from :
    https://chromium-review.googlesource.com/c/chromium/src/+/2188057
    
    Bug: 928948
    Change-Id: I8bf6338304a1ddb90c4696c98b15b58cc49e3c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2244453
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#779066}

--- base/win/scoped_variant_unittest.cc
+++ base/win/scoped_variant_unittest.cc
@@ -9,8 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/win/dispatch_stub.h"
 #include "base/win/scoped_variant.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+using base::win::test::DispatchStub;
 
 namespace base {
 namespace win {
@@ -28,41 +31,6 @@ void InitializeVariantWithBstr(VARIANT* var) {
   var->vt = VT_BSTR;
   V_BSTR(var) = ::SysAllocString(kTestString);
 }
-
-// An unimplemented IDispatch subclass.
-class DispatchStub
-    : public Microsoft::WRL::RuntimeClass<
-          Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>,
-          IDispatch> {
- public:
-  DispatchStub() = default;
-  DispatchStub(const DispatchStub&) = delete;
-  DispatchStub& operator=(const DispatchStub&) = delete;
-
-  // IDispatch:
-  IFACEMETHODIMP GetTypeInfoCount(UINT*) override { return E_NOTIMPL; }
-  IFACEMETHODIMP GetTypeInfo(UINT, LCID, ITypeInfo**) override {
-    return E_NOTIMPL;
-  }
-  IFACEMETHODIMP GetIDsOfNames(REFIID,
-                               LPOLESTR*,
-                               UINT,
-                               LCID,
-                               DISPID*) override {
-    return E_NOTIMPL;
-  }
-
-  IFACEMETHODIMP Invoke(DISPID,
-                        REFIID,
-                        LCID,
-                        WORD,
-                        DISPPARAMS*,
-                        VARIANT*,
-                        EXCEPINFO*,
-                        UINT*) override {
-    return E_NOTIMPL;
-  }
-};
 
 void ExpectRefCount(ULONG expected_refcount, IUnknown* object) {
   // In general, code should not check the values of AddRef() and Release().
