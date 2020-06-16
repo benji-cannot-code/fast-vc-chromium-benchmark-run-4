27e1c4158f3070626630387843930bdfad6e6441
Sebastien Lalancette <seblalancette@chromium.org>
2020-06-16 16:00:09 +0000

    [iOS][QRCode] Add Help Button Accessibility Label
    Couldn't let ConfirmationAlertViewController set the accessibility label
    as, at it's directory location, it doesn't have access to localized
    strings.
    
    Bug: 1093930
    Change-Id: I29778304f3e25e74d1ae2c494115e90c90f6eed3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241999
    Commit-Queue: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778831}

--- ios/chrome/browser/ui/qr_generator/qr_generator_coordinator.mm
+++ ios/chrome/browser/ui/qr_generator/qr_generator_coordinator.mm
@@ -79,7 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.viewController setPageURL:net::NSURLWithGURL(_URL)];
   [self.viewController setTitleString:self.title];
   [self.viewController setActionHandler:self];
-  [self.viewController setHelpButtonAvailable:YES];
 
   [self.baseViewController presentViewController:self.viewController
                                         animated:YES
