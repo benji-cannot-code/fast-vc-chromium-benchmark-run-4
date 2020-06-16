69e5a4a508425aa60cde32f4692dd379ff558af6
Megan Jablonski <megjablon@chromium.org>
2020-06-16 22:26:39 +0000

    Only reset the target language when removed from list
    Bug: 995977
    Change-Id: I2abc391e5cde840e7ecb7ecda460a264cb6eb5e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247243
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779035}

--- chrome/browser/extensions/api/language_settings_private/language_settings_private_api.cc
+++ chrome/browser/extensions/api/language_settings_private/language_settings_private_api.cc
@@ -334,7 +334,6 @@ LanguageSettingsPrivateEnableLanguageFunction::Run() {
   }
 
   translate_prefs->AddToLanguageList(language_code, /*force_blocked=*/false);
-  translate_prefs->ResetRecentTargetLanguage();
 
   return RespondNow(NoArguments());
 }
@@ -368,7 +367,9 @@ LanguageSettingsPrivateDisableLanguageFunction::Run() {
   }
 
   translate_prefs->RemoveFromLanguageList(language_code);
-  translate_prefs->ResetRecentTargetLanguage();
+  if (language_code == translate_prefs->GetRecentTargetLanguage()) {
+    translate_prefs->ResetRecentTargetLanguage();
+  }
 
   return RespondNow(NoArguments());
 }
@@ -398,7 +399,6 @@ LanguageSettingsPrivateSetEnableTranslationForLanguageFunction::Run() {
   } else {
     translate_prefs->BlockLanguage(language_code);
   }
-  translate_prefs->ResetRecentTargetLanguage();
 
   return RespondNow(NoArguments());
 }
@@ -451,7 +451,6 @@ LanguageSettingsPrivateMoveLanguageFunction::Run() {
   const int offset = 1;
   translate_prefs->RearrangeLanguage(language_code, where, offset,
                                      supported_language_codes);
-  translate_prefs->ResetRecentTargetLanguage();
 
   return RespondNow(NoArguments());
 }
@@ -493,8 +492,8 @@ LanguageSettingsPrivateGetSpellcheckWordsFunction::Run() {
   return RespondLater();
 }
 
-void
-LanguageSettingsPrivateGetSpellcheckWordsFunction::OnCustomDictionaryLoaded() {
+void LanguageSettingsPrivateGetSpellcheckWordsFunction::
+    OnCustomDictionaryLoaded() {
   SpellcheckService* service =
       SpellcheckServiceFactory::GetForContext(browser_context());
   service->GetCustomDictionary()->RemoveObserver(this);
@@ -502,9 +501,9 @@ LanguageSettingsPrivateGetSpellcheckWordsFunction::OnCustomDictionaryLoaded() {
   Release();
 }
 
-void
-LanguageSettingsPrivateGetSpellcheckWordsFunction::OnCustomDictionaryChanged(
-    const SpellcheckCustomDictionary::Change& dictionary_change) {
+void LanguageSettingsPrivateGetSpellcheckWordsFunction::
+    OnCustomDictionaryChanged(
+        const SpellcheckCustomDictionary::Change& dictionary_change) {
   NOTREACHED() << "SpellcheckCustomDictionary::Observer: "
                   "OnCustomDictionaryChanged() called before "
                   "OnCustomDictionaryLoaded()";
@@ -579,8 +578,7 @@ LanguageSettingsPrivateRemoveSpellcheckWordFunction::Run() {
 
 LanguageSettingsPrivateGetTranslateTargetLanguageFunction::
     LanguageSettingsPrivateGetTranslateTargetLanguageFunction()
-    : chrome_details_(this) {
-}
+    : chrome_details_(this) {}
 
 LanguageSettingsPrivateGetTranslateTargetLanguageFunction::
     ~LanguageSettingsPrivateGetTranslateTargetLanguageFunction() = default;
@@ -658,8 +656,7 @@ LanguageSettingsPrivateGetInputMethodListsFunction::Run() {
     InputMethodDescriptors ext_ime_descriptors;
     ime_state->GetInputMethodExtensions(&ext_ime_descriptors);
     PopulateInputMethodListFromDescriptors(
-        ext_ime_descriptors,
-        &input_method_lists.third_party_extension_imes);
+        ext_ime_descriptors, &input_method_lists.third_party_extension_imes);
   }
 
   return RespondNow(OneArgument(input_method_lists.ToValue()));
