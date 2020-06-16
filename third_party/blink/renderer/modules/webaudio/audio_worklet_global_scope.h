48caedad0dff5254ff27e068ecfb4e0e472847ae
Hongchan Choi <hongchan@chromium.org>
2020-06-16 22:49:40 +0000

    Avoid memory allocation in AudioWorkletProcessor.process()
    Based on the spec [1], the current implementation of
    AudioWorkletProcessor needs to create a new data container
    (i.e. WebIDL sequence<>) for input, output, and param arrays.
    
    With the new spec change [2], this CL changes the overall design
    of the audio processing callback:
    
    1. Moves the processing call from AudioWorkletGlobalScope to
       AudioWorkletProcessor object.
    2. AudioWorkletProcessor now keeps the data container within
       the object and allocate memory when it is needed.
    
    The preliminary benchmark shows the sizable improvement in the
    audio stream quality. The glitch score
    (= buffer underrun/total callback) is improved by ~9x in the
    low-tier machine. [3]
    
    This is an API change [4], but the real world impact would be
    negligible because there's no functionality change.
    
    [1]: https://webaudio.github.io/web-audio-api/#dom-audioworkletprocessor-process-inputs-outputs-parameters-inputs
    [2]: https://github.com/WebAudio/web-audio-api/issues/1933#issuecomment-616632754
    [3]: https://bugs.chromium.org/p/chromium/issues/detail?id=1086665#c2
    [4]: https://chromestatus.com/feature/5647541725036544
    
    
    Bug: 1071085, 1086665
    Change-Id: I3e664754973d4d86649d38c1807c6b9d7830fb96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218702
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779052}

--- third_party/blink/renderer/modules/webaudio/audio_worklet_global_scope.h
+++ third_party/blink/renderer/modules/webaudio/audio_worklet_global_scope.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class AudioBus;
 class AudioWorkletProcessor;
 class AudioWorkletProcessorDefinition;
 class CrossThreadAudioWorkletProcessorInfo;
@@ -77,14 +76,6 @@ class MODULES_EXPORT AudioWorkletGlobalScope final : public WorkletGlobalScope {
       const String& name,
       MessagePortChannel,
       scoped_refptr<SerializedScriptValue> node_options);
-
-  // Invokes the JS audio processing function from an instance of
-  // AudioWorkletProcessor, along with given AudioBuffer from the audio graph.
-  bool Process(
-      AudioWorkletProcessor*,
-      Vector<scoped_refptr<AudioBus>>* input_buses,
-      Vector<AudioBus*>* output_buses,
-      HashMap<String, std::unique_ptr<AudioFloatArray>>* param_value_map);
 
   AudioWorkletProcessorDefinition* FindDefinition(const String& name);
 
