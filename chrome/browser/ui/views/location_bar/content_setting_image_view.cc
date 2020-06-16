821d5486ad67e5020dd24b5224a9587f15c453d6
Mohamed Amir Yosef <mamir@chromium.org>
2020-06-16 10:44:23 +0000

    [UI] Add title support to feature promo bubbles
    When used for the password bubble, this is how it looks:
    Screenshot:https://screenshot.googleplex.com/NdctJthyEtR
    
    
    Bug: 1044038
    Change-Id: I7fd3e5718695fdc904ba92b4b39a498d837db9b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246580
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778730}

--- chrome/browser/ui/views/location_bar/content_setting_image_view.cc
+++ chrome/browser/ui/views/location_bar/content_setting_image_view.cc
@@ -89,8 +89,7 @@ ContentSettingImageView::ContentSettingImageView(
     SetID(*view_id);
 }
 
-ContentSettingImageView::~ContentSettingImageView() {
-}
+ContentSettingImageView::~ContentSettingImageView() {}
 
 void ContentSettingImageView::Update() {
   content::WebContents* web_contents =
@@ -243,6 +242,7 @@ void ContentSettingImageView::AnimationEnded(const gfx::Animation* animation) {
     indicator_promo_ = FeaturePromoBubbleView::CreateOwned(
         this, views::BubbleBorder::TOP_RIGHT,
         FeaturePromoBubbleView::ActivationAction::ACTIVATE,
+        /*title_string_specifier=*/base::nullopt,
         IDS_NOTIFICATIONS_QUIET_PERMISSION_NEW_REQUEST_PROMO, promo_width,
         base::nullopt, base::nullopt);
 
