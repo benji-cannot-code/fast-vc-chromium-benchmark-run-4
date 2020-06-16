2c315d81465f106fb13c5d16e8826e4bb3f199c2
Francois Beaufort <beaufort.francois@gmail.com>
2020-06-16 19:12:12 +0000

    [PTZ] Add pan_tilt_zoom_supported to VideoInputDeviceCapabilities
    This CL simply adds a boolean pan_tilt_zoom_supported to
    VideoInputDeviceCapabilities that will be used in an upcoming CL
    in getUserMedia constraints processing.
    
    Change-Id: I5b271eefea52d6c9293476d15a0c102342f4bb9d
    Bug: 934063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245900
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/master@{#778922}

--- third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_video_device.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_video_device.h
@@ -33,7 +33,8 @@ struct MODULES_EXPORT VideoInputDeviceCapabilities {
   VideoInputDeviceCapabilities(String device_id,
                                String group_id,
                                Vector<media::VideoCaptureFormat> formats,
-                               media::VideoFacingMode facing_mode);
+                               media::VideoFacingMode facing_mode,
+                               bool pan_tilt_zoom_supported);
   VideoInputDeviceCapabilities();
   VideoInputDeviceCapabilities(VideoInputDeviceCapabilities&& other);
   VideoInputDeviceCapabilities& operator=(VideoInputDeviceCapabilities&& other);
@@ -43,6 +44,7 @@ struct MODULES_EXPORT VideoInputDeviceCapabilities {
   String group_id;
   Vector<media::VideoCaptureFormat> formats;
   media::VideoFacingMode facing_mode;
+  bool pan_tilt_zoom_supported;
 };
 
 struct MODULES_EXPORT VideoDeviceCaptureCapabilities {
