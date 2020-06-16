48c9702a2dccf48b9ff194fd091151987877c7ff
Eric Seckler <eseckler@chromium.org>
2020-06-16 17:49:40 +0000

    content/android: Reduce & move power UMA collection onto thread pool.
    With the addition of per-thread UMAs, the collection became a little bit
    more expensive. This patch moves it to a background thread and reduces
    the collection interval for the browser and GPU processes, which
    (i)  are persistent processes (so data loss at termination is less of
         an issue) and
    (ii) tend to execute many small main thread tasks.
    
    Bug: 1081760,1094434
    Change-Id: I847179ae78c024bb506772c1c42804e9d896f762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245906
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Bo <boliu@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778878}

--- content/common/android/cpu_time_metrics_unittest.cc
+++ content/common/android/cpu_time_metrics_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/synchronization/waitable_event.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/task_environment.h"
 #include "base/threading/thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -24,6 +25,7 @@ void WorkForOneCpuSec(base::WaitableEvent* event) {
 }
 
 TEST(CpuTimeMetricsTest, RecordsMetrics) {
+  base::test::TaskEnvironment task_environment;
   base::HistogramTester histograms;
   base::Thread thread1("StackSamplingProfiler");
 
