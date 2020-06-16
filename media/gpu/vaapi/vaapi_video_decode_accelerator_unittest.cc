7f269ac85d7cc8c27ba12a91f61767d97c1dce5b
Andres Calderon Jaramillo <andrescj@chromium.org>
2020-06-16 18:13:12 +0000

    VaVDA: Use BufferAllocationMode::kNone for H.264.
    This CL enables H.264 decoding with BufferAllocationMode::kNone. The
    main blocker for enabling this was increased CPU consumption due to busy
    work when there is an available surface but the corresponding picture
    buffer has not come back from the client (see [1]).
    
    This CL does not eliminate this busy loop. Instead, it makes sure that
    the number of requested buffers is at least 13 (picked to imitate the
    value returned by VP9Decoder::GetRequiredNumOfPictures()). This makes it
    more likely that the decoder can get a buffer without waiting on one to
    come back from the client.
    
    As validation, this is the output of dump_intel_rapl_consumption while
    playing
    http://crosvideo.appspot.com/?codec=h264&resolution=1080&loop=true on an
    i7 eve (with the new VD disabled):
    
    Before this CL:
    ========================================================================
    
     package-0       core     uncore       dram       psys
      2.667667   1.359338   0.188050   0.860169   2.832958
      1.492605   0.466284   0.179845   0.761450   1.859494
      2.465552   1.333110   0.173884   0.826528   2.429251
      1.836305   0.701381   0.175284   0.825935   2.083826
      1.397930   0.331338   0.175203   0.764998   1.783907
      1.453937   0.396079   0.175568   0.762897   1.804858
      1.617633   0.551016   0.172171   0.770288   1.930921
      1.398794   0.377560   0.178041   0.762288   1.761070
      1.309214   0.300763   0.171568   0.748048   1.685083
      4.094596   2.987037   0.221237   0.785434   3.152690
    
    After forcing BufferAllocationMode::kNone without any other changes:
    ========================================================================
    
     package-0       core     uncore       dram       psys
     10.091497   8.403499   0.137035   1.066655   6.793493
      6.107595   4.291023   0.142334   1.282748   4.898286
      4.173701   2.382291   0.132389   1.202908   3.855559
      4.276854   2.500189   0.126471   1.216745   3.911385
      4.716722   3.027957   0.134404   1.154900   4.138097
      4.547350   3.342606   0.137797   0.721388   3.806403
      5.852608   4.761959   0.133650   0.685367   4.167034
      6.440512   5.366580   0.130992   0.692277   4.334328
      6.471564   5.462571   0.135755   0.610058   4.239434
      6.522625   5.574209   0.130200   0.602120   4.179017
    
    After this CL:
    ========================================================================
    
     package-0       core     uncore       dram       psys
      7.261862   5.491319   0.183383   1.410826   5.420041
      5.723818   3.829009   0.142398   1.491444   4.720379
      4.364147   2.652774   0.137776   1.144099   3.956767
      1.597286   0.459902   0.141311   0.751768   2.134646
      1.428828   0.465833   0.140055   0.718547   1.791793
      2.307202   1.213556   0.141450   0.792714   2.334663
      1.753186   0.647150   0.136820   0.764778   2.088220
      1.276018   0.308548   0.134811   0.728569   1.546158
      1.285961   0.308340   0.140440   0.719776   1.522937
      1.579406   0.594378   0.131040   0.720050   1.698702
    
    Note that according to top -d 0.5, the CPU usage of the GPU process
    hovers at around 20%.
    
    ========================================================================
    
    Therefore, increasing the number of required buffers allows us to enable
    kNone without regressing on power consumption.
    
    [1] https://bugs.chromium.org/p/chromium/issues/detail?id=910986#c32
    
    Bug: 910986, 1054097
    Test: dump_intel_rapl_consumption on eve while playing 1080p H.264.
    Change-Id: Ib1aa566e2af54439d9c6c3339c522758c5f699a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246846
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#778890}

--- media/gpu/vaapi/vaapi_video_decode_accelerator_unittest.cc
+++ media/gpu/vaapi/vaapi_video_decode_accelerator_unittest.cc
@@ -38,10 +38,10 @@ struct TestParams {
 constexpr int32_t kBitstreamId = 123;
 constexpr size_t kInputSize = 256;
 
-constexpr size_t kNumPictures = 4;
+constexpr size_t kNumPictures = 14;
 const gfx::Size kPictureSize(64, 48);
 
-constexpr size_t kNewNumPictures = 3;
+constexpr size_t kNewNumPictures = 13;
 const gfx::Size kNewPictureSize(64, 48);
 
 MATCHER_P2(IsExpectedDecoderBuffer, data_size, decrypt_config, "") {
@@ -489,6 +489,7 @@ TEST_P(VaapiVideoDecodeAcceleratorTest,
 
 constexpr TestParams kTestCases[] = {
     {H264PROFILE_MIN, false /* decode_using_client_picture_buffers */},
+    {H264PROFILE_MIN, true /* decode_using_client_picture_buffers */},
     {VP8PROFILE_MIN, false /* decode_using_client_picture_buffers */},
     {VP9PROFILE_MIN, false /* decode_using_client_picture_buffers */},
     {VP9PROFILE_MIN, true /* decode_using_client_picture_buffers */}};
