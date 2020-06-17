b54760563cdbd7bd13a9f3a2913b62f206d002bc
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-06-17 04:44:07 +0000

    [CrOS settings] Migrate bluetooth_pairing_dialog/ to Polymer3
    Change-Id: Idad74ffbdf2e288b00edf67160a73cc274ea4da1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246785
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779168}

--- chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.cc
+++ chrome/browser/ui/webui/chromeos/bluetooth_pairing_dialog.cc
@@ -8,8 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chromeos/bluetooth_dialog_localized_strings_provider.h"
+#include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/bluetooth_pairing_dialog_resources.h"
+#include "chrome/grit/bluetooth_pairing_dialog_resources_map.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui.h"
@@ -20,6 +22,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 namespace {
+
+#if !BUILDFLAG(OPTIMIZE_WEBUI)
+namespace {
+const char kGeneratedPath[] =
+    "@out_folder@/gen/chrome/browser/resources/chromeos/"
+    "bluetooth_pairing_dialog/";
+}
+#endif
 
 constexpr int kBluetoothPairingDialogHeight = 375;
 
@@ -104,17 +114,17 @@ BluetoothPairingDialogUI::BluetoothPairingDialogUI(content::WebUI* web_ui)
 
   AddBluetoothStrings(source);
   source->AddLocalizedString("title", IDS_SETTINGS_BLUETOOTH_PAIR_DEVICE_TITLE);
-  source->UseStringsJs();
 #if BUILDFLAG(OPTIMIZE_WEBUI)
-  source->SetDefaultResource(IDR_BLUETOOTH_PAIRING_DIALOG_VULCANIZED_HTML);
-  source->AddResourcePath("crisper.js",
-                          IDR_BLUETOOTH_PAIRING_DIALOG_CRISPER_JS);
+  webui::SetupBundledWebUIDataSource(
+      source, "bluetooth_pairing_dialog.js",
+      IDR_BLUETOOTH_PAIRING_DIALOG_ROLLUP_JS,
+      IDR_BLUETOOTH_PAIRING_DIALOG_CONTAINER_HTML);
 #else
-  source->SetDefaultResource(IDR_BLUETOOTH_PAIRING_DIALOG_CONTAINER_HTML);
-  source->AddResourcePath("bluetooth_pairing_dialog.html",
-                          IDR_BLUETOOTH_PAIRING_DIALOG_HTML);
-  source->AddResourcePath("bluetooth_pairing_dialog.js",
-                          IDR_BLUETOOTH_PAIRING_DIALOG_JS);
+  webui::SetupWebUIDataSource(
+      source,
+      base::make_span(kBluetoothPairingDialogResources,
+                      kBluetoothPairingDialogResourcesSize),
+      kGeneratedPath, IDR_BLUETOOTH_PAIRING_DIALOG_CONTAINER_HTML);
 #endif
   content::WebUIDataSource::Add(Profile::FromWebUI(web_ui), source);
 }
