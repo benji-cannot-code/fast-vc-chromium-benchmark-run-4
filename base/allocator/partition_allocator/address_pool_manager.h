e2b88a860de21f63dc7e9aa74100f06e5fcfc411
Bartek Nowierski <bartekn@chromium.org>
2020-06-17 05:29:55 +0000

    Remove make_unique from GigaCage implementation
    Having make_unique (which calls malloc) won't work well once malloc
    starts using PartitionAlloc, due to reentrancy.
    
    Bug: 1086388
    Change-Id: I037420ae3fa41f6340da38682c6a210b2b1ce33b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246368
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779190}

--- base/allocator/partition_allocator/address_pool_manager.h
+++ base/allocator/partition_allocator/address_pool_manager.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_ALLOCATOR_PARTITION_ALLOCATOR_ADDRESS_POOL_MANAGER_H_
 
 #include <bitset>
-#include <memory>
 
 #include "base/allocator/partition_allocator/partition_alloc_constants.h"
 #include "base/atomicops.h"
@@ -50,8 +49,12 @@ class BASE_EXPORT AddressPoolManager {
 
   class Pool {
    public:
-    Pool(uintptr_t ptr, size_t length);
+    Pool();
     ~Pool();
+
+    void Initialize(uintptr_t ptr, size_t length);
+    bool IsInitialized();
+    void Reset();
 
     uintptr_t FindChunk(size_t size);
     void FreeChunk(uintptr_t address, size_t size);
@@ -64,26 +67,23 @@ class BASE_EXPORT AddressPoolManager {
     static constexpr size_t kMaxBits = kMaxSupportedSize / kSuperPageSize;
     base::Lock lock_;
     std::bitset<kMaxBits> alloc_bitset_ GUARDED_BY(lock_);
-
-    const size_t total_bits_;
-    const uintptr_t address_begin_;
-#if DCHECK_IS_ON()
-    const uintptr_t address_end_;
-#endif
-
     // An index of a bit in the bitset before which we know for sure there all
     // 1s. This is a best-effort hint in the sense that there still may be lots
     // of 1s after this index, but at least we know there is no point in
     // starting the search before it.
-    size_t bit_hint_;
+    size_t bit_hint_ GUARDED_BY(lock_);
 
-    DISALLOW_COPY_AND_ASSIGN(Pool);
+    size_t total_bits_ = 0;
+    uintptr_t address_begin_ = 0;
+#if DCHECK_IS_ON()
+    uintptr_t address_end_ = 0;
+#endif
   };
 
   ALWAYS_INLINE Pool* GetPool(pool_handle handle);
 
   static constexpr size_t kNumPools = 2;
-  std::unique_ptr<Pool> pools_[kNumPools];
+  Pool pools_[kNumPools];
 
   friend class NoDestructor<AddressPoolManager>;
   DISALLOW_COPY_AND_ASSIGN(AddressPoolManager);
