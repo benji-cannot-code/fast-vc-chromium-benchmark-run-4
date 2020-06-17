89abb76138c084a1362116452848fcdf71d0a6f0
Hans Wennborg <hans@chromium.org>
2020-06-17 17:58:48 +0000

    Include-what-you-use related to logging.h (courgette)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    Bug: 1031540
    Change-Id: I0e8ccb83f217a2c7861373b2a967a3eb49922dbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249759
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779400}

--- courgette/label_manager.cc
+++ courgette/label_manager.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/check.h"
+#include "base/check_op.h"
 #include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/numerics/safe_math.h"
