4c356c5c5e33e73a12b77dfd61bd6354124363f4
Jose Dapena Paz <jdapena@igalia.com>
2020-06-17 10:42:01 +0000

    IWYU: include memory in ManagementService
    std::unique_ptr requires including <memory>.
    
    Bug: 819294
    Change-Id: I0da9f4affbdfd7a55f54e0e1f744b8f4483d386b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248678
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#779254}

--- components/policy/core/common/management/management_service.h
+++ components/policy/core/common/management/management_service.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_POLICY_CORE_COMMON_MANAGEMENT_MANAGEMENT_SERVICE_H_
 #define COMPONENTS_POLICY_CORE_COMMON_MANAGEMENT_MANAGEMENT_SERVICE_H_
 
+#include <memory>
+
 #include "base/containers/flat_set.h"
 #include "components/policy/policy_export.h"
 
