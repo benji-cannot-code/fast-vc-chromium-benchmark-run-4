8ab9418dc432ac7b0f7eccfcd4eb82f6833a3e61
Hans Wennborg <hans@chromium.org>
2020-06-17 16:58:50 +0000

    Include-what-you-use related to logging.h (ipc)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    TBR=kinuko
    
    Bug: 1031540
    Change-Id: I11f6bd375b17f0d62f50c960756c07dd20d102e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247895
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/master@{#779370}

--- ipc/ipc_message_attachment.cc
+++ ipc/ipc_message_attachment.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/scoped_file.h"
 #include "base/logging.h"
+#include "base/notreached.h"
 #include "ipc/ipc_mojo_handle_attachment.h"
 #include "mojo/public/cpp/system/platform_handle.h"
 
--- ipc/ipc_mojo_param_traits.cc
+++ ipc/ipc_mojo_param_traits.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ipc/ipc_mojo_param_traits.h"
 
+#include "base/logging.h"
 #include "ipc/ipc_message_utils.h"
 #include "ipc/ipc_mojo_handle_attachment.h"
 #include "ipc/ipc_mojo_message_helper.h"
