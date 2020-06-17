3d54816f7e92a2b345ff7dacab8b28f0770dfad4
David Maunder <davidjm@chromium.org>
2020-06-17 00:31:24 +0000

    Cache Callbacks in PersistedTabData
    PersistedTabData provides a mechanism for acquiring serialized UserData
    objects in storage and attaching them to a Tab. The caching mechanism
    provides the capability to cache callbacks acquiring the same data
    which yields a performance improvement. It also means we avoid a bug
    of the following:
    1 FooTabData.from(...)
    2 FooTabData.from(...)
    3 setUserData changing what is stored on FooTabData
    4 2) returning after 3) and changing what is stored on FooTabData
    
    Bug: 1059602
    Change-Id: Ib3534922ace1a653068caa17a78a7eb46d307dd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236045
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779097}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/state/PersistedTabData.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/state/PersistedTabData.java
@@ -9,9 +9,16 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.UserData;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.tab.Tab;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * PersistedTabData is Tab data persisted across restarts
@@ -23,6 +30,7 @@ import org.chromium.chrome.browser.tab.Tab;
  */
 public abstract class PersistedTabData implements UserData {
     private static final String TAG = "PTD";
+    private static final Map<String, List<Callback>> sCachedCallbacks = new HashMap<>();
     protected final Tab mTab;
     private final PersistedTabDataStorage mPersistedTabDataStorage;
     private final String mPersistedTabDataId;
@@ -68,6 +76,7 @@ public abstract class PersistedTabData implements UserData {
     protected static <T extends PersistedTabData> void from(Tab tab,
             PersistedTabDataFactory<T> factory, Supplier<T> supplier, Class<T> clazz,
             Callback<T> callback) {
+        ThreadUtils.assertOnUiThread();
         // TODO(crbug.com/1059602) cache callbacks
         T persistedTabDataFromTab = getUserData(tab, clazz);
         if (persistedTabDataFromTab != null) {
@@ -75,6 +84,10 @@ public abstract class PersistedTabData implements UserData {
             callback.onResult(persistedTabDataFromTab);
             return;
         }
+        String key = String.format(Locale.ENGLISH, "%d-%s", tab.getId(), clazz.toString());
+        addCallback(key, callback);
+        // Only load data for the same key once
+        if (sCachedCallbacks.get(key).size() > 1) return;
         PersistedTabDataConfiguration config =
                 PersistedTabDataConfiguration.get(clazz, tab.isIncognito());
         config.storage.restore(tab.getId(), config.id, (data) -> {
@@ -87,8 +100,18 @@ public abstract class PersistedTabData implements UserData {
             if (persistedTabData != null) {
                 setUserData(tab, clazz, persistedTabData);
             }
-            callback.onResult(persistedTabData);
+            for (Callback cachedCallback : sCachedCallbacks.get(key)) {
+                cachedCallback.onResult(persistedTabData);
+            }
+            sCachedCallbacks.remove(key);
         });
+    }
+
+    private static <T extends PersistedTabData> void addCallback(String key, Callback<T> callback) {
+        if (!sCachedCallbacks.containsKey(key)) {
+            sCachedCallbacks.put(key, new LinkedList<>());
+        }
+        sCachedCallbacks.get(key).add(callback);
     }
 
     /**
