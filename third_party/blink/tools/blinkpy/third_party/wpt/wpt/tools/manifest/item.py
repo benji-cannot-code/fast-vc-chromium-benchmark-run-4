18a68f52c4a349b474faccf2af1dde3beb1f8eac
Stephen McGruer <smcgruer@chromium.org>
2020-06-17 15:24:27 +0000

    Revert "Roll WPT internal tools to 3016dfa710e2114595431f6506f1c7d5729c2484"
    This reverts commit f1e5e2aa470dbf008f02035b9c5c48cbf82b8242.
    
    Reason for revert: Broke WPT importer: crbug.com/1095643
    
    TBR=robertma@chromium.org
    
    Change-Id: Ice2e5f837095b2ea09c48bef7f6565d145d0d32c
    
    Bug: 1095643
    Change-Id: Ice2e5f837095b2ea09c48bef7f6565d145d0d32c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248061
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Robert Ma <robertma@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779324}

--- third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/manifest/item.py
+++ third_party/blink/tools/blinkpy/third_party/wpt/wpt/tools/manifest/item.py
@@ -303,12 +303,6 @@ class RefTest(URLManifestItem):
                    **extras)
 
 
-class PrintRefTest(RefTest):
-    __slots__ = ("references",)
-
-    item_type = "print-reftest"
-
-
 class ManualTest(URLManifestItem):
     __slots__ = ()
 
