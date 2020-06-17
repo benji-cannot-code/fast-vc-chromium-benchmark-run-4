4e127287b7810b791c50b11a4dac7adf7e5bba21
Hans Wennborg <hans@chromium.org>
2020-06-17 15:34:51 +0000

    Include-what-you-use related to logging.h (ppapi)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    Bug: 1031540
    Change-Id: Icaa271ea7e32c0494a7b3f578696e397e7cd0a48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249675
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779331}

--- ppapi/proxy/raw_var_data.cc
+++ ppapi/proxy/raw_var_data.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/raw_var_data.h"
 
 #include "base/containers/stack.h"
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/unsafe_shared_memory_region.h"
 #include "base/stl_util.h"
--- ppapi/shared_impl/resource_tracker.cc
+++ ppapi/shared_impl/resource_tracker.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/compiler_specific.h"
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "ppapi/shared_impl/callback_tracker.h"
 #include "ppapi/shared_impl/id_assignment.h"
