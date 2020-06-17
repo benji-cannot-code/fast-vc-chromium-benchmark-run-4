eb20f458f7f4b3062e891e44241bafc4f914adec
Hans Wennborg <hans@chromium.org>
2020-06-17 17:43:05 +0000

    Include-what-you-use related to logging.h (printing)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    Bug: 1031540
    Change-Id: I1d225674c41774f32ef8f3102e119a59e63d2b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247897
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779393}

--- printing/metafile.cc
+++ printing/metafile.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file.h"
+#include "base/logging.h"
 #include "base/numerics/safe_conversions.h"
 
 namespace printing {
