a43050f3739b6e02c6ee978d769d3f49c8ae82f4
Eugene But <eugenebut@chromium.org>
2020-06-17 20:39:58 +0000

    [ios][breadcrumbs] Log zoom and scroll events
    Some crashes required user to scroll the page in order to reproduce.
    This CL adds both zoom and scroll logging to breadcrumbs.
    Scroll events are throttled similarly to inforbar replacement to prevent
    pulluting breadcrumbs.
    
    Bug: 1083426
    Change-Id: Iccaca9acd42a8f4b2e8f34f1540ce1b5b0a0f43a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247398
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779448}

--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_browser_agent_unittest.mm
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_browser_agent_unittest.mm
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state_manager.h"
 #include "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_keyed_service.h"
 #include "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_keyed_service_factory.h"
-#include "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.h"
+#import "ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.h"
 #include "ios/chrome/browser/infobars/infobar_manager_impl.h"
 #import "ios/chrome/browser/main/test_browser.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
--- ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.h
+++ ios/chrome/browser/crash_report/breadcrumbs/breadcrumb_manager_tab_helper.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
 
+@protocol CRWWebViewScrollViewProxyObserver;
+
 namespace web {
 class WebState;
 }  // namespace web
@@ -39,6 +41,13 @@ extern const char kBreadcrumbInfobarRemoved[];
 // Name of OnInfoBarReplaced event
 // (see infobars::InfoBarManager::Observer::OnInfoBarReplaced).
 extern const char kBreadcrumbInfobarReplaced[];
+
+// Name of Scroll event, logged when web contents scroll view finishes
+// scrolling.
+extern const char kBreadcrumbScroll[];
+
+// Name of Zoom event, logged when web contents scroll view finishes zooming.
+extern const char kBreadcrumbZoom[];
 
 // Constants below represent metadata for breadcrumb events.
 
@@ -139,9 +148,16 @@ class BreadcrumbManagerTabHelper
   // is received.
   int sequentially_replaced_infobars_ = 0;
 
+  // A counter which is incremented for each scroll event. This value is reset
+  // when any other event is logged.
+  int sequentially_scrolled_ = 0;
+
   // Manages this object as an observer of infobars.
   ScopedObserver<infobars::InfoBarManager, infobars::InfoBarManager::Observer>
       infobar_observer_;
+
+  // Allows observing Objective-C object for Scroll and Zoom events.
+  __strong id<CRWWebViewScrollViewProxyObserver> scroll_observer_;
 
   WEB_STATE_USER_DATA_KEY_DECL();
 };
