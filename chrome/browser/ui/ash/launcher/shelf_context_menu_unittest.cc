f44966dd671290f5c20f8b43888d8fd7e37d2ec1
Eric Willigers <ericwilligers@chromium.org>
2020-06-17 08:56:23 +0000

    Desktop PWAs: Remove log message from ShelfContextMenuTest
    A debugging message was inadvertantly committed.
    
    TBR=dominickn@chromium.org
    
    Bug: 1086833
    Change-Id: I213a07dc739f252ac079a654868e1cbe1affad84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247949
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Eric Willigers <ericwilligers@chromium.org>
    Auto-Submit: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779231}

--- chrome/browser/ui/ash/launcher/shelf_context_menu_unittest.cc
+++ chrome/browser/ui/ash/launcher/shelf_context_menu_unittest.cc
@@ -254,7 +254,6 @@ class ShelfContextMenuTest
                                EXPECT_EQ(installed_app_id, app_id);
                                EXPECT_EQ(code,
                                          InstallResultCode::kSuccessNewInstall);
-                               LOG(ERROR) << "app installed";
                                run_loop.Quit();
                              }));
     run_loop.Run();
