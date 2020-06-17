1a0cf91270c99abad4e8bc44f42a5485940fd6ce
sczs <sczs@chromium.org>
2020-06-17 05:59:37 +0000

    [ios] Dismiss container banner when manager changes.
    When the InfobarManager will change dismiss the current Banner being
    presented in order to avoid the banner to be dismissed when the manager
    has already changed leading to some crashes.
    
    Bug: 1054024
    Change-Id: Ia85b09b0bff300887a896b1a1c0e5887735edaa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248859
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779198}

--- ios/chrome/browser/ui/infobars/infobar_container_coordinator.mm
+++ ios/chrome/browser/ui/infobars/infobar_container_coordinator.mm
@@ -244,6 +244,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)infobarManagerWillChange {
+  if (IsInfobarUIRebootEnabled()) {
+    [self dismissInfobarBannerAnimated:NO completion:nil];
+  }
   self.infobarCoordinators = [NSMutableArray array];
   self.infobarCoordinatorsToPresent = [NSMutableArray array];
 }
