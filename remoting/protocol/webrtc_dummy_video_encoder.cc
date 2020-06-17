d31673d13f8a4b6d21b72033fa6590cc650a69ee
Lambros Lambrou <lambroslambrou@chromium.org>
2020-06-17 04:47:26 +0000

    [remoting] Add VP9 profile-1 support.
    This adds a VP9 codec with profile-1 support (lossless-color) to the
    WebrtcDummyVideoEncoderFactory. This also prepares for Chrome
    profile-1 support by setting the video stream to be
    unidirectional (sendonly).
    
    Bug: 706220
    Change-Id: If3b86cebf998c5eda10e799a39373ad02d3ca774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240454
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779174}

--- remoting/protocol/webrtc_dummy_video_encoder.cc
+++ remoting/protocol/webrtc_dummy_video_encoder.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "remoting/protocol/video_channel_state_observer.h"
 #include "third_party/webrtc/api/video_codecs/sdp_video_format.h"
+#include "third_party/webrtc/media/base/vp9_profile.h"
 
 #if defined(USE_H264_ENCODER)
 #include "media/video/h264_parser.h"
@@ -236,7 +237,11 @@ WebrtcDummyVideoEncoderFactory::WebrtcDummyVideoEncoderFactory()
     : main_task_runner_(base::ThreadTaskRunnerHandle::Get()) {
   formats_.push_back(webrtc::SdpVideoFormat("VP8"));
   formats_.push_back(webrtc::SdpVideoFormat("VP9"));
+  formats_.push_back(
+      webrtc::SdpVideoFormat("VP9", {{webrtc::kVP9FmtpProfileId, "1"}}));
+#if defined(USE_H264_ENCODER)
   formats_.push_back(webrtc::SdpVideoFormat("H264"));
+#endif
 }
 
 WebrtcDummyVideoEncoderFactory::~WebrtcDummyVideoEncoderFactory() {
