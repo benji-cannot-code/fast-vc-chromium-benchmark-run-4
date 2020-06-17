933b9f12ad8920cd07b4fd864ae194864127d588
Rachel Carpenter <carpenterr@chromium.org>
2020-06-17 01:06:57 +0000

    chromeos: Create first-run field trial for the Help App.
    Creates a field trial to control the first run experience of the Help
    App. The trial is client controlled because the first run experience
    happens before a variations seed is available.
    
    The trial group chosen on first run is persisted to local state prefs
    and reused on subsequent runs. This keeps the behaviour of the help app
    stable between opens. Local state prefs can be reset via powerwash which
    will results in re-randomisation, but this also sends the user through
    the first-run flow again so their experience should be consistent from
    the end of the OOBE (out-of-box experience) onwards.
    
    Tracking bug: b/158540665
    Launch bug: https://crbug.com/1042109
    
    Tested: Manually, running through the first run experience a few times
    to determine that the group was randomly selected and correct experience
    happened for the given group.
    
    Bug: b/138969582
    Change-Id: Id843af832c0106fecfd6e4544f5c16fd5cb4933c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245158
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Rachel Carpenter <carpenterr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779118}

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/first_run/help_app_first_run_field_trial.h"
 #include "chrome/browser/chromeos/sync/split_settings_sync_field_trial.h"
 #include "chromeos/services/multidevice_setup/public/cpp/first_run_field_trial.h"
 #endif
@@ -93,9 +94,10 @@ void ChromeBrowserFieldTrials::SetupFeatureControllingFieldTrials(
 #endif
   }
 #if defined(OS_CHROMEOS)
-  // This trial is fully client controlled and must be configured whether or not
-  // a seed is available.
+  // These trials are fully client controlled and must be configured whether or
+  // not a seed is available.
   split_settings_sync_field_trial::Create(feature_list, local_state_);
+  help_app_first_run_field_trial::Create(feature_list, local_state_);
 #endif
 }
 
