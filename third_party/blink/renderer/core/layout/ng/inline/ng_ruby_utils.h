1828e70bc4a534a3514fae9c5bdc5aae3ffa7ea2
Kent Tamura <tkent@chromium.org>
2020-06-17 15:27:46 +0000

    RubyNG: Move ruby-related functions to ng_ruby_utils.{cc,h}
    Moves the following functions:
     - NGInlineLayoutAlgorithm::ComputeAnnotationOverflow()
     - LastLineTextLogicalBottom() in ng_block_layout_algorithm.cc
     - FirstLineTextLogicalTop()  in ng_block_layout_algorithm.cc
    
    This CL has no behavior changes.
    
    Bug: 1069817
    Change-Id: Ie59211a2644f81a4e43bcde5f7010f8765b2955a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249431
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779327}

--- third_party/blink/renderer/core/layout/ng/inline/ng_ruby_utils.h
+++ third_party/blink/renderer/core/layout/ng/inline/ng_ruby_utils.h
@@ -10,8 +10,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class ComputedStyle;
 class NGLineInfo;
+class NGLogicalLineItems;
+class NGPhysicalBoxFragment;
 struct NGInlineItemResult;
+struct NGLineHeightMetrics;
+
+// Returns the logical bottom offset of the last line text, relative to
+// |container| origin. This is used to decide ruby annotation box position.
+//
+// See NGBlockLayoutAlgorithm::LayoutRubyText().
+LayoutUnit LastLineTextLogicalBottom(const NGPhysicalBoxFragment& container,
+                                     LayoutUnit default_value);
+
+// Returns the logical top offset of the first line text, relative to
+// |container| origin. This is used to decide ruby annotation box position.
+//
+// See NGBlockLayoutAlgorithm::LayoutRubyText().
+LayoutUnit FirstLineTextLogicalTop(const NGPhysicalBoxFragment& container,
+                                   LayoutUnit default_value);
 
 struct NGAnnotationOverhang {
   LayoutUnit start;
@@ -37,6 +55,21 @@ bool CanApplyStartOverhang(const NGLineInfo& line_info,
 // This function may update a NGInlineItemResult representing RubyRun
 // in |line_info|
 LayoutUnit CommitPendingEndOverhang(NGLineInfo* line_info);
+
+// Compute over/under annotation overflow/space for the specified line.
+//
+// Return value:
+//   .ascent > 0: The amount of annotation overflow at the line-top side
+//   .ascent < 0: The amount of annotation space which the next line at the
+//                line-top side can consume.
+//   .descent > 0: The amount of annotation overflow at the line-bottom side.
+//   .descent < 0: The amount of annotation space which the next line at the
+//                 line-bottom side can consume.
+NGLineHeightMetrics ComputeAnnotationOverflow(
+    const NGLogicalLineItems& logical_line,
+    const NGLineHeightMetrics& line_box_metrics,
+    LayoutUnit line_block_start,
+    const ComputedStyle& line_style);
 
 }  // namespace blink
 
