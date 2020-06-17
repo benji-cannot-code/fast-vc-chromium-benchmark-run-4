974e6b953a80b60eb3ddc48ca30fbcb1716c3a11
Jan Krcal <jkrcal@chromium.org>
2020-06-17 14:47:34 +0000

    [Sync] Remove an obsolete histogram
    This CL removes an expired histogram in the Sync Directory code that
    is not used any more and will be deleted later in 2020.
    
    Bug: 1089542
    Change-Id: I07a9c4f18d7676944ade32200a1a795c4271484c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248571
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779316}

--- components/sync/syncable/directory.cc
+++ components/sync/syncable/directory.cc
@@ -1501,7 +1501,6 @@ void Directory::UnmarkDirtyEntry(WriteTransaction* trans, Entry* entry) {
 }
 
 void Directory::OnCatastrophicError() {
-  UMA_HISTOGRAM_BOOLEAN("Sync.DirectoryCatastrophicError", true);
   ReadTransaction trans(FROM_HERE, this);
   OnUnrecoverableError(&trans, FROM_HERE,
                        "Catastrophic error detected, Sync DB is unrecoverable");
