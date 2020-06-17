876248d73085bfcde607346947dd3aac692adb65
Hans Wennborg <hans@chromium.org>
2020-06-17 16:04:52 +0000

    Include-what-you-use related to logging.h (url)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    Bug: 1031540
    Change-Id: I3708de40ea110f27b8d3ab1e9e63df72cc2dba70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249765
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779344}

--- url/url_idna_icu.cc
+++ url/url_idna_icu.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 #include <string.h>
 
+#include <ostream>
+
 #include "base/check_op.h"
 #include "base/no_destructor.h"
 #include "third_party/icu/source/common/unicode/uidna.h"
