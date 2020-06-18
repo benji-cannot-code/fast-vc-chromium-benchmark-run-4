d929c295a08d8dfbca537d228e2a585efbe597b6
Hans Wennborg <hans@chromium.org>
2020-06-18 11:14:56 +0000

    Include-what-you-use related to logging.h (gin)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    Bug: 1031540
    Change-Id: I2b34d25846b971723bacb00e495ed219faa02e81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249761
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779766}

--- gin/data_object_builder_unittest.cc
+++ gin/data_object_builder_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/data_object_builder.h"
 
 #include "base/bind.h"
+#include "base/logging.h"
 #include "gin/dictionary.h"
 #include "gin/public/isolate_holder.h"
 #include "gin/test/v8_test.h"
