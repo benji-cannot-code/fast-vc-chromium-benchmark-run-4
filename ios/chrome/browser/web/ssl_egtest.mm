53eb77828975f76dff8343903c762729c47371c6
Ali Juma <ajuma@chromium.org>
2020-06-18 15:35:50 +0000

    [iOS] Don't remove navigation for committed SSL interstitial in didFailProvisionalNavigation
    The WKNavigation for a committed interstitial is used in
    the callback passed to ChromeWebClient::PrepareErrorPage, in
    order to commit the associated navigation item. For SSL
    interstitials, this callback is called later, asynchronously.
    This means that deleting the navigation in
    didFailProvisionalNavigation later leads to a crash when
    attempting to access the navigation's item, in the case
    where the error page load is triggered in
    didFailProvisionalNavigation. This happens in
    ErrorRetryStateMachine::DidFailProvisionalNavigation when
    the current webview URL is a restore_session.html URL.
    
    This CL fixes this crash by not deleting the navigation in
    didFailProvisionalNavigation when it is for a committed SSL
    interstital. There is already similar logic for deciding
    whether to delete a navigation in didFinishNavigation.
    
    Change-Id: Ib8aabeeeca33f63b313daa0772990b8b841d462a
    Bug: 1050808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248137
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Livvie Lin <livvielin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779833}

--- ios/chrome/browser/web/ssl_egtest.mm
+++ ios/chrome/browser/web/ssl_egtest.mm
@@ -28,6 +28,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation SSLTestCase
 
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  config.features_enabled.push_back(web::features::kSSLCommittedInterstitials);
+  config.relaunch_policy = NoForceRelaunchAndResetState;
+  return config;
+}
+
 - (void)setUp {
   [super setUp];
   _HTTPSServer = std::make_unique<net::test_server::EmbeddedTestServer>(
@@ -38,17 +45,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Test loading a page with a bad SSL certificate from the NTP, to avoid
 // https://crbug.com/1067250 from regressing.
 - (void)testBadSSLOnNTP {
-  if (!base::FeatureList::IsEnabled(
-          web::features::kSSLCommittedInterstitials)) {
-    // The content of the interstitial isn't in the webstate in that case.
-    EARL_GREY_TEST_SKIPPED(@"The test needs committed interstitials enabled.");
-  }
-
   GREYAssertTrue(_HTTPSServer->Start(), @"Test server failed to start.");
 
   const GURL pageURL = _HTTPSServer->GetURL("/echo");
   [ChromeEarlGrey loadURL:pageURL];
 
+  [ChromeEarlGrey waitForWebStateContainingText:l10n_util::GetStringUTF8(
+                                                    IDS_SSL_V2_HEADING)];
+}
+
+// Test loading a page with a bad SSL certificate during session restore, to
+// avoid regressing https://crbug.com/1050808.
+- (void)testBadSSLInSessionRestore {
+  GREYAssertTrue(_HTTPSServer->Start(), @"Test server failed to start.");
+
+  GURL pageURL = _HTTPSServer->GetURL("/echo");
+  [ChromeEarlGrey loadURL:pageURL];
+
+  [ChromeEarlGrey waitForWebStateContainingText:l10n_util::GetStringUTF8(
+                                                    IDS_SSL_V2_HEADING)];
+
+  [ChromeEarlGrey triggerRestoreViaTabGridRemoveAllUndo];
   [ChromeEarlGrey waitForWebStateContainingText:l10n_util::GetStringUTF8(
                                                     IDS_SSL_V2_HEADING)];
 }
