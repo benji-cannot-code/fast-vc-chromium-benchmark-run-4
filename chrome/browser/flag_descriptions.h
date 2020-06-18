097fe3f10de05c356ec6b76b77bd70cbba3b7dae
OlivierLi <olivierli@chromium.org>
2020-06-18 15:40:08 +0000

    Introduce flag to control intensive wakeup throttling.
    Bug: 1075553
    Change-Id: Ie863e35e110966efa62c4a3eaf8ac4dd4b0fa46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248072
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Oliver Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779834}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1049,6 +1049,9 @@ extern const char kDelayAsyncScriptExecutionDescription[];
 extern const char kPrefetchPrivacyChangesName[];
 extern const char kPrefetchPrivacyChangesDescription[];
 
+extern const char kIntensiveWakeUpThrottlingName[];
+extern const char kIntensiveWakeUpThrottlingDescription[];
+
 extern const char kPrinterStatusName[];
 extern const char kPrinterStatusDescription[];
 
--- third_party/blink/renderer/platform/scheduler/common/features.cc
+++ third_party/blink/renderer/platform/scheduler/common/features.cc
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/common/features.h"
 
 #include "base/command_line.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/switches.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 namespace scheduler {
@@ -46,18 +48,6 @@ PolicyOverride GetIntensiveWakeUpThrottlingPolicyOverride() {
 
 }  // namespace
 
-const base::Feature kIntensiveWakeUpThrottling{
-    "IntensiveWakeUpThrottling", base::FEATURE_DISABLED_BY_DEFAULT};
-
-const base::FeatureParam<int>
-    kIntensiveWakeUpThrottling_DurationBetweenWakeUpsSeconds{
-        &kIntensiveWakeUpThrottling, "duration_between_wake_ups_seconds",
-        kIntensiveWakeUpThrottling_DurationBetweenWakeUpsSeconds_Default};
-
-const base::FeatureParam<int> kIntensiveWakeUpThrottling_GracePeriodSeconds{
-    &kIntensiveWakeUpThrottling, "grace_period_seconds",
-    kIntensiveWakeUpThrottling_GracePeriodSeconds_Default};
-
 void ClearIntensiveWakeUpThrottlingPolicyOverrideCacheForTesting() {
   g_intensive_wake_up_throttling_policy_override_cached_ = false;
 }
@@ -68,7 +58,7 @@ bool IsIntensiveWakeUpThrottlingEnabled() {
   if (policy != PolicyOverride::NO_OVERRIDE)
     return policy == PolicyOverride::FORCE_ENABLE;
   // Otherwise respect the base::Feature.
-  return base::FeatureList::IsEnabled(kIntensiveWakeUpThrottling);
+  return base::FeatureList::IsEnabled(features::kIntensiveWakeUpThrottling);
 }
 
 // If a policy override is specified then stick to the published defaults so
@@ -77,6 +67,15 @@ bool IsIntensiveWakeUpThrottlingEnabled() {
 
 base::TimeDelta GetIntensiveWakeUpThrottlingDurationBetweenWakeUps() {
   DCHECK(IsIntensiveWakeUpThrottlingEnabled());
+
+  // Controls the period during which at most 1 wake up from throttleable
+  // TaskQueues in a page can take place.
+  static const base::FeatureParam<int>
+      kIntensiveWakeUpThrottling_DurationBetweenWakeUpsSeconds{
+          &features::kIntensiveWakeUpThrottling,
+          kIntensiveWakeUpThrottling_DurationBetweenWakeUpsSeconds_Name,
+          kIntensiveWakeUpThrottling_DurationBetweenWakeUpsSeconds_Default};
+
   int seconds =
       kIntensiveWakeUpThrottling_DurationBetweenWakeUpsSeconds_Default;
   if (GetIntensiveWakeUpThrottlingPolicyOverride() ==
@@ -88,6 +87,15 @@ base::TimeDelta GetIntensiveWakeUpThrottlingDurationBetweenWakeUps() {
 
 base::TimeDelta GetIntensiveWakeUpThrottlingGracePeriod() {
   DCHECK(IsIntensiveWakeUpThrottlingEnabled());
+
+  // Controls the time that elapses after a page is backgrounded before the
+  // throttling policy takes effect.
+  static const base::FeatureParam<int>
+      kIntensiveWakeUpThrottling_GracePeriodSeconds{
+          &features::kIntensiveWakeUpThrottling,
+          kIntensiveWakeUpThrottling_GracePeriodSeconds_Name,
+          kIntensiveWakeUpThrottling_GracePeriodSeconds_Default};
+
   int seconds = kIntensiveWakeUpThrottling_GracePeriodSeconds_Default;
   if (GetIntensiveWakeUpThrottlingPolicyOverride() ==
       PolicyOverride::NO_OVERRIDE) {
