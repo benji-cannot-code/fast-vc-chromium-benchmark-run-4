f47517024bf003667e0a5853a21ab2d6c86533c8
Anders Hartvoll Ruud <andruud@chromium.org>
2020-06-18 08:12:35 +0000

    [mpc] Move kMaxDependencies to StyleResolverState
    Instead of collecting all dependencies in a (potentially big) HashMap,
    and then rejecting them cache-time, it's better to reject excessive
    entries in the dependency map in the first place.
    
    This way declarations like "all:inherit" won't (pointlessly) create a
    huge HashMap internally.
    
    Bug: 1057072
    Change-Id: Ib88c964cdadad2469b9635d510647be8499c76df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247765
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779708}

--- third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_state.cc
@@ -243,6 +243,9 @@ const CSSValue& StyleResolverState::ResolveLightDarkPair(
 void StyleResolverState::MarkDependency(const CSSProperty& property) {
   if (!RuntimeEnabledFeatures::CSSMatchedPropertiesCacheDependenciesEnabled())
     return;
+  if (!HasValidDependencies())
+    return;
+
   has_incomparable_dependency_ |= !property.IsComputedValueComparable();
   dependencies_.insert(property.GetCSSPropertyName());
 }
