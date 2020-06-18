a28fd63e057b3ffc6f16e63933048bf920b73c70
Chris Harrelson <chrishtr@chromium.org>
2020-06-18 03:40:43 +0000

    Don't show scrolling contents layers that are also backface-invisible.
    In this situation, we have an element like this:
    
    <div id=target style="transform: ...; backface-visibility: hidden;
                overflow: scroll">
    </div>
    
    It will have two transform and one for scrolling:
    
    Transform
       ScrollTranslation
    
    However, overflow clipping is also a grouping property [1], so the
    ScrollTranslation will have flattens_inherited_transform true
    regardless of the transform-style style of #target.
    
    With TransformInterop on, definitely the scrolling contents should
    not be shown (except for pseudo- and stacking context descendants).
    This CL changes the behavior for non-TransformInterop also, because
    it doesn't seem reasonable to have the behavior the way it is right
    now.
    
    [1] https://drafts.csswg.org/css-transforms-2/#grouping-property-values
    
    Bug: 363609
    
    Change-Id: Ib1f356e80644582120e37261d368f774cbcaa6e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246437
    Auto-Submit: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779637}

--- cc/trees/transform_node.cc
+++ cc/trees/transform_node.cc
@@ -33,6 +33,7 @@ TransformNode::TransformNode()
       moved_by_outer_viewport_bounds_delta_y(false),
       in_subtree_of_page_scale_layer(false),
       transform_changed(false),
+      delegates_to_parent_for_backface(false),
       maximum_animation_scale(kNotScaled),
       starting_animation_scale(kNotScaled) {}
 
@@ -63,6 +64,8 @@ bool TransformNode::operator==(const TransformNode& other) const {
              other.moved_by_outer_viewport_bounds_delta_y &&
          in_subtree_of_page_scale_layer ==
              other.in_subtree_of_page_scale_layer &&
+         delegates_to_parent_for_backface ==
+             other.delegates_to_parent_for_backface &&
          transform_changed == other.transform_changed &&
          scroll_offset == other.scroll_offset &&
          snap_amount == other.snap_amount &&
