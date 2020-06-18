93d5ae036fcf7ee4f372187f8df2ac5a39a93f4e
Stephen McGruer <smcgruer@chromium.org>
2020-06-18 18:22:23 +0000

    Revert "Refactor: Decouple PasswordListItem from embedder"
    This reverts commit a89d80c456e2f2c11c807b7ecc40cce6242d88a4.
    
    Reason for revert: Being reverted as part of a revert chain for a recent CL that is suspect of causing multiple issues; see https://chromium-review.googlesource.com/c/chromium/src/+/2252612
    
    Original change's description:
    > Refactor: Decouple PasswordListItem from embedder
    > 
    > This CL addresses a few design issues with PasswordListItem that predate
    > the introduction of PasswordsListHandler.
    > 
    > - PasswordListItem would refer to a "menu" even though it knows nothing
    > about such menu, which is actually provided by the embedder. This CL
    > replaces mentions to passwordMenu in the component with what it actually
    > represents: the "more actions" button.
    > - The event fired by clicking the button was not documented in the file
    > overview. Moreover, the typedef for the event lived in the embedder. Now
    > the file exports the typedef.
    > - Tests that simulated clicks on the button would use the $$('#id')
    > syntax instead of the $.id one. This is now fixed.
    > 
    > Bug: None
    > Change-Id: Ie815237134236468c639e73831bdb3eb7db36f98
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248561
    > Commit-Queue: Victor Vianna <victorvianna@google.com>
    > Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    > Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#779777}
    
    TBR=fhorschig@chromium.org,aee@chromium.org,victorvianna@google.com
    
    Change-Id: Id573b073d8d5081d5c05527896cfc6e230c4d2ea
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252614
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779919}

--- chrome/browser/resources/settings/autofill_page/passwords_list_handler.js
+++ chrome/browser/resources/settings/autofill_page/passwords_list_handler.js
@@ -27,7 +27,6 @@ import {loadTimeData} from '../i18n_setup.js';
 // <if expr="chromeos">
 import {BlockingRequestManager} from './blocking_request_manager.js';
 // </if>
-import {PasswordMoreActionsClickedEvent} from './password_list_item.js';
 import {PasswordManagerImpl} from './password_manager_proxy.js';
 import {PasswordRemoveDialogPasswordsRemovedEvent} from './password_remove_dialog.js';
 
@@ -102,7 +101,7 @@ Polymer({
   ],
 
   listeners: {
-    'password-more-actions-clicked': 'onPasswordMoreActionsClicked_',
+    'password-menu-tap': 'onPasswordMenuTap_',
     'password-remove-dialog-passwords-removed':
         'onPasswordRemoveDialogPasswordsRemoved_',
   },
@@ -123,10 +122,11 @@ Polymer({
 
   /**
    * Opens the password action menu.
-   * @param {PasswordMoreActionsClickedEvent} event
+   * @param {!Event<!{target: !HTMLElement, listItem:
+   *     !PasswordListItemElement}>} event
    * @private
    */
-  onPasswordMoreActionsClicked_(event) {
+  onPasswordMenuTap_(event) {
     const target = event.detail.target;
 
     this.activePassword = event.detail.listItem;
