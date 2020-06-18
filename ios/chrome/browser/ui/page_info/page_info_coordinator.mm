96422df1b41793f71aa0a738c479c77e5f626406
Ewann <ewannpv@google.com>
2020-06-18 11:59:19 +0000

    [iOS][Chrome-Guard] Presents Page Info using FormSheet
    This CL presents views related to page info using FormSheet.
    
    Bug: 1095644
    Change-Id: I58f2f2d343a16912f9effb78ff4ad26a42b569a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249778
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779774}

--- ios/chrome/browser/ui/page_info/page_info_coordinator.mm
+++ ios/chrome/browser/ui/page_info/page_info_coordinator.mm
@@ -80,6 +80,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.navigationController =
       [[TableViewNavigationController alloc] initWithTable:self.viewController];
+  self.navigationController.modalPresentationStyle =
+      UIModalPresentationFormSheet;
 
   self.dispatcher = self.browser->GetCommandDispatcher();
   self.viewController.handler =
--- ios/chrome/browser/ui/settings/privacy/cookies_coordinator.mm
+++ ios/chrome/browser/ui/settings/privacy/cookies_coordinator.mm
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     TableViewNavigationController* navigationController =
         [[TableViewNavigationController alloc]
             initWithTable:self.viewController];
+    navigationController.modalPresentationStyle = UIModalPresentationFormSheet;
 
     [self.baseViewController presentViewController:navigationController
                                           animated:YES
