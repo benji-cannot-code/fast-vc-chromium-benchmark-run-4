374a0d3121eef9925dc876a37a1030d4516453aa
Dominik Röttsches <drott@chromium.org>
2020-06-18 09:22:17 +0000

    Refactor computing underline thickness as part of decoration info
    Break out underline thickness computation into its own function as
    preparation for computing underline thickness not only per current
    ComputedStyle but for each of the ComputedStyle AppliedTextDecorations.
    
    No functional change.
    
    Bug: 1096038
    Change-Id: I1384ec2a23735b6c8b3541e0535ca6ba5712beee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249758
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779730}

--- third_party/blink/renderer/core/paint/text_painter_base.h
+++ third_party/blink/renderer/core/paint/text_painter_base.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/decoration_info.h"
 #include "third_party/blink/renderer/core/paint/text_paint_style.h"
 #include "third_party/blink/renderer/core/style/applied_text_decoration.h"
+#include "third_party/blink/renderer/core/style/text_decoration_thickness.h"
 #include "third_party/blink/renderer/platform/fonts/font.h"
 #include "third_party/blink/renderer/platform/graphics/color.h"
 #include "third_party/blink/renderer/platform/transforms/affine_transform.h"
@@ -75,6 +76,12 @@ class CORE_EXPORT TextPainterBase {
                              FontBaseline,
                              const ComputedStyle&,
                              const ComputedStyle* decorating_box_style);
+
+  float ComputeUnderlineThickness(
+      const ResolvedUnderlinePosition& underline_position,
+      const TextDecorationThickness& applied_decoration_thickness,
+      const ComputedStyle&,
+      const ComputedStyle* decorating_box_style);
 
   static Color TextColorForWhiteBackground(Color);
   static TextPaintStyle TextPaintingStyle(const Document&,
