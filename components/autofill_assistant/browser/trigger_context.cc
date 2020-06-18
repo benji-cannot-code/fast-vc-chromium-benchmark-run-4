0f931a7873cfe4445b168d667784b50ae108bd83
sandromaggi <sandromaggi@google.com>
2020-06-18 15:28:34 +0000

    [Autofill Assistant] Add method to extract experiment
    This adds a new method to the |TriggerContext| enabling us to check if
    a certain experiment is present.
    
    Bug: b/158750294
    Change-Id: Ibff03efef920c2e9afad6eb0b11a071ff2fa3864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250069
    Commit-Queue: Sandro Maggi <sandromaggi@google.com>
    Reviewed-by: Clemens Arbesser <arbesser@google.com>
    Cr-Commit-Position: refs/heads/master@{#779827}

--- components/autofill_assistant/browser/trigger_context.cc
+++ components/autofill_assistant/browser/trigger_context.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "components/autofill_assistant/browser/trigger_context.h"
+#include "base/strings/string_split.h"
 
 namespace autofill_assistant {
 
@@ -60,6 +61,15 @@ std::string TriggerContextImpl::experiment_ids() const {
   return experiment_ids_;
 }
 
+bool TriggerContextImpl::HasExperimentId(
+    const std::string& experiment_id) const {
+  std::vector<std::string> experiments = base::SplitString(
+      experiment_ids_, ",", base::WhitespaceHandling::TRIM_WHITESPACE,
+      base::SplitResult::SPLIT_WANT_NONEMPTY);
+  return std::find(experiments.begin(), experiments.end(), experiment_id) !=
+         experiments.end();
+}
+
 bool TriggerContextImpl::is_cct() const {
   return cct_;
 }
@@ -112,6 +122,16 @@ std::string MergedTriggerContext::experiment_ids() const {
     experiment_ids.append(context->experiment_ids());
   }
   return experiment_ids;
+}
+
+bool MergedTriggerContext::HasExperimentId(
+    const std::string& experiment_id) const {
+  for (const TriggerContext* context : contexts_) {
+    if (context->HasExperimentId(experiment_id)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 bool MergedTriggerContext::is_cct() const {
--- components/autofill_assistant/browser/trigger_context.h
+++ components/autofill_assistant/browser/trigger_context.h
@@ -49,6 +49,9 @@ class TriggerContext {
   // Returns a comma-separated set of experiment ids.
   virtual std::string experiment_ids() const = 0;
 
+  // Returns whether an experiment is contained in |experiment_ids|.
+  virtual bool HasExperimentId(const std::string& experiment_id) const = 0;
+
   // Returns true if we're in a Chrome Custom Tab created for Autofill
   // Assistant, originally created through AutofillAssistantFacade.start(), in
   // Java.
@@ -76,12 +79,6 @@ class TriggerContextImpl : public TriggerContext {
 
   ~TriggerContextImpl() override;
 
-  // Implements TriggerContext:
-  void AddParameters(google::protobuf::RepeatedPtrField<ScriptParameterProto>*
-                         dest) const override;
-  base::Optional<std::string> GetParameter(
-      const std::string& name) const override;
-
   void SetCCT(bool value) { cct_ = value; }
   void SetOnboardingShown(bool value) { onboarding_shown_ = value; }
   void SetDirectAction(bool value) { direct_action_ = value; }
@@ -89,7 +86,13 @@ class TriggerContextImpl : public TriggerContext {
     caller_account_hash_ = value;
   }
 
+  // Implements TriggerContext:
+  void AddParameters(google::protobuf::RepeatedPtrField<ScriptParameterProto>*
+                         dest) const override;
+  base::Optional<std::string> GetParameter(
+      const std::string& name) const override;
   std::string experiment_ids() const override;
+  bool HasExperimentId(const std::string& experiment_id) const override;
   bool is_cct() const override;
   bool is_onboarding_shown() const override;
   bool is_direct_action() const override;
@@ -126,6 +129,7 @@ class MergedTriggerContext : public TriggerContext {
   base::Optional<std::string> GetParameter(
       const std::string& name) const override;
   std::string experiment_ids() const override;
+  bool HasExperimentId(const std::string& experiment_id) const override;
   bool is_cct() const override;
   bool is_onboarding_shown() const override;
   bool is_direct_action() const override;
