3b257670afabf19b5b666b6d28c2a728b554b139
Victor Costan <pwnall@chromium.org>
2020-06-18 21:42:38 +0000

    WebSQL: Remove unnecessary StorageType handling in QuotaClient.
    QuotaClient's methods will only be called with a StorageType in the list
    of types provided to QuotaManager::RegisterClient(). WebSQL only
    provides kTemporary, like most storage APIs. Therefore, its QuotaClient
    implementation does not need to handle other StorageType values.
    
    Bug: 1016065
    Change-Id: I727edcfdd4e4ed08c15b7b41001fdeb3d2a87059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250832
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780035}

--- storage/browser/database/database_quota_client.cc
+++ storage/browser/database/database_quota_client.cc
@@ -106,12 +106,7 @@ void DatabaseQuotaClient::GetOriginUsage(const url::Origin& origin,
                                          GetUsageCallback callback) {
   DCHECK(!callback.is_null());
   DCHECK(db_tracker_.get());
-
-  // All databases are in the default bucket.
-  if (type != StorageType::kTemporary) {
-    std::move(callback).Run(0);
-    return;
-  }
+  DCHECK_EQ(type, StorageType::kTemporary);
 
   base::PostTaskAndReplyWithResult(
       db_tracker_->task_runner(), FROM_HERE,
@@ -124,12 +119,7 @@ void DatabaseQuotaClient::GetOriginsForType(StorageType type,
                                             GetOriginsCallback callback) {
   DCHECK(!callback.is_null());
   DCHECK(db_tracker_.get());
-
-  // All databases are in the default bucket.
-  if (type != StorageType::kTemporary) {
-    std::move(callback).Run(std::set<url::Origin>());
-    return;
-  }
+  DCHECK_EQ(type, StorageType::kTemporary);
 
   std::set<url::Origin>* origins_ptr = new std::set<url::Origin>();
   db_tracker_->task_runner()->PostTaskAndReply(
@@ -145,12 +135,7 @@ void DatabaseQuotaClient::GetOriginsForHost(StorageType type,
                                             GetOriginsCallback callback) {
   DCHECK(!callback.is_null());
   DCHECK(db_tracker_.get());
-
-  // All databases are in the default bucket.
-  if (type != StorageType::kTemporary) {
-    std::move(callback).Run(std::set<url::Origin>());
-    return;
-  }
+  DCHECK_EQ(type, StorageType::kTemporary);
 
   std::set<url::Origin>* origins_ptr = new std::set<url::Origin>();
   db_tracker_->task_runner()->PostTaskAndReply(
@@ -167,12 +152,7 @@ void DatabaseQuotaClient::DeleteOriginData(const url::Origin& origin,
                                            DeletionCallback callback) {
   DCHECK(!callback.is_null());
   DCHECK(db_tracker_.get());
-
-  // All databases are in the default bucket.
-  if (type != StorageType::kTemporary) {
-    std::move(callback).Run(blink::mojom::QuotaStatusCode::kOk);
-    return;
-  }
+  DCHECK_EQ(type, StorageType::kTemporary);
 
   // DidDeleteOriginData() translates the net::Error response to a
   // blink::mojom::QuotaStatusCode if necessary, and no-ops as appropriate if
@@ -192,6 +172,8 @@ void DatabaseQuotaClient::DeleteOriginData(const url::Origin& origin,
 
 void DatabaseQuotaClient::PerformStorageCleanup(blink::mojom::StorageType type,
                                                 base::OnceClosure callback) {
+  DCHECK(!callback.is_null());
+  DCHECK_EQ(type, StorageType::kTemporary);
   std::move(callback).Run();
 }
 
