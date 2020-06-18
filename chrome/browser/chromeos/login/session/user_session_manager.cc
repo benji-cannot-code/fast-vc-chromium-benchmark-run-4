845af2c32034012adc15966a7e0429b49c3e4f45
Saurabh Nijhara <snijhara@google.com>
2020-06-18 11:32:41 +0000

    Show last day notification on user sign in
    As per product requirements of minimum chrome version policy, it is
    required to show notifications (for update required in case of network
    limitations and to return the device in case of end-of-life) on login
    if it is the last day to reach the deadline. Like other notifications
    of this policy, this notification on sign in is also only applicable
    to managed users to maintain consistency.
    
    This behaviour helps to curb the problem of notification being missed
    if policy is received when un-managed user is signed in and later
    a managed user signs in. Only the last day notification is shown on
    managed user sign in to avoid too much spamming.
    
    
    Bug: 1048607
    Change-Id: I01a8d52bbc4a003d5e1323140885881887211176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248567
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Commit-Queue: Saurabh Nijhara <snijhara@google.com>
    Cr-Commit-Position: refs/heads/master@{#779770}

--- chrome/browser/chromeos/login/session/user_session_manager.cc
+++ chrome/browser/chromeos/login/session/user_session_manager.cc
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/users/supervised_user_manager.h"
 #include "chrome/browser/chromeos/login/wizard_controller.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
+#include "chrome/browser/chromeos/policy/minimum_version_policy_handler.h"
 #include "chrome/browser/chromeos/policy/tpm_auto_update_mode_policy_handler.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
@@ -385,6 +386,12 @@ void PersistChallengeResponseKeys(const UserContext& user_context) {
 bool IsNewProfile(Profile* profile) {
   return user_manager::UserManager::Get()->IsCurrentUserNew() ||
          profile->IsNewProfile();
+}
+
+policy::MinimumVersionPolicyHandler* GetMinimumVersionPolicyHandler() {
+  return g_browser_process->platform_part()
+      ->browser_policy_connector_chromeos()
+      ->GetMinimumVersionPolicyHandler();
 }
 
 }  // namespace
@@ -1841,6 +1848,8 @@ void UserSessionManager::ShowNotificationsIfNeeded(Profile* profile) {
       ->browser_policy_connector_chromeos()
       ->GetTPMAutoUpdateModePolicyHandler()
       ->ShowTPMAutoUpdateNotificationIfNeeded();
+
+  GetMinimumVersionPolicyHandler()->MaybeShowNotificationOnLogin();
 }
 
 void UserSessionManager::MaybeLaunchSettings(Profile* profile) {
