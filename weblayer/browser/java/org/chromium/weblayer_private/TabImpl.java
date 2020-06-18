2de579dba8e28fc91ce48b47ed9562ec1676bb45
Colin Blundell <blundell@chromium.org>
2020-06-18 11:44:03 +0000

    [WebLayer] Add initial instrumentation test of infobars
    This CL sets up the infrastructure for private instrumentation tests of
    WebLayer's infobars and adds an initial test: that addition of an
    infobar impacts the visual state of the infobar container as expected.
    
    BUG=1093846
    
    Change-Id: Id5cdc73f02a4eadb46b3733451541fbd1f796441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247836
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779772}

--- weblayer/browser/java/org/chromium/weblayer_private/TabImpl.java
+++ weblayer/browser/java/org/chromium/weblayer_private/TabImpl.java
@@ -397,6 +397,11 @@ public final class TabImpl extends ITab.Stub implements LoginPrompt.Observer {
         return mNativeTab;
     }
 
+    @VisibleForTesting
+    public InfoBarContainer getInfoBarContainerForTesting() {
+        return mInfoBarContainer;
+    }
+
     @Override
     public NavigationControllerImpl createNavigationController(INavigationControllerClient client) {
         StrictModeWorkaround.apply();
---
+++ weblayer/browser/java/org/chromium/weblayer_private/test/TestInfoBar.java
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.weblayer_private.test;
+
+import androidx.annotation.VisibleForTesting;
+
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.NativeMethods;
+import org.chromium.content_public.browser.WebContents;
+import org.chromium.weblayer_private.InfoBar;
+import org.chromium.weblayer_private.InfoBarCompactLayout;
+import org.chromium.weblayer_private.TabImpl;
+
+/**
+ * A test infobar.
+ */
+@JNINamespace("weblayer")
+public class TestInfoBar extends InfoBar {
+    @VisibleForTesting
+    public TestInfoBar() {
+        super(0, 0, null, null);
+    }
+
+    @Override
+    protected boolean usesCompactLayout() {
+        return true;
+    }
+
+    @Override
+    protected void createCompactLayoutContent(InfoBarCompactLayout layout) {
+        new InfoBarCompactLayout.MessageBuilder(layout)
+                .withText("I am a compact infobar")
+                .buildAndInsert();
+    }
+
+    @CalledByNative
+    private static TestInfoBar create() {
+        return new TestInfoBar();
+    }
+
+    public static void show(TabImpl tab) {
+        TestInfoBarJni.get().show(tab.getWebContents());
+    }
+
+    @NativeMethods
+    interface Natives {
+        void show(WebContents webContents);
+    }
+}
