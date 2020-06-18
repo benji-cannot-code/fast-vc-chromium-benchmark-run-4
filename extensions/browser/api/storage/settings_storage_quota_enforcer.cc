815742d985d6c51f44c387974180de453cbc7059
Hans Wennborg <hans@chromium.org>
2020-06-18 20:37:26 +0000

    Include-what-you-use related to logging.h (extensions)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    (This doesn't add direct includes for all uses of CHECK etc, only the
    ones that will be broken by subsequent CLs to cleanup includes.)
    
    Bug: 1031540
    Change-Id: Ie8e24df57a4ca317a7932b13aee4ea8f39c84d27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248577
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780006}

--- extensions/browser/api/storage/settings_storage_quota_enforcer.cc
+++ extensions/browser/api/storage/settings_storage_quota_enforcer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/json/json_writer.h"
+#include "base/logging.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "extensions/common/extension_api.h"
