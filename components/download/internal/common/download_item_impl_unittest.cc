f78da03bcdfaaea1ceebb90150003594c3d17ded
Xing Liu <xingliu@chromium.org>
2020-06-18 00:03:28 +0000

    Download: Remove unnecessary unit test.
    A unit test only for compiling MockDownloadItem is not needed.
    
    Bug: NONE
    Change-Id: I106c2a9a5bacad516373905df1d4e8141ff1c441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250930
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779558}

--- components/download/internal/common/download_item_impl_unittest.cc
+++ components/download/internal/common/download_item_impl_unittest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/common/download_item_impl_delegate.h"
 #include "components/download/public/common/download_url_parameters.h"
 #include "components/download/public/common/mock_download_file.h"
-#include "components/download/public/common/mock_download_item.h"
 #include "crypto/secure_hash.h"
 #include "net/http/http_response_headers.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -2600,10 +2599,6 @@ TEST_P(DownloadItemDestinationUpdateRaceTest, IntermediateRenameSucceeds) {
 
   item_->Cancel(true);
   task_environment_.RunUntilIdle();
-}
-
-TEST(MockDownloadItem, Compiles) {
-  MockDownloadItem mock_item;
 }
 
 }  // namespace download
