a149ac1776d435d4c89626493dcb21690151b2c6
Ewann <ewannpv@google.com>
2020-06-18 09:24:07 +0000

    [iOS][Chrome-Guard] Adds Cookies mediator to NTP
    This CL links the cookies mediator to the NTP. The switch also updates
    his state according to the current cookies setting.
    
    Bug: 1063824
    Change-Id: I072f14668d236bde6d4327156696619cb6090dc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247029
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779732}

--- ios/chrome/browser/ui/ntp/incognito_view_controller.h
+++ ios/chrome/browser/ui/ntp/incognito_view_controller.h
@@ -8,15 +8,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-class UrlLoadingBrowserAgent;
+#import "ios/chrome/browser/ui/settings/privacy/cookies_consumer.h"
 
 @protocol NewTabPageControllerDelegate;
+@protocol PrivacyCookiesCommands;
+class UrlLoadingBrowserAgent;
 
-@interface IncognitoViewController : UIViewController
+@interface IncognitoViewController : UIViewController <PrivacyCookiesConsumer>
 
 // Init with the given loader object. |loader| may be nil, but isn't
 // retained so it must outlive this controller.
 - (instancetype)initWithUrlLoader:(UrlLoadingBrowserAgent*)URLLoader;
+
+// Handler used to update Cookies settings.
+@property(nonatomic, weak) id<PrivacyCookiesCommands> handler;
 
 @end
 
