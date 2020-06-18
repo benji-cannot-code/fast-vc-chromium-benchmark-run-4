c4986ac1a21f8443eeb876a539f6a6cdd3c6800f
Lucas Furukawa Gadani <lfg@chromium.org>
2020-06-18 15:34:59 +0000

    Portals: Apply dangling markup restrictions to <portal> element.
    Bug: 967204
    Change-Id: I276e6f8d8bdc3d317f5ce58f138d77601ee3c878
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248059
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779832}

---
+++ third_party/blink/web_tests/wpt_internal/portals/resources/report.py
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import time
+import json
+
+
+def main(request, response):
+    op = request.GET.first("op")
+    id = request.GET.first("id")
+    timeout = 1.0
+
+    if op == "retrieve":
+        t0 = time.time()
+        while time.time() - t0 < timeout:
+            time.sleep(0.1)
+            with request.server.stash.lock:
+                value = request.server.stash.take(key=id)
+            if value is not None:
+                return [("Content-Type", "application/json")], value
+        return [("Content-Type", "application/json")], json.dumps({'error': 'No such report.', 'id': id})
+
+    # save report
+    with request.server.stash.lock:
+        request.server.stash.take(key=id)
+        request.server.stash.put(key=id, value=json.dumps({'url': request.url}))
+
+    # return acknowledgement report
+    return [("Content-Type", "text/plain")], "Recorded report " + id
