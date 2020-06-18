e63485c088ef297f5e615cb980ce2ea9b679a06a
Austin Tankiang <austinct@chromium.org>
2020-06-18 11:22:35 +0000

    Create PreferenceWatcher in DriveIntegrationService
    PreferenceWatcher is private to DriveIntegrationService, so there's no
    point having it be passed in.
    
    Bug: 1096355
    Change-Id: I753c1c452d6873768f89d25f6737732624cc8b98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251268
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Sergei Datsenko <dats@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779768}

--- chrome/browser/chromeos/file_manager/external_filesystem_apitest.cc
+++ chrome/browser/chromeos/file_manager/external_filesystem_apitest.cc
@@ -518,7 +518,7 @@ class DriveFileSystemExtensionApiTest : public FileSystemExtensionApiTestBase {
     fake_drivefs_helper_ = std::make_unique<drive::FakeDriveFsHelper>(
         profile, drivefs_mount_point.DirName());
     return new drive::DriveIntegrationService(
-        profile, nullptr, "", test_cache_root_.GetPath(),
+        profile, "", test_cache_root_.GetPath(),
         fake_drivefs_helper_->CreateFakeDriveFsListenerFactory());
   }
 
@@ -616,7 +616,7 @@ class MultiProfileDriveFileSystemExtensionApiTest :
     const auto& drivefs_helper = fake_drivefs_helpers_[profile] =
         std::make_unique<drive::FakeDriveFsHelper>(profile, drivefs_dir);
     return new drive::DriveIntegrationService(
-        profile, nullptr, std::string(), cache_dir,
+        profile, std::string(), cache_dir,
         drivefs_helper->CreateFakeDriveFsListenerFactory());
   }
 
@@ -688,7 +688,7 @@ class LocalAndDriveFileSystemExtensionApiTest
     fake_drivefs_helper_ = std::make_unique<drive::FakeDriveFsHelper>(
         profile, drivefs_mount_point.DirName());
     return new drive::DriveIntegrationService(
-        profile, nullptr, "", test_cache_root_.GetPath(),
+        profile, "", test_cache_root_.GetPath(),
         fake_drivefs_helper_->CreateFakeDriveFsListenerFactory());
   }
 
