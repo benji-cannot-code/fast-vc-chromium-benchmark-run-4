1d5c2c67aa134a7361f13f6e96a880262ff55f70
Sorin Jianu <sorin@chromium.org>
2020-06-18 01:05:30 +0000

    RequestSender sets the content type header as application/json.
    This is done uniformly for all clients of update_client including
    the chrome/updater.
    
    Unfortunately, the content-type is still done by changing each
    network stack, since it can't be done cleanly with a "Content-Type"
    header.
    
    Bug: 1095704
    
    Change-Id: I16a7523e16cd846075fee8d28852b8ddb7eaf265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248721
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779590}

--- chrome/updater/win/net/network_winhttp.h
+++ chrome/updater/win/net/network_winhttp.h
@@ -49,6 +49,7 @@ class NetworkFetcherWinHTTP
   void PostRequest(
       const GURL& url,
       const std::string& post_data,
+      const std::string& content_type,
       const base::flat_map<std::string, std::string>& post_additional_headers,
       FetchStartedCallback fetch_started_callback,
       FetchProgressCallback fetch_progress_callback,
@@ -130,7 +131,7 @@ class NetworkFetcherWinHTTP
   std::string path_for_request_;
 
   base::StringPiece16 verb_;
-  base::StringPiece16 content_type_;
+  base::string16 content_type_;
   WriteDataCallback write_data_callback_;
   HRESULT net_error_ = S_OK;
   std::string etag_;
--- components/update_client/net/network_impl.h
+++ components/update_client/net/network_impl.h
@@ -35,6 +35,7 @@ class NetworkFetcherImpl : public NetworkFetcher {
   void PostRequest(
       const GURL& url,
       const std::string& post_data,
+      const std::string& content_type,
       const base::flat_map<std::string, std::string>& post_additional_headers,
       ResponseStartedCallback response_started_callback,
       ProgressCallback progress_callback,
