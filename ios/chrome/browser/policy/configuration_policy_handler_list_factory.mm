89a14ddad8f71a1682e2ee8186586f1d56e0f77e
Sébastien Séguin-Gagnon <sebsg@chromium.org>
2020-06-18 17:52:44 +0000

    [iOS Policy] Add ChromeVariations policy for iOS.
    Bug: 1092180
    Change-Id: Ib274cb26a85aa9ebc9de34793338c67b935b5cec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248135
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: sebsg <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779886}

--- ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
+++ ios/chrome/browser/policy/configuration_policy_handler_list_factory.mm
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/search_engines/default_search_policy_handler.h"
 #include "components/translate/core/browser/translate_pref_names.h"
+#include "components/variations/pref_names.h"
+#include "components/variations/service/variations_service.h"
 #include "ios/chrome/browser/policy/policy_features.h"
 #include "ios/chrome/browser/pref_names.h"
 
@@ -39,6 +41,9 @@ namespace {
 // that directly map to a single preference.
 // clang-format off
 const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
+  { policy::key::kChromeVariations,
+    variations::prefs::kVariationsRestrictionsByPolicy,
+    base::Value::Type::INTEGER },
   { policy::key::kEditBookmarksEnabled,
     bookmarks::prefs::kEditBookmarksEnabled,
     base::Value::Type::BOOLEAN },
