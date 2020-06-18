656912f9cae3a8a41c71fdb1cdd63f2a19c84175
Andrey Zaytsev <andzaytsev@google.com>
2020-06-18 16:54:41 +0000

    Safety check on Android: implemented initial UI
    Screenshots:
    Light mode: https://screenshot.googleplex.com/c5hr9M0ypqp.png
    Dark mode: https://screenshot.googleplex.com/iv2wB2Dih0s.png
    
    Bug: 1070620
    Change-Id: Ie256680755395d0de1713af48bb8975f96f7a213
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238066
    Commit-Queue: Theresa  <twellington@chromium.org>
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Auto-Submit: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/master@{#779864}

--- chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckSettingsFragment.java
+++ chrome/browser/safety_check/android/java/src/org/chromium/chrome/browser/safety_check/SafetyCheckSettingsFragment.java
@@ -6,8 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.safety_check;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.preference.PreferenceFragmentCompat;
+
+import org.chromium.components.browser_ui.settings.SettingsUtils;
+import org.chromium.ui.widget.ButtonCompat;
 
 /**
  * Fragment containing Safety check.
@@ -15,6 +22,23 @@ import androidx.preference.PreferenceFragmentCompat;
 public class SafetyCheckSettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
+        SettingsUtils.addPreferencesFromResource(this, R.xml.safety_check_preferences);
         getActivity().setTitle(R.string.prefs_safety_check);
     }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LinearLayout view =
+                (LinearLayout) super.onCreateView(inflater, container, savedInstanceState);
+
+        // Add a button to the bottom of the preferences view.
+        ButtonCompat checkButton =
+                (ButtonCompat) inflater.inflate(R.layout.safety_check_button, view, false);
+        checkButton.setOnClickListener((View v) -> onSafetyCheckButtonClicked());
+        view.addView(checkButton);
+        return view;
+    }
+
+    private void onSafetyCheckButtonClicked() {}
 }
