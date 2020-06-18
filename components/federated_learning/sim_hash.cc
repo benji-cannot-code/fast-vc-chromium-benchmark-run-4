8131ca8fab25e320d2588b2ac470cc425b5e6870
Jose Dapena Paz <jdapena@igalia.com>
2020-06-18 17:54:53 +0000

    IWYU: include cmath for using std::sqrt in federated_learning sim hash
    Bug: 819294
    Change-Id: I4785f465efb51cb7d2c63cc3df5ed86c8e53362d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248620
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#779890}

--- components/federated_learning/sim_hash.cc
+++ components/federated_learning/sim_hash.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/legacy_hash.h"
 
 #include <algorithm>
+#include <cmath>
 
 namespace federated_learning {
 
