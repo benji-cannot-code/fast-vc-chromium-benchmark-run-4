4437b592c5472285074bcbc9b93c75ee1277cd66
Yuwei Huang <yuweih@chromium.org>
2020-06-18 01:33:59 +0000

    [remoting host] Allow client to override min and max bitrate
    We have had some reports from users that claim the session performance
    is pretty bad and they can improve it by hardcoding a higher min
    bitrate.
    
    To help us debug this issue, this CL adds support to the host to apply
    min/max bitrate from session options.
    
    Bug: 1096197
    Change-Id: Ifd76fb81b9d69581740110a34f547e92efb63c33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250662
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779605}

--- remoting/protocol/webrtc_transport.h
+++ remoting/protocol/webrtc_transport.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <string>
+#include <tuple>
 #include <vector>
 
 #include "base/macros.h"
@@ -153,19 +154,20 @@ class WebrtcTransport : public Transport, public SessionOptionsProvider {
   void OnStatsDelivered(
       const rtc::scoped_refptr<const webrtc::RTCStatsReport>& report);
 
-  // Returns the max bitrate to set for this connection, taking into
-  // account any relay bitrate cap. If the relay status is unknown, this
-  // returns the default maximum bitrate.
-  int MaxBitrateForConnection();
+  // Returns the min (first element) and max (second element) bitrate for this
+  // connection, taking into account any relay bitrate cap and client overrides.
+  // The default range is [0, default max bixrate]. Client overrides that go
+  // beyond this bound or exceed the relay server's max bitrate will be ignored.
+  std::tuple<int, int> BitratesForConnection();
 
   // Sets bitrates on the PeerConnection.
   // Called after SetRemoteDescription(), but also called if the relay status
   // changes.
-  void SetPeerConnectionBitrates(int max_bitrate_bps);
+  void SetPeerConnectionBitrates(int min_bitrate_bps, int max_bitrate_bps);
 
   // Sets bitrates on the (video) sender. Called when the sender is created, but
   // also called if the relay status changes.
-  void SetSenderBitrates(int max_bitrate_bps);
+  void SetSenderBitrates(int min_bitrate_bps, int max_bitrate_bps);
 
   void RequestRtcStats();
   void RequestNegotiation();
