f5ce7756735854df4b94233dbea5d42d4af7c6af
Elly Fong-Jones <ellyjones@chromium.org>
2020-06-18 18:04:17 +0000

    cbuiv apps: use new widget/dialog setters
    This change:
    * Migrates AppBlockDialogView to use SetTitle
    * Migrates AppDialogView to use SetIcon, SetShowCloseButton, and
      SetShowIcon
    * Migrates AppPauseDialogView to use SetTitle and SetAcceptCallback
    * Migrates AppUninstallDialogView to use SetTitle
    * Removes many now-unneeded member variables from these classes
    
    Bug: 1075649
    Change-Id: Ifd8df11b87f77a3b337394c20a7c2b6dd638f7dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250508
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779905}

--- chrome/browser/ui/views/apps/app_dialog/app_dialog_view.cc
+++ chrome/browser/ui/views/apps/app_dialog/app_dialog_view.cc
@@ -13,11 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 
-AppDialogView::AppDialogView(const std::string& app_name,
-                             const gfx::ImageSkia& image)
-    : BubbleDialogDelegateView(nullptr, views::BubbleBorder::NONE),
-      app_name_(app_name),
-      image_(image) {}
+AppDialogView::AppDialogView(const gfx::ImageSkia& image)
+    : BubbleDialogDelegateView(nullptr, views::BubbleBorder::NONE) {
+  SetIcon(image);
+  SetShowCloseButton(false);
+  SetShowIcon(true);
+}
 
 AppDialogView::~AppDialogView() = default;
 
@@ -30,18 +31,6 @@ gfx::Size AppDialogView::CalculatePreferredSize() const {
 
 ui::ModalType AppDialogView::GetModalType() const {
   return ui::MODAL_TYPE_SYSTEM;
-}
-
-gfx::ImageSkia AppDialogView::GetWindowIcon() {
-  return image_;
-}
-
-bool AppDialogView::ShouldShowCloseButton() const {
-  return false;
-}
-
-bool AppDialogView::ShouldShowWindowIcon() const {
-  return true;
 }
 
 void AppDialogView::InitializeView(const base::string16& heading_text) {
