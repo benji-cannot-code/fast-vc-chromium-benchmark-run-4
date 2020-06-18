dbuskariol-org/chromium

commit d95b3abae0ca9b722b823989fd74ddfd5103c9b2

Android: Add host_jar_path vs device_jar_path to build logic
Fixes desugared .jar files incorrectly being used in java_binary() /
junit_binary().

Bug: 1078484
Change-Id: Ia49a10d3e907844ed75ee658604e8eeaa2993fb4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2241865
Commit-Queue: Andrew Grieve <agrieve@chromium.org>
Commit-Queue: Peter Wen <wnwen@chromium.org>
Reviewed-by: Peter Wen <wnwen@chromium.org>
Cr-Commit-Position: refs/heads/master@{#778433}

build/android/gyp/write_build_config.py
@@ -242,18 +242,22 @@ through Proguard or other tools. For most targets this is generated
 from sources, with a name like `$target_name.javac.jar`. However, when using
 a prebuilt jar, this will point to the source archive directly.
 
-* `deps_info['jar_path']`:
+* `deps_info['device_jar_path']`:
 Path to a file that is the result of processing
-`deps_info['unprocessed_jar_path']` with various tools.
+`deps_info['unprocessed_jar_path']` with various tools (ready to be dexed).
+
+* `deps_info['host_jar_path']`:
+Path to a file that is the result of processing
+`deps_info['unprocessed_jar_path']` with various tools (use by java_binary).
 
 * `deps_info['interface_jar_path']:
 Path to the interface jar generated for this library. This corresponds to
 a jar file that only contains declarations. Generated by running the `ijar` on
-`deps_info['jar_path']` or the `turbine` tool on source files.
+`deps_info['unprocessed_jar_path']` or the `turbine` tool on source files.
 
 * `deps_info['dex_path']`:
-Path to the `.dex` file generated for this target, from `deps_info['jar_path']`
-unless this comes from a prebuilt `.aar` archive.
+Path to the `.dex` file generated for this target, from
+`deps_info['device_jar_path']` unless this comes from a prebuilt `.aar` archive.
 
 * `deps_info['is_prebuilt']`:
 True to indicate that this target corresponds to a prebuilt `.jar` file.
@@ -323,10 +327,10 @@ all entries from the `java_library` type, and adds:
 * `deps_info['main_class']`:
 Name of the main Java class that serves as an entry point for the binary.
 
-* `deps_info['java_runtime_classpath']`:
+* `deps_info['device_classpath']`:
 The classpath used when running a Java or Android binary. Essentially the
-collection of all `deps_info['jar_path']` entries for the target and all its
-dependencies.
+collection of all `deps_info['device_jar_path']` entries for the target and all
+its dependencies.
 
 
 ## <a name="target_junit_binary">Target type `junit_binary`</a>:
@@ -885,7 +889,8 @@ def main(argv):
       help='Consider the assets as locale paks in BuildConfig.java')
 
   # java library options
-  parser.add_option('--jar-path', help='Path to target\'s jar output.')
+  parser.add_option('--device-jar-path', help='Path to .jar for dexing.')
+  parser.add_option('--host-jar-path', help='Path to .jar for java_binary.')
   parser.add_option('--unprocessed-jar-path',
       help='Path to the .jar to use for javac classpath purposes.')
   parser.add_option(
@@ -1048,11 +1053,13 @@ def main(argv):
   if options.fail:
     parser.error('\n'.join(build_utils.ParseGnList(options.fail)))
 
-  jar_path_options = ['jar_path', 'unprocessed_jar_path', 'interface_jar_path']
+  lib_options = ['unprocessed_jar_path', 'interface_jar_path']
+  device_lib_options = ['device_jar_path', 'dex_path']
   required_options_map = {
-      'android_apk': ['build_config', 'dex_path'] + jar_path_options,
-      'android_app_bundle_module': ['build_config', 'dex_path',
-          'final_dex_path', 'res_size_info'] + jar_path_options,
+      'android_apk': ['build_config'] + lib_options + device_lib_options,
+      'android_app_bundle_module':
+      ['build_config', 'final_dex_path', 'res_size_info'] + lib_options +
+      device_lib_options,
       'android_assets': ['build_config'],
       'android_resources': ['build_config', 'resources_zip'],
       'dist_aar': ['build_config'],
@@ -1060,9 +1067,9 @@ def main(argv):
       'group': ['build_config'],
       'java_annotation_processor': ['build_config', 'main_class'],
       'java_binary': ['build_config'],
-      'java_library': ['build_config'] + jar_path_options,
+      'java_library': ['build_config', 'host_jar_path'] + lib_options,
       'junit_binary': ['build_config'],
-      'system_java_library': ['build_config'],
+      'system_java_library': ['build_config', 'unprocessed_jar_path'],
       'android_app_bundle': ['build_config', 'module_build_configs'],
   }
   required_options = required_options_map.get(options.type)
@@ -1102,10 +1109,10 @@ def main(argv):
           '--library-renames can only be used with --type=android_apk or '
           '--type=android_app_bundle_module')
 
-  if options.jar_path and options.supports_android and not options.dex_path:
+  if options.device_jar_path and not options.dex_path:
     raise Exception('java_library that supports Android requires a dex path.')
-  if any(getattr(options, x) for x in jar_path_options):
-    for attr in jar_path_options:
+  if any(getattr(options, x) for x in lib_options):
+    for attr in lib_options:
       if not getattr(options, attr):
         raise('Expected %s to be set.' % attr)
 
@@ -1270,10 +1277,13 @@ def main(argv):
   if is_java_target:
     # Classpath values filled in below (after applying tested_apk_config).
     config['javac'] = {}
-    if options.jar_path:
-      deps_info['jar_path'] = options.jar_path
+    if options.unprocessed_jar_path:
       deps_info['unprocessed_jar_path'] = options.unprocessed_jar_path
       deps_info['interface_jar_path'] = options.interface_jar_path
+    if options.device_jar_path:
+      deps_info['device_jar_path'] = options.device_jar_path
+    if options.host_jar_path:
+      deps_info['host_jar_path'] = options.host_jar_path
     if options.enable_jetify:
       deps_info['jetified_jar_path'] = options.jetified_jar_path
     else:
@@ -1469,15 +1479,24 @@ def main(argv):
 
   if is_java_target or options.type == 'android_app_bundle':
     # The classpath to use to run this target (or as an input to ProGuard).
-    java_full_classpath = []
-    if is_java_target and options.jar_path:
-      java_full_classpath.append(options.jar_path)
-    java_full_classpath.extend(c['jar_path'] for c in all_library_deps)
+    device_classpath = []
+    if is_java_target and options.device_jar_path:
+      device_classpath.append(options.device_jar_path)
+    device_classpath.extend(
+        c.get('device_jar_path') for c in all_library_deps
+        if c.get('device_jar_path'))
     if options.type == 'android_app_bundle':
       for d in deps.Direct('android_app_bundle_module'):
-        java_full_classpath.extend(
-            c for c in d.get('java_runtime_classpath', [])
-            if c not in java_full_classpath)
+        device_classpath.extend(c for c in d.get('device_classpath', [])
+                                if c not in device_classpath)
+
+  if options.type in ('dist_jar', 'java_binary', 'junit_binary'):
+    # The classpath to use to run this target.
+    host_classpath = []
+    if options.host_jar_path:
+      host_classpath.append(options.host_jar_path)
+    host_classpath.extend(c['host_jar_path'] for c in all_library_deps)
+    deps_info['host_classpath'] = host_classpath
 
   all_configs = build_utils.ParseGnList(options.proguard_configs)
   deps_info['proguard_configs'] = list(all_configs)
@@ -1573,7 +1592,7 @@ def main(argv):
       if dep_config['type'] == 'android_app_bundle':
         base_config = GetDepConfig(dep_config['base_module_config'])
       extra_main_r_text_files.append(base_config['r_text_path'])
-      static_lib_jar_paths[config_path] = base_config['jar_path']
+      static_lib_jar_paths[config_path] = base_config['device_jar_path']
       all_configs.extend(dep_config['proguard_all_configs'])
       extra_proguard_classpath_jars.extend(
           dep_config['proguard_classpath_jars'])
@@ -1588,19 +1607,19 @@ def main(argv):
       for package in base_config['extra_package_names']:
         if package not in extra_package_names:
           extra_package_names.append(package)
-      for cp_entry in dep_config['java_runtime_classpath']:
+      for cp_entry in dep_config['device_classpath']:
         configs_by_classpath_entry[cp_entry].append(config_path)
 
-    for cp_entry in java_full_classpath:
+    for cp_entry in device_classpath:
       configs_by_classpath_entry[cp_entry].append(options.build_config)
 
     for cp_entry, candidate_configs in configs_by_classpath_entry.iteritems():
       config_path = (candidate_configs[0]
                      if len(candidate_configs) == 1 else options.build_config)
       classpath_entries_by_owning_config[config_path].append(cp_entry)
-      java_full_classpath.append(cp_entry)
+      device_classpath.append(cp_entry)
 
-    java_full_classpath = sorted(set(java_full_classpath))
+    device_classpath = sorted(set(device_classpath))
 
   deps_info['static_library_proguard_mapping_output_paths'] = sorted([
       d['proguard_mapping_path']
@@ -1645,7 +1664,7 @@ def main(argv):
       deps_proguard_enabled = []
       deps_proguard_disabled = []
       for d in deps.Direct('android_app_bundle_module'):
-        if not d['java_runtime_classpath']:
+        if not d['device_classpath']:
           # We don't care about modules that have no Java code for proguarding.
           continue
         if d['proguard_enabled']:
@@ -1695,10 +1714,10 @@ def main(argv):
 
     # Add all tested classes to the test's classpath to ensure that the test's
     # java code is a superset of the tested apk's java code
-    java_full_classpath_extended = list(java_full_classpath)
-    java_full_classpath_extended.extend(
-        p for p in tested_apk_config['java_runtime_classpath']
-        if p not in java_full_classpath)
+    device_classpath_extended = list(device_classpath)
+    device_classpath_extended.extend(
+        p for p in tested_apk_config['device_classpath']
+        if p not in device_classpath)
     # Include in the classpath classes that are added directly to the apk under
     # test (those that are not a part of a java_library).
     javac_classpath.append(tested_apk_config['unprocessed_jar_path'])
@@ -1721,9 +1740,9 @@ def main(argv):
     tested_apk_library_deps = tested_apk_deps.All('java_library')
     tested_apk_dex_files = {c['dex_path'] for c in tested_apk_library_deps}
     all_dex_files = [p for p in all_dex_files if p not in tested_apk_dex_files]
-    tested_apk_jar_files = set(tested_apk_config['java_runtime_classpath'])
-    java_full_classpath = [
-        p for p in java_full_classpath if p not in tested_apk_jar_files
+    tested_apk_jar_files = set(tested_apk_config['device_classpath'])
+    device_classpath = [
+        p for p in device_classpath if p not in tested_apk_jar_files
     ]
 
   if options.type in ('android_apk', 'dist_aar', 'dist_jar',
@@ -1744,10 +1763,16 @@ def main(argv):
   if is_java_target:
     config['javac']['classpath'] = javac_classpath
     config['javac']['interface_classpath'] = javac_interface_classpath
-    # Direct() will be of type 'java_annotation_processor'.
+    # Direct() will be of type 'java_annotation_processor', and so not included
+    # in All('java_library').
+    # Annotation processors run as part of the build, so need host_jar_path.
     config['javac']['processor_classpath'] = [
-        c['jar_path'] for c in processor_deps.Direct() if c.get('jar_path')] + [
-        c['jar_path'] for c in processor_deps.All('java_library')]
+        c['host_jar_path'] for c in processor_deps.Direct()
+        if c.get('host_jar_path')
+    ]
+    config['javac']['processor_classpath'] += [
+        c['host_jar_path'] for c in processor_deps.All('java_library')
+    ]
     config['javac']['processor_classes'] = [
         c['main_class'] for c in processor_deps.Direct()]
     deps_info['javac_full_classpath'] = javac_full_classpath
@@ -1758,19 +1783,18 @@ def main(argv):
     javac_full_classpath = set()
     for d in deps.Direct('android_app_bundle_module'):
       javac_full_classpath.update(p for p in d['javac_full_classpath'])
-      javac_full_classpath.add(d['jar_path'])
+      javac_full_classpath.add(d['unprocessed_jar_path'])
     deps_info['javac_full_classpath'] = sorted(javac_full_classpath)
 
-  if options.type in ('android_apk', 'dist_jar', 'java_binary', 'junit_binary',
-      'android_app_bundle_module', 'android_app_bundle'):
-    deps_info['java_runtime_classpath'] = java_full_classpath
+  if options.type in ('android_apk', 'dist_jar', 'android_app_bundle_module',
+                      'android_app_bundle'):
+    deps_info['device_classpath'] = device_classpath
     if options.tested_apk_config:
-      deps_info['java_runtime_classpath_extended'] = (
-          java_full_classpath_extended)
+      deps_info['java_runtime_classpath_extended'] = (device_classpath_extended)
 
   if options.type in ('android_apk', 'dist_jar'):
     all_interface_jars = []
-    if options.jar_path:
+    if options.interface_jar_path:
       all_interface_jars.append(options.interface_jar_path)
     all_interface_jars.extend(c['interface_jar_path'] for c in all_library_deps)
 
@@ -1862,7 +1886,7 @@ def main(argv):
   # are not duplicated on the feature module.
   if base_module_build_config:
     base = base_module_build_config
-    RemoveObjDups(config, base, 'deps_info', 'java_runtime_classpath')
+    RemoveObjDups(config, base, 'deps_info', 'device_classpath')
     RemoveObjDups(config, base, 'deps_info', 'javac_full_classpath')
     RemoveObjDups(config, base, 'deps_info', 'javac_full_interface_classpath')
     RemoveObjDups(config, base, 'deps_info', 'jetified_full_jar_classpath')

BUILD.gn
@@ -356,7 +356,6 @@ group("gn_all") {
       "//tools/android:push_apps_to_background",
       "//tools/android/audio_focus_grabber:audio_focus_grabber_apk",
       "//tools/android/customtabs_benchmark:customtabs_benchmark_apk",
-      "//tools/android/errorprone_plugin:errorprone_plugin_java",
       "//tools/android/kerberos/SpnegoAuthenticator:spnego_authenticator_apk",
       "//ui/android:ui_junit_tests",
       "//weblayer/public/java:client_aar",

build/android/bytecode/BUILD.gn
@@ -17,5 +17,4 @@ java_binary("bytecode_processor") {
   ]
   wrapper_script_name = "helper/bytecode_processor"
   enable_bytecode_checks = false
-  enable_desugar = false
 }

build/android/gyp/dist_aar.py
@@ -14,7 +14,7 @@ import sys
 import tempfile
 import zipfile
 
-from filter_zip import CreatePathTransform
+import filter_zip
 from util import build_utils
 
 
@@ -117,8 +117,8 @@ def main(args):
         build_utils.AddToZipHermetic(
             z, 'AndroidManifest.xml', src_path=options.android_manifest)
 
-        path_transform = CreatePathTransform(options.jar_excluded_globs,
-                                             options.jar_included_globs, [])
+        path_transform = filter_zip.CreatePathTransform(
+            options.jar_excluded_globs, options.jar_included_globs, [])
         with tempfile.NamedTemporaryFile() as jar_file:
           build_utils.MergeZips(
               jar_file.name, options.jars, path_transform=path_transform)

build/android/gyp/filter_zip.py
@@ -5,6 +5,7 @@
 # found in the LICENSE file.
 
 import argparse
+import shutil
 import sys
 
 from util import build_utils
@@ -20,6 +21,21 @@ _RESOURCE_CLASSES = [
 
 def CreatePathTransform(exclude_globs, include_globs,
                         strip_resource_classes_for):
+  """Returns a function to strip paths for the given patterns.
+
+  Args:
+    exclude_globs: List of globs that if matched should be excluded.
+    include_globs: List of globs that if not matched should be excluded.
+    strip_resource_classes_for: List of Java packages for which to strip
+       R.java classes from.
+
+  Returns:
+    * None if no filters are needed.
+    * A function "(path) -> path" that returns None when |path| should be
+          stripped, or |path| otherwise.
+  """
+  if not (exclude_globs or include_globs or strip_resource_classes_for):
+    return None
   exclude_globs = list(exclude_globs or [])
   if strip_resource_classes_for:
     exclude_globs.extend(p.replace('.', '/') + '/' + f
@@ -52,19 +68,18 @@ def main():
   argv = build_utils.ExpandFileArgs(sys.argv[1:])
   args = parser.parse_args(argv)
 
-  if args.exclude_globs:
-    args.exclude_globs = build_utils.ParseGnList(args.exclude_globs)
-  if args.include_globs:
-    args.include_globs= build_utils.ParseGnList(args.include_globs)
-  if args.strip_resource_classes_for:
-    args.strip_resource_classes_for = build_utils.ParseGnList(
-        args.strip_resource_classes_for)
+  args.exclude_globs = build_utils.ParseGnList(args.exclude_globs)
+  args.include_globs = build_utils.ParseGnList(args.include_globs)
+  args.strip_resource_classes_for = build_utils.ParseGnList(
+      args.strip_resource_classes_for)
 
   path_transform = CreatePathTransform(args.exclude_globs, args.include_globs,
                                        args.strip_resource_classes_for)
   with build_utils.AtomicOutput(args.output) as f:
-    build_utils.MergeZips(
-        f.name, [args.input], path_transform=path_transform)
+    if path_transform:
+      build_utils.MergeZips(f.name, [args.input], path_transform=path_transform)
+    else:
+      shutil.copy(args.input, f.name)
 
 
 if __name__ == '__main__':

build/config/android/copy_ex.gni
@@ -28,6 +28,7 @@ template("copy_ex") {
                            [
                              "data",
                              "deps",
+                             "public_deps",
                              "testonly",
                              "visibility",
                            ])

build/config/android/rules.gni
@@ -1624,7 +1624,7 @@ if (enable_java_templates) {
         } else if (_use_unprocessed_jars) {
           args += [ "--input-zips=@FileArg($_rebased_build_config:deps_info:javac_full_classpath)" ]
         } else {
-          args += [ "--input-zips=@FileArg($_rebased_build_config:deps_info:java_runtime_classpath)" ]
+          args += [ "--input-zips=@FileArg($_rebased_build_config:deps_info:device_classpath)" ]
         }
       }
       _excludes = []
@@ -2139,8 +2139,7 @@ if (enable_java_templates) {
   template("android_apk_or_module") {
     forward_variables_from(invoker, [ "testonly" ])
     assert(defined(invoker.android_manifest))
-    _out_dir = "$target_out_dir/$target_name"
-    _base_path = "$_out_dir/$target_name"
+    _base_path = "$target_out_dir/$target_name/$target_name"
     _build_config = "$target_gen_dir/$target_name.build_config"
     _build_config_target = "$target_name$build_config_target_suffix"
 
@@ -2153,9 +2152,6 @@ if (enable_java_templates) {
       _target_sdk_version = invoker.target_sdk_version
     }
 
-    # JUnit tests use resource zip files. These must not be put in gen/
-    # directory or they will not be available to tester bots.
-    _jar_path = "$_base_path.jar"
     _template_name = target_name
 
     _is_bundle_module =
@@ -2352,8 +2348,7 @@ if (enable_java_templates) {
     _is_static_library_provider =
         defined(invoker.static_library_dependent_targets) && _proguard_enabled
     if (_is_static_library_provider) {
-      _static_library_sync_dex_path =
-          "$_out_dir/static_library_synchronized_proguard.r8dex.jar"
+      _static_library_sync_dex_path = "$_base_path.synchronized.r8dex.jar"
       _resource_ids_provider_deps = []
       foreach(_target, invoker.static_library_dependent_targets) {
         if (_target.is_resource_ids_provider) {
@@ -2800,7 +2795,6 @@ if (enable_java_templates) {
       supports_android = true
       requires_android = true
       srcjar_deps = _srcjar_deps
-      final_jar_path = _jar_path
       if (defined(_final_dex_path)) {
         final_dex_path = _final_dex_path
       }
@@ -2932,7 +2926,8 @@ if (enable_java_templates) {
             main_dex_list_input_classes_filearg = "@FileArg(${_rebased_build_config}:deps_info:java_runtime_classpath_extended)"
           }
         } else {
-          input_classes_filearg = "@FileArg($_rebased_build_config:deps_info:java_runtime_classpath)"
+          input_classes_filearg =
+              "@FileArg($_rebased_build_config:deps_info:device_classpath)"
         }
 
         if (_is_static_library_provider) {

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 55be62bfe29dac73cfee7d46da59139254a23af5

Android: Fix enable_jetify build option
Option is currently unused in chrome, so went unnoticed when it broke in
d95b3abae0ca9b722b823989fd74ddfd5103c9b2

Bug: 1078484
Change-Id: I14b71a0c81f6cfebad527f4ef5131a39dae08896
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252605
Commit-Queue: Andrew Grieve <agrieve@chromium.org>
Commit-Queue: Peter Wen <wnwen@chromium.org>
Auto-Submit: Andrew Grieve <agrieve@chromium.org>
Reviewed-by: Peter Wen <wnwen@chromium.org>
Cr-Commit-Position: refs/heads/master@{#780022}

build/android/gyp/write_build_config.py
@@ -900,9 +900,6 @@ def main(argv):
   parser.add_option(
       '--jetified-jar-path',
       help='Path to the jetified.jar to use for javac classpath purposes.')
-  parser.add_option('--enable-jetify',
-                    action='store_true',
-                    help='Whether to use jetified or non-jetified classpath.')
   parser.add_option('--is-prebuilt', action='store_true',
                     help='Whether the jar was compiled or pre-compiled.')
   parser.add_option('--java-sources-file', help='Path to .sources file')
@@ -1285,10 +1282,8 @@ def main(argv):
       deps_info['device_jar_path'] = options.device_jar_path
     if options.host_jar_path:
       deps_info['host_jar_path'] = options.host_jar_path
-    if options.enable_jetify:
-      deps_info['jetified_jar_path'] = options.jetified_jar_path
-    else:
-      deps_info['jetified_jar_path'] = options.interface_jar_path
+    deps_info['jetified_jar_path'] = (options.jetified_jar_path
+                                      or options.interface_jar_path)
     if options.dex_path:
       deps_info['dex_path'] = options.dex_path
       if is_apk_or_module_target:

build/config/android/internal_rules.gni
@@ -251,9 +251,6 @@ template("write_build_config") {
         rebase_path(invoker.java_resources_jar, root_build_dir),
       ]
     }
-    if (defined(invoker.enable_jetify) && invoker.enable_jetify) {
-      args += [ "--enable-jetify" ]
-    }
     if (defined(invoker.jetified_jar_path)) {
       args += [
         "--jetified-jar-path",
@@ -1765,14 +1762,11 @@ if (enable_java_templates) {
     not_needed([ "_rebased_build_config" ])
     not_needed(invoker, [ "build_config_dep" ])
 
-    _enable_jetify = defined(invoker.enable_jetify) && invoker.enable_jetify
     _deps = invoker.jar_deps
     _previous_output_jar = invoker.input_jar_path
 
-    if (_enable_jetify) {
+    if (defined(invoker.jetified_jar_path)) {
       _jetify_target = "${target_name}__jetify"
-      _jetify_input_jar = _previous_output_jar
-      _jetify_output_jar = "$target_out_dir/$target_name.jetified.jar"
 
       action_with_pydeps(_jetify_target) {
         script = "//build/android/gyp/jetify_jar.py"
@@ -1783,15 +1777,15 @@ if (enable_java_templates) {
             "//third_party/jetifier_standalone/config/ignore_R.config"
 
         inputs = [
-          _jetify_input_jar,
+          _previous_output_jar,
           _jetify_jar_path,
         ]
-        outputs = [ _jetify_output_jar ]
+        outputs = [ invoker.jetified_jar_path ]
         args = [
           "--input-path",
           rebase_path(invoker.input_jar_path, root_build_dir),
           "--output-path",
-          rebase_path(_jetify_output_jar, root_build_dir),
+          rebase_path(invoker.jetified_jar_path, root_build_dir),
           "--jetify-path",
           rebase_path(_jetify_jar_path, root_build_dir),
           "--jetify-config-path",
@@ -1801,7 +1795,7 @@ if (enable_java_templates) {
 
       _deps = []
       _deps = [ ":$_jetify_target" ]
-      _previous_output_jar = _jetify_output_jar
+      _previous_output_jar = invoker.jetified_jar_path
     }
 
     # Create the .jar in lib.java for use by java_binary.
@@ -3415,6 +3409,11 @@ if (enable_java_templates) {
         _unprocessed_jar_path = _javac_jar_path
       }
     }
+    if (_build_host_jar || _build_device_jar) {
+      if (defined(invoker.enable_jetify) && invoker.enable_jetify) {
+        _jetified_jar_path = "$target_out_dir/${_main_target_name}.jetified.jar"
+      }
+    }
 
     if (_is_prebuilt || _has_sources) {
       _java_header_deps = []
@@ -3544,8 +3543,9 @@ if (enable_java_templates) {
       chromium_code = _chromium_code
       build_config = _build_config
       is_prebuilt = _is_prebuilt
-      enable_jetify = defined(invoker.enable_jetify) && invoker.enable_jetify
-      jetified_jar_path = "$target_out_dir/${invoker.target_name}__process_prebuilt.jetified.jar"
+      if (defined(invoker.enable_jetify) && invoker.enable_jetify) {
+        jetified_jar_path = _jetified_jar_path
+      }
 
       possible_config_deps = []
       if (defined(_extra_java_deps)) {
@@ -3714,7 +3714,6 @@ if (enable_java_templates) {
       process_java_prebuilt(_process_prebuilt_target_name) {
         forward_variables_from(invoker,
                                [
-                                 "enable_jetify",
                                  "jar_excluded_patterns",
                                  "jar_included_patterns",
                                ])
@@ -3739,6 +3738,9 @@ if (enable_java_templates) {
           if (enable_desugar) {
             classpath_deps = _full_classpath_deps
           }
+        }
+        if (defined(invoker.enable_jetify) && invoker.enable_jetify) {
+          jetified_jar_path = _jetified_jar_path
         }
 
         # proguard_configs listed on java_library targets need to be marked