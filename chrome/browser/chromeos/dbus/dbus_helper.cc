085f5c7856d4b59383de7f69afe981fb22f8a8a8
Azeem Arshad <azeemarshad@chromium.org>
2020-06-18 21:59:18 +0000

    [Cellular] Add Hermes DBus client classes.
    This CL adds client class for Hermes Manager and Profile
    objects. See http://go/cros-esim-design-doc for details.
    
    Bug: 1093185
    Change-Id: I4ca9d3da0d6e7b5d67ef27bf804a8de572c7ae44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2238409
    Commit-Queue: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780043}

--- chrome/browser/chromeos/dbus/dbus_helper.cc
+++ chrome/browser/chromeos/dbus/dbus_helper.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/cups_proxy/cups_proxy_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/dlcservice/dlcservice_client.h"
+#include "chromeos/dbus/hermes/hermes_clients.h"
 #include "chromeos/dbus/initialize_dbus_client.h"
 #include "chromeos/dbus/ip_peripheral/ip_peripheral_service_client.h"
 #include "chromeos/dbus/kerberos/kerberos_client.h"
@@ -71,6 +72,7 @@ void InitializeDBus() {
   InitializeDBusClient<CryptohomeClient>(bus);
   InitializeDBusClient<CupsProxyClient>(bus);
   InitializeDBusClient<DlcserviceClient>(bus);
+  hermes_clients::Initialize(bus);
   InitializeDBusClient<IpPeripheralServiceClient>(bus);
   InitializeDBusClient<KerberosClient>(bus);
   InitializeDBusClient<MachineLearningClient>(bus);
@@ -112,6 +114,7 @@ void ShutdownDBus() {
   MachineLearningClient::Shutdown();
   KerberosClient::Shutdown();
   IpPeripheralServiceClient::Shutdown();
+  hermes_clients::Shutdown();
   DlcserviceClient::Shutdown();
   CupsProxyClient::Shutdown();
   CryptohomeClient::Shutdown();
---
+++ chromeos/dbus/hermes/hermes_test_utils.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/dbus/hermes/hermes_test_utils.h"
+
+#include "chromeos/dbus/hermes/hermes_response_status.h"
+
+namespace chromeos {
+namespace hermes_test_utils {
+
+void CopyHermesStatus(HermesResponseStatus* dest_status,
+                      HermesResponseStatus status) {
+  *dest_status = status;
+}
+
+}  // namespace hermes_test_utils
+}  // namespace chromeos
