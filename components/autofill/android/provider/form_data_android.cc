398f3e99584c1a866e60233f18e6260d8c658b4a
Tao Bai <michaelbai@chromium.org>
2020-06-19 21:11:16 +0000

    Refactoring: Move the Android autofill code to provider dir
    This should make the code structure more clear.
    
    Bug: 1096235
    Change-Id: Ie84293a0ffad11eb79301c638299e23aaa2522f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250704
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Tao Bai <michaelbai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780410}

--- components/autofill/android/provider/form_data_android.cc
+++ components/autofill/android/provider/form_data_android.cc
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/autofill/android/form_data_android.h"
+#include "components/autofill/android/provider/form_data_android.h"
 
 #include "base/android/jni_string.h"
-#include "components/autofill/android/form_field_data_android.h"
-#include "components/autofill/android/jni_headers/FormData_jni.h"
+#include "components/autofill/android/provider/form_field_data_android.h"
+#include "components/autofill/android/provider/jni_headers/FormData_jni.h"
 #include "components/autofill/core/browser/form_structure.h"
 
 using base::android::AttachCurrentThread;
