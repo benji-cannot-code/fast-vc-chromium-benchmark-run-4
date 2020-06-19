a816ab0b0af4688bcd167f60734b7de017200b4f
Xiaohan Wang <xhwang@chromium.org>
2020-06-19 21:16:34 +0000

    media: Pre-read CDM host file
    This would improve performance when reading these files.
    
    Bug: b/158337735
    Change-Id: I8fdf05c4cff2c05b7f717cd8c8d1b0189c9f6f69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236242
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780419}

--- media/cdm/cdm_host_file.cc
+++ media/cdm/cdm_host_file.cc
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/files/file_util.h"
 #include "base/logging.h"
+#include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "media/cdm/api/content_decryption_module_ext.h"
 
@@ -36,6 +38,9 @@ std::unique_ptr<CdmHostFile> CdmHostFile::Create(
                       base::File::FLAG_OPEN | base::File::FLAG_READ);
   DVLOG(1) << "  " << sig_file.IsValid() << ": "
            << sig_file_path.MaybeAsASCII();
+
+  // Preread file at |file_path| for better performance.
+  ignore_result(PreReadFile(file_path, /*is_executable=*/false));
 
   return std::unique_ptr<CdmHostFile>(
       new CdmHostFile(file_path, std::move(file), std::move(sig_file)));
