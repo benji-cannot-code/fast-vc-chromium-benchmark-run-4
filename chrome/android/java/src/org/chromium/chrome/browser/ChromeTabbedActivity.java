7db3ad30c1ff0d285f295d983bb6f33c7bd5e830
Lijin Shen <lazzzis@google.com>
2020-06-19 22:33:36 +0000

    Add metrics recording for default browser promo
    Add metrics for recording:
    1. the state of the default brwoser when promo is displayed
    2. the result of the promo
    3. the reason of dismissing the dialog
    
    Bug: 1090103
    Change-Id: I38c04bb12272a3128afcb6f0762c8ee16ea4b121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248453
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780478}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -152,6 +152,7 @@ import org.chromium.chrome.browser.toolbar.top.ToolbarControlContainer;
 import org.chromium.chrome.browser.ui.RootUiCoordinator;
 import org.chromium.chrome.browser.ui.TabObscuringHandler;
 import org.chromium.chrome.browser.ui.appmenu.AppMenuPropertiesDelegate;
+import org.chromium.chrome.browser.ui.default_browser_promo.DefaultBrowserPromoUtils;
 import org.chromium.chrome.browser.undo_tab_close_snackbar.UndoBarController;
 import org.chromium.chrome.browser.usage_stats.UsageStatsService;
 import org.chromium.chrome.browser.util.ChromeAccessibilityUtil;
@@ -1717,6 +1718,8 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
             ChromeSurveyController.initialize(mTabModelSelectorImpl);
 
             HomepagePromoVariationManager.getInstance().tagSyntheticHomepagePromoSeenGroup();
+
+            DefaultBrowserPromoUtils.maybeRecordOutcomeOnStart();
         });
 
         DeferredStartupHandler.getInstance().addDeferredTask(() -> {
