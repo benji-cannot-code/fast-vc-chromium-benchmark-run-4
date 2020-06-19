bf6f2caedb37c55a938031e1336fb9f78c25dabe
Kyle Horimoto <khorimoto@chromium.org>
2020-06-19 21:17:50 +0000

    [WebUI] Serve shared skia Mojo JS from chrome://resources/mojo/skia/
    This fixes an issue where both the New Tab Page and Chrome OS Settings
    independently included generated .mojom files from the same directory,
    which in some cases caused a race condition which broke the build.
    
    Fixed: 1096255
    Change-Id: I68c6bff99167025aea68effcecb1fc22d29e61fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251163
    Auto-Submit: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780422}

--- content/browser/webui/shared_resources_data_source.cc
+++ content/browser/webui/shared_resources_data_source.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_constants.h"
 #include "mojo/public/js/grit/mojo_bindings_resources.h"
 #include "mojo/public/js/grit/mojo_bindings_resources_map.h"
+#include "skia/grit/skia_resources.h"
+#include "skia/grit/skia_resources_map.h"
 #include "ui/base/layout.h"
 #include "ui/base/webui/web_ui_util.h"
 #include "ui/resources/grit/webui_resources.h"
@@ -140,6 +142,17 @@ const std::map<int, std::string> CreateMojoResourceIdToAliasMap() {
          "mojo/mojo/public/mojom/base/time.mojom-lite.js"},
 #endif  // defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX) ||
         //  defined(OS_ANDROID)
+  };
+}
+
+const std::map<int, std::string> CreateSkiaResourceIdToAliasMap() {
+  return std::map<int, std::string>{
+      {IDR_SKIA_BITMAP_MOJOM_LITE_JS,
+       "mojo/skia/public/mojom/bitmap.mojom-lite.js"},
+      {IDR_SKIA_IMAGE_INFO_MOJOM_LITE_JS,
+       "mojo/skia/public/mojom/image_info.mojom-lite.js"},
+      {IDR_SKIA_SKCOLOR_MOJOM_LITE_JS,
+       "mojo/skia/public/mojom/skcolor.mojom-lite.js"},
   };
 }
 
@@ -278,6 +291,8 @@ const ResourcesMap* CreateResourcesMap() {
   AddAliasedResourcesToMap(CreateMojoResourceIdToAliasMap(),
                            kMojoBindingsResources, kMojoBindingsResourcesSize,
                            result);
+  AddAliasedResourcesToMap(CreateSkiaResourceIdToAliasMap(), kSkiaResources,
+                           kSkiaResourcesSize, result);
 #if defined(OS_CHROMEOS)
   AddAliasedResourcesToMap(CreateChromeosMojoResourceIdToAliasMap(),
                            kChromeosResources, kChromeosResourcesSize, result);
--- tools/gritsettings/resource_ids.spec
+++ tools/gritsettings/resource_ids.spec
@@ -554,7 +554,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   "services/services_strings.grd": {
     "messages": [3460],
   },
-
+  "skia/skia_resources.grd": {
+    "includes": [3470],
+  },
   "third_party/blink/public/blink_image_resources.grd": {
     "structures": [3480],
   },
