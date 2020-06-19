8fc50a2436f21fdcf6a89125d76f819b4f3665e6
Tibor Goldschwendt <tiborg@chromium.org>
2020-06-19 19:30:04 +0000

    [webui][ntp] Support interactive doodles on themed NTPs
    Adds feature flag WebUIThemeModeDoodles, which enables showing Doodles
    on themed and dark mode NTPs. Furthermore, this CLs implements the dark
    mode change protocol for interactive doodles. The protocol looks as
    follows:
    
    1. Append theme_messages=0 to the doodle URL. This tells the doodle that
       the mode change messaging protocol with version 0 is enabled.
    
    2. Every time the mode changes postMessage
       {
         cmd: 'changeMode',
         dark: true|false,
       }
       to the doodle.
    
    3. If the doodle postMessages
       {
         cmd: 'sendMode',
       }
       respond with the postMessage from 2. above. This is to work around
       a race condition where the NTP could postMessage the mode before the
       doodle has installed a message listener.
    
    Bug: 688960
    Change-Id: Icc1e333f33fd1f01a2c8d1d4a581fdf360ca7090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2253220
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780392}

--- chrome/test/data/webui/new_tab_page/logo_test.js
+++ chrome/test/data/webui/new_tab_page/logo_test.js
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {$$, BrowserProxy} from 'chrome://new-tab-page/new_tab_page.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 import {assertNotStyle, assertStyle, createTestProxy, keydown} from 'chrome://test/new_tab_page/test_support.js';
 import {eventToPromise, flushTasks} from 'chrome://test/test_util.m.js';
 
@@ -62,7 +63,7 @@ function createImageDoodle(width, height) {
   };
 }
 
-suite('NewTabPageLogoTest', () => {
+function createSuite(themeModeDoodlesEnabled) {
   /**
    * @implements {BrowserProxy}
    * @extends {TestBrowserProxy}
@@ -78,6 +79,10 @@ suite('NewTabPageLogoTest', () => {
     await flushTasks();
     return logo;
   }
+
+  suiteSetup(() => {
+    loadTimeData.overrideValues({themeModeDoodlesEnabled});
+  });
 
   setup(() => {
     PolymerTest.clearBody();
@@ -174,15 +179,60 @@ suite('NewTabPageLogoTest', () => {
         }
       }
     });
+    logo.dark = false;
 
     // Assert.
     assertNotStyle($$(logo, '#doodle'), 'display', 'none');
     assertEquals($$(logo, '#logo'), null);
-    assertEquals($$(logo, '#iframe').src, 'https://foo.com/');
     assertNotStyle($$(logo, '#iframe'), 'display', 'none');
     assertStyle($$(logo, '#iframe'), 'width', '200px');
     assertStyle($$(logo, '#iframe'), 'height', '100px');
     assertStyle($$(logo, '#imageContainer'), 'display', 'none');
+    if (themeModeDoodlesEnabled) {
+      assertEquals(
+          $$(logo, '#iframe').src, 'https://foo.com/?theme_messages=0');
+      assertEquals(1, testProxy.getCallCount('postMessage'));
+      const [iframe, {cmd, dark}, origin] =
+          await testProxy.whenCalled('postMessage');
+      assertEquals($$(logo, '#iframe'), iframe);
+      assertEquals('changeMode', cmd);
+      assertEquals(false, dark);
+      assertEquals('https://foo.com', origin);
+    } else {
+      assertEquals($$(logo, '#iframe').src, 'https://foo.com/');
+    }
+  });
+
+  test('message only after mode has been set', async () => {
+    // Act (no mode).
+    const logo = await createLogo({
+      content: {
+        interactiveDoodle: {
+          url: {url: 'https://foo.com'},
+          width: 200,
+          height: 100,
+        }
+      }
+    });
+
+    // Assert (no mode).
+    assertEquals(0, testProxy.getCallCount('postMessage'));
+
+    // Act (setting mode).
+    logo.dark = true;
+
+    // Assert (setting mode).
+    if (themeModeDoodlesEnabled) {
+      assertEquals(1, testProxy.getCallCount('postMessage'));
+      const [iframe, {cmd, dark}, origin] =
+          await testProxy.whenCalled('postMessage');
+      assertEquals($$(logo, '#iframe'), iframe);
+      assertEquals('changeMode', cmd);
+      assertEquals(true, dark);
+      assertEquals('https://foo.com', origin);
+    } else {
+      assertEquals(0, testProxy.getCallCount('postMessage'));
+    }
   });
 
   test('disallowing doodle shows logo', async () => {
@@ -294,6 +344,30 @@ suite('NewTabPageLogoTest', () => {
     // Assert.
     assertEquals($$(logo, '#iframe').offsetHeight, height);
     assertEquals($$(logo, '#iframe').offsetWidth, width);
+  });
+
+  test('receiving mode message sends mode', async () => {
+    // Arrange.
+    const logo = await createLogo(
+        {content: {interactiveDoodle: {url: {url: 'https://foo.com'}}}});
+    logo.dark = false;
+    testProxy.resetResolver('postMessage');
+
+    // Act.
+    window.postMessage({cmd: 'sendMode'}, '*');
+    await flushTasks();
+
+    // Assert.
+    if (themeModeDoodlesEnabled) {
+      assertEquals(1, testProxy.getCallCount('postMessage'));
+      const [_, {cmd, dark}, origin] =
+          await testProxy.whenCalled('postMessage');
+      assertEquals('changeMode', cmd);
+      assertEquals(false, dark);
+      assertEquals('https://foo.com', origin);
+    } else {
+      assertEquals(0, testProxy.getCallCount('postMessage'));
+    }
   });
 
   test('clicking simple doodle opens link', async () => {
@@ -511,5 +585,14 @@ suite('NewTabPageLogoTest', () => {
     assertEquals(newTabPage.mojom.DoodleShareChannel.TWITTER, channel);
     assertEquals('supi', doodleId);
     assertEquals('123', shareId);
+  });
+}
+
+suite('NewTabPageLogoTest', () => {
+  [true, false].forEach(themeModeDoodlesEnabled => {
+    const enabled = themeModeDoodlesEnabled ? 'enabled' : 'disabled';
+    suite(`theme mode doodles ${enabled}`, () => {
+      createSuite(themeModeDoodlesEnabled);
+    });
   });
 });
