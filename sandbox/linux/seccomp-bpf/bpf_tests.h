26461dd137a183dc95609d3001c3445640cf4a2a
Hans Wennborg <hans@chromium.org>
2020-06-19 21:28:10 +0000

    Remove/replace unnecessary includes of logging.h (sandbox/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: Ie7d23361d3fdf85452b0147ba0b67149c1b8b5b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254030
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780439}

--- sandbox/linux/seccomp-bpf/bpf_tests.h
+++ sandbox/linux/seccomp-bpf/bpf_tests.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#include "base/logging.h"
+#include "base/check.h"
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "sandbox/linux/seccomp-bpf/bpf_tester_compatibility_delegate.h"
