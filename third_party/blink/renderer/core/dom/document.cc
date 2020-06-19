f711e45aac43116d769c1f2ff877d993589363f1
Tal Pressman <talp@chromium.org>
2020-06-19 07:28:48 +0000

    Remove unused include (thread_scheduler.h).
    Change-Id: I9689fd0a2501e810e0ea1ac91d8042e697b5f422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2253599
    Commit-Queue: Tal Pressman <talp@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Tal Pressman <talp@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780200}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -315,7 +315,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/scheduler/public/event_loop.h"
 #include "third_party/blink/renderer/platform/scheduler/public/frame_or_worker_scheduler.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cross_thread_task.h"
-#include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
 #include "third_party/blink/renderer/platform/text/platform_locale.h"
 #include "third_party/blink/renderer/platform/web_test_support.h"
 #include "third_party/blink/renderer/platform/weborigin/origin_access_entry.h"
