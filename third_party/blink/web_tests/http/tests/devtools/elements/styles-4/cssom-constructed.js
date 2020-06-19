847d8c647fd466f5f8ac46d64823322cde33fa6d
Alex Rudenko <alexrudenko@chromium.org>
2020-06-19 10:06:21 +0000

    Send CSS.styleSheetChanged when a stylesheet is mutated
    Sending CSS.styleSheetChanged allows DevTools to pick up the latest
    styles from the mutated stylesheet.
    
    Fixed: 1096371
    Change-Id: I9acdee212309e2f6f98cdf8bdbbecf9046fd01f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2253739
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780240}

--- third_party/blink/web_tests/http/tests/devtools/elements/styles-4/cssom-constructed.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/styles-4/cssom-constructed.js
@@ -16,10 +16,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       document.adoptedStyleSheets = [s];
   `);
 
-  ElementsTestRunner.selectNodeAndWaitForStyles('inspected', dump);
+  ElementsTestRunner.selectNodeAndWaitForStyles('inspected', modify);
 
-  async function dump() {
+  async function modify() {
     await ElementsTestRunner.dumpSelectedElementStyles(true);
-    TestRunner.completeTest();
+
+    TestRunner.cssModel.addEventListener(SDK.CSSModel.Events.StyleSheetChanged, onStyleSheetChanged, this);
+
+    function onStyleSheetChanged(event) {
+      TestRunner.addResult('StyleSheetChanged triggered');
+      TestRunner.completeTest();
+    }
+
+    await TestRunner.evaluateInPagePromise(`
+      s.insertRule('div {color: green}');
+    `);
   }
 })();
