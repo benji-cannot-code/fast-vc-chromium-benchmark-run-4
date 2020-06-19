40acedcc250935ab8b981df7728f0a3090e2896d
Lingqi Chi <lingqi@chromium.org>
2020-06-19 07:20:18 +0000

    Network Service: Use allowlist for IgnoreErrorsCertVerifier
    This CL replaces "whitelist" with "allowlist" in
    IgnoreErrorsCertVerifier.
    
    Bug: 842296
    Change-Id: I501b451fbb76dc48e5977b2a855913ee79028462
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247947
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780198}

--- services/network/ignore_errors_cert_verifier.h
+++ services/network/ignore_errors_cert_verifier.h
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace network {
 
 // IgnoreErrorsCertVerifier wraps another CertVerifier in order to ignore
-// verification errors from certificate chains that match a whitelist of SPKI
+// verification errors from certificate chains that match a allowlist of SPKI
 // fingerprints.
 class COMPONENT_EXPORT(NETWORK_SERVICE) IgnoreErrorsCertVerifier
     : public net::CertVerifier {
@@ -29,7 +29,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) IgnoreErrorsCertVerifier
   // --user-data-dir flag is missing, or --ignore-certificate-errors-spki-list
   // flag is missing then MaybeWrapCertVerifier returns the supplied verifier.
   // Otherwise it returns an IgnoreErrorsCertVerifier wrapping the supplied
-  // verifier using the whitelist from the
+  // verifier using the allowlist from the
   // --ignore-certificate-errors-spki-list flag.
   //
   // As the --user-data-dir flag is embedder defined, the flag to check for
@@ -40,13 +40,13 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) IgnoreErrorsCertVerifier
       std::unique_ptr<net::CertVerifier> verifier);
 
   IgnoreErrorsCertVerifier(std::unique_ptr<net::CertVerifier> verifier,
-                           SPKIHashSet whitelist);
+                           SPKIHashSet allowlist);
 
   ~IgnoreErrorsCertVerifier() override;
 
   // Verify skips certificate verification and returns OK if any of the
   // certificates from the chain in |params| match one of the SPKI fingerprints
-  // from the whitelist. Otherwise, it invokes Verify on the wrapped verifier
+  // from the allowlist. Otherwise, it invokes Verify on the wrapped verifier
   // and returns the result.
   int Verify(const RequestParams& params,
              net::CertVerifyResult* verify_result,
@@ -58,10 +58,10 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) IgnoreErrorsCertVerifier
  private:
   friend class IgnoreErrorsCertVerifierTest;
 
-  void SetWhitelistForTesting(const SPKIHashSet& whitelist);
+  void SetAllowlistForTesting(const SPKIHashSet& whitelist);
 
   std::unique_ptr<net::CertVerifier> verifier_;
-  SPKIHashSet whitelist_;
+  SPKIHashSet allowlist_;
 
   DISALLOW_COPY_AND_ASSIGN(IgnoreErrorsCertVerifier);
 };
