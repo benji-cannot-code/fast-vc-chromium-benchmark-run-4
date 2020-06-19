4c111b19ae4483a0063f68298a71a6f1530a7edd
Hans Wennborg <hans@chromium.org>
2020-06-19 21:30:18 +0000

    Remove/replace unnecessary includes of logging.h (remoting/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: If603687117132c2449e3088e2bb28566431aed9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254149
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780443}

--- remoting/base/result.h
+++ remoting/base/result.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <utility>
 
-#include "base/logging.h"
+#include "base/check.h"
 #include "base/optional.h"
 
 // Result<SuccessType, ErrorType> represents the success or failure of an
