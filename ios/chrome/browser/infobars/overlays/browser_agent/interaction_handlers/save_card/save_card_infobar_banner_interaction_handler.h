b5e118b116d7e396abe09ac6139f5af5843eaae6
Chris Lu <thegreenfrog@chromium.org>
2020-06-19 17:54:34 +0000

    [ios] Create SaveCard Overlay Banner Interaction Handlers
    This change adds SaveCardInfobarBannerInteractionHandler and
    SaveCardInfobarBannerOverlayRequestCallbackInstaller to handle
    banner responses.
    
    Mock delegate and InteractionHandler classes are added to allow
    for testing of the added classes.
    
    Bug: 1071914
    Change-Id: I4d58b9443458946a9001aa73f6e79015e8198db6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250896
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780368}

---
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/save_card/save_card_infobar_banner_interaction_handler.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_SAVE_CARD_SAVE_CARD_INFOBAR_BANNER_INTERACTION_HANDLER_H_
+#define IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_SAVE_CARD_SAVE_CARD_INFOBAR_BANNER_INTERACTION_HANDLER_H_
+
+#include <string.h>
+
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/common/infobar_banner_interaction_handler.h"
+
+class InfobarBannerOverlayRequestCallbackInstaller;
+
+namespace autofill {
+class AutofillSaveCardInfoBarDelegateMobile;
+}
+
+// Helper object that updates the model layer for interaction events with the
+// SaveCard infobar banner UI.
+class SaveCardInfobarBannerInteractionHandler
+    : public InfobarBannerInteractionHandler {
+ public:
+  SaveCardInfobarBannerInteractionHandler();
+  ~SaveCardInfobarBannerInteractionHandler() override;
+
+  // Instructs the handler to update the credentials with |cardholder_name|,
+  // |expiration_date_month|, and |expiration_date_year|. This replaces
+  // MainButtonTapped.
+  virtual void SaveCredentials(InfoBarIOS* infobar,
+                               base::string16 cardholder_name,
+                               base::string16 expiration_date_month,
+                               base::string16 expiration_date_year);
+
+ private:
+  // InfobarBannerInteractionHandler:
+  std::unique_ptr<InfobarBannerOverlayRequestCallbackInstaller>
+  CreateBannerInstaller() override;
+
+  // Returns the SaveCard delegate from |infobar|.
+  autofill::AutofillSaveCardInfoBarDelegateMobile* GetInfobarDelegate(
+      InfoBarIOS* infobar);
+};
+
+#endif  // IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_SAVE_CARD_SAVE_CARD_INFOBAR_BANNER_INTERACTION_HANDLER_H_
---
+++ ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/test/mock_save_card_banner_infobar_interaction_handler.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_TEST_MOCK_SAVE_CARD_BANNER_INFOBAR_INTERACTION_HANDLER_H_
+#define IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_TEST_MOCK_SAVE_CARD_BANNER_INFOBAR_INTERACTION_HANDLER_H_
+
+#import "ios/chrome/browser/infobars/overlays/browser_agent/interaction_handlers/save_card/save_card_infobar_banner_interaction_handler.h"
+
+#include <string.h>
+
+#include "testing/gmock/include/gmock/gmock.h"
+
+class InfoBarIOS;
+
+// Mock version of SaveCardInfobarBannerInteractionHandler for use in tests.
+class MockSaveCardInfobarBannerInteractionHandler
+    : public SaveCardInfobarBannerInteractionHandler {
+ public:
+  MockSaveCardInfobarBannerInteractionHandler();
+  ~MockSaveCardInfobarBannerInteractionHandler() override;
+
+  MOCK_METHOD4(SaveCredentials,
+               void(InfoBarIOS* infobar,
+                    base::string16 cardholder_name,
+                    base::string16 expiration_date_month,
+                    base::string16 expiration_date_year));
+};
+
+#endif  // IOS_CHROME_BROWSER_INFOBARS_OVERLAYS_BROWSER_AGENT_INTERACTION_HANDLERS_TEST_MOCK_SAVE_CARD_BANNER_INFOBAR_INTERACTION_HANDLER_H_
