2a17ba1f245391314833e4cbc34ccb0fccf69abc
Hans Wennborg <hans@chromium.org>
2020-06-19 21:15:39 +0000

    Remove/replace unnecessary includes of logging.h (gin/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: I88a9ca7c6ffd492c741bfbf067c4e006589e8e7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254661
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780418}

--- gin/converter.h
+++ gin/converter.h
@@ -13,7 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <vector>
 
-#include "base/logging.h"
+#include "base/check.h"
+#include "base/notreached.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_piece.h"
 #include "gin/gin_export.h"
