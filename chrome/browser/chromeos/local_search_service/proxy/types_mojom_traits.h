ead4b7f098e25a8e640a0cc35b96ddf844015aa0
Rachel Wong <wrong@chromium.org>
2020-06-19 08:24:34 +0000

    [search service] Add and implement proxy interface.
    This CL defines and implements the search service proxy API.
    - IndexProxy implements an interface that is a wrapper around an Index;
    - LocalSearchServiceProxy implements an interface that is a wrapper
      around LocalSearchService and owns various IndexProxies;
    - LocalSearchServiceProxyFactory is responsible for the creation of
      LocalSearchServiceProxies.
    
    Bug: 1092767
    Change-Id: Icb2fa359fe9d05bc4ae5b5bc977550ed659e8262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249428
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780210}

--- chrome/browser/chromeos/local_search_service/proxy/types_mojom_traits.h
+++ chrome/browser/chromeos/local_search_service/proxy/types_mojom_traits.h
@@ -8,10 +8,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/local_search_service/index.h"
 #include "chrome/browser/chromeos/local_search_service/local_search_service.h"
+#include "chrome/browser/chromeos/local_search_service/proxy/local_search_service_proxy.mojom.h"
 #include "chrome/browser/chromeos/local_search_service/proxy/types.mojom.h"
 #include "mojo/public/cpp/bindings/struct_traits.h"
 
 namespace mojo {
+
+// TODO(crbug/1092767): Consolidate the API to use mojo enums instead of
+// EnumTraits.
+
+template <>
+struct EnumTraits<local_search_service::mojom::IndexId,
+                  local_search_service::IndexId> {
+  static local_search_service::mojom::IndexId ToMojom(
+      local_search_service::IndexId input);
+  static bool FromMojom(local_search_service::mojom::IndexId input,
+                        local_search_service::IndexId* output);
+};
 
 template <>
 struct StructTraits<local_search_service::mojom::ContentDataView,
