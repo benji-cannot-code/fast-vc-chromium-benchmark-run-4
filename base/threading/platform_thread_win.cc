71cafc4dda3aea9fbd5d5cd79de1468c7390ae05
calamity <calamity@chromium.org>
2020-06-19 03:36:09 +0000

    Revert "Avoid running TLS destructors at background priority"
    This reverts commit 8e4ff34877b17390891a690a3b61aa95e163d574.
    
    Reason for revert: Causing failures for VaryingFileSize/StorageQueueTest.WriteAndRepeatedlyImmediateUploadWithConfirmations/0
    VaryingFileSize/StorageQueueTest.WriteAndRepeatedlyUploadWithConfirmations/1
    VaryingFileSize/StorageQueueTest.WriteIntoNewStorageQueueAndUpload/2
    
    See https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%28dbg%29%281%29/83037
    
    
    Original change's description:
    > Avoid running TLS destructors at background priority
    > 
    > This CL is adding a ScopedThreadPriority that save the current
    > thread priority and restore it when leaving the scope. This is
    > required since it is possible to run code outside of the Run(...)
    > scope with Background priority.
    > 
    > On Windows, this is causing shutdown hangs because the LoaderLock
    > can be held by a background thread while doing TLS destruction.
    > 
    > 
    > Without this fix, the DCHECK in the code run with Background priority
    > and fails:
    > 
    > [9888:28656:0617/210735.064:FATAL:thread_local_storage.cc(331)] Check failed: PlatformThread::GetCurrentThreadPriority() != ThreadPriority::BACKGROUND (0 vs. 0)
    > Backtrace:
    >         base::debug::CollectStackTrace [0x00007FF92445E7D2+18] (C:\src\chromium\src\base\debug\stack_trace_win.cc:284)
    >         base::debug::StackTrace::StackTrace [0x00007FF9243668F2+18] (C:\src\chromium\src\base\debug\stack_trace.cc:203)
    >         logging::LogMessage::~LogMessage [0x00007FF92437F094+148] (C:\src\chromium\src\base\logging.cc:605)
    >         logging::LogMessage::~LogMessage [0x00007FF92437FE40+16] (C:\src\chromium\src\base\logging.cc:598)
    >         base::internal::PlatformThreadLocalStorage::OnThreadExit [0x00007FF9244425FA+170] (C:\src\chromium\src\base\threading\thread_local_storage.cc:333)
    >         RtlActivateActivationContextUnsafeFast [0x00007FF96440B583+291]
    >         RtlActivateActivationContextUnsafeFast [0x00007FF96440B67F+543]
    >         LdrShutdownThread [0x00007FF964408B8C+348]
    >         RtlExitUserThread [0x00007FF9644436FE+62]
    >         BaseThreadInitThunk [0x00007FF96240403C+28]
    >         RtlUserThreadStart [0x00007FF964443691+33]
    > 
    > Bug: 1096203
    > Change-Id: Ibc011ac370dde4b6a872da6ac81938f52965a5a6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251199
    > Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: François Doray <fdoray@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#779932}
    
    TBR=gab@chromium.org,fdoray@chromium.org,etienneb@chromium.org
    
    Change-Id: I7b26100abe6bc5fc53a74d056a530175695d0378
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1096203
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251570
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780155}

--- base/threading/platform_thread_win.cc
+++ base/threading/platform_thread_win.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/scoped_blocking_call.h"
-#include "base/threading/scoped_thread_priority.h"
 #include "base/threading/thread_id_name_manager.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/time/time_override.h"
@@ -116,13 +115,6 @@ DWORD __stdcall ThreadFunc(void* params) {
         scoped_platform_handle.Get(),
         PlatformThread::CurrentId());
   }
-
-  // Ensure thread priority is at least NORMAL before initiating thread
-  // destruction. Thread destruction on Windows holds the LdrLock while
-  // performing TLS destruction which causes hangs if performed at background
-  // priority (priority inversion) (see: http://crbug.com/1096203).
-  if (PlatformThread::GetCurrentThreadPriority() < ThreadPriority::NORMAL)
-    PlatformThread::SetCurrentThreadPriority(ThreadPriority::NORMAL);
 
   return 0;
 }
