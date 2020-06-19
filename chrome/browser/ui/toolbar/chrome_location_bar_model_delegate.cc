af06a021537e07aa2da50523f4975f2e2e9ba1d8
Livvie Lin <livvielin@chromium.org>
2020-06-19 21:33:37 +0000

    Check if context menu feature is enabled in ShouldPreventElision
    Makes sure that the URL remains elided if the context menu
    option is removed, especially if the "Always Show Full URLs"
    pref was previously turned on.
    
    Bug: 1095975
    Change-Id: Id5ab1d5c384483286bebb3e3e36cf5880db975d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252932
    Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780449}

--- chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.cc
+++ chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.h"
 
 #include "base/check.h"
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "build/build_config.h"
 #include "chrome/browser/autocomplete/autocomplete_classifier_factory.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_pages/buildflags/buildflags.h"
 #include "components/omnibox/browser/autocomplete_input.h"
 #include "components/omnibox/browser/omnibox_prefs.h"
+#include "components/omnibox/common/omnibox_features.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "components/security_interstitials/content/security_interstitial_tab_helper.h"
@@ -77,6 +79,14 @@ bool ChromeLocationBarModelDelegate::GetURL(GURL* url) const {
 }
 
 bool ChromeLocationBarModelDelegate::ShouldPreventElision() {
+  // Don't record a histogram or prevent elision if the user is in a state
+  // where the show full URLs pref is enabled but the context menu option is
+  // disabled.
+  if (!base::FeatureList::IsEnabled(omnibox::kOmniboxContextMenuShowFullUrls) &&
+      GetElisionConfig() == ELISION_CONFIG_TURNED_OFF_BY_PREF) {
+    return false;
+  }
+
   RecordElisionConfig();
   if (GetElisionConfig() != ELISION_CONFIG_DEFAULT) {
     return true;
