f5e43ed3c6e94db9767ef17b832ccb49de70e8b9
Hans Wennborg <hans@chromium.org>
2020-06-19 21:32:51 +0000

    Remove/replace unnecessary includes of logging.h (extensions/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: If8491a48b188182c4faf21ef2a77105aa849c2b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254660
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780448}

--- extensions/browser/api/declarative/deduping_factory.h
+++ extensions/browser/api/declarative/deduping_factory.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_map>
 #include <unordered_set>
 
+#include "base/check.h"
 #include "base/compiler_specific.h"
-#include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/stl_util.h"
--- extensions/common/constants.h
+++ extensions/common/constants.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_COMMON_CONSTANTS_H_
 
 #include "base/files/file_path.h"
-#include "base/logging.h"
 #include "base/strings/string_piece_forward.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
 #include "components/version_info/channel.h"
