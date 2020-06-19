157c71c9099c86457505f6ff7ea547d16f83b8ce
Majid Valipour <majidvp@chromium.org>
2020-06-19 21:21:01 +0000

    WebOTP: Remove extraneous call to TimeTicks:Now
    TimeTicks::Now() is fairly cheap but not calling it twice is even
    cheaper.
    
    This CL is expected to have no functional impact.
    
    Change-Id: I3b230d1ff22d29e88b594205c825bcbe68b6f558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252746
    Auto-Submit: Majid Valipour <majidvp@chromium.org>
    Reviewed-by: Sam Goto <goto@chromium.org>
    Commit-Queue: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780427}

--- content/browser/sms/sms_service.cc
+++ content/browser/sms/sms_service.cc
@@ -104,7 +104,8 @@ void SmsService::OnReceive(const std::string& one_time_code) {
   DCHECK(!one_time_code_);
   DCHECK(!start_time_.is_null());
 
-  RecordSmsReceiveTime(base::TimeTicks::Now() - start_time_);
+  auto now = base::TimeTicks::Now();
+  RecordSmsReceiveTime(now - start_time_);
 
   one_time_code_ = one_time_code;
 
@@ -115,7 +116,7 @@ void SmsService::OnReceive(const std::string& one_time_code) {
     return;
   }
 
-  receive_time_ = base::TimeTicks::Now();
+  receive_time_ = now;
   OpenInfoBar(one_time_code);
 }
 
