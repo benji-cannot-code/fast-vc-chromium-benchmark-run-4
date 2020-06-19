6e67ecc39531f048c057dfe47079e38a861a0775
Thomas Tellier <tellier@google.com>
2020-06-19 15:36:44 +0000

    Add disable by default feature flag for login display password button
    This CL reintroduces the submit button on the login / lock screen password textfield and disable the login display password button feature by default, by putting it under a feature flag.
    
    Bug: 1090319
    Change-Id: I521a19262726f7c0566d824b0efe5aa1c9abb7a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247826
    Commit-Queue: Thomas Tellier <tellier@google.com>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780311}

--- ash/login/ui/login_auth_user_view_unittest.cc
+++ ash/login/ui/login_auth_user_view_unittest.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/dbus/power/fake_power_manager_client.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/events/event_utils.h"
@@ -191,6 +193,39 @@ TEST_F(LoginAuthUserViewUnittest,
 }
 
 TEST_F(LoginAuthUserViewUnittest, PasswordFieldChangeOnUpdateUser) {
+  LoginAuthUserView::TestApi test_auth_user_view(view_);
+  LoginPasswordView::TestApi password_test(test_auth_user_view.password_view());
+
+  const auto password = base::ASCIIToUTF16("abc1");
+  password_test.textfield()->SetText(password);
+  view_->UpdateForUser(user_);
+  EXPECT_EQ(password_test.textfield()->GetText(), password);
+
+  auto another_user = CreateUser("user2@domain.com");
+  view_->UpdateForUser(another_user);
+  EXPECT_TRUE(password_test.textfield()->GetText().empty());
+}
+
+// LoginAuthUserViewUnittest with display password button feature enabled.
+class LoginAuthUserViewUnittestFeatureEnabled
+    : public LoginAuthUserViewUnittest {
+ protected:
+  LoginAuthUserViewUnittestFeatureEnabled() {
+    feature_list_.InitWithFeatures(
+        {chromeos::features::kLoginDisplayPasswordButton}, {});
+  }
+  LoginAuthUserViewUnittestFeatureEnabled(
+      const LoginAuthUserViewUnittestFeatureEnabled&) = delete;
+  LoginAuthUserViewUnittestFeatureEnabled& operator=(
+      const LoginAuthUserViewUnittestFeatureEnabled&) = delete;
+  ~LoginAuthUserViewUnittestFeatureEnabled() override = default;
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+TEST_F(LoginAuthUserViewUnittestFeatureEnabled,
+       PasswordFieldChangeOnUpdateUser) {
   LoginAuthUserView::TestApi test_auth_user_view(view_);
   LoginPasswordView::TestApi password_test(test_auth_user_view.password_view());
 
