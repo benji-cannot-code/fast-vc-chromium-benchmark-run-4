27b025ae969f9e74e36aefe1d44ab48a14c392f2
Scott Violet <sky@chromium.org>
2020-06-19 21:12:17 +0000

    weblayer: remove AccessibilityUtil
    I refactored the chrome code, so the WebLayer copy is no longer
    necessary.
    
    BUG=none
    TEST=none
    
    Change-Id: I191b3159ec92355ff4ef6014c245f811226e491e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254666
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780411}

--- weblayer/browser/java/org/chromium/weblayer_private/WebLayerAccessibilityUtil.java
+++ weblayer/browser/java/org/chromium/weblayer_private/WebLayerAccessibilityUtil.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.weblayer_private;
 
+import org.chromium.ui.util.AccessibilityUtil;
+
 /**
  * Exposes information about the current accessibility state.
  */
