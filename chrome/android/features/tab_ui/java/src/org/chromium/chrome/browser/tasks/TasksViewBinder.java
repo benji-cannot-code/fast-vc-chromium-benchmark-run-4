f58839cf26eaa3f922c42d2944814494f76aa914
gogerald <gogerald@google.com>
2020-06-19 19:16:07 +0000

    Reland "[StartSurface] Switch to normal tab model on resume to V2 start surface"
    This is a reland of 9b955d94b8a295dcb636f5b63b513ebc267d4936
    
    Original change's description:
    > [StartSurface] Switch to normal tab model on resume to V2 start surface
    > 
    > This CL also solves the scroll issue of the incognito description.
    > 
    > Screen records:
    > Resume:
    > https://drive.google.com/file/d/1jPFCAEwTDzI4PO-Ef-HdvRV6KeZoW4fD/view?usp=sharing
    > 
    > Scroll:
    > https://drive.google.com/file/d/1C2lFMBKmdTaipWcHBG9mAYCJNuhoiYK_/view?usp=sharing
    > 
    > Bug: 1093506
    > Change-Id: I5d7365daa530b23f897a19665076cbc32238224f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2243334
    > Commit-Queue: Ganggui Tang <gogerald@chromium.org>
    > Auto-Submit: Ganggui Tang <gogerald@chromium.org>
    > Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    > Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#779878}
    
    Bug: 1093506
    Change-Id: Ie49546277d347eea6682064a41a859f52b882a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254578
    Auto-Submit: Ganggui Tang <gogerald@chromium.org>
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780386}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/TasksViewBinder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/TasksViewBinder.java
@@ -18,6 +18,7 @@ import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_FAKE_S
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_INCOGNITO;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_INCOGNITO_DESCRIPTION_INITIALIZED;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_INCOGNITO_DESCRIPTION_VISIBLE;
+import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_SURFACE_BODY_VISIBLE;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_TAB_CAROUSEL_VISIBLE;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.IS_VOICE_RECOGNITION_BUTTON_VISIBLE;
 import static org.chromium.chrome.browser.tasks.TasksSurfaceProperties.MORE_TABS_CLICK_LISTENER;
@@ -75,6 +76,8 @@ class TasksViewBinder {
                 model.get(INCOGNITO_COOKIE_CONTROLS_MANAGER).updateIfNecessary();
             }
             view.setIncognitoDescriptionVisibility(isVisible);
+        } else if (propertyKey == IS_SURFACE_BODY_VISIBLE) {
+            view.setSurfaceBodyVisibility(model.get(IS_SURFACE_BODY_VISIBLE));
         } else if (propertyKey == IS_TAB_CAROUSEL_VISIBLE) {
             view.setTabCarouselVisibility(model.get(IS_TAB_CAROUSEL_VISIBLE));
         } else if (propertyKey == IS_VOICE_RECOGNITION_BUTTON_VISIBLE) {
