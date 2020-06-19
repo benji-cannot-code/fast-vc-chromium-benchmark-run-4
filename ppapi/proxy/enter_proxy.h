fe86d377ae7c6502c6577587499c27b6cc562cf7
Hans Wennborg <hans@chromium.org>
2020-06-19 21:39:34 +0000

    Remove/replace unnecessary includes of logging.h (ppapi/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: I36a2cdc31d2b641f3ba3d13cefcdaa9d942ed9ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254126
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780456}

--- ppapi/proxy/enter_proxy.h
+++ ppapi/proxy/enter_proxy.h
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#include "base/logging.h"
+#include "base/check.h"
+#include "base/notreached.h"
 #include "ppapi/cpp/completion_callback.h"
 #include "ppapi/proxy/host_dispatcher.h"
 #include "ppapi/proxy/plugin_dispatcher.h"
