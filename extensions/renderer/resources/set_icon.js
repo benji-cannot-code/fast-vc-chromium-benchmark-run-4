bbfc52d0300b85dfe5fa51355d241096cd5163c5
Devlin Cronin <rdevlin.cronin@chromium.org>
2020-06-19 15:43:48 +0000

    [Extensions] Fix unsafe variable access in set_icon.js
    set_icon.js checked the `self` variable to determine whether the script
    was running in a ServiceWorker (such as a ServiceWorker-backed
    extension). However, this is unsafe because, unlike `window`, `self` can
    be overwritten by other script. If an extension specified a value for
    `self`, that would be used instead - and would subsequently crash if the
    value didn't support the `in` operator (i.e., wasn't an Object).
    
    When this happened, the custom bindings would fail to register any
    hooks. This, then, would result in API calls to browserAction.setIcon()
    in passing the arguments directly to the browser-side handling. When
    this happened, the browser would classify it as a bad message (because
    it didn't undergo the expected argument modifications), and would kill
    the renderer.
    
    Fix this by adding a native binding to return whether the script is
    running in a ServiceWorker context. Add a regression test.
    
    Bug: 1087948
    Change-Id: I1bb593af735dec6d7a2a25050e62677f23c23dce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2253284
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780315}

--- extensions/renderer/resources/set_icon.js
+++ extensions/renderer/resources/set_icon.js
@@ -4,9 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 var exceptionHandler = require('uncaught_exception_handler');
-var SetIconCommon = requireNative('setIcon').SetIconCommon;
-
-var inServiceWorker = 'ServiceWorkerGlobalScope' in self;
+var natives = requireNative('setIcon');
+var SetIconCommon = natives.SetIconCommon;
+var inServiceWorker = natives.IsInServiceWorker();
 
 function loadImagePathForServiceWorker(path, callback, failureCallback) {
   let fetchPromise = fetch(path);
