4fcd94e25e43ba23e3fd405dcf60775a34eee7f1
Will Cassella <cassew@google.com>
2020-06-19 16:37:04 +0000

    Prevent poster rendering over video if LoadTimerFired runs after Play
    If HTMLMediaElement::Play is called before
    HTMLMediaElement::LoadTimerFired is run, then LoadTimerFired will
    set the 'show-poster-flag' to 'true', and nothing will unset it even
    after playback begins. This was because the call to
    'SetShowPosterFlag(true)' was in the wrong location, this CL moves it to
    the right location, and adds a test to prevent regression.
    
    Bug: 1094778, 1095320
    Change-Id: Ia1c558a35f2816136994c0013061d2e00ae90ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249459
    Commit-Queue: Will Cassella <cassew@google.com>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780338}

--- third_party/blink/renderer/core/html/media/autoplay_policy.cc
+++ third_party/blink/renderer/core/html/media/autoplay_policy.cc
@@ -345,6 +345,7 @@ void AutoplayPolicy::OnIntersectionChangedForAutoplay(
 
   if (ShouldAutoplay()) {
     element_->paused_ = false;
+    element_->SetShowPosterFlag(false);
     element_->ScheduleEvent(event_type_names::kPlay);
     element_->ScheduleNotifyPlaying();
 
--- third_party/blink/renderer/core/html/media/html_media_element.cc
+++ third_party/blink/renderer/core/html/media/html_media_element.cc
@@ -1186,8 +1186,6 @@ void HTMLMediaElement::LoadResource(const WebMediaPlayerSource& source,
 
   StartProgressEventTimer();
 
-  SetShowPosterFlag(true);
-
   SetPlayerPreload();
 
   DCHECK(!media_source_);
@@ -1532,11 +1530,14 @@ void HTMLMediaElement::WaitForSourceChange() {
   StopPeriodicTimers();
   load_state_ = kWaitingForSource;
 
-  // 6.17 - Waiting: Set the element's networkState attribute to the
+  // 17 - Waiting: Set the element's networkState attribute to the
   // NETWORK_NO_SOURCE value
   SetNetworkState(kNetworkNoSource);
 
-  // 6.18 - Set the element's delaying-the-load-event flag to false. This stops
+  // 18 - Set the element's show poster flag to true.
+  SetShowPosterFlag(true);
+
+  // 19 - Set the element's delaying-the-load-event flag to false. This stops
   // delaying the load event.
   SetShouldDelayLoadEvent(false);
 
@@ -1942,6 +1943,9 @@ void HTMLMediaElement::SetReadyState(ReadyState state) {
 }
 
 void HTMLMediaElement::SetShowPosterFlag(bool value) {
+  DVLOG(3) << "SetShowPosterFlag(" << *this << ", " << value
+           << ") - current state is " << show_poster_flag_;
+
   if (value == show_poster_flag_)
     return;
 
