e12e0ad7bde4c097facc654e2f5b5d51ed61361e
Rushan Suleymanov <rushans@google.com>
2020-06-19 16:25:36 +0000

    [Sync] Add UMA histogram to count bookmarks without full title.
    This patch introduces two histograms for initial merge and remote
    update which counts number of remote specifics without full title:
    * Sync.BookmarkWithoutFullTitle.OnRemoteUpdate
    * Sync.BookmarkWithoutFullTitle.OnInitialMerge
    
    Bug: 1061411
    Change-Id: Ife656adc183473a4860ee15a4941d9b25cb01061
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252048
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/master@{#780330}

--- components/sync_bookmarks/bookmark_model_merger.cc
+++ components/sync_bookmarks/bookmark_model_merger.cc
@@ -589,6 +589,7 @@ void BookmarkModelMerger::MergeSubtree(
   if (!local_subtree_root->is_permanent_node() &&
       IsFullTitleReuploadNeeded(remote_update_entity.specifics.bookmark())) {
     bookmark_tracker_->IncrementSequenceNumber(entity);
+    ++valid_updates_without_full_title_;
   }
 
   // If there are remote child updates, try to match them.
@@ -741,6 +742,7 @@ void BookmarkModelMerger::ProcessRemoteCreation(
       specifics);
   if (IsFullTitleReuploadNeeded(specifics.bookmark())) {
     bookmark_tracker_->IncrementSequenceNumber(entity);
+    ++valid_updates_without_full_title_;
   }
 
   // Recursively, match by GUID or, if not possible, create local node for all
