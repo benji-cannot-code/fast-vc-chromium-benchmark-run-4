e02dac2fc3bd72b7bc6f88eea04d421b76aa411c
Dale Curtis <dalecurtis@chromium.org>
2020-06-20 00:55:30 +0000

    Add decodeMetadata() method to ImageDecoder.
    Allows API users with ReadableStreams as input to know when they can
    start using the metadata fields on the ImageDecoder.
    
    R=sandersd
    
    Bug: 1073995
    Change-Id: I6d32fcf7e2dda5927e62cd370c50238cb96d5074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254455
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780531}

--- third_party/blink/renderer/modules/webcodecs/image_decoder_external.cc
+++ third_party/blink/renderer/modules/webcodecs/image_decoder_external.cc
@@ -89,23 +89,23 @@ ImageDecoderExternal::ImageDecoderExternal(ScriptState* script_state,
   // TODO: Data is owned by the caller who may be free to manipulate it. We will
   // probably need to make a copy to our own internal data or neuter the buffers
   // as seen by JS.
-  auto sr = SegmentReader::CreateFromSkData(
+  segment_reader_ = SegmentReader::CreateFromSkData(
       SkData::MakeWithoutCopy(buffer.Data(), buffer.ByteLengthAsSizeT()));
-  if (!sr) {
+  if (!segment_reader_) {
     exception_state.ThrowDOMException(DOMExceptionCode::kConstraintError,
                                       "Failed to read image data");
     return;
   }
 
   data_complete_ = true;
-  MaybeCreateImageDecoder(std::move(sr));
+  MaybeCreateImageDecoder();
   if (!decoder_) {
     exception_state.ThrowDOMException(DOMExceptionCode::kConstraintError,
                                       "Failed to create image decoder");
     return;
   }
 
-  UpdateFrameAndRepetitionCount();
+  MaybeUpdateMetadata();
   if (decoder_->Failed()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kOperationError,
                                       "Image decoding failed");
@@ -126,6 +126,16 @@ ScriptPromise ImageDecoderExternal::decode(uint32_t frame_index,
   pending_decodes_.push_back(MakeGarbageCollected<DecodeRequest>(
       resolver, frame_index, complete_frames_only));
   MaybeSatisfyPendingDecodes();
+  return promise;
+}
+
+ScriptPromise ImageDecoderExternal::decodeMetadata() {
+  DVLOG(1) << __func__;
+
+  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver>(script_state_);
+  auto promise = resolver->Promise();
+  pending_metadata_decodes_.push_back(resolver);
+  MaybeSatisfyPendingMetadataDecodes();
   return promise;
 }
 
@@ -166,11 +176,11 @@ void ImageDecoderExternal::OnStateChange() {
 
     data_complete_ = result == BytesConsumer::Result::kDone;
     if (!decoder_)
-      MaybeCreateImageDecoder(nullptr);
+      MaybeCreateImageDecoder();
     else
       decoder_->SetData(stream_buffer_, data_complete_);
 
-    UpdateFrameAndRepetitionCount();
+    MaybeUpdateMetadata();
     MaybeSatisfyPendingDecodes();
   }
 }
@@ -183,6 +193,7 @@ void ImageDecoderExternal::Trace(Visitor* visitor) const {
   visitor->Trace(script_state_);
   visitor->Trace(consumer_);
   visitor->Trace(pending_decodes_);
+  visitor->Trace(pending_metadata_decodes_);
   visitor->Trace(init_data_);
   visitor->Trace(options_);
   ScriptWrappable::Trace(visitor);
@@ -269,8 +280,15 @@ void ImageDecoderExternal::MaybeSatisfyPendingDecodes() {
       static_cast<wtf_size_t>(new_end - pending_decodes_.begin()));
 }
 
-void ImageDecoderExternal::MaybeCreateImageDecoder(
-    scoped_refptr<SegmentReader> sr) {
+void ImageDecoderExternal::MaybeSatisfyPendingMetadataDecodes() {
+  DCHECK(decoder_);
+  DCHECK(decoder_->Failed() || frame_count_ > 0);
+  for (auto& resolver : pending_metadata_decodes_)
+    resolver->Resolve();
+  pending_metadata_decodes_.clear();
+}
+
+void ImageDecoderExternal::MaybeCreateImageDecoder() {
   // TODO: This does not handle SVG Images since they use another "decoder." It
   // is highly coupled with the DOM today, so isn't suitable for this API.
 
@@ -299,7 +317,6 @@ void ImageDecoderExternal::MaybeCreateImageDecoder(
     if (!ImageDecoder::HasSufficientDataToSniffImageType(*stream_buffer_))
       return;
 
-    DCHECK(!sr);
     decoder_ = ImageDecoder::Create(
         stream_buffer_, data_complete_, premultiply_alpha,
         ImageDecoder::kHighBitDepthToHalfFloat, color_behavior,
@@ -309,18 +326,20 @@ void ImageDecoderExternal::MaybeCreateImageDecoder(
 
   DCHECK(data_complete_);
   decoder_ = ImageDecoder::Create(
-      std::move(sr), data_complete_, premultiply_alpha,
+      segment_reader_, data_complete_, premultiply_alpha,
       ImageDecoder::kHighBitDepthToHalfFloat, color_behavior,
       ImageDecoder::OverrideAllowDecodeToYuv::kDeny, desired_size);
 }
 
-void ImageDecoderExternal::UpdateFrameAndRepetitionCount() {
+void ImageDecoderExternal::MaybeUpdateMetadata() {
   if (!decoder_)
     return;
 
   const size_t decoded_frame_count = decoder_->FrameCount();
-  if (decoder_->Failed())
+  if (decoder_->Failed()) {
+    MaybeSatisfyPendingMetadataDecodes();
     return;
+  }
 
   // TODO: Is this useful? We should have each decoder indicate its own mime
   // type then.
@@ -333,6 +352,8 @@ void ImageDecoderExternal::UpdateFrameAndRepetitionCount() {
   const int decoded_repetition_count = decoder_->RepetitionCount();
   if (decoded_repetition_count > 0)
     repetition_count_ = decoded_repetition_count;
+
+  MaybeSatisfyPendingMetadataDecodes();
 }
 
 }  // namespace blink
--- third_party/blink/renderer/modules/webcodecs/image_decoder_external.h
+++ third_party/blink/renderer/modules/webcodecs/image_decoder_external.h
@@ -40,6 +40,7 @@ class MODULES_EXPORT ImageDecoderExternal final : public ScriptWrappable,
 
   // image_decoder.idl implementation.
   ScriptPromise decode(uint32_t frame_index, bool complete_frames_only);
+  ScriptPromise decodeMetadata();
   uint32_t frameCount() const;
   String type() const;
   uint32_t repetitionCount() const;
@@ -54,14 +55,18 @@ class MODULES_EXPORT ImageDecoderExternal final : public ScriptWrappable,
 
  private:
   void MaybeSatisfyPendingDecodes();
-  void MaybeCreateImageDecoder(scoped_refptr<SegmentReader> sr);
-  void UpdateFrameAndRepetitionCount();
+  void MaybeSatisfyPendingMetadataDecodes();
+  void MaybeCreateImageDecoder();
+  void MaybeUpdateMetadata();
 
   Member<ScriptState> script_state_;
 
   // Used when a ReadableStream is provided.
   Member<ReadableStreamBytesConsumer> consumer_;
   scoped_refptr<SharedBuffer> stream_buffer_;
+
+  // Used when all data is provided at construction time.
+  scoped_refptr<SegmentReader> segment_reader_;
 
   // Construction parameters.
   Member<const ImageDecoderInit> init_data_;
@@ -87,6 +92,7 @@ class MODULES_EXPORT ImageDecoderExternal final : public ScriptWrappable,
     bool complete = false;
   };
   HeapVector<Member<DecodeRequest>> pending_decodes_;
+  HeapVector<Member<ScriptPromiseResolver>> pending_metadata_decodes_;
 
   // When decode() of incomplete frames has been requested, we need to track the
   // generation id for each SkBitmap that we've handed out. So that we can defer
