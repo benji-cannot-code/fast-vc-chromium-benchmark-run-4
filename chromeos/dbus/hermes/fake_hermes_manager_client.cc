25ee5f80966d9cb2b0cc50f149d0f68717b5f07b
Hans Wennborg <hans@chromium.org>
2020-06-20 02:05:30 +0000

    Remove/replace unnecessary includes of logging.h (chromeos/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: I13d9920bd16c8931b4f0a6dac3b5bed345a8f901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254323
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780543}

--- chromeos/dbus/hermes/fake_hermes_manager_client.cc
+++ chromeos/dbus/hermes/fake_hermes_manager_client.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chromeos/dbus/hermes/fake_hermes_manager_client.h"
+
 #include "base/command_line.h"
+#include "base/logging.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/threading/thread_task_runner_handle.h"
