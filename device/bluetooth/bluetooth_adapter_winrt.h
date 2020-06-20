59e5faff48782b663733f7d6a00c68dcd5cd4a75
Martin Kreichgauer <martinkr@chromium.org>
2020-06-20 01:23:21 +0000

    bluetooth/winrt: Observe discovery failures
    Change BluetoothAdapterWinrt to subscribe to
    BluetoothLEAdvertisementWatcher::Stopped events. The event fires if
    scanning for LE advertisements has been cancelled by the application or
    due to an error. Once the event is received, mark all active
    BluetoothDiscoverySession instances inactive, which will cause
    BluetoothAdapter::IsDiscovering() to return false.
    
    Also make BluetoothAdapterWinrt call Observer::DiscoveringChanged().
    
    Change-Id: If4abf65f09b4d20b89b53adb63dad89939ddcceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2255549
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Martin Kreichgauer <martinkr@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780539}

--- device/bluetooth/bluetooth_adapter_winrt.h
+++ device/bluetooth/bluetooth_adapter_winrt.h
@@ -199,7 +199,13 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterWinrt : public BluetoothAdapter {
       ABI::Windows::Devices::Bluetooth::Advertisement::
           IBluetoothLEAdvertisementWatcher* watcher,
       ABI::Windows::Devices::Bluetooth::Advertisement::
-          IBluetoothLEAdvertisementReceivedEventArgs* received);
+          IBluetoothLEAdvertisementReceivedEventArgs* args);
+
+  void OnAdvertisementWatcherStopped(
+      ABI::Windows::Devices::Bluetooth::Advertisement::
+          IBluetoothLEAdvertisementWatcher* watcher,
+      ABI::Windows::Devices::Bluetooth::Advertisement::
+          IBluetoothLEAdvertisementWatcherStoppedEventArgs* args);
 
   void OnRegisterAdvertisement(BluetoothAdvertisement* advertisement,
                                const CreateAdvertisementCallback& callback);
@@ -213,7 +219,7 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterWinrt : public BluetoothAdapter {
 
   void TryRemovePoweredRadioEventHandlers();
 
-  void RemoveAdvertisementReceivedHandler();
+  void RemoveAdvertisementWatcherEventHandlers();
 
   bool is_initialized_ = false;
   bool radio_access_allowed_ = false;
@@ -238,7 +244,8 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterWinrt : public BluetoothAdapter {
 
   std::vector<scoped_refptr<BluetoothAdvertisement>> pending_advertisements_;
 
-  EventRegistrationToken advertisement_received_token_;
+  base::Optional<EventRegistrationToken> advertisement_received_token_;
+  base::Optional<EventRegistrationToken> advertisement_watcher_stopped_token_;
   Microsoft::WRL::ComPtr<ABI::Windows::Devices::Bluetooth::Advertisement::
                              IBluetoothLEAdvertisementWatcher>
       ble_advertisement_watcher_;
