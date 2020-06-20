e6176ee20f042d39f1dba0781953991baf435e9b
tby <tby@chromium.org>
2020-06-20 07:50:02 +0000

    Simplify when launcher suggestion chip results are created
    Currently, the results shown in the suggestion chips are generated on
    the previous launcher *close* ready for the next launcher open.
    Speculatively, this is because the latency requirements for the chips
    are tight, and generating app results may have been too slow in the
    past.
    
    However this out-of-step update is a source of bugs for non-app
    results. All current providers of chip results appear to be more than
    fast enough to produce results synchronously on launcher open. This CL
    removes the on-close search, and adds an on-open search.
    
    Bug: 1034842
    Change-Id: Ic2c7cbd1e37753003ef74456c1cf25f23fc7d4c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245952
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Commit-Queue: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780572}

--- chrome/browser/ui/app_list/app_list_client_impl.cc
+++ chrome/browser/ui/app_list/app_list_client_impl.cc
@@ -237,6 +237,8 @@ void AppListClientImpl::GetContextMenuModel(
 
 void AppListClientImpl::OnAppListVisibilityWillChange(bool visible) {
   app_list_target_visibility_ = visible;
+  if (visible && search_controller_)
+    search_controller_->Start(base::string16());
 }
 
 void AppListClientImpl::OnAppListVisibilityChanged(bool visible) {
