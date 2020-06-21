a9113d3976906a7d7b7429b4ada4a949c924d62c
John L Chen <zuan@chromium.org>
2020-06-21 22:35:37 +0000

    Expose crytpohome UserDataAuth killswitch over DBus
    This CL exposes the finch flag that controls if cryptohome's new UserDataAuth
    dbus interface is disabled.
    
    CQ-DEPEND=CL:2245154
    
    Bug: b:151062600
    Test: Tested with dbus-send
    Change-Id: Ifdafe22c47b2af5609cbb0cd565791878dc452fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245157
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: John L Chen <zuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780635}

--- chrome/browser/chromeos/dbus/chrome_features_service_provider.h
+++ chrome/browser/chromeos/dbus/chrome_features_service_provider.h
@@ -72,6 +72,9 @@ class ChromeFeaturesServiceProvider
   void IsCryptohomeUserDataAuthEnabled(
       dbus::MethodCall* method_call,
       dbus::ExportedObject::ResponseSender response_sender);
+  void IsCryptohomeUserDataAuthKillswitchEnabled(
+      dbus::MethodCall* method_call,
+      dbus::ExportedObject::ResponseSender response_sender);
   void IsPluginVmEnabled(dbus::MethodCall* method_call,
                          dbus::ExportedObject::ResponseSender response_sender);
   void IsUsbguardEnabled(dbus::MethodCall* method_call,
