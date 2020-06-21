01b281190a5e2e18e53669d3ae8dbbae1609cf55
Shakti Sahu <shaktisahu@chromium.org>
2020-06-21 06:49:06 +0000

    Query Tiles : Fixed layout animation crash
    Bug: 1096437
    Change-Id: If9e8e1165b0118b4a18761a21e24cb47e5a76f4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252568
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780616}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/image_tiles/TileListView.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/image_tiles/TileListView.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
 
@@ -59,7 +61,7 @@ class TileListView {
         mView.setItemAnimator(null);
         mLayoutAnimationController =
                 AnimationUtils.loadLayoutAnimation(context, R.anim.image_grid_enter);
-
+        configureAnimationListener();
         mTileSizeSupplier = new TileSizeSupplier(context);
 
         PropertyModelChangeProcessor.create(
@@ -87,7 +89,25 @@ class TileListView {
      * Called to show enter animation for the list items.
      */
     void showAnimation(boolean animate) {
-        // TODO(shaktisahu): Fix animations.
+        if (animate) {
+            mView.setLayoutAnimation(mLayoutAnimationController);
+            mView.scheduleLayoutAnimation();
+        }
+    }
+
+    private void configureAnimationListener() {
+        mView.setLayoutAnimationListener(new AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mView.setLayoutAnimation(null);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
     }
 
     private class ItemDecorationImpl extends ItemDecoration {
