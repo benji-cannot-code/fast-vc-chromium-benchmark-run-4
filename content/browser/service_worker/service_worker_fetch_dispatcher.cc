9538e38fa7284c5e843c96d92e45c7549b1356cd
Lingqi Chi <lingqi@chromium.org>
2020-06-22 01:57:17 +0000

    cleanup: Delete unused dependency in service_worker_fetch_dispatcher.cc
    content/browser/service_worker/service_worker_fetch_dispatcher.cc
    includes services/network/throttling/throttling_controller.h but does
    not use it.
    This CL removes this dependency. Then private implementation
    ThrottlingController won't be used outside the services/network.
    
    Bug: 1049894
    Change-Id: I9dbbc6c70a113b3ecb500afb5e7cc536d294807f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249430
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780646}

--- content/browser/service_worker/service_worker_fetch_dispatcher.cc
+++ content/browser/service_worker/service_worker_fetch_dispatcher.cc
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/url_request.h"
 #include "services/network/public/cpp/wrapper_shared_url_loader_factory.h"
-#include "services/network/throttling/throttling_controller.h"
 #include "third_party/blink/public/common/service_worker/service_worker_status_code.h"
 
 namespace content {
