55eccaabaa23c71d1508406ae4078fdb09eb11ce
Hans Wennborg <hans@chromium.org>
2020-06-22 17:38:58 +0000

    Remove/replace unnecessary includes of logging.h (fuchsia/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: I3f6402a9d9187a95f1d5cb298d8beb44729db9dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254148
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780855}

--- fuchsia/runners/common/web_component.h
+++ fuchsia/runners/common/web_component.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/fuchsia/scoped_service_binding.h"
 #include "base/fuchsia/startup_context.h"
-#include "base/logging.h"
 #include "fuchsia/base/lifecycle_impl.h"
 #include "url/gurl.h"
 
