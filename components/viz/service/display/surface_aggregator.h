47cdf51067114469bf45e0be2842c3da99c76882
Bo Liu <boliu@chromium.org>
2020-06-22 19:14:30 +0000

    viz: Skip emitting render pass quad with empty visible rect
    Fixed: 1096736
    Change-Id: I18f84514dafc627b728c1665b2888b492f3eaf65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254080
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780901}

--- components/viz/service/display/surface_aggregator.h
+++ components/viz/service/display/surface_aggregator.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <utility>
+#include <vector>
 
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
