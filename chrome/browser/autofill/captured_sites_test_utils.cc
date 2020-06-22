95b8743b57019359569de0c7a8194b50861ae9a3
Christoph Schwering <schwering@google.com>
2020-06-22 09:58:46 +0000

    [Autofill] Allowed replay test to block process.
    The AutofillCapturedSitesInteractiveTest test crashes on Linux
    because the process is not allowed to be blocking. This CL remedies
    this by adding a ScopedAllowBlocking RAII variable with minimal
    necessary scope.
    
    Bug: 1097526, 1090042
    Change-Id: I6475d67be1cf308d4d2086076a7a0b2e7eb82ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2255360
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/master@{#780702}

--- chrome/browser/autofill/captured_sites_test_utils.cc
+++ chrome/browser/autofill/captured_sites_test_utils.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "build/build_config.h"
@@ -331,9 +332,12 @@ TestRecipeReplayer::GetValidationFailures() const {
 bool TestRecipeReplayer::OverrideAutofillClock(
     const base::FilePath capture_file_path) {
   std::string json_text;
-  if (!base::ReadFileToString(capture_file_path, &json_text)) {
-    VLOG(1) << kClockNotSetMessage << "Could not read file";
-    return false;
+  {
+    base::ScopedAllowBlockingForTesting allow_blocking;
+    if (!base::ReadFileToString(capture_file_path, &json_text)) {
+      VLOG(1) << kClockNotSetMessage << "Could not read file";
+      return false;
+    }
   }
   // Decompress the json text from gzip.
   std::string decompressed_json_text;
