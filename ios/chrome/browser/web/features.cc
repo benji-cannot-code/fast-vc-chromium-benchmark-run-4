fa09407535227c91e5921f5869629a4a4410cf6b
Robbie Gibson <rkgibson@google.com>
2020-06-22 15:14:42 +0000

    [iOS] Enable WebPageTextAccessibility by default for stable launch
    Bug: 1049559
    Change-Id: Iffda7ed060a5e6220efd1d2ab3b322a3c105d08c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254279
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/master@{#780781}

--- ios/chrome/browser/web/features.cc
+++ ios/chrome/browser/web/features.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web {
 
-const base::Feature kWebPageTextAccessibility{
-    "WebPageTextAccessibility", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kWebPageTextAccessibility{"WebPageTextAccessibility",
+                                              base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kMobileGoogleSRP{"MobileGoogleSRP",
                                      base::FEATURE_DISABLED_BY_DEFAULT};
