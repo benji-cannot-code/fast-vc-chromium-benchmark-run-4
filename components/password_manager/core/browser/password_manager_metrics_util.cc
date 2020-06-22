f07f8fe183d246efdf858c520f872ae11120748f
Mohamed Amir Yosef <mamir@chromium.org>
2020-06-22 22:36:10 +0000

    [Passwords] Split SaveUIDismissalReason metric by user state
    Bug: 1063852
    Change-Id: Ie617cafbde51b1eccf689c09ab261ce62b121b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2256207
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781012}

--- components/password_manager/core/browser/password_manager_metrics_util.cc
+++ components/password_manager/core/browser/password_manager_metrics_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/strcat.h"
 #include "components/autofill/core/common/password_generation_util.h"
+#include "components/password_manager/core/common/password_manager_features.h"
 
 using autofill::password_generation::PasswordGenerationType;
 
@@ -58,9 +59,21 @@ void LogGeneralUIDismissalReason(UIDismissalReason reason) {
                                 NUM_UI_RESPONSES);
 }
 
-void LogSaveUIDismissalReason(UIDismissalReason reason) {
+void LogSaveUIDismissalReason(
+    UIDismissalReason reason,
+    base::Optional<PasswordAccountStorageUserState> user_state) {
   base::UmaHistogramEnumeration("PasswordManager.SaveUIDismissalReason", reason,
                                 NUM_UI_RESPONSES);
+
+  if (user_state.has_value()) {
+    DCHECK(base::FeatureList::IsEnabled(
+        password_manager::features::kEnablePasswordsAccountStorage));
+    std::string suffix =
+        GetPasswordAccountStorageUserStateHistogramSuffix(user_state.value());
+    base::UmaHistogramEnumeration(
+        "PasswordManager.SaveUIDismissalReason." + suffix, reason,
+        NUM_UI_RESPONSES);
+  }
 }
 
 void LogSaveUIDismissalReasonAfterUnblacklisting(UIDismissalReason reason) {
--- ios/chrome/browser/passwords/ios_chrome_save_password_infobar_delegate.mm
+++ ios/chrome/browser/passwords/ios_chrome_save_password_infobar_delegate.mm
@@ -99,7 +99,8 @@ void RecordDismissalMetrics(
     password_manager::metrics_util::LogUpdateUIDismissalReason(
         infobar_response);
   } else {
-    password_manager::metrics_util::LogSaveUIDismissalReason(infobar_response);
+    password_manager::metrics_util::LogSaveUIDismissalReason(
+        infobar_response, /*user_state=*/base::nullopt);
   }
 }
 
