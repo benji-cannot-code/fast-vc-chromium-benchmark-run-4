aab4a5ab2268dea9e3ca836300a3b8deea6c31e6
Henrique Nakashima <hnakashima@chromium.org>
2020-06-22 19:22:15 +0000

    Shorten package names in dependency visualization
    The shortener is Chrome-specific. It replaces:
    "org.chromium.chrome.browser.foo" with ".c.b.foo"
    
    Also git ignore json_graph.txt
    
    Bug: 1093962
    Change-Id: Id1d84f7b2478672c7db6ef6370bb86654687cdc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254096
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780908}

--- tools/android/dependency_analysis/js/process_graph_json.js
+++ tools/android/dependency_analysis/js/process_graph_json.js
@@ -4,14 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {Node, Edge, Graph} from './graph_model.js';
-
-/**
- * TODO(yjlong): Compute the "shortest disambiguated name" in Python and include
- * it in the JSON so we do not need to do this calculation.
- * @param {number} name The full package name to shorten.
- * @return {number} The shortened package name.
- */
-const shortName = name => name.substring(name.lastIndexOf('.') + 1);
+import {shortenPackageName} from './chrome_hooks.js';
 
 /**
  * A graph read from JSON.
@@ -29,7 +22,7 @@ const shortName = name => name.substring(name.lastIndexOf('.') + 1);
 function parseGraphFromJson(jsonGraph) {
   const graph = new Graph();
   for (const nodeData of jsonGraph.nodes) {
-    const node = new Node(nodeData.name, shortName(nodeData.name));
+    const node = new Node(nodeData.name, shortenPackageName(nodeData.name));
     graph.addNodeIfNew(node);
   }
   for (const edgeData of jsonGraph.edges) {
