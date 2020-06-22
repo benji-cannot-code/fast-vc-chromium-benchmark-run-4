d5974a78eab49c0f80ed740afe4c366a39572f41
Xing Liu <xingliu@chromium.org>
2020-06-22 18:29:29 +0000

    Download later: Read DownloadSchedule from database.
    This CL reads DownloadSchedule from in progress download db.
    
    Bug: 1078454
    Change-Id: Icec7eed9edb38ce526cd60b72de90aaeac817335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2253150
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780877}

--- components/download/public/common/mock_download_item_impl.cc
+++ components/download/public/common/mock_download_item_impl.cc
@@ -38,6 +38,7 @@ MockDownloadItemImpl::MockDownloadItemImpl(DownloadItemImplDelegate* delegate)
                        base::Time(),
                        true,
                        DownloadItem::ReceivedSlices(),
+                       base::nullopt /*download_schedule*/,
                        nullptr /* download_entry */) {}
 
 MockDownloadItemImpl::~MockDownloadItemImpl() = default;
