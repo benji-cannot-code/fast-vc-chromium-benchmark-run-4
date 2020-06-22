fd4a19ab24aad0dc966983a1ba75b46e2aab103b
Lucas Furukawa Gadani <lfg@chromium.org>
2020-06-22 17:58:08 +0000

    Remove OutOfBlinkFrameAncestors feature.
    Now that it has rolled out to stable, the feature used to launch can be
    removed.
    
    Bug: 1032139
    Change-Id: I6b5fe013d15ed4eabaa4c3803c7829b9ebcaf106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250666
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780868}

--- content/browser/frame_host/ancestor_throttle_browsertest.cc
+++ content/browser/frame_host/ancestor_throttle_browsertest.cc
@@ -32,12 +32,7 @@ namespace {
 class AncestorThrottleTest : public ContentBrowserTest,
                              public ::testing::WithParamInterface<bool> {
  public:
-  AncestorThrottleTest() {
-    if (GetParam()) {
-      feature_list_.InitAndEnableFeature(
-          network::features::kOutOfBlinkFrameAncestors);
-    }
-  }
+  AncestorThrottleTest() = default;
 
  protected:
   void SetUpOnMainThread() override {
@@ -50,7 +45,7 @@ class AncestorThrottleTest : public ContentBrowserTest,
 };
 
 // Tests that an iframe navigation with frame-anecestors 'none' is blocked.
-IN_PROC_BROWSER_TEST_P(AncestorThrottleTest, FailedCSP) {
+IN_PROC_BROWSER_TEST_F(AncestorThrottleTest, FailedCSP) {
   GURL parent_url(
       embedded_test_server()->GetURL("foo.com", "/page_with_iframe.html"));
   GURL iframe_url(
@@ -74,7 +69,7 @@ IN_PROC_BROWSER_TEST_P(AncestorThrottleTest, FailedCSP) {
 
 // Tests that redirecting on a forbidden frame-ancestors will still commit if
 // the final response does not have a CSP policy that prevents the navigation.
-IN_PROC_BROWSER_TEST_P(AncestorThrottleTest, RedirectCommitsIfNoCSP) {
+IN_PROC_BROWSER_TEST_F(AncestorThrottleTest, RedirectCommitsIfNoCSP) {
   GURL parent_url(
       embedded_test_server()->GetURL("foo.com", "/page_with_iframe.html"));
   GURL iframe_url(
@@ -99,7 +94,7 @@ IN_PROC_BROWSER_TEST_P(AncestorThrottleTest, RedirectCommitsIfNoCSP) {
 
 // Tests that redirecting on a forbidden frame-ancestors will not commit if
 // the final response does have a CSP policy that prevents the navigation.
-IN_PROC_BROWSER_TEST_P(AncestorThrottleTest, RedirectFails) {
+IN_PROC_BROWSER_TEST_F(AncestorThrottleTest, RedirectFails) {
   GURL parent_url(
       embedded_test_server()->GetURL("foo.com", "/page_with_iframe.html"));
   GURL iframe_url(embedded_test_server()->GetURL(
@@ -122,7 +117,7 @@ IN_PROC_BROWSER_TEST_P(AncestorThrottleTest, RedirectFails) {
 }
 
 // Check that we don't process CSP for 204 responses.
-IN_PROC_BROWSER_TEST_P(AncestorThrottleTest, Response204CSP) {
+IN_PROC_BROWSER_TEST_F(AncestorThrottleTest, Response204CSP) {
   GURL parent_url(
       embedded_test_server()->GetURL("foo.com", "/page_with_iframe.html"));
   GURL iframe_url(embedded_test_server()->GetURL(
@@ -171,7 +166,7 @@ class AncestorThrottleSXGTest : public AncestorThrottleTest {
   SignedExchangeBrowserTestHelper sxg_test_helper_;
 };
 
-IN_PROC_BROWSER_TEST_P(AncestorThrottleSXGTest, SXGWithCSP) {
+IN_PROC_BROWSER_TEST_F(AncestorThrottleSXGTest, SXGWithCSP) {
   sxg_test_helper_.InstallMockCert(mock_cert_verifier_.mock_cert_verifier());
   sxg_test_helper_.InstallMockCertChainInterceptor();
 
@@ -188,9 +183,6 @@ IN_PROC_BROWSER_TEST_P(AncestorThrottleSXGTest, SXGWithCSP) {
   EXPECT_TRUE(
       iframe_node->current_frame_host()->GetLastCommittedOrigin().opaque());
 }
-
-INSTANTIATE_TEST_SUITE_P(All, AncestorThrottleTest, ::testing::Bool());
-INSTANTIATE_TEST_SUITE_P(All, AncestorThrottleSXGTest, ::testing::Bool());
 
 }  // namespace
 
