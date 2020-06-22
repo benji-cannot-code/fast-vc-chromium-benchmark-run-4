481cb75ce1add1dbe43a153d9cca0727e08fb729
Andy Paicu <andypaicu@chromium.org>
2020-06-22 17:51:20 +0000

    Correctly mock geolocation for some flaky tests
    This is an "educated guess" fix for flaky tests that I was unable to
    reproduce locally.
    
    Bug: 1092176, 1094585
    Change-Id: I2789c903b96443b7004f578dff14b8e6ac522c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2257326
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780863}

--- chrome/android/javatests/src/org/chromium/chrome/browser/permissions/RuntimePermissionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/permissions/RuntimePermissionTest.java
@@ -30,6 +30,7 @@ import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.permissions.PermissionTestRule.PermissionUpdateWaiter;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.util.browser.LocationSettingsTestUtil;
 import org.chromium.components.offline_items_collection.ContentId;
 import org.chromium.components.permissions.R;
 import org.chromium.content_public.browser.test.util.Criteria;
@@ -156,6 +157,11 @@ public class RuntimePermissionTest {
         });
     }
 
+    private void setupGeolocationSystemMock() {
+        LocationSettingsTestUtil.setSystemLocationSettingEnabled(true);
+        LocationProviderOverrider.setLocationProviderImpl(new MockLocationProvider());
+    }
+
     /**
      * Run a test related to the runtime permission prompt, based on the specified parameters.
      * @param testUrl The URL of the test page to load in order to run the text.
@@ -236,7 +242,7 @@ public class RuntimePermissionTest {
     @MediumTest
     @Feature({"RuntimePermissions", "Location"})
     public void testAllowRuntimeLocation() throws Exception {
-        LocationProviderOverrider.setLocationProviderImpl(new MockLocationProvider());
+        setupGeolocationSystemMock();
 
         runTest(GEOLOCATION_TEST, true /* expectPermissionAllowed */,
                 true /* permissionPromptAllow */, RuntimePromptResponse.GRANT,
@@ -273,7 +279,7 @@ public class RuntimePermissionTest {
     @MediumTest
     @Feature({"RuntimePermissions", "Location"})
     public void testDenyRuntimeLocation() throws Exception {
-        LocationProviderOverrider.setLocationProviderImpl(new MockLocationProvider());
+        setupGeolocationSystemMock();
 
         runTest(GEOLOCATION_TEST, false /* expectPermissionAllowed */,
                 true /* permissionPromptAllow */, RuntimePromptResponse.DENY,
@@ -344,7 +350,7 @@ public class RuntimePermissionTest {
     @MediumTest
     @Feature({"RuntimePermissions", "Location"})
     public void testDenyTriggersNoRuntime() throws Exception {
-        LocationProviderOverrider.setLocationProviderImpl(new MockLocationProvider());
+        setupGeolocationSystemMock();
 
         runTest(GEOLOCATION_TEST, false /* expectPermissionAllowed */,
                 false /* permissionPromptAllow */, RuntimePromptResponse.ASSERT_NEVER_ASKED,
@@ -401,7 +407,7 @@ public class RuntimePermissionTest {
     @MediumTest
     @Feature({"RuntimePermissions", "Location"})
     public void testAlreadyGrantedRuntimeLocation() throws Exception {
-        LocationProviderOverrider.setLocationProviderImpl(new MockLocationProvider());
+        setupGeolocationSystemMock();
 
         runTest(GEOLOCATION_TEST, true /* expectPermissionAllowed */,
                 true /* permissionPromptAllow */, RuntimePromptResponse.ALREADY_GRANTED,
@@ -415,7 +421,7 @@ public class RuntimePermissionTest {
     @MediumTest
     @Feature({"RuntimePermissions", "Location"})
     public void testAllowRuntimeLocationIncognito() throws Exception {
-        LocationProviderOverrider.setLocationProviderImpl(new MockLocationProvider());
+        setupGeolocationSystemMock();
         mPermissionTestRule.newIncognitoTabFromMenu();
 
         runTest(GEOLOCATION_TEST, true /* expectPermissionAllowed */,
