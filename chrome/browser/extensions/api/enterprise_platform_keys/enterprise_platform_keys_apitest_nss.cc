1f3aabbc62c8e5c546e725ad8a39cfdcf994295b
Omar Morsi <omorsi@google.com>
2020-06-22 20:47:52 +0000

    Fix a memory leak in platform keys tests
    This CL replaces a SECKEYPrivateKey pointer that was not being freed
    with a crypto::ScopedSECKEYPrivateKey.
    
    Bug: 1054911
    Change-Id: I450003487a18ad15fd0d4d6d5e35bacf72bf7c30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2257692
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Omar Morsi <omorsi@google.com>
    Cr-Commit-Position: refs/heads/master@{#780956}

--- chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_apitest_nss.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys/enterprise_platform_keys_apitest_nss.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test.h"
 #include "crypto/nss_util_internal.h"
+#include "crypto/scoped_nss_types.h"
 #include "crypto/scoped_test_system_nss_key_slot.h"
 #include "extensions/browser/extension_registry.h"
 #include "net/cert/nss_cert_database.h"
@@ -118,17 +119,17 @@ void ImportPrivateKeyPKCS8ToSlot(const unsigned char* pkcs8_der,
       const_cast<unsigned char*>(pkcs8_der),
       pkcs8_der_size};
 
-  SECKEYPrivateKey* seckey = NULL;
-  ASSERT_EQ(SECSuccess,
-            PK11_ImportDERPrivateKeyInfoAndReturnKey(slot,
-                                                     &pki_der_user,
-                                                     NULL,    // nickname
-                                                     NULL,    // publicValue
-                                                     true,    // isPerm
-                                                     true,    // isPrivate
-                                                     KU_ALL,  // usage
-                                                     &seckey,
-                                                     NULL));
+  SECKEYPrivateKey* seckey_raw = nullptr;
+  ASSERT_EQ(SECSuccess, PK11_ImportDERPrivateKeyInfoAndReturnKey(
+                            slot, &pki_der_user,
+                            /*nickname=*/nullptr,
+                            /*publicValue=*/nullptr,
+                            /*isPerm=*/true,
+                            /*isPrivate=*/true,
+                            /*usage=*/KU_ALL, &seckey_raw, /*wincx=*/nullptr));
+
+  // Make sure that the memory allocated for the key gets freed.
+  crypto::ScopedSECKEYPrivateKey seckey(seckey_raw);
 }
 
 // The managed_storage extension has a key defined in its manifest, so that
