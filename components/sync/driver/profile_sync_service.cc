77a5de7b1d9d21572a9fe000b9840bc40448f7fc
Maksim Moskvitin <mmoskvitin@google.com>
2020-06-22 10:13:28 +0000

    Remove dead encryption code from SyncTest
    Bug: 1016702
    Change-Id: I75da209fc1cbe8a93eb3f9cd229097161064bf29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2253766
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780706}

--- components/sync/driver/profile_sync_service.cc
+++ components/sync/driver/profile_sync_service.cc
@@ -1593,11 +1593,6 @@ ProfileSyncService::GetTypeStatusMapForDebugging() {
   return std::move(result);
 }
 
-bool ProfileSyncService::IsEncryptionPendingForTest() const {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  return user_settings_->IsEncryptionPending();
-}
-
 void ProfileSyncService::OnSyncManagedPrefChange(bool is_sync_managed) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (is_sync_managed) {
