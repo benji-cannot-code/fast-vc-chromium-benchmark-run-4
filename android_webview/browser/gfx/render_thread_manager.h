a1af8782c84f628d723fc7a580882c6f795a5706
Hans Wennborg <hans@chromium.org>
2020-06-22 16:06:34 +0000

    Remove/replace unnecessary includes of logging.h (android_webview/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: If7e09c6168c289fc12e602b1ba7738d82b791875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254283
    Commit-Queue: Richard Coles <torne@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780805}

--- android_webview/browser/gfx/render_thread_manager.h
+++ android_webview/browser/gfx/render_thread_manager.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/gfx/hardware_renderer.h"
 #include "android_webview/browser/gfx/parent_compositor_draw_constraints.h"
 #include "android_webview/browser/gfx/root_frame_sink.h"
-#include "base/logging.h"
+#include "base/check.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "base/single_thread_task_runner.h"
