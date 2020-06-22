9575127e68a127434e1f4111aee71a853beee437
Andy Paicu <andypaicu@chromium.org>
2020-06-22 17:36:43 +0000

    Fixed race condition causing cookies test to be flaky
    The script inside the page is not guaranteed to execute before the
    script injected via `runJavaScriptCodeInCurrentTab` which introduced a
    race condition causing the test to be flaky.
    
    I've updated the tests to use a function for clearing cookies that is
    manually called.
    
    Bug: 1090274
    Change-Id: I1a714ac9053bec47aa4ad108f0493fd6ede607a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247763
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780851}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
@@ -443,7 +443,8 @@ public class SiteSettingsTest {
         final String url = mPermissionRule.getURL("/chrome/test/data/android/cookie.html");
 
         // Load the page and clear any set cookies.
-        mPermissionRule.loadUrl(url + "#clear");
+        mPermissionRule.loadUrl(url);
+        mPermissionRule.runJavaScriptCodeInCurrentTab("clearCookie()");
         Assert.assertEquals("\"\"", mPermissionRule.runJavaScriptCodeInCurrentTab("getCookie()"));
         mPermissionRule.runJavaScriptCodeInCurrentTab("setCookie()");
         Assert.assertEquals(
@@ -470,7 +471,8 @@ public class SiteSettingsTest {
         final String url = mPermissionRule.getURL("/chrome/test/data/android/cookie.html");
 
         // Load the page and clear any set cookies.
-        mPermissionRule.loadUrl(url + "#clear");
+        mPermissionRule.loadUrl(url);
+        mPermissionRule.runJavaScriptCodeInCurrentTab("clearCookie()");
         Assert.assertEquals("\"\"", mPermissionRule.runJavaScriptCodeInCurrentTab("getCookie()"));
         mPermissionRule.runJavaScriptCodeInCurrentTab("setCookie()");
         Assert.assertEquals("\"\"", mPermissionRule.runJavaScriptCodeInCurrentTab("getCookie()"));
@@ -484,7 +486,6 @@ public class SiteSettingsTest {
      * Blocks specific sites from setting cookies and ensures that no cookies can be set.
      */
     @Test
-    @DisabledTest(message = "crbug.com/1090274")
     @SmallTest
     @Feature({"Preferences"})
     // Todo(eokoyomon) figure out how to set and test third party cookie setting in this test
@@ -499,7 +500,8 @@ public class SiteSettingsTest {
         final String url = mPermissionRule.getURL("/chrome/test/data/android/cookie.html");
 
         // Load the page and clear any set cookies.
-        mPermissionRule.loadUrl(url + "#clear");
+        mPermissionRule.loadUrl(url);
+        mPermissionRule.runJavaScriptCodeInCurrentTab("clearCookie()");
         Assert.assertEquals("\"\"", mPermissionRule.runJavaScriptCodeInCurrentTab("getCookie()"));
 
         // Check cookies can be set for this website when there is no rule.
@@ -508,7 +510,8 @@ public class SiteSettingsTest {
                 "\"Foo=Bar\"", mPermissionRule.runJavaScriptCodeInCurrentTab("getCookie()"));
 
         // Set specific rule to block site and ensure it cannot set cookies.
-        mPermissionRule.loadUrl(url + "#clear");
+        mPermissionRule.loadUrl(url);
+        mPermissionRule.runJavaScriptCodeInCurrentTab("clearCookie()");
         settingsActivity =
                 SiteSettingsTestUtils.startSiteSettingsCategory(SiteSettingsCategory.Type.COOKIES);
         setBlockCookiesSiteException(settingsActivity, url, false);
