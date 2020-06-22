982cee274132782c6f72653642543b512b335868
Hans Wennborg <hans@chromium.org>
2020-06-22 17:15:46 +0000

    Remove/replace unnecessary includes of logging.h (url/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: I95573a0dab0a5ae62f8f88004e198bd0a6ef5319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254760
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780847}

--- url/third_party/mozilla/url_parse.cc
+++ url/third_party/mozilla/url_parse.cc
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdlib.h>
 
-#include "base/logging.h"
+#include "base/check_op.h"
 #include "url/url_parse_internal.h"
 #include "url/url_util.h"
 #include "url/url_util_internal.h"
