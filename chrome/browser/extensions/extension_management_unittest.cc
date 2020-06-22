3dbac3870108d8141e777955fdccd51c1ec58fec
Nigel Tao <nigeltao@chromium.org>
2020-06-22 16:54:54 +0000

    Move JSONParser-specific enums out of JSONReader
    JSONReader is the API. JSONParser is *an* implementation, and we would
    like to experiment (in future commits) with different implementations
    (crbug.com/1069271). Doing so is easier if they don't also have to match
    exact error code values for invalid JSON input. In practice, API callers
    don't care about the error code value (if less than 1000).
    
    Also swap the order of the JSON_SYNTAX_ERROR and JSON_INVALID_ESCAPE
    enum values so that JSON_SYNTAX_ERROR takes the enum value 1, the same
    as the new kErrorCodeInvalidFormat enum value.
    
    Also update a switch statement in
    components/policy/core/common/config_dir_policy_loader.cc to also catch
    JSON_TOO_LARGE, JSON_UNREPRESENTABLE_NUMBER and any future enums added.
    
    Bug: 1069271
    Bug: 1070409
    Change-Id: I786c5d7ffc77e4d7927614a133bf1e7aae7eda5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251561
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780827}

--- chrome/browser/extensions/extension_management_unittest.cc
+++ chrome/browser/extensions/extension_management_unittest.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/json/json_parser.h"
+#include "base/json/json_reader.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/scoped_feature_list.h"
--- components/schema_org/extractor.cc
+++ components/schema_org/extractor.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/json/json_parser.h"
 #include "base/no_destructor.h"
 #include "base/strings/string_number_conversions.h"
 #include "components/schema_org/common/improved_metadata.mojom.h"
