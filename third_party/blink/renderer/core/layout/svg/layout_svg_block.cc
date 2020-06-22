31fe4a9c0938b3719af26a78d583b3905d66c096
Fredrik Söderqvist <fs@opera.com>
2020-06-22 10:26:22 +0000

    Track reference-box dependent transforms for LayoutSVGBlock
    Like in SVG LayoutObjects deriving from LayoutSVGModelObject, we need to
    handle implicit transform changes caused by changes to the viewport.
    Add the equivalent CheckForImplicitTransformChange() method that
    LayoutSVGModelObject has and hook it up in UpdateTransformAfterLayout().
    
    Bug: 1094020
    Change-Id: Iedd05bb7e2de2bde25a6ce9f01cf1903dc1d6a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250000
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780707}

--- third_party/blink/renderer/core/layout/svg/layout_svg_block.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_block.cc
@@ -29,13 +29,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/svg/svg_layout_support.h"
 #include "third_party/blink/renderer/core/layout/svg/svg_resources.h"
 #include "third_party/blink/renderer/core/layout/svg/svg_resources_cache.h"
+#include "third_party/blink/renderer/core/layout/svg/transform_helper.h"
 #include "third_party/blink/renderer/core/style/shadow_list.h"
 #include "third_party/blink/renderer/core/svg/svg_element.h"
 
 namespace blink {
 
 LayoutSVGBlock::LayoutSVGBlock(SVGElement* element)
-    : LayoutBlockFlow(element), needs_transform_update_(true) {}
+    : LayoutBlockFlow(element),
+      needs_transform_update_(true),
+      transform_uses_reference_box_(false) {}
 
 SVGElement* LayoutSVGBlock::GetElement() const {
   return To<SVGElement>(LayoutObject::GetNode());
@@ -52,7 +55,27 @@ void LayoutSVGBlock::UpdateFromStyle() {
   SetFloating(false);
 }
 
-bool LayoutSVGBlock::UpdateTransformAfterLayout() {
+bool LayoutSVGBlock::CheckForImplicitTransformChange(bool bbox_changed) const {
+  // If the transform is relative to the reference box, check relevant
+  // conditions to see if we need to recompute the transform.
+  switch (StyleRef().TransformBox()) {
+    case ETransformBox::kViewBox:
+      return SVGLayoutSupport::LayoutSizeOfNearestViewportChanged(this);
+    case ETransformBox::kFillBox:
+      return bbox_changed;
+  }
+  NOTREACHED();
+  return false;
+}
+
+bool LayoutSVGBlock::UpdateTransformAfterLayout(bool bounds_changed) {
+  // If our transform depends on the reference box, we need to check if it needs
+  // to be updated.
+  if (!needs_transform_update_ && transform_uses_reference_box_) {
+    needs_transform_update_ = CheckForImplicitTransformChange(bounds_changed);
+    if (needs_transform_update_)
+      SetNeedsPaintPropertyUpdate();
+  }
   if (!needs_transform_update_)
     return false;
   local_transform_ =
@@ -63,6 +86,9 @@ bool LayoutSVGBlock::UpdateTransformAfterLayout() {
 
 void LayoutSVGBlock::StyleDidChange(StyleDifference diff,
                                     const ComputedStyle* old_style) {
+  transform_uses_reference_box_ =
+      TransformHelper::DependsOnReferenceBox(StyleRef());
+
   // Since layout depends on the bounds of the filter, we need to force layout
   // when the filter changes.
   if (diff.FilterChanged())
