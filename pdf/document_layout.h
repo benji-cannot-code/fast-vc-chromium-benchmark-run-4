0620f850952f9ab0d2a43b2e5d9e9c0a71ab22d1
Hans Wennborg <hans@chromium.org>
2020-06-22 16:13:53 +0000

    Remove/replace unnecessary includes of logging.h (pdf/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: Id0f04386d2024d6646d7bc05761954eeedd1e338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254779
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780810}

--- pdf/document_layout.h
+++ pdf/document_layout.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 #include <vector>
 
-#include "base/logging.h"
+#include "base/check_op.h"
 #include "pdf/draw_utils/coordinates.h"
 #include "pdf/page_orientation.h"
 #include "ppapi/cpp/rect.h"
