99a62ad69537682bdbad7a7126d440eea02fa6f4
Clark DuVall <cduvall@chromium.org>
2020-06-22 20:21:20 +0000

    [WebLayer] Hook up client certificate picker for WebLayer
    The client certificate picker was componentized in
    http://crrev.com/c/2253031. This hooks up the picker UI in WebLayer.
    
    Bug: 1025606
    Change-Id: I4549bc8105d3c6afd5b024e17b609d8d8c683e46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2253080
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780940}

--- components/browser_ui/client_certificate/android/java/src/org/chromium/components/browser_ui/client_certificate/SSLClientCertificateRequest.java
+++ components/browser_ui/client_certificate/android/java/src/org/chromium/components/browser_ui/client_certificate/SSLClientCertificateRequest.java
@@ -18,6 +18,7 @@ import android.util.Log;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AlertDialog;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -212,10 +213,10 @@ public class SSLClientCertificateRequest {
      */
     @VisibleForTesting
     static class CertSelectionFailureDialog {
-        private final Activity mActivity;
+        private final Context mContext;
 
-        public CertSelectionFailureDialog(Activity activity) {
-            mActivity = activity;
+        public CertSelectionFailureDialog(Context context) {
+            mContext = context;
         }
 
         /**
@@ -223,7 +224,7 @@ public class SSLClientCertificateRequest {
          */
         public void show() {
             final AlertDialog.Builder builder = new UiUtils.CompatibleAlertDialogBuilder(
-                    mActivity, R.style.Theme_Chromium_AlertDialog);
+                    mContext, R.style.Theme_Chromium_AlertDialog);
             builder.setTitle(R.string.client_cert_unsupported_title)
                     .setMessage(R.string.client_cert_unsupported_message)
                     .setNegativeButton(R.string.close,
@@ -253,7 +254,10 @@ public class SSLClientCertificateRequest {
             final int port) {
         ThreadUtils.assertOnUiThread();
 
-        final Activity activity = window.getActivity().get();
+        // Use the context for the failure dialog in case the activity doesn't have the correct
+        // resources.
+        final Context context = window.getContext().get();
+        final Activity activity = ContextUtils.activityFromContext(context);
         if (activity == null) {
             Log.w(TAG, "Certificate request on GC'd activity.");
             return false;
@@ -277,7 +281,7 @@ public class SSLClientCertificateRequest {
                 new KeyChainCertSelectionCallback(activity.getApplicationContext(), nativePtr);
         KeyChainCertSelectionWrapper keyChain = new KeyChainCertSelectionWrapper(
                 activity, callback, keyTypes, principals, hostName, port, null);
-        maybeShowCertSelection(keyChain, callback, new CertSelectionFailureDialog(activity));
+        maybeShowCertSelection(keyChain, callback, new CertSelectionFailureDialog(context));
 
         // We've taken ownership of the native ssl request object.
         return true;
--- weblayer/browser/content_browser_client_impl.cc
+++ weblayer/browser/content_browser_client_impl.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_prefs/user_prefs.h"
 #include "components/variations/service/variations_service.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/client_certificate_delegate.h"
 #include "content/public/browser/devtools_manager_delegate.h"
 #include "content/public/browser/generated_code_cache_settings.h"
 #include "content/public/browser/navigation_handle.h"
@@ -54,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/window_container_type.mojom.h"
 #include "mojo/public/cpp/bindings/binder_map.h"
 #include "net/proxy_resolution/proxy_config.h"
+#include "net/ssl/client_cert_identity.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/network_service.h"
 #include "services/network/public/mojom/network_context.mojom.h"
@@ -97,6 +99,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "base/android/path_utils.h"
 #include "base/bind.h"
+#include "components/browser_ui/client_certificate/android/ssl_client_certificate_request.h"
 #include "components/cdm/browser/cdm_message_filter_android.h"
 #include "components/cdm/browser/media_drm_storage_impl.h"  // nogncheck
 #include "components/crash/content/browser/crash_handler_host_linux.h"
@@ -446,6 +449,20 @@ void ContentBrowserClientImpl::PersistIsolatedOrigin(
   base::Value value(origin.Serialize());
   if (!base::Contains(list->GetList(), value))
     list->Append(std::move(value));
+}
+
+base::OnceClosure ContentBrowserClientImpl::SelectClientCertificate(
+    content::WebContents* web_contents,
+    net::SSLCertRequestInfo* cert_request_info,
+    net::ClientCertIdentityList client_certs,
+    std::unique_ptr<content::ClientCertificateDelegate> delegate) {
+#if defined(OS_ANDROID)
+  return browser_ui::ShowSSLClientCertificateSelector(
+      web_contents, cert_request_info, std::move(delegate));
+#else
+  delegate->ContinueWithCertificate(nullptr, nullptr);
+  return base::OnceClosure();
+#endif
 }
 
 bool ContentBrowserClientImpl::CanCreateWindow(
