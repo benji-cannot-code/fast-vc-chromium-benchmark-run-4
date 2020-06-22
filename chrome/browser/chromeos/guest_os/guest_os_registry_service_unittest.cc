5372927a74b582d53562e2a721ff6a3e260db4f7
Joel Hockey <joelhockey@chromium.org>
2020-06-22 12:38:25 +0000

    Use vm_type for ClearApplicationList
    Change UpdateApplicationList to use vm_type to filter sending
    PluginVm to observers.
    
    TBR=nancylingwang@chromium.org
    
    Bug: b/158617133
    Change-Id: I3c642292b324bf3c0173dc533793130ab3bc2756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2256112
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780741}

--- chrome/browser/chromeos/guest_os/guest_os_registry_service_unittest.cc
+++ chrome/browser/chromeos/guest_os/guest_os_registry_service_unittest.cc
@@ -188,6 +188,7 @@ TEST_F(GuestOsRegistryServiceTest, Observer) {
 TEST_F(GuestOsRegistryServiceTest, NoObserverForPvmDefault) {
   ApplicationList app_list = crostini::CrostiniTestHelper::BasicAppList(
       "app 1", "PvmDefault", "container");
+  app_list.set_vm_type(vm_tools::apps::ApplicationList_VmType_PLUGIN_VM);
   std::string app_id_1 = crostini::CrostiniTestHelper::GenerateAppId(
       "app 1", "PvmDefault", "container");
 
@@ -207,7 +208,9 @@ TEST_F(GuestOsRegistryServiceTest, NoObserverForPvmDefault) {
                                 testing::UnorderedElementsAre(app_id_1),
                                 testing::IsEmpty()))
       .Times(0);
-  service()->ClearApplicationList("PvmDefault", "");
+  service()->ClearApplicationList(
+      GuestOsRegistryService::VmType::ApplicationList_VmType_PLUGIN_VM,
+      "PvmDefault", "");
 }
 
 TEST_F(GuestOsRegistryServiceTest, ZeroAppsInstalledHistogram) {
@@ -354,7 +357,9 @@ TEST_F(GuestOsRegistryServiceTest, ClearApplicationList) {
                                          app_id_1, app_id_2, app_id_3, app_id_4,
                                          crostini::GetTerminalId()));
 
-  service()->ClearApplicationList("vm 2", "");
+  service()->ClearApplicationList(
+      GuestOsRegistryService::VmType::ApplicationList_VmType_TERMINA, "vm 2",
+      "");
 
   EXPECT_THAT(GetRegisteredAppIds(),
               testing::UnorderedElementsAre(app_id_1, app_id_2,
