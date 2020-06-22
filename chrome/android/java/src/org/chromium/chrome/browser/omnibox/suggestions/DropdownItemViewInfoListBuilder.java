c721d2afd2eaf8badc5b33c0b58c589e09b4f392
Tomasz Wiszkowski <ender@google.com>
2020-06-22 21:00:22 +0000

    Extract DropdownItemViewInfoListManager from AutocompleteMediator
    This change moves the suggestions list management (expanding and
    collapsing groups, updating model values etc) from an
    AutocompleteMediator to a dedicated class, and supplies a suite of
    tests to validate behavior.
    
    Bug: 1073169
    Change-Id: I8ec99398c06bf52db1a6aa9b80f38bdc74963d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2236395
    Commit-Queue: Ender <ender@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780968}

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListBuilder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListBuilder.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.omnibox.suggestions;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
@@ -199,6 +200,7 @@ class DropdownItemViewInfoListBuilder {
      * @return List of DropdownItemViewInfo representing the corresponding content of the
      *          suggestions list.
      */
+    @NonNull
     List<DropdownItemViewInfo> buildDropdownViewInfoList(AutocompleteResult autocompleteResult) {
         mHeaderProcessor.onSuggestionsReceived();
         for (int index = 0; index < mPriorityOrderedSuggestionProcessors.size(); index++) {
