33db0f740ba814fb3e2025b4cdf7bdc12d5d600b
Emily Stark <estark@google.com>
2020-06-22 20:37:21 +0000

    Reland "Hook up Always Show Full URLs option to omnibox path-hiding experiments"
    This is a reland of 43de05d571b9cf1e0d3246f58562741c87c1c59b
    
    Original change's description:
    > Hook up Always Show Full URLs option to omnibox path-hiding experiments
    > 
    > This CL hooks up the Always Show Full URLs option via OmniboxEditModel
    > to the path-hiding omnibox UI experiments. Users will now be able to
    > opt out of reveal-on-hover and show-on-interaction to always show the
    > full URL without any elisions.
    > 
    > Bug: 1094760
    > Change-Id: I3d5f15e7fcb6863701f00042daee9867fcbcf0bd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246863
    > Commit-Queue: Emily Stark <estark@chromium.org>
    > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#779389}
    
    Bug: 1094760
    Change-Id: I15c2f74c9598b1b5d88867513c1ed227324fb807
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252461
    Commit-Queue: Emily Stark <estark@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780953}

--- chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/macros.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
@@ -29,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/omnibox/browser/omnibox_popup_model.h"
 #include "components/omnibox/browser/test_scheme_classifier.h"
+#include "components/omnibox/common/omnibox_features.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/url_constants.h"
 #include "content/public/test/browser_test.h"
@@ -68,7 +70,10 @@ void SetClipboardText(ui::ClipboardBuffer buffer, const std::string& text) {
 
 class OmniboxViewViewsTest : public InProcessBrowserTest {
  protected:
-  OmniboxViewViewsTest() {}
+  OmniboxViewViewsTest() {
+    scoped_feature_list_.InitAndEnableFeature(
+        omnibox::kOmniboxContextMenuShowFullUrls);
+  }
 
   static void GetOmniboxViewForBrowser(const Browser* browser,
                                        OmniboxView** omnibox_view) {
@@ -135,6 +140,8 @@ class OmniboxViewViewsTest : public InProcessBrowserTest {
 #endif
     return native_window;
   }
+
+  base::test::ScopedFeatureList scoped_feature_list_;
 
   DISALLOW_COPY_AND_ASSIGN(OmniboxViewViewsTest);
 };
@@ -706,6 +713,34 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, ReloadAfterKill) {
   EXPECT_EQ(base::ASCIIToUTF16(""), omnibox_view_views->GetText());
   EXPECT_EQ(GURL(url::kAboutBlankURL),
             browser()->location_bar_model()->GetURL());
+}
+
+// Omnibox un-elides and elides URL appropriately according to the Always Show
+// Full URLs setting.
+IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, AlwaysShowFullURLs) {
+  OmniboxView* omnibox_view = nullptr;
+  ASSERT_NO_FATAL_FAILURE(GetOmniboxViewForBrowser(browser(), &omnibox_view));
+  OmniboxViewViews* omnibox_view_views =
+      static_cast<OmniboxViewViews*>(omnibox_view);
+
+  ASSERT_TRUE(embedded_test_server()->Start());
+  GURL url = embedded_test_server()->GetURL("/title1.html");
+  base::string16 url_text = base::ASCIIToUTF16(url.spec());
+
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  // By default, the elided URL should be shown.
+  EXPECT_EQ(url_text,
+            base::ASCIIToUTF16("http://") + omnibox_view_views->GetText());
+
+  // After toggling the setting, the full URL should be shown.
+  chrome::ToggleShowFullURLs(browser());
+  EXPECT_EQ(url_text, omnibox_view_views->GetText());
+
+  // Toggling the setting again should go back to the elided URL.
+  chrome::ToggleShowFullURLs(browser());
+  EXPECT_EQ(url_text,
+            base::ASCIIToUTF16("http://") + omnibox_view_views->GetText());
 }
 
 // The following set of tests require UIA accessibility support, which only
