bef99dcafa3ad4e93b3fbe2da24e9e13b81431a6
Hans Wennborg <hans@chromium.org>
2020-06-22 10:27:52 +0000

    Remove/replace unnecessary includes of logging.h (sql/)
    If the file just needs the CHECK/CHECK_OP/NOTREACHED
    macros, use the appropriate header for that instead.
    Or if logging.h is not needed at all, remove it.
    
    This is both a nice cleanup (logging.h is a big header,
    and including it unnecessarily has compile-time costs),
    and part of the final step towards making logging.h no
    longer include check.h and the others.
    
    Bug: 1031540
    Change-Id: I06bc0c29ef1369ac3d104c404a032e2937d500c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254325
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Victor Costan <pwnall@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#780708}

--- sql/recover_module/pager.h
+++ sql/recover_module/pager.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstdint>
 #include <memory>
 
-#include "base/logging.h"
+#include "base/check_op.h"
 #include "base/sequence_checker.h"
 
 struct sqlite3_file;
--- sql/recover_module/table.h
+++ sql/recover_module/table.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "base/logging.h"
+#include "base/check_op.h"
 #include "base/optional.h"
 #include "sql/recover_module/parsing.h"
 #include "third_party/sqlite/sqlite3.h"
