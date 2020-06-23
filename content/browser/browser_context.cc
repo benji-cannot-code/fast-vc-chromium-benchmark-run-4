4dde7c65ec6e2fba74e9891bf91e9114113f771e
Aaron Colwell <acolwell@chromium.org>
2020-06-23 00:28:41 +0000

    Remove can_be_default param from GetStoragePartitionConfigForSite().
    Removing can_be_default parameter because all callers specify true for
    it. This parameter was used for the isolated app effort
    (https://crbug.com/69335), and is no longer being used outside
    of extensions.
    
    Bug: 1085275 159932
    Change-Id: I880eaf5aa956b707eea3fc33ae190104e40335ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250220
    Commit-Queue: Aaron Colwell <acolwell@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Auto-Submit: Aaron Colwell <acolwell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781073}

--- content/browser/browser_context.cc
+++ content/browser/browser_context.cc
@@ -260,7 +260,7 @@ StoragePartition* BrowserContext::GetStoragePartition(
 
   if (site_instance) {
     GetContentClient()->browser()->GetStoragePartitionConfigForSite(
-        browser_context, site_instance->GetSiteURL(), true, &partition_domain,
+        browser_context, site_instance->GetSiteURL(), &partition_domain,
         &partition_name, &in_memory);
   }
 
@@ -277,8 +277,7 @@ StoragePartition* BrowserContext::GetStoragePartitionForSite(
   bool in_memory;
 
   GetContentClient()->browser()->GetStoragePartitionConfigForSite(
-      browser_context, site, true, &partition_domain, &partition_name,
-      &in_memory);
+      browser_context, site, &partition_domain, &partition_name, &in_memory);
 
   return GetStoragePartitionFromConfig(browser_context, partition_domain,
                                        partition_name, in_memory, can_create);
