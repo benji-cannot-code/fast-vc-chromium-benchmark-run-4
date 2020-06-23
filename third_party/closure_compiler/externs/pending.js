cfb057c9fc88bdeb5ecb54dd1b755166f3fd38d5
dpapad <dpapad@chromium.org>
2020-06-23 17:12:24 +0000

    PDF Viewer: Leverage native URLSearchParams in OpenPDFParamsParser.
    - Change parseUrlParams_() implementation to use URLSearchParams,
      instead of manually extracting key/value pairs from the URL.
    - Replace getUiUrlParams() with shouldShowToolbar(). The former was
      too generic for its current usage. It can be re-generalized if/when
      that is actually needed.
    - Add some missing JS externs for OpenPDFParamsParser in pending.js
      externs, until the JS compiler externs are updated accordingly.
    
    Bug: None
    Change-Id: I3ce47d06332a61db5be93bd86f032d2db6e78c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254716
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781394}

--- third_party/closure_compiler/externs/pending.js
+++ third_party/closure_compiler/externs/pending.js
@@ -130,3 +130,10 @@ AnimationEffectReadOnly.prototype.getTiming = function() {};
 
 /** @return {!Array<!Object>} */
 AnimationEffectReadOnly.prototype.getKeyframes = function() {};
+
+/**
+ * @return {!IteratorIterable<!Array<string>>}
+ * @nosideeffects
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/keys
+ */
+URLSearchParams.prototype.keys = function() {};
