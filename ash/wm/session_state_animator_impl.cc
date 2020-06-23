fe93964023e07acd41a6497666c1e3ea1b9b763a
Sammie Quon <sammiequon@chromium.org>
2020-06-23 22:40:59 +0000

    wm: cleanup: Merge wm_window_animations into window_animations.
    Test: none
    Bug: none
    Change-Id: Ibc087b3fb0579ea3980ecb03a92b1e2133aa8d7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258856
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781583}

--- ash/wm/session_state_animator_impl.cc
+++ ash/wm/session_state_animator_impl.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
-#include "ash/wm/wm_window_animations.h"
+#include "ash/wm/window_animations.h"
 #include "base/barrier_closure.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window_event_dispatcher.h"
