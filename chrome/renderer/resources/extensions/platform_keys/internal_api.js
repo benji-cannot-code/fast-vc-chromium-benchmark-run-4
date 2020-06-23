940fc502972c9586cb4ab60ea21f61ef7ca1741b
Maria Petrisor <mpetrisor@chromium.org>
2020-06-23 13:22:06 +0000

    Add getKeyPairBySpki to platformKeys
    Add the new method chrome.platformKeys.getKeyPairBySpki() that,
    similarly to chrome.platformKeys.getKeyPair() returns subtleCrypto
    objects for a private and a public key.
    The key pair already exists on the device, and getKeyPairBySpki()
    receives a public_key_spki_der as input to identify which key pair
    to return.
    The already existing API function getKeyPair() receives a certificate,
    extracting the public_key_spki_der from it and returning the key
    pair based on that spki.
    In our use case (the Imprivata extension) we don't have access to a
    certificate in order for us to use getKeyPair(). A key pair is needed
    in Imprivata for sending authorization requests (signing data in the
    request with the private key) to the Imprivata server, which verifies
    our authorization request using the public key.
    
    Bug: 1073466
    Change-Id: I44ef0335ede498b0e5f4878554050999dbbeda85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2190675
    Commit-Queue: Maria Petrisor <mpetrisor@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Omar Morsi <omorsi@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781300}

--- chrome/renderer/resources/extensions/platform_keys/internal_api.js
+++ chrome/renderer/resources/extensions/platform_keys/internal_api.js
@@ -3,8 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(crbug.com/1096029): Make consumers use the API directly by calling
+// getInternalApi themselves.
 var binding = getInternalApi('platformKeysInternal');
 
 exports.$set('selectClientCertificates', binding.selectClientCertificates);
 exports.$set('sign', binding.sign);
 exports.$set('getPublicKey', binding.getPublicKey);
+exports.$set('getPublicKeyBySpki', binding.getPublicKeyBySpki);
--- chrome/renderer/resources/extensions/platform_keys_custom_bindings.js
+++ chrome/renderer/resources/extensions/platform_keys_custom_bindings.js
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Custom binding for the platformKeys API.
 
 var SubtleCrypto = require('platformKeys.SubtleCrypto').SubtleCrypto;
-var getPublicKey = require('platformKeys.getPublicKey').getPublicKey;
+var publicKeyUtil = require('platformKeys.getPublicKeyUtil');
+var getPublicKey = publicKeyUtil.getPublicKey;
+var getPublicKeyBySpki = publicKeyUtil.getPublicKeyBySpki;
 var internalAPI = require('platformKeys.internalAPI');
 
 var keyModule = require('platformKeys.Key');
@@ -60,5 +62,19 @@ apiBridge.registerCustomHook(function(api) {
           callback(createPublicKey(publicKey, algorithm),
                    createPrivateKey(publicKey, algorithm));
         });
+      });
+
+  apiFunctions.setHandleRequest(
+      'getKeyPairBySpki', function(publicKeySpkiDer, params, callback) {
+        getPublicKeyBySpki(
+            publicKeySpkiDer, params, function(publicKey, algorithm) {
+              if (bindingUtil.hasLastError()) {
+                callback();
+                return;
+              }
+              callback(
+                  createPublicKey(publicKey, algorithm),
+                  createPrivateKey(publicKey, algorithm));
+            });
       });
 });
