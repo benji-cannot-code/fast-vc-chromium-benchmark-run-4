22d130c79d5c1bbfff7469f2158e1c78233c4116
Hiroki Sato <hirokisato@google.com>
2020-06-23 02:30:19 +0000

    arc-a11y: Don't compute name from descendants for virtual nodes
    CL:2222068 modified ARC++ focusability and accessibility name
    computation so that focusable views gets name from its non-focusable
    descendants. We should not apply this to virtual nodes like WebView.
    
    AX-Relnotes: n/a.
    Bug: b:152930082
    Test: unit_tests --gtest_filter="AXTreeSourceArcTest.*"
    Test: manual. Open Gmail app. WebView nodes (leaf nodes) correctly gets focus from ChromeVox.
    Change-Id: I8bc7b46f1b557aa13b4953243ebbd53425f0052f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2256922
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Reviewed-by: Sara Kato <sarakato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781131}

--- chrome/browser/chromeos/arc/accessibility/accessibility_node_info_data_wrapper.cc
+++ chrome/browser/chromeos/arc/accessibility/accessibility_node_info_data_wrapper.cc
@@ -102,6 +102,11 @@ bool AccessibilityNodeInfoDataWrapper::CanBeAccessibilityFocused() const {
 
 bool AccessibilityNodeInfoDataWrapper::IsAccessibilityFocusableContainer()
     const {
+  if (IsVirtualNode()) {
+    return GetProperty(AXBooleanProperty::SCREEN_READER_FOCUSABLE) ||
+           GetProperty(AXBooleanProperty::FOCUSABLE);
+  }
+
   if (!IsImportantInAndroid() || (IsScrollableContainer() && !HasText()))
     return false;
 
@@ -634,6 +639,9 @@ bool AccessibilityNodeInfoDataWrapper::HasText() const {
 }
 
 bool AccessibilityNodeInfoDataWrapper::HasAccessibilityFocusableText() const {
+  if (IsVirtualNode())
+    return HasText();
+
   if (!IsImportantInAndroid() || !HasText())
     return false;
 
@@ -661,7 +669,7 @@ void AccessibilityNodeInfoDataWrapper::ComputeNameFromContents(
 
 void AccessibilityNodeInfoDataWrapper::ComputeNameFromContentsInternal(
     std::vector<std::string>* names) const {
-  if (IsAccessibilityFocusableContainer())
+  if (IsVirtualNode() || IsAccessibilityFocusableContainer())
     return;
 
   // Take the name from either content description or text. It's not clear
