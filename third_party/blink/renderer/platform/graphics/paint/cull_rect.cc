00a271c3536705213902ba84cd1e2d1bce96e6aa
Xianzhu Wang <wangxianzhu@chromium.org>
2020-06-23 16:06:57 +0000

    [CompositeAfterPaint] Fix cull rect in printing
    Change-Id: If25a2df97a3bf5a322c02d650eb5aaeff86f7722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258423
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781356}

--- third_party/blink/renderer/platform/graphics/paint/cull_rect.cc
+++ third_party/blink/renderer/platform/graphics/paint/cull_rect.cc
@@ -52,6 +52,15 @@ void CullRect::Move(const IntSize& offset) {
     rect_.Move(offset);
 }
 
+void CullRect::Move(const FloatSize& offset) {
+  if (IsInfinite())
+    return;
+
+  FloatRect float_rect(rect_);
+  float_rect.Move(offset);
+  rect_ = EnclosingIntRect(float_rect);
+}
+
 static void MapRect(const TransformPaintPropertyNode& transform,
                     IntRect& rect) {
   if (transform.IsIdentityOr2DTranslation()) {
@@ -64,15 +73,12 @@ static void MapRect(const TransformPaintPropertyNode& transform,
 }
 
 CullRect::ApplyTransformResult CullRect::ApplyTransformInternal(
-    const TransformPaintPropertyNode& transform,
-    bool clip_to_scroll_container) {
+    const TransformPaintPropertyNode& transform) {
   if (RuntimeEnabledFeatures::CompositeAfterPaintEnabled()) {
     if (const auto* scroll = transform.ScrollNode()) {
-      if (clip_to_scroll_container) {
-        rect_.Intersect(scroll->ContainerRect());
-        if (rect_.IsEmpty())
-          return kNotExpanded;
-      }
+      rect_.Intersect(scroll->ContainerRect());
+      if (rect_.IsEmpty())
+        return kNotExpanded;
 
       MapRect(transform, rect_);
 
@@ -104,8 +110,7 @@ CullRect::ApplyTransformResult CullRect::ApplyTransformInternal(
 
 void CullRect::ApplyTransforms(const TransformPaintPropertyNode& source,
                                const TransformPaintPropertyNode& destination,
-                               const base::Optional<CullRect>& old_cull_rect,
-                               bool clip_to_scroll_container) {
+                               const base::Optional<CullRect>& old_cull_rect) {
   DCHECK(RuntimeEnabledFeatures::CompositeAfterPaintEnabled());
 
   Vector<const TransformPaintPropertyNode*> scroll_translations;
@@ -131,7 +136,7 @@ void CullRect::ApplyTransforms(const TransformPaintPropertyNode& source,
           *last_transform, *scroll_translation->Parent(), rect_);
     }
     last_scroll_translation_result =
-        ApplyTransformInternal(*scroll_translation, clip_to_scroll_container);
+        ApplyTransformInternal(*scroll_translation);
     last_transform = scroll_translation;
   }
 
