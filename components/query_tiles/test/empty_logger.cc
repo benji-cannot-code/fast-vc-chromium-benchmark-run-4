670d023dc1c4b150e4b4c8b2db6e981c2966aa30
Hesen Zhang <hesen@google.com>
2020-06-23 20:38:13 +0000

    [Query Tiles]: Logger Setup.
    Part I to setup logger for TileService.
    
    Logger: The public interface to the log code.  Can be queried for state
    and returns base::Value objects.
    Logger::Observer: Used to notify external components (e.g. WebUI) about
    internal state changes.
    LogSink: The internal interface that should be notified by internal
    components about potential log-worthy changes.
    LogSource: The internal interface that Logger will request state from.
    
    Also Add BlackHoleLogSink and EmptyLogger for test purpose.
    
    
    Bug: 1066556
    Change-Id: Ib8ee202c650cf72de20ab5cb6b9038ea51d5f651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250891
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Hesen Zhang <hesen@chromium.org>
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Hesen Zhang <hesen@chromium.org>
    Auto-Submit: Hesen Zhang <hesen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781514}

---
+++ components/query_tiles/test/empty_logger.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/query_tiles/test/empty_logger.h"
+
+#include "base/values.h"
+
+namespace query_tiles {
+namespace test {
+
+void EmptyLogger::AddObserver(Observer* observer) {}
+
+void EmptyLogger::RemoveObserver(Observer* observer) {}
+
+base::Value EmptyLogger::GetServiceStatus() {
+  return base::Value();
+}
+
+base::Value EmptyLogger::GetTileData() {
+  return base::Value();
+}
+
+}  // namespace test
+}  // namespace query_tiles
