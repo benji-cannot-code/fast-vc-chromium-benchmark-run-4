99a744c6b3f8c5180014ccc89cbf605984fe1d34
James Long <yjlong@google.com>
2020-06-23 20:48:42 +0000

    Add inbound/outbound nodes to Android dependency visualization
    Two inputs were added controlling the inbound/outbound node display. The
    visualization now does a BFS outwards from the filtered node set to find
    more nodes to show, specific details on this logic can be found in
    Graph.getDataForD3. The inbound/outbound nodes are color coded with the
    closer ones being darker.
    
    Bug: 1093962
    Change-Id: I56ee9c9401a46bc0a45ca9274954429155a3b27d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258253
    Commit-Queue: James Long <yjlong@google.com>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781523}

--- tools/android/dependency_analysis/js/graph_store.js
+++ tools/android/dependency_analysis/js/graph_store.js
@@ -34,6 +34,8 @@ class GraphStore {
     // of what the state will look like.
     this.state = {
       includedNodes: [],
+      inboundDepth: 0,
+      outboundDepth: 0,
     };
   }
 
@@ -60,6 +62,14 @@ class GraphStore {
       // element and the deleted element, then pop.
       this.state.includedNodes.splice(deleteIndex, 1);
     }
+  }
+
+  setInboundDepth(depth) {
+    this.state.inboundDepth = depth;
+  }
+
+  setOutboundDepth(depth) {
+    this.state.outboundDepth = depth;
   }
 }
 
