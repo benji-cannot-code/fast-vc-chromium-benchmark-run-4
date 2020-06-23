42da04a1aaf3cb262fca5847360014dade4719d6
Liviu Tinta <liviutinta@chromium.org>
2020-06-23 14:12:50 +0000

    Add TRACE to LayerTreeHostImpl::ScrollUpdate
    Adding this TRACE would make it easier to look into wheel events bugs.
    
    Bug: 1087088
    Change-Id: Id90516476e6112699e8d030107f16c80a97e0773
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250592
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781324}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -4867,6 +4867,8 @@ InputHandlerScrollResult LayerTreeHostImpl::ScrollUpdate(
 
   // The current_native_scrolling_element should only be set for ScrollBegin.
   DCHECK(!scroll_state->data()->current_native_scrolling_element());
+  TRACE_EVENT2("cc", "LayerTreeHostImpl::ScrollUpdate", "dx",
+               scroll_state->delta_x(), "dy", scroll_state->delta_y());
 
   if (!CurrentlyScrollingNode())
     return InputHandlerScrollResult();
