ccb2e1c825fa13c8b6a478d230a9637b5228213e
Matt Falkenhagen <falken@chromium.org>
2020-06-23 05:21:07 +0000

    Loading Predictor: Add Stop() to PrefetchManager.
    Allows cancelling queued prefetch jobs, for parity with preconnect
    manager.
    
    Bug: 1092329
    Change-Id: I1bb227fe9b0a0efe5abb7ec6279a5b8c91073818
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2255947
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781187}

--- chrome/browser/predictors/prefetch_manager.h
+++ chrome/browser/predictors/prefetch_manager.h
@@ -58,6 +58,7 @@ struct PrefetchInfo {
 
   GURL url;
   size_t job_count = 0;
+  bool was_canceled = false;
   std::unique_ptr<PrefetchStats> stats;
   // Owns |this|.
   PrefetchManager* const manager;
@@ -110,8 +111,9 @@ class PrefetchManager {
   // Starts prefetch jobs keyed by |url|.
   void Start(const GURL& url, std::vector<PrefetchRequest> requests);
 
-  // TODO(falken): Implement Stop().
-  // void Stop(const GURL& url);
+  // Stops further prefetch jobs keyed by |url|. Queued jobs will never start;
+  // started jobs will continue to completion.
+  void Stop(const GURL& url);
 
   base::WeakPtr<PrefetchManager> GetWeakPtr() {
     return weak_factory_.GetWeakPtr();
