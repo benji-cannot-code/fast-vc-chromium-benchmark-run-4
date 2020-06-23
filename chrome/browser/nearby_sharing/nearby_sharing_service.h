e755a08f7996a945ab1432aa8bc56d56a43bdc65
Michael Hansen <hansenmichael@google.com>
2020-06-23 19:56:02 +0000

    [Nearby] Broadcast using FastInitiationManager.
    Adds support for Fast Initiation advertising to NearbySharingService.
    When StartAdvertising() is called, the service will acquire a
    BluetoothAdapter and use it to create a FastInitiationManager, which
    will handle broadcasting the advertisement.
    
    Bug: b:154846208
    Change-Id: I5ea0de14901bf14d7f20dd80ef21582c21b10665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2211132
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/master@{#781476}

--- chrome/browser/nearby_sharing/nearby_sharing_service.h
+++ chrome/browser/nearby_sharing/nearby_sharing_service.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NEARBY_SHARING_NEARBY_SHARING_SERVICE_H_
 #define CHROME_BROWSER_NEARBY_SHARING_NEARBY_SHARING_SERVICE_H_
 
+#include <string>
+#include <vector>
+
 #include "base/callback.h"
 #include "base/files/file_path.h"
 #include "chrome/browser/nearby_sharing/share_target_discovered_callback.h"
