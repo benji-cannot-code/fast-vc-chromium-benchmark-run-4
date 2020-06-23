32f1d2a25fab27dde753428ef1f88333517d074a
David Van Cleve <davidvc@chromium.org>
2020-06-23 21:15:02 +0000

    Trust Tokens: Allow nonexistent directories in database paths
    In some circumstances, ProfileNetworkContextService can set the
    directories it populates in NetworkContextParams (for the cookie store,
    reporting store, and so on) to a nonexistent path. Currently, the Trust
    Tokens backing store doesn't attempt to create directories when
    nonexistent, causing some tests to fail flakily when sql::Database::Open
    hits a LOG(DFATAL) after attempting to open a database at a path with a
    nonexistent parent directory. (It's flaky because the failure races with
    other backing stores that do attempt to create the directory in this
    scenario.)
    
    This change hopefully resolves the failures, by making
    SQLiteTrustTokenPersister::CreateForFilePath create the given path's
    directory if it does not exist.
    
    R=mmenke
    
    Fixed: 1098019
    Change-Id: Id075718035e1fd7e29cebc063d9126f1f33c4c83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258685
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781534}

--- services/network/trust_tokens/sqlite_trust_token_persister.cc
+++ services/network/trust_tokens/sqlite_trust_token_persister.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_split.h"
@@ -146,6 +147,11 @@ void SQLiteTrustTokenPersister::CreateForFilePath(
   TrustTokenDatabaseOwner::Create(
       /*db_opener=*/base::BindOnce(
           [](const base::FilePath& path, sql::Database* db) {
+            const base::FilePath directory = path.DirName();
+            if (!base::PathExists(directory) &&
+                !base::CreateDirectory(directory)) {
+              return false;
+            }
             return db->Open(path);
           },
           path),
