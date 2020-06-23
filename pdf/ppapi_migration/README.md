777762578c100cabce18a8de0457e9ab80557ed2
K. Moon <kmoon@chromium.org>
2020-06-23 21:18:37 +0000

    Create pdf/ppapi_migration directory
    Introduces a new directory for temporary code related to the PDF
    viewer's PPAPI migration. Code in this directory should abstract away
    usage of PPAPI, so it may easily be removed after the migration is done.
    
    Also moves the existing pdf/geometry_conversions.{cc, h} files, as an
    early example of the kind of code that should be in this directory.
    
    Bug: 1061182
    Change-Id: I01372843f4149a607ef1b6dab80277986c177d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261413
    Commit-Queue: K Moon <kmoon@chromium.org>
    Reviewed-by: Daniel Hosseinian <dhoss@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781536}

---
+++ pdf/ppapi_migration/README.md
@@ -0,0 +1,6 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# PPAPI migration utilities
+
+This directory contains utilities for bridging from legacy APIs during the PDF
+viewer's [migration from Pepper](https://crbug.com/702993). The utilities should
+be designed for easy removal once the migration is complete.
--- pdf/ppapi_migration/geometry_conversions.cc
+++ pdf/ppapi_migration/geometry_conversions.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "pdf/geometry_conversions.h"
+#include "pdf/ppapi_migration/geometry_conversions.h"
 
 #include "ppapi/c/pp_rect.h"
 #include "ppapi/c/pp_size.h"
