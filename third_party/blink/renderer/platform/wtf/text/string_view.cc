007b76bd97c11fc1cffa3287e38cd208c29b2f0c
Albert J. Wong <ajwong@chromium.org>
2020-06-23 23:02:42 +0000

    Generalize and clean up Lower/UpperASCII() code.
    Allow implementation sharing of the LowerASCII() and UpperASCII() code in
    StringImpl and StringView.
    
    Also clean up LowerASCII() and UpperASCII() implementations to avoid
    extra conditionals and rely more on compiler vectorization as opposed to
    hand-rolled optimizations.
    
    Inspiration taken from Richard Townsend's CL here
    https://chromium-review.googlesource.com/c/chromium/src/+/2237979
    
    This is partially broken out of
      https://chromium-review.googlesource.com/c/chromium/src/+/2204539
    
    which in turn is broken out of
      https://chromium-review.googlesource.com/c/chromium/src/+/1557854
    
    Bug: 1083392
    Change-Id: I8831acc511e1630cb733fc5062321e626b682b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2259143
    Commit-Queue: Albert J. Wong <ajwong@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781591}

--- third_party/blink/renderer/platform/wtf/text/string_view.cc
+++ third_party/blink/renderer/platform/wtf/text/string_view.cc
@@ -10,6 +10,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace WTF {
+namespace {
+class StackStringViewAllocator {
+ public:
+  explicit StackStringViewAllocator(
+      StringView::StackBackingStore& backing_store)
+      : backing_store_(backing_store) {}
+  using ResultStringType = StringView;
+
+  template <typename CharType>
+  StringView Alloc(wtf_size_t length, CharType*& buffer) {
+    buffer = backing_store_.Realloc<CharType>(length);
+    return StringView(buffer, length);
+  }
+
+  StringView CoerceOriginal(StringView string) { return string; }
+
+ private:
+  StringView::StackBackingStore& backing_store_;
+};
+}  // namespace
 
 StringView::StringView(const UChar* chars)
     : StringView(chars, chars ? LengthOfNullTerminatedString(chars) : 0) {}
@@ -108,6 +128,12 @@ bool EqualIgnoringASCIICase(const StringView& a, const StringView& b) {
     return EqualIgnoringASCIICase(a.Characters16(), b.Characters8(),
                                   a.length());
   return EqualIgnoringASCIICase(a.Characters16(), b.Characters16(), a.length());
+}
+
+StringView StringView::LowerASCIIMaybeUsingBuffer(
+    StackBackingStore& buffer) const {
+  return ConvertASCIICase(*this, LowerConverter(),
+                          StackStringViewAllocator(buffer));
 }
 
 }  // namespace WTF
