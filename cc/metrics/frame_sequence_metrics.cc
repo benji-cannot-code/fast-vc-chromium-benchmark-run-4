b2b7309b406b72d773b15479aba14dc6f844dc10
Sadrul Habib Chowdhury <sadrul@chromium.org>
2020-06-23 02:25:19 +0000

    [cc/metrics] Create instant trace-events when a UMA is reported.
    Create a trace-event at the point when UMA is reported, so that it
    is easier to associate the UMA with the associated frames.
    
    BUG=1095186
    
    Change-Id: Id78695d21f3178e69c6025f580c2db49a67e3360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251300
    Auto-Submit: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781128}

--- cc/metrics/frame_sequence_metrics.cc
+++ cc/metrics/frame_sequence_metrics.cc
@@ -341,6 +341,11 @@ base::Optional<int> FrameSequenceMetrics::ThroughputData::ReportHistogram(
   ThroughputUkmReporter* const ukm_reporter = metrics->ukm_reporter();
 
   if (is_animation) {
+    TRACE_EVENT_INSTANT2("cc,benchmark", "PercentDroppedFrames.AllAnimations",
+                         TRACE_EVENT_SCOPE_THREAD, "frames_expected",
+                         data.frames_expected, "frames_produced",
+                         data.frames_produced);
+
     UMA_HISTOGRAM_PERCENTAGE(
         "Graphics.Smoothness.PercentDroppedFrames.AllAnimations", percent);
     if (ukm_reporter) {
@@ -350,6 +355,10 @@ base::Optional<int> FrameSequenceMetrics::ThroughputData::ReportHistogram(
   }
 
   if (is_interaction) {
+    TRACE_EVENT_INSTANT2("cc,benchmark", "PercentDroppedFrames.AllInteractions",
+                         TRACE_EVENT_SCOPE_THREAD, "frames_expected",
+                         data.frames_expected, "frames_produced",
+                         data.frames_produced);
     UMA_HISTOGRAM_PERCENTAGE(
         "Graphics.Smoothness.PercentDroppedFrames.AllInteractions", percent);
     if (ukm_reporter) {
@@ -359,6 +368,10 @@ base::Optional<int> FrameSequenceMetrics::ThroughputData::ReportHistogram(
   }
 
   if (is_animation || is_interaction) {
+    TRACE_EVENT_INSTANT2("cc,benchmark", "PercentDroppedFrames.AllSequences",
+                         TRACE_EVENT_SCOPE_THREAD, "frames_expected",
+                         data.frames_expected, "frames_produced",
+                         data.frames_produced);
     UMA_HISTOGRAM_PERCENTAGE(
         "Graphics.Smoothness.PercentDroppedFrames.AllSequences", percent);
     if (ukm_reporter) {
