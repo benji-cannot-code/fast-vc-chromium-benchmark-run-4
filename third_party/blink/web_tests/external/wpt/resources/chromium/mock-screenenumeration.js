9774ffec451a03d224373bf37c6ccdf946ed7f1a
Arnaud Mandy <arnaud.mandy@intel.com>
2020-06-23 21:03:41 +0000

    wpt: screen enumeration: split screen ID settings
    Split setIds(internalId, primaryId) into 2 distinct methods,
    setInternalId(id) and setPrimaryId(id).
    
    Bug: 1075656
    Change-Id: I312950db43d0044cbaf923e458912816a06834e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245616
    Commit-Queue: Arnaud Mandy <arnaud.mandy@intel.com>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781530}

--- third_party/blink/web_tests/external/wpt/resources/chromium/mock-screenenumeration.js
+++ third_party/blink/web_tests/external/wpt/resources/chromium/mock-screenenumeration.js
@@ -21,8 +21,11 @@ var ScreenEnumerationTest = (() => {
       this.success_ = false;
     }
 
-    setIds(internalId, primaryId) {
+    setInternalId(internalId) {
       this.internalId_ = internalId;
+    }
+
+    setPrimaryId(primaryId) {
       this.primaryId_ = primaryId;
     }
 
