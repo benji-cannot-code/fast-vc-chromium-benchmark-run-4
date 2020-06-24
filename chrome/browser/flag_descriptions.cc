9097cbb10ac5e19d0843deb89100da9a988b479a
Melissa Zhang <melzhang@chromium.org>
2020-06-24 02:44:23 +0000

    [Sharesheet] Add flag for sharesheet.
    This CL adds a feature flag for enabling the sharesheet on desktop
    platforms.
    
    Bug: 1097623
    Change-Id: I5a99db0ed041b3669046a900ad3bedfbbb3f57fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2256941
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Commit-Queue: Melissa Zhang <melzhang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781673}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3865,6 +3865,9 @@ const char kSchedulerConfigurationConservative[] =
 const char kSchedulerConfigurationPerformance[] =
     "Enables Hyper-Threading on relevant CPUs.";
 
+const char kSharesheetName[] = "Sharesheet";
+const char kSharesheetDescription[] = "Chrome OS sharesheet.";
+
 const char kShowBluetoothDebugLogToggleName[] =
     "Show Bluetooth debug log toggle";
 const char kShowBluetoothDebugLogToggleDescription[] =
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2270,6 +2270,9 @@ extern const char kSchedulerConfigurationDescription[];
 extern const char kSchedulerConfigurationConservative[];
 extern const char kSchedulerConfigurationPerformance[];
 
+extern const char kSharesheetName[];
+extern const char kSharesheetDescription[];
+
 extern const char kShowBluetoothDebugLogToggleName[];
 extern const char kShowBluetoothDebugLogToggleDescription[];
 
