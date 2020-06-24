0cb2e0a91e149d3d975052c322c1b317cd06f166
zhaoyangli <zhaoyangli@chromium.org>
2020-06-24 00:57:57 +0000

    Roll src/ios/third_party/edo/src/ 243fc89ae..97121c640 (22 commits)
    https://chromium.googlesource.com/external/github.com/google/eDistantObject.git/+log/243fc89ae95b..97121c64019f
    
    $ git log 243fc89ae..97121c640 --date=short --no-merges --format='%ad %ae %s'
    2020-06-20 mobile-devx-github-bot Wrap EDOClientService::errorHandler c functions in extern "C"
    2020-06-10 albertbow Add warning message to `isKindOfClass:`.
    2020-05-02 sskhandp Add -Wunguarded-availability and fix availability warning
    2020-04-29 mobile-devx-github-bot Fix up EDO to work with OCMock 3.6
    2020-04-21 haowoo Use lightweight generics to retrieve root objects.
    2020-04-18 albertbow Internal change
    2020-04-16 mobile-devx-github-bot Internal change
    2020-04-16 sskhandp Internal change
    2020-04-09 mobile-devx-github-bot Fix up .andDo blocks to not call retainArguments that can accidentally cause retain loops in mocks.
    2020-02-19 sskhandp Store queue in variable to avoid repetitive calls.
    2020-02-13 mobile-devx-github-bot Make sure strongSelf and strongSelf.handlerSyncQueue is not nil.
    2020-02-11 albertbow Minor fix on comments.
    2020-02-03 tirodkar Fix project name
    2020-01-10 albertbow Rollforward EDORemoteException for user-thrown exceptions in remote invocation.
    2020-01-07 albertbow Roll forward the fallback of EDORemoteException to be the subclass of NSException.
    2020-01-02 mobile-devx-github-bot Internal change
    2020-01-02 albertbow Fallback EDORemoteException to be the subclass of NSException.
    2019-12-18 albertbow Rollback EDORemoteException.
    2019-12-17 albertbow Allow one-way blacklisting types in remote invocation.
    2019-12-17 albertbow Use EDORemoteException for user-thrown exceptions in remote invocation.
    2019-12-16 albertbow Expose EDORemoteException.
    2019-12-12 albertbow Create EDOInvocationException class.
    
    Created with:
      roll-dep src/ios/third_party/edo/src
    
    Changed BUILD.gn file and a test file in crashpad for the test to pass.
    
    Bug: 1097387
    Change-Id: Id6af6c2098ad32df72038efadd4cfbcd533bfb08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258418
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781640}

--- third_party/crashpad/crashpad/test/ios/crash_type_xctest.mm
+++ third_party/crashpad/crashpad/test/ios/crash_type_xctest.mm
@@ -50,9 +50,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Override EDO default error handler.  Without this, the default EDO error
   // handler will throw an error and fail the test.
-  [EDOClientService setErrorHandler:^(NSError* error){
+  EDOSetClientErrorHandler(^(NSError* error){
       // Do nothing.
-  }];
+  });
 }
 
 - (void)setUp {
