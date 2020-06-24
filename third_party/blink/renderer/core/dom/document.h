d2a696568a832215e978683e0533b1f6133eedc5
Nate Chapin <japhet@chromium.org>
2020-06-24 00:29:15 +0000

    Reland "Clean up Document referrer functions"
    * Remove Document::SetReferrerPolicy and OutgoingReferrer, use the
      LocalDOMWindow variants
    * Remove most of the uses of Document::GetReferrerPolicy
    
    Bug: 1029822
    Change-Id: I33bd81b95b033307ccdb2a9a835f2c952006e12f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258426
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781631}

--- third_party/blink/renderer/core/dom/document.h
+++ third_party/blink/renderer/core/dom/document.h
@@ -317,10 +317,10 @@ class CORE_EXPORT Document : public ContainerNode,
 
   using TreeScope::getElementById;
 
-  // TODO(crbug.com/1029822) Former ExecutionContext overrides. Most of these
-  // should move to LocalDOMWindow.
+  // Gets the associated LocalDOMWindow even if this Document is associated with
+  // an HTMLImportsController.
   LocalDOMWindow* ExecutingWindow() const;
-  String OutgoingReferrer() const;
+
   network::mojom::ReferrerPolicy GetReferrerPolicy() const;
 
   bool DocumentPolicyFeatureObserved(
@@ -339,7 +339,6 @@ class CORE_EXPORT Document : public ContainerNode,
   bool IsSandboxed(network::mojom::blink::WebSandboxFlags mask) const;
   SecureContextMode GetSecureContextMode() const;
   void SetSecureContextModeForTesting(SecureContextMode);
-  void SetReferrerPolicy(network::mojom::ReferrerPolicy);
   OriginTrialContext* GetOriginTrialContext() const;
 
   String addressSpaceForBindings(ScriptState*) const;
--- third_party/blink/renderer/core/loader/private/prerender_handle.h
+++ third_party/blink/renderer/core/loader/private/prerender_handle.h
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+class ExecutionContext;
 class Document;
 class PrerenderClient;
 
@@ -62,7 +63,7 @@ class PrerenderHandle final : public GarbageCollected<PrerenderHandle>,
 
   using PassKey = util::PassKey<PrerenderHandle>;
   PrerenderHandle(PassKey,
-                  Document&,
+                  ExecutionContext*,
                   PrerenderClient*,
                   const KURL&,
                   HeapMojoRemote<mojom::blink::PrerenderHandle,
