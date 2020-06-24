2218f37bc0b764c01147af9dc1e2cf373bca1183
David Schinazi <dschinazi@chromium.org>
2020-06-24 01:09:09 +0000

    Introduce Legacy Version Encapsulation
    This CL rolls QUICHE to include two changes:
    Legacy Version Encapsulation, and a fix to
    ParseQuicTagVector. It additionally adds the
    support required for Legacy Version
    Encapsulation to work, and adds a command-line
    flag that allows us to test it. This CL also
    removes quic_utils_chromium since every method
    in it has now been moved to QUICHE.
    
    R=renjietang@chromium.org
    
    Change-Id: Iac2423df461656a47398f4579c279a22254390e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261103
    Commit-Queue: David Schinazi <dschinazi@chromium.org>
    Reviewed-by: Renjie Tang <renjietang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781645}

--- components/network_session_configurator/browser/network_session_configurator.cc
+++ components/network_session_configurator/browser/network_session_configurator.cc
@@ -28,10 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_stream_factory.h"
 #include "net/quic/platform/impl/quic_flags_impl.h"
 #include "net/quic/quic_context.h"
-#include "net/quic/quic_utils_chromium.h"
 #include "net/spdy/spdy_session.h"
 #include "net/spdy/spdy_session_pool.h"
 #include "net/third_party/quiche/src/quic/core/quic_packets.h"
+#include "net/third_party/quiche/src/quic/core/quic_tag.h"
 #include "net/third_party/quiche/src/spdy/core/spdy_protocol.h"
 
 #if defined(OS_MACOSX) && !defined(OS_IOS)
@@ -206,7 +206,7 @@ quic::QuicTagVector GetQuicConnectionOptions(
     return quic::QuicTagVector();
   }
 
-  return net::ParseQuicConnectionOptions(it->second);
+  return quic::ParseQuicTagVector(it->second);
 }
 
 quic::QuicTagVector GetQuicClientConnectionOptions(
@@ -216,7 +216,7 @@ quic::QuicTagVector GetQuicClientConnectionOptions(
     return quic::QuicTagVector();
   }
 
-  return net::ParseQuicConnectionOptions(it->second);
+  return quic::ParseQuicTagVector(it->second);
 }
 
 bool ShouldQuicCloseSessionsOnIpChange(
@@ -626,8 +626,13 @@ void ParseCommandLineAndFieldTrials(const base::CommandLine& command_line,
 
   if (params->enable_quic) {
     if (command_line.HasSwitch(switches::kQuicConnectionOptions)) {
-      quic_params->connection_options = net::ParseQuicConnectionOptions(
+      quic_params->connection_options = quic::ParseQuicTagVector(
           command_line.GetSwitchValueASCII(switches::kQuicConnectionOptions));
+    }
+    if (command_line.HasSwitch(switches::kQuicClientConnectionOptions)) {
+      quic_params->client_connection_options =
+          quic::ParseQuicTagVector(command_line.GetSwitchValueASCII(
+              switches::kQuicClientConnectionOptions));
     }
 
     if (command_line.HasSwitch(switches::kQuicMaxPacketLength)) {
