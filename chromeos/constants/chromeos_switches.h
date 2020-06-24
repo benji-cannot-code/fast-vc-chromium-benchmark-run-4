6e3d90796680d785574f8b1d6e42b42344080e36
Yves Arrouye <drcrash@google.com>
2020-06-24 20:16:32 +0000

    Add a flag to control whether to use PSM or not
    By default the absence of the flag means that the feature is disabled.
    This is to protect code while in development.
    
    Bug: chromium:1098854
    Test: Manual
    Change-Id: I20063bf28ea74d9aeaa1bb541809fc6ab7162c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264577
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Auto-Submit: Yves Arrouye <drcrash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782032}

--- chromeos/constants/chromeos_switches.h
+++ chromeos/constants/chromeos_switches.h
@@ -129,6 +129,8 @@ extern const char kEnterpriseEnableForcedReEnrollment[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const char kEnterpriseEnableInitialEnrollment[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+extern const char kEnterpriseEnablePrivateSetMembership[];
+COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const char kEnterpriseEnableZeroTouchEnrollment[];
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const char kEnterpriseEnrollmentInitialModulus[];
