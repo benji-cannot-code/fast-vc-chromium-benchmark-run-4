3298a43ba66e1441ddcec5f4eda5e800cd57265c
Charlie Hu <chenleihu@google.com>
2020-06-24 20:09:50 +0000

    Fix timeout issue using HashSet as DocumentPolicyFeatureSet
    Using |DefaultHash<mojom::blink::DocumentPolicyFeature>| to substitute
    |IntHash<mojom::blink::DocumentPolicyFeature>| fixes the timeout
    issue in release build.
    
    Note: Although |DefaultHash<mojom::blink::DocumentPolicyFeature>| is
    defined in document_policy_feature.mojom-blink-forward.h, its
    hash impl |blink_mojom_internal_DocumentPolicyFeature_DataHashFn| is
    defined in document_policy_feature.mojom-blink.h. Include forward.h
    is not enough for DefaultHash to be used as HashTrait.
    
    Bug: 1069021
    Change-Id: Ia267c2de06f8fbc787e471103fcccc8ce861b1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264854
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Charlie Hu <chenleihu@google.com>
    Cr-Commit-Position: refs/heads/master@{#782020}

--- third_party/blink/renderer/core/feature_policy/policy_helper.h
+++ third_party/blink/renderer/core/feature_policy/policy_helper.h
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/public/common/feature_policy/feature_policy.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom-blink.h"
-#include "third_party/blink/public/mojom/feature_policy/document_policy_feature.mojom-blink-forward.h"
+#include "third_party/blink/public/mojom/feature_policy/document_policy_feature.mojom-blink.h"
 #include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink-forward.h"
 #include "third_party/blink/public/mojom/feature_policy/feature_policy_feature.mojom-blink-forward.h"
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
-#include "third_party/blink/renderer/platform/wtf/linked_hash_set.h"
+#include "third_party/blink/renderer/platform/wtf/hash_set.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -61,11 +61,7 @@ class PolicyParserMessageBuffer {
 
 using FeatureNameMap = HashMap<String, mojom::blink::FeaturePolicyFeature>;
 
-// TODO(crbug.com/1069021): Use WTF::HashSet as DocumentPolicyFeatureSet
-// container will cause unknown timeout in release build only.
-using DocumentPolicyFeatureSet =
-    LinkedHashSet<mojom::blink::DocumentPolicyFeature,
-                  IntHash<mojom::blink::DocumentPolicyFeature>>;
+using DocumentPolicyFeatureSet = HashSet<mojom::blink::DocumentPolicyFeature>;
 
 class FeatureContext;
 
