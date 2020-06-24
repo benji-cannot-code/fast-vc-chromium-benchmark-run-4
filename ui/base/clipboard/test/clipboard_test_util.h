dff66969bb0e2ccb6a7ea702a0d9335cd1b89ae3
Darwin Huang <huangdarwin@chromium.org>
2020-06-24 20:58:18 +0000

    ui/base/clipboard: Remove unused include.
    Change-Id: Ie2125b4bc4cf46db8f0e0f73f80dd30224ae049a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254352
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Reviewed-by: Jarryd Goodman <jarrydg@chromium.org>
    Auto-Submit: Darwin Huang <huangdarwin@chromium.org>
    Commit-Queue: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782057}

--- ui/base/clipboard/test/clipboard_test_util.h
+++ ui/base/clipboard/test/clipboard_test_util.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_CLIPBOARD_TEST_CLIPBOARD_TEST_UTIL_H_
 #define UI_BASE_CLIPBOARD_TEST_CLIPBOARD_TEST_UTIL_H_
 
-#include "base/macros.h"
-
 class SkBitmap;
 
 namespace ui {
