2b7972bf983e4b42886688906a61bc0e95f34274
David Van Cleve <davidvc@chromium.org>
2020-06-24 19:52:16 +0000

    Remove the guts of content::Referrer::SanitizeForRequest
    content::Referrer::SanitizeForRequest duplicates some relatively
    intricate URL-related logic that exists independently in //net. This
    change changes the method to be a thin wrapper around
    net::URLRequestJob::ComputeReferrerForPolicy, the other location that
    contains this logic.
    
    There's one behavior change: SanitizeForRequest no longer removes
    referrers for requests with non-HTTP(S) destination URLs. This makes it
    consistent with the other two places in Chromium where we compute
    referrers: net::URLRequestJob::ComputeReferrerForPolicy and
    blink::SecurityPolicy::GenerateReferrer. (I guess the benefit of
    having the same code in three places is that you can majority vote when
    the behavior disagrees!)
    
    R=mmenke
    
    Test: Existing tests (content_unittests, chrome:browser_tests) pass
    Bug: 1094526
    Change-Id: I1004db373d686b877ebe492efbd200669151e789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2243721
    Commit-Queue: David Van Cleve <davidvc@chromium.org>
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781990}

--- net/url_request/url_request_job_unittest.cc
+++ net/url_request/url_request_job_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_request_test_util.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/url_util.h"
 
 using net::test::IsError;
 using net::test::IsOk;
@@ -717,7 +718,9 @@ TEST(URLRequestJobComputeReferrer, DoesntTruncateReferrerWithLongRef) {
 
 TEST(URLRequestJobComputeReferrer, InvalidSchemeReferrer) {
   const GURL kOriginalReferrer("about:blank");
-  ASSERT_FALSE(kOriginalReferrer.SchemeIsValidForReferrer());
+  ASSERT_FALSE(url::IsReferrerScheme(
+      kOriginalReferrer.spec().data(),
+      kOriginalReferrer.parsed_for_possibly_invalid_spec().scheme));
 
   EXPECT_EQ(URLRequestJob::ComputeReferrerForPolicy(
                 URLRequest::NEVER_CLEAR_REFERRER, kOriginalReferrer,
