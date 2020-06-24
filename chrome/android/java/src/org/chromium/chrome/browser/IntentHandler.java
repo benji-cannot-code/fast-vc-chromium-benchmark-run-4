534ca3b293930e2ff0ff53b42b829e8aab4952ca
Evan Stade <estade@chromium.org>
2020-06-24 17:20:57 +0000

    WebLayer: share directly to WebApk as appropriate
    This matches the external intent handling in Chrome, where a WebApk is
    opened without a chooser dialog when it's the only non-browser handler
    for an intent.
    
    WebApkValidator, WebApkVerifySignature, and some constants are moved
    to //components/webapk in a directory and build target structure that
    mirrors //chrome/android/webapk, i.e. separate "client" and "common"
    libraries.
    
    Bug: 1063874
    Change-Id: I55fb0968489a4a919bb383b182413e913091a020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254350
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781934}

--- chrome/android/java/src/org/chromium/chrome/browser/IntentHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/IntentHandler.java
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser;
 
-import static org.chromium.webapk.lib.common.WebApkConstants.WEBAPK_PACKAGE_PREFIX;
+import static org.chromium.components.webapk.lib.common.WebApkConstants.WEBAPK_PACKAGE_PREFIX;
 
 import android.app.Activity;
 import android.app.KeyguardManager;
