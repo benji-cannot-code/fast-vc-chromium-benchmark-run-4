f05111f5178d1d6f479dc1e9de095f6b38e35586
Miguel Casas-Sanchez <mcasas@chromium.org>
2020-06-24 03:02:23 +0000

    RELAND: base/power_monitor: Add method to query thermal state
    This CL relands crrev.com/c/2248729 that got reverted due to breaking
    Mac Builder (dbg) and others (power_monitor_device_source_unittest.cc
    did not include logging.h and it couldn't find 'DVLOG'). The original
    CL description only said "wip wip" o_O, which is sad.
    
    Essentially this CL adds a GetCurrentThermalState() to PowerMonitor to
    actively retrieve the current ThermalState. The ThermalState is sent to
    PowerObservers, but newly created clients still need to query it at
    least one time.
    
    TBR=fdoray@chromium.org since the diff is just an include file.
    
    
    Bug: 1071431
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248729
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Reviewed-by: François Doray <fdoray@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781322}
    
    Change-Id: Ia62ddfd8afc167473f3456e4982f51eaa52a833f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261363
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781678}

--- base/test/power_monitor_test_base.cc
+++ base/test/power_monitor_test_base.cc
@@ -12,13 +12,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-PowerMonitorTestSource::PowerMonitorTestSource()
-    : test_on_battery_power_(false) {
+PowerMonitorTestSource::PowerMonitorTestSource() {
   DCHECK(MessageLoopCurrent::Get())
       << "PowerMonitorTestSource requires a MessageLoop.";
 }
 
 PowerMonitorTestSource::~PowerMonitorTestSource() = default;
+
+PowerObserver::DeviceThermalState
+PowerMonitorTestSource::GetCurrentThermalState() {
+  return current_thermal_state_;
+}
 
 void PowerMonitorTestSource::GeneratePowerStateEvent(bool on_battery_power) {
   test_on_battery_power_ = on_battery_power;
@@ -43,6 +47,7 @@ bool PowerMonitorTestSource::IsOnBatteryPowerImpl() {
 void PowerMonitorTestSource::GenerateThermalThrottlingEvent(
     PowerObserver::DeviceThermalState new_thermal_state) {
   ProcessThermalEvent(new_thermal_state);
+  current_thermal_state_ = new_thermal_state;
   RunLoop().RunUntilIdle();
 }
 
