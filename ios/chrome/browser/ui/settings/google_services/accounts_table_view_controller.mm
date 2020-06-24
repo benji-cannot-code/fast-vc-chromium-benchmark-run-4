c33e3fcf34a112513d1456249561a7e482f49870
Nohemi Fernandez <fernandex@chromium.org>
2020-06-24 13:32:29 +0000

    [iOS] Re-enable EarlGrey test testSignInDisconnectFromChromeManaged.
    This patch also removes mentions of 'experimental' in code.
    
    Bug: 1097994
    Change-Id: I8bd8a8748e7d7511d514d705ad5bcc3898464901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2260777
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781833}

--- ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/google_services/accounts_table_view_controller.mm
@@ -68,10 +68,9 @@ typedef NS_ENUM(NSInteger, SectionIdentifier) {
 typedef NS_ENUM(NSInteger, ItemType) {
   ItemTypeAccount = kItemTypeEnumZero,
   ItemTypeAddAccount,
-  // Provides experimental and production sign out items. In the experimental
-  // item, this type is used only for non-managed accounts.
+  // Provides sign out items used only for non-managed accounts.
   ItemTypeSignOut,
-  // Experimental sign out item that clears Chrome data. Used for both managed
+  // Sign out item that clears Chrome data. Used for both managed
   // and non-managed accounts.
   ItemTypeSignOutAndClearData,
   ItemTypeHeader,
@@ -242,11 +241,11 @@ typedef NS_ENUM(NSInteger, ItemType) {
   [model addSectionWithIdentifier:SectionIdentifierSignOut];
   // Adds a signout option if the account is not managed.
   if (![self authService]->IsAuthenticatedIdentityManaged()) {
-    [model addItem:[self experimentalSignOutItem]
+    [model addItem:[self signOutItem]
         toSectionWithIdentifier:SectionIdentifierSignOut];
   }
   // Adds a signout and clear data option.
-  [model addItem:[self experimentalSignOutAndClearDataItem]
+  [model addItem:[self signOutAndClearDataItem]
       toSectionWithIdentifier:SectionIdentifierSignOut];
 
   // Adds a footer with signout explanation depending on the type of
@@ -313,16 +312,6 @@ typedef NS_ENUM(NSInteger, ItemType) {
 }
 
 - (TableViewItem*)signOutItem {
-  TableViewDetailTextItem* item =
-      [[TableViewDetailTextItem alloc] initWithType:ItemTypeSignOut];
-  item.text =
-      l10n_util::GetNSString(IDS_IOS_OPTIONS_ACCOUNTS_SIGN_OUT_TURN_OFF_SYNC);
-  item.accessibilityTraits |= UIAccessibilityTraitButton;
-  item.accessibilityIdentifier = kSettingsAccountsTableViewSignoutCellId;
-  return item;
-}
-
-- (TableViewItem*)experimentalSignOutItem {
   TableViewTextItem* item =
       [[TableViewTextItem alloc] initWithType:ItemTypeSignOut];
   item.text =
@@ -333,7 +322,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
   return item;
 }
 
-- (TableViewItem*)experimentalSignOutAndClearDataItem {
+- (TableViewItem*)signOutAndClearDataItem {
   TableViewTextItem* item =
       [[TableViewTextItem alloc] initWithType:ItemTypeSignOutAndClearData];
   item.text = l10n_util::GetNSString(
