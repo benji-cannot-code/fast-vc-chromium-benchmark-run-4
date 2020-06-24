796268a9377594595f80470a0ae44ed6d8166c3a
Nico Weber <thakis@chromium.org>
2020-06-24 15:31:41 +0000

    mac: Make mach_o_image_reader_mac_unittest.cc build with the MacOSX11.sdk.
    PAGE_SIZE now needs an explicit include of mac/vm_param.h.
    
    Bug: 1098738
    Change-Id: Ib56e3b080919edb08aa12aeae0fe3357d5022881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264255
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781881}

--- chrome/common/safe_browsing/mach_o_image_reader_mac_unittest.cc
+++ chrome/common/safe_browsing/mach_o_image_reader_mac_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <libkern/OSByteOrder.h>
 #include <mach-o/fat.h>
 #include <mach-o/loader.h>
+#include <mach/vm_param.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
