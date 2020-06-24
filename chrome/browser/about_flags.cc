fa1722d32f0c0fc96dc39424316714f72127e30b
Robert Ogden <robertogden@chromium.org>
2020-06-24 22:45:46 +0000

    Add field trial param and about:flag for IsolatedPrerender NSP
    Leaves the command line flag for use in browsertesting and in the
    about:flag.
    
    Note: The browser tests can't set features with params because of this:
    http://shortn/_ZytN1SHiqN
    
    Bug: 1082505
    Change-Id: I474fe012c6a00b15f38d70334d677a29815b24b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265178
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782107}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/permissions/quiet_notification_permission_ui_config.h"
 #include "chrome/browser/predictors/loading_predictor_config.h"
 #include "chrome/browser/prerender/isolated/isolated_prerender_features.h"
+#include "chrome/browser/prerender/isolated/isolated_prerender_params.h"
 #include "chrome/browser/prerender/prerender_field_trial.h"
 #include "chrome/browser/resource_coordinator/tab_manager_features.h"
 #include "chrome/browser/search/ntp_features.h"
@@ -2715,6 +2716,10 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_WITH_PARAMS_VALUE_TYPE(features::kIsolatePrerenders,
                                     kIsolatedPrerenderFeatureWithPrefetchLimit,
                                     "Prefetch Limit")},
+    {"enable-google-srp-isolated-prerender-nsp",
+     flag_descriptions::kEnableSRPIsolatedPrerendersNSPName,
+     flag_descriptions::kEnableSRPIsolatedPrerendersNSPDescription, kOsAll,
+     SINGLE_VALUE_TYPE(kIsolatedPrerenderEnableNSPCmdLineFlag)},
     {"allow-insecure-localhost", flag_descriptions::kAllowInsecureLocalhostName,
      flag_descriptions::kAllowInsecureLocalhostDescription, kOsAll,
      SINGLE_VALUE_TYPE(switches::kAllowInsecureLocalhost)},
--- chrome/browser/prerender/isolated/isolated_prerender_params.h
+++ chrome/browser/prerender/isolated/isolated_prerender_params.h
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "url/gurl.h"
 
+// This command line flag enables NoStatePrefetch on Isolated Prerenders.
+extern const char kIsolatedPrerenderEnableNSPCmdLineFlag[];
+
 // Returns true if the Isolated Prerender feature is enabled.
 bool IsolatedPrerenderIsEnabled();
 
