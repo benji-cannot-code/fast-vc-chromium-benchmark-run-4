8f995c310131496197bd70a9c9b46e7465e4059e
rajendrant <rajendrant@chromium.org>
2020-06-24 04:56:39 +0000

    Fix crash due to empty android activity
    Bug: 1098423
    Change-Id: Ib34f10604513f119544af3a4db243ac76af292e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2262256
    Commit-Queue: rajendrant <rajendrant@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781704}

--- chrome/browser/previews/android/previews_android_bridge.cc
+++ chrome/browser/previews/android/previews_android_bridge.cc
@@ -23,7 +23,7 @@ static jlong JNI_PreviewsAndroidBridge_Init(
 }
 
 // static
-void PreviewsAndroidBridge::CreateHttpsImageCompressionInfoBar(
+bool PreviewsAndroidBridge::CreateHttpsImageCompressionInfoBar(
     content::WebContents* web_contents) {
   TabAndroid* tab_android = TabAndroid::FromWebContents(web_contents);
   DCHECK(tab_android);
@@ -32,7 +32,7 @@ void PreviewsAndroidBridge::CreateHttpsImageCompressionInfoBar(
       tab_android->GetJavaObject();
   DCHECK(!j_tab_android.is_null());
 
-  Java_PreviewsAndroidBridge_createHttpsImageCompressionInfoBar(
+  return Java_PreviewsAndroidBridge_createHttpsImageCompressionInfoBar(
       base::android::AttachCurrentThread(), j_tab_android);
 }
 
