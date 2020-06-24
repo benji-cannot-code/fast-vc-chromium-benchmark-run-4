433e8c8beda22761e05a959d4f808a22db14ba05
Lei Zhang <thestig@chromium.org>
2020-06-24 07:55:36 +0000

    Properly include <string>.
    <string>, not "string".
    
    Change-Id: I16343119e3432d1fa307462e208b8c887c425650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261794
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781760}

--- chrome/browser/extensions/media_router_extension_access_logger_impl.cc
+++ chrome/browser/extensions/media_router_extension_access_logger_impl.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/media_router_extension_access_logger_impl.h"
 
-#include "string"
+#include <string>
 
 #include "base/bind.h"
 #include "base/check.h"
--- components/autofill/content/renderer/focus_test_utils.h
+++ components/autofill/content/renderer/focus_test_utils.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CONTENT_RENDERER_FOCUS_TEST_UTILS_H_
 #define COMPONENTS_AUTOFILL_CONTENT_RENDERER_FOCUS_TEST_UTILS_H_
 
-#include "base/callback.h"
-#include "string"
+#include <string>
 
+#include "base/callback.h"
 #include "third_party/blink/public/web/web_document.h"
 
 namespace autofill {
