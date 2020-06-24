0d7902ba1fb0593cf3354f2c065465e29d36e4a2
Robert Ogden <robertogden@chromium.org>
2020-06-24 04:54:21 +0000

    IsolatedPrerender Correctly Handles Cookies
    IsolatedPrerender handles cookies in the following way:
    * Cookies which are set on the prefetched mainframe are kept in the
      isolated NetworkContext's cookie jar.
    * Subresources fetched during NoStatePrefetch use the isolated
      NetworkContext's cookie jar and so send cookies as appropriate.
    * When the user navigates to a link that was prefetched, any cookies
      for that page are copied over to the normal profile.
    * Subresources that were not prefetched before the click use the
      prefetch cookie when they are made after the click with the normal
      profile.
    
    Note that copying the cookies over to the normal profile is a multi-
    step process during which the prefetched mainframe is blocked from
    committing to ensure page correctness.
    
    Important: Opaque IsolationInfo's block all cookies so this CL also
    transitions IsolatedPrerenders to a normal navigation-style NIK and
    IsolationInfo. This is ok because all prefetches are occuring in a
    separate NetworkContext from the rest of the browser and therefore
    were never at risk of having shared resources.
    
    Bug: 1096765
    Change-Id: I090fc57d46936b4972bd799eab3e4b022019bbc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2253200
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781703}

--- chrome/browser/prerender/isolated/isolated_prerender_url_loader_interceptor.h
+++ chrome/browser/prerender/isolated/isolated_prerender_url_loader_interceptor.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback_forward.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
 #include "base/optional.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
@@ -45,6 +46,12 @@ class IsolatedPrerenderURLLoaderInterceptor
   GetPrefetchedResponse(const GURL& url);
 
  private:
+  // Ensures the cookies from the mainframe have been copied to the normal
+  // profile before calling |InterceptPrefetchedNavigation|.
+  void EnsureCookiesCopiedAndInterceptPrefetchedNavigation(
+      const network::ResourceRequest& tentative_resource_request,
+      std::unique_ptr<PrefetchedMainframeResponseContainer> prefetch);
+
   void InterceptPrefetchedNavigation(
       const network::ResourceRequest& tentative_resource_request,
       std::unique_ptr<PrefetchedMainframeResponseContainer>);
@@ -84,10 +91,17 @@ class IsolatedPrerenderURLLoaderInterceptor
   // null. Used to calculate probe latency which is reported to the tab helper.
   base::Optional<base::TimeTicks> probe_start_time_;
 
+  // The time when we started waiting for cookies to be copied, delaying the
+  // navigation. Used to calculate total cookie wait time.
+  base::Optional<base::TimeTicks> cookie_copy_start_time_;
+
   // Set in |MaybeCreateLoader| and used in |On[DoNot]InterceptRequest|.
   content::URLLoaderRequestInterceptor::LoaderCallback loader_callback_;
 
   SEQUENCE_CHECKER(sequence_checker_);
+
+  base::WeakPtrFactory<IsolatedPrerenderURLLoaderInterceptor> weak_factory_{
+      this};
 
   DISALLOW_COPY_AND_ASSIGN(IsolatedPrerenderURLLoaderInterceptor);
 };
