62e633fd0bdfd42fd64240c168aa6d458a0401af
Peter Kasting <pkasting@chromium.org>
2020-06-24 00:21:46 +0000

    Clean up linting directives after recent linter changes.
    runtime/references is no longer checked by default; remove NOLINT
    annotations.
    
    presubmit_canned_checks.py now handles [] as callers intend; remove
    "no-op fake filter" arguments.
    
    Bug: none
    Change-Id: I7767c1b2bc45a07c0ce78b9855f085668ea7efa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248883
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781624}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -87,11 +87,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VISIT_REP(field) \
   visitor.Visit(proto, #field, proto.field());
 
-// NOLINT(runtime/references) is necessary to avoid a presubmit warning about
-// V& not being const.
 #define VISIT_PROTO_FIELDS(proto) \
   template <class V>              \
-  void VisitProtoFields(V& visitor, proto)  // NOLINT(runtime/references)
+  void VisitProtoFields(V& visitor, proto)
 
 namespace syncer {
 
