0a4debce7385d5d90d225c202e9af1b9df92c354
Mustafa Emre Acer <meacer@chromium.org>
2020-06-24 04:18:04 +0000

    Add unit tests for the lookalike URL throttle
    Bug: 1095120
    Change-Id: Idb462875093168546ceb11a2566bd784e37f9d62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258900
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781698}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle.h
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle.h
@@ -52,6 +52,10 @@ class LookalikeUrlNavigationThrottle : public content::NavigationThrottle {
   static std::unique_ptr<LookalikeUrlNavigationThrottle>
   MaybeCreateNavigationThrottle(content::NavigationHandle* navigation_handle);
 
+  // The throttle normally ignores testing profiles and returns PROCEED. This
+  // function forces unit tests to not ignore them .
+  void SetUseTestProfileForTesting() { use_test_profile_ = true; }
+
  private:
   // Checks whether the navigation to |url| can proceed. If
   // |check_safe_redirect| is true, will check if a safe redirect led to |url|.
@@ -78,6 +82,7 @@ class LookalikeUrlNavigationThrottle : public content::NavigationThrottle {
                                        LookalikeUrlMatchType match_type);
 
   Profile* profile_;
+  bool use_test_profile_ = false;
   base::WeakPtrFactory<LookalikeUrlNavigationThrottle> weak_factory_{this};
 };
 
--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
@@ -5,6 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/lookalikes/lookalike_url_navigation_throttle.h"
 
+#include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
+#include "components/lookalikes/core/features.h"
+#include "content/public/test/mock_navigation_handle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace lookalikes {
@@ -75,6 +80,48 @@ TEST(LookalikeUrlNavigationThrottleTest, IsSafeRedirect) {
   EXPECT_FALSE(IsSafeRedirect("example.com", {GURL("http://éxample.com/path"),
                                               GURL("http://intermediate.com/p"),
                                               GURL("http://éxample.com/dir")}));
+}
+
+class LookalikeThrottleTest : public ChromeRenderViewHostTestHarness {};
+
+// Tests that spoofy hostnames are properly handled in the throttle.
+TEST_F(LookalikeThrottleTest, SpoofsBlocked) {
+  base::HistogramTester test;
+
+  const struct TestCase {
+    const char* hostname;
+    bool expected_blocked;
+  } kTestCases[] = {
+      {"private.hostname", false},
+      {"example·com.com", true},
+      {"🍕.com", true},
+  };
+
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(
+      lookalikes::features::kLookalikeInterstitialForPunycode);
+
+  for (const TestCase& test_case : kTestCases) {
+    GURL url(std::string("http://") + test_case.hostname);
+    content::MockNavigationHandle handle(url, main_rfh());
+    handle.set_page_transition(ui::PAGE_TRANSITION_TYPED);
+
+    auto throttle =
+        LookalikeUrlNavigationThrottle::MaybeCreateNavigationThrottle(&handle);
+    ASSERT_TRUE(throttle);
+    throttle->SetUseTestProfileForTesting();
+
+    EXPECT_EQ(content::NavigationThrottle::PROCEED,
+              throttle->WillStartRequest().action());
+
+    if (test_case.expected_blocked) {
+      EXPECT_EQ(content::NavigationThrottle::CANCEL,
+                throttle->WillProcessResponse().action());
+    } else {
+      EXPECT_EQ(content::NavigationThrottle::PROCEED,
+                throttle->WillProcessResponse().action());
+    }
+  }
 }
 
 }  // namespace lookalikes
