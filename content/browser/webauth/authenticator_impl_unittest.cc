be3b68d8bb6bd85b1bfb5f98a85d808f9eed7305
Nigel Tao <nigeltao@chromium.org>
2020-06-24 07:12:27 +0000

    Reland "Move JSONParser-specific enums out of JSONReader"
    This is a reland of ce50ad19ef6c47d6f2832ad07963bd313cfe078f
    
    Original change's description:
    > Move JSONParser-specific enums out of JSONReader
    > 
    > JSONReader is the API. JSONParser is *an* implementation, and we would
    > like to experiment (in future commits) with different implementations
    > (crbug.com/1069271). Doing so is easier if they don't also have to match
    > exact error code values for invalid JSON input. In practice, API callers
    > don't care about the error code value (if less than 1000).
    > 
    > Also swap the order of the JSON_SYNTAX_ERROR and JSON_INVALID_ESCAPE
    > enum values so that JSON_SYNTAX_ERROR takes the enum value 1, the same
    > as the new kErrorCodeInvalidFormat enum value.
    > 
    > Also update a switch statement in
    > components/policy/core/common/config_dir_policy_loader.cc to also catch
    > JSON_TOO_LARGE, JSON_UNREPRESENTABLE_NUMBER and any future enums added.
    > 
    > Bug: 1069271
    > Bug: 1070409
    > Change-Id: I786c5d7ffc77e4d7927614a133bf1e7aae7eda5d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251561
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Reviewed-by: Robert Sesek <rsesek@chromium.org>
    > Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#780827}
    
    Bug: 1069271, 1070409
    Change-Id: Id9df75be88bdedb664fa7c539160bb0fa3922efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2257658
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781744}

--- content/browser/webauth/authenticator_impl_unittest.cc
+++ content/browser/webauth/authenticator_impl_unittest.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/compiler_specific.h"
-#include "base/json/json_parser.h"
+#include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/run_loop.h"
 #include "base/stl_util.h"
--- ios/web/navigation/wk_navigation_util_unittest.mm
+++ ios/web/navigation/wk_navigation_util_unittest.mm
@@ -140,7 +140,8 @@ TEST_F(WKNavigationUtilTest, CreateRestoreSessionUrlForLargeSession) {
   // Extract session JSON from restoration URL.
   base::JSONReader::ValueWithError value_with_error =
       ExtractSessionDict(restore_session_url);
-  ASSERT_EQ(base::JSONReader::JSON_NO_ERROR, value_with_error.error_code);
+  ASSERT_EQ(base::ValueDeserializer::kErrorCodeNoError,
+            value_with_error.error_code);
   ASSERT_TRUE(value_with_error.value.has_value());
 
   // Verify that all titles and URLs are present.
@@ -175,7 +176,8 @@ TEST_F(WKNavigationUtilTest, CreateRestoreSessionUrlForExtraLargeForwardList) {
   // Extract session JSON from restoration URL.
   base::JSONReader::ValueWithError value_with_error =
       ExtractSessionDict(restore_session_url);
-  ASSERT_EQ(base::JSONReader::JSON_NO_ERROR, value_with_error.error_code);
+  ASSERT_EQ(base::ValueDeserializer::kErrorCodeNoError,
+            value_with_error.error_code);
   ASSERT_TRUE(value_with_error.value.has_value());
 
   // Verify that first kMaxSessionSize titles and URLs are present.
@@ -220,7 +222,8 @@ TEST_F(WKNavigationUtilTest, CreateRestoreSessionUrlForExtraLargeBackList) {
   // Extract session JSON from restoration URL.
   base::JSONReader::ValueWithError value_with_error =
       ExtractSessionDict(restore_session_url);
-  ASSERT_EQ(base::JSONReader::JSON_NO_ERROR, value_with_error.error_code);
+  ASSERT_EQ(base::ValueDeserializer::kErrorCodeNoError,
+            value_with_error.error_code);
   ASSERT_TRUE(value_with_error.value.has_value());
 
   // Verify that last kMaxSessionSize titles and URLs are present.
@@ -266,7 +269,8 @@ TEST_F(WKNavigationUtilTest,
   // Extract session JSON from restoration URL.
   base::JSONReader::ValueWithError value_with_error =
       ExtractSessionDict(restore_session_url);
-  ASSERT_EQ(base::JSONReader::JSON_NO_ERROR, value_with_error.error_code);
+  ASSERT_EQ(base::ValueDeserializer::kErrorCodeNoError,
+            value_with_error.error_code);
   ASSERT_TRUE(value_with_error.value.has_value());
 
   // Verify that last kMaxSessionSize titles and URLs are present.
