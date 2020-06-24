e1a5b66681762743a2d70f1440847784018faf06
Anastasia Helfinstein <anastasi@google.com>
2020-06-24 01:26:42 +0000

    [Switch Access] Improve handling of repeated events
    Reduce lag of several seconds following actions such as resizing a
    window by only responding to the most recent AutomationEvent.
    
    AX-Relnotes: n/a.
    Bug: 1095232
    Change-Id: Ib797fd27415f937a550b6edf5dd0371ea3859fdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2255673
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781651}

---
+++ chrome/browser/resources/chromeos/accessibility/common/repeated_event_handler_test.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Include test fixture.
+GEN_INCLUDE([
+  '../chromevox/testing/chromevox_next_e2e_test_base.js',
+  'repeated_event_handler.js'
+]);
+
+/** Test fixture for array_util.js. */
+RepeatedEventHandlerTest = class extends ChromeVoxNextE2ETest {};
+
+TEST_F('RepeatedEventHandlerTest', 'RepeatedEventHandledOnce', function() {
+  this.runWithLoadedTree('', (root) => {
+    this.handlerCallCount = 0;
+    const handler = () => this.handlerCallCount++;
+
+    const repeatedHandler = new RepeatedEventHandler(root, 'focus', handler);
+
+    // Simulate events being fired.
+    repeatedHandler.onEvent_();
+    repeatedHandler.onEvent_();
+    repeatedHandler.onEvent_();
+    repeatedHandler.onEvent_();
+    repeatedHandler.onEvent_();
+
+    // Yield before verify how many times the handler was called.
+    setTimeout(() => assertEquals(this.handlerCallCount, 1), 0);
+  });
+});
