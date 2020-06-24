40ec6d5431b5d1c8115cf2de6a8ebe7680fbdd2c
Sigurd Schneider <sigurds@chromium.org>
2020-06-24 09:13:37 +0000

    Fix include to use shared mojo definitions
    The file ads_page_load_metrics_observer.cc included a
    mojo definition file from blink, instead of the shared
    version. This resulted in flaky build failures, as the
    blink version of the mojo is not a GN dependency, and
    was sometimes not yet generated.
    
    TBR=jkarlin@chromium.org
    
    Bug: chromium:1095617
    Change-Id: Id6930a4f7af08c6ff0344f853dea7b79ce2d3748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2262613
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#781777}

--- chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/ad_metrics/ads_page_load_metrics_observer.cc
@@ -45,7 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/metrics_utils.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
-#include "third_party/blink/public/mojom/devtools/inspector_issue.mojom-blink.h"
+#include "third_party/blink/public/mojom/devtools/inspector_issue.mojom-shared.h"
 #include "third_party/blink/public/mojom/web_feature/web_feature.mojom.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/gfx/geometry/size.h"
