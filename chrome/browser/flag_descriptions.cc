2103ed94e81f2381da9133121c409ca8abbf4a64
Mounir Lamouri <mlamouri@chromium.org>
2020-06-25 02:20:19 +0000

    Flags: remove enable-native-controls.
    The flag was technically removed but some crumbs stayed behind.
    
    Bug: None
    Change-Id: Iceff8d8a307761d3829cd59ee9144e85d1d7a4a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261692
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Auto-Submit: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782204}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -3652,11 +3652,6 @@ const char kEnableQuickAnswersTextAnnotatorName[] =
 const char kEnableQuickAnswersTextAnnotatorDescription[] =
     "Enable Quick Answers text annotator.";
 
-const char kEnableVideoPlayerNativeControlsName[] =
-    "Enable native controls in video player app";
-const char kEnableVideoPlayerNativeControlsDescription[] =
-    "Enable native controls in video player app";
-
 const char kTrimOnFreezeName[] = "Trim Working Set on freeze";
 const char kTrimOnFreezeDescription[] = "Trim Working Set on all frames frozen";
 
