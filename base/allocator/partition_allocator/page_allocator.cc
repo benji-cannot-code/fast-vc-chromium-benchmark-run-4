e8c8d3a3b87b03d4f1c4018e6a9fc9483e5acaab
Benoit Lize <lizeb@chromium.org>
2020-06-25 14:51:50 +0000

    base/allocator: Remove SpinLock from PageAllocator.
    Move away from custom locking in another place, towards base::Lock.
    
    Bug: 1061437
    Change-Id: Ib658ece96eb88adee289f3f39c64e83bfd451ee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2266478
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782431}

--- base/allocator/partition_allocator/page_allocator.cc
+++ base/allocator/partition_allocator/page_allocator.cc
@@ -12,11 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/allocator/partition_allocator/address_space_randomization.h"
 #include "base/allocator/partition_allocator/page_allocator_internal.h"
 #include "base/allocator/partition_allocator/partition_alloc_check.h"
-#include "base/allocator/partition_allocator/spin_lock.h"
 #include "base/bits.h"
 #include "base/check_op.h"
 #include "base/no_destructor.h"
 #include "base/numerics/checked_math.h"
+#include "base/synchronization/lock.h"
 #include "build/build_config.h"
 
 #if defined(OS_WIN)
@@ -38,14 +38,14 @@ namespace base {
 namespace {
 
 // We may reserve/release address space on different threads.
-subtle::SpinLock& GetReserveLock() {
-  static NoDestructor<subtle::SpinLock> s_reserveLock;
-  return *s_reserveLock;
+Lock& GetReserveLock() {
+  static NoDestructor<Lock> lock;
+  return *lock;
 }
 
 // We only support a single block of reserved address space.
-void* s_reservation_address = nullptr;
-size_t s_reservation_size = 0;
+void* s_reservation_address GUARDED_BY(GetReserveLock()) = nullptr;
+size_t s_reservation_size GUARDED_BY(GetReserveLock()) = 0;
 
 void* AllocPagesIncludingReserved(void* address,
                                   size_t length,
@@ -225,7 +225,7 @@ void DiscardSystemPages(void* address, size_t length) {
 
 bool ReserveAddressSpace(size_t size) {
   // To avoid deadlock, call only SystemAllocPages.
-  subtle::SpinLock::Guard guard(GetReserveLock());
+  AutoLock guard(GetReserveLock());
   if (s_reservation_address == nullptr) {
     void* mem = SystemAllocPages(nullptr, size, PageInaccessible,
                                  PageTag::kChromium, false);
@@ -243,7 +243,7 @@ bool ReserveAddressSpace(size_t size) {
 
 bool ReleaseReservation() {
   // To avoid deadlock, call only FreePages.
-  subtle::SpinLock::Guard guard(GetReserveLock());
+  AutoLock guard(GetReserveLock());
   if (!s_reservation_address)
     return false;
 
@@ -254,7 +254,7 @@ bool ReleaseReservation() {
 }
 
 bool HasReservationForTesting() {
-  subtle::SpinLock::Guard guard(GetReserveLock());
+  AutoLock guard(GetReserveLock());
   return s_reservation_address != nullptr;
 }
 
