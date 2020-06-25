f013ef12c358efab7fc2e65b60bbdd3a020c9af1
Himanshu Jaju <himanshujaju@chromium.org>
2020-06-25 22:28:15 +0000

    Tachyon in Chromium.
    Implements the HTTP APIs for Tachyon Express to send and receive
    messages between devices. Tachyon would be used as the signaling
    channel for webrtc messages in Chrome Sharing. The actual mojo
    implementation and tying it to the utility process is upcoming in
    the next CL.
    
    Bug: 1082305
    Change-Id: I62e1c3a2deb8a6ef376e474b5ce353a3cb863232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250061
    Commit-Queue: Himanshu Jaju <himanshujaju@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Alex Chau <alexchau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782712}

---
+++ chrome/browser/nearby_sharing/tachyon/fake_token_fetcher.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NEARBY_SHARING_TACHYON_FAKE_TOKEN_FETCHER_H_
+#define CHROME_BROWSER_NEARBY_SHARING_TACHYON_FAKE_TOKEN_FETCHER_H_
+
+#include <string>
+
+#include "chrome/browser/nearby_sharing/tachyon/token_fetcher.h"
+
+class FakeTokenFetcher : public TokenFetcher {
+ public:
+  FakeTokenFetcher();
+  ~FakeTokenFetcher() override;
+
+  void GetAccessToken(
+      base::OnceCallback<void(const std::string& token)> callback) override;
+  void SetAccessToken(const std::string& token);
+
+ private:
+  std::string token_;
+};
+
+#endif  // CHROME_BROWSER_NEARBY_SHARING_TACHYON_FAKE_TOKEN_FETCHER_H_
