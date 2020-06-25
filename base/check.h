9325f67141745d206984cb7164018b783e40e0c1
Hans Wennborg <hans@chromium.org>
2020-06-25 21:39:25 +0000

    Reland "[base] Stop including check.h, notreached.h, etc. in logging.h"
    This is a reland of f85481f892e03cd068e7f7543d76222c678b0bdb
    Fixes since the previous commit:
    https://chrome-internal-review.googlesource.com/c/chrome/deps/amd/+/3133484
    https://chrome-internal-review.googlesource.com/c/chrome/assistant/+/3133487
    https://chrome-internal-review.googlesource.com/c/chrome/assistant/+/3135083
    https://chromium-review.googlesource.com/c/chromium/src/+/2264371
    
    Original change's description:
    > [base] Stop including check.h, notreached.h, etc. in logging.h
    >
    > The CHECK, CHECK_EQ etc., and NOTREACHED macros have moved out
    > of logging.h into separate, much cheaper to include, headers.
    >
    > Now that the code has been updated to use the new headers, and
    > to not rergess on that, stop including them in logging.h.
    >
    > Bug: 1031540
    > Change-Id: Idfa891b991cbca1dfef93630c0f37b1b022f99df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264413
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Auto-Submit: Hans Wennborg <hans@chromium.org>
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#781970}
    
    TBR=thakis
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Bug: 1031540
    Change-Id: Ie889e031d229745f93363c7bb2605c4f65591f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264297
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782682}

--- base/check.h
+++ base/check.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/base_export.h"
 #include "base/compiler_specific.h"
+#include "base/dcheck_is_on.h"
 #include "base/immediate_crash.h"
 
 // This header defines the CHECK, DCHECK, and DPCHECK macros.
@@ -125,12 +126,6 @@ class BASE_EXPORT CheckError {
       ::logging::CheckError::PCheck(__FILE__, __LINE__, #condition).stream(), \
       !ANALYZER_ASSUME_TRUE(condition))
 
-#endif
-
-#if defined(NDEBUG) && !defined(DCHECK_ALWAYS_ON)
-#define DCHECK_IS_ON() false
-#else
-#define DCHECK_IS_ON() true
 #endif
 
 #if DCHECK_IS_ON()
