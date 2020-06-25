3e0c64236775c8cfb3cfe6b9c9100839fee27444
Regan Hsu <hsuregan@google.com>
2020-06-25 04:30:50 +0000

    [OsSettingsSearch] Highlight search field text on ctrl + F.
    Bug: 1096884
    Change-Id: I72cb4347b663f47c955a472bdd0222ad94dd7adc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265457
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782255}

--- chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.js
+++ chrome/browser/resources/settings/chromeos/os_settings_ui/os_settings_ui.js
@@ -323,6 +323,7 @@ cr.define('settings', function() {
         return false;
       }
       this.$$('os-toolbar').getSearchField().showAndFocus();
+      this.$$('os-toolbar').getSearchField().getSearchInput().select();
       return true;
     },
 
