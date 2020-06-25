5c06bcc1820ce5cb028c0f84ff4279003f7dc4cb
Rahul Singh (EDGE) <rahsin@microsoft.com>
2020-06-25 10:31:34 +0000

    DPWA: Update a PWA when Shortcuts Menu Icons content changes
    This CL adds code in ManifestUpdateTask to compare the on disk
    shortcuts_menu_icons_bitmaps with the ones downloaded from the app's
    manifest. When a change is detected, we update the PWA.
    
    Specifically, this code will help in cases where the icon size and url
    are the same but the content(SkBitmap) has changed.
    
    Testing:
    This CL includes a test case in manifest_manager_browsertest.cc to
    exercise this code.
    
    Bug: 1069298
    Change-Id: Iaa257495fd0d1985d9a519a6e012810898471efe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2262125
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782354}

--- chrome/browser/web_applications/manifest_update_task.cc
+++ chrome/browser/web_applications/manifest_update_task.cc
@@ -26,6 +26,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
+namespace {
+
+bool HaveIconContentsChanged(
+    const std::map<SquareSizePx, SkBitmap>& disk_icon_bitmaps,
+    const std::map<SquareSizePx, SkBitmap>& downloaded_icon_bitmaps) {
+  if (downloaded_icon_bitmaps.size() != disk_icon_bitmaps.size())
+    return true;
+
+  for (const std::pair<const SquareSizePx, SkBitmap>& entry :
+       downloaded_icon_bitmaps) {
+    SquareSizePx size = entry.first;
+    const SkBitmap& downloaded_bitmap = entry.second;
+
+    auto it = disk_icon_bitmaps.find(size);
+    if (it == disk_icon_bitmaps.end())
+      return true;
+
+    const SkBitmap& disk_bitmap = it->second;
+    if (!gfx::BitmapsAreEqual(downloaded_bitmap, disk_bitmap))
+      return true;
+  }
+
+  return false;
+}
+
+}  // namespace
+
 ManifestUpdateTask::ManifestUpdateTask(const GURL& url,
                                        const AppId& app_id,
                                        content::WebContents* web_contents,
@@ -209,7 +236,15 @@ void ManifestUpdateTask::OnAllIconsRead(
     return;
   }
 
-  DestroySelf(ManifestUpdateResult::kAppUpToDate);
+  if (base::FeatureList::IsEnabled(
+          features::kDesktopPWAsAppIconShortcutsMenu)) {
+    icon_manager_.ReadAllShortcutsMenuIcons(
+        app_id_,
+        base::BindOnce(&ManifestUpdateTask::OnAllShortcutsMenuIconsRead,
+                       AsWeakPtr()));
+  } else {
+    DestroySelf(ManifestUpdateResult::kAppUpToDate);
+  }
 }
 
 bool ManifestUpdateTask::IsUpdateNeededForIconContents(
@@ -217,20 +252,43 @@ bool ManifestUpdateTask::IsUpdateNeededForIconContents(
   DCHECK(web_application_info_.has_value());
   const std::map<SquareSizePx, SkBitmap>& downloaded_icon_bitmaps =
       web_application_info_->icon_bitmaps;
-  if (disk_icon_bitmaps.size() != disk_icon_bitmaps.size())
+  if (HaveIconContentsChanged(disk_icon_bitmaps, downloaded_icon_bitmaps))
     return true;
 
-  for (const std::pair<const SquareSizePx, SkBitmap>& entry :
-       downloaded_icon_bitmaps) {
-    SquareSizePx size = entry.first;
-    const SkBitmap& downloaded_bitmap = entry.second;
+  return false;
+}
 
-    auto it = disk_icon_bitmaps.find(size);
-    if (it == disk_icon_bitmaps.end())
-      return true;
+void ManifestUpdateTask::OnAllShortcutsMenuIconsRead(
+    ShortcutsMenuIconsBitmaps disk_shortcuts_menu_icons_bitmaps) {
+  DCHECK(stage_ == Stage::kPendingIconReadFromDisk);
 
-    const SkBitmap& disk_bitmap = it->second;
-    if (!gfx::BitmapsAreEqual(downloaded_bitmap, disk_bitmap))
+  DCHECK(web_application_info_.has_value());
+
+  if (IsUpdateNeededForShortcutsMenuIconsContents(
+          disk_shortcuts_menu_icons_bitmaps)) {
+    UpdateAfterWindowsClose();
+    return;
+  }
+
+  DestroySelf(ManifestUpdateResult::kAppUpToDate);
+}
+
+bool ManifestUpdateTask::IsUpdateNeededForShortcutsMenuIconsContents(
+    const ShortcutsMenuIconsBitmaps& disk_shortcuts_menu_icons_bitmaps) const {
+  DCHECK(web_application_info_.has_value());
+  const ShortcutsMenuIconsBitmaps& downloaded_shortcuts_menu_icons_bitmaps =
+      web_application_info_->shortcuts_menu_icons_bitmaps;
+  if (downloaded_shortcuts_menu_icons_bitmaps.size() !=
+      disk_shortcuts_menu_icons_bitmaps.size()) {
+    return true;
+  }
+
+  for (size_t i = 0; i < downloaded_shortcuts_menu_icons_bitmaps.size(); ++i) {
+    const std::map<SquareSizePx, SkBitmap>& downloaded_icon_bitmaps =
+        downloaded_shortcuts_menu_icons_bitmaps[i];
+    const std::map<SquareSizePx, SkBitmap>& disk_icon_bitmaps =
+        disk_shortcuts_menu_icons_bitmaps[i];
+    if (HaveIconContentsChanged(disk_icon_bitmaps, downloaded_icon_bitmaps))
       return true;
   }
 
