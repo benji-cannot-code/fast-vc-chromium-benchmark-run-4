cea2f8b3c92c8c1620f1d9f656c33538c5429d00
Sahel Sharify <sahel@chromium.org>
2020-06-25 21:44:23 +0000

    [Payment]Use count for refetching missing icons.
    This cl adds an UMA use counter to count the number of cases where
    Chrome crawls to refetch the missing icon of installed payment apps.
    The icon refetch logic will be removed once this count drops to zero.
    
    Bug: 1069010
    Change-Id: I59260337c2743bff3c1197f2369f78f083962f14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265137
    Commit-Queue: Sahel Sharify <sahel@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782688}

--- components/payments/content/installable_payment_app_crawler.cc
+++ components/payments/content/installable_payment_app_crawler.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/payments/content/icon/icon_size.h"
@@ -84,6 +85,7 @@ void InstallablePaymentAppCrawler::Start(
   } else {
     // Crawl to refetch missing icons of already installed apps.
     crawling_mode_ = CrawlingMode::kMissingIconRefetch;
+    UMA_HISTOGRAM_BOOLEAN("PaymentRequest.RefetchIconForInstalledApp", true);
     method_manifest_urls_for_icon_refetch_ =
         std::move(method_manifest_urls_for_icon_refetch);
     for (const auto& method : method_manifest_urls_for_icon_refetch_) {
