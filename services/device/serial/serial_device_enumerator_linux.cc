9f4553f9c1373d770377981486dfdb3cbf11bdc9
Reilly Grant <reillyg@chromium.org>
2020-06-25 00:31:52 +0000

    serial: Implement persistent permissions on Linux and macOS
    This change updates the serial port enumerators for Linux and macOS to
    read the serial number (if available) for USB devices and use this to
    construct a persistent ID for these devices.
    
    This means that only USB devices will have permissions stored
    persistently. Support for remembering other types of devices will be
    investigated in the future.
    
    Bug: 908836
    Change-Id: I28c53b880e69723447c445d447d677f2799c2ebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265357
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782158}

--- services/device/serial/serial_device_enumerator_linux.cc
+++ services/device/serial/serial_device_enumerator_linux.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "device/udev_linux/udev.h"
 
@@ -167,6 +168,8 @@ void SerialDeviceEnumeratorLinux::CreatePort(ScopedUdevDevicePtr device,
       udev_device_get_property_value(device.get(), "ID_PRODUCT_ID");
   const char* product_name_enc =
       udev_device_get_property_value(device.get(), "ID_MODEL_ENC");
+  const char* serial_number =
+      udev_device_get_property_value(device.get(), "ID_SERIAL_SHORT");
 
   uint32_t int_value;
   if (vendor_id && base::HexStringToUInt(vendor_id, &int_value)) {
@@ -178,7 +181,12 @@ void SerialDeviceEnumeratorLinux::CreatePort(ScopedUdevDevicePtr device,
     info->has_product_id = true;
   }
   if (product_name_enc)
-    info->display_name.emplace(device::UdevDecodeString(product_name_enc));
+    info->display_name = device::UdevDecodeString(product_name_enc);
+
+  if (info->has_vendor_id && info->has_product_id && serial_number) {
+    info->persistent_id = base::StringPrintf("%04X-%04X-%s", info->vendor_id,
+                                             info->product_id, serial_number);
+  }
 
   paths_.insert(std::make_pair(syspath, token));
   AddPort(std::move(info));
