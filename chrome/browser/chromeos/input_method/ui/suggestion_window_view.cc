c459e47fa03007d2a2cc2da65f6d26e4fa457aa9
My Nguyen <myy@chromium.org>
2020-06-25 07:53:19 +0000

    Reland "Add preference for personal info suggester showing setting link"
    This reverts commit ac4792b3abe93d0ba4741e073d240821a7f20419.
    
    Reason for revert: Struct fixed to avoid uninitialized value. 
    
    Original change's description:
    > Revert "Add preference for personal info suggester showing setting link"
    > 
    > This reverts commit bd23dacef6567842939178afd0722a0085c1e876.
    > 
    > Reason for revert: in order to resolve merge conflict for https://chromium-review.googlesource.com/c/chromium/src/+/2250107
    > 
    > Original change's description:
    > > Add preference for personal info suggester showing setting link
    > > 
    > > Should stop showing setting link after showing 10 times or when user
    > > have accepted it.
    > > 
    > > See: http://go/personal-info-setting-link
    > > 
    > > Bug: 1094267
    > > Change-Id: I9d9c5f09aa77f56445722c5fd626a8caaa04da7d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2245583
    > > Commit-Queue: My Nguyen <myy@chromium.org>
    > > Reviewed-by: Jing Wang <jiwan@chromium.org>
    > > Reviewed-by: Darren Shen <shend@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#779602}
    > 
    > TBR=shend@chromium.org,myy@chromium.org,jiwan@chromium.org
    > 
    > Change-Id: If873508473e418a2c9fd93d2b31fd47d05aa902b
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1094267
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2250253
    > Reviewed-by: Maxim Kolosovskiy <kolos@chromium.org>
    > Commit-Queue: Maxim Kolosovskiy <kolos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#779741}
    
    TBR=kolos@chromium.org,shend@chromium.org,myy@chromium.org,jiwan@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1094267
    Change-Id: Iffe0dff0edf73e240cd3e476729c29f1465fe933
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2262072
    Commit-Queue: My Nguyen <myy@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782316}

--- chrome/browser/chromeos/input_method/ui/suggestion_window_view.cc
+++ chrome/browser/chromeos/input_method/ui/suggestion_window_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/input_method/ui/assistive_delegate.h"
 #include "chrome/browser/chromeos/input_method/ui/border_factory.h"
+#include "chrome/browser/chromeos/input_method/ui/suggestion_details.h"
 #include "chrome/browser/chromeos/input_method/ui/suggestion_view.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
@@ -120,9 +121,11 @@ void SuggestionWindowView::Show(const SuggestionDetails& details) {
   MaybeInitializeSuggestionViews(1);
   candidate_views_[0]->SetEnabled(true);
   candidate_views_[0]->SetView(details);
-  candidate_views_[0]->SetMinWidth(
-      setting_link_view_->GetPreferredSize().width());
-  setting_link_view_->SetVisible(true);
+  if (details.show_setting_link) {
+    candidate_views_[0]->SetMinWidth(
+        setting_link_view_->GetPreferredSize().width());
+  }
+  setting_link_view_->SetVisible(details.show_setting_link);
   MakeVisible();
 }
 
