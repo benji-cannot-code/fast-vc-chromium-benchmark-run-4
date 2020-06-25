b34d058bc6056d9b799e125480d0bef9e588da24
Alexandre Courbot <acourbot@chromium.org>
2020-06-25 02:32:10 +0000

    media/gpu/v4l2vd: rename v4l2_slice_video_decoder to v4l2_video_decoder
    The new VD-based video decoder was initially built to support stateless
    (slice-based) decoders, but it is now able to handle both stateless and
    stateful ones now. Rename its source files to a shorter and more
    accurate name.
    
    BUG=b:149663704
    TEST=VDAtest --use_vd runs on Hana-kernelnext.
    
    Change-Id: Ief2250ee88d7c64a5b98b3dd57cef61e861a9eb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2262752
    Commit-Queue: Alexandre Courbot <acourbot@chromium.org>
    Reviewed-by: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: David Staessens <dstaessens@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782207}

--- media/gpu/chromeos/chromeos_video_decoder_factory.cc
+++ media/gpu/chromeos/chromeos_video_decoder_factory.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(USE_V4L2_CODEC)
-#include "media/gpu/v4l2/v4l2_slice_video_decoder.h"
+#include "media/gpu/v4l2/v4l2_video_decoder.h"
 #endif
 
 namespace media {
--- media/gpu/v4l2/v4l2_video_decoder.h
+++ media/gpu/v4l2/v4l2_video_decoder.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_GPU_V4L2_V4L2_SLICE_VIDEO_DECODER_H_
-#define MEDIA_GPU_V4L2_V4L2_SLICE_VIDEO_DECODER_H_
+#ifndef MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_H_
+#define MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_H_
 
 #include <linux/videodev2.h>
 
@@ -174,4 +174,4 @@ class MEDIA_GPU_EXPORT V4L2SliceVideoDecoder
 
 }  // namespace media
 
-#endif  // MEDIA_GPU_V4L2_V4L2_SLICE_VIDEO_DECODER_H_
+#endif  // MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_H_
