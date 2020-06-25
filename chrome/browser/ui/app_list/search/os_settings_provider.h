ab4052850b1696dfa8fb493b053d3d4050d2623c
Jia <jiameng@chromium.org>
2020-06-25 04:10:30 +0000

    [cros-launcher-settings] Observe settings availability change
    This cl adds an observer to Searchhandler to observe changes in
    settings results availability, and refresh results when necessary.
    
    Settings team resolved a bug (crbug.com/1096867) in SearchHandler that
    refreshes stale results. This cl ensures we can pick up the changes in
    the Launcher too.
    
    Bug: 1068851
    Change-Id: Icb3cdc7726320f132f558d8076017b4de846ab82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265694
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782249}

--- chrome/browser/ui/app_list/search/os_settings_provider.h
+++ chrome/browser/ui/app_list/search/os_settings_provider.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/settings/chromeos/search/search.mojom.h"
 #include "components/services/app_service/public/cpp/app_registry_cache.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
 
 class Profile;
 
@@ -58,8 +60,10 @@ class OsSettingsResult : public ChromeSearchResult {
 
 // Provider results for OS settings based on a search query. No results are
 // provided for zero-state.
-class OsSettingsProvider : public SearchProvider,
-                           public apps::AppRegistryCache::Observer {
+class OsSettingsProvider
+    : public SearchProvider,
+      public apps::AppRegistryCache::Observer,
+      public chromeos::settings::mojom::SearchResultsObserver {
  public:
   explicit OsSettingsProvider(Profile* profile);
   ~OsSettingsProvider() override;
@@ -69,12 +73,16 @@ class OsSettingsProvider : public SearchProvider,
 
   // SearchProvider:
   void Start(const base::string16& query) override;
+  void ViewClosing() override;
   ash::AppListSearchResultType ResultType() override;
 
   // apps::AppRegistryCache::Observer:
   void OnAppUpdate(const apps::AppUpdate& update) override;
   void OnAppRegistryCacheWillBeDestroyed(
       apps::AppRegistryCache* cache) override;
+
+  // mojom::SearchResultsObserver:
+  void OnSearchResultAvailabilityChanged() override;
 
  private:
   void OnSearchReturned(
@@ -115,6 +123,11 @@ class OsSettingsProvider : public SearchProvider,
   chromeos::settings::SearchHandler* const search_handler_;
   apps::AppServiceProxy* app_service_proxy_;
   gfx::ImageSkia icon_;
+
+  // Last query. It is reset when view is closed.
+  base::string16 last_query_;
+  mojo::Receiver<chromeos::settings::mojom::SearchResultsObserver>
+      search_results_observer_receiver_{this};
 
   base::WeakPtrFactory<OsSettingsProvider> weak_factory_{this};
 };
