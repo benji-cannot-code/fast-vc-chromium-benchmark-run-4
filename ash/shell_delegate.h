69d96a8c5a8c11022ec12fe2d88f54436c659269
Sammie Quon <sammiequon@chromium.org>
2020-06-25 22:41:38 +0000

    Revert "wm: Add display settings shortcut in context menu."
    This reverts commit a84e8b6808a266d0182b0fee4aab035ec5156e19.
    
    Reason for revert: UI review thumbs down this change. We
    sent a rebuttal, but i will first revert before branch. If
    we get permission then its ok to punt to M86.
    
    Original change's description:
    > wm: Add display settings shortcut in context menu.
    > 
    > Details/mocks are linked in the linked bug.
    > 
    > Test: manual
    > Bug: 1095140
    > Change-Id: Ic8dc25d686355b80dc5f537eda67836e6c97f172
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2225978
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#778590}
    
    TBR=xiyuan@chromium.org,sammiequon@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 1095140
    Change-Id: I1d065a7d798e73a6151282d58d0843101d019ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268120
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782717}

--- ash/shell_delegate.h
+++ ash/shell_delegate.h
@@ -101,8 +101,6 @@ class ASH_EXPORT ShellDelegate {
   virtual media_session::mojom::MediaSessionService* GetMediaSessionService();
 
   virtual void OpenKeyboardShortcutHelpPage() const {}
-
-  virtual void OpenDisplaySettings() const {}
 };
 
 }  // namespace ash
