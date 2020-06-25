5744cd7827a5c1af4ed6290738131aa6ee0aba9e
Mike Wasserman <msw@chromium.org>
2020-06-25 18:00:49 +0000

    Window Placement: Add 'recommended' suffix for site settings ask label
    The original change missed this suffix from the original mocks.
      https://chromium-review.googlesource.com/c/chromium/src/+/2174969
    Caught in UX implementation review (not shipping in M85, so no rush!):
      https://docs.google.com/presentation/d/1iSdJI3MhMMOeXr9Qzp3a5NlgT-l0AXdUAyJKylA-KgE/edit?ts=5ee124cc#slide=id.g88508b5609_1_50
    
    Test with chrome://flags#enable-experimental-web-platform-features
    Also fix some other string descriptions.
    
    Bug: 897300
    Test: chrome://settings/content/windowPlacement shows suffix
    Change-Id: I21942ca99980df9dac033f1739d7ddfc802df47d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264465
    Auto-Submit: Michael Wasserman <msw@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782536}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -1922,6 +1922,8 @@ void AddSiteSettingsStrings(content::WebUIDataSource* html_source,
      IDS_SETTINGS_SITE_SETTINGS_WINDOW_PLACEMENT},
     {"siteSettingsWindowPlacementAsk",
      IDS_SETTINGS_SITE_SETTINGS_WINDOW_PLACEMENT_ASK},
+    {"siteSettingsWindowPlacementAskRecommended",
+     IDS_SETTINGS_SITE_SETTINGS_WINDOW_PLACEMENT_ASK_RECOMMENDED},
     {"siteSettingsWindowPlacementBlock",
      IDS_SETTINGS_SITE_SETTINGS_WINDOW_PLACEMENT_BLOCK},
   };
