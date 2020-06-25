e52e25f3c7ada404fa3386a45f5cece4037eb9e7
Kevin Babbitt <kbabbitt@microsoft.com>
2020-06-25 17:52:11 +0000

    Revert "Move StyleColor methods to .cc file"
    This reverts commit f2c7893f9c82aacc921817ab552bd4ba974e7523.
    
    Reason for revert: crbug.com/1096683
    
    Original change description:
    > Move StyleColor methods to .cc file
    >
    > Code cleanup in preparation for making system color keywords compute to
    > themselves.
    >
    > Bug: 1081945
    > Change-Id: I72921d005fcf1ddc31957cdf939b51a8fdfdb4e9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2247150
    > Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    > Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    > Cr-Commit-Position: refs/heads/master@{#778891}
    
    Bug: 1096683
    Change-Id: I2397ab2c95c724c1803b19042ca50c90fd950969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261360
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#782531}

--- third_party/blink/renderer/core/css/style_color.cc
+++ third_party/blink/renderer/core/css/style_color.cc
@@ -9,34 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-StyleColor::StyleColor() : color_keyword_(CSSValueID::kCurrentcolor) {}
-
-StyleColor::StyleColor(Color color)
-    : color_(color), color_keyword_(CSSValueID::kInvalid) {}
-
-StyleColor::StyleColor(CSSValueID keyword) : color_keyword_(keyword) {}
-
-StyleColor StyleColor::CurrentColor() {
-  return StyleColor();
-}
-
-bool StyleColor::IsCurrentColor() const {
-  return color_keyword_ == CSSValueID::kCurrentcolor;
-}
-
-Color StyleColor::GetColor() const {
-  DCHECK(!IsCurrentColor());
-  return color_;
-}
-
-Color StyleColor::Resolve(Color current_color) const {
-  return IsCurrentColor() ? current_color : color_;
-}
-
-bool StyleColor::HasAlpha() const {
-  return !IsCurrentColor() && color_.HasAlpha();
-}
-
 Color StyleColor::ColorFromKeyword(CSSValueID keyword,
                                    WebColorScheme color_scheme) {
   if (const char* value_name = getValueName(keyword)) {
@@ -78,16 +50,6 @@ bool StyleColor::IsColorKeyword(CSSValueID id) {
 bool StyleColor::IsSystemColor(CSSValueID id) {
   return (id >= CSSValueID::kActiveborder && id <= CSSValueID::kWindowtext) ||
          id == CSSValueID::kMenu;
-}
-
-bool operator==(const StyleColor& a, const StyleColor& b) {
-  if (a.IsCurrentColor() || b.IsCurrentColor())
-    return a.IsCurrentColor() && b.IsCurrentColor();
-  return a.GetColor() == b.GetColor();
-}
-
-bool operator!=(const StyleColor& a, const StyleColor& b) {
-  return !(a == b);
 }
 
 }  // namespace blink
