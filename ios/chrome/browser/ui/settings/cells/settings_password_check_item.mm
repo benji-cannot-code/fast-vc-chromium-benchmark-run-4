9ab4d867026aad8075047434708303dc476a4268
Viktor Semeniuk <vsemeniuk@google.com>
2020-06-25 06:44:43 +0000

    [iOS][Password Check] Password Check on Settings > Password
    This change adds new section to Passwords Table View Controller which
    is responsible for password check feature. It displays password check
    status and allow user to run the check.
    
    Bug: 1075494
    Change-Id: I00e4593bd4deb7ac4575480fd18a9fe91f6eb07d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246685
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782300}

--- ios/chrome/browser/ui/settings/cells/settings_password_check_item.mm
+++ ios/chrome/browser/ui/settings/cells/settings_password_check_item.mm
@@ -36,10 +36,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.indicatorHidden ? [cell hideActivityIndicator]
                          : [cell showActivityIndicator];
     cell.textLabel.textColor = UIColor.cr_labelColor;
+    cell.accessibilityTraits &= ~UIAccessibilityTraitNotEnabled;
   } else {
     [cell setIconImage:nil withTintColor:nil];
     [cell hideActivityIndicator];
     cell.textLabel.textColor = UIColor.cr_secondaryLabelColor;
+    cell.accessibilityTraits |= UIAccessibilityTraitNotEnabled;
   }
 }
 
