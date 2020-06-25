36d368284bae4ebefd6310dbee6e22dd9e51279a
Jason Chase <chasej@chromium.org>
2020-06-25 16:56:57 +0000

    Install origin trial bindings on correct global object
    The logic to install bindings for origin trial exposed features handled
    global objects incorrectly. This didn't matter unless a member was
    exposed *both* to Window and ServiceWorker.
    See crbug.com/908133#c7 for details.
    
    This CL adds type checks to make sure that the current global object
    matches the target type before installing.
    
    Bug: 908133
    Change-Id: I1538f602b022857d43db79d72d78abdff6468ab6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261408
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782492}

--- third_party/blink/web_tests/http/tests/origin_trials/webexposed/resources/cookiestore-origin-trial-interfaces-serviceworker-enabled.php
+++ third_party/blink/web_tests/http/tests/origin_trials/webexposed/resources/cookiestore-origin-trial-interfaces-serviceworker-enabled.php
@@ -19,6 +19,7 @@ test(t => {
 
 test(t => {
   assert_true('cookieStore' in self, 'cookieStore property exists on global');
+  assert_not_equals(self.cookieStore, undefined, 'cookieStore property can be accessed on self');
   assert_true('oncookiechange' in self,
               'oncookiechange property exists on global');
 }, 'Cookie Store API entry points in Origin-Trial enabled serviceworker.');
