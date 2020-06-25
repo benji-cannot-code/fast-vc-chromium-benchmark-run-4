ff1b7d2d09f38c3c516b0e119f3acdcb54b3048e
Rachel Wong <wrong@chromium.org>
2020-06-25 03:37:45 +0000

    [consent toggle] Add enterprise policy for Suggested Content.
    This change adds a policy that sets Suggested Content to default
    disabled for enterprise users.
    
    Bug: 1079169
    Change-Id: I994ff0efd9c5ff7605f0508fdf1c3556b45ecccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2255736
    Commit-Queue: Rachel Wong <wrong@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782234}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -1117,6 +1117,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kDeletePrintJobHistoryAllowed,
     prefs::kDeletePrintJobHistoryAllowed,
     base::Value::Type::BOOLEAN },
+  { key::kSuggestedContentEnabled,
+    chromeos::prefs::kSuggestedContentEnabled,
+    base::Value::Type::BOOLEAN },
 
 #else  // defined(OS_CHROMEOS)
   { key::kMetricsReportingEnabled,
