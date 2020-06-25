ef8a21d77034f653bc89f77b37cffdd5c6fda86e
Daniel Hosseinian <dhoss@chromium.org>
2020-06-25 02:09:52 +0000

    Read printer options and send them on Linux print jobs
    On some platforms, printing backends may have printer settings that
    are intended to be sent during print jobs, but those are currently
    dropped. Place those settings/options in the job settings in the
    |advanced_settings| field and send them in Linux print jobs. That way
    the lpoptions can be sent.
    
    Bug: 466717
    Change-Id: I9950cd352dad00897d8378df24d405c6b6341525
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2240282
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Daniel Hosseinian <dhoss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782198}

--- printing/print_settings.h
+++ printing/print_settings.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/optional.h"
 #include "base/strings/string16.h"
+#include "build/build_config.h"
 #include "printing/mojom/print.mojom.h"
 #include "printing/page_range.h"
 #include "printing/page_setup.h"
@@ -20,11 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 
-#if defined(OS_CHROMEOS)
+#if defined(OS_LINUX)
 #include <map>
 
 #include "base/values.h"
-#endif  // defined(OS_CHROMEOS)
+#endif  // defined(OS_LINUX)
 
 namespace printing {
 
@@ -69,9 +70,9 @@ class PRINTING_EXPORT PrintSettings {
     }
   };
 
-#if defined(OS_CHROMEOS)
+#if defined(OS_LINUX)
   using AdvancedSettings = std::map<std::string, base::Value>;
-#endif  // defined(OS_CHROMEOS)
+#endif  // defined(OS_LINUX)
 
   PrintSettings();
   ~PrintSettings();
@@ -206,6 +207,13 @@ class PRINTING_EXPORT PrintSettings {
     pages_per_sheet_ = pages_per_sheet;
   }
 
+#if defined(OS_LINUX)
+  AdvancedSettings& advanced_settings() { return advanced_settings_; }
+  const AdvancedSettings& advanced_settings() const {
+    return advanced_settings_;
+  }
+#endif  // defined(OS_LINUX)
+
 #if defined(OS_CHROMEOS)
   void set_send_user_info(bool send_user_info) {
     send_user_info_ = send_user_info;
@@ -217,11 +225,6 @@ class PRINTING_EXPORT PrintSettings {
 
   void set_pin_value(const std::string& pin_value) { pin_value_ = pin_value; }
   const std::string& pin_value() const { return pin_value_; }
-
-  AdvancedSettings& advanced_settings() { return advanced_settings_; }
-  const AdvancedSettings& advanced_settings() const {
-    return advanced_settings_;
-  }
 #endif  // defined(OS_CHROMEOS)
 
   // Cookie generator. It is used to initialize PrintedDocument with its
@@ -303,6 +306,11 @@ class PRINTING_EXPORT PrintSettings {
   // Number of pages per sheet.
   int pages_per_sheet_;
 
+#if defined(OS_LINUX)
+  // Advanced settings.
+  AdvancedSettings advanced_settings_;
+#endif  // defined(OS_LINUX)
+
 #if defined(OS_CHROMEOS)
   // Whether to send user info.
   bool send_user_info_;
@@ -312,9 +320,6 @@ class PRINTING_EXPORT PrintSettings {
 
   // PIN code entered by the user.
   std::string pin_value_;
-
-  // Advanced settings.
-  AdvancedSettings advanced_settings_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(PrintSettings);
