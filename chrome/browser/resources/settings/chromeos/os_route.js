c95c37b2a1b49c93cad6193eb57abea04adf5dec
Nnamdi Theodore Johnson-Kanu <tjohnsonkanu@google.com>
2020-06-25 01:14:39 +0000

    [CrOS settings] Re-enable bluetooth test, fix flakiness
    This CL re-enables Polymer3 bluetooth test that was disabled because it
    was flaky. It also fixes the issue that caused the flakiness.
    
    The test was disabled in this CL https://chromium-review.googlesource.com/c/chromium/src/+/2254139
    
    Added metrics_recorder, os_route, os_settings_routes to closure compile
    
    Fix: 1097021
    Change-Id: I5649cf8ff1aa5890f214dd38997611a070eecf53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261140
    Commit-Queue: Nnamdi Theodore Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782176}

--- chrome/browser/resources/settings/chromeos/os_route.js
+++ chrome/browser/resources/settings/chromeos/os_route.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // #import {OsSettingsRoutes} from './os_settings_routes.m.js'
 // #import {Route, Router} from '../router.m.js';
+// #import {loadTimeData} from 'chrome://resources/js/load_time_data.m.js';
 
 cr.define('settings', function() {
   /**
