fefa3bd61473269f18d1c12b243db2af4d517eae
Ella Ge <eirage@chromium.org>
2020-06-25 15:36:31 +0000

    Change TWA sendExtraCommand result flag to EXTRA_COMMAND_SUCCESS
    This CL change the TWA sendExtraCommand return bundle to include
    a "success" boolean, instead of using the command name as key.
    
    Bug: 1069506
    Change-Id: I660c646587f304a9e63f27ad860b6bbf49427acb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2267217
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782450}

--- chrome/test/android/test_trusted_web_activity/src/org/chromium/chrome/browser/browserservices/TestTrustedWebActivityService.java
+++ chrome/test/android/test_trusted_web_activity/src/org/chromium/chrome/browser/browserservices/TestTrustedWebActivityService.java
@@ -32,6 +32,7 @@ public class TestTrustedWebActivityService extends TrustedWebActivityService {
     private static final String LOCATION_ARG_ENABLE_HIGH_ACCURACY = "enableHighAccuracy";
     private static final String EXTRA_NEW_LOCATION_AVAILABLE_CALLBACK = "onNewLocationAvailable";
     private static final String EXTRA_NEW_LOCATION_ERROR_CALLBACK = "onNewLocationError";
+    private static final String EXTRA_COMMAND_SUCCESS = "success";
 
     private final TokenStore mTokenStore = new InMemoryStore();
 
@@ -83,7 +84,7 @@ public class TestTrustedWebActivityService extends TrustedWebActivityService {
                 } catch (RemoteException e) {
                 }
 
-                executionResult.putBoolean(commandName, true);
+                executionResult.putBoolean(EXTRA_COMMAND_SUCCESS, true);
                 break;
             }
             case START_LOCATION_COMMAND_NAME: {
@@ -100,15 +101,15 @@ public class TestTrustedWebActivityService extends TrustedWebActivityService {
                 } catch (RemoteException e) {
                 }
 
-                executionResult.putBoolean(commandName, true);
+                executionResult.putBoolean(EXTRA_COMMAND_SUCCESS, true);
                 break;
             }
             case STOP_LOCATION_COMMAND_NAME: {
-                executionResult.putBoolean(commandName, true);
+                executionResult.putBoolean(EXTRA_COMMAND_SUCCESS, true);
                 break;
             }
             default:
-                executionResult.putBoolean(commandName, false);
+                executionResult.putBoolean(EXTRA_COMMAND_SUCCESS, false);
         }
         return executionResult;
     }
