6631a615901e60308479a1c0539dd25652e23c8f
Scott Violet <sky@chromium.org>
2020-06-25 02:27:49 +0000

    weblayer: enable text-to-speech for weblayer
    This makes text-to-speech stop if the hosting WebContents is
    hidden, which differs from Clank (Clank stops when all activities
    are hidden). I think stopping when hidden is good for the current
    use case, but will need to be revisted at some point.
    
    BUG=1025626
    TEST=wpt tests should cover this
    
    Change-Id: I631da77676f7822583e379cf2e51d5534d8199cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264621
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782206}

--- weblayer/browser/content_browser_client_impl.cc
+++ weblayer/browser/content_browser_client_impl.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/tts_controller.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/service_names.mojom.h"
@@ -666,6 +667,12 @@ void ContentBrowserClientImpl::RenderProcessWillLaunch(
 scoped_refptr<content::QuotaPermissionContext>
 ContentBrowserClientImpl::CreateQuotaPermissionContext() {
   return base::MakeRefCounted<permissions::QuotaPermissionContextImpl>();
+}
+
+content::TtsPlatform* ContentBrowserClientImpl::GetTtsPlatform() {
+  // TODO(sky): figure out a better way to integrate this.
+  content::TtsController::GetInstance()->SetStopSpeakingWhenHidden(true);
+  return nullptr;
 }
 
 void ContentBrowserClientImpl::CreateFeatureListAndFieldTrials() {
--- weblayer/browser/content_browser_client_impl.h
+++ weblayer/browser/content_browser_client_impl.h
@@ -135,6 +135,7 @@ class ContentBrowserClientImpl : public content::ContentBrowserClient {
   content::SpeechRecognitionManagerDelegate*
   CreateSpeechRecognitionManagerDelegate() override;
   ukm::UkmService* GetUkmService() override;
+  content::TtsPlatform* GetTtsPlatform() override;
 
   void CreateFeatureListAndFieldTrials();
 
