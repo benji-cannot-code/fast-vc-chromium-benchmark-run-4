2193afde6fc4ae27dfcac29975644677dafac9ca
Christos Froussios <cfroussios@chromium.org>
2020-06-25 13:01:18 +0000

    [Sheriff] disable NavigationPredictorPreconnectClientBrowserTestWithSearch.PreconnectSearchWithFeature
    It is flaky (pass/fail) on Win ASan
    
    TBR=robertogden@chromium.org
    
    Bug: 1099204
    Change-Id: I096a68808403a8d8dda5f4d7f3aedd8560c24bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2266512
    Reviewed-by: Christos Froussios <cfroussios@chromium.org>
    Commit-Queue: Christos Froussios <cfroussios@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782388}

--- chrome/browser/navigation_predictor/navigation_predictor_preconnect_client_browsertest.cc
+++ chrome/browser/navigation_predictor/navigation_predictor_preconnect_client_browsertest.cc
@@ -171,7 +171,6 @@ class NavigationPredictorPreconnectClientBrowserTestWithUnusedIdleSocketTimeout
 IN_PROC_BROWSER_TEST_F(
     NavigationPredictorPreconnectClientBrowserTestWithUnusedIdleSocketTimeout,
     ActionAccuracy_timeout) {
-
   const GURL& url = GetTestURL("/page_with_same_host_anchor_element.html");
   ui_test_utils::NavigateToURL(browser(), url);
 
@@ -305,8 +304,13 @@ class NavigationPredictorPreconnectClientBrowserTestWithSearch
   base::test::ScopedFeatureList feature_list_;
 };
 
+#if defined(OS_WIN) && defined(ADDRESS_SANITIZER)
+#define MAYBE_PreconnectSearchWithFeature DISABLED_PreconnectSearchWithFeature
+#else
+#define MAYBE_PreconnectSearchWithFeature PreconnectSearchWithFeature
+#endif
 IN_PROC_BROWSER_TEST_F(NavigationPredictorPreconnectClientBrowserTestWithSearch,
-                       PreconnectSearchWithFeature) {
+                       MAYBE_PreconnectSearchWithFeature) {
   static const char kShortName[] = "test";
   static const char kSearchURL[] =
       "/anchors_different_area.html?q={searchTerms}";
