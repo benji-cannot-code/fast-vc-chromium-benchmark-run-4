9a37c2511ab4af2baada59ebb9b601751b77a5b9
Eugene But <eugenebut@chromium.org>
2020-06-25 16:49:07 +0000

    Disable OverlayPresentationContextImplTest.ContainedOverlayUI on iOS 12
    TBR: michaeldo@chromium.org
    Bug: 1099287
    Change-Id: Ib8f56b71b574b91f51099b7366d9e5778217854d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2267442
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782489}

--- ios/chrome/browser/ui/overlays/overlay_presentation_context_impl_unittest.mm
+++ ios/chrome/browser/ui/overlays/overlay_presentation_context_impl_unittest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/overlays/overlay_presentation_context_impl.h"
 
 #include "base/bind.h"
+#include "base/ios/ios_util.h"
 #import "base/test/ios/wait_util.h"
 #import "ios/chrome/browser/main/test_browser.h"
 #include "ios/chrome/browser/overlays/public/overlay_presentation_context_observer.h"
@@ -289,6 +290,11 @@ TEST_F(OverlayPresentationContextImplTest, CanShowRequest) {
 
 // Tests the presentation flow for contained overlay UI.
 TEST_F(OverlayPresentationContextImplTest, ContainedOverlayUI) {
+  if (!base::ios::IsRunningOnOrLater(13, 0, 0)) {
+    // TODO(crbug.com/1099287): Test is failing on iOS 12.4.
+    return;
+  }
+
   std::unique_ptr<OverlayRequest> request =
       OverlayRequest::CreateWithConfig<TestContainedOverlay>();
   context_.PrepareToShowOverlayUI(request.get());
