f973a30c07243a2e26408647e753c502da1a7c35
Jason Lin <lxj@google.com>
2020-06-25 05:24:27 +0000

    Set app icon on Plugin VM app window
    Bug: 1094924
    Change-Id: I4a657f9b3ea417e35f04ee53f0a7b92ceb152420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2248396
    Commit-Queue: Jason Lin <lxj@google.com>
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782275}

--- chrome/browser/ui/ash/launcher/app_service/app_service_app_window_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/app_service/app_service_app_window_launcher_controller.cc
@@ -32,8 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/grit/chrome_unscaled_resources.h"
 #include "components/account_id/account_id.h"
 #include "components/arc/arc_util.h"
+#include "components/exo/shell_surface_base.h"
 #include "components/services/app_service/public/cpp/instance.h"
 #include "components/services/app_service/public/mojom/types.mojom-shared.h"
 #include "components/services/app_service/public/mojom/types.mojom.h"
@@ -41,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/env.h"
 #include "ui/aura/window.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/image/image_skia.h"
 #include "ui/views/widget/widget.h"
 
 namespace {
@@ -480,6 +484,14 @@ void AppServiceAppWindowLauncherController::RegisterWindow(
     }
 
     AddWindowToShelf(window, shelf_id);
+
+    if (plugin_vm::IsPluginVmAppWindow(window)) {
+      // Set an icon for the Plugin VM app window.
+      static_cast<exo::ShellSurfaceBase*>(
+          views::Widget::GetWidgetForNativeWindow(window)->widget_delegate())
+          ->SetIcon(*ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+              IDR_LOGO_PLUGIN_VM_DEFAULT_32));
+    }
   }
 }
 
