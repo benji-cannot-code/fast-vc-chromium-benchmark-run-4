f1b8755a9532f084b28cf1547e9964534b358f33
Gauthier Ambard <gambard@chromium.org>
2020-06-25 07:32:26 +0000

    [iOS] Fix NTP animation when opening tab via keyboard
    This CL changes the order in which the BVC/the omnibox is being focused
    when opening a new tab from the TabGrid using keyboard shortcut.
    Previously the omnibox was focused then the BVC, leading to an
    animation glitch where the omnibox and the doodle disappeared.
    
    This CL fixes it by focusing first the BVC then runs the completion of
    the animation (which contains the focus of the omnibox), letting the
    animation follow its natural course.
    
    This should be relatively safe as the only completion block passed in
    the TabGrid animation is the focusing of the omnibox.
    So inverting the order of the calls should only have an effect in that
    case.
    
    The launchMask isn't used when BVC is contained.
    
    Fixed: 1096517
    Change-Id: I6a53e99e5c1a598401c668390c177e4bef2fc06a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264052
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782309}

--- ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_grid/tab_grid_coordinator.mm
@@ -363,6 +363,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ProceduralBlock extendedCompletion = ^{
     [self.tabSwitcher.delegate
         tabSwitcherDismissTransitionDidEnd:self.tabSwitcher];
+    if (base::FeatureList::IsEnabled(kContainedBVC)) {
+      [self.bvcContainer.currentBVC becomeFirstResponder];
+    }
     if (completion) {
       completion();
     }
@@ -384,8 +387,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                         toBrowser:self.bvcContainer
                                    withCompletion:^{
                                      extendedCompletion();
-                                     [self.bvcContainer
-                                             .currentBVC becomeFirstResponder];
                                    }];
 
   } else {
