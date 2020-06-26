46764e715ae5cb59ce5eeb3953ee979b706bbef0
Miguel Casas <mcasas@chromium.org>
2020-06-26 13:39:15 +0000

    ui/ozone/..drm/: commit gamma correction on next page flip
    DrmDisplay::SetColorSpace() ends up setting the de/gamma ramps instantly
    via HardwareDisplayPlaneManager(Atomic)::CommitGammaCorrection(). When
    transitioning SDR-HDR or viceversa, this causes a glitch because the
    de/gamma ramp is committed separately from the page flip containing the
    new content. This CL fixes that by adding a parameter |on_next_commit|
    to DrmDisplay::CommitGammaCorrection() and wiring it down to the
    appropriate HardwareDisplayPlaneManagerAtomic where, if set, the code
    stores the new gamma/degamma ramps and pushes them on the next Commit().
    
    Bug: b:158781255
    Change-Id: I89ad16f15d40f1c9674ce78f68400b74d506b00d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264771
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782951}

--- ui/ozone/platform/drm/gpu/hardware_display_plane_manager.cc
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_manager.cc
@@ -402,4 +402,9 @@ bool HardwareDisplayPlaneManager::InitializeCrtcState() {
   return true;
 }
 
+bool HardwareDisplayPlaneManager::CommitGammaCorrection(
+    const CrtcProperties& crtc_props) {
+  return true;
+}
+
 }  // namespace ui
