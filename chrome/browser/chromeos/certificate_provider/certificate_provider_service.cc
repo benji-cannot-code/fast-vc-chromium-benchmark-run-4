f4beb4a0f417421043c0c2617f23e6a469816481
Maksim Ivanov <emaxx@chromium.org>
2020-06-26 18:36:18 +0000

    Defer data hashing in cert provider service
    Refactor CertificateProviderService to defer the input data hashing till
    the very last point before sending the signature request to the
    extension. As a consequence, change the CryptohomeKeyDelegate D-Bus
    service to pass the unhashed data. Also kill the call in the
    CertStoreInstance Mojo service that was passing pre-hashed data (that
    service will anyway be deleted in one of follow-ups - see b/119914122).
    
    This is a preparation step for introducing a new version of the
    chrome.certificateProvider API that will pass the unhashed inputs to the
    extension.
    
    There's no use-visible changes expected. All changes, except the one of
    CertStoreInstance, are backwards-compatible, and the CertStoreInstance
    is an unlaunched functionality that will be deleted soon (see
    b/119914122).
    
    Bug: 1078761
    Change-Id: I656f46c41fdd5fd779e27abe76ee6c01ea2349f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246177
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783089}

--- chrome/browser/chromeos/certificate_provider/certificate_provider_service.cc
+++ chrome/browser/chromeos/certificate_provider/certificate_provider_service.cc
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_task_runner_handle.h"
 #include "chrome/browser/chromeos/certificate_provider/certificate_provider.h"
 #include "net/base/net_errors.h"
-#include "third_party/boringssl/src/include/openssl/digest.h"
-#include "third_party/boringssl/src/include/openssl/ssl.h"
 
 namespace chromeos {
 
@@ -185,25 +183,13 @@ void CertificateProviderService::SSLPrivateKey::Sign(
   // PostSignResult().
   callback = base::BindOnce(&PostSignResult, std::move(callback));
 
-  // The extension expects the input to be hashed ahead of time.
-  const EVP_MD* md = SSL_get_signature_algorithm_digest(algorithm);
-  uint8_t digest[EVP_MAX_MD_SIZE];
-  unsigned digest_len;
-  if (!md || !EVP_Digest(input.data(), input.size(), digest, &digest_len, md,
-                         nullptr)) {
-    std::move(callback).Run(net::ERR_SSL_CLIENT_AUTH_SIGNATURE_FAILED,
-                            /*signature=*/{});
-    return;
-  }
-
   if (!service_) {
     std::move(callback).Run(net::ERR_FAILED, /*signature=*/{});
     return;
   }
 
   service_->RequestSignatureFromExtension(
-      extension_id_, cert_info_.certificate, algorithm,
-      base::make_span(digest, digest_len),
+      extension_id_, cert_info_.certificate, algorithm, input,
       /*authenticating_user_account_id=*/{}, std::move(callback));
 }
 
@@ -332,7 +318,7 @@ void CertificateProviderService::OnExtensionUnloaded(
 void CertificateProviderService::RequestSignatureBySpki(
     const std::string& subject_public_key_info,
     uint16_t algorithm,
-    base::span<const uint8_t> digest,
+    base::span<const uint8_t> input,
     const base::Optional<AccountId>& authenticating_user_account_id,
     net::SSLPrivateKey::SignCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -348,7 +334,7 @@ void CertificateProviderService::RequestSignatureBySpki(
   }
 
   RequestSignatureFromExtension(extension_id, info.certificate, algorithm,
-                                digest, authenticating_user_account_id,
+                                input, authenticating_user_account_id,
                                 std::move(callback));
 }
 
@@ -455,7 +441,7 @@ void CertificateProviderService::RequestSignatureFromExtension(
     const std::string& extension_id,
     const scoped_refptr<net::X509Certificate>& certificate,
     uint16_t algorithm,
-    base::span<const uint8_t> digest,
+    base::span<const uint8_t> input,
     const base::Optional<AccountId>& authenticating_user_account_id,
     net::SSLPrivateKey::SignCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -471,7 +457,7 @@ void CertificateProviderService::RequestSignatureFromExtension(
   pin_dialog_manager_.AddSignRequestId(extension_id, sign_request_id,
                                        authenticating_user_account_id);
   if (!delegate_->DispatchSignRequestToExtension(
-          extension_id, sign_request_id, algorithm, certificate, digest)) {
+          extension_id, sign_request_id, algorithm, certificate, input)) {
     // TODO(crbug.com/1046860): Remove logging after stabilizing the feature.
     VLOG(1) << "Failed to dispatch signature request to extension "
             << extension_id << " id " << sign_request_id;
