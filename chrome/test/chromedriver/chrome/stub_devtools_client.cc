6980b839244de96644b1b0330bc2be9135cb4ae6
Lucía Cantú-Miller <cantumiller@google.com>
2020-06-26 23:12:07 +0000

    Chromedriver refactor ConnectIfNecessary
    DevToolsClientImpl::ConnectIfNecessary method has been refactored as two
    methods (ConnectIfNecessary & SetUpDevTools). This with the intention to
    separate responsibilities and make code cleaner.
    
    Bug: chromedriver:3454
    Change-Id: If6de5aecf614226638f0c2e5754764b118153e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268313
    Commit-Queue: Lucía Cantú-Miller <cantumiller@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783237}

--- chrome/test/chromedriver/chrome/stub_devtools_client.cc
+++ chrome/test/chromedriver/chrome/stub_devtools_client.cc
@@ -26,6 +26,10 @@ Status StubDevToolsClient::ConnectIfNecessary() {
   return Status(kOk);
 }
 
+Status StubDevToolsClient::SetUpDevTools() {
+  return Status(kOk);
+}
+
 Status StubDevToolsClient::SendCommand(
     const std::string& method,
     const base::DictionaryValue& params) {
