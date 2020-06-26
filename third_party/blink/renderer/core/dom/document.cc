0ce66e642da709c8a6098ac3748cab1d6a4af182
Nate Chapin <japhet@chromium.org>
2020-06-26 01:15:48 +0000

    Speculative fix for crbug.com/1099191
    In https://chromium-review.googlesource.com/c/chromium/src/+/2258426, I
    added an early-exit to address crashes in document.open(). It looks like we
    probably need the same early-exit in document.write().
    
    Two tests depend on the old behavior of invoking document.write() from
    a detached context. However, the new behavior matches firefox's behavior
    (though they thrown an exception), so I think these tests are asserting
    unnecessary behavior.
    
    Bug: 1099191
    Change-Id: Iad039d6a7203b4321602399c0cb0feed39b32d3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2267446
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782794}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -4281,6 +4281,9 @@ void Document::write(const String& text,
     return;
   }
 
+  if (entered_document && !entered_document->GetExecutionContext())
+    return;
+
   if (ignore_opens_and_writes_for_abort_)
     return;
 
