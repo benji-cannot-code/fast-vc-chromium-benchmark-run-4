940ebf49b68fbe673bed071d175a3543d862b8ea
Alice Wang <aliceywang@chromium.org>
2020-06-26 13:49:59 +0000

    [Android] Migrate to AccountManagerTestRule in signin integration tests
    This CL migrates the usage of SigninTestUtil in signin integration
    tests to AccountManagerTestRule.
    
    Bug: 1092474
    Change-Id: Ie21d8409ad9359a55919221c99adbba566cc0881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2267317
    Reviewed-by: Tanmoy Mollik <triploblastic@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Alice Wang <aliceywang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782958}

--- components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/util/FakeAccountManagerFacade.java
+++ components/signin/core/browser/android/javatests/src/org/chromium/components/signin/test/util/FakeAccountManagerFacade.java
@@ -94,7 +94,9 @@ public class FakeAccountManagerFacade implements AccountManagerFacade {
     }
 
     @Override
-    public void getGoogleAccounts(Callback<AccountManagerResult<List<Account>>> callback) {}
+    public void getGoogleAccounts(Callback<AccountManagerResult<List<Account>>> callback) {
+        callback.onResult(new AccountManagerResult<>(getGoogleAccounts()));
+    }
 
     @Override
     public boolean hasGoogleAccountAuthenticator() {
