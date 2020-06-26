1dcae3ca03e2b1b82c743ac9cf22cf5f147cd75b
Livvie Lin <livvielin@chromium.org>
2020-06-26 18:25:59 +0000

    [iOS] Call CancelAndDisplayError from ShouldAllowResponse
    For lookalike interstitials, call CancelAndDisplayError from
    ShouldAllowResponse rather than at the request stage, so that the
    provisional load has already started. This will allow PrepareErrorPage
    to trigger.
    
    This CL also puts a placeholder method in ChromeWebClient for preparing
    the lookalike interstitial HTML when it sees a lookalike error code.
    
    Bug: 1058898
    Change-Id: Iff830bb48cdee60cc1e2b8b5f74ebe858c2d5993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2261614
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Livvie Lin <livvielin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783085}

--- ios/chrome/browser/web/chrome_web_client.mm
+++ ios/chrome/browser/web/chrome_web_client.mm
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/web/error_page_util.h"
 #include "ios/chrome/browser/web/features.h"
 #import "ios/components/security_interstitials/ios_blocking_page_tab_helper.h"
+#import "ios/components/security_interstitials/lookalikes/lookalike_url_error.h"
 #include "ios/components/webui/web_ui_url_constants.h"
 #include "ios/public/provider/chrome/browser/browser_url_rewriter_provider.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
@@ -94,6 +95,13 @@ NSString* GetSafeBrowsingErrorPageHTML(web::WebState* web_state,
   security_interstitials::IOSBlockingPageTabHelper::FromWebState(web_state)
       ->AssociateBlockingPage(navigation_id, std::move(page));
 
+  return base::SysUTF8ToNSString(error_page_content);
+}
+
+// Returns the lookalike error page HTML.
+NSString* GetLookalikeErrorPageHTML(web::WebState* web_state,
+                                    int64_t navigation_id) {
+  std::string error_page_content = "lookalike error";
   return base::SysUTF8ToNSString(error_page_content);
 }
 
@@ -300,6 +308,11 @@ void ChromeWebClient::PrepareErrorPage(
     DCHECK_EQ(kUnsafeResourceErrorCode, final_underlying_error.code);
     std::move(error_html_callback)
         .Run(GetSafeBrowsingErrorPageHTML(web_state, navigation_id));
+  } else if ([final_underlying_error.domain isEqual:kLookalikeUrlErrorDomain]) {
+    // Only kLookalikeUrlErrorCode is supported.
+    DCHECK_EQ(kLookalikeUrlErrorCode, final_underlying_error.code);
+    std::move(error_html_callback)
+        .Run(GetLookalikeErrorPageHTML(web_state, navigation_id));
   } else if (info.has_value()) {
     base::OnceCallback<void(bool)> proceed_callback;
     base::OnceCallback<void(NSString*)> blocking_page_callback =
