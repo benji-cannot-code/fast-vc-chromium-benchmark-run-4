883c83e0a3b570f4f7f6eda4bc21f735addb524e
Lindsay Pasricha <lindsayw@chromium.org>
2020-06-26 15:53:34 +0000

    Revert "[iOS] Fix broken tests due to missing completion in fake ViewController."
    This reverts commit cf4e7dcbe4554f6f7ee07f6c9d4851e5ab7b4cd4.
    
    Reason for revert: AddAccountSigninManagerTests are failing again
    
    Original change's description:
    > [iOS] Fix broken tests due to missing completion in fake ViewController.
    > 
    > AddAccountSigninManagerTests were failing due to a missing run
    > completion. Remove intermediary method that causes this failure.
    > 
    > Bug: 1078815
    > Change-Id: I1766e593927cd40be318cf66bb67d11f5ed47006
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264057
    > Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#782928}
    
    TBR=jlebel@chromium.org,fernandex@chromium.org
    
    Change-Id: I13a4ea3529c35567c607eeb77ce9e9c690ac8cbe
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1078815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2269879
    Commit-Queue: Lindsay Pasricha <lindsayw@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783006}

--- ios/public/provider/chrome/browser/signin/fake_chrome_identity_interaction_manager.mm
+++ ios/public/provider/chrome/browser/signin/fake_chrome_identity_interaction_manager.mm
@@ -147,7 +147,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self runCompletionCallbackWithError:error];
     return;
   }
-  [_viewController
+  [self.delegate interactionManager:self
       dismissViewControllerAnimated:animated
                          completion:^{
                            [self runCompletionCallbackWithError:error];
