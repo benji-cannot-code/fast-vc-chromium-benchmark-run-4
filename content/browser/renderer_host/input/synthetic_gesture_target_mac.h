c8f7f8d18bf3929628af738c2a56d10e0bb4d131
Collin Baker <collinbaker@chromium.org>
2020-06-26 17:56:16 +0000

    Revert "Reland "Use wheelTick in forms/resources/common-wheel-event.js""
    This reverts commit 89c9ea26502232637b9e935098a885a3d6956178.
    
    Reason for revert: two builds after this flaked in ../month-suggestion-picker-mouse-operations.html and ../datetimelocal-suggestion-picker-mouse-operations.html:
    
    https://ci.chromium.org/p/chromium/builders/ci/Win7%20Tests%20%281%29
    
    Original change's description:
    > Reland "Use wheelTick in forms/resources/common-wheel-event.js"
    > 
    > After we finish the scroll unification, the scrolls happens mainly on
    > the compositor thread, and the scroll code in the main thread will be
    > removed. eventSender sends the scroll events to main thread, so it
    > would not work after the scroll unification. This CL we should replace
    > eventSender.mouseScrollBy with wheelTick which uses
    > gpuBenchmarking.smoothScrollByXY in
    > forms/resources/common-wheel-event.js.
    > 
    > TBR=ellyjones@chromium.org,avi@chromium.org,sky@chromium.org,bokan@chromium.org,lanwei@chromium.org
    > 
    > Bug: 1047176
    > Change-Id: Ib45dce450fe395d53a443612033305bccef6b19d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268227
    > Reviewed-by: Lan Wei <lanwei@chromium.org>
    > Commit-Queue: Lan Wei <lanwei@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#783007}
    
    TBR=ellyjones@chromium.org,avi@chromium.org,sky@chromium.org,bokan@chromium.org,lanwei@chromium.org
    
    Change-Id: Ia0617e5e297d571c6812af0a31e69b26688d37ba
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1047176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2269750
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783058}

--- content/browser/renderer_host/input/synthetic_gesture_target_mac.h
+++ content/browser/renderer_host/input/synthetic_gesture_target_mac.h
@@ -43,8 +43,6 @@ class SyntheticGestureTargetMac : public SyntheticGestureTargetBase {
 
  private:
   RenderWidgetHostViewMac* GetView() const;
-  bool PointIsWithinContents(RenderWidgetHostView* view,
-                             const gfx::PointF& point);
   RenderWidgetHostViewCocoa* cocoa_view_;
 
   DISALLOW_COPY_AND_ASSIGN(SyntheticGestureTargetMac);
