47d4aba90b87acfd6270327e331c0ec4216841e1
Steven Bingler <bingler@chromium.org>
2020-06-26 15:28:44 +0000

    Create an class so native code can query Java's EnterpriseInfo
    Create a native class that other classes can use to query ownership
    state of the Android device from the Java side EnterpriseInfo.
    
    This class provides a function that accepts a callback. When this
    function is called the callback is stored in a list and a request to
    the Java side is made. If any more requests are received they are stored
    in the list while we wait on the response from the Java side.
    
    Once the Java side response is received all callbacks stored in the list
    are run.
    
    Bug: 1085168
    Change-Id: I49429d13f9a407e3650fe265d4e010e7e169ee68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268657
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782997}

--- chrome/android/java/src/org/chromium/chrome/browser/policy/EnterpriseInfo.java
+++ chrome/android/java/src/org/chromium/chrome/browser/policy/EnterpriseInfo.java
@@ -15,6 +15,8 @@ import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.NativeMethods;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.task.AsyncTask;
 import org.chromium.base.task.TaskTraits;
@@ -44,6 +46,24 @@ public final class EnterpriseInfo {
             mDeviceOwned = isDeviceOwned;
             mProfileOwned = isProfileOwned;
         }
+    }
+
+    /**
+     * Returns, via callback, whether the device has a device owner or a profile owner for native.
+     */
+    @CalledByNative
+    public static void getManagedStateForNative() {
+        Callback<OwnedState> callback = (result) -> {
+            if (result == null) {
+                // Unable to determine the owned state, assume it's not owned.
+                EnterpriseInfoJni.get().updateNativeOwnedState(false, false);
+            }
+
+            EnterpriseInfoJni.get().updateNativeOwnedState(
+                    result.mDeviceOwned, result.mProfileOwned);
+        };
+
+        getDeviceEnterpriseInfo(callback);
     }
 
     /**
@@ -141,5 +161,10 @@ public final class EnterpriseInfo {
         RecordHistogram.recordBooleanHistogram("EnterpriseCheck.IsManaged", state.mProfileOwned);
         RecordHistogram.recordBooleanHistogram(
                 "EnterpriseCheck.IsFullyManaged", state.mDeviceOwned);
+    }
+
+    @NativeMethods
+    interface Natives {
+        void updateNativeOwnedState(boolean hasProfileOwnerApp, boolean hasDeviceOwnerApp);
     }
 }
