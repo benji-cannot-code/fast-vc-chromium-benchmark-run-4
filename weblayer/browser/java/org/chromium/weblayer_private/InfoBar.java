baf279c98fee7812bf01615c43d14c879ffa160c
Colin Blundell <blundell@chromium.org>
2020-06-26 15:11:14 +0000

    [WebLayer] Reuse Chrome's InfoBarCompactLayout.java
    //chrome's InfoBarCompactLayout.java has been componentized and
    //weblayer can now use it directly rather than a custom fork. This CL
    makes that change. The only differences in the //weblayer version were
    the following:
    
    - It had brought in the createCloseButton() implementation from
      //chrome's InfoBarLayout.java as InfoBarLayout.java wasn't brought
      into //weblayer for the initial implementation. The componentized
      InfoBarCompactLayout.java calls the now-componentized
      InfoBarLayout.java directly. The implementations are identical.
    - It forked a string resource; this CL undoes that forking. Again,
      the values of the resource were identical.
    
    There is no functional change.
    
    Bug: 1099169
    Change-Id: Idefdf0c0726f79512f10cc82ddc93ffd453f7dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265994
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782989}

--- weblayer/browser/java/org/chromium/weblayer_private/InfoBar.java
+++ weblayer/browser/java/org/chromium/weblayer_private/InfoBar.java
@@ -17,6 +17,7 @@ import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
 import org.chromium.chrome.browser.infobar.InfoBarIdentifier;
+import org.chromium.components.infobars.InfoBarCompactLayout;
 import org.chromium.components.infobars.InfoBarInteractionHandler;
 import org.chromium.components.infobars.InfoBarLayout;
 import org.chromium.ui.modelutil.PropertyModel;
