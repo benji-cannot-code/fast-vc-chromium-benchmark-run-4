7fcc8a4c144d70785e591f4078256adc0da43d33
Nico Weber <thakis@chromium.org>
2020-06-26 18:15:00 +0000

    mac: Make sandbox/ build on arm64.
    Bug: 1098899
    Change-Id: I7f03ab3d7305beec8057351f6c0aa50ccb9e4503
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270700
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783075}

--- sandbox/mac/sandbox_logging.cc
+++ sandbox/mac/sandbox_logging.cc
@@ -16,12 +16,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <string>
 
+#if __x86_64__
 #define ABORT()                                                                \
   {                                                                            \
     asm volatile(                                                              \
         "int3; ud2; push %0;" ::"i"(static_cast<unsigned char>(__COUNTER__))); \
     __builtin_unreachable();                                                   \
   }
+#elif __arm64__
+#define ABORT()                                                             \
+  {                                                                         \
+    asm volatile("udf %0;" ::"i"(static_cast<unsigned char>(__COUNTER__))); \
+    __builtin_unreachable();                                                \
+  }
+#endif
 
 extern "C" {
 void abort_report_np(const char*, ...);
