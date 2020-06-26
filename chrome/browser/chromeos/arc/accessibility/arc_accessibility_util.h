a494dd2233e2746d599f7821012acdd34590d70a
Saho Kobayashi <sahok@chromium.org>
2020-06-26 05:08:31 +0000

    Send kAriaAttributeChanged from state description.
    Send kAriaAttributeChanged event when state description change is sent
    from non range widget.
    
          AXTreeSourceArcTest.StateDescriptionChangedEvent
    
    Bug: b:154433831
    Test: ArcAccessibilityUtilTest.FromContentChangeTypesToAXEvent
    Change-Id: I676dda6d6322375db2286510ab4759a5f4bc2fed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2262159
    Commit-Queue: Saho Kobayashi <sahok@chromium.org>
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Reviewed-by: Sara Kato <sarakato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782855}

--- chrome/browser/chromeos/arc/accessibility/arc_accessibility_util.h
+++ chrome/browser/chromeos/arc/accessibility/arc_accessibility_util.h
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace arc {
 class AccessibilityInfoDataWrapper;
+// This function is only called when EventType is WINDOW_STATE_CHANGED or
+// WINDOW_CONTENT_CHANGED.
 base::Optional<ax::mojom::Event> FromContentChangeTypesToAXEvent(
     const std::vector<int>& arc_content_change_types,
     const AccessibilityInfoDataWrapper& source_node);
