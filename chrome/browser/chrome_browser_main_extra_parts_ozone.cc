c2176384323b9cf503d474ece1994374162ba694
Maksim Sisov <msisov@igalia.com>
2020-06-26 10:10:32 +0000

    X11 and Ozone: fix includes in ChromeBrowserMainExtraPartsOzone
    Verifying local use_x11 && use_ozone build and sending for review
    the fixups.
    
    TBR=sky@chromium.org
    
    Bug: 1085700
    Change-Id: Idca3d217224339fcf53dfdc4c65fb33c2ecb84b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2266672
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#782906}

--- chrome/browser/chrome_browser_main_extra_parts_ozone.cc
+++ chrome/browser/chrome_browser_main_extra_parts_ozone.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_X11)
 #include "ui/base/x/x11_error_handler.h"
-#else
+#endif
+
+#if defined(USE_OZONE)
 #include "ui/base/ui_base_features.h"
 #include "ui/ozone/public/ozone_platform.h"
 #endif
