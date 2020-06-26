e0191cf4d7480362db782d943409aee153f176c9
Jeremy Leconte <jleconte@google.com>
2020-06-26 08:47:12 +0000

    Missing notreached include header.
    Bug: None
    Change-Id: I14f5e823e956e49eb72955497afa4fbdd04fa9eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270057
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782891}

--- base/message_loop/message_pump_libevent.cc
+++ base/message_loop/message_pump_libevent.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
+#include "base/notreached.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/third_party/libevent/event.h"
 #include "base/time/time.h"
