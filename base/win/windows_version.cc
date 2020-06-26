c0655c1cf8b051f55f09cf0030a9ceeb289daee3
Bruce Dawson <brucedawson@chromium.org>
2020-06-26 20:03:57 +0000

    Updates to require and use latest Windows 10 SDK
    Now that Chrome builds with (and requires) the Windows 10 SDK we should
    enforce that in code, remove definitions that are no longer needed, and
    set NTDDI_VERSION=NTDDI_WIN10_VB to make the new features available.
    
    Bug: 999747,1089996
    Change-Id: I71b6b611826b1c92d8484d74dd2f27c5335eff6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268383
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783145}

--- base/win/windows_version.cc
+++ base/win/windows_version.cc
@@ -24,8 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #error VS 2017 Update 3.2 or higher is required
 #endif
 
-#if !defined(NTDDI_WIN10_19H1)
-#error Windows 10.0.18362.0 SDK or higher required.
+#if !defined(NTDDI_WIN10_VB)
+#error Windows 10.0.19041.0 SDK or higher required.
 #endif
 
 namespace base {
