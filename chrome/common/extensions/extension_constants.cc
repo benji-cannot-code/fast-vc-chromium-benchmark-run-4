2349aeb0da2c7e49242d09a851d3ec7df975ea91
Ryan Keane <rwkeane@google.com>
2020-06-26 21:23:05 +0000

    De-duplicate Extensions Constants
    This CL De-duplicates a constant introduced in CL
    https://chromium-review.googlesource.com/c/chromium/src/+/2249054
    
    Change-Id: I6649c75994571da9cfa7de939c435cfac546499a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265435
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Auto-Submit: Ryan Keane <rwkeane@google.com>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783194}

--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -43,7 +43,6 @@ const char kHTermDevAppId[] = "okddffdblfhhnmhodogpojmfkjmhinfp";
 const char kIdentityApiUiAppId[] = "ahjaciijnoiaklcomgnblndopackapon";
 const char kTextEditorAppId[] = "mmfbcljfglbokpmkimbfghdkjmjhdgbg";
 const char kInAppPaymentsSupportAppId[] = "nmmhkkegccagdldgiimedpiccmgmieda";
-const char kMediaRouterStableExtensionId[] = "pkedcjkdefgpdelpbcmbmeomcjbeemfm";
 
 const char* const kBuiltInFirstPartyExtensionIds[] = {
     kCalculatorAppId,
@@ -69,7 +68,7 @@ const char* const kBuiltInFirstPartyExtensionIds[] = {
     kIdentityApiUiAppId,
     kTextEditorAppId,
     kInAppPaymentsSupportAppId,
-    kMediaRouterStableExtensionId,
+    kCastExtensionIdRelease,
 #if defined(OS_CHROMEOS)
     kAssessmentAssistantExtensionId,
     kAccessibilityCommonExtensionId,
