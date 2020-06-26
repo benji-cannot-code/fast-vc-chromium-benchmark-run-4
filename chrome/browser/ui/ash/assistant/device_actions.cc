801d7186eca6a0f2515f3a55c8cf457e2ce608d4
Xiaohui Chen <xiaohuic@google.com>
2020-06-26 17:42:32 +0000

    assistant: update AppStatus enum name
    Bug: None
    Change-Id: I906c85c64f30e3bce4782225cf52c5a25cf470eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268458
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783048}

--- chrome/browser/ui/ash/assistant/device_actions.cc
+++ chrome/browser/ui/ash/assistant/device_actions.cc
@@ -178,7 +178,7 @@ void DeviceActions::SetSwitchAccessEnabled(bool enabled) {
 
 bool DeviceActions::OpenAndroidApp(const AndroidAppInfo& app_info) {
   auto status = delegate_->GetAndroidAppStatus(app_info.package_name);
-  if (status != AppStatus::AVAILABLE)
+  if (status != AppStatus::kAvailable)
     return false;
 
   auto* app = ARC_GET_INSTANCE_FOR_METHOD(
@@ -231,7 +231,7 @@ void DeviceActions::RemoveAppListEventSubscriber(
 base::Optional<std::string> DeviceActions::GetAndroidAppLaunchIntent(
     const AndroidAppInfo& app_info) {
   auto status = delegate_->GetAndroidAppStatus(app_info.package_name);
-  if (status != AppStatus::AVAILABLE)
+  if (status != AppStatus::kAvailable)
     return base::nullopt;
 
   return GetLaunchIntent(std::move(app_info));
