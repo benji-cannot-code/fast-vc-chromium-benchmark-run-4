1db857f282320e5bcbc445aceddeedbbe6be2423
Joel Hockey <joelhockey@chromium.org>
2020-06-26 09:07:11 +0000

    Use AppService to get GuestOsFileTasks icons
    Remove the dedicated Crostini/GuestOs code for fetching icons.  This
    code was written before AppService existed to manage icons.
    
    When icons are not found for crostini, return the default penguin,
    but for Plugin VM, return an empty icon so that AppService will use
    its defaults.
    
    Bug: b/158617133
    Bug: b/159050715
    Change-Id: I0ea16e6b795f8e8cdace315f6c9b500781bd0b73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2269619
    Reviewed-by: Nancy Wang <nancylingwang@chromium.org>
    Reviewed-by: Timothy Loh <timloh@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782897}

--- chrome/browser/chromeos/crostini/crostini_util.h
+++ chrome/browser/chromeos/crostini/crostini_util.h
@@ -16,16 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chromeos/crostini/crostini_simple_types.h"
 #include "storage/browser/file_system/file_system_url.h"
-#include "ui/base/resource/scale_factor.h"
 
 namespace base {
 class FilePath;
-class TimeDelta;
 }  // namespace base
-
-namespace gfx {
-class ImageSkia;
-}  // namespace gfx
 
 namespace views {
 class Widget;
@@ -110,17 +104,6 @@ void LaunchCrostiniApp(Profile* profile,
                        int64_t display_id,
                        const std::vector<storage::FileSystemURL>& files,
                        LaunchCrostiniAppCallback callback);
-
-// Convenience wrapper around CrostiniAppIconLoader. As requesting icons from
-// the container can be slow, we just use the default (penguin) icons after the
-// timeout elapses. Subsequent calls would get the correct icons once loaded.
-void LoadIcons(Profile* profile,
-               const std::vector<std::string>& app_ids,
-               int resource_size_in_dip,
-               ui::ScaleFactor scale_factor,
-               base::TimeDelta timeout,
-               base::OnceCallback<void(const std::vector<gfx::ImageSkia>&)>
-                   icons_loaded_callback);
 
 // Retrieves cryptohome_id from profile.
 std::string CryptohomeIdForProfile(Profile* profile);
