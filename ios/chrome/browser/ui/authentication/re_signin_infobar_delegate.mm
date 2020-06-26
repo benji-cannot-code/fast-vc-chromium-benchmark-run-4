f9abc4734f68a4dfeca1f3b096720cfd69642be9
Nohemi Fernandez <fernandex@chromium.org>
2020-06-26 15:01:19 +0000

    [iOS] Move protocol header to public sign-in directory.
    Clean up signin_interaction/ directory, which was part of the old
    sign-in architecture.
    
    Bug: 971989
    Change-Id: I099c0247d69861c6836672840dc5ddb66bad4299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270201
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782984}

--- ios/chrome/browser/ui/authentication/re_signin_infobar_delegate.mm
+++ ios/chrome/browser/ui/authentication/re_signin_infobar_delegate.mm
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/signin/authentication_service.h"
 #include "ios/chrome/browser/signin/authentication_service_factory.h"
 #import "ios/chrome/browser/ui/commands/show_signin_command.h"
-#import "ios/chrome/browser/ui/signin_interaction/public/signin_presenter.h"
 #include "ios/chrome/grit/ios_strings.h"
+#import "ios/public/provider/chrome/browser/signin/signin_presenter.h"
 #include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
