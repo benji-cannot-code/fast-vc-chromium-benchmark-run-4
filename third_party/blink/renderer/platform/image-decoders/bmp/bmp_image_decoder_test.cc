f20ea25503e607f82eca81632ccc65a89786fa68
Philip Rogers <pdr@chromium.org>
2020-06-26 15:02:21 +0000

    Replace Lenna with Grace Hopper
    This patch replaces "Lenna" images with images of Grace Hopper.
    
    Bug: 1099702
    Change-Id: I32cf958bd2b6fbef5388da24de846a90ac0af9bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270110
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782985}

--- third_party/blink/renderer/platform/image-decoders/bmp/bmp_image_decoder_test.cc
+++ third_party/blink/renderer/platform/image-decoders/bmp/bmp_image_decoder_test.cc
@@ -24,7 +24,8 @@ std::unique_ptr<ImageDecoder> CreateBMPDecoder() {
 }  // anonymous namespace
 
 TEST(BMPImageDecoderTest, isSizeAvailable) {
-  static constexpr char kBmpFile[] = "/images/resources/lenna.bmp";  // 256x256
+  // This image is 256x256.
+  static constexpr char kBmpFile[] = "/images/resources/gracehopper.bmp";
   scoped_refptr<SharedBuffer> data = ReadFile(kBmpFile);
   ASSERT_TRUE(data.get());
 
@@ -36,7 +37,8 @@ TEST(BMPImageDecoderTest, isSizeAvailable) {
 }
 
 TEST(BMPImageDecoderTest, parseAndDecode) {
-  static constexpr char kBmpFile[] = "/images/resources/lenna.bmp";  // 256x256
+  // This image is 256x256.
+  static constexpr char kBmpFile[] = "/images/resources/gracehopper.bmp";
   scoped_refptr<SharedBuffer> data = ReadFile(kBmpFile);
   ASSERT_TRUE(data.get());
 
@@ -82,7 +84,7 @@ TEST(BMPImageDecoderTest, int32MinHeight) {
 // size (when BMPImageDecoder stops while it may still have input data to
 // read) and a call to do a full decode.
 TEST(BMPImageDecoderTest, mergeBuffer) {
-  static constexpr char kBmpFile[] = "/images/resources/lenna.bmp";
+  static constexpr char kBmpFile[] = "/images/resources/gracehopper.bmp";
   TestMergeBuffer(&CreateBMPDecoder, kBmpFile);
 }
 
