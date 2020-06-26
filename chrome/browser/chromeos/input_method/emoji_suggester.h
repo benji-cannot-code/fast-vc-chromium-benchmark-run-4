6cf4f5473ba928c314f0e1828e400bd2324f9850
My Nguyen <myy@chromium.org>
2020-06-26 05:34:30 +0000

    Add press down and number to accept emoji
    Implement according to spec at http://go/e14s-emoji-addition-mock
    
    Pressing down will still move to the next candidate, pressing number
    after that will accept the number emoji.
    
    Bug: 1098692
    Change-Id: Iad1cd39efa0f2ed5ccfad875b8018058cc3d63ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2263692
    Commit-Queue: My Nguyen <myy@chromium.org>
    Reviewed-by: David Vallet <dvallet@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782860}

--- chrome/browser/chromeos/input_method/emoji_suggester.h
+++ chrome/browser/chromeos/input_method/emoji_suggester.h
@@ -40,6 +40,7 @@ class EmojiSuggester : public Suggester {
   void LoadEmojiMap();
   void OnEmojiDataLoaded(const std::string& emoji_data);
   void RecordAcceptanceIndex(int index);
+  void ResetState();
 
   InputMethodEngine* const engine_;
 
@@ -48,6 +49,8 @@ class EmojiSuggester : public Suggester {
 
   // If we are showing a suggestion right now.
   bool suggestion_shown_ = false;
+
+  std::string last_event_key_;
 
   // The current list of candidates.
   std::vector<base::string16> candidates_;
