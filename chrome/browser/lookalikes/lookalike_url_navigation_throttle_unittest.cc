53748274fa07842b963b3f2756bac8573bff08f4
Mustafa Emre Acer <meacer@chromium.org>
2020-06-26 22:08:20 +0000

    Do not interstitial if the IDN contains only ASCII and emoji
    Chrome presently doesn't allow internationalized domain names to
    contain pictographs or emoji. These domain names fail ICU spoof checks.
    Recent changes introduced a variant of the lookalike interstitial for
    domains failing ICU spoof checks which means domains containing emojis
    will now be blocked with an interstitial.
    
    Some browsers such as Safari allow domain names to contain emojis, so
    blocking them outright doesn't feel right. This CL un-interstitials
    domains that consist of only ASCII and/or emoji-like characters.
    
    Bug: 1095120
    Change-Id: Ibb1914d72359b60e033178d1f657f90062f38cca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268594
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783216}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
@@ -91,10 +91,19 @@ TEST_F(LookalikeThrottleTest, SpoofsBlocked) {
   const struct TestCase {
     const char* hostname;
     bool expected_blocked;
-  } kTestCases[] = {{"private.hostname", false},
-                    {"example·com.com", true},
-                    {"🍕.com", true},
-                    {"þook.com", true}};
+  } kTestCases[] = {
+      // ASCII private domain.
+      {"private.hostname", false},
+      // Unsafe middle dot.
+      {"example·com.com", true},
+      // Fails ICU spoof checks.
+      {"lɔlocked.com", true},
+      // Contains a TLD specific character
+      {"þook.com", true},
+      // Also fails ICU spoof checks, but is allowed because consists of only
+      // emoji and ASCII.
+      {"🍕.com", false},
+  };
 
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeature(
@@ -115,10 +124,12 @@ TEST_F(LookalikeThrottleTest, SpoofsBlocked) {
 
     if (test_case.expected_blocked) {
       EXPECT_EQ(content::NavigationThrottle::CANCEL,
-                throttle->WillProcessResponse().action());
+                throttle->WillProcessResponse().action())
+          << "Failed: " << test_case.hostname;
     } else {
       EXPECT_EQ(content::NavigationThrottle::PROCEED,
-                throttle->WillProcessResponse().action());
+                throttle->WillProcessResponse().action())
+          << "Failed: " << test_case.hostname;
     }
   }
 }
