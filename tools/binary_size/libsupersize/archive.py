fff6c2d812091459a582b0f0214a19d4146638bf
Samuel Huang <huangs@chromium.org>
2020-06-26 15:07:34 +0000

    [SuperSize] Allow archiving of .apk files without native code.
    Previously, running SuperSize-archive on .apk files without native code
    (.so files) would lead to failure. The workaround was to specify the
    --java-only flag, but this was non-ideal.
    
    This CL fixes the above, so SuperSize can archive .apk files normally
    without the flag. This change is also propagated to Trichrome .ssargs
    generation (for WebView in particular).
    
    Bug: 1040645
    Change-Id: I19f5fe1f03ea74f789fd8fd12a072fe073df4021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2269878
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782986}

--- tools/binary_size/libsupersize/archive.py
+++ tools/binary_size/libsupersize/archive.py
@@ -132,6 +132,8 @@ class ContainerArchiveOptions:
 
     self.analyze_java = not (sub_args.native_only or sub_args.no_java
                              or self.relocations_mode)
+    # This may be further disabled downstream, e.g., for the case where an APK
+    # is specified, but it contains no .so files.
     self.analyze_native = not (sub_args.java_only or sub_args.no_native)
 
 
@@ -1468,7 +1470,7 @@ def CreateContainerAndSymbols(knobs=None,
     raw_symbols is a list of Symbol objects.
   """
   knobs = knobs or SectionSizeKnobs()
-  if apk_path and elf_path:
+  if apk_path and apk_so_path:
     # Extraction takes around 1 second, so do it in parallel.
     apk_elf_result = parallel.ForkAndCall(_ElfInfoFromApk,
                                           (apk_path, apk_so_path, tool_prefix))
@@ -1944,7 +1946,9 @@ def _DeduceNativeInfo(tentative_output_dir, apk_path, elf_path, map_path,
           f for f in z.infolist()
           if f.filename.endswith('.so') and f.file_size > 0
       ]
-    assert lib_infos, 'APK has no .so files.'
+    if not lib_infos:
+      return None, map_path, None
+
     # TODO(agrieve): Add support for multiple .so files, and take into account
     #     secondary architectures.
     apk_so_path = max(lib_infos, key=lambda x: x.file_size).filename
@@ -2072,6 +2076,9 @@ def _DeduceMainPaths(args, on_config_error):
       elf_path, map_path, apk_so_path = _DeduceNativeInfo(
           output_directory, apk_path, sub_args.elf_file
           or sub_args.aux_elf_file, sub_args.map_file, on_config_error)
+      if not (elf_path or map_path or apk_so_path):
+        opts.analyze_native = False
+    if opts.analyze_native:
       if map_path:
         linker_name = _DetectLinkerName(map_path)
         logging.info('Linker name: %s' % linker_name)
