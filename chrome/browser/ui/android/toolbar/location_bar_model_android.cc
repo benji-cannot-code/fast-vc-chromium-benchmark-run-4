5e4b7dc8584de6c75b29366b8e8651aebd16d8d1
Moe Ahmadi <mahmadi@chromium.org>
2020-06-26 18:27:42 +0000

    [omnibox] Fix page classification detection for interactions from WebUI NTP
    Omnibox page classification detection logic is failing to correctly
    report interactions from the WebUI NTP. This is due to the WebUI NTP
    not using the instant process. This CL fixes that by making the
    ChromeLocationBarModelDelegate::IsInstantNTP logic correctly identify
    both the local NTP and the WebUI NTP without the need for checking
    whether the NTP uses the instant process.
    
    As a result interactions from the WebUI NTP will continue to be reported
    as OmniboxEventProto::INSTANT_NTP_WITH_[OMNIBOX|FAKEBOX]_AS_STARTING_FOCUS
    without the NTP actually using the instant process.
    
    This logic *should* eventually be refactord into
    chrome/browser/search/search.cc once the WebUI NTP is fully launched and
    the references to the instant process are removed.
    
    Bug: 1098513
    Change-Id: I6d4a9e75a45cd70232ffa3541b122c639a428a4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265415
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783086}

--- chrome/browser/ui/android/toolbar/location_bar_model_android.cc
+++ chrome/browser/ui/android/toolbar/location_bar_model_android.cc
@@ -78,7 +78,7 @@ content::WebContents* LocationBarModelAndroid::GetActiveWebContents() const {
   return content::WebContents::FromJavaWebContents(jweb_contents);
 }
 
-bool LocationBarModelAndroid::IsInstantNTP() const {
+bool LocationBarModelAndroid::IsNewTabPage() const {
   GURL url;
   if (!GetURL(&url))
     return false;
