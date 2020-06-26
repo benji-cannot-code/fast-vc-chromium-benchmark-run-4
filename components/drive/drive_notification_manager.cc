392f12201b225dbe4313143c3fb1bd2cd365f3c0
Marc Treib <treib@chromium.org>
2020-06-26 13:56:20 +0000

    Remove cacheinvalidation references from components/drive
    third_party/cacheinvalidation is not actually used anymore, and will be
    deleted soon.
    
    Bug: 1099672
    Change-Id: Ife72947a7c50f87eae0d39986fcaea134d2352e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270145
    Commit-Queue: Kazuhiro Inaba <kinaba@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#782962}

--- components/drive/drive_notification_manager.cc
+++ components/drive/drive_notification_manager.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/drive/drive_notification_observer.h"
 #include "components/invalidation/public/invalidation_service.h"
 #include "components/invalidation/public/topic_invalidation_map.h"
-#include "google/cacheinvalidation/types.pb.h"
 
 namespace drive {
 
