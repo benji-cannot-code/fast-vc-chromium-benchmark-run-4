b0a62e10c8cca46c2011b0b7efc554e5ec5db1ca
Ng Zhi An <zhin@chromium.org>
2020-06-26 18:43:26 +0000

    [Py3] Get blink_gc_plugin Py3 ready
    Proper fix for process-graph.py (changes were reverted in
    https://crrev.com/c/2266652).
    
    Bug: chromium:941669
    Change-Id: I38242be26371246a33a88630a1480ce8446b7680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2267737
    Commit-Queue: Zhi An Ng <zhin@chromium.org>
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783094}

--- tools/clang/blink_gc_plugin/process-graph.py
+++ tools/clang/blink_gc_plugin/process-graph.py
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from __future__ import print_function
-from StringIO import StringIO
 import argparse, os, sys, json, subprocess, pickle
+
+try:
+  from StringIO import StringIO  # Python 2
+except:
+  from io import StringIO
 
 parser = argparse.ArgumentParser(
   description =
