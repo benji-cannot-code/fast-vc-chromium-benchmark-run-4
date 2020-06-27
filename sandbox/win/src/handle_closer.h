ca4274449d828101034bcac180ae598f102a21e5
Alex Gough <ajgo@chromium.org>
2020-06-27 01:07:48 +0000

    Clarify ownership for TargetProcess in sandbox.
    std::unique_ptr<TargetProcess> introduced to make transfer of
    ownership to policy and thus JobTracker or ProcessTracker clearer.
    TargetProcess isn't reference counted so these stub methods are
    removed.
    
    SpawnCleanup can now just be tracker->Terminate() or left until destruction
    as target is cleaned up (terminated and closed) when it goes out of scope.
    Calling terminate ahead of cleanup should be ok, so we leave calls to
    ->Terminate() as long as tracker is not owned by the policy.
    
    Policy can now track processes as well as jobs through introduction of
    OnProcessFinished method. This may make it easier to add shared
    policies in future.
    
    Change inspired by comments relating to #480639 and from crash reports
    in #702552 (probably a handle closing bug) where this change may
    move some stacks around.
    
    Tests: sbox_unittests, sbox_integration_tests, plus manual check with --allow-no-sandbox-job.
    
    Bug: 480639,702552
    Change-Id: Iac8b5a84b4d0530ab6c3a5cb939efde314422b79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249378
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783271}

--- sandbox/win/src/handle_closer.h
+++ sandbox/win/src/handle_closer.h
@@ -54,7 +54,7 @@ class HandleCloser {
   ResultCode AddHandle(const wchar_t* handle_type, const wchar_t* handle_name);
 
   // Serializes and copies the closer table into the target process.
-  bool InitializeTargetHandles(TargetProcess* target);
+  bool InitializeTargetHandles(TargetProcess& target);
 
  private:
   // Calculates the memory needed to copy the serialized handles list (rounded
