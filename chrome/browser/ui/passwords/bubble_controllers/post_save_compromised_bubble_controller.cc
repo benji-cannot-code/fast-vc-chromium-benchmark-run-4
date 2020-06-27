e0c417c73a9114be1adb2d80bc87b78b5dad1aca
Vasilii Sukhanov <vasilii@chromium.org>
2020-06-27 17:03:29 +0000

    Implement a notification about the compromised passwords.
    The bubble follows the Save/Update password bubble. There are 3 states:
    - Safe state.
    - Compromised password fixed, more to go.
    - N compromised passwords to fix.
    
    Bug: 1049200
    Change-Id: I5bc444f089a7beb884d4fc9f6a5193ae1784e6b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2267321
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783331}

--- chrome/browser/ui/passwords/bubble_controllers/post_save_compromised_bubble_controller.cc
+++ chrome/browser/ui/passwords/bubble_controllers/post_save_compromised_bubble_controller.cc
@@ -5,12 +5,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/passwords/bubble_controllers/post_save_compromised_bubble_controller.h"
 
+#include "chrome/browser/ui/passwords/passwords_model_delegate.h"
+#include "chrome/grit/generated_resources.h"
+#include "chrome/grit/theme_resources.h"
+#include "components/password_manager/core/common/password_manager_ui.h"
+#include "ui/base/l10n/l10n_util.h"
+
 PostSaveCompromisedBubbleController::PostSaveCompromisedBubbleController(
     base::WeakPtr<PasswordsModelDelegate> delegate)
     : PasswordBubbleControllerBase(
           std::move(delegate),
           password_manager::metrics_util::
-              AUTOMATIC_COMPROMISED_CREDENTIALS_REMINDER) {}
+              AUTOMATIC_COMPROMISED_CREDENTIALS_REMINDER) {
+  switch (delegate_->GetState()) {
+    case password_manager::ui::PASSWORD_UPDATED_SAFE_STATE:
+      type_ = BubbleType::kPasswordUpdatedSafeState;
+      break;
+    case password_manager::ui::PASSWORD_UPDATED_MORE_TO_FIX:
+      type_ = BubbleType::kPasswordUpdatedWithMoreToFix;
+      break;
+    case password_manager::ui::PASSWORD_UPDATED_UNSAFE_STATE:
+      type_ = BubbleType::kUnsafeState;
+      break;
+    default:
+      NOTREACHED();
+  }
+}
 
 PostSaveCompromisedBubbleController::~PostSaveCompromisedBubbleController() {
   // Make sure the interactions are reported even if Views didn't notify the
@@ -19,8 +39,64 @@ PostSaveCompromisedBubbleController::~PostSaveCompromisedBubbleController() {
     OnBubbleClosing();
 }
 
+base::string16 PostSaveCompromisedBubbleController::GetBody() const {
+  switch (type_) {
+    case BubbleType::kPasswordUpdatedSafeState:
+      return l10n_util::GetStringUTF16(
+          IDS_PASSWORD_MANAGER_SAFE_STATE_BODY_MESSAGE);
+    case BubbleType::kPasswordUpdatedWithMoreToFix:
+      return l10n_util::GetPluralStringFUTF16(
+          IDS_PASSWORD_MANAGER_MORE_TO_FIX_BODY_MESSAGE,
+          delegate_->GetTotalNumberCompromisedPasswords());
+    case BubbleType::kUnsafeState:
+      return l10n_util::GetPluralStringFUTF16(
+          IDS_PASSWORD_MANAGER_UNSAFE_STATE_BODY_MESSAGE,
+          delegate_->GetTotalNumberCompromisedPasswords());
+  }
+}
+
+base::string16 PostSaveCompromisedBubbleController::GetButtonText() const {
+  switch (type_) {
+    case BubbleType::kPasswordUpdatedSafeState:
+      return base::string16();
+    case BubbleType::kPasswordUpdatedWithMoreToFix:
+      return l10n_util::GetStringUTF16(
+          IDS_PASSWORD_MANAGER_CHECK_REMAINING_BUTTON);
+    case BubbleType::kUnsafeState:
+      return l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_CHECK_BUTTON);
+  }
+}
+
+int PostSaveCompromisedBubbleController::GetImageID(bool dark) const {
+  switch (type_) {
+    case BubbleType::kPasswordUpdatedSafeState:
+      return dark ? IDR_SAVED_PASSWORDS_SAFE_STATE_DARK
+                  : IDR_SAVED_PASSWORDS_SAFE_STATE;
+    case BubbleType::kPasswordUpdatedWithMoreToFix:
+      return dark ? IDR_SAVED_PASSWORDS_NEUTRAL_STATE_DARK
+                  : IDR_SAVED_PASSWORDS_NEUTRAL_STATE;
+    case BubbleType::kUnsafeState:
+      return dark ? IDR_SAVED_PASSWORDS_WARNING_STATE_DARK
+                  : IDR_SAVED_PASSWORDS_WARNING_STATE;
+  }
+}
+
+void PostSaveCompromisedBubbleController::OnAccepted() {
+  if (delegate_)
+    delegate_->NavigateToPasswordCheckup();
+}
+
 base::string16 PostSaveCompromisedBubbleController::GetTitle() const {
-  return base::string16();
+  switch (type_) {
+    case BubbleType::kPasswordUpdatedSafeState:
+    case BubbleType::kPasswordUpdatedWithMoreToFix:
+      return l10n_util::GetStringUTF16(
+          IDS_PASSWORD_MANAGER_UPDATED_BUBBLE_TITLE);
+    case BubbleType::kUnsafeState:
+      return l10n_util::GetPluralStringFUTF16(
+          IDS_PASSWORD_MANAGER_COMPROMISED_REMINDER_TITLE,
+          delegate_->GetTotalNumberCompromisedPasswords());
+  }
 }
 
 void PostSaveCompromisedBubbleController::ReportInteractions() {}
