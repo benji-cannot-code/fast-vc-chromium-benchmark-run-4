d1a93e200dc441bb496ad7d1760bf629bb8d76b8
dpapad <dpapad@chromium.org>
2020-06-27 01:05:10 +0000

    PDF Viewer Update: Add basic layout for new toolbar.
    Specifically:
     - Add button to expand/collapse the sidenav.
     - Add placeholder for the document's title.
     - Add basic layout for page/zoom/rotate controls.
     - Add download, print, and action menu buttons.
    
    None of the buttons are currently hooked up. Also the logic to
    handle very narrow window sizes will be added in later CLs.
    
    Bug: 1098124
    Change-Id: Idd6f454b5ff840245f041de068a6bf19a9b71f9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268543
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: dpapad <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783269}

--- chrome/browser/resources/pdf/elements/viewer-pdf-toolbar-new.js
+++ chrome/browser/resources/pdf/elements/viewer-pdf-toolbar-new.js
@@ -3,6 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.m.js';
+import 'chrome://resources/cr_elements/icons.m.js';
+
+import './icons.js';
+import './viewer-page-selector.js';
+
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 class ViewerPdfToolbarNewElement extends PolymerElement {
