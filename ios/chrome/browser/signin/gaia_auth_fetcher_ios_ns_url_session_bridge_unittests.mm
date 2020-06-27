678fc87d1de6724cdf73dbd57cb9e2b013167806
Nico Weber <thakis@chromium.org>
2020-06-27 16:23:58 +0000

    Revert "Disable some GaiaAuthFetcherIOSNSURLSessionBridgeTest tests  on iOS 12 device"
    This reverts commit 92e109128024a4551792fac8f3f22d27bcfb5efb.
    
    Reason for revert: These tests still make cq runs fail for me, see e.g.
    https://ci.chromium.org/p/chrome/builders/try/iphone-device/515?
    Looks like they don't work yet, and disabling on iOS 12 isn't
    sufficient.
    
    Original change's description:
    > Disable some GaiaAuthFetcherIOSNSURLSessionBridgeTest tests  on iOS 12 device
    > 
    > Bug: 1099879
    > Change-Id: I2a56bfe667b311ba16a4808af1e5202eb84938ba
    > TBR: msarda@chromium.org
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270627
    > Reviewed-by: Eugene But <eugenebut@chromium.org>
    > Commit-Queue: Eugene But <eugenebut@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#783195}
    
    TBR=msarda@chromium.org,eugenebut@chromium.org
    
    Change-Id: I750e3dce002c754e8d6da294dca8ab0ca47b8ce4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1099879
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2271468
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783328}

--- ios/chrome/browser/signin/gaia_auth_fetcher_ios_ns_url_session_bridge_unittests.mm
+++ ios/chrome/browser/signin/gaia_auth_fetcher_ios_ns_url_session_bridge_unittests.mm
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
-#include "base/ios/ios_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/chrome/browser/signin/feature_flags.h"
@@ -329,12 +328,6 @@ GaiaAuthFetcherIOSNSURLSessionBridgeTest::GetHeaderFieldsWithCookies(
 // Tests to send a request with no cookies set in the cookie store and receive
 // multiples cookies from the request.
 TEST_F(GaiaAuthFetcherIOSNSURLSessionBridgeTest, FetchWithEmptyCookieStore) {
-#if !(TARGET_OS_SIMULATOR)
-  if (!base::ios::IsRunningOnOrLater(13, 0, 0)) {
-    // TODO(crbug.com/1099879): This test is failing on iOS 12.4 device.
-    return;
-  }
-#endif
   ns_url_session_bridge_->Fetch(GetFetchGURL(), "", "", false);
   OCMExpect([http_cookie_storage_mock_
       storeCookies:@[]
@@ -380,12 +373,6 @@ TEST_F(GaiaAuthFetcherIOSNSURLSessionBridgeTest,
 // Tests to a request with a redirect. One cookie is received by the first
 // request, and a second one by the redirected request.
 TEST_F(GaiaAuthFetcherIOSNSURLSessionBridgeTest, FetchWithRedirect) {
-#if !(TARGET_OS_SIMULATOR)
-  if (!base::ios::IsRunningOnOrLater(13, 0, 0)) {
-    // TODO(crbug.com/1099879): This test is failing on iOS 12.4 device.
-    return;
-  }
-#endif
   ns_url_session_bridge_->Fetch(GetFetchGURL(), "", "", false);
   OCMExpect([http_cookie_storage_mock_
       storeCookies:@[]
