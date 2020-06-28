4f70ab9457f8cb2419c21cbba0835aa569613623
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-06-28 23:52:22 +0000

    Import wpt@e4d0d4f1d3383211571cdd0a3e1d5281e0398649
    Using wpt-import in Chromium c6952cd61c22426ba2dfaf7abeec71d534b93b18.
    With Chromium commits locally applied on WPT:
    3398d444d7 "[COOP] moving reporting tests to reporting/"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    TBR=lpz@google.com
    
    No-Export: true
    Change-Id: Ic87e65abffc1a46194aa1a99f2a11ec6759606c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2271584
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#783391}

--- third_party/blink/web_tests/external/wpt/websockets/cookies/support/set-cookie.py
+++ third_party/blink/web_tests/external/wpt/websockets/cookies/support/set-cookie.py
@@ -1,7 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 from six.moves.urllib import parse
 
+from wptserve.utils import isomorphic_encode
 
 def main(request, response):
-    response.headers.set('Set-Cookie', parse.unquote(request.url_parts.query))
-    return [("Content-Type", "text/plain")], ""
+    response.headers.set(b'Set-Cookie', isomorphic_encode(parse.unquote(request.url_parts.query)))
+    return [(b"Content-Type", b"text/plain")], b""
