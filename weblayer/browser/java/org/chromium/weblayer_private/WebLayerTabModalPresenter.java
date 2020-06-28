4ce29780a173b23a164cf9b79e325da18fd7cab9
Peter Kotwicz <pkotwicz@chromium.org>
2020-06-28 23:46:12 +0000

    [Android StrictMode] Add LayoutInflater#inflate() wrapper
    Add LayoutInflater#inflate() wrapper - LayoutInflaterUtils.java - which
    suppresses StrictMode. A wrapper class is used because we want to:
    - ignore strict mode violations caused by Web Layer and webview
      LayoutInflater#inflate() calls
    - not ignore strict mode violations caused by Web Layer and webview
    embedder  LayoutInflater#inflate() calls
    
    BUG=2258578
    
    Change-Id: I430e7a29af6cb977667c9fb34499aa9d30d1438c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268637
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783390}

--- weblayer/browser/java/org/chromium/weblayer_private/WebLayerTabModalPresenter.java
+++ weblayer/browser/java/org/chromium/weblayer_private/WebLayerTabModalPresenter.java
@@ -6,16 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.weblayer_private;
 
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-import org.chromium.base.StrictModeContext;
 import org.chromium.components.browser_ui.modaldialog.R;
 import org.chromium.components.browser_ui.modaldialog.TabModalPresenter;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.common.BrowserControlsState;
+import org.chromium.ui.LayoutInflaterUtils;
 import org.chromium.ui.modelutil.PropertyModel;
 
 /**
@@ -54,11 +53,8 @@ public class WebLayerTabModalPresenter extends TabModalPresenter {
     }
 
     private FrameLayout loadDialogContainer() {
-        // LayoutInflater may trigger accessing the disk.
-        try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
-            return (FrameLayout) LayoutInflater.from(mContext).inflate(
-                    R.layout.modal_dialog_container, null);
-        }
+        return (FrameLayout) LayoutInflaterUtils.inflate(
+                mContext, R.layout.modal_dialog_container, null);
     }
 
     @Override
