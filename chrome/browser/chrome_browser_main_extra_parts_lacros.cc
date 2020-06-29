44ac239ed223e30b27ebe7a13616f47294253dd3
James Cook <jamescook@chromium.org>
2020-06-29 22:32:51 +0000

    lacros: Fix startup race for select file dialog
    Construct LacrosChromeServiceImpl as soon as threads are ready. This
    allows the mojo::Remotes for ash-side APIs to be bound immediately.
    Thus they are safe to use any time LacrosChromeServiceImpl exists.
    Outgoing calls will be queued until connection to ash-chrome.
    
          pipe to ash-chrome. Hit Ctrl-O/Ctrl-S. No crash.
    
    Test: Launch lacros-chrome from cmd line so it doesn't have a mojo
    Bug: 1090587
    Change-Id: Iace0a9a2b26ba7e7fd3dbb2132b9454b3e86c1f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2271065
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Darin Fisher <darin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783706}

---
+++ chrome/browser/chrome_browser_main_extra_parts_lacros.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chrome_browser_main_extra_parts_lacros.h"
+
+#include "chromeos/lacros/browser/lacros_chrome_service_impl.h"
+
+ChromeBrowserMainExtraPartsLacros::ChromeBrowserMainExtraPartsLacros() =
+    default;
+
+ChromeBrowserMainExtraPartsLacros::~ChromeBrowserMainExtraPartsLacros() =
+    default;
+
+void ChromeBrowserMainExtraPartsLacros::PostCreateThreads() {
+  lacros_chrome_service_ =
+      std::make_unique<chromeos::LacrosChromeServiceImpl>();
+}
