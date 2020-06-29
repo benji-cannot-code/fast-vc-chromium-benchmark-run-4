293cddba4216b9c38aca99338c46cf016a8020e8
Wei Li <weili@chromium.org>
2020-06-29 21:53:41 +0000

    Add unit tests for accessibility alert window
    As part of a larger effort to increase ui/views code coverage, this CL
    added unit tests and fixed an condition.
    
    Bug: none
    Change-Id: I2791a05f07c0a3ec8c85d80b8400178c726bd4b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2271243
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Commit-Queue: Wei Li <weili@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783690}

--- ui/views/accessibility/accessibility_alert_window.h
+++ ui/views/accessibility/accessibility_alert_window.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/gtest_prod_util.h"
 #include "base/scoped_observer.h"
 #include "ui/aura/env.h"
 #include "ui/aura/env_observer.h"
@@ -36,6 +37,9 @@ class VIEWS_EXPORT AccessibilityAlertWindow : public aura::EnvObserver {
   void HandleAlert(const std::string& alert_string);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(AccessibilityAlertWindowTest, HandleAlert);
+  FRIEND_TEST_ALL_PREFIXES(AccessibilityAlertWindowTest, OnWillDestroyEnv);
+
   // aura::EnvObserver:
   void OnWillDestroyEnv() override;
 
