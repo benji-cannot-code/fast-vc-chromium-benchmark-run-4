e6d00b136a3755a5c6c44a8c2e4737ad3de6e432
Anastasia Helfinstein <anastasi@google.com>
2020-06-29 19:54:16 +0000

    [Switch Access] Remove assigned key codes from settings dropdowns
    In order to allow Switch Access users to adjust their settings
    independently, this changes the settings page behavior to remove key
    codes that are already assigned from all other dropdowns, rather than
    unassigning key codes from all other dropdowns when a value is selected.
    
    AX-Relnotes: n/a.
    Bug: 1095221
    Change-Id: I2b3789292e75d36b391f7bd5f145efbe241abeb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252852
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/master@{#783634}

---
+++ chrome/browser/resources/settings/chromeos/os_a11y_page/switch_access_constants.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Available switch assignment values.
+ * @enum {number}
+ */
+const SwitchAccessAssignmentValue = {
+  NONE: 0,
+  ONE: 1,
+  TWO: 2,
+  THREE: 3,
+  FOUR: 4,
+  FIVE: 5,
+  SPACE: 6,
+  ENTER: 7,
+};
+
+/**
+ * Available commands.
+ * @enum {string}
+ */
+const SwitchAccessCommand = {
+  NEXT: 'next',
+  PREVIOUS: 'previous',
+  SELECT: 'select'
+};
