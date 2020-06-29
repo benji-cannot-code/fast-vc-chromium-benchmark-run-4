9bfee66bd9242667aca6780b1a6a098bfa194f06
Lukasz Anforowicz <lukasza@chromium.org>
2020-06-29 20:23:43 +0000

    Removing blink::Platform::CreateDefaultURLLoaderFactory method.
    After recent CLs there are no more callers of
    blink::Platform::CreateDefaultURLLoaderFactory - let's remove this
    method.  This CL helps ensure that Blink code should always go through a
    frame-specific URLLoaderFactory.
    
    After this CL, https://crbug.com/891872 can be marked as fixed, although
    there are some follow-up work items for the remaining process-wide
    factory usage - these can be tracked in https://crbug.com/1098938 going
    forward.
    
    Fixed: 891872
    Change-Id: I2c42031a1c6c92f049dc975b880184eaa3a68bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264503
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783649}

--- third_party/blink/renderer/platform/testing/testing_platform_support.cc
+++ third_party/blink/renderer/platform/testing/testing_platform_support.cc
@@ -108,12 +108,6 @@ WebString TestingPlatformSupport::DefaultLocale() {
   return WebString::FromUTF8("en-US");
 }
 
-std::unique_ptr<WebURLLoaderFactory>
-TestingPlatformSupport::CreateDefaultURLLoaderFactory() {
-  return old_platform_ ? old_platform_->CreateDefaultURLLoaderFactory()
-                       : nullptr;
-}
-
 WebData TestingPlatformSupport::GetDataResource(int resource_id,
                                                 ui::ScaleFactor scale_factor) {
   return old_platform_
