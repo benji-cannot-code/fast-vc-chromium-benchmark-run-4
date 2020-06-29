216f89bda2a4674d367b15825e726c950b1d2b43
Nektarios Paisios <nektar@chromium.org>
2020-06-29 07:11:17 +0000

    Marks all text fields in Views as leaf nodes in accessibility
    Text fields are considered leaf nodes in the accessibility trees of all supported platforms.
    If a text field has static text descendants in the internal accessibility tree, they are used to compute
    the field's value.
    Otherwise, the value attribute is trusted.
    This is now a uniform approach between Views and Web.
    The reason that we prefer to compute the value of a text field from its descendants is that
    the HTML value attribute doesn't always reflect all the text in the field, including all white space.
    
    AX-Relnotes: Fixed regression with reading in the address bar and other text fields
    in Views, e.g. Add Bookmark dialog. (Regression found only in Canary.)
    
    R=aleventhal@chromium.org, dmazzoni@chromium.org, msw@chromium.org
    
    Change-Id: I664654beb16585fe555424d3f2e564da1084aed7
    Bug: 1098056
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264559
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Auto-Submit: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783430}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -331,6 +331,12 @@ Textfield::Textfield()
   AddAccelerator(ui::Accelerator(ui::VKEY_C, ui::EF_CONTROL_DOWN));
   AddAccelerator(ui::Accelerator(ui::VKEY_V, ui::EF_CONTROL_DOWN));
 #endif
+
+  // Sometimes there are additional ignored views, such as the View representing
+  // the cursor, inside the text field. These should always be ignored by
+  // accessibility since a plain text field should always be a leaf node in the
+  // accessibility trees of all the platforms we support.
+  GetViewAccessibility().OverrideIsLeaf(true);
 }
 
 Textfield::~Textfield() {
@@ -1028,6 +1034,7 @@ void Textfield::OnDragDone() {
 
 void Textfield::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   node_data->role = ax::mojom::Role::kTextField;
+
   if (label_ax_id_) {
     node_data->AddIntListAttribute(ax::mojom::IntListAttribute::kLabelledbyIds,
                                    {label_ax_id_});
