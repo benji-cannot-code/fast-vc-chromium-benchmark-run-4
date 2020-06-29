15e66af41ceb36ba949dc3c77194aae2ab648af4
edchin <edchin@chromium.org>
2020-06-29 13:39:23 +0000

    [ios] Add URL drop interaction to NTP
    Supports dropping (from dragging) a URL onto anywhere
    on the NTP to navigate the tab to the URL.
    
    This is behind a feature flag.
    
    Bug: 1099462
    Change-Id: Ica064a57cdb8fe2a275fd738e5e2ba1c5c29f9b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268878
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783506}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -16,6 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/ntp_tiles/most_visited_sites.h"
 #include "components/prefs/pref_service.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/drag_and_drop/drag_and_drop_flag.h"
+#import "ios/chrome/browser/drag_and_drop/drop_and_navigate_delegate.h"
+#import "ios/chrome/browser/drag_and_drop/drop_and_navigate_interaction.h"
 #include "ios/chrome/browser/favicon/ios_chrome_large_icon_cache_factory.h"
 #include "ios/chrome/browser/favicon/ios_chrome_large_icon_service_factory.h"
 #include "ios/chrome/browser/favicon/large_icon_cache.h"
@@ -50,6 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/voice/voice_search_availability.h"
 #import "ios/public/provider/chrome/browser/chrome_browser_provider.h"
 #import "ios/public/provider/chrome/browser/discover_feed/discover_feed_provider.h"
+#import "ios/web/public/navigation/navigation_manager.h"
+#import "ios/web/public/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -57,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface ContentSuggestionsCoordinator () <
     ContentSuggestionsViewControllerAudience,
+    DropAndNavigateDelegate,
     OverscrollActionsControllerDelegate> {
   // Helper object managing the availability of the voice search feature.
   VoiceSearchAvailability _voiceSearchAvailability;
@@ -232,6 +238,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                       headerController:self.headerController];
   self.NTPMediator.headerCollectionInteractionHandler =
       self.headerCollectionInteractionHandler;
+
+  if (DragAndDropIsEnabled()) {
+    [self.suggestionsViewController.collectionView
+        addInteraction:[[DropAndNavigateInteraction alloc]
+                           initWithDelegate:self]];
+  }
 }
 
 - (void)stop {
@@ -314,6 +326,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (OverscrollActionsController*)controller {
   // Fullscreen isn't supported here.
   return nullptr;
+}
+
+#pragma mark - DropAndNavigateDelegate
+
+- (void)URLWasDropped:(GURL const&)URL {
+  web::NavigationManager::WebLoadParams params(URL);
+  params.transition_type = ui::PAGE_TRANSITION_TYPED;
+  self.webState->GetNavigationManager()->LoadURLWithParams(params);
 }
 
 #pragma mark - Public methods
