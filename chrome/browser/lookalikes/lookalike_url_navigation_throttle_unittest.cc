2e7e0cac59238a53cda7607f3728245a326aeb65
Mustafa Emre Acer <meacer@chromium.org>
2020-06-29 23:13:07 +0000

    Show an interstitial for most spoof check results
    This CL displays the lookalike interstitial for remaining IDN spoof
    check results with a few exceptions, as described in the comments.
    
    Bug: 1095120
    Change-Id: I13c2fb2517b8c5104ef7902b15d11c9531b2c672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2271166
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783728}

--- chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
+++ chrome/browser/lookalikes/lookalike_url_navigation_throttle_unittest.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/lookalikes/core/features.h"
+#include "components/url_formatter/spoof_checks/idn_spoof_checker.h"
+#include "components/url_formatter/url_formatter.h"
 #include "content/public/test/mock_navigation_handle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -91,18 +93,42 @@ TEST_F(LookalikeThrottleTest, SpoofsBlocked) {
   const struct TestCase {
     const char* hostname;
     bool expected_blocked;
+    url_formatter::IDNSpoofChecker::Result expected_spoof_check_result;
   } kTestCases[] = {
       // ASCII private domain.
-      {"private.hostname", false},
-      // Unsafe middle dot.
-      {"example·com.com", true},
-      // Fails ICU spoof checks.
-      {"lɔlocked.com", true},
-      // Contains a TLD specific character
-      {"þook.com", true},
-      // Also fails ICU spoof checks, but is allowed because consists of only
+      {"private.hostname", false,
+       url_formatter::IDNSpoofChecker::Result::kNone},
+
+      // lɔlocked.com, fails ICU spoof checks.
+      {"xn--llocked-9bd.com", true,
+       url_formatter::IDNSpoofChecker::Result::kICUSpoofChecks},
+      // þook.com, contains a TLD specific character (þ).
+      {"xn--ook-ooa.com", true,
+       url_formatter::IDNSpoofChecker::Result::kTLDSpecificCharacters},
+      // example·com.com, unsafe middle dot.
+      {"xn--examplecom-rra.com", true,
+       url_formatter::IDNSpoofChecker::Result::kUnsafeMiddleDot},
+      // scope.com, with scope in Cyrillic. Whole script confusable.
+      {"xn--e1argc3h.com", true,
+       url_formatter::IDNSpoofChecker::Result::kWholeScriptConfusable},
+      //  Non-ASCII Latin with Non-Latin character
+      {"xn--caf-dma9024xvpg.kr", true,
+       url_formatter::IDNSpoofChecker::Result::
+           kNonAsciiLatinCharMixedWithNonLatin},
+      // testーsite.com, has dangerous pattern (ー is CJK character).
+      {"xn--testsite-1g5g.com", true,
+       url_formatter::IDNSpoofChecker::Result::kDangerousPattern},
+
+      // TODO(crbug.com/1100485): Add an example for digit lookalikes.
+
+      // 🍕.com, fails ICU spoof checks, but is allowed because consists of only
       // emoji and ASCII.
-      {"🍕.com", false},
+      {"xn--vi8h.com", false,
+       url_formatter::IDNSpoofChecker::Result::kICUSpoofChecks},
+      // sparkasse-gießen.de, has a deviation character (ß). This is in punycode
+      // because GURL canonicalizes ß to ss.
+      {"xn--sparkasse-gieen-2ib.de", false,
+       url_formatter::IDNSpoofChecker::Result::kDeviationCharacters},
   };
 
   base::test::ScopedFeatureList feature_list;
@@ -110,6 +136,12 @@ TEST_F(LookalikeThrottleTest, SpoofsBlocked) {
       lookalikes::features::kLookalikeInterstitialForPunycode);
 
   for (const TestCase& test_case : kTestCases) {
+    url_formatter::IDNConversionResult idn_result =
+        url_formatter::UnsafeIDNToUnicodeWithDetails(test_case.hostname);
+    ASSERT_EQ(test_case.expected_spoof_check_result,
+              idn_result.spoof_check_result)
+        << test_case.hostname;
+
     GURL url(std::string("http://") + test_case.hostname);
     content::MockNavigationHandle handle(url, main_rfh());
     handle.set_page_transition(ui::PAGE_TRANSITION_TYPED);
