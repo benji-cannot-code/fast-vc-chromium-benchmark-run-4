9a41ae3c1189de4f4619a11b324237e39166c2e6
edchin <edchin@chromium.org>
2020-06-29 19:03:52 +0000

    [ios] Use VisibleURL for drag and drop
    Using LastCommittedURL returns an empty URL during session
    restoration. Use VisibleURL for drag items.
    
    Bug: 1100398
    Change-Id: Ifc7bbd6c4e1f48b5065a0e462fa13c387c6e9567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2271162
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783620}

--- ios/chrome/browser/drag_and_drop/drag_item_util.mm
+++ ios/chrome/browser/drag_and_drop/drag_item_util.mm
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 UIDragItem* CreateTabDragItem(web::WebState* web_state) {
   DCHECK(web_state);
-  NSURL* url = net::NSURLWithGURL(web_state->GetLastCommittedURL());
+  NSURL* url = net::NSURLWithGURL(web_state->GetVisibleURL());
   NSItemProvider* item_provider = [[NSItemProvider alloc] initWithObject:url];
   UIDragItem* drag_item =
       [[UIDragItem alloc] initWithItemProvider:item_provider];
