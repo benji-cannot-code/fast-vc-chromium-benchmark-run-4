73d28b3ea4a396813a2e98545c361ed26f01553d
Swapnil <swapnilgupta@google.com>
2020-06-29 15:09:00 +0000

    Update field names to sync with server side
    The request to the server side fails because of incorrect field names.
    This causes the events for ARC++ and policy based extensions to fail
    to upload. Hence, to sync with the server side, the field entries
    are updated.
    
    Bug: 1100264
    Change-Id: Ic66ca06ca2755c6415ea654dc2212e6a07f98b00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270556
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Swapnil Gupta <swapnilgupta@google.com>
    Cr-Commit-Position: refs/heads/master@{#783528}

--- components/policy/core/common/cloud/realtime_reporting_job_configuration.cc
+++ components/policy/core/common/cloud/realtime_reporting_job_configuration.cc
@@ -22,7 +22,7 @@ namespace em = enterprise_management;
 namespace policy {
 
 const char RealtimeReportingJobConfiguration::kContextKey[] = "context";
-const char RealtimeReportingJobConfiguration::kEventListKey[] = "eventList";
+const char RealtimeReportingJobConfiguration::kEventListKey[] = "events";
 
 const char RealtimeReportingJobConfiguration::kBrowserIdKey[] =
     "browser.browserId";
