a5276c1a9b1ac07f84775457e750688b163eb9a9
Antonio Gomes <tonikitoo@igalia.com>
2020-06-29 11:20:48 +0000

    Remove WebMediaStreamTrack::Initialize()
    This CL starts of the next phase of the clean up effort
    on the MediaStream and PeerConnection classes: reducing the API
    exposed in the wrapper Blink/Media API classes.
    
    Particularly, this CL removes the WebMediaStreamTrack::Initialize()
    overloaded methods, since no code out of blink/renderer uses them anymore.
    For this, the remaining two callsites are changed to call
    MediaStreamComponent and MediaStreamSource methods directly instead.
    
    BUG=704136
    R=guidou@chromium.org
    
    Change-Id: I228c781414153d5f5b2ad8fae6ce696876e6879a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264862
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783480}

--- third_party/blink/public/platform/web_media_stream_track.h
+++ third_party/blink/public/platform/web_media_stream_track.h
@@ -109,10 +109,6 @@ class WebMediaStreamTrack {
   }
   BLINK_PLATFORM_EXPORT void Assign(const WebMediaStreamTrack&);
 
-  BLINK_PLATFORM_EXPORT void Initialize(const WebMediaStreamSource&);
-  BLINK_PLATFORM_EXPORT void Initialize(const WebString& id,
-                                        const WebMediaStreamSource&);
-
   BLINK_PLATFORM_EXPORT void Reset();
   bool IsNull() const { return private_.IsNull(); }
 
