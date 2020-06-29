13779b49019656d2904122699bace0befb81c12a
Xianzhu Wang <wangxianzhu@chromium.org>
2020-06-29 16:38:53 +0000

    Use unaliased blink effect node when populating cc effect node stable_id
    Bug: 1095167
    Change-Id: I66081188bccb9b89c1f852fd8ea8e9375389cb88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2272118
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783567}

--- third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.cc
+++ third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.cc
@@ -1082,8 +1082,7 @@ void PropertyTreeManager::BuildEffectNodesRecursively(
     effect_node.clip_id = output_clip_id;
     effect_node.transform_id =
         EnsureCompositorTransformNode(next_effect.LocalTransformSpace());
-    effect_node.stable_id =
-        next_effect_arg.GetCompositorElementId().GetStableId();
+    effect_node.stable_id = next_effect.GetCompositorElementId().GetStableId();
   }
 
   if (!has_multiple_groups)
