518afc952d855f28e495a5cc95f98d7a88d6e8e2
Moe Ahmadi <mahmadi@chromium.org>
2020-06-29 19:10:32 +0000

    [NTP] Bumps expiry milestones for NTP and Realbox related flags to M86
    This is to allow the test and eng teams to continue to control those
    features from chrome://flags until they are fully launched.
    
    Bug: 1100319, 1100305
    Change-Id: I286c8c9cc2b1522b340ff4c4a661ee32aee7f8aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2273804
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Esmael Elmoslimany <aee@chromium.org>
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783623}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1710,9 +1710,6 @@ extern const char kEnableAccessibilityLiveCaptionsDescription[];
 extern const char kCastMediaRouteProviderName[];
 extern const char kCastMediaRouteProviderDescription[];
 
-extern const char kNtpConfirmSuggestionRemovalsName[];
-extern const char kNtpConfirmSuggestionRemovalsDescription[];
-
 extern const char kNtpDismissPromosName[];
 extern const char kNtpDismissPromosDescription[];
 
