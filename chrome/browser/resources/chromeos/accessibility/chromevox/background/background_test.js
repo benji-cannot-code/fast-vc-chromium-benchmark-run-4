806c268a59533c3143a9842396b047d7bec3d16e
David Tseng <dtseng@chromium.org>
2020-06-29 21:32:39 +0000

    Centralize all pointer-type output in BackgroundMouseHandler
    This is meant as a purely refactoring change and should have no functional user change.
    
    R=akihiroota@chromium.org
    
    Bug: none
    Test: browser_tests --gtest_filter=ChromeVoxBackground*.*HitTest*
    Change-Id: Id361780aeb90545e65f28f7ee3de09160ac8adcf
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270842
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783680}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/background_test.js
@@ -17,7 +17,7 @@ ChromeVoxBackgroundTest = class extends ChromeVoxNextE2ETest {
     window.EventType = chrome.automation.EventType;
     window.RoleType = chrome.automation.RoleType;
     window.doCmd = this.doCmd;
-    window.doHover = this.doHover;
+    window.simulateHitTestResult = this.simulateHitTestResult;
     window.press = this.press;
     window.Mod = constants.ModifierFlag;
 
@@ -45,10 +45,9 @@ ChromeVoxBackgroundTest = class extends ChromeVoxNextE2ETest {
     };
   }
 
-  doHover(node) {
+  simulateHitTestResult(node) {
     return () => {
-      const event = new CustomAutomationEvent(EventType.HOVER, node, 'action');
-      DesktopAutomationHandler.instance.onHover(event);
+      BackgroundMouseHandler.instance.handleHitTestResult(node);
     };
   }
 
@@ -1938,7 +1937,7 @@ TEST_F('ChromeVoxBackgroundTest', 'HoverTargetsLeafNode', function() {
         assertNotNullNorUndefined(buttonP);
         const buttonText = buttonP.firstChild;
         assertNotNullNorUndefined(buttonText);
-        mockFeedback.call(doHover(buttonText))
+        mockFeedback.call(simulateHitTestResult(buttonText))
             .expectSpeech('Jefferson')
             .expectSpeech('Button')
             .replay();
@@ -2620,7 +2619,7 @@ TEST_F('ChromeVoxBackgroundTest', 'HitTestOnExoSurface', function() {
 });
 
 TEST_F('ChromeVoxBackgroundTest', 'HoverSkipsContainers', function() {
-  DesktopAutomationHandler.MIN_HOVER_EXIT_SOUND_DELAY_MS = -1;
+  BackgroundMouseHandler.MIN_HOVER_EXIT_SOUND_DELAY_MS = -1;
   const mockFeedback = this.createMockFeedback();
   this.runWithLoadedTree(
       `
@@ -2641,7 +2640,7 @@ TEST_F('ChromeVoxBackgroundTest', 'HoverSkipsContainers', function() {
         assertNotNullNorUndefined(button);
         const group = button.parent;
         assertNotNullNorUndefined(group);
-        mockFeedback.call(doHover(button))
+        mockFeedback.call(simulateHitTestResult(button))
             .expectSpeech('Button')
             .call(() => {
               // Override the role to simulate panes which are only found in
@@ -2652,10 +2651,10 @@ TEST_F('ChromeVoxBackgroundTest', 'HoverSkipsContainers', function() {
                 }
               });
             })
-            .call(doHover(group))
+            .call(simulateHitTestResult(group))
             .expectSpeech('range cleared!')
             .expectEarcon(Earcon.TOUCH_EXIT)
-            .call(doHover(group))
+            .call(simulateHitTestResult(group))
             .expectSpeech('range cleared!')
             .expectEarcon(Earcon.TOUCH_EXIT)
             .replay();
