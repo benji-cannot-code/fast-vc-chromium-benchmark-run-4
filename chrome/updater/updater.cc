91690df3b06145735d9dfef1cfe39e9cf8a708a0
Sorin Jianu <sorin@chromium.org>
2020-06-29 23:48:47 +0000

    Rename InstallApp (and similar symbols) to AppInstall.
    This is a lexical change to be consistent with the naming for the
    other App entities which follow the pattern App*.
    
    Bug: 1060807
    Change-Id: I94e2d5748c56b38f380078c831c453361150713d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274589
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783743}

--- chrome/updater/updater.cc
+++ chrome/updater/updater.cc
@@ -24,13 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "chrome/updater/app/server/win/server.h"
 #include "chrome/updater/app/server/win/service_main.h"
-#include "chrome/updater/win/install_app.h"
+#include "chrome/updater/win/app_install.h"
 #endif
 
 #if defined(OS_MACOSX)
 #include "chrome/updater/app/server/mac/server.h"
+#include "chrome/updater/mac/setup/app_install.h"
 #include "chrome/updater/mac/setup/app_swap.h"
-#include "chrome/updater/mac/setup/install_app.h"
 #endif
 
 // Instructions For Windows.
--- chrome/updater/win/app_install.h
+++ chrome/updater/win/app_install.h
@@ -1,10 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2020 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_UPDATER_MAC_SETUP_INSTALL_APP_H_
-#define CHROME_UPDATER_MAC_SETUP_INSTALL_APP_H_
+#ifndef CHROME_UPDATER_WIN_APP_INSTALL_H_
+#define CHROME_UPDATER_WIN_APP_INSTALL_H_
+
+#include <string>
 
 #include "base/memory/scoped_refptr.h"
 
@@ -16,4 +18,4 @@ scoped_refptr<App> MakeAppInstall();
 
 }  // namespace updater
 
-#endif  // CHROME_UPDATER_MAC_SETUP_INSTALL_APP_H_
+#endif  // CHROME_UPDATER_WIN_APP_INSTALL_H_
