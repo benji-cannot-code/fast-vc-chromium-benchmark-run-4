90069c11457d7a8e4130c633c90f6289456bb1ac
Abhijeet Singh <siabhijeet@google.com>
2020-06-29 22:38:29 +0000

    Update announcements for Quick Answers
    The announcements and alerts for the Quick-Answers related views are
    updated to be able to better assist the feature users and guide them
    through the widget when the screen-reader is turned on.
    
    Bug: b:152059023
    Test: Tested on Atlas test device.
    Change-Id: I243993a560ed0499ec44a87181ba0e1dd7f859cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2271164
    Commit-Queue: Abhijeet Singh <siabhijeet@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783709}

--- ash/quick_answers/ui/user_consent_view.h
+++ ash/quick_answers/ui/user_consent_view.h
@@ -47,6 +47,7 @@ class UserConsentView : public views::View, public views::ButtonListener {
   gfx::Size CalculatePreferredSize() const override;
   void OnFocus() override;
   views::FocusTraversable* GetPaneFocusTraversable() override;
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
 
   // views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
