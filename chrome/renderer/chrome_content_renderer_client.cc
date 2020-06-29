ec7aed2f15db7f3c7d7f735b8dc39aa8b12e5990
Etienne Pierre-doray <etiennep@chromium.org>
2020-06-29 20:24:52 +0000

    [tracing]: Add v8 unwinder to tracing sampler.
    Track global pointer to main thread sampler to access from
    ChromeContentRendererClient.
    
    Bug: 1077046
    Change-Id: Icf575c7a03905c844ce2bd8065129a0df341a9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251087
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Reviewed-by: oysteine <oysteine@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783651}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -129,6 +129,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "printing/buildflags/buildflags.h"
 #include "services/network/public/cpp/is_potentially_trustworthy.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
+#include "services/tracing/public/cpp/stack_sampling/tracing_sampler_profiler.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/common/privacy_budget/identifiability_study_settings.h"
@@ -356,6 +357,11 @@ void ChromeContentRendererClient::RenderThreadStarted() {
 
   main_thread_profiler_->SetAuxUnwinderFactory(base::BindRepeating(
       &CreateV8Unwinder, base::Unretained(v8::Isolate::GetCurrent())));
+
+  // In the case of single process mode, the v8 unwinding will not work.
+  tracing::TracingSamplerProfiler::SetAuxUnwinderFactoryOnMainThread(
+      base::BindRepeating(&CreateV8Unwinder,
+                          base::Unretained(v8::Isolate::GetCurrent())));
 
   thread->SetRendererProcessType(
       IsStandaloneContentExtensionProcess()
