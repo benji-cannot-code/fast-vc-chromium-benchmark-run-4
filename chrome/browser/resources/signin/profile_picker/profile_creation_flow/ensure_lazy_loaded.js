ee4bced1e2e464a309291c1f8f961a9b34c4ccfc
Monica Basta <msalama@chromium.org>
2020-06-29 18:13:29 +0000

    [ProfilePicker]: Add navigation mechanism to the profile picker.
    This CL does the following:
    (1) Adds a navigation mechanism for the different screens of the
    picker:
    - Main view to pick, edit, add profile.
    - Profile creation flow: profile type choice, local customization,
    signin. The view to be shown in the first step of
    'chrome://profile-picker/new-profile' is not fixed and will depend on a
    number of factors.
    
    (2) Creates one of the profile creation views for the new profile type
    choice (Signed In or Local). The profile creation flow should be lazy
    loaded since the picker is shown on startup and the creation flow is
    only needed if the user decides to create a new space/profile.
    
    Bug: 1063856
    Change-Id: Ic59fb34ff6a9fc8fbfcb087c22cdfb3f8c9e689b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2260616
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783601}

---
+++ chrome/browser/resources/signin/profile_picker/profile_creation_flow/ensure_lazy_loaded.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @type {?Promise<void>} */
+let lazyLoadPromise = null;
+
+/** @return {!Promise<void>} Resolves when the lazy load module is imported. */
+export function ensureLazyLoaded() {
+  if (!lazyLoadPromise) {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = './profile_creation_flow/lazy_load.js';
+    document.body.appendChild(script);
+
+    lazyLoadPromise = Promise.all([
+      'profile-type-choice',
+    ].map(name => customElements.whenDefined(name)));
+  }
+  return lazyLoadPromise;
+}
