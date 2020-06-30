8323c204aeec165bc8a7fac6bdd73753880c9e0b
Parastoo Geranmayeh <parastoog@google.com>
2020-06-30 16:22:05 +0000

    Revert "[PTZ] Measure prevalence of cameras supporting PTZ"
    This reverts commit 56bfd87f8f3e96e42f79b39b602d1380faaea47b.
    
    Reason for revert: causing crbug/1100948
    
    Original change's description:
    > [PTZ] Measure prevalence of cameras supporting PTZ
    > 
    > This CL adds metrics for how many users have cameras with PTZ
    > capabilities. This metric is triggered only when a permission prompt
    > would show up.
    > 
    > Bug: 990185
    > Change-Id: Icba513c14977ef1249ed68788fc91d5059b1c0c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2273237
    > Commit-Queue: François Beaufort <beaufort.francois@gmail.com>
    > Reviewed-by: Alex Ilin <alexilin@chromium.org>
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#783901}
    
    TBR=beaufort.francois@gmail.com,reillyg@chromium.org,guidou@chromium.org,alexilin@chromium.org
    
    Change-Id: I20ad67145572820ef146e8c4c91ac50c5e41e308
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 990185, 1100948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276392
    Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    Commit-Queue: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/master@{#783990}

--- components/webrtc/media_stream_devices_controller.cc
+++ components/webrtc/media_stream_devices_controller.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/bind.h"
-#include "base/metrics/histogram_functions.h"
 #include "components/permissions/permission_manager.h"
 #include "components/permissions/permission_result.h"
 #include "components/permissions/permissions_client.h"
@@ -114,15 +113,13 @@ void MediaStreamDevicesController::RequestPermissions(
     will_prompt_for_video =
         permission_status.content_setting == CONTENT_SETTING_ASK;
 
-    bool has_pan_tilt_zoom_camera = controller->HasAvailableDevices(
-        ContentSettingsType::CAMERA_PAN_TILT_ZOOM,
-        request.requested_video_device_id);
-    base::UmaHistogramBoolean("WebRTC.MediaStreamDevices.HasPanTiltZoomCamera",
-                              has_pan_tilt_zoom_camera);
     // Request CAMERA_PAN_TILT_ZOOM only if the the website requested
     // the pan-tilt-zoom permission and there are suitable PTZ capable devices
     // available.
-    if (request.request_pan_tilt_zoom_permission && has_pan_tilt_zoom_camera) {
+    if (request.request_pan_tilt_zoom_permission &&
+        controller->HasAvailableDevices(
+            ContentSettingsType::CAMERA_PAN_TILT_ZOOM,
+            request.requested_video_device_id)) {
       permissions::PermissionResult permission_status =
           permission_manager->GetPermissionStatusForFrame(
               ContentSettingsType::CAMERA_PAN_TILT_ZOOM, rfh,
