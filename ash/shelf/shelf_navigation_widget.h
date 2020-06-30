00d8214f849e4b78d695c1f68791cc80d2d51f52
Toni Barzic <tbarzic@chromium.org>
2020-06-30 23:05:29 +0000

    Do not restart shelf nav widget animations with same target values
    Shelf navigation widget layout may get updated while the previous
    animation is still in progress. Do not restart the animation if the
    target opacity or bounds do not change.
    
    While here, do not update shelf navigation layout from shelf config
    observer. Shelf layout manager handles shelf config changes, and will
    call UpdateLayout as necessary (so calls from the shelf navigation
    widget would be duplicative).
    
    BUG=1101084
    
    Change-Id: I3a89847ebfe63e69fe869623e58c58b35c38d966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276654
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784194}

--- ash/shelf/shelf_navigation_widget.h
+++ ash/shelf/shelf_navigation_widget.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
-#include "ash/public/cpp/shelf_config.h"
 #include "ash/shelf/shelf_component.h"
 #include "ui/views/accessible_pane_view.h"
 #include "ui/views/widget/widget.h"
@@ -33,7 +32,6 @@ class ShelfView;
 // The shelf navigation widget holds the home button and (when in tablet mode)
 // the back button.
 class ASH_EXPORT ShelfNavigationWidget : public ShelfComponent,
-                                         public ShelfConfig::Observer,
                                          public views::Widget {
  public:
   class TestApi {
@@ -80,9 +78,6 @@ class ASH_EXPORT ShelfNavigationWidget : public ShelfComponent,
   // Sets whether the last focusable child (instead of the first) should be
   // focused when activating this widget.
   void SetDefaultLastFocusableChild(bool default_last_focusable_child);
-
-  // ShelfConfig::Observer:
-  void OnShelfConfigUpdated() override;
 
   // ShelfComponent:
   void CalculateTargetBounds() override;
