08b9d45558f6fb6d3158514fd4c24399277591d8
Stephan Hartmann <stha09@googlemail.com>
2020-06-30 15:58:38 +0000

    IWYU: add includes for uint64_t and std::string
    Bug: 819294
    Change-Id: Iae844f8b6cf6bba879cb320c95ebf88f65e1385f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2273161
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783978}

--- components/federated_learning/sim_hash.h
+++ components/federated_learning/sim_hash.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_FEDERATED_LEARNING_SIM_HASH_H_
 #define COMPONENTS_FEDERATED_LEARNING_SIM_HASH_H_
 
+#include <stdint.h>
 #include <set>
+#include <string>
 #include <unordered_set>
 
 namespace federated_learning {
