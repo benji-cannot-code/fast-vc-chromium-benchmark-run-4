ff46e52b4a10186a4a598170de53e40ac476182e
Greg Daniel <egdaniel@google.com>
2020-06-30 15:14:38 +0000

    Update skia flush call to latest API.
    This removes the need to set kNone for the backend access on each flush
    as well as add some uses of the default flush() call instead of passing
    in empty GrFlushInfo.
    
    Change-Id: I65717f8f7b5d51d6a6c18f9a3e548b5b335bda49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276171
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Cr-Commit-Position: refs/heads/master@{#783958}

--- gpu/vulkan/demo/vulkan_demo.cc
+++ gpu/vulkan/demo/vulkan_demo.cc
@@ -200,7 +200,6 @@ void VulkanDemo::RenderFrame() {
   GrBackendSemaphore semaphore;
   semaphore.initVulkan(scoped_write_->GetEndSemaphore());
   GrFlushInfo flush_info = {
-      .fFlags = kNone_GrFlushFlags,
       .fNumSemaphores = 1,
       .fSignalSemaphores = &semaphore,
   };
