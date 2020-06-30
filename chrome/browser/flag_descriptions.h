e2ecac8de59c8dd54a86218cde53aabaab7528d1
Pranav Batra <batrapranav@chromium.org>
2020-06-30 18:37:24 +0000

    Remove expired flag arc-cups-api
    Bug: chromium:832266
    Change-Id: Icc7eef9b97c54e5777493bdf43db0996e9bc03c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274140
    Reviewed-by: Sean Kau <skau@chromium.org>
    Reviewed-by: Jesse Schettler <jschettler@chromium.org>
    Commit-Queue: Jesse Schettler <jschettler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784060}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1904,9 +1904,6 @@ extern const char kAppServiceIntentHandlingDescription[];
 extern const char kArcApplicationZoomName[];
 extern const char kArcApplicationZoomDescription[];
 
-extern const char kArcCupsApiName[];
-extern const char kArcCupsApiDescription[];
-
 extern const char kArcCustomTabsExperimentName[];
 extern const char kArcCustomTabsExperimentDescription[];
 
--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -463,8 +463,6 @@ extern const base::Feature kUserActivityEventLogging;
 #endif
 
 #if defined(OS_CHROMEOS)
-COMPONENT_EXPORT(CHROME_FEATURES) extern const base::Feature kArcCupsApi;
-
 COMPONENT_EXPORT(CHROME_FEATURES) extern const base::Feature kQuickUnlockPin;
 
 COMPONENT_EXPORT(CHROME_FEATURES)
