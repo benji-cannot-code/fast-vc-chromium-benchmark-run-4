e4e55d13fc0178396542bcff7a826043b88567b5
John Z Wu <jzw@chromium.org>
2020-06-30 22:20:30 +0000

    Move "Suggest password..." string into components
    This will be shared between ios/chrome and ios/web_view.
    Also deleted extra space in the string.
    
    Bug: 1097353
    Change-Id: I3bef90ec663d1b092c6dd79e8b9131f4d97688a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2259513
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784175}

--- ios/chrome/browser/passwords/password_controller_unittest.mm
+++ ios/chrome/browser/passwords/password_controller_unittest.mm
@@ -1640,7 +1640,7 @@ TEST_F(PasswordControllerTest, CheckPasswordGenerationSuggestion) {
       @[(@"var evt = document.createEvent('Events');"
          "password_.focus();"),
         @""],
-      @[@"user0 ••••••••", @"abc ••••••••", @"Suggest  Password\u2026"],
+      @[@"user0 ••••••••", @"abc ••••••••", @"Suggest Password\u2026"],
       @"[]=, onkeyup=false, onchange=false"
     },
   };
