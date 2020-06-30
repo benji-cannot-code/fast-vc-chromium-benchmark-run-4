504ad151dd5cc297ab9648e9bd44ed1362d9390f
Richard Knoll <knollr@chromium.org>
2020-06-30 16:31:44 +0000

    [Nearby] Add nearby-progress component
    This component renders an indefinite spinner with a device name below
    to indicate transfer progress. The indefinite state is shown when
    waiting for the user to accept or decline a transfer.
    
    Bug: 1099710
    Change-Id: Ie48b20c7ce04b64e5710670e5d92699dbc6e34f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2272837
    Commit-Queue: Richard Knoll <knollr@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783992}

--- chrome/browser/resources/nearby_share/nearby_confirmation_page.js
+++ chrome/browser/resources/nearby_share/nearby_confirmation_page.js
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * are not currently in the contacts of this user.
  */
 
+import './nearby_progress.js';
+
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 Polymer({
