9d975afba209eec76404e6465d8803d0ced2123e
Eleonora Rocchi <erocchi@google.com>
2020-06-30 14:01:41 +0000

    [PwdCheckAndroid] Show the check passwords button only if the user is signed in.
    This CL adds the code to show the check passwords button only if the user is signed in. It modifies the tests to ensure the button is correctly shown when the feature flag is enabled.
    
    Bug: 1092444
    Change-Id: I06a00048ab5d14c23f1f03381f563a32edbdbd80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270471
    Commit-Queue: Eleonora Rocchi <erocchi@google.com>
    Reviewed-by: Friedrich [CET] <fhorschig@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783936}

--- chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/PasswordSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/PasswordSettingsTest.java
@@ -722,19 +722,38 @@ public class PasswordSettingsTest {
     }
 
     /**
-     * Check that the check passwords preference is shown when the corresponding feature is enabled.
+     * Check that the check passwords preference is shown when the corresponding feature is enabled
+     * and the user is signed in.
      */
     @Test
     @SmallTest
     @Feature({"Preferences"})
     @EnableFeatures(ChromeFeatureList.PASSWORD_CHECK)
-    public void testCheckPasswordsEnabled() {
+    public void testCheckPasswordsEnabledSignedIn() {
+        mBrowserTestRule.addAndSignInTestAccount();
         final SettingsActivity settingsActivity = mSettingsActivityTestRule.startSettingsActivity();
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
             PasswordSettings passwordPrefs = mSettingsActivityTestRule.getFragment();
             Assert.assertNotNull(
                     passwordPrefs.findPreference(PasswordSettings.PREF_CHECK_PASSWORDS));
+        });
+    }
+
+    /**
+     * Check that the check passwords preference is not shown when the corresponding feature is
+     * enabled but the user is not signed in.
+     */
+    @Test
+    @SmallTest
+    @Feature({"Preferences"})
+    @EnableFeatures(ChromeFeatureList.PASSWORD_CHECK)
+    public void testCheckPasswordsEnabledNotSignedIn() {
+        final SettingsActivity settingsActivity = mSettingsActivityTestRule.startSettingsActivity();
+
+        TestThreadUtils.runOnUiThreadBlocking(() -> {
+            PasswordSettings passwordPrefs = mSettingsActivityTestRule.getFragment();
+            Assert.assertNull(passwordPrefs.findPreference(PasswordSettings.PREF_CHECK_PASSWORDS));
         });
     }
 
@@ -747,6 +766,7 @@ public class PasswordSettingsTest {
     @Feature({"Preferences"})
     @DisableFeatures(ChromeFeatureList.PASSWORD_CHECK)
     public void testCheckPasswordsDisabled() {
+        mBrowserTestRule.addAndSignInTestAccount();
         final SettingsActivity settingsActivity = mSettingsActivityTestRule.startSettingsActivity();
 
         TestThreadUtils.runOnUiThreadBlocking(() -> {
