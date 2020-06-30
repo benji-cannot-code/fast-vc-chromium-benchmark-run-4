ebdc446860215d27a1a45e8809267c3b556786c4
Donn Denman <donnd@chromium.org>
2020-06-30 22:58:21 +0000

    [TTS] Disable clock sanity checks: for flaky bots.
    Disables sanity checks on clock times that are primarily used
    as a signal for Tap suppression.
    
    This should help bots with flaky clocks or ones that are not set
    correctly to still pass tests.
    
    BUG = 1094008, 1101070
    
    Change-Id: Idd7d019cacd77b987b95c3a082a6bbaffb3c7a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276676
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Donn Denman <donnd@chromium.org>
    Auto-Submit: Donn Denman <donnd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784191}

--- components/contextual_search/core/browser/weekly_activity_storage.cc
+++ components/contextual_search/core/browser/weekly_activity_storage.cc
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Used for validation in debug build.  Week numbers are > 2300 as of year 2016.
-const int kReasonableMinWeek = 2000;
+// TODO(donnd): reenable this const.  https://crbug.com/1094008. See below.
+// const int kReasonableMinWeek = 2000;
 
 }  // namespace
 
@@ -61,7 +62,9 @@ int WeeklyActivityStorage::GetWeekRemainder(int which_week) {
 }
 
 void WeeklyActivityStorage::EnsureHasActivity(int which_week) {
-  DCHECK(which_week > kReasonableMinWeek);
+  // TODO(donnd): reenable this DCHECK.  Some bots have bad clocks or time
+  // settings, causing flaky test failures. https://crbug.com/1094008.
+  // DCHECK(which_week > kReasonableMinWeek);
 
   // If still on the newest week we're done!
   int newest_week = ReadNewestWeekWritten();
