75d1f5bdbb9d7fd3e7c4b36f2f7e0e35b11e44a6
Sasha McIntosh <sashamcintosh@chromium.org>
2020-06-30 20:47:41 +0000

    cc: Fix retention on YUV planes in PaintImage
    When an SkImage is created with YUV planes, the caller is responsible
    for lifetime of those planes. This change ensures that texture backed
    PaintImages maintain a reference to the YUV planes |sk_planes_| used to
    generate an SkImage as well as the |sk_image_| itself.
    
    Bug: 1091347
    Change-Id: If253b3f4d047432dc4ab48d99563168ee7d5ea1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264259
    Commit-Queue: Sasha McIntosh <sashamcintosh@chromium.org>
    Reviewed-by: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784119}

--- cc/paint/image_transfer_cache_entry.cc
+++ cc/paint/image_transfer_cache_entry.cc
@@ -25,6 +25,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 namespace {
+struct Context {
+  const std::vector<sk_sp<SkImage>> sk_planes_;
+};
+
+void ReleaseContext(SkImage::ReleaseContext context) {
+  auto* texture_context = static_cast<Context*>(context);
+  delete texture_context;
+}
 
 // Creates a SkImage backed by the YUV textures corresponding to |plane_images|.
 // The layout is specified by |plane_images_format|). The backend textures are
@@ -82,10 +90,11 @@ sk_sp<SkImage> MakeYUVImageFromUploadedPlanes(
     return nullptr;
   }
   plane_indices[SkYUVAIndex::kA_Index] = {-1, SkColorChannel::kR};
+  Context* ctx = new Context{plane_images};
   sk_sp<SkImage> image = SkImage::MakeFromYUVATextures(
       context, yuv_color_space, plane_backend_textures.data(), plane_indices,
       plane_images[0]->dimensions(), kTopLeft_GrSurfaceOrigin,
-      std::move(image_color_space));
+      std::move(image_color_space), ReleaseContext, ctx);
   if (!image) {
     DLOG(ERROR) << "Could not create YUV image";
     return nullptr;
