409180e3a66b0812db557f9396f22caf6fd450b1
kylechar <kylechar@chromium.org>
2020-06-30 15:18:35 +0000

    Fix flaky CopyOutRequest tests
    SkiaCopyOutputScalingPixelTest.ScaledCopyOfDrawnFrame has been flaky
    since https://crrev.com/c/2256225 landed. This change made it so that
    CopyOutputResults were serialized on worker threads, which breaks the
    ordering the test expects. Switch to using the test main thread to run
    the result callbacks to remove data race on |dummy_ran|.
    
    Bug: 1100421
    Change-Id: Id19df859ed6af48cce85c16689122fa33e171184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274257
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#783960}

--- components/viz/service/display/copy_output_scaling_pixeltest.cc
+++ components/viz/service/display/copy_output_scaling_pixeltest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/run_loop.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "build/build_config.h"
 #include "cc/test/pixel_test.h"
 #include "cc/test/pixel_test_utils.h"
@@ -146,6 +147,8 @@ class CopyOutputScalingPixelTest
       // results of the main copy request (below) if the GL state is not
       // properly restored.
       request->SetUniformScaleRatio(1, 10);
+      // Ensure the result callback is run on test main thread.
+      request->set_result_task_runner(base::SequencedTaskRunnerHandle::Get());
       list.front()->copy_requests.push_back(std::move(request));
 
       // Add a copy request to the root RenderPass, to capture the results of
@@ -165,6 +168,8 @@ class CopyOutputScalingPixelTest
       request->set_result_selection(
           copy_output::ComputeResultRect(copy_rect, scale_from_, scale_to_));
       request->SetScaleRatio(scale_from_, scale_to_);
+      // Ensure the result callback is run on test main thread.
+      request->set_result_task_runner(base::SequencedTaskRunnerHandle::Get());
       list.back()->copy_requests.push_back(std::move(request));
 
       renderer()->DecideRenderPassAllocationsForFrame(list);
