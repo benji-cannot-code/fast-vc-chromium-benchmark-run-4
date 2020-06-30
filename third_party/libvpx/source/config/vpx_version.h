d3217a5fa94f13b73e7b2583668070d8a13d715b
Marco Paniconi <marpan@google.com>
2020-06-30 21:08:28 +0000

    Roll src/third_party/libvpx/source/libvpx/ 769129fb2..de4aedaec (5 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/769129fb29fc..de4aedaec33c
    
    $ git log 769129fb2..de4aedaec --date=short --no-merges --format='%ad %ae %s'
    2020-06-26 marpan vp9-svc: Fix to setting frame size for dynamic resize
    2020-06-23 marpan vp9-svc: Allow scale_references for single layer svc
    2020-06-18 jzern tools/lint-hunks.py: skip third_party files
    2020-06-17 jzern update googletest to v1.10.0
    2020-06-18 jzern vp9_skip_loopfilter_test: make Init() return a bool
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    
    Change-Id: Ib7048b0c6d3445637799d1a73868d1d41f2cc880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276343
    Reviewed-by: James Zern <jzern@google.com>
    Commit-Queue: Marco Paniconi <marpan@google.com>
    Cr-Commit-Position: refs/heads/master@{#784138}

--- third_party/libvpx/source/config/vpx_version.h
+++ third_party/libvpx/source/config/vpx_version.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 8
 #define VERSION_PATCH 2
-#define VERSION_EXTRA "202-g769129fb2"
+#define VERSION_EXTRA "209-gde4aedaec"
 #define VERSION_PACKED \
   ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | (VERSION_PATCH))
-#define VERSION_STRING_NOSP "v1.8.2-202-g769129fb2"
-#define VERSION_STRING " v1.8.2-202-g769129fb2"
+#define VERSION_STRING_NOSP "v1.8.2-209-gde4aedaec"
+#define VERSION_STRING " v1.8.2-209-gde4aedaec"
