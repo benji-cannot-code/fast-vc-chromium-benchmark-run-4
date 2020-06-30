1a5f86a4f4b735e10f1307d977c7b21e2f10b327
Guillaume Jenkins <gujen@google.com>
2020-06-30 15:25:40 +0000

    [iOS CBCM] Move ReportUploader to components
    Moves ReportUploader and its unit tests to components/enterprise so it
    can be reused by iOS code.
    
    Bug: 1091921
    Change-Id: I425c587a45c8f25d12c16303a2731b6ac53ad158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274582
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Asanka Herath <asanka@chromium.org>
    Commit-Queue: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/master@{#783966}

--- components/enterprise/browser/reporting/report_uploader.h
+++ components/enterprise/browser/reporting/report_uploader.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ENTERPRISE_REPORTING_REPORT_UPLOADER_H_
-#define CHROME_BROWSER_ENTERPRISE_REPORTING_REPORT_UPLOADER_H_
+#ifndef COMPONENTS_ENTERPRISE_BROWSER_REPORTING_REPORT_UPLOADER_H_
+#define COMPONENTS_ENTERPRISE_BROWSER_REPORTING_REPORT_UPLOADER_H_
 
 #include <memory>
 #include <queue>
@@ -100,4 +100,4 @@ enum ReportResponseMetricsStatus {
 
 }  // namespace enterprise_reporting
 
-#endif  // CHROME_BROWSER_ENTERPRISE_REPORTING_REPORT_UPLOADER_H_
+#endif  // COMPONENTS_ENTERPRISE_BROWSER_REPORTING_REPORT_UPLOADER_H_
