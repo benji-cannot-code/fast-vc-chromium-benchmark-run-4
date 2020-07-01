b11d3226a5d15e90fd5924ec0a544cd35a45ccde
Christian Dullweber <dullweber@chromium.org>
2020-07-01 15:05:07 +0000

    Disable flaky SyncTest#testReenableMasterSyncFirst
    Tbr: treib@chromium.org
    Bug: 1100890
    Change-Id: If5c4219493f82bf7f43aa004244c8d9fc6fdd93c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2275888
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784386}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/SyncTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/SyncTest.java
@@ -171,6 +171,7 @@ public class SyncTest {
     @Test
     @LargeTest
     @Feature({"Sync"})
+    @DisabledTest(message = "Test is flaky crbug.com/1100890")
     public void testReenableMasterSyncFirst() {
         Account account = mSyncTestRule.setUpAccountAndSignInForTesting();
         String authority = AndroidSyncSettings.get().getContractAuthority();
