115e409c832149d96f34901ddae4a6005cb1624f
Sreeja Kamishetty <sreejakshetty@chromium.org>
2020-07-01 15:44:01 +0000

    Only blocklist SpeechRecognizer for bfcache on calling start
    Previously (https://crrev.com/c/2224981), we have blocklisted
    SpeechRecognizer in the renderer while initializing but this is
    very aggressive with BackForwardCache coverage.
    
    In this CL, we move the blocklist to the browser process when
    we call the SpeechRecognitionDispatcherHost::Start.
    
    Bug: 1101265
    Change-Id: I991a75380149f3f5aab9da6de6a715d5b932ce24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277061
    Commit-Queue: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784414}

--- third_party/blink/renderer/modules/speech/speech_recognition_controller.cc
+++ third_party/blink/renderer/modules/speech/speech_recognition_controller.cc
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/speech/speech_grammar_list.h"
 #include "third_party/blink/renderer/modules/speech/speech_recognition.h"
 #include "third_party/blink/renderer/platform/heap/heap.h"
-#include "third_party/blink/renderer/platform/scheduler/public/frame_scheduler.h"
-#include "third_party/blink/renderer/platform/scheduler/public/scheduling_policy.h"
 
 namespace blink {
 
@@ -54,11 +52,7 @@ SpeechRecognitionController* SpeechRecognitionController::From(
 }
 
 SpeechRecognitionController::SpeechRecognitionController(LocalDOMWindow& window)
-    : Supplement<LocalDOMWindow>(window),
-      speech_recognizer_(&window),
-      feature_handle_for_scheduler_(window.GetScheduler()->RegisterFeature(
-          SchedulingPolicy::Feature::kSpeechRecognizer,
-          {SchedulingPolicy::RecordMetricsForBackForwardCache()})) {}
+    : Supplement<LocalDOMWindow>(window), speech_recognizer_(&window) {}
 
 SpeechRecognitionController::~SpeechRecognitionController() {
   // FIXME: Call m_client->pageDestroyed(); once we have implemented a client.
