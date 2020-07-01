30821b3963c10091e145d8ba8b520a2d27d35be6
Xiaohan Wang <xhwang@chromium.org>
2020-07-01 19:45:28 +0000

    misc: Replace uses of ComPtr::GetAddressOf() with ComPtr::operator&
    Microsoft::WRL::ComPtr<T>::GetAddressOf() is banned due to the ease
    of causing memory leaks.
    
    Also replace CopyTo(foo.GetAddressOf()) with As(&foo) since CopyTo(&foo)
    does not work if we are copying to different interfaces.
    
    Also replace QueryInterface(foo.GetAddressOf()) with
    QueryInterface(IID_PPV_ARGS(&foo)).
    
    Tbr: zmo@chromium.org,thestig@chromium.org,juncai@chromium.org,jamiewalch@chromium.org
    Bug: 914910
    Change-Id: I52739a601332822c33d07a22027d8b15600131aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2271532
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784529}

--- base/win/shortcut.cc
+++ base/win/shortcut.cc
@@ -150,8 +150,7 @@ bool CreateOrUpdateShortcutLink(const FilePath& shortcut_path,
        ShortcutProperties::PROPERTIES_TOAST_ACTIVATOR_CLSID) != 0;
   if (has_app_id || has_dual_mode || has_toast_activator_clsid) {
     ComPtr<IPropertyStore> property_store;
-    if (FAILED(i_shell_link.CopyTo(property_store.GetAddressOf())) ||
-        !property_store.Get())
+    if (FAILED(i_shell_link.As(&property_store)) || !property_store.Get())
       return false;
 
     if (has_app_id && !SetAppIdForPropertyStore(property_store.Get(),
@@ -219,7 +218,7 @@ bool ResolveShortcutProperties(const FilePath& shortcut_path,
 
   ComPtr<IPersistFile> persist;
   // Query IShellLink for the IPersistFile interface.
-  if (FAILED(i_shell_link.CopyTo(persist.GetAddressOf())))
+  if (FAILED(i_shell_link.As(&persist)))
     return false;
 
   // Load the shell link.
@@ -269,7 +268,7 @@ bool ResolveShortcutProperties(const FilePath& shortcut_path,
                  ShortcutProperties::PROPERTIES_DUAL_MODE |
                  ShortcutProperties::PROPERTIES_TOAST_ACTIVATOR_CLSID)) {
     ComPtr<IPropertyStore> property_store;
-    if (FAILED(i_shell_link.CopyTo(property_store.GetAddressOf())))
+    if (FAILED(i_shell_link.As(&property_store)))
       return false;
 
     if (options & ShortcutProperties::PROPERTIES_APP_ID) {
--- remoting/host/win/rdp_client_window.cc
+++ remoting/host/win/rdp_client_window.cc
@@ -279,13 +279,13 @@ LRESULT RdpClientWindow::OnCreate(CREATESTRUCT* create_struct) {
 
   // Instantiate the RDP ActiveX control.
   result = activex_window.CreateControlEx(
-      OLESTR("MsTscAx.MsTscAx"), nullptr, nullptr, control.GetAddressOf(),
+      OLESTR("MsTscAx.MsTscAx"), nullptr, nullptr, &control,
       __uuidof(mstsc::IMsTscAxEvents),
       reinterpret_cast<IUnknown*>(static_cast<RdpEventsSink*>(this)));
   if (FAILED(result))
     return LogOnCreateError(result);
 
-  result = control.CopyTo(client_.GetAddressOf());
+  result = control.As(&client_);
   if (FAILED(result))
     return LogOnCreateError(result);
 
@@ -303,7 +303,7 @@ LRESULT RdpClientWindow::OnCreate(CREATESTRUCT* create_struct) {
     return LogOnCreateError(result);
 
   // Check to see if the platform exposes the interface used for resizing.
-  result = client_.CopyTo(client_9_.GetAddressOf());
+  result = client_.As(&client_9_);
   if (FAILED(result) && result != E_NOINTERFACE) {
     return LogOnCreateError(result);
   }
@@ -314,7 +314,7 @@ LRESULT RdpClientWindow::OnCreate(CREATESTRUCT* create_struct) {
     return LogOnCreateError(result);
 
   // Fetch IMsRdpClientAdvancedSettings interface for the client.
-  result = client_->get_AdvancedSettings2(client_settings_.GetAddressOf());
+  result = client_->get_AdvancedSettings2(&client_settings_);
   if (FAILED(result))
     return LogOnCreateError(result);
 
@@ -368,7 +368,7 @@ LRESULT RdpClientWindow::OnCreate(CREATESTRUCT* create_struct) {
   if (FAILED(result))
     return LogOnCreateError(result);
 
-  result = client_->get_SecuredSettings2(secured_settings2.GetAddressOf());
+  result = client_->get_SecuredSettings2(&secured_settings2);
   if (SUCCEEDED(result)) {
     result =
         secured_settings2->put_AudioRedirectionMode(kRdpAudioModeRedirect);
@@ -376,7 +376,7 @@ LRESULT RdpClientWindow::OnCreate(CREATESTRUCT* create_struct) {
       return LogOnCreateError(result);
   }
 
-  result = client_->get_SecuredSettings(secured_settings.GetAddressOf());
+  result = client_->get_SecuredSettings(&secured_settings);
   if (FAILED(result))
     return LogOnCreateError(result);
 
@@ -468,7 +468,7 @@ HRESULT RdpClientWindow::OnDisconnected(long reason) {
   // Get the error message as well.
   base::win::ScopedBstr error_message;
   Microsoft::WRL::ComPtr<mstsc::IMsRdpClient5> client5;
-  result = client_.CopyTo(client5.GetAddressOf());
+  result = client_.As(&client5);
   if (SUCCEEDED(result)) {
     result = client5->GetErrorDescription(reason, extended_code,
                                           error_message.Receive());
