2e3f5fd4af6c46a8faf7de5d3880340937d108f8
Javier Ernesto Flores Robles <javierrobles@chromium.org>
2020-07-01 11:25:25 +0000

    [iOS][MF] Dock keyboard on tear down
    Dock the keyboard on tear down when an attempt to undock it was made.
    
    Bug: 1092828
    Change-Id: I6dc338f8b4b03e3f81d2d36f2f660c491bcffa8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270066
    Commit-Queue: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Auto-Submit: Javier Ernesto Flores Robles <javierrobles@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784366}

--- ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator_egtest.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator_egtest.mm
@@ -32,6 +32,11 @@ using chrome_test_util::ManualFallbackProfileTableViewWindowMatcher;
 
 namespace {
 
+// Using |isKeyboadDocked| requires to inject a UITextField in the window and
+// wait for it to be shown. For performance reasons, only try to dock the
+// keyboard in |tearDown| if it was undocked during the test.
+bool gKeyboardUndockAttempted = false;
+
 constexpr char kFormElementName[] = "name";
 constexpr char kFormElementCity[] = "city";
 
@@ -78,10 +83,10 @@ BOOL UndockAndSplitKeyboard() {
   [[EarlGrey
       selectElementWithMatcher:[KeyboardAppInterface keyboardWindowMatcher]]
       performAction:[KeyboardAppInterface keyboardUndockAction]];
+  gKeyboardUndockAttempted = YES;
 
   // If a dummy textfield was created for this, remove it.
   [textField removeFromSuperview];
-
   return ![KeyboardAppInterface isKeyboadDocked];
 }
 
@@ -148,6 +153,11 @@ BOOL WaitForKeyboardToAppear() {
 }
 
 - (void)tearDown {
+  if (gKeyboardUndockAttempted) {
+    gKeyboardUndockAttempted = NO;
+    DockKeyboard();
+  }
+
   [AutofillAppInterface clearProfilesStore];
 
   // Leaving a picker on iPads causes problems with the docking logic. This
