79f03f4c27f98147cc99e28dff0ddb08e6a9347f
Noel Gordon <noel@chromium.org>
2020-07-01 00:51:30 +0000

    [dragdrop] Tests should send a 'dragend' event
    The test drag and drop test helper sends a 'dragEnd' event but nothing
    responds to that event in files app, nor is it defined in the spec.
    
    Make it spec-conformant: change 'dragEnd' => 'dragend' and the browser
    will properly handle it and files app will correctly process it.
    
    Bug: 1062902
    Change-Id: I3ed0ba7c7e5f586132dcb88f34e5537ff7587a08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276163
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Auto-Submit: Noel Gordon <noel@chromium.org>
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784244}

--- ui/file_manager/file_manager/background/js/runtime_loaded_test_util.js
+++ ui/file_manager/file_manager/background/js/runtime_loaded_test_util.js
@@ -858,7 +858,7 @@ test.util.async.fakeDragAndDrop =
             break;
           case 4:
             if (!skipDrop) {
-              event = new DragEvent('dragEnd', sourceOptions);
+              event = new DragEvent('dragend', sourceOptions);
               result = source.dispatchEvent(event);
             }
             break;
