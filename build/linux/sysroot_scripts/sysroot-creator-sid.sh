08cf06fdbe1746210b83fb79cc9747aa0861e5be
Tom Anderson <thomasanderson@chromium.org>
2020-07-01 21:01:46 +0000

    [XProto] Remove xcbproto from sysroots
    It's no longer necessary after xcbproto is added to third_party in:
    https://chromium-review.googlesource.com/c/chromium/src/+/2269546
    
    R=thestig
    
    Change-Id: Ie475b124684de6f504149386274b567386bc6bbe
    Bug: 1066670
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276730
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784556}

--- build/linux/sysroot_scripts/sysroot-creator-sid.sh
+++ build/linux/sysroot_scripts/sysroot-creator-sid.sh
@@ -362,7 +362,6 @@ DEBIAN_PACKAGES="\
   libzstd1
   linux-libc-dev
   mesa-common-dev
-  python-xcbgen
   shared-mime-info
   speech-dispatcher
   uuid-dev
@@ -379,7 +378,6 @@ DEBIAN_PACKAGES="\
   x11proto-scrnsaver-dev
   x11proto-xext-dev
   x11proto-xinerama-dev
-  xcb-proto
   zlib1g
   zlib1g-dev
 "
