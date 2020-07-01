2a3cd4506aa59f979165d4c01da805f04cc75d5d
David Bertoni <dbertoni@chromium.org>
2020-07-01 18:15:07 +0000

    [Extensions] Re-enable Window Open API test.
    This test was disabled on Linux, Windows, and Mac debug. It
    passed 3000 iterations on Linux and Windows, and 600 iterations
    on Mac debug without flakes.
    
    Bug: 253417
    Change-Id: Id71ceac3a8b5ae43145f93935b92851fbe8d04e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2255661
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784492}

--- chrome/browser/extensions/window_open_interactive_apitest.cc
+++ chrome/browser/extensions/window_open_interactive_apitest.cc
@@ -3,21 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/command_line.h"
-#include "build/build_config.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/test/result_catcher.h"
 
 namespace extensions {
 
-// http://crbug.com/253417 for NDEBUG
-#if (defined(OS_WIN) || defined(OS_MACOSX)) && defined(NDEBUG)
-// Focus test fails if there is no window manager on Linux.
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WindowOpenFocus) {
   ASSERT_TRUE(RunExtensionTest("window_open/focus")) << message_;
 }
-#endif
 
 // The test uses the chrome.browserAction.openPopup API, which requires that the
 // window can automatically be activated.
