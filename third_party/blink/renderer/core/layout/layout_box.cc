8b6ea2279e54da2fa796c797d5542e78738d1284
Koji Ishii <kojii@chromium.org>
2020-07-01 03:30:22 +0000

    Eliminate destroyed or moved |LayoutObject| from |PostLayoutChildren|
    This patch changes the iterator of |PostLayoutChildren| to
    eliminate fragments for destroyed or moved |LayoutObject|.
    
    The root cause is probably pre-paint with dirty tree, and
    this patch does not address that, but makes fragment
    traversal safer.
    
    This patch is speculative.
    
    Bug: 1099863
    Change-Id: Ia78d55cc43a50f54f4b547ae8febc025770ac004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2275944
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784303}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -2657,7 +2657,8 @@ const NGLayoutResult* LayoutBox::GetCachedLayoutResult() const {
   const NGLayoutResult* result = layout_results_[0].get();
   if (result->IsSingleUse())
     return nullptr;
-  DCHECK(result->PhysicalFragment().IsAlive() || BeingDestroyed());
+  DCHECK(!result->PhysicalFragment().IsLayoutObjectDestroyedOrMoved() ||
+         BeingDestroyed());
   DCHECK_EQ(layout_results_.size(), 1u);
   return result;
 }
