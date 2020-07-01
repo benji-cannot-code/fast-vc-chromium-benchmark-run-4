0ea3d0656cd54e7fa4bd0906d1f14b679181cf04
Haiyang Pan <hypan@google.com>
2020-07-01 16:48:06 +0000

    Revert "Download later: Implements lots of things in download later dialog."
    This reverts commit 5a04dad7028dd27e3119d1d12ddcdce821094bc9.
    
    Reason for revert: Tests crash consistently since the builds
    https://ci.chromium.org/p/chromium/builders/ci/android-pie-x86-rel/1294
    https://ci.chromium.org/p/chromium/builders/ci/android-arm64-proguard-rel/2083
    And flakily since https://ci.chromium.org/p/chromium/builders/ci/android-pie-arm64-rel/5696
    
    Original change's description:
    > Download later: Implements lots of things in download later dialog.
    > 
    > This CL does the following:
    > 
    > 1. Implements the checkbox.
    > 2. Implements the edit location text, which triggers location dialog.
    > 3. Make download later dialog owns a date time picker, so other callers
    > can share the glue code between later dialog and date time picker.
    > 4. Lots of tests added, lots of code refactored.
    > 
    > TBR=dtrainor@chromium.org
    > 
    > Bug: 1099989,1078454
    > Change-Id: I3dd53c0a0694c91789398382c6deb11fbfe8006d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268467
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: Hesen Zhang <hesen@chromium.org>
    > Commit-Queue: Xing Liu <xingliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#784260}
    
    TBR=dtrainor@chromium.org,qinmin@chromium.org,shaktisahu@chromium.org,xingliu@chromium.org,hesen@chromium.org
    
    Change-Id: Idd5217d490c209c38a2fc1174d771daa9c877c62
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1099989, 1078454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278124
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/master@{#784447}

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadLocationDialogCoordinator.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadLocationDialogCoordinator.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.download.dialogs;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.text.TextUtils;
@@ -53,20 +54,20 @@ public class DownloadLocationDialogCoordinator implements ModalDialogProperties.
 
     /**
      * Shows the download location dialog.
-     * @param context The {@link Context} for the dialog.
+     * @param activity The activity that provides android {@link Context} to the dialog.
      * @param modalDialogManager {@link ModalDialogManager} to control the dialog.
      * @param totalBytes The total download file size. May be 0 if not available.
      * @param dialogType The type of the location dialog.
      * @param suggestedPath The suggested file path used by the location dialog.
      */
-    public void showDialog(Context context, ModalDialogManager modalDialogManager, long totalBytes,
-            @DownloadLocationDialogType int dialogType, String suggestedPath) {
-        if (context == null || modalDialogManager == null) {
+    public void showDialog(Activity activity, ModalDialogManager modalDialogManager,
+            long totalBytes, @DownloadLocationDialogType int dialogType, String suggestedPath) {
+        if (activity == null || modalDialogManager == null) {
             onDismiss(null, DialogDismissalCause.ACTIVITY_DESTROYED);
             return;
         }
 
-        mContext = context;
+        mContext = activity;
         mModalDialogManager = modalDialogManager;
         mTotalBytes = totalBytes;
         mDialogType = dialogType;
