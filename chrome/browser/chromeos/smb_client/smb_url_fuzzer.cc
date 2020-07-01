d5601dd5f65e72037ebb9e9d591aca0db9a94fef
Anand K Mistry <amistry@chromium.org>
2020-07-01 08:25:37 +0000

    Add missing logging.h include for SMB URL fuzzer
    The header was previously included transitively.
    
    Fixed: 1101222
    Change-Id: Ic8a4beeab5dc2f6cd61c63c119c93688ae1dd04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276357
    Auto-Submit: Anand K Mistry <amistry@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784358}

--- chrome/browser/chromeos/smb_client/smb_url_fuzzer.cc
+++ chrome/browser/chromeos/smb_client/smb_url_fuzzer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/at_exit.h"
 #include "base/i18n/icu_util.h"
+#include "base/logging.h"
 #include "chrome/browser/chromeos/smb_client/smb_url.h"
 
 // This is a workaround for https://crbug.com/778929.
