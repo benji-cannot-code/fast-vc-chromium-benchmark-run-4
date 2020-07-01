9a5e5335b07e21ac475890a885b65f970306fb7d
Meilin Wang <meilinw@google.com>
2020-07-01 19:07:40 +0000

    ambient: Fix screen goes off when battery is full.
    Bug: b/160202872
    Test: unittest.
    Change-Id: I355c704fd2adda7d96dfbb3c972964e2da7a1e6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2275342
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784515}

--- ash/ambient/ambient_controller.cc
+++ ash/ambient/ambient_controller.cc
@@ -81,6 +81,13 @@ std::string GetWidgetName() {
   return "InSessionAmbientModeContainer";
 }
 
+// Returns true if the device is currently connected to a charger.
+bool IsChargerConnected() {
+  return (PowerStatus::Get()->IsBatteryCharging() ||
+          PowerStatus::Get()->IsBatteryFull()) &&
+         PowerStatus::Get()->IsLinePowerConnected();
+}
+
 }  // namespace
 
 // AmbientController::InactivityMonitor----------------------------------
@@ -169,9 +176,8 @@ void AmbientController::OnAmbientUiVisibilityChanged(
       // This will be no-op if the view is already visible.
       container_view_->SetVisible(true);
 
-      if (PowerStatus::Get()->IsBatteryCharging()) {
-        // Requires wake lock to prevent display from sleeping when the battery
-        // is charging.
+      if (IsChargerConnected()) {
+        // Requires wake lock to prevent display from sleeping.
         AcquireWakeLock();
       }
       // Observes the |PowerStatus| on the battery charging status change for
@@ -263,7 +269,7 @@ void AmbientController::OnPowerStatusChanged() {
     return;
   }
 
-  if (PowerStatus::Get()->IsBatteryCharging()) {
+  if (IsChargerConnected()) {
     AcquireWakeLock();
   } else {
     ReleaseWakeLock();
