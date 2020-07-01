9f7655b30d7503518b9248eb2e5758f27e221ffa
Sophey Dong <sophey@chromium.org>
2020-07-01 06:08:59 +0000

    Pass in URL from the Sharing Hub to generate the QRCode.
    Also fix the issue where the QR Code was not being loaded after revoking permissions.
    
    Bug: 1079467, 1083351, 1099291
    Change-Id: Ia5b61a461e0321e46631119c22a15ef851d8ddd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2265248
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Gayane Petrosyan <gayane@chromium.org>
    Reviewed-by: Tanya Gupta <tgupta@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784342}

--- chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ChromeProvidedSharingOptionsProvider.java
+++ chrome/browser/share/android/java/src/org/chromium/chrome/browser/share/share_sheet/ChromeProvidedSharingOptionsProvider.java
@@ -272,7 +272,7 @@ class ChromeProvidedSharingOptionsProvider {
                             "Sharing.SharingHubAndroid.TimeToShare",
                             System.currentTimeMillis() - mShareStartTime);
                     mBottomSheetController.hideContent(mBottomSheetContent, true);
-                    QrCodeCoordinator qrCodeCoordinator = new QrCodeCoordinator(mActivity);
+                    QrCodeCoordinator qrCodeCoordinator = new QrCodeCoordinator(mActivity, mUrl);
                     qrCodeCoordinator.show();
                 },
                 /*isFirstParty=*/true);
