e758a73d26ebd44f987a635aa6b9051da3fd6695
Yuichiro Hanada <yhanada@google.com>
2020-07-01 16:23:34 +0000

    Tell the proxy IME to send a received key event.
    When ARC apps receive a key event before IME, a key event which is not
    consumed by the apps has to be sent to the IME.
    The new mojo method is added to let the proxy IME ask ArcImeService
    whether it should send back a key event.
    
    Bug: b:148193316
    Change-Id: I6bbb83b9b3ab58a11ab5e9c01ce626255871818f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2269386
    Reviewed-by: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: Mattias Nissler <mnissler@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784440}

--- components/arc/ime/arc_ime_service.cc
+++ components/arc/ime/arc_ime_service.cc
@@ -388,6 +388,11 @@ void ArcImeService::OnCursorRectChangedWithSurroundingText(
     input_method->OnCaretBoundsChanged(this);
 }
 
+bool ArcImeService::ShouldEnableKeyEventForwarding() {
+  return base::FeatureList::IsEnabled(
+      chromeos::features::kArcPreImeKeyEventSupport);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // Overridden from ash::KeyboardControllerObserver
 void ArcImeService::OnKeyboardAppearanceChanged(
