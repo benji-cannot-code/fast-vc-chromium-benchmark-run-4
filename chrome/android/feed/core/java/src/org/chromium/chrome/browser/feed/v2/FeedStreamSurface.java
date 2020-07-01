cd724c9dd5f195eae40840cbb3fbafc7392cd8a0
Ian Wells <iwells@chromium.org>
2020-07-01 21:18:07 +0000

    feedv2: Send capability bits for dismiss and undo-dismiss
    Also, add the "undo" button to the undo snackbar. Undoable dismiss should work now.
    
    Bug: 1044139
    Change-Id: I404b92ab3bb784bf0b71fe3d4afdb7b37f7beed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278140
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784568}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedStreamSurface.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/v2/FeedStreamSurface.java
@@ -511,6 +511,7 @@ public class FeedStreamSurface implements SurfaceActionsHandler, FeedActionsHand
                                     }
                                 },
                                 Snackbar.TYPE_ACTION, Snackbar.UMA_FEED_NTP_STREAM)
+                        .setAction(actionLabel, /*actionData=*/null)
                         .setDuration(durationMs));
     }
 
