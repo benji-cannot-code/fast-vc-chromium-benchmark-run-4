f8df4cc5aa96bf060d9f7b8e900b498a603480f0
Fergal Daly <fergal@chromium.org>
2020-07-02 04:04:49 +0000

    Stop dividing the command-line-provided number of test jobs by 2.
    The value should be half of the number of CPU cores or whatever
    --test-launcher-jobs says. This change makes base::NumParallelJobs
    return the number of CPU cores divided by a cores_per_job factor
    supplied by each caller.
    
    This also adds error checking on the result where it wasn't already
    present.
    
    
    
    Bug: 1096909
    Change-Id: I7c5bbf15697157993ebe07667e243201f6808fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2251568
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Ilia Samsonov <isamsonov@google.com>
    Reviewed-by: Mostyn Bramley-Moore <mostynb@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784687}

--- extensions/shell/test/shell_tests_main.cc
+++ extensions/shell/test/shell_tests_main.cc
@@ -10,10 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 int main(int argc, char** argv) {
   base::CommandLine::Init(argc, argv);
-  size_t parallel_jobs = base::NumParallelJobs();
-  if (parallel_jobs > 1U) {
-    parallel_jobs /= 2U;
-  }
+  size_t parallel_jobs = base::NumParallelJobs(/*cores_per_job=*/2);
+  if (parallel_jobs == 0U)
+    return 1;
   extensions::AppShellTestLauncherDelegate launcher_delegate;
   return content::LaunchTests(&launcher_delegate, parallel_jobs, argc, argv);
 }
