90e65e395ef71de36145a9b03b590fe5d4d5ee77
Wei Lee <wtlee@google.com>
2020-07-02 07:40:26 +0000

    [CCA WebUI] Add required permission for CCA SWA
    Bug: 980846
    Test: None
    Change-Id: I22f1324b3d654baea2658d08fa3392b314699c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2257081
    Commit-Queue: Wei Lee <wtlee@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784716}

--- chromeos/components/camera_app_ui/url_constants.cc
+++ chromeos/components/camera_app_ui/url_constants.cc
@@ -8,5 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 const char kChromeUICameraAppHost[] = "camera-app";
+const char kChromeUICameraAppURL[] = "chrome://camera-app/";
 
 }  // namespace chromeos
