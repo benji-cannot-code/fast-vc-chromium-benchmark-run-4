ee9e6724ba462f804bc0fa293ae6929904df14de
Steven Bennetts <stevenjb@chromium.org>
2020-07-02 00:06:14 +0000

    NetworkState: Limit SignalStrength updates
    We currently use a threshold for comparing active network changes,
    but not for individual SignalStrength property changes. This CL
    fixes that so that only changes exceeding the threshold generate
    events.
    
    Bug: 1091567
    Change-Id: Iafbe807279ed074a85bea4f27710b7963e2992e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278415
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784622}

--- chromeos/network/network_state_handler.cc
+++ chromeos/network/network_state_handler.cc
@@ -36,9 +36,6 @@ namespace chromeos {
 
 namespace {
 
-// Ignore changes to signal strength less than this value for active networks.
-const int kSignalStrengthChangeThreshold = 5;
-
 // Constants used for logging.
 constexpr char kReasonStateChange[] = "State Change";
 constexpr char kReasonChange[] = "New Network";
@@ -127,7 +124,7 @@ class NetworkStateHandler::ActiveNetworkState {
            activation_state_ == network->activation_state() &&
            connect_requested_ == network->connect_requested() &&
            (abs(signal_strength_ - network->signal_strength()) <
-            kSignalStrengthChangeThreshold) &&
+            NetworkState::kSignalStrengthChangeThreshold) &&
            network_technology_ == network->network_technology();
   }
 
