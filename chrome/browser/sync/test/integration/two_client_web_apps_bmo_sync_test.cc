d6b8602e614a7e819b16b197f79c4c6d441a703a
Jeremy Roman <jbroman@chromium.org>
2020-07-02 18:24:31 +0000

    Sheriffing: Disable two TwoClientWebAppsBMOSyncTest tests on all configurations.
    They are flaky in several different configurations.
    
    Bug: 1099847
    Change-Id: Ic4d45a619bd72e070dbf7eb75ee3e55687341560
    Tbr: jkrcal@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2280221
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784884}

--- chrome/browser/sync/test/integration/two_client_web_apps_bmo_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_web_apps_bmo_sync_test.cc
@@ -183,16 +183,9 @@ IN_PROC_BROWSER_TEST_F(TwoClientWebAppsBMOSyncTest,
   EXPECT_TRUE(AllProfilesHaveSameWebAppIds());
 }
 
-// Flakily fails on Mac ASAN, Linux TSAN: crbug.com/1099847
-#if defined(THREAD_SANITIZER) || defined(ADDRESS_SANITIZER)
-#define MAYBE_SyncDoubleInstallationDifferentNames \
-  DISABLED_SyncDoubleInstallationDifferentNames
-#else
-#define MAYBE_SyncDoubleInstallationDifferentNames \
-  SyncDoubleInstallationDifferentNames
-#endif
+// Flakily fails on multiple configurations. https://crbug.com/1099847
 IN_PROC_BROWSER_TEST_F(TwoClientWebAppsBMOSyncTest,
-                       MAYBE_SyncDoubleInstallationDifferentNames) {
+                       DISABLED_SyncDoubleInstallationDifferentNames) {
   ASSERT_TRUE(SetupSync());
   ASSERT_TRUE(AllProfilesHaveSameWebAppIds());
 
@@ -220,16 +213,10 @@ IN_PROC_BROWSER_TEST_F(TwoClientWebAppsBMOSyncTest,
   EXPECT_EQ(GetRegistrar(GetProfile(1)).GetAppShortName(app_id), "Test name 2");
 }
 
-// Flakily fails on Mac ASAN: crbug.com/1099847
-#if defined(THREAD_SANITIZER) || defined(ADDRESS_SANITIZER)
-#define MAYBE_SyncDoubleInstallationDifferentUserDisplayMode \
-  DISABLED_SyncDoubleInstallationDifferentUserDisplayMode
-#else
-#define MAYBE_SyncDoubleInstallationDifferentUserDisplayMode \
-  SyncDoubleInstallationDifferentUserDisplayMode
-#endif
-IN_PROC_BROWSER_TEST_F(TwoClientWebAppsBMOSyncTest,
-                       MAYBE_SyncDoubleInstallationDifferentUserDisplayMode) {
+// Flakily fails on multiple configurations. https://crbug.com/1099847
+IN_PROC_BROWSER_TEST_F(
+    TwoClientWebAppsBMOSyncTest,
+    DISABLED_SyncDoubleInstallationDifferentUserDisplayMode) {
   ASSERT_TRUE(SetupSync());
   ASSERT_TRUE(AllProfilesHaveSameWebAppIds());
 
