f5a7e2aca00adce928997bd3dc101f4e76f5967b
edchin <edchin@chromium.org>
2020-07-02 21:42:24 +0000

    [ios] Add URL drag interaction to History
    Bug: 1099921
    Change-Id: I9307d0b2d7aef3742116d4cee124853ba88c4f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276647
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784923}

--- ios/chrome/browser/ui/history/history_table_view_controller.mm
+++ ios/chrome/browser/ui/history/history_table_view_controller.mm
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_formatter/url_formatter.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
+#include "ios/chrome/browser/drag_and_drop/drag_and_drop_flag.h"
+#import "ios/chrome/browser/drag_and_drop/drag_item_util.h"
+#import "ios/chrome/browser/drag_and_drop/table_view_url_drag_drop_handler.h"
 #import "ios/chrome/browser/main/browser.h"
 #import "ios/chrome/browser/metrics/new_tab_page_uma.h"
 #include "ios/chrome/browser/sync/sync_setup_service.h"
@@ -77,13 +80,14 @@ const CGFloat kButtonDefaultFontSize = 15.0;
 const CGFloat kButtonHorizontalPadding = 30.0;
 }  // namespace
 
-@interface HistoryTableViewController ()<HistoryEntriesStatusItemDelegate,
-                                         HistoryEntryInserterDelegate,
-                                         HistoryEntryItemDelegate,
-                                         TableViewTextLinkCellDelegate,
-                                         UISearchControllerDelegate,
-                                         UISearchResultsUpdating,
-                                         UISearchBarDelegate> {
+@interface HistoryTableViewController () <HistoryEntriesStatusItemDelegate,
+                                          HistoryEntryInserterDelegate,
+                                          HistoryEntryItemDelegate,
+                                          TableViewTextLinkCellDelegate,
+                                          TableViewURLDragDataSource,
+                                          UISearchControllerDelegate,
+                                          UISearchResultsUpdating,
+                                          UISearchBarDelegate> {
   // Closure to request next page of history.
   base::OnceClosure _query_history_continuation;
 }
@@ -121,6 +125,8 @@ const CGFloat kButtonHorizontalPadding = 30.0;
 @property(nonatomic, strong) UIBarButtonItem* editButton;
 // Scrim when search box in focused.
 @property(nonatomic, strong) UIControl* scrimView;
+// Handler for URL drag interactions.
+@property(nonatomic, strong) TableViewURLDragDropHandler* dragDropHandler;
 @end
 
 @implementation HistoryTableViewController
@@ -180,6 +186,14 @@ const CGFloat kButtonHorizontalPadding = 30.0;
                   action:@selector
                   (displayContextMenuInvokedByGestureRecognizer:)];
   [self.tableView addGestureRecognizer:longPressRecognizer];
+
+  if (DragAndDropIsEnabled()) {
+    self.dragDropHandler = [[TableViewURLDragDropHandler alloc] init];
+    self.dragDropHandler.origin = WindowActivityHistoryOrigin;
+    self.dragDropHandler.dragDataSource = self;
+    self.tableView.dragDelegate = self.dragDropHandler;
+    self.tableView.dragInteractionEnabled = YES;
+  }
 
   // NavigationController configuration.
   self.title = l10n_util::GetNSString(IDS_HISTORY_TITLE);
@@ -659,6 +673,28 @@ const CGFloat kButtonHorizontalPadding = 30.0;
 
     [self fetchHistoryForQuery:_currentQuery continuation:true];
   }
+}
+
+#pragma mark - TableViewURLDragDataSource
+
+- (URLInfo*)tableView:(UITableView*)tableView
+    URLInfoAtIndexPath:(NSIndexPath*)indexPath {
+  if (self.tableView.isEditing)
+    return nil;
+
+  TableViewItem* item = [self.tableViewModel itemAtIndexPath:indexPath];
+  switch (item.type) {
+    case ItemTypeHistoryEntry: {
+      HistoryEntryItem* URLItem =
+          base::mac::ObjCCastStrict<HistoryEntryItem>(item);
+      return [[URLInfo alloc] initWithURL:URLItem.URL title:URLItem.text];
+    }
+    case ItemTypeEntriesStatus:
+    case ItemTypeActivityIndicator:
+    case ItemTypeEntriesStatusWithLink:
+      break;
+  }
+  return nil;
 }
 
 #pragma mark - Private methods
