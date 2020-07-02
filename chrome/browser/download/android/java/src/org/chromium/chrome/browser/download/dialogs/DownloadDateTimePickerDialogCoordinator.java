441a7752ab58e6ca1916e946eac9490b2ac06c14
Xing Liu <xingliu@chromium.org>
2020-07-02 23:43:33 +0000

    Revert "[Reland] Download later: Implements lots of things in download later dialog."
    This reverts commit 4dfab152a319a4332f6025845dd0f8eba543250a.
    
    Reason for revert: Test still failing, don't know why, no crash log.
    
    Original change's description:
    > [Reland] Download later: Implements lots of things in download later dialog.
    > 
    > Revert:https://chromium-review.googlesource.com/c/chromium/src/+/2278124
    > Fix: Use espresso to wait for the dialog to show properly, don't do all
    > the stuff on UI thread.
    > 
    > This CL does the following:
    > 
    > 1. Implements the checkbox.
    > 2. Implements the edit location text, which triggers location dialog.
    > 3. Make download later dialog owns a date time picker, so other callers
    > can share the glue code between later dialog and date time picker.
    > 4. Lots of tests added, lots of code refactored.
    > 
    > TBR=dtrainor@chromium.org
    > 
    > Bug: 1099989,1078454
    > Change-Id: I5e7ddfb865a5695eb5def70ae05e72971ec01df3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268467
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: Hesen Zhang <hesen@chromium.org>
    > Commit-Queue: Xing Liu <xingliu@chromium.org>
    > Cr-Original-Commit-Position: refs/heads/master@{#784260}
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278226
    > Reviewed-by: Xing Liu <xingliu@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#784918}
    
    TBR=dtrainor@chromium.org,shaktisahu@chromium.org,xingliu@chromium.org,hesen@chromium.org
    
    Change-Id: I18fbba1821ce01dad1be0b46b134445db7e674bb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1099989, 1078454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2280335
    Reviewed-by: Xing Liu <xingliu@chromium.org>
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784943}

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadDateTimePickerDialogCoordinator.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/dialogs/DownloadDateTimePickerDialogCoordinator.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.download.dialogs;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 
@@ -18,6 +19,8 @@ import org.chromium.ui.modaldialog.ModalDialogProperties;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * The coordinator for download date time picker. The user can pick an exact time to start the
  * download later. The dialog has two stage:
@@ -25,6 +28,8 @@ import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
  * 2. A clock to let the user to pick a time.
  */
 public class DownloadDateTimePickerDialogCoordinator implements ModalDialogProperties.Controller {
+    /** The maximum time that the user can select in the dialog.*/
+    public static final long MAX_TIME = TimeUnit.DAYS.toMillis(7); /* 7 days */
     /**
      * The controller that receives events from the date time picker.
      */
@@ -60,25 +65,24 @@ public class DownloadDateTimePickerDialogCoordinator implements ModalDialogPrope
 
     /**
      * Shows the date time picker.
-     * @param context The {@link Context} for the date time picker.
      * @param windowAndroid The window android handle that provides contexts.
      * @param model The model that defines the application data used to update the UI view.
      */
     public void showDialog(
-            Context context, ModalDialogManager modalDialogManager, PropertyModel model) {
-        if (context == null || modalDialogManager == null) {
+            Activity activity, ModalDialogManager modalDialogManager, PropertyModel model) {
+        if (activity == null || modalDialogManager == null) {
             onDismiss(null, DialogDismissalCause.ACTIVITY_DESTROYED);
             return;
         }
 
         mModalDialogManager = modalDialogManager;
         mModel = model;
-        mView = (DownloadDateTimePickerView) LayoutInflater.from(context).inflate(
+        mView = (DownloadDateTimePickerView) LayoutInflater.from(activity).inflate(
                 R.layout.download_later_date_time_picker_dialog, null);
         mProcessor = PropertyModelChangeProcessor.create(mModel, mView,
                 DownloadDateTimePickerView.Binder::bind, true /*performInitialBind*/);
         mModalDialogManager.showDialog(
-                getModalDialogModel(context), ModalDialogManager.ModalDialogType.APP);
+                getModalDialogModel(activity), ModalDialogManager.ModalDialogType.APP);
     }
 
     /**
