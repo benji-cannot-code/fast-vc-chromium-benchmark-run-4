c861b913aaf4c9f706203dd489e6b1c7587137d6
Antonio Sartori <antoniosartori@chromium.org>
2020-07-02 09:56:04 +0000

    Implement CSPEE Blanket Enforcement logic out-of-blink
    This changes adds to the AncestorThrottle a check for the step
    "Does response allow blanket enforcement of policy from request" of
    Content Security Policy: Embedded Enforcement:
    https://w3c.github.io/webappsec-cspee/#origin-allowed Behind the flag
    
    This is one of the steps of moving CSPEE out-of-blink and is hidden
    under the flag network::features::kOutOfBlinkCSPEE.
    
    Change-Id: Id3092322134e055810d4006e63e6974ee64315be
    Bug: 1094909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218019
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784753}

--- content/browser/frame_host/ancestor_throttle.h
+++ content/browser/frame_host/ancestor_throttle.h
@@ -13,6 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "content/public/browser/navigation_throttle.h"
 #include "services/network/public/mojom/content_security_policy.mojom-forward.h"
+#include "services/network/public/mojom/parsed_headers.mojom-forward.h"
+
+class GURL;
+
+namespace url {
+class Origin;
+}
 
 namespace net {
 class HttpResponseHeaders;
@@ -52,6 +59,8 @@ class CONTENT_EXPORT AncestorThrottle : public NavigationThrottle {
   FRIEND_TEST_ALL_PREFIXES(AncestorThrottleTest, ErrorsParsingXFrameOptions);
   FRIEND_TEST_ALL_PREFIXES(AncestorThrottleTest,
                            IgnoreWhenFrameAncestorsPresent);
+  FRIEND_TEST_ALL_PREFIXES(AncestorThrottleTest,
+                           AllowsBlanketEnforcementOfRequiredCSP);
 
   explicit AncestorThrottle(NavigationHandle* handle);
   NavigationThrottle::ThrottleCheckResult ProcessResponseImpl(
@@ -64,6 +73,11 @@ class CONTENT_EXPORT AncestorThrottle : public NavigationThrottle {
   CheckResult EvaluateFrameAncestors(
       const std::vector<network::mojom::ContentSecurityPolicyPtr>&
           content_security_policy);
+  CheckResult EvaluateCSPEmbeddedEnforcement();
+  static bool AllowsBlanketEnforcementOfRequiredCSP(
+      const url::Origin& request_origin,
+      const GURL& response_url,
+      const network::mojom::AllowCSPFromHeaderValuePtr& allow_csp_from);
 
   // Parses an 'X-Frame-Options' header. If the result is either CONFLICT
   // or INVALID, |header_value| will be populated with the value which caused
