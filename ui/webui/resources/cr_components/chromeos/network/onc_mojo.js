ab80c0834392fef1b4d2e1c86c36d17be7ad6c0e
Steven Bennetts <stevenjb@chromium.org>
2020-07-02 00:06:48 +0000

    Settings: Network: Limit complete device state updates
    We are sometimes triggering extra property updates when
    DeviceStateProperties.scanning changes more frequently than expected.
    
    This CL addresses that by limiting changes to scanning to just that
    property.
    
    Bug: 1091567
    Change-Id: I1514d02361e85f6f50ed1326faaad1d3e4de4e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278403
    Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    Reviewed-by: Jon Mann <jonmann@chromium.org>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784623}

--- ui/webui/resources/cr_components/chromeos/network/onc_mojo.js
+++ ui/webui/resources/cr_components/chromeos/network/onc_mojo.js
@@ -1059,6 +1059,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     assertNotReached();
     return 'OncNotConnected';
   }
+
+  /**
+   * Returns true the IPAddress bytes match.
+   * @param {?network.mojom.IPAddress|undefined} a
+   * @param {?network.mojom.IPAddress|undefined} b
+   * @return {boolean}
+   */
+  static ipAddressMatch(a, b) {
+    if (!a || !b) {
+      return !!a === !!b;
+    }
+    const abytes = a.addressBytes;
+    const bbytes = b.addressBytes;
+    if (abytes.length !== bbytes.length) {
+      return false;
+    }
+    for (let i = 0; i < abytes.length; ++i) {
+      if (abytes[i] !== bbytes[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Returns true the SIMLockStatus properties match.
+   * @param {?chromeos.networkConfig.mojom.SIMLockStatus|undefined} a
+   * @param {?chromeos.networkConfig.mojom.SIMLockStatus|undefined} b
+   * @return {boolean}
+   */
+  static simLockStatusMatch(a, b) {
+    if (!a || !b) {
+      return !!a === !!b;
+    }
+    return a.lockType === b.lockType && a.lockEnabled === b.lockEnabled &&
+        a.retriesLeft === b.retriesLeft;
+  }
 }
 
 /** @typedef {chromeos.networkConfig.mojom.DeviceStateProperties} */
