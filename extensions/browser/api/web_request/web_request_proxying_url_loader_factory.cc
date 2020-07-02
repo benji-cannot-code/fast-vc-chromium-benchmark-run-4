ab58ed9a3dcbb774bd957b2fec904b29a6ea8422
Yutaka Hirano <yhirano@chromium.org>
2020-07-02 10:18:56 +0000

    [Web Request] Fix state_ setting in InProgressRequest::onRequestError
    We ignore the given State, which leads to excessive "kInPreogress" in
    the UMA.
    
    Bug: 1101702
    Change-Id: Ic9a1ebf4039dc34f1253f32a497f83a0f6181a8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278911
    Auto-Submit: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Yutaka Hirano <yhirano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784757}

--- extensions/browser/api/web_request/web_request_proxying_url_loader_factory.cc
+++ extensions/browser/api/web_request/web_request_proxying_url_loader_factory.cc
@@ -1025,6 +1025,7 @@ void WebRequestProxyingURLLoaderFactory::InProgressRequest::OnRequestError(
   ExtensionWebRequestEventRouter::GetInstance()->OnErrorOccurred(
       factory_->browser_context_, &info_.value(), true /* started */,
       status.error_code);
+  state_ = state;
 
   // Deletes |this|.
   factory_->RemoveRequest(network_service_request_id_, request_id_);
