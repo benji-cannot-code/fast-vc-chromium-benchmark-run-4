c9a2922e3b587df8802753f9b8cc470c9033e180
Taylor Bergquist <tbergquist@chromium.org>
2020-07-02 01:55:19 +0000

    Fix browser sizing with tab scrolling enabled.
    Bug: 1096645
    Change-Id: Icd97f36514efc2f031cfe7aff0cb899a340ade38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278616
    Reviewed-by: Connie Wan <connily@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784665}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_frame.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
+#include "chrome/browser/ui/views/frame/tab_strip_region_view.h"
 #include "chrome/browser/ui/views/frame/top_container_view.h"
 #include "chrome/browser/ui/views/profiles/profile_indicator_icon.h"
 #include "chrome/browser/ui/views/tab_icon_view.h"
@@ -414,7 +415,7 @@ gfx::Size BrowserNonClientFrameViewAsh::GetMinimumSize() const {
     // Ensure that the minimum width is enough to hold a minimum width tab strip
     // at its usual insets.
     const int min_tabstrip_width =
-        browser_view()->tabstrip()->GetMinimumSize().width();
+        browser_view()->tab_strip_region_view()->GetMinimumSize().width();
     min_width =
         std::max(min_width, min_tabstrip_width + GetTabStripLeftInset() +
                                 GetTabStripRightInset());
