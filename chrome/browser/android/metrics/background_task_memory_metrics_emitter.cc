c1b95570b3cdc179af4fa753e3393f6b6f81c382
Ella Ge <eirage@chromium.org>
2020-07-02 17:06:05 +0000

    [CodeHealth] Convert base::Bind in chrome/browser/android/
    Bug: 1007635
    Change-Id: I6130122826e752443bdc7019cec05b1657f1fe92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279911
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784859}

--- chrome/browser/android/metrics/background_task_memory_metrics_emitter.cc
+++ chrome/browser/android/metrics/background_task_memory_metrics_emitter.cc
@@ -41,10 +41,10 @@ void BackgroundTaskMemoryMetricsEmitter::FetchAndEmitProcessMemoryMetrics() {
   }
 
   // The callback keeps this object alive until the callback is invoked.
-  auto callback =
-      base::Bind(&BackgroundTaskMemoryMetricsEmitter::ReceivedMemoryDump, this);
+  auto callback = base::BindOnce(
+      &BackgroundTaskMemoryMetricsEmitter::ReceivedMemoryDump, this);
 
-  RequestGlobalDump(callback);
+  RequestGlobalDump(std::move(callback));
 }
 
 base::android::ApplicationState
