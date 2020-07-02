9ebdc89fefaf32f06480c40d1190fde4af483184
Guido Urdaneta <guidou@chromium.org>
2020-07-02 07:19:33 +0000

    Revert "NetworkState: Limit SignalStrength updates"
    This reverts commit f9eac47f4cd5881d6f7b3dff774c8e08f454160b.
    
    Reason for revert: Causing Linux ChromiumOS MSAN bot failure.
    First failure:
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/19697
    
    Sample logs:
    [5405:5405:0701/201606.886571:2227947411:ERROR:device_event_log_impl.cc(208)] [20:16:06.886] Network: shill_property_handler.cc:38 Error parsing key as list: ProhibitedTechnologies
    ==5405==WARNING: MemorySanitizer: use-of-uninitialized-value
        #0 0x564109903c06 in chromeos::ManagedState::GetIntegerValue(std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> > const&, base::Value const&, int*) ./../../chromeos/network/managed_state.cc:119:7
     
    ...
    
      Uninitialized value was created by an allocation of 'signal_strength' in the stack frame of function '_ZN8chromeos12NetworkState15PropertyChangedERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEERKN4base5ValueE'
        #0 0x5641099dbffc in chromeos::NetworkState::PropertyChanged(std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> > const&, base::Value const&) ./../../chromeos/network/network_state.cc:88:0
    
    
    
    Original change's description:
    > NetworkState: Limit SignalStrength updates
    > 
    > We currently use a threshold for comparing active network changes,
    > but not for individual SignalStrength property changes. This CL
    > fixes that so that only changes exceeding the threshold generate
    > events.
    > 
    > Bug: 1091567
    > Change-Id: Iafbe807279ed074a85bea4f27710b7963e2992e0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278415
    > Reviewed-by: Jon Mann <jonmann@chromium.org>
    > Reviewed-by: Azeem Arshad <azeemarshad@chromium.org>
    > Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#784622}
    
    TBR=stevenjb@chromium.org,jonmann@chromium.org,azeemarshad@chromium.org
    
    Change-Id: I01038780de93c5ab35faa2ceb81198e0b4f5b405
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1091567
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278243
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784713}

--- chromeos/network/network_state_handler.cc
+++ chromeos/network/network_state_handler.cc
@@ -36,6 +36,9 @@ namespace chromeos {
 
 namespace {
 
+// Ignore changes to signal strength less than this value for active networks.
+const int kSignalStrengthChangeThreshold = 5;
+
 // Constants used for logging.
 constexpr char kReasonStateChange[] = "State Change";
 constexpr char kReasonChange[] = "New Network";
@@ -124,7 +127,7 @@ class NetworkStateHandler::ActiveNetworkState {
            activation_state_ == network->activation_state() &&
            connect_requested_ == network->connect_requested() &&
            (abs(signal_strength_ - network->signal_strength()) <
-            NetworkState::kSignalStrengthChangeThreshold) &&
+            kSignalStrengthChangeThreshold) &&
            network_technology_ == network->network_technology();
   }
 
