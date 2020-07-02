e1f139a9eaed1456ba5cb46d218fee333eebad1e
Thomas Lukaszewicz <tluk@chromium.org>
2020-07-02 05:11:30 +0000

    Improved TreeView test coverage
    Asserted paint test code to improve test coverage of TreeView and
    TreeViewDrawingProvider classes.
    
    Bug: None
    Change-Id: I0f1af957ff926cb64b42bc0f47ad4072e70f7ed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2267847
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784701}

--- ui/views/controls/tree/tree_view_unittest.cc
+++ ui/views/controls/tree/tree_view_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/accessibility/platform/ax_platform_node_delegate.h"
 #include "ui/base/models/tree_node_model.h"
+#include "ui/compositor/canvas_painter.h"
 #include "ui/views/accessibility/ax_virtual_view.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/accessibility/view_ax_platform_node_delegate.h"
@@ -163,11 +164,11 @@ class TreeViewTest : public ViewsTestBase {
 void TreeViewTest::SetUp() {
   ViewsTestBase::SetUp();
   widget_ = std::make_unique<Widget>();
-  Widget::InitParams params = CreateParams(Widget::InitParams::TYPE_WINDOW);
+  Widget::InitParams params =
+      CreateParams(Widget::InitParams::TYPE_WINDOW_FRAMELESS);
   params.bounds = gfx::Rect(0, 0, 200, 200);
   widget_->Init(std::move(params));
-  tree_ =
-      widget_->GetContentsView()->AddChildView(std::make_unique<TreeView>());
+  tree_ = widget_->SetContentsView(std::make_unique<TreeView>());
   tree_->RequestFocus();
 
   ViewAccessibility::AccessibilityEventsCallback accessibility_events_callback =
@@ -375,6 +376,16 @@ std::string TreeViewTest::InternalNodeAsString(TreeView::InternalNode* node) {
 TEST_F(TreeViewTest, MetadataTest) {
   tree_->SetModel(&model_);
   test::TestViewMetadata(tree_);
+}
+
+TEST_F(TreeViewTest, TreeViewPaintCoverage) {
+  tree_->SetModel(&model_);
+  SkBitmap bitmap;
+  gfx::Size size = tree_->size();
+  ui::CanvasPainter canvas_painter(&bitmap, size, 1.f, SK_ColorTRANSPARENT,
+                                   false);
+  widget_->GetRootView()->Paint(
+      PaintInfo::CreateRootPaintInfo(canvas_painter.context(), size));
 }
 
 // Verifies setting model correctly updates internal state.
