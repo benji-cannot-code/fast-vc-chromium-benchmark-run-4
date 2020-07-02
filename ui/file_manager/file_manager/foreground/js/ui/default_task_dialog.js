aa82c1b45cf222e96d58e618cccb45bd45ad9c3f
Luciano Pacheco <lucmult@chromium.org>
2020-07-02 08:07:26 +0000

    Files app: Fix list scroll for change default app dialog
    Add "pointer-events: none;" in the pseudo-element, to fix the clickable
    area for the last item in the list that. The pseudo-element covers the
    last item, so we avoid taking the click in the pseudo-element.
    
    Add scroll event listener to detect when the scroll has reached the
    bottom of the list and remove the CSS class "bottom-shadow" which
    controls when the gradient shadow should be displayed in the bottom of
    the list.
    
    Tested manually, integration tests coming in the dependent CL.
    
    Bug: 1100164
    Change-Id: Id27dca900f850fec088a5567182bcb4276f7264f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278745
    Reviewed-by: Alex Danilo <adanilo@chromium.org>
    Commit-Queue: Alex Danilo <adanilo@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784729}

--- ui/file_manager/file_manager/foreground/js/ui/default_task_dialog.js
+++ ui/file_manager/file_manager/foreground/js/ui/default_task_dialog.js
@@ -37,6 +37,15 @@ cr.define('cr.filebrowser', () => {
       this.list_.addEventListener('click', this.onSelected_.bind(this));
       this.list_.addEventListener('change', this.onListChange_.bind(this));
 
+      /**
+       * RequestAnimationFrame id used for throllting the list scroll event
+       * listener.
+       * @private {?number}
+       */
+      this.listScrollRaf_ = null;
+      this.list_.addEventListener(
+          'scroll', this.onListScroll_.bind(this), {passive: true});
+
       this.initialFocusElement_ = this.list_;
 
       /** @private {?function(*)} */
@@ -49,6 +58,21 @@ cr.define('cr.filebrowser', () => {
       this.list_.itemConstructor = function(item) {
         return self.renderItem(item);
       };
+    }
+
+    onListScroll_(event) {
+      if (this.listScrollRaf_ &&
+          !this.frame.classList.contains('scrollable-list')) {
+        return;
+      }
+
+      this.listScrollRaf_ = window.requestAnimationFrame(() => {
+        const atTheBottom = this.list_.scrollHeight - this.list_.scrollTop ===
+            this.list_.clientHeight;
+        this.frame.classList.toggle('bottom-shadow', !atTheBottom);
+
+        this.listScrollRaf_ = null;
+      });
     }
 
     /**
@@ -112,6 +136,8 @@ cr.define('cr.filebrowser', () => {
       for (let i = 0; i < items.length; i++) {
         this.dataModel_.push(items[i]);
       }
+      this.frame.classList.toggle('scrollable-list', items.length > 6);
+      this.frame.classList.toggle('bottom-shadow', items.length > 6);
       this.selectionModel_.selectedIndex = defaultIndex;
       this.list_.endBatchUpdates();
     }
