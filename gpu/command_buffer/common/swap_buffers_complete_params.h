3f0b860f6242dab6eedd62f2f8f6087a3959d78a
Peng Huang <penghuang@chromium.org>
2020-07-03 04:59:07 +0000

    Vulkan: tracking damage area in SkiaOutputDeviceVulkan
    Since max_frames_pending is 1 for SkiaOutputDeviceVulkan, so chrome will
    not start a new frame until SwapBuffersAck is received, so we can use
    tracking damage area in SkiaOutputDeviceVulkan, and pass the damage area
    to SkiaOutputSurfaceImpl via SwapBuffers completion callback. With it,
    VulkanSwapChain doesn't need to make sure the acquired images are in
    the present order. So remove related code as well.
    
    Change-Id: Iee95d3e43fcc5c6f79c443f9f07b132c29aa8a53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277976
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784980}

--- gpu/command_buffer/common/swap_buffers_complete_params.h
+++ gpu/command_buffer/common/swap_buffers_complete_params.h
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GPU_COMMAND_BUFFER_COMMON_SWAP_BUFFERS_COMPLETE_PARAMS_H_
 #define GPU_COMMAND_BUFFER_COMMON_SWAP_BUFFERS_COMPLETE_PARAMS_H_
 
+#include "base/optional.h"
 #include "gpu/command_buffer/common/texture_in_use_response.h"
 #include "ui/gfx/ca_layer_params.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/swap_result.h"
 
 namespace gpu {
@@ -21,6 +23,12 @@ struct GPU_EXPORT SwapBuffersCompleteParams {
   ~SwapBuffersCompleteParams();
 
   gfx::SwapResponse swap_response;
+
+  // Damage area of the current backing buffer compare to the previous swapped
+  // buffer. The renderer can use it as hint for minimizing drawing area for the
+  // next frame.
+  base::Optional<gfx::Rect> frame_buffer_damage_area;
+
   // Used only on macOS, for coordinating IOSurface reuse with the system
   // WindowServer.
   gpu::TextureInUseResponses texture_in_use_responses;
