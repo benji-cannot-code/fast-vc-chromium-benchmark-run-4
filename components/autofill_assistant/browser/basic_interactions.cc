7ccb2e81b5ff0bbd600734f2c60e69935134eb0f
Clemens Arbesser <arbesser@google.com>
2020-07-03 15:52:14 +0000

    [Autofill Assistant] Add opaque server payloads to user model.
    This CL also deprecates LoginOptionResponse, since the new field is a
    generalization of that proto.
    
    Bug: b/145043394
    Change-Id: I24dba4332f5f32b277b42b7a544a546eda03ed72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279896
    Commit-Queue: Clemens Arbesser <arbesser@google.com>
    Reviewed-by: Marian Fechete <marianfe@google.com>
    Reviewed-by: Luca Hunkeler <hluca@google.com>
    Cr-Commit-Position: refs/heads/master@{#785060}

--- components/autofill_assistant/browser/basic_interactions.cc
+++ components/autofill_assistant/browser/basic_interactions.cc
@@ -106,7 +106,7 @@ bool ValueToString(UserModel* user_model,
     case ValueProto::kUserActions:
     case ValueProto::kLoginOptions:
     case ValueProto::kCreditCardResponse:
-    case ValueProto::kLoginOptionResponse:
+    case ValueProto::kServerPayload:
       DVLOG(2) << "Error evaluating " << __func__
                << ": does not support values of type " << value->kind_case();
       return false;
@@ -208,7 +208,7 @@ bool ValueToString(UserModel* user_model,
       case ValueProto::kUserActions:
       case ValueProto::kLoginOptions:
       case ValueProto::kCreditCardResponse:
-      case ValueProto::kLoginOptionResponse:
+      case ValueProto::kServerPayload:
       case ValueProto::KIND_NOT_SET:
         NOTREACHED();
         return false;
@@ -373,8 +373,7 @@ bool CreateLoginOptionResponse(UserModel* user_model,
 
   // The result is intentionally not client_side_only, irrespective of input.
   ValueProto result;
-  result.mutable_login_option_response()->set_payload(
-      value->login_options().values(0).payload());
+  result.set_server_payload(value->login_options().values(0).payload());
   user_model->SetValue(result_model_identifier, result);
   return true;
 }
