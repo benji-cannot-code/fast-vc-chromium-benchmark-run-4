cc12a5eacd8e9463da642454769ad18da4042020
Kenichi Ishibashi <bashi@chromium.org>
2020-07-03 09:58:47 +0000

    Change signature of ServiceWorkerStorageControl::FindRegistrationForId
    This CL changes |origin| parameter of the method to an optional
    parameter. The motivation behind the change is to support
    FindRegistrationForIdOnly() without adding a new mojo method.
    Adding a mojo method has non trivial costs in terms of Android
    binary size. We can minimize binary size increase by merging
    FindRegistrationForIdOnly() into FindRegistrationForId().
    
    Bug: 1055677
    Change-Id: I27340a42285583a74aca5bad85da545f0d731d2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274866
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785018}

--- content/browser/service_worker/service_worker_storage_control_impl_unittest.cc
+++ content/browser/service_worker/service_worker_storage_control_impl_unittest.cc
@@ -217,8 +217,9 @@ class ServiceWorkerStorageControlImplTest : public testing::Test {
     return return_value;
   }
 
-  FindRegistrationResult FindRegistrationForId(int64_t registration_id,
-                                               const GURL& origin) {
+  FindRegistrationResult FindRegistrationForId(
+      int64_t registration_id,
+      const base::Optional<GURL>& origin) {
     FindRegistrationResult return_value;
     base::RunLoop loop;
     storage()->FindRegistrationForId(
@@ -688,6 +689,12 @@ TEST_F(ServiceWorkerStorageControlImplTest, FindRegistration_NoRegistration) {
         FindRegistrationForId(kRegistrationId, kScope.GetOrigin());
     EXPECT_EQ(result->status, DatabaseStatus::kErrorNotFound);
   }
+
+  {
+    FindRegistrationResult result =
+        FindRegistrationForId(kRegistrationId, base::nullopt);
+    EXPECT_EQ(result->status, DatabaseStatus::kErrorNotFound);
+  }
 }
 
 // Tests that storing/finding/deleting a registration work.
@@ -742,6 +749,8 @@ TEST_F(ServiceWorkerStorageControlImplTest, StoreAndDeleteRegistration) {
     result = FindRegistrationForScope(kScope);
     EXPECT_EQ(result->status, DatabaseStatus::kOk);
     result = FindRegistrationForId(kRegistrationId, kScope.GetOrigin());
+    EXPECT_EQ(result->status, DatabaseStatus::kOk);
+    result = FindRegistrationForId(kRegistrationId, base::nullopt);
     EXPECT_EQ(result->status, DatabaseStatus::kOk);
   }
 
