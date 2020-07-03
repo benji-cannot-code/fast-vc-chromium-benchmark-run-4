dba08b2e9eeb6565810560d62cad60f134e3353a
Mark Cogan <marq@google.com>
2020-07-03 15:22:07 +0000

    [iOS] Add SceneStateBrowserAgent
    This CL adds a browser agent (SceneStateBrowserAgent) to associate
    a scene state with a Browser, so that coordinators and other objects
    can access SceneState facilities if they have a Browser.
    
    This approach is preferred over the (simpler) mechanism of just adding
    a SceneState member to Browser. Conceptually that would be a layering
    violation since SceneState is part of the UI layer.
    
    BrowserViewWrangler is responsible for associating the SceneState with
    each browser when it is created. Since this involves injecting UI-layer
    information, it can't be handled in AttachBrowserAgents(). To do this,
    BrowserViewWrangler is now initialized with a SceneState. This also
    means that the BVW's sessionID doesn't need to be added separately.
    
    Code that needs access to the scene state given a browser should use:
    
    SceneStateBrowserAgent::FromBrowser(browser)->GetSceneState().
    
    Bug: 1098268
    Change-Id: I73997a848163e97d0ad579d862482b5a21fb1a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278056
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785046}

--- ios/chrome/browser/ui/main/browser_view_wrangler.mm
+++ ios/chrome/browser/ui/main/browser_view_wrangler.mm
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/commands/application_commands.h"
 #import "ios/chrome/browser/ui/commands/browsing_data_commands.h"
 #import "ios/chrome/browser/ui/commands/command_dispatcher.h"
+#import "ios/chrome/browser/ui/main/scene_state.h"
+#import "ios/chrome/browser/ui/main/scene_state_browser_agent.h"
 #import "ios/chrome/browser/ui/util/multi_window_support.h"
 #import "ios/chrome/browser/web_state_list/web_state_list.h"
 
@@ -92,6 +94,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @interface BrowserViewWrangler () {
   ChromeBrowserState* _browserState;
+  SceneState* _sceneState;
   __weak id<ApplicationCommands> _applicationCommandEndpoint;
   __weak id<BrowsingDataCommands> _browsingDataCommandEndpoint;
   BOOL _isShutdown;
@@ -99,6 +102,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   std::unique_ptr<Browser> _mainBrowser;
   std::unique_ptr<Browser> _otrBrowser;
 }
+
+// Opaque session ID from _sceneState, nil when multi-window isn't enabled.
+@property(nonatomic, readonly) NSString* sessionID;
 
 @property(nonatomic, strong, readwrite) WrangledBrowser* mainInterface;
 @property(nonatomic, strong, readwrite) WrangledBrowser* incognitoInterface;
@@ -109,8 +115,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @property(nonatomic, readonly) Browser* mainBrowser;
 @property(nonatomic, readonly) Browser* otrBrowser;
 
-// Setters for the main and otr Browsers.
-- (void)setMainBrowser:(std::unique_ptr<Browser>)browser;
+// The main browser can't be set after creation, but they can be
+// cleared (setting them to nullptr).
+- (void)clearMainBrowser;
+// The OTR browser can be reset after creation.
 - (void)setOtrBrowser:(std::unique_ptr<Browser>)browser;
 
 // Creates a new off-the-record ("incognito") browser state for |_browserState|,
@@ -127,12 +135,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @synthesize currentInterface = _currentInterface;
 
 - (instancetype)initWithBrowserState:(ChromeBrowserState*)browserState
+                          sceneState:(SceneState*)sceneState
           applicationCommandEndpoint:
               (id<ApplicationCommands>)applicationCommandEndpoint
          browsingDataCommandEndpoint:
              (id<BrowsingDataCommands>)browsingDataCommandEndpoint {
   if ((self = [super init])) {
     _browserState = browserState;
+    _sceneState = sceneState;
     _applicationCommandEndpoint = applicationCommandEndpoint;
     _browsingDataCommandEndpoint = browsingDataCommandEndpoint;
   }
@@ -148,10 +158,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BrowserList* browserList =
       BrowserListFactory::GetForBrowserState(_mainBrowser->GetBrowserState());
   browserList->AddBrowser(_mainBrowser.get());
+
+  // Associate |_sceneState| with the new browser.
+  SceneStateBrowserAgent::CreateForBrowser(_mainBrowser.get(), _sceneState);
+
   [self dispatchToEndpointsForBrowser:_mainBrowser.get()];
 
   SessionRestorationBrowserAgent::FromBrowser(_mainBrowser.get())
-      ->SetSessionID(base::SysNSStringToUTF8(_sessionID));
+      ->SetSessionID(base::SysNSStringToUTF8(self.sessionID));
   SessionRestorationBrowserAgent::FromBrowser(_mainBrowser.get())
       ->RestoreSession();
   breakpad::MonitorTabStateForWebStateList(_mainBrowser->GetWebStateList());
@@ -168,6 +182,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 #pragma mark - BrowserViewInformation property implementations
+
+- (NSString*)sessionID {
+  NSString* sessionID = nil;
+  if (IsMultiwindowSupported()) {
+    if (@available(iOS 13, *)) {
+      sessionID = _sceneState.scene.session.persistentIdentifier;
+    }
+  }
+  return sessionID;
+}
 
 - (void)setCurrentInterface:(WrangledBrowser*)interface {
   DCHECK(interface);
@@ -226,7 +250,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return _otrBrowser.get();
 }
 
-- (void)setMainBrowser:(std::unique_ptr<Browser>)mainBrowser {
+- (void)clearMainBrowser {
   if (_mainBrowser.get()) {
     WebStateList* webStateList = self.mainBrowser->GetWebStateList();
     breakpad::StopMonitoringTabStateForWebStateList(webStateList);
@@ -234,7 +258,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self.mainBrowser->GetTabModel() disconnect];
   }
 
-  _mainBrowser = std::move(mainBrowser);
+  _mainBrowser = nullptr;
+  ;
 }
 
 - (void)setOtrBrowser:(std::unique_ptr<Browser>)otrBrowser {
@@ -342,7 +367,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Handles removing observers, stopping breakpad monitoring, and closing all
   // tabs.
-  [self setMainBrowser:nullptr];
+  [self clearMainBrowser];
   [self setOtrBrowser:nullptr];
 
   _browserState = nullptr;
@@ -363,11 +388,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   browserList->AddIncognitoBrowser(browser.get());
   [self dispatchToEndpointsForBrowser:browser.get()];
   SessionRestorationBrowserAgent::FromBrowser(browser.get())
-      ->SetSessionID(base::SysNSStringToUTF8(_sessionID));
+      ->SetSessionID(base::SysNSStringToUTF8(self.sessionID));
   if (restorePersistedState) {
     SessionRestorationBrowserAgent::FromBrowser(browser.get())
         ->RestoreSession();
   }
+
+  // Associate the same SceneState with the new OTR browser as is associated
+  // with the main browser.
+  SceneStateBrowserAgent::CreateForBrowser(browser.get(), _sceneState);
 
   breakpad::MonitorTabStateForWebStateList(browser->GetWebStateList());
 
