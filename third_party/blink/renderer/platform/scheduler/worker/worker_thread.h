706be601c9f7354fe44f1133ff395ac3978a88e2
Carlos Caballero <carlscab@google.com>
2020-07-03 08:35:22 +0000

    Remove unused include message_loop_current.h
    Bug: 891670
    Change-Id: I4494a3b08928c78785329363c6d14b1d80d7b005
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279771
    Reviewed-by: Sreeja Kamishetty <sreejakshetty@chromium.org>
    Commit-Queue: Carlos Caballero <carlscab@google.com>
    Cr-Commit-Position: refs/heads/master@{#785000}

--- third_party/blink/renderer/platform/scheduler/worker/worker_thread.h
+++ third_party/blink/renderer/platform/scheduler/worker/worker_thread.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_SCHEDULER_WORKER_WORKER_THREAD_H_
 
 #include "base/callback_forward.h"
-#include "base/message_loop/message_loop_current.h"
 #include "base/run_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/atomic_flag.h"
