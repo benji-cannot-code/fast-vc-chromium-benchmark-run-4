80b37492785785acd1025bdbe5bb4e600d0091c3
Jana Grill <janagrill@chromium.org>
2020-07-03 14:48:47 +0000

    Fix click on ADB policy change notifications
    Clicking on the body of any ADB policy change notification should not
    trigger a restart, therefore it should be explicitly checked that the
    |button_index| is set to 0 before triggering a restart.
    
    Bug: 1101428
    Test: Trigger a notification and then check that no restart is triggered when the notification body is clicked.
    Change-Id: I26fc0bb365c5b4a6d3373637fe54c79c36424d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277995
    Commit-Queue: Jana Grill <janagrill@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785037}

--- chrome/browser/chromeos/ui/adb_sideloading_policy_change_notification.h
+++ chrome/browser/chromeos/ui/adb_sideloading_policy_change_notification.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_UI_ADB_SIDELOADING_POLICY_CHANGE_NOTIFICATION_H_
 
 #include "base/memory/weak_ptr.h"
+#include "base/optional.h"
 
 namespace chromeos {
 
@@ -43,7 +44,7 @@ class AdbSideloadingPolicyChangeNotification {
   virtual ~AdbSideloadingPolicyChangeNotification();
 
   virtual void Show(Type type);
-  void HandleNotificationClick();
+  void HandleNotificationClick(base::Optional<int> button_index);
 
  private:
   base::WeakPtrFactory<AdbSideloadingPolicyChangeNotification>
