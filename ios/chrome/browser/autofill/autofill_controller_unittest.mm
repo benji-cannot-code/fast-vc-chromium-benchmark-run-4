049ace227cee5a3146060ccfb4e80fca257353b9
Olivier Robin <olivierrobin@google.com>
2020-07-03 12:18:52 +0000

    Disable AutofillControllerTest.ProfileSuggestionsTwoAnonymousForms
    Test is failing after https://chromium-review.googlesource.com/c/chromium/src/+/2236396
    
    Bug: 1101971
    Change-Id: I042c6ed8f3d0686f948973a323c8644b1ca4913a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279781
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785029}

--- ios/chrome/browser/autofill/autofill_controller_unittest.mm
+++ ios/chrome/browser/autofill/autofill_controller_unittest.mm
@@ -453,7 +453,8 @@ TEST_F(AutofillControllerTest, ProfileSuggestions) {
 
 // Tests that the system is able to offer suggestions for an anonymous form when
 // there is another anonymous form on the page.
-TEST_F(AutofillControllerTest, ProfileSuggestionsTwoAnonymousForms) {
+// TODO(crbug.com/1101971): Reenable the test.
+TEST_F(AutofillControllerTest, DISABLED_ProfileSuggestionsTwoAnonymousForms) {
   SetUpForSuggestions(
       [NSString stringWithFormat:@"%@%@", kProfileFormHtml, kProfileFormHtml],
       1);
