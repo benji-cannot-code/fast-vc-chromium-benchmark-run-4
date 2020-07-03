34b8dd834a1e41a4fbc2c2b957d75a366b6cce63
Peter Kotwicz <pkotwicz@chromium.org>
2020-07-03 01:09:58 +0000

    Deprecate Mobile.Splash.TranslucencyRemovalFailed
    The metric was added a year ago. According to the metric
    Activity#convertFromTranslucent() never fails.
    
    BUG=1099566
    
    Change-Id: I678b8037f45189f8ee17375645b8dff644bd6f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2269583
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#784952}

--- chrome/android/java/src/org/chromium/chrome/browser/browserservices/ui/splashscreen/SplashController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browserservices/ui/splashscreen/SplashController.java
@@ -18,7 +18,6 @@ import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
-import org.chromium.base.Log;
 import org.chromium.base.ObserverList;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.metrics.RecordHistogram;
@@ -325,10 +324,6 @@ public class SplashController
             method.setAccessible(true);
             method.invoke(mActivity);
         } catch (ReflectiveOperationException e) {
-            // Method not found or threw an exception.
-            RecordHistogram.recordBooleanHistogram("Mobile.Splash.TranslucencyRemovalFailed", true);
-            assert false : "Failed to remove activity translucency reflectively";
-            Log.e(TAG, "Failed to remove activity translucency reflectively");
         }
 
         notifyTranslucencyRemoved();
