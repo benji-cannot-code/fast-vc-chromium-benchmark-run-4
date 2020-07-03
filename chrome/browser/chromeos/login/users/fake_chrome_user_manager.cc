22644c86749ba1ede2dddaec58d4d2f3883a2eea
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-07-03 15:45:21 +0000

    [Kiosk] Remove redundant methods ***KioskAppLoggedIn
    These methods have almost the same implementation, yet they are
    duplicated. Let's get rid of them.
    
    Bug: 1015383
    Change-Id: Ib41532e52d73b28b59ee552d40b272d5bf98de05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279123
    Commit-Queue: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785056}

--- chrome/browser/chromeos/login/users/fake_chrome_user_manager.cc
+++ chrome/browser/chromeos/login/users/fake_chrome_user_manager.cc
@@ -716,10 +716,6 @@ void FakeChromeUserManager::DemoAccountLoggedIn() {
 
 void FakeChromeUserManager::KioskAppLoggedIn(user_manager::User* user) {}
 
-void FakeChromeUserManager::ArcKioskAppLoggedIn(user_manager::User* user) {}
-
-void FakeChromeUserManager::WebKioskAppLoggedIn(user_manager::User* user) {}
-
 void FakeChromeUserManager::PublicAccountUserLoggedIn(
     user_manager::User* user) {
   NOTREACHED();
