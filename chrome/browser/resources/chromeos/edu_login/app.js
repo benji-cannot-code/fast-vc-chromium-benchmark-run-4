53024b227af006ecedb44f90feee5d11e0c845aa
Anastasiia Nikolaienko <anastasiian@chromium.org>
2020-07-03 16:15:31 +0000

    Add error screens to EDU account addition flow
    Add 'No internet' and 'Cannot add account' error screens.
    
    Bug: 1097030
    Change-Id: I400ffa1677fa797cf43c2b020f34dde9af51ed1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2259913
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785065}

--- chrome/browser/resources/chromeos/edu_login/app.js
+++ chrome/browser/resources/chromeos/edu_login/app.js
@@ -8,6 +8,7 @@ import './edu_login_parents.js';
 import './edu_login_parent_signin.js';
 import './edu_login_parent_info.js';
 import './edu_login_signin.js';
+import './edu_login_error.js';
 import 'chrome://resources/cr_elements/cr_view_manager/cr_view_manager.m.js';
 
 import {assert} from 'chrome://resources/js/assert.m.js';
@@ -70,6 +71,15 @@ Polymer({
      * @private {?EduLoginParams}
      */
     loginParams_: Object,
+
+    /**
+     * Whether the error screen should be shown.
+     * @private {boolean}
+     */
+    isErrorShown_: {
+      type: Boolean,
+      value: false,
+    }
   },
 
   listeners: {
