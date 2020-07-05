f1bf69ac438c635e2846c58b8482760566b853f7
Anastasiia Nikolaienko <anastasiian@chromium.org>
2020-07-05 23:06:50 +0000

    Show 'Cannot add account' error
    Show 'Cannot add account' error in EDU login flow, if we receive an
    error from handler.
    
    Bug: 1097030
    Change-Id: I667c031b9c638560764db512e8f931203dec0f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2259993
    Commit-Queue: Anastasiia N <anastasiian@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785242}

--- chrome/browser/resources/chromeos/edu_login/app.js
+++ chrome/browser/resources/chromeos/edu_login/app.js
@@ -15,7 +15,7 @@ import {assert} from 'chrome://resources/js/assert.m.js';
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {EduAccountLoginBrowserProxyImpl} from './browser_proxy.js';
-import {EduCoexistenceFlowResult, EduLoginParams, ParentAccount} from './edu_login_util.js';
+import {EduCoexistenceFlowResult, EduLoginErrorType, EduLoginParams, ParentAccount} from './edu_login_util.js';
 
 /** @enum {string} */
 const Steps = {
@@ -79,12 +79,19 @@ Polymer({
     isErrorShown_: {
       type: Boolean,
       value: false,
-    }
+    },
+
+    /** @private {EduLoginErrorType} */
+    errorType_: {
+      type: String,
+      value: '',
+    },
   },
 
   listeners: {
     'go-next': 'onGoNext_',
     'go-back': 'onGoBack_',
+    'edu-login-error': 'onError_',
   },
 
   /** @override */
@@ -136,5 +143,14 @@ Polymer({
     assert(result !== undefined);
     EduAccountLoginBrowserProxyImpl.getInstance()
         .updateEduCoexistenceFlowResult(result);
+  },
+
+  /**
+   * @param {!Event} e
+   * @private
+   */
+  onError_(e) {
+    this.errorType_ = e.detail.errorType;
+    this.isErrorShown_ = true;
   },
 });
