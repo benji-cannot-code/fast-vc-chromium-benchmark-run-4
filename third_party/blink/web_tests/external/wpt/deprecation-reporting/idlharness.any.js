76f39c175e99878b5a85a2121ab132026713fae3
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2020-07-06 19:53:32 +0000

    Import wpt@ca5a211e4f8ed3ed2ae3a3e4738e43ecc47fbbf8
    Using wpt-import in Chromium 02ac5e4a6def817b40771d5d823d7482383d90f5.
    With Chromium commits locally applied on WPT:
    62915de102 "Remove .tentative flag from Split Cache Web Platform Test"
    
    
    Note to sheriffs: This CL imports external tests and adds
    expectations for those tests; if this CL is large and causes
    a few new failures, please fix the failures by adding new
    lines to TestExpectations rather than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/master/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hbos@chromium.org, hta@chromium.org:
      external/wpt/webrtc
    hongchan@chromium.org, rtoy@chromium.org:
      external/wpt/webaudio
    rego@igalia.com:
      external/wpt/css/css-ui
    
    NOAUTOREVERT=true
    TBR=robertma@google.com
    
    No-Export: true
    Change-Id: I0cfdf59d06bd4cb52da283de065a9026619fe4e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283228
    Reviewed-by: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/master@{#785501}

---
+++ third_party/blink/web_tests/external/wpt/deprecation-reporting/idlharness.any.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/WebIDLParser.js
+// META: script=/resources/idlharness.js
+
+'use strict';
+
+idl_test(
+  ['deprecation-reporting'],
+  ['reporting'],
+  idl_array => {
+    idl_array.add_objects({
+      // TODO: objects
+    });
+  }
+);
