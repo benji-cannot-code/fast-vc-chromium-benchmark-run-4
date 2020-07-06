a98a48431919fe79dd6f79c2ca1500575cec8fed
Monica Basta <msalama@chromium.org>
2020-07-06 15:58:57 +0000

    [ProfilePicker]: Adjust native window size based on mocks.
    Bug: 1063856
    Change-Id: I124ebf8f9ed869af592c0774350a49345b7d4781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279826
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785406}

--- chrome/browser/ui/views/profiles/profile_picker_view.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view.cc
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 ProfilePickerView* g_profile_picker_view = nullptr;
-constexpr int kWindowWidth = 800;
-constexpr int kWindowHeight = 600;
+constexpr int kWindowWidth = 1024;
+constexpr int kWindowHeight = 758;
 }  // namespace
 
 // static
