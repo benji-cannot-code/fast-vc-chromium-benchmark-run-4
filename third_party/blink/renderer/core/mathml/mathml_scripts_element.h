c35a420dbbe5041d569d3f9c6e4169c521def8c9
Rob Buis <rbuis@igalia.com>
2020-07-06 21:22:44 +0000

    [mathml] Implement the mmultiscripts element
    Implement mmultiscripts element [1] and its possible
    children <mprescripts> and <none>.
    
    [1] https://mathml-refresh.github.io/mathml-core/#prescripts-and-tensor-indices-mmultiscripts
    
    Bug: 6606
    Change-Id: Ia9e48d50902546d7527e4ba063838c4a9f0d2b9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264334
    Commit-Queue: Rob Buis <rbuis@igalia.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785551}

--- third_party/blink/renderer/core/mathml/mathml_scripts_element.h
+++ third_party/blink/renderer/core/mathml/mathml_scripts_element.h
@@ -12,7 +12,15 @@ namespace blink {
 
 class Document;
 
-enum class MathScriptType { kSub, kSuper, kSubSup, kUnder, kOver, kUnderOver };
+enum class MathScriptType {
+  kSub,
+  kSuper,
+  kSubSup,
+  kMultiscripts,
+  kUnder,
+  kOver,
+  kUnderOver
+};
 
 class CORE_EXPORT MathMLScriptsElement : public MathMLElement {
  public:
@@ -40,7 +48,8 @@ struct DowncastTraits<MathMLScriptsElement> {
            mathml_element.HasTagName(mathml_names::kMunderoverTag) ||
            mathml_element.HasTagName(mathml_names::kMsubTag) ||
            mathml_element.HasTagName(mathml_names::kMsupTag) ||
-           mathml_element.HasTagName(mathml_names::kMsubsupTag);
+           mathml_element.HasTagName(mathml_names::kMsubsupTag) ||
+           mathml_element.HasTagName(mathml_names::kMmultiscriptsTag);
   }
 };
 
