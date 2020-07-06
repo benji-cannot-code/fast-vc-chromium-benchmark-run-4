814fee0caacaffa29c6cc6a2275799766a4256df
Bo Liu <boliu@chromium.org>
2020-07-06 23:01:14 +0000

    base: Fix bindService with empty intent
    This is a follow up bug fix from
    https://chromium-review.googlesource.com/c/chromium/src/+/2233312
    
    Bug happens when:
    The fallback timeout is triggered at once so sFallbackEnabled
    Try to bind to a non-sandboxed process, which does not have a fallback
    so mFallbackServiceName is null.
    
    In this case, the intent sent to bindService is just empty, thus it's
    going to fail. Fix by checking for mFallbackServiceName being null, and
    avoid using it if that's the case.
    
    Bug: 994961
    Change-Id: I51e62898c0c1cadd4a89ce184c59ba47e2b1f5c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283393
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Bo <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785567}

--- base/android/java/src/org/chromium/base/process_launcher/ChildProcessConnection.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildProcessConnection.java
@@ -290,7 +290,8 @@ public class ChildProcessConnection {
             }
         };
 
-        createBindings(sFallbackEnabled ? mFallbackServiceName : mServiceName);
+        createBindings(sFallbackEnabled && mFallbackServiceName != null ? mFallbackServiceName
+                                                                        : mServiceName);
     }
 
     private void createBindings(ComponentName serviceName) {
