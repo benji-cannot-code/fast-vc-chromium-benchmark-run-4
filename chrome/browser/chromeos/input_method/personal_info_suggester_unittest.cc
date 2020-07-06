be4897e47ba63e0df68bb5b488ba2987f3cfde79
Jing Wang <jiwan@chromium.org>
2020-07-06 06:41:13 +0000

    Suggest full address in personal info suggester.
    Now we will suggest a full address including city, state and zip code.
    Note that although the country field will not be included in the
    suggestion, it is essential for generating the full address correctly.
    
    Test: tested on Chrome on Linux, updated the unit test.
    Bug: 1101654
    Change-Id: I221b0cb81079342129e768eeaba0f3210394292e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278902
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785289}

--- chrome/browser/chromeos/input_method/personal_info_suggester_unittest.cc
+++ chrome/browser/chromeos/input_method/personal_info_suggester_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/constants/chromeos_pref_names.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
+#include "components/autofill/core/browser/geo/country_names.h"
 #include "components/autofill/core/browser/test_autofill_client.h"
 #include "components/autofill/core/browser/test_personal_data_manager.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -176,7 +177,8 @@ class PersonalInfoSuggesterTest : public testing::Test {
   const base::string16 first_name_ = base::UTF8ToUTF16("John");
   const base::string16 last_name_ = base::UTF8ToUTF16("Wayne");
   const base::string16 full_name_ = base::UTF8ToUTF16("John Wayne");
-  const base::string16 address_ = base::UTF8ToUTF16("1 Dream Road Hollywood");
+  const base::string16 address_ =
+      base::UTF8ToUTF16("1 Dream Road, Hollywood, CA 12345");
   const base::string16 phone_number_ = base::UTF8ToUTF16("16505678910");
 };
 
@@ -227,10 +229,19 @@ TEST_F(PersonalInfoSuggesterTest, SuggestNames) {
 }
 
 TEST_F(PersonalInfoSuggesterTest, SuggestAddress) {
+  autofill::CountryNames::SetLocaleString("en-US");
   autofill::AutofillProfile autofill_profile(base::GenerateGUID(),
                                              autofill::test::kEmptyOrigin);
-  autofill_profile.SetRawInfo(
-      autofill::ServerFieldType::ADDRESS_HOME_STREET_ADDRESS, address_);
+  autofill_profile.SetRawInfo(autofill::ServerFieldType::ADDRESS_HOME_LINE1,
+                              base::UTF8ToUTF16("1 Dream Road"));
+  autofill_profile.SetRawInfo(autofill::ServerFieldType::ADDRESS_HOME_CITY,
+                              base::UTF8ToUTF16("Hollywood"));
+  autofill_profile.SetRawInfo(autofill::ServerFieldType::ADDRESS_HOME_ZIP,
+                              base::UTF8ToUTF16("12345"));
+  autofill_profile.SetRawInfo(autofill::ServerFieldType::ADDRESS_HOME_STATE,
+                              base::UTF8ToUTF16("CA"));
+  autofill_profile.SetRawInfo(autofill::ServerFieldType::ADDRESS_HOME_COUNTRY,
+                              base::UTF8ToUTF16("US"));
   personal_data_->AddProfile(autofill_profile);
 
   suggester_->Suggest(base::UTF8ToUTF16("my address is "));
