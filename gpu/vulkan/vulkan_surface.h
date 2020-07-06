3f49fd87c7475fde795e34da11e8990ef56ee699
Peng Huang <penghuang@chromium.org>
2020-07-06 13:58:36 +0000

    vulkan: fix a crash in SkiaOutputDeviceVulkan.
    The crash is because we assume vulkan driver will always create
    swapchain with the number of images which is specified by
    VkSwapchainCreateInfoKHR::minImageCount, but sometimes, the driver
    will not obey it, and then it causes problems. fix the crash by using
    the number from the swapchain instead of assuming it.
    
    Bug: 1099132
    Change-Id: I70868d1bf9efee4d845ff21a236af33eac401ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2281177
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Peng Huang <penghuang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785357}

--- gpu/vulkan/vulkan_surface.h
+++ gpu/vulkan/vulkan_surface.h
@@ -67,7 +67,6 @@ class COMPONENT_EXPORT(VULKAN) VulkanSurface {
   const gfx::Size& image_size() const { return image_size_; }
   VkImageUsageFlags image_usage_flags() const { return image_usage_flags_; }
   gfx::OverlayTransform transform() const { return transform_; }
-  uint32_t image_count() const { return image_count_; }
   VkSurfaceFormatKHR surface_format() const { return surface_format_; }
 
  private:
@@ -93,9 +92,6 @@ class COMPONENT_EXPORT(VULKAN) VulkanSurface {
 
   // Swap chain pre-transform.
   gfx::OverlayTransform transform_ = gfx::OVERLAY_TRANSFORM_INVALID;
-
-  // Swap chain image count.
-  uint32_t image_count_ = 0u;
 
   std::unique_ptr<VulkanSwapChain> swap_chain_;
 
