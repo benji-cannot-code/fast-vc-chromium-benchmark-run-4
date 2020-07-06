067cd757c3ac2a39eccd2d3f875230f5e882a54c
Lei Zhang <thestig@chromium.org>
2020-07-06 21:10:34 +0000

    Get rid of the dependency from pdf/ to chrome/.
    Instead of directly using enum ContentRestrictions from
    chrome/common/content_restriction.h, introduce a new enum at the PPAPI
    layer that has the exact same values and use that instead.
    
    Bug: 1100703
    Change-Id: I89424aaff0f304af2621fd31b41dc71d21526077
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274962
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785546}

--- chrome/browser/ui/pdf/chrome_pdf_web_contents_helper_client.cc
+++ chrome/browser/ui/pdf/chrome_pdf_web_contents_helper_client.cc
@@ -7,9 +7,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/download/download_stats.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
+#include "chrome/common/content_restriction.h"
 #include "extensions/browser/guest_view/mime_handler_view/mime_handler_view_guest.h"
+#include "ppapi/c/private/ppb_pdf.h"
 
 namespace {
+
+// For the UpdateContentRestrictions() call below, ensure the enum values in
+// chrome/common/content_restriction.h and ppapi/c/private/ppb_pdf.h match.
+#define STATIC_ASSERT_ENUM(a, b)                            \
+  static_assert(static_cast<int>(a) == static_cast<int>(b), \
+                "mismatching enums: " #a)
+
+STATIC_ASSERT_ENUM(CONTENT_RESTRICTION_COPY, PP_CONTENT_RESTRICTION_COPY);
+STATIC_ASSERT_ENUM(CONTENT_RESTRICTION_CUT, PP_CONTENT_RESTRICTION_CUT);
+STATIC_ASSERT_ENUM(CONTENT_RESTRICTION_PASTE, PP_CONTENT_RESTRICTION_PASTE);
+STATIC_ASSERT_ENUM(CONTENT_RESTRICTION_PRINT, PP_CONTENT_RESTRICTION_PRINT);
+STATIC_ASSERT_ENUM(CONTENT_RESTRICTION_SAVE, PP_CONTENT_RESTRICTION_SAVE);
 
 content::WebContents* GetWebContentsToUse(
     content::WebContents* web_contents) {
@@ -37,7 +51,7 @@ void ChromePDFWebContentsHelperClient::UpdateContentRestrictions(
 
   CoreTabHelper* core_tab_helper =
       CoreTabHelper::FromWebContents(web_contents_to_use);
-  // |core_tab_helper| is NULL for WebViewGuest.
+  // |core_tab_helper| is null for WebViewGuest.
   if (core_tab_helper)
     core_tab_helper->UpdateContentRestrictions(content_restrictions);
 }
