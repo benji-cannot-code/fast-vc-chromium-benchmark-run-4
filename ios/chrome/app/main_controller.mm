46166f8f04f8c5d3ac4649e857f9d5547ba01e05
Julian Mentasti-Meza <jmentasti@google.com>
2020-07-06 19:37:00 +0000

    Reland "[iOS] Single screen screenshot metric action."
    This reverts commit 1b493129ec31916f2ec5a31ff98a9be81facc9d5.
    
    Reason for revert: Reverting this revert because there is a fix to the issue.
    
    Original change's description:
    > Revert "[iOS] Single screen screenshot metric action."
    >
    > This reverts commit 7a90fb2e7fe1ae3cc51857767ab626372c20311c.
    >
    > Reason for revert:
    >
    > Test fails on iphone-device builder.
    > https://ci.chromium.org/p/chrome/builders/ci/iphone-device/9322
    > https://ci.chromium.org/p/chrome/builders/ci/iphone-device/9323
    > https://ci.chromium.org/p/chrome/builders/ci/iphone-device/9324
    >
    > Original change's description:
    > > [iOS] Single screen screenshot metric action.
    > >
    > > Creation of the 'MobileSingleScreenScreenshot' metric and its collection when a user takes a screenshot of a single screen.
    > >
    > > Change-Id: Ib95e031a36f922450f602e6fdfb7ccf14928cf76
    > > Bug: 1065909
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2218552
    > > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > > Reviewed-by: Mark Cogan <marq@chromium.org>
    > > Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    > > Commit-Queue: Julian Mentasti-Meza <jmentasti@google.com>
    > > Cr-Commit-Position: refs/heads/master@{#780918}
    >
    > TBR=rkaplow@chromium.org,marq@chromium.org,seblalancette@chromium.org,jmentasti@google.com
    >
    > Change-Id: I4813e9434c61f00c31401e902778fddcfbad5517
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 1065909
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258898
    > Reviewed-by: edchin <edchin@chromium.org>
    > Commit-Queue: edchin <edchin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#781083}
    
    
    Bug: 1065909
    Change-Id: I45aaf2bb556313ece6aadcb91e8b385cfd343461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264618
    Commit-Queue: Julian Mentasti-Meza <jmentasti@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: edchin <edchin@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785495}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ios/chrome/browser/ntp_snippets/ios_chrome_content_suggestions_service_factory.h"
 #import "ios/chrome/browser/omaha/omaha_service.h"
 #include "ios/chrome/browser/pref_names.h"
+#import "ios/chrome/browser/screenshot/screenshot_metrics_recorder.h"
 #import "ios/chrome/browser/search_engines/extension_search_engine_data_updater.h"
 #include "ios/chrome/browser/search_engines/search_engines_util.h"
 #include "ios/chrome/browser/search_engines/template_url_service_factory.h"
@@ -269,6 +270,10 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
 
 // The ChromeBrowserState associated with the main (non-OTR) browsing mode.
 @property(nonatomic, assign) ChromeBrowserState* mainBrowserState;  // Weak.
+
+// Handles collecting metrics on user triggered screenshots
+@property(nonatomic, strong)
+    ScreenshotMetricsRecorder* screenshotMetricsRecorder;
 
 // Returns whether the restore infobar should be displayed.
 - (bool)mustShowRestoreInfobar;
@@ -564,6 +569,9 @@ void MainControllerAuthenticationServiceDelegate::ClearBrowsingData(
 
   // Now that everything is properly set up, run the tests.
   tests_hook::RunTestsIfPresent();
+
+  self.screenshotMetricsRecorder = [[ScreenshotMetricsRecorder alloc] init];
+  [self.screenshotMetricsRecorder startRecordingMetrics];
 }
 
 - (void)startUpBrowserForegroundInitialization {
