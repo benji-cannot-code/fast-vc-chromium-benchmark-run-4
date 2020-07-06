9d413d14a4686cd1675ef9657b9f7e1c3e3c1be5
Maksim Ivanov <emaxx@chromium.org>
2020-07-06 11:10:29 +0000

    Introduce ExtensionForceInstallMixin for tests
    Create a test mixin ExtensionForceInstallMixin that helps in testing of
    force-installed extensions.
    
    In this initial version, only device policy faked via
    DevicePolicyCrosTestHelper is supported; the extension has to be already
    packed into a CRX file. The mixin hosts the CRX file in an embedded test
    server, generates and hosts an update manifest XML and configures the
    device policy.
    
    Also partially migrate signin_profile_extensions_policy_browsertest.cc
    to the new mixin.
    
    Bug: 1090941
    Change-Id: Ic2b82dc15d0bfcf18e68d267d78312ce2311789f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277812
    Reviewed-by: Omar Morsi <omorsi@google.com>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785327}

--- chrome/browser/chromeos/policy/signin_profile_extensions_policy_browsertest.cc
+++ chrome/browser/chromeos/policy/signin_profile_extensions_policy_browsertest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/macros.h"
+#include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
 #include "base/version.h"
@@ -16,8 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/extensions/crx_installer.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/policy/extension_force_install_mixin.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/common/chrome_paths.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/notification_details.h"
@@ -55,8 +58,8 @@ namespace {
 // * The manual testing app which is whitelisted for running in the sign-in
 //   profile:
 const char kWhitelistedAppId[] = "bjaiihebfngildkcjkjckolinodhliff";
-const char kWhitelistedAppUpdateManifestPath[] =
-    "/extensions/signin_screen_manual_test_app/crx/update_manifest.xml";
+const char kWhitelistedAppCrxPath[] =
+    "extensions/signin_screen_manual_test_app/app_signed_by_webstore.crx";
 // * A trivial test app which is NOT whitelisted for running in the sign-in
 //   profile:
 const char kNotWhitelistedAppId[] = "mockapnacjbcdncmpkjngjalkhphojek";
@@ -65,8 +68,9 @@ const char kNotWhitelistedUpdateManifestPath[] =
 // * A trivial test extension which is whitelisted for running in the sign-in
 //   profile:
 const char kWhitelistedExtensionId[] = "ngjobkbdodapjbbncmagbccommkggmnj";
-const char kWhitelistedExtensionUpdateManifestPath[] =
-    "/extensions/signin_screen_manual_test_extension/update_manifest.xml";
+const char kWhitelistedExtensionCrxPath[] =
+    "extensions/signin_screen_manual_test_extension/"
+    "extension_signed_by_webstore.crx";
 // * A trivial test extension which is NOT whitelisted for running in the
 //   sign-in profile:
 const char kNotWhitelistedExtensionId[] = "mockepjebcnmhmhcahfddgfcdgkdifnc";
@@ -213,9 +217,24 @@ class SigninProfileExtensionsPolicyTest
   SigninProfileExtensionsPolicyTest()
       : SigninProfileExtensionsPolicyTestBase(version_info::Channel::STABLE) {}
 
+  void SetUpOnMainThread() override {
+    SigninProfileExtensionsPolicyTestBase::SetUpOnMainThread();
+
+    extension_force_install_mixin_.InitWithDevicePolicyCrosTestHelper(
+        GetInitialProfile(), policy_helper());
+  }
+
+  ExtensionForceInstallMixin extension_force_install_mixin_{&mixin_host_};
+
  private:
   DISALLOW_COPY_AND_ASSIGN(SigninProfileExtensionsPolicyTest);
 };
+
+base::FilePath GetTestDataDir() {
+  base::FilePath test_data_dir;
+  EXPECT_TRUE(base::PathService::Get(chrome::DIR_TEST_DATA, &test_data_dir));
+  return test_data_dir;
+}
 
 }  // namespace
 
@@ -227,8 +246,8 @@ IN_PROC_BROWSER_TEST_F(SigninProfileExtensionsPolicyTest,
   extensions::TestExtensionRegistryObserver registry_observer(
       extensions::ExtensionRegistry::Get(profile), kWhitelistedAppId);
 
-  AddExtensionForForceInstallation(kWhitelistedAppId,
-                                   kWhitelistedAppUpdateManifestPath);
+  EXPECT_TRUE(extension_force_install_mixin_.ForceInstallFromCrx(
+      GetTestDataDir().AppendASCII(kWhitelistedAppCrxPath)));
 
   registry_observer.WaitForExtensionLoaded();
   const extensions::Extension* extension =
@@ -263,8 +282,8 @@ IN_PROC_BROWSER_TEST_F(SigninProfileExtensionsPolicyTest,
   extensions::TestExtensionRegistryObserver registry_observer(
       extensions::ExtensionRegistry::Get(profile), kWhitelistedExtensionId);
 
-  AddExtensionForForceInstallation(kWhitelistedExtensionId,
-                                   kWhitelistedExtensionUpdateManifestPath);
+  EXPECT_TRUE(extension_force_install_mixin_.ForceInstallFromCrx(
+      GetTestDataDir().AppendASCII(kWhitelistedExtensionCrxPath)));
 
   registry_observer.WaitForExtensionLoaded();
   const extensions::Extension* extension =
@@ -303,8 +322,8 @@ IN_PROC_BROWSER_TEST_F(SigninProfileExtensionsPolicyTest, BackgroundPage) {
   EXPECT_FALSE(
       chromeos::ProfileHelper::SigninProfileHasLoginScreenExtensions());
   ExtensionBackgroundPageReadyObserver page_observer(kWhitelistedAppId);
-  AddExtensionForForceInstallation(kWhitelistedAppId,
-                                   kWhitelistedAppUpdateManifestPath);
+  EXPECT_TRUE(extension_force_install_mixin_.ForceInstallFromCrx(
+      GetTestDataDir().AppendASCII(kWhitelistedAppCrxPath)));
   page_observer.Wait();
   EXPECT_TRUE(chromeos::ProfileHelper::SigninProfileHasLoginScreenExtensions());
 }
@@ -319,10 +338,10 @@ IN_PROC_BROWSER_TEST_F(SigninProfileExtensionsPolicyTest,
   extensions::TestExtensionRegistryObserver registry_observer2(
       extensions::ExtensionRegistry::Get(profile), kWhitelistedExtensionId);
 
-  AddExtensionForForceInstallation(kWhitelistedAppId,
-                                   kWhitelistedAppUpdateManifestPath);
-  AddExtensionForForceInstallation(kWhitelistedExtensionId,
-                                   kWhitelistedExtensionUpdateManifestPath);
+  EXPECT_TRUE(extension_force_install_mixin_.ForceInstallFromCrx(
+      GetTestDataDir().AppendASCII(kWhitelistedAppCrxPath)));
+  EXPECT_TRUE(extension_force_install_mixin_.ForceInstallFromCrx(
+      GetTestDataDir().AppendASCII(kWhitelistedExtensionCrxPath)));
 
   registry_observer1.WaitForExtensionLoaded();
   registry_observer2.WaitForExtensionLoaded();
@@ -338,10 +357,10 @@ IN_PROC_BROWSER_TEST_F(SigninProfileExtensionsPolicyTest,
   ExtensionBackgroundPageReadyObserver page_observer_for_extension(
       kWhitelistedExtensionId);
 
-  AddExtensionForForceInstallation(kWhitelistedAppId,
-                                   kWhitelistedAppUpdateManifestPath);
-  AddExtensionForForceInstallation(kWhitelistedExtensionId,
-                                   kWhitelistedExtensionUpdateManifestPath);
+  EXPECT_TRUE(extension_force_install_mixin_.ForceInstallFromCrx(
+      GetTestDataDir().AppendASCII(kWhitelistedAppCrxPath)));
+  EXPECT_TRUE(extension_force_install_mixin_.ForceInstallFromCrx(
+      GetTestDataDir().AppendASCII(kWhitelistedExtensionCrxPath)));
 
   page_observer_for_app.Wait();
   page_observer_for_extension.Wait();
@@ -377,8 +396,8 @@ class SigninProfileExtensionsPolicyOfflineLaunchTest
             extensions::ExtensionRegistry::Get(GetInitialProfile()),
             kWhitelistedAppId);
 
-    AddExtensionForForceInstallation(kWhitelistedAppId,
-                                     kWhitelistedAppUpdateManifestPath);
+    EXPECT_TRUE(extension_force_install_mixin_.ForceInstallFromCrx(
+        GetTestDataDir().AppendASCII(kWhitelistedAppCrxPath)));
 
     // In the non-PRE test, this simulates inability to make network requests
     // for fetching the extension update manifest and CRX files. In the PRE test
