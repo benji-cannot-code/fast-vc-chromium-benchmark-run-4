d97deb19f8bc9d6de544d783eced4c1f8fe8b6be
Kevin Babbitt <kbabbitt@microsoft.com>
2020-07-07 23:14:01 +0000

    Mark StyleColor(Color) constructor as explicit
    Tricium flagged this constructor in crrev.com/c/2261190 since single-
    argument constructors can result in unintentional implicit conversions.
    Mark the constructor as explicit and update the remaining call sites to
    match.
    
    Bug: 1098859
    Change-Id: Ied51476ad87473a4ceb2349cc7b1a71b0eabf60e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274860
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786047}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.cc
@@ -1387,7 +1387,7 @@ ShadowData StyleBuilderConverter::ConvertShadow(
       color = StyleColor(Color::kBlack);
       if (auto* color_value =
               DynamicTo<cssvalue::CSSColorValue>(shadow.color.Get())) {
-        color = color_value->Value();
+        color = StyleColor(color_value->Value());
       } else {
         CSSValueID value_id =
             To<CSSIdentifierValue>(*shadow.color).GetValueID();
@@ -1402,8 +1402,8 @@ ShadowData StyleBuilderConverter::ConvertShadow(
           case CSSValueID::kCurrentcolor:
             break;
           default:
-            color = StyleColor::ColorFromKeyword(
-                value_id, ComputedStyle::InitialStyle().UsedColorScheme());
+            color = StyleColor(StyleColor::ColorFromKeyword(
+                value_id, ComputedStyle::InitialStyle().UsedColorScheme()));
         }
       }
     }
@@ -1495,8 +1495,8 @@ StyleColor StyleBuilderConverter::ConvertStyleColor(StyleResolverState& state,
   if (identifier_value &&
       identifier_value->GetValueID() == CSSValueID::kCurrentcolor)
     return StyleColor::CurrentColor();
-  return state.GetDocument().GetTextLinkColors().ColorFromCSSValue(
-      value, Color(), state.Style()->UsedColorScheme(), for_visited_link);
+  return StyleColor(state.GetDocument().GetTextLinkColors().ColorFromCSSValue(
+      value, Color(), state.Style()->UsedColorScheme(), for_visited_link));
 }
 
 CSSValueID StyleBuilderConverter::ConvertCSSValueID(StyleResolverState& state,
