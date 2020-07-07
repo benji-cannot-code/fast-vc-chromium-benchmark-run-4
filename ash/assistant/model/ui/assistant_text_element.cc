2090eef605945a703b16a97ae62ff48cf12854da
Jeffrey Young <cowmoo@google.com>
2020-07-07 19:38:05 +0000

    Assistant: ensure single generic error message
    Assistant processing v2 introduced a situation that allowed
    multiple error messages to flush to the screen due to
    AssistantInteractionControllerImpl::OnTtsStarted being
    called multiple times.
    
    Add AssistantResponse::ContainsUiElement to check if duplicate
    error elements already exist before adding more.
    
    Bug: b/158499400
    Change-Id: Ia90199ef8b733132fd5433ca47bb7905926b2f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258196
    Commit-Queue: Jeffrey Young <cowmoo@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#785935}

--- ash/assistant/model/ui/assistant_text_element.cc
+++ ash/assistant/model/ui/assistant_text_element.cc
@@ -14,4 +14,9 @@ AssistantTextElement::AssistantTextElement(const std::string& text)
 
 AssistantTextElement::~AssistantTextElement() = default;
 
+bool AssistantTextElement::Compare(const AssistantUiElement& other) const {
+  return other.type() == AssistantUiElementType::kText &&
+         static_cast<const AssistantTextElement&>(other).text() == text_;
+}
+
 }  // namespace ash
---
+++ ash/assistant/ui/main_stage/assistant_error_element_view.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/assistant/ui/main_stage/assistant_error_element_view.h"
+
+#include "ash/assistant/model/ui/assistant_error_element.h"
+#include "ui/base/l10n/l10n_util.h"
+
+namespace ash {
+
+AssistantErrorElementView::AssistantErrorElementView(
+    const AssistantErrorElement* error_element)
+    : AssistantTextElementView(
+          l10n_util::GetStringUTF8(error_element->message_id())) {}
+
+const char* AssistantErrorElementView::GetClassName() const {
+  return "AssistantErrorElementView";
+}
+
+}  // namespace ash
