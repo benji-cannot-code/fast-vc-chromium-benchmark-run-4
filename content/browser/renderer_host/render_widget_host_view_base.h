969fe46a6887e5750f369e4b17532b298f6124d2
Lucas Gadani <lfg@chromium.org>
2020-07-07 18:13:27 +0000

    Fix crash when processing input.
    This CL fixes a  crash when a RenderWidgetHostView is already destroyed
    by the time the query IPC is processed.
    
    Bug: 1101001
    Change-Id: I046abacf6334fcfd56e3ea2bca07fdb24b5fff4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283824
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785873}

--- content/browser/renderer_host/render_widget_host_view_base.h
+++ content/browser/renderer_host/render_widget_host_view_base.h
@@ -626,6 +626,8 @@ class CONTENT_EXPORT RenderWidgetHostViewBase
       EarlyTouchpadFlingCancelationOnInertialGSUAckNotConsumed);
   FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostDelegatedInkMetadataTest,
                            FlagGetsSetFromRenderFrameMetadata);
+  FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostInputEventRouterTest,
+                           QueryResultAfterChildViewDead);
 
   void SynchronizeVisualProperties();
 
