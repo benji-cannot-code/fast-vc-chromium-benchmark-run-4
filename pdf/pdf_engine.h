31dfd04a5ee638f85745d3cafdd96059690a2b6a
Virender Singh <virens@microsoft.com>
2020-07-07 10:28:52 +0000

    Add explicit constructor and destructor to PDF accessibility structures
    This changes adds explicit constructors to the following structures
    - AccessibilityLinkInfo
    - AccessibilityImageInfo
    - AccessibilityHighlightInfo
    
    It is done primarily to be consistent with AccessibilityTextFieldInfo
    and to avoid code bloat because of implicit inline constructors.
    
    Bug: 1015350
    Change-Id: I548d01bec7b25f002985a4f07435cf624852279a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2275509
    Commit-Queue: Virender Singh <virens@microsoft.com>
    Reviewed-by: Ankit Kumar 🌪️ <ankk@microsoft.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785651}

--- pdf/pdf_engine.h
+++ pdf/pdf_engine.h
@@ -274,6 +274,10 @@ class PDFEngine {
   };
 
   struct AccessibilityLinkInfo {
+    AccessibilityLinkInfo();
+    AccessibilityLinkInfo(const AccessibilityLinkInfo& that);
+    ~AccessibilityLinkInfo();
+
     std::string url;
     int start_char_index;
     int char_count;
@@ -281,11 +285,19 @@ class PDFEngine {
   };
 
   struct AccessibilityImageInfo {
+    AccessibilityImageInfo();
+    AccessibilityImageInfo(const AccessibilityImageInfo& that);
+    ~AccessibilityImageInfo();
+
     std::string alt_text;
     pp::FloatRect bounds;
   };
 
   struct AccessibilityHighlightInfo {
+    AccessibilityHighlightInfo();
+    AccessibilityHighlightInfo(const AccessibilityHighlightInfo& that);
+    ~AccessibilityHighlightInfo();
+
     int start_char_index = -1;
     int char_count;
     pp::FloatRect bounds;
