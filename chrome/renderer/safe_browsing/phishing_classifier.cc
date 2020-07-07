28c5eed90511a7428515001be186701b44cd4b94
Daniel Rubery <drubery@chromium.org>
2020-07-07 17:49:56 +0000

    Add time-related metrics for the client side visual phishing detection
    This CL adds metrics to monitor the performance of the CSD visual model.
    We measure the amount of time it takes to extract visual features, and
    how long is spent on the comparison of those features with the model.
    
    Bug: 1068613
    Change-Id: I294b6cb2b217470c422f82ce388ec448f11e6375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276335
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Bettina Dea <bdea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785851}

--- chrome/renderer/safe_browsing/phishing_classifier.cc
+++ chrome/renderer/safe_browsing/phishing_classifier.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/location.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_util.h"
@@ -164,6 +165,8 @@ void PhishingClassifier::TermExtractionFinished(bool success) {
 }
 
 void PhishingClassifier::ExtractVisualFeatures() {
+  base::TimeTicks start_time = base::TimeTicks::Now();
+
   blink::WebLocalFrame* frame = render_frame_->GetWebFrame();
   gfx::Rect bounds = gfx::Rect(0, 0, frame->DocumentSize().width,
                                frame->DocumentSize().height);
@@ -184,6 +187,9 @@ void PhishingClassifier::ExtractVisualFeatures() {
       /*is_main_frame=*/true);
   cc_canvas.SetPaintPreviewTracker(tracker.get());
   VisualExtractionFinished(frame->CapturePaintPreview(bounds, &cc_canvas));
+
+  base::UmaHistogramTimes("SBClientPhishing.VisualFeatureTime",
+                          base::TimeTicks::Now() - start_time);
 }
 
 void PhishingClassifier::VisualExtractionFinished(bool success) {
@@ -215,9 +221,12 @@ void PhishingClassifier::VisualExtractionFinished(bool success) {
   verdict.set_client_score(score);
   verdict.set_is_phishing(score >= scorer_->threshold_probability());
 
+  base::TimeTicks visual_matching_start = base::TimeTicks::Now();
   if (scorer_->GetMatchingVisualTargets(*bitmap_, &verdict)) {
     verdict.set_is_phishing(true);
   }
+  base::UmaHistogramTimes("SBClientPhishing.VisualComparisonTime",
+                          base::TimeTicks::Now() - visual_matching_start);
 
   RunCallback(verdict);
 }
