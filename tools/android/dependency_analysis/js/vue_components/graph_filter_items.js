cee93c33946f01e5b1e74552397d3a03fa1fe8c7
James Long <yjlong@google.com>
2020-07-07 20:50:39 +0000

    Migrate Android dependency visualization to Vue components
    Vue instances initialized in `index.js` have been moved to a component
    folder in support of the upcoming changes which will involve having
    multiple endpoints (for class and package graphs). The move to a
    components-based structure will hopefully reduce duplicate code between
    the two very similar (for now) pages.
    
    Bug: 1093962
    Change-Id: I22ae0e5b5604386e341db9293a39aa5121474a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2281169
    Commit-Queue: James Long <yjlong@google.com>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785972}

---
+++ tools/android/dependency_analysis/js/vue_components/graph_filter_items.js
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CUSTOM_EVENTS} from '../vue_custom_events.js';
+
+const GraphFilterItems = {
+  props: ['nodeFilterData'],
+  data: function() {
+    return this.nodeFilterData;
+  },
+  methods: {
+    removeFilter: function(e) {
+      const filterText = e.target.textContent;
+      this.$emit(CUSTOM_EVENTS.FILTER_ELEMENT_CLICKED, filterText.trim());
+    },
+  },
+  template: `
+    <ul class="filter-items">
+      <li @click="removeFilter" v-for="node in nodeList">
+        {{node}}
+      </li>
+    </ul>`,
+};
+
+export {
+  GraphFilterItems,
+};
