516ccefbd1578818b5909f52124fc58ce7c3fec8
Mustafa Emre Acer <meacer@chromium.org>
2020-07-07 07:24:00 +0000

    Presubmit: Change translation screenshot warnings to errors
    Presubmit currently warns when a UI string is missing translation
    screenshots. We'll now require all strings to have associated
    screenshots. This CL changes the warnings to errors.
    
    Bug: 1087641
    Change-Id: I0968dee994e304bd9c7c59bab56d5f1b0325d5d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283261
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785613}

--- PRESUBMIT_test.py
+++ PRESUBMIT_test.py
@@ -2943,6 +2943,7 @@ class StringTest(unittest.TestCase):
                                                       MockOutputApi())
     self.assertEqual(1, len(warnings))
     self.assertEqual(self.GENERATE_SIGNATURES_MESSAGE, warnings[0].message)
+    self.assertEqual('error', warnings[0].type)
     self.assertEqual([
       os.path.join('part_grdp', 'IDS_PART_TEST2.png.sha1'),
       os.path.join('test_grd', 'IDS_TEST2.png.sha1')],
@@ -2957,6 +2958,7 @@ class StringTest(unittest.TestCase):
     warnings = PRESUBMIT._CheckStrings(input_api,
                                                       MockOutputApi())
     self.assertEqual(1, len(warnings))
+    self.assertEqual('error', warnings[0].type)
     self.assertEqual(self.GENERATE_SIGNATURES_MESSAGE, warnings[0].message)
     self.assertEqual([
         os.path.join('part_grdp', 'IDS_PART_TEST1.png.sha1'),
@@ -2984,9 +2986,11 @@ class StringTest(unittest.TestCase):
     warnings = PRESUBMIT._CheckStrings(input_api,
                                                       MockOutputApi())
     self.assertEqual(2, len(warnings))
+    self.assertEqual('error', warnings[0].type)
     self.assertEqual(self.DO_NOT_UPLOAD_PNG_MESSAGE, warnings[0].message)
     self.assertEqual([os.path.join('test_grd', 'IDS_TEST1.png')],
                      warnings[0].items)
+    self.assertEqual('error', warnings[1].type)
     self.assertEqual(self.GENERATE_SIGNATURES_MESSAGE, warnings[1].message)
     self.assertEqual([os.path.join('test_grd', 'IDS_TEST1.png.sha1')],
                      warnings[1].items)
@@ -3019,10 +3023,12 @@ class StringTest(unittest.TestCase):
     warnings = PRESUBMIT._CheckStrings(input_api,
                                                       MockOutputApi())
     self.assertEqual(2, len(warnings))
+    self.assertEqual('error', warnings[0].type)
     self.assertEqual(self.DO_NOT_UPLOAD_PNG_MESSAGE, warnings[0].message)
     self.assertEqual([os.path.join('part_grdp', 'IDS_PART_TEST1.png'),
                       os.path.join('test_grd', 'IDS_TEST1.png')],
                      warnings[0].items)
+    self.assertEqual('error', warnings[0].type)
     self.assertEqual(self.GENERATE_SIGNATURES_MESSAGE, warnings[1].message)
     self.assertEqual([os.path.join('part_grdp', 'IDS_PART_TEST1.png.sha1'),
                       os.path.join('test_grd', 'IDS_TEST1.png.sha1')],
@@ -3092,6 +3098,7 @@ class StringTest(unittest.TestCase):
     warnings = PRESUBMIT._CheckStrings(input_api,
                                                       MockOutputApi())
     self.assertEqual(1, len(warnings))
+    self.assertEqual('error', warnings[0].type)
     self.assertEqual(self.REMOVE_SIGNATURES_MESSAGE, warnings[0].message)
     self.assertEqual([
         os.path.join('part_grdp', 'IDS_PART_TEST1.png.sha1'),
@@ -3132,6 +3139,7 @@ class StringTest(unittest.TestCase):
     warnings = PRESUBMIT._CheckStrings(input_api,
                                                       MockOutputApi())
     self.assertEqual(1, len(warnings))
+    self.assertEqual('error', warnings[0].type)
     self.assertEqual(self.REMOVE_SIGNATURES_MESSAGE, warnings[0].message)
     self.assertEqual([os.path.join('part_grdp', 'IDS_PART_TEST1.png.sha1'),
                       os.path.join('test_grd', 'IDS_TEST1.png.sha1')
