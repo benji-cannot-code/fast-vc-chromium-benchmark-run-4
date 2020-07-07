2841ed0c1e76d4d64303d51dd579039267819f66
Tommy Steimel <steimel@chromium.org>
2020-07-07 19:31:38 +0000

    Kaleidoscope: Add public usage of cr_elements/
    This CL adds two public Polymer wrappers to keep Kaleidoscope's
    cr_elements/ usage in the public repo.
    
    This CL adds a new CSS var used to set a background color on a
    CrDrawerElement.
    
    Bug: b:157737119
    Change-Id: Ibc15f4cf2f497142c68b73a0b20f85ed0a1cc719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2254452
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: dpapad <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785931}

---
+++ chrome/browser/resources/kaleidoscope/side_nav_container.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '//resources/cr_elements/cr_drawer/cr_drawer.m.js';
+
+import {html, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+class KaleidoscopeSideNavContainerElement extends PolymerElement {
+  static get is() {
+    return 'kaleidoscope-side-nav-container';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+
+  // Toggles the CrDrawerElement.
+  toggle() {
+    /** @type {!CrDrawerElement} */ (this.$.drawer).toggle();
+  }
+}
+
+customElements.define(
+    KaleidoscopeSideNavContainerElement.is,
+    KaleidoscopeSideNavContainerElement);
---
+++ chrome/browser/resources/kaleidoscope/toolbar.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '//resources/cr_elements/cr_toolbar/cr_toolbar.m.js';
+
+import {html, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+class KaleidoscopeToolbarElement extends PolymerElement {
+  static get is() {
+    return 'kaleidoscope-toolbar';
+  }
+
+  static get template() {
+    return html`{__html_template__}`;
+  }
+}
+
+customElements.define(
+    KaleidoscopeToolbarElement.is, KaleidoscopeToolbarElement);
