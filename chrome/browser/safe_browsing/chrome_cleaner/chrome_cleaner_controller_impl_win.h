c25e6254add77943cfb49e4a67f90147e369cbba
Sorin Jianu <sorin@chromium.org>
2020-07-07 21:14:54 +0000

    Revert "Reland "Safety check: add tests for the Chrome cleaner child backend""
    This reverts commit f55b2a091b045e4e90c5ac38d252b3fdfc8286f7.
    
    Reason for revert: This is still breaking the official Chrome builders, for instance: https://ci.chromium.org/p/chrome/builders/ci/win64-trunk
    
    Sorry for the revert of the reland.
    
    Original change's description:
    > Reland "Safety check: add tests for the Chrome cleaner child backend"
    > 
    > This is a reland of 9d1db5b829efe3d7b499e32cd6de7daee889ec0a
    > Fixes the missing import and constant.
    > 
    > Original change's description:
    > > Safety check: add tests for the Chrome cleaner child backend
    > >
    > > * Test that each possible CCT status results in the corresponding
    > >   safety check child status and display string.
    > > * Test that CCT being enterprise managed is handled correctly.
    > >
    > > Bug: 1087263
    > > Change-Id: I84d93408800cb4fbe8d138578c586a0ae89dbfc9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249980
    > > Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    > > Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    > > Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#783942}
    > 
    > Bug: 1087263
    > Change-Id: Iddb785080835e437517c7adba7eacaec20356ca6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279752
    > Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@chromium.org>
    > Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#785664}
    
    TBR=joenotcharles@chromium.org,aee@chromium.org,rainhard@chromium.org
    
    Change-Id: I03883f5f6bdf59d46b725b76e649d5a4c2a7158a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1087263
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2285386
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785990}

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_impl_win.h
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_controller_impl_win.h
@@ -85,7 +85,6 @@ class ChromeCleanerControllerImpl : public ChromeCleanerController {
   // Force the current controller's state for tests that check the effect of
   // starting and completing reporter runs.
   void SetStateForTesting(State state);
-  void SetIdleForTesting(IdleReason idle_reason);
 
  private:
   ChromeCleanerControllerImpl();
