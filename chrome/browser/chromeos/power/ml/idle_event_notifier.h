c3a1cc0b09eba4c5f7d34dcba9ccdfba3be12ea3
Peter Kasting <pkasting@chromium.org>
2020-07-07 09:54:21 +0000

    Make a few InXXX() functions constexpr.
    With the recent saturation fixes, these are now simple enough to
    inline without increasing binary size significantly.  Doing this
    allows using them in constexpr contexts, of which there are a few (at
    least for InMinutes()).
    
    On my 64-bit Windows "official" non-branded release build, chrome.dll
    had the following sizes:
    
    Before saturation fixes: 147661312 bytes
    After saturation fixes:  147660800 bytes
    After this constexpr CL: 147663872 bytes
    
    Bug: none
    Change-Id: I786ed3f36e0952bd766d3826f0eca34fe676af30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2281730
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Satoru Takabayashi <satorux@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785640}

--- chrome/browser/chromeos/power/ml/idle_event_notifier.h
+++ chrome/browser/chromeos/power/ml/idle_event_notifier.h
@@ -53,7 +53,7 @@ class IdleEventNotifier : public PowerManagerClient::Observer,
 
   // Granularity of input events is per minute.
   static constexpr int kNumUserInputEventsBuckets =
-      kUserInputEventsDuration / base::TimeDelta::FromMinutes(1);
+      kUserInputEventsDuration.InMinutes();
 
   struct ActivityData {
     ActivityData();
