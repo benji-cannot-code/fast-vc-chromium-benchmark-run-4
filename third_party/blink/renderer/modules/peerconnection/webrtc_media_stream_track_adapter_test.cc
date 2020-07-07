7e65a3c26528bc676cf1aa9f61ae75c277897ca2
Antonio Gomes <tonikitoo@igalia.com>
2020-07-07 14:39:28 +0000

    Change WebRtcMediaStreamTrackAdapterTest to operate over MediaStreamSource
    ... instead of WebMediaStreamSource.
    
    BUG=704136
    R=guidou@chromium.org
    
    Change-Id: I335a78a565b03c6db198cf921e02ef26a3ae0392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283763
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#785725}

--- third_party/blink/renderer/modules/peerconnection/webrtc_media_stream_track_adapter_test.cc
+++ third_party/blink/renderer/modules/peerconnection/webrtc_media_stream_track_adapter_test.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/synchronization/waitable_event.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/platform/modules/mediastream/web_media_stream_source.h"
 #include "third_party/blink/public/platform/scheduler/test/renderer_scheduler_test_support.h"
 #include "third_party/blink/public/platform/web_string.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_video_track.h"
@@ -23,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/peerconnection/mock_peer_connection_dependency_factory.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_audio_source.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_component.h"
+#include "third_party/blink/renderer/platform/mediastream/media_stream_source.h"
 #include "third_party/blink/renderer/platform/testing/io_task_runner_testing_platform_support.h"
 
 namespace blink {
@@ -60,20 +60,18 @@ class WebRtcMediaStreamTrackAdapterTest : public ::testing::Test {
     return component;
   }
 
-  blink::WebMediaStreamTrack CreateLocalVideoTrack() {
-    blink::WebMediaStreamSource web_source;
-    web_source.Initialize(blink::WebString::FromUTF8("local_video_id"),
-                          blink::WebMediaStreamSource::kTypeVideo,
-                          blink::WebString::FromUTF8("local_video_track"),
-                          false);
-    blink::MockMediaStreamVideoSource* video_source =
-        new blink::MockMediaStreamVideoSource();
-    // Takes ownership of |video_source|.
-    web_source.SetPlatformSource(base::WrapUnique(video_source));
+  MediaStreamComponent* CreateLocalVideoTrack() {
+    auto* source = MakeGarbageCollected<MediaStreamSource>(
+        String::FromUTF8("local_video_id"), MediaStreamSource::kTypeVideo,
+        String::FromUTF8("local_video_track"), false);
+    auto video_source = std::make_unique<MockMediaStreamVideoSource>();
+    auto* video_source_ptr = video_source.get();
+    video_source->SetOwner(source);
+    source->SetPlatformSource(std::move(video_source));
 
-    return blink::MediaStreamVideoTrack::CreateVideoTrack(
-        video_source, blink::MediaStreamVideoSource::ConstraintsOnceCallback(),
-        true);
+    return MediaStreamVideoTrack::CreateVideoTrack(
+        video_source_ptr,
+        blink::MediaStreamVideoSource::ConstraintsOnceCallback(), true);
   }
 
   void CreateRemoteTrackAdapter(
