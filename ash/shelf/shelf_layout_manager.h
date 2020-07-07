c8eadfae99fe7b2c35b37394e745cbc1078ed4a7
Toni Barzic <tbarzic@chromium.org>
2020-07-07 17:43:47 +0000

    Send out ShelfConfigUpdated when tablet mode changes
    Notify shelf config observers that the shelf config has changed when
    tablet mode changes - some shelf dimensions depend on tablet mode
    state, so shelf may need relayout when tablet mode changes. This is
    generally done when hiding shelf buttons in tablet mode is enabled, but
    depending on user settings, that may not be the case.
    
    Also, stop observing tablet mode state in ShelfLayoutManager, as it's
    already observing both ShelfConfig, so now that shelf config is updated
    when tablet mode changes, it should be enough to observe shelf config
    only.
    
    BUG=None
    
    Change-Id: I3c9f84ec9fa4d03bcd2838ea43cd7ae5869f26a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283580
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Toni Baržić <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785847}

--- ash/shelf/shelf_layout_manager.h
+++ ash/shelf/shelf_layout_manager.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/session/session_observer.h"
 #include "ash/public/cpp/shelf_config.h"
 #include "ash/public/cpp/shelf_types.h"
-#include "ash/public/cpp/tablet_mode_observer.h"
 #include "ash/public/cpp/wallpaper_controller.h"
 #include "ash/public/cpp/wallpaper_controller_observer.h"
 #include "ash/shelf/shelf.h"
@@ -84,8 +83,7 @@ class ASH_EXPORT ShelfLayoutManager
       public LocaleChangeObserver,
       public DesksController::Observer,
       public message_center::MessageCenterObserver,
-      public ShelfConfig::Observer,
-      public TabletModeObserver {
+      public ShelfConfig::Observer {
  public:
   // Suspend work area updates within its scope. Note that relevant
   // ShelfLayoutManager must outlive this class.
@@ -272,10 +270,6 @@ class ASH_EXPORT ShelfLayoutManager
 
   // ShelfConfig::Observer:
   void OnShelfConfigUpdated() override;
-
-  // TabletModeObserver:
-  void OnTabletModeStarted() override;
-  void OnTabletModeEnded() override;
 
   float GetOpacity() const;
 
