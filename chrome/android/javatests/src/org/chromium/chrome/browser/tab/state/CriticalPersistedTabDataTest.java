cf16bb5492ff074dd42968b6a7954ed8a0ae313d
David Maunder <davidjm@chromium.org>
2020-07-07 18:39:11 +0000

    Disabling testEncryptedSaveRestoreDelete pending an investigation
    The test is flaky
    
    Bug: 1101760
    Change-Id: I183c61503bb63c4ec90dd86f089723e4e5c42dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2284967
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785893}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/state/CriticalPersistedTabDataTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/state/CriticalPersistedTabDataTest.java
@@ -17,6 +17,7 @@ import org.mockito.MockitoAnnotations;
 import org.chromium.base.Callback;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.tab.MockTab;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.ChromeBrowserTestRule;
@@ -61,6 +62,7 @@ public class CriticalPersistedTabDataTest {
 
     @SmallTest
     @Test
+    @DisabledTest(message = "https://crbug.com/1101760")
     public void testEncryptedSaveRestoreDelete() throws InterruptedException {
         testSaveRestoreDelete(true);
     }
