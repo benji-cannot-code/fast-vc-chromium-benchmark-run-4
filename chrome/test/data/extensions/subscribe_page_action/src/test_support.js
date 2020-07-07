7b4751e770363b0efba9c7d9e3ec984d2af2d04f
Finnur Thorarinsson <finnur@chromium.org>
2020-07-07 18:06:14 +0000

    RSS Extension: Build script, closure, hardening.
    - Sandbox the iframe and sanitize the input used.
    - Remove special 'synchronous' testing flag (not needed).
      - Multi-line quote requirement also no longer needed.
    - Change default reader list
      - (replace deprecated Bloglines service with Newsblur).
    - Deflake and re-enable the RSS extension tests.
    - Up the version by two (from 2.2.3 to 2.2.5, since 2.2.4
      is live already).
    
    Bug: 340354, b/137075999
    Change-Id: I6eac37fae9e2228aeabe68ccf1fd34b9b36ac6ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2264059
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785861}

---
+++ chrome/test/data/extensions/subscribe_page_action/src/test_support.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Provides a default implementation for the sanitizer (a pass-through), so that
+// tests can be run on the extension directly (without having to Closure compile
+// it first).
+
+var goog = {
+  dom: {
+    safe: {
+      setAnchorHref: function(element, href) {
+        element.href = href;
+      },
+      setInnerHtml: function(element, html) {
+        element.innerHTML = html;
+      }
+    }
+  },
+  html: {
+    sanitizer: {
+      HtmlSanitizer: {
+        sanitize: function(data) {
+          return data;
+        }
+      }
+    },
+    SafeUrl: {
+      sanitize: function(data) {
+        return data;
+      }
+    }
+  },
+
+  provide: function(namespace) {},
+  require: function(namespace) {}
+};
