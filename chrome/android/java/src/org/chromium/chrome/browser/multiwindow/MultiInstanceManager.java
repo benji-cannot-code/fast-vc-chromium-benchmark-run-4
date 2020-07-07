0cdf8ec9628364c0d0c0452343554d432cad43fc
Lijin Shen <lazzzis@google.com>
2020-07-07 23:34:58 +0000

    Merge tabs of multi-instance when display is changed.
    On partner foldable phones, when the secondary display is off, there
    is only one targetable displays and only #onDisplayChanged is called.
    This CL tries to merge tabs #onDisplayChanged as well.
    
    Bug: 1088202
    Change-Id: Id21960b40bceb4b04fce7a2718c0e33bee0f4920
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2272131
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/master@{#786055}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiInstanceManager.java
@@ -21,6 +21,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.ActivityState;
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.CommandLine;
 import org.chromium.base.metrics.RecordUserAction;
@@ -86,6 +87,7 @@ public class MultiInstanceManager
     private boolean mShouldMergeOnConfigurationChange;
     private boolean mIsRecreating;
     private int mDisplayId;
+    private static List<Integer> sTestDisplayIds;
 
     /**
      * Create a new {@link MultiInstanceManager}.
@@ -221,7 +223,13 @@ public class MultiInstanceManager
 
             @Override
             public void onDisplayChanged(int displayId) {
-                // TODO(crbug.com/824954): try to merge tabs sharing logic w/ onDisplayRemoved
+                if (displayId == mDisplayId) return;
+                List<Integer> ids = sTestDisplayIds != null
+                        ? sTestDisplayIds
+                        : ApiCompatibilityUtils.getTargetableDisplayIds(mActivity);
+                if (ids.size() == 1 && ids.get(0).equals(mDisplayId)) {
+                    maybeMergeTabs();
+                }
             }
         };
         displayManager.registerDisplayListener(mDisplayListener, null);
@@ -433,5 +441,10 @@ public class MultiInstanceManager
     @VisibleForTesting
     public DisplayManager.DisplayListener getDisplayListenerForTesting() {
         return mDisplayListener;
+    }
+
+    @VisibleForTesting
+    public static void setTestDisplayIds(List<Integer> testDisplayIds) {
+        sTestDisplayIds = testDisplayIds;
     }
 }
