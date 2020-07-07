4a7b33e94d3d1cc566a677f51e638275e92dc883
Chris Cunningham <chcunningham@chromium.org>
2020-07-07 22:10:49 +0000

    WebCodecs: AudioDecoder templated blink bindings
    This connects the IDL definitions to real code. We introduce
    DecoderTemplate to house the common request processing between
    audio/video decoders. A follow up cl will convert VideoDecoder to use
    this as well.
    
    For now AudioDecoder always uses FFmpeg and hard codes the codec to AAC.
    
    Change-Id: I891fbe095e57a240db2b528ced81ee1d6f14ef31
    
    Bug: 1094090
    Change-Id: I891fbe095e57a240db2b528ced81ee1d6f14ef31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2274584
    Commit-Queue: Chrome Cunningham <chcunningham@chromium.org>
    Auto-Submit: Chrome Cunningham <chcunningham@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786017}

---
+++ third_party/blink/renderer/modules/webcodecs/audio_decoder_test.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_audio_decoder_init.h"
+#include "third_party/blink/renderer/platform/heap/heap.h"
+
+#include "third_party/blink/renderer/modules/webcodecs/audio_decoder.h"
+
+namespace blink {
+
+TEST(AudioDecoderTest, Construction) {
+  V8TestingScope v8_scope;
+
+  auto* init = MakeGarbageCollected<AudioDecoderInit>();
+
+  auto* decoder = MakeGarbageCollected<AudioDecoder>(
+      v8_scope.GetScriptState(), init, v8_scope.GetExceptionState());
+
+  EXPECT_EQ(decoder->decodeQueueSize(), 0);
+}
+
+}  // namespace blink
---
+++ third_party/blink/renderer/modules/webcodecs/encoded_audio_metadata.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_WEBCODECS_ENCODED_AUDIO_METADATA_H_
+#define THIRD_PARTY_BLINK_RENDERER_MODULES_WEBCODECS_ENCODED_AUDIO_METADATA_H_
+
+#include "base/time/time.h"
+
+namespace blink {
+
+struct EncodedAudioMetadata {
+  bool key_frame = false;
+  base::TimeDelta timestamp;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_WEBCODECS_ENCODED_AUDIO_METADATA_H_
