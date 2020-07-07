b547a1b59ff7981503dace41462ce792c91261ff
Bo Liu <boliu@chromium.org>
2020-07-07 15:56:25 +0000

    gpu: Avoid holding onto lock while destroy Sequence
    There are reports that destroying Sequence can be expensive, and lock up
    other threads. So destroy it outside of the gpu::Scheduler lock.
    
    Bug: 916977
    Change-Id: I774cf8188e81f5ebed5fd37edd45278b415df7cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277110
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785764}

--- gpu/command_buffer/service/scheduler.h
+++ gpu/command_buffer/service/scheduler.h
@@ -201,6 +201,8 @@ class GPU_EXPORT Scheduler {
     SchedulingPriority current_priority() const { return current_priority_; }
 
    private:
+    friend class Scheduler;
+
     enum RunningState { IDLE, SCHEDULED, RUNNING };
 
     struct WaitFence {
