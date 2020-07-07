1eb40b35ca3a09a874a9c6d5fb3dac301a723d44
Xiaoling Bao <xiaolingbao@google.com>
2020-07-07 05:46:11 +0000

    Add device management (DM) request/response parser.
    This is part of the changes for enterprise support in 
    Chromium updater. The added code is to construct the 
    request send to DM server and parse the response into 
    an internal data structure.
    
    This feature is a rewrite of a similar feature already
    present in Omaha (Google Update).
    
    This CL also introduces the dependency on //crypto. This is to enable
    the parser to verify the SHA256 signature in the response and to
    extract the public key for future response verification. In the unit
    test, crypto lib is also used for signature creation. 
    
    Bug: 1068797
    Change-Id: Ieaa3565ff7293cfb52858ae39d18a794ef7a8823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276535
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S. Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785582}

--- chrome/updater/dm_cached_policy_info.h
+++ chrome/updater/dm_cached_policy_info.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_UPDATER_DM_CACHED_POLICY_INFO_H_
 #define CHROME_UPDATER_DM_CACHED_POLICY_INFO_H_
 
+#include <stdint.h>
 #include <string>
 
 namespace updater {
@@ -13,23 +14,21 @@ namespace updater {
 class CachedPolicyInfo {
  public:
   CachedPolicyInfo();
-  CachedPolicyInfo(const CachedPolicyInfo&) = delete;
-  CachedPolicyInfo& operator=(const CachedPolicyInfo& other) = delete;
   ~CachedPolicyInfo();
 
   // Populate members with serialized data of DM PolicyFetchResponse.
   bool Populate(const std::string& raw_response);
 
   // Public key of the policy.
-  std::string PublicKey() const { return key_; }
+  std::string public_key() const { return key_; }
 
   // Version of the public key. -1 means the key is not versioned or unknown.
-  int32_t KeyVersion() const { return key_version_; }
+  int32_t key_version() const { return key_version_; }
 
-  bool HasKeyVersion() const { return key_version_ >= 0; }
+  bool has_key_version() const { return key_version_ >= 0; }
 
   // Signing timestamp.
-  int64_t TimeStamp() const { return timestamp_; }
+  int64_t timestamp() const { return timestamp_; }
 
  private:
   std::string key_;
