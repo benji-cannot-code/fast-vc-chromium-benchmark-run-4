456d0d224b725d576923cc58ae080ac0004744f5
Clark DuVall <cduvall@chromium.org>
2020-07-07 17:05:17 +0000

    [WebLayer] Fix app name in site settings warning strings
    The strings telling users to turn on Android system settings had
    "Chrome" hardcoded in them. This was incorrect for WebLayer, as well as
    Chromium and non-stable Chrome versions.
    
    Skip-Translation-Screenshots-Check: True
    Bug: 1063433
    Change-Id: I267b0b30cca0cca5074bceb981157e56ce78ccf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2284354
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785810}

--- chrome/android/java/src/org/chromium/chrome/browser/site_settings/ChromeSiteSettingsClient.java
+++ chrome/android/java/src/org/chromium/chrome/browser/site_settings/ChromeSiteSettingsClient.java
@@ -178,4 +178,9 @@ public class ChromeSiteSettingsClient implements SiteSettingsClient {
     public String getChannelIdForOrigin(String origin) {
         return SiteChannelsManager.getInstance().getChannelIdForOrigin(origin);
     }
+
+    @Override
+    public String getAppName() {
+        return mContext.getString(R.string.app_name);
+    }
 }
