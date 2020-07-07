a1e01abacd358c622bb789905df00d07e65d1c1a
sauski <sauski@google.com>
2020-07-07 17:40:12 +0000

    Access Context Auditing: Store non-persistent cookie records
    To support recording access for non-persistent cookies, the Access
    Context Audit service and databases are extended to store non-
    persistent cookies alongside persistent cookies. These records are
    cleared on startup unless restoring the previous session.
    
    Bug: 1101675
    Change-Id: I7ee7a47530f8f0d6b05fc8890b94c605f839c2ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2273057
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/master@{#785843}

--- chrome/browser/browsing_data/access_context_audit_database.h
+++ chrome/browser/browsing_data/access_context_audit_database.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/canonical_cookie.h"
 #include "sql/database.h"
 #include "sql/init_status.h"
+#include "sql/meta_table.h"
 #include "sql/test/test_helpers.h"
 #include "url/gurl.h"
 
@@ -42,13 +43,16 @@ class AccessContextAuditDatabase
                  const std::string& name,
                  const std::string& domain,
                  const std::string& path,
-                 const base::Time& last_access_time);
+                 const base::Time& last_access_time,
+                 bool is_persistent);
     AccessRecord(const GURL& top_frame_origin,
                  const StorageAPIType& type,
                  const GURL& origin,
                  const base::Time& last_access_time);
-    AccessRecord(const AccessRecord& other);
     ~AccessRecord();
+    AccessRecord(const AccessRecord& other);
+    AccessRecord& operator=(const AccessRecord& other);
+
     GURL top_frame_origin;
     StorageAPIType type;
 
@@ -61,13 +65,17 @@ class AccessContextAuditDatabase
     GURL origin;
 
     base::Time last_access_time;
+
+    // When |type| is kCookie, indicates the record will be cleared on startup
+    // unless the database is started with restore_non_persistent_cookies.
+    bool is_persistent;
   };
 
   explicit AccessContextAuditDatabase(
       const base::FilePath& path_to_database_dir);
 
   // Initialises internal database. Must be called prior to any other usage.
-  void Init();
+  void Init(bool restore_non_persistent_cookies);
 
   // Persists the provided list of |records| in the database.
   void AddRecords(const std::vector<AccessRecord>& records);
@@ -101,6 +109,7 @@ class AccessContextAuditDatabase
   bool InitializeSchema();
 
   sql::Database db_;
+  sql::MetaTable meta_table_;
   base::FilePath db_file_path_;
 };
 
