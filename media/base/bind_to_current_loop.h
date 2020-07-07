febb546bb60c6638806eaa28e07bd3222fa5ba12
Matt Reynolds <mattreynolds@google.com>
2020-07-07 19:13:32 +0000

    Convert base::Bind in bind_to_current_loop.h file comment
    base::BindToCurrentLoop already uses Once/RepeatingCallbacks but the
    file-level comment still refers to base::Bind. This CL updates the
    comment to refer to the new types.
    
    BUG=1007799
    
    Change-Id: Ib8005e301f6d3d729570dda8102673d636fb61d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283060
    Reviewed-by: Chrome Cunningham <chcunningham@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Auto-Submit: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785923}

--- media/base/bind_to_current_loop.h
+++ media/base/bind_to_current_loop.h
@@ -14,10 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/single_thread_task_runner.h"
 #include "base/threading/sequenced_task_runner_handle.h"
 
-// This is a helper utility for base::Bind()ing callbacks to a given
-// TaskRunner. The typical use is when |a| (of class |A|) wants to hand a
-// callback such as base::Bind(&A::AMethod, a) to |b|, but needs to ensure that
-// when |b| executes the callback, it does so on |a|'s task_runner's
+// This is a helper utility for binding a OnceCallback or RepeatingCallback to a
+// given TaskRunner. The typical use is when |a| (of class |A|) wants to hand a
+// callback such as base::BindOnce(&A::AMethod, a) to |b|, but needs to ensure
+// that when |b| executes the callback, it does so on |a|'s task_runner's
 // MessageLoop.
 //
 // Typical usage: request to be called back on the current thread:
