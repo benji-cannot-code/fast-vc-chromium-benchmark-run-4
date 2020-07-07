7d019668cc38a08ac52cf2f33cb848e23f32805d
Megan Jablonski <megjablon@chromium.org>
2020-07-07 16:53:15 +0000

    Fix Translate Tab UI bugs
    Fix three Tab UI bugs:
    1. The always translate checkbox in the menu was retranslating an
    already translated page when toggled.
    
    2. When reverting a translation from the tab UI, the model's view state
    was not being updated. This meant if a user untranslated the page after
    clicking the always translate button in the menu, then clicked always
    translate again, the page wouldn't translate since the model's state is
    incorrectly "translated" rather than "before translate".
    
    3. The 'always translate' button beneath the tab UI was not being
    updated when the always translate menu item was being clicked.
    
    Additionally this CL removes some unused code that was missed in the
    experiment cleanup.
    
    Bug: 1099571
    Change-Id: I0b3545463acc257ab69cd616bce2e9e833cf2ac4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2269751
    Reviewed-by: anthonyvd <anthonyvd@chromium.org>
    Reviewed-by: Doug Arnett <dougarnett@chromium.org>
    Commit-Queue: Megan Jablonski <megjablon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785801}

--- chrome/browser/ui/views/translate/translate_bubble_view.h
+++ chrome/browser/ui/views/translate/translate_bubble_view.h
@@ -118,8 +118,7 @@ class TranslateBubbleView : public LocationBarBubbleDelegateView,
     BUTTON_ID_ALWAYS_TRANSLATE,
     BUTTON_ID_OPTIONS_MENU,
     BUTTON_ID_CLOSE,
-    BUTTON_ID_RESET,
-    BUTTON_ID_RETURN
+    BUTTON_ID_RESET
   };
 
   enum ComboboxID {
@@ -156,6 +155,8 @@ class TranslateBubbleView : public LocationBarBubbleDelegateView,
                            TabSelectedAfterTranslation);
   FRIEND_TEST_ALL_PREFIXES(TranslateBubbleViewTest,
                            AlwaysTranslateTriggerTranslation);
+  FRIEND_TEST_ALL_PREFIXES(TranslateBubbleViewTest,
+                           ShowOriginalUpdatesViewState);
 
   TranslateBubbleView(views::View* anchor_view,
                       std::unique_ptr<TranslateBubbleModel> model,
@@ -200,9 +201,6 @@ class TranslateBubbleView : public LocationBarBubbleDelegateView,
   // Creates source language label and combobox for Tab UI advanced view. Caller
   // takes ownership of the returned view.
   std::unique_ptr<views::View> CreateViewAdvancedTarget();
-
-  // Tab UI presents the same view for before/during/after translate state.
-  bool IsEquivalentState(TranslateBubbleModel::ViewState view_state);
 
   // Creates the 'advanced' view to show source/target language combobox. Caller
   // takes ownership of the returned view.
