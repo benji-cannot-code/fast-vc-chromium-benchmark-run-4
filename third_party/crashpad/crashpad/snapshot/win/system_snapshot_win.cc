f829b5eee1f1d49a3b3eefd7193e63a9f1127a48
Adam Kallai <kadam@inf.u-szeged.hu>
2020-07-07 16:33:09 +0000

    Update Crashpad to e46f30cc5497c16ccbf8489a6ef880283703603c
    e46f30cc5497 fix: include missing header file
    
    Bug: 1100363
    Change-Id: Ie98728d3495f89356260be89e85f4d041b360972
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283215
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785786}

--- third_party/crashpad/crashpad/snapshot/win/system_snapshot_win.cc
+++ third_party/crashpad/crashpad/snapshot/win/system_snapshot_win.cc
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/check_op.h"
 #include "base/logging.h"
+#include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
