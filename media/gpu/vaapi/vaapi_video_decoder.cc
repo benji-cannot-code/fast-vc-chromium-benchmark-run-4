fa63669935718aa60ba458626bb2f468a1c81839
Miguel Casas <mcasas@chromium.org>
2020-07-07 18:07:29 +0000

    media/gpu/vaapi: Add TRACE for Decode() call
    The new direct Vaapi VideoDecoder doesn't have tracing, this CL adds the
    first such meas, for the Decode() call, similar to
    https://source.chromium.org/chromium/chromium/src/+/master:media/gpu/vaapi/vaapi_video_decode_accelerator.cc;l=446
    
    Bug: 1040291
    Change-Id: I25b70cecb48435cd5539cbf927c0f2547d584649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283249
    Auto-Submit: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785864}

--- media/gpu/vaapi/vaapi_video_decoder.cc
+++ media/gpu/vaapi/vaapi_video_decoder.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/memory/ptr_util.h"
+#include "base/trace_event/trace_event.h"
 #include "media/base/bind_to_current_loop.h"
 #include "media/base/format_utils.h"
 #include "media/base/video_frame.h"
@@ -223,7 +224,9 @@ void VaapiVideoDecoder::HandleDecodeTask() {
     return;
   }
 
+  TRACE_EVENT_BEGIN0("media,gpu", "VaapiVideoDecoder::Decode");
   AcceleratedVideoDecoder::DecodeResult decode_result = decoder_->Decode();
+  TRACE_EVENT_END0("media,gpu", "VaapiVideoDecoder::Decode");
   switch (decode_result) {
     case AcceleratedVideoDecoder::kRanOutOfStreamData:
       // Decoding was successful, notify client and try to schedule the next
