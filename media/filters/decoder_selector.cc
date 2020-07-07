e4ba277aa9d8208b33eb8a465161d990bdcbe6f4
Will Cassella <cassew@google.com>
2020-07-07 18:09:44 +0000

    Add feature to disable decoder prioritization
    By default, `media::DecoderSelector` uses a decoder prioritization
    function that tries to balance power and performance with a resolution
    -based hardware decoder cutoff. This CL adds a default-disabled feature
    to instead use a function that prioritizes nothing, which is useful
    for testing.
    
    Bug: 684792
    Change-Id: Iac0dc0e12fd9f5a41f344eefcbe002b4a1e84724
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278702
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785868}

--- media/filters/decoder_selector.cc
+++ media/filters/decoder_selector.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback_helpers.h"
+#include "base/feature_list.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/cdm_context.h"
 #include "media/base/demuxer_stream.h"
 #include "media/base/media_log.h"
+#include "media/base/media_switches.h"
 #include "media/base/video_decoder.h"
 #include "media/filters/decoder_stream_traits.h"
 #include "media/filters/decrypting_demuxer_stream.h"
@@ -30,6 +32,11 @@ namespace media {
 namespace {
 
 const char kSelectDecoderTrace[] = "DecoderSelector::SelectDecoder";
+
+template <typename T>
+DecoderPriority UnspecifiedDecoderPriority(const T& /*config*/) {
+  return DecoderPriority::kUnspecified;
+}
 
 DecoderPriority GetDefaultVideoDecoderPriority(
     const VideoDecoderConfig& config) {
@@ -45,18 +52,19 @@ DecoderPriority GetDefaultVideoDecoderPriority(
              : DecoderPriority::kPreferPlatformDecoders;
 }
 
-DecoderPriority GetDefaultAudioDecoderPriority(
-    const AudioDecoderConfig& /*config*/) {
-  // Platform audio decoders are not currently prioritized or deprioritized
-  return DecoderPriority::kUnspecified;
-}
-
 void SetDefaultDecoderPriorityCB(VideoDecoderSelector::DecoderPriorityCB* out) {
-  *out = base::BindRepeating(GetDefaultVideoDecoderPriority);
+  if (base::FeatureList::IsEnabled(kResolutionBasedDecoderPriority)) {
+    *out = base::BindRepeating(GetDefaultVideoDecoderPriority);
+  } else {
+    *out = base::BindRepeating<DecoderPriority(const VideoDecoderConfig&)>(
+        UnspecifiedDecoderPriority);
+  }
 }
 
 void SetDefaultDecoderPriorityCB(AudioDecoderSelector::DecoderPriorityCB* out) {
-  *out = base::BindRepeating(GetDefaultAudioDecoderPriority);
+  // Platform audio decoders are not currently prioritized or deprioritized
+  *out = base::BindRepeating<DecoderPriority(const AudioDecoderConfig&)>(
+      UnspecifiedDecoderPriority);
 }
 
 }  // namespace
