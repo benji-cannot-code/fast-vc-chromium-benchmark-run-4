15a61a525a11a8d3fd09c228f5e11057c0359259
ckitagawa <ckitagawa@chromium.org>
2020-07-07 16:27:48 +0000

    [Paint Preview] Player visibility model
    This CL simplifies the visibility state of the player so that there are
    fewer mVisible* lists and instead uses the visibility property of View.
    
    Bug: 1099722
    Change-Id: I57745d54f0e8447e9abc83c8e7394838a2a19c36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2273460
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#785778}

--- components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/frame/PlayerFrameProperties.java
+++ components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/frame/PlayerFrameProperties.java
@@ -32,12 +32,12 @@ class PlayerFrameProperties {
     static final PropertyModel.WritableObjectPropertyKey<Rect> VIEWPORT =
             new PropertyModel.WritableObjectPropertyKey<>(true);
     /**
-     * List of currently visible sub-frame {@link View}s.
+     * List of sub-frame {@link View}s.
      */
     static final PropertyModel.WritableObjectPropertyKey<List<View>> SUBFRAME_VIEWS =
             new PropertyModel.WritableObjectPropertyKey<>();
     /**
-     * List of currently visible sub-frame clip rects.
+     * List of sub-frame clip rects.
      */
     static final PropertyModel.WritableObjectPropertyKey<List<Rect>> SUBFRAME_RECTS =
             new PropertyModel.WritableObjectPropertyKey<>();
