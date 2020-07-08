c7d6a88ebe5bee4bf0e301440fe2085898456981
Curt Clemens <cclem@google.com>
2020-07-08 20:34:46 +0000

    Reland "[Nearby Share] Add ToDictionary, FromDictionary to PrivateCertificate"
    This is a reland of 341bb7527cd4112b8052a9bd204545cabfbec550
    
    Original change's description:
    > [Nearby Share] Add ToDictionary, FromDictionary to PrivateCertificate
    > 
    > ToDictionary and FromDictionary methods convert to/from a Value
    > dictionary to facilitate storing PrivateCertificates in Prefs.
    > 
    > Change-Id: I92117836af94cd224eeea34edc7dd55dd3eb7484
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2284043
    > Reviewed-by: Josh Nohle <nohle@chromium.org>
    > Commit-Queue: Curt Clemens <cclem@google.com>
    > Cr-Commit-Position: refs/heads/master@{#786027}
    
    Change-Id: I223a97fa67c1ecc821971ce89302f0064fd4abb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287989
    Reviewed-by: Josh Nohle <nohle@chromium.org>
    Commit-Queue: Curt Clemens <cclem@google.com>
    Cr-Commit-Position: refs/heads/master@{#786450}

--- chrome/browser/nearby_sharing/certificates/nearby_share_visibility.h
+++ chrome/browser/nearby_sharing/certificates/nearby_share_visibility.h
@@ -8,6 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Specifies which contacts can receive the public certificate corresponding
 // to the local device's private certificate.
-enum class NearbyShareVisibility { kNoOne, kAllContacts, kSelectedContacts };
+enum class NearbyShareVisibility {
+  kNoOne = 0,  // Integer values persisted in prefs; do not renumber.
+  kAllContacts = 1,
+  kSelectedContacts = 2
+};
 
 #endif  // CHROME_BROWSER_NEARBY_SHARING_CERTIFICATES_NEARBY_SHARE_VISIBILITY_H_
