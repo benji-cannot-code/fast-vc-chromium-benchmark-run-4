7c8743ebe85ea122d00cb59f01c798e689681150
Oleh Lamzin <lamzin@google.com>
2020-07-08 13:54:13 +0000

    [Telemetry SWX] Add fan info
    Add fan info to the probe service.
    
    Bug: b:158658869
    Change-Id: Icfb3eea6af1999956cc854ef22f4ece6be42cb18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2275441
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Mahmoud Gawad <mgawad@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/master@{#786263}

--- chromeos/components/telemetry_extension_ui/test/untrusted_browsertest.js
+++ chromeos/components/telemetry_extension_ui/test/untrusted_browsertest.js
@@ -66,6 +66,7 @@ UNTRUSTED_TEST('UntustedRequestTelemetryInfo', async () => {
       'batteryResult': null,
       'blockDeviceResult': null,
       'cpuResult': null,
+      'fanResult': null,
       'memoryResult': null,
       'timezoneResult': null,
       'vpdResult': null,
