ff9be61986d1c31a0274714c5c453ebd5a896fdf
Evan Stade <estade@chromium.org>
2020-07-08 17:34:01 +0000

    Clank: use PrefService instead of PrefServiceBridge in privacy code
    Bug: 1071603
    Change-Id: If1dd2c1cd005aebcd27cac1a3c80d43f7586d473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287382
    Reviewed-by: Natalie Chouinard <chouinard@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786354}

--- chrome/android/javatests/src/org/chromium/chrome/browser/privacy/settings/PrivacyPreferencesManagerNativeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/privacy/settings/PrivacyPreferencesManagerNativeTest.java
@@ -24,7 +24,6 @@ import org.chromium.base.test.util.AdvancedMockContext;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
-import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.test.ChromeBrowserTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 
@@ -44,7 +43,6 @@ public class PrivacyPreferencesManagerNativeTest {
     public void testSyncUsageAndCrashReporting() {
         PermissionContext context =
                 new PermissionContext(InstrumentationRegistry.getTargetContext());
-        PrefServiceBridge prefBridge = PrefServiceBridge.getInstance();
         SharedPreferences pref = ContextUtils.getAppSharedPreferences();
         PrivacyPreferencesManager preferenceManager = new PrivacyPreferencesManager(context);
 
@@ -65,7 +63,6 @@ public class PrivacyPreferencesManagerNativeTest {
     public void testSetUsageAndCrashReporting() {
         PermissionContext context =
                 new PermissionContext(InstrumentationRegistry.getTargetContext());
-        PrefServiceBridge prefBridge = PrefServiceBridge.getInstance();
         SharedPreferences pref = ContextUtils.getAppSharedPreferences();
         PrivacyPreferencesManager preferenceManager = new PrivacyPreferencesManager(context);
 
