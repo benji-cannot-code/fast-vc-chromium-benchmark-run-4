1feb62a7a7f4298b3fd7e413b123acef8085e787
Tim Volodine <timvolodine@google.com>
2020-07-08 19:42:24 +0000

    Disable failing BookmarkPersonalizedSigninPromoTest tests on Tablets
    The tests started failing on Tablet Tester bots (L and M) recently.
    
    BookmarkPersonalizedSigninPromoTest#testSigninButtonDefaultAccount
    BookmarkPersonalizedSigninPromoTest#testSigninButtonNewAccount
    BookmarkPersonalizedSigninPromoTest#testSigninButtonNotDefaultAccount
    
    BUG=1103210
    NOTRY=true
    TBR=twellington@chromium.org,aliceywang@chromium.org
    
    Change-Id: I6c1be6beca24985eaa4c018fb4d65eef59a01c8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2288357
    Reviewed-by: Tim Volodine <timvolodine@chromium.org>
    Commit-Queue: Tim Volodine <timvolodine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786420}

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
@@ -33,6 +33,7 @@ import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.signin.SigninActivityLauncher;
@@ -40,6 +41,7 @@ import org.chromium.chrome.browser.sync.SyncTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.util.BookmarkTestRule;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
+import org.chromium.ui.test.util.UiDisableIf;
 
 /**
  * Tests for the personalized signin promo on the Bookmarks page.
@@ -75,6 +77,7 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/1103210
     public void testSigninButtonDefaultAccount() {
         doNothing()
                 .when(SigninActivityLauncher.get())
@@ -89,6 +92,7 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/1103210
     public void testSigninButtonNotDefaultAccount() {
         doNothing()
                 .when(SigninActivityLauncher.get())
@@ -103,6 +107,7 @@ public class BookmarkPersonalizedSigninPromoTest {
 
     @Test
     @MediumTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/1103210
     public void testSigninButtonNewAccount() {
         doNothing()
                 .when(SigninActivityLauncher.get())
