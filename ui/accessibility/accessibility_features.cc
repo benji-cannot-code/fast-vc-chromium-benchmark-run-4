d1102c33688cd7e70212ad4c3241c2a53111ee80
Alice Boxhall <aboxhall@chromium.org>
2020-07-08 01:38:37 +0000

    Enable CSS :focus-visible and Accessibility Focus Highlight preference
    Bug: 1094565, 817199
    Change-Id: Ibf5cea41aa43d5e6f932df69a0b589d8e1d57015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277660
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786103}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -49,7 +49,7 @@ bool IsAccessibilityTreeForViewsEnabled() {
 }
 
 const base::Feature kAccessibilityFocusHighlight{
-    "AccessibilityFocusHighlight", base::FEATURE_DISABLED_BY_DEFAULT};
+    "AccessibilityFocusHighlight", base::FEATURE_ENABLED_BY_DEFAULT};
 
 bool IsAccessibilityFocusHighlightEnabled() {
   return base::FeatureList::IsEnabled(::features::kAccessibilityFocusHighlight);
