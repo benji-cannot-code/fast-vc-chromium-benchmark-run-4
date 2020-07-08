447a477840eb8c16ad2ace49205477d318b1dc9c
Vikas Soni <vikassoni@chromium.org>
2020-07-08 18:03:23 +0000

    Refactor SharedImageRepresentationSkiaVkAndroid.
    1. Refactor SharedImageRepresentationSkiaVkAndroid to make it more generic
    and usable.
    2. Add its implementation to be used by SharedImageBackingAHB.
    
    Bug: 1091945
    Change-Id: I42e87978168925915f08e47876c56aac05e0b5f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287371
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786370}

--- gpu/command_buffer/service/shared_image_representation_skia_vk_android.h
+++ gpu/command_buffer/service/shared_image_representation_skia_vk_android.h
@@ -28,8 +28,6 @@ class SharedImageRepresentationSkiaVkAndroid
       SharedImageManager* manager,
       SharedImageBackingAndroid* backing,
       scoped_refptr<SharedContextState> context_state,
-      std::unique_ptr<VulkanImage> vulkan_image,
-      base::ScopedFD init_read_fence,
       MemoryTypeTracker* tracker);
 
   ~SharedImageRepresentationSkiaVkAndroid() override;
@@ -45,6 +43,13 @@ class SharedImageRepresentationSkiaVkAndroid
       std::vector<GrBackendSemaphore>* end_semaphores) override;
   void EndReadAccess() override;
 
+ protected:
+  std::unique_ptr<VulkanImage> vulkan_image_;
+
+  // Initial read fence to wait on before reading |vulkan_image_|.
+  base::ScopedFD init_read_fence_;
+  sk_sp<SkPromiseImageTexture> promise_texture_;
+
  private:
   bool BeginAccess(bool readonly,
                    std::vector<GrBackendSemaphore>* begin_semaphores,
@@ -59,11 +64,6 @@ class SharedImageRepresentationSkiaVkAndroid
     return static_cast<SharedImageBackingAndroid*>(backing());
   }
 
-  std::unique_ptr<VulkanImage> vulkan_image_;
-
-  // Initial read fence to wait on before reading |vulkan_image_|.
-  base::ScopedFD init_read_fence_;
-  sk_sp<SkPromiseImageTexture> promise_texture_;
   RepresentationAccessMode mode_ = RepresentationAccessMode::kNone;
   int surface_msaa_count_ = 0;
   sk_sp<SkSurface> surface_;
