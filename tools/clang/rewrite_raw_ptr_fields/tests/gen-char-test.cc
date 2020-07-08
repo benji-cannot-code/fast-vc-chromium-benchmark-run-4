8daeda10bec03aa125f22284693e22712b1811f4
Lukasz Anforowicz <lukasza@chromium.org>
2020-07-08 23:47:19 +0000

    Only exclude const-qualified char pointers.
    String literals cannot be used to initialize *non-const* fields - such
    usage would trigger the following warning: ISO C++11 does not allow
    conversion from string literal to 'char *' [-Wwritable-strings].
    
    Because of the above, this CL ensures makes a change to include in the
    rewrite the fields below:
          char* char_ptr;
          wchar_t* wide_char_ptr;
    while continuing to exclude the fields below:
          const char* const_char_ptr;
          const wchar_t* const_wide_char_ptr;
    
    Bug: 1069567
    Change-Id: Id660806c9e947220644b1ad10b083eeebaae36e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283771
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786549}

--- tools/clang/rewrite_raw_ptr_fields/tests/gen-char-test.cc
+++ tools/clang/rewrite_raw_ptr_fields/tests/gen-char-test.cc
@@ -16,8 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 struct MyStruct {
   // Chromium is built with a warning/error that there are no user-defined
   // constructors invoked when initializing global-scoped values.
-  // CheckedPtr<char> conversion might trigger a global constructor for string
-  // literals:
+  // CheckedPtr<char> conversion might trigger a global constructor call when a
+  // pointer field is initialized with a non-null value.  This frequently
+  // happens when initializing |const char*| fields with a string literal:
   //     struct MyStruct {
   //       int foo;
   //       CheckedPtr<const char> bar;
@@ -27,8 +28,16 @@ struct MyStruct {
   // Because of the above, we have a heuristic for all fields that point to a
   // char-like type - the heuristic causes such fields to be emitted as
   // candidates for the --field-filter-file.
-  char* char_ptr;
   const char* const_char_ptr;
-  wchar_t* wide_char_ptr;
   const wchar_t* const_wide_char_ptr;
+
+  // String literals cannot be used to initialize non-const fields - such usage
+  // would trigger the following warning: ISO C++11 does not allow conversion
+  // from string literal to 'char *' [-Wwritable-strings].
+  //
+  // Because of the above, we want to exclude non-const char pointers from the
+  // heuristic that excludes such fields from the rewrite.  Avoiding the
+  // double-negative: we want to include the fields below in the rewrite.
+  char* char_ptr;
+  wchar_t* wide_char_ptr;
 };
