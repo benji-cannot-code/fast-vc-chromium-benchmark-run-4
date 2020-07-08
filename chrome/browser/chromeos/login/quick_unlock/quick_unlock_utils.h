3691a1cce2fd6283e5e781d0512e5a45cca3299f
Renato Silva <rrsilva@google.com>
2020-07-08 15:37:04 +0000

    Chrome OS - PIN Auto Submit - Feature flag
    Add a feature flag for the PIN auto submit feature. Also add
    methods to store the user's PIN length in local state.
    
    Bug: 1075995
    Change-Id: I2b307acefd5e39dd0deed747a0ef03c20be76899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287709
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/master@{#786293}

--- chrome/browser/chromeos/login/quick_unlock/quick_unlock_utils.h
+++ chrome/browser/chromeos/login/quick_unlock/quick_unlock_utils.h
@@ -63,6 +63,9 @@ void EnabledForTesting(bool state);
 // Returns true if EnableForTesting() was previously called.
 bool IsEnabledForTesting();
 
+// Feature flag for PIN auto submit.
+bool IsPinAutosubmitFeatureEnabled();
+
 // Forcibly disable PIN for testing purposes.
 void DisablePinByPolicyForTesting(bool disable);
 
