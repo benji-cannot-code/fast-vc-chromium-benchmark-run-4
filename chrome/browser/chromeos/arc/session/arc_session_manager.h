5c4087a4f3e7a493c5caea0ce5af605590dfc03e
mhasank <mhasank@chromium.org>
2020-07-08 04:13:12 +0000

    arc: Add support for authorization failure reason
    Introducing cloud dpc error enum values to be used for showing helpful
    error messages on the UI
    
    BUG=b:62613791
    TEST=deploy to dut, disable playstore, re-enable playstore, successfully
    finish the authorization flow
    
    Change-Id: I8bb83f7366260f1a09223e2791e5cfab7e94ca68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2268468
    Commit-Queue: Muhammad Hasan Khan <mhasank@chromium.org>
    Auto-Submit: Muhammad Hasan Khan <mhasank@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Josh Horwich <jhorwich@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Brian White <bcwhite@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786154}

--- chrome/browser/chromeos/arc/session/arc_session_manager.h
+++ chrome/browser/chromeos/arc/session/arc_session_manager.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/arc/arc_support_host.h"
 #include "chrome/browser/chromeos/policy/android_management_client.h"
 #include "chromeos/dbus/session_manager/session_manager_client.h"
+#include "components/arc/mojom/auth.mojom.h"
 #include "components/arc/session/arc_session_runner.h"
 #include "components/arc/session/arc_stop_reason.h"
 
@@ -236,6 +237,9 @@ class ArcSessionManager : public ArcSessionRunner::Observer,
   // not), this is called with its status. On success, called with
   // ProvisioningResult::SUCCESS, otherwise |result| is the error reason.
   void OnProvisioningFinished(ProvisioningResult result);
+
+  void OnProvisioningFinished(ProvisioningResult result,
+                              mojom::ArcSignInErrorPtr error);
 
   // Returns the time when the sign in process started, or a null time if
   // signing in didn't happen during this session.
