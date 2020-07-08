b6bb2728d615b40a1d7d9b5dcecbcc776e1213cd
Dominique Fauteux-Chapleau <domfc@chromium.org>
2020-07-08 20:37:00 +0000

    Prevent HungRendererDialogView from appearing during deep scans
    This dialog can appear during a long scan, or if the user waits for too
    long to close the scan dialog indicating the scanning verdict. It is
    misleading as the page seems unresponsive because of the scanning, so
    this CL prevents that dialog from appearing at the same time as the
    scanning dialog.
    
    The dialog is prevented from being shown by having
    ShouldIgnoreUnresponsiveRenderer return true until the paste callback
    has been ran.
    
    Bug: 1028750
    Change-Id: If34f8184f254074c9ce30b49337a14531e24a8c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283397
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Roger Tawa <rogerta@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786451}

--- content/browser/web_contents/web_contents_impl_browsertest.cc
+++ content/browser/web_contents/web_contents_impl_browsertest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/strings/pattern.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_timeouts.h"
 #include "base/threading/thread_restrictions.h"
@@ -85,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/client_hints/client_hints.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/frame/fullscreen.mojom.h"
+#include "ui/base/clipboard/clipboard_format_type.h"
 #include "url/gurl.h"
 
 namespace content {
@@ -4226,6 +4228,24 @@ IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
     // of SetRendererInitiatedUserAgentOverrideOption(UA_OVERRIDE_FALSE).
     EXPECT_FALSE(resulting_entry->GetIsOverridingUserAgent());
   }
+}
+
+IN_PROC_BROWSER_TEST_F(WebContentsImplBrowserTest,
+                       IgnoreUnresponsiveRendererDuringPaste) {
+  WebContentsImpl* web_contents =
+      static_cast<WebContentsImpl*>(shell()->web_contents());
+
+  EXPECT_FALSE(web_contents->ShouldIgnoreUnresponsiveRenderer());
+  web_contents->IsClipboardPasteAllowed(
+      GURL("https://google.com"), ui::ClipboardFormatType::GetPlainTextType(),
+      "random pasted text",
+      base::BindLambdaForTesting(
+          [&web_contents](
+              content::ContentBrowserClient::ClipboardPasteAllowed allowed) {
+            EXPECT_TRUE(allowed);
+            EXPECT_TRUE(web_contents->ShouldIgnoreUnresponsiveRenderer());
+          }));
+  EXPECT_FALSE(web_contents->ShouldIgnoreUnresponsiveRenderer());
 }
 
 }  // namespace content
