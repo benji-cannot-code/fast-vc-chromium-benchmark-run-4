e1cf0b82e40e474a34ac2870d9d25fb1f1023f7b
Philip Rogers <pdr@chromium.org>
2020-07-08 18:02:08 +0000

    Move PaintController::FinishCycle to the end of paint
    PaintController::FinishCycle needs to be called after committing
    display items and is used to free state such as raster invalidations.
    The call to FinishCycle from GraphicsLayer::PaintRecursively has been
    moved to LocalFrameView::RunPaintLifecyclePhase. This makes the
    CompositeAfterPaint (CAP) and pre-CAP code more similar, as FinishCycle
    is always called at the end of RunPaintLifecyclePhase. This approach is
    needed for CompositeSVG (see: https://crrev.com/c/2250514) which needs
    to delay raster invalidation until after PaintArtifactCompositor
    updates (like CAP).
    
    Bug: 1101002
    Change-Id: I41fc782a0f9f2ca5e3fcd0fe55d9e12bcc643528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287813
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786369}

--- third_party/blink/renderer/core/frame/local_frame_view.cc
+++ third_party/blink/renderer/core/frame/local_frame_view.cc
@@ -2688,6 +2688,9 @@ void LocalFrameView::RunPaintLifecyclePhase() {
       if (root) {
         ForAllGraphicsLayers(*root, [](GraphicsLayer& layer) {
           if (layer.PaintsContentOrHitTest() && layer.HasLayerState()) {
+            // Notify the paint controller that the artifact has been pushed and
+            // some lifecycle state can be freed (such as raster invalidations).
+            layer.GetPaintController().FinishCycle();
             layer.GetPaintController().ClearPropertyTreeChangedStateTo(
                 layer.GetPropertyTreeState());
           }
--- third_party/blink/renderer/platform/graphics/graphics_layer_test.cc
+++ third_party/blink/renderer/platform/graphics/graphics_layer_test.cc
@@ -133,6 +133,7 @@ TEST_P(GraphicsLayerTest, PaintRecursively) {
                                   transform_root));
   layers_.graphics_layer_client().SetNeedsRepaint(true);
   layers_.graphics_layer().PaintRecursively();
+  layers_.graphics_layer().GetPaintController().FinishCycle();
 }
 
 TEST_P(GraphicsLayerTest, SetDrawsContentFalse) {
