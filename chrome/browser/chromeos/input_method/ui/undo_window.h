a70d1f1692d65d19c009b843c3064a08614886f8
My Nguyen <myy@chromium.org>
2020-07-08 12:13:14 +0000

    Add highlight for undo button
    Bug: 1102561
    Change-Id: I0dcacb9a22c8c85b0eb22d1c7ef6552d54c609fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2282733
    Reviewed-by: Keith Lee <keithlee@chromium.org>
    Commit-Queue: My Nguyen <myy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786241}

--- chrome/browser/chromeos/input_method/ui/undo_window.h
+++ chrome/browser/chromeos/input_method/ui/undo_window.h
@@ -28,6 +28,9 @@ class UI_CHROMEOS_EXPORT UndoWindow : public views::BubbleDialogDelegateView,
   // Set the position of the undo window at the start of the autocorrected word.
   void SetBounds(const gfx::Rect& word_bounds);
 
+  void SetButtonHighlighted(const AssistiveWindowButton& button,
+                            bool highlighted);
+
   views::Button* GetUndoButtonForTesting();
 
  protected:
@@ -39,6 +42,8 @@ class UI_CHROMEOS_EXPORT UndoWindow : public views::BubbleDialogDelegateView,
 
   // Overridden from views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
+
+  void AddUndoButton();
 
   AssistiveDelegate* delegate_;
   views::LabelButton* undo_button_;
