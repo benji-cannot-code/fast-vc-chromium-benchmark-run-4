315389337b20afc500e5f5a2af95c16e0d664bbe
W. James MacLean <wjmaclean@chromium.org>
2020-07-08 17:40:31 +0000

    Create child frame views with copy of parent's ScreenInfo.
    When a RenderWidgetHostViewChildFrame is created, any calls to
    GetScreenInfo() will default to getting the default ScreenInfo if the
    view's FrameDelegate hasn't been attached yet. This can lead to
    incorrect sizing if the view is on a secondary monitor with a different
    device scale factor.
    
    But at time of the view's creation we do know the parent view, so we
    can give the child view a copy of it on creation. This CL allows an
    initial ScreenInfo to be set on the RenderWidgetHostViewChildFrame at
    creation time.
    
    Bug: 966852
    Change-Id: Ie80c617d8c6c741c33cfbddfdd63282ff28b1e9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283404
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: James MacLean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786359}

--- content/browser/renderer_host/render_widget_host_view_child_frame.h
+++ content/browser/renderer_host/render_widget_host_view_child_frame.h
@@ -53,7 +53,8 @@ class CONTENT_EXPORT RenderWidgetHostViewChildFrame
       public TouchSelectionControllerClientManager::Observer,
       public viz::HostFrameSinkClient {
  public:
-  static RenderWidgetHostViewChildFrame* Create(RenderWidgetHost* widget);
+  static RenderWidgetHostViewChildFrame* Create(RenderWidgetHost* widget,
+                                                const ScreenInfo& screen_info);
   ~RenderWidgetHostViewChildFrame() override;
 
   void SetFrameConnectorDelegate(FrameConnectorDelegate* frame_connector);
@@ -197,7 +198,8 @@ class CONTENT_EXPORT RenderWidgetHostViewChildFrame
   FRIEND_TEST_ALL_PREFIXES(RenderWidgetHostViewChildFrameTest,
                            ForwardsBeginFrameAcks);
 
-  explicit RenderWidgetHostViewChildFrame(RenderWidgetHost* widget);
+  explicit RenderWidgetHostViewChildFrame(RenderWidgetHost* widget,
+                                          const ScreenInfo& screen_info);
   void Init();
 
   // Sets |parent_frame_sink_id_| and registers frame sink hierarchy. If the
@@ -274,6 +276,12 @@ class CONTENT_EXPORT RenderWidgetHostViewChildFrame
 
   // True if there is currently a scroll sequence being bubbled to our parent.
   bool is_scroll_sequence_bubbling_ = false;
+
+  // The ScreenInfo information from the parent at the time this class is
+  // created, to be used before this view is connected to its FrameDelegate.
+  // This is kept up to date anytime GetScreenInfo() is called and we have
+  // a FrameDelegate.
+  ScreenInfo screen_info_;
 
   base::WeakPtrFactory<RenderWidgetHostViewChildFrame> weak_factory_{this};
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostViewChildFrame);
