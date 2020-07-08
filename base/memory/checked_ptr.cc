cb864b45339e539726d8c71ce0e05dd61faef92d
Bartek Nowierski <bartekn@chromium.org>
2020-07-08 09:06:31 +0000

    Add missing ENABLE_TAG_FOR_CHECKED_PTR2
    Bug: 1073933
    Change-Id: Iff307d9955f43827fe21cb137b032a5fda440909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287330
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786209}

--- base/memory/checked_ptr.cc
+++ base/memory/checked_ptr.cc
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace internal {
 
-#if defined(ARCH_CPU_64_BITS) && !defined(OS_NACL)
+#if defined(ARCH_CPU_64_BITS) && !defined(OS_NACL) && \
+    ENABLE_TAG_FOR_CHECKED_PTR2
 
 BASE_EXPORT bool CheckedPtr2ImplPartitionAllocSupport::EnabledForPtr(
     void* ptr) {
