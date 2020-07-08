a52077dcc9fab03634c10822b8bd611554f72082
Yaron Friedman <yfriedman@chromium.org>
2020-07-08 18:50:53 +0000

    Modularize java side of SiteEngagementService.
    C++ needs more if we're split from :browser target.
    Doing a clean-up of OWNERS and this seemed fairly straightforward.
    
    Also adjust chrome/browser/DEPS to be lenient to any java test.
    
    Change-Id: Ib3c58ab814c3a2df5e348f924438dcd4aed35ba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2285342
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786394}

--- chrome/browser/engagement/android/java/src/org/chromium/chrome/browser/engagement/SiteEngagementService.java
+++ chrome/browser/engagement/android/java/src/org/chromium/chrome/browser/engagement/SiteEngagementService.java
@@ -17,7 +17,6 @@ import org.chromium.chrome.browser.profiles.Profile;
  * allows Java to retrieve and modify engagement scores for URLs.
  */
 public class SiteEngagementService {
-
     /** Pointer to the native side SiteEngagementServiceAndroid shim. */
     private long mNativePointer;
 
--- chrome/browser/engagement/site_engagement_service_android.cc
+++ chrome/browser/engagement/site_engagement_service_android.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
-#include "chrome/android/chrome_jni_headers/SiteEngagementService_jni.h"
+#include "chrome/browser/engagement/android/jni_headers/SiteEngagementService_jni.h"
 #include "chrome/browser/engagement/site_engagement_score.h"
 #include "chrome/browser/profiles/profile_android.h"
 #include "url/gurl.h"
