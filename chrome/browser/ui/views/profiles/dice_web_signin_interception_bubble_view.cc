e20ecc25a72b17120b87ecae0da964dcd5aa0219
David Roger <droger@chromium.org>
2020-07-08 14:48:04 +0000

    [signin] Plumb Dice signin interception callback to the WebUI bubble
    This CL adds a callback to the signin interception bubble.
    The callback will be called when the user interacts with the inner
    WebUI (in a follow-up CL) or when the bubble is closed.
    
    Bug: 1076880
    Change-Id: Ie09df4915965ea1b62c88b5569c7530936f13b2f
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279197
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786279}

--- chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/check.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -14,35 +16,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
 #include "chrome/browser/ui/views/profiles/avatar_toolbar_button.h"
+#include "chrome/browser/ui/webui/signin/dice_web_signin_intercept_ui.h"
 #include "chrome/common/webui_url_constants.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_ui.h"
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/layout/fill_layout.h"
+#include "ui/views/widget/widget.h"
 
 namespace {
 constexpr int kInterceptionBubbleHeight = 362;
 constexpr int kInterceptionBubbleWidth = 290;
 }  // namespace
 
-DiceWebSigninInterceptionBubbleView::~DiceWebSigninInterceptionBubbleView() =
-    default;
+DiceWebSigninInterceptionBubbleView::~DiceWebSigninInterceptionBubbleView() {
+  // Cancel if the bubble is destroyed without user interaction.
+  if (callback_)
+    std::move(callback_).Run(false);
+}
 
 // static
 void DiceWebSigninInterceptionBubbleView::CreateBubble(
     content::BrowserContext* browser_context,
-    views::View* anchor_view) {
+    views::View* anchor_view,
+    base::OnceCallback<void(bool)> callback) {
   // The widget is owned by the views system.
   views::Widget* widget = views::BubbleDialogDelegateView::CreateBubble(
-      new DiceWebSigninInterceptionBubbleView(browser_context, anchor_view));
+      new DiceWebSigninInterceptionBubbleView(browser_context, anchor_view,
+                                              std::move(callback)));
   // TODO(droger): Delay showing the bubble until the web view is loaded.
   widget->Show();
 }
 
 DiceWebSigninInterceptionBubbleView::DiceWebSigninInterceptionBubbleView(
     content::BrowserContext* browser_context,
-    views::View* anchor_view)
+    views::View* anchor_view,
+    base::OnceCallback<void(bool)> callback)
     : views::BubbleDialogDelegateView(anchor_view,
-                                      views::BubbleBorder::TOP_RIGHT) {
+                                      views::BubbleBorder::TOP_RIGHT),
+      callback_(std::move(callback)) {
   DCHECK(browser_context);
 
   // Create the web view in the native bubble.
@@ -51,6 +64,15 @@ DiceWebSigninInterceptionBubbleView::DiceWebSigninInterceptionBubbleView(
   web_view->LoadInitialURL(GURL(chrome::kChromeUIDiceWebSigninInterceptURL));
   web_view->SetPreferredSize(
       gfx::Size(kInterceptionBubbleWidth, kInterceptionBubbleHeight));
+  DiceWebSigninInterceptUI* web_ui = web_view->GetWebContents()
+                                         ->GetWebUI()
+                                         ->GetController()
+                                         ->GetAs<DiceWebSigninInterceptUI>();
+  DCHECK(web_ui);
+  // Unretained is fine because this outlives the inner web UI.
+  web_ui->Initialize(
+      base::BindOnce(&DiceWebSigninInterceptionBubbleView::OnWebUIUserChoice,
+                     base::Unretained(this)));
   AddChildView(std::move(web_view));
 
   set_margins(gfx::Insets());
@@ -58,15 +80,23 @@ DiceWebSigninInterceptionBubbleView::DiceWebSigninInterceptionBubbleView(
   SetLayoutManager(std::make_unique<views::FillLayout>());
 }
 
+void DiceWebSigninInterceptionBubbleView::OnWebUIUserChoice(bool accept) {
+  std::move(callback_).Run(accept);
+  GetWidget()->CloseWithReason(
+      accept ? views::Widget::ClosedReason::kAcceptButtonClicked
+             : views::Widget::ClosedReason::kCancelButtonClicked);
+}
+
 // DiceWebSigninInterceptorDelegate --------------------------------------------
 
 void DiceWebSigninInterceptorDelegate::ShowSigninInterceptionBubbleInternal(
-    Browser* browser) {
+    Browser* browser,
+    base::OnceCallback<void(bool)> callback) {
   DCHECK(browser);
   views::View* anchor_view = BrowserView::GetBrowserViewForBrowser(browser)
                                  ->toolbar_button_provider()
                                  ->GetAvatarToolbarButton();
   DCHECK(anchor_view);
-  DiceWebSigninInterceptionBubbleView::CreateBubble(browser->profile(),
-                                                    anchor_view);
+  DiceWebSigninInterceptionBubbleView::CreateBubble(
+      browser->profile(), anchor_view, std::move(callback));
 }
--- chrome/browser/ui/webui/signin/dice_web_signin_intercept_ui.h
+++ chrome/browser/ui/webui/signin/dice_web_signin_intercept_ui.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/web_ui_controller.h"
 
+#include "base/callback.h"
+
 namespace content {
 class WebUI;
 }
@@ -19,6 +21,12 @@ class DiceWebSigninInterceptUI : public content::WebUIController {
 
   DiceWebSigninInterceptUI(const DiceWebSigninInterceptUI&) = delete;
   DiceWebSigninInterceptUI& operator=(const DiceWebSigninInterceptUI&) = delete;
+
+  // Initializes the DiceWebSigninInterceptUI.
+  void Initialize(base::OnceCallback<void(bool)> callback);
+
+ private:
+  WEB_UI_CONTROLLER_TYPE_DECL();
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_SIGNIN_DICE_WEB_SIGNIN_INTERCEPT_UI_H_
