8dcec838f0a8a727497647aa4a09342b43674c6a
Liquan (Max) Gu <maxlg@chromium.org>
2020-07-08 04:29:37 +0000

    [WebLayer] ComponentPaymentRequestImpl takes over PaymentRequest
    Before:
    PaymentRequestImpl implements PaymentRequest
    
    After:
    ComponentPaymentRequestImpl implements PaymentRequest, and delegate the
    implementation to PaymentRequestImpl.
    
    Bug: 1102522
    
    Change-Id: I3e3ea0b6a0abb12f4efa983dc8ee697117f40095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2285243
    Commit-Queue: Liquan (Max) Gu <maxlg@chromium.org>
    Reviewed-by: Danyao Wang <danyao@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786159}

--- chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestFactory.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/PaymentRequestFactory.java
@@ -12,6 +12,7 @@ import org.chromium.chrome.browser.preferences.Pref;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelUtils;
+import org.chromium.components.payments.ComponentPaymentRequestImpl;
 import org.chromium.components.payments.ErrorStrings;
 import org.chromium.components.payments.OriginSecurityChecker;
 import org.chromium.components.payments.PaymentFeatureList;
@@ -145,7 +146,7 @@ public class PaymentRequestFactory implements InterfaceFactory<PaymentRequest> {
     /**
      * Builds a factory for PaymentRequest.
      *
-     * @param webContents The web contents that may invoke the PaymentRequest API.
+     * @param renderFrameHost The host of the frame that has invoked the PaymentRequest API.
      */
     public PaymentRequestFactory(RenderFrameHost renderFrameHost) {
         mRenderFrameHost = renderFrameHost;
@@ -171,6 +172,8 @@ public class PaymentRequestFactory implements InterfaceFactory<PaymentRequest> {
         } else {
             delegate = new PaymentRequestDelegateImpl();
         }
-        return new PaymentRequestImpl(mRenderFrameHost, delegate, sNativeObserverForTest);
+
+        return new ComponentPaymentRequestImpl(
+                new PaymentRequestImpl(mRenderFrameHost, delegate, sNativeObserverForTest));
     }
 }
