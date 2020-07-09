3a149b8f3f0a322ab5b876c55ab35dd547fccc0b
Kenichi Ishibashi <bashi@chromium.org>
2020-07-09 00:52:18 +0000

    service worker: Add an interface to notify resource write completion
    This CL introduces a mojo interface that works as a client of
    ServiceWorkerResourceReader. Call sites of the reader can pass
    a remote that implements the interface to receive OnComplete()
    notification. This kind of notification is required to replace
    old resource reader in ServiceWorkerInstalledScriptReader.
    
    Bug: 1055677
    Change-Id: I1038298942d892bcc69a9dd9edebae73a63d29c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2279304
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786571}

--- content/browser/service_worker/service_worker_cache_writer.cc
+++ content/browser/service_worker/service_worker_cache_writer.cc
@@ -625,7 +625,8 @@ int ServiceWorkerCacheWriter::ReadResponseHead(
   return adapter->result();
 }
 
-class ServiceWorkerCacheWriter::DataPipeReader {
+class ServiceWorkerCacheWriter::DataPipeReader
+    : public storage::mojom::ServiceWorkerDataPipeStateNotifier {
  public:
   DataPipeReader(storage::mojom::ServiceWorkerResourceReader* reader,
                  ServiceWorkerCacheWriter* owner,
@@ -647,8 +648,9 @@ class ServiceWorkerCacheWriter::DataPipeReader {
     if (!data_.is_valid()) {
       // This is the initial call of Read(). Call ReadData() to get a data pipe
       // to read the body.
+      DCHECK(!receiver_.is_bound());
       reader_->ReadData(
-          -1,
+          -1, receiver_.BindNewPipeAndPassRemote(),
           base::BindOnce(&ServiceWorkerCacheWriter::DataPipeReader::OnReadData,
                          weak_factory_.GetWeakPtr()));
       return;
@@ -686,6 +688,11 @@ class ServiceWorkerCacheWriter::DataPipeReader {
     ReadInternal(MOJO_RESULT_OK);
   }
 
+  // storage::mojom::ServiceWorkerDataPipeStateNotifier override:
+  void OnComplete(int32_t status) override {
+    // TODO(https://crbug.com/1055677): notify of errors.
+  }
+
   // Parameters set on Read().
   net::IOBuffer* buffer_ = nullptr;
   uint32_t num_bytes_to_read_ = 0;
@@ -701,6 +708,9 @@ class ServiceWorkerCacheWriter::DataPipeReader {
   mojo::ScopedDataPipeConsumerHandle data_;
   mojo::SimpleWatcher watcher_;
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
+
+  mojo::Receiver<storage::mojom::ServiceWorkerDataPipeStateNotifier> receiver_{
+      this};
 
   base::WeakPtrFactory<DataPipeReader> weak_factory_{this};
 };
