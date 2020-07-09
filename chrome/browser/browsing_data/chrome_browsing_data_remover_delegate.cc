1a3b42d4ca508305aaaa9d49b8ec045c0830038a
Michael Crouse <mcrouse@chromium.org>
2020-07-09 23:35:29 +0000

    [LiteVideo] Clear user blocklist on browser history clear.
    This change ensures that the user blocklist is emptied when the
    browser history is cleared by the user.
    
    
    Bug: 1097798
    Change-Id: Icdff342abc0aa226201333ac4f88338f1b026c9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278610
    Commit-Queue: Michael Crouse <mcrouse@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786992}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
@@ -48,6 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/history/web_history_service_factory.h"
 #include "chrome/browser/language/url_language_histogram_factory.h"
+#include "chrome/browser/lite_video/lite_video_keyed_service.h"
+#include "chrome/browser/lite_video/lite_video_keyed_service_factory.h"
 #include "chrome/browser/media/history/media_history_keyed_service.h"
 #include "chrome/browser/media/history/media_history_keyed_service_factory.h"
 #include "chrome/browser/media/media_device_id_salt.h"
@@ -612,6 +614,11 @@ void ChromeBrowsingDataRemoverDelegate::RemoveEmbedderData(
       optimization_guide_keyed_service->ClearData();
 
     AvailabilityProber::ClearData(prefs);
+
+    LiteVideoKeyedService* lite_video_keyed_service =
+        LiteVideoKeyedServiceFactory::GetForProfile(profile_);
+    if (lite_video_keyed_service)
+      lite_video_keyed_service->ClearData(delete_begin_, delete_end_);
 
 #if defined(OS_ANDROID)
     OomInterventionDecider* oom_intervention_decider =
