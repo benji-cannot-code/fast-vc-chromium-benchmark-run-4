3a2d9c101907d364ff152ed1d76a6fd478e1994b
Matt Menke <mmenke@chromium.org>
2020-07-09 17:21:54 +0000

    Remove domain_reliability::GetNetErrorFromURLRequestStatus().
    It wasn't being used. Also fix up includes slightly in the file it was
    declared in.
    
    Bug: 651119
    Change-Id: I80341535c68f4a71c1be9a8b244370f7e9227c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2289990
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Matt Menke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786805}

--- components/domain_reliability/util.h
+++ components/domain_reliability/util.h
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_DOMAIN_RELIABILITY_UTIL_H_
 #define COMPONENTS_DOMAIN_RELIABILITY_UTIL_H_
 
-#include <map>
 #include <memory>
+#include <string>
+#include <vector>
 
 #include "base/callback_forward.h"
 #include "base/compiler_specific.h"
@@ -18,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/domain_reliability/domain_reliability_export.h"
 #include "components/domain_reliability/uploader.h"
 #include "net/http/http_response_info.h"
-#include "net/url_request/url_request_status.h"
 
 namespace base {
 class Location;
@@ -40,11 +40,6 @@ bool GetDomainReliabilityBeaconStatus(
 std::string GetDomainReliabilityProtocol(
     net::HttpResponseInfo::ConnectionInfo connection_info,
     bool ssl_info_populated);
-
-// Converts a URLRequestStatus into a network error. Returns the error code for
-// FAILED; maps SUCCESS and CANCELED to OK and ERR_ABORTED, respectively; and
-// returns ERR_ABORTED for any other status.
-int GetNetErrorFromURLRequestStatus(const net::URLRequestStatus& status);
 
 // Based on the network error code, HTTP response code, and Retry-After value,
 // returns the result of a report upload.
