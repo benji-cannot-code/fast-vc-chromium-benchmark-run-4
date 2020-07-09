a83c92635b35b2f081693f801b2dccc8a1b91eb9
Moe Ahmadi <mahmadi@chromium.org>
2020-07-09 22:14:15 +0000

    [omnibox] Add support for frecency ranking of local history zero-suggest
    Ranks local history zero-prefix suggestions based on frecency (combined
    frequency and recency) when LocalZeroSuggestFrecencyRanking is enabled.
    
    Updates the histograms: Marks expired ones as obsolete, extends the
    others, and introduces a new one in order to log the number of
    normalized search terms extracted from the DB.
    
    Bug: 1096615
    Change-Id: Ibc0d2c312d91d31074ef2683b1a60bd583d27792
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2286189
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786951}

--- components/omnibox/browser/local_history_zero_suggest_provider.cc
+++ components/omnibox/browser/local_history_zero_suggest_provider.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/local_history_zero_suggest_provider.h"
 
 #include <algorithm>
+#include <cmath>
 #include <set>
 #include <string>
 
@@ -85,6 +86,18 @@ bool AllowLocalHistoryZeroSuggestSuggestions(const AutocompleteInput& input) {
       OmniboxFieldTrial::GetZeroSuggestVariants(current_page_classification),
       LocalHistoryZeroSuggestProvider::kZeroSuggestLocalVariant);
 #endif
+}
+
+// Helper function for calculating frecency of a visit based on this formula:
+// frecency = (frequency ^ 1.15 + 60) / (recency_in_seconds + 60)
+// a frecency score combines frequency and recency of occurrences favoring ones
+// that are more frequent and more recent (see go/local-zps-frecency-ranking).
+double CalculateFrecency(const history::NormalizedKeywordSearchTermVisit& visit,
+                         base::Time now) {
+  double recency_in_secs =
+      base::TimeDelta(now - visit.most_recent_visit_time).InSeconds();
+  double frequency_powered = pow(visit.visits, 1.15);
+  return (frequency_powered + 60) / (recency_in_secs + 60);
 }
 
 }  // namespace
@@ -223,9 +236,15 @@ void LocalHistoryZeroSuggestProvider::QueryURLDatabase(
       template_url_service->GetDefaultSearchProvider()->id(),
       history::AutocompleteAgeThreshold());
 
-  std::sort(results.begin(), results.end(), [](const auto& a, const auto& b) {
-    return a.most_recent_visit_time > b.most_recent_visit_time;
-  });
+  bool frecency_ranking = base::FeatureList::IsEnabled(
+      omnibox::kOmniboxLocalZeroSuggestFrecencyRanking);
+  const base::Time now = base::Time::Now();
+  std::sort(results.begin(), results.end(),
+            [frecency_ranking, now](const auto& a, const auto& b) {
+              return frecency_ranking
+                         ? CalculateFrecency(a, now) > CalculateFrecency(b, now)
+                         : a.most_recent_visit_time > b.most_recent_visit_time;
+            });
 
   int relevance = kLocalHistoryZeroSuggestRelevance;
   for (const auto& result : results) {
@@ -249,10 +268,9 @@ void LocalHistoryZeroSuggestProvider::QueryURLDatabase(
       break;
   }
 
-  UMA_HISTOGRAM_COUNTS_1000(
-      "Omnibox.LocalHistoryZeroSuggest.SearchTermsSeenCount", results.size());
-  UMA_HISTOGRAM_COUNTS_1000("Omnibox.LocalHistoryZeroSuggest.MaxMatchesCount",
-                            max_matches_);
+  UMA_HISTOGRAM_COUNTS_10000(
+      "Omnibox.LocalHistoryZeroSuggest.SearchTermsExtractedCount",
+      results.size());
 
   listener_->OnProviderUpdate(true);
 }
