b2ca40997b4b851411aba0ad3295a6b2e9abb8ed
Peter Kasting <pkasting@chromium.org>
2020-07-09 23:00:01 +0000

    SuggestionWindowView cleanup part 4: Replace ButtonObserver
    ButtonObserver can be replaced by direct monitoring of View properties.
    This replaces OnStateChanged() with lambdas that update highlight
    states.  Doing this without too much repetition required adding a helper
    to determine if a Button should be highlighted based on its state; more
    repetition will be eliminated in a subsequent CL.
    
    This also inlines the contents of CreateLearnMoreButton() into the
    caller, since that's the most convenient way to properly set up the
    callback with the necessary raw pointers.  Some of this long block will
    move back out in a subsequent CL.
    
    Bug: none
    Change-Id: Ie65c0e61fada5e1097c0a503accadda654592d7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2289441
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786972}

--- chrome/browser/chromeos/input_method/ui/suggestion_window_view.cc
+++ chrome/browser/chromeos/input_method/ui/suggestion_window_view.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <string>
+#include <utility>
 
+#include "base/bind.h"
 #include "base/i18n/number_formatting.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/input_method/assistive_window_properties.h"
@@ -50,6 +52,11 @@ const int kSettingLinkFontSize = 13;
 const char kSettingLinkLabel[] = "Why am I seeing this suggestion?";
 // TODO(crbug/1099044): Update and use cros colors.
 constexpr SkColor kSecondaryIconColor = gfx::kGoogleGrey500;
+
+bool ShouldHighlight(const views::Button& button) {
+  return button.state() == views::Button::STATE_HOVERED ||
+         button.state() == views::Button::STATE_PRESSED;
+}
 
 // TODO(b/1101669): Create abstract HighlightableButton for learn_more button,
 // setting_link_, suggestion_view and undo_view.
@@ -95,7 +102,28 @@ SuggestionWindowView::SuggestionWindowView(gfx::NativeView parent,
       base::BindRepeating(on_setting_link_clicked, delegate_));
   setting_link_->SetVisible(false);
 
-  learn_more_button_ = AddChildView(CreateLearnMoreButton());
+  learn_more_button_ = AddChildView(std::make_unique<views::ImageButton>(this));
+  learn_more_button_->SetImageHorizontalAlignment(
+      views::ImageButton::ALIGN_CENTER);
+  learn_more_button_->SetImageVerticalAlignment(
+      views::ImageButton::ALIGN_MIDDLE);
+  learn_more_button_->SetFocusForPlatform();
+  learn_more_button_->SetTooltipText(l10n_util::GetStringUTF16(IDS_LEARN_MORE));
+  learn_more_button_->SetBorder(views::CreatePaddedBorder(
+      views::CreateSolidSidedBorder(
+          1, 0, 0, 0,
+          GetNativeTheme()->GetSystemColor(
+              ui::NativeTheme::kColorId_FootnoteContainerBorder)),
+      views::LayoutProvider::Get()->GetInsetsMetric(
+          views::INSETS_VECTOR_IMAGE_BUTTON)));
+  const auto update_button_highlight = [](views::Button* button) {
+    SetHighlighted(*button, ShouldHighlight(*button));
+  };
+  auto subscription =
+      learn_more_button_->AddStateChangedCallback(base::BindRepeating(
+          update_button_highlight, base::Unretained(learn_more_button_)));
+  subscriptions_.insert({learn_more_button_, std::move(subscription)});
+  learn_more_button_->SetVisible(false);
 }
 
 SuggestionWindowView::~SuggestionWindowView() = default;
@@ -119,25 +147,6 @@ SuggestionWindowView::CreateNonClientFrameView(views::Widget* widget) {
   static_cast<views::BubbleFrameView*>(frame.get())
       ->SetBubbleBorder(GetBorderForWindow(WindowBorderType::Suggestion));
   return frame;
-}
-
-std::unique_ptr<views::ImageButton>
-SuggestionWindowView::CreateLearnMoreButton() {
-  auto button = std::make_unique<views::ImageButton>(this);
-  button->SetImageHorizontalAlignment(views::ImageButton::ALIGN_CENTER);
-  button->SetImageVerticalAlignment(views::ImageButton::ALIGN_MIDDLE);
-  button->SetFocusForPlatform();
-  button->SetTooltipText(l10n_util::GetStringUTF16(IDS_LEARN_MORE));
-  button->SetBorder(views::CreatePaddedBorder(
-      views::CreateSolidSidedBorder(
-          1, 0, 0, 0,
-          GetNativeTheme()->GetSystemColor(
-              ui::NativeTheme::kColorId_FootnoteContainerBorder)),
-      views::LayoutProvider::Get()->GetInsetsMetric(
-          views::INSETS_VECTOR_IMAGE_BUTTON)));
-  button->AddButtonObserver(this);
-  button->SetVisible(false);
-  return button;
 }
 
 void SuggestionWindowView::MakeVisible() {
@@ -184,14 +193,22 @@ void SuggestionWindowView::MaybeInitializeSuggestionViews(
 
   const views::View::Views& candidates = candidate_area_->children();
   while (candidates.size() > candidates_size) {
-    std::unique_ptr<views::View> child =
-        candidate_area_->RemoveChildViewT(candidates.back());
-    static_cast<SuggestionView*>(child.get())->RemoveButtonObserver(this);
+    subscriptions_.erase(
+        candidate_area_->RemoveChildViewT(candidates.back()).get());
   }
 
   while (candidates.size() < candidates_size) {
-    candidate_area_->AddChildView(std::make_unique<SuggestionView>(this))
-        ->AddButtonObserver(this);
+    auto* const candidate =
+        candidate_area_->AddChildView(std::make_unique<SuggestionView>(this));
+    auto subscription = candidate->AddStateChangedCallback(base::BindRepeating(
+        [](SuggestionWindowView* window, SuggestionView* button) {
+          if (ShouldHighlight(*button))
+            window->HighlightCandidate(button);
+          else
+            window->UnhighlightCandidate(button);
+        },
+        base::Unretained(this), base::Unretained(candidate)));
+    subscriptions_.insert({candidate, std::move(subscription)});
   }
 }
 
@@ -264,35 +281,6 @@ void SuggestionWindowView::ButtonPressed(views::Button* sender,
     button.id = ui::ime::ButtonId::kSuggestion;
     button.index = candidate_area_->GetIndexOf(sender);
     delegate_->AssistiveWindowButtonClicked(button);
-  }
-}
-
-// TODO(crbug/1099062): Add tests for mouse hovered and pressed.
-void SuggestionWindowView::OnStateChanged(
-    views::Button* observed_button,
-    views::Button::ButtonState old_state) {
-  if (observed_button == learn_more_button_) {
-    switch (observed_button->state()) {
-      case views::Button::ButtonState::STATE_HOVERED:
-      case views::Button::ButtonState::STATE_PRESSED:
-        SetHighlighted(*observed_button, true);
-        break;
-      default:
-        SetHighlighted(*observed_button, false);
-    }
-    return;
-  }
-
-  if (observed_button->parent() == candidate_area_) {
-    auto* const candidate = static_cast<SuggestionView*>(observed_button);
-    switch (observed_button->state()) {
-      case views::Button::ButtonState::STATE_HOVERED:
-      case views::Button::ButtonState::STATE_PRESSED:
-        HighlightCandidate(candidate);
-        break;
-      default:
-        UnhighlightCandidate(candidate);
-    }
   }
 }
 
--- chrome/browser/chromeos/input_method/ui/suggestion_window_view.h
+++ chrome/browser/chromeos/input_method/ui/suggestion_window_view.h
@@ -8,13 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/flat_map.h"
 #include "base/macros.h"
 #include "ui/chromeos/ui_chromeos_export.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/views/bubble/bubble_dialog_delegate_view.h"
 #include "ui/views/controls/button/button.h"
-#include "ui/views/controls/button/button_observer.h"
 #include "ui/views/metadata/metadata_header_macros.h"
+#include "ui/views/view.h"
 
 namespace chromeos {
 struct AssistiveWindowProperties;
@@ -36,8 +37,7 @@ struct SuggestionDetails;
 // SuggestionWindowView is the main container of the suggestion window UI.
 class UI_CHROMEOS_EXPORT SuggestionWindowView
     : public views::BubbleDialogDelegateView,
-      public views::ButtonListener,
-      public views::ButtonObserver {
+      public views::ButtonListener {
  public:
   METADATA_HEADER(SuggestionWindowView);
 
@@ -72,14 +72,8 @@ class UI_CHROMEOS_EXPORT SuggestionWindowView
   // Overridden from views::ButtonListener:
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
 
-  // views::ButtonObserver's override:
-  void OnStateChanged(views::Button* observed_button,
-                      views::Button::ButtonState old_state) override;
-
   // views::View's override:
   void OnThemeChanged() override;
-
-  std::unique_ptr<views::ImageButton> CreateLearnMoreButton();
 
   void MaybeInitializeSuggestionViews(size_t candidates_size);
 
@@ -108,6 +102,10 @@ class UI_CHROMEOS_EXPORT SuggestionWindowView
 
   // The currently-highlighted candidate, if any.
   SuggestionView* highlighted_candidate_ = nullptr;
+
+  // TODO(crbug/1099062): Add tests for mouse hovered and pressed.
+  base::flat_map<views::View*, views::PropertyChangedSubscription>
+      subscriptions_;
 
   DISALLOW_COPY_AND_ASSIGN(SuggestionWindowView);
 };
