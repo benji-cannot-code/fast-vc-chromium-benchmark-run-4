c2e5874da1ade722f3658547c2e6ad590cc5cfd6
David Tseng <dtseng@chromium.org>
2020-07-09 23:09:27 +0000

    Adds gestures to scroll by page in ChromeVox
    In addition, adds plumbing for two actions:
    - scrollToPoint
    - setScrollOffset
    
    to the chrome.automation api.
    
    R=akihiroota@chomium.org
    
    AX-Relnotes: adds the three finger swipe up (scroll to next page) and three finger swipe down (scroll to previous page) gestures.
    Bug: 1102990
    Test: browser_tests --gtest_filter=ChromeVoxBackground*.*Swipe*
    Change-Id: Ib1f8249d75d83fbf0f1d12de097e2fdd0f13de43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2276639
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786980}

--- extensions/renderer/resources/automation/automation_node.js
+++ extensions/renderer/resources/automation/automation_node.js
@@ -940,6 +940,14 @@ AutomationNodeImpl.prototype = {
     this.performAction_('scrollRight', {}, opt_callback);
   },
 
+  scrollToPoint: function(x, y) {
+    this.performAction_('scrollToPoint', {x, y});
+  },
+
+  setScrollOffset: function(x, y) {
+    this.performAction_('setScrollOffset', {x, y});
+  },
+
   setAccessibilityFocus: function() {
     SetAccessibilityFocus(this.treeID, this.id);
   },
@@ -1797,8 +1805,11 @@ function AutomationNode() {
 }
 utils.expose(AutomationNode, AutomationNodeImpl, {
   functions: [
+    'addEventListener',
+    'boundsForRange',
     'createPosition',
     'doDefault',
+    'domQuerySelector',
     'find',
     'findAll',
     'focus',
@@ -1806,19 +1817,23 @@ utils.expose(AutomationNode, AutomationNodeImpl, {
     'getNextTextMatch',
     'hitTest',
     'hitTestWithReply',
+    'languageAnnotationForStringAttribute',
     'makeVisible',
     'matches',
     'performCustomAction',
     'performStandardAction',
+    'removeEventListener',
     'replaceSelectedText',
     'resumeMedia',
     'scrollBackward',
-    'scrollForward',
-    'scrollUp',
     'scrollDown',
+    'scrollForward',
     'scrollLeft',
     'scrollRight',
+    'scrollToPoint',
+    'scrollUp',
     'setAccessibilityFocus',
+    'setScrollOffset',
     'setSelection',
     'setSequentialFocusNavigationStartingPoint',
     'setValue',
@@ -1826,12 +1841,7 @@ utils.expose(AutomationNode, AutomationNodeImpl, {
     'startDuckingMedia',
     'stopDuckingMedia',
     'suspendMedia',
-    'addEventListener',
-    'removeEventListener',
-    'domQuerySelector',
     'toString',
-    'boundsForRange',
-    'languageAnnotationForStringAttribute',
   ],
   readonly: $Array.concat(
       publicAttributes,
