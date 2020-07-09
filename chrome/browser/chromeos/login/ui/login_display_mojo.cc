63d85a43b48afdf7ef292784b7deb025e9ffbd11
Roman Sorokin <rsorokin@chromium.org>
2020-07-09 08:25:59 +0000

    cros: Do not show webui error if dialog is closed
    During login error Chrome tries to show error on the webui side. It
    might happen that the webui dialog is closed. I think it worked before
    because we reloaded Gaia or cleared error state on show
    
    Fixed: 1100274
    Change-Id: Idb0af184f1cdd1eb24121709284fac128211ce3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2278248
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Auto-Submit: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Denis Kuznetsov [CET] <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786664}

--- chrome/browser/chromeos/login/ui/login_display_mojo.cc
+++ chrome/browser/chromeos/login/ui/login_display_mojo.cc
@@ -155,6 +155,9 @@ void LoginDisplayMojo::ShowError(int error_msg_id,
   if (!webui_handler_)
     return;
 
+  if (!host_->IsOobeUIDialogVisible())
+    return;
+
   std::string error_text;
   switch (error_msg_id) {
     case IDS_LOGIN_ERROR_CAPTIVE_PORTAL:
