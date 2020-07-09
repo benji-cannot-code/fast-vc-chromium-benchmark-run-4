5f0849c3b0e7398d78b1d611083a6ff94078f686
Rayan Kanso <rayankans@google.com>
2020-07-09 18:15:54 +0000

    Add a new credentials mode to allow uncredentialed requests.
    The CL also adds a DO_NOT_SEND_CERTS load flag and a new privacy
    mode key. This allows sending a new request without client certificates.
    A new privacy mode is required so the socket pool can work as expected.
    
    Bug: 775438
    Change-Id: I0c706c78bf574f00c22024408e9376051d5796d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252798
    Commit-Queue: Rayan Kanso <rayankans@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786832}

--- net/url_request/url_request_netlog_params.cc
+++ net/url_request/url_request_netlog_params.cc
@@ -17,6 +17,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+namespace {
+
+std::string PrivacyModeDebugString(PrivacyMode privacy_mode) {
+  switch (privacy_mode) {
+    case PRIVACY_MODE_DISABLED:
+      return "disabled";
+    case PRIVACY_MODE_ENABLED:
+      return "enabled";
+    case PRIVACY_MODE_ENABLED_WITHOUT_CLIENT_CERTS:
+      return "enabled without client certs";
+  }
+  return "";
+}
+
+}  // namespace
+
 base::Value NetLogURLRequestConstructorParams(
     const GURL& url,
     RequestPriority priority,
@@ -41,7 +57,7 @@ base::Value NetLogURLRequestStartParams(
   dict.SetStringKey("url", url.possibly_invalid_spec());
   dict.SetStringKey("method", method);
   dict.SetIntKey("load_flags", load_flags);
-  dict.SetIntKey("privacy_mode", privacy_mode == PRIVACY_MODE_ENABLED);
+  dict.SetStringKey("privacy_mode", PrivacyModeDebugString(privacy_mode));
   dict.SetStringKey("network_isolation_key",
                     network_isolation_key.ToDebugString());
   dict.SetStringKey("site_for_cookies", site_for_cookies.ToDebugString());
