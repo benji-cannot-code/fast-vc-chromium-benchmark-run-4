a11bc1d47256e73c40e4498042eb1d6317a7763e
Mohamed Amir Yosef <mamir@chromium.org>
2020-07-09 08:45:12 +0000

    [Passwords] Add favicon to manage passwords bubble
    Screenshots:
    Before butter: https://screenshot.googleplex.com/vekhOFHgVSE
    After butter: https://screenshot.googleplex.com/wWscc5jTrfG
    
    Bug: 1099704
    Change-Id: Ic3dc149f88aee113667284320cc8595d13e02b26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2282801
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786666}

--- chrome/browser/ui/passwords/bubble_controllers/items_bubble_controller.h
+++ chrome/browser/ui/passwords/bubble_controllers/items_bubble_controller.h
@@ -9,9 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/passwords/bubble_controllers/password_bubble_controller_base.h"
 
 #include "base/memory/weak_ptr.h"
+#include "base/task/cancelable_task_tracker.h"
 #include "components/password_manager/core/browser/manage_passwords_referrer.h"
+#include "ui/gfx/image/image.h"
 
 class PasswordsModelDelegate;
+
+namespace favicon_base {
+struct FaviconImageResult;
+}
 
 // This controller provides data and actions for the PasswordItemsView.
 class ItemsBubbleController : public PasswordBubbleControllerBase {
@@ -28,17 +34,33 @@ class ItemsBubbleController : public PasswordBubbleControllerBase {
   void OnPasswordAction(const autofill::PasswordForm& password_form,
                         PasswordAction action);
 
+  // Makes a request to the favicon service for the icon of current visible URL.
+  // The request to the favicon store is canceled on destruction of the
+  // controller.
+  void RequestFavicon(
+      base::OnceCallback<void(const gfx::Image&)> favicon_ready_callback);
+
   // Returns the available credentials which match the current site.
   const std::vector<autofill::PasswordForm>& local_credentials() const {
     return local_credentials_;
   }
 
  private:
+  // Called when the favicon was retrieved. It invokes |favicon_ready_callback|
+  // passing the retrieved favicon.
+  void OnFaviconReady(
+      base::OnceCallback<void(const gfx::Image&)> favicon_ready_callback,
+      const favicon_base::FaviconImageResult& result);
+
   // PasswordBubbleControllerBase methods:
   base::string16 GetTitle() const override;
   void ReportInteractions() override;
 
   const std::vector<autofill::PasswordForm> local_credentials_;
+
+  // Used to track a requested favicon.
+  base::CancelableTaskTracker favicon_tracker_;
+
   const base::string16 title_;
   // Dismissal reason for a password bubble.
   password_manager::metrics_util::UIDismissalReason dismissal_reason_ =
