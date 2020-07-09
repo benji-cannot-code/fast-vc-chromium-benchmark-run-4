d40d1a7d26627c8451ec289ab62fa507031d15ae
Minggang Wang <minggang.wang@intel.com>
2020-07-09 14:28:44 +0000

    Refactor out AlwaysAccessNetwork
    This CL refactors out AlwaysAccessNetwork function and puts it to Blink's
    public API.
    
    Bug: 860403
    Change-Id: I42a600fd822ef8488b78ac0b16860da40896ba9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2281544
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Minggang Wang <minggang.wang@intel.com>
    Cr-Commit-Position: refs/heads/master@{#786735}

---
+++ third_party/blink/common/loader/network_utils.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/public/common/loader/network_utils.h"
+
+namespace blink {
+
+bool AlwaysAccessNetwork(
+    const scoped_refptr<net::HttpResponseHeaders>& headers) {
+  if (!headers)
+    return false;
+
+  // RFC 2616, section 14.9.
+  return headers->HasHeaderValue("cache-control", "no-cache") ||
+         headers->HasHeaderValue("cache-control", "no-store") ||
+         headers->HasHeaderValue("pragma", "no-cache") ||
+         headers->HasHeaderValue("vary", "*");
+}
+
+}  // namespace blink
