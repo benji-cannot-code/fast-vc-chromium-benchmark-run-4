fa458aab366775311a6e1134047e63409569c196
Mustafa Emre Acer <meacer@chromium.org>
2020-07-09 03:47:55 +0000

    Cleanup IDN spoof checker comments
    Change-Id: Ib40248adae14cbdcdbf389f5689308110261b555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2289373
    Auto-Submit: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786619}

--- components/url_formatter/spoof_checks/skeleton_generator.cc
+++ components/url_formatter/spoof_checks/skeleton_generator.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/url_formatter/spoof_checks/skeleton_generator.h"
 
-#include <ostream>
-
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_piece.h"
 #include "third_party/icu/source/i18n/unicode/regex.h"
