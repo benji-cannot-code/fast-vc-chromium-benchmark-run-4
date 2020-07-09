f3cbe5a015befc1dac6117ea19468d6eb17c90b9
Kyle Williams <kdgwill@google.com>
2020-07-09 15:20:42 +0000

    cfm: Create Cfm Service Context DBUS client
    Create the CfmClient to call into cfm service context daemon exposed
    through DBus API, allowing service bootstrapping in a Chrome style
    callable interface.
    
    Note: We intentionally avoid initializing CfMClient pending complete
    security review
    
    BUG=b:139411674, b:144483733
    TEST='autoninja -C out/Default chromeos:chromeos_unittests \
      && ./out/Default/chromeos_unittests --dbus-stub \
      --single-process-tests --gtest_filter=CfmHotlineClientTest.*'
    
    Change-Id: I22d21146178edc957626b0e69ae6f9f89721311d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2277673
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786751}

---
+++ chromeos/dbus/cfm/fake_cfm_hotline_client.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_DBUS_CFM_FAKE_CFM_HOTLINE_CLIENT_H_
+#define CHROMEOS_DBUS_CFM_FAKE_CFM_HOTLINE_CLIENT_H_
+
+#include "base/callback_forward.h"
+#include "base/files/scoped_file.h"
+#include "base/macros.h"
+
+#include "chromeos/dbus/cfm/cfm_hotline_client.h"
+
+namespace chromeos {
+
+// Fake implementation of CfmHotlineClient. This is currently a no-op fake.
+class FakeCfmHotlineClient : public CfmHotlineClient {
+ public:
+  FakeCfmHotlineClient();
+  FakeCfmHotlineClient(const FakeCfmHotlineClient&) = delete;
+  FakeCfmHotlineClient& operator=(const FakeCfmHotlineClient&) = delete;
+  ~FakeCfmHotlineClient() override;
+
+  // CfmHotlineClient:
+  void WaitForServiceToBeAvailable(
+      dbus::ObjectProxy::WaitForServiceToBeAvailableCallback callback) override;
+  void BootstrapMojoConnection(
+      base::ScopedFD fd,
+      BootstrapMojoConnectionCallback result_callback) override;
+};
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_DBUS_CFM_FAKE_CFM_HOTLINE_CLIENT_H_
