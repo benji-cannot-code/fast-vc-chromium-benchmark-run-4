cf7e86927314d59e2c8fe3a477a580c2abb96400
Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
2020-07-09 11:53:44 +0000

    Add structure to discovery page
    Bug: 1103190
    Change-Id: I880bf121b337e3ca5fc43815025506e0c06c4211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2287478
    Reviewed-by: Richard Knoll <knollr@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786702}

--- chrome/browser/resources/nearby_share/nearby_discovery_page.js
+++ chrome/browser/resources/nearby_share/nearby_discovery_page.js
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * the Nearby Share flow. It shows a list of devices to select from.
  */
 
+import 'chrome://resources/cr_elements/cr_button/cr_button.m.js';
+import './nearby_preview.js';
+
 import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 Polymer({
