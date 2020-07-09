07f34232dc3907e26ad5734b29cbfc4f9ac8db7c
Maggie Cai <mxcai@chromium.org>
2020-07-09 05:35:56 +0000

    [IntentHandling] Launch Arc app with shared files.
    This CL adds the implementation to launch arc app with share files. We
    need to use the arc file system bridge because it contains all file
    handling permissions. In the future, we can clean up the interface a
    little bit to take intent instead of open url request.
    
    BUG=1092784
    
    Test: Manually test files with Gmail and Maps.
    Change-Id: Iba6bb1fbd5dfb1fac353320e9190d15f48d2ca0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2286433
    Commit-Queue: Maggie Cai <mxcai@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#786639}

---
+++ chrome/browser/apps/app_service/file_utils.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_APPS_APP_SERVICE_FILE_UTILS_H_
+#define CHROME_BROWSER_APPS_APP_SERVICE_FILE_UTILS_H_
+
+#include <vector>
+
+class Profile;
+class GURL;
+
+namespace storage {
+class FileSystemURL;
+}
+
+namespace apps {
+std::vector<storage::FileSystemURL> GetFileSystemURL(
+    Profile* profile,
+    const std::vector<GURL>& file_urls);
+}  // namespace apps
+
+#endif  // CHROME_BROWSER_APPS_APP_SERVICE_FILE_UTILS_H_
