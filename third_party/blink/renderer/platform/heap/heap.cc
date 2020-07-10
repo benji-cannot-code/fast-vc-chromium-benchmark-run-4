c637c3b4f735665b748078dfc73ded98befa86fa
Omer Katz <omerkatz@chromium.org>
2020-07-10 10:29:06 +0000

    heap: Enable scopes for diagnostics
    Bug: 986235
    Change-Id: I3e359b493c0e3ad1e21abb96847ff807c715d00e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292070
    Commit-Queue: Omer Katz <omerkatz@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Omer Katz <omerkatz@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787147}

--- third_party/blink/renderer/platform/heap/heap.cc
+++ third_party/blink/renderer/platform/heap/heap.cc
@@ -274,7 +274,7 @@ void ThreadHeap::FlushEphemeronPairs(EphemeronProcessing ephemeron_processing) {
       return;
   }
 
-  ThreadHeapStatsCollector::Scope stats_scope(
+  ThreadHeapStatsCollector::EnabledScope stats_scope(
       stats_collector(), ThreadHeapStatsCollector::kMarkFlushEphemeronPairs);
 
   EphemeronPairsWorklist::View view(discovered_ephemeron_pairs_worklist_.get(),
@@ -385,7 +385,7 @@ bool ThreadHeap::AdvanceMarking(MarkingVisitor* visitor,
     {
       // Iteratively mark all objects that are reachable from the objects
       // currently pushed onto the marking worklist.
-      ThreadHeapStatsCollector::Scope stats_scope(
+      ThreadHeapStatsCollector::EnabledScope stats_scope(
           stats_collector(), ThreadHeapStatsCollector::kMarkProcessWorklists);
 
       // Start with mutator-thread-only worklists (not fully constructed).
@@ -410,7 +410,7 @@ bool ThreadHeap::AdvanceMarking(MarkingVisitor* visitor,
       }
 
       {
-        ThreadHeapStatsCollector::Scope inner_scope(
+        ThreadHeapStatsCollector::EnabledScope inner_scope(
             stats_collector(),
             ThreadHeapStatsCollector::kMarkFlushV8References);
         finished = FlushV8References(deadline);
@@ -419,7 +419,7 @@ bool ThreadHeap::AdvanceMarking(MarkingVisitor* visitor,
       }
 
       {
-        ThreadHeapStatsCollector::Scope inner_scope(
+        ThreadHeapStatsCollector::EnabledScope inner_scope(
             stats_collector(),
             ThreadHeapStatsCollector::kMarkProcessNotFullyconstructeddWorklist);
         // Convert |previously_not_fully_constructed_worklist_| to
@@ -437,7 +437,7 @@ bool ThreadHeap::AdvanceMarking(MarkingVisitor* visitor,
       }
 
       {
-        ThreadHeapStatsCollector::Scope inner_scope(
+        ThreadHeapStatsCollector::EnabledScope inner_scope(
             stats_collector(),
             ThreadHeapStatsCollector::kMarkProcessMarkingWorklist);
         finished = DrainWorklistWithDeadline(
@@ -455,7 +455,7 @@ bool ThreadHeap::AdvanceMarking(MarkingVisitor* visitor,
       }
 
       {
-        ThreadHeapStatsCollector::Scope inner_scope(
+        ThreadHeapStatsCollector::EnabledScope inner_scope(
             stats_collector(),
             ThreadHeapStatsCollector::kMarkProcessWriteBarrierWorklist);
         finished = DrainWorklistWithDeadline(
