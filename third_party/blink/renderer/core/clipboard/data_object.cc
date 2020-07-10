d9086a1b1c65aa81568ee0dc5036b7b6e5ac6d27
Henrique Ferreiro <hferreiro@igalia.com>
2020-07-10 14:23:35 +0000

    Remove key modifiers from DropData and WebDragData
    While key modifiers are used and stored in DataObject, they were only
    present in WebDragData and DropData to support BrowserPlugin code that
    was removed in https://crrev.com/c/2287300.
    
    After this change, the modifiers are only passed as a parameter in drag
    and drop methods when needed. Their type was changed to uint32_t in some
    methods to unify it with the methods already migrated to Mojo.
    
    Bug: 1039255
    Change-Id: I7fadfa371834ab862d4f4587ee6b77e50de5e36c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2289425
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#787196}

--- third_party/blink/renderer/core/clipboard/data_object.cc
+++ third_party/blink/renderer/core/clipboard/data_object.cc
@@ -334,7 +334,6 @@ DataObject* DataObject::Create(WebDragData data) {
 
 WebDragData DataObject::ToWebDragData() {
   WebDragData data;
-  data.SetModifierKeyState(modifiers_);
   WebVector<WebDragData::Item> item_list(length());
 
   for (wtf_size_t i = 0; i < length(); ++i) {
