ac3ec3e6a7784a27f3eaf026109bba9f68eeee0c
Reilly Grant <reillyg@chromium.org>
2020-07-10 02:31:44 +0000

    bluetooth: Convert callbacks in BluetoothDiscoverySession
    This change converts the remaining callbacks in
    BluetoothDiscoverySession to base::OnceCallback. There should be no
    functional change.
    
    Bug: 834319
    Change-Id: Iaf306d1fede40b8c3aad030a6499eb7d9521d367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290993
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Ovidio de Jesús Ruiz-Henríquez <odejesush@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787064}

--- chromeos/services/secure_channel/ble_synchronizer_base.h
+++ chromeos/services/secure_channel/ble_synchronizer_base.h
@@ -44,8 +44,8 @@ class BleSynchronizerBase {
       device::BluetoothAdapter::ErrorCallback error_callback);
   void StopDiscoverySession(
       base::WeakPtr<device::BluetoothDiscoverySession> discovery_session,
-      const base::Closure& callback,
-      const device::BluetoothDiscoverySession::ErrorCallback& error_callback);
+      base::OnceClosure callback,
+      device::BluetoothDiscoverySession::ErrorCallback error_callback);
 
  protected:
   enum class CommandType {
@@ -93,12 +93,12 @@ class BleSynchronizerBase {
   struct StopDiscoveryArgs {
     StopDiscoveryArgs(
         base::WeakPtr<device::BluetoothDiscoverySession> discovery_session,
-        const base::Closure& callback,
-        const device::BluetoothDiscoverySession::ErrorCallback& error_callback);
+        base::OnceClosure callback,
+        device::BluetoothDiscoverySession::ErrorCallback error_callback);
     virtual ~StopDiscoveryArgs();
 
     base::WeakPtr<device::BluetoothDiscoverySession> discovery_session;
-    base::Closure callback;
+    base::OnceClosure callback;
     device::BluetoothDiscoverySession::ErrorCallback error_callback;
   };
 
