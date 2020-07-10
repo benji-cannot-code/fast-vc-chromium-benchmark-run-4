43b9479183480221e2fc300165a5b8f0a34e7837
Xi Han <hanxi@google.com>
2020-07-10 22:26:44 +0000

    [Instant Start] Use OnCreateTimestampMs() for startup metrics recording.
    Bug: 1092931, 1104351
    Change-Id: Id403d7836e40870357c4adee51e4796291ca04e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292127
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787408}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -1727,7 +1727,7 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
             DefaultBrowserPromoUtils.maybeRecordOutcomeOnStart();
 
             if (mStartSurface != null && mOverviewShownOnStart) {
-                mStartSurface.onOverviewShownAtLaunch(getOnCreateTimestampUptimeMs());
+                mStartSurface.onOverviewShownAtLaunch(getOnCreateTimestampMs());
             }
         });
     }
