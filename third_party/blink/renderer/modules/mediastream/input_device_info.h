6242cb7cb326782cadf12ecf15ce8f40aef93c6d
Antonio Gomes <tonikitoo@igalia.com>
2020-07-10 02:53:27 +0000

    Move Capabilities from WebMediaStreamSource to MediaStreamSource
    This is now possible after switching most of Blink to operate
    over MediaStreamSource.
    
    CL also moves the struct definition from using WebVector/WebString
    to the corresponding WTF types.
    
    BUG=704136, 919392
    
    R=guidou@chromium.org, haraken@chromium.org
    
    Change-Id: I2eddffdafd5a9cc3d3ea8688b270fa3b33b8c893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290272
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787069}

--- third_party/blink/renderer/modules/mediastream/input_device_info.h
+++ third_party/blink/renderer/modules/mediastream/input_device_info.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_INPUT_DEVICE_INFO_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_INPUT_DEVICE_INFO_H_
 
-#include "third_party/blink/public/platform/modules/mediastream/web_media_stream_source.h"
 #include "third_party/blink/renderer/modules/mediastream/media_device_info.h"
+#include "third_party/blink/renderer/platform/mediastream/media_stream_source.h"
 
 namespace blink {
 
@@ -28,7 +28,7 @@ class InputDeviceInfo final : public MediaDeviceInfo {
   MediaTrackCapabilities* getCapabilities() const;
 
  private:
-  WebMediaStreamSource::Capabilities platform_capabilities_;
+  MediaStreamSource::Capabilities platform_capabilities_;
 };
 
 }  // namespace blink
--- third_party/blink/renderer/modules/mediastream/media_stream_constraints_util.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_constraints_util.h
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/video_facing.h"
 #include "media/capture/video_capture_types.h"
-#include "third_party/blink/public/platform/modules/mediastream/web_media_stream_source.h"
 #include "third_party/blink/renderer/modules/mediastream/media_stream_constraints_util_sets.h"
 #include "third_party/blink/renderer/modules/mediastream/video_track_adapter_settings.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/mediastream/media_constraints.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_audio_processor_options.h"
+#include "third_party/blink/renderer/platform/mediastream/media_stream_source.h"
 
 namespace blink {
 
@@ -335,7 +335,7 @@ double StringConstraintFitnessDistance(
 
 // This method computes capabilities for a video source based on the given
 // |formats|. |facing_mode| is valid only in case of video device capture.
-MODULES_EXPORT blink::WebMediaStreamSource::Capabilities
+MODULES_EXPORT MediaStreamSource::Capabilities
 ComputeCapabilitiesForVideoSource(
     const blink::WebString& device_id,
     const media::VideoCaptureFormats& formats,
