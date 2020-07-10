d697f562171ae8f49f533193ea356636adba784a
Yuki Shiino <yukishiino@chromium.org>
2020-07-10 10:10:05 +0000

    bind-gen: Fix the return type of NativeFileSystemHandle::kind
    The new bindings generator won't accept |char*| as IDL DOMString.
    Only String, AtomicString, or WebString should be used.
    
    Bug: 839389
    Change-Id: I06a02f0a8c176b0fcc400637da11fcf7c88ee57e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291563
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787143}

--- third_party/blink/renderer/modules/native_file_system/native_file_system_handle.h
+++ third_party/blink/renderer/modules/native_file_system/native_file_system_handle.h
@@ -36,7 +36,7 @@ class NativeFileSystemHandle : public ScriptWrappable,
 
   virtual bool isFile() const { return false; }
   virtual bool isDirectory() const { return false; }
-  const char* kind() const { return isFile() ? "file" : "directory"; }
+  const String kind() const { return isFile() ? "file" : "directory"; }
   const String& name() const { return name_; }
 
   ScriptPromise queryPermission(ScriptState*,
