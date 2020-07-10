4a1c0a46095da5d3129926b3eba13dd26fda07c6
ckitagawa <ckitagawa@chromium.org>
2020-07-10 18:26:55 +0000

    [Paint Preview] Scroll Controller
    This CL moves scrolling logic from PlayerFrameMediator to
    PlayerFrameScrollController. This is mostly a mechanical change just
    moving code and reordering methods.
    
    Of note is that this CL also
    - Replaces moveViewport() with updateVisuals() as moving the viewport
      is now handled outside of the mediator.
    - Creates a MediatorDelegate so that mediator helper classes can call
      the mediator through an limited API.
    
    Bug: 1099722
    Change-Id: I7ab14c7a4c1b18b750756d54438be25f72507da8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2288280
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Reviewed-by: Mehran Mahmoudi <mahmoudi@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787298}

---
+++ components/paint_preview/player/android/java/src/org/chromium/components/paintpreview/player/frame/PlayerFrameMediatorDelegate.java
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.paintpreview.player.frame;
+
+/**
+ * API of the PlayerFrameMediator to helper classes.
+ */
+public interface PlayerFrameMediatorDelegate {
+    /**
+     * Triggers an update of the visual contents of the PlayerFrameView. This fetches updates the
+     * model and fetches any new bitmaps asynchronously.
+     * @param scaleChanged Indicates that the scale changed so all current bitmaps need to be
+     *     discarded.
+     */
+    void updateVisuals(boolean scaleChanged);
+}
