cdf97a9ffd8164415b1df255590f8b44eb4092b0
zhaoyangli <zhaoyangli@chromium.org>
2020-07-10 20:49:54 +0000

    [iOS][test runner] Add 'is_unexpected' key to iOS test results.
    Add this key with value `True` to any failed tests, so that LUCI UI can
    display failed test names in each swarming task for iOS builders.
    - Added the field when finalizing result in standard_json_util module.
    - Added and fixed broken tests.
    
    Bug: 1102920
    Change-Id: Iaad9adabd704d289888fa0f2afee66298dd53900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2289066
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787354}

--- ios/build/bots/scripts/standard_json_util.py
+++ ios/build/bots/scripts/standard_json_util.py
@@ -35,6 +35,10 @@ class StdJson():
     If flaky=True, or if 'FAIL' already set in 'actual',
     apply is_flaky=True for all test(s).
     """
+    if not test:
+      LOGGER.warn('Empty or None test name passed to standard_json_util')
+      return
+
     if test in self.tests:
       self.tests[test]['actual'] = self.tests[test]['actual'] + " PASS"
     else:
@@ -42,6 +46,8 @@ class StdJson():
 
     if flaky or 'FAIL' in self.tests[test]['actual']:
       self.tests[test]['is_flaky'] = True
+
+    self.tests[test].pop('is_unexpected', None)
 
   def mark_all_passed(self, tests, flaky=False):
     """Mark all tests as PASS"""
@@ -54,10 +60,19 @@ class StdJson():
     Params:
       test (str): a test in format "{TestCase}/{testMethod}"
     """
+    if not test:
+      LOGGER.warn('Empty or None test name passed to standard_json_util')
+      return
+
     if test in self.tests:
       self.tests[test]['actual'] = self.tests[test]['actual'] + " FAIL"
+      self.tests[test]['is_unexpected'] = True
     else:
-      self.tests[test] = {'expected': 'PASS', 'actual': 'FAIL'}
+      self.tests[test] = {
+          'expected': 'PASS',
+          'actual': 'FAIL',
+          'is_unexpected': True
+      }
 
   def mark_all_failed(self, tests):
     """Mark all tests as FAIL"""
@@ -70,6 +85,10 @@ class StdJson():
     Params:
       test (str): a test in format "{TestCase}/{testMethod}"
     """
+    if not test:
+      LOGGER.warn('Empty or None test name passed to standard_json_util')
+      return
+
     self.tests[test] = {'expected': 'SKIP', 'actual': 'SKIP'}
 
   def mark_all_skipped(self, tests):
@@ -82,8 +101,16 @@ class StdJson():
     Params:
       test (str): a test in format "{TestCase}/{testMethod}"
     """
+    if not test:
+      LOGGER.warn('Empty or None test name passed to standard_json_util')
+      return
 
     if test in self.tests:
       self.tests[test]['actual'] = self.tests[test]['actual'] + " TIMEOUT"
+      self.tests[test]['is_unexpected'] = True
     else:
-      self.tests[test] = {'expected': 'PASS', 'actual': 'TIMEOUT'}
+      self.tests[test] = {
+          'expected': 'PASS',
+          'actual': 'TIMEOUT',
+          'is_unexpected': True
+      }
