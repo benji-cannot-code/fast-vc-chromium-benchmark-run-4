d782cfbe9e8a33769e7a55c409a1caf1726d6449
Joe Mason <joenotcharles@google.com>
2020-07-10 05:37:26 +0000

    [PM] Move V8PerFrameMemoryDecorator to v8_memory/
    R=chrisha
    
    Bug: 1080672
    Change-Id: I52041306ec452d07cdc40d09e7e05b38d81b27f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283402
    Commit-Queue: Joe Mason <joenotcharles@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787093}

--- components/performance_manager/v8_memory/v8_per_frame_memory_decorator_unittest.cc
+++ components/performance_manager/v8_memory/v8_per_frame_memory_decorator_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/performance_manager/public/decorators/v8_per_frame_memory_decorator.h"
+#include "components/performance_manager/public/v8_memory/v8_per_frame_memory_decorator.h"
 
 #include <memory>
 #include <utility>
