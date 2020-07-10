03213f4953fb2ca901764527653bd83251ff7376
Kent Tamura <tkent@chromium.org>
2020-07-10 05:48:33 +0000

    Move three data members of blink::Document to blink::DocumentData
    This CL reduces the estimated pre-processed size of document.h by
    2.38MB.
    
    This CL has no user-visible behavior changes.
    
    Bug: 242216, 860402
    Change-Id: I1d5b11fcb9b4b6a9f1c7640f23ff878ec4612bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290478
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787096}

--- third_party/blink/renderer/core/html/html_dialog_element.cc
+++ third_party/blink/renderer/core/html/html_dialog_element.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/accessibility/ax_object_cache.h"
 #include "third_party/blink/renderer/core/dom/events/event.h"
 #include "third_party/blink/renderer/core/dom/flat_tree_traversal.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
--- third_party/blink/renderer/modules/delegated_ink/ink.cc
+++ third_party/blink/renderer/modules/delegated_ink/ink.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/delegated_ink/ink.h"
 
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
