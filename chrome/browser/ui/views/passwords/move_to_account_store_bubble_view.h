a5914e44329d4d92254638fd1d824fc479e143bc
Mohamed Amir Yosef <mamir@chromium.org>
2020-07-10 20:32:39 +0000

    [Password] Add moving banner to move passwords bubble
    This CL brings the moving bubble very close the mocks in the linked bug
    
    Screenshots:
    Favicon available: https://screenshot.googleplex.com/g1M17ZqWTbn
    Fallback: https://screenshot.googleplex.com/s5TMH5cZ3at
    
    Bug: 1100814
    Change-Id: Icaa1e3998cc6e1c21edd79b9acd84751f45b0d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2283213
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787351}

--- chrome/browser/ui/views/passwords/move_to_account_store_bubble_view.h
+++ chrome/browser/ui/views/passwords/move_to_account_store_bubble_view.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Bubble asking the user to move a profile credential to their account store.
 class MoveToAccountStoreBubbleView : public PasswordBubbleViewBase {
+  class MovingBannerView;
+
  public:
   explicit MoveToAccountStoreBubbleView(content::WebContents* web_contents,
                                         views::View* anchor_view);
@@ -25,6 +27,14 @@ class MoveToAccountStoreBubbleView : public PasswordBubbleViewBase {
   bool ShouldShowCloseButton() const override;
   MoveToAccountStoreBubbleController* GetController() override;
   const MoveToAccountStoreBubbleController* GetController() const override;
+
+  // Called when the favicon is loaded. If |favicon| isn't empty, it updates
+  // |favicon| in |moving_banner_|
+  void OnFaviconReady(const gfx::Image& favicon);
+
+  // Holds a pointer to the banner illustarting that a password is being moved
+  // from the device to the account.
+  MovingBannerView* moving_banner_;
 
   MoveToAccountStoreBubbleController controller_;
 };
