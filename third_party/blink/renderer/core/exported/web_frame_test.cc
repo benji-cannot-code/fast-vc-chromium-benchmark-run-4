9d421f31eb24c4aa4f4220ee8c26f98e5197f46e
Justin Miron <justinmiron@google.com>
2020-07-10 21:27:52 +0000

    Replace propagating a viewport offset with calculation from transform.
    This change:
    1. Calculates the existing viewport_offset field in local
    frame's from the main frame transform instead of a calculating it in
    a remote frame and propagating it to the remote frame. The field is
    removed from the propagated viewport intersection state.
    2. Updates tests to propagate a main frame transform equivalent to
    propagating a viewport offset.
    
    Change-Id: I05e299d4c91125d4f3c5ac0f7e330d8c3a99530f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2246554
    Commit-Queue: Justin Miron <justinmiron@google.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787375}

--- third_party/blink/renderer/core/exported/web_frame_test.cc
+++ third_party/blink/renderer/core/exported/web_frame_test.cc
@@ -195,6 +195,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/mojom/text_input_state.mojom-blink.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/events/keycodes/dom/dom_key.h"
+#include "ui/gfx/transform.h"
 #include "v8/include/v8.h"
 
 using blink::url_test_helpers::ToKURL;
@@ -13361,14 +13362,15 @@ TEST_F(WebFrameTest, RemoteViewportAndMainframeIntersections) {
   // element.
   WebFrameWidget* widget = local_frame->FrameWidget();
   ASSERT_TRUE(widget);
-  gfx::Point viewport_offset(7, -11);
+  gfx::Transform viewport_transform;
+  viewport_transform.Translate(7, -11);
   WebRect viewport_intersection(0, 11, 200, 89);
   WebRect mainframe_intersection(0, 0, 200, 140);
   FrameOcclusionState occlusion_state = FrameOcclusionState::kUnknown;
 
   widget->SetRemoteViewportIntersection(
-      {viewport_offset, viewport_intersection, mainframe_intersection,
-       viewport_intersection, occlusion_state});
+      {viewport_intersection, mainframe_intersection, viewport_intersection,
+       occlusion_state, WebSize(), gfx::Point(), viewport_transform});
 
   // The viewport intersection should be applied by the layout geometry mapping
   // code when these flags are used.
