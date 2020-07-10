d554ae19d01b4de59b080dae191abe6c0d8eaa87
Quang Minh Tuan Nguyen <nqmtuan@google.com>
2020-07-10 16:33:59 +0000

    Convert base::Bind in cronet/android/test
    Bug: 1007680
    Change-Id: If95e5f682e6d0ee1c7ad379d230a3859d826e0a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291553
    Commit-Queue: Adam Langley <agl@chromium.org>
    Auto-Submit: Quang Minh Tuan Nguyen <nqmtuan@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787236}

--- components/cronet/android/test/test_upload_data_stream_handler.cc
+++ components/cronet/android/test/test_upload_data_stream_handler.cc
@@ -115,8 +115,8 @@ void TestUploadDataStreamHandler::InitOnNetworkThread() {
   read_buffer_ = nullptr;
   bytes_read_ = 0;
   int res = upload_data_stream_->Init(
-      base::Bind(&TestUploadDataStreamHandler::OnInitCompleted,
-                 base::Unretained(this)),
+      base::BindOnce(&TestUploadDataStreamHandler::OnInitCompleted,
+                     base::Unretained(this)),
       net::NetLogWithSource());
   JNIEnv* env = base::android::AttachCurrentThread();
   cronet::Java_TestUploadDataStreamHandler_onInitCalled(
@@ -136,8 +136,8 @@ void TestUploadDataStreamHandler::ReadOnNetworkThread() {
 
   int bytes_read = upload_data_stream_->Read(
       read_buffer_.get(), kReadBufferSize,
-      base::Bind(&TestUploadDataStreamHandler::OnReadCompleted,
-                 base::Unretained(this)));
+      base::BindOnce(&TestUploadDataStreamHandler::OnReadCompleted,
+                     base::Unretained(this)));
   if (bytes_read == net::OK) {
     bytes_read_ = bytes_read;
     NotifyJavaReadCompleted();
