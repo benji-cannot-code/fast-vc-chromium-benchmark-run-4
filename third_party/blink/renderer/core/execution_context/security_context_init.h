cba95ffa785a1e84d0f0d767e5a02c366c6d21f2
Nate Chapin <japhet@chromium.org>
2020-07-10 17:41:23 +0000

    Don't send stuff unnecessarily through DocumentInit
    Change-Id: If88d0e6f86b688058b52123a281f6e6935165296
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2288755
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787281}

--- third_party/blink/renderer/core/execution_context/security_context_init.h
+++ third_party/blink/renderer/core/execution_context/security_context_init.h
@@ -8,7 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/mojom/web_sandbox_flags.mojom-blink.h"
 #include "third_party/blink/public/common/feature_policy/feature_policy.h"
+#include "third_party/blink/public/common/frame/frame_policy.h"
+#include "third_party/blink/public/mojom/feature_policy/feature_policy.mojom-blink.h"
 #include "third_party/blink/public/mojom/feature_policy/feature_policy_feature.mojom-blink.h"
+#include "third_party/blink/public/mojom/security_context/insecure_request_policy.mojom-blink-forward.h"
+#include "third_party/blink/public/web/web_origin_policy.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/feature_policy/policy_helper.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
@@ -23,6 +27,7 @@ class DocumentInit;
 class Frame;
 class LocalFrame;
 class OriginTrialContext;
+class ResourceResponse;
 class SecurityOrigin;
 
 class CORE_EXPORT SecurityContextInit : public FeaturePolicyParserDelegate {
@@ -31,6 +36,18 @@ class CORE_EXPORT SecurityContextInit : public FeaturePolicyParserDelegate {
  public:
   SecurityContextInit(scoped_refptr<SecurityOrigin>, OriginTrialContext*);
   explicit SecurityContextInit(const DocumentInit&);
+
+  void CalculateSecureContextMode(LocalFrame* frame);
+  void InitializeOriginTrials(const String& origin_trials_header);
+  void CalculateFeaturePolicy(
+      LocalFrame* frame,
+      bool is_view_source,
+      const ResourceResponse& response,
+      const base::Optional<WebOriginPolicy>& origin_policy,
+      const FramePolicy& frame_policy);
+  void CalculateDocumentPolicy(
+      const DocumentPolicy::ParsedDocumentPolicy& document_policy,
+      const String& report_only_document_policy_header);
 
   const scoped_refptr<SecurityOrigin>& GetSecurityOrigin() const {
     return security_origin_;
@@ -71,11 +88,6 @@ class CORE_EXPORT SecurityContextInit : public FeaturePolicyParserDelegate {
   bool FeatureEnabled(OriginTrialFeature feature) const override;
 
  private:
-  void InitializeDocumentPolicy(const DocumentInit&);
-  void InitializeFeaturePolicy(const DocumentInit&);
-  void InitializeSecureContextMode(const DocumentInit&);
-  void InitializeOriginTrials(const DocumentInit&);
-
   ExecutionContext* execution_context_ = nullptr;
   ContentSecurityPolicy* csp_ = nullptr;
   network::mojom::blink::WebSandboxFlags sandbox_flags_ =
