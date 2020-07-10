8338b584da1b81bebbe90310fa2080d072a24b07
Mei Liang <meiliang@chromium.org>
2020-07-10 23:43:32 +0000

    [Tablet] Fix toolbar swipe auto focus on omnibox
    crrev.com/c/1464118 affects the TabModelObserver ordering. It adds
    TabModelFilter as a TabModelObserver to the system, and TabModelFilter
    becomes the first set of Observers to be notified when TabModel changed.
    More specifically, the TabModelFilter receives notification before the
    TabModelObserver in TabModelSelectorBase and eventually causes the
    CompositorViewHolder#updateContentOverlayVisibility(true) with a stale
    |mView| and affecting the focus.
    
    This CL addresses this issue by:
      1. Attaches TabModelSelectorBase#TabModelObserver to TabModelFilter,
         So it becomes the first Observer to be notified as before.
      2. Verifies the |mView| in CompositorViewHolder is the current tab
         view when show is requested.
      3. Removes redundant logic in LayoutManagerChromeTablet. Don't need
         to call show StaticLayout explicitly, the LayoutManager will handle
         that automatically when the ToolbarSwipeLayout hides.
    
    Change-Id: I4e588194d8866b06dac1255532ec7094d5eabc1d
    Bug: 944777, 1073535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290772
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Yusuf Ozuysal <yusufo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787435}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorBase.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorBase.java
@@ -77,9 +77,8 @@ public abstract class TabModelSelectorBase implements TabModelSelector {
                 notifyChanged();
             }
         };
-        for (TabModel model : models) {
-            model.addObserver(tabModelObserver);
-        }
+
+        mTabModelFilterProvider.addTabModelFilterObserver(tabModelObserver);
 
         if (sObserver != null) {
             addObserver(sObserver);
--- chrome/android/junit/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorImplTest.java
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabmodel;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 import android.app.Activity;
 
@@ -60,6 +62,9 @@ public class TabModelSelectorImplTest {
                 .when(mMockTabPersistencePolicy)
                 .getStateFileName();
 
+        doReturn(mock(TabModelFilter.class))
+                .when(mMockTabModelFilterFactory)
+                .createTabModelFilter(any());
         mTabCreatorManager = new MockTabCreatorManager();
         mTabModelSelector = new TabModelSelectorImpl(mActivity, mTabCreatorManager,
                 mMockTabPersistencePolicy, mMockTabModelFilterFactory, mNextTabPolicySupplier,
