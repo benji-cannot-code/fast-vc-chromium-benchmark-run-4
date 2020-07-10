601ed62aca925cc04e32d2df9ddb983c062ebf58
Rune Lillesveen <futhark@chromium.org>
2020-07-10 15:54:23 +0000

    Use constructor document for IsConstructed().
    The spec[1] term is that the constructor document is set to the
    associated document for the global object. Rename the
    associated_document_ to constructor_document_ to make it clearer this is
    about constructed stylesheets. The constructor_document_ is always non-
    null for constructed stylesheets and always null for non-constructed
    stylesheets. Remove the boolean is_constructed_ which doesn't add
    anything extra.
    
    @import is not allowed in constructed stylesheets per spec, but if we
    did support it, there was a case where we would not return the
    constructor_document_ as OwnerDocument() for @imports. Fix that so that
    it won't be a problem if we introduce @import support.
    
    [1] https://wicg.github.io/construct-stylesheets/#constructing-stylesheets
    
    Change-Id: I1d0a8ba0a21f48c7ec5f70b2831794c8a26ea020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2282757
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787225}

--- third_party/blink/renderer/core/css/document_style_sheet_collection.cc
+++ third_party/blink/renderer/core/css/document_style_sheet_collection.cc
@@ -94,7 +94,7 @@ void DocumentStyleSheetCollection::CollectStyleSheetsFromCandidates(
         !sheet->CanBeActivated(
             GetDocument().GetStyleEngine().PreferredStylesheetSetName()))
       continue;
-    DCHECK_EQ(GetDocument(), sheet->AssociatedDocument());
+    DCHECK_EQ(GetDocument(), sheet->ConstructorDocument());
     collector.AppendSheetForList(sheet);
     collector.AppendActiveStyleSheet(
         std::make_pair(sheet, engine.RuleSetForSheet(*sheet)));
