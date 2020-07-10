6dae4b1f6c09b8e7158a46f1573d57963b7581b7
Olesia Marukhno <olesiamarukhno@google.com>
2020-07-10 17:21:23 +0000

    [omnibox] Update styling for omnibox button row, add icons
    Bug: 1104264
    
    Change-Id: I33ea8bc0ed3d30e8b92e88caefc609879b4e2b76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2260635
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787269}

--- chrome/browser/ui/views/omnibox/omnibox_suggestion_button_row_view.h
+++ chrome/browser/ui/views/omnibox/omnibox_suggestion_button_row_view.h
@@ -35,6 +35,7 @@ class OmniboxSuggestionButtonRowView : public views::View,
 
   // views::View:
   void Layout() override;
+  void OnThemeChanged() override;
 
  private:
   // Get the popup model from the view.
@@ -42,6 +43,9 @@ class OmniboxSuggestionButtonRowView : public views::View,
 
   // Digs into the model with index to get the match for owning result view.
   const AutocompleteMatch& match() const;
+
+  void SetPillButtonVisibility(views::MdTextButton* button,
+                               OmniboxPopupModel::LineState state);
 
   OmniboxPopupContentsView* const popup_contents_view_;
   size_t const model_index_;
