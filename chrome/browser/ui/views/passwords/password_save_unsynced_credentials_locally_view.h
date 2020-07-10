422f1266e9278df5e143401caff3678e349af7e6
Victor Hugo Vianna Silva <victorvianna@google.com>
2020-07-10 11:37:01 +0000

    [b4p/sign-out] Allow saving subset of pwds in bubble controller
    This CL modifies the signature of the saving method in the controller
    so that in future CLs the view can use it to save only a subset of the
    passwords, as chosen by the user.
    
    Bug: 1098672
    Change-Id: I5596cf89ad42c46d6c04bfa21f9ad8d2da17ae5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2289851
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787160}

--- chrome/browser/ui/views/passwords/password_save_unsynced_credentials_locally_view.h
+++ chrome/browser/ui/views/passwords/password_save_unsynced_credentials_locally_view.h
@@ -33,6 +33,8 @@ class PasswordSaveUnsyncedCredentialsLocallyView
 
   void CreateLayout();
 
+  void OnSaveClicked();
+
   // LocationBarBubbleDelegateView:
   bool ShouldShowCloseButton() const override;
   gfx::Size CalculatePreferredSize() const override;
