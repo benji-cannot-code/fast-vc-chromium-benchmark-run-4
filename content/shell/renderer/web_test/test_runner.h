a708043d4019bfa17de8ec0337222566afe9d18b
danakj <danakj@chromium.org>
2020-07-10 20:47:52 +0000

    Disconnect main_view_ from AddLoadingFrame and installing bindings
    This removes 2 dependencies on the |main_view_| which is a problematic
    pointer when the main frame can be swapped on navigation in the future.
    Instead we use the current frame pointer to determine what we want to
    do.
    
    R=avi@chromium.org
    
    Bug: 866140, 1069111
    Change-Id: I4d41d3c3afaedc3d57424b8a4592037dc5aa9c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291973
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787352}

--- content/shell/renderer/web_test/test_runner.h
+++ content/shell/renderer/web_test/test_runner.h
@@ -506,8 +506,6 @@ class TestRunner {
   ///////////////////////////////////////////////////////////////////////////
   // Internal helpers
 
-  bool IsFramePartOfMainTestWindow(blink::WebFrame*) const;
-
   void CheckResponseMimeType();
 
   mojo::AssociatedRemote<mojom::WebTestControlHost>&
