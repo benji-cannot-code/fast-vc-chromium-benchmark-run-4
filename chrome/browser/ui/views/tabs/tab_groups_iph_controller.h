f35e4d089d498d186fd798c7d6a4f2bb1cd1531a
Collin Baker <collinbaker@chromium.org>
2020-07-11 00:27:59 +0000

    Dismiss Tab Groups IPH when a tab context menu is opened
    Bug: 1102655
    Change-Id: I1a250eb518a00245fcf648c96895a8beceb3f22d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290696
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787454}

--- chrome/browser/ui/views/tabs/tab_groups_iph_controller.h
+++ chrome/browser/ui/views/tabs/tab_groups_iph_controller.h
@@ -41,6 +41,8 @@ class TabGroupsIPHController : public TabStripModelObserver,
   TabGroupsIPHController(Browser* browser, GetTabViewCallback get_tab_view);
   ~TabGroupsIPHController() override;
 
+  void TabContextMenuOpened();
+
   // TabStripModelObserver:
   void OnTabStripModelChanged(
       TabStripModel* tab_strip_model,
