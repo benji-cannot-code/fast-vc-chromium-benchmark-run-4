bb4c42fb6b34a2e362c73e7cb6bd19dd8105eb60
Gavin Williams <gavinwill@chromium.org>
2020-07-11 00:48:20 +0000

    Add a disabled state to destination dropdown
    When dropdown is in a disabled state, remove the tabindex from the
    dropdown to prevent it from being focusable.
    
    Bug: 1059607
    Change-Id: I5a91884996ca0404e6baa9f6d44ecd8565540533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291236
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787458}

--- chrome/browser/resources/print_preview/ui/destination_dropdown_cros.js
+++ chrome/browser/resources/print_preview/ui/destination_dropdown_cros.js
@@ -37,6 +37,8 @@ Polymer({
     disabled: {
       type: Boolean,
       value: false,
+      observer: 'updateTabIndex_',
+      reflectToAttribute: true,
     },
 
     driveDestinationKey: String,
@@ -60,6 +62,7 @@ Polymer({
   attached() {
     this.pointerDownListener_ = event => this.onPointerDown_(event);
     document.addEventListener('pointerdown', this.pointerDownListener_);
+    this.updateTabIndex_();
   },
 
   /** @override */
@@ -297,5 +300,15 @@ Polymer({
       return PrinterState.GOOD;
     }
     return PrinterState.ERROR;
+  },
+
+  /**
+   * Sets tabindex to -1 when dropdown is disabled to prevent the dropdown from
+   * being focusable.
+   * @private
+   */
+  updateTabIndex_() {
+    this.$$('#destination-dropdown')
+        .setAttribute('tabindex', this.disabled ? '-1' : '0');
   },
 });
