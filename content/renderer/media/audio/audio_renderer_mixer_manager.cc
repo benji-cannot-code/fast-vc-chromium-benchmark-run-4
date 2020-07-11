02ed0b9cbaa2cb794218e1c63f4e71d68cbde97e
Alex Gough <ajgo@chromium.org>
2020-07-11 07:14:24 +0000

    Use size_t refcount in mixer map and check for overflow.
    It is unlikely that this can be controlled as the universe of
    entries is limited by other resources on the system.
    
    Bug: 1101547
    Change-Id: I7c4825e3480d64557e30691da97c74a74c3c7a3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2285751
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Emircan Uysaler <emircan@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787504}

--- content/renderer/media/audio/audio_renderer_mixer_manager.cc
+++ content/renderer/media/audio/audio_renderer_mixer_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/audio/audio_renderer_mixer_manager.h"
 
 #include <algorithm>
+#include <limits>
 #include <string>
 #include <utility>
 
@@ -155,7 +156,9 @@ media::AudioRendererMixer* AudioRendererMixerManager::GetMixer(
 
   auto it = mixers_.find(key);
   if (it != mixers_.end()) {
-    it->second.ref_count++;
+    auto new_count = ++it->second.ref_count;
+    CHECK(new_count != std::numeric_limits<decltype(new_count)>::max());
+
     DVLOG(1) << "Reusing mixer: " << it->second.mixer;
 
     // Sink will now be released unused, but still must be stopped.
