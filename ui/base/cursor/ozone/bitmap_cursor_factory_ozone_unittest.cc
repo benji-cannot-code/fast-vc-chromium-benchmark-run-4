386aed359d4d0c765a9160c4ca8d3a610eff6c34
Henrique Ferreiro <hferreiro@igalia.com>
2020-07-12 01:03:08 +0000

    Test BitmapCursorFactoryOzone's invisible cursor
    Add test for https://crrev.com/c/2274631.
    
    Originally, BitmapCursorFactoryOzone::GetDefaultCursor() returned
    nullptr both for an invisible cursor (CursorType::kNone) and when no
    default cursor was available. The fix above changed the method's return
    type to base::Optional. This test checks that requesting and invisible
    cursor returns nullptr instead of base::nullopt.
    
    Bug: 1099873
    Change-Id: I7b027eb7ad0bf904e25217451b79ad6858977e70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292438
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#787554}

---
+++ ui/base/cursor/ozone/bitmap_cursor_factory_ozone_unittest.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/base/cursor/ozone/bitmap_cursor_factory_ozone.h"
+
+#include "base/optional.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/cursor/cursor.h"
+#include "ui/base/cursor/mojom/cursor_type.mojom-shared.h"
+
+namespace ui {
+
+TEST(BitmapCursorFactoryOzoneTest, InvisibleCursor) {
+  BitmapCursorFactoryOzone cursor_factory;
+
+  base::Optional<PlatformCursor> cursor =
+      cursor_factory.GetDefaultCursor(mojom::CursorType::kNone);
+  // The invisible cursor should be nullptr, not base::nullopt.
+  ASSERT_TRUE(cursor.has_value());
+  EXPECT_EQ(cursor, nullptr);
+}
+
+}  // namespace ui
