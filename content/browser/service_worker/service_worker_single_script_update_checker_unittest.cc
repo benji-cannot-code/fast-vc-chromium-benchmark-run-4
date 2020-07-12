d5a6d567056b11cd3c243c6b0af0666520a22bf5
Kenichi Ishibashi <bashi@chromium.org>
2020-07-12 23:27:13 +0000

    ServiceWorkerSingleScriptUpdateCheckerTest doesn't need test helper
    Tests in ServiceWorkerSingleScriptUpdateCheckerTest don't depend
    on EmbeddedWorkerTestHelper. Remove the helper.
    
    Bug: N/A
    Change-Id: Ia236ee0bfe20e38b18185a2653f6a6d30d363619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291295
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787604}

--- content/browser/service_worker/service_worker_single_script_update_checker_unittest.cc
+++ content/browser/service_worker/service_worker_single_script_update_checker_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind_test_util.h"
 #include "base/test/scoped_feature_list.h"
-#include "content/browser/service_worker/embedded_worker_test_helper.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
 #include "content/browser/service_worker/service_worker_test_utils.h"
 #include "content/public/test/browser_task_environment.h"
@@ -71,13 +70,6 @@ class ServiceWorkerSingleScriptUpdateCheckerTest : public testing::Test {
     base::RunLoop().RunUntilIdle();
   }
   ~ServiceWorkerSingleScriptUpdateCheckerTest() override = default;
-
-  ServiceWorkerStorage* storage() { return helper_->context()->storage(); }
-
-  void SetUp() override {
-    helper_ = std::make_unique<EmbeddedWorkerTestHelper>(base::FilePath());
-    storage()->LazyInitializeForTest();
-  }
 
   size_t TotalBytes(const std::vector<std::string>& data_chunks) {
     size_t bytes = 0;
@@ -194,7 +186,6 @@ class ServiceWorkerSingleScriptUpdateCheckerTest : public testing::Test {
 
  protected:
   BrowserTaskEnvironment task_environment_;
-  std::unique_ptr<EmbeddedWorkerTestHelper> helper_;
   std::unique_ptr<TestBrowserContext> browser_context_;
 
  private:
