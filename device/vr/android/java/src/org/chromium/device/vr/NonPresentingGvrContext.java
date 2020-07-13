7ddc7b1a24846c92a5d4af977605681ecf85871d
Peter Kotwicz <pkotwicz@chromium.org>
2020-07-13 22:11:08 +0000

    [Android] Rewrite Chrome strict mode handling
    This CL:
    - Fixes strict mode detection on Android P
    - Imports list of "unfixable strict mode violations" from other google
      apps
    - Avoids reporting strict mode violation (uploading stack trace) or
      preforming penalty if the strict mode violation is whitelisted.
    
    BUG=1038384
    
    Change-Id: Ib125d08104225af0e1dafb1be1afc5ef8d8e128e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2258578
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787921}

--- device/vr/android/java/src/org/chromium/device/vr/NonPresentingGvrContext.java
+++ device/vr/android/java/src/org/chromium/device/vr/NonPresentingGvrContext.java
@@ -6,13 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.device.vr;
 
 import android.content.Context;
-import android.os.StrictMode;
 import android.view.Display;
 
 import com.google.vr.cardboard.DisplaySynchronizer;
 import com.google.vr.ndk.base.GvrApi;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.StrictModeContext;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
 import org.chromium.base.annotations.NativeMethods;
@@ -35,20 +35,20 @@ public class NonPresentingGvrContext {
         mNativeGvrDevice = nativeGvrDevice;
         Context context = ContextUtils.getApplicationContext();
         Display display = DisplayAndroidManager.getDefaultDisplayForContext(context);
-        mDisplaySynchronizer = new DisplaySynchronizer(context, display) {
-            @Override
-            public void onConfigurationChanged() {
-                super.onConfigurationChanged();
-                onDisplayConfigurationChanged();
-            }
-        };
+
+        try (StrictModeContext ignored = StrictModeContext.allowDiskReads()) {
+            mDisplaySynchronizer = new DisplaySynchronizer(context, display) {
+                @Override
+                public void onConfigurationChanged() {
+                    super.onConfigurationChanged();
+                    onDisplayConfigurationChanged();
+                }
+            };
+        }
 
         // Creating the GvrApi can sometimes create the Daydream config file.
-        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskWrites();
-        try {
+        try (StrictModeContext ignored = StrictModeContext.allowDiskWrites()) {
             mGvrApi = new GvrApi(context, mDisplaySynchronizer);
-        } finally {
-            StrictMode.setThreadPolicy(oldPolicy);
         }
         resume();
     }
