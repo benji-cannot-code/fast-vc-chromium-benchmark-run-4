1bb65e7e1c204f41f9cef646f5eadfaf6e6f142f
Yulun Wu <yulunwu@chromium.org>
2020-07-13 19:07:18 +0000

    Track HomeToOverview nudge dismiss metrics.
    Some metrics are missing for home to overview gesture. This CL adds
    metrics to HomeToOverviewController::HideNudge().
    
    Bug: 1101105
    Change-Id: I166cce0a489925f0e03ed61b0808044af35aaea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2293459
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Toni Baržić <tbarzic@chromium.org>
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787829}

--- ash/shelf/home_to_overview_nudge_controller.cc
+++ ash/shelf/home_to_overview_nudge_controller.cc
@@ -120,6 +120,25 @@ class ObserverToCloseWidget : public ui::ImplicitAnimationObserver {
   views::Widget* const widget_;
 };
 
+void RecordNudgeMetrics(
+    HomeToOverviewNudgeController::HideTransition transition) {
+  switch (transition) {
+    case (HomeToOverviewNudgeController::HideTransition::kUserTap):
+      LogNudgeDismissedMetrics(contextual_tooltip::TooltipType::kHomeToOverview,
+                               contextual_tooltip::DismissNudgeReason::kTap);
+      break;
+    case (HomeToOverviewNudgeController::HideTransition::kNudgeTimeout):
+      LogNudgeDismissedMetrics(
+          contextual_tooltip::TooltipType::kHomeToOverview,
+          contextual_tooltip::DismissNudgeReason::kTimeout);
+      break;
+    case (HomeToOverviewNudgeController::HideTransition::kShelfStateChange):
+      LogNudgeDismissedMetrics(contextual_tooltip::TooltipType::kHomeToOverview,
+                               contextual_tooltip::DismissNudgeReason::kOther);
+      break;
+  }
+}
+
 }  // namespace
 
 HomeToOverviewNudgeController::HomeToOverviewNudgeController(
@@ -306,6 +325,8 @@ void HomeToOverviewNudgeController::ShowNudge() {
 void HomeToOverviewNudgeController::HideNudge(HideTransition transition) {
   if (!nudge_)
     return;
+
+  RecordNudgeMetrics(transition);
 
   auto animate_hide_transform = [](HideTransition transition,
                                    ui::Layer* layer) {
--- ash/shelf/home_to_overview_nudge_controller_unittest.cc
+++ ash/shelf/home_to_overview_nudge_controller_unittest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller_test_api.h"
 #include "ash/wm/window_state.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/simple_test_clock.h"
 #include "ui/compositor/layer.h"
@@ -227,6 +228,7 @@ TEST_F(HomeToOverviewNudgeControllerTest, NoNudgeBeforeLogin) {
 // first time, nudge should remain visible until the hotseat state changes. On
 // subsequent shows, the nudge should be hidden after a timeout.
 TEST_F(HomeToOverviewNudgeControllerTest, ShownOnHomeScreen) {
+  base::HistogramTester histogram_tester;
   CreateUserSessions(1);
 
   // The nudge should not be shown in clamshell.
@@ -292,6 +294,9 @@ TEST_F(HomeToOverviewNudgeControllerTest, ShownOnHomeScreen) {
   GetNudgeController()->FireHideTimerForTesting();
   EXPECT_FALSE(GetNudgeController()->nudge_for_testing());
   EXPECT_EQ(gfx::Transform(), GetHotseatWidget()->GetLayer()->transform());
+  histogram_tester.ExpectBucketCount(
+      "Ash.ContextualNudgeDismissContext.HomeToOverview",
+      contextual_tooltip::DismissNudgeReason::kTimeout, 1);
 }
 
 // Tests that the nudge eventually stops showing.
@@ -326,6 +331,7 @@ TEST_F(HomeToOverviewNudgeControllerTest, ShownLimitedNumberOfTimes) {
 
 // Tests that the nudge is hidden when tablet mode exits.
 TEST_F(HomeToOverviewNudgeControllerTest, HiddenOnTabletModeExit) {
+  base::HistogramTester histogram_tester;
   TabletModeControllerTestApi().EnterTabletMode();
   CreateUserSessions(1);
   ScopedWindowList extra_windows = CreateAndMinimizeWindows(2);
@@ -339,6 +345,10 @@ TEST_F(HomeToOverviewNudgeControllerTest, HiddenOnTabletModeExit) {
   EXPECT_FALSE(GetNudgeController()->nudge_for_testing());
   EXPECT_EQ(gfx::Transform(),
             GetHotseatWidget()->GetLayer()->GetTargetTransform());
+
+  histogram_tester.ExpectBucketCount(
+      "Ash.ContextualNudgeDismissContext.HomeToOverview",
+      contextual_tooltip::DismissNudgeReason::kOther, 1);
 }
 
 // Tests that the nudge show is canceled when tablet mode exits.
@@ -503,6 +513,7 @@ TEST_F(HomeToOverviewNudgeControllerTest, NoCrashIfNudgeWidgetGetsClosed) {
 
 // Tests that tapping on the nudge hides the nudge.
 TEST_F(HomeToOverviewNudgeControllerTest, TapOnTheNudgeClosesTheNudge) {
+  base::HistogramTester histogram_tester;
   TabletModeControllerTestApi().EnterTabletMode();
   CreateUserSessions(1);
   ScopedWindowList windows = CreateAndMinimizeWindows(2);
@@ -524,6 +535,10 @@ TEST_F(HomeToOverviewNudgeControllerTest, TapOnTheNudgeClosesTheNudge) {
 
   EXPECT_EQ(gfx::Transform(),
             GetHotseatWidget()->GetLayer()->GetTargetTransform());
+
+  histogram_tester.ExpectBucketCount(
+      "Ash.ContextualNudgeDismissContext.HomeToOverview",
+      contextual_tooltip::DismissNudgeReason::kTap, 1);
 }
 
 TEST_F(HomeToOverviewNudgeControllerTest, TapOnTheNudgeDuringShowAnimation) {
@@ -623,7 +638,7 @@ TEST_F(HomeToOverviewNudgeControllerTest, NoNudgeAfterSuccessfulGestures) {
                 transition_timer->FireNow();
             }));
 
-    // No point oof continuing the test if transition to overview failed.
+    // No point in continuing the test if transition to overview failed.
     ASSERT_TRUE(Shell::Get()->overview_controller()->InOverviewSession());
   }
 
