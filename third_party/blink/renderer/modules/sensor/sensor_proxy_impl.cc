79a2b69e5d6c624c5a0eac2aaaab847c8c533941
Minoru Chikamune <chikamune@chromium.org>
2020-07-13 02:24:27 +0000

    Deprecate HeapMojoWrapperMode::kWithoutContextObserver for SensorProxyImpl.
    The HeapMojoWrapperMode::kWithoutContextObserver was a HeapMojoWrapper's "safe mofe" to keep the existing behavior during the release freeze.
    
    Bug: 1049056
    Change-Id: I0ee6b4c02ee1f21ac0fadf28611e63fa05be68c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291797
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Tal Pressman <talp@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787615}

--- third_party/blink/renderer/modules/sensor/sensor_proxy_impl.cc
+++ third_party/blink/renderer/modules/sensor/sensor_proxy_impl.cc
@@ -80,7 +80,7 @@ std::pair<double, double> SensorProxyImpl::GetFrequencyLimits() const {
 }
 
 void SensorProxyImpl::Suspend() {
-  if (suspended_)
+  if (suspended_ || !sensor_remote_.is_bound())
     return;
 
   sensor_remote_->Suspend();
@@ -89,7 +89,7 @@ void SensorProxyImpl::Suspend() {
 }
 
 void SensorProxyImpl::Resume() {
-  if (!suspended_)
+  if (!suspended_ || !sensor_remote_.is_bound())
     return;
 
   sensor_remote_->Resume();
--- third_party/blink/renderer/modules/sensor/sensor_proxy_impl.h
+++ third_party/blink/renderer/modules/sensor/sensor_proxy_impl.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/heap/handle.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
-#include "third_party/blink/renderer/platform/mojo/heap_mojo_wrapper_mode.h"
 #include "third_party/blink/renderer/platform/timer.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
@@ -76,12 +75,8 @@ class SensorProxyImpl final : public SensorProxy,
 
   device::mojom::blink::ReportingMode mode_ =
       device::mojom::blink::ReportingMode::CONTINUOUS;
-  HeapMojoRemote<device::mojom::blink::Sensor,
-                 HeapMojoWrapperMode::kForceWithoutContextObserver>
-      sensor_remote_;
-  HeapMojoReceiver<device::mojom::blink::SensorClient,
-                   SensorProxyImpl,
-                   HeapMojoWrapperMode::kForceWithoutContextObserver>
+  HeapMojoRemote<device::mojom::blink::Sensor> sensor_remote_;
+  HeapMojoReceiver<device::mojom::blink::SensorClient, SensorProxyImpl>
       client_receiver_;
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
