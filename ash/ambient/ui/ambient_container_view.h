a2aa88182efecfac80651abe4b92c3b95f434880
Meilin Wang <meilinw@google.com>
2020-07-13 02:53:30 +0000

    ambient: Add mouse move exit point.
    Bug: b/153041587
    Test: covered by unittest.
    Change-Id: Ic8141b9c91199b5d798f5ef8fc518caed1efc35d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2288359
    Commit-Queue: Meilin Wang <meilinw@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787618}

--- ash/ambient/ui/ambient_container_view.h
+++ ash/ambient/ui/ambient_container_view.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_AMBIENT_UI_AMBIENT_CONTAINER_VIEW_H_
 #define ASH_AMBIENT_UI_AMBIENT_CONTAINER_VIEW_H_
 
+#include <memory>
+
 #include "ash/ash_export.h"
 #include "base/macros.h"
 #include "ui/views/view.h"
@@ -41,8 +43,8 @@ class ASH_EXPORT AmbientContainerView : public views::View {
   void LayoutGlanceableInfoView();
   void LayoutAssistantView();
 
-  // Invoked on key press events.
-  void HandleKeyEvent();
+  // Invoked on specific types of events.
+  void HandleEvent();
 
   PhotoView* photo_view_for_testing() { return photo_view_; }
 
