ccb18cc675f43e51219b6d9c1ffd604395a59740
Anatoliy Potapchuk <apotapchuk@chromium.org>
2020-07-13 16:02:49 +0000

    [Kiosk] Make KioskLaunchController more verbose
    To simplify potential debugging of kiosk issues, let's add more logs
    about state changes in KioskLaunchController.
    
    Bug: 1015383
    Change-Id: I04a3eed739d46a3762295a8e376bae221965a761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292294
    Auto-Submit: Anatoliy Potapchuk <apotapchuk@chromium.org>
    Commit-Queue: Roman Sorokin [CET] <rsorokin@chromium.org>
    Reviewed-by: Roman Sorokin [CET] <rsorokin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787742}

--- chrome/browser/chromeos/login/kiosk_launch_controller.cc
+++ chrome/browser/chromeos/login/kiosk_launch_controller.cc
@@ -195,6 +195,8 @@ void KioskLaunchController::OnCancelAppLaunch() {
   if (KioskAppManager::Get()->GetDisableBailoutShortcut())
     return;
 
+  SYSLOG(INFO) << "Canceling kiosk app launch.";
+
   KioskAppLaunchError::Save(KioskAppLaunchError::USER_CANCEL);
   CleanUp();
   chrome::AttemptUserExit();
@@ -263,6 +265,7 @@ void KioskLaunchController::CloseSplashScreen() {
 }
 
 void KioskLaunchController::OnAppInstalling() {
+  SYSLOG(INFO) << "Kiosk app started installing.";
   app_state_ = AppState::INSTALLING;
   if (!splash_screen_view_)
     return;
@@ -274,6 +277,7 @@ void KioskLaunchController::OnAppInstalling() {
 }
 
 void KioskLaunchController::OnAppPrepared() {
+  SYSLOG(INFO) << "Kiosk app is ready to launch.";
   app_state_ = AppState::INSTALLED;
 
   if (!splash_screen_view_)
@@ -461,6 +465,7 @@ bool KioskLaunchController::NeedOwnerAuthToConfigureNetwork() {
 }
 
 void KioskLaunchController::MaybeShowNetworkConfigureUI() {
+  SYSLOG(INFO) << "Network configure UI was requested to be shown.";
   if (!splash_screen_view_)
     return;
 
@@ -481,6 +486,7 @@ void KioskLaunchController::MaybeShowNetworkConfigureUI() {
 
 void KioskLaunchController::ShowNetworkConfigureUI() {
   if (!profile_) {
+    SYSLOG(INFO) << "Postponing network dialog till profile is loaded.";
     splash_screen_view_->UpdateAppLaunchState(
         AppLaunchSplashScreenView::
             APP_LAUNCH_STATE_SHOWING_NETWORK_CONFIGURE_UI);
@@ -494,8 +500,10 @@ void KioskLaunchController::ShowNetworkConfigureUI() {
   network_ui_state_ = NetworkUIState::SHOWING;
   splash_screen_view_->ShowNetworkConfigureUI();
 }
+
 void KioskLaunchController::CloseNetworkConfigureScreenIfTimedout() {
   if (network_ui_state_ == NetworkUIState::SHOWING && network_wait_timedout_) {
+    SYSLOG(INFO) << "We are back online, closing network configure screen.";
     splash_screen_view_->ToggleNetworkConfig(false);
     network_ui_state_ = NetworkUIState::NOT_SHOWING;
   }
@@ -544,6 +552,8 @@ void KioskLaunchController::OnNetworkStateChanged(bool online) {
   }
 
   if (app_state_ == AppState::INSTALLING && !online) {
+    SYSLOG(WARNING)
+        << "Connection lost during installation, restarting launcher.";
     app_state_ = AppState::INIT_NETWORK;
     app_launcher_->RestartLauncher();
     MaybeShowNetworkConfigureUI();
