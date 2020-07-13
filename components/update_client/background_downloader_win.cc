43df6c95423876bf13bd9f88fb11a1e0da348ab7
Sorin Jianu <sorin@chromium.org>
2020-07-13 19:54:40 +0000

    Make CrxDownloader a ref-counted thread safe type.
    This avoid retention issues and allows instances of CrxDownloader to
    be shared between sequences when interfacing with RPC modules such as
    BITS/DO for Windows.
    
    There are some mechanical changes such as removing macros.h and
    replacing thread checkers with sequence checkers (but not for the
    BITS downloader, which has thread affinity due to COM RPC).
    
    Change-Id: I62e159d4ae8999ec70da94247f91f9cc26a5d278
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2293461
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787857}

--- components/update_client/background_downloader_win.cc
+++ components/update_client/background_downloader_win.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/location.h"
 #include "base/logging.h"
-#include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/string_piece.h"
@@ -397,7 +396,7 @@ void CleanupJob(const ComPtr<IBackgroundCopyJob>& job) {
 }  // namespace
 
 BackgroundDownloader::BackgroundDownloader(
-    std::unique_ptr<CrxDownloader> successor)
+    scoped_refptr<CrxDownloader> successor)
     : CrxDownloader(std::move(successor)),
       com_task_runner_(base::ThreadPool::CreateCOMSTATaskRunner(
           kTaskTraitsBackgroundDownloader)),
@@ -418,15 +417,14 @@ void BackgroundDownloader::StartTimer() {
 void BackgroundDownloader::OnTimer() {
   DCHECK(thread_checker_.CalledOnValidThread());
   com_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&BackgroundDownloader::OnDownloading,
-                                base::Unretained(this)));
+      FROM_HERE, base::BindOnce(&BackgroundDownloader::OnDownloading, this));
 }
 
 void BackgroundDownloader::DoStartDownload(const GURL& url) {
   DCHECK(thread_checker_.CalledOnValidThread());
   com_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&BackgroundDownloader::BeginDownload,
-                                base::Unretained(this), url));
+      FROM_HERE,
+      base::BindOnce(&BackgroundDownloader::BeginDownload, this, url));
 }
 
 // Called one time when this class is asked to do a download.
@@ -445,9 +443,8 @@ void BackgroundDownloader::BeginDownload(const GURL& url) {
   VLOG(1) << "Starting BITS download for: " << url.spec();
 
   ResetInterfacePointers();
-  main_task_runner()->PostTask(FROM_HERE,
-                               base::BindOnce(&BackgroundDownloader::StartTimer,
-                                              base::Unretained(this)));
+  main_task_runner()->PostTask(
+      FROM_HERE, base::BindOnce(&BackgroundDownloader::StartTimer, this));
 }
 
 // Creates or opens an existing BITS job to download the |url|, and handles
@@ -536,9 +533,8 @@ void BackgroundDownloader::OnDownloading() {
     return;
 
   ResetInterfacePointers();
-  main_task_runner()->PostTask(FROM_HERE,
-                               base::BindOnce(&BackgroundDownloader::StartTimer,
-                                              base::Unretained(this)));
+  main_task_runner()->PostTask(
+      FROM_HERE, base::BindOnce(&BackgroundDownloader::StartTimer, this));
 }
 
 // Completes the BITS download, picks up the file path of the response, and
@@ -583,9 +579,8 @@ void BackgroundDownloader::EndDownload(HRESULT error) {
   if (!result.error)
     result.response = response_;
   main_task_runner()->PostTask(
-      FROM_HERE, base::BindOnce(&BackgroundDownloader::OnDownloadComplete,
-                                base::Unretained(this), is_handled, result,
-                                download_metrics));
+      FROM_HERE, base::BindOnce(&BackgroundDownloader::OnDownloadComplete, this,
+                                is_handled, result, download_metrics));
 
   // Once the task is posted to the the main thread, this object may be deleted
   // by its owner. It is not safe to access members of this object on this task
@@ -667,9 +662,8 @@ bool BackgroundDownloader::OnStateTransferring() {
   GetJobByteCount(job_, &downloaded_bytes, &total_bytes);
 
   main_task_runner()->PostTask(
-      FROM_HERE,
-      base::BindOnce(&BackgroundDownloader::OnDownloadProgress,
-                     base::Unretained(this), downloaded_bytes, total_bytes));
+      FROM_HERE, base::BindOnce(&BackgroundDownloader::OnDownloadProgress, this,
+                                downloaded_bytes, total_bytes));
   return false;
 }
 
--- components/update_client/crx_downloader_unittest.cc
+++ components/update_client/crx_downloader_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/update_client/crx_downloader.h"
 
-#include <memory>
 #include <utility>
 
 #include "base/bind.h"
@@ -70,7 +69,7 @@ class CrxDownloaderTest : public testing::Test {
                    int net_error);
 
  protected:
-  std::unique_ptr<CrxDownloader> crx_downloader_;
+  scoped_refptr<CrxDownloader> crx_downloader_;
 
   network::TestURLLoaderFactory test_url_loader_factory_;
 
@@ -121,7 +120,7 @@ void CrxDownloaderTest::SetUp() {
 }
 
 void CrxDownloaderTest::TearDown() {
-  crx_downloader_.reset();
+  crx_downloader_ = nullptr;
 }
 
 void CrxDownloaderTest::Quit() {
@@ -176,6 +175,7 @@ void CrxDownloaderTest::RunThreads() {
   RunThreadsUntilIdle();
 }
 
+// TODO(crbug.com/1104691): rewrite the tests to not use RunUntilIdle().
 void CrxDownloaderTest::RunThreadsUntilIdle() {
   task_environment_.RunUntilIdle();
   base::RunLoop().RunUntilIdle();
