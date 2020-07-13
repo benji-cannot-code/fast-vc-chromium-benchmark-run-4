101053eadd2270382818a7e9f9e852b56e94a975
Sinan Sahin <sinansahin@google.com>
2020-07-13 21:49:44 +0000

    [Offline indicator v2] Animate Start surface
    This CL builds upon crrev.com/c/2119610 and updates the top margin every
    time the offsets change instead of doing so only on height changes.
    
    Bug: 1102898
    Change-Id: Ibb2a5b0a185dc84d3b937f9ed7a624087bdfd151
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2285248
    Reviewed-by: Ganggui Tang <gogerald@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/master@{#787910}

--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/SecondaryTasksSurfaceViewBinder.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/SecondaryTasksSurfaceViewBinder.java
@@ -8,7 +8,7 @@ package org.chromium.chrome.features.start_surface;
 import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.IS_SECONDARY_SURFACE_VISIBLE;
 import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.IS_SHOWING_OVERVIEW;
 import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.IS_SHOWING_STACK_TAB_SWITCHER;
-import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.TOP_BAR_HEIGHT;
+import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.TOP_MARGIN;
 
 import android.view.View;
 import android.view.ViewGroup.MarginLayoutParams;
@@ -26,8 +26,8 @@ class SecondaryTasksSurfaceViewBinder {
             updateVisibility(viewHolder, model);
         } else if (IS_SHOWING_STACK_TAB_SWITCHER == propertyKey) {
             updateVisibility(viewHolder, model);
-        } else if (TOP_BAR_HEIGHT == propertyKey) {
-            setTopBarHeight(viewHolder, model.get(TOP_BAR_HEIGHT));
+        } else if (TOP_MARGIN == propertyKey) {
+            setTopBarHeight(viewHolder, model.get(TOP_MARGIN));
         }
     }
 
@@ -40,7 +40,7 @@ class SecondaryTasksSurfaceViewBinder {
             viewHolder.parentView.addView(viewHolder.tasksSurfaceView);
             MarginLayoutParams layoutParams =
                     (MarginLayoutParams) viewHolder.tasksSurfaceView.getLayoutParams();
-            layoutParams.topMargin = model.get(TOP_BAR_HEIGHT);
+            layoutParams.topMargin = model.get(TOP_MARGIN);
         }
 
         viewHolder.tasksSurfaceView.setVisibility(isShowing ? View.VISIBLE : View.GONE);
--- chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
+++ chrome/android/features/start_surface/internal/java/src/org/chromium/chrome/features/start_surface/StartSurfaceMediator.java
@@ -27,7 +27,7 @@ import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.
 import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.IS_SECONDARY_SURFACE_VISIBLE;
 import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.IS_SHOWING_OVERVIEW;
 import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.IS_SHOWING_STACK_TAB_SWITCHER;
-import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.TOP_BAR_HEIGHT;
+import static org.chromium.chrome.features.start_surface.StartSurfaceProperties.TOP_MARGIN;
 
 import android.content.res.Resources;
 import android.view.View;
@@ -258,9 +258,10 @@ class StartSurfaceMediator
 
             mBrowserControlsObserver = new BrowserControlsStateProvider.Observer() {
                 @Override
-                public void onTopControlsHeightChanged(
-                        int topControlsHeight, int topControlsMinHeight) {
-                    mPropertyModel.set(TOP_BAR_HEIGHT, topControlsHeight);
+                public void onControlsOffsetChanged(int topOffset, int topControlsMinHeightOffset,
+                        int bottomOffset, int bottomControlsMinHeightOffset, boolean needsAnimate) {
+                    mPropertyModel.set(
+                            TOP_MARGIN, mBrowserControlsStateProvider.getContentOffset());
                 }
 
                 @Override
@@ -560,8 +561,7 @@ class StartSurfaceMediator
                 mBrowserControlsStateProvider.addObserver(mBrowserControlsObserver);
             }
 
-            mPropertyModel.set(
-                    TOP_BAR_HEIGHT, mBrowserControlsStateProvider.getTopControlsHeight());
+            mPropertyModel.set(TOP_MARGIN, mBrowserControlsStateProvider.getTopControlsHeight());
 
             mPropertyModel.set(IS_SHOWING_OVERVIEW, true);
             if (mFakeboxDelegate != null) {
