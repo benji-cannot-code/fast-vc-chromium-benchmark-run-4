2378057dce56640ddb88fa3f6d67c5c2992b3182
Daniel Nicoara <dnicoara@chromium.org>
2020-07-13 21:03:53 +0000

    cast: Add callback to Enable/Disable display API
    The caller may need to perform operations after the display is
    enabled/disabled. Since the display configuration API is async, provide
    the callbacks such that callers can use them to schedule dependent work.
    
    BUG=b/156411764
    TEST=Compiled
    
    Change-Id: I044767c922eabbec123e4399a5774ccd12d1742d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296000
    Reviewed-by: Albert Chaulk <achaulk@chromium.org>
    Commit-Queue: Daniel Nicoara <dnicoara@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787897}

--- chromecast/browser/cast_display_configurator.h
+++ chromecast/browser/cast_display_configurator.h
@@ -12,12 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
 #include "ui/display/display.h"
+#include "ui/display/types/native_display_delegate.h"
 #include "ui/display/types/native_display_observer.h"
 
 namespace display {
 class DisplayMode;
 class DisplaySnapshot;
-class NativeDisplayDelegate;
 struct GammaRampRGBEntry;
 }  // namespace display
 
@@ -46,8 +46,8 @@ class CastDisplayConfigurator : public display::NativeDisplayObserver {
   void OnConfigurationChanged() override;
   void OnDisplaySnapshotsInvalidated() override {}
 
-  void EnableDisplay();
-  void DisableDisplay();
+  void EnableDisplay(display::ConfigureCallback callback);
+  void DisableDisplay(display::ConfigureCallback callback);
 
   void ConfigureDisplayFromCommandLine();
   void SetColorMatrix(const std::vector<float>& color_matrix);
