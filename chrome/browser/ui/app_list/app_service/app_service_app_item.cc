e37f62a869efdb39565e3f7d8253b5528b7436ed
nancylingwang@google.com <nancylingwang@chromium.org>
2020-07-13 19:29:27 +0000

    Remove extension_app_context_menu
    Modify unit tests to use AppServiceContextMenu and make fake extension
    apps.
    
    Unlike ExtensionAppContextMenu, AppServiceContextMenu only generates
    the enabled command, so the unit tests are modified to check the enabled
    items.
    
    BUG=1016159
    
    Change-Id: I245ea01de0f5a5ef05cb2bb5575babd34d466add
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2280972
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Nancy Wang <nancylingwang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787841}

--- chrome/browser/ui/app_list/app_service/app_service_app_item.cc
+++ chrome/browser/ui/app_list/app_service/app_service_app_item.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/crostini/crostini_util.h"
 #include "chrome/browser/ui/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ui/app_list/app_service/app_service_context_menu.h"
-#include "chrome/browser/ui/app_list/extension_app_context_menu.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 #include "chrome/common/chrome_features.h"
 
