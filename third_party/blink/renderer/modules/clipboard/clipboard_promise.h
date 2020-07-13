de781a34780d2ab91bd4272993ab543476023c7d
Dylan Sleeper <dsleeps@google.com>
2020-07-13 20:05:58 +0000

    Clipboard API: Implement the HTML clipboard reader and writer.
    This CL adds two subclasses: ClipboardHTMLReader and
    ClipboardHTMLWriter. When HTML is read and written it is "sanitized"
    which means that all script objects are removed from the html. They
    are both entirely synchronous because it is impossible to use HTML
    DOM nodes off of the main thread.
    
    Design Doc: https://docs.google.com/document/d/1tzjfckWUKAAxEbKcUjg21uz7LFDMQ7ZDzfTTj3Lp93Y/edit
    
    Bug: 931839
    Change-Id: I29b26f1c721102005bcbb5911549a9546aed3006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2243532
    Commit-Queue: Dylan Sleeper <dsleeps@google.com>
    Reviewed-by: Jarryd Goodman <jarrydg@chromium.org>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787862}

--- third_party/blink/renderer/modules/clipboard/clipboard_promise.h
+++ third_party/blink/renderer/modules/clipboard/clipboard_promise.h
@@ -55,6 +55,8 @@ class ClipboardPromise final : public GarbageCollected<ClipboardPromise>,
   // Adds the blob to the clipboard items.
   void OnRead(Blob* blob);
 
+  LocalFrame* GetLocalFrame() const;
+
   void Trace(Visitor*) const override;
 
  private:
@@ -85,7 +87,6 @@ class ClipboardPromise final : public GarbageCollected<ClipboardPromise>,
       bool allow_without_sanitization,
       base::OnceCallback<void(::blink::mojom::PermissionStatus)> callback);
 
-  LocalFrame* GetLocalFrame() const;
   scoped_refptr<base::SingleThreadTaskRunner> GetTaskRunner();
 
   Member<ScriptState> script_state_;
