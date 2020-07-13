9ca8f34c32515cf2e1f589e5c0ec70fc7430b692
John Lee <johntlee@chromium.org>
2020-07-13 17:52:32 +0000

    Settings WebUI: Fix alignment on All Sites page
    https://i.imgur.com/wcaYtf9.png
    
    Fixed: 1102273
    Change-Id: I774d8022f13ef3f8d7ce978c742585312f6c5188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290190
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Esmael Elmoslimany <aee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787787}

--- chrome/test/data/webui/settings/site_entry_tests.js
+++ chrome/test/data/webui/settings/site_entry_tests.js
@@ -96,7 +96,7 @@ suite('SiteEntry', function() {
     flush();
     const collapseChild = testElement.$.originList.get();
     flush();
-    assertEquals(3, collapseChild.querySelectorAll('.list-item').length);
+    assertEquals(3, collapseChild.querySelectorAll('.origin-link').length);
   });
 
   test('expands and closes to show more origins', function() {
@@ -138,7 +138,7 @@ suite('SiteEntry', function() {
     flush();
     const collapseChild = testElement.$.originList.get();
     flush();
-    const originList = collapseChild.querySelectorAll('.list-item');
+    const originList = collapseChild.querySelectorAll('.origin-link');
     assertEquals(3, originList.length);
 
     // Test clicking on one of these origins takes the user to Site Details,
@@ -382,7 +382,7 @@ suite('SiteEntry', function() {
     flush();
     const collapseChild = testElement.$.originList.get();
     flush();
-    const origins = collapseChild.querySelectorAll('.list-item');
+    const origins = collapseChild.querySelectorAll('.origin-link');
     assertEquals(3, origins.length);
     assertEquals(
         'www.example.com',
@@ -412,7 +412,7 @@ suite('SiteEntry', function() {
     flush();
     const collapseChild = testElement.$.originList.get();
     flush();
-    const origins = collapseChild.querySelectorAll('.list-item');
+    const origins = collapseChild.querySelectorAll('.origin-link');
     assertEquals(3, origins.length);
     assertEquals(
         'www.example.com',
@@ -442,7 +442,7 @@ suite('SiteEntry', function() {
     flush();
     const collapseChild = testElement.$.originList.get();
     flush();
-    const origins = collapseChild.querySelectorAll('.list-item');
+    const origins = collapseChild.querySelectorAll('.origin-link');
     assertEquals(3, origins.length);
     assertEquals(
         'example.com',
