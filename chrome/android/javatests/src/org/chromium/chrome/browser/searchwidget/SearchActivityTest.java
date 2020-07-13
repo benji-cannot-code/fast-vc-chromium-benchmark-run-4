0122224b79ef2d97377ebcbafe49b483b6f18f0c
David Trainor <dtrainor@chromium.org>
2020-07-13 20:47:27 +0000

    Disabling flaky tests
    Just going to disable SearchActivityTest#... tests that are flaking on
    the bots.
    
    Tbr: gangwu@chromium.org
    Bug: 1100967
    Change-Id: Ia8abe36da611cf2206f5a1edb893ce8930411df0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295828
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Auto-Submit: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787886}

--- chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/searchwidget/SearchActivityTest.java
@@ -16,7 +16,6 @@ import android.app.Instrumentation.ActivityMonitor;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.view.KeyEvent;
 import android.view.ViewGroup;
@@ -42,7 +41,7 @@ import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
@@ -555,8 +554,7 @@ public class SearchActivityTest {
 
     @Test
     @SmallTest
-    @DisableIf.
-    Build(message = "https://crbug.com/1100967", sdk_is_less_than = Build.VERSION_CODES.M)
+    @DisabledTest(message = "Flaky - https://crbug.com/1100967")
     @Features.
     EnableFeatures({ChromeFeatureList.OMNIBOX_ENABLE_CLIPBOARD_PROVIDER_IMAGE_SUGGESTIONS})
     public void testImageSearch() throws InterruptedException, Exception {
@@ -637,8 +635,7 @@ public class SearchActivityTest {
 
     @Test
     @SmallTest
-    @DisableIf.
-    Build(message = "https://crbug.com/1100967", sdk_is_less_than = Build.VERSION_CODES.M)
+    @DisabledTest(message = "Flaky - https://crbug.com/1100967")
     @Features.
     EnableFeatures({ChromeFeatureList.OMNIBOX_ENABLE_CLIPBOARD_PROVIDER_IMAGE_SUGGESTIONS})
     public void testImageSearch_OnlyTrustedIntentCanPost() throws InterruptedException, Exception {
