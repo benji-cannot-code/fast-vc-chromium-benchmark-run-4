24bd5536806b3dc385ef54b873f76732a110424e
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2020-07-13 17:32:47 +0000

    [LayoutNG] Insert OOF nodes in existing fragmentainer
    Because the OOF-positioned elements are laid out after the
    fragmentainers in a fragmentation context, we need a way to insert them
    inside of an already laid out fragmentainer. This CL introduces a
    fragmentainer "mutable copy" to which we can append NGLayoutResults
    without a relayout. Once we append the resulting fragment to the
    "mutable copy", we replace the original by this one in the
    NGContainerFragmentBuilder.
    
    Bug: 1079031
    Change-Id: I40610943f591575ab631f24d6a2b3f9e363a46af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2252927
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787771}

--- third_party/blink/renderer/core/layout/ng/ng_simplified_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/ng/ng_simplified_layout_algorithm.h
@@ -46,7 +46,7 @@ class CORE_EXPORT NGSimplifiedLayoutAlgorithm
   scoped_refptr<const NGLayoutResult> Layout() override;
   MinMaxSizesResult ComputeMinMaxSizes(const MinMaxSizesInput&) const override {
     NOTREACHED();
-    return {MinMaxSizes(), true};
+    return {MinMaxSizes(), /* depends_on_percentage_block_size */ true};
   }
 
   NOINLINE scoped_refptr<const NGLayoutResult> LayoutWithItemsBuilder();
