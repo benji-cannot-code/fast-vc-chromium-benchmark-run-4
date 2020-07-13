cb3fdd645f74c0daa706b713fb96a2f3ddaae9a8
Koji Ishii <kojii@chromium.org>
2020-07-13 18:25:37 +0000

    Add NGPhysicalFragmentList
    Supports range-based for loop to get |NGPhysicalBoxFragment|.
    
    Bug: 829028
    Change-Id: I8473172ea723c725f63637307e189ae8abc52f83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2291293
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787804}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -2912,6 +2912,11 @@ const NGPhysicalBoxFragment* LayoutBox::GetPhysicalFragment(
   return &To<NGPhysicalBoxFragment>(layout_results_[index]->PhysicalFragment());
 }
 
+const NGPhysicalBoxFragment&
+LayoutBox::NGPhysicalFragmentList::Iterator::operator*() const {
+  return To<NGPhysicalBoxFragment>((*iterator_)->PhysicalFragment());
+}
+
 const FragmentData* LayoutBox::FragmentDataFromPhysicalFragment(
     const NGPhysicalBoxFragment& physical_fragment) const {
   const FragmentData* fragment_data = &FirstFragment();
