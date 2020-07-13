082781691dc0187dc53cfc58767627918d9ffab3
Bailey Berro <baileyberro@chromium.org>
2020-07-13 20:21:56 +0000

    Add support for Display Layout Preview
    This change adds a new mojo API DragDisplayDelta() to support showing
    display alignment indicators.
    
    Note: This is a continutation of crrev.com/c/2146921
    
    Bug: 1070122
    Change-Id: I18b1b13680b4a989765528af9b9afd87f8752fbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2161873
    Commit-Queue: Bailey Berro <baileyberro@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787870}

--- ash/display/cros_display_config.cc
+++ ash/display/cros_display_config.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "ash/display/display_alignment_controller.h"
 #include "ash/display/display_configuration_controller.h"
 #include "ash/display/display_highlight_controller.h"
 #include "ash/display/display_prefs.h"
@@ -1001,10 +1002,19 @@ OverscanCalibrator* CrosDisplayConfig::GetOverscanCalibrator(
   return iter == overscan_calibrators_.end() ? nullptr : iter->second.get();
 }
 
-void CrosDisplayConfig::HighlightDisplay(int64_t id) {
+void CrosDisplayConfig::HighlightDisplay(int64_t display_id) {
   DCHECK(base::FeatureList::IsEnabled(features::kDisplayIdentification));
 
-  Shell::Get()->display_highlight_controller()->SetHighlightedDisplay(id);
+  Shell::Get()->display_highlight_controller()->SetHighlightedDisplay(
+      display_id);
+}
+
+void CrosDisplayConfig::DragDisplayDelta(int64_t display_id,
+                                         int32_t delta_x,
+                                         int32_t delta_y) {
+  DCHECK(features::IsDisplayAlignmentAssistanceEnabled());
+  Shell::Get()->display_alignment_controller()->DisplayDragged(
+      display_id, delta_x, delta_y);
 }
 
 }  // namespace ash
--- ash/display/cros_display_config_unittest.cc
+++ ash/display/cros_display_config_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/display/cros_display_config.h"
 
+#include "ash/display/display_alignment_controller.h"
 #include "ash/display/display_highlight_controller.h"
 #include "ash/display/screen_orientation_controller.h"
 #include "ash/display/touch_calibrator_controller.h"
@@ -82,7 +83,8 @@ class CrosDisplayConfigTest : public AshTestBase {
   ~CrosDisplayConfigTest() override {}
 
   void SetUp() override {
-    scoped_feature_list_.InitAndEnableFeature(features::kDisplayIdentification);
+    scoped_feature_list_.InitWithFeatures(
+        {features::kDisplayIdentification, features::kDisplayAlignAssist}, {});
 
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kUseFirstDisplayAsInternal);
@@ -202,6 +204,17 @@ class CrosDisplayConfigTest : public AshTestBase {
 
   void HighlightDisplay(int64_t id) {
     cros_display_config_->HighlightDisplay(id);
+  }
+
+  void DragDisplayDelta(int64_t id, int32_t delta_x, int32_t delta_y) {
+    cros_display_config_->DragDisplayDelta(id, delta_x, delta_y);
+  }
+
+  bool PreviewIndicatorsExist() {
+    return !Shell::Get()
+                ->display_alignment_controller()
+                ->GetActiveIndicatorsForTesting()
+                .empty();
   }
 
   CrosDisplayConfig* cros_display_config() { return cros_display_config_; }
@@ -841,6 +854,18 @@ TEST_F(CrosDisplayConfigTest, HighlightDisplayInvalid) {
 
   EXPECT_EQ(Shell::Get()->display_highlight_controller()->GetWidgetForTesting(),
             nullptr);
+}
+
+TEST_F(CrosDisplayConfigTest, DragDisplayDelta) {
+  UpdateDisplay("500x400,500x400");
+
+  const auto& display = display_manager()->GetDisplayAt(0);
+
+  EXPECT_FALSE(PreviewIndicatorsExist());
+
+  DragDisplayDelta(display.id(), 0, 16);
+
+  EXPECT_TRUE(PreviewIndicatorsExist());
 }
 
 }  // namespace ash
