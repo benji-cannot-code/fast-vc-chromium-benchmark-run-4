072bd20a9588519e05476873cad277c30209e5c6
Mohamed Amir Yosef <mamir@chromium.org>
2020-07-13 15:14:04 +0000

    [Sheriff] Disable Password Manager Bubble unit tests on Windows
    All tests that show a bubble with a button, are flaky on Windows.
    
    We have good test coverage since the tests still run on Linux and Mac.
    However, on Windows, they are flaky due to http://crbug.com/968222.
    
    Change-Id: I46709a1f295a137e87527343bad962b273134a8d
    Bug: 1104699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2294980
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787727}

--- chrome/browser/ui/views/passwords/password_save_unsynced_credentials_locally_view_unittest.cc
+++ chrome/browser/ui/views/passwords/password_save_unsynced_credentials_locally_view_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/passwords/passwords_model_delegate_mock.h"
 #include "chrome/browser/ui/views/passwords/password_bubble_view_test_base.h"
 #include "components/autofill/core/common/password_form.h"
@@ -56,7 +57,14 @@ void PasswordSaveUnsyncedCredentialsLocallyViewTest::TearDown() {
   PasswordBubbleViewTestBase::TearDown();
 }
 
-TEST_F(PasswordSaveUnsyncedCredentialsLocallyViewTest, HasTitleAndTwoButtons) {
+// Flaky on Windows due to http://crbug.com/968222
+#if defined(OS_WIN)
+#define MAYBE_HasTitleAndTwoButtons DISABLED_HasTitleAndTwoButtons
+#else
+#define MAYBE_HasTitleAndTwoButtons HasTitleAndTwoButtons
+#endif
+TEST_F(PasswordSaveUnsyncedCredentialsLocallyViewTest,
+       MAYBE_HasTitleAndTwoButtons) {
   CreateViewAndShow();
   EXPECT_TRUE(view_->ShouldShowWindowTitle());
   EXPECT_TRUE(view_->GetOkButton());
