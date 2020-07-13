6a93090a7a0f3245b06a08636ce2542f46b9529f
Lei Zhang <thestig@chromium.org>
2020-07-13 22:09:57 +0000

    Fix some stragglers still using deprecated form of base::DeleteFile().
    Switch to the 1-parameter version, or switch to
    base::DeletePathRecursively().
    
    Bug: 1009837
    Change-Id: Iab8258f3d8b29c308758096299a8e6702230484a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2293282
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#787919}

--- content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
+++ content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
@@ -176,7 +176,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 
   // Verify that no other files exist and remove temp dir.
   EXPECT_TRUE(base::IsDirectoryEmpty(temp_dir_path));
-  EXPECT_TRUE(base::DeleteFile(temp_dir_path, false));
+  EXPECT_TRUE(base::DeleteFile(temp_dir_path));
 
   base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
 }
@@ -229,7 +229,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 
   // Verify that no files exist and remove temp dir.
   EXPECT_TRUE(base::IsDirectoryEmpty(temp_dir_path));
-  EXPECT_TRUE(base::DeleteFile(temp_dir_path, false));
+  EXPECT_TRUE(base::DeleteFile(temp_dir_path));
 
   base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
 }
@@ -334,7 +334,7 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 
   // Verify that no other files exist and remove temp dir.
   EXPECT_TRUE(base::IsDirectoryEmpty(temp_dir_path));
-  EXPECT_TRUE(base::DeleteFile(temp_dir_path, false));
+  EXPECT_TRUE(base::DeleteFile(temp_dir_path));
 
   base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
 }
