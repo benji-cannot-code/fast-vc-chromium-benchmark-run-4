383d1f76beef54def22ec93ed66f297b05716cd5
Steve Becker <stevebe@microsoft.com>
2020-07-14 22:37:45 +0000

    SessionMessageWindow::OnWndProc() should use DefWindowProc()
    SessionMessageWindow::OnWndProc() returns true without setting the
    LRESULT* result out function parameter.  The caller then uses
    |result|, which contains an uninitialized value.
    
    The fix updates SessionMessageWindow::OnWndProc() to always return
    false, which makes the caller use DefWindowProc() to set |result|.
    
    Bug: 1058556
    Change-Id: I2d1876049e27e9043e9b3a690ffc6697369ea255
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295980
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Cr-Commit-Position: refs/heads/master@{#788380}

--- content/browser/screenlock_monitor/screenlock_monitor_device_source_win.cc
+++ content/browser/screenlock_monitor/screenlock_monitor_device_source_win.cc
@@ -50,7 +50,7 @@ bool ScreenlockMonitorDeviceSource::SessionMessageWindow::OnWndProc(
   if (message == WM_WTSSESSION_CHANGE) {
     ProcessWTSSessionLockMessage(wparam);
   }
-  return true;
+  return false;
 }
 
 void ScreenlockMonitorDeviceSource::SessionMessageWindow::
