f87b564c4d961f1491e8e97b11de970e679f72e0
Antonio Gomes <tonikitoo@igalia.com>
2020-07-14 12:57:17 +0000

    Switch MediaStreamVideoTrack away from std::vector
    ... in favor of WTF::Vector.
    
    BUG=704136
    R=guidou@chromium.org
    
    Change-Id: I09d6c7bb078e972e04f89200729bfb5c382dd7f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292806
    Auto-Submit: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788112}

--- third_party/blink/renderer/modules/mediastream/media_stream_video_track.h
+++ third_party/blink/renderer/modules/mediastream/media_stream_video_track.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_MEDIASTREAM_MEDIA_STREAM_VIDEO_TRACK_H_
 
 #include <memory>
-#include <vector>
 
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/modules/mediastream/encoded_video_frame.h"
 #include "third_party/blink/public/web/modules/mediastream/media_stream_video_source.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
 
@@ -171,8 +171,8 @@ class MODULES_EXPORT MediaStreamVideoTrack
   // or data flow changes are being called on the main thread.
   THREAD_CHECKER(main_render_thread_checker_);
 
-  std::vector<WebMediaStreamSink*> sinks_;
-  std::vector<WebMediaStreamSink*> encoded_sinks_;
+  Vector<WebMediaStreamSink*> sinks_;
+  Vector<WebMediaStreamSink*> encoded_sinks_;
 
   // |FrameDeliverer| is an internal helper object used for delivering video
   // frames on the IO-thread using callbacks to all registered tracks.
