44c6daf0805efca6552da9b34948efbfc80184a2
Jordan Bayles <jophba@chromium.org>
2020-07-14 22:40:39 +0000

    [Mirroring] Delete the CastRemotingSender
    This patch fulfills the various TODOs in the attached bug by deleting
    the entire class, since it is effectively no longer used outside of
    unit testing.
    
    Bug: 1015477
    
    Change-Id: I1ff22b0d9c8357c8bf75286f2f247fb2092d0eae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296010
    Reviewed-by: Yuri Wiitala <miu@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788382}

--- chrome/browser/media/cast_remoting_connector.cc
+++ chrome/browser/media/cast_remoting_connector.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/router/media_router_feature.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
-#include "components/mirroring/browser/cast_remoting_sender.h"
 #include "components/prefs/pref_service.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "content/public/browser/browser_thread.h"
