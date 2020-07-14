b71b7fc2732e7b7e07f4081cce5c896fbe71221d
Natalie Chouinard <chouinard@chromium.org>
2020-07-14 17:07:55 +0000

    Use default preference layout in About Chrome
    Switch the About Chrome OS Version preference from a custom layout to
    the default, with the extra text view for a deprecated OS warning
    inflated and attached only when needed.
    
    This fixes an issue with incorrect layout of this preference for RTL
    languages. See bug for screenshots.
    
    Bug: 1102862
    Change-Id: I2b58ce230cde8f8f0399925cda0e092cef01e859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2292971
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Natalie Chouinard <chouinard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788214}

--- chrome/android/java/src/org/chromium/chrome/browser/about_settings/AboutChromePreferenceOSVersion.java
+++ chrome/android/java/src/org/chromium/chrome/browser/about_settings/AboutChromePreferenceOSVersion.java
@@ -7,7 +7,8 @@ package org.chromium.chrome.browser.about_settings;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
@@ -25,7 +26,6 @@ public class AboutChromePreferenceOSVersion extends Preference {
      */
     public AboutChromePreferenceOSVersion(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setLayoutResource(R.layout.os_version_unsupported_preference);
     }
 
     @Override
@@ -34,6 +34,7 @@ public class AboutChromePreferenceOSVersion extends Preference {
         // Show additional information only if the OS version is not supported.
         if (VersionNumberGetter.isCurrentOsVersionSupported()) return;
 
-        holder.findViewById(R.id.os_deprecation_warning).setVisibility(View.VISIBLE);
+        ViewGroup root = (ViewGroup) holder.findViewById(android.R.id.summary).getParent();
+        LayoutInflater.from(getContext()).inflate(R.layout.os_version_unsupported_text, root, true);
     }
 }
