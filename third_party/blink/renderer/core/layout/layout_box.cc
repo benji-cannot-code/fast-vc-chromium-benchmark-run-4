7570cbc06a1902247abc821b94ec0f4cbb7a2c09
Xianzhu Wang <wangxianzhu@chromium.org>
2020-07-14 18:35:50 +0000

    Don't include empty rects when calculating bounding box of outlines
    Years ago we painted outlines around empty rects, so we needed
    UnionRectEvenIfEmpty() when calculating bounding box of outlines.
    Now we don't need it. Remove to simplify code.
    
    Bug: 1104064
    Change-Id: I2f0b6c1ee7bce470294605abf457c8a2f1c3e2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296250
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788270}

--- third_party/blink/renderer/core/layout/layout_box.cc
+++ third_party/blink/renderer/core/layout/layout_box.cc
@@ -5921,7 +5921,7 @@ LayoutRectOutsets LayoutBox::ComputeVisualEffectOverflowOutsets() {
   if (style.HasOutline()) {
     Vector<PhysicalRect> outline_rects = OutlineRects(
         PhysicalOffset(), OutlineRectsShouldIncludeBlockVisualOverflow());
-    PhysicalRect rect = UnionRectEvenIfEmpty(outline_rects);
+    PhysicalRect rect = UnionRect(outline_rects);
     bool outline_affected = rect.size != PhysicalSizeToBeNoop(Size());
     SetOutlineMayBeAffectedByDescendants(outline_affected);
     rect.Inflate(LayoutUnit(style.OutlineOutsetExtent()));
