e24e6efc42105bc1c619fe3acfb64bef4d9cd374
Lijin Shen <lazzzis@google.com>
2020-07-14 16:08:32 +0000

    Add a param for default browser promo to allow promo with url launching
    1. Remove intent data if this intent is sent out by default browser
    promo utils to prevent from launching a new tab.
    2. Promo with different category and action if url is provided.
    
    Bug: 1090103
    Change-Id: Icd982617d637f812511eb3872a005076f9882df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295207
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788186}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -863,6 +863,7 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
             return;
         }
 
+        DefaultBrowserPromoUtils.maybeRemoveIntentData(intent);
         mIntentHandlingTimeMs = SystemClock.uptimeMillis();
         super.onNewIntent(intent);
     }
