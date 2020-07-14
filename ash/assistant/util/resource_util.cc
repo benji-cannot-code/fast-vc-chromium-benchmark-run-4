8d8874e53cbb132551427a33fb88b551c657ee12
David Black <dmblack@google.com>
2020-07-14 20:26:57 +0000

    Add icons for Assistant onboarding.
    Default mode: http://shortn/_hj9BYfnLUH
    EDU mode: http://shortn/_N00fMwVfWD
    
    Bug: b:2289074
    Change-Id: I0c8a052c4ff2e3f25b74ba6d84081c488350be28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290952
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/master@{#788323}

--- ash/assistant/util/resource_util.cc
+++ ash/assistant/util/resource_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/assistant/util/resource_util.h"
 
+#include <map>
 #include <sstream>
 #include <string>
 
@@ -35,6 +36,14 @@ constexpr char kIconResourceType[] = "icon";
 
 // Icon names.
 constexpr char kAssistantIconName[] = "assistant";
+constexpr char kCalculateIconName[] = "calculate";
+constexpr char kConversionPathIconName[] = "conversationPath";
+constexpr char kPersonPinCircleIconName[] = "personPinCircle";
+constexpr char kScreenshotIconName[] = "screenshot";
+constexpr char kSentimentVerySatisfiedIconName[] = "sentimentVerySatisfied";
+constexpr char kStraightenIconName[] = "straighten";
+constexpr char kTimerIconName[] = "timer";
+constexpr char kTranslateIconName[] = "translate";
 
 // Helpers ---------------------------------------------------------------------
 
@@ -46,8 +55,20 @@ SkColor ToColor(const std::string& aarrggbb) {
 }
 
 IconName ToIconName(const std::string& name) {
-  DCHECK_EQ(name, kAssistantIconName);
-  return IconName::kAssistant;
+  const std::map<std::string, IconName> icon_names_by_string_name = {
+      {kAssistantIconName, IconName::kAssistant},
+      {kCalculateIconName, IconName::kCalculate},
+      {kConversionPathIconName, IconName::kConversionPath},
+      {kPersonPinCircleIconName, IconName::kPersonPinCircle},
+      {kScreenshotIconName, IconName::kScreenshot},
+      {kSentimentVerySatisfiedIconName, IconName::kSentimentVerySatisfied},
+      {kStraightenIconName, IconName::kStraighten},
+      {kTimerIconName, IconName::kTimer},
+      {kTranslateIconName, IconName::kTranslate},
+  };
+  auto it = icon_names_by_string_name.find(name);
+  DCHECK(it != icon_names_by_string_name.end());
+  return it->second;
 }
 
 std::string ToString(SkColor color) {
@@ -60,6 +81,22 @@ std::string ToString(IconName name) {
   switch (name) {
     case IconName::kAssistant:
       return kAssistantIconName;
+    case IconName::kCalculate:
+      return kCalculateIconName;
+    case IconName::kConversionPath:
+      return kConversionPathIconName;
+    case IconName::kPersonPinCircle:
+      return kPersonPinCircleIconName;
+    case IconName::kScreenshot:
+      return kScreenshotIconName;
+    case IconName::kSentimentVerySatisfied:
+      return kSentimentVerySatisfiedIconName;
+    case IconName::kStraighten:
+      return kStraightenIconName;
+    case IconName::kTimer:
+      return kTimerIconName;
+    case IconName::kTranslate:
+      return kTranslateIconName;
   }
   NOTREACHED();
   return std::string();
@@ -99,6 +136,22 @@ const gfx::VectorIcon& ToVectorIcon(IconName name) {
   switch (name) {
     case IconName::kAssistant:
       return ash::kAssistantIcon;
+    case IconName::kCalculate:
+      return ash::kCalculateIcon;
+    case IconName::kConversionPath:
+      return ash::kConversionPathIcon;
+    case IconName::kPersonPinCircle:
+      return ash::kPersonPinCircleIcon;
+    case IconName::kScreenshot:
+      return ash::kScreenshotIcon;
+    case IconName::kSentimentVerySatisfied:
+      return ash::kSentimentVerySatisfiedIcon;
+    case IconName::kStraighten:
+      return ash::kStraightenIcon;
+    case IconName::kTimer:
+      return ash::kTimerIcon;
+    case IconName::kTranslate:
+      return ash::kTranslateIcon;
   }
   NOTREACHED();
   return gfx::kNoneIcon;
