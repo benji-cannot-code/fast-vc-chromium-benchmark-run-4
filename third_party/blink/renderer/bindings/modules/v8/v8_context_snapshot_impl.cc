ec896f93bd5d78c2a3be272b719157fd55e55da2
Yuki Shiino <yukishiino@chromium.org>
2020-07-14 13:00:07 +0000

    bind-gen: Fix wrapper association in V8ContextSnapshotImpl
    Fixes the wrapper association of window.document in
    V8ContextSnapshotImpl.
    
    Bug: 839389
    Change-Id: I87729a96feeabf290af054d389b8ae51679c9b30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2295380
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788117}

--- third_party/blink/renderer/bindings/modules/v8/v8_context_snapshot_impl.cc
+++ third_party/blink/renderer/bindings/modules/v8/v8_context_snapshot_impl.cc
@@ -131,6 +131,7 @@ struct DeserializerData {
 
  public:
   v8::Isolate* isolate;
+  const DOMWrapperWorld& world;
   HTMLDocument* html_document;
 };
 
@@ -207,13 +208,19 @@ void DeserializeInternalFieldCallback(v8::Local<v8::Object> object,
       reinterpret_cast<DeserializerData*>(data);
 
   switch (static_cast<InternalFieldSerializedValue>(value)) {
-    case InternalFieldSerializedValue::kSwHTMLDocument:
+    case InternalFieldSerializedValue::kSwHTMLDocument: {
       CHECK_EQ(index, kV8DOMWrapperObjectIndex);
       CHECK(deserializer_data->html_document);
+      CHECK(deserializer_data->world.IsMainWorld());
       V8DOMWrapper::SetNativeInfo(deserializer_data->isolate, object,
                                   V8HTMLDocument::GetWrapperTypeInfo(),
                                   deserializer_data->html_document);
+      bool result = deserializer_data->html_document->SetWrapper(
+          deserializer_data->isolate, V8HTMLDocument::GetWrapperTypeInfo(),
+          object);
+      CHECK(result);
       break;
+    }
     case InternalFieldSerializedValue::kSwWindow:
       CHECK_EQ(index, kV8DOMWrapperObjectIndex);
       // The global object's internal fields will be set in LocalWindowProxy.
@@ -221,9 +228,9 @@ void DeserializeInternalFieldCallback(v8::Local<v8::Object> object,
     case InternalFieldSerializedValue::kWtiHTMLDocument:
       CHECK_EQ(index, kV8DOMWrapperTypeIndex);
       CHECK(deserializer_data->html_document);
-      V8DOMWrapper::SetNativeInfo(deserializer_data->isolate, object,
-                                  V8HTMLDocument::GetWrapperTypeInfo(),
-                                  deserializer_data->html_document);
+      CHECK(deserializer_data->world.IsMainWorld());
+      // The internal field of WrapperTypeInfo must be filled in
+      // kSwHTMLDocument case.
       break;
     case InternalFieldSerializedValue::kWtiWindow:
       CHECK_EQ(index, kV8DOMWrapperTypeIndex);
@@ -314,7 +321,7 @@ v8::Local<v8::Context> V8ContextSnapshotImpl::CreateContext(
     html_document = nullptr;
   }
 
-  DeserializerData deserializer_data = {isolate, html_document};
+  DeserializerData deserializer_data = {isolate, world, html_document};
   v8::DeserializeInternalFieldsCallback internal_field_desrializer(
       DeserializeInternalFieldCallback, &deserializer_data);
   return v8::Context::FromSnapshot(
