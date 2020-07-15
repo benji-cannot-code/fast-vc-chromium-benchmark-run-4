9d84671ab402320af91d4f57701d8ffe0d9de098
Stepan Khapugin <stkhapugin@chromium.org>
2020-07-15 17:19:55 +0000

    [multiball] Remove sceneController property.
    This property was used before there was an "official" way
    to access scene controllers. Now it can be fetched through
    the app state.
    
    Change-Id: I11abb2508323000b2ed317f9ad2de12f748510d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299631
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788695}

--- ios/chrome/app/main_application_delegate.mm
+++ ios/chrome/app/main_application_delegate.mm
@@ -88,7 +88,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // TODO(crbug.com/1040501): remove this.
       // This is temporary plumbing that's not supposed to be here.
       _sceneController.mainController = (id<MainControllerGuts>)_mainController;
-      _mainController.sceneController = _sceneController;
       _tabSwitcherProtocol = _sceneController;
       _tabOpener = _sceneController;
     }
--- ios/chrome/app/main_controller.h
+++ ios/chrome/app/main_controller.h
@@ -12,16 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/application_delegate/browser_launcher.h"
 #import "ios/chrome/app/application_delegate/startup_information.h"
 #import "ios/chrome/app/main_controller_guts.h"
-#import "ios/chrome/browser/ui/commands/application_commands.h"
 #import "ios/chrome/browser/ui/commands/browsing_data_commands.h"
 
 @class AppState;
 @class MetricsMediator;
 @protocol BrowsingDataCommands;
-@protocol ConnectionInformation;
-@protocol SceneControllerGuts;
-@protocol TabOpening;
-@protocol TabSwitcherDelegate;
 
 // The main controller of the application, owned by the MainWindow nib. Also
 // serves as the delegate for the app. Owns all the various top-level
@@ -45,14 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This metrics mediator is used to check and update the metrics accordingly to
 // to the user preferences.
 @property(nonatomic, weak) MetricsMediator* metricsMediator;
-
-// For temporary plumbing only.
-@property(nonatomic, weak) id<ApplicationCommands,
-                              TabSwitcherDelegate,
-                              ConnectionInformation,
-                              SceneControllerGuts,
-                              TabOpening>
-    sceneController;
 
 @end
 
