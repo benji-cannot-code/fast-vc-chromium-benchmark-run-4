b3e2f196d71c5ae6fd2174e7d076ab160211fdad
Leonid Baraz <lbaraz@chromium.org>
2020-07-15 20:48:47 +0000

    Add Flush API to Storage
    Bug: b:161266545
    Change-Id: I57f47b6c8acff90de983ca4c553b6f4fdacfba0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2296636
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Zach Trudo <zatrudo@google.com>
    Cr-Commit-Position: refs/heads/master@{#788768}

--- chrome/browser/policy/messaging_layer/storage/storage_queue.cc
+++ chrome/browser/policy/messaging_layer/storage/storage_queue.cc
@@ -151,7 +151,7 @@ Status StorageQueue::Init() {
   // Initiate periodic uploading, if needed.
   if (!options_.upload_period().is_zero()) {
     upload_timer_.Start(FROM_HERE, options_.upload_period(), this,
-                        &StorageQueue::PeriodicUpload);
+                        &StorageQueue::Flush);
   }
   return Status::StatusOK();
 }
@@ -565,18 +565,6 @@ class StorageQueue::ReadContext : public TaskRunnerContext<Status> {
   SEQUENCE_CHECKER(read_sequence_checker_);
 };
 
-void StorageQueue::PeriodicUpload() {
-  // Note: new uploader created every time PeriodicUpload is called.
-  StatusOr<std::unique_ptr<UploaderInterface>> uploader =
-      start_upload_cb_.Run();
-  if (!uploader.ok()) {
-    LOG(ERROR) << "Failed to provide the Uploader, status="
-               << uploader.status();
-    return;
-  }
-  Start<ReadContext>(std::move(uploader.ValueOrDie()), this);
-}
-
 class StorageQueue::WriteContext : public TaskRunnerContext<Status> {
  public:
   WriteContext(base::span<const uint8_t> data,
@@ -779,6 +767,18 @@ Status StorageQueue::RemoveUnusedFiles(uint64_t seq_number) {
   }
   // Even if there were errors, ignore them.
   return Status::StatusOK();
+}
+
+void StorageQueue::Flush() {
+  // Note: new uploader created every time Flush is called.
+  StatusOr<std::unique_ptr<UploaderInterface>> uploader =
+      start_upload_cb_.Run();
+  if (!uploader.ok()) {
+    LOG(ERROR) << "Failed to provide the Uploader, status="
+               << uploader.status();
+    return;
+  }
+  Start<ReadContext>(std::move(uploader.ValueOrDie()), this);
 }
 
 //
