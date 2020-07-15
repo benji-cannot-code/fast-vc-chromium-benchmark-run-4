96d7428f4c2a886c16715f3894887fd26ea6a35e
Hajime Hoshi <hajimehoshi@chromium.org>
2020-07-15 02:10:15 +0000

    Revert "Remove DisableGPU flag in PluginPowerSaver browser test"
    This reverts commit a8517b723d5fd751d58ed5c22a48d69684ea2092.
    
    Reason for revert: Unit tests failure: crbug.com/1105711
    
    Original change's description:
    > Remove DisableGPU flag in PluginPowerSaver browser test
    > 
    > It's not clear why this should cause flakes, this will allow running
    > the tests on ChromeOS.
    > 
    > Change-Id: Ife1867798873ca7233b2c2eb2ac56868c0fe2971
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2290271
    > Reviewed-by: Tommy Li <tommycli@chromium.org>
    > Reviewed-by: danakj <danakj@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#788319}
    
    TBR=danakj@chromium.org,bokan@chromium.org,tommycli@chromium.org
    
    Change-Id: I38dc00b297fd723d45f55b2b9384c2f9c4ac57dd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2298762
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788451}

--- chrome/browser/plugins/plugin_power_saver_browsertest.cc
+++ chrome/browser/plugins/plugin_power_saver_browsertest.cc
@@ -342,6 +342,13 @@ class PluginPowerSaverPixelTest : public PluginPowerSaverBrowserTest {
     PluginPowerSaverBrowserTest::SetUp();
   }
 
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    // The pixel tests run more reliably in software mode.
+    command_line->AppendSwitch(switches::kDisableGpu);
+
+    PluginPowerSaverBrowserTest::SetUpCommandLine(command_line);
+  }
+
  protected:
   void LoadHTML(const std::string& file) override {
     gfx::Rect bounds(gfx::Rect(0, 0, kBrowserWidth, kBrowserHeight));
@@ -357,8 +364,10 @@ class PluginPowerSaverPixelTest : public PluginPowerSaverBrowserTest {
 
 // This test fail on macOS 10.12. https://crbug.com/599484.
 // Flaky on Windows, Asan, and Msan. See crbug.com/549285 and crbug.com/512140.
+// Because ChromeOS cannot use software rendering and the pixel tests continue
+// to flake with hardware acceleration, disable these on ChromeOS.
 #if defined(OS_MACOSX) || defined(OS_WIN) || defined(ADDRESS_SANITIZER) || \
-    defined(MEMORY_SANITIZER)
+    defined(MEMORY_SANITIZER) || defined(OS_CHROMEOS)
 #define MAYBE_SmallCrossOrigin DISABLED_SmallCrossOrigin
 #else
 #define MAYBE_SmallCrossOrigin SmallCrossOrigin
@@ -380,7 +389,10 @@ IN_PROC_BROWSER_TEST_F(PluginPowerSaverPixelTest, MAYBE_SmallCrossOrigin) {
 }
 
 // Flaky on Windows, Asan, and Msan. See crbug.com/549285 and crbug.com/512140.
-#if defined(OS_WIN) || defined(ADDRESS_SANITIZER) || defined(MEMORY_SANITIZER)
+// Because ChromeOS cannot use software rendering and the pixel tests continue
+// to flake with hardware acceleration, disable these on ChromeOS.
+#if defined(OS_WIN) || defined(ADDRESS_SANITIZER) || \
+    defined(MEMORY_SANITIZER) || defined(OS_CHROMEOS)
 #define MAYBE_SmallerThanPlayIcon DISABLED_SmallerThanPlayIcon
 #else
 #define MAYBE_SmallerThanPlayIcon SmallerThanPlayIcon
@@ -402,8 +414,10 @@ IN_PROC_BROWSER_TEST_F(PluginPowerSaverPixelTest, MAYBE_SmallerThanPlayIcon) {
 
 // This test fail on macOS 10.12. https://crbug.com/599484.
 // Flaky on Windows, Asan, and Msan. See crbug.com/549285 and crbug.com/512140.
+// Because ChromeOS cannot use software rendering and the pixel tests continue
+// to flake with hardware acceleration, disable these on ChromeOS.
 #if defined(OS_MACOSX) || defined(OS_WIN) || defined(ADDRESS_SANITIZER) || \
-    defined(MEMORY_SANITIZER)
+    defined(MEMORY_SANITIZER) || defined(OS_CHROMEOS)
 #define MAYBE_PosterTests DISABLED_PosterTests
 #else
 #define MAYBE_PosterTests PosterTests
