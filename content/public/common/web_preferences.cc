7eb0689dc783d5dfde4d3b11c1635c4a67e904fd
Collin Baker <collinbaker@chromium.org>
2020-07-15 16:44:35 +0000

    Enable touch drag-and-drop in WebUI tab strip
    Touch drag-and-drop is enabled or disabled through WebPreferences
    instead of a switch check in the renderer. The setting defaults to
    the switch state but can be overridden.
    
    The WebUI tab strip enables it always.
    
    Bug: 229301
    Change-Id: Ie3be6167f3c3c5d1fe21beb0238d6b2e0bdf05fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2270848
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788679}

--- content/public/common/web_preferences.cc
+++ content/public/common/web_preferences.cc
@@ -9,8 +9,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "third_party/blink/public/web/web_settings.h"
+#include "ui/base/ui_base_switches_util.h"
 
 using blink::WebSettings;
+
+namespace {
+
+bool IsTouchDragDropEnabled() {
+  // Cache the enabled state so it isn't queried on every WebPreferences
+  // creation. Note that this means unit tests can't override the state.
+  static const bool enabled = switches::IsTouchDragDropEnabled();
+  return enabled;
+}
+
+}  // namespace
 
 namespace content {
 
@@ -229,7 +241,8 @@ WebPreferences::WebPreferences()
       network_quality_estimator_web_holdback(
           net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN),
       allow_mixed_content_upgrades(true),
-      always_show_focus(false) {
+      always_show_focus(false),
+      touch_drag_drop_enabled(IsTouchDragDropEnabled()) {
   standard_font_family_map[kCommonScript] =
       base::ASCIIToUTF16("Times New Roman");
   fixed_font_family_map[kCommonScript] = base::ASCIIToUTF16("Courier New");
