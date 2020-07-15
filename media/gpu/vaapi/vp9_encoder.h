378cde3a4a8a871e9a47453b69fa9d5db1ffb184
Hirokazu Honda <hiroh@chromium.org>
2020-07-15 04:13:48 +0000

    media/gpu/vaapi/VaapiVEA: Support VP9 temporal layer encoding
    This enables VaapiVEA to produce temporal layer bitstream.
    The temporal layer bitstream structure is defined in
    VP9TemporalLayer class. It now supports two and three temporal
    layer structures. VA-API encoder driver works in CQP mode only
    if temporal layer encoding because VA-API driver on gen 9 devices
    doesn't support bitrate control of each bitrate layer in CBR mode.
    
    Design doc: go/vaapi-vp9-TL
    
    Bug: 1030199
    Test: vp9_encoder_unittest
    Test: appr.tc/?debug=loopback&vsc=vp9&vrc=vp9 with --force-fieldtrials=WebRTC-SupportVP9SVC/EnabledByFlag_1SL3TL/ --enable-features=VaapiVP9Encoder
    Change-Id: Ie46b4b52730c59684ea00c27bdf1afaaf07bf991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2150525
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788485}

--- media/gpu/vaapi/vp9_encoder.h
+++ media/gpu/vaapi/vp9_encoder.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/vp9_reference_frame_vector.h"
 
 namespace media {
+class VP9TemporalLayers;
 class VP9RateControl;
 
 class VP9Encoder : public AcceleratedVideoEncoder {
@@ -95,14 +96,18 @@ class VP9Encoder : public AcceleratedVideoEncoder {
   ScalingSettings GetScalingSettings() const override;
   bool PrepareEncodeJob(EncodeJob* encode_job) override;
   void BitrateControlUpdate(uint64_t encoded_chunk_size_bytes) override;
+  BitstreamBufferMetadata GetMetadata(EncodeJob* encode_job,
+                                      size_t payload_size) override;
 
  private:
   friend class VP9EncoderTest;
 
   void set_rate_ctrl_for_testing(std::unique_ptr<VP9RateControl> rate_ctrl);
 
-  void InitializeFrameHeader();
-  void UpdateFrameHeader(bool keyframe);
+  Vp9FrameHeader GetDefaultFrameHeader(const bool keyframe) const;
+  void SetFrameHeader(bool keyframe,
+                      VP9Picture* picture,
+                      std::array<bool, kVp9NumRefsPerFrame>* ref_frames_used);
   void UpdateReferenceFrames(scoped_refptr<VP9Picture> picture);
   void Reset();
 
@@ -115,8 +120,8 @@ class VP9Encoder : public AcceleratedVideoEncoder {
 
   EncodeParams current_params_;
 
-  Vp9FrameHeader current_frame_hdr_;
   Vp9ReferenceFrameVector reference_frames_;
+  std::unique_ptr<VP9TemporalLayers> temporal_layers_;
 
   std::unique_ptr<VP9RateControl> rate_ctrl_;
   const std::unique_ptr<Accelerator> accelerator_;
@@ -124,7 +129,6 @@ class VP9Encoder : public AcceleratedVideoEncoder {
   SEQUENCE_CHECKER(sequence_checker_);
   DISALLOW_COPY_AND_ASSIGN(VP9Encoder);
 };
-
 }  // namespace media
 
 #endif  // MEDIA_GPU_VAAPI_VP9_ENCODER_H_
