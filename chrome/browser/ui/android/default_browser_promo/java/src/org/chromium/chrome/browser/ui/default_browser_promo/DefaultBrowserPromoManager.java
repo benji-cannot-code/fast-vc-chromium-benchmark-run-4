ccb8eee6c87d01a4b3230de8439b5e0b119f5b2a
Lijin Shen <lazzzis@google.com>
2020-07-15 22:24:10 +0000

    Add suffix to promo intent key in default browser promo
    Add a suffix to the key of intent data to distinguish intent from
    another chrome channel. Otherwise, intent from another chrome channel
    leads to an unnecessary metrics logging.
    
    Bug: 1090103
    Change-Id: Iead382e1c052759da2c491c4d87bf61d142698f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2299523
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa  <twellington@chromium.org>
    Reviewed-by: Pavel Yatsuk <pavely@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788781}

--- chrome/browser/ui/android/default_browser_promo/java/src/org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoManager.java
+++ chrome/browser/ui/android/default_browser_promo/java/src/org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoManager.java
@@ -159,7 +159,7 @@ public class DefaultBrowserPromoManager implements PauseResumeWithNativeObserver
                 intent.setAction(Intent.ACTION_MAIN);
                 intent.addCategory(Intent.CATEGORY_APP_BROWSER);
             }
-            intent.putExtra(DefaultBrowserPromoUtils.DISAMBIGUATION_SHEET_PROMOED_KEY, true);
+            intent.putExtra(DefaultBrowserPromoUtils.getDisambiguationSheetPromoedKey(), true);
             mActivity.startActivity(intent);
         });
     }
