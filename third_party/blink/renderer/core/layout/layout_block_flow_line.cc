a102a4fe77d68988404c87f0dadff372f2a71103
Keishi Hattori <keishi@chromium.org>
2020-07-15 23:15:53 +0000

    Add STACK_ALLOCATED to LayoutObject referencing structs
    Add STACK_ALLOCATED to LayoutObject referencing structs in anticipation of oilpanization of LayoutObjects.
    
    Bug: 1030176
    Change-Id: I81bc2a6db7b6ce891885ed106311299dd6c26b9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2298884
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#788784}

--- third_party/blink/renderer/core/layout/layout_block_flow_line.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow_line.cc
@@ -1459,6 +1459,9 @@ void LayoutBlockFlow::MarkDirtyFloatsForPaintInvalidation(
 //     can have distinct borders/margin/padding that contribute to the min/max
 //     width.
 struct InlineMinMaxIterator {
+  STACK_ALLOCATED();
+
+ public:
   LayoutObject* parent;
   LayoutObject* current;
   bool end_of_inline;
